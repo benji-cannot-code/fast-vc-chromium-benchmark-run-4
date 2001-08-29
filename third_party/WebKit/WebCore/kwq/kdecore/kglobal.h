19aca3b0e136050e407d3b4587b00eff95f93052
gramps <gramps@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-08-29 15:43:05 +0000

    Added predeclaration of KCharsets
    git-svn-id: svn://svn.chromium.org/blink/trunk@40 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/kdecore/kglobal.h
+++ third_party/WebKit/WebCore/kwq/kdecore/kglobal.h
@@ -27,9 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef KGLOBAL_H_
 #define KGLOBAL_H_
 
+class KCharsets;
+
 class KGlobal {
 public:
-	static KCharsets *charsets();
+    static KCharsets *charsets();
 };
 
 #endif
