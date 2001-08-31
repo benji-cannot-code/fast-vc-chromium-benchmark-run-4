aeb6bd9711c9a4c69e31b6f9e53756251724028c
gramps <gramps@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-08-31 19:18:08 +0000

    More changes to get this beast to compile
    git-svn-id: svn://svn.chromium.org/blink/trunk@60 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kwq/qt/qtextcodec.h
+++ third_party/WebKit/WebCore/src/kwq/qt/qtextcodec.h
@@ -32,19 +32,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class QTextDecoder {
 public:
-    virtual QString toUnicode(const char* chars, int len) = 0;
+    virtual QString toUnicode(const char *, int) = 0;
 };
 
 class QTextCodec {
 public:
-    static QTextCodec *codecForMib(int mib);
-    static QTextCodec *codecForName(const char* hint, int accuracy=0);
+    static QTextCodec *codecForMib(int);
+    static QTextCodec *codecForName(const char *, int accuracy = 0);
+    static QTextCodec *codecForLocale();
 
+    virtual const char* name() const = 0;
     virtual int mibEnum() const = 0;
     virtual QTextDecoder *makeDecoder() const;
-    virtual QString toUnicode(const char *chars, int len) const;
-    QString toUnicode(const QByteArray&, int len) const;
-    QString toUnicode(const char *chars) const;
+    QCString fromUnicode(const QString &) const;
+    virtual QString toUnicode(const char *, int) const;
+    QString toUnicode(const QByteArray &, int) const;
+    QString toUnicode(const char *) const;
 };
 
 #endif
