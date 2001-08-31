c134fab8c7b7536122d10ce15659b1900ec2c00f
gramps <gramps@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-08-31 19:24:11 +0000

    More changes to get this beast to compile
    git-svn-id: svn://svn.chromium.org/blink/trunk@62 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kwq/qt/qarray.h
+++ third_party/WebKit/WebCore/src/kwq/qt/qarray.h
@@ -31,13 +31,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 template <class T> class QArray {
 public:
+    QArray(int);
     T &at(uint) const;
     T *data() const;
 
     uint size() const;
-    bool resize(uint size);
+    bool resize(uint);
     
-    T& operator[](int i) const;
+    T &operator[](int) const;
 };
 
 #endif
