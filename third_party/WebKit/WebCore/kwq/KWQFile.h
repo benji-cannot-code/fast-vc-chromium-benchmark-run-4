267a66136dd823be25fe7f80485cf8588b0e4f43
gramps <gramps@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-08-31 15:38:18 +0000

    More changes to get this beast to compile
    git-svn-id: svn://svn.chromium.org/blink/trunk@58 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQFile.h
+++ third_party/WebKit/WebCore/kwq/KWQFile.h
@@ -27,20 +27,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef QFILE_H_
 #define QFILE_H_
 
-class QString;
-
-QString locate(const char *type, const QString& filename, const KInstance* instance = KGlobal::instance());
+#include "qstring.h"
 
 class QFile {
 public:
     QFile();
-    QFile(const QString &name);
-
+    QFile(const QString &);
     bool open(int);
     void close();
     uint size() const;
-
-    int readBlock(char *data, uint len);
+    int readBlock(char *, uint);
 };
 
 #endif
--- third_party/WebKit/WebCore/kwq/KWQKStandardDirs.h
+++ third_party/WebKit/WebCore/kwq/KWQKStandardDirs.h
@@ -27,9 +27,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef KSTDDIRS_H_
 #define KSTDDIRS_H_
 
+#include <qstring.h>
+#include "kglobal.h"
+
 class KStandardDirs {
 public:
-    QString saveLocation(const char *type, const QString& suffix = QString::null, bool create = true) const;
+    QString saveLocation(const char *, const QString &suffix = QString::null, bool create = true) const;
 };
+
+QString locate(const char *type, const QString& filename, const KInstance* instance = KGlobal::instance());
 
 #endif
