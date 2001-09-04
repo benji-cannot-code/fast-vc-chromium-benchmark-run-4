28fb8948800a5c46f789ab53b6625e3d041a89cf
gramps <gramps@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-09-04 23:09:03 +0000

    Include qcursor.h to workaround bug in khtml/rendering/render_frames.cpp
    git-svn-id: svn://svn.chromium.org/blink/trunk@84 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQWidget.h
+++ third_party/WebKit/WebCore/kwq/KWQWidget.h
@@ -35,6 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qstyle.h"
 #include "qfont.h"
 
+// FIXME: include to workaround bug in khtml/rendering/render_frames.cpp
+#include "qcursor.h"
+
 class QWidget : public QObject, public QPaintDevice {
 public:
     // FIXME: do any of these methods need to be virtual?
