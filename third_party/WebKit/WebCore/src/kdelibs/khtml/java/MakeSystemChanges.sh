b2d15932c6a92c68cfc344a88d2782ccd96f9199
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-09-04 16:45:03 +0000

    Added some more complete dependency checking to the build system
    git-svn-id: svn://svn.chromium.org/blink/trunk@71 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kdelibs/khtml/java/MakeSystemChanges.sh
+++ third_party/WebKit/WebCore/src/kdelibs/khtml/java/MakeSystemChanges.sh
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/sh
 
-FILES='Makefile.am configure.in.in'
+FILES='Makefile.am configure.in.in javaembed.cpp'
 for file in $FILES; do
         if (test -f "$file"); then
                 A=`grep APPLE $file`
