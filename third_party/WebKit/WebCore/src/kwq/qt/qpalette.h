0d575844f6b8fd25dec2d670b13c288493fa4f34
gramps <gramps@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-09-05 13:29:18 +0000

    Added color and normal function members
    git-svn-id: svn://svn.chromium.org/blink/trunk@93 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kwq/qt/qpalette.h
+++ third_party/WebKit/WebCore/src/kwq/qt/qpalette.h
@@ -33,7 +33,8 @@ class QColorGroup {
 public:
     enum ColorRole { Foreground, Shadow, Light, Mid, Midlight, Dark, Base, ButtonText, Button, Background, Text };
 
-    void setColor(ColorRole role, const QColor &color);
+    const QColor &color(ColorRole) const;
+    void setColor(ColorRole, const QColor &);
     const QColor &foreground() const;
     const QColor &shadow() const;
     const QColor &light() const;
@@ -55,6 +56,7 @@ public:
     const QColorGroup &active() const;
     const QColorGroup &inactive() const;
     const QColorGroup &disabled() const;
+    const QColorGroup &normal() const;
 }; 
 
 #endif
