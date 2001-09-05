63cb907dc0ae7c885d6a911ed0bd38867e1975c2
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-09-05 23:59:07 +0000

    Fixed the top-level #define directives for the header files
    git-svn-id: svn://svn.chromium.org/blink/trunk@111 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQIcon.h
+++ third_party/WebKit/WebCore/kwq/KWQIcon.h
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#ifndef KICONTHEME_H_
-#define KICONTHEME_H_
+#ifndef KWQICON_H_
+#define KWQICON_H_
 
 class KIcon {
 public:
--- third_party/WebKit/WebCore/src/kwq/KWQCollection.h
+++ third_party/WebKit/WebCore/src/kwq/KWQCollection.h
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#ifndef QCOLLECTION_H_
-#define QCOLLECTION_H_
+#ifndef KWQCOLLECTION_H_
+#define KWQCOLLECTION_H_
 
 class QCollection {
 public:
