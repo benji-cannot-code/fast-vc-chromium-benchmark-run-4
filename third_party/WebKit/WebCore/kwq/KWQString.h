da69c2937ad81ac06796a3dd7fe4da3970f88a3f
gramps <gramps@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-09-10 16:27:48 +0000

    Begin implementation
    git-svn-id: svn://svn.chromium.org/blink/trunk@120 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQString.h
+++ third_party/WebKit/WebCore/kwq/KWQString.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef QSTRING_H_
 #define QSTRING_H_
 
+#include <CoreFoundation/CFString.h>
 #include "qcstring.h"
 
 class QString;
@@ -93,6 +94,11 @@ public:
     friend int operator!=(char, QChar);
     friend int operator!=(QChar, char);
     operator char() const;
+
+    // data members ------------------------------------------------------------
+
+// NOTE: this is NOT private:
+    UniChar c;
 
 // protected -------------------------------------------------------------------
 // private ---------------------------------------------------------------------
@@ -203,6 +209,11 @@ public:
     QString &operator+=(const QString &);
     operator QChar () const;
 
+    // data members ------------------------------------------------------------
+
+// NOTE: this is NOT private:
+    CFMutableStringRef s;
+    
 // protected -------------------------------------------------------------------
 // private ---------------------------------------------------------------------
     
@@ -226,7 +237,7 @@ QString operator+(char, const QString &);
 
 // class QConstString ==========================================================
 
-class QConstString {
+class QConstString : /* NOTE: this is NOT private */ QString {
 public:
 
     // typedefs ----------------------------------------------------------------
