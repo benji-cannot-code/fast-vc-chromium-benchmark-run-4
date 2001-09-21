54844f521d20ec78b40cb56c9f486db8b37f0c56
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-09-21 21:10:59 +0000

    Added some more tests. Added some more collection implementation files.
    git-svn-id: svn://svn.chromium.org/blink/trunk@173 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebCore/kwq/tests/qt/qrect-test.cpp
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#include <iostream>
+
+#include <qrect.h>
+
+int main() {
+
+    QRect r0;
+    QRect r1 = QRect(0,0,100,100);
+    QRect r2 = QRect(100,100,200,200);
+    QRect r3 = r1.unite(r2);
+    QRect r4 = r3;
+    QRect r5 = r3;
+
+    r5.setLeft(10);
+    r5.setTop(10);
+    r5.setRight(20);
+    r5.setBottom(20);
+        
+    cout << r1 << endl;
+    cout << r2 << endl;
+    cout << r3 << endl;
+    cout << r0.isNull() << endl;
+    cout << r1.isNull() << endl;
+    cout << (r1 == r4) << endl;
+    cout << (r3 == r4) << endl;
+    cout << (r1 != r4) << endl;
+    cout << (r3 != r4) << endl;
+    cout << r5 << endl;
+
+    return 0;
+}
