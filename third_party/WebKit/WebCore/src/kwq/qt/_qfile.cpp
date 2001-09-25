53ad2285536a1751a5d28164970fb668969c5afc
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-09-25 23:46:48 +0000

    Now works with -disable-kwq-complete
    git-svn-id: svn://svn.chromium.org/blink/trunk@197 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kwq/qt/_qfile.cpp
+++ third_party/WebKit/WebCore/src/kwq/qt/_qfile.cpp
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "qfile.h"
 
+#ifdef _KWQ_COMPLETE_
+
 extern bool qt_file_access( const QString& fn, int t );
 
 QFile::QFile()
@@ -696,3 +698,5 @@ void QFile::close()
 
     return;
 }
+
+#endif
