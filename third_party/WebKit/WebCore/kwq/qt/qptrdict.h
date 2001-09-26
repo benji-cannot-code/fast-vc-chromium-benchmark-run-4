9869977425a7255d6d8b80b045f43c17705a612f
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-09-26 22:24:05 +0000

    Removed append from qptrdict.h
    git-svn-id: svn://svn.chromium.org/blink/trunk@206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/qt/qptrdict.h
+++ third_party/WebKit/WebCore/kwq/qt/qptrdict.h
@@ -62,7 +62,7 @@ public:
     uint count() const;
     T *take(void *);
 
-    void append(const T *);
+    //void append(const T *);
     void insert(void *, const T *);
     void remove(void *);
 
