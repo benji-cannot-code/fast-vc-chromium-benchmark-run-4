f395547d4dbf8d2e2309b0a99816ef2cd851c362
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-09-28 19:45:44 +0000

    *** empty log message ***
    git-svn-id: svn://svn.chromium.org/blink/trunk@222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/tests/draw/draw.mm
+++ third_party/WebKit/WebCore/kwq/tests/draw/draw.mm
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Classes tested (and needed to link):
  *	QApplication
  *	QBrush
- *	QByteArray
+ *	QByteArray 
  *	QColor
  *	QFont
  *	QFontMetrics
