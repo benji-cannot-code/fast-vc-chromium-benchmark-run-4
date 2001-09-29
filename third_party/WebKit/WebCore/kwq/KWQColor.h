af36f4e94cab27d93b70313dc8a7f8250c88f5f3
gramps <gramps@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-09-29 01:33:19 +0000

    Added hack from qstring.h to prevent Fixed, Rect, and Boolean problem
    git-svn-id: svn://svn.chromium.org/blink/trunk@232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQColor.h
+++ third_party/WebKit/WebCore/kwq/KWQColor.h
@@ -35,7 +35,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qstring.h>
 
 #if (defined(__APPLE__) && defined(__OBJC__) && defined(__cplusplus))
+#define Fixed MacFixed
+#define Rect MacRect
+#define Boolean MacBoolean
 #import <Cocoa/Cocoa.h>
+#undef Fixed
+#undef Rect
+#undef Boolean
 #endif
 
 
--- third_party/WebKit/WebCore/src/kwq/qt/qfont.h
+++ third_party/WebKit/WebCore/src/kwq/qt/qfont.h
@@ -34,7 +34,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class QString;
 
 #if (defined(__APPLE__) && defined(__OBJC__) && defined(__cplusplus))
+#define Fixed MacFixed
+#define Rect MacRect
+#define Boolean MacBoolean
 #import <Cocoa/Cocoa.h>
+#undef Fixed
+#undef Rect
+#undef Boolean
 #endif
 
 // class QFont =================================================================
