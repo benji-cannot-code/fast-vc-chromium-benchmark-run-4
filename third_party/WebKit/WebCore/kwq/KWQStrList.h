4bfbb4088d53bd1aa2dfabc0313538173b2a4ddb
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-10-01 15:11:47 +0000

    Modified to support switchable borrowing of implementation
    git-svn-id: svn://svn.chromium.org/blink/trunk@235 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQStrList.h
+++ third_party/WebKit/WebCore/kwq/KWQStrList.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <config.h>
 #endif
 
+#ifndef USING_BORROWED_QSTRINGLIST
+
 #include <qstring.h>
 #include <qlist.h>
 
@@ -62,4 +64,7 @@ public:
 
 }; // class QStrList ===========================================================
 
+#else // USING_BORROWED_QSTRINGLIST
+// This will help to keep the linker from complaining about empty archives
+void KWQStrList_Dummy() {}
 #endif
