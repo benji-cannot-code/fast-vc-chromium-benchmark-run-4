35aa0b3f5565348fe4c4d612b8206b355e82a1fb
gramps <gramps@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-10-11 01:52:12 +0000

    Redefine Fixed around include of Cocoa.h for build voodoo
    git-svn-id: svn://svn.chromium.org/blink/trunk@309 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKGlobal.mm
+++ third_party/WebKit/WebCore/kwq/KWQKGlobal.mm
@@ -27,7 +27,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <kglobal.h>
 
+#define Fixed MacFixed
+#define Rect MacRect
+#define Boolean MacBoolean
 #include <Cocoa/Cocoa.h>
+#undef Fixed
+#undef Rect
+#undef Boolean
 
 KInstance *KGlobal::instance()
 {
