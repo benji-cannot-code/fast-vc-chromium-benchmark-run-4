2962aa40196fed62e13bfd4a99f98d7988c90cc8
gramps <gramps@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-10-15 19:27:45 +0000

    Modified the stubs so they won't actually explode for now
    git-svn-id: svn://svn.chromium.org/blink/trunk@328 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kwq/KWQKstddirs.mm
+++ third_party/WebKit/WebCore/src/kwq/KWQKstddirs.mm
@@ -25,17 +25,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include <kstddirs.h>
+#include <kwqdebug.h>
 
-
-QString locate(const char *type, const QString& filename, 
-    const KInstance* instance=KGlobal::instance())
+QString locate(const char *type, const QString& filename,
+        const KInstance* instance)
 {
+    _logNotYetImplemented();
+    return QString();
 }
 
-
-QString locateLocal(const char *type, const QString &filename, 
-    const KInstance *instance=KGlobal::instance())
+QString locateLocal(const char *type, const QString &filename,
+        const KInstance *instance)
 {
+    _logNotYetImplemented();
+    return QString();
 }
-
-
