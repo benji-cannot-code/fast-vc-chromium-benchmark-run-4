c18d1a74b51691ab2b750c92793b41af9bf331de
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-10-19 16:54:14 +0000

    Removed a release temporarily.
    git-svn-id: svn://svn.chromium.org/blink/trunk@374 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kwq/KWQColor.mm
+++ third_party/WebKit/WebCore/src/kwq/KWQColor.mm
@@ -96,8 +96,8 @@ QColor::QColor(const QColor &copyFrom)
 {
     if (color == copyFrom.color)
         return;
-    if (color != nil)
-        [color release];
+    //if (color != nil)
+    //    [color release];
     if (copyFrom.color != nil)
         color = [copyFrom.color retain];
     else
