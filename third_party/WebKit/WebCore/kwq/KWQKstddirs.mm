71bf513ed88326c479dcc3066f80bf8ae315ca83
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-10-26 19:02:29 +0000

    locate and locateLocal now return /symroots + filename
    git-svn-id: svn://svn.chromium.org/blink/trunk@388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKstddirs.mm
+++ third_party/WebKit/WebCore/kwq/KWQKstddirs.mm
@@ -30,13 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 QString locate(const char *type, const QString& filename,
         const KInstance* instance)
 {
-    _logNotYetImplemented();
-    return QString();
+    return "/symroots/appdata/"+filename;
 }
 
 QString locateLocal(const char *type, const QString &filename,
         const KInstance *instance)
 {
-    _logNotYetImplemented();
-    return QString();
+    return "/symroots/appdata/"+filename;
 }
