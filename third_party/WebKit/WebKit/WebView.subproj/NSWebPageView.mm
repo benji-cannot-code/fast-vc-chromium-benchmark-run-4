ecdfbddca14aea2f22ebf4e0ce38845459d7e9f5
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-11-01 23:45:34 +0000

    Added template +initialize for Chris.
    git-svn-id: svn://svn.chromium.org/blink/trunk@398 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/NSWebPageView.mm
+++ third_party/WebKit/WebKit/WebView.subproj/NSWebPageView.mm
@@ -5,4 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/NSWebPageView.h>
 
 @implementation NSWebPageView
+
++ (void)initialize {
+#if 0
+    NSAutoreleasePool *localPool;
+    NSUserDefaults *userDefaults;
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+        @"value", @"key",
+    };
+
+    [[NSUserDefaults standardUserDefaults] registerDefaults:dict];
+#endif
+}
+
 @end
