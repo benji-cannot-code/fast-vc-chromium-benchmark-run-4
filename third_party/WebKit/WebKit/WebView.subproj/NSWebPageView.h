8690fe9ae0377de8c4d31bec105bcf056361a476
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-11-02 21:23:50 +0000

    Update framework project to include public headers.
    git-svn-id: svn://svn.chromium.org/blink/trunk@402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/NSWebPageView.h
+++ third_party/WebKit/WebKit/WebView.subproj/NSWebPageView.h
@@ -6,10 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 */
 #import <Cocoa/Cocoa.h>
 
+#import <WebKit/NSWebPageDataSource.h>
+
 @interface NSWebPageView : NSView
 {
 @private
     id _viewPrivate;
 }
+
+- initWithFrame: (NSRect)frame dataSource: (NSWebPageDataSource *)dataSource;
+
+- (NSWebPageDataSource *)dataSource;
 
 @end
--- third_party/WebKit/WebKit/WebView.subproj/NSWebPageView.mm
+++ third_party/WebKit/WebKit/WebView.subproj/NSWebPageView.mm
@@ -6,16 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation NSWebPageView
 
-+ (void)initialize {
-#if 0
-    NSAutoreleasePool *localPool;
-    NSUserDefaults *userDefaults;
-    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-        @"value", @"key",
-    };
-
-    [[NSUserDefaults standardUserDefaults] registerDefaults:dict];
-#endif
-}
 
 @end
