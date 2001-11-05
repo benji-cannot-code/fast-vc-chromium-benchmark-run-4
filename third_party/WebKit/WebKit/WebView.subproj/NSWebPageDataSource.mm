3c06af3ee99c2716aa8f65c84ea6cfe350cecd59
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-11-05 18:33:39 +0000

    Added fonts sizes to defaults
    git-svn-id: svn://svn.chromium.org/blink/trunk@406 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/NSWebPageDataSource.mm
+++ third_party/WebKit/WebKit/WebView.subproj/NSWebPageDataSource.mm
@@ -10,13 +10,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     NSAutoreleasePool *localPool;
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSArray *fontSizeArray = [NSArray arrayWithObjects:@"6", @"8", @"10", @"12", @"14", @"16", @"18", @"20", nil];
+    
     NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:
         @"Arial", 		@"stdFontName",
         @"Courier",  		@"fixedFontName",
         @"Times-Roman", 	@"serifFontName",
         @"Arial", 		@"sansSerifFontName", 
         @"Times-Roman", 	@"cursiveFontName",
-        @"Times-Roman", 	@"fantasyFontName", nil];
+        @"Times-Roman", 	@"fantasyFontName",
+        @"6", 			@"minFontSize",
+        fontSizeArray,		@"fontSizes",
+        nil];
 
     [defaults registerDefaults:dict];
 
