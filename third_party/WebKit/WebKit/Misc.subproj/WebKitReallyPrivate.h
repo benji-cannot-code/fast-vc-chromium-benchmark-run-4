bad26fee3e83412619b302490b431c89ee669cf3
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-11-08 21:33:43 +0000

    Added checks to validate arguments passed in through public API methods. Invalid arguments now throw exceptions.
    git-svn-id: svn://svn.chromium.org/blink/trunk@427 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebKitReallyPrivate.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebKitReallyPrivate.h
@@ -4,3 +4,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 */
 
 #import "_NSMonitor.h"
+
+#define WEBKIT_ASSERT_VALID_ARG(arg,expr) \
+    do { \
+        if (!(expr)) { \
+            NSString *reason = [NSString stringWithFormat:@"'%s' fails check: '%s'", #arg, #expr]; \
+            [[NSException exceptionWithName:NSInvalidArgumentException reason:reason userInfo: nil] raise]; \
+        } \
+    } while (0)
+    
+#define WEBKIT_ASSERT_NOT_NIL(arg) \
+    do { \
+        if ((arg) == nil) { \
+            NSString *reason = [NSString stringWithFormat:@"'%s' is nil", #arg]; \
+            [[NSException exceptionWithName:NSInvalidArgumentException reason:reason userInfo: nil] raise]; \
+        } \
+    } while (0)
