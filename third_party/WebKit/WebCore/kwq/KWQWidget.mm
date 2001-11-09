6db0b59794fc97f16c43113ab937afa09448427d
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-11-09 22:12:47 +0000

    Fixed sizeHint().
    git-svn-id: svn://svn.chromium.org/blink/trunk@430 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQWidget.mm
+++ third_party/WebKit/WebCore/kwq/KWQWidget.mm
@@ -110,7 +110,8 @@ QWidget::~QWidget()
 
 QSize QWidget::sizeHint() const 
 {
-    _logNeverImplemented();
+    // May be overriden by subclasses.
+    return QSize(0,0);
 }
 
 void QWidget::resize(int w, int h) 
@@ -476,12 +477,14 @@ void QWidget::erase()
 QWidget *QWidget::focusWidget() const
 {
     _logNeverImplemented();
+    return 0;
 }
 
 
 QPoint QWidget::mapFromGlobal(const QPoint &) const
 {
     _logNeverImplemented();
+    return QPoint(0,0);
 }
 
 
