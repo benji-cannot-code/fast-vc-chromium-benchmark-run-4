01b1fb687d68efec6d447dd59e07a2344a77cbb4
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-11-29 17:31:21 +0000

    Various clean ups to the loader code. Added first hacks at the history portion of the framework.
    git-svn-id: svn://svn.chromium.org/blink/trunk@469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/History.subproj/IFBackForwardList.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//
+//  WKBackForwardList.h
+//  WebKit
+//
+//  Created by Kenneth Kocienda on Thu Nov 29 2001.
+//  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WKURIList.h"
+
+@interface WKBackForwardList : NSObject {
+    WKURIList *uriList;
+}
+
+
+
+@end
---
+++ third_party/WebKit/WebKit/History.subproj/WebHistoryList.h
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*	WKURIList.h
+	Copyright 2001, Apple, Inc. All rights reserved.
+*/
+
+#import <Foundation/Foundation.h>
+
+#import "WKURIEntry.h"
+
+typedef struct WKURIListNode WKURIListNode;
+
+@interface WKURIList : NSObject 
+{
+    WKURIListNode *_head;
+    WKURIListNode *_tail;
+    int _count;
+    BOOL _allowsDuplicates;
+    int _maximumSize;
+}
+
+-(id)init;
+
+-(BOOL)allowsDuplicates;
+-(void)setAllowsDuplicates:(BOOL)allowsDuplicates;
+
+-(int)count;
+
+-(int)maximumSize;
+-(void)setMaximumSize:(int)size;
+
+-(WKURIEntry *)addURL:(NSURL *)url withTitle:(NSString *)title;
+-(void)addEntry:(WKURIEntry *)entry;
+-(WKURIEntry *)removeURL:(NSURL *)url;
+-(BOOL)removeEntry:(WKURIEntry *)entry;
+
+-(WKURIEntry *)entryForURL:(NSURL *)url;
+-(WKURIEntry *)entryAtIndex:(int)index;
+-(WKURIEntry *)removeEntryAtIndex:(int)index;
+-(void)removeEntriesToIndex:(int)index;
+
+@end
