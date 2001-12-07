893cef4f00ac31e7f94b42ddaeee9b7bae5d6554
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-12-07 18:57:58 +0000

    Fixed warning
    git-svn-id: svn://svn.chromium.org/blink/trunk@479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQMetrics.h
+++ third_party/WebKit/WebCore/kwq/KWQMetrics.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSLayoutManager *)layoutManagerForString: (NSString *)string;
 - (void)setColor: (NSColor *)color;
 - (void)setFont: (NSFont *)aFont;
+- (NSDictionary *)attributes;
 @end
 
 @interface KWQLayoutFragment : NSObject
