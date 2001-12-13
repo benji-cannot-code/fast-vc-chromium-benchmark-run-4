4705c8e427c56e8a9eec9d8704e66f73184d004e
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-12-13 22:02:28 +0000

    Changes to support the model being a tree of documents.
    git-svn-id: svn://svn.chromium.org/blink/trunk@497 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFWebController.h
+++ third_party/WebKit/WebKit/WebView.subproj/IFWebController.h
@@ -81,7 +81,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             - (BOOL)directsAllLinksToSystemBrowser;
             
         Removed WKError.  This will be described in WKError.h.
+  
+  2001-12-13
+  
+        Removed WKFrameSetHandler, placed that functionality on WKWebDataSource.
         
+        Changed WKLocationChangeHandler to add a parameter specifying the data source
+        that sent the message.
 */
 
 
@@ -94,13 +100,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
      describe snippets of behavior, but do we explicity reference them anywhere,
      or do we just use the umbrella protocol?]
 */
-@protocol WKWebController <WKLoadHandler, WKScriptContextHandler, WKFrameSetHandler, WKCredentialsHandler, WKLocationChangeHandler>
+@protocol WKWebController <WKLoadHandler, WKScriptContextHandler, WKCredentialsHandler, WKLocationChangeHandler>
+@end
 
-- (void)setDataSource: (WKWebDataSource *)dataSource;
-- (WKWebDataSource *)dataSource
 
-- (void)setView: (WKWebView *)view;
-- (WKWebView *)view;
+
+/*
+   ============================================================================= 
+
+    Is WKConcreteWebController the right name?  The name fits with the
+    scheme used by Foundation, although Foundation's concrete classes
+    typically aren't public.
+*/
+@interface WKDefaultWebController : NSObject <WKWebController>
+
+- initWithView: (WKWebView *) dataSource: (WKWebDataSource *)dataSource;
+
+- setDirectsAllLinksToSystemBrowser: (BOOL)flag
+- (BOOL)directsAllLinksToSystemBrowser;
+
+- (void)setView: (WKWebView *)view andDataSource: (WKWebDataSource *)dataSource;
+- (WKWebView *)viewForDataSource: (WKWebDataSource *)dataSource;
+- (WKWebDataSource *)dataSourceForView: (WKWebVew *)view
+
+- (WKWebView *)mainView;
+- (WKWebDataSource *)mainDataSource;
+
+- (void)createViewForDataSource: (WKWebDataSource *)dataSource inFrameNamed: (NSString *)name;
+
+- (void)createViewForDataSource: (WKWebDataSource *)dataSource inIFrame: (id)iFrameIdentifier;
 
 @end
 
@@ -128,22 +156,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
    ============================================================================= 
 
-    Is WKConcreteWebController the right name?  The name fits with the
-    scheme used by Foundation, although Foundation's concrete classes
-    typically aren't public.
-*/
-@interface WKDefaultWebController : NSObject <WKWebController>
-
-- initWithView: (WKWebView *) dataSource: (WKWebDataSource *)dataSource;
-
-- setDirectsAllLinksToSystemBrowser: (BOOL)flag
-- (BOOL)directsAllLinksToSystemBrowser;
-
-@end
-
-/*
-   ============================================================================= 
-
     See the comments in WKWebPageView above for more description about this protocol.
 */
 @protocol WKLocationChangeHandler
@@ -159,6 +171,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)inputURL: (NSURL *)inputURL resolvedTo: (NSURL *)resolvedURL;
 - (void)serverRedirectTo: (NSURL *)url;
+
+@end
+
+
+/*
+   ============================================================================= 
+
+    A frame-aware version of the the WKLocationChangeHandler
+
+    See the comments in WKWebPageView above for more description about this protocol.
+*/
+@protocol WKLocationChangeHandler
+
+// locationWillChangeTo: is required, but will it be sent by the dataSource?  More
+// likely the controller will receive a change request from the view.  That argues for
+// placing locationWillChangeTo: in a different protocol, and making it more or a complete
+// handshake.
+- (BOOL)locationWillChangeTo: (NSURL *)url;
+
+- (void)locationChangeStartedByDataSource: (WKWebDataSource *)dataSource;
+- (void)locationChangeCancelled: (WKError *)error byDataSource: (WKWebDataSource *)dataSource;
+- (void)locationChangeStopped: (WKError *)error byDataSource: (WKWebDataSource *)dataSource;
+- (void)locationChangeFinished byDataSource: (WKWebDataSource *)dataSource;
+
+- (void)receivedPageTitle: (NSString *)title byDataSource: (WKWebDataSource *)dataSource;
+
+- (void)inputURL: (NSURL *)inputURL resolvedTo: (NSURL *)resolvedURL byDataSource: (WKWebDataSource *)dataSource;
+- (void)serverRedirectTo: (NSURL *)url byDataSource: (WKWebDataSource *)dataSource;
 
 @end
 
@@ -196,11 +236,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     typically for non-base URLs this should be done after a URL (i.e. image)
     has been completely downloaded.
 */
-- (void)receivedProgress: (WKLoadProgress *)progress forResource: (NSString *)resourceDescription;
+- (void)receivedProgress: (WKLoadProgress *)progress forResource: (NSString *)resourceDescription fromDataSource: (WKWebDataSource *)dataSource;
 
-- (void)receivedError: (WKError *)error forResource: (NSString *)resourceDescription partialProgress: (WKLoadProgress *)progress;
+- (void)receivedError: (WKError *)error forResource: (NSString *)resourceDescription partialProgress: (WKLoadProgress *)progress fromDataSource: (WKWebDataSource *)dataSource;
 
 @end
+
 
 /*
    ============================================================================= 
@@ -232,7 +273,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 */
 @protocol WKWebDataSourceErrorHandler
-- error: (WKError *)error;
+- error: (WKError *)error inDataSource: (WKWebDataSource *)dataSource;
 @end
 
 
@@ -246,25 +287,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol WKScriptContextHandler
 
 // setStatusText and statusText are used by Javascript's status bar text methods.
-- (void)setStatusText: (NSString *)text;
-- (NSString *)statusText;
+- (void)setStatusText: (NSString *)text forDataSource: (WKWebDataSource *)dataSource;
+- (NSString *)statusTextForDataSource: (WKWebDataSource *)dataSource;
 
 // Need API for things like window size and position, window ids,
 // screen goemetry.  Essentially all the 'view' items that are
 // accessible from Javascript.
 
-@end
-
-
-/*
-   ============================================================================= 
-
-*/
-@protocol WKFrameSetHandler
-- (NSArray *)frameNames;
-- (id <WKFrame>) findFrameNamed: (NSString *)name;
-- (BOOL)frameExists: (NSString *)name;
-- (void)openURL: (NSURL *)url inFrame: (id <WKFrame>) frame;
 @end
 
 
--- third_party/WebKit/WebKit/WebView.subproj/WKWebDataSource.h
+++ third_party/WebKit/WebKit/WebView.subproj/WKWebDataSource.h
@@ -34,7 +34,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     
     Added finalURL and isRedirected.
    
+    2001-12-13
+    
+        Remove setBase: and setBaseTarget:
         
+        Changed return type of baseTarget to (NSString *)
+        
+        Added the following two methods:
+            - (WKDataSource *)parent;
+            - (NSArry *)children;
+            - (BOOL)isMainDocument;
+  
+        Added the following methods:
+        
+            - (NSArray *)frameNames;
+            - (WKWebDataSource) findDataSourceForFrameNamed: (NSString *)name;
+            - (BOOL)frameExists: (NSString *)name;
+            - (void)openURL: (NSURL *)url inFrameNamed: (NSString *)frameName;
+            - (void)openURL: (NSURL *)url inIFrame: (id)iFrameIdentifier;
+                    
    ============================================================================= 
 */
    
@@ -57,8 +75,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - initWithLoader: (WKURILoader *)loader;
 
 
+// Returns nil if this data source represents the main document.  Otherwise
+// returns the parent data source.
+- (WKDataSource *)parent;
+
+
+// Returns YES if this is the main document.  The main document is the 'top'
+// document, typically either a frameset or a normal HTML document.
+- (BOOL)isMainDocument;
+
+// Returns an array of WKWebDataSource.  The data sources in the array are
+// the data source assoicated with a frame set or iframe.  If the main document
+// is not a frameset, or has not iframes children will return nil.
+- (NSArray *)children;
+
+
+// Returns an array of NSStrings or nil.  The NSStrings corresponds to
+// frame names.  If this data source is the main document and has no
+// frames then frameNames will return nil.
+- (NSArray *)frameNames;
+
+// findDataSourceForFrameNamed: returns the child data source associated with
+// the frame named 'name', or nil. 
+- (WKWebDataSource) findDataSourceForFrameNamed: (NSString *)name;
+
+
+- (BOOL)frameExists: (NSString *)name;
+
+
+- (void)openURL: (NSURL *)url inFrameNamed: (NSString *)frameName;
+
+
+- (void)openURL: (NSURL *)url inIFrame: (id)iFrameIdentifier;
+
+
 // Set the controller for this data source.  NOTE:  The controller is not retained by the
-// data source.  Perhaps setController: should be private?
+// data source.  Perhaps setController: should be private?  Perhaps the back pointers
+// can be managed externally, i.e. + controllerForDataSource: as a class method on 
+// WKDefaultWebController?
 - (void)setController: (id <WKWebController>)controller;
 - (id <WKWebController>)controller;
 
@@ -111,10 +165,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 // URL reference point, these should probably not be public for 1.0.
-- setBase: (NSURL *)url;
 - (NSURL *)base;
-- setBaseTarget: (NSURL *)url;
-- (NSURL *)baseTarget;
+- (NSString *)baseTarget;
 
 
 - (NSString *)encoding;
