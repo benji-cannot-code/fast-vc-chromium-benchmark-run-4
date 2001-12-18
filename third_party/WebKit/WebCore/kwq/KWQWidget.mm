0ced004737401ed48d85c3c14b0e0f1f3984d79b
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2001-12-18 18:23:55 +0000

    KWQWidget.mm
    git-svn-id: svn://svn.chromium.org/blink/trunk@514 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQWidget.mm
+++ third_party/WebKit/WebCore/kwq/KWQWidget.mm
@@ -503,7 +503,7 @@ void *QWidget::getView() const
 void QWidget::setView(void *view)
 {
     if (data->view)
-        [data->view autorelease];
+        [data->view release];
     data->view = [view retain];
         
     NSRect frame = [data->view frame];
