d3d7ec0eadf1819501d2a24402441a4605ccec23
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-01-07 21:37:46 +0000

    Moved plug-in instance creation to WKPluginView from WKPlugin
    git-svn-id: svn://svn.chromium.org/blink/trunk@525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kwq/WKPluginView.mm
+++ third_party/WebKit/WebCore/src/kwq/WKPluginView.mm
@@ -67,9 +67,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     window.clipRect.bottom = 450;
     window.clipRect.right = 640;
     window.type = NPWindowTypeDrawable;
-    /*SetPort(nPort.port);
+    SetPort(nPort.port);
     LineTo((int)rect.size.width, (int)rect.size.height);
-    MoveTo(0,0);*/
+    MoveTo(0,0);
     npErr = NPP_SetWindow(instance, &window);
     KWQDebug("NPP_SetWindow: %d rect.size.height=%d rect.size.width=%d port=%d\n", npErr, (int)rect.size.height, (int)rect.size.width, (int)nPort.port);
     
