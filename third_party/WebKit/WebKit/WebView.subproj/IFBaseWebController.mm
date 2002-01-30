4fe25cd3baec3de008c1ec26fccaa14a1ded1d0a
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-01-30 01:07:10 +0000

    Added setMainView: for John.
    git-svn-id: svn://svn.chromium.org/blink/trunk@564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFBaseWebController.mm
+++ third_party/WebKit/WebKit/WebView.subproj/IFBaseWebController.mm
@@ -107,22 +107,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     WKDefaultWebControllerPrivate *data = ((WKDefaultWebControllerPrivate *)_controllerPrivate);
 
-    [data->viewMap release];
+    [data->viewMap autorelease];
     data->viewMap = [[NSMutableDictionary alloc] init];
     
-    [data->dataSourceMap release];
+    [data->dataSourceMap autorelease];
     data->dataSourceMap = [[NSMutableDictionary alloc] init];
     
-    [data->mainView release];
+    [data->mainView autorelease];
     data->mainView = [view retain];
     [view _setController: self];
     
-    [data->mainDataSource release];
+    [data->mainDataSource autorelease];
     data->mainDataSource = [dataSource retain];
     [dataSource _setController: self];
 
-    [data->viewMap setObject: view forKey: [WKObjectHolder holderWithObject:dataSource]];
-    [data->dataSourceMap setObject: dataSource forKey: [WKObjectHolder holderWithObject:view]];
+    if (dataSource != nil && view != nil){
+        [data->viewMap setObject: view forKey: [WKObjectHolder holderWithObject:dataSource]];
+
+        [data->dataSourceMap setObject: dataSource forKey: [WKObjectHolder holderWithObject:view]];
+    }
     
     [view dataSourceChanged];
 }
@@ -156,6 +159,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     WKDefaultWebControllerPrivate *data = ((WKDefaultWebControllerPrivate *)_controllerPrivate);
     return [data->dataSourceMap objectForKey: [WKObjectHolder holderWithObject:view]];
+}
+
+- (void)setMainView: (WKWebView *)m;
+{
+    WKDefaultWebControllerPrivate *data = ((WKDefaultWebControllerPrivate *)_controllerPrivate);
+    [self setView: m andDataSource: data->mainDataSource];
 }
 
 
--- third_party/WebKit/WebKit/WebView.subproj/IFDefaultWebController.h
+++ third_party/WebKit/WebKit/WebView.subproj/IFDefaultWebController.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Find the data source for the specified view.
 - (WKWebDataSource *)dataSourceForView: (WKWebView *)view;
 
+- (void)setMainView: (WKWebView *)view;
 - (WKWebView *)mainView;
 
 - (void)setMainDataSource: (WKWebDataSource *)dataSource;
