c11116d0a709c235d8bb0661e828387ceb8e15fa
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-01-31 23:23:12 +0000

    Added support for hidden plug-ins. Added focus events
    git-svn-id: svn://svn.chromium.org/blink/trunk@571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kwq/npapi.mm
+++ third_party/WebKit/WebCore/src/kwq/npapi.mm
@@ -1,33 +1,34 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "npapi.h"
 #include <WKPluginView.h>
+#include "kwqdebug.h"
 
 // general plug-in to browser functions
 
 const char* NPN_UserAgent(NPP instance){
-    printf("NPN_UserAgent\n");
+    KWQDebug("NPN_UserAgent\n");
     return "IE";
 }
 
 void* NPN_MemAlloc(UInt32 size){
-    printf("NPN_MemAlloc\n");
+    KWQDebug("NPN_MemAlloc\n");
     return malloc(size);
 
 }
 
 void NPN_MemFree(void* ptr){
-    printf("NPN_MemFree\n");
+    KWQDebug("NPN_MemFree\n");
     free(ptr);
 
 }
 
 UInt32 NPN_MemFlush(UInt32 size){
-    printf("NPN_MemFlush\n");
+    KWQDebug("NPN_MemFlush\n");
     return 0;
 }
 
 void NPN_ReloadPlugins(NPBool reloadPages){
-    printf("NPN_ReloadPlugins\n");
+    KWQDebug("NPN_ReloadPlugins\n");
 
 }
 
