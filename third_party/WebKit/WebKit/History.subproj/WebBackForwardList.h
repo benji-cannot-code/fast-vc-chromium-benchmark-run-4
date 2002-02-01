16fdd7b2640c805baf136d197f4e667b875f7406
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-02-01 19:02:31 +0000

    Added call to examine the entry at the current index in 	the back/forward list without modifying the list. I needed 	this to save context data (in my case, scroll position) 	on the currently-viewed entry.
    	* History.subproj/WKBackForwardList.h:
    	* History.subproj/WKBackForwardList.m:
    	(-[WKBackForwardList currentEntry]):
    	Just returns the entry at the current index.
    
    	Added useful mechanism and hideous hack for using remembered scroll positions
    	with the back/forward list. There's no way to do this cleanly at this level;
    	we have to get WebKit involved. Most of this code is fine though, only the
    	restoring part should have to be redone.
    
    	* BackForwardListEntry.h:
    	* BackForwardListEntry.m: (+[BackForwardListEntry entryWithURL:]),
    	(-[BackForwardListEntry scrollPoint]), (-[BackForwardListEntry setScrollPoint:]):
    	New subclass of WKURIEntry; remembers a scroll point with each entry, and adds
    	a convenience constructor. Some or all of this may move into WebKit later.
    
    	* BrowserDocument.m:
    	(-[BrowserDocument saveScrollPosition]):
    	New method, reads the current scroll position from the view and stores it
    	into the current back/forward list entry.
    
    	(-[BrowserDocument restoreScrollPosition]):
    	New method, reads the previously-saved scroll position from the current
    	back/forward list entry and scrolls the view to match.
    
    	(-[BrowserDocument restoreScrollPositionFromTimerHack:]),
    	(-[BrowserDocument restoreScrollPositionLaterHack]):
    	Timer-based hackery to wait a little while before calling restoreScrollPosition
    	since calling it before the new page has loaded has no effect. This will have
    	to be replaced with a WebKit-based mechanism for telling the webView what the
    	incoming page's initial scroll point should be.
    
    	(-[BrowserDocument goToURLBypassingBackForwardList:]),
    	(-[BrowserDocument goToURL:]),
    	(-[BrowserDocument goBack]),
    	(-[BrowserDocument goForward]):
    	Call the save/restore methods at the right places.
    
    	* WebBrowser.pbproj/project.pbxproj: Updated for new files
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebBackForwardList.h
+++ third_party/WebKit/WebKit/History.subproj/WebBackForwardList.h
@@ -17,8 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 -(id)init;
 
 -(void)addEntry:(WKURIEntry *)entry;
+
 -(WKURIEntry *)back;
 -(WKURIEntry *)forward;
+
+-(WKURIEntry *)currentEntry;
 
 -(NSArray *)backList;
 -(NSArray *)forwardList;
--- third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
+++ third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
@@ -57,6 +57,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return result;
 }
 
+-(WKURIEntry *)currentEntry
+{
+    return [uriList entryAtIndex:index];
+}
+
 -(WKURIEntry *)forward
 {
     WKURIEntry *result;
