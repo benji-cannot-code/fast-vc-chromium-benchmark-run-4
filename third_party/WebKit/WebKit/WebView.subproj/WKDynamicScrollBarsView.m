e85802dc0d6d8dde5700e25429db173f1eb92bda
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-02-01 21:56:36 +0000

    Added files from WebBrowser`
    git-svn-id: svn://svn.chromium.org/blink/trunk@582 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/WebView.subproj/WKDynamicScrollBarsView.m
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//
+//  DynamicScrollBarsView.m
+//  WebBrowser
+//
+//  Created by John Sullivan on Tue Jan 22 2002.
+//  Copyright (c) 2001 Apple Computer, Inc. All rights reserved.
+//
+
+#import <WebKit/WKDynamicScrollBarsView.h>
+
+@implementation WKDynamicScrollBarsView
+
+// make the horizontal and vertical scroll bars come and go as needed
+- (void) reflectScrolledClipView: (NSClipView*)clipView
+{
+    if( clipView == [self contentView] ) {
+        BOOL scrollsVertically = [[self documentView] bounds].size.height > [self contentSize].height;
+        BOOL scrollsHorizontally = [[self documentView] bounds].size.width > [self contentSize].width;
+
+        [self setHasVerticalScroller: scrollsVertically];
+        [self setHasHorizontalScroller: scrollsHorizontally];
+    }
+    [super reflectScrolledClipView: clipView];
+}
+
+@end
