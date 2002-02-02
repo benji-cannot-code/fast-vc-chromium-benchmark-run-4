eabb8dff7a82d44eb6d9919932a59eeaf2eae4a3
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-02-02 05:06:39 +0000

    2002-02-01  Kenneth Kocienda  <kocienda@apple.com>
            More cleanups to the no-copy NSData API.
            This time the callback from the cache/loader has been
            simplified from:
            - (void)WKURLHandle:(WKURLHandle *)sender resourceDataDidBecomeAvailable:(UInt8 *)bytes offset:(unsigned)offset length:(unsigned)length;
            to:
            - (void)WKURLHandle:(WKURLHandle *)sender resourceDataDidBecomeAvailable:(NSData *)data;
            This was made possible by some rearrangement in the private API.
    	Changed name of WKByteBlockData to WKData.
    
            * include/WCURLHandle.h:
            * src/kwq/KWQKHTMLPart.mm: (-[KHTMLPartLoadClient
            WCURLHandle:resourceDataDidBecomeAvailable:userData:]):
            * src/kwq/KWQKloader.mm: (-[URLLoadClient
            WCURLHandle:resourceDataDidBecomeAvailable:userData:]):
            * CacheLoader.subproj/WCURLHandleGlue.m: (-[WCURLHandleClientProxy
            WKURLHandle:resourceDataDidBecomeAvailable:]):
            * CacheLoader.subproj/WKURLHandle.m: (-[WKURLHandleSynchronousClient
            WKURLHandle:resourceDataDidBecomeAvailable:]), (-[WKURLHandle
            notifyClientsDidLoadData]):
            * CacheLoader.subproj/WKURLHandleCGlue.h:
            * CacheLoader.subproj/WKURLHandleCGlue.m: (-[WKURLHandleCGlueClient
            WKURLHandle:resourceDataDidBecomeAvailable:]):
            * CacheLoader.subproj/WKURLHandleClient.h:
            * Misc.subproj/WKMutableDataPrivate.h:
            * Misc.subproj/WKMutableDataPrivate.m: (-[WKMutableData
            dataObjectsGreaterThanMatchedOffset:]), (+[WKData dataWithByteBlock:]), (-[WKData
            subdataWithRange:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@586 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/include/WCURLHandle.h
+++ third_party/WebKit/WebCore/include/WCURLHandle.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)WCURLHandleResourceDidBeginLoading:(id)sender userData:(void *)userData;
 - (void)WCURLHandleResourceDidCancelLoading:(id)sender userData:(void *)userData;
 - (void)WCURLHandleResourceDidFinishLoading:(id)sender userData:(void *)userData;
-- (void)WCURLHandle:(id)sender resourceDataDidBecomeAvailable:(NSData *)data offset:(int)offset length:(int)length userData:(void *)userData;
+- (void)WCURLHandle:(id)sender resourceDataDidBecomeAvailable:(NSData *)data userData:(void *)userData;
 - (void)WCURLHandle:(id)sender resourceDidFailLoadingWithResult:(int)result userData:(void *)userData;
 
 @end
