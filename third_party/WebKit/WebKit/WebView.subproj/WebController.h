dca4d4c0c065ec8040bdaf218db4c059d9e84f53
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-02-07 02:15:47 +0000

    Fixed comment.
    git-svn-id: svn://svn.chromium.org/blink/trunk@599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebController.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebController.h
@@ -84,48 +84,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class IFWebView;
 @class IFWebFrame;
 
-#ifdef TENTATIVE_API
-/*
-   ============================================================================= 
-
-    IFWebViewDelegates implement protocols that modify the behavior of
-    IFWebViews.  A IFWebView does not require a delegate.
-*/
-@protocol IFWebViewDelegate <?>
-@end
-
-
-/*
-   ============================================================================= 
-
-    IFWebDataSourceDelegate implement protocols that modify the behavior of
-    IFWebDataSources.  A IFWebDataSources does not require a delegate.
-*/
-@protocol IFWebDataSourceDelegate <?>
-@end
-
-
-/*
-   ============================================================================= 
-
-    See the comments in IFWebPageView above for more description about this protocol.
-*/
-@protocol IFLocationChangeHandler
-
-- (BOOL)locationWillChangeTo: (NSURL *)url;
-
-- (void)locationChangeStarted;
-- (void)locationChangeInProgress;
-- (void)locationChangeDone: (IFError *)error;
-
-- (void)receivedPageTitle: (NSString *)title;
-
-- (void)serverRedirectTo: (NSURL *)url;
-
-@end
-#endif
-
-
 /*
    ============================================================================= 
 
@@ -134,6 +92,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     See the comments in IFWebPageView above for more description about this protocol.
 */
 @protocol IFLocationChangeHandler
+
+// This API will need to be extended to support some notion of the context in which
+// a location is changing, i.e. was it initiated by a user click, by a programmatic
+// manipulation of the DOM, is the frame an iframe, or a frame.
 
 // locationWillChangeTo: is required, but will it be sent by the dataSource?  More
 // likely the controller will receive a change request from the view.  That argues for
