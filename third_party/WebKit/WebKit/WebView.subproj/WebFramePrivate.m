82c30d9abaff99ff87a2851d5c6de15b63b324a3
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-02-08 01:56:23 +0000

    New file.
    git-svn-id: svn://svn.chromium.org/blink/trunk@605 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/WebView.subproj/WebFramePrivate.m
@@ -0,0 +1,49 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*	
+        IFWebFramePrivate.mm
+	    
+	    Copyright 2001, Apple, Inc. All rights reserved.
+*/
+#import <WebKit/IFWebDataSource.h>
+#import <WebKit/IFWebFramePrivate.h>
+
+@implementation IFWebFramePrivate
+
+- (void)dealloc
+{
+    [name autorelease];
+    [view autorelease];
+    [dataSource autorelease];
+}
+
+- (NSString *)name { return name; }
+- (void)setName: (NSString *)n 
+{
+    [name autorelease];
+    name = [n retain];
+}
+
+
+- view { return view; }
+- (void)setView: v 
+{ 
+    [view autorelease];
+    view = [v retain];
+}
+
+- (IFWebDataSource *)dataSource { return dataSource; }
+- (void)setDataSource: (IFWebDataSource *)d
+{ 
+    [dataSource autorelease];
+    dataSource = [d retain];
+}
+
+
+- (void *)renderFramePart { return renderFramePart; }
+- (void)setRenderFramePart: (void *)p 
+{
+    renderFramePart = p;
+}
+
+
+@end
