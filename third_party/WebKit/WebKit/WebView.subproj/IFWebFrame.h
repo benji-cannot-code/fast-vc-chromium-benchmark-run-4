0728bebdf64a7e8d6cc3366b1fbfd599bc905110
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-02-08 01:38:15 +0000

    factor private class out of IFWebFrame.
    git-svn-id: svn://svn.chromium.org/blink/trunk@603 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFWebFrame.h
+++ third_party/WebKit/WebKit/WebView.subproj/IFWebFrame.h
@@ -12,11 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface IFWebFrame : NSObject
 {
-    // FIXME:  Create private class for ivars.
-    NSString *name;
-    id view;
-    IFWebDataSource *dataSource;
-    void *renderFramePart;
+@private
+    id _framePrivate;
 }
 
 - initWithName: (NSString *)name view: view dataSource: (IFWebDataSource *)dataSource;
@@ -31,4 +28,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // private
 - (void)_setRenderFramePart: (void *)p;
 - (void *)_renderFramePart;
+
 @end
