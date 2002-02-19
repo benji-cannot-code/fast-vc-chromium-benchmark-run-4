17d7c1bdeda8b68fca0876fc367ad10d7604859c
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-02-19 01:18:08 +0000

    First piece of implementing IFWebHistory. None of the mutators do anything, 	and the accessors return canned data. But at least all the temporary hackery 	is hiding behind legitimate API.
    	* History.subproj/IFWebHistory.h:
    	* History.subproj/IFWebHistory.m:
    
    	(+[IFWebHistory sharedWebHistory]):
    	Implemented sensibly.
    
    	(-[IFWebHistory createTestEntryWithURLString:title:date:]),
    	(-[IFWebHistory	testDataDates]), (-[IFWebHistory testData]),
    	Private temporary hackery to return fake data.
    
    	(-[IFWebHistory orderedLastVisitedDays]),
    	(-[IFWebHistory	orderedEntriesLastVisitedOnDay:]):
    	Implemented using temporary hackery.
    
    	(-[IFWebHistory addEntry:]),
    	(-[IFWebHistory removeEntry:]),
    	(-[IFWebHistory removeAllEntries]):
    	Unimplemented mutator methods.
    
    	(-[IFWebHistory	entriesWithAddressContainingString:]),
    	(-[IFWebHistory	entriesWithTitleOrAddressContainingString:]),
    	(-[IFWebHistory containsURL:]):
    	Methods that we'll need eventually, currently unimplemented. We may flesh out
    	the API a little more before we actually implement any of these.
    
    	(-[NSCalendarDate daysSinceDate:]): Convenience method in category; will
    	probably move to another file soon.
    
    	* WebKit.pbproj/project.pbxproj: Updated for new files
    
    
    
    
        Made history implementation use IFWebHistory; removed temporary hackery.
        (Actually it was mostly just moved to IFWebHistory, but now it's hiding
        behind a legitimate API.)
    
    	* GlobalHistory.m:
    	(+[GlobalHistory testData]): Removed this temporary hackery.
    
    	(-[GlobalHistory outlineView:child:ofItem:]),
    	(-[GlobalHistory outlineView:isItemExpandable:]),
    	(-[GlobalHistory outlineView:numberOfChildrenOfItem:]),
    	(-[GlobalHistory outlineView:objectValueForTableColumn:byItem:]):
    	Replaced hacky implementation with use of new IFWebHistory API.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@642 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/History.subproj/IFWebHistory.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//
+//  IFWebHistory.h
+//  WebKit
+//
+//  Created by John Sullivan on Mon Feb 18 2002.
+//  Copyright (c) 2002 Apple Computer, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <WebKit/IFURIEntry.h>
+
+@interface IFWebHistory : NSObject {
+@private
+    id _historyPrivate;
+}
+
++ (IFWebHistory *)sharedWebHistory;
+
+// modifying contents
+- (void)addEntry: (IFURIEntry *)entry;
+- (void)removeEntry: (IFURIEntry *)entry;
+- (void)removeAllEntries;
+
+// retrieving contents for date-based presentation
+
+// get an array of NSCalendarDate, each one representing a unique day that contains one
+// or more history entries, ordered from most recent to oldest.
+- (NSArray *)orderedLastVisitedDays;
+
+// get an array of IFURIEntry that were last visited on the day represented by the
+// specified NSCalendarDate, ordered from most recent to oldest.
+- (NSArray *)orderedEntriesLastVisitedOnDay: (NSCalendarDate *)calendarDate;
+
+// retrieving contents for autocompletion in location field
+- (NSArray *)entriesWithAddressContainingString: (NSString *)string;
+
+// retrieving contents for searching (maybe replace with state-based search API)
+- (NSArray *)entriesWithTitleOrAddressContainingString: (NSString *)string;
+
+// testing contents for visited-link mechanism
+- (BOOL)containsURL: (NSURL *)url;
+
+@end
