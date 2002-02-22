9c8a516fb8f0eac88aa4e24b64fef6eb797ef42e
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-02-22 20:36:45 +0000

    Implemented missing getter for provisionalDataSource.
    git-svn-id: svn://svn.chromium.org/blink/trunk@658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFWebFrame.mm
+++ third_party/WebKit/WebKit/WebView.subproj/IFWebFrame.mm
@@ -80,6 +80,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 
+- (IFWebDataSource *)provisionalDataSource
+{
+    IFWebFramePrivate *data = (IFWebFramePrivate *)_framePrivate;
+    return [data provisionalDataSource];
+}
+
+
 
 - (IFWebDataSource *)dataSource
 {
--- third_party/WebKit/WebKit/WebView.subproj/IFWebFramePrivate.h
+++ third_party/WebKit/WebKit/WebView.subproj/IFWebFramePrivate.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - view;
 - (void)setDataSource: (IFWebDataSource *)d;
 - (IFWebDataSource *)dataSource;
+- (void)setProvisionalDataSource: (IFWebDataSource *)d;
+- (IFWebDataSource *)provisionalDataSource;
 - (void)setRenderFramePart: (void *)p;
 - (void *)renderFramePart;
 
