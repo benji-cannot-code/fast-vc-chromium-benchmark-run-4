36f212c5afd5f2cc26d32f531d12fa3a2dcf2f4e
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-02-22 22:26:32 +0000

    Added relatively complete unit tests for the two simple methods in 	this category. These found bugs! Yay for unit tests!
    	* Tests/WebFoundation-Misc/ifnscalendardateextensions-test.chk:
    	* Tests/WebFoundation-Misc/ifnscalendardateextensions-test.m: (main):
    
    	Fixed problems in NSCalendarDate extensions revealed by unit tests.
    
    	* Misc.subproj/IFNSCalendarDateExtensions.h:
    	* Misc.subproj/IFNSCalendarDateExtensions.m:
    	Removed [NSCalendarDate daysSinceDate:], which was broken.
    	(-[NSCalendarDate compareDay:]): New method, returns NSComparisonResult.
    	(-[NSCalendarDate isToday]): Rewrote to use compareDay:
    
    	* WebFoundation.pbproj/project.pbxproj: Didn't change this on purpose, probably
    	auto-changed by opening it in Puma.
    
    
    	Updated for modified NSCalendarDate extensions API.
    
    	* History.subproj/IFWebHistoryPrivate.m:
    	(-[IFWebHistoryPrivate findIndex:forDay:]): use compareDate: instead of
    	daysSinceDate:, which no longer exists.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@661 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebHistoryPrivate.m
+++ third_party/WebKit/WebKit/History.subproj/WebHistoryPrivate.m
@@ -49,11 +49,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     //FIXME: just does linear search through days; inefficient if many days
     count = [_datesWithEntries count];
     for (*index = 0; *index < count; ++*index) {
-        int deltaDays = [date daysSinceDate: [_datesWithEntries objectAtIndex: *index]];
-        if (deltaDays == 0) {
+        NSComparisonResult result = [date compareDay: [_datesWithEntries objectAtIndex: *index]];
+        if (result == NSOrderedSame) {
             return YES;
         }
-        if (deltaDays > 0) {
+        if (result == NSOrderedDescending) {
             return NO;
         }
     }
