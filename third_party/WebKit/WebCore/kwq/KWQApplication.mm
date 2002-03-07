d89f2211dd632e838732f702f1f073d726697547
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-07 19:14:53 +0000

    Intentional break to test jersey
    git-svn-id: svn://svn.chromium.org/blink/trunk@710 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQApplication.mm
+++ third_party/WebKit/WebCore/kwq/KWQApplication.mm
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <qapplication.h>
 #include <qpalette.h>
-
+df
 // FIXME: 
 static QPalette *DEFAULT_PALETTE = NULL;
 static QSize *DEFAULT_SIZE = NULL;
