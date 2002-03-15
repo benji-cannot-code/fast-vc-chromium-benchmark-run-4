4b3d968031a5d2fc0f6c65f276d05f876ad19a6b
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-15 23:08:39 +0000

    iMore string usage optimizations.  Got another 10% or so.
    git-svn-id: svn://svn.chromium.org/blink/trunk@753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_text.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_text.cpp
@@ -44,6 +44,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define QT_ALLOC_QCHAR_VEC( N ) (QChar*) new char[ sizeof(QChar)*( N ) ]
 #define QT_DELETE_QCHAR_VEC( P ) delete[] ((char*)( P ))
 
+#ifdef APPLE_CHANGES
+#define OPTIMIZE_STRING_USAGE
+#ifdef OPTIMIZE_STRING_USAGE
+static CFMutableStringRef reuseableString = 0;
+#endif
+#endif
+
+
 using namespace khtml;
 using namespace DOM;
 
@@ -660,13 +668,6 @@ void RenderText::print( QPainter *p, int x, int y, int w, int h,
     printObject(p, x, y, w, h, tx, ty);
 }
 
-#ifdef APPLE_CHANGES
-#define OPTIMIZE_STRING_USAGE
-#ifdef OPTIMIZE_STRING_USAGE
-static CFMutableStringRef reuseableString = 0;
-#endif
-#endif
-
 void RenderText::calcMinMaxWidth()
 {
     //kdDebug( 6040 ) << "Text::calcMinMaxWidth(): known=" << minMaxKnown() << endl;
@@ -718,11 +719,7 @@ void RenderText::calcMinMaxWidth()
                 if(currMinWidth > m_minWidth) m_minWidth = currMinWidth;
                 currMinWidth = 0;
 #if (defined(APPLE_CHANGES) && defined(OPTIMIZE_STRING_USAGE))
-                if (reuseableString == 0)
-                    reuseableString = CFStringCreateMutableWithExternalCharactersNoCopy (kCFAllocatorDefault, (UniChar *)(str->s+i+wordlen), wordlen, wordlen, kCFAllocatorDefault);
-                else
-                    CFStringSetExternalCharactersNoCopy (reuseableString, (UniChar *)(str->s+i+wordlen), 1, 1);
-                currMaxWidth += _fm._width(reuseableString);
+                currMaxWidth += _fm._width(QString::gstring_toCFString(&reuseableString, (UniChar *)(str->s+i+wordlen), 1));
 #else
                 currMaxWidth += _fm.width( *(str->s+i+wordlen) );
 #endif
@@ -903,7 +900,11 @@ unsigned int RenderText::width(unsigned int from, unsigned int len, QFontMetrics
     if( len == 1)
         w = _fm->width( *(str->s+from) );
     else
+#if (defined(APPLE_CHANGES) && defined(OPTIMIZE_STRING_USAGE))
+        w = _fm->_width(QString::gstring_toCFString(&reuseableString, (UniChar *)(str->s+from), len));
+#else
         w = _fm->width(QConstString(str->s+from, len).string());
+#endif
 
     // ### add margins and support for RTL
 
