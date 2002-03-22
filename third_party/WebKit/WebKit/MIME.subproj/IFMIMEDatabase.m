975c848c5f7f27fc8406e74e2939eb575280b0a3
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-22 23:11:24 +0000

    * WebKit.pbproj/project.pbxproj:         Added the MIME classes to WebKit.
    git-svn-id: svn://svn.chromium.org/blink/trunk@810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/MIME.subproj/IFMIMEDatabase.m
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//
+//  IFMIMEDatabase.m
+//  WebKit
+//
+//  Created by Chris Blumenberg on Fri Mar 22 2002.
+//  Copyright (c) 2002 Apple Computer Inc.. All rights reserved.
+//
+
+#import "IFMIMEDatabase.h"
+
+
+@implementation IFMIMEDatabase
+
++ (IFMIMEDatabase *)sharedMIMEDatabase
+{
+    return nil;
+}
+
+
+- (IFMIMEHandler *)MIMEHandlerForMIMEType:(NSString *)mimeType
+{
+    return nil;
+}
+
+
+- (IFMIMEHandler *)MIMEHandlerForURL:(NSURL *)url
+{
+    return nil;
+}
+
+@end
