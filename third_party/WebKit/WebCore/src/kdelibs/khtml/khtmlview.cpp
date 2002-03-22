9902f8f4efc2d4ba0c787d4c3ee08c09db80f106
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-22 08:44:13 +0000

    Fixed the following post-merge crashers:
    	2885602 - Alexander crashes when clicking on a particular link in Google groups
    	2885564 - Alexander crashes on load failures
    
    	* src/kdelibs/khtml/khtmlview.cpp: (KHTMLView::~KHTMLView): Fix
    	Radar 2885602 by detaching the document from the view when the
    	view is destroyed. This was making the document reference a
    	destroyed view later.
    
    	* src/kwq/KWQKHTMLPart.mm:
    	(KHTMLPart::~KHTMLPart): Fix Radar 2885564 by checking if m_doc is
    	NULL, since it's now inititally set to NULL.
    	(KHTMLPart::KHTMLPart, KHTMLPart::init): Formatting tweaks.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kdelibs/khtml/khtmlview.cpp
+++ third_party/WebKit/WebCore/src/kdelibs/khtml/khtmlview.cpp
@@ -230,7 +230,6 @@ KHTMLView::KHTMLView( KHTMLPart *part, QWidget *parent, const char *name)
 
 KHTMLView::~KHTMLView()
 {
-#ifndef APPLE_CHANGES
     if (m_part)
     {
         //WABA: Is this Ok? Do I need to deref it as well?
@@ -239,7 +238,6 @@ KHTMLView::~KHTMLView()
         if (doc)
             doc->detach();
     }
-#endif /* APPLE_CHANGES not defined */
     delete d; d = 0;
 }
 
