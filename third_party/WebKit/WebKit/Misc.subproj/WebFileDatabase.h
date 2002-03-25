895b837975dca77cef0e51bb0bfa9413e0b8cbce
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-25 17:45:44 +0000

    2002-03-25  Kenneth Kocienda  <kocienda@apple.com>
            Fix for this bug:
    
            Radar 2867562 (Disk cache should write new entries lazily)
    
            * Database.subproj/IFURLFileDatabase.h:
            * Database.subproj/IFURLFileDatabase.m: (+[IFURLFileDatabaseOp
            opWithCode:key:object:]), (-[IFURLFileDatabaseOp initWithCode:key:object:]),
            (-[IFURLFileDatabaseOp opcode]), (-[IFURLFileDatabaseOp key]),
            (-[IFURLFileDatabaseOp object]), (-[IFURLFileDatabaseOp perform:]),
            (-[IFURLFileDatabaseOp dealloc]), (-[IFURLFileDatabase initWithPath:]),
            (-[IFURLFileDatabase dealloc]), (-[IFURLFileDatabase setTimer]),
            (-[IFURLFileDatabase setObject:forKey:]), (-[IFURLFileDatabase
            removeObjectForKey:]), (-[IFURLFileDatabase removeAllObjects]),
            (-[IFURLFileDatabase objectForKey:]), (-[IFURLFileDatabase
            performSetObject:forKey:]), (-[IFURLFileDatabase performRemoveObjectForKey:]),
            (-[IFURLFileDatabase lazySync:]), (-[IFURLFileDatabase sync]):
            * WebFoundation.pbproj/project.pbxproj:
    
            Simple fix to remove Objective-C include when compiling with
            a straight C compiler.
    
            * CacheLoader.subproj/IFURLCacheLoaderConstants.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.h
@@ -7,9 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "IFDatabase.h"
 
-
 @interface IFURLFileDatabase : IFDatabase 
 {
+    NSMutableArray *ops;
+    NSMutableDictionary *setCache;
+    NSMutableSet *removeCache;
+    NSTimer *timer;
+    NSTimeInterval touch;
     NSLock *mutex;
 }
+
+-(void)performSetObject:(id)object forKey:(id)key;
+-(void)performRemoveObjectForKey:(id)key;
+
 @end
