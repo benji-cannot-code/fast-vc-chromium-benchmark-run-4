966265c97def6c39984f95ff7e7bd6b8e87d3d5c
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-25 23:59:34 +0000

    Added private API to allow browser to access the DOM tree.         Added support for the "View BOM Tree" menu.
    git-svn-id: svn://svn.chromium.org/blink/trunk@845 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFWebViewPrivate.mm
+++ third_party/WebKit/WebKit/WebView.subproj/IFWebViewPrivate.mm
@@ -99,6 +99,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return ((IFWebViewPrivate *)_viewPrivate)->widget;    
 }
 
+- (DOM::DocumentImpl *)_document
+{
+    KHTMLPart *part = ((IFWebViewPrivate *)_viewPrivate)->widget->part();
+    if (part){
+        return part->xmlDocImpl();
+    }
+    return 0;
+}
+
++ (NSString *)_nodeName: (DOM::NodeImpl *)node
+{
+    return [NSString stringWithCString:  node->nodeName().string().latin1()];
+}
+
++ (NSString *)_nodeValue: (DOM::NodeImpl *)node
+{
+    return [NSString stringWithCString:  node->nodeValue().string().latin1()];
+}
+
++ (NSString *)_nodeHTML: (DOM::NodeImpl *)node
+{
+    return QSTRING_TO_NSSTRING(node->recursive_toHTML(1));
+}
+
 - (khtml::RenderObject *)_renderRoot
 {
     KHTMLPart *part = ((IFWebViewPrivate *)_viewPrivate)->widget->part();
