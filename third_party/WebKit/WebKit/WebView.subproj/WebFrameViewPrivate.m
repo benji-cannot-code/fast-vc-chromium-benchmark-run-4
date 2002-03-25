f69153b4c1844d2eda5ebfbf9b56b3e91e565f76
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-25 22:18:58 +0000

    Added support for the "View Render Tree" menu. i        Added private API to allow browser to access the render tree.
    git-svn-id: svn://svn.chromium.org/blink/trunk@844 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrameViewPrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrameViewPrivate.m
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Includes from KDE
 #include <khtmlview.h>
+#include <html/html_documentimpl.h>
 
 @implementation IFWebViewPrivate
 
@@ -96,6 +97,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (KHTMLView *)_widget
 {
     return ((IFWebViewPrivate *)_viewPrivate)->widget;    
+}
+
+- (khtml::RenderObject *)_renderRoot
+{
+    KHTMLPart *part = ((IFWebViewPrivate *)_viewPrivate)->widget->part();
+    DOM::DocumentImpl *doc;
+    if (part){
+        doc = part->xmlDocImpl();
+        if (doc)
+            return doc->renderer();
+    }
+    return 0;
 }
 
 
