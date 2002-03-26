9bc6c6a2bef0abcdec7df7874129dcc112a23391
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-26 02:52:01 +0000

    Removed conditional check that prevent layout of iframes.  I don't         understand why the check was being done.
            Tweaks to force layout of frames and iframes.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@849 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kdelibs/khtml/rendering/render_flow.cpp
+++ third_party/WebKit/WebCore/src/kdelibs/khtml/rendering/render_flow.cpp
@@ -266,7 +266,9 @@ void RenderFlow::layout()
 //    kdDebug( 6040 ) << "childrenInline()=" << childrenInline() << endl;
     if(childrenInline()) {
         // ### make bidi resumeable so that we can get rid of this ugly hack
+#ifndef APPLE_CHANGES
         if (!m_blockBidi)
+#endif
             layoutInlineChildren();
     }
     else
