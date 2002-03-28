c8a7ce7a032b587cbfa1e9c7ad743b398f560fb8
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-28 03:36:43 +0000

    Private preferences.
    git-svn-id: svn://svn.chromium.org/blink/trunk@876 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/WebView.subproj/IFPreferencesPrivate.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*	
+        IFPreferences.h
+	Copyright 2001, Apple, Inc. All rights reserved.
+
+        Public header file.
+*/
+#import <Cocoa/Cocoa.h>
+
+#import <WebKit/IFPreferences.h>
+
+@interface IFPreferences (IFPrivate)
+
+- (NSTimeInterval)_initialTimedLayoutDelay;
+- (BOOL)_initialTimedLayoutEnabled;
+
+@end
+
