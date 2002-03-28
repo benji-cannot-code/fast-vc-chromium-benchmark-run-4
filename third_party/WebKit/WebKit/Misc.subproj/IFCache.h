d7d66de392c99d437ee2efb8dc1ad7b2ed3f39d4
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-28 23:18:24 +0000

    New private interface for getting at the cache.
    	* Misc.subproj/IFCache.h: New.
    	* Misc.subproj/IFCache.mm: New.
    	* WebKit.pbproj/project.pbxproj: Added IFCache files.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/Misc.subproj/IFCache.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//
+//  IFCache.h
+//  WebKit
+//
+//  Created by Darin Adler on Thu Mar 28 2002.
+//  Copyright (c) 2002 Apple Computer, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IFCache : NSObject
+{
+}
++ (NSArray *)getStatistics;
++ (void)empty;
++ (void)setDisabled:(BOOL)disabled;
+@end
