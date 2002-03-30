c0edbd1e25e5a84b244be399bc8f95b2b06ffff5
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-03-30 11:01:26 +0000

    Turned JavaScriptCore into a framework, and made it build with Project Builder in preparation for submission:
    Top level:
    	* Tests/kde/Makefile.am: Link against JavaScriptCore.framework
    	instead of the defunct libJavaScriptCore.dylib.
    	* Tests/khtmlview/WebViewTest/WebViewTest.pbproj/project.pbxproj: Likewise.
    	* Tests/qt/Makefile.am: Likewise.
    
    JavaScriptCore:
    	Converted JavaScriptCore to build with Project Builder, in
    	preparation for B&I submission.
    
    	* English.lproj/InfoPlist.strings: Added.
    	* JavaScriptCore.pbproj/.cvsignore: Added.
    	* JavaScriptCore.pbproj/project.pbxproj: Added.
    
    	* .cvsignore: Update the set of ignored things.
    
    	* Makefile.am: Hand off to PB for the main build, but still handle
    	the generated files and the test program.
    
    	* kjs/Makefile.am: Don't build anything except the generated
    	source files.
    
    	* kjs/KWQDef.h, kjs/config.h: Added minimal versions of these
    	files to get kjs to build.
    
    	Check in all the genrated files, since Project Builder isn't up to
    	the task of handling built sources:
    
    	* kjs/array_object.lut.h: Added.
    	* kjs/date_object.lut.h: Added.
    	* kjs/grammar.cpp: Added.
    	* kjs/grammar.cpp.h: Added.
    	* kjs/grammar.h: Added.
    	* kjs/lexer.lut.h: Added.
    	* kjs/math_object.lut.h: Added.
    	* kjs/number_object.lut.h: Added.
    	* kjs/string_object.lut.h: Added.
    
    	* kjs/.cvsignore: Update set of ignored things.
    
    WebCore:
    	* src/Makefile.am: Link against JavaScriptCore.framework instead
    	of the defunct libJavaScriptCore.dylib.
    
    WebKit:
    	* WebKit.pbproj/project.pbxproj: Link against
    	JavaScriptCore.framework instead of the defunct
    	libJavaScriptCore.dylib.
    
    WebBrowser:
    	* WebBrowser.pbproj/project.pbxproj: Link against
    	JavaScriptCore.framework instead of the defunct
    	libJavaScriptCore.dylib.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/JavaScriptCore/kjs/number_object.lut.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* Automatically generated from number_object.cpp using ./create_hash_table. DO NOT EDIT ! */
+
+#include "lookup.h"
+
+namespace KJS {
+
+const struct HashEntry numberTableEntries[] = {
+   { "POSITIVE_INFINITY", NumberObjectImp::PosInfinity, DontEnum, 0, 0 },
+   { "MAX_VALUE", NumberObjectImp::MaxValue, DontEnum, 0, 0 },
+   { 0, 0, 0, 0, 0 },
+   { "NaN", NumberObjectImp::NaNValue, DontEnum, 0, &numberTableEntries[5] },
+   { "MIN_VALUE", NumberObjectImp::MinValue, DontEnum, 0, 0 },
+   { "NEGATIVE_INFINITY", NumberObjectImp::NegInfinity, DontEnum, 0, 0 }
+};
+
+const struct HashTable numberTable = { 2, 6, numberTableEntries, 5 };
+
+}; // namespace
