c69a8c5203274b6902437fed3ecc2983b7253649
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-04-01 20:46:23 +0000

    Added more logging to show time of layouts.
    git-svn-id: svn://svn.chromium.org/blink/trunk@921 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFWebView.mm
+++ third_party/WebKit/WebKit/WebView.subproj/IFWebView.mm
@@ -116,22 +116,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     KHTMLView *widget = ((IFWebViewPrivate *)_viewPrivate)->widget;
 
 #define _KWQ_TIMING        
-#ifdef _KWQ_TIMING        
-    double start = CFAbsoluteTimeGetCurrent();
-#endif
-
     if (widget->part()->xmlDocImpl() && 
         widget->part()->xmlDocImpl()->renderer()){
         if (((IFWebViewPrivate *)_viewPrivate)->needsToApplyStyles){
+#ifdef _KWQ_TIMING        
+    double start = CFAbsoluteTimeGetCurrent();
+#endif
             widget->part()->xmlDocImpl()->recalcStyle(DOM::NodeImpl::Force);
             ((IFWebViewPrivate *)_viewPrivate)->needsToApplyStyles = NO;
-        }
-    }
-
 #ifdef _KWQ_TIMING        
     double thisTime = CFAbsoluteTimeGetCurrent() - start;
     WEBKITDEBUGLEVEL2 (WEBKIT_LOG_TIMING, "%s apply style seconds = %f\n", widget->part()->baseURL().url().latin1(), thisTime);
 #endif
+        }
+    }
+
 }
 
 
@@ -142,25 +141,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     KHTMLView *widget = ((IFWebViewPrivate *)_viewPrivate)->widget;
 
+    if (widget->part()->xmlDocImpl() && 
+        widget->part()->xmlDocImpl()->renderer()){
+        if (((IFWebViewPrivate *)_viewPrivate)->needsLayout){
 #ifdef _KWQ_TIMING        
     double start = CFAbsoluteTimeGetCurrent();
 #endif
 
-    if (widget->part()->xmlDocImpl() && 
-        widget->part()->xmlDocImpl()->renderer()){
-        if (((IFWebViewPrivate *)_viewPrivate)->needsLayout){
             WEBKITDEBUGLEVEL (WEBKIT_LOG_VIEW, "doing layout\n");
             //double start = CFAbsoluteTimeGetCurrent();
             widget->layout();
             //WebKitDebugAtLevel (WEBKIT_LOG_TIMING, "layout time %e\n", CFAbsoluteTimeGetCurrent() - start);
             ((IFWebViewPrivate *)_viewPrivate)->needsLayout = NO;
-        }
-    }
-
 #ifdef _KWQ_TIMING        
     double thisTime = CFAbsoluteTimeGetCurrent() - start;
     WEBKITDEBUGLEVEL2 (WEBKIT_LOG_TIMING, "%s layout seconds = %f\n", widget->part()->baseURL().url().latin1(), thisTime);
 #endif
+        }
+    }
+
 }
 
 
