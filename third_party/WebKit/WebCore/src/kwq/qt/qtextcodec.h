a809c01e319917a07aace944394686af5775dc80
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-04-03 22:17:32 +0000

    * acconfig.h: Remove a bunch of unused stuff. 	* configure.in: Remove a bunch of unused stuff. Pass "" for the 	strip flags rather than "-x" for debug case so we don't do 	any stripping at all and have symbols.
    WebCore:
    
    	* src/Makefile.am: Don't strip at all if STRIP_FLAGS are empty.
    	* src/kwq/qt/qtextcodec.h: Remove stray undef.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@957 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kwq/qt/qtextcodec.h
+++ third_party/WebKit/WebCore/src/kwq/qt/qtextcodec.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // USING_BORROWED_QSTRING ======================================================
 #ifdef USING_BORROWED_QSTRING
 
-#undef QT_NO_TEXTCODEC
 #include <_qtextcodec.h>
 
 #else
