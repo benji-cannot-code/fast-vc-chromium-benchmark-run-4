8ed7bc31466d1c0c3d04038423c1fc74c89a98a9
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-04-05 03:07:12 +0000

    Lazily dealloc controller and data source ONLY after all loads         have completed.
            Extra bulletproofing to prevent script execution when in partially
            dealloced state.
    
            Guarantee that browser document isn't dangling after
            deallocing the WebController.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@970 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFBaseWebControllerPrivate.mm
+++ third_party/WebKit/WebKit/WebView.subproj/IFBaseWebControllerPrivate.mm
@@ -24,10 +24,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return self;
 }
 
+- (void)_clearControllerReferences: (IFWebFrame *)aFrame
+{
+    NSArray *frames;
+    IFWebFrame *nextFrame;
+    int i, count;
+        
+    [aFrame _setController: nil];
+
+    // Walk the frame tree, niling the controller.
+    frames = [[aFrame dataSource] children];
+    count = [frames count];
+    for (i = 0; i < count; i++){
+        nextFrame = [frames objectAtIndex: i];
+        [self _clearControllerReferences: nextFrame];
+    }
+}
+
 - (void)dealloc
 {
+    [self _clearControllerReferences: mainFrame];
+
     [mainFrame reset];
-    [mainFrame _setController: nil];
+    
     [mainFrame autorelease];
     [super dealloc];
 }
--- third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
@@ -12,6 +12,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <WCWebDataSource.h>
 
+@interface _IFDataSourceHolder : NSObject
+{
+    IFWebDataSource *dataSource;
+}
+- initWithDataSource: (IFWebDataSource *)c;
+- (void)_checkReadyToDealloc: userInfo;
+@end
+@implementation _IFDataSourceHolder
+- initWithDataSource: (IFWebDataSource *)d
+{
+    dataSource = d;	// Non-retained
+    return [super init];
+}
+
+- (void)_checkReadyToDealloc: userInfo
+{
+    if (![dataSource isLoading])
+        [dataSource dealloc];
+    else {
+        [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector: @selector(_checkReadyToDealloc:) userInfo: nil repeats:FALSE];
+    }
+}
+@end
+
 @implementation IFWebDataSource
 
 static id IFWebDataSourceMake(void *url) 
@@ -38,9 +62,14 @@ static id IFWebDataSourceMake(void *url)
     return self;
 }
 
-- retain
-{
-    return [super retain];
+- (oneway void)release {
+    if ([self retainCount] == 1){
+        _IFDataSourceHolder *ch = [[[_IFDataSourceHolder alloc] initWithDataSource: self] autorelease];
+        [self stopLoading];
+        [NSTimer scheduledTimerWithTimeInterval:1.0 target:ch selector: @selector(_checkReadyToDealloc:) userInfo: nil repeats:FALSE];
+        return;
+    }
+    [super release];
 }
 
 - (void)dealloc
