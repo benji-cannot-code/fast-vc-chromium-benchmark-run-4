6cd097adaa14954a7faf8fcd9c5330b8a80890ab
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-04-16 22:30:07 +0000

    * src/kwq/kio/jobclasses.h: 	* src/kwq/KWQKjobclasses.mm: (TransferJob::setError): New.
    	* src/kwq/KWQKloader.mm: (-[URLLoadClient IFURLHandleResourceDidCancelLoading:]),
    	(-[URLLoadClient IFURLHandle:resourceDidFailLoadingWithResult:]):
    	Set an error code so that the load will abort intead of continuing.
    
    	* src/kwq/KWQKHTMLPart.mm: Removed some unused code.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/src/kwq/KWQKjobclasses.mm
+++ third_party/WebKit/WebCore/src/kwq/KWQKjobclasses.mm
@@ -69,7 +69,7 @@ QString Job::errorString()
 }
 
 
-void Job::kill(bool quietly=TRUE)
+void Job::kill(bool quietly)
 {
     _logNotYetImplemented();
 }
@@ -154,6 +154,11 @@ int TransferJob::error()
     return _status;
 }
 
+void TransferJob::setError(int e)
+{
+    _status = e;
+}
+
 QString TransferJob::queryMetaData(const QString &key)
 {
     NSString *_key;
@@ -176,7 +181,7 @@ void TransferJob::addMetaData(const QString &key, const QString &value)
     [d->metaData setObject:_value forKey:_key];
 }
 
-void TransferJob::kill(bool quietly=TRUE)
+void TransferJob::kill(bool quietly)
 {
     [d->handle cancelLoadInBackground];
 }
