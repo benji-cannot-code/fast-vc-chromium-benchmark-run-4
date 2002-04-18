d4951f975a299cf5b3e74c80a34f54b11a1d596c
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-04-18 01:00:30 +0000

    Reviewed by: Darin Adler  <darin@apple.com>
    	* kjs/testkjs.cpp: Don't include <iostream.h> to avoid gcc3
    	warning.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1041 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
@@ -22,7 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include <stdio.h>
+#if !APPLE_CHANGES
 #include <iostream.h>
+#endif
 
 #include "value.h"
 #include "object.h"
