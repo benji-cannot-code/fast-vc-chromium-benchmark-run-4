13686247b62ffa3f3abbcce79d63d801227ed7d1
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-04-22 14:39:52 +0000

    * Plugins.subproj/IFPluginView.mm: 	(-[IFPluginView initWithFrame:plugin:url:mime:arguments:mode:]): 	Fix the fix. It was bumping argsCount even when not putting an 	argument into the array.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1056 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPluginView.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPluginView.m
@@ -49,7 +49,6 @@ newCString(NSString *string)
 - initWithFrame:(NSRect)r plugin:(WCPlugin *)plug url:(NSString *)location mime:(NSString *)mimeType arguments:(NSDictionary *)arguments mode:(uint16)mode
 {
     NSString *baseURLString;
-    unsigned maxArguments;
 
     [super initWithFrame: r];
     
@@ -97,10 +96,8 @@ newCString(NSString *string)
         // These arrays are passed to NPP_New, but the strings need to be
         // modifiable and live the entire life of the plugin.
         
-        maxArguments = [arguments count];
-        
-        cAttributes = new char * [maxArguments];
-        cValues = new char * [maxArguments];
+        cAttributes = new char * [[arguments count]];
+        cValues = new char * [[arguments count]];
         
         NSEnumerator *e = [arguments keyEnumerator];
         NSString *key;
@@ -108,8 +105,8 @@ newCString(NSString *string)
             if (![key isEqualToString:@"wkfullmode"]) {
                 cAttributes[argsCount] = newCString(key);
                 cValues[argsCount] = newCString([arguments objectForKey:key]);
+                argsCount++;
             }
-	    argsCount++;
         }
     }
     
