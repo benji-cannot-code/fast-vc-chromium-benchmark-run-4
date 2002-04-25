fdfa5406bc92b900d50a3fd55596b6aebf7f0438
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-04-25 16:45:32 +0000

    Fixed 2911915 (Exception in -[IFWebView mouseMoved])
    	* WebView.subproj/IFWebView.mm:
    	(-[IFWebView mouseDown:]): Changed "mouseUp" to "mouseDown"
    	in exception message.
    
    	(-[IFWebView mouseMoved:]): Removed bogus event-type checking
    	that caused this method to (always?) throw an exception when
    	invoked.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrameView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrameView.m
@@ -423,7 +423,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         state = Qt::MidButton;
     }
     else {
-        [NSException raise:IFRuntimeError format:@"IFWebView::mouseUp: unknown button type"];
+        [NSException raise:IFRuntimeError format:@"IFWebView::mouseDown: unknown button type"];
         button = 0; state = 0; // Shutup the compiler.
     }
     NSPoint p = [event locationInWindow];
@@ -437,27 +437,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)mouseMoved: (NSEvent *)event
 {
-    int button, state;
-     
-    if ([event type] == NSLeftMouseDown){
-        button = Qt::LeftButton;
-        state = Qt::LeftButton;
-    }
-    else if ([event type] == NSRightMouseDown){
-        button = Qt::RightButton;
-        state = Qt::RightButton;
-    }
-    else if ([event type] == NSOtherMouseDown){
-        button = Qt::MidButton;
-        state = Qt::MidButton;
-    }
-    else {
-        [NSException raise:IFRuntimeError format:@"IFWebView::mouseUp: unknown button type"];
-        button = 0; state = 0; // Shutup the compiler.
-    }
     NSPoint p = [event locationInWindow];
     
-    QMouseEvent *kEvent = new QMouseEvent(QEvent::MouseButtonPress, QPoint((int)p.x, (int)p.y), button, state);
+    QMouseEvent *kEvent = new QMouseEvent(QEvent::MouseButtonPress, QPoint((int)p.x, (int)p.y), 0, 0);
     KHTMLView *widget = _private->widget;
     if (widget != 0l) {
         widget->viewportMouseMoveEvent(kEvent);
