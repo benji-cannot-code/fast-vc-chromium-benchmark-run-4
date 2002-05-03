9f4f644c8c52afc24b3a15a3082b431fdde1e063
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-05-03 18:07:05 +0000

    * AuthenticationManager.subproj/IFAuthenticationRequest.m: 	(-[IFAuthenticationRequest initWithURL:realm:username:passwordSentInClear:previousFailures:callback:]): 	* AuthenticationManager.subproj/IFAuthenticationResult.m: 	(-[IFAuthenticationResult initWithUsername:password:]): 	* Database.subproj/IFDatabase.m: 	(-[IFDatabase initWithPath:]): 	Simplify code by using [copy] instead of [NSString initWithString:].
    git-svn-id: svn://svn.chromium.org/blink/trunk@1091 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebDatabase.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebDatabase.m
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     if ((self = [super init])) {
     
-        path = [[NSString alloc] initWithString:[thePath stringByStandardizingPath]];
+        path = [[thePath stringByStandardizingPath] copy];
         isOpen = NO;
     
         return self;
