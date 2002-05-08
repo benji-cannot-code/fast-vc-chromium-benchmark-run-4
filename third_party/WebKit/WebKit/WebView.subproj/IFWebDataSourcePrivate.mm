34af5ef446e6ed8c111da04cfa998271d6763f73
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-05-08 18:37:00 +0000

    Forgot include.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFWebDataSourcePrivate.mm
+++ third_party/WebKit/WebKit/WebView.subproj/IFWebDataSourcePrivate.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKitDebug.h>
 #import <WebFoundation/IFURLHandle.h>
 #import <WebFoundation/IFError.h>
+#import <WebFoundation/IFNSStringExtensions.h>
 #import <WebKit/IFLocationChangeHandler.h>
 #import <khtml_part.h>
 #import "IFWebController.h"
