1993f47118c9adb8c82ff6d29978dfada1cb6e35
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-05-09 03:05:18 +0000

    Added optimizations for text rendering.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1114 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.h
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.h
@@ -12,24 +12,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef float IFGlyphWidth;
 
+
+typedef struct _WidthMap {
+    ATSGlyphRef startRange;
+    ATSGlyphRef endRange;
+    struct _WidthMap *next;
+    IFGlyphWidth *widths;
+} WidthMap;
+
+typedef struct _GlyphMap {
+    UniChar startRange;
+    UniChar endRange;
+    struct _GlyphMap *next;
+    ATSGlyphRef *glyphs;
+} GlyphMap;
+
 @interface IFTextRenderer : NSObject <WebCoreTextRenderer>
 {
+@public
     NSFont *font;
     int ascent;
     int descent;
     int lineSpacing;
     
     ATSStyleGroupPtr styleGroup;
-    ATSGlyphVector glyphVector;
     unsigned int widthCacheSize;
     IFGlyphWidth *widthCache;
-    ATSGlyphRef *characterToGlyph;
+    GlyphMap *characterToGlyphMap;
+    WidthMap *glyphToWidthMap;
     
     NSArray *substituteFontRenderers;
 }
 
 - initWithFont:(NSFont *)font;
-- (NSFont *)convertCharacters: (const unichar *)characters length: (int)numCharacters glyphs: (ATSGlyphVector *)glyphs;
+- (void)convertCharacters: (const unichar *)characters length: (int)numCharacters glyphs: (ATSGlyphVector *)glyphs;
+- (ATSGlyphRef)extendCharacterToGlyphMapToInclude:(UniChar) c;
+- (WidthMap *)extendGlyphToWidthMapToInclude:(ATSGlyphRef)glyphID;
 
 
 @end
