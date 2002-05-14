b553e91dbbf9a90cd6f3a594e0c316a985b7cb0f
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-05-14 19:16:01 +0000

    * History.subproj/IFURIEntry.h: 	Oops, deleted the prototype for setLastVisitedDate 	accidentally, which made Jersey sad.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1146 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebHistoryItem.h
+++ third_party/WebKit/WebKit/History.subproj/WebHistoryItem.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 -(void)setTitle:(NSString *)title;
 -(void)setDisplayTitle:(NSString *)displayTitle;
 -(void)setImage:(NSImage *)image;
+-(void)setLastVisitedDate:(NSCalendarDate *)date;
 
 -(unsigned)hash;
 -(BOOL)isEqual:(id)anObject;
