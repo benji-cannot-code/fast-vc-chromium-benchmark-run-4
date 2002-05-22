956ab2649d49c573a5c04a0e25a94ea280bfb139
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-05-22 23:10:39 +0000

    2002-05-22  Kenneth Kocienda  <kocienda@apple.com>
            Reviewed by: Richard Williamson
    
            Removed an assertion that was causing problems with cache object removals.
    
            * Database.subproj/IFURLFileDatabase.m:
            (-[IFURLFileDatabaseOp initWithCode:key:object:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
@@ -195,7 +195,6 @@ static void URLFileReaderInit(void)
 -(id)initWithCode:(IFURLFileDatabaseOpCode)theOpcode key:(id)theKey object:(id)theObject
 {
     WEBFOUNDATION_ASSERT_NOT_NIL(theKey);
-    WEBFOUNDATION_ASSERT_NOT_NIL(theObject);
 
     if ((self = [super init])) {
         
