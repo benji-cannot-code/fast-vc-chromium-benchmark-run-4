53a398a28726299220428296b1cd3fc9c1800345
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-05-22 20:31:44 +0000

    Fixed crasher caused by not retaining.         Fixed targeting bug.
            * Plugins.subproj/IFPluginView.mm:
            (-[IFPluginView initWithFrame:plugin:url:mime:arguments:mode:]):
            (-[IFPluginView getURLNotify:target:notifyData:]):
            (-[IFPluginView getURL:target:]):
            (-[IFPluginView postURLNotify:target:len:buf:file:notifyData:]):
            (-[IFPluginView postURL:target:len:buf:file:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1195 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/IFPluginView.mm
+++ third_party/WebKit/WebKit/Plugins.subproj/IFPluginView.mm
@@ -322,7 +322,7 @@ static char *newCString(NSString *string)
     }
     
     streams = [[NSMutableArray alloc] init];
-    notificationData = [NSMutableDictionary dictionaryWithCapacity:1];
+    notificationData = [[NSMutableDictionary dictionaryWithCapacity:1] retain];
     
     // Initialize globals
     canRestart = YES;
@@ -631,7 +631,7 @@ static char *newCString(NSString *string)
         return NPERR_INVALID_URL;
         
     if(target)
-        theTarget = [NSString stringWithCString:url];
+        theTarget = [NSString stringWithCString:target];
     
     return [self loadURL:[NSString stringWithCString:url] inTarget:theTarget withNotifyData:notifyData andHandleAttributes:nil];
 }
@@ -646,9 +646,9 @@ static char *newCString(NSString *string)
         return NPERR_INVALID_URL;
         
     if(target)
-        theTarget = [NSString stringWithCString:url];
+        theTarget = [NSString stringWithCString:target];
     
-    return [self loadURL:[NSString stringWithCString:url] inTarget:theTarget withNotifyData:nil andHandleAttributes:nil];
+    return [self loadURL:[NSString stringWithCString:url] inTarget:theTarget withNotifyData:NULL andHandleAttributes:nil];
 }
 
 -(NPError)postURLNotify:(const char *)url target:(const char *)target len:(UInt32)len buf:(const char *)buf file:(NPBool)file notifyData:(void *)notifyData
@@ -664,7 +664,7 @@ static char *newCString(NSString *string)
         return NPERR_INVALID_URL;
         
     if(target)
-        theTarget = [NSString stringWithCString:url];
+        theTarget = [NSString stringWithCString:target];
  
     if(file){
         if([[NSString stringWithCString:buf] _IF_looksLikeAbsoluteURL]){
@@ -696,7 +696,7 @@ static char *newCString(NSString *string)
         return NPERR_INVALID_URL;
         
     if(target)
-        theTarget = [NSString stringWithCString:url];
+        theTarget = [NSString stringWithCString:target];
         
     return [self postURLNotify:url target:target len:len buf:buf file:file notifyData:NULL];
 }
