7796933b605b7a34d238f88a441e719d1b94f99d
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-05-28 22:11:32 +0000

    Backed out band-aid add to fixed malformed resources URLs.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
@@ -174,8 +174,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     if (_private->urlHandles == nil)
         _private->urlHandles = [[NSMutableArray alloc] init];
-    if(handle)
-        [_private->urlHandles addObject: handle];
+    [_private->urlHandles addObject: handle];
     [self _setLoading:YES];
 }
 
