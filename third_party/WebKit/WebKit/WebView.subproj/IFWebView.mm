4bebd4ee82711ea68e169ca3df29f5b3ec5e1687
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-05-29 20:26:00 +0000

    Removed acceptsFirstMouse override, this fixes at least:
    	2930713 -- clicking on an empty part of window to bring it to front
    	takes focus away from page address field
    
    	2938028 -- Link cursor doesn't appear when browser window not
    	frontmost, but clicks activate links
    
    	* WebView.subproj/IFWebView.mm:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFWebView.mm
+++ third_party/WebKit/WebKit/WebView.subproj/IFWebView.mm
@@ -69,12 +69,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 
-- (BOOL)acceptsFirstMouse:(NSEvent *)theEvent
-{
-    return YES;
-}
-
-
 // Note that the controller is not retained.
 - (id <IFWebController>)controller
 {
