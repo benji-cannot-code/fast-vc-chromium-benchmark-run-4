5f7229cc6ffaa600f95e863ae9e9e1334d9e75c0
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-05-31 18:33:55 +0000

    * kwq/kio/jobclasses.h: Removed SimpleJob and made Job just enough for 	TransferJob to inherit from it. Removed unused stuff and made url() return 	an NSURL rather than a KURL. 	* kwq/KWQKjobclasses.mm: Removed much of the code, and reworked what 	remains to simplify it. 	* kwq/KWQKloader.mm: Update users of TransferJob::url since it now returns 	an NSURL rather than a KURL.
    	* kwq/kio/job.h: Simplify.
    	* kwq/KWQKJob.mm: Simplify.
    
    	* kwq/kio/scheduler.h: Emptied this file out.
    	* kwq/KWQKScheduler.mm: Removed.
    	* kwq/Makefile.am: Removed KWQKScheduler.mm.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1246 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKJob.mm
+++ third_party/WebKit/WebCore/kwq/KWQKJob.mm
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2001 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2001, 2002 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -24,25 +24,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#include <kwqdebug.h>
 #include <job.h>
+#include <kwqdebug.h>
 
 namespace KIO {
 
-SimpleJob *http_update_cache(const KURL &, bool, time_t)
+void http_update_cache(const KURL &, bool, time_t)
 {
     _logNotYetImplemented();
-    return 0L;
-}
-
-TransferJob *get(const KURL &url, bool reload=false, bool showProgressInfo=true)
-{
-    TransferJob *result;
-    
-    result = new TransferJob(url, reload, showProgressInfo);
-
-    return result;
 }
 
 } // namespace KIO
-
