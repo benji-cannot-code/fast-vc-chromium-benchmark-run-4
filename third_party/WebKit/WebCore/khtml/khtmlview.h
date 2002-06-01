47a338fbd86594d9ab4e1d703e7e067d469be1a9
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-01 03:57:01 +0000

    * khtml/khtmlview.h: Add KWQKHTMLPartImpl as friend class, remove khtml_part.h 	include that we added inside APPLE_CHANGES.
    	* kwq/KWQKHTMLPart.mm: (KWQKHTMLPartImpl::end): Reinstate a line of code that
    	required friend status.
    
    	* kwq/KWQView.mm: Add include of khtml_part.h.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1250 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtmlview.h
+++ third_party/WebKit/WebCore/khtml/khtmlview.h
@@ -57,9 +57,6 @@ namespace khtml {
     void applyRule(DOM::CSSProperty *prop);
 };
 
-#ifdef APPLE_CHANGES
-#include "khtml_part.h"
-#endif /* APPLE_CHANGES */
 class KHTMLPart;
 class KHTMLViewPrivate;
 
@@ -86,6 +83,9 @@ class KHTMLView : public QScrollView
     friend class khtml::RenderWidget;
     friend class khtml::CSSStyleSelector;
     friend void khtml::applyRule(DOM::CSSProperty *prop);
+#ifdef APPLE_CHANGES
+    friend class KWQKHTMLPartImpl;
+#endif
 
 public:
     /**
--- third_party/WebKit/WebCore/kwq/KWQView.mm
+++ third_party/WebKit/WebCore/kwq/KWQView.mm
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "kwqdebug.h"
 
 #include <khtmlview.h>
+#include <khtml_part.h>
 #include <qwidget.h>
 #include <qpainter.h>
 #include <qevent.h>
