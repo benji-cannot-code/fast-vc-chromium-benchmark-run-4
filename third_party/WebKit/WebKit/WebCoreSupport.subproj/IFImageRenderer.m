7b770a58e84566cd345cb446e4a2f410fc0cb441
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-01 19:40:08 +0000

    * WebCoreSupport.subproj/IFImageRenderer.m: 	(-[IFImageRenderer beginAnimationInView:inRect:fromRect:]): 	Remove an assert I added, since it is firing, and instead fixed the storage leak 	that inspired me to put the assert in.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1253 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/IFImageRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/IFImageRenderer.m
@@ -81,14 +81,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)beginAnimationInView: (NSView *)view inRect: (NSRect)ir fromRect: (NSRect)fr
 {
-    WEBKIT_ASSERT(frameView == nil);
-    
     [self stopAnimation];
     
     if ([self frameCount] > 1) {
         imageRect = fr;
         targetRect = ir;
-        frameView = [view retain];
+        [view retain];
+        [frameView release];
+        frameView = view;
         frameTimer = [[NSTimer scheduledTimerWithTimeInterval:[self frameDuration]
                                                        target:self
                                                      selector:@selector(nextFrame:)
