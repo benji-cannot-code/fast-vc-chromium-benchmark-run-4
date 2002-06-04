331fa7e5f4f29211995de1f2e4629f774c9dc736
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-04 23:27:16 +0000

    WebBrowser:
    	Added the Downloads window!
    
    	* AppController.h:
    	* AppController.m:
    	(-[AppController showReleaseNotes:]): no changes
    	(-[AppController showDownloads:]): added for menu item
    	* DownloadMonitor.h:
    	* DownloadMonitor.m: Removed protocol conforming
    	(-[DownloadMonitor init]):
    	(+[DownloadMonitor sharedDownloadMonitor]):
    	(-[DownloadMonitor windowDidLoad]): added
    	(-[DownloadMonitor updateProgressUI]): added
    	(-[DownloadMonitor _findEntryForDataSource:]): minor clean up
    	(-[DownloadMonitor monitorDownload:withProgress:error:dataSource:]): spawns progress timer
    	(-[DownloadMonitor validateUserInterfaceItem:]): added
    	(-[DownloadMonitor makeSelectedDownloadsPerformSelector:]): added
    	(-[DownloadMonitor openSelectedDownloads:]): added
    	(-[DownloadMonitor alwaysOpenSelectedDownloads:]): added
    	(-[DownloadMonitor stopSelectedDownloads:]): added
    	(-[DownloadMonitor revealSelectedDownloads:]): added
    	(-[DownloadMonitor toolbar:itemForItemIdentifier:willBeInsertedIntoToolbar:]): added
    	(-[DownloadMonitor toolbarAllowedItemIdentifiers:]): added
    	(-[DownloadMonitor toolbarDefaultItemIdentifiers:]): added
    	* DownloadProgressEntry.h:
    	* DownloadProgressEntry.m: Made DownloadProgressEntry own the download entry subview
    	(-[DownloadProgressEntry _initWithResource:progress:error:dataSource:]):
    	(-[DownloadProgressEntry dealloc]):
    	(-[DownloadProgressEntry URLStringResized:]): added
    	(-[DownloadProgressEntry awakeFromNib]): added
    	(-[DownloadProgressEntry updateProgressUI]): added
    	(-[DownloadProgressEntry open]): added
    	(-[DownloadProgressEntry alwaysOpen]): added
    	(-[DownloadProgressEntry stop]): added
    	(-[DownloadProgressEntry reveal]): added
    	(-[DownloadProgressEntry view]): added
    	* English.lproj/DownloadEntry.nib: Added.
    	* English.lproj/Downloads.nib: Added.
    	* English.lproj/MainMenu.nib: added Downloads to menu
    	* ListView.h: Added.
    	* ListView.m: Added. Simple class that appends views to each other. Implements selection.
    	(-[ListView initWithFrame:]):
    	(-[ListView dealloc]):
    	(-[ListView drawRect:]):
    	(-[ListView acceptsFirstResponder]):
    	(-[ListView mouseDown:]):
    	(-[ListView isFlipped]):
    	(-[ListView isOpaque]):
    	(-[ListView validateUserInterfaceItem:]):
    	(-[ListView addViewToList:]):
    	(-[ListView isSelectedView:]):
    	(-[ListView setDelegate:]):
    	* LocationChangeHandler.m:
    	(-[LocationChangeHandler requestContentPolicyForMIMEType:]): Better filename from URL algorithm
    	* ResourceProgressEntry.h: Made error and statusString public
    	* Resources/Images/always_open.tiff: Added.
    	* Resources/Images/open.tiff: Added.
    	* Resources/Images/reveal.tiff: Added.
    	* Resources/Images/stop.tiff: Added.
    	* WebBrowser.pbproj/project.pbxproj:
    
    WebKit:
    
    	* MIME.subproj/IFDownloadHandler.mm: Use public methods
    	* WebView.subproj/IFWebDataSource.h: Made downloadPath public
    	* WebView.subproj/IFWebDataSource.mm:
    	(-[IFWebDataSource downloadPath]):
    	* WebView.subproj/IFWebDataSourcePrivate.h:
    	* WebView.subproj/IFWebDataSourcePrivate.mm:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFWebDataSourcePrivate.mm
+++ third_party/WebKit/WebKit/WebView.subproj/IFWebDataSourcePrivate.mm
@@ -280,11 +280,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     _private->locationChangeHandler = l;
 }
 
-- (NSString *)_downloadPath
-{
-    return _private->downloadPath;
-}
-
 - (void) _setDownloadPath:(NSString *)path
 {
     [_private->downloadPath release];
