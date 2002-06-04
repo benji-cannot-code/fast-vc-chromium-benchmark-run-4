165af1055b350ffe3be45ba8159fdcf71f935fe9
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-04 05:23:40 +0000

    * LICENSE: Add 2002.
    	* kwq/KWQApplication.mm:
    	* kwq/kdecore/kapplication.h:
    	* kwq/qt/qapplication.h:
            Found a way to simplify the application objects by making more of
            the functions static so we don't need to actually allocate an object.
    
    	* kwq/Makefile.am:
    	* kwq/KWQKApplication.mm: Removed.
    	* kwq/kdecore/kapp.h: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1260 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQApplication.mm
+++ third_party/WebKit/WebCore/kwq/KWQApplication.mm
@@ -33,19 +33,19 @@ QPalette QApplication::palette(const QWidget *p)
     return pal;
 }
 
-// FIXME: Do we need to handle multiple screens?
-
-int QDesktopWidget::width() const
+int QDesktopWidget::width()
 {
     return (int)[[NSScreen mainScreen] frame].size.width;
 }
     
-int QDesktopWidget::height() const
+int QDesktopWidget::height()
 {
     return (int)[[NSScreen mainScreen] frame].size.height;
 }
 
-int QDesktopWidget::screenNumber(QWidget *) const
+// FIXME: Do we need to handle multiple screens?
+
+int QDesktopWidget::screenNumber(QWidget *)
 {
     return 0;
 }
@@ -54,17 +54,6 @@ QRect QDesktopWidget::screenGeometry(int screenNumber)
 {
     NSRect rect = [[NSScreen mainScreen] frame];
     return QRect((int)rect.origin.x, (int)rect.origin.y, (int)rect.size.width, (int)rect.size.height);
-}
-
-QDesktopWidget *QApplication::desktop()
-{
-    static QDesktopWidget desktopWidget;
-    return &desktopWidget;
-}
-
-int QApplication::startDragDistance()
-{
-     return 2;
 }
 
 QSize QApplication::globalStrut()
@@ -83,19 +72,7 @@ void QApplication::restoreOverrideCursor()
     _logNotYetImplemented();
 }
 
-bool QApplication::sendEvent(QObject *o, QEvent *e)
-{
-    return o ? o->event(e) : false;
-}
-
-void QApplication::sendPostedEvents(QObject *receiver, int event_type)
-{
-    _logNotYetImplemented();
-}
-
-QApplication *qApp = NULL;
-
-QStyle &QApplication::style() const
+QStyle &QApplication::style()
 {
     static QStyle style;
     return style;
