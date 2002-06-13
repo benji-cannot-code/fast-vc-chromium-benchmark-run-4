bdad4d5a8977eb3dae687025667ade64933627ac
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-13 08:22:44 +0000

    Removed MIME.subproj. Moved IFDownloadHandler to Misc.
            * MIME.subproj/IFDownloadHandler.h: Removed.
            * MIME.subproj/IFDownloadHandler.mm: Removed.
            * Misc.subproj/IFDownloadHandler.h: Added.
            * Misc.subproj/IFDownloadHandler.mm: Added.
            (-[IFDownloadHandler initWithDataSource:]):
            (-[IFDownloadHandler dealloc]):
            (-[IFDownloadHandler receivedData:isComplete:]):
            (-[IFDownloadHandler cancel]):
            * WebKit.pbproj/project.pbxproj:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1342 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/Misc.subproj/WebDownloadHandler.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//
+//  IFDownloadHandler.h
+//  WebKit
+//
+//  Created by Chris Blumenberg on Thu Apr 11 2002.
+//  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <WebKit/IFWebDataSource.h>
+
+@interface IFDownloadHandler : NSObject {
+    IFWebDataSource *dataSource;
+    NSFileHandle *fileHandle;
+}
+
+- initWithDataSource:(IFWebDataSource *)dSource;
+- (void)receivedData:(NSData *)data isComplete:(BOOL)isComplete;
+- (void)cancel;
+@end
