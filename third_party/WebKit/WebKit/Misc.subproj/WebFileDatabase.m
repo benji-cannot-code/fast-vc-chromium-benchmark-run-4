4c6500e288ff5660fbe893a19ffd10c7246cc106
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-15 20:04:35 +0000

    * WebFoundation.pbproj/project.pbxproj: Removed explicit PFE_FILE_C_DIALECTS now that 	Project Builder handles this automatically. Removed explicit USE_GCC3 since that's 	implicit now.
    	* CacheLoader.subproj/IFHTTPURLProtocolHandler.m:
    	* CacheLoader.subproj/IFNetworkSettings.m:
    	* CacheLoader.subproj/IFURLLoad.m:
    	* CacheLoader.subproj/IFURLLoadManager.m:
    	* CookieManager.subproj/IFCookieManager.m:
    	* Database.subproj/IFURLFileDatabase.m:
    	* Misc.subproj/IFError.m:
    	* Misc.subproj/IFFileTypeMappings.m:
    	* Misc.subproj/IFNSThreadExtensions.m:
    	Use PTHREAD_ONCE_INIT now that the warning is fixed; also make some minor improvements to how
    	it's used, making some globals static rather than extern.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1372 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
@@ -19,9 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SIZE_FILE_NAME @".size"
 #define SIZE_FILE_NAME_CSTRING ".size"
 
-// The next line is a workaround for Radar 2905545. Once that's fixed, it can use PTHREAD_ONCE_INIT.
-static void databaseInit(void);
-static pthread_once_t databaseInitControl = {_PTHREAD_ONCE_SIG_init, {}};
+static pthread_once_t databaseInitControl = PTHREAD_ONCE_INIT;
 static NSNumber *IFURLFileDirectoryPosixPermissions;
 static NSNumber *IFURLFilePosixPermissions;
 
@@ -123,9 +121,6 @@ static CFArrayRef CreateArrayListingFilesSortedByAccessTime(const char *path) {
 static NSMutableSet *notMappableFileNameSet = nil;
 static NSLock *mutex;
 
-// The next line is a workaround for Radar 2905545. Once that's fixed, it can use PTHREAD_ONCE_INIT.
-static pthread_once_t cacheFileReaderControl = {_PTHREAD_ONCE_SIG_init, {}};
-
 static void URLFileReaderInit(void)
 {
     mutex = [[NSLock alloc] init];
@@ -140,6 +135,7 @@ static void URLFileReaderInit(void)
     struct stat statInfo;
     const char *fileSystemPath;
     BOOL fileNotMappable;
+    static pthread_once_t cacheFileReaderControl = PTHREAD_ONCE_INIT;
 
     pthread_once(&cacheFileReaderControl, URLFileReaderInit);
 
