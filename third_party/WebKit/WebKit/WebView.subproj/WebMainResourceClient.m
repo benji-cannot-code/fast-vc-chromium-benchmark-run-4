b319a81aa7261200832891449ffe8ae2f86b27e7
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-15 09:42:51 +0000

    Added temporary workardound for Radar 2954901 by making the assertion more lenient.
    	* WebView.subproj/IFMainURLHandleClient.mm:
    	(-[IFMainURLHandleClient IFURLHandleResourceDidCancelLoading:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1367 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebMainResourceClient.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebMainResourceClient.m
@@ -62,8 +62,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)IFURLHandleResourceDidCancelLoading:(IFURLHandle *)sender
 {
     WEBKITDEBUGLEVEL (WEBKIT_LOG_LOADING, "url = %s\n", DEBUG_OBJECT([sender url]));
-    
+
+    // FIXME Radar 2954901: I replaced the assertion below with a more lenient one,
+    // since cancel messages are sometimes sent before begin.
+#if wEBFOUNDATION_LOAD_MESSAGES_FIXED    
     WEBKIT_ASSERT([url isEqual:[sender redirectedURL] ? [sender redirectedURL] : [sender url]]);
+#else
+    WEBKIT_ASSERT(url == nil || [url isEqual:[sender redirectedURL] ? [sender redirectedURL] : [sender url]]);
+#endif    
     
     IFLoadProgress *loadProgress = [[IFLoadProgress alloc] init];
     loadProgress->totalToLoad = -1;
