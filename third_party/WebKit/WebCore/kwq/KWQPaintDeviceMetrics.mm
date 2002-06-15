b467f357d3ac9f960ffbb64c9a6cefd27f35970b
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-15 11:55:03 +0000

    Get current bits per pixel from CoreGraphics, thereby fixing
            Radar 2943736 - JavaScript screen.colorDepth and
    screen.pixelDepth properties always returns 32,
            regardless of real depth
    
            * kwq/KWQPaintDeviceMetrics.mm:
    	(QPaintDeviceMetrics::depth):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1370 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQPaintDeviceMetrics.mm
+++ third_party/WebKit/WebCore/kwq/KWQPaintDeviceMetrics.mm
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <kwqdebug.h>
 
+#import <ApplicationServices/ApplicationServices.h>
+
+
 QPaintDeviceMetrics::QPaintDeviceMetrics(const QPaintDevice *)
 {
 }
@@ -42,8 +45,7 @@ int QPaintDeviceMetrics::logicalDpiY() const
 
 int QPaintDeviceMetrics::depth() const
 {
-    _logPartiallyImplemented();
-    return 32;
+    return CGDisplayBitsPerPixel(kCGDirectMainDisplay);
 }
 
 
