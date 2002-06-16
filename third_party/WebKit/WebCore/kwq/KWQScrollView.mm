93e9748a155c384dd803319dfb6a4aff289dbe09
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-16 07:04:34 +0000

    WebCore:
    	Fix frame-related calls. I was always searching all the frames, which was disastrous.
    	Also do name changes.
    
    	* kwq/KWQKHTMLPart.mm:
    	(KWQKHTMLPartImpl::urlSelected): Use descendantFrameNamed rather than frameNamed.
    	(KWQKHTMLPartImpl::requestFrame): Use childFrameNamed rather than frameNamed.
    	(KWQKHTMLPartImpl::frameExists): Use childFrameNamed rather than frameNamed.
    
    	* kwq/KWQScrollView.mm: (QScrollView::addChild): Asserts I used while debugging.
    
    	* kwq/WebCoreBridge.h: Add new calls. Change names of old ones to include the
    	word frame consistently. (Also bits of new work that will do no harm, but are
    	not really needed yet.)
    
    WebKit:
    
    	Update for frame-related changes in WebCore.
    
    	* WebCoreSupport.subproj/IFWebCoreBridge.mm:
    	(-[IFWebFrame _bridge]): Look at provisional data source first.
    	(-[IFWebCoreBridge dealloc]): Release the data source.
    	(-[IFWebCoreBridge parentFrame]): Renamed.
    	(-[IFWebCoreBridge childFrames]): Renamed.
    	(-[IFWebCoreBridge childFrameNamed:]): Added.
    	(-[IFWebCoreBridge descendantFrameNamed:]): Added.
    	(-[IFWebCoreBridge didFinishLoadingWithHandle:]): Don't call _didStopLoading twice
    	when we finish loading.
    
    	Also changed controller getters to call [dataSource controller] and got rid of the
    	[IFWebCoreBridge controller] method.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQScrollView.mm
+++ third_party/WebKit/WebCore/kwq/KWQScrollView.mm
@@ -190,9 +190,11 @@ void QScrollView::addChild(QWidget* child, int x, int y)
 {
     NSView *thisView, *thisDocView, *subView;
 
+    KWQ_ASSERT(child != this);
+
     if (child->x() != x || child->y() != y)
         child->move(x, y);
-        
+    
     thisView = getView();
     thisDocView = [thisView _IF_getDocumentView];
     if (thisDocView)
@@ -202,6 +204,8 @@ void QScrollView::addChild(QWidget* child, int x, int y)
     if ([subView _IF_isScrollView]) {
         subView = [subView superview];
     }
+    
+    KWQ_ASSERT(subView != thisView);
 
     if ([subView superview] == thisView) {
         return;
