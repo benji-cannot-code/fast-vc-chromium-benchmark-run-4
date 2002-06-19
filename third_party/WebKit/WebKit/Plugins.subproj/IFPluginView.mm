62d4db967f105dc672f56f633dab5a6f2ce1f1a5
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-19 18:51:04 +0000

    WebKit:
            Call setWindow in layout so that plug-in content is drawn without
            needing to resize.
    
            * Plugins.subproj/IFPluginView.mm:
            (-[IFPluginView layout]):
            (-[IFPluginView viewHasMoved:]):
    
    WebBrowser:
    
            Fix for 2956361 - use redirected name for downloaded files
            Simplified mime parsing in Defaults.plist
    
            * Defaults.plist:
            * DownloadProgressEntry.m:
            (-[DownloadProgressEntry _initWithResource:progress:error:dataSource:]):
            * LocationChangeHandler.m:
            (-[LocationChangeHandler requestContentPolicyForMIMEType:dataSource:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/IFPluginView.mm
+++ third_party/WebKit/WebKit/Plugins.subproj/IFPluginView.mm
@@ -549,6 +549,7 @@ static char *newCString(NSString *string)
     
     [self setFrame:NSMakeRect(0, 0, superFrame.size.width, superFrame.size.height)];
     [self setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    [self setWindow];
 }
 
 
@@ -578,9 +579,9 @@ static char *newCString(NSString *string)
 
 -(void) viewHasMoved:(NSNotification *)notification
 {
-    [self sendUpdateEvent];
+    [self sendUpdateEvent]; 
     [self setWindow];
-    
+
     // reset the tracking rect
     [self removeTrackingRect:trackingTag];
     trackingTag = [self addTrackingRect:[self bounds] owner:self userData:nil assumeInside:NO];
