1e52a55ddd125a1a93ab120867547d5e3fa8dec9
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-26 20:44:34 +0000

    - fixed 2971024 -- wrong item is bookmarked
    	-[IFWebDataSource redirectedURL] was returning the URL of a
    	sub-page resource.
    
            * WebCoreSupport.subproj/IFResourceURLHandleClient.m:
            (-[IFResourceURLHandleClient IFURLHandle:didRedirectToURL:]):
    	Removed evil call to _setFinalURL here. It had no right to live.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/IFResourceURLHandleClient.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/IFResourceURLHandleClient.m
@@ -180,7 +180,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     WEBKIT_ASSERT(currentURL != nil);
     WEBKIT_ASSERT([URL isEqual:[handle redirectedURL]]);
 
-    [dataSource _setFinalURL:URL];
     [[dataSource _bridge] setURL:URL];
 
     // FIXME: We do want to tell the client about redirects.
