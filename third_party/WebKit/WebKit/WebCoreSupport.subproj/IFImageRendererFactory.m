4b69ce0f87d928e9a1e24230f4302502b6eed922
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-26 19:35:57 +0000

    * WebCoreSupport.subproj/IFImageRendererFactory.m:         (-[IFImageRendererFactory imageRenderer]): Fix NSBitmapImageRep leak.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/IFImageRendererFactory.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/IFImageRendererFactory.m
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     NSBitmapImageRep *rep = [[NSBitmapImageRep alloc] initForIncrementalLoad];
     [imageRenderer addRepresentation: rep];
+    [rep release];
     [imageRenderer setFlipped: YES];
 
     [imageRenderer setScalesWhenResized: NO];
