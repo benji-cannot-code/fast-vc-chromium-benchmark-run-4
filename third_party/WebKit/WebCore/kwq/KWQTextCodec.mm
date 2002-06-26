70baedf00c02c45792c918f3a305baff0e090dfe
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-26 19:01:59 +0000

    - fixed 2969378 -- "A with hat" characters (Unicode 00C2) showing up at Zingermans.com
            * kwq/KWQCharsets.mm: (buildDictionaries): Put UTF-16 in the dictionary, but pointing
    	at the string encoding for UTF-8. This mimics what Internet Explorer does.
    
            * kwq/KWQtextcodec.mm: (QTextCodec::fromUnicode): Fix implementation so it will convert
    	the specified encoding instead of always Latin-1.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextCodec.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextCodec.mm
@@ -66,7 +66,7 @@ QTextCodec *QTextCodec::codecForName(const char *name, int accuracy)
 {
     CFStringRef cfname;
     CFStringEncoding encoding;
-
+    
     cfname = CFStringCreateWithCString(NULL, name, kCFStringEncodingASCII);
 
     encoding = KWQCFStringEncodingFromIANACharsetName(cfname);
@@ -91,7 +91,7 @@ const char *QTextCodec::name() const
 
 int QTextCodec::mibEnum() const
 {
-  return KWQCFStringEncodingToMIB(encoding);
+    return KWQCFStringEncodingToMIB(encoding);
 }
 
 QTextDecoder *QTextCodec::makeDecoder() const
@@ -101,7 +101,14 @@ QTextDecoder *QTextCodec::makeDecoder() const
 
 QCString QTextCodec::fromUnicode(const QString &qcs) const
 {
-    return QCString(qcs.latin1());
+    CFStringRef cfs = qcs.getCFMutableString();
+    CFRange range = CFRangeMake(0, CFStringGetLength(cfs));
+    CFIndex bufferLength;
+    CFStringGetBytes(cfs, range, encoding, '?', false, NULL, 0x7FFFFFFF, &bufferLength);
+    QCString result(bufferLength + 1);
+    CFStringGetBytes(cfs, range, encoding, '?', false, (UInt8 *)result.data(), bufferLength, &bufferLength);
+    result[bufferLength] = 0;
+    return result;
 }
 
 QString QTextCodec::toUnicode(const char *chs, int len) const
