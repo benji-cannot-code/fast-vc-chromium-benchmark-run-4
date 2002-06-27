c7697c7c8cd145f0937668a5447691d9d8157c2d
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-27 16:29:13 +0000

    Removed old methods and references.
            * WebCoreSupport.subproj/IFTextRenderer.m:
            (-[IFTextRenderer drawString:atPoint:withColor:]):
            (-[IFTextRenderer slowDrawCharacters:length:fromCharacterPosition:toCharacterPosition:atPoint:withTextColor:backgroundColor:attemptFontSubstitution:]):
            (-[IFTextRenderer _drawCharacters:length:fromCharacterPosition:toCharacterPosition:atPoint:withTextColor:backgroundColor:]):
            Fixed final fragment rendering glitch.
    
            Removed calls to old method.
    
            * kwq/KWQPainter.mm:
            (QPainter::drawText):
            * kwq/WebCoreTextRenderer.h:
            Removed old methods.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1455 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreTextRenderer.h
+++ third_party/WebKit/WebCore/kwq/WebCoreTextRenderer.h
@@ -33,10 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (int)descent;
 - (int)lineSpacing;
 
-- (void)drawString:(NSString *)string atPoint:(NSPoint)point withColor:(NSColor *)color;
-- (void)drawCharacters:(const UniChar *)characters length: (unsigned)length atPoint:(NSPoint)point withTextColor:(NSColor *)color;
-- (void)drawUnderlineForString:(NSString *)string atPoint:(NSPoint)point withColor:(NSColor *)color;
 - (void)drawCharacters:(const UniChar *)characters stringLength: (unsigned int)length fromCharacterPosition: (int)from toCharacterPosition: (int)to atPoint:(NSPoint)point withTextColor:(NSColor *)textColor backgroundColor: (NSColor *)backgroundColor;
+
+- (void)drawString:(NSString *)string atPoint:(NSPoint)point withColor:(NSColor *)color;
+
+- (void)drawUnderlineForString:(NSString *)string atPoint:(NSPoint)point withColor:(NSColor *)color;
 
 - (void)drawString:(NSString *)string inRect:(NSRect)rect withColor:(NSColor *)color paragraphStyle:(NSParagraphStyle *)style;
 
