3f79bf36ec6fd1a76f0cf6c1f4a6df79e60983ac
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-06-28 23:53:46 +0000

    * WebCoreSupport.subproj/IFImageRenderer.h: Renamed statusOfCache to 	patternColorLoadStatus for clarity.         * WebCoreSupport.subproj/IFImageRenderer.m:         (-[IFImageRenderer copyWithZone:]): Add FIXME.         (-[IFImageRenderer tileInRect:fromPoint:]): Add optimization for the 	case where a single draw of the image will cover the entire area to be 	tiled. Also add comments and make other small improvements.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.h
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSView *frameView;
     NSRect imageRect;
     NSRect targetRect;
-    NSColor *patternColor;
     int loadStatus;
-    int statusOfCache;
+    NSColor *patternColor;
+    int patternColorLoadStatus;
 }
 
 + (void)stopAnimationsInView: (NSView *)aView;
