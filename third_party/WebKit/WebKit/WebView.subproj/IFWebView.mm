7b74cdee861fb579b00412db8faf60297881f68e
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-02 20:57:13 +0000

    Made IFWebView fill with white when there's no document view. This fixes:
    	- 2978210 -- problems with "hide status bar"
    	- 2978742 -- Resizing initially-empty Alexander window results in garbage in window
    
            * WebView.subproj/IFWebView.mm:
            (-[IFWebView drawRect:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1493 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFWebView.mm
+++ third_party/WebKit/WebKit/WebView.subproj/IFWebView.mm
@@ -190,6 +190,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)drawRect:(NSRect)rect
 {
+    if ([self documentView] == nil) {
+        // Need to paint ourselves if there's no documentView to do it instead.
+        [[NSColor whiteColor] set];
+        NSRectFill(rect);
+    }
 }
 
 - (NSWindow *)window
