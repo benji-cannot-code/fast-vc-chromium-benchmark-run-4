c86ae36750e7f16dcc9b083f8084e8e7dc06d5da
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-02 15:16:56 +0000

    * WebView.subproj/IFMainURLHandleClient.mm: Fix comment.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/IFMainURLHandleClient.mm
+++ third_party/WebKit/WebKit/WebView.subproj/IFMainURLHandleClient.mm
@@ -22,8 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebFoundation/IFError.h>
 #import <WebFoundation/IFFileTypeMappings.h>
 
-// FIXME: This is almost completely redundant with the KWQURLLoadClient in WebCore.
-// We shouldn't have two almost-identical classes that don't share code.
+// FIXME: This is quite similar IFResourceURLHandleClient; they should share code.
 
 @implementation IFMainURLHandleClient
 
