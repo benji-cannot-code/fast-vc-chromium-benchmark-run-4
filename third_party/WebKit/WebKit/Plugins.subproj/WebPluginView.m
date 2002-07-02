9e6ee88c198f11b86d7e9097926c6474ae36fe2b
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-02 21:45:14 +0000

    WebKit:
            * Plugins.subproj/IFPluginView.mm:
            (-[IFPluginView setWindow]), (-[IFPluginView start]), (-[IFPluginView stop]):
    	Fix unused variable issue that affects deployment builds.
    
    WebBrowser:
    
            * LocationChangeHandler.m: Improve comments.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPluginView.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPluginView.m
@@ -401,7 +401,10 @@ static char *newCString(NSString *string)
 {
     [self setUpWindowAndPort];
 
-    NPError npErr = NPP_SetWindow(instance, &window);
+#ifndef NDEBUG
+    NPError npErr =
+#endif
+    NPP_SetWindow(instance, &window);
     WEBKITDEBUGLEVEL(WEBKIT_LOG_PLUGINS, "NPP_SetWindow: %d, port=0x%08x\n", npErr, (int)nPort.port);
 
 #if 0
@@ -416,7 +419,6 @@ static char *newCString(NSString *string)
 
 -(void)start
 {
-    NPError npErr;
     NSNotificationCenter *notificationCenter;
     NSWindow *theWindow;
     IFPluginStream *stream;
@@ -426,7 +428,10 @@ static char *newCString(NSString *string)
     
     isStarted = YES;
     
-    npErr = NPP_New((char *)[mime cString], instance, fullMode ? NP_FULL : NP_EMBED, argsCount, cAttributes, cValues, NULL);
+#ifndef NDEBUG
+    NPError npErr =
+#endif
+    NPP_New((char *)[mime cString], instance, fullMode ? NP_FULL : NP_EMBED, argsCount, cAttributes, cValues, NULL);
     WEBKITDEBUGLEVEL(WEBKIT_LOG_PLUGINS, "NPP_New: %d\n", npErr);
     
     // Create a WindowRef is one doesn't already exist
@@ -475,8 +480,6 @@ static char *newCString(NSString *string)
 
 - (void)stop
 {
-    NPError npErr;
-    
     if (!isStarted)
         return;
     isStarted = NO;
@@ -501,7 +504,10 @@ static char *newCString(NSString *string)
     [webFrame release];
     [webDataSource release];    
     
-    npErr = NPP_Destroy(instance, NULL);
+#ifndef NDEBUG
+    NPError npErr =
+#endif
+    NPP_Destroy(instance, NULL);
     WEBKITDEBUGLEVEL(WEBKIT_LOG_PLUGINS, "NPP_Destroy: %d\n", npErr);
 }
 
