5d59ddd057d9ae211c37279226718554dda09b06
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-03 22:58:55 +0000

    - Fix for 2975750 	- Removed Java workaround since that will be fixed soon.
            * Plugins.subproj/IFPlugin.h:
            * Plugins.subproj/IFPlugin.m:
            (-[IFPlugin _openResourceFile]): added
            (-[IFPlugin _closeResourceFile:]): added
            (-[IFPlugin _getPluginInfo]): rename
            (-[IFPlugin initWithPath:]): simplified
            (-[IFPlugin load]): calls _openResourceFile
            (-[IFPlugin unload]): calls _closeResourceFile
            * Plugins.subproj/IFPluginView.mm:
            (-[IFPluginView setUpWindowAndPort]): removed Java workaround
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1512 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPlugin.h
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPlugin.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     uint16 pluginVersion;
     CFBundleRef bundle;
     CFragConnectionID connID; 
+    SInt16 resourceRef;
     
     NPP_NewProcPtr NPP_New;
     NPP_DestroyProcPtr NPP_Destroy;
