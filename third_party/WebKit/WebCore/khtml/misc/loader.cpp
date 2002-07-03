95b72f7baef2a585ab433b7c7caa8bbcf7f50ba9
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-03 19:34:58 +0000

    A small KURL speed improvement.
            * khtml/misc/loader.cpp:
            (Cache::requestImage):
            (Cache::requestStyleSheet):
            (Cache::requestScript):
    	Don't do the isMalformed check, because it's expensive and has no clear benefit.
    	There's no harm in caching null results for malformed URLs.
    
            * kwq/KWQKURL.mm:
            (KURL::KURL): Make the default constructor faster by not running all the
    	normalize code on the empty string.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/misc/loader.cpp
+++ third_party/WebKit/WebCore/khtml/misc/loader.cpp
@@ -1342,6 +1342,9 @@ CachedImage *Cache::requestImage( DocLoader* dl, const DOMString & url, bool rel
         cachePolicy = KIO::CC_Verify;
     }
 
+#ifdef APPLE_CHANGES
+    // Checking if the URL is malformed is lots of extra work for little benefit.
+#else
     if( kurl.isMalformed() )
     {
 #ifdef CACHE_DEBUG
@@ -1349,6 +1352,7 @@ CachedImage *Cache::requestImage( DocLoader* dl, const DOMString & url, bool rel
 #endif
       return 0;
     }
+#endif
 
     CachedObject *o = 0;
     if (!reload)
@@ -1417,11 +1421,15 @@ CachedCSSStyleSheet *Cache::requestStyleSheet( DocLoader* dl, const DOMString & 
         cachePolicy = KIO::CC_Verify;
     }
 
+#ifdef APPLE_CHANGES
+    // Checking if the URL is malformed is lots of extra work for little benefit.
+#else
     if( kurl.isMalformed() )
     {
       kdDebug( 6060 ) << "Cache: Malformed url: " << kurl.url() << endl;
       return 0;
     }
+#endif
 
     CachedObject *o = cache->find(kurl.url());
     if(!o)
@@ -1497,11 +1505,15 @@ CachedScript *Cache::requestScript( DocLoader* dl, const DOM::DOMString &url, bo
         cachePolicy = KIO::CC_Verify;
     }
 
+#ifdef APPLE_CHANGES
+    // Checking if the URL is malformed is lots of extra work for little benefit.
+#else
     if( kurl.isMalformed() )
     {
       kdDebug( 6060 ) << "Cache: Malformed url: " << kurl.url() << endl;
       return 0;
     }
+#endif
 
     CachedObject *o = cache->find(kurl.url());
     if(!o)
