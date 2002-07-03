bea21c1b1b42e38a4c25e5c5c19740244e795a49
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-03 02:49:38 +0000

    Fixed asseration failure caused by an iframe not having a correct mime type.         We now assume that local files without extensions are html files.
            * WebView.subproj/IFWebControllerPrivate.mm:
            (+[IFWebController _MIMETypeForFile:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebViewPrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebViewPrivate.m
@@ -210,6 +210,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     NSString *extension = [path pathExtension];
     
+    if([extension isEqualToString:@""])
+        return @"text/html";
+        
     return [[IFFileTypeMappings sharedMappings] MIMETypeForExtension:extension];
 }
 
