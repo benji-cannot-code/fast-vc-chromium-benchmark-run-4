dad2e8d39ce2e73bdd7f7ba286992a6a38e16078
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-04 20:21:10 +0000

    WebKit:
            * WebView.subproj/IFDynamicScrollBarsView.h:
    	Updated FIXME to mention bug number.
    
    WebBrowser:
    
    	- finished a few details for 2953570 and 2953580 -- Open and
    	Reveal in Downloads window don't handle	missing-file case well
    	- fixed 2981774 -- Status bar text not centered vertically
    	- removed vestiges of ancient mechanism for swapping browser
    	window view between webview & search modes
    
            * English.lproj/Browser.nib: Made the status bar a few pixels shorter.
    	We may decide that it looks bad for it to be a different height than the
    	favorites bar, but Don wanted to try this. Also made IFWebView a direct
    	subview of the window's contentView, and eliminated the extra views that
    	were part of the ancient web/search mode mechanism.
    
            * BrowserWindow.h: Removed tabView outlet.
    
            * BrowserWindow.m:
            (-[BrowserWindow toggleFavoritesBarWithAnimation:]):
            (-[BrowserWindow toggleStatusBarWithAnimation:]):
    	Made these methods use webView instead of tabView.
    
            * DownloadMonitor.m:
            (-[DownloadMonitor _canOpenSelectedDownloads]):
            (-[DownloadMonitor _canStopSelectedDownloads]):
            (-[DownloadMonitor _canRevealSelectedDownloads]):
            (-[DownloadMonitor _canForgetSelectedDownloads]):
    	Renamed these from "_totalSelectedAndXXXXable" and made them return BOOLs rather
    	than ints, since that's all the callers cared about. Made them (except _canReveal)
    	return as soon as a single valid entry is found (_canReveal has to make sure there
    	is exactly one valid entry). Made _canOpen and _canReveal return YES even if file
    	doesn't exist, so that the user will get the explanatory message if they click on
    	Open or Reveal in this case.
    
            (-[DownloadMonitor validateUserInterfaceItem:]):
    	Updated for method name and return-value changes; restructured so that it returns
    	as soon as the right selector is checked, rather than continuing to check against
    	other selectors.
    
    	(-[DownloadMonitor _showMissingFileErrorForOperation:andFile:]): Removed this
    	method; it was constructing messages using substitution patterns that might
    	not work in other languages.
    
            (-[DownloadMonitor openSelectedDownloads:]):
    	Made the error message handle the multiple-missing-files case properly; let
    	all openable items be opened regardless of how many unopenable items there are
    	(formerly bailed at the first unopenable item). Moved sheet-displaying code
    	for this case into here.
    
            (-[DownloadMonitor revealSelectedDownloads:]): Moved sheet-displaying code
    	for this case into here.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDynamicScrollBarsView.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebDynamicScrollBarsView.h
@@ -11,9 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebCoreFrameView.h>
 
-// FIXME: This has grown to be more than just a dynamic scroll bar view.
-// We want to rename this class at least, and perhaps break it up into
-// more than one class.
+// FIXME 2980779: This has grown to be more than just a dynamic scroll bar view,
+// and it no longer works outside of WebKit.
 
 @interface IFDynamicScrollBarsView : NSScrollView <WebCoreFrameView>
 {
