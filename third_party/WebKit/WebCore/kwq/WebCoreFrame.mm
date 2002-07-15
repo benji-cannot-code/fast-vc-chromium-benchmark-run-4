36048981ae9491277ce00ad76d554f92f9640deb
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-15 06:51:23 +0000

    WebCore:
            * kwq/WebCoreFrame.mm: (-[WebCoreFrame dealloc]): Fix NULL-dereferencing
    	problem that was hidden by the "leak the world" bug in WebBrowser.
    
    WebBrowser:
    
    	Fix "leak the world" bug.
    
            * BrowserDocument.m: (-[BrowserDocument close]): Having the web controller
    	as its own window context creates a reference cycle. As with the other similar
    	cases here, break the cycle by explicitly setting it to nil.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreFrame.mm
+++ third_party/WebKit/WebCore/kwq/WebCoreFrame.mm
@@ -15,7 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)dealloc
 {
-    renderPart->deref();
+    if (renderPart) {
+        renderPart->deref();
+    }
     [super dealloc];
 }
 
