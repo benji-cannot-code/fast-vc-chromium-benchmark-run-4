d417f5508a89b37660892db9169b514532abab49
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-15 15:50:57 +0000

    top level:
            * Tests/qt/*.cpp:
    	Removed unnecessary includes of <config.h>.
    
    WebCore:
    
            * WebCore.pbproj/project.pbxproj: Removed unused qpopupmenu.h.
            * kwq/kdecore/ksimpleconfig.h: Removed unnecessary include of <config.h>.
            * kwq/kio/global.h: Removed unnecessary include of <config.h>.
            * kwq/kio/kprinter.h: Emptied out this file.
            * kwq/qt/qpopupmenu.h: Removed.
            * kwq/qt/qstylesheet.h: Removed unnecessary include of <config.h>.
            * kwq/qt/qtextedit.h: Removed unnecessary include of <config.h>.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/qt/qtextedit.h
+++ third_party/WebKit/WebCore/kwq/qt/qtextedit.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef QTEXTEDIT_H_
 #define QTEXTEDIT_H_
 
-#include <config.h>
 #include <qscrollview.h>
 
 class QTextEdit : public QScrollView
