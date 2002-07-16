6ceaaa870ebeac42c38cadba6d27236cf3b4a5af
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-16 23:17:47 +0000

    WebCore:
    	- fixed 2997244 -- crash when visiting cnn.com followed by another page
    
    	Turns out the requestFrame code I've been tinkering with used to be
    	dead code until we accidentally re-triggered the code path.
    
            * khtml/html/html_baseimpl.cpp: (HTMLFrameElementImpl::attach):
    	Remove APPLE_CHANGES that were causing us to reuse the same frame which
    	were an attempt to fix a frame targeting problem that was actually elsewhere.
    
            * kwq/KWQKHTMLPartImpl.mm: (KWQKHTMLPartImpl::requestFrame): Remove the code
    	that handles the case of an already-existing frame with the same name. This
    	is handled at a higher level.
    
            * kwq/WebCoreBridge.h: Remove now-unneeded childFrameNamed method.
    
    WebKit:
    
            * WebCoreSupport.subproj/WebBridge.m:
    	Remove childFrameNamed since it's not needed, and every one of these bridge
    	methods adds potential confusion.
    	(-[WebBridge descendantFrameNamed:]): Make this look for descendants in the
    	provisional data sources first. This is the same change Richard made to
    	childFrameNamed, but it turns out that this is the method that's really used.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreBridge.h
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.h
@@ -117,7 +117,6 @@ typedef khtml::RenderPart KHTMLRenderPart;
 
 - (WebCoreFrameBridge *)frame;
 - (NSArray *)childFrames; // WebCoreFrameBridge objects
-- (WebCoreFrameBridge *)childFrameNamed:(NSString *)name;
 - (WebCoreFrameBridge *)descendantFrameNamed:(NSString *)name;
 
 - (WebCoreFrameBridge *)mainFrame;
--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -61,19 +61,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return frameBridges;
 }
 
-- (WebCoreFrameBridge *)childFrameNamed:(NSString *)name
-{
-    WebDataSource *pd;
-    
-    pd = [[dataSource webFrame] provisionalDataSource];
-    if (pd)
-        return [[pd frameNamed: name] _frameBridge];
-
-    return [[dataSource frameNamed:name] _frameBridge];
-}
-
 - (WebCoreFrameBridge *)descendantFrameNamed:(NSString *)name
 {
+    WebCoreFrameBridge *provisionalBridge = [[[[dataSource webFrame] provisionalDataSource] frameNamed:name] _frameBridge];
+    if (provisionalBridge) {
+        return provisionalBridge;
+    }
     return [[[dataSource webFrame] frameNamed:name] _frameBridge];
 }
 
