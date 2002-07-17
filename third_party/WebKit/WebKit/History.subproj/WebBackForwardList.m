48c722080b1374f1009d622446af8a8865775afb
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-17 04:26:54 +0000

    WebKit:
            * History.subproj/WebBackForwardList.m: Replace MyCompanyName with Apple Computer.
            (-[WebBackForwardList init]): Fix strange [super init] logic to be standard.
            * History.subproj/WebHistoryItem.h: Replace MyCompanyName with Apple Computer.
            * Misc.subproj/WebDownloadHandler.h: Replace MyCompanyName with Apple Computer.
    
    WebBrowser:
    
            * LocationTextField.h: Replace MyCompanyName with Apple Computer.
            * LocationTextField.m: Replace MyCompanyName with Apple Computer.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
+++ third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //  WebKit
 //
 //  Created by Kenneth Kocienda on Thu Nov 29 2001.
-//  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2001 Apple Computer, Inc. All rights reserved.
 //
 
 #import "WebBackForwardList.h"
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 -(id)init
 {
-    if (self != [super init])
-    {
+    self = [super init];
+    if (!self) {
         return nil;
     }
     
--- third_party/WebKit/WebKit/Misc.subproj/WebDownloadHandler.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebDownloadHandler.h
@@ -4,13 +4,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //  WebKit
 //
 //  Created by Chris Blumenberg on Thu Apr 11 2002.
-//  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2002 Apple computer, Inc. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import <WebKit/WebDataSource.h>
 
-@interface WebDownloadHandler : NSObject {
+@class WebDataSource;
+
+@interface WebDownloadHandler : NSObject
+{
     WebDataSource *dataSource;
     NSFileHandle *fileHandle;
 }
