e3f33711979826ece4ae72e0f566bdfbf02edd5a
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-17 00:11:01 +0000

    Fixed field length test.  We were testing >= instead of >.         * kwq/KWQNSTextField.mm:         (-[KWQNSTextFieldFormatter isPartialStringValid:newEditingString:errorDescription:]):
    git-svn-id: svn://svn.chromium.org/blink/trunk@1575 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQNSTextField.mm
+++ third_party/WebKit/WebCore/kwq/KWQNSTextField.mm
@@ -219,7 +219,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (BOOL)isPartialStringValid:(NSString *)partialString newEditingString:(NSString **)newString errorDescription:(NSString **)error
 {
-    if ((int)[partialString length] >= maxLength){
+    if ((int)[partialString length] > maxLength){
         *newString = nil;
         return NO;
     }
