28d2924cd10cd2e1585b1ad5e42a03c739ae3fcf
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-19 19:58:25 +0000

    * History.subproj/WebHistoryItem.m:         (-[WebHistoryItem image]): call [WebIconLoader defaultIcon]         * Misc.subproj/WebIconLoader.m:         (+[WebIconLoader defaultIcon]): move bundle image loading code from [WebHistoryItem image] to here.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1602 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebHistoryItem.m
+++ third_party/WebKit/WebKit/History.subproj/WebHistoryItem.m
@@ -7,8 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //  Copyright (c) 2001, 2002 Apple Computer, Inc. All rights reserved.
 //
 
-#import "WebHistoryItem.h"
-#import "WebKitReallyPrivate.h"
+#import <WebKit/WebHistoryItem.h>
+#import <WebKit/WebIconLoader.h>
+#import <WebKit/WebKitReallyPrivate.h>
 
 #import <WebFoundation/WebNSURLExtras.h>
 
@@ -66,26 +67,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 -(NSImage *)image
-{
-    static NSImage *defaultImage = nil;
-    static BOOL loadedDefaultImage = NO;
-    
+{    
     if (_image != nil) {
         return _image;
     }
 
-    // Attempt to load default image only once, to avoid performance penalty of repeatedly
-    // trying and failing to find it.
-    if (!loadedDefaultImage) {
-        NSString *pathForDefaultImage =
-            [[NSBundle bundleForClass:[WebHistoryItem class]] pathForResource:@"url_icon" ofType:@"tiff"];
-        if (pathForDefaultImage != nil) {
-            defaultImage = [[NSImage alloc] initByReferencingFile: pathForDefaultImage];
-        }
-        loadedDefaultImage = YES;
-    }
-
-    return defaultImage;
+    return [WebIconLoader defaultIcon];
 }
 
 -(NSCalendarDate *)lastVisitedDate
--- third_party/WebKit/WebKit/Misc.subproj/WebIconLoader.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebIconLoader.m
@@ -37,7 +37,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 + (NSImage *)defaultIcon
 {
-    return [NSImage imageNamed:@"url_icon"];
+    static NSImage *defaultIcon = nil;
+    static BOOL loadedDefaultImage = NO;
+    
+    // Attempt to load default image only once, to avoid performance penalty of repeatedly
+    // trying and failing to find it.
+    if (!loadedDefaultImage) {
+        NSString *pathForDefaultImage =
+            [[NSBundle bundleForClass:[self class]] pathForResource:@"url_icon" ofType:@"tiff"];
+        if (pathForDefaultImage != nil) {
+            defaultIcon = [[NSImage alloc] initByReferencingFile: pathForDefaultImage];
+        }
+        loadedDefaultImage = YES;
+    }
+
+    return defaultIcon;
 }
 
 - initWithURL:(NSURL *)iconURL
