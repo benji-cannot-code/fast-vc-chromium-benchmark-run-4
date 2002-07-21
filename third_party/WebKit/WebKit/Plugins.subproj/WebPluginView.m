e71f1a5f070119efc6b56600dda81594ce782675
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-21 04:59:14 +0000

    * WebKit.pbproj/project.pbxproj: Turn stricter warnings back on now that we 	don't use C++ any more.
            * Plugins.subproj/WebPluginView.m:
            (-[WebPluginView sendActivateEvent:]): Fix cast that stricter warning didn't like.
            (-[WebPluginView sendUpdateEvent]): Fix cast that stricter warning didn't like.
            (-[WebPluginView frameStateChanged:]): Remove unneeded cast that stricter warning didn't like.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1613 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPluginView.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPluginView.m
@@ -113,7 +113,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     
     [self getCarbonEvent:&event];
     event.what = activateEvt;
-    event.message = (UInt32)[[self window] _windowRef];
+    WindowRef windowRef = [[self window] _windowRef];
+    event.message = (UInt32)windowRef;
     if (activate)
         event.modifiers |= activMask;
     
@@ -129,7 +130,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     
     [self getCarbonEvent:&event];
     event.what = updateEvt;
-    event.message = (UInt32)[[self window] _windowRef];
+    WindowRef windowRef = [[self window] _windowRef];
+    event.message = (UInt32)windowRef;
 
     acceptedEvent = NPP_HandleEvent(instance, &event); 
     
@@ -682,8 +684,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         return;
     
     notifyData = [notifyDataValue pointerValue];
-    frameState = (WebFrameState)[[[notification userInfo] objectForKey:WebCurrentFrameState] intValue];
-    if(frameState == WebFrameStateComplete){
+    frameState = [[[notification userInfo] objectForKey:WebCurrentFrameState] intValue];
+    if (frameState == WebFrameStateComplete) {
         NPP_URLNotify(instance, [[url absoluteString] cString], NPRES_DONE, notifyData);
     }
     //FIXME: Need to send other NPReasons
