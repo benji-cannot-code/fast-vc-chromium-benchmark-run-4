89919e4ad2b3ee95ab6036495773c475e8f30591
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-07-24 22:54:59 +0000

    Call [self _locationChangeHandler] instead of _private-locationChangeHandler
            * WebView.subproj/WebDataSourcePrivate.m:
            (-[WebDataSource receivedPageIcon:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1662 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
@@ -469,7 +469,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)receivedPageIcon:(NSImage *)image
 {
-    [_private->locationChangeHandler receivedPageIcon:image forDataSource:self];
+    [[self _locationChangeHandler] receivedPageIcon:image forDataSource:self];
 }
 
 - (void)_loadIcon
