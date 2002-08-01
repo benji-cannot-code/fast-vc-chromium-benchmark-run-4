f0775b8b3829287d2c264a71bce60aa5dfb6844c
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-01 20:09:01 +0000

    Tweaked WebCore cache settings. The WebCore cache will now grow 	to 1 MB (was 512k). The maximum cacheable item is 16k instead of 40k.
            * khtml/misc/loader.cpp
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1717 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/misc/loader.cpp
+++ third_party/WebKit/WebCore/khtml/misc/loader.cpp
@@ -32,9 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "loader.h"
 
 // up to which size is a picture for sure cacheable
-#define MAXCACHEABLE 40*1024
+#define MAXCACHEABLE 16*1024
 // default cache size
-#define DEFCACHESIZE 512*1024
+#define DEFCACHESIZE 1024*1024
 
 #include <qasyncio.h>
 #include <qasyncimageio.h>
