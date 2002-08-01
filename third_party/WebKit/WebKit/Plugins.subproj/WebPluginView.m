12d0ade84a75fbc110ad633618a9f16df4ccda50
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-01 21:09:10 +0000

    Fixed: 2970530 - Command keys are not getting passed in to plugins
            * Plugins.subproj/WebPluginView.m:
            (-[WebPluginView performKeyEquivalent:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1719 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPluginView.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPluginView.m
@@ -280,6 +280,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
 }
 
+- (BOOL)performKeyEquivalent:(NSEvent *)theEvent
+{
+    EventRecord event;
+    bool acceptedEvent;
+
+    [self getCarbonEvent:&event withEvent:theEvent];
+    event.what = keyDown;
+
+    if(event.message == 0){
+        event.message = [self keyMessageForEvent:theEvent];
+    }
+
+    acceptedEvent = NPP_HandleEvent(instance, &event);
+
+    WEBKITDEBUGLEVEL(WEBKIT_LOG_PLUGINS, "NPP_HandleEvent(performKeyEquivalent): %d charCode:%c keyCode:%lu\n",
+                     acceptedEvent, (char) (event.message & charCodeMask), (event.message & keyCodeMask));
+    
+    return acceptedEvent;
+}
+
 // Must subclass menuForEvent: for right-click to work.
 - (NSMenu *)menuForEvent:(NSEvent *)theEvent
 {
