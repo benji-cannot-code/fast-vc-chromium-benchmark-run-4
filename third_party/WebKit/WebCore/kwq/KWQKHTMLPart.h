29da0ecc0d9914be43730cdff7fb7ce85ef17b58
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-01 21:48:56 +0000

    Remove KWQKHTMLPartImpl's setBaseURL method and m_baseURL field, 	since nothing should really need them any more.
            * kwq/KWQKHTMLPartImpl.h:
            * kwq/KWQKHTMLPartImpl.mm:
            (KWQKHTMLPartImpl::begin): Set base URL on document, if it exists.
            (KWQKHTMLPartImpl::requestFrame): Get base URL from document.
            (KWQKHTMLPartImpl::requestObject): Ditto.
            (KWQKHTMLPartImpl::frames): Ditto.
            (KWQKHTMLPartImpl::setBaseURL): Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1721 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKHTMLPart.h
+++ third_party/WebKit/WebCore/kwq/KWQKHTMLPart.h
@@ -80,8 +80,6 @@ public:
 
     bool gotoBaseAnchor();
 
-    void setBaseURL(const KURL &);
-
     void setTitle(const DOM::DOMString &);
     void setStatusBarText(const QString &status);
 
@@ -117,8 +115,6 @@ private:
     WebCoreBridge *bridge;
 
     int m_redirectionTimer;
-    
-    KURL m_baseURL;
     
     friend class KHTMLPart;
 };
