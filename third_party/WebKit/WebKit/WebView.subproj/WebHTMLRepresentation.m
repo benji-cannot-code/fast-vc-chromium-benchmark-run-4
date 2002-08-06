68b59c9f33f5ec37b6d52a00d4f30dcc41b0ce5b
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-06 06:04:05 +0000

    WebCore:
    	Fixed crashers and leaks that come up when changing the view
    	associated with a part.
    
            * khtml/khtmlview.cpp:
            (KHTMLView::~KHTMLView): Don't zero out the part's view, because
    	that will mess up setting a new one.
    
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge createKHTMLViewWithNSView:width:height:marginWidth:marginHeight:]):
    	Destroy the old view if we own it.
    
    WebKit:
    
    	fixed 2974993 - window.open does not return the right window object
    	fixed 3017140 - WebKit creates a new KHTMLPart per page; should
    	reuse it like KHTML does
    
            * WebView.subproj/WebHTMLRepresentation.m:
            (-[WebHTMLRepresentation setDataSource:]): Don't swap the part at
    	this point. Now it gets reused.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1746 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLRepresentation.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLRepresentation.m
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setDataSource:(WebDataSource *)dataSource
 {
-    [[dataSource webFrame] _changeBridge];
     _private->bridge = [[dataSource webFrame] _bridge];
 }
 
