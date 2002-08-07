006d97e4065cadcf59c9114dfc7b33d17c988a76
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-07 19:19:19 +0000

    Fixed scrollview related drawing turd problems.         (3000844 and 2896459).
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView drawRect:]):
            * WebView.subproj/WebView.m:
            (-[WebView setFrame:]):
    
            Removed debug log.
    
            * kwq/KWQFrame.mm:
            (QFrame::setFrameStyle):
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge drawRect:withPainter:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1769 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -344,6 +344,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     WEBKITDEBUGLEVEL (WEBKIT_LOG_VIEW, "%s drawing\n", DEBUG_OBJECT(self));
 
+    if ([self inLiveResize]){
+        if (!NSEqualRects(rect, [self visibleRect])){
+            rect = [self visibleRect];
+            [self setNeedsLayout: YES];
+        }
+    }
+
     [self reapplyStyles];
 
     [self layout];
