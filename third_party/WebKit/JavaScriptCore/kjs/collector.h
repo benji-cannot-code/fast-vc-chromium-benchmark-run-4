0138b177596b415c9b3f82e833984b1c7cffd2f8
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-09 23:24:41 +0000

    - fixed 2948835 - JavaScriptCore locking is too fine grained, makes it too slow
    	* kjs/collector.cpp:
            (Collector::allocate):
            (Collector::collect):
            (Collector::finalCheck):
            (Collector::numInterpreters):
            (Collector::numGCNotAllowedObjects):
            (Collector::numReferencedObjects):
            * kjs/collector.h:
            * kjs/internal.cpp:
            (initializeInterpreterLock):
            (lockInterpreter):
            (unlockInterpreter):
            (Parser::parse):
            (InterpreterImp::InterpreterImp):
            (InterpreterImp::clear):
            (InterpreterImp::evaluate):
            * kjs/value.cpp:
            (ValueImp::ValueImp):
            (ValueImp::setGcAllowed):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/collector.h
+++ third_party/WebKit/JavaScriptCore/kjs/collector.h
@@ -93,8 +93,6 @@ namespace KJS {
     static int numInterpreters();
     static int numGCNotAllowedObjects();
     static int numReferencedObjects();
-    static void lock();
-    static void unlock();
 #endif
   private:
     static CollectorBlock* root;
