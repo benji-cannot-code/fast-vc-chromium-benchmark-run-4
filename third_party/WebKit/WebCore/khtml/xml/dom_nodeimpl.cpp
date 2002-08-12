c6f9cbeb18fc409ecd248ab2ccdaa84372b373fa
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-12 23:57:25 +0000

    Make the news ticker on livepage.apple.com scroll properly. 	It still doesn't clip though.  Fixing this involved correctly 	dirtying the render tree when dynamic DOM changes occurred 	(this was a bug in KHTML's DOM), as well as providing an 	implementation of the MSIE offsetWidth extension that took 	inlines into account (KHTML's impl was always returning a width 	of 0 for inlines).
            * khtml/ecma/kjs_dom.cpp:
            (DOMNode::getValueProperty):
            * khtml/rendering/render_flow.cpp:
            (RenderFlow::offsetWidth):
            (RenderFlow::offsetHeight):
            * khtml/rendering/render_flow.h:
            * khtml/rendering/render_object.h:
            * khtml/xml/dom_nodeimpl.cpp:
            (NodeBaseImpl::insertBefore):
            (NodeBaseImpl::replaceChild):
            (NodeBaseImpl::appendChild):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
@@ -1074,8 +1074,14 @@ NodeImpl *NodeBaseImpl::insertBefore ( NodeImpl *newChild, NodeImpl *refChild, i
 
         // Add child to the rendering tree
         // ### should we detach() it first if it's already attached?
-        if (attached() && !child->attached())
+        if (attached() && !child->attached()) {
             child->attach();
+            // This is extremely important, as otherwise a fresh layout
+            // isn't scheduled, and you end up with stale data (especially
+            // with inline runs of text). -dwh
+            if (child->renderer())
+                child->renderer()->setLayouted(false);
+        }
 
         // Dispatch the mutation events
         dispatchChildInsertedEvents(child,exceptioncode);
@@ -1143,8 +1149,14 @@ NodeImpl *NodeBaseImpl::replaceChild ( NodeImpl *newChild, NodeImpl *oldChild, i
 
         // Add child to the rendering tree
         // ### should we detach() it first if it's already attached?
-        if (attached() && !child->attached())
+        if (attached() && !child->attached()) {
             child->attach();
+            // This is extremely important, as otherwise a fresh layout
+            // isn't scheduled, and you end up with stale data (especially
+            // with inline runs of text). -dwh
+            if (child->renderer())
+                child->renderer()->setLayouted(false);
+        }
 
         // Dispatch the mutation events
         dispatchChildInsertedEvents(child,exceptioncode);
@@ -1290,9 +1302,15 @@ NodeImpl *NodeBaseImpl::appendChild ( NodeImpl *newChild, int &exceptioncode )
 
         // Add child to the rendering tree
         // ### should we detach() it first if it's already attached?
-        if (attached() && !child->attached())
+        if (attached() && !child->attached()) {
             child->attach();
-
+            // This is extremely important, as otherwise a fresh layout
+            // isn't scheduled, and you end up with stale data (especially
+            // with inline runs of text). -dwh
+            if (child->renderer())
+                child->renderer()->setLayouted(false);
+        }
+          
         // Dispatch the mutation events
         dispatchChildInsertedEvents(child,exceptioncode);
 
