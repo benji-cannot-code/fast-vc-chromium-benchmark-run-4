9b4dcb6383d020a8c83f31e2a5f59700dc7edb22
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-13 07:39:10 +0000

    Small speed improvement. 3% faster on cvs-js-performance, no 	measurable change on cvs-static-urls.
            * kjs/collector.cpp:
            (Collector::collect): Combine 3 loops over all objects into one,
    	to reduce flat time and improve locality of reference.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/collector.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/collector.cpp
@@ -208,6 +208,7 @@ bool Collector::collect()
   block = root;
   while (block) {
     ValueImp **r = (ValueImp**)block->mem;
+      int del = 0;
     for (int i = 0; i < block->size; i++, r++) {
       ValueImp *imp = (*r);
       // Can delete if refcount==0, created==true, gcAllowed==true, and marked==false
@@ -218,17 +219,6 @@ bool Collector::collect()
         //fprintf( stderr, "Collector::deleting ValueImp %p (%s)\n", (void*)imp, typeid(*imp).name());
         imp->~ValueImp();
       }
-    }
-    block = block->next;
-  }
-
-  // 2nd step: free memory
-  block = root;
-  while (block) {
-    ValueImp **r = (ValueImp**)block->mem;
-    int del = 0;
-    for (int i = 0; i < block->size; i++, r++) {
-      ValueImp *imp = (*r);
       if (imp && (imp->_flags & ValueImp::VI_DESTRUCTED) != 0) {
 	free(imp);
         *r = 0L;
@@ -237,14 +227,8 @@ bool Collector::collect()
     }
     filled -= del;
     block->filled -= del;
-    block = block->next;
     if (del)
       deleted = true;
-  }
-
-  // delete the empty containers
-  block = root;
-  while (block) {
     CollectorBlock *next = block->next;
     if (block->filled == 0) {
       if (block->prev)
@@ -260,6 +244,7 @@ bool Collector::collect()
     }
     block = next;
   }
+
 #if 0
   // This is useful to track down memory leaks
   static int s_count = 0;
