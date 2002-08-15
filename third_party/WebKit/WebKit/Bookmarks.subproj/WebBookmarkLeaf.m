5ef206252a2b037d654b386c5ec019dbcca5981b
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-15 23:14:42 +0000

    WebKit:
    	- fixed 3025770 -- "New Bookmark" button hits assertion and crashes
    
            * Bookmarks.subproj/WebBookmarkLeaf.m:
            (-[WebBookmarkLeaf setURLString:]): Instead of asserting that parameter
    	is not nil, handle the nil case by converting to empty string. This was
    	another regression from yesterday's favIcon changes.
    
    WebBrowser:
    
    	- fixed remaining case in 3015942 -- Using Bookmarks New Separator button causes
    	loss of keyboard focus
    	- fixed 3023789 -- Find dialog should hide on deactivate to match Mail/TextEdit
    
            * BookmarksController.m:
            (-[BookmarksController _focusOnNewlyCreatedBookmark:]): Make outline view
    	first responder here.
    
            * OutlineViewPlus.m:
            (-[OutlineViewPlus textDidEndEditing:]): restructured a little.
    
    	* English.pbproj/Find.nib: checked "Hide on deactivate" checkbox;
    	actually I checked this in accidentally with my previous checkin, so
    	this comment is late.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1833 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Bookmarks.subproj/WebBookmarkLeaf.m
+++ third_party/WebKit/WebKit/Bookmarks.subproj/WebBookmarkLeaf.m
@@ -151,7 +151,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setURLString:(NSString *)URLString
 {
-    WEBKIT_ASSERT_VALID_ARG (URLString, URLString != nil);
+    if (URLString == nil) {
+        URLString = @"";
+    }
+    
     if ([URLString isEqualToString:_URLString]) {
         return;
     }
