3b87d45cb11155e52feb48deba1e6ef44902d7a1
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-19 00:31:59 +0000

    * kjs/internal.cpp: (KJS::printInfo): Remove one more CompletionType 	that Maciej missed.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/internal.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/internal.cpp
@@ -1082,9 +1082,6 @@ void KJS::printInfo(ExecState *exec, const char *s, const Value &o, int lineno)
     case ListType:
       name = "List";
       break;
-    case CompletionType:
-      name = "Completion";
-      break;
     default:
       break;
     }
