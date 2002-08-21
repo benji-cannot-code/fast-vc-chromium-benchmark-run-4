2cbb14e28d6a16891d8581912d2c3c661c2e7d30
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-21 23:31:49 +0000

    * kwq/KWQNSViewExtras.m: (-[NSClipView _KWQ_scrollRectToVisible:inView:]): 	Use bounds, not visibleRect. Seems to work better.
    git-svn-id: svn://svn.chromium.org/blink/trunk@1894 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQNSViewExtras.m
+++ third_party/WebKit/WebCore/kwq/KWQNSViewExtras.m
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)_KWQ_scrollRectToVisible:(NSRect)rect inView:(NSView *)view
 {
     NSRect exposeRect = [self convertRect:rect fromView:view];
-    NSRect visibleRect = [self visibleRect];
+    NSRect visibleRect = [self bounds];
     
     if (!NSContainsRect(visibleRect, exposeRect)) {
         // Make an expose rectangle that will end up centering the passed-in rectangle horizontally.
