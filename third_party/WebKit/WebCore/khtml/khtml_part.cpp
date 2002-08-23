5e705bd73ae6287092e15a81953a1d1d1e2e8a24
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-08-23 03:39:31 +0000

    top level:
    	Fix test results to no longer reflect KURL or CFURL oddities that
    	we are not emulating.
    
            * Tests/kde/kurl-test.chk:
            * Tests/kde/kurl-test.cpp:
            (testURL): Replay mistaken n with \n.
    
    WebCore:
    
    	Brand new KURL implementation which avoids CFURL, CFString, and
    	all that other allocation-happy goodness and instead does as much
    	work as possible with raw ASCII buffers.
    
    	For me this gave a ~5% performance improvement on cvs-base.
    
            * kwq/KWQKURL.mm:
            (KURL::KURL):
            (KURL::hasPath):
            (KURL::protocol):
            (KURL::host):
            (KURL::port):
            (KURL::pass):
            (KURL::user):
            (KURL::ref):
            (KURL::query):
            (KURL::path):
            (KURL::setProtocol):
            (KURL::setHost):
            (KURL::setPort):
            (KURL::setRef):
            (KURL::setQuery):
            (KURL::setPath):
            (KURL::prettyURL):
            (KURL::decode_string):
            (escapeBadChars):
            (KURL::parse):
            (KURL::getNSURL):
            * kwq/kdecore/kurl.h:
            * khtml/khtml_part.cpp:
            (KHTMLPart::end): Don't call KURL::clearCaches() any more.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtml_part.cpp
+++ third_party/WebKit/WebCore/khtml/khtml_part.cpp
@@ -1452,10 +1452,6 @@ void KHTMLPart::end()
         write(d->m_decoder->flush());
     if (d->m_doc)
 	d->m_doc->finishParsing();
-
-#ifdef APPLE_CHANGES
-    KURL::clearCaches();
-#endif
 }
 
 #ifndef APPLE_CHANGES
