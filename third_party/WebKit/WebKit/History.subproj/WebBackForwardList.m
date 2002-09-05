d26ef4ef4341679df4a026b26811f3fa3e35c163
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-05 06:24:43 +0000

    One more pass of refinement on that last change.
            * History.subproj/WebBackForwardList.m: (-[WebBackForwardList addEntry:]):
    	Replace the last entry instead of just ignoring if this one matches.
    
            * History.subproj/WebHistoryList.h: Add replaceEntryAtIndex. Remove addURL: and removeURL:.
    
            * History.subproj/WebHistoryList.m:
            (-[WebHistoryList addEntry:]): Call removeEntry: rather than using an entire pasted copy here.
            (-[WebHistoryList removeEntry:]): Use a hash of the URL and the URL rather than relying on the
    	fact that WebHistoryItem hashes and does isEqual based on the URL only.
            (-[WebHistoryList replaceEntryAtIndex:withEntry:]): Added. Used by the code above.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
+++ third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
@@ -36,13 +36,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 -(void)addEntry:(WebHistoryItem *)entry
 {
-    // If the last entry matches this new entry, don't add a new one, since we are
-    // presumably just doing a reload.
+    // If the last entry matches this new entry, then replace it rather than adding
+    // a new one, since we are doing a reload.
     if ([uriList count] > index) {
         WebHistoryItem *lastEntry = [uriList entryAtIndex:index];
         if ([[lastEntry URL] isEqual:[entry URL]]
             && ([lastEntry target] == [entry target] || [[lastEntry target] isEqual:[entry target]])
             && ([lastEntry parent] == [entry parent] || [[lastEntry parent] isEqual:[entry parent]])) {
+            [uriList replaceEntryAtIndex:index withEntry:entry];
             return;
         }
     }
