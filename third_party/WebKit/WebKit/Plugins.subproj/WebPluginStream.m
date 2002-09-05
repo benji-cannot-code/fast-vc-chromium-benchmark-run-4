1bdb8aebeca5ade683582db0de44f3c6fc49d0ba
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-05 20:38:08 +0000

    Fixed 3021365 - Crash failing to load plugin from local file
            * Plugins.subproj/WebPluginStream.h:
            * Plugins.subproj/WebPluginStream.m:
            (-[WebNetscapePluginStream receivedData:withHandle:]):
            (-[WebNetscapePluginStream receivedError:]):
            (-[WebNetscapePluginStream receivedData:withDataSource:]):
            (-[WebNetscapePluginStream handleDidReceiveData:data:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1970 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPluginStream.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPluginStream.m
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 	Copyright (c) 2002, Apple, Inc. All rights reserved.
 */
 
+#import <WebKit/npapi.h>
 #import <WebKit/WebLoadProgress.h>
 #import <WebKit/WebPluginStream.h>
 #import <WebKit/WebView.h>
@@ -17,10 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebFoundation/WebResourceRequest.h>
 
 @interface WebNetscapePluginStream (ClassInternal)
-- (void)receivedData:(NSData *)data;
+- (void)receivedData:(NSData *)data withHandle:(WebResourceHandle *)handle;
 - (void)receivedError:(NPError)error;
 - (void)finishedLoadingWithData:(NSData *)data;
-- (void)setUpGlobalsWithHandle:(WebResourceHandle *)handle;
 @end
 
 @interface WebNetscapePluginStream (WebResourceClient) <WebResourceClient>
@@ -114,33 +114,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     view = nil;
 }
 
-- (void)setUpGlobalsWithHandle:(WebResourceHandle *)handle
-{
-    NSString *URLString = [[handle URL] absoluteString];
-    char *cURL = (char *)malloc([URLString cStringLength]+1);
-    [URLString getCString:cURL];
-
-    npStream.ndata = self;
-    npStream.URL = cURL;
-    npStream.end = 0;
-    npStream.lastmodified = 0;
-    npStream.notifyData = notifyData;
-    offset = 0;
-    
-    mimeType = [[handle contentType] retain];
-}
-
-- (void)receivedData:(NSData *)data
-{
-    int32 numBytes;
-    NPError npErr;
-    
+- (void)receivedData:(NSData *)data withHandle:(WebResourceHandle *)handle
+{    
     if(isFirstChunk){
-        isFirstChunk = NO;
+
+        NSString *mimeType = [handle contentType];
+        NSString *URLString = [[handle URL] absoluteString];
+        char *cURL = (char *)malloc([URLString cStringLength]+1);
+        [URLString getCString:cURL];
+
+        NSNumber *timeInterval = [handle attributeForKey:@"Last-Modified"];
+        uint32 lastModified;
         
-        //FIXME: Need a way to check if stream is seekable
+        if(timeInterval){
+            NSTimeInterval lastModifiedInterval = [[NSDate dateWithTimeIntervalSinceReferenceDate:[timeInterval doubleValue]] timeIntervalSince1970];
+            if(lastModifiedInterval < 0){
+                lastModified = 0;
+            }else{
+                lastModified = (uint32)lastModifiedInterval;
+            }
+        }else{
+            lastModified = 0;
+        }
         
-        npErr = NPP_NewStream(instance, (char *)[mimeType cString], &npStream, NO, &transferMode);
+        npStream.ndata = self;
+        npStream.URL = cURL;
+        npStream.end = [handle contentLength];
+        npStream.lastmodified = lastModified;
+        npStream.notifyData = notifyData;
+        
+        offset = 0;
+        
+        // FIXME: Need a way to check if stream is seekable
+        
+        NPError npErr = NPP_NewStream(instance, (char *)[mimeType cString], &npStream, NO, &transferMode);
         WEBKITDEBUGLEVEL(WEBKIT_LOG_PLUGINS, "NPP_NewStream: %d %s\n", npErr, [[URL absoluteString] cString]);
         
         if(npErr != NPERR_NO_ERROR){
@@ -159,6 +166,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             [self stop];
             return;
         }
+        
+        isFirstChunk = NO;
     }
 
     if(transferMode == NP_ASFILE || transferMode == NP_ASFILEONLY) {
@@ -167,7 +176,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
 
     if(transferMode != NP_ASFILEONLY){
-        numBytes = NPP_WriteReady(instance, &npStream);
+        int32 numBytes = NPP_WriteReady(instance, &npStream);
         WEBKITDEBUGLEVEL(WEBKIT_LOG_PLUGINS, "NPP_WriteReady bytes=%lu\n", numBytes);
         
         numBytes = NPP_Write(instance, &npStream, offset, [data length], (void *)[data bytes]);
@@ -179,10 +188,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)receivedError:(NPError)error
 {
-    NPError npErr;
-    
-    npErr = NPP_DestroyStream(instance, &npStream, error);
-    WEBKITDEBUGLEVEL(WEBKIT_LOG_PLUGINS, "NPP_DestroyStream: %d\n", npErr);
+    // Don't report error before we've called NPP_NewStream
+    if(!isFirstChunk){
+        NPError npErr = NPP_DestroyStream(instance, &npStream, error);
+        WEBKITDEBUGLEVEL(WEBKIT_LOG_PLUGINS, "NPP_DestroyStream: %d\n", npErr);
+    }
 }
 
 - (void)finishedLoadingWithData:(NSData *)data
@@ -230,10 +240,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         instance = [view pluginInstance];
 
         [self getFunctionPointersFromPluginView:view];
-        [self setUpGlobalsWithHandle:[dataSource _mainHandle]];
     }
-    
-    [self receivedData:data];
+
+    [self receivedData:data withHandle:[dataSource _mainHandle]];
 }
 
 - (void)receivedError:(WebError *)error withDataSource:(WebDataSource *)dataSource
@@ -270,10 +279,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     WebController *webController = [view webController];
 
-    if(isFirstChunk){
-        [self setUpGlobalsWithHandle:handle];
-    }
-    [self receivedData:data];
+    [self receivedData:data withHandle:handle];
     
     [webController _receivedProgress:[WebLoadProgress progressWithResourceHandle:handle]
         forResourceHandle: handle fromDataSource: [view webDataSource] complete: NO];
