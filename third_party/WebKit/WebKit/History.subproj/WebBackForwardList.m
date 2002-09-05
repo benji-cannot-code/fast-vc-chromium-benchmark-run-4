4d55aea705af501b73b53f67bc4d802796111d21
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-05 23:35:54 +0000

    Removed debugging.
            * khtml/khtml_part.cpp:
            (KHTMLPart::closeURL):
    
            Removed debugging.
    
            * History.subproj/WebBackForwardList.m:
            (-[WebBackForwardList addEntry:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
+++ third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 -(void)addEntry:(WebHistoryItem *)entry
 {
-    printf ("addEntry %s\n", [[[entry URL] absoluteString] cString]);
     // If the last entry matches this new entry, then replace it rather than adding
     // a new one, since we are doing a reload.
     if ([uriList count] > index) {
