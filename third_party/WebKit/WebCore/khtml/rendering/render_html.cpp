248d735b408666b64d6d8027642911e2c421af52
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-08 09:48:48 +0000

    Landing the new layer system and turning it on for painting of 	all types of positioned blocks (and for painting the root 	and HTML layers as well).
    	Event handling is not yet using the new layer system, so you
    	may see oddities like DHTML menus now displaying correctly but
    	events not working (since the event code may still be stacking
    	incorrectly and think the DHTML is behind other content).
    
            * khtml/css/cssstyleselector.cpp:
            * khtml/rendering/render_box.cpp:
            (RenderBox::setStyle):
            (RenderBox::calcHeight):
            (RenderBox::calcAbsoluteHorizontal):
            * khtml/rendering/render_container.cpp:
            (RenderContainer::addChild):
            (RenderContainer::removeChildNode):
            * khtml/rendering/render_flow.cpp:
            (RenderFlow::setStyle):
            (RenderFlow::printObject):
            (RenderFlow::printSpecialObjects):
            (RenderFlow::layout):
            (RenderFlow::layoutSpecialObjects):
            (RenderFlow::layoutBlockChildren):
            (RenderFlow::insertSpecialObject):
            * khtml/rendering/render_flow.h:
            * khtml/rendering/render_html.cpp:
            (RenderHtml::layout):
            * khtml/rendering/render_image.cpp:
            (RenderImage::printObject):
            * khtml/rendering/render_layer.cpp:
            (RenderLayer::updateLayerPosition):
            (RenderLayer::enclosingAncestor):
            (RenderLayer::addChild):
            (RenderLayer::convertToLayerCoords):
            (RenderLayer::paint):
            (RenderLayer::constructZTree):
            (RenderLayer::RenderZTreeNode::constructLayerList):
            * khtml/rendering/render_layer.h:
            * khtml/rendering/render_replaced.cpp:
            (RenderReplaced::print):
            (RenderWidget::printObject):
            * khtml/rendering/render_root.cpp:
            (RenderRoot::calcHeight):
            (RenderRoot::layout):
            (RenderRoot::printObject):
            * khtml/rendering/render_root.h:
            * khtml/rendering/render_style.cpp:
            (StyleBoxData::StyleBoxData):
            (StyleBoxData::operator==):
            * khtml/rendering/render_style.h:
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge drawRect:withPainter:]):
            * kwq/qt/qrect.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@1994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_html.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_html.cpp
@@ -122,6 +122,9 @@ void RenderHtml::layout()
     if( m_height + margins < lp )
 	m_height = lp - margins;
 
+    layer()->setHeight(m_height);
+    layer()->setWidth(m_width);
+    
     //kdDebug(0) << "docHeight = " << m_height << endl;
 }
 
