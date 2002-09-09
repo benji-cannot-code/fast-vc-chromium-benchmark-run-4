46696d018aa9d0642bed0cbbf63055893baaff69
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-09 21:52:39 +0000

    Missed one call through asking WebResourceHandle for contentLength 	instead of WebResourceResponse.
            * Plugins.subproj/WebPluginStream.m:
            (-[WebNetscapePluginStream receivedData:withHandle:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2013 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPluginStream.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPluginStream.m
@@ -142,7 +142,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         
         npStream.ndata = self;
         npStream.URL = cURL;
-        npStream.end = [handle contentLength];
+        npStream.end = [[handle response] contentLength];
         npStream.lastmodified = lastModified;
         npStream.notifyData = notifyData;
         
