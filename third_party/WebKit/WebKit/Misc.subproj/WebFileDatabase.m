02d7eda680d61ebe01c6eb1b1dc8e5a7005d9437
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-09 17:47:24 +0000

    I removed WebCacheLoaderConstantsPrivate.h, but did not check for who 	was importing that file. Duh!
    	Includes now fixed.
    
            * CacheLoader.subproj/WebCacheObject.m:
            * CacheLoader.subproj/WebDiskCache.m:
            * CacheLoader.subproj/WebMemoryCache.m:
            * CacheLoader.subproj/WebResourceHandle.m:
            * CacheLoader.subproj/WebResourceHandleInternal.h:
            * CacheLoader.subproj/WebResourceHandlePrivate.h:
            * CacheLoader.subproj/WebResourceLoad.h:
            * Database.subproj/WebFileDatabase.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "WebFileDatabase.h"
 #import "WebNSFileManagerExtras.h"
-#import "WebCacheLoaderConstantsPrivate.h"
 #import "WebFoundationLogging.h"
 
 #define SIZE_FILE_NAME @".size"
