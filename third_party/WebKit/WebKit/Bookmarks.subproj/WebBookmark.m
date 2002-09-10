2db28c7e1ee9bafc73164a02e3b707a74f722dd4
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-10 00:33:11 +0000

    - fixed 3003605 -- Leaking 1 WebView after second run of static PLT
    	Fixing this should also help with footprint.
    
            * WebView.subproj/WebFrame.m:
            (-[WebFrame dealloc]): Remove old workaround for problem where we
    	always deallocated the frames from timer cleanup. I found a better
    	fix for this.
            (-[WebFrame stopLoading]): Explicitly fire the scheduled layout
    	timer if any when we stop loading, rather than leaving it hanging.
    
            * WebView.subproj/WebFramePrivate.m: (-[WebFrame _setState:]):
    	Explicitly fire the scheduled layout timer if any.
    
            * WebView.subproj/WebDataSource.m: (-[WebDataSource isLoading]):
    	Use an autorelease pool here so we don't end up putting all the
    	frames in the main event loop's autorelease pool.
    
            * Bookmarks.subproj/WebBookmark.m:
            (-[WebBookmark init]): Use copy instead of retain.
            (-[WebBookmark identifier]): Use copy instead of retain.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2016 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Bookmarks.subproj/WebBookmark.m
+++ third_party/WebKit/WebKit/Bookmarks.subproj/WebBookmark.m
@@ -31,7 +31,7 @@ static unsigned _highestUsedID = 0;
 - (id)init
 {
     [super init];
-    _identifier = [[WebBookmark _generateUniqueIdentifier] retain];
+    _identifier = [[WebBookmark _generateUniqueIdentifier] copy];
     return self;
 }
 
@@ -46,7 +46,7 @@ static unsigned _highestUsedID = 0;
 - (NSString *)identifier
 {
     ASSERT(_identifier != nil);
-    return [[_identifier retain] autorelease];
+    return [[_identifier copy] autorelease];
 }
 
 - (id)copyWithZone:(NSZone *)zone
--- third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
@@ -227,16 +227,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if ([_private->subresourceClients count]) {
 	return YES;
     }
-     
+    
+    // Put in the auto-release pool because it's common to call this from a run loop source,
+    // and then the entire list of frames lasts until the next autorelease.
+    NSAutoreleasePool *pool = [NSAutoreleasePool new];
     NSEnumerator *e = [[self children] objectEnumerator];
     WebFrame *childFrame;
     while ((childFrame = [e nextObject])) {
         if ([[childFrame dataSource] isLoading] || [[childFrame provisionalDataSource] isLoading]) {
-            return YES;
+            break;
         }
     }
+    [pool release];
     
-    return NO;
+    return childFrame != nil;
 }
 
 
