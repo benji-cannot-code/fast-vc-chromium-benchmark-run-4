c3be7f0c05bc2045fc6ed126820507fb80858528
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-13 06:48:20 +0000

    Don't initiate a drag if an autoscroll happened.
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView mouseDragged:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2056 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -424,13 +424,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)mouseDragged:(NSEvent *)event
 {
     // Ensure that we're visible wrt the event location.
-    [self autoscroll:event];
+    BOOL didScroll = [self autoscroll:event];
+    
+    if (didScroll){
+        _private->mouseDownPoint.x = -FLT_MAX;
+        _private->mouseDownPoint.y = -FLT_MAX;
+    }
     
     // Now do WebKit dragging.
     float deltaX = ABS([event locationInWindow].x - _private->mouseDownPoint.x);
     float deltaY = ABS([event locationInWindow].y - _private->mouseDownPoint.y);
 
-    if (deltaX >= DragStartXHysteresis || deltaY >= DragStartYHysteresis){
+    if ((deltaX >= DragStartXHysteresis || deltaY >= DragStartYHysteresis) && !didScroll){
         NSPoint point = [self convertPoint:_private->mouseDownPoint fromView:nil];
         NSDictionary *element = [self _elementAtPoint: point];
         NSURL *linkURL = [element objectForKey: WebContextMenuElementLinkURLKey];
