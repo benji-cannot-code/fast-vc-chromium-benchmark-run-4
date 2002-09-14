e24a962c479649e3db961a9a211397eaa7ecc5ef
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-14 01:07:25 +0000

    Fixed navigation to link on mouse up after drag.
            * khtml/khtmlview.cpp:
            (KHTMLView::viewportMousePressEvent):
            (KHTMLView::viewportMouseDoubleClickEvent):
            Change khtml's definition of double click to be based on
            mouse up of second click, not mouse down.
    
            (KHTMLView::viewportMouseReleaseEvent):
    
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge mouseUp:]):
            (-[WebCoreBridge mouseDown:]):
            Changed double click to be on mouse up with click count of two.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2067 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreBridge.mm
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.mm
@@ -367,8 +367,14 @@ using khtml::RenderPart;
     state |= [self stateForEvent:event];
     
     if (part->impl->view()) {
-        QMouseEvent kEvent(QEvent::MouseButtonRelease, QPoint((int)p.x, (int)p.y), button, state, [event clickCount]);
-        part->impl->view()->viewportMouseReleaseEvent(&kEvent);
+        if ([event clickCount] % 2 == 0){
+            QMouseEvent kEvent(QEvent::MouseButtonDblClick, QPoint((int)p.x, (int)p.y), button, state, [event clickCount]);
+            part->impl->view()->viewportMouseDoubleClickEvent(&kEvent);
+        }
+        else {
+            QMouseEvent kEvent(QEvent::MouseButtonRelease, QPoint((int)p.x, (int)p.y), button, state, [event clickCount]);
+            part->impl->view()->viewportMouseReleaseEvent(&kEvent);
+        }
     }
 }
 
@@ -394,14 +400,8 @@ using khtml::RenderPart;
     state |= [self stateForEvent:event];
     
     if (part->impl->view()) {
-        if ([event clickCount] % 2 == 0){
-            QMouseEvent kEvent(QEvent::MouseButtonDblClick, QPoint((int)p.x, (int)p.y), button, state, [event clickCount]);
-            part->impl->view()->viewportMouseDoubleClickEvent(&kEvent);
-        }
-        else {
-            QMouseEvent kEvent(QEvent::MouseButtonPress, QPoint((int)p.x, (int)p.y), button, state, [event clickCount]);
-            part->impl->view()->viewportMousePressEvent(&kEvent);
-        }
+        QMouseEvent kEvent(QEvent::MouseButtonPress, QPoint((int)p.x, (int)p.y), button, state, [event clickCount]);
+        part->impl->view()->viewportMousePressEvent(&kEvent);
     }
 }
 
