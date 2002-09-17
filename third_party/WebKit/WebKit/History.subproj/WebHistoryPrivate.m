eb3200ee29929cf974265d7ce3b7d2d21b0cf4bf
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-17 01:24:54 +0000

    * History.subproj/WebHistoryPrivate.m: Do that for real.
    git-svn-id: svn://svn.chromium.org/blink/trunk@2074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebHistoryPrivate.m
+++ third_party/WebKit/WebKit/History.subproj/WebHistoryPrivate.m
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation WebHistoryPrivate
 
-#define FIX_VISITED
+//#define FIX_VISITED
 
 #pragma mark OBJECT FRAMEWORK
 
