6d4269a702cfdabb3ee56193bf7f68b5307dc1e0
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-20 06:25:57 +0000

    WebFoundation:
            * Makefile.am: Make `doc' target depend on `all'.
    
    WebKit:
    
            * Makefile.am: Make `doc' target depend on `all'.
            * WebView.subproj/WebContextMenuHandler.h: Fix typo found by `make doc'.
            * WebView.subproj/WebLocationChangeHandler.h: Likewise.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2103 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebLocationChangeHandler.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebLocationChangeHandler.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class WebFrame;
 
 /*!
-    @protcol WebLocationChangeHandler
+    @protocol WebLocationChangeHandler
     @abstract A controller's WebLocationChangeHandler track changes it's frame's location. 
 */
 @protocol WebLocationChangeHandler <NSObject>
