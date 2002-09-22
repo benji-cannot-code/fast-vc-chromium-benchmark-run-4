be2afab814f744fac208c02f9889e54d6adbd0b8
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-22 05:52:15 +0000

    Fix crashing part of 3057265 -- REGRESSION: Crash loading http://www.undetectables.co.uk/
            * khtml/khtml_part.cpp: (KHTMLPart::processObjectRequest):
    	Remove special case for javascript and about:blank that was leading to a crash.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2116 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtml_part.cpp
+++ third_party/WebKit/WebCore/khtml/khtml_part.cpp
@@ -2867,6 +2867,9 @@ bool KHTMLPart::processObjectRequest( khtml::ChildFrame *child, const KURL &_url
   if ( child->m_extension )
     child->m_extension->setURLArgs( child->m_args );
 
+#if APPLE_CHANGES
+  if ( !url.isEmpty() )
+#else
   if(url.protocol() == "javascript" || url.url() == "about:blank") {
       if (!child->m_part->inherits("KHTMLPart"))
           return false;
@@ -2885,6 +2888,7 @@ bool KHTMLPart::processObjectRequest( khtml::ChildFrame *child, const KURL &_url
       return true;
   }
   else if ( !url.isEmpty() )
+#endif
   {
       //kdDebug( 6050 ) << "opening " << url.url() << " in frame " << child->m_part << endl;
       return child->m_part->openURL( url );
