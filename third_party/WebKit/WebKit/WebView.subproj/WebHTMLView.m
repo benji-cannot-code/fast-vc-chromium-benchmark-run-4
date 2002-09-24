d731baee095bfe818b02e940584f4b093bbb6a69
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-24 22:20:28 +0000

    - fixed 3059266 -- REGRESSION: clicking in web view when location field is 	focused does not focus web view
            * WebView.subproj/WebHTMLView.m: (-[WebHTMLView needsPanelToBecomeKey]):
    	Must override this to return YES, because NSClipView makes it return NO
    	even though we accept the first responder.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2148 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -115,8 +115,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [super dealloc];
 }
 
-
 - (BOOL)acceptsFirstResponder
+{
+    return YES;
+}
+
+- (BOOL)needsPanelToBecomeKey
 {
     return YES;
 }
