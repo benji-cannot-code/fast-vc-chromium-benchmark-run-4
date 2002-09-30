2cedcce7a2cee36ad2c52719223c55a5cd03a9fd
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-09-30 18:09:08 +0000

    * WebView.subproj/WebDataSource.m: (-[WebDataSource initWithRequest:]): 	Remove the ill-advised "don't even create a WebDataSource if WebResourceHandle 	can't handle this request" code.
            * WebView.subproj/WebDataSourcePrivate.m: (-[WebDataSource _startLoading]):
    	Rearrange checks so we won't get confused if we can't create a handle.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2208 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
@@ -42,10 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 -(id)initWithRequest:(WebResourceRequest *)request
 {
-    if (![WebResourceHandle canInitWithRequest:request]) {
-        return nil;
-    }
-    
     self = [super init];
     if (!self) {
         return nil;
--- third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
@@ -183,8 +183,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     
     [[_private->controller locationChangeDelegate] locationChangeStartedForDataSource:self];
 
-    // Fire this guy up.
-    if (!_private->mainHandle) {
+    if (!_private->mainClient) {
         _private->mainClient = [[WebMainResourceClient alloc] initWithDataSource:self];
 
 	if ([self webFrame] == [[self controller] mainFrame]) {
@@ -194,10 +193,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 	}
 
         _private->mainHandle = [[WebResourceHandle alloc] initWithRequest:_private->request delegate:_private->mainClient];
-        
-        ASSERT(_private->mainHandle);
     }
-    [_private->mainClient didStartLoadingWithURL:[_private->mainHandle URL]];
+    
+    if (_private->mainHandle) {
+        [_private->mainClient didStartLoadingWithURL:[_private->mainHandle URL]];
+    }
 }
 
 - (void)_addSubresourceClient:(WebSubresourceClient *)client
