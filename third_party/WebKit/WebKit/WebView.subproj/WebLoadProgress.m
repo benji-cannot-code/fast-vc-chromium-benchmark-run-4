6366b708bc2db1ba2d53d2027aa921a51a246c3b
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-01 17:12:16 +0000

    WebFoundation:
    	This change focuses on "status codes". Status codes are an enum that describe
    	the state of a handle's load (e.g. loading, finished, cancelled).
    
    	Removed status code from WebResourceResponse, and moved it to WebResourceHandle.
    	The status code is now available as private API on WebResourceHandle. This
    	change made it possible to make some nice cleanups and simplifications.
    
    	The great majority of these changes merely move the method calls from the old
    	design of calling WebResourceResponse to access the status code to calling
    	the WebResourceHandle to get the status code.
    
            * CacheLoader.subproj/IFURLHandleC.h:
            * CacheLoader.subproj/WebCacheLoaderConstants.h:
            * CacheLoader.subproj/WebResourceHandle.m:
            (+[WebResourceHandle sendSynchronousRequest:])
            (-[WebResourceHandle initWithRequest:])
            (-[WebResourceHandle loadWithDelegate:])
            (-[WebResourceHandle cancel])
            * CacheLoader.subproj/WebResourceHandleInternal.h:
            * CacheLoader.subproj/WebResourceHandlePrivate.h:
            * CacheLoader.subproj/WebResourceHandlePrivate.m:
            (-[WebResourceHandle _didReceiveResponseMetadata:])
            (-[WebResourceHandle _didLoadData:])
            (-[WebResourceHandle _backgroundLoadComplete])
            (-[WebResourceHandle _backgroundLoadFailed])
            (-[WebResourceHandle _didRedirectToURL:])
            (-[WebResourceHandle _load])
            (-[WebResourceHandle _statusCode])
            (-[WebResourceHandle _setStatusCode:])
            * CacheLoader.subproj/WebResourceLoad.h:
            * CacheLoader.subproj/WebResourceLoad.m:
            (-[WebResourceLoad cancel])
            (-[WebResourceLoad isCancelled])
            (-[WebResourceLoad succeeded])
            (-[WebResourceLoad failedWithResultCode:inDomain:failingURL:])
            (-[WebResourceLoad addHandle:])
            (-[WebResourceLoad _beginLoad])
            (_loadThrottler)
            * CacheLoader.subproj/WebResourceResponse.h:
            * CacheLoader.subproj/WebResourceResponse.m:
            (-[WebResourceResponse _setResourceData:])
            * CacheLoader.subproj/WebResourceResponsePrivate.h:
    
    WebKit:
    
            * WebView.subproj/WebLoadProgress.m:
            (-[WebLoadProgress initWithResourceHandle:]): Status code moved from WebResourceResponse
    	to WebResourceHandle private API.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2216 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebLoadProgress.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebLoadProgress.m
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         return [self init];
     }
     int b = [theResponse contentLengthReceived];
-    int t = [theResponse statusCode] == WebResourceHandleStatusLoadComplete ? b : [theResponse contentLength];
+    int t = [handle _statusCode] == WebResourceHandleStatusLoadComplete ? b : [theResponse contentLength];
     return [self initWithBytesSoFar:b totalToLoad:t];
 }
 
