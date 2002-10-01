a6fa8810b27234ff7e7e6d056bfd923cbb5ab61e
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-01 22:25:42 +0000

    WebFoundation:
    	Implemented WebResourceRequest "category" design for extending a
    	request with protocol-specific data.
    
            * CacheLoader.subproj/IFHTTPURLHandleC.m: Now imports WebHTTPResourceRequest.h.
            * CacheLoader.subproj/WebProtocolHandler.h: Added partOfRequest:withClass:createIfDoesNotExist: method.
            * CacheLoader.subproj/WebProtocolHandler.m:
            (+[WebProtocolHandler partOfRequest:withClass:createIfDoesNotExist:]):
            * CacheLoader.subproj/WebResourceLoad.m: Now imports WebHTTPResourceRequest.h.
            * CacheLoader.subproj/WebResourceLoadManager.m: Ditto.
            * CacheLoader.subproj/WebResourceRequest.h: Removed HTTP-protocol-specific fields and methods.
            * CacheLoader.subproj/WebResourceRequest.m: Ditto.
            (-[WebResourceRequest initWithURL:]): Deal with fact that HTTP-protocol-specific fields and methods have been removed.
            (-[WebResourceRequest copyWithZone:]): Ditto. Copy protocol-specific parts.
            (-[WebResourceRequest copyWithURL:]): Ditto. Ditto. :)
            (-[WebResourceRequest _partWithClass:createIfDoesNotExist:]): Added implementation.
            (-[WebResourceRequest dealloc]): Deal with fact that HTTP-protocol-specific fields and methods have been removed.
            * CacheLoader.subproj/WebResourceRequestPrivate.h: Added.
            * Misc.subproj/WebFoundation.h: Now imports WebHTTPResourceRequest.h.
            * ProtocolHandlers.subproj/WebCFNetworkHTTPProtocolHandler.m: Now imports WebHTTPResourceRequest.h.
            * ProtocolHandlers.subproj/WebHTTPProtocolHandler.m: Ditto.
            * ProtocolHandlers.subproj/WebHTTPResourceRequest.h: Added.
            * ProtocolHandlers.subproj/WebHTTPResourceRequest.m: Added.
            * ProtocolHandlers.subproj/WebHTTPResourceRequestParameters.h: Added.
            * ProtocolHandlers.subproj/WebHTTPResourceRequestParameters.m: Added.
            * ProtocolHandlers.subproj/WebSimpleHTTPProtocolHandler.m: Now imports WebHTTPResourceRequest.h.
            * WebFoundation.pbproj/project.pbxproj
    
    WebKit:
    
            Implemented WebResourceRequest "category" design for extending a
            request with protocol-specific data.
    
            * WebCoreSupport.subproj/WebBridge.m: Now imports WebHTTPResourceRequest.h.
            * WebCoreSupport.subproj/WebSubresourceClient.m: Ditto.
            * WebView.subproj/WebMainResourceClient.m: Ditto.
    
    WebBrowser:
    
            * BrowserDocument.m: Now imports WebHTTPResourceRequest.h to get at protocol-specific
    	request information.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2219 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebFoundation/WebNSStringExtras.h>
 #import <WebFoundation/WebResourceHandle.h>
 #import <WebFoundation/WebResourceRequest.h>
+#import <WebFoundation/WebHTTPResourceRequest.h>
 
 @interface NSApplication (DeclarationStolenFromAppKit)
 - (void)_cycleWindowsReversed:(BOOL)reversed;
