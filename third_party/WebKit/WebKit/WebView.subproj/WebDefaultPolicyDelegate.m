544d2e96bf4ecc8d12abdd007bd46fae8be9fc18
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-02 17:03:05 +0000

    WebKit:
            * WebView.subproj/WebControllerPolicyDelegate.h: Change modifierMask to
    	modifierFlags to match NSEvent.
            * WebView.subproj/WebDefaultPolicyDelegate.m:
            (-[WebDefaultPolicyDelegate clickPolicyForElement:button:modifierFlags:]): Ditto.
            * WebView.subproj/WebHTMLViewPrivate.m:
            (-[WebHTMLView _continueAfterClickPolicyForEvent:]): Ditto.
    
    WebBrowser:
    
    	Start of the "open window behind" feature.
    
            * BrowserDocument.m:
            (-[BrowserDocument close]): Move a cancel here from the BrowserWebController
    	resetDocument method that really didn't belong there.
            (-[BrowserDocument _goToURL:referrer:withFallbackURLs:]): Rearrange this
    	function so that the part that is just "go to a particular WebResourceRequest"
    	is clearly separated, in preparation for using a method in WebController some day.
    
            * BrowserDocumentController.h: Add URLWindowPolicy enum, and use it as the
    	parameter for goToURL and tryMultipleURLs.
            * BrowserDocumentController.m:
            (-[BrowserDocumentController openDocumentWithContentsOfURL:referrer:behind:):
    	Remove display: parameter, and add behind: parameter.
            (-[BrowserDocumentController _goToEachURL:referrer:windowPolicy:]): Update to take
    	a windowPolicy parameter instead of separate booleans.
            (-[BrowserDocumentController goToEachURL:]): Use windowPolicy.
            (-[BrowserDocumentController openDocument:]): Use windowPolicy.
            (-[BrowserDocumentController goToURL:]): Use windowPolicy.
            (-[BrowserDocumentController goToURL:referrer:windowPolicy:]): Use windowPolicy.
            (-[BrowserDocumentController tryMultipleURLs:windowPolicy:]): Use windowPolicy.
    
            * BrowserWebController.m:
            (-[BrowserWebController resetDocument]): Move cancel to BrowserDocument close.
            (-[BrowserWebController openNewWindowWithURL:referrer:]): Use windowPolicy.
            (-[BrowserWebController pluginNotFoundSheetEnded:returnCode:contextInfo:]):
    	Use windowPolicy.
            (-[BrowserWebController clickPolicyForElement:button:modifierFlags:]):
    	Update name to match change in WebKit.
    
            * ActivityViewer.m: (-[ActivityViewer _goToClickedItem:]):
            * AppController.m:
            (-[AppController application:openFile:]):
            (-[AppController handleGURLEvent:withReplyEvent:]):
            * BrowserWebBookmarkExtras.m: (-[WebBookmark _goToForceNewWindow:]):
            * Test/PageLoadTestRunner.m: (-[PageLoadTestRunner goToPage]):
    	Update to use windowPolicy.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2225 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDefaultPolicyDelegate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDefaultPolicyDelegate.m
@@ -66,7 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSLog (@"pluginNotFoundForMIMEType:pluginPageURL: - MIME %@, URL ", mime, URL);
 }
 
-- (WebClickPolicy *)clickPolicyForElement: (NSDictionary *)elementInformation button: (NSEventType)eventType modifierMask: (unsigned int)eventMask
+- (WebClickPolicy *)clickPolicyForElement: (NSDictionary *)elementInformation button: (NSEventType)eventType modifierFlags: (unsigned int)modifierFlags
 {
     return [WebClickPolicy webPolicyWithClickAction:WebClickPolicyShow URL:nil andPath:nil];
 }
