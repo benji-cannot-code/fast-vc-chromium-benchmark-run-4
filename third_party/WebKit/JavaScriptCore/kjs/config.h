fd169aafe062a667161772120b553517fcc1600a
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-04 21:07:38 +0000

    * kjs/config.h: Define HAVE_FUNC_STRTOLL.         * kjs/function.cpp: (GlobalFuncImp::call): 	Change parseInt to use strtoll if available.
    git-svn-id: svn://svn.chromium.org/blink/trunk@2249 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/config.h
+++ third_party/WebKit/JavaScriptCore/kjs/config.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_FUNC_ISINF 1
 #define HAVE_FUNC_ISNAN 1
+#define HAVE_FUNC_STRTOLL 1
 #define HAVE_STRING_H 1
 #define HAVE_STRINGS_H 1
 #define HAVE_SYS_TIME_H 1
