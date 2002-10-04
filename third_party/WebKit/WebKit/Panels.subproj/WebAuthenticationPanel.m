dc2759f728c5fd5de334a50f163b3a1b9d524481
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-04 20:43:01 +0000

    Tools:
            * Scripts/extract-localizable-strings: Add dprintf, NSLog, and
    	NSException to the list of tokens that introduce non-localizable
    	strings, and added handling for square brackets.
    
    WebFoundation:
    
            * CacheLoader.subproj/WebResourceLoad.m: (_loadSweeper):
            * Database.subproj/WebFileDatabase.m: (-[WebFileReader initWithPath:]):
            * ProtocolHandlers.subproj/WebSimpleHTTPProtocolHandler.m:
            (-[WebSimpleHTTPProtocolHandler createHTTPRequest]):
    	Changed logging code to take advantage of the fact that we once-again support %@.
    
            * ProtocolHandlers.subproj/WebCFNetworkHTTPProtocolHandler.m:
            (-[WebCFNetworkHTTPProtocolHandler addRequestMessageBody:]):
            * ProtocolHandlers.subproj/WebSimpleHTTPProtocolHandler.m:
            (-[WebSimpleHTTPProtocolHandler _addRequestMessageBody:]):
    	Fixed logging code to eliminate a leak.
    
            * ProtocolHandlers.subproj/WebHTTPProtocolHandler.m:
            (-[WebHTTPProtocolHandler debugStringForCacheAttributeValue:]):
    	Changed this code to just call description instead of using string formatting.
    
            * English.lproj/StringsNotToBeLocalized.txt: Add more exceptions. We now have a
    	completely-clean run of extract-localizable-strings in WebFoundation!
    
            * English.lproj/Localizable.strings: Re-generated.
    
    WebKit:
    
            * Panels.subproj/WebAuthenticationPanel.m:
            (-[WebAuthenticationPanel loadNib]): Use ERROR instead of NSLog here.
            (-[WebAuthenticationPanel setUpForRequest:]): Mark some strings for localization.
    
            * WebKit.pbproj/project.pbxproj: Change the encoding on a lot more files to UTF-8.
    
            * English.lproj/StringsNotToBeLocalized.txt: Added some new exceptions.
            * English.lproj/Localizable.strings: Re-generated.
    
    WebBrowser:
    
    	More progress on localization.
    
            * English.lproj/StringsNotToBeLocalized.txt: Added a few strings.
    
            * GlobalHistory.m: (-[GlobalHistory outlineView:objectValueForTableColumn:byItem:]):
    	Marked a string for localization.
    
            * FrameProgressEntry.m: (-[FrameProgressEntry updateKeyFrom:to:dataSource:]):
    	Added an assert in case we run into an issue like the one from earlier today.
    
            * English.lproj/Localizable.strings: Re-generated.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2247 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Panels.subproj/WebAuthenticationPanel.m
+++ third_party/WebKit/WebKit/Panels.subproj/WebAuthenticationPanel.m
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 #import <WebKit/WebAuthenticationPanel.h>
-#import <WebKit/WebStandardPanelsPrivate.h>
-#import <WebFoundation/WebAssertions.h>
 
+#import <WebFoundation/WebAssertions.h>
+#import <WebFoundation/WebLocalizableStrings.h>
+
+#import <WebKit/WebStandardPanelsPrivate.h>
 
 #define WebAuthenticationPanelNibName @"WebAuthenticationPanel"
 
@@ -76,8 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             nibLoaded = YES;
             [imageView setImage:[NSImage imageNamed:@"NSApplicationIcon"]];
         } else {
-            NSLog(@"%s:%d  %s: couldn't load nib named '%@'",
-                  __FILE__, __LINE__, __FUNCTION__,WebAuthenticationPanelNibName);
+            ERROR("couldn't load nib named '%@'", WebAuthenticationPanelNibName);
             return FALSE;
         }
     }
@@ -85,7 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 // Methods related to displaying the panel
-
 
 -(void)setUpForRequest:(WebAuthenticationRequest *)req
 {
@@ -97,11 +97,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // failure count (if the user tried and failed, the dialog should
     // explain possible reasons)
     // FIXME Radar 2876446: need to automatically adjust height of main label
-    [mainLabel setStringValue:[NSString stringWithFormat:@"To view this page, you need to log in to area \"%@\" on %@.", [resource realm], [[resource URL] host]]];
+    [mainLabel setStringValue:[NSString stringWithFormat:
+        UI_STRING("To view this page, you need to log in to area “%@” on %@.",
+            "prompt string in authentication panel"),
+        [resource realm], [[resource URL] host]]];
     if ([resource receivesCredentialSecurely]) {
-        [smallLabel setStringValue:@"Your log-in information will be sent securely."];
+        [smallLabel setStringValue:
+            UI_STRING("Your log-in information will be sent securely.",
+                "message in authentication panel")];
     } else {
-        [smallLabel setStringValue:@"Your password will be sent in the clear."];
+        [smallLabel setStringValue:
+            UI_STRING("Your password will be sent in the clear.",
+                "message in authentication panel")];
     }
 
     if ([resource username] != nil) {
