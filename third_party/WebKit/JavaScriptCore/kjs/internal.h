5e0b88468845806ba28d5cecaffca6b544712e1f
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-05 03:31:11 +0000

    - fixed 2992187 -- Should use curly quotes in status bar messages, 	window titles, and error dialogs
            * BrowserWindowController.m:
            (-[BrowserWindowController performQuickSearch:]):
            (-[BrowserWindowController goToToolbarLocation:]):
    	Change the last two straight apostrophes to curly ones.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/internal.h
+++ third_party/WebKit/JavaScriptCore/kjs/internal.h
@@ -160,7 +160,8 @@ namespace KJS {
     friend class ListIterator;
     friend class List;
     friend class InterpreterImp;
-  public:
+    friend class ObjectImp;
+  private:
     ListImp();
     ~ListImp();
 
