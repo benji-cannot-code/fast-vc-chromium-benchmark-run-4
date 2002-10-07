2d8a70fe70e882d0812f0773f029a89b1dc75a31
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-07 21:06:32 +0000

    Add a port to the background thread's run loop so it will not exit 	the run method constantly.
            * Database.subproj/WebFileDatabase.m:
            (+[WebFileDatabase _syncLoop:])
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
@@ -472,6 +472,7 @@ static void URLFileReaderInit(void)
     WebSetThreadPriority(WebMinThreadPriority);
 
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    NSPort *placeholderPort;
 
     BEGIN_EXCEPTION_HANDLER
 
@@ -480,7 +481,10 @@ static void URLFileReaderInit(void)
     while (YES) {
         BEGIN_EXCEPTION_HANDLER
         // we specifically use an NSRunLoop here to get autorelease pool support
-        [[NSRunLoop currentRunLoop] run];
+        placeholderPort = [NSPort port];
+        [syncRunLoop addPort:placeholderPort forMode:NSDefaultRunLoopMode];
+        [syncRunLoop run];
+        [syncRunLoop removePort:placeholderPort forMode:NSDefaultRunLoopMode];
         END_EXCEPTION_HANDLER
     }
 
