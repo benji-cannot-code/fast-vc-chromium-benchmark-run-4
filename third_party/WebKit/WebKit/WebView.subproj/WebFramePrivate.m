7e38ad21e373cb29ec173d471fa941ff8958e97a
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-08 21:12:31 +0000

    WebFoundation:
    	Some changes to make some progress on this bug:
    
    	Radar 3015613 (Search results disappear when backtracking)
    
    	This set of changes does not fix this bug, but it does provide much of the WebFoundation
    	support we'll need for a complete fix. In order to finish the fix, we need to make
    	some API changes in WebKit.
    
            * CacheLoader.subproj/WebResourceLoad.m:
            (-[WebResourceLoad _cacheKey]): Add HTTP request method to cache key.
            * CacheLoader.subproj/WebResourceRequest.m:
            (-[WebResourceRequest initWithURL:]): Explicitly set defaults for requestCachePolicy and responseCachePolicy.
            * ProtocolHandlers.subproj/WebHTTPProtocolHandler.m:
            (-[WebHTTPProtocolHandler shouldCacheResponse:]): Do not unconditionally determine that HEAD, POST, PUT, and
    	DELETE methods are uncacheable. This change is made so we can show cached results when a user uses back/forward
    	or history to do navigation.
    
    WebKit:
    
    	Use WebFrameLoadType variable to set the request cache policy for the WebResourceRequest.
    	This will help us to provide cached results for POST requests when the user navigates
    	using the back/forward list or history for navigation, while making sure we do not
    	use cached results when the user pokes the submit button for a form they have
    	submitted previously.
    
            * WebView.subproj/WebFramePrivate.m:
            (-[WebFrame _goToItem:withFrameLoadType:])
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2276 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFramePrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFramePrivate.m
@@ -28,8 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebFoundation/WebError.h>
 #import <WebFoundation/WebNSURLExtras.h>
+#import <WebFoundation/WebNSStringExtras.h>
 #import <WebFoundation/WebResourceHandle.h>
 #import <WebFoundation/WebResourceRequest.h>
+#import <WebFoundation/WebHTTPResourceRequest.h>
 
 static const char * const stateNames[] = {
     "WebFrameStateProvisional",
@@ -686,9 +688,36 @@ static const char * const stateNames[] = {
     }
     else {
         request = [[WebResourceRequest alloc] initWithURL:itemURL];
-        if (type == WebFrameLoadTypeBack || type == WebFrameLoadTypeForward) {
-            [request setRequestCachePolicy:WebRequestCachePolicyReturnCacheObjectLoadFromOriginIfNoCacheObject];
+    
+        // set the request cache policy based on the type of request we have
+        // however, allow any previously set value to take precendence
+        if ([request requestCachePolicy] == WebRequestCachePolicyUseProtocolDefault) {
+            switch (type) {
+                case WebFrameLoadTypeStandard:
+                    // if it's not a GET, reload from origin
+                    // unsure whether this is the best policy
+                    // other methods might be OK to get from the cache
+                    if (![[request method] _web_isCaseInsensitiveEqualToString:@"GET"]) {
+                        [request setRequestCachePolicy:WebRequestCachePolicyLoadFromOrigin];
+                    }
+                    break;
+                case WebFrameLoadTypeReload:
+                    [request setRequestCachePolicy:WebRequestCachePolicyLoadFromOrigin];
+                    break;
+                case WebFrameLoadTypeBack:
+                case WebFrameLoadTypeForward:
+                case WebFrameLoadTypeIndexedBack:
+                case WebFrameLoadTypeIndexedForward:
+                case WebFrameLoadTypeIntermediateBack:
+                    [request setRequestCachePolicy:WebRequestCachePolicyReturnCacheObjectLoadFromOriginIfNoCacheObject];
+                    break;
+                case WebFrameLoadTypeInternal:
+                case WebFrameLoadTypeReloadAllowingStaleData:
+                    // no-op: leave as protocol default
+                    break;
+            }
         }
+        
         dataSource = [[WebDataSource alloc] initWithRequest:request];
         [request release];
         [self setProvisionalDataSource: dataSource];
