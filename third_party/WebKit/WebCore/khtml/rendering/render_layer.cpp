ee4cf3281f64f9aef189fbe1dc5bfb763d4fd0ce
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-09 23:35:03 +0000

    Fix a subtle bug with layers.  When a parent is sorted with its 	children the parent's zindex must be treated as 0, so that 	negative children draw behind the layer's content and positive 	children (or auto children) draw in front of the layer's content.
    	This fix makes chrysler.com work again (and doubtless many other
    	messed up DHTML sites).
    
            * khtml/rendering/render_layer.cpp:
            (RenderLayer::constructZTree):
            (RenderLayer::RenderZTreeNode::constructLayerList):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_layer.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_layer.cpp
@@ -316,6 +316,12 @@ RenderLayer::constructZTree(QRect damageRect,
             RenderZTreeNode* leaf = new RenderZTreeNode(layerElt);
             leaf->next = returnNode->child;
             returnNode->child = leaf;
+            
+            // We are an interior node and have other child layers.  Our layer
+            // will need to be sorted with the other layers as though it has
+            // a z-index of 0.
+            if (!layerElt->zauto)
+                layerElt->zindex = 0;
         }
         else
             returnNode->layerElement = layerElt;
@@ -444,7 +450,6 @@ void RenderLayer::RenderZTreeNode::constructLayerList(QPtrVector<RenderLayerElem
             buffer->resize(2*(buffer->size()+1));
         
         buffer->insert(buffer->count(), layerElement);
-        layerElement->zindex = explicitZIndex;
         return;
     }
 
