3a6be943814f09d927484276c2ddf8194b608829
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-10 05:33:04 +0000

    - fixed 3072015 -- REGRESSION: frames multiply endlessly
            * WebCoreSupport.subproj/WebBridge.m: (-[WebBridge setFrame:]):
    	Remove code that sets the parent from here, because it's too early, always nil.
            * WebView.subproj/WebFramePrivate.m: (-[WebFrame _addChild:]):
    	Do it here instead.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2296 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -212,7 +212,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if (frame == nil) {
 	// FIXME: non-retained because data source owns representation owns bridge
 	frame = webFrame;
-        [self setParent:[[frame parent] _bridge]];
         [self setTextSizeMultiplier:[[frame controller] textSizeMultiplier]];
     } else {
 	ASSERT(frame == webFrame);
