c184ceb7a1a526e7f21ecccdd6db14ab498ff8f3
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-15 20:01:54 +0000

    - fixed 3075777 -- hysteresis for dragging links on web pages 	is still too large
            * WebView.subproj/WebHTMLView.m: changed hysteresis constants;
    	added comment.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2331 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -31,8 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Needed for the mouse moved notification.
 #import <AppKit/NSResponder_Private.h>
 
-#define DragStartXHysteresis  		5.0
-#define DragStartYHysteresis  		5.0
+// These are a little larger than typical because dragging links is a fairly
+// advanced feature that can confuse non-power-users
+#define DragStartXHysteresis  		10.0
+#define DragStartYHysteresis  		10.0
 
 #define DRAG_LABEL_BORDER_X		4.0
 #define DRAG_LABEL_BORDER_Y		2.0
