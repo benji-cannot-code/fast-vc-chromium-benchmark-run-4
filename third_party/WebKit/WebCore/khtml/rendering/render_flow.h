e080cb30451af9629a135e640c35192ca5abf1b7
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-16 21:55:08 +0000

    A very substantial change.  Absolute/fixed positioned elements that 	are NOT renderflows were not getting layers constructed for them. 	They do now.
    	Also, floats now have layers constructed for them.  This allows
    	printSpecialObjects to be completely eliminated, enables smarter
    	damge rect intersection testing for faster painting, and fixes
    	regressions on sites like www.demian5.com.
    
            * khtml/rendering/render_box.cpp:
            (RenderBox::setStyle):
            * khtml/rendering/render_flow.cpp:
            (RenderFlow::printObject):
            (RenderFlow::addOverHangingFloats):
            (RenderFlow::printTree):
            * khtml/rendering/render_flow.h:
            * khtml/rendering/render_layer.cpp:
            (RenderLayer::constructZTree):
            * khtml/rendering/render_root.cpp:
            (RenderRoot::printObject):
            * khtml/rendering/render_table.cpp:
            (RenderTable::print):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_flow.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_flow.h
@@ -65,9 +65,7 @@ public:
                         int tx, int ty);
     virtual void printObject( QPainter *, int x, int y, int w, int h,
                         int tx, int ty);
-    void printSpecialObjects( QPainter *, int x, int y, int w, int h,
-                        int tx, int ty);
-
+    
     virtual void layout( );
 
     virtual void close();
@@ -165,7 +163,6 @@ protected:
 	    left = 0;
 	    width = 0;
             count = 0;
-            noPaint = false;
 
         }
         RenderObject* node;
@@ -175,8 +172,7 @@ protected:
         short width;
         short count;
         Type type : 2; // left or right aligned
-        bool noPaint : 1;
-
+        
         bool operator==(const SpecialObject& ) const
         {
             return false;
