5a7159474ff49416791bea0f6cc7657546b989f6
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-16 23:28:31 +0000

    When render objects were being dynamically wrapped in anonymous 	blocks, the layer parenting connection was being broken without 	being reconnected when the wrapping finished.  This patch 	shifts the layer connection into the functions that are also 	called in the dynamic case.
    	With this fix, all objects on mrskin finally show up.  Yeehah.
    	brainjar.com's brain is back too.
    
    	This patch also stops treating images as inline if they are
    	using abs/fixed positioning, since according to the CSS spec,
    	abs/fixed positioned elements should be treated as block-level
    	content.
    
            * khtml/rendering/render_container.cpp:
            (RenderContainer::addChild):
            (RenderContainer::appendChildNode):
            (RenderContainer::insertChildNode):
            * khtml/rendering/render_image.cpp:
            (RenderImage::setStyle):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_container.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_container.cpp
@@ -135,10 +135,6 @@ void RenderContainer::addChild(RenderObject *newChild, RenderObject *beforeChild
     }
     newChild->setLayouted( false );
     newChild->setMinMaxKnown( false );
-
-    // Keep our layer hierarchy updated.
-    if (newChild->layer())
-        enclosingLayer()->addChild(newChild->layer());
 }
 
 RenderObject* RenderContainer::removeChildNode(RenderObject* oldChild)
@@ -253,6 +249,10 @@ void RenderContainer::appendChildNode(RenderObject* newChild)
     setLastChild(newChild);
     newChild->setLayouted( false );
     newChild->setMinMaxKnown( false );
+    
+    // Keep our layer hierarchy updated.
+    if (newChild->layer())
+        enclosingLayer()->addChild(newChild->layer());
 }
 
 void RenderContainer::insertChildNode(RenderObject* child, RenderObject* beforeChild)
@@ -279,6 +279,12 @@ void RenderContainer::insertChildNode(RenderObject* child, RenderObject* beforeC
     child->setParent(this);
     child->setLayouted( false );
     child->setMinMaxKnown( false );
+    
+    // Keep our layer hierarchy updated.
+    // XXX Need this to do an insertion and really find the right place to
+    // put the new layer. -dwh
+    if (child->layer())
+        enclosingLayer()->addChild(child->layer());
 }
 
 
