9632d58b84af897192af9536550f5aa94801f314
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-18 20:26:49 +0000

    Removed unneeded import of WebFoundation/WebResourceLoadManager.h
            * Misc.subproj/WebIconLoader.m
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2370 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebIconLoader.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebIconLoader.m
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebIconDatabasePrivate.h>
 
 #import <WebFoundation/WebNSURLExtras.h>
-#import <WebFoundation/WebResourceLoadManager.h>
 #import <WebFoundation/WebResourceHandle.h>
 #import <WebFoundation/WebResourceRequest.h>
 
