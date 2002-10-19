f7300c252b881bc917a8884e0ddb9e331f79cb71
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-19 08:11:20 +0000

    A couple of whitespace fixes.  What's really odd about these 	fixes is that they *work* but only if you load the page by 	hitting "Back".  I suspect that I've now fixed the bugs on 	KHTML's end, and we have some sort of whitepsace dropping 	problem on our end.
            * khtml/html/htmlparser.cpp:
            (KHTMLParser::insertNode):
            * khtml/html/htmltokenizer.cpp:
            (HTMLTokenizer::write):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2375 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/htmltokenizer.cpp
+++ third_party/WebKit/WebCore/khtml/html/htmltokenizer.cpp
@@ -1442,7 +1442,8 @@ void HTMLTokenizer::write( const QString &str, bool appendData )
                 else if ( !parser->selectMode() &&
                           ( !parser->noSpaces() || dest > buffer )) {
                     addPending();
-                    discard = AllDiscard;
+                    discard = NoneDiscard; // Set to NoneDiscard. AllDiscard will cause loss of 
+                                           // spaces in this case <tag> <img> <tag> -dwh
                 }
                 // just forget it
                 else
