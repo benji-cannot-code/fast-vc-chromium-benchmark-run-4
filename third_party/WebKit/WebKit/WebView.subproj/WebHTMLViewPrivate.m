7d3c1d2c43c6cd61abd3c776a8391adae2c6620d
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-20 23:54:52 +0000

    - fixed a minor problem with autorelease I just ran into
            * WebView.subproj/WebHTMLViewPrivate.m: (+[WebHTMLView load]):
    	Create an autorelease pool for the benefit of the class initialize
    	functions that will be called.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewPrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewPrivate.m
@@ -63,12 +63,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation WebHTMLView (WebPrivate)
 
-// Danger Will Robinson.  We have to poseAsClass: as early as possible
+// Danger Will Robinson. We have to poseAsClass: as early as possible
 // so that any NSViews will be created with the appropriate poser.
 + (void)load
 {
-    [[WebNSTextView class] poseAsClass:[NSTextView class]];
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     [[WebNSView class] poseAsClass:[NSView class]];
+    [[WebNSTextView class] poseAsClass:[NSTextView class]];
+    [pool release];
 }
 
 
