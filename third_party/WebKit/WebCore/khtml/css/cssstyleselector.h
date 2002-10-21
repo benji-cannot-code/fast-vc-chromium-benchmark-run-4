fa6599b718e4e2ec7da9b87707ef5886aab7d121
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-21 22:41:51 +0000

    Implement support for a new @-rule called @konq-quirks.  This 	will allow us to put style rules into html4.css that only 	apply for pages in quirks mode.
            * khtml/css/css_ruleimpl.cpp:
            (CSSQuirksRuleImpl::CSSQuirksRuleImpl):
            (CSSQuirksRuleImpl::~CSSQuirksRuleImpl):
            (CSSQuirksRuleImpl::cssRules):
            (CSSQuirksRuleImpl::appendRule):
            (CSSQuirksRuleImpl::insertRule):
            (CSSQuirksRuleImpl::deleteRule):
            * khtml/css/css_ruleimpl.h:
            * khtml/css/cssparser.cpp:
            (StyleBaseImpl::parseAtRule):
            * khtml/css/cssparser.h:
            * khtml/css/cssstyleselector.cpp:
            * khtml/css/cssstyleselector.h:
            * khtml/dom/css_rule.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2401 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/css/cssstyleselector.h
+++ third_party/WebKit/WebCore/khtml/css/cssstyleselector.h
@@ -160,6 +160,7 @@ namespace khtml
 
 	static DOM::CSSStyleSheetImpl *defaultSheet;
 	static CSSStyleSelectorList *defaultStyle;
+    static CSSStyleSelectorList *defaultQuirksStyle;
 	static CSSStyleSelectorList *defaultPrintStyle;
 	CSSStyleSelectorList *authorStyle;
         CSSStyleSelectorList *userStyle;
@@ -297,7 +298,8 @@ namespace khtml
 	virtual ~CSSStyleSelectorList();
 
 	void append( DOM::CSSStyleSheetImpl *sheet,
-                 const DOM::DOMString &medium = "screen" );
+                 const DOM::DOMString &medium = "screen",
+                 int quirksMode = 0 );
 
 	void collect( QPtrList<DOM::CSSSelector> *selectorList, CSSOrderedPropertyList *propList,
 		      Source regular, Source important );
