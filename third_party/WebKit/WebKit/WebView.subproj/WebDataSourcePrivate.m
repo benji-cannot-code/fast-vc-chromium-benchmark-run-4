89e21dab767dff27d91dbf7fc1c72e3eb41d9096
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-24 09:02:27 +0000

    Flag error instead of ASSERTing as a result of fix to 3083013.         Not clear why this condition occurs, but better than ASSERTing         for Alex 29.
            * WebView.subproj/WebDataSourcePrivate.m:
            (-[WebDataSource _setRequest:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2447 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
@@ -272,7 +272,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // source is committed. It would be a WebFoundation bug if it sent
     // a redirect callback after commit.
     ASSERT(!_private->committed);
-    ASSERT(_private->request != request);
+    if (_private->request == request){
+        ERROR ("_private->request == request\n");
+        return;
+    }
     
     [request retain];
     [_private->request release];
