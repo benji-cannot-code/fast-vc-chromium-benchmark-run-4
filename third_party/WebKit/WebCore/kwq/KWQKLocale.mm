09c1a0549eb595cdae3072042a45431383e6f277
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-24 07:35:53 +0000

    * kwq/KWQKLocale.mm: (i18n): Make it obvious when non-localized 	strings from WebCore show up in the UI, but putting the string 	"not localized: " in front.
            * kwq/KWQKMessageBox.mm:
            (KMessageBox::error): Fix use of NSRunAlertPanel so that we won't
    	accidentally try to interpret % sequences in the messages we are displaying.
            (KMessageBox::warningYesNo): Ditto.
            (KMessageBox::questionYesNo): Ditto.
            (KMessageBox::sorry): Ditto.
            (KMessageBox::information): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKLocale.mm
+++ third_party/WebKit/WebCore/kwq/KWQKLocale.mm
@@ -28,16 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <KWQLogging.h>
 #import <qstring.h>
 
-@interface KWQKLocaleBundleDummy : NSObject { }
-@end
-@implementation KWQKLocaleBundleDummy
-@end
-
 QString i18n(const char *text)
 {
-    NSBundle *bundle = [NSBundle bundleForClass:[KWQKLocaleBundleDummy class]];
-    NSString *locString = [bundle localizedStringForKey:[NSString stringWithCString:text] value:nil table:nil];
-    return QString::fromNSString(locString);
+    return QString("not localized: ") + text;
 }
 
 QString KLocale::language() const
