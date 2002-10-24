5a2d241f7dc7e9c5465fc53797f22e9603ef5b84
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-24 16:28:30 +0000

    Don't leak request.
            * WebView.subproj/WebBaseResourceHandleDelegate.m:
            (-[WebBaseResourceHandleDelegate handle:willSendRequest:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebLoader.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebLoader.m
@@ -123,6 +123,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [newRequest setUserAgent:[[dataSource controller] userAgentForURL:[newRequest URL]]];
 
     // No need to retain here, will be copied after delegate callback.
+    [request autorelease];
     request = newRequest;
 
     if (identifier == nil){
