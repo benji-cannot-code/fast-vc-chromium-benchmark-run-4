7758fb9e3e4004c6027b2ff5c067ccb940bb1719
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-27 06:45:23 +0000

    - fixed a bug in the WebCore cache that caused it to not cache very much after         a while, and also caused a crash I saw twice on my machine
    	Adjust total size of items in the LRU list, when the size of one of the
    	items that's in the list is changed.
    
            * khtml/misc/loader.h: Added a size parameter (default 0) to the CachedObject
    	constructor. Added a protected CachedObject::setSize() and made m_size private.
    	Added Cache::adjustSize() and Cache::checkLRUAndUncacheableListIntegrity().
            * khtml/misc/loader.cpp:
            (CachedObject::setSize): Added. This is now the only legal way to change
    	the size of a cached object, and it calls Cache::adjustSize.
            (CachedCSSStyleSheet::CachedCSSStyleSheet): Pass the length into the base
    	class's constructor instead of setting m_size.
            (CachedCSSStyleSheet::data): Call setSize() instead of setting m_size.
    	Also get the size back with size() rather than m_size.
            (CachedScript::CachedScript): Pass the length into the base class's
    	constructor instead of setting m_size.
            (CachedScript::data): Call setSize() instead of setting m_size. Also get
    	the size back with size() rather than m_size.
            (CachedImage::CachedImage): Don't set m_size to 0, since the base class's
    	constructor does that already.
            (CachedImage::clear): Use setSize() to set the size to 0 instead of setting
    	m_size directly.
            (CachedImage::data): Use setSize() instead of setting m_size directly.
            (Cache::checkLRUAndUncacheableListIntegrity): Useful debugging function.
    	Left in but ifdef'd out.
            (Cache::adjustSize): Added. Changes the "total LRU list size" by the
    	passed-in delta, but only if the passed-in object is in the LRU list.
    
            * kwq/KWQLineEdit.mm:
            * kwq/KWQTextField.h:
            * kwq/KWQTextField.mm:
    	Renamed the Objective C class KWQNSTextField to KWQTextField.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2484 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/misc/loader.h
+++ third_party/WebKit/WebCore/khtml/misc/loader.h
@@ -100,12 +100,12 @@ namespace khtml
 	    Uncacheable   // to big to be cached,
 	};  	          // will be destroyed as soon as possible
 
-	CachedObject(const DOM::DOMString &url, Type type, KIO::CacheControl _cachePolicy, time_t _expireDate)
+	CachedObject(const DOM::DOMString &url, Type type, KIO::CacheControl _cachePolicy, time_t _expireDate, int size = 0)
 	{
 	    m_url = url;
 	    m_type = type;
 	    m_status = Pending;
-	    m_size = 0;
+	    m_size = size;
 	    m_free = false;
 	    m_cachePolicy = _cachePolicy;
 	    m_request = 0;
@@ -173,6 +173,8 @@ namespace khtml
         void setAccept(const QString &_accept) { m_accept = _accept; }
 
     protected:
+        void setSize(int size);
+        
         QPtrList<CachedObjectClient> m_clients;
 
 	DOM::DOMString m_url;
@@ -183,7 +185,9 @@ namespace khtml
 #endif
 	Type m_type;
 	Status m_status;
+    private:
 	int m_size;
+    protected:
 	time_t m_expireDate;
 	KIO::CacheControl m_cachePolicy;
         bool m_free : 1;
@@ -534,6 +538,9 @@ namespace khtml
 
         static void insertInLRUList(CachedObject *);
         static void removeFromLRUList(CachedObject *);
+        static void adjustSize(CachedObject *, int sizeDelta);
+        
+        static void checkLRUAndUncacheableListIntegrity();
 
         protected:
 	static QDict<CachedObject> *cache;
