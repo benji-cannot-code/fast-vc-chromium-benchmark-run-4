e9c07c60a8ee6f4ab7f66098216a56c6474ebe23
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-30 19:34:05 +0000

    Fixed: 3088122 - Assertion failure/crash closing window with QuickTime streaming in
            * WebView.subproj/WebDataSourcePrivate.m:
            (-[WebDataSource _setMainDocumentError:]): call receivedError on the representation.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
@@ -354,6 +354,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [error retain];
     [_private->mainDocumentError release];
     _private->mainDocumentError = error;
+
+    [[self representation] receivedError:error withDataSource:self];
 }
 
 - (void)_clearErrors
