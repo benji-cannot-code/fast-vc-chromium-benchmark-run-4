0f7e07be21360ef0f90d815585d0fa5a91e0ae25
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-30 04:50:42 +0000

    Created Downloads.subproj to hold the to-be-implemented download decoders.
            * Downloads.subproj/WebDownloadDecoder.h: Added.
            * Downloads.subproj/WebDownloadHandler.h: Added.
            * Downloads.subproj/WebDownloadHandler.m: Added.
            (-[WebDownloadHandler initWithDataSource:]):
            (-[WebDownloadHandler dealloc]):
            (-[WebDownloadHandler errorWithCode:]):
            (-[WebDownloadHandler receivedResponse:]):
            (-[WebDownloadHandler receivedData:]):
            (-[WebDownloadHandler finishedLoading]):
            (-[WebDownloadHandler cancel]):
            * Misc.subproj/WebDownloadHandler.h: Removed.
            * Misc.subproj/WebDownloadHandler.m: Removed.
            * WebKit.pbproj/project.pbxproj:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/Downloads.subproj/WebDownloadHandler.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//
+//  WebDownloadHandler.h
+//  WebKit
+//
+//  Created by Chris Blumenberg on Thu Apr 11 2002.
+//  Copyright (c) 2002 Apple computer, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class WebDataSource;
+@class WebError;
+@class WebResourceResponse;
+
+@interface WebDownloadHandler : NSObject
+{
+    WebDataSource *dataSource;
+    NSFileHandle *fileHandle;
+}
+
+- initWithDataSource:(WebDataSource *)dSource;
+- (WebError *)receivedResponse:(WebResourceResponse *)response; 
+- (void)receivedData:(NSData *)data;
+- (WebError *)finishedLoading;
+- (WebError *)cancel;
+@end
