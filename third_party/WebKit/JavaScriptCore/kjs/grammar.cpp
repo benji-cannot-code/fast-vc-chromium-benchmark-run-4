4a992b29878a31e88a6ae456d247c783bf936f0b
sheridan <sheridan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-10-31 18:05:48 +0000

    Alex-30 version markers
    git-svn-id: svn://svn.chromium.org/blink/trunk@2519 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/grammar.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/grammar.cpp
@@ -112,7 +112,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* default values for bison */
 #define YYDEBUG 0
 #define YYMAXDEPTH 0
-#if !APPLE_CHANGES
+#ifdef APPLE_CHANGES
+#else
 #define YYERROR_VERBOSE
 #endif
 #define DBG(l, s, e) { l->setLoc(s.first_line, e.last_line, Parser::sid); } // location
