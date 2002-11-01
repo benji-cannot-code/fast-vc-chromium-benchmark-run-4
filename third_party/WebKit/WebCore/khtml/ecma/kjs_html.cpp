9f6d3235e04e42d62715e623773764addcd8cfff
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-01 01:15:22 +0000

    - fixed huge leak when doing page load test
            * khtml/ecma/kjs_binding.h:
            * khtml/ecma/kjs_binding.cpp: Remove ScriptInterpreter::mark().
    	We don't want to mark the elements of m_domObjects, because we don't want
    	to keep them around the entire lifetime of the interpreter. This is fine
    	because we already have forgetDOMObject(), which takes care of when they
    	are deallocated.
    
            * khtml/ecma/kjs_dom.cpp:
            * khtml/ecma/kjs_dom.h:
            * khtml/ecma/kjs_html.cpp:
            * khtml/ecma/kjs_html.h:
    	Changed lots of parameters to pass by const reference for a bit of a code
    	size and performance gain.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_html.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_html.cpp
@@ -2634,7 +2634,7 @@ IMPLEMENT_PROTOTYPE(HTMLCollectionProto,HTMLCollectionProtoFunc)
 
 const ClassInfo HTMLCollection::info = { "HTMLCollection", 0, 0, 0 };
 
-HTMLCollection::HTMLCollection(ExecState *exec, DOM::HTMLCollection c)
+HTMLCollection::HTMLCollection(ExecState *exec, const DOM::HTMLCollection &c)
   : DOMObject(HTMLCollectionProto::self(exec)), collection(c) {}
 
 HTMLCollection::~HTMLCollection()
@@ -3006,12 +3006,12 @@ Image::~Image()
   if ( img ) img->deref(this);
 }
 
-Value KJS::getHTMLCollection(ExecState *exec,DOM::HTMLCollection c)
+Value KJS::getHTMLCollection(ExecState *exec, const DOM::HTMLCollection &c)
 {
   return cacheDOMObject<DOM::HTMLCollection, KJS::HTMLCollection>(exec, c);
 }
 
-Value KJS::getSelectHTMLCollection(ExecState *exec, DOM::HTMLCollection c, DOM::HTMLSelectElement e)
+Value KJS::getSelectHTMLCollection(ExecState *exec, const DOM::HTMLCollection &c, const DOM::HTMLSelectElement &e)
 {
   DOMObject *ret;
   if (c.isNull())
