3cce4670cd6f74c74784ba1fe1bf5f5ead75f79a
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-02 02:16:25 +0000

    - applied the same "subclass responsibility" pattern here as in 	some of the other WebCore classes
            * kwq/WebCoreImageRendererFactory.h: Move the subclass responsibility into a protocol.
            * kwq/WebCoreImageRendererFactory.m: Remove now-unneeded stubs.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreImageRendererFactory.h
+++ third_party/WebKit/WebCore/kwq/WebCoreImageRendererFactory.h
@@ -28,14 +28,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @protocol WebCoreImageRenderer;
 
+@protocol WebCoreImageRendererFactory
+
+- (id <WebCoreImageRenderer>)imageRenderer;
+- (id <WebCoreImageRenderer>)imageRendererWithBytes:(const void *)bytes length:(unsigned)length;
+- (id <WebCoreImageRenderer>)imageRendererWithSize:(NSSize)size;
+
+@end
+
 @interface WebCoreImageRendererFactory : NSObject
 {
 }
 
 + (WebCoreImageRendererFactory *)sharedFactory;
+@end
 
-- (id <WebCoreImageRenderer>)imageRenderer;
-- (id <WebCoreImageRenderer>)imageRendererWithBytes: (const void *)bytes length:(unsigned)length;
-- (id <WebCoreImageRenderer>)imageRendererWithSize: (NSSize)s;
-
+@interface WebCoreImageRendererFactory (SubclassResponsibility) <WebCoreImageRendererFactory>
 @end
