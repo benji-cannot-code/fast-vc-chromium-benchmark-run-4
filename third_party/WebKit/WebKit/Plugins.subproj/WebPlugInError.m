dee4f818af7fa516c25b0cc1b8a1bf971d5b1452
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-04 07:49:14 +0000

    WebKit:
            * Plugins.subproj/WebPluginError.h:
            * Plugins.subproj/WebPluginError.m:
    	Not __MyCompanyName__, but rather Apple Computer.
    
    WebBrowser:
    
            * DownloadProgressEntry.m:
            * DownloadRow.h:
            * DownloadRow.m:
            * DownloadViewController.h:
            * DownloadViewController.m:
            * DownloadWindowController.h:
            * DownloadWindowController.m:
            * ServicesProvider.h:
            * ServicesProvider.m:
    	Not __MyCompanyName__, but rather Apple Computer.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2547 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPlugInError.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPlugInError.m
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //  WebKit
 //
 //  Created by Chris Blumenberg on Fri Nov 01 2002.
-//  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2002 Apple Computer, Inc. All rights reserved.
 //
 
 #import <WebKit/WebKitErrors.h>
