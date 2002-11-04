388f132dce65e20969a72a5013182a9f8f0b2f9d
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-04 18:58:10 +0000

    * Downloads.subproj/WebMacBinaryDecoder.h: Removed many unneeded fields.         * Downloads.subproj/WebMacBinaryDecoder.m:         (+[WebMacBinaryDecoder canDecodeHeaderData:]): Added comments. Changed maximum filename 	length to 63 as the format supports, rather than the 31 that's based on historical 	HFS limitations. Fix endian dependency in CRC check.         (-[WebMacBinaryDecoder decodeData:dataForkData:resourceForkData:]): Simplified this, 	and removed the state machine and the accumulator. Also added some asserts. Fixed endian 	dependency in the code to extract the header.         (-[WebMacBinaryDecoder finishDecoding]): New simpler check now that we don't have a 	_streamComplete boolean.         (-[WebMacBinaryDecoder fileAttributes]): Rewrote to take advantage of the above changes, 	and also to use kCFAbsoluteTimeIntervalSince1904 rather than parsing a date string.
            * Downloads.subproj/crc16.h: Tweaks.
            * Downloads.subproj/crc16.m: Tweaks.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2551 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Downloads.subproj/crc16.h
+++ third_party/WebKit/WebKit/Downloads.subproj/crc16.h
@@ -6,7 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 u_int16_t CRC16(const u_int8_t *p, u_int32_t len, u_int16_t seed);
 
-
-
-
 #endif
--- third_party/WebKit/WebKit/Downloads.subproj/crc16.m
+++ third_party/WebKit/WebKit/Downloads.subproj/crc16.m
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#include <sys/types.h>
 #include "crc16.h"
 
 static const u_int16_t gMagic[] = {
@@ -46,18 +45,11 @@ static const u_int16_t gMagic[] = {
  */
 u_int16_t CRC16(const u_int8_t *p, u_int32_t len, u_int16_t seed)
 {
-	u_int16_t	reg;				/* crc computed so far */
-	u_int32_t	i;
-
-	reg = seed;				/* start with seed */
-//	while (len--) {
-//		reg = (reg << 8) ^ gMagic[(reg >> 8) ^ *p++];
-//	}
-	for (i = 0; i < len; i++, p++)
-	{
-		reg ^= (*p << 8);
-		reg = (reg << 8) ^ gMagic[(u_int8_t)(reg >> 8)];
-  	}
-
-	return (reg);
-} /* CRC16() */
+    u_int16_t crc = seed;
+    u_int32_t i;
+    for (i = 0; i < len; i++, p++) {
+        crc ^= (*p << 8);
+        crc = (crc << 8) ^ gMagic[(u_int8_t)(crc >> 8)];
+    }
+    return crc;
+}
