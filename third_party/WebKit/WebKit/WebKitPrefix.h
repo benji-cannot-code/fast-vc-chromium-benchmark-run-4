beef46e7eafb1e55fd52c77e0dadc95caebfbb4b
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-05 02:00:55 +0000

    i        Changes to support cursive letter forms.  It works, but I've         disabled it for now.  It's buggy and needs cleanup.         Arabic now renders correctly except for ligature substitution.
            * Misc.subproj/WebUnicode.h:
            * Misc.subproj/WebUnicode.m:
            (_unicodeIsMark):
            (getShape):
            (prevChar):
            (nextChar):
            (prevLogicalCharJoins):
            (nextLogicalCharJoins):
            (glyphVariantLogical):
            (shapedString):
            * Misc.subproj/WebUnicodeTables.m:
            * WebCoreSupport.subproj/WebTextRenderer.m:
            (-[WebTextRenderer drawCharacters:stringLength:fromCharacterPosition:toCharacterPosition:atPoint:withPadding:withTextColor:backgroundColor:rightToLeft:]):
            (-[WebTextRenderer _floatWidthForCharacters:stringLength:fromCharacterPosition:numberOfCharacters:withPadding:applyRounding:attemptFontSubstitution:widths:fonts:glyphs:numGlyphs:]):
            * WebKitPrefix.h:
    
            Changes to support cursive letter forms.
    
            * kwq/KWQChar.mm:
            * kwq/WebCoreUnicode.h:
    
            Updated to lastest API.
    
            * SimpleViewer/DocumentController.m:
            (-[ResourceLoadDelegate initWithDocumentController:]):
            (-[ResourceLoadDelegate updateStatus]):
            (-[ResourceLoadDelegate identifierForInitialRequest:fromDataSource:]):
            (-[ResourceLoadDelegate resource:willSendRequest:fromDataSource:]):
            (-[ResourceLoadDelegate resource:didFinishLoadingFromDataSource:]):
            (-[ResourceLoadDelegate resource:didFailLoadingWithError:fromDataSource:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2558 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebKitPrefix.h
+++ third_party/WebKit/WebKit/WebKitPrefix.h
@@ -15,8 +15,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <QD/ATSUnicodePriv.h>
 #import <CoreGraphics/CoreGraphics.h>
 
+#ifdef __OBJC__
 #import <Cocoa/Cocoa.h>
 #import <Foundation/NSPrivateDecls.h>
+#endif
 
 #import <Carbon/Carbon.h>
 
+#ifdef __cplusplus
+
+#include <cstddef>
+#include <new>
+
+#endif
