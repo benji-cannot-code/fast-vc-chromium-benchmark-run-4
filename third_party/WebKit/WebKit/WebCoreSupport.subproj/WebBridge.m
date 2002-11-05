103d2ed13bdd777826ca94af6a09763fdd8142c7
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-05 11:20:16 +0000

    - fixed 3083732 - SJ: window comes out blank on samsung site
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge createChildFrameNamed:withURL:renderPart:allowsScrolling:marginWidth:marginHeight:]):
    	Don't start loading if the URL is exactly the same as the parent
    	URL. We may need a more stringent check, but this seems to match
    	the tolerance of other browsers OK.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -307,7 +307,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [[newFrame webView] _setMarginWidth:width];
     [[newFrame webView] _setMarginHeight:height];
 
-    [newFrame _loadURL:URL loadType:WebFrameLoadTypeInternal clientRedirect:NO triggeringEvent:nil];
+    // We must avoid loading the document itself as a subframe, like
+    // other browsers do, otherwise bugs like Radar 3083732
+    if (![[[URL _web_URLByRemovingFragment] absoluteURL] isEqual:[[[frame dataSource] URL] absoluteURL]]) {
+	[newFrame _loadURL:URL loadType:WebFrameLoadTypeInternal clientRedirect:NO triggeringEvent:nil];
+    }
 
     return [newFrame _bridge];
 }
