ffa77b395d24ea490c8c1c8c335c529ca286899a
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-05 19:35:22 +0000

    Fix for this bug:
    	Radar 3092747 (javascript cookieEnabled property returns incorrect value)
    
    	Now, we return "true" for navigator.cookieEnabled when
    	"Only accept cookies from the same domain as the current page" option
    	is selected is user preferences.
    
            * WebCoreSupport.subproj/WebCookieAdapter.m:
            (-[WebCookieAdapter cookiesEnabled])
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebCookieAdapter.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebCookieAdapter.m
@@ -27,8 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     BOOL result;
 
-    result = ([[WebCookieManager sharedCookieManager] acceptPolicy] == WebCookieAcceptPolicyAlways);
-
+    WebCookieAcceptPolicy acceptPolicy = [[WebCookieManager sharedCookieManager] acceptPolicy];
+    result = (acceptPolicy == WebCookieAcceptPolicyAlways || acceptPolicy == WebCookieAcceptPolicyOnlyFromMainDocumentDomain);
+    
     return result;
 }
 
