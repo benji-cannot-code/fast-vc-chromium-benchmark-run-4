dd57f38d9e6a064be0d5d0d86e88ff2e8e452f6b
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-05 17:08:33 +0000

    The -(NSEnumeration *)keys method has never been implemented, and is unneeded. 	Removing from API. This has no external impact as this class 	is entirely private to WebFoundation.
            * Database.subproj/WebDatabase.h:
            * Database.subproj/WebDatabase.m:
            * Database.subproj/WebFileDatabase.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2562 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebDatabase.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebDatabase.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 -(void)removeObjectForKey:(id)key;
 -(void)removeAllObjects;
 -(id)objectForKey:(id)key;
--(NSEnumerator *)keys;
 
 @end
 
