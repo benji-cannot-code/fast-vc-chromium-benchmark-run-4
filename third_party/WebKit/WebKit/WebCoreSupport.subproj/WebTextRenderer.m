eb3081b3917784a290c59168dbe2ccd18125d165
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-07 23:50:15 +0000

    Fixed updating of status bar and activity window when         loading plugin content as main document. (3084311)
            * LoadProgressMonitor.m:
            (-[LoadProgressMonitor resource:didReceiveContentLength:fromDataSource:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2589 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.m
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <QD/ATSUnicodePriv.h>
 
+#import <AppKit/NSFont_Private.h>
+
 #import <float.h>
 
 #define NON_BREAKING_SPACE 0x00A0
@@ -295,7 +297,20 @@ static BOOL bufferTextDrawing = NO;
     }
     
     status = ATSUConvertCharToGlyphs(styleGroup, characters, 0, numCharacters, 0, glyphs);
-    
+
+#ifdef DEBUG_GLYPHS
+    int foundGlyphs = 0;
+    ATSLayoutRecord *glyphRecord;
+    for (i = 0; i < numCharacters; i++) {
+        glyphRecord = (ATSLayoutRecord *)glyphs->firstRecord;
+        for (i = 0; i < numCharacters; i++) {
+            if (glyphRecord->glyphID != 0)
+                foundGlyphs++;
+            glyphRecord = (ATSLayoutRecord *)((char *)glyphRecord + glyphs->recordSize);
+        }
+    }
+    printf ("For %s found %d glyphs in range 0x%04x to 0x%04x\n", [[font displayName] cString], foundGlyphs, characters[0], characters[numCharacters-1]);
+#endif
     if (buffer != localBuffer) {
         free(buffer);
     }
@@ -735,6 +750,8 @@ static const char *joiningNames[] = {
             glyphID = [self extendCharacterToGlyphMapToInclude: c];
         }
 
+        // FIXME:  look at next character to determine if it is non-base, then
+        // determine the characters cluster from this base character.
 #ifdef DEBUG_DIACRITICAL
         if (IsNonBaseChar(c)){
             printf ("NonBaseCharacter 0x%04x, joining attribute %d(%s), combining class %d, direction %d, glyph %d, width %f\n", c, WebCoreUnicodeJoiningFunction(c), joiningNames(WebCoreUnicodeJoiningFunction(c)), WebCoreUnicodeCombiningClassFunction(c), WebCoreUnicodeDirectionFunction(c), glyphID, widthForGlyph(self, glyphToWidthMap, glyphID));
@@ -764,6 +781,13 @@ static const char *joiningNames[] = {
                 }
                 numGlyphs += cNumGlyphs;
             }
+#ifdef DEBUG_MISSING_GLYPH
+            else {
+                BOOL hasFont1 = [[NSFont coveredCharacterCache] characterIsMember:c];
+                BOOL hasFont2 = [[NSFont coveredCharacterCache] characterIsMember:originalCharacters[i]];
+                printf ("Unable to find glyph for base character 0x%04x (0x%04x) in font %s(%s) hasFont1 = %d, hasFont2 = %d\n", c, originalCharacters[i], [[font displayName] cString], [[substituteFont displayName] cString], (int)hasFont1, (int)hasFont2);
+            }
+#endif
         }
         
         // If we have a valid glyph OR if we couldn't find a substitute font
