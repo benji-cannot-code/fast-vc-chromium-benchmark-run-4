b0f6567165e96b46951ce22f78671b80b783208c
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-07 01:10:10 +0000

    WebKit:
            * Bookmarks.subproj/WebBookmark.m:
            (+[WebBookmark bookmarkOfType:]):
    	made this handle WebBookmarkTypeProxy
    
    WebBrowser:
    
    	- fixed 3092708 -- allow option for Bookmarks Toolbar as submenu of Bookmarks menu
    
    	The wording and possibly layout in Preferences will change soon, but the
    	behavior is now implemented as SJ specified.
    
            * Defaults.plist:
            * PreferenceKeys.h:
    	New preference keys for the new options
    
            * Preferences.subproj/English.lproj/BookmarkPreferences.nib:
    	rejigger everything in the bookmarks preferences panel
    
            * Preferences.subproj/BookmarkPreferences.h:
            * Preferences.subproj/BookmarkPreferences.m:
            (-[BookmarkPreferences updateBookmarksViews]):
            (-[BookmarkPreferences toggleBookmarksToolbarIncludesAddressBook:]):
            (-[BookmarkPreferences toggleBookmarksToolbarIncludesRendezvous:]):
            (-[BookmarkPreferences toggleBookmarksMenuAndToolbarAreSame:]):
            (-[BookmarkPreferences toggleBookmarksMenuIncludesBookmarksToolbar:]):
            (-[BookmarkPreferences toggleBookmarksMenuIncludesRendezvous:]):
            (-[BookmarkPreferences initializeFromDefaults]):
    	reworked all the code to handle the new options, including the way
    	some options change when others are checked
    
            * BookmarksController.h:
    	made addressBookProxyTitle and rendezvousProxyTitle public
            * BookmarksController.m:
            (-[BookmarksController addChildrenOfBookmark:toMenu:]):
    	new method, generalized from code elsewhere (handles keyboard equivalents
    	for toolbar items)
            (-[BookmarksController addMenuItemForBookmark:toMenu:]):
    	now calls addChildrenOfBookmark:toMenu:
            (-[BookmarksController addFavoritesSubmenu]):
    	new method, calls addChildrenOfBookmark:toMenu:
            (-[BookmarksController addSpecialBookmarkSourcesToMenu]):
    	now handles toolbar as submenu, plus fancier logic to match new preferences
            (-[BookmarksController _addBookmarksToMenu]):
    	now calls addChildrenOfBookmark:toMenu:
            (-[BookmarksController receivedBrowserPreferenceValueChangedNotification:]):
    	now checks for new preferences also
    
            * FavoriteButton.m:
            (-[FavoriteButton setBookmark:]):
    	now handles WebBookmarkTypeProxy -- like folder in appearance, but not a
    	drag target
            (-[FavoriteButton menu]):
    	now handles WebBookmarkTypeProxy (by returning nil for the context menu)
            (-[FavoriteButton hasContentsMenu]):
    	new method, just checks bookmark type
            (-[FavoriteButton _contentsMenu]):
    	now handles WebBookmarkTypeProxy by using BookmarkSource protocol to
    	extract contents
            (-[FavoriteButton mouseDown:]):
    	check hasContentMenu instead of assuming which types have menus
    
            * FavoritesBar.m:
            (-[FavoritesBar addButtonForBookmark:withLeftEdge:]):
    	new method, extracted from _refreshButtons
            (-[FavoritesBar includeRendezvous]),
            (-[FavoritesBar includeAddressBook]):
    	new methods, check with NSUserDefaults
            (-[FavoritesBar _refreshButtons]):
    	take includeRendezvous and includeAddressBook into account; now
    	calls addButtonForBookmark:withLeftEdge:
            (-[FavoritesBar refreshButtonsSoon]):
    	new method, extracted from _favoritesChanged
            (-[FavoritesBar preferenceValueChanged:]):
    	new method, call refreshButtons if the right prefs have changed
            (-[FavoritesBar _favoritesChanged:]):
    	now calls refreshButtonsSoon
            (-[FavoritesBar initWithView:]):
    	register for preferences value changed notifications
            (-[FavoritesBar dropIndexFromDraggingInfo:]):
    	take includeRendezvous and includeAddressBook into account
            (-[FavoritesBar performDragOperation:]):
    	take includeRendezvous and includeAddressBook into account
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2582 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Bookmarks.subproj/WebBookmark.m
+++ third_party/WebKit/WebKit/Bookmarks.subproj/WebBookmark.m
@@ -142,6 +142,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         return [[[WebBookmarkList alloc] init] autorelease];
     } else if (type == WebBookmarkTypeLeaf) {
         return [[[WebBookmarkLeaf alloc] init] autorelease];
+    } else if (type == WebBookmarkTypeProxy) {
+        return [[[WebBookmarkProxy alloc] init] autorelease];
     }
 
     return nil;
