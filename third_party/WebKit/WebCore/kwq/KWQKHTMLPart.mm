958a5ccaf5580699f433bf4ffd188e8aaffa8883
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-08 07:47:12 +0000

    * kwq/KWQKHTMLPart.mm: (KWQKHTMLPart::slotData): 	Add an assert in the hopes of catching bug 3084704 a little earlier.
    git-svn-id: svn://svn.chromium.org/blink/trunk@2597 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKHTMLPart.mm
+++ third_party/WebKit/WebCore/kwq/KWQKHTMLPart.mm
@@ -154,14 +154,15 @@ void KWQKHTMLPart::slotData(NSString *encoding, bool forceEncoding, const char *
         d->m_workingURL = KURL();
     }
 
+    ASSERT(d->m_doc);
+    ASSERT(d->m_doc->parsing());
+    
     if (encoding) {
         part->setEncoding(QString::fromNSString(encoding), forceEncoding);
     } else {
         part->setEncoding(QString::null, false);
     }
     
-    ASSERT(d->m_doc != NULL);
-
     part->write(bytes, length);
 }
 
