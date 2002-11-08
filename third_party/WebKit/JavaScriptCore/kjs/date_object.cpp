d4960d78978d5fd2ecf69e379051abb159fd4569
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-08 03:10:40 +0000

    - partial fix to 3073230 - JavaScript time calls do I/O by 	lastat()ing /etc/localtime
            * kjs/date_object.cpp:
            (mktimeUsingCF): Implementation of mktime using CF.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2593 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/date_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/date_object.cpp
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define gmtime(x) gmtimeUsingCF(x)
 #define localtime(x) localtimeUsingCF(x)
+#define mktime(x) mktimeUsingCF(x)
 
 struct tm *tmUsingCF(time_t tv, CFTimeZoneRef timeZone)
 {
@@ -111,6 +112,27 @@ struct tm *localtimeUsingCF(const time_t *tv)
     CFRelease(timeZone);
     return result;
 }
+
+time_t mktimeUsingCF(struct tm *tm)
+{
+    CFTimeZoneRef timeZone = CFTimeZoneCopyDefault();
+
+    CFGregorianDate date;
+    date.second = tm->tm_sec;
+    date.minute = tm->tm_min;
+    date.hour = tm->tm_hour;
+    date.day = tm->tm_mday;
+    date.month = tm->tm_mon + 1;
+    date.year = tm->tm_year + 1900;
+
+    CFAbsoluteTime absoluteTime = CFGregorianDateGetAbsoluteTime(date, timeZone);
+
+    time_t result = (time_t) (absoluteTime + kCFAbsoluteTimeIntervalSince1970);
+
+    return result;
+}
+
+
 
 #endif // APPLE_CHANGES
 
