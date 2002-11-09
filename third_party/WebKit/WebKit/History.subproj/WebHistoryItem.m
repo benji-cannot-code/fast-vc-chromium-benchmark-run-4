7590d3f5f02347e240a172270ac2a7a3c164c08c
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-09 07:46:49 +0000

    * History.subproj/WebHistoryItem.m:         (-[WebHistoryItem setTitle:]): Use display title if it matches, so 	we don't end up with two identical strings, as when reading from 	the property list.         (-[WebHistoryItem setDisplayTitle:]): The same thing, the other way round.
    git-svn-id: svn://svn.chromium.org/blink/trunk@2613 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebHistoryItem.m
+++ third_party/WebKit/WebKit/History.subproj/WebHistoryItem.m
@@ -140,9 +140,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setTitle:(NSString *)title
 {
-    NSString *copy = [title copy];
+    NSString *newTitle;
+    if (title && [title isEqualToString:_displayTitle]) {
+        newTitle = [_displayTitle retain];
+    } else {
+        newTitle = [title copy];
+    }
     [_title release];
-    _title = copy;
+    _title = newTitle;
 }
 
 - (void)setTarget:(NSString *)target
@@ -161,9 +166,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setDisplayTitle:(NSString *)displayTitle
 {
-    NSString *copy = [displayTitle copy];
+    NSString *newDisplayTitle;
+    if (displayTitle && [displayTitle isEqualToString:_title]) {
+        newDisplayTitle = [_title retain];
+    } else {
+        newDisplayTitle = [displayTitle copy];
+    }
     [_displayTitle release];
-    _displayTitle = copy;
+    _displayTitle = newDisplayTitle;
 }
 
 - (void)setLastVisitedDate:(NSCalendarDate *)date
