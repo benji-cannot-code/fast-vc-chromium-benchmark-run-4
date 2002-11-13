b63b954e4b03933e44efada76d06f3b60edb49bc
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-13 21:40:12 +0000

    Bump WebCore cache to 4 MB (was 1 MB).
            * khtml/misc/loader.cpp
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/misc/loader.cpp
+++ third_party/WebKit/WebCore/khtml/misc/loader.cpp
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // up to which size is a picture for sure cacheable
 #define MAXCACHEABLE 16*1024
 // default cache size
-#define DEFCACHESIZE 1024*1024
+#define DEFCACHESIZE 4096*1024
 
 #include <qasyncio.h>
 #include <qasyncimageio.h>
