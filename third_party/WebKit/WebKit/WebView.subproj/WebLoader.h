b3d399109f9057a98c9f1c091a1c3f6e2c799ea9
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-15 11:33:30 +0000

    Wrap content policy invocation to look asynchronous.
            * WebView.subproj/WebBaseResourceHandleDelegate.h:
            * WebView.subproj/WebMainResourceClient.m:
            (-[WebMainResourceClient continueAfterContentPolicy:response:]):
            (-[WebMainResourceClient checkContentPolicyForResponse:andCallSelector:]):
            (-[WebMainResourceClient handle:didReceiveResponse:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebLoader.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebLoader.h
@@ -19,8 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
 @protected
     WebDataSource *dataSource;
-@private
     WebResourceHandle *handle;
+@private
     WebResourceRequest *request;
     WebResourceResponse *response;
     id identifier;
