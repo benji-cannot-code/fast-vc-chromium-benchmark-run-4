ab85cf1beedb27e89155d58ec173d8722c83e38d
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-19 23:00:17 +0000

    Fixed: 3068112 - extra line breaks when copying from source window
            * Misc.subproj/WebSearchableTextView.m:
            (-[WebSearchableTextView copy:]): Convert CRLF to LF
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2765 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebSearchableTextView.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebSearchableTextView.m
@@ -43,6 +43,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return lastFindWasSuccessful;
 }
 
+- (void)copy:(id)sender
+{
+    if ([self isRichText]) {
+        [super copy:sender];
+    }else{
+        //Convert CRLF to LF to workaround: 3105538 - Carbon doesn't convert text with CRLF to LF
+        NSMutableString *string = [[self string] mutableCopy];
+        [string replaceOccurrencesOfString:@"\r\n" withString:@"\n" options:0 range:NSMakeRange(0, [string length])];
+
+        NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+        [pasteboard declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:self];
+        [pasteboard setString:string forType:NSStringPboardType];
+    }
+}
+
 @end
 
 @implementation NSString (_Web_StringTextFinding)
