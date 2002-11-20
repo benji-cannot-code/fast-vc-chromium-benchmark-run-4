08da65f3d5ab5f9d2ddaa9d4cb33faf12033b4aa
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-20 00:24:42 +0000

    Fixed: 3092588 - redraw errors in QT controller if window is in background
    	We need to send update events after we activate/deactivate after all.
    
            * Plugins.subproj/WebBaseNetscapePluginView.m:
            (-[WebBaseNetscapePluginView windowBecameKey:]):
            (-[WebBaseNetscapePluginView windowResignedKey:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.m
@@ -701,11 +701,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 -(void)windowBecameKey:(NSNotification *)notification
 {
     [self sendActivateEvent:YES];
+    [self sendUpdateEvent];
 }
 
 -(void)windowResignedKey:(NSNotification *)notification
 {
     [self sendActivateEvent:NO];
+    [self sendUpdateEvent];
 }
 
 -(void)windowDidMiniaturize:(NSNotification *)notification
