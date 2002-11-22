4361f3a184653bcf1d395c41082f115fc139ba13
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-22 18:48:11 +0000

    Changed to use simplified SPI.  Drawing and measuring using         WebKit's text renderer are provided as category methods         on NSString.
            * EtchedStringView.m:
            (-[EtchedStringView drawRect:]):
    
            Simplified drawing and measuring SPI for use by Alex.
    
            * Misc.subproj/WebKitNSStringExtras.h: Added.
            * Misc.subproj/WebKitNSStringExtras.m: Added.
            (-[NSString widthForString:font:]):
            * WebKit.pbproj/project.pbxproj:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2826 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/Misc.subproj/WebKitNSStringExtras.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+    WebKitNSStringExtras.h
+    Private (SPI) header
+    Copyright 2002, Apple, Inc. All rights reserved.
+*/
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSString (WebKitExtras)
+
+- (void)drawString:(NSString *)string atPoint:(NSPoint)point font: (NSFont *)font textColor:(NSColor *)textColor;
+
+- (float)widthForString:(NSString *)string font: (NSFont *)font;
+
+@end
