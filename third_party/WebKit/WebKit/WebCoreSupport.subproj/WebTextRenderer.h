b69fe8253c2ca1ec0faa323b991d24106739f236
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-22 22:19:21 +0000

    Fixed rendering issues associated with 3100120.         We now correctly map surrogate pairs in UTF-16.  khtml still has         issues dealing with characters outside BMP.  These are tracked         with 3109251 and 3109258.
            Surrogate pairs are treated as exceptions and have their own character
            to glyph map.
    
            * Misc.subproj/WebUnicode.h:
            * Misc.subproj/WebUnicode.m:
            (shapedString):
            * WebCoreSupport.subproj/WebTextRenderer.h:
            * WebCoreSupport.subproj/WebTextRenderer.m:
            (glyphForUnicodeCharacter):
            (findLengthOfCharacterCluster):
            (-[WebTextRenderer substituteFontForString:]):
            (-[WebTextRenderer convertUnicodeCharacters:length:toGlyphs:]):
            (-[WebTextRenderer _floatWidthForCharacters:stringLength:fromCharacterPosition:numberOfCharacters:withPadding:applyRounding:attemptFontSubstitution:widths:fonts:glyphs:numGlyphs:letterSpacing:wordSpacing:]):
            (-[WebTextRenderer extendUnicodeCharacterToGlyphMapToInclude:]):
            (-[WebTextRenderer extendCharacterToGlyphMapToInclude:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.h
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef struct WidthMap WidthMap;
 typedef struct GlyphMap GlyphMap;
+typedef struct UnicodeGlyphMap UnicodeGlyphMap;
 
 @interface WebTextRenderer : NSObject <WebCoreTextRenderer>
 {
@@ -20,7 +21,8 @@ typedef struct GlyphMap GlyphMap;
     
 @public
     NSFont *font;
-    GlyphMap *characterToGlyphMap;
+    GlyphMap *characterToGlyphMap;			// Used for 16bit clean unicode characters.
+    UnicodeGlyphMap *unicodeCharacterToGlyphMap; 	// Used for surrogates.
     WidthMap *glyphToWidthMap;
     float spaceWidth;
     float adjustedSpaceWidth;
