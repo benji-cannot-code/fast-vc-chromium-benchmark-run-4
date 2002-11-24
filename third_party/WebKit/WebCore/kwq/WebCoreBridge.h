7829615ed672314a84c18f32d91d85e3b6a121a9
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-24 15:56:17 +0000

    WebCore:
    	- fixed 3067939 - no support for window.document.lastModified
    
            * kwq/WebCoreBridge.h:
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge openURL:reload:headers:lastModified:]): Set
    	"modified" metadata based on passed in last mdified date.
    
    WebKit:
    
    	- fixed 3067939 - no support for window.document.lastModified
    
            * WebView.subproj/WebDataSourcePrivate.m:
            (-[WebDataSource _commitIfReady]): Pass last modified date to WebCore.
            * WebView.subproj/WebFramePrivate.m:
            (-[WebFrame _continueFragmentScrollAfterNavigationPolicy:request:]): Pass
    	nil last modified date.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreBridge.h
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.h
@@ -95,7 +95,7 @@ enum FrameBorderStyle {
 
 - (void)setParent:(WebCoreBridge *)parent;
 
-- (void)openURL:(NSString *)URL reload:(BOOL)reload headers:(NSDictionary *)headers;
+- (void)openURL:(NSString *)URL reload:(BOOL)reload headers:(NSDictionary *)headers lastModified:(NSDate *)lastModified;
 - (void)addData:(NSData *)data withEncoding:(NSString *)encoding;
 - (void)addData:(NSData *)data withOverrideEncoding:(NSString *)encoding;
 - (void)closeURL;
