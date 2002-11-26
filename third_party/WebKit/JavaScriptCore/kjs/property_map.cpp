c7c089778096da42eab72be4eee6a97c028c5971
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-11-26 23:34:56 +0000

    * kjs/property_map.cpp: 	(PropertyMap::save): Look at the attributes the same way in the single hash entry 	case as in the actual hash table case. Change the rule for which attributes to save 	to "attributes that don't have the ReadOnly, DontEnum, or Function bit set".         Also fix bug where saving an empty property map would leave the count set to the old value.
    git-svn-id: svn://svn.chromium.org/blink/trunk@2882 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/property_map.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/property_map.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "object.h"
 #include "reference_list.h"
 
+#define DEBUG_PROPERTIES 0
 #define DO_CONSISTENCY_CHECK 0
 #define DUMP_STATISTICS 0
 #define USE_SINGLE_ENTRY 1
@@ -62,7 +63,7 @@ struct PropertyMapHashTable
     int keyCount;
     PropertyMapHashTableEntry entries[1];
 };
-    
+
 class SavedProperty {
 public:
     Identifier key;
@@ -74,9 +75,7 @@ SavedProperties::SavedProperties() : _count(0), _properties(0) { }
 
 SavedProperties::~SavedProperties()
 {
-    if (_properties){
-        delete [] _properties;
-    }
+    delete [] _properties;
 }
 
 // Algorithm concepts from Algorithms in C++, Sedgewick.
@@ -188,7 +187,7 @@ ValueImp *PropertyMap::get(const Identifier &name) const
     return 0;
 }
 
-#ifdef DEBUG_PROPERTIES
+#if DEBUG_PROPERTIES
 static void printAttributes(int attributes)
 {
     if (attributes == 0)
@@ -212,7 +211,7 @@ void PropertyMap::put(const Identifier &name, ValueImp *value, int attributes)
 
     UString::Rep *rep = name._ustring.rep;
     
-#ifdef DEBUG_PROPERTIES
+#if DEBUG_PROPERTIES
     printf ("adding property %s, attributes = 0x%08x (", name.ascii(), attributes);
     printAttributes(attributes);
     printf (")\n");
@@ -440,39 +439,40 @@ void PropertyMap::save(SavedProperties &p) const
 
     if (!_table) {
 #if USE_SINGLE_ENTRY
-        if (_singleEntry.key)
+        if (_singleEntry.key && !(_singleEntry.attributes & (ReadOnly | DontEnum | Function)))
             ++count;
 #endif
     } else {
         for (int i = 0; i != _table->size; ++i)
-            if (_table->entries[i].key && (_table->entries[i].attributes == 0 || _table->entries[i].attributes == (DontDelete | Internal)))
-            //if (_table->entries[i].key)
+            if (_table->entries[i].key && !(_table->entries[i].attributes & (ReadOnly | DontEnum | Function)))
                 ++count;
     }
 
     delete [] p._properties;
+
+    p._count = count;
+
     if (count == 0) {
         p._properties = 0;
         return;
     }
     
     p._properties = new SavedProperty [count];
-    p._count = count;
     
     SavedProperty *prop = p._properties;
     
     if (!_table) {
 #if USE_SINGLE_ENTRY
-        if (_singleEntry.key) {
+        if (_singleEntry.key && !(_singleEntry.attributes & (ReadOnly | DontEnum | Function))) {
             prop->key = Identifier(_singleEntry.key);
             prop->value = Value(_singleEntry.value);
+            prop->attributes = _singleEntry.attributes;
             ++prop;
         }
 #endif
     } else {
         for (int i = 0; i != _table->size; ++i) {
-            if (_table->entries[i].key && (_table->entries[i].attributes == 0 || _table->entries[i].attributes == (DontDelete | Internal))) {
-            //if (_table->entries[i].key) {
+            if (_table->entries[i].key && !(_table->entries[i].attributes & (ReadOnly | DontEnum | Function))) {
                 prop->key = Identifier(_table->entries[i].key);
                 prop->value = Value(_table->entries[i].value);
                 prop->attributes = _table->entries[i].attributes;
@@ -484,9 +484,8 @@ void PropertyMap::save(SavedProperties &p) const
 
 void PropertyMap::restore(const SavedProperties &p)
 {
-    for (int i = 0; i != p._count; ++i){
+    for (int i = 0; i != p._count; ++i)
         put(p._properties[i].key, p._properties[i].value.imp(), p._properties[i].attributes);
-    }
 }
 
 #if DO_CONSISTENCY_CHECK
