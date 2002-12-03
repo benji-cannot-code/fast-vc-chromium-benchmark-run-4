c5ea94d57162f7f6d2cca0dfd14a567f208793d1
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-03 21:36:37 +0000

    - fixed 3114796 -- WORLD LEAKS: 1 WebFrame leaked on trivial source file with <html> tag only 	(probably fixed a ton of other bugs too, since this always leaks)
    	Reviewed by Chris.
    
            * WebView.subproj/WebHTMLViewPrivate.m: (-[WebHTMLView _elementAtPoint:]):
    	Added a missing autorelease.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2908 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewPrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewPrivate.m
@@ -179,7 +179,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     [elementInfo setObject:webFrame forKey:WebElementFrameKey];
     
-    return elementInfo;
+    return [elementInfo autorelease];
 }
 
 - (void)_setAsideSubviews
