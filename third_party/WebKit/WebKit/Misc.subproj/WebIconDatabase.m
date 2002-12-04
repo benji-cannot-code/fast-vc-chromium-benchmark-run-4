33d7f8030a8aea656c77cc0d22d0a8ed4646d97e
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-04 00:21:45 +0000

    Fixed: 3115073 - REGRESSION: plants.com is crashing in WebIconDB with bad retain count
    	The plants.com favicon was marked to be removed from disk and thus it had no retain count. The problem was that it was still on disk and _hasIconForIconURL would return YES. If hasIconForIconURL returns YES, its is ok to call  _setIconURL:forSiteURL:. Since there was no retain count, the assert in _setIconURL:forSiteURL: would be hit.
    
    	We now consider icons without retain counts to not exist even if they're on disk.
    
            * Misc.subproj/WebIconDatabase.m:
            (-[WebIconDatabase _updateFileDatabase]):
            (-[WebIconDatabase _setIconURL:forSiteURL:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2914 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebIconDatabase.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebIconDatabase.m
@@ -293,7 +293,8 @@ NSSize WebIconLargeSize = {128, 128};
 
     WebFileDatabase *fileDB = _private->fileDatabase;
 
-    // Erase icons that have been released that are on disk
+    // Erase icons that have been released that are on disk.
+    // Must remove icons before writing them to disk or else we could potentially remove the newly written ones.
     NSEnumerator *enumerator = [_private->iconsToEraseWithURLs objectEnumerator];
     NSString *iconURLString;
     
@@ -330,8 +331,9 @@ NSSize WebIconLargeSize = {128, 128};
 - (BOOL)_hasIconForIconURL:(NSURL *)iconURL;
 {
     NSString *iconURLString = [iconURL absoluteString];
-    return ([_private->iconURLToIcons objectForKey:iconURLString] ||
-            [_private->iconsOnDiskWithURLs containsObject:iconURLString]);
+    return (([_private->iconURLToIcons objectForKey:iconURLString] ||
+             [_private->iconsOnDiskWithURLs containsObject:iconURLString]) &&
+            [_private->iconURLToRetainCount objectForKey:iconURLString]);
 }
 
 - (NSMutableDictionary *)_iconsForIconURLString:(NSString *)iconURLString
@@ -416,7 +418,6 @@ NSSize WebIconLargeSize = {128, 128};
     ASSERT(iconURL);
     ASSERT(siteURL);
     ASSERT([self _hasIconForIconURL:iconURL]);
-    ASSERT([_private->iconURLToRetainCount objectForKey:[iconURL absoluteString]]);
     
     NSString *siteURLString = [siteURL absoluteString];
     NSString *iconURLString = [iconURL absoluteString];
