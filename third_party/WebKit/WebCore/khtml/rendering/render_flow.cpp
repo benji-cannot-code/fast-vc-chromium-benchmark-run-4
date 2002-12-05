dea6e68691f9b38a09f82a511742471db7a5a5d8
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-05 02:30:54 +0000

    Fix assert on compuserve page.  <form> under <table> should not 	get a layout() call.
            Reviewed by: gramps
    
            * khtml/rendering/render_flow.cpp:
            (RenderFlow::layout):
            * khtml/rendering/render_table.cpp:
            (RenderTable::layout):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2939 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_flow.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_flow.cpp
@@ -227,8 +227,8 @@ void RenderFlow::layout()
 
     KHTMLAssert( !layouted() );
     KHTMLAssert( minMaxKnown() );
-    //KHTMLAssert(!isInline());
-
+    KHTMLAssert(!isInline());
+        
     int oldWidth = m_width;
 
     calcWidth();
