b7b97b1748b3b502acefe4b4577bb18b2c2af55b
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-09 23:16:28 +0000

    Oops.
    git-svn-id: svn://svn.chromium.org/blink/trunk@2980 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -925,10 +925,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
 }
 
+#if 0
+
 - (NSView *)hitTest:(NSPoint)point
 {
     // We handle all clicks. We don't allow subviews to handle them.
     return [super hitTest:point] ? self : nil;
 }
+
+#endif
 
 @end
