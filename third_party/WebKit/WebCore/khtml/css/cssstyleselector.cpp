ed9273c291a7a45385761b358d89f5598819390e
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-09 07:12:19 +0000

    Fix for 3007276, styles do not cascade properly through 	tables.  The font was being reset to the default font 	(konq_default) when it needed to be reset to the body's font. 	I added a new font family value called konq_body and the 	style system resolves that value to be the body's font family list.
            Reviewed by: gramps and mjs
    
            * khtml/css/cssstyleselector.cpp:
            * khtml/html/html_tableimpl.cpp:
            (HTMLTableElementImpl::attach):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/css/cssstyleselector.cpp
+++ third_party/WebKit/WebCore/khtml/css/cssstyleselector.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "css/csshelper.h"
 #include "rendering/render_object.h"
 #include "html/html_documentimpl.h"
+#include "html/html_elementimpl.h"
 #include "xml/dom_elementimpl.h"
 #include "dom/css_rule.h"
 #include "dom/css_value.h"
@@ -2569,6 +2570,28 @@ void CSSStyleSelector::applyRule( DOM::CSSProperty *prop )
                     // from an enclosing different family like monospace.
                     face = settings->stdFontName();
                     fontDef.setGenericFamily(FontDef::eStandard);
+                }
+                else if (face == "konq_body") {
+                    // Obtain the <body> element's font information,
+                    // and use its family list.
+                    DOM::DocumentImpl* doc = element->getDocument();
+                    if (doc && doc->isHTMLDocument()) {
+                        DOM::HTMLDocumentImpl* htmldoc = 
+                          static_cast<DOM::HTMLDocumentImpl*>(doc);
+                        DOM::HTMLElementImpl* body = htmldoc->body();
+                        if (body && body->renderer()) {
+                            FontDef& bodyFontDef = 
+                              (FontDef&)(body->renderer()->style()->htmlFont().fontDef);
+                            fontDef.family = bodyFontDef.firstFamily();
+                            fontDef.genericFamily = bodyFontDef.genericFamily;
+                            if (style->setFontDef( fontDef )) {
+                                fontDirty = true;
+                            }
+                            break;
+                        }
+                    }
+                    
+                    face = settings->stdFontName();
                 }
     
                 if ( !face.isEmpty() ) {
