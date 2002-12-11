56bcdef1f17f71683fd0dc174ae89de41977e668
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-11 05:26:50 +0000

    - fixed 3124081 -- REGRESSION: partial progress is left in address field after download
            Reviewed by Chris.
    
            * WebView.subproj/WebMainResourceClient.m:
            (-[WebMainResourceClient interruptForPolicyChangeAndKeepLoading:]):
    	Put in code to call the locationChangeDone: method on the location change delegate if
    	keepLoading is YES. Since the data source is not finished loading WebFrame won't do it.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@2994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebMainResourceLoader.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebMainResourceLoader.m
@@ -106,6 +106,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // Must call receivedError before _clearProvisionalDataSource because
     // if we remove the data source from the frame, we can't get back to the frame any more.
     [self receivedError:interruptError complete:!keepLoading];
+
+    // Deliver the error to the location change delegate.
+    // We have to do this explicitly because since we are still loading, WebFrame
+    // won't do it for us. There's probably a better way to do this, but this should
+    // do for now.
+    if (keepLoading) {
+        [[[dataSource controller] locationChangeDelegate]
+            locationChangeDone:interruptError forDataSource:dataSource];
+    }
+	
     [[dataSource webFrame] _clearProvisionalDataSource];
     
     [self notifyDelegatesOfInterruptionByPolicyChange];
@@ -181,8 +191,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 	
 	if ([dataSource downloadPath] == nil) {
             // FIXME: Should this be the filename or path?
-	    NSString *saveFilename = [[[dataSource controller] policyDelegate] saveFilenameForResponse:r
-                                                                                            andRequest:req];
+	    NSString *saveFilename = [[[dataSource controller] policyDelegate]
+                saveFilenameForResponse:r andRequest:req];
             // FIXME: Maybe there a cleaner way handle the bad filename case?
             if(!saveFilename || [saveFilename length] == 0){
                 saveFilename = NSHomeDirectory();
@@ -191,7 +201,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 	}
 
         [self interruptForPolicyChangeAndKeepLoading:YES];
-	
+        
 	// Hand off the dataSource to the download handler.  This will cause the remaining
 	// handle delegate callbacks to go to the controller's download delegate.
 	downloadHandler = [[WebDownloadHandler alloc] initWithDataSource:dataSource];
