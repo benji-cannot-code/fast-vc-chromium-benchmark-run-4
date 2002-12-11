da0d47ca1d681744e4092de38462463d9bc8ac4d
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-11 22:37:03 +0000

    Reviewed by: Gramps
    	Change name of cache size file from ".size" to "size"
    
            * Database.subproj/WebFileDatabase.m:
            * English.lproj/StringsNotToBeLocalized.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebFoundationLogging.h"
 #import "WebSystemBits.h"
 
-#define SIZE_FILE_NAME @".size"
-#define SIZE_FILE_NAME_CSTRING ".size"
+#define SIZE_FILE_NAME @"size"
+#define SIZE_FILE_NAME_CSTRING "size"
 
 #if ERROR_DISABLED
 #define BEGIN_EXCEPTION_HANDLER
