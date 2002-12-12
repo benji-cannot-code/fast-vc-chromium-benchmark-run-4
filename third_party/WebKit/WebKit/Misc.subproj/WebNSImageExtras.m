4cc8fa7f6f3beebccaac9589dc875ef9e9f9cedc
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-12 17:15:13 +0000

    Fixed 3094525 - Need to use SPI to fix flipped drag image problem
    	We call the new SPI.  I also had to rework the dissolve steps to get it
    	to non flip the image in Panther in millions (but leave the old code for
    	the Jaguar case).
    
            Reviewed by cblu
    
            * Misc.subproj/WebNSImageExtras.m:
            (+[NSImage load]):  Call the SPI.
            (-[NSImage _web_dissolveToFraction:]):  Add new way of building the image for Panther.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3020 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebNSImageExtras.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebNSImageExtras.m
@@ -10,7 +10,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKitLogging.h>
 #import <WebKit/WebNSImageExtras.h>
 
+// see +load for details
+@interface NSBitmapImageRep (SPINeededForJagGreen)
++ (void)_setEnableFlippedImageFix:(BOOL)f;
+@end
+static BOOL AKBugIsFixed = NO;
+
+
 @implementation NSImage (WebExtras)
+
++ (void)load
+{
+    // See 3094525, 3065097, 2767424.  We need to call this SPI to get it fixed in JagGreen.
+    // Pre-green we're SOL.  In Panther the fix is always on.
+    //
+    // FIXME 3125264: We can nuke this code, the category above, and half the code in
+    // _web_dissolveToFraction when we drop Jag support.
+    //
+    if ([[NSBitmapImageRep class] respondsToSelector:@selector(_setEnableFlippedImageFix:)]) {
+        [NSBitmapImageRep _setEnableFlippedImageFix:YES];
+        AKBugIsFixed = YES;
+    }
+}
 
 - (void)_web_scaleToMaxSize:(NSSize)size
 {
@@ -38,20 +59,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)_web_dissolveToFraction:(float)delta
 {
-    NSImage *dissolvedImage = [[[NSImage alloc] initWithSize:[self size]] autorelease];
-    BOOL isFlipped = [self isFlipped];
-    
-    [self setFlipped:NO];
-    
-    [dissolvedImage lockFocus];
-    [self dissolveToPoint:NSZeroPoint fraction: delta];
-    [dissolvedImage unlockFocus];
+    NSImage *dissolvedImage = [[NSImage alloc] initWithSize:[self size]];
 
-    [self lockFocus];
-    [dissolvedImage compositeToPoint:NSZeroPoint operation:NSCompositeCopy];
-    [self unlockFocus];
+    if (AKBugIsFixed) {
+        // In this case the dragging image is always correct.
+        [dissolvedImage setFlipped:[self isFlipped]];
 
-    [self setFlipped:isFlipped];
+        [dissolvedImage lockFocus];
+        [self dissolveToPoint:NSMakePoint(0, [self size].height) fraction: delta];
+        [dissolvedImage unlockFocus];
+
+        [self lockFocus];
+        [dissolvedImage compositeToPoint:NSMakePoint(0, [self size].height)   operation:NSCompositeCopy];
+        [self unlockFocus];
+    } else {
+        // In this case Thousands mode will have an inverted drag image.  Millions is OK.
+        // FIXME 3125264: this branch of code can go when we drop Jaguar support.
+        BOOL isFlipped = [self isFlipped];
+        [self setFlipped:NO];
+
+        [dissolvedImage lockFocus];
+        [self dissolveToPoint:NSZeroPoint fraction: delta];
+        [dissolvedImage unlockFocus];
+
+        [self lockFocus];
+        [dissolvedImage compositeToPoint:NSZeroPoint operation:NSCompositeCopy];
+        [self unlockFocus];
+
+        [self setFlipped:isFlipped];
+    }
+    [dissolvedImage release];
 }
 
 @end
