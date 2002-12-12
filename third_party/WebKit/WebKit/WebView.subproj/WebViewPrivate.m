0cb02242432013fa018c3c7a69d4efc82cdf8afe
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-12 08:48:28 +0000

    Fixed 3125585.  One click crasher option clicking on         any link.  Added additional check to curtail overly         zealous ASSERT.
            Reviewed by mjs.
    
            * WebView.subproj/WebControllerPrivate.m:
            (-[WebController _mainReceivedError:fromDataSource:complete:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3012 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebViewPrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebViewPrivate.m
@@ -153,7 +153,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     ASSERT(error);
     ASSERT(dataSource);
-    ASSERT([dataSource webFrame]);
+#ifndef NDEBUG
+    if (![dataSource isDownloading])
+        ASSERT([dataSource webFrame]);
+#endif    
 
     [dataSource _setMainDocumentError: error];
 
