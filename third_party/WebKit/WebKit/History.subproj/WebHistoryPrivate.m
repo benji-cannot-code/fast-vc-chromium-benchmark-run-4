467135bbf9fa2f401b29234cc72c61cccb10eba8
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-13 02:29:47 +0000

    3117101 - PLT slows down as history fills up
    	I saw a 3% sloth effect from a huge (100k) history.  This change cut that cost in half.
    	I think we still have a marginally measurable cost for our worst practical case.
    
            Reviewed by rjw
    
            * History.subproj/WebHistoryPrivate.m:
            (-[WebHistoryPrivate insertEntry:atDateIndex:]):  Comment.
            (-[WebHistoryPrivate removeEntryForURLString:]):  Use removeIdentical
    	instead of remove, since we don't need to do all the equals comparisons.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebHistoryPrivate.m
+++ third_party/WebKit/WebKit/History.subproj/WebHistoryPrivate.m
@@ -99,6 +99,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     entryDate = [entry lastVisitedDate];
     entriesForDate = [_entriesByDate objectAtIndex: dateIndex];
     count = [entriesForDate count];
+    // optimized for inserting oldest to youngest
     for (index = 0; index < count; ++index) {
         if ([entryDate compare: [[entriesForDate objectAtIndex: index] lastVisitedDate]] != NSOrderedAscending) {
             break;
@@ -127,7 +128,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     ASSERT(foundDate);
     
     entriesForDate = [_entriesByDate objectAtIndex: dateIndex];
-    [entriesForDate removeObject: entry];
+    [entriesForDate removeObjectIdenticalTo: entry];
 
     // remove this date entirely if there are no other entries on it
     if ([entriesForDate count] == 0) {
