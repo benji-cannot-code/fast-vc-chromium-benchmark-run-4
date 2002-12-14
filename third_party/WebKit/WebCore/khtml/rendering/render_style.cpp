fb9a2a8a9e3f621b27b411128473f0e8ef58c3ed
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-14 01:02:24 +0000

    Fix for 3098045.
    	Do not treat XHTML-style tags e.g., <br/> or <table/> as
    	self-closing.  This is absolutely wrong, and it does not
    	match the behavior of other browsers.  According to the HTML4
    	spec for parsing, / is an erroneous character and should simply
    	be dropped, so <table/> should be treated like <table>.
    
    	Fixes for crashes on glazman's blog and on tantek's blog.  Both
    	still mis-render, but at least they don't crash.
    
            Reviewed by gramps (and darin too)
    
            * khtml/html/htmltokenizer.cpp:
    	* khtml/rendering/render_container.cpp:
    	* khtml/rendering/render_style.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3045 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_style.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_style.cpp
@@ -483,9 +483,12 @@ void RenderStyle::setContent(DOM::DOMStringImpl* s)
 	content = new ContentData;
     else
 	content->clearContent();
-
+    
+    if (!s)
+        s = new DOM::DOMStringImpl("");
     content->_content.text = s;
     content->_content.text->ref();
+        
     content->_contentType = CONTENT_TEXT;
 }
 
