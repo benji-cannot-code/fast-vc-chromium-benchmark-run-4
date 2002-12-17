3674200fc9af5bb1dc23a7ba4db810af745f64cb
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-17 08:54:17 +0000

    top level:
            Reviewed by Maciej.
    
    	- remove leftover bits of NO_LICENSE
    
            * acconfig.h: Remove NO_LICENSE.
            * configure.in: Remove all the NO_LICENSE-related bits.
    
    WebBrowser:
    
            Reviewed by Maciej.
    
    	- remove leftover bits of NO_LICENSE
    
            * WebBrowser.pbproj/project.pbxproj: Remove build styles setting HAVE_CONFIG_H and also
    	INCLUDE_TEST_SUITE too, because that hasn't been used anywhere for like forever.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/config.h
+++ third_party/WebKit/WebCore/config.h
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-/* This turns off the licensing code */
-
 /* Define if using alloca.c.  */
 /* #undef C_ALLOCA */
 
