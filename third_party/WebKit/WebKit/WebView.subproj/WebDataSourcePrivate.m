b597cb4d96482243a66840734a58ec26a5d8bd41
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-17 01:38:51 +0000

    Fixed: 3129503 - Crash cancelling download after closing browser window that started download
            Reviewed by rjw.
    
            * WebView.subproj/WebDataSourcePrivate.m:
            (-[WebDataSource _setLoading:]): Added comment about our tragic dependence on a non-retained reference to the controller.
            (-[WebDataSource _recursiveStopLoading]): Call webFrame before calling _stopLoading because we release the controller in _stopLoading and we depend on it in webFrame.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3094 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.m
@@ -91,9 +91,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)_setLoading:(BOOL)loading
 {
     ASSERT_ARG(loading, loading == NO || loading == YES);
-    
-    if (_private->loading == loading)
+
+    if (_private->loading == loading) {
         return;
+    }
+    
     _private->loading = loading;
     
     if (loading) {
@@ -101,6 +103,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         [_private->controller retain];
     } else {
         [_private->controller release];
+        // FIXME: It would be cleanest to set the controller to nil here.  Keeping a non-retained reference
+        // to the controller is dangerous. WebSubresourceClient actually depends on this non-retained reference
+        // when starting loads after the data source has stoppped loading.
         [self release];
     }
 }
@@ -238,8 +243,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)_recursiveStopLoading
 {
     [self retain];
-    [self _stopLoading];
+    
+    // We depend on the controller in webFrame and we release it in _stopLoading,
+    // so call webFrame first so we don't send a message the released controller (3129503).
     [[[self webFrame] children] makeObjectsPerformSelector:@selector(stopLoading)];
+    [self _stopLoading];
+    
     [self release];
 }
 
