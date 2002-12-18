cf0abf33f5ea0e2d6cfd26d81e1ce95bc422fe52
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-18 07:33:02 +0000

    NOWRAP was completely broken.  This fixes the problem 	and makes porsche.com look ok again.
    	Bug #3131287.
    
            Reviewed by gramps
    
            * khtml/rendering/bidi.cpp:
            (RenderFlow::findNextLineBreak):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3117 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
@@ -1252,6 +1252,7 @@ BidiIterator RenderFlow::findNextLineBreak(BidiIterator &start, QPtrList<BidiIte
             // proportional font, needs a bit more work.
             int lastSpace = pos;
             bool isPre = o->style()->whiteSpace() == PRE;
+            
             //QChar space[1]; space[0] = ' ';
             //int spaceWidth = f->width(space, 1, 0);
             while(len) {
@@ -1306,7 +1307,7 @@ BidiIterator RenderFlow::findNextLineBreak(BidiIterator &start, QPtrList<BidiIte
                         }
                     }
         
-                    if ( !isPre && w + tmpW > width )
+                    if (w + tmpW > width && o->style()->whiteSpace() == NORMAL)
                         goto end;
 
                     lBreak.obj = o;
@@ -1426,7 +1427,7 @@ BidiIterator RenderFlow::findNextLineBreak(BidiIterator &start, QPtrList<BidiIte
 #ifdef DEBUG_LINEBREAKS
     kdDebug( 6041 ) << "end of par, width = " << width << " linewidth = " << w + tmpW << endl;
 #endif
-    if( w + tmpW <= width ) {
+    if( w + tmpW <= width || (last && last->style()->whiteSpace() == NOWRAP)) {
         lBreak.obj = 0;
         lBreak.pos = 0;
     }
