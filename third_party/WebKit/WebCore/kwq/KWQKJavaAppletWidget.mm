6821d95ccede01abd5d3d2245134a30a8181948d
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-19 06:18:15 +0000

    Fixed: 3131267 - REGRESSION: Java 1.4.1 Applets Restarted When Window is Resized 	Fixed: 3131244 - REGRESSION: Java 1.4.1 Multiple Copies of Applet Started When Window is Resized
    	Dave recently patched khtml with a fix for media content inside of table cells. The patch causes the java widget to get the layout call more than once. Calling layout more than once is perfectly allowed, but the java widget wasn't prepared for that and would create a new java view for each layout. We now only create a java view when the java view hasn't already been created.
    
            Reviewed by darin.
    
            * kwq/KWQKJavaAppletWidget.mm:
            (KJavaAppletWidget::showApplet): If the view of the widget is still a KWQView, we haven't replaced it with the Java view. This stops multiple view from being created.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3133 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKJavaAppletWidget.mm
+++ third_party/WebKit/WebCore/kwq/KWQKJavaAppletWidget.mm
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "KWQKJavaAppletContext.h"
 #import "KWQKURL.h"
 #import "KWQKHTMLPart.h"
+#import "KWQView.h"
 #import "WebCoreBridge.h"
 
 KJavaAppletWidget::KJavaAppletWidget(KJavaAppletContext *c, QWidget *)
@@ -61,8 +62,12 @@ void KJavaAppletWidget::processArguments(const QMap<QString, QString> &arguments
 
 void KJavaAppletWidget::showApplet()
 {
-    setView([KWQ(_context->part())->bridge()
-        viewForJavaAppletWithFrame:NSMakeRect(pos().x(), pos().y(), size().width(), size().height())
-                        attributes:_parameters
-                           baseURL:_baseURL.getNSString()]);
+    // If the view is a KWQView, we haven't replaced it with the Java view yet.
+    // Only set the Java view once.
+    if ([getView() isKindOfClass:[KWQView class]]) {
+        setView([KWQ(_context->part())->bridge()
+            viewForJavaAppletWithFrame:NSMakeRect(pos().x(), pos().y(), size().width(), size().height())
+                            attributes:_parameters
+                            baseURL:_baseURL.getNSString()]);
+    }
 }
