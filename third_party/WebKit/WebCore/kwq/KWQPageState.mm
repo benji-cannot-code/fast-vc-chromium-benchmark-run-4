f487a0b1471d87528acf45557a15bf67943d89aa
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-19 06:34:54 +0000

    Added an ASSERT to ensure that 3127329 isn't still happening.         This bug should have been fixed be earlier changes.
            Reviewed by darin.
    
            * kwq/KWQPageState.mm:
            (-[KWQPageState dealloc]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3134 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQPageState.mm
+++ third_party/WebKit/WebCore/kwq/KWQPageState.mm
@@ -78,6 +78,8 @@ using KJS::SavedProperties;
     if (document) {
         KHTMLView *view = document->view();
 
+        ASSERT (view);
+        
         KWQKHTMLPart::clearTimers(view);
 
         document->setInPageCache(NO);
