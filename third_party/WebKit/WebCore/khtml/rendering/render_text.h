ffa914404215baf3fcff6230e4fb9221f1c9f0d7
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-20 20:41:42 +0000

    WebCore:         Fixed 3133261.  This fix really has two parts.  This first part         fixes the stupid stack allocated buffer that caused the crash.         The second part makes any RenderText with a large string perform         much faster.  The page mentioned in the bug used to load and draw         in about 15 minutes.  Now it loads in about 10 seconds and draws in about         2 seconds.  The performance optimization caches a widths array         for the string in the RenderText, and only updates that array if         the font or text for the RenderText change.
            Reviewed by john.
    
            * khtml/rendering/render_text.cpp:
            (RenderText::RenderText):
            (RenderText::setStyle):
            (RenderText::~RenderText):
            (RenderText::computeWidths):
            (RenderText::widthFromBuffer):
            (RenderText::trimmedMinMaxWidth):
            (RenderText::calcMinMaxWidth):
            (RenderText::setText):
            (RenderText::width):
            * khtml/rendering/render_text.h:
    
    WebKit:
            Fixed 3133261.  This fix really has two parts.  This first part
            is here in WebTextRenderer.  The second part adds some width
            caching to RenderText.  I was using a stack allocated array,
            this would blow out the stack for large strings.
    
            Reviewed by john.
    
            * WebCoreSupport.subproj/WebTextRenderer.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3152 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_text.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_text.h
@@ -218,6 +218,8 @@ public:
 #if APPLE_CHANGES
     TextSlave * findTextSlave( int offset, int &pos );
     TextSlaveArray textSlaves() { return m_lines; }
+    int widthFromBuffer(const Font *, int start, int len) const;
+    void computeWidths();
 #endif
 
 protected:
@@ -237,6 +239,8 @@ protected: // members
     short m_maxWidth;
     short m_beginMinWidth;
     short m_endMinWidth;
+    short m_beginMaxWidth;
+    short m_endMaxWidth;
     
     SelectionState m_selectionState : 3 ;
     bool m_hasBreakableChar : 1; // Whether or not we can be broken into multiple lines.
@@ -245,6 +249,9 @@ protected: // members
     bool m_hasEndWS : 1; // Whether or not we end with WS (only true if we aren't pre)
     
     // 19 bits left
+#if APPLE_CHANGES
+    float *m_widths;
+#endif
 };
 
 
