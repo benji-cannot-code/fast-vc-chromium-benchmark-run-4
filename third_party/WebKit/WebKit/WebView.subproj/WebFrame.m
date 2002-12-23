e3ddf5dd10913758a26ba2df69b60f4870b67531
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-23 22:34:55 +0000

    Reviewed by John and Don.
    	- fixed 3134282 -- REGRESSION: text encoding setting reverts when you go to a new location
    
            * WebView.subproj/WebFrame.m: (-[WebFrame loadRequest:]): Propagate an override encoding if there
    	was an existing data source and it had an override encoding.
            * WebView.subproj/WebFramePrivate.m: (-[WebFrame _loadRequest:triggeringAction:loadType:]): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3176 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
@@ -120,6 +120,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     } else {
         loadType = WebFrameLoadTypeStandard;
     }
+
+    [newDataSource _setOverrideEncoding:[[self dataSource] _overrideEncoding]];
+
     [self _loadDataSource:newDataSource withLoadType:loadType];
     [newDataSource release];
 }
