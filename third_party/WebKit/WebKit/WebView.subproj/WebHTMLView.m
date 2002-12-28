72100cf0254effb4bb58b017349a4a957484517a
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-28 18:33:08 +0000

    Reviewed by Don.
    	- fixed 3136206 -- Can't display images in Ambrosia Software image viewer
    
    	WebKit wasn't creating the frames correctly when they weren't HTML.
    
            * WebView.subproj/WebFramePrivate.m: (-[WebFrame _makeDocumentView]): Added. Calls the
    	WebView to make the document view and then installs the new view, as WebHTMLView used to do.
            (-[WebFrame _transitionToCommitted:]): Call _makeDocumentView instead of calling WebView
    	directly. These are all the calls to _makeDocumentViewForDataSource:.
    
            * WebView.subproj/WebViewPrivate.h: Add return value to _makeDocumentViewForDataSource:.
            * WebView.subproj/WebViewPrivate.m: (-[WebView _makeDocumentViewForDataSource:]):
    	Return the newly-created view.
    
            * WebView.subproj/WebHTMLView.m: (-[WebHTMLView setDataSource:]): Removed the code here
    	since it's done by WebFrame now.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -938,11 +938,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //------------------------------------------------------------------------------------
 - (void)setDataSource:(WebDataSource *)dataSource 
 {
-    [[dataSource _bridge]
-        createKHTMLViewWithNSView:self
-        marginWidth:[[[dataSource webFrame] webView] _marginWidth]
-        marginHeight:[[[dataSource webFrame] webView] _marginHeight]];
-    [[self _bridge] installInFrame:[[self _web_parentWebView] frameScrollView]];
 }
 
 - (void)dataSourceUpdated:(WebDataSource *)dataSource
