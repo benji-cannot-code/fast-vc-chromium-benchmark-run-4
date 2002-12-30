65f56d7c47e6eba969a5ffce8900fd51dbee1453
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-30 19:19:35 +0000

    Reviewed by Don and Ken.
    	- fixed 3136797 -- crash when Adobe SVG Viewer plug-in puts up modal dialog
    
            * Plugins.subproj/WebBaseNetscapePluginView.h: Add inSetWindow boolean.
            * Plugins.subproj/WebBaseNetscapePluginView.m:
            (-[WebBaseNetscapePluginView sendEvent:]): Don't send any events to a plug-in while it's
    	inside NPP_SetWindow. We don't want to implement more general reentrancy protection, because
    	it could cause trouble for plugins that can handle it correctly, but it's unlikely that any
    	legitimate use would require reentrant calls while inside NPP_SetWindow, and that's the case
    	that crashes for the SVG viewer plug-in when it presents its registration dialog.
            (-[WebBaseNetscapePluginView setWindow]): Set boolean.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3210 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.h
+++ third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.h
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NPP_t instanceStruct;
 
     BOOL canRestart;
-    BOOL isHidden;
     BOOL isStarted;
+    BOOL inSetWindow;
             
     NSString *MIMEType;
     NSURL *baseURL;
