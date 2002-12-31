7640350c94b03932dbd9c265f5732ff8e504dda0
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2002-12-31 05:42:59 +0000

    3137110 - REGRESSION: calls from 2nd and subsequent instances of Java 1.4.1 plug-in seem to be ignored
    	We had previously made changes to "numb" a plugin controller after its frame
    	was tossed or changed content.  We now finish the job by making sure a new
    	controller is created when we go to a new page with plugins.  To so this we
    	release the old controller whenever we flush the plugins from a page.
    
            Reviewed by Maciej.
    
            * WebView.subproj/WebFramePrivate.m:
            (-[WebFrame _detachFromParent]):  Call newly factored code.
            (-[WebFrame _transitionToCommitted:]):  Call newly factored code.
            (-[WebFrame _destroyPluginController]):  New method to tear down plugins.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFramePrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFramePrivate.m
@@ -102,6 +102,8 @@ Repeat load of the same URL (by any other means of navigation other than the rel
 - (WebHistoryItem *)_createItemTreeWithTargetFrame:(WebFrame *)targetFrame clippedAtTarget:(BOOL)doClip;
 
 - (void)_resetBackForwardListToCurrent;
+
+- (void)_destroyPluginController;
 @end
 
 @implementation WebFramePrivate
@@ -338,7 +340,7 @@ Repeat load of the same URL (by any other means of navigation other than the rel
     _private->bridge = nil;
 
     // Destroy plug-ins before blowing away the view.
-    [_private->pluginController destroyAllPlugins];
+    [self _destroyPluginController];
 
     [self stopLoading];
     [self _saveScrollPositionToItem:[_private currentItem]];
@@ -508,7 +510,7 @@ Repeat load of the same URL (by any other means of navigation other than the rel
     ASSERT([self controller] != nil);
 
     // Destroy plug-ins before blowing away the view.
-    [_private->pluginController destroyAllPlugins];
+    [self _destroyPluginController];
         
     switch ([self _state]) {
         case WebFrameStateProvisional:
@@ -1615,6 +1617,18 @@ static CFAbsoluteTime _timeOfLastCompletedLoad;
     child->_private->parent = self;
     [[child _bridge] setParent:_private->bridge];
     [[child dataSource] _setOverrideEncoding:[[self dataSource] _overrideEncoding]];   
+}
+
+- (void)_destroyPluginController
+{
+    [_private->pluginController destroyAllPlugins];
+    // We flush this PluginController, so if a later page requests one we will make a new one
+    // instead of reusing the old one.  Plugin views retain their PluginController, so a
+    // wayward view might try to message an old controller even after we call destroyAllPlugins.
+    // We don't want to have a controller that has to both ignore requests from stale views
+    // and honor those from current views, so it's easiest to make a new controller.
+    [_private->pluginController release];
+    _private->pluginController = nil;
 }
 
 - (WebPluginController *)_pluginController
