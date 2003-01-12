12be8d24c1dc4c6fa9e522f9abd38f7871cef1a6
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-12 18:43:08 +0000

    Reviewed by John.
    	- changes inspired by things I noticed reviewing diffs vs. KDE when preparing the tarball
    
            * kjs/function.cpp: (GlobalFuncImp::call): Use strtol when strtoll is
    	not available. Do #ifndef NDEBUG, not #if !NDEBUG.
            * kjs/function.h: Do #ifndef NDEBUG, not #if !NDEBUG.
            * kjs/internal.cpp:
            (InterpreterImp::initGlobalObject): Do #ifndef NDEBUG, not #if !NDEBUG.
            (KJS::printInfo): Remove case for ListType and remove default case that just
    	ends up suppressing the "missing case" warning and does no good.
            * kjs/interpreter.cpp: (Interpreter::evaluate): Do #ifndef NDEBUG, not #if !NDEBUG.
            * kjs/nodes.cpp:
            (Node::finalCheck): Fix accidentally-deleted code in an ifdef we never compile.
            (FunctionCallNode::evaluate): Remove bogus XXX comment. Maciej put this comment in,
            and together we determined it's not needed.
            (TypeOfNode::evaluate): Ditto.
            * kjs/object.cpp: Remove assert that refers to ListType.
            * kjs/value.h: Remove ListType.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3306 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/internal.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/internal.cpp
@@ -649,7 +649,7 @@ void InterpreterImp::initGlobalObject()
   global.put(globExec,"isFinite",   Object(new GlobalFuncImp(globExec,funcProto,GlobalFuncImp::IsFinite,   1)), DontEnum);
   global.put(globExec,"escape",     Object(new GlobalFuncImp(globExec,funcProto,GlobalFuncImp::Escape,     1)), DontEnum);
   global.put(globExec,"unescape",   Object(new GlobalFuncImp(globExec,funcProto,GlobalFuncImp::UnEscape,   1)), DontEnum);
-#if !NDEBUG
+#ifndef NDEBUG
   global.put(globExec,"kjsprint",   Object(new GlobalFuncImp(globExec,funcProto,GlobalFuncImp::KJSPrint,   1)), DontEnum);
 #endif
 
@@ -896,11 +896,6 @@ void KJS::printInfo(ExecState *exec, const char *s, const Value &o, int lineno)
       name = Object::dynamicCast(v).className();
       if (name.isNull())
         name = "(unknown class)";
-      break;
-    case ListType:
-      name = "List";
-      break;
-    default:
       break;
     }
     UString vString = v.toString(exec);
--- third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
@@ -116,6 +116,8 @@ Reference Node::evaluateReference(ExecState *exec)
 void Node::finalCheck()
 {
   fprintf( stderr, "Node::finalCheck(): list count       : %d\n", (int)s_nodes.size() );
+  std::list<Node *>::iterator it = s_nodes->begin();
+  for ( uint i = 0; it != s_nodes->end() ; ++it, ++i )
     fprintf( stderr, "[%d] Still having node %p (%s) (refcount %d)\n", i, (void*)*it, typeid( **it ).name(), (*it)->refcount );
   delete s_nodes;
   s_nodes = 0L;
@@ -728,7 +730,6 @@ Value FunctionCallNode::evaluate(ExecState *exec)
 #endif
 
   Value thisVal;
-  // XXX - should check for constant reference
   if (ref.isMutable())
     thisVal = ref.getBase(exec);
   else
@@ -862,7 +863,6 @@ Value TypeOfNode::evaluate(ExecState *exec)
   const char *s = 0L;
   Reference ref = expr->evaluateReference(exec);
   KJS_CHECKEXCEPTIONVALUE
-  // XXX - Really should check if this is a constant reference
   if (ref.isMutable()) {
     Value b = ref.getBase(exec);
     if (b.type() == NullType)
