b3f677735437ceae0716994d6f1ed589f1a2a081
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-12 05:13:14 +0000

    Reviewed by John.
    	- fixed 3144479 -- put TITLE from links in status bar
    
            * WebKit.exp: Add _WebElementLinkTitleKey and also sort this file.
    
            * WebCoreSupport.subproj/WebImageRenderer.m: Need to touch a file to get the above
    	to have any effect, so update the copyright date here.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3302 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*	
         WebImageRenderer.m
-	Copyright (c) 2002, Apple, Inc. All rights reserved.
+	Copyright (c) 2002, 2003, Apple, Inc. All rights reserved.
 */
 
 #import <WebKit/WebImageRenderer.h>
