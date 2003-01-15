bcb1cd5c168d426c745f29c88c8b08c0721157b7
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-15 16:42:41 +0000

    Reviewed by Ken.
    	- rolled in a change from the KJS folks
    
    	Fixes a bug where the date functions would not accept non-strings.
    	And provides a bit of a speedup.
    
            * kjs/date_object.h: Change parameter type for parseDate.
            * kjs/date_object.cpp:
            (DateObjectFuncImp::call): Always call toString, don't check the type.
            (KJS::parseDate): Take a UString parameter, not a String parameter.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3327 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/date_object.h
+++ third_party/WebKit/JavaScriptCore/kjs/date_object.h
@@ -120,7 +120,7 @@ namespace KJS {
   };
 
   // helper functions
-  Value parseDate(const String &s);
+  Value parseDate(const UString &u);
   time_t KRFCDate_parseDate(const UString &_date); 
   Value timeClip(const Value &t);
 
