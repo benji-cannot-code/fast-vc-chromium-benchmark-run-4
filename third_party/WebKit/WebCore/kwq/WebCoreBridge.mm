11c869280d20943bd659d75d2444216f799b3fc9
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-15 16:40:09 +0000

    WebCore:
            Reviewed by Ken.
    
    	- fixed problem where Content-Type was coming in with suffixes like "charset"
    
            * kwq/WebCoreBridge.h: Add contentType and refresh parameters, and remove "headers"
    	parameter, from openURL method.
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge openURL:reload:contentType:refresh:lastModified:pageCache:]): Get the
    	content type from a parameter, and the refresh header from a separate parameter rather
    	than extracting both from a headers dictionary.
    
    WebKit:
    
            Reviewed by Ken.
    
    	- fixed problem where Content-Type was going into WebCore with suffixes like "charset"
    
            * WebView.subproj/WebDataSourcePrivate.m: (-[WebDataSource _commitIfReady:]):
    	Pass in contentType from the response, rather than the "Content-Type" header.
    	Also pass in the refresh header separately.
    
            * English.lproj/StringsNotToBeLocalized.txt: Updated for above change.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3326 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreBridge.mm
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.mm
@@ -139,7 +139,7 @@ static bool initializedObjectCacheSize = FALSE;
     _part->setParent([parent part]);
 }
 
-- (void)openURL:(NSString *)URL reload:(BOOL)reload headers:(NSDictionary *)headers lastModified:(NSDate *)lastModified pageCache:(NSDictionary *)pageCache
+- (void)openURL:(NSString *)URL reload:(BOOL)reload contentType:(NSString *)contentType refresh:(NSString *)refresh lastModified:(NSDate *)lastModified pageCache:(NSDictionary *)pageCache
 {
     if (pageCache) {
         KWQPageState *state = [pageCache objectForKey:@"WebCorePageState"];
@@ -151,7 +151,6 @@ static bool initializedObjectCacheSize = FALSE;
     // arguments
     URLArgs args(_part->browserExtension()->urlArgs());
     args.reload = reload;
-    NSString *contentType = [headers objectForKey:@"Content-Type"];
     if (contentType) {
         args.serviceType = QString::fromNSString(contentType);
     }
@@ -161,9 +160,8 @@ static bool initializedObjectCacheSize = FALSE;
     _part->didOpenURL([URL cString]);
 
     // things we have to set up after calling didOpenURL
-    NSString *refreshHeader = [headers objectForKey:@"Refresh"];
-    if (refreshHeader) {
-        _part->addMetaData("http-refresh", QString::fromNSString(refreshHeader));
+    if (refresh) {
+        _part->addMetaData("http-refresh", QString::fromNSString(refresh));
     }
     if (lastModified) {
 	_part->addMetaData("modified", QString::fromNSString([lastModified description]));
