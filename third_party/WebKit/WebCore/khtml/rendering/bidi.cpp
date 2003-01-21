01b2757e3d3dc8033561f22130e84576853640c7
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-21 00:00:55 +0000

    Gah. Fix positioned objects and floating objects so that they 	don't append themselves to the list of runs in normal flow. 	An astoundingly basic bug. :(
            Reviewed by john
    
            * khtml/rendering/bidi.cpp:
            (appendRunsForObject):
            * khtml/rendering/render_flow.cpp:
            (RenderFlow::addChildToFlow):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3357 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
@@ -265,7 +265,7 @@ inline QChar::Direction BidiIterator::direction() const
 
 static void appendRunsForObject(int start, int end, RenderObject* obj)
 {
-    if (start > end)
+    if (start > end || obj->isSpecial())
         return;
         
     BidiIterator* nextMidpoint = (smidpoints && smidpoints->count()) ? smidpoints->at(0) : 0;
