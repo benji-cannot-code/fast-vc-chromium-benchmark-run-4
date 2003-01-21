982462cc33e11f3d35989a88338b1749717a4cb0
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-21 00:09:19 +0000

    Nit fix to remove a dead "cursor" ivar found while grepping.
            Reviewed by Richard.
    
            * WebView.subproj/WebDynamicScrollBarsView.h:
            * WebView.subproj/WebDynamicScrollBarsView.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3358 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDynamicScrollBarsView.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebDynamicScrollBarsView.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface WebDynamicScrollBarsView : NSScrollView <WebCoreFrameView>
 {
-    NSCursor *cursor;
     BOOL disallowsScrolling;
     BOOL suppressLayout;
 }
--- third_party/WebKit/WebKit/WebView.subproj/WebDynamicScrollBarsView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDynamicScrollBarsView.m
@@ -104,10 +104,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return !disallowsScrolling;
 }
 
-- (void)dealloc
-{
-    [cursor release];
-    [super dealloc];
-}
-
 @end
