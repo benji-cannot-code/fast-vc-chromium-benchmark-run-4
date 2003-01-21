d3221e8b7c6843bcde980186e6fa48f73255a8b6
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-21 19:54:36 +0000

    Reviewed by Nobody, fixing an accidental commit.
            * khtml/khtml_part.cpp:
    	* khtml/html/html_documentimpl.cpp:
            * khtml/xml/dom_docimpl.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_docimpl.h
+++ third_party/WebKit/WebCore/khtml/xml/dom_docimpl.h
@@ -296,8 +296,6 @@ public:
 
     void setParsing(bool b) { m_bParsing = b; }
     bool parsing() const { return m_bParsing; }
-    void setLoading(bool b) { m_bLoading = b; }
-    bool loading() const { return m_bLoading; }
 
     void setTextColor( DOMString color ) { m_textColor = color; }
     DOMString textColor() const { return m_textColor; }
@@ -502,7 +500,6 @@ protected:
     bool m_loadingSheet;
     bool visuallyOrdered;
     bool m_bParsing;
-    bool m_bLoading;
     bool m_docChanged;
     bool m_styleSelectorDirty;
     bool m_inStyleRecalc;
