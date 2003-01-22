951b170bafa979c8fe36d3eed866c46e9e270f24
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-22 19:57:58 +0000

    Removed comment that I added by mistake.
            Reviewed by me.
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView draggingSourceOperationMaskForLocal:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3401 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -632,7 +632,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (unsigned)draggingSourceOperationMaskForLocal:(BOOL)isLocal
 {
-    NSLog(@"draggingSourceOperationMaskForLocal");
     return (NSDragOperationGeneric|NSDragOperationCopy);
 }
 
