393e4e6b2439f7a28e90f80ff0b0794f796b245c
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-22 21:50:53 +0000

    Reviewed by Maciej.
    	- fixed 3153673 -- spoof as MacIE to get into http://www.mazdausa.com
    	- fixed 3153678 -- spoof as MacIE for http://wap.sonyericsson.com/
    
            * WebView.subproj/WebUserAgentSpoofTable.gperf: Add the two new entries.
    	Also reorganize existing entries a bit.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3404 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebUserAgentSpoofTable.c
+++ third_party/WebKit/WebKit/WebView.subproj/WebUserAgentSpoofTable.c
@@ -28,7 +28,7 @@ hash (register const char *str, register unsigned int len)
       27, 27, 27, 27, 27, 27, 27,  0,  0,  0,
        0,  0, 10,  0,  0,  0, 14, 27,  0,  0,
        0,  0,  0, 27,  0,  0,  0,  0,  5,  0,
-      27,  5, 27, 27, 27, 27, 27, 27, 27, 27,
+      27,  5,  0, 27, 27, 27, 27, 27, 27, 27,
       27, 27, 27, 27, 27, 27, 27, 27, 27, 27,
       27, 27, 27, 27, 27, 27, 27, 27, 27, 27,
       27, 27, 27, 27, 27, 27, 27, 27, 27, 27,
@@ -48,6 +48,10 @@ hash (register const char *str, register unsigned int len)
   switch (hval)
     {
       default:
+      case 20:
+        hval += asso_values[(unsigned char)str[19]];
+      case 19:
+        hval += asso_values[(unsigned char)str[18]];
       case 18:
         hval += asso_values[(unsigned char)str[17]];
       case 17:
@@ -97,9 +101,9 @@ _web_findSpoofTableEntry (register const char *str, register unsigned int len)
 {
   enum
     {
-      TOTAL_KEYWORDS = 14,
+      TOTAL_KEYWORDS = 16,
       MIN_WORD_LENGTH = 6,
-      MAX_WORD_LENGTH = 18,
+      MAX_WORD_LENGTH = 20,
       MIN_HASH_VALUE = 9,
       MAX_HASH_VALUE = 26
     };
@@ -111,7 +115,7 @@ _web_findSpoofTableEntry (register const char *str, register unsigned int len)
       {"watch.com", MacIE},
       {"battle.net", MacIE},
       {"porsche.com", MacIE},
-      {"",0},
+      {"mazdausa.com", MacIE},
       {"hondacars.com", MacIE},
       {"abcnews.go.com", WinIE},
       {"",0},
@@ -124,7 +128,7 @@ _web_findSpoofTableEntry (register const char *str, register unsigned int len)
       {"firstusa.com", MacIE},
       {"microsoft.com", MacIE},
       {"jaguar.com", MacIE},
-      {"",0},
+      {"wap.sonyericsson.com", MacIE},
       {"bang-olufsen.com", MacIE}
     };
 
