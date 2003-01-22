f8c3b8b1c99fd37ffe0e7745afdedd38a3040d8a
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-22 00:12:40 +0000

    - correct our copyrights to 2003; copyright is based on year of publication, not year worked on
    git-svn-id: svn://svn.chromium.org/blink/trunk@3376 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_layer.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_layer.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2002, 2003 Apple Computer, Inc.
+ * Copyright (C) 2003 Apple Computer, Inc.
  *
  * Portions are Copyright (C) 1998 Netscape Communications Corporation.
  *
--- third_party/WebKit/WebCore/khtml/xml/dom2_eventsimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom2_eventsimpl.cpp
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  * (C) 2001 Peter Kelly (pmk@post.com)
  * (C) 2001 Tobias Anton (anton@stud.fbi.fh-darmstadt.de)
- * Copyright (C) 2002 Apple Computer, Inc.
+ * Copyright (C) 2003 Apple Computer, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
