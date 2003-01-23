edc488558832a8b498784cdece3e23eacd4e4c4f
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-23 21:52:41 +0000

    Fix for 3142124.
    	When pseudo-elements have display:none, they should not generate
    	rendering objects.
    
            Reviewed by darin.
    
            * khtml/rendering/render_container.cpp:
            (RenderContainer::insertPseudoChild):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3429 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_container.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_container.cpp
@@ -213,7 +213,7 @@ void RenderContainer::insertPseudoChild(RenderStyle::PseudoId type, RenderObject
 
     RenderStyle* pseudo = child->style()->getPseudoStyle(type);
 
-    if (pseudo)
+    if (pseudo && pseudo->display() != NONE)
     {
         if (pseudo->contentType()==CONTENT_TEXT)
         {
