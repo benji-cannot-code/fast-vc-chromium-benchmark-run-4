47e58a7c85b7cacc838d803427caed9cbfd73afd
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-23 21:54:35 +0000

    Reviewed by Darin.
            * WebView.subproj/WebController.m: Add missing static.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3430 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebView.m
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebFoundation/WebNSUserDefaultsExtras.h>
 #import <WebFoundation/WebResourceHandle.h>
 
-const struct UserAgentSpoofTableEntry *_web_findSpoofTableEntry(const char *, unsigned);
+static const struct UserAgentSpoofTableEntry *_web_findSpoofTableEntry(const char *, unsigned);
 
 // Turn off inlining to avoid warning with newer gcc.
 #undef __inline
