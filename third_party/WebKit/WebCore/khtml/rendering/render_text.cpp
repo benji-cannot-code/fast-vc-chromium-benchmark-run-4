afca8f2d3f4f5c391da3a4fef2456bd10c14c174
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-24 03:46:17 +0000

    WebCore:         Fixed whitespace exclusion code.  Fixes many sites         that have problems that were exposed after         fixing 3119777.
            Reviewed by hyatt.
    
            * khtml/rendering/bidi.cpp:
            (RenderFlow::bidiReorderLine):
            (RenderFlow::findNextLineBreak):
            * khtml/rendering/break_lines.cpp:
            * khtml/rendering/render_text.cpp:
            (RenderText::calcMinMaxWidth):
    
    Test:
    	Tests for the fix above.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_text.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_text.cpp
@@ -967,7 +967,7 @@ void RenderText::calcMinMaxWidth()
                 str->s[i] = ' ';
         }
         
-        bool oldSpace = isSpace;
+        bool previousCharacterIsSpace = isSpace;
         isSpace = str->s[i].direction() == QChar::DirWS;
         
         if ((isSpace || isNewline) && i == 0)
@@ -975,7 +975,7 @@ void RenderText::calcMinMaxWidth()
         if ((isSpace || isNewline) && i == len-1)
             m_hasEndWS = true;
             
-        if (!ignoringSpaces && !isPre && oldSpace && isSpace)
+        if (!ignoringSpaces && !isPre && previousCharacterIsSpace && isSpace)
             ignoringSpaces = true;
         
         if (ignoringSpaces && !isSpace)
