e97012ff6fb49e1a8b2b1324bc251a4900e64fb3
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-27 22:13:25 +0000

    WebKit:         Make emptying the page cache synchronous when "Empty Cache"         is selected from menu.  More bulletproofing to ensure that         3155781 doesn't happen.
            Reviewed by trey.
    
            * History.subproj/WebBackForwardList.m:
            (-[WebBackForwardList clearPageCache]):
    
    WebCore:
            Reviewed by trey.
    
            Added ASSERT trap to help track down non-repro crash reported
            by 3144546.
    
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::openURLFromPageCache):
    
            Remove assert that caused 3155781.  We believe this was happening
            because of a delayed release of the page cache after emptying cache
            from "Empty Cache" menu item.
    
            * kwq/KWQPageState.mm:
            (-[KWQPageState dealloc]):
    
    *.lut.h changes are spurious.
    *.c changes are spurious.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3466 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
+++ third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
@@ -194,6 +194,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     for (i = 0; i < (int)[_entries count]; i++) {
         [[_entries objectAtIndex:i] setHasPageCache:NO];
     }
+    [WebHistoryItem _releaseAllPendingPageCaches];
 }
 
 static BOOL pageCacheSizeModified = NO;
