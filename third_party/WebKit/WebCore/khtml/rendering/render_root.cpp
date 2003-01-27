a6ae716533fbd6971114ae5c28eee0ffc5f6f2da
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-27 07:41:38 +0000

    WebCore:
    	Fixed: 3156725 - Partially selected links show extra underlining when dragged
    	Fixed: 3156962 - Text drag image sometimes doesn't include last selected text node
    
            Reviewed by darin.
    
            * khtml/rendering/render_root.cpp:
            (RenderRoot::selectionRect): include all nodes that are selected
            * khtml/rendering/render_text.cpp:
            (TextSlave::paintDecoration): removed stray ";"
            (RenderText::paintObject): call paintDecoration with default parameters for "from" and "to" when not painting the selection only
            * khtml/rendering/render_text.h:
    
    WebKit:
    
    	Fixed: 3156725 - Partially selected links show extra underlining when dragged
    
            Reviewed by darin.
    
            * WebCoreSupport.subproj/WebTextRenderer.m:
            (drawLineForCharacters...): don't ignore "from" "to" parameters
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3460 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_root.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_root.cpp
@@ -304,10 +304,6 @@ QRect RenderRoot::selectionRect() const
 
     while (r && r != m_selectionEnd)
     {
-        if (r->selectionState() == SelectionInside && r != m_selectionStart) {
-            selectionRect = selectionRect.unite(enclosingPositionedRect(r));
-        }
-        
         RenderObject* n;
         if ( !(n = r->firstChild()) ){
             if ( !(n = r->nextSibling()) )
@@ -320,6 +316,9 @@ QRect RenderRoot::selectionRect() const
             }
         }
         r = n;
+        if (r) {
+            selectionRect = selectionRect.unite(enclosingPositionedRect(r));
+        }
     }
 
     return selectionRect;
