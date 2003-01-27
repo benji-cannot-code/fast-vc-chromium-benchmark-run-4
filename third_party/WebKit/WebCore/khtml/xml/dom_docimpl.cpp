280a7d3f6b9fdc493b4fc08e219fedc65dff6476
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-27 07:33:59 +0000

    Reviewed by Maciej.
    	- fixed 3126431 -- failed assertion `!attached()' at silkair.net
    
            * khtml/rendering/render_image.h: Fix isImage which for some strange reason was
            returning false. Not important because no one calls it. Added dispatchLoadEvent
            and detach member functions.
            * khtml/rendering/render_image.cpp:
            (RenderImage::notifyFinished): Instead of dispatching the load or error event
            immediately, call dispatchImageLoadEventSoon on the document.
            (RenderImage::dispatchLoadEvent): Added. Moved the code to do the actual
    	event dispatch in here.
            (RenderImage::detach): Remove the image from the document in case it has a
            pending event that was not delivered yet before calling the base class detach.
    
            * khtml/xml/dom_docimpl.h: Add dispatchImageLoadEventSoon, dispatchImageLoadEventsNow,
            removeImage, and an override of timerEvent. Moved APPLE_CHANGES code to a better place.
            * khtml/xml/dom_docimpl.cpp: Moved APPLE_CHANGES code to a better place and made these changes:
            (DocumentImpl::DocumentImpl): Initialize m_imageLoadEventTimer to 0.
            (DocumentImpl::detach): Redo the APPLE_CHANGES to make them easier to read.
            Empty out the m_imageLoadEventDispatchSoonList and the m_imageLoadEventDispatchingList
            to avoid an O(n^2) operation as we detach each image and remove it from the list.
            (DocumentImpl::dispatchImageLoadEventSoon): Added. Adds image to list and sets up timer.
            (DocumentImpl::removeImage): Added. Removes image from lists, and cancels timer if needed.
            (DocumentImpl::dispatchImageLoadEventsNow): Added. Copies list, and call images to dispatch load events.
            (DocumentImpl::timerEvent): Added. Calls dispatchImageLoadEventsNow.
    
            * khtml/html/html_documentimpl.cpp: (HTMLDocumentImpl::close):
            Dispatch all pending image load events before dispatching the document load.
    
            * force-clean-timestamp: Force rebuild because Project Builder doesn't do
    	header dependencies well enough to deal with changes to the document class.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_docimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_docimpl.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "rendering/render_root.h"
 #include "rendering/render_replaced.h"
+#include "rendering/render_image.h"
 #include "render_arena.h"
 
 #include "khtmlview.h"
@@ -228,9 +229,9 @@ QPtrList<DocumentImpl> * DocumentImpl::changedDocuments = 0;
 // KHTMLView might be 0
 DocumentImpl::DocumentImpl(DOMImplementationImpl *_implementation, KHTMLView *v)
     : NodeBaseImpl( new DocumentPtr() )
+    , m_imageLoadEventTimer(0)
 #if APPLE_CHANGES
-    , m_finishedParsing(this, SIGNAL(finishedParsing())), m_inPageCache(0),
-    m_passwordFields(0), m_secureForms(0)
+    , m_finishedParsing(this, SIGNAL(finishedParsing())), m_inPageCache(0), m_passwordFields(0), m_secureForms(0)
 #endif
 {
     document->doc = this;
@@ -1023,14 +1024,22 @@ void DocumentImpl::restoreRenderer(RenderObject* render)
 
 void DocumentImpl::detach()
 {
-#if APPLE_CHANGES
-    if (!m_inPageCache){
-        //printf ("detaching document\n");
-#endif
     RenderObject* render = m_render;
 
     // indicate destruction mode,  i.e. attached() but m_render == 0
     m_render = 0;
+    
+#if APPLE_CHANGES
+    if (m_inPageCache) {
+        return;
+    }
+#endif
+
+    // Empty out these lists as a performance optimization, since detaching
+    // all the individual render objects will cause all the RenderImage
+    // objects to remove themselves from the lists.
+    m_imageLoadEventDispatchSoonList.clear();
+    m_imageLoadEventDispatchingList.clear();
 
     NodeBaseImpl::detach();
 
@@ -1043,12 +1052,6 @@ void DocumentImpl::detach()
         delete m_renderArena;
         m_renderArena = 0;
     }
-#if APPLE_CHANGES
-    }
-    else {
-        m_render = 0;
-    }
-#endif
 }
 
 void DocumentImpl::setVisuallyOrdered()
@@ -2131,6 +2134,96 @@ EventListener *DocumentImpl::createHTMLEventListener(QString code)
     return view()->part()->createHTMLEventListener(code);
 }
 
+void DocumentImpl::dispatchImageLoadEventSoon(RenderImage *image)
+{
+    m_imageLoadEventDispatchSoonList.append(image);
+    if (!m_imageLoadEventTimer) {
+        m_imageLoadEventTimer = startTimer(0);
+    }
+}
+
+void DocumentImpl::removeImage(RenderImage *image)
+{
+    m_imageLoadEventDispatchSoonList.remove(image);
+    m_imageLoadEventDispatchingList.remove(image);
+    if (m_imageLoadEventDispatchSoonList.isEmpty() && m_imageLoadEventTimer) {
+        killTimer(m_imageLoadEventTimer);
+        m_imageLoadEventTimer = 0;
+    }
+}
+
+void DocumentImpl::dispatchImageLoadEventsNow()
+{
+    if (m_imageLoadEventTimer) {
+        killTimer(m_imageLoadEventTimer);
+        m_imageLoadEventTimer = 0;
+    }
+    
+    m_imageLoadEventDispatchingList = m_imageLoadEventDispatchSoonList;
+    m_imageLoadEventDispatchSoonList.clear();
+    for (QPtrListIterator<RenderImage> it(m_imageLoadEventDispatchingList); it.current(); ) {
+        RenderImage *image = it.current();
+        // Must advance iterator *before* dispatching call.
+        // Otherwise, it might be advanced automatically if dispatching the call had a side effect
+        // of destroying the current RenderImage, and then we would advance past the *next* item,
+        // missing one altogether.
+        ++it;
+        image->dispatchLoadEvent();
+    }
+    m_imageLoadEventDispatchingList.clear();
+}
+
+void DocumentImpl::timerEvent(QTimerEvent *)
+{
+    dispatchImageLoadEventsNow();
+}
+
+#if APPLE_CHANGES
+
+bool DocumentImpl::inPageCache()
+{
+    return m_inPageCache;
+}
+
+void DocumentImpl::setInPageCache(bool flag)
+{
+    m_inPageCache = flag;
+}
+
+void DocumentImpl::passwordFieldAdded()
+{
+    m_passwordFields++;
+}
+
+void DocumentImpl::passwordFieldRemoved()
+{
+    assert(m_passwordFields > 0);
+    m_passwordFields--;
+}
+
+bool DocumentImpl::hasPasswordField() const
+{
+    return m_passwordFields > 0;
+}
+
+void DocumentImpl::secureFormAdded()
+{
+    m_secureForms++;
+}
+
+void DocumentImpl::secureFormRemoved()
+{
+    assert(m_secureForms > 0);
+    m_secureForms--;
+}
+
+bool DocumentImpl::hasSecureForm() const
+{
+    return m_secureForms > 0;
+}
+
+#endif // APPLE_CHANGES
+
 // ----------------------------------------------------------------------------
 
 DocumentFragmentImpl::DocumentFragmentImpl(DocumentPtr *doc) : NodeBaseImpl(doc)
@@ -2234,50 +2327,5 @@ NodeImpl *DocumentTypeImpl::cloneNode ( bool /*deep*/ )
     // so we do not support it...
     return 0;
 }
-
-#if APPLE_CHANGES
-bool DocumentImpl::inPageCache()
-{
-    return m_inPageCache;
-}
-
-void DocumentImpl::setInPageCache(bool flag)
-{
-    m_inPageCache = flag;
-}
-
-void DocumentImpl::passwordFieldAdded()
-{
-    m_passwordFields++;
-}
-
-void DocumentImpl::passwordFieldRemoved()
-{
-    assert(m_passwordFields > 0);
-    m_passwordFields--;
-}
-
-bool DocumentImpl::hasPasswordField() const
-{
-    return m_passwordFields > 0;
-}
-
-void DocumentImpl::secureFormAdded()
-{
-    m_secureForms++;
-}
-
-void DocumentImpl::secureFormRemoved()
-{
-    assert(m_secureForms > 0);
-    m_secureForms--;
-}
-
-bool DocumentImpl::hasSecureForm() const
-{
-    return m_secureForms > 0;
-}
-
-#endif
 
 #include "dom_docimpl.moc"
