343062fdbfb6344a925094ecb5aa81380b014daf
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-28 16:23:10 +0000

    Reviewed by Ken.
    	- fixed 3156197 -- leak in -[WebBookmarkList initFromDictionaryRepresentation:withGroup:]
    
            * Bookmarks.subproj/WebBookmarkList.m:
            (-[WebBookmarkList initFromDictionaryRepresentation:withGroup:]):
    	Fixed storage leak by not allocating the list here, because [self init] will
            be called and that will allocate the list.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3473 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Bookmarks.subproj/WebBookmarkList.m
+++ third_party/WebKit/WebKit/Bookmarks.subproj/WebBookmarkList.m
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         return nil;
     }
 
-    _list = [[NSMutableArray alloc] init];
     _title = [[dict objectForKey:TitleKey] copy];
 
     NSArray *storedChildren = [dict objectForKey:ChildrenKey];
