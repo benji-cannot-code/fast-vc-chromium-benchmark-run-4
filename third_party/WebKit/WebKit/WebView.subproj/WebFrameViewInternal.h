b1c86b1105b68ee0cb506b035ccd7d10cee18f22
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-29 21:53:53 +0000

    3159750 - REGRESSION: cursor is I-beam when over a standalone image
    	We now reset the cursor to arrow before switching doc views.
    
    	Also, made various replacements of "id <WebDocumentView>" with "NSView < WebDocumentView> *"
    	to get better compile time checking.
    
            Reviewed by Darin.
    
            * WebView.subproj/WebFramePrivate.m:
            (-[WebFrame _makeDocumentView]):  Use NSView*
            (-[WebFrame _transitionToCommitted:]): Use NSView*
            * WebView.subproj/WebViewPrivate.h: Use NSView*
            * WebView.subproj/WebViewPrivate.m:
            (-[WebView _setDocumentView:]): Use NSView*.  Reset cursor.
            (-[WebView _makeDocumentViewForDataSource:]): Use NSView*
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3493 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrameViewInternal.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrameViewInternal.h
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 @interface WebView (WebPrivate)
-- (void)_setDocumentView:(id <WebDocumentView>)view;
-- (id <WebDocumentView>)_makeDocumentViewForDataSource:(WebDataSource *)dataSource;
+- (void)_setDocumentView:(NSView <WebDocumentView> *)view;
+- (NSView <WebDocumentView> *)_makeDocumentViewForDataSource:(WebDataSource *)dataSource;
 - (void)_setController:(WebController *)controller;
 - (int)_marginWidth;
 - (int)_marginHeight;
