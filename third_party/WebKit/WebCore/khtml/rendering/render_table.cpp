5b422541ca5174a638a5e5c74ec9412600ef58fc
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-29 23:21:28 +0000

    Fix for tables.  They weren't initializing their max top/bottom 	values for margin collapsing, so their margins were being 	ignored!
            Reviewed by darin
    
            * khtml/rendering/render_flow.cpp:
            (RenderFlow::layout):
            * khtml/rendering/render_flow.h:
            * khtml/rendering/render_table.cpp:
            (RenderTable::layout):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_table.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_table.cpp
@@ -240,7 +240,8 @@ void RenderTable::layout()
     //kdDebug( 6040 ) << renderName() << "(Table)"<< this << " ::layout0() width=" << width() << ", layouted=" << layouted() << endl;
 
     m_height = 0;
-
+    initMaxMarginValues();
+    
     //int oldWidth = m_width;
     calcWidth();
 
