a81fccf1780b245d9a4a3d48b7d607a3bef193d9
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-01-31 16:54:22 +0000

    3161088 - disabled select elements are not disabled when scrolling list used
    	We use a tableview delegate method to prevent the user from changing the
    	selection when disabled.  Too bad tableviews don't know how to disable themselves.
    	We also condition the cell used for drawing to match our enabled state.
    
            Reviewed by Maciej.
    
            * kwq/KWQListBox.h:  Keep track of whether we are enabled.
            * kwq/KWQListBox.mm:
            (QListBox::QListBox):  Init new flag.
            (QListBox::setEnabled):  Set new flag.
            (QListBox::isEnabled):  Getter for new flag.
            (-[KWQListBoxTableViewDelegate selectionShouldChangeInTableView:]):
    	Disallow selection changes if disabled.
            (-[KWQListBoxTableViewDelegate tableView:willDisplayCell:forTableColumn:row:]):
    	Enable cell based on our enabled state to get visual feedback of being disabled.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3522 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQListBox.h
+++ third_party/WebKit/WebCore/kwq/KWQListBox.h
@@ -61,6 +61,8 @@ public:
     void endBatchInsert();
     void setSelected(int, bool);
     bool isSelected(int) const;
+    void setEnabled(bool enabled);
+    bool isEnabled();
     
     bool changingSelection() { return _changingSelection; }
     void clicked() { _clicked.call(); }
@@ -72,6 +74,7 @@ private:
     NSMutableArray *_items;
     bool _insertingItems;
     bool _changingSelection;
+    bool _enabled;
     mutable float _width;
     mutable bool _widthGood;
     
