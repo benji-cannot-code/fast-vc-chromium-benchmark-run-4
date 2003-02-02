06258f0b367e374032aff49d05d6363e4151c083
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-02-02 01:29:17 +0000

    - fix mistake that broke build
            * khtml/rendering/render_block.h: Add include of <qsortedlist.h>.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3540 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_block.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_block.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef RENDER_BLOCK_H
 #define RENDER_BLOCK_H
 
+#include <qsortedlist.h>
+
 #include "render_flow.h"
 
 namespace khtml {
