cb5c431b18fb4733bde78f33440136118446a958
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-02-13 22:40:07 +0000

    Reviewed by Trey.
            - fixed 3162806 -- REGRESSION: handle == h assertion when clicking headphone link at cnn.com
    
            * khtml/ecma/kjs_window.cpp: (HistoryFunc::tryCall): Instead of trying to actually do the
            history navigation here, call the new KHTMLPart::scheduleHistoryNavigation.
    
            * khtml/khtml_part.cpp:
            (KHTMLPart::init): Add a few more #if !APPLE_CHANGES so we can leave out more unused fields.
            (KHTMLPart::openURL): Ditto.
            (KHTMLPart::createJavaContext): Ditto.
            (KHTMLPart::clear): Also initialize m_scheduledRedirection to noRedirectionScheduled.
            (KHTMLPart::checkCompleted): Use m_scheduledRedirection rather than m_redirectURL to check
            whether a redirection is already in progress.
            (KHTMLPart::scheduleRedirection): Use m_scheduledRedirection rather than m_redirectURL to check
            whether a redirection is already in progress, and set m_scheduledRedirection to redirectionScheduled.
            (KHTMLPart::scheduleHistoryNavigation): Added. Does the same thing as scheduleRedirection,
            but always uses a delay of 0, sets m_scheduledRedirection to historyNavigationScheduled, and
            sets m_scheduledHistoryNavigationSteps.
            (KHTMLPart::slotRedirect): Add an if statement that does the history navigation. The code is
            moved here from HistoryFunc::tryCall. Also set m_scheduledRedirection to noRedirectionScheduled.
            (KHTMLPart::urlSelected): Add a few more #if !APPLE_CHANGES so we can leave out more unused fields.
            (KHTMLPart::requestObject): Ditto.
            (KHTMLPart::submitForm): Ditto.
            (KHTMLPart::slotParentCompleted): Use m_scheduledRedirection rather than m_redirectURL to check
            whether a redirection is already in progress.
            (KHTMLPart::setZoomFactor): Add a few more #if !APPLE_CHANGES so we can leave out more unused fields.
    
            * khtml/khtml_part.h: Added scheduleHistoryNavigation.
            * khtml/khtmlpart_p.h: Added RedirectionScheduled enum. Added more #if !APPLE_CHANGES to leave out
            a bunch more fields we don't use. Added m_scheduledRedirection and m_scheduledHistoryNavigationSteps.
            * kwq/KWQKHTMLPart.mm: (KWQKHTMLPart::redirectionTimerStartedOrStopped): Check and don't do anything
            if the timer was being used for history redirection.
    
            * khtml/html/html_documentimpl.cpp: (HTMLDocumentImpl::close):
            Check m_scheduledRedirection instead of m_redirectURL to figure out if there is a redirection
            in progress. Not strictly necessary since we really don't care about the history case here,
            but consistent with the rest.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3646 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtmlpart_p.h
+++ third_party/WebKit/WebCore/khtml/khtmlpart_p.h
@@ -94,6 +94,8 @@ typedef FrameList::Iterator FrameIt;
 
 static int khtml_part_dcop_counter = 0;
 
+enum RedirectionScheduled { noRedirectionScheduled, redirectionScheduled, historyNavigationScheduled };
+
 class KHTMLPartPrivate
 {
 public:
@@ -109,8 +111,8 @@ public:
     m_bLoadEventEmitted = true;
     m_bUnloadEventEmitted = true;
     m_cachePolicy = KIO::CC_Verify;
-    m_manager = 0L;
 #if !APPLE_CHANGES
+    m_manager = 0L;
     m_settings = new KHTMLSettings(*KHTMLFactory::defaultHTMLSettings());
 #endif
     m_bClearing = false;
@@ -125,8 +127,10 @@ public:
     m_jobPercent = 0;
     m_haveEncoding = false;
     m_activeFrame = 0L;
+#if !APPLE_CHANGES
     m_findDialog = 0;
     m_ssl_in_use = false;
+#endif
 #ifndef Q_WS_QWS
     m_javaContext = 0;
 #endif
@@ -151,6 +155,7 @@ public:
 
     m_bFirstData = true;
     m_submitForm = 0;
+    m_scheduledRedirection = noRedirectionScheduled;
     m_delayRedirect = 0;
 
     m_bPendingChildRedirection = false;
@@ -168,7 +173,9 @@ public:
             m_bPluginsForce = part->d->m_bPluginsForce;
             m_bPluginsOverride = part->d->m_bPluginsOverride;
             // Same for SSL settings
+#if !APPLE_CHANGES
             m_ssl_in_use = part->d->m_ssl_in_use;
+#endif
             m_onlyLocalReferences = part->d->m_onlyLocalReferences;
             m_zoomFactor = part->d->m_zoomFactor;
         }
@@ -242,6 +249,7 @@ public:
   QString m_kjsDefaultStatusBarText;
   QString m_lastModified;
 
+#if !APPLE_CHANGES
   // QStrings for SSL metadata
   // Note: When adding new variables don't forget to update ::saveState()/::restoreState()!
   bool m_ssl_in_use;
@@ -254,6 +262,7 @@ public:
           m_ssl_cipher_used_bits,
           m_ssl_cipher_bits,
           m_ssl_cert_state;
+#endif
 
   bool m_bComplete:1;
   bool m_bLoadEventEmitted:1;
@@ -268,9 +277,13 @@ public:
   KIO::CacheControl m_cachePolicy;
   QTimer m_redirectionTimer;
   QTime m_parsetime;
+
+  RedirectionScheduled m_scheduledRedirection;
   double m_delayRedirect;
   QString m_redirectURL;
+  int m_scheduledHistoryNavigationSteps;
 
+#if !APPLE_CHANGES
   KAction *m_paViewDocument;
   KAction *m_paViewFrame;
   KAction *m_paSaveBackground;
@@ -292,6 +305,7 @@ public:
 
   QString m_popupMenuXML;
   KHTMLPart::GUIProfile m_guiProfile;
+#endif
 
   int m_zoomFactor;
 
@@ -318,13 +332,13 @@ public:
   DOM::Node m_mousePressNode; //node under the mouse when the mouse was pressed (set in the mouse handler)
 
 #if APPLE_CHANGES
-    DOM::Node m_initialSelectionStart;
-    long m_initialSelectionStartOffset;
-    DOM::Node m_initialSelectionEnd;
-    long m_initialSelectionEndOffset;
-    bool m_selectionInitiatedWithDoubleClick:1;
-    bool m_selectionInitiatedWithTripleClick:1;
-    bool m_mouseMovedSinceLastMousePress;
+  DOM::Node m_initialSelectionStart;
+  long m_initialSelectionStartOffset;
+  DOM::Node m_initialSelectionEnd;
+  long m_initialSelectionEndOffset;
+  bool m_selectionInitiatedWithDoubleClick:1;
+  bool m_selectionInitiatedWithTripleClick:1;
+  bool m_mouseMovedSinceLastMousePress:1;
 #endif
   DOM::Node m_selectionStart;
   long m_startOffset;
@@ -355,6 +369,7 @@ public:
   unsigned long m_totalObjectCount;
   unsigned int m_jobPercent;
 
+#if !APPLE_CHANGES
   KHTMLFind *m_findDialog;
 
   struct findState
@@ -367,6 +382,7 @@ public:
   };
 
   findState m_lastFindState;
+#endif
 
   //QGuardedPtr<KParts::Part> m_activeFrame;
   KParts::Part * m_activeFrame;
