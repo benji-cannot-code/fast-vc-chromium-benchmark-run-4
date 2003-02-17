5490d7bb3fa4ec315aefe3caa590fc49a02c4fef
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-02-17 18:53:19 +0000

    2938062 Mouse cursor does not change to link cursor over non-button form submission controls
            Reviewed by Darin
    
            * khtml/khtmlview.cpp:
            (isSubmitImage):  Helper function to ID submit buttons with images.
            (KHTMLView::viewportMouseMoveEvent):  Set cursor for submit buttons with images.
            * kwq/WebCoreBridge.mm:
            (inputElementFromDOMElement):  Simplify previously written casting goop.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3653 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtmlview.cpp
+++ third_party/WebKit/WebCore/khtml/khtmlview.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "html/html_documentimpl.h"
 #include "html/html_inlineimpl.h"
+#include "html/html_formimpl.h"
 #include "rendering/render_object.h"
 #include "rendering/render_root.h"
 #include "rendering/render_style.h"
@@ -540,6 +541,14 @@ void KHTMLView::viewportMouseDoubleClickEvent( QMouseEvent *_mouse )
 #endif    
 }
 
+static bool isSubmitImage(DOM::NodeImpl *node)
+{
+    return node->isHTMLElement()
+//        && static_cast<HTMLElementImpl*>(node)->id() == ID_INPUT
+        && node->id() == ID_INPUT
+        && static_cast<HTMLInputElementImpl*>(node)->inputType() == HTMLInputElementImpl::IMAGE;
+}
+
 void KHTMLView::viewportMouseMoveEvent( QMouseEvent * _mouse )
 {
     if(!m_part->xmlDocImpl()) return;
@@ -585,7 +594,8 @@ void KHTMLView::viewportMouseMoveEvent( QMouseEvent * _mouse )
         if ( d->mousePressed )
             // during selection, use an IBeam no matter what we're over
             c = KCursor::ibeamCursor();
-        else if ( mev.url.length() && m_part->settings()->changeCursor() )
+        else if ( (mev.url.length() || isSubmitImage(mev.innerNode.handle()))
+                  && m_part->settings()->changeCursor() )
             c = m_part->urlCursor();
         else if ( mev.innerNode.nodeType() == Node::TEXT_NODE
                   || mev.innerNode.nodeType() == Node::CDATA_SECTION_NODE )
