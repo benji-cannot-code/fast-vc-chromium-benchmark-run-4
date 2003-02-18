95a1ec963d83bbcfaab7169a1a4527a0c5720d9a
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-02-18 00:13:03 +0000

    Reviewed by Dave.
            - fixed 3174332 -- REGRESSION: bad initial layout due to empty QFontMetrics
    
            * khtml/rendering/render_html.cpp: (RenderHtml::setStyle):
            Don't set the style's to display:BLOCK if it's display:NONE. This prevents
            us from modifying the special FOUC style among other things.
    
            - unrelated code cleanup
    
            * khtml/html/html_baseimpl.cpp: Moved code around that was grouped badly.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3659 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_baseimpl.cpp
+++ third_party/WebKit/WebCore/khtml/html/html_baseimpl.cpp
@@ -583,23 +583,6 @@ NodeImpl::Id HTMLHeadElementImpl::id() const
     return ID_HEAD;
 }
 
-bool HTMLHtmlElementImpl::rendererIsNeeded(RenderStyle *style)
-{
-    // Ignore display: none.
-    return true;
-}
-
-RenderObject *HTMLHtmlElementImpl::createRenderer(RenderArena *arena, RenderStyle *style)
-{
-    return new (arena) RenderHtml(this);
-}
-
-void HTMLHtmlElementImpl::attach()
-{
-    createRendererIfNeeded();
-    NodeBaseImpl::attach();
-}
-
 // -------------------------------------------------------------------------
 
 HTMLHtmlElementImpl::HTMLHtmlElementImpl(DocumentPtr *doc)
@@ -616,6 +599,22 @@ NodeImpl::Id HTMLHtmlElementImpl::id() const
     return ID_HTML;
 }
 
+bool HTMLHtmlElementImpl::rendererIsNeeded(RenderStyle *style)
+{
+    // Ignore display: none.
+    return true;
+}
+
+RenderObject *HTMLHtmlElementImpl::createRenderer(RenderArena *arena, RenderStyle *style)
+{
+    return new (arena) RenderHtml(this);
+}
+
+void HTMLHtmlElementImpl::attach()
+{
+    createRendererIfNeeded();
+    NodeBaseImpl::attach();
+}
 
 // -------------------------------------------------------------------------
 
