7380f65096281da97c32412e35f4b2afe067c6ac
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-02-19 03:02:32 +0000

    WebCore:
    	Reviewed by Chris.
    
    	Merged changes from Safari-58-1-branch.
    
        2003-02-11  Maciej Stachowiak  <mjs@apple.com>
    
            Reviewed by Trey.
    
            * WebCore.pbproj/project.pbxproj: Include /System/Library/PrivateFrameworks.
    
    WebKit:
    
    	Reviewed by Chris.
    
    	Merged changes from Safari-58-1-branch.
    
        2003-02-11  Richard Williamson   <rjw@apple.com>
    
            Add import of CGFontCache.h, which is no longer
            included by CoreGraphicsPrivate.h.
    
            Reviewed by Vicki.
    
            * WebCoreSupport.subproj/WebTextRendererFactory.m:
    
        2003-02-11  Maciej Stachowiak  <mjs@apple.com>
    
            Reviewed by Darin.
    
            * WebCoreSupport.subproj/WebTextRenderer.m: Change (void **) cast
    	to (void *) for compatibility with the latest Panther.
    
        2003-02-11  Maciej Stachowiak  <mjs@apple.com>
    
            Reviewed by Trey.
    
            * WebKit.pbproj/project.pbxproj: Add /System/Library/PrivateFrameworks
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3671 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRendererFactory.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRendererFactory.m
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <CoreGraphics/CoreGraphicsPrivate.h>
 #import <CoreGraphics/CGFontLCDSupport.h>
+#import <CoreGraphics/CGFontCache.h>
 
 #import <mach-o/dyld.h>
 
