c2665396f3078ada7086bdcc904eada25048a5d4
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-02-20 22:04:08 +0000

    Just a comment marking a bug I found.  3178518.
    git-svn-id: svn://svn.chromium.org/blink/trunk@3679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextField.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextField.mm
@@ -307,6 +307,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)fieldEditorWillBecomeFirstResponder
 {
+    //FIXME:  Note due to 3178518, this code is never called.
     QFocusEvent event(QEvent::FocusIn);
     (const_cast<QObject *>(widget->eventFilterObject()))->eventFilter(widget, &event);
 }
