c32cd24c4be267b3a2951201e7a3104a2274f262
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-02-28 11:15:07 +0000

    WebKit:
            Reviewed by Richard.
    
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge window]): Avoid calling removed call.
            * WebView.subproj/WebView.m: Remove override of window method.
            * WebView.subproj/WebWindowOperationsDelegate.h:
    	Adjust for new API.
    
    WebBrowser:
    
            Reviewed by Richard.
    
    	Minimal implementation of new window operations delegate API.
    
            * BrowserWebController.m:
            (-[BrowserWebController closeWindow]):
            (-[BrowserWebController focusWindow]):
            (-[BrowserWebController unfocusWindow]):
            (-[BrowserWebController firstResponderInWindow]):
            (-[BrowserWebController makeFirstResponderInWindow:]):
            (-[BrowserWebController isResizable]):
            (-[BrowserWebController setResizbale:]):
            (-[BrowserWebController setFrame:]):
            (-[BrowserWebController frame]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3715 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -166,7 +166,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSWindow *)window
 {
     ASSERT(frame != nil);
-    return [[[frame controller] windowOperationsDelegate] window];
+    return [[frame webView] window];
 }
 
 - (WebDataSource *)dataSource
