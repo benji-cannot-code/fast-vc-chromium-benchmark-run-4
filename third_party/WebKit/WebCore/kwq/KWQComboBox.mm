2e42924bb3c788cae1d03d55115437e119f3a22c
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-03 06:48:51 +0000

    Use a KWQPopUpButton in place of NSPopUpButton.  The only addition is that 	it implements the KWQWidgetHolder protocol.  This is needed in order to 	auto set PopUps during autofill.
            Reviewed by Maciej.
    
            * kwq/KWQComboBox.mm:
            (QComboBox::QComboBox):  Make a KWQPopUpButton instead of NSPopUpButton.
            (QComboBox::~QComboBox):  trivial s/KWQPopUpButton/NSPopUpButton/
            (QComboBox::insertItem): ditto
            (QComboBox::sizeHint): ditto
            (QComboBox::baselinePosition): ditto
            (QComboBox::clear): ditto
            (QComboBox::setCurrentItem): ditto
            (QComboBox::currentItem): ditto
            (-[KWQPopUpButtonCell widget]):  Also implement KWQWidgetHolder
            (-[KWQPopUpButton widget]):  Implement KWQWidgetHolder by calling our cell.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQComboBox.mm
+++ third_party/WebKit/WebCore/kwq/KWQComboBox.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "KWQComboBox.h"
 
+#import "KWQView.h"
 #import "KWQKHTMLPart.h"
 #import "WebCoreBridge.h"
 
@@ -59,11 +60,14 @@ using khtml::RenderWidget;
 - (void)action:(id)sender;
 @end
 
-@interface KWQPopUpButtonCell : NSPopUpButtonCell
+@interface KWQPopUpButtonCell : NSPopUpButtonCell <KWQWidgetHolder>
 {
     QWidget *widget;
 }
 - initWithWidget:(QWidget *)widget;
+@end
+
+@interface KWQPopUpButton : NSPopUpButton <KWQWidgetHolder>
 @end
 
 QComboBox::QComboBox()
@@ -71,7 +75,7 @@ QComboBox::QComboBox()
     , _widthGood(false)
     , _activated(this, SIGNAL(activated(int)))
 {
-    NSPopUpButton *button = [[NSPopUpButton alloc] init];
+    KWQPopUpButton *button = [[KWQPopUpButton alloc] init];
     
     KWQPopUpButtonCell *cell = [[KWQPopUpButtonCell alloc] initWithWidget:this];
     [button setCell:cell];
@@ -90,14 +94,14 @@ QComboBox::QComboBox()
 
 QComboBox::~QComboBox()
 {
-    NSPopUpButton *button = (NSPopUpButton *)getView();
+    KWQPopUpButton *button = (KWQPopUpButton *)getView();
     [button setTarget:nil];
     [_adapter release];
 }
 
 void QComboBox::insertItem(const QString &text, int index)
 {
-    NSPopUpButton *button = (NSPopUpButton *)getView();
+    KWQPopUpButton *button = (KWQPopUpButton *)getView();
     int numItems = [button numberOfItems];
     if (index < 0) {
         index = numItems;
@@ -115,7 +119,7 @@ void QComboBox::insertItem(const QString &text, int index)
 
 QSize QComboBox::sizeHint() const 
 {
-    NSPopUpButton *button = (NSPopUpButton *)getView();
+    KWQPopUpButton *button = (KWQPopUpButton *)getView();
     
     float width;
     if (_widthGood) {
@@ -158,27 +162,27 @@ void QComboBox::setFrameGeometry(const QRect &r)
 int QComboBox::baselinePosition() const
 {
     // Menu text is at the top.
-    NSPopUpButton *button = (NSButton *)getView();
+    KWQPopUpButton *button = (KWQPopUpButton *)getView();
     return (int)ceil(-TOP_MARGIN + VERTICAL_FUDGE_FACTOR
         + TEXT_VERTICAL_NUDGE + [[button font] ascender]);
 }
 
 void QComboBox::clear()
 {
-    NSPopUpButton *button = (NSPopUpButton *)getView();
+    KWQPopUpButton *button = (KWQPopUpButton *)getView();
     [button removeAllItems];
     _widthGood = false;
 }
 
 void QComboBox::setCurrentItem(int index)
 {
-    NSPopUpButton *button = (NSPopUpButton *)getView();
+    KWQPopUpButton *button = (KWQPopUpButton *)getView();
     [button selectItemAtIndex:index];
 }
 
 int QComboBox::currentItem() const
 {
-    NSPopUpButton *button = (NSPopUpButton *)getView();
+    KWQPopUpButton *button = (KWQPopUpButton *)getView();
     return [button indexOfSelectedItem];
 }
 
@@ -238,6 +242,20 @@ int QComboBox::currentItem() const
     cellFrame.origin.y += TEXT_VERTICAL_NUDGE;
     cellFrame.size.height -= TEXT_VERTICAL_NUDGE;
     [super drawInteriorWithFrame:cellFrame inView:controlView];
+}
+
+- (QWidget *)widget
+{
+    return widget;
+}
+
+@end
+
+@implementation KWQPopUpButton
+
+- (QWidget *)widget
+{
+    return [(KWQPopUpButtonCell *)[self cell] widget];
 }
 
 @end
