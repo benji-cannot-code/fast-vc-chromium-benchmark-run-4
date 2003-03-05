6ba229bb27250d83a6299b1583796c4f60538b34
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-05 01:11:05 +0000

    WebKit:
            Reviewed by Darin
    
            * Misc.subproj/WebKitNSStringExtras.h:
            * Misc.subproj/WebKitNSStringExtras.m:
            (-[NSString _web_widthWithFont:]):
    	(-[NSString _web_drawAtPoint:font:textColor:):
    	removed the unnecessary string parameter from these two
    	NSString methods.
    
    WebBrowser:
    
    	- updated all callers to WebKitNSStringExtras methods that I renamed.
    
    	Reviewed by Darin
    
            * BrowserNSStringExtras.m:
            (-[NSString drawDoubledInRect:withTopColor:bottomColor:font:centered:flipped:]):
    	replace _web_drawString:atPoint:... with _web_drawAtPoint:...
            * DownloadRow.m:
            (-[DownloadRow bestWidth]):
    	replace _web_widthOfString:font:... with _web_widthWithFont:...
            * TabBarView.m:
            (-[TabBarView _leftTruncateWordsFrom:withFont:width:maxWordsToRemove:]):
    	ditto
            * TabButton.m:
            (-[TabButton _updateCellAttributes]):
    	ditto
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3748 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebKitNSStringExtras.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebKitNSStringExtras.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface NSString (WebKitExtras)
 
-- (void)_web_drawString:(NSString *)string atPoint:(NSPoint)point font: (NSFont *)font textColor:(NSColor *)textColor;
+- (void)_web_drawAtPoint:(NSPoint)point font: (NSFont *)font textColor:(NSColor *)textColor;
 
-- (float)_web_widthForString:(NSString *)string font: (NSFont *)font;
+- (float)_web_widthWithFont:(NSFont *)font;
 
 @end
