b06176b02fbd02fbf456131b105decafd2e3c5df
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-05 16:19:44 +0000

    Reviewed by John.
            - fixed 3189442 -- REGRESSION: utf-16-little-endian.html crashes making decoder
    
            * kwq/KWQCharsets.mm: (encodingNameHash): Re-add tolower call to hash function to
            match the equality function.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQCharsets.mm
+++ third_party/WebKit/WebCore/kwq/KWQCharsets.mm
@@ -125,7 +125,7 @@ static CFHashCode encodingNameHash(const void *value)
         if (!c) {
             break;
         }
-        h += c;
+        h += tolower(c);
 	h += (h << 10); 
 	h ^= (h << 6); 
     }
