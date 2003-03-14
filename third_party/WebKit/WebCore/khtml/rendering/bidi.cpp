5acfd8a4e1ca25a0de783cb5cbdcff1acb20e33f
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-14 20:13:23 +0000

    Fix for ew.com, hrweb and arstechnica forums.  A <br> should 	not create a nonempty line in the case where the previous line 	didn't break cleanly.  The fix is to have a new boolean that 	tracks how the previous line broke and to have <br> check that 	to figure out if it should set the line to be non-empty.
            Reviewed by gramps
    
            * khtml/rendering/bidi.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3833 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
@@ -62,6 +62,7 @@ static uint sCurrMidpoint = 0;
 static bool betweenMidpoints = false;
 
 static bool isLineEmpty = true;
+static bool previousLineBrokeAtBR = true;
 static QChar::Direction dir;
 static bool adjustEmbeddding = false;
 static bool emptyRun = true;
@@ -1200,7 +1201,8 @@ static void buildCompactRuns(RenderObject* compactObj)
     
         betweenMidpoints = false;
         isLineEmpty = true;
-    
+        previousLineBrokeAtBR = true;
+        
         end = compactBlock->findNextLineBreak(start);
         if (!isLineEmpty)
             compactBlock->bidiReorderLine(start, end);
@@ -1287,6 +1289,8 @@ void RenderBlock::layoutInlineChildren(bool relayoutChildren)
         sCompactFirstBidiRun = sCompactLastBidiRun = 0;
         sCompactBidiRunCount = 0;
 
+        previousLineBrokeAtBR = true;
+        
         while( !end.atEnd() ) {
             start = end;
             betweenMidpoints = false;
@@ -1434,6 +1438,9 @@ BidiIterator RenderBlock::findNextLineBreak(BidiIterator &start)
     RenderObject *last = o;
     int pos = start.pos;
 
+    bool prevLineBrokeCleanly = previousLineBrokeAtBR;
+    previousLineBrokeAtBR = false;
+    
     while( o ) {
 #ifdef DEBUG_LINEBREAKS
         kdDebug(6041) << "new object "<< o <<" width = " << w <<" tmpw = " << tmpW << endl;
@@ -1445,9 +1452,13 @@ BidiIterator RenderBlock::findNextLineBreak(BidiIterator &start)
              
                 // A <br> always breaks a line, so don't let the line be collapsed
                 // away. Also, the space at the end of a line with a <br> does not
-                // get collapsed away. -dwh
-                isLineEmpty = false;
+                // get collapsed away.  It only does this if the previous line broke
+                // cleanly.  Otherwise the <br> has no effect on whether the line is
+                // empty or not.
+                if (prevLineBrokeCleanly)
+                    isLineEmpty = false;
                 trailingSpaceObject = 0;
+                previousLineBrokeAtBR = true;
                 
                 //check the clear status
                 EClear clear = o->style()->clear();
