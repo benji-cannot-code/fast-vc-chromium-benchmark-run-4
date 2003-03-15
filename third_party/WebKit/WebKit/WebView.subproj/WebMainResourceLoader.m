7adf92a0ad329eac719f1b07058c8b73005177e7
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-15 02:29:36 +0000

    Fixed: 3198961 - REGRESSION: Stopping load of plug-in content is not reflected in UI
            Reviewed by mjs.
    
            * WebView.subproj/WebMainResourceClient.m:
            (-[WebMainResourceClient cancelWithError:]): call receivedError so [dataSource _receivedError:error complete:YES] is called
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebMainResourceLoader.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebMainResourceLoader.m
@@ -86,7 +86,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 -(void)cancelWithError:(WebError *)error
 {
     [self cancelContentPolicy];
-    [super cancelWithError:error];
+    [resource cancel];
+    [self receivedError:error];
 }
 
 - (WebError *)interruptForPolicyChangeError
