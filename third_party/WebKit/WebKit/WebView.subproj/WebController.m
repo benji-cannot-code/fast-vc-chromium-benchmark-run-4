ae0e3b6b806bf05d298c05aba8682d1311ddf75a
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-17 18:20:06 +0000

    Reviewed by Trey and Maciej.
    	- fixed 3199154 -- REGRESSION: world leaks on any page load test
    
            * WebView.subproj/WebController.m: (-[WebController initWithView:frameName:groupName:]):
            Call through to initWithFrame, the designated initializer, not init. Calling [super init] results
            in calling our initWithFrame method, resulting in two calls to the _commonInitialization method.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebController.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebController.m
@@ -130,7 +130,7 @@ NSString *WebElementLinkTitleKey = 		@"WebElementLinkTitle";
 
 - initWithView: (WebFrameView *)view frameName: (NSString *)frameName groupName: (NSString *)groupName;
 {
-    [super init];
+    [super initWithFrame: NSZeroRect];
     [self _commonInitialization: view frameName:frameName groupName:groupName];
     return self;
 }
