02cbc2f0ef145d143502bc2869d462e92807e536
vicki <vicki@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-20 22:18:55 +0000

    don't include WebFoundation.h
            Reviewed by cblu.
    
            * Plugins.subproj/WebBaseNetscapePluginStream.m:
            * Plugins.subproj/WebNetscapePluginRepresentation.m:
            * Plugins.subproj/WebNetscapePluginStream.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3883 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginStream.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginStream.m
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKitLogging.h>
 #import <WebKit/WebNetscapePluginPackage.h>
 
-#import <WebFoundation/WebFoundation.h>
+#import <WebFoundation/WebResponse.h>
 #import <WebFoundation/WebNSFileManagerExtras.h>
 
 @implementation WebBaseNetscapePluginStream
--- third_party/WebKit/WebKit/Plugins.subproj/WebNetscapePluginRepresentation.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebNetscapePluginRepresentation.m
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebNetscapePluginRepresentation.h>
 
 #import <WebFoundation/WebAssertions.h>
-#import <WebFoundation/WebFoundation.h>
+#import <WebFoundation/WebError.h>
 
 @implementation WebNetscapePluginRepresentation
 
