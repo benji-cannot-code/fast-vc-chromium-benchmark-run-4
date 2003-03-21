201756729ab36cb560ef9ae7600fae61437a36a6
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-21 00:15:22 +0000

    WebKit:
    	Properly handle file URL directory errors.
    
            Reviewed by trey.
    
            * Misc.subproj/WebIconDatabase.m:
            (-[WebIconDatabase _iconForFileURL:withSize:]): when file URL has no path, return generic file icon
            * WebView.subproj/WebMainResourceClient.m:
            (-[WebMainResourceClient interruptForPolicyChangeError]): set the URL, don't set it to nil!
    
    WebBrowser:
    
    	Fixed: 3204064 - REGRESSION: PDF URL is left in location for after download
    	Properly handle file URL directory errors.
    
            Reviewed by darin.
    
            * BrowserWebController.m:
            (-[BrowserWebController expectedOrCurrentURL]): don't use the URL of the provisional data source if it is not loading.
            * LocationChangeHandler.m:
            (-[LocationChangeHandler webView:locationChangeDone:forDataSource:]): tweak, use [NSWorkspace isFilePackageAtPath:] instead of LaunchServices because it is easier.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3885 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebMainResourceClient.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebMainResourceClient.m
@@ -84,7 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     return [WebError errorWithCode:WebKitErrorLocationChangeInterruptedByPolicyChange
                           inDomain:WebErrorDomainWebKit
-                        failingURL:nil];
+                        failingURL:[[request URL] absoluteString]];
 }
 
 -(void)stopLoadingForPolicyChange
