71d318904b76d8ee0e9185e3d134ebd907630909
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-23 08:02:47 +0000

    Fix the top of the gamespot.com page on the PLT.  The 	clear status for <br> should be ignored if/when we decide 	to ignore the <br> because the previous line didn't break 	cleanly.
            Reviewed by gramps
    
            * khtml/rendering/bidi.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
@@ -1342,7 +1342,7 @@ void RenderBlock::layoutInlineChildren(bool relayoutChildren)
                 m_firstLine = false;
                 newLine();
             }
-
+             
             sNumMidpoints = 0;
             sCurrMidpoint = 0;
             sCompactFirstBidiRun = sCompactLastBidiRun = 0;
@@ -1461,11 +1461,12 @@ BidiIterator RenderBlock::findNextLineBreak(BidiIterator &start)
                     isLineEmpty = false;
                 trailingSpaceObject = 0;
                 previousLineBrokeAtBR = true;
-                
-                //check the clear status
-                EClear clear = o->style()->clear();
-                if(clear != CNONE) {
-                    m_clearStatus = (EClear) (m_clearStatus | clear);
+
+                if (!isLineEmpty) {
+                    // only check the clear status for non-empty lines.
+                    EClear clear = o->style()->clear();
+                    if(clear != CNONE)
+                        m_clearStatus = (EClear) (m_clearStatus | clear);
                 }
             }
             goto end;
@@ -1687,7 +1688,12 @@ BidiIterator RenderBlock::findNextLineBreak(BidiIterator &start)
         RenderObject* next = Bidinext(start.par, o);
         bool isNormal = o->style()->whiteSpace() == NORMAL;
         bool checkForBreak = isNormal;
-        if (next && o->isText() && next->isText() && !next->isBR()) {
+        if (o->style()->whiteSpace() == NOWRAP && last &&
+            last->style()->whiteSpace() == NORMAL &&
+            w + tmpW > width+1 &&
+            lBreak.obj == last)
+            checkForBreak = true;
+        else if (next && o->isText() && next->isText() && !next->isBR()) {
             if (isNormal || (next->style()->whiteSpace() == NORMAL)) {
                 if (currentCharacterIsSpace)
                     checkForBreak = true;
