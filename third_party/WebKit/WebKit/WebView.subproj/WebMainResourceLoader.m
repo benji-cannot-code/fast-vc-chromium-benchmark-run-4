694f8b27dc6da97f514be4961f18b76a60022406
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-25 22:08:30 +0000

    Reviewed by Richard
    	Fixed a bug that could occur in the new immutable/mutable
    	request scheme. When opening a javascript window, it was
    	possible for a request passed as a method argument to be
    	released during the course of a method, particularly
    	after that request was passed to willSendRequest:.
    	The solution is to ask the data source for its current
    	request rather than using the one stored in the method
    	argument.
    
            * WebView.subproj/WebMainResourceClient.m:
            (-[WebMainResourceClient startLoading:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3917 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebMainResourceLoader.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebMainResourceLoader.m
@@ -273,7 +273,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 	[self resource:resource willSendRequest:r];
 
 	WebResponse *rsp = [[WebResponse alloc] init];
-	[rsp setURL:[r URL]];
+	[rsp setURL:[[[self dataSource] request] URL]];
 	[rsp setContentType:@"text/html"];
 	[rsp setContentLength:0];
 	[self resource:resource didReceiveResponse:rsp];
