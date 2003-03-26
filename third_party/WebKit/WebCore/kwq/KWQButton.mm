2f5efb3ab4514162d9951b2e7fa65463b6c8efaa
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-26 16:46:31 +0000

    Reviewed by Ken.
            - fixed 3208468 -- REGRESSION: checkbox immediately unchecks the first time you check it
    
            The stateChanged signal must be delivered before the mouse up event is handled.
    
            * kwq/KWQButton.mm:
            (-[KWQButton initWithQButton:]): Use [self init] instead of [super init]; no real difference,
            just makes more logical sense.
            (-[KWQButton action:]): Don't send the mouse up event here.
            (-[KWQButton sendConsumedMouseUpIfNeeded]): Added. New method that sends the consumed mouse
            up event.
            (-[KWQButton mouseDown:]): Use a simplified scheme that uses only a single boolean to control
            sending the consumed mouse up event. Call sendConsumedMouseUpIfNeeded to do the deed.
            (QButton::clicked): Added comment to explain why it's important to send the consumed mouse
            up event here. Added a call to the new sendConsumedMouseUpIfNeeded method, replacing the
            code that was formerly in -[KWQButton action:].
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3923 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQButton.mm
+++ third_party/WebKit/WebCore/kwq/KWQButton.mm
@@ -33,44 +33,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface KWQButton : NSButton
 {
     QButton *button;
-    BOOL processingMouseEvent;
-    BOOL clickedDuringMouseEvent;
+    BOOL needToSendConsumedMouseUp;
 }
 
-- initWithQButton:(QButton *)b;
-- (void)action:(id)sender;
+- (id)initWithQButton:(QButton *)b;
+- (void)sendConsumedMouseUpIfNeeded;
+
 @end
 
 @implementation KWQButton
 
 
-- initWithQButton:(QButton *)b
+- (id)initWithQButton:(QButton *)b
 {
     button = b;
-    return [super init];
+    return [self init];
 }
 
 - (void)action:(id)sender
 {
-    if (processingMouseEvent) {
-	clickedDuringMouseEvent = true;
+    button->clicked();
+}
+
+- (void)sendConsumedMouseUpIfNeeded
+{
+    if (needToSendConsumedMouseUp) {
+	needToSendConsumedMouseUp = NO;
 	button->sendConsumedMouseUp();
     } 
-
-    button->clicked();
 }
 
 -(void)mouseDown:(NSEvent *)event
 {
-    processingMouseEvent = true;
+    needToSendConsumedMouseUp = YES;
     [super mouseDown:event];
-    processingMouseEvent = false;
-
-    if (clickedDuringMouseEvent) {
-	clickedDuringMouseEvent = false;
-    } else {
-	button->sendConsumedMouseUp();
-    }
+    [self sendConsumedMouseUpIfNeeded];
 }
 
 
@@ -113,7 +110,13 @@ QString QButton::text() const
 
 void QButton::clicked()
 {
+    // Order of signals is:
+    //   1) signals in subclasses (stateChanged, not sure if there are any others)
+    //   2) mouse up
+    //   3) clicked
+    // Proper behavior of check boxes, at least, depends on this order.
+    
+    KWQButton *button = (KWQButton *)getView();
+    [button sendConsumedMouseUpIfNeeded];
     m_clicked.call();
 }
-
-
