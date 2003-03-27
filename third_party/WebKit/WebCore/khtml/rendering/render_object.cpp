f7a180fe9291e5036de4a17af54ef4e23bab7785
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-27 23:05:52 +0000

    Fix for 3166374.  When a float is removed, crawl up and down the 	tree looking for blocks that contain the float and dirty them all.
            Reviewed by darin
    
            * ChangeLog:
            * khtml/rendering/render_block.cpp:
            * khtml/rendering/render_block.h:
            * khtml/rendering/render_object.cpp:
            (RenderObject::markAllDescendantsWithFloatsForLayout):
            (RenderObject::removeFromObjectLists):
            * khtml/rendering/render_object.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3949 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_object.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_object.cpp
@@ -352,7 +352,7 @@ RenderObject* RenderObject::offsetParent() const
     return curr;
 }
 
-void RenderObject::markAllDescendantsWithFloatsForLayout()
+void RenderObject::markAllDescendantsWithFloatsForLayout(RenderObject*)
 {
 }
 
@@ -1112,11 +1112,15 @@ void RenderObject::invalidateLayout()
 void RenderObject::removeFromObjectLists()
 {
     if (isFloating()) {
-	RenderObject *p;
-	for (p = parent(); p; p = p->parent()) {
-            if (p->isRenderBlock()) 
-		static_cast<RenderBlock*>(p)->removeFloatingObject(this);
+        RenderBlock* outermostBlock = 0;
+        for (RenderObject* p = parent(); p; p = p->parent()) {
+            if (p->isRenderBlock())
+                outermostBlock = static_cast<RenderBlock*>(p);
+            if (!p->isRenderBlock() || !p->containsFloat(this) || p->isFloatingOrPositioned())
+                break;
 	}
+        if (outermostBlock)
+            outermostBlock->markAllDescendantsWithFloatsForLayout(this);
     }
 
     if (isPositioned()) {
