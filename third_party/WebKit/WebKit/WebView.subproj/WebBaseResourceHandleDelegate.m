fbf269ee5384c7dbbf333f1834bdbfc841746766
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-03-28 19:47:31 +0000

    WebFoundation:
            Reviewed by John
    
    	Made SPI out of NSURLConnection code that did not make the API cut.
    
    	Removed timeout interval code from NSURLConnection. This is now
    	a per-request value.
    
            SPI added to NSURLRequest to modify the default per-request
    	timeout interval.
    
            * CacheLoader.subproj/NSURLConnection.h:
            * CacheLoader.subproj/NSURLConnection.m:
            (+[NSURLConnection resourceLoaderRunLoop]):
            (-[NSURLConnection setDefersCallbacks:]):
            (-[NSURLConnection defersCallbacks]):
            (-[NSURLConnection _checkIfTimedOut]):
            * CacheLoader.subproj/NSURLConnectionPrivate.h:
            * CacheLoader.subproj/NSURLRequest.m:
            (-[NSURLRequest initWithURL:]):
            (-[NSURLRequest timeoutInterval]):
            (+[NSURLRequest setDefaultTimeoutInterval:]):
            (+[NSURLRequest defaultTimeoutInterval]):
            * CacheLoader.subproj/NSURLRequestPrivate.h:
            * CookieManager.subproj/WebCookieManager.m:
            * ProtocolHandlers.subproj/WebFTPProtocol.m:
            * ProtocolHandlers.subproj/WebSSLProxyWrapperStream.m:
            * ProtocolHandlers.subproj/WebSimpleHTTPProtocol.m:
    
    WebKit:
    
            Reviewed by John
    
    	Imports NSURLConnection private header to access NSURLConnection code
    	that did not make the API cut.
    
            * WebView.subproj/WebBaseResourceHandleDelegate.m:
            * WebView.subproj/WebMainResourceClient.m:
    
    WebBrowser:
    
            Reviewed by John
    
    	There is now a per-request timeout for NSURLRequest instead
    	of one default on the NSURLConnection class. Error string
    	code updated to account for this.
    
    	PageLoadTestRunner now uses SPI added to NSURLRequest to
    	modify the default timeout interval.
    
            * LocationChangeError.m:
            (-[LocationChangeHandler specializedMessageForError:]):
            * Test/PageLoadTestRunner.m:
            (-[PageLoadTestRunner startTest]):
            (-[PageLoadTestRunner stop]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@3956 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebBaseResourceHandleDelegate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebBaseResourceHandleDelegate.m
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebFoundation/WebError.h>
 
 #import <WebFoundation/NSURLConnection.h>
+#import <WebFoundation/NSURLConnectionPrivate.h>
 #import <WebFoundation/NSURLRequest.h>
 #import <WebFoundation/NSURLRequestPrivate.h>
 #import <WebFoundation/NSURLResponse.h>
--- third_party/WebKit/WebKit/WebView.subproj/WebMainResourceLoader.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebMainResourceLoader.m
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebFoundation/WebFileTypeMappings.h>
 #import <WebFoundation/WebNSURLExtras.h>
 #import <WebFoundation/NSURLConnection.h>
+#import <WebFoundation/NSURLConnectionPrivate.h>
 #import <WebFoundation/NSURLRequest.h>
 #import <WebFoundation/NSURLRequestPrivate.h>
 #import <WebFoundation/NSURLResponse.h>
