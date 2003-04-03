f2403a57d966b66cafd1b33fec5e26ab92df9b40
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-03 00:37:09 +0000

    Fix for 3200447.  Use class_pose to pose so as to avoid         indirect invocation of appkit class initializers.
            Reviewed by darin.
    
            * WebView.subproj/WebHTMLViewPrivate.m:
            (+[WebHTMLView load]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4003 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewPrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewPrivate.m
@@ -32,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebViewPrivate.h>
 #import <WebKit/WebWindowOperationsDelegate.h>
 
+// Imported for direct call to class_poseAs.  Should be removed
+// if we ever drop posing hacks.
+#import <objc/objc-class.h>
 
 // These are a little larger than typical because dragging links is a fairly
 // advanced feature that can confuse non-power-users
@@ -108,9 +111,14 @@ static BOOL forceRealHitTest = NO;
 + (void)load
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    [[WebNSView class] poseAsClass:[NSView class]];
-    [[WebNSTextView class] poseAsClass:[NSTextView class]];
-    [[WebNSWindow class] poseAsClass:[NSWindow class]];
+    
+    // Avoid indirect invocation of any class initializers.  This is a work-around to prevent
+    // the +initializers being called before the REQUIRED AppKit initialization
+    // that's done in +[NSApplication load].
+    class_poseAs(NSClassFromString(@"WebNSView"), NSClassFromString(@"NSView"));
+    class_poseAs(NSClassFromString(@"WebNSTextView"), NSClassFromString(@"NSTextView"));
+    class_poseAs(NSClassFromString(@"WebNSWindow"), NSClassFromString(@"NSWindow"));
+
     [pool release];
 }
 
