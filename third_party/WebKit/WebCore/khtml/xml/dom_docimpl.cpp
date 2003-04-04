aab5283d362419e836a108c7b983da2650d6ffa4
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-04 21:31:40 +0000

    Reviewed by John and Richard.
    	- fixed 3218725 -- REGRESSION: reproducible crash in recalcStyle on reload
    
            * khtml/xml/dom_docimpl.cpp: (DocumentImpl::DocumentImpl): Initialize m_paintDevice to 0.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_docimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_docimpl.cpp
@@ -238,6 +238,7 @@ DocumentImpl::DocumentImpl(DOMImplementationImpl *_implementation, KHTMLView *v)
 {
     document->doc = this;
 
+    m_paintDevice = 0;
     m_paintDeviceMetrics = 0;
 
     m_view = v;
