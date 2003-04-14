8f5b5c19863268f7b028e700fb5207c1813a1dda
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-14 18:03:41 +0000

    Fixed: 3162338 - Embedding SVG with <object type="image/svg+xml"> doesn't work
            Reviewed by dave.
    
            * WebCoreSupport.subproj/WebImageRendererFactory.m:
            (-[WebImageRendererFactory supportedMIMETypes]): code moved from +[WebImageView supportedImageMIMETypes]
            * WebView.subproj/WebImageView.m:
            (+[WebImageView supportedImageMIMETypes]): return -[WebImageRendererFactory supportedMIMETypes]
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRendererFactory.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRendererFactory.m
@@ -23,11 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
-#import <Cocoa/Cocoa.h>
 
 #import <WebKit/WebImageRendererFactory.h>
 #import <WebKit/WebImageRenderer.h>
+
 #import <WebFoundation/WebAssertions.h>
+#import <WebFoundation/WebFileTypeMappings.h>
 
 @implementation WebImageRendererFactory
 
@@ -79,6 +80,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     WebImageRenderer *imageRenderer = [[[WebImageRenderer alloc] initWithSize:s] autorelease];
     [imageRenderer setScalesWhenResized:NO];
     return imageRenderer;
+}
+
+- (NSArray *)supportedMIMETypes
+{
+    static NSArray *imageMIMETypes = nil;
+
+    if(!imageMIMETypes){
+        NSArray *unsupportedTypes = [NSArray arrayWithObjects:
+            @"application/pdf",
+            @"application/postscript",
+            nil];
+        
+        NSEnumerator *enumerator = [[NSImage imageFileTypes] objectEnumerator];
+        WebFileTypeMappings *mappings = [WebFileTypeMappings sharedMappings];
+        NSMutableSet *mimes = [NSMutableSet set];
+        NSString *type;
+
+        while ((type = [enumerator nextObject]) != nil) {
+            NSString *mime = [mappings MIMETypeForExtension:type];
+            if(mime && ![mime isEqualToString:@"application/octet-stream"] &&
+               ![unsupportedTypes containsObject:mime]){
+                [mimes addObject:mime];
+            }
+        }
+
+        imageMIMETypes = [[mimes allObjects] retain];
+    }
+
+    return imageMIMETypes;
 }
 
 
