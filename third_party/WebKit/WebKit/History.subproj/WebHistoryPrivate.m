7bf71dcfa748adefc98e35192a378e70319060dd
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-18 00:43:58 +0000

    - fixed 3232332 -- History file should be versioned since 	we might change it in the future 	- fixed 3220355 -- Console error message at launch when 	there's no history file
    	Note: a downside of this change is that the history formats
    	before and after this change are not compatible. You will
    	get no history each time you cross that boundary by running
    	different Safaris.
    
            Reviewed by Maciej.
    
            * History.subproj/WebHistoryPrivate.m:
            (-[WebHistoryPrivate _loadHistoryGuts:]):
    	check for file-doesn't-exist case before complaining about
    	being unable to read existing file; expect to read dictionary
    	rather than array, and check version in dictionary.
            (-[WebHistoryPrivate _saveHistoryGuts:]):
    	save dictionary that includes version as well as array of
    	items by date.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4137 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebHistoryPrivate.m
+++ third_party/WebKit/WebKit/History.subproj/WebHistoryPrivate.m
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebFoundation/WebNSCalendarDateExtras.h>
 #import <WebFoundation/WebNSURLExtras.h>
 
+NSString *FileVersionKey = @"WebHistoryFileVersion";
+NSString *DatesArrayKey = @"WebHistoryDates";
+
+#define currentFileVersion	1
 
 @implementation WebHistoryPrivate
 
@@ -345,9 +349,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (BOOL)_loadHistoryGuts: (int *)numberOfItemsLoaded
 {
-    NSArray *array;
     NSEnumerator *enumerator;
-    NSDictionary *dictionary;
     int index;
     int limit;
     NSCalendarDate *ageLimitDate;
@@ -355,12 +357,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     *numberOfItemsLoaded = 0;
 
-    array = [NSArray arrayWithContentsOfURL: [self URL]];
-    if (array == nil) {
-        ERROR("attempt to read history from %@ failed; perhaps contents are corrupted", [[self URL] absoluteString]);
+    NSDictionary *fileAsDictionary = [NSDictionary dictionaryWithContentsOfURL: [self URL]];
+    if (fileAsDictionary == nil) {
+#if !ERROR_DISABLED
+        if ([[self URL] isFileURL] && [[NSFileManager defaultManager] fileExistsAtPath: [[self URL] path]]) {
+            ERROR("unable to read history from file %@; perhaps contents are corrupted", [[self URL] path]);
+        }
+#endif
         return NO;
     }
 
+    NSNumber *fileVersionObject = [fileAsDictionary objectForKey:FileVersionKey];
+    int fileVersion;
+    // we don't trust data read from disk, so double-check
+    if (fileVersionObject != nil && [fileVersionObject isKindOfClass:[NSNumber class]]) {
+        fileVersion = [fileVersionObject intValue];
+    } else {
+        ERROR("history file version can't be determined, therefore not loading");
+        return NO;
+    }
+    if (fileVersion > currentFileVersion) {
+        ERROR("history file version is %d, newer than newest known version %d, therefore not loading", fileVersion, currentFileVersion);
+        return NO;
+    }    
+
+    NSArray *array = [fileAsDictionary objectForKey:DatesArrayKey];
+        
     limit = [[NSUserDefaults standardUserDefaults] integerForKey: @"WebKitHistoryItemLimit"];
     ageLimitDate = [self _ageLimitDate];
     index = 0;
@@ -368,10 +390,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     enumerator = [array reverseObjectEnumerator];
     ageLimitPassed = NO;
 
-    while ((dictionary = [enumerator nextObject]) != nil) {
+    NSDictionary *itemAsDictionary;
+    while ((itemAsDictionary = [enumerator nextObject]) != nil) {
         WebHistoryItem *entry;
 
-        entry = [[[WebHistoryItem alloc] initFromDictionaryRepresentation: dictionary] autorelease];
+        entry = [[[WebHistoryItem alloc] initFromDictionaryRepresentation:itemAsDictionary] autorelease];
 
         if ([entry URL] == nil || [entry lastVisitedDate] == nil) {
             // entry without URL is useless; data on disk must have been bad; ignore this one
@@ -418,12 +441,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (BOOL)_saveHistoryGuts: (int *)numberOfItemsSaved
 {
-    NSArray *array;
     *numberOfItemsSaved = 0;
 
-    array = [self arrayRepresentation];
-    if (![array writeToURL:[self URL] atomically:YES]) {
-        ERROR("attempt to save %@ to %@ failed", array, [[self URL] absoluteString]);
+    NSArray *array = [self arrayRepresentation];
+    NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+        array, DatesArrayKey,
+        [NSNumber numberWithInt:currentFileVersion], FileVersionKey,
+        nil];
+    if (![dictionary writeToURL:[self URL] atomically:YES]) {
+        ERROR("attempt to save %@ to %@ failed", dictionary, [[self URL] absoluteString]);
         return NO;
     }
     
