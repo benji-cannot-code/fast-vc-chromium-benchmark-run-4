0c3a173521989ae5ba0d7ae54c47b8251ef72dfa
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-22 02:12:31 +0000

    WebFoundation:
            Reviewed by Richard.
    
    	Renamed headers and implementation files for new auth API.
    
            * AuthenticationManager.subproj/NSURLAuthenticationChallenge.h:
            * AuthenticationManager.subproj/NSURLAuthenticationChallenge.m:
            * AuthenticationManager.subproj/NSURLCredential.h:
            * AuthenticationManager.subproj/NSURLCredential.m:
            * AuthenticationManager.subproj/NSURLCredentialStorage.h:
            * AuthenticationManager.subproj/NSURLCredentialStorage.m:
            * AuthenticationManager.subproj/NSURLProtectionSpace.h:
            * AuthenticationManager.subproj/NSURLProtectionSpace.m:
            * AuthenticationManager.subproj/WebAuthenticatingResource.h: Removed.
            * AuthenticationManager.subproj/WebAuthenticatingResource.m: Removed.
            * AuthenticationManager.subproj/WebAuthenticationHandler.h:
            * AuthenticationManager.subproj/WebAuthenticationManager.h: Removed.
            * AuthenticationManager.subproj/WebAuthenticationManager.m: Removed.
            * AuthenticationManager.subproj/WebAuthenticationManagerCallback.h:
            * AuthenticationManager.subproj/WebAuthenticationManagerPrivate.h:
            * AuthenticationManager.subproj/WebAuthenticationRequest.h: Removed.
            * AuthenticationManager.subproj/WebAuthenticationRequest.m: Removed.
            * AuthenticationManager.subproj/WebAuthenticationRequestPrivate.h:
            * AuthenticationManager.subproj/WebAuthenticationResult.h: Removed.
            * AuthenticationManager.subproj/WebCredential.h: Removed.
            * AuthenticationManager.subproj/WebCredential.m: Removed.
            * AuthenticationManager.subproj/WebCredentialStorage.h:
            * AuthenticationManager.subproj/WebCredentialStorage.m:
            * AuthenticationManager.subproj/WebKeychainCredential.h:
            * WebFoundation.pbproj/project.pbxproj:
    
    WebKit:
    
            Reviewed by Richard.
    
    	Updated for file renames.
    
            * Panels.subproj/WebAuthenticationPanel.h:
            * Panels.subproj/WebPanelAuthenticationHandler.h:
            * Panels.subproj/WebStandardPanels.m:
    
    WebBrowser:
    
            Reviewed by Richard.
    
    	Updated for file renames.
    
            * AppController.m:
            * FormCompletionController.m:
            * Preferences.subproj/PasswordsEditor.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4149 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Panels.subproj/WebPanelAuthenticationHandler.h
+++ third_party/WebKit/WebKit/Panels.subproj/WebPanelAuthenticationHandler.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #import <Foundation/Foundation.h>
-#import <WebFoundation/WebAuthenticationManager.h>
+#import <WebFoundation/NSURLCredentialStorage.h>
 
 
 @interface WebPanelAuthenticationHandler : NSObject <WebAuthenticationHandler>
--- third_party/WebKit/WebKit/Panels.subproj/WebStandardPanels.m
+++ third_party/WebKit/WebKit/Panels.subproj/WebStandardPanels.m
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebStandardPanelsPrivate.h>
 
 #import <WebFoundation/WebAssertions.h>
-#import <WebFoundation/WebAuthenticationManager.h>
+#import <WebFoundation/NSURLCredentialStorage.h>
 
 #import <Carbon/Carbon.h>
 
