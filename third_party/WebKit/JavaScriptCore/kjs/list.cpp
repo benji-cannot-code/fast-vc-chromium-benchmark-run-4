76c8385b116ef120fa852e8a377600e1e7046e0c
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-23 00:09:32 +0000

    Reviewed by Ken.
    	Improved List pool for 3% speed improvement on cvs-js-ibench
    
            * kjs/list.cpp: Replaced the roving cursor with a free list and
    	raised the high water mark to 384.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/list.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/list.cpp
@@ -29,11 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace KJS {
 
 // tunable parameters
-const int poolSize = 32; // must be a power of 2
+const int poolSize = 384;
 const int inlineValuesSize = 4;
 
-// derived constants
-const int poolSizeMask = poolSize - 1;
 
 enum ListImpState { unusedInPool = 0, usedInPool, usedOnHeap, immortal };
 
@@ -44,13 +42,16 @@ struct ListImp : ListImpBase
     int capacity;
     ValueImp **overflow;
 
+    ListImp *nextInFreeList;
+
 #if DUMP_STATISTICS
     int sizeHighWaterMark;
 #endif
 };
 
 static ListImp pool[poolSize];
-static int poolCursor;
+static ListImp *poolFreeList;
+static int poolUsed;
 
 #if DUMP_STATISTICS
 
@@ -89,18 +90,13 @@ ListStatisticsExitLogger::~ListStatisticsExitLogger()
 static inline ListImp *allocateListImp()
 {
     // Find a free one in the pool.
-    int c = poolCursor;
-    int i = c;
-    do {
-        ListImp *imp = &pool[i];
-        ListImpState s = imp->state;
-        i = (i + 1) & poolSizeMask;
-        if (s == unusedInPool) {
-            poolCursor = i;
-            imp->state = usedInPool;
-            return imp;
-        }
-    } while (i != c);
+    if (poolUsed < poolSize) {
+	ListImp *imp = poolFreeList ? poolFreeList : &pool[0];
+	poolFreeList = imp->nextInFreeList ? imp->nextInFreeList : imp + 1;
+	imp->state = usedInPool;
+	poolUsed++;
+	return imp;
+    }
     
     ListImp *imp = new ListImp;
     imp->state = usedOnHeap;
@@ -109,10 +105,14 @@ static inline ListImp *allocateListImp()
 
 static inline void deallocateListImp(ListImp *imp)
 {
-    if (imp->state == usedInPool)
+    if (imp->state == usedInPool) {
         imp->state = unusedInPool;
-    else
+	imp->nextInFreeList = poolFreeList;
+	poolFreeList = imp;
+	poolUsed--;
+    } else {
         delete imp;
+    }
 }
 
 List::List() : _impBase(allocateListImp()), _needsMarking(false)
