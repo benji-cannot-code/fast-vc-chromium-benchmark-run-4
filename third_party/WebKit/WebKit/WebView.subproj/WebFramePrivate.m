71aa5b262270dd9c8073b36e93e452be199e42d8
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-23 18:03:37 +0000

    WebKit:
    	Renamed the policy delegate methods.
    
            Reviewed by john.
    
            * WebView.subproj/WebDefaultPolicyDelegate.m:
            (-[WebDefaultPolicyDelegate webView:unableToImplementPolicyWithError:frame:]):
            (-[WebDefaultPolicyDelegate webView:decidePolicyForNavigationAction:request:frame:decisionListener:]):
            (-[WebDefaultPolicyDelegate webView:decidePolicyForNewWindowAction:request:newFrameName:decisionListener:]):
            * WebView.subproj/WebFramePrivate.m:
            (-[WebFrame _handleUnimplementablePolicyWithErrorCode:forURL:]):
            (-[WebFrame _checkNewWindowPolicyForRequest:action:frameName:formState:andCall:withSelector:]):
            (-[WebFrame _checkNavigationPolicyForRequest:dataSource:formState:andCall:withSelector:]):
            * WebView.subproj/WebMainResourceClient.m:
            (-[WebMainResourceClient checkContentPolicyForResponse:]):
            * WebView.subproj/WebPolicyDelegate.h:
    
    WebBrowser:
    
    	Updated WebBrowser to the renamed WebPolicyDelegate methods.
    
            Reviewed by john.
    
            * BrowserWebController.m:
            (-[BrowserWebController webView:unableToImplementPolicyWithError:frame:]):
            (-[BrowserWebController openURLExternallyWithRequest:inFrame:]):
            (-[BrowserWebController decidePolicyForAction:request:frame:newFrameName:decisionListener:]):
            (-[BrowserWebController webView:decidePolicyForNavigationAction:request:frame:decisionListener:]):
            (-[BrowserWebController webView:decidePolicyForNewWindowAction:request:newFrameName:decisionListener:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4161 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFramePrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFramePrivate.m
@@ -1043,7 +1043,7 @@ static CFAbsoluteTime _timeOfLastCompletedLoad;
     WebError *error = [WebError _webKitErrorWithCode:code
 				          failingURL:[URL absoluteString]];
     WebView *c = [self webView];
-    [[c _policyDelegateForwarder] webView:c unableToImplementPolicyWithError:error inFrame:self];    
+    [[c _policyDelegateForwarder] webView:c unableToImplementPolicyWithError:error frame:self];    
 }
 
 - (void)_clearProvisionalDataSource
@@ -1383,10 +1383,10 @@ static CFAbsoluteTime _timeOfLastCompletedLoad;
     _private->policyFormState = [formState retain];
 
     WebView *c = [self webView];
-    [[c _policyDelegateForwarder] webView:c decideNewWindowPolicyForAction:action
-						                      andRequest:request
-						                    newFrameName:frameName
-						                decisionListener:listener];
+    [[c _policyDelegateForwarder] webView:c decidePolicyForNewWindowAction:action
+                                                                   request:request
+                                                              newFrameName:frameName
+                                                          decisionListener:listener];
     
     [listener release];
 }
@@ -1455,10 +1455,10 @@ static CFAbsoluteTime _timeOfLastCompletedLoad;
     _private->policyFormState = [formState retain];
 
     WebView *c = [self webView];
-    [[c _policyDelegateForwarder] webView:c decideNavigationPolicyForAction:action
-                                                             andRequest:request
-                                                                inFrame:self
-                                                       decisionListener:listener];
+    [[c _policyDelegateForwarder] webView:c decidePolicyForNavigationAction:action
+                                                                    request:request
+                                                                      frame:self
+                                                           decisionListener:listener];
     
     [listener release];
 }
--- third_party/WebKit/WebKit/WebView.subproj/WebMainResourceClient.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebMainResourceClient.m
@@ -214,10 +214,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     WebView *c = [dataSource _controller];
     [c setDefersCallbacks:YES];
-    [[c _policyDelegateForwarder] webView:c decideContentPolicyForMIMEType:[r MIMEType]
-                                                                   andRequest:[dataSource request]
-                                                                      inFrame:[dataSource webFrame]
-                                                             decisionListener:listener];
+    [[c _policyDelegateForwarder] webView:c decidePolicyForMIMEType:[r MIMEType]
+                                                            request:[dataSource request]
+                                                              frame:[dataSource webFrame]
+                                                   decisionListener:listener];
 }
 
 
