c1d02968d1d97227bcaf330ac3b35c9d1afc9936
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-25 22:07:45 +0000

    WebKit:
    	Don't do "@class WebDataSource" as that class isn't mentioned in this file.
    
            * WebView.subproj/WebFrameLoadDelegate.h:
    
    WebBrowser:
    
            Do "@class WebDataSource" as that class is mentioned in this file.
    
            * LocationChangeError.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4190 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrameLoadDelegate.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrameLoadDelegate.h
@@ -6,12 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         Public header file.
 */
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
-@class WebView;
-@class WebDataSource;
 @class NSError;
 @class WebFrame;
+@class WebView;
 
 /*!
     @category WebFrameLoadDelegate
