165f2ab7958f23c652368dfbc163aeb3fb7fbe28
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-25 00:24:42 +0000

    WebFoundation:
            Reviewed by Chris.
    
    	Update Challenge API to mostly match proposal, except for the
    	parts that can't be done until auth handling is moved from the
    	authentication handler to the connection delegate.
    
            * AuthenticationManager.subproj/NSURLAuthenticationChallenge.h:
            * AuthenticationManager.subproj/NSURLAuthenticationChallenge.m:
            (-[NSURLAuthenticationChallengePrivate initWithProtectionSpace:proposedCredential:previousFailureCount:failureResponse:error:delegate:]):
            (-[NSURLAuthenticationChallengePrivate dealloc]):
            (-[NSURLAuthenticationChallenge _initWithProtectionSpace:proposedCredential:previousFailureCount:failureResponse:error:delegate:]):
            (-[NSURLAuthenticationChallenge proposedCredential]):
            (-[NSURLAuthenticationChallenge cancel]):
            (-[NSURLAuthenticationChallenge error]):
            (-[NSURLAuthenticationChallenge failureResponse]):
            * AuthenticationManager.subproj/NSURLCredentialStorage.m:
            (-[NSURLCredentialStorage _invokeHandlersWithURL:realm:forRequest:failureCount:failureResponse:withCallback:context:]):
            (-[NSURLCredentialStorage _invokeHandlersWithURL:proxy:forRequest:failureCount:failureResponse:withCallback:context:]):
            (-[NSURLCredentialStorage _addCredentialsToRetryHTTPRequest:afterFailureResponse:nsFailureResponse:failureCount:withCallback:context:]):
            * AuthenticationManager.subproj/WebAuthenticationHandlerDispatcher.m:
            (-[WebAuthenticationHandlerDispatcher startAuthentication:]):
            * AuthenticationManager.subproj/WebAuthenticationManagerPrivate.h:
            * AuthenticationManager.subproj/WebAuthenticationRequestPrivate.h:
            * AuthenticationManager.subproj/WebCredentialStorage.m:
            (-[WebCredentialStorage setDefaultCredential:forProtectionSpace:]):
            * ProtocolHandlers.subproj/WebCFNetworkHTTPProtocol.m:
            (-[WebCFNetworkHTTPProtocol addAuthenticationFieldsToHeader]):
    
    WebKit:
    
            Reviewed by Chris.
    
            * Panels.subproj/WebAuthenticationPanel.m:
            (-[WebAuthenticationPanel setUpForChallenge:]): Update for new Challenge API.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4179 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Panels.subproj/WebAuthenticationPanel.m
+++ third_party/WebKit/WebKit/Panels.subproj/WebAuthenticationPanel.m
@@ -105,7 +105,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSString *realm = [space realm];
     NSString *message;
 
-    if ([chall previousFailureCount] == 0) {
+    if ([challenge previousFailureCount] == 0) {
         if ([space isProxy]) {
             message = [NSString stringWithFormat:UI_STRING("To view this page, you need to log in to the %@ proxy server %@.",
                                                            "prompt string in authentication panel"),
@@ -140,8 +140,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 "message in authentication panel")];
     }
 
-    if ([chall defaultUsername] != nil) {
-        [username setStringValue:[chall defaultUsername]];
+    if ([[challenge proposedCredential] user] != nil) {
+        [username setStringValue:[[challenge proposedCredential] user]];
         [panel setInitialFirstResponder:password];
     } else {
         [username setStringValue:@""];
