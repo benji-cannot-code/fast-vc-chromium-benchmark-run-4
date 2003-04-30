ed723329babe4de75c2d5505c752c3be46c7dafd
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-30 22:41:35 +0000

    Tests:
            Reviewed by Richard
    
    	Changed cookie-related constants and enums to
    	the API-approved names. Also did some text
    	search and replace in comments to catch usages
    	of now-obsolete names.
    
            * CookieManager/wkcookie-test.m:
            (main):
            * CookieManager/wkcookiemanager-test.m:
            (main):
    
    WebFoundation:
    
            Reviewed by Richard
    
            Changed cookie-related constants and enums to
            the API-approved names. Also did some text
            search and replace in comments to catch usages
            of now-obsolete names.
    
            * CookieManager.subproj/NSHTTPCookie.h:
            * CookieManager.subproj/NSHTTPCookie.m:
            (-[NSHTTPCookie initWithDictionary:]):
            (-[NSHTTPCookie init]):
            (-[NSHTTPCookie dictionaryRepresentation]):
            (+[NSHTTPCookie cookieWithV0Spec:forURL:locationHeader:]):
            (-[NSHTTPCookie description]):
            * CookieManager.subproj/NSHTTPCookiePrivate.h:
            * CookieManager.subproj/NSHTTPCookieStorage.h:
            * CookieManager.subproj/NSHTTPCookieStorage.m:
            (-[NSHTTPCookieStorage _readAcceptCookiesPreference]):
            (-[NSHTTPCookieStorage _setCookies:forURL:policyBaseURL:]):
            (-[NSHTTPCookieStorage init]):
            (-[NSHTTPCookieStorage acceptPolicy]):
            (-[NSHTTPCookieStorage setAcceptPolicy:]):
            (-[NSHTTPCookieStorage description]):
            * English.lproj/StringsNotToBeLocalized.txt:
            * WebFoundation-base.exp:
    
    WebKit:
    
            Reviewed by Richard
    
            Changed cookie-related constants and enums to
            the API-approved names. Also did some text
            search and replace in comments to catch usages
            of now-obsolete names.
    
            * WebCoreSupport.subproj/WebCookieAdapter.m:
            (-[WebCookieAdapter cookiesEnabled]):
    
    WebBrowser:
    
            Reviewed by Richard
    
            Changed cookie-related constants and enums to
            the API-approved names. Also did some text
            search and replace in comments to catch usages
            of now-obsolete names.
    
            * English.lproj/Localizable.strings:
            * Preferences.subproj/SecurityPreferences.m:
            (-[SecurityPreferences takeCookiePolicyFrom:]):
            (-[SecurityPreferences updateCookiesViews]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4224 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebCookieAdapter.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebCookieAdapter.m
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     BOOL result;
 
-    WebCookieAcceptPolicy acceptPolicy = [[NSHTTPCookieStorage sharedCookieManager] acceptPolicy];
-    result = (acceptPolicy == WebCookieAcceptPolicyAlways || acceptPolicy == WebCookieAcceptPolicyOnlyFromMainDocumentDomain);
+    NSHTTPCookieAcceptPolicy acceptPolicy = [[NSHTTPCookieStorage sharedCookieManager] acceptPolicy];
+    result = (acceptPolicy == NSHTTPCookieAcceptPolicyAlways || acceptPolicy == NSHTTPCookieAcceptPolicyOnlyFromMainDocumentDomain);
     
     return result;
 }
