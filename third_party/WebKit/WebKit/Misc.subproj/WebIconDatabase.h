af87d94bc0443f6aa59acc4f1d59fa4c6d6fb305
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-30 22:20:26 +0000

    WebKit:
            FIXED: Clients of WebKit should have separate icon DB's
    
            Reviewed by rjw.
    
            * English.lproj/StringsNotToBeLocalized.txt:
            * Misc.subproj/WebIconDatabase.h:
            * Misc.subproj/WebIconDatabase.m:
            (-[WebIconDatabase _createFileDatabase]): If the icon db hasn't been set using the defaults, create o directory using the bundle id.
            * WebKit.exp:
    
    WebBrowser:
    
    	FIXED: Clients of WebKit should have separate icon DB's
    
            Reviewed by rjw.
    
            * AppController.m:
            (+[AppController initialize]): Moved the icon DB rename code here from WebKit. Set the icon DB path.
            * Defaults.plist: Removed unused "safe MIME types" defaults
            * English.lproj/StringsNotToBeLocalized.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4223 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebIconDatabase.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebIconDatabase.h
@@ -15,6 +15,8 @@ extern NSString *WebIconDatabaseDidAddIconNotification;
 
 extern NSString *WebIconNotificationUserInfoURLKey;
 
+extern NSString *WebIconDatabaseDirectoryDefaultsKey;
+
 extern NSSize WebIconSmallSize;  // 16 x 16
 extern NSSize WebIconMediumSize; // 32 x 32
 extern NSSize WebIconLargeSize;  // 128 x 128
--- third_party/WebKit/WebKit/Misc.subproj/WebIconDatabase.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebIconDatabase.m
@@ -16,21 +16,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebFoundation/WebNSURLExtras.h>
 #import <WebFoundation/WebFileDatabase.h>
 
-#define WebIconDatabaseOldContainingDirectory ([NSString stringWithFormat:@"%@/%@", NSHomeDirectory(), @"Library/Caches/com.apple.WebKit"])
-#define WebIconDatabaseNewContainingDirectory ([NSString stringWithFormat:@"%@/%@", NSHomeDirectory(), @"Library/Caches/WebKit"])
-
-#define WebIconDatabaseDefaultDirectory ([NSString stringWithFormat:@"%@/%@", NSHomeDirectory(), @"Library/Caches/WebKit/Icons"])
-
-NSString * const WebIconDatabaseVersionKey = @"WebIconDatabaseVersion";
-NSString * const WebIconsOnDiskKey = @"WebIconsOnDisk";
-NSString * const WebURLToIconURLKey = @"WebSiteURLToIconURLKey";
-NSString * const WebIconURLToURLsKey = @"WebIconURLToSiteURLs";
-NSString * const WebHostToURLsKey = @"WebHostToSiteURLs";
+NSString * const WebIconDatabaseVersionKey = 	@"WebIconDatabaseVersion";
+NSString * const WebIconsOnDiskKey = 		@"WebIconsOnDisk";
+NSString * const WebURLToIconURLKey = 		@"WebSiteURLToIconURLKey";
+NSString * const WebIconURLToURLsKey = 		@"WebIconURLToSiteURLs";
+NSString * const WebHostToURLsKey = 		@"WebHostToSiteURLs";
 
 static const int WebIconDatabaseCurrentVersion = 1;
 
 NSString *WebIconDatabaseDidAddIconNotification = @"WebIconDatabaseDidAddIconNotification";
-NSString *WebIconNotificationUserInfoURLKey = @"WebIconNotificationUserInfoURLKey";
+NSString *WebIconNotificationUserInfoURLKey =     @"WebIconNotificationUserInfoURLKey";
+
+NSString *WebIconDatabaseDirectoryDefaultsKey =   @"WebIconDatabaseDirectoryDefaultsKey";
 
 NSSize WebIconSmallSize = {16, 16};
 NSSize WebIconMediumSize = {32, 32};
@@ -221,16 +218,12 @@ NSSize WebIconLargeSize = {128, 128};
 
 - (void)_createFileDatabase
 {
-    // Move over the old database, if we have one. If the new database
-    // already exists, this call will fail with ENOTEMPTY, so we don't
-    // need to worry about overwriting it.
-    rename([WebIconDatabaseOldContainingDirectory fileSystemRepresentation], [WebIconDatabaseNewContainingDirectory fileSystemRepresentation]);
-
     // FIXME: Make defaults key public somehow
-    NSString *databaseDirectory = [[NSUserDefaults standardUserDefaults] objectForKey:@"WebIconDatabaseDirectory"];
+    NSString *databaseDirectory = [[NSUserDefaults standardUserDefaults] objectForKey:WebIconDatabaseDirectoryDefaultsKey];
 
     if (!databaseDirectory) {
-        databaseDirectory = WebIconDatabaseDefaultDirectory;
+        NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
+        databaseDirectory = [NSString stringWithFormat:@"%@/Library/Caches/WebKitIcons/%@", NSHomeDirectory(), bundleIdentifier];
     }
 
     _private->fileDatabase = [[WebFileDatabase alloc] initWithPath:databaseDirectory];
