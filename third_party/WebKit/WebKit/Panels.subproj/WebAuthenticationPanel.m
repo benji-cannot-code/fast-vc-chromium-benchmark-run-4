095d92047f4df78aa2e8ecb740ff06704c669056
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-04-30 23:46:21 +0000

    WebFoundation:
            Reviewed by Darin.
    
    	Some simple renames:
    
    	sharedURLCredentialStorage --> sharedCredentialStorage
    	URLCredentialWithUser:password:persistence: --> credentialWithUser:password:persistence:
    
            * AuthenticationManager.subproj/NSHTTPAuthenticator.m:
            (-[NSHTTPAuthenticator _usernameForRealm:URL:]):
            (-[NSHTTPAuthenticator _applyHTTPProxyCredentials:]):
            (-[NSHTTPAuthenticator _applyHTTPCredentials:]):
            (-[NSHTTPAuthenticator _proxyUsernameForURL:]):
            (-[NSHTTPAuthenticator _askDelegateWithURL:realm:forRequest:failureCount:failureResponse:connection:withCallback:context:]):
            (-[NSHTTPAuthenticator _askDelegateWithURL:proxy:forRequest:failureCount:failureResponse:connection:withCallback:context:]):
            (-[NSHTTPAuthenticator NSURLAuthenticationChallenge:useCredential:]):
            (-[NSHTTPAuthenticator _setDefaultUserInfoFromURL:]):
            (-[NSHTTPAuthenticator _removePasswordForRealm:URL:]):
            (-[NSHTTPAuthenticator _removeProxyPasswordForURL:]):
            (-[NSHTTPAuthenticator init]):
            * AuthenticationManager.subproj/NSURLCredential.h:
            * AuthenticationManager.subproj/NSURLCredential.m:
            (+[NSURLCredential credentialWithUser:password:persistence:]):
            * AuthenticationManager.subproj/NSURLCredentialStorage.h:
            * AuthenticationManager.subproj/NSURLCredentialStorage.m:
            (-[NSURLCredentialStorage init]):
            (initSharedCredentialStorage):
            (initCredentialStorage):
            (+[NSURLCredentialStorage sharedCredentialStorage]):
    
    WebKit:
    
            Reviewed by Darin.
    
    	Some simple renames:
    
    	sharedURLCredentialStorage --> sharedCredentialStorage
    	URLCredentialWithUser:password:persistence: --> credentialWithUser:password:persistence:
    
            * Panels.subproj/WebAuthenticationPanel.m:
            (-[WebAuthenticationPanel runAsModalDialogWithChallenge:]):
            (-[WebAuthenticationPanel sheetDidEnd:returnCode:contextInfo:]):
    
    WebBrowser:
    
            Reviewed by Darin.
    
    	Some simple renames:
    
    	sharedURLCredentialStorage --> sharedCredentialStorage
    	URLCredentialWithUser:password:persistence: --> credentialWithUser:password:persistence:
    
            * AppController.m:
            (-[AppController removeAllCredentials]):
            * FormCompletionController.m:
            (+[FormCompletionController _frame:willSubmitLoginForm:withValues:formInfo:submissionListener:]):
            (+[FormCompletionController _passwordSheetDismissed:returnCode:contextInfo:]):
            (+[FormCompletionController _credentialMatchesForString:frame:]):
            * Preferences.subproj/PasswordsEditor.m:
            (-[PasswordsEditor resetCredentials]):
            (-[PasswordsEditor startMonitoringCredentials]):
            (-[PasswordsEditor stopMonitoringCredentials]):
            (-[PasswordsEditor removeSelectedItems:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Panels.subproj/WebAuthenticationPanel.m
+++ third_party/WebKit/WebKit/Panels.subproj/WebAuthenticationPanel.m
@@ -156,7 +156,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSURLCredential *credential = nil;
 
     if ([[NSApplication sharedApplication] runModalForWindow:panel] == 0) {
-        credential = [NSURLCredential URLCredentialWithUser:[username stringValue] password:[password stringValue] persistence:([remember state] == NSOnState) ? NSURLCredentialPersistencePermanent : NSURLCredentialPersistenceForSession];
+        credential = [NSURLCredential credentialWithUser:[username stringValue] password:[password stringValue] persistence:([remember state] == NSOnState) ? NSURLCredentialPersistencePermanent : NSURLCredentialPersistenceForSession];
     }
 
     [callback performSelector:selector withObject:chall withObject:credential];
@@ -183,7 +183,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     ASSERT(challenge != nil);
 
     if (returnCode == 0) {
-        credential = [NSURLCredential URLCredentialWithUser:[username stringValue] password:[password stringValue] persistence:([remember state] == NSOnState) ? NSURLCredentialPersistencePermanent : NSURLCredentialPersistenceForSession];
+        credential = [NSURLCredential credentialWithUser:[username stringValue] password:[password stringValue] persistence:([remember state] == NSOnState) ? NSURLCredentialPersistencePermanent : NSURLCredentialPersistenceForSession];
     }
 
     // We take this tricky approach to nilling out and releasing the challenge
