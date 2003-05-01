8b5aef0909fe38a6649d3a7323690f4ceb07b44c
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-01 16:33:48 +0000

    Tests:
            Reviewed by Darin
    
    	Updated to use approved NSHTTPCookieStorage API.
    	Note that there are some discrepancies in the
    	test results, but they seem to be the result
    	of changes in the way we handle cookie paths,
    	an aspect of cookies I did not change at all
    	with this patch. The difference must have
    	crept in some time ago.
    
            * CookieManager/wkcookie-test.chk:
            * CookieManager/wkcookiemanager-test.chk:
            * CookieManager/wkcookiemanager-test.m:
            (main):
    
    WebFoundation:
    
            Reviewed by Darin
    
            Updated to provide and use approved NSHTTPCookieStorage API.
    
            +sharedCookieManager -> +sharedHTTPCookieStorage
            -setAcceptPolicy: -> -setCookieAcceptPolicy:
            -acceptPolicy -> -cookieAcceptPolicy
    	-cookieRequestHeaderFieldsForURL: -> -cookiesForURL:
    	-setCookiesFromResponseHeader:forURL:policyBaseURL: -> -setCookies:forURL:mainDocumentURL:
    
    	Added back compare: method in NSHTTPCookie. Our tests use it.
    
            * CookieManager.subproj/NSHTTPCookie.m:
            (-[NSHTTPCookie compare:]):
            * CookieManager.subproj/NSHTTPCookieStorage.h:
            * CookieManager.subproj/NSHTTPCookieStorage.m:
            (-[NSHTTPCookieStorage _cookieRequestHeaderFieldsForURL:withExtraCookies:]):
            (-[NSHTTPCookieStorage _cookiesForURL:withExtraCookies:]):
            (-[NSHTTPCookieStorage init]):
            (+[NSHTTPCookieStorage sharedHTTPCookieStorage]):
            (-[NSHTTPCookieStorage cookiesForURL:]):
            (-[NSHTTPCookieStorage setCookies:forURL:mainDocumentURL:]):
            (-[NSHTTPCookieStorage cookieAcceptPolicy]):
            (-[NSHTTPCookieStorage setCookieAcceptPolicy:]):
            * CookieManager.subproj/NSHTTPCookieStoragePrivate.h:
            * ProtocolHandlers.subproj/WebCFNetworkHTTPProtocol.m:
            (-[WebCFNetworkHTTPProtocol addCookieFieldToHeader]):
            (-[WebCFNetworkHTTPProtocol setResponseHeaderUsingHTTPResponse:andCall:context:]):
            * ProtocolHandlers.subproj/WebSimpleHTTPProtocol.m:
            (-[WebSimpleHTTPProtocol buildHTTPRequest]):
            (-[WebSimpleHTTPProtocol _parseHeaderFromData:]):
    
    WebKit:
    
            Reviewed by Darin
    
            Updated to use approved NSHTTPCookieStorage API.
    
            +sharedCookieManager -> +sharedHTTPCookieStorage
            -acceptPolicy -> -cookieAcceptPolicy
            -cookieRequestHeaderFieldsForURL: -> -cookiesForURL:
            -setCookiesFromResponseHeader:forURL:policyBaseURL: -> -setCookies:forURL:mainDocumentURL:
    
            * WebCoreSupport.subproj/WebCookieAdapter.m:
            (-[WebCookieAdapter cookiesEnabled]):
            (-[WebCookieAdapter cookiesForURL:]):
            (-[WebCookieAdapter setCookies:forURL:policyBaseURL:]):
    
    WebBrowser:
    
            Reviewed by Darin
    
    	Updated to use approved NSHTTPCookieStorage API.
    
    	+sharedCookieManager -> +sharedHTTPCookieStorage
    	-setAcceptPolicy: -> -setCookieAcceptPolicy:
    	-acceptPolicy -> -cookieAcceptPolicy
    
            * AppController.m:
            (-[AppController deleteAllCookies]):
            * Preferences.subproj/AcceptedCookies.m:
            (-[AcceptedCookies updateCookieData]):
            (-[AcceptedCookies removeSelectedItems:]):
            (-[AcceptedCookies registerForCookieChangeNotifications]):
            * Preferences.subproj/SecurityPreferences.m:
            (-[SecurityPreferences takeCookiePolicyFrom:]):
            (-[SecurityPreferences updateCookiesViews]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebCookieAdapter.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebCookieAdapter.m
@@ -27,21 +27,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     BOOL result;
 
-    NSHTTPCookieAcceptPolicy acceptPolicy = [[NSHTTPCookieStorage sharedCookieManager] acceptPolicy];
-    result = (acceptPolicy == NSHTTPCookieAcceptPolicyAlways || acceptPolicy == NSHTTPCookieAcceptPolicyOnlyFromMainDocumentDomain);
+    NSHTTPCookieAcceptPolicy cookieAcceptPolicy = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookieAcceptPolicy];
+    result = (cookieAcceptPolicy == NSHTTPCookieAcceptPolicyAlways || cookieAcceptPolicy == NSHTTPCookieAcceptPolicyOnlyFromMainDocumentDomain);
     
     return result;
 }
 
-- (NSString *)cookiesForURL:(NSString *)URL
+- (NSString *)cookiesForURL:(NSString *)URLString
 {
-    return [[[NSHTTPCookieStorage sharedCookieManager] cookieRequestHeaderFieldsForURL:[NSURL _web_URLWithString:URL]] objectForKey:@"Cookie"];
+    NSURL *URL = [NSURL _web_URLWithString:URLString];
+    NSArray *cookiesForURL = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:URL];
+    NSDictionary *header = [NSHTTPCookie requestHeaderFieldsWithCookies:cookiesForURL];
+    return [header objectForKey:@"Cookie"];
 }
 
-- (void)setCookies:(NSString *)cookies forURL:(NSString *)URL policyBaseURL:(NSString *)policyBaseURL
+- (void)setCookies:(NSString *)cookieString forURL:(NSString *)URLString policyBaseURL:(NSString *)policyBaseURL
 {
-    [[NSHTTPCookieStorage sharedCookieManager] setCookiesFromResponseHeader:[NSDictionary dictionaryWithObject:cookies forKey:@"Set-Cookie"]
-        forURL:[NSURL _web_URLWithString:URL] policyBaseURL:[NSURL _web_URLWithString:policyBaseURL]];    
+    NSURL *URL = [NSURL _web_URLWithString:URLString];
+    NSArray *cookies = [NSHTTPCookie cookiesWithResponseHeaderFields:[NSDictionary dictionaryWithObject:cookieString forKey:@"Set-Cookie"] forURL:URL];
+    [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookies:cookies forURL:URL mainDocumentURL:[NSURL _web_URLWithString:policyBaseURL]];    
 }
 
 @end
