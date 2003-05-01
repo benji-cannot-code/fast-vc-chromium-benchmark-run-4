cc91d9ffaaad332080768e7617b72170a5fd5966
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-01 16:35:11 +0000

    Reviewed by Gramps
    	Updated copyright notices on every header and
    	implementation file in the framework.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4242 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebDatabase.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebDatabase.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/*	WebDatabase.h
-	Copyright 2002-2003, Apple, Inc. All rights reserved.
+/*	
+    WebDatabase.h
+    Copyright (C) 2003 Apple Computer, Inc. All rights reserved.    
 */
 
 #import <Foundation/Foundation.h>
--- third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/*	WebFileDatabase.h
-	Copyright 2002-2003, Apple, Inc. All rights reserved.
+/*	
+    WebFileDatabase.h
+    Copyright (C) 2003 Apple Computer, Inc. All rights reserved.    
 */
 
 #import <Foundation/Foundation.h>
