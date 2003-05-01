e93109e23d4e4e1d7c5cf1d6c9e4504e40c26b23
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-01 01:56:08 +0000

    Copy MIME type when copying WebDataProtocol's parameters.
            Reviewed by Ken.
    
            * WebView.subproj/WebDataProtocol.m:
            (-[WebDataRequestParameters copyWithZone:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataProtocol.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataProtocol.m
@@ -32,6 +32,7 @@ static NSString *WebDataRequestPropertyKey = @"WebDataRequest";
     newInstance->data = [data copyWithZone:zone];
     newInstance->encoding = [encoding copyWithZone:zone];
     newInstance->baseURL = [baseURL copyWithZone:zone];
+    newInstance->MIMEType = [MIMEType copyWithZone:zone];
     return newInstance;
 }
 
