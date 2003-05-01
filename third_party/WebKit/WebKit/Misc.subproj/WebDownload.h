50211221a3963276d4fcdf2d88f47722bb9ba67c
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-01 00:40:32 +0000

    Reviewed by Ken.
    	Minor header tweaks.
    
            * Misc.subproj/WebDownload.h: Fix copyright notice.
            * Panels.subproj/WebAuthenticationPanel.h: Added 2003 to copyright.
            * WebView.subproj/WebAuthenticationChallenge.h: Fix copyright notice, add docs.
            * WebView.subproj/WebAuthenticationChallengeInternal.h: Fix copyright notice.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebDownload.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebDownload.h
@@ -1,6 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/*	WebDownload.h
-        Copyright 2003, Apple, Inc. All rights reserved.
+/*	
+    WebDownload.h
+    Copyright (C) 2003 Apple Computer, Inc. All rights reserved.    
+    
+    Public header file.
 */
 
 #import <WebFoundation/NSURLDownload.h>
--- third_party/WebKit/WebKit/WebView.subproj/WebAuthenticationChallengeInternal.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebAuthenticationChallengeInternal.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*	
-    WebAuthenticationChallengeInternal.h
-    Copyright 2003 Apple, Inc. All rights reserved.
+    WebAuthenticationChallenge.h
+    Copyright (C) 2003 Apple Computer, Inc. All rights reserved.    
 */
 
 #import <WebKit/WebAuthenticationChallenge.h>
