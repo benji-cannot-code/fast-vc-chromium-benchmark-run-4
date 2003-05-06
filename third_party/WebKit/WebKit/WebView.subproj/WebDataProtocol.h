2df45375c8643e27d0df03916d08a519ef38aabc
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-06 02:47:39 +0000

    Reviewed by Richard.
    	Fix build by separating mutating data protocol methods into
    	category on NSMutableURLRequest. Also, formatting fixes.
    
            * WebView.subproj/WebDataProtocol.h:
            * WebView.subproj/WebDataProtocol.m:
            (+[NSURLRequest _webDataRequestURLForData:]):
            (-[NSURLRequest _webDataRequestData]):
            (-[NSURLRequest _webDataRequestEncoding]):
            (-[NSURLRequest _webDataRequestMIMEType]):
            (-[NSURLRequest _webDataRequestBaseURL]):
            (-[NSURLRequest _webDataRequestExternalRequest]):
            (-[NSMutableURLRequest _webDataRequestSetData:]):
            * WebView.subproj/WebFrame.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4280 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataProtocol.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataProtocol.h
@@ -20,13 +20,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface NSURLRequest (WebDataRequest)
 + (NSURL *)_webDataRequestURLForData:(NSData *)data;
-- (NSData *)_webDataRequestData;
-- (void)_webDataRequestSetData:(NSData *)data;
-- (NSString *)_webDataRequestEncoding;
-- (void)_webDataRequestSetEncoding:(NSString *)encoding;
-- (NSString *)_webDataRequestMIMEType;
-- (void)_webDataRequestSetMIMEType:(NSString *)MIMEType;
 - (NSURL *)_webDataRequestBaseURL;
-- (void)_webDataRequestSetBaseURL:(NSURL *)baseURL;
+- (NSData *)_webDataRequestData;
+- (NSString *)_webDataRequestEncoding;
+- (NSString *)_webDataRequestMIMEType;
 - (NSMutableURLRequest *)_webDataRequestExternalRequest;
 @end
+
+@interface NSMutableURLRequest (WebDataRequest)
+- (void)_webDataRequestSetData:(NSData *)data;
+- (void)_webDataRequestSetEncoding:(NSString *)encoding;
+- (void)_webDataRequestSetMIMEType:(NSString *)MIMEType;
+- (void)_webDataRequestSetBaseURL:(NSURL *)baseURL;
+@end
+
--- third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
@@ -115,7 +115,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)loadData:(NSData *)data MIMEType:(NSString *)MIMEType textEncodingName: (NSString *)encodingName baseURL:(NSURL *)URL;
 {
     NSURL *fakeURL = [NSURLRequest _webDataRequestURLForData: data];
-    NSURLRequest *request = [[[NSURLRequest alloc] initWithURL: fakeURL] autorelease];
+    NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] initWithURL: fakeURL] autorelease];
     [request _webDataRequestSetData:data];
     [request _webDataRequestSetEncoding:encodingName];
     [request _webDataRequestSetBaseURL:URL];
