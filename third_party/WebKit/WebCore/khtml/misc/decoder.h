6233cef05db2160e323399b8c5f4c93ce209bebe
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-09 17:17:14 +0000

    Reviewed by John.
            - fixed 3191943 -- XML parsing error occurs when loading shift-JIS Japanese XML file
    
            * khtml/misc/decoder.h: Add a new type of encoding, EncodingFromXMLHeader.
            * khtml/misc/decoder.cpp:
            (findXMLEncoding): Added.
            (Decoder::decode): Call findXMLEncoding to extract the encoding from the XML header.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4331 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/misc/decoder.h
+++ third_party/WebKit/WebCore/khtml/misc/decoder.h
@@ -37,6 +37,7 @@ public:
     enum EncodingType {
         DefaultEncoding,
         AutoDetectedEncoding,
+        EncodingFromXMLHeader,
         EncodingFromMetaTag,
         EncodingFromHTTPHeader,
         UserChosenEncoding
@@ -59,11 +60,13 @@ public:
 protected:
     // codec used for decoding. default is Latin1.
     QTextCodec *m_codec;
-    QTextDecoder *m_decoder; // only used for utf16
+    QTextDecoder *m_decoder;
     QCString enc;
     EncodingType m_type;
 
 #if APPLE_CHANGES
+    // Our version of QString works well for all-8-bit characters, and allows null characters.
+    // This works better than QCString when there are null characters involved.
     QString buffer;
 #else
     QCString buffer;
@@ -74,5 +77,6 @@ protected:
     bool visualRTL;
 };
 
-};
+}
+
 #endif
