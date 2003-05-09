a3a6b644d271348c18cca26e92ad071289f37144
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-09 01:16:29 +0000

    Fix the definition of isRoot() so that it doesn't depend on 	the object being in the render tree yet (and to avoid any 	virtual function calls as well).
    	Make sure that requiresLayer() returns true if the object is
    	the root.  This way the layer isn't destroyed when you e.g.,
    	hit Cmd+ to increase font size.
    
    	This fixes bug 3252496.
    
            Reviewed by rjw
    
            * khtml/rendering/render_block.cpp:
            * khtml/rendering/render_box.cpp:
            (RenderBox::setStyle):
            * khtml/rendering/render_object.cpp:
            (RenderObject::isRoot):
            * khtml/rendering/render_object.h:
            * khtml/xml/dom_elementimpl.cpp:
            (ElementImpl::createRenderer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_object.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_object.cpp
@@ -160,6 +160,11 @@ RenderObject::~RenderObject()
         m_style->deref();
 }
 
+bool RenderObject::isRoot() const {
+    return element() && element()->renderer() == this &&
+           element()->getDocument()->documentElement() == element();
+}
+
 bool RenderObject::canHaveChildren() const
 {
     return false;
--- third_party/WebKit/WebCore/khtml/rendering/render_object.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_object.h
@@ -118,7 +118,9 @@ public:
     RenderLayer* findNextLayer(RenderLayer* parentLayer, RenderObject* startPoint,
                                bool checkParent=true);
     virtual void positionChildLayers() { }
-    virtual bool requiresLayer() { return !isTableCell() && (isPositioned() || isRelPositioned()); }
+    virtual bool requiresLayer() {
+        return isRoot() || (!isTableCell() && (isPositioned() || isRelPositioned()));
+    }
     
     virtual QRect getOverflowClipRect(int tx, int ty) { return QRect(0,0,0,0); }
     virtual QRect getClipRect(int tx, int ty) { return QRect(0,0,0,0); }
@@ -185,7 +187,7 @@ public:
     virtual bool isListItem() const { return false; }
     virtual bool isListMarker() const { return false; }
     virtual bool isCanvas() const { return false; }
-    bool isRoot() const { return parent() && parent()->isCanvas(); }
+    bool isRoot() const;
     virtual bool isBR() const { return false; }
     virtual bool isTableCell() const { return false; }
     virtual bool isTableRow() const { return false; }
