825a2407874a312b17f1aaefbf5496d964e1323e
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-12 16:11:00 +0000

    Reviewed by John.
    	- fixed 3255281 -- REGRESSION: objc_error in key loop code, closing tab containing aintitcool.com page
    
            * kwq/KWQTextField.mm:
            (-[KWQSecureTextField nextKeyView]): Change back to using our own nextKeyView
            implementation. By returning the nextKeyView from KWQTextField, we ended up making
            AppKit very upset about the key view loop. This undoes a change I made last
            Thursday, so the regression is very new and was never in a release.
            (-[KWQSecureTextField previousKeyView]): More of the same.
            (-[KWQSecureTextField nextValidKeyView]): Ditto.
            (-[KWQSecureTextField previousValidKeyView]): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4352 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextField.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextField.mm
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Another is hook up next and previous key views to KHTML.
 @interface KWQSecureTextField : NSSecureTextField <KWQWidgetHolder>
 {
+    BOOL inNextValidKeyView;
     BOOL inSetFrameSize;
 }
 @end
@@ -503,26 +504,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSView *)nextKeyView
 {
-    ASSERT([self delegate] == nil || [[self delegate] isKindOfClass:[KWQTextField class]]);
-    return [(KWQTextField *)[self delegate] nextKeyView];
+    return inNextValidKeyView
+        ? KWQKHTMLPart::nextKeyViewForWidget([self widget], KWQSelectingNext)
+        : [super nextKeyView];
 }
 
 - (NSView *)previousKeyView
 {
-    ASSERT([self delegate] == nil || [[self delegate] isKindOfClass:[KWQTextField class]]);
-    return [(KWQTextField *)[self delegate] previousKeyView];
+   return inNextValidKeyView
+        ? KWQKHTMLPart::nextKeyViewForWidget([self widget], KWQSelectingPrevious)
+        : [super previousKeyView];
 }
 
 - (NSView *)nextValidKeyView
 {
-    ASSERT([self delegate] == nil || [[self delegate] isKindOfClass:[KWQTextField class]]);
-    return [(KWQTextField *)[self delegate] nextValidKeyView];
+    inNextValidKeyView = YES;
+    NSView *view = [super nextValidKeyView];
+    inNextValidKeyView = NO;
+    return view;
 }
 
 - (NSView *)previousValidKeyView
 {
-    ASSERT([self delegate] == nil || [[self delegate] isKindOfClass:[KWQTextField class]]);
-    return [(KWQTextField *)[self delegate] previousValidKeyView];
+    inNextValidKeyView = YES;
+    NSView *view = [super previousValidKeyView];
+    inNextValidKeyView = NO;
+    return view;
 }
 
 // The currentEditor method does not work for secure text fields.
