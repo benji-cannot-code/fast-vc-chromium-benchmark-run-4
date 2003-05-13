6ca413a48acf5b1ff273d4d25ca47036dbc9f895
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-13 20:04:56 +0000

    WebFoundation:
    	API CHANGE
    
            Reviewed by Darin.
    
    	- renamed NSErrorFailingURLKey to NSErrorFailingURLStringKey
    	- removed NSURLCFStreamErrorDomain
    
            * Misc.subproj/NSURLError.h:
            * Misc.subproj/WebNSErrorExtras.m:
            (-[NSError _web_initWithDomain:code:failingURL:]):
            (-[NSError _web_failingURL]):
            * WebFoundation-base.exp:
    
    WebKit:
    
            Reviewed by Darin.
    
    	- renamed NSErrorFailingURLKey to NSErrorFailingURLStringKey
    
            * Misc.subproj/WebKitErrors.m:
            (-[NSError _initWithPluginErrorCode:contentURLString:pluginPageURLString:pluginName:MIMEType:]):
    
    WebBrowser:
    
            Reviewed by Darin.
    
    	- renamed NSErrorFailingURLKey to NSErrorFailingURLStringKey
    
            * LoadProgressMonitor.m:
            (-[LoadProgressMonitor webView:plugInFailedWithError:dataSource:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebKitErrors.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebKitErrors.m
@@ -59,7 +59,7 @@ static void registerErrors(void);
     
     NSMutableDictionary *userInfo = [[NSMutableDictionary alloc] init];
     if (contentURLString) {
-        [userInfo setObject:contentURLString forKey:NSErrorFailingURLKey];
+        [userInfo setObject:contentURLString forKey:NSErrorFailingURLStringKey];
     }
     if (pluginPageURLString) {
         [userInfo setObject:pluginPageURLString forKey:WebKitErrorPlugInPageURLStringKey];
