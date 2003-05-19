d77d3cf2059136b69cd08f1055475541d9a78f8e
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-19 18:55:29 +0000

    Reviewed by Darin
    	Fixed build problem with declaring MIMEType as a category
    	on NSObject. This method needs to be typed to return NSString.
    
            * kwq/KWQLoader.mm
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQLoader.mm
+++ third_party/WebKit/WebCore/kwq/KWQLoader.mm
@@ -99,7 +99,7 @@ void KWQReleaseResponse(void *response)
 }
 
 @interface NSObject (WebPrivateResponse)
-- (void *)MIMEType;
+- (NSString *)MIMEType;
 @end
 
 void *KWQResponseMIMEType(void *response)
