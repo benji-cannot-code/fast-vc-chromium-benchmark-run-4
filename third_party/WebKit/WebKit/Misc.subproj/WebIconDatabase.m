51118e8ae7ea1ef9dc537a5d97641a84946e5896
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-21 20:10:42 +0000

    These problems: 	3184359 - icon exception closing window while typing 	3245476 - Safari-78 crashes or hangs after IMDB Find and using the history menu to go back ... 	are not or are no longer reproducible. They were caused by an exception raised in WebKit. 	Since we don't use exceptions in WebKit, I've replaced the exception with an assert.
    	Reviewed by john.
    
            * Misc.subproj/WebIconDatabase.m:
            (-[WebIconDatabase _releaseFutureIconForURL:]): assert instead of exception if failure
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4406 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebIconDatabase.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebIconDatabase.m
@@ -541,7 +541,7 @@ NSSize WebIconLargeSize = {128, 128};
     NSNumber *retainCount = [_private->iconURLToRetainCount objectForKey:iconURLString];
 
     if (!retainCount) {
-        ERROR("Tried to release a non-retained icon: %@", iconURLString);
+        ASSERT_NOT_REACHED();
         return;
     }
     
@@ -595,9 +595,8 @@ NSSize WebIconLargeSize = {128, 128};
     
     NSNumber *retainCount = [_private->futureURLToRetainCount objectForKey:URL];
 
-    if(!retainCount){
-        [NSException raise:NSGenericException
-                    format:@"Releasing a future icon that was not previously retained."];
+    if (!retainCount) {
+        ASSERT_NOT_REACHED();
         return;
     }
 
