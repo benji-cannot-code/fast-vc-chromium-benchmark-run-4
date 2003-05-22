84b6427a83fea246bd72ddae5712f02b75ae5ce3
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-22 05:58:37 +0000

    This patch deals with the main cause of regressions from 	the minimum font size removal.  Because we stored font sizes 	as integers, we ended up flooring font sizes, and when percentages 	were nested, the magnitude of error increased dramatically.
    	This patch changes FontDef to store a float size instead of an
    	integer size.  The style system thus always computes a precise
    	floating point value for a font, with no error introduced and
    	only once it has that final computed value does it round to the
    	nearest pixel for the QFont.
    
    	This fixes 3265628, www.anandtech.com.
    
            Reviewed by rjw
    
            * khtml/css/css_valueimpl.cpp:
            (CSSPrimitiveValueImpl::computeLengthFloat):
            * khtml/css/cssstyleselector.cpp:
            * khtml/rendering/font.cpp:
            (Font::update):
            * khtml/rendering/font.h:
            * khtml/xml/dom_docimpl.cpp:
            (DocumentImpl::recalcStyle):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4411 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/font.h
+++ third_party/WebKit/WebCore/khtml/rendering/font.h
@@ -43,7 +43,7 @@ class FontDef
 {
 public:
     FontDef()
-        : size( 0 ), italic( false ), smallCaps( false ), sizeSpecified(false), weight( 50 ), 
+        : size( 0.0f ), italic( false ), smallCaps( false ), sizeSpecified(false), weight( 50 ), 
           genericFamily(0), hasNbsp( true )
 #if APPLE_CHANGES
           , usePrinterFont( false )
@@ -72,7 +72,16 @@ public:
     KWQFontFamily &firstFamily() { return family; }
     
     KWQFontFamily family;
-    short int size;
+
+    int pixelSize() const { return int(size + 0.5f); }
+    float floatSize() const { return size; }
+    void setSize(float s) { size = s; }
+    
+private:
+    float size; // Making this private forces people to think about what value they want,
+                // either the floatSize or the pixelSize.
+
+public:
     bool italic 		: 1;
     bool smallCaps 		: 1;
     bool sizeSpecified		: 1;  // Whether or not CSS specified an explicit size
@@ -111,6 +120,8 @@ public:
                 wordSpacing == other.wordSpacing );
     }
 
+    const FontDef& getFontDef() const { return fontDef; }
+    
     void update( QPaintDeviceMetrics *devMetrics ) const;
 
                    
