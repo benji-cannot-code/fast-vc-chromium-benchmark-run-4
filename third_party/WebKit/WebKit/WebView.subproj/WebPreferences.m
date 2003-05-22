6d19d4602e9282526a5e4d2773d4e705706310f9
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-22 20:39:16 +0000

    Fixed 3266464.  Build problem on panther caused by overly 	pedantic gcc.
            Reviewed by John.
    
            * WebView.subproj/WebPreferences.m:
            (-[WebPreferences initWithCoder:]):
            * WebView.subproj/WebView.m:
            (-[WebView initWithCoder:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4416 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebPreferences.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebPreferences.m
@@ -98,10 +98,10 @@ enum { WebPreferencesVersion = 1 };
 
 - (id)initWithCoder:(NSCoder *)decoder
 {
+NS_DURING
     int version;
     id result = nil;
 
-NS_DURING
 
     _private = [[WebPreferencesPrivate alloc] init];
     [decoder decodeValueOfObjCType:@encode(int) at:&version];
@@ -120,17 +120,11 @@ NS_DURING
             result = self;
         }
     }
-    
-NS_HANDLER
-
-    result = nil;
-
-NS_ENDHANDLER
-
-    if (result == nil)
-        [self release];
-
     return result;
+NS_HANDLER
+    [self release];
+    return nil;
+NS_ENDHANDLER
 }
 
 - (void)encodeWithCoder:(NSCoder *)encoder
