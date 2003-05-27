333c9f642f8443c09d842f83c1bdac48f9e3be87
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-27 20:50:34 +0000

    Fixed: 3242864 - repro assertion failure in WebIconDatabase.m for www.shutterfly.com
            Reviewed by john.
    
            * Misc.subproj/WebIconLoader.m:
            (-[WebIconLoader connectionDidFinishLoading:]): don't set the icon on the DB if it has no representations
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4427 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebIconLoader.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebIconLoader.m
@@ -99,7 +99,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
     NSImage *icon = [[NSImage alloc] initWithData:_private->resourceData];
-    if (icon) {
+    if (icon && [[icon representations] count]) {
         [[WebIconDatabase sharedIconDatabase] _setIcon:icon forIconURL:[[self URL] absoluteString]];
     } else {
 	[[WebIconDatabase sharedIconDatabase] _setHaveNoIconForIconURL:[[self URL] absoluteString]];
