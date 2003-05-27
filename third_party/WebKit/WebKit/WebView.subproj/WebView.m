88a5ed03a05532bf66141a0c47dc133c620a708b
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-27 18:34:05 +0000

    Fixes for IB.
            Reviewed by Eric Seymour.
    
            * WebView.subproj/WebPreferences.m:
            (-[WebPreferences initWithCoder:]):
    	Added retain to uniqued instance
    
            * WebView.subproj/WebView.m:
            (-[WebView initWithCoder:]):
            (-[WebView setPreferences:]):
    	Added release check to global uniquing dictionary.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4425 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebView.m
@@ -161,7 +161,7 @@ NS_DURING
 NS_HANDLER
 
     result = nil;
-    [self autorelease];
+    [self release];
 
 NS_ENDHANDLER
 
@@ -204,6 +204,7 @@ NS_ENDHANDLER
 {
     if (_private->preferences != prefs){
         [[NSNotificationCenter defaultCenter] removeObserver: self name: WebPreferencesChangedNotification object: [self preferences]];
+        [WebPreferences _removeReferenceForIdentifier: [_private->preferences identifier]];
         [_private->preferences release];
         _private->preferences = [prefs retain];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_preferencesChangedNotification:)
