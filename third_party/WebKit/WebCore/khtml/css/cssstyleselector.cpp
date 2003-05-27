e76a4a4d9f6a5c1bd6339d07d678efbe942f4535
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-27 20:04:26 +0000

    Fix for 3268589, static position miscomputed when the positioned object is the last item in its enclosing block and when its previous sibling is a text run that ends in a space.  In this case we need to clear our trailingSpaceObject so that we don't mistakenly ignore the positioned object when building up bidi runs.
    	Fix for 3260053, table-layout of inherit not supported.  This
    	caused a crash.  The fix is to just add in the support for the inherit
    	value to the style selector.
    
            Reviewed by john
    
            * khtml/rendering/bidi.cpp:
    	* khtml/css/cssstyleselector.cpp
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/css/cssstyleselector.cpp
+++ third_party/WebKit/WebCore/khtml/css/cssstyleselector.cpp
@@ -1951,6 +1951,12 @@ void CSSStyleSelector::applyRule( int id, DOM::CSSValueImpl *value )
 //     case CSS_PROP_SPEAK_NUMERAL:
 //     case CSS_PROP_SPEAK_PUNCTUATION:
      case CSS_PROP_TABLE_LAYOUT: {
+        if(value->cssValueType() == CSSValue::CSS_INHERIT) {
+            if(!parentNode) return;
+            style->setTableLayout(parentStyle->tableLayout());
+            return;
+        }
+
         if ( !primitiveValue->getIdent() )
             return;
 
