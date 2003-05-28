50d992c40424609d368c254d03ff41eb4d6f4c43
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-05-28 20:47:29 +0000

    Fixed 3165631 (and other similar). 	Fixed 3262592.
    	We now set NSImage's cache mode to NSImageCacheNever during
    	progressive loads.  It gets reset to NSImageCacheDefault when
    	loads complete.
    
    	If an image is scaled, NSImage appears to create a NSCacheImageRep
    	with the wrong size during progessive image loading.  Specifically
    	it appears to create a cached rep with the original size.
    
            Reviewed by Chris.
    
            * WebCoreSupport.subproj/WebImageRenderer.m:
            (-[WebImageRenderer incrementalLoadWithBytes:length:complete:]):
            (-[WebImageRenderer drawClippedToValidInRect:fromRect:]):
            * WebCoreSupport.subproj/WebImageRendererFactory.m:
            (-[WebImageRendererFactory imageRendererWithMIMEType:]):
    
            * WebView.subproj/WebPreferences.h:
    	Updated headerdoc comments.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4440 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRendererFactory.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRendererFactory.m
@@ -53,6 +53,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [imageRenderer addRepresentation:rep];
     [rep release];
     [imageRenderer setFlipped:YES];
+    
+    // Turn the default caching mode back on when the image has completed load.
+    // Caching intermediate progressive representations causes problems for
+    // progressive loads.  We also rely on the NSBitmapImageRep surviving during
+    // incremental loads.  See 3165631 and 3262592.
+    [imageRenderer setCacheMode: NSImageCacheNever];
 
     [imageRenderer setScalesWhenResized:NO];
     return [imageRenderer autorelease];
