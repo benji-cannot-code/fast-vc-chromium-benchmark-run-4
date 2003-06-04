02e9dad54c901f536568439fe8b6ae405dc4845a
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-06-04 02:45:02 +0000

    WebKit:
            Fixed: 3278496 - <rdar://problem/3278496>:	NSURLDownload: initWithSource and source should be renamed to initWithRequest and request
    
            Reviewed by rjw.
    
            * Misc.subproj/WebDownload.m:
            (-[WebDownload initWithRequest:delegate:]):
            (-[WebDownload _initWithRequest:delegate:directory:]):
            * WebView.subproj/WebImageView.m:
            (-[WebImageView setNeedsDisplay:]):
            * WebView.subproj/WebViewPrivate.m:
            (-[WebView _downloadURL:toDirectory:]):
    
    WebBrowser:
    
            Fixed: 3278496 - <rdar://problem/3278496>:	NSURLDownload: initWithSource and source should be renamed to initWithRequest and request
    
            Reviewed by rjw.
    
            * DownloadMonitor.m:
            (-[DownloadMonitor downloadDidBegin:]):
            (-[DownloadMonitor startDownloadForRequest:mayOpenWhenDone:]):
            * DownloadProgressEntry.h:
            * DownloadProgressEntry.m:
            (-[DownloadProgressEntry reload]):
    
    WebKitExamples:
    
            Fixed: 3278496 - <rdar://problem/3278496>:	NSURLDownload: initWithSource and source should be renamed to initWithRequest and request
    
            Reviewed by rjw.
    
            * CarbonDownloader/CarbonURLDownload.mm:
            (CarbonURLDownload::CarbonURLDownload):
            * Downloader/MyDocument.m:
            (-[MyDocument downloadOrCancel:]):
            (-[MyDocument savePanelDidEnd:returnCode:contextInfo:]):
            (-[MyDocument download:didFailWithError:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4478 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebDownload.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebDownload.m
@@ -157,11 +157,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return self;
 }
 
-- (id)initWithSource:(NSURLRequest *)request delegate:(id)delegate
+- (id)initWithRequest:(NSURLRequest *)request delegate:(id)delegate
 {
     self = [self init];
     [_webInternal setRealDelegate:delegate];
-    [super initWithSource:request delegate:_webInternal];
+    [super initWithRequest:request delegate:_webInternal];
     return self;
 }
 
@@ -177,13 +177,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return self;
 }
 
-- (id)_initWithSource:(NSURLRequest *)request
-             delegate:(id)delegate
-            directory:(NSString *)directory
+- (id)_initWithRequest:(NSURLRequest *)request
+              delegate:(id)delegate
+             directory:(NSString *)directory
 {
     self = [self init];
     [_webInternal setRealDelegate:delegate];
-    [super _initWithSource:request delegate:_webInternal directory:directory];
+    [super _initWithRequest:request delegate:_webInternal directory:directory];
     return self;
 }
 
