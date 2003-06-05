9860814930ea4eea84d70755d97977855e0a76b7
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-06-05 00:26:38 +0000

    - fixed 3279957 -- REGRESSION: assertion failure at launch in 	Decoder::~Decoder() importing bookmarks
    	Richard actually wrote this fix.
            Reviewed by me, Darin
    
            * kwq/WebCoreEncodings.mm:
            (+[WebCoreEncodings decodeData:]):
    	don't use stack-based decoder; use new instead.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4483 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreEncodings.mm
+++ third_party/WebKit/WebCore/kwq/WebCoreEncodings.mm
@@ -32,9 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 + (NSString *)decodeData:(NSData *)data
 {
-    khtml::Decoder decoder;
-    QString result = decoder.decode(static_cast<const char *>([data bytes]), [data length]);
-    result += decoder.flush();
+    khtml::Decoder *decoder = new khtml::Decoder();
+    QString result = decoder->decode(static_cast<const char *>([data bytes]), [data length]);
+    result += decoder->flush();
+    decoder->deref();
     return result.getNSString();
 }
 
