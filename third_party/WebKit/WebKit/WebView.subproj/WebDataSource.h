9b71eefcf488cc0794d207d1dfbab414c041ec65
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-06-06 23:48:57 +0000

    Fixed 3283236.  Remove use of forward declarations in public header because 	CodeWarrior pukes on 'em.
            Reviewed by Chris.
    
            * WebView.subproj/WebDataSource.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSource.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSource.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#import <WebKit/WebDocument.h>
+
 @class WebDataSourcePrivate;
 @class WebFrame;
 @class NSURLConnection;
@@ -15,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class NSMutableURLRequest;
 @class NSURLResponse;
 
-@protocol WebDocumentRepresentation;
 
 /*!
     @class WebDataSource
