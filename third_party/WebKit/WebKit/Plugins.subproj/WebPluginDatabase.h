ad29523a63b5a8698e267d1056ae9f387b2836e9
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-06-09 22:40:07 +0000

    <rdar://problem/3283359>: don't load Cocoa Java plug-in if in Carbon app
            Reviewed by darin.
    
            * Plugins.subproj/WebPluginDatabase.h: removed pluginForFilename, wasn't being used
            * Plugins.subproj/WebPluginDatabase.m:
            (-[WebPluginDatabase pluginForKey:withEnumeratorSelector:]): don't use the plug-in if ![self isCocoa] && [[[webPlugin bundle] bundleIdentifier] isEqualToString:JavaCocoaPluginIdentifier]
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4507 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPluginDatabase.h
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPluginDatabase.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Plug-ins are returned in this order: New plug-in (WBPL), Mach-O Netscape, CFM Netscape
 - (WebBasePluginPackage *)pluginForMIMEType:(NSString *)mimeType;
 - (WebBasePluginPackage *)pluginForExtension:(NSString *)extension;
-- (WebBasePluginPackage *)pluginForFilename:(NSString *)filename;
 
 - (NSArray *)plugins;
 
