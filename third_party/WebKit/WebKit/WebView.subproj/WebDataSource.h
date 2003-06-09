383fa62525e320e5e226e90c57ea8e8022d6a34f
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-06-09 23:32:42 +0000

    * WebView.subproj/WebDataSource.h: Removed FIXME related to pageTitle.
    git-svn-id: svn://svn.chromium.org/blink/trunk@4508 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSource.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSource.h
@@ -99,7 +99,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*!
     @method pageTitle
     @result Returns nil or the page title.
-    // FIXME move to WebHTMLRepresentation
 */
 - (NSString *)pageTitle;
 
