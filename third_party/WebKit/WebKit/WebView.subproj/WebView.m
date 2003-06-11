06859c0bf7731d30aadf86a7ddc97b59d776b6bd
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-06-11 01:51:54 +0000

    2003-06-10  Richard Williams   <rjw@apple.com>
    	Back out incorrect fix to 3287862.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4520 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebView.m
@@ -95,6 +95,7 @@ enum { WebViewVersion = 1 };
 {
     NSRect f = [self frame];
     WebFrameView *wv = [[WebFrameView alloc] initWithFrame: NSMakeRect(0,0,f.size.width,f.size.height)];
+    [wv setAutoresizingMask: NSViewWidthSizable | NSViewHeightSizable];
     [self addSubview: wv];
     [wv release];
 
@@ -129,7 +130,6 @@ enum { WebViewVersion = 1 };
 {
     [super initWithFrame: f];
     [self _commonInitializationFrameName:frameName groupName:groupName];
-    [self setAutoresizingMask: NSViewWidthSizable | NSViewHeightSizable];
     return self;
 }
 
