bc496ac93193cfd07a902750d34317c1ddc18e67
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-06-12 16:43:34 +0000

    WebCore:
            Fixed by Richard, reviewed by me.
    
    	- fixed 3289047 -- REGRESSION: can't go back after using form at attwireless.com
    
            * khtml/khtmlpart_p.h: Add an "executing JavaScript form action" flag.
            * kwq/WebCoreBridge.h: Add an "executing JavaScript form action" flag parameter to
            the reportClientRedirectToURL method.
    
            * khtml/khtml_part.cpp: (KHTMLPart::submitForm): In the JavaScript case, set a
            flag to say we are executing a JavaScript form action.
            * kwq/KWQKHTMLPart.mm: (KWQKHTMLPart::redirectionTimerStartedOrStopped): Pass the
            isJavaScriptFormAction state over the bridge.
    
    WebKit:
    
            Fixed by Richard, reviewed by me.
    
    	- fixed 3289047 -- REGRESSION: can't go back after using form at attwireless.com
    
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge reportClientRedirectToURL:delay:fireDate:lockHistory:isJavaScriptFormAction:]):
            Added isJavaScriptFormAction parameter, passed on to frame.
    
            * WebView.subproj/WebFramePrivate.h: Added isJavaScriptFormAction parameter.
            * WebView.subproj/WebFramePrivate.m:
            (-[WebFrame _clientRedirectedTo:delay:fireDate:lockHistory:isJavaScriptFormAction:]):
            If isJavaScriptFormAction is YES, then don't treat this is a "quick redirect" which is
            merged with the previous page for purposes of back/forward.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4528 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtmlpart_p.h
+++ third_party/WebKit/WebCore/khtml/khtmlpart_p.h
@@ -396,6 +396,8 @@ public:
   bool m_newJSInterpreterExists; // set to 1 by setOpenedByJS, for window.open
 
   bool m_bPendingChildRedirection;
+
+  bool m_executingJavaScriptFormAction;
 };
 
 #endif
