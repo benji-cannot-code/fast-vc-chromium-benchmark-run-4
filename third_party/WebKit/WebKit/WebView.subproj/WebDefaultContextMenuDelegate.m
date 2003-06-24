567aaa21800bc31734575bc7f27296afdc7655aa
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-06-24 18:36:40 +0000

    Renamed the context menu item Download Image To Disk" to "Download Image to Disk".
            Reviewed by john.
    
            * English.lproj/Localizable.strings:
            * WebView.subproj/WebDefaultContextMenuDelegate.m:
            (-[WebDefaultUIDelegate menuItemWithTag:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4563 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDefaultContextMenuDelegate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDefaultContextMenuDelegate.m
@@ -49,7 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             action = @selector(openImageInNewWindow:);
             break;
         case WebMenuItemTagDownloadImageToDisk:
-            title = UI_STRING("Download Image To Disk", "Download Image To Disk context menu item");
+            title = UI_STRING("Download Image to Disk", "Download Image To Disk context menu item");
             action = @selector(downloadImageToDisk:);
             break;
         case WebMenuItemTagCopyImageToClipboard:
