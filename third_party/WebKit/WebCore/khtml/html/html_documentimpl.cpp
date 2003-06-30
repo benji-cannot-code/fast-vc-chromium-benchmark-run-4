9319d0c9b1b0596a4be95c65d8f1e3c1b7208a60
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-06-30 21:40:06 +0000

    Fixed: <rdar://problem/3310066>: All images use wrong map if page has image map of name ""
            Reviewed by dave.
    
            * config.h:
            * khtml/html/html_documentimpl.cpp:
            (HTMLDocumentImpl::getMap): return 0 if getMap is called with a NULL string instead of treating it like an empty string
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_documentimpl.cpp
+++ third_party/WebKit/WebCore/khtml/html/html_documentimpl.cpp
@@ -316,6 +316,10 @@ void HTMLDocumentImpl::slotHistoryChanged()
 
 HTMLMapElementImpl* HTMLDocumentImpl::getMap(const DOMString& _url)
 {
+    if (_url.isNull()) {
+        return 0;
+    }
+    
     QString url = _url.string();
     QString s;
     int pos = url.find('#');
