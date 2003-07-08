ba874ab1144fb902d5b07455b54ffc67d7b4abcd
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-07-08 19:01:28 +0000

    Fixed: <rdar://problem/3272398>: Safari strips/modifies underscores in EMBED tags
            Reviewed by darin.
    
            * khtml/html/htmltokenizer.cpp:
            (HTMLTokenizer::parseTag): use tolower() to lowercase a char, don't use | 0x20 because that will cause chars like '_' to turn into junk
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4602 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/htmltokenizer.cpp
+++ third_party/WebKit/WebCore/khtml/html/htmltokenizer.cpp
@@ -912,11 +912,7 @@ void HTMLTokenizer::parseTag(DOMStringIt &src)
                     finish = true;
                     break;
                 }
-                // this is a nasty performance trick. will work for the A-Z
-                // characters, but not for others. if it contains one,
-                // we fail anyway
-                char cc = curchar;
-                cBuffer[cBufferPos++] = cc | 0x20;
+                cBuffer[cBufferPos++] = tolower(curchar);
                 ++src;
             }
 
@@ -1029,7 +1025,7 @@ void HTMLTokenizer::parseTag(DOMStringIt &src)
                         break;
                     }
                 }
-                cBuffer[cBufferPos++] = (char) curchar | 0x20;
+                cBuffer[cBufferPos++] = tolower(curchar);
                 ++src;
             }
             if ( cBufferPos == CBUFLEN ) {
