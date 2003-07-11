4647af826484a15c66bb034593fe4397940127c1
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-07-11 17:54:32 +0000

    Make sure image compositing obeys the current global alpha that 	is in effect, e.g., if someone has set an opacity within the 	current layer.
    	It's worth noting that I'm not even sure *why* this patch works,
    	but it does, even with nested opacity layers.
    
            Reviewed by darin
    
            * WebCoreSupport.subproj/WebImageRenderer.m:
            (-[WebImageRenderer drawClippedToValidInRect:fromRect:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4625 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebCore/WebCoreImageRenderer.h>
 #import <WebKit/WebAssertions.h>
 
+#import <CoreGraphics/CGContextGState.h>
+
 extern NSString *NSImageLoopCount;
 
 #define MINIMUM_DURATION (1.0/30.0)
@@ -278,7 +280,8 @@ static NSMutableSet *activeImageRenderers;
     }
     
     // This is the operation that handles transparent portions of the source image correctly.
-    [self drawInRect:ir fromRect:fr operation:NSCompositeSourceOver fraction:1.0];
+    CGContextRef context = (CGContextRef)([[NSGraphicsContext currentContext] graphicsPort]);
+    [self drawInRect:ir fromRect:fr operation:NSCompositeSourceOver fraction:CGContextGetAlpha(context)];
 }
 
 - (void)nextFrame:(id)context
