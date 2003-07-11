02c778aa0dfbd119407f6e9523eb77d85287b21d
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-07-11 16:24:37 +0000

    Reviewed by Chris.
            - fixed 3305391 -- repro crash in QPixmap::QPixmap after click in SVG using Adobe SVG plugin
    
            * kwq/WebCoreBridge.mm: (-[WebCoreBridge elementAtPoint:]): Remove code that tried to determine
            if something is an image by looking at the node type. Instead look at the object in the render
            tree and ask it if it's an image. Also use logic that exactly matches what RenderImage uses.
    
            * kwq/KWQDOMNode.h: Remove unused isImage function.
            * kwq/KWQDOMNode.cpp: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQDOMNode.h
+++ third_party/WebKit/WebCore/kwq/KWQDOMNode.h
@@ -24,12 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "dom_nodeimpl.h"
+#include "dom_nodeimpl.h"
 
-namespace DOM {
-    class NodeImpl;
-}
-
-bool isImage(DOM::NodeImpl *);
 DOM::NodeImpl::Id idFromNode(DOM::NodeImpl *);
-
--- third_party/WebKit/WebCore/kwq/WebCoreBridge.mm
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.mm
@@ -30,8 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "dom_node.h"
 #import "dom_docimpl.h"
 #import "dom_nodeimpl.h"
-#import "html_formimpl.h"
 #import "html_documentimpl.h"
+#import "html_formimpl.h"
+#import "html_imageimpl.h"
 #import "htmlattrs.h"
 #import "htmltags.h"
 #import "khtml_part.h"
@@ -43,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "render_canvas.h"
 #import "render_style.h"
 #import "render_replaced.h"
-using khtml::RenderWidget;
 
 #import <JavaScriptCore/property_map.h>
 
@@ -71,11 +71,12 @@ using DOM::NodeImpl;
 
 using khtml::Decoder;
 using khtml::parseURL;
+using khtml::RenderCanvas;
 using khtml::RenderImage;
 using khtml::RenderObject;
 using khtml::RenderPart;
 using khtml::RenderStyle;
-using khtml::RenderCanvas;
+using khtml::RenderWidget;
 
 using KJS::SavedProperties;
 using KJS::SavedBuiltins;
@@ -705,37 +706,42 @@ static HTMLFormElementImpl *formElementFromDOMElement(id <WebDOMElement>element)
     }
 
     NodeImpl *node = nodeInfo.innerNonSharedNode();
-    if (node && isImage(node)){
+    if (node && node->renderer() && node->renderer()->isImage()) {
         ElementImpl *i = static_cast<ElementImpl*>(node);
-        DOMString attr = i->getAttribute(ATTR_SRC);
-        if (attr.isEmpty()) {
-            // Look for the URL in the DATA attribute of the OBJECT tag.
-            attr = i->getAttribute(ATTR_DATA);
-        }
 
+        // FIXME: Code copied from RenderImage::updateFromElement; should share.
+        DOMString attr;
+        if (idFromNode(i) == ID_OBJECT) {
+            attr = i->getAttribute(ATTR_DATA);
+        } else {
+            attr = i->getAttribute(ATTR_SRC);
+        }
         if (!attr.isEmpty()) {
             [element setObject:_part->xmlDocImpl()->completeURL(attr.string()).getNSString() forKey:WebCoreElementImageURLKey];
         }
         
-        DOMString alt = i->getAttribute(ATTR_ALT);
+        // FIXME: Code copied from RenderImage::updateFromElement; should share.
+        DOMString alt;
+        if (idFromNode(i) == ID_INPUT)
+            alt = static_cast<HTMLInputElementImpl *>(i)->altText();
+        else if (idFromNode(i) == ID_IMG)
+            alt = static_cast<HTMLImageElementImpl *>(i)->altText();
         if (!alt.isNull()) {
             QString altText = alt.string();
             altText.replace('\\', _part->backslashAsCurrencySymbol());
             [element setObject:altText.getNSString() forKey:WebCoreElementImageAltStringKey];
         }
-        
-        RenderImage *r = (RenderImage *)node->renderer();
-        if (r) {
-            int x, y;
-            if (r->absolutePosition(x, y)) {
-                NSValue *rect = [NSValue valueWithRect:NSMakeRect(x, y, r->contentWidth(), r->contentHeight())];
-                [element setObject:rect forKey:WebCoreElementImageRectKey];
-            }
-            
-            NSImage *image = r->pixmap().image();
-            if (image) {
-                [element setObject:image forKey:WebCoreElementImageKey];
-            }
+
+        RenderImage *r = static_cast<RenderImage *>(node->renderer());
+        int x, y;
+        if (r->absolutePosition(x, y)) {
+            NSValue *rect = [NSValue valueWithRect:NSMakeRect(x, y, r->contentWidth(), r->contentHeight())];
+            [element setObject:rect forKey:WebCoreElementImageRectKey];
+        }
+
+        NSImage *image = r->pixmap().image();
+        if (image) {
+            [element setObject:image forKey:WebCoreElementImageKey];
         }
     }
     
