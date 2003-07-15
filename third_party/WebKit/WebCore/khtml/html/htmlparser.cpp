952df4bf0f0168a1e6a436de4b717cec860e9a1d
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-07-15 04:18:49 +0000

    Reviewed by Maciej.
            - fixed 3244447 -- add KOI8-U charset support
            - fixed 3327152 -- unicodeFFFE and unicodeFEFF are handled backwards by WebCore
            - fixed 3327157 -- x-mac-ukrainian should be mapped to MacCyrillic
            - fixed 3327160 -- shift-jis, shift_jis, x-sjis should be mapped to the DOS variant of Shift JIS
            - fixed 3327162 -- most Simplified Chinese encoding choices should be mapped to the DOS variant of EUC-CN
            - fixed 3327166 -- use DOS variant of EUC_KR
    
            * kwq/KWQCharsets.mm: Added some defines so we could use encodings that don't
            have any kCFStringEncoding constant in a header file.
            * kwq/mac-encodings.txt: Change table entries in accordance with the above bug reports.
            * kwq/KWQCharsetData.c: Regenerated.
    
            - other changes, inspired by investigation of various bugs
    
            * kwq/KWQButton.mm: (-[KWQButton sendConsumedMouseUpIfNeeded]): Make robust against cases where
            sending the consumed mouse up event destroys the QButton.
    
            * khtml/html/htmlparser.cpp:
            (KHTMLParser::reopenResidualStyleTags): Rearrange code slightly for clarity, added comment.
            (KHTMLParser::freeBlock): Removed unnecessary line of code.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4655 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/htmlparser.cpp
+++ third_party/WebKit/WebCore/khtml/html/htmlparser.cpp
@@ -1371,18 +1371,15 @@ void KHTMLParser::handleResidualStyleCloseTagAcrossBlocks(HTMLStackElem* elem)
 
 void KHTMLParser::reopenResidualStyleTags(HTMLStackElem* elem, bool inMalformedTable)
 {
-    // Nothing required.
-    if (!elem)
-        return;
-
-    // We have some tags that need to be reopened.
-    int exceptionCode = 0;
+    // Loop for each tag that needs to be reopened.
     while (elem) {
         // Create a shallow clone of the DOM node for this element.
         NodeImpl* newNode = elem->node->cloneNode(false); 
 
         // Append the new node.
+        int exceptionCode = 0;
         current->appendChild(newNode, exceptionCode);
+        // FIXME: Is it really OK to ignore the exception here?
 
         // Now push a new stack element for this node we just created.
         pushBlock(elem->id, elem->level);
@@ -1550,7 +1547,6 @@ void KHTMLParser::freeBlock()
 {
     while (blockStack)
         popOneBlock();
-    blockStack = 0;
 }
 
 void KHTMLParser::createHead()
--- third_party/WebKit/WebCore/kwq/KWQButton.mm
+++ third_party/WebKit/WebCore/kwq/KWQButton.mm
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation KWQButton
 
-
 - (id)initWithQButton:(QButton *)b
 {
     button = b;
@@ -59,7 +58,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     if (needToSendConsumedMouseUp) {
 	needToSendConsumedMouseUp = NO;
-	button->sendConsumedMouseUp();
+	if ([self target]) {
+            button->sendConsumedMouseUp();
+        }
     } 
 }
 
@@ -69,7 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [super mouseDown:event];
     [self sendConsumedMouseUpIfNeeded];
 }
-
 
 @end
 
