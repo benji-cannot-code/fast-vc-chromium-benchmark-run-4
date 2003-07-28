b7b6effb9431c0ff8cd56395ef0da19b4138921c
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-07-28 21:58:28 +0000

    Reviewed by Richard.
    	- fixed 3343050 - nil-deref in saveInterpreterBuiltins running Sherlock via MallocDebug in Panther7B20
    	- fixed 3343942 - Safari crashes when clicking on link w/ JavaScript turned off
    
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::saveInterpreterBuiltins): Don't attempt to save
    	when JavaScript is off.
            (KWQKHTMLPart::restoreInterpreterBuiltins): Don't attempt to restore
    	when JavaScript is off.
            (KWQKHTMLPart::saveWindowProperties): Remove ERROR, since that
    	case is normal when JS is off.
            (KWQKHTMLPart::saveLocationProperties): Remove ERROR, since that
    	case is normal when JS is off.
            (KWQKHTMLPart::restoreWindowProperties): Remove ERROR, since that
    	case is normal when JS is off.
            (KWQKHTMLPart::restoreLocationProperties): Remove ERROR, since that
    	case is normal when JS is off.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4723 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKHTMLPart.mm
+++ third_party/WebKit/WebCore/kwq/KWQKHTMLPart.mm
@@ -945,8 +945,6 @@ void KWQKHTMLPart::saveWindowProperties(SavedProperties *windowProperties)
     Window *window = Window::retrieveWindow(this);
     if (window)
         window->saveProperties(*windowProperties);
-    else
-        ERROR("NULL window");
 }
 
 void KWQKHTMLPart::saveLocationProperties(SavedProperties *locationProperties)
@@ -954,8 +952,6 @@ void KWQKHTMLPart::saveLocationProperties(SavedProperties *locationProperties)
     Window *window = Window::retrieveWindow(this);
     if (window)
         window->location()->saveProperties(*locationProperties);
-    else
-        ERROR("NULL window");
 }
 
 void KWQKHTMLPart::restoreWindowProperties(SavedProperties *windowProperties)
@@ -963,8 +959,6 @@ void KWQKHTMLPart::restoreWindowProperties(SavedProperties *windowProperties)
     Window *window = Window::retrieveWindow(this);
     if (window)
         window->restoreProperties(*windowProperties);
-    else
-        ERROR("NULL window");
 }
 
 void KWQKHTMLPart::restoreLocationProperties(SavedProperties *locationProperties)
@@ -972,18 +966,20 @@ void KWQKHTMLPart::restoreLocationProperties(SavedProperties *locationProperties
     Window *window = Window::retrieveWindow(this);
     if (window)
         window->location()->restoreProperties(*locationProperties);
-    else
-        ERROR("NULL window");
 }
 
 void KWQKHTMLPart::saveInterpreterBuiltins(SavedBuiltins &interpreterBuiltins)
 {
-    jScript()->interpreter()->saveBuiltins(interpreterBuiltins);
+    if (jScript() && jScript()->interpreter()) {
+	jScript()->interpreter()->saveBuiltins(interpreterBuiltins);
+    }
 }
 
 void KWQKHTMLPart::restoreInterpreterBuiltins(const SavedBuiltins &interpreterBuiltins)
 {
-    jScript()->interpreter()->restoreBuiltins(interpreterBuiltins);
+    if (jScript() && jScript()->interpreter()) {
+	jScript()->interpreter()->restoreBuiltins(interpreterBuiltins);
+    }
 }
 
 void KWQKHTMLPart::openURLFromPageCache(KWQPageState *state)
