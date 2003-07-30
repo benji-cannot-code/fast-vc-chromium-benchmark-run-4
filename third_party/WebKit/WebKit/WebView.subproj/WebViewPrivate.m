6cbbee0eb99ce98c13eaefa9a3774f8128dc1e56
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-07-30 21:02:38 +0000

    Backout hack for IB.  IB will deal with the issue internally.
    git-svn-id: svn://svn.chromium.org/blink/trunk@4734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebViewPrivate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebViewPrivate.m
@@ -829,12 +829,6 @@ NSString *_WebMainFrameURLKey = @"mainFrameURL";
     }
 }
 
-// Hack that IB calls to ensure that autoresizeFlags get set correctly.
-- (void)_finishedMakingConnections
-{
-	[self setAutoresizesSubviews:YES];
-}
-
 @end
 
 
