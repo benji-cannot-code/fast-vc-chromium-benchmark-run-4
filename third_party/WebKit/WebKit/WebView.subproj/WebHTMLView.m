755e592419d0697d5c9f95a8aee3d1a5bc25f693
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-04 21:41:21 +0000

    Reviewed by Richard.
            - fixed 3298153 -- get "screen font while printing" error, bad stuff happens after that (Sherlock, Safari)
    
            * WebView.subproj/WebHTMLView.m: (-[WebHTMLView drawRect:]):
            If we find ourselves in drawRect with the wrong printing mode, that usually
            means we're being printed as part of some larger print process, so do the
            layout in printing mode.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4764 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <AppKit/NSResponder_Private.h>
 #import <CoreGraphics/CGContextGState.h>
 
+@interface WebHTMLView (WebHTMLViewPrivate)
+- (void)_setPrinting:(BOOL)printing pageWidth:(float)pageWidth;
+@end
+
 @interface NSArray (WebHTMLView)
 - (void)_web_makePluginViewsPerformSelector:(SEL)selector withObject:(id)object;
 @end
@@ -519,11 +523,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     LOG(View, "%@ drawing", self);
     
-    WebTextRendererFactory *textRendererFactory = [WebTextRendererFactory sharedFactory];
-    
     BOOL subviewsWereSetAside = _private->subviewsSetAside;
     if (subviewsWereSetAside) {
         [self _restoreSubviews];
+    }
+    
+    // This helps when we print as part of a larger print process.
+    // If the WebHTMLView itself is what we're printing, then we will never have to do this.
+    BOOL wasInPrintingMode = _private->printing;
+    BOOL isPrinting = ![NSGraphicsContext currentContextDrawingToScreen];
+    if (wasInPrintingMode != isPrinting) {
+        [self _setPrinting:isPrinting pageWidth:0];
     }
     
     if ([[self _bridge] needsLayout]) {
@@ -562,16 +572,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [(WebClipView *)[self superview] setAdditionalClip:rect];
 
     NS_DURING {
-        NSView *focusView = [NSView focusView];
-        if ([WebTextRenderer shouldBufferTextDrawing] && focusView)
-            [textRendererFactory startCoalesceTextDrawing];
+        WebTextRendererFactory *textRendererFactoryIfCoalescing = nil;
+        if ([WebTextRenderer shouldBufferTextDrawing] && [NSView focusView]) {
+            textRendererFactoryIfCoalescing = [WebTextRendererFactory sharedFactory];
+            [textRendererFactoryIfCoalescing startCoalesceTextDrawing];
+        }
 
         //double start = CFAbsoluteTimeGetCurrent();
         [[self _bridge] drawRect:rect];
         //LOG(Timing, "draw time %e", CFAbsoluteTimeGetCurrent() - start);
 
-        if ([WebTextRenderer shouldBufferTextDrawing] && focusView)
-            [textRendererFactory endCoalesceTextDrawing];
+        if (textRendererFactoryIfCoalescing != nil) {
+            [textRendererFactoryIfCoalescing endCoalesceTextDrawing];
+        }
 
         [(WebClipView *)[self superview] resetAdditionalClip];
 
@@ -607,6 +620,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     double thisTime = CFAbsoluteTimeGetCurrent() - start;
     LOG(Timing, "%s draw seconds = %f", widget->part()->baseURL().URL().latin1(), thisTime);
 #endif
+
+    if (wasInPrintingMode != isPrinting) {
+        [self _setPrinting:wasInPrintingMode pageWidth:0];
+    }
 
     if (subviewsWereSetAside) {
         [self _setAsideSubviews];
