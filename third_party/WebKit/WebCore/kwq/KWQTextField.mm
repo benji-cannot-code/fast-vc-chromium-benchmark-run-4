a6eb23c7db3fced7ba8bfb3a7c00be78e7586fa1
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-07 20:27:10 +0000

    WebCore:
            Reviewed by Richard.
    
            - fixed 3347114 -- remove vestiges of posing, including init routine, from WebKit
    
            * kwq/KWQTextArea.mm: (-[KWQTextAreaTextView keyDown:]): Remove check for old
            versions of AppKit that let the WebKit posing handle key down events.
            * kwq/KWQTextField.mm: Removed fieldEditorDidMouseDown: methods.
    
            * WebCore.pbproj/project.pbxproj: Removed WebCoreFirstResponderChanges.h.
            * kwq/WebCoreFirstResponderChanges.h: Removed.
    
    WebKit:
    
            Reviewed by Richard.
    
            - fixed 3347114 -- remove vestiges of posing, including init routine, from WebKit
    
            * WebView.subproj/WebHTMLViewPrivate.m: Removed WebNSTextView, WebNSView, and WebNSWindow,
            removed the code to have them pose as NSTextView, NSView, and NSWindow, and removed excess
            imports that are no longer needed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4784 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextField.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextField.mm
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "KWQNSViewExtras.h"
 #import "KWQView.h"
 #import "WebCoreBridge.h"
-#import "WebCoreFirstResponderChanges.h"
 
 @interface KWQTextField (KWQInternal)
 - (void)setHasFocus:(BOOL)hasFocus;
@@ -413,16 +412,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return widget;
 }
 
-// FIXME: We can remove this once we require AppKit-705 or newer.
-- (void)fieldEditorDidMouseDown:(NSEvent *)event
-{
-    if (!widget) {
-	return;
-    }
-    widget->sendConsumedMouseUp();
-    widget->clicked();
-}
-
 - (void)setAlignment:(NSTextAlignment)alignment
 {
     [secureField setAlignment:alignment];
@@ -733,13 +722,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     ASSERT([[self delegate] isKindOfClass:[KWQTextField class]]);
     return [(KWQTextField *)[self delegate] widget];
-}
-
-// FIXME: We can remove this once we require AppKit-705 or newer.
-- (void)fieldEditorDidMouseDown:(NSEvent *)event
-{
-    ASSERT([[self delegate] isKindOfClass:[KWQTextField class]]);
-    [[self delegate] fieldEditorDidMouseDown:event];
 }
 
 - (void)textDidEndEditing:(NSNotification *)notification
