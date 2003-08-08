0ea1539145a0d8c829319579183a2330faedfb41
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-08 15:21:12 +0000

    Reviewed by John Sullivan.
            - fixed 3365527 -- subscripting JavaScript strings does not work (leads to hang at www.newmagna.com.au)
    
            The JavaScript specification says nothing about this, but other browsers seem to give
            read-only access to the characters in a string as if the string was an array of characters.
    
            * kjs/array_object.cpp:
            (ArrayInstanceImp::get): Update to use a public toArrayIndex function instead of our own getArrayIndex
            function, so we can share with string.
            (ArrayInstanceImp::put): Ditto.
            (ArrayInstanceImp::hasProperty): Ditto.
            (ArrayInstanceImp::setLength): Ditto.
    
            * kjs/ustring.h: Add toArrayIndex.
            * kjs/ustring.cpp: (UString::toArrayIndex): Added. Implements the rule from array.
            * kjs/identifier.h: Add a forwarding function so we can use toArrayIndex.
    
            * kjs/string_object.cpp:
            (StringInstanceImp::get): Return a single character string if the property name is an array index.
            (StringInstanceImp::hasProperty): Return true for property names that are suitable array indices.
    
            * JavaScriptCore.pbproj/project.pbxproj: Let Xcode be Xcode.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4792 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
@@ -71,25 +71,14 @@ ArrayInstanceImp::~ArrayInstanceImp()
   free(storage);
 }
 
-// Rule from ECMA 15.2 about what an array index is.
-// Must exactly match string form of an unsigned integer, and be less than 2^32 - 1.
-bool getArrayIndex(const Identifier &propertyName, unsigned &index)
-{
-  bool ok;
-  unsigned i = propertyName.toStrictUInt32(&ok);
-  if (!ok || i >= 0xFFFFFFFFU)
-    return false;
-  index = i;
-  return true;
-}
-
 Value ArrayInstanceImp::get(ExecState *exec, const Identifier &propertyName) const
 {
   if (propertyName == lengthPropertyName)
     return Number(length);
 
-  unsigned index;
-  if (getArrayIndex(propertyName, index)) {
+  bool ok;
+  unsigned index = propertyName.toArrayIndex(&ok);
+  if (ok) {
     if (index >= length)
       return Undefined();
     if (index < storageLength) {
@@ -121,8 +110,9 @@ void ArrayInstanceImp::put(ExecState *exec, const Identifier &propertyName, cons
     return;
   }
   
-  unsigned index;
-  if (getArrayIndex(propertyName, index)) {
+  bool ok;
+  unsigned index = propertyName.toArrayIndex(&ok);
+  if (ok) {
     put(exec, index, value, attr);
     return;
   }
@@ -154,8 +144,9 @@ bool ArrayInstanceImp::hasProperty(ExecState *exec, const Identifier &propertyNa
   if (propertyName == lengthPropertyName)
     return true;
   
-  unsigned index;
-  if (getArrayIndex(propertyName, index)) {
+  bool ok;
+  unsigned index = propertyName.toArrayIndex(&ok);
+  if (ok) {
     if (index >= length)
       return false;
     if (index < storageLength) {
@@ -262,8 +253,9 @@ void ArrayInstanceImp::setLength(unsigned newLength, ExecState *exec)
     ReferenceListIterator it = sparseProperties.begin();
     while (it != sparseProperties.end()) {
       Reference ref = it++;
-      unsigned index;
-      if (getArrayIndex(ref.getPropertyName(exec), index) && index > newLength) {
+      bool ok;
+      unsigned index = ref.getPropertyName(exec).toArrayIndex(&ok);
+      if (ok && index > newLength) {
 	ref.deleteValue(exec);
       }
     }
