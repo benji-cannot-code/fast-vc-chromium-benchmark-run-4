f76229907f977d1dcf8b7b60cdbd2ca3018be9ba
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-11 23:41:02 +0000

    Fix for 3349142, blocks inside inlines with generated content cause the generated 	content to get all screwed up, especially if you try to print.  This patch fixes 	generated content to be continuation-aware (and vice versa), so that the content 	behaves correctly when inlines get split.
            Reviewed by darin
    
            * khtml/rendering/render_block.cpp:
            * khtml/rendering/render_container.cpp:
            (RenderContainer::updatePseudoChild):
            * khtml/rendering/render_container.h:
            * khtml/rendering/render_flow.cpp:
            (RenderFlow::addChild):
            * khtml/rendering/render_inline.cpp:
            (RenderInline::isInlineContinuation):
            (RenderInline::addChildToFlow):
            (RenderInline::cloneInline):
            (RenderInline::splitInlines):
            * khtml/rendering/render_inline.h:
            * khtml/rendering/render_object.cpp:
            (RenderObject::continuation):
            (RenderObject::isInlineContinuation):
            * khtml/rendering/render_object.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4804 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_object.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_object.cpp
@@ -181,6 +181,16 @@ bool RenderObject::canHaveChildren() const
     return false;
 }
 
+RenderFlow* RenderObject::continuation() const
+{
+    return 0;
+}
+
+bool RenderObject::isInlineContinuation() const
+{
+    return false;
+}
+
 void RenderObject::addChild(RenderObject* , RenderObject *)
 {
     KHTMLAssert(0);
