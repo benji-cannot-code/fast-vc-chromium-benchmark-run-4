fde89d308b6de29350316b4dede394f3e2c07918
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-11 21:13:38 +0000

    Fixed: <rdar://problem/3163589>: Macromedia Flash 6 cannot take Asian text entry in Safari
            Revidewed by darin.
    
            * Plugins.subproj/WebBaseNetscapePluginView.m:
            (-[WebBaseNetscapePluginView installKeyEventHandler]): new
            (-[WebBaseNetscapePluginView removeKeyEventHandler]): new
            (-[WebBaseNetscapePluginView becomeFirstResponder]): call installKeyEventHandler
            (-[WebBaseNetscapePluginView resignFirstResponder]): call removeKeyEventHandler
            (-[WebBaseNetscapePluginView keyUp:]): call TSMProcessRawKeyEvent so key events go through the machinery and UI that plug-ins expect
            (-[WebBaseNetscapePluginView keyDown:]): call TSMProcessRawKeyEvent so key events go through the machinery and UI that plug-ins expect
            (TSMEventHandler): turn the TSM event into a series of EventRecords and pass them to the plug-in
            (-[WebBaseNetscapePluginView stop]): call removeKeyEventHandler because resignFirstResponder may not get called
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.h
+++ third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.h
@@ -58,6 +58,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NPP_URLNotifyProcPtr NPP_URLNotify;
     NPP_GetValueProcPtr NPP_GetValue;
     NPP_SetValueProcPtr NPP_SetValue;
+    
+    EventHandlerRef keyEventHandler;
 }
 
 + (WebBaseNetscapePluginView *)currentPluginView;
