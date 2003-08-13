8c742eaed3a649364b80e72b26bc41b1f7af210f
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-13 20:23:33 +0000

    WebKit:
            Reviewed by Darin
    
            Fix for this bug:
    
            <rdar://problem/3374487>: URLs with UTF-8 escape sequences can't be accessed
    	when typed in the Safari location bar
    
            * Misc.subproj/WebNSPasteboardExtras.m:
            (-[NSPasteboard _web_bestURL]): Call _web_URLWithUserTypedString: to make a
    	URL from this type of string.
            * Misc.subproj/WebNSURLExtras.h:
            * Misc.subproj/WebNSURLExtras.m:
            (hexDigit): Added.
            (+[NSURL _web_URLWithUserTypedString:]): Added. Creates a URL from a string
    	that is typed in a user, for example, in the Safari location bar.
    
    WebBrowser:
    
            Reviewed by Darin
    
    	Fix for this bug:
    
    	<rdar://problem/3374487>: URLs with UTF-8 escape sequences can't be accessed
    	when typed in the Safari location bar
    
            * BrowserNSStringExtras.m:
            (-[NSString possibleURLsForUserTypedString]): Use _web_URLWithUserTypedString:
    	to make URL from this type of string.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4814 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebNSURLExtras.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebNSURLExtras.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface NSURL (WebNSURLExtras)
 
++ (NSURL *)_web_URLWithUserTypedString:(NSString *)string;
+
 + (NSURL *)_web_URLWithDataAsString:(NSString *)string;
 + (NSURL *)_web_URLWithDataAsString:(NSString *)string relativeToURL:(NSURL *)baseURL;
 
