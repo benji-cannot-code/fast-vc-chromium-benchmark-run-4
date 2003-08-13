576ad29afb5b0810d39b354ea550deca53e8dd18
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-13 01:25:01 +0000

    - fixed 3369505 -- leaks of NSCFTimer after running through the 	cvs-base test suite
            Reviewed by Richard
    
            * WebView.subproj/WebFrame.m:
            (-[WebFrame stopLoading]):
    	release timer before nil'ing it out
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4812 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
@@ -145,6 +145,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [_private->provisionalDataSource _stopLoading];
     [_private->dataSource _stopLoading];
     [_private->scheduledLayoutTimer invalidate];
+    [_private->scheduledLayoutTimer release];
     _private->scheduledLayoutTimer = nil;
 
     // Release the provisional data source because there's no point in keeping it around since it is unused in this case.
