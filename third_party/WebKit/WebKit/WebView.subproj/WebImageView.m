5cd1ed7e30f0260fc11e6e05a5cb05834f450cc1
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-21 21:05:54 +0000

    - fixed 3387950 -- REGRESSION (85-89): Standalone image in Safari prints much 	smaller than in Jaguar
            Reviewed by Chris.
    
            * WebView.subproj/WebImageView.m:
            (-[WebImageView adjustFrameSize]):
    	renamed from setFrameSizeUsingImage; now sets the frame size to exactly
    	the image size (as it did in Jaguar always) when we're not drawing to
    	the screen.
            (-[WebImageView setFrameSize:]):
    	updated for name change
            (-[WebImageView layout]):
    	ditto
            (-[WebImageView beginDocument]):
    	adjust frame size (before printing)
            (-[WebImageView endDocument]):
    	adjust frame size (after printing)
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4871 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebImageView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebImageView.m
@@ -76,26 +76,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [[rep image] beginAnimationInRect:drawingRect fromRect:drawingRect];
 }
 
-// Ensures that the view always fills the content area (so we draw over the previous page)
-// and that the view is at least as large as the image.
-- (void)setFrameSizeUsingImage
+- (void)adjustFrameSize
 {
-    NSSize size = [[self _web_superviewOfClass:[NSClipView class]] frame].size;
-    NSSize imageSize = [[rep image] size];
-    size.width = MAX(size.width, imageSize.width);
-    size.height = MAX(size.height, imageSize.height);
+    NSSize size = [[rep image] size];
+    
+    // When drawing on screen, ensure that the view always fills the content area 
+    // (so we draw over the entire previous page), and that the view is at least 
+    // as large as the image.. Otherwise we're printing, and we want the image to 
+    // fill the view so that the printed size doesn't depend on the window size.
+    if ([NSGraphicsContext currentContextDrawingToScreen]) {
+        NSSize clipViewSize = [[self _web_superviewOfClass:[NSClipView class]] frame].size;
+        size.width = MAX(size.width, clipViewSize.width);
+        size.height = MAX(size.height, clipViewSize.height);
+    }
+    
     [super setFrameSize:size];
 }
 
 - (void)setFrameSize:(NSSize)size
 {
-    [self setFrameSizeUsingImage];
+    [self adjustFrameSize];
 }
 
 - (void)layout
 {
-    [self setFrameSizeUsingImage];    
+    [self adjustFrameSize];    
     needsLayout = NO;
+}
+
+- (void)beginDocument
+{
+    [self adjustFrameSize];
+    [super beginDocument];
+}
+
+- (void)endDocument
+{
+    [super endDocument];
+    [self adjustFrameSize];
 }
 
 - (void)setDataSource:(WebDataSource *)dataSource
