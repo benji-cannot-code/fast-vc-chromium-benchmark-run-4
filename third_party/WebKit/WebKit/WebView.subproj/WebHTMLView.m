5790d59a199d9764aab66dcdccd6f30be17667bf
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-22 04:21:33 +0000

    * WebView.subproj/WebHTMLView.m: (-[WebHTMLView _setPrinting:pageWidth:adjustViewSize:]):         Tweak to printing fix: Be sure to call adjustViewSize after applying styles and doing layout.
    git-svn-id: svn://svn.chromium.org/blink/trunk@4877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebViewPrivate.h>
 
 #import <AppKit/NSResponder_Private.h>
-#import <CoreGraphics/CGContextPrivate.h>
 #import <CoreGraphics/CGContextGState.h>
 
 @interface WebHTMLView (WebHTMLViewPrivate)
@@ -891,16 +890,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         }
     }
 
-    if (adjustViewSize){
-        [[self _bridge] adjustViewSize];
-    }
-    
     if (printing != _private->printing) {
         _private->printing = printing;
         [self setNeedsToApplyStyles:YES];
         [self setNeedsLayout:YES];
         [self layoutToPageWidth:pageWidth];
         [self setNeedsDisplay:NO];
+        if (adjustViewSize) {
+            [[self _bridge] adjustViewSize];
+        }
     }
 }
 
