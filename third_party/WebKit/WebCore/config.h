3625107373ad8d330ea1cb0ef0c0fd7ddf1caf83
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-22 04:22:35 +0000

    * Makefile.am: Work around DLCFN issue to prevent constant churn of config.h.         * config.h: Regenerated without DLCFN.
    git-svn-id: svn://svn.chromium.org/blink/trunk@4878 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/config.h
+++ third_party/WebKit/WebCore/config.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_DIRENT_H 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
 
 /* Define to 1 if you have the <float.h> header file. */
 #define HAVE_FLOAT_H 1
