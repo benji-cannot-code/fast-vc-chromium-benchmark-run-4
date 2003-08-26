7e251ea66d0eddbf1020347f7800c36ad497f740
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-26 23:13:37 +0000

    Reviewed by Maciej.
            - re-fixed 3366542 -- filename with non-ASCII name left out of Content-Disposition for <input type=file>
    
            * khtml/html/html_formimpl.cpp: (HTMLFormElementImpl::formData): Added missing # sign.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_formimpl.cpp
+++ third_party/WebKit/WebCore/khtml/html/html_formimpl.cpp
@@ -358,7 +358,7 @@ QByteArray HTMLFormElementImpl::formData(bool& ok)
                                 onlyfilename.append(&c, 1);
                             } else {
                                 QString ampersandEscape;
-                                ampersandEscape.sprintf("&%hu;", c.unicode());
+                                ampersandEscape.sprintf("&#%hu;", c.unicode());
                                 onlyfilename.append(ampersandEscape);
                             }
                         }
