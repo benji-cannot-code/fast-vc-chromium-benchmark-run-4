c623ad499da8e1fd18ea662e90a58db60de82aa0
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-27 06:41:12 +0000

    WebCore:
            Reviewed by Maciej.
    
            - follow-on to fix for 3310943 -- REGRESSION (Panther): textareas in forms sometimes draw blank (bugreporter)
    
            * kwq/KWQListBox.mm: (QListBox::QListBox): Remove call to releaseGState. This was here to
            work around a problem we thought was an AppKit bug, but it turns out it was caused by
            NSView hackery in WebKit. We now do the WebKit part in a way that does not create the problem.
    
    WebKit:
    
            Reviewed by Maciej.
    
            - fixed 3310943 -- REGRESSION (Panther): textareas in forms sometimes draw blank (bugreporter)
    
            There are two fixes here. The more elegant and slightly faster one that requires a new AppKit,
            and the less elegant one that works without AppKit support. By including both we don't have to
            worry about timing of submission of WebKit vs. AppKit but we get the good, elegant fix. Later,
            we can delete the less elegant fix.
    
            * WebView.subproj/WebClipView.m:
            (+[WebClipView initialize]): Set up a boolean global so we only hav to do the "does AppKit support
            _focusRingVisibleRect" check one time.
            (-[WebClipView resetAdditionalClip]): Only do the renewGState thing if we don't have the
            _focusRingVisibleRect method, but if we do the renewGState thing, do it to self and all descendants
            using _web_renewGStateDeep.
            (-[WebClipView setAdditionalClip:]): Ditto.
            (-[WebClipView visibleRect]): Only limit this based on the additional clip if we don't have the
            _focusRingVisibleRect method.
            (-[WebClipView _focusRingVisibleRect]): Override the new method. Harmless if it's an old AppKit
            that doesn't have the method yet.
            (-[NSView _web_renewGStateDeep]): Implemented this helper method. We can get rid of it once we
            are entirely on the new AppKit.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebClipView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebClipView.m
@@ -11,13 +11,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebKit/WebAssertions.h>
 
+// WebClipView's entire reason for existing is to set the clip used by focus ring redrawing.
+// There's no easy way to prevent the focus ring from drawing outside the passed-in clip rectangle
+// because it expects to have to draw outside the bounds of the view it's being drawn for.
+// But it looks for the enclosing clip view, which gives us a hook we can use to control it.
+// The "additional clip" is a clip for focus ring redrawing.
+
+// FIXME: Change terminology from "additional clip" to "focus ring clip".
+
+@interface NSView (WebClipViewAdditions)
+- (void)_web_renewGStateDeep;
+@end
+
 @implementation WebClipView
+
+static BOOL NSViewHasFocusRingVisibleRect;
+
++ (void)initialize
+{
+    NSViewHasFocusRingVisibleRect = [NSView instancesRespondToSelector:@selector(_focusRingVisibleRect)];
+}
 
 - (void)resetAdditionalClip
 {
     ASSERT(_haveAdditionalClip);
     _haveAdditionalClip = NO;
-    [self renewGState];
+    if (!NSViewHasFocusRingVisibleRect) {
+        [self _web_renewGStateDeep];
+    }
 }
 
 - (void)setAdditionalClip:(NSRect)additionalClip
@@ -25,7 +46,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     ASSERT(!_haveAdditionalClip);
     _haveAdditionalClip = YES;
     _additionalClip = additionalClip;
-    [self renewGState];
+    if (!NSViewHasFocusRingVisibleRect) {
+        [self _web_renewGStateDeep];
+    }
 }
 
 - (BOOL)hasAdditionalClip
@@ -42,10 +65,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSRect)visibleRect
 {
     NSRect rect = [super visibleRect];
+    if (_haveAdditionalClip && !NSViewHasFocusRingVisibleRect) {
+        rect = NSIntersectionRect(rect, _additionalClip);
+    }
+    return rect;
+}
+
+- (NSRect)_focusRingVisibleRect
+{
+    NSRect rect = [self visibleRect];
     if (_haveAdditionalClip) {
         rect = NSIntersectionRect(rect, _additionalClip);
     }
     return rect;
 }
 
+@end
+
+@implementation NSView (WebClipViewAdditions)
+
+- (void)_web_renewGStateDeep
+{
+    [[self subviews] makeObjectsPerformSelector:@selector(_web_renewGStateDeep)];
+    [self renewGState];
+}
+ 
 @end
