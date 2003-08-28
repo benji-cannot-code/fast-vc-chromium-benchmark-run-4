08f7a1c5a106fd89df9ccf0b688f6811038f4a25
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-28 02:50:55 +0000

    Reviewed by John
    	- fixed rdar://problem/3397316 - sherlock crash: KJS::Collector::allocate(unsigned long)
    
    	* kjs/internal.cpp:
            (InterpreterImp::InterpreterImp): Hold the lock a bit longer, so
    	the call to initGlobalObject is covered.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4909 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/internal.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/internal.cpp
@@ -524,7 +524,6 @@ InterpreterImp::InterpreterImp(Interpreter *interp, const Object &glob)
     s_hook = next = prev = this;
     globalInit();
   }
-  unlockInterpreter();
 
   global = glob;
   globExec = new ExecState(m_interpreter,0);
@@ -535,6 +534,7 @@ InterpreterImp::InterpreterImp(Interpreter *interp, const Object &glob)
   initGlobalObject();
 
   recursion = 0;
+  unlockInterpreter();
 }
 
 void InterpreterImp::lock()
