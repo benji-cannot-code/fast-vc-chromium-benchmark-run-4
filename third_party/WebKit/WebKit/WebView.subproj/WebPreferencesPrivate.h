db0fcfd5bff64b0cdfeec08fb8a1c4db56c3778e
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-29 22:02:24 +0000

    Fixed 3401334.  Use IB document key when checking for reference removal on instances of WebPreferences.
    	Also removed _userDefaultsKeysForIB, no longer needed by IB.
    
            Reviewed by John.
    
            * WebView.subproj/WebPreferences.m:
            (+[WebPreferences _removeReferenceForIdentifier:]):
            (-[WebPreferences _postPreferencesChangesNotification]):
            * WebView.subproj/WebPreferencesPrivate.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4916 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebPreferencesPrivate.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebPreferencesPrivate.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (WebPreferences *)_getInstanceForIdentifier:(NSString *)identifier;
 + (void)_setInstance:(WebPreferences *)instance forIdentifier:(NSString *)identifier;
 + (void)_removeReferenceForIdentifier:(NSString *)identifier;
-+ (NSArray *)_userDefaultsKeysForIB;
 - (NSTimeInterval)_backForwardCacheExpirationInterval;
 
 + (void)_setIBCreatorID:(NSString *)string;
