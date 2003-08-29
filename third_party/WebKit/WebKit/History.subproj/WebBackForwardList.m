ddcb9ce7d192c9734cc41b40ddd63abc4e0c1fef
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-08-29 21:52:59 +0000

    Fixed 3400807.  Don't release state associated with the current b/f item.  We shouldn't normally have page cache state associated with the current item (3401376).  This fix guarantees that we won't prematurely release the page cache state for the current item.
            Reviewed byJohn Sullivan.
    
            * History.subproj/WebBackForwardList.m:
            (-[WebBackForwardList _clearPageCache]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@4915 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
+++ third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
@@ -238,8 +238,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)_clearPageCache
 {
     int i;
+    WebHistoryItem *currentItem = [self currentItem];
+    
     for (i = 0; i < (int)[_private->entries count]; i++) {
-        [[_private->entries objectAtIndex:i] setHasPageCache:NO];
+        WebHistoryItem *item;
+        // Don't clear the current item.  Objects are still in use.
+        item = [_private->entries objectAtIndex:i];
+        if (item != currentItem)
+            [item setHasPageCache:NO];
     }
     [WebHistoryItem _releaseAllPendingPageCaches];
 }
