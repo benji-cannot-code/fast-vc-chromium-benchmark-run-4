b9af91dd65e668b61a57a3d1c592417ae1036e6e
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-09-24 04:52:35 +0000

    Reviewed by Ken.
            - fixed 3390850 -- Shift_JIS in meta tag ignored because <img> tag in JavaScript comes first (vintagecomp.com)
    
            * khtml/misc/decoder.cpp: (Decoder::decode): Don't let tags inside a script make us think we're in the body.
            Later we can enhance this even more.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5042 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/misc/decoder.cpp
+++ third_party/WebKit/WebCore/khtml/misc/decoder.cpp
@@ -484,6 +484,7 @@ QString Decoder::decode(const char *data, int len)
             const char *ptr = buffer.data();
             const char *pEnd = buffer.length();
 #endif
+            bool inScript = false;
             while(ptr != pEnd)
             {
                 if(*ptr == '<') {
@@ -573,7 +574,11 @@ QString Decoder::decode(const char *data, int len)
 			}
 		    }
                     case ID_SCRIPT:
+                        inScript = true;
+                        break;
                     case (ID_SCRIPT+ID_CLOSE_TAG):
+                        inScript = false;
+                        break;
                     case ID_NOSCRIPT:
                     case (ID_NOSCRIPT+ID_CLOSE_TAG):
                     case ID_STYLE:
@@ -592,6 +597,8 @@ QString Decoder::decode(const char *data, int len)
                     case (0 + ID_CLOSE_TAG ):
                         break;
                     default:
+                        if (inScript)
+                            break;
                         body = true;
 #ifdef DECODE_DEBUG
 			kdDebug( 6005 ) << "Decoder: no charset found. Id=" << id << endl;
