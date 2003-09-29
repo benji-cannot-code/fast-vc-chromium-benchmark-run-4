fbdfe35797119c2ea2eae1540994896717026670
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-09-29 23:04:02 +0000

    Fixed: <rdar://problem/3332460>: nil-deref in khtml::RenderWidget::eventFilter with onchange handler
            Reviewed by darin.
    
            * kwq/KWQTextField.mm:
            (-[KWQTextField controlTextDidEndEditing:]): call setHasFocus:NO last so we have the widget that lets us get to the bridge and lets us call controlTextDidEndEditing
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5081 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextField.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextField.mm
@@ -247,14 +247,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 -(void)controlTextDidEndEditing:(NSNotification *)notification
 {
-    [self setHasFocus:NO];
-
     if (!widget) {
 	return;
     }
 
     WebCoreBridge *bridge = KWQKHTMLPart::bridgeForWidget(widget);
     [bridge controlTextDidEndEditing:notification];
+    
+    [self setHasFocus:NO];
 }
 
 -(void)controlTextDidChange:(NSNotification *)notification
