1bbe0c82d41e7a14374d0f3eaf326910c8a25122
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-09-30 18:46:27 +0000

    Improvements to scrolling and layout.  Also fixing 3264346, body overflow should 	apply to document's scrollbars.
            Reviewed by darin
    
            * WebView.subproj/WebDynamicScrollBarsView.h:
            * WebView.subproj/WebDynamicScrollBarsView.m:
            (-[WebDynamicScrollBarsView initWithFrame:]):
            (-[WebDynamicScrollBarsView setScrollBarsSuppressed:repaintOnUnsuppress:]):
            (-[WebDynamicScrollBarsView updateScrollers]):
            (-[WebDynamicScrollBarsView reflectScrolledClipView:]):
            (-[WebDynamicScrollBarsView setAllowsScrolling:]):
            (-[WebDynamicScrollBarsView allowsScrolling]):
            (-[WebDynamicScrollBarsView setAllowsHorizontalScrolling:]):
            (-[WebDynamicScrollBarsView setAllowsVerticalScrolling:]):
            (-[WebDynamicScrollBarsView allowsHorizontalScrolling]):
            (-[WebDynamicScrollBarsView allowsVerticalScrolling]):
            (-[WebDynamicScrollBarsView horizontalScrollingMode]):
            (-[WebDynamicScrollBarsView verticalScrollingMode]):
            (-[WebDynamicScrollBarsView setHorizontalScrollingMode:]):
            (-[WebDynamicScrollBarsView setVerticalScrollingMode:]):
            (-[WebDynamicScrollBarsView setScrollingMode:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDynamicScrollBarsView.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebDynamicScrollBarsView.h
@@ -15,9 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface WebDynamicScrollBarsView : WebCoreScrollView <WebCoreFrameView>
 {
-    BOOL disallowsHorizontalScrolling;
-    BOOL disallowsVerticalScrolling;
+    WebCoreScrollBarMode hScroll;
+    WebCoreScrollBarMode vScroll;
     BOOL suppressLayout;
+    BOOL suppressScrollers;
 }
 
 - (void)setAllowsHorizontalScrolling:(BOOL)flag;
