d03ccc533a52856844e284280b037282121e356f
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-09-30 23:07:26 +0000

    Fixed: <rdar://problem/3006869>: show image dimensions in title bar when single image is loaded
            Reviewed by rjw.
    
            * English.lproj/Localizable.strings:
            * WebView.subproj/WebImageRepresentation.m:
            (-[WebImageRepresentation title]): return "foo.jpg 50x50 pixels"
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5094 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebImageRepresentation.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebImageRepresentation.m
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "WebImageRepresentation.h"
 
-#import <WebCore/WebCoreImageRenderer.h>
 #import <WebKit/WebDataSource.h>
 #import <WebKit/WebImageRenderer.h>
 #import <WebKit/WebImageRendererFactory.h>
+#import <WebKit/WebLocalizableStrings.h>
+
+#import <WebCore/WebCoreImageRenderer.h>
 
 #import <Foundation/NSURLRequest.h>
 
@@ -77,7 +79,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSString *)title
 {
-    return nil;
+    NSString *lastPathComponent = [[URL path] lastPathComponent];
+    NSSize size = [image size];
+    if (!NSEqualSizes(size, NSZeroSize)) {
+        return [NSString stringWithFormat:UI_STRING("%@ %dx%d pixels", "window title for a standalone image"), lastPathComponent, (int)size.width, (int)size.height];
+    }
+    return lastPathComponent;
 }
 
 @end
