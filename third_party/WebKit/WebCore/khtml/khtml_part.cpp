6c761c1c0d9736e0844a9f9be49a3fb3e2c1b7a9
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-07 00:01:43 +0000

    Reviewed by Ken.
            - preparation for the upcoming whitespace patch; refactored some of the code that extracts text
    
            * khtml/khtml_part.h: Added text() member function.
            * khtml/khtml_part.cpp:
            (KHTMLPart::text): Added. Refactored from selectedText; takes a DOM::Range as a parameter.
            Also added code to change newlines to spaces when extracting text from the DOM.
            (KHTMLPart::selectedText): Now calls text().
            (KHTMLPart::selection): Removed stray declaration.
    
            * kwq/KWQAccObject.mm: (-[KWQAccObject value]): Changed to call KHTMLPart::text.
            * kwq/WebCoreBridge.mm: (-[WebCoreBridge elementAtPoint:]): Changed to call KHTMLPart::text
            instead of doing it ourselves.
    
            * kwq/KWQKHTMLPart.mm: (KWQKHTMLPart::attributedString): Added code to change change '\n'
            newlines to spaces when extracting text from the DOM.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5140 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtml_part.cpp
+++ third_party/WebKit/WebCore/khtml/khtml_part.cpp
@@ -2217,7 +2217,7 @@ bool KHTMLPart::findTextNext( const QString &str, bool forward, bool caseSensiti
     }
 }
 
-QString KHTMLPart::selectedText() const
+QString KHTMLPart::text(const DOM::Range &r) const
 {
     // FIXME: This whole function should use the render tree and not the DOM tree, since elements could
     // be hidden using CSS, or additional generated content could be added.  For now, we just make sure
@@ -2226,7 +2226,7 @@ QString KHTMLPart::selectedText() const
   bool hasNewLine = true;
   bool addedSpace = true;
   QString text;
-  DOM::Node n = d->m_selectionStart;
+  DOM::Node n = r.startContainer();
   while(!n.isNull()) {
       if(n.nodeType() == DOM::Node::TEXT_NODE) {
           if (hasNewLine) {
@@ -2234,8 +2234,8 @@ QString KHTMLPart::selectedText() const
               hasNewLine = false;
           }
           QString str = n.nodeValue().string();
-          int start = (n == d->m_selectionStart) ? d->m_startOffset : -1;
-          int end = (n == d->m_selectionEnd) ? d->m_endOffset : -1;
+          int start = (n == r.startContainer()) ? r.startOffset() : -1;
+          int end = (n == r.endContainer()) ? r.endOffset() : -1;
           RenderObject* renderer = n.handle()->renderer();
           if (renderer && renderer->isText()) {
               if (renderer->style()->whiteSpace() == khtml::PRE) {
@@ -2262,7 +2262,9 @@ QString KHTMLPart::selectedText() const
                           bool spaceBetweenRuns = false;
                           if (runStart >= runs[i]->m_start &&
                               runStart < runs[i]->m_start + runs[i]->m_len) {
-                              text += str.mid(runStart, runEnd - runStart);
+                              QString runText = str.mid(runStart, runEnd - runStart);
+                              runText.replace('\n', ' ');
+                              text += runText;
                               start = -1;
                               spaceBetweenRuns = i+1 < runs.count() && runs[i+1]->m_start > runEnd;
                               addedSpace = str[runEnd-1].direction() == QChar::DirWS;
@@ -2319,7 +2321,7 @@ QString KHTMLPart::selectedText() const
             break;
         }
       }
-      if(n == d->m_selectionEnd) break;
+      if(n == r.endContainer()) break;
       DOM::Node next = n.firstChild();
       if(next.isNull()) next = n.nextSibling();
       while( next.isNull() && !n.parentNode().isNull() ) {
@@ -2376,6 +2378,11 @@ QString KHTMLPart::selectedText() const
     return text.mid(start, end-start);
 }
 
+QString KHTMLPart::selectedText() const
+{
+    return text(selection());
+}
+
 bool KHTMLPart::hasSelection() const
 {
   return ( !d->m_selectionStart.isNull() &&
@@ -2384,7 +2391,7 @@ bool KHTMLPart::hasSelection() const
 
 DOM::Range KHTMLPart::selection() const
 {
-    DOM::Range r = document().createRange();DOM::Range();
+    DOM::Range r = document().createRange();
     r.setStart( d->m_selectionStart, d->m_startOffset );
     r.setEnd( d->m_selectionEnd, d->m_endOffset );
     return r;
