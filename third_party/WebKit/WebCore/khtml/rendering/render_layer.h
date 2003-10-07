1710e795184010569cc62a9c68189a0e607071fa
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-07 20:15:59 +0000

    Fix for 3363421, event handlers could be triggered for content outside an overflow:hidden 	area.  The layer checks that test for intersection/point containment need to only include 	layers with overhanging floats if the element is overflow:visible.
    	Fix for 3366801, assignment to scrollLeft/Top of an overflow:hidden layer makes the layer
    	disappear.  overflow:hidden blocks actually were never computing their scroll dimensions,
    	and so had bogus answers for those values.
    
    	Fix for 3366686, no reliable scrollHeight/Width reporting for overflow:hidden or
    	overflow:visible elements.  The former was caused by the same bug as 3366801.  The
    	latter was just me using the wrong method (clientWidth/Height instead of
    	overflowWidth/Height).
    
    	This patch also tightens the assignment to scrollLeft/Top to not do anything if you don't
    	have an overflow value other than visible.
    
            Reviewed by darin
    
            * khtml/ecma/kjs_dom.cpp:
            (DOMNode::putValue):
            * khtml/rendering/render_block.cpp:
            (khtml::RenderBlock::layoutBlock):
            * khtml/rendering/render_flexbox.cpp:
            (khtml::RenderFlexibleBox::layoutBlock):
            * khtml/rendering/render_layer.cpp:
            (RenderLayer::RenderLayer):
            (RenderLayer::scrollToOffset):
            (RenderLayer::scrollWidth):
            (RenderLayer::scrollHeight):
            (RenderLayer::computeScrollDimensions):
            (RenderLayer::updateScrollInfoAfterLayout):
            (RenderLayer::intersectsDamageRect):
            (RenderLayer::containsPoint):
            * khtml/rendering/render_layer.h:
            * khtml/rendering/render_object.cpp:
            (RenderObject::scrollWidth):
            (RenderObject::scrollHeight):
            * khtml/rendering/render_object.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5144 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_layer.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_layer.h
@@ -121,9 +121,9 @@ public:
 
     void setWidth(short w) { m_width = w; }
     void setHeight(int h) { m_height = h; }
-    
-    short scrollWidth() const { return m_scrollWidth; }
-    int scrollHeight() const { return m_scrollHeight; }
+
+    short scrollWidth();
+    int scrollHeight();
     
     void setPos( int xPos, int yPos ) {
         m_x = xPos;
@@ -149,7 +149,7 @@ public:
 #ifdef APPLE_CHANGES
     void paintScrollbars(QPainter* p, const QRect& damageRect);
 #endif
-    void checkScrollbarsAfterLayout();
+    void updateScrollInfoAfterLayout();
     void slotValueChanged(int);
     void updateScrollPositionFromScrollbars();
 
@@ -229,6 +229,8 @@ private:
     RenderLayer* nodeAtPointForLayer(RenderLayer* rootLayer, RenderObject::NodeInfo& info,
                                      int x, int y, const QRect& hitTestRect);
 
+    void computeScrollDimensions(bool* needHBar = 0, bool* needVBar = 0);
+    
 protected:   
     RenderObject* m_object;
     
@@ -263,6 +265,7 @@ protected:
     // The width/height of our scrolled area.
     short m_scrollWidth;
     int m_scrollHeight;
+    bool m_scrollDimensionsDirty;
     
     // For layers with overflow, we have a pair of scrollbars.
     QScrollBar* m_hBar;
--- third_party/WebKit/WebCore/khtml/rendering/render_object.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_object.cpp
@@ -386,7 +386,7 @@ RenderObject::clientWidth() const
         (layer() ? layer()->verticalScrollbarWidth() : 0);
 }
 
-short
+int
 RenderObject::clientHeight() const
 {
     return height() - borderTop() - borderBottom() -
@@ -398,13 +398,13 @@ RenderObject::clientHeight() const
 short
 RenderObject::scrollWidth() const
 {
-    return (style()->hidesOverflow() && layer()) ? layer()->scrollWidth() : clientWidth();
+    return (style()->hidesOverflow() && layer()) ? layer()->scrollWidth() : overflowWidth();
 }
 
-short
+int
 RenderObject::scrollHeight() const
 {
-    return (style()->hidesOverflow() && layer()) ? layer()->scrollHeight() : clientHeight();
+    return (style()->hidesOverflow() && layer()) ? layer()->scrollHeight() : overflowHeight();
 }
 
 bool
