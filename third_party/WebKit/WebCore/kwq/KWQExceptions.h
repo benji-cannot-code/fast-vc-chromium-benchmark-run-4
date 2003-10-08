71b54f9c9e1d7e2f2dac479d5db867f686ace503
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-08 23:27:40 +0000

    Fix development build.
            * kwq/KWQExceptions.h: Correct assertion args.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5149 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQExceptions.h
+++ third_party/WebKit/WebCore/kwq/KWQExceptions.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
      if (ASSERT_DISABLED) { \
      NSLog(@"Uncaught exception - %@\n", localException); \
      } else { \
-     ASSERT_WITH_MESSAGE(@"Uncaught exception - %@", localException); \
+     ASSERT_WITH_MESSAGE(0, "Uncaught exception - %@", localException); \
      } \
 NS_ENDHANDLER
 
