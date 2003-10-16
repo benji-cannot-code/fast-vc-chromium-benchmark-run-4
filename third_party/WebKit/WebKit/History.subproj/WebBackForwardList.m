301da323c9d20ab2da8ff00ecf565c306b8b8440
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-16 21:59:13 +0000

    Fixed 3453991.  We weren't setting the array cursor correctly after changing 	capacity.
            Reviewed by John.
    
            * ChangeLog:
            * History.subproj/WebBackForwardList.m:
            (-[WebBackForwardList setCapacity:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
+++ third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
@@ -198,6 +198,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         [_private->entries removeObjectsInRange: forwardRange];
         currSize -= forwardRange.length;
     }
+    if (_private->current > (int)([_private->entries count] - 1))
+        _private->current = [_private->entries count] - 1;
+        
     _private->maximumSize = size;
 }
 
