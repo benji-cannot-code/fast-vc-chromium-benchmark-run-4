c9ad68ebc03d68a056a987f28f8fe5ea9b44aafa
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-21 18:47:50 +0000

    WebCore:
    	Fixed: <rdar://problem/3176170>: OBJECT tag with no or empty TYPE is mishandled
    
            Reviewed by rjw.
    
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::createPart): call renamed frameRequiredForMIMEType:URL:
            * kwq/WebCoreBridge.h: renamed frameRequiredForMIMEType: to frameRequiredForMIMEType:URL:
    
    WebKit:
    
    	Fixed: <rdar://problem/3176170>: OBJECT tag with no or empty TYPE is mishandled
    
            Reviewed by rjw.
    
            * Plugins.subproj/WebPluginDatabase.m:
            (-[WebPluginDatabase pluginForExtension:]): If no plug-in is found from the extension, attempt to map from the extension to a MIME type using our mappings and find a plug-in from the MIME type. This improves our chances of finding a plug-in when no MIME type is specified.
            * WebCoreSupport.subproj/WebBridge.m:
    	(-[WebBridge viewForPluginWithURL:attributes:baseURL:MIMEType:]): If the passed MIME is empty, nil it out so that clients only need to check for nil. This avoids error sheets complaining about "" MIME types.
            (-[WebBridge frameRequiredForMIMEType:URL:]): Renamed to include URL. If no MIME is specified, only create a plug-in view if we can map from the extension.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreBridge.h
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.h
@@ -322,7 +322,7 @@ typedef enum {
 
 - (int)getObjectCacheSize;
 
-- (BOOL)frameRequiredForMIMEType:(NSString*)mimeType;
+- (BOOL)frameRequiredForMIMEType:(NSString*)MIMEType URL:(NSURL *)URL;
 
 - (void)loadEmptyDocumentSynchronously;
 
