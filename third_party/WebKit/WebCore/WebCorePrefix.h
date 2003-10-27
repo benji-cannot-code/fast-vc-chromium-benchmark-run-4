764f447bbd41dd9f91ff22a3139ecd1f07b16588
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-27 06:31:02 +0000

    Reviewed by Maciej.
            - fixed uses of NULL in non-pointer contexts
    
            * khtml/khtml_part.cpp: (findWordBoundary): Changed NULL to 0 and did a little code
            cleanup of the surrounding code.
            * khtml/rendering/break_lines.cpp: (khtml::isBreakable): Changed NULL to 0 and did
            a tiny bit of cleanup.
    
            * WebCorePrefix.h: Add a definition of NULL here that takes advantage of the GNU
            __null feature even if the system C library doesn't.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCorePrefix.h
+++ third_party/WebKit/WebCore/WebCorePrefix.h
@@ -1,4 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#ifdef __cplusplus
+#define NULL __null
+#else
+#define NULL ((void *)0)
+#endif
+
 #include <assert.h>
 #include <ctype.h>
 #include <fcntl.h>
