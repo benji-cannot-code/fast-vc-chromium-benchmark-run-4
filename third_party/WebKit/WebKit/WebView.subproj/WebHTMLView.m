39dc7aff400382378432daf9877a6bba8860d6c4
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-28 00:51:35 +0000

    - fixed 3441258 -- hysteresis to start dragging a link is too small; too easy to start drag
            Reviewed by Chris.
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView _handleMouseDragged:]):
            Split DragHysteresis into two values, one for links and one for images. Make
            the link one much larger than the image one (since dragging an image doesn't
            occur accidentally in the ways that dragging a link does).
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5280 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -43,9 +43,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/NSURL_NSURLExtras.h>
 #import <Foundation/NSURLRequestPrivate.h>
 
-// These are a little larger than typical because dragging links is a fairly
-// advanced feature that can confuse non-power-users
-#define DragHysteresis  		10.0
+// The link drag hysteresis is much larger than the others because there
+// needs to be enough space to cancel the link press without starting a link drag,
+// and because dragging links is rare.
+#define LinkDragHysteresis              40.0
+#define ImageDragHysteresis  		5.0
 #define TextDragHysteresis  		3.0
 #define TextDragDelay			0.15
 
@@ -628,7 +630,8 @@ static WebHTMLView *lastHitView = nil;
     float deltaY = ABS(mouseDraggedPoint.y - mouseDownPoint.y);
     
     // Drag hysteresis hasn't been met yet but we don't want to do other drag actions like selection.
-    if (((imageURL || linkURL) && deltaX < DragHysteresis && deltaY < DragHysteresis) ||
+    if ((imageURL && deltaX < ImageDragHysteresis && deltaY < ImageDragHysteresis) ||
+        (linkURL && deltaX < LinkDragHysteresis && deltaY < LinkDragHysteresis) ||
         (isSelectedText && deltaX < TextDragHysteresis && deltaY < TextDragHysteresis)) {
 	return;
     }
