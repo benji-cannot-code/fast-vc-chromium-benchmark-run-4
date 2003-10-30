c55bbdf56181a67611b06e82c7578be728055a75
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-30 07:31:33 +0000

    Reviewed by Darin.
    	- fixed 3467632 - Leak of plugin info visiting http://www.ebay.com
    
            * Plugins.subproj/WebBasePluginPackage.m:
            (-[WebBasePluginPackage dealloc]): Release lastModifiedDate.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebBasePluginPackage.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebBasePluginPackage.m
@@ -161,6 +161,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [extensionToMIME release];
 
     [bundle release];
+
+    [lastModifiedDate release];
     
     [super dealloc];
 }
