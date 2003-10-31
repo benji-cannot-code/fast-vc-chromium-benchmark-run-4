6e23b8f2d6f1b0911eb31db7e08e704965626aa7
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-31 00:11:44 +0000

    Fix for 3468916, outline needs its own paint action so that focus rings don't draw under other content.
            Reviewed by kocienda
    
            * khtml/rendering/render_block.cpp:
            (khtml::RenderBlock::paintObject):
            (khtml::RenderBlock::paintFloats):
            * khtml/rendering/render_image.cpp:
            (RenderImage::paintObject):
            * khtml/rendering/render_inline.cpp:
            (RenderInline::paintObject):
            * khtml/rendering/render_layer.cpp:
            (RenderLayer::paintLayer):
            * khtml/rendering/render_object.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5333 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_object.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_object.h
@@ -72,6 +72,7 @@ typedef enum {
     PaintActionChildBackgrounds,
     PaintActionFloat,
     PaintActionForeground,
+    PaintActionOutline,
     PaintActionSelection,
     PaintActionCollapsedTableBorders
 } PaintAction;
