2107215374e712523ec521e0dc21ca309d3fc9fb
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-31 19:29:02 +0000

    Fix for 3467419, highlighted text in "search in books" Amazon feature is obscured.  The 	site is using the CSS3 opacity property.  The CSS3 Color module is far enough along now 	that opacity can safely be used without the "-khtml-" in front of it.
    	Fix for 3466542, the minimum font size bug.  I am introducing a new minimum font size pref that
    	really is a hard override.
    
            Reviewed by gramps (opacity) and john (min font size)
    
            * khtml/css/cssparser.cpp:
            (CSSParser::parseValue):
            * khtml/css/cssproperties.c:
            (hash_prop):
            (findProp):
            * khtml/css/cssproperties.h:
            * khtml/css/cssproperties.in:
            * khtml/css/cssstyleselector.cpp:
            (khtml::CSSStyleSelector::applyRule):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5340 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreSettings.mm
+++ third_party/WebKit/WebCore/kwq/WebCoreSettings.mm
@@ -174,6 +174,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return minimumFontSize;
 }
 
+- (void)setMinimumLogicalFontSize:(float)size
+{
+    if (minimumLogicalFontSize == size) {
+        return;
+    }
+    minimumLogicalFontSize = size;
+    settings->setMinLogicalFontSize((int)rint(size));
+    [self _updateAllViews];
+}
+
+- (float)minimumLogicalFontSize
+{
+    return minimumLogicalFontSize;
+}
+
 - (void)setDefaultFontSize:(float)size
 {
     if (defaultFontSize == size) {
