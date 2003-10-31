564122ed03045aa4ec3c66b93c65f661de8891af
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-10-31 01:27:11 +0000

    Fixes for 3469057, outlines not drawn on aintitcool.com and also for 3469178, objects with width/height of 0 	incorrectly excluded from tabbing.
            Reviewed by darin
    
            * khtml/html/html_formimpl.cpp:
            (HTMLGenericFormElementImpl::isSelectable):
            * khtml/html/html_inlineimpl.cpp:
            (HTMLAnchorElementImpl::isSelectable):
            * khtml/rendering/render_flow.cpp:
            (RenderFlow::getAbsoluteRepaintRect):
            * khtml/rendering/render_inline.cpp:
            (RenderInline::addFocusRingRects):
            (RenderInline::paintOutline):
            * khtml/rendering/render_object.cpp:
            (RenderObject::addFocusRingRects):
            (RenderObject::getAbsoluteRepaintRectWithOutline):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5335 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_flow.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_flow.cpp
@@ -266,6 +266,11 @@ QRect RenderFlow::getAbsoluteRepaintRect()
                     r = r.unite(childRect);
                 }
             }
+            
+            if (continuation() && !continuation()->isInline()) {
+                QRect contRect = continuation()->getAbsoluteRepaintRectWithOutline(ow);
+                r = r.unite(contRect);
+            }
         }
 
         return r;
