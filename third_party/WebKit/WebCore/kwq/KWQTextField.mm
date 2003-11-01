cbfc493ee8c3da8470a6f0d95c97b83d23bab6e8
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-01 01:01:05 +0000

    Reviewed by Darin
    	Fix for this bug:
    
    	<rdar://problem/3440719>: Read-only text field form controls
    	do not keyboard focus and do not take part in tabbing
    
            * kwq/KWQTextField.mm:
            (-[KWQTextField acceptsFirstResponder]): We want our text fields
            to accept first responder even if not editable.
    
    	Once this is fixed, this one comes along for free:
    
    	<rdar://problem/3440710>: Read-only textarea form controls should select all text when focused
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5348 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextField.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextField.mm
@@ -395,6 +395,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return view;
 }
 
+- (BOOL)acceptsFirstResponder
+{
+    return [self isEnabled];
+}
+
 - (BOOL)becomeFirstResponder
 {
     if ([self passwordMode]) {
