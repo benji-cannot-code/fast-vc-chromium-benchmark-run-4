4c70324a45df72fcd777355bc7fc714260d609ef
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-02 05:37:29 +0000

    Reviewed by Maciej.
            - fixed 3470882 -- storage leaks in WebDownload code
            - fixed 3470884 -- download is always nil in downloadWindowForAuthenticationSheet: call from WebDownload
    
            * Misc.subproj/WebDownload.m:
            (-[WebDownloadInternal initWithDownload:]): Removed this method, which was never called.
            (-[WebDownloadInternal dealloc]): Added missing call to [super dealloc] to fix one cause
            of a leak of the WebDownloadInternal object itself. Removed the release of webDownload,
            which was always nil, and if it wasn't would end up causing a leak due to a reference cycle.
            (-[WebDownloadInternal download:didReceiveAuthenticationChallenge:]): Remove the use of
            webDownload, which was always nil, and instead use the download parameter passed to us,
            casting it to WebDownload, since it's guaranteed to be one.
            (-[WebDownload _setRealDelegate:]): Added. Shared by the methods below to set up the real
            delegate before calling init. The old code called init twice, causing an second call to the
            superclass's init method, which caused it to create an extra copy of its internal structure,
            as well as causing us to create two WebDownloadInternal objects.
            (-[WebDownload init]): Don't allocate a second WebDownloadInternal if _setRealDelegate already
            allocated it for us. Before we would allocate and leak an extra one each time.
            (-[WebDownload dealloc]): Added. Releases the WebDownloadInternal. This is the second cause
            of the leak of the WebDownloadInternal object.
            (-[WebDownload initWithRequest:delegate:]): Call [self _setRealDelegate:] instead of calling
            [self init] and then [_webInternal setRealDelegate:], avoiding the leaks caused by doing it
            the other way.
            (-[WebDownload _initWithLoadingConnection:request:response:delegate:proxy:]): Ditto.
            (-[WebDownload _initWithLoadingResource:request:response:delegate:proxy:]): Ditto.
            (-[WebDownload _initWithRequest:delegate:directory:]): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5355 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebDownload.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebDownload.m
@@ -5,12 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebKit/WebDownload.h>
 
-#import <Foundation/NSURLDownload.h>
 #import <Foundation/NSURLAuthenticationChallenge.h>
+#import <Foundation/NSURLDownload.h>
 #import <Foundation/NSURLDownloadPrivate.h>
+#import <WebKit/WebAssertions.h>
 #import <WebKit/WebPanelAuthenticationHandler.h>
 
 // FIXME: Remove these declarations because _initWithLoadingConnection is declared in NSURLDownloadPrivate.h
+// and _initWithLoadingResource is obsolete, once we compile only with the new Foundation.
 @interface NSURLDownload (WebDownloadCapability)
 - (id)_initWithLoadingConnection:(NSURLConnection *)connection
                          request:(NSURLRequest *)request
@@ -28,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
 @public
     id realDelegate;
-    WebDownload *webDownload;
 }
 
 - (void)setRealDelegate:(id)rd;
@@ -37,19 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation WebDownloadInternal
 
-- (id)initWithDownload:(WebDownload *)dl
-{
-    self = [super init];
-    if (self != nil) {
-	webDownload = [dl retain];
-    }
-    return self;
-}
-
 - (void)dealloc
 {
-    [webDownload release];
     [realDelegate release];
+    [super dealloc];
 }
 
 - (void)setRealDelegate:(id)rd
@@ -95,7 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     } else {
 	NSWindow *window = nil;
 	if ([realDelegate respondsToSelector:@selector(downloadWindowForAuthenticationSheet:)]) {
-	    window = [realDelegate downloadWindowForAuthenticationSheet:webDownload];
+	    window = [realDelegate downloadWindowForAuthenticationSheet:(WebDownload *)download];
 	}
 
 	[[WebPanelAuthenticationHandler sharedHandler] startAuthentication:challenge window:window];
@@ -158,37 +150,50 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-
-
 @implementation WebDownload
+
+- (void)_setRealDelegate:(id)delegate
+{
+    if (_webInternal == nil) {
+        _webInternal = [[WebDownloadInternal alloc] init];
+        [_webInternal setRealDelegate:delegate];
+    } else {
+        ASSERT(_webInternal == delegate);
+    }
+}
 
 - (id)init
 {
     self = [super init];
     if (self != nil) {
-	_webInternal = [[WebDownloadInternal alloc] init];
+        // _webInternal can be set up before init by _setRealDelegate
+	if (_webInternal == nil) {
+            _webInternal = [[WebDownloadInternal alloc] init];
+        }
     }
     return self;
 }
 
+- (void)dealloc
+{
+    [_webInternal release];
+    [super dealloc];
+}
+
 - (id)initWithRequest:(NSURLRequest *)request delegate:(id)delegate
 {
-    self = [self init];
-    [_webInternal setRealDelegate:delegate];
-    [super initWithRequest:request delegate:_webInternal];
-    return self;
+    [self _setRealDelegate:delegate];
+    return [super initWithRequest:request delegate:_webInternal];
 }
 
 - (id)_initWithLoadingConnection:(NSURLConnection *)connection
                          request:(NSURLRequest *)request
                         response:(NSURLResponse *)response
                         delegate:(id)delegate
-                           proxy:(NSURLConnectionDelegateProxy *)proxy;
+                           proxy:(NSURLConnectionDelegateProxy *)proxy
 {
-    self = [self init];
-    [_webInternal setRealDelegate:delegate];
-    [super _initWithLoadingConnection:connection request:request response:response delegate:_webInternal proxy:proxy];
-    return self;
+    [self _setRealDelegate:delegate];
+    return [super _initWithLoadingConnection:connection request:request response:response delegate:_webInternal proxy:proxy];
 }
 
 // FIXME: Remove this override because it no longer exists in newer Foundations.
@@ -196,22 +201,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                        request:(NSURLRequest *)request
                       response:(NSURLResponse *)response
                       delegate:(id)delegate
-                         proxy:(NSURLConnectionDelegateProxy *)proxy;
+                         proxy:(NSURLConnectionDelegateProxy *)proxy
 {
-    self = [self init];
-    [_webInternal setRealDelegate:delegate];
-    [super _initWithLoadingResource:connection request:request response:response delegate:_webInternal proxy:proxy];
-    return self;
+    [self _setRealDelegate:delegate];
+    return [super _initWithLoadingResource:connection request:request response:response delegate:_webInternal proxy:proxy];
 }
 
 - (id)_initWithRequest:(NSURLRequest *)request
               delegate:(id)delegate
              directory:(NSString *)directory
 {
-    self = [self init];
-    [_webInternal setRealDelegate:delegate];
-    [super _initWithRequest:request delegate:_webInternal directory:directory];
-    return self;
+    [self _setRealDelegate:delegate];
+    return [super _initWithRequest:request delegate:_webInternal directory:directory];
 }
 
 @end
