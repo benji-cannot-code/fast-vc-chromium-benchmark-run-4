791ebb079f66b3b76dd9ed2da622ec48815a9262
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-03 22:43:22 +0000

    Fix for 3472030, line-height not properly reset in all cases when the font shorthand is used.
            Reviewed by mjs
    
            * khtml/css/cssparser.cpp:
            (CSSParser::parseFont):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5369 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/css/cssparser.cpp
+++ third_party/WebKit/WebCore/khtml/css/cssparser.cpp
@@ -1514,9 +1514,10 @@ bool CSSParser::parseFont( bool important )
 	value = valueList->next();
 	if ( !value )
 	    goto invalid;
-    } else {
-	font->lineHeight = new CSSPrimitiveValueImpl( CSS_VAL_NORMAL );
     }
+    
+    if (!font->lineHeight)
+	font->lineHeight = new CSSPrimitiveValueImpl( CSS_VAL_NORMAL );
 
 //     kdDebug( 6080 ) << "  got line height current=" << valueList->currentValue << endl;
     // font family must come now
