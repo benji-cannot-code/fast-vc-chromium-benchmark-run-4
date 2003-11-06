4a2f8ff33f0479c1d57fc98fbba976f0223f41a0
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-06 00:17:20 +0000

    WebKit: 	Fixed 3029966.  Animated backgrounds specified in <BODY> don't animate 	Fixed 3474824.  Tiled animated GIFs don't animate. 	Fixed 3029966.  Animated backgrounds specified with CSS don't animate.
            Reviewed by Chris.
    
            * WebCoreSupport.subproj/WebImageRenderer.h:
            * WebCoreSupport.subproj/WebImageRenderer.m:
            (-[WebImageRenderer nextFrame:]):
            (-[WebImageRenderer drawImageInRect:fromRect:]):
            (-[WebImageRenderer startAnimationIfNecessary]):
            (-[WebImageRenderer tileInRect:fromPoint:]):
            * WebView.subproj/WebImageView.m:
            (-[WebImageView drawRect:]):
    
    WebCore:
    	Fixed 3474957.  Changed name of image rendering method to better reflect it's actual function.
    
    	- (void)beginAnimationInRect:(NSRect)ir fromRect:(NSRect)fr;
    
    	renamed to
    
    	- (void)drawImageInRect:(NSRect)ir fromRect:(NSRect)fr;
    
    
            Reviewed by Chris.
    
            * kwq/KWQPainter.mm:
            (QPainter::drawPixmap):
            * kwq/WebCoreImageRenderer.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5396 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreImageRenderer.h
+++ third_party/WebKit/WebCore/kwq/WebCoreImageRenderer.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSSize)size;
 - (void)resize:(NSSize)s;
-- (void)beginAnimationInRect:(NSRect)ir fromRect:(NSRect)fr;
+- (void)drawImageInRect:(NSRect)ir fromRect:(NSRect)fr;
 - (void)stopAnimation;
 - (void)tileInRect:(NSRect)r fromPoint:(NSPoint)p;
 - (BOOL)isNull;
--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.h
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.h
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     int repetitionsComplete;
     BOOL animationFinished;
+    
+    NSPoint tilePoint;
+    BOOL animatedTile;
 
     NSString *MIMEType;
     BOOL isNull;
