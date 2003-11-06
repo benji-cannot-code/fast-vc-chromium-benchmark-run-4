53b72abad66c842b0b78326fcf78d8ea5c776836
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-06 06:26:16 +0000

    Reviewed by Maciej.
            - fixed 3475109 -- support keyboard event object properties of altkey, ctrlKey, shiftKey
    
            We had the DOM Level 3 KeyboardEvent class already; I just had to add JavaScript bindings.
    
            * khtml/ecma/kjs_events.h: Added DOMKeyboardEvent class.
            * khtml/ecma/kjs_events.cpp:
            (KJS::getDOMEvent): Added code to make a DOMKeyboardEvent if the event is a DOM::KeyboardEvent.
            (DOMKeyboardEvent::~DOMKeyboardEvent): Added.
            (DOMKeyboardEvent::classInfo): Added.
            (DOMKeyboardEvent::tryGet): Added.
            (DOMKeyboardEvent::getValueProperty): Added.
            (DOMKeyboardEventProtoFunc::tryCall): Added.
    
            * khtml/dom/dom2_events.cpp:
            (KeyboardEvent::keyIdentifier): Added.
            (KeyboardEvent::keyLocation): Added.
    
            * khtml/ecma/kjs_events.lut.h: Regenerated.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/dom/dom2_events.cpp
+++ third_party/WebKit/WebCore/khtml/dom/dom2_events.cpp
@@ -634,7 +634,7 @@ void MutationEvent::initMutationEvent(const DOMString &typeArg,
 
 // -----------------------------------------------------------------------------
 
-KeyboardEvent::KeyboardEvent() : UIEvent()
+KeyboardEvent::KeyboardEvent()
 {
 }
 
@@ -642,9 +642,9 @@ KeyboardEvent::KeyboardEvent(const KeyboardEvent &other) : UIEvent(other)
 {
 }
 
-KeyboardEvent::KeyboardEvent(const Event &other) : UIEvent()
+KeyboardEvent::KeyboardEvent(const Event &other)
 {
-    (*this)=other;
+    *this = other;
 }
 
 KeyboardEvent::KeyboardEvent(KeyboardEventImpl *impl) : UIEvent(impl)
@@ -671,6 +671,22 @@ KeyboardEvent &KeyboardEvent::operator = (const Event &other)
 
 KeyboardEvent::~KeyboardEvent()
 {
+}
+
+DOMString KeyboardEvent::keyIdentifier() const
+{
+    if (!impl)
+	throw DOMException(DOMException::INVALID_STATE_ERR);
+
+    return static_cast<KeyboardEventImpl*>(impl)->keyIdentifier();
+}
+
+unsigned long KeyboardEvent::keyLocation() const
+{
+    if (!impl)
+	throw DOMException(DOMException::INVALID_STATE_ERR);
+
+    return static_cast<KeyboardEventImpl*>(impl)->keyLocation();
 }
 
 bool KeyboardEvent::ctrlKey() const
