8c80664c20c11f76072865fcd0b82200372aba11
vicki <vicki@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-11 18:34:40 +0000

    Reviewed by sullivan.
    	- rolled Merlot build fix onto the trunk
    
            * WebCorePrefix.h: added #include <sys/types.h>
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5452 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCorePrefix.h
+++ third_party/WebKit/WebCore/WebCorePrefix.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <assert.h>
 #include <ctype.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <pthread.h>
 #include <regex.h>
