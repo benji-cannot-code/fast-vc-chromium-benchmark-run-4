43dff5c7324af78c161dde4da2122369b3893da2
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-12 01:30:11 +0000

    Reviewed by John.
    	- fixed 3479537 - reproducible crash in KJS::WindowFunc::tryCall
    
            * khtml/ecma/kjs_window.cpp:
            (WindowFunc::tryCall): Check part and doc impl for null.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_window.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_window.cpp
@@ -1081,7 +1081,8 @@ Value WindowFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
 
   switch (id) {
   case Window::Alert:
-    part->xmlDocImpl()->updateRendering();
+    if (part && part->xmlDocImpl())
+      part->xmlDocImpl()->updateRendering();
 #if APPLE_CHANGES
     KWQ(part)->runJavaScriptAlert(str);
 #else
@@ -1089,7 +1090,8 @@ Value WindowFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
 #endif
     return Undefined();
   case Window::Confirm:
-    part->xmlDocImpl()->updateRendering();
+    if (part && part->xmlDocImpl())
+      part->xmlDocImpl()->updateRendering();
 #if APPLE_CHANGES
     return Boolean(KWQ(part)->runJavaScriptConfirm(str));
 #else
@@ -1097,7 +1099,8 @@ Value WindowFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
                                                 i18n("OK"), i18n("Cancel")) == KMessageBox::Yes));
 #endif
   case Window::Prompt:
-    part->xmlDocImpl()->updateRendering();
+    if (part && part->xmlDocImpl())
+      part->xmlDocImpl()->updateRendering();
     bool ok;
 #if APPLE_CHANGES
     ok = KWQ(part)->runJavaScriptPrompt(str, args.size() >= 2 ? args[1].toString(exec).qstring() : QString::null, str2);
