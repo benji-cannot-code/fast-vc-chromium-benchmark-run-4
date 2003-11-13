91cb645900662422308cf73b1b8e7e48fed60a22
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-13 01:56:13 +0000

    Added utility functions for calling JNI methods.
            Reviewed by Chris.
    
            * JavaScriptCore.pbproj/project.pbxproj:
            * Makefile.am:
            * bindings/Makefile.am: Added.
            * bindings/jni/Makefile.am: Added.
            * bindings/jni/jni_utility.cpp: Added.
            (attachToJavaVM):
            (callJNIMethod):
            (callJNIVoidMethod):
            (callJNIObjectMethod):
            (callJNIByteMethod):
            (callJNICharMethod):
            (callJNIShortMethod):
            (callJNIIntMethod):
            (callJNILongMethod):
            (callJNIFloatMethod):
            (callJNIDoubleMethod):
            * bindings/jni/jni_utility.h: Added.
            * bindings/objc/Makefile.am: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5473 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/JavaScriptCore/bindings/jni/jni_utility.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#include <JavaVM/jni.h>
+
+jobject callJNIObjectMethod( jobject obj, const char *name, const char *sig, ... );
+void callJNIVoidMethod( jobject obj, const char *name, const char *sig, ... );
+jboolean callJNIBooleanMethod( jobject obj, const char *name, const char *sig, ... );
+jbyte callJNIByteMethod( jobject obj, const char *name, const char *sig, ... );
+jchar callJNICharMethod( jobject obj, const char *name, const char *sig, ... );
+jshort callJNIShortMethod( jobject obj, const char *name, const char *sig, ... );
+jint callJNIIntMethod( jobject obj, const char *name, const char *sig, ... );
+jlong callJNILongMethod( jobject obj, const char *name, const char *sig, ... );
+jfloat callJNIFloatMethod( jobject obj, const char *name, const char *sig, ... );
+jdouble callJNIDoubleMethod( jobject obj, const char *name, const char *sig, ... );
