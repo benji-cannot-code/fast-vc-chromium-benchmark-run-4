fb7aa40c723d099acb406e18405a8492058c2a63
vicki <vicki@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-14 20:05:37 +0000

    - rolled out Darin's fixes for 3457162 and 3160035, since these changes broke plugins on macromedia.com and disney.go.com
            * WebView.subproj/WebBaseResourceHandleDelegate.m:
            (-[WebBaseResourceHandleDelegate setDataSource:]):
            * WebView.subproj/WebDataSource.m:
            (-[WebDataSource _addSubresourceClient:]):
            * WebView.subproj/WebMainResourceClient.h:
            * WebView.subproj/WebMainResourceClient.m:
            (-[WebMainResourceClient dealloc]):
            (-[WebMainResourceClient loadWithRequest:]):
            (-[WebMainResourceClient setDefersCallbacks:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebBaseResourceHandleDelegate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebBaseResourceHandleDelegate.m
@@ -156,8 +156,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     [downloadDelegate release];
     downloadDelegate = [[webView downloadDelegate] retain];
-
-    [self setDefersCallbacks:[webView defersCallbacks]];
 }
 
 - (WebDataSource *)dataSource
--- third_party/WebKit/WebKit/WebView.subproj/WebMainResourceClient.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebMainResourceClient.m
@@ -51,8 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)dealloc
 {
-    [_initialRequest release];
-
     [proxy setDelegate:nil];
     [proxy release];
     
@@ -314,16 +312,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self receivedError:error];
 }
 
-- (void)loadWithRequestNow:(NSURLRequest *)r
+- (BOOL)loadWithRequest:(NSURLRequest *)r
 {
     ASSERT(connection == nil);
-    ASSERT(![self defersCallbacks]);
-    ASSERT(![[dataSource _webView] defersCallbacks]);
     
     // Send this synthetic delegate callback since clients expect it, and
     // we no longer send the callback from within NSURLConnection for
     // initial requests.
-    r = [self connection:nil willSendRequest:r redirectResponse:nil];
+    r = [proxy connection:nil willSendRequest:r redirectResponse:nil];
 
     NSURL *URL = [r URL];
     BOOL shouldLoadEmpty = [URL _webkit_shouldLoadAsEmptyDocument];
@@ -335,25 +331,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             MIMEType = [WebView _generatedMIMETypeForURLScheme:[URL scheme]];
         }
 
-        NSURLResponse *resp = [[NSURLResponse alloc] initWithURL:URL MIMEType:MIMEType
+	NSURLResponse *resp = [[NSURLResponse alloc] initWithURL:URL MIMEType:MIMEType
             expectedContentLength:0 textEncodingName:nil];
 	[self connection:nil didReceiveResponse:resp];
 	[resp release];
     } else {
         connection = [[NSURLConnection alloc] initWithRequest:r delegate:proxy];
-    }
-}
-
-- (BOOL)loadWithRequest:(NSURLRequest *)r
-{
-    ASSERT(connection == nil);
-    
-    if (![self defersCallbacks]) {
-        [self loadWithRequestNow:r];
-    } else {
-        NSURLRequest *copy = [r copy];
-        [_initialRequest release];
-        _initialRequest = copy;
+        if ([self defersCallbacks]) {
+            [connection setDefersCallbacks:YES];
+        }
     }
 
     return YES;
@@ -361,15 +347,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setDefersCallbacks:(BOOL)defers
 {
-    [super setDefersCallbacks:defers];
-    if (!defers) {
-        NSURLRequest *r = _initialRequest;
-        if (r != nil) {
-            _initialRequest = nil;
-            [self loadWithRequestNow:r];
-            [r release];
-        }
+    if (request
+            && ![[request URL] _webkit_shouldLoadAsEmptyDocument]
+            && ![WebView _representationExistsForURLScheme:[[request URL] scheme]]) {
+	[super setDefersCallbacks:defers];
     }
 }
 
+
 @end
+
