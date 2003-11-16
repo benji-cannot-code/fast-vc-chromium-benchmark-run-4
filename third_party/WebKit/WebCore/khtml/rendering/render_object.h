6def9569ec6fb285e235568d3f1a8deea3e0984a
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-16 20:48:12 +0000

    3485717, Cleanup of the float code.  Eliminated the -khtml-flow-mode style property in favor of two simple methods, 	avoidsFloats() and usesLineWidth().  avoidsFloats can be called to tell whether an object does not want 	floats to intrude into its space (and that it should also move to avoid floats).  usesLineWidth is called 	to find out if an object's containing block width is the line width or the content width, and has all the 	right smarts to emulate WinIE's behaviors in both quirks and strict mode.
    	Also fixed a bug (3485718) where overflow:scroll blocks with auto height did not factor in the horizontal scrollbar's
    	height (thus causing it to paint on top of content).
    
            Reviewed by darin
    
            * khtml/css/cssparser.cpp:
            (CSSParser::parseValue):
            * khtml/css/cssstyleselector.cpp:
            (khtml::CSSStyleSelector::applyRule):
            * khtml/css/cssvalues.c:
            (hash_val):
            (findValue):
            * khtml/css/cssvalues.h:
            * khtml/css/cssvalues.in:
            * khtml/css/html4.css:
            * khtml/rendering/bidi.cpp:
            (khtml::RenderBlock::layoutInlineChildren):
            * khtml/rendering/render_block.cpp:
            (khtml::RenderBlock::layoutBlock):
            (khtml::RenderBlock::layoutBlockChildren):
            (khtml::RenderBlock::clearFloats):
            * khtml/rendering/render_box.cpp:
            (RenderBox::containingBlockWidth):
            (RenderBox::calcWidth):
            * khtml/rendering/render_object.cpp:
            (RenderObject::isHR):
            * khtml/rendering/render_object.h:
            (khtml::RenderObject::avoidsFloats):
            (khtml::RenderObject::usesLineWidth):
            * khtml/rendering/render_style.h:
            (khtml::RenderStyle::NonInheritedFlags::operator==):
            (khtml::RenderStyle::setBitDefaults):
            (khtml::RenderStyle::initialTextDecoration):
            * khtml/rendering/render_table.cpp:
            (RenderTable::calcWidth):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5511 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_object.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_object.h
@@ -224,6 +224,7 @@ public:
     virtual bool isCanvas() const { return false; }
     bool isRoot() const;
     bool isBody() const;
+    bool isHR() const;
     virtual bool isBR() const { return false; }
     virtual bool isTableCell() const { return false; }
     virtual bool isTableRow() const { return false; }
@@ -633,7 +634,10 @@ public:
     virtual bool containsFloat(RenderObject* o) { return false; }
     virtual bool hasOverhangingFloats() { return false; }
 
-    // positioning of inline childs (bidi)
+    bool avoidsFloats() const;
+    bool usesLineWidth() const;
+
+    // positioning of inline children (bidi)
     virtual void position(InlineBox*, int, int, bool) {}
 
     enum SelectionState {
--- third_party/WebKit/WebCore/khtml/rendering/render_style.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_style.h
@@ -728,7 +728,6 @@ protected:
             (_table_layout == other._table_layout) &&
             (_page_break_before == other._page_break_before) &&
             (_page_break_after == other._page_break_after) &&
-            (_flowAroundFloats == other._flowAroundFloats) &&
             (_styleType == other._styleType) &&
             (_affectedByHover == other._affectedByHover) &&
             (_affectedByActive == other._affectedByActive) &&
@@ -753,8 +752,6 @@ protected:
         
         EPageBreak _page_break_before : 2;
         EPageBreak _page_break_after : 2;
-        
-        bool _flowAroundFloats :1;
 
         PseudoId _styleType : 3;
         bool _affectedByHover : 1;
@@ -816,7 +813,6 @@ protected:
 	noninherited_flags._table_layout = initialTableLayout();
         noninherited_flags._page_break_before = initialPageBreak();
         noninherited_flags._page_break_after = initialPageBreak();
-	noninherited_flags._flowAroundFloats = initialFlowAroundFloats();
 	noninherited_flags._styleType = NOPSEUDO;
         noninherited_flags._affectedByHover = false;
         noninherited_flags._affectedByActive = false;
@@ -1142,9 +1138,6 @@ public:
     bool htmlHacks() const { return inherited_flags._htmlHacks; }
     void setHtmlHacks(bool b=true) { inherited_flags._htmlHacks = b; }
 
-    bool flowAroundFloats() const { return  noninherited_flags._flowAroundFloats; }
-    void setFlowAroundFloats(bool b=true) {  noninherited_flags._flowAroundFloats = b; }
-
     bool hasAutoZIndex() { return box->z_auto; }
     void setHasAutoZIndex() { SET_VAR(box, z_auto, true) }
     int zIndex() const { return box->z_index; }
@@ -1246,7 +1239,6 @@ public:
     static Length initialLineHeight() { return Length(-100, Percent); }
     static ETextAlign initialTextAlign() { return TAAUTO; }
     static ETextDecoration initialTextDecoration() { return TDNONE; }
-    static bool initialFlowAroundFloats() { return false; }
     static int initialOutlineOffset() { return 0; }
     static float initialOpacity() { return 1.0f; }
     static EBoxAlignment initialBoxAlign() { return BSTRETCH; }
