3d13f0b96f41099ba346dbebca767a1139358a85
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-16 21:25:23 +0000

    WebCore:
            Reviewed by John.
    
    	- fixed 3362841 - javascript History Object length property is always 0
    
            * kwq/KWQKPartsBrowserInterface.mm:
            (KParts::BrowserInterface::property): For the historyLength property,
    	retturn a UInt variant, not Int, because that's what the kjs_window code
    	expects.
    
    WebKit:
    
            Reviewed by John.
    
    	- fixed 3362841 - javascript History Object length property is always 0
    
    	* WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge historyLength]): Add one to the length to match other browsers.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5522 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -880,7 +880,7 @@ static BOOL loggedObjectCacheSize = NO;
 
 - (int)historyLength
 {
-    return [[[_frame webView] backForwardList] backListCount];
+    return [[[_frame webView] backForwardList] backListCount] + 1;
 }
 
 - (void)goBackOrForward:(int)distance
