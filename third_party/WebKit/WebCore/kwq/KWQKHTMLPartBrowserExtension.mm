d28ff7f3d2b7167c4d94cbbe260a2b8018a82de5
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-17 05:34:36 +0000

    Reviewed by Darin.
            * kwq/KWQKHTMLPartBrowserExtension.mm:
            (KHTMLPartBrowserExtension::createNewWindow): Check for the
    	returned part being NULL, not the out value pointer.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5534 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKHTMLPartBrowserExtension.mm
+++ third_party/WebKit/WebCore/kwq/KWQKHTMLPartBrowserExtension.mm
@@ -134,10 +134,13 @@ void KHTMLPartBrowserExtension::createNewWindow(const KURL &url,
     }
     
     [bridge showWindow];
+
+    if ([bridge part]) {
+	[bridge part]->setName(urlArgs.frameName);
+    }
     
     if (partResult) {
 	*partResult = [bridge part];
-	(*partResult)->setName(urlArgs.frameName);
     }
     return;
 
