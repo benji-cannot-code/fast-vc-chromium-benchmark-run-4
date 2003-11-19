5f511ccc0b2733548ec02a2b22c505ff5b11131a
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-19 22:21:40 +0000

    WebKit: 	More LiveConnect stuff.  Horrible polling hack that 	blocks main thread waiting for applet to fully initialize.
            Reviewed by Ken.
    
            * Plugins.subproj/WebPluginController.m:
            (-[WebPluginController addPlugin:]):
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge pollForAppletInView:]):
    
    WebCore:
    	More LiveConnect stuff.  With that patch we make the applet
    	visible to JavaScript in an Bindings::Instance.  Next step
    	it to flush out implementation of RuntimeObjectImp and
    	RuntimeFunctionImp.
    
            Reviewed by Ken.
    
            * khtml/html/html_objectimpl.cpp:
            (HTMLAppletElementImpl::HTMLAppletElementImpl):
            (HTMLAppletElementImpl::~HTMLAppletElementImpl):
            (HTMLAppletElementImpl::getAppletInstance):
            * khtml/html/html_objectimpl.h:
            * kwq/KWQKHTMLPart.h:
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::getAppletInstanceForView):
            * kwq/WebCoreBridge.h:
    
    JavaScript:
    	More LiveConnect stuff.
    
            Reviewed by Ken.
    
            * JavaScriptCore.pbproj/project.pbxproj:
            * bindings/jni/jni_class.cpp: Added.
            (JavaClass::_commonInit):
            (JavaClass::JavaClass):
            (_createClassesByNameIfNecessary):
            (JavaClass::classForName):
            (JavaClass::classForInstance):
            (JavaClass::methodNamed):
            (JavaClass::fieldNamed):
            * bindings/jni/jni_class.h: Added.
            (Bindings::JavaClass::_commonDelete):
            (Bindings::JavaClass::~JavaClass):
            (Bindings::JavaClass::_commonCopy):
            (Bindings::JavaClass::JavaClass):
            (Bindings::JavaClass::operator=):
            (Bindings::JavaClass::name):
            (Bindings::JavaClass::constructorAt):
            (Bindings::JavaClass::numConstructors):
            * bindings/jni/jni_instance.cpp: Added.
            (JavaInstance::JavaInstance):
            (JavaInstance::~JavaInstance):
            * bindings/jni/jni_instance.h: Added.
            (Bindings::JObjectWrapper::JObjectWrapper):
            (Bindings::JObjectWrapper::~JObjectWrapper):
            (Bindings::JObjectWrapper::ref):
            (Bindings::JObjectWrapper::deref):
            (Bindings::JavaInstance::getClass):
            (Bindings::JavaInstance::operator=):
            * bindings/jni/jni_runtime.cpp:
            (JavaMethod::JavaMethod):
            * bindings/jni/jni_runtime.h:
            (Bindings::JavaString::JavaString):
            (Bindings::JavaString::~JavaString):
            (Bindings::JavaString::operator=):
            * bindings/jni/jni_utility.cpp:
            (getJavaVM):
            (getJNIEnv):
            (getCharactersFromJString):
            (releaseCharactersForJString):
            (getCharactersFromJStringInEnv):
            (releaseCharactersForJStringInEnv):
            * bindings/jni/jni_utility.h:
            * bindings/runtime.cpp:
            (Instance::createBindingForLanguageInstance):
            * bindings/runtime.h:
            (Bindings::Instance::):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPluginController.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPluginController.m
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/NSURL_NSURLExtras.h>
 #import <Foundation/NSURLRequest.h>
 
-@interface NSView (WebInternal)
-- (void *)getApplet;
+@interface NSView (PluginSecrets)
+- (void)setContainingWindow:(NSWindow *)w;
 @end
 
 @implementation WebPluginController
@@ -73,25 +73,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     
     if (![_views containsObject:view]) {
         [_views addObject:view];
-
+        
         LOG(Plugins, "initializing plug-in %@", view);
         [view pluginInitialize];
 
         if (_started) {
             LOG(Plugins, "starting plug-in %@", view);
             [view pluginStart];
-
-            [self performSelector:@selector(_delayedGetApplet:) withObject:view afterDelay: 4.0];
+            
+            if ([view respondsToSelector:@selector(setContainingWindow:)])
+                [view setContainingWindow:[_HTMLView window]];
         }
     }
-}
-
-// Temporary hack until we add notification from plugin that applet has been
-// activated.
-- (void)_delayedGetApplet: (NSView *)view
-{
-    if ([view respondsToSelector: @selector(getApplet)])
-        NSLog (@"%@ getApplet = %p\n", view, [view getApplet]);
 }
 
 - (void)destroyAllPlugins
