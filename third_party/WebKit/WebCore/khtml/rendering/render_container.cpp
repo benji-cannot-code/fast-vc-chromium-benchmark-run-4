1d3b157d884680e07cc943796901cb86cf954051
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-20 04:28:38 +0000

    Fix for 3486963, crash because generated content was referencing a deleted object.  Fix is to update the 	variable to a sane value after deleting old generated content.
            Reviewed by darin
    
            * khtml/rendering/render_container.cpp:
            (RenderContainer::updatePseudoChild):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5596 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_container.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_container.cpp
@@ -243,6 +243,7 @@ void RenderContainer::updatePseudoChild(RenderStyle::PseudoId type, RenderObject
         if (child && child->style()->styleType() == type) {
             oldContentPresent = false;
             removeChild(child);
+            child = (type == RenderStyle::BEFORE) ? firstChild() : lastChild();
         }
     }
 
