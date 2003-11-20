cfa65e292416f2a97e75467ebefc494174984170
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-20 00:46:23 +0000

    Reviewed by John.
    	- fixed 3486998 - Implement asynchronous http loading for XMLHttpRequest
    
            * khtml/ecma/xmlhttprequest.cpp:
            (KJS::XMLHttpRequestQObject::XMLHttpRequestQObject): New QObject to use to
    	connect to signals.
            (KJS::XMLHttpRequestQObject::slotData): Call XMLHttpRequest.
            (KJS::XMLHttpRequestQObject::slotFinished): Ditto.
            (KJS::XMLHttpRequestQObject::slotRedirection): Ditto.
            (KJS::XMLHttpRequest::getValueProperty): Implemented, onrequeststatechange,
    	readyState and responseText.
            (KJS::XMLHttpRequest::putValue): Implemented onrequeststatechange.
            (KJS::XMLHttpRequest::XMLHttpRequest): Initialize new fields.
            (KJS::XMLHttpRequest::~XMLHttpRequest): Delete decoder.
            (KJS::XMLHttpRequest::changeState): Update state, and send event if needed.
            (KJS::XMLHttpRequest::open): Save the parameters.
            (KJS::XMLHttpRequest::send): Fire up a post or get job.
            (KJS::XMLHttpRequest::slotFinished): go to final state.
            (KJS::XMLHttpRequest::slotRedirection): Just stub for now.
            (KJS::XMLHttpRequest::slotData): Update state; decode & save data.
            (KJS::XMLHttpRequestProtoFunc::tryCall): Implemented open and send.
            * khtml/ecma/xmlhttprequest.h: Prototyped new stuff.
            * khtml/xml/dom2_eventsimpl.cpp:
            (EventImpl::typeToId): Added "readystatechange" event.
            (EventImpl::idToType): Ditto.
            * khtml/xml/dom2_eventsimpl.h: Ditto.
            * kwq/KWQLoader.h:
            * kwq/KWQLoader.mm:
            (KWQServeRequest): New variant that doesn't need a Request.
            * kwq/KWQSlot.mm:
            (KWQSlot::KWQSlot): Handle XMLHttpRequestQObject's slots.
            (KWQSlot::call): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQSlot.mm
+++ third_party/WebKit/WebCore/kwq/KWQSlot.mm
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "kjs_window.h"
 #import "render_form.h"
 #import "render_layer.h"
+#import "xmlhttprequest.h"
 
 using DOM::DocumentImpl;
 using khtml::CachedObject;
@@ -48,6 +49,7 @@ using khtml::RenderWidget;
 using khtml::RenderScrollMediator;
 using KIO::Job;
 using KJS::WindowQObject;
+using KJS::XMLHttpRequestQObject;
 
 enum FunctionNumber {
     signalFinishedParsing,
@@ -72,10 +74,13 @@ enum FunctionNumber {
     slotTextChangedWithString_RenderFileButton,
     slotValueChanged,
     slotWidgetDestructed,
-    slotData,
-    slotRedirection,
+    slotData_Loader,
+    slotData_XMLHttpRequest,
+    slotRedirection_KHTMLPart,
+    slotRedirection_XMLHttpRequest,
     slotFinished_KHTMLPart,
     slotFinished_Loader,
+    slotFinished_XMLHttpRequest,
     slotReceivedResponse
 };
 
@@ -126,17 +131,27 @@ KWQSlot::KWQSlot(QObject *object, const char *member)
 	    m_function = slotTextChangedWithString_RenderFileButton;
 	}
     } else if (KWQNamesMatch(member, SLOT(slotData(KIO::Job *, const char *, int)))) {
-	ASSERT(dynamic_cast<khtml::Loader *>(object));
-	m_function = slotData;
+	ASSERT(dynamic_cast<Loader *>(object) || dynamic_cast<XMLHttpRequestQObject *>(object));
+	if (dynamic_cast<Loader *>(object)) {
+	    m_function = slotData_Loader;
+	} else {
+	    m_function = slotData_XMLHttpRequest;
+	}
     } else if (KWQNamesMatch(member, SLOT(slotRedirection(KIO::Job *, const KURL&)))) {
-	ASSERT(dynamic_cast<KHTMLPart *>(object));
-	m_function = slotRedirection;
+	ASSERT(dynamic_cast<KHTMLPart *>(object) || dynamic_cast<XMLHttpRequestQObject *>(object));
+	if (dynamic_cast<KHTMLPart *>(object)) {
+	    m_function = slotRedirection_KHTMLPart;
+	} else {
+	    m_function = slotRedirection_XMLHttpRequest;
+	}
     } else if (KWQNamesMatch(member, SLOT(slotFinished(KIO::Job *)))) {
-	ASSERT(dynamic_cast<khtml::Loader *>(object) || dynamic_cast<KHTMLPart *>(object));
+	ASSERT(dynamic_cast<khtml::Loader *>(object) || dynamic_cast<KHTMLPart *>(object) || dynamic_cast<XMLHttpRequestQObject *>(object));
 	if (dynamic_cast<khtml::Loader *>(object)) {
 	    m_function = slotFinished_Loader;
-	} else {
+	} else if (dynamic_cast<KHTMLPart *>(object)) {
 	    m_function = slotFinished_KHTMLPart;
+	} else {
+	    m_function = slotFinished_XMLHttpRequest;
 	}
     } else if (KWQNamesMatch(member, SLOT(slotReceivedResponse(KIO::Job *, void *)))) {
 	ASSERT(dynamic_cast<khtml::Loader *>(object));
@@ -254,6 +269,9 @@ void KWQSlot::call(Job *job) const
         case slotFinished_Loader:
             static_cast<Loader *>(m_object.pointer())->slotFinished(job);
             return;
+        case slotFinished_XMLHttpRequest:
+            static_cast<XMLHttpRequestQObject *>(m_object.pointer())->slotFinished(job);
+            return;
     }
     
     call();
@@ -266,8 +284,11 @@ void KWQSlot::call(Job *job, const char *data, int size) const
     }
     
     switch (m_function) {
-        case slotData:
+        case slotData_Loader:
 	    static_cast<Loader *>(m_object.pointer())->slotData(job, data, size);
+	    return;
+        case slotData_XMLHttpRequest:
+	    static_cast<XMLHttpRequestQObject *>(m_object.pointer())->slotData(job, data, size);
 	    return;
     }
 
@@ -281,8 +302,11 @@ void KWQSlot::call(Job *job, const KURL &url) const
     }
     
     switch (m_function) {
-        case slotRedirection:
+        case slotRedirection_KHTMLPart:
 	    static_cast<KHTMLPart *>(m_object.pointer())->slotRedirection(job, url);
+	    return;
+        case slotRedirection_XMLHttpRequest:
+	    static_cast<XMLHttpRequestQObject *>(m_object.pointer())->slotRedirection(job, url);
 	    return;
     }
 
