cd245f92240defbb3a5769e5335ef3914cc4e6e4
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-11-21 00:53:22 +0000

    Reviewed by Darin.
    	- fixed 3487201 - Implement abort for XMLHttpRequest async loads
    
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge startLoadingResource:withURL:postData:]):
            * WebCoreSupport.subproj/WebSubresourceClient.h:
            * WebCoreSupport.subproj/WebSubresourceClient.m:
            (+[WebSubresourceClient startLoadingResource:withRequest:referrer:forDataSource:]):
            (+[WebSubresourceClient startLoadingResource:withURL:referrer:forDataSource:]):
            (+[WebSubresourceClient startLoadingResource:withURL:postData:referrer:forDataSource:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -378,6 +378,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                         forDataSource:[self dataSource]];
 }
 
+- (id <WebCoreResourceHandle>)startLoadingResource:(id <WebCoreResourceLoader>)resourceLoader withURL:(NSURL *)URL postData:(NSData *)data
+{
+    // If we are no longer attached to a WebView, this must be an attempted load from an
+    // onUnload handler, so let's just block it.
+    if ([[self dataSource] _webView] == nil) {
+	return nil;
+    }
+
+    return [WebSubresourceClient startLoadingResource:resourceLoader
+                                              withURL:URL
+				             postData:data
+                                             referrer:[self referrer]
+                                        forDataSource:[self dataSource]];
+}
+
 - (void)objectLoadedFromCacheWithURL:(NSURL *)URL response: response size:(unsigned)bytes
 {
     ASSERT(_frame != nil);
--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebSubresourceLoader.h
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebSubresourceLoader.h
@@ -21,4 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (WebSubresourceClient *)startLoadingResource:(id <WebCoreResourceLoader>)rLoader
     withURL:(NSURL *)URL referrer:(NSString *)referrer forDataSource:(WebDataSource *)source;
 
++ (WebSubresourceClient *)startLoadingResource:(id <WebCoreResourceLoader>)rLoader
+    withURL:(NSURL *)URL postData:(NSData *)data referrer:(NSString *)referrer forDataSource:(WebDataSource *)source;
+
 @end
