00d170d8e45908dd7ac2d3cfeb832faa39680463
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-01 19:05:21 +0000

    WebKit:
            Reviewed by John
    
    	Fix for this bug:
    
    	<rdar://problem/3496873>: Move key event helper functions to WebKit
    
            * Misc.subproj/WebNSEventExtras.h: Add declarations for new key event
    	helpers.
            * Misc.subproj/WebNSEventExtras.m:
            (-[NSEvent _web_isKeyEvent:]): Added.
            (-[NSEvent _web_isDeleteKeyEvent]): Added.
            (-[NSEvent _web_isEscapeKeyEvent]): Added.
            (-[NSEvent _web_isOptionTabKeyEvent]): Added.
            (-[NSEvent _web_isReturnOrEnterKeyEvent]): Added.
            (-[NSEvent _web_isTabKeyEvent]): Added.
            * WebKit.pbproj/project.pbxproj: Made WebNSEventExtras.h a private header
    	so WebBrowser can use the new helpers.
    
    WebBrowser:
    
            Reviewed by John
    
    	Fix for this bug:
    
    	<rdar://problem/3496873>: Move key event helper functions to WebKit
    
            * BookmarksViewController.m:
            (-[BookmarksViewController handleKeyDown:inSourceColumn:]): Move
    	to new key event helper.
            * BrowserNSEventExtras.h: Remove key event helpers from here. WebBrowser
    	will now pick these up from WebKit.
            * BrowserNSEventExtras.m: Ditto.
            * BrowserWindowController.m: Move to new key event helpers.
            (-[BrowserWindowController cancel:]): Ditto.
            (-[BrowserWindowController windowWillHandleKeyEvent:]): Ditto.
            (-[BrowserWindowController performQuickSearch:]): Ditto.
            * DownloadViewController.m:
            (-[DownloadViewController keyDown:]): Ditto.
            * Preferences.subproj/SheetWithTableController.m:
            (-[SheetWithTableController tableView:keyDown:]): Ditto.
            * TitleBarButton.m: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebNSEventExtras.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebNSEventExtras.h
@@ -8,6 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface NSEvent (WebExtras)
 
+-(BOOL)_web_isKeyEvent:(unichar)key;
+
+-(BOOL)_web_isDeleteKeyEvent;
+-(BOOL)_web_isEscapeKeyEvent;
+-(BOOL)_web_isOptionTabKeyEvent;
+-(BOOL)_web_isReturnOrEnterKeyEvent;
 -(BOOL)_web_isTabKeyEvent;
 
 @end
