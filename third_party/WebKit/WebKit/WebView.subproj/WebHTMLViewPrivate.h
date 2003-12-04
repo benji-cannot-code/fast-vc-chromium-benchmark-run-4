59a030562aa02b7c0a26c8cc1a295761f5f6443c
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-04 16:28:01 +0000

    Reviewed by Ken.
            - fixed 3498426: assertion failure in tooltip code at macosx.apple.com
    
            * WebView.subproj/WebHTMLView.m: (-[WebHTMLView _setToolTip:]): Work around
            the apparent bug in AppKit (3500217) that causes it to return 0 for the tool tip
            tag by using removeAllToolTips and not storing the tag at all. Besides the assertion
            failure there may also be a symptom of a "stuck" tool tip and a small memory
            leak until the window is closed.
            * WebView.subproj/WebHTMLViewPrivate.h: Remove unused toolTipTag.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewPrivate.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewPrivate.h
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     WebPluginController *pluginController;
     
     NSString *toolTip;
-    NSToolTipTag toolTipTag;
     id trackingRectOwner;
     void *trackingRectUserData;
     
