f6664675ad6225187f92ba5dde8f3c9691d6752a
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-05 00:28:00 +0000

    JavaScriptCore: 	LiveConnect:  Added support for setting the value of Java 	fields.
            Reviewed by Chris.
    
            * bindings/jni/jni_instance.cpp:
            (JavaInstance::invokeMethod):
            * bindings/jni/jni_runtime.cpp:
            (JavaParameter::JavaParameter):
            (JavaField::JavaField):
            (JavaField::valueFromInstance):
            (JavaField::setValueToInstance):
            (JavaMethod::JavaMethod):
            * bindings/jni/jni_runtime.h:
            (Bindings::JavaField::getJNIType):
            * bindings/jni/jni_utility.cpp:
            (JNITypeFromClassName):
            (convertValueToJValue):
            * bindings/jni/jni_utility.h:
            * bindings/runtime.cpp:
            (Instance::setValueOfField):
            * bindings/runtime.h:
            * bindings/runtime_object.cpp:
            (RuntimeObjectImp::get):
            (RuntimeObjectImp::put):
            (RuntimeObjectImp::defaultValue):
    
    Tests:
            Added test of setting Java fields from JavaScript.
    
            Reviewed by Chris.
    
            * LiveConnect/Blink/Holder.java:
            (Holder):
            * LiveConnect/Blink/test.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5697 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/runtime_object.cpp
+++ third_party/WebKit/JavaScriptCore/bindings/runtime_object.cpp
@@ -65,7 +65,7 @@ Value RuntimeObjectImp::get(ExecState *exec, const Identifier &propertyName) con
 {
     // See if the instance have a field with the specified name.
     Field *aField = instance->getClass()->fieldNamed(propertyName.ascii());
-    if (aField){
+    if (aField) {
         return instance->getValueOfField (aField); 
     }
     
@@ -84,8 +84,11 @@ Value RuntimeObjectImp::get(ExecState *exec, const Identifier &propertyName) con
 void RuntimeObjectImp::put(ExecState *exec, const Identifier &propertyName,
                     const Value &value, int attr)
 {
-    printf ("%s: NOT YET IMPLEMENTED %p: propertyName %s\n", __PRETTY_FUNCTION__, instance, propertyName.ascii());
     // Set the value of the property.
+    Field *aField = instance->getClass()->fieldNamed(propertyName.ascii());
+    if (aField) {
+        getInternalInstance()->setValueOfField(exec, aField, value);
+    }
 }
 
 bool RuntimeObjectImp::canPut(ExecState *exec, const Identifier &propertyName) const
@@ -113,8 +116,6 @@ bool RuntimeObjectImp::deleteProperty(ExecState *exec,
 
 Value RuntimeObjectImp::defaultValue(ExecState *exec, Type hint) const
 {
-    // Return a string representation of the instance.
-    
     // FIXME:  Convert to appropriate type based on hint.
     // If UnspecifiedType should only convert to string if
     // native class is a string.  
