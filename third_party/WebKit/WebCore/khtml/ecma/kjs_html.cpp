68f6715623f3cc983bec0b0cb761c1eaae6f9fef
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-10 01:23:53 +0000

    Implement start() and stop() for marquees.
            Reviewed by darin
    
            * khtml/ecma/kjs_html.cpp:
            (KJS::HTMLElement::classInfo):
            (KJS::HTMLElementFunction::tryCall):
            * khtml/ecma/kjs_html.h:
            (KJS::HTMLElement::):
            * khtml/ecma/kjs_html.lut.h:
            (KJS::):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5739 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_html.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_html.cpp
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "misc/htmltags.h"
 
 #include "rendering/render_object.h"
+#include "rendering/render_layer.h"
 
 #include <kdebug.h>
 
@@ -488,6 +489,7 @@ const ClassInfo KJS::HTMLElement::tablecell_info = { "HTMLTableCellElement", &KJ
 const ClassInfo KJS::HTMLElement::frameSet_info = { "HTMLFrameSetElement", &KJS::HTMLElement::info, &HTMLFrameSetElementTable, 0 };
 const ClassInfo KJS::HTMLElement::frame_info = { "HTMLFrameElement", &KJS::HTMLElement::info, &HTMLFrameElementTable, 0 };
 const ClassInfo KJS::HTMLElement::iFrame_info = { "HTMLIFrameElement", &KJS::HTMLElement::info, &HTMLIFrameElementTable, 0 };
+const ClassInfo KJS::HTMLElement::marquee_info = { "HTMLMarqueeElement", &KJS::HTMLElement::info, &HTMLMarqueeElementTable, 0 };
 
 const ClassInfo* KJS::HTMLElement::classInfo() const
 {
@@ -611,6 +613,8 @@ const ClassInfo* KJS::HTMLElement::classInfo() const
     return &frame_info;
   case ID_IFRAME:
     return &iFrame_info;
+  case ID_MARQUEE:
+    return &marquee_info;
   default:
     return &info;
   }
@@ -1067,6 +1071,11 @@ const ClassInfo* KJS::HTMLElement::classInfo() const
   scrolling	  KJS::HTMLElement::IFrameScrolling		DontDelete
   src		  KJS::HTMLElement::IFrameSrc			DontDelete
   width		  KJS::HTMLElement::IFrameWidth			DontDelete
+@end
+
+@begin HTMLMarqueeElementTable 2
+  start           KJS::HTMLElement::MarqueeStart		DontDelete|Function 0
+  stop            KJS::HTMLElement::MarqueeStop                 DontDelete|Function 0
 @end
 */
 
@@ -2099,6 +2108,22 @@ Value KJS::HTMLElementFunction::tryCall(ExecState *exec, Object &thisObj, const 
         return Undefined();
       }
     }
+    case ID_MARQUEE: {
+        if (id == KJS::HTMLElement::MarqueeStart && element.handle()->renderer() && 
+            element.handle()->renderer()->layer() &&
+            element.handle()->renderer()->layer()->marquee()) {
+            element.handle()->renderer()->layer()->marquee()->start();
+            return Undefined();
+        }
+        else if (id == KJS::HTMLElement::MarqueeStop && element.handle()->renderer() && 
+                 element.handle()->renderer()->layer() &&
+                 element.handle()->renderer()->layer()->marquee()) {
+            element.handle()->renderer()->layer()->marquee()->suspend();
+            return Undefined();
+        }
+        break;
+    }
+        
     break;
   }
 
