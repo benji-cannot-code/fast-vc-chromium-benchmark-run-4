afabe734e6321fd44ee38fdf2a0be8c6ccc4f04d
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-11 06:56:10 +0000

    Reviewed by Darin.
    	<rdar://problem/3504156>: XMLHttpRequest reuse test fails
    	<rdar://problem/3505158>: XMLHttpRequest "abort with open" test fails
    
            * khtml/ecma/xmlhttprequest.cpp:
            (KJS::XMLHttpRequest::open):
            (KJS::XMLHttpRequest::abort):
            (KJS::XMLHttpRequestProtoFunc::tryCall):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5760 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/xmlhttprequest.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/xmlhttprequest.cpp
@@ -264,10 +264,27 @@ bool XMLHttpRequest::urlMatchesDocumentDomain(const KURL& _url) const
 
 void XMLHttpRequest::open(const QString& _method, const KURL& _url, bool _async)
 {
+  abort();
+  aborted = false;
+
+  // clear stuff from possible previous load
+  requestHeaders = QString();
+  responseHeaders = QString();
+  response = QString();
+  createdDocument = false;
+  responseXML = DOM::Document();
+
+  changeState(Uninitialized);
+
+  if (aborted) {
+    return;
+  }
+
   if (!urlMatchesDocumentDomain(_url)) {
     return;
   }
 
+  
   method = _method;
   url = _url;
   async = _async;
@@ -334,6 +351,10 @@ void XMLHttpRequest::abort()
   if (job) {
     job->kill();
     job = 0;
+  }
+  if (decoder) {
+    decoder->deref();
+    decoder = 0;
   }
   aborted = true;
 }
@@ -549,10 +570,6 @@ Value XMLHttpRequestProtoFunc::tryCall(ExecState *exec, Object &thisObj, const L
 	return Undefined();
       }
     
-      if (request->state != Uninitialized) {
-	return Undefined();
-      }
-
       QString method = args[0].toString(exec).qstring();
       KURL url = KURL(Window::retrieveActive(exec)->part()->htmlDocument().completeURL(args[1].toString(exec).qstring()).string());
 
