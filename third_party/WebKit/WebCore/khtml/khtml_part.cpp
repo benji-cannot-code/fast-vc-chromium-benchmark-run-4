be089fd3e0d5b97858784c490b5a4ebb78a8e01a
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-11 05:15:54 +0000

    Reviewed by Maciej.
            - fixed 3506739: nil-deref in DocumentImpl::setSelection triple clicking (generated content)
    
            * khtml/khtml_part.cpp: (KHTMLPart::khtmlMousePressEvent): Check for the case where
            m_selectionEnd is 0. This doesn't fix the behavior completely, but it does prevent a crash.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtml_part.cpp
+++ third_party/WebKit/WebCore/khtml/khtml_part.cpp
@@ -4614,7 +4614,7 @@ void KHTMLPart::khtmlMousePressEvent( khtml::MousePressEvent *event )
                 }
             }
         }
-        if (d->m_selectionStart == 0)
+        if (d->m_selectionStart == 0 || d->m_selectionEnd == 0)
             d->m_doc->clearSelection();
         else{
             d->m_initialSelectionStart = d->m_selectionStart;
@@ -4653,7 +4653,7 @@ void KHTMLPart::khtmlMousePressEvent( khtml::MousePressEvent *event )
                 startAndEndLineNodesIncludingNode (node, startOffset, d->m_selectionStart, d->m_startOffset, d->m_selectionEnd, d->m_endOffset);
             }
         }
-        if (d->m_selectionStart == 0)
+        if (d->m_selectionStart == 0 || d->m_selectionEnd == 0)
             d->m_doc->clearSelection();
         else {
             d->m_initialSelectionStart = d->m_selectionStart;
