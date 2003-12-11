a62c07b541273defefe9b5a5ae91548bc8a227f0
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-11 04:49:51 +0000

    Reviewed by Maciej.
            - fixed 3506706: REGRESSION (100-115): onkeyup handlers called twice for active input fields
    
            * kwq/KWQTextArea.mm: (-[KWQTextAreaTextView keyUp:]): Don't call super because we
            don't want to pass the event down the responder chain.
            * kwq/KWQTextField.mm: (-[KWQTextField textView:shouldHandleEvent:]): Return NO for
            key up events because we don't want to pass the event down the responder chain.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextField.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextField.mm
@@ -506,9 +506,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 	return YES;
     }
 
-    if ([event type] == NSKeyDown || [event type] == NSKeyUp) {
+    NSEventType type = [event type];
+    if (type == NSKeyDown || type == NSKeyUp) {
         WebCoreBridge *bridge = KWQKHTMLPart::bridgeForWidget(widget);
-        return ![bridge interceptKeyEvent:event toView:view];
+        BOOL intercepted = [bridge interceptKeyEvent:event toView:view];
+        // Always return NO for key up events because we don't want them
+        // passed along the responder chain. This is arguably a bug in
+        // NSTextView; see Radar 3507083.
+        return type != NSKeyUp && !intercepted;
     }
     return YES;
 }
