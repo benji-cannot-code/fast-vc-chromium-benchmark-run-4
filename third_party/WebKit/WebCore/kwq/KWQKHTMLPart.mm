b5c74a724ab4d71e85000dbddc20aa04ea50ec25
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-12 17:32:00 +0000

    JavaScriptCore: 	Ensure that all calls from Java into JavaScript are 	performed on a designated thread (the main thread).
            Reviewed by Ken.
    
            * bindings/jni_jsobject.cpp:
            (isJavaScriptThread):
            (rootForImp):
            (Bindings::performJavaScriptAccess):
            (Bindings::completedJavaScriptAccess):
            (Bindings::initializeJavaScriptAccessLock):
            (Bindings::lockJavaScriptAccess):
            (Bindings::unlockJavaScriptAccess):
            (Bindings::dispatchToJavaScriptThread):
            (Bindings::RootObject::setFindRootObjectForNativeHandleFunction):
            (Bindings::RootObject::removeAllJavaReferencesForRoot):
            (Bindings::JSObject::invoke):
            (Bindings::JSObject::JSObject):
            (Bindings::JSObject::call):
            (Bindings::JSObject::eval):
            (Bindings::JSObject::getMember):
            (Bindings::JSObject::setMember):
            (Bindings::JSObject::removeMember):
            (Bindings::JSObject::getSlot):
            (Bindings::JSObject::setSlot):
            (Bindings::JSObject::toString):
            (Bindings::JSObject::finalize):
            (Bindings::JSObject::getWindow):
            * bindings/jni_jsobject.h:
            (Bindings::RootObject::~RootObject):
            (Bindings::RootObject::findRootObjectForNativeHandleFunction):
            (Bindings::RootObject::runLoop):
            (Bindings::RootObject::performJavaScriptSource):
            (Bindings::):
    
    WebCore:
    	Replace calls to global functions with static member functions.
    
            Reviewed by Ken.
    
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::cleanupPluginRootObjects):
            * kwq/WebCoreBridge.mm:
            (rootForView):
            (-[WebCoreBridge init]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5777 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKHTMLPart.mm
+++ third_party/WebKit/WebCore/kwq/KWQKHTMLPart.mm
@@ -2788,7 +2788,7 @@ void KWQKHTMLPart::cleanupPluginRootObjects()
 {
     Bindings::RootObject *root;
     while ((root = rootObjects.getLast())) {
-        KJS_removeAllJavaReferencesForRoot (root);
+        Bindings::RootObject::removeAllJavaReferencesForRoot (root);
         rootObjects.removeLast();
     }
 }
