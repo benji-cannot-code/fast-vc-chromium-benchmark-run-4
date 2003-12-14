89c2701f6652f82b3e760685728fa11a857e626c
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-14 23:50:33 +0000

    Reviewed by Darin.
    	<rdar://problem/3269212>: FILEMAKER: can't use Option objects created in a different window
    
            * khtml/ecma/kjs_binding.cpp:
            (ScriptInterpreter::updateDOMObjectDocument): Update per-document
    	caching for the DOM object - let it be protected by both the old
    	and the new document.
            * khtml/ecma/kjs_binding.h: Prototype new method.
            * khtml/xml/dom_nodeimpl.cpp:
            (NodeImpl::setDocument): New method to change a node's document pointer.
            (NodeImpl::checkAddChild): If the new child node is owned by the
    	wrong document but not actually in it, change its document (using
    	the two aforementioned methods) instead of throwing an exception.
    	* khtml/xml/dom_nodeimpl.h: Prototype new method.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "rendering/render_text.h"
 
+#include "ecma/kjs_binding.h"
 #include "ecma/kjs_proxy.h"
 #include "khtmlview.h"
 #include "khtml_part.h"
@@ -74,6 +75,20 @@ NodeImpl::NodeImpl(DocumentPtr *doc)
 {
     if (document)
         document->ref();
+}
+
+void NodeImpl::setDocument(DocumentPtr *doc)
+{
+    if (inDocument())
+	return;
+    
+    if (doc)
+	doc->ref();
+    
+    if (document)
+	document->deref();
+
+    document = doc;
 }
 
 NodeImpl::~NodeImpl()
@@ -882,13 +897,23 @@ void NodeImpl::checkAddChild(NodeImpl *newChild, int &exceptioncode)
         return;
     }
 
+    bool shouldAdoptChild = false;
+
     // WRONG_DOCUMENT_ERR: Raised if newChild was created from a different document than the one that
     // created this node.
     // We assume that if newChild is a DocumentFragment, all children are created from the same document
     // as the fragment itself (otherwise they could not have been added as children)
     if (newChild->getDocument() != getDocument()) {
-        exceptioncode = DOMException::WRONG_DOCUMENT_ERR;
-        return;
+	// but if the child is not in a document yet then loosen the
+	// restriction, so that e.g. creating an element with the Option()
+	// constructor and then adding it to a different document works,
+	// as it does in Mozilla and Mac IE.
+	if (!newChild->inDocument()) {
+	    shouldAdoptChild = true;
+	} else {
+	    exceptioncode = DOMException::WRONG_DOCUMENT_ERR;
+	    return;
+	}
     }
 
     // HIERARCHY_REQUEST_ERR: Raised if this node is of a type that does not allow children of the type of the
@@ -917,6 +942,12 @@ void NodeImpl::checkAddChild(NodeImpl *newChild, int &exceptioncode)
             exceptioncode = DOMException::HIERARCHY_REQUEST_ERR;
             return;
         }
+    }
+
+    // only do this once we know there won't be an exception
+    if (shouldAdoptChild) {
+	KJS::ScriptInterpreter::updateDOMObjectDocument(newChild, newChild->getDocument(), getDocument());
+	newChild->setDocument(getDocument()->docPtr());
     }
 }
 
