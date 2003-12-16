58f3a90462af6128818bd259c461f23e8947215e
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-16 21:19:03 +0000

    Ack!  More assertions.  Lock ALL entry points into the interpreter! 	(3511733).
            Reviewed by Ken.
    
            * bindings/jni_jsobject.cpp:
            (Bindings::JSObject::call):
            (Bindings::JSObject::eval):
            (Bindings::JSObject::getMember):
            (Bindings::JSObject::setMember):
            (Bindings::JSObject::removeMember):
            (Bindings::JSObject::getSlot):
            (Bindings::JSObject::setSlot):
            (Bindings::JSObject::convertJObjectToValue):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/jni_jsobject.cpp
+++ third_party/WebKit/JavaScriptCore/bindings/jni_jsobject.cpp
@@ -427,7 +427,9 @@ jobject JSObject::call(jstring methodName, jobjectArray args) const
 
     // Lookup the function object.
     ExecState *exec = _root->interpreter()->globalExec();
+    Interpreter::lock();
     Value func = _imp->get (exec, Identifier (JavaString(methodName).ustring()));
+    Interpreter::unlock();
     if (func.isNull() || func.type() == UndefinedType) {
         // Maybe throw an exception here?
         return 0;
@@ -437,7 +439,9 @@ jobject JSObject::call(jstring methodName, jobjectArray args) const
     ObjectImp *funcImp = static_cast<ObjectImp*>(func.imp());
     Object thisObj = Object(const_cast<ObjectImp*>(_imp));
     List argList = listFromJArray(args);
+    Interpreter::lock();
     Value result = funcImp->call (exec, thisObj, argList);
+    Interpreter::unlock();
 
     // Convert and return the result of the function call.
     return convertValueToJObject (result);
@@ -448,7 +452,9 @@ jobject JSObject::eval(jstring script) const
     JS_LOG ("script = %s\n", JavaString(script).characters());
 
     Object thisObj = Object(const_cast<ObjectImp*>(_imp));
+    Interpreter::lock();
     KJS::Value result = _root->interpreter()->evaluate(JavaString(script).ustring(),thisObj).value();
+    Interpreter::unlock();
     return convertValueToJObject (result);
 }
 
@@ -458,9 +464,9 @@ jobject JSObject::getMember(jstring memberName) const
 
     ExecState *exec = _root->interpreter()->globalExec();
 
-    _root->interpreter()->lock();
+    Interpreter::lock();
     Value result = _imp->get (exec, Identifier (JavaString(memberName).ustring()));
-    _root->interpreter()->unlock();
+    Interpreter::unlock();
 
     return convertValueToJObject (result);
 }
@@ -469,7 +475,9 @@ void JSObject::setMember(jstring memberName, jobject value) const
 {
     JS_LOG ("memberName = %s, value = %p\n", JavaString(memberName).characters(), value);
     ExecState *exec = _root->interpreter()->globalExec();
+    Interpreter::lock();
     _imp->put (exec, Identifier (JavaString(memberName).ustring()), convertJObjectToValue(value));
+    Interpreter::unlock();
 }
 
 
@@ -478,7 +486,9 @@ void JSObject::removeMember(jstring memberName) const
     JS_LOG ("memberName = %s\n", JavaString(memberName).characters());
 
     ExecState *exec = _root->interpreter()->globalExec();
+    Interpreter::lock();
     _imp->deleteProperty (exec, Identifier (JavaString(memberName).ustring()));
+    Interpreter::unlock();
 }
 
 
@@ -487,9 +497,9 @@ jobject JSObject::getSlot(jint index) const
     JS_LOG ("index = %d\n", index);
 
     ExecState *exec = _root->interpreter()->globalExec();
-    _root->interpreter()->lock();
+    Interpreter::lock();
     Value result = _imp->get (exec, (unsigned)index);
-    _root->interpreter()->unlock();
+    Interpreter::unlock();
 
     return convertValueToJObject (result);
 }
@@ -500,7 +510,9 @@ void JSObject::setSlot(jint index, jobject value) const
     JS_LOG ("index = %d, value = %p\n", index, value);
 
     ExecState *exec = _root->interpreter()->globalExec();
+    Interpreter::lock();
     _imp->put (exec, (unsigned)index, convertJObjectToValue(value));
+    Interpreter::unlock();
 }
 
 
@@ -650,9 +662,9 @@ KJS::Value JSObject::convertJObjectToValue (jobject theObject) const
         return KJS::Object(const_cast<KJS::ObjectImp*>(imp));
     }
 
-    _root->interpreter()->lock();
+    Interpreter::lock();
     KJS::RuntimeObjectImp *newImp = new KJS::RuntimeObjectImp(new Bindings::JavaInstance (theObject));
-    _root->interpreter()->unlock();
+    Interpreter::unlock();
 
     return KJS::Object(newImp);
 }
