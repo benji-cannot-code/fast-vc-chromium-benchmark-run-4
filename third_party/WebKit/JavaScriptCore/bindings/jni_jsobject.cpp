bbbce10e00c61d718970d57f03cab69a84dfba65
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-16 01:27:59 +0000

    args weren't passed to 'call' invocation.  d'oh. 	lock interpreter when we create instances of JS impls.
            Reviewed by Maciej.
    
            * bindings/jni_jsobject.cpp:
            (Bindings::JSObject::call):
            (Bindings::JSObject::eval):
            (Bindings::JSObject::getMember):
            (Bindings::JSObject::setMember):
            (Bindings::JSObject::getSlot):
            (Bindings::JSObject::convertValueToJObject):
            (Bindings::JSObject::convertJObjectToValue):
            (Bindings::JSObject::listFromJArray):
            * bindings/jni_jsobject.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/jni_jsobject.cpp
+++ third_party/WebKit/JavaScriptCore/bindings/jni_jsobject.cpp
@@ -445,7 +445,7 @@ jobject JSObject::call(jstring methodName, jobjectArray args) const
     Value result = funcImp->call (exec, thisObj, argList);
 
     // Convert and return the result of the function call.
-    return convertValueToJObject (exec, _root, result);
+    return convertValueToJObject (result);
 }
 
 jobject JSObject::eval(jstring script) const
@@ -454,9 +454,7 @@ jobject JSObject::eval(jstring script) const
 
     Object thisObj = Object(const_cast<ObjectImp*>(_imp));
     KJS::Value result = _root->interpreter()->evaluate(JavaString(script).ustring(),thisObj).value();
-    ExecState *exec = _root->interpreter()->globalExec();
-
-    return convertValueToJObject (exec, _root, result);
+    return convertValueToJObject (result);
 }
 
 jobject JSObject::getMember(jstring memberName) const
@@ -466,12 +464,12 @@ jobject JSObject::getMember(jstring memberName) const
     ExecState *exec = _root->interpreter()->globalExec();
     Value result = _imp->get (exec, Identifier (JavaString(memberName).ustring()));
 
-    return convertValueToJObject (exec, _root, result);
+    return convertValueToJObject (result);
 }
 
 void JSObject::setMember(jstring memberName, jobject value) const
 {
-    JS_LOG ("memberName = %s\n", JavaString(memberName).characters());
+    JS_LOG ("memberName = %s, value = %p\n", JavaString(memberName).characters(), value);
     ExecState *exec = _root->interpreter()->globalExec();
     _imp->put (exec, Identifier (JavaString(memberName).ustring()), convertJObjectToValue(value));
 }
@@ -493,7 +491,7 @@ jobject JSObject::getSlot(jint index) const
     ExecState *exec = _root->interpreter()->globalExec();
     Value result = _imp->get (exec, (unsigned)index);
 
-    return convertValueToJObject (exec, _root, result);
+    return convertValueToJObject (result);
 }
 
 
@@ -553,8 +551,9 @@ jlong JSObject::createNative(jlong nativeHandle)
     return ptr_to_jlong(0);
 }
 
-jobject JSObject::convertValueToJObject (KJS::ExecState *exec, const Bindings::RootObject *root, KJS::Value value)
+jobject JSObject::convertValueToJObject (KJS::Value value) const
 {
+    ExecState *exec = _root->interpreter()->globalExec();
     JNIEnv *env = getJNIEnv();
     jobject result = 0;
     
@@ -607,7 +606,7 @@ jobject JSObject::convertValueToJObject (KJS::ExecState *exec, const Bindings::R
                 
                 // Bump our 'meta' reference count for the imp.  We maintain the reference
                 // until either finalize is called or the applet shuts down.
-                addJavaReference (root, imp);
+                addJavaReference (_root, imp);
             }
         }
         // All other types will result in an undefined object.
@@ -626,7 +625,7 @@ jobject JSObject::convertValueToJObject (KJS::ExecState *exec, const Bindings::R
     return result;
 }
 
-KJS::Value JSObject::convertJObjectToValue (jobject theObject)
+KJS::Value JSObject::convertJObjectToValue (jobject theObject) const
 {
     // Instances of netscape.javascript.JSObject get converted back to
     // JavaScript objects.  All other objects are wrapped.  It's not
@@ -651,18 +650,22 @@ KJS::Value JSObject::convertJObjectToValue (jobject theObject)
         return KJS::Object(const_cast<KJS::ObjectImp*>(imp));
     }
 
-    return KJS::Object(new KJS::RuntimeObjectImp(new Bindings::JavaInstance (theObject)));
+    _root->interpreter()->lock();
+    KJS::RuntimeObjectImp *newImp = new KJS::RuntimeObjectImp(new Bindings::JavaInstance (theObject));
+    _root->interpreter()->unlock();
+
+    return KJS::Object(newImp);
 }
 
 KJS::List JSObject::listFromJArray(jobjectArray jArray) const
 {
     JNIEnv *env = getJNIEnv();
-    long i, numArgs = env->GetArrayLength (jArray);
+    long i, numObjects = jArray ? env->GetArrayLength (jArray) : 0;
     KJS::List aList;
     
-    for (i = 0; i < numArgs; i++) {
-        jobject anArg = env->GetObjectArrayElement ((jobjectArray)jArray, i);
-        aList.append (convertJObjectToValue(anArg));
+    for (i = 0; i < numObjects; i++) {
+        jobject anObject = env->GetObjectArrayElement ((jobjectArray)jArray, i);
+        aList.append (convertJObjectToValue(anObject));
     }
     return aList;
 }
@@ -693,6 +696,7 @@ jobject KJS_JSObject_JSObjectCall (JNIEnv *env, jclass jsClass, jlong nativeHand
     context.type = Call;
     context.nativeHandle = nativeHandle;
     context.string = methodName;
+    context.args = args;
     return JSObject::invoke (&context).l;
 }
 
