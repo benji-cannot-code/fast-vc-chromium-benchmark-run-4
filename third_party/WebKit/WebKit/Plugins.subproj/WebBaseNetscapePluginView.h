59c05d6b76e4237d651db5b43f9407868450008e
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2003-12-18 00:44:19 +0000

    Fixed: 	<rdar://problem/3393758>: REGRESSION (85-100): Flash onKeyUp event non-functional 	<rdar://problem/3479020>: REGRESSION (85-100): Safari sends plug-in key events to wrong instance of plug-in
            Reviewed by darin.
    
            * Plugins.subproj/WebBaseNetscapePluginView.h:
            * Plugins.subproj/WebBaseNetscapePluginView.m:
            (-[WebBaseNetscapePluginView sendEvent:]): set suspendKeyUpEvents to NO
            (-[WebBaseNetscapePluginView keyMessageForEvent:]): copied from CVS
            (-[WebBaseNetscapePluginView keyUp:]): if !suspendKeyUpEvents, send the keyUp event
            (-[WebBaseNetscapePluginView keyDown:]): set suspendKeyUpEvents to YES
            (-[WebBaseNetscapePluginView windowBecameKey:]): call SetUserFocusWindow
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5826 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.h
+++ third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     BOOL isStarted;
     BOOL inSetWindow;
+    BOOL suspendKeyUpEvents;
 
     int32 specifiedHeight;
     int32 specifiedWidth;
