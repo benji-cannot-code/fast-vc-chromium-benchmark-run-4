4230410d86961cee8eb1dc5f41874ac637246342
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-01-09 22:47:11 +0000

    Fixes for 3510669 and 3515442, blank frame problems caused by WebKit's use of a separate needsLayout boolean.
            Reviewed by darin
    
            * kwq/WebCoreBridge.h:
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge setNeedsLayout]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreBridge.h
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.h
@@ -150,6 +150,7 @@ typedef enum {
 - (void)forceLayoutWithMinimumPageWidth:(float)minPageWidth maximumPageWidth:(float)maxPageWidth adjustingViewSize:(BOOL)adjustSizeFlag;
 - (void)sendResizeEvent;
 - (BOOL)needsLayout;
+- (void)setNeedsLayout;
 - (void)drawRect:(NSRect)rect;
 - (void)adjustPageHeightNew:(float *)newBottom top:(float)oldTop bottom:(float)oldBottom limit:(float)bottomLimit;
 - (NSArray*)computePageRectsWithPrintWidth:(float)printWidth printHeight:(float)printHeight;
@@ -316,7 +317,6 @@ typedef enum {
 - (NSArray *)documentState;
 
 - (void)setNeedsReapplyStyles;
-- (void)setNeedsLayout;
 
 // OK to be an NSString rather than an NSURL.
 // This URL is only used for coloring visited links.
