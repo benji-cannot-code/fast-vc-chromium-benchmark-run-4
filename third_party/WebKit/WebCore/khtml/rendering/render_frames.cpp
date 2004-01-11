baffc774ae70554c4773059d9620e08140c3292e
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-01-11 19:06:35 +0000

    Fix for frame repainting bugs (3510669, 3515442).  This is a narrower fix than the previous attempt.
            Reviewed by darin
    
            * khtml/rendering/render_frames.cpp:
            (RenderPart::updateWidgetPositions):
            * khtml/rendering/render_frames.h:
            * khtml/rendering/render_replaced.cpp:
            (RenderWidget::updateWidgetPositions):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5887 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_frames.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_frames.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "misc/htmltags.h"
 #include "khtmlview.h"
 #include "khtml_part.h"
+#include "render_arena.h"
 
 #include <kapplication.h>
 #include <kcursor.h>
@@ -972,5 +973,35 @@ void RenderPartObject::slotViewCleared()
         }
   }
 }
+
+#if APPLE_CHANGES
+// FIXME: This should not be necessary.  Remove this once WebKit knows to properly schedule
+// layouts using WebCore when objects resize.
+void RenderPart::updateWidgetPositions()
+{
+    if (!m_widget)
+        return;
+    
+    int x, y, width, height;
+    absolutePosition(x,y);
+    x += borderLeft() + paddingLeft();
+    y += borderTop() + paddingTop();
+    width = m_width - borderLeft() - borderRight() - paddingLeft() - paddingRight();
+    height = m_height - borderTop() - borderBottom() - paddingTop() - paddingBottom();
+    QRect newBounds(x,y,width,height);
+    if (newBounds != m_widget->frameGeometry()) {
+        // The widget changed positions.  Update the frame geometry.
+        RenderArena *arena = ref();
+        element()->ref();
+        m_widget->setFrameGeometry(newBounds);
+        element()->deref();
+        deref(arena);
+        
+        QScrollView *view = static_cast<QScrollView *>(m_widget);
+        if (view && view->inherits("KHTMLView"))
+            static_cast<KHTMLView*>(view)->scheduleRelayout();
+    }
+}
+#endif
 
 #include "render_frames.moc"
