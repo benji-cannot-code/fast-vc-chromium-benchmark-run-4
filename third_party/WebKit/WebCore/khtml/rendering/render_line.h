8e8a8a360e7101bbeaef192d2aff3f13e4f24dba
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-01-26 22:20:58 +0000

    Reviewed by Hyatt
            * khtml/rendering/render_line.h:
            (khtml::InlineBox::isFirstLineStyle): Helper to return whether
    	the line box has the first line style bit set.
            * khtml/rendering/render_text.cpp:
            (RenderText::caretPos): Noe correctly accounts for white space
    	which can precede an inline text box.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5976 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_line.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_line.h
@@ -72,6 +72,7 @@ public:
     }
 
     void setFirstLineStyleBit(bool f) { m_firstLine = f; }
+    bool isFirstLineStyle() const { return m_firstLine; }
 
     InlineBox* nextOnLine() const { return m_next; }
     InlineBox* prevOnLine() const { return m_prev; }
