60b0c0472001138bfcb56b94c999fca70aefcab3
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-01-26 22:35:54 +0000

    Reviewed by John.
            - fixed <rdar://problem/3521379>: image dimensions uses lowercase x instead of multiplication sign
    
            * WebView.subproj/WebImageRepresentation.m: (-[WebImageRepresentation title]):
            Change string to use multiplication sign instead of x.
            * English.lproj/Localizable.strings: Updated.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5979 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebImageRepresentation.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebImageRepresentation.m
@@ -82,7 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     NSSize size = [image size];
     if (!NSEqualSizes(size, NSZeroSize)) {
-        return [NSString stringWithFormat:UI_STRING("%@ %.0fx%.0f pixels", "window title for a standalone image"), filename, size.width, size.height];
+        return [NSString stringWithFormat:UI_STRING("%@ %.0f×%.0f pixels", "window title for a standalone image (uses multiplication symbol, not x)"), filename, size.width, size.height];
     }
     return filename;
 }
