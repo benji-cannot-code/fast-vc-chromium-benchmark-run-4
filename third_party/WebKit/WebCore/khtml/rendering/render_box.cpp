0b22e2483c77d0a78844f47a0ebcc76ecd842f6c
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-01-27 10:01:55 +0000

    Make a change suggested by kocienda to add a remove() method to InlineBoxes.
            Reviewed by kocienda
    
            * khtml/rendering/render_box.cpp:
            (RenderBox::detach):
            (RenderBox::position):
            * khtml/rendering/render_line.cpp:
            (InlineBox::remove):
            * khtml/rendering/render_line.h:
            * khtml/rendering/render_text.cpp:
            (RenderText::detach):
            (RenderText::position):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@5989 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_box.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_box.cpp
@@ -127,7 +127,7 @@ void RenderBox::detach()
     
     if (m_inlineBoxWrapper) {
         if (!documentBeingDestroyed())
-            m_inlineBoxWrapper->parent()->removeChild(m_inlineBoxWrapper);
+            m_inlineBoxWrapper->remove();
         m_inlineBoxWrapper->detach(arena);
     }
 
@@ -570,7 +570,7 @@ void RenderBox::position(InlineBox* box, int from, int len, bool reverse)
             m_staticY = box->yPos();
 
         // Nuke the box.
-        box->parent()->removeChild(box);
+        box->remove();
         box->detach(renderArena());
     }
     else if (isReplaced()) {
