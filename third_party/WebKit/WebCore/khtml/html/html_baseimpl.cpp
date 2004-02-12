760ee361bf629359bc2d459f1eab42d4dbc89918
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-02-12 23:58:03 +0000

    Fix for 3557850 and 3557607.  Make sure to setParent(0) on the body's link declaration, so that TreeShared 	will delete it.
            * khtml/html/html_baseimpl.cpp:
            (HTMLBodyElementImpl::~HTMLBodyElementImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6082 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_baseimpl.cpp
+++ third_party/WebKit/WebCore/khtml/html/html_baseimpl.cpp
@@ -56,8 +56,10 @@ HTMLBodyElementImpl::HTMLBodyElementImpl(DocumentPtr *doc)
 
 HTMLBodyElementImpl::~HTMLBodyElementImpl()
 {
-    if (m_linkDecl)
+    if (m_linkDecl) {
+        m_linkDecl->setParent(0);
         m_linkDecl->deref();
+    }
 }
 
 NodeImpl::Id HTMLBodyElementImpl::id() const
