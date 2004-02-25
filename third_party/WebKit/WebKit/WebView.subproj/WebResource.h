2d7ff142739c3b198e4825cce71b8d61de308a0b
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-02-25 03:21:17 +0000

    I forgot to add these files in my last check-in.
            * WebView.subproj/WebResource.h: Added.
            * WebView.subproj/WebResource.m: Added.
            (-[WebResourcePrivate dealloc]):
            (-[WebResource initWithData:URL:MIMEType:textEncodingName:]):
            (-[WebResource dealloc]):
            (-[WebResource data]):
            (-[WebResource URL]):
            (-[WebResource MIMEType]):
            (-[WebResource textEncodingName]):
            (+[WebResource _resourcesFromPropertyLists:]):
            (+[WebResource _propertyListsFromResources:]):
            (-[WebResource _initWithPropertyList:]):
            (-[WebResource _initWithCachedResponse:originalURL:]):
            (-[WebResource _propertyListRepresentation]):
            (-[WebResource _cachedResponseRepresentation]):
            * WebView.subproj/WebResourcePrivate.h: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6115 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/WebView.subproj/WebResource.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*	
+    WebResource.h
+    Copyright (C) 2004 Apple Computer, Inc. All rights reserved.    
+ 
+    Public header file.
+*/
+
+#import <Foundation/Foundation.h>
+
+@class WebResourcePrivate;
+
+extern NSString *WebHTMLPboardType;
+
+@interface WebResource : NSObject 
+{
+@private
+    WebResourcePrivate *_private;
+}
+
+- (id)initWithData:(NSData *)data URL:(NSURL *)URL MIMEType:(NSString *)MIMEType textEncodingName:(NSString *)textEncodingName;
+
+- (NSData *)data;
+- (NSURL *)URL;
+- (NSString *)MIMEType;
+- (NSString *)textEncodingName;
+
+@end
---
+++ third_party/WebKit/WebKit/WebView.subproj/WebResourcePrivate.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*	
+    WebResourcePrivate.h
+    Copyright (C) 2004 Apple Computer, Inc. All rights reserved.    
+ 
+    Private header file.
+*/
+
+#import <WebKit/WebResource.h>
+
+extern NSString *WebMainResourceKey;
+extern NSString *WebResourceDataKey;
+extern NSString *WebResourceMIMETypeKey;
+extern NSString *WebResourceURLKey;
+extern NSString *WebResourceTextEncodingNameKey;
+extern NSString *WebSubresourcesKey;
+
+@interface WebResource (WebResourcePrivate)
+
++ (NSArray *)_resourcesFromPropertyLists:(NSArray *)propertyLists;
++ (NSArray *)_propertyListsFromResources:(NSArray *)resources;
+
+- (id)_initWithPropertyList:(id)propertyList;
+- (id)_initWithCachedResponse:(NSCachedURLResponse *)response originalURL:(NSURL *)originalURL;
+
+- (id)_propertyListRepresentation;
+- (NSCachedURLResponse *)_cachedResponseRepresentation;
+
+@end
