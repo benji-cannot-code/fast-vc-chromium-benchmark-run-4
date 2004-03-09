1be1ff67e4302aaddefd63139aa6f63718cdd266
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-03-09 18:50:48 +0000

    (1) Change root element in a document to have a role of AXWebArea. 	(2) Add support for AXURL to links and children of links. 	(3) Add support for AXLinkUIElements to the AXWebArea
            Reviewed by john
    
            * khtml/dom/html_misc.h:
            * kwq/KWQAccObject.mm:
            (-[KWQAccObject role]):
            (-[KWQAccObject roleDescription]):
            (-[KWQAccObject accessibilityAttributeNames]):
            (-[KWQAccObject accessibilityAttributeValue:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/dom/html_misc.h
+++ third_party/WebKit/WebCore/khtml/dom/html_misc.h
@@ -127,8 +127,8 @@ class HTMLCollection
 public:
     HTMLCollection();
     HTMLCollection(const HTMLCollection &other);
-protected:
     HTMLCollection(NodeImpl *base, int type);
+
 public:
 
     HTMLCollection & operator = (const HTMLCollection &other);
