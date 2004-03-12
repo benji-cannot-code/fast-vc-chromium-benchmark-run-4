6957859e386891aa5ed67ea3f0e12fe00da03e60
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-03-12 00:07:06 +0000

    Reviewed by Dave
            I have clarified the concept (at least in my head)
            that each edit command has an obligation to put the
            selection in the right place after it is applied,
            unapplied, or reapplied. Right now, each edit command
            calls setStartingSelection and setEndingSelection to do
            this, I made these function move the selection on
            setting.
    
            * khtml/editing/htmlediting_impl.cpp:
            (EditCommandImpl::setStartingSelection)
            (EditCommandImpl::setEndingSelection)
            (InputTextCommandImpl::coalesce)
            (InputTextCommandImpl::deleteCharacter)
            (InputTextCommandImpl::execute)
            * khtml/editing/htmlediting_impl.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6211 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting_impl.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting_impl.cpp
@@ -188,6 +188,18 @@ KHTMLSelection EditCommandImpl::currentSelection() const
     return m_document->part()->selection();
 }
 
+void EditCommandImpl::setStartingSelection(const KHTMLSelection &s)
+{
+    m_startingSelection = s;
+    moveToStartingSelection();
+}
+
+void EditCommandImpl::setEndingSelection(const KHTMLSelection &s)
+{
+    m_endingSelection = s;
+    moveToEndingSelection();
+}
+
 void EditCommandImpl::moveToStartingSelection()
 {
     ASSERT(m_document);
@@ -803,7 +815,6 @@ void InputTextCommandImpl::coalesce(const DOMString &text)
     ASSERT(state() == Applied);
     execute(text);
     m_text += text;
-    moveToEndingSelection();
 }
 
 void InputTextCommandImpl::deleteCharacter()
@@ -824,7 +835,6 @@ void InputTextCommandImpl::deleteCharacter()
         ASSERT(exceptionCode == 0);
         selection = KHTMLSelection(textNode, offset);
         setEndingSelection(selection);
-        moveToEndingSelection();
         m_text = m_text.string().left(m_text.length() - 1);
     }
 }
@@ -843,7 +853,6 @@ void InputTextCommandImpl::execute(const DOMString &text)
     insertText(textNode, selection.startOffset(), text);
     selection = KHTMLSelection(selection.startNode(), selection.startOffset() + text.length());
     setEndingSelection(selection);
-    moveToEndingSelection();
 }
 
 //------------------------------------------------------------------------------------------
