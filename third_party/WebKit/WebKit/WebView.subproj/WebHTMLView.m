ad3db7b0899d214a1de428b9145ec405bb81462f
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-03-17 21:06:32 +0000

    Fixed 3591667.  Plugin view is added to view hierarchy before calling init.
            Reviewed by Ken.
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView addSubview:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6235 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -1218,11 +1218,11 @@ static WebHTMLView *lastHitView = nil;
 
 - (void)addSubview:(NSView *)view
 {
+    [super addSubview:view];
+
     if ([view conformsToProtocol:@protocol(WebPlugin)]) {
         [[self _pluginController] addPlugin:view];
     }
-
-    [super addSubview:view];
 }
 
 - (void)reapplyStyles
