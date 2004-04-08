8b9b8f0deeb53e9aa159b23291098323309af186
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-04-08 22:31:49 +0000

    Renamed the PasteHTMLCommand to PasteMarkupCommand. Removed PasteImageCommand because we will use PasteMarkupCommand for pasting images.
            Reviewed by kocienda.
    
            * khtml/editing/htmlediting.cpp:
            (PasteMarkupCommand::PasteMarkupCommand):
            (PasteMarkupCommand::~PasteMarkupCommand):
            (PasteMarkupCommand::impl):
            (PasteMarkupCommand::markupString):
            * khtml/editing/htmlediting.h:
            (khtml::):
            * khtml/editing/htmlediting_impl.cpp:
            (PasteMarkupCommandImpl::PasteMarkupCommandImpl):
            (PasteMarkupCommandImpl::~PasteMarkupCommandImpl):
            (PasteMarkupCommandImpl::commandID):
            (PasteMarkupCommandImpl::doApply):
            * khtml/editing/htmlediting_impl.h:
            (khtml::PasteMarkupCommandImpl::markupString):
            * khtml/khtml_part.cpp:
            (KHTMLPart::pasteMarkupString):
            * khtml/khtml_part.h:
            * kwq/WebCoreBridge.h:
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge pasteMarkupString:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6335 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting.h
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.h
@@ -56,8 +56,7 @@ class InputTextCommandImpl;
 class InsertNodeBeforeCommandImpl;
 class InsertTextCommandImpl;
 class JoinTextNodesCommandImpl;
-class PasteHTMLCommandImpl;
-class PasteImageCommandImpl;
+class PasteMarkupCommandImpl;
 class RemoveNodeCommandImpl;
 class RemoveNodeAndPruneCommandImpl;
 class SplitTextNodeCommandImpl;
@@ -78,8 +77,7 @@ enum ECommandID {
     InsertNodeBeforeCommandID,
     InsertTextCommandID,
     JoinTextNodesCommandID,
-    PasteHTMLCommandID,
-    PasteImageCommandID,
+    PasteMarkupCommandID,
     RemoveNodeCommandID,
     RemoveNodeAndPruneCommandID,
     SplitTextNodeCommandID,
@@ -319,31 +317,18 @@ private:
 };
 
 //------------------------------------------------------------------------------------------
-// PasteHTMLCommand
+// PasteMarkupCommand
 
-class PasteHTMLCommand : public CompositeEditCommand
+class PasteMarkupCommand : public CompositeEditCommand
 {
 public:
-    PasteHTMLCommand(DOM::DocumentImpl *document, const DOM::DOMString &HTMLString);
-    virtual ~PasteHTMLCommand();
+    PasteMarkupCommand(DOM::DocumentImpl *document, const DOM::DOMString &markupString);
+    virtual ~PasteMarkupCommand();
 
-    DOM::DOMString HTMLString() const;
+    DOM::DOMString markupString() const;
 
 private:
-    inline PasteHTMLCommandImpl *impl() const;
-};
-
-//------------------------------------------------------------------------------------------
-// PasteImageCommand
-
-class PasteImageCommand : public CompositeEditCommand
-{
-public:
-    PasteImageCommand(DOM::DocumentImpl *document, const DOM::DOMString &src);
-    virtual ~PasteImageCommand();
-        
-private:
-    inline PasteImageCommandImpl *impl() const;
+    inline PasteMarkupCommandImpl *impl() const;
 };
 
 //------------------------------------------------------------------------------------------
