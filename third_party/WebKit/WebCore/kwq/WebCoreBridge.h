4ffc666d2923c7d137a782e6a6c1e88a8e786713
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-04-08 22:42:19 +0000

    Rolling out previous commit. Tree was closed. Doh!
    git-svn-id: svn://svn.chromium.org/blink/trunk@6336 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreBridge.h
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.h
@@ -231,6 +231,7 @@ typedef enum {
 - (BOOL)moveCaretToPoint:(NSPoint)point;
 
 - (void)pasteMarkupString:(NSString *)markupString;
+- (void)pasteImageWithURL:(NSURL *)URL;
 
 - (void)deleteSelection;
 - (BOOL)haveSelection;
