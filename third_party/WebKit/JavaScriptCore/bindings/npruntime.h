6788568e5ded438e201e10c59f604f45c402a44a
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-04-09 19:49:04 +0000

    Fixed build problem.
    git-svn-id: svn://svn.chromium.org/blink/trunk@6345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/npruntime.h
+++ third_party/WebKit/JavaScriptCore/bindings/npruntime.h
@@ -182,7 +182,6 @@ struct NPClass
     NPGetPropertyFunctionPtr getProperty;
     NPSetPropertyFunctionPtr setProperty;
 };
-typedef struct NPClass NPClass;
 
 #define kNPClassStructVersion1 1
 #define kNPClassStructVersionCurrent kNPClassStructVersion1
