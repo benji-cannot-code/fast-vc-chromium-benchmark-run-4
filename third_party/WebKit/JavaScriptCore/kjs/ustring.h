f6da38b13da04e301144047516304660218360f3
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-04-19 21:26:40 +0000

    Reviewed by Dave.
    	Optimize prepend using the shared substring optimization. Also,
    	limit the applicability of shared append and shared prepend. If
    	you overdo it, it does more harm than good, because you create a
    	bunch of strings that are disqualified from future shared
    	append/prepend, for not much immediate savings in allocate/copy
    	expense.
    
            * kjs/ustring.cpp:
            (KJS::):
            (KJS::UString::Rep::create):
            (KJS::UString::expandedSize):
            (KJS::UString::usedPreCapacity):
            (KJS::UString::expandCapacity):
            (KJS::UString::expandPreCapacity):
            (KJS::UString::UString):
            (KJS::UString::append):
            (KJS::UString::operator=):
            * kjs/ustring.h:
            (KJS::UString::Rep::data):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6425 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/ustring.h
+++ third_party/WebKit/JavaScriptCore/kjs/ustring.h
@@ -210,7 +210,7 @@ namespace KJS {
       static Rep *create(Rep *base, int offset, int length);
       void destroy();
       
-      UChar *data() const { return baseString ? (baseString->buf + offset) : (buf + offset); }
+      UChar *data() const { return baseString ? (baseString->buf + baseString->preCapacity + offset) : (buf + preCapacity + offset); }
       int size() const { return len; }
       
       unsigned hash() const { if (_hash == 0) _hash = computeHash(data(), len); return _hash; }
@@ -232,6 +232,8 @@ namespace KJS {
       UChar *buf;
       int usedCapacity;
       int capacity;
+      int usedPreCapacity;
+      int preCapacity;
       
       static Rep null;
       static Rep empty;
@@ -455,9 +457,11 @@ namespace KJS {
     void attach(Rep *r);
     void detach();
     void release();
-    int expandedSize(int size) const;
+    int expandedSize(int size, int otherSize) const;
     int usedCapacity() const;
+    int usedPreCapacity() const;
     void expandCapacity(int requiredLength);
+    void expandPreCapacity(int requiredPreCap);
 
     Rep *rep;
   };
