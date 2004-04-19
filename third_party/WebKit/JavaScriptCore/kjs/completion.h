0b1bbd848ad239945189eb3b0a0d5dc171ad29ac
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-04-19 00:17:56 +0000

    Reviewed by Richard.
    	No more need for Completion or Reference to privately inherit from
    	Value, none of the superclass functionality is used.
    
            * kjs/completion.h:
            * kjs/reference.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6423 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/completion.h
+++ third_party/WebKit/JavaScriptCore/kjs/completion.h
@@ -46,7 +46,7 @@ namespace KJS {
    *
    * @short Handle for a Completion type.
    */
-  class Completion : private Value {
+  class Completion {
   public:
     Completion(ComplType c = Normal, const Value& v = Value(),
                const Identifier &t = Identifier::null())
--- third_party/WebKit/JavaScriptCore/kjs/reference.h
+++ third_party/WebKit/JavaScriptCore/kjs/reference.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace KJS {
 
-  class Reference : private Value {
+  class Reference {
     friend class ReferenceList;
     friend class ReferenceListIterator;
   public:
