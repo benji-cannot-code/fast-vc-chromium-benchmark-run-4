faffd3951081866c0f79455b5a9b7bbf9dde56b6
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-04-26 02:33:32 +0000

    Reviewed by Dave.
            - fixed <rdar://problem/3633091>: "add overrideMimeType function to XMLHttpRequest"
    
            * khtml/ecma/xmlhttprequest.cpp:
            (KJS::XMLHttpRequest::getValueProperty): Add logic to override MIME type if
            OverrideMIMEType was called.
            (KJS::XMLHttpRequestProtoFunc::tryCall): Store the MIME type when overrideMimeType is called.
            * khtml/ecma/xmlhttprequest.h: Add OverrideMIMEType to the XMLHttpRequest property enum,
            and also add a MIMETypeOverride data member to the class.
            * khtml/ecma/xmlhttprequest.lut.h: Regenerated.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6481 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/xmlhttprequest.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/xmlhttprequest.cpp
@@ -53,6 +53,7 @@ using khtml::Decoder;
   getAllResponseHeaders	XMLHttpRequest::GetAllResponseHeaders	DontDelete|Function 0
   getResponseHeader	XMLHttpRequest::GetResponseHeader	DontDelete|Function 1
   open			XMLHttpRequest::Open			DontDelete|Function 5
+  overrideMimeType      XMLHttpRequest::OverrideMIMEType        DontDelete|Function 1
   send			XMLHttpRequest::Send			DontDelete|Function 1
   setRequestHeader	XMLHttpRequest::SetRequestHeader	DontDelete|Function 2
 @end
@@ -136,11 +137,17 @@ Value XMLHttpRequest::getValueProperty(ExecState *exec, int token) const
       return Undefined();
     }
     if (!createdDocument) {
-      QString mimeType = "text/xml";
+      QString mimeType;
       
-      Value header = getResponseHeader("Content-Type");
-      if (header.type() != UndefinedType) {
-	mimeType = QStringList::split(";", header.toString(exec).qstring())[0].stripWhiteSpace();
+      if (MIMETypeOverride.isEmpty()) {
+        Value header = getResponseHeader("Content-Type");
+        if (header.type() == UndefinedType) {
+          mimeType = "text/xml";
+        } else {
+	  mimeType = QStringList::split(";", header.toString(exec).qstring())[0].stripWhiteSpace();
+        }
+      } else {
+        mimeType = MIMETypeOverride;
       }
       
       if (mimeType == "text/xml" || mimeType == "application/xml" || mimeType == "application/xhtml+xml") {
@@ -647,6 +654,12 @@ Value XMLHttpRequestProtoFunc::tryCall(ExecState *exec, Object &thisObj, const L
     
     request->setRequestHeader(args[0].toString(exec).qstring(), args[1].toString(exec).qstring());
     
+    return Undefined();
+  case XMLHttpRequest::OverrideMIMEType:
+    if (args.size() != 1) {
+      return Undefined();
+    }
+    request->MIMETypeOverride = args[0].toString(exec).qstring();
     return Undefined();
   }
 
