e2fcf8438588d77da42ba25d5c493dd1df28d090
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-04-26 23:10:44 +0000

    WebCore: 	Added support for specifying composite operation on an 	image element.  Attribute name is "composite", possible values 	are:
    	clear,
    	copy
    	source-over
    	source-in
    	source-out
    	source-atop
    	destination-over
    	destination-in
    	destination-out
    	destination-atop
    	xor
    	darker
    	highlight
    	lighter
    
    	<img composite="source-over" src="triangle.png">
    	<img style="position:relative; left:-200px;" composite="destination-in" src="circle.png">
    
            Reviewed by Ken.
    
            * khtml/html/html_imageimpl.cpp:
            (HTMLImageElementImpl::parseHTMLAttribute):
            * khtml/html/html_imageimpl.h:
            (DOM::HTMLImageElementImpl::compositeOperator):
            * khtml/misc/htmlattrs.c:
            (findAttr):
            * khtml/misc/htmlattrs.h:
            * khtml/misc/htmlattrs.in:
            * khtml/rendering/render_image.cpp:
            (RenderImage::paint):
            * kwq/KWQPainter.h:
            * kwq/KWQPainter.mm:
            (compositeOperatorFromString):
            (QPainter::drawPixmap):
            * kwq/WebCoreImageRenderer.h:
    
    WebKit:
    	Added support for specifying composite operation on an
    	image element, i.e.:
    
    	<img composite="source-over" src="triangle.png">
    	<img style="position:relative; left:-200px;" composite="destination-in" src="circle.png">
    
    	This feature was requested by the dashboard guys.  They can use it to apply
    	transparency masks to widgies.
    
    
            Reviewed by Ken.
    
            * WebCoreSupport.subproj/WebImageRenderer.h:
            * WebCoreSupport.subproj/WebImageRenderer.m:
            (-[WebImageRenderer initWithMIMEType:]):
            (-[WebImageRenderer initWithData:MIMEType:]):
            (-[WebImageRenderer initWithContentsOfFile:]):
            (-[WebImageRenderer copyWithZone:]):
            (-[WebImageRenderer drawClippedToValidInRect:fromRect:]):
            (-[WebImageRenderer drawImageInRect:fromRect:]):
            (-[WebImageRenderer drawImageInRect:fromRect:compositeOperator:]):
    
    WebBrowser:
            Added debug menu to make browser window transparent.  Useful
            for testing widgies.
    
            * Debug/DebugUtilities.m:
            (-[DebugUtilities createDebugMenu]):
            (-[BrowserDocument toggleBackForwardEnabled:]):
            (-[BrowserDocument toggleTransparentWindow:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreImageRenderer.h
+++ third_party/WebKit/WebCore/kwq/WebCoreImageRenderer.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSSize)size;
 - (void)resize:(NSSize)s;
 - (void)drawImageInRect:(NSRect)ir fromRect:(NSRect)fr;
+- (void)drawImageInRect:(NSRect)ir fromRect:(NSRect)fr compositeOperator:(NSCompositingOperation)compsiteOperator;
 - (void)stopAnimation;
 - (void)tileInRect:(NSRect)r fromPoint:(NSPoint)p;
 - (BOOL)isNull;
--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
@@ -56,7 +56,9 @@ static NSMutableSet *activeImageRenderers;
         loadStatus = NSImageRepLoadStatusUnknownType;
         MIMEType = [MIME copy];
         isNull = YES;
+        compositeOperator = (int)NSCompositeSourceOver;
     }
+    
     return self;
 }
 
@@ -75,6 +77,7 @@ static NSMutableSet *activeImageRenderers;
             result->loadStatus = NSImageRepLoadStatusUnknownType;
             result->MIMEType = [MIME copy];
             result->isNull = [data length] == 0;
+            result->compositeOperator = (int)NSCompositeSourceOver;
         }
 
     NS_HANDLER
@@ -101,6 +104,7 @@ static NSMutableSet *activeImageRenderers;
             [result setCacheMode:NSImageCacheNever];
     
             result->loadStatus = NSImageRepLoadStatusUnknownType;
+            result->compositeOperator = (int)NSCompositeSourceOver;
         }
         
     NS_HANDLER
@@ -137,6 +141,7 @@ static NSMutableSet *activeImageRenderers;
     copy->frameTimer = nil;
     copy->frameView = nil;
     copy->patternColor = nil;
+    copy->compositeOperator = compositeOperator;
         
     return copy;
 }
@@ -321,7 +326,7 @@ static NSMutableSet *activeImageRenderers;
     }
     
     // This is the operation that handles transparent portions of the source image correctly.
-    [self drawInRect:ir fromRect:fr operation:NSCompositeSourceOver fraction: 1.0];
+    [self drawInRect:ir fromRect:fr operation:compositeOperator fraction: 1.0];
 }
 
 - (void)nextFrame:(id)context
@@ -366,6 +371,12 @@ static NSMutableSet *activeImageRenderers;
         targetRect = ir;
         [self startAnimationIfNecessary];
     }
+}
+
+- (void)drawImageInRect:(NSRect)ir fromRect:(NSRect)fr compositeOperator:(NSCompositingOperation)operator
+{
+    compositeOperator = operator;
+    [self drawImageInRect:ir fromRect:fr];
 }
 
 - (void)startAnimationIfNecessary
