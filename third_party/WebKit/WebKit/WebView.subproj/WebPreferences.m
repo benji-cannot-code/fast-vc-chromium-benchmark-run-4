afdebba3428019c1b9659034463cf2816343136d
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-04-28 20:05:38 +0000

    WebKit:
            - fixed these bugs:
            <rdar://problem/3636570>: "API: [WebPreferences tabsToLinks] should be public API"
            <rdar://problem/3610597>: "API: could turn "stealth browsing" preference into API"
    
            Reviewed by Darin.
    
            I just moved the declarations and implementations from one place to another.
            (No clients in WebKit needed their #imports updated.) This confused cvs diff quite a bit.
    
            * WebView.subproj/WebPreferences.h:
            * WebView.subproj/WebPreferences.m:
            (-[WebPreferences setTabsToLinks:]):
            (-[WebPreferences tabsToLinks]):
            (-[WebPreferences setPrivateBrowsingEnabled:]):
            (-[WebPreferences privateBrowsingEnabled]):
            (-[WebPreferences _pageCacheSize]):
            (-[WebPreferences _objectCacheSize]):
            (-[WebPreferences _backForwardCacheExpirationInterval]):
            * WebView.subproj/WebPreferencesPrivate.h:
    
    WebBrowser:
    
            - fixed up #imports for WebPreferencesPrivate
    
            Reviewed by Darin.
    
            * AppController.m:
            removed #import <WebKit/WebPreferencesPrivate.h>
            * BrowserWindowController.m:
            replaced #import <WebKit/WebPreferencesPrivate.h> with #import <WebKit/WebPreferences.h>
            * DownloadProgressEntry.m:
            ditto
            * FormCompletionController.m:
            ditto
            * Preferences.subproj/AdvancedPreferences.m:
            removed #import <WebKit/WebPreferencesPrivate.h>
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6506 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebPreferences.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebPreferences.m
@@ -547,25 +547,6 @@ NS_ENDHANDLER
     return _private->autosaves;
 }
 
-@end
-
-@implementation WebPreferences (WebPrivate)
-
-- (int)_pageCacheSize
-{
-    return [[NSUserDefaults standardUserDefaults] integerForKey:WebKitPageCacheSizePreferenceKey];
-}
-
-- (int)_objectCacheSize
-{
-    return [[NSUserDefaults standardUserDefaults] integerForKey:WebKitObjectCacheSizePreferenceKey];
-}
-
-- (NSTimeInterval)_backForwardCacheExpirationInterval
-{
-    return (NSTimeInterval)[[NSUserDefaults standardUserDefaults] floatForKey:WebKitBackForwardCacheExpirationIntervalKey];
-}
-
 - (void)setTabsToLinks:(BOOL)flag
 {
     [self _setBoolValue: flag forKey: WebKitTabToLinksPreferenceKey];
@@ -584,6 +565,25 @@ NS_ENDHANDLER
 - (BOOL)privateBrowsingEnabled
 {
     return [[NSUserDefaults standardUserDefaults] boolForKey:WebKitPrivateBrowsingEnabledPreferenceKey];
+}
+
+@end
+
+@implementation WebPreferences (WebPrivate)
+
+- (int)_pageCacheSize
+{
+    return [[NSUserDefaults standardUserDefaults] integerForKey:WebKitPageCacheSizePreferenceKey];
+}
+
+- (int)_objectCacheSize
+{
+    return [[NSUserDefaults standardUserDefaults] integerForKey:WebKitObjectCacheSizePreferenceKey];
+}
+
+- (NSTimeInterval)_backForwardCacheExpirationInterval
+{
+    return (NSTimeInterval)[[NSUserDefaults standardUserDefaults] floatForKey:WebKitBackForwardCacheExpirationIntervalKey];
 }
 
 static NSMutableDictionary *webPreferencesInstances = nil;
