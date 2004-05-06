81d672583e3b2498de101da2e89260aef19d9bca
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-05-06 21:41:14 +0000

    * DOM.subproj/WebDOMOperations.h: improved a header doc comment
    git-svn-id: svn://svn.chromium.org/blink/trunk@6550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/DOM.subproj/WebDOMOperations.h
+++ third_party/WebKit/WebKit/DOM.subproj/WebDOMOperations.h
@@ -33,6 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*!
     @method URLWithAttributeString
     @abstract Constructs a URL given an attribute string.
+    @discussion This method constructs a URL given an attribute string just as WebKit does. 
+    An attribute string is the value of an attribute of an element such as the href attribute on 
+    the DOMHTMLAnchorElement class. This method is only applicable to attributes that refer to URLs.
 */
 - (NSURL *)URLWithAttributeString:(NSString *)string;
 
