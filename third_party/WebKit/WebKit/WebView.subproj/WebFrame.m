78a62a7d3a0e3958912c4333c2057cb86f80c423
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-05-10 23:34:23 +0000

    Fix build.
            * WebView.subproj/WebFrame.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
@@ -103,7 +103,7 @@ NSString *WebPageCacheDocumentViewKey = @"WebPageCacheDocumentViewKey";
 
 @interface NSArray (WebSafeMakePerform)
 
-- (void)_web_safeMakeObjectsPerformSelector:(SEL)aSelector
+- (void)_web_safeMakeObjectsPerformSelector:(SEL)aSelector;
 
 @end
 
