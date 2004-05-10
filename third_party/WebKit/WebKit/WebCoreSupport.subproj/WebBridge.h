15bc6c9a4edd1f59167164111a295841ec2da96c
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-05-10 23:21:17 +0000

    Reviewed by Ken.
    	- avoid messing with undo manager needlessly for ~1% HTML iBench speedup
    
            * WebCoreSupport.subproj/WebBridge.h:
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge registerCommandForUndo:]): mark undo/redo item flag
            (-[WebBridge registerCommandForRedo:]): ditto
            (-[WebBridge clearUndoRedoOperations]): check flag before removing items,
    	and clear it after removing them
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6570 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.h
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     BOOL _keyboardUIModeAccessed;
     BOOL _doingClientRedirect;
     BOOL _inNextKeyViewOutsideWebFrameViews;
+    BOOL _haveUndoRedoOperations;
 }
 
 - (id)initWithWebFrame:(WebFrame *)webFrame;
