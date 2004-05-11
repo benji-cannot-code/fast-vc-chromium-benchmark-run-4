a8c34e169f91f0d87d05249e3e4c0795ac37b122
vicki <vicki@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-05-11 17:30:43 +0000

    fix B&I build failure caused by case sensitivity
    git-svn-id: svn://svn.chromium.org/blink/trunk@6575 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKSSLKeyGen.h
+++ third_party/WebKit/WebCore/kwq/KWQKSSLKeyGen.h
@@ -28,7 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define KSSLKEYGEN_H_
 
 #include "KWQStringList.h"
-#include "QWidget.h"
+
+class KURL;
 
 class KSSLKeyGen
 {
