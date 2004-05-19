f5473df9b90c667b50b8a580ae75caa1c82f16ec
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-05-19 02:50:21 +0000

    Added exception logging.  Also check for exception and 	set results as appropriate.
            Reviewed by Maciej (partially reviewed).
    
            * bindings/objc/WebScriptObject.mm:
            (-[WebScriptObject callWebScriptMethod:withArguments:]):
            (-[WebScriptObject evaluateWebScript:]):
            (-[WebScriptObject setValue:forKey:]):
            (-[WebScriptObject valueForKey:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/objc/WebScriptObject.mm
+++ third_party/WebKit/JavaScriptCore/bindings/objc/WebScriptObject.mm
@@ -39,6 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using namespace KJS;
 using namespace KJS::Bindings;
 
+#define LOG_EXCEPTION(exec) \
+    if (Interpreter::shouldPrintExceptions()) \
+        NSLog (@"%s:%d:  JavaScript exception:  %s\n", __FILE__, __LINE__, exec->exception().toObject(exec).get(exec, messagePropertyName).toString(exec).ascii());
+
 @interface WebScriptObjectPrivate : NSObject
 {
     KJS::ObjectImp *imp;
@@ -129,6 +133,11 @@ static KJS::List listFromNSArray(ExecState *exec, NSArray *array)
     Value result = funcImp->call (exec, thisObj, argList);
     Interpreter::unlock();
 
+    if (exec->hadException()) {
+        LOG_EXCEPTION (exec);
+        result = Undefined();
+    }
+
     // Convert and return the result of the function call.
     id resultObj = [WebScriptObject _convertValueToObjcValue:result root:_private->root];
 
@@ -146,6 +155,11 @@ static KJS::List listFromNSArray(ExecState *exec, NSArray *array)
     KJS::Value result = _private->root->interpreter()->evaluate(v.toString(exec)).value();
     Interpreter::unlock();
     
+    if (exec->hadException()) {
+        LOG_EXCEPTION (exec);
+        result = Undefined();
+    }
+
     id resultObj = [WebScriptObject _convertValueToObjcValue:result root:_private->root];
 
     _didExecute(self);
@@ -161,6 +175,10 @@ static KJS::List listFromNSArray(ExecState *exec, NSArray *array)
    _private->imp->put (exec, Identifier (v.toString(exec)), (convertObjcValueToValue(exec, &value, ObjcObjectType)));
     Interpreter::unlock();
 
+    if (exec->hadException()) {
+        LOG_EXCEPTION (exec);
+    }
+
     _didExecute(self);
 }
 
@@ -172,6 +190,11 @@ static KJS::List listFromNSArray(ExecState *exec, NSArray *array)
     Value result = _private->imp->get (exec, Identifier (v.toString(exec)));
     Interpreter::unlock();
     
+    if (exec->hadException()) {
+        LOG_EXCEPTION (exec);
+        result = Undefined();
+    }
+
     id resultObj = [WebScriptObject _convertValueToObjcValue:result root:_private->root];
 
     _didExecute(self);
@@ -186,6 +209,10 @@ static KJS::List listFromNSArray(ExecState *exec, NSArray *array)
     Value v = convertObjcValueToValue(exec, &key, ObjcObjectType);
     _private->imp->deleteProperty (exec, Identifier (v.toString(exec)));
     Interpreter::unlock();
+
+    if (exec->hadException()) {
+        LOG_EXCEPTION (exec);
+    }
 
     _didExecute(self);
 }
@@ -214,6 +241,11 @@ static KJS::List listFromNSArray(ExecState *exec, NSArray *array)
     Value result = _private->imp->get (exec, (unsigned)index);
     Interpreter::unlock();
 
+    if (exec->hadException()) {
+        LOG_EXCEPTION (exec);
+        result = Undefined();
+    }
+
     id resultObj = [WebScriptObject _convertValueToObjcValue:result root:_private->root];
 
     _didExecute(self);
@@ -227,6 +259,10 @@ static KJS::List listFromNSArray(ExecState *exec, NSArray *array)
     Interpreter::lock();
     _private->imp->put (exec, (unsigned)index, (convertObjcValueToValue(exec, &value, ObjcObjectType)));
     Interpreter::unlock();
+
+    if (exec->hadException()) {
+        LOG_EXCEPTION (exec);
+    }
 
     _didExecute(self);
 }
