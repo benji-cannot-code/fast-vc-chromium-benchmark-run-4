0930bc3a4b2bdd2ad2b17c5ac0ad67acf22dc228
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-05-21 08:34:45 +0000

    Reviewed by Maciej.
            - fixed <rdar://problem/3090663>: (scroll to top of page when anchor is "#top" or "#")
    
            * khtml/khtml_part.cpp:
            (KHTMLPart::gotoAnchor): Detect anchor by checking for non-null, not non-empty, because an
            empty anchor is not the same as no anchor.
            (KHTMLPart::gotoAnchor): Add special case for anchors named "" and "top" (case insensitive),
            to scroll to the top left of the frame to match what WinIE and Mozilla do.
            (KHTMLPart::checkCompleted): Check for non-null here too for consistency.
    
            * kwq/KWQKURL.mm:
            (KURL::ref): Change check so that we return empty, not null, if there's an empty ref,
            as opposed to no ref.
            (KURL::hasRef): Return true if there's an empty ref, as opposed to no ref.
            (KURL::parse): Preserve an empty fragment when parsing.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKURL.mm
+++ third_party/WebKit/WebCore/kwq/KWQKURL.mm
@@ -628,11 +628,7 @@ QString KURL::user() const
 
 QString KURL::ref() const
 {
-    if (!m_isValid) {
-	return QString();
-    }
-    
-    if (fragmentEndPos <= queryEndPos + 1) {
+    if (!m_isValid || fragmentEndPos == queryEndPos) {
 	return QString();
     }
 
@@ -641,7 +637,7 @@ QString KURL::ref() const
 
 bool KURL::hasRef() const
 {
-    return m_isValid && fragmentEndPos > queryEndPos + 1;
+    return m_isValid && fragmentEndPos != queryEndPos;
 }
 
 QString KURL::query() const
@@ -1410,7 +1406,7 @@ void KURL::parse(const char *url, const QString *originalString)
     queryEndPos = p - buffer;
     
     // add fragment, escaping bad characters
-    if (fragmentEnd != fragmentStart) {
+    if (fragmentEnd != queryEnd) {
 	*p++ = '#';
 	appendEscapingBadChars(p, url + fragmentStart, fragmentEnd - fragmentStart);
     }
