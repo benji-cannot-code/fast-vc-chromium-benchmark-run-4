668ea70ec922eb71a0d26b4fd3c6f8f735ebfe58
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-05-24 20:40:30 +0000

    Reviewed by John
            Fixed some life-cycle issues with node iterators and detaching. Big deal is
            to detach in DOMNodeIterator dealloc. Otherwise, NodeIterators created from
            obj-c leak.
    
            * khtml/xml/dom2_traversalimpl.cpp:
            (DOM::NodeIteratorImpl::detach): Only detach if not yet detached.
            * khtml/xml/dom2_traversalimpl.h:
            (DOM::NodeIteratorImpl::setDetached): New convenience.
            * kwq/DOM.mm:
            (-[DOMNodeIterator dealloc]): Detach here, if not yet detached; fixes leak.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/DOM.mm
+++ third_party/WebKit/WebCore/kwq/DOM.mm
@@ -1943,8 +1943,10 @@ inline Document DocumentImpl::createInstance(DocumentImpl *impl)
 {
     if (m_filter)
         [m_filter release];
-    if (_internal)
+    if (_internal) {
+        [self detach];
         DOM_cast<NodeIteratorImpl *>(_internal)->deref();
+    }
     [super dealloc];
 }
 
