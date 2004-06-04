4bb29b7f74eeb3373bbbc254b625934a5546ac90
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-06-04 01:01:36 +0000

    Add SPI to allow setting of plugin load path.
            Reviewed by Hyatt.
    
            * Plugins.subproj/WebPluginDatabase.h:
            * Plugins.subproj/WebPluginDatabase.m:
            (+[WebPluginDatabase setAdditionalWebPlugInPaths:]):
            (pluginLocations):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6759 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPluginDatabase.h
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPluginDatabase.h
@@ -27,3 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)loadPluginIfNeededForMIMEType:(NSString *)MIMEType;
 
 @end
+
+@interface NSObject (WebPlugInDatabase)
+
++ (void)setAdditionalWebPlugInPaths:(NSArray *)path;
+
+@end
