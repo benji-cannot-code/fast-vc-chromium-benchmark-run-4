0721a2e8df4908fd5620a3a5eb66604af11a652c
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-06-05 03:21:30 +0000

    WebCore:
    	DHTML dragging uses the Cocoa NSDragOperation on both the source and dest ends.
    
    	In addition, I caught a prime gaffe where I had used "dropAllowed" instead of "effectAllowed"
    	for that particular WinIE property.  That is renamed throughout.
    
            Reviewed by rjw
    
            * khtml/ecma/kjs_events.cpp:
            (stringOrUndefined):  New little helper.
            (Clipboard::getValueProperty):  Return Undefined if dropEffect or effectAllowed are not set.
            (Clipboard::putValue):  Rename.
            * khtml/ecma/kjs_events.h:
            (KJS::Clipboard::):  Rename.
            * khtml/ecma/kjs_events.lut.h:
            * khtml/xml/dom2_eventsimpl.h:  Rename.
            * kwq/KWQClipboard.h: Rename, add access to Cocoa drag op.
            * kwq/KWQClipboard.mm:
            (KWQClipboard::dropEffect):   These 4 just moved in the file.
            (KWQClipboard::setDropEffect):
            (KWQClipboard::effectAllowed):
            (KWQClipboard::setEffectAllowed):
            (cocoaOpFromIEOp):  Convert from an IE operation string to a Cocoa DragOp
            (IEOpFromCocoaOp):  and vice-versa
            (KWQClipboard::sourceOperation):  Return effectAllowed, converted to a NSDragOp
            (KWQClipboard::destinationOperation):  Return dropEffect, converted to a NSDragOp
            (KWQClipboard::setSourceOperation):  Set effectAllowed, using a Cocoa value
            (KWQClipboard::setDestinationOperation):  Set dropEffect, using a Cocoa value
            * kwq/KWQKHTMLPart.h:
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::dispatchDragSrcEvent):  Return the drag op specified by the source element.
            (KWQKHTMLPart::khtmlMouseMoveEvent):  Pass our drag op up to WebKit.
            (KWQKHTMLPart::dragSourceMovedTo):  Pass NULL for new arg.
            (KWQKHTMLPart::dragSourceEndedAt):  Ditto
            * kwq/WebCoreBridge.h:
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge dragOperationForDraggingInfo:]):  Set the incoming dragOp onto the clipboard
    	so DHTML can access it.  In addition, validate whatever op DHTML returns so we play well
    	with Cocoa.
    
    WebKit:
    
    	DHTML dragging uses the Cocoa NSDragOperation on both the source and dest ends.
    	Most of the real work is in WebCore.
    
            Reviewed by rjw
    
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge startDraggingImage:at:operation:event:]):  Pass the drag op along.
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView _startDraggingImage:at:operation:event:]):  Remember drag op from WC.
            (-[WebHTMLView mouseDragged:]):  Pass None for the op (alternate code path that will
    	be going away).
            (-[WebHTMLView draggingSourceOperationMaskForLocal:]):  Use the WC drag op.
            * WebView.subproj/WebHTMLViewInternal.h:
            * WebView.subproj/WebHTMLViewPrivate.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6779 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewInternal.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewInternal.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     BOOL initiatedDrag;
     // Is WebCore handling drag destination duties (DHTML dragging)?
     BOOL webCoreHandlingDrag;
+    NSDragOperation webCoreDragOp;
     
     id savedSubviews;
     BOOL subviewsSetAside;
