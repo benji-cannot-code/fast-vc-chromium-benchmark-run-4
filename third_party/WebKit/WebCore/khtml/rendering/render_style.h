ceed5a1200ed57b0f0b3e2c10ad8dd099a395ced
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-06-09 22:58:15 +0000

    Implement parsing and setting of the text-overflow CSS3 property.
            Reviewed by kocienda
    
            * khtml/css/cssparser.cpp:
            (CSSParser::parseValue):
            * khtml/css/cssproperties.c:
            (hash_prop):
            (findProp):
            * khtml/css/cssproperties.h:
            * khtml/css/cssproperties.in:
            * khtml/css/cssstyleselector.cpp:
            (khtml::CSSStyleSelector::applyProperty):
            * khtml/css/cssvalues.c:
            (hash_val):
            (findValue):
            * khtml/css/cssvalues.h:
            * khtml/css/cssvalues.in:
            * khtml/rendering/render_style.cpp:
            (textOverflow):
            (StyleCSS3NonInheritedData::operator==):
            (RenderStyle::diff):
            * khtml/rendering/render_style.h:
            (khtml::RenderStyle::textOverflow):
            (khtml::RenderStyle::setTextOverflow):
            (khtml::RenderStyle::initialTextOverflow):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_style.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_style.h
@@ -559,6 +559,7 @@ public:
     DataRef<StyleMarqueeData> marquee; // Marquee properties
     EUserDrag userDrag : 2; // Whether or not a drag can be initiated by this element.
     bool userSelect : 1;  // Whether or not the element is selectable.
+    bool textOverflow : 1; // Whether or not lines that spill out should be truncated with "..."
 
 #ifndef KHTML_NO_XBL
     BindingURI* bindingURI; // The XBL binding URI list.
@@ -1093,6 +1094,7 @@ public:
     EUserModify userModify() const { return css3InheritedData->userModify; }
     EUserDrag userDrag() const { return css3NonInheritedData->userDrag; }
     bool userSelect() const { return css3NonInheritedData->userSelect; }
+    bool textOverflow() const { return css3NonInheritedData->textOverflow; }
     // End CSS3 Getters
 
 #if APPLE_CHANGES
@@ -1271,6 +1273,7 @@ public:
     void setUserModify(EUserModify u) { SET_VAR(css3InheritedData, userModify, u); }
     void setUserDrag(EUserDrag d) { SET_VAR(css3NonInheritedData, userDrag, d); }
     void setUserSelect(bool b) { SET_VAR(css3NonInheritedData, userSelect, b); }
+    void setTextOverflow(bool b) { SET_VAR(css3NonInheritedData, textOverflow, b); }
     // End CSS3 Setters
    
 #if APPLE_CHANGES
@@ -1376,6 +1379,7 @@ public:
     static EUserModify initialUserModify() { return READ_ONLY; }
     static EUserDrag initialUserDrag() { return DRAG_AUTO; }
     static bool initialUserSelect() { return true; }
+    static bool initialTextOverflow() { return false; }
 
 #if APPLE_CHANGES
     // Keep these at the end.
