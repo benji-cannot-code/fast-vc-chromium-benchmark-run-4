2b95f39fd6b8e23f6825329680caebd0faf8c068
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-06-15 08:30:25 +0000

    - fixed crash introduced by my earlier change
            * WebCoreSupport.subproj/WebImageRenderer.m:
            (-[WebImageContext dealloc]): Set _cgsContext to 0 before calling super as before.
            (-[WebImageContext finalize]): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6852 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
@@ -67,8 +67,9 @@ extern NSString *NSImageLoopCount;
     if (self != nil) {
 	_bounds     = b;
 	_isFlipped  = YES;
-        if (context)
+        if (context) {
             _cgsContext = CGContextRetain(context);
+        }
     }
     
     return self;
@@ -79,6 +80,7 @@ extern NSString *NSImageLoopCount;
     [_focusStack release];
     if (_cgsContext) {
         CGContextRelease(_cgsContext);
+        _cgsContext = 0; // super dealloc may also release
     }
     [super dealloc];
 }
@@ -87,6 +89,7 @@ extern NSString *NSImageLoopCount;
 {
     if (_cgsContext) {
         CGContextRelease(_cgsContext);
+        _cgsContext = 0; // super finalize may also release
     }
     [super finalize];
 }
@@ -382,7 +385,7 @@ static NSMutableSet *activeImageRenderers;
     [originalData release];
     
     if (context) {
-        CFRelease(context);
+        CGContextRelease(context);
     }
 
     [_PDFDoc release];
@@ -396,7 +399,7 @@ static NSMutableSet *activeImageRenderers;
     ASSERT(frameView == nil);
 
     if (context) {
-        CFRelease(context);
+        CGContextRelease(context);
     }
 
     [super finalize];
@@ -677,9 +680,9 @@ static NSMutableSet *activeImageRenderers;
     
     if (aContext != context) {
         if (aContext)
-            CFRetain(aContext);
+            CGContextRetain(aContext);
         if (context)
-            CFRelease(context);
+            CGContextRelease(context);
         context = aContext;
     }
         
