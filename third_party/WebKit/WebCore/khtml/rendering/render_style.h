a6e7506389e71be29a4696a5226bd0aa145bad73
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-06-16 06:29:26 +0000

    Reviewed by Dave.
    	<rdar://problem/3695907>: (can't enable selection inside parent where it is disabled via CSS)
    
            * khtml/css/cssparser.cpp:
            (CSSParser::parseValue):
            * khtml/css/cssstyleselector.cpp:
            (khtml::CSSStyleSelector::applyProperty):
            * khtml/rendering/render_object.cpp:
            (RenderObject::shouldSelect):
            * khtml/rendering/render_style.h:
            (khtml::):
            (khtml::RenderStyle::userSelect):
            (khtml::RenderStyle::setUserSelect):
            (khtml::RenderStyle::initialUserSelect):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_style.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_style.h
@@ -532,6 +532,12 @@ enum EUserDrag {
     DRAG_AUTO, DRAG_NONE, DRAG_ELEMENT
 };
 
+// CSS3 User Select Values
+
+enum EUserSelect {
+    SELECT_AUTO, SELECT_NONE, SELECT_TEXT
+};
+
 // This struct is for rarely used non-inherited CSS3 properties.  By grouping them together,
 // we save space, and only allocate this object when someone actually uses
 // a non-inherited CSS3 property.
@@ -558,7 +564,7 @@ public:
     DataRef<StyleFlexibleBoxData> flexibleBox; // Flexible box properties 
     DataRef<StyleMarqueeData> marquee; // Marquee properties
     EUserDrag userDrag : 2; // Whether or not a drag can be initiated by this element.
-    bool userSelect : 1;  // Whether or not the element is selectable.
+    EUserSelect userSelect : 2;  // Whether or not the element is selectable.
     bool textOverflow : 1; // Whether or not lines that spill out should be truncated with "..."
 
 #ifndef KHTML_NO_XBL
@@ -1101,7 +1107,7 @@ public:
     EMarqueeDirection marqueeDirection() { return css3NonInheritedData->marquee->direction; }
     EUserModify userModify() const { return css3InheritedData->userModify; }
     EUserDrag userDrag() const { return css3NonInheritedData->userDrag; }
-    bool userSelect() const { return css3NonInheritedData->userSelect; }
+    EUserSelect userSelect() const { return css3NonInheritedData->userSelect; }
     bool textOverflow() const { return css3NonInheritedData->textOverflow; }
     // End CSS3 Getters
 
@@ -1281,7 +1287,7 @@ public:
     void setMarqueeLoopCount(int i) { SET_VAR(css3NonInheritedData.access()->marquee, loops, i); }
     void setUserModify(EUserModify u) { SET_VAR(css3InheritedData, userModify, u); }
     void setUserDrag(EUserDrag d) { SET_VAR(css3NonInheritedData, userDrag, d); }
-    void setUserSelect(bool b) { SET_VAR(css3NonInheritedData, userSelect, b); }
+    void setUserSelect(EUserSelect s) { SET_VAR(css3NonInheritedData, userSelect, s); }
     void setTextOverflow(bool b) { SET_VAR(css3NonInheritedData, textOverflow, b); }
     // End CSS3 Setters
    
@@ -1388,7 +1394,7 @@ public:
     static EMarqueeDirection initialMarqueeDirection() { return MAUTO; }
     static EUserModify initialUserModify() { return READ_ONLY; }
     static EUserDrag initialUserDrag() { return DRAG_AUTO; }
-    static bool initialUserSelect() { return true; }
+    static EUserSelect initialUserSelect() { return SELECT_AUTO; }
     static bool initialTextOverflow() { return false; }
 
 #if APPLE_CHANGES
