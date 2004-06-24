07078cde81e9f7c8c5fd81dc9561ef1e0b13fa52
trey <trey@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-06-24 20:20:44 +0000

    3679986 - screenX and screenY are flipped and relative to the bottom left of the WebView, rather than the screen 	3699510 - synthesized click events have bogus screen coords
            Reviewed by Darin.
    
            * khtml/khtmlview.cpp:
            (KHTMLView::dispatchDragEvent):  Generate screen coords for drag events using new func.
            (KHTMLView::dispatchMouseEvent):   Ditto.
            * khtml/khtmlview.h:
            * khtml/xml/dom_nodeimpl.cpp:
            (NodeImpl::dispatchMouseEvent):  Comment tricky semantics.
    	Calc screen coords in apple-specific way.
            * kwq/KWQKHTMLView.mm:
            (KHTMLView::viewportToGlobal):  Passthrough to window widget.
            * kwq/KWQScrollView.mm:
            * kwq/KWQWindowWidget.h:
            * kwq/KWQWindowWidget.mm:
            (KWQWindowWidget::mapToGlobal):  Call former code factored to new method below.
            (KWQWindowWidget::viewportToGlobal):  New method to convert "viewport" (which for us
    	really means NSWindow coords) to screen coords.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6922 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtmlview.h
+++ third_party/WebKit/WebCore/khtml/khtmlview.h
@@ -219,6 +219,8 @@ public:
 #if APPLE_CHANGES
     QWidget *topLevelWidget() const;
     QPoint mapToGlobal(const QPoint &) const;
+    // maps "viewport" (actually Cocoa window coords) to screen coords
+    QPoint viewportToGlobal(const QPoint &) const;
     void adjustViewSize();
     void initScrollBars();
 #endif
