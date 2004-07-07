7c76c91f4cb7498e1648d0679fcd23ae536f60f0
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-07-07 23:24:25 +0000

    Fix for 3712133, crash from first-line pseudo-style use.
            Reviewed by kocienda
    
            * khtml/css/cssstyleselector.cpp:
            (khtml::CSSStyleSelector::styleForElement):
            * khtml/css/cssstyleselector.h:
            * khtml/rendering/render_object.cpp:
            (RenderObject::getPseudoStyle):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@6971 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/css/cssstyleselector.cpp
+++ third_party/WebKit/WebCore/khtml/css/cssstyleselector.cpp
@@ -631,7 +631,7 @@ RenderStyle* CSSStyleSelector::locateSharedStyle()
     return 0;
 }
 
-RenderStyle* CSSStyleSelector::styleForElement(ElementImpl* e, RenderStyle* defaultParent)
+RenderStyle* CSSStyleSelector::styleForElement(ElementImpl* e, RenderStyle* defaultParent, bool allowSharing)
 {
     if (!e->getDocument()->haveStylesheetsLoaded()) {
         if (!styleNotYetAvailable) {
@@ -643,9 +643,11 @@ RenderStyle* CSSStyleSelector::styleForElement(ElementImpl* e, RenderStyle* defa
     }
     
     initElementAndPseudoState(e);
-    style = locateSharedStyle();
-    if (style)
-        return style;
+    if (allowSharing) {
+        style = locateSharedStyle();
+        if (style)
+            return style;
+    }
     initForStyleResolve(e, defaultParent);
 
     style = new (e->getDocument()->renderArena()) RenderStyle();
