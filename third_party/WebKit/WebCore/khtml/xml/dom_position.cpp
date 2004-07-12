9cd0ca71168edbe745116579447f4e707f8f3c18
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-07-12 22:29:20 +0000

    Reviewed by Hyatt
            * khtml/xml/dom_position.cpp:
            (DOM::Position::inRenderedContent): I broke selection drawing with my last patch. This
            function is now used in selection drawing, and it was making an unneeded check to see
            that content was editable to answer whether or not is was rendered. Bad. Fixed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_position.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_position.cpp
@@ -621,7 +621,7 @@ bool Position::inRenderedContent() const
         return false;
         
     RenderObject *renderer = node()->renderer();
-    if (!renderer || !renderer->isEditable())
+    if (!renderer)
         return false;
     
     if (renderer->style()->visibility() != khtml::VISIBLE)
