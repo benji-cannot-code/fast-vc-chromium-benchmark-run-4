8bb0f340141325da4b34595416e0cd4351d79fdb
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-07-12 22:03:21 +0000

    Reviewed by John.
    	<rdar://problem/3706080>: (REGRESSION (125.8-147u): Nested <ul> do not display bullets)
    	<rdar://problem/3676376>: (Second level bullets not printed when printing Xcode release notes)
    
            * kwq/KWQPainter.h:
            * kwq/KWQPainter.mm:
            (CGColorFromNSColor):
            (QPainter::drawEllipse):
            (QPainter::setShadow):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQPainter.h
+++ third_party/WebKit/WebCore/kwq/KWQPainter.h
@@ -143,7 +143,6 @@ private:
     void _setColorFromBrush();
     void _setColorFromPen();
 
-    // A fillRect designed to work around buggy behavior in NSRectFill.
     void _fillRect(float x, float y, float w, float h, const QColor& color);
     
     void _drawPoints(const QPointArray &_points, bool winding, int index, int _npoints, bool fill);
