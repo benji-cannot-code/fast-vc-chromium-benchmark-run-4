8c8d35e773b1123143694f4eaad73bb86d2c7ab6
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-07-16 20:37:56 +0000

    Reviewed by Hyatt
            Fix for this bug:
    
            <rdar://problem/3687216> editable inline causes crash when focused
    
            * khtml/xml/dom_docimpl.cpp:
            (DocumentImpl::relinquishesEditingFocus): Add null check for rootEditableElement.
            (DocumentImpl::acceptsEditingFocus): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7041 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_docimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_docimpl.cpp
@@ -2297,10 +2297,10 @@ bool DocumentImpl::relinquishesEditingFocus(NodeImpl *node)
     assert(node);
     assert(node->isContentEditable());
 
-    if (!part())
-        return true;
-    
     NodeImpl *rootImpl = node->rootEditableElement();
+    if (!part() || !rootImpl)
+        return false;
+
     Node root(rootImpl);
     Range range(root, 0, root, rootImpl->childNodeCount());
     return part()->shouldEndEditing(range);
@@ -2311,10 +2311,10 @@ bool DocumentImpl::acceptsEditingFocus(NodeImpl *node)
     assert(node);
     assert(node->isContentEditable());
 
-    if (!part())
-        return true;
-
     NodeImpl *rootImpl = node->rootEditableElement();
+    if (!part() || !rootImpl)
+        return false;
+
     Node root(rootImpl);
     Range range(root, 0, root, rootImpl->childNodeCount());
     return part()->shouldBeginEditing(range);
