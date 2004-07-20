6333a9646071c4b5e0c2745c6ff433f7924ffcea
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-07-20 08:12:08 +0000

    Reviewed by Trey.
    	<rdar://problem/3721428> REGRESSION (125.8-146): external javascript statements produce extra garbage character (sina.com)
    
    	* khtml/misc/stringit.h:
            (khtml::TokenizerSubstring::TokenizerSubstring): For the apple branch, use the new
    	stableUnicode() method to get the unicode pointer.
            * kwq/KWQString.h:
            * kwq/KWQString.mm:
            (QString::detachIfInternal): Reorganize this to be a bit less wacky about refcounts.
    	It does not leave around a zombie internal data handle but rather destroys it right away,
    	and leaves the object pointing to the new handle (which it can then deref). This makes
    	the code more clear.
            (QString::~QString): Simplify.
            (QString::stableUnicode): New method that detaches a copy of the KWQStringData if it
    	is internal to a string besides this one. This guarantees that if you get the unicode()
    	pointer, it won't go bad so long as this string is still alive.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQString.h
+++ third_party/WebKit/WebCore/kwq/KWQString.h
@@ -363,7 +363,7 @@ struct KWQStringData {
     
     bool isUnicodeInternal() const { return (char *)_unicode == _internalBuffer; }
     bool isAsciiInternal() const { return _ascii == _internalBuffer; }
-    
+
     uint refCount;
     uint _length;
     mutable QChar *_unicode;
@@ -414,6 +414,7 @@ public:
     uint length() const;
 
     const QChar *unicode() const;
+    const QChar *stableUnicode();
     const char *latin1() const;
     const char *ascii() const;
     bool isAllASCII() const;
@@ -544,7 +545,7 @@ public:
     NSString *getNSString() const;
 
     void setBufferFromCFString(CFStringRef);
-    
+
 private:
     // Used by QConstString.
     QString(KWQStringData *constData, bool /*dummy*/);
