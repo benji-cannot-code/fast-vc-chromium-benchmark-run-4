0ed4f7ef4cad54f8aba65d6a7ff3a44fe0d0ecef
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-07-22 22:14:06 +0000

    * Plugins.subproj/npruntime.h: Update with new version from newer JavaScriptCore.
    git-svn-id: svn://svn.chromium.org/blink/trunk@7098 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/npruntime.h
+++ third_party/WebKit/WebKit/Plugins.subproj/npruntime.h
@@ -172,7 +172,7 @@ void NPN_InitializeVariantWithVariant (NPVariant *destination, const NPVariant *
 	NPVariant (with type NPVariantUndefinedType)          Undefined
 	NPVariant (with type NPVariantNullType)               Null
 	NPArray                                               Array (restricted)
-	NObject                                               Object or (NPObject wrapper)
+	NPObject                                              Object or (NPObject wrapper)
 
 */
 
