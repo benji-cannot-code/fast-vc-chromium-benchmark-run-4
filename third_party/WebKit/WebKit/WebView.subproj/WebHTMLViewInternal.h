5ef4f683eb1a3b640a3e35f7e700de005feff813
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-07-30 04:18:19 +0000

    Reviewed by John.
    	Added stubbed-out versions of all the NSTextInput protocol
    	methods, and put comments in places where we will need to call the
    	input manager to make input methods work 100%.
    
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge respondToChangedSelection]):
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView mouseDown:]):
            (-[WebHTMLView mouseDragged:]):
            (-[WebHTMLView mouseUp:]):
            (-[WebHTMLView _interceptEditingKeyEvent:]):
            (-[WebHTMLView keyDown:]):
            (-[WebHTMLView _selectionChanged]):
            (-[WebHTMLView _delegateDragSourceActionMask]):
            (-[WebHTMLView validAttributesForMarkedText]):
            (-[WebHTMLView characterIndexForPoint:]):
            (-[WebHTMLView firstRectForCharacterRange:]):
            (-[WebHTMLView selectedRange]):
            (-[WebHTMLView markedRange]):
            (-[WebHTMLView attributedSubstringFromRange:]):
            (-[WebHTMLView conversationIdentifier]):
            (-[WebHTMLView hasMarkedText]):
            (-[WebHTMLView unmarkText]):
            (-[WebHTMLView setMarkedText:selectedRange:]):
            (-[WebHTMLView doCommandBySelector:]):
            (-[WebHTMLView insertText:]):
            * WebView.subproj/WebHTMLViewInternal.h:
            * WebView.subproj/WebView.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7160 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewInternal.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewInternal.h
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 @interface WebHTMLView (WebInternal)
+- (void)_selectionChanged;
 - (void)_updateFontPanel;
 - (unsigned int)_delegateDragSourceActionMask;
 @end
