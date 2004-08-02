0b100ec060961dc94da8c757d887bb4d57682d23
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-02 22:31:00 +0000

    WebCore:
            Reviewed by Darin.
    
            WebCore part of fix for <rdar://problem/3631868> NSToolbar adoption:
            Tab key should cycle around toolbar and page content
    
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge nextKeyViewInsideWebFrameViews]):
            Made this method start looking from the current focus node. This won't
            affect any existing callers because there were no existing callers.
            (-[WebCoreBridge previousKeyViewInsideWebFrameViews]):
            ditto
    
    WebKit:
    
            Reviewed by Darin.
    
            WebKit part of fix for <rdar://problem/3631868> NSToolbar adoption:
            Tab key should cycle around toolbar and page content
    
            * WebView.subproj/WebHTMLView.m:
            removed overrides of nextKeyView and previousKeyView
            (-[WebHTMLView nextValidKeyView]):
            call super only if we can't move the focus within the frame hierarchy
            (-[WebHTMLView previousValidKeyView]):
            ditto
            * WebView.subproj/WebHTMLViewInternal.h:
            removed nextKeyViewAccessShouldMoveFocus ivar
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewInternal.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewInternal.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     BOOL needsLayout;
     BOOL needsToApplyStyles;
     BOOL inWindow;
-    BOOL nextKeyViewAccessShouldMoveFocus;
     BOOL ignoringMouseDraggedEvents;
     BOOL printing;
     BOOL initiatedDrag;
