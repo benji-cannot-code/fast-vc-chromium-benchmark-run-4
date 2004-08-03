84c11a595cf992a0285adc22ecdc256a1b99a703
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-03 21:41:23 +0000

    Reviewed by Ken.
    	- remove assertions that asserted text is non-empty, just avoid
    	doing anything for the empty case; this is needed because input
    	methods like to insert empty text in various cases.
    
            * khtml/editing/htmlediting_impl.cpp:
            (khtml::InsertTextCommandImpl::InsertTextCommandImpl):
            (khtml::InsertTextCommandImpl::doApply):
            (khtml::InsertTextCommandImpl::doUnapply):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7182 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting_impl.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting_impl.cpp
@@ -1847,7 +1847,6 @@ InsertTextCommandImpl::InsertTextCommandImpl(DocumentImpl *document, TextImpl *n
 {
     ASSERT(m_node);
     ASSERT(m_offset >= 0);
-    ASSERT(text.length() > 0);
     
     m_node->ref();
     m_text = text.copy(); // make a copy to ensure that the string never changes
@@ -1867,7 +1866,10 @@ int InsertTextCommandImpl::commandID() const
 void InsertTextCommandImpl::doApply()
 {
     ASSERT(m_node);
-    ASSERT(!m_text.isEmpty());
+
+    if (m_text.isEmpty())
+	return;
+
 
     int exceptionCode = 0;
     m_node->insertData(m_offset, m_text, exceptionCode);
@@ -1878,6 +1880,9 @@ void InsertTextCommandImpl::doUnapply()
 {
     ASSERT(m_node);
     ASSERT(!m_text.isEmpty());
+
+    if (m_text.isEmpty())
+	return;
 
     int exceptionCode = 0;
     m_node->deleteData(m_offset, m_text.length(), exceptionCode);
