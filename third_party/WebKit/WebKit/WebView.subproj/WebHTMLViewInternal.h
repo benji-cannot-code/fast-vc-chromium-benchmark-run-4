6e9f492c59531dc99cb6a9e64f475f6cfe785466
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-03 22:37:23 +0000

    Reviewed by Ken.
    	- implemented enough of the NSTextInput protocol and added the
    	proper calls to NSInputManager to allow input methods to
    	work. However, the text is not marked yet.
    
            * WebView.subproj/WebHTMLViewInternal.h: Added new fields to track
    	marked range.
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView markedRange]): Implemented
            (-[WebHTMLView conversationIdentifier]): Implemented
            (-[WebHTMLView hasMarkedText]): Implemented
            (-[WebHTMLView unmarkText]): Implemented
            (-[WebHTMLView setMarkedText:selectedRange:]): Implemented - does
    	not yet handle attributes in attributed strings
            (-[WebHTMLView insertText:]): Modified to handle replacing or abandoning
    	the marked text when set.
            (-[WebHTMLView _selectMarkedText]): new helper method, self-explanatory
            (-[WebHTMLView _setMarkedDOMRange:]): hitto.
            (-[WebHTMLView _selectRangeInMarkedText:]): ditto
            (-[WebHTMLView _discardMarkedText]): ditto
            (-[WebHTMLView _selectionIsInsideMarkedText]): ditto
            (-[WebHTMLView _updateSelectionForInputManager]): ditto
    
    	- did a bit of refactoring while I was in here.
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView _shouldReplaceSelectionWithText:givenAction:]): added this new
    	helper method to simplify the common case of calling the shouldInsertText: delegate
            (-[WebHTMLView pasteAsPlainText:]): use it
            (-[WebHTMLView insertTab:]): ditto
            (-[WebHTMLView insertNewline:]): ditto
            (-[WebHTMLView _changeWordCaseWithSelector:]): ditto
            (-[WebHTMLView _changeSpellingToWord:]): ditto
            (-[WebHTMLView _selectionChanged]): ditto
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7184 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewInternal.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewInternal.h
@@ -44,6 +44,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSArray* pageRects;
 
     BOOL resigningFirstResponder;
+
+    DOMNode *markedTextNode;
+    NSRange markedRangeInNode;
+    BOOL ignoreMarkedTextSelectionChange;
 }
 @end
 
