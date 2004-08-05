ed12509ff790489d504addde1b9ca2afd5d25583
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-05 21:52:32 +0000

    WebKit:
            Fixed unnecessary import of NSURLFileTypeMappings.
    
            * WebView.subproj/WebMainResourceClient.m:
    
    WebBrowser:
    
    	Fixed unnecessary import of NSURLFileTypeMappings.
    
            * BrowserWebController.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7201 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebMainResourceClient.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebMainResourceClient.m
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/NSHTTPCookie.h>
 #import <Foundation/NSError_NSURLExtras.h>
 #import <Foundation/NSString_NSURLExtras.h>
-#import <Foundation/NSURLFileTypeMappings.h>
 #import <Foundation/NSURLConnection.h>
 #import <Foundation/NSURLConnectionPrivate.h>
 #import <Foundation/NSURLDownloadPrivate.h>
