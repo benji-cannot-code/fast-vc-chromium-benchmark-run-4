c45e1637d790e6ad7d6867fc74f05cc709441985
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-11 06:26:32 +0000

    Reviewed by Trey.
            - fixed <rdar://problem/3710123> Loading iframe that replaces content in the parent document crashes Safari
    
            I fixed three problems:
    
                1) script interpreter destroyed while it was interpreting scripts, caused random havoc
                2) code trying to get to view after view was detached from part, caused nil-deref
                3) signals sent to parent after child was no longer in the parent's frames list, caused nil-deref
    
            Now the test page works fine. Hope the real sites do too.
    
            * khtml/khtml_part.h: Add connectChild and disconnectChild helper functions (private).
            * khtml/khtml_part.cpp:
            (KHTMLPart::clear): Call disconnectChild on each frame as we detach it (see below).
            (KHTMLPart::end): Ref the part at the start, and deref the part at the end, of this function.
            Otherwise, we can end up destroying the part, and hence the interpreter, inside a script that
            the interpreter itself is running.
            (KHTMLPart::slotFinishedParsing): Add another check for a nil m_view, after the call to
            checkCompleted.
            (KHTMLPart::checkCompleted): Remove bogus if statement with empty body.
            (KHTMLPart::processObjectRequest): Call disconnectChild to disconnect the child <-> parent signals of the
            old child that the new one is replacing, and connectChild to connect the signals (nicer factoring).
            (KHTMLPart::slotChildCompleted): Fixed up a confusing boolean if/expression to be simpler. Not related to
            the bug fix, but an earlier version of the fix had changes in this function.
            (KHTMLPart::connectChild): Added. Connects the appropriate signals for a child frame.
            (KHTMLPart::disconnectChild): Added. Disconnects the same signals that connectChild connects.
    
            * kwq/KWQKHTMLPart.mm: (KHTMLPart::frameDetached): Added a call to disconnectChild before removing the
            child from the frames list.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtml_part.cpp
+++ third_party/WebKit/WebCore/khtml/khtml_part.cpp
@@ -104,6 +104,7 @@ using namespace DOM;
 #endif
 
 using khtml::ApplyStyleCommand;
+using khtml::ChildFrame;
 using khtml::Decoder;
 using khtml::DeleteSelectionCommand;
 using khtml::EditCommand;
@@ -1019,6 +1020,7 @@ void KHTMLPart::clear()
     {
       if ( (*it).m_part )
       {
+        disconnectChild(&*it);
 #if !APPLE_CHANGES
         partManager()->removePart( (*it).m_part );
 #endif
@@ -1622,11 +1624,18 @@ void KHTMLPart::write( const QString &str )
 
 void KHTMLPart::end()
 {
+    // Make sure we're not deallocated half-way through due to JavaScript in the onload handler.
+    // With this ref we guarantee that if we are shut down, we won't actually destroy the object
+    // until we return here.
+    ref();
+
     // make sure nothing's left in there...
     if(d->m_decoder)
         write(d->m_decoder->flush());
     if (d->m_doc)
 	d->m_doc->finishParsing();
+
+    deref();
 }
 
 #if !APPLE_CHANGES
@@ -1680,6 +1689,9 @@ void KHTMLPart::slotFinishedParsing()
     return; // We are probably being destructed.
     
   checkCompleted();
+
+  if (!d->m_view)
+    return; // We are being destroyed by something checkCompleted called.
 
   // check if the scrollbars are really needed for the content
   // if not, remove them, relayout, and repaint
@@ -1795,19 +1807,11 @@ void KHTMLPart::checkCompleted()
 
   checkEmitLoadEvent(); // if we didn't do it before
 
-#if APPLE_CHANGES
-  if (d->m_view) {
-#endif
-
 #if !APPLE_CHANGES
   // check that the view has not been moved by the user  
   if ( !m_url.hasRef() && d->m_view->contentsY() == 0 )
       d->m_view->setContentsPos( d->m_extension->urlArgs().xOffset,
                                  d->m_extension->urlArgs().yOffset );
-#endif
-
-#if APPLE_CHANGES
-  } // if (d->m_view)
 #endif
 
   if ( d->m_scheduledRedirection != noRedirectionScheduled )
@@ -3102,6 +3106,7 @@ bool KHTMLPart::processObjectRequest( khtml::ChildFrame *child, const KURL &_url
     //CRITICAL STUFF
     if ( child->m_part )
     {
+      disconnectChild(child);
 #if !APPLE_CHANGES
       partManager()->removePart( (KParts::ReadOnlyPart *)child->m_part );
 #endif
@@ -3122,21 +3127,7 @@ bool KHTMLPart::processObjectRequest( khtml::ChildFrame *child, const KURL &_url
     child->m_part = part;
     assert( ((void*) child->m_part) != 0);
 
-    if ( child->m_type != khtml::ChildFrame::Object )
-    {
-      connect( part, SIGNAL( started( KIO::Job *) ),
-               this, SLOT( slotChildStarted( KIO::Job *) ) );
-      connect( part, SIGNAL( completed() ),
-               this, SLOT( slotChildCompleted() ) );
-      connect( part, SIGNAL( completed(bool) ),
-               this, SLOT( slotChildCompleted(bool) ) );
-      connect( part, SIGNAL( setStatusBarText( const QString & ) ),
-               this, SIGNAL( setStatusBarText( const QString & ) ) );
-      connect( this, SIGNAL( completed() ),
-               part, SLOT( slotParentCompleted() ) );
-      connect( this, SIGNAL( completed(bool) ),
-               part, SLOT( slotParentCompleted() ) );
-    }
+    connectChild(child);
 
 #if APPLE_CHANGES
   }
@@ -3566,8 +3557,8 @@ void KHTMLPart::slotChildCompleted( bool complete )
   child->m_bCompleted = true;
   child->m_args = KParts::URLArgs();
 
-  if ( parentPart() == 0 )
-    d->m_bPendingChildRedirection = (d->m_bPendingChildRedirection || complete);
+  if ( complete && parentPart() == 0 )
+    d->m_bPendingChildRedirection = true;
 
   checkCompleted();
 }
@@ -5448,6 +5439,46 @@ void KHTMLPart::print()
 }
 
 #endif
+
+void KHTMLPart::connectChild(const khtml::ChildFrame *child) const
+{
+    ReadOnlyPart *part = child->m_part;
+    if (part && child->m_type != ChildFrame::Object)
+    {
+        connect( part, SIGNAL( started( KIO::Job *) ),
+                 this, SLOT( slotChildStarted( KIO::Job *) ) );
+        connect( part, SIGNAL( completed() ),
+                 this, SLOT( slotChildCompleted() ) );
+        connect( part, SIGNAL( completed(bool) ),
+                 this, SLOT( slotChildCompleted(bool) ) );
+        connect( part, SIGNAL( setStatusBarText( const QString & ) ),
+                 this, SIGNAL( setStatusBarText( const QString & ) ) );
+        connect( this, SIGNAL( completed() ),
+                 part, SLOT( slotParentCompleted() ) );
+        connect( this, SIGNAL( completed(bool) ),
+                 part, SLOT( slotParentCompleted() ) );
+    }
+}
+
+void KHTMLPart::disconnectChild(const khtml::ChildFrame *child) const
+{
+    ReadOnlyPart *part = child->m_part;
+    if (part && child->m_type != ChildFrame::Object)
+    {
+        disconnect( part, SIGNAL( started( KIO::Job *) ),
+                    this, SLOT( slotChildStarted( KIO::Job *) ) );
+        disconnect( part, SIGNAL( completed() ),
+                    this, SLOT( slotChildCompleted() ) );
+        disconnect( part, SIGNAL( completed(bool) ),
+                    this, SLOT( slotChildCompleted(bool) ) );
+        disconnect( part, SIGNAL( setStatusBarText( const QString & ) ),
+                    this, SIGNAL( setStatusBarText( const QString & ) ) );
+        disconnect( this, SIGNAL( completed() ),
+                    part, SLOT( slotParentCompleted() ) );
+        disconnect( this, SIGNAL( completed(bool) ),
+                    part, SLOT( slotParentCompleted() ) );
+    }
+}
 
 using namespace KParts;
 #include "khtml_part.moc"
