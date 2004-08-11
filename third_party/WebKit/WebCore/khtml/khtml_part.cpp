b19e332e76dc597d91cd25d0cd601dc2e1bd5565
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-11 16:45:12 +0000

    Reviewed by John
            Fix for this bug:
    
            <rdar://problem/3732702> crash in CSSComputedStyleDeclarationImpl running devtools.com editing sample code
    
            * khtml/khtml_part.cpp:
            (KHTMLPart::selectionComputedStyle): Added a null check.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7229 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtml_part.cpp
+++ third_party/WebKit/WebCore/khtml/khtml_part.cpp
@@ -5408,6 +5408,9 @@ CSSStyleDeclarationImpl *KHTMLPart::selectionComputedStyle(NodeImpl *&nodeToRemo
     Position pos(range.startContainer().handle(), range.startOffset());
     assert(pos.notEmpty());
     ElementImpl *elem = pos.element();
+    if (!elem)
+        return 0;
+    
     ElementImpl *styleElement = elem;
     int exceptionCode = 0;
 
