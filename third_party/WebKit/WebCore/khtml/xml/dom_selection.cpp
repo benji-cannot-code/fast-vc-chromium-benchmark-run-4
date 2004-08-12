eb4ff4a2ca238cca6c7a5d6593967f28e69c9ea7
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-12 23:58:03 +0000

    Reviewed by Chris
            Fix for this bug:
    
            <rdar://problem/3761014> command-down-arrow takes you to start of document instead of end of document
    
            * khtml/xml/dom_selection.cpp:
            (DOM::Selection::modifyExtendingRightForward): Code used to assume, incorrectly, that index 1 of the
            document element was beyond the last node in the document. But this is not true, since the document
            element is the HTML element (generally). Instead, move to the index equal to the number of children
            of the document element. This puts us past everything.
            (DOM::Selection::modifyMovingRightForward): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7249 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_selection.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_selection.cpp
@@ -215,9 +215,11 @@ Position Selection::modifyExtendingRightForward(ETextGranularity granularity)
         case PARAGRAPH:
             // not implemented
             break;
-        case DOCUMENT:
-            pos = Position(start().node()->getDocument()->documentElement(), 1);
+        case DOCUMENT: {
+            ElementImpl *elem = start().node()->getDocument()->documentElement();
+            pos = Position(elem, elem->childNodeCount());
             break;
+        }
         case LINE_BOUNDARY: {
             Selection selection;
             startAndEndLineNodesIncludingNode(end().node(), end().offset(), selection);
@@ -248,9 +250,11 @@ Position Selection::modifyMovingRightForward(ETextGranularity granularity)
         case PARAGRAPH:
             // not implemented
             break;
-        case DOCUMENT:
-            pos = Position(start().node()->getDocument()->documentElement(), 1);
+        case DOCUMENT: {
+            ElementImpl *elem = start().node()->getDocument()->documentElement();
+            pos = Position(elem, elem->childNodeCount());
             break;
+        }
         case LINE_BOUNDARY: {
             Selection selection;
             startAndEndLineNodesIncludingNode(end().node(), end().offset(), selection);
