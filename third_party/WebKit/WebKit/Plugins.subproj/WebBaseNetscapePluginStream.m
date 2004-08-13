11e2a1a424f8e3ea7a53e014d1cbb59a4f50e381
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-13 17:45:12 +0000

    Reviewed by Chris.
            - fixed <rdar://problem/3760924> Carbon path passed in NPP_StreamAsFile must be in local character set, not UTF-8
    
            * Plugins.subproj/WebBaseNetscapePluginStream.m:
            (CarbonPathFromPOSIXPath): Added. New function that uses the path pieces from an FSSpec.
            This has many advantages; the big one that fixes the bug is that it gives the mangled names
            that work even for files that have names that can't otherwise be encoded in Carbon-style
            path names. I didn't write this from scratch: I started with the method in Foundation
            and just changed it to use FSSpec.
            (-[WebBaseNetscapePluginStream destroyStream]): Remove code that used NSString and just call
            CarbonPathFromPOSIXPath instead.
    
            * Plugins.subproj/npapi.h: The Revision tag wanted to touch this file.
            I think we are going to have some trouble with this; I'd like to take that out.
    
            * English.lproj/StringsNotToBeLocalized.txt: Updated.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginStream.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginStream.m
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/NSFileManager_NSURLExtras.h>
 #import <Foundation/NSURL_NSURLExtras.h>
 
+static const char *CarbonPathFromPOSIXPath(const char *posixPath);
+
 @implementation WebBaseNetscapePluginStream
 
 - (void)dealloc
@@ -151,9 +153,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     
     if (reason == NPRES_DONE && (transferMode == NP_ASFILE || transferMode == NP_ASFILEONLY)) {
         ASSERT(path != NULL);
-        NSString *carbonPath = [[NSFileManager defaultManager] _web_carbonPathForPath:[NSString stringWithUTF8String:path]];
-        NPP_StreamAsFile(instance, &stream, [carbonPath fileSystemRepresentation]);
-        LOG(Plugins, "NPP_StreamAsFile URL=%@ path=%@", URL, carbonPath);
+        const char *carbonPath = CarbonPathFromPOSIXPath(path);
+        ASSERT(carbonPath != NULL);
+        NPP_StreamAsFile(instance, &stream, carbonPath);
+        LOG(Plugins, "NPP_StreamAsFile URL=%@ path=%s", URL, carbonPath);
     }
     
     NPError npErr;
@@ -290,3 +293,56 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 @end
+
+static const char *CarbonPathFromPOSIXPath(const char *posixPath)
+{
+    // Returns NULL if path is to file that does not exist.
+    // Doesn't add a trailing colon for directories; this is a problem for paths to a volume,
+    // so this function would need to be revised if we ever wanted to call it with that.
+
+    OSStatus error;
+    FSCatalogInfo info;
+
+    // Make an FSRef.
+    FSRef ref;
+    error = FSPathMakeRef((const UInt8 *)posixPath, &ref, NULL);
+    if (error != noErr) {
+        return NULL;
+    }
+
+    // Get volume refNum.
+    error = FSGetCatalogInfo(&ref, kFSCatInfoVolume, &info, NULL, NULL, NULL);
+    if (error != noErr) {
+        return NULL;
+    }
+
+    // Get root directory FSRef.
+    FSRef rootRef;
+    error = FSGetVolumeInfo(info.volume, 0, NULL, kFSVolInfoNone, NULL, NULL, &rootRef);
+    if (error != noErr) {
+        return NULL;
+    }
+
+    // Get the pieces of the path.
+    NSMutableData *carbonPath = [NSMutableData dataWithBytes:"" length:1];
+    BOOL needColon = NO;
+    for (;;) {
+        FSSpec spec;
+        FSRef parentRef;
+        error = FSGetCatalogInfo(&ref, kFSCatInfoNone, NULL, NULL, &spec, &parentRef);
+        if (error != noErr) {
+            return NULL;
+        }
+        if (needColon) {
+            [carbonPath replaceBytesInRange:NSMakeRange(0, 0) withBytes:":" length:1];
+        }
+        [carbonPath replaceBytesInRange:NSMakeRange(0, 0) withBytes:&spec.name[1] length:spec.name[0]];
+        needColon = YES;
+        if (FSCompareFSRefs(&ref, &rootRef) == noErr) {
+            break;
+        }
+        ref = parentRef;
+    }
+
+    return (const char *)[carbonPath bytes];
+}
