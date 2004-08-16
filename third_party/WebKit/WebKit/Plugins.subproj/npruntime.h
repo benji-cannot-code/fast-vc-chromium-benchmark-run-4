990d451a8ec5745f2436cb9c221a00f4134a0515
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-16 03:41:56 +0000

    WebKit:         More changes to np headers.
            Reviewed by Darin.
    
            * Plugins.subproj/WebNetscapePluginPackage.m:
            (-[WebNetscapePluginPackage load]):
            * Plugins.subproj/npapi.h:
            * Plugins.subproj/npfunctions.h:
            * Plugins.subproj/npruntime.h:
    
    JavaScriptCore:
    	More updates to np headers.  Implemented new NPN functions.
    
            Reviewed by Darin.
    
            * bindings/NP_jsobject.cpp:
            (NPN_HasProperty):
            (NPN_HasMethod):
            * bindings/npapi.h:
            * bindings/npruntime.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7260 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/npruntime.h
+++ third_party/WebKit/WebKit/Plugins.subproj/npruntime.h
@@ -103,7 +103,8 @@ extern "C" {
     header.
 
     To the extent possible the assumptions about the scripting
-    language used by the scripting environment have been minimized.*/
+    language used by the scripting environment have been minimized.
+*/
 
 
 /*
