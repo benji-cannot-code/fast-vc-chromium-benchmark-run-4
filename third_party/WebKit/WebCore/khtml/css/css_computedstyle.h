a2f879a4ca084e9e374455fa411e41b09a426a9e
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-18 17:34:22 +0000

    Coded by Darin and Ken
            * khtml/css/css_computedstyle.cpp: Added CopyProperties static array. This contains
            the properties we implement that we also want to copy in the new
            CSSComputedStyleDeclarationImpl::copy described below.
            (DOM::CSSComputedStyleDeclarationImpl::copy): New function. In this class, copies the
            computed values of all the properties listed in CopyProperties. In essence, this makes
            a freeze-dired version of a computed style.
            (DOM::CSSComputedStyleDeclarationImpl::diff): Removes every property from the passed-in
            CSSStyleDeclarationImpl that is also in the computed style.
            * khtml/css/css_computedstyle.h:
            * khtml/css/css_valueimpl.cpp:
            (CSSStyleDeclarationImpl::copy): New function. In this class, the copy operation is
            straightforward. Returns a copy that will be unchanged when the original changes.
            * khtml/css/css_valueimpl.h:
            (DOM::CSSStyleDeclarationImpl::values): Added a accessor suitable for use when the
            CSSStyleDeclarationImpl is const.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/css/css_computedstyle.h
+++ third_party/WebKit/WebCore/khtml/css/css_computedstyle.h
@@ -59,6 +59,9 @@ public:
     virtual void setProperty(const DOMString &propertyString);
     virtual DOMString item(unsigned long index) const;
 
+    virtual CSSStyleDeclarationImpl *copy() const;
+    void diff(CSSStyleDeclarationImpl *) const;
+
 private:
     CSSProperty property(int id) const;
 
