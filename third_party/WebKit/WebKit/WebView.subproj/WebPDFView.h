a621c650f7c8a44b55cd7cf04d925f11b470064c
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-18 01:16:50 +0000

    Fixed <rdar://problem/3759093> Need PDF context menu: "Open in Preview" or other external app
            Added support for "Open with ..." in PDF view.
    
            Reviewed by Chris and Trey.
    
            * ChangeLog:
            * English.lproj/Localizable.strings:
            * English.lproj/StringsNotToBeLocalized.txt:
            * WebView.subproj/WebPDFView.h:
            * WebView.subproj/WebPDFView.m:
            (-[WebPDFView initWithFrame:]):
            (-[WebPDFView dealloc]):
            (applicationInfoForMIMEType):
            (-[WebPDFView path]):
            (-[WebPDFView menuForEvent:]):
            (-[WebPDFView setDataSource:]):
            (-[WebPDFView layout]):
            (-[WebPDFView viewDidMoveToHostWindow]):
            (-[WebPDFView openWithFinder:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7283 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebPDFView.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebPDFView.h
@@ -9,7 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Quartz/Quartz.h>
 
+@class WebDataSource;
+
 @interface WebPDFView : PDFView <WebDocumentView>
+{
+    WebDataSource *dataSource;
+    NSString *path;
+    BOOL written;
+}
 @end
 
 #endif  // MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_3
