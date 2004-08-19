fa59f07253f8c8e8efb7379668b3bc2d3507187b
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-19 00:15:47 +0000

    Make the header overlap the footer in the zero-line case.  Changes to Emerson's template will ensure the 	header draws over the footer.
            Reviewed by darin
    
            * khtml/rendering/render_block.cpp:
            (khtml::getHeightForLineCount):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7289 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_block.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_block.cpp
@@ -2893,8 +2893,7 @@ int getHeightForLineCount(RenderBlock* block, int l, bool includeBottom, int& co
                     normalFlowChildWithoutLines = obj;
             }
             if (normalFlowChildWithoutLines && l == 0)
-                return normalFlowChildWithoutLines->yPos() + normalFlowChildWithoutLines->height() + 
-                    (includeBottom ? (block->borderBottom() + block->paddingBottom()) : 0);
+                return normalFlowChildWithoutLines->yPos() + normalFlowChildWithoutLines->height();
         }
     }
     
