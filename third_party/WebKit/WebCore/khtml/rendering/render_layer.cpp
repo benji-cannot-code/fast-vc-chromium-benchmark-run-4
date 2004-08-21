683fa303074f3f209321989f347713e9c573c961
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-21 00:58:24 +0000

    Divorce the notion of a marquee being stopped from JS from the notion of being suspended by the back/forward cache.
            * khtml/ecma/kjs_html.cpp:
            (KJS::HTMLElementFunction::tryCall):
            * khtml/rendering/render_layer.cpp:
            (m_direction):
            (Marquee::start):
            (Marquee::stop):
            (Marquee::updateMarqueePosition):
            * khtml/rendering/render_layer.h:
            (khtml::):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_layer.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_layer.cpp
@@ -1289,7 +1289,7 @@ void RenderLayer::suspendMarquees()
 
 Marquee::Marquee(RenderLayer* l)
 :m_layer(l), m_currentLoop(0), m_timerId(0), m_start(0), m_end(0), m_speed(0), m_unfurlPos(0), m_reset(false),
- m_suspended(false), m_whiteSpace(NORMAL), m_direction(MAUTO)
+ m_suspended(false), m_stopped(false), m_whiteSpace(NORMAL), m_direction(MAUTO)
 {
 }
 
@@ -1388,6 +1388,8 @@ void Marquee::start()
     if (m_timerId || m_layer->renderer()->style()->marqueeIncrement().value == 0)
         return;
     
+    m_stopped = false;
+
     if (!m_suspended) {
         if (isUnfurlMarquee()) {
             bool forward = direction() == MDOWN || direction() == MRIGHT;
@@ -1418,6 +1420,16 @@ void Marquee::suspend()
     m_suspended = true;
 }
 
+void Marquee::stop()
+{
+    if (m_timerId) {
+        killTimer(m_timerId);
+        m_timerId = 0;
+    }
+    
+    m_stopped = true;
+}
+
 void Marquee::updateMarqueePosition()
 {
     bool activate = (m_totalLoops <= 0 || m_currentLoop < m_totalLoops);
@@ -1437,7 +1449,8 @@ void Marquee::updateMarqueePosition()
             m_start = computePosition(direction(), behavior == MALTERNATE);
             m_end = computePosition(reverseDirection(), behavior == MALTERNATE || behavior == MSLIDE);
         }
-        start();
+        if (!m_stopped)
+            start();
     }
 }
 
