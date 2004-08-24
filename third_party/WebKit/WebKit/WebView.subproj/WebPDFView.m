cfdcfbd16f3c3cf383923a0ed0d05e3f2e74860e
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-24 21:17:30 +0000

    Fixed <rdar://problem/3770469> Some PDFs open with line of previous page above PDF view
            Set height resize flags on WebPDFView.
    
            Reviewed by Chris.
    
            * WebView.subproj/WebPDFView.m:
            (-[WebPDFView initWithFrame:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7342 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebPDFView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebPDFView.m
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     self = [super initWithFrame:frame];
     if (self) {
-        [self setAutoresizingMask:NSViewWidthSizable];
+        [self setAutoresizingMask:NSViewWidthSizable|NSViewHeightSizable];
             written = NO;
     }
     return self;
