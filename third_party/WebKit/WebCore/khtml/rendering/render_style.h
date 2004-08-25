1b93135527adbba92f3ada56e091a90afa91514c
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-25 19:04:54 +0000

    Fix the "extra space in TypePad blogs" Emerson problem by adding the ability to collapse away margins. 	Also added support for explicitly preventing margin collapsing.
            Reviewed by mjs
    
            * khtml/css/cssparser.cpp:
            (CSSParser::parseValue):
            * khtml/css/cssproperties.c:
            (hash_prop):
            (findProp):
            * khtml/css/cssproperties.h:
            * khtml/css/cssproperties.in:
            * khtml/css/cssstyleselector.cpp:
            (khtml::CSSStyleSelector::applyProperty):
            * khtml/css/cssvalues.c:
            (hash_val):
            (findValue):
            * khtml/css/cssvalues.h:
            * khtml/css/cssvalues.in:
            * khtml/rendering/render_block.cpp:
            (khtml::RenderBlock::isSelfCollapsingBlock):
            (khtml::RenderBlock::layoutBlockChildren):
            * khtml/rendering/render_style.cpp:
            (StyleSurroundData::StyleSurroundData):
            (StyleSurroundData::operator==):
            (RenderStyle::diff):
            * khtml/rendering/render_style.h:
            (khtml::):
            (khtml::RenderStyle::marginTopCollapse):
            (khtml::RenderStyle::marginBottomCollapse):
            (khtml::RenderStyle::setMarginTopCollapse):
            (khtml::RenderStyle::setMarginBottomCollapse):
            (khtml::RenderStyle::initialMarginTopCollapse):
            (khtml::RenderStyle::initialMarginBottomCollapse):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7362 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_style.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_style.h
@@ -285,6 +285,8 @@ public:
 
 };
 
+enum EMarginCollapse { MCOLLAPSE, MSEPARATE, MDISCARD };
+
 class StyleSurroundData : public Shared<StyleSurroundData>
 {
 public:
@@ -300,6 +302,8 @@ public:
     LengthBox margin;
     LengthBox padding;
     BorderData border;
+    EMarginCollapse marginTopCollapse : 2;
+    EMarginCollapse marginBottomCollapse : 2;
 };
 
 
@@ -1109,6 +1113,8 @@ public:
     EUserDrag userDrag() const { return css3NonInheritedData->userDrag; }
     EUserSelect userSelect() const { return css3NonInheritedData->userSelect; }
     bool textOverflow() const { return css3NonInheritedData->textOverflow; }
+    EMarginCollapse marginTopCollapse() const { return surround->marginTopCollapse; }
+    EMarginCollapse marginBottomCollapse() const { return surround->marginBottomCollapse; }
     // End CSS3 Getters
 
 #if APPLE_CHANGES
@@ -1289,6 +1295,8 @@ public:
     void setUserDrag(EUserDrag d) { SET_VAR(css3NonInheritedData, userDrag, d); }
     void setUserSelect(EUserSelect s) { SET_VAR(css3NonInheritedData, userSelect, s); }
     void setTextOverflow(bool b) { SET_VAR(css3NonInheritedData, textOverflow, b); }
+    void setMarginTopCollapse(EMarginCollapse c) { SET_VAR(surround, marginTopCollapse, c); }
+    void setMarginBottomCollapse(EMarginCollapse c) { SET_VAR(surround, marginBottomCollapse, c); }
     // End CSS3 Setters
    
 #if APPLE_CHANGES
@@ -1396,6 +1404,8 @@ public:
     static EUserDrag initialUserDrag() { return DRAG_AUTO; }
     static EUserSelect initialUserSelect() { return SELECT_AUTO; }
     static bool initialTextOverflow() { return false; }
+    static EMarginCollapse initialMarginTopCollapse() { return MCOLLAPSE; }
+    static EMarginCollapse initialMarginBottomCollapse() { return MCOLLAPSE; }
 
 #if APPLE_CHANGES
     // Keep these at the end.
