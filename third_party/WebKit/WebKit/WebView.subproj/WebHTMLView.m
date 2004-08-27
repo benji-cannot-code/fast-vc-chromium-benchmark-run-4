7d75b4c4582ad94601a4adacbba88e501a5c6f02
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-27 00:38:25 +0000

    Tweak to last check-in.
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView _documentFragmentFromPasteboard:allowPlainText:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -293,8 +293,8 @@ static WebElementOrTextFilter *elementOrTextFilterInstance = nil;
         return fragment;
     }
     
-    NSAttributedString *string = nil;
     if ([NSAttributedString instancesRespondToSelector:@selector(_documentFromRange:document:documentAttributes:subresources:)]) {
+        NSAttributedString *string = nil;
         if ([types containsObject:NSRTFDPboardType]) {
             string = [[NSAttributedString alloc] initWithRTFD:[pasteboard dataForType:NSRTFDPboardType] documentAttributes:NULL];
         }
@@ -305,9 +305,8 @@ static WebElementOrTextFilter *elementOrTextFilterInstance = nil;
             NSArray *elements = [[NSArray alloc] initWithObjects:@"style", nil];
             NSDictionary *documentAttributes = [[NSDictionary alloc] initWithObjectsAndKeys:elements, NSExcludedElementsDocumentAttribute, nil];
             [elements release];
-            NSRange range = NSMakeRange(0, [string length]);
             NSArray *subresources;
-            DOMDocumentFragment *fragment = [string _documentFromRange:range 
+            DOMDocumentFragment *fragment = [string _documentFromRange:NSMakeRange(0, [string length]) 
                                                               document:[[self _bridge] DOMDocument] 
                                                     documentAttributes:documentAttributes
                                                           subresources:&subresources];
