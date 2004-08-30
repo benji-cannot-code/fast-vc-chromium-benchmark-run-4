458f25f512a3b44106b871d13ace86e200070acc
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-08-30 18:00:15 +0000

    Reviewed by Ken.
            - improved multiple submit logic in preparation for making command-click on a form
              button load a form in another frame
    
            * kwq/KWQKHTMLPart.h: Added const to a bunch of member functions for cleanup.
            Added prepareForUserAction member function.
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::submitForm): Simplified logic and removed special case for "http" and "https".
            (KWQKHTMLPart::renderer): Added const.
            (KWQKHTMLPart::keyEvent): Added call to prepareForUserAction.
            (KWQKHTMLPart::lastEventIsMouseUp): Added const.
            (KWQKHTMLPart::eventMayStartDrag): Added const.
            (KWQKHTMLPart::mouseDown): Added call to prepareForUserAction.
            (KWQKHTMLPart::overrideMediaType): Added const.
            (KWQKHTMLPart::canGoBackOrForward): Added const.
            (KWQKHTMLPart::prepareForUserAction): Added. Clears _submittedFormURL.
            * kwq/KWQAccObject.mm: (-[KWQAccObject accessibilityPerformAction:]): Added call to prepareForUserAction.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKHTMLPart.h
+++ third_party/WebKit/WebCore/kwq/KWQKHTMLPart.h
@@ -196,7 +196,7 @@ public:
 
     void KWQKHTMLPart::addMessageToConsole(const QString &message,  unsigned int lineNumber, const QString &sourceID);
     using KHTMLPart::xmlDocImpl;
-    khtml::RenderObject *renderer();
+    khtml::RenderObject *renderer() const;
     void forceLayout();
     void forceLayoutWithPageWidthRange(float minPageWidth, float maxPageWidth);
     void sendResizeEvent();
@@ -241,10 +241,11 @@ public:
     void sendFakeEventsAfterWidgetTracking(NSEvent *initiatingEvent);
     void mouseMoved(NSEvent *);
     bool keyEvent(NSEvent *);
-    bool lastEventIsMouseUp();
+
+    bool lastEventIsMouseUp() const;
     void setActivationEventNumber(int num) { _activationEventNumber = num; }
 
-    bool eventMayStartDrag(NSEvent *);
+    bool eventMayStartDrag(NSEvent *) const;
     void dragSourceMovedTo(const QPoint &loc);
     void dragSourceEndedAt(const QPoint &loc, NSDragOperation operation);
 
@@ -256,6 +257,10 @@ public:
     bool tryPaste();
     
     bool sendContextMenuEvent(NSEvent *);
+
+    // Call this method before handling a new user action, like on a mouse down or key down.
+    // Currently, all this does is clear the "don't submit form twice" data member.
+    void prepareForUserAction();
 
     void clearTimers();
     static void clearTimers(KHTMLView *);
@@ -280,7 +285,7 @@ public:
 
     KWQWindowWidget *topLevelWidget();
     
-    QString overrideMediaType();
+    QString overrideMediaType() const;
     
     void setMediaType(const QString &);
 
@@ -333,7 +338,8 @@ public:
     DOM::Range markedRange() const;
     void setMarkedRange(const DOM::Range &);
 
-    bool canGoBackOrForward(int distance);
+    bool canGoBackOrForward(int distance) const;
+
 private:
     virtual void khtmlMousePressEvent(khtml::MousePressEvent *);
     virtual void khtmlMouseDoubleClickEvent(khtml::MouseDoubleClickEvent *);
