806160c4659661988973635d667d69d4bc66a9db
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-01 22:17:57 +0000

    Fixed deployment build failure.
            * WebView.subproj/WebDefaultContextMenuDelegate.m:
            (-[WebDefaultUIDelegate menuItemWithTag:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7429 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDefaultContextMenuDelegate.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDefaultContextMenuDelegate.m
@@ -34,8 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSMenuItem *menuItem = [[[NSMenuItem alloc] init] autorelease];
     [menuItem setTag:tag];
     
-    NSString *title;
-    SEL action;
+    NSString *title = nil;
+    SEL action = NULL;
     
     switch(tag) {
         case WebMenuItemTagOpenLinkInNewWindow:
