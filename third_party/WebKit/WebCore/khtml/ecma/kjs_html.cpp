3237d939ef34889dcb88df760ec1981426635671
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-01 22:53:29 +0000

    Fixed panther build glitch.
            Reviewed by Ken.
    
            * khtml/ecma/kjs_html.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7430 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_html.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_html.cpp
@@ -3375,6 +3375,10 @@ static bool isImagePattern(const Value &value)
     return false;
 }
 
+extern "C" {
+    extern void CGContextReplacePathWithStrokedPath(CGContextRef c);
+}
+
 Value KJS::Context2DFunction::tryCall(ExecState *exec, Object &thisObj, const List &args)
 {
     if (!thisObj.inherits(&Context2D::info)) {
