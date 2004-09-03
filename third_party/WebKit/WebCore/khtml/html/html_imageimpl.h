70815e189a7a6b6bd3ef79c7b157af8e1c930c4e
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-03 17:35:22 +0000

    Made drawImage() in <canvas> compliant with whatwg spec.
            Reviewed by John.
    
            * khtml/ecma/kjs_html.cpp:
            (KJS::Context2DFunction::tryCall):
            * khtml/html/html_imageimpl.h:
            (DOM::HTMLImageElementImpl::pixmap):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7444 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_imageimpl.h
+++ third_party/WebKit/WebCore/khtml/html/html_imageimpl.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "misc/khtmllayout.h"
 #include "rendering/render_object.h"
 
+#include <loader.h>
+
 #include <qregion.h>
 #include <qmap.h>
 #include <qpixmap.h>
@@ -96,6 +98,7 @@ public:
 
 #if APPLE_CHANGES
     QString compositeOperator() const { return _compositeOperator; }
+    const QPixmap &pixmap() { return m_imageLoader.image()->pixmap(); }
 #endif
     
 protected:
