56394db2077d8ff0aa0526cfc89d2d789b55ba02
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-04 00:21:49 +0000

    Fix for 3779083, deleting large amount of text is really slow.  Don't use NodeLists in editing remove commands.
            Reviewed by darin
    
            * khtml/editing/htmlediting_impl.cpp:
            (khtml::RemoveNodeCommandImpl::RemoveNodeCommandImpl):
            (khtml::RemoveNodePreservingChildrenCommandImpl::doApply):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting_impl.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting_impl.cpp
@@ -2116,14 +2116,7 @@ RemoveNodeCommandImpl::RemoveNodeCommandImpl(DocumentImpl *document, NodeImpl *r
     ASSERT(m_parent);
     m_parent->ref();
     
-    NodeListImpl *children = m_parent->childNodes();
-    for (int i = children->length(); i >= 0; i--) {
-        NodeImpl *node = children->item(i);
-        if (node == m_removeChild)
-            break;
-        m_refChild = node;
-    }
-    
+    m_refChild = m_removeChild->nextSibling();
     if (m_refChild)
         m_refChild->ref();
 }
@@ -2189,12 +2182,9 @@ int RemoveNodePreservingChildrenCommandImpl::commandID() const
 
 void RemoveNodePreservingChildrenCommandImpl::doApply()
 {
-    NodeListImpl *children = node()->childNodes();
-    int length = children->length();
-    for (int i = 0; i < length; i++) {
-        NodeImpl *child = children->item(0);
-        removeNode(child);
-        insertNodeBefore(child, node());
+    while (NodeImpl* curr = node()->firstChild()) {
+        removeNode(curr);
+        insertNodeBefore(curr, node());
     }
     removeNode(node());
 }
