3d9e85972bc565d0753f14c357a7dbd984cec894
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-07 21:26:06 +0000

    Fixed: 	<rdar://problem/3080103> Need to pass cmd-modified keys to plug-ins 	<rdar://problem/3751509> can't use safari edit menu to copy and paste with Vantage Learning's My Access
            Reviewed by NOBODY (OOPS!).
    
            * Plugins.subproj/WebBaseNetscapePluginView.m: fake up command-key events for cut, copy, paste and select all so these menu items work for plug-ins
            (-[WebBaseNetscapePluginView sendModifierEventWithKeyCode:character:]):
            (-[WebBaseNetscapePluginView cut:]):
            (-[WebBaseNetscapePluginView copy:]):
            (-[WebBaseNetscapePluginView paste:]):
            (-[WebBaseNetscapePluginView selectAll:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7471 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebURLsWithTitles.m
+++ third_party/WebKit/WebKit/History.subproj/WebURLsWithTitles.m
@@ -78,7 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     count = [URLStrings count];
     URLs = [NSMutableArray arrayWithCapacity:count];
     for (index = 0; index < count; ++index) {
-        [URLs addObject:[NSURL _web_URLWithDataAsString:[URLStrings objectAtIndex:index]]];
+        [URLs addObject:[NSURL _web_URLWithUserTypedString:[URLStrings objectAtIndex:index]]];
     }
 
     return URLs;
