cb8295c210035643e9354ddece898429310ce10d
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-08 21:48:05 +0000

    Fixed: <rdar://problem/3792893> WebBaseResourceHandleDelegate always returns cached data for subresource loads
            Reviewed by rjw.
    
            * WebView.subproj/WebBaseResourceHandleDelegate.m:
            (-[WebBaseResourceHandleDelegate _canUseResourceForRequest:]): new
            (-[WebBaseResourceHandleDelegate loadWithRequest:]): call _canUseResourceForRequest:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7488 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebLoader.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebLoader.m
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/NSURLResponse.h>
 #import <Foundation/NSURLResponsePrivate.h>
 #import <Foundation/NSError_NSURLExtras.h>
+#import <Foundation/NSString_NSURLExtras.h>
 
 #import <WebKit/WebAssertions.h>
 #import <WebKit/WebDataProtocol.h>
@@ -147,6 +148,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
 }
 
+// This is copied from [NSHTTPURLProtocol _cachedResponsePassesValidityChecks] and modified for our needs.
+// FIXME: It would be nice to eventually to share this code somehow.
+- (BOOL)_canUseResourceForRequest:(NSURLRequest *)theRequest
+{
+    NSURLRequestCachePolicy policy = [theRequest cachePolicy];
+        
+    if (policy == NSURLRequestReturnCacheDataElseLoad) {
+        return YES;
+    } else if (policy == NSURLRequestReturnCacheDataDontLoad) {
+        return YES;
+    } else if (policy == NSURLRequestReloadIgnoringCacheData) {
+        return NO;
+    } else if ([theRequest valueForHTTPHeaderField:@"must-revalidate"] != nil) {
+        return NO;
+    } else if ([theRequest valueForHTTPHeaderField:@"proxy-revalidate"] != nil) {
+        return NO;
+    } else if ([theRequest valueForHTTPHeaderField:@"If-Modified-Since"] != nil) {
+        return NO;
+    } else if ([theRequest valueForHTTPHeaderField:@"Cache-Control"] != nil) {
+	return NO;
+    } else if ([[theRequest HTTPMethod] _web_isCaseInsensitiveEqualToString:@"POST"]) {
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
 - (BOOL)loadWithRequest:(NSURLRequest *)r
 {
     ASSERT(connection == nil);
@@ -161,7 +189,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     r = [self connection:connection willSendRequest:r redirectResponse:nil];
     
-    if ([[r URL] isEqual:originalURL]) {
+    if ([[r URL] isEqual:originalURL] && [self _canUseResourceForRequest:r]) {
         resource = [dataSource subresourceForURL:originalURL];
         if (resource) {
             [resource retain];
