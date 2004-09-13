fdb9b2baccad35b2c06f28a37d787674b68abee9
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-13 06:05:36 +0000

    Reviewed by Maciej.
            - fixed <rdar://problem/3792082> zero character codes in UTF-16 input cause crash
    
            * kwq/KWQTextCodec.mm: (KWQTextDecoder::convertUTF16): Fixed code that was using the wrong variable
            to decrement the input length, causing buffer overrun.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7519 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextCodec.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextCodec.mm
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2003 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2004 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -322,7 +322,7 @@ QString KWQTextDecoder::convertUTF16(const unsigned char *s, int length)
             }
         }
         result.append(reinterpret_cast<QChar *>(buffer), bufferLength);
-        len -= bufferLength * 2;
+        len -= runLength * 2;
     }
     
     if (len) {
