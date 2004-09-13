67ad6593e060e726ca7356c6b27ed55e38fce616
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-13 17:56:28 +0000

    Support for: <rdar://problem/3794790> drop rate or time remaining from download status when window is too small to fit it
            Reviewed by john.
    
            * Misc.subproj/WebStringTruncator.h:
            * Misc.subproj/WebStringTruncator.m:
            (+[WebStringTruncator widthOfString:font:]): new
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebStringTruncator.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebStringTruncator.h
@@ -22,4 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 + (NSString *)rightTruncateString:(NSString *)string toWidth:(float)maxWidth withFont:(NSFont *)font;
 
++ (float)widthOfString:(NSString *)string font:(NSFont *)font;
+
 @end
