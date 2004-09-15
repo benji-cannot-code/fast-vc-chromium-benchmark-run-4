3c49a6c3a61cd42ff69a1081fb6c253fcd5f7bac
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-15 05:57:49 +0000

    Reviewed by Maciej.
            - fixed <rdar://problem/3788894> REGRESSION (Mail): ctrl-t emacs key binding does not work (transpose)
            - fixed <rdar://problem/3798946> REGRESSION (Mail): Cursor does not disappear when typing
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView keyDown:]): Hide cursor by calling setHiddenUntilMouseMoves:YES.
            (-[WebHTMLView transpose:]): Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7569 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -2521,6 +2521,8 @@ static WebHTMLView *lastHitView = nil;
 
 - (void)keyDown:(NSEvent *)event
 {
+    BOOL callSuper = NO;
+
     _private->keyDownEvent = event;
 
     WebBridge *bridge = [self _bridge];
@@ -2535,8 +2537,13 @@ static WebHTMLView *lastHitView = nil;
         if ([self _canType] && [self _interceptEditingKeyEvent:event]) {
             // Consumed by key bindings manager.
         } else {
-            [super keyDown:event];
+            callSuper = YES;
         }
+    }
+    if (callSuper) {
+        [super keyDown:event];
+    } else {
+        [NSCursor setHiddenUntilMouseMoves:YES];
     }
 
     _private->keyDownEvent = nil;
@@ -3622,6 +3629,28 @@ static DOMRange *unionDOMRanges(DOMRange *a, DOMRange *b)
     [bridge setMarkDOMRange:selection];
 }
 
+- (void)transpose:(id)sender
+{
+    WebBridge *bridge = [self _bridge];
+    DOMRange *r = [bridge rangeOfCharactersAroundCaret];
+    if (!r) {
+        return;
+    }
+    NSString *characters = [bridge stringForRange:r];
+    if ([characters length] != 2) {
+        return;
+    }
+    NSString *transposed = [[characters substringFromIndex:1] stringByAppendingString:[characters substringToIndex:1]];
+    WebView *webView = [self _webView];
+    if (![[webView _editingDelegateForwarder] webView:webView shouldChangeSelectedDOMRange:[self _selectedRange] toDOMRange:r affinity:NSSelectionAffinityUpstream stillSelecting:NO]) {
+        return;
+    }
+    [bridge setSelectedDOMRange:r affinity:NSSelectionAffinityUpstream];
+    if ([self _shouldReplaceSelectionWithText:transposed givenAction:WebViewInsertActionTyped]) {
+        [bridge replaceSelectionWithText:transposed selectReplacement:NO];
+    }
+}
+
 #if 0
 
 // CSS does not have a way to specify an outline font, which may make this difficult to implement.
@@ -3633,8 +3662,6 @@ static DOMRange *unionDOMRanges(DOMRange *a, DOMRange *b)
 - (void)insertTable:(id)sender;
 
 // === methods with standard key bindings
-
-- (void)transpose:(id)sender;
 
 // Implementing these requires motion by paragraph.
 // Currently move right by paragraph is actually "move to end of paragraph".
