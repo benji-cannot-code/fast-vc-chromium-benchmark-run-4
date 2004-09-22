94633d79b23fc1ad95a9175e0da70cd4c0a373f8
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-22 22:17:28 +0000

    Reviewed by Hyatt
            * khtml/css/css_computedstyle.cpp:
            (DOM::CSSComputedStyleDeclarationImpl::getPropertyCSSValue): New overloaded
    	version of this function, one that takes a flag to determine whether to
    	perform a document updateLayout() call before querying the style system.
            * khtml/css/css_computedstyle.h:
            (DOM::EUpdateLayout): Give a symbolic constant to true/false for the
    	purpose of the new call to getPropertyCSSValue.
            * khtml/editing/htmlediting_impl.cpp:
            (khtml::StyleChange::currentlyHasStyle): Pass DoNotUpdateLayout to call to
    	getPropertyCSSValue.
            (khtml::CompositeEditCommandImpl::applyTypingStyle): Add in top-level calls
    	to updateLayout before doing style changes that now do not update styles
    	themselves.
            (khtml::ApplyStyleCommandImpl::doApply): Ditto.
            (khtml::ApplyStyleCommandImpl::removeCSSStyle): Pass DoNotUpdateLayout to call to
            getPropertyCSSValue.
            (khtml::DeleteSelectionCommandImpl::computeTypingStyle): Remove this dead code.
            * khtml/editing/htmlediting_impl.h: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7636 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/css/css_computedstyle.h
+++ third_party/WebKit/WebCore/khtml/css/css_computedstyle.h
@@ -38,6 +38,8 @@ class CSSValueImpl;
 class DOMString;
 class NodeImpl;
 
+enum EUpdateLayout { DoNotUpdateLayout = false, UpdateLayout = true };
+
 class CSSComputedStyleDeclarationImpl : public CSSStyleDeclarationImpl
 {
 public:
@@ -48,6 +50,7 @@ public:
     virtual void setCssText(const DOMString &);
 
     virtual CSSValueImpl *getPropertyCSSValue(int propertyID) const;
+    CSSValueImpl *getPropertyCSSValue(int propertyID, bool updateLayout) const;
     virtual DOMString getPropertyValue(int propertyID) const;
     virtual bool getPropertyPriority(int propertyID) const;
 
