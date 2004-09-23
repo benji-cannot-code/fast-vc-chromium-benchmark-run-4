20d328b4e7be454f799cfda24c118ffdaa130c61
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-23 16:08:35 +0000

    Reviewed by Ken.
            - fixed <rdar://problem/3812758> 8S266: attaching mutation events to an editable div crashes Safari
    
            * khtml/xml/dom_nodeimpl.cpp:
            (NodeImpl::dispatchEvent): Added ref/deref so this works if passed a "floating" event object.
            (NodeImpl::dispatchGenericEvent): Ditto.
            (NodeImpl::dispatchHTMLEvent): Removed unneeded ref/deref now that dispatchEvent takes care of it.
            (NodeImpl::dispatchMouseEvent): Ditto.
            (NodeImpl::dispatchUIEvent): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7653 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
@@ -553,6 +553,8 @@ EventListener *NodeImpl::getHTMLEventListener(int id)
 
 bool NodeImpl::dispatchEvent(EventImpl *evt, int &exceptioncode, bool tempEvent)
 {
+    evt->ref();
+
     evt->setTarget(this);
 
     KHTMLPart *part = document->document()->part();
@@ -573,11 +575,15 @@ bool NodeImpl::dispatchEvent(EventImpl *evt, int &exceptioncode, bool tempEvent)
     if (view)
         view->deref();
 
+    evt->deref();
+
     return ret;
 }
 
 bool NodeImpl::dispatchGenericEvent( EventImpl *evt, int &/*exceptioncode */)
 {
+    evt->ref();
+
     // ### check that type specified
 
     // work out what nodes to send event to
@@ -660,17 +666,18 @@ bool NodeImpl::dispatchGenericEvent( EventImpl *evt, int &/*exceptioncode */)
 
     DocumentImpl::updateDocumentsRendering();
 
-    return !evt->defaultPrevented(); // ### what if defaultPrevented was called before dispatchEvent?
+    bool defaultPrevented = evt->defaultPrevented();
+
+    evt->deref();
+
+    return !defaultPrevented; // ### what if defaultPrevented was called before dispatchEvent?
 }
 
 bool NodeImpl::dispatchHTMLEvent(int _id, bool canBubbleArg, bool cancelableArg)
 {
     int exceptioncode = 0;
     EventImpl *evt = new EventImpl(static_cast<EventImpl::EventId>(_id),canBubbleArg,cancelableArg);
-    evt->ref();
-    bool r = dispatchEvent(evt,exceptioncode,true);
-    evt->deref();
-    return r;
+    return dispatchEvent(evt,exceptioncode,true);
 }
 
 bool NodeImpl::dispatchWindowEvent(int _id, bool canBubbleArg, bool cancelableArg)
@@ -801,10 +808,7 @@ bool NodeImpl::dispatchMouseEvent(QMouseEvent *_mouse, int overrideId, int overr
     EventImpl *evt = new MouseEventImpl(evtId,true,cancelable,getDocument()->defaultView(),
                    detail,screenX,screenY,clientX,clientY,ctrlKey,altKey,shiftKey,metaKey,
                    button,0);
-    evt->ref();
-    bool r = dispatchEvent(evt,exceptioncode,true);
-    evt->deref();
-    return r;
+    return dispatchEvent(evt,exceptioncode,true);
 
 }
 
@@ -821,10 +825,7 @@ bool NodeImpl::dispatchUIEvent(int _id, int detail)
     int exceptioncode = 0;
     UIEventImpl *evt = new UIEventImpl(static_cast<EventImpl::EventId>(_id),true,
                                        cancelable,getDocument()->defaultView(),detail);
-    evt->ref();
-    bool r = dispatchEvent(evt,exceptioncode,true);
-    evt->deref();
-    return r;
+    return dispatchEvent(evt,exceptioncode,true);
 }
 
 bool NodeImpl::dispatchSubtreeModifiedEvent()
