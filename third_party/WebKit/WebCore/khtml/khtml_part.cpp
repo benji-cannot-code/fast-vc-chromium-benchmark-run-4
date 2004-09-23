122f44132933e39d0ae1e5a6a7fa5cd17e58ffcc
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-23 16:05:58 +0000

    Reviewed by Ken.
            - fixed some minor mistakes discovered by code inspection
    
            * khtml/khtml_part.cpp:
            (KHTMLPart::setSelection): Changed to not grab focus if clearing the selection.
            Grabbing focus when we get the selection is also probably something that should be
            conditional, since you can use a WebView in a mode where it can have selection even
            when not first responder, ala NSTextView.
            (KHTMLPart::setCaretVisible): Changed to not grab focus if making the caret invisible.
            Not sure if it's ever important to grab focus when making the caret visible, but by
            code inspection it seemed that was unlikely to do harm.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7652 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtml_part.cpp
+++ third_party/WebKit/WebCore/khtml/khtml_part.cpp
@@ -2355,7 +2355,8 @@ void KHTMLPart::setSelection(const Selection &s, bool closeTyping, bool unmarkOl
         }
 #endif
         d->m_selection = s;
-        setFocusNodeIfNeeded();
+        if (!s.isNone())
+            setFocusNodeIfNeeded();
         notifySelectionChanged(closeTyping);
     }
 }
@@ -2389,9 +2390,9 @@ void KHTMLPart::setCaretVisible(bool flag)
 {
     if (d->m_caretVisible == flag)
         return;
-
     clearCaretRectIfNeeded();
-    setFocusNodeIfNeeded();
+    if (flag)
+        setFocusNodeIfNeeded();
     d->m_caretVisible = flag;
     selectionLayoutChanged();
 }
