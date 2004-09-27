23725771d94b2b36fd694aaaedaaf016dbefa49f
kdecker <kdecker@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-09-27 18:00:13 +0000

    Reviewed by John.
            * khtml/css/css_base.cpp:
            (CSSSelector::selectorText): changed another ATTR_CLASS case to properly return class selector names.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7693 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/css/css_base.cpp
+++ third_party/WebKit/WebCore/khtml/css/css_base.cpp
@@ -283,7 +283,7 @@ DOMString CSSSelector::selectorText() const
             str += "#";
             str += cs->value.string();
         }
-        else if ( cs->attr == ATTR_CLASS && cs->match == CSSSelector::List )
+        else if ( cs->attr == ATTR_CLASS && cs->match == CSSSelector::Class )
         {
             str += ".";
             str += cs->value.string();
