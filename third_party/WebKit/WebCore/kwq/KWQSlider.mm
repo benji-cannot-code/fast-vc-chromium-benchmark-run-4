ddc34c41266d91599df3fc84e5d430f5a2b6aeec
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-10-01 06:43:37 +0000

    Reviewed by Maciej.
            - fixed <rdar://problem/3461499> JavaScript function document.open() is buggy with 2nd argument "replace"
    
            Experiments with Firefox indicate that document.open() should not be treated as window.open() unless there
            are more than two parameters. Also, Firefox does not implement the "replace" behavior, so we don't have
            to worry about it either.
    
            * khtml/ecma/kjs_html.cpp: (KJS::HTMLDocFunction::tryCall): Only forward to window if there are more than
            two parameters, rather than if there are more than one.
    
            - fixed <rdar://problem/3672933> oninput is firing at page load time for <input type=range>
    
            * kwq/KWQSlider.mm: (QSlider::setValue): Don't emit a signal here. This follows the usual pattern, where
            a signal is only emitted for changes that are not explicitly requested by the caller.
    
            - fixed <rdar://problem/3821167> leaks of something allocated by recalcStyle after loading altavista page from cvs-base
    
            * khtml/xml/dom_docimpl.cpp: (DocumentImpl::recalcStyle): Ref and deref the style we allocate so it's
            not left floating if setStyle decides not to ref it.
    
            - fixed <rdar://problem/3821172> leak of HTML attribute string after loading the ESPN page from cvs-base
    
            * khtml/html/html_elementimpl.cpp: (HTMLElementImpl::parseHTMLAttribute): Use the DOMString version of
            the lower() operation so we don't end up leaving a DOMStringImpl * floating if AtomicString decides not
            to ref it.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7743 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQSlider.mm
+++ third_party/WebKit/WebCore/kwq/KWQSlider.mm
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2003 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2004 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -110,8 +110,7 @@ void QSlider::setValue(double v)
     
     KWQSlider* slider = (KWQSlider*)getView();
     [slider setDoubleValue: val];
-    
-    sliderValueChanged(); // Emit the signal that indicates our value has changed.
+    m_val = val;
 }
 
 void QSlider::setMinValue(double v)
