836b3ea0580c62d7a108107f0012fc629ed00036
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-10-01 15:18:31 +0000

    Reviewed by John.
            - fixed <rdar://problem/3782117> CrashTracer: ..722 crashes at com.apple.WebCore: DOM::HTMLBodyElementImpl::insertedIntoDocument + 0x2c (AOL website)
    
            * khtml/html/html_baseimpl.cpp:
            (HTMLBodyElementImpl::insertedIntoDocument): Check for nil document case.
            (HTMLFrameElementImpl::isURLAllowed): Ditto.
            (HTMLFrameElementImpl::openURL): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7745 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_baseimpl.cpp
+++ third_party/WebKit/WebCore/khtml/html/html_baseimpl.cpp
@@ -197,9 +197,10 @@ void HTMLBodyElementImpl::insertedIntoDocument()
 {
     HTMLElementImpl::insertedIntoDocument();
 
-    // FIXME: perhaps all this stuff should be in attach() instead of here...
+    // FIXME: perhaps this code should be in attach() instead of here
 
-    KHTMLView* w = getDocument()->view();
+    DocumentImpl *d = getDocument();
+    KHTMLView *w = d ? d->view() : 0;
     if (w && w->marginWidth() != -1) {
         QString s;
         s.sprintf( "%d", w->marginWidth() );
@@ -245,7 +246,8 @@ bool HTMLFrameElementImpl::isURLAllowed(const AtomicString &URLString) const
         return true;
     }
     
-    KHTMLView *w = getDocument()->view();
+    DocumentImpl *d = getDocument();
+    KHTMLView *w = d ? d->view() : 0;
 
     if (!w) {
 	return false;
@@ -311,7 +313,8 @@ void HTMLFrameElementImpl::updateForNewURL()
 
 void HTMLFrameElementImpl::openURL()
 {
-    KHTMLView *w = getDocument()->view();
+    DocumentImpl *d = getDocument();
+    KHTMLView *w = d ? d->view() : 0;
     if (!w) {
         return;
     }
