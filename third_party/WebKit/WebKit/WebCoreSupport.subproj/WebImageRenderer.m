5f04068d4d79bcc8b3676eee2f222d94d911c169
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-10-04 17:43:49 +0000

    Reviewed by Ken.
            - fixed a potential storage leak when we turn on CGImageRef image rendering
    
            * WebCoreSupport.subproj/WebImageRenderer.m: (-[WebImageRenderer dealloc]): Fix potential storage leak
            by adding [super dealloc], but leak was not real yet because the code is commented out.
    
            - make paste style work with color as part of fix to <rdar://problem/3814237> REGRESSION (Mail):
              Copy/paste style does not set color in Mail compose window
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView _selectionFontAttributes]): Change structure so it's easy to add more attributes.
            For now I haven't added any yet.
            (-[WebHTMLView _colorAsString:]): Moved this earlier in the file.
            (-[WebHTMLView _shadowAsString:]): Ditto.
            (-[WebHTMLView _styleFromFontAttributes:]): Add background color, foreground color, and text shadow.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     [MIMEType release];
     [imageData release];
+    [super dealloc];
 }
 
 - copyWithZone:(NSZone *)zone
