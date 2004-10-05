cab394fc309977db55229c02b24af58f6ab392be
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-10-05 21:16:50 +0000

    Fixed <rdar://problem/3825442> first click lost for Dashboard 	Allow dashboard to force acceptsFirstMouse:
            Reviewed by Chris.
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView acceptsFirstMouse:]):
            * WebView.subproj/WebView.m:
            (-[WebView _dashboardBehavior:]):
            * WebView.subproj/WebViewInternal.h:
            * WebView.subproj/WebViewPrivate.h:
    
            * WebCoreSupport.subproj/WebImageRenderer.h:
            Comment change only
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7770 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebView.m
@@ -1194,6 +1194,10 @@ NSString *_WebMainFrameURLKey =         @"mainFrameURL";
             _private->dashboardBehaviorAlwaysSendActiveNullEventsToPlugIns = flag;
             break;
         }
+        case WebDashboardBehaviorAlwaysAcceptsFirstMouse: {
+            _private->dashboardBehaviorAlwaysAcceptsFirstMouse = flag;
+            break;
+        }
     }
 }
 
@@ -1205,6 +1209,9 @@ NSString *_WebMainFrameURLKey =         @"mainFrameURL";
         }
         case WebDashboardBehaviorAlwaysSendActiveNullEventsToPlugIns: {
             return _private->dashboardBehaviorAlwaysSendActiveNullEventsToPlugIns;
+        }
+        case WebDashboardBehaviorAlwaysAcceptsFirstMouse: {
+            return _private->dashboardBehaviorAlwaysAcceptsFirstMouse;
         }
     }
     return NO;
--- third_party/WebKit/WebKit/WebView.subproj/WebViewPrivate.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebViewPrivate.h
@@ -34,7 +34,8 @@ extern NSString *_WebMainFrameURLKey;
 
 typedef enum {
 	WebDashboardBehaviorAlwaysSendMouseEventsToAllWindows,
-	WebDashboardBehaviorAlwaysSendActiveNullEventsToPlugIns
+	WebDashboardBehaviorAlwaysSendActiveNullEventsToPlugIns,
+	WebDashboardBehaviorAlwaysAcceptsFirstMouse
 } WebDashboardBehavior;
 
 
