8a0bd9f31da1652d4d1fe3b6a80cc0e20194bd3d
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-10-09 21:22:43 +0000

    Reviewed by Kevin.
            - fixed <rdar://problem/3822618> REGRESSION (164-165): expedia.com's popup help doesn't work
    
            * kjs/reference.cpp: (Reference::putValue): Change so that references not found in any object
            work with the window object of the page the function is in, not the page of the caller. This
            is what all other browsers do. This code was hidden before by the "everything is defined on
            window object" hack in WebCore.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/reference.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/reference.cpp
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // -*- c-basic-offset: 2 -*-
 /*
  *  This file is part of the KDE libraries
- *  Copyright (C) 2003 Apple Computer, Inc
+ *  Copyright (C) 2004 Apple Computer, Inc
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -153,7 +153,7 @@ void Reference::putValue(ExecState *exec, const Value &w)
 #endif
   Value o = getBase(exec);
   if (o.type() == NullType)
-    o = exec->dynamicInterpreter()->globalObject();
+    o = exec->lexicalInterpreter()->globalObject();
 
   if (propertyNameIsNumber)
     return static_cast<ObjectImp*>(o.imp())->put(exec,propertyNameAsNumber, w);
