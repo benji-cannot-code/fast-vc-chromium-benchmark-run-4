fb4bb32113143e69e41d1e0ca0cb93c5e037bc5f
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-10-09 22:44:32 +0000

    Reviewed by Adele.
            - fixed <rdar://problem/3829452> REGRESSION (156-157): onload handler doesn't run on page with meta refresh of 0 duration (new Apple start page)
    
            The fix for <rdar://problem/3773150> made it so <meta> redirects prevent tokenizing the rest of the page.
            This is incorrect; the reason the JavaScript-triggered loads prevent tokenizing is that they take place
            "right away" in other browsers, but that is not true of <meta> redirect. We fixed this by using a separate
            call for <meta> redirect and not preventing tokenizing when that's in effect.
    
            * khtml/khtml_part.h: Removed userGesture parameter from scheduleRedirection. Renamed
            isImmediateRedirectPending to isScheduledLocationChangePending. Added scheduleLocationChange.
            * khtml/khtml_part.cpp:
            (KHTMLPart::openURL): Updated for new constant name.
            (KHTMLPart::scheduleRedirection): Removed now-unneeded userGesture parameter, and removed code that
            does the special case for redirection during load; a <meta> refresh can never be one of those special
            redirects during a load because it redirects the frame itself, not another frame. Also tightened up
            the logic by always stopping the redirect timer even if we aren't restarting it.
            (KHTMLPart::scheduleLocationChange): Added. Like scheduleRedirection, but with a different constant
            so we can tell it apart and always a delay of 0. The "redirection during load" case was moved in here
            and renamed to locationChangeScheduledDuringLoad.
            (KHTMLPart::isScheduledLocationChangePending): Renamed from isImmediateRedirectPending. This now
            returns true only for location changes and history navigation, not <meta> redirects.
            (KHTMLPart::scheduleHistoryNavigation): Tightened up logic to do the stop() outside the if as above,
            and got rid of a silly timer delay computation that always resulted in 0.
    
            * khtml/khtmlpart_p.h: Added a new value to the RedirectionScheduled enum for scheduleLocationChange
            and also renamed one of the existing values.
    
            * khtml/html/htmltokenizer.cpp: (HTMLTokenizer::write): Changed to use isScheduledLocationChangePending instead
            of isImmediateRedirectPending, because we do want to continue tokenizing if it's actually a redirect.
    
            * khtml/ecma/kjs_html.cpp: (KJS::HTMLDocument::putValue): Changed to call the new scheduleLocationChange
            instead of calling scheduleRedirection with delay of 0.
            * khtml/ecma/kjs_window.cpp:
            (Window::put): Ditto.
            (WindowFunc::tryCall): Ditto.
            (Location::put): Ditto.
            (LocationFunc::tryCall): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_window.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_window.cpp
@@ -899,11 +899,9 @@ void Window::put(ExecState* exec, const Identifier &propertyName, const Value &v
           bool userGesture = static_cast<ScriptInterpreter *>(exec->dynamicInterpreter())->wasRunByUserGesture();
 #if APPLE_CHANGES
           // We want a new history item if this JS was called via a user gesture
-          m_part->scheduleRedirection(0, dstUrl, !userGesture, userGesture);
+          m_part->scheduleLocationChange(dstUrl, !userGesture, userGesture);
 #else
-          m_part->scheduleRedirection(0,
-                                      dstUrl,
-                                      false /*don't lock history*/, userGesture);
+          m_part->scheduleLocationChange(dstUrl, false /*don't lock history*/, userGesture);
 #endif
         }
       }
@@ -1457,7 +1455,7 @@ Value WindowFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
           while ( part->parentPart() )
               part = part->parentPart();
           bool userGesture = static_cast<ScriptInterpreter *>(exec->dynamicInterpreter())->wasRunByUserGesture();
-          part->scheduleRedirection(0, url.url(), false/*don't lock history*/, userGesture);
+          part->scheduleLocationChange(url.url(), false/*don't lock history*/, userGesture);
           return Window::retrieve(part);
       }
       if ( uargs.frameName == "_parent" )
@@ -1466,7 +1464,7 @@ Value WindowFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
           if ( part->parentPart() )
               part = part->parentPart();
           bool userGesture = static_cast<ScriptInterpreter *>(exec->dynamicInterpreter())->wasRunByUserGesture();
-          part->scheduleRedirection(0, url.url(), false/*don't lock history*/, userGesture);
+          part->scheduleLocationChange(url.url(), false/*don't lock history*/, userGesture);
           return Window::retrieve(part);
       }
       uargs.serviceType = "text/html";
@@ -1496,7 +1494,7 @@ Value WindowFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
         if (!url.isEmpty()) {
           bool userGesture = static_cast<ScriptInterpreter *>(exec->dynamicInterpreter())->wasRunByUserGesture();
           // FIXME: Need to pass referrer here.
-          khtmlpart->scheduleRedirection(0, url.url(), false, userGesture);
+          khtmlpart->scheduleLocationChange(url.url(), false, userGesture);
 	}
 #else
         uargs.serviceType = QString::null;
@@ -2123,9 +2121,9 @@ void Location::put(ExecState *exec, const Identifier &p, const Value &v, int att
   bool userGesture = static_cast<ScriptInterpreter *>(exec->dynamicInterpreter())->wasRunByUserGesture();
 #if APPLE_CHANGES
   // We want a new history item if this JS was called via a user gesture
-  m_part->scheduleRedirection(0, url.url(), !userGesture, userGesture);
+  m_part->scheduleLocationChange(url.url(), !userGesture, userGesture);
 #else
-  m_part->scheduleRedirection(0, url.url(), false /*don't lock history*/, userGesture);
+  m_part->scheduleLocationChange(url.url(), false /*don't lock history*/, userGesture);
 #endif
 }
 
@@ -2164,14 +2162,14 @@ Value LocationFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
       KHTMLPart* p = Window::retrieveActive(exec)->part();
       if ( p ) {
 	bool userGesture = static_cast<ScriptInterpreter *>(exec->dynamicInterpreter())->wasRunByUserGesture();
-        part->scheduleRedirection(0, p->htmlDocument().completeURL(str).string(), true /*lock history*/, userGesture);
+        part->scheduleLocationChange(p->htmlDocument().completeURL(str).string(), true /*lock history*/, userGesture);
       }
       break;
     }
     case Location::Reload:
     {
       bool userGesture = static_cast<ScriptInterpreter *>(exec->dynamicInterpreter())->wasRunByUserGesture();
-      part->scheduleRedirection(0, part->url().url(), true/*lock history*/, userGesture);
+      part->scheduleLocationChange(part->url().url(), true/*lock history*/, userGesture);
       break;
     }
     case Location::ToString:
