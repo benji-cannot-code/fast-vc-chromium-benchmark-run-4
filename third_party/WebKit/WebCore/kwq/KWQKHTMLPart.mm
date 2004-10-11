f93229e5c63f17eb1a1e537301091b4fe14e22ca
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-10-11 17:49:24 +0000

    Reviewed by John
            This is a partial fix to this bug:
    
            <rdar://problem/3832886> increase quote level on new mail document leads to immediate
            crash in caret painting code
    
            To eliminate the bad behavior for good, I have done some investigations in Mail code,
            and I have sent a suggested code change on to Grant. Basically, Mail can't add empty
            blocks (like blockquote elements used for quoting) to documents without giving those
            blocks some content (so they have a height).
    
            I added some other crash protections below.
    
            * khtml/editing/selection.cpp:
            (khtml::Selection::layout): Check for non-null position after calls to VisiblePosition, since
            the VisiblePosition constructors may fail to find a visible spot in the document. Also, add
            a couple position-has-renderer assertion checks.
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::styleForSelectionStart): Take out pos.isNotNull() assertion since VisiblePosition
            may not yield a position. This assertion is a holdover from before we had VisiblePosition.
            (KWQKHTMLPart::fontForSelection): Rearrange the code a little to deal with possible null results from
            calls to helpers.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7818 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKHTMLPart.mm
+++ third_party/WebKit/WebCore/kwq/KWQKHTMLPart.mm
@@ -3442,7 +3442,6 @@ RenderStyle *KWQKHTMLPart::styleForSelectionStart(NodeImpl *&nodeToRemove) const
         return 0;
     
     Position pos = VisiblePosition(d->m_selection.start(), UPSTREAM).deepEquivalent();
-    ASSERT(pos.isNotNull());
     if (!pos.inRenderedContent())
         return 0;
     NodeImpl *node = pos.node();
@@ -3477,8 +3476,12 @@ NSFont *KWQKHTMLPart::fontForSelection(bool *hasMultipleFonts) const
 
     if (!d->m_selection.isRange()) {
         NodeImpl *nodeToRemove;
-        NSFont *result = styleForSelectionStart(nodeToRemove)->font().getNSFont();
+        RenderStyle *style = styleForSelectionStart(nodeToRemove); // sets nodeToRemove
 
+        NSFont *result = 0;
+        if (style)
+            result = style->font().getNSFont();
+        
         if (nodeToRemove) {
             int exceptionCode;
             nodeToRemove->remove(exceptionCode);
