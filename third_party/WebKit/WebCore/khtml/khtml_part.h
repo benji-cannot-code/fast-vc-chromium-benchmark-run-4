254175f8707d85a4c8da06ba1804f832d785456d
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-10-14 20:38:16 +0000

    WebCore:
            Reviewed by John
    
            Final fix for these bugs:
    
            <rdar://problem/3806306> HTML editing puts spaces at start of line
            <rdar://problem/3814252> HTML editing groups space with word causing wrapping
    
            This change sets some new CSS properties that have been added to WebCore to
            enable whitespace-handling and line-breaking features that make WebView work
            more like a text editor.
    
            * khtml/css/cssstyleselector.cpp:
            (khtml::CSSStyleSelector::applyProperty): Add and remove special editing CSS properties
            based on property value.
            * khtml/html/html_elementimpl.cpp:
            (HTMLElementImpl::setContentEditable): Add and remove special editing CSS properties
            based on attribute value.
            * khtml/khtml_part.cpp:
            (KHTMLPart::applyEditingStyleToBodyElement): New helper. Calls applyEditingStyleToElement on
            body element.
            (KHTMLPart::removeEditingStyleFromBodyElement): New helper. Calls removeEditingStyleFromElement on
            body element.
            (KHTMLPart::applyEditingStyleToElement): Adds special editing CSS properties to passed in element.
            (KHTMLPart::removeEditingStyleFromElement): Removes special editing CSS properties from passed in element.
            * khtml/khtml_part.h: Add new declarations.
            * kwq/WebCoreBridge.h: Ditto.
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge applyEditingStyleToBodyElement]): Call through to similarly-named function on KHTMLPart.
            (-[WebCoreBridge removeEditingStyleFromBodyElement]): Ditto.
            (-[WebCoreBridge applyEditingStyleToElement:]): Ditto.
            (-[WebCoreBridge removeEditingStyleFromElement:]): Ditto.
    
    WebKit:
    
            Reviewed by John
    
            Final fix for these bugs:
    
            <rdar://problem/3806306> HTML editing puts spaces at start of line
            <rdar://problem/3814252> HTML editing groups space with word causing wrapping
    
            This change sets some new CSS properties that have been added to WebCore to
            enable whitespace-handling and line-breaking features that make WebView work
            more like a text editor.
    
            * WebView.subproj/WebHTMLRepresentation.m:
            (-[WebHTMLRepresentation finishedLoadingWithDataSource:]): Turn on special editing
            CSS properties when loading an HTML document into a WebView that is editable.
            * WebView.subproj/WebView.m:
            (-[WebView setEditable:]): Add and remove special editing CSS properties in current
            document being displayed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtml_part.h
+++ third_party/WebKit/WebCore/khtml/khtml_part.h
@@ -47,6 +47,7 @@ namespace DOM
   class DOMString;
   class Document;
   class DocumentImpl;
+  class ElementImpl;
   class EventListener;
   class HTMLAnchorElementImpl;
   class HTMLDocument;
@@ -884,6 +885,10 @@ public:
   TriState selectionHasStyle(DOM::CSSStyleDeclarationImpl *) const;
   bool selectionStartHasStyle(DOM::CSSStyleDeclarationImpl *) const;
   DOM::DOMString selectionStartStylePropertyValue(int stylePropertyID) const;
+  void applyEditingStyleToBodyElement() const;
+  void removeEditingStyleFromBodyElement() const;
+  void applyEditingStyleToElement(DOM::ElementImpl *) const;
+  void removeEditingStyleFromElement(DOM::ElementImpl *) const;
   void print();
 
   // Used to keep the part alive when running a script that might destroy it.
--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLRepresentation.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLRepresentation.m
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKitStatisticsPrivate.h>
 #import <WebKit/WebNSObjectExtras.h>
 #import <WebKit/WebResourcePrivate.h>
+#import <WebKit/WebView.h>
 
 #import <Foundation/NSString_NSURLExtras.h>
 #import <Foundation/NSURLResponse.h>
@@ -125,7 +126,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)finishedLoadingWithDataSource:(WebDataSource *)dataSource
 {
-    if ([dataSource webFrame]) {
+    WebFrame *frame = [dataSource webFrame];
+    if (frame) {
         if ([self _isDisplayingWebArchive]) {
             [self loadArchive];
         } else {
@@ -133,6 +135,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             // way to get work done that is normally done when the first bit of data is
             // received, even for the case of a document with no data (like about:blank).
             [_private->bridge receivedData:nil textEncodingName:[[_private->dataSource response] textEncodingName]];
+        }
+        
+        WebView *webView = [frame webView];
+        if ([webView isEditable]) {
+            [_private->bridge applyEditingStyleToBodyElement];
         }
     }
 }
