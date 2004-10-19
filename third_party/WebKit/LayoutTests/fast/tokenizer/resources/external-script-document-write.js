0b49608f65427f5be4627ccbbd62eeb1df2342d1
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-10-19 19:01:15 +0000

    2004-10-19  Darin Adler  <darin@apple.com>
            Reviewed by Maciej.
    
            - follow-on to my fix yesterday, which broke a layout test because I rolled out a fix that Maciej had done
    
            * khtml/html/htmltokenizer.cpp: (khtml::HTMLTokenizer::write): Need to check the actual queue of external
            scripts being loaded here. If the current code being run is the external script itself, then we don't want
            to defer parsing. But loadingExtScript has to stay true until after the script runs. The old code would
            assume that any time we're running a script there's no need to look at loadingExtScript, but that was also
            wrong since there can be a script loading in that case too. Layout tests check for both problems.
    
            * layout-tests/fast/tokenizer/external-script-document-write-expected.txt: Added.
            * layout-tests/fast/tokenizer/external-script-document-write.html: Added.
            * layout-tests/fast/tokenizer/resources/external-script-document-write.js: Added.
    
            * layout-tests/fast/tokenizer/004.html: Fixed line endings (were CR, should be LF).
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7857 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/tokenizer/resources/external-script-document-write.js
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+document.write('<span id="x"></span>');
+document.write("This should be a span: " + document.getElementById('x'));
