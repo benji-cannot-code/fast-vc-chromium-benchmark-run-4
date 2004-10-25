50fe66b054a642551bccd225b1d491d64cad05ac
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-10-25 18:26:01 +0000

    Reviewed by Darin.
            Fix for <rdar://problem/3619890> Feature request: designMode
    
            This change implements the designMode property of a document.  This is an IE property that is also supported by Mozilla.
            This will enable more JS editing compatibility.
    
            * khtml/ecma/kjs_html.cpp:
            (KJS::HTMLDocument::tryGet): added case for designMode
            (KJS::HTMLDocument::putValue): added case for designMode
            * khtml/ecma/kjs_html.lut.h: (KJS::): regenerated
            * khtml/khtml_part.cpp: (KHTMLPart::isContentEditable): Now returns designMode value
            * khtml/xml/dom_docimpl.cpp:
            (DocumentImpl::DocumentImpl): initialize m_designMode member variable
            (DocumentImpl::setDesignMode): added function to assign m_designMode value
            (DocumentImpl::getDesignMode): return m_designMode value
            (DocumentImpl::inDesignMode): if designMode is inherited, this will find the appropriate parent document designMode and return that value.
                Otherwise, it will just return the m_designMode value.
            (DocumentImpl::parentDocument):
            * khtml/xml/dom_docimpl.h: (DOM::DocumentImpl::): added InheritedBool enum, prototypes, and m_designMode member variable.
            * kwq/KWQKHTMLPart.mm: (KWQKHTMLPart::isContentEditable): added check for isContentEditable function in KHTMLPart
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7887 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_html.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_html.cpp
@@ -66,6 +66,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace KJS;
 
+using DOM::DocumentImpl;
+using DOM::DOMString;
 using DOM::HTMLFrameElementImpl;
 using DOM::HTMLIFrameElementImpl;
 
@@ -175,6 +177,7 @@ const ClassInfo KJS::HTMLDocument::info =
   height		HTMLDocument::Height		DontDelete|ReadOnly
   width			HTMLDocument::Width		DontDelete|ReadOnly
   dir			HTMLDocument::Dir		DontDelete
+  designMode            HTMLDocument::DesignMode        DontDelete
 #potentially obsolete array properties
 # layers
 # plugins
@@ -292,6 +295,13 @@ Value KJS::HTMLDocument::tryGet(ExecState *exec, const Identifier &propertyName)
       return Number(view ? view->contentsWidth() : 0);
     case Dir:
       return String(body.dir());
+    case DesignMode:
+    {
+      DocumentImpl *docimpl = static_cast<DocumentImpl *>(doc.handle());
+      if (!docimpl)
+        return Undefined();
+      return String(docimpl->inDesignMode() ? "on" : "off");  
+    }
     }
   }
 
@@ -448,6 +458,22 @@ void KJS::HTMLDocument::putValue(ExecState *exec, int token, const Value& value,
     break;
   case Dir:
     body.setDir(value.toString(exec).string());
+    break;
+  case DesignMode:
+    {
+      DocumentImpl *docimpl = static_cast<DocumentImpl *>(doc.handle());
+      if (!docimpl)
+        break;
+      DOMString modeString = value.toString(exec).string();
+      DocumentImpl::InheritedBool mode;
+      if (!strcasecmp(modeString, "on"))
+        mode = DocumentImpl::on;
+      else if (!strcasecmp(modeString, "off"))
+        mode = DocumentImpl::off;
+      else
+        mode = DocumentImpl::inherit;
+      docimpl->setDesignMode(mode);
+     }
     break;
   default:
     kdWarning() << "HTMLDocument::putValue unhandled token " << token << endl;
