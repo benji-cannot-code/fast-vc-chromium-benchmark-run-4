9414c14ce397a4d01a9f7c3d05e1b79d3cdabdd1
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-11-06 00:45:14 +0000

    Fixed: <rdar://problem/3854218> Safari is sometimes really slow because of increased null events to plug-ins
            * Plugins.subproj/WebBaseNetscapePluginView.m: reverted null event interval to 0.02
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7953 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.m
@@ -39,8 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This is not yet in QuickdrawPriv.h, although it's supposed to be.
 void CallDrawingNotifications(CGrafPtr port, Rect *mayDrawIntoThisRect, int drawingType);
 
-// Send null events 100 times a second when active, so plug-ins like Flash get high frame rates.
-#define NullEventIntervalActive 	0.01
+// Send null events 50 times a second when active, so plug-ins like Flash get high frame rates.
+#define NullEventIntervalActive 	0.02
 #define NullEventIntervalNotActive	0.25
 
 #define LoginWindowDidSwitchFromUserNotification    @"WebLoginWindowDidSwitchFromUserNotification"
