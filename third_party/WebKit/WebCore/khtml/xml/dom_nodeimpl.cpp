d612d8453f0bbec3b6482ba695065b1deec44964
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-11-08 18:31:03 +0000

    Reviewed by Darin.
            Renamed NodeImpl::enclosingNonBlockFlowElement to NodeImpl::enclosingInlineElement, per Hyatt.
    
            * khtml/editing/htmlediting.cpp:
            (khtml::DeleteSelectionCommand::moveNodesAfterNode):
            * khtml/xml/dom_nodeimpl.cpp:
            (NodeImpl::enclosingInlineElement):
            * khtml/xml/dom_nodeimpl.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7954 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
@@ -1366,7 +1366,7 @@ ElementImpl *NodeImpl::enclosingBlockFlowElement() const
     return 0;
 }
 
-ElementImpl *NodeImpl::enclosingNonBlockFlowElement() const
+ElementImpl *NodeImpl::enclosingInlineElement() const
 {
     NodeImpl *n = const_cast<NodeImpl *>(this);
     NodeImpl *p;
