2fc56e02ea4fa2b5e080d9c486116d6f6342f1fb
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-11-09 00:22:02 +0000

    Fixed: <rdar://problem/3870907> WebCore unnecessary links against JavaVM and Security
            Reviewed by darin.
    
            * WebCore.pbproj/project.pbxproj: stop unnecessary linking
            * khtml/html/html_objectimpl.h: don't unnecessarily include JavaVM header
            * kwq/KWQKHTMLPart.h: ditto
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7957 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_objectimpl.h
+++ third_party/WebKit/WebCore/khtml/html/html_objectimpl.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qstringlist.h>
 
 #if APPLE_CHANGES
-#include <JavaVM/jni.h>
 #include <JavaScriptCore/runtime.h>
 #endif
 
--- third_party/WebKit/WebCore/kwq/KWQKHTMLPart.h
+++ third_party/WebKit/WebCore/kwq/KWQKHTMLPart.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <CoreFoundation/CoreFoundation.h>
 
-#import <JavaVM/jni.h>
 #import <JavaScriptCore/jni_jsobject.h>
 #import <JavaScriptCore/npruntime.h>
 #import <JavaScriptCore/NP_jsobject.h>
