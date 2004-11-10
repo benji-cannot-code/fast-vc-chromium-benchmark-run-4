74f4838ba77a6deada3063fb6fac25ff9c98c744
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-11-10 16:58:30 +0000

    Reviewed by Harrison
            Some cleanups and fixes in upstream and downstream functions.
    
            Removed redundant checks for isBlockFlow() when calling enclosingBlockFlowElement().
            Blocks do not need to skip the call to enclosingBlockFlowElement() for fear that the
            block's enclosing block will be returned.
    
            Remove code from upstream that confined the serach to block boundaries outside of
            the code which runs in the StayInBlock case. This code was redundant, and caused
            incorrect results to be returned in the DoNotStayInBlock case.
    
            In downstream, the check for crossing into a new block should use the equivalentDeepPosition()
            node, not the the this pointer's node.
    
            * khtml/xml/dom_position.cpp:
            (DOM::Position::upstream)
            (DOM::Position::downstream)
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_position.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_position.cpp
@@ -369,14 +369,14 @@ Position Position::upstream(EStayInBlock stayInBlock) const
     if (!startNode)
         return Position();
 
-    NodeImpl *block = startNode->isBlockFlow() ? startNode : startNode->enclosingBlockFlowElement();
+    NodeImpl *block = startNode->enclosingBlockFlowElement();
     
     PositionIterator it(start);
     for (; !it.atStart(); it.previous()) {
         NodeImpl *currentNode = it.current().node();
 
         if (stayInBlock) {
-            NodeImpl *currentBlock = currentNode->isBlockFlow() ? currentNode : currentNode->enclosingBlockFlowElement();
+            NodeImpl *currentBlock = currentNode->enclosingBlockFlowElement();
             if (block != currentBlock)
                 return it.next();
         }
@@ -388,7 +388,7 @@ Position Position::upstream(EStayInBlock stayInBlock) const
         if (renderer->style()->visibility() != VISIBLE)
             continue;
 
-        if ((currentNode != startNode && renderer->isBlockFlow()) || renderer->isReplaced() || renderer->isBR()) {
+        if (renderer->isReplaced() || renderer->isBR()) {
             if (it.current().offset() >= renderer->caretMaxOffset())
                 return Position(currentNode, renderer->caretMaxOffset());
             else
@@ -425,14 +425,14 @@ Position Position::downstream(EStayInBlock stayInBlock) const
     if (!startNode)
         return Position();
 
-    NodeImpl *block = startNode->isBlockFlow() ? startNode : startNode->enclosingBlockFlowElement();
+    NodeImpl *block = startNode->enclosingBlockFlowElement();
     
     PositionIterator it(start);            
     for (; !it.atEnd(); it.next()) {   
         NodeImpl *currentNode = it.current().node();
 
         if (stayInBlock) {
-            NodeImpl *currentBlock = currentNode->isBlockFlow() ? currentNode : currentNode->enclosingBlockFlowElement();
+            NodeImpl *currentBlock = currentNode->enclosingBlockFlowElement();
             if (block != currentBlock)
                 return it.previous();
         }
@@ -444,7 +444,7 @@ Position Position::downstream(EStayInBlock stayInBlock) const
         if (renderer->style()->visibility() != VISIBLE)
             continue;
 
-        if ((currentNode != node() && renderer->isBlockFlow()) || renderer->isReplaced() || renderer->isBR()) {
+        if ((currentNode != startNode && renderer->isBlockFlow()) || renderer->isReplaced() || renderer->isBR()) {
             if (it.current().offset() <= renderer->caretMinOffset())
                 return Position(currentNode, renderer->caretMinOffset());
             else
