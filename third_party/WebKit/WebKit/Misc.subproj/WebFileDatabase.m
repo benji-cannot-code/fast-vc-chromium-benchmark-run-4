387305ee4aabf3c007b14e6c6620eb9fe7fa47ec
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-11-10 22:39:20 +0000

    Fixed: <rdar://problem/3396872> ICONS: icon DB inconsistencies can cause slowness during startup, idle and quit
            Reviewed by john.
    
            * Misc.subproj/WebFileDatabase.m:
            (-[WebFileDatabase objectForKey:]): added logging code
            * Misc.subproj/WebIconDatabase.m:
            (-[WebIconDatabase init]): use alloc, init rather than autorelease, retain
            (-[WebIconDatabase _loadIconDictionaries]): use 1 object for mapping icon URLs to site URLs and vice versa rather than 3. This avoids inconsistencies and is faster.
            (-[WebIconDatabase _updateFileDatabase]): write 1 object out
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7970 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebFileDatabase.m
@@ -444,6 +444,8 @@ static void databaseInit()
     [data release];
     [filePath release];
 
+    LOG(Timing, "getting value for %@ took %f", key, (CFAbsoluteTimeGetCurrent() - touch));
+    
     return result;
 }
 
