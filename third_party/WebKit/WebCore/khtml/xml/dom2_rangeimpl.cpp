a7be8197f96b2a0fe935a38596fa2ee203d3562a
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-11-11 23:15:37 +0000

    Reviewed by Hyatt
            * khtml/editing/htmlediting.cpp:
            (khtml::CompositeEditCommand::deleteInsignificantText): Call new compareBoundaryPoints convenience.
            (khtml::ApplyStyleCommand::removeBlockStyle): Ditto.
            (khtml::ApplyStyleCommand::removeInlineStyle): Ditto.
            (khtml::ApplyStyleCommand::nodeFullySelected): Ditto.
            (khtml::DeleteSelectionCommand::initializePositionData): Ditto.
            * khtml/xml/dom2_rangeimpl.cpp:
            (DOM::RangeImpl::compareBoundaryPoints): New convenience variant of this function which takes two Position objects.
            * khtml/xml/dom2_rangeimpl.h: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7981 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom2_rangeimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom2_rangeimpl.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "html/html_elementimpl.h"
 #include "misc/htmltags.h"
 #include "editing/visible_text.h"
+#include "xml/dom_position.h"
 
 #include "render_block.h"
 
@@ -376,6 +377,11 @@ short RangeImpl::compareBoundaryPoints( NodeImpl *containerA, long offsetA, Node
     if( childAOffset == childBOffset )  return 0;    // A is equal to B
     if( childAOffset < childBOffset )   return -1;    // A is before B
     else  return 1;                        // A is after B
+}
+
+short RangeImpl::compareBoundaryPoints( const Position &a, const Position &b )
+{
+    return compareBoundaryPoints(a.node(), a.offset(), b.node(), b.offset());
 }
 
 bool RangeImpl::boundaryPointsValid(  ) const
