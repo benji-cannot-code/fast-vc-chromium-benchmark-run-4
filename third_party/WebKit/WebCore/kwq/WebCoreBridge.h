f1c95263f7781acc78df9dc92a965ae630fb3254
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-11-12 01:49:22 +0000

    Reviewed by John.
            - fixed <rdar://problem/3846152> REGRESSION (125-166): can't drag text out of <input type=text> fields
    
            * kwq/WebCoreBridge.h: Added wasFirstResponderAtMouseDownTime: method.
    
            * kwq/KWQKHTMLPart.h: Removed _firstResponderAtMouseDownTime.
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::passWidgetMouseDownEventToWidget): Use the new wasFirstResponderAtMouseDownTime:
            method on the bridge instead of _firstResponderAtMouseDownTime. This will return YES for the case where
            we started with the NSTextField as first responder, and then took focus away and gave it back, which
            makes dragging text work again.
            (KWQKHTMLPart::mouseDown): Removed code to set _firstResponderAtMouseDownTime.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@7986 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreBridge.h
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.h
@@ -430,7 +430,9 @@ typedef enum {
 - (BOOL)windowIsResizable;
 
 - (NSResponder *)firstResponder;
-- (void)makeFirstResponder:(NSResponder *)view;
+- (void)makeFirstResponder:(NSResponder *)responder;
+
+- (BOOL)wasFirstResponderAtMouseDownTime:(NSResponder *)responder;
 
 - (void)closeWindowSoon;
 
