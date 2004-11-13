46c0664e672765637b3a5a98f8896dd09a7b2252
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-11-13 02:54:31 +0000

    Reviewed by Gramps.
    	- fixed another bug in the last checkin, isCacheValid was unitialized, resulting in
    	sometimes using a huge bogus length value.
    
            * khtml/xml/dom_nodeimpl.cpp:
            (NodeListImpl::NodeListImpl): Initialize isCacheValid.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8000 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
@@ -2213,8 +2213,9 @@ void NodeBaseImpl::dispatchChildRemovalEvents( NodeImpl *child, int &exceptionco
 
 
 NodeListImpl::NodeListImpl(NodeImpl *_rootNode)
+    : rootNode(_rootNode),
+      isCacheValid(false)
 {
-    rootNode = _rootNode;
     rootNode->ref();
     rootNode->registerNodeList(this);
 }    
