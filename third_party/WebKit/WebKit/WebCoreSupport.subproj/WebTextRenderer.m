146c25b58146e5ca2713d10ee5dbd6618253bbfb
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-12-02 21:37:50 +0000

    Fixed build problem on Tiger8A821.  Private macro and function 	we were using have been deprecated,
            Reviewed by Vicki.
    
            * WebCoreSupport.subproj/WebTextRenderer.m:
            (-[WebTextRenderer initWithFont:usingPrinterFont:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8104 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.m
@@ -74,6 +74,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SMALLCAPS_FONTSIZE_MULTIPLIER 0.7
 #define INVALID_WIDTH -(__FLT_MAX__)
 
+#if !defined(ScaleEmToUnits)
+#define CONTEXT_DPI	(72.0)
+
+#define ScaleEmToUnits(X, U_PER_EM)	(X * ((1.0 * CONTEXT_DPI) / (CONTEXT_DPI * U_PER_EM)))
+#endif
+
 // Datatypes
 typedef float WebGlyphWidth;
 typedef UInt32 UnicodeChar;
@@ -335,10 +341,9 @@ static BOOL alwaysUseATSU = NO;
         }
 
         // Report the problem.
-        ERROR ("Corrupt font detected, using %@ in place of %@ (%d glyphs) located at \"%@\".", 
+        ERROR ("Corrupt font detected, using %@ in place of %@ located at \"%@\".", 
                     [font familyName], 
                     [initialFont familyName],
-                    ATSFontGetGlyphCount(ATSFontRefFromNSFont(initialFont)),
                     filePath);
     }
 
