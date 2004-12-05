dd94c602aa74f362b680f21547f57e635a2d0aac
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-12-05 20:40:25 +0000

    - fixed small problem in my check-in from yesterday
            * kwq/KWQEvent.mm:
            (positionForEvent): Get location from event without raising exception if it's the wrong type.
            (clickCountForEvent): Same, for clickCount.
            (QMouseEvent::QMouseEvent): Use the new helper functions so this can be constructed even with
            the wrong type of NSEvent. Required for cases where a keyboard event causes a "click" and we need
            to synthesize a QMouseEvent for KHTML internal use, using the key down NSEvent.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8123 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQEvent.mm
+++ third_party/WebKit/WebCore/kwq/KWQEvent.mm
@@ -747,6 +747,42 @@ static int nonMouseButtonsForEvent(NSEvent *event)
     return buttons;
 }
 
+static QPoint positionForEvent(NSEvent *event)
+{
+    switch ([event type]) {
+        case NSLeftMouseDown:
+        case NSLeftMouseUp:
+        case NSLeftMouseDragged:
+        case NSRightMouseDown:
+        case NSRightMouseUp:
+        case NSRightMouseDragged:
+        case NSOtherMouseDown:
+        case NSOtherMouseUp:
+        case NSOtherMouseDragged:
+            return QPoint([event locationInWindow]);
+        default:
+            return QPoint();
+    }
+}
+
+static int clickCountForEvent(NSEvent *event)
+{
+    switch ([event type]) {
+        case NSLeftMouseDown:
+        case NSLeftMouseUp:
+        case NSLeftMouseDragged:
+        case NSRightMouseDown:
+        case NSRightMouseUp:
+        case NSRightMouseDragged:
+        case NSOtherMouseDown:
+        case NSOtherMouseUp:
+        case NSOtherMouseDragged:
+            return [event clickCount];
+        default:
+            return 0;
+    }
+}
+
 // ======== 
 
 QEvent::~QEvent()
@@ -763,10 +799,10 @@ QMouseEvent::QMouseEvent(Type type, const QPoint &position, int button, int stat
 
 QMouseEvent::QMouseEvent(Type type, NSEvent *event)
     : QEvent(type)
-    , _position([event locationInWindow])
+    , _position(positionForEvent(event))
     , _button(mouseButtonForEvent(event))
     , _state(nonMouseButtonsForEvent(event))
-    , _clickCount([event clickCount])
+    , _clickCount(clickCountForEvent(event))
 {
     fixState();
 }
@@ -776,10 +812,10 @@ QMouseEvent::QMouseEvent(Type type)
 {
     NSEvent *event = [NSApp currentEvent];
     if (event) {
-        _position = QPoint([event locationInWindow]);
+        _position = positionForEvent(event);
         _button = mouseButtonForEvent(event);
         _state = nonMouseButtonsForEvent(event);
-        _clickCount = [event clickCount];
+        _clickCount = clickCountForEvent(event);
     }
     fixState();
 }
