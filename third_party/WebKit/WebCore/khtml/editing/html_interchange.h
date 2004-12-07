b2496d71aeada2784fc28ccc65f5c2cece2bb39e
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-12-07 17:02:11 +0000

    Reviewed by John
            Changed name of constant from KHTMLInterchangeNewline to AppleInterchangeNewline.
            I discussed this with Hyatt and he agreed that going with "Apple" names was OK.
    
            * khtml/editing/html_interchange.h: Name change, as described above.
            * khtml/editing/htmlediting.cpp:
            (khtml::ReplacementFragment::isInterchangeNewlineComment): Ditto.
            * khtml/xml/dom2_rangeimpl.cpp:
            (DOM::RangeImpl::toHTML): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8146 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/html_interchange.h
+++ third_party/WebKit/WebCore/khtml/editing/html_interchange.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class QString;
 
-#define KHTMLInterchangeNewline   "KHTMLInterchangeNewline"
+#define AppleInterchangeNewline   "Apple-interchange-newline"
 #define AppleConvertedSpace       "Apple-converted-space"
 
 enum EAnnotateForInterchange { DoNotAnnotateForInterchange, AnnotateForInterchange };
--- third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
@@ -3127,7 +3127,7 @@ void ReplacementFragment::pruneEmptyNodes()
 
 bool ReplacementFragment::isInterchangeNewlineComment(const NodeImpl *node)
 {
-    return isComment(node) && node->nodeValue() == KHTMLInterchangeNewline;
+    return isComment(node) && node->nodeValue() == AppleInterchangeNewline;
 }
 
 bool ReplacementFragment::isInterchangeConvertedSpaceSpan(const NodeImpl *node)
