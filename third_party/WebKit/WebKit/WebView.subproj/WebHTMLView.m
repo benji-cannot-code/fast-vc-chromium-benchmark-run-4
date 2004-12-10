e018a6a927740c580e2a218dab48f37334ae5e92
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-12-10 18:53:47 +0000

    Reviewed by Ken.
            - fixed <rdar://problem/3855127> Error while printing w/o sheet, then window is left in a bad state,
            if there's no default printer set
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView beginDocument]):
            Our implementation of knowsPageRange puts the WebHTMLView into a special "printing mode". We must
            exit the "printing mode" to return to normal behavior. This is normally done in endDocument.
            However, it turns out that if there's an exception in [super beginDocument], then endDocument
            will not be called (lame-o AppKit API). So, we handle that case by catching the exception and
            exiting the "printing mode" in beginDocument when it occurs.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8183 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -2716,6 +2716,18 @@ static WebHTMLView *lastHitView = nil;
     [[self _webView] _drawHeaderAndFooter];
 }
 
+- (void)beginDocument
+{
+    NS_DURING
+        [super beginDocument];
+    NS_HANDLER
+        // Exception during [super beginDocument] means that endDocument will not get called,
+        // so we need to clean up our "print mode" here.
+        [self _setPrinting:NO minimumPageWidth:0.0 maximumPageWidth:0.0 adjustViewSize:YES];
+        [[self window] setAutodisplay:YES];
+    NS_ENDHANDLER
+}
+
 - (void)endDocument
 {
     [super endDocument];
