7ab86f7640f3465fb430a40c5f654291d0bb0822
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2004-12-13 22:41:27 +0000

    Reviewed by John
            Fix for this bug:
    
            <rdar://problem/3917863> REGRESSION (Mail): pasting two lines of plain text copied from an RTF document results in two styles
    
            Code to figuire out the end node to merge was missing the font tag in the second paragraph
            written out by AppKit convert-to-HTML function. I refined the algorithm to be smarter.
    
            * khtml/editing/htmlediting.cpp:
            (khtml::ReplacementFragment::mergeEndNode): Refine algorithm used to walk through the fragment being pasted
            looking for the node that is the last inline in the last block of the fragment. The old algorithm was
            insufficiently powerful.
            (khtml::ReplacementFragment::enclosingBlock): New helper function.
            * khtml/editing/htmlediting.h: Add declaration for new helper function.
            * layout-tests/editing/pasteboard/paste-text-011-expected.txt: Added.
            * layout-tests/editing/pasteboard/paste-text-011.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8195 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting.h
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.h
@@ -642,6 +642,7 @@ private:
     static bool isInterchangeConvertedSpaceSpan(const DOM::NodeImpl *);
 
     // A couple simple DOM helpers
+    DOM::NodeImpl *enclosingBlock(DOM::NodeImpl *) const;
     void removeNode(DOM::NodeImpl *);
     void insertNodeBefore(DOM::NodeImpl *node, DOM::NodeImpl *refNode);
 
