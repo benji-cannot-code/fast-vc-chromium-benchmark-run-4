4fd2758aa048b72269678d4694ad952457060b4d
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-03 21:38:26 +0000

    Reviewed by Harrison
            Fix for this bug:
    
            <rdar://problem/3928250> REGRESSION (Mail): Typing style lost after hitting return key
    
            * khtml/editing/htmlediting.cpp:
            (khtml::InsertLineBreakCommand::preservesTypingStyle): Now implemented, returning yes for this command.
            (khtml::InsertParagraphSeparatorCommand::InsertParagraphSeparatorCommand): Initialize new typing style member variable.
            (khtml::InsertParagraphSeparatorCommand::~InsertParagraphSeparatorCommand): Deref new typing style member variable.
            (khtml::InsertParagraphSeparatorCommand::preservesTypingStyle): Now implemented, returning yes for this command.
            (khtml::InsertParagraphSeparatorCommand::setFullTypingStyleBeforeInsertion): New function to set the typing style
            (khtml::InsertParagraphSeparatorCommand::calculateAndSetTypingStyleAfterInsertion): Function called after the <p>
            insertion is done. This function diffs the style created in setFullTypingStyleBeforeInsertion() with the style
            of the new <p> and only sets those styles needed to preserve the style in effect before the insertion.
            (khtml::InsertParagraphSeparatorCommand::doApply): Call new functions.
            (khtml::TypingCommand::preservesTypingStyle): Now yes for inserting line breaks and paragraphs.
             * khtml/editing/htmlediting.h: Declare new functions.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8283 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
@@ -2041,6 +2041,11 @@ InsertLineBreakCommand::InsertLineBreakCommand(DocumentImpl *document)
 {
 }
 
+bool InsertLineBreakCommand::preservesTypingStyle() const
+{
+    return true;
+}
+
 void InsertLineBreakCommand::insertNodeAfterPosition(NodeImpl *node, const Position &pos)
 {
     // Insert the BR after the caret position. In the case the
@@ -2202,13 +2207,20 @@ void InsertNodeBeforeCommand::doUnapply()
 // InsertParagraphSeparatorCommand
 
 InsertParagraphSeparatorCommand::InsertParagraphSeparatorCommand(DocumentImpl *document) 
-    : CompositeEditCommand(document)
+    : CompositeEditCommand(document), m_fullTypingStyle(0)
 {
 }
 
 InsertParagraphSeparatorCommand::~InsertParagraphSeparatorCommand() 
 {
     derefNodesInList(clonedNodes);
+    if (m_fullTypingStyle)
+        m_fullTypingStyle->deref();
+}
+
+bool InsertParagraphSeparatorCommand::preservesTypingStyle() const
+{
+    return true;
 }
 
 ElementImpl *InsertParagraphSeparatorCommand::createParagraphElement()
@@ -2217,6 +2229,36 @@ ElementImpl *InsertParagraphSeparatorCommand::createParagraphElement()
     element->ref();
     clonedNodes.append(element);
     return element;
+}
+
+void InsertParagraphSeparatorCommand::setFullTypingStyleBeforeInsertion(const Position &pos)
+{
+    // FIXME: Improve typing style.
+    // See this bug: <rdar://problem/3769899> Implementation of typing style needs improvement
+    CSSComputedStyleDeclarationImpl *computedStyle = pos.computedStyle();
+    computedStyle->ref();
+    if (m_fullTypingStyle)
+        m_fullTypingStyle->deref();
+    m_fullTypingStyle = computedStyle->copyInheritableProperties();
+    m_fullTypingStyle->ref();
+    computedStyle->deref();
+}
+
+void InsertParagraphSeparatorCommand::calculateAndSetTypingStyleAfterInsertion()
+{
+    // FIXME: Improve typing style.
+    // See this bug: <rdar://problem/3769899> Implementation of typing style needs improvement
+    if (!m_fullTypingStyle)
+        return;
+
+    CSSComputedStyleDeclarationImpl endingStyle(endingSelection().start().node());
+    endingStyle.diff(m_fullTypingStyle);
+    if (!m_fullTypingStyle->length()) {
+        m_fullTypingStyle->deref();
+        m_fullTypingStyle = 0;
+    }
+    document()->part()->setTypingStyle(m_fullTypingStyle);
+    setTypingStyle(m_fullTypingStyle);
 }
 
 void InsertParagraphSeparatorCommand::doApply()
@@ -2231,19 +2273,24 @@ void InsertParagraphSeparatorCommand::doApply()
     // moving the selection downstream so it is in the ending block (if that block is
     // still around, that is).
     Position pos = selection.start();
+        
     if (selection.isRange()) {
         NodeImpl *startBlockBeforeDelete = selection.start().node()->enclosingBlockFlowElement();
         NodeImpl *endBlockBeforeDelete = selection.end().node()->enclosingBlockFlowElement();
         bool doneAfterDelete = startBlockBeforeDelete != endBlockBeforeDelete;
+        setFullTypingStyleBeforeInsertion(pos);
         deleteSelection(false, false);
         if (doneAfterDelete) {
             document()->updateLayout();
             setEndingSelection(endingSelection().start().downstream());
             rebalanceWhitespace();
+            calculateAndSetTypingStyleAfterInsertion();
             return;
         }
         pos = endingSelection().start();
     }
+
+    setFullTypingStyleBeforeInsertion(pos);
 
     // Find the start block.
     NodeImpl *startNode = pos.node();
@@ -2258,7 +2305,7 @@ void InsertParagraphSeparatorCommand::doApply()
 
     // This is the block that is going to be inserted.
     NodeImpl *blockToInsert = startBlockIsRoot ? createParagraphElement() : startBlock->cloneNode(false);
-    
+
     //---------------------------------------------------------------------
     // Handle empty block case.
     if (isFirstInBlock && isLastInBlock) {
@@ -2274,6 +2321,7 @@ void InsertParagraphSeparatorCommand::doApply()
         }
         insertBlockPlaceholderIfNeeded(blockToInsert);
         setEndingSelection(Position(blockToInsert, 0));
+        calculateAndSetTypingStyleAfterInsertion();
         return;
     }
 
@@ -2288,6 +2336,7 @@ void InsertParagraphSeparatorCommand::doApply()
         insertNodeBefore(blockToInsert, refNode);
         insertBlockPlaceholderIfNeeded(blockToInsert);
         setEndingSelection(pos);
+        calculateAndSetTypingStyleAfterInsertion();
         return;
     }
 
@@ -2301,6 +2350,7 @@ void InsertParagraphSeparatorCommand::doApply()
         insertNodeAfter(blockToInsert, refNode);
         insertBlockPlaceholderIfNeeded(blockToInsert);
         setEndingSelection(Position(blockToInsert, 0));
+        calculateAndSetTypingStyleAfterInsertion();
         return;
     }
 
@@ -2402,6 +2452,7 @@ void InsertParagraphSeparatorCommand::doApply()
 
     setEndingSelection(Position(blockToInsert, 0));
     rebalanceWhitespace();
+    calculateAndSetTypingStyleAfterInsertion();
 }
 
 //------------------------------------------------------------------------------------------
@@ -4059,9 +4110,9 @@ bool TypingCommand::preservesTypingStyle() const
 {
     switch (m_commandType) {
         case DeleteKey:
-            return true;
-        case InsertLineBreak:
         case InsertParagraphSeparator:
+        case InsertLineBreak:
+            return true;
         case InsertParagraphSeparatorInQuotedContent:
         case InsertText:
             return false;
--- third_party/WebKit/WebCore/khtml/editing/htmlediting.h
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.h
@@ -406,6 +406,7 @@ public:
     virtual void doApply();
 
 private:
+    virtual bool preservesTypingStyle() const;
     void insertNodeAfterPosition(DOM::NodeImpl *node, const DOM::Position &pos);
     void insertNodeBeforePosition(DOM::NodeImpl *node, const DOM::Position &pos);
 };
@@ -423,9 +424,14 @@ public:
 
 private:
     DOM::ElementImpl *createParagraphElement();
+    void setFullTypingStyleBeforeInsertion(const DOM::Position &);
+    void calculateAndSetTypingStyleAfterInsertion();
+
+    virtual bool preservesTypingStyle() const;
 
     QPtrList<DOM::NodeImpl> ancestors;
     QPtrList<DOM::NodeImpl> clonedNodes;
+    DOM::CSSMutableStyleDeclarationImpl *m_fullTypingStyle;
 };
 
 //------------------------------------------------------------------------------------------
