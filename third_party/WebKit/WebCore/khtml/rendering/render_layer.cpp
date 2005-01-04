08bdde6951e8e98186df7d17f8d7af8247777d74
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-04 23:19:09 +0000

    Fix for 3937203, force an update of the dashboard regions when scrollbars are added/removed.
            Reviewed by kevin
    
            * khtml/rendering/render_layer.cpp:
            (RenderLayer::updateScrollInfoAfterLayout):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8297 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_layer.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_layer.cpp
@@ -56,6 +56,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qscrollbar.h>
 #include <qptrvector.h>
 
+#if APPLE_CHANGES
+#include "KWQKHTMLPart.h" // For Dashboard.
+#endif
+
 using namespace DOM;
 using namespace khtml;
 
@@ -675,6 +679,13 @@ RenderLayer::updateScrollInfoAfterLayout()
         setHasVerticalScrollbar(needVerticalBar);
        
         m_object->repaint();
+
+#if APPLE_CHANGES
+        // Force an update since we know the scrollbars have changed things.
+        if (m_object->document()->hasDashboardRegions())
+            KWQ(m_object->document()->part())->dashboardRegionsChanged();
+#endif
+
         if (m_object->style()->overflow() == OAUTO) {
             // Our proprietary overflow: overlay value doesn't trigger a layout.
             m_object->setNeedsLayout(true);
