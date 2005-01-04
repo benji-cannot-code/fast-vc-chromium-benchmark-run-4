a953ba55bc2e13b6ab474d54024b9c7d1cb9e0b0
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-04 18:11:55 +0000

    Fix for 3904562, make sure to clear the outline dictionary when drawing so that random focus rings dont get drawn 	all over the place.
            Reviewed by john
    
            * khtml/rendering/render_flow.cpp:
            (RenderFlow::paintLines):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_flow.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_flow.cpp
@@ -402,6 +402,7 @@ void RenderFlow::paintLines(PaintInfo& i, int _tx, int _ty)
 #endif
                 objects.current()->paintOutlines(i.p, _tx, _ty);
         }
+        i.outlineObjects->clear();
     }
 }
 
