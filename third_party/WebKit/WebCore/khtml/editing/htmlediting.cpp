34d9dd8d1b9d3343be98e7e4c70f5a8610fae7bd
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-07 22:42:04 +0000

    Reviewed by Kevin
            Fix for these bugs:
    
            <rdar://problem/3939523> in some cases, text does not retain style info after pressing return twice
            <rdar://problem/3944492> after pressing return twice, text is bold when it shouldn't be
    
            * khtml/editing/htmlediting.cpp:
            (khtml::InsertParagraphSeparatorCommand::setFullTypingStyleBeforeInsertion):
            Merge the typing style with the computed style for the current position. Fixes both bugs.
            * khtml/editing/htmlediting.h:
            * layout-tests/editing/inserting/insert-div-023-expected.txt: Added.
            * layout-tests/editing/inserting/insert-div-023.html: Added.
            * layout-tests/editing/inserting/insert-div-024-expected.txt: Added.
            * layout-tests/editing/inserting/insert-div-024.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
@@ -2242,6 +2242,10 @@ void InsertParagraphSeparatorCommand::setFullTypingStyleBeforeInsertion(const Po
     m_fullTypingStyle = computedStyle->copyInheritableProperties();
     m_fullTypingStyle->ref();
     computedStyle->deref();
+    
+    CSSMutableStyleDeclarationImpl *typingStyle = document()->part()->typingStyle();
+    if (typingStyle)
+        m_fullTypingStyle->merge(typingStyle);
 }
 
 void InsertParagraphSeparatorCommand::calculateAndSetTypingStyleAfterInsertion()
