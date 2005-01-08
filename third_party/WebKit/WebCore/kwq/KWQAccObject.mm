851cf38d760b8d1a7e808ea0ecd2c1cf2c8f59cb
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-08 00:17:30 +0000

    Reviewed by Darin.
            <rdar://problem/3942619> AX: Support sentence ax attributes
    
            Needed to use the unicode utilities properly.  Twas lame before.
    
            * khtml/editing/visible_units.cpp:
            (khtml::previousBoundary):
            (khtml::nextBoundary):
            (khtml::startOfWord):
            (khtml::endOfWord):
            (khtml::previousWordPosition):
            (khtml::nextWordPosition):
            (khtml::startOfSentence):
            (khtml::endOfSentence):
            (khtml::previousSentencePosition):
            (khtml::nextSentencePosition):
            * kwq/KWQAccObject.mm:
            (-[KWQAccObject accessibilityAttributeNames]):
            (-[KWQAccObject accessibilityAttributeValue:]):
            (-[KWQAccObject accessibilityParameterizedAttributeNames]):
            (-[KWQAccObject doAXAttributedStringForTextMarkerRange:]):
            * kwq/KWQTextUtilities.mm:
            (currentTextBreakLocaleID):
            (KWQFindSentenceBoundary):
            (KWQFindNextSentenceFromIndex):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8321 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQAccObject.mm
+++ third_party/WebKit/WebCore/kwq/KWQAccObject.mm
@@ -562,7 +562,7 @@ static QRect boundingBoxRect(RenderObject* obj)
 // no parameterized attributes in Panther... they were introduced in Tiger
 #else
             (NSString *) kAXSelectedTextMarkerRangeAttribute,
-//          (NSString *) kAXVisibleCharacterTextMarkerRangeAttribute,    // NOTE: BUG FO 4
+//          (NSString *) kAXVisibleCharacterTextMarkerRangeAttribute,    // NOTE: <rdar://problem/3942582>
             (NSString *) kAXStartTextMarkerAttribute,
             (NSString *) kAXEndTextMarkerAttribute,
 #endif
@@ -586,7 +586,7 @@ static QRect boundingBoxRect(RenderObject* obj)
 // no parameterized attributes in Panther... they were introduced in Tiger
 #else
             (NSString *) kAXSelectedTextMarkerRangeAttribute,
-//          (NSString *) kAXVisibleCharacterTextMarkerRangeAttribute,     // NOTE: BUG FO 4
+//          (NSString *) kAXVisibleCharacterTextMarkerRangeAttribute,     // NOTE: <rdar://problem/3942582>
             (NSString *) kAXStartTextMarkerAttribute,
             (NSString *) kAXEndTextMarkerAttribute,
 #endif
@@ -612,7 +612,7 @@ static QRect boundingBoxRect(RenderObject* obj)
 // no parameterized attributes in Panther... they were introduced in Tiger
 #else
             (NSString *) kAXSelectedTextMarkerRangeAttribute,
-//          (NSString *) kAXVisibleCharacterTextMarkerRangeAttribute,     // NOTE: BUG FO 4
+//          (NSString *) kAXVisibleCharacterTextMarkerRangeAttribute,     // NOTE: NOTE: <rdar://problem/3942582>
             (NSString *) kAXStartTextMarkerAttribute,
             (NSString *) kAXEndTextMarkerAttribute,
 #endif
@@ -886,10 +886,6 @@ static QRect boundingBoxRect(RenderObject* obj)
         return (id) [self textMarkerRangeFromVisiblePositions:startPosition andEndPos:endPosition];
     }
     
-    if ([attributeName isEqualToString: (NSString *) kAXVisibleCharacterTextMarkerRangeAttribute]) {
-        return nil;     // NOTE: BUG FO 4
-    }
-    
     if ([attributeName isEqualToString: (NSString *) kAXStartTextMarkerAttribute]) {
         return (id) [self textMarkerForVisiblePosition: VisiblePosition([self topRenderer]->positionForCoordinates (0, 0, nil))];
     }
@@ -916,8 +912,8 @@ static QRect boundingBoxRect(RenderObject* obj)
             kAXStringForTextMarkerRangeParameterizedAttribute,
             kAXTextMarkerForPositionParameterizedAttribute,
             kAXBoundsForTextMarkerRangeParameterizedAttribute,
-//          kAXStyleTextMarkerRangeForTextMarkerParameterizedAttribute,                 // NOTE: BUG FO 3
-//          kAXAttributedStringForTextMarkerRangeParameterizedAttribute,                // NOTE: BUG FO 2
+//          kAXStyleTextMarkerRangeForTextMarkerParameterizedAttribute,           // NOTE: <rdar://problem/3942606>
+//          kAXAttributedStringForTextMarkerRangeParameterizedAttribute,          // NOTE: <rdar://problem/3942647>
             kAXTextMarkerRangeForUnorderedTextMarkersParameterizedAttribute,
             kAXNextTextMarkerForTextMarkerParameterizedAttribute,
             kAXPreviousTextMarkerForTextMarkerParameterizedAttribute,
@@ -925,14 +921,14 @@ static QRect boundingBoxRect(RenderObject* obj)
             kAXRightWordTextMarkerRangeForTextMarkerParameterizedAttribute,
             kAXLeftLineTextMarkerRangeForTextMarkerParameterizedAttribute,
             kAXRightLineTextMarkerRangeForTextMarkerParameterizedAttribute,
-//          kAXSentenceTextMarkerRangeForTextMarkerParameterizedAttribute,              // NOTE: BUG FO 2
+            kAXSentenceTextMarkerRangeForTextMarkerParameterizedAttribute,
             kAXParagraphTextMarkerRangeForTextMarkerParameterizedAttribute,
             kAXNextWordEndTextMarkerForTextMarkerParameterizedAttribute,
             kAXPreviousWordStartTextMarkerForTextMarkerParameterizedAttribute,
             kAXNextLineEndTextMarkerForTextMarkerParameterizedAttribute,
             kAXPreviousLineStartTextMarkerForTextMarkerParameterizedAttribute,
-//          kAXNextSentenceEndTextMarkerForTextMarkerParameterizedAttribute,            // NOTE: BUG FO 2
-//          kAXPreviousSentenceStartTextMarkerForTextMarkerParameterizedAttribute,      // NOTE: BUG FO 2
+            kAXNextSentenceEndTextMarkerForTextMarkerParameterizedAttribute,
+            kAXPreviousSentenceStartTextMarkerForTextMarkerParameterizedAttribute,
             kAXNextParagraphEndTextMarkerForTextMarkerParameterizedAttribute,
             kAXPreviousParagraphStartTextMarkerForTextMarkerParameterizedAttribute,
             kAXLengthForTextMarkerRangeParameterizedAttribute,
@@ -1083,7 +1079,7 @@ static QRect boundingBoxRect(RenderObject* obj)
 
 - (id)doAXAttributedStringForTextMarkerRange: (AXTextMarkerRangeRef) textMarkerRange
 {
-    // NOTE: BUG FO 2 Needs to make AX attributed string 
+    // NOTE: <rdar://problem/3942606>. Needs to make AX attributed string instead
     // Patti: Get the basic done first.  Add various text attribute support incrementally,
     // starting with attachment and hyperlink.  The rest of the attributes can be FO2/3.
 
