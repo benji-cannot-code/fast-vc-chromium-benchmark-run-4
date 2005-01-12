75007208b26820c2b1cccb518d81e6c2261db5ce
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-12 23:24:18 +0000

    Fixed <rdar://problem/3923356> REGRESSION: Java/JavaScript security checks working incorrectly
    	We were always returning the first "root" object for all runtime
    	objects.  Changed 0 in loop to i, the index.
    
            Reviewed by David Harrison.
    
            * bindings/runtime_root.cpp:
            (KJS::Bindings::rootForImp):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/runtime_root.cpp
+++ third_party/WebKit/JavaScriptCore/bindings/runtime_root.cpp
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using namespace KJS;
 using namespace KJS::Bindings;
 
-
 // Java does NOT always call finalize (and thus KJS_JSObject_JSFinalize) when
 // it collects an objects.  This presents some difficulties.  We must ensure
 // the a JSObject's corresponding JavaScript object doesn't get collected.  We
@@ -116,7 +115,7 @@ const Bindings::RootObject *KJS::Bindings::rootForImp (ObjectImp *imp)
         for(i = 0; i < count; i++) {
             CFMutableDictionaryRef referencesDictionary = (CFMutableDictionaryRef)allValues[i];
             if (CFDictionaryGetValue(referencesDictionary, imp) != 0) {
-                rootObject = (const Bindings::RootObject *)allKeys[0];
+                rootObject = (const Bindings::RootObject *)allKeys[i];
                 break;
             }
         }
