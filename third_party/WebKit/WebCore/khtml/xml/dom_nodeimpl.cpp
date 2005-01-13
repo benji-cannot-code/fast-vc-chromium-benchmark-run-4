28e7dd40ac59209c1945c1c8aa244e680119f81c
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-13 01:19:52 +0000

    Reviewed by Darin Adler.
            * khtml/xml/dom_nodeimpl.cpp:
            (NodeImpl::displayNode):
            Add quotes around text node content.
            (NodeBaseImpl::childNode):
            Add nil check to return nil rather than crash when child node not found.
            * kwq/KWQAccObject.mm:
            (-[KWQAccObject doAXStringForTextMarkerRange:]):
            Pass range compliant positions to TextIterator.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8352 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.cpp
@@ -1382,7 +1382,7 @@ void NodeImpl::displayNode(const char *prefix)
     if (!prefix)
         prefix = "";
     if (isTextNode())
-        fprintf(stderr, "%s%s\t%p %s\n", prefix, nodeName().string().latin1(), this, nodeValue().string().latin1());
+        fprintf(stderr, "%s%s\t%p \"%s\"\n", prefix, nodeName().string().latin1(), this, nodeValue().string().latin1());
     else
         fprintf(stderr, "%s%s\t%p\n", prefix, nodeName().string().latin1(), this);
 }
@@ -2096,7 +2096,7 @@ NodeImpl *NodeBaseImpl::childNode(unsigned long index)
 {
     unsigned long i;
     NodeImpl *n = firstChild();
-    for (i = 0; i < index; i++)
+    for (i = 0; n != 0 && i < index; i++)
         n = n->nextSibling();
     return n;
 }
