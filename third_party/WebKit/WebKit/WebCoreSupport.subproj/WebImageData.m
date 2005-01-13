1dd356b0cce365039161afac6098fadcc1f1db42
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-13 22:13:04 +0000

    Fixed <rdar://problem/3951911> REGRESSION: Animated GIF images with loop counts no longer update
    	Draw last image after animation loop terminates.  (We
    	were drawing the image at index+1, which doesn't exist!)
    
            Reviewed by Darin.
    
            * WebCoreSupport.subproj/WebImageData.m:
            (-[WebImageData _nextFrame:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8365 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageData.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageData.m
@@ -755,6 +755,7 @@ static NSMutableSet *activeAnimations;
         repetitionsComplete += 1;
         if ([self _repetitionCount] && repetitionsComplete >= [self _repetitionCount]) {
             animationFinished = YES;
+	    currentFrame--;
             return;
         }
         currentFrame = 0;
