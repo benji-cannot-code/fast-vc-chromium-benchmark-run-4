047c1895dd233c34e1d742ddea0bfa478f86ff66
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-18 01:06:27 +0000

    * DOM.subproj/DOMPrivate.h: Check in generated file.
    git-svn-id: svn://svn.chromium.org/blink/trunk@8391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/DOM.subproj/DOMPrivate.h
+++ third_party/WebKit/WebKit/DOM.subproj/DOMPrivate.h
@@ -39,3 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface DOMElement (WebPrivate)
 - (NSFont *)_font;
 @end
+
+@interface DOMCSSStyleDeclaration (WebPrivate)
+- (NSString *)_fontSizeDelta;
+- (void)_setFontSizeDelta:(NSString *)fontSizeDelta;
+@end
