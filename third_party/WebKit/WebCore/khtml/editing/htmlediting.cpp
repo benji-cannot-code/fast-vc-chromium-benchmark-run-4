3601c1216744799338229287b25ca60c8d2197dd
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-20 00:01:58 +0000

    Reviewed by John
            * khtml/editing/htmlediting.cpp:
            (khtml::InsertParagraphSeparatorCommand::applyStyleAfterInsertion): Small, cosmetic change John and I decided
            to do on my last checkin, but I forgot to do before landing.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8405 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
@@ -2597,11 +2597,7 @@ void InsertParagraphSeparatorCommand::applyStyleAfterInsertion()
 
     CSSComputedStyleDeclarationImpl endingStyle(endingSelection().start().node());
     endingStyle.diff(m_style);
-    if (!m_style->length()) {
-        m_style->deref();
-        m_style = 0;
-    }
-    else {
+    if (m_style->length() > 0) {
         applyStyle(m_style);
     }
 }
