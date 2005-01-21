4709b51fbf1e4e8c8e62856edaffdf815e09edb2
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-21 01:22:08 +0000

    Reviewed by me, changes by Han Ming Ong.
            - <rdar://problem/3964302> SWB: A few files need to be updated to be compilable under GCC 4.0
    
            * bindings/objc/WebScriptObjectPrivate.h: Make members public.
            * kjs/lookup.h: Change "value.h" to "object.h" because we need KJS::Object to compile a template.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8421 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/objc/WebScriptObjectPrivate.h
+++ third_party/WebKit/JavaScriptCore/bindings/objc/WebScriptObjectPrivate.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface WebScriptObjectPrivate : NSObject
 {
+@public
     KJS::ObjectImp *imp;
     const KJS::Bindings::RootObject *executionContext;
     const KJS::Bindings::RootObject *originExecutionContext;
--- third_party/WebKit/JavaScriptCore/kjs/lookup.h
+++ third_party/WebKit/JavaScriptCore/kjs/lookup.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define _KJSLOOKUP_H_
 
 #include "identifier.h"
-#include "value.h"
+#include "object.h"
 #include <stdio.h>
 
 namespace KJS {
