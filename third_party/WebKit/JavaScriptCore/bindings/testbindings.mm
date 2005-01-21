96758327a2c204f5ae36876aa65248f90f1dc937
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-21 00:41:59 +0000

    Fixed <rdar://problem/3964634> undefined property value from binding seems to evaluate to true in an if statement
    	The comprehensive fix for this problem requires new API, as described in 3965326.  However,
    	given that we can't add new API at this point, the 'ObjcFallbackObjectImp' will behave
    	like and Undefined object if  invokeUndefinedMethodFromWebScript:withArguments: isn't
    	implemented on the bound object.
    
            Reviewed by Chris.
    
            * bindings/objc/objc_runtime.h:
            * bindings/objc/objc_runtime.mm:
            (ObjcFallbackObjectImp::type):
            (ObjcFallbackObjectImp::implementsCall):
            (ObjcFallbackObjectImp::toBoolean):
            * bindings/testbindings.mm:
            (+[MyFirstInterface isSelectorExcludedFromWebScript:]):
            (+[MyFirstInterface isKeyExcludedFromWebScript:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8418 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/testbindings.mm
+++ third_party/WebKit/JavaScriptCore/bindings/testbindings.mm
@@ -92,6 +92,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return nil;
 }
 
++ (BOOL)isSelectorExcludedFromWebScript:(SEL)aSelector
+{
+    return NO;
+}
+
++ (BOOL)isKeyExcludedFromWebScript:(const char *)name
+{
+    return NO;
+}
+
+/*
 - (id)invokeUndefinedMethodFromWebScript:(NSString *)name withArguments:(NSArray *)args;
 {
 	NSLog (@"Call to undefined method %@", name);
@@ -102,12 +113,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 	}
 	return @"success";
 }
+*/
 
+/*
 - (id)valueForUndefinedKey:(NSString *)key
 {
 	NSLog (@"%s:  key = %@", __PRETTY_FUNCTION__, key);
 	return @"aValue";
 }
+*/
 
 - (void)setValue:(id)value forUndefinedKey:(NSString *)key
 {
