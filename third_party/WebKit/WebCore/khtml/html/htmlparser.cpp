e985fc28531fb65cfdb4de2f66494007ef1c65b6
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-21 21:45:20 +0000

    Fix for 3966349, hang loading page.  Make sure that loops using popOneBlock properly check for a null 	blockStack to avoid an infinite loop.
            Reviewed by kocienda
    
            * khtml/html/htmlparser.cpp:
            (KHTMLParser::parseToken):
            (KHTMLParser::insertNode):
            (KHTMLParser::popInlineBlocks):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8427 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/htmlparser.cpp
+++ third_party/WebKit/WebCore/khtml/html/htmlparser.cpp
@@ -258,7 +258,7 @@ void KHTMLParser::parseToken(Token *t)
 
     // if this tag is forbidden inside the current context, pop
     // blocks until we are allowed to add it...
-    while (t->id <= ID_LAST_TAG && forbiddenTag[t->id]) {
+    while (blockStack && t->id <= ID_LAST_TAG && forbiddenTag[t->id]) {
 #ifdef PARSER_DEBUG
         kdDebug( 6035 ) << "t->id: " << t->id << " is forbidden :-( " << endl;
 #endif
@@ -360,7 +360,7 @@ bool KHTMLParser::insertNode(NodeImpl *n, bool flat)
         case ID_TD:
             if (inStrayTableContent && !isTableRelatedTag(current->id())) {
                 // pop out to the nearest enclosing table-related tag.
-                while (!isTableRelatedTag(current->id()))
+                while (blockStack && !isTableRelatedTag(current->id()))
                     popOneBlock();
                 return insertNode(n);
             }
@@ -540,7 +540,7 @@ bool KHTMLParser::insertNode(NodeImpl *n, bool flat)
         case ID_TFOOT:
         case ID_COLGROUP: {
             if (isTableRelatedTag(current->id())) {
-                while (current->id() != ID_TABLE && isTableRelatedTag(current->id()))
+                while (blockStack && current->id() != ID_TABLE && isTableRelatedTag(current->id()))
                     popOneBlock();
                 return insertNode(n);
             }
@@ -1437,7 +1437,7 @@ void KHTMLParser::popOneBlock(bool delBlock)
 
 void KHTMLParser::popInlineBlocks()
 {
-    while(current->isInline())
+    while (blockStack && current->isInline())
         popOneBlock();
 }
 
