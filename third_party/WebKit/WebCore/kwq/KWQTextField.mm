2b14582b7aab444ab365d8df65939850238ca352
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-24 22:21:48 +0000

    Reviewed by Kevin.
            - fixed <rdar://problem/3969884> REGRESSION (179-180): Typing password not echoed as "bullets" at paypal.com when field is first focused
    
            * kwq/KWQTextField.mm: (-[KWQTextFieldController initWithTextField:QLineEdit:]):
            Sizing up the form editor, which fixes things for normal fields and search fields,
            screws things up for password fields. Eventually, I'll need to figure out why this is
            and file a bug so that the AppKit team fixes it. In the mean time, just check for the
            case of the secure text field, and don't size up in that case.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextField.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextField.mm
@@ -100,7 +100,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // Set a non-empty size in case the field is made first responder before it
     // is positioned and sized by KHTML code. This dodges bad behavior in AppKit
     // if you try to create an editor for a 0-sized text field.
-    [field setFrameSize:NSMakeSize(100, 100)];
+    // For a secure text field, this is both unnecessary and harmful, so leave
+    // it out; worth figuring out why at some point.
+    if (![field isKindOfClass:[NSSecureTextField class]]) {
+        [field setFrameSize:NSMakeSize(100, 100)];
+    }
 
     return self;
 }
