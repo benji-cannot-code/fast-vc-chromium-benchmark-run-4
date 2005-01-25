19efaa857b1f49fec8a86e19c787dd6a57f2e518
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-25 21:34:22 +0000

    Reviewed by John
            Fix for this bug:
    
            <rdar://problem/3972851> REGRESSION (179-180+): Repro crash in ApplyStyleCommand::cleanUpEmptyStyleSpans
    
            * khtml/editing/htmlediting.cpp:
            (khtml::ApplyStyleCommand::cleanUpEmptyStyleSpans): Added two null checks.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8447 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
@@ -1707,7 +1707,7 @@ void ApplyStyleCommand::cleanUpEmptyStyleSpans(const Position &start, const Posi
     for (node = start.node(); node && !node->previousSibling(); node = node->parentNode()) {
     }
 
-    if (isEmptyStyleSpan(node->previousSibling())) {
+    if (node && isEmptyStyleSpan(node->previousSibling())) {
         removeNodePreservingChildren(node->previousSibling());
     }
 
@@ -1743,7 +1743,7 @@ void ApplyStyleCommand::cleanUpEmptyStyleSpans(const Position &start, const Posi
     
     for (node = end.node(); node && !node->nextSibling(); node = node->parentNode()) {
     }
-    if (isEmptyStyleSpan(node->nextSibling())) {
+    if (node && isEmptyStyleSpan(node->nextSibling())) {
         removeNodePreservingChildren(node->nextSibling());
     }
 }
