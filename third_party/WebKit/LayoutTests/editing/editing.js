5d399dcdd39e70eed335d63a03084ba407808221
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-28 00:43:57 +0000

    WebCore:
            Reviewed by Maciej
    
            Fix for this bug:
    
            <rdar://problem/3973254> Deletions of ranges does not coalesce correctly with subsequent typing
    
            * khtml/editing/htmlediting.cpp:
            (khtml::InsertTextCommand::deleteCharacter): Remove this dead code.
            (khtml::ReplaceSelectionCommand::editingAction): Moved this code to a better place in the file.
            It was in with unrelated functions.
            (khtml::TypingCommand::TypingCommand): Reorganize initialization list so it is easier to read.
            (khtml::TypingCommand::issueCommandForDeleteKey): Remove dead code. Roll in remaining code into deleteKeyPressed.
            (khtml::TypingCommand::deleteKeyPressed): Add in support for carrying along smart delete flag.
            (khtml::TypingCommand::forwardDeleteKeyPressed): New function. Makes forward delete work like
            "regular" delete in terms of coalescing typing.
            (khtml::TypingCommand::doApply): Add case for ForwardDeleteKey.
            (khtml::TypingCommand::preservesTypingStyle): Ditto.
            * khtml/editing/htmlediting.h:
            (khtml::TypingCommand::): Add ForwardDeleteKey constant. Remove a couple declarations for now-dead code.
            (khtml::TypingCommand::smartDelete): New accessor.
            (khtml::TypingCommand::setSmartDelete): Ditto.
            * kwq/WebCoreBridge.h:
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge deleteKeyPressedWithSmartDelete:]): Add smart delete flag.
            (-[WebCoreBridge forwardDeleteKeyPressedWithSmartDelete:]): New method.
            * khtml/editing/jsediting.cpp: Add supprt for ForwardDelete command
            * layout-tests/editing/editing.js: Ditto.
            * layout-tests/editing/deleting/delete-and-undo-expected.txt: Added.
            * layout-tests/editing/deleting/delete-and-undo.html: Added.
            * layout-tests/editing/deleting/forward-delete-expected.txt: Added.
            * layout-tests/editing/deleting/forward-delete.html: Added.
    
    WebKit:
    
            Reviewed by Maciej
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView _handleKillRing:prepend:]): New helper function. Code factored out from
             _deleteRange:killRing:prepend:smartDeleteOK:isTypingAction:.
            (-[WebHTMLView _deleteRange:killRing:prepend:smartDeleteOK:isTypingAction:]): No longer takes
            unused preflight argument. Now takes new isTypingAction argument. Uses isTypingAction to
            determine which flavor of delete command to call.
            (-[WebHTMLView _deleteSelection]): No longer passes unused preflight argument. Now passes new isTypingAction argument.
            (-[WebHTMLView _deleteWithDirection:granularity:killRing:isTypingAction:]): Rearranged code to call
            correct flavor of delete command, based on whether typing should be preserved. Some other cleanups.
            (-[WebHTMLView deleteForward:]): Add _isEditable check.
            (-[WebHTMLView deleteBackward:]): Now calls _deleteWithDirection instead of having unique behavior different
            from forward delete.
            (-[WebHTMLView deleteWordForward:]): Add new isTypingAction flag to _deleteWithDirection call.
            (-[WebHTMLView deleteWordBackward:]): Ditto.
            (-[WebHTMLView deleteToBeginningOfLine:]): Ditto.
            (-[WebHTMLView deleteToEndOfLine:]): Ditto.
            (-[WebHTMLView deleteToBeginningOfParagraph:]): Ditto.
            (-[WebHTMLView deleteToEndOfParagraph:]): Ditto.
            (-[WebHTMLView deleteToMark:]): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8464 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/editing/editing.js
+++ third_party/WebKit/LayoutTests/editing/editing.js
@@ -485,6 +485,21 @@ function deleteCommand() {
 
 //-------------------------------------------------------------------------------------------------------
 
+function execForwardDeleteCommand() {
+    document.execCommand("ForwardDelete");
+}
+function forwardDeleteCommand() {
+    if (commandDelay > 0) {
+        window.setTimeout(execForwardDeleteCommand, commandCount * commandDelay);
+        commandCount++;
+    }
+    else {
+        execForwardDeleteCommand();
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------
+
 function runEditingTest() {
     var elem = document.getElementById("test");
     var selection = window.getSelection();
