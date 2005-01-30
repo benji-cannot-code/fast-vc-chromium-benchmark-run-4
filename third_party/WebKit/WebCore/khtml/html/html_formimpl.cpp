e97fb6bef51f0e3522c7c152a00aac4914458e13
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-01-30 18:21:08 +0000

    Reviewed by John.
            - fixed <rdar://problem/3977000> form data set posted for <input type="image" ...> omits name/value pair
    
            * khtml/html/html_formimpl.cpp:
            (DOM::HTMLInputElementImpl::appendFormData): Append a name/value pair for type IMAGE in addition to the X/Y
            coordinates.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_formimpl.cpp
+++ third_party/WebKit/WebCore/khtml/html/html_formimpl.cpp
@@ -1786,6 +1786,8 @@ bool HTMLInputElementImpl::appendFormData(FormDataList &encoding, bool multipart
             {
                 encoding.appendData(name().isEmpty() ? QString::fromLatin1("x") : (name().string() + ".x"), clickX());
                 encoding.appendData(name().isEmpty() ? QString::fromLatin1("y") : (name().string() + ".y"), clickY());
+                if (!name().isEmpty() && !value().isEmpty())
+                    encoding.appendData(name(), value());
                 return true;
             }
             break;
