a4e6f762f676c7011fc161dc5b955ac3a8bb7768
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-02 19:09:36 +0000

    Reviewed by Darin
            Fix for this bug:
    
            <rdar://problem/3823109> WebKit should support -toggleBaseWritingDirection: (bidi editing support)
    
            Since base writing direction is a paragraph-level property in AppKit, and we use the CSS direction
            property in WebCore, which can be applied to inline elements as well as blocks, a new notion has
            been added to the WebCore apply style logic. It is now possible to force all properties in a
            style declaration to be applied as though they were block properties.
    
            * khtml/editing/htmlediting.cpp:
            (khtml::ApplyStyleCommand::ApplyStyleCommand): Add an enum and a flag to this command that controls whether to force all
            properties in the style to be applied as block styles. Default is "no", retaining previous behavior.
            (khtml::ApplyStyleCommand::doApply): Switch on new flag to apply styles as before, or force all preoperties to be applied
            as block styles.
            * khtml/editing/htmlediting.h:
            (khtml::ApplyStyleCommand::): Add an enum and a flag, as above.
            * khtml/khtml_part.cpp:
            (KHTMLPart::applyParagraphStyle): New method to force application of all style properties as block styles.
            * khtml/khtml_part.h: Update header declarations.
            * kwq/KWQKHTMLPart.h: Update header declarations.
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::baseWritingDirectionForSelectionStart): Accessor to help WebKit do a toggle operation on
            writing direction.
            * kwq/WebCoreBridge.h: Update header declarations.
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge applyParagraphStyle:withUndoAction:]): New method to force application of all style properties as block styles.
            (-[WebCoreBridge baseWritingDirectionForSelectionStart]): Accessor to help WebKit do a toggle operation on
            writing direction.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/visible_units.cpp
+++ third_party/WebKit/WebCore/khtml/editing/visible_units.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <qstring.h>
 
+#include "htmltags.h"
 #include "misc/helper.h"
 #include "rendering/render_text.h"
 #include "visible_position.h"
@@ -283,7 +284,7 @@ VisiblePosition endOfLine(const VisiblePosition &c, EAffinity affinity, EInclude
     if (!rootBox)
         return VisiblePosition();
     
-    InlineBox *endBox = rootBox->lastChild();
+    InlineBox *endBox = rootBox->lastLeafChild();
     if (!endBox)
         return VisiblePosition();
 
@@ -296,7 +297,10 @@ VisiblePosition endOfLine(const VisiblePosition &c, EAffinity affinity, EInclude
         return VisiblePosition();
 
     long endOffset = 1;
-    if (endBox->isInlineTextBox()) {
+    if (endNode->id() == ID_BR) {
+        endOffset = 0;
+    }
+    else if (endBox->isInlineTextBox()) {
         InlineTextBox *endTextBox = static_cast<InlineTextBox *>(endBox);
         endOffset = endTextBox->m_start + endTextBox->m_len;
     }
--- third_party/WebKit/WebCore/kwq/KWQKHTMLPart.h
+++ third_party/WebKit/WebCore/kwq/KWQKHTMLPart.h
@@ -109,6 +109,8 @@ class NSView;
 class WebCoreBridge;
 class WebScriptObject;
 
+typedef int NSWritingDirection;
+
 #endif
 
 enum KWQSelectionDirection {
@@ -238,6 +240,8 @@ public:
 
     NSFont *fontForSelection(bool *hasMultipleFonts) const;
     NSDictionary *fontAttributesForSelectionStart() const;
+    
+    NSWritingDirection baseWritingDirectionForSelectionStart() const;
 
     void markMisspellingsInAdjacentWords(const khtml::VisiblePosition &);
     void markMisspellings(const khtml::Selection &);
