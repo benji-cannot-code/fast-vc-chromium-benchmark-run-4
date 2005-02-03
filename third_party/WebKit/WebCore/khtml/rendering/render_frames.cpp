e674920a4be14782efd199571a77532f76cec86f
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-03 00:47:31 +0000

    Fixed: <rdar://problem/3960304> can't load a particular applet (at www.escape.de) unless it's the first applet to be loaded
            Reviewed by hyatt.
    
            * khtml/rendering/render_frames.cpp:
            (RenderPartObject::updateWidget): when getting the MIME type from the PARAM tag, make sure the MIME type is the text from the left of the semi-colon if there is one. We do this elsewhere as well.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8508 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_frames.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_frames.cpp
@@ -748,6 +748,10 @@ void RenderPartObject::updateWidget()
               }
               if (serviceType.isEmpty() && name == "type") {
                   serviceType = p->value();
+                  int pos = serviceType.find( ";" );
+                  if (pos != -1) {
+                      serviceType = serviceType.left(pos);
+                  }
               }
               if (!embed) {
                   bool dummyValue = true;
