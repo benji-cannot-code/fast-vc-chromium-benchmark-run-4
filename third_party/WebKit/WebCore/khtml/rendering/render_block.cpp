780914205de406673b8ade5bd90c273bdf68afb4
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-10 10:21:00 +0000

    Fix for 3980778, repro crash in RootInlineBox::paint.  Make sure that when merging blocks we dirty them for layout 	up front. That way as we do the merge, we dont attempt to examine the lines in dirtyLinesForChangedChild.
            Reviewed by mjs
    
            * khtml/rendering/render_block.cpp:
            (khtml::RenderBlock::removeChild):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8560 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_block.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_block.cpp
@@ -310,6 +310,7 @@ void RenderBlock::removeChild(RenderObject *oldChild)
     if (canDeleteAnonymousBlocks && prev && next) {
         // Take all the children out of the |next| block and put them in
         // the |prev| block.
+        prev->setNeedsLayoutAndMinMaxRecalc();
         RenderObject* o = next->firstChild();
         while (o) {
             RenderObject* no = o;
@@ -317,8 +318,6 @@ void RenderBlock::removeChild(RenderObject *oldChild)
             prev->appendChildNode(next->removeChildNode(no));
             no->setNeedsLayoutAndMinMaxRecalc();
         }
-        prev->setNeedsLayoutAndMinMaxRecalc();
-
         // Nuke the now-empty block.
         next->detach();
     }
@@ -330,6 +329,7 @@ void RenderBlock::removeChild(RenderObject *oldChild)
         // The removal has knocked us down to containing only a single anonymous
         // box.  We can go ahead and pull the content right back up into our
         // box.
+        setNeedsLayoutAndMinMaxRecalc();
         RenderObject* anonBlock = removeChildNode(child);
         m_childrenInline = true;
         RenderObject* o = anonBlock->firstChild();
@@ -339,7 +339,6 @@ void RenderBlock::removeChild(RenderObject *oldChild)
             appendChildNode(anonBlock->removeChildNode(no));
             no->setNeedsLayoutAndMinMaxRecalc();
         }
-        setNeedsLayoutAndMinMaxRecalc();
 
         // Nuke the now-empty block.
         anonBlock->detach();
