2072a377e50fb6750abe99b64369732b3cd2c8bd
vicki <vicki@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-16 19:55:51 +0000

    Reviewed by me, code change by Darin.
            - fixed the build on Panther
    
            * WebView.subproj/WebFormDataStream.m: (webSetHTTPBody):
            Added a Panther-specific code path that just loads all the data into one big
            NSData object. This means that bug 3686434 won't be fixed on SUPanWheat; we'll
            still load the file into memory before sending it to the server on Panther.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFormDataStream.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFormDataStream.m
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebAssertions.h"
 #import "WebNSObjectExtras.h"
 
+#if !BUILDING_ON_PANTHER
+
 static void formEventCallback(CFReadStreamRef stream, CFStreamEventType type, void *context);
 
 typedef struct {
@@ -260,6 +262,8 @@ static void formEventCallback(CFReadStreamRef stream, CFStreamEventType type, vo
     }
 }
 
+#endif // !BUILDING_ON_PANTHER
+
 void webSetHTTPBody(NSMutableURLRequest *request, NSArray *formData)
 {
     unsigned count = [formData count];
@@ -272,6 +276,8 @@ void webSetHTTPBody(NSMutableURLRequest *request, NSArray *formData)
             return;
         }
     }
+
+#if !BUILDING_ON_PANTHER
 
     // Precompute the content length so NSURLConnection doesn't use chunked mode.
     long long length = 0;
@@ -305,4 +311,33 @@ void webSetHTTPBody(NSMutableURLRequest *request, NSArray *formData)
     CFReadStreamRef stream = CFReadStreamCreate(NULL, &callBacks, formData);
     [request setHTTPBodyStream:(NSInputStream *)stream];
     CFRelease(stream);
+
+#else
+
+    NSMutableData *allData = [[NSMutableData alloc] init];
+
+    unsigned i;
+    for (i = 0; i < count; ++i) {
+        id data = [formData objectAtIndex:i];
+        if ([data isKindOfClass:[NSData class]]) {
+            NSData *d = data;
+            [allData appendData:d];
+        } else if ([data isKindOfClass:[NSString class]]) {
+            NSString *s = data;
+            NSData *d = [[NSData alloc] initWithContentsOfFile:s];
+            if (d != nil) {
+                [allData appendData:d];
+                [d release];
+            }
+        } else {
+            ERROR("item in form data array is neither NSData nor NSString");
+            return;
+        }
+    }
+
+    [request setHTTPBody:allData];
+    
+    [allData release];
+
+#endif
 }
