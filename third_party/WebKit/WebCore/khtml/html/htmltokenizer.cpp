71378919a3812bc2ccc567631431e31b0ae6c26d
jens <jens@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-17 19:53:50 +0000

    Fix for rdar://3963151 "Mail only pasted ~950 of 1407 text lines into my message!" Force tokenizer to run synchronously while parsing document fragments, so it doesnt stop halfway through and cause truncated content.
    git-svn-id: svn://svn.chromium.org/blink/trunk@8611 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/htmltokenizer.cpp
+++ third_party/WebKit/WebCore/khtml/html/htmltokenizer.cpp
@@ -299,6 +299,7 @@ void HTMLTokenizer::reset()
     }
     timerId = 0;
     allowYield = false;
+    forceSynchronous = false;
 
     currToken.reset();
 }
@@ -343,6 +344,12 @@ void HTMLTokenizer::begin()
     lineno = 0;
     scriptStartLineno = 0;
     tagStartLineno = 0;
+    forceSynchronous = false;
+}
+
+void HTMLTokenizer::setForceSynchronous(bool force)
+{
+    forceSynchronous = force;
 }
 
 void HTMLTokenizer::processListing(TokenizerString list)
@@ -1813,7 +1820,7 @@ bool HTMLTokenizer::continueProcessing(int& processedCount, const QTime& startTi
     // processed a certain number of characters.
     bool allowedYield = allowYield;
     allowYield = false;
-    if (!loadingExtScript && !m_executingScript && (processedCount > TOKENIZER_CHUNK_SIZE || allowedYield)) {
+    if (!loadingExtScript && !forceSynchronous && !m_executingScript && (processedCount > TOKENIZER_CHUNK_SIZE || allowedYield)) {
         processedCount = 0;
         if (startTime.elapsed() > TOKENIZER_TIME_DELAY) {
             /* FIXME: We'd like to yield aggressively to give stylesheets the opportunity to
--- third_party/WebKit/WebCore/khtml/html/htmltokenizer.h
+++ third_party/WebKit/WebCore/khtml/html/htmltokenizer.h
@@ -132,6 +132,7 @@ public:
     virtual void write(const TokenizerString &str, bool appendData);
     virtual void finish();
     virtual void setOnHold(bool onHold);
+    virtual void setForceSynchronous(bool force);
     virtual bool isWaitingForScripts() const;
     virtual void stopped();
     virtual bool processingData() const;
@@ -354,6 +355,7 @@ protected:
     // The timer for continued processing.
     int timerId;
     bool allowYield;
+    bool forceSynchronous;  // disables yielding
 
     bool includesCommentsInDOM;
 
