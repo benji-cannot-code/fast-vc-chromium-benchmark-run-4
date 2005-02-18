42a4239e2715da4084e8f0e2117f467834f2be3f
jens <jens@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-18 21:39:27 +0000

    [3603191] REGRESSION: Applets not receiving all of the Applet Parameters in Java 1.4.1/1.4.2. Fix is to defer instantiating the applet plugin until the applet tag and its nested param tags have been completely parsed.
    git-svn-id: svn://svn.chromium.org/blink/trunk@8634 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_objectimpl.cpp
+++ third_party/WebKit/WebCore/khtml/html/html_objectimpl.cpp
@@ -60,6 +60,7 @@ HTMLAppletElementImpl::HTMLAppletElementImpl(DocumentPtr *doc)
   : HTMLElementImpl(doc)
 {
     appletInstance = 0;
+    m_allParamsAvailable = false;
 }
 
 HTMLAppletElementImpl::~HTMLAppletElementImpl()
@@ -210,6 +211,19 @@ KJS::Bindings::Instance *HTMLAppletElementImpl::getAppletInstance() const
         }
     }
     return appletInstance;
+}
+
+void HTMLAppletElementImpl::setAllParamsAvailable()
+{
+    // The parser just reached </applet>, so all the params are available now.
+    m_allParamsAvailable = true;
+    if( m_render )
+        m_render->setNeedsLayout(); // This will cause it to create its widget & the Java applet
+}
+
+bool HTMLAppletElementImpl::allParamsAvailable()
+{
+    return m_allParamsAvailable;
 }
 #endif
 
