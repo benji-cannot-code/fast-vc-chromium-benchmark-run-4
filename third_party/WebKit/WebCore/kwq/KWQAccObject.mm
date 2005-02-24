c5137bfbc1b9fa2f2fe23441e2bc94a5633c861a
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-24 22:17:27 +0000

    Reviewed by Ken.
            <rdar://problem/3990849> AX: textMarkerRange for an AXUIElement within an AXWebArea
    
            * kwq/KWQAccObject.mm:
            (-[KWQAccObject textMarkerRange]):
            (-[KWQAccObject accessibilityParameterizedAttributeNames]):
            (-[KWQAccObject doAXTextMarkerRangeForUIElement:]):
            (-[KWQAccObject accessibilityAttributeValue:forParameter:]):
            Added AXTextMarkerRangeForUIElement, or you can send textMarkerRange to the UIElement itself.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQAccObject.mm
+++ third_party/WebKit/WebCore/kwq/KWQAccObject.mm
@@ -801,6 +801,16 @@ static QRect boundingBoxRect(RenderObject* obj)
     AXTextMarkerRef endTextMarker   = [self textMarkerForVisiblePosition: endPosition];
     return [self textMarkerRangeFromMarkers: startTextMarker andEndMarker:endTextMarker];
 }
+
+- (AXTextMarkerRangeRef)textMarkerRange
+{
+    if (!m_renderer)
+        return nil;
+        
+    AXTextMarkerRef startTextMarker = [self textMarkerForVisiblePosition: VisiblePosition(m_renderer->element(), m_renderer->caretMinOffset(), khtml::VP_DEFAULT_AFFINITY)];
+    AXTextMarkerRef endTextMarker   = [self textMarkerForVisiblePosition: VisiblePosition(m_renderer->element(), m_renderer->caretMaxRenderedOffset(), khtml::VP_DEFAULT_AFFINITY)];
+    return [self textMarkerRangeFromMarkers: startTextMarker andEndMarker:endTextMarker];
+}
 #endif
 
 - (DocumentImpl *)topDocument
@@ -959,6 +969,7 @@ static QRect boundingBoxRect(RenderObject* obj)
     if (paramAttributes == nil) {
         paramAttributes = [[NSArray alloc] initWithObjects:
             @"AXUIElementForTextMarker",
+            @"AXTextMarkerRangeForUIElement",
             kAXLineForTextMarkerParameterizedAttribute,
             kAXTextMarkerRangeForLineParameterizedAttribute,
             kAXStringForTextMarkerRangeParameterizedAttribute,
@@ -1001,6 +1012,11 @@ static QRect boundingBoxRect(RenderObject* obj)
         return nil;
     
     return obj->document()->getAccObjectCache()->accObject(obj);
+}
+
+- (id)doAXTextMarkerRangeForUIElement: (id) uiElement
+{
+    return (id)[uiElement textMarkerRange];
 }
 
 - (id)doAXLineForTextMarker: (AXTextMarkerRef) textMarker
@@ -1656,6 +1672,7 @@ static void AXAttributedStringAppendReplaced (NSMutableAttributedString *attrStr
     AXValueRef              value = nil;
     NSNumber *              number = nil;
     NSArray *               array = nil;
+    KWQAccObject *          uiElement = nil;
     CGPoint                 point;
     CGSize                  size;
     CGRect                  rect;
@@ -1674,6 +1691,9 @@ static void AXAttributedStringAppendReplaced (NSMutableAttributedString *attrStr
 
     else if (CFGetTypeID(parameter) == AXTextMarkerRangeGetTypeID())
         textMarkerRange = (AXTextMarkerRangeRef) parameter;
+
+    else if ([parameter isKindOfClass:[KWQAccObject self]])
+        uiElement = (KWQAccObject *) parameter;
 
     else if ([parameter isKindOfClass:[NSNumber self]])
         number = parameter;
@@ -1707,6 +1727,9 @@ static void AXAttributedStringAppendReplaced (NSMutableAttributedString *attrStr
     // dispatch
     if ([attribute isEqualToString: @"AXUIElementForTextMarker"])
         return [self doAXUIElementForTextMarker: textMarker];
+
+    if ([attribute isEqualToString: @"AXTextMarkerRangeForUIElement"])
+        return [self doAXTextMarkerRangeForUIElement: uiElement];
 
     if ([attribute isEqualToString: (NSString *) kAXLineForTextMarkerParameterizedAttribute])
         return [self doAXLineForTextMarker: textMarker];
