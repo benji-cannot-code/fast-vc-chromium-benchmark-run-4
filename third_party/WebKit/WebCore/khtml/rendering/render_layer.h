df4d038fdf23d34a7014a3f1ffd8929dd6c56cdb
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-25 17:33:10 +0000

    Fix for 3975039, scrolling is slow in huge RSS views.  Optimize the calculation of clip rects for overflow:hidden 	layers.  Also optimize layer movement when scrolling overflow sections.
            Reviewed by darin
    
            * khtml/rendering/render_layer.cpp:
            (throw):
            (ClipRects::operator delete):
            (ClipRects::detach):
            (RenderLayer::RenderLayer):
            (RenderLayer::~RenderLayer):
            (RenderLayer::updateLayerPosition):
            (RenderLayer::removeOnlyThisLayer):
            (RenderLayer::insertOnlyThisLayer):
            (RenderLayer::scrollToOffset):
            (RenderLayer::hitTest):
            (RenderLayer::calculateClipRects):
            (RenderLayer::calculateRects):
            (RenderLayer::containsPoint):
            (RenderLayer::clearClipRects):
            (RenderLayer::clearClipRect):
            * khtml/rendering/render_layer.h:
            (khtml::ClipRects::m_refCnt):
            (khtml::ClipRects::overflowClipRect):
            (khtml::ClipRects::fixedClipRect):
            (khtml::ClipRects::posClipRect):
            (khtml::ClipRects::ref):
            (khtml::ClipRects::deref):
            (khtml::RenderLayer::clipRects):
            * khtml/rendering/render_object.cpp:
            (RenderObject::setStyle):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8692 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_layer.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_layer.h
@@ -76,6 +76,39 @@ private:
     RenderLayer* m_layer;
 };
 
+class ClipRects
+{
+public:
+    ClipRects(const QRect& r) :m_overflowClipRect(r), m_fixedClipRect(r), m_posClipRect(r), m_refCnt(0) {}
+    ClipRects(const QRect& o, const QRect& f, const QRect& p)
+      :m_overflowClipRect(o), m_fixedClipRect(f), m_posClipRect(p), m_refCnt(0) {}
+
+    const QRect& overflowClipRect() { return m_overflowClipRect; }
+    const QRect& fixedClipRect() { return m_fixedClipRect; }
+    const QRect& posClipRect() { return m_posClipRect; }
+
+    void ref() { m_refCnt++; }
+    void deref(RenderArena* renderArena) { if (--m_refCnt == 0) detach(renderArena); }
+    
+    void detach(RenderArena* renderArena);
+
+    // Overloaded new operator.
+    void* operator new(size_t sz, RenderArena* renderArena) throw();    
+
+    // Overridden to prevent the normal delete from being called.
+    void operator delete(void* ptr, size_t sz);
+        
+private:
+    // The normal operator new is disallowed on all render objects.
+    void* operator new(size_t sz) throw();
+
+private:
+    QRect m_overflowClipRect;
+    QRect m_fixedClipRect;
+    QRect m_posClipRect;
+    uint m_refCnt;
+};
+
 // This class handles the auto-scrolling of layers with overflow: marquee.
 class Marquee: public QObject
 {
@@ -212,6 +245,9 @@ public:
     void relativePositionOffset(int& relX, int& relY) {
         relX += m_relX; relY += m_relY;
     }
+     
+    void clearClipRects();
+    void clearClipRect();
 
     // Get the enclosing stacking context for this layer.  A stacking context is a layer
     // that has a non-auto z-index.
@@ -244,8 +280,8 @@ public:
     // for painting/event handling.
     void calculateRects(const RenderLayer* rootLayer, const QRect& paintDirtyRect, QRect& layerBounds,
                         QRect& backgroundRect, QRect& foregroundRect);
-    void calculateClipRects(const RenderLayer* rootLayer, QRect& overflowClipRect,
-                            QRect& posClipRect, QRect& fixedClipRect);
+    void calculateClipRects(const RenderLayer* rootLayer);
+    ClipRects* clipRects() const { return m_clipRects; }
 
     bool intersectsDamageRect(const QRect& layerBounds, const QRect& damageRect) const;
     bool containsPoint(int x, int y, const QRect& damageRect) const;
@@ -281,7 +317,7 @@ private:
     RenderLayer* hitTestLayer(RenderLayer* rootLayer, RenderObject::NodeInfo& info,
                               int x, int y, const QRect& hitTestRect);
     void computeScrollDimensions(bool* needHBar = 0, bool* needVBar = 0);
-    
+
 protected:   
     RenderObject* m_object;
     
@@ -327,8 +363,11 @@ protected:
     QPtrVector<RenderLayer>* m_posZOrderList;
     QPtrVector<RenderLayer>* m_negZOrderList;
     
+    ClipRects* m_clipRects;      // Cached clip rects used when painting and hit testing.
+
     bool m_scrollDimensionsDirty : 1;
     bool m_zOrderListsDirty : 1;
+
 #if APPLE_CHANGES
     bool m_usedTransparency : 1; // Tracks whether we need to close a transparent layer, i.e., whether
                                  // we ended up painting this layer or any descendants (and therefore need to
