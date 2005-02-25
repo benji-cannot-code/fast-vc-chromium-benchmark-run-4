cdfda0e4da977968da30a8761b4b661f73b4f6ef
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-25 00:00:43 +0000

    Fixed: <rdar://problem/4020110> Safari crashes in setAllData while taking a www.zoomerang.com survey
            Reviewed by rjw.
    
            * kwq/KWQResourceLoader.mm:
            (-[KWQResourceLoader finishJobAndHandle:]): clear the job after we've deleted to avoid reentrancy
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8686 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQResourceLoader.mm
+++ third_party/WebKit/WebCore/kwq/KWQResourceLoader.mm
@@ -92,9 +92,10 @@ using KIO::TransferJob;
 
     if (job) {
         job->emitResult(data);
+        [handle release];
     }
     delete job;
-    [handle release];
+    job = 0;
 }
 
 - (void)cancel
