3f91ff905786e62de7c6cea3b42c281e8000f036
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-25 19:34:52 +0000

    Fixed: <rdar://problem/4023566> Stickies: Crash in ReplacementFragment::insertFragmentForTestRendering on paste
            Reviewed by darin.
    
            * khtml/editing/htmlediting.cpp:
            (khtml::ApplyStyleCommand::addInlineStyleIfNeeded):
            (khtml::ReplacementFragment::insertFragmentForTestRendering):
            (khtml::floatRefdElement):
            (khtml::createDefaultParagraphElement):
            (khtml::createBlockPlaceholderElement):
            (khtml::createFontElement):
            (khtml::createStyleSpanElement):
            * khtml/editing/htmlediting.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
@@ -2177,8 +2177,10 @@ void ApplyStyleCommand::addInlineStyleIfNeeded(CSSMutableStyleDeclarationImpl *s
 
     if (styleChange.cssStyle().length() > 0) {
         ElementImpl *styleElement = createStyleSpanElement(document());
+        styleElement->ref();
         styleElement->setAttribute(ATTR_STYLE, styleChange.cssStyle());
         insertNodeBefore(styleElement, startNode);
+        styleElement->deref();
         surroundNodeRangeWithElement(startNode, endNode, styleElement);
     }
 
@@ -3107,6 +3109,7 @@ bool InsertParagraphSeparatorCommand::preservesTypingStyle() const
     return true;
 }
 
+// FIXME: callers of createParagraphElement are not properly deref'ing the returned ref'd element. 
 ElementImpl *InsertParagraphSeparatorCommand::createParagraphElement()
 {
     ElementImpl *element = createDefaultParagraphElement(document());
@@ -4248,6 +4251,7 @@ NodeImpl *ReplacementFragment::insertFragmentForTestRendering()
         return 0;
 
     ElementImpl *holder = createDefaultParagraphElement(m_document);
+    holder->ref();
     
     int exceptionCode = 0;
     holder->appendChild(m_fragment, exceptionCode);
@@ -4258,7 +4262,7 @@ NodeImpl *ReplacementFragment::insertFragmentForTestRendering()
     
     m_document->updateLayout();
     
-    return holder;
+    return floatRefdElement(holder);
 }
 
 void ReplacementFragment::restoreTestRenderingNodesToFragment(NodeImpl *holder)
@@ -5546,15 +5550,19 @@ bool TypingCommand::isTypingCommand() const
     return true;
 }
 
+ElementImpl *floatRefdElement(ElementImpl *node)
+{
+    node->setParent(node->getDocument());
+    node->deref();
+    node->setParent(0);
+    return node;
+}
+
 ElementImpl *createDefaultParagraphElement(DocumentImpl *document)
 {
-    // We would need this margin-zeroing and attribute-setter code back if we ever 
-    // return to using <p> elements for default paragraphs.
-    //static const DOMString defaultParagraphStyle("margin-top: 0; margin-bottom: 0");
     int exceptionCode = 0;
     ElementImpl *element = document->createHTMLElement("div", exceptionCode);
     ASSERT(exceptionCode == 0);
-    //element->setAttribute(ATTR_STYLE, defaultParagraphStyle);
     return element;
 }
 
@@ -5562,9 +5570,10 @@ ElementImpl *createBlockPlaceholderElement(DocumentImpl *document)
 {
     int exceptionCode = 0;
     ElementImpl *breakNode = document->createHTMLElement("br", exceptionCode);
+    breakNode->ref();
     ASSERT(exceptionCode == 0);
     breakNode->setAttribute(ATTR_CLASS, blockPlaceholderClassString());
-    return breakNode;
+    return floatRefdElement(breakNode);
 }
 
 ElementImpl *createBreakElement(DocumentImpl *document)
@@ -5579,18 +5588,20 @@ ElementImpl *createFontElement(DocumentImpl *document)
 {
     int exceptionCode = 0;
     ElementImpl *fontNode = document->createHTMLElement("font", exceptionCode);
+    fontNode->ref();
     ASSERT(exceptionCode == 0);
     fontNode->setAttribute(ATTR_CLASS, styleSpanClassString());
-    return fontNode;
+    return floatRefdElement(fontNode);
 }
 
 ElementImpl *createStyleSpanElement(DocumentImpl *document)
 {
     int exceptionCode = 0;
     ElementImpl *styleElement = document->createHTMLElement("SPAN", exceptionCode);
+    styleElement->ref();
     ASSERT(exceptionCode == 0);
     styleElement->setAttribute(ATTR_CLASS, styleSpanClassString());
-    return styleElement;
+    return floatRefdElement(styleElement);
 }
 
 bool isNodeRendered(const NodeImpl *node)
