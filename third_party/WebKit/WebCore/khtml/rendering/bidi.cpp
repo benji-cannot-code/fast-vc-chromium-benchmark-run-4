c769731f04ec3096d2fcac421b27de871d7ff29f
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-25 23:41:39 +0000

    Fixed <rdar://problem/3382926> Bidi neutrals at RTL/LTR boundaries not handled correctly.
    	Second pass at fixing 3382926 w/o causing layout regressions.  Same concept:
    	if directionality of text's element is RTL and first character has neutral directionality
    	then set the initial directionality to RTL.
    
            Reviewed by Hyatt.
    
            * khtml/rendering/bidi.cpp:
            (khtml::RenderBlock::bidiReorderLine):
            (khtml::RenderBlock::determineStartPosition):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8708 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
@@ -60,7 +60,6 @@ struct BidiIterator
     unsigned int pos;
 };
 
-
 struct BidiStatus {
     BidiStatus() : eor(QChar::DirON), lastStrong(QChar::DirON), last(QChar::DirON) {}
     
@@ -870,6 +869,19 @@ void RenderBlock::bidiReorderLine(const BidiIterator &start, const BidiIterator 
     //    context->ref();
 
     dir = QChar::DirON;
+    
+    // Adopt the directionality of the text's element if specified as RTL
+    // and the first position is neutral.
+    if (start.direction() == QChar::DirON) {
+        if (start.obj) {
+            if (start.obj->style()->direction() == RTL)
+                dir = QChar::DirR;
+        }
+        else if (style()->direction() == RTL) {
+            dir = QChar::DirR;
+        }
+    }
+
     emptyRun = true;
 
     numSpaces = 0;
@@ -877,6 +889,7 @@ void RenderBlock::bidiReorderLine(const BidiIterator &start, const BidiIterator 
     bidi.current = start;
     bidi.last = bidi.current;
     bool atEnd = false;
+
     while( 1 ) {
 
         QChar::Direction dirCurrent;
@@ -1672,6 +1685,7 @@ RootInlineBox* RenderBlock::determineStartPosition(bool fullLayout, BidiIterator
         
     adjustEmbedding = true;
     start = BidiIterator(this, startObj, pos);
+    
     adjustEmbedding = false;
     
     return curr;
