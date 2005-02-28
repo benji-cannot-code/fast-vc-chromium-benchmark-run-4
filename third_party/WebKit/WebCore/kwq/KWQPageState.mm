eef2be8392a2282c291ae3be6999087c92324229
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-02-28 23:10:43 +0000

    Fixed <rdar://problem/4026985> CrashTracer: ...14 crashes at com.apple.WebCore: -[KWQPageState invalidate] + 32
    	Added more nil checking and ASSERTS.
    	Without a reproducible case this is hard to definitively resolve.
    
            Reviewed by John Sullivan.
    
            * kwq/KWQPageState.mm:
            (-[KWQPageState invalidate]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8719 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQPageState.mm
+++ third_party/WebKit/WebCore/kwq/KWQPageState.mm
@@ -107,11 +107,14 @@ using KJS::SavedBuiltins;
 {
     // Should only ever invalidate once.
     ASSERT(document);
+    ASSERT(document->view());
     ASSERT(!document->inPageCache());
 
-    document->view()->deref();
-    document->deref();
-
+    if (document && document->view()) {
+        document->view()->deref();
+        document->deref();
+    }
+    
     [self clear];
 }
 
