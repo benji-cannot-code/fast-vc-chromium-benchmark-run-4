16c7ddf68d0974b2fa6c058a530e4dcd145d4bc8
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-01 22:27:42 +0000

    Reviewed by Darin.
            - fixed <rdar://problem/3987482> Format>Style>Italic is not enabled when a
            compose window is empty (works in Blot)
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView becomeFirstResponder]):
            call _updateFontPanel here so NSFontManager knows the right font for the menu
            items and the font panel
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -2920,6 +2920,7 @@ static WebHTMLView *lastHitView = nil;
         [[self window] makeFirstResponder:view];
     }
     [self updateFocusState];
+    [self _updateFontPanel];
     _private->startNewKillRingSequence = YES;
     return YES;
 }
