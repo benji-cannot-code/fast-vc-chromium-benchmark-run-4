05641b08ef6b68423fc7e42198fe77dd8d0eeebc
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-03 01:13:06 +0000

    Fixed: <rdar://problem/4029010> Expose method to retrieve drag image for WebView's selection
            Reviewed by sullivan.
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView _startDraggingImage:at:operation:event:sourceIsDHTML:DHTMLWroteData:]): call _selectionDraggingImage
            (-[WebHTMLView _selectionDraggingImage]): new SPI for Mail, factored from previous method
            (-[WebHTMLView _selectionDraggingRect]): new SPI for Mail
            * WebView.subproj/WebHTMLViewPrivate.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewPrivate.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLViewPrivate.h
@@ -64,4 +64,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BOOL)_transparentBackground;
 - (void)_setTransparentBackground:(BOOL)f;
 
+// SPI's for Mail.
+- (NSImage *)_selectionDraggingImage;
+- (NSRect)_selectionDraggingRect;
+
 @end
