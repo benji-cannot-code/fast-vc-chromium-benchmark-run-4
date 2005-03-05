57a5fbfac8d2968fa4edece3693af0117a36fd37
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-05 23:20:51 +0000

    Reviewed by Don.
            - fixed <rdar://problem/4038478> Crash in renderer viewing RSS feed at feed://wvs.topleftpixel.com/index.rdf
    
            * khtml/xml/dom_docimpl.cpp: (widgetForNode): Check for nil before going from node to renderer.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8797 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_docimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_docimpl.cpp
@@ -2450,6 +2450,8 @@ void DocumentImpl::setDashboardRegions (const QValueList<DashboardRegionValue>& 
 
 static QWidget *widgetForNode(NodeImpl *focusNode)
 {
+    if (!focusNode)
+        return 0;
     RenderObject *renderer = focusNode->renderer();
     if (!renderer || !renderer->isWidget())
         return 0;
