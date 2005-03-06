b9c87c1e756bd645405b4f56ea5bfb8aa11e1162
kali <kali@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-06 08:03:56 +0000

    - fixed problem with searching for BIDI text boxes (3587609) - fixed missing initalizer of RTL flag in WebCoreStyle
    git-svn-id: svn://svn.chromium.org/blink/trunk@8801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQFontMetrics.mm
+++ third_party/WebKit/WebCore/kwq/KWQFontMetrics.mm
@@ -300,7 +300,6 @@ int QFontMetrics::checkSelectionPoint (QChar *s, int slen, int pos, int len, int
     }
     
     CREATE_FAMILY_ARRAY(data->font(), families);
-
     WebCoreTextRun run;
     WebCoreInitializeTextRun(&run, (const UniChar *)s, slen, pos, pos+len);
     
@@ -311,6 +310,7 @@ int QFontMetrics::checkSelectionPoint (QChar *s, int slen, int pos, int len, int
     style.smallCaps = smallCaps;
     style.families = families;
     style.padding = toAdd;
+    style.rtl = reversed;
 
     return [data->getRenderer() pointToOffset:&run style:&style position:x reversed:reversed includePartialGlyphs:includePartialGlyphs];
 }
