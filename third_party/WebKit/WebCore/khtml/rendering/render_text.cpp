2a3450c8f7b8a1cc1b4657ca073eb2e30d1b4cc8
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-08 02:27:50 +0000

    Reviewed by Darin.
            - <rdar://problem/4040868> REGRESSION (Mail, 403-403+): Drag and drop deletes text,
            many other bad editing problems
    
            * khtml/rendering/render_text.cpp:
            (RenderText::caretMaxOffset):
            A "-" should have been a "+" in this method that was tweaked an hour ago. Editing
            was completely horked.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8822 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_text.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_text.cpp
@@ -1776,7 +1776,7 @@ long RenderText::caretMaxOffset() const
     InlineTextBox* box = lastTextBox();
     if (!box) 
         return str->l;
-    int maxOffset = box->m_start - box->m_len;
+    int maxOffset = box->m_start + box->m_len;
     for (box = box->prevTextBox(); box; box = box->prevTextBox())
 	maxOffset = kMax(maxOffset,box->m_start + box->m_len);
     return maxOffset;
