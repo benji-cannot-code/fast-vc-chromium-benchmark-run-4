c1f48639f3d7ecd5d3a2862bd631d99cc00768fe
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-09 00:00:52 +0000

    Fixed <rdar://problem/4042570> Need to check image properties for presence of width/height properties
    	ImageIO-55 changed how image properties are created.  They
    	are now created incrementally.  So we need "re-get" the image
    	properties if the properties we care about (width/height) aren't
    	in the property dictionary.
    
            Reviewed by John.
    
            * WebCoreSupport.subproj/WebImageData.h:
            * WebCoreSupport.subproj/WebImageData.m:
            (-[WebImageData init]):
            (-[WebImageData fileProperties]):
            (-[WebImageData propertiesAtIndex:]):
            (-[WebImageData _isSizeAvailable]):
            (-[WebImageData incrementalLoadWithBytes:length:complete:callback:]):
            (-[WebImageData size]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageData.h
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageData.h
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     
     BOOL isSolidColor;                              // Is frame 0 a solid color?
     CGColorRef solidColor;                          // If isSolidColor this is the color, or NULL for transparent
+    
+    BOOL sizeAvailable;
 }
 
 - (size_t)numberOfImages;
