33a35990a952096e1dfa626176b5ea22d93a4a41
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-09 18:37:58 +0000

    * DOM.subproj/DOMPrivate.h: Checked in file copied from WebCore.
    git-svn-id: svn://svn.chromium.org/blink/trunk@8833 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/DOM.subproj/DOMPrivate.h
+++ third_party/WebKit/WebKit/DOM.subproj/DOMPrivate.h
@@ -45,3 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSString *)_fontSizeDelta;
 - (void)_setFontSizeDelta:(NSString *)fontSizeDelta;
 @end
+
+@interface DOMHTMLDocument (WebPrivate)
+- (DOMDocumentFragment *)_createDocumentFragmentWithMarkupString:(NSString *)markupString baseURLString:(NSString *)baseURLString;
+- (DOMDocumentFragment *)_createDocumentFragmentWithText:(NSString *)text;
+@end
