50ffb42821b3f44384823e11d795937cd01d7b75
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-09 00:08:04 +0000

    Reviewed by Ken.
            <rdar://problem/4039006> REGRESSION (Mail): Command-left-arrow goes to the start of the wrong line in particular message
    
            * khtml/editing/visible_units.cpp:
            (khtml::startOfLine):
            Use firstLeafChild() instead of firstChild().
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8829 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/visible_units.cpp
+++ third_party/WebKit/WebCore/khtml/editing/visible_units.cpp
@@ -286,7 +286,7 @@ VisiblePosition startOfLine(const VisiblePosition &c)
     if (!rootBox)
         return VisiblePosition();
     
-    InlineBox *startBox = rootBox->firstChild();
+    InlineBox *startBox = rootBox->firstLeafChild();
     if (!startBox)
         return VisiblePosition();
 
@@ -303,6 +303,7 @@ VisiblePosition startOfLine(const VisiblePosition &c)
         InlineTextBox *startTextBox = static_cast<InlineTextBox *>(startBox);
         startOffset = startTextBox->m_start;
     }
+    
     return VisiblePosition(startNode, startOffset, DOWNSTREAM);
 }
 
