148c72812532ff30fc20dc075620986e7a1aded4
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-10 21:26:44 +0000

    Reviewed by Vicki.
            - fixed <rdar://problem/4045843> Going back/forward to error page
            hits assertion in -[WebDataSource(WebPrivate) _setData:]
    
            * WebView.subproj/WebDataSource.m:
            (-[WebDataSource _setData:]):
            Removed bogus assertion
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8845 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
@@ -304,7 +304,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)_setData:(NSData *)data
 {
-    ASSERT(_private->resourceData == nil);
     [data retain];
     [_private->resourceData release];
     _private->resourceData = data;
