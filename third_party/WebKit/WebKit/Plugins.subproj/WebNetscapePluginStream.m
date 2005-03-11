d1009d35673eb7be65dd568c78a53549d11b71df
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-11 01:27:18 +0000

    Reviewed by Vicki.
    	<rdar://problem/4046510> REGRESSION (TOT): All Flash and Shockwave plugin-based web content missing
    
    	* Plugins.subproj/WebNetscapePluginStream.m:
            (-[WebNetscapePluginStream initWithRequest:pluginPointer:notifyData:sendNotification:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebNetscapePluginStream.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebNetscapePluginStream.m
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     WebBridge *bridge = [[view webFrame] _bridge];
     BOOL hideReferrer;
-    if (![bridge canLoadURL:[request URL] fromReferrer:[bridge referrer] hideReferrer:&hideReferrer])
+    if (![bridge canLoadURL:[theRequest URL] fromReferrer:[bridge referrer] hideReferrer:&hideReferrer])
         return nil;
 
     if ([self initWithRequestURL:[theRequest URL]
