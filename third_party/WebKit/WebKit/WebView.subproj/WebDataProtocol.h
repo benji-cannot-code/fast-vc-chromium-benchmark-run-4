146403b2d63fee2a44a8b3a9836fc870948bb334
rjw <rjw@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-11 01:43:15 +0000

    Fixed <rdar://problem/4040321> Exception: Someone's trying to encode a WebDataRequestParameters instance
            Reviewed by Darin.
    
    	If a delegate returns a mutated applewebdata: request in it's willSendRequest:
    	method, we don't load using the WebDataRequest.  Instead we do a normal load.
    	Unfortunately, if the request they return is mutated *copy* of the applewebdata:
    	request it will hold the applewebdata: special properties.  These properties
    	will be encoded into the cache.   They should not be.  So, to fix, we sanitize the
    	request, by removing the special properties from the request.
    
    	Note that we had to dig into the private guts of NSURLRequest because there is
    	no public mechanism to remove properties from a request, see 4046775.
    
            * WebView.subproj/WebBaseResourceHandleDelegate.m:
            (-[WebBaseResourceHandleDelegate willSendRequest:redirectResponse:]):
            * WebView.subproj/WebDataProtocol.h:
            * WebView.subproj/WebDataProtocol.m:
            (-[NSURLRequest _webDataRequestExternalRequest]):
            (-[NSURLRequest _webDataRequestSanitize]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataProtocol.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataProtocol.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSString *)_webDataRequestEncoding;
 - (NSString *)_webDataRequestMIMEType;
 - (NSMutableURLRequest *)_webDataRequestExternalRequest;
+- (void)_webDataRequestSanitize;
 @end
 
 @interface NSMutableURLRequest (WebDataRequest)
