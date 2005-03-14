22c4a56f5ff8d78d35fb33b3a94190d8bde395a1
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-14 17:49:31 +0000

    Reviewed by Ken and Maciej.
            - fixed <rdar://problem/4049040> REGRESSION (403-405): security check prevents user stylesheet from loading (Dictionary.app doesn't work at all!)
    
            * Plugins.subproj/WebNetscapePluginStream.m: (-[WebNetscapePluginStream initWithRequest:pluginPointer:notifyData:sendNotification:]):
            Allow plug-in subresource streams to load with any URL, ignoring the "canLoadURL" method's restriction (only
            file URLs can load other file URLs), which now applies only to main resources, like web pages in frames or
            object tags and plug-in main resources.
    
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge startLoadingResource:withURL:customHeaders:]): Allow subresources to load with any URL, as above.
            This allows things like images, stylesheets, and JavaScript to be loaded without the "canLoadURL" method's
            restriction.
            (-[WebBridge startLoadingResource:withURL:customHeaders:postData:]): Ditto.
            (-[WebBridge syncLoadResourceWithURL:customHeaders:postData:finalURL:responseHeaders:statusCode:]): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8874 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -423,9 +423,10 @@ NSString *WebPluginContainerKey =   @"WebPluginContainer";
 	return nil;
     }
 
+    // Since this is a subresource, we can load any URL (we ignore the return value).
+    // But we still want to know whether we should hide the referrer or not, so we call the canLoadURL method.
     BOOL hideReferrer;
-    if (![self canLoadURL:URL fromReferrer:[self referrer] hideReferrer:&hideReferrer])
-        return nil;
+    [self canLoadURL:URL fromReferrer:[self referrer] hideReferrer:&hideReferrer];
 
     return [WebSubresourceClient startLoadingResource:resourceLoader
                                               withURL:URL
@@ -442,9 +443,10 @@ NSString *WebPluginContainerKey =   @"WebPluginContainer";
 	return nil;
     }
 
+    // Since this is a subresource, we can load any URL (we ignore the return value).
+    // But we still want to know whether we should hide the referrer or not, so we call the canLoadURL method.
     BOOL hideReferrer;
-    if (![self canLoadURL:URL fromReferrer:[self referrer] hideReferrer:&hideReferrer])
-        return nil;
+    [self canLoadURL:URL fromReferrer:[self referrer] hideReferrer:&hideReferrer];
 
     return [WebSubresourceClient startLoadingResource:resourceLoader
                                               withURL:URL
@@ -473,9 +475,10 @@ NSString *WebPluginContainerKey =   @"WebPluginContainer";
 
 - (NSData *)syncLoadResourceWithURL:(NSURL *)URL customHeaders:(NSDictionary *)requestHeaders postData:(NSArray *)postData finalURL:(NSURL **)finalURL responseHeaders:(NSDictionary **)responseHeaderDict statusCode:(int *)statusCode
 {
+    // Since this is a subresource, we can load any URL (we ignore the return value).
+    // But we still want to know whether we should hide the referrer or not, so we call the canLoadURL method.
     BOOL hideReferrer;
-    if (![self canLoadURL:URL fromReferrer:[self referrer] hideReferrer:&hideReferrer])
-        return nil;
+    [self canLoadURL:URL fromReferrer:[self referrer] hideReferrer:&hideReferrer];
 
     NSMutableURLRequest *newRequest = [[NSMutableURLRequest alloc] initWithURL:URL];
 
