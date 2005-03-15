84cf0739290217307b71bf8a4da20fad083a3462
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-15 01:45:23 +0000

    Reviewed by Harrison.
            - fixed <rdar://problem/4049776> Seed: Mail: Disable spellcheck leaves red artifacts
    
            * WebView.subproj/WebFrameInternal.h: Added _unmarkAllMisspellings.
            * WebView.subproj/WebFrame.m: (-[WebFrame _unmarkAllMisspellings]): Added.
            Calls unmarkAllMisspellings on the bridge and self and all subframes.
    
            * WebView.subproj/WebView.m: (-[WebView setContinuousSpellCheckingEnabled:]):
            Call _unmarkAllMisspellings on the main frame when turning continuous spell checking off.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8889 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
@@ -2588,6 +2588,12 @@ static CFAbsoluteTime _timeOfLastCompletedLoad;
     [request release];
 }
 
+- (void)_unmarkAllMisspellings
+{
+    [[self _bridge] unmarkAllMisspellings];
+    [_private->children makeObjectsPerformSelector:@selector(_unmarkAllMisspellings)];
+}
+
 @end
 
 @implementation WebFormState : NSObject
--- third_party/WebKit/WebKit/WebView.subproj/WebFrameInternal.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrameInternal.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)_setInternalLoadDelegate:(id)internalLoadDelegate;
 - (id)_internalLoadDelegate;
 - (void)_sendResourceLoadDelegateMessagesForURL:(NSURL *)URL response:(NSURLResponse *)response length:(unsigned)length;
+- (void)_unmarkAllMisspellings;
 
 @end
 
