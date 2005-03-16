e480629e75d30695eb36a02739058b9ddd4bb1a1
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-16 03:19:17 +0000

    Reviewed by John.
    	<rdar://problem/4053266> Pressing return a few times right after a link makes the new blank lines part of the link
    
            * khtml/editing/htmlediting.cpp:
    	(khtml::InsertLineBreakCommand::doApply): Use
    	positionOutsideContainingSpecialElement in the right two places.
            (khtml::InsertParagraphSeparatorCommand::doApply): Ditto.
    
    	- move all these helper functions higher in the file
    
            * khtml/editing/htmlediting.cpp:
            (khtml::isSpecialElement):
            (khtml::isFirstVisiblePositionInSpecialElement):
            (khtml::positionBeforeNode):
            (khtml::positionBeforeContainingSpecialElement):
            (khtml::maxRangeOffset):
            (khtml::isLastVisiblePositionInSpecialElement):
            (khtml::positionAfterNode):
            (khtml::positionAfterContainingSpecialElement):
            (khtml::positionOutsideContainingSpecialElement):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
@@ -1230,6 +1230,120 @@ void CompositeEditCommand::moveParagraphContentsToNewBlockIfNecessary(const Posi
     }
 }
 
+static bool isSpecialElement(NodeImpl *n)
+{
+    if (!n->isHTMLElement())
+        return false;
+
+    if (n->id() == ID_A && n->hasAnchor())
+        return true;
+
+    return false;
+}
+
+static bool isFirstVisiblePositionInSpecialElement(const Position& pos)
+{
+    VisiblePosition vPos = VisiblePosition(pos, DOWNSTREAM);
+
+    for (NodeImpl *n = pos.node(); n; n = n->parentNode()) {
+        if (VisiblePosition(n, 0, DOWNSTREAM) != vPos)
+            return false;
+        if (n->rootEditableElement() == NULL)
+            return false;
+        if (isSpecialElement(n))
+            return true;
+    }
+
+    return false;
+}
+
+static Position positionBeforeNode(NodeImpl *node)
+{
+    return Position(node->parentNode(), node->nodeIndex());
+}
+
+static Position positionBeforeContainingSpecialElement(const Position& pos)
+{
+    ASSERT(isFirstVisiblePositionInSpecialElement(pos));
+
+    VisiblePosition vPos = VisiblePosition(pos, DOWNSTREAM);
+    
+    NodeImpl *outermostSpecialElement = NULL;
+
+    for (NodeImpl *n = pos.node(); n; n = n->parentNode()) {
+        if (VisiblePosition(n, 0, DOWNSTREAM) != vPos)
+            break;
+        if (n->rootEditableElement() == NULL)
+            break;
+        if (isSpecialElement(n))
+            outermostSpecialElement = n;
+    }
+    
+    ASSERT(outermostSpecialElement);
+
+    return positionBeforeNode(outermostSpecialElement);
+}
+
+static bool isLastVisiblePositionInSpecialElement(const Position& pos)
+{
+    // make sure to get a range-compliant version of the position
+    Position rangePos = VisiblePosition(pos, DOWNSTREAM).position();
+
+    VisiblePosition vPos = VisiblePosition(rangePos, DOWNSTREAM);
+
+    for (NodeImpl *n = rangePos.node(); n; n = n->parentNode()) {
+        if (VisiblePosition(n, maxRangeOffset(n), DOWNSTREAM) != vPos)
+            return false;
+        if (n->rootEditableElement() == NULL)
+            return false;
+        if (isSpecialElement(n))
+            return true;
+    }
+
+    return false;
+}
+
+static Position positionAfterNode(NodeImpl *node)
+{
+    return Position(node->parentNode(), node->nodeIndex() + 1);
+}
+
+static Position positionAfterContainingSpecialElement(const Position& pos)
+{
+    ASSERT(isLastVisiblePositionInSpecialElement(pos));
+
+    // make sure to get a range-compliant version of the position
+    Position rangePos = VisiblePosition(pos, DOWNSTREAM).position();
+
+    VisiblePosition vPos = VisiblePosition(rangePos, DOWNSTREAM);
+
+    NodeImpl *outermostSpecialElement = NULL;
+
+    for (NodeImpl *n = rangePos.node(); n; n = n->parentNode()) {
+        if (VisiblePosition(n, maxRangeOffset(n), DOWNSTREAM) != vPos)
+            break;
+        if (n->rootEditableElement() == NULL)
+            break;
+        if (isSpecialElement(n))
+            outermostSpecialElement = n;
+    }
+    
+    ASSERT(outermostSpecialElement);
+
+    return positionAfterNode(outermostSpecialElement);
+}
+
+static Position positionOutsideContainingSpecialElement(const Position &pos)
+{
+    if (isFirstVisiblePositionInSpecialElement(pos)) {
+        return positionBeforeContainingSpecialElement(pos);
+    } else if (isLastVisiblePositionInSpecialElement(pos)) {
+        return positionAfterContainingSpecialElement(pos);
+    }
+
+    return pos;
+}
+
 //==========================================================================================
 // Concrete commands
 //------------------------------------------------------------------------------------------
@@ -3029,6 +3143,9 @@ void InsertLineBreakCommand::doApply()
     NodeImpl *nodeToInsert = breakNode;
     
     Position pos(selection.start().upstream(StayInBlock));
+
+    pos = positionOutsideContainingSpecialElement(pos);
+
     bool atStart = pos.offset() <= pos.node()->caretMinOffset();
     bool atEnd = pos.offset() >= pos.node()->caretMaxOffset();
     bool atEndOfBlock = isLastVisiblePositionInBlock(VisiblePosition(pos, selection.startAffinity()));
@@ -3241,6 +3358,8 @@ void InsertParagraphSeparatorCommand::doApply()
         affinity = endingSelection().startAffinity();
     }
 
+    pos = positionOutsideContainingSpecialElement(pos);
+
     calculateStyleBeforeInsertion(pos);
 
     // Find the start block.
@@ -3283,6 +3402,7 @@ void InsertParagraphSeparatorCommand::doApply()
     if (upstreamInDifferentBlock || isFirstInBlock) {
         LOG(Editing, "insert paragraph separator: first in block case");
         pos = pos.downstream(StayInBlock);
+        pos = positionOutsideContainingSpecialElement(pos);
         NodeImpl *refNode = isFirstInBlock && !startBlockIsRoot ? startBlock : pos.node();
         insertNodeBefore(blockToInsert, refNode);
         appendBlockPlaceholder(blockToInsert);
@@ -3578,109 +3698,6 @@ void InsertTextCommand::doApply()
 {
 }
 
-static bool isSpecialElement(NodeImpl *n)
-{
-    if (!n->isHTMLElement())
-        return false;
-
-    if (n->id() == ID_A && n->hasAnchor())
-        return true;
-
-    return false;
-}
-
-static bool isFirstVisiblePositionInSpecialElement(const Position& pos)
-{
-    VisiblePosition vPos = VisiblePosition(pos, DOWNSTREAM);
-
-    for (NodeImpl *n = pos.node(); n; n = n->parentNode()) {
-        if (VisiblePosition(n, 0, DOWNSTREAM) != vPos)
-            return false;
-        if (n->rootEditableElement() == NULL)
-            return false;
-        if (isSpecialElement(n))
-            return true;
-    }
-
-    return false;
-}
-
-Position positionBeforeNode(NodeImpl *node)
-{
-    return Position(node->parentNode(), node->nodeIndex());
-}
-
-Position positionBeforeContainingSpecialElement(const Position& pos)
-{
-    ASSERT(isFirstVisiblePositionInSpecialElement(pos));
-
-    VisiblePosition vPos = VisiblePosition(pos, DOWNSTREAM);
-    
-    NodeImpl *outermostSpecialElement = NULL;
-
-    for (NodeImpl *n = pos.node(); n; n = n->parentNode()) {
-        if (VisiblePosition(n, 0, DOWNSTREAM) != vPos)
-            break;
-        if (n->rootEditableElement() == NULL)
-            break;
-        if (isSpecialElement(n))
-            outermostSpecialElement = n;
-    }
-    
-    ASSERT(outermostSpecialElement);
-
-    return positionBeforeNode(outermostSpecialElement);
-}
-
-static bool isLastVisiblePositionInSpecialElement(const Position& pos)
-{
-    // make sure to get a range-compliant version of the position
-    Position rangePos = VisiblePosition(pos, DOWNSTREAM).position();
-
-    VisiblePosition vPos = VisiblePosition(rangePos, DOWNSTREAM);
-
-    for (NodeImpl *n = rangePos.node(); n; n = n->parentNode()) {
-        if (VisiblePosition(n, maxRangeOffset(n), DOWNSTREAM) != vPos)
-            return false;
-        if (n->rootEditableElement() == NULL)
-            return false;
-        if (isSpecialElement(n))
-            return true;
-    }
-
-    return false;
-}
-
-Position positionAfterNode(NodeImpl *node)
-{
-    return Position(node->parentNode(), node->nodeIndex() + 1);
-}
-
-Position positionAfterContainingSpecialElement(const Position& pos)
-{
-    ASSERT(isLastVisiblePositionInSpecialElement(pos));
-
-    // make sure to get a range-compliant version of the position
-    Position rangePos = VisiblePosition(pos, DOWNSTREAM).position();
-
-    VisiblePosition vPos = VisiblePosition(rangePos, DOWNSTREAM);
-
-    NodeImpl *outermostSpecialElement = NULL;
-
-    for (NodeImpl *n = rangePos.node(); n; n = n->parentNode()) {
-        if (VisiblePosition(n, maxRangeOffset(n), DOWNSTREAM) != vPos)
-            break;
-        if (n->rootEditableElement() == NULL)
-            break;
-        if (isSpecialElement(n))
-            outermostSpecialElement = n;
-    }
-    
-    ASSERT(outermostSpecialElement);
-
-    return positionAfterNode(outermostSpecialElement);
-}
-
 Position InsertTextCommand::prepareForTextInsertion(bool adjustDownstream)
 {
     // Prepare for text input by looking at the current position.
@@ -3696,11 +3713,7 @@ Position InsertTextCommand::prepareForTextInsertion(bool adjustDownstream)
     
     Selection typingStyleRange;
 
-    if (isFirstVisiblePositionInSpecialElement(pos)) {
-        pos = positionBeforeContainingSpecialElement(pos);
-    } else if (isLastVisiblePositionInSpecialElement(pos)) {
-        pos = positionAfterContainingSpecialElement(pos);
-    }
+    pos = positionOutsideContainingSpecialElement(pos);
 
     if (!pos.node()->isTextNode()) {
         NodeImpl *textNode = document()->createEditingTextNode("");
@@ -4624,6 +4637,7 @@ void ReplaceSelectionCommand::doApply()
     // collect information about the current selection, prior to deleting the selection
     Selection selection = endingSelection();
     ASSERT(selection.isCaretOrRange());
+
     VisiblePosition visibleStart(selection.start(), selection.startAffinity());
     VisiblePosition visibleEnd(selection.end(), selection.endAffinity());
     bool startAtStartOfBlock = isFirstVisiblePositionInBlock(visibleStart);
@@ -4701,6 +4715,8 @@ void ReplaceSelectionCommand::doApply()
 
     if (startAtStartOfBlock && startBlock->inDocument())
         startPos = Position(startBlock, 0);
+
+    startPos = positionOutsideContainingSpecialElement(startPos);
 
     KHTMLPart *part = document()->part();
     if (m_matchStyle) {
