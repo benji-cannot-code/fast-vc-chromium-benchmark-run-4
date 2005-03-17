3220d2cc3513214e9c93fb981793ac399726ea41
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-03-17 02:09:02 +0000

    Reviewed by Maciej.
            <rdar://problem/4054590> AX: Dictionary panel does not work when page is scrolled on Safari
    
            * kwq/KWQAccObject.mm:
            (-[KWQAccObject doAXTextMarkerForPosition:]):
            Add in the view's contentsX and contentsY to the point.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@8907 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQAccObject.mm
+++ third_party/WebKit/WebCore/kwq/KWQAccObject.mm
@@ -1095,7 +1095,8 @@ static QRect boundingBoxRect(RenderObject* obj)
 - (id)doAXTextMarkerForPosition: (NSPoint) point
 {
     // convert absolute point to view coordinates
-    NSView *view = [self topView]->getView();
+    KHTMLView *docView = [self topView];
+    NSView *view = docView->getView();
     RenderObject *renderer = [self topRenderer];
     NodeImpl *innerNode = NULL;
     NSPoint ourpoint;
@@ -1105,6 +1106,9 @@ static QRect boundingBoxRect(RenderObject* obj)
         // ask the document layer to hitTest
         NSPoint windowCoord = [[view window] convertScreenToBase: point];
         ourpoint = [view convertPoint:windowCoord fromView:nil];
+        ourpoint.x += docView->contentsX();
+        ourpoint.y += docView->contentsY();
+        
         RenderObject::NodeInfo nodeInfo(true, true);
         renderer->layer()->hitTest(nodeInfo, (int)ourpoint.x, (int)ourpoint.y);
         innerNode = nodeInfo.innerNode();
@@ -1127,7 +1131,8 @@ static QRect boundingBoxRect(RenderObject* obj)
         if (!document)
             break;
         renderer = document->renderer();
-        view = static_cast<KHTMLView *>(widget)->getDocumentView();
+        docView = static_cast<KHTMLView *>(widget);
+        view = docView->getDocumentView();
     }
     
     // get position within the node
