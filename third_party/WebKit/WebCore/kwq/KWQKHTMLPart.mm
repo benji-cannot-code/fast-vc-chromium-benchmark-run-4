de4812dc5154eb48bcc35b02bd41cb6952608c94
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-04-12 15:56:17 +0000

    Left out an #import in previous checkin.
    git-svn-id: svn://svn.chromium.org/blink/trunk@9001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQKHTMLPart.mm
+++ third_party/WebKit/WebCore/kwq/KWQKHTMLPart.mm
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "KWQFoundationExtras.h"
 #import "KWQKJobClasses.h"
 #import "KWQLogging.h"
+#import "KWQNSViewExtras.h"
 #import "KWQPageState.h"
 #import "KWQPrinter.h"
 #import "KWQRegExp.h"
