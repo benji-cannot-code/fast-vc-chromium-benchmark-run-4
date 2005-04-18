a9f50c10d16e2c5b66c418b3710f0fe2f745e585
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-04-18 20:57:04 +0000

    Fix min-height so that when it resolves to auto it does not use the box's intrinsic height.
            * khtml/rendering/render_box.cpp:
            (RenderBox::calcHeight):
            (RenderBox::calcHeightUsing):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_box.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_box.cpp
@@ -989,8 +989,12 @@ void RenderBox::calcHeight()
         int height;
         if (checkMinMaxHeight) {
             height = calcHeightUsing(style()->height());
-            int minH = calcHeightUsing(style()->minHeight());
+            if (height == -1)
+                height = m_height;
+            int minH = calcHeightUsing(style()->minHeight()); // Leave as -1 if unset.
             int maxH = style()->maxHeight().value == UNDEFINED ? height : calcHeightUsing(style()->maxHeight());
+            if (maxH == -1)
+                maxH = height;
             height = kMin(maxH, height);
             height = kMax(minH, height);
         }
@@ -1027,8 +1031,8 @@ void RenderBox::calcHeight()
 
 int RenderBox::calcHeightUsing(const Length& h)
 {
+    int height = -1;
     if (!h.isVariable()) {
-        int height = -1;
         if (h.isFixed())
             height = h.value;
         else if (h.isPercent())
@@ -1038,7 +1042,7 @@ int RenderBox::calcHeightUsing(const Length& h)
             return height;
         }
     }
-    return m_height;
+    return height;
 }
 
 int RenderBox::calcPercentageHeight(const Length& height)
