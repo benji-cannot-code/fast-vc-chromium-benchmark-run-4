bd9254423b4ff8dd9d3651aa1d190bbf7b4b8d6b
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-04-26 22:39:33 +0000

    Reviewed by Darin, Maciej.
            <rdar://problem/4075576> Deleting text in new message borks content
    
            Fixed by removing the methods that attempted to preserve the position.  The idea
            of preserving position is a very recent one, and turned out to not actually address
            the problem it was intended to fix (see below).  Further, is unclear how the position
            could be preserved in a form that could be properly used later on.   Therefore,
            removing the code to work like before is the preferred alternative for this software
            update.  I've written <rdar://problem/4099839> to cover the bug that position
            preservation was supposed to fix (but did not).  Added layout tests for
            both this bug and 4099839.  Also, updated existing tests with correct expected results
            (delete-at-paragraph-boundaries-003 and 004).
    
            * khtml/editing/htmlediting.cpp:
            (khtml::CompositeEditCommand::removeFullySelectedNode):
            (khtml::CompositeEditCommand::removeChildrenInRange):
            (khtml::DeleteSelectionCommand::handleGeneralDelete):
            * khtml/editing/htmlediting.h:
            * layout-tests/editing/deleting/delete-at-paragraph-boundaries-003-expected.txt:
            * layout-tests/editing/deleting/delete-at-paragraph-boundaries-004-expected.txt:
            * layout-tests/editing/deleting/delete-at-paragraph-boundaries-007-expected.txt: Added.
            * layout-tests/editing/deleting/delete-at-paragraph-boundaries-007.html: Added.
            * layout-tests/editing/deleting/delete-at-paragraph-boundaries-008-expected.txt: Added.
            * layout-tests/editing/deleting/delete-at-paragraph-boundaries-008.html: Added.
            * layout-tests/editing/deleting/delete-at-paragraph-boundaries-009-expected.txt: Added.
            * layout-tests/editing/deleting/delete-at-paragraph-boundaries-009.html: Added.
            * layout-tests/editing/deleting/delete-at-paragraph-boundaries-010-expected.txt: Added.
            * layout-tests/editing/deleting/delete-at-paragraph-boundaries-010.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9054 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.cpp
@@ -854,7 +854,7 @@ void CompositeEditCommand::appendNode(NodeImpl *appendChild, NodeImpl *parent)
     applyCommandToComposite(cmd);
 }
 
-void CompositeEditCommand::removeFullySelectedNodePreservingPosition(NodeImpl *node, Position &pos)
+void CompositeEditCommand::removeFullySelectedNode(NodeImpl *node)
 {
     if (isTableStructureNode(node) || node == node->rootEditableElement()) {
         // Do not remove an element of table structure; remove its contents.
@@ -863,37 +863,24 @@ void CompositeEditCommand::removeFullySelectedNodePreservingPosition(NodeImpl *n
         while (child) {
             NodeImpl *remove = child;
             child = child->nextSibling();
-            removeFullySelectedNodePreservingPosition(remove, pos);
+            removeFullySelectedNode(remove);
         }
     }
     else {
-        removeNodePreservingPosition(node, pos);
+        removeNode(node);
     }
 }
 
-void CompositeEditCommand::removeChildrenInRangePreservingPosition(NodeImpl *node, int from, int to, Position &pos)
+void CompositeEditCommand::removeChildrenInRange(NodeImpl *node, int from, int to)
 {
     NodeImpl *nodeToRemove = node->childNode(from);
     for (int i = from; i < to; i++) {
         ASSERT(nodeToRemove);
         NodeImpl *next = nodeToRemove->nextSibling();
-        removeNodePreservingPosition(nodeToRemove, pos);
+        removeNode(nodeToRemove);
         nodeToRemove = next;
     }
 }
-
-void CompositeEditCommand::removeNodePreservingPosition(NodeImpl *removeChild, Position &pos)
-{
-    if (removeChild == pos.node() || pos.node()->isAncestor(removeChild)) {
-        pos = Position(removeChild->parentNode(), removeChild->nodeIndex());
-    } else if (removeChild->parentNode() == pos.node() && removeChild->nodeIndex() < (unsigned)pos.offset()) {
-        pos = Position(pos.node(), pos.offset() - 1);
-    }
-
-    EditCommandPtr cmd(new RemoveNodeCommand(document(), removeChild));
-    applyCommandToComposite(cmd);
-}
-
 
 void CompositeEditCommand::removeNode(NodeImpl *removeChild)
 {
@@ -2837,7 +2824,7 @@ void DeleteSelectionCommand::handleGeneralDelete()
         if (!m_startNode->renderer() || 
             (startOffset == 0 && m_downstreamEnd.offset() >= maxDeepOffset(m_startNode))) {
             // just delete
-            removeFullySelectedNodePreservingPosition(m_startNode, m_upstreamStart);
+            removeFullySelectedNode(m_startNode);
         } else if (m_downstreamEnd.offset() - startOffset > 0) {
             if (m_startNode->isTextNode()) {
                 // in a text node that needs to be trimmed
@@ -2845,7 +2832,7 @@ void DeleteSelectionCommand::handleGeneralDelete()
                 deleteTextFromNode(text, startOffset, m_downstreamEnd.offset() - startOffset);
                 m_trailingWhitespaceValid = false;
             } else {
-                removeChildrenInRangePreservingPosition(m_startNode, startOffset, m_downstreamEnd.offset(), m_upstreamStart);
+                removeChildrenInRange(m_startNode, startOffset, m_downstreamEnd.offset());
                 m_endingPosition = m_upstreamStart;
             }
         }
@@ -2878,14 +2865,14 @@ void DeleteSelectionCommand::handleGeneralDelete()
                     ASSERT(node->nodeIndex() < (unsigned)m_downstreamEnd.offset());
                     m_downstreamEnd = Position(m_downstreamEnd.node(), m_downstreamEnd.offset() - 1);
                 }
-                removeFullySelectedNodePreservingPosition(node, m_upstreamStart);
+                removeFullySelectedNode(node);
                 node = nextNode;
             } else {
                 NodeImpl *n = node->lastChild();
                 while (n && n->lastChild())
                     n = n->lastChild();
                 if (n == m_downstreamEnd.node() && m_downstreamEnd.offset() >= m_downstreamEnd.node()->caretMaxOffset()) {
-                    removeFullySelectedNodePreservingPosition(node, m_upstreamStart);
+                    removeFullySelectedNode(node);
                     m_trailingWhitespaceValid = false;
                     node = 0;
                 } 
@@ -2905,7 +2892,7 @@ void DeleteSelectionCommand::handleGeneralDelete()
                     m_upstreamStart = Position(m_downstreamEnd.node()->parentNode(), m_downstreamEnd.node()->nodeIndex());
                 }
                 
-                removeFullySelectedNodePreservingPosition(m_downstreamEnd.node(), m_upstreamStart);
+                removeFullySelectedNode(m_downstreamEnd.node());
                 m_trailingWhitespaceValid = false;
             } else {
                 if (m_downstreamEnd.node()->isTextNode()) {
@@ -2925,7 +2912,7 @@ void DeleteSelectionCommand::handleGeneralDelete()
                         if (n)
                             offset = n->nodeIndex() + 1;
                     }
-                    removeChildrenInRangePreservingPosition(m_downstreamEnd.node(), offset, m_downstreamEnd.offset(), m_upstreamStart);
+                    removeChildrenInRange(m_downstreamEnd.node(), offset, m_downstreamEnd.offset());
                     m_downstreamEnd = Position(m_downstreamEnd.node(), offset);
                 }
             }
