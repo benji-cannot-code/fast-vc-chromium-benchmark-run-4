23bddb630ca4526b24169083bcf8b4b94726c900
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-05-03 20:55:32 +0000

    Reviewed by Dave Hyatt.         No new layout tests needed.
            - eliminated the bogus kMin/kMax macros that we had in addition to inline functions
              of the same name
    
            * kwq/KWQKGlobal.h: Remove the kMin/kMax macros.
    
            * khtml/css/cssstyleselector.cpp:
            (khtml::CSSStyleSelector::applyProperty): Change type of constant so both sides
            of kMin calls match.
            (khtml::CSSStyleSelector::fontSizeForKeyword): Ditto.
            * khtml/html/htmltokenizer.cpp: (khtml::HTMLTokenizer::parseEntity): Ditto.
    
            - remove unused parameter to dirtyLinesFromChangedChild for clarity
    
            * khtml/rendering/render_object.h: Don't take the parameter.
            * khtml/rendering/render_object.cpp: (RenderObject::dirtyLinesFromChangedChild): Ditto.
    
            * khtml/rendering/render_flow.h: Don't take the parameter.
            * khtml/rendering/render_flow.cpp:
            (RenderFlow::detach): Don't pass the parameter.
            (RenderFlow::dirtyLinesFromChangedChild): Don't take the parameter.
    
            * khtml/rendering/render_text.cpp: (RenderText::detach): Don't pass the parameter.
    
            - convert DOM::NodeImpl into an abstract base class by making a couple of functions
              pure virtual for clarity
    
            * khtml/xml/dom_nodeimpl.h: Made nodeName and nodeType pure virtual.
            * khtml/xml/dom_nodeimpl.cpp: Remove bodies of nodeName and nodeType.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9096 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_flow.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_flow.cpp
@@ -212,7 +212,7 @@ void RenderFlow::detach()
             }
         }
         else if (isInline() && parent())
-            parent()->dirtyLinesFromChangedChild(this, false);
+            parent()->dirtyLinesFromChangedChild(this);
     }
 
     deleteLineBoxes();
@@ -220,7 +220,7 @@ void RenderFlow::detach()
     RenderContainer::detach();
 }
 
-void RenderFlow::dirtyLinesFromChangedChild(RenderObject* child, bool adding)
+void RenderFlow::dirtyLinesFromChangedChild(RenderObject* child)
 {
     if (!parent() || selfNeedsLayout() || isTable())
         return;
--- third_party/WebKit/WebCore/khtml/rendering/render_text.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_text.cpp
@@ -839,7 +839,7 @@ void RenderText::detach()
                 box->remove();
         }
         else if (parent())
-            parent()->dirtyLinesFromChangedChild(this, false);
+            parent()->dirtyLinesFromChangedChild(this);
     }
     deleteTextBoxes();
     RenderObject::detach();
