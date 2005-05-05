e423e9c318dc2c413703dca482493bee727a0af1
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-05-05 17:55:06 +0000

    Reviewed by Dave Hyatt.
            - fixed <rdar://problem/4109564> REGRESSION (Atlanta): maps.google.com doesn't always center California correctly
    
            * khtml/ecma/kjs_events.cpp: (offsetFromTarget): Fix two places that said X where they should say Y.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9119 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_events.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_events.cpp
@@ -699,7 +699,7 @@ Value DOMMouseEvent::tryGet(ExecState *exec, const Identifier &p) const
 static QPoint offsetFromTarget(const MouseRelatedEventImpl *e)
 {
     int x = e->clientX();
-    int y = e->clientX();
+    int y = e->clientY();
 
     NodeImpl *n = e->target();
     if (n) {
@@ -711,7 +711,7 @@ static QPoint offsetFromTarget(const MouseRelatedEventImpl *e)
                 int rx, ry;
                 if (r->absolutePosition(rx, ry)) {
                     x -= rx;
-                    x -= ry;
+                    y -= ry;
                 }
             }
         }
