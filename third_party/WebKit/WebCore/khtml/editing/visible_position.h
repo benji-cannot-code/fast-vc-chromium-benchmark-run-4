2d4d4eb3f87684ded482d2060ea90722c85124a7
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-05-09 19:04:29 +0000

    Reviewed by Dave Harrison.
    	- remove remaining uses of upstream/downstream DoNotStayInBlock
    
            * khtml/editing/htmlediting.cpp:
            (khtml::CompositeEditCommand::moveParagraphContentsToNewBlockIfNecessary):
            (khtml::ApplyStyleCommand::nodeFullySelected):
            (khtml::ApplyStyleCommand::nodeFullyUnselected):
            (khtml::DeleteSelectionCommand::insertPlaceholderForAncestorBlockContent):
            (khtml::InsertParagraphSeparatorCommand::doApply):
            (khtml::InsertParagraphSeparatorInQuotedContentCommand::doApply):
            (khtml::InsertTextCommand::insertSpace):
            (khtml::ReplaceSelectionCommand::doApply):
    
            * khtml/editing/visible_position.cpp:
            (khtml::enclosingBlockFlowElement): New helper function.
    	* khtml/editing/visible_position.h:
    
            * khtml/editing/visible_units.cpp:
            (khtml::inSameBlock): Check enclosing block flows instead of comparing
    	visible block starts. Two nested blocks may have the same visible start but
    	different visible ends, so the old check would give false positives.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9142 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/visible_position.h
+++ third_party/WebKit/WebCore/khtml/editing/visible_position.h
@@ -132,6 +132,8 @@ VisiblePosition endVisiblePosition(const DOM::RangeImpl *, EAffinity);
 
 void setAffinityUsingLinePosition(VisiblePosition &);
 
+DOM::NodeImpl *enclosingBlockFlowElement(const VisiblePosition &);
+
 bool visiblePositionsOnDifferentLines(const VisiblePosition &, const VisiblePosition &);
 bool visiblePositionsInDifferentBlocks(const VisiblePosition &, const VisiblePosition &);
 bool isFirstVisiblePositionOnLine(const VisiblePosition &);
