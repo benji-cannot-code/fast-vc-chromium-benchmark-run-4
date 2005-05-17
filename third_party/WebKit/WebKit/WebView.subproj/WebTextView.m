656e16a5328fcd864f26efa6b45278768697d04b
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-05-17 17:55:47 +0000

    Fixed: <rdar://problem/4119282> clicking a link in an RTF file opens the link with NSWorkspace without the usual security checks or WebView delegate control
            Reviewed by mjs.
    
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge loadURL:referrer:reload:userGesture:target:triggeringEvent:form:formValues:]): pass the passed referrer to canLoadURL::: not [self referrer]
            (-[WebBridge postWithURL:referrer:target:data:contentType:triggeringEvent:form:formValues:]): ditto
            * WebView.subproj/WebTextView.m:
            (-[WebTextView clickedOnLink:atIndex:]): call the loadURL bridge method so that security checks are made, command/option clicks work, policy delegate is consulted etc.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9177 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebTextView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebTextView.m
@@ -7,16 +7,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebTextView.h>
 
 #import <WebKit/WebAssertions.h>
-#import <Foundation/NSURLResponse.h>
-
+#import <WebKit/WebBridge.h>
 #import <WebKit/WebDataSourcePrivate.h>
 #import <WebKit/WebDocumentInternal.h>
+#import <WebKit/WebFramePrivate.h>
 #import <WebKit/WebFrameView.h>
 #import <WebKit/WebNSObjectExtras.h>
+#import <WebKit/WebNSURLExtras.h>
 #import <WebKit/WebNSViewExtras.h>
 #import <WebKit/WebPreferences.h>
 #import <WebKit/WebTextRendererFactory.h>
 #import <WebKit/WebViewPrivate.h>
+
+#import <Foundation/NSURLResponse.h>
+
+@interface NSTextView (AppKitSecret)
++ (NSURL *)_URLForString:(NSString *)string;
+@end
 
 @interface WebTextView (ForwardDeclarations)
 - (void)_updateTextSizeMultiplier;
@@ -326,6 +333,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         [self setSelectedRange:NSMakeRange(0,0)];
     }
     return resign;
+}
+
+- (void)clickedOnLink:(id)link atIndex:(unsigned)charIndex
+{
+    NSURL *URL = nil;
+    if ([link isKindOfClass:[NSURL class]]) {
+        URL = (NSURL *)link;
+    } else if ([link isKindOfClass:[NSString class]]) {
+        URL = [[self class] _URLForString:(NSString *)link];
+    }
+    if (URL != nil) {    
+        // Call the bridge because this is where our security checks are made.
+        WebFrame *frame = [[self _web_parentWebFrameView] webFrame];
+        [[frame _bridge] loadURL:URL 
+                        referrer:[[[[frame dataSource] request] URL] _web_originalDataAsString]
+                          reload:NO
+                     userGesture:YES       
+                          target:nil
+                 triggeringEvent:[[self window] currentEvent]
+                            form:nil 
+                      formValues:nil];
+    }
 }
 
 #pragma mark PRINTING
