001550ad2d6ff970704dd4c4322a069e7472023c
cblu <cblu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-05-20 20:50:20 +0000

    Fixed: <rdar://problem/4098786> sync. XMLHttpRequest works w/o AllowNetworkAccess key because load delegate is not consulted
    	Synchronous loads did not cause the willSendRequest method on the resource load delegate to be called. This is the method that Dashboard uses to enforce AllowNetworkAccess and this must be called to avoid exploits.
    
            Reviewed by sullivan.
    
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge objectLoadedFromCacheWithURL:response:data:]): call [WebFrame _requestFromDelegateForRequest:identifier:error:] then
    	[WebFrame _saveResourceAndSendRemainingDelegateMessagesWithRequest:identifier:response:data:error:] so synthetic resource load delegate
    	methods are called and the data is saved as a WebResource for resources in the WebCore cache.
    
            (-[WebBridge syncLoadResourceWithURL:customHeaders:postData:finalURL:responseHeaders:statusCode:]): call [WebFrame _requestFromDelegateForRequest:identifier:error:],
    	respect its result, do the load and then call [WebFrame _saveResourceAndSendRemainingDelegateMessagesWithRequest:identifier:response:data:error:]
    	for synchronous loads
    
            * WebView.subproj/WebFrame.m:
            (-[WebFrame _opened]): call [WebFrame _requestFromDelegateForRequest:identifier:error:] then
    	[WebFrame _sendRemainingDelegateMessagesWithIdentifier:response:length:error:] so synthetic resource load delegate methods are called
    	for subresrources in the page cache
    
    	(-[WebFrame _requestFromDelegateForRequest:identifier:error:]): new, was part of the removed _sendResourceLoadDelegateMessagesForURL:::
    	This method calls identifierForInitialRequest and willSendRequest.
    
            (-[WebFrame _sendRemainingDelegateMessagesWithIdentifier:response:length:error:]): new, was part of the removed _sendResourceLoadDelegateMessagesForURL:::
    	This method calls the remaining resource load delegate messages.
    
            (-[WebFrame _saveResourceAndSendRemainingDelegateMessagesWithRequest:identifier:response:data:error:]): new, saves the resource and calls
    	[WebFrame _sendRemainingDelegateMessagesWithIdentifier:response:length:error:]
    
            * WebView.subproj/WebFrameInternal.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9183 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrameInternal.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrameInternal.h
@@ -8,9 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)_updateDrawsBackground;
 - (void)_setInternalLoadDelegate:(id)internalLoadDelegate;
 - (id)_internalLoadDelegate;
-- (void)_sendResourceLoadDelegateMessagesForURL:(NSURL *)URL response:(NSURLResponse *)response length:(unsigned)length;
 - (void)_unmarkAllMisspellings;
 
+- (NSURLRequest *)_requestFromDelegateForRequest:(NSURLRequest *)request identifier:(NSString **)identifier error:(NSError **)error;
+- (void)_sendRemainingDelegateMessagesWithIdentifier:(NSString *)identifier response:(NSURLResponse *)response length:(unsigned)length error:(NSError *)error;
+- (void)_saveResourceAndSendRemainingDelegateMessagesWithRequest:(NSURLRequest *)request
+                                                      identifier:(NSString *)identifier 
+                                                        response:(NSURLResponse *)response 
+                                                            data:(NSData *)data
+                                                           error:(NSError *)error;
 @end
 
 @interface NSObject (WebInternalFrameLoadDelegate)
