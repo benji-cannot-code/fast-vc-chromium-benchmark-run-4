6546b37a8376bf60dd2cb5f562664e042673c369
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-05-24 17:44:19 +0000

    Reviewed by John.
    	- fixed <rdar://problem/3598589> onunload handler doesn't fire when a window is closed
    
            Test cases added:
            * layout-tests/fast/events/onunload-expected.txt: Added.
            * layout-tests/fast/events/onunload.html: Added.
    
            * khtml/xml/dom_docimpl.cpp:
            (DocumentImpl::detach): Don't remove all event handlers from the document here...
            * khtml/khtml_part.cpp:
            (KHTMLPart::closeURL): ...instead do it here, after firing the unload event, but only
    	if the document is not in the back/forward cache...
            * kwq/KWQPageState.mm:
            (-[KWQPageState finalize]): ...and take care of the back/forward cache case here.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9200 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQPageState.mm
+++ third_party/WebKit/WebCore/kwq/KWQPageState.mm
@@ -135,6 +135,7 @@ using KJS::SavedBuiltins;
         document->setInPageCache(NO);
         if (detached) {
             document->detach();
+            document->removeAllEventListenersFromAllNodes();
         }
         document->deref();
         
@@ -170,6 +171,7 @@ using KJS::SavedBuiltins;
         document->setInPageCache(NO);
         if (detached) {
             document->detach();
+            document->removeAllEventListenersFromAllNodes();
         }
         document->deref();
         
