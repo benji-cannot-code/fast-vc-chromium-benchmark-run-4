d501d896e4662171a7f271caae58801252d6c10a
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-05-26 22:48:36 +0000

    <rdar://problem/4120518> Mail: control-T in an empty message crashes mail
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge issueTransposeCommand]):
            New, to support transpose in JS.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9220 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -1419,6 +1419,14 @@ static id <WebFormDelegate> formDelegate(WebBridge *self)
     [[_frame webView] pasteAsPlainText:nil];
 }
 
+- (void)issueTransposeCommand
+{
+    NSView <WebDocumentView> *view = [[_frame frameView] documentView];
+    if ([view isKindOfClass:[WebHTMLView class]]) {
+        [(WebHTMLView *)view transpose:nil];
+    }
+}
+
 - (BOOL)canPaste
 {
     return [[_frame webView] _canPaste];
