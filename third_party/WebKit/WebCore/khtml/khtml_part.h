8c94495ff1f1cf2f6246bfec038f8374f85bb479
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-05-26 20:32:01 +0000

    Missed this in previous commit.
    git-svn-id: svn://svn.chromium.org/blink/trunk@9218 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtml_part.h
+++ third_party/WebKit/WebCore/khtml/khtml_part.h
@@ -636,6 +636,11 @@ public:
   void setDragCaret(const khtml::Selection &);
   
   /**
+   * Transposes characters either side of caret selection.
+   */
+  void transpose();
+  
+  /**
    * Clears the current selection.
    */
   void clearSelection();
