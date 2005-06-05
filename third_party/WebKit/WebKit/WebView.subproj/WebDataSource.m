4e9baf63b467f4949bca42ce5ecb951019ae2d67
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-05 20:57:29 +0000

    Reviewed by Hyatt.
            - fixed build that I broke with the license change (some includes of WebException were still around)
    
            * WebKit.pbproj/project.pbxproj: Removed references to WebException.h/m.
            * WebView.subproj/WebDataSource.m: Removed include of WebException.h.
            * WebView.subproj/WebHTMLView.m: Ditto.
            * WebView.subproj/WebView.m: Ditto.
    
            - fixed build under gcc 4.0 (some code moved here from Foundation had warnings)
    
            * Misc.subproj/WebNSDataExtras.m:
            (-[NSString _web_capitalizeRFC822HeaderFieldName]): Use char instead of UInt8.
            (-[NSData _webkit_guessedMIMEType]):  Use char instead of UInt8, and take out now-
            unneeded type casts.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebDefaultResourceLoadDelegate.h>
 #import <WebKit/WebDocument.h>
 #import <WebKit/WebDOMOperationsPrivate.h>
-#import <WebKit/WebException.h>
 #import <WebKit/WebFrameLoadDelegate.h>
 #import <WebKit/WebFramePrivate.h>
 #import <WebKit/WebFrameView.h>
--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebDocumentInternal.h>
 #import <WebKit/WebDOMOperationsPrivate.h>
 #import <WebKit/WebEditingDelegate.h>
-#import <WebKit/WebException.h>
 #import <WebKit/WebFramePrivate.h>
 #import <WebKit/WebFrameViewInternal.h>
 #import <WebKit/WebHTMLViewInternal.h>
