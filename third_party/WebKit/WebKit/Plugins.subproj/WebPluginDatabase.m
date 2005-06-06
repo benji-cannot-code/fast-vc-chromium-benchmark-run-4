cd039ff162a7c9ce9b18b57a94941c61aeb194b8
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-06 02:39:21 +0000

    - remove another forgotten include
            * Plugins.subproj/WebPluginDatabase.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9289 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebPluginDatabase.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebPluginDatabase.m
@@ -43,8 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebViewPrivate.h>
 #import <WebKitSystemInterface.h>
 
-#import <CoreGraphics/CPSProcesses.h>
-
 @implementation WebPluginDatabase
 
 static WebPluginDatabase *database = nil;
