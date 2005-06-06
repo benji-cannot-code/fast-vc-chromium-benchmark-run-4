541fc3b6e73ce1f20000856a8ddf6c82e353ad9e
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-06 13:26:17 +0000

    - more SPI removal, for font cache and CFStream
            * WebCoreSupport.subproj/WebTextRendererFactory.m:
            (getLCDScaleParameters):
            (+[WebTextRendererFactory createSharedFactory]):
            * WebView.subproj/WebFormDataStream.m:
            (formCanRead):
            (formEventCallback):
            (webSetHTTPBody):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9305 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFormDataStream.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFormDataStream.m
@@ -34,10 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <sys/types.h>
 #import <sys/stat.h>
 
-#import <CoreFoundation/CFStreamAbstract.h>
-
 #import "WebAssertions.h"
 #import "WebNSObjectExtras.h"
+#import <WebKitSystemInterface.h>
 
 #if !BUILDING_ON_PANTHER
 
@@ -228,7 +227,7 @@ static Boolean formCanRead(CFReadStreamRef stream, void *context)
         openNextStream(form);
     }
     if (!form->currentStream) {
-        CFReadStreamSignalEvent(stream, kCFStreamEventEndEncountered, NULL);
+		WKSignalCFReadStreamEnd(stream);
         return FALSE;
     }
     return CFReadStreamHasBytesAvailable(form->currentStream);
@@ -269,17 +268,17 @@ static void formEventCallback(CFReadStreamRef stream, CFStreamEventType type, vo
 
     switch (type) {
     case kCFStreamEventHasBytesAvailable:
-        CFReadStreamSignalEvent(form->formStream, kCFStreamEventHasBytesAvailable, NULL);
+        WKSignalCFReadStreamHasBytes(form->formStream);
         break;
     case kCFStreamEventErrorOccurred: {
         CFStreamError readStreamError = CFReadStreamGetError(stream);
-        CFReadStreamSignalEvent(form->formStream, kCFStreamEventErrorOccurred, &readStreamError);
+        WKSignalCFReadStreamError(form->formStream, &readStreamError);
         break;
     }
     case kCFStreamEventEndEncountered:
         openNextStream(form);
         if (!form->currentStream) {
-            CFReadStreamSignalEvent(form->formStream, kCFStreamEventEndEncountered, NULL);
+            WKSignalCFReadStreamEnd(form->formStream);
         }
         break;
     case kCFStreamEventNone:
@@ -336,12 +335,8 @@ void webSetHTTPBody(NSMutableURLRequest *request, NSArray *formData)
     [request setValue:[NSString stringWithFormat:@"%lld", length] forHTTPHeaderField:@"Content-Length"];
 
     // Create and set the stream.
-    CFReadStreamCallBacks callBacks = { 1, formCreate, formFinalize, NULL,
-        formOpen, NULL, formRead, NULL, formCanRead, formClose,
-        NULL, NULL, NULL, formSchedule, formUnschedule
-    };
-    CFReadStreamRef stream = CFReadStreamCreate(NULL, &callBacks, formData);
-    [request setHTTPBodyStream:(NSInputStream *)stream];
+	CFReadStreamRef stream = WKCreateCustomCFReadStream(formCreate, formFinalize, formOpen, formRead, formCanRead, formClose, formSchedule, formUnschedule, formData);
+	[request setHTTPBodyStream:(NSInputStream *)stream];
     CFRelease(stream);
 
 #else
