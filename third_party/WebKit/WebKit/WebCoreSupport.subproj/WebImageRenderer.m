a41671fe059bca09ed0fc81faf39df4f223133b8
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-06 11:16:11 +0000

    - remove unneeded header include
            * WebCoreSupport.subproj/WebImageRenderer.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9297 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
@@ -39,9 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebCore/WebCoreImageRenderer.h>
 
-#import <CoreGraphics/CGContextPrivate.h>
-
-
 #ifdef USE_CGIMAGEREF
 
 #import <WebKit/WebImageData.h>
