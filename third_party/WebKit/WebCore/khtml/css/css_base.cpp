82d727dcec5d0c5eab568acf9f067054f09926ef
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-11 05:31:17 +0000

    Fix for bugzilla bug 3335, add support for the CSS3 indirect adjacent sibling selector.  Patch merge 	from KHTML tree by Nick Shanks.
            Reviewed by hyatt
    
            Test cases added: fast/selectors/046.html
    
            * khtml/css/css_base.cpp:
            (CSSSelector::selectorText):
            * khtml/css/css_base.h:
            (DOM::CSSSelector::):
            * khtml/css/cssstyleselector.cpp:
            (khtml::CSSStyleSelector::checkSelector):
            * khtml/css/parser.y:
            * layout-tests/fast/selectors/046-expected.txt: Added.
            * layout-tests/fast/selectors/046.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9358 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/css/css_base.cpp
+++ third_party/WebKit/WebCore/khtml/css/css_base.cpp
@@ -304,8 +304,10 @@ DOMString CSSSelector::selectorText() const
     }
     if ( cs->tagHistory ) {
         DOMString tagHistoryText = cs->tagHistory->selectorText();
-        if ( cs->relation == Sibling )
+		if ( cs->relation == DirectAdjacent )
             str = tagHistoryText + " + " + str;
+        else if ( cs->relation == IndirectAdjacent )
+            str = tagHistoryText + " ~ " + str;
         else if ( cs->relation == Child )
             str = tagHistoryText + " > " + str;
         else if ( cs->relation == SubSelector )
