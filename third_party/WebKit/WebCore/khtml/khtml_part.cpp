b14aa22b73ae7cd64e8401b77502fc9fdac96a1e
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-13 07:27:07 +0000

    Patch from Andrew Wellington <proton@wiretapped.net>         Reviewed by me.
    	- fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=3302
    	(Safari crashes when going to acid2 test page once it's in cache)
    
            No test cases addded since the repro steps require use of tabbed browsing.
    
            * khtml/khtml_part.cpp:
            (KHTMLPart::handleFallbackContent): check for nil
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9375 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/khtml_part.cpp
+++ third_party/WebKit/WebCore/khtml/khtml_part.cpp
@@ -5943,6 +5943,8 @@ void KHTMLPart::handleFallbackContent()
     if (!renderPart)
         return;
     HTMLObjectElementImpl* elt = static_cast<HTMLObjectElementImpl *>(renderPart->element());
+    if (!elt)
+        return;
     elt->renderFallbackContent();
 }
 
