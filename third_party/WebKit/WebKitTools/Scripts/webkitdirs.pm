6269bdd7b18b3d10cc8f1e43ae474d4f568d3973
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-13 02:45:26 +0000

    Changes by Michael Gaiman.         Reviewed by me.
            - fixed <http://bugzilla.opendarwin.org/show_bug.cgi?id=3487>
              WebKit no longer builds after configuration supporting build changes
    
            * Scripts/webkitdirs.pm: Chomp off the result of `pwd`, and don't die when no Configuration file.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9371 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -79,7 +79,9 @@ sub determineBaseProductDir
         @baseProductDirOption = ();
     } else {
         chdirWebKit();
-        $baseProductDir = `pwd` . "/WebKitBuild";
+        my $dir = `pwd`;
+        chomp $dir;
+        $baseProductDir = "$dir/WebKitBuild";
         @baseProductDirOption = ("SYMROOT=$baseProductDir");
     }
     $baseProductDir =~ s|^~/|$ENV{HOME}/|;
@@ -89,7 +91,7 @@ sub determineConfiguration
 {
     return if defined $configuration;
     determineBaseProductDir();
-    open CONFIGURATION, "$baseProductDir/Configuration" or die;
+    open CONFIGURATION, "$baseProductDir/Configuration";
     $configuration = <CONFIGURATION>;
     close CONFIGURATION;
     if ($configuration) {
