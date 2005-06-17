bbd38787face57ba42e99829a2e31721365af2bf
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-17 18:14:27 +0000

    Patch contributed by Joos de Valk <webkit@joostdevalk.nl>.
    	Fixes: http://bugzilla.opendarwin.org/show_bug.cgi?id=3408
    
            Reviewed by Darin.
    
            Test cases added: None. Test requires manual interaction.
    	Manual test available at: http://bugzilla.opendarwin.org/attachment.cgi?id=2207
    
            * khtml/html/html_formimpl.cpp: Eliminated HTMLButtonElementImpl::click. HTMLButtonElementImpl now inherits HTMLElementImpl::click.
            * khtml/html/html_formimpl.h: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_formimpl.h
+++ third_party/WebKit/WebCore/khtml/html/html_formimpl.h
@@ -254,7 +254,6 @@ public:
     virtual bool isActivatedSubmit() const;
     virtual void setActivatedSubmit(bool flag);
 
-    virtual void click(bool sendMouseEvents);
     virtual void accessKeyAction(bool sendToAnyElement);
 
     DOMString accessKey() const;
