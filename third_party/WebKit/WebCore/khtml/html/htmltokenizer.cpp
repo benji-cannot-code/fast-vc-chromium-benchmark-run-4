bc4b284bac69210ec86fac430c3db56ad60ffa13
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-17 20:00:11 +0000

    Reviewed by Maciej.
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=3419
            <rdar://problem/4111752> REGRESSION (125-312/412): Safari doesn't display edmunds.com pages first time through
    
            * khtml/html/htmltokenizer.cpp: (khtml::HTMLTokenizer::parseTag): set 'script' flag when
              parsing <script src="foo" /> case.  In this case, parseTag calls the scriptHandler function which expects
              that flag to be set, and sets it back to false at the appropriate time.
    
            Test cases added: These still don't test the cached script cases, but they're good regression
            tests for the tokenizer.  We'll have to come up with a good way to test the cached cases later.
    
            * layout-tests/fast/tokenizer/external-script-document-write_2-expected.txt: Added.
            * layout-tests/fast/tokenizer/external-script-document-write_2.html: Added.
            * layout-tests/fast/tokenizer/resources/empty_script.js: Added.
            * layout-tests/fast/tokenizer/script_extra_close-expected.txt: Added.
            * layout-tests/fast/tokenizer/script_extra_close.html: Added.
            * layout-tests/fast/tokenizer/script_quirk-expected.txt: Added.
            * layout-tests/fast/tokenizer/script_quirk.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9414 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/htmltokenizer.cpp
+++ third_party/WebKit/WebCore/khtml/html/htmltokenizer.cpp
@@ -1435,8 +1435,10 @@ void HTMLTokenizer::parseTag(TokenizerString &src)
                     script = true;
                     parseSpecial(src);
                 }
-                else if (tagID <= ID_CLOSE_TAG) // Handle <script src="foo"/>
+                else if (tagID <= ID_CLOSE_TAG) { // Handle <script src="foo"/>
+                    script = true;
                     scriptHandler();
+                }
                 break;
             case ID_STYLE:
                 if (beginTag) {
