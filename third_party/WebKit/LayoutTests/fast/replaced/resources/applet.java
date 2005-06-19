56048ad476acbb4d93a9298e75d3a66be41d9c87
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-19 05:50:24 +0000

    - disabling the applet tests, since they are causing other tests to fail
            * layout-tests/fast/replaced/applet-hspace-expected.txt: Removed.
            * layout-tests/fast/replaced/applet-hspace.html: Removed.
            * layout-tests/fast/replaced/applet-vspace-expected.txt: Removed.
            * layout-tests/fast/replaced/applet-vspace.html: Removed.
            * layout-tests/fast/replaced/resources/applet.class: Removed.
            * layout-tests/fast/replaced/resources/applet.java: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9432 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/replaced/resources/applet.java
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-import java.awt.*;
-import java.applet.*;
-public class applet extends Applet
-{
-    public void paint(Graphics g) {
-        g.fillRect(0, 0, 10000, 10000);
-    }
-}
