8102177ccc6bbb85fcd707ad1314c1d458dce674
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-19 05:00:25 +0000

    Changes by Anders Carlsson.         Reviewed by me.
            Test cases added:
            * layout-tests/fast/forms/focus-expected.txt: Added.
            * layout-tests/fast/forms/focus.html: Added.
            * layout-tests/fast/forms/legend-access-key-expected.txt: Added.
            * layout-tests/fast/forms/legend-access-key.html: Added.
    
            - fixed <rdar://problem/3481846> HTML4: accesskey not supported by HTML legend elements
              also <http://bugzilla.opendarwin.org/show_bug.cgi?id=3407>
            - added focus methods for <label> and <legend> elements
            - fixed crash when using JavaScript-created events that prevented tests from running
    
            * khtml/ecma/kjs_html.h: Added focus methods.
            * khtml/ecma/kjs_html.cpp: (KJS::KJS::HTMLElementFunction::tryCall): Added bindings for
            focus for <label> and <legend>. (Sorry about the merging, Dave.)
    
            * khtml/html/html_formimpl.h: Added new functions.
            * khtml/html/html_formimpl.cpp:
            (DOM::HTMLLabelElementImpl::focus): Added.
            (DOM::HTMLLegendElementImpl::formElement): Added.
            (DOM::HTMLLegendElementImpl::focus): Added.
            (DOM::HTMLLegendElementImpl::accessKeyAction): Added.
    
            * khtml/xml/dom_docimpl.cpp: (DocumentImpl::defaultEventHandler): If there's no QKeyEvent,
            then get the key name from keyIdentifier() instead of the QKeyEvent's unmodifiedText().
            The old code would just do a nil-deref.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9429 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_formimpl.h
+++ third_party/WebKit/WebCore/khtml/html/html_formimpl.h
@@ -471,6 +471,8 @@ public:
     DOMString htmlFor() const;
     void setHtmlFor(const DOMString &);
 
+    void focus();
+
  private:
     DOMString m_formElementID;
 };
@@ -490,11 +492,20 @@ public:
 
     virtual DOMString type() const;
 
+    virtual void accessKeyAction(bool sendToAnyElement);
+
+    /**
+     * The first form element in the legend's fieldset 
+     */
+    ElementImpl *formElement();
+
     DOMString accessKey() const;
     void setAccessKey(const DOMString &);
 
     DOMString align() const;
     void setAlign(const DOMString &);
+    
+    void focus();
 };
 
 // -------------------------------------------------------------------------
