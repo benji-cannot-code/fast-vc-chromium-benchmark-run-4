83523fd662a3bd42251d4b5f3d3f9237ef4de275
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-19 02:27:36 +0000

    Change by Niels Leenheer (also done by Dave Hyatt on stage at WWDC).         Reviewed by me.
            Test cases added:
            * layout-tests/fast/replaced/applet-hspace-expected.txt: Added.
            * layout-tests/fast/replaced/applet-hspace.html: Added.
            * layout-tests/fast/replaced/applet-vspace-expected.txt: Added.
            * layout-tests/fast/replaced/applet-vspace.html: Added.
            * layout-tests/fast/replaced/resources/applet.class: Added.
            * layout-tests/fast/replaced/resources/applet.java: Added.
    
            - fixed <http://bugzilla.opendarwin.org/show_bug.cgi?id=3246>
              HSPACE and VSPACE attributes are ignored in APPLET element
    
            * khtml/html/html_objectimpl.cpp:
            (DOM::HTMLAppletElementImpl::mapToEntry): Add cases for ATTR_VSPACE and ATTR_HSPACE.
            (DOM::HTMLAppletElementImpl::parseMappedAttribute): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9425 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/replaced/resources/applet.java
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import java.awt.*;
+import java.applet.*;
+public class applet extends Applet
+{
+    public void paint(Graphics g) {
+        g.fillRect(0, 0, 10000, 10000);
+    }
+}
