be4f1c21920373db2258ed1c4980fa1e807a2cb4
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-22 18:01:02 +0000

    Change by Anders Carlsson.
            - added support for dumping title changes
    
            * DumpRenderTree/DumpRenderTree.m:
            (-[WaitUntilDoneDelegate webView:didReceiveTitle:forFrame:]): Added. Dump title change
            if requested by JavaScript.
            (+[LayoutTestController isSelectorExcludedFromWebScript:]): Added dumpTitleChanges.
            (-[LayoutTestController dumpTitleChanges]): Added, sets flag.
            (dumpRenderTree): Start flag as NO.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9465 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -51,6 +51,7 @@ static volatile BOOL done;
 static WebFrame *frame;
 static BOOL waitLayoutTest;
 static BOOL dumpAsText;
+static BOOL dumpTitleChanges;
 
 int main(int argc, const char *argv[])
 {
@@ -172,13 +173,22 @@ static void dump(void)
     printf("ALERT: %s\n", [message UTF8String]);
 }
 
+- (void)webView:(WebView *)sender didReceiveTitle:(NSString *)title forFrame:(WebFrame *)frame
+{
+    if (dumpTitleChanges)
+        printf("TITLE CHANGED: %s\n", [title UTF8String]);
+}
+
 @end
 
 @implementation LayoutTestController
 
 + (BOOL)isSelectorExcludedFromWebScript:(SEL)aSelector
 {
-    if (aSelector == @selector(waitUntilDone) || aSelector == @selector(notifyDone) || aSelector == @selector(dumpAsText))
+    if (aSelector == @selector(waitUntilDone)
+            || aSelector == @selector(notifyDone)
+            || aSelector == @selector(dumpAsText)
+            || aSelector == @selector(dumpTitleChanges))
         return NO;
     return YES;
 }
@@ -199,6 +209,11 @@ static void dump(void)
     dumpAsText = YES;
 }
 
+- (void)dumpTitleChanges
+{
+    dumpTitleChanges = YES;
+}
+
 @end
 
 static void dumpRenderTree(const char *filename)
@@ -216,6 +231,7 @@ static void dumpRenderTree(const char *filename)
 
     done = NO;
     dumpAsText = NO;
+    dumpTitleChanges = NO;
     waitLayoutTest = NO;
 
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
