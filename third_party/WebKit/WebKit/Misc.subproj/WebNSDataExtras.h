e4b7b7d0719c588b68a7f17a70bad52a702a8178
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-22 16:20:01 +0000

    Change by Michael Gaiman.         Reviewed by me.
            - fixed <http://bugzilla.opendarwin.org/show_bug.cgi?id=3436>
              Missing implementation of -[NSData(WebNSDateExtras) _webkit_parseRFC822HeaderFields]
    
            * Misc.subproj/WebNSDataExtras.h: Fixed name of category say NSData, not NSDate.
            * Misc.subproj/WebNSDataExtras.m: (-[NSData _webkit_parseRFC822HeaderFields]): Fixed method name.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebNSDataExtras.h
+++ third_party/WebKit/WebKit/Misc.subproj/WebNSDataExtras.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define WEB_GUESS_MIME_TYPE_PEEK_LENGTH 1024
 
-@interface NSData (WebNSDateExtras)
+@interface NSData (WebNSDataExtras)
 
 -(BOOL)_web_isCaseInsensitiveEqualToCString:(const char *)string;
 -(NSMutableDictionary *)_webkit_parseRFC822HeaderFields;
