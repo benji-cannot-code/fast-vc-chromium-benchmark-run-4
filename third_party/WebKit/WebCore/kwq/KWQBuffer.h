348d391f6333403df1100a0c06777541366747dd
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-06-24 21:05:11 +0000

    - <http://bugzilla.opendarwin.org/show_bug.cgi?id=3673>             Adds QBuffer::setBuffer(QByteArray) support to KWQBuffer, and adds a QBuffer(QByteArray) constructor.
            Reviewed by Darin
    
            Test cases added: Non required
    
            * kwq/KWQBuffer.h:
            (QBuffer::QBuffer):
            * kwq/KWQBuffer.mm:
            (QBuffer::setBuffer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9484 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQBuffer.h
+++ third_party/WebKit/WebCore/kwq/KWQBuffer.h
@@ -36,7 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class QBuffer {
 public:
     QBuffer() : opened(false), pos(0) { }
+    QBuffer(QByteArray b) : opened(false), ba(b), pos(0) { }
 
+    bool setBuffer(QByteArray);
     QByteArray buffer() const { return ba; }
     uint size() const { return ba.size(); }
     bool isOpen() const { return opened; }
