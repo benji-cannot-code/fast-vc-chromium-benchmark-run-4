4f092e53abe2550f4f4e4a0312fad2e87a45aa5d
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-07 17:06:54 +0000

    Reviewed by sullivan.
            Test cases added: (NONE)
    
            * kwq/KWQDef.h: added #include <cmath> to fix build failure
            introduced by last checkin
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9612 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQDef.h
+++ third_party/WebKit/WebCore/kwq/KWQDef.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define KWQDEF_H_
 
 #include <sys/types.h>
+#include <cmath>
+
 #ifndef KWQ_UNSIGNED_TYPES_DEFINED
 #define KWQ_UNSIGNED_TYPES_DEFINED
 typedef unsigned char uchar;
