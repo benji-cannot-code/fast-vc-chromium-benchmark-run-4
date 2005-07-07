a98d09ea57355e6f752d841e50002dea5ddb4a4d
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-07 07:17:28 +0000

    Bug #: 3826 Submitted by: eseidel Reviewed by: mjs         * khtml/rendering/render_canvasimage.cpp: <cmath.h>         * khtml/rendering/render_image.cpp: <cmath.h> instead of <math.h>         * kwq/KWQDef.h: 64bit support for KDOM         (qRound): inline mapping to lround.         Newer patch, now gcc 3.3 compatible.         Thanks to ggaren for the 3.3 fix.         http://bugzilla.opendarwin.org/show_bug.cgi?id=3826
    git-svn-id: svn://svn.chromium.org/blink/trunk@9608 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_canvasimage.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_canvasimage.cpp
@@ -44,7 +44,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "html/dtd.h"
 #include "xml/dom2_eventsimpl.h"
 #include "html/html_documentimpl.h"
-#include <math.h>
+
+// Must include <cmath> instead of <math.h> because of a bug in the
+// gcc 3.3 library version of <math.h> where if you include both
+// <cmath> and <math.h> the macros necessary for functions like
+// isnan are not defined.
+#include <cmath>
 
 
 
