17c46988e9cab7e63826652329dc42712a9d796d
kdecker <kdecker@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-11 16:57:39 +0000

    Reviewed by cblu and mjs.
    	Fixed: <rdar://problem/4099552> REGRESSION: Safari 1.3 Netscape API NPN_PostURL[Notify] no longer allows manual headers
    
    	Most plugins (flash) send 2 CRFL's between the header and body of their POST requests, while the adboe plugin sends two LF's. This caused us to send custom headers as part of the actual POST data itself, and correspondently, would skew Content-Length.
    
            * Plugins.subproj/WebBaseNetscapePluginView.m:
            (-[NSData _web_locationAfterFirstBlankLine]): Look for two LF's as well
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9653 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.m
@@ -1799,7 +1799,7 @@ static OSStatus TSMEventHandler(EventHandlerCallRef inHandlerRef, EventRef inEve
     return [self length] > 0 && ((const char *)[self bytes])[0] == '\n';
 }
 
-// Returns the position after 2 CRLF's or 1 CRLF if it is the first line.
+
 - (unsigned)_web_locationAfterFirstBlankLine
 {
     const char *bytes = (const char *)[self bytes];
@@ -1807,6 +1807,13 @@ static OSStatus TSMEventHandler(EventHandlerCallRef inHandlerRef, EventRef inEve
     
     unsigned i;
     for (i = 0; i < length - 4; i++) {
+        
+        //most plugins (Flash) send 2 CRFL's between the header and body of their POST requests, while the adboe plugin sends two LF's.
+        if (bytes[i] == '\n' && bytes[i+1] == '\n') {
+            return i+2;
+        }
+        
+        // Returns the position after 2 CRLF's or 1 CRLF if it is the first line.
         if (bytes[i] == '\r' && bytes[i+1] == '\n') {
             i += 2;
             if (i == 2) {
