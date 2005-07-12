380b8f8625954ec8a5e66c27545088e3940e53c0
kocienda <kocienda@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-12 16:37:26 +0000

    Reviewed by me
            Test cases added: (NONE)
    
            * kwq/KWQTextCodec.mm: Fix build breakage. /me is a real genius.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9675 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextCodec.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextCodec.mm
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "KWQAssertions.h"
 #import "KWQCharsets.h"
 #import "KWQLogging.h"
-#import "WebCoreTextConversionMethod.h"
 
 #import <unicode/ucnv.h>
 #import <unicode/utypes.h>
