def3d49c9ccb531f8f94bbf90ce0206c110a7408
bdakin <bdakin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-12 20:26:42 +0000

    Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=3942 and marquee problems in Radar bug 	4172312. Vertical marquees default to 200px, and horizontal marquees do not allow text to be 	truncated by a too-small height attribute.
            Reviewed by Hyatt.
    
            Test cases added: (NONE)
    
            * khtml/html/html_blockimpl.cpp: deleted former changes to make marquee height work correctly.
            (HTMLMarqueeElementImpl::mapToEntry):
            (HTMLMarqueeElementImpl::parseMappedAttribute):
            * khtml/rendering/render_layer.cpp: this is where the height issues are now addressed
            (Marquee::updateMarqueeStyle):
            * khtml/rendering/render_layer.h:
            (khtml::):
            * khtml/rendering/render_style.h: added function fontSize()
            (khtml::RenderStyle::fontSize):
            * khtml/xml/dom_elementimpl.h: deleted addition of EMarquee hash key
            (DOM::):
            * layout-tests/fast/css/MarqueeLayoutTest-expected.txt:
            * layout-tests/fast/css/MarqueeLayoutTest.html: added vertical marquees to test.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9700 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_blockimpl.cpp
+++ third_party/WebKit/WebCore/khtml/html/html_blockimpl.cpp
@@ -348,9 +348,6 @@ bool HTMLMarqueeElementImpl::mapToEntry(NodeImpl::Id attr, MappedAttributeEntry&
 {
     switch (attr) {
         case ATTR_HEIGHT:   
-            // Needed to create eMarquee hash key to treat height attribute like WinIE. Fix for half of 4172312.
-            result = eMarquee;
-            return false;
         case ATTR_WIDTH:
         case ATTR_BGCOLOR:
         case ATTR_VSPACE:
@@ -365,9 +362,9 @@ bool HTMLMarqueeElementImpl::mapToEntry(NodeImpl::Id attr, MappedAttributeEntry&
         default:
             break;
     }
+    
     return HTMLElementImpl::mapToEntry(attr, result);
 }
-            
             
 void HTMLMarqueeElementImpl::parseMappedAttribute(MappedAttributeImpl *attr)
 {
@@ -379,7 +376,7 @@ void HTMLMarqueeElementImpl::parseMappedAttribute(MappedAttributeImpl *attr)
             break;
         case ATTR_HEIGHT:
             if (!attr->value().isEmpty())
-                addCSSLength(attr, CSS_PROP_MIN_HEIGHT, attr->value());
+                addCSSLength(attr, CSS_PROP_HEIGHT, attr->value());
             break;
         case ATTR_BGCOLOR:
             if (!attr->value().isEmpty())
