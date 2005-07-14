bb9555ed1bfb2365174806e7f5e836c467b15175
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-14 23:51:44 +0000

    added missing #import to fix build 	* WebView.subproj/WebPDFView.m
    git-svn-id: svn://svn.chromium.org/blink/trunk@9784 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebPDFView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebPDFView.m
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebDocumentInternal.h>
 #import <WebKit/WebFrame.h>
 #import <WebKit/WebFrameInternal.h>
+#import <WebKit/WebFrameView.h>
 #import <WebKit/WebLocalizableStrings.h>
 #import <WebKit/WebNSPasteboardExtras.h>
 #import <WebKit/WebNSViewExtras.h>
