85f937572980965c4befc0fe7ba518bd07fa909c
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-14 17:24:31 +0000

    JavaScriptCore:
    	-landed fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=3412
    	Object.prototype is missing toLocaleString
    
    	- patch by Mark Rowe (bdash) <opendarwin.org@bdash.net.nz>
    
    	-layout test info in webcore changelog
    
            Reviewed by mjs.
    
            * kjs/object_object.cpp:
            (ObjectPrototypeImp::ObjectPrototypeImp):
            (ObjectProtoFuncImp::call):
            * kjs/object_object.h:
            (KJS::ObjectProtoFuncImp::):
    
    WebCore:
    
    	-landed layout test for http://bugzilla.opendarwin.org/show_bug.cgi?id=3412
    	Object.prototype is missing toLocaleString
    
            Reviewed by mjs.
    
            Test cases added:
            * layout-tests/fast/js/object-prototype-toLocaleString-expected.txt: Added.
            * layout-tests/fast/js/object-prototype-toLocaleString.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9758 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/object_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/object_object.cpp
@@ -39,9 +39,10 @@ ObjectPrototypeImp::ObjectPrototypeImp(ExecState *exec,
   : ObjectImp() // [[Prototype]] is Null()
 {
     Value protect(this);
-    putDirect(toStringPropertyName, new ObjectProtoFuncImp(exec,funcProto,ObjectProtoFuncImp::ToString,  0), DontEnum);
-    putDirect(valueOfPropertyName,  new ObjectProtoFuncImp(exec,funcProto,ObjectProtoFuncImp::ValueOf,   0), DontEnum);
-    putDirect("hasOwnProperty", new ObjectProtoFuncImp(exec,funcProto,ObjectProtoFuncImp::HasOwnProperty,1), DontEnum);
+    putDirect(toStringPropertyName, new ObjectProtoFuncImp(exec,funcProto,ObjectProtoFuncImp::ToString,            0), DontEnum);
+    putDirect(toLocaleStringPropertyName, new ObjectProtoFuncImp(exec,funcProto,ObjectProtoFuncImp::ToLocaleString,0), DontEnum);
+    putDirect(valueOfPropertyName,  new ObjectProtoFuncImp(exec,funcProto,ObjectProtoFuncImp::ValueOf,             0), DontEnum);
+    putDirect("hasOwnProperty", new ObjectProtoFuncImp(exec,funcProto,ObjectProtoFuncImp::HasOwnProperty,          1), DontEnum);
 }
 
 
@@ -75,6 +76,8 @@ Value ObjectProtoFuncImp::call(ExecState *exec, Object &thisObj, const List &arg
             bool exists = thisObj.hasOwnProperty(exec, propertyName);
             return Value(exists ? BooleanImp::staticTrue : BooleanImp::staticFalse);
         }
+        case ToLocaleString:
+          return thisObj.imp()->toString(exec);
         case ToString:
         default:
             return String("[object " + thisObj.className() + "]");
