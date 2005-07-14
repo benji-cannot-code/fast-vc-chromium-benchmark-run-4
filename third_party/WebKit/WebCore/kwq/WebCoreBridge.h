740fbfd10f7894944325509a790030e1cc729be2
vicki <vicki@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-14 16:29:10 +0000

    WebCore:
            Reviewed by Kocienda.
    
            - WebCore part of fix for  <rdar://problem/4172380> [GENENTECH] window.opener
            not available when child opened via target="_new"
    
            Add a setOpener function to the WebCore bridge, and call this function when opening
            new windows through Web Kit.
    
            * kwq/WebCoreBridge.h:
            * kwq/WebCoreBridge.mm:
    
    WebKit:
    
            Reviewed by Kocienda.
    
    	- WebKit part of fix for  <rdar://problem/4172380> [GENENTECH] window.opener
    	not available when child opened via target="_new"
    
    	Add a setOpener function to the WebCore bridge, and call this function when opening
    	new windows through Web Kit.
    
            * WebView.subproj/WebFrame.m:
            (-[WebFrame _continueLoadRequestAfterNewWindowPolicy:frameName:formState:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreBridge.h
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.h
@@ -234,6 +234,7 @@ typedef enum
 - (NSString *)referrer;
 - (NSString *)domain;
 - (WebCoreBridge *)opener;
+- (void)setOpener:(WebCoreBridge *)bridge;
 
 - (void)installInFrame:(NSView *)view;
 - (void)removeFromFrame;
--- third_party/WebKit/WebCore/kwq/WebCoreBridge.mm
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.mm
@@ -1347,6 +1347,14 @@ static HTMLFormElementImpl *formElementFromDOMElement(DOMElement *element)
     return nil;
 }
 
+- (void)setOpener:(WebCoreBridge *)bridge;
+{
+    KHTMLPart *p = [bridge part];
+    
+    if (p)
+        p->setOpener(_part);
+}
+
 + (NSString *)stringWithData:(NSData *)data textEncoding:(CFStringEncoding)textEncoding
 {
     if (textEncoding == kCFStringEncodingInvalidId || textEncoding == kCFStringEncodingISOLatin1) {
