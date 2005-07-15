43e8146ab590000b290aabebdf4049e9fbf02a8b
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-15 22:59:18 +0000

    Written by Trey Matteson         Reviewed by me.
            Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=4013
                    text find doesn't wrap in PDF files
    
            This just works once WebPDFView implements the WebDocumentText protocol, which is
            mostly just a matter of forwarding the methods to PDFKit appropriately.
    
            * WebView.subproj/WebPDFView.h:
            * WebView.subproj/WebPDFView.m:
            (-[WebPDFView supportsTextEncoding]):
            (-[WebPDFView string]):
            (-[WebPDFView attributedString]):
            (-[WebPDFView selectedString]):
            (-[WebPDFView selectedAttributedString]):
            (-[WebPDFView selectAll]):
            (-[WebPDFView deselectAll]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9814 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebPDFView.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebPDFView.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @protocol _web_WebDocumentTextSizing;
 
-@interface WebPDFView : NSView <WebDocumentView, WebDocumentSearching, _web_WebDocumentTextSizing>
+@interface WebPDFView : NSView <WebDocumentView, WebDocumentSearching, WebDocumentText, _web_WebDocumentTextSizing>
 {
     PDFView *PDFSubview;
     WebDataSource *dataSource;
