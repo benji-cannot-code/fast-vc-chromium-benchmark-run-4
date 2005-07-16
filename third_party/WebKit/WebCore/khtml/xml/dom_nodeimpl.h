81bbc55fe036e940b10d49466a08cbeb5747362b
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-16 06:45:19 +0000

    Written by Anders Carlsson  <andersca@mac.com>         Reviewed by Dave Hyatt.
    	- Fixes <http://bugzilla.opendarwin.org/show_bug.cgi?id=3653>
    
            Test cases added:
    	layout-tests/fast/dom/createDocumentType.html: Added
    	layout-tests/fast/dom/createDocumentType-expected.txt: Added
    
            * khtml/xml/dom_nodeimpl.cpp: (DocumentPtr::nullDocumentPtr):
            * khtml/xml/dom_nodeimpl.h:
    	Add nullDocumentPtr() function which returns a shared DocumentPtr
    	object whose document always is 0.
    
            * khtml/xml/dom_docimpl.cpp: (DOMImplementationImpl::createDocumentType):
    	Pass nullDocumentPtr as the document type's document.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9818 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.h
+++ third_party/WebKit/WebCore/khtml/xml/dom_nodeimpl.h
@@ -82,6 +82,8 @@ class DocumentPtr : public khtml::Shared<DocumentPtr>
 {
 public:
     DocumentImpl *document() const { return doc; }
+    static DocumentPtr *nullDocumentPtr();
+    
 private:
     DocumentPtr() { doc = 0; }
     friend class DocumentImpl;
