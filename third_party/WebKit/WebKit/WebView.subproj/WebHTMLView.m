7e2964841f959216fbb9c7e625788c3ab987750d
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-18 20:36:53 +0000

    Reviewed by Richard Williamson.
            - fixed <rdar://problem/4184366> WebPDFView should conform to the WebDocumentSelection protocol
    
            * Misc.subproj/WebNSAttributedStringExtras.h: Added.
            * Misc.subproj/WebNSAttributedStringExtras.m: Added.
            (-[NSAttributedString _web_attributedStringByStrippingAttachmentCharacters]):
            New category on NSAttributedString, initially contains this one method that had been in WebHTMLView.
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView _writeSelectionWithPasteboardTypes:toPasteboard:cachedAttributedString:]):
            now uses _web_attributedStringByStrippingAttachmentCharacters
    
            * WebView.subproj/WebPDFView.h:
            now conforms to WebDocumentSelection protocol
            * WebView.subproj/WebPDFView.m:
            (-[WebPDFView selectionRect]):
            new, implementation of WebDocumentSelection protocol method
            (-[WebPDFView pasteboardTypesForSelection]):
            ditto
            (-[WebPDFView writeSelectionWithPasteboardTypes:toPasteboard:]):
            ditto
    
            * WebKit.pbproj/project.pbxproj:
            updated for new files
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9823 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKitLogging.h>
 #import <WebKit/WebKitNSStringExtras.h>
 #import <WebKit/WebNetscapePluginEmbeddedView.h>
+#import <WebKit/WebNSAttributedStringExtras.h>
 #import <WebKit/WebNSEventExtras.h>
 #import <WebKit/WebNSFileManagerExtras.h>
 #import <WebKit/WebNSImageExtras.h>
@@ -574,36 +575,6 @@ void *_NSSoftLinkingGetFrameworkFuncPtr(NSString *inUmbrellaFrameworkName,
     return hitView;
 }
 
-// This method is copied from NSTextView
-- (NSAttributedString *)_stripAttachmentCharactersFromAttributedString:(NSAttributedString *)originalAttributedString
-{
-    NSRange attachmentRange;
-    NSString *originalString = [originalAttributedString string];
-    static NSString *attachmentCharString = nil;
-    
-    if (!attachmentCharString) {
-        unichar chars[2];
-        if (!attachmentCharString) {
-            chars[0] = NSAttachmentCharacter;
-            chars[1] = 0;
-            attachmentCharString = [[NSString alloc] initWithCharacters:chars length:1];
-        }
-    }
-    
-    attachmentRange = [originalString rangeOfString:attachmentCharString];
-    if (attachmentRange.location != NSNotFound && attachmentRange.length > 0) {
-        NSMutableAttributedString *newAttributedString = [[originalAttributedString mutableCopyWithZone:NULL] autorelease];
-        
-        while (attachmentRange.location != NSNotFound && attachmentRange.length > 0) {
-            [newAttributedString replaceCharactersInRange:attachmentRange withString:@""];
-            attachmentRange = [[newAttributedString string] rangeOfString:attachmentCharString];
-        }
-        return newAttributedString;
-    } else {
-        return originalAttributedString;
-    }
-}
-
 - (void)_writeSelectionWithPasteboardTypes:(NSArray *)types toPasteboard:(NSPasteboard *)pasteboard cachedAttributedString:(NSAttributedString *)attributedString
 {
     // Put HTML on the pasteboard.
@@ -625,7 +596,7 @@ void *_NSSoftLinkingGetFrameworkFuncPtr(NSString *inUmbrellaFrameworkName,
             attributedString = [self selectedAttributedString];
         }
         if ([attributedString containsAttachments]) {
-            attributedString = [self _stripAttachmentCharactersFromAttributedString:attributedString];
+            attributedString = [attributedString _web_attributedStringByStrippingAttachmentCharacters];
         }
         NSData *RTFData = [attributedString RTFFromRange:NSMakeRange(0, [attributedString length]) documentAttributes:nil];
         [pasteboard setData:RTFData forType:NSRTFPboardType];
