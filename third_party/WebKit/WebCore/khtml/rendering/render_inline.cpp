927343a492431ce830420c4daea04aa7f941d33e
bdakin <bdakin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-21 16:33:33 +0000

    Reviewed by Hyatt.
    	-fix for <rdar://problem/4169069> Also fixes the behavior of generated content
    	when it is specified within a list element.
    
            Test cases added: (NONE)
    
            * khtml/rendering/render_block.cpp:
            (khtml::RenderBlock::setStyle): Added loop to skip over list elements hen updating
    					generatedcontent.
            (khtml::RenderBlock::updateFirstLetter): Added clause so that the function updates the
    						first letter instead of just creating it.
            * khtml/rendering/render_inline.cpp:
            (RenderInline::setStyle): Added loop to skip over list elements when updating generated
    				content.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9854 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_inline.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_inline.cpp
@@ -65,7 +65,9 @@ void RenderInline::setStyle(RenderStyle* _style)
     m_lineHeight = -1;
     
     // Update pseudos for :before and :after now.
-    updatePseudoChild(RenderStyle::BEFORE, firstChild());
+    RenderObject* first = firstChild();
+    while (first && first->isListMarker()) first = first->nextSibling();
+    updatePseudoChild(RenderStyle::BEFORE, first);
     updatePseudoChild(RenderStyle::AFTER, lastChild());
 }
 
