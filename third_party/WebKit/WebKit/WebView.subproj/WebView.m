a01de9ec7661d76b190ca359aade0ed51337b985
vicki <vicki@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-26 00:11:33 +0000

    Reviewed by Darin.
    	- fixed <rdar://problem/3470523> Safari's user agent should be changed to say Intel rather
    	than PPC on Intel machines
    
            * WebView.subproj/WebView.m: add conditional #defines for "PPC" and "Intel"
            (-[WebView userAgentForURL:]): use this variable when constructing the user agent string
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9892 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebView.m
@@ -88,6 +88,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/NSURLConnection.h>
 
+#if __ppc__
+#define PROCESSOR "PPC"
+#elif __i386__
+#define PROCESSOR "Intel"
+#else
+#error Unknown architecture
+#endif
+
 #define FOR_EACH_RESPONDER_SELECTOR(macro) \
 macro(alignCenter) \
 macro(alignJustified) \
@@ -1926,7 +1934,7 @@ NS_ENDHANDLER
     return [[[self mainFrame] _bridge] windowScriptObject];
 }
 
-
+	
 // Get the appropriate user-agent string for a particular URL.
 // Since we no longer automatically spoof, this no longer requires looking at the URL.
 - (NSString *)userAgentForURL:(NSURL *)URL
@@ -1936,18 +1944,16 @@ NS_ENDHANDLER
         return [[userAgent retain] autorelease];
     }
     
-    // FIXME: Some day we will start reporting the actual CPU here instead of hardcoding PPC.
-
     NSString *language = [NSUserDefaults _webkit_preferredLanguageCode];
     id sourceVersion = [[NSBundle bundleForClass:[WebView class]]
         objectForInfoDictionaryKey:(id)kCFBundleVersionKey];
     NSString *applicationName = _private->applicationNameForUserAgent;
 
     if ([applicationName length]) {
-        userAgent = [NSString stringWithFormat:@"Mozilla/5.0 (Macintosh; U; PPC Mac OS X; %@) AppleWebKit/%@ (KHTML, like Gecko) %@",
+        userAgent = [NSString stringWithFormat:@"Mozilla/5.0 (Macintosh; U; " PROCESSOR " Mac OS X; %@) AppleWebKit/%@ (KHTML, like Gecko) %@",
             language, sourceVersion, applicationName];
     } else {
-        userAgent = [NSString stringWithFormat:@"Mozilla/5.0 (Macintosh; U; PPC Mac OS X; %@) AppleWebKit/%@ (KHTML, like Gecko)",
+        userAgent = [NSString stringWithFormat:@"Mozilla/5.0 (Macintosh; U; " PROCESSOR " Mac OS X; %@) AppleWebKit/%@ (KHTML, like Gecko)",
             language, sourceVersion];
     }
 
