85db2bcb8edfc80ae9d909e075a161e6eaf916f1
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-29 21:01:37 +0000

    Reviewed by Chris Blumenberg.
            * WebView.subproj/WebFrameView.m:
            (-[WebFrameView _firstResponderIsFormControl]):
            renamed from _firstResponderIsControl for clarity. Explicitly rejects WebHTMLView, since
            it's now a control.
            (-[WebFrameView keyDown:]):
            updated for renamed method.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9949 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrameView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrameView.m
@@ -627,9 +627,15 @@ static NSMutableDictionary *viewTypes;
     [self _scrollLineVertically:NO];
 }
 
-- (BOOL)_firstResponderIsControl
+- (BOOL)_firstResponderIsFormControl
 {
-    return [[[self window] firstResponder] isKindOfClass:[NSControl class]];
+    NSResponder *firstResponder = [[self window] firstResponder];
+    
+    // WebHTMLView is an NSControl subclass these days, but it's not a form control
+    if ([firstResponder isKindOfClass:[WebHTMLView class]]) {
+        return NO;
+    }
+    return [firstResponder isKindOfClass:[NSControl class]];
 }
 
 - (void)keyDown:(NSEvent *)event
@@ -660,7 +666,7 @@ static NSMutableDictionary *viewTypes;
                 // Checking for a control will allow events to percolate 
                 // correctly when the focus is on a form control and we
                 // are in full keyboard access mode.
-                if (![self allowsScrolling] || [self _firstResponderIsControl]) {
+                if (![self allowsScrolling] || [self _firstResponderIsFormControl]) {
                     callSuper = YES;
                     break;
                 }
