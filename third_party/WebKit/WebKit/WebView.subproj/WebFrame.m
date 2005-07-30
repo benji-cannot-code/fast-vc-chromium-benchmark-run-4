00e8895d45321e89f59df26bdf58185f74e999f2
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-07-30 08:38:00 +0000

    Build fixes for previous change (missing includes)
            * WebView.subproj/WebFrame.m:
            * WebView.subproj/WebScriptDebugDelegate.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@9975 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrame.m
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebResourcePrivate.h>
 #import <WebKit/WebViewInternal.h>
 #import <WebKit/WebUIDelegate.h>
+#import <WebKit/WebScriptDebugDelegatePrivate.h>
 
 #import <objc/objc-runtime.h>
 
