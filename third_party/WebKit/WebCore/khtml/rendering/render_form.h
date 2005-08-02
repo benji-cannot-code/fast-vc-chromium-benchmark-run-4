56042b5a71d9755e89617da34c93a562eb51e078
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-02 20:28:41 +0000

    - landed layout tests for fix to <rdar://problem/3572585>         window.open fails if name param = the name of a window just         closed in same function
            Fix reviewed by darin.
    
            Test cases added:
    
            * manual-tests/open-after-close.html: Added.
            * manual-tests/resources/open-after-close-popup.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10012 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_form.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_form.h
@@ -37,7 +37,6 @@ class QListboxItem;
 
 #include <qtextedit.h>
 #include <klineedit.h>
-#include <qcheckbox.h>
 #include <qradiobutton.h>
 #include <qpushbutton.h>
 #include <qhbox.h>
@@ -142,24 +141,6 @@ public:
 
 protected:
     virtual bool isRenderButton() const { return true; }
-};
-
-// -------------------------------------------------------------------------
-
-class RenderCheckBox : public RenderButton
-{
-    Q_OBJECT
-public:
-    RenderCheckBox(DOM::HTMLInputElementImpl* node);
-
-    virtual const char *renderName() const { return "RenderCheckBox"; }
-    virtual void updateFromElement();
-    virtual void calcMinMaxWidth();
-
-    QCheckBox *widget() const { return static_cast<QCheckBox*>(m_widget); }
-
-public slots:
-    virtual void slotStateChanged(int state);
 };
 
 // -------------------------------------------------------------------------
--- third_party/WebKit/WebCore/kwq/KWQButton.mm
+++ third_party/WebKit/WebCore/kwq/KWQButton.mm
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "KWQButton.h"
 
 #import "KWQAssertions.h"
-#import "KWQCheckBox.h"
 #import "KWQExceptions.h"
 #import "KWQKHTMLPart.h"
 #import "KWQNSViewExtras.h"
