5b13a3affbc8866bef8223da669b3bc422285a51
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-05 18:22:43 +0000

    Reviewed by kocienda
            Fix for:
            - <rdar://problem/4132360> HIM-8A428:Space key works like Return key in Mail application.
            - <rdar://problem/4122708> Mail: when composing new mail with a Korean input method, the first space doesn't work
    
            If the user terminates marked text with a space, a string including marked characters and a normal,
            breaking space, is passed to WebCore in order to replace the previous marked sequence.  The fix ensures
            that the space becomes a non breaking space if necessary.
    
            Test cases added:
            * manual-tests/korean-input-space.html
    
            * khtml/editing/insert_text_command.cpp:
            (khtml::InsertTextCommand::input):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10051 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/htmlediting.h
+++ third_party/WebKit/WebCore/khtml/editing/htmlediting.h
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "selection.h"
 #include "shared.h"
 
+#define NON_BREAKING_SPACE 0xa0
+
 namespace DOM {
     class CSSMutableStyleDeclarationImpl;
     class CSSProperty;
