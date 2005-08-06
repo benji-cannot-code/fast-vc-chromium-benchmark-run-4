bc0f5708400a37baaba892644be4eed98f31fa56
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-06 01:16:25 +0000

    -fixed <rdar://problem/4207220> REGRESSION (DENVER): Crash occurs         after clicking on Hangman applet
            Reviewed by darin.
    
            * kjs/object.cpp:
            (KJS::ObjectImp::hasProperty): added check for null prototype.
    
            FIXME: The long-term plan is to make runtime objects use JS Null()
            instead of null pointers, which will allow us to eliminate null
            checks, improving performance.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/object.cpp
@@ -327,7 +327,7 @@ bool ObjectImp::hasProperty(ExecState *exec, const Identifier &propertyName) con
   if (hasOwnProperty(exec, propertyName))
     return true;
 
-  if (_proto->dispatchType() != ObjectType) {
+  if (!_proto || _proto->dispatchType() != ObjectType) {
     return false;
   }
 
@@ -340,7 +340,7 @@ bool ObjectImp::hasProperty(ExecState *exec, unsigned propertyName) const
     if (hasOwnProperty(exec, propertyName))
       return true;
 
-    if (_proto->dispatchType() != ObjectType) {
+    if (!_proto || _proto->dispatchType() != ObjectType) {
       return false;
     }
 
