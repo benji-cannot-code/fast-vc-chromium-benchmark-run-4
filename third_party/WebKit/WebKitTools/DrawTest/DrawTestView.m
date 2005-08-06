bbbeb7a8e344466e984b99c4a5e404a744931a7b
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-06 08:44:31 +0000

    Bug #: 4252 Submitted by: eseidel Reviewed by: darin         * DrawTest/DrawTestView.m:         (-[DrawTestView toggleFilterSupport:]): missing negation.         One half of fix for toggling filter support.         http://bugzilla.opendarwin.org/show_bug.cgi?id=4252
    git-svn-id: svn://svn.chromium.org/blink/trunk@10067 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DrawTest/DrawTestView.m
+++ third_party/WebKit/WebKitTools/DrawTest/DrawTestView.m
@@ -90,7 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (IBAction)toggleFilterSupport:(id)sender
 {
-    [DrawView setFilterSupportEnabled:[DrawView isFilterSupportEnabled]];
+    [DrawView setFilterSupportEnabled:![DrawView isFilterSupportEnabled]];
     [self setNeedsDisplay:YES];
 }
 
