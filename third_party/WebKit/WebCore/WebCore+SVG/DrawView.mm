70fb733a6ef1123d451e781da8e0d64759c1ac9b
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-06 08:49:14 +0000

    Bug #: 4252 Submitted by: eseidel Reviewed by: darin         * WebCore+SVG/DrawView.mm:         (+[DrawView setFilterSupportEnabled:]): Moved from DrawViewPrivate         (+[DrawView isFilterSupportEnabled]): Moved from DrawViewPrivate         One half of fix for toggling filter support.         http://bugzilla.opendarwin.org/show_bug.cgi?id=4252
    git-svn-id: svn://svn.chromium.org/blink/trunk@10068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCore+SVG/DrawView.mm
+++ third_party/WebKit/WebCore/WebCore+SVG/DrawView.mm
@@ -93,16 +93,6 @@ typedef enum {
 
 @implementation DrawViewPrivate
 
-+ (void)setFilterSupportEnabled:(BOOL)enabled
-{
-    KRenderingDeviceQuartz::setFiltersEnabled(enabled);
-}
-
-+ (BOOL)isFilterSupportEnabled
-{
-    return KRenderingDeviceQuartz::filtersEnabled();
-}
-
 - (id)initWithDrawView:(DrawView *)view
 {
     if ((self = [super init])) {
@@ -191,6 +181,16 @@ NSArray *DrawViewDragTypes;
 {
     //DrawViewDragTypes = [[NSArray arrayWithObject:NSFilenamesPboardType] retain];
     [self setKeys:[NSArray arrayWithObject:@"toolMode"] triggerChangeNotificationsForDependentKey:@"selectedCanvasItems"];
+}
+
++ (void)setFilterSupportEnabled:(BOOL)enabled
+{
+    KRenderingDeviceQuartz::setFiltersEnabled(enabled);
+}
+
++ (BOOL)isFilterSupportEnabled
+{
+    return KRenderingDeviceQuartz::filtersEnabled();
 }
 
 - (id)initWithFrame:(NSRect)frameRect
