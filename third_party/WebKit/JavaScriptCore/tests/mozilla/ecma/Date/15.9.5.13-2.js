86e8cf051ce9b35eb83142d51be15cb5b429b27f
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-08 16:39:57 +0000

    Reviewed by John Sullivan.
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=4325
              Mozilla Date tests have an unnecessary loop that runs 1970 times before each test
    
            * tests/mozilla/ecma/shell.js: Added TIME_YEAR_0 constant.
    
            * tests/mozilla/ecma/Date/15.9.5.10-1.js: Removed the loop and changed code to use the constant.
            * tests/mozilla/ecma/Date/15.9.5.10-10.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-11.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-12.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-13.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-2.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-3.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-4.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-5.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-6.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-7.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-8.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.10-9.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.11-2.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.12-1.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.12-2.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.12-3.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.12-4.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.12-5.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.12-6.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.12-7.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.12-8.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.13-2.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.13-8.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.14.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.15.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.16.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.17.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.18.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.19.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.20.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.21-1.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.21-2.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.21-3.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.21-4.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.21-5.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.21-6.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.21-7.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.21-8.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.22-1.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.22-2.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.22-3.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.22-4.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.22-5.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.22-6.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.22-7.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.22-8.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.23-4.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.23-5.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.23-6.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.23-7.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.23-8.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.23-9.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.5.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.6.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.7.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.8.js: Ditto.
            * tests/mozilla/ecma/Date/15.9.5.9.js: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10088 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/tests/mozilla/ecma/Date/15.9.5.13-2.js
+++ third_party/WebKit/JavaScriptCore/tests/mozilla/ecma/Date/15.9.5.13-2.js
@@ -44,12 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     var TZ_ADJUST = TZ_DIFF * msPerHour;
 
-    // calculate time for year 0
-    for ( var time = 0, year = 1969; year >= 0; year-- ) {
-        time -= TimeInYear(year);
-    }
 
-    addTestCase( time );
+    addTestCase( TIME_YEAR_0 );
     test();
 function addTestCase( t ) {
     for ( var m = 0; m < 12; m++ ) {
