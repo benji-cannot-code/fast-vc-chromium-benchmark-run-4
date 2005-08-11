217c61edf017e2baae3e38f8af8a81dd3aaeab7a
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-11 09:39:49 +0000

    WebCore:
            At Least Rougly Glanced At by Anders.
    
    	- fix change with totally breaks the layout tests.
    
    	Changed all the tests below to avoid dumping more than once -
    	since this now prints more than one separator, it totally throws
    	off the layout test driver so it needs to be avoided.
    
            * layout-tests/dom/html/level2/html/HTMLFormElement10-expected.txt:
            * layout-tests/dom/html/level2/html/HTMLFormElement10.html:
            * layout-tests/dom/html/level2/html/HTMLFormElement10.js:
            * layout-tests/fast/events/onunload-expected.txt:
            * layout-tests/fast/events/onunload.html:
            * layout-tests/fast/js/location-assign-expected.txt:
            * layout-tests/fast/js/location-assign.html:
    
    WebKitTools:
    
            At Least Rougly Glanced At by Anders.
    
    	- fix change with totally breaks the layout tests.
    
            * DumpRenderTree/DumpRenderTree.m:
            (-[WaitUntilDoneDelegate webView:didCommitLoadForFrame:]): Make tests unready if you start another
    	load, to avoid dumping twice accidentally.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10134 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/dom/html/level2/html/HTMLFormElement10.js
+++ third_party/WebKit/LayoutTests/dom/html/level2/html/HTMLFormElement10.js
@@ -96,6 +96,10 @@ function HTMLFormElement10() {
       nodeList = doc.getElementsByTagName("form");
       assertSize("Asize",1,nodeList);
 testNode = nodeList.item(0);
+
+      if (window.layoutTestController)
+          layoutTestController.waitUntilDone();
+
       testNode.submit();
       
 }
