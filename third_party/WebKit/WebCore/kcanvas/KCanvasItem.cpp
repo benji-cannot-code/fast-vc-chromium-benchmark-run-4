94652972565a663f3a2e808aa88a248ac4471e98
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-16 09:17:41 +0000

    Bug #: 4445 Submitted by: Tobias Lidskog <tobiaslidskog@mac.com> Reviewed by: eseidel         * kcanvas/KCanvasItem.cpp:         (KCanvasItem::bbox):         (KCanvasItem::invalidate):         Now caches both stroke and fill bboxes for better performance.         http://bugzilla.opendarwin.org/show_bug.cgi?id=4445
    git-svn-id: svn://svn.chromium.org/blink/trunk@10212 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kcanvas/KCanvasItem.cpp
+++ third_party/WebKit/WebCore/kcanvas/KCanvasItem.cpp
@@ -66,7 +66,7 @@ public:
 	int zIndex;
 	KCanvasUserData path, userData;
 
-	QRect bbox;
+	QRect fillBBox, strokeBbox;
 
 	KCanvasContainer *parent;
 	KCanvasItem *prev;
@@ -120,19 +120,22 @@ bool KCanvasItem::strokeContains(const QPoint &p) const
 
 QRect KCanvasItem::bbox(bool includeStroke) const
 {
-	if(!d->bbox.isValid())
-	{
-		if(d->path && canvas() && canvas()->renderingDevice())
-		{
-			QRect bbox = bboxPath(includeStroke);
-			if(!includeStroke)
-				return bbox;
+    QRect result;
+    
+    if (!d->path || !canvas() || !canvas()->renderingDevice())
+        return result;
 
-			d->bbox = bbox; // cache it
-		}
-	}
-
-	return d->bbox;
+    if (includeStroke) {
+        if(!d->strokeBbox.isValid())
+            d->strokeBbox = bboxPath(true);
+        result = d->strokeBbox;
+    } else {
+        if(!d->fillBBox.isValid())
+            d->fillBBox = bboxPath(false);
+        result = d->fillBBox;
+    }
+    
+    return result;
 }
 
 bool KCanvasItem::hitsPath(const QPoint &hitPoint, bool fill) const
@@ -191,7 +194,8 @@ void KCanvasItem::invalidate() const
 	if(d->canvas)
 	{
 		d->canvas->invalidate(this);
-		d->bbox = QRect();
+		d->fillBBox = QRect();
+                d->strokeBbox = QRect();
 	}
 }
 
