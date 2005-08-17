6677868577146421806b5063e24186f4bb9e7c8d
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-17 01:07:26 +0000

    Reviewed by Geoff.
            - changed main_thread_malloc so we don't have two conflicting versions of the same function
              This fixes a crash I was seeing running the WebKit tests.
    
            * khtml/misc/main_thread_malloc.h: Took out all the ifdefs from this header.
            * khtml/misc/main_thread_malloc.cpp: Added non-NDEBUG versions of the functions that just call
            the system malloc, and put the NDEBUG versions in an #else.
    
            * WebCore-tests.exp: Export the new stuff so the tests still work.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10223 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/misc/main_thread_malloc.cpp
+++ third_party/WebKit/WebCore/khtml/misc/main_thread_malloc.cpp
@@ -225,7 +225,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     probably don't want to touch unless you are extending or adapting malloc.
 */
 
+#include "main_thread_malloc.h"
+
 namespace khtml {
+
+#ifndef NDEBUG
+
+// In debugging builds, use the system malloc for its debugging features.
+
+void *main_thread_malloc(size_t n)
+{
+    return malloc(n);
+}
+
+void *main_thread_calloc(size_t n_elements, size_t element_size)
+{
+    return calloc(n_elements, element_size);
+}
+
+void main_thread_free(void* p)
+{
+    free(p);
+}
+
+void *main_thread_realloc(void* p, size_t n)
+{
+    return realloc(p, n);
+}
+
+#else
 
 /*
   WIN32 sets up defaults for MS environment and compilers.
@@ -5414,7 +5442,9 @@ static int cpuinfo (int whole, CHUNK_SIZE_T  *kernel, CHUNK_SIZE_T  *user) {
 
 #endif /* WIN32 */
 
-};  /* end of namespace KJS */
+#endif
+
+}  /* end of namespace KJS */
 
 /* ------------------------------------------------------------
 History:
