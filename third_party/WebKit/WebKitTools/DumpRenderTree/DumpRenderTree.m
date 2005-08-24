f2727a4397161e0e45edc95256a8d2a0d5815796
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-24 18:53:46 +0000

    Reviewed by Maciej.
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=4625
              DumpRenderTree --pixel-tests crash
            - landed some other small changes I had sitting in my tree
    
            * DumpRenderTree/DumpRenderTree.m:
            (main): Don't bother saving and restoring the preferences. Not sure
            why this was ever done since the preferences are specific to DumpRenderTree.
            Clear delegates before releasing the WebView, because you can't count on the
            order of object deallocation.
            (dumpRenderTree): Fix code that releases a string before storing it in a
            global variable.
    
            * Scripts/run-webkit-tests: Use "-s" rather than a function to get the size
            of a file. Fix lots of cases that were using tabs for indenting to use spaces instead.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10315 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -55,8 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 static void dumpRenderTree(const char *filename);
-NSString *md5HashStringForBitmap(NSBitmapImageRep *bitmap);
-
+static NSString *md5HashStringForBitmap(NSBitmapImageRep *bitmap);
 
 static volatile BOOL done;
 static WebFrame *frame;
@@ -73,17 +72,6 @@ int main(int argc, const char *argv[])
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-    WebPreferences *preferences = [WebPreferences standardPreferences];
-    
-    NSString *standardFontFamily = [preferences standardFontFamily];
-    NSString *fixedFontFamily = [preferences fixedFontFamily];
-    NSString *serifFontFamily = [preferences serifFontFamily];
-    NSString *sansSerifFontFamily = [preferences sansSerifFontFamily];
-    NSString *cursiveFontFamily = [preferences cursiveFontFamily];
-    NSString *fantasyFontFamily = [preferences fantasyFontFamily];
-    int defaultFontSize = [preferences defaultFontSize];
-    int defaultFixedFontSize = [preferences defaultFixedFontSize];
-    int minimumFontSize = [preferences minimumFontSize];
     int width = 800;
     int height = 600;
     
@@ -95,8 +83,9 @@ int main(int argc, const char *argv[])
         {"notree", no_argument, &dumpTree, NO},
         {NULL, 0, NULL, 0}
     };
-    int option;
 
+    WebPreferences *preferences = [WebPreferences standardPreferences];
+    
     [preferences setStandardFontFamily:@"Times"];
     [preferences setFixedFontFamily:@"Courier"];
     [preferences setSerifFontFamily:@"Times"];
@@ -107,6 +96,7 @@ int main(int argc, const char *argv[])
     [preferences setDefaultFixedFontSize:13];
     [preferences setMinimumFontSize:9];
 
+    int option;
     while ((option = getopt_long(argc, (char * const *)argv, "", options, NULL)) != -1)
         switch (option) {
             case 'w':
@@ -163,16 +153,10 @@ int main(int argc, const char *argv[])
         }
     }
     
-    [preferences setStandardFontFamily:standardFontFamily];
-    [preferences setFixedFontFamily:fixedFontFamily];
-    [preferences setSerifFontFamily:serifFontFamily];
-    [preferences setSansSerifFontFamily:sansSerifFontFamily];
-    [preferences setCursiveFontFamily:cursiveFontFamily];
-    [preferences setFantasyFontFamily:fantasyFontFamily];
-    [preferences setDefaultFontSize:defaultFontSize];
-    [preferences setDefaultFixedFontSize:defaultFixedFontSize];
-    [preferences setMinimumFontSize:minimumFontSize];
-    
+    [webView setFrameLoadDelegate:nil];
+    [webView setEditingDelegate:nil];
+    [webView setUIDelegate:nil];
+
     [webView release];
     [delegate release];
     [editingDelegate release];
@@ -458,7 +442,6 @@ static void dumpRenderTree(const char *filename)
     }
 
     CFURLRef URL = CFURLCreateWithFileSystemPath(NULL, filenameString, kCFURLPOSIXPathStyle, FALSE);
-    CFRelease(filenameString);
     if (URL == NULL) {
         fprintf(stderr, "can't turn %s into a CFURL\n", filename);
         return;
@@ -469,7 +452,9 @@ static void dumpRenderTree(const char *filename)
     waitToDump = NO;
     dumpAsText = NO;
     dumpTitleChanges = NO;
-    currentTest = (NSString *) filenameString;
+    if (currentTest != nil)
+        CFRelease(currentTest);
+    currentTest = (NSString *)filenameString;
 
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     [frame loadRequest:[NSURLRequest requestWithURL:(NSURL *)URL]];
@@ -500,4 +485,3 @@ NSString *md5HashStringForBitmap(NSBitmapImageRep *bitmap)
 
     return [NSString stringWithUTF8String:hex];
 }
-
