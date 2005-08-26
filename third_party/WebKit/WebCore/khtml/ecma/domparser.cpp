f17c3eaede14980c4bf6d4c4fa34aac127f52cea
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-26 19:14:27 +0000

    Fix copyright header
    git-svn-id: svn://svn.chromium.org/blink/trunk@10343 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/domparser.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/domparser.cpp
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // -*- c-basic-offset: 2 -*-
 /*
  *  This file is part of the KDE libraries
- *  Copyright (C) 2003 Apple Computer, Inc.
+ *  Copyright (C) 2005 Anders Carlsson (andersca@mac.com)
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
