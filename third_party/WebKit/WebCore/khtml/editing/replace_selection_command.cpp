22756618c855cd27c8907bd5b10af6cc1c58ed67
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-08-29 23:39:17 +0000

    Written by Justin Garcia         Reviewed by me and Darin Adler
            Test cases added: none, doesn't affect layout
    
            - fixed <rdar://problem/4232303> many objects leaked from HTMLTokenizer::parseTag()
            within -[WebView paste:], seen running webkit tests (probably affects Mail)
    
            * khtml/editing/replace_selection_command.cpp:
            (khtml::ReplacementFragment::removeNode):
            guard removeChild call with ref/deref, so if this is the last reference it won't
            be left floating, and will properly self-destruct
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10390 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/replace_selection_command.cpp
+++ third_party/WebKit/WebCore/khtml/editing/replace_selection_command.cpp
@@ -270,7 +270,9 @@ void ReplacementFragment::removeNode(NodeImpl *node)
         return;
         
     int exceptionCode = 0;
+    node->ref();
     parent->removeChild(node, exceptionCode);
+    node->deref();
     ASSERT(exceptionCode == 0);
 }
 
