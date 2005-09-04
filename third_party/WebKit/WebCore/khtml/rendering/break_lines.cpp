31c88decb3a1136f5d7f408d8add2d0e32a1d2a5
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-04 08:23:40 +0000

    - fixed the build
            * khtml/rendering/break_lines.cpp: Added missing include of <qstring.h>.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/break_lines.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/break_lines.cpp
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "break_lines.h"
 
+#include <qstring.h>
+
 #include <CoreServices/CoreServices.h>
 
 namespace khtml {
