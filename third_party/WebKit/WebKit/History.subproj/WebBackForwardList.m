66401273603757e8c27a7261dc2d6a5a5531563b
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-06 23:46:39 +0000

    - fixed build bustage from last checkin.
            Reviewed by haytt.
    
            * History.subproj/WebBackForwardList.m:
            (-[WebBackForwardList pageCacheSize]): updated debug printf since we no
            longer have a variable called 'multiplier'
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
+++ third_party/WebKit/WebKit/History.subproj/WebBackForwardList.m
@@ -322,7 +322,7 @@ static BOOL loggedPageCacheSize = NO;
 
 #ifndef NDEBUG
         if (!loggedPageCacheSize){
-            LOG (CacheSizes, "Page cache size set to %d pages.", s * multiplier);
+            LOG (CacheSizes, "Page cache size set to %d pages.", _private->pageCacheSize);
             loggedPageCacheSize = YES;
         }
 #endif
