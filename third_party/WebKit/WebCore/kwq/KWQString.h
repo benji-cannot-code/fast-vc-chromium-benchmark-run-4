f5bd71b534642259a5139136798a1cee8e022046
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-12 03:40:38 +0000

    Reviewed by Eric.
            - changed back the name of a couple of string() functions that were renamed to qstring() by accident
    
            * kwq/KWQString.h: (QConstString::string): Changed function name back to string() from qstring().
            This is a function from Qt, so the name needs to match Qt -- that's just how KWQ works.
    
            * khtml/xml/dom_atomicstringlist.h: Changed the name of the function that returns the current
            AtomicString to string() back from qstring()!
    
            * khtml/css/cssstyleselector.cpp:
            (khtml::CSSStyleSelector::matchRules): Use string() instead of qstring().
            (khtml::checkPseudoState): Ditto.
            (khtml::CSSStyleSelector::checkOneSelector): Ditto.
            * khtml/dom/dom_string.cpp: (DOM::DOMString::percentage): Ditto.
            * khtml/html/htmltokenizer.cpp: (khtml::HTMLTokenizer::finish): Ditto.
            * khtml/xml/dom_stringimpl.cpp:
            (DOM::DOMStringImpl::DOMStringImpl): Small tweak to streamline a tiny bit by using an already-computed
            boolean instead of re-checking a couple of data members.
            (DOM::parseLength): Use string() instead of qstring().
            (DOM::DOMStringImpl::toInt): Ditto.
    
            - removed some dead code
    
            * khtml/xml/dom_elementimpl.cpp: Removed XMLElementImpl.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10522 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQString.h
+++ third_party/WebKit/WebCore/kwq/KWQString.h
@@ -753,7 +753,7 @@ class QConstString : private QString {
 public:
     QConstString(const QChar *, uint);
     ~QConstString();
-    const QString &qstring() const { return *this; }
+    const QString &string() const { return *this; }
 };
 
 #endif
