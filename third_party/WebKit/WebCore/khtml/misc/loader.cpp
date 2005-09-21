0a20fcf84be285265668f34adb94cd1386f3ea97
vicki <vicki@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-21 21:05:57 +0000

    Changes by Romain Brestac, reviewed by Darin.
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=3418
            (unicode characters not preserved after a xslt transformation)
    
            * khtml/misc/loader.cpp:
            (CachedXSLStyleSheet::CachedXSLStyleSheet):
            (CachedXSLStyleSheet::data):
            * khtml/misc/loader.h:
            * khtml/xsl/xslt_processorimpl.cpp:
            (DOM::XSLTProcessorImpl::addToResult):
            (DOM::XSLTProcessorImpl::documentFromXMLDocPtr):
    
    	Test cases added:
    
            * layout-tests/fast/xsl/resources/unicode.xsl: Added.
            * layout-tests/fast/xsl/resources/xslt-enc-cyr.xsl: Added.
            * layout-tests/fast/xsl/resources/xslt-enc.xsl: Added.
            * layout-tests/fast/xsl/resources/xslt-enc16.xsl: Added.
            * layout-tests/fast/xsl/xslt-enc-cyr-expected.checksum: Added.
            * layout-tests/fast/xsl/xslt-enc-cyr-expected.png: Added.
            * layout-tests/fast/xsl/xslt-enc-cyr-expected.txt: Added.
            * layout-tests/fast/xsl/xslt-enc-cyr.xml: Added.
            * layout-tests/fast/xsl/xslt-enc-expected.checksum: Added.
            * layout-tests/fast/xsl/xslt-enc-expected.png: Added.
            * layout-tests/fast/xsl/xslt-enc-expected.txt: Added.
            * layout-tests/fast/xsl/xslt-enc.xml: Added.
            * layout-tests/fast/xsl/xslt-enc16-expected.checksum: Added.
            * layout-tests/fast/xsl/xslt-enc16-expected.png: Added.
            * layout-tests/fast/xsl/xslt-enc16-expected.txt: Added.
            * layout-tests/fast/xsl/xslt-enc16.xml: Added.
            * layout-tests/fast/xsl/xslt-enc16to16-expected.checksum: Added.
            * layout-tests/fast/xsl/xslt-enc16to16-expected.png: Added.
            * layout-tests/fast/xsl/xslt-enc16to16-expected.txt: Added.
            * layout-tests/fast/xsl/xslt-enc16to16.xml: Added.
            * layout-tests/fast/xsl/xslt_unicode-expected.checksum: Added.
            * layout-tests/fast/xsl/xslt_unicode-expected.png: Added.
            * layout-tests/fast/xsl/xslt_unicode-expected.txt: Added.
            * layout-tests/fast/xsl/xslt_unicode.xml: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/misc/loader.cpp
+++ third_party/WebKit/WebCore/khtml/misc/loader.cpp
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <kdebug.h>
 #include "khtml_factory.h"
 #include "khtml_part.h"
+#include "decoder.h"
 
 #include "html/html_documentimpl.h"
 #include "css/css_stylesheetimpl.h"
@@ -1099,7 +1100,7 @@ CachedXSLStyleSheet::CachedXSLStyleSheet(DocLoader* dl, const DOMString &url, KI
     // load the file
     Cache::loader()->load(dl, this, false);
     m_loading = true;
-    m_codec = QTextCodec::codecForName("iso8859-1");
+    m_decoder = new Decoder;
 }
 
 void CachedXSLStyleSheet::ref(CachedObjectClient *c)
@@ -1123,7 +1124,7 @@ void CachedXSLStyleSheet::data(QBuffer &buffer, bool eof)
     if(!eof) return;
     buffer.close();
     setSize(buffer.buffer().size());
-    QString data = m_codec->toUnicode( buffer.buffer().data(), size() );
+    QString data = m_decoder->decode( buffer.buffer().data(), size() );
     m_sheet = DOMString(data);
     m_loading = false;
     
--- third_party/WebKit/WebCore/khtml/misc/loader.h
+++ third_party/WebKit/WebCore/khtml/misc/loader.h
@@ -87,6 +87,7 @@ namespace khtml
     class CachedObject;
     class Request;
     class DocLoader;
+    class Decoder;
 
  #define MAX_LRU_LISTS 20
     
@@ -432,7 +433,7 @@ public:
         
 protected:
         DOM::DOMString m_sheet;
-        QTextCodec* m_codec;
+        Decoder* m_decoder;
     };
 #endif
     
