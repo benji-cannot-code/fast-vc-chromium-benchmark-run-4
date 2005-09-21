6b4a8496f5ec91855fa7278fa222b7e41e6ec6bf
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-21 01:10:54 +0000

    - Oops. The 4263434 change was only appropriate on the branch. Rolling out.
            Reviewed by eric.
    
            * kjs/internal.cpp:
            (KJS::InterpreterImp::mark):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10586 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/internal.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/internal.cpp
@@ -645,7 +645,7 @@ void InterpreterImp::mark()
     _context->mark();
   if (global)
       global->mark();
-  if (globExec && globExec._exception)
+  if (globExec._exception)
       globExec._exception->mark();
 }
 
