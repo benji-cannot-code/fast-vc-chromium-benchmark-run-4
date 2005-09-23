6a04d14051267154dc98c77efb60b22dcb8277fb
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-23 18:20:30 +0000

    Reviewed by Eric.
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=4309
              CSSStyleSheet objects don't get Obj-C DOMCSSStyleSheet wrappers
    
            * kwq/DOM-CSS.mm:
            (-[DOMStyleSheet _initWithStyleSheetImpl:]): Changed the name of this method to be consistent
            with other method names in the DOM wrappers. The old name had an extraneous "DOM" in it.
            (+[DOMStyleSheet _DOMStyleSheetWithImpl:]): Create a wrapper of an appropriate class
            that matches the actual style sheet. This is analogous to what +[DOMNode _nodeWithImpl:] does.
            (+[DOMCSSStyleSheet _CSSStyleSheetWithImpl:]): Call through to the base class. The only difference
            here is the static type of the parameter, which ensures a compile-time error if you pass the
            wrong type of pointer.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10604 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/DOM-CSS.mm
+++ third_party/WebKit/WebCore/kwq/DOM-CSS.mm
@@ -170,7 +170,7 @@ using DOM::StyleSheetListImpl;
 
 @implementation DOMStyleSheet (WebCoreInternal)
 
-- (id)_initWithDOMStyleSheetImpl:(StyleSheetImpl *)impl
+- (id)_initWithStyleSheetImpl:(StyleSheetImpl *)impl
 {
     [super _init];
     _internal = DOM_cast<DOMObjectInternal *>(impl);
@@ -189,7 +189,12 @@ using DOM::StyleSheetListImpl;
     if (cachedInstance)
         return [[cachedInstance retain] autorelease];
     
-    return [[[self alloc] _initWithDOMStyleSheetImpl:impl] autorelease];
+    Class wrapperClass;
+    if (impl->isCSSStyleSheet())
+        wrapperClass = [DOMCSSStyleSheet class];
+    else
+        wrapperClass = [DOMStyleSheet class];
+    return [[[wrapperClass alloc] _initWithStyleSheetImpl:impl] autorelease];
 }
 
 @end
@@ -263,22 +268,6 @@ using DOM::StyleSheetListImpl;
 
 @implementation DOMCSSStyleSheet
 
-- (void)dealloc
-{
-    if (_internal) {
-        DOM_cast<CSSStyleSheetImpl *>(_internal)->deref();
-    }
-    [super dealloc];
-}
-
-- (void)finalize
-{
-    if (_internal) {
-        DOM_cast<CSSStyleSheetImpl *>(_internal)->deref();
-    }
-    [super finalize];
-}
-
 - (CSSStyleSheetImpl *)_CSSStyleSheetImpl
 {
     return DOM_cast<CSSStyleSheetImpl *>(_internal);
@@ -313,26 +302,9 @@ using DOM::StyleSheetListImpl;
 
 @implementation DOMCSSStyleSheet (WebCoreInternal)
 
-- (id)_initWithCSSStyleSheetImpl:(CSSStyleSheetImpl *)impl
-{
-    [super _init];
-    _internal = DOM_cast<DOMObjectInternal *>(impl);
-    impl->ref();
-    addDOMWrapper(self, impl);
-    return self;
-}
-
 + (DOMCSSStyleSheet *)_CSSStyleSheetWithImpl:(CSSStyleSheetImpl *)impl
 {
-    if (!impl)
-        return nil;
-    
-    id cachedInstance;
-    cachedInstance = getDOMWrapper(impl);
-    if (cachedInstance)
-        return [[cachedInstance retain] autorelease];
-    
-    return [[[self alloc] _initWithCSSStyleSheetImpl:impl] autorelease];
+    return (DOMCSSStyleSheet *)[DOMStyleSheet _DOMStyleSheetWithImpl:impl];
 }
 
 @end
