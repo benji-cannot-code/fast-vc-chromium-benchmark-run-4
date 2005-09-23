4b46eee26e0271270c792fc82a176a4d8b8b1967
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-23 18:24:58 +0000

    Reviewed by Eric.
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=4310
              "sheet" method is unimplemented in Obj-C DOM API
    
            * kwq/DOM-CSS.mm: (-[DOMObject sheet]): Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10605 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/DOM-CSS.mm
+++ third_party/WebKit/WebCore/kwq/DOM-CSS.mm
@@ -34,10 +34,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "css_valueimpl.h"
 #import "css_value.h"
 #import "dom_string.h"
+#import "dom_xmlimpl.h"
 #import "KWQColor.h"
 #import "shared.h"
 #import "dom_stringimpl.h"
 #import "dom2_viewsimpl.h"
+#import "html_headimpl.h"
 
 #import "DOMInternal.h"
 #import "KWQAssertions.h"
@@ -59,7 +61,10 @@ using DOM::CSSStyleSheetImpl;
 using DOM::CSSValueImpl;
 using DOM::CSSValueListImpl;
 using DOM::DOMString;
+using DOM::HTMLLinkElementImpl;
+using DOM::HTMLStyleElementImpl;
 using DOM::MediaListImpl;
+using DOM::ProcessingInstructionImpl;
 using DOM::RectImpl;
 using DOM::StyleSheetImpl;
 using DOM::StyleSheetListImpl;
@@ -2505,13 +2510,22 @@ void removeWrapperForRGB(QRgb value)
 
 //------------------------------------------------------------------------------------------
 
-
 @implementation DOMObject (DOMLinkStyle)
 
 - (DOMStyleSheet *)sheet
 {
-    ERROR("unimplemented");
-    return nil;
+    StyleSheetImpl *sheet;
+
+    if ([self isKindOfClass:[DOMProcessingInstruction class]])
+        sheet = static_cast<ProcessingInstructionImpl *>([(DOMProcessingInstruction *)self _nodeImpl])->sheet();
+    else if ([self isKindOfClass:[DOMHTMLLinkElement class]])
+        sheet = static_cast<HTMLLinkElementImpl *>([(DOMHTMLLinkElement *)self _nodeImpl])->sheet();
+    else if ([self isKindOfClass:[DOMHTMLStyleElement class]])
+        sheet = static_cast<HTMLStyleElementImpl *>([(DOMHTMLStyleElement *)self _nodeImpl])->sheet();
+    else
+        return nil;
+
+    return [DOMStyleSheet _DOMStyleSheetWithImpl:sheet];
 }
 
 @end
