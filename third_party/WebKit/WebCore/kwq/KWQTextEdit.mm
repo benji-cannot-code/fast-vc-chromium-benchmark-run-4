8a2e0bdd20ba9f4fc6c150cbac131a2a31d19802
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-24 11:05:34 +0000

    Reviewed by Eric.         Landed by Darin (after taking tabs out).
            - worked around problems compiling with various versions of gcc 4
              http://bugzilla.opendarwin.org/show_bug.cgi?id=5071
              http://bugzilla.opendarwin.org/show_bug.cgi?id=5086
    
            * kwq/KWQLineEdit.mm:
            (QLineEdit::setCursorPosition): Don't use NSMakeRange.
            (QLineEdit::setSelection): Ditto.
            * kwq/KWQPainter.mm:
            (QPainter::drawFloatPixmap): Rearrange things so we don't get a warning.
            * kwq/KWQTextEdit.mm:
            (QTextEdit::selectionEnd): Don't use NSMaxRange.
            (QTextEdit::setSelectionRange): Don't use NSMakeRange.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10613 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQTextEdit.mm
+++ third_party/WebKit/WebCore/kwq/KWQTextEdit.mm
@@ -202,7 +202,7 @@ int QTextEdit::selectionEnd()
     NSRange range = [textView selectedRange];
     if (range.location == NSNotFound)
         return 0;
-    return NSMaxRange(range);
+    return range.location + range.length; // Use NSMaxRange when 4213314 is fixed
     KWQ_UNBLOCK_EXCEPTIONS;
     
     return 0;
@@ -315,7 +315,8 @@ void QTextEdit::setSelectionRange(int start, int length)
     if (newStart + newLength > maxlen) {
         newLength = maxlen - newStart;
     }
-    [textView setSelectedRange:NSMakeRange(newStart, newLength)];
+    NSRange tempRange = {newStart, newLength}; // 4213314
+    [textView setSelectedRange:tempRange];
     KWQ_UNBLOCK_EXCEPTIONS;
 }
 
