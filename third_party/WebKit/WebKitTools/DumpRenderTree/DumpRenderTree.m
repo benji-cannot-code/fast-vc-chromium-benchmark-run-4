2561ff59f09ef75b69e8cca64fd6f19f6a37a5f0
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-24 13:50:40 +0000

    WebCore:
            - patch for <http://bugzilla.opendarwin.org/show_bug.cgi?id=4963>
              "Would like to simulate human interaction with webview"
    
            Test cases added:
            * layout-tests/editing/selection/fake-drag.html: Added.
            * layout-tests/editing/selection/fake-doubleclick: Added.
    
    WebKitTools:
    
            Reviewed by Maciej.
            Landed by Darin.
    
            - patch for <http://bugzilla.opendarwin.org/show_bug.cgi?id=4963>
            "Would like to simulate human interaction with webview"
    
            * DumpRenderTree/DumpRenderTree.m:
            (-[WaitUntilDoneDelegate webView:windowScriptObjectAvailable:]):
            (+[EventSendingController isSelectorExcludedFromWebScript:]):
            (+[EventSendingController webScriptNameForSelector:]):
            (-[EventSendingController init]):
            (-[EventSendingController mouseDown]):
            (-[EventSendingController mouseUp]):
            (-[EventSendingController mouseMoveToX:Y:]):
            Add eventSender javascript object, that sends fake mouse events to the webview.
    
            * DumpRenderTree/DumpRenderTree.xcodeproj/project.pbxproj:
            link with Carbon.framework
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10620 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -41,6 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebPreferences.h>
 #import <WebKit/WebView.h>
 
+#import <Carbon/Carbon.h> // for GetCurrentEventTime()
+
 #define COMMON_DIGEST_FOR_OPENSSL
 #import <CommonCrypto/CommonDigest.h>
 #import <getopt.h>
@@ -52,6 +54,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 @interface LayoutTestController : NSObject
+@end
+
+@interface EventSendingController : NSObject
+{
+    NSPoint last;
+    BOOL down;
+    int clickCount;
+    NSTimeInterval lastClick;
+}
+
 @end
 
 static void dumpRenderTree(const char *filename);
@@ -264,6 +276,9 @@ static void dump(void)
     LayoutTestController *ltc = [[LayoutTestController alloc] init];
     [(id)obj setValue:ltc forKey:@"layoutTestController"];
     [ltc release];
+    EventSendingController *esc = [[EventSendingController alloc] init];
+    [(id)obj setValue:esc forKey:@"eventSender"];
+    [esc release];
 
 }
 
@@ -440,6 +455,76 @@ static void dump(void)
 - (void)dumpTitleChanges
 {
     dumpTitleChanges = YES;
+}
+
+@end
+
+@implementation EventSendingController
+
++ (BOOL)isSelectorExcludedFromWebScript:(SEL)aSelector
+{
+    if (aSelector == @selector(mouseDown)
+            || aSelector == @selector(mouseUp)
+            || aSelector == @selector(mouseMoveToX:Y:))
+        return NO;
+    return YES;
+}
+
++ (NSString *)webScriptNameForSelector:(SEL)aSelector
+{
+    if(aSelector == @selector(mouseMoveToX:Y:))
+        return @"mouseMoveTo";
+    return nil;
+}
+
+- (id)init
+{
+    last = NSMakePoint(0, 0);
+    down = NO;
+    clickCount = 0;
+    lastClick = 0;
+    return self;
+}
+
+- (void)mouseDown
+{
+    if(GetCurrentEventTime() - lastClick >= 1)
+        clickCount = 1;
+    else
+        clickCount++;
+    NSEvent *event = [NSEvent mouseEventWithType:NSLeftMouseDown location:last modifierFlags:nil timestamp:GetCurrentEventTime() windowNumber:0 context:[NSGraphicsContext currentContext] eventNumber:nil clickCount:clickCount pressure:nil];
+
+    NSView *subView = [[frame webView] hitTest:[event locationInWindow]];
+    if (subView) {
+        [subView mouseDown:event];
+        down = YES;
+    }
+}
+
+- (void)mouseUp
+{
+    NSEvent *event = [NSEvent mouseEventWithType:NSLeftMouseUp location:last modifierFlags:nil timestamp:GetCurrentEventTime() windowNumber:0 context:[NSGraphicsContext currentContext] eventNumber:nil clickCount:clickCount pressure:nil];
+
+    NSView *subView = [[frame webView] hitTest:[event locationInWindow]];
+    if (subView) {
+        [subView mouseUp:event];
+        down = NO;
+        lastClick = [event timestamp];
+    }
+}
+
+- (void)mouseMoveToX:(int)x Y:(int)y
+{
+    last = NSMakePoint(x, [[frame webView] frame].size.height - y);
+    NSEvent *event = [NSEvent mouseEventWithType:(down ? NSLeftMouseDragged : NSMouseMoved) location:last modifierFlags:nil timestamp:GetCurrentEventTime() windowNumber:0 context:[NSGraphicsContext currentContext] eventNumber:nil clickCount:(down ? clickCount : 0) pressure:nil];
+
+    NSView *subView = [[frame webView] hitTest:[event locationInWindow]];
+    if (subView) {
+        if (down)
+            [subView mouseDragged:event];
+        else
+            [subView mouseMoved:event];
+    }
 }
 
 @end
