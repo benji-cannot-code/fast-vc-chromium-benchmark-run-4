7c98326fd06eddcd7791e544aaed34e4e825a839
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-26 22:54:24 +0000

    Bug #: 4907 Submitted by: eseidel Reviewed by: hyatt         * DumpRenderTree/DumpRenderTree.m:         (dump): updated error message         * Scripts/check-dom-results: now includes xhtml         Tool updates for xhtml.         http://bugzilla.opendarwin.org/show_bug.cgi?id=4907
    git-svn-id: svn://svn.chromium.org/blink/trunk@10627 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -200,7 +200,7 @@ static void dump(void)
             result = [frame renderTreeAsExternalRepresentation];
         
         if (!result)
-            puts("error");
+            printf("ERROR: nil result from %s", dumpAsText ? "[documentElement innerText]" : "[frame renderTreeAsExternalRepresentation]");
         else
             fputs([result UTF8String], stdout);
         
