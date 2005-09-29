2041f1ba628f25bd5fd3bfcfe21b78b6128588ce
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-09-29 22:05:12 +0000

    Bug #: 5176 Submitted by: Mitz Pettel <opendarwin.org@mitzpettel.com> Reviewed by: darin         Fix JSC memory smasher in TOT.         http://bugzilla.opendarwin.org/show_bug.cgi?id=5176
            * pcre/pcre_exec.c:
            (match):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10663 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/pcre/pcre_exec.c
+++ third_party/WebKit/JavaScriptCore/pcre/pcre_exec.c
@@ -1810,7 +1810,18 @@ for (;;)
 #if PCRE_UTF16
       int dc;
       ecode += length;
-      GETCHARINC(dc, eptr);
+      switch (md->end_subject - eptr)
+      {
+        case 0:
+          RRETURN(MATCH_NOMATCH);
+        case 1:
+          dc = *eptr++;
+          if (IS_LEADING_SURROGATE(dc))
+            RRETURN(MATCH_NOMATCH);
+          break;
+        default:
+          GETCHARINC(dc, eptr);
+      }
       if (fc != dc) RRETURN(MATCH_NOMATCH);
 #else
       if (length > md->end_subject - eptr) RRETURN(MATCH_NOMATCH);
@@ -1838,7 +1849,9 @@ for (;;)
       ecode++;
       GETUTF8CHARLEN(fc, ecode, length);
 
-#if !PCRE_UTF16
+#if PCRE_UTF16
+      if (md->end_subject - eptr == 0) RRETURN(MATCH_NOMATCH);
+#else
       if (length > md->end_subject - eptr) RRETURN(MATCH_NOMATCH);
 #endif
 
@@ -1862,7 +1875,14 @@ for (;;)
       else
         {
         int dc;
-        GETCHARINC(dc, eptr);
+#if PCRE_UTF16
+        if (md->end_subject - eptr == 1) {
+          dc = *eptr++;
+          if (IS_LEADING_SURROGATE(dc))
+            RRETURN(MATCH_NOMATCH);
+        } else
+#endif
+          GETCHARINC(dc, eptr);
         ecode += length;
 
         /* If we have Unicode property support, we can use it to test the other
