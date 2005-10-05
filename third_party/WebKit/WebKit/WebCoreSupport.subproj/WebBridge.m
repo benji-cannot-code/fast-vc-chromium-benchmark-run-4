eb05ff3076ea29c377903807405d8d88e301a9ce
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-05 21:07:33 +0000

    Reviewed by Eric.
    	<rdar://problem/4158439> Safari appears to hang when sending synchronous XMLHttpRequest that gets no server response
    
    	No testcase - not testable w/o network.
    
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge syncLoadResourceWithURL:customHeaders:postData:finalURL:responseHeaders:statusCode:]): Set a timeout of 10.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10754 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -512,6 +512,7 @@ NSString *WebPluginContainerKey =   @"WebPluginContainer";
     [self canLoadURL:URL fromReferrer:[self referrer] hideReferrer:&hideReferrer];
 
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:URL];
+    [request setTimeoutInterval:10];
 
     if (postData) {
         [request setHTTPMethod:@"POST"];
