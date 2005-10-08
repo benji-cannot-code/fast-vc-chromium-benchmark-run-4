b4f673a507a5b589c8367f89bf21923309fc10f1
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-08 09:18:15 +0000

    Bug #: 5240 Submitted by: Rob Buis  <rwlbuis@xs4all.nl> Reviewed by: eseidel         Fix to handle forward referencing <use> elements.         http://bugzilla.opendarwin.org/show_bug.cgi?id=5240
            * ksvg2/svg/SVGDocumentImpl.cpp:
            (SVGDocumentImpl::executeScripts): delayed close.
            (SVGDocumentImpl::addForwardReference): new function.
            * ksvg2/svg/SVGDocumentImpl.h:
            * ksvg2/svg/SVGUseElementImpl.cpp:
            (SVGUseElementImpl::close): delay close, if missing reference.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10788 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGUseElementImpl.cpp
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGUseElementImpl.cpp
@@ -130,7 +130,10 @@ void SVGUseElementImpl::close()
     KDOM::ElementImpl *targetElement = ownerDocument()->getElementById(targetId.handle());
     SVGElementImpl *target = svg_dynamic_cast(targetElement);
     if (!target)
+    {
+        getDocument()->addForwardReference(this);
         return;
+    }
 
     float _x = x()->baseVal()->value(), _y = y()->baseVal()->value();
     float _w = width()->baseVal()->value(), _h = height()->baseVal()->value();
@@ -180,6 +183,8 @@ void SVGUseElementImpl::close()
         appendChild(dummy);
         dummy->appendChild(root);
     }
+
+    SVGElementImpl::close();
 }
 
 bool SVGUseElementImpl::hasChildNodes() const
