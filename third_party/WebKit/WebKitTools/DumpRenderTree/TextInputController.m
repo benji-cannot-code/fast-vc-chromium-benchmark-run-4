c53b87e23efedaaec3f13f3a948ecc8c8b7fb280
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-09 23:15:32 +0000

    Fixes the following build error when building Deployment: DumpRenderTree/TextInputController.m: In function '-[TextInputController textInput]': DumpRenderTree/TextInputController.m:87: warning: class 'NSView <WebDocumentView>' does not implement the 'NSTextInput' protocol
    git-svn-id: svn://svn.chromium.org/blink/trunk@10803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/TextInputController.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/TextInputController.m
@@ -84,7 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSObject <NSTextInput> *)textInput
 {
-    NSView <WebDocumentView> *view = [[[webView mainFrame] frameView] documentView];
+    NSView <NSTextInput> *view = [[[webView mainFrame] frameView] documentView];
     return [view conformsToProtocol:@protocol(NSTextInput)] ? view : nil;
 }
 
