449d1c5950ca301fa4290945f2390ef4a7f498ce
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-09 04:25:00 +0000

    Reviewed, tweaked, and landed by Darin.
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=5189
              pcre_exec.c fails to compile using MSVC
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=5190
              KJS config.h adjustment for Win32
    
            * kjs/config.h: Make sure HAVE_MMAP and HAVE_SBRK are off for Win32.
            Turn HAVE_ERRNO_H on for Mac OS X. Sort defines so they are easy to compare
            with each other. Remove #undef of DEBUG_COLLECTOR.
            * pcre/pcre_exec.c: (match): Work around strange MSVC complaint by splitting
            the definition of a local variable into a separate declaration and
            initialization.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10798 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/config.h
+++ third_party/WebKit/JavaScriptCore/kjs/config.h
@@ -1,8 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if __APPLE__
 
+#define HAVE_ERRNO_H 1
 #define HAVE_FUNC_ISINF 1
 #define HAVE_FUNC_ISNAN 1
+#define HAVE_MMAP 1
+#define HAVE_SBRK 1
 #define HAVE_STRINGS_H 1
 #define HAVE_SYS_PARAM_H 1
 #define HAVE_SYS_TIME_H 1
@@ -16,31 +19,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #else
 
+#define HAVE_ERRNO_H 1
 #define HAVE_FUNC_ISINF 1
 #define HAVE_FUNC_ISNAN 1
+#define HAVE_MMAP 1
+#define HAVE_SBRK 1
 #define HAVE_STRINGS_H 1
 #define HAVE_SYS_PARAM_H 1
 #define HAVE_SYS_TIME_H 1
 #define TIME_WITH_SYS_TIME 1
-#define HAVE_ERRNO_H 1
 
 #endif
 
 #define HAVE_FUNC_STRTOLL 1
 #define HAVE_ICU 1
 #define HAVE_PCREPOSIX 1
-#define HAVE_STRING_H 1
 #define HAVE_STDINT_H 1
-
-#define HAVE_MMAP 1
-#define HAVE_SBRK 1
+#define HAVE_STRING_H 1
 
 #ifdef __ppc__
 #define WORDS_BIGENDIAN 1
 #endif
-
-/* define to debug garbage collection */
-#undef DEBUG_COLLECTOR
 
 #define KXC_CHANGES 1
 
