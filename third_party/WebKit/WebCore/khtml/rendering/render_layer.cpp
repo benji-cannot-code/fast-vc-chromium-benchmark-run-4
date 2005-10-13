43cf42f6fd9a89b40be1e54c5001c368a23fe89b
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-13 22:45:56 +0000

    Reviewed by hyatt
            <rdar://problem/3643259> contentEditable=true elements do not acquire the caret or focus halo when overflow:scroll
    
            Test cases added:
            * fast/clip/outline-overflowClip
            Test cases changed (since I added the outline rect to the render tree dump)
            * fast/clip/001
            * fast/clip/009
            * fast/clip/010
            * fast/clip/011
            * fast/clip/012
            * fast/clip/013
            * fast/clip/014
            * fast/clip/016
    
            * khtml/rendering/render_layer.cpp:
            Clip the outline by a new rect, which is equal to the foreground rect w/o clipping
            by the overflow rect.
    
            (khtml::RenderLayer::paintLayer):
            (khtml::RenderLayer::hitTestLayer):
            (khtml::RenderLayer::calculateRects):
            * khtml/rendering/render_layer.h:
            * kwq/KWQRenderTreeDebug.cpp:
            (write):
            (writeLayers):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10846 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_layer.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_layer.cpp
@@ -875,8 +875,8 @@ RenderLayer::paintLayer(RenderLayer* rootLayer, QPainter *p,
                         RenderObject *paintingRoot)
 {
     // Calculate the clip rects we should use.
-    QRect layerBounds, damageRect, clipRectToApply;
-    calculateRects(rootLayer, paintDirtyRect, layerBounds, damageRect, clipRectToApply);
+    QRect layerBounds, damageRect, clipRectToApply, outlineRect;
+    calculateRects(rootLayer, paintDirtyRect, layerBounds, damageRect, clipRectToApply, outlineRect);
     int x = layerBounds.x();
     int y = layerBounds.y();
                              
@@ -954,12 +954,15 @@ RenderLayer::paintLayer(RenderLayer* rootLayer, QPainter *p,
             renderer()->paint(info, tx, ty);
             info.phase = PaintActionForeground;
             renderer()->paint(info, tx, ty);
-            info.phase = PaintActionOutline;
-            renderer()->paint(info, tx, ty);
         }
 
         // Now restore our clip.
         restoreClip(p, paintDirtyRect, clipRectToApply);
+        
+        setClip(p, paintDirtyRect, outlineRect);
+        info.phase = PaintActionOutline;
+        renderer()->paint(info, tx, ty);
+        restoreClip(p, paintDirtyRect, outlineRect);
     }
     
     // Now walk the sorted list of children with positive z-indices.
@@ -1013,8 +1016,8 @@ RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderObject::NodeInfo& info,
                           int xMousePos, int yMousePos, const QRect& hitTestRect)
 {
     // Calculate the clip rects we should use.
-    QRect layerBounds, bgRect, fgRect;
-    calculateRects(rootLayer, hitTestRect, layerBounds, bgRect, fgRect);
+    QRect layerBounds, bgRect, fgRect, outlineRect;
+    calculateRects(rootLayer, hitTestRect, layerBounds, bgRect, fgRect, outlineRect);
     
     // Ensure our z-order lists are up-to-date.
     updateZOrderLists();
@@ -1149,7 +1152,7 @@ void RenderLayer::calculateClipRects(const RenderLayer* rootLayer)
 }
 
 void RenderLayer::calculateRects(const RenderLayer* rootLayer, const QRect& paintDirtyRect, QRect& layerBounds,
-                                 QRect& backgroundRect, QRect& foregroundRect)
+                                 QRect& backgroundRect, QRect& foregroundRect, QRect& outlineRect)
 {
     if (parent()) {
         parent()->calculateClipRects(rootLayer);
@@ -1160,6 +1163,7 @@ void RenderLayer::calculateRects(const RenderLayer* rootLayer, const QRect& pain
     } else
         backgroundRect = paintDirtyRect;
     foregroundRect = backgroundRect;
+    outlineRect = backgroundRect;
     
     int x = 0;
     int y = 0;
@@ -1176,6 +1180,7 @@ void RenderLayer::calculateRects(const RenderLayer* rootLayer, const QRect& pain
             QRect newPosClip = m_object->getClipRect(x,y);
             backgroundRect = backgroundRect.intersect(newPosClip);
             foregroundRect = foregroundRect.intersect(newPosClip);
+            outlineRect = outlineRect.intersect(newPosClip);
         }
 
         // If we establish a clip at all, then go ahead and make sure our background
--- third_party/WebKit/WebCore/kwq/KWQRenderTreeDebug.cpp
+++ third_party/WebKit/WebCore/kwq/KWQRenderTreeDebug.cpp
@@ -299,7 +299,7 @@ static void write(QTextStream &ts, const RenderObject &o, int indent = 0)
 }
 
 static void write(QTextStream &ts, RenderLayer &l,
-                  const QRect& layerBounds, const QRect& backgroundClipRect, const QRect& clipRect,
+                  const QRect& layerBounds, const QRect& backgroundClipRect, const QRect& clipRect, const QRect& outlineClipRect,
                   int layerType = 0, int indent = 0)
 {
     writeIndent(ts, indent);
@@ -311,6 +311,8 @@ static void write(QTextStream &ts, RenderLayer &l,
         ts << " backgroundClip " << backgroundClipRect;
     if (layerBounds != layerBounds.intersect(clipRect))
         ts << " clip " << clipRect;
+    if (layerBounds != layerBounds.intersect(outlineClipRect))
+        ts << " outlineClip " << outlineClipRect;
 
     if (l.renderer()->hasOverflowClip()) {
         if (l.scrollXOffset())
@@ -338,8 +340,8 @@ static void writeLayers(QTextStream &ts, const RenderLayer* rootLayer, RenderLay
                         const QRect& paintDirtyRect, int indent)
 {
     // Calculate the clip rects we should use.
-    QRect layerBounds, damageRect, clipRectToApply;
-    l->calculateRects(rootLayer, paintDirtyRect, layerBounds, damageRect, clipRectToApply);
+    QRect layerBounds, damageRect, clipRectToApply, outlineRect;
+    l->calculateRects(rootLayer, paintDirtyRect, layerBounds, damageRect, clipRectToApply, outlineRect);
     
     // Ensure our z-order lists are up-to-date.
     l->updateZOrderLists();
@@ -347,7 +349,7 @@ static void writeLayers(QTextStream &ts, const RenderLayer* rootLayer, RenderLay
     bool shouldPaint = l->intersectsDamageRect(layerBounds, damageRect);
     QPtrVector<RenderLayer>* negList = l->negZOrderList();
     if (shouldPaint && negList && negList->count() > 0)
-        write(ts, *l, layerBounds, damageRect, clipRectToApply, -1, indent);
+        write(ts, *l, layerBounds, damageRect, clipRectToApply, outlineRect, -1, indent);
 
     if (negList) {
         for (unsigned i = 0; i != negList->count(); ++i)
@@ -355,7 +357,7 @@ static void writeLayers(QTextStream &ts, const RenderLayer* rootLayer, RenderLay
     }
 
     if (shouldPaint)
-        write(ts, *l, layerBounds, damageRect, clipRectToApply, negList && negList->count() > 0, indent);
+        write(ts, *l, layerBounds, damageRect, clipRectToApply, outlineRect, negList && negList->count() > 0, indent);
 
     QPtrVector<RenderLayer>* posList = l->posZOrderList();
     if (posList) {
