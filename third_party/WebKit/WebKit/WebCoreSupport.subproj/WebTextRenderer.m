23594f1d80796e4ecf1d95699239d1fbb030c422
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-21 16:35:25 +0000

    Reviewed and landed by Darin.
            - fixed a couple regressions caused by my last check-in
              http://bugzilla.opendarwin.org/show_bug.cgi?id=5437
              http://bugzilla.opendarwin.org/show_bug.cgi?id=5443
    
            * WebCoreSupport.subproj/WebTextRenderer.m:
            (-[WebTextRenderer CG_drawHighlightForRun:style:geometry:]): Compute background width
            correctly, by subtracting position after run from position before run.
            (addDirectionalOverride): Make the range include only the characters between the
            directional override characters, not the directional override characters themselves.
            (initializeWidthIterator): Correctly compute "widthToStart" based on the offset to
            the beginning of the run, not to the end of the run!
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.m
@@ -867,7 +867,7 @@ static void drawGlyphs(NSFont *font, NSColor *color, CGGlyph *glyphs, CGSize *ad
     advanceWidthIterator(&it, run->from, 0, 0, 0);
     float beforeWidth = it.runWidthSoFar;
     advanceWidthIterator(&it, run->to, 0, 0, 0);
-    float backgroundWidth = it.runWidthSoFar;
+    float backgroundWidth = it.runWidthSoFar - beforeWidth;
     if (style->rtl) {
         advanceWidthIterator(&it, run->length, 0, 0, 0);
         float afterWidth = it.runWidthSoFar;
@@ -1310,8 +1310,8 @@ static WebCoreTextRun addDirectionalOverride(const WebCoreTextRun *run, bool rtl
 
     WebCoreTextRun runWithOverride;
 
-    runWithOverride.from = from;
-    runWithOverride.to = to + 2;
+    runWithOverride.from = from + 1;
+    runWithOverride.to = to + 1;
     runWithOverride.length = run->length + 2;
     runWithOverride.characters = charactersWithOverride;
 
@@ -1661,7 +1661,7 @@ static void initializeWidthIterator(WidthIterator *iterator, WebTextRenderer *re
         startPositionRun.to = run->length;
         WidthIterator startPositionIterator;
         initializeWidthIterator(&startPositionIterator, renderer, &startPositionRun, style);
-        advanceWidthIterator(&startPositionIterator, run->to, 0, 0, 0);
+        advanceWidthIterator(&startPositionIterator, run->from, 0, 0, 0);
         iterator->widthToStart = startPositionIterator.runWidthSoFar;
     }
 }
