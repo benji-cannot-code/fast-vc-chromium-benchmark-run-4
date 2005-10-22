31d418bfcb37205a80fdd31747e201e6bcdcf7ae
bdakin <bdakin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-22 01:10:32 +0000

    *** empty log message ***
    git-svn-id: svn://svn.chromium.org/blink/trunk@10902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_flow.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_flow.cpp
@@ -185,6 +185,7 @@ void RenderFlow::deleteLineBoxes()
 void RenderFlow::destroy()
 {
     RenderContainer::destroyChildren();
+    m_continuation = 0;
     
     if (!documentBeingDestroyed()) {
         if (m_firstLineBox) {
