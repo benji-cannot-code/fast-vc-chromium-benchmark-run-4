14831da2ef2e11b3dfb517e35ddea735019180da
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-24 01:43:03 +0000

    Reviewed by Maciej.
            For better abstraction, made the tokenizer -- instead of the
            data source -- responsible for calling [WebFrame _checkLoadComplete]
            when the tokenizer stops.
    
            * WebView.subproj/WebDataSource.m:
            (-[WebDataSource _stopLoadingInternal]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10921 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
@@ -453,13 +453,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [_private->iconLoader stopLoading];
 
     // The same goes for the bridge/part, which may still be parsing.
-    if (_private->committed) {
+    if (_private->committed)
         [[self _bridge] stopLoading];
-      
-        // If the data source was done loading, but the tokenizer was still running, the frame
-        // didn't have a chance to fire its didFinish delegates, so we give it a chance here.
-        [[self webFrame] _checkLoadComplete];
-    }
 
     if (!_private->loading) {
 	return;
