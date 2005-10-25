7037b73f2044a574710670788b30d0e1e3f802f6
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-25 22:32:55 +0000

    Reviewed by Vicki.
            Fixed <rdar://problem/4098450> RoboHelp-generated html help system crashes in latest Safari -KWQValueListImpl::copyOnWrite
    
            The tokenizer's timer was causing the tokenizer to be deleted twice.
            In timerEvent, we'll now check to see if the write has destroyed the tokenizer before trying to do it again.
    
            * khtml/html/htmltokenizer.cpp:
            (khtml::HTMLTokenizer::write): Returns a boolean to indicate whether end() gets called
            (khtml::HTMLTokenizer::timerEvent): Moved code from allDataProcessed, and now, checks to see if write() called end() to notify WebKit that processing is done.
            * khtml/html/htmltokenizer.h: write() returns a bool.
            * khtml/xml/xml_tokenizer.cpp: (khtml::XMLTokenizer::write): returns a bool (always false in the XMLTokenizer case).
            * khtml/xml/xml_tokenizer.h: ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/xml_tokenizer.cpp
+++ third_party/WebKit/WebCore/khtml/xml/xml_tokenizer.cpp
@@ -93,7 +93,7 @@ public:
     enum ErrorType { warning, nonFatal, fatal };
 
     // from Tokenizer
-    virtual void write(const TokenizerString &str, bool);
+    virtual bool write(const TokenizerString &str, bool);
     virtual void finish();
     virtual void setOnHold(bool onHold);
     virtual bool isWaitingForScripts() const;
@@ -258,9 +258,10 @@ XMLTokenizer::~XMLTokenizer()
         m_cachedScript->deref(this);
 }
 
-void XMLTokenizer::write(const TokenizerString &s, bool /*appendData*/ )
+bool XMLTokenizer::write(const TokenizerString &s, bool /*appendData*/ )
 {
     m_xmlCode += s.toString();
+    return false;
 }
 
 void XMLTokenizer::setOnHold(bool onHold)
