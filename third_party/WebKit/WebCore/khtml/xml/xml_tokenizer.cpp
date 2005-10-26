ec11fc01d7ae379eab8f31e71a29b024537f14f1
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-26 06:10:11 +0000

    LayoutTests:
            * dom/xhtml/level1/core/hc_documentgetdoctype-expected.txt: Expect success.
    
    WebCore:
    
            Reviewed by Eric.
    
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=5315
              need to set document type based on contents of <!DOCTYPE>
              (fixes one layout test)
    
            * khtml/html/html_documentimpl.cpp: (DOM::HTMLDocumentImpl::determineParseMode):
            Use the new setDocType function instead of modifying m_doctype directly.
    
            * khtml/xml/dom_docimpl.h: Changed to use SharedPtr more.
            (DOM::DocumentImpl::realDocType): Add get() since we're using SharedPtr.
            (DOM::DocumentImpl::setDocType): Added.
            * khtml/xml/dom_docimpl.cpp:
            (DOMImplementationImpl::createDocument): Use setDocType instead of using
            realDocType()->copyFrom to set up the document type in the new document.
            (DocumentImpl::DocumentImpl): Don't create an empty document type during constructor.
            (DocumentImpl::~DocumentImpl): Remove explicit deref since we're using SharedPtr.
            (DocumentImpl::doctype): Add get() since we're using SharedPtr.
            (DocumentTypeImpl::DocumentTypeImpl): Streamlined implementation since we're using SharedPtr.
    
            * khtml/xml/xml_tokenizer.cpp:
            (khtml::XMLTokenizer::internalSubset): Added. Calls setDocType.
            (khtml::internalSubsetHandler): Added. Calls xmlSAX2InternalSubset after calling our function.
            (khtml::XMLTokenizer::finish): Changed to pass our internalSubsetHandler function pointer.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/xml_tokenizer.cpp
+++ third_party/WebKit/WebCore/khtml/xml/xml_tokenizer.cpp
@@ -43,25 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <qptrstack.h>
 
-using namespace DOM::HTMLNames;
-
-using DOM::AttributeImpl;
-using DOM::NamedAttrMapImpl;
-using DOM::DocumentFragmentImpl;
-using DOM::DocumentImpl;
-using DOM::DocumentPtr;
-using DOM::DOMString;
-using DOM::DOMStringImpl;
-using DOM::ElementImpl;
-using DOM::HTMLScriptElementImpl;
-using DOM::HTMLTableSectionElementImpl;
-using DOM::Node;
-using DOM::NodeImpl;
-using DOM::nullAtom;
-using DOM::ProcessingInstructionImpl;
-using DOM::QualifiedName;
-using DOM::TextImpl;
-using DOM::AtomicString;
+using namespace DOM;
+using namespace HTMLNames;
 
 namespace khtml {
 
@@ -114,6 +97,7 @@ public:
     void processingInstruction(const xmlChar *target, const xmlChar *data);
     void cdataBlock(const xmlChar *s, int len);
     void comment(const xmlChar *s);
+    void internalSubset(const xmlChar *name, const xmlChar *externalID, const xmlChar *systemID);
 
 private:
     void end();
@@ -135,7 +119,7 @@ private:
     QString m_xmlCode;
 
     xmlParserCtxtPtr m_context;
-    DOM::NodeImpl *m_currentNode;
+    NodeImpl *m_currentNode;
 
     bool m_sawError;
     bool m_sawXSLTransform;
@@ -387,7 +371,7 @@ void XMLTokenizer::startElementNs(const xmlChar *xmlLocalName, const xmlChar *xm
 
 void XMLTokenizer::endElementNs()
 {
-    if (m_parserStopped) 
+    if (m_parserStopped)
         return;
 
     if (m_currentNode->nodeType() == Node::TEXT_NODE)
@@ -402,7 +386,7 @@ void XMLTokenizer::endElementNs()
 
 void XMLTokenizer::characters(const xmlChar *s, int len)
 {
-    if (m_parserStopped) 
+    if (m_parserStopped)
         return;
     
     if (m_currentNode->nodeType() == Node::TEXT_NODE ||
@@ -540,13 +524,26 @@ void XMLTokenizer::cdataBlock(const xmlChar *s, int len)
 
 void XMLTokenizer::comment(const xmlChar *s)
 {
-    if (m_parserStopped) 
+    if (m_parserStopped)
         return;
     
     if (m_currentNode->nodeType() == Node::TEXT_NODE)
         exitText();
     // ### handle exceptions
     m_currentNode->addChild(m_doc->document()->createComment(toQString(s)));
+}
+
+void XMLTokenizer::internalSubset(const xmlChar *name, const xmlChar *externalID, const xmlChar *systemID)
+{
+    if (m_parserStopped)
+        return;
+
+    DocumentPtr *docPtr = m_doc;
+    DocumentImpl *doc = docPtr->document();
+    if (!doc)
+        return;
+
+    doc->setDocType(new DocumentTypeImpl(docPtr, toQString(name), toQString(externalID), toQString(systemID)));
 }
 
 inline XMLTokenizer *getTokenizer(void *closure)
@@ -616,13 +613,19 @@ static xmlEntityPtr getEntityHandler(void *closure, const xmlChar *name)
     if(ent)
         return ent;
 
-    // Workaround a libxml SAX2 bug whereby charactersHandler is called twice
+    // Work around a libxml SAX2 bug that causes charactersHandler to be called twice.
     bool inAttr = ctxt->instate == XML_PARSER_ATTRIBUTE_VALUE;
     ent = xmlGetDocEntity(ctxt->myDoc, name);
-    if(ent)
+    if (ent)
         ctxt->replaceEntities = inAttr || (ent->etype != XML_INTERNAL_GENERAL_ENTITY);
     
     return ent;
+}
+
+static void internalSubsetHandler(void *closure, const xmlChar *name, const xmlChar *externalID, const xmlChar *systemID)
+{
+    getTokenizer(closure)->internalSubset(name, externalID, systemID);
+    xmlSAX2InternalSubset(closure, name, externalID, systemID);
 }
 
 void XMLTokenizer::finish()
@@ -640,7 +643,7 @@ void XMLTokenizer::finish()
     sax.endElementNs = endElementNsHandler;
     sax.getEntity = getEntityHandler;
     sax.startDocument = xmlSAX2StartDocument;
-    sax.internalSubset = xmlSAX2InternalSubset;
+    sax.internalSubset = internalSubsetHandler;
     sax.entityDecl = xmlSAX2EntityDecl;
     sax.initialized = XML_SAX2_MAGIC;
     
