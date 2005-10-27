4902f6e50b2473efd21bb07b06a32c6e0b8ca263
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-27 22:34:52 +0000

    Bug #: 3275 Submitted by: eseidel Reviewed by: hyatt, darin         Add XSLTProcessor support to WebCore's JavaScript bindings.         <rdar://problem/3642402> add XSLTProcessor to WebCore's JavaScript support         http://bugzilla.opendarwin.org/show_bug.cgi?id=3275
            * WebCore.xcodeproj/project.pbxproj:
            * khtml/ecma/XSLTProcessor.cpp: Added.
            (KJS::):
            (KJS::XSLTProcessor::XSLTProcessor):
            (KJS::XSLTProcessor::~XSLTProcessor):
            (KJS::XSLTProcessorProtoFunc::callAsFunction):
            * khtml/ecma/XSLTProcessor.h: Added.
            (KJS::XSLTProcessor::classInfo):
            (KJS::XSLTProcessor::):
            (KJS::XSLTProcessor::impl):
            (KJS::XSLTProcessorConstructorImp::XSLTProcessorConstructorImp):
            (KJS::XSLTProcessorConstructorImp::implementsConstruct):
            (KJS::XSLTProcessorConstructorImp::construct):
            * khtml/ecma/kjs_window.cpp:
            (KJS::Window::getValueProperty):
            * khtml/ecma/kjs_window.h:
            (KJS::Window::):
            * khtml/khtml_part.cpp:
            (KHTMLPart::replaceDocImpl):
            * khtml/xml/dom_docimpl.cpp:
            (DocumentImpl::DocumentImpl):
            (DocumentImpl::~DocumentImpl):
            (DocumentImpl::applyXSLTransform):
            * khtml/xml/dom_docimpl.h:
            (DOM::DocumentImpl::transformSourceDocument):
            (DOM::DocumentImpl::setTransformSourceDocument):
            * khtml/xml/xml_tokenizer.cpp:
            (khtml::xmlDocPtrForString):
            (khtml::XMLTokenizer::setTransformSource):
            * khtml/xml/xml_tokenizer.h:
            * khtml/xsl/xslt_processorimpl.cpp:
            (DOM::parseErrorFunc):
            (DOM::stylesheetLoadFunc):
            (DOM::setXSLTLoadCallBack):
            (DOM::writeToQString):
            (DOM::saveResultToString):
            (DOM::transformTextStringToXHTMLDocumentString):
            (DOM::xsltParamArrayFromQDict):
            (DOM::freeXsltParamArray):
            (DOM::XSLTProcessorImpl::createDocumentFromSource):
            (DOM::createFragmentFromSource):
            (DOM::xsltStylesheetPointer):
            (DOM::xmlDocPtrFromNode):
            (DOM::resultMIMEType):
            (DOM::XSLTProcessorImpl::transformToString):
            (DOM::XSLTProcessorImpl::transformToDocument):
            (DOM::XSLTProcessorImpl::transformToFragment):
            (DOM::XSLTProcessorImpl::setParameter):
            (DOM::XSLTProcessorImpl::getParameter):
            (DOM::XSLTProcessorImpl::removeParameter):
            * khtml/xsl/xslt_processorimpl.h:
            (DOM::XSLTProcessorImpl::XSLTProcessorImpl):
            (DOM::XSLTProcessorImpl::setXSLStylesheet):
            (DOM::XSLTProcessorImpl::importStylesheet):
            (DOM::XSLTProcessorImpl::clearParameters):
            (DOM::XSLTProcessorImpl::reset):
            (DOM::XSLTProcessorImpl::xslStylesheet):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10988 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_window.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_window.cpp
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "kjs_events.h"
 #include "xmlhttprequest.h"
 #include "xmlserializer.h"
+#include "XSLTProcessor.h"
 #include "domparser.h"
 
 #include "khtmlview.h"
@@ -213,7 +214,7 @@ ValueImp *Screen::getValueProperty(ExecState *exec, int token) const
 const ClassInfo Window::info = { "Window", 0, &WindowTable, 0 };
 
 /*
-@begin WindowTable 91
+@begin WindowTable 103
   closed	Window::Closed		DontDelete|ReadOnly
   crypto	Window::Crypto		DontDelete|ReadOnly
   defaultStatus	Window::DefaultStatus	DontDelete
@@ -271,6 +272,7 @@ const ClassInfo Window::info = { "Window", 0, &WindowTable, 0 };
   XMLHttpRequest	Window::XMLHttpRequest	DontDelete|ReadOnly
   XMLSerializer	Window::XMLSerializer	DontDelete|ReadOnly
   DOMParser	Window::DOMParser	DontDelete|ReadOnly
+  XSLTProcessor	Window::XSLTProcessor	DontDelete|ReadOnly
   alert		Window::Alert		DontDelete|Function 1
   confirm	Window::Confirm		DontDelete|Function 1
   prompt	Window::Prompt		DontDelete|Function 2
@@ -867,6 +869,8 @@ ValueImp *Window::getValueProperty(ExecState *exec, int token) const
       return new XMLSerializerConstructorImp(exec);
     case DOMParser:
       return new DOMParserConstructorImp(exec, m_part->xmlDocImpl());
+    case XSLTProcessor:
+      return new XSLTProcessorConstructorImp(exec);
     case FrameElement:
       if (DocumentImpl *doc = m_part->xmlDocImpl())
         if (ElementImpl *fe = doc->ownerElement())
