acba396378885e866ccbeb063a077396419f83a0
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-10-28 00:06:07 +0000

    Bug #: none Submitted by: eseidel Reviewed by: none needed, svg build fix only.         No review needed, svg build fix only.
            * kdom/xpointer/XPointerExpressionImpl.cpp: "Shared.h" to "kdom/Shared.h"
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@10994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/SVGSupport/kdom/xpointer/XPointerExpressionImpl.cpp
+++ third_party/WebKit/SVGSupport/kdom/xpointer/XPointerExpressionImpl.cpp
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "Shared.h"
+#include "kdom/Shared.h"
 #include "NodeImpl.h"
 #include "kdomxpointer.h"
 #include "DocumentImpl.h"
