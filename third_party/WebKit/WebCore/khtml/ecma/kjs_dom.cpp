4457046f044fb6a8ac4ccca9490fc90eefaa25db
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-11-01 16:59:53 +0000

    2005-11-01  Anders Carlsson  <andersca@mac.com>
            Reviewed by Darin.
    
            * khtml/ecma/kjs_dom.cpp:
            (KJS::DOMNode::getValueProperty):
            (KJS::DOMNode::putValueProperty):
            * khtml/ecma/kjs_dom.h:
            (KJS::DOMNode::):
            Add setter and setter for textContent.
    
            * khtml/xml/dom_nodeimpl.cpp:
            (DOM::NodeImpl::textContent):
            (DOM::NodeImpl::setTextContent):
            * khtml/xml/dom_nodeimpl.h:
            Implement textContent and setTextContent.
    
            * kwq/DOM.mm:
            (-[DOMNode textContent]):
            (-[DOMNode setTextContent:]):
            Add Objective C wrappers.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11013 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_dom.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_dom.cpp
@@ -194,7 +194,7 @@ bool DOMNode::toBoolean(ExecState *) const
 }
 
 /* Source for DOMNodeTable. Use "make hashtables" to regenerate.
-@begin DOMNodeTable 68
+@begin DOMNodeTable 69
   nodeName	DOMNode::NodeName	DontDelete|ReadOnly
   nodeValue	DOMNode::NodeValue	DontDelete
   nodeType	DOMNode::NodeType	DontDelete|ReadOnly
@@ -211,6 +211,8 @@ bool DOMNode::toBoolean(ExecState *) const
   prefix	DOMNode::Prefix		DontDelete
   localName	DOMNode::LocalName	DontDelete|ReadOnly
   ownerDocument	DOMNode::OwnerDocument	DontDelete|ReadOnly
+# DOM3
+  textContent   DOMNode::TextContent    DontDelete
 #
   onabort	DOMNode::OnAbort		DontDelete
   onblur	DOMNode::OnBlur			DontDelete
@@ -306,6 +308,8 @@ ValueImp *DOMNode::getValueProperty(ExecState *exec, int token) const
     return getStringOrNull(node.localName());
   case OwnerDocument:
     return getDOMNode(exec,node.ownerDocument());
+  case TextContent:
+    return getStringOrNull(node.textContent());
   case OnAbort:
     return getListener(abortEvent);
   case OnBlur:
@@ -456,6 +460,12 @@ void DOMNode::putValueProperty(ExecState *exec, int token, ValueImp *value, int 
     break;
   case Prefix:
     node.setPrefix(value->toString(exec).domString().impl(), exception);
+    break;
+  case TextContent:
+    if (value->isNull())
+      node.setTextContent(DOMString(), exception);
+    else
+      node.setTextContent(value->toString(exec).domString(), exception);
     break;
   case OnAbort:
     setListener(exec,abortEvent,value);
