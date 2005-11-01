2a40765b1faf1d03db103c56c582ca98ddbdb2a8
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-11-01 07:16:08 +0000

    Bug #: 5529 Submitted by: eseidel Reviewed by: hyatt         Now ignores XSLT PIs in documents resulting from transforms.         http://bugzilla.opendarwin.org/show_bug.cgi?id=5529
            * khtml/xml/dom_docimpl.cpp:
            (DocumentImpl::recalcStyleSelector):
            * khtml/xml/xml_tokenizer.cpp:
            (khtml::XMLTokenizer::processingInstruction):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11011 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_docimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_docimpl.cpp
@@ -2070,7 +2070,9 @@ void DocumentImpl::recalcStyleSelector()
             sheet = pi->sheet();
 #ifdef KHTML_XSLT
             if (pi->isXSL()) {
-                applyXSLTransform(pi);
+                // Don't apply transforms to already transformed documents -- <rdar://problem/4132806>
+                if (!transformSourceDocument())
+                    applyXSLTransform(pi);
                 return;
             }
 #endif
