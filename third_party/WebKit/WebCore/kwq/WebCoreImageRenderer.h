a57c459c8d1e593179325bbe2077486df22b7453
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-11-06 21:47:28 +0000

    2005-11-06  Anders Carlsson  <andersca@mac.com>
            Reviewed by Darin.
    
            * kwq/KWQClipboard.mm:
            (KWQClipboard::dragNSImage):
            Get the NSImage from the image renderer.
    
            * kwq/WebCoreImageRenderer.h:
            Add image method. This already exists in WebImageRenderer in
            WebKit.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11084 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreImageRenderer.h
+++ third_party/WebKit/WebCore/kwq/WebCoreImageRenderer.h
@@ -49,6 +49,7 @@ typedef enum {
 - (void)decreaseUseCount;
 - (void)flushRasterCache;
 - (CGImageRef)imageRef;
+- (NSImage *)image;
 - (void)resetAnimation;
 - (void)setAnimationRect:(NSRect)r;
 @end
