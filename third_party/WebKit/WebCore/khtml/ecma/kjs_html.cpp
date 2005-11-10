1bd2752d1d4d89a0c2baf48ee4fbbd0ab40a8c2e
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-11-10 22:55:15 +0000

    Bug #: 3473 Submitted by: eseidel Reviewed by: ggaren         * khtml/ecma/kjs_html.cpp:         (KJS::Image::getValueProperty): resolve relative urls in Image::src         http://bugzilla.opendarwin.org/show_bug.cgi?id=3473         <rdar://problem/3787988> Preloaded Image objects .src field is not a fully resolved path (3473)
    git-svn-id: svn://svn.chromium.org/blink/trunk@11116 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_html.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_html.cpp
@@ -3650,7 +3650,7 @@ ValueImp *Image::getValueProperty(ExecState *, int token) const
 {
   switch (token) {
   case Src:
-    return String(src);
+    return jsString(doc ? doc->completeURL(src.domString()) : src);
   case Complete:
     return Boolean(!img || img->status() >= khtml::CachedObject::Persistent);
   case OnLoad:
