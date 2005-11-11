b1ab5cdf2b5b0c0b68b0f7e1abb8f9dfaacf19ba
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-11-11 03:16:20 +0000

    LayoutTests:
            Reviewed by Maciej.
    
            Layout test for <rdar://problem/4327363> GAP: tabs not treated as whitespace in option element
    
            * fast/forms/option-strip-whitespace-expected.checksum: Added.
            * fast/forms/option-strip-whitespace-expected.png: Added.
            * fast/forms/option-strip-whitespace-expected.txt: Added.
            * fast/forms/option-strip-whitespace.html: Added.
    
    WebCore:
    
            Reviewed by Maciej.
    
            - fixed <rdar://problem/4327363> GAP: tabs not treated as whitespace in option element
    
            Added
            * fast/forms/option-strip-whitespace.html
    
            * khtml/rendering/render_form.cpp: (RenderSelect::updateFromElement): call simplifyWhiteSpace() on the itemText or label to collapse whitespace.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11136 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_form.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_form.cpp
@@ -975,6 +975,8 @@ void RenderSelect::updateFromElement()
                 // In WinIE, an optgroup can't start or end with whitespace (other than the indent
                 // we give it).  We match this behavior.
                 label = label.stripWhiteSpace();
+                // We want to collapse our whitespace too.  This will match other browsers.
+                label = label.simplifyWhiteSpace();
                 
 #if APPLE_CHANGES
                 if (m_useListBox)
@@ -998,6 +1000,8 @@ void RenderSelect::updateFromElement()
 
                 // In WinIE, leading and trailing whitespace is ignored in options. We match this behavior.
                 itemText = itemText.stripWhiteSpace();
+                // We want to collapse our whitespace too.  This will match other browsers.
+                itemText = itemText.simplifyWhiteSpace();
                 
                 if (listItems[listIndex]->parentNode()->hasTagName(optgroupTag))
                     itemText.prepend("    ");
