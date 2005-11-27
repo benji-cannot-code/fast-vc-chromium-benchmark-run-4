fefa7c17770c0ee54129338bcf0ea866672ebd5f
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-11-27 06:36:30 +0000

    Bug #: 5230 Submitted by: ap Reviewed by: mjs         Reviewed by mjs.  Committed by eseidel.
            Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=5230
            "characterIndexForPoint: not yet implemented"
    
            * WebView.subproj/WebHTMLView.m:
            (-[WebHTMLView characterIndexForPoint:]):
            (-[WebHTMLView firstRectForCharacterRange:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11303 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -4906,8 +4906,18 @@ static NSArray *validAttributes = nil;
 
 - (unsigned int)characterIndexForPoint:(NSPoint)thePoint
 {
-    ERROR("TEXTINPUT: characterIndexForPoint: not yet implemented");
-    return 0;
+    NSWindow *window = [self window];
+    WebBridge *bridge = [self _bridge];
+
+    if (window)
+        thePoint = [window convertScreenToBase:thePoint];
+    thePoint = [self convertPoint:thePoint fromView:nil];
+
+    DOMRange *range = [bridge characterRangeAtPoint:thePoint];
+    if (!range)
+        return NSNotFound;
+    
+    return [bridge convertDOMRangeToNSRange:range].location;
 }
 
 - (NSRect)firstRectForCharacterRange:(NSRange)theRange
@@ -4917,7 +4927,7 @@ static NSArray *validAttributes = nil;
     DOMRange *range;
     
     if ([self hasMarkedText]) {
-        range = [bridge convertToObjCDOMRange:theRange];
+        range = [bridge convertNSRangeToDOMRange:theRange];
     }
     else {
         range = [self _selectedRange];
