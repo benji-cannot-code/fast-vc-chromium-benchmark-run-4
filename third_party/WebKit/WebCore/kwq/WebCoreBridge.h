2543b2c2b67188decf18930a01852f065f99d8e2
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-11-27 01:50:50 +0000

    Bug #: 5230 Submitted by: ap Reviewed by: mjs         Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=5230         "characterIndexForPoint: not yet implemented"
            * kwq/WebCoreBridge.h:
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge firstRectForDOMRange:]):
            (-[WebCoreBridge convertNSRangeToDOMRange:]):
            (-[WebCoreBridge convertDOMRangeToNSRange:]):
            (-[WebCoreBridge characterRangeAtPoint:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11298 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/WebCoreBridge.h
+++ third_party/WebKit/WebCore/kwq/WebCoreBridge.h
@@ -396,7 +396,8 @@ typedef enum
 - (void)selectNSRange:(NSRange)range;
 - (NSRange)selectedNSRange;
 - (NSRange)markedTextNSRange;
-- (DOMRange *)convertToObjCDOMRange:(NSRange)range;
+- (DOMRange *)convertNSRangeToDOMRange:(NSRange)range;
+- (NSRange)convertDOMRangeToNSRange:(DOMRange *)range;
 
 - (DOMDocumentFragment *)documentFragmentWithMarkupString:(NSString *)markupString baseURLString:(NSString *)baseURLString;
 - (DOMDocumentFragment *)documentFragmentWithText:(NSString *)text;
@@ -417,6 +418,7 @@ typedef enum
 - (void)removeDragCaret;
 - (DOMRange *)dragCaretDOMRange;
 - (DOMRange *)editableDOMRangeForPoint:(NSPoint)point;
+- (DOMRange *)characterRangeAtPoint:(NSPoint)point;
 
 - (void)deleteSelectionWithSmartDelete:(BOOL)smartDelete;
 - (void)deleteKeyPressedWithSmartDelete:(BOOL)smartDelete;
