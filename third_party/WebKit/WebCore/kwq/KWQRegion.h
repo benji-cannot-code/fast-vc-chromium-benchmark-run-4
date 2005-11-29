d7e14b7e501bc0de75830bad4ff30dd81e5c29bd
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-11-29 10:11:20 +0000

    Bug #: 5310 Submitted by: mitz Reviewed by: hyatt         Not possible to test (functionality shouldn't change).
            Switch QRegion from using NSBezierPath to CGPath
            http://bugzilla.opendarwin.org/show_bug.cgi?id=5310
    
            * kwq/KWQRegion.h:
            * kwq/KWQRegion.mm:
            (QRegion::QRegion):
            (QRegion::~QRegion):
            (QRegion::operator=):
            (QRegion::contains):
            (QRegion::translate):
            (QRegion::boundingRect):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQRegion.h
+++ third_party/WebKit/WebCore/kwq/KWQRegion.h
@@ -31,11 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KWQImage.h"
 #include "KWQRect.h"
 
-#ifdef __OBJC__
-@class NSBezierPath;
-#else
-class NSBezierPath;
-#endif
+typedef struct CGPath *CGMutablePathRef;
 
 class QRegion {
 public:
@@ -56,7 +52,7 @@ public:
     void translate(int deltaX, int deltaY);
 
 private:
-    NSBezierPath *path;    
+    CGMutablePathRef path;    
 };
 
 #endif
