20daacc94c63473436fd988acefbf4914e6f9a6e
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-02 22:53:31 +0000

    <rdar://problem/4345030>         Denver REGRESSION (10.4.2-10.4.3): Two identical warnings on "Back" from Amazon's package tracker
            Before checking the navigation policy for a request, the request is compared
            against the last checked request.  If the two are the same, no check is done.
            In the bug, the two requests are identical except for the boolean on
            NSURLRequests that tells Foundation to support multipart loads for that request.
            One request was the one that was used to start servicing "Back" operation, and
            the second was being used to start loading the page (it needed to be reloaded
            because it was the result of a form submission). Set the boolean on all
            NSURLRequests, not just one's that are about to be used to start a load.
    
            Reviewed by harrison
    
            * WebView.subproj/WebDataSource.m:
            (-[WebDataSource _startLoading:]):
            (-[WebDataSource initWithRequest:]):
            * WebView.subproj/WebDataSourcePrivate.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSource.m
@@ -398,7 +398,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             identifier = [[WebDefaultResourceLoadDelegate sharedResourceLoadDelegate] webView:_private->webView identifierForInitialRequest:_private->originalRequest fromDataSource:self];
             
         _private->mainResourceLoader = [[WebMainResourceLoader alloc] initWithDataSource:self];
-        [_private->mainResourceLoader setSupportsMultipartContent:WKSupportsMultipartXMixedReplace(_private->request)];
+        [_private->mainResourceLoader setSupportsMultipartContent:_private->supportsMultipartContent];
+        
         [_private->mainResourceLoader setIdentifier: identifier];
         [[self webFrame] _addExtraFieldsToRequest:_private->request alwaysFromRequest: NO];
         if (![_private->mainResourceLoader loadWithRequest:_private->request]) {
@@ -1091,6 +1092,7 @@ static inline void addTypesFromClass(NSMutableDictionary *allTypes, Class class,
     
     LOG(Loading, "creating datasource for %@", [request URL]);
     _private->request = [_private->originalRequest mutableCopy];
+    _private->supportsMultipartContent = WKSupportsMultipartXMixedReplace(_private->request);
 
     ++WebDataSourceCount;
     
--- third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebDataSourcePrivate.h
@@ -131,6 +131,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     
     NSMutableDictionary *subresources;
     NSMutableDictionary *pendingSubframeArchives;
+    
+    BOOL supportsMultipartContent;
 }
 
 @end
