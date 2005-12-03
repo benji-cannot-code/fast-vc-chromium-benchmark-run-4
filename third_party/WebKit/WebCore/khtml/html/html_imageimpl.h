11dfb56067f7f0796ae9bd3ccf0836943f39fac0
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-03 20:16:43 +0000

    2005-12-01  Anders Carlsson  <andersca@mac.com>
            Reviewed by Darin.
    
            - Fix <http://bugzilla.opendarwin.org/show_bug.cgi?id=4340>
            Safari not supporting disabled in multi select option tag.
    
            * khtml/rendering/render_form.cpp:
            (RenderSelect::updateFromElement):
            Update calls to append functions to set the enabled state of items.
    
            * kwq/KWQComboBox.h:
            (QComboBox::appendItem):
            (QComboBox::appendGroupLabel):
            (QComboBox::appendSeparator):
            Add enabled parameter to append functions.
    
            * kwq/KWQComboBox.mm:
            (QComboBox::QComboBox):
            Don't autoenable menu items on the popup button since that's handled
            manually now.
    
            (QComboBox::setTitle):
            Set enabled state on menu items.
    
            (QComboBox::appendItem):
            Add enabled parameter.
    
            * kwq/KWQListBox.h:
            (KWQListBoxItem::KWQListBoxItem):
            Add enabled variable to item struct.
    
            (QListBox::appendItem):
            (QListBox::appendGroupLabel):
            Add enabled parameter to append functions.
    
            * kwq/KWQListBox.mm:
            (QListBox::appendItem):
            Add enabled parameter.
    
            (-[KWQTableView tableView:shouldSelectRow:]):
            Only select the row if the item is enabled.
    
            (-[KWQTableView drawRow:clipRect:]):
            Draw using the disabled color if the row is disabled.
    
            * manual-tests/disabled-option-elements.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11423 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/html/html_imageimpl.h
+++ third_party/WebKit/WebCore/khtml/html/html_imageimpl.h
@@ -141,6 +141,8 @@ public:
     int x() const;
     int y() const;
 
+    bool complete() const;
+    
 protected:
     HTMLImageLoader m_imageLoader;
     DOMString usemap;
--- third_party/WebKit/WebCore/kwq/KWQListBox.mm
+++ third_party/WebKit/WebCore/kwq/KWQListBox.mm
@@ -193,9 +193,9 @@ void QListBox::setSelectionMode(SelectionMode mode)
     KWQ_UNBLOCK_EXCEPTIONS;
 }
 
-void QListBox::appendItem(const QString &text, KWQListBoxItemType type)
+void QListBox::appendItem(const QString &text, KWQListBoxItemType type, bool enabled)
 {
-    _items.append(KWQListBoxItem(text, type));
+    _items.append(KWQListBoxItem(text, type, enabled));
     _widthGood = false;
 }
 
@@ -667,7 +667,12 @@ static Boolean KWQTableViewTypeSelectCallback(UInt32 index, void *listDataPtr, v
 
 - (BOOL)tableView:(NSTableView *)tableView shouldSelectRow:(int)row
 {
-    return _box && _box->itemAtIndex(row).type == KWQListBoxOption;
+    if (!_box)
+        return NO;
+    
+    const KWQListBoxItem &item = _box->itemAtIndex(row);
+    
+    return item.type == KWQListBoxOption && item.enabled;
 }
 
 - (BOOL)selectionShouldChangeInTableView:(NSTableView *)aTableView
@@ -684,7 +689,7 @@ static Boolean KWQTableViewTypeSelectCallback(UInt32 index, void *listDataPtr, v
     const KWQListBoxItem &item = _box->itemAtIndex(row);
 
     NSColor *color;
-    if (_box->isEnabled()) {
+    if (_box->isEnabled() && item.enabled) {
         if ([self isRowSelected:row] && [[self window] firstResponder] == self && ([[self window] isKeyWindow] || ![[self window] canBecomeKeyWindow])) {
             color = [NSColor alternateSelectedControlTextColor];
         } else {
