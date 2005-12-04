2d58f16b164f66cc40f7eb3c95714f8f73e4b940
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-04 00:48:05 +0000

    <http://bugzilla.opendarwin.org/show_bug.cgi?id=5856>         Selection based in an editable block can extend outside
            validate() used to do expansion of the selection.  The expansion
            itself wasn't validated, so it could create a selection
            that extended outside an editable area.  Also m_base and
            m_extent weren't updated with the results of the expansion.
    
            Reviewed by darin
    
            Layout tests added:
            * editing/selection/expanding-selections
            * editing/selection/expanding-selections2
    
            * khtml/editing/SelectionController.cpp:
            (khtml::SelectionController::expandUsingGranularity): Does the expansion.
            (khtml::SelectionController::validate): Removed the granularity parameter.
            * khtml/editing/SelectionController.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/editing/SelectionController.h
+++ third_party/WebKit/WebCore/khtml/editing/SelectionController.h
@@ -122,7 +122,7 @@ private:
     enum EPositionType { START, END, BASE, EXTENT };
 
     void init(EAffinity affinity);
-    void validate(ETextGranularity granularity = CHARACTER);
+    void validate();
     void adjustExtentForEditableContent();
 
     VisiblePosition modifyExtendingRightForward(ETextGranularity);
