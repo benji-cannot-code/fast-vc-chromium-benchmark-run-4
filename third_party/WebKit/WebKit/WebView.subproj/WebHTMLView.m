1bdff8b2398bffceccb7c2ca6fffc6e6d4ca350d
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-08 00:19:20 +0000

    Reviewed by Tim Omernick.
            - <rdar://problem/4331488> TOT Assertion failure in -[WebHTMLView nextValidKeyView]
            @ home.netscape.com
    
            * WebView.subproj/WebHTMLView.m:
            (-[NSArray nextValidKeyView]):
            Removed assert that I added a while back. In this case at least, the assertion is
            overzealous, and I can't recreate the tortured chain of logic that led me to adding
            this assertion in the first place.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11487 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView.subproj/WebHTMLView.m
@@ -2937,7 +2937,6 @@ done:
         // WebFrameView has this same kind of logic for the previousValidKeyView case.
         if (view == nil || view == self) {
             ASSERT([self _frame] != [[self _webView] mainFrame]);
-            ASSERT(lookInsideWebFrameViews);
             view = [[self _bridge] nextValidKeyViewOutsideWebFrameViews];
         }
     }
