46423a2dd61dc96295f331384b7934c19395afd2
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-10 19:05:22 +0000

    JavaScriptCore:
            Reviewed by Maciej, landed by Darin.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=3539
              Array join and toString methods do not support circular references
    
            * kjs/array_object.cpp: (ArrayProtoFuncImp::callAsFunction):
            Added set of visited objects -- don't recurse if item is already in the set.
    
    LayoutTests:
    
            New layout test for http://bugzilla.opendarwin.org/show_bug.cgi?id=3539
    
            * fast/js/cyclic-ref-toString-expected.txt: Added.
            * fast/js/cyclic-ref-toString.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11519 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "reference_list.h"
 #include "types.h"
 #include "value.h"
+#include "HashSet.h"
 
 #include "array_object.lut.h"
 
@@ -447,9 +448,13 @@ ValueImp *ArrayProtoFuncImp::callAsFunction(ExecState *exec, ObjectImp *thisObj,
 
     // fall through
   case Join: {
+    static HashSet< ObjectImp*, PointerHash<ObjectImp*> > visitedElems;
+    if (visitedElems.contains(thisObj))
+      return jsString("");
     UString separator = ",";
     UString str = "";
 
+    visitedElems.insert(thisObj);
     if (!args[0]->isUndefined())
       separator = args[0]->toString(exec);
     for (unsigned int k = 0; k < length; k++) {
@@ -489,6 +494,7 @@ ValueImp *ArrayProtoFuncImp::callAsFunction(ExecState *exec, ObjectImp *thisObj,
       if ( exec->hadException() )
         break;
     }
+    visitedElems.remove(thisObj);
     result = String(str);
     break;
   }
