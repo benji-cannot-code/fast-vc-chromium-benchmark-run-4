f966ad624bf33cd98a6817d17864c23fb5fefd4f
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-12 17:01:42 +0000

    LayoutTests:
            Reviewed by Eric.
    
            Test case for http://bugzilla.opendarwin.org/show_bug.cgi?id=5538
    
            * fast/xsl/resources/xslt-entity.xsl: Added.
            * fast/xsl/xslt-entity-expected.checksum: Added.
            * fast/xsl/xslt-entity-expected.png: Added.
            * fast/xsl/xslt-entity-expected.txt: Added.
            * fast/xsl/xslt-entity.xml: Added.
    
    WebCore:
    
            Reviewed by Eric.
    
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=5538
              XSLT crashes with multi-word internal entity declarations
    
            Added the crashing specimen as a layout test.
    
            * khtml/xsl/xsl_stylesheetimpl.cpp: (DOM::XSLStyleSheetImpl::loadChildSheets):
            Check that the node is an element node before calling IS_XSLT_ELEM.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11535 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/xsl/resources/xslt-entity.xsl
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?xml version="1.0" encoding="utf-8" ?>
+<!DOCTYPE test-document [
+<!ENTITY HELLO "Hello World, Success!">
+]>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:template match="/">
+		<P>&HELLO;</P>
+    </xsl:template>
+</xsl:stylesheet>
+
--- third_party/WebKit/WebCore/khtml/xsl/xsl_stylesheetimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xsl/xsl_stylesheetimpl.cpp
@@ -149,7 +149,7 @@ void XSLStyleSheetImpl::loadChildSheets()
                 curr = curr->next;
                 continue;
             }
-            if (IS_XSLT_ELEM(curr) && IS_XSLT_NAME(curr, "import")) {
+            if (curr->type == XML_ELEMENT_NODE && IS_XSLT_ELEM(curr) && IS_XSLT_NAME(curr, "import")) {
                 xmlChar* uriRef = xsltGetNsProp(curr, (const xmlChar *)"href", XSLT_NAMESPACE);
                 QString buff = QString::fromUtf8((const char*)uriRef);
                 loadChildSheet(buff);
@@ -160,7 +160,7 @@ void XSLStyleSheetImpl::loadChildSheets()
 
         // Now handle includes.
         while (curr) {
-            if (IS_XSLT_ELEM(curr) && IS_XSLT_NAME(curr, "include")) {
+            if (curr->type == XML_ELEMENT_NODE && IS_XSLT_ELEM(curr) && IS_XSLT_NAME(curr, "include")) {
                 xmlChar* uriRef = xsltGetNsProp(curr, (const xmlChar *)"href", XSLT_NAMESPACE);
                 QString buff = QString::fromUtf8((const char*)uriRef);
                 loadChildSheet(buff);
