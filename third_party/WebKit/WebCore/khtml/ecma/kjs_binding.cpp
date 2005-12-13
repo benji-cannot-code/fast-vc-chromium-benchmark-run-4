bb6d6f79a1e5ec90ab5021688b90d4d729faaec8
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-13 11:46:56 +0000

    - build fix for last-minute part of previous change
            * khtml/ecma/kjs_binding.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11562 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_binding.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_binding.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "kjs_window.h"
 #include <kjs/internal.h> // for InterpreterImp
 #include <kjs/collector.h>
+#include <kxmlcore/HashMap.h>
 
 #include "dom/dom_exception.h"
 #include "dom/dom2_events.h"
