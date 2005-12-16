4c360cd03478ac52f44bc57ee4b0ea1b9a62d57f
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-16 20:33:21 +0000

    Don't include bits/cpp_type_traits on win32
    git-svn-id: svn://svn.chromium.org/blink/trunk@11628 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kxmlcore/HashTraits.h
+++ third_party/WebKit/JavaScriptCore/kxmlcore/HashTraits.h
@@ -25,7 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define KXMLCORE_HASH_TRAITS_H
 
 #include <utility>
+#ifndef WIN32
 #include <bits/cpp_type_traits.h>
+#endif
 
 namespace KXMLCore {
 
