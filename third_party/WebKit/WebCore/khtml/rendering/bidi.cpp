e647ef25410101797859fcbf3d58c4b38db09bff
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-16 18:18:50 +0000

    Reviewed and landed by Darin.
            Test: fast/text/international/bidi-european-terminators.html
    
            Fix for: http://bugzilla.opendarwin.org/show_bug.cgi?id=6014
            Bidi algorithm: incorrect resolved levels for neutrals between R and
            ET ON L
    
            * khtml/rendering/bidi.cpp:
            (khtml::RenderBlock::bidiReorderLine):
            Keep track of the last position before the current run of ETs directly
            using a new BidiState member, lastBeforeET.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11621 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/bidi.cpp
@@ -72,6 +72,7 @@ struct BidiState {
     bool adjustEmbedding;
     BidiIterator endOfLine;
     bool reachedEndOfLine;
+    BidiIterator lastBeforeET;
 };
 
 inline bool operator==(const BidiStatus& status1, const BidiStatus& status2)
@@ -1126,12 +1127,6 @@ void RenderBlock::bidiReorderLine(const BidiIterator& start, const BidiIterator&
             if (bidi.status.lastStrong != QChar::DirAL) {
                 // if last strong was AL change EN to AN
                 switch (bidi.status.last) {
-                    case QChar::DirET:
-                        if (bidi.status.lastStrong == QChar::DirR || bidi.status.lastStrong == QChar::DirAL) {
-                            appendRun(bidi);
-                            bidi.dir = QChar::DirEN;
-			}
-			// fall through
                     case QChar::DirEN:
                     case QChar::DirL:
                         break;
@@ -1146,6 +1141,7 @@ void RenderBlock::bidiReorderLine(const BidiIterator& start, const BidiIterator&
                     case QChar::DirCS:
                         if (bidi.status.eor == QChar::DirEN)
                             break;
+                    case QChar::DirET:
                     case QChar::DirBN:
                     case QChar::DirB:
                     case QChar::DirS:
@@ -1153,7 +1149,7 @@ void RenderBlock::bidiReorderLine(const BidiIterator& start, const BidiIterator&
                     case QChar::DirON:
                         if (bidi.status.eor == QChar::DirR) {
                             // neutrals go to R
-                            bidi.eor = bidi.last;
+                            bidi.eor = bidi.status.last == QChar::DirET ? bidi.lastBeforeET : bidi.last;
                             appendRun(bidi);
                             bidi.dir = QChar::DirEN;
                         } else if (bidi.status.eor != QChar::DirL &&
@@ -1161,7 +1157,7 @@ void RenderBlock::bidiReorderLine(const BidiIterator& start, const BidiIterator&
                                  bidi.dir != QChar::DirL) {
                             // numbers on both sides, neutrals get right to left direction
                             appendRun(bidi);
-                            bidi.eor = bidi.last;
+                            bidi.eor = bidi.status.last == QChar::DirET ? bidi.lastBeforeET : bidi.last;
                             bidi.dir = QChar::DirR;
                             appendRun(bidi);
                             bidi.dir = QChar::DirEN;
@@ -1233,19 +1229,8 @@ void RenderBlock::bidiReorderLine(const BidiIterator& start, const BidiIterator&
                 dirCurrent = QChar::DirEN;
                 bidi.eor = bidi.current;
                 bidi.status.eor = dirCurrent;
-                break;
-            } else if ((bidi.status.eor == QChar::DirR || bidi.status.eor == QChar::DirAL || bidi.status.eor == QChar::DirAN || (bidi.status.eor == QChar::DirEN && bidi.status.lastStrong == QChar::DirR)) && bidi.last!=bidi.current) {
-                // most of the time this is unnecessary, but we need to secure the R run in case
-                // the ET ends up being neutral and followed by L
-                if (bidi.status.last!=QChar::DirET) {
-                    bidi.dir = bidi.status.eor;
-                    appendRun(bidi);
-                    bidi.eor = bidi.last;
-                }
-                bidi.status.eor = QChar::DirR;
-                bidi.dir = QChar::DirR;
-                break;
-            }
+            } else if (bidi.status.last != QChar::DirET)
+                bidi.lastBeforeET = emptyRun ? bidi.eor : bidi.last;
             break;
 
         // boundary neutrals should be ignored
