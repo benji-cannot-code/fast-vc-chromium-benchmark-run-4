f014d3a2a17dc90b3ee68b1c8f126452b89acc7f
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-18 22:23:29 +0000

    LayoutTests:
            Reviewed and landed by Darin.
    
            - XSLT document encoding tests
    
            * fast/xsl/xslt-doc-enc-expected.txt: Added.
            * fast/xsl/xslt-doc-enc.xml: Added.
            * fast/xsl/xslt-doc-enc.xsl: Added.
            * fast/xsl/xslt-doc-noenc-expected.txt: Added.
            * fast/xsl/xslt-doc-noenc.xml: Added.
            * fast/xsl/xslt-doc-noenc.xsl: Added.
    
    WebCore:
    
            Reviewed and landed by Darin.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=5927
              Result of XSLT transformation has no charset defined
    
            * khtml/xml/dom_docimpl.cpp: (DocumentImpl::applyXSLTransform):
            Get encoding from transform and pass in to XSLTProcessor.
    
            * khtml/xsl/xslt_processorimpl.h:
            * khtml/xsl/xslt_processorimpl.cpp:
            (DOM::XSLTProcessorImpl::createDocumentFromSource): Added encoding parameter.
            Create a Decoder and use it to decode the source document (default encoding UTF-8).
            (DOM::XSLTProcessorImpl::transformToString): Add an encoding result. Set it to the
            encoding from the libxml2 document object.
            (DOM::XSLTProcessorImpl::transformToDocument): Get encoding from transform and pass
            in to XSLTProcessor.
            (DOM::XSLTProcessorImpl::transformToFragment): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11654 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/xsl/xslt-doc-enc.xsl
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?xml version="1.0" encoding="iso8859-5"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+    <xsl:output method="html" encoding="KOI8-R"/>
+    <xsl:template match="TEST">
+        <html>
+            <body>
+              <script>
+                if (window.layoutTestController)
+                  layoutTestController.dumpAsText();
+                
+              	//alert(document.characterSet); // works in Firefox
+              	document.write("Encoding: " + document.characterSet);
+              </script>
+            </body>
+        </html>
+  </xsl:template>
+
+</xsl:stylesheet>
--- third_party/WebKit/WebCore/khtml/xsl/xslt_processorimpl.cpp
+++ third_party/WebKit/WebCore/khtml/xsl/xslt_processorimpl.cpp
@@ -178,7 +178,7 @@ static void freeXsltParamArray(const char **params)
 }
 
 
-RefPtr<DocumentImpl> XSLTProcessorImpl::createDocumentFromSource(const QString &sourceString, const QString &sourceMIMEType, NodeImpl *sourceNode, KHTMLView *view)
+RefPtr<DocumentImpl> XSLTProcessorImpl::createDocumentFromSource(const QString &sourceString, const QString &sourceEncoding, const QString &sourceMIMEType, NodeImpl *sourceNode, KHTMLView *view)
 {
     RefPtr<DocumentImpl> ownerDocument = sourceNode->getDocument();
     bool sourceIsDocument = (sourceNode == ownerDocument.get());
@@ -209,6 +209,12 @@ RefPtr<DocumentImpl> XSLTProcessorImpl::createDocumentFromSource(const QString &
         result->setBaseURL(ownerDocument->baseURL());
     }
     result->determineParseMode(documentSource); // Make sure we parse in the correct mode.
+    
+    Decoder *decoder = new Decoder;
+    decoder->setEncoding(sourceEncoding.isEmpty() ? "UTF-8" : sourceEncoding.latin1(), Decoder::EncodingFromXMLHeader);
+    result->setDecoder(decoder);
+    decoder->deref();
+    
     result->write(documentSource);
     result->finishParsing();
     result->setParsing(false);
@@ -284,7 +290,7 @@ static inline QString resultMIMEType(xmlDocPtr resultDoc, xsltStylesheetPtr shee
     return QString("application/xml");
 }
 
-bool XSLTProcessorImpl::transformToString(NodeImpl *sourceNode, QString &mimeType, QString &resultString)
+bool XSLTProcessorImpl::transformToString(NodeImpl *sourceNode, QString &mimeType, QString &resultString, QString &resultEncoding)
 {
     RefPtr<DocumentImpl> ownerDocument = sourceNode->getDocument();
     RefPtr<XSLStyleSheetImpl> cachedStylesheet = m_stylesheet;
@@ -308,6 +314,7 @@ bool XSLTProcessorImpl::transformToString(NodeImpl *sourceNode, QString &mimeTyp
         return false;
     
     mimeType = resultMIMEType(resultDoc, sheet);
+    resultEncoding = (char *)resultDoc->encoding;
     
     xsltFreeStylesheet(sheet);
     xmlFreeDoc(resultDoc);
@@ -319,16 +326,18 @@ RefPtr<DocumentImpl> XSLTProcessorImpl::transformToDocument(NodeImpl *sourceNode
 {
     QString resultMIMEType;
     QString resultString;
-    if (!transformToString(sourceNode, resultMIMEType, resultString))
+    QString resultEncoding;
+    if (!transformToString(sourceNode, resultMIMEType, resultString, resultEncoding))
         return 0;
-    return createDocumentFromSource(resultString, resultMIMEType, sourceNode);
+    return createDocumentFromSource(resultString, resultEncoding, resultMIMEType, sourceNode);
 }
 
 RefPtr<DocumentFragmentImpl> XSLTProcessorImpl::transformToFragment(NodeImpl *sourceNode, DocumentImpl *outputDoc)
 {
     QString resultMIMEType;
     QString resultString;
-    if (!transformToString(sourceNode, resultMIMEType, resultString))
+    QString resultEncoding;
+    if (!transformToString(sourceNode, resultMIMEType, resultString, resultEncoding))
         return 0;
     return createFragmentFromSource(resultString, resultMIMEType, sourceNode, outputDoc);
 }
