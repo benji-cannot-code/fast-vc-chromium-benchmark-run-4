41d55e5ad273cae6726b7ba58a3c8455a7ff230e
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-20 18:29:13 +0000

    Reviewed by Darin, committed by Adele.
            - fix deployment builds broken by the ATSUI multiple renderers patch
            http://bugzilla.opendarwin.org/show_bug.cgi?id=6153
    
            * WebCoreSupport.subproj/WebTextRenderer.m:
            (createATSULayoutParameters): Assign initial values, which will never be used,
            to substituteRenderer and firstSmallCap, to avoid uninitialized variable warnings.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11681 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebTextRenderer.m
@@ -1290,7 +1290,7 @@ static void createATSULayoutParameters(ATSULayoutParameters *params, WebTextRend
     UniCharArrayOffset substituteOffset = runFrom;
     UniCharCount substituteLength;
     UniCharArrayOffset lastOffset;
-    WebTextRenderer *substituteRenderer;
+    WebTextRenderer *substituteRenderer = 0;;
 
     while (substituteOffset < runTo) {
         lastOffset = substituteOffset;
@@ -1309,10 +1309,10 @@ static void createATSULayoutParameters(ATSULayoutParameters *params, WebTextRend
         }
 
         bool isSmallCap = false;
+        UniCharArrayOffset firstSmallCap = 0;
         WebTextRenderer *r = renderer;
         UniCharArrayOffset i;
         for (i = lastOffset;  ; i++) {
-            UniCharArrayOffset firstSmallCap;
             if (i == substituteOffset || i == substituteOffset + substituteLength) {
                 if (isSmallCap) {
                     isSmallCap = false;
