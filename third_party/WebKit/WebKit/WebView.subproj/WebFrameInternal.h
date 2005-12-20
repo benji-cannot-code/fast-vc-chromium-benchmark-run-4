1998bb6c96d654292e2bebbfe7ccb676126f7364
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-20 08:32:56 +0000

    Reviewed by Eric.
    	- Move handling of frame namespaces down to WebFrame.
    	- Put some internal class declarations in the implementation file.
    
            * WebView.subproj/WebControllerSets.m:
            (+[WebFrameNamespaces addWebView:toFrameNamespace:]):
            (+[WebFrameNamespaces webViewsInFrameNamespace:]):
            * WebView.subproj/WebFrame.m:
            (-[WebFramePrivate dealloc]):
            (-[WebFrame _setFrameNamespace:]):
            (-[WebFrame _frameNamespace]):
            * WebView.subproj/WebFrameInternal.h:
            * WebView.subproj/WebFramePrivate.h:
            * WebView.subproj/WebView.m:
            (-[WebView _close]):
            (-[WebView _findFrameNamed:sourceFrame:]):
            (-[WebView setGroupName:]):
            (-[WebView groupName]):
            * WebView.subproj/WebViewInternal.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView.subproj/WebFrameInternal.h
+++ third_party/WebKit/WebKit/WebView.subproj/WebFrameInternal.h
@@ -50,6 +50,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                                             data:(NSData *)data
                                                            error:(NSError *)error;
 - (void)_setupForReplace;
+
+- (void)_setFrameNamespace:(NSString *)namespace;
+- (NSString *)_frameNamespace;
+
 @end
 
 @interface NSObject (WebInternalFrameLoadDelegate)
