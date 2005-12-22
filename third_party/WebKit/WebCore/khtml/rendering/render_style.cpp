764d0b8aeb9ecbcf2b09d97d4b2657be097a1500
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-22 16:46:39 +0000

    Reviewed by Eric.
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=6167
              RenderStyle default constructor should initialize its members for speed
    
            * khtml/rendering/DataRef.h: (khtml::DataRef::operator=): Eliminate an extra
            branch by doing ref before deref instead of == check.
    
            * khtml/rendering/render_style.cpp:
            (khtml::initDefaultStyle): Added. Function to initialize the default style
            for use in constructor.
            (khtml::RenderStyle::RenderStyle): Changed constructor to initalize all the
            members with constructor syntax instead of using assignment on all of them.
            * khtml/rendering/render_style.h: Removed static data member _default --
            it's now a file scope global instead.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11726 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_style.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_style.cpp
@@ -35,10 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "kdebug.h"
 
-using namespace khtml;
+using namespace DOM;
 
-using DOM::DOMStringImpl;
-using DOM::DOMString;
+namespace khtml {
+
+static RenderStyle *defaultStyle;
 
 StyleSurroundData::StyleSurroundData()
     : margin( Fixed ), padding( Auto )
@@ -513,31 +514,32 @@ void RenderStyle::arenaDelete(RenderArena *arena)
     arena->free(*(size_t *)this, this);
 }
 
-RenderStyle::RenderStyle()
-:m_pseudoState(PseudoUnknown), m_affectedByAttributeSelectors(false)
+inline RenderStyle *initDefaultStyle()
 {
-    m_ref = 0;
-    
-    if (!_default)
-	_default = ::new RenderStyle(true);
+    if (!defaultStyle)
+        defaultStyle = ::new RenderStyle(true);
+    return defaultStyle;
+}
 
-    box = _default->box;
-    visual = _default->visual;
-    background = _default->background;
-    surround = _default->surround;
-    css3NonInheritedData = _default->css3NonInheritedData;
-    css3InheritedData = _default->css3InheritedData;
-    
-    inherited = _default->inherited;
-
+RenderStyle::RenderStyle()
+    : box(initDefaultStyle()->box)
+    , visual(defaultStyle->visual)
+    , background(defaultStyle->background)
+    , surround(defaultStyle->surround)
+    , css3NonInheritedData(defaultStyle->css3NonInheritedData)
+    , css3InheritedData(defaultStyle->css3InheritedData)
+    , inherited(defaultStyle->inherited)
+    , pseudoStyle(0)
+    , content(0)
+    , m_pseudoState(PseudoUnknown)
+    , m_affectedByAttributeSelectors(false)
+    , m_ref(0)
 #if SVG_SUPPORT
-    m_svgStyle = _default->m_svgStyle;
+    , m_svgStyle(defaultStyle->m_svgStyle)
 #endif
 
-    setBitDefaults();
-
-    pseudoStyle = 0;
-    content = 0;
+{
+    setBitDefaults(); // Would it be faster to copy this from the default style?
 }
 
 RenderStyle::RenderStyle(bool)
@@ -871,14 +873,11 @@ RenderStyle::Diff RenderStyle::diff( const RenderStyle *other ) const
 }
 
 
-RenderStyle* RenderStyle::_default = 0;
 //int RenderStyle::counter = 0;
 //int SharedData::counter = 0;
 
 void RenderStyle::cleanup()
 {
-    delete _default;
-    _default = 0;
 //    counter = 0;
 //    SharedData::counter = 0;
 }
@@ -1128,4 +1127,6 @@ const QValueList<StyleDashboardRegion>& RenderStyle::noneDashboardRegions()
         noneListInitialized = true;
     }
     return noneList;
+}
+
 }
--- third_party/WebKit/WebCore/khtml/rendering/render_style.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_style.h
@@ -1033,9 +1033,6 @@ protected:
     
 // !END SYNC!
 
-// static default style
-    static RenderStyle* _default;
-
 protected:
     void setBitDefaults()
     {
