aa4aea554761c0048e010fd1ccd1f4e203454e0f
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-25 08:11:18 +0000

    Bug #: none Submitted by: eseidel Reviewed by: mjs         Unbreak HashTableConstIterator++ by returning const_iterator
            * kxmlcore/HashTable.h:
            (KXMLCore::HashTableConstIterator::operator++): use const_iterator
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11762 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kxmlcore/HashTable.h
+++ third_party/WebKit/JavaScriptCore/kxmlcore/HashTable.h
@@ -148,7 +148,7 @@ namespace KXMLCore {
             }
         }
         
-        iterator& operator++()
+        const_iterator& operator++()
         {
             ++m_position;
             skipEmptyBuckets();
