230fcd879d36d2deffe08ff0bcdbcaf38ce3037a
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-26 22:23:55 +0000

    Make sure assert.h is included by including it in config.h
    git-svn-id: svn://svn.chromium.org/blink/trunk@11769 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/identifier.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/identifier.cpp
@@ -140,7 +140,7 @@ PassRefPtr<UString::Rep> Identifier::add(const char *c)
 
 struct UCharBuffer {
     const UChar *s;
-    uint length;
+    unsigned int length;
 };
 
 struct UCharBufferTranslator
