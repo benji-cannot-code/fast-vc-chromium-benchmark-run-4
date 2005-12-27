da71bae0e988a7e9abbb10ae7c5ab1fa2a2b7064
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-27 20:02:01 +0000

    2005-12-27  Anders Carlsson  <andersca@mac.com>
            Reviewed by Darin.
    
            * kjs/array_object.cpp:
            (ArrayProtoFunc::callAsFunction):
            Implement filter and map. Also, make the existing
            array iteration functions not invoke the callback for
            non-existing properties, just as Mozilla does now.
    
            * kjs/array_object.h:
            (KJS::ArrayProtoFunc::):
            Add filter and map.
    
            * tests/mozilla/expected.html:
            Update, two 1.6 tests now pass.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11785 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_object.h
+++ third_party/WebKit/JavaScriptCore/kjs/array_object.h
@@ -46,7 +46,7 @@ namespace KJS {
 
     enum { ToString, ToLocaleString, Concat, Join, Pop, Push,
           Reverse, Shift, Slice, Sort, Splice, UnShift, 
-          Every, ForEach, Some, IndexOf };
+          Every, ForEach, Some, IndexOf, Filter, Map };
   private:
     int id;
   };
