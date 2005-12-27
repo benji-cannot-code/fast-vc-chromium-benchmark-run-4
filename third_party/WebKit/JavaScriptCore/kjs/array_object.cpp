feba29b2b945a29f222090290ec394cbb81aefd7
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-27 11:07:49 +0000

    JavaScriptCore:
            Reviewed and landed by Maciej.
    
    	- fixed 6234: Can delete array index property incorrectly.
    	http://bugzilla.opendarwin.org/show_bug.cgi?id=6234
    
            * kjs/array_object.cpp:
            (ArrayInstance::deleteProperty): use toArrayIndex instead of toUInt32 when
    	looking for array properties.
    
    LayoutTests:
    
            Reviewed, tweaked and landed by Maciej.
    
    	- test case for 6234: Can delete array index property incorrectly.
    	http://bugzilla.opendarwin.org/show_bug.cgi?id=6234
    
            * fast/js/array-float-delete.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11775 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
@@ -162,7 +162,7 @@ bool ArrayInstance::deleteProperty(ExecState *exec, const Identifier &propertyNa
     return false;
   
   bool ok;
-  uint32_t index = propertyName.toUInt32(&ok);
+  uint32_t index = propertyName.toArrayIndex(&ok);
   if (ok) {
     if (index >= length)
       return true;
