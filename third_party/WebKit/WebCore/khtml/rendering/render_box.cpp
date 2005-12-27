8586932fd26c5a67fc4f43b86381bdd9c56b18fa
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2005-12-27 18:39:34 +0000

    LayoutTests:
            Reviewed by Maciej, landed by Darin.
    
            - fixed <http://bugzilla.opendarwin.org/show_bug.cgi?id=4101>
            CSS Fibonacci spiral using 4 images doesn't render correctly
    
            * fast/replaced/absolute-image-sizing-expected.checksum: Added.
            * fast/replaced/absolute-image-sizing-expected.png: Added.
            * fast/replaced/absolute-image-sizing-expected.txt: Added.
            * fast/replaced/absolute-image-sizing.html: Added.
    
    WebCore:
    
            Reviewed by Maciej, landed by Darin.
    
            - fixed <http://bugzilla.opendarwin.org/show_bug.cgi?id=4101>
            CSS Fibonacci spiral using 4 images doesn't render correctly
    
            Added layout tests:
            * fast/replaced/absolute-image-sizing.html
    
            * khtml/rendering/render_box.cpp:
            (RenderBox::calcAbsoluteHorizontalValues): Use calcReplacedWidth() instead of intrinsicWidth()
            for replaced elements.
            (RenderBox::calcAbsoluteVerticalValues): Same thing for height.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_box.cpp
+++ third_party/WebKit/WebCore/khtml/rendering/render_box.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2005 Allan Sandfeld Jensen (kde@carewolf.com)
+ *           (C) 2005 Samuel Weinig (sam.weinig@gmail.com)
  * Copyright (C) 2005 Apple Computer, Inc.
  *
  * This library is free software; you can redistribute it and/or
@@ -1442,7 +1443,7 @@ void RenderBox::calcAbsoluteHorizontalValues(WidthType widthType, RenderObject* 
     if (!width.isIntrinsicOrAuto())
         w = calcContentBoxWidth(width.width(cw));
     else if (isReplaced())
-        w = intrinsicWidth();
+        w = calcReplacedWidth();
 
     if (l != AUTO && w != AUTO && r != AUTO) {
         // left, width, right all given, play with margins
@@ -1620,7 +1621,7 @@ void RenderBox::calcAbsoluteVerticalValues(HeightType heightType, RenderObject* 
             ourHeight = h + pab;
     }
     else if (isReplaced())
-        h = intrinsicHeight();
+        h = calcReplacedHeight();
 
     int static_top=0;
     if ((t == AUTO && b == AUTO) || style()->top().isStatic()) {
