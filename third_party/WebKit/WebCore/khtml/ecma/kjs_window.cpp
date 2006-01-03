fafdc38a68a394f13053742772de835d1fb420ba
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-03 23:21:21 +0000

    Reviewed by harrison
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=4609>
            window.getSelection().toString() is undefined (range object
            returned from window.getSelection() is useless)
    
            toString was defined as a method on the JS Selection object,
            but it didn't return anything.
    
            Added layout test:
            * editing/selection/toString.html
    
            * khtml/ecma/kjs_window.cpp:
            (KJS::SelectionFunc::callAsFunction):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_window.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_window.cpp
@@ -2423,7 +2423,8 @@ JSValue *SelectionFunc::callAsFunction(ExecState *exec, JSObject *thisObj, const
                 break;
             case Selection::GetRangeAt:
                 return getDOMRange(exec, s.getRangeAt(args[0]->toInt32(exec)).get());
-                break;
+            case Selection::ToString:
+                return jsString(s.toString());
         }
         
         part->setSelection(s);
