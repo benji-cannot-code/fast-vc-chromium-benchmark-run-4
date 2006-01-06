1f670eccbb3183517aef021ec976bc0bfa9490d8
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-06 22:49:05 +0000

    Fix build bustage on Mac.
    git-svn-id: svn://svn.chromium.org/blink/trunk@11919 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/internal.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/internal.cpp
@@ -51,9 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if WIN32
 #include <float.h>
 #define copysign(a, b) _copysign(a, b)
+template void * const & KXMLCore::extractFirst<struct std::pair<void *,void *> >(struct std::pair<void *, void *> const &);
 #endif
-
-template void * const & KXMLCore::extractFirst<struct std::pair<void *,void *>>(struct std::pair<void *, void *> const &);
 
 extern int kjsyyparse();
 
