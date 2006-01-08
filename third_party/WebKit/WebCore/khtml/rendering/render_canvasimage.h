d7c1a29fa02a01054cfc7289c0254f8f70ceda29
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-08 06:02:17 +0000

    2006-01-07  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Eric.
    
            - fixed WebCore build
    
            * khtml/rendering/render_canvasimage.h: Remove unneeded extern declarations.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11929 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/rendering/render_canvasimage.h
+++ third_party/WebKit/WebCore/khtml/rendering/render_canvasimage.h
@@ -36,14 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <ApplicationServices/ApplicationServices.h>
 
-extern "C" {
-
-extern void CGContextReplacePathWithStrokedPath(CGContextRef c);
-extern CGImageRef CGBitmapContextCreateImage(CGContextRef c);
-
-}
-
-
 namespace khtml {
 
 class DocLoader;
