70387bbca81456f3425ba1a054e76746d939d7aa
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-11 17:28:05 +0000

    JavaScriptCore:
            Reviewed by Maciej and Darin, landed by Darin.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=5939
              final comma in javascript object prevents parsing
    
            * kjs/grammar.y: Added rule to allow trailing comma in
            object construction.
    
    LayoutTests:
    
            Reviewed by Maciej, reworked and landed by Darin.
    
            - test for http://bugzilla.opendarwin.org/show_bug.cgi?id=5939
              final comma in javascript object prevents parsing
    
            * fast/js/resources/object-extra-comma.js: Added.
            * fast/js/object-extra-comma.html: Added.
            * fast/js/object-extra-comma-expected.txt: Added.
    
            * fast/js/resources/js-test-pre.js: Fixed a typo.
    
            * fast/js/*: Set some MIME types.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/object-extra-comma.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+    'This test checks some object construction cases, including ' +
+    '<a href="http://bugzilla.opendarwin.org/show_bug.cgi?id=5939">5939: final comma in javascript object prevents parsing</a>' +
+    '.');
+
+shouldBe("var foo = { 'bar' : 'YES' }; foo.bar", "'YES'");
+shouldBe("var foo = { 'bar' : 'YES', }; foo.bar", "'YES'");
+shouldBe("var foo = { 'bar' : 'YES' , }; foo.bar", "'YES'");
+shouldThrow("var foo = { , 'bar' : 'YES' }; foo.bar");
+shouldThrow("var foo = { 'bar' : 'YES',, }; foo.bar");
+
+var successfullyParsed = true;
