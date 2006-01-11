2b4d56a380acaf5e1c0b714c21c20d4daee538c7
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-11 16:19:46 +0000

    Reviewed and landed by Darin.
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=6486
              JavaScriptCore should use system malloc on Windows
    
            * kjs/config.h: Add USE_SYSTEM_MALLOC to the Win32 section.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11998 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/config.h
+++ third_party/WebKit/JavaScriptCore/kjs/config.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_FLOAT_H 1
 #define HAVE_FUNC__FINITE 1
 #define HAVE_SYS_TIMEB_H 1
+#define USE_SYSTEM_MALLOC 1
 
 #include <assert.h>
 
