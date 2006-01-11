9531cfc1f4d855bc57b628a8f464b261d475530d
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-11 10:08:12 +0000

    2006-01-11  Anders Carlsson  <andersca@mac.com>
            Build fix.
            
            * ForwardingHeaders/ksvg2/KSVGView.h:
            Rename KHTMLView to FrameView and include FrameView.h
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@11997 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ForwardingHeaders/ksvg2/KSVGView.h
+++ third_party/WebKit/WebCore/ForwardingHeaders/ksvg2/KSVGView.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#include "khtmlview.h"
-class KHTMLView;
+#include "FrameView.h"
+class FrameView;
 namespace KDOM {
-    using ::KHTMLView;
+    using ::FrameView;
 }
