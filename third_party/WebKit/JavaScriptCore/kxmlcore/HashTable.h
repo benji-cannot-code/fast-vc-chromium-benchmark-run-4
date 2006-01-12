f6dfc58c374059ec5fb5d1a43b02fd3a5d5ddf17
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-12 18:23:02 +0000

    Reviewed by Maciej. Add missing assert.h and make it compile with the qmake file.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kxmlcore/HashTable.h
+++ third_party/WebKit/JavaScriptCore/kxmlcore/HashTable.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "FastMalloc.h"
 #include "HashTraits.h"
+#include <assert.h>
 #include <utility>
 #include <algorithm>
 
