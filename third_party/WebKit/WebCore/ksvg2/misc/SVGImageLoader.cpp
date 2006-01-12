46d87e2e1cdcda4707658cd8460652345f694eed
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-12 11:48:45 +0000

    Not reviewed.
            - fixed build breakage
    
            * ksvg2/misc/SVGImageLoader.cpp: Add dom_docimpl.h include
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12021 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/misc/SVGImageLoader.cpp
+++ third_party/WebKit/WebCore/ksvg2/misc/SVGImageLoader.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <kdom/core/AttrImpl.h>
 #include <kdom/cache/KDOMLoader.h>
 #include "DocLoader.h"
+#include "dom_docimpl.h"
 
 #include "SVGNames.h"
 #include "SVGImageElementImpl.h"
