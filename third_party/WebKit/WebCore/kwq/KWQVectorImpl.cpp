f7abccf9b6e4cf97b9d0f65941749835c60a57c4
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-13 08:14:08 +0000

    Oops, land this file again (checked in wrong version last time).
    git-svn-id: svn://svn.chromium.org/blink/trunk@12063 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQVectorImpl.cpp
+++ third_party/WebKit/WebCore/kwq/KWQVectorImpl.cpp
@@ -56,12 +56,12 @@ KWQVectorImpl::~KWQVectorImpl()
 void KWQVectorImpl::clear(bool delItems)
 {
     if (delItems) {
-	for (uint i = 0; i < m_size; ++i) {
+        for (uint i = 0; i < m_size; ++i) {
             void *item = m_data[i];
-	    if (item) {
-		m_deleteItemFunction(item);
-	    }
-	}
+            if (item) {
+                m_deleteItemFunction(item);
+            }
+        }
     }
 
     fastFree(m_data);
@@ -114,9 +114,8 @@ bool KWQVectorImpl::resize(uint size, bool delItems)
 
 bool KWQVectorImpl::insert(uint n, void *item, bool delItems)
 {
-    if (n >= m_size) {
-	return false;
-    }
+    if (n >= m_size)
+        return false;
 
     void *oldItem = m_data[n];
     if (oldItem) {
