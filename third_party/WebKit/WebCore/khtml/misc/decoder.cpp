bd4e76449cd08d1e111468b4199aae2bad29b94a
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-13 05:54:51 +0000

    Make the misc directory compile on Win32.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12056 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/misc/decoder.cpp
+++ third_party/WebKit/WebCore/khtml/misc/decoder.cpp
@@ -47,7 +47,7 @@ using DOM::nullAtom;
 class KanjiCode
 {
 public:
-    enum Type {ASCII, JIS, EUC, SJIS, UNICODE, UTF8 };
+    enum Type { ASCII, JIS, EUC, SJIS, UTF16, UTF8 };
     static enum Type judge(const char *str, int length);
     static const int ESC;
     static const int _SS2_;
