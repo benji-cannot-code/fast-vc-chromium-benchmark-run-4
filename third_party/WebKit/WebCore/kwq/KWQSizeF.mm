00ff16aa42649c6344ba6d34b344750e2156f350
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-13 22:51:57 +0000

    Fix the last bit of bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQSizeF.mm
+++ third_party/WebKit/WebCore/kwq/KWQSizeF.mm
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#import "IntSizeF.h"
+#import "KWQSizeF.h"
 #import "IntSize.h"
 
 QSizeF::QSizeF() : w(-1.0f), h(-1.0f)
