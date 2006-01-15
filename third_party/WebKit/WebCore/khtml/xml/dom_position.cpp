301126159e1049d05a0b93144134a85085a889ea
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-15 04:55:54 +0000

    More khtml namespace bustage, this time on Win32 (caused by the removal of SHared from the khtml namespace.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12102 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/dom_position.cpp
+++ third_party/WebKit/WebCore/khtml/xml/dom_position.cpp
@@ -54,7 +54,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using khtml::EAffinity;
 using khtml::InlineBox;
 using khtml::InlineTextBox;
+using khtml::isAtomicNode;
 using khtml::isCollapsibleWhitespace;
+using khtml::maxDeepOffset;
 using khtml::RenderBlock;
 using khtml::RenderFlow;
 using khtml::RenderObject;
