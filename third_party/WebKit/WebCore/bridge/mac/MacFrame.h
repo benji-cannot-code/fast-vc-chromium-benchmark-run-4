69825228c5866c9056be1d0034fa4f2da1610942
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-15 02:38:23 +0000

    Fix win32 build bustage caused by the file moves all day.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12095 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/mac/MacFrame.h
+++ third_party/WebKit/WebCore/bridge/mac/MacFrame.h
@@ -192,10 +192,13 @@ public:
     QString userAgent() const;
 
     QString mimeTypeForFileName(const QString &) const;
-    
+
+#if __APPLE__
+    // FIXME: This ifdef can be removed when Win32 no longer needs to include MacFrame.h
     NSRect visibleSelectionRect() const;
     NSImage *selectionImage() const;
     NSImage *snapshotDragImage(DOM::NodeImpl *node, NSRect *imageRect, NSRect *elementRect) const;
+#endif
     bool dispatchDragSrcEvent(const DOM::AtomicString &eventType, const IntPoint &loc) const;
 
     NSFont *fontForSelection(bool *hasMultipleFonts) const;
@@ -327,7 +330,10 @@ private:
     
     bool dispatchCPPEvent(const DOM::AtomicString &eventType, KWQClipboard::AccessPolicy policy);
 
+#if __APPLE__
+    // FIXME: This can be removed when Win32 no longer needs to include MacFrame.h
     NSImage *imageFromRect(NSRect rect) const;
+#endif
 
     void freeClipboard();
 
