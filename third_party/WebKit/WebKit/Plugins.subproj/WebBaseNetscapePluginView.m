36fc69e016e800ac33a17d7ea56be62ecafe23e7
tomernic <tomernic@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-16 20:04:31 +0000

    Fixed <rdar://problem/4211707> NPAPI ref count behavior differs with Mozilla
    git-svn-id: svn://svn.chromium.org/blink/trunk@12131 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.m
+++ third_party/WebKit/WebKit/Plugins.subproj/WebBaseNetscapePluginView.m
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebViewPrivate.h>
 #import <WebKit/WebUIDelegate.h>
 #import <WebKitSystemInterface.h>
+#import <JavaScriptCore/npruntime_impl.h>
 
 #import <Carbon/Carbon.h>
 
@@ -1797,8 +1798,15 @@ static OSStatus TSMEventHandler(EventHandlerCallRef inHandlerRef, EventRef inEve
 - (NPError)getVariable:(NPNVariable)variable value:(void *)value
 {
     if (variable == NPNVWindowNPObject) {
+        NPObject *windowScriptObject = [[[self webFrame] _bridge] windowScriptNPObject];
+
+        // Return value is expected to be retained, as described here: <http://www.mozilla.org/projects/plugins/npruntime.html#browseraccess>
+        if (windowScriptObject)
+            _NPN_RetainObject(windowScriptObject);
+        
         void **v = (void **)value;
-        *v = [[[self webFrame] _bridge] windowScriptNPObject];
+        *v = windowScriptObject;
+        
         return NPERR_NO_ERROR;
     }
     return NPERR_GENERIC_ERROR;
