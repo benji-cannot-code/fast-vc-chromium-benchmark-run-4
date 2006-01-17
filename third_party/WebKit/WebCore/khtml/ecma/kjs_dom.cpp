ad436602b1c7e4c0b4148fa50cb1a47bf29d0a22
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-17 20:52:42 +0000

    Land support for a style method on DOMAttr from JS that will         allow the Web Inspector to include presentational attributes         in the style list.
            Reviewed by xenon
    
            * khtml/ecma/kjs_dom.cpp:
            (KJS::DOMAttr::getValueProperty):
            * khtml/ecma/kjs_dom.h:
            (KJS::DOMAttr::):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12154 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_dom.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_dom.cpp
@@ -792,6 +792,7 @@ const ClassInfo DOMAttr::info = { "Attr", &DOMNode::info, &DOMAttrTable, 0 };
   specified     DOMAttr::Specified      DontDelete|ReadOnly
   value         DOMAttr::ValueProperty  DontDelete
   ownerElement  DOMAttr::OwnerElement   DontDelete|ReadOnly
+  style         DOMAttr::Style          DontDelete|ReadOnly
 @end
 */
 
@@ -817,6 +818,8 @@ JSValue *DOMAttr::getValueProperty(ExecState *exec, int token) const
     return jsStringOrNull(attr->value());
   case OwnerElement: // DOM2
     return getDOMNode(exec, attr->ownerElement());
+  case Style: // Web Inspector Extension
+    return getDOMCSSStyleDeclaration(exec, attr->style());
   }
   return NULL; // not reached
 }
