64ebeadb0269746b2fcef202f18c38e3a5dea8d6
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-18 16:51:31 +0000

    2006-01-18  Anders Carlsson  <andersca@mac.com>
            Fix build.
            
            * khtml/ecma/kjs_navigator.cpp:
            * page/FrameView.cpp:
            Remove kstandarddirs.h includes.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_navigator.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_navigator.cpp
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include <klocale.h>
 
-#include <kstandarddirs.h>
 #include <kconfig.h>
 #include <kdebug.h>
 
--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MacFrame.h"
 
 #include <kcursor.h>
-#include <kstandarddirs.h>
 
 #include <qpainter.h>
 #include <qpaintdevicemetrics.h>
