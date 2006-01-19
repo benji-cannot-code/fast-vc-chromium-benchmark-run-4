5ac216e9fdec725fa03965572e3622b8defd27e2
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-19 00:39:28 +0000

    Make sure FrameView includes MacFrame.h on mac only.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -59,6 +59,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CachedImage.h"
 
+#if __APPLE__
+#include "MacFrame.h"
+#endif
+
 // #define INSTRUMENT_LAYOUT_SCHEDULING 1
 
 using namespace DOM;
