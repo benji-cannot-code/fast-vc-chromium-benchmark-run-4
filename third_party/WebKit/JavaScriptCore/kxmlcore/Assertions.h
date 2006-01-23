d457864af15a5d1ad5454d9d8512e92deb5bf599
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-23 07:01:37 +0000

    Rubber stamp from Maciej:  - Assertions is only valid in __APPLE__ right now
    git-svn-id: svn://svn.chromium.org/blink/trunk@12304 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kxmlcore/Assertions.h
+++ third_party/WebKit/JavaScriptCore/kxmlcore/Assertions.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // For non-debug builds, everything is disabled by default.
 // Defining any of the symbols explicitly prevents this from having any effect.
 
-#ifdef WIN32
+#if !__APPLE__
 #define ASSERT_DISABLED 1 // FIXME: We have to undo all the assert macros, since they are currently in a .mm file and use obj-c.
 #else
 #ifdef NDEBUG
