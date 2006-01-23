7d5115a74a25ac712c17d3ceaf848ae0f7cb0b85
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-23 10:14:04 +0000

    - fix layout tests
            * rendering/RenderTable.cpp: (WebCore::RenderTable::addChild): Added
            missing code to set wrapInAnonymousSection false in the case where
            adding a footer.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12310 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTable.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTable.cpp
@@ -122,6 +122,7 @@ void RenderTable::addChild(RenderObject* child, RenderObject* beforeChild)
             if (!foot) {
                 if (child->isTableSection())
                     foot = static_cast<RenderTableSection *>(child);
+                wrapInAnonymousSection = false;
                 break;
             }
             // fall through
