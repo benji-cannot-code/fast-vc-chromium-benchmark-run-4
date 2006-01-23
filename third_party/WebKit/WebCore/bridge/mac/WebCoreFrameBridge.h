69b3d4f6b52aab0ca3277e50880167231b68ba70
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-23 23:38:57 +0000

    Fix for when building in a clean sandbox and WebKit isn't built yet.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/mac/WebCoreFrameBridge.h
+++ third_party/WebKit/WebCore/bridge/mac/WebCoreFrameBridge.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <JavaScriptCore/npruntime.h>
 #import <JavaVM/jni.h>
 #import <WebCore/WebCoreKeyboardAccess.h>
-#import <WebKit/WebView.h>
 
 #ifdef __cplusplus
 
@@ -495,7 +494,7 @@ typedef enum
 - (WebCoreFrameBridge *)mainFrame;
 - (void)frameDetached;
 - (NSView *)documentView;
-- (WebView *)webView;
+- (NSView *)webView;
 
 - (void)loadURL:(NSURL *)URL referrer:(NSString *)referrer reload:(BOOL)reload userGesture:(BOOL)forUser target:(NSString *)target triggeringEvent:(NSEvent *)event form:(DOMElement *)form formValues:(NSDictionary *)values;
 - (void)postWithURL:(NSURL *)URL referrer:(NSString *)referrer target:(NSString *)target data:(NSArray *)data contentType:(NSString *)contentType triggeringEvent:(NSEvent *)event form:(DOMElement *)form formValues:(NSDictionary *)values;
