ec5a561964b5bd4da3973d9e256d95aba7b119f8
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-23 21:41:36 +0000

    Reviewed by Tim Hatcher.
            - it's "Franklin Street", not "Franklin Steet"
    
            * kjs/array_instance.h:
            * kjs/array_object.cpp:
            * kjs/array_object.h:
            * kjs/bool_object.cpp:
            * kjs/bool_object.h:
            * kjs/collector.cpp:
            * kjs/collector.h:
            * kjs/completion.h:
            * kjs/context.h:
            * kjs/date_object.cpp:
            * kjs/date_object.h:
            * kjs/debugger.cpp:
            * kjs/debugger.h:
            * kjs/dtoa.h:
            * kjs/error_object.cpp:
            * kjs/error_object.h:
            * kjs/function.cpp:
            * kjs/function.h:
            * kjs/function_object.cpp:
            * kjs/function_object.h:
            * kjs/grammar.y:
            * kjs/identifier.cpp:
            * kjs/identifier.h:
            * kjs/internal.cpp:
            * kjs/internal.h:
            * kjs/interpreter.cpp:
            * kjs/interpreter.h:
            * kjs/lexer.cpp:
            * kjs/lexer.h:
            * kjs/list.cpp:
            * kjs/list.h:
            * kjs/lookup.cpp:
            * kjs/lookup.h:
            * kjs/math_object.cpp:
            * kjs/math_object.h:
            * kjs/nodes.cpp:
            * kjs/nodes.h:
            * kjs/nodes2string.cpp:
            * kjs/number_object.cpp:
            * kjs/number_object.h:
            * kjs/object.cpp:
            * kjs/object.h:
            * kjs/object_object.cpp:
            * kjs/object_object.h:
            * kjs/operations.cpp:
            * kjs/operations.h:
            * kjs/property_map.cpp:
            * kjs/property_map.h:
            * kjs/property_slot.cpp:
            * kjs/property_slot.h:
            * kjs/reference.cpp:
            * kjs/reference.h:
            * kjs/reference_list.cpp:
            * kjs/reference_list.h:
            * kjs/regexp.cpp:
            * kjs/regexp.h:
            * kjs/regexp_object.cpp:
            * kjs/regexp_object.h:
            * kjs/scope_chain.cpp:
            * kjs/scope_chain.h:
            * kjs/simple_number.h:
            * kjs/string_object.cpp:
            * kjs/string_object.h:
            * kjs/testkjs.cpp:
            * kjs/types.h:
            * kjs/ustring.cpp:
            * kjs/ustring.h:
            * kjs/value.cpp:
            * kjs/value.h:
            * kxmlcore/AlwaysInline.h:
            * kxmlcore/ListRefPtr.h:
            * kxmlcore/PassRefPtr.h:
            * kxmlcore/RefPtr.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/date_object.h
+++ third_party/WebKit/JavaScriptCore/kjs/date_object.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301  USA
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 
--- third_party/WebKit/JavaScriptCore/kjs/lexer.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/lexer.cpp
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  *  You should have received a copy of the GNU Library General Public License
  *  along with this library; see the file COPYING.LIB.  If not, write to
- *  the Free Software Foundation, Inc., 51 Franklin Steet, Fifth Floor,
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  *  Boston, MA 02110-1301, USA.
  *
  */
