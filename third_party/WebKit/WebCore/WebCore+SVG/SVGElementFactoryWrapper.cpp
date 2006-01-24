af4009e7e00925de4dab44c23e3d2a383da67ae6
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-24 10:13:54 +0000

    Touch the wrapper so it rebuilds. We need a better solution for this.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12333 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCore+SVG/SVGElementFactoryWrapper.cpp
+++ third_party/WebKit/WebCore/WebCore+SVG/SVGElementFactoryWrapper.cpp
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if SVG_SUPPORT
+// Added this comment to touch the wrapper so it rebuilds.
 #include "SVGElementFactory.cpp"
 #endif //SVG_SUPPORT
