fd2b0d6b5469008efc46fcad6a2aa1c3e0eb37bd
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-27 06:05:17 +0000

    Reviewed by Eric.
            - various leaks on "fast" LayoutTests
            
            http://bugzilla.opendarwin.org/show_bug.cgi?id=6819
            http://bugzilla.opendarwin.org/show_bug.cgi?id=6823
            http://bugzilla.opendarwin.org/show_bug.cgi?id=6824
            
            It turns out these all had the same cause.
            
            * page/Frame.cpp:
            (Frame::clear): If we have a document, make sure to cancel parsing,
            in case it has a tokenizer and parser that are keeping it alive. Use
            cancelParsing to avoid possibly accidentally firing the onload handler.
            * khtml/xml/DocumentImpl.cpp:
            (WebCore::DocumentImpl::cancelParsing): Factored out of implicitOpen.
            (WebCore::DocumentImpl::implicitOpen): Call cancelParsing now.
            * khtml/xml/DocumentImpl.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/DocumentImpl.h
+++ third_party/WebKit/WebCore/khtml/xml/DocumentImpl.h
@@ -277,6 +277,8 @@ public:
     void implicitOpen();
     void close();
     void implicitClose();
+    void cancelParsing();
+
     void write ( const DOMString &text );
     void write ( const QString &text );
     void writeln ( const DOMString &text );
