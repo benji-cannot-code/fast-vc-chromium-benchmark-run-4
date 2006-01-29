2a22b93441e4d7aedb6f723ba43eb6a80f2fdaf9
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-29 01:50:32 +0000

    Eliminate three unused methods on Image: increase/DecreaseUseCount and flushRasterCache.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12433 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
+++ third_party/WebKit/WebKit/WebCoreSupport.subproj/WebImageRenderer.m
@@ -249,18 +249,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self _startOrContinueAnimationIfNecessary];
 }
 
-- (void)increaseUseCount
-{
-}
-
-- (void)decreaseUseCount
-{
-}
-
-- (void)flushRasterCache
-{
-}
-
 - (CGImageRef)imageRef
 {
     return [imageData imageAtIndex:0];
