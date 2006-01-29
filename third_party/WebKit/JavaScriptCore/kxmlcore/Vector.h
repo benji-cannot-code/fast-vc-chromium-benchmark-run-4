e72ba4b2c77b642ef3392dcd4e928e70ffc5176a
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-29 06:47:45 +0000

    Reviewed by Darin.
            - miscellaneous Vector improvements
    
            * kxmlcore/Vector.h:
            (KXMLCore::Vector::at): Add range-checking asserts.
            (KXMLCore::Vector::first): Added as a convenience.
            (KXMLCore::Vector::last): Convenience for stack-style use.
            (KXMLCore::Vector::removeLast): ditto
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kxmlcore/Vector.h
+++ third_party/WebKit/JavaScriptCore/kxmlcore/Vector.h
@@ -26,8 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Assertions.h"
 #include <stdlib.h>
-#include "VectorTraits.h"
 #include <utility>
+#include "VectorTraits.h"
 
 namespace KXMLCore {
 
@@ -309,8 +309,17 @@ namespace KXMLCore {
         size_t capacity() const { return m_impl.capacity(); }
         bool isEmpty() const { return !size(); }
 
-        T& at(size_t i) { return m_impl.buffer()[i]; }
-        const T& at(size_t i) const { return m_impl.buffer()[i]; }
+        T& at(size_t i) 
+        { 
+            ASSERT(i < size());
+            return m_impl.buffer()[i]; 
+        }
+        const T& at(size_t i) const 
+        {
+            ASSERT(i < size());
+            return m_impl.buffer()[i]; 
+        }
+
         T& operator[](size_t i) { return at(i); }
         const T& operator[](size_t i) const { return at(i); }
         T& operator[](int i) { return at(i); }
@@ -326,6 +335,11 @@ namespace KXMLCore {
         const_iterator begin() const { return data(); }
         const_iterator end() const { return begin() + m_size; }
         
+        T& first() { return at(0); }
+        const T& first() const { return at(0); }
+        T& last() { return at(size() - 1); }
+        const T& last() const { return at(size() - 1); }
+
         void resize(size_t size);
         void reserveCapacity(size_t newCapacity);
 
@@ -333,6 +347,12 @@ namespace KXMLCore {
 
         template<typename U>
         void append(const U& u);
+
+        void removeLast() 
+        {
+            ASSERT(!isEmpty());
+            resize(size() - 1); 
+        }
 
         Vector(size_t size, const T& val)
             : m_size(size)
