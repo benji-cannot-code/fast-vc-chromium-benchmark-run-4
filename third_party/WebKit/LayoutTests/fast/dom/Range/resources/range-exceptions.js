477458807d393eac7e3c039f89b86b8382509d33
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-29 18:11:03 +0000

    - added a test for the names of range exceptions, which were wrong           in my last commit until I made the fix Alexey suggested
            * fast/dom/Range: Added.
            * fast/dom/Range/range-exceptions.html: Added.
            * fast/dom/Range/resources: Added.
            * fast/dom/Range/resources/range-exceptions.js: Added.
            * fast/dom/Range/resources/TEMPLATE.html: Added.
    
            * fast/js/resources/js-test-pre.js: Escape & and < characters in the
            output so you can have tests with HTML fragments as expected results.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12458 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/Range/resources/range-exceptions.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This test checks a few DOM range exceptions."
+);
+
+// Test to be sure the name BAD_BOUNDARYPOINTS_ERR dumps properly.
+var node = document.createElement("DIV");
+node.innerHTML = "<BAR>AB<MOO>C</MOO>DE</BAR>";
+shouldBe("node.innerHTML", "'<BAR>AB<MOO>C</MOO>DE</BAR>'");
+var range = document.createRange();
+range.setStart(node.firstChild, 1);
+range.setEnd(node.firstChild, 2);
+var foo = document.createElement("FOO");
+shouldBe("foo.outerHTML", "'<FOO></FOO>'");
+shouldThrow("range.surroundContents(foo)");
+
+var successfullyParsed = true;
