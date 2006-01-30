9cf7c4892a8793aa37bc1fe48aace268a2f9a255
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-30 07:13:37 +0000

    2006-01-29  Eric Seidel <eseidel@apple.com>
            Reviewed by darin.
    
            Added back #include <qvaluelist.h> to fix WIN32 build.
    
            * rendering/render_style.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12472 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/render_style.h
+++ third_party/WebKit/WebCore/rendering/render_style.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qfontmetrics.h>
 #include <qlist.h>
 #include <qpalette.h>
+#include <qvaluelist.h>
 
 #include "khtmllayout.h"
 #include "Shared.h"
