727788a23c98b2504df9cea282001023d523c11a
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-30 04:50:26 +0000

    2006-01-29  Eric Seidel <eseidel@apple.com>
            Reviewed by hyatt.
    
            Fix build on Win32.
    
            * kjs/lookup.h: fixed ::cacheGlobalObject
            * kxmlcore/Vector.h:
            (KXMLCore::Vector::operator[]): use unsigned long
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12468 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/lookup.h
+++ third_party/WebKit/JavaScriptCore/kjs/lookup.h
@@ -296,7 +296,7 @@ inline KJS::JSObject *cacheGlobalObject(KJS::ExecState *exec, const KJS::Identif
  */
 #define KJS_DEFINE_PROTOTYPE(ClassProto) \
   class ClassProto : public KJS::JSObject { \
-    friend KJS::JSObject *cacheGlobalObject<ClassProto>(KJS::ExecState *exec, const KJS::Identifier &propertyName); \
+  friend KJS::JSObject *::cacheGlobalObject<ClassProto>(KJS::ExecState *exec, const KJS::Identifier &propertyName); \
   public: \
     static KJS::JSObject *ClassProto::self(KJS::ExecState *exec); \
     virtual const KJS::ClassInfo *classInfo() const { return &info; } \
@@ -312,7 +312,7 @@ inline KJS::JSObject *cacheGlobalObject(KJS::ExecState *exec, const KJS::Identif
     const ClassInfo ClassProto::info = { ClassName, 0, &ClassProto##Table, 0 }; \
     JSObject *ClassProto::self(ExecState *exec) \
     { \
-      return cacheGlobalObject<ClassProto>(exec, "[[" ClassName ".prototype]]"); \
+        return ::cacheGlobalObject<ClassProto>(exec, "[[" ClassName ".prototype]]"); \
     } \
     bool ClassProto::getOwnPropertySlot(ExecState *exec, const Identifier& propertyName, PropertySlot& slot) \
     { \
@@ -323,7 +323,7 @@ inline KJS::JSObject *cacheGlobalObject(KJS::ExecState *exec, const KJS::Identif
     const ClassInfo ClassProto::info = { ClassName, 0, &ClassProto##Table, 0 }; \
     JSObject *ClassProto::self(ExecState *exec) \
     { \
-      return cacheGlobalObject<ClassProto>(exec, "[[" ClassName ".prototype]]"); \
+        return ::cacheGlobalObject<ClassProto>(exec, "[[" ClassName ".prototype]]"); \
     } \
     bool ClassProto::getOwnPropertySlot(ExecState *exec, const Identifier& propertyName, PropertySlot& slot) \
     { \
