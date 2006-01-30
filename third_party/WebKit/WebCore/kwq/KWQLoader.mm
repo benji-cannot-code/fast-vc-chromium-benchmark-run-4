b03e5c12bd27f972820e5f185df13430bc310d26
bdakin <bdakin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-01-30 18:45:20 +0000

    eviewed by Geoff
            Fix for <rdar://problem/4298052> Crash in KWQCheckIfReloading at 
            http://www.inquisitorx.com/beta/
    
            It is possible for the DocLoader's frame to be nil when the 
            DocLoader is not nil, so we need nil checks.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12478 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQLoader.mm
+++ third_party/WebKit/WebCore/kwq/KWQLoader.mm
@@ -111,6 +111,10 @@ bool KWQServeRequest(Loader *loader, Request *request, TransferJob *job)
 bool KWQServeRequest(Loader *loader, DocLoader *docLoader, TransferJob *job)
 {
     MacFrame *frame = static_cast<MacFrame *>(docLoader->frame());
+    
+    if (!frame)
+        return false;
+    
     WebCoreFrameBridge *bridge = frame->bridge();
 
     frame->didTellBridgeAboutLoad(job->url().url());
@@ -168,6 +172,10 @@ NSString *KWQHeaderStringFromDictionary(NSDictionary *headers, int statusCode)
 ByteArray KWQServeSynchronousRequest(Loader *loader, DocLoader *docLoader, TransferJob *job, KURL &finalURL, QString &responseHeaders)
 {
     MacFrame *frame = static_cast<MacFrame *>(docLoader->frame());
+    
+    if (!frame)
+        return ByteArray();
+    
     WebCoreFrameBridge *bridge = frame->bridge();
 
     frame->didTellBridgeAboutLoad(job->url().url());
@@ -216,7 +224,8 @@ int KWQNumberOfPendingOrLoadingRequests(khtml::DocLoader *dl)
 bool KWQCheckIfReloading(DocLoader *loader)
 {
     KWQ_BLOCK_EXCEPTIONS;
-    return [static_cast<MacFrame *>(loader->frame())->bridge() isReloading];
+    if (MacFrame *frame = static_cast<MacFrame *>(loader->frame()))
+        return [frame->bridge() isReloading];
     KWQ_UNBLOCK_EXCEPTIONS;
 
     return false;
@@ -244,7 +253,7 @@ void KWQCheckCacheObjectStatus(DocLoader *loader, CachedObject *cachedObject)
     // Notify the caller that we "loaded".
     MacFrame *frame = static_cast<MacFrame *>(loader->frame());
 
-    if (!frame->haveToldBridgeAboutLoad(cachedObject->url())) {
+    if (frame && !frame->haveToldBridgeAboutLoad(cachedObject->url())) {
         WebCoreFrameBridge *bridge = frame->bridge();
         
         KWQ_BLOCK_EXCEPTIONS;
@@ -329,8 +338,10 @@ time_t KWQCacheObjectExpiresTime(khtml::DocLoader *docLoader, NSURLResponse *res
     KWQ_BLOCK_EXCEPTIONS;
     
     MacFrame *frame = static_cast<MacFrame *>(docLoader->frame());
-    WebCoreFrameBridge *bridge = frame->bridge();
-    return [bridge expiresTimeForResponse:(NSURLResponse *)response];
+    if (frame) {
+        WebCoreFrameBridge *bridge = frame->bridge();
+        return [bridge expiresTimeForResponse:(NSURLResponse *)response];
+    }
     
     KWQ_UNBLOCK_EXCEPTIONS;
     
