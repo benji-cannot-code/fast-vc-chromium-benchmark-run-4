9fdd6337c60f61845829180adb09e1e35a59199a
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-01 06:24:32 +0000

    Reviewed by Darin.
            - fixed leak of hundreds of thousands of JS parser nodes on the layout tests, and added an exit counter
            that would catch them
    
            * kjs/nodes.cpp:
            (NodeCounter::~NodeCounter): Added debug-only node counter.
            (Node::Node):
            (Node::~Node):
            * kxmlcore/VectorTraits.h: Simple classes like RefPtr do in fact need destruction.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12512 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kxmlcore/VectorTraits.h
+++ third_party/WebKit/JavaScriptCore/kxmlcore/VectorTraits.h
@@ -79,7 +79,7 @@ namespace KXMLCore {
 
     struct SimpleClassVectorTraits
     {
-        static const bool needsDestruction = false;
+        static const bool needsDestruction = true;
         static const bool needsInitialization = true;
         static const bool canInitializeWithMemset = true;
         static const bool canMoveWithMemcpy = true;
