e6bccb49c86e88b81e489bf5bbc196d7a5816be1
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-03 07:18:00 +0000

    Reviewed by Adele.
            - remove all use of the ChildFrame from outside Frame.cpp, in preparation for removing it
    
            * bridge/mac/MacFrame.h:
            * bridge/mac/MacFrame.mm:
            (WebCore::MacFrame::frameDetached): Factor out the ChildFrame manipulations into a base
            class method.
            (WebCore::MacFrame::objectContentType): factored out from createPart, calls the bridge to
            determine contents for an Object tag.
            (WebCore::MacFrame::createPlugin): factor out code from createPart that makes a Plugin.
            (WebCore::MacFrame::createFrame): ditto for code that makes a Frame.
            (WebCore::MacFrame::createEmptyDocument): factored out part into the superclass
            * khtml/xml/DocumentImpl.cpp:
            (WebCore::DocumentImpl::ownerElement): Call new ownerElement method on Frame
            instead of mucking at Frame internals.
            * page/Frame.cpp:
            (WebCore::Frame::createPart): Moved here from MacFrame, but some of the guts were
            factored out.
            (WebCore::Frame::ownerElement): New method, logic moved from DocumentImpl.
            (WebCore::Frame::frameDetached): New virtual method, the part of this that
            pokes at ChildFrame structures.
            (WebCore::Frame::updateBaseURLForEmptyDocument): Helper that looks at the ChildFrame
            array for the benefit of createEmptyDocument.
            * page/Frame.h:
            * page/Page.h: Fix up modeline.
            * page/Plugin.h: Added.
            (WebCore::Plugin::Plugin): Moved here from MacFrame.mm, since it is now used by Frame.
            (WebCore::Plugin::view): ditto
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12542 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/DocumentImpl.cpp
+++ third_party/WebKit/WebCore/khtml/xml/DocumentImpl.cpp
@@ -31,8 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DocumentFragmentImpl.h"
 #include "DocumentTypeImpl.h"
 #include "EventNames.h"
-#include "FramePrivate.h"
+#include "Frame.h"
+#include "FrameTreeNode.h"
 #include "FrameView.h"
+#include "SelectionController.h"
 #include "KWQAccObjectCache.h"
 #include "KWQEvent.h"
 #include "KWQLogging.h"
@@ -2343,22 +2345,9 @@ void DocumentImpl::imageLoadEventTimerFired(Timer<DocumentImpl>*)
 
 ElementImpl *DocumentImpl::ownerElement()
 {
-    FrameView *childView = view();
-    if (!childView)
+    if (!frame())
         return 0;
-    Frame *childPart = childView->frame();
-    if (!childPart)
-        return 0;
-    Frame *parent = childPart->treeNode()->parent();
-    if (!parent)
-        return 0;
-    ChildFrame *childFrame = parent->childFrame(childPart);
-    if (!childFrame)
-        return 0;
-    RenderPart *renderPart = childFrame->m_renderer;
-    if (!renderPart)
-        return 0;
-    return static_cast<ElementImpl *>(renderPart->element());
+    return frame()->ownerElement();
 }
 
 DOMString DocumentImpl::referrer() const
