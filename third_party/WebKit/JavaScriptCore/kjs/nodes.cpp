633da25d7de49410105defb1a707ba475f9c7823
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-05 01:43:16 +0000

    Reviewed by Maciej.
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=5210
              REGRESSION: for/in loop with var changes global variable instead of making local
    
            Test: fast/js/for-in-var-scope.html
    
            * kjs/nodes.cpp:
            (valueForReadModifyAssignment): Use ALWAYS_INLINE macro.
            (ForInNode::execute): Break out of the scope chain loop once we find and set the
            loop variable. We don't want to set multiple loop variables.
            (ForInNode::processVarDecls): Process the declaration of the loop variable.
    
            - other cleanup
    
            * kjs/object.cpp: (KJS::tryGetAndCallProperty): Use ALWAYS_INLINE macro.
            * kxmlcore/FastMalloc.cpp: Change to use ALWAYS_INLINE macro from AlwaysInline.h
            instead of defining it here a second time.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
@@ -1238,12 +1238,7 @@ JSValue *ConditionalNode::evaluate(ExecState *exec)
 
 // ECMA 11.13
 
-#if __GNUC__
-// gcc refuses to inline this without the always_inline, but inlining it does help
-static inline JSValue *valueForReadModifyAssignment(ExecState * exec, JSValue *v1, JSValue *v2, Operator oper) __attribute__((always_inline));
-#endif
-
-static inline JSValue *valueForReadModifyAssignment(ExecState * exec, JSValue *v1, JSValue *v2, Operator oper)
+static ALWAYS_INLINE JSValue *valueForReadModifyAssignment(ExecState * exec, JSValue *v1, JSValue *v2, Operator oper)
 {
   JSValue *v;
   int i1;
@@ -1860,9 +1855,10 @@ Completion ForInNode::execute(ExecState *exec)
         JSObject *o;
         do { 
             o = *iter;
-            if (o->getPropertySlot(exec, ident, slot))
+            if (o->getPropertySlot(exec, ident, slot)) {
                 o->put(exec, ident, str);
-            
+                break;
+            }
             ++iter;
         } while (iter != end);
         
@@ -1916,6 +1912,8 @@ Completion ForInNode::execute(ExecState *exec)
 
 void ForInNode::processVarDecls(ExecState *exec)
 {
+  if (varDecl)
+    varDecl->processVarDecls(exec);
   statement->processVarDecls(exec);
 }
 
