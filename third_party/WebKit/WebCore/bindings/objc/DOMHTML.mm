0fb6b50322dc55ec026081c1dd1cb4dd7dbf8933
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-05 20:31:03 +0000

    Reviewed by Geoff.
            - fix <rdar://problem/4197997> -[DOMHTMLSelectElement options] always returns an empty list
    
            * khtml/html/HTMLOptionsCollectionImpl.h: Changed to be a subclass of HTMLCollectionImpl.
            * khtml/html/HTMLOptionsCollectionImpl.cpp: Added.
    
            * WebCore.vcproj/WebCore/WebCore.vcproj: Added new source file.
            * WebCore.xcodeproj/project.pbxproj: Ditto.
    
            * bindings/objc/DOMHTML.mm:
            (-[DOMHTMLOptionsCollection setLength:]): Handle possible exception. 
            (-[DOMHTMLSelectElement options]): Add get() since this is now a PassRefPtr.
    
            * khtml/ecma/kjs_html.cpp:
            (KJS::HTMLElement::selectIndexGetter): Call options() instead of optionsHTMLCollection().
            (KJS::HTMLElement::selectGetter): Ditto.
            (KJS::KJS::HTMLElement::put): Ditto.
            (KJS::HTMLElement::selectSetter): Ditto.
            (KJS::KJS::HTMLSelectCollection::put): Ditto.
    
            * khtml/html/HTMLSelectElementImpl.h: Tweaked formatting. Eliminated m_options.
            Changed m_minwidth and m_size to int instead of short. Removed optionsHTMLCollection().
            Changed options() to return a PassRefPtr.
            * khtml/html/HTMLSelectElementImpl.cpp:
            (WebCore::HTMLSelectElementImpl::~HTMLSelectElementImpl): Get rid of code to drop m_options.
            (WebCore::HTMLSelectElementImpl::remove): Remove unnecesssary RefPtr use, since that's
            handled fine by removeChild.
            (WebCore::HTMLSelectElementImpl::options): Change to create a new HTMLOptionsCollectionImpl
            each time, like other collection calls. Maybe we should change this eventually to use a
            HashMap so we can pass out the same collection over and over again, or could have a pointer
            stored in the select element that's managed by the collection itself.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12586 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/DOMHTML.mm
+++ third_party/WebKit/WebCore/bindings/objc/DOMHTML.mm
@@ -226,7 +226,9 @@ using namespace DOM::HTMLNames;
 
 - (void)setLength:(unsigned)length
 {
-    [self _optionsCollectionImpl]->setLength(length);
+    ExceptionCode ec;
+    [self _optionsCollectionImpl]->setLength(length, ec);
+    raiseOnDOMError(ec);
 }
 
 - (DOMNode *)item:(unsigned)index
@@ -1031,7 +1033,7 @@ using namespace DOM::HTMLNames;
 
 - (DOMHTMLOptionsCollection *)options
 {
-    return [DOMHTMLOptionsCollection _optionsCollectionWithImpl:[self _selectElementImpl]->options()];
+    return [DOMHTMLOptionsCollection _optionsCollectionWithImpl:[self _selectElementImpl]->options().get()];
 }
 
 - (BOOL)disabled
--- third_party/WebKit/WebCore/khtml/html/HTMLSelectElementImpl.h
+++ third_party/WebKit/WebCore/khtml/html/HTMLSelectElementImpl.h
@@ -27,26 +27,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef HTML_HTMLSelectElementImpl_H
 #define HTML_HTMLSelectElementImpl_H
 
+#include "Array.h"
 #include "HTMLGenericFormElementImpl.h"
 
-#include "Array.h"
-
-namespace khtml {
-    class RenderSelect;
-};
-
-namespace DOM {
+namespace WebCore {
 
 class HTMLOptionElementImpl;
 class HTMLOptionsCollectionImpl;
+class RenderSelect;
 
-class HTMLSelectElementImpl : public HTMLGenericFormElementImpl
-{
-    friend class khtml::RenderSelect;
+class HTMLSelectElementImpl : public HTMLGenericFormElementImpl {
+    friend class RenderSelect;
 
 public:
-    HTMLSelectElementImpl(DocumentImpl *doc, HTMLFormElementImpl *f = 0);
-    HTMLSelectElementImpl(const QualifiedName& tagName, DocumentImpl *doc, HTMLFormElementImpl *f = 0);
+    HTMLSelectElementImpl(DocumentImpl*, HTMLFormElementImpl* = 0);
+    HTMLSelectElementImpl(const QualifiedName& tagName, DocumentImpl*, HTMLFormElementImpl* = 0);
     ~HTMLSelectElementImpl();
 
     virtual int tagPriority() const { return 6; }
@@ -54,10 +49,10 @@ public:
 
     DOMString type() const;
 
-    virtual void recalcStyle( StyleChange );
+    virtual void recalcStyle(StyleChange);
 
     int selectedIndex() const;
-    void setSelectedIndex( int index );
+    void setSelectedIndex(int index);
 
     virtual bool isEnumeratable() const { return true; }
 
@@ -69,14 +64,13 @@ public:
 
     bool multiple() const { return m_multiple; }
 
-    void add ( HTMLElementImpl *element, HTMLElementImpl *before, int &exceptioncode );
-    void remove ( int index );
+    void add(HTMLElementImpl* element, HTMLElementImpl* before, ExceptionCode&);
+    void remove(int index);
 
     DOMString value();
-    void setValue(const DOMString &);
+    void setValue(const DOMString&);
     
-    HTMLOptionsCollectionImpl* options();
-    RefPtr<HTMLCollectionImpl> optionsHTMLCollection(); // FIXME: Remove this and migrate to options().
+    PassRefPtr<HTMLOptionsCollectionImpl> options();
 
     virtual bool maintainsState() { return true; }
     virtual QString state();
@@ -90,9 +84,9 @@ public:
 
     virtual void childrenChanged();
 
-    virtual void parseMappedAttribute(MappedAttributeImpl *attr);
+    virtual void parseMappedAttribute(MappedAttributeImpl*);
 
-    virtual khtml::RenderObject *createRenderer(RenderArena *, khtml::RenderStyle *);
+    virtual RenderObject* createRenderer(RenderArena*, RenderStyle *);
     virtual bool appendFormData(FormDataList&, bool);
 
     // get the actual listbox index of the optionIndexth option
@@ -108,9 +102,9 @@ public:
         return m_listItems;
     }
     virtual void reset();
-    void notifyOptionSelected(HTMLOptionElementImpl *selectedOption, bool selected);
+    void notifyOptionSelected(HTMLOptionElementImpl* selectedOption, bool selected);
 
-    virtual void defaultEventHandler(EventImpl *evt);
+    virtual void defaultEventHandler(EventImpl*);
     virtual void accessKeyAction(bool sendToAnyElement);
 
     void setMultiple(bool);
@@ -120,11 +114,9 @@ public:
 private:
     void recalcListItems();
 
-protected:
     mutable Array<HTMLElementImpl*> m_listItems;
-    RefPtr<HTMLOptionsCollectionImpl> m_options;
-    short m_minwidth;
-    short m_size;
+    int m_minwidth;
+    int m_size;
     bool m_multiple;
     bool m_recalcListItems;
 };
