c245bfd97901efd8b5e2c1133c9ef9944627d85a
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-05 08:29:13 +0000

    Fix by David Kilzer, reviewed by Darin, landed by ap (with a tweak suggested by Darin).
            - Fixed --no-svg build on http://build.webkit.org/.
    
            * kwq/KWQPainter.mm: Include "FloatRect.h".
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12569 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQPainter.mm
+++ third_party/WebKit/WebCore/kwq/KWQPainter.mm
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "KWQFoundationExtras.h"
 #import "Pen.h"
 #import "Image.h"
+#import "FloatRect.h"
 #import "KWQPrinter.h"
 #import "KWQRegion.h"
 #import "KWQFontMetrics.h"
