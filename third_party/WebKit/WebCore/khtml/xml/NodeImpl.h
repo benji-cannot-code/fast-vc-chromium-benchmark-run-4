42b0db1652d7d6b771d2741f760f46492a8f1a4f
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-05 19:00:30 +0000

    - rolled out Maciej's event cleanup patch, which is causing problems
            I believe he has a fix, and he'll re-land the patch with the fix later.
    
            Original fix was <http://bugzilla.opendarwin.org/show_bug.cgi?id=7079>.
            Problems described in <http://bugzilla.opendarwin.org/show_bug.cgi?id=7084>.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/xml/NodeImpl.h
+++ third_party/WebKit/WebCore/khtml/xml/NodeImpl.h
@@ -273,7 +273,7 @@ public:
     void removeAllEventListeners();
 
     bool dispatchEvent(PassRefPtr<EventImpl>, ExceptionCode&, bool tempEvent = false);
-    bool dispatchGenericEvent(PassRefPtr<EventImpl>, ExceptionCode&, bool tempEvent = false);
+    bool dispatchGenericEvent(PassRefPtr<EventImpl>, ExceptionCode&);
     bool dispatchHTMLEvent(const AtomicString& eventType, bool canBubble, bool cancelable);
     bool dispatchWindowEvent(const AtomicString& eventType, bool canBubble, bool cancelable);
     bool dispatchMouseEvent(QMouseEvent*, const AtomicString& overrideType,
