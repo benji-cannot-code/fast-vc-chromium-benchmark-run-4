974d5a1237ffdb8f6e2a8c0d7ce32034449b8943
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-05 01:48:00 +0000

    Reviewed by Maciej.
            * Misc.subproj/WebCoreStatistics.h:
            * Misc.subproj/WebCoreStatistics.m:
            (+[WebCoreStatistics javaScriptRootObjectTypeCounts]):
            javaScriptRootObjecTypeCounts -> javaScriptRootObjectTypeCounts
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12566 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc.subproj/WebCoreStatistics.m
+++ third_party/WebKit/WebKit/Misc.subproj/WebCoreStatistics.m
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2005, 2006 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -82,7 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return [WebCoreJavaScript rootObjectTypeCounts];
 }
 
-+ (NSCountedSet *)javaScriptRootObjecTypeCounts
++ (NSCountedSet *)javaScriptRootObjectTypeCounts
 {
     return [WebCoreJavaScript rootObjectTypeCounts];
 }
