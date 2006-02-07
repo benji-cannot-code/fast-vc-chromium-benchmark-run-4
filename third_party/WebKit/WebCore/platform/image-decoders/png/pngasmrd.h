e7cc2a11bf225bfc46875f0127e44a5f49f479d5
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-07 11:31:51 +0000

    Add libpng and the zlib supporting library in preparation for iplementing PNG decoding.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebCore/platform/image-decoders/png/pngasmrd.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* pngasmrd.h - assembler version of utilities to read a PNG file
+ *
+ * libpng 1.2.5 - October 2, 2002
+ * For conditions of distribution and use, see copyright notice in png.h
+ * Copyright (c) 2002 Glenn Randers-Pehrson
+ *
+ */
+
+/* This file is obsolete in libpng-1.0.9 and later; its contents now appear
+ * at the end of pngconf.h.
+ */
