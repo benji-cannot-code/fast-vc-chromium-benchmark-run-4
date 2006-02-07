ae1e5024e29cd1e070b9f8a5831d440c0c855c52
vicki <vicki@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-07 03:39:54 +0000

    Reviewed by Darin and Eric.
            - fix <rdar://problem/4414918> REGRESSION: images dragged into contenteditable area
            end up outside of editable area (6525)
    
            * bridge/mac/WebCoreFrameBridge.mm:
            (-[WebCoreFrameBridge RenderObject::nodeInfoAtPoint:]): Moved code from
            elementAtPoint to this new method.  This method takes a point and returns a NodeInfo
            for the deepest node at that point.
            (-[WebCoreFrameBridge elementAtPoint:]): Use new nodeInfoAtPoint method.
            (-[WebCoreFrameBridge _visiblePositionForPoint:]): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12610 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/mac/WebCoreFrameBridge.mm
+++ third_party/WebKit/WebCore/bridge/mac/WebCoreFrameBridge.mm
@@ -107,6 +107,8 @@ using KJS::Window;
 
 using KJS::Bindings::RootObject;
 
+using WebCore::RenderObject;
+
 NSString *WebCoreElementDOMNodeKey =            @"WebElementDOMNode";
 NSString *WebCoreElementFrameKey =              @"WebElementFrame";
 NSString *WebCoreElementImageAltStringKey =     @"WebElementImageAltString";
@@ -124,6 +126,7 @@ NSString *WebCorePageCacheStateKey =            @"WebCorePageCacheState";
 
 @interface WebCoreFrameBridge (WebCoreBridgeInternal)
 - (RootObject *)executionContextForView:(NSView *)aView;
+- (RenderObject::NodeInfo)nodeInfoAtPoint:(NSPoint)point;
 @end
 
 static RootObject *rootForView(void *v)
@@ -1244,33 +1247,7 @@ static HTMLFormElementImpl *formElementFromDOMElement(DOMElement *element)
     if (!renderer) 
         return nil;
     
-    RenderObject::NodeInfo nodeInfo(true, true);
-    renderer->layer()->hitTest(nodeInfo, (int)point.x, (int)point.y);
-
-    NodeImpl *n;
-    QWidget *widget = 0;
-    IntPoint widgetPoint(point);
-    
-    while (true) {
-        n = nodeInfo.innerNode();
-        if (!n || !n->renderer() || !n->renderer()->isWidget())
-            break;
-        widget = static_cast<RenderWidget *>(n->renderer())->widget();
-        if (!widget || !widget->inherits("FrameView"))
-            break;
-        Frame *kpart = static_cast<HTMLFrameElementImpl *>(n)->contentPart();
-        if (!kpart || !static_cast<MacFrame *>(kpart)->renderer())
-            break;
-        int absX, absY;
-        n->renderer()->absolutePosition(absX, absY, true);
-        FrameView *view = static_cast<FrameView *>(widget);
-        widgetPoint.setX(widgetPoint.x() - absX + view->contentsX());
-        widgetPoint.setY(widgetPoint.y() - absY + view->contentsY());
-
-        RenderObject::NodeInfo widgetNodeInfo(true, true);
-        static_cast<MacFrame *>(kpart)->renderer()->layer()->hitTest(widgetNodeInfo, widgetPoint.x(), widgetPoint.y());
-        nodeInfo = widgetNodeInfo;
-    }
+    RenderObject::NodeInfo nodeInfo = [self nodeInfoAtPoint:point];
     
     NSMutableDictionary *element = [NSMutableDictionary dictionary];
     [element setObject:[NSNumber numberWithBool:m_frame->isPointInsideSelection((int)point.x, (int)point.y)]
@@ -2226,8 +2203,8 @@ static HTMLFormElementImpl *formElementFromDOMElement(DOMElement *element)
         return VisiblePosition();
     }
     
-    RenderObject::NodeInfo nodeInfo(true, true);
-    renderer->layer()->hitTest(nodeInfo, (int)point.x, (int)point.y);
+    RenderObject::NodeInfo nodeInfo = [self nodeInfoAtPoint:point];
+    
     NodeImpl *node = nodeInfo.innerNode();
     if (!node || !node->renderer())
         return VisiblePosition();
@@ -2611,6 +2588,41 @@ static HTMLFormElementImpl *formElementFromDOMElement(DOMElement *element)
     root->setInterpreter(frame->jScript()->interpreter());
     frame->addPluginRootObject(root);
     return root;
+}
+
+- (RenderObject::NodeInfo)nodeInfoAtPoint:(NSPoint)point
+{
+    RenderObject *renderer = m_frame->renderer();
+
+    RenderObject::NodeInfo nodeInfo(true, true);
+    renderer->layer()->hitTest(nodeInfo, (int)point.x, (int)point.y);
+
+    NodeImpl *n;
+    QWidget *widget = 0;
+    IntPoint widgetPoint(point);
+    
+    while (true) {
+        n = nodeInfo.innerNode();
+        if (!n || !n->renderer() || !n->renderer()->isWidget())
+            break;
+        widget = static_cast<RenderWidget *>(n->renderer())->widget();
+        if (!widget || !widget->inherits("FrameView"))
+            break;
+        Frame *kpart = static_cast<HTMLFrameElementImpl *>(n)->contentPart();
+        if (!kpart || !static_cast<MacFrame *>(kpart)->renderer())
+            break;
+        int absX, absY;
+        n->renderer()->absolutePosition(absX, absY, true);
+        FrameView *view = static_cast<FrameView *>(widget);
+        widgetPoint.setX(widgetPoint.x() - absX + view->contentsX());
+        widgetPoint.setY(widgetPoint.y() - absY + view->contentsY());
+
+        RenderObject::NodeInfo widgetNodeInfo(true, true);
+        static_cast<MacFrame *>(kpart)->renderer()->layer()->hitTest(widgetNodeInfo, widgetPoint.x(), widgetPoint.y());
+        nodeInfo = widgetNodeInfo;
+    }
+    
+    return nodeInfo;
 }
 
 @end
