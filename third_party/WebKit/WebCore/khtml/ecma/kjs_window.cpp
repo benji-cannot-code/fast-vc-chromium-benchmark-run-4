8cb41ee09d36a5e3061d96401b5d6e987670f8a7
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-07 21:32:55 +0000

    Reviewed by darin.
            - Fixed <rdar://problem/4425269> REGRESSION: wrong size pop-up when 
            downloading attachment at webmail.mac.com (6882)
    
            The bug was that the feature string parser didn't always initialize the
            *Set variables, so WebCore assumed a height was set when it wasn't,
            and used a garbage height variable.
    
            While there, I fixed two other bugs: (1) The parser wasn't chewing up 
            as many characters as I thought it was. (Chewing up extra characters 
            is necessary to match Win IE.) (2) We considered \t and \f to be 
            whitespace, but Win IE doesn't.
    
            * manual-tests/window-open-features-parsing.html: Added these cases,
            fixed up comments.
    
            * bridge/BrowserExtension.h: Cleaned up declaration order a bit.
            * khtml/ecma/kjs_window.cpp:
            (KJS::isSeparator): New function, tells you if a character is a
            separator
            (KJS::parseWindowFeatures): Always initialize *Set to false. Drive
            parsing based on invalid characters ('separators') rather than valid 
            ones, to match Win IE.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12647 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/ecma/kjs_window.cpp
+++ third_party/WebKit/WebCore/khtml/ecma/kjs_window.cpp
@@ -1446,6 +1446,12 @@ static void setWindowFeature(const DOMString& keyString, const DOMString& valueS
         windowArgs.scrollBarsVisible = value;
 }
 
+// Though isspace() considers \t and \v to be whitespace, Win IE doesn't.
+static bool isSeparator(QChar c)
+{
+    return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '=' || c == ',' || c == '\0';
+}
+
 static void parseWindowFeatures(const DOMString& features, WindowArgs& windowArgs)
 {
     /*
@@ -1459,6 +1465,11 @@ static void parseWindowFeatures(const DOMString& features, WindowArgs& windowArg
     windowArgs.dialog = false;
     windowArgs.fullscreen = false;
     
+    windowArgs.xSet = false;
+    windowArgs.ySet = false;
+    windowArgs.widthSet = false;
+    windowArgs.heightSet = false;
+    
     if (features.length() == 0) {
         windowArgs.menuBarVisible = true;
         windowArgs.statusBarVisible = true;
@@ -1466,11 +1477,6 @@ static void parseWindowFeatures(const DOMString& features, WindowArgs& windowArg
         windowArgs.locationBarVisible = true;
         windowArgs.scrollBarsVisible = true;
         windowArgs.resizable = true;
-        
-        windowArgs.xSet = false;
-        windowArgs.ySet = false;
-        windowArgs.widthSet = false;
-        windowArgs.heightSet = false;
         
         return;
     }
@@ -1490,16 +1496,16 @@ static void parseWindowFeatures(const DOMString& features, WindowArgs& windowArg
     int length = features.length();
     DOMString buffer = features.lower();
     while (i < length) {
-        // skip to first letter or number, but don't skip past the end of the string
-        while (!buffer[i].isLetterOrNumber()) {
+        // skip to first non-separator, but don't skip past the end of the string
+        while (isSeparator(buffer[i])) {
             if (i >= length)
                 break;
             i++;
         }
         keyBegin = i;
         
-        // skip to first non-letter, non-number
-        while (buffer[i].isLetterOrNumber())
+        // skip to first separator
+        while (!isSeparator(buffer[i]))
             i++;
         keyEnd = i;
         
@@ -1510,18 +1516,20 @@ static void parseWindowFeatures(const DOMString& features, WindowArgs& windowArg
             i++;
         }
         
-        // skip to first letter or number, but don't skip past a ',' or the end of the string
-        while (!buffer[i].isLetterOrNumber()) {
+        // skip to first non-separator, but don't skip past a ',' or the end of the string
+        while (isSeparator(buffer[i])) {
             if (buffer[i] == ',' || i >= length)
                 break;
             i++;
         }
         valueBegin = i;
         
-        // skip to first non-letter, non-number
-        while (buffer[i].isLetterOrNumber())
+        // skip to first separator
+        while (!isSeparator(buffer[i]))
             i++;
         valueEnd = i;
+        
+        assert(i <= length);
 
         DOMString keyString(buffer.substring(keyBegin, keyEnd - keyBegin));
         DOMString valueString(buffer.substring(valueBegin, valueEnd - valueBegin));
