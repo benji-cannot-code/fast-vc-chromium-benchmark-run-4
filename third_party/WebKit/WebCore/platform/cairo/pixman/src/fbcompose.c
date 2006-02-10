88f955c52db9d76660ffa84a447ecc24f5939732
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-10 09:47:56 +0000

    Merge cairo fixes from Mozilla and one additional fix after discussion with vlad.  Code is now smarter about knowing when to blt vs. alpha blend.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12731 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/cairo/pixman/src/fbcompose.c
+++ third_party/WebKit/WebCore/platform/cairo/pixman/src/fbcompose.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include "pixman-xserver-compat.h"
 #include "fbpict.h"
+#include "fbmmx.h"
 
 #ifdef RENDER
 
@@ -3761,6 +3762,24 @@ fbCompositeRect (const FbComposeData *data, CARD32 *scanline_buffer)
 	CombineFuncC compose = composeFunctions.combineC[data->op];
 	if (!compose)
 	    return;
+
+	/* XXX: The non-MMX version of some of the fbCompose functions
+	 * overwrite the source or mask data (ones that use
+	 * fbCombineMaskC, fbCombineMaskAlphaC, or fbCombineMaskValueC
+	 * as helpers).  This causes problems with the optimization in
+	 * this function that only fetches the source or mask once if
+	 * possible.  If we're on a non-MMX machine, disable this
+	 * optimization as a bandaid fix.
+	 *
+	 * https://bugs.freedesktop.org/show_bug.cgi?id=5777
+	 */
+#ifdef USE_MMX
+	if (!fbHaveMMX())
+#endif
+	{
+	    srcClass = SourcePictClassUnknown;
+	    maskClass = SourcePictClassUnknown;
+	}
 
 	for (i = 0; i < data->height; ++i) {
 	    /* fill first half of scanline with source */
