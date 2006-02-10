81cdb4906e5b6f268d479b5f004d49ee78616983
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-10 16:41:21 +0000

    - fixed the build, this time for sure
            * bindings/js/JSDOMCore.cpp: Touch this file, since the project doesn't get
            the dependencies right.
            * bindings/js/JSDOMEvents.cpp: Ditto.
    
            * bridge/mac/WebCoreFrameBridge.mm: (aeDescFromJSValue): Fix an uninitialized
            variable warning and also tightened the code for Boolean, String, and Number
            up a bit.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDOMCore.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDOMCore.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 
 // These are generated files.
+// I keep modifying and rewording this line here to make the file rebuild.
 
 #include "JSAttr.cpp"
 #include "JSCharacterData.cpp"
--- third_party/WebKit/WebCore/bindings/js/JSDOMEvents.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDOMEvents.cpp
@@ -1,7 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 
-// These are generated files
+// These are generated files.
+// I keep modifying and rewording this line here to make the file rebuild.
 
 #include "JSMutationEvent.cpp"
 #include "JSWheelEvent.cpp"
