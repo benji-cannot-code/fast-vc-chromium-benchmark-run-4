e00e17dfee2894c47a619b23ac5cef8fca54af87
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-10 01:20:59 +0000

    Reviewed by Hyatt.
            - <rdar://problem/4430614> RenderThemeMac crashes under GC
    
            * rendering/render_theme_mac.mm:
            (WebCore::RenderThemeMac::RenderThemeMac): Initialize all the pointers.
            (WebCore::RenderThemeMac::setCheckboxCellState): Use KWQRetainNSRelease so
            we can retain a pointer to an ObjC object in a C++ class.
            (WebCore::RenderThemeMac::setRadioCellState): Ditto.
            (WebCore::RenderThemeMac::setButtonCellState): Ditto.
            (WebCore::RenderThemeMac::setTextFieldCellState): Ditto.
    
            * kwq/KWQResourceLoader.h: Remove unneeded forward declaration.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12726 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQResourceLoader.h
+++ third_party/WebKit/WebCore/kwq/KWQResourceLoader.h
@@ -26,10 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "WebCoreResourceLoader.h"
 
-namespace khtml {
-    class Loader;
-}
-
 namespace KIO {
     class TransferJob;
 }
