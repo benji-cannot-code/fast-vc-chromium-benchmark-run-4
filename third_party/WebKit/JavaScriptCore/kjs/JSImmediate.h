5cbcd1093cbc1e37d51ec9078a62f0fdd15ff6f0
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-13 22:49:32 +0000

    Fix Win32 JavaScriptCore bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12784 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/JSImmediate.h
+++ third_party/WebKit/JavaScriptCore/kjs/JSImmediate.h
@@ -108,8 +108,9 @@ public:
 
             return tag(doubleUnion.asBits, NumberType);
         } else {
-            // could just return 0 here, but nicer to be explicit about not supporting the platform well
+            // could just return 0 without aborting, but nicer to be explicit about not supporting the platform well
             abort();
+            return 0;
         }
     }
     
@@ -125,8 +126,10 @@ public:
             DoubleUnion doubleUnion;
             doubleUnion.asBits = unTag(v);
             return doubleUnion.asDouble;
-        } else
+        } else {
             abort();
+            return 0;
+        }
     }
 
     static bool toBoolean(const JSValue *v)
@@ -203,8 +206,10 @@ private:
             return NaN32AsBits;
         else if (JSImmediate::is64bit())
             return NaN64AsBits;
-        else
+        else {
             abort();
+            return 0;
+        }
     }
 
     static uintptr_t zeroAsBits()
@@ -221,8 +226,10 @@ private:
             return One32AsBits;
         else if (JSImmediate::is64bit())
             return One64AsBits;
-        else
+        else {
             abort();
+            return 0;
+        }
     }
 };
 
