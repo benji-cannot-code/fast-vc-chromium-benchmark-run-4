70417b62594e9bbe20c6b83da34f7ecffcad622b
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-14 16:57:47 +0000

    Reviewed by Tim Omernick.
            Support for highlighting multiple text matches.
    
            * WebView/WebHTMLViewPrivate.h:
            * WebView/WebHTMLView.m:
            (-[WebHTMLView highlightAllMatchesForString:caseSensitive:]):
            new method, calls through to bridge
            (-[WebHTMLView clearHighlightedMatches]):
            ditto
    
            * WebView/WebViewPrivate.h:
            * WebView/WebView.m:
            (-[WebView highlightAllMatchesForString:caseSensitive:]):
            new method, calls through to documentView. For now this is hardwired to only work
            with WebHTMLViews.
            (-[WebView clearHighlightedMatches]):
            ditto
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebHTMLViewPrivate.h
+++ third_party/WebKit/WebKit/WebView/WebHTMLViewPrivate.h
@@ -97,4 +97,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)_setWindowHasFocus:(BOOL)flag;
 - (void)_setDisplaysWithFocusAttributes:(BOOL)flag;
 
+// These methods might end up moving into a protocol, so different document types can specify
+// whether or not they implement the protocol.
+- (unsigned)highlightAllMatchesForString:(NSString *)string caseSensitive:(BOOL)caseFlag;
+- (void)clearHighlightedMatches;
+
 @end
