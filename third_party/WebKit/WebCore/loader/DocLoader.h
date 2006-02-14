141bfca3921b87768ac4f804246461f2b3255f75
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-14 17:55:31 +0000

    Build fix for case-sensitive drives.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/DocLoader.h
+++ third_party/WebKit/WebCore/loader/DocLoader.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef KHTML_DocLoader_h
 #define KHTML_DocLoader_h
 
-#include "String.h"
 #include <khtml_settings.h>
 #include <kio/global.h>
 #include <qbuffer.h>
