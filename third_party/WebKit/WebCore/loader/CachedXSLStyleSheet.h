f92342b8e97c028e68e223d9d3eea4122e838ae0
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-14 18:01:29 +0000

    More build fixes for case-sensitive drives. The #include "String.h" does not fail on normal HFS+ drives.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/CachedXSLStyleSheet.h
+++ third_party/WebKit/WebCore/loader/CachedXSLStyleSheet.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CachedObject.h"
 
 #include <kio/global.h>
-#include "String.h"
 
 class QString;
 class QBuffer;
