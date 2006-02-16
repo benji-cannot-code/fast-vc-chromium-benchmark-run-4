4c6f21b25305feff05f9bfe565e66ac5ed445efb
tomernic <tomernic@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-16 18:32:31 +0000

    Reviewed by Geoff.
            <rdar://problem/4428609> Flash Player 8.0.22 can crash Safari (and WebKit apps) with
            javascript disabled (7015)
    
            * bindings/NP_jsobject.cpp:
            (_NPN_CreateNoScriptObject):
            Returns an NPObject which is not bound to a JavaScript object.  This kind of NPObject
            can be given to a plugin as the "window script object" when JavaScript is disabled.
            The object has a custom NPClass, NPNoScriptObjectClass, which has no defined methods.
            Because of this, none of the NPN_* functions called by the plugin on this "no script
            object" will cause entry into JavaScript code.
            (_NPN_InvokeDefault):
            Make sure the NPVariant is filled before returning from this function.  This never
            mattered before because we never reached this case, having only created NPObjects of
            the class NPScriptObjectClass.
            (_NPN_Invoke):
            ditto
            (_NPN_Evaluate):
            ditto
            (_NPN_GetProperty):
            ditto
    
            * bindings/NP_jsobject.h:
            Declared _NPN_CreateNoScriptObject().
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/NP_jsobject.cpp
+++ third_party/WebKit/JavaScriptCore/bindings/NP_jsobject.cpp
@@ -73,7 +73,22 @@ static NPClass _javascriptClass = {
     0
 };
 
+static NPClass _noScriptClass = { 
+    1,
+    0, 
+    0, 
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+};
+
 NPClass *NPScriptObjectClass = &_javascriptClass;
+NPClass *NPNoScriptObjectClass = &_noScriptClass;
 
 static Identifier identifierFromNPIdentifier(const NPUTF8 *name)
 {
@@ -111,6 +126,11 @@ NPObject *_NPN_CreateScriptObject (NPP npp, KJS::JSObject *imp, const KJS::Bindi
     return (NPObject *)obj;
 }
 
+NPObject *_NPN_CreateNoScriptObject(void)
+{
+    return _NPN_CreateObject(0, NPNoScriptObjectClass);
+}
+
 bool _NPN_InvokeDefault (NPP npp, NPObject *o, const NPVariant *args, uint32_t argCount, NPVariant *result)
 {
     if (o->_class == NPScriptObjectClass) {
@@ -123,6 +143,7 @@ bool _NPN_InvokeDefault (NPP npp, NPObject *o, const NPVariant *args, uint32_t a
         }
     }
     
+    NPN_InitializeVariantAsUndefined(result);
     return true;
 }
 
@@ -174,6 +195,7 @@ bool _NPN_Invoke (NPP npp, NPObject *o, NPIdentifier methodName, const NPVariant
     } else if (o->_class->invoke)
         return o->_class->invoke (o, methodName, args, argCount, result);
     
+    NPN_InitializeVariantAsUndefined(result);
     return true;
 }
 
@@ -210,6 +232,8 @@ bool _NPN_Evaluate (NPP npp, NPObject *o, NPString *s, NPVariant *variant)
     
         return true;
     }
+
+    NPN_InitializeVariantAsUndefined(variant);
     return false;
 }
 
@@ -255,6 +279,8 @@ bool _NPN_GetProperty (NPP npp, NPObject *o, NPIdentifier propertyName, NPVarian
             return false;
         }
     }
+
+    NPN_InitializeVariantAsUndefined(variant);
     return false;
 }
 
