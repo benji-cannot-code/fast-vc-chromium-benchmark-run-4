0024150169975750842cfc298d3890bf1afd98a1
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-16 22:47:55 +0000

    LayoutTests:
            Updating results for fix:
            http://bugzilla.opendarwin.org/show_bug.cgi?id=6812
            Missing focus ring on new text fields
    
            * fast/forms/input-appearance-focus-expected.checksum:
            * fast/forms/input-appearance-focus-expected.png:
    
    WebCore:
    
            Reviewed by Darin.
    
            - Fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=6812
            Missing focus ring on new text fields
    
            Tests:
            * fast/forms/input-appearance-focus.html: Updated.
    
            * rendering/render_object.cpp: (WebCore::RenderObject::paintOutline): 
             call supportsFocusRing instead of checking the appearance.
            * rendering/render_theme.cpp: (khtml::RenderTheme::supportsFocusRing): Added. 
             Checks appearance.  Theme will draw focus ring if there's an appearance, but not for the new text fields.
            * rendering/render_theme.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/render_object.cpp
+++ third_party/WebKit/WebCore/rendering/render_object.cpp
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "render_inline.h"
 #include "render_line.h"
 #include "render_list.h"
+#include "render_theme.h"
 #include "VisiblePosition.h"
 #include <qmatrix.h>
 #include <qpainter.h>
@@ -1351,10 +1352,8 @@ void RenderObject::paintOutline(QPainter *p, int _tx, int _ty, int w, int h, con
     int offset = style->outlineOffset();
     
     if (style->outlineStyleIsAuto()) {
-        if (!style->hasAppearance()) {
-            // Only paint the focus ring by hand if there is no custom appearance
-            // specified.  Otherwise we let the theme paint the focus ring, since the ring
-            // might not be rectangular (or match the dimensions of the control exactly).
+        if (!theme()->supportsFocusRing(style)) {
+            // Only paint the focus ring by hand if the theme isn't able to draw the focus ring.
             p->initFocusRing(ow, offset, oc);
             addFocusRingRects(p, _tx, _ty);
             p->drawFocusRing();
