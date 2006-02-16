2be9fdff569aa65f630946598a13716791c35626
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-16 23:03:32 +0000

    Fix build failure.
    git-svn-id: svn://svn.chromium.org/blink/trunk@12858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/objc/WebScriptObject.mm
+++ third_party/WebKit/JavaScriptCore/bindings/objc/WebScriptObject.mm
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "list.h"
 #import "value.h"
 
-#import "objc_jsobject.h"
 #import "objc_instance.h"
 #import "objc_utility.h"
 
