99124eb0e8cb864d71d0f0595d58fa81a5ff1dad
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-19 01:31:31 +0000

    JavaScriptCore:
            Test: fast/js/toString-exception.html
    
            Reviewed by Maciej.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=7343
              REGRESSION: fast/js/toString-overrides.html fails when run multiple times
    
            * kjs/array_object.cpp:
            (ArrayProtoFunc::callAsFunction): Remove the object from the visited elements set before
            returning an error.
    
    LayoutTests:
    
            Reviewed by Maciej.
    
            - test for http://bugzilla.opendarwin.org/show_bug.cgi?id=7343
              REGRESSION: fast/js/toString-overrides.html fails when run multiple times
    
            * fast/js/resources/toString-exception.js: Added.
            * fast/js/toString-exception-expected.txt: Added.
            * fast/js/toString-exception.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/toString-exception.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+'This test checks for regression against <a href="http://bugzilla.opendarwin.org/show_bug.cgi?id=7343">7343: REGRESSION: fast/js/toString-overrides.html fails when run multiple times</a>.'
+);
+
+var a = [{ toString : 0 }];
+
+try {
+    a.toString();
+} catch (e) {
+}
+
+var caught = false;
+
+try {
+  a.toString();
+} catch (e) {
+caught = true;
+}
+
+shouldBeTrue("caught");
+
+var successfullyParsed = true;
