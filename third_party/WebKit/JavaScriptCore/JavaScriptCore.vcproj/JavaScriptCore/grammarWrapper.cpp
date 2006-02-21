c0f83cd1ed481598374e24d26fdc122beff6f568
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-21 22:19:38 +0000

    2006-02-21  Eric Seidel  <eseidel@apple.com>
            Reviewed by ggaren.
    
            Add grammarWrapper.cpp to work around visual studio bug plaguing buildbot.
    
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj:
            * JavaScriptCore.vcproj/JavaScriptCore/grammarWrapper.cpp: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12927 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/grammarWrapper.cpp
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// This file exists to work around a bug in Visual Studio 2005
+// Occasionally on launch VS05 will fail to correctly find and
+// identify generated files, thereby excluding them from the build
+// list and defaulting the "build rule" to "custom build rule"
+// instead of whatever may be appropriate for the file type.
+#include "grammar.cpp"
