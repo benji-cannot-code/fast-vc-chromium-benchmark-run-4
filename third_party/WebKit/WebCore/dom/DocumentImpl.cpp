d0ea9604416109481318e12c874974c9ee371947
vicki <vicki@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-21 22:55:29 +0000

    Reviewed by Adele.
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=7363
            REGRESSION (r12872): Repro crash when clicking the Quick Reply box in Gmail
    
            Add a nil check for the renderer, and refetch the renderer for the old focus node
            after dispatching the event. 
    
            * dom/DocumentImpl.cpp:
            (WebCore::DocumentImpl::setFocusNode):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/DocumentImpl.cpp
+++ third_party/WebKit/WebCore/dom/DocumentImpl.cpp
@@ -2052,9 +2052,10 @@ bool DocumentImpl::setFocusNode(PassRefPtr<NodeImpl> newFocusNode)
                 
         // Dispatch a change event for text fields or textareas that have been edited
         RenderObject *r = static_cast<RenderObject*>(oldFocusNode.get()->renderer());
-        if ((r->isTextArea() || r->isTextField()) && r->isEdited()) {
+        if (r && (r->isTextArea() || r->isTextField()) && r->isEdited()) {
             oldFocusNode->dispatchHTMLEvent(changeEvent, true, false);
-            r->setEdited(false);
+            if ((r = static_cast<RenderObject*>(oldFocusNode.get()->renderer())))
+                r->setEdited(false);
         }
 
         oldFocusNode->dispatchHTMLEvent(blurEvent, false, false);
