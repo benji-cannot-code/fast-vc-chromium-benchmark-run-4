cd670374003678f630b8be870f91117df0a612c9
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-21 19:17:18 +0000

    2006-02-21  Eric Seidel  <eseidel@apple.com>
            Reviewed by ggaren.
    
            * kjs/testkjs.cpp: #if out timeval code on win32
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
@@ -35,7 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#if HAVE_SYS_TIME_H
 #include <sys/time.h>
+#endif
 
 using namespace KJS;
 using namespace KXMLCore;
@@ -51,8 +53,11 @@ public:
     long getElapsedMS(); // call stop() first
     
 private:
+#if !WIN32
+    // Windows does not have timeval, disabling this class for now (bug 7399)
     timeval m_startTime;
     timeval m_stopTime;
+#endif
 };
 
 void StopWatch::start()
