6df28fa3134c8c75404c9b3278ed2fbc0a529c1e
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-21 09:23:03 +0000

    Platform build fix, not reviewed.
            - undef min and max, and include <algorithm> so std::min works
    
            * rendering/font.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12915 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/font.h
+++ third_party/WebKit/WebCore/rendering/font.h
@@ -36,6 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qfontmetrics.h>
 #include <qpainter.h>
 
+#undef min
+#undef max
+#include <algorithm>
+
 namespace khtml {
 
 class RenderStyle;
