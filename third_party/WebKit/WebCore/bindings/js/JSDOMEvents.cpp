d51394a03aff611a79c58acf6f138bc6704726c9
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-24 07:16:15 +0000

    - fix build by making some pointless changes to files that Xcode doesn't         rebuild otherwise
            * bindings/js/JSDOMCore.cpp:
            * bindings/js/JSDOMEvents.cpp:
            * dom/Element.idl:
            * dom/WheelEvent.idl:
            
            - add -Y 3 flag to linking; this makes the linker say what files reference 
            unresolved symbols
            
            * WebCore.xcodeproj/project.pbxproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12962 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDOMEvents.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDOMEvents.cpp
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 
 // These are generated files.
-// I keep modifying this line to make the file rebuild.
 
 #include "JSMutationEvent.cpp"
 #include "JSWheelEvent.cpp"
