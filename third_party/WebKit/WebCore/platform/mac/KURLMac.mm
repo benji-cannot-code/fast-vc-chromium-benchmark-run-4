b019a777f4178c418228d09a5b84af9cf2010e51
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-24 05:01:51 +0000

    - fix build (header name changed)
            * platform/mac/KURLMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12956 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/KURLMac.mm
+++ third_party/WebKit/WebCore/platform/mac/KURLMac.mm
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #import "KURL.h"
 
+#import "FoundationExtras.h"
 #import <kxmlcore/Assertions.h>
-#import "KWQFoundationExtras.h"
 #import <kxmlcore/Vector.h>
 
 // KURL
