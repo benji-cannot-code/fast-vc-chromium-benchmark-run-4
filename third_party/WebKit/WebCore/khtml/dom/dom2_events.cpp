1c0a518b35179dd01143c2966e0f2b7e0dd77937
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-24 19:11:41 +0000

    2006-02-24  Eric Seidel  <eseidel@apple.com>
            Reviewed by mjs.
    
            Remove delcarations causing duplicate symbols on win32.
    
            * khtml/dom/dom2_events.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12971 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/khtml/dom/dom2_events.cpp
+++ third_party/WebKit/WebCore/khtml/dom/dom2_events.cpp
@@ -29,12 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace DOM {
 
-const unsigned KeyboardEvent::DOM_KEY_LOCATION_STANDARD;
-const unsigned KeyboardEvent::DOM_KEY_LOCATION_LEFT;
-const unsigned KeyboardEvent::DOM_KEY_LOCATION_RIGHT;
-const unsigned KeyboardEvent::DOM_KEY_LOCATION_NUMPAD;
-const unsigned KeyboardEvent::DOM_KEY_LOCATION_UNKNOWN;
-
 EventListener::EventListener()
 {
 }
