6c08d1accbc1e0cafe88ad318daaed4adf7cf815
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-27 03:08:02 +0000

    - try again after seeing errors from buildbot
            * platform/win/TemporaryLinkStubs.cpp: Added more includes.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12992 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/win/TemporaryLinkStubs.cpp
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KWQKJavaAppletWidget.h"
 #include "KWQScrollBar.h"
 #include "Path.h"
+#include "MouseEvent.h"
+#include "KWQKCookieJar.h"
 
 using namespace WebCore;
 
