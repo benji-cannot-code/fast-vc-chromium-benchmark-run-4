3730217c7f74047cd684e13748439e259fd63d4b
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-27 03:31:44 +0000

    Reviewed by Maciej.
            - more Win32 link fixes
    
            * loader/CachedObject.h: Put some Mac-specific stuff inside
            __APPLE__ ifdefs.
            * platform/win/TemporaryLinkStubs.cpp: Added more stubs.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@12994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/CachedObject.h
+++ third_party/WebKit/WebCore/loader/CachedObject.h
@@ -147,10 +147,12 @@ namespace WebCore
 
         void setRequest(Request*);
 
+#if __APPLE__
         NSURLResponse* response() const { return m_response; }
         void setResponse(NSURLResponse*);
         NSData* allData() const { return m_allData; }
         void setAllData(NSData*);
+#endif
 
         bool canDelete() const { return m_clients.isEmpty() && !m_request; }
 
@@ -173,8 +175,10 @@ namespace WebCore
         DOMString m_url;
         QString m_accept;
         Request *m_request;
+#if __APPLE__
         NSURLResponse *m_response;
         NSData *m_allData;
+#endif
         Type m_type;
         Status m_status;
     private:
