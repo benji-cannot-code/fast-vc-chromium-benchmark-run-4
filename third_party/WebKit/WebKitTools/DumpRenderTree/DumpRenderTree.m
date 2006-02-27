671bd2c1344b6b4da04022dab1688157d84d0c52
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-27 23:08:13 +0000

    2006-02-27  Eric Seidel  <eseidel@apple.com>
            Reviewed by ggaren.
    
            Test new KConfig -> PlugInInfoStore change.
            http://bugzilla.opendarwin.org/show_bug.cgi?id=7498
    
            * DumpRenderTree/DumpRenderTree.m:
            (main): load test netscape plugin
            * Scripts/run-webkit-tests: style update
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13021 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebPreferences.h>
 #import <WebKit/WebView.h>
 #import <WebKit/WebHTMLViewPrivate.h>
+#import <WebKit/WebPluginDatabase.h>
 
 #import <Carbon/Carbon.h>                           // for GetCurrentEventTime()
 #import <ApplicationServices/ApplicationServices.h> // for CMSetDefaultProfileBySpace
@@ -230,6 +231,10 @@ int main(int argc, const char *argv[])
     [webView setEditingDelegate:editingDelegate];
     [webView setUIDelegate:delegate];
     frame = [webView mainFrame];
+    
+    NSString *pwd = [[NSString stringWithCString:argv[0]] stringByDeletingLastPathComponent];
+    [WebPluginDatabase setAdditionalWebPlugInPaths:[NSArray arrayWithObject:pwd]];
+    [[WebPluginDatabase installedPlugins] refresh];
 
     // The back/forward cache is causing problems due to layouts during transition from one page to another.
     // So, turn it off for now, but we might want to turn it back on some day.
