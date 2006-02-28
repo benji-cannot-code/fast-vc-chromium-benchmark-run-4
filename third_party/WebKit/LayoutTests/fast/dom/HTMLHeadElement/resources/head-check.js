a64ac33957c703080e5305f702a0a824e6faba43
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-28 05:44:49 +0000

    LayoutTests:
            Reviewed by Darin.
    
            - Tests for http://bugzilla.opendarwin.org/show_bug.cgi?id=7321
            REGRESSION: style tag in body causes two head elements to appear in the DOM
    
            The head-check.html document uses an iframe as a laboratory for loading each
            head-check-N.html file and checking its DOM, with inspiration from Ian Hixie's
            Live DOM Viewer: http://software.hixie.ch/utilities/js/live-dom-viewer/
    
            Each of the four head-check-N.html files tests a different code path for
            creating a document's head element in WebCore/khtml/html/htmlparser.cpp.
    
            * editing/inserting/editing-empty-divs-expected.txt: Corrected <html> child
            index for <body> since the document previously had two <head> nodes.
            * fast/dom/HTMLHeadElement: Added.
            * fast/dom/HTMLHeadElement/head-check-expected.txt: Added.
            * fast/dom/HTMLHeadElement/head-check.html: Added.
            * fast/dom/HTMLHeadElement/resources: Added.
            * fast/dom/HTMLHeadElement/resources/head-check-1.html: Added.
            * fast/dom/HTMLHeadElement/resources/head-check-2.html: Added.
            * fast/dom/HTMLHeadElement/resources/head-check-3.html: Added.
            * fast/dom/HTMLHeadElement/resources/head-check-4.html: Added.
            * fast/dom/HTMLHeadElement/resources/head-check.js: Added.
    
    WebCore:
    
            Reviewed by Darin.
    
            - Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=7321
            REGRESSION: style tag in body causes two head elements to appear in the DOM
    
            * khtml/html/htmlparser.cpp: Handle the head node like form and map nodes.
            (WebCore::HTMLParser::parseToken):
            (WebCore::HTMLParser::headCreateErrorCheck):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13044 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/HTMLHeadElement/resources/head-check.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var maxTests = 4;
+var currentTest = 1;
+var iframe;
+
+if (window.layoutTestController) {
+    window.layoutTestController.waitUntilDone();
+}
+
+function testAndLoadNext() {
+    iframe = document.getElementById("iframe");
+    if (iframe.src.substring(0, 5) == "data:") {
+        iframe.src = "resources/head-check-" + currentTest + ".html";
+        return;
+    }
+    debug("Testing: " + iframe.src.substring(iframe.src.lastIndexOf("/") + 1));
+    shouldBe("iframe.contentWindow.document.getElementsByTagName('head').length", "1");
+    shouldBe("iframe.contentWindow.document.firstChild.firstChild.nodeName.toLowerCase()", "'head'");
+    debug("");
+    if (currentTest < maxTests) {
+        currentTest++;
+        iframe.src = "data:text/html,";
+    }
+    else {
+        iframe.style.display = "none";
+        debug('<span class="pass">TEST COMPLETE</span>');
+        if (window.layoutTestController) {
+            window.layoutTestController.notifyDone();
+        }
+    }
+}
