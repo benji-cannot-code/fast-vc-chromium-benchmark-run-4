10db4eb3d1d14ba523216cc2f8626ad3fc8f105f
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-02-28 21:00:55 +0000

    LayoutTests:
            Reviewed by mjs
            
            Tests for:
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=6647>
            TinyMCE: document.execCommand("insertHTML", ...) unimplemented
    
            * editing/editing.js:
            * editing/execCommand/insertHTML-expected.checksum: Added.
            * editing/execCommand/insertHTML-expected.png: Added.
            * editing/execCommand/insertHTML-expected.txt: Added.
            * editing/execCommand/insertHTML.html: Added.
    
    WebCore:
    
            Reviewed by mjs
            
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=6647>
            TinyMCE: document.execCommand("insertHTML", ...) unimplemented
    
            * editing/jsediting.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13058 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/jsediting.cpp
+++ third_party/WebKit/WebCore/editing/jsediting.cpp
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "css_valueimpl.h"
 #include "cssproperties.h"
 #include "htmlediting.h"
+#include "markup.h"
+#include "ReplaceSelectionCommand.h"
 #include "TypingCommand.h"
 #include <kxmlcore/HashMap.h>
 
@@ -231,6 +233,16 @@ bool execFontSizeDelta(Frame *frame, bool userInterface, const DOMString &value)
 bool execForeColor(Frame *frame, bool userInterface, const DOMString &value)
 {
     return execStyleChange(frame,  CSS_PROP_COLOR, value);
+}
+
+bool execInsertHTML(Frame* frame, bool userInterface, const String& value)
+{
+    QString baseURL = "";
+    bool selectReplacement = false;
+    RefPtr<DocumentFragmentImpl> fragment = createFragmentFromMarkup(frame->document(), value.qstring(), baseURL);
+    EditCommandPtr cmd(new ReplaceSelectionCommand(frame->document(), fragment.get(), selectReplacement));
+    cmd.apply();
+    return true;
 }
 
 bool execIndent(Frame *frame, bool userInterface, const DOMString &value)
@@ -517,6 +529,7 @@ CommandMap *createCommandDictionary()
         { "ForeColor", { execForeColor, enabledAnySelection, stateNone, valueForeColor } },
         { "ForwardDelete", { execForwardDelete, enabledAnySelection, stateNone, valueNull } },
         { "Indent", { execIndent, enabledAnySelection, stateNone, valueNull } },
+        { "InsertHTML", { execInsertHTML, enabledAnySelection, stateNone, valueNull } },
         { "InsertLineBreak", { execInsertLineBreak, enabledAnySelection, stateNone, valueNull } },
         { "InsertParagraph", { execInsertParagraph, enabledAnySelection, stateNone, valueNull } },
         { "InsertNewlineInQuotedContent", { execInsertNewlineInQuotedContent, enabledAnySelection, stateNone, valueNull } },
