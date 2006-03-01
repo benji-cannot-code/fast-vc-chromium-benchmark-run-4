a958c08572526ae215f3f343eb1aa313c6452882
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-01 09:12:05 +0000

    Rubber stamped by Hyatt.
            - fix win32 build (and make dumprendertree run)
    
            * kwq/KWQComboBox.h:
            * platform/win/TemporaryLinkStubs.cpp:
            (QTextEdit::selectionEnd):
            (Widget::~Widget):
            (GraphicsContext::beginTransparencyLayer):
            (QComboBox::populate):
            (GraphicsContext::drawConvexPolygon):
            (QFontMetrics::selectionRectForText):
            (GraphicsContext::setShadow):
            (GraphicsContext::clearFocusRing):
            (GraphicsContext::drawImageInRect):
            (KWQFileButton::setFilename):
            (KWQFileButton::focusPolicy):
            (KWQFileButton::frameGeometry):
            (QSlider::focusPolicy):
            (WebCore::GraphicsContext::font):
            (QTextEdit::setWritingDirection):
            (QFontMetrics::checkSelectionPoint):
            (QFontMetrics::isFixedPitch):
            (WebCore::TransferJob::addMetaData):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQComboBox.h
+++ third_party/WebKit/WebCore/kwq/KWQComboBox.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 class KWQComboBoxAdapter;
 class NSMenuItem;
+class NSFont;
 #endif
 
 class QComboBox : public Widget {
