ccbfb0ae647b8946de9cdcdb6f9ed8c3d93d7122
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-02 05:12:24 +0000

    LayoutTests:
            Reviewed by darin
            
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=7059>
            TinyMCE: Select All + delete sends you Back intsead of deleting
    
            * editing/resources/select-all-iframe-src.html: Added.
            * editing/selection/select-all-iframe-expected.checksum: Added.
            * editing/selection/select-all-iframe-expected.png: Added.
            * editing/selection/select-all-iframe-expected.txt: Added.
            * editing/selection/select-all-iframe.html: Added.
    
    WebCore:
    
            Reviewed by darin
            
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=7059>
            TinyMCE: Select All + delete sends you Back intsead of deleting
    
            * page/Frame.cpp:
            (WebCore::Frame::selectFrameElementInParentIfFullySelected):
    
    WebKitTools:
    
            Reviewed by darin
            
            Give the events that eventSender sends a unique eventNumber
    
            * DumpRenderTree/DumpRenderTree.m:
            (-[EventSendingController mouseDown]):
            (-[EventSendingController mouseUp]):
            (-[EventSendingController mouseMoveToX:Y:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13081 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     BOOL down;
     int clickCount;
     NSTimeInterval lastClick;
+    int eventNumber;
 }
 @end
 
@@ -687,7 +688,7 @@ static void dump(void)
         clickCount = 1;
     else
         clickCount++;
-    NSEvent *event = [NSEvent mouseEventWithType:NSLeftMouseDown location:lastMousePosition modifierFlags:nil timestamp:GetCurrentEventTime() windowNumber:0 context:[NSGraphicsContext currentContext] eventNumber:nil clickCount:clickCount pressure:nil];
+    NSEvent *event = [NSEvent mouseEventWithType:NSLeftMouseDown location:lastMousePosition modifierFlags:nil timestamp:GetCurrentEventTime() windowNumber:0 context:[NSGraphicsContext currentContext] eventNumber:++eventNumber clickCount:clickCount pressure:nil];
 
     NSView *subView = [[frame webView] hitTest:[event locationInWindow]];
     if (subView) {
@@ -699,7 +700,7 @@ static void dump(void)
 - (void)mouseUp
 {
     [[[frame frameView] documentView] layout];
-    NSEvent *event = [NSEvent mouseEventWithType:NSLeftMouseUp location:lastMousePosition modifierFlags:nil timestamp:GetCurrentEventTime() windowNumber:0 context:[NSGraphicsContext currentContext] eventNumber:nil clickCount:clickCount pressure:nil];
+    NSEvent *event = [NSEvent mouseEventWithType:NSLeftMouseUp location:lastMousePosition modifierFlags:nil timestamp:GetCurrentEventTime() windowNumber:0 context:[NSGraphicsContext currentContext] eventNumber:++eventNumber clickCount:clickCount pressure:nil];
 
     NSView *subView = [[frame webView] hitTest:[event locationInWindow]];
     if (subView) {
@@ -723,7 +724,7 @@ static void dump(void)
 - (void)mouseMoveToX:(int)x Y:(int)y
 {
     lastMousePosition = NSMakePoint(x, [[frame webView] frame].size.height - y);
-    NSEvent *event = [NSEvent mouseEventWithType:(down ? NSLeftMouseDragged : NSMouseMoved) location:lastMousePosition modifierFlags:nil timestamp:GetCurrentEventTime() windowNumber:0 context:[NSGraphicsContext currentContext] eventNumber:nil clickCount:(down ? clickCount : 0) pressure:nil];
+    NSEvent *event = [NSEvent mouseEventWithType:(down ? NSLeftMouseDragged : NSMouseMoved) location:lastMousePosition modifierFlags:nil timestamp:GetCurrentEventTime() windowNumber:0 context:[NSGraphicsContext currentContext] eventNumber:++eventNumber clickCount:(down ? clickCount : 0) pressure:nil];
 
     NSView *subView = [[frame webView] hitTest:[event locationInWindow]];
     if (subView) {
