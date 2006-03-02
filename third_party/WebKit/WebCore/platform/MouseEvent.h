48d453495aa8a906eca16434cdb85e45bc1ad943
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-02 21:41:25 +0000

    2006-03-02  Eric Seidel  <eseidel@apple.com>
            Reviewed by ggaren.
    
            Add (partially stubbed out) mouse event handling for Win32.
    
            * WebCore.vcproj/WebCore/WebCore.vcproj:
            * platform/IntPoint.h:
            * platform/MouseEvent.h:
            * platform/win/IntPointWin.cpp: Added.
            (WebCore::IntPoint::IntPoint):
            (WebCore::IntPoint::operator POINT):
            * platform/win/MouseEventWin.cpp: Added.
            (WebCore::MouseEvent::MouseEvent):
            * platform/win/TemporaryLinkStubs.cpp:
            (QLineEdit::setMaxLength):
            (ScrollView::scrollBy):
            (WebCore::Widget::setFocus):
            (FrameWin::respondToChangedSelection):
            (FrameWin::runJavaScriptPrompt):
            (FrameWin::recordFormValue):
            (FrameWin::registerCommandForRedo):
            (FrameWin::runJavaScriptAlert):
            (FrameWin::runJavaScriptConfirm):
            (FrameWin::openURL):
            (FrameWin::registerCommandForUndo):
            (FrameWin::addMessageToConsole):
            (FrameWin::mimeTypeForFileName):
            (FrameWin::shouldChangeSelection):
            (FrameWin::markMisspellingsInAdjacentWords):
            (FrameWin::markMisspellings):
            (FrameWin::personalbarVisible):
            (FrameWin::objectContentType):
            (FrameWin::createFrame):
            (FrameWin::createPlugin):
            (BrowserExtensionWin::setTypedIconURL):
            (BrowserExtensionWin::getHistoryLength):
            (BrowserExtensionWin::canRunModal):
            (BrowserExtensionWin::openURLNotify):
            (BrowserExtensionWin::createNewWindow):
            (BrowserExtensionWin::canRunModalNow):
            (BrowserExtensionWin::runModal):
            (BrowserExtensionWin::goBackOrForward):
            (BrowserExtensionWin::setIconURL):
            (FrameWin::passSubframeEventToSubframe):
            (FrameWin::lastEventIsMouseUp):
            (BrowserExtensionWin::BrowserExtensionWin):
            (Widget::setCursor):
            (ScrollView::visibleHeight):
            (ScrollView::visibleWidth):
            (ScrollView::setContentsPos):
            (ScrollView::contentsX):
            (ScrollView::contentsY):
            (ScrollView::contentsHeight):
            (ScrollView::contentsWidth):
            (ScrollView::viewportToContents):
            (TransferJob::TransferJob):
            (TransferJob::addMetaData):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/MouseEvent.h
+++ third_party/WebKit/WebCore/platform/MouseEvent.h
@@ -37,6 +37,12 @@ class NSEvent;
 #endif
 #endif
 
+#if WIN32
+typedef struct HWND__ *HWND;
+typedef unsigned    WPARAM;
+typedef long        LPARAM;
+#endif
+
 namespace WebCore {
 
     // These button numbers match the one used in the DOM API.
@@ -45,6 +51,9 @@ namespace WebCore {
     class MouseEvent {
     public:
         MouseEvent(); // "current event"
+#if WIN32
+        MouseEvent(HWND hWnd, WPARAM wParam, LPARAM lParam, int clickCount);
+#endif
 
         const IntPoint& pos() const { return m_position; }
         int x() const { return m_position.x(); }
