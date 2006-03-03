7aabf1363d6afd8a3b17c9f00ce09541dcb63452
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-03 09:18:22 +0000

    Reviewed by Eric.
            - enough link stubs to "display" local copy of WebKit site w/o hitting breakpoint
    
            * platform/win/TemporaryLinkStubs.cpp:
            (QLineEdit::setReadOnly):
            (QTextEdit::setCursorPosition):
            (QLineEdit::setCursorPosition):
            (QFontMetrics::width):
            (GraphicsContext::drawText):
            (GraphicsContext::drawLineForText):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13111 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/win/TemporaryLinkStubs.cpp
@@ -93,7 +93,6 @@ int QTextEdit::selectionStart() { notImplemented(); return 0; }
 int ScrollView::scrollXOffset() const { notImplemented(); return 0; }
 bool QListBox::isSelected(int) const { notImplemented(); return 0; }
 void QLineEdit::setReadOnly(bool) { notImplemented(); }
-void GraphicsContext::drawLineForText(int,int,int,int) { notImplemented(); }
 QComboBox::~QComboBox() { notImplemented(); }
 Cursor::Cursor(Image*) { notImplemented(); }
 Widget::FocusPolicy QComboBox::focusPolicy() const { notImplemented(); return NoFocus; }
@@ -108,13 +107,11 @@ IntRect Widget::frameGeometry() const { notImplemented(); return IntRect(); }
 void QListBox::setSelected(int,bool) { notImplemented(); }
 void GraphicsContext::addFocusRingRect(int,int,int,int) { notImplemented(); }
 void QTextEdit::setCursorPosition(int,int) { notImplemented(); }
-int QFontMetrics::width(QString const&,int,int,int) const { notImplemented(); return 0; }
 void Widget::setEnabled(bool) { notImplemented(); }
 void QTextEdit::setSelectionEnd(int) { notImplemented(); }
 void QComboBox::populate() { notImplemented(); }
 void QTextEdit::setAlignment(Qt::AlignmentFlags) { notImplemented(); }
 void QLineEdit::setCursorPosition(int) { notImplemented(); }
-void GraphicsContext::drawText(int,int,int,int,int,int,int,QString const&) { notImplemented(); }
 KJavaAppletWidget::KJavaAppletWidget(IntSize const&,Frame*,KXMLCore::HashMap<String,String> const&) { notImplemented(); }
 QListBox::QListBox() { notImplemented(); }
 QString QLineEdit::selectedText() const { notImplemented(); return QString(); }
@@ -331,6 +328,7 @@ float QFontMetrics::floatWidth(QChar const*,int,int,int,int,int,int,int,bool) co
 int QFontMetrics::lineSpacing() const { return 10; }
 int QFontMetrics::ascent() const { return 10; }
 int QFontMetrics::height() const { return 10; }
+int QFontMetrics::width(QString const&,int,int,int) const { return 10; }
 int QFontMetrics::descent() const { return 10; }
 void QFontMetrics::setFontDescription(class WebCore::FontDescription const&) { }
 bool QFontMetrics::isFixedPitch(void)const { return 0; }
@@ -375,9 +373,12 @@ void ScrollView::setStaticBackground(bool) { }
 
 TransferJob::TransferJob(WebCore::TransferJobClient*,const String&,KURL const&) { }
 void TransferJob::addMetaData(QString const&,QString const&) { }
+bool TransferJob::start(class WebCore::DocLoader *){ return false; }
 
 Font const& GraphicsContext::font() const { return localFont; }
 void GraphicsContext::setFont(Font const&) { }
+void GraphicsContext::drawText(int,int,int,int,int,int,int,QString const&) { }
 void GraphicsContext::drawText(int,int,int,int,QChar const*,int,int,int,int,Color const&,TextDirection,bool,int,int,bool) { }
+void GraphicsContext::drawLineForText(int,int,int,int) { }
 QFontMetrics GraphicsContext::fontMetrics() const { return QFontMetrics(); }
 
