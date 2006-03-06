8b59891e9656a7d5e06f4e88c1f2e087e7c5b1e2
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-06 23:36:31 +0000

    2006-03-06  Eric Seidel  <eseidel@apple.com>
            Reviewed by darin.
    
            Fix win32 build.
    
            * platform/win/TemporaryLinkStubs.cpp:
            (QScrollBar::QScrollBar):
            (QTextEdit::setAlignment):
            (Widget::client):
            (Widget::setClient):
            (QLineEdit::setAlignment):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13180 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/win/TemporaryLinkStubs.cpp
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "loader.h"
 #include "FrameView.h"
 #include "KWQScrollBar.h"
-#include "KWQObject.h"
-#include "KWQSignal.h"
 #include "KWQKJavaAppletWidget.h"
 #include "KWQScrollBar.h"
 #include "Path.h"
@@ -49,7 +47,7 @@ Widget* FrameView::topLevelWidget() const { notImplemented(); return 0; }
 QScrollBar::~QScrollBar() { notImplemented(); }
 void QScrollBar::setSteps(int,int) { notImplemented(); }
 bool QScrollBar::scroll(KWQScrollDirection,KWQScrollGranularity,float) { notImplemented(); return 0; }
-QScrollBar::QScrollBar(Qt::Orientation,Widget*) { notImplemented(); }
+QScrollBar::QScrollBar(ScrollBarOrientation) { notImplemented(); }
 bool QScrollBar::setValue(int) { notImplemented(); return 0; }
 void QScrollBar::setKnobProportion(int,int) { notImplemented(); }
 
@@ -84,7 +82,7 @@ void QTextEdit::setSelectionStart(int) { notImplemented(); }
 void QTextEdit::setCursorPosition(int,int) { notImplemented(); }
 String QTextEdit::text() const { notImplemented(); return String(); }
 void QTextEdit::setWordWrap(QTextEdit::WrapStyle) { notImplemented(); }
-void QTextEdit::setAlignment(Qt::AlignmentFlags) { notImplemented(); }
+void QTextEdit::setAlignment(HorizontalAlignment) { notImplemented(); }
 void QTextEdit::setSelectionEnd(int) { notImplemented(); }
 void QTextEdit::getCursorPosition(int*,int*) const { notImplemented(); }
 void QTextEdit::setSelectionRange(int,int) { notImplemented(); }
@@ -99,6 +97,8 @@ void Widget::setIsSelected(bool) { notImplemented(); }
 void Widget::lockDrawingFocus() { notImplemented(); }
 void Widget::unlockDrawingFocus() { notImplemented(); }
 void Widget::setDrawingAlpha(float) { notImplemented(); }
+WidgetClient* Widget::client() const { notImplemented(); return 0; }
+void Widget::setClient(WidgetClient*) { notImplemented(); }
 
 KJavaAppletWidget::KJavaAppletWidget(IntSize const&,Frame*,KXMLCore::HashMap<String,String> const&) { notImplemented(); }
 
@@ -344,7 +344,7 @@ bool RenderThemeWin::paintTextField(RenderObject*, const RenderObject::PaintInfo
 QLineEdit::QLineEdit(QLineEdit::Type) { }
 QLineEdit::~QLineEdit() { }
 void QLineEdit::setFont(WebCore::Font const&) { }
-void QLineEdit::setAlignment(Qt::AlignmentFlags) { }
+void QLineEdit::setAlignment(HorizontalAlignment) { }
 void QLineEdit::setWritingDirection(TextDirection) { }
 int QLineEdit::maxLength() const { return 0; }
 void QLineEdit::setMaxLength(int) { }
