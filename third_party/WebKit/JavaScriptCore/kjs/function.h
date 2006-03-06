85ddfb1fafc554bb5d2a3679d6f546f3cb507b93
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-06 05:29:48 +0000

    Reviewed by Maciej.
            - http://bugzilla.opendarwin.org/show_bug.cgi?id=7616
              get all references to KJS::Node out of internal.h
    
            * JavaScriptCore.xcodeproj/project.pbxproj: Updated for file changes.
    
            * kjs/Parser.cpp: Added.
            * kjs/Parser.h: Added.
    
            * kjs/internal.cpp: Removed the Parser class.
            * kjs/internal.h: Ditto. Also removed unnecessary declarations of classes
            not used in this header.
    
            * kjs/nodes.h: Added an include of "Parser.h".
            * kjs/function.h: Added a declaration of FunctionBodyNode.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13153 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/function.h
+++ third_party/WebKit/JavaScriptCore/kjs/function.h
@@ -30,8 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace KJS {
 
-  class Parameter;
   class ActivationImp;
+  class FunctionBodyNode;
+  class Parameter;
 
   /**
    * @short Implementation class for internal Functions.
--- third_party/WebKit/JavaScriptCore/kjs/internal.h
+++ third_party/WebKit/JavaScriptCore/kjs/internal.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *  This file is part of the KDE libraries
  *  Copyright (C) 1999-2001 Harri Porten (porten@kde.org)
  *  Copyright (C) 2001 Peter Kelly (pmk@post.com)
- *  Copyright (C) 2003 Apple Computer, Inc.
+ *  Copyright (C) 2003, 2004, 2005, 2006 Apple Computer, Inc.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -41,12 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace KJS {
 
-  class Node;
-  class ProgramNode;
-  class FunctionBodyNode;
-  class FunctionPrototype;
-  class FunctionImp;
   class Debugger;
+  class FunctionPrototype;
 
   // ---------------------------------------------------------------------------
   //                            Primitive impls
@@ -136,34 +132,13 @@ namespace KJS {
 
 
   // ---------------------------------------------------------------------------
-  //                            Parsing & evaluation
+  //                            Evaluation
   // ---------------------------------------------------------------------------
 
   enum CodeType { GlobalCode,
                   EvalCode,
                   FunctionCode,
                   AnonymousCode };
-
-  /**
-   * @internal
-   *
-   * Parses ECMAScript source code and converts into ProgramNode objects, which
-   * represent the root of a parse tree. This class provides a conveniant workaround
-   * for the problem of the bison parser working in a static context.
-   */
-  class Parser {
-  public:
-    static RefPtr<ProgramNode> parse(const UString &sourceURL, int startingLineNumber,
-                                        const UChar *code, unsigned int length, int *sourceId = 0,
-                                        int *errLine = 0, UString *errMsg = 0);
-    static void accept(ProgramNode *prog);
-
-    static void saveNewNode(Node *node);
-    static void noteNodeCycle(Node *node);
-    static void removeNodeCycle(Node *node);
-
-    static int sid;
-  };
 
   class SavedBuiltinsInternal {
     friend class InterpreterImp;
