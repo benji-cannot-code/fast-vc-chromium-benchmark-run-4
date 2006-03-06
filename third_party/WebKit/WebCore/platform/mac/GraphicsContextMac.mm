0bd24ca599f2a8a5bc306b6430d76964a0ffce3a
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-06 04:56:45 +0000

    2006-03-05  Eric Seidel  <eseidel@apple.com>
            Add "Font.h" to (try to) fix the no-svg build.
    
            * platform/mac/GraphicsContextMac.mm:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13152 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/GraphicsContextMac.mm
+++ third_party/WebKit/WebCore/platform/mac/GraphicsContextMac.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "config.h"
 #import "GraphicsContext.h"
 
+#import "Font.h"
 #import "FloatRect.h"
 #import "FoundationExtras.h"
 #import "IntPointArray.h"
