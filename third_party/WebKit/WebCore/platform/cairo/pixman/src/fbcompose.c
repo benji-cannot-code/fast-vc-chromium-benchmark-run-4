949ecd036268d484ce48f2228aeb8fa343c489ae
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-08 23:53:07 +0000

    merge cairo to the mozilla trunk.
    git-svn-id: svn://svn.chromium.org/blink/trunk@13222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/cairo/pixman/src/fbcompose.c
+++ third_party/WebKit/WebCore/platform/cairo/pixman/src/fbcompose.c
@@ -45,8 +45,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define M_PI 3.14159265358979323846
 #endif
 
-// #define PIXMAN_CONVOLUTION
-// #define PIXMAN_INDEXED_FORMATS
+
+/* #define PIXMAN_CONVOLUTION */
+/* #define PIXMAN_INDEXED_FORMATS */
 
 static Bool
 PictureTransformPoint3d (pixman_transform_t *transform,
