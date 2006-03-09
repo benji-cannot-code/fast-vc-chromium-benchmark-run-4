eca008d75440e6d96dcddc0d6e8875f9039eb1b4
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-09 19:43:30 +0000

    LayoutTests:
            - Tests for http://bugzilla.opendarwin.org/show_bug.cgi?id=6814
            Implement selection methods for RenderTextField
    
            * fast/forms/input-appearance-selection-expected.checksum: Added.
            * fast/forms/input-appearance-selection-expected.png: Added.
            * fast/forms/input-appearance-selection-expected.txt: Added.
            * fast/forms/input-appearance-selection.html: Added.
    
    WebCore:
    
            Reviewed by Darin.
    
            - Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=6814
            Implement selection methods for RenderTextField
    
            Test: fast/forms/input-appearance-selection.html
    
            * rendering/RenderTextField.cpp:
            (WebCore::RenderTextField::createDivStyle): removed extra space.
            (WebCore::RenderTextField::select): Removed checks for document and m_div since there should always be a document and 
             m_div will be initialized as the renderer gets attached.
            (WebCore::RenderTextField::selectionStart): Calls indexForVisiblePosition.
            (WebCore::RenderTextField::selectionEnd): ditto.
            (WebCore::RenderTextField::setSelectionStart): calls setSelectionRange.
            (WebCore::RenderTextField::setSelectionEnd): ditto.
            (WebCore::RenderTextField::setSelectionRange): gets VisiblePositions for both indices, and sets the selection.
            (WebCore::RenderTextField::visiblePositionForIndex): Added helper method.
            (WebCore::RenderTextField::indexForVisiblePosition): Added helper method.
            * rendering/RenderTextField.h: Added new helper methods for selection.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13234 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTextField.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTextField.cpp
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "RenderTextField.h"
 
+#include <algorithm>
 #include "DocumentImpl.h"
 #include "Frame.h"
 #include "RenderText.h"
@@ -29,10 +30,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLInputElementImpl.h"
 #include "HTMLTextFieldInnerElementImpl.h"
 #include "dom_elementimpl.h"
+#include "SelectionController.h"
+#include "VisiblePosition.h"
+#include "visible_text.h"
 
 namespace WebCore {
 
 using namespace HTMLNames;
+using namespace std;
 
 RenderTextField::RenderTextField(NodeImpl* node)
 :RenderBlock(node), m_dirty(false)
@@ -65,7 +70,6 @@ RenderStyle* RenderTextField::createDivStyle(RenderStyle* startStyle)
     divStyle->setOverflow(OHIDDEN);
     divStyle->setWhiteSpace(NOWRAP);
     divStyle->setUserModify(READ_WRITE);
-    
     return divStyle;
 }
 
@@ -128,36 +132,69 @@ void RenderTextField::updateFromElement()
 
 int RenderTextField::selectionStart()
 {
-    // FIXME: Implement this.
-    return 0;
+    return indexForVisiblePosition(document()->frame()->selection().start());        
 }
 
 int RenderTextField::selectionEnd()
 {
-    // FIXME: Implement this.
-    return 0;
+    return indexForVisiblePosition(document()->frame()->selection().end());        
 }
 
 void RenderTextField::setSelectionStart(int start)
 {
-    // FIXME: Implement this.
+    setSelectionRange(start, selectionEnd());
 }
  
 void RenderTextField::setSelectionEnd(int end)
 {
-    // FIXME: Implement this.
+    setSelectionRange(selectionStart(), end);
 }
     
 void RenderTextField::select()
 {
-    DocumentImpl* doc = document();
-    if (doc && m_div)
-        doc->frame()->selectContentsOfNode(m_div.get());
+    if (m_div)
+        document()->frame()->selectContentsOfNode(m_div.get());
 }
 
 void RenderTextField::setSelectionRange(int start, int end)
 {
-    // FIXME: Implement this.
+    end = max(end, 0);
+    start = min(max(start, 0), end);
+    
+    VisiblePosition startPosition = visiblePositionForIndex(start);
+    VisiblePosition endPosition = visiblePositionForIndex(end);
+    
+    ASSERT(startPosition.isNotNull() && endPosition.isNotNull());
+    ASSERT(startPosition.deepEquivalent().node()->rootEditableElement() == m_div && endPosition.deepEquivalent().node()->rootEditableElement() == m_div);
+    
+    SelectionController sel = SelectionController(startPosition, endPosition);    
+    if (document()->frame()->shouldChangeSelection(sel))
+        document()->frame()->setSelection(sel);
+}
+
+VisiblePosition RenderTextField::visiblePositionForIndex(int index)
+{    
+    if (index <= 0)
+        return VisiblePosition(m_div.get(), 0, DOWNSTREAM);
+    ExceptionCode ec = 0;
+    RefPtr<RangeImpl> range = new RangeImpl(document());
+    range->selectNodeContents(m_div.get(), ec);
+    CharacterIterator it(range.get());
+    if (!it.atEnd())
+        it.advance(index - 1);
+    return VisiblePosition(it.range()->endContainer(ec), it.range()->endOffset(ec), UPSTREAM);
+}
+
+int RenderTextField::indexForVisiblePosition(const VisiblePosition& pos)
+{
+    Position indexPosition = pos.deepEquivalent();
+    if (!indexPosition.node() || indexPosition.node()->rootEditableElement() != m_div)
+        return 0;
+    ExceptionCode ec = 0;
+    RefPtr<RangeImpl> range = new RangeImpl(document());
+    range->setStart(m_div.get(), 0, ec);
+    range->setEnd(indexPosition.node(), indexPosition.offset(), ec);
+    return TextIterator::rangeLength(range.get());
 }
 
 void RenderTextField::subtreeHasChanged()
