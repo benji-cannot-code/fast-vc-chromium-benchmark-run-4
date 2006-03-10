e964abc11f3655820e4cc4ea82faf41d1f52d4b0
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-10 21:15:21 +0000

    2006-03-10  Eric Seidel  <eseidel@apple.com>
            Fix build.
    
            * platform/win/FontWin.cpp:
            (WebCore::getFontData):
            * platform/win/TemporaryLinkStubs.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13251 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/win/TemporaryLinkStubs.cpp
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "BrowserExtensionWin.h"
 #include "TransferJob.h"
 #include "RenderThemeWin.h"
-#include "FontRenderer.h"
 #include "TextBoundaries.h"
 
 using namespace WebCore;
