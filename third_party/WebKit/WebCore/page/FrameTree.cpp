3e99750720f0c034eba4dfc032e73709bb134d3e
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-11 03:25:35 +0000

    - fix Windows build
            * page/FrameTree.cpp: Add include of <stdarg.h>.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameTree.cpp
+++ third_party/WebKit/WebCore/page/FrameTree.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <kxmlcore/Assertions.h>
 #include <kxmlcore/Vector.h>
+#include <stdarg.h>
 
 using std::swap;
 
