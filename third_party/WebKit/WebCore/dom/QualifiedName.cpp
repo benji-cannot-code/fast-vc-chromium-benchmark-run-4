4e2e0db940e4c0ca9838aed20c087bd42b7687f4
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-12 22:09:08 +0000

    - Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=7735         REGRESSION: crash on importing bookmarks
            No test - only reproducible when Safari imports the bookmarks.
    
            * dom/EventNames.cpp:
            (WebCore::EventNames::init): Initialize AtomicString before using it.
            * dom/QualifiedName.cpp:
            (WebCore::QualifiedName::init): Ditto.
            * khtml/html/htmlnames.cpp:
            (WebCore::HTMLNames::init): Ditto.
            * ksvg2/scripts/make_names.pl: Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/QualifiedName.cpp
+++ third_party/WebKit/WebCore/dom/QualifiedName.cpp
@@ -180,6 +180,8 @@ void QualifiedName::init()
     static bool initialized;
     if (!initialized) {
         // Use placement new to initialize the globals.
+        
+        AtomicString::init();
         new ((void*)&anyName) QualifiedName(nullAtom, starAtom, starAtom);
         initialized = true;
     }
