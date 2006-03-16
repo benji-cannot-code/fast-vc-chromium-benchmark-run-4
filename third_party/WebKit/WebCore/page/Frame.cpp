279073c622af0c0d7d3c80ed9171554b6b4f5022
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-16 00:40:11 +0000

    Reviewed by Eric.
            - Fixes win32 build.
    
            * page/Frame.cpp: (WebCore::Frame::autoscrollTimerFired): Calls isMouseButtonDown instead of CG function.
            * platform/MouseEvent.h: Added isMouseButtonDown
            * platform/mac/MouseEventMac.mm: (WebCore::MouseEvent::isMouseButtonDown): Calls CG function to get state of mouse button.
            * platform/win/TemporaryLinkStubs.cpp: (MouseEvent::isMouseButtonDown): Added stub.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -2685,7 +2685,7 @@ void Frame::handleAutoscroll(RenderLayer* layer)
 
 void Frame::autoscrollTimerFired(Timer<Frame>*)
 {
-    bool isStillDown = CGEventSourceButtonState(kCGEventSourceStateCombinedSessionState, kCGMouseButtonLeft);   
+    bool isStillDown = MouseEvent::isMouseButtonDown(LeftButton);  
     if (!isStillDown){
         stopAutoscrollTimer();
         return;
