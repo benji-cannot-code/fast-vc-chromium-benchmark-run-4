8370ace67dc9541960dc70c73e50dad5ed964597
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-17 21:58:59 +0000

    Reviewed by Adele.
            - clean up ResourceRequest to be closer to a proper request interface
    
            * page/ResourceRequest.h: folded URL into ResourceRequest, other cleanup
            (WebCore::ResourceRequest::ResourceRequest): Add new explicit constructor
            that takes a KURL.
            (WebCore::ResourceRequest::url): getter for KURL
            (WebCore::ResourceRequest::setURL): corresponding setter
            (WebCore::ResourceRequest::referrer): new getter for referrer, instead
            of storing it in m_metaData (it was the only remaining real use of it)
            (WebCore::ResourceRequest::setReferrer): corresponding setter
            * bridge/BrowserExtension.h: 
            - removed unused openURLRequest and openURLNotify methods
            - folded KURL parameter into ResourceRequest
            * bridge/mac/BrowserExtensionMac.h: remove KURL args from methods that
            also take a ResourceRequest
            * bridge/mac/BrowserExtensionMac.mm:
            (WebCore::BrowserExtensionMac::createNewWindow): adjust for ResourceRequest
            changes.
            * bridge/mac/MacFrame.h: remove KURL args from methods that also take a
            ResourceRequest
            * bridge/mac/MacFrame.mm:
            (WebCore::MacFrame::openURLRequest): adjust for ResourceRequest changes
            (WebCore::MacFrame::submitForm): ditto
            (WebCore::MacFrame::urlSelected): ditto
            * bridge/mac/WebCoreFrameBridge.mm:
            (-[WebCoreFrameBridge openURL:reload:contentType:refresh:lastModified:pageCache:]): 
            adjust for ResourceRequest changes
            * bridge/win/BrowserExtensionWin.h:
            * khtml/ecma/kjs_window.cpp:
            (KJS::createNewWindow): adjust for ResourceRequest changes
            (KJS::WindowFunc::callAsFunction): ditto
            * page/Frame.cpp:
            (WebCore::Frame::didOpenURL): adjust for ResourceRequest changes
            (WebCore::Frame::receivedFirstData): ditto
            (WebCore::Frame::begin): ditto
            (WebCore::Frame::completeURL): ditto
            (WebCore::Frame::changeLocation): ditto
            (WebCore::Frame::urlSelected): ditto
            (WebCore::Frame::requestFrame): ditto
            (WebCore::Frame::submitForm): ditto
            * page/Frame.h:
            * platform/win/TemporaryLinkStubs.cpp:
            (BrowserExtensionWin::createNewWindow): ditto
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13364 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/mac/BrowserExtensionMac.h
+++ third_party/WebKit/WebCore/bridge/mac/BrowserExtensionMac.h
@@ -35,19 +35,13 @@ class BrowserExtensionMac : public BrowserExtension {
 public:
     BrowserExtensionMac(Frame *);
  
-    virtual void openURLRequest(const KURL&, 
-                                const ResourceRequest& request = ResourceRequest());
-    virtual void openURLNotify();
-     
-    virtual void createNewWindow(const KURL& url, 
-                                 const ResourceRequest& request = ResourceRequest());
-    virtual void createNewWindow(const KURL& url,
-                                 const ResourceRequest& request, 
-                                 const WindowArgs& winArgs, 
+    virtual void createNewWindow(const ResourceRequest&);
+    virtual void createNewWindow(const ResourceRequest&, 
+                                 const WindowArgs&, 
                                  Frame*& part);
 
-    virtual void setIconURL(const KURL& request);
-    virtual void setTypedIconURL(const KURL& url, const QString& type);
+    virtual void setIconURL(const KURL&);
+    virtual void setTypedIconURL(const KURL&, const QString& type);
 
     virtual int getHistoryLength();
     virtual void goBackOrForward(int distance);
@@ -57,9 +51,8 @@ public:
     virtual void runModal();
     
 private:
-     void createNewWindow(const KURL& url, 
-                          const ResourceRequest& request, 
-                          const WindowArgs& winArgs, 
+     void createNewWindow(const ResourceRequest&, 
+                          const WindowArgs&, 
                           Frame** part);
 
      MacFrame *m_frame;
--- third_party/WebKit/WebCore/bridge/mac/BrowserExtensionMac.mm
+++ third_party/WebKit/WebCore/bridge/mac/BrowserExtensionMac.mm
@@ -41,35 +41,19 @@ BrowserExtensionMac::BrowserExtensionMac(Frame *frame)
 {
 }
 
-void BrowserExtensionMac::openURLRequest(const KURL& url, const ResourceRequest& request)
+void BrowserExtensionMac::createNewWindow(const ResourceRequest& request) 
 {
-    if (url.protocol().lower() == "javascript") {
-	m_frame->createEmptyDocument();
-	m_frame->replaceContentsWithScriptResult(url);
-     } else {
-	m_frame->openURLRequest(url, request);
-    }
+    createNewWindow(request, WindowArgs(), NULL);
 }
 
-void BrowserExtensionMac::openURLNotify()
-{
-}
-
-void BrowserExtensionMac::createNewWindow(const KURL& url, const ResourceRequest& request) 
-{
-    createNewWindow(url, request, WindowArgs(), NULL);
-}
-
-void BrowserExtensionMac::createNewWindow(const KURL& url, 
-                                          const ResourceRequest& request, 
+void BrowserExtensionMac::createNewWindow(const ResourceRequest& request, 
                                           const WindowArgs& winArgs, 
                                           Frame*& part)
 {
-    createNewWindow(url, request, winArgs, &part);
+    createNewWindow(request, winArgs, &part);
 }
 
-void BrowserExtensionMac::createNewWindow(const KURL& url, 
-                                          const ResourceRequest& request, 
+void BrowserExtensionMac::createNewWindow(const ResourceRequest& request, 
                                           const WindowArgs& winArgs, 
                                           Frame** partResult)
 { 
@@ -79,13 +63,15 @@ void BrowserExtensionMac::createNewWindow(const KURL& url,
 
     if (partResult)
 	*partResult = NULL;
+    
+    const KURL& url = request.url();
 
     NSString *frameName = request.frameName.length() == 0 ? nil : request.frameName.getNSString();
     if (frameName) {
         // FIXME: Can't we just use m_frame->findFrame?
         if (WebCoreFrameBridge *bridge = [m_frame->bridge() findFrameNamed:frameName]) {
             if (!url.isEmpty()) {
-                DOMString argsReferrer = request.metaData().get("referrer");
+                DOMString argsReferrer = request.referrer();
                 NSString *referrer;
                 if (!argsReferrer.isEmpty())
                     referrer = argsReferrer;
