373cd944700b74d0f6424547e2a3ff75af4ff2a1
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-21 23:36:17 +0000

    Reviewed by Darin.
            - fix horrible build fallout from my attr fix
    
            * bindings/js/JSCanvasRenderingContext2DBase.cpp:
            * bindings/js/JSDOMCore.cpp:
            * bindings/js/JSDOMEvents.cpp:
            * bindings/js/JSDOMHTML.cpp:
            * bindings/scripts/CodeGeneratorJS.pm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSCanvasRenderingContext2DBase.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSCanvasRenderingContext2DBase.cpp
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Boston, MA 02111-1307, USA.
  */
 
+// This file needs to be touched every once in a while.
+
 #include "config.h"
 #include "JSCanvasRenderingContext2DBase.h"
 
--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -767,7 +767,7 @@ sub TypeCanFailConversion
   my $type = $signature->type;
 
   if ($type eq "boolean") {
-    return 1;
+    return 0;
   } elsif ($type eq "unsigned long" or $type eq "long") {
     return 0; # or can it?
   } elsif ($type eq "unsigned short") {
@@ -781,15 +781,15 @@ sub TypeCanFailConversion
   } elsif ($type eq "DOMString") {
       return 0;
   } elsif ($type eq "views::AbstractView") {
-      return 1;
+      return 0;
   } elsif ($type eq "Node") {
-      return 1;
+      return 0;
   } elsif ($type eq "Attr") {
       return 1;
   } elsif ($type eq "DocumentType") {
-      return 1;
+      return 0;
   } elsif ($type eq "Range") {
-      return 1;
+      return 0;
   } else {
     die "Don't know whether a JS value can fail conversion to type $type."
   }
@@ -800,7 +800,7 @@ sub JSValueToNative
   my $signature = shift;
   my $value = shift;
   my $okParam = shift;
-  my $maybeOkParam = "${okParam}" ? ", ${okParam}" : "";
+  my $maybeOkParam = $okParam ? ", ${okParam}" : "";
   
   my $type = $signature->type;
 
