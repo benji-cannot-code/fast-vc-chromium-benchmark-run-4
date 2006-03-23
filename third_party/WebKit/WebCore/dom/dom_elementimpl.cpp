c08a1357156062e77e3fe6662956926d9d6d85ba
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-23 01:21:25 +0000

    2006-03-22  Eric Seidel  <eseidel@apple.com>
            Reviewed by hyatt.
    
            Fix attribute mutation events to not fire for "style" attributes.
            We lazily update style attributes, so sending mutation events for them makes no sense anymore.
            <rdar://problem/4474910> repro assertion failure @ apple.com/store: !eventDispatchForbidden()
    
            Test: fast/events/delayed-style-mutation-event-crash.html
    
            * dom/dom_elementimpl.cpp:
            (WebCore::Element::dispatchAttrRemovalEvent): add assert
            (WebCore::Element::dispatchAttrAdditionEvent): add assert
            (WebCore::NamedAttrMap::addAttribute):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/dom_elementimpl.cpp
+++ third_party/WebKit/WebCore/dom/dom_elementimpl.cpp
@@ -622,6 +622,7 @@ bool Element::childTypeAllowed(NodeType type)
 
 void Element::dispatchAttrRemovalEvent(Attribute*)
 {
+    assert(!eventDispatchForbidden());
 #if 0
     if (!getDocument()->hasListenerType(Document::DOMATTRMODIFIED_LISTENER))
         return;
@@ -633,6 +634,7 @@ void Element::dispatchAttrRemovalEvent(Attribute*)
 
 void Element::dispatchAttrAdditionEvent(Attribute *attr)
 {
+    assert(!eventDispatchForbidden());
 #if 0
     if (!getDocument()->hasListenerType(Document::DOMATTRMODIFIED_LISTENER))
         return;
@@ -1026,7 +1028,8 @@ void NamedAttrMap::detachFromElement()
 NamedAttrMap& NamedAttrMap::operator=(const NamedAttrMap& other)
 {
     // clone all attributes in the other map, but attach to our element
-    if (!element) return *this;
+    if (!element)
+        return *this;
 
     // If assigning the map changes the id attribute, we need to call
     // updateId.
@@ -1034,9 +1037,8 @@ NamedAttrMap& NamedAttrMap::operator=(const NamedAttrMap& other)
     Attribute *oldId = getAttributeItem(idAttr);
     Attribute *newId = other.getAttributeItem(idAttr);
 
-    if (oldId || newId) {
+    if (oldId || newId)
         element->updateId(oldId ? oldId->value() : nullAtom, newId ? newId->value() : nullAtom);
-    }
 
     clearAttributes();
     len = other.len;
@@ -1081,8 +1083,11 @@ void NamedAttrMap::addAttribute(Attribute *attribute)
     if (element) {
         RefPtr<Attribute> a = attribute;
         element->attributeChanged(a.get());
-        element->dispatchAttrAdditionEvent(a.get());
-        element->dispatchSubtreeModifiedEvent(false);
+        // Because of our updateStyleAttributeIfNeeded() style modification events are never sent at the right time, so don't bother sending them.
+        if (a->name() != styleAttr) {
+            element->dispatchAttrAdditionEvent(a.get());
+            element->dispatchSubtreeModifiedEvent(false);
+        }
     }
 }
 
@@ -1105,8 +1110,7 @@ void NamedAttrMap::removeAttribute(const QualifiedName& name)
         fastFree(attrs);
         attrs = 0;
         len = 0;
-    }
-    else {
+    } else {
         Attribute **newAttrs = static_cast<Attribute **>(fastMalloc((len - 1) * sizeof(Attribute *)));
         unsigned i;
         for (i = 0; i < unsigned(index); i++)
