2afba0bb29d90329db4cb0fb5926bedf2aebacc7
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-25 10:46:44 +0000

    2006-03-23  Eric Seidel  <eseidel@apple.com>
            Reviewed by mjs.
    
            Shave .2% on PLT by removing bogus strcmp.
            http://bugzilla.opendarwin.org/show_bug.cgi?id=7938
    
            Test: fast/parser/tag-with-exclamation-point.html
    
            * html/HTMLTokenizer.cpp:
            (WebCore::HTMLTokenizer::parseTag): remove bogus strcmp
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13478 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
+++ third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
@@ -886,8 +886,7 @@ HTMLTokenizer::State HTMLTokenizer::parseTag(SegmentedString &src, State state)
                 char* ptr = cBuffer;
                 unsigned int len = cBufferPos;
                 cBuffer[cBufferPos] = '\0';
-                if ((cBufferPos > 0) && (*ptr == '/'))
-                {
+                if ((cBufferPos > 0) && (*ptr == '/')) {
                     // End Tag
                     beginTag = false;
                     ptr++;
@@ -902,7 +901,8 @@ HTMLTokenizer::State HTMLTokenizer::parseTag(SegmentedString &src, State state)
                     ptr[--len] = '\0';
 
                 // Now that we've shaved off any invalid / that might have followed the name), make the tag.
-                if (ptr[0] != '!' && strcmp(ptr, "!doctype") != 0) {
+                // FIXME: FireFox and WinIE turn !foo nodes into comments, we ignore comments. (fast/parser/tag-with-exclamation-point.html)
+                if (ptr[0] != '!') {
                     currToken.tagName = AtomicString(ptr);
                     currToken.beginTag = beginTag;
                 }
