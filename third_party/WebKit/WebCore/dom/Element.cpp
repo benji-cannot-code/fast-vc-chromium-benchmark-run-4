a9bc66cbcc52dee5233a7029facec92ea899f074
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-26 22:58:26 +0000

    2006-03-25  Mitz Pettel  <opendarwin.org@mitzpettel.com>
            Reviewed by darin.  Landed by eseidel.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=7916
              Box repaint rect does not include the left overflow
    
            * manual-tests/left-overflow-repaint.html: Added.
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::overflowRect): Changed m_overflowWidth and m_overflowHeight
            to overflowWidth() and overflowHeight() since RenderTable overrides the latter.
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::getAbsoluteRepaintRect): Include top and left overflows.
            * rendering/RenderFlow.cpp:
            (WebCore::RenderFlow::getAbsoluteRepaintRect): Removed redundant code.
            * rendering/RenderTableCell.cpp: Removed subclass implementation of
            getAbsoluteRepaintRect().
            * rendering/RenderTableCell.h:
            * rendering/bidi.cpp:
            (WebCore::RenderBlock::layoutInlineChildren): Include left overflow in repaint rect.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13483 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Element.cpp
+++ third_party/WebKit/WebCore/dom/Element.cpp
@@ -26,10 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "Element.h"
 
-#include "CSSStyleSelector.h"
+#include "cssstyleselector.h"
 #include "Document.h"
 #include "ExceptionCode.h"
-#include "htmlnames.h"
+#include "HTMLNames.h"
 #include "Frame.h"
 #include "htmlnames.h"
 #include "NamedAttrMap.h"
--- third_party/WebKit/WebCore/rendering/RenderTableCell.h
+++ third_party/WebKit/WebCore/rendering/RenderTableCell.h
@@ -107,8 +107,6 @@ public:
     virtual void dump(QTextStream *stream, DeprecatedString ind = "") const;
 #endif
 
-    virtual IntRect getAbsoluteRepaintRect();
-    
     void paintBackgroundsBehindCell(PaintInfo&, int tx, int ty, RenderObject* backgroundObject);
 
 protected:
