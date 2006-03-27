e0ef388cc270220f5808075a2fc1e47d077edb7e
tomernic <tomernic@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-27 22:07:12 +0000

    Reviewed by Tim Hatcher.
            Part of <rdar://problem/4448350> Deprecated ObjC language API used in JavaScriptCore, WebCore,
            WebKit and WebBrowser
    
            * Carbon/HIViewAdapter.h:
            HIViewAdapter is no longer an NSView subclass, since we no longer pose it as NSView.
            * Carbon/HIViewAdapter.m:
            (+[NSView bindHIViewToNSView:nsView:]):
            Replace individual NSView methods instead of posing as NSView.
            (_webkit_NSView_setNeedsDisplay):
            Replacement implementation of -[NSView setNeedsDisplay:].
            (_webkit_NSView_setNeedsDisplayInRect):
            Replacement implementation of -[NSView setNeedsDisplayInRect:]
            (_webkit_NSView_nextValidKeyView):
            Replacement implementation of -[NSView nextValidKeyView]
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13496 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Carbon/HIViewAdapter.h
+++ third_party/WebKit/WebKit/Carbon/HIViewAdapter.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKit.h>
 #include <HIToolbox/HIView.h>
 
-@interface HIViewAdapter : NSView
+@interface HIViewAdapter : NSObject
 
 + (void)bindHIViewToNSView:(HIViewRef)hiView nsView:(NSView*)nsView;
 + (void)unbindNSView:(NSView*)nsView;
