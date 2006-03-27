cfec2337fd58074f815baed4946b197eb6876f13
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-27 22:21:09 +0000

    At the request of Michael Emmel <mike.emmel@gmail.com>.
            - fix compilation for Linux
              http://bugzilla.opendarwin.org/show_bug.cgi?id=8013
    
            * rendering/render_style.h: Add declaration of CSSStyleSelector. Also tweaked
            formatting a bit and removed some extraneous WebCore:: prefixes.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/render_style.h
+++ third_party/WebKit/WebCore/rendering/render_style.h
@@ -58,14 +58,12 @@ template<typename T, typename U> inline bool compareEqual(const T& t, const U& u
 class RenderArena;
 
 namespace WebCore {
-    class StringImpl;
-    class ShadowValue;
-}
 
-namespace WebCore {
-
-    class CachedImage;
-    class CachedObject;
+class CSSStyleSelector;
+class CachedImage;
+class CachedObject;
+class ShadowValue;
+class StringImpl;
 
 enum PseudoState { PseudoUnknown, PseudoNone, PseudoAnyLink, PseudoLink, PseudoVisited};
 
@@ -617,7 +615,7 @@ struct ShadowData {
 
 #ifndef KHTML_NO_XBL
 struct BindingURI {
-    BindingURI(WebCore::StringImpl*);
+    BindingURI(StringImpl*);
     ~BindingURI();
 
     BindingURI* copy();
@@ -630,10 +628,10 @@ struct BindingURI {
     BindingURI* next() { return m_next; }
     void setNext(BindingURI* n) { m_next = n; }
     
-    WebCore::StringImpl* uri() { return m_uri; }
+    StringImpl* uri() { return m_uri; }
     
     BindingURI* m_next;
-    WebCore::StringImpl* m_uri;
+    StringImpl* m_uri;
 };
 #endif
 
@@ -797,7 +795,7 @@ public:
     CachedImage *style_image;
     CachedImage *cursor_image;
 
-    WebCore::Font font;
+    Font font;
     Color color;
     
     short horizontal_border_spacing;
@@ -849,14 +847,14 @@ struct ContentData {
 
     ContentType contentType() { return _contentType; }
 
-    WebCore::StringImpl* contentText() { if (contentType() == CONTENT_TEXT) return _content.text; return 0; }
+    StringImpl* contentText() { if (contentType() == CONTENT_TEXT) return _content.text; return 0; }
     CachedObject* contentObject() { if (contentType() == CONTENT_OBJECT) return _content.object; return 0; }
     
     ContentType _contentType;
 
     union {
         CachedObject* object;
-        WebCore::StringImpl* text;
+        StringImpl* text;
         // counters...
     } _content ;
 
@@ -1022,7 +1020,7 @@ protected:
     int m_ref;
     
 #if SVG_SUPPORT
-    DataRef<WebCore::SVGRenderStyle> m_svgStyle;
+    DataRef<SVGRenderStyle> m_svgStyle;
 #endif
     
 // !END SYNC!
@@ -1502,7 +1500,7 @@ public:
     void inheritBindingURIs(BindingURI* other) {
         SET_VAR(css3NonInheritedData, bindingURI, other->copy());
     }
-    void addBindingURI(WebCore::StringImpl* uri);
+    void addBindingURI(StringImpl* uri);
 #endif
     void setOutlineOffset(unsigned short v) { SET_VAR(background, m_outline._offset, v) }
     void setTextShadow(ShadowData* val, bool add=false);
@@ -1539,13 +1537,13 @@ public:
     void setTextSizeAdjust(bool b) { SET_VAR(css3InheritedData, textSizeAdjust, b); }
 
 #if SVG_SUPPORT
-    const WebCore::SVGRenderStyle* svgStyle() const { return m_svgStyle.get(); }
-    WebCore::SVGRenderStyle* accessSVGStyle() { return m_svgStyle.access(); }
+    const SVGRenderStyle* svgStyle() const { return m_svgStyle.get(); }
+    SVGRenderStyle* accessSVGStyle() { return m_svgStyle.access(); }
 #endif
 
     ContentData* contentData() { return content; }
     bool contentDataEquivalent(const RenderStyle *otherStyle) const;
-    void setContent(WebCore::StringImpl* s, bool add = false);
+    void setContent(StringImpl* s, bool add = false);
     void setContent(CachedObject* o, bool add = false);
 
     bool inheritedNotEqual( RenderStyle *other ) const;
@@ -1661,7 +1659,6 @@ public:
     static const DeprecatedValueList<StyleDashboardRegion>& noneDashboardRegions();
 };
 
-
-} // namespace
+} // namespace WebCore
 
 #endif
