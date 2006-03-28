1ec9c6242cf3e3e95f97e1f72c41e16ac1137ecf
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-28 23:52:49 +0000

    Reviewed by Eric.
            fixes <rdar://problem/4458539> Unable to include Security(public) and WebKit(private) headers
    
            * bindings/npapi.h: added #defines after the #ifndefs
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13537 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/npapi.h
+++ third_party/WebKit/JavaScriptCore/bindings/npapi.h
@@ -106,19 +106,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*----------------------------------------------------------------------*/
 
 #ifndef _UINT16
+#define _UINT16
 typedef unsigned short uint16;
 #endif
+
 #ifndef _UINT32
+#define _UINT32
 #if __LP64__
 typedef unsigned int uint32;
 #else /* __LP64__ */
 typedef unsigned long uint32;
 #endif /* __LP64__ */
 #endif
+
 #ifndef _INT16
+#define _INT16
 typedef short int16;
 #endif
+
 #ifndef _INT32
+#define _INT32
 #if __LP64__
 typedef int int32;
 #else /* __LP64__ */
