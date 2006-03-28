bcb16a75852b5cb4fcb41733af2e0828aed23e6f
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-28 01:07:28 +0000

    Based on a patch by Michael Emmel <mike.emmel@gmail.com>.
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=8012
              TransferJob.cpp includes non-existent "String.h" (capital S)
    
            * platform/TransferJob.cpp: Remove unneeded include of "String.h".
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13522 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/TransferJob.cpp
+++ third_party/WebKit/WebCore/platform/TransferJob.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2004 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2004, 2006 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,14 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "TransferJob.h"
-
 #include "TransferJobInternal.h"
 
 #include "KURL.h"
 #include "KWQLoader.h"
 #include "Logging.h"
 #include "formdata.h"
-#include "String.h"
 
 namespace WebCore {
 
