727fb3173f0435ea1af7af28c291cd11513f982d
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-28 00:54:30 +0000

    Reviewed by Darin.
            <rdar://problem/4427002> REGRESSION: VoiceOver doesn't read heading level text in Safari (Range selectNodeContents broken)
    
            Better fix than previous checkin, since maxDeepOffset is really an editing hack.
    
            * editing/selection/selectNodeContents-textNode.html: Added.
    
            * dom/Range.cpp:
            (WebCore::Range::selectNodeContents):
            Use maxOffset if offsetInCharacters, otherwise use childNodeCount.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13520 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Range.cpp
+++ third_party/WebKit/WebCore/dom/Range.cpp
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Document.h"
 #include "ExceptionCode.h"
-#include "htmlediting.h"
 #include "HTMLElement.h"
 #include "RenderBlock.h"
 #include "VisiblePosition.h"
@@ -1080,7 +1079,7 @@ void Range::selectNodeContents( Node *refNode, ExceptionCode& ec)
     m_startContainer = refNode;
     m_startOffset = 0;
     m_endContainer = refNode;
-    m_endOffset = maxDeepOffset(refNode);
+    m_endOffset = refNode->offsetInCharacters() ? refNode->maxOffset() : refNode->childNodeCount();
 }
 
 void Range::surroundContents(PassRefPtr<Node> passNewParent, ExceptionCode& ec)
