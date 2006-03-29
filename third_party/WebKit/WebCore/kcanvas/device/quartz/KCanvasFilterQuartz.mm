f00eec4ecab2b36f544add46d3e7010a74d66c42
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-29 02:46:20 +0000

    2006-03-28  Eric Seidel  <eseidel@apple.com>
            Reviewed by darin.
    
            Remove global initializers.
    
            * kcanvas/device/quartz/KCanvasFilterQuartz.mm:
            * ksvg2/svg/SVGAngle.cpp:
            * ksvg2/svg/SVGColor.cpp:
            * ksvg2/svg/SVGLength.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13549 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kcanvas/device/quartz/KCanvasFilterQuartz.mm
+++ third_party/WebKit/WebCore/kcanvas/device/quartz/KCanvasFilterQuartz.mm
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-static DeprecatedString KCPreviousFilterOutputName = "__previousOutput__";
+static const char* const KCPreviousFilterOutputName = "__previousOutput__";
 
 static inline CIColor *ciColor(const Color &c)
 {
