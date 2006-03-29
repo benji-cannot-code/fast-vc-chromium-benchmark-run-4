c8bd9d9532d7a613f5c5427f34998bf5a0ac3eff
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-29 00:09:20 +0000

    Reviewed by Geoff.
            - change some code that resulted in init routines on Mac OS X -- if the framework has
              init routines it will use memory and slow down applications that link with WebKit
              even in cases where those applications don't use WebKit
    
            * kjs/date_object.cpp: Changed constants that were derived by multiplying other constants
            to use immediate numbers instead. Apparently, double constant expressions of the type we
            had here are evaluated at load time.
    
            * kjs/list.cpp: Can't use OwnArrayPtr in ListImp because of the global instances of
            ListImp, so go back to using a plain old pointer.
            (KJS::List::List): Set overflow to 0 when initializing ListImp.
            (KJS::List::release): Replace a clear call with a delete and explicit set to 0.
            (KJS::List::append): Use raw pointers, and do a delete [] instead of finessing it with
            a swap of OwnArrayPtr.
            (KJS::List::copyFrom): Remove now-unneeded get().
            (KJS::List::copyTail): Ditto.
    
            * kjs/ustring.cpp: Changed UString::Rep::empty initializer a bit so that it doesn't get
            a static initializer routine. Had to get rid of one level of constant to get the compiler
            to understand it could initialize without any code.
    
            - added a build step that checks for init routines
    
            * JavaScriptCore.xcodeproj/project.pbxproj: Deleted now-unused custom build rule that
            was replaced by the generate-derived-sources script a while back. Added a custom build
            phase that invokes the check-for-global-initializers script.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13541 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/list.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/list.cpp
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "internal.h"
 #include <algorithm>
-#include <kxmlcore/OwnArrayPtr.h>
 
 #define DUMP_STATISTICS 0
 
@@ -43,7 +42,7 @@ struct ListImp : ListImpBase
 {
     ListImpState state;
     int capacity;
-    OwnArrayPtr<JSValue*> overflow;
+    JSValue** overflow;
 
     union {
         JSValue *values[inlineValuesSize];
@@ -172,6 +171,8 @@ List::List() : _impBase(allocateListImp()), _needsMarking(false)
     imp->refCount = 1;
     imp->valueRefCount = 1;
     imp->capacity = 0;
+    imp->overflow = 0;
+
 #if DUMP_STATISTICS
     if (++numLists > numListsHighWaterMark)
         numListsHighWaterMark = numLists;
@@ -186,6 +187,7 @@ List::List(bool needsMarking) : _impBase(allocateListImp()), _needsMarking(needs
     imp->refCount = 1;
     imp->valueRefCount = !needsMarking;
     imp->capacity = 0;
+    imp->overflow = 0;
 
 #if DUMP_STATISTICS
     if (++numLists > numListsHighWaterMark)
@@ -211,7 +213,8 @@ void List::release()
             ++numListsBiggerThan[i];
 #endif
 
-    imp->overflow.clear();
+    delete [] imp->overflow;
+    imp->overflow = 0;
 
     if (imp->state == usedInPool) {
         imp->state = unusedInPool;
@@ -272,12 +275,13 @@ void List::append(JSValue *v)
     
     if (i >= imp->capacity) {
         int newCapacity = i * 2;
-        OwnArrayPtr<JSValue*> newOverflow(new JSValue* [newCapacity - inlineValuesSize]);
-        JSValue** oldOverflow = imp->overflow.get();
+        JSValue** newOverflow = new JSValue* [newCapacity - inlineValuesSize];
+        JSValue** oldOverflow = imp->overflow;
         int oldOverflowSize = i - inlineValuesSize;
         for (int j = 0; j != oldOverflowSize; j++)
             newOverflow[j] = oldOverflow[j];
-        imp->overflow.swap(newOverflow);
+        delete [] oldOverflow;
+        imp->overflow = newOverflow;
         imp->capacity = newCapacity;
     }
     
@@ -301,7 +305,7 @@ void List::copyFrom(const List& other)
     for (int i = 0; i != inlineSize; ++i)
         append(imp->values[i]);
 
-    JSValue** overflow = imp->overflow.get();
+    JSValue** overflow = imp->overflow;
     int overflowSize = size - inlineSize;
     for (int i = 0; i != overflowSize; ++i)
         append(overflow[i]);
@@ -320,7 +324,7 @@ List List::copyTail() const
     for (int i = 1; i < inlineSize; ++i)
         copy.append(imp->values[i]);
 
-    JSValue** overflow = imp->overflow.get();
+    JSValue** overflow = imp->overflow;
     int overflowSize = size - inlineSize;
     for (int i = 0; i < overflowSize; ++i)
         copy.append(overflow[i]);
