948a5ee219bd473f6a720899fe5038ee6b91619f
tomernic <tomernic@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-30 23:48:53 +0000

    Reviewed by NOBODY (just fixing copyrights)
            * DumpRenderTree/ObjCPlugin.h:
    	Fixed copyright.
            * DumpRenderTree/ObjCPlugin.m: ditto
            * DumpRenderTree/ObjCPluginFunction.h: ditto
            * DumpRenderTree/ObjCPluginFunction.m: ditto
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13587 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/ObjCPlugin.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/ObjCPlugin.h
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2005 Apple Computer, Inc.  All rights reserved.
- * Copyright (C) 2005 Ben La Monica <ben.lamonica@gmail.com>.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
