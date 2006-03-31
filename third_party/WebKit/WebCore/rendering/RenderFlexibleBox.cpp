2ed05a34d1b369fa7f4444f5c000fffe48d76a06
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-31 05:49:47 +0000

    LayoutTests:
            Reviewed by Darin.
    
            - Test for http://bugzilla.opendarwin.org/show_bug.cgi?id=8056
              Flexible boxes do not repaint their top, left and children's overflows
    
            * fast/repaint/flexible-box-overflow-expected.checksum: Added.
            * fast/repaint/flexible-box-overflow-expected.png: Added.
            * fast/repaint/flexible-box-overflow-expected.txt: Added.
            * fast/repaint/flexible-box-overflow-horizontal-expected.checksum: Added.
            * fast/repaint/flexible-box-overflow-horizontal-expected.png: Added.
            * fast/repaint/flexible-box-overflow-horizontal-expected.txt: Added.
            * fast/repaint/flexible-box-overflow-horizontal.html: Added.
            * fast/repaint/flexible-box-overflow.html: Added.
    
    WebCore:
    
            Tests: fast/repaint/flexible-box-overflow.html fast/repaint/flexible-box-overflow-horizontal.html
    
            Reviewed by Darin.
            
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=8056
              Flexible boxes do not repaint their top, left and children's overflows
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::layoutBlockChildren):
            * rendering/RenderFlexibleBox.cpp:
            (WebCore::FlexBoxIterator::next):
            (WebCore::RenderFlexibleBox::layoutHorizontalBox): Update top overflow when
            determining vertical positions. Update horizontal overflows after horizontal
            positions are determined.
            (WebCore::RenderFlexibleBox::layoutVerticalBox):  Update left overflow when
            determining horizontal positions. Update vertical overflows after vertical
            positions are determined.
            (WebCore::RenderFlexibleBox::allowedChildFlex):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13596 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderFlexibleBox.cpp
+++ third_party/WebKit/WebCore/rendering/RenderFlexibleBox.cpp
@@ -71,8 +71,7 @@ public:
                     if (currentOrdinal > lastOrdinal)
                         return 0;
                     current = box->firstChild();
-                }
-                else {
+                } else {
                     currentOrdinal--;
                     if (currentOrdinal == 0)
                         return 0;
@@ -491,6 +490,7 @@ void RenderFlexibleBox::layoutHorizontalBox(bool relayoutChildren)
 
             placeChild(child, xPos, childY);
             m_overflowHeight = kMax(m_overflowHeight, childY + child->overflowHeight(false));
+            m_overflowTop = kMin(m_overflowTop, child->yPos() + child->overflowTop(false));
             
             xPos += child->width() + child->marginRight();
     
@@ -576,9 +576,6 @@ void RenderFlexibleBox::layoutHorizontalBox(bool relayoutChildren)
 
     m_flexingChildren = false;
     
-    if (xPos > m_overflowWidth)
-        m_overflowWidth = xPos;
-
     if (remainingSpace > 0 && ((style()->direction() == LTR && style()->boxPack() != BSTART) ||
                                (style()->direction() == RTL && style()->boxPack() != BEND))) {
         // Children must be repositioned.
@@ -622,8 +619,7 @@ void RenderFlexibleBox::layoutHorizontalBox(bool relayoutChildren)
                     child = iterator.next();
                 }
             }
-        }
-        else {
+        } else {
             if (style()->boxPack() == BCENTER)
                 offset += remainingSpace/2;
             else // END for LTR, START for RTL
@@ -638,6 +634,22 @@ void RenderFlexibleBox::layoutHorizontalBox(bool relayoutChildren)
                 child = iterator.next();
             }
         }
+    }
+    
+    child = iterator.first();
+    while (child && child->isPositioned()) {
+        child = iterator.next();
+    }
+    
+    if (child) {
+        m_overflowLeft = kMin(child->xPos() + child->overflowLeft(false), m_overflowLeft);
+
+        RenderObject* lastChild = child;
+        while ((child = iterator.next())) {
+            if (!child->isPositioned())
+                lastChild = child;
+        }
+        m_overflowWidth = kMax(lastChild->xPos() + lastChild->overflowWidth(false), m_overflowWidth);
     }
     
     // So that the calcHeight in layoutBlock() knows to relayout positioned objects because of
@@ -839,10 +851,8 @@ void RenderFlexibleBox::layoutVerticalBox(bool relayoutChildren)
             m_height += child->height() + child->marginBottom();
     
             // See if this child has made our overflow need to grow.
-            // XXXdwh Work with left overflow as well as right overflow.
-            int rightChildPos = child->xPos() + kMax(child->overflowWidth(false), child->width());
-            if (rightChildPos > m_overflowWidth)
-                m_overflowWidth = rightChildPos;
+            m_overflowWidth = kMax(child->xPos() + child->overflowWidth(false), m_overflowWidth);
+            m_overflowLeft = kMin(child->xPos() + child->overflowLeft(false), m_overflowLeft);
             
             child = iterator.next();
         }
@@ -982,8 +992,7 @@ void RenderFlexibleBox::layoutVerticalBox(bool relayoutChildren)
                     child = iterator.next();
                 }
             }
-        }
-        else {
+        } else {
             if (style()->boxPack() == BCENTER)
                 offset += remainingSpace/2;
             else // END
@@ -1000,6 +1009,22 @@ void RenderFlexibleBox::layoutVerticalBox(bool relayoutChildren)
         }
     }
     
+    child = iterator.first();
+    while (child && child->isPositioned()) {
+        child = iterator.next();
+    }
+    
+    if (child) {
+        m_overflowTop = kMin(child->yPos() + child->overflowTop(false), m_overflowTop);
+
+        RenderObject* lastChild = child;
+        while ((child = iterator.next())) {
+            if (!child->isPositioned())
+                lastChild = child;
+        }
+        m_overflowHeight = kMax(lastChild->yPos() + lastChild->overflowHeight(false), m_overflowHeight);
+    }
+
     // So that the calcHeight in layoutBlock() knows to relayout positioned objects because of
     // a height change, we revert our height back to the intrinsic height before returning.
     if (heightSpecified)
@@ -1041,8 +1066,7 @@ int RenderFlexibleBox::allowedChildFlex(RenderObject* child, bool expanding, uns
             if (maxW == INT_MAX)
                 return maxW;
             return kMax(0, maxW - w);
-        }
-        else {
+        } else {
             // FIXME: For now just handle fixed values.
             int maxH = INT_MAX;
             int h = child->overrideHeight() - (child->borderTop() + child->borderBottom() + child->paddingTop() + child->paddingBottom());
@@ -1068,8 +1092,7 @@ int RenderFlexibleBox::allowedChildFlex(RenderObject* child, bool expanding, uns
             
         int allowedShrinkage = kMin(0, minW - w);
         return allowedShrinkage;
-    }
-    else {
+    } else {
         if (child->style()->minHeight().isFixed()) {
             int minH = child->style()->minHeight().value();
             int h = child->contentHeight();
