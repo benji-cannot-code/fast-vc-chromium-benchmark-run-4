28f3e2b8a022f1f3661ee51857b128920dca9568
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-31 07:37:53 +0000

    2006-03-31  Anders Carlsson  <andersca@mac.com>
            Reviewed by Maciej.
    
            Add getter/setter GC tests.
            
            * fast/js/getter-setter-gc-expected.txt: Added.
            * fast/js/getter-setter-gc.html: Added.
            * fast/js/resources/getter-setter-gc.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/getter-setter-gc.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This test checks whether getters and setters work correctly with garbage collection."
+);
+
+var o = {};
+o.__defineGetter__("x", function() { return 242; })
+
+shouldBe('o.x', '242');
+
+// Force a gc
+var i = 0;
+var s;
+while (i < 5000) {
+    i = i+1.11;
+    s = s + " ";
+}
+
+shouldBe('o.x', '242')
+
+successfullyParsed = true;
