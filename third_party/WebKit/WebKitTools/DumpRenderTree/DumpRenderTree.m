cbb34c1c64cb26c332c21d62d4dc0c2dce1f1e58
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-31 18:51:40 +0000

    LayoutTests:
            - updated results for tests that involve scrolling
    
            * editing/selection/focus_editable_html-expected.txt: Added expected scroll position.
            * fast/dom/focus-contenteditable-expected.txt: Ditto.
            * fast/overflow/scrollRevealButton.html: Ditto.
    
            - updated results for tests that involve new exception names
    
            * fast/dom/Element/attr-param-typechecking-expected.txt: Updated.
    
            * fast/dynamic/insertAdjacentElement-expected.txt: Updated.
            * fast/dynamic/insertAdjacentElement-expected.png: Added.
            * fast/dynamic/insertAdjacentElement-expected.checksum: Added.
    
    WebCore:
    
            Reviewed by John Sullivan.
    
            * khtml/ecma/kjs_binding.cpp: Added names for VALIDATION_ERR and
            TYPE_MISMATCH_ERR; new DOM Level 3 errors that need to be listed in
            the mapping from error code to error name.
    
            * dom/Element.idl: Removed comment from bad old days where we had to
            touch these files to make them rebuild.
    
    WebKitTools:
    
            Reviewed by John Sullivan.
    
            - added a "--reset-results" option to run-webkit-tests so you can reset
              the results without first deleting expected results
            - <rdar://problem/4185878> add scroll position to dumpRenderTree
    
            * Scripts/run-webkit-tests: Rearranged the code a bit to make the sequence
            more logical. Moved all the subroutines to the end of the file. Added a
            "--force" option.
    
            * DumpRenderTree/DumpRenderTree.m:
            (main): Added a new --dump-all-pixels option, used when forcing run-webkit-tests
            to generate new output for all tests it runs.
            (dump): Dump the scroll position if it's non-zero. Always dump the image when
            the --dump-all-pixels option is passed. Also tightened up the image dumping
            code and replaced the incorrect use of +[NSGraphicsContext saveGraphicsState]
            with code to save and restore the context.
    
            * DumpRenderTree/DumpRenderTree.h: Tweaked includes a bit and added (void).
            * DumpRenderTree/EventSendingController.m: Added now-needed include.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13611 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -51,13 +51,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <getopt.h>
 #import <malloc/malloc.h>
 
-#import "TextInputController.h"
-#import "NavigationController.h"
 #import "AppleScriptController.h"
-#import "EventSendingController.h"
+#import "DumpRenderTreeDraggingInfo.h"
 #import "EditingDelegate.h"
+#import "EventSendingController.h"
+#import "NavigationController.h"
 #import "ObjCPlugin.h"
 #import "ObjCPluginFunction.h"
+#import "TextInputController.h"
 
 @interface DumpRenderTreeWindow : NSWindow
 @end
@@ -85,6 +86,7 @@ static BOOL dumpAsText;
 static BOOL dumpSelectionRect;
 static BOOL dumpTitleChanges;
 static int dumpPixels = NO;
+static int dumpAllPixels = NO;
 static int testRepaintDefault = NO;
 static BOOL testRepaint = NO;
 static int repaintSweepHorizontallyDefault = NO;
@@ -100,7 +102,7 @@ static CGColorSpaceRef sharedColorSpace;
 const unsigned maxViewHeight = 600;
 const unsigned maxViewWidth = 800;
 
-BOOL doneLoading()
+BOOL doneLoading(void)
 {
     return done;
 }
@@ -187,11 +189,12 @@ int main(int argc, const char *argv[])
     class_poseAs(objc_getClass("DumpRenderTreeWindow"), objc_getClass("NSWindow"));
     
     struct option options[] = {
-        {"pixel-tests", no_argument, &dumpPixels, YES},
-        {"tree", no_argument, &dumpTree, YES},
-        {"notree", no_argument, &dumpTree, NO},
-        {"repaint", no_argument, &testRepaintDefault, YES},
+        {"dump-all-pixels", no_argument, &dumpAllPixels, YES},
         {"horizontal-sweep", no_argument, &repaintSweepHorizontallyDefault, YES},
+        {"notree", no_argument, &dumpTree, NO},
+        {"pixel-tests", no_argument, &dumpPixels, YES},
+        {"repaint", no_argument, &testRepaintDefault, YES},
+        {"tree", no_argument, &dumpTree, YES},
         {NULL, 0, NULL, 0}
     };
 
@@ -350,31 +353,31 @@ static void dump(void)
         
         if (!result)
             printf("ERROR: nil result from %s", dumpAsText ? "[documentElement innerText]" : "[frame renderTreeAsExternalRepresentation]");
-        else
+        else {
             fputs([result UTF8String], stdout);
-        
+            if (!dumpAsText) {
+                NSPoint scrollPosition = [[[[frame frameView] documentView] superview] bounds].origin;
+                if (scrollPosition.x != 0 || scrollPosition.y != 0)
+                    printf("scrolled to %0.f,%0.f\n", scrollPosition.x, scrollPosition.y);
+            }
+        }
+
         if (printSeparators)
             puts("#EOF");
     }
     
     if (dumpPixels) {
         if (!dumpAsText) {
-            // FIXME: It's unfortunate that we hardcode the file naming scheme here.
-            // At one time, the perl script had all the knowledge about file layout.
-            // Some day we should restore that setup by passing in more parameters to this tool.
-
-            NSString *baseTestPath = [currentTest stringByDeletingPathExtension];
-            NSString *baselineHashPath = [baseTestPath stringByAppendingString:@"-expected.checksum"];
-            NSString *baselineHash = [NSString stringWithContentsOfFile:baselineHashPath encoding:NSUTF8StringEncoding error:nil];
-            NSString *baselineImagePath = [baseTestPath stringByAppendingString:@"-expected.png"];
-            
             // grab a bitmap from the view
-            WebView *view = [frame webView];
-            NSSize webViewSize = [[frame webView] frame].size;
+            WebView* view = [frame webView];
+            NSSize webViewSize = [view frame].size;
+
             CGContextRef cgContext = CGBitmapContextCreate(screenCaptureBuffer, webViewSize.width, webViewSize.height, 8, webViewSize.width * 4, sharedColorSpace, kCGBitmapByteOrder32Host | kCGImageAlphaPremultipliedLast);
-            NSGraphicsContext *nsContext = [NSGraphicsContext graphicsContextWithGraphicsPort:cgContext flipped:NO];
-            [NSGraphicsContext saveGraphicsState];
+
+            NSGraphicsContext* savedContext = [[[NSGraphicsContext currentContext] retain] autorelease];
+            NSGraphicsContext* nsContext = [NSGraphicsContext graphicsContextWithGraphicsPort:cgContext flipped:NO];
             [NSGraphicsContext setCurrentContext:nsContext];
+
             if (!testRepaint)
                 [view displayRectIgnoringOpacity:NSMakeRect(0, 0, webViewSize.width, webViewSize.height) inContext:nsContext];
             else if (!repaintSweepHorizontally) {
@@ -397,16 +400,35 @@ static void dump(void)
                     [NSBezierPath strokeRect:[documentView convertRect:[(id <WebDocumentSelection>)documentView selectionRect] fromView:nil]];
                 }
             }
-            [NSGraphicsContext restoreGraphicsState];
+
+            [NSGraphicsContext setCurrentContext:savedContext];
             
-            // has the actual hash to compare to the expected image's hash
             CGImageRef bitmapImage = CGBitmapContextCreateImage(cgContext);
+            CGContextRelease(cgContext);
+
+            // compute the actual hash to compare to the expected image's hash
             NSString *actualHash = md5HashStringForBitmap(bitmapImage);
             printf("\nActualHash: %s\n", [actualHash UTF8String]);
-            printf("BaselineHash: %s\n", [baselineHash UTF8String]);
+
+            BOOL dumpImage;
+            if (dumpAllPixels)
+                dumpImage = YES;
+            else {
+                // FIXME: It's unfortunate that we hardcode the file naming scheme here.
+                // At one time, the perl script had all the knowledge about file layout.
+                // Some day we should restore that setup by passing in more parameters to this tool.
+                NSString *baseTestPath = [currentTest stringByDeletingPathExtension];
+                NSString *baselineHashPath = [baseTestPath stringByAppendingString:@"-expected.checksum"];
+                NSString *baselineHash = [NSString stringWithContentsOfFile:baselineHashPath encoding:NSUTF8StringEncoding error:nil];
+                NSString *baselineImagePath = [baseTestPath stringByAppendingString:@"-expected.png"];
+
+                printf("BaselineHash: %s\n", [baselineHash UTF8String]);
+
+                /// send the image to stdout if the hash mismatches or if there's no file in the file system
+                dumpImage = ![baselineHash isEqualToString:actualHash] || access([baselineImagePath fileSystemRepresentation], F_OK) != 0;
+            }
             
-            // if the hashes don't match, send image back to stdout for diff comparision
-            if (![baselineHash isEqualToString:actualHash] || access([baselineImagePath fileSystemRepresentation], F_OK) != 0) {
+            if (dumpImage) {
                 CFMutableDataRef imageData = CFDataCreateMutable(0, 0);
                 CGImageDestinationRef imageDest = CGImageDestinationCreateWithData(imageData, CFSTR("public.png"), 1, 0);
                 CGImageDestinationAddImage(imageDest, bitmapImage, 0);
@@ -416,8 +438,8 @@ static void dump(void)
                 fwrite(CFDataGetBytePtr(imageData), 1, CFDataGetLength(imageData), stdout);
                 CFRelease(imageData);
             }
+
             CGImageRelease(bitmapImage);
-            CGContextRelease(cgContext);
         }
 
         printf("#EOF\n");
--- third_party/WebKit/WebKitTools/DumpRenderTree/EventSendingController.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/EventSendingController.m
@@ -28,7 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #import "EventSendingController.h"
+
 #import "DumpRenderTree.h"
+#import "DumpRenderTreeDraggingInfo.h"
 
 #import <Carbon/Carbon.h>                           // for GetCurrentEventTime()
 #import <WebKit/WebKit.h>
