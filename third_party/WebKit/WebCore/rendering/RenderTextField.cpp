1a08720e3ed91da236ed166021135e3bae70ae4b
tomernic <tomernic@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-03-31 20:58:33 +0000

    LayoutTests:
            Reviewed by Adele.
    
            Layout test for:
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=7858>
            <rdar://problem/4483359> REGRESSION: New text field doesn't recognize the read only attribute
    
            * fast/forms/input-appearance-readonly-expected.txt: Added.
            * fast/forms/input-appearance-readonly.html: Added.
    
    WebCore:
    
            Reviewed by Adele.
    
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=7858>
            <rdar://problem/4483359> REGRESSION: New text field doesn't recognize the read only attribute
            
            * rendering/RenderTextField.cpp:
            (WebCore::RenderTextField::createDivStyle):
            Set user modify based on the form element's readOnly().
            (WebCore::RenderTextField::updateFromElement):
            ditto
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTextField.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTextField.cpp
@@ -71,10 +71,14 @@ RenderStyle* RenderTextField::createDivStyle(RenderStyle* startStyle)
     divStyle->setDisplay(BLOCK);
     divStyle->setOverflow(OHIDDEN);
     divStyle->setWhiteSpace(NOWRAP);
-    divStyle->setUserModify(READ_WRITE_PLAINTEXT_ONLY);
+
+    if (element()->hasTagName(inputTag))
+        divStyle->setUserModify(static_cast<HTMLGenericFormElement *>(element())->readOnly() ? READ_ONLY : READ_WRITE_PLAINTEXT_ONLY);
+
     // We're adding this extra pixel of padding to match WinIE.
     divStyle->setPaddingLeft(Length(1, Fixed));
     divStyle->setPaddingRight(Length(1, Fixed));
+
     return divStyle;
 }
 
@@ -106,6 +110,8 @@ void RenderTextField::updateFromElement()
             // Add div to Render tree
             RenderBlock::addChild(divRenderer);
         }
+
+        m_div->renderer()->style()->setUserModify(static_cast<HTMLGenericFormElement *>(element())->readOnly() ? READ_ONLY : READ_WRITE_PLAINTEXT_ONLY);
         
         if (!input->valueMatchesRenderer()) {
             String oldText = this->text();
