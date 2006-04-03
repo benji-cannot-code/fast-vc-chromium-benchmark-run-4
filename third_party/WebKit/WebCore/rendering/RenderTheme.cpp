1f7e46bc5a52f0c06a4d2aa7f8fc11a4540aa7d2
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-03 21:58:47 +0000

    Win32 theme work. Bug 8162. r=anders
    git-svn-id: svn://svn.chromium.org/blink/trunk@13657 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTheme.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTheme.cpp
@@ -134,14 +134,13 @@ bool RenderTheme::isControlStyled(const RenderStyle* style, const BorderData& bo
     switch (style->appearance()) {
         case PushButtonAppearance:
         case SquareButtonAppearance:
-        case ButtonAppearance: {
+        case ButtonAppearance:
+        case TextFieldAppearance: {
             // Test the style to see if the UA border and background match.
             return (style->border() != border ||
                     *style->backgroundLayers() != background ||
                     style->backgroundColor() != backgroundColor);
         }
-        case TextFieldAppearance:
-            return style->border() != border;
         default:
             return false;
     }
@@ -157,7 +156,7 @@ bool RenderTheme::supportsFocusRing(const RenderStyle* style) const
 bool RenderTheme::stateChanged(RenderObject* o, ControlState state) const
 {
     // Default implementation assumes the controls dont respond to changes in :hover state
-    if (state == HoverState)
+    if (state == HoverState && !supportsHover(o->style()))
         return false;
         
     // Assume pressed state is only responded to if the control is enabled.
@@ -202,6 +201,20 @@ bool RenderTheme::isPressed(const RenderObject* o) const
     if (!o->element())
         return false;
     return o->element()->active();
+}
+
+bool RenderTheme::isReadOnly(const RenderObject* o) const
+{
+    if (!o->element())
+        return false;
+    return o->element()->isReadOnly();
+}
+
+bool RenderTheme::isHovered(const RenderObject* o) const
+{
+    if (!o->element())
+        return false;
+    return o->element()->hovered();
 }
 
 void RenderTheme::adjustCheckboxStyle(CSSStyleSelector* selector, RenderStyle* style, Element* e) const
