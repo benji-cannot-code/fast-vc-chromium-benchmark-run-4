408f88e763dc5e3aaa4449b1f0e3cacb447ec355
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-03 22:06:06 +0000

    Reviewed by Adele.
            Fixes <rdar://problem/4498338> JavaScriptCore fails to compile for ppc64
            Other 64 bit build fixes.
    
            * kjs/collector.cpp:
            (KJS::Collector::markOtherThreadConservatively): test for __DARWIN_UNIX03 and use __r1
            * kjs/dtoa.cpp:
            (Bigint::): cast PRIVATE_mem to unsigned to prevent warning
            * bindings/jni/jni_utility.cpp:
            (KJS::Bindings::getJavaVM): cast jniError to long to prevent format warning
            (KJS::Bindings::getJNIEnv): cast jniError to long to prevent format warning
            * bindings/runtime_root.cpp:
            (KJS::Bindings::addNativeReference): cast CFDictionaryGetValue to unsigned long to prevent warning
            (KJS::Bindings::removeNativeReference): cast CFDictionaryGetValue to unsigned long to prevent warning
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/runtime_root.cpp
+++ third_party/WebKit/JavaScriptCore/bindings/runtime_root.cpp
@@ -160,7 +160,7 @@ void KJS::Bindings::addNativeReference (const Bindings::RootObject *root, JSObje
     if (root) {
         CFMutableDictionaryRef referencesDictionary = getReferencesDictionary (root);
         
-        unsigned int numReferences = (unsigned int)CFDictionaryGetValue (referencesDictionary, imp);
+        unsigned long numReferences = (unsigned long)CFDictionaryGetValue (referencesDictionary, imp);
         if (numReferences == 0) {
             JSLock lock;
             gcProtect(imp);
@@ -180,7 +180,7 @@ void KJS::Bindings::removeNativeReference (JSObject *imp)
     CFMutableDictionaryRef referencesDictionary = findReferenceDictionary (imp);
 
     if (referencesDictionary) {
-        unsigned int numReferences = (unsigned int)CFDictionaryGetValue (referencesDictionary, imp);
+        unsigned long numReferences = (unsigned long)CFDictionaryGetValue (referencesDictionary, imp);
         if (numReferences == 1) {
             JSLock lock;
             gcUnprotect(imp);
