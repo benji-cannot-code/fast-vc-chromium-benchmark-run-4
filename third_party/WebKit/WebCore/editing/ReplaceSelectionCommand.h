46d878edb29b6a7c1a53f95a884c57cab0daf317
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-05 21:53:23 +0000

    LayoutTests:
            Reviewed by darin
            
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=8198>
            Hitting an assert on undo paste
    
            * editing/pasteboard/undoable-fragment-removes-expected.checksum: Added.
            * editing/pasteboard/undoable-fragment-removes-expected.png: Added.
            * editing/pasteboard/undoable-fragment-removes-expected.txt: Added.
            * editing/pasteboard/undoable-fragment-removes.html: Added.
    
    WebCore:
    
            Reviewed by darin
            
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=8198>
            Hitting an assert on undo paste
            
            ReplaceSelectionCommand was doing a combination of undoable and non-undoable 
            removes from the ReplacementFragment.  On Undo Paste, the undoable removes 
            couldn't be undone because the tree was in a different state than it was
            at the time of the remove.  This patch makes all the removes from the fragment 
            non-undoable.  We could make them all undoable, but I can't think of any reason 
            why we'd want the fragment to be reconstructed on an Undo Paste.
    
            * editing/AppendNodeCommand.cpp:
            (WebCore::AppendNodeCommand::doApply):
            Assert that the node to append isn't already in a tree, since if it is, it will 
            be removed in a non-undoable way.
            * editing/InsertNodeBeforeCommand.cpp:
            (WebCore::InsertNodeBeforeCommand::doApply): Ditto.
            * editing/ReplaceSelectionCommand.cpp:
            (WebCore::ReplaceSelectionCommand::doApply): 
            Nodes were being moved from the fragment to the document with undoable inserts.  
            Undoable inserts implicitly remove the node (in a non-undoable way) from its 
            old location if it is already in a tree.  I now explicitly remove the nodes 
            from the fragment before inserting them into the document to make it clear that 
            they are being removed in a non-non-undoable way.  I also changed the one undoable 
            remove from the fragment to a non-undoable remove.
            * editing/ReplaceSelectionCommand.h: 
            Made ReplacementFragment's non-undoable removeNode public.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13705 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/ReplaceSelectionCommand.h
+++ third_party/WebKit/WebCore/editing/ReplaceSelectionCommand.h
@@ -78,6 +78,8 @@ public:
     bool hasInterchangeNewlineAtEnd() const { return m_hasInterchangeNewlineAtEnd; }
     
     bool isBlockFlow(Node*) const;
+    
+    void removeNode(PassRefPtr<Node>);
 
 private:
     // no copy construction or assignment
@@ -95,9 +97,7 @@ private:
     int renderedBlocks(Node*);
     void removeStyleNodes();
 
-    // A couple simple DOM helpers
     Node *enclosingBlock(Node *) const;
-    void removeNode(PassRefPtr<Node>);
     void removeNodePreservingChildren(Node *);
     void insertNodeBefore(Node *node, Node *refNode);
 
