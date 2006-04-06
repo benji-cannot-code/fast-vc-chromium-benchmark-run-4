35131f7efc4795980df5944dcc3054efc3f64179
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-06 06:01:20 +0000

    - fixed a storage leak from that last check-in
            * WebView/WebUnarchivingState.m: (-[WebUnarchivingState dealloc]):
            Release the two dictionaries.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13716 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebUnarchivingState.m
+++ third_party/WebKit/WebKit/WebView/WebUnarchivingState.m
@@ -47,6 +47,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return self;
 }
 
+- (void)dealloc
+{
+    [archivedSubframes release];
+    [archivedResources release];
+    [super dealloc];
+}
+
 - (void)addArchive:(WebArchive *)archive
 {
     NSEnumerator *enumerator = [[archive subresources] objectEnumerator];
