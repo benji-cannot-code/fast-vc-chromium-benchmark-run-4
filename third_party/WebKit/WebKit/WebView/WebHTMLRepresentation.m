eb11d80f129da6d722901b4d811d6038ee5c96ac
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-07 23:50:17 +0000

    Reviewed by Darin.
            - simplify archive loading
    
            * WebView/WebDataSource.m:
            (-[WebDataSource representation]): fix whitespace
            * WebView/WebHTMLRepresentation.m:
            (-[WebHTMLRepresentation loadArchive]): Use WebFrame's loadArchive: instead
            of trying to do a manual load here.
            (-[WebHTMLRepresentation documentSource]): No more special case for WebArchive.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebHTMLRepresentation.m
+++ third_party/WebKit/WebKit/WebView/WebHTMLRepresentation.m
@@ -129,28 +129,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)loadArchive
 {
     WebArchive *archive = [[WebArchive alloc] initWithData:[_private->dataSource data]];
-    WebResource *mainResource = [archive mainResource];
-    if (!mainResource) {
-        [archive release];
-        return;
-    }
-    
-    NSData *data = [mainResource data];
-    [data retain];
-    [_private->parsedArchiveData release];
-    _private->parsedArchiveData = data;
-    
-    [_private->dataSource _addToUnarchiveState:archive];
-    [archive release];
-
-    [_private->bridge closeURL];
-    [_private->bridge openURL:[mainResource URL]
-                       reload:NO 
-                  contentType:[mainResource MIMEType]
-                      refresh:NO
-                 lastModified:nil
-                    pageCache:nil];
-    [_private->bridge receivedData:data textEncodingName:[mainResource textEncodingName]];
+    [[_private->dataSource webFrame] loadArchive:archive];
 }
 
 - (void)finishedLoadingWithDataSource:(WebDataSource *)dataSource
@@ -180,11 +159,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSString *)documentSource
 {
-    if ([self _isDisplayingWebArchive]) {
-        return [[[NSString alloc] initWithData:_private->parsedArchiveData encoding:NSUTF8StringEncoding] autorelease];
-    } else {
-        return [WebFrameBridge stringWithData:[_private->dataSource data] textEncoding:[_private->bridge textEncoding]];
-    }
+    NSData *data = [_private->dataSource data];
+    CFStringEncoding encoding = [_private->bridge textEncoding];
+    return [WebFrameBridge stringWithData:data textEncoding:encoding];
 }
 
 - (NSString *)title
