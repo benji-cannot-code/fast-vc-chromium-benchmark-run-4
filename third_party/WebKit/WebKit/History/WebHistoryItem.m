f7dbc00ed80861cf7aefaae4709adc1a5e5715e0
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-11 04:17:43 +0000

    Reviewed by Tim Omernick.
            - fixed <rdar://problem/4139799> Seed: Safari: Private Browsing leaves traces in Icon Cache
            
            * Misc/WebIconDatabasePrivate.h:
            new ivars: pageURLsBoundDuringPrivateBrowsing, iconURLsBoundDuringPrivateBrowsing, and privateBrowsingEnabled
    
            * Misc/WebIconDatabase.m:
            (-[NSMutableDictionary init]):
            initialize new ivars, and listen for notifications that WebPreferences changed so we can react to changes
            to private browsing.
            (-[NSMutableDictionary iconForURL:withSize:cache:]):
            Don't remove icon URL from extraRetain dictionary; that's now done in _forgetIconForIconURLString. (I left a
            comment here earlier about why I was worried about this change, but I convinced myself that it's fine.)
            (-[WebIconDatabase removeAllIcons]):
            Removed no-longer-true (and never very clear) comment, and braces. Also remove all objects from the two
            private-browsing-related dictionaries.
            (-[WebIconDatabase _setIcon:forIconURL:]):
            remember icon URL if private browsing is enabled
            (-[WebIconDatabase _setHaveNoIconForIconURL:]):
            remember icon URL if private browsing is enabled
            (-[WebIconDatabase _setIconURL:forURL:]):
            added an assert that helped me out at one point
            (-[WebIconDatabase _clearDictionaries]):
            clear the two new dictionaries too
            (-[WebIconDatabase _loadIconDictionaries]):
            made an existing ERROR not fire in the expected case where there are no icons at all on disk
            (-[WebIconDatabase _updateFileDatabase]):
            when saving the pageURLToIconURL dictionary to disk, first remove any values that were created during
            private browsing
            (-[WebIconDatabase _retainIconForIconURLString:]):
            skip the code that deals with saving changes to disk if private browsing is enabled
            (-[WebIconDatabase _forgetIconForIconURLString:]):
            Remove the icon URL from extraRetain dictionary here. We're forgetting everything about this icon URL
            so we should forget its former extraRetain count too.
            (-[WebIconDatabase _resetCachedWebPreferences:]):
            Cache the new value of private browsing. If it has now been turned off, forget everything we learned
            while it was on. This causes (e.g.) icons for bookmarks or pre-existing history items to be forgotten
            if the icon was only learned during private browsing.
    
            * History/WebHistoryItem.m:
            removed an unnecessary #import I happened to notice
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13817 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History/WebHistoryItem.m
+++ third_party/WebKit/WebKit/History/WebHistoryItem.m
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebFrameView.h>
 #import <WebKit/WebHTMLViewPrivate.h>
 #import <WebKit/WebIconDatabase.h>
-#import <WebKit/WebIconLoader.h>
 #import <WebKit/WebKitLogging.h>
 #import <WebKit/WebKitNSStringExtras.h>
 #import <WebKit/WebNSDictionaryExtras.h>
--- third_party/WebKit/WebKit/Misc/WebIconDatabasePrivate.h
+++ third_party/WebKit/WebKit/Misc/WebIconDatabasePrivate.h
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     NSMutableDictionary *iconURLToIcons;
     NSMutableDictionary *iconURLToPageURLs;
-    NSMutableDictionary *pageURLToIconURL;    
+    NSMutableDictionary *pageURLToIconURL;
     CFMutableDictionaryRef pageURLToRetainCount;
     CFMutableDictionaryRef iconURLToExtraRetainCount;
     
@@ -48,11 +48,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSMutableSet *iconsToSaveWithURLs;
     NSMutableSet *iconURLsWithNoIcons;
     NSMutableSet *originalIconsOnDiskWithURLs;
+    NSMutableSet *pageURLsBoundDuringPrivateBrowsing;
+    NSMutableSet *iconURLsBoundDuringPrivateBrowsing;
     
     int cleanupCount;
 
     BOOL didCleanup;
     BOOL waitingToCleanup;
+    BOOL privateBrowsingEnabled;
 
     NSMutableDictionary *htmlIcons;
     NSMutableDictionary *defaultIcons;
