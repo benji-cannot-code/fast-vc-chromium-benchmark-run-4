9af6e17f6cc51473602970e00c79406a4bbd471f
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-12 16:08:14 +0000

    - another attempt to get things building
            * bindings/js/kjs_proxy.cpp: Add "kjs_events.h" include, needed when not
            building SVG (so for Windows too).
            * editing/TextIterator.h: Add back include of Vector.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13834 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/kjs_proxy.cpp
+++ third_party/WebKit/WebCore/bindings/js/kjs_proxy.cpp
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "kjs_proxy.h"
 
+#include "kjs_events.h"
 #include "kjs_window.h"
 #include "Frame.h"
 
