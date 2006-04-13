2771d6a609158322ff18c1000340336c2296aa1f
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-13 17:03:16 +0000

    WebCore:         Reviewed by Darin.
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=7602
            Only use fixupChar for entities
    
            * html/HTMLTokenizer.cpp: Only use fixUpChar() when handling entities.
            (WebCore::HTMLTokenizer::parseSpecial):
            (WebCore::HTMLTokenizer::parseText):
            (WebCore::HTMLTokenizer::parseTag):
            (WebCore::HTMLTokenizer::write):
            * platform/StreamingTextDecoder.cpp:
            (WebCore::StreamingTextDecoder::convert): Remove the special case for Latin-1, because it is already handled 
            via effectiveEncoding().
            * platform/StreamingTextDecoder.h: Remove convertLatin1().
    
    LayoutTests:
            Tests that Latin-1 is handled as windows-1252
            (see http://bugzilla.opendarwin.org/show_bug.cgi?id=7602). Some existing tests
            already did cover this, but only indirectly.
    
            * fast/encoding/latin1-winlatin-expected.txt: Added.
            * fast/encoding/latin1-winlatin.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
+++ third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
               (C) 1999 Antti Koivisto (koivisto@kde.org)
               (C) 2001 Dirk Mueller (mueller@kde.org)
     Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.
+    Copyright (C) 2005, 2006 Alexey Proskuryakov (ap@nypop.com)
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -82,11 +83,7 @@ static const char titleEnd [] = "</title";
 //
 // There may be better equivalents
 
-// We need this for entities at least. For non-entity text, we could
-// handle this in the text encoding.
-
-// To cover non-entity text, I think this function would need to be called
-// in more places. There seem to be some places that don't call fixUpChar.
+// We only need this for entities. For non-entity text, we handle this in the text encoding.
 
 static const unsigned short windowsLatin1ExtensionArray[32] = {
     0x20AC, 0x0081, 0x201A, 0x0192, 0x201E, 0x2026, 0x2020, 0x2021, // 80-87
@@ -329,7 +326,7 @@ HTMLTokenizer::State HTMLTokenizer::parseSpecial(SegmentedString &src, State sta
             scriptCodeSize = scriptCodeDest-scriptCode;
         }
         else {
-            scriptCode[scriptCodeSize++] = fixUpChar(*src);
+            scriptCode[scriptCodeSize++] = *src;
             ++src;
         }
     }
@@ -639,7 +636,7 @@ HTMLTokenizer::State HTMLTokenizer::parseText(SegmentedString &src, State state)
             state.setSkipLF(true);
             *dest++ = '\n';
         } else
-            *dest++ = fixUpChar(cc);
+            *dest++ = cc;
         ++src;
     }
 
@@ -1063,7 +1060,7 @@ HTMLTokenizer::State HTMLTokenizer::parseTag(SegmentedString &src, State state)
                         break;
                     }
                 }
-                *dest++ = fixUpChar(*src);
+                *dest++ = *src;
                 ++src;
             }
             break;
@@ -1094,7 +1091,7 @@ HTMLTokenizer::State HTMLTokenizer::parseTag(SegmentedString &src, State state)
                     }
                 }
 
-                *dest++ = fixUpChar(*src);
+                *dest++ = *src;
                 ++src;
             }
             break;
@@ -1456,7 +1453,7 @@ bool HTMLTokenizer::write(const SegmentedString &str, bool appendData)
             ++src;
         } else {
             state.setDiscardLF(false);
-            *dest++ = fixUpChar(*src);
+            *dest++ = *src;
             ++src;
         }
     }
