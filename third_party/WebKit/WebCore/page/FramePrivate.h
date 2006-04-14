d9c80c11a62a3add176310c0091db8e1cea7a9d8
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-14 22:01:59 +0000

    2006-04-14  Eric Seidel  <eseidel@apple.com>
            Reviewed by beth.
    
            Fix win32 build.
    
            * WebCore.vcproj/WebCore/WebCore.vcproj:
            * page/FramePrivate.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13873 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FramePrivate.h
+++ third_party/WebKit/WebCore/page/FramePrivate.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef khtmlpart_p_h
 #define khtmlpart_p_h
 
+#include "DOMWindow.h"
 #include "EditCommand.h"
 #include "Frame.h"
 #include "FrameTree.h"
