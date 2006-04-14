26e4ab4b30f01ba9ba8cd156d25cbf019b512569
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-14 01:17:21 +0000

    Reviewed by Geoff.
            - moved a few things out of kwq and cleaned up the
              Java-applet-related renderers
    
            * WebCore.vcproj/WebCore/WebCore.vcproj: Updated for file location
            and name changes.
            * WebCore.xcodeproj/project.pbxproj: Ditto.
    
            * kwq/JavaAppletWidget.h: Moved.
            * kwq/JavaAppletWidget.mm: Moved.
            * kwq/RegularExpression.cpp: Moved.
            * kwq/RegularExpression.h: Moved.
            * kwq/RenderTreeAsText.cpp: Moved.
            * kwq/RenderTreeAsText.h: Moved.
            * rendering/render_applet.cpp: Moved.
            * rendering/render_applet.h: Moved.
    
            * bridge/JavaAppletWidget.h: Moved here.
            * bridge/mac/JavaAppletWidget.mm: Moved here.
            * platform/RegularExpression.cpp: Moved here.
            * platform/RegularExpression.h: Moved here.
            * rendering/RenderTreeAsText.cpp: Moved here.
            * rendering/RenderTreeAsText.h: Moved here.
    
            * rendering/RenderApplet.h: Moved here and made changes.
            Removed unused element() function.
            * rendering/RenderApplet.cpp: Moved here and made changes.
            (WebCore::RenderApplet::RenderApplet): Changed parameter type to
            be more precise (HTMLAppletElement).
            (WebCore::RenderApplet::intrinsicWidth): Removed unnecessary type
            cast and simplified.
            (WebCore::RenderApplet::intrinsicHeight): Ditto.
            (WebCore::RenderApplet::createWidgetIfNecessary): Straightened out
            the if statements and changed to use node() instead of element().
            (WebCore::RenderApplet::layout): Removed unneeded check before
            calling createWidgetIfNecessary.
    
            * rendering/RenderEmptyApplet.h: Moved here and made changes.
            Removed unneeded overrides of intrinsicWidth and intrinsicHeight.
            * rendering/RenderEmptyApplet.cpp: Moved here and made changes.
            (WebCore::RenderEmptyApplet::RenderEmptyApplet): Added code to
            set the intrinsic width and height.
    
            * rendering/render_replaced.cpp:
            (WebCore::RenderWidget::RenderWidget): Initialize m_widget
            with contructor syntax.
            (WebCore::RenderWidget::paint): Changed _tx and _ty to be just
            tx and ty. Rearranged the code so the transparent wash will draw
            even if m_widget is 0.
    
            * html/html_objectimpl.cpp: Update includes for new file names.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderApplet.h
+++ third_party/WebKit/WebCore/rendering/RenderApplet.h
@@ -22,48 +22,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
-#ifndef render_applet_h
-#define render_applet_h
+#ifndef RenderApplet_h
+#define RenderApplet_h
 
-#include "HTMLElement.h"
 #include "StringHash.h"
 #include "render_replaced.h"
 
 namespace WebCore {
 
-class RenderApplet : public RenderWidget
-{
-public:
-    RenderApplet(HTMLElement*, const HashMap<String, String>& args);
-    virtual ~RenderApplet();
+    class HTMLAppletElement;
 
-    virtual const char* renderName() const { return "RenderApplet"; }
+    class RenderApplet : public RenderWidget {
+    public:
+        RenderApplet(HTMLAppletElement*, const HashMap<String, String>& args);
+        virtual ~RenderApplet();
+        virtual const char* renderName() const { return "RenderApplet"; }
+        virtual bool isApplet() const { return true; }
+        virtual void layout();
+        virtual int intrinsicWidth() const;
+        virtual int intrinsicHeight() const;
 
-    void createWidgetIfNecessary();
+        void createWidgetIfNecessary();
 
-    virtual void layout();
-    virtual int intrinsicWidth() const;
-    virtual int intrinsicHeight() const;
-    virtual bool isApplet() const { return true; }
-
-    HTMLElement *element() const
-        { return static_cast<HTMLElement*>(RenderObject::element()); }
-
-private:
-    HashMap<String, String> m_args;
-};
-
-class RenderEmptyApplet : public RenderWidget
-{
-public:
-    RenderEmptyApplet(Node*);
-
-    virtual const char* renderName() const { return "RenderEmptyApplet"; }
-
-    virtual int intrinsicWidth() const;
-    virtual int intrinsicHeight() const;
-    virtual void layout();
-};
+    private:
+        HashMap<String, String> m_args;
+    };
 
 }
 
