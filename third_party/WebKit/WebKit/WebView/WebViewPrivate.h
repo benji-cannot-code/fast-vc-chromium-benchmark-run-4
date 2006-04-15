55757d912d83b4a596d3079d2da94f4007156df7
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-15 05:38:19 +0000

    JavaScriptCore:
            Reviewed by Timothy.
    
            Bug 8389: support for Cocoa bindings - binding an NSTreeController to the WebView's DOM
            http://bugzilla.opendarwin.org/show_bug.cgi?id=8389
    
            Adds a category to WebScriptObject with array accessors for KVC/KVO.
    
            If super valueForKey: fails it will call valueForUndefinedKey:, which is
            important because it causes the right behavior to happen with bindings using
            the "Raises for Not Applicable Keys" flag and the "Not Applicable Placeholder"
    
            * bindings/objc/WebScriptObject.mm:
            (-[WebScriptObject valueForKey:]):
            (-[WebScriptObject count]):
            (-[WebScriptObject objectAtIndex:]):
            (-[WebUndefined description]): return "undefined"
    
    WebKit:
    
            Reviewed by Timothy.
    
            Bug 8389: support for Cocoa bindings - binding an NSTreeController to the WebView's DOM
            http://bugzilla.opendarwin.org/show_bug.cgi?id=8389
    
            Added a controller class, WebController, that is a subclass of
            NSTreeController that has a new outlet/binding for the WebView.
    
            * WebCoreSupport/WebFrameBridge.m:
            (-[WebFrameBridge windowObjectCleared]): notify bindings about the document change
            * WebView/WebView.m:
            (-[WebView _progressStarted:]): notify bindings about the document change
            (-[WebView _finalProgressComplete]): notify bindings about the document change
            (-[WebView _declaredKeys]): added a key for the main frame document
            (-[WebController init]):
            (-[WebController exposedBindings]):
            (-[WebController valueClassForBinding:]):
            (-[WebController setContent:]):
            (-[WebController webView]):
            (-[WebController setWebView:]):
            (-[WebView mainFrameDocument]): get the main frame's DOMDocument
            * WebView/WebViewPrivate.h: Adds mainFrameDocument to pending public.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebViewPrivate.h
+++ third_party/WebKit/WebKit/WebView/WebViewPrivate.h
@@ -65,6 +65,7 @@ extern NSString *_WebIsLoadingKey;
 extern NSString *_WebMainFrameIconKey;
 extern NSString *_WebMainFrameTitleKey;
 extern NSString *_WebMainFrameURLKey;
+extern NSString *_WebMainFrameDocumentKey;
 
 extern NSString *WebElementTitleKey;   // NSString of the title of the element (pending public, used by Safari)
 
@@ -75,6 +76,12 @@ typedef enum {
     WebDashboardBehaviorAllowWheelScrolling
 } WebDashboardBehavior;
 
+@interface WebController : NSTreeController {
+    IBOutlet WebView *webView;
+}
+- (WebView *)webView;
+- (void)setWebView:(WebView *)newWebView;
+@end
 
 @interface WebView (WebPendingPublic)
 
@@ -83,6 +90,7 @@ typedef enum {
 - (BOOL)isLoading;
 - (NSString *)mainFrameTitle;
 - (NSImage *)mainFrameIcon;
+- (DOMDocument *)mainFrameDocument;
 
 - (void)setDrawsBackground:(BOOL)drawsBackround;
 - (BOOL)drawsBackground;
