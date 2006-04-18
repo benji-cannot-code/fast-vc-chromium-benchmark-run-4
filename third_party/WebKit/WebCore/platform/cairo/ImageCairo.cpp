ccc02b4e6d6c01f7ddc685acc2019a42eee60dfa
bdakin <bdakin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-18 23:10:20 +0000

    Reviewed by Eric.
            Build fix for Windows. Just a few typos from background-size patch.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13961 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/cairo/ImageCairo.cpp
+++ third_party/WebKit/WebCore/platform/cairo/ImageCairo.cpp
@@ -144,7 +144,7 @@ void Image::tileInRect(const FloatRect& dstRect, const FloatPoint& srcPoint, con
         return;
 
     IntSize intrinsicImageSize = size();                       
-    FloatRect srcRect(srcPoint, selfSize);
+    FloatRect srcRect(srcPoint, intrinsicImageSize);
     FloatPoint point = srcPoint;
 
     // Check and see if a single draw of the image can cover the entire area we are supposed to tile.
@@ -156,7 +156,7 @@ void Image::tileInRect(const FloatRect& dstRect, const FloatPoint& srcPoint, con
     // scaled tile. 
     float scaleX = 1.0;
     float scaleY = 1.0;
-    cairo_matrix_t* mat;
+    cairo_matrix_t mat;
     cairo_matrix_init_identity(&mat);
     if (tileSize.width() != intrinsicImageSize.width() || tileSize.height() != intrinsicImageSize.height()) {
         scaleX = intrinsicImageSize.width() / tileSize.width();
