050e65ef9351e1ab1b0a8a9ec126d82b43ed8fd1
jdevalk <jdevalk@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-18 21:56:31 +0000

    Added webkit detection scripts and info on how to use them.
    git-svn-id: svn://svn.chromium.org/blink/trunk@13958 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKitSite/webdevelopers/webkit_version.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function parse_webkit_version(version) {
+  var bits = version.split(".");
+  var is_nightly = (version[version.length - 1] == "+");
+  if (is_nightly) {
+    var minor = "+";
+  } else {
+    var minor = parseInt(bits[1]);
+    // If minor is Not a Number (NaN) return an empty string
+    if (isNaN(minor)) {
+      minor = "";
+    }
+  }
+  return {major: parseInt(bits[0]), minor: minor, is_nightly: is_nightly};
+}
+
+function get_webkit_version() {
+  var regex = new RegExp("\\(.*\\) AppleWebKit/(.*) \\((.*)");
+  var matches = regex.exec(navigator.userAgent);
+  if (matches) {
+    var webkit_version = parse_webkit_version(matches[1]);    
+  } 
+  return {major: webkit_version['major'], minor: webkit_version['minor'], is_nightly: webkit_version['is_nightly']};
+}  
