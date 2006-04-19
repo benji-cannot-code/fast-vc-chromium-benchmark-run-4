ffba78ef1daa29482c132ada575e9e611ff27ff2
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-19 11:03:52 +0000

    2006-04-18  Rob Buis  <buis@kde.org>
            Reviewed by eseidel & darin.  Landed by eseidel.
    
            No automated tests possible (from javascript).
    
            Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=6664:
            Inspector does not highlight SVG elements properly
    
            Make sure RenderObject::absoluteBoundingBoxRect works for
            svg specific render objects by overriding absoluteRects.
            This fixes highlighting in the Inspector of svg shapes, paths,
            images and text.
    
            * kcanvas/RenderPath.cpp:
            (WebCore::RenderPath::absoluteRects):
            * kcanvas/RenderPath.h:
            * kcanvas/RenderSVGImage.cpp:
            (WebCore::RenderSVGImage::getAbsoluteRepaintRect):
            (WebCore::RenderSVGImage::absoluteRects):
            * kcanvas/RenderSVGImage.h:
            * kcanvas/RenderSVGText.cpp:
            (WebCore::RenderSVGText::absoluteRects):
            * kcanvas/RenderSVGText.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13968 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kcanvas/RenderPath.cpp
+++ third_party/WebKit/WebCore/kcanvas/RenderPath.cpp
@@ -243,6 +243,11 @@ void RenderPath::paint(PaintInfo &paintInfo, int parentX, int parentY)
         paintInfo.p->restore();
 }
 
+void RenderPath::absoluteRects(DeprecatedValueList<IntRect>& rects, int _tx, int _ty)
+{
+    rects.append(getAbsoluteRepaintRect());
+}
+
 bool RenderPath::nodeAtPoint(NodeInfo& info, int _x, int _y, int _tx, int _ty, HitTestAction hitTestAction)
 {
     // We only draw in the forground phase, so we only hit-test then.
