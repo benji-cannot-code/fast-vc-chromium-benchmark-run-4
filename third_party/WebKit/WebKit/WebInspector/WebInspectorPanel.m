6308a0604e8ab12c5a0b5881599b6ee98d2f9962
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-20 01:44:35 +0000

    Reviewed by Timothy.
            http://bugzilla.opendarwin.org/show_bug.cgi?id=6637
            Bug 6637: selecting node in Inspector after closing window crashes Safari
    
            When the window the Web Inspector was inspecting is closed, the inspector goes into its
            no-selection state. Choosing to inspect another element activates it again. The inspector
            will also follow the WebView if the URL changes and select the root element on the new page.
    
            * WebInspector/WebInspector.m:
            (-[NSWindow setWebFrame:]):
            (-[NSWindow setRootDOMNode:]):
            (-[WebInspector _revealAndSelectNodeInTree:]):
            (-[WebInspector _update]):
            (-[WebInspector _updateRoot]):
            (-[WebInspector inspectedWebViewProgressFinished:]):
            (-[WebInspector inspectedWindowWillClose:]):
            (-[WebInspector webView:didFinishLoadForFrame:]):
            * WebInspector/WebInspectorPanel.m:
            (-[WebInspectorPanel canBecomeMainWindow]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13976 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebInspector/WebInspectorPanel.m
+++ third_party/WebKit/WebKit/WebInspector/WebInspectorPanel.m
@@ -44,7 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (BOOL)canBecomeMainWindow
 {
-    return YES;
+    // prevent the inspector from confusing other panels
+    return NO;
 }
 
 - (void)moveWindow:(NSEvent *)event
