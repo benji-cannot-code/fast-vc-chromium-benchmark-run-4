16176b31fc1903e7ae432ad439b31300fd8473c4
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-21 16:29:02 +0000

    LayoutTests:
            Tests for global constants related to CSSRule, CSSValue, Event, and
            NodeFilter objects.
    
            * fast/dom/constants-expected.txt: Added.
            * fast/dom/constants.html: Added.
            * fast/dom/prototype-chain-expected.txt:
            * fast/dom/resources/stylesheet.css: Added.
            * fast/dom/xmlhttprequest-get-expected.txt:
    
    WebCore:
    
            Reviewed by Maciej.
    
            - Added autogeneration of JS bindings for CSSRule, CSSValue,
            Event, and NodeFilter.  
    
            - Made related prototype objects hold the relevant constants, to
            match Mozilla and the DOM 2 spec. (Previously, only the related
            constructor objects held those constants, in accordance with the
            DOM 3 spec.)
    
            - Fixed up remaining Windows build issues.
    
            * DerivedSources.make: Added new autogenerated files
            * WebCore.xcodeproj/project.pbxproj: ditto
            * bindings/js/kjs_css.cpp:
            (KJS::DOMCSSRule::classInfo):
            (KJS::DOMCSSRule::getOwnPropertySlot): scope call to classInfo()
            because it's virtual and DOMCSSRule has a derrived class now.
            (KJS::DOMCSSRule::put): ditto
            (KJS::DOMCSSRuleFunc::callAsFunction):
            (KJS::toJS):
            * bindings/js/kjs_css.h:
            * bindings/js/kjs_events.cpp:
            (KJS::toJS):
            * bindings/js/kjs_events.h:
            * bindings/js/kjs_html.cpp:
            (KJS::OptionConstructorImp::OptionConstructorImp):
            * bindings/js/kjs_traversal.cpp:
            (KJS::toJS):
            * bindings/js/kjs_traversal.h:
            * bindings/js/kjs_window.cpp: 
            (KJS::Window::getValueProperty): added CSSValue global object
            * bindings/js/kjs_window.h:
            (KJS::Window::):
            * bindings/scripts/CodeGeneratorJS.pm: Changed generator to write
            constants to prototype objects (previously only wrote constants to
            constructor objects)
            * css/CSSPrimitiveValue.idl: Removed LegacyParent since CSSValue now
            exists in IDL
            * css/CSSRule.idl: Added.
            * css/CSSValue.idl: Added.
            * css/css_ruleimpl.h:
            (WebCore::CSSRule::):
            * css/css_valueimpl.h:
            * dom/Event.idl: Added.
            * dom/MutationEvent.idl: Removed LegacyParent since Event now exists
            in IDL
            * dom/NodeFilter.idl: Added.
            * dom/UIEvent.idl: Removed LegacyParent since event now exists in IDL
            * dom/dom2_eventsimpl.h:
            (WebCore::Event::):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@13997 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/kjs_traversal.h
+++ third_party/WebKit/WebCore/bindings/js/kjs_traversal.h
@@ -50,17 +50,6 @@ namespace KJS {
     RefPtr<WebCore::NodeIterator> m_impl;
   };
 
-  // Constructor object NodeFilter
-  class NodeFilterConstructor : public DOMObject {
-  public:
-    NodeFilterConstructor(ExecState*) { }
-    virtual bool getOwnPropertySlot(ExecState*, const Identifier&, PropertySlot&);
-    JSValue *getValueProperty(ExecState*, int token) const;
-    // no put - all read-only
-    virtual const ClassInfo* classInfo() const { return &info; }
-    static const ClassInfo info;
-  };
-
   class DOMNodeFilter : public DOMObject {
   public:
     DOMNodeFilter(ExecState*, WebCore::NodeFilter*);
@@ -94,7 +83,6 @@ namespace KJS {
   };
 
   JSValue* toJS(ExecState*, WebCore::NodeIterator*);
-  JSValue* getNodeFilterConstructor(ExecState*);
   JSValue* toJS(ExecState*, WebCore::NodeFilter*);
   JSValue* toJS(ExecState*, WebCore::TreeWalker*);
 
