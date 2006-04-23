6bddd5d0f0683fdefb7a74fe00ba3ed53a31e564
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-23 19:16:26 +0000

    Reviewed by Darin.
            - http://bugzilla.opendarwin.org/show_bug.cgi?id=6905
              DumpRenderTree needs a way to force painting (to allow invalidation tests)
    
            * DumpRenderTree/DumpRenderTree.m:
            (dump): If display() has been called during the test, grab the pixels from
            the view, after letting it repaint invalidated rects.
            (+[LayoutTestController isSelectorExcludedFromWebScript:]):
            (-[LayoutTestController display]): Added. Calls -display on the view
            and changes the subsequent behavior of dump().
            (dumpRenderTree):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14024 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -87,6 +87,7 @@ static BOOL dumpSelectionRect;
 static BOOL dumpTitleChanges;
 static int dumpPixels = NO;
 static int dumpAllPixels = NO;
+static BOOL readFromWindow = NO;
 static int testRepaintDefault = NO;
 static BOOL testRepaint = NO;
 static int repaintSweepHorizontallyDefault = NO;
@@ -398,7 +399,15 @@ static void dump(void)
             NSGraphicsContext* nsContext = [NSGraphicsContext graphicsContextWithGraphicsPort:cgContext flipped:NO];
             [NSGraphicsContext setCurrentContext:nsContext];
 
-            if (!testRepaint)
+            if (readFromWindow) {
+                NSBitmapImageRep *imageRep;
+                [view displayIfNeeded];
+                [view lockFocus];
+                imageRep = [[NSBitmapImageRep alloc] initWithFocusedViewRect:[view frame]];
+                [view unlockFocus];
+                [imageRep draw];
+                [imageRep release];
+            } else if (!testRepaint)
                 [view displayRectIgnoringOpacity:NSMakeRect(0, 0, webViewSize.width, webViewSize.height) inContext:nsContext];
             else if (!repaintSweepHorizontally) {
                 NSRect line = NSMakeRect(0, 0, webViewSize.width, 1);
@@ -579,6 +588,7 @@ static void dump(void)
             || aSelector == @selector(setWindowIsKey:)
             || aSelector == @selector(setMainFrameIsFirstResponder:)
             || aSelector == @selector(dumpSelectionRect)
+            || aSelector == @selector(display)
             || aSelector == @selector(testRepaint)
             || aSelector == @selector(repaintSweepHorizontally))
         return NO;
@@ -640,6 +650,12 @@ static void dump(void)
         [(WebHTMLView *)documentView _updateFocusState];
 }
 
+- (void)display
+{
+    [[frame webView] display];
+    readFromWindow = YES;
+}
+
 - (void)testRepaint
 {
     testRepaint = YES;
@@ -683,6 +699,7 @@ static void dumpRenderTree(const char *pathOrURL)
     dumpAsText = NO;
     dumpSelectionRect = NO;
     dumpTitleChanges = NO;
+    readFromWindow = NO;
     testRepaint = testRepaintDefault;
     repaintSweepHorizontally = repaintSweepHorizontallyDefault;
     if (currentTest != nil)
