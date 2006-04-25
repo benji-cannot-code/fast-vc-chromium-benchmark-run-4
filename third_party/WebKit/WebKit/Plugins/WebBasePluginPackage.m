aeb0b437728af8ba2d887316c68d740aab5cd297
tomernic <tomernic@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-25 22:42:04 +0000

    Reviewed by Eric.
            <rdar://problem/4526120> -[WebBasePluginPackage finalize] leaks the CFBundle (under GC only)
    
            * Plugins/WebBasePluginPackage.m:
            (-[WebBasePluginPackage finalize]):
            Release the CFBundle here.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins/WebBasePluginPackage.m
+++ third_party/WebKit/WebKit/Plugins/WebBasePluginPackage.m
@@ -264,6 +264,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     ASSERT(!isLoaded);
 
+    if (cfBundle)
+        CFRelease(cfBundle);
+
     [super finalize];
 }
 
