9174a0744d053f65b0603d98bc0fdaafd49c56ca
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-25 08:11:15 +0000

    Reviewed by Eric.
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=8575
            New KWQFileButton leaks reported by buildbot
    
            * kwq/KWQFileButton.mm:
            (KWQFileButton::KWQFileButton): Add a missing release
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kwq/KWQFileButton.mm
+++ third_party/WebKit/WebCore/kwq/KWQFileButton.mm
@@ -389,6 +389,7 @@ KWQFileButton::KWQFileButton(Frame *frame)
 
     _buttonView = [[WebCoreFileButton alloc] initWithWidget:this];
     setView(_buttonView);
+    [_buttonView release];
 
     END_BLOCK_OBJC_EXCEPTIONS;
 }
