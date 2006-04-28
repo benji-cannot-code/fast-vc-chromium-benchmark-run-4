280e68b7a9a6527bf017b575ce4fbb95b1d8c296
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-28 16:02:45 +0000

    2006-04-28  Mitz Pettel  <opendarwin.org@mitzpettel.com>
            Reviewed by hyatt, landed by ap.
    
    WebCore:
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=6769
              REGRESSION: Incomplete repaint when a cell's extra bottom margin grows
    
            Test: fast/repaint/table-extra-bottom-grow.html
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::overflowRect): Changed to not add the top/left overflow twice and
            allow the normal overflow height to overlap with the bottom extra height.
            * rendering/RenderTableRow.cpp:
            (WebCore::RenderTableRow::layout): Removed the code that resets the extra heights.
            * rendering/RenderTableSection.cpp:
            (WebCore::RenderTableSection::layoutRows): If the top extra height changed or the
            bottom extra height increased, just repaint the entire cell.
    
    LayoutTests:
            * fast/repaint/table-extra-bottom-grow-expected.checksum: Added.
            * fast/repaint/table-extra-bottom-grow-expected.png: Added.
            * fast/repaint/table-extra-bottom-grow-expected.txt: Added.
            * fast/repaint/table-extra-bottom-grow.html: Added.
            * fast/repaint/table-cell-moved-expected.checksum: Removed excessive painting.
            * fast/repaint/table-cell-moved-expected.png: Ditto.
            * fast/repaint/bugzilla-6278-expected.checksum: Ditto.
            * fast/repaint/bugzilla-6278-expected.png: Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14101 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTableRow.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTableRow.cpp
@@ -112,8 +112,6 @@ void RenderTableRow::layout()
             if (child->needsLayout()) {
                 cell->calcVerticalMargins();
                 cell->layout();
-                cell->setCellTopExtra(0);
-                cell->setCellBottomExtra(0);
             }
         }
     }
