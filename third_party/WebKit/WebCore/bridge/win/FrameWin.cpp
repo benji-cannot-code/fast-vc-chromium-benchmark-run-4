b516364e6b2488299c064952c7403a3df9d1bbdc
sfalken <sfalken@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-04-29 00:12:18 +0000

    2006-04-28  Steve Falkenburg  <sfalken@apple.com>
            Reviewed by eric.
    
            No test necessary - fixing Win32 build, turned off C++ exceptions, turned off RTTI
    
            * WebCore.vcproj/WebCore/WebCore.vcproj:
            * bridge/win/FrameWin.cpp:
            (WebCore::FrameWin::~FrameWin):
            * bridge/win/PageWin.cpp:
            (WebCore::Page::Page):
            * page/Page.h:
            * platform/win/TemporaryLinkStubs.cpp:
            (FrameWin::saveDocumentState):
            (Path::contains):
            (Path::boundingRect):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14116 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/win/FrameWin.cpp
+++ third_party/WebKit/WebCore/bridge/win/FrameWin.cpp
@@ -58,6 +58,8 @@ FrameWin::FrameWin(Page* page, RenderPart* renderPart, FrameWinClient* client)
 
 FrameWin::~FrameWin()
 {
+    setView(0);
+    clearRecordedFormValues();    
 }
 
 void FrameWin::urlSelected(const ResourceRequest& request)
--- third_party/WebKit/WebCore/bridge/win/PageWin.cpp
+++ third_party/WebKit/WebCore/bridge/win/PageWin.cpp
@@ -33,6 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+Page::Page()
+    : m_frameCount(0)
+{
+    init();
+}
+
 static HWND rootWindowForFrame(const Frame* frame)
 {
     if (!frame)
