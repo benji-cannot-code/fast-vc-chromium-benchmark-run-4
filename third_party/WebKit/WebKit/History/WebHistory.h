6cd21d00155aed448ca518ce7940f1b667318bc9
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-01 15:17:41 +0000

    Reviewed by Tim Omernick.
            - fixed <rdar://problem/3126419> history load enforces history limit, but deletes the newest instead of oldest items
            - added notification reporting items discarded during load because the age limit or item count limit is exceeded
            - a few other minor tweaks
    
            * History/WebHistory.h:
            fixed a typo and an incorrect method name
            
            * History/WebHistoryPrivate.h:
            Added declaration of WebHistoryItemsDiscardedWhileLoadingNotification. Also changed signature of
            WebHistoryPrivate method -loadFromURL:error: to have new collectDiscardedItemsInto: parameter.
            Also deleted declarations of two methods that didn't actually exist (loadHistory and initWithFile:), 
            and added comments about which methods should become public API, WebKit-internal, or file-internal.
    
            * History/WebHistory.m:
            (-[WebHistoryPrivate arrayRepresentation]):
            This method, called only by _saveHistoryGuts:, used to deliberately leave out items that violated
            either the age limit or the item count limit. Now all the items are included (and thus saved), and
            all the pruning is done at load time, so clients can keep track of the pruned items by observing
            the new WebHistoryItemsDiscardedWhileLoadingNotification
            (-[WebHistoryPrivate _loadHistoryGutsFromURL:savedItemsCount:collectDiscardedItemsInto:error:]):
            Now keeps track of all the items that violated the age limit or item count limit in the new
            collectedDiscardedItemsInto: parameter. Also, now processes items in forward order rather than
            reverse order to fix 3126419. Now uses compare: rather than _webkit_compareDay: to check against
            age limit; this is faster and also more correct (most noticeable with small age limits).
            (-[WebHistoryPrivate loadFromURL:collectDiscardedItemsInto:error:]):
            new collectDiscardedItemsInto: parameter, passed into _loadHistoryGuts:...
            (-[WebHistory loadFromURL:error:]):
            Now sends new WebHistoryItemsDiscardedWhileLoadingNotification if any items were discarded due
            to age limit or item count limit.
            
            * WebKit.exp:
            exported symbol for WebHistoryItemsDiscardedWhileLoadingNotification
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14127 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History/WebHistory.h
+++ third_party/WebKit/WebKit/History/WebHistory.h
@@ -39,11 +39,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     @constant WebHistoryItemsAddedNotification Posted from addItems:.  This 
     notification comes with a userInfo dictionary that contains the array of
     items added.  The key for the array is WebHistoryItemsKey.
-    @constant WebHistoryItemsRemovedNotification Posted from and removeItems:.  
+    @constant WebHistoryItemsRemovedNotification Posted from removeItems:.  
     This notification comes with a userInfo dictionary that contains the array of
     items removed.  The key for the array is WebHistoryItemsKey.
     @constant WebHistoryAllItemsRemovedNotification Posted from removeAllItems
-    @constant WebHistoryLoadedNotification Posted from loadHistory.
+    @constant WebHistoryLoadedNotification Posted from loadFromURL:error:.
 */
 extern NSString *WebHistoryItemsAddedNotification;
 extern NSString *WebHistoryItemsRemovedNotification;
