d805c653e4e24558096195a4f6c6532897308517
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-01 08:34:40 +0000

    2006-04-30  Rob Buis  <buis@kde.org>
            Reviewed by eseidel.  Landed by eseidel.
    
            Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=8651:
            CGContextSetLineDash: invalid dash array: emmited during WebKit tests
    
            Properly discard invalid stroke-dasharray properties like in
            invalid-css.svg.  (Tests updated.)
    
            * ksvg2/css/SVGCSSParser.cpp:
            (WebCore::CSSParser::parseSVGStrokeDasharray):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14126 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/mac/FrameMac.h
+++ third_party/WebKit/WebCore/bridge/mac/FrameMac.h
@@ -143,13 +143,13 @@ public:
 
     virtual void unfocusWindow();
     
-    void openURLFromPageCache(KWQPageState* state);
+    void openURLFromPageCache(KWQPageState*);
 
     virtual void saveDocumentState();
     virtual void restoreDocumentState();
     
     virtual void addMessageToConsole(const String& message,  unsigned int lineNumber, const String& sourceID);
-    virtual void setDisplaysWithFocusAttributes(bool flag);
+    virtual void setDisplaysWithFocusAttributes(bool);
     
     NSView* nextKeyView(Node* startingPoint, KWQSelectionDirection);
     NSView* nextKeyViewInFrameHierarchy(Node* startingPoint, KWQSelectionDirection);
@@ -162,8 +162,8 @@ public:
     
     static bool currentEventIsMouseDownInWidget(Widget* candidate);
     
-    virtual void runJavaScriptAlert(const String& message);
-    virtual bool runJavaScriptConfirm(const String& message);
+    virtual void runJavaScriptAlert(const String&);
+    virtual bool runJavaScriptConfirm(const String&);
     virtual bool runJavaScriptPrompt(const String& message, const String& defaultValue, String& result);
     virtual bool locationbarVisible();
     virtual bool menubarVisible();
@@ -324,7 +324,7 @@ private:
     
     bool dispatchCPPEvent(const AtomicString &eventType, ClipboardMac::AccessPolicy policy);
 
-    NSImage* imageFromRect(NSRect rect) const;
+    NSImage* imageFromRect(NSRect) const;
 
     void freeClipboard();
 
