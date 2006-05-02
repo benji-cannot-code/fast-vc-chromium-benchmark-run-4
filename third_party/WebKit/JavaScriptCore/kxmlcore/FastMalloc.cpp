74207760870a84910bfa9b748b3251d7fd317bc5
sfalken <sfalken@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-02 22:48:58 +0000

    2006-05-02  Steve Falkenburg  <sfalken@apple.com>
            Reviewed by eric.
    
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj: set NDEBUG for release build
            * kxmlcore/FastMalloc.cpp: Prevent USE_SYSTEM_MALLOC from being defined twice
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14162 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kxmlcore/FastMalloc.cpp
+++ third_party/WebKit/JavaScriptCore/kxmlcore/FastMalloc.cpp
@@ -66,10 +66,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "FastMalloc.h"
 
+#ifndef USE_SYSTEM_MALLOC
 #ifndef NDEBUG
 #define USE_SYSTEM_MALLOC 1
 #else
 #define USE_SYSTEM_MALLOC 0
+#endif
 #endif
 
 #if USE_SYSTEM_MALLOC
