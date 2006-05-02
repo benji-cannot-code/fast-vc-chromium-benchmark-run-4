62532fc56eef4179d86baef8f3a36dbf34221b1b
sfalken <sfalken@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-02 22:54:04 +0000

    2006-05-02  Steve Falkenburg  <sfalken@apple.com>
            Reviewed by eric.
    
            WARNING: NO TEST CASES ADDED OR CHANGED
    
            * WebCore.vcproj/WebCore/WebCore.vcproj: defined NDEBUG in release build
            * bridge/win/FrameWin.cpp:
            (WebCore::FrameWin::FrameWin): turned on javascript.
            * kwq/KWQKHTMLSettings.h: 
            (KHTMLSettings::KHTMLSettings): initialized settings to 0
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/win/FrameWin.cpp
+++ third_party/WebKit/WebCore/bridge/win/FrameWin.cpp
@@ -51,6 +51,7 @@ FrameWin::FrameWin(Page* page, RenderPart* renderPart, FrameWinClient* client)
     settings->setFixedFontName("Courier New");
     settings->setSansSerifFontName("Arial");
     settings->setStdFontName("Times New Roman");
+    settings->setIsJavaScriptEnabled(true);
 
     setSettings(settings);
     m_client = client;
