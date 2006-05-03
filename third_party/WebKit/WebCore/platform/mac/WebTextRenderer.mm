18184ccb67034a39102365f81458cc3efb158cde
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-03 00:02:38 +0000

    Partial fix for the crash in bugzilla bug 8088.  There's a third crash         even after fixing this though.
            Reviewed by darin
    
            * platform/mac/WebTextRenderer.mm:
            (WebCore::WidthMap::widths):
            (WebCore::extendWidthMap):
            * platform/mac/WebTextRendererFactory.mm:
            (-[WebTextRendererFactory clearCaches]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14167 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/WebTextRenderer.mm
+++ third_party/WebKit/WebCore/platform/mac/WebTextRenderer.mm
@@ -89,6 +89,8 @@ namespace WebCore
 typedef float WebGlyphWidth;
 
 struct WidthMap {
+    WidthMap() :next(0), widths(0) {}
+
     ATSGlyphRef startRange;
     ATSGlyphRef endRange;
     WidthMap *next;
@@ -1186,7 +1188,7 @@ static ATSGlyphRef extendGlyphMap(WebTextRenderer *renderer, UChar32 c)
 
 static WidthMap *extendWidthMap(WebTextRenderer *renderer, ATSGlyphRef glyph)
 {
-    WidthMap *map = (WidthMap *)calloc(1, sizeof(WidthMap));
+    WidthMap *map = new WidthMap;
     unsigned end;
     ATSGlyphRef start;
     unsigned blockSize;
