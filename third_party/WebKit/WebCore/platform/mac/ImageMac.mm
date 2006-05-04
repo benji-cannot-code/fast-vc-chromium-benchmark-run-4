fb5a7efc4b4079ebd9db23309eb4b78f8b357501
bdakin <bdakin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-04 18:20:56 +0000

    --This        Reviewed by Hyatt.
            Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=8732 
            Backgrounds with background-size and background-origin do not 
            always lay out correctly
    
            * platform/cairo/ImageCairo.cpp:
            (WebCore::Image::drawTiled): We no longer need to scale the source 
            point since it will now come in with the scale already taken into 
            account.
            * platform/mac/ImageMac.mm:
            (WebCore::Image::drawTiled): Same as above.
            * rendering/RenderBox.cpp:
            (WebCore::cacluateBackgroundSize): Now calculate the background 
            size in a helper method.
            (WebCore::RenderBox::paintBackgroundExtended): Calculate the 
            background size before calculating cx, cy, cw, ch, sx, and sy so 
            that they can all be calculated with the scaled image size taken 
            into account.
    
    
    M    LayoutTests/ChangeLog
    M    LayoutTests/fast/backgrounds/size/backgroundSize18-expected.checksum
    M    LayoutTests/fast/backgrounds/size/backgroundSize12-expected.png
    M    LayoutTests/fast/backgrounds/size/backgroundSize19-expected.checksum
    M    LayoutTests/fast/backgrounds/size/backgroundSize10-expected.png
    M    LayoutTests/fast/backgrounds/size/backgroundSize18-expected.png
    M    LayoutTests/fast/backgrounds/size/backgroundSize19-expected.png
    M    LayoutTests/fast/backgrounds/size/backgroundSize10-expected.checksum
    M    LayoutTests/fast/backgrounds/size/backgroundSize12-expected.checksum
    M    WebCore/platform/mac/ImageMac.mm
    M    WebCore/platform/cairo/ImageCairo.cpp
    M    WebCore/rendering/RenderBox.cpp
    M    WebCore/ChangeLog
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/ImageMac.mm
+++ third_party/WebKit/WebCore/platform/mac/ImageMac.mm
@@ -305,8 +305,6 @@ void Image::drawTiled(GraphicsContext* ctxt, const FloatRect& destRect, const Fl
         scaleX = tileSize.width() / intrinsicTileSize.width;
         scaleY = tileSize.height() / intrinsicTileSize.height;
         patternTransform = CGAffineTransformMakeScale(scaleX, scaleY);
-        
-        scaledPoint = CGPointApplyAffineTransform(scaledPoint, patternTransform);
         scaledTileSize = tileSize;
     }
 
