07a837eb3bc2e19ef3d0abe565c0470a946954e5
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-10 21:08:31 +0000

    Rename isSpace to treatAsSpace.  Move it and the rounding hack function into         Font and make them static methods (inlined in the header).  Make the rounding         hack character table a static member as well.  Remove the redundant space/rounding         functions from FontData.mm.
            Reviewed by darin
    
            * platform/Font.cpp:
            (WebCore::):
            (WebCore::m_finalRoundingWidth):
            (WebCore::WidthIterator::advance):
            * platform/Font.h:
            (WebCore::Font::treatAsSpace):
            (WebCore::Font::isRoundingHackCharacter):
            * platform/mac/FontData.mm:
            (WebCore::overrideLayoutOperation):
            (WebCore::createATSULayoutParameters):
            (WebCore::initializeWidthIterator):
            (WebCore::advanceWidthIterator):
            * platform/mac/FontMac.mm:
            (WebCore::overrideLayoutOperation):
            (WebCore::ATSULayoutParameters::initialize):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14292 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Font.cpp
+++ third_party/WebKit/WebCore/platform/Font.cpp
@@ -41,18 +41,11 @@ namespace WebCore {
 #if __APPLE__
 
 // FIXME: Cross-platform eventually, but for now we compile only on OS X.
-#define SPACE 0x0020
-#define NO_BREAK_SPACE 0x00A0
 
 // According to http://www.unicode.org/Public/UNIDATA/UCD.html#Canonical_Combining_Class_Values
 #define HIRAGANA_KATAKANA_VOICING_MARKS 8
 
-bool isSpace(unsigned c)
-{
-    return c == SPACE || c == '\t' || c == '\n' || c == NO_BREAK_SPACE;
-}
-
-static const uint8_t isRoundingHackCharacterTable[0x100] = {
+const uint8_t Font::gRoundingHackCharacterTable[256] = {
     0, 0, 0, 0, 0, 0, 0, 0, 0, 1 /*\t*/, 1 /*\n*/, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     1 /*space*/, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 /*-*/, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 /*?*/,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -62,11 +55,6 @@ static const uint8_t isRoundingHackCharacterTable[0x100] = {
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
-
-bool isRoundingHackCharacter(UChar32 c)
-{
-    return (((c & ~0xFF) == 0 && isRoundingHackCharacterTable[c]));
-}
 
 struct WidthIterator {
     WidthIterator(const Font* font, const UChar* str, int from, int to, int len,
@@ -117,7 +105,7 @@ WidthIterator::WidthIterator(const Font* font, const UChar* str, int from, int t
     } else {
         float numSpaces = 0;
         for (int i = from; i < to; i++)
-            if (isSpace(m_characters[i]))
+            if (Font::treatAsSpace(m_characters[i]))
                 numSpaces++;
 
         m_padding = toAdd;
@@ -240,7 +228,7 @@ void WidthIterator::advance(int offset, GlyphBuffer* glyphBuffer)
             if (width && m_font->letterSpacing())
                 width += m_font->letterSpacing();
 
-            if (isSpace(c)) {
+            if (Font::treatAsSpace(c)) {
                 // Account for padding. WebCore uses space padding to justify text.
                 // We distribute the specified padding over the available spaces in the run.
                 if (m_padding) {
@@ -256,7 +244,7 @@ void WidthIterator::advance(int offset, GlyphBuffer* glyphBuffer)
 
                 // Account for word spacing.
                 // We apply additional space between "words" by adding width to the space character.
-                if (currentCharacter != 0 && !isSpace(cp[-1]) && m_font->wordSpacing())
+                if (currentCharacter != 0 && !Font::treatAsSpace(cp[-1]) && m_font->wordSpacing())
                     width += m_font->wordSpacing();
             }
         }
@@ -274,12 +262,12 @@ void WidthIterator::advance(int offset, GlyphBuffer* glyphBuffer)
 
         // Force characters that are used to determine word boundaries for the rounding hack
         // to be integer width, so following words will start on an integer boundary.
-        if (m_applyWordRounding && isRoundingHackCharacter(c))
+        if (m_applyWordRounding && Font::isRoundingHackCharacter(c))
             width = ceilf(width);
 
         // Check to see if the next character is a "rounding hack character", if so, adjust
         // width so that the total run width will be on an integer boundary.
-        if ((m_applyWordRounding && currentCharacter < m_len && isRoundingHackCharacter(*cp))
+        if ((m_applyWordRounding && currentCharacter < m_len && Font::isRoundingHackCharacter(*cp))
                 || (m_applyRunRounding && currentCharacter >= m_to)) {
             float totalWidth = m_widthToStart + runWidthSoFar + width;
             width += ceilf(totalWidth) - totalWidth;
