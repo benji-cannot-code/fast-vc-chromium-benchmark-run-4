d0079c30538c5945c91b8235a98097f5025b08b5
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-10 06:04:29 +0000

    - another attempt to fix the Windows build
            * bindings/js/kjs_window.cpp: (KJS::isSeparator): Take a UChar, not a KJS::UChar.
            * bridge/win/FrameWin.cpp:
            (WebCore::FrameWin::runJavaScriptAlert): Use UChar instead of QChar.
            (WebCore::FrameWin::runJavaScriptConfirm): Ditto.
            * editing/TextIterator.h: Include DeprecatedString.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14276 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/TextIterator.h
+++ third_party/WebKit/WebCore/editing/TextIterator.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef KHTML_EDITING_VISIBLE_TEXT_H
 #define KHTML_EDITING_VISIBLE_TEXT_H
 
+#include "DeprecatedString.h"
 #include "InlineTextBox.h"
 #include "Range.h"
 #include <wtf/Vector.h>
