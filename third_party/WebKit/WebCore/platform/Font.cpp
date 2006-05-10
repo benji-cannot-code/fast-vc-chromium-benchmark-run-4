23d7e05cda5d216e323404b4354dd437f7fd6227
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-10 15:11:47 +0000

    2006-05-10  Mitz Pettel  <opendarwin.org@mitzpettel.com>
            Reviewed and landed by Anders.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=8830
              REGRESSION: Words render on top of each other when fallback font is used
    
            Already covered by several pixel tests in fast/text
    
            * platform/Font.cpp:
            (WebCore::Font::drawSimpleText):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Font.cpp
+++ third_party/WebKit/WebCore/platform/Font.cpp
@@ -507,7 +507,7 @@ void Font::drawSimpleText(GraphicsContext* context, const IntPoint& point, int t
             drawGlyphs(context, fontData, glyphBuffer, lastFrom, nextGlyph - lastFrom, startPoint);
             lastFrom = nextGlyph;
             fontData = nextFontData;
-            startX = nextX;
+            startPoint.setX(nextX);
         }
         nextX += glyphBuffer.advanceAt(nextGlyph);
         nextGlyph++;
