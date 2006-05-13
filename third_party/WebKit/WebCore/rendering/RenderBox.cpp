6078286977a51bf757cd89de802d2297725d52e5
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-13 16:19:24 +0000

    Ok, now RenderBox.cpp actually compiles for me.
    git-svn-id: svn://svn.chromium.org/blink/trunk@14357 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBox.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBox.cpp
@@ -2022,8 +2022,8 @@ void RenderBox::calcAbsoluteHorizontalReplaced()
      *    ('rtl'), set 'margin-left' ('margin-right') to zero and solve for
      *    'margin-right' ('margin-left').
     \*-----------------------------------------------------------------------*/
-    int leftValue;
-    int rightValue;
+    int leftValue = 0;
+    int rightValue = 0;
 
     if (marginLeft.isAuto() && marginRight.isAuto()) {
         // 'left' and 'right' cannot be 'auto' due to step 3
@@ -2164,8 +2164,8 @@ void RenderBox::calcAbsoluteVerticalReplaced()
      *    'auto', solve the equation under the extra constraint that the two
      *    margins must get equal values.
     \*-----------------------------------------------------------------------*/
-    int topValue;
-    int bottomValue;
+    int topValue = 0;
+    int bottomValue = 0;
 
     if (marginTop.isAuto() && marginBottom.isAuto()) {
         // 'top' and 'bottom' cannot be 'auto' due to step 2 and 3 combinded.
