9663ea72ec326cd43ebe6a75be3b7a35440c6b77
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-13 00:57:03 +0000

    2006-05-12  Eric Seidel  <eseidel@apple.com>
            Reviewed by beth.
    
            Fix build error.
    
            * rendering/InlineFlowBox.cpp:
            * rendering/RootInlineBox.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14344 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/InlineFlowBox.cpp
+++ third_party/WebKit/WebCore/rendering/InlineFlowBox.cpp
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CachedImage.h"
 #include "Document.h"
-#include "ElipseBox.h"
+#include "EllipsisBox.h"
 #include "GraphicsContext.h"
 #include "InlineTextBox.h"
 #include "RootInlineBox.h"
