6c5010b190fb299d16343e7710af1c6a4d32ce8e
sfalken <sfalken@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-14 03:58:01 +0000

    2006-05-14  Steve Falkenburg  <sfalken@apple.com>
            Reviewed by maciej.
    
            Fix Windows build except for fontwin.cpp.
    
            * WebCore/platform/win/FontWin.cpp:
            * WebCore.vcproj/WebCore/WebCore.vcproj:
            * bridge/win/FrameWin.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/win/FrameWin.cpp
+++ third_party/WebKit/WebCore/bridge/win/FrameWin.cpp
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "PlatformKeyboardEvent.h"
 #include "KWQKHTMLSettings.h"
-#include "render_frames.h"
+#include "RenderFrame.h"
 #include "Plugin.h"
 #include "TransferJob.h"
 #include "FramePrivate.h"
