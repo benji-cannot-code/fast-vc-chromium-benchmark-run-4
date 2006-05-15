83e8bc4fe9655666ae560e572a069e4dbb60852a
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-15 17:14:05 +0000

    2006-05-15  Eric Seidel  <eseidel@apple.com>
            Fix linux build.
    
            * html/HTMLBodyElement.cpp: Fix case of #include "cssstyleselector.h"
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14377 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLBodyElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLBodyElement.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "css_stylesheetimpl.h"
 #include "CSSPropertyNames.h"
-#include "CSSStyleSelector.h"
+#include "cssstyleselector.h"
 #include "CSSValueKeywords.h"
 #include "Document.h"
 #include "EventNames.h"
