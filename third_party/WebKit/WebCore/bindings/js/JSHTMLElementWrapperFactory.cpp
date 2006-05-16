19467285227c7c3b77907bf7be6459a6a55ca71f
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-16 10:02:07 +0000

    2006-05-16  Eric Seidel  <eseidel@apple.com>
            Reviewed by andersca.
    
            Autogenerate BASE, HEAD, LINK, STYLE and TITLE JS bindings.
    
            Already covered by existing dom tests.
    
            * DerivedSources.make:
            * WebCore.xcodeproj/project.pbxproj:
            * bindings/js/JSHTMLElementWrapperFactory.cpp:
            (WebCore::createBaseWrapper):
            (WebCore::createHeadWrapper):
            (WebCore::createLinkWrapper):
            (WebCore::createStyleWrapper):
            (WebCore::createTitleWrapper):
            (WebCore::createJSWrapper):
            * bindings/js/kjs_html.cpp:
            (KJS::):
            (KJS::JSHTMLElement::classInfo):
            (KJS::JSHTMLElement::accessors):
            * bindings/js/kjs_html.h:
            * html/HTMLBaseElement.idl: Added.
            * html/HTMLHeadElement.idl: Added.
            * html/HTMLLinkElement.idl: Added.
            * html/HTMLStyleElement.idl: Added.
            * html/HTMLTitleElement.idl: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSHTMLElementWrapperFactory.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSHTMLElementWrapperFactory.cpp
@@ -20,12 +20,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "JSHTMLElementWrapperFactory.h"
 
+#include "HTMLBaseElement.h"
 #include "HTMLCanvasElement.h"
+#include "HTMLHeadElement.h"
+#include "HTMLLinkElement.h"
 #include "HTMLMetaElement.h"
+#include "HTMLStyleElement.h"
+#include "HTMLTitleElement.h"
 #include "HTMLNames.h"
+#include "JSHTMLBaseElement.h"
 #include "JSHTMLCanvasElement.h"
+#include "JSHTMLHeadElement.h"
+#include "JSHTMLLinkElement.h"
 #include "JSHTMLMetaElement.h"
-#include "JSHTMLElement.h"
+#include "JSHTMLStyleElement.h"
+#include "JSHTMLTitleElement.h"
 #include "kjs_html.h"
 
 using namespace KJS;
@@ -36,9 +45,24 @@ using namespace HTMLNames;
 
 typedef DOMNode* (*CreateHTMLElementWrapperFunction)(ExecState*, PassRefPtr<HTMLElement>);
 
+static DOMNode* createBaseWrapper(ExecState* exec, PassRefPtr<HTMLElement> element)
+{
+    return new JSHTMLBaseElement(exec, static_cast<HTMLBaseElement*>(element.get()));
+}
+
 static DOMNode* createCanvasWrapper(ExecState* exec, PassRefPtr<HTMLElement> element)
 {
     return new JSHTMLCanvasElement(exec, static_cast<HTMLCanvasElement*>(element.get()));
+}
+
+static DOMNode* createHeadWrapper(ExecState* exec, PassRefPtr<HTMLElement> element)
+{
+    return new JSHTMLHeadElement(exec, static_cast<HTMLHeadElement*>(element.get()));
+}
+
+static DOMNode* createLinkWrapper(ExecState* exec, PassRefPtr<HTMLElement> element)
+{
+    return new JSHTMLLinkElement(exec, static_cast<HTMLLinkElement*>(element.get()));
 }
 
 static DOMNode* createMetaWrapper(ExecState* exec, PassRefPtr<HTMLElement> element)
@@ -46,12 +70,27 @@ static DOMNode* createMetaWrapper(ExecState* exec, PassRefPtr<HTMLElement> eleme
     return new JSHTMLMetaElement(exec, static_cast<HTMLMetaElement*>(element.get()));
 }
 
+static DOMNode* createStyleWrapper(ExecState* exec, PassRefPtr<HTMLElement> element)
+{
+    return new JSHTMLStyleElement(exec, static_cast<HTMLStyleElement*>(element.get()));
+}
+
+static DOMNode* createTitleWrapper(ExecState* exec, PassRefPtr<HTMLElement> element)
+{
+    return new JSHTMLTitleElement(exec, static_cast<HTMLTitleElement*>(element.get()));
+}
+
 DOMNode* createJSWrapper(ExecState* exec, PassRefPtr<HTMLElement> element)
 {
     static HashMap<WebCore::AtomicStringImpl*, CreateHTMLElementWrapperFunction> map;
     if (map.isEmpty()) {
+        map.set(baseTag.localName().impl(), createBaseWrapper);
         map.set(canvasTag.localName().impl(), createCanvasWrapper);
+        map.set(headTag.localName().impl(), createHeadWrapper);
+        map.set(linkTag.localName().impl(), createLinkWrapper);
         map.set(metaTag.localName().impl(), createMetaWrapper);
+        map.set(styleTag.localName().impl(), createStyleWrapper);
+        map.set(titleTag.localName().impl(), createTitleWrapper);
     }
     CreateHTMLElementWrapperFunction f = map.get(element->localName().impl());
     if (f)
