8ba6b59a54fc943557f4bf1e79f210086f79efd0
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-20 05:52:02 +0000

    Fix Win32 bustage caused by image document.  supportsType is not implemented yet for ImageCairo, so we'll return false rather than true to keep every document from being an image document.
    git-svn-id: svn://svn.chromium.org/blink/trunk@14499 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/cairo/ImageCairo.cpp
+++ third_party/WebKit/WebCore/platform/cairo/ImageCairo.cpp
@@ -69,7 +69,7 @@ Image* Image::loadResource(const char *name)
 bool Image::supportsType(const String& type)
 {
     // FIXME: Implement.
-    return true;
+    return false;
 }
 
 // Drawing Routines
