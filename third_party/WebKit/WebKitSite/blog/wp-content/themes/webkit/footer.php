860bd7bbebc7a1d67723785822041dcdee6881f7
jdevalk <jdevalk@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-24 22:32:28 +0000

    Adding Google Analytics code, to create visitor stats of our site.
    git-svn-id: svn://svn.chromium.org/blink/trunk@14567 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitSite/blog/wp-content/themes/webkit/footer.php
+++ third_party/WebKit/WebKitSite/blog/wp-content/themes/webkit/footer.php
@@ -13,6 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 </div>
 
 <?php wp_footer(); ?>
-
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-336489-2";
+urchinTracker();
+</script>
 </body>
 </html>
