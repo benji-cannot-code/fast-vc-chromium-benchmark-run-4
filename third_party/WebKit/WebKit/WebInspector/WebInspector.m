33fee497e45feabf38c79aa43a6d6c5bea861df3
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-25 20:15:22 +0000

    Build fix
    git-svn-id: svn://svn.chromium.org/blink/trunk@14592 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebInspector/WebInspector.m
+++ third_party/WebKit/WebKit/WebInspector/WebInspector.m
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebFrame.h"
 #import "WebLocalizableStrings.h"
 #import "WebKitNSStringExtras.h"
+#import "WebTypesInternal.h"
 
 #import <WebKit/DOMCore.h>
 #import <WebKit/DOMHTML.h>
