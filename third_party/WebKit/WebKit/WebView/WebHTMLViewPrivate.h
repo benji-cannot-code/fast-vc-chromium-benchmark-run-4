72a41a8c8963575beccd90a0633f8d5594dd8b9d
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-05-26 19:34:18 +0000

    Reviewed by John Sullivan.
            <rdar://problem/4514529> Add a list type parameter and a return value to _increaseSelectionListLevel
    
            * WebView/WebHTMLView.m:
            (-[WebHTMLView _increaseSelectionListLevel]):
            - Now returns DOMNode*
    
            (-[WebHTMLView _increaseSelectionListLevelOrdered]):
            (-[WebHTMLView _increaseSelectionListLevelUnordered]):
            - Added.  These also return DOMNode*
    
            * WebView/WebHTMLViewPrivate.h:
            - Updated as above
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14614 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebHTMLViewPrivate.h
+++ third_party/WebKit/WebKit/WebView/WebHTMLViewPrivate.h
@@ -97,7 +97,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSRect)_selectionDraggingRect;
 - (BOOL)_canIncreaseSelectionListLevel;
 - (BOOL)_canDecreaseSelectionListLevel;
-- (void)_increaseSelectionListLevel;
+- (DOMNode *)_increaseSelectionListLevel;
+- (DOMNode *)_increaseSelectionListLevelOrdered;
+- (DOMNode *)_increaseSelectionListLevelUnordered;
 - (void)_decreaseSelectionListLevel;
 
 // SPI for DumpRenderTree
