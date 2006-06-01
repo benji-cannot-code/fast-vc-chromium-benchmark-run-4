627d9778606aa8a438ff9b9e74432cde508f243e
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-01 05:02:18 +0000

    LayoutTests:
            Reviewed by Geoff.
    
            Add a test for http://bugzilla.opendarwin.org/show_bug.cgi?id=8024
    
            * fast/dom/document-all-input.html: Added.
    
    WebCore:
    
            Reviewed by Geoff.
    
            Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=8024
            "Go" button (search) doesn't work at fifa.com (document.all.FormName)
    
            Allow input elements with name attributes as possible named
            items for document.all.
    
            * html/HTMLCollection.cpp:
            (WebCore::HTMLCollection::checkForNameMatch):
            (WebCore::HTMLCollection::updateNameCache):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14654 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLCollection.cpp
+++ third_party/WebKit/WebCore/html/HTMLCollection.cpp
@@ -271,7 +271,7 @@ bool HTMLCollection::checkForNameMatch(Node *node, bool checkName, const String 
             if (type == DOC_ALL && 
                 !(e->hasLocalName(imgTag) || e->hasLocalName(formTag) ||
                   e->hasLocalName(appletTag) || e->hasLocalName(objectTag) ||
-                  e->hasLocalName(embedTag)))
+                  e->hasLocalName(embedTag) || e->hasLocalName(inputTag)))
                 return false;
 
             return e->getAttribute(nameAttr) == name && e->getAttribute(idAttr) != name;
@@ -284,7 +284,7 @@ bool HTMLCollection::checkForNameMatch(Node *node, bool checkName, const String 
             if (type == DOC_ALL && 
                 !(e->hasLocalName(imgTag) || e->hasLocalName(formTag) ||
                   e->hasLocalName(appletTag) || e->hasLocalName(objectTag) ||
-                  e->hasLocalName(embedTag)))
+                  e->hasLocalName(embedTag) || e->hasLocalName(inputTag)))
                 return false;
 
             return e->getAttribute(nameAttr).domString().lower() == name.lower() &&
@@ -350,7 +350,7 @@ void HTMLCollection::updateNameCache() const
             && (type != DOC_ALL || 
                 (e->hasLocalName(imgTag) || e->hasLocalName(formTag) ||
                  e->hasLocalName(appletTag) || e->hasLocalName(objectTag) ||
-                 e->hasLocalName(embedTag)))) {
+                 e->hasLocalName(embedTag) || e->hasLocalName(inputTag)))) {
             // add to name cache
             Vector<Node*>* nameVector = info->nameCache.get(nameAttrVal.impl());
             if (!nameVector) {
