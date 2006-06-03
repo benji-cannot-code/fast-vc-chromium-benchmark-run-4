c703f9ddf705ee372ed14085f8e48cb3d1232ef4
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-03 22:23:52 +0000

    LayoutTests:
            Reviewed by mjs.  Tweaked and landed by ddkilzer.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=7281
            With java disabled wrong information is displayed.
    
            * fast/replaced/applet-rendering-java-disabled.html: Added.
            * fast/replaced/applet-rendering-java-disabled-expected.checksum: Added.
            * fast/replaced/applet-rendering-java-disabled-expected.html: Added.
            * fast/replaced/applet-rendering-java-disabled-expected.png: Added.
            * tables/mozilla_expected_failures/bugs/bug8499-expected.checksum: Changed after removing RenderEmptyApplet.
            * tables/mozilla_expected_failures/bugs/bug8499-expected.png: Ditto.
            * tables/mozilla_expected_failures/bugs/bug8499-expected.txt: Ditto.
    
    WebCore:
    
            Reviewed by mjs.  Tweaked and landed by ddkilzer.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=7281
            With java disabled wrong information is displayed.
    
            Test: fast/replaced/applet-rendering-java-disabled.html
    
            Match WinIE, render applet content inline when java is
            disabled. 
    
            * WebCore.vcproj/WebCore/WebCore.vcproj: Removed RenderEmptyApplet.cpp and RenderEmptyApplet.h.
            * WebCore.xcodeproj/project.pbxproj: Ditto.
            * WebCoreSources.bkl: Ditto.
            * html/HTMLAppletElement.cpp:
            (WebCore::HTMLAppletElement::createRenderer):
            * rendering/RenderEmptyApplet.cpp: Removed.
            * rendering/RenderEmptyApplet.h: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14715 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLAppletElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLAppletElement.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLDocument.h"
 #include "HTMLNames.h"
 #include "RenderApplet.h"
-#include "RenderEmptyApplet.h"
+#include "RenderInline.h"
 
 namespace WebCore {
 
@@ -136,9 +136,7 @@ RenderObject *HTMLAppletElement::createRenderer(RenderArena *arena, RenderStyle 
         return new (document()->renderArena()) RenderApplet(this, args);
     }
 
-    // ### remove me. we should never show an empty applet, instead
-    // render the alternative content given by the webpage
-    return new (document()->renderArena()) RenderEmptyApplet(this);
+    return new (document()->renderArena()) RenderInline(this);
 }
 
 #if __APPLE__
