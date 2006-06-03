2289cd35936ac57c10b293c838ebc7bbf683f933
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-03 20:03:50 +0000

    - fix Windows build
            * platform/Widget.h: Moved scaleFactor function out of Apple-specific
            section of the header into the common part.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Widget.h
+++ third_party/WebKit/WebCore/platform/Widget.h
@@ -93,6 +93,8 @@ namespace WebCore {
 
         virtual IntPoint mapFromGlobal(const IntPoint&) const;
 
+        float scaleFactor() const;
+
         bool hasFocus() const;
         void setFocus();
         void clearFocus();
@@ -138,8 +140,6 @@ namespace WebCore {
         NSView* getOuterView() const;
         void setView(NSView*);
         
-        float scaleFactor() const;
-
         void sendConsumedMouseUp();
         
         static void beforeMouseDown(NSView*);
