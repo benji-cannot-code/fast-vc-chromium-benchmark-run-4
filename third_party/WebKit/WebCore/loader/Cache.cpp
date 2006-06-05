2d89051a6475a894c4318c69455d2fd6455ce0d7
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-05 04:15:18 +0000

    LayoutTests:
            - test for http://bugzilla.opendarwin.org/show_bug.cgi?id=9176
              REGRESSION: repro crash in WebCore::StringImpl::hash() const + 28 (StringImpl.h:67)
    
            * fast/loader/link-no-URL.html: Added.
            * fast/loader/link-no-URL-expected.txt: Added.
    
    WebCore:
    
            Reviewed by Maciej.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=9176
              REGRESSION: repro crash in WebCore::StringImpl::hash() const + 28 (StringImpl.h:67)
    
            Test: fast/loader/link-no-URL.html
    
            * loader/Cache.h:
            * loader/Cache.cpp:
            (WebCore::Cache::updateCacheStatus): Removed the URL parameter. Instead get the URL
            from the cached object itself. Callers were passing in the wrong URL. When it was a
            null string, it caused the crash mentioned above. But in other cases it may have
            caused some other minor problems as well. Added an assert to catch cases where the
            URL is null (should never happen).
            (WebCore::Cache::requestImage): Changed call site to not pass URL.
            (WebCore::Cache::requestStyleSheet): Ditto.
            (WebCore::Cache::requestScript): Ditto.
            (WebCore::Cache::requestXSLStyleSheet): Ditto.
            (WebCore::Cache::requestXBLDocument): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/Cache.cpp
+++ third_party/WebKit/WebCore/loader/Cache.cpp
@@ -110,18 +110,19 @@ void Cache::clear()
     delete docloaders; docloaders = 0;
 }
 
-void Cache::updateCacheStatus(DocLoader* dl, const String& url, CachedObject* o)
+void Cache::updateCacheStatus(DocLoader* dl, CachedObject* o)
 {
     moveToHeadOfLRUList(o);
     if (dl) {
+        ASSERT(!o->url().isNull());
         if (cacheDisabled)
-            dl->m_docObjects.remove(url);
+            dl->m_docObjects.remove(o->url());
         else
-            dl->m_docObjects.set(url, o);
+            dl->m_docObjects.set(o->url(), o);
     }
 }
 
-CachedImage *Cache::requestImage(DocLoader* dl, const String& url, bool reload, time_t expireDate)
+CachedImage* Cache::requestImage(DocLoader* dl, const String& url, bool reload, time_t expireDate)
 {
     // this brings the _url to a standard form...
     KURL kurl;
@@ -132,7 +133,7 @@ CachedImage *Cache::requestImage(DocLoader* dl, const String& url, bool reload, 
     return requestImage(dl, kurl, reload, expireDate);
 }
 
-CachedImage *Cache::requestImage(DocLoader* dl, const KURL& url, bool reload, time_t expireDate)
+CachedImage* Cache::requestImage(DocLoader* dl, const KURL& url, bool reload, time_t expireDate)
 {
     KIO::CacheControl cachePolicy;
     if (dl)
@@ -174,11 +175,11 @@ CachedImage *Cache::requestImage(DocLoader* dl, const KURL& url, bool reload, ti
         kdDebug(6060) << "Cache: using cached: " << kurl.url() << ", status " << o->status() << endl;
 #endif
 
-    updateCacheStatus(dl, o->url(), o);
+    updateCacheStatus(dl, o);
     return static_cast<CachedImage *>(o);
 }
 
-CachedCSSStyleSheet *Cache::requestStyleSheet(DocLoader* dl, const String& url, bool reload, time_t expireDate, const DeprecatedString& charset)
+CachedCSSStyleSheet* Cache::requestStyleSheet(DocLoader* dl, const String& url, bool reload, time_t expireDate, const DeprecatedString& charset)
 {
     // this brings the _url to a standard form...
     KURL kurl;
@@ -224,11 +225,11 @@ CachedCSSStyleSheet *Cache::requestStyleSheet(DocLoader* dl, const String& url, 
         kdDebug(6060) << "Cache: using cached: " << kurl.url() << endl;
 #endif
 
-    updateCacheStatus(dl, url, o);
+    updateCacheStatus(dl, o);
     return static_cast<CachedCSSStyleSheet *>(o);
 }
 
-CachedScript *Cache::requestScript(DocLoader* dl, const String& url, bool reload, time_t expireDate, const DeprecatedString& charset)
+CachedScript* Cache::requestScript(DocLoader* dl, const String& url, bool reload, time_t expireDate, const DeprecatedString& charset)
 {
     // this brings the _url to a standard form...
     KURL kurl;
@@ -275,7 +276,7 @@ CachedScript *Cache::requestScript(DocLoader* dl, const String& url, bool reload
         kdDebug(6060) << "Cache: using cached: " << kurl.url() << endl;
 #endif
 
-    updateCacheStatus(dl, url, o);
+    updateCacheStatus(dl, o);
     return static_cast<CachedScript *>(o);
 }
 
@@ -326,7 +327,7 @@ CachedXSLStyleSheet* Cache::requestXSLStyleSheet(DocLoader* dl, const String& ur
         kdDebug(6060) << "Cache: using cached: " << kurl.url() << endl;
 #endif
     
-    updateCacheStatus(dl, url, o);
+    updateCacheStatus(dl, o);
     return static_cast<CachedXSLStyleSheet*>(o);
 }
 #endif
@@ -378,7 +379,7 @@ CachedXBLDocument* Cache::requestXBLDocument(DocLoader* dl, const String& url, b
         kdDebug(6060) << "Cache: using cached: " << kurl.url() << endl;
 #endif
     
-    updateCacheStatus(dl, url, o);
+    updateCacheStatus(dl, o);
     return static_cast<CachedXBLDocument*>(o);
 }
 #endif
