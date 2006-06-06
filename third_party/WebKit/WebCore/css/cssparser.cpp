94a10ae275b9bc2ca59e0f621cc067185a58c36f
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-06 03:13:33 +0000

    LayoutTests:
            Reviewed by mjs.
    
            testcases for http://bugzilla.opendarwin.org/show_bug.cgi?id=5564
            'font' shorthand parsing should be more tolerant in quirks mode
    
            * fast/css/font_property_compatibility-expected.checksum: Added.
            * fast/css/font_property_compatibility-expected.png: Added.
            * fast/css/font_property_compatibility-expected.txt: Added.
            * fast/css/font_property_compatibility.html: Added.
            * fast/css/font_property_normal-expected.checksum: Added.
            * fast/css/font_property_normal-expected.png: Added.
            * fast/css/font_property_normal-expected.txt: Added.
            * fast/css/font_property_normal.html: Added.
    
    WebCore:
    
            Reviewed by mjs.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=5564
            'font' shorthand parsing should be more tolerant in quirks mode
    
            When not in strict mode accept font property specifications
            lacking a font family part, like WinIE does.
    
            * css/cssparser.cpp:
            (WebCore::CSSParser::parseFont):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14740 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/cssparser.cpp
+++ third_party/WebKit/WebCore/css/cssparser.cpp
@@ -2033,7 +2033,7 @@ bool CSSParser::parseFont(bool important)
             break;
         value = valueList->next();
     }
-    if (!value)
+    if (strict && !value)
         goto invalid;
 
     // set undefined values to default
@@ -2051,10 +2051,10 @@ bool CSSParser::parseFont(bool important)
     else if (validUnit(value, FLength|FPercent, strict))
         font->size = new CSSPrimitiveValue(value->fValue, (CSSPrimitiveValue::UnitTypes) value->unit);
     value = valueList->next();
-    if (!font->size || !value)
+    if (!font->size || (strict && !value))
         goto invalid;
 
-    if (value->unit == Value::Operator && value->iValue == '/') {
+    if (value && value->unit == Value::Operator && value->iValue == '/') {
         // line-height
         value = valueList->next();
         if (!value)
@@ -2076,7 +2076,9 @@ bool CSSParser::parseFont(bool important)
     // font family must come now
     font->family = parseFontFamily();
 
-    if (valueList->current() || !font->family)
+    if (!strict && !font->family)
+        font->family = new CSSValueList;
+    else if (valueList->current() || !font->family)
         goto invalid;
 
     addProperty(CSS_PROP_FONT, font, important);
