a22bcb78c103547502e5d94bd0664db1846db695
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-08 04:04:38 +0000

    LayoutTests:
            Reviewed by levi
    
            * editing/deleting/delete-4038408-fix-expected.txt:
            * editing/input/attributed-substring-from-range-lines-expected.txt:
            * editing/inserting/insert-3654864-fix-expected.txt:
            * editing/inserting/insert-3659587-fix-expected.txt:
            * editing/inserting/insert-3775316-fix-expected.txt:
            * editing/inserting/insert-at-end-01-expected.txt:
            * editing/inserting/insert-at-end-02-expected.txt:
            * editing/inserting/insert-br-001-expected.txt:
            * editing/inserting/insert-br-005-expected.txt:
    
    WebCore:
    
            Reviewed by levi
            
            Fixed some comments and removed an unused variable.
    
            * editing/CompositeEditCommand.cpp:
            (WebCore::CompositeEditCommand::removeBlockPlaceholder):
            Don't remove a br if it isn't at the start of a block, since
            it isn't really a "block placeholder".
            * editing/DeleteSelectionCommand.cpp:
            (WebCore::DeleteSelectionCommand::initializePositionData):
            (WebCore::DeleteSelectionCommand::mergeParagraphs):
            (WebCore::DeleteSelectionCommand::doApply):
            * editing/InsertLineBreakCommand.cpp:
            (WebCore::InsertLineBreakCommand::doApply):
            * editing/ReplaceSelectionCommand.cpp:
            (WebCore::ReplaceSelectionCommand::doApply):
            (WebCore::ReplaceSelectionCommand::completeHTMLReplacement):
            * editing/htmlediting.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/CompositeEditCommand.h
+++ third_party/WebKit/WebCore/editing/CompositeEditCommand.h
@@ -94,7 +94,7 @@ protected:
     WebCore::Node *appendBlockPlaceholder(WebCore::Node *);
     WebCore::Node *insertBlockPlaceholder(const WebCore::Position &pos);
     WebCore::Node *addBlockPlaceholderIfNeeded(WebCore::Node *);
-    bool removeBlockPlaceholder(const VisiblePosition&);
+    void removeBlockPlaceholder(const VisiblePosition&);
 
     void moveParagraphContentsToNewBlockIfNecessary(const WebCore::Position &);
     
