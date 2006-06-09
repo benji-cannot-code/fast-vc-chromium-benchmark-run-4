0f7730d1c9c64c153fe447050ff9fbb822dc5aca
sfalken <sfalken@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-09 18:40:55 +0000

    2006-06-09  Steve Falkenburg  <sfalken@apple.com>
            Fix build break
    
            * WebCore.vcproj/WebCore/WebCore.vcproj:
            * css/MediaQueryEvaluator.cpp:
            * platform/win/TemporaryLinkStubs.cpp:
            (GraphicsContext::addRoundedRectClip):
            (GraphicsContext::addInnerRoundedRectClip):
            (WebCore::screenDepthPerComponent):
            (WebCore::screenIsMonochrome):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14780 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/MediaQueryEvaluator.cpp
+++ third_party/WebKit/WebCore/css/MediaQueryEvaluator.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CSSPrimitiveValue.h"
 #include "CSSValueList.h"
+#include "DeprecatedString.h"
 #include "FloatRect.h"
 #include "IntRect.h"
 #include "MediaFeatureNames.h"
