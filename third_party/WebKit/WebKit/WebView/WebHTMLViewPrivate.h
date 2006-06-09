eb7afc6baf2e46bec5a27f91e860ae6a67c98e5a
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-09 23:37:58 +0000

    Rename updateFocusState to updateActiveState.
            * WebView/WebHTMLView.m:
            (-[WebHTMLView _updateActiveState]):
            (-[NSArray viewDidMoveToWindow]):
            (-[NSArray windowDidBecomeKey:]):
            (-[NSArray windowDidResignKey:]):
            (-[NSArray becomeFirstResponder]):
            (-[NSArray resignFirstResponder]):
            (-[WebHTMLView _formControlIsBecomingFirstResponder:]):
            (-[WebHTMLView _formControlIsResigningFirstResponder:]):
            * WebView/WebHTMLViewPrivate.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebHTMLViewPrivate.h
+++ third_party/WebKit/WebKit/WebView/WebHTMLViewPrivate.h
@@ -109,7 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)_removeHighlighterOfType:(NSString*)type;
 
 // SPI for DumpRenderTree
-- (void)_updateFocusState;
+- (void)_updateActiveState;
 
 // These methods might end up moving into a protocol, so different document types can specify
 // whether or not they implement the protocol.
