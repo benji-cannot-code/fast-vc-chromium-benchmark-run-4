3d452906cff43922f4a8a6f55c403d4c3970d590
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-10 04:34:00 +0000

    - Build fix after last JSC check-in (oops!)
            * bridge/mac/WebCoreScriptDebugger.mm:
            (-[WebCoreScriptCallFrame scopeChain]):
            (-[WebCoreScriptCallFrame functionName]):
            (-[WebCoreScriptCallFrame evaluateWebScript:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/mac/WebCoreScriptDebugger.mm
+++ third_party/WebKit/WebCore/bridge/mac/WebCoreScriptDebugger.mm
@@ -258,7 +258,7 @@ class WebCoreScriptDebuggerImp : public KJS::Debugger {
 
 - (NSArray *)scopeChain
 {
-    ContextImp *context = _state->context().imp();
+    Context* context = _state->context();
 
     if (!context) {  // global frame
         return [NSArray arrayWithObject:_globalObj];
@@ -283,7 +283,7 @@ class WebCoreScriptDebuggerImp : public KJS::Debugger {
 
 - (NSString *)functionName
 {
-    ContextImp *context = _state->context().imp();
+    Context* context = _state->context();
 
     if (context) {
         FunctionImp *func = context->function();
@@ -321,7 +321,7 @@ class WebCoreScriptDebuggerImp : public KJS::Debugger {
 
     // find "eval"
     JSObject *eval = NULL;
-    if (state->context().imp()) {  // "eval" won't work without context (i.e. at global scope)
+    if (state->context()) {  // "eval" won't work without context (i.e. at global scope)
         JSValue *v = globObj->get(state, "eval");
         if (v->isObject() && static_cast<JSObject *>(v)->implementsCall())
             eval = static_cast<JSObject *>(v);
