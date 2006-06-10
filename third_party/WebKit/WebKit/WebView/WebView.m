742ebdfea3be4bb02f97923ac463bd4b0a188755
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-10 13:38:38 +0000

    <http://bugzilla.opendarwin.org/show_bug.cgi?id=9384>         WebView's initWithCoder: method does not set useBackForwardList         correctly
            Reviewed by John Sullivan.
    
            * WebView/WebView.m:
            (-[WebView initWithCoder:]):
            Make sure that the function variable useBackForwardList is correctly
            set, so that the copy in the _private ivar is set.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14804 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebView.m
+++ third_party/WebKit/WebKit/WebView/WebView.m
@@ -1639,7 +1639,7 @@ NS_DURING
         frameName = [decoder decodeObjectForKey:@"FrameName"];
         groupName = [decoder decodeObjectForKey:@"GroupName"];
         preferences = [decoder decodeObjectForKey:@"Preferences"];
-	result->_private->useBackForwardList = [decoder decodeBoolForKey:@"UseBackForwardList"];
+	useBackForwardList = [decoder decodeBoolForKey:@"UseBackForwardList"];
     } else {
         int version;
         [decoder decodeValueOfObjCType:@encode(int) at:&version];
