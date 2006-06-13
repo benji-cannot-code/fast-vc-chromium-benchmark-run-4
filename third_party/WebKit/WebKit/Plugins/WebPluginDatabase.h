7e994d6ccee559a13432b0344dc91c22996b1e1a
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-13 13:37:18 +0000

    WebCore:
    2006-06-13  Anders Carlsson  <acarlsson@apple.com>
    
            Reviewed by Geoff.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=9406
            REGRESSION: fix for bug 9390 broke two layout tests
            
            * loader/PluginDocument.cpp:
            (WebCore::PluginTokenizer::writeRawData):
            Call finished() after setting up the document structure so we'll emit onload events.
    
    WebKit:
    
    2006-06-13  Anders Carlsson  <acarlsson@apple.com>
    
            Reviewed by Geoff.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=9406
            REGRESSION: fix for bug 9390 broke two layout tests
    
            * Plugins/WebPluginDatabase.h:
            (-[WebPluginDatabase isMIMETypeRegistered:]):
            Add new function isMIMETypeRegistered which returns whether a given MIME type has a 
            plugin registered. 
    
            * Plugins/WebPluginDatabase.m:
            (-[WebPluginDatabase init]):        
            Init set of registered MIME types.
            
            (-[WebPluginDatabase refresh]):
            Add and remove MIME types from the set of registered MIME types when registering and unregistering
            plugin MIME types.
            
            (-[WebPluginDatabase dealloc]):
            Release set of registered MIME types.
            
            * WebCoreSupport/WebFrameBridge.m:
            (-[WebFrameBridge determineObjectFromMIMEType:URL:]):
            Use isMIMETypeRegistered here.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins/WebPluginDatabase.h
+++ third_party/WebKit/WebKit/Plugins/WebPluginDatabase.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface WebPluginDatabase : NSObject
 {
     NSMutableSet *plugins;
+    NSMutableSet *registeredMIMETypes;
 }
 
 + (WebPluginDatabase *)installedPlugins;
@@ -41,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Plug-ins are returned in this order: New plug-in (WBPL), Mach-O Netscape, CFM Netscape
 - (WebBasePluginPackage *)pluginForMIMEType:(NSString *)mimeType;
 - (WebBasePluginPackage *)pluginForExtension:(NSString *)extension;
+
+- (BOOL)isMIMETypeRegistered:(NSString *)MIMEType;
 
 - (NSArray *)plugins;
 
