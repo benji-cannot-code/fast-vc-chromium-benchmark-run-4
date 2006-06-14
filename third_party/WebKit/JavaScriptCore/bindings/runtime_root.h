a4eb77bd5667831883bb3a8d0a9cc52397ead4ec
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-14 21:45:06 +0000

    Reviewed by Beth.
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=9438
            Someone broke ToT: cannot build
    
            * JavaScriptCore.xcodeproj/project.pbxproj:
            * bindings/runtime_root.h: Changed "Interpreter.h" to "interpreter.h"
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/runtime_root.h
+++ third_party/WebKit/JavaScriptCore/bindings/runtime_root.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef RUNTIME_ROOT_H_
 #define RUNTIME_ROOT_H_
 
-#include "Interpreter.h"
+#include "interpreter.h"
 #include "jni_jsobject.h"
 #include "protect.h"
 
