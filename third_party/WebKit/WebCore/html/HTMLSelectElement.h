8acd6f02faf69921ada89f96d5ccb06abb6834b1
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-17 15:37:58 +0000

    LayoutTests:
            Reviewed by Darin.
    
            Test cases for
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=6282>
    
            * fast/dom/select-selectedIndex-multiple.html: Added.
            * fast/dom/select-selectedIndex.html: Added.
    
    WebCore:
    
            Reviewed by Darin.
    
            Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=6282:
            Adding new Option with new Option(text, value, defaultSelected, selected) fails to update selectedIndex
    
            Update selectedIndex when a new option is added using javascript.
    
            * bindings/js/kjs_html.cpp:
            (KJS::JSHTMLSelectCollection::put):
            * html/HTMLSelectElement.cpp:
            (WebCore::HTMLSelectElement::setSelectedIndex):
            (WebCore::HTMLSelectElement::setOption):
            (WebCore::HTMLSelectElement::setLength):
            * html/HTMLSelectElement.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14898 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLSelectElement.h
+++ third_party/WebKit/WebCore/html/HTMLSelectElement.h
@@ -52,7 +52,7 @@ public:
     virtual void recalcStyle(StyleChange);
 
     int selectedIndex() const;
-    void setSelectedIndex(int index);
+    void setSelectedIndex(int index, bool = true);
 
     virtual bool isEnumeratable() const { return true; }
 
@@ -110,6 +110,9 @@ public:
     void setMultiple(bool);
 
     void setSize(int);
+
+    void setOption(unsigned index, HTMLOptionElement*, ExceptionCode&);
+    void setLength(unsigned, ExceptionCode&);
 
     virtual Node* namedItem(const String &name, bool caseSensitive = true);
 
