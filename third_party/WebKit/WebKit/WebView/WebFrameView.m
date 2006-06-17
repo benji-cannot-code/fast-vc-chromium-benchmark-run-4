d4f2a6d17011d7db5eaf162ce02aa8c87af91c2f
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-17 15:29:59 +0000

    WebCore:
            Reviewed by Darin.
    
            - test for http://bugzilla.opendarwin.org/show_bug.cgi?id=9466
              Assertion failure when dragging an image from the document into Safari's address bar
    
            * manual-tests/reset-initiatedDrag.html: Added.
    
    WebKit:
    
            Reviewed by Darin.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=9466
              Assertion failure when dragging an image from the document into Safari's address bar
    
            * WebView/WebFrameView.m:
            (-[WebFrameView _setDocumentView:]): Reset the WebView's initiatedDrag flag
            when the document view is changed.
            * WebView/WebHTMLView.m:
            (-[WebHTMLView draggedImage:endedAt:operation:]): Changed the ASSERT to allow
            for drags that end after the view has been removed from the WebView.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14897 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebFrameView.m
+++ third_party/WebKit/WebKit/WebView/WebFrameView.m
@@ -180,6 +180,7 @@ enum {
 - (void)_setDocumentView:(NSView <WebDocumentView> *)view
 {
     WebDynamicScrollBarsView *sv = (WebDynamicScrollBarsView *)[self _scrollView];
+    [[self _webView] _setInitiatedDrag:NO];
     
     [sv setSuppressLayout:YES];
     
