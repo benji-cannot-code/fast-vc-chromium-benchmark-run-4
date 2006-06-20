98a6be2e6904ae68aa0004d37a8780ce16bb77d8
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-20 21:04:08 +0000

    Reviewed by Tim Hatcher.
            * WebView/WebMainResourceLoader.m: Added missing header to fix build on Leopard.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14925 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebMainResourceLoader.m
+++ third_party/WebKit/WebKit/WebView/WebMainResourceLoader.m
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKitNSStringExtras.h>
 #import <WebKit/WebNSObjectExtras.h>
 #import <WebKit/WebNSURLExtras.h>
+#import <WebKit/WebViewInternal.h>
 
 // FIXME: More that is in common with WebSubresourceLoader should move up into WebLoader.
 
