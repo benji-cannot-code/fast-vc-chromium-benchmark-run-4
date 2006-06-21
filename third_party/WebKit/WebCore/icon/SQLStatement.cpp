f00d6334632edd50c610b2e5e4b24bb822f5e6a0
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-21 01:42:48 +0000

    Reviewed by Maciej
            Fixed capitalization mistake that I made, someone else fixed, then I accidentally made it go back to the old way
    
            * icon/SQLStatement.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14934 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/icon/SQLStatement.cpp
+++ third_party/WebKit/WebCore/icon/SQLStatement.cpp
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "SQLDatabase.h"
 
-#include <wtf/assertions.h>
+#include <wtf/Assertions.h>
 #include "Logging.h"
 
 namespace WebCore {
