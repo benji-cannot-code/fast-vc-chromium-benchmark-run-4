9fb43cdd17182a88837092321c1722399647afbe
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-22 10:38:20 +0000

    JavaScriptCore:
            Build fix.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=9539
            Another case error preventing build
    
            * API/APICast.h: Changed "UString.h" to "ustring.h".
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14969 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/APICast.h
+++ third_party/WebKit/JavaScriptCore/API/APICast.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define APICast_h
 
 #include "JSValueRef.h"
-#include "UString.h"
+#include "ustring.h"
 
 namespace KJS {
     class ExecState;
