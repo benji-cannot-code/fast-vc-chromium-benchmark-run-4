7e3e5b87b311761bbf9f8df90c2552de4cf714ee
tomernic <tomernic@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-22 19:01:39 +0000

    Reviewed by NOBODY (build fix)
            * WebView/WebFrameLoader.m:
            Import JavaScriptCore/Assertions.h instead of WebKit/WebAssertions.h (which no longer exists)
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14977 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebFrameLoader.m
+++ third_party/WebKit/WebKit/WebView/WebFrameLoader.m
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebKit/WebFrameLoader.h>
 
-#import <WebKit/WebAssertions.h>
+#import <JavaScriptCore/Assertions.h>
 #import <WebKit/WebDataSource.h>
 #import <WebKit/WebFrameInternal.h>
 #import <WebKit/WebIconLoader.h>
