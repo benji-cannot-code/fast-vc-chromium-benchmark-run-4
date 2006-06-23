3c9c9334ccec8a2965f8fa332e12fef8b90aa1cd
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-23 04:07:18 +0000

    LayoutTests:
            Reviewed by Hyatt.  Patch by Mitz.
    
            - test and updated results for http://bugzilla.opendarwin.org/show_bug.cgi?id=9497
              REGRESSION: Incomplete repainting when scrolling thumbnails on Flickr
    
            * fast/repaint/clipped-relative-expected.checksum: Added.
            * fast/repaint/clipped-relative-expected.png: Added.
            * fast/repaint/clipped-relative-expected.txt: Added.
            * fast/repaint/clipped-relative.html: Added.
            * fast/repaint/containing-block-position-change-expected.checksum: Updated.
            * fast/repaint/containing-block-position-change-expected.png: Updated.
    
    WebCore:
    
            Reviewed by Hyatt.  Patch by Mitz.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=9497
              REGRESSION: Incomplete repainting when scrolling thumbnails on Flickr
    
            Test: fast/repaint/clipped-relative.html
    
            * page/FrameView.cpp:
            (WebCore::FrameView::layout):
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::removePositionedObjects): Removed call to computeRepaintRects().
            Previously, it was needed to update the cached rects before RenderBox::setStyle()
            called updateLayerPositions().
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::setStyle): Changed to call updateLayerPositions() for a newly-
            created layer only if it does not need layout. Otherwise,  updateLayerPositions() will
            be called after layout. This saves a repaint in some cases.
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::checkForRepaintOnResize): Renamed computeRepaintRects() to
            this and deleted the code that cached the absolute position and repaint rects.
            (WebCore::RenderLayer::updateLayerPositions): Added code to cache the absolute
            position and repaint rects right after updating the layer's position.
            * rendering/RenderLayer.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@14983 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBlock.cpp
@@ -1781,10 +1781,8 @@ void RenderBlock::removePositionedObjects(RenderBlock* o)
     DeprecatedPtrListIterator<RenderObject> it(*m_positionedObjects);
     while (it.current()) {
         if (!o || it.current()->hasAncestor(o)) {
-            if (o) {
+            if (o)
                 it.current()->setChildNeedsLayout(true, false);
-                it.current()->layer()->computeRepaintRects();
-            }
             m_positionedObjects->removeRef(it.current());
         } else
             ++it;
