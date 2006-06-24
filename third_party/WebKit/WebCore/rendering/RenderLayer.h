827e9f082f525e992983098ac8d23873e103a7eb
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-24 16:02:10 +0000

    2006-06-24  Mitz Pettel  <opendarwin.org@mitzpettel.com>
            Reviewed by Darin, landed by ap.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=9547
              Resize corner does not track the mouse accurately
    
            Test: fast/css/resize-corner-tracking.html
    
            * page/FrameView.cpp:
            (WebCore::FrameView::handleMousePressEvent): Store the offset from the layer's
            resizing corner where dragging has started.
            (WebCore::FrameView::handleMouseMoveEvent): Pass the stored offset to the
            layer's resize method.
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::resize): Added an offset parameter, which is added to
            the mouse location.
            For objects that may have intrinsic margins applied to them,
            added code to lift the margins into the object's style in order to keep them
            constant.
            Changed to take the content size as the base size if the box-sizing property
            is content-box.
            (WebCore::RenderLayer::offsetFromResizeCorner): Added. Returns the offset of the
            given point from the corner that tracks the mouse when resizing.
            * rendering/RenderLayer.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15014 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.h
+++ third_party/WebKit/WebCore/rendering/RenderLayer.h
@@ -242,6 +242,7 @@ public:
     void positionScrollbars(const IntRect& absBounds);
     void positionResizeControl();
     bool isPointInResizeControl(const IntPoint&);
+    IntSize offsetFromResizeCorner(const IntPoint&) const;
     void paintScrollbars(GraphicsContext*, const IntRect& damageRect);
     void paintResizeControl(GraphicsContext*);
     void updateScrollInfoAfterLayout();
@@ -251,7 +252,7 @@ public:
     bool shouldAutoscroll();
     IntRect resizeControlRect() { return m_resizeControlRect; }
     void setResizeControlRect(const IntRect& r) { m_resizeControlRect = r; }
-    void resize(const PlatformMouseEvent&);
+    void resize(const PlatformMouseEvent&, const IntSize&);
     bool inResizeMode() const { return m_inResizeMode; }
     void setInResizeMode(bool b) { m_inResizeMode = b; }
     
