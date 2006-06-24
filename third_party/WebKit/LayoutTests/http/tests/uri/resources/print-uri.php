3ac45e30d6b3d304f183858b29e349ddb0656bc7
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-24 15:00:16 +0000

    Reviewed by Darin.
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=7461
              Always encode the path part of an URI as UTF-8
    
            Test: http/tests/uri/utf8-path.html
    
            * platform/KURL.cpp:
            (encodeRelativeString): Always set pathEncoding to UTF-8.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15010 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/uri/resources/print-uri.php
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+    header("Content-Type: text/javascript");
+    header("Expires: Thu, 01 Dec 2003 16:00:00 GMT\n");
+    header("Cache-Control: no-store, no-cache, must-revalidate\n");
+    header("Pragma: no-cache\n");
+    echo "document.write('" . $_SERVER['REQUEST_URI'] . "');";
+?>
