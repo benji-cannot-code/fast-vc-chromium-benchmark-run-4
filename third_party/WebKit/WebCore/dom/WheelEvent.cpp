384fc4b3a5b59eb24ad3c71d833775fe9f21495d
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-25 20:37:37 +0000

    WebCore:
            Reviewed by Darin.
    
            Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=8707:
            event.clientX and event.clientY should be relative to the viewport, not the canvas
    
            Make sure clientX and clientY take into account the scroll offsets
            of the view.
    
            * dom/EventTargetNode.cpp:
            (WebCore::EventTargetNode::dispatchMouseEvent):
            * dom/MouseEvent.cpp:
            (WebCore::MouseEvent::MouseEvent):
            * dom/MouseEvent.h:
            * dom/MouseRelatedEvent.cpp:
            (WebCore::MouseRelatedEvent::MouseRelatedEvent):
            (WebCore::MouseRelatedEvent::initCoordinates):
            * dom/MouseRelatedEvent.h:
            * dom/WheelEvent.cpp:
            (WebCore::WheelEvent::WheelEvent):
            * page/FrameView.cpp:
            (WebCore::FrameView::dispatchDragEvent):
    
    LayoutTests:
    
            Reviewed by Darin.
    
            Test cases for
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=8707>
            event.clientX and event.clientY should be relative to the viewport, not the canvas
    
            * fast/dom/window-xy-properties-expected.txt: Added.
            * fast/dom/window-xy-properties.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15032 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/WheelEvent.cpp
+++ third_party/WebKit/WebCore/dom/WheelEvent.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "WheelEvent.h"
+#include "Frame.h"
 
 #include "EventNames.h"
 
@@ -43,6 +44,8 @@ WheelEvent::WheelEvent(bool horizontal, int wheelDelta, AbstractView* view,
                        bool ctrlKey, bool altKey, bool shiftKey, bool metaKey)
     : MouseRelatedEvent(horizontal ? khtmlHorizontalmousewheelEvent : mousewheelEvent,
                         true, true, view, 0, screenX, screenY, clientX, clientY, 
+                        clientX - (view && view->frame() && view->frame()->view()) ? view->frame()->view()->contentsX() : 0,
+                        clientY - (view && view->frame() && view->frame()->view()) ? view->frame()->view()->contentsY() : 0,
                         ctrlKey, altKey, shiftKey, metaKey)
     , m_horizontal(horizontal)
     , m_wheelDelta(wheelDelta)
