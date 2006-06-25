2e8a728d553e0253f68d69a4174f37e454dad458
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-25 20:32:37 +0000

    Leak fix
    git-svn-id: svn://svn.chromium.org/blink/trunk@15031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/DefaultDelegates/WebScriptDebugServer.m
+++ third_party/WebKit/WebKit/DefaultDelegates/WebScriptDebugServer.m
@@ -75,6 +75,7 @@ static unsigned listenerCount = 0;
             [[NSBundle mainBundle] bundleIdentifier], WebScriptDebugServerProcessBundleIdentifierKey,
             [NSNumber numberWithInt:[processInfo processIdentifier]], WebScriptDebugServerProcessIdentifierKey, nil];
         [[NSDistributedNotificationCenter defaultCenter] postNotificationName:WebScriptDebugServerDidLoadNotification object:serverName userInfo:info];
+        [info release];
     } else {
         [serverConnection release];
         serverConnection = nil;
