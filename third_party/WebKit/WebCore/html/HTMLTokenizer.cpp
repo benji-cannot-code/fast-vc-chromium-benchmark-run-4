dc18afe2205134e8549d6b049fa86f1562bd78e5
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-28 03:32:56 +0000

    LayoutTests:
            Reviewed by Darin.
    
            - test for http://bugzilla.opendarwin.org/show_bug.cgi?id=9317
              REGRESSION: crash in HTML tokenizer at Japanese Apple support page
    
            * fast/tokenizer/write-unclosed-script-expected.txt: Added.
            * fast/tokenizer/write-unclosed-script.html: Added.
    
    WebCore:
    
            Reviewed by Maciej and Darin.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=9317
              REGRESSION: crash in HTML tokenizer at Japanese Apple support page
    
            Test: fast/tokenizer/write-unclosed-script.html
    
            * html/HTMLTokenizer.cpp:
            (WebCore::HTMLTokenizer::scriptHandler): Changed to reset the inScript flag
            and scriptCodeSize before possible script execution, but not afterwards.
            This way, if script execution write()s a <script> tag without closing it,
            the tokenizer is left in inScript state. Added code to set the requestingScript
            flag around the request for the cached script, to let notifiyFinished() know
            that it's being called under scriptHandler().
            (WebCore::HTMLTokenizer::scriptExecution): Removed code that saved and restored
            the inScript flag. This function is always entered now with inScript being false.
            (WebCore::HTMLTokenizer::notifyFinished): Changed to use the new requestingScript
            state flag instead of the inScript flag, which is always false now when entering
            this function.
            * html/HTMLTokenizer.h:
            Added the requestingScript state bit, used to tell notifyFinished() that it
            is being called under scriptHandler() (which happens when the script is already
            in cache).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15075 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
+++ third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
@@ -391,6 +391,9 @@ HTMLTokenizer::State HTMLTokenizer::scriptHandler(State state)
     SegmentedString *savedPrependingSrc = currentPrependingSrc;
     SegmentedString prependingSrc;
     currentPrependingSrc = &prependingSrc;
+    state.setInScript(false);
+    scriptCodeSize = scriptCodeResync = 0;
+
     if (!parser->skipMode() && !followingFrameset) {
         if (cs) {
             if (savedPrependingSrc)
@@ -398,13 +401,14 @@ HTMLTokenizer::State HTMLTokenizer::scriptHandler(State state)
             else
                 pendingSrc.prepend(src);
             setSrc(SegmentedString());
-            scriptCodeSize = scriptCodeResync = 0;
 
             // the ref() call below may call notifyFinished if the script is already in cache,
             // and that mucks with the state directly, so we must write it back to the object.
+            state.setRequestingScript(true);
             m_state = state;
             cs->ref(this);
             state = m_state;
+            state.setRequestingScript(false);
             // will be 0 if script was already loaded and ref() executed it
             if (!pendingScripts.isEmpty())
                 state.setLoadingExtScript(true);
@@ -415,13 +419,9 @@ HTMLTokenizer::State HTMLTokenizer::scriptHandler(State state)
             else
                 prependingSrc = src;
             setSrc(SegmentedString());
-            scriptCodeSize = scriptCodeResync = 0;
             state = scriptExecution(exScript, state, DeprecatedString::null, scriptStartLineno);
         }
     }
-
-    state.setInScript(false);
-    scriptCodeSize = scriptCodeResync = 0;
 
     if (!m_executingScript && !state.loadingExtScript()) {
         src.append(pendingSrc);
@@ -456,9 +456,7 @@ HTMLTokenizer::State HTMLTokenizer::scriptExecution(const DeprecatedString& str,
 {
     if (m_fragment || !m_doc->frame())
         return state;
-    bool oldscript = state.inScript();
     m_executingScript++;
-    state.setInScript(false);
     DeprecatedString url = scriptURL.isNull() ? m_doc->frame()->document()->URL() : scriptURL;
 
     SegmentedString *savedPrependingSrc = currentPrependingSrc;
@@ -482,7 +480,6 @@ HTMLTokenizer::State HTMLTokenizer::scriptExecution(const DeprecatedString& str,
 #endif
     
     m_executingScript--;
-    state.setInScript(oldscript);
 
     if (!m_executingScript && !state.loadingExtScript()) {
         src.append(pendingSrc);
@@ -1703,10 +1700,10 @@ void HTMLTokenizer::notifyFinished(CachedObject*)
 #endif
         }
 
-        // 'inScript' is true when we are called synchronously from
-        // parseScript(). In that case parseScript() will take care
-        // of 'scriptOutput'.
-        if (!m_state.inScript()) {
+        // 'requestingScript' is true when we are called synchronously from
+        // scriptHandler(). In that case scriptHandler() will take care
+        // of pendingSrc.
+        if (!m_state.requestingScript()) {
             SegmentedString rest = pendingSrc;
             pendingSrc.clear();
             write(rest, false);
