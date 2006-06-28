db6c6d1fd95be02372a5d504ded15bbc2eaef2c8
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-28 15:42:13 +0000

    LayoutTests:
            Reviewed by Darin and Hyatt.
    
            - test for http://bugzilla.opendarwin.org/show_bug.cgi?id=9561
              REGRESSION: Content property on :before of button causes hang on click or hover
    
            * fast/forms/button-generated-content-expected.checksum: Added.
            * fast/forms/button-generated-content-expected.png: Added.
            * fast/forms/button-generated-content-expected.txt: Added.
            * fast/forms/button-generated-content.html: Added.
    
    WebCore:
    
            Reviewed by Darin and Hyatt.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=9561
              REGRESSION: Content property on :before of button causes hang on click or hover
    
            Test: fast/forms/button-generated-content.html
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::setStyle):
            * rendering/RenderButton.cpp:
            (WebCore::RenderButton::updatePseudoChild): Added. Calls updatePseudoChildForObject()
            on the button itself or on its inner div, if it has one.
            * rendering/RenderButton.h:
            (WebCore::RenderButton::createsAnonymousWrapper): Added. Returns true.
            * rendering/RenderContainer.cpp:
            (WebCore::RenderContainer::pseudoChild): Added. Returns the child containing
            generated content of the requested type if there is one, or otherwise the
            child before/after which such generated content should come.
            (WebCore::RenderContainer::updatePseudoChild): Removed the child parameter and moved the guts
            of this method into updatePseudoChildForObject, which this method calls unless the object
            is an anonymous wrapper.
            (WebCore::RenderContainer::updatePseudoChildForObject): Added. Does the what
            updatePseudoChildForObject did previously, but fetches the pseudo style from
            the styledObject parameter.
            * rendering/RenderContainer.h:
            * rendering/RenderInline.cpp:
            (WebCore::RenderInline::setStyle):
            (WebCore::RenderInline::addChildToFlow):
            (WebCore::RenderInline::splitInlines):
            (WebCore::RenderInline::splitFlow):
            * rendering/RenderObject.h:
            (WebCore::RenderObject::createsAnonymousWrapper): Added. This method replaces
            allowsReusingAnonymousChild and returns the opposite of what that method used to
            return. Returns true if the object creates an anonymous child to contain all its
            other children.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderButton.cpp
+++ third_party/WebKit/WebCore/rendering/RenderButton.cpp
@@ -97,6 +97,14 @@ void RenderButton::updateFromElement()
     }
 }
 
+void RenderButton::updatePseudoChild(RenderStyle::PseudoId type)
+{
+    if (m_inner)
+        m_inner->updatePseudoChildForObject(type, this);
+    else
+        updatePseudoChildForObject(type, this);
+}
+
 void RenderButton::paintObject(PaintInfo& i, int _tx, int _ty)
 {
     // Push a clip.
