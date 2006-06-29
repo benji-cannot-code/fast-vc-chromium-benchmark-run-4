53032af9734a46f703b5d4fca30373051d6be77f
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-29 00:32:55 +0000

    WebCore:
            Add support for custom highlighting to paint at the root line box level
            (and to inflate the box it uses so that it can go outside the bounds of
            the line if necessary).
    
            Reviewed by harrison
    
            * bridge/mac/FrameMac.h:
            * bridge/mac/FrameMac.mm:
            (WebCore::FrameMac::customHighlightLineRect):
            (WebCore::FrameMac::paintCustomHighlight):
            * bridge/mac/WebCoreFrameBridge.h:
            * rendering/InlineTextBox.cpp:
            (WebCore::InlineTextBox::paintCustomHighlight):
            * rendering/RootInlineBox.cpp:
            (WebCore::RootInlineBox::paintCustomHighlight):
            (WebCore::RootInlineBox::paint):
            * rendering/RootInlineBox.h:
    
    WebKit:
    
            Fix custom highlighting so that you can paint the entire line (and go
            outside the bounds of the line).
    
            Reviewed by harrison
    
            * WebCoreSupport/WebFrameBridge.m:
            (-[WebFrameBridge customHighlightRect:forLine:]):
            (-[WebFrameBridge paintCustomHighlight:forBox:onLine:behindText:entireLine:]):
            * WebKit.xcodeproj/project.pbxproj:
            * WebView/WebHTMLViewPrivate.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15090 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebHTMLViewPrivate.h
+++ third_party/WebKit/WebKit/WebView/WebHTMLViewPrivate.h
@@ -38,7 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class WebPluginController;
  
 @protocol WebHTMLHighlighter
-- (void)paintHighlightForBox:(NSRect)boxRect onLine:(NSRect)lineRect behindText:(BOOL)text;
+- (NSRect)highlightRectForLine:(NSRect)lineRect;
+- (void)paintHighlightForBox:(NSRect)boxRect onLine:(NSRect)lineRect behindText:(BOOL)text entireLine:(BOOL)line;
 @end
 
 @interface WebHTMLView (WebPrivate)
