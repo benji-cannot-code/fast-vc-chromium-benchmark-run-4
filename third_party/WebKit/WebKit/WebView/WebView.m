8917dd61d1b2089cb00d4649aded510afdc60fae
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-30 18:56:59 +0000

    Reviewed by Darin.
            Only enable shouldCloseWithWindow when ObjC GC is enabled.
            This maintains backwards compatibility with applications
            that expect a WebView to be usable after the window closes.
    
            * WebView/WebView.m:
            (-[WebViewPrivate init]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebView.m
+++ third_party/WebKit/WebKit/WebView/WebView.m
@@ -379,7 +379,7 @@ NSString *_WebMainFrameDocumentKey =    @"mainFrameDocument";
     settings = [[WebCoreSettings alloc] init];
     dashboardBehaviorAllowWheelScrolling = YES;
     tabKeyCyclesThroughElements = YES;
-    shouldCloseWithWindow = YES;
+    shouldCloseWithWindow = objc_collecting_enabled();
 
     return self;
 }
