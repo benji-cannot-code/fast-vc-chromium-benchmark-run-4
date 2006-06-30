9049f92e6a5672b068b32628ce46d89f48e03379
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-30 22:17:51 +0000

    LayoutTests:
            Reviewed by levi
    
            * editing/pasteboard/pasting-object-expected.checksum: Added.
            * editing/pasteboard/pasting-object-expected.png: Added.
            * editing/pasteboard/pasting-object-expected.txt: Added.
            * editing/pasteboard/pasting-object.html: Added.
    
    WebCore:
    
            Reviewed by levi
    
            * editing/htmlediting.cpp:
            (WebCore::canHaveChildrenForEditing): Don't insert children into
            object tags while editing.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15113 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/editing/htmlediting.cpp
@@ -84,6 +84,7 @@ bool canHaveChildrenForEditing(const Node* node)
            !node->hasTagName(brTag) &&
            !node->hasTagName(imgTag) &&
            !node->hasTagName(buttonTag) &&
+           !node->hasTagName(objectTag) &&
            !node->isTextNode();
 }
 
