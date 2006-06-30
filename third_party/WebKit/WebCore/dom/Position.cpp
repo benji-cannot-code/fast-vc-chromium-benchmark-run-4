5e14fcf2d49c0459acf7951c097263717d69dd47
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-06-30 19:43:03 +0000

    LayoutTests:
            Reviewed by harrison
            
            Added:
            * editing/selection/skip-non-editable-1-expected.txt: Added.
            * editing/selection/skip-non-editable-1.html: Added.
            * editing/selection/skip-non-editable-2-expected.txt: Added.
            * editing/selection/skip-non-editable-2.html: Added.
    
            Equivalent render tree:
            * editing/execCommand/format-block-with-braces-expected.txt:
            
            Fixes:
            * editing/selection/iframe-expected.checksum:
            * editing/selection/iframe-expected.png:
            * editing/selection/iframe-expected.txt:
            * editing/selection/inline-table-expected.checksum:
            * editing/selection/inline-table-expected.png:
            * editing/selection/inline-table-expected.txt:
            * editing/selection/move-between-blocks-no-001-expected.checksum:
            * editing/selection/move-between-blocks-no-001-expected.png:
            * editing/selection/move-between-blocks-no-001-expected.txt:
            * editing/selection/select-box-expected.checksum:
            * editing/selection/select-box-expected.png:
            * editing/selection/select-box-expected.txt:
    
    WebCore:
    
            Reviewed by harrison
            
            <rdar://problem/4598309
            Caret gets stuck when it hits non editable content within an editable body
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=9510>
            Caret will jump out of an editable region
            
            * dom/Position.cpp:
            (WebCore::Position::upstream): Don't move into a new editable region so that 
            candidates that that straddle an editable/non-editable boundary will 
            correspond to different VisiblePositions.
            (WebCore::Position::downstream): Ditto.
            * editing/SelectionController.cpp:
            (WebCore::SelectionController::modifyMovingRightForward): Use VisiblePosition::next(true)
            (WebCore::SelectionController::modifyMovingLeftBackward): Use VisiblePosition::previous(true)
            * editing/VisiblePosition.cpp:
            (WebCore::VisiblePosition::next): Added code to jump to the next editable region as long
            as it has the same highest editable root.
            (WebCore::VisiblePosition::previous): Ditto.
            * editing/VisiblePosition.h:
            * editing/htmlediting.cpp:
            (WebCore::highestEditableRoot): Added, returns the highest editable root of a node if that
            node is in editable content, otherwise 0.
            * editing/htmlediting.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15112 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Position.cpp
+++ third_party/WebKit/WebCore/dom/Position.cpp
@@ -288,9 +288,13 @@ Position Position::upstream() const
     Node *block = startNode->enclosingBlockFlowOrTableElement();
     Position lastVisible = *this;
     Position currentPos = start;
+    Node* originalRoot = node()->rootEditableElement();
     for (; !currentPos.atStart(); currentPos = currentPos.previous(UsingComposedCharacters)) {
         Node *currentNode = currentPos.node();
         int currentOffset = currentPos.offset();
+        
+        if (currentNode->rootEditableElement() != originalRoot)
+            break;
 
         // Don't enter a new enclosing block flow or table element.  There is code below that
         // terminates early if we're about to leave an enclosing block flow or table element.
@@ -362,9 +366,13 @@ Position Position::downstream() const
     Node *block = startNode->enclosingBlockFlowOrTableElement();
     Position lastVisible = *this;
     Position currentPos = start;
+    Node* originalRoot = node()->rootEditableElement();
     for (; !currentPos.atEnd(); currentPos = currentPos.next(UsingComposedCharacters)) {   
         Node *currentNode = currentPos.node();
         int currentOffset = currentPos.offset();
+        
+        if (currentNode->rootEditableElement() != originalRoot)
+            break;
 
         // stop before going above the body, up into the head
         // return the last visible streamer position
