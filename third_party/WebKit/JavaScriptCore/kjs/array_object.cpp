bc393d3a1cd4f17362c8d51bbcd2f6ee2b55482e
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-01 05:02:23 +0000

    Reviewed and tweaked by Darin.
            - Compile fixes for wx port / gcc 4.0.2
    
            * kjs/array_object.cpp:
            Added missing headers. 
    
            * kjs/ExecState.h:
            gcc needs class prototypes before defining those classes as friend classes
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15125 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "array_object.lut.h"
 
 #include "error_object.h"
+#include "lookup.h"
 #include "operations.h"
 #include "reference_list.h"
 #include <wtf/HashSet.h>
--- third_party/WebKit/JavaScriptCore/kjs/interpreter.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/interpreter.cpp
@@ -57,8 +57,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <assert.h>
 #include <math.h>
-#include <stdio.h>
 #include <signal.h>
+#include <stdio.h>
 
 namespace KJS {
 
