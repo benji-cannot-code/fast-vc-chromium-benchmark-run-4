bf58e8b4b70d7c334bde599c2de26e9dab412314
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-02 02:50:01 +0000

    WebKit:
            Reviewed by NOBODY (fixed Tim's build fix).
    
            * WebView/WebView.m: Added back missing '/' at the beginning of the file.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15131 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebView.m
+++ third_party/WebKit/WebKit/WebView/WebView.m
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-*
+/*
  * Copyright (C) 2005, 2006 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
