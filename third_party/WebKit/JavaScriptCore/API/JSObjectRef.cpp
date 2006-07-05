9a289e57195a6bb4b171e41aff28b9a451927f42
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-05 18:07:57 +0000

    RS by Beth.
            Moved some code around for more logical file separation.
    
            * API/JSBase.h:
            * API/JSContextRef.h:
            * API/JSObjectRef.cpp:
            * API/JSValueRef.cpp:
            (JSValueToObject):
            * API/JSValueRef.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSObjectRef.cpp
+++ third_party/WebKit/JavaScriptCore/API/JSObjectRef.cpp
@@ -41,20 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace KJS;
 
-JSObjectRef JSValueToObject(JSContextRef context, JSValueRef value)
-{
-    JSLock lock;
-    ExecState* exec = toJS(context);
-    JSValue* jsValue = toJS(value);
-
-    JSObjectRef objectRef = toRef(jsValue->toObject(exec));
-    if (exec->hadException()) {
-        exec->clearException();
-        objectRef = NULL;
-    }
-    return objectRef;
-}    
-
 JSObjectRef JSObjectMake(JSContextRef context, JSClassRef jsClass, JSObjectRef prototype)
 {
     JSLock lock;
