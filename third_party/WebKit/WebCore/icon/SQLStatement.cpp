6cd78d2a1282d5bf754f92b0c40a9aeb77a868bc
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-05 23:45:45 +0000

    Reviewed by Tim Omernick.
            IconDatabase now respects private browsing via the use of in memory, temporary tables
    
            * bridge/mac/WebCoreIconDatabaseBridge.mm:
            (-[WebCoreIconDatabaseBridge iconForPageURL:withSize:]):
            (-[WebCoreIconDatabaseBridge iconURLForPageURL:]): Renamed WebCore::IconDatabase methods, changes reflected here
    
            * icon/IconDatabase.cpp:
            (WebCore::IconDatabase::clearDatabase):
            (WebCore::IconDatabase::deletePrivateTables):
            (WebCore::IconDatabase::imageDataForIconID):
            (WebCore::IconDatabase::imageDataForIconURL):
            (WebCore::IconDatabase::imageDataForPageURL):
            (WebCore::IconDatabase::setPrivateBrowsingEnabled):
            (WebCore::IconDatabase::iconForPageURL):
            (WebCore::IconDatabase::iconURLForPageURL):
            (WebCore::IconDatabase::setIconDataForIconURL):
            (WebCore::IconDatabase::performSetIconDataForIconID):
            (WebCore::IconDatabase::establishTemporaryIconIDForEscapedIconURL):
            (WebCore::IconDatabase::establishIconIDForEscapedIconURL):
            (WebCore::IconDatabase::setHaveNoIconForIconURL):
            (WebCore::IconDatabase::setIconURLForPageURL):
            (WebCore::IconDatabase::performSetIconURLForPageURL):
            (WebCore::IconDatabase::hasIconForIconURL):
            -Some style/comment cleanups, but mainly putting private browsing handling in place
    
            * icon/IconDatabase.h:  Some methods added, others renamed, some comments added
    
            * icon/SQLStatement.cpp:
            (WebCore::SQLStatement::getColumnName):
            (WebCore::SQLStatement::getColumnName16):
            (WebCore::SQLStatement::getColumnText):
            (WebCore::SQLStatement::getColumnText16):
            -The above 4 had changes to return String() for empty results instead of "" as it's less expensive;
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15175 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/icon/SQLStatement.cpp
+++ third_party/WebKit/WebCore/icon/SQLStatement.cpp
@@ -149,9 +149,9 @@ String SQLStatement::getColumnName(int col)
 {
     if (!m_statement)
         if (prepareAndStep() != SQLITE_ROW)
-            return "";
+            return String();
     if (columnCount() <= col)
-        return "";
+        return String();
         
     return String(sqlite3_column_name(m_statement, col));
 }
@@ -160,9 +160,9 @@ String SQLStatement::getColumnName16(int col)
 {
     if (!m_statement)
         if (prepareAndStep() != SQLITE_ROW)
-            return "";
+            return String();
     if (columnCount() <= col)
-        return "";
+        return String();
     return String((const UChar*)sqlite3_column_name16(m_statement, col));
 }
     
@@ -170,9 +170,9 @@ String SQLStatement::getColumnText(int col)
 {
     if (!m_statement)
         if (prepareAndStep() != SQLITE_ROW)
-            return "";
+            return String();
     if (columnCount() <= col)
-        return "";
+        return String();
     return String((const char*)sqlite3_column_text(m_statement, col));
 }
 
@@ -180,9 +180,9 @@ String SQLStatement::getColumnText16(int col)
 {
     if (!m_statement)
         if (prepareAndStep() != SQLITE_ROW)
-            return "";
+            return String();
     if (columnCount() <= col)
-        return "";
+        return String();
     return String((const UChar*)sqlite3_column_text16(m_statement, col));
 }
     
