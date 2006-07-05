a3fac40b2605c6d7b166e2475693758bcb02f3cf
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-05 23:27:20 +0000

    Reviewed by Darin and Adele.
            - fixed <rdar://problem/4609162> REGRESSION: When tabbing between links on a page, 
            the window doesn't scroll to link that's not in view
    
            * html/HTMLAnchorElement.cpp:
            * html/HTMLAnchorElement.h:
            removed HTMLAnchorElement::focus and HTMLAnchorElement::blur. 
            
            HTMLAnchorElement::focus just called document()->setFocusNode(). The scroll-to-reveal
            code used to be inside document()->setFocusNode(), but now it's in Element::focus, and this override
            was preventing that code from running.
            
            HTMLAnchorElement::blur was exactly the same implementation as Element::blur (and there aren't any
            other implementations of ::blur in the class hierarchy in between). This change has no practical
            effect, other than removing duplicate code.
            
            Adele and I think that it was an oversight not to remove these two methods on 12/20/05
            these methods were removed from HTMLButtonElementImpl and HTMLInputElementImpl.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15173 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLAnchorElement.h
+++ third_party/WebKit/WebCore/html/HTMLAnchorElement.h
@@ -86,9 +86,6 @@ public:
     String type() const;
     void setType(const String&);
 
-    void blur();
-    void focus();
-
     String hash() const;
     String host() const;
     String hostname() const;
