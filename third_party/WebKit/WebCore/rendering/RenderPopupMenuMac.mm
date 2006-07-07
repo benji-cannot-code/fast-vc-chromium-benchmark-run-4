a1a02d844b8b04da8f49993e1aa91b7e06a07f16
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-07 02:29:11 +0000

    Reviewed by Maciej.
            Fixed new popup control to open up when the spacebar and the up and down arrows are pressed.
    
            - Fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=9762
            [Native pop up] Highlighted item text is black instead of white
    
            * html/HTMLSelectElement.cpp:
            (WebCore::HTMLSelectElement::isKeyboardFocusable): Added.
            (WebCore::HTMLSelectElement::isMouseFocusable): Added.
            (WebCore::HTMLSelectElement::defaultEventHandler): Updated to showPopup for up and down arrow keys and spacebar.
            * html/HTMLSelectElement.h: (WebCore::HTMLSelectElement::shouldUseMenuList): Made this const.
            * rendering/RenderPopupMenu.cpp: (WebCore::RenderPopupMenu::populate): Added FIXME.
            * rendering/RenderPopupMenuMac.mm:
            (WebCore::RenderPopupMenuMac::showPopup): Updated to save the NSEvent that triggers the popup, so we can send
            fake mouseUp and keyUp events when the NSMenu returns.
            (WebCore::RenderPopupMenuMac::addGroupLabel): Only set the NSForegroundColorAttributeName attribute if the text isn't black.
            Follow-up bug about other colors: http://bugzilla.opendarwin.org/show_bug.cgi?id=9769
            (WebCore::RenderPopupMenuMac::addOption): ditto.
            * bridge/mac/FrameMac.h: (WebCore::FrameMac::currentEvent): Returns the current NSEvent.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderPopupMenuMac.mm
+++ third_party/WebKit/WebCore/rendering/RenderPopupMenuMac.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "config.h"
 #import "RenderPopupMenuMac.h"
 
+#import "FrameMac.h"
 #import "FrameView.h"
 #import "HTMLNames.h"
 #import "HTMLOptionElement.h"
@@ -65,6 +66,12 @@ void RenderPopupMenuMac::populate()
 
 void RenderPopupMenuMac::showPopup(const IntRect& r, FrameView* v, int index)
 {
+    FrameMac* frame = Mac(v->frame());
+    NSEvent* evt = frame->currentEvent();
+    // Save the current event that triggered the popup, so we can clean up our event
+    // state after the NSMenu goes away.
+    [evt retain];
+    
     if (!popup) {
         popup = [[[NSPopUpButtonCell alloc] initTextCell:@"" pullsDown:NO] retain];
         [popup setUsesItemFromMenu:NO];
@@ -116,6 +123,13 @@ void RenderPopupMenuMac::showPopup(const IntRect& r, FrameView* v, int index)
         getRenderMenuList()->valueChanged(newIndex);
 
     [popup dismissPopUp];
+    
+    if (frame) {
+        // Give WebCore a chance to fix up its event state, since the popup eats all the
+        // events during tracking.
+        frame->sendFakeEventsAfterWidgetTracking(evt);
+    }
+    [evt release];
 }
 
 void RenderPopupMenuMac::addSeparator()
@@ -140,7 +154,8 @@ void RenderPopupMenuMac::addGroupLabel(HTMLOptGroupElement* element)
     NSMutableDictionary* attributes = [[NSMutableDictionary alloc] init];
     if (s->font() != Font())
         [attributes setObject:s->font().getNSFont() forKey:NSFontAttributeName];
-    [attributes setObject:nsColor(s->color()) forKey:NSForegroundColorAttributeName];
+    if (s->color() != Color::black)
+        [attributes setObject:nsColor(s->color()) forKey:NSForegroundColorAttributeName];
     NSAttributedString *string = [[NSAttributedString alloc] initWithString:text attributes:attributes];
 
     [menuItem setAttributedTitle:string];
@@ -169,7 +184,9 @@ void RenderPopupMenuMac::addOption(HTMLOptionElement* element)
     NSMutableDictionary* attributes = [[NSMutableDictionary alloc] init];
     if (s->font() != Font())
         [attributes setObject:s->font().getNSFont() forKey:NSFontAttributeName];
-    [attributes setObject:nsColor(s->color()) forKey:NSForegroundColorAttributeName];
+    if (s->color() != Color::black)
+        [attributes setObject:nsColor(s->color()) forKey:NSForegroundColorAttributeName];
+    // FIXME: Find a way to customize text color when an item is highlighted.
     NSAttributedString *string = [[NSAttributedString alloc] initWithString:text attributes:attributes];
 
     [menuItem setAttributedTitle:string];
