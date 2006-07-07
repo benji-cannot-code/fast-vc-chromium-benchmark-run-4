4fd24990b86e9b5e96d97b89fecc195e752e2cf1
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-07 04:48:40 +0000

    Reviewed by Tim Hatcher.
            - fix 26 Node leaks on fast/dom tests
    
            * dom/Document.cpp:
            (WebCore::Document::removedLastRef): delete tokenizer - if we are still parsing at this point, we don't
            want nodes held by the parser to keep the document alive.
            * html/HTMLParser.cpp:
            (WebCore::RefNonDocNodePtr::RefNonDocNodePtr): Smart pointer that acts like RefPtr for Node,
            but avoids refing document nodes to avoid cycles.
            (WebCore::RefNonDocNodePtr::~RefNonDocNodePtr):
            (WebCore::RefNonDocNodePtr::get):
            (WebCore::RefNonDocNodePtr::operator*):
            (WebCore::RefNonDocNodePtr::operator->):
            (WebCore::RefNonDocNodePtr::operator!):
            (WebCore::RefNonDocNodePtr::operator UnspecifiedBoolType):
            (WebCore::RefNonDocNodePtr::operator=):
            (WebCore::operator==):
            (WebCore::operator!=):
            (WebCore::HTMLStackElem): Use a RefNonDocNodePtr instead of RefPtr<Node>
            (WebCore::HTMLParser::handleResidualStyleCloseTagAcrossBlocks): Use a RefPtr here to avoid crashing
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15197 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -305,6 +305,9 @@ void Document::removedLastRef()
         removeAllChildren();
 
         m_markers.clear();
+
+        delete m_tokenizer;
+        m_tokenizer = 0;
     } else
         delete this;
 }
