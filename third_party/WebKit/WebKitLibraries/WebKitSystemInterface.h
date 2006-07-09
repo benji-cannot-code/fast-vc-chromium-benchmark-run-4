a70b879f319fbb3096576fa0f767211366a6405e
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-09 21:31:24 +0000

    2006-07-09  Anders Carlsson  <acarlsson@apple.com>
            Reviewed by Darin.
    
            * WebKitSystemInterface.h:
            * libWebKitSystemInterface.a:
            Add WKPathFromFont.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
+++ third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
@@ -141,3 +141,4 @@ CGContextRef WKNSWindowOverrideCGContext(NSWindow *, CGContextRef);
 void WKNSWindowRestoreCGContext(NSWindow *, CGContextRef);
 
 BOOL WKSupportsMultipartXMixedReplace(NSMutableURLRequest *request);
+NSString* WKPathFromFont(NSFont *font);
