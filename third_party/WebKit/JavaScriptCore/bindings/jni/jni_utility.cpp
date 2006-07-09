1d55b53788654c43f7c9d693543fef5fd7b940e8
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-09 04:58:18 +0000

    Reviewed by Maciej.
            Moved KJS_GetCreatedJavaVMs to jni_utility.cpp.
            Switched KJS_GetCreatedJavaVMs over to use dlopen and dlsym
            now that NSAddImage, NSLookupSymbolInImage and NSAddressOfSymbol
            are deprecated in Leopard.
    
            * JavaScriptCore.exp:
            * JavaScriptCore.xcodeproj/project.pbxproj:
            * bindings/jni/jni_utility.cpp:
            (KJS::Bindings::KJS_GetCreatedJavaVMs):
            * bindings/softlinking.c: Removed.
            * bindings/softlinking.h: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/jni/jni_utility.cpp
+++ third_party/WebKit/JavaScriptCore/bindings/jni/jni_utility.cpp
@@ -32,11 +32,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "jni_runtime.h"
 #include "runtime_array.h"
 #include "runtime_object.h"
-#include "softlinking.h"
+#include <dlfcn.h>
 
 namespace KJS {
 
 namespace Bindings {
+
+static jint KJS_GetCreatedJavaVMs(JavaVM** vmBuf, jsize bufLen, jsize* nVMs)
+{
+    static void* javaVMFramework = 0;
+    if (!javaVMFramework)
+        javaVMFramework = dlopen("/System/Library/Frameworks/JavaVM.framework/JavaVM", RTLD_LAZY);
+    if (!javaVMFramework)
+        return JNI_ERR;
+
+    static jint(*functionPointer)(JavaVM**, jsize, jsize *) = 0;
+    if (!functionPointer)
+        functionPointer = (jint(*)(JavaVM**, jsize, jsize *))dlsym(javaVMFramework, "JNI_GetCreatedJavaVMs");
+    if (!functionPointer)
+        return JNI_ERR;
+    return functionPointer(vmBuf, bufLen, nVMs);
+}
 
 static JavaVM *jvm = 0;
 
