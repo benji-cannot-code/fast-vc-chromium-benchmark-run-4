d78fe6e04f32e3f71f5c6f876f0f2a99e1c3efdf
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-10 07:40:05 +0000

    - try to fix Windows build
            * rendering/RenderLayer.h: Correct a forward declaration of ScrollBar.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15293 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.h
+++ third_party/WebKit/WebCore/rendering/RenderLayer.h
@@ -49,8 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Timer.h"
 #include "WidgetClient.h"
 
-class ScrollBar;
-
 namespace WebCore {
 
 class CachedResource;
@@ -60,6 +58,7 @@ class RenderObject;
 class RenderStyle;
 class RenderTable;
 class RenderText;
+class ScrollBar;
 
 class ClipRects
 {
