23596a7332402cb6006ece289571aba28943105f
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-10 00:57:30 +0000

    Reviewed by Darin.
            Bug 9814: Move new WebView API that has been through API review to public headers
            http://bugzilla.opendarwin.org/show_bug.cgi?id=9814
    
            * WebView/WebView.h:
            * WebView/WebView.m:
            (-[WebView close]):
            (-[WebView setShouldCloseWithWindow:]):
            (-[WebView shouldCloseWithWindow]):
            (-[WebView selectedFrame]):
            (-[WebView setMainFrameURL:]):
            (-[WebView mainFrameURL]):
            (-[WebView isLoading]):
            (-[WebView mainFrameTitle]):
            (-[WebView mainFrameIcon]):
            (-[WebView mainFrameDocument]):
            (-[WebView setDrawsBackground:]):
            (-[WebView drawsBackground]):
            (-[WebView toggleSmartInsertDelete:]):
            (-[WebView toggleContinuousSpellChecking:]):
            (-[WebView canMakeTextStandardSize]):
            (-[WebView makeTextStandardSize:]):
            (-[WebView maintainsInactiveSelection]):
            * WebView/WebViewPrivate.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15272 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebView.h
+++ third_party/WebKit/WebKit/WebView/WebView.h
@@ -186,6 +186,32 @@ extern NSString *WebViewProgressFinishedNotification;
 - (id)initWithFrame:(NSRect)frame frameName:(NSString *)frameName groupName:(NSString *)groupName;
 
 /*!
+    @method close
+    @abstract Cancels any pending load operations. Once the receiver is closed it will no longer
+    respond to new requests or fire any more delegate methods.
+    @discussion A garbage collected application is required to call close when the receiver is no longer needed.
+    The close method will be called automatically when the window or hostWindow closes and shouldCloseWithWindow returns YES.
+    A non-garbage collected application can still call close, providing a convenient way to prevent receiver
+    from doing any more loading and firing any future delegate methods.
+*/
+- (void)close;
+
+/*!
+    @method setShouldCloseWithWindow:
+    @abstract Set whether the receiver closes when either it's window or hostWindow closes.
+    @param close YES if the receiver should close when either it's window or hostWindow closes, otherwise NO.
+*/
+- (void)setShouldCloseWithWindow:(BOOL)close;
+
+/*!
+    @method shouldCloseWithWindow
+    @abstract Returns whether the receiver closes when either it's window or hostWindow closes.
+    @discussion Defaults to YES in garbage collected applications, otherwise NO to maintain backwards compatibility.
+    @result YES if the receiver closes when either it's window or hostWindow closes, otherwise NO.
+*/
+- (BOOL)shouldCloseWithWindow;
+
+/*!
     @method setUIDelegate:
     @abstract Set the WebView's WebUIDelegate.
     @param delegate The WebUIDelegate to set as the delegate.
@@ -263,6 +289,15 @@ extern NSString *WebViewProgressFinishedNotification;
     @result The main frame.
 */    
 - (WebFrame *)mainFrame;
+
+/*!
+    @method selectedFrame
+    @abstract Return the frame that has the active selection.  
+    @discussion Returns the frame that contains the first responder, if any. Otherwise returns the
+    frame that contains a non-zero-length selection, if any. Returns nil if no frame meets these criteria.
+    @result The selected frame.
+*/
+- (WebFrame *)selectedFrame;
 
 /*!
     @method backForwardList
@@ -492,7 +527,6 @@ extern NSString *WebViewProgressFinishedNotification;
 */
 - (NSString *)groupName;
 
-
 /*!
     @method estimatedProgress
     @discussion An estimate of the percent complete for a document load.  This
@@ -504,6 +538,12 @@ extern NSString *WebViewProgressFinishedNotification;
     WebResourceLoadDelegate.
 */
 - (double)estimatedProgress;
+
+/*!
+    @method isLoading
+    @discussion Returns YES if there are any pending loads.
+*/
+- (BOOL)isLoading;
 
 /*!
     @method elementAtPoint:
@@ -556,6 +596,50 @@ extern NSString *WebViewProgressFinishedNotification;
 */
 - (void)removeDragCaret;
 
+/*!
+    @method setDrawsBackground:
+    @param drawsBackround YES to cause the receiver to draw a default white background, NO otherwise.
+    @abstract Sets whether the receiver draws a default white background when the loaded page has no background specified.
+*/
+- (void)setDrawsBackground:(BOOL)drawsBackround;
+
+/*!
+    @method drawsBackground
+    @result Returns YES if the receiver draws a default white background, NO otherwise.
+*/
+- (BOOL)drawsBackground;
+
+/*!
+    @method setMainFrameURL:
+    @param URLString The URL to load in the mainFrame.
+*/
+- (void)setMainFrameURL:(NSString *)URLString;
+
+/*!
+    @method mainFrameURL
+    @result Returns the main frame's current URL.
+*/
+- (NSString *)mainFrameURL;
+
+/*!
+    @method mainFrameDocument
+    @result Returns the main frame's DOMDocument.
+*/
+- (DOMDocument *)mainFrameDocument;
+
+/*!
+    @method mainFrameTitle
+    @result Returns the main frame's title if any, otherwise an empty string.
+*/
+- (NSString *)mainFrameTitle;
+
+/*!
+    @method mainFrameIcon
+    @discussion The methods returns the site icon for the current page loaded in the mainFrame.
+    @result Returns the main frame's icon if any, otherwise nil.
+*/
+- (NSImage *)mainFrameIcon;
+
 @end
 
 
@@ -571,6 +655,10 @@ extern NSString *WebViewProgressFinishedNotification;
 - (IBAction)makeTextLarger:(id)sender;
 - (BOOL)canMakeTextSmaller;
 - (IBAction)makeTextSmaller:(id)sender;
+- (BOOL)canMakeTextStandardSize;
+- (IBAction)makeTextStandardSize:(id)sender;
+- (IBAction)toggleContinuousSpellChecking:(id)sender;
+- (IBAction)toggleSmartInsertDelete:(id)sender;
 @end
 
 
@@ -591,6 +679,7 @@ extern NSString * const WebViewDidChangeSelectionNotification;
 - (void)setSelectedDOMRange:(DOMRange *)range affinity:(NSSelectionAffinity)selectionAffinity;
 - (DOMRange *)selectedDOMRange;
 - (NSSelectionAffinity)selectionAffinity;
+- (BOOL)maintainsInactiveSelection;
 - (void)setEditable:(BOOL)flag;
 - (BOOL)isEditable;
 - (void)setTypingStyle:(DOMCSSStyleDeclaration *)style;
@@ -642,6 +731,12 @@ extern NSString * const WebViewDidChangeSelectionNotification;
 
 - (void)startSpeaking:(id)sender;
 - (void)stopSpeaking:(id)sender;
+
+- (void)moveToBeginningOfSentence:(id)sender;
+- (void)moveToBeginningOfSentenceAndModifySelection:(id)sender;
+- (void)moveToEndOfSentence:(id)sender;
+- (void)moveToEndOfSentenceAndModifySelection:(id)sender;
+- (void)selectSentence:(id)sender;
 
 /* 
 The following methods are declared in NSResponder.h.
