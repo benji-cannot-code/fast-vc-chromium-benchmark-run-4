d0471ada35eed105474f99edb99ee2f2fb7ee73f
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-12 11:28:35 +0000

    WebKitTools:
            Reviewed by ggaren.
    
            Bug 9863: Drosera needs to show something at launch
            http://bugzilla.opendarwin.org/show_bug.cgi?id=9863
    
            * Drosera/DebuggerApplication.m:
            (-[DebuggerApplication applicationDidFinishLaunching:]): Show the attach window on launch.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Drosera/DebuggerApplication.m
+++ third_party/WebKit/WebKitTools/Drosera/DebuggerApplication.m
@@ -51,6 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(serverLoaded:) name:WebScriptDebugServerQueryReplyNotification object:nil];
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(serverUnloaded:) name:WebScriptDebugServerWillUnloadNotification object:nil];
     [[NSDistributedNotificationCenter defaultCenter] postNotificationName:WebScriptDebugServerQueryNotification object:nil];
+
+    [self showAttachPanel:nil];
 }
 
 #pragma mark -
