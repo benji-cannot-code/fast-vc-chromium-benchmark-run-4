c8790235444825d60412dc7868839497f3533b76
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-13 23:17:08 +0000

    Reviewed by Dave Hyatt.
            <rdar://problem/4624203> -webkit-highlight should be behind images
    
            * rendering/RenderImage.cpp:
            (WebCore::RenderImage::paint):
            * rendering/RenderListMarker.cpp:
            (WebCore::RenderListMarker::paint):
            * rendering/RenderWidget.cpp:
            (WebCore::RenderWidget::paint):
            Call custom highlighter before painting the image, marker, or widget.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderWidget.cpp
+++ third_party/WebKit/WebCore/rendering/RenderWidget.cpp
@@ -180,6 +180,11 @@ void RenderWidget::paint(PaintInfo& i, int tx, int ty)
     if (!m_view || i.phase != PaintPhaseForeground || style()->visibility() != VISIBLE)
         return;
 
+#if PLATFORM(MAC)
+    if (style()->highlight() != nullAtom && !i.p->paintingDisabled())
+        paintCustomHighlight(tx - m_x, ty - m_y, style()->highlight(), true);
+#endif
+
     if (m_widget) {
         // Move the widget if necessary.  We normally move and resize widgets during layout, but sometimes
         // widgets can move without layout occurring (most notably when you scroll a document that
@@ -190,11 +195,6 @@ void RenderWidget::paint(PaintInfo& i, int tx, int ty)
         // to paint itself.  That way it will composite properly with z-indexed layers.
         m_widget->paint(i.p, i.r);
     }
-
-#if PLATFORM(MAC)
-    if (style()->highlight() != nullAtom && !i.p->paintingDisabled())
-        paintCustomHighlight(tx - m_x, ty - m_y, style()->highlight(), false);
-#endif
 
     // Paint a partially transparent wash over selected widgets.
     if (isSelected() && !document()->printing())
