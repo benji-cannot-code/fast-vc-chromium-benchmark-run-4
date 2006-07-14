9ff8e17c63a307ffd2aac45ac8dbfc084631ef24
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-14 20:54:09 +0000

    2006-07-14  Anders Carlsson  <acarlsson@apple.com>
            Reviewed by Adele and Justin.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=9658
            <rdar://problem/4613948>
            REGRESSION: Check Spelling does not work in textarea elements
            
            * bridge/mac/FrameMac.mm:
            (WebCore::FrameMac::advanceToNextMisspelling):
            Don't use setStartBefore or setEndAfter on the search range because for shadow trees, there
            is no notion of before and after. Instead, use setStart and setEnd and pass in the start and end
            indices respectively.
            
            * dom/Range.cpp:
            (WebCore::Range::checkNodeBA):
            Allow range operations on shadow trees.
    
            * manual-tests/form-element-spelling.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15435 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/mac/FrameMac.mm
+++ third_party/WebKit/WebCore/bridge/mac/FrameMac.mm
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebCoreViewFactory.h"
 #import "WebDashboardRegion.h"
 #import "csshelper.h"
+#import "htmlediting.h"
 #import "kjs_window.h"
 #import "visible_units.h"
 #import <JavaScriptCore/NP_jsobject.h>
@@ -696,7 +697,7 @@ String FrameMac::advanceToNextMisspelling(bool startBeforeSelection)
     
     // topNode defines the whole range we want to operate on 
     Node *topNode = editableNode->rootEditableElement();
-    searchRange->setEndAfter(topNode, exception);
+    searchRange->setEnd(topNode, maxDeepOffset(topNode), exception);
 
     // Make sure start of searchRange is not in the middle of a word.  Jumping back a char and then
     // forward by a word happens to do the trick.
@@ -758,7 +759,7 @@ String FrameMac::advanceToNextMisspelling(bool startBeforeSelection)
             } else {
                 // we've gone from the selection to the end of doc, now wrap around
                 wrapped = YES;
-                searchRange->setStartBefore(topNode, exception);
+                searchRange->setStart(topNode, 0, exception);
                 // going until the end of the very first chunk we tested is far enough
                 searchRange->setEnd(searchEndAfterWrapNode, searchEndAfterWrapOffset, exception);
                 it = WordAwareIterator(searchRange.get());
