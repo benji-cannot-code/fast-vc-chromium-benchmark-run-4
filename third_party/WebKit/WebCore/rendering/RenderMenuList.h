8cb57098fa0fda8b53f17ab0bd93f62f4a09d6d9
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-15 15:14:15 +0000

    LayoutTests:
            Reviewed by John Sullivan.
    
            - test for http://bugzilla.opendarwin.org/show_bug.cgi?id=9883
              <rdar://problem/4631821>
              REGRESSION: NativePopups don't work correctly in some forum software
    
            * fast/forms/select-selected-expected.checksum: Added.
            * fast/forms/select-selected-expected.png: Added.
            * fast/forms/select-selected-expected.txt: Added.
            * fast/forms/select-selected.html: Added.
    
    WebCore:
    
            Reviewed by John Sullivan.
    
            - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=9883
              <rdar://problem/4631821>
              REGRESSION: NativePopups don't work correctly in some forum software
    
            Test: fast/forms/select-selected.html
    
            * rendering/RenderMenuList.h:
            * rendering/RenderMenuList.cpp:
            (WebCore::RenderMenuList::updateFromElement): Added code to map the selected option
            index to a list index before using it to index into the list.
            (WebCore::RenderMenuList::showPopup): Added code to map the selection option index
            to a list index before passing it to the menu renderer.
            (WebCore::RenderMenuList::valueChanged): Added code to map the list index back to a
            option index before calling setSelectedIndex (that function takes an option index).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderMenuList.h
+++ third_party/WebKit/WebCore/rendering/RenderMenuList.h
@@ -51,7 +51,7 @@ public:
     void showPopup();
 
     void setOptionsChanged(bool c) { m_optionsChanged = c; }
-    void valueChanged(unsigned index);
+    void valueChanged(unsigned listIndex);
 
 private:
     void createInnerBlock();
