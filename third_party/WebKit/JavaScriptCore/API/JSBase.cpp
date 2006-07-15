2eed162303ed4db55afb8fcc9df130b7c12e898d
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-15 06:32:53 +0000

    Build fix for case-sensitive file systems.
    git-svn-id: svn://svn.chromium.org/blink/trunk@15445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSBase.cpp
+++ third_party/WebKit/JavaScriptCore/API/JSBase.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "APICast.h"
 
 #include <kjs/ExecState.h>
-#include <kjs/Interpreter.h>
+#include <kjs/interpreter.h>
 #include <kjs/JSLock.h>
 #include <kjs/object.h>
 
