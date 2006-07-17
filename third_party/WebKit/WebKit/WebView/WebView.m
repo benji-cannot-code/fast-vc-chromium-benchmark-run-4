0a083b940ce69bebaa1012c4e30165302887fbdb
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-17 19:38:48 +0000

    LayoutTests:
            Reviewed by levi
    
            * editing/selection/drag-to-contenteditable-iframe-expected.txt:
            * editing/selection/subframe-with-selection-expected.txt:
    
    WebKit:
    
            Reviewed by levi
            
            Rolled the first fix for:
            <http://bugzilla.opendarwin.org/show_bug.cgi?id=9642>
            GMail Editor: Operations that use drop down menus blow away the selection
            back in and removed the call to _clearSelectionInOtherFrames from 
            -[WebHTMLView becomeFirstResponder] to fix the bug.
    
            * WebView/WebHTMLView.m:
            (-[NSArray maintainsInactiveSelection]):
            (-[NSArray becomeFirstResponder]):
            * WebView/WebView.m:
            (-[WebView maintainsInactiveSelection]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15487 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebView.m
+++ third_party/WebKit/WebKit/WebView/WebView.m
@@ -3049,7 +3049,7 @@ static WebFrame *incrementFrame(WebFrame *curr, BOOL forward, BOOL wrapFlag)
 
 - (BOOL)maintainsInactiveSelection
 {
-    return [self isEditable];
+    return NO;
 }
 
 - (void)setSelectedDOMRange:(DOMRange *)range affinity:(NSSelectionAffinity)selectionAffinity
