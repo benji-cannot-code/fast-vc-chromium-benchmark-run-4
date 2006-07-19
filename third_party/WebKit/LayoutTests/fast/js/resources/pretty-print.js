752a8c0329fee985d1963e167f29134f182e75d6
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-19 18:30:02 +0000

    2006-07-19  Anders Carlsson  <acarlsson@apple.com>
            * fast/js/pretty-print-expected.txt:
            Update result.
            
            * fast/js/resources/pretty-print.js:
            Fix description string.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/resources/pretty-print.js
+++ third_party/WebKit/LayoutTests/fast/js/resources/pretty-print.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//description(
-"This test checks that regexps and strings with are pretty-printed correctly"
-//);
+description(
+"This test checks that regexps and strings with special characters are pretty-printed correctly"
+);
 
 function f() {
     var re = /test/g;
