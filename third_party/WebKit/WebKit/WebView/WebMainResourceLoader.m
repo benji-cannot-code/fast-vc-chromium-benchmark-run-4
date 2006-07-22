649f6b10222b54e3e2a3a71ea455e7b40f99eaca
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-22 06:34:15 +0000

    Reviewed by Maciej.
            <rdar://problem/4609195> Help Viewer loads empty window
            (not getting didFailLoadingWithError: callback)
    
            Call super's didFailWithError before _receivedMainResourceError
            because _receivedMainResourceError will cause the datasource's
            frame to be set to nil before the didFailLoadingWithError delegate
            callback is sent. (This order is needed now that WebDataSource does
            not hold on to the WebView; it uses the WebFrame to get to the WebView.
            If the WebFrame is nil we can't get to the WebView's resource load delegate.)
    
            * WebView/WebMainResourceLoader.m:
            (-[WebMainResourceLoader receivedError:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebMainResourceLoader.m
+++ third_party/WebKit/WebKit/WebView/WebMainResourceLoader.m
@@ -85,9 +85,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     // Calling _receivedMainResourceError will likely result in a call to release, so we must retain.
     [self retain];
-    WebDataSource *ds = [dataSource retain];
-    [dataSource _receivedMainResourceError:error complete:YES];
+    WebDataSource *ds = [dataSource retain]; // super's didFailWithError will release the datasource
     [super didFailWithError:error];
+    [ds _receivedMainResourceError:error complete:YES];
     [ds release];
     [self release];
 }
