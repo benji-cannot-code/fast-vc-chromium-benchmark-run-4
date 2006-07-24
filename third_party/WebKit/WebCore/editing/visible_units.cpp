f8825aa72109bc7d957f64603f195dcdad1de632
aliceli1 <aliceli1@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-24 02:51:04 +0000

    Reviewed by Adele.         Actually Adele figured out how to fix the layout test failures.  Landing this patch for her.
            * editing/visible_units.cpp:
            (WebCore::previousLinePosition):
            When setting selection endpoints, don't traverse down into nodes where editing would ignore its contents. Use the parent node instead.  
            (WebCore::nextLinePosition):
            same as above
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/visible_units.cpp
+++ third_party/WebKit/WebCore/editing/visible_units.cpp
@@ -420,6 +420,9 @@ VisiblePosition previousLinePosition(const VisiblePosition &visiblePosition, int
         if (containingBlock->hasOverflowClip())
             containingBlock->layer()->subtractScrollOffset(absx, absy);
         RenderObject *renderer = root->closestLeafChildForXPos(x, absx)->object();
+        Node* node = renderer->element();
+        if (editingIgnoresContent(node))
+            return Position(node->parent(), node->nodeIndex());
         return renderer->positionForCoordinates(x, absy + root->topOverflow());
     }
     
@@ -485,6 +488,9 @@ VisiblePosition nextLinePosition(const VisiblePosition &visiblePosition, int x)
         if (containingBlock->hasOverflowClip())
             containingBlock->layer()->subtractScrollOffset(absx, absy);
         RenderObject *renderer = root->closestLeafChildForXPos(x, absx)->object();
+        Node* node = renderer->element();
+        if (editingIgnoresContent(node))
+            return Position(node->parent(), node->nodeIndex());
         return renderer->positionForCoordinates(x, absy + root->topOverflow());
     }    
 
