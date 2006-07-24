4e6ebf05aa6d242c1e59986ccb9297a432b9c296
rwlbuis <rwlbuis@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-24 11:45:34 +0000

    Reviewed by Darin.
            http://bugzilla.opendarwin.org/show_bug.cgi?id=5257
            setYear() does not match FireFox/IE behavior
    
            Make sure the right values end up in tm_year.
    
            * kjs/date_object.cpp:
            (KJS::formatTime):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/kde/resources/Date-setYear.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+debug("Start Of Test");
+
+var d = new Date();
+shouldBe("d.setYear(-1), d.getFullYear()", "-1");
+shouldBe("d.setYear(0), d.getFullYear()", "1900");
+shouldBe("d.setYear(1), d.getFullYear()", "1901");
+shouldBe("d.setYear(99), d.getFullYear()", "1999");
+shouldBe("d.setYear(100), d.getFullYear()", "100");
+shouldBe("d.setYear(2050), d.getFullYear()", "2050");
+shouldBe("d.setYear(1899), d.getFullYear()", "1899");
+shouldBe("d.setYear(2000), d.getFullYear()", "2000");
+shouldBe("d.setYear(2100), d.getFullYear()", "2100");
+
+successfullyParsed = true
