0372280aebc27aeb1bac2b58b9cc39c4e35b308b
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-28 19:58:30 +0000

    WebCore:
            Reviewed by John.
    
            Adding isContentEditable to DOMNode's private category. 
    
            * bindings/objc/DOMPrivate.h:
    
    WebKit:
    
            Reviewed by John.
    
            The context menu code should be checking isContentEditable
            on DOMNode not just DOMElement. This is needed because DOMText
            will be the node class of any text that is clicked.
    
            * DefaultDelegates/WebDefaultContextMenuDelegate.m:
            (-[WebDefaultUIDelegate webView:contextMenuItemsForElement:defaultMenuItems:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15670 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/DOMPrivate.h
+++ third_party/WebKit/WebCore/bindings/objc/DOMPrivate.h
@@ -39,6 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSColor *)_color;
 @end
 
+@interface DOMNode (WebPrivate)
+- (BOOL)isContentEditable;
+@end
+
 @interface DOMElement (WebPrivate)
 - (NSFont *)_font;
 - (NSData *)_imageTIFFRepresentation;
