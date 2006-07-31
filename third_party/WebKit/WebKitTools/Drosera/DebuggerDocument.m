7bc544ddaec7f57e32d04ce9fc916c3213cf0261
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-07-31 06:15:05 +0000

    Reviewed by Maciej.
            Bug 9686: [Drosera] Need the ability to break into Drosera on Javascript exceptions
            http://bugzilla.opendarwin.org/show_bug.cgi?id=9686
    
            * Drosera/DebuggerDocument.m:
            (-[WebScriptObject switchToServerNamed:]): Remove as listener before resuming to
            ensure that the server does not try and notify us of events related to the resumption.
            (-[WebScriptObject webView:exceptionWasRaised:sourceId:line:forWebFrame:]): Delegate
            call through to JavaScript.
            * Drosera/debugger.js: Pause debugger when exception is raised.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15701 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Drosera/DebuggerDocument.m
+++ third_party/WebKit/WebKitTools/Drosera/DebuggerDocument.m
@@ -277,8 +277,8 @@ static NSString *DebuggerStepOutToolbarItem = @"DebuggerStepOutToolbarItem";
     if (server) {
         [[NSNotificationCenter defaultCenter] removeObserver:self name:NSConnectionDidDieNotification object:[(NSDistantObject *)server connectionForProxy]];
         if ([[(NSDistantObject *)server connectionForProxy] isValid]) {
-            [self resume];
             [server removeListener:self];
+            [self resume];
         }
     }
 
@@ -641,5 +641,15 @@ static NSString *DebuggerStepOutToolbarItem = @"DebuggerStepOutToolbarItem";
     id old = currentFrame;
     currentFrame = [[frame caller] retain];
     [old release];
+}
+
+- (void)webView:(WebView *)view exceptionWasRaised:(WebScriptCallFrame *)frame sourceId:(int)sid line:(int)lineno forWebFrame:(WebFrame *)webFrame
+{
+    if (!webViewLoaded)
+        return;
+
+    NSArray *args = [[NSArray alloc] initWithObjects:[NSNumber numberWithInt:sid], [NSNumber numberWithInt:lineno], nil];
+    [[webView windowScriptObject] callWebScriptMethod:@"exceptionWasRaised" withArguments:args];
+    [args release];
 }
 @end
