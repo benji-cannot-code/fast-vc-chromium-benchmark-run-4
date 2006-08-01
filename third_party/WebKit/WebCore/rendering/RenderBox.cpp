1f3050b6d41a059d1c8829358c9ad618e2e9c689
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-01 04:14:20 +0000

    Reviewed by Hyatt.
            - removed obsolete cell margin hack
    
            * rendering/RenderBox.cpp: (WebCore::RenderBox::calcVerticalMargins):
            Set top and bottom margins to 0 for table cells instead of -16384.
            No effect on layout (including layout tests).
    
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
            Added a few comments about differences between this class and the
            what the CSS specification says about computed style.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBox.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBox.cpp
@@ -53,8 +53,6 @@ namespace WebCore {
 
 using namespace HTMLNames;
 
-#define TABLECELLMARGIN -0x4000
-
 RenderBox::RenderBox(Node* node)
     : RenderObject(node)
 {
@@ -1431,10 +1429,9 @@ int RenderBox::availableHeightUsing(const Length& h) const
 
 void RenderBox::calcVerticalMargins()
 {
-    if( isTableCell() ) {
-        // table margins are basically infinite
-        m_marginTop = TABLECELLMARGIN;
-        m_marginBottom = TABLECELLMARGIN;
+    if (isTableCell()) {
+        m_marginTop = 0;
+        m_marginBottom = 0;
         return;
     }
 
