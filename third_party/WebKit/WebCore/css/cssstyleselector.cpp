9caca923328634293355ee4a20fa3be607bee161
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-01 06:15:38 +0000

    LayoutTests:
            Reviewed by Darin.
    
            - tests changed by http://bugzilla.opendarwin.org/show_bug.cgi?id=9955
            Colors scaled from [0, 1] range to [0, 255] incorrectly
    
            * fast/css/hsl-color-expected.checksum:
            * fast/css/hsl-color-expected.png:
            * fast/css/hsl-color-expected.txt:
            * fast/forms/input-disabled-color-expected.checksum:
            * fast/forms/input-disabled-color-expected.png:
            * fast/forms/input-disabled-color-expected.txt:
    
    WebCore:
    
            Reviewed by Darin.
    
            - patch for http://bugzilla.opendarwin.org/show_bug.cgi?id=9955
            Colors scaled from [0, 1] range to [0, 255] incorrectly
            
            Fixes scaling issue and fixes Color to follow style guidelines.
    
            * css/cssstyleselector.cpp:
            (WebCore::CSSStyleSelector::getColorFromPrimitiveValue):
            * platform/Color.cpp:
            (WebCore::calcHue):
            (WebCore::makeRGBAFromHSLA):
            (WebCore::Color::Color):
            (WebCore::Color::name):
            (WebCore::Color::setNamedColor):
            (WebCore::Color::light):
            (WebCore::Color::dark):
            (WebCore::blend):
            * platform/Color.h:
            (WebCore::Color::Color):
            (WebCore::Color::isValid):
            (WebCore::Color::red):
            (WebCore::Color::green):
            (WebCore::Color::blue):
            (WebCore::Color::alpha):
            (WebCore::Color::rgb):
            (WebCore::Color::setRGB):
            (WebCore::operator==):
            (WebCore::operator!=):
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::drawBorder):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/cssstyleselector.cpp
+++ third_party/WebKit/WebCore/css/cssstyleselector.cpp
@@ -4528,7 +4528,7 @@ Color CSSStyleSelector::getColorFromPrimitiveValue(CSSPrimitiveValue* primitiveV
         else
             col = colorForCSSValue(ident);
     } else if (primitiveValue->primitiveType() == CSSPrimitiveValue::CSS_RGBCOLOR)
-        col.setRgb(primitiveValue->getRGBColorValue());
+        col.setRGB(primitiveValue->getRGBColorValue());
     return col;
 }
 
