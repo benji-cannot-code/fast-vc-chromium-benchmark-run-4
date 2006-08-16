826eaf2fa79c25f3f140d668beec611bc2e83b1c
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-16 18:03:52 +0000

    Reviewed by John
            Updated the DB version number and added a helpful comment explaining its meaning
    
            * loader/icon/IconDatabase.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/icon/IconDatabase.cpp
+++ third_party/WebKit/WebCore/loader/icon/IconDatabase.cpp
@@ -44,7 +44,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 IconDatabase* IconDatabase::m_sharedInstance = 0;
-const int IconDatabase::currentDatabaseVersion = 3;
+
+// This version number is in the DB and marks the current generation of the schema
+// Theoretically once the switch is flipped this should never change
+// Currently, an out-of-date schema causes the DB to be wiped and reset.  This isn't 
+// so bad during development but in the future, we would need to write a conversion
+// function to advance older released schemas to "current"
+const int IconDatabase::currentDatabaseVersion = 4;
 
 // Icons expire once a day
 const int IconDatabase::iconExpirationTime = 60*60*24; 
