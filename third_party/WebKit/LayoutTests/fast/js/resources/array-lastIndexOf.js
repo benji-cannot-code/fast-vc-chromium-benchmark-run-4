1da331ad080f4027f69a026fa93321b94d6fa1e2
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-21 17:43:55 +0000

    2006-08-21  Vladimir Olexa  <vladimir.olexa@gmail.com>
            Reviewed by Darin.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=6252
            JavaScript 1.6 Array.lastIndexOf
    
            Test: fast/js/array-lastIndexOf.html
    
            * kjs/array_object.cpp:
            (ArrayProtoFunc::callAsFunction): Added a LastIndexOf case.
            * kjs/array_object.h:
            (KJS::ArrayProtoFunc::): Added LastIndexOf to enum.
            * tests/mozilla/expected.html: Two more tests now pass.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/array-lastIndexOf.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+'This test checks lastIndexOf for various values in an array'
+);
+
+
+var testArray = [2, 5, 9, 2];
+var lastIndex = 0;
+
+lastIndex = testArray.lastIndexOf(2,-500);
+shouldBe('lastIndex', '-1');
+lastIndex = testArray.lastIndexOf(9,500);
+shouldBe('lastIndex', '2');
+lastIndex = testArray.lastIndexOf(2);
+shouldBe('lastIndex', '3');
+lastIndex = testArray.lastIndexOf(7);
+shouldBe('lastIndex', '-1');
+lastIndex = testArray.lastIndexOf(2, 3);
+shouldBe('lastIndex', '3');
+lastIndex = testArray.lastIndexOf(2, 2);
+shouldBe('lastIndex', '0');
+lastIndex = testArray.lastIndexOf(2, -2);
+shouldBe('lastIndex', '0');
+lastIndex = testArray.lastIndexOf(2, -1);
+shouldBe('lastIndex', '3');
+
+var successfullyParsed = true;
