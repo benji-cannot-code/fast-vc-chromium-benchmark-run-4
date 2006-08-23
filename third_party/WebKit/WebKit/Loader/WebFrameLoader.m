b136beb548174ed101ac1db8383a96c5531d66b4
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-23 05:49:04 +0000

    LayoutTests:
            Reviewed by Eric.
    
            - test for <rdar://problem/4683948> REGRESSION: Assertion failure in [FrameProgressEntry addChild:forDataSource:] (fandango.com)
            
            (I both added a WebKit assertion that would fail under this test, and then did a fix to make it not fire)
            
            * fast/loader/start-load-in-unload-expected.txt: Added.
            * fast/loader/start-load-in-unload.html: Added.
    
    WebKit:
    
            Reviewed by Eric.
    
            <rdar://problem/4683948> REGRESSION: Assertion failure in [FrameProgressEntry addChild:forDataSource:] (fandango.com)
            
            * Loader/WebFrameLoader.m:
            (-[WebFrameLoader addSubresourceLoader:]): Add a WebKit-level assertion that should fire when this bad
            situation occurs.
            * Loader/WebSubresourceLoader.m:
            (+[WebSubresourceLoader startLoadingResource:withRequest:customHeaders:referrer:forFrameLoader:]): Prevent
            the situation from occuring.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@15982 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Loader/WebFrameLoader.m
+++ third_party/WebKit/WebKit/Loader/WebFrameLoader.m
@@ -155,6 +155,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)addSubresourceLoader:(WebLoader *)loader
 {
+    ASSERT(!provisionalDataSource);
     if (subresourceLoaders == nil)
         subresourceLoaders = [[NSMutableArray alloc] init];
     [subresourceLoaders addObject:loader];
