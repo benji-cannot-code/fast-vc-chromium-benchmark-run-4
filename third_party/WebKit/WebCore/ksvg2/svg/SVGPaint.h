0845b38bea83a71e13c9946b52267f48314bd864
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-27 07:09:16 +0000

    2006-08-26  Eric Seidel  <eric@eseidel.com>
            Reviewed by hyatt.
    
            pointer-events attribute does not work.
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10415
    
            * kcanvas/RenderPath.cpp:
            (WebCore::RenderPath::pointerEventsHitRules): new function to contain pointer-events hit logic
            (WebCore::RenderPath::nodeAtPoint): respect pointer-events property
            * kcanvas/RenderPath.h:
            (WebCore::RenderPath::PointerEventsHitRules::PointerEventsHitRules):
            * ksvg2/css/SVGCSSParser.cpp:
            (WebCore::CSSParser::parseSVGValue):
            * ksvg2/svg/SVGPaint.cpp: Fix this to use a real enum value
            (WebCore::SVGPaint::SVGPaint):
            (WebCore::SVGPaint::paintType):
            (WebCore::SVGPaint::uri):
            (WebCore::SVGPaint::setUri):
            (WebCore::SVGPaint::setPaint):
            * ksvg2/svg/SVGPaint.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGPaint.h
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGPaint.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define KSVG_SVGPaintImpl_H
 #ifdef SVG_SUPPORT
 
+#include "ksvg.h"
 #include <SVGColor.h>
 
 namespace WebCore {
@@ -32,22 +33,22 @@ namespace WebCore {
     class SVGPaint : public SVGColor {
     public:
         SVGPaint();
-        SVGPaint(StringImpl *uri);
-        SVGPaint(unsigned short paintType);
-        SVGPaint(unsigned short paintType, StringImpl *uri, StringImpl *rgbPaint = 0, StringImpl *iccPaint = 0);
+        SVGPaint(StringImpl* uri);
+        SVGPaint(SVGPaintType);
+        SVGPaint(SVGPaintType, StringImpl* uri, StringImpl* rgbPaint = 0, StringImpl* iccPaint = 0);
         virtual ~SVGPaint();
 
         // 'SVGPaint' functions
-        unsigned short paintType() const;
-        StringImpl *uri() const;
+        SVGPaintType paintType() const;
+        StringImpl* uri() const;
 
-        void setUri(StringImpl *uri);
-        void setPaint(unsigned short paintType, StringImpl *uri, StringImpl *rgbPaint, StringImpl *iccPaint);
+        void setUri(StringImpl*);
+        void setPaint(SVGPaintType, StringImpl* uri, StringImpl* rgbPaint, StringImpl* iccPaint);
 
         virtual String cssText() const;
 
     private:
-        unsigned short m_paintType;
+        SVGPaintType m_paintType;
         String m_uri;
     };
 
