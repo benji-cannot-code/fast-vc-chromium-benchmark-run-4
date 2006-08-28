a05da4cecb695875b722b6b4666dc56a28e19a32
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-28 18:49:55 +0000

    2006-08-28  Nikolas Zimmermann  <zimmermann@kde.org>
            Reviewed and landed by ap.
    
            Fixes one chunk of: http://bugzilla.opendarwin.org/show_bug.cgi?id=10604
            Provide stub implementation of RenderPopupMenuQt.
    
            * CMakeLists.txt:
            * platform/qt/RenderPopupMenuQt.cpp: Added.
            (WebCore::RenderPopupMenuQt::RenderPopupMenuQt):
            (WebCore::RenderPopupMenuQt::~RenderPopupMenuQt):
            (WebCore::RenderPopupMenuQt::clear):
            (WebCore::RenderPopupMenuQt::populate):
            (WebCore::RenderPopupMenuQt::showPopup):
            (WebCore::RenderPopupMenuQt::hidePopup):
            (WebCore::RenderPopupMenuQt::addSeparator):
            (WebCore::RenderPopupMenuQt::addGroupLabel):
            (WebCore::RenderPopupMenuQt::addOption):
            * platform/qt/RenderPopupMenuQt.h: Added.
            * platform/qt/RenderThemeQt.cpp:
            (WebCore::RenderThemeQt::systemFont):
            (WebCore::RenderThemeQt::createPopupMenu):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/RenderThemeQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/RenderThemeQt.cpp
@@ -31,8 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QPainter>
 #include <QStyleOptionButton>
 
+#include "Document.h"
 #include "RenderTheme.h"
 #include "GraphicsContext.h"
+#include "RenderPopupMenuQt.h"
 
 #define notImplemented() do { fprintf(stderr, "FIXME: UNIMPLEMENTED: %s:%d\n", __FILE__, __LINE__); } while(0)
 
@@ -117,10 +119,9 @@ void RenderThemeQt::systemFont(int propId, FontDescription& fontDescription) con
     // no-op
 }
 
-RenderPopupMenu* RenderThemeQt::createPopupMenu(RenderArena*, Document*, RenderMenuList*)
+RenderPopupMenu* RenderThemeQt::createPopupMenu(RenderArena* arena, Document* doc, RenderMenuList* menuList)
 {
-    notImplemented();
-    return 0;
+    return new (arena) RenderPopupMenuQt(doc, menuList);
 }
 
 void RenderThemeQt::addIntrinsicMargins(RenderStyle* style) const
