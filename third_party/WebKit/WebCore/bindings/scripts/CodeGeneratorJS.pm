9e7a94b7695cdba42576313c4a6dbc9031a8559f
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-29 23:19:33 +0000

    2006-08-29  Eric Seidel  <eric@eseidel.com>
            Reviewed by kevin.
    
            Re-enable path-related SVG JavaScript bindings.
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10623
            Split all SVGPathSeg*.idl files into Abs and Rel pieces.
            Move all SVGPath*.idl files into ksvg2/svg
    
            * DerivedSources.make:
            * WebCore.xcodeproj/project.pbxproj:
            * bindings/scripts/CodeGeneratorJS.pm: Special case Abs and Rel header includes
            * ksvg2/bindings/idl/svg/SVGAnimatedNumber.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathElement.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSeg.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegArc.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegClosePath.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegCurvetoCubic.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegCurvetoCubicSmooth.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegCurvetoQuadratic.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegCurvetoQuadraticSmooth.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegLineto.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegLinetoHorizontal.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegLinetoVertical.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegList.idl: Removed.
            * ksvg2/bindings/idl/svg/SVGPathSegMoveto.idl: Removed.
            * ksvg2/ksvg.h: move SVGPathSegment enum
            * ksvg2/svg/SVGPathElement.cpp: move SVGPathSegment enum
            (WebCore::SVGPathElement::toPathData):
            * ksvg2/svg/SVGPathSeg.cpp:
            (WebCore::SVGPathSeg::SVGPathSeg):
            * ksvg2/svg/SVGPathSeg.h:
            (WebCore::SVGPathSeg::):
            * ksvg2/svg/SVGPathSegArcAbs.idl: Added.
            * ksvg2/svg/SVGPathSegArcRel.idl: Added.
            * ksvg2/svg/SVGPathSegCurvetoCubicAbs.idl: Added.
            * ksvg2/svg/SVGPathSegCurvetoCubicRel.idl: Added.
            * ksvg2/svg/SVGPathSegCurvetoCubicSmoothAbs.idl: Added.
            * ksvg2/svg/SVGPathSegCurvetoCubicSmoothRel.idl: Added.
            * ksvg2/svg/SVGPathSegCurvetoQuadraticAbs.idl: Added.
            * ksvg2/svg/SVGPathSegCurvetoQuadraticRel.idl: Added.
            * ksvg2/svg/SVGPathSegCurvetoQuadraticSmoothAbs.idl: Added.
            * ksvg2/svg/SVGPathSegCurvetoQuadraticSmoothRel.idl: Added.
            * ksvg2/svg/SVGPathSegLinetoAbs.idl: Added.
            * ksvg2/svg/SVGPathSegLinetoHorizontalAbs.idl: Added.
            * ksvg2/svg/SVGPathSegLinetoHorizontalRel.idl: Added.
            * ksvg2/svg/SVGPathSegLinetoRel.idl: Added.
            * ksvg2/svg/SVGPathSegLinetoVerticalAbs.idl: Added.
            * ksvg2/svg/SVGPathSegLinetoVerticalRel.idl: Added.
            * ksvg2/svg/SVGPathSegMovetoAbs.idl: Added.
            * ksvg2/svg/SVGPathSegMovetoRel.idl: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16106 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -179,6 +179,10 @@ sub AddIncludesForType
     # reorganization, we won't need these special cases.
     if ($codeGenerator->IsPrimitiveType($type)
         or $type eq "DOMString" or $type eq "DOMObject" or $type eq "RGBColor" or $type eq "Rect") {
+    } elsif ($type =~ /SVGPathSeg/) {
+        $joinedName = $type;
+        $joinedName =~ s/Abs|Rel//;
+        $implIncludes{"${joinedName}.h"} = 1;
     } else {
         # default, include the same named file
         $implIncludes{"${type}.h"} = 1;
@@ -1045,6 +1049,7 @@ sub TypeCanFailConversion
            $type eq "XPathEvaluator" or
            $type eq "XPathNSResolver" or
            $type eq "XPathResult" or
+           $type eq "SVGPathSeg" or
            $type eq "SVGMatrix" or
            $type eq "SVGRect" or
            $type eq "SVGElement" or
@@ -1224,6 +1229,12 @@ sub NativeToJSValue
              $type eq "SVGNumber") {
         $implIncludes{"JS$type.h"} = 1;
         return "getJS$type(exec, $value)";
+    } elsif ($type =~ /SVGPathSeg/) {
+        $implIncludes{"JS$type.h"} = 1;
+        $joinedName = $type;
+        $joinedName =~ s/Abs|Rel//;
+        $implIncludes{"$joinedName.h"} = 1;
+        return "toJS(exec, $value)";
     }
 
     # Default, include header with same name.
--- third_party/WebKit/WebCore/ksvg2/svg/SVGPathSeg.cpp
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGPathSeg.cpp
@@ -25,15 +25,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifdef SVG_SUPPORT
 #include "SVGPathSeg.h"
 
-using namespace WebCore;
+namespace WebCore {
 
-SVGPathSeg::SVGPathSeg(const SVGStyledElement *context)
-: Shared<SVGPathSeg>(), m_context(context)
+SVGPathSeg::SVGPathSeg(const SVGStyledElement* context)
+    : Shared<SVGPathSeg>()
+    , m_context(context)
 {
 }
 
 SVGPathSeg::~SVGPathSeg()
 {
+}
+
 }
 
 // vim:ts=4:noet
