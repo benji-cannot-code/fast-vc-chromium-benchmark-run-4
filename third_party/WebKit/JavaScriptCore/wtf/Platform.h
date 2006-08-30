c3f75b32ed60bb6299a2d6a9276cb7b77f5d14bf
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-30 18:29:55 +0000

    JavaScriptCore:
            Reviewed by Tim H.
    
            Commit KDE related tweaks, to be able to
            differentiate between a Qt-only or a KDE build.
    
            * CMakeLists.txt: Install wtf-unity library.
            * wtf/Platform.h: Add define for the KDE platform.
    
    WebCore:
    
            Reviewed by Tim H.
    
            Commit KDE related tweaks, to be able to
            differentiate between a Qt-only or a KDE build.
    
            * CMakeLists.txt: Move global variables up to trunk/CMakeLists.txt.
                              Also rename USE_WEBKIT_SVG_SUPPORT to WEBKIT_USE_SVG_SUPPORT.
    
    WebKitTools:
    
            Reviewed by Tim H.
    
            Commit KDE related tweaks, to be able to
            differentiate between a Qt-only or a KDE build.
    
            * DumpRenderTree/DumpRenderTree.qtproj/CMakeLists.txt: Add ksvg2/ includes.
            * QtLauncher/CMakeLists.txt: Add ksvg2/ includes.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16115 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -70,6 +70,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* PLATFORM(WIN) */
 #if defined(BUILDING_QT__)
 #define KXMLCORE_PLATFORM_QT 1
+
+/* PLATFORM(KDE) */
+#if defined(BUILDING_KDE__)
+#define KXMLCORE_PLATFORM_KDE 1
+#endif
+
 #elif PLATFORM(DARWIN)
 #define KXMLCORE_PLATFORM_MAC 1
 #elif PLATFORM(WIN_OS)
@@ -161,7 +167,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* for Unicode, KDE uses Qt, everything else uses ICU */
 #if PLATFORM(KDE)
-#define KXMLCORE_USE_QT4_UNICODE 1
+// FIXME: Not using Qt4 unicode for now!
+#define KXMLCORE_USE_ICU_UNICODE 1
 #else
 #define KXMLCORE_USE_ICU_UNICODE 1
 #endif
