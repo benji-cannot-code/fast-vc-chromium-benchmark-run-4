0292b3eb746ff3fa78292fd7533e02fea7d71a1b
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-30 08:11:17 +0000

    WebCore:
            Reviewed by Kevin Decker (Sarge)
    
            <rdar://problem/4678414> - New IconDB needs to delete icons when asked
            <rdar://problem/4707718> - If user's Icon directory is unwritable, Safari will crash at startup
    
            * bridge/mac/WebCoreIconDatabaseBridge.h:
            * bridge/mac/WebCoreIconDatabaseBridge.mm:
            (-[WebCoreIconDatabaseBridge removeAllIcons]):  Added
            * loader/icon/IconDatabase.cpp:
            (WebCore::IconDatabase::open): If DB file is not writeable, create an in-memory DB for this session
            (WebCore::IconDatabase::close): Use new deleteAllPreparedStatements()
            (WebCore::IconDatabase::removeAllIcons): Actually implemented
            (WebCore::IconDatabase::deleteAllPreparedStatements): Added for convinience/consistency
            (WebCore::IconDatabase::setPrivateBrowsingEnabled): Use new SQLDatabase::clearAllTables()
            * loader/icon/IconDatabase.h:
            * loader/icon/SQLDatabase.cpp:
            (WebCore::SQLDatabase::clearAllTables): Moved this from IconDatabase as it actually belongs here
            (WebCore::SQLDatabase::vacuum): Added
            * loader/icon/SQLDatabase.h:
            (WebCore::SQLDatabase::path): changed name from getPath()
    
    WebKit:
    
            Reviewed by Kevin Decker (Sarge)
    
            <rdar://problem/4678414> - New IconDB needs to delete icons when asked
    
            * Misc/WebIconDatabase.m:
            (-[WebIconDatabase removeAllIcons]): Call through to WebCore to remove icons, then send notification
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16111 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/icon/IconDatabase.h
+++ third_party/WebKit/WebCore/loader/icon/IconDatabase.h
@@ -66,6 +66,8 @@ public:
     bool isOpen() { return m_mainDB.isOpen() && m_privateBrowsingDB.isOpen(); }
     void close();
     
+    void removeAllIcons();
+    
     bool isEmpty();
  
     Image* iconForPageURL(const String&, const IntSize&, bool cache = true);
@@ -113,10 +115,7 @@ private:
     void forgetIconForIconURLFromDatabase(const String&);
     
     void setIconURLForPageURLInDatabase(const String&, const String&);
-    
-    // Wipe all icons from the DB
-    void removeAllIcons();
-    
+        
     // Called by the startup timer, this method removes all icons that are unretained
     // after initial retains are complete, and pageURLs that are dangling
     void pruneUnretainedIconsOnStartup(Timer<IconDatabase>*);
@@ -133,10 +132,7 @@ private:
     
     // Do a quick check to make sure the database tables are in place and the db version is current
     bool isValidDatabase(SQLDatabase&);
-    
-    // Delete all tables from the given database
-    void clearDatabaseTables(SQLDatabase&);
-    
+        
     // Create the tables and triggers for the given database.
     void createDatabaseTables(SQLDatabase&);
     
@@ -184,6 +180,7 @@ private:
     void imageDataForIconURLQuery(SQLDatabase& db, const String& iconURL, Vector<unsigned char>& result);
     SQLStatement *m_imageDataForIconURLStatement;
 
+    void deleteAllPreparedStatements(bool withSync);
 
     // FIXME: This method is currently implemented in WebCoreIconDatabaseBridge so we can be in ObjC++ and fire off a loader in Webkit
     // Once all of the loader logic is sufficiently moved into WebCore we need to move this implementation to IconDatabase.cpp
