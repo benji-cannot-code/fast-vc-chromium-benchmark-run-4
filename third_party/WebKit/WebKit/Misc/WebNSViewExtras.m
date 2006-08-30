24dc79b67435a8f63d66476882c17069b9d80fdc
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-30 16:50:36 +0000

    Reviewed by Eric and Tim H.
            Bug 10634: -webView:dragDestinationActionMaskForDraggingInfo: is ignored
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10634
            
            Remove the check for canShowFile: from _web_bestURL: since
            it shouldn't be concerned with whether or not the view
            can show the URL, merely return the most appropriate URL.
    
            * Misc/WebNSPasteboardExtras.m:
            (-[NSPasteboard _web_bestURL]):
            * Misc/WebNSViewExtras.m:
            (-[NSView _web_dragOperationForDraggingInfo:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16112 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc/WebNSViewExtras.m
+++ third_party/WebKit/WebKit/Misc/WebNSViewExtras.m
@@ -155,10 +155,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         ![[self window] attachedSheet] &&
         [sender draggingSource] != self &&
         [[sender draggingPasteboard] _web_bestURL]) {
+
         return NSDragOperationCopy;
-    } else {
-        return NSDragOperationNone;
     }
+    
+    return NSDragOperationNone;
 }
 
 - (void)_web_DragImageForElement:(DOMElement *)element
