698d0bf10d366431b7502733f6f4fb701d5994c7
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-08-30 05:07:28 +0000

    WebCore:
            Reviewed by Alice
    
            Added a truth value to setIconURLForPageURL so WebKit can avoid sending a notification
            This is a win on the iBench
    
            * bridge/mac/WebCoreIconDatabaseBridge.h:
            * bridge/mac/WebCoreIconDatabaseBridge.mm:
            (-[WebCoreIconDatabaseBridge _setIconURL:forPageURL:]):
            * loader/icon/IconDatabase.cpp:
            (WebCore::IconDatabase::setIconURLForPageURL):
            * loader/icon/IconDatabase.h:
    
    WebKit:
    
            Reviewed by Alice
    
            Added a truth value check for to setIconURL:forURL so WebKit can avoid sending a notification
            This is a win on the iBench
    
            * Misc/WebIconDatabase.m:
            (-[WebIconDatabase _setIconURL:forURL:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/icon/IconDatabase.cpp
+++ third_party/WebKit/WebCore/loader/icon/IconDatabase.cpp
@@ -569,7 +569,7 @@ void IconDatabase::setHaveNoIconForIconURL(const String& iconURL)
     setIconDataForIconURL(0, 0, iconURL);
 }
 
-void IconDatabase::setIconURLForPageURL(const String& iconURL, const String& pageURL)
+bool IconDatabase::setIconURLForPageURL(const String& iconURL, const String& pageURL)
 {
     ASSERT(!iconURL.isEmpty());
     ASSERT(!pageURL.isEmpty());
@@ -577,7 +577,7 @@ void IconDatabase::setIconURLForPageURL(const String& iconURL, const String& pag
     // If the urls already map to each other, bail.
     // This happens surprisingly often, and seems to cream iBench performance
     if (m_pageURLToIconURLMap.get(pageURL) == iconURL)
-        return;
+        return false;
 
     // If this pageURL is retained, we have some work to do on the IconURL retain counts
     if (m_pageURLToRetainCount.contains(pageURL)) {
@@ -606,6 +606,8 @@ void IconDatabase::setIconURLForPageURL(const String& iconURL, const String& pag
     // Then start the timer to commit this change - or further delay the timer if it
     // was already started
     m_updateTimer.startOneShot(updateTimerDelay);
+    
+    return true;
 }
 
 void IconDatabase::setIconURLForPageURLInDatabase(const String& iconURL, const String& pageURL)
