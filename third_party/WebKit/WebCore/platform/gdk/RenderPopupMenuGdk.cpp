0ce91df59cebf464ff1fbdab2b2b48af432096ed
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-02 19:15:52 +0000

    JavaScriptCore:
            Reviewed by Tim H.
    
            Bug 10454: Unix bakefile fixes
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10454
    
            * JavaScriptCoreSources.bkl:
    
    WebCore:
    
            Reviewed by Tim H.
    
            Bug 10454: Unix bakefile fixes
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10454
    
            * WebCoreSources.bkl:
            * platform/gdk/RenderPopupMenuGdk.cpp:
            (WebCore::RenderPopupMenuGdk::RenderPopupMenuGdk):
            * platform/gdk/RenderPopupMenuGdk.h:
            * platform/gdk/RenderThemeGdk.cpp:
            (WebCore::RenderThemeGdk::createPopupMenu):
            (WebCore::RenderThemeGdk::systemFont):
            * platform/gdk/RenderThemeGdk.h:
            * platform/gdk/TemporaryLinkStubs.cpp:
            * webcore-base.bkl:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gdk/RenderPopupMenuGdk.cpp
+++ third_party/WebKit/WebCore/platform/gdk/RenderPopupMenuGdk.cpp
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-RenderPopupMenuGdk::RenderPopupMenuGdk(Node* element)
-    : RenderPopupMenu(element)
+RenderPopupMenuGdk::RenderPopupMenuGdk(Node* element,  RenderMenuList* list)
+    : RenderPopupMenu(element,list)
 {
 }
 
--- third_party/WebKit/WebCore/platform/gdk/RenderPopupMenuGdk.h
+++ third_party/WebKit/WebCore/platform/gdk/RenderPopupMenuGdk.h
@@ -34,7 +34,7 @@ class HTMLOptGroupElement;
 
 class RenderPopupMenuGdk : public RenderPopupMenu {
 public:
-    RenderPopupMenuGdk(Node*);
+    RenderPopupMenuGdk(Node*,  RenderMenuList*);
     ~RenderPopupMenuGdk();
 
     virtual void clear();
