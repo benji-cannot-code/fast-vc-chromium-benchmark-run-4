4250d19f2f84864910cebe9d6d5a646049867280
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-02 17:25:46 +0000

    Reviewed by Anders.
            - Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=10667
              Password: Double-click should select-all to avoid exposing word boundaries
    
            Test: fast/forms/password-doubleclick-selection.html
    
            * editing/visible_units.cpp:
            (WebCore::previousBoundary): When searching for boundaries in renderers that use the textSecurity property, 
             convert characters in strings to alpha-numeric characters (in this case, all 'x's) so that every character
             isn't treated as a punctuation boundary.
            (WebCore::nextBoundary): ditto.
            * editing/TextIterator.cpp: (WebCore::SimplifiedBackwardsTextIterator::handleTextNode): Updated to use the renderer's 
              string instead of the node value.  This matches the base class implementation of handleTextNode.
    
            Code cleanup.
            * editing/ReplaceSelectionCommand.cpp: (WebCore::ReplaceSelectionCommand::doApply): 
              Avoid unnecessary check for password field case if the smart replace condition is false.
            * page/Frame.cpp: (WebCore::Frame::mayCopy): Removed duplicate code.  Calls isSelectionInPasswordField now.
            * rendering/RenderText.cpp: (WebCore::RenderText::setText): Updated formatting.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16192 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -2190,13 +2190,7 @@ void Frame::pasteAndMatchStyle()
 
 bool Frame::mayCopy()
 {
-    Node* startNode = selection().start().node();
-    if (startNode) {
-        startNode = startNode->shadowAncestorNode();
-        if (startNode->hasTagName(inputTag) && static_cast<HTMLInputElement*>(startNode)->inputType() == HTMLInputElement::PASSWORD)
-            return false;
-    }
-    return true;
+    return !isSelectionInPasswordField();
 }
 
 void Frame::transpose()
