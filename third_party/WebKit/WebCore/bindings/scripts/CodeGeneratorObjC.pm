bcfa6b2b8dfc072b9a18843feec1e64b2bda2967
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-03 21:11:09 +0000

    Build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@16204 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorObjC.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorObjC.pm
@@ -30,6 +30,8 @@ my $outputDir = "";
 my %implIncludes = ();
 my %headerForwardDeclarations = ();
 my %headerForwardDeclarationsForProtocols = ();
+my $buildingForTigerOrEarlier = 1 if $ENV{"MACOSX_DEPLOYMENT_TARGET"} and $ENV{"MACOSX_DEPLOYMENT_TARGET"} <= 10.4;
+my $buildingForLeopardOrLater = 1 if $ENV{"MACOSX_DEPLOYMENT_TARGET"} and $ENV{"MACOSX_DEPLOYMENT_TARGET"} >= 10.5;
 
 my $exceptionInit = "WebCore::ExceptionCode ec = 0;";
 my $exceptionRaiseOnError = "raiseOnDOMError(ec);";
@@ -453,7 +455,7 @@ sub GenerateHeader
             my $attributeType = GetObjCType($attribute->signature->type);
             my $attributeIsReadonly = ($attribute->type =~ /^readonly/);
 
-            if ($ENV{"MACOSX_DEPLOYMENT_TARGET"} and $ENV{"MACOSX_DEPLOYMENT_TARGET"} >= 10.5) {
+            if ($buildingForLeopardOrLater) {
                 my $property = "\@property" . ($attributeIsReadonly ? "(readonly)" : "") . " " . $attributeType . ($attributeType =~ /\*$/ ? "" : " ") . $attributeName . ";\n";
                 push(@headerAttributes, $property);
             } else {
@@ -514,7 +516,7 @@ sub GenerateHeader
             if (@{$function->parameters} > 1 and $function->signature->extendedAttributes->{"OldStyleObjC"}) {
                 my $deprecatedFunctionSig = $functionSig;
                 $deprecatedFunctionSig =~ s/\s\w+:/ :/g; # remove parameter names
-                $deprecatedFunctionSig =~ s/;\n$/ DEPRECATED_IN_MAC_OS_X_VERSION_10_5_AND_LATER;\n/;
+                $deprecatedFunctionSig =~ s/;\n$/ DEPRECATED_IN_MAC_OS_X_VERSION_10_5_AND_LATER;\n/ if $buildingForLeopardOrLater;
                 push(@deprecatedHeaderFunctions, $deprecatedFunctionSig);
             }
         }
