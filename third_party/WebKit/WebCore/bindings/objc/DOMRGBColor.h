7907712575377f85f58699b191ff0f5b7cfe7e9d
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-03 13:45:00 +0000

    Fixes build for regression tests.
    git-svn-id: svn://svn.chromium.org/blink/trunk@16197 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/DOMRGBColor.h
+++ third_party/WebKit/WebCore/bindings/objc/DOMRGBColor.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebCore/DOMObject.h>
 
+@class NSColor;
 @class DOMCSSPrimitiveValue;
 
 @interface DOMRGBColor : DOMObject
