1f54021530a500323892b3e6da9a324489f13ed0
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-06 07:09:41 +0000

    2006-09-05  MorganL  <morlmor@yahoocom>
            Reviewed/landed by aroben.
    
            Fixes bug 10743: Windows build is busted.
    
            WebCore:
                    * WebCore.vcproj/WebCore/WebCore.vcproj:
                    * bridge/win/FrameWin.h:
                    * platform/ResourceLoaderInternal.h:
                    * platform/TextEncodingRegistry.cpp:
                    * platform/win/ResourceLoaderWin.cpp:
                    (WebCore::ResourceLoaderWndProc):
                    (WebCore::ResourceLoader::start):
                    * platform/win/TemporaryLinkStubs.cpp:
                    (FrameWin::markMisspellings):
                    (FrameWin::shouldChangeSelection):
                    (FrameWin::respondToChangedSelection):
                    (FrameWin::respondToChangedContents):
                    (RenderThemeWin::adjustMenuListStyle):
    
            WebKit:
                    * COM/WebView.cpp:
                    (WebView::keyPress):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16248 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/TextEncodingRegistry.cpp
+++ third_party/WebKit/WebCore/platform/TextEncodingRegistry.cpp
@@ -39,6 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if PLATFORM(MAC)
 #include "StreamingTextDecoderMac.h"
 #endif
+#if PLATFORM(WIN)
+#include <ctype.h>
+#endif
 
 namespace WebCore {
 
