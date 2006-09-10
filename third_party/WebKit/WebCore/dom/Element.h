fb3c8b05ad55a59842c02261980df64cb9be6e85
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-10 04:55:03 +0000

    Implement support for the WinIE clientLeft and clientTop extensions.
            Reviewed by aroben
    
            * dom/Element.cpp:
            (WebCore::Element::clientLeft):
            (WebCore::Element::clientTop):
            * dom/Element.h:
            * dom/Element.idl:
            * rendering/RenderObject.h:
            (WebCore::RenderObject::clientLeft):
            (WebCore::RenderObject::clientTop):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16279 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Element.h
+++ third_party/WebKit/WebCore/dom/Element.h
@@ -78,6 +78,8 @@ public:
     int offsetWidth();
     int offsetHeight();
     Element* offsetParent();
+    int clientLeft();
+    int clientTop();
     int clientWidth();
     int clientHeight();
     int scrollLeft();
