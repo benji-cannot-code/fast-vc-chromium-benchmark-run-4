5da0b644c3a132b2e82b6f2910367a2025847474
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-11 02:26:10 +0000

    Forgetting header #define protection is BAAAAAD         (and re-alphabatized the project file)
            * WebCore.xcodeproj/project.pbxproj:
            * platform/IntSizeHash.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16292 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/IntSizeHash.h
+++ third_party/WebKit/WebCore/platform/IntSizeHash.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Boston, MA 02111-1307, USA.
  *
  */
+#ifndef INTSIZEHASH_H_
+#define INTSIZEHASH_H_
 
 #include "IntSize.h"
 #include <wtf/HashMap.h>
@@ -33,3 +35,4 @@ namespace WTF {
 
 } // namespace WTF
 
+#endif
