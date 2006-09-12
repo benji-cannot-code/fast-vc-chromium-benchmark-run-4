5a5ea86bb846e1721b026fb3be350850cd1fef30
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-12 00:22:53 +0000

    Build fix. KeyScript is deprecated in Leopard, we need a new solution for this <rdar://problem/4727607>
    git-svn-id: svn://svn.chromium.org/blink/trunk@16311 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/mac/FrameMac.mm
+++ third_party/WebKit/WebCore/bridge/mac/FrameMac.mm
@@ -3351,10 +3351,15 @@ void FrameMac::setSecureKeyboardEntry(bool enable)
 {
     if (enable) {
         EnableSecureEventInput();
+// FIXME: KeyScript is deprecated in Leopard, we need a new solution for this <rdar://problem/4727607>
+#if MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_4
         KeyScript(enableRomanKeyboardsOnly);
+#endif
     } else {
         DisableSecureEventInput();
+#if MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_4
         KeyScript(smKeyEnableKybds);
+#endif
     }
 }
 
