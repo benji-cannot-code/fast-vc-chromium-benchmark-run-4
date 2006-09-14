fe8bfe754576276ae81675eafb58d30be6371fa4
sfalken <sfalken@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-14 01:59:26 +0000

    2006-09-12  Steve Falkenburg  <sfalken@apple.com>
            Reviewed by Ada.
            
            Build tweaks (doing JavaScriptCore now since it doesn't have
            dependencies).
    
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj:
            * JavaScriptCore.vcproj/JavaScriptCore/build-generated-files.sh:
            * JavaScriptCore.vcproj/JavaScriptCore/dstroot-to-sdk.cmd: Added.
            * JavaScriptCore.vcproj/dftables/dftables.vcproj:
            * JavaScriptCore.vcproj/testkjs/testkjs.vcproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/build-generated-files.sh
+++ third_party/WebKit/JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/build-generated-files.sh
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebKitUnixDir=`cygpath -a -u "$2"`
 
 # Set up the directory that will hold all our generated files.
-DerivedSourcesDir="$WebKitUnixDir/JavaScriptCore.intermediate/$1/JavaScriptCore.intermediate/DerivedSources"
+DerivedSourcesDir="$WebKitUnixDir/obj/JavaScriptCore/$1/DerivedSources"
 mkdir -p "$DerivedSourcesDir"
 
 # Invoke the create_hash_table perl script to create all of our lookup tables
@@ -62,8 +62,10 @@ if [ ../../kjs/grammar.y -nt "$DerivedSourcesDir/grammar.cpp" ]; then
 fi
 
 echo "Copying ICU DLLS..."
-WebKitOutputConfigDir="$WebKitUnixDir/$1"
+WebKitOutputConfigDir="$WebKitUnixDir/bin/JavaScriptCore/$1"
 mkdir -p "$WebKitOutputConfigDir"
 
-cp ../../../icu/bin/icudt34.dll "$WebKitOutputConfigDir" || exit 1
-cp ../../../icu/bin/icuuc34.dll "$WebKitOutputConfigDir" || exit 1
+if [ -e "../../../icu/bin/icudt34.dll" ]; then
+    cp ../../../icu/bin/icudt34.dll "$WebKitOutputConfigDir" || exit 1
+    cp ../../../icu/bin/icuuc34.dll "$WebKitOutputConfigDir" || exit 1
+fi
