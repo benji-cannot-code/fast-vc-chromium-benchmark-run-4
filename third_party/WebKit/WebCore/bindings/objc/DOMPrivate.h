6f03d2a06991fa7081f092220b5833ab87a3473b
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-16 15:13:59 +0000

    2006-09-16  Mark Rowe  <opendarwin.org@bdash.net.nz>
            Reviewed by Anders.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10887
            Fix build error
    
            * bindings/objc/DOMPrivate.h: Remove reference to DOMEventPrivate.h.
    
    2006-09-16  Mark Rowe  <opendarwin.org@bdash.net.nz>
    
            Reviewed by Anders.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10887
            Fix build error
    
            * MigrateHeaders.make: Remove reference to DOMEventPrivate.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/DOMPrivate.h
+++ third_party/WebKit/WebCore/bindings/objc/DOMPrivate.h
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebCore/DOMDocumentPrivate.h>
 #import <WebCore/DOMElementPrivate.h>
-#import <WebCore/DOMEventPrivate.h>
 #import <WebCore/DOMHTMLAnchorElementPrivate.h>
 #import <WebCore/DOMHTMLAreaElementPrivate.h>
 #import <WebCore/DOMHTMLBodyElementPrivate.h>
