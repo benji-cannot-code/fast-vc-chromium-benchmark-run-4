f6d4b27b6eb3b2e604697f11738952e0d8561691
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-18 06:45:54 +0000

    2006-09-17  Eric Seidel  <eric@eseidel.com>
            Reviewed by ap.
            
            REGRESSION (r16245): double-clicking on javascript exceptions fails to show source
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10813
    
            * bridge/mac/WebCoreFrameBridge.mm:
            (+[WebCoreFrameBridge stringWithData:textEncodingName:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16419 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/mac/WebCoreFrameBridge.mm
+++ third_party/WebKit/WebCore/bridge/mac/WebCoreFrameBridge.mm
@@ -1489,7 +1489,8 @@ static HTMLFormElement *formElementFromDOMElement(DOMElement *element)
 
 + (NSString *)stringWithData:(NSData *)data textEncodingName:(NSString *)textEncodingName
 {
-    return WebCore::TextEncoding(textEncodingName).decode(reinterpret_cast<const char*>([data bytes]), [data length]);
+    NSString* name = textEncodingName ? textEncodingName : @"iso-8859-1";
+    return WebCore::TextEncoding(name).decode(reinterpret_cast<const char*>([data bytes]), [data length]);
 }
 
 - (BOOL)needsLayout
