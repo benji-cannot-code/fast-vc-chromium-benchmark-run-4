89227e911d528dfc6399623e13fddce09a177c23
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-19 09:37:10 +0000

    2006-09-19  Krzysztof Kowalczyk <kkowalczyk@gmail.com>
            Reviewed by eseidel.  Landed by eseidel.
            
            Build fix only, no tests affected.
    
            * platform/gdk/FontPlatformDataGdk.cpp:
            (WebCore::FontPlatformData::FontPlatformData):
            (WebCore::FontPlatformData::~FontPlatformData):
            * platform/gdk/FrameGdk.h:
            * platform/gdk/TemporaryLinkStubs.cpp:
            (FrameGdk::isLoadTypeReload):
            (FrameGdk::originalRequestURL):
            (IconDatabase::setIconURLForPageURL):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gdk/FrameGdk.h
+++ third_party/WebKit/WebCore/platform/gdk/FrameGdk.h
@@ -128,6 +128,7 @@ public:
     virtual bool shouldInterruptJavaScript();
 
     bool keyPress(const PlatformKeyboardEvent&);
+    virtual KURL originalRequestURL() const;
 
     virtual void receivedData(ResourceLoader*, const char*, int);
     virtual void receivedAllData(ResourceLoader*,PlatformData);
@@ -137,7 +138,9 @@ public:
     virtual Frame* createFrame(const KURL&, const String& name, Element* ownerElement, const String& referrer);
 
 private:
+    virtual bool isLoadTypeReload();
     virtual bool passMouseDownEventToWidget(Widget*);
+
     FrameGdkClient* m_client;
     GdkDrawable* m_drawable;
 };
