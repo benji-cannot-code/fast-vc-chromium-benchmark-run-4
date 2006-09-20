be9f372cc0f38eca9a9f00dd65fdf1487a9f5ab6
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-20 09:52:25 +0000

    Fix for 10945, WheelEvent should support two dimensions at once, since         MacBooks allow for diagonal scrolling.  Also enhance the PlatformWheelEvent         to allow for diagonal scrolling.
            Reviewed by Eric
    
            * dom/EventTargetNode.cpp:
            (WebCore::EventTargetNode::dispatchWheelEvent):
            * dom/WheelEvent.cpp:
            (WebCore::WheelEvent::WheelEvent):
            (WebCore::WheelEvent::initWheelEvent):
            * dom/WheelEvent.h:
            (WebCore::WheelEvent::wheelDelta):
            (WebCore::WheelEvent::wheelDeltaX):
            (WebCore::WheelEvent::wheelDeltaY):
            * dom/WheelEvent.idl:
            * page/FrameView.cpp:
            (WebCore::FrameView::handleWheelEvent):
            * platform/PlatformWheelEvent.h:
            (WebCore::PlatformWheelEvent::deltaX):
            (WebCore::PlatformWheelEvent::deltaY):
            (WebCore::PlatformWheelEvent::normalize):
            * platform/mac/WheelEventMac.mm:
            (WebCore::PlatformWheelEvent::PlatformWheelEvent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16467 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/EventTargetNode.cpp
+++ third_party/WebKit/WebCore/dom/EventTargetNode.cpp
@@ -447,7 +447,7 @@ bool EventTargetNode::dispatchMouseEvent(const AtomicString& eventType, int butt
 void EventTargetNode::dispatchWheelEvent(PlatformWheelEvent& e)
 {
     assert(!eventDispatchForbidden());
-    if (e.delta() == 0)
+    if (e.deltaX() == 0 && e.deltaY() == 0)
         return;
     
     FrameView* view = document()->view();
@@ -456,7 +456,7 @@ void EventTargetNode::dispatchWheelEvent(PlatformWheelEvent& e)
     
     IntPoint pos = view->viewportToContents(e.pos());
     
-    RefPtr<WheelEvent> we = new WheelEvent(e.isHorizontal(), e.delta(),
+    RefPtr<WheelEvent> we = new WheelEvent(e.deltaX(), e.deltaY(),
                                            document()->defaultView(), e.globalX(), e.globalY(), pos.x(), pos.y(),
                                            e.ctrlKey(), e.altKey(), e.shiftKey(), e.metaKey());
     ExceptionCode ec = 0;
