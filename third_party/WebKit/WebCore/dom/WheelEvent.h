cee81924114e7ea6af9cdee86939ea21e8ce7d98
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-20 14:23:03 +0000

    Build Fix.  Adds isHorizontal attribute back to WheelEvent         for the Objective-C bindings.  If it is found that isHorizontal         is not used, we should remove it again but also remove it's         declaration from PublicDOMInterfaces.h
            * dom/WheelEvent.h:
            (WebCore::WheelEvent::isHorizontal):
            * dom/WheelEvent.idl:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16468 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/WheelEvent.h
+++ third_party/WebKit/WebCore/dom/WheelEvent.h
@@ -47,6 +47,9 @@ namespace WebCore {
         int wheelDeltaX() const { return m_wheelDeltaX; }
         int wheelDeltaY() const { return m_wheelDeltaY; }
 
+        // Needed for Objective-C legacy support
+        bool isHorizontal() const { return m_wheelDeltaX; }
+
     private:
         virtual bool isWheelEvent() const;
 
