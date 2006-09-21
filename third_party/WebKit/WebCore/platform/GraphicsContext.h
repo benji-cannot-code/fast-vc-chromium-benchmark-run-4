2e7ac26b0b7d5e881031b62b9e596360dbff3a56
seangies <seangies@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-21 21:58:40 +0000

    Factored out get/release of HDC from cairo GraphicsContext.
    git-svn-id: svn://svn.chromium.org/blink/trunk@16511 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/GraphicsContext.h
+++ third_party/WebKit/WebCore/platform/GraphicsContext.h
@@ -170,6 +170,8 @@ namespace WebCore {
 
 #if PLATFORM(WIN)
         GraphicsContext(HDC); // FIXME: To be removed.
+        HDC getWindowsContext();
+        void releaseWindowsContext();
 #endif
 
     private:
