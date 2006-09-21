b085a2e9c5c36b7b6720fce7b648ecbafd82f008
rwlbuis <rwlbuis@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-21 07:29:08 +0000

    Reviewed by eseidel.
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10946
            new marker code draws end-marker in wrong place
    
            Keep track of the start of the subpath so we can handle
            closeTo/moveTo correctly.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kcanvas/RenderPath.cpp
+++ third_party/WebKit/WebCore/kcanvas/RenderPath.cpp
@@ -315,6 +315,7 @@ enum MarkerType {
 
 struct MarkerData {
     FloatPoint origin;
+    FloatPoint subpathStart;
     double strokeWidth;
     FloatPoint inslopePoints[2];
     FloatPoint outslopePoints[2];
@@ -336,6 +337,7 @@ DrawMarkersData::DrawMarkersData(GraphicsContext* c, KCanvasMarker *start, KCanv
     , midMarker(mid)
 {
     previousMarkerData.origin = FloatPoint();
+    previousMarkerData.subpathStart = FloatPoint();
     previousMarkerData.strokeWidth = strokeWidth;
     previousMarkerData.marker = start;
     previousMarkerData.type = Start;
@@ -383,11 +385,17 @@ static inline void updateMarkerDataForElement(MarkerData &previousMarkerData, co
         previousMarkerData.origin = points[2];
         break;
     case PathElementMoveToPoint:
+        previousMarkerData.subpathStart = points[0];
     case PathElementAddLineToPoint:
-    case PathElementCloseSubpath:
         previousMarkerData.inslopePoints[0] = previousMarkerData.origin;
         previousMarkerData.inslopePoints[1] = points[0];
         previousMarkerData.origin = points[0];
+        break;
+    case PathElementCloseSubpath:
+        previousMarkerData.inslopePoints[0] = previousMarkerData.origin;
+        previousMarkerData.inslopePoints[1] = points[0];
+        previousMarkerData.origin = previousMarkerData.subpathStart;
+        previousMarkerData.subpathStart = FloatPoint();
     }
 }
 
