04e03c906584a7176e67877988d7b08dd9ad41bc
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-21 22:36:08 +0000

    2006-09-21  Matt Lilek  <pewtermoose@gmail.com>
            Reviewed by Eric.
    
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10975
            Update the blog template to valid HTML 4.01 Strict.
    
            * blog/wp-content/themes/webkit/comments-popup.php:
            * blog/wp-content/themes/webkit/comments.php:
            * blog/wp-content/themes/webkit/footer.php:
            * blog/wp-content/themes/webkit/header.php:
            * blog/wp-content/themes/webkit/style.css: Add missing semi-colon that the validator complained about.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitSite/blog/wp-content/themes/webkit/footer.php
+++ third_party/WebKit/WebKitSite/blog/wp-content/themes/webkit/footer.php
@@ -4,12 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php get_sidebar(); ?>
 
 <div id="footer">
-	<?php bloginfo('name'); ?> site is powered by 
-	<a href="http://wordpress.org">WordPress</a>
-	<br /><a href="feed:<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-	and <a href="feed:<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
-	<br> <?php  wp_register("", " <strong>|</strong> "); wp_loginout(); ?>
-	<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. -->
+    <?php bloginfo('name'); ?> site is powered by 
+    <a href="http://wordpress.org">WordPress</a>
+    <br><a href="feed:<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
+    and <a href="feed:<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+    <br> <?php  wp_register("", " <strong>|</strong> "); wp_loginout(); ?>
+    <!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. -->
 </div>
 
 <?php wp_footer(); ?>
