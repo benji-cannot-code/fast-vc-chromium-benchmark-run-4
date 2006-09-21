72f4f65eb828b4d4329a9ff03041ca885b1b287a
rwlbuis <rwlbuis@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-21 21:22:14 +0000

    Hmm, forgot to commit kcanvas/ changes as part of linux build fixes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@16509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/kcanvas/device/qt/KCanvasClipperQt.h
+++ third_party/WebKit/WebCore/kcanvas/device/qt/KCanvasClipperQt.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef KCanvasClipperQt_h
 #define KCanvasClipperQt_h
 
-#include "KCanvasResources.h"
+#include "KCanvasClipper.h"
 
 namespace WebCore {
 
--- third_party/WebKit/WebCore/kcanvas/device/qt/KRenderingDeviceQt.cpp
+++ third_party/WebKit/WebCore/kcanvas/device/qt/KRenderingDeviceQt.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AffineTransform.h"
 #include "GraphicsContext.h"
 #include "KCanvasClipperQt.h"
+#include "KCanvasMarker.h"
 #include "KRenderingDeviceQt.h"
 #include "KRenderingPaintServerSolidQt.h"
 #include "KRenderingPaintServerGradientQt.h"
