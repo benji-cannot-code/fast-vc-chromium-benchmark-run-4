796b633c37d565c54fbf6f1fddc6b57d6f6f75b7
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-22 18:18:03 +0000

    Reviewed by Geoff.
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10803
            REGRESSION (r15536-r15544): manual-tests/bugzilla-6821.html failing
    
            * page/FrameView.cpp:
            (WebCore::FrameView::hoverTimerFired): Use the current event.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -1238,7 +1238,7 @@ void FrameView::layoutTimerFired(Timer<FrameView>*)
 void FrameView::hoverTimerFired(Timer<FrameView>*)
 {
     d->hoverTimer.stop();
-    prepareMouseEvent(false, false, true, PlatformMouseEvent());
+    prepareMouseEvent(false, false, true, PlatformMouseEvent(PlatformMouseEvent::currentEvent));
 }
 
 void FrameView::scheduleRelayout()
