0814434286b61c947637d5f0504f81fcfdb8ac7d
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-25 17:37:39 +0000

    Build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@16558 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/DOMRangeException.h
+++ third_party/WebKit/WebCore/bindings/objc/DOMRangeException.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
+@class NSString;
+
 extern NSString * const DOMRangeException;
 
 enum DOMRangeExceptionCode {
