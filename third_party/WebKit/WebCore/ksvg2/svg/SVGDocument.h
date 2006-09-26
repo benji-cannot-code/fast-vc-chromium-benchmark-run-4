e548304315ace490a04c5ac4ba2cbb8ae3ad2b1c
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-26 12:56:30 +0000

    2006-09-26  Eric Seidel  <eric@eseidel.com>
            Reviewed by Tim H.
    
            SVGDocument::createElement does not create elements in the SVG namespace
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10932
            
            Test: svg/custom/createelement.svg
    
            * ksvg2/svg/SVGDocument.cpp:
            (WebCore::SVGDocument::createElement):
            * ksvg2/svg/SVGDocument.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16575 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGDocument.h
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGDocument.h
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     Boston, MA 02111-1307, USA.
 */
 
-#ifndef KSVG_SVGDocumentImpl_H
-#define KSVG_SVGDocumentImpl_H
+#ifndef SVGDocument_H
+#define SVGDocument_H
 #ifdef SVG_SUPPORT
 
 #include "Document.h"
@@ -40,7 +40,9 @@ namespace WebCore {
         
         virtual bool isSVGDocument() const { return true; }
 
-        SVGSVGElement *rootElement() const;
+        SVGSVGElement* rootElement() const;
+        
+        virtual PassRefPtr<Element> SVGDocument::createElement(const String& tagName, ExceptionCode&);
         
         void dispatchZoomEvent(float prevScale, float newScale);
         void dispatchScrollEvent();
@@ -49,6 +51,6 @@ namespace WebCore {
 } // namespace WebCore
 
 #endif // SVG_SUPPORT
-#endif // KSVG_SVGDocumentImpl_H
+#endif // SVGDocument_H
 
 // vim:ts=4:noet
