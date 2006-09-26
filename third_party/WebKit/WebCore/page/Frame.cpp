9e4e5099543e61ecca1b51a29bbe0c9d226da2b6
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-26 17:12:45 +0000

    Reviewed by Darin.
            Temporary work-around for frame lifetime issue.
    
            * page/Frame.cpp:
            (WebCore::Frame::clear):
            (WebCore::Frame::disconnectOwnerElement):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16585 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -468,6 +468,14 @@ void Frame::clear(bool clearWindowProperties)
   d->m_bCleared = true;
   d->m_mousePressNode = 0;
 
+#if !PLATFORM(MAC)
+  // FIXME: This is a temporary hack to work around a mismatch between WebCore and WebKit
+  // regarding frame lifetime. The proper solution is to move all frame management
+  // into WebCore, so frames can work the same way on all platforms.
+  for (Frame* descendant = tree()->firstChild(); descendant; descendant = descendant->tree()->traverseNext())
+      descendant->disconnectOwnerElement();
+#endif
+
   if (d->m_doc) {
     d->m_doc->cancelParsing();
     d->m_doc->detach();
@@ -3640,7 +3648,7 @@ void Frame::started()
 
 void Frame::disconnectOwnerElement()
 {
-    if (d->m_ownerElement)
+    if (d->m_ownerElement && d->m_page)
         d->m_page->decrementFrameCount();
         
     d->m_ownerElement = 0;
