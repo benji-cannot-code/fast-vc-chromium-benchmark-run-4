77ae5026ad94334a8856ecb27ec5d98eb49bdf8a
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-26 19:19:32 +0000

    LayoutTests:
            Reviewed by John and TimH.
            
            <rdar://problem/4743256> Seed: Ctrl-Y key binding does nothing when kill ring is empty
    
            * editing/pasteboard/emacs-cntl-y-001-expected.checksum: Added.
            * editing/pasteboard/emacs-cntl-y-001-expected.png: Added.
            * editing/pasteboard/emacs-cntl-y-001-expected.txt: Added.
            * editing/pasteboard/emacs-cntl-y-001.html: Added.
    
    WebKit:
    
            Reviewed by John and TimH.
    
            <rdar://problem/4743256> Seed: Ctrl-Y key binding does nothing when kill ring is empty
    
            Use deleteBackward: when the killring string is empty. Was always using insertText:,
            but that ends up early-returning if the string to insert is empty.
            
            * WebView/WebHTMLView.m:
            (-[NSArray yank:]):
            (-[NSArray yankAndSelect:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16586 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView/WebHTMLView.m
@@ -4571,8 +4571,13 @@ NSStrokeColorAttributeName        /* NSColor, default nil: same as foreground co
 {
     if (![self _canEdit])
         return;
-        
-    [self insertText:_NSYankFromKillRing()];
+    
+    NSString* yankee = _NSYankFromKillRing();
+    if ([yankee length])
+        [self insertText:yankee];
+    else
+        [self deleteBackward:nil];
+
     _NSSetKillRingToYankedState();
 }
 
@@ -4581,7 +4586,12 @@ NSStrokeColorAttributeName        /* NSColor, default nil: same as foreground co
     if (![self _canEdit])
         return;
 
-    [self _insertText:_NSYankPreviousFromKillRing() selectInsertedText:YES];
+    NSString* yankee = _NSYankPreviousFromKillRing();
+    if ([yankee length])
+        [self _insertText:yankee selectInsertedText:YES];
+    else
+        [self deleteBackward:nil];
+        
     _NSSetKillRingToYankedState();
 }
 
