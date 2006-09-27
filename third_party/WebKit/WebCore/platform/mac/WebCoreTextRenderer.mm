ed6ce7a32fcda8eecbc5e73337bf96d757df7c8a
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-27 00:56:36 +0000

    Reviewed by Steve.
            - clean up options for font code path
    
            * platform/Font.h:
            * platform/Font.cpp:
            (WebCore::Font::setCodePath): Added. Replaces boolean version.
            (WebCore::Font::canUseGlyphCache): Update to handle "never use complex" case too.
    
            * platform/mac/WebCoreTextRenderer.mm:
            (WebCoreSetAlwaysUseATSU): Change to call setCodePath.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/WebCoreTextRenderer.mm
+++ third_party/WebKit/WebCore/platform/mac/WebCoreTextRenderer.mm
@@ -83,7 +83,7 @@ bool WebCoreShouldUseFontSmoothing()
 
 void WebCoreSetAlwaysUseATSU(bool useATSU)
 {
-    Font::setAlwaysUseComplexPath(useATSU);
+    Font::setCodePath(useATSU ? Font::Complex : Font::Auto);
 }
 
 NSFont* WebCoreFindFont(NSString* familyName, NSFontTraitMask traits, int size)
