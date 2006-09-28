0c745acc4dfa8ed4d2f3aeeb792815c95947cb6d
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-28 21:33:07 +0000

    Reviewed by Adele.
            Remove no-longer-used PopUpButton.
    
            * WebCore.vcproj/WebCore/WebCore.vcproj: Remove PopUpButton.h
            * WebCore.xcodeproj/project.pbxproj: Remove PopUpButton.h,
    	PopUpButtonMac.mm
            * platform/PopUpButton.h: Removed.
            * platform/mac/PopUpButtonMac.mm: Removed.
            * platform/win/TemporaryLinkStubs.cpp: Remove PopUpButton:: methods
            * rendering/DeprecatedRenderSelect.cpp: Change #include of
    	PopUpButton.h to ListBox.h
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/win/TemporaryLinkStubs.cpp
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TextField.h"
 #include "FileChooser.h"
 #include "Font.h"
-#include "PopUpButton.h"
+#include "ListBox.h"
 #include "IntPoint.h"
 #include "Widget.h"
 #include "GraphicsContext.h"
@@ -79,9 +79,6 @@ using namespace WebCore;
 } while (0)
 
 void FrameView::updateBorder() { notImplemented(); }
-
-Widget::FocusPolicy PopUpButton::focusPolicy() const { notImplemented(); return NoFocus; }
-void PopUpButton::populate() { notImplemented(); }
 
 void Widget::enableFlushDrawing() { notImplemented(); }
 bool Widget::isEnabled() const { notImplemented(); return 0; }
@@ -331,18 +328,6 @@ void TextField::setColors(Color const&,Color const&) { }
 IntSize TextField::sizeForCharacterWidth(int) const { return IntSize(); }
 int TextField::baselinePosition(int) const { return 0; }
 void TextField::setLiveSearch(bool) { }
-
-PopUpButton::PopUpButton() { }
-PopUpButton::~PopUpButton() { }
-void PopUpButton::setFont(WebCore::Font const&) { }
-int PopUpButton::baselinePosition(int) const { return 0; }
-void PopUpButton::setWritingDirection(TextDirection) { }
-void PopUpButton::clear() { }
-void PopUpButton::appendItem(DeprecatedString const&,ListBoxItemType,bool) { }
-void PopUpButton::setCurrentItem(int) { }
-IntSize PopUpButton::sizeHint() const { return IntSize(); }
-IntRect PopUpButton::frameGeometry() const { return IntRect(); }
-void PopUpButton::setFrameGeometry(IntRect const&) { }
 
 PlatformScrollBar::PlatformScrollBar(ScrollBarClient* client, ScrollBarOrientation orientation) : ScrollBar(client, orientation) { }
 PlatformScrollBar::~PlatformScrollBar() { }
