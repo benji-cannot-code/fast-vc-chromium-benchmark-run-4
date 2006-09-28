cbccf94bca4dbf665c349cde931df5ca590e129a
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-28 00:58:52 +0000

    Reviewed by Adele.
            Fixes a GC stack overflow crash.
            The change is to move from a linked list implementation of Parameters to a Vector.
            The problem with the linked list is that each one creates it's own stack frame when being destroyed and in extreme cases this caused the stack to overflow. 
    
            * kjs/function.cpp:
            (KJS::Parameter::Parameter):
            (KJS::FunctionImp::addParameter):
            (KJS::FunctionImp::parameterString):
            (KJS::FunctionImp::processParameters):
            (KJS::FunctionImp::lengthGetter):
            (KJS::FunctionImp::getParameterName):
            * kjs/function.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16606 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/function.h
+++ third_party/WebKit/JavaScriptCore/kjs/function.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "internal.h"
 #include <wtf/OwnPtr.h>
+#include <wtf/Vector.h>
 
 namespace KJS {
 
@@ -93,7 +94,7 @@ namespace KJS {
 
     virtual void mark();
   protected:
-    OwnPtr<Parameter> param;
+    Vector<Parameter> params;
 
   private:
     ScopeChain _scope;
