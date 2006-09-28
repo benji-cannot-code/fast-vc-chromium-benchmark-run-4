35a907622e5da1fcce554fbd064993b93864f072
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-09-28 21:28:49 +0000

    Suggested by Darin.
            Moved an extern declaration from inside a method to the top of the file.
    
            * DumpRenderTree/EventSendingController.m:
            (-[EventSendingController clearKillRing]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16623 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/EventSendingController.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/EventSendingController.m
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKit.h>
 #import <WebKit/DOMPrivate.h>
 
+extern void _NSNewKillRingSequence();
+
 NSPoint lastMousePosition;
 NSArray *webkitDomEventNames;
 
@@ -137,8 +139,6 @@ NSArray *webkitDomEventNames;
 
 - (void)clearKillRing
 {
-    extern void _NSNewKillRingSequence();
-    
     _NSNewKillRingSequence();
 }
 
