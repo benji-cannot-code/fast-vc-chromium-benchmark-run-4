204c674b3b0c77fa54d3e8125a1f92a5ee7b6653
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-02 10:57:23 +0000

    2006-10-02  Nikolas Zimmermann  <zimmermann@kde.org>
            Reviewed by eseidel.  Landed by eseidel.
    
            Fix win32 build, which has no inttypes.h
    
            * wtf/Assertions.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16713 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Assertions.h
+++ third_party/WebKit/JavaScriptCore/wtf/Assertions.h
@@ -39,7 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 */
 
 #include "Platform.h"
+
+#if !PLATFORM(WIN)
 #include <inttypes.h>
+#endif
 
 #ifdef NDEBUG
 #define ASSERTIONS_DISABLED_DEFAULT 1
