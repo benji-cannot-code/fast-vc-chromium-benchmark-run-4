e683335c939534aad7f51485185b04c786316101
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-03 23:28:49 +0000

    LayoutTests:
            Reviewed by harrison
    
            * editing/execCommand/paste-1-expected.checksum: Added.
            * editing/execCommand/paste-1-expected.png: Added.
            * editing/execCommand/paste-1-expected.txt: Added.
            * editing/execCommand/paste-1.html: Added.
            * editing/execCommand/paste-2-expected.checksum: Added.
            * editing/execCommand/paste-2-expected.png: Added.
            * editing/execCommand/paste-2-expected.txt: Added.
            * editing/execCommand/paste-2.html: Added.
    
    WebKit:
    
            Reviewed by harrison
            
            execCommand("Cut"/"Copy"/"Paste") broken in editable subframes.
    
            * WebCoreSupport/WebFrameBridge.m:
            (-[WebFrameBridge issueCutCommand]): Issue the command on the WebHTMLView, not the WebView.
            (-[WebFrameBridge issueCopyCommand]): Ditto.
            (-[WebFrameBridge issuePasteCommand]): Ditto.
            (-[WebFrameBridge issuePasteAndMatchStyleCommand]): Ditto.
            (-[WebFrameBridge issueTransposeCommand]): Fixed formatting.
            (-[WebFrameBridge canPaste]): Ask the WebHTMLView, not the WebView.
            * WebView/WebHTMLView.m:
            (-[WebHTMLView copy:]): Moved to WebInternal
            (-[WebHTMLView cut:]): Ditto.
            (-[WebHTMLView paste:]): Ditto.
            (-[WebHTMLView pasteAsPlainText:]): Ditto.
            * WebView/WebHTMLViewInternal.h:
            * WebView/WebView.m: Removed the now unused _canPaste.
            * WebView/WebViewInternal.h: Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebHTMLViewInternal.h
+++ third_party/WebKit/WebKit/WebView/WebHTMLViewInternal.h
@@ -111,5 +111,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (WebFrame *)_frame;
 - (void)_setInitiatedDrag:(BOOL)flag;
 - (BOOL)_initiatedDrag;
+- (void)copy:(id)sender;
+- (void)cut:(id)sender;
+- (void)paste:(id)sender;
+- (void)pasteAsPlainText:(id)sender;
 @end
 
--- third_party/WebKit/WebKit/WebView/WebViewInternal.h
+++ third_party/WebKit/WebKit/WebView/WebViewInternal.h
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BOOL)_shouldChangeSelectedDOMRange:(DOMRange *)currentRange toDOMRange:(DOMRange *)proposedRange affinity:(NSSelectionAffinity)selectionAffinity stillSelecting:(BOOL)flag;
 - (BOOL)_shouldBeginEditingInDOMRange:(DOMRange *)range;
 - (BOOL)_shouldEndEditingInDOMRange:(DOMRange *)range;
-- (BOOL)_canPaste;
 @end
 
 @interface WebView (AllWebViews)
