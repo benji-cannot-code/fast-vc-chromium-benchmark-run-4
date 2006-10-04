0e0b5b43d9073d1d93adf5151eae58c48f211a70
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-04 04:45:28 +0000

    Fix mac build bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@16766 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Widget.cpp
+++ third_party/WebKit/WebCore/platform/Widget.cpp
@@ -101,11 +101,4 @@ bool Widget::isFrameView() const
     return false;
 }
 
-IntRect Widget::convertToContainingWindow(const IntRect& rect) const
-{
-    IntRect convertedRect = rect;
-    convertedRect.setLocation(convertToContainingWindow(convertedRect.location()));
-    return convertedRect;
-}
-
 }
