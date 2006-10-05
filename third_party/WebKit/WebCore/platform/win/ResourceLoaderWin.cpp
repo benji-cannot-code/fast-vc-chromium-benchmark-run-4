2aae1a6c0ac1ea744aee5917636076ceb972c76e
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-05 20:27:50 +0000

    2006-10-05  Don Gibson  <dgibson77@gmail.com>
    WebCore:
    
            Reviewed by Darin, landed by Adam.
    
            http://bugs.webkit.org/show_bug.cgi?id=11176
            Fix win32 build, adapt to Maciej's ResourceLoader changes.
    
            * platform/win/ResourceLoaderWin.cpp:
            (WebCore::ResourceLoader::start):
    
    WebKit:
    
            Reviewed by Darin, landed by Adam.
    
            http://bugs.webkit.org/show_bug.cgi?id=11176
            Fix win32 build, adapt to Maciej's ResourceLoader changes.
    
            * COM/WebFrame.cpp:
            (WebFrame::loadDataSource):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16818 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/ResourceLoaderWin.cpp
+++ third_party/WebKit/WebCore/platform/win/ResourceLoaderWin.cpp
@@ -306,6 +306,7 @@ static void __stdcall transferJobStatusCallback(HINTERNET internetHandle,
 
 bool ResourceLoader::start(DocLoader* docLoader)
 {
+    ref();
     if (d->URL.isLocalFile()) {
         DeprecatedString path = d->URL.path();
         // windows does not enjoy a leading slash on paths
