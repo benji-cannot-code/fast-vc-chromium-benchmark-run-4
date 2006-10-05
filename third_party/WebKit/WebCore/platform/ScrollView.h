a6b8b504e3c9bc34e4b1b505db411fcf8a67748c
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-05 08:58:48 +0000

    Widget focusability work.
    git-svn-id: svn://svn.chromium.org/blink/trunk@16801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ScrollView.h
+++ third_party/WebKit/WebCore/platform/ScrollView.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ScrollView_H
 
 #include "ScrollBarMode.h"
+#include "ScrollBar.h"
 #include "Widget.h"
 #include <wtf/Platform.h>
 
@@ -90,6 +91,8 @@ namespace WebCore {
         // On Mac the underlying NSScrollView just does the scrolling, but on other platforms
         // (like Windows), we need this method in order to do the scroll ourselves.
         void wheelEvent(PlatformWheelEvent&);
+
+        void scroll(ScrollDirection, ScrollGranularity);
 
 #if PLATFORM(MAC)
         NSView* getDocumentView() const;
