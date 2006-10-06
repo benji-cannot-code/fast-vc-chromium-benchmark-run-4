5eedcf6c227abe40b8f62840a2cc1e0d69b57abc
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-06 10:18:40 +0000

    Backing out my previous change.
            * WebCore.xcodeproj/project.pbxproj:
            * page/FrameView.cpp:
            (WebCore::FrameView::adjustViewSize):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16844 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -306,11 +306,6 @@ void FrameView::adjustViewSize()
         resizeContents(docw, doch);
     }
 }
-    
-IntRect FrameView::windowResizerRect() const
-{
-    return IntRect();
-}
 
 void FrameView::applyOverflowToViewport(RenderObject* o, ScrollbarMode& hMode, ScrollbarMode& vMode)
 {
