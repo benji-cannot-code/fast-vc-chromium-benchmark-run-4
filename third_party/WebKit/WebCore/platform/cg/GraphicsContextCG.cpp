cb78eab7688871bfa760eb0fad04a4f1dd9ad8fa
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-06 05:30:09 +0000

    Temporary hack.
    git-svn-id: svn://svn.chromium.org/blink/trunk@16837 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/cg/GraphicsContextCG.cpp
+++ third_party/WebKit/WebCore/platform/cg/GraphicsContextCG.cpp
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KRenderingDeviceQuartz.h"
 #endif
 
-#include "GraphicsContextPlatformPrivate.h"
+#include <GraphicsContextPlatformPrivate.h> // FIXME: Temporary.
 
 using namespace std;
 
