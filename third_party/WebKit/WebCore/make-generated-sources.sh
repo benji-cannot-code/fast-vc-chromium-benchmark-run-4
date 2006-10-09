1258c58a33ce73bb291695ef5df1a9add1a9f143
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-09 23:08:47 +0000

    2006-10-09  Krzysztof Kowalczyk  <kkowalczyk@gmail.com>
            Reviewed by Geoff.
    
            Improve gdk build compiler flags (show warning, no rtti and exceptions).
    
            * jscore.bkl:
    
    2006-10-09  Krzysztof Kowalczyk  <kkowalczyk@gmail.com>
    
            Reviewed by Geoff.
    
            Improve gdk build compiler options.
    
            * Bakefiles.bkgen:
            * presets.bkl:
    
    2006-10-09  Krzysztof Kowalczyk  <kkowalczyk@gmail.com>
    
            Reviewed by Geoff.
    
            Fix memory leaks discovered via valgrind. Gdk build fixes.
    
            * Projects/gdk/webcore-gdk.bkl:
            * make-generated-sources.sh:
            * platform/cairo/ImageCairo.cpp:
            (WebCore::Image::draw):
            * platform/gdk/FrameGdk.cpp:
            (WebCore::FrameGdk::handleGdkEvent):
            * platform/gdk/FrameGdk.h:
            * platform/gdk/PlatformScrollBar.h:
            (WebCore::PlatformScrollbar::horizontalScrollbarHeight):
            (WebCore::PlatformScrollbar::verticalScrollbarWidth):
    
    2006-10-09  Krzysztof Kowalczyk  <kkowalczyk@gmail.com>
    
            Reviewed by Geoff.
    
            Add -exit-after-loading option to make gdklauncher quit after fully loading and rendering
            a page. This allows automatic testing via e.g. valgrind.
    
            * GdkLauncher/gdklauncher.bkl:
            * GdkLauncher/main.cpp:
            (LauncherFrameGdk::LauncherFrameGdk):
            (LauncherFrameGdk::setExitAfterLoading):
            (LauncherFrameGdk::handledOnloadEvents):
            (handle_event):
            (main):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/make-generated-sources.sh
+++ third_party/WebKit/WebCore/make-generated-sources.sh
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/sh
+export SOURCE_ROOT=$PWD
 export SRCROOT=$PWD
 export WebCore=$PWD
 export CREATE_HASH_TABLE="$SRCROOT/../JavaScriptCore/kjs/create_hash_table"
@@ -9,5 +10,5 @@ cd DerivedSources/WebCore
 
 # note ENCODINGS_PREFIX needs to be a ws string so it does not turn into
 # a null value
-make -f ../../DerivedSources.make ENCODINGS_FILE=$1 ENCODINGS_PREFIX=" "
+make -f ../../DerivedSources.make ENCODINGS_FILE=$1 ENCODINGS_PREFIX="\" \""
 cd ../..
