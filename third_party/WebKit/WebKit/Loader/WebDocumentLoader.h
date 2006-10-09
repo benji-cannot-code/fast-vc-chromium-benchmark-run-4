5a2c79ecaa5623fab9b7268ba7bb39a3e6faa1dd
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-09 20:06:26 +0000

    Reviewed by Darin.
            - wean WebFrameLoader from WebDataSource private stuff
            
            (actually just tweaks tot he above to make merging my future
            patches easier since Darin did a lot of the same stuff)
    
            * Loader/WebDocumentLoader.h:
            * Loader/WebDocumentLoader.m:
            (-[WebDocumentLoader URLForHistory]):
            * Loader/WebFrameLoader.m:
            (-[WebFrameLoader _loadRequest:archive:]):
            (-[WebFrameLoader revertToProvisionalWithDocumentLoader:]):
            (-[WebFrameLoader documentLoader:setMainDocumentError:]):
            (-[WebFrameLoader finalSetupForReplaceWithDocumentLoader:]):
            (-[WebFrameLoader didChangeTitleForDocument:]):
            (-[WebFrameLoader loadDocumentLoader:withLoadType:formState:]):
            * Loader/WebFrameLoaderClient.h:
            * WebView/WebDataSource.m:
            (-[WebDataSource _URLForHistory]):
            * WebView/WebFrame.m:
            (-[WebFrame _addDocumentLoader:toUnarchiveState:]):
            (-[WebFrame _revertToProvisionalStateForDocumentLoader:]):
            (-[WebFrame _setMainDocumentError:forDocumentLoader:]):
            (-[WebFrame _clearUnarchivingStateForLoader:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16936 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Loader/WebDocumentLoader.h
+++ third_party/WebKit/WebKit/Loader/WebDocumentLoader.h
@@ -140,5 +140,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSURLRequest *)lastCheckedRequest;
 - (void)stopRecordingResponses;
 - (NSString *)title;
+- (NSURL *)URLForHistory;
 
 @end
