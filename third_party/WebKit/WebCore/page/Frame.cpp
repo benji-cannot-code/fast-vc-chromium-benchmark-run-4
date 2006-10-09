d9386f2f54ad243717447e33773f676768c07c33
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-09 18:29:40 +0000

    Reviewed by Adele.
            Remove redundant call to Frame::cancelAndClear from ~Frame, since it's
            already called frome ~FrameMac.
    
            * page/Frame.cpp:
            (WebCore::Frame::~Frame):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16932 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -192,8 +192,6 @@ Frame::~Frame()
     --FrameCounter::count;
 #endif
 
-    cancelAndClear();
-
     if (d->m_jscript && d->m_jscript->haveInterpreter())
         if (Window* w = Window::retrieveWindow(this)) {
             w->disconnectFrame();
