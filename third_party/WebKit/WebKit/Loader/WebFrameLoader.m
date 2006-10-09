7a15f8d5523ad2c3134f41a60d3af9512941726b
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-09 06:19:50 +0000

    Not reviewed.
            - fix accidental build break due to editing while committing
    
            * Loader/WebFrameLoader.m:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Loader/WebFrameLoader.m
+++ third_party/WebKit/WebKit/Loader/WebFrameLoader.m
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKitSystemInterface.h>
 
 #import "WebDataSourceInternal.h"
+#import "WebDefaultUIDelegate.h"
 #import "WebDocumentLoaderMac.h"
 #import "WebFrameInternal.h"
 #import "WebFrameViewInternal.h"
@@ -51,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebNSURLRequestExtras.h"
 #import "WebResourcePrivate.h"
 #import "WebScriptDebugServerPrivate.h"
+#import "WebUIDelegate.h"
 #import "WebViewInternal.h"
 
 @implementation WebFrameLoader
