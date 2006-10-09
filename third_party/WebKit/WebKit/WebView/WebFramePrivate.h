d8d8053db04ce307690cb4e2b34b799395f0cb30
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-09 18:04:35 +0000

    Reviewed by Brady.
            - removed almost all direct use of WebView from WebFrameLoader
    
            * Loader/WebFrameLoader.m:
            (-[WebFrameLoader defersCallbacksChanged]):
            (-[WebFrameLoader defersCallbacks]):
            (-[WebFrameLoader clearProvisionalLoad]):
            (-[WebFrameLoader _willSendRequest:forResource:redirectResponse:]):
            (-[WebFrameLoader _didReceiveResponse:forResource:]):
            (-[WebFrameLoader _didReceiveData:contentLength:forResource:]):
            (-[WebFrameLoader _didFinishLoadingForResource:]):
            (-[WebFrameLoader _didFailLoadingWithError:forResource:]):
            (-[WebFrameLoader closeOldDataSources]):
            (-[WebFrameLoader _notifyIconChanged:]):
            (-[WebFrameLoader prepareForLoadStart]):
            (-[WebFrameLoader willChangeTitleForDocument:]):
            (-[WebFrameLoader didChangeTitleForDocument:]):
            (-[WebFrameLoader continueAfterNewWindowPolicy:]):
            (-[WebFrameLoader continueAfterNavigationPolicy:]):
            (-[WebFrameLoader continueLoadRequestAfterNavigationPolicy:formState:]):
            (-[WebFrameLoader didFirstLayout]):
            (-[WebFrameLoader transitionToCommitted:]):
            (-[WebFrameLoader checkLoadCompleteForThisFrame]):
            (-[WebFrameLoader addExtraFieldsToRequest:mainResource:alwaysFromRequest:]):
            * Loader/WebFrameLoaderClient.h:
            * WebKit.xcodeproj/project.pbxproj:
            * WebView/WebFrame.m:
            (-[WebFrame _currentBackForwardListItemToResetTo]):
            (-[WebFrame _hasBackForwardList]):
            (-[WebFrame _resetBackForwardList]):
            (-[WebFrame _dispatchDidReceiveIcon:]):
            (-[WebFrame _dispatchDidStartProvisionalLoadForFrame]):
            (-[WebFrame _dispatchDidCommitLoadForFrame]):
            (-[WebFrame _dispatchDidFailProvisionalLoadWithError:]):
            (-[WebFrame _dispatchDidFailLoadWithError:]):
            (-[WebFrame _dispatchDidFinishLoadForFrame]):
            (-[WebFrame _progressStarted]):
            (-[WebFrame _progressCompleted]):
            (-[WebFrame _incrementProgressForIdentifier:response:]):
            (-[WebFrame _incrementProgressForIdentifier:data:]):
            (-[WebFrame _completeProgressForIdentifier:]):
            (-[WebFrame _setMainFrameDocumentReady:]):
            (-[WebFrame _willChangeTitleForDocument:]):
            (-[WebFrame _didChangeTitleForDocument:]):
            (-[WebFrame _startDownloadWithRequest:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16930 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebFramePrivate.h
+++ third_party/WebKit/WebKit/WebView/WebFramePrivate.h
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This header contains the WebFrame SPI.
 
-// But it also contains a bunch of internal stuff that should be moved to WebFrameInternal.h
-
 #import <WebKit/WebFrame.h>
 
 // Keys for accessing the values in the page cache dictionary.
