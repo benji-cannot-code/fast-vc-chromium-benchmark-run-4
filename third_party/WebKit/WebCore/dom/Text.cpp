2a3bbc39f9e6b8aa8544ce8c9ed0dff05a25e860
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-11 02:12:38 +0000

    2006-10-10  Oliver Hunt  <oliver@apple.com>
            Reviewed by Anders 
    
            Unbreak build bots
    
            * dom/Text.cpp:
            * kcanvas/RenderSVGText.cpp:
            (WebCore::RenderSVGText::layout):
            * kcanvas/SVGInlineFlowBox.cpp:
            (WebCore::placePositionedBoxesHorizontally):
            (WebCore::placeBoxesVerticallyWithAbsBaseline):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@16980 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Text.cpp
+++ third_party/WebKit/WebCore/dom/Text.cpp
@@ -28,7 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "ExceptionCode.h"
 #include "RenderText.h"
+
+#ifdef SVG_SUPPORT
 #include "RenderSVGInlineText.h"
+#endif // SVG_SUPPORT
 
 namespace WebCore {
 
