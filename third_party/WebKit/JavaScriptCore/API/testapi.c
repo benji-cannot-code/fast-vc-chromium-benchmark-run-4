50d33bd4f976cb319f02699881e4ec4d9b6b52e9
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-12 20:42:56 +0000

    Reviewed by Maciej.
            Removed JSObjectMakeWithPrototype, clarified some comments. We really
            don't want people to manage their own prototypes, so we don't want an
            extra function in the API devoted to just that. People can still manage
            their own prototypes if they really want by using JSObjectSetPrototype.
    
            * API/JSClassRef.cpp:
            (OpaqueJSClass::createNoAutomaticPrototype):
            (OpaqueJSClass::create):
            * API/JSClassRef.h:
            * API/JSObjectRef.cpp:
            (JSClassCreate):
            (JSObjectMake):
            * API/JSObjectRef.h:
            * API/testapi.c:
            (main):
            * JavaScriptCore.exp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17017 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/testapi.c
+++ third_party/WebKit/JavaScriptCore/API/testapi.c
@@ -498,7 +498,8 @@ int main(int argc, char* argv[])
     JSValueRef jsZero = JSValueMakeNumber(context, 0);
     JSValueRef jsOne = JSValueMakeNumber(context, 1);
     JSValueRef jsOneThird = JSValueMakeNumber(context, 1.0 / 3.0);
-    JSObjectRef jsObjectNoProto = JSObjectMakeWithPrototype(context, NULL, NULL, JSValueMakeNull(context));
+    JSObjectRef jsObjectNoProto = JSObjectMake(context, NULL, NULL);
+    JSObjectSetPrototype(context, jsObjectNoProto, JSValueMakeNull(context));
 
     // FIXME: test funny utf8 characters
     JSStringRef jsEmptyIString = JSStringCreateWithUTF8CString("");
@@ -793,7 +794,7 @@ int main(int argc, char* argv[])
     assert(count == 1); // jsCFString should not be enumerated
 
     JSClassDefinition nullDefinition = kJSClassDefinitionEmpty;
-    nullDefinition.attributes = kJSClassAttributeNoPrototype;
+    nullDefinition.attributes = kJSClassAttributeNoAutomaticPrototype;
     JSClassRef nullClass = JSClassCreate(&nullDefinition);
     JSClassRelease(nullClass);
     
