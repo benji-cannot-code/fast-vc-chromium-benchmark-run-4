fdde3363f8ec22806642b8517e18429700401a71
rwlbuis <rwlbuis@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-12 10:22:46 +0000

    Reviewed by Eric.
            Fix Qt/Linux build.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/FileChooserQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/FileChooserQt.cpp
@@ -36,6 +36,12 @@ FileChooser::~FileChooser()
     notImplemented();
 }
 
+PassRefPtr<FileChooser> FileChooser::create(Document*, RenderFileUploadControl*)
+{
+    notImplemented();
+    return 0;
+}
+
 void FileChooser::openFileChooser()
 {
     notImplemented();
