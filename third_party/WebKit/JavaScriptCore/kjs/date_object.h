f8847456864eb9b0dab417eeb7d5238aae5b4b90
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-13 17:31:11 +0000

    Reviewed by Adam, Geoff, Darin.
           Fixed displaying the UTC offset and time zone string, as well as renamed the GregorianDateTime structure and clean up. 
    
            * ChangeLog:
            * kjs/DateMath.cpp:
            (KJS::getUTCOffset):
            (KJS::getDSTOffsetSimple):
            (KJS::gregorianDateTimeToMS):
            (KJS::msToGregorianDateTime):
            * kjs/DateMath.h:
            (KJS::GregorianDateTime::GregorianDateTime):
            (KJS::GregorianDateTime::~GregorianDateTime):
            (KJS::GregorianDateTime::toTM):
            * kjs/date_object.cpp:
            (KJS::gmtoffset):
            (KJS::formatDate):
            (KJS::formatDateUTCVariant):
            (KJS::formatTime):
            (KJS::fillStructuresUsingTimeArgs):
            (KJS::fillStructuresUsingDateArgs):
            (KJS::DateInstance::getTime):
            (KJS::DateInstance::getUTCTime):
            (KJS::DateProtoFunc::callAsFunction):
            (KJS::DateObjectImp::construct):
            (KJS::DateObjectImp::callAsFunction):
            (KJS::DateObjectFuncImp::callAsFunction):
            (KJS::parseDate):
            * kjs/date_object.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/date_object.h
+++ third_party/WebKit/JavaScriptCore/kjs/date_object.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace KJS {
 
-    struct tm;
+    struct GregorianDateTime;
 
     class FunctionPrototype;
     class ObjectPrototype;
@@ -36,10 +36,10 @@ namespace KJS {
     public:
         DateInstance(JSObject *proto);
         
-        bool getTime(tm &t, int &gmtoffset) const;
-        bool getUTCTime(tm &t) const;
-        bool getTime(double &ms, int &gmtoffset) const;
-        bool getUTCTime(double &ms) const;
+        bool getTime(GregorianDateTime&, int& offset) const;
+        bool getUTCTime(GregorianDateTime&) const;
+        bool getTime(double& milli, int& offset) const;
+        bool getUTCTime(double& milli) const;
         
         virtual const ClassInfo *classInfo() const { return &info; }
         static const ClassInfo info;
