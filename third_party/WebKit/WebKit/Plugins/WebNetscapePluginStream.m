dbc0dee8301f85b08e1c322711afa39445a6571e
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-13 17:22:26 +0000

    Changed by Darin, reviewed by me.
            * Plugins/WebNetscapePluginStream.m: Fixed case of import so we can compile on
            case-sensitive file system.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins/WebNetscapePluginStream.m
+++ third_party/WebKit/WebKit/Plugins/WebNetscapePluginStream.m
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKitLogging.h>
 #import <WebKit/WebNetscapePluginEmbeddedView.h>
 #import <WebKit/WebNetscapePluginPackage.h>
-#import <WebCore/WebNetscapePluginStreamLoader.h>
+#import <WebCore/WebNetscapePlugInStreamLoader.h>
 #import <WebKit/WebNSURLRequestExtras.h>
 #import <WebKit/WebViewInternal.h>
 #import <WebCore/WebFrameLoader.h>
