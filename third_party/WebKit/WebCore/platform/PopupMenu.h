92257bca20e7dd4af466a2cfc89a30bfc4ad8499
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-16 21:44:00 +0000

    Reviewed by John.
            More menulist cleanup/tweaking.
    
            * html/HTMLSelectElement.cpp:
            (WebCore::HTMLSelectElement::setSelectedIndex): Renamed 'index'
            parameter to 'optionIndex' for clarity.
            (WebCore::HTMLSelectElement::menuListDefaultEventHandler): Skip over
            items that are not enabled <option> elements when using arrow keys, and
            make sure we pass an option index to setSelectedIndex.
            * platform/PopupMenu.h:
            (WebCore::PopupMenu::setWasClicked): Specify default argument.
            * rendering/RenderMenuList.cpp:
            (WebCore::RenderMenuList::setTextFromOption): Strip whitespace from
            option text before setting the control text so that options within
            <optgroup>s don't appear indented in the actual control.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17071 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/PopupMenu.h
+++ third_party/WebKit/WebCore/platform/PopupMenu.h
@@ -33,6 +33,7 @@ class NSPopUpButtonCell;
 #endif
 #elif PLATFORM(WIN)
 typedef struct HWND__* HWND;
+typedef struct tagDRAWITEMSTRUCT* LPDRAWITEMSTRUCT;
 #endif
 
 namespace WebCore {
@@ -59,7 +60,7 @@ public:
     bool down();
     
     bool wasClicked() const { return m_wasClicked; }
-    void setWasClicked(bool b) { m_wasClicked = b; }
+    void setWasClicked(bool b = true) { m_wasClicked = b; }
 
     int focusedIndex() const;
     
@@ -67,6 +68,7 @@ public:
 
 #if PLATFORM(WIN)
     HWND popupHandle() const { return m_popup; }
+    void drawItem(LPDRAWITEMSTRUCT);
 #endif
 
 protected:
