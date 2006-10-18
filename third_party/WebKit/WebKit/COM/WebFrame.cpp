662ea205456ca830ee4c7dd372d6e26cd906037c
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-18 21:12:33 +0000

    WebCore:
            Reviewed by Maciej.
    
            More Win32 buildfix.
    
            * bridge/win/FrameWin.h:
    
    WebKit:
    
            Reviewed by Maciej.
    
            Win32 build fix.
    
            * COM/WebFrame.cpp:
            (WebFrame::createNewWindow):
            * COM/WebFrame.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17118 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/COM/WebFrame.cpp
+++ third_party/WebKit/WebKit/COM/WebFrame.cpp
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
+ * Copyright (C) 2006 Samuel Weinig (sam.weinig@gmail.com)
  * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -35,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebHistoryItem.h"
 
 #pragma warning( push, 0 )
+#include "BrowserExtension.h"
 #include "Cache.h"
 #include "Document.h"
 #include "FrameView.h"
@@ -569,19 +571,7 @@ void WebFrame::receivedAllData(ResourceLoader*, PlatformData data)
 
 // FrameWinClient
 
-void WebFrame::createNewWindow(const WebCore::ResourceRequest&)
-{
-    // FIXME: This seems to never get called. Is it necessary?
-    IWebUIDelegate* uiDelegate = NULL;
-    if (FAILED(d->webView->uiDelegate(&uiDelegate)) || !uiDelegate)
-        return;
-    IWebView* new_view = NULL;
-    uiDelegate->createWebViewWithRequest(d->webView, NULL, &new_view);
-}
-
-void WebFrame::createNewWindow(const WebCore::ResourceRequest&,
-                               const WebCore::WindowArgs&,
-                               WebCore::Frame*& part)
+void WebFrame::createNewWindow(const WebCore::ResourceRequest&, const WebCore::WindowFeatures&, WebCore::Frame*& /*newFrame*/)
 {
     IWebUIDelegate* uiDelegate = NULL;
     if (FAILED(d->webView->uiDelegate(&uiDelegate)) || !uiDelegate)
--- third_party/WebKit/WebKit/COM/WebFrame.h
+++ third_party/WebKit/WebKit/COM/WebFrame.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
     class Frame;
+    struct WindowFeatures;
 }
 
 typedef enum {
@@ -138,10 +139,7 @@ public:
     virtual void receivedAllData(WebCore::ResourceLoader*, WebCore::PlatformData);
 
     // FrameWinClient
-    virtual void createNewWindow(const WebCore::ResourceRequest&);
-    virtual void createNewWindow(const WebCore::ResourceRequest&,
-                                 const WebCore::WindowArgs&,
-                                 WebCore::Frame*& part);
+    virtual void createNewWindow(const WebCore::ResourceRequest&, const WebCore::WindowFeatures&, WebCore::Frame*& newFrame);
     virtual void openURL(const WebCore::DeprecatedString&, bool lockHistory);
     virtual void submitForm(const WebCore::String& method, const WebCore::KURL&, const WebCore::FormData*);
     virtual void setTitle(const WebCore::String& title);
