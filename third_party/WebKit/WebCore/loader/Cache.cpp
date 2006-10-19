dae9d0427388ee3a995284680c0fe3da023b196c
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-19 16:15:21 +0000

    2006-10-19  Anders Carlsson  <acarlsson@apple.com>
            * loader/Cache.cpp:
            Get rid of #include "loader.h", Cache.h already includes this and it breaks on operating systems
            with case-sensitive file systems.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17134 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/Cache.cpp
+++ third_party/WebKit/WebCore/loader/Cache.cpp
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "Image.h"
 #include "ResourceLoader.h"
-#include "loader.h"
 
 using namespace std;
 
