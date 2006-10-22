912ae21e3cd7b518d5b5113fe2457cc9f43e78f9
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-22 02:38:38 +0000

    2006-10-21  Anders Carlsson  <acarlsson@apple.com>
            Reviewed by Darin.
    
            * loader/mac/LoaderFunctionsMac.mm:
            (WebCore::ServeSynchronousRequest):
            * platform/mac/ResourceLoaderMac.mm:
            (WebCore::ResourceLoader::start):
            Don't leak the header dictionary.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17201 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/mac/LoaderFunctionsMac.mm
+++ third_party/WebKit/WebCore/loader/mac/LoaderFunctionsMac.mm
@@ -104,7 +104,7 @@ Vector<char> ServeSynchronousRequest(Loader *loader, DocLoader *docLoader, Resou
     const HashMap<String, String>& requestHeaders = job->requestHeaders();
 
     if (!requestHeaders.isEmpty())
-        headerDict = [[NSDictionary _webcore_dictionaryWithHeaderMap:requestHeaders] retain];
+        headerDict = [NSDictionary _webcore_dictionaryWithHeaderMap:requestHeaders];
     
     NSArray *postData = nil;
     if (!job->postData().elements().isEmpty())
@@ -114,7 +114,6 @@ Vector<char> ServeSynchronousRequest(Loader *loader, DocLoader *docLoader, Resou
     NSDictionary *responseHeaderDict = nil;
     int statusCode = 0;
     NSData *resultData = [bridge syncLoadResourceWithMethod:job->method() URL:job->url().getNSURL() customHeaders:headerDict postData:postData finalURL:&finalNSURL responseHeaders:&responseHeaderDict statusCode:&statusCode];
-    [headerDict release];
     
     job->kill();
 
