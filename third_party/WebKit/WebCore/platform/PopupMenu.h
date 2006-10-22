21826360a6ce39a50d42cc76ddf1a62d0de8a115
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-22 03:19:37 +0000

    Reviewed by Anders.
            Popup tweaks.
    
            * platform/PopupMenu.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17205 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/PopupMenu.h
+++ third_party/WebKit/WebCore/platform/PopupMenu.h
@@ -68,7 +68,7 @@ public:
     IntRect clientRect() const;
 
     int listIndexAtPoint(const IntPoint& point) { return (point.y() + m_scrollOffset) / m_itemHeight; }
-    bool setFocusedIndex(int index, bool fireOnChange = false);
+    bool setFocusedIndex(int index, bool setControlText = true, bool fireOnChange = false);
     int focusedIndex() const { return m_focusedIndex; }
 
     void paint(const IntRect& damageRect, HDC hdc = 0);
@@ -80,6 +80,9 @@ public:
 
     void setScrollOffset(int offset) { m_scrollOffset = offset; }
     int scrollOffset() const { return m_scrollOffset; }
+
+    void scrollTo(int);
+    void scrollToRevealSelection();
 
     void incrementWheelDelta(int delta);
     void reduceWheelDelta(int delta);
