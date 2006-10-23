7b445eb9ec1787920059fd0c9496c20d1cda68a0
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-23 18:58:38 +0000

    Build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@17217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport/WebEditorClient.mm
+++ third_party/WebKit/WebKit/WebCoreSupport/WebEditorClient.mm
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "WebEditorClient.h"
 
+#import <WebKit/DOMHTMLElement.h>
+#import <WebKit/DOMRange.h>
 #import "WebView.h"
 #import "WebViewInternal.h"
 #import "WebEditingDelegate.h"
