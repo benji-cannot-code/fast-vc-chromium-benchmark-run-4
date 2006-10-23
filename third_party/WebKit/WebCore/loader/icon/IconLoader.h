bd917dfd0b06d139b096f3f5f7d97baeccd0a969
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-23 16:50:56 +0000

    Rubberstamped by Brady.
            * loader/icon/IconLoader.h: Changed angle-brackets to quotes for
            #include of ResourceLoader.h, since it's a WebCore header.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17214 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/icon/IconLoader.h
+++ third_party/WebKit/WebCore/loader/icon/IconLoader.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ICONLOADER_H_
 #define ICONLOADER_H_
 
-#include <ResourceLoader.h>
+#include "ResourceLoader.h"
 
 namespace WebCore {
 
