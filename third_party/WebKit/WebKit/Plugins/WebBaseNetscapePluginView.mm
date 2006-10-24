ae2dd10abbd161c02b3a17b76eddc506d8fb9ed7
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-24 21:25:25 +0000

    Build fix for un-initialized var.
    git-svn-id: svn://svn.chromium.org/blink/trunk@17262 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins/WebBaseNetscapePluginView.mm
+++ third_party/WebKit/WebKit/Plugins/WebBaseNetscapePluginView.mm
@@ -2121,7 +2121,7 @@ static OSStatus TSMEventHandler(EventHandlerCallRef inHandlerRef, EventRef inEve
 - (void)invalidateRegion:(NPRegion)invalidRegion
 {
     LOG(Plugins, "NPN_InvalidateRegion");
-    NSRect invalidRect;
+    NSRect invalidRect = NSZeroRect;
     switch (drawingModel) {
 #ifndef NP_NO_QUICKDRAW
         case NPDrawingModelQuickDraw:
