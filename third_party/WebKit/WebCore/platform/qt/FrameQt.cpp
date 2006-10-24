63685f04ad86f42a5f1f48a46e576980a7575a8e
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-24 19:17:06 +0000

    Reviewed by Anders.
            Nixed a few more redundant loading functions in Frame. Did my best to update
            the non-Mac builds.
    
            * bridge/mac/FrameMac.h:
            * bridge/mac/FrameMac.mm:
            (WebCore::FrameMac::urlSelected): Changed always 'true' argument for userGesture
            to actually check for a user gesture. New callers rely on this behavior,
            and it seems like the previous behavior must have been a bug.
            * page/Frame.cpp:
            (WebCore::Frame::requestFrame):
            (WebCore::Frame::frameLoader):
            (WebCore::Frame::redirectionTimerFired):
            * page/Frame.h:
            * platform/gdk/FrameGdk.cpp:
            * platform/gdk/FrameGdk.h:
            * platform/gdk/TemporaryLinkStubs.cpp:
            (FrameGdk::issueCopyCommand):
            * platform/qt/FrameQt.cpp:
            * platform/qt/FrameQt.h:
            * platform/win/TemporaryLinkStubs.cpp:
            (WebCore::FrameWin::shouldInterruptJavaScript):
            (WebCore::FrameWin::issueCopyCommand):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/FrameQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/FrameQt.cpp
@@ -127,15 +127,6 @@ FrameQt::~FrameQt()
     cancelAndClear();
 }
 
-bool FrameQt::openURL(const KURL& url)
-{
-    if (!m_client)
-        return false;
-
-    m_client->openURL(url);
-    return true;
-}
-
 void FrameQt::submitForm(const FrameLoadRequest& frameLoadRequest)
 {
     ResourceRequest request = frameLoadRequest.m_request;
@@ -305,11 +296,6 @@ void FrameQt::saveDocumentState()
 void FrameQt::restoreDocumentState()
 {
     // FIXME: Implement this as soon a KPart is created...
-}
-
-void FrameQt::openURLRequest(const FrameLoadRequest& request)
-{
-    urlSelected(request, 0);
 }
 
 void FrameQt::scheduleClose()
