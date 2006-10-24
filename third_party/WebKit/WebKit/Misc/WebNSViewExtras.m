f493a4d5d8789dc8d2253a6dfaa0be0c917fefe4
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-24 22:37:06 +0000

    Build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@17269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc/WebNSViewExtras.m
+++ third_party/WebKit/WebKit/Misc/WebNSViewExtras.m
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebNSImageExtras.h>
 #import <WebKit/WebNSPasteboardExtras.h>
 #import <WebKit/WebNSURLExtras.h>
+#import <WebKit/WebView.h>
 #import <WebKitSystemInterface.h>
 
 #define WebDragStartHysteresisX                 5.0f
