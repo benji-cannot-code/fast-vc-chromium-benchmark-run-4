42e837adbb8d85c699df397b759de68c525a6866
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-25 05:38:53 +0000

    2006-10-24  Anders Carlsson  <acarlsson@apple.com>
            Reviewed by Darin.
    
            * WebKitPrefix.h:
            Include FastMalloc.h from C++ code.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17281 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebKitPrefix.h
+++ third_party/WebKit/WebKit/WebKitPrefix.h
@@ -67,6 +67,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <JavaScriptCore/Platform.h>
 
+#ifdef __cplusplus
+#include <wtf/FastMalloc.h>
+#endif
+
 /* Work around bug with C++ library that screws up Objective-C++ when exception support is disabled. */
 #undef try
 #undef catch
