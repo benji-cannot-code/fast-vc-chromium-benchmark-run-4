4b6d0f6e3fc339728f3be2e7b855eaa763e604f9
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-28 04:14:58 +0000

    2006-10-27  Oliver Hunt  <oliver@apple.com>
            Reviewed by Maciej.
    
            build fix
    
            * dom/Document.cpp:
            (WebCore::Document::referrer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17395 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -43,7 +43,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EventNames.h"
 #include "ExceptionCode.h"
 #include "Frame.h"
+#if PLATFORM(MAC)
 #include "FrameLoader.h"
+#endif
 #include "FrameTree.h"
 #include "HTMLBodyElement.h"
 #include "HTMLDocument.h"
@@ -2374,9 +2376,10 @@ Element *Document::ownerElement() const
 
 String Document::referrer() const
 {
+#if PLATFORM(MAC)
     if (frame())
         return frame()->loader()->referrer();
-    
+#endif
     return String();
 }
 
