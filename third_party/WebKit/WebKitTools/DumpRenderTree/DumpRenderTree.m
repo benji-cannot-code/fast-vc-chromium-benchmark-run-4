f51f103860c3611aa65f3b1ef30ebcc0b233071e
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-29 09:36:21 +0000

    LayoutTests:
            * fast/events/js-keyboard-event-creation-expected.txt: Update test that
            is affected in a minor way by the change to DumpRenderTree.
    
    WebKitTools:
    
            Reviewed by Mitz.
    
            * DumpRenderTree/DumpRenderTree.m: (-[DumpRenderTreeWindow keyDown:]):
            Added. Does nothing, which prevents a beep.
    
            * Scripts/do-webcore-rename: Tweaked plans for renaming a bit.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17409 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -1054,6 +1054,12 @@ static NSString *md5HashStringForBitmap(CGImageRef bitmap)
     return windowIsKey;
 }
 
+- (void)keyDown:(id)sender
+{
+    // Do nothing, avoiding the beep we'd otherwise get from NSResponder,
+    // once we get to the end of the responder chain.
+}
+
 @end
 
 @implementation DumpRenderTreeEvent
