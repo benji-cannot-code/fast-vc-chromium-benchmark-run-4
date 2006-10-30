6c1cf1900381f240abc8c680faf2ebd8ee2fc56b
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-30 17:18:45 +0000

    Reviewed by John Sullivan.
            * kjs/SavedBuiltins.h: Added needed include.
            * wtf/OwnPtr.h: (WTF::OwnPtr::set): Fixed mistake in assertion.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17437 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/SavedBuiltins.h
+++ third_party/WebKit/JavaScriptCore/kjs/SavedBuiltins.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SavedBuiltins_H
 #define SavedBuiltins_H
 
+#include "protect.h"
+
 namespace KJS {
     
 class SavedBuiltinsInternal;
