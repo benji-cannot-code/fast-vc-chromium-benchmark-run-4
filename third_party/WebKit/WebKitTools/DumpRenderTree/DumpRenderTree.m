47358fca0c2f41676574794bce54f71b330a4633
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-31 05:50:14 +0000

    - fixed build
            * DumpRenderTree/DumpRenderTree.m: (-[LocalPasteboard setString:forType:]):
            Don't use CFPasteboard.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17472 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -57,7 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebView.h>
 #import <getopt.h>
 #import <malloc/malloc.h>
-#import <CoreFoundation/CFPasteboard.h>
+#import <CoreFoundation/CoreFoundation.h>
 #import <objc/objc-runtime.h>                       // for class_poseAs
 
 #define COMMON_DIGEST_FOR_OPENSSL
@@ -1138,8 +1138,12 @@ static NSString *md5HashStringForBitmap(CGImageRef bitmap)
 - (BOOL)setString:(NSString *)string forType:(NSString *)dataType
 {
     CFDataRef data = NULL;
-    if (string)
-        data = CFPasteboardCreateDataForString(NULL, (CFStringRef)string);
+    if (string) {
+        if ([string length] == 0)
+            data = CFDataCreate(NULL, NULL, 0);
+        else
+            data = CFStringCreateExternalRepresentation(NULL, (CFStringRef)string, kCFStringEncodingUTF8, 0);
+    }
     BOOL result = [self setData:(NSData *)data forType:dataType];
     if (data)
         CFRelease(data);
