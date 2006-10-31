6f690f12b1511b2382169c924269f631eb2dfae2
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-31 23:03:24 +0000

    WebCore:
            Reviewed by Beth and Adam
    
            Support for displaying tooltips for bad grammar. Currently this always displays the same tooltip; 
            next I'll make it use a string that's relevant for a specific grammar error.
    
            * WebCore.exp:
            export symbol for spellingToolTip function
            
            * rendering/HitTestResult.h:
            * rendering/HitTestResult.cpp:
            (WebCore::HitTestResult::spellingToolTip):
            new function, returns the string to be used in a tool tip that describes the questionable grammar
            
            * rendering/InlineTextBox.h:
            * rendering/InlineTextBox.cpp:
            (WebCore::InlineTextBox::paintSpellingOrGrammarMarker):
            now takes a style and font, needed to compute the rect representing the range containing
            questionable grammar. Computes the rect and associates it with the marker.
            (WebCore::InlineTextBox::paintDocumentMarkers):
            Pass in the style and font now needed by paintSpellingOrGrammarMarker
    
    WebKit:
    
            Reviewed by Beth and Adam
            
            Display a tooltip when hovering over marked bad grammar.
    
            * Misc/WebElementDictionary.m:
            (+[WebElementDictionary initializeLookupTable]):
            support spelling tool tip
            (-[WebElementDictionary _spellingToolTip]):
            new method, calls through to HitTestResult
            
            * WebView/WebHTMLView.m:
            (-[WebHTMLView _updateMouseoverWithEvent:]):
            Check for a spelling tool tip; if found, prefer it over the other possible tool tips.
            Check for empty strings instead of just nil strings being, since values from 
            WebElementDictionary are empty strings.
            
            * WebView/WebViewPrivate.h:
            declare new string constant WebElementSpellingToolTipKey
            * WebView/WebView.mm:
            define new string constant WebElementSpellingToolTipKey
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc/WebElementDictionary.m
+++ third_party/WebKit/WebKit/Misc/WebElementDictionary.m
@@ -86,6 +86,7 @@ static void cacheValueForKey(const void *key, const void *value, void *self)
     addLookupKey(WebElementImageRectKey, @selector(_imageRect), WebElementSelf);
     addLookupKey(WebElementImageURLKey, @selector(absoluteImageURL), WebElementInnerNonSharedNode);
     addLookupKey(WebElementIsSelectedKey, @selector(_isSelected), WebElementSelf);
+    addLookupKey(WebElementSpellingToolTipKey, @selector(_spellingToolTip), WebElementSelf);
     addLookupKey(WebElementTitleKey, @selector(_title), WebElementSelf);
     addLookupKey(WebElementLinkURLKey, @selector(absoluteLinkURL), WebElementURLElement);
     addLookupKey(WebElementLinkTargetFrameKey, @selector(_targetWebFrame), WebElementSelf);
@@ -195,6 +196,11 @@ static void cacheValueForKey(const void *key, const void *value, void *self)
 {
     FrameMac* webCoreFrame = Mac(_result->targetFrame());
     return kit(webCoreFrame);
+}
+
+- (NSString *)_spellingToolTip
+{
+    return _result->spellingToolTip();
 }
 
 - (NSString *)_title
