8561d6c9880d2dc2c162d430bc22459338b628e4
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-10-31 04:48:08 +0000

    Reviewed by Tim Hatcher.
            * DumpRenderTree/DumpRenderTree.m: Changed to allocate a local pasteboard.
            This should make our buildbot tests no longer need a pasteboard server.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17468 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/UIDelegate.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/UIDelegate.h
@@ -29,14 +29,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  
 #import <Cocoa/Cocoa.h>
 
-@class WebFrame;
-
-@interface UIDelegate : NSObject {
-
-}
-
+@interface UIDelegate : NSObject
 @end
-
-extern NSPoint lastMousePosition;
-extern BOOL windowIsKey;
-extern WebFrame *frame;
--- third_party/WebKit/WebKitTools/DumpRenderTree/UIDelegate.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/UIDelegate.m
@@ -27,10 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
 #import "UIDelegate.h"
 
+#import "DumpRenderTree.h"
 #import "DumpRenderTreeDraggingInfo.h"
+#import "EventSendingController.h"
 #import <WebKit/WebFramePrivate.h>
 #import <WebKit/WebHTMLViewPrivate.h>
 #import <WebKit/WebView.h>
