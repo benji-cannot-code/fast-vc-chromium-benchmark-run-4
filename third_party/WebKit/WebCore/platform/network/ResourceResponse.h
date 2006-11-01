9b470f331447cd5c20dc5b81820d4fffcd015589
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-01 19:43:21 +0000

    2006-11-01  Anders Carlsson  <acarlsson@apple.com>
            Reviewed by Oliver, Brady.
    
            * platform/network/ResourceResponse.h:
            (WebCore::ResourceResponse::setLastModifiedDate):
            (WebCore::ResourceResponse::lastModifiedDate):
            Add getters and setters for last modified dadte.
            
            * platform/network/cf/ResourceResponseCFNet.cpp:
            (WebCore::getResourceResponse):
            Fetch the last modified date. Add correct offset to expired date.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17519 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceResponse.h
+++ third_party/WebKit/WebCore/platform/network/ResourceResponse.h
@@ -72,6 +72,9 @@ public:
     void setExpirationDate(time_t expirationDate) { m_expirationDate = expirationDate; }
     time_t expirationDate() const { return m_expirationDate; }
 
+    void setLastModifiedDate(time_t lastModifiedDate) { m_lastModifiedDate = lastModifiedDate; }
+    time_t lastModifiedDate() const { return m_lastModifiedDate; }
+
  private:
     KURL m_url;
     String m_mimeType;
@@ -82,6 +85,7 @@ public:
     String m_httpStatusText;
     HTTPHeaderMap m_httpHeaderFields;
     time_t m_expirationDate;
+    time_t m_lastModifiedDate;
 };
 
 } // namespace WebCore
