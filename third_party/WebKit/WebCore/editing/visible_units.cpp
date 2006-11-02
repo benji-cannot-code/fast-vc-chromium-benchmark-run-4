d3f1a66b24f75d6c8b629afe9a62e1bd41df1f4e
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-02 03:52:33 +0000

    LayoutTests:
            Reviewed by darin
            
            <rdar://problem/4062865>
            Copy/paste of a select element fails to include the options
    
            Updated to reflect fix:
            * editing/pasteboard/4641033-expected.checksum:
            * editing/pasteboard/4641033-expected.png:
            * editing/pasteboard/4641033-expected.txt:
            * editing/pasteboard/4641033.html:
            
            Added:
            * editing/inserting/before-after-input-element-expected.checksum: Added.
            * editing/inserting/before-after-input-element-expected.png: Added.
            * editing/inserting/before-after-input-element-expected.txt: Added.
            * editing/inserting/before-after-input-element.html: Added.
            * editing/pasteboard/input-field-1-expected.checksum: Added.
            * editing/pasteboard/input-field-1-expected.png: Added.
            * editing/pasteboard/input-field-1-expected.txt: Added.
            * editing/pasteboard/input-field-1.html: Added.
            * editing/pasteboard/select-element-1-expected.checksum: Added.
            * editing/pasteboard/select-element-1-expected.png: Added.
            * editing/pasteboard/select-element-1-expected.txt: Added.
            * editing/pasteboard/select-element-1.html: Added.
            * editing/selection/select-element-paragraph-boundary-expected.checksum: Added.
            * editing/selection/select-element-paragraph-boundary-expected.png: Added.
            * editing/selection/select-element-paragraph-boundary-expected.txt: Added.
            * editing/selection/select-element-paragraph-boundary.html: Added.
    
    WebCore:
    
            Reviewed by darin
    
            <rdar://problem/4062865>
            Copy/paste of a select element fails to include the options
            
            * editing/SelectionController.cpp:
            (WebCore::SelectionController::modify): Added paragraphBoundary.
            * editing/htmlediting.cpp:
            (WebCore::canHaveChildrenForEditing): Added checks for input elements
            and textareas.  Insertion operations would fail when performed just 
            before/after one of these elements b/c the content would be put inside 
            the element.
            (WebCore::enclosingNodeWithTag): Fixed a problem when calling these in non
            editable content, and made the code to stop at an root faster (don't check
            isDescendantOf on every iteration).
            (WebCore::enclosingNodeOfType): Ditto.
            (WebCore::enclosingList): Ditto.
            (WebCore::enclosingListChild): Ditto.  Added a FIXME, this function seems
            inappropriately named.
            * editing/markup.cpp:
            (WebCore::startMarkup): Use the text node's value instead of its rendered
            content for text nodes inside select elements.  One might also turn off
            annotation when createMarkup enters a select element, but createMarkup
            is iterative, not recursive, so doing so would be complicated.
            (WebCore::createMarkup): Add markup for unrendered nodes if they are 
            descendants of a select element.
            * editing/visible_units.cpp:
            (WebCore::startOfParagraph): Migrate to isBlock/enclosingBlock.  Fixes bug
            where various replaced elements can't be copied when they are the only
            thing selected.
            (WebCore::endOfParagraph): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17532 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/visible_units.cpp
+++ third_party/WebKit/WebCore/editing/visible_units.cpp
@@ -579,7 +579,7 @@ VisiblePosition startOfParagraph(const VisiblePosition &c)
         && p.offset() == maxDeepOffset(startNode))
         return VisiblePosition(Position(startNode, 0));
 
-    Node *startBlock = startNode->enclosingBlockFlowElement();
+    Node* startBlock = enclosingBlock(startNode);
 
     Node *node = startNode;
     int offset = p.offset();
@@ -598,8 +598,8 @@ VisiblePosition startOfParagraph(const VisiblePosition &c)
             n = n->traversePreviousNodePostOrder(startBlock);
             continue;
         }
-        // FIXME: isBlockFlow should not exclude non-inline tables
-        if (r->isBR() || r->isBlockFlow() || (r->isTable() && !r->isInline()))
+        
+        if (r->isBR() || isBlock(n))
             break;
             
         if (r->isText()) {
@@ -641,7 +641,7 @@ VisiblePosition endOfParagraph(const VisiblePosition &c)
         && p.offset() == 0)
         return VisiblePosition(Position(startNode, maxDeepOffset(startNode)));
     
-    Node *startBlock = startNode->enclosingBlockFlowElement();
+    Node* startBlock = enclosingBlock(startNode);
     Node *stayInsideBlock = startBlock;
     
     Node *node = startNode;
@@ -662,8 +662,7 @@ VisiblePosition endOfParagraph(const VisiblePosition &c)
             continue;
         }
         
-        // FIXME: isBlockFlow should not exclude non-inline tables
-        if (r->isBR() || r->isBlockFlow() || (r->isTable() && !r->isInline()))
+        if (r->isBR() || isBlock(n))
             break;
             
         // FIXME: We avoid returning a position where the renderer can't accept the caret.
