f04b072de8a0e53cd0146e36b59f184110e5a538
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-03 09:33:19 +0000

    Reviewed and landed by Maciej.
            Compile on the Qt platform.
    
            * ksvg2/misc/KCanvasRenderingStyle.h:
            * platform/qt/RenderThemeQt.cpp:
            (WebCore::RenderThemeQt::getStylePainterAndWidgetFromPaintInfo):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/misc/KCanvasRenderingStyle.h
+++ third_party/WebKit/WebCore/ksvg2/misc/KCanvasRenderingStyle.h
@@ -28,7 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/Vector.h>
 
+#if PLATFORM(CG)
 #include "QuartzSupport.h"
+#endif
 
 namespace WebCore {
 
