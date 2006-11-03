fdc6d1b0c8c830220bf737de6a5bd2813edd244b
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-03 03:27:31 +0000

    Reviewed by Geoff, landed by Anders.
            Fix dependency problems caused when running move-js-headers.sh
            
            * move-js-headers.sh:
            Use cp -p instead of just cp so that the modification time
            of the original files are used.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/move-js-headers.sh
+++ third_party/WebKit/WebCore/move-js-headers.sh
@@ -2,6 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/sh
 
 mkdir -p include/JavaScriptCore
-find ../JavaScriptCore/ -name \*.h -exec cp {} include/JavaScriptCore/. \;
+find ../JavaScriptCore/ -name \*.h -exec cp -p {} include/JavaScriptCore/. \;
 rm include/JavaScriptCore/config.h
 rm include/JavaScriptCore/JavaScriptCorePrefix.h
