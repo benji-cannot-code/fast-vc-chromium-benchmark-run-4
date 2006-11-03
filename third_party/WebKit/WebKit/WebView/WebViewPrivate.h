b42548a7037a79ca8441f8a608b5154e65e7e9d7
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-03 00:27:52 +0000

    Build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@17548 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebViewPrivate.h
+++ third_party/WebKit/WebKit/WebView/WebViewPrivate.h
@@ -99,9 +99,13 @@ typedef enum {
 - (void)setWebView:(WebView *)newWebView;
 @end
 
-@interface WebView (WebPendingPublic)
+@interface WebView (WebViewEditingActionsPendingPublic)
 
 - (void)outdent:(id)sender;
+
+@end
+
+@interface WebView (WebPendingPublic)
 
 - (void)setMainFrameDocumentReady:(BOOL)mainFrameDocumentReady;
 
