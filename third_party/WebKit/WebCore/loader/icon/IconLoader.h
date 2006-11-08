15b3de69d46c57d0c865f87b6e0af3247046b459
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-08 07:24:38 +0000

    - another attempt to fix Qt build
            * loader/icon/IconLoader.h: Added missing include.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17655 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/icon/IconLoader.h
+++ third_party/WebKit/WebCore/loader/icon/IconLoader.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "KURL.h"
 #include "ResourceHandleClient.h"
+#include <memory>
 #include <wtf/Noncopyable.h>
 #include <wtf/Vector.h>
 
