4d038b1575f8b5cc1a70e9692884371088a0b348
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-09 16:31:08 +0000

    * rendering/RenderObject.h: Oops! Fixed accidentally checked-in ifdef.
    git-svn-id: svn://svn.chromium.org/blink/trunk@17689 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderObject.h
+++ third_party/WebKit/WebCore/rendering/RenderObject.h
@@ -29,15 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RenderObject_h
 
 #include "CachedResourceClient.h"
-#if 0
 #include "DeprecatedValueList.h"
 #include "RenderStyle.h"
 #include "ScrollBar.h"
 #include "VisiblePosition.h"
 #include <algorithm>
 #include <wtf/HashMap.h>
-#endif
-#include "PlatformString.h"
 
 namespace WebCore {
 
