f7db0ff724e0da8950c455257d1dc93c1dbe5398
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-09 05:58:27 +0000

    Build fix.
            * rendering/RenderObject.h:
            (WebCore::RenderObject::maxTopMargin):
            (WebCore::RenderObject::maxBottomMargin):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17680 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderObject.h
+++ third_party/WebKit/WebCore/rendering/RenderObject.h
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <wtf/HashMap.h>
 
+using namespace std;
+
 /*
  *  The painting of a layer occurs in three distinct phases.  Each phase involves
  *  a recursive descent into the layer's render objects. The first phase is the background phase.
@@ -655,8 +657,8 @@ public:
     virtual bool isTopMarginQuirk() const { return false; }
     virtual bool isBottomMarginQuirk() const { return false; }
 
-    virtual int maxTopMargin(bool positive) const { return positive ? std::max(0, marginTop()) : -std::min(0, marginTop()); }
-    virtual int maxBottomMargin(bool positive) const { return positive ? std::max(0, marginBottom()) : -std::min(0, marginBottom()); }
+    virtual int maxTopMargin(bool positive) const { return positive ? max(0, marginTop()) : min(0, -marginTop()); }
+    virtual int maxBottomMargin(bool positive) const { return positive ? max(0, marginBottom()) : min(0, -marginBottom()); }
 
     virtual int marginTop() const { return 0; }
     virtual int marginBottom() const { return 0; }
