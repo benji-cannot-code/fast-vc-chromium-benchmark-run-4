f0ef51fc9fbeb421ef47f8641314914185d29901
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-11 18:08:40 +0000

    Reviewed by Maciej
            Fixed header guard
    
            * loader/FrameLoaderClient.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoaderClient.h
+++ third_party/WebKit/WebCore/loader/FrameLoaderClient.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #ifndef FrameLoaderClient_h
-#define FrameLoaderClient_j
+#define FrameLoaderClient_h
 
 #include "FrameLoaderTypes.h"
 #include <wtf/Forward.h>
