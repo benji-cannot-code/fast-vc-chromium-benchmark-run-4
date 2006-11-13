66ab3dc2e64a4da71b7e27ae08aba1c6534848e2
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-13 23:56:01 +0000

    Reviewed by darin
            * editing/DeleteSelectionCommand.cpp:
            (WebCore::DeleteSelectionCommand::saveFullySelectedAnchor):
            * editing/Editor.cpp:
            (WebCore::Editor::appliedEditing):
            * editing/Editor.h:
            (WebCore::Editor::setRemovedAnchor): Pass a PassRefPtr.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/DeleteSelectionCommand.cpp
+++ third_party/WebKit/WebCore/editing/DeleteSelectionCommand.cpp
@@ -524,7 +524,7 @@ void DeleteSelectionCommand::saveFullySelectedAnchor()
     Node* afterEndAnchor = enclosingNodeWithTag(visibleEnd.next().deepEquivalent().upstream().node(), aTag);
 
     if (startAnchor && startAnchor == endAnchor && startAnchor != beforeStartAnchor && endAnchor != afterEndAnchor)
-        document()->frame()->editor()->setRemovedAnchor(startAnchor->cloneNode(false).get());
+        document()->frame()->editor()->setRemovedAnchor(startAnchor->cloneNode(false));
 }
 
 void DeleteSelectionCommand::doApply()
