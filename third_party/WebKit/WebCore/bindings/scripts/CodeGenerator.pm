c7679b5b3971c8b3265b07158734cb0d31690ecb
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-15 05:17:41 +0000

    2006-11-14  Mark Rowe  <bdash@webkit.org>
            Reviewed by Oliver.
    
            Build fix
    
            * bindings/scripts/CodeGenerator.pm:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17775 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGenerator.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGenerator.pm
@@ -241,7 +241,7 @@ sub ScanDirectory
 
         # Recurisvely enter directory
         if (-d "$thisDir/$name") {
-            $object->ScanDirectory($interface, "$thisDir/$name", $useDirectory, $reportAllFiles);
+            $object->ScanDirectory($interface, "$directory/$name", $useDirectory, $reportAllFiles);
             next;
         }
 
