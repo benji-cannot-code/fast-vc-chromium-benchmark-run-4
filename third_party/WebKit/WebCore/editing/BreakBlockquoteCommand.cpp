11bf750d05ad5ef844021d9eff021e8b186fafb8
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-15 17:34:13 +0000

    Reviewed by Geoff.
            Use Vector instead of DeprecatedPtrList.
            
            * editing/ApplyStyleCommand.cpp:
            (WebCore::ApplyStyleCommand::applyBlockStyle):
            * editing/BreakBlockquoteCommand.cpp:
            * rendering/RenderFlow.cpp:
            (WebCore::RenderFlow::paintOutline):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/BreakBlockquoteCommand.cpp
+++ third_party/WebKit/WebCore/editing/BreakBlockquoteCommand.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "BreakBlockquoteCommand.h"
 
-#include "DeprecatedPtrList.h"
 #include "Element.h"
 #include "HTMLNames.h"
 #include "Text.h"
