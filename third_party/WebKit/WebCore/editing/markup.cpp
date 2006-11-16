e860384bd67437f0482854c76f5309bd8ea046cc
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-16 19:58:02 +0000

    Reviewed by Adam.
            <rdar://problem/4796730> table deletion elements are serialized out if they are visible when innerHTML or a Web Archive is made
    
            * editing/markup.cpp:
            (WebCore::createMarkup): disable the delete button so it's elements are not serialized into the markup
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17815 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/markup.cpp
+++ third_party/WebKit/WebCore/editing/markup.cpp
@@ -34,10 +34,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSStyleRule.h"
 #include "cssstyleselector.h"
 #include "Comment.h"
+#include "DeleteButtonController.h"
 #include "DeprecatedStringList.h"
 #include "Document.h"
 #include "DocumentFragment.h"
 #include "DocumentType.h"
+#include "Editor.h"
+#include "Frame.h"
 #include "HTMLElement.h"
 #include "HTMLNames.h"
 #include "InlineTextBox.h"
@@ -325,6 +328,8 @@ DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotat
     ASSERT(ec == 0);
 
     Document *doc = commonAncestor->document();
+    // disable the delete button so it's elements are not serialized into the markup
+    doc->frame()->editor()->deleteButtonController()->disable();
     doc->updateLayoutIgnorePendingStylesheets();
 
     Node *commonAncestorBlock = 0;
@@ -338,8 +343,11 @@ DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotat
                 commonAncestorBlock = table;
         }
     }
-    if (!commonAncestorBlock)
+
+    if (!commonAncestorBlock) {
+        doc->frame()->editor()->deleteButtonController()->enable();
         return "";
+    }
 
     DeprecatedStringList markups;
     Node *pastEnd = range->pastEndNode();
@@ -355,9 +363,11 @@ DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotat
     VisiblePosition visibleStart(range->startPosition(), VP_DEFAULT_AFFINITY);
     VisiblePosition visibleEnd(range->endPosition(), VP_DEFAULT_AFFINITY);
     if (annotate && needInterchangeNewlineAfter(visibleStart)) {
-        if (visibleStart == visibleEnd.previous())
+        if (visibleStart == visibleEnd.previous()) {
+            doc->frame()->editor()->deleteButtonController()->enable();
             return interchangeNewlineString;
-            
+        }
+
         markups.append(interchangeNewlineString);
         startNode = visibleStart.next().deepEquivalent().node();
     }
@@ -514,6 +524,7 @@ DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotat
     markups.prepend(openTag);
     markups.append("</span>");
 
+    doc->frame()->editor()->deleteButtonController()->enable();
     return markups.join("");
 }
 
@@ -536,8 +547,12 @@ DeprecatedString createMarkup(const Node* node, EChildrenOnly includeChildren,
     Vector<Node*>* nodes, EAnnotateForInterchange annotate)
 {
     ASSERT(annotate == DoNotAnnotateForInterchange); // annotation not yet implemented for this code path
+    // disable the delete button so it's elements are not serialized into the markup
+    node->document()->frame()->editor()->deleteButtonController()->disable();
     node->document()->updateLayoutIgnorePendingStylesheets();
-    return markup(const_cast<Node*>(node), includeChildren, false, nodes);
+    DeprecatedString result(markup(const_cast<Node*>(node), includeChildren, false, nodes));
+    node->document()->frame()->editor()->deleteButtonController()->enable();
+    return result;
 }
 
 static void fillContainerFromString(ContainerNode* paragraph, const DeprecatedString& string)
