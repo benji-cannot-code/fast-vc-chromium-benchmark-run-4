c9a67220b7a66815b7664026eaffe717c5f8e5d8
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-16 02:24:44 +0000

    Reviewed by Adam.
            Adding MathExtras header.
    
            * rendering/RenderSlider.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderSlider.cpp
+++ third_party/WebKit/WebCore/rendering/RenderSlider.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLDivElement.h"
 #include "HTMLNames.h"
 #include "MouseEvent.h"
+#include <wtf/MathExtras.h>
 
 using std::min;
 
