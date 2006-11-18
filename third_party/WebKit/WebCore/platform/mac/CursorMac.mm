c12bd0342c4823fac09a8f232cce285d22865efa
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-18 10:00:31 +0000

    2006-11-17  Lars Naesbye Christensen  <larsnaesbye@stud.ku.dk>
            Reviewed by Tim H.
    
            http://bugs.webkit.org/show_bug.cgi?id=11638
            [CSS 2.1+3] add support for alias, progress, no-drop and not-allowed cursor styles
    
            * Resources/aliasCursor.png: Added.
            * Resources/noDropCursor.png: Added.
            * Resources/progressCursor.png: Added.
            * WebCore.xcodeproj/project.pbxproj:
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
            * css/CSSValueKeywords.in:
            * manual-tests/cursor.html:
            * page/EventHandler.cpp:
            (WebCore::selectCursor):
            * platform/Cursor.h:
            * platform/mac/CursorMac.mm:
            (WebCore::handCursor):
            (WebCore::aliasCursor):
            (WebCore::progressCursor):
            (WebCore::noDropCursor):
            * rendering/RenderStyle.h:
            (WebCore::):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17841 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/CursorMac.mm
+++ third_party/WebKit/WebCore/platform/mac/CursorMac.mm
@@ -118,7 +118,6 @@ const Cursor& crossCursor()
 
 const Cursor& handCursor()
 {
-    // FIXME: Use [NSCursor pointingHandCursor]?
     static Cursor c = leakNamedCursor("linkCursor", 6, 1);
     return c;
 }
@@ -144,6 +143,24 @@ const Cursor& cellCursor()
 const Cursor& contextMenuCursor()
 {
     static Cursor c = leakNamedCursor("contextMenuCursor", 1, 1);
+    return c;
+}
+
+const Cursor& aliasCursor()
+{
+    static Cursor c = leakNamedCursor("aliasCursor", 11, 3);
+    return c;
+}
+
+const Cursor& progressCursor()
+{
+    static Cursor c = leakNamedCursor("progressCursor", 1, 1);
+    return c;
+}
+
+const Cursor& noDropCursor()
+{
+    static Cursor c = leakNamedCursor("noDropCursor", 1, 1);
     return c;
 }
 
