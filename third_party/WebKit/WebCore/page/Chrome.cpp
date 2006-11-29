f3fc80c1453aa6df181a5e12af0814ce43e40e7f
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-29 22:14:22 +0000

    Reviewed by Anders Carlsson.
            Fixed http://bugs.webkit.org/show_bug.cgi?id=11712
            REGRESSION: Crash when clicking JS link on crateandbarrel.com (window.open("http[...]"))
            
            This was an accidental commit by Anders.
    
            Chrome can't use the FrameView at window.open time, because it doesn't
            exist yet.
            
            * page/Chrome.cpp:
            (WebCore::Chrome::pageRect):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17931 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Chrome.cpp
+++ third_party/WebKit/WebCore/page/Chrome.cpp
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ChromeClient.h"
 #include "FloatRect.h"
-#include "Frame.h"
-#include "FrameView.h"
 #include "Page.h"
 #include "ResourceLoader.h"
 #include <wtf/PassRefPtr.h>
@@ -58,7 +56,7 @@ FloatRect Chrome::windowRect() const
 
 FloatRect Chrome::pageRect() const
 {
-    return m_page->mainFrame()->view()->visibleContentRect();
+    return m_client->pageRect();
 }
         
 float Chrome::scaleFactor()
