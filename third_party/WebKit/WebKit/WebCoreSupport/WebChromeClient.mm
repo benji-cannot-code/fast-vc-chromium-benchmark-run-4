6cf074dc794745f171ec69af18fa6eb7b956ffda
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-30 02:01:24 +0000

    Build fix:  ResourceRequestMac.h was removed in r17911.
    git-svn-id: svn://svn.chromium.org/blink/trunk@17935 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport/WebChromeClient.mm
+++ third_party/WebKit/WebKit/WebCoreSupport/WebChromeClient.mm
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebCore/FloatRect.h>
 #import <WebCore/FrameLoadRequest.h>
 #import <WebCore/PlatformString.h>
-#import <WebCore/ResourceRequestMac.h>
+#import <WebCore/ResourceRequest.h>
 #import <WebCore/Screen.h>
 #import <wtf/PassRefPtr.h>
 
