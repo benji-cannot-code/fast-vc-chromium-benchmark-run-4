c418e51e4b45518046b44f0bcbde186be88de2cc
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-11-30 19:23:01 +0000

    Reviewed by Tim's rubberstamp
            Amazingly, DocumentLoader.h survived all this time without a header guard...
    
            * loader/DocumentLoader.h:  Added header guard
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17940 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/DocumentLoader.h
+++ third_party/WebKit/WebCore/loader/DocumentLoader.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#ifndef DocumentLoader_H_
+#define DocumentLoader_H_
 
 #include "NavigationAction.h"
 #include "Shared.h"
@@ -196,3 +198,5 @@ namespace WebCore {
     };
 
 }
+
+#endif // DocumentLoader_H_
