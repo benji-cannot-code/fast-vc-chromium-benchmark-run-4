eb4d32bb94c3637887eb5b130de7d2facb6c1c13
zimmermann <zimmermann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-03 11:55:12 +0000

    Reviewed by Rob. Move some files around, which have been forgotton in the platform/graphics move.
    git-svn-id: svn://svn.chromium.org/blink/trunk@17990 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/GraphicsContextMac.mm
+++ third_party/WebKit/WebCore/platform/graphics/mac/GraphicsContextMac.mm
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "GraphicsContext.h"
 #import "SVGResourceImage.h"
 
-#import "../graphics/cg/GraphicsContextPlatformPrivate.h"
+#import "../cg/GraphicsContextPlatformPrivate.h"
 
 #import "WebCoreSystemInterface.h"
 
