18e0076b273154ff769b35d891043bbb1957b794
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-04 17:07:53 +0000

    Reviewed by Anders
            - fixed <rdar://problem/4857833> REGRESSION: When ctrl-clicking on a misspelled word, "Ignore Spelling" and "Learn Spelling" 
              menu items not displayed in the contextual menu
    
            * WebView/WebHTMLView.m:
            (-[WebHTMLView _isSelectionMisspelled]):
            We were computing isSelectionMisspelled by calling WebCore, but then ignoring the result and always
            returning NO. D'oh!
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@17997 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView/WebHTMLView.m
@@ -1715,7 +1715,7 @@ static WebHTMLView *lastHitView = nil;
 - (BOOL)_isSelectionMisspelled
 {
     if (Frame* coreFrame = core([self _frame]))
-        coreFrame->isSelectionMisspelled();
+        return coreFrame->isSelectionMisspelled();
     return NO;
 }
 
