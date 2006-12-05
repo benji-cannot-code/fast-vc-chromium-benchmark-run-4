745a20c490be9d99de374ac208389d9d0e5ab0d8
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-05 21:24:03 +0000

    Reviewed by Darin
            Grammar/Spelling code re-homing, baby step one: put isGrammarCheckingEnabled in Editor
            since that's where isContinousSpellCheckingEnabled lives.
    
            * editing/Editor.h:
            * editing/Editor.cpp:
            (WebCore::Editor::isGrammarCheckingEnabled):
            new method, calls through to client a la isContinuousSpellCheckingEnabled
    
            * bridge/mac/FrameMac.mm:
            (WebCore::FrameMac::advanceToNextMisspelling):
            call editor() rather than editor()->client()
            (WebCore::FrameMac::markMisspellings):
            ditto
    
            * platform/ContextMenu.cpp:
            (WebCore::ContextMenu::populate):
            ditto
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/Editor.h
+++ third_party/WebKit/WebCore/editing/Editor.h
@@ -118,6 +118,7 @@ public:
     bool execCommand(const String&);
     
     bool isContinuousSpellCheckingEnabled();
+    bool isGrammarCheckingEnabled();
     int spellCheckerDocumentTag();
 
     bool shouldBeginEditing(Range* range);
