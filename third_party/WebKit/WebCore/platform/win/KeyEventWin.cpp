6732ec0c5ff26489d66fbe2d348594d48c0047b6
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-06 21:46:18 +0000

    Reviewed by Geof.
            - Returned the semantic meaning of the mask to be the actual repeat count,
            and changed auto_repeat to correctly differnetiate between keypressed and keydown.
    
            * platform/win/KeyEventWin.cpp:
            (WebCore::PlatformKeyboardEvent::PlatformKeyboardEvent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18044 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/KeyEventWin.cpp
+++ third_party/WebKit/WebCore/platform/win/KeyEventWin.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PlatformKeyboardEvent.h"
 #include <windows.h>
 
-#define REPEAT_COUNT_MASK           0x0000FFFE
+#define REPEAT_COUNT_MASK           0x0000FFFF
 #define NEW_RELEASE_STATE_MASK      0x80000000
 #define PREVIOUS_DOWN_STATE_MASK    0x40000000
 #define ALT_KEY_DOWN_MASK           0x20000000
@@ -142,7 +142,7 @@ PlatformKeyboardEvent::PlatformKeyboardEvent(HWND hWnd, WPARAM wParam, LPARAM lP
     , m_unmodifiedText(singleCharacterString(wParam))
     , m_keyIdentifier(keyIdentifierForWindowsKeyCode(wParam))
     , m_isKeyUp((lParam & NEW_RELEASE_STATE_MASK))
-    , m_autoRepeat(lParam & REPEAT_COUNT_MASK)
+    , m_autoRepeat((lParam & REPEAT_COUNT_MASK) > 1)
     , m_WindowsKeyCode(wParam)
     , m_isKeypad(false) // FIXME
     , m_shiftKey(GetKeyState(VK_SHIFT) & HIGH_BIT_MASK_SHORT)
