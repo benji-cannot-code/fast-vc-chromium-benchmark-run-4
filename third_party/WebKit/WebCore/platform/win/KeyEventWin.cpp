9d03601de9d2622c62200e5e61f257e51a24a9ba
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-06 02:01:03 +0000

    Reviewed by John.
            - keydown is now a keydown event instead of a keypress event.
              Fixes some form submission odditties.
    
            * platform/win/KeyEventWin.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18038 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/KeyEventWin.cpp
+++ third_party/WebKit/WebCore/platform/win/KeyEventWin.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PlatformKeyboardEvent.h"
 #include <windows.h>
 
-#define REPEAT_COUNT_MASK           0x0000FFFF
+#define REPEAT_COUNT_MASK           0x0000FFFE
 #define NEW_RELEASE_STATE_MASK      0x80000000
 #define PREVIOUS_DOWN_STATE_MASK    0x40000000
 #define ALT_KEY_DOWN_MASK           0x20000000
