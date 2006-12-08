28c3844393129331557932e35f1bf7b0c2c5528e
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-08 03:22:27 +0000

    2006-12-07  Kevin Fyure  <digdog@macports.org>
            Reviewed by Darin.
    
            http://bugs.webkit.org/show_bug.cgi?id=11545
            Disable the testcases do not follow the ECMA-262v3 specification.
            
            * tests/mozilla/expected.html: Update Results.
            * tests/mozilla/js1_2/String/concat.js:
            4 tests disabled. The result of concat Array object is not followinig
            ECMA 15.5.4.6
            * tests/mozilla/js1_2/function/Number.js:
            1 test disabled. The result of Array object to Number object conversion 
            is not following ECMA 9.3. And the test was duplicated in 
            ecma/TypeConversion/9.3-1.js
            * tests/mozilla/js1_2/function/String.js:
            2 tests disabled. The result of Object/Array object to String object 
            conversion is not following ECMA 15.5.1.1 and ECMA 9.8
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18071 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/tests/mozilla/js1_2/String/concat.js
+++ third_party/WebKit/JavaScriptCore/tests/mozilla/js1_2/String/concat.js
@@ -50,8 +50,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 	testcases[count++] = new TestCase( SECTION, "aString.concat(345)    ", "test string345",       aString.concat(345).toString());
 	testcases[count++] = new TestCase( SECTION, "aString.concat(true)   ", "test stringtrue",      aString.concat(true).toString());
 	testcases[count++] = new TestCase( SECTION, "aString.concat(null)   ", "test stringnull",      aString.concat(null).toString());
-	testcases[count++] = new TestCase( SECTION, "aString.concat([])     ", "test string[]",          aString.concat([]).toString());
-	testcases[count++] = new TestCase( SECTION, "aString.concat([1,2,3])", "test string[1, 2, 3]",     aString.concat([1,2,3]).toString());
+	/*
+	http://bugs.webkit.org/show_bug.cgi?id=11545#c3
+	According to ECMA 15.5.4.6, the argument of concat should send to ToString and
+	convert into a string value (not String object). So these arguments will be
+	convert into '' and '1,2,3' under ECMA-262v3, not the js1.2 expected '[]' and
+	'[1,2,3]'
+	*/
+	//testcases[count++] = new TestCase( SECTION, "aString.concat([])     ", "test string[]",          aString.concat([]).toString());
+	//testcases[count++] = new TestCase( SECTION, "aString.concat([1,2,3])", "test string[1, 2, 3]",     aString.concat([1,2,3]).toString());
 
 	testcases[count++] = new TestCase( SECTION, "'abcde'.concat(' more')", "abcde more",     'abcde'.concat(' more').toString());
 	testcases[count++] = new TestCase( SECTION, "'abcde'.concat(bString)", "abcde another ", 'abcde'.concat(bString).toString());
@@ -59,8 +66,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 	testcases[count++] = new TestCase( SECTION, "'abcde'.concat(345)    ", "abcde345",       'abcde'.concat(345).toString());
 	testcases[count++] = new TestCase( SECTION, "'abcde'.concat(true)   ", "abcdetrue",      'abcde'.concat(true).toString());
 	testcases[count++] = new TestCase( SECTION, "'abcde'.concat(null)   ", "abcdenull",      'abcde'.concat(null).toString());
-	testcases[count++] = new TestCase( SECTION, "'abcde'.concat([])     ", "abcde[]",          'abcde'.concat([]).toString());
-	testcases[count++] = new TestCase( SECTION, "'abcde'.concat([1,2,3])", "abcde[1, 2, 3]",     'abcde'.concat([1,2,3]).toString());
+	/*
+	http://bugs.webkit.org/show_bug.cgi?id=11545#c3
+	According to ECMA 15.5.4.6, the argument of concat should send to ToString and
+	convert into a string value (not String object). So these arguments will be
+	convert into '' and '1,2,3' under ECMA-262v3, not the js1.2 expected '[]' and
+	'[1,2,3]'
+	*/
+	//testcases[count++] = new TestCase( SECTION, "'abcde'.concat([])     ", "abcde[]",          'abcde'.concat([]).toString());
+	//testcases[count++] = new TestCase( SECTION, "'abcde'.concat([1,2,3])", "abcde[1, 2, 3]",     'abcde'.concat([1,2,3]).toString());
 
 	//what should this do:
 	testcases[count++] = new TestCase( SECTION, "'abcde'.concat()       ", "abcde",          'abcde'.concat().toString());
