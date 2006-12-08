8f01e948765286faa22aa9e34d6cd58218b1af15
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-08 03:53:08 +0000

    Reviewed by Oliver.
            Some small context menu-related fixes.
    
            * html/HTMLFrameSetElement.cpp:
            (WebCore::HTMLFrameSetElement::defaultEventHandler): Don't try to
            resize on a contextmenuEvent.
            * html/HTMLInputElement.cpp:
            (WebCore::HTMLInputElement::defaultEventHandler): Only pass the event
            off to HTMLGenericFormElement if we haven't handled it.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18072 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLFrameSetElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLFrameSetElement.cpp
@@ -139,7 +139,7 @@ void HTMLFrameSetElement::attach()
 
 void HTMLFrameSetElement::defaultEventHandler(Event* evt)
 {
-    if (evt->isMouseEvent() && !noresize && renderer()) {
+    if (evt->isMouseEvent() && evt->type() != contextmenuEvent && !noresize && renderer()) {
         static_cast<RenderFrameSet*>(renderer())->userResize(static_cast<MouseEvent*>(evt));
         evt->setDefaultHandled();
     }
--- third_party/WebKit/WebCore/html/HTMLInputElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLInputElement.cpp
@@ -1379,8 +1379,9 @@ void HTMLInputElement::defaultEventHandler(Event* evt)
         if (evt->isMouseEvent() || evt->isDragEvent() || evt->isWheelEvent())
             slider->forwardEvent(evt);
     }
-            
-    HTMLGenericFormElement::defaultEventHandler(evt);
+
+    if (!evt->defaultHandled())
+        HTMLGenericFormElement::defaultEventHandler(evt);
 }
 
 bool HTMLInputElement::isURLAttribute(Attribute *attr) const
