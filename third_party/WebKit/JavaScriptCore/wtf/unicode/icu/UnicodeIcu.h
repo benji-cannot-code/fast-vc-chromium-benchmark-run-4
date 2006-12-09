be5311dc986945d98f5b317ae44e7b3570dfb3ef
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-09 23:53:30 +0000

    JavaScriptCore:
            Reviewed by Darin.
    
            Some updates in reaction to r18098.
    
            * wtf/unicode/icu/UnicodeIcu.h: Use !! to convert UBool to bool in all
            cases.
            (WTF::Unicode::toLower):
            (WTF::Unicode::toUpper):
            (WTF::Unicode::isDigit):
            (WTF::Unicode::isSpace):
            (WTF::Unicode::isPunct):
            (WTF::Unicode::isLower):
            (WTF::Unicode::isUpper):
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj:
            * JavaScriptCore.vcproj/WTF/WTF.vcproj:
    
    WebCore:
    
            Reviewed by Darin.
    
            Changed all ForwardingHeaders to use #include instead of #import.
    
            * ForwardingHeaders/kjs/JSLock.h:
            * ForwardingHeaders/kjs/SavedBuiltins.h:
            * ForwardingHeaders/kjs/collector.h:
            * ForwardingHeaders/kjs/dtoa.h:
            * ForwardingHeaders/kjs/internal.h:
            * ForwardingHeaders/kjs/interpreter.h:
            * ForwardingHeaders/kjs/lookup.h:
            * ForwardingHeaders/kjs/object.h:
            * ForwardingHeaders/kjs/property_map.h:
            * ForwardingHeaders/kjs/protect.h:
            * ForwardingHeaders/wtf/AlwaysInline.h:
            * ForwardingHeaders/wtf/Assertions.h:
            * ForwardingHeaders/wtf/FastMalloc.h:
            * ForwardingHeaders/wtf/GetPtr.h:
            * ForwardingHeaders/wtf/HashCountedSet.h:
            * ForwardingHeaders/wtf/HashMap.h:
            * ForwardingHeaders/wtf/HashSet.h:
            * ForwardingHeaders/wtf/HashTraits.h:
            * ForwardingHeaders/wtf/MathExtras.h:
            * ForwardingHeaders/wtf/Noncopyable.h:
            * ForwardingHeaders/wtf/OwnArrayPtr.h:
            * ForwardingHeaders/wtf/OwnPtr.h:
            * ForwardingHeaders/wtf/PassRefPtr.h:
            * ForwardingHeaders/wtf/Platform.h:
            * ForwardingHeaders/wtf/RefPtr.h:
            * ForwardingHeaders/wtf/Vector.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18108 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/unicode/icu/UnicodeIcu.h
+++ third_party/WebKit/JavaScriptCore/wtf/unicode/icu/UnicodeIcu.h
@@ -155,7 +155,7 @@ namespace WTF {
     {
       UErrorCode status = U_ZERO_ERROR;
       int32_t realLength = u_strToLower(result, resultLength, src, srcLength, "", &status);
-      *error = U_FAILURE(status);
+      *error = !!U_FAILURE(status);
       return realLength;
     }
 
@@ -193,7 +193,7 @@ namespace WTF {
     {
       UErrorCode status = U_ZERO_ERROR;
       int32_t realLength = u_strToUpper(result, resultLength, src, srcLength, "", &status);
-      *error = U_FAILURE(status);
+      *error = !!U_FAILURE(status);
       return realLength;
     }
 
@@ -219,17 +219,17 @@ namespace WTF {
     
     inline bool isDigit(int32_t c)
     {
-      return u_isdigit(c);
+      return !!u_isdigit(c);
     }
 
     inline bool isSpace(int32_t c)
     {
-      return u_isspace(c);
+      return !!u_isspace(c);
     }
 
     inline bool isPunct(int32_t c)
     {
-      return u_ispunct(c);
+      return !!u_ispunct(c);
     }
 
     inline int32_t mirroredChar(int32_t c)
@@ -248,12 +248,12 @@ namespace WTF {
 
     inline bool isLower(int32_t c)
     {
-      return u_islower(c);
+      return !!u_islower(c);
     }
 
     inline bool isUpper(int32_t c)
     {
-      return u_isUUppercase(c);
+      return !!u_isUUppercase(c);
       }
 
     inline int digitValue(int32_t c)
