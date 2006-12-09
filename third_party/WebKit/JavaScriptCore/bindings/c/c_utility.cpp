c5738391bae290ba68da0a37beb1da7f4df568df
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-09 22:40:10 +0000

    Patch by Lars Knoll, comment out ICU dependency on Qt platform (unused code).
            Reviewed by Darin.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18107 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/c/c_utility.cpp
+++ third_party/WebKit/JavaScriptCore/bindings/c/c_utility.cpp
@@ -34,7 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "npruntime_priv.h"
 #include "runtime_object.h"
 #include "runtime_root.h"
+#include "Platform.h"
+#if USE(ICU_UNICODE)
 #include <unicode/ucnv.h>
+#endif
 
 namespace KJS { namespace Bindings {
 
@@ -47,6 +50,7 @@ void convertNPStringToUTF16(const NPString *string, NPUTF16 **UTF16Chars, unsign
 // Requires free() of returned UTF16Chars.
 void convertUTF8ToUTF16(const NPUTF8 *UTF8Chars, int UTF8Length, NPUTF16 **UTF16Chars, unsigned int *UTF16Length)
 {
+#if USE(ICU_UNICODE)
     assert(UTF8Chars || UTF8Length == 0);
     assert(UTF16Chars);
     
@@ -81,6 +85,9 @@ void convertUTF8ToUTF16(const NPUTF8 *UTF8Chars, int UTF8Length, NPUTF16 **UTF16
         for (unsigned i = 0; i < *UTF16Length; i++)
             (*UTF16Chars)[i] = UTF8Chars[i] & 0xFF;
     }
+#else
+    assert(!"Implement me!");    
+#endif
 }
 
 // Variant value must be released with NPReleaseVariantValue()
