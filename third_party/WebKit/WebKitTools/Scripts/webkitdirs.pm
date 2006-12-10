2b7d4e129bafffe668664770a7cd3b239f892095
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-10 03:57:00 +0000

    Remove reference to Unity.  Reviewed by Zack.
    git-svn-id: svn://svn.chromium.org/blink/trunk@18120 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -305,7 +305,7 @@ sub hasSVGSupport
     my $path = shift;
 
     if ((isQt()) and ($path =~ /WebCore/)) {
-        $path .= "/../lib/libWebCore-unity.so";
+        $path .= "/../lib/libWebCore.so";
     }
 
     open NM, "-|", "nm", $path or die;
