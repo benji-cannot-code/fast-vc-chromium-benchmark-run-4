ce4bc033780096845b5fa0fcd73b1449f07f1c37
harrison <harrison@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-11 18:03:41 +0000

    Fix previous checkin where I committed the wrong file.
            <rdar://problem/4863611> Xyle Scope crashes at launch due to WebCore-521.29.3
    
            * WebView/WebFrame.mm:
            (-[WebFrame frameElement]):
            Add nil check.
            
            * WebView/WebPreferences.m:
            (-[WebPreferences editableLinkBehavior]):
            Reverted to previous.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18154 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebFrame.mm
+++ third_party/WebKit/WebKit/WebView/WebFrame.mm
@@ -1172,7 +1172,7 @@ static inline WebDataSource *dataSource(DocumentLoader* loader)
     if (!coreFrame)
         return nil;
     Element* element = coreFrame->ownerElement();
-    if (!element->isHTMLElement())
+    if (!element || !element->isHTMLElement())
         return nil;
     return kit(static_cast<HTMLElement*>(element));
 }
