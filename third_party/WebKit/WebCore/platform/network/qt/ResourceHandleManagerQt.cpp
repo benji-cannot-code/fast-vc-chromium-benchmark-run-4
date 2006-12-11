740b05313b9e8e2ec2ec6fad115c6b4a0f4ba72e
zack <zack@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-11 00:13:41 +0000

    Fixes loading of local files (from Lars)
    git-svn-id: svn://svn.chromium.org/blink/trunk@18144 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/ResourceHandleManagerQt.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/ResourceHandleManagerQt.cpp
@@ -345,7 +345,7 @@ FileLoader::FileLoader()
 
 void FileLoader::request(RequestQt* request)
 {
-    DEBUG() << "request";
+    DEBUG() << "FileLoader::request";
     KURL url = request->url;
     Q_ASSERT(url.isLocalFile());
     int error = 0;
@@ -353,7 +353,10 @@ void FileLoader::request(RequestQt* request)
     if (request->postData.isEmpty()) {
         QFile f(QString(url.path()));
         DEBUG() << "opening" << QString(url.path());
+
         if (f.open(QIODevice::ReadOnly)) {
+            emit receivedResponse(request);
+        
             QByteArray data = f.readAll();
             emit receivedData(request, data);
         } else {
