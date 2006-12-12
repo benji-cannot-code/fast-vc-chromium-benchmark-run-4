d120231f9a2c4b07db24507312c9515346d64963
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-12 23:45:20 +0000

    Suggested by Darin.
            * loader/ResourceLoader.cpp:
            Move down include of ResourceHandle.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18189 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/ResourceLoader.cpp
+++ third_party/WebKit/WebCore/loader/ResourceLoader.cpp
@@ -28,11 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "ResourceHandle.h"
 #include "ResourceLoader.h"
 
 #include "Frame.h"
 #include "Page.h"
+#include "ResourceHandle.h"
 
 namespace WebCore {
 
