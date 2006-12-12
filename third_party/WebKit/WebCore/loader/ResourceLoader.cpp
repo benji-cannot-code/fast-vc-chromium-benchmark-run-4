ba636e9a24da99f58775f57c327a23a2777db88a
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-12 21:42:24 +0000

    Try fixing the non-Mac builds.
            * loader/ResourceLoader.cpp:
            (WebCore::ResourceLoader::willSendRequest):
            (WebCore::ResourceLoader::didReceiveResponse):
            (WebCore::ResourceLoader::didReceiveData):
            (WebCore::ResourceLoader::didFinishLoading):
            (WebCore::ResourceLoader::didFail):
            * loader/SubresourceLoader.cpp:
            (WebCore::SubresourceLoader::create):
            * loader/SubresourceLoader.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18185 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/ResourceLoader.cpp
+++ third_party/WebKit/WebCore/loader/ResourceLoader.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+#include "ResourceHandle.h"
 #include "ResourceLoader.h"
 
 #include "Frame.h"
@@ -52,6 +53,26 @@ ResourceLoader::ResourceLoader(Frame* frame)
 }
 
 void ResourceLoader::setDefersLoading(bool)
+{
+}
+
+void ResourceLoader::willSendRequest(ResourceHandle*, ResourceRequest& request, const ResourceResponse& redirectResponse)
+{
+}
+
+void ResourceLoader::didReceiveResponse(ResourceHandle*, const ResourceResponse& response)
+{
+}
+
+void ResourceLoader::didReceiveData(ResourceHandle*, const char* data, int length, int lengthReceived)
+{
+}
+
+void ResourceLoader::didFinishLoading(ResourceHandle*)
+{
+}
+
+void ResourceLoader::didFail(ResourceHandle*, const ResourceError& error)
 {
 }
 
