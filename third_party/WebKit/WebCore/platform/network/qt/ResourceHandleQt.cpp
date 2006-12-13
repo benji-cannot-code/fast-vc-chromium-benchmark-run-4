6a915a56a3517490af9852545febb70f7bd49ba4
zack <zack@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-13 13:16:53 +0000

    make it compile after last nights changes from lars (r=maciej,me)
    git-svn-id: svn://svn.chromium.org/blink/trunk@18196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/ResourceHandleQt.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/ResourceHandleQt.cpp
@@ -41,6 +41,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceHandleManagerQt.h"
 #include "ResourceHandleInternal.h"
 
+#define notImplemented() do { fprintf(stderr, "FIXME: UNIMPLEMENTED: %s:%d (%s)\n", \
+           __FILE__, __LINE__, __FUNCTION__); } while(0)
+
 namespace WebCore {
 
 ResourceHandleInternal::~ResourceHandleInternal()
@@ -52,15 +55,12 @@ ResourceHandle::~ResourceHandle()
     cancel();
 }
 
-bool ResourceHandle::start(DocLoader* docLoader)
+bool ResourceHandle::start(Frame* frame)
 {
-    FrameQt* frame = 0;
-    if (docLoader) {
-        frame = QtFrame(docLoader->frame());
-        ASSERT(frame);
-    }
+    ASSERT(frame);
+    FrameQt* f = QtFrame(frame);
 
-    ResourceHandleManager::self()->add(this, frame ? frame->client() : 0);
+    ResourceHandleManager::self()->add(this, f ? f->client() : 0);
     return true;
 }
 
@@ -68,5 +68,11 @@ void ResourceHandle::cancel()
 {
     ResourceHandleManager::self()->cancel(this);
 }
+
+bool ResourceHandle::loadsBlocked()
+{
+    notImplemented();
+}
+
 
 } // namespace WebCore
