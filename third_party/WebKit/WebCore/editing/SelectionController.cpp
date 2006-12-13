0fd107bd9a8ef59e4a32b586f4b74a50bd0dd5ed
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-13 23:10:32 +0000

    Reviewed by Darin Adler, Dave Hyatt.
            Beefed up FocusController to handle frame focus. Changed implementors to
            use FocusController, rather than the widget hierarchy, to focus frames.
    
            (WebCore::FocusController::focusedNode): Made a minor change to focus a frame
            before focusing the element inside of it. This matches FF.
            (WebCore::FocusController::focusedFrame):
            * platform/Widget.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/SelectionController.cpp
+++ third_party/WebKit/WebCore/editing/SelectionController.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Editor.h"
 #include "Element.h"
 #include "EventNames.h"
+#include "FocusController.h"
 #include "Frame.h"
 #include "FrameTree.h"
 #include "FrameView.h"
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLNames.h"
 #include "HitTestRequest.h"
 #include "HitTestResult.h"
+#include "Page.h"
 #include "RenderView.h"
 #include "TextIterator.h"
 #include "TypingCommand.h"
@@ -996,11 +998,11 @@ bool SelectionController::contains(const IntPoint& point)
 void SelectionController::selectFrameElementInParentIfFullySelected()
 {
     // Find the parent frame; if there is none, then we have nothing to do.
-    Frame *parent = m_frame->tree()->parent();
+    Frame* parent = m_frame->tree()->parent();
     if (!parent)
         return;
-    FrameView *parentView = parent->view();
-    if (!parentView)
+    Page* page = m_frame->page();
+    if (!page)
         return;
 
     // Check if the selection contains the entire frame contents; if not, then there is nothing to do.
@@ -1012,13 +1014,13 @@ void SelectionController::selectFrameElementInParentIfFullySelected()
         return;
 
     // Get to the <iframe> or <frame> (or even <object>) element in the parent frame.
-    Document *doc = m_frame->document();
+    Document* doc = m_frame->document();
     if (!doc)
         return;
-    Element *ownerElement = doc->ownerElement();
+    Element* ownerElement = doc->ownerElement();
     if (!ownerElement)
         return;
-    Node *ownerElementParent = ownerElement->parentNode();
+    Node* ownerElementParent = ownerElement->parentNode();
     if (!ownerElementParent)
         return;
         
@@ -1034,7 +1036,7 @@ void SelectionController::selectFrameElementInParentIfFullySelected()
     // Focus on the parent frame, and then select from before this element to after.
     Selection newSelection(beforeOwnerElement, afterOwnerElement);
     if (parent->shouldChangeSelection(newSelection)) {
-        parentView->setFocus();
+        page->focusController()->setFocusedFrame(parent);
         parent->selectionController()->setSelection(newSelection);
     }
 }
