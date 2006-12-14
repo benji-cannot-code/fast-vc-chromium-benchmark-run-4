44110d65198e9ab9be238fba0116c97514f9f2c1
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-14 01:30:02 +0000

    * loader/FrameLoader.h:         id is mac-only.
    git-svn-id: svn://svn.chromium.org/blink/trunk@18206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/WebCore/loader/FrameLoader.h
@@ -488,7 +488,9 @@ namespace WebCore {
 
         // Also not cool.
         void startLoading();
+#if PLATFORM(MAC)
         bool startLoadingMainResource(ResourceRequest&, id identifier);
+#endif
         void stopLoadingSubframes();
 
         void clearProvisionalLoad();
