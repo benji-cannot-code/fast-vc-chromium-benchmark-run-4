c850e9f631c63d73c9c10420716f0fdca08d6123
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-14 22:13:59 +0000

    Reviewed by John.
            General loader cleanup, convert NSURLResponse to ResourceResponse in some places.
            
            * loader/DocumentLoader.h:
            * loader/SubresourceLoader.h:
            * loader/mac/DocumentLoaderMac.mm:
            (WebCore::DocumentLoader::addResponse):
            * loader/mac/FrameLoaderMac.mm:
            (WebCore::FrameLoader::opened):
            * loader/mac/SubresourceLoaderMac.mm:
            (WebCore::SubresourceLoader::create):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18219 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/DocumentLoader.h
+++ third_party/WebKit/WebCore/loader/DocumentLoader.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Shared.h"
 #include "PlatformString.h"
 #include "ResourceRequest.h"
+#include "ResourceResponse.h"
 #include <wtf/Vector.h>
 
 #if PLATFORM(MAC)
@@ -58,9 +59,7 @@ namespace WebCore {
     class FrameLoader;
     class KURL;
 
-#if PLATFORM(MAC)
-    typedef Vector<RetainPtr<NSURLResponse> > ResponseVector;
-#endif
+    typedef Vector<ResourceResponse> ResponseVector;
 
     class DocumentLoader : public Shared<DocumentLoader> {
     public:
@@ -113,10 +112,10 @@ namespace WebCore {
         void setPrimaryLoadComplete(bool);
         void setTitle(const String&);
         String overrideEncoding() const;
-#if PLATFORM(MAC)
-        void addResponse(NSURLResponse *);
+
+        void addResponse(const ResourceResponse&);
         const ResponseVector& responses() const;
-#endif
+
         const NavigationAction& triggeringAction() const;
         void setTriggeringAction(const NavigationAction&);
         void setOverrideEncoding(const String&);
@@ -192,13 +191,11 @@ namespace WebCore {
         // so we can avoid asking again needlessly.
         ResourceRequest m_lastCheckedRequest;
 
-#if PLATFORM(MAC)
         // We retain all the received responses so we can play back the
         // WebResourceLoadDelegate messages if the item is loaded from the
         // page cache.
         ResponseVector m_responses;
         bool m_stopRecordingResponses;
-#endif
     };
 
 }
