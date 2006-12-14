98dfa7a85aa68b07f8794465c3970745151ef383
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-14 18:05:16 +0000

    Reviewed by Darin.
            http://bugs.webkit.org/show_bug.cgi?id=11828
            Fix a leak of NSURLRequest on each update - RetainPtr retains the object itself.
    
            * platform/network/mac/ResourceRequestMac.mm:
            (WebCore::ResourceRequest::doUpdatePlatformRequest):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18215 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/mac/ResourceRequestMac.mm
+++ third_party/WebKit/WebCore/platform/network/mac/ResourceRequestMac.mm
@@ -91,7 +91,7 @@ void ResourceRequest::doUpdatePlatformRequest()
     if (formData && !formData->isEmpty())
         WebCore::setHTTPBody(nsRequest, formData);
     
-    m_nsRequest = nsRequest;
+    m_nsRequest.adopt(nsRequest);
 }
 
 }
