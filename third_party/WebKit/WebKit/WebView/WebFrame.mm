00cf5ae58122144d0ef455b3cf4873ffee902839
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-15 02:25:22 +0000

    Reviewed by Darin.
            <rdar://problem/4827549> need NSRange-based selection support exposed.
    
            * WebView/WebFrame.mm:
            (-[WebFrame _selectedNSRange]):
            (-[WebFrame _selectNSRange:]):
            * WebView/WebFramePrivate.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebFrame.mm
+++ third_party/WebKit/WebKit/WebView/WebFrame.mm
@@ -940,7 +940,6 @@ WebView *getWebView(WebFrame *webFrame)
 }
 
 #if !ASSERT_DISABLED
-
 - (BOOL)_atMostOneFrameHasSelection
 {
     // FIXME: 4186050 is one known case that makes this debug check fail.
@@ -1110,6 +1109,16 @@ static inline WebDataSource *dataSource(DocumentLoader* loader)
         if ([documentView isKindOfClass:[WebHTMLView class]])
             [(WebHTMLView *)documentView _resumeNullEventsForAllNetscapePlugins];
     }
+}
+
+- (NSRange)_selectedNSRange
+{
+    return [_private->bridge selectedNSRange];
+}
+
+- (void)_selectNSRange:(NSRange)range
+{
+    [_private->bridge selectNSRange:range];
 }
 
 @end
