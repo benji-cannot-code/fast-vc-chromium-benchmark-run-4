64ed0c5d88c3e351324991f99dcace70dd1ea09a
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-16 21:30:18 +0000

    LayoutTests:
            Reviewed by Maciej.
    
            Test for http://bugs.webkit.org/show_bug.cgi?id=11189
            <rdar://problem/4770249> REGRESSION (NativeListBox): When a list box has focus, pressing command- A (select all) doesn't selected all items in list
    
            * fast/forms/listbox-select-all-expected.txt: Added.
            * fast/forms/listbox-select-all.html: Added.
    
    WebCore:
    
            Reviewed by Maciej.
    
            Fix for http://bugs.webkit.org/show_bug.cgi?id=11189
            <rdar://problem/4770249> REGRESSION (NativeListBox): When a list box has focus, pressing command- A (select all) doesn't selected all items in list
    
            Test: fast/forms/listbox-select-all.html
    
            * dom/Node.h: We should try to find a more appropriate place to put these methods.
            (WebCore::Node::canSelectAll): Added.
            (WebCore::Node::selectAll): Added.
            * editing/SelectionController.cpp: (WebCore::SelectionController::selectAll): Before selecting editable content, give the focused node
              a chance to select its content.
            * html/HTMLSelectElement.cpp: (WebCore::HTMLSelectElement::selectAll): Selects all items in a multi-select list box.  Single-select
              list boxes will just do nothing, but we still want the select to handle this case when it has focus, rather than selecting other content.
            * html/HTMLSelectElement.h: (WebCore::HTMLSelectElement::canSelectAll): Returns true for list boxes.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18261 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLSelectElement.h
+++ third_party/WebKit/WebCore/html/HTMLSelectElement.h
@@ -52,6 +52,8 @@ public:
     
     virtual bool isKeyboardFocusable(KeyboardEvent*) const;
     virtual bool isMouseFocusable() const;
+    virtual bool canSelectAll() const { return !usesMenuList(); }
+    virtual void selectAll();
 
     virtual void recalcStyle(StyleChange);
 
