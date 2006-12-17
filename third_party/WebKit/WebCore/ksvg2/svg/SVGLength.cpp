ef9b8926245a89351b520bdc7d4a60cf6514c3b3
zimmermann <zimmermann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-17 12:52:00 +0000

    Build fix, not reviewed.
    This should hopefully fix Qt build.
    * ksvg2/svg/SVGLength.cpp: Include math.h for Qt/Linux build.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGLength.cpp
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGLength.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGStyledElement.h"
 #include "svgpathparser.h"
 
+#include <math.h>
 #include <wtf/Assertions.h>
 
 namespace WebCore {
