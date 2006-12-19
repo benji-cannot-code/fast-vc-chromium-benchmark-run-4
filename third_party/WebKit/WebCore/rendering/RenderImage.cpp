2a4e57023b01b0dd2e78b7f2cd8e6fd89fbca363
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-19 10:20:38 +0000

    Fix drawing of image alt text..
    git-svn-id: svn://svn.chromium.org/blink/trunk@18309 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderImage.cpp
+++ third_party/WebKit/WebCore/rendering/RenderImage.cpp
@@ -251,7 +251,7 @@ void RenderImage::paint(PaintInfo& paintInfo, int tx, int ty)
                 DeprecatedString text = m_altText.deprecatedString();
                 text.replace('\\', backslashAsCurrencySymbol());
                 context->setFont(style()->font());
-                context->setStrokeColor(style()->color());
+                context->setFillColor(style()->color());
                 int ax = tx + leftBorder + leftPad;
                 int ay = ty + topBorder + topPad;
                 const Font& font = style()->font();
