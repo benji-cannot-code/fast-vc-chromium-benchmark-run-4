5c7365194c108725958c775bc7a9c99773886a0f
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-20 11:32:54 +0000

    Fix warning on buildbot on mac.
    git-svn-id: svn://svn.chromium.org/blink/trunk@18343 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/InlineTextBox.cpp
+++ third_party/WebKit/WebCore/rendering/InlineTextBox.cpp
@@ -384,7 +384,7 @@ void InlineTextBox::paint(RenderObject::PaintInfo& paintInfo, int tx, int ty)
                     selectionTextShadow = pseudoStyle->textShadow();
             }
             
-            short strokeWidth = pseudoStyle->textStrokeWidth();
+            float strokeWidth = pseudoStyle->textStrokeWidth();
             if (strokeWidth != selectionStrokeWidth) {
                 if (!paintSelectedTextOnly)
                     paintSelectedTextSeparately = true;
