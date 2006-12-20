9be0c2cc15baa9968d4e835d4d7ff173961a8059
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-20 06:00:38 +0000

    LayoutTests:
            Reviewed by Adele.
    
            <rdar://problem/4726407>
            [9A255] Crash in WebCore::EventTargetNode::dispatchEvent
    
            * fast/dom/resources/script-element-gc.js: Added.
            * fast/dom/script-element-gc-expected.txt: Added.
            * fast/dom/script-element-gc.html: Added.
    
    WebCore:
    
            Reviewed by Adele.
    
            <rdar://problem/4726407> 
            [9A255] Crash in WebCore::EventTargetNode::dispatchEvent
            
            * html/HTMLScriptElement.cpp:
            (WebCore::HTMLScriptElement::notifyFinished):
            Ref the script element while running the script since it can be freed
            when executing the script.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18335 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/resources/script-element-gc.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function gc()
+{
+    if (window.GCController)
+        return GCController.collect();
+
+    for (var i = 0; i < 10000; i++) { // > force garbage collection (FF requires about 9K allocations before a collect)
+        var s = new String("");
+    }
+}
+
+function removeScriptElement() {
+    var s = document.getElementById('theScript');
+    s.parentNode.removeChild(s);
+}
+
+removeScriptElement();
+gc();
