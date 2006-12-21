85f768609254a47ebbd50d04bdf015686f59afbc
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-21 08:08:59 +0000

    2006-12-21  Mark Rowe  <bdash@webkit.org>
            Reviewed by Geoff.
    
            http://bugs.webkit.org/show_bug.cgi?id=11888
            Bug 11888: REGRESSION (r18320): Web Inspector panes broken
    
            * WebInspector/webInspector/inspector.js: Use removeProperty to reset a style property to its
            initial value.  Bail out early from updateNodePane if Inspector has not yet been set.
    
    2006-12-21  Mark Rowe  <bdash@webkit.org>
    
            Reviewed by Geoff.
    
            http://bugs.webkit.org/show_bug.cgi?id=11888
            Bug 11888: REGRESSION (r18320): Web Inspector panes broken
    
            * Drosera/debugger.js: Use removeProperty to reset a style property to its initial value.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18374 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Drosera/debugger.js
+++ third_party/WebKit/WebKitTools/Drosera/debugger.js
@@ -159,7 +159,7 @@ function dividerDragEnd(element, dividerDrag, dividerDragEnd, event)
     element.dragging = false;
     document.removeEventListener("mousemove", dividerDrag, true);
     document.removeEventListener("mouseup", dividerDragEnd, true);
-    document.body.style.cursor = null;
+    document.body.style.removeProperty("cursor");
 }
 
 function dividerDrag(event) 
@@ -541,7 +541,7 @@ function breakpointDrag(event)
     if (!draggingBreakpoint) {
         sourcesDocument.removeEventListener("mousemove", breakpointDrag, true);
         sourcesDocument.removeEventListener("mouseup", breakpointDragEnd, true);
-        sourcesDocument.body.style.cursor = null;
+        sourcesDocument.body.style.removeProperty("cursor");
         return;
     }
 
@@ -582,7 +582,7 @@ function breakpointDrag(event)
             if (!dragImage) {
                 sourcesDocument.removeEventListener("mousemove", breakpointDrag, true);
                 sourcesDocument.removeEventListener("mouseup", breakpointDragEnd, true);
-                sourcesDocument.body.style.cursor = null;
+                sourcesDocument.body.style.removeProperty("cursor");
                 return;
             }
 
@@ -591,7 +591,7 @@ function breakpointDrag(event)
             if (x > 40)
                 dragImage.style.visibility = "hidden";
             else
-                dragImage.style.visibility = null;
+                dragImage.style.removeProperty("visibility");
         }
 
         draggingBreakpoint.dragLastX = x;
@@ -604,7 +604,7 @@ function breakpointDragEnd(event)
     var sourcesDocument = document.getElementById("sources").contentDocument;
     sourcesDocument.removeEventListener("mousemove", breakpointDrag, true);
     sourcesDocument.removeEventListener("mouseup", breakpointDragEnd, true);
-    sourcesDocument.body.style.cursor = null;
+    sourcesDocument.body.style.removeProperty("cursor");
 
     var dragImage = sourcesDocument.getElementById("breakpointDrag");
     if (!dragImage)
@@ -1013,7 +1013,7 @@ function loadFile(fileIndex, manageNavLists)
         file.loaded = true;
     }
 
-    file.element.style.display = null;
+    file.element.style.removeProperty("display");
 
     document.getElementById("filesPopupButtonContent").innerText = (file.url ? file.url : "(unknown script)");
     
