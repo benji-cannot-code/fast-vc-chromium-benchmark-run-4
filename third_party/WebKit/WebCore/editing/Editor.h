0b003a3f21c088a0dd7296cc53d61cd592935379
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-22 18:49:14 +0000

    Reviewed by Kevin Decker
            - renamed markMisspellingsInAdjacentWords to markMisspellingsAfterTypingToPosition
              in anticipation of fixing it to work more correctly with grammar checking, where
              only checking adjacent words isn't good enough.
    
            * editing/Editor.h:
            renamed markMisspellingsInAdjacentWords to markMisspellingsAfterTypingToPosition 
    
            * editing/TypingCommand.cpp:
            (WebCore::TypingCommand::markMisspellingsAfterTyping):
            updated for name change
    
            * editing/mac/EditorMac.mm:
            (WebCore::Editor::markMisspellingsAfterTypingToPosition):
            renamed from markMisspellingsInAdjacentWords and added FIXME comment
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/Editor.h
+++ third_party/WebKit/WebCore/editing/Editor.h
@@ -145,7 +145,7 @@ public:
     bool isSelectionMisspelled();
     Vector<String> guessesForMisspelledSelection();
     Vector<String> guessesForUngrammaticalSelection();
-    void markMisspellingsInAdjacentWords(const VisiblePosition&);
+    void markMisspellingsAfterTypingToPosition(const VisiblePosition&);
     void markMisspellings(const Selection&);
     void advanceToNextMisspelling(bool startBeforeSelection = false);
     void showSpellingGuessPanel();
