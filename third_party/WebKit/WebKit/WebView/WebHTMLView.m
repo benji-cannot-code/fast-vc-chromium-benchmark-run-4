a297b91369b79414a747fafdd0cbdfc4516a5ddb
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-22 14:11:15 +0000

    Reviewed by Geoff Garen
            - improved concept and implementation of my previous checkin after discussing with Darin
    
            * WebView/WebViewPrivate.h:
            * WebView/WebView.mm:
            (-[WebView setHoverFeedbackSuspended:]):
            renamed from setIgnoresMouseMovedEvents, and now tells the main WebHTMLView (if any) that this state has
            changed. Telling just the main WebHTMLView is a bit of an implementation hack. Hopefully someday we can
            rework the document architecture and make this kind of thing less hacky (but this is following existing
            customs, so I don't feel too bad)
            (-[WebView isHoverFeedbackSuspended]):
            renamed from ignoresMouseMovedEvents
    
            * WebView/WebHTMLViewInternal.h:
            declare _hoverFeedbackSuspendedChanged
    
            * WebView/WebHTMLView.m:
            (-[WebHTMLView _updateMouseoverWithEvent:]):
            test the webView's state of the hit-tested WebHTMLView rather than self 
            (-[WebHTMLView _hoverFeedbackSuspendedChanged]):
            generate a fake mouse-moved event, which simulates the mouse moving away from
            the current element or back over it
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebHTMLView.m
+++ third_party/WebKit/WebKit/WebView/WebHTMLView.m
@@ -1121,7 +1121,7 @@ static WebHTMLView *lastHitView = nil;
 
 - (void)_updateMouseoverWithEvent:(NSEvent *)event
 {
-    if (_private->closed || [[self _webView] ignoresMouseMovedEvents])
+    if (_private->closed)
         return;
 
     NSView *contentView = [[event window] contentView];
@@ -1132,8 +1132,8 @@ static WebHTMLView *lastHitView = nil;
     forceWebHTMLViewHitTest = NO;
     
     WebHTMLView *view = nil;
-    if ([hitView isKindOfClass:[WebHTMLView class]]) 
-        view = (WebHTMLView *)hitView; 
+    if ([hitView isKindOfClass:[WebHTMLView class]] && ![[(WebHTMLView *)hitView _webView] isHoverFeedbackSuspended])
+        view = (WebHTMLView *)hitView;    
 
     if (view)
         [view retain];
@@ -5231,6 +5231,11 @@ static DOMRange *unionDOMRanges(DOMRange *a, DOMRange *b)
     rect.origin = [[self window] convertBaseToScreen:rect.origin];
     NSData *data = [attrString RTFFromRange:NSMakeRange(0, [attrString length]) documentAttributes:nil];
     dictionaryServiceWindowShow(data, rect, (writingDirection == NSWritingDirectionRightToLeft) ? 1 : 0);
+}
+
+- (void)_hoverFeedbackSuspendedChanged
+{
+    [self _updateMouseoverWithFakeEvent];
 }
 
 @end
--- third_party/WebKit/WebKit/WebView/WebHTMLViewInternal.h
+++ third_party/WebKit/WebKit/WebView/WebHTMLViewInternal.h
@@ -116,6 +116,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)paste:(id)sender;
 - (void)pasteAsPlainText:(id)sender;
 - (void)_lookUpInDictionaryFromMenu:(id)sender;
+- (void)_hoverFeedbackSuspendedChanged;
 #if !BUILDING_ON_TIGER
 - (BOOL)isGrammarCheckingEnabled;
 - (void)setGrammarCheckingEnabled:(BOOL)flag;
