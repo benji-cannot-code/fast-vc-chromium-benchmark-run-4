a3303c9359a0599c444cb697401822fb0e44de5f
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-24 07:19:11 +0000

    Reviewed by Maciej Stachowiak.
            Added --threaded support to run-webkit-tests and DumpRenderTree.
            
            In "threaded" mode, DRT runs a concurrent JavaScript thread with each test,
            stressing the thread safety of JavaScriptCore and the JavaScriptCore/WebCore 
            interface. This is useful for tracking down bugs you might see on a system
            configured to use a PAC file.
            
            Ironically, I can proudly state that very few layout tests pass.
    
            * DumpRenderTree/DumpRenderTree.m: Added javaScriptThread and helper functions
            for starting and stopping it.
            (runJavaScriptThread): helper function
            (startJavaScriptThread): helper function
            (stopJavaScriptThread): helper function
    
            (dumpRenderTree): Added --threaded command line argument.
            * Scripts/run-webkit-tests: ditto
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -39,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "TextInputController.h"
 #import "UIDelegate.h"
 #import <ApplicationServices/ApplicationServices.h> // for CMSetDefaultProfileBySpace
+#import <CoreFoundation/CoreFoundation.h>
+#import <JavaScriptCore/JavaScriptCore.h>
 #import <WebKit/DOMElementPrivate.h>
 #import <WebKit/DOMExtensions.h>
 #import <WebKit/DOMRange.h>
@@ -58,8 +60,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebView.h>
 #import <getopt.h>
 #import <malloc/malloc.h>
-#import <CoreFoundation/CoreFoundation.h>
 #import <objc/objc-runtime.h>                       // for class_poseAs
+#import <pthread.h>
 
 #define COMMON_DIGEST_FOR_OPENSSL
 #import <CommonCrypto/CommonDigest.h>               // for MD5 functions
@@ -119,6 +121,7 @@ static BOOL dumpBackForwardList;
 static BOOL dumpChildFrameScrollPositions;
 static int dumpPixels = NO;
 static int dumpAllPixels = NO;
+static int threaded = NO;
 static BOOL readFromWindow = NO;
 static int testRepaintDefault = NO;
 static BOOL testRepaint = NO;
@@ -139,6 +142,49 @@ static BOOL workQueueFrozen;
 
 const unsigned maxViewHeight = 600;
 const unsigned maxViewWidth = 800;
+
+// Loops forever, running a script
+void* runJavaScriptThread(void* arg)
+{
+    char* script =
+    " \
+    var array = []; \
+    for (var i = 0; i < 1000; i++) { \
+        array.push(String(i)); \
+    } \
+    ";
+
+    while(1) {
+        JSGlobalContextRef ctx = JSGlobalContextCreate(NULL);
+        JSStringRef scriptRef = JSStringCreateWithUTF8CString(script);
+
+        JSValueRef exception = NULL;
+        JSEvaluateScript(ctx, scriptRef, NULL, NULL, 0, &exception);
+        assert(!exception);
+        
+        JSGlobalContextRelease(ctx);
+        JSStringRelease(scriptRef);
+        
+        JSGarbageCollect(ctx);
+
+        pthread_testcancel(); // Allow thread termination
+    }
+}
+
+static pthread_t javaScriptThread;
+
+static void startJavaScriptThread(void)
+{
+    assert(!javaScriptThread);
+    pthread_create(&javaScriptThread, NULL, runJavaScriptThread, NULL);
+}
+
+static void stopJavaScriptThread(void)
+{
+    assert(javaScriptThread);
+    pthread_cancel(javaScriptThread);
+    javaScriptThread = NULL;
+}
 
 BOOL doneLoading(void)
 {
@@ -241,6 +287,7 @@ void dumpRenderTree(int argc, const char *argv[])
         {"pixel-tests", no_argument, &dumpPixels, YES},
         {"repaint", no_argument, &testRepaintDefault, YES},
         {"tree", no_argument, &dumpTree, YES},
+        {"threaded", no_argument, &threaded, YES},
         {NULL, 0, NULL, 0}
     };
 
@@ -348,6 +395,9 @@ void dumpRenderTree(int argc, const char *argv[])
     // dynamic scrollbars properly. Without it, every frame will always have scrollbars.
     NSBitmapImageRep *imageRep = [webView bitmapImageRepForCachingDisplayInRect:[webView bounds]];
     [webView cacheDisplayInRect:[webView bounds] toBitmapImageRep:imageRep];
+
+    if (threaded)
+        startJavaScriptThread();
     
     if (argc == optind+1 && strcmp(argv[optind], "-") == 0) {
         char filenameBuffer[2048];
@@ -368,6 +418,9 @@ void dumpRenderTree(int argc, const char *argv[])
         for (int i = optind; i != argc; ++i)
             runTest(argv[i]);
     }
+
+    if (threaded)
+        stopJavaScriptThread();
     
     [workQueue release];
 
