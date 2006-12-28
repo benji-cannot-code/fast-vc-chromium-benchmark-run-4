d0decd55e6a572e5386d70cde5e4038c89a08fce
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-28 22:53:32 +0000

    2006-12-28  David Kilzer  <ddkilzer@webkit.org>
            Reviewed by Mitz.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=12016
              REGRESSION: fast/text/stroking-decorations.html test fails with bus error in no-svg build
    
            No test cases (no change in functionality).
    
            * platform/TextStream.cpp: Properly initialize presicionFormats.
            * rendering/RenderTreeAsText.cpp:
            (WebCore::externalRepresentation): HTML tests now print out the stroke width, which is a float,
            so the precision needs to be set.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/TextStream.cpp
+++ third_party/WebKit/WebCore/platform/TextStream.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 const size_t integerOrPointerAsStringBufferSize = 100; // large enough for any integer or pointer in string format, including trailing null character
-const char* const precisionFormats[6] = { "%.0f", "%.1f", "%.2f", "%.3f", "%.4f", "%.5f" "%.6f"}; 
+const char* const precisionFormats[7] = { "%.0f", "%.1f", "%.2f", "%.3f", "%.4f", "%.5f", "%.6f"}; 
 const int maxPrecision = 6; // must match size of precisionFormats
 const int defaultPrecision = 6; // matches qt and sprintf(.., "%f", ...) behaviour
 
