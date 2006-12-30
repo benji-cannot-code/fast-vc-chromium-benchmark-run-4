2aaadf788f04d8f4886a1d92dc295b16421596a0
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2006-12-30 08:34:00 +0000

    Reviewed by Mitz.
            http://bugs.webkit.org/show_bug.cgi?id=11998
            Incorrect serialization of quotation marks in XML attributes.
    
            Test: fast/dom/serialize-attribute.xhtml
    
            * editing/markup.cpp:
            (WebCore::escapeTextForMarkup): Added an isAttributeValue parameter, as the quotation mark
            only needs to be encoded in attribute values.
            (WebCore::startMarkup): Pass appropriate isAttributeValue to escapeTextForMarkup().
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18484 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/markup.cpp
+++ third_party/WebKit/WebCore/editing/markup.cpp
@@ -62,7 +62,7 @@ using namespace HTMLNames;
 
 static inline bool shouldSelfClose(const Node *node);
 
-static DeprecatedString escapeTextForMarkup(const DeprecatedString &in)
+static DeprecatedString escapeTextForMarkup(const DeprecatedString &in, bool isAttributeValue)
 {
     DeprecatedString s = "";
 
@@ -78,6 +78,12 @@ static DeprecatedString escapeTextForMarkup(const DeprecatedString &in)
             case '>':
                 s += "&gt;";
                 break;
+            case '"':
+                if (isAttributeValue) {
+                    s += "&quot;";
+                    break;
+                }
+                // fall through
             default:
                 s += in[i];
         }
@@ -164,7 +170,7 @@ static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnot
             }
             bool useRenderedText = annotate && !enclosingNodeWithTag(const_cast<Node*>(node), selectTag);
             
-            DeprecatedString markup = useRenderedText ? escapeTextForMarkup(renderedText(node, range)) : escapeTextForMarkup(stringValueForRange(node, range).deprecatedString());            
+            DeprecatedString markup = useRenderedText ? escapeTextForMarkup(renderedText(node, range), false) : escapeTextForMarkup(stringValueForRange(node, range).deprecatedString(), false);
             if (defaultStyle) {
                 Node *element = node->parentNode();
                 if (element) {
@@ -238,7 +244,7 @@ static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnot
                     markup += " " + attr->name().localName().deprecatedString();
                 else
                     markup += " " + attr->name().toString().deprecatedString();
-                markup += "=\"" + escapeTextForMarkup(value.deprecatedString()) + "\"";
+                markup += "=\"" + escapeTextForMarkup(value.deprecatedString(), true) + "\"";
             }
             
             if (additionalStyle.length() > 0)
