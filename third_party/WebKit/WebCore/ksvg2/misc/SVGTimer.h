0c487e872727705cd8d4a89e616205fe999e2ca7
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-01 06:47:28 +0000

    2007-01-01  Eric Seidel  <eric@webkit.org>
            Reviewed by olliej.
    
            Split out animationsByElement() logic in preparation for adding AnimationCompositor class
            Add addColorsAndClamp helper (since adding and clamping is the common case)
    
            * ksvg2/misc/SVGTimer.cpp:
            (WebCore::SVGTimer::animationsByElement): split out from notifyAll
            (WebCore::SVGTimer::notifyAll):
            * ksvg2/misc/SVGTimer.h:
            * ksvg2/svg/SVGAnimateColorElement.cpp:
            (WebCore::SVGAnimateColorElement::addColorsAndClamp): added.
            * ksvg2/svg/SVGAnimateColorElement.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/misc/SVGTimer.h
+++ third_party/WebKit/WebCore/ksvg2/misc/SVGTimer.h
@@ -26,10 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "TimeScheduler.h"
 #include "Timer.h"
-#include "wtf/HashSet.h"
+#include <wtf/HashSet.h>
+#include <wtf/HashMap.h>
 
 namespace WebCore {
 
+class SVGElement;
 class SVGAnimationElement;
 
 typedef HashSet<SVGAnimationElement*> SVGNotifySet;
@@ -51,6 +53,9 @@ public:
 
 private:
     double calculateTimePercentage(double elapsed, double start, double end, double duration, double repetitions);
+    
+    typedef HashMap<SVGElement*, Vector<SVGAnimationElement*> > TargetAnimationMap;
+    TargetAnimationMap animationsByElement(double elapsedTime);
 
     TimeScheduler* m_scheduler;
     double m_interval;
