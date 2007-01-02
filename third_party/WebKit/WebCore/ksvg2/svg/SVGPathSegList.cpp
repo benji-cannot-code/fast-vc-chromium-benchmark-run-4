37745aec309d1c21622b7c3c3ca1811e05928b89
zack <zack@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-02 10:13:06 +0000

    fix compile for svg as well (filenames are case-sensitive)
    git-svn-id: svn://svn.chromium.org/blink/trunk@18520 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGPathSegList.cpp
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGPathSegList.cpp
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FloatPoint.h"
 #include "Path.h"
 #include "PathTraversalState.h"
-#include "SVGPathSegMoveTo.h"
-#include "SVGPathSegLineTo.h"
+#include "SVGPathSegMoveto.h"
+#include "SVGPathSegLineto.h"
 #include "SVGPathSegCurvetoCubic.h"
 
 namespace WebCore {
