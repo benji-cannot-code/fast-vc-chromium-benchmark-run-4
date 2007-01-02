005ba24172fb6eb5caa9371c9199d55aa13716f2
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-02 17:41:50 +0000

    LayoutTests:
            Reviewed by Mitz and Anders.
    
            Testcases for http://bugs.webkit.org/show_bug.cgi?id=7075
    
            * fast/forms/textarea-align-expected.checksum: Added.
            * fast/forms/textarea-align-expected.png: Added.
            * fast/forms/textarea-align-expected.txt: Added.
            * fast/forms/textarea-align.html: Added.
    
    WebCore:
    
            Reviewed by Mitz and Anders.
    
            Patch for http://bugs.webkit.org/show_bug.cgi?id=7075
            textarea with align=right should not cause contained text to 
            be right-justified
    
            Don't map align for textarea elements.
    
            Test: fast/forms/textarea-align.html
    
            * html/HTMLTextAreaElement.cpp:
            (WebCore::HTMLTextAreaElement::parseMappedAttribute):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTextAreaElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLTextAreaElement.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
  * Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.
  *           (C) 2006 Alexey Proskuryakov (ap@nypop.com)
+ * Copyright (C) 2007 Samuel Weinig (sam@webkit.org)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -150,6 +151,9 @@ void HTMLTextAreaElement::parseMappedAttribute(MappedAttribute *attr)
             renderer()->setNeedsLayoutAndMinMaxRecalc();
     } else if (attr->name() == accesskeyAttr) {
         // ignore for the moment
+    } else if (attr->name() == alignAttr) {
+        // Don't map 'align' attribute.  This matches what Firefox, Opera and IE do.
+        // See http://bugs.webkit.org/show_bug.cgi?id=7075
     } else if (attr->name() == onfocusAttr)
         setHTMLEventListener(focusEvent, attr);
     else if (attr->name() == onblurAttr)
