faf750dedab828634c2aa3a078a52bc2194ecdce
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-05 16:15:22 +0000

    Reviewed by Geoff.
            http://bugs.webkit.org/show_bug.cgi?id=7296
            Disable JavaScript exceptions when dynamically changing CSS properties.
    
            * bindings/js/kjs_css.cpp:
            (KJS::DOMCSSStyleDeclaration::put): Removed the Dashboard quirk logic.
    
            * css/CSSMutableStyleDeclaration.cpp:
            (WebCore::CSSMutableStyleDeclaration::setProperty): Don't raise an exception if parsing fails.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18618 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/kjs_css.cpp
+++ third_party/WebKit/WebCore/bindings/js/kjs_css.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSStyleRule.h"
 #include "CSSValueList.h"
 #include "Document.h"
-#include "Frame.h"
 #include "HTMLNames.h"
 #include "HTMLStyleElement.h"
 #include "JSCSSPrimitiveValue.h"
@@ -45,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSCSSRuleList.h"
 #include "JSCSSValueList.h"
 #include "MediaList.h"
-#include "Settings.h"
 #include "StyleSheetList.h"
 #include "kjs_dom.h"
 
@@ -56,6 +54,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifdef SVG_SUPPORT
 #include "JSSVGColor.h"
 #include "JSSVGPaint.h"
+#include "SVGColor.h"
+#include "SVGPaint.h"
 #endif
 
 using namespace WebCore;
@@ -226,20 +226,6 @@ void DOMCSSStyleDeclaration::put(ExecState* exec, const Identifier &propertyName
 #ifdef KJS_VERBOSE
       kdDebug(6070) << "DOMCSSStyleDeclaration: prop=" << prop << " propvalue=" << propvalue << endl;
 #endif
-      StyleSheet* stylesheet = styleDecl.stylesheet();
-      // Computed style declarations don't have stylesheets.
-      if (stylesheet) {
-        ASSERT(stylesheet->isCSSStyleSheet());
-        if (Frame* frame = static_cast<CSSStyleSheet*>(stylesheet)->doc()->frame())
-          if (frame->settings()->usesDashboardBackwardCompatibilityMode()) {
-            styleDecl.removeProperty(prop, exception);
-            if (!exception) {
-              ExceptionCode exceptionIgnored = 0;
-              styleDecl.setProperty(prop, propValue, exceptionIgnored);
-            }
-            return;
-          }
-      }
       styleDecl.setProperty(prop, propValue, exception);
     } else {
       DOMObject::put(exec, propertyName, value, attr);
