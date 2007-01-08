812979052c8399d6264b1035c7df510921d77f4e
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-08 19:55:56 +0000

    Reviewed by Darin.
            - fix http://bugs.webkit.org/show_bug.cgi?id=12162
              The inheritableProperties array is duplicated
    
            No test possible (no change to functionality).
    
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::CSSComputedStyleDeclaration::removeComputedInheritablePropertiesFrom): Added.
            Removes the inheritable properties that CSSComputedStyleDeclaration knows how to compute
            from the given mutable declaration.
            * css/CSSComputedStyleDeclaration.h:
            * css/CSSMutableStyleDeclaration.cpp: Removed one copy of the inheritableProperties array.
            * css/CSSMutableStyleDeclaration.h:
            * editing/ReplaceSelectionCommand.cpp:
            (WebCore::ReplaceSelectionCommand::removeRedundantStyles): Changed to call
            CSSComputedStyleDeclaration::removeComputedInheritablePropertiesFrom.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18676 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSComputedStyleDeclaration.h
+++ third_party/WebKit/WebCore/css/CSSComputedStyleDeclaration.h
@@ -59,6 +59,8 @@ public:
 
     PassRefPtr<CSSMutableStyleDeclaration> copyInheritableProperties() const;
 
+    static void removeComputedInheritablePropertiesFrom(CSSMutableStyleDeclaration*);
+
 private:
     virtual void setCssText(const String&, ExceptionCode&);
     virtual String removeProperty(int propertyID, ExceptionCode&);
