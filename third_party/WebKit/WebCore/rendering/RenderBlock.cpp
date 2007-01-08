997329147df6feabd6536d0a04f563f65e9983a0
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-08 19:17:52 +0000

    Reviewed by Mitz.
            Attempt to fix build for buildbots.
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::columnGap):
            (WebCore::RenderBlock::calcColumnWidth):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18672 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBlock.cpp
@@ -2788,7 +2788,7 @@ int RenderBlock::columnGap() const
 {
     if (style()->hasNormalColumnGap())
         return style()->fontDescription().computedPixelSize(); // "1em" is recommended as the normal gap setting. Matches <p> margins.
-    return style()->columnGap();
+    return static_cast<int>(style()->columnGap());
 }
 
 void RenderBlock::calcColumnWidth()
@@ -2814,14 +2814,14 @@ void RenderBlock::calcColumnWidth()
             m_columnWidth = (availWidth - (m_columnCount - 1) * colGap) / m_columnCount;
         }
     } else if (style()->hasAutoColumnCount()) {
-        int colWidth = style()->columnWidth();
+        int colWidth = static_cast<int>(style()->columnWidth());
         if (colWidth < availWidth) {
             m_columnCount = (availWidth + colGap) / (colWidth + colGap);
             m_columnWidth = (availWidth - (m_columnCount - 1) * colGap) / m_columnCount;
         }
     } else {
         // Both are set.
-        int colWidth = style()->columnWidth();
+        int colWidth = static_cast<int>(style()->columnWidth());
         int colCount = style()->columnCount();
     
         if (colCount * colWidth + (colCount - 1) * colGap <= availWidth) {
