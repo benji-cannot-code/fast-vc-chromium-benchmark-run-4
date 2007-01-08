9b4df09285b2467a138c1b50722f9a1ace212165
rwlbuis <rwlbuis@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-08 13:43:10 +0000

    Reviewed by bdash.
            Initialize the member vars, fixes testcases
            pointer-events-path.svg and pointer-events-text.svg
            on the PPC build.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18665 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/misc/PointerEventsHitRules.cpp
+++ third_party/WebKit/WebCore/ksvg2/misc/PointerEventsHitRules.cpp
@@ -27,6 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 PointerEventsHitRules::PointerEventsHitRules(ESVGHitTesting hitTesting, EPointerEvents pointerEvents)
+    : requireVisible(false)
+    , requireFill(false)
+    , requireStroke(false)
+    , canHitStroke(false)
+    , canHitFill(false)
 {
     if (hitTesting == SVG_PATH_HITTESTING) {
         switch (pointerEvents)
