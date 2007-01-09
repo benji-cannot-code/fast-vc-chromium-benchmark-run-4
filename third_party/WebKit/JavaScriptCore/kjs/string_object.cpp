5fb48d03b3c7c8c7f6c1de43012a8bdca54dc89b
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-09 21:13:39 +0000

    - fix build
            * kjs/string_object.cpp: (KJS::StringProtoFunc::callAsFunction):
            Change types.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18717 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/string_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/string_object.cpp
@@ -683,8 +683,8 @@ JSValue *StringProtoFunc::callAsFunction(ExecState *exec, JSObject *thisObj, con
   case ToLocaleLowerCase: { // FIXME: See http://www.unicode.org/Public/UNIDATA/SpecialCasing.txt for locale-sensitive mappings that aren't implemented.
     u = s;
     u.copyForWriting();
-    Unicode::UChar* dataPtr = reinterpret_cast<Unicode::UChar*>(u.rep()->data());
-    Unicode::UChar* destIfNeeded;
+    ::UChar* dataPtr = reinterpret_cast<::UChar*>(u.rep()->data());
+    ::UChar* destIfNeeded;
 
     int len = Unicode::toLower(dataPtr, u.size(), destIfNeeded);
     if (len >= 0)
@@ -699,8 +699,8 @@ JSValue *StringProtoFunc::callAsFunction(ExecState *exec, JSObject *thisObj, con
   case ToLocaleUpperCase: { // FIXME: See http://www.unicode.org/Public/UNIDATA/SpecialCasing.txt for locale-sensitive mappings that aren't implemented.
     u = s;
     u.copyForWriting();
-    Unicode::UChar* dataPtr = reinterpret_cast<Unicode::UChar*>(u.rep()->data());
-    Unicode::UChar* destIfNeeded;
+    ::UChar* dataPtr = reinterpret_cast<::UChar*>(u.rep()->data());
+    ::UChar* destIfNeeded;
 
     int len = Unicode::toUpper(dataPtr, u.size(), destIfNeeded);
     if (len >= 0)
