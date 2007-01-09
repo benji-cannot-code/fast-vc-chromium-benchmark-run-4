ff09c885ddb7e5347403992e16b12bcab7091d1f
rwlbuis <rwlbuis@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-09 13:56:31 +0000

    Reviewed by Eric.
            http://bugs.webkit.org/show_bug.cgi?id=12129
            repaint issue after setting viewBox attribute with ECMAScript
    
            Make sure RenderSVGContainer updates after setting viewBox from js.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18705 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGSVGElement.cpp
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGSVGElement.cpp
@@ -211,8 +211,10 @@ void SVGSVGElement::parseMappedAttribute(MappedAttribute* attr)
             return;
         if (SVGExternalResourcesRequired::parseMappedAttribute(attr))
             return;
-        if (SVGFitToViewBox::parseMappedAttribute(attr) && renderer())
+        if (SVGFitToViewBox::parseMappedAttribute(attr) && renderer()) {
             static_cast<RenderSVGContainer*>(renderer())->setViewBox(viewBox());
+            return;
+        }
         if (SVGZoomAndPan::parseMappedAttribute(attr))
             return;
 
