d72e277ed3a4d56093a4369805616f19cc27b588
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-10 07:24:06 +0000

    Break up painting and hit testing of children into helper methods and add multi-column versions for both.
            Make incremental repainting able to invalidate the right spot in multiple column layouts so that e.g., link
            hover works.
    
            Turn off selection gap filling in columns for now until it can be made to work correctly.
    
            Improve the column balancing heuristic by adding in the block's lineHeight/2 to each column initially to
            account for column break adjustments that shrink the columns slightly.  Avoid clipping of the last column's
            contents by ensuring it gets all the remaining available height.
    
            Reviewed by mitz
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::paintColumns):
            (WebCore::RenderBlock::paintContents):
            (WebCore::RenderBlock::paintObject):
            (WebCore::RenderBlock::fillSelectionGaps):
            (WebCore::RenderBlock::nodeAtPoint):
            (WebCore::RenderBlock::hitTestColumns):
            (WebCore::RenderBlock::layoutColumns):
            (WebCore::RenderBlock::adjustRepaintRectForColumns):
            * rendering/RenderBlock.h:
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::computeAbsoluteRepaintRect):
            * rendering/RenderFlow.cpp:
            (WebCore::RenderFlow::getAbsoluteRepaintRect):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBlock.h
+++ third_party/WebKit/WebCore/rendering/RenderBlock.h
@@ -143,6 +143,7 @@ public:
     virtual void paintObject(PaintInfo&, int tx, int ty);
     void paintFloats(PaintInfo&, int tx, int ty, bool paintSelection = false);
     void paintContents(PaintInfo&, int tx, int ty);
+    void paintColumns(PaintInfo&, int tx, int ty);
     void paintChildren(PaintInfo&, int tx, int ty);
     void paintEllipsisBoxes(PaintInfo&, int tx, int ty);
     void paintSelection(PaintInfo&, int tx, int ty);
@@ -191,6 +192,7 @@ public:
     int leftOffset(int y) const { return leftRelOffset(y, leftOffset(), true); }
 
     virtual bool nodeAtPoint(const HitTestRequest&, HitTestResult&, int x, int y, int tx, int ty, HitTestAction);
+    virtual bool hitTestColumns(const HitTestRequest&, HitTestResult&, int x, int y, int tx, int ty, HitTestAction);
     virtual bool hitTestContents(const HitTestRequest&, HitTestResult&, int x, int y, int tx, int ty, HitTestAction);
 
     virtual bool isPointInScrollbar(HitTestResult&, int x, int y, int tx, int ty);
@@ -280,7 +282,8 @@ public:
     void clearTruncation();
 
     virtual bool hasColumns() const { return m_columnCount > 1; }
-    
+    void adjustRepaintRectForColumns(IntRect&) const;
+
 protected:
     void newLine();
     virtual bool hasLineIfEmpty() const;
