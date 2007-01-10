6ca227508aeed6fb5a38e1a0ab6d23c900f059cf
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-10 00:54:14 +0000

    Reviewed by Brady.
            Add default constructor to SharedBuffer.
    
            * platform/SharedBuffer.cpp:
            (WebCore::SharedBuffer::SharedBuffer):
            * platform/SharedBuffer.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/SharedBuffer.cpp
+++ third_party/WebKit/WebCore/platform/SharedBuffer.cpp
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+SharedBuffer::SharedBuffer()
+{
+}
+
 SharedBuffer::SharedBuffer(const char* data, int size)
 {
     m_buffer.append(data, size);
