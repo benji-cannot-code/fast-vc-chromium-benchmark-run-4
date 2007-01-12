016cb5424a086e0bca8034869cdbdb7efa542bc4
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-12 08:39:40 +0000

    Fix the Qt build after the Authentication changes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@18800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceHandleInternal.h
+++ third_party/WebKit/WebCore/platform/network/ResourceHandleInternal.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ResourceHandleInternal_h
 
 #include "ResourceRequest.h"
+#include "AuthenticationChallenge.h"
 
 #if USE(CFNETWORK)
 #include <CFNetwork/CFURLConnectionPriv.h>
