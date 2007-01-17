2e021dd7ce027ad7522234b30094d3a906e6900c
eseidel <eseidel@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-17 12:21:12 +0000

    2007-01-17  Eric Seidel  <eric@webkit.org>
            No review, build fix only.
    
            * platform/graphics/cg/ImageCG.cpp: correct drawPatternCombined to drawPattern
            (WebCore::Image::drawPattern):
            (WebCore::Image::drawTiled):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18905 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/ImageCG.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cg/ImageCG.cpp
@@ -184,7 +184,7 @@ void Image::drawPatternCallback(void* info, CGContextRef context)
     CGContextDrawImage(context, GraphicsContext(context).roundToDevicePixels(FloatRect(0, data->size().height() - h, w, h)), image);
 }
 
-void Image::drawPatternCombined(GraphicsContext* ctxt, const FloatRect& tileRect, const AffineTransform& patternTransform,
+void Image::drawPattern(GraphicsContext* ctxt, const FloatRect& tileRect, const AffineTransform& patternTransform,
                                 const FloatPoint& phase, CompositeOperator op, const FloatRect& destRect)
 {
     static const CGPatternCallbacks patternCallbacks = { 0, drawPatternCallback, NULL };
@@ -264,7 +264,7 @@ void Image::drawTiled(GraphicsContext* ctxt, const FloatRect& destRect, const Fl
     }
 
     FloatRect tileRect(FloatPoint(), intrinsicTileSize);    
-    drawPatternCombined(ctxt, tileRect, patternTransform, oneTileRect.location(), op, destRect);
+    drawPattern(ctxt, tileRect, patternTransform, oneTileRect.location(), op, destRect);
     
     startAnimation();
 }
@@ -293,7 +293,7 @@ void Image::drawTiled(GraphicsContext* ctxt, const FloatRect& dstRect, const Flo
         vPhase -= fmodf(dstRect.height(), scale.height() * srcRect.height()) / 2.0f;
     FloatPoint patternPhase(dstRect.x() - hPhase, dstRect.y() - vPhase);
     
-    drawPatternCombined(ctxt, srcRect, patternTransform, patternPhase, op, dstRect);
+    drawPattern(ctxt, srcRect, patternTransform, patternPhase, op, dstRect);
 
     startAnimation();
 }
