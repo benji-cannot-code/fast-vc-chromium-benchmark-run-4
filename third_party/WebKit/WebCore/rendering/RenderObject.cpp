4313761a7a72295b73a7ace1d71ff724b3406e89
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-17 21:50:42 +0000

    LayoutTests:
            Reviewed by Beth.
    
            Test for http://bugs.webkit.org/show_bug.cgi?id=10139
            <rdar://problem/4823028> REGRESSION: Reproducible crash when selecting a popup/button with ::selection pseudostyle
    
            * fast/css/pseudostyle-anonymous-text-expected.txt: Added.
            * fast/css/pseudostyle-anonymous-text.html: Added.
    
    WebCore:
    
            Reviewed by Beth.
    
            Fix for  http://bugs.webkit.org/show_bug.cgi?id=10139
            <rdar://problem/4823028> REGRESSION: Reproducible crash when selecting a popup/button with ::selection pseudostyle
    
            Test: fast/css/pseudostyle-anonymous-text.html
    
            * rendering/RenderObject.cpp: (WebCore::RenderObject::getPseudoStyle): Nil check the node.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18915 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/WebCore/rendering/RenderObject.cpp
@@ -2859,8 +2859,8 @@ RenderStyle* RenderObject::getPseudoStyle(RenderStyle::PseudoId pseudo, RenderSt
         return result;
 
     Node* node = element();
-    if (isText())
-        node = element()->parentNode();
+    if (node && isText())
+        node = node->parentNode();
     if (!node)
         return 0;
 
