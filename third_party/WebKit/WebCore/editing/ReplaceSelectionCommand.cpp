2133d63fb3a18f6d31e6a2c86f81a628490333e0
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-18 19:13:14 +0000

    Reviewed by John.
            - fixed <rdar://problem/4887416> REGRESSION (SearchField): Assertion failure in HTMLInputElement::setValueFromRenderer when editing via drag and drop (11846)
            http://bugs.webkit.org/show_bug.cgi?id=11846
            
            * editing/ReplaceSelectionCommand.cpp:
            (WebCore::ReplaceSelectionCommand::doApply): Don't insert extra paragraph separators to avoid
            nesting blocks in plaintext mode, since that's not an issue under normal circumstances.
            * html/HTMLInputElement.h:
            * manual-tests/drag-move-in-search-field.html: Added.
            * rendering/RenderTextControl.cpp:
            (WebCore::RenderTextControl::subtreeHasChanged): constrain the value; we'd
            rather truncate it than end up with an illegal value here.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18944 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/ReplaceSelectionCommand.cpp
+++ third_party/WebKit/WebCore/editing/ReplaceSelectionCommand.cpp
@@ -459,7 +459,8 @@ void ReplaceSelectionCommand::doApply()
     if (selection.isNone() || !selection.start().node())
         return;
     
-    if (!selection.isContentRichlyEditable())
+    bool selectionIsPlainText = !selection.isContentRichlyEditable();
+    if (selectionIsPlainText)
         m_matchStyle = true;
     
     Element* currentRoot = selection.rootEditableElement();
@@ -482,7 +483,8 @@ void ReplaceSelectionCommand::doApply()
     
     if (selectionStartWasStartOfParagraph && selectionEndWasEndOfParagraph ||
         startBlock == currentRoot ||
-        startBlock && startBlock->renderer() && startBlock->renderer()->isListItem())
+        startBlock && startBlock->renderer() && startBlock->renderer()->isListItem() ||
+        selectionIsPlainText)
         m_preventNesting = false;
     
     Position insertionPos = selection.start();
