a52e2dd54c9f86b8ed72946a42869b0d9c846bcd
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-18 02:30:54 +0000

    LayoutTests:
            Reviewed by darin
    
            <rdar://problem/4920488>
            REGRESSION: A crash occurs at WebCore::Range::processContents () when removing formatting from selected text (that contains a link)
    
            * editing/execCommand/4920488-expected.checksum: Added.
            * editing/execCommand/4920488-expected.png: Added.
            * editing/execCommand/4920488-expected.txt: Added.
            * editing/execCommand/4920488.html: Added.
            * fast/dom/Range/compareBoundaryPoints-1-expected.txt: Added.
            * fast/dom/Range/compareBoundaryPoints-1.html: Added.
    
    WebCore:
    
            Reviewed by darin
            
            <rdar://problem/4920488>
            REGRESSION: A crash occurs at WebCore::Range::processContents () when removing formatting from selected text (that contains a link)
    
            * dom/Range.cpp:
            (WebCore::Range::commonAncestorContainer): Return null if the nodes
            don't have a common ancestor.  As a side effect, the public 
            commonAncestorContainer now sets an exception if the Range starts
            in one document and ends in another, or starts or ends in a tree
            that's been removed from the document.  The spec doesn't cover
            this and there is no precedent in other browsers for this case because
            they don't allow invalid Ranges (you can create one with WebCore
            by removing a node that contains the start/end of a Range).
            (WebCore::Range::compareBoundaryPoints): If the the two positions
            are in nodes that have no common ancestor, return null.  This doesn't
            change the behavior of the public compareBoundaryPoints: it already
            throws an exception if the two positions are in nodes that don't have
            a common ancestor.  This follows the spec.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Range.cpp
+++ third_party/WebKit/WebCore/dom/Range.cpp
@@ -157,9 +157,6 @@ Node *Range::commonAncestorContainer(Node *containerA, Node *containerB)
         if (parentStart == parentEnd)
             break;
     }
-
-    if (!parentStart)
-        return containerA->document()->documentElement();
         
     return parentStart;
 }
@@ -507,6 +504,8 @@ short Range::compareBoundaryPoints( Node *containerA, int offsetA, Node *contain
     // case 4: containers A & B are siblings, or children of siblings
     // ### we need to do a traversal here instead
     Node *cmnRoot = commonAncestorContainer(containerA,containerB);
+    if (!cmnRoot)
+        return 0;
     Node *childA = containerA;
     while (childA && childA->parentNode() != cmnRoot)
         childA = childA->parentNode();
