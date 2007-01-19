c93641dc3e49c7e61db0c6b1b3f20a7a306932b0
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-19 01:23:23 +0000

    2007-01-18  Mitz Pettel  <mitz@webkit.org>
            Reviewed by Maciej.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=12039
              Assertion failure in WebCore::Font::primaryFont
    
            Test: fast/text/font-initial.html
    
            * css/cssstyleselector.cpp:
            (WebCore::CSSStyleSelector::applyProperty): Give 'font: initial' the
            Standard generic family. This makes the FontDescription different from
            that of the initial font of a GraphicsContext, ensuring that the latter
            will not be used.
    
    2007-01-18  Mitz Pettel  <mitz@webkit.org>
    
            Reviewed by Maciej.
    
            - test for http://bugs.webkit.org/show_bug.cgi?id=12039
              Assertion failure in WebCore::Font::primaryFont
    
            * fast/text/font-initial-expected.checksum: Added.
            * fast/text/font-initial-expected.png: Added.
            * fast/text/font-initial-expected.txt: Added.
            * fast/text/font-initial.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18971 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/cssstyleselector.cpp
+++ third_party/WebKit/WebCore/css/cssstyleselector.cpp
@@ -3570,6 +3570,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
                 fontDirty = true;
         } else if (isInitial) {
             FontDescription fontDescription;
+            fontDescription.setGenericFamily(FontDescription::StandardFamily);
             style->setLineHeight(RenderStyle::initialLineHeight());
             if (style->setFontDescription(fontDescription))
                 fontDirty = true;
