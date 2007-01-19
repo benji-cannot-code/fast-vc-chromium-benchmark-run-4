071801d0ff2d3848f228a67053f123f45a66e5d4
bdakin <bdakin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-19 21:57:23 +0000

    WebCore:         Reviewed by Maciej and Darin.
            Fix for <rdar://problem/4702420> REGRESSION: Null characters need 
            to be stripped from text nodes (townhall.com)
    
            This was a regression from the fix for <rdar://problem/3727939>
            Safari strips \0 characters from HTML tags making them valid, which
            was fixed with r12652. We still need to strip null characters in
            text nodes.
    
            * html/HTMLTokenizer.cpp:
            (WebCore::HTMLTokenizer::write): No behavior change here, this is
            just to make Darin happy.
            (WebCore::HTMLTokenizer::processToken): Call into StringImpl's new 
            createStrippingNull function instead of the normal StringImpl 
            constructor.
            * platform/StringImpl.cpp:
            (WebCore::StringImpl::createStrippingNull): Strip null.
            * platform/StringImpl.h:
    
    LayoutTests:
            Reviewed by Maciej and Darin.
    
            New layout tests and some updates results for the fix for <rdar://
            problem/4702420> REGRESSION: Null characters need to be stripped 
            from text nodes (townhall.com)
    
            * fast/dom/stripNullFromTextNodes-expected.checksum: Added.
            * fast/dom/stripNullFromTextNodes-expected.png: Added.
            * fast/dom/stripNullFromTextNodes-expected.txt: Added.
            * fast/dom/stripNullFromTextNodes.html: Added.
            * fast/js/null-char-in-string-expected.txt: New results.
            * fast/text/stripNullFromText-expected.checksum: Added.
            * fast/text/stripNullFromText-expected.png: Added.
            * fast/text/stripNullFromText-expected.txt: Added.
            * fast/text/stripNullFromText.html: Added.
            * tables/mozilla/images/adforce_imgis_com-expected.txt: New 
            results.
            * tables/mozilla_expected_failures/images/adforce_imgis_com-
            expected.checksum: Removed. This is the same test as the one above. 
            The same test was in two places.
            * tables/mozilla_expected_failures/images/adforce_imgis_com-
            expected.png: Removed.
            * tables/mozilla_expected_failures/images/adforce_imgis_com-
            expected.txt: Removed.
            * tables/mozilla_expected_failures/images/adforce_imgis_com.html: 
            Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@18988 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
+++ third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
@@ -1409,7 +1409,7 @@ bool HTMLTokenizer::write(const SegmentedString& str, bool appendData)
             ++src;
         } else {
             state.setDiscardLF(false);
-            *dest++ = *src;
+            *dest++ = cc;
             ++src;
         }
     }
@@ -1545,7 +1545,7 @@ PassRefPtr<Node> HTMLTokenizer::processToken()
         }
 
 #endif
-        currToken.text = new StringImpl( buffer, dest - buffer );
+        currToken.text = StringImpl::createStrippingNull(buffer, dest - buffer);
         if (currToken.tagName != commentAtom)
             currToken.tagName = textAtom;
     } else if (currToken.tagName == nullAtom) {
