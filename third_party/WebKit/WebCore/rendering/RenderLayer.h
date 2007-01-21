b1d5693afd9f4ef1450f863637d238a68377f0df
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-21 00:35:32 +0000

    LayoutTests:
            Reviewed by Darin.
    
            - test for http://bugs.webkit.org/show_bug.cgi?id=8360
              Repro crash when onscroll handler deletes the scrolled object
    
            * fast/layers/removed-by-scroll-handler-expected.checksum: Added.
            * fast/layers/removed-by-scroll-handler-expected.png: Added.
            * fast/layers/removed-by-scroll-handler-expected.txt: Added.
            * fast/layers/removed-by-scroll-handler.html: Added.
    
    WebCore:
    
            Reviewed by Darin.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=8360
              Repro crash when onscroll handler deletes the scrolled object
    
            Test: fast/layers/removed-by-scroll-handler.html
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::RenderLayer):
            (WebCore::RenderLayer::dispatchScrollEvent): Added. Called when the timer
            fires and dispatches the scroll event to the layer's element.
            (WebCore::RenderLayer::scrollToOffset): Instead of dispatching the scroll
            event from this function, which is called during layout, schedule a 0-duration
            timer to dispatch the event after layout.
            (WebCore::RenderLayer::scrollRectToVisible):
            (WebCore::RenderLayer::updateScrollInfoAfterLayout):
            (WebCore::Marquee::start):
            * rendering/RenderLayer.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.h
+++ third_party/WebKit/WebCore/rendering/RenderLayer.h
@@ -378,6 +378,8 @@ private:
     void dirtyVisibleDescendantStatus();
     void updateVisibilityStatus();
 
+    void dispatchScrollEvent(Timer<RenderLayer>*);
+
 protected:   
     RenderObject* m_object;
 
@@ -453,6 +455,8 @@ protected:
     bool m_hasVisibleDescendant : 1;
 
     Marquee* m_marquee; // Used by layers with overflow:marquee
+    
+    Timer<RenderLayer> m_scrollEventTimer;
 };
 
 } // namespace WebCore
