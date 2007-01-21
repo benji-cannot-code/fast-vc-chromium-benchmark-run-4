0d3cc3f28c55c09665ca3e1142afda8ed275e249
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-21 23:42:37 +0000

    LayoutTests:
            Rolling out patch for http://bugs.webkit.org/show_bug.cgi?id=8360.
    
            * fast/layers/removed-by-scroll-handler-expected.checksum: Removed.
            * fast/layers/removed-by-scroll-handler-expected.png: Removed.
            * fast/layers/removed-by-scroll-handler-expected.txt: Removed.
            * fast/layers/removed-by-scroll-handler.html: Removed.
    
    WebCore:
    
            Rolling out patch for http://bugs.webkit.org/show_bug.cgi?id=8360.
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::RenderLayer):
            (WebCore::RenderLayer::scrollToOffset):
            (WebCore::RenderLayer::scrollRectToVisible):
            (WebCore::RenderLayer::updateScrollInfoAfterLayout):
            (WebCore::Marquee::start):
            * rendering/RenderLayer.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19016 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.h
+++ third_party/WebKit/WebCore/rendering/RenderLayer.h
@@ -378,8 +378,6 @@ private:
     void dirtyVisibleDescendantStatus();
     void updateVisibilityStatus();
 
-    void dispatchScrollEvent(Timer<RenderLayer>*);
-
 protected:   
     RenderObject* m_object;
 
@@ -455,8 +453,6 @@ protected:
     bool m_hasVisibleDescendant : 1;
 
     Marquee* m_marquee; // Used by layers with overflow:marquee
-    
-    Timer<RenderLayer> m_scrollEventTimer;
 };
 
 } // namespace WebCore
