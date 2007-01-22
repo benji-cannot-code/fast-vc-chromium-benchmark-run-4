ec90c82f204ba61472c5b6ddc762bb4a13dbf6ae
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-22 21:15:37 +0000

    Reviewed by Beth.
            - fix http://bugs.webkit.org/show_bug.cgi?id=12369
              REGRESSION (r19027): amazon.com home page crashes WebKit r19028 debug build
    
            I caused this a few hours ago. Oops!
    
            * rendering/RenderText.cpp: (WebCore::RenderText::setStyle):
            Compute whether the font changed before calling RenderObject::setStyle.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderText.cpp
+++ third_party/WebKit/WebCore/rendering/RenderText.cpp
@@ -79,6 +79,8 @@ void RenderText::setStyle(RenderStyle* newStyle)
     if (oldStyle == newStyle)
         return;
 
+    bool fontChanged = !oldStyle || oldStyle->font() != newStyle->font();
+
     ETextTransform oldTransform = oldStyle ? oldStyle->textTransform() : TTNONE;
     ETextSecurity oldSecurity = oldStyle ? oldStyle->textSecurity() : TSNONE;
 
@@ -88,7 +90,7 @@ void RenderText::setStyle(RenderStyle* newStyle)
         if (RefPtr<StringImpl> textToTransform = originalText())
             setText(textToTransform.release(), true);
 
-    if (!oldStyle || oldStyle->font() != newStyle->font())
+    if (fontChanged)
         updateMonospaceCharacterWidth();
 }
 
