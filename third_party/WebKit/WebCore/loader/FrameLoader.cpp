e853992cf869d1a6a44c13d3bed3e8917537655e
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-22 02:46:06 +0000

    Reviewed by Maciej.
            - fix <rdar://problem/4930503> REGRESSION: Page changes title when
              command-clicking on named anchor link (12299)
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::continueFragmentScrollAfterNavigationPolicy):
            Return if shouldContinue is false. This matches the other similar
            functions and prevents us from doing extra work in the "ignore" case.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19022 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -3140,10 +3140,13 @@ void FrameLoader::callContinueFragmentScrollAfterNavigationPolicy(void* argument
 
 void FrameLoader::continueFragmentScrollAfterNavigationPolicy(const ResourceRequest& request, bool shouldContinue)
 {
-    KURL URL = request.url();
-    
     bool isRedirect = m_quickRedirectComing;
     m_quickRedirectComing = false;
+
+    if (!shouldContinue)
+        return;
+
+    KURL URL = request.url();
     
     m_documentLoader->replaceRequestURLForAnchorScroll(URL);
     if (!isRedirect && !shouldTreatURLAsSameAsCurrent(URL)) {
