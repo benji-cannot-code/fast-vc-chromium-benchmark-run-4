b280613421612bb060deeedf68cd5c9783f8d778
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-23 09:15:55 +0000

    2007-01-23  Oliver Hunt  <oliver@apple.com>
            Reviewed by Adam.
    
            Qt build stubs for Drop logic
    
            * WebCoreSupport/DragClientQt.cpp: Added.
            (WebCore::DragClientQt::actionMaskForDrag):
            (WebCore::DragClientQt::willPerformDragDestinationAction):
            * WebCoreSupport/DragClientQt.h: Added.
            * WebKitPart/WebKitPart.cpp:
            (WebKitPart::initView):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19041 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitQt/WebKitPart/WebKitPart.cpp
+++ third_party/WebKit/WebKitQt/WebKitPart/WebKitPart.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameView.h"
 #include "ChromeClientQt.h"
 #include "ContextMenuClientQt.h"
+#include "DragClientQt.h"
 #include "EditorClientQt.h"
 #include "KURL.h"
 
@@ -108,7 +109,7 @@ void WebKitPart::initView(QWidget* parentWidget, GUIProfile prof)
     m_client = new WebKitPartClient(this);
  
     // Initialize WebCore in Qt platform mode...
-    Page* page = new Page(new ChromeClientQt(), new ContextMenuClientQt(), new EditorClientQt());
+    Page* page = new Page(new ChromeClientQt(), new ContextMenuClientQt(), new EditorClientQt(), new DragClientQt());
     Frame* frame = new FrameQt(page, 0, m_client);
 
     m_frame = frame;
