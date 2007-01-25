6a055dd93f2221b09365321da93d0b3a6d4e3e81
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-25 04:38:38 +0000

    JavaScriptCore:
            Fixed up #include order for style. No review necessary.
    
            * API/JSStringRef.cpp:
    
    WebCore:
    
            New forwarding header. No review necessary.
    
            * ForwardingHeaders/kjs/ustring.h: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSStringRef.cpp
+++ third_party/WebKit/JavaScriptCore/API/JSStringRef.cpp
@@ -26,9 +26,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include <wtf/Platform.h>
-#include "APICast.h"
 #include "JSStringRef.h"
 
+#include "APICast.h"
 #include <kjs/JSLock.h>
 #include <kjs/JSType.h>
 #include <kjs/internal.h>
--- third_party/WebKit/WebCore/bindings/js/JSDocumentCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDocumentCustom.cpp
@@ -33,3 +33,4 @@ void JSDocument::mark()
 }
     
 }
+
