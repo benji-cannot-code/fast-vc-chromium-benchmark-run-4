f4483907fbd0ed24eb57838ca499157e0474f5e6
aliceli1 <aliceli1@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-25 08:38:32 +0000

    LayoutTests:
            Reviewed by Maciej.
    
            added test for 
            <rdar://problem/4808039> REGRESSION: eBay ID check never completes
            also known as
            http://bugs.webkit.org/show_bug.cgi?id=11230
    
            * http/tests/misc/location-replace-crossdomain-expected.txt: Added.
            * http/tests/misc/location-replace-crossdomain.html: Added.
            * http/tests/misc/resources/failure.html: Added.
            * http/tests/misc/resources/success.html: Added.
    
    WebCore:
    
            Reviewed by Maciej.
    
            fixed <rdar://problem/4808039> REGRESSION: eBay ID check never completes
            Also known as http://bugs.webkit.org/show_bug.cgi?id=11230
    
            * bindings/js/kjs_window.cpp:
            It's not necessary to check safeScript when doing a location.replace, .reload, or .assign
            (KJS::Location::getOwnPropertySlot):
            (KJS::LocationFunc::callAsFunction):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/kjs_window.cpp
+++ third_party/WebKit/WebCore/bindings/js/kjs_window.cpp
@@ -2133,10 +2133,13 @@ bool Location::getOwnPropertySlot(ExecState *exec, const Identifier& propertyNam
     return false;
   
   const Window* window = Window::retrieveWindow(m_frame);
-  if (!window || !window->isSafeScript(exec)) {
+  
+  const HashEntry *entry = Lookup::findEntry(&LocationTable, propertyName);
+  if (!entry || (entry->value != Replace && entry->value != Reload && entry->value != Assign))
+    if (!window || !window->isSafeScript(exec)) {
       slot.setUndefined(this);
       return true;
-  }
+    }
 
   return getStaticPropertySlot<LocationFunc, Location, JSObject>(exec, &LocationTable, this, propertyName, slot);
 }
@@ -2230,7 +2233,7 @@ JSValue *LocationFunc::callAsFunction(ExecState *exec, JSObject *thisObj, const 
   if (frame) {
       
     Window* window = Window::retrieveWindow(frame);
-    if (!window->isSafeScript(exec) && id != Location::Replace)
+    if (id != Location::Replace && !window->isSafeScript(exec))
         return jsUndefined();
       
     switch (id) {
