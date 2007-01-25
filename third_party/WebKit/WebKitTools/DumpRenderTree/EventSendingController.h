764cdf153e8fec4c50a13181481f9578ee8a18ee
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-25 04:12:45 +0000

    Reviewed by Maciej.
            - made the deferral of mouse events until drag completes conditional
              it's needed for drag testing, and harmful for selection testing
    
            * DumpRenderTree/EventSendingController.h: Added inDragMode boolean.
            * DumpRenderTree/EventSendingController.m:
            (+[EventSendingController isSelectorExcludedFromWebScript:]): Added setDragMode:.
            (+[EventSendingController webScriptNameForSelector:]): Added name for setDragMode,
            and remove unneeded clearKillRing name.
            (-[EventSendingController init]): Initialize inDragMode to true.
            (-[EventSendingController dealloc]): Removed overzealous assertions -- we should
            not be asserting things that are dependent on the test content!
            (-[EventSendingController leapForward:]): Only queue events in drag mode.
            (-[EventSendingController setDragMode:]): Added.
            (-[EventSendingController mouseDown]): Removed overzealous assertion.
            (-[EventSendingController mouseUp]): Removed overzealous assertions.
            Only queue events in drag mode.
            (-[EventSendingController mouseMoveToX:Y:]): Only queue events in drag mode.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/EventSendingController.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/EventSendingController.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005, 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2005, 2006, 2007 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,11 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
  
 #import <Cocoa/Cocoa.h>
-
 #import <WebKit/WebKit.h>
 
-@interface EventSendingController : NSObject <DOMEventListener> {
+@interface EventSendingController : NSObject <DOMEventListener>
+{
     BOOL down;
+    BOOL inDragMode;
     int clickCount;
     NSTimeInterval lastClick;
     int eventNumber;
@@ -49,4 +50,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 extern NSPoint lastMousePosition;
-
--- third_party/WebKit/WebKitTools/DumpRenderTree/EventSendingController.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/EventSendingController.m
@@ -108,7 +108,8 @@ BOOL replayingSavedEvents;
             || aSelector == @selector(keyDown:withModifiers:)
             || aSelector == @selector(enableDOMUIEventLogging:)
             || aSelector == @selector(fireKeyboardEventsToElement:)
-            || aSelector == @selector(clearKillRing))
+            || aSelector == @selector(clearKillRing)
+            || aSelector == @selector(setDragMode:))
         return NO;
     return YES;
 }
@@ -125,17 +126,22 @@ BOOL replayingSavedEvents;
         return @"enableDOMUIEventLogging";
     if (aSelector == @selector(fireKeyboardEventsToElement:))
         return @"fireKeyboardEventsToElement";
-    if (aSelector == @selector(clearKillRing))
-        return @"clearKillRing";
+    if (aSelector == @selector(setDragMode:))
+        return @"setDragMode";
     return nil;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self)
+        inDragMode = YES;
+    return self;
 }
 
 - (void)dealloc
 {
-    assert(!down);
-    assert([savedMouseEvents count] == 0);
     [savedMouseEvents release];
-    savedMouseEvents = nil;
     [super dealloc];
 }
 
@@ -146,7 +152,7 @@ BOOL replayingSavedEvents;
 
 - (void)leapForward:(int)milliseconds
 {
-    if (down && !replayingSavedEvents) {
+    if (inDragMode && down && !replayingSavedEvents) {
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[EventSendingController instanceMethodSignatureForSelector:@selector(leapForward:)]];
         [invocation setTarget:self];
         [invocation setSelector:@selector(leapForward:)];
@@ -165,11 +171,13 @@ BOOL replayingSavedEvents;
     _NSNewKillRingSequence();
 }
 
+- (void)setDragMode:(BOOL)dragMode
+{
+    inDragMode = dragMode;
+}
+
 - (void)mouseDown
 {
-    assert(!down);
-    assert(!replayingSavedEvents);
-
     [[[frame frameView] documentView] layout];
     if ([self currentEventTime] - lastClick >= 1)
         clickCount = 1;
@@ -194,9 +202,7 @@ BOOL replayingSavedEvents;
 
 - (void)mouseUp
 {
-    assert(down);
-
-    if (!replayingSavedEvents) {
+    if (inDragMode && !replayingSavedEvents) {
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[EventSendingController instanceMethodSignatureForSelector:@selector(mouseUp)]];
         [invocation setTarget:self];
         [invocation setSelector:@selector(mouseUp)];
@@ -206,7 +212,6 @@ BOOL replayingSavedEvents;
 
         return;
     }
-
 
     [[[frame frameView] documentView] layout];
     NSEvent *event = [NSEvent mouseEventWithType:NSLeftMouseUp 
@@ -245,7 +250,7 @@ BOOL replayingSavedEvents;
 
 - (void)mouseMoveToX:(int)x Y:(int)y
 {
-    if (down && !replayingSavedEvents) {
+    if (inDragMode && down && !replayingSavedEvents) {
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[EventSendingController instanceMethodSignatureForSelector:@selector(mouseMoveToX:Y:)]];
         [invocation setTarget:self];
         [invocation setSelector:@selector(mouseMoveToX:Y:)];
