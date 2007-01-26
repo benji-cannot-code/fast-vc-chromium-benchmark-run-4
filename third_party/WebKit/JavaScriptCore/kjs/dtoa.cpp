ae360faa78cf1236607683960fa4b8f7641d747b
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-26 07:30:21 +0000

    Patch from Jan Kraemer.
            Reviewed by Maciej
    
            Fix for http://bugs.webkit.org/show_bug.cgi?id=12382
    
            Fix crash on architectures with 32 bit ints and
            64 bit longs (For example Linux on AMD64)
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19136 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/dtoa.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/dtoa.cpp
@@ -189,7 +189,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 #ifndef Long
-#define Long long
+#define Long int
 #endif
 #ifndef ULong
 typedef unsigned Long ULong;
