ef080752f42ca36c45a7f99cd84b42d56e2eefe6
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-27 01:35:06 +0000

    2007-01-26  Oliver Hunt  <oliver@apple.com>
            Build Fix
    
            * ksvg2/svg/ColorDistance.cpp:
            * ksvg2/svg/SVGAnimationElement.cpp:
            (WebCore::SVGAnimationElement::handleTimerEvent):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/ColorDistance.cpp
+++ third_party/WebKit/WebCore/ksvg2/svg/ColorDistance.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifdef SVG_SUPPORT
 #include "ColorDistance.h"
 #include "Color.h"
+#include <wtf/MathExtras.h>
 
 namespace WebCore {
 
