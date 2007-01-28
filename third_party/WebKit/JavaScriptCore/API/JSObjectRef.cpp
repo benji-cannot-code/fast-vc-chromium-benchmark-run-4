8d86e96f5531dc69453a8614bed13c12af47e2f4
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-28 02:58:47 +0000

    Reviewed by Beth Dakin.
            Added some missing JSLocks, which might fix <rdar://problem/4889707>.
    
            We need to lock whenever we might allocate memory because our FastMalloc
            implementation requires clients to register their threads, which we do
            through JSLock. 
            
            We also need to lock whenever modifying ref-counts because they're not 
            thread-safe.
    
            * API/JSObjectRef.cpp:
            (JSClassCreate): Allocates memory
            (JSClassRetain): Modifies a ref-count
            (JSClassRelease): Modifies a ref-count
            (JSPropertyNameArrayRetain): Modifies a ref-count
            (JSPropertyNameArrayRelease): Modifies a ref-count
            * API/JSStringRef.cpp:
            (JSStringRetain): Modifies a ref-count
            * API/JSValueRef.cpp:
            (JSValueIsInstanceOfConstructor): Might allocate memory if an exception
            is thrown.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSObjectRef.cpp
+++ third_party/WebKit/JavaScriptCore/API/JSObjectRef.cpp
@@ -45,6 +45,7 @@ using namespace KJS;
 
 JSClassRef JSClassCreate(JSClassDefinition* definition)
 {
+    JSLock lock;
     JSClassRef jsClass = (definition->attributes & kJSClassAttributeNoAutomaticPrototype)
         ? OpaqueJSClass::createNoAutomaticPrototype(definition)
         : OpaqueJSClass::create(definition);
@@ -54,12 +55,14 @@ JSClassRef JSClassCreate(JSClassDefinition* definition)
 
 JSClassRef JSClassRetain(JSClassRef jsClass)
 {
+    JSLock lock;
     ++jsClass->refCount;
     return jsClass;
 }
 
 void JSClassRelease(JSClassRef jsClass)
 {
+    JSLock lock;
     if (--jsClass->refCount == 0)
         delete jsClass;
 }
@@ -332,12 +335,14 @@ JSPropertyNameArrayRef JSObjectCopyPropertyNames(JSContextRef ctx, JSObjectRef o
 
 JSPropertyNameArrayRef JSPropertyNameArrayRetain(JSPropertyNameArrayRef array)
 {
+    JSLock lock;
     ++array->refCount;
     return array;
 }
 
 void JSPropertyNameArrayRelease(JSPropertyNameArrayRef array)
 {
+    JSLock lock;
     if (--array->refCount == 0)
         delete array;
 }
