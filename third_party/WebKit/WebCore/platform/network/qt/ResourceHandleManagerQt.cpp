74fbc659ec797357200764a2606d3c1fa794e956
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-29 06:28:40 +0000

    Fix redirects with the Qt resource implementation.
    git-svn-id: svn://svn.chromium.org/blink/trunk@19210 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/ResourceHandleManagerQt.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/ResourceHandleManagerQt.cpp
@@ -263,7 +263,7 @@ void ResourceHandleManager::receivedResponse(RequestQt* request)
 
 void ResourceHandleManager::receivedData(RequestQt* request, const QByteArray& data)
 {
-    if (request->cancelled)
+    if (request->cancelled || request->redirected)
         return;
     Q_ASSERT(pendingRequests.value(request->resource) == request);
 
