f05ae7acd2b45106ece9c048bbaf28a52ae8de0e
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-29 14:28:38 +0000

    2007-01-30  Mark Rowe  <mrowe@apple.com>
            Reviewed by Maciej.
    
            http://bugs.webkit.org/show_bug.cgi?id=12458
            Bug 12458: Crash in W3C-SVG-1.1/animate-elem-09-t.svg running layout tests under guard malloc
    
            * ksvg2/svg/SVGAnimationElement.cpp: 
            (WebCore::parseValues): ptr either points to a ';' or off the end of the buffer.  We're
            interested in what comes before the ';', so start there instead.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGAnimationElement.cpp
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGAnimationElement.cpp
@@ -150,7 +150,7 @@ static void parseValues(Vector<String>& values, const String& value)
             break;
         
         // walk backwards from the ; to ignore any whitespace
-        const UChar* valueEnd = ptr;
+        const UChar* valueEnd = ptr - 1;
         while (valueStart < valueEnd && isWhitespace(*valueEnd))
             valueEnd--;
         
