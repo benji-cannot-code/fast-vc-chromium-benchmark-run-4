3196600df6632c5680458f426623c764766d9880
zack <zack@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-29 13:37:48 +0000

    Take the scrollbars into considation when computing the contents size of the main frame
    git-svn-id: svn://svn.chromium.org/blink/trunk@19221 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/PlatformScrollBarQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/PlatformScrollBarQt.cpp
@@ -57,7 +57,7 @@ void PlatformScrollbar::setEnabled(bool e)
 
 void PlatformScrollbar::paint(GraphicsContext* ctxt, const IntRect& damageRect)
 {
-    Widget::paint(ctxt, damageRect);
+    //Widget::paint(ctxt, damageRect);
 }
 
 void PlatformScrollbar::updateThumbPosition()
