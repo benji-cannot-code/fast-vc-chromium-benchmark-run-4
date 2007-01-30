aa2c16296371cfdee00a175ef85e2041bd0db270
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-30 23:56:54 +0000

    Reviewed by Darin.
            <rdar://problem/4961953> Stop using NSString deprecated methods like initWithCString:
    
            * Misc/WebNSImageExtras.m:
            (-[NSImage _web_saveAndOpen]):
            * WebKit.xcodeproj/project.pbxproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc/WebNSImageExtras.m
+++ third_party/WebKit/WebKit/Misc/WebNSImageExtras.m
@@ -85,7 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         NSData *data = [self TIFFRepresentation];
         write(fd, [data bytes], [data length]);
         close(fd);
-        [[NSWorkspace sharedWorkspace] openFile:[NSString stringWithCString:path]];
+        [[NSWorkspace sharedWorkspace] openFile:[NSString stringWithUTF8String:path]];
     }
 }
 
