39e39b515e4b9a80545eeb3264dbb614eae6790b
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-31 03:31:04 +0000

    LayoutTests:
            Reviewed by Darin.
    
            http://bugs.webkit.org/show_bug.cgi?id=11882
            Need a way to regression test .webarchive output files
    
            Created webarchive tests that exercise various features of the WebArchive format.
    
            * webarchive/resources/apple.gif: Copied from fast/canvas/resources/apple.gif.
            * webarchive/resources/dump-webarchive.js: Added.
            * webarchive/resources/test-frame.html: Added.
            * webarchive/resources/test-style.css: Added.
            * webarchive/test-body-background-expected.txt: Added.
            * webarchive/test-body-background.html: Added.
            * webarchive/test-duplicate-resources-expected.txt: Added.
            * webarchive/test-duplicate-resources.html: Added.
            * webarchive/test-frameset-expected.txt: Added.
            * webarchive/test-frameset.html: Added.
            * webarchive/test-img-src-expected.txt: Added.
            * webarchive/test-img-src.html: Added.
            * webarchive/test-input-src-expected.txt: Added.
            * webarchive/test-input-src.html: Added.
            * webarchive/test-link-href-expected.txt: Added.
            * webarchive/test-link-href.html: Added.
            * webarchive/test-object-data-expected.txt: Added.
            * webarchive/test-object-data.html: Added.
            * webarchive/test-script-src-expected.txt: Added.
            * webarchive/test-script-src.html: Added.
            * webarchive/test-table-background-expected.txt: Added.
            * webarchive/test-table-background.html: Added.
            * webarchive/test-td-background-expected.txt: Added.
            * webarchive/test-td-background.html: Added.
    
    WebKitTools:
    
            Reviewed by Darin.
    
            http://bugs.webkit.org/show_bug.cgi?id=11882
            Need a way to regression test .webarchive output files
    
            Implement layoutTestController.dumpAsWebArchive() to test WebArchive format.
            Alters WebResourceResponse and WebResourceURL properties within WebArchive format
            to normalize URLs to remove path where WebKit was checked out.  Also converts
            WebDataResource properties from data to string if the corresponding
            WebResourceMIMEType property starts with "text/" or equals
            "application/x-javascript".
    
            * DumpRenderTree/DumpRenderTree.m:
            (convertWebResourceDataToString): Added.
            (normalizeWebResourceURL): Added.
            (normalizeWebResourceResponse): Added.
            (serializeWebArchiveToXML): Added.
            (dump):
            (+[LayoutTestController isSelectorExcludedFromWebScript:]):
            (-[LayoutTestController dumpAsWebArchive]): Added.
            (runTest):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005, 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2005, 2006, 2007 Apple, Inc.  All rights reserved.
  *           (C) 2007 Graham Dennis (graham.dennis@gmail.com)
  *
  * Redistribution and use in source and binary forms, with or without
@@ -121,6 +121,7 @@ static WebFrame *topLoadingFrame;     // !nil iff a load is in progress
 static BOOL waitToDump;     // TRUE if waitUntilDone() has been called, but notifyDone() has not yet been called
 
 static BOOL dumpAsText;
+static BOOL dumpAsWebArchive;
 static BOOL dumpSelectionRect;
 static BOOL dumpTitleChanges;
 static BOOL dumpBackForwardList;
@@ -506,6 +507,103 @@ static void dumpFrameScrollPosition(WebFrame *f)
     }
 }
 
+static void convertWebResourceDataToString(NSMutableDictionary *resource)
+{
+    NSString *mimeType = [resource objectForKey:@"WebResourceMIMEType"];
+    if ([mimeType hasPrefix:@"text/"] || [mimeType isEqualToString:@"application/x-javascript"]) {
+        NSData *data = [resource objectForKey:@"WebResourceData"];
+        NSString *dataAsString = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+        [resource setObject:dataAsString forKey:@"WebResourceData"];
+    }
+}
+
+static void normalizeWebResourceURL(NSMutableString *webResourceURL, NSString *oldURLBase)
+{
+    [webResourceURL replaceOccurrencesOfString:oldURLBase
+                                    withString:@"file://"
+                                       options:NSLiteralSearch
+                                         range:NSMakeRange(0, [webResourceURL length])];
+}
+
+static void normalizeWebResourceResponse(NSMutableDictionary *propertyList, NSString *oldURLBase)
+{
+    NSURLResponse *response = nil;
+    NSData *responseData = [propertyList objectForKey:@"WebResourceResponse"]; // WebResourceResponseKey in WebResource.m
+    if ([responseData isKindOfClass:[NSData class]]) {
+        // Decode NSURLResponse
+        NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc] initForReadingWithData:responseData];
+        response = [unarchiver decodeObjectForKey:@"WebResourceResponse"]; // WebResourceResponseKey in WebResource.m
+        [unarchiver finishDecoding];
+        [unarchiver release];
+
+        // Create replacement NSURLReponse
+        NSMutableString *URL = [[[NSMutableString alloc] initWithContentsOfURL:[response URL]] autorelease];
+        normalizeWebResourceURL(URL, oldURLBase);
+        NSURLResponse *newResponse = [[NSURLResponse alloc] initWithURL:[[[NSURL alloc] initWithString:URL] autorelease]
+                                                               MIMEType:[response MIMEType]
+                                                  expectedContentLength:[response expectedContentLength]
+                                                       textEncodingName:[response textEncodingName]];
+        [newResponse autorelease];
+
+        // Encode replacement NSURLResponse
+        NSMutableData *newResponseData = [[NSMutableData alloc] init];
+        NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc] initForWritingWithMutableData:newResponseData];
+        [archiver encodeObject:newResponse forKey:@"WebResourceResponse"];
+        [archiver finishEncoding];
+        [archiver release];
+        [propertyList setObject:newResponseData forKey:@"WebResourceResponse"]; // WebResourceResponseKey in WebResource.m
+        [newResponseData release];
+    }
+}
+
+static NSString *serializeWebArchiveToXML(WebArchive *webArchive)
+{
+    NSString *errorString;
+    NSMutableDictionary *propertyList = [NSPropertyListSerialization propertyListFromData:[webArchive data]
+                                                                         mutabilityOption:NSPropertyListMutableContainersAndLeaves
+                                                                                   format:NULL
+                                                                         errorDescription:&errorString];
+    if (!propertyList)
+        return errorString;
+
+    // Normalize WebResourceResponse and WebResourceURL values in plist for testing
+    NSString *cwdURL = [@"file://" stringByAppendingString:[[[NSFileManager defaultManager] currentDirectoryPath] stringByExpandingTildeInPath]];
+
+    NSMutableArray *resources = [NSMutableArray arrayWithCapacity:1];
+    [resources addObject:propertyList];
+
+    while ([resources count]) {
+        NSMutableDictionary *resourcePropertyList = [resources objectAtIndex:0];
+        [resources removeObjectAtIndex:0];
+
+        NSMutableDictionary *mainResource = [resourcePropertyList objectForKey:@"WebMainResource"];
+        normalizeWebResourceURL([mainResource objectForKey:@"WebResourceURL"], cwdURL);
+        convertWebResourceDataToString(mainResource);
+
+        // Add subframeArchives to list for processing
+        NSMutableArray *subframeArchives = [resourcePropertyList objectForKey:@"WebSubframeArchives"]; // WebSubframeArchivesKey in WebArchive.m
+        if (subframeArchives)
+            [resources addObjectsFromArray:subframeArchives];
+
+        NSMutableArray *subresources = [resourcePropertyList objectForKey:@"WebSubresources"]; // WebSubresourcesKey in WebArchive.m
+        NSEnumerator *enumerator = [subresources objectEnumerator];
+        NSMutableDictionary *subresourcePropertyList;
+        while ((subresourcePropertyList = [enumerator nextObject])) {
+            normalizeWebResourceURL([subresourcePropertyList objectForKey:@"WebResourceURL"], cwdURL);
+            normalizeWebResourceResponse(subresourcePropertyList, cwdURL);
+            convertWebResourceDataToString(subresourcePropertyList);
+        }
+    }
+
+    NSData *xmlData = [NSPropertyListSerialization dataFromPropertyList:propertyList
+                                                                 format:NSPropertyListXMLFormat_v1_0
+                                                       errorDescription:&errorString];
+    if (!xmlData)
+        return errorString;
+
+    return [[[NSMutableString alloc] initWithData:xmlData encoding:NSUTF8StringEncoding] autorelease];
+}
+
 static void dump(void)
 {
     NSString *result = nil;
@@ -514,6 +612,9 @@ static void dump(void)
         if (dumpAsText) {
             DOMElement *documentElement = [[frame DOMDocument] documentElement];
             result = [[(DOMElement *)documentElement innerText] stringByAppendingString:@"\n"];
+        } else if (dumpAsWebArchive) {
+            WebArchive *webArchive = [[frame DOMDocument] webArchive];
+            result = serializeWebArchiveToXML(webArchive);
         } else {
             bool isSVGW3CTest = ([currentTest rangeOfString:@"svg/W3C-SVG-1.1"].length);
             if (isSVGW3CTest)
@@ -522,12 +623,19 @@ static void dump(void)
                 [[frame webView] setFrameSize:NSMakeSize(maxViewWidth, maxViewHeight)];
             result = [frame renderTreeAsExternalRepresentation];
         }
-        
-        if (!result)
-            printf("ERROR: nil result from %s", dumpAsText ? "[documentElement innerText]" : "[frame renderTreeAsExternalRepresentation]");
-        else {
+
+        if (!result) {
+            const char *errorMessage;
+            if (dumpAsText)
+                errorMessage = "[documentElement innerText]";
+            else if (dumpAsWebArchive)
+                errorMessage = "[[frame DOMDocument] webArchive]";
+            else
+                errorMessage = "[frame renderTreeAsExternalRepresentation]";
+            printf("ERROR: nil result from %s", errorMessage);
+        } else {
             fputs([result UTF8String], stdout);
-            if (!dumpAsText)
+            if (!dumpAsText && !dumpAsWebArchive)
                 dumpFrameScrollPosition(frame);
         }
 
@@ -568,7 +676,7 @@ static void dump(void)
     }
     
     if (dumpPixels) {
-        if (!dumpAsText) {
+        if (!dumpAsText && !dumpAsWebArchive) {
             // grab a bitmap from the view
             WebView* view = [frame webView];
             NSSize webViewSize = [view frame].size;
@@ -769,6 +877,7 @@ static void dump(void)
     if (aSelector == @selector(waitUntilDone)
             || aSelector == @selector(notifyDone)
             || aSelector == @selector(dumpAsText)
+            || aSelector == @selector(dumpAsWebArchive)
             || aSelector == @selector(dumpTitleChanges)
             || aSelector == @selector(dumpBackForwardList)
             || aSelector == @selector(dumpChildFrameScrollPositions)
@@ -853,6 +962,11 @@ static void dump(void)
 - (void)dumpAsText
 {
     dumpAsText = YES;
+}
+
+- (void)dumpAsWebArchive
+{
+    dumpAsWebArchive = YES;
 }
 
 - (void)dumpSelectionRect
@@ -1051,6 +1165,7 @@ static void runTest(const char *pathOrURL)
     topLoadingFrame = nil;
     waitToDump = NO;
     dumpAsText = NO;
+    dumpAsWebArchive = NO;
     dumpChildFrameScrollPositions = NO;
     shouldDumpEditingCallbacks = NO;
     dumpSelectionRect = NO;
