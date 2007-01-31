bca62dccd995ca3748bb662c2d89157c97619817
aliceli1 <aliceli1@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-01-31 21:15:57 +0000

    Reviewed by Tim Hatcher.
            Turning an accidental API change to an SPI change
    
            * WebView/WebEditingDelegate.h:
            * WebView/WebEditingDelegatePrivate.h:
            move some declarations into private header.  
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19308 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebEditingDelegatePrivate.h
+++ third_party/WebKit/WebKit/WebView/WebEditingDelegatePrivate.h
@@ -31,4 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface NSObject (WebViewEditingDelegatePrivate)
 - (BOOL)webView:(WebView *)webView shouldShowDeleteInterfaceForElement:(DOMHTMLElement *)element;
+- (void)webView:(WebView *)webView didWriteSelectionToPasteboard:(NSPasteboard *)pasteboard;
+- (void)webView:(WebView *)webView didSetSelectionTypesForPasteboard:(NSPasteboard *)pasteboard;
 @end
