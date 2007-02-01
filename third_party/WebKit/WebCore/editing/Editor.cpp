47d4656ae70a05fdb33d1213709e75a66a1100ee
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-01 01:55:30 +0000

    Reviewed by Adam.
            Fix for http://bugs.webkit.org/show_bug.cgi?id=12516
            REGRESSION (r19313): All keyboard navigation has stopped working
    
            For non-editable selections, handleKeyPress doesn't need to call _interceptEditingKeyEvent. 
    
            * editing/Editor.cpp:
            (WebCore::selectionForEvent): Moved this to the top of the file.
            (WebCore::Editor::handleKeyPress): Do the equivalent of the _canEdit check that used to be in WebKit WebEditorClient::handleKeyPress.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19319 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/Editor.cpp
+++ third_party/WebKit/WebCore/editing/Editor.cpp
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLTextAreaElement.h"
 #include "HitTestResult.h"
 #include "IndentOutdentCommand.h"
+#include "KeyboardEvent.h"
 #include "KURL.h"
 #include "Page.h"
 #include "Pasteboard.h"
@@ -69,6 +70,30 @@ namespace WebCore {
 using namespace EventNames;
 using namespace HTMLNames;
 
+// When an event handler has moved the selection outside of a text control
+// we should use the target control's selection for this editing operation.
+static Selection selectionForEvent(Frame* frame, Event* event)
+{
+    Page* page = frame->page();
+    if (!page)
+        return Selection();
+    Selection selection = page->selection();
+    if (!event)
+        return selection;
+    Node* target = event->target()->toNode();
+    Node* selectionStart = selection.start().node();
+    
+    // If the target is a text control, and the current selection is outside of its shadow tree,
+    // then use the saved selection for that text control.
+    if (target && (!selectionStart || target->shadowAncestorNode() != selectionStart->shadowAncestorNode())) {
+        if (target->hasTagName(inputTag) && static_cast<HTMLInputElement*>(target)->isTextField())
+            return static_cast<HTMLInputElement*>(target)->selection();
+        if (target->hasTagName(textareaTag))
+            return static_cast<HTMLTextAreaElement*>(target)->selection();
+    }
+    return selection;
+}
+
 EditorClient* Editor::client() const
 {
     if (Page* page = m_frame->page())
@@ -78,8 +103,11 @@ EditorClient* Editor::client() const
 
 void Editor::handleKeyPress(KeyboardEvent* event)
 {
-    if (EditorClient* c = client())
-        c->handleKeyPress(event);
+    if (EditorClient* c = client()) {
+        Selection selection = selectionForEvent(m_frame, event);
+        if (selection.isCaretOrRange() && selection.isContentEditable())
+            c->handleKeyPress(event);
+    }
 }
 
 bool Editor::canEdit() const
@@ -1180,30 +1208,6 @@ bool Editor::execCommand(const AtomicString& command)
     }
     
     return handled;
-}
-
-// When an event handler has moved the selection outside of a text control
-// we should use the target control's selection for this editing operation.
-static Selection selectionForEvent(Frame* frame, Event* event)
-{
-    Page* page = frame->page();
-    if (!page)
-        return Selection();
-    Selection selection = page->selection();
-    if (!event)
-        return selection;
-    Node* target = event->target()->toNode();
-    Node* selectionStart = selection.start().node();
-    
-    // If the target is a text control, and the current selection is outside of its shadow tree,
-    // then use the saved selection for that text control.
-    if (target && (!selectionStart || target->shadowAncestorNode() != selectionStart->shadowAncestorNode())) {
-        if (target->hasTagName(inputTag) && static_cast<HTMLInputElement*>(target)->isTextField())
-            return static_cast<HTMLInputElement*>(target)->selection();
-        if (target->hasTagName(textareaTag))
-            return static_cast<HTMLTextAreaElement*>(target)->selection();
-    }
-    return selection;
 }
 
 bool Editor::insertText(const String& text, bool selectInsertedText, Event* triggeringEvent)
