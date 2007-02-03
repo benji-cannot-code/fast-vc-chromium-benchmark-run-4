e0ee1c93d74bb49aa337eb7834cebdeaf8ba3022
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-03 20:22:06 +0000

    Reviewed by Darin.
            http://bugs.webkit.org/show_bug.cgi?id=12485
            XPath id() function doesn't work as root in paths
    
            Test: fast/xpath/id-path.html
    
            * xml/XPathPath.cpp:
            (WebCore::XPath::Path::doEvaluate): Implemented.
            (WebCore::XPath::LocationPath::evaluate): A new overload that takes an initial NodeVector.
            (WebCore::XPath::LocationPath::doEvaluate): Moved most of the logic to the new evaluate().
    
            * xml/XPathPath.h: Added a new evaluate() overload and a using declaration to keep the
            generic one visible.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/xml/XPathPath.h
+++ third_party/WebKit/WebCore/xml/XPathPath.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifdef XPATH_SUPPORT
 
 #include "XPathExpressionNode.h"
+#include "XPathUtil.h"
 
 int xpathyyparse(void*);
 
@@ -57,6 +58,9 @@ namespace WebCore {
         public:
             LocationPath();
             virtual ~LocationPath();
+
+            using Expression::evaluate;
+            Value evaluate(const NodeVector& startNodes) const;
 
             void optimize();
 
