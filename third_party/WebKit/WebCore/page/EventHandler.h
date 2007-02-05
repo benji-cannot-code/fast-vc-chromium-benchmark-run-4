61b8a6bda9e34dc058352208baa770cf3401c196
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-05 02:37:02 +0000

    2007-02-04  Oliver Hunt  <oliver@apple.com>
            Reviewed by Adam.
    
            Tidying up EventHandler, and moving more of
            EventHandlerMac.mm to EventHandler.cpp
    
            Also added EventHandler::eventLoopHandleMouse[Dragged,Up]
            these are basically helper functions to allow correct event
            handling on MacOS.        
    
            * page/EventHandler.cpp:
            (WebCore::EventHandler::handleMouseUp):
            (WebCore::EventHandler::dragSourceMovedTo):
            (WebCore::EventHandler::dragSourceEndedAt):
            (WebCore::EventHandler::dispatchDragSrcEvent):
               Moved from EventHandlerMac to EventHandler
    
            * page/EventHandler.h:
            (WebCore::EventHandler::eventLoopHandleMouseUp):
            (WebCore::EventHandler::eventLoopHandleMouseDragged):
              Null implementations of two Mac helper functions
            * page/mac/EventHandlerMac.mm:
            (WebCore::EventHandler::eventLoopHandleMouseDragged):           
            (WebCore::EventHandler::handleDrag):
              Much tidying, reduced nesting, made less dependent on NS types   
            (WebCore::EventHandler::eventLoopHandleMouseDragged):
            (WebCore::EventHandler::eventLoopHandleMouseUp):  
              Helper functions to let us process events properly on MacOS
    
            * page/mac/WebCoreFrameBridge.mm:
            (-[WebCoreFrameBridge dragSourceEndedAt:operation:]):  
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19396 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/EventHandler.h
+++ third_party/WebKit/WebCore/page/EventHandler.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EventHandler_h
 #define EventHandler_h
 
+#include "DragActions.h"
 #include "PlatformMouseEvent.h"
 #include "ScrollTypes.h"
 #include "Timer.h"
@@ -65,7 +66,7 @@ class RenderObject;
 class RenderWidget;
 class VisiblePosition;
 class Widget;
-
+    
 struct HitTestRequest;
 
 extern const int LinkDragHysteresis;
@@ -143,7 +144,22 @@ public:
     void defaultKeyboardEventHandler(KeyboardEvent*);
     
     bool eventMayStartDrag(const PlatformMouseEvent&) const;
-
+    
+    void dragSourceMovedTo(const PlatformMouseEvent&);
+    void dragSourceEndedAt(const PlatformMouseEvent&, DragOperation);
+    
+    //The following methods are needed to allow correct event modelling on Mac
+    //they are called at the beginning of handleMouseUp and handleDrag.  
+    //If they return true it indicates that they have consumed the event
+#if PLATFORM(MAC)
+    bool eventLoopHandleMouseUp(const MouseEventWithHitTestResults&);
+    bool eventLoopHandleMouseDragged(const MouseEventWithHitTestResults&);
+#else
+    bool eventLoopHandleMouseUp(const MouseEventWithHitTestResults&) { return false; }
+    bool eventLoopHandleMouseDragged(const MouseEventWithHitTestResults&) { return false; }    
+#endif
+    
+    
 #if PLATFORM(MAC)
     PassRefPtr<KeyboardEvent> currentKeyboardEvent() const;
 
@@ -158,9 +174,6 @@ public:
     void sendFakeEventsAfterWidgetTracking(NSEvent* initiatingEvent);
 
     void setActivationEventNumber(int num) { m_activationEventNumber = num; }
-
-    void dragSourceMovedTo(const PlatformMouseEvent&);
-    void dragSourceEndedAt(const PlatformMouseEvent&, NSDragOperation);
 
     NSEvent *currentNSEvent();
 #endif
