58603d9866248411c10ab51318ccfa63d90ffff8
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-06 20:26:33 +0000

    2007-02-06  Mitz Pettel  <mitz@webkit.org>
            Reviewed by Darin.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=12518
              Betsson.com crashes browser
    
            Test: fast/layers/generated-layer-scrollbar-crash.html
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::createScrollbar): Changed element()->document() to
            document() to work with anonymous objects.
            (WebCore::RenderLayer::updateOverflowStatus): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19435 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayer.cpp
@@ -988,7 +988,7 @@ PassRefPtr<Scrollbar> RenderLayer::createScrollbar(ScrollbarOrientation orientat
 {
     if (Scrollbar::hasPlatformScrollbars()) {
         RefPtr<PlatformScrollbar> widget = new PlatformScrollbar(this, orientation, RegularScrollbar);
-        m_object->element()->document()->view()->addChild(widget.get());
+        m_object->document()->view()->addChild(widget.get());
         return widget.release();
     }
     
@@ -1162,7 +1162,7 @@ void RenderLayer::updateOverflowStatus(bool horizontalOverflow, bool verticalOve
         m_horizontalOverflow = horizontalOverflow;
         m_verticalOverflow = verticalOverflow;
         
-        if (FrameView* frameView = m_object->element()->document()->view())
+        if (FrameView* frameView = m_object->document()->view())
             frameView->scheduleEvent(new OverflowEvent(horizontalOverflowChanged, horizontalOverflow, verticalOverflowChanged, verticalOverflow),
             EventTargetNodeCast(m_object->element()), true);
     }
