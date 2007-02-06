c3637a37f4a77d2493ffece65879289f8d064dbd
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-06 22:17:20 +0000

    LayoutTests:
            Reviewed by john
    
            <rdar://problem/4959067>
            Bullets get erased or moved down to another line
            <rdar://problem/4912529>
            Seed: Bulleted list in Note in Mail.app does not work with Japanese input mode
    
            * editing/inserting/4959067-expected.checksum: Added.
            * editing/inserting/4959067-expected.png: Added.
            * editing/inserting/4959067-expected.txt: Added.
            * editing/inserting/4959067.html: Added.
    
    WebCore:
    
            Reviewed by john
    
            <rdar://problem/4959067>
            Bullets get erased or moved down to another line
            <rdar://problem/4912529>
            Seed: Bulleted list in Note in Mail.app does not work with Japanese input mode
            
            Do not expand the selection to include fully selected 
            special elements when insering text over a selection.
    
            * editing/CompositeEditCommand.cpp:
            (WebCore::CompositeEditCommand::deleteSelection): Pass
            expandForSpecialElements through to DeleteSelectionCommand's constructor.
            (WebCore::CompositeEditCommand::moveParagraphs): Use the 
            convenience function.
            * editing/CompositeEditCommand.h:
            * editing/DeleteSelectionCommand.h:
            * editing/InsertTextCommand.cpp:
            (WebCore::InsertTextCommand::input): Do not expand the selection
            to be deleted for special elements.
            * editing/ReplaceSelectionCommand.cpp:
            (WebCore::ReplaceSelectionCommand::doApply): Added a FIXME about 
            when we should expand the selection.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/editing/htmlediting.cpp
@@ -305,7 +305,7 @@ Position rangeCompliantEquivalent(const Position& pos)
     Node *node = pos.node();
     
     if (pos.offset() <= 0) {
-        if (node->parentNode() && (node->hasTagName(brTag) || editingIgnoresContent(node)))
+        if (node->parentNode() && (node->hasTagName(brTag) || editingIgnoresContent(node)) || isTableElement(node))
             return positionBeforeNode(node);
         return Position(node, 0);
     }
@@ -328,6 +328,9 @@ Position rangeCompliantEquivalent(const Position& pos)
         ASSERT_NOT_REACHED();
         return node->parentNode() ? positionBeforeNode(node) : Position(node, 0);
     }
+    
+    if (pos.offset() == maxCompliantOffset && (editingIgnoresContent(node) || isTableElement(node)))
+        return positionAfterNode(node);
     
     return Position(pos);
 }
