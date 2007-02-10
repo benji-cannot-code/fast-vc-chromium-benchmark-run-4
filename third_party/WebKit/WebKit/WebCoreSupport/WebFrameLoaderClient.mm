57abcfb4f881d2dbdd037ef20260a38c5b39f479
kdecker <kdecker@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-10 00:58:29 +0000

    Reviewed by Darin & Maciej.
            Fixed: <rdar://problem/4930688> REGRESSION: missing images when reloading webarchives (11962)
            
            * WebCoreSupport/WebFrameLoaderClient.mm:
            (WebFrameLoaderClient::canUseArchivedResource): The bug here is that because a reload sets a cache
            policy of NSURLRequestReloadIgnoringCacheData (rightfully so), this method was refusing to load
            subresources in WebArchives. It's OK to use archive subresources for the NSURLRequestReloadIgnoringCacheData
            cache policy because we're not worried about the actual contents of a WebArchive changing on disk. 
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19549 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport/WebFrameLoaderClient.mm
+++ third_party/WebKit/WebKit/WebCoreSupport/WebFrameLoaderClient.mm
@@ -926,8 +926,6 @@ bool WebFrameLoaderClient::canUseArchivedResource(NSURLRequest *request) const
         return true;
     if (policy == NSURLRequestReturnCacheDataDontLoad)
         return true;
-    if (policy == NSURLRequestReloadIgnoringCacheData)
-        return false;
     if ([request valueForHTTPHeaderField:@"must-revalidate"] != nil)
         return false;
     if ([request valueForHTTPHeaderField:@"proxy-revalidate"] != nil)
