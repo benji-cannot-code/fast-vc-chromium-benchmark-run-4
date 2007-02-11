c2d2f453b3c73de2cba2c2bde458619666bdb9d6
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-11 01:01:01 +0000

    WebCore:
            Reviewed by Adam.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=11621
              REGRESSION (Native slider): slider thumb not updated when the mouse is dragged/released out of range
    
            No automated test case because dumping the render tree updates layer positions anyway.
    
            * manual-tests/slider-thumb-tracking.html: Added.
            * rendering/RenderSlider.cpp:
            (WebCore::RenderSlider::setCurrentPosition): Added call to updateLayerPosition()
            for the thumb's layer.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19556 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderSlider.cpp
+++ third_party/WebKit/WebCore/rendering/RenderSlider.cpp
@@ -366,6 +366,7 @@ void RenderSlider::setCurrentPosition(int pos)
     else
         m_thumb->renderer()->style()->setLeft(Length(pos, Fixed));
 
+    m_thumb->renderer()->layer()->updateLayerPosition();
     repaint();
     m_thumb->renderer()->repaint();
 }
