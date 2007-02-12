7994c8a5fce83b86281f100735efd22696eafc6f
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-12 07:37:07 +0000

    Reviewed by Maciej
            First in what will be a series of HistoryItem enhancements to help debugging
    
            * history/HistoryItem.cpp:
            (WebCore::HistoryItem::showTree):
            (WebCore::HistoryItem::showTreeWithIndent):
            (showTree): Outside of WebCore namespace, and extern "C" - to make even the 
              DWARF debugger able to find it...  *sigh* 
            * history/HistoryItem.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/history/HistoryItem.h
+++ third_party/WebKit/WebCore/history/HistoryItem.h
@@ -148,7 +148,8 @@ public:
     void releasePageCache();  
     
 #ifndef NDEBUG
-    void print() const;
+    int showTree() const;
+    int showTreeWithIndent(unsigned indentLevel) const;
 #endif
 
 private:
@@ -190,5 +191,10 @@ private:
 }; //class HistoryItem
 
 } //namespace WebCore
+
+#ifndef NDEBUG
+// Outside the WebCore namespace for ease of invocation from gdb.
+extern "C" int showTree(const WebCore::HistoryItem*);
+#endif
 
 #endif // HISTORYITEM_H
