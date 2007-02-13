896798230872dce6aebcf7b1f5cded29b8eb9984
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-13 08:59:33 +0000

    Reviewed by Maciej and Adam
            Fixes <rdar://problem/4985497> - Crash when opening a new window to "Same Page"
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::loadItem): Null check documentLoader() since if we're a brand new 
              WebView, cloning the history of another view, we have no documentLoader()
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19597 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -3727,7 +3727,9 @@ void FrameLoader::loadItem(HistoryItem* item, FrameLoadType loadType)
 {
     KURL itemURL = item->url();
     KURL itemOriginalURL = item->originalURL();
-    KURL currentURL = documentLoader()->URL();
+    KURL currentURL;
+    if (documentLoader())
+        currentURL = documentLoader()->URL();
     RefPtr<FormData> formData = item->formData();
 
     // Are we navigating to an anchor within the page?
