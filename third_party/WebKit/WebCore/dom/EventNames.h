2faeb0ff761cf1620a1b124c7ce3582dc26820c8
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-14 01:39:11 +0000

    Rubber stamped by Maciej.
            * dom/EventNames.h: Resort names. Removed unused names.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19616 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/EventNames.h
+++ third_party/WebKit/WebCore/dom/EventNames.h
@@ -35,6 +35,7 @@ namespace WebCore { namespace EventNames {
     macro(beforecopy) \
     macro(beforecut) \
     macro(beforepaste) \
+    macro(beforeunload) \
     macro(blur) \
     macro(change) \
     macro(click) \
@@ -63,7 +64,6 @@ namespace WebCore { namespace EventNames {
     macro(mouseup) \
     macro(mousewheel) \
     macro(overflowchanged) \
-    macro(beforeunload) \
     macro(paste) \
     macro(readystatechange) \
     macro(reset) \
@@ -91,9 +91,7 @@ namespace WebCore { namespace EventNames {
     macro(khtmlBeforeTextInserted) \
     macro(khtmlDragdrop) \
     macro(khtmlEditableContentChanged) \
-    macro(khtmlHorizontalmousewheel) \
     macro(khtmlMove) \
-    macro(khtmlOrigclickMouseup) \
     \
 // end of DOM_EVENT_NAMES_FOR_EACH
 
