afa2ca7dbe92f6ba49c8b7ad59cea66b3d1b9e57
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-14 23:42:44 +0000

    Reviewed by Darin.
            Add new canCompareWithMemcmp vector trait and use it to determine whether
            operator== can use memcmp.
            
            * wtf/Vector.h:
            (WTF::):
            (WTF::VectorTypeOperations::compare):
            (WTF::operator==):
            * wtf/VectorTraits.h:
            (WTF::):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19631 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/VectorTraits.h
+++ third_party/WebKit/JavaScriptCore/wtf/VectorTraits.h
@@ -61,6 +61,7 @@ namespace WTF {
         static const bool canMoveWithMemcpy = false;
         static const bool canCopyWithMemcpy = false;
         static const bool canFillWithMemset = false;
+        static const bool canCompareWithMemcmp = false;
     };
 
     template<typename T>
@@ -72,6 +73,7 @@ namespace WTF {
         static const bool canMoveWithMemcpy = true;
         static const bool canCopyWithMemcpy = true;
         static const bool canFillWithMemset = sizeof(T) == sizeof(char);
+        static const bool canCompareWithMemcmp = true;
     };
 
     template<typename T>
@@ -85,6 +87,7 @@ namespace WTF {
         static const bool canMoveWithMemcpy = true;
         static const bool canCopyWithMemcpy = false;
         static const bool canFillWithMemset = false;
+        static const bool canCompareWithMemcmp = true;
     };
 
     // we know RefPtr is simple enough that initializing to 0 and moving with memcpy
@@ -104,6 +107,7 @@ namespace WTF {
         static const bool canMoveWithMemcpy = FirstTraits::canMoveWithMemcpy && SecondTraits::canMoveWithMemcpy;
         static const bool canCopyWithMemcpy = FirstTraits::canCopyWithMemcpy && SecondTraits::canCopyWithMemcpy;
         static const bool canFillWithMemset = false;
+        static const bool canCompareWithMemcmp = FirstTraits::canCompareWithMemcmp && SecondTraits::canCompareWithMemcmp;
     };
 
 } // namespace WTF
