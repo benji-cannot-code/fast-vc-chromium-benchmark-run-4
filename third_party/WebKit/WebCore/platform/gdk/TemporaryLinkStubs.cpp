cfe0b501b6511cab00858a341dabc5189ade5229
kjk <kjk@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-17 06:33:26 +0000

    Reviewed by Adam Roben.
            Move Path implementation from TemporaryLinkStubs.cpp to
            its own file.
            Implement setLineCap, setLineJoin and setMiterLimit for
            cairo's GraphicsContext.
    
            * WebCoreSources.bkl:
            * platform/gdk/TemporaryLinkStubs.cpp:
            * platform/graphics/cairo/GraphicsContextCairo.cpp:
            (WebCore::GraphicsContext::setPlatformFillColor):
            (WebCore::GraphicsContext::setPlatformStrokeColor):
            (WebCore::GraphicsContext::setLineCap):
            (WebCore::GraphicsContext::setLineJoin):
            (WebCore::GraphicsContext::setMiterLimit):
            * platform/graphics/cairo/PathCairo.cpp: Added.
            (WebCore::Path::Path):
            (WebCore::Path::~Path):
            (WebCore::Path::contains):
            (WebCore::Path::translate):
            (WebCore::Path::boundingRect):
            (WebCore::Path::operator=):
            (WebCore::Path::clear):
            (WebCore::Path::moveTo):
            (WebCore::Path::addLineTo):
            (WebCore::Path::addQuadCurveTo):
            (WebCore::Path::addBezierCurveTo):
            (WebCore::Path::addArcTo):
            (WebCore::Path::closeSubpath):
            (WebCore::Path::addArc):
            (WebCore::Path::addRect):
            (WebCore::Path::addEllipse):
            (WebCore::Path::transform):
            (WebCore::Path::apply):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19675 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gdk/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/gdk/TemporaryLinkStubs.cpp
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Node.h"
 #include "PageCache.h"
 #include "Pasteboard.h"
-#include "Path.h"
 #include "PlatformMouseEvent.h"
 #include "PlatformScrollBar.h"
 #include "PlugInInfoStore.h"
@@ -230,26 +229,6 @@ int ScrollView::updateScrollInfo(short type, int current, int max, int pageSize)
 IntPoint ScrollView::windowToContents(const IntPoint& point) const { return point; }
 IntPoint ScrollView::contentsToWindow(const IntPoint& point) const { return point; }
 PlatformScrollbar* ScrollView::scrollbarUnderMouse(const PlatformMouseEvent& mouseEvent) { return 0; }
-
-Path::Path(){ }
-Path::~Path(){ }
-Path::Path(const Path&){ }
-bool Path::contains(const FloatPoint&, WindRule rule) const{ return false; }
-void Path::translate(const FloatSize&){ }
-FloatRect Path::boundingRect() const { return FloatRect(); }
-Path& Path::operator=(const Path&){ return * this; }
-void Path::clear() { }
-void Path::moveTo(const FloatPoint&) { }
-void Path::addLineTo(const FloatPoint&) { }
-void Path::addQuadCurveTo(const FloatPoint&, const FloatPoint&) { }
-void Path::addBezierCurveTo(const FloatPoint&, const FloatPoint&, const FloatPoint&) { }
-void Path::addArcTo(const FloatPoint&, const FloatPoint&, float) { }
-void Path::closeSubpath() { }
-void Path::addArc(const FloatPoint&, float, float, float, bool) { }
-void Path::addRect(const FloatRect&) { }
-void Path::addEllipse(const FloatRect&) { }
-void Path::transform(const AffineTransform& transform) {}
-void Path::apply(void* info, PathApplierFunction) const {}
 
 PlatformScrollbar::PlatformScrollbar(ScrollbarClient* client, ScrollbarOrientation orientation, ScrollbarControlSize controlSize) : Scrollbar(client, orientation, controlSize) { notImplemented(); }
 PlatformScrollbar::~PlatformScrollbar() { notImplemented(); }
