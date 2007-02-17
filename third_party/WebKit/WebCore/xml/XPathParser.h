a145e1e69119da3366d9de8f58984918e63d5755
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-17 22:09:10 +0000

    LayoutTests:
            Reviewed by Maciej.
    
            http://bugs.webkit.org/show_bug.cgi?id=12733
            XPath namespace is attached to a wrong step
    
            * fast/xpath/namespace-vs-predicate-expected.txt: Added.
            * fast/xpath/namespace-vs-predicate.xhtml: Added.
    
    WebCore:
    
            Reviewed by Maciej.
    
            http://bugs.webkit.org/show_bug.cgi?id=12733
            XPath namespace is attached to a wrong step
    
            * xml/XPathGrammar.y: NAMETEST is different from other NodeTests in that its
            resulting type is an expanded name, rather than a string. It is not really possible 
            to store the namespace inside Parser, because it gets used in wrong steps then.
    
            * xml/XPathParser.cpp:
            (WebCore::XPath::Parser::expandQName):
            * xml/XPathParser.h:
            Moved code that expands a QName from XPathGrammar.y. Removed m_currentNamespaceURI.
    
            * xml/XPathStep.cpp:
            (WebCore::XPath::Step::Step):
            * xml/XPathStep.h:
            Added a constructor that takes a namespace.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/xml/XPathParser.h
+++ third_party/WebKit/WebCore/xml/XPathParser.h
@@ -64,6 +64,8 @@ namespace WebCore {
             Parser();
 
             XPathNSResolver* resolver() const { return m_resolver.get(); }
+            bool expandQName(const String& qName, String& localName, String& namespaceURI);
+
             Expression* parseStatement(const String& statement, PassRefPtr<XPathNSResolver>, ExceptionCode&);
 
             static Parser* current() { return currentParser; }
@@ -72,7 +74,6 @@ namespace WebCore {
 
             Expression* m_topExpr;
             bool m_gotNamespaceError;
-            String m_currentNamespaceURI;
 
             void registerParseNode(ParseNode*);
             void unregisterParseNode(ParseNode*);
--- third_party/WebKit/WebCore/xml/XPathStep.cpp
+++ third_party/WebKit/WebCore/xml/XPathStep.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright 2005 Frerich Raabe <raabe@kde.org>
  * Copyright (C) 2006 Apple Computer, Inc.
+ * Copyright (C) 2007 Alexey Proskuryakov <ap@webkit.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,12 +40,18 @@ namespace WebCore {
 namespace XPath {
 
 Step::Step(Axis axis, const String& nodeTest, const Vector<Predicate*>& predicates)
-    : m_axis(axis), m_nodeTest(nodeTest), m_predicates(predicates)
+    : m_axis(axis)
+    , m_nodeTest(nodeTest)
+    , m_predicates(predicates)
 {
-    Parser* parser = Parser::current();
-    ASSERT(parser);
-    m_namespaceURI = parser->m_currentNamespaceURI;
-    parser->m_currentNamespaceURI = String();
+}
+
+Step::Step(Axis axis, const String& nodeTest, const String& namespaceURI, const Vector<Predicate*>& predicates)
+    : m_axis(axis)
+    , m_nodeTest(nodeTest)
+    , m_namespaceURI(namespaceURI)
+    , m_predicates(predicates)
+{
 }
 
 Step::~Step()
