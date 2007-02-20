4d79ad2c42ebef1fb5c81301aa427f41f17e711a
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-20 20:41:54 +0000

    Reviewed by John.
            * Misc/WebKitNSStringExtras.h: Added _webkit_userVisibleBundleVersionFromFullVersion.
            * Misc/WebKitNSStringExtras.m:
            (-[NSString _webkit_userVisibleBundleVersionFromFullVersion]): If the version is 4 digits long or longer,
            then the first digit represents the version of the OS. Our user agent string should not
            include this first digit, so strip it off and report the rest as the version.
            * WebView/WebView.mm:
            (-[WebView _computeUserAgent]): Call _webkit_userVisibleBundleVersionFromFullVersion on the CFBundleVersion.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19739 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc/WebKitNSStringExtras.h
+++ third_party/WebKit/WebKit/Misc/WebKitNSStringExtras.h
@@ -55,4 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSString *)_webkit_stringByCollapsingWhitespaceCharacters;
 - (NSString *)_webkit_fixedCarbonPOSIXPath;
 
+- (NSString *)_webkit_userVisibleBundleVersionFromFullVersion;
+
 @end
