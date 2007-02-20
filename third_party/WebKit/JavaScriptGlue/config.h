71cd97ba92cc9e5f6ef972f0ad82c5bfcbc32b32
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-20 05:19:51 +0000

    Reviewed by Geoff.
            <rdar://problem/4636301> Deprecate JavaScriptGlue
    
            * JavaScriptGlue.h: adds DEPRECATED_IN_MAC_OS_X_VERSION_10_5_AND_LATER to everything
            * JavaScriptGlue.xcodeproj/project.pbxproj: use -Wno-deprecated-declarations to keep the project building
            * config.h: define DEPRECATED_IN_MAC_OS_X_VERSION_10_5_AND_LATER as empty on other systems
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptGlue/config.h
+++ third_party/WebKit/JavaScriptGlue/config.h
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#ifndef DEPRECATED_IN_MAC_OS_X_VERSION_10_5_AND_LATER
+#define DEPRECATED_IN_MAC_OS_X_VERSION_10_5_AND_LATER
+#endif
+
 #include <wtf/Platform.h>
 #include <kjs/ExecState.h>
 
