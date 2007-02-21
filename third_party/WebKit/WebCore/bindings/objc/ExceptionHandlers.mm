127db1b307d08e95ef05993d42ebc5806bd568b5
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-21 00:48:30 +0000

    Better build fix, rolling out part of r19579.
    git-svn-id: svn://svn.chromium.org/blink/trunk@19757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/ExceptionHandlers.mm
+++ third_party/WebKit/WebCore/bindings/objc/ExceptionHandlers.mm
@@ -32,29 +32,39 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGException.h"
 #include "XPathEvaluator.h"
 
+NSString * const DOMException = @"DOMException";
+NSString * const DOMRangeException = @"DOMRangeException";
+NSString * const DOMEventException = @"DOMEventException";
+#ifdef SVG_SUPPORT
+NSString * const DOMSVGException = @"DOMSVGException";
+#endif
+#ifdef XPATH_SUPPORT
+NSString * const DOMXPathException = @"DOMXPathException";
+#endif
+
 namespace WebCore {
 
 void raiseDOMException(ExceptionCode ec)
 {
     ASSERT(ec);
 
-    NSString *name = @"DOMException";
+    NSString *name = DOMException;
 
     int code = ec;
     if (ec >= RangeExceptionOffset && ec <= RangeExceptionMax) {
-        name = @"DOMRangeException";
+        name = DOMRangeException;
         code -= RangeExceptionOffset;
     } else if (ec >= EventExceptionOffset && ec <= EventExceptionMax) {
-        name = @"DOMEventException";
+        name = DOMEventException;
         code -= EventExceptionOffset;
 #ifdef SVG_SUPPORT
     } else if (ec >= SVGExceptionOffset && ec <= SVGExceptionMax) {
-        name = @"DOMSVGException";
+        name = DOMSVGException;
         code -= SVGExceptionOffset;
 #endif
 #ifdef XPATH_SUPPORT
     } else if (ec >= XPathExceptionOffset && ec <= XPathExceptionMax) {
-        name = @"DOMXPathException";
+        name = DOMXPathException;
         code -= XPathExceptionOffset;
 #endif
     }
