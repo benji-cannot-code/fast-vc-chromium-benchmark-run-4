5b982ffb197534643a49a92c7f755524bfabf3d9
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-21 07:30:06 +0000

    Preemptive build fix.
            * platform/mac/FontCacheMac.mm: Remove unused (and non-existent) header.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/FontCacheMac.mm
+++ third_party/WebKit/WebCore/platform/mac/FontCacheMac.mm
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "Font.h"
 #import "FontData.h"
 #import "FontPlatformData.h"
-#import "WebCoreStringTruncator.h"
 #import "WebCoreSystemInterface.h"
 #import "WebFontCache.h"
 
