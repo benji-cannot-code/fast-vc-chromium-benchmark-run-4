ea62b8e8b09327c7af231da69e6ae76f3b6ee955
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-21 00:40:34 +0000

    Build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@19756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/DOM.mm
+++ third_party/WebKit/WebCore/bindings/objc/DOM.mm
@@ -72,6 +72,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "SVGNames.h"
 #endif
 
+NSString * const DOMException = @"DOMException";
+NSString * const DOMEventException = @"DOMEventException";
+NSString * const DOMRangeException = @"DOMRangeException";
+NSString * const DOMXPathException = @"DOMXPathException";
+
+#ifdef SVG_SUPPORT
+NSString * const DOMSVGException = @"DOMSVGException";
+#endif
+
 namespace WebCore {
 
 class ObjCEventListener : public EventListener {
