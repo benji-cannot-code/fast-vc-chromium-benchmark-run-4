e9aa982fc63c36c3c9a4a1279965c4610f05dd5d
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-22 01:14:30 +0000

    Rubber-stamped by Darin.
            * bindings/objc/PublicDOMInterfaces.h: Add missing classes that are really public.
            Also adds the deprecated selector for getComputedStyle: on DOMDocument.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/PublicDOMInterfaces.h
+++ third_party/WebKit/WebCore/bindings/objc/PublicDOMInterfaces.h
@@ -71,6 +71,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (DOMHTMLDocument *)createHTMLDocument:(NSString *)title;
 @end
 
+@interface DOMAbstractView : DOMObject
+@property(readonly, retain) DOMDocument *document;
+@end
+
 @interface DOMDocument : DOMNode
 @property(readonly, retain) DOMDocumentType *doctype;
 @property(readonly, retain) DOMImplementation *implementation;
@@ -102,6 +106,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (DOMCSSStyleDeclaration *)createCSSStyleDeclaration;
 - (DOMCSSStyleDeclaration *)getOverrideStyle:(DOMElement *)element :(NSString *)pseudoElement;
 - (DOMCSSStyleDeclaration *)getOverrideStyle:(DOMElement *)element pseudoElement:(NSString *)pseudoElement;
+- (DOMCSSStyleDeclaration *)getComputedStyle:(DOMElement *)element :(NSString *)pseudoElement;
 - (DOMCSSStyleDeclaration *)getComputedStyle:(DOMElement *)element pseudoElement:(NSString *)pseudoElement;
 - (DOMCSSRuleList *)getMatchedCSSRules:(DOMElement *)element pseudoElement:(NSString *)pseudoElement;
 - (DOMCSSRuleList *)getMatchedCSSRules:(DOMElement *)element pseudoElement:(NSString *)pseudoElement authorOnly:(BOOL)authorOnly;
@@ -765,6 +770,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(copy) NSString *type;
 @end
 
+@interface DOMStyleSheetList : DOMObject
+@property(readonly) unsigned length;
+- (DOMStyleSheet *)item:(unsigned)index;
+@end
+
 @interface DOMCSSCharsetRule : DOMCSSRule
 @property(readonly, copy) NSString *encoding;
 @end
@@ -805,6 +815,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (DOMRGBColor *)getRGBColorValue;
 @end
 
+@interface DOMRGBColor : DOMObject
+@property(readonly, retain) DOMCSSPrimitiveValue *red;
+@property(readonly, retain) DOMCSSPrimitiveValue *green;
+@property(readonly, retain) DOMCSSPrimitiveValue *blue;
+@property(readonly, retain) DOMCSSPrimitiveValue *alpha;
+@property(readonly, copy) NSColor *color;
+@end
+
 @interface DOMCSSRule : DOMObject
 @property(readonly) unsigned short type;
 @property(copy) NSString *cssText;
@@ -835,6 +853,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface DOMCSSStyleRule : DOMCSSRule
 @property(copy) NSString *selectorText;
 @property(readonly, retain) DOMCSSStyleDeclaration *style;
+@end
+
+@interface DOMStyleSheet : DOMObject
+@property(readonly, copy) NSString *type;
+@property BOOL disabled;
+@property(readonly, retain) DOMNode *ownerNode;
+@property(readonly, retain) DOMStyleSheet *parentStyleSheet;
+@property(readonly, copy) NSString *href;
+@property(readonly, copy) NSString *title;
+@property(readonly, retain) DOMMediaList *media;
 @end
 
 @interface DOMCSSStyleSheet : DOMStyleSheet
@@ -987,6 +1015,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (DOMNode *)nextNode;
 - (DOMNode *)previousNode;
 - (void)detach;
+@end
+
+@interface DOMMediaList : DOMObject
+@property(copy) NSString *mediaText;
+@property(readonly) unsigned length;
+- (NSString *)item:(unsigned)index;
+- (void)deleteMedium:(NSString *)oldMedium;
+- (void)appendMedium:(NSString *)newMedium;
 @end
 
 @interface DOMTreeWalker : DOMObject
