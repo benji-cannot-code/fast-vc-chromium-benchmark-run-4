9eb1c359d6afb5490520cc5e2b4042523d555d1b
zack <zack@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-22 16:52:15 +0000

    Fixing the default scroll offset, cleaning up private methods and making the default look not use any frames. r=Lars
    git-svn-id: svn://svn.chromium.org/blink/trunk@19793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitQt/Api/qwebframe_p.h
+++ third_party/WebKit/WebKitQt/Api/qwebframe_p.h
@@ -71,6 +71,7 @@ public:
               QWebFrameData *frameData);
 
     void _q_adjustScrollbars();
+    void _q_handleKeyEvent(QKeyEvent *ev, bool isKeyUp);
 
     QWebFrame *q;
     WebCore::FrameLoaderClientQt *frameLoaderClient;
