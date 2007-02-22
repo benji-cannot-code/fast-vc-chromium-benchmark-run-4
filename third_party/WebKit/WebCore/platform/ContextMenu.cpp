511a2e6df2de9be3bf9676b6493d506c4ac95d33
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-22 20:50:13 +0000

    Reviewed by John.
            Fix <rdar://problem/5016969> REGRESSION: "Check Spelling As You Type" context menu item is never checked
    
            * platform/ContextMenu.cpp: (WebCore::ContextMenu::checkOrEnableIfNeeded): Set shouldCheck for ContextMenuItemTagCheckSpellingWhileTyping.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19799 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ContextMenu.cpp
+++ third_party/WebKit/WebCore/platform/ContextMenu.cpp
@@ -467,6 +467,9 @@ void ContextMenu::checkOrEnableIfNeeded(ContextMenuItem& item) const
         case ContextMenuItemTagNoGuessesFound:
             shouldEnable = false;
             break;
+        case ContextMenuItemTagCheckSpellingWhileTyping:
+            shouldCheck = frame->editor()->isContinuousSpellCheckingEnabled();
+            break;
         case ContextMenuItemTagNoAction:
         case ContextMenuItemTagOpenLinkInNewWindow:
         case ContextMenuItemTagDownloadLinkToDisk:
@@ -496,7 +499,6 @@ void ContextMenu::checkOrEnableIfNeeded(ContextMenuItem& item) const
         case ContextMenuItemTagOpenLink:
         case ContextMenuItemTagIgnoreGrammar:
         case ContextMenuItemTagSpellingMenu:
-        case ContextMenuItemTagCheckSpellingWhileTyping:
         case ContextMenuItemTagFontMenu:
         case ContextMenuItemTagShowFonts:
         case ContextMenuItemTagStyles:
