0ac1d3c0a3b7c1f2903b6240702cf21aa8ab1941
kjk <kjk@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-24 08:45:04 +0000

    WebCore:
            Reviewed by Adam Roben.
    
            Gdk improvements.
    
            * platform/gdk/FrameGdk.cpp: only scroll on keydown, not both keydown and keyup.
            (WebCore::FrameGdk::keyPress):
            (WebCore::FrameGdk::handleGdkEvent):
            (WebCore::Frame::print):
            (WebCore::Frame::issueTransposeCommand):
            (WebCore::Frame::respondToChangedSelection):
            (WebCore::Frame::cleanupPlatformScriptObjects):
            (WebCore::Frame::dragImageForSelection):
            * platform/network/gdk/ResourceHandleManager.cpp: don't free memory that
            doesn't belong to us.
            (WebCore::ResourceHandleManager::downloadTimerCallback):
    
      WebKitTools:
    
            Reviewed by Adam Roben.
    
            Improve gdklauncher: add text field for entering url.
    
            * GdkLauncher/gdklauncher.bkl:
            * GdkLauncher/main.cpp:
            (strEmpty):
            (strEq):
            (handleGdkEvent):
            (goToUrlBarText):
            (goButtonClickedCb):
            (urlBarEnterCb):
            (registerRenderingAreaEvents):
            (frameResizeCb):
            (frameDestroyCb):
            (main):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19839 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gdk/FrameGdk.cpp
+++ third_party/WebKit/WebCore/platform/gdk/FrameGdk.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HitTestRequest.h"
 #include "HitTestResult.h"
 #include "KeyboardCodes.h"
+#include "NotImplementedGdk.h"
 #include "Page.h"
 #include "PlatformKeyboardEvent.h"
 #include "PlatformMouseEvent.h"
@@ -56,8 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <gdk/gdk.h>
 #include <gtk/gtk.h>
-
-#define notImplemented() do { fprintf(stderr, "FIXME: UNIMPLEMENTED %s %s:%d\n", __PRETTY_FUNCTION__, __FILE__, __LINE__); } while(0)
 
 // This function loads resources from WebKit
 // This does not belong here and I'm not sure where
@@ -154,13 +153,16 @@ bool FrameGdk::keyPress(const PlatformKeyboardEvent& keyEvent)
 
     switch (keyEvent.WindowsKeyCode()) {
         case VK_LEFT:
-            doScroll(renderer(), true, -120);
+            if (!keyEvent.isKeyUp())
+                doScroll(renderer(), true, -120);
             break;
         case VK_RIGHT:
-            doScroll(renderer(), true, 120);
+            if (!keyEvent.isKeyUp())
+                doScroll(renderer(), true, 120);
             break;
         case VK_UP:
-            doScroll(renderer(), false, -120);
+            if (!keyEvent.isKeyUp())
+                doScroll(renderer(), false, -120);
             break;
         case VK_PRIOR:
             // FIXME: implement me
@@ -169,20 +171,26 @@ bool FrameGdk::keyPress(const PlatformKeyboardEvent& keyEvent)
             // FIXME: implement me
             break;
         case VK_DOWN:
-            doScroll(renderer(), false, 120);
+            if (!keyEvent.isKeyUp())
+                doScroll(renderer(), false, 120);
             break;
         case VK_HOME:
-            renderer()->layer()->scrollToOffset(0, 0, true, true);
-            doScroll(renderer(), false, 120);
+            if (!keyEvent.isKeyUp()) {
+                renderer()->layer()->scrollToOffset(0, 0, true, true);
+                doScroll(renderer(), false, 120);
+            }
             break;
         case VK_END:
-            renderer()->layer()->scrollToOffset(0, renderer()->height(), true, true);
+            if (!keyEvent.isKeyUp())
+                renderer()->layer()->scrollToOffset(0, renderer()->height(), true, true);
             break;
         case VK_SPACE:
-            if (keyEvent.shiftKey())
-                doScroll(renderer(), false, -120);
-            else
-                doScroll(renderer(), false, 120);
+            if (!keyEvent.isKeyUp()) {
+                if (keyEvent.shiftKey())
+                    doScroll(renderer(), false, -120);
+                else
+                    doScroll(renderer(), false, 120);
+            }
             break;
     }
     return true;
@@ -198,7 +206,20 @@ void FrameGdk::handleGdkEvent(GdkEvent* event)
             gdk_window_begin_paint_region(event->any.window, event->expose.region);
             cairo_t* cr = gdk_cairo_create(event->any.window);
             GraphicsContext ctx(cr);
-            paint(&ctx, IntRect(clip.x, clip.y, clip.width, clip.height));
+            if (renderer()) {
+                if (view()->layoutPending())
+                    view()->layout();
+                IntRect rect(clip.x, clip.y, clip.width, clip.height);
+                paint(&ctx, rect);
+            } else {
+                IntRect rect(clip.x, clip.y, clip.width, clip.height);
+                ctx.fillRect(rect, Color(0xff, 0xff, 0xff));
+#if 0 // FIXME: crashes because font subsystem isn't ready yet
+                StringImpl txt("Welcome to Gdk Web Browser");
+                TextRun textRun(txt);
+                ctx.drawText(textRun, IntPoint(10,10));
+#endif
+            }
             cairo_destroy(cr);
             gdk_window_end_paint(event->any.window);
             break;
@@ -262,21 +283,23 @@ void FrameGdk::handleGdkEvent(GdkEvent* event)
 
 void Frame::print() 
 {
-    notImplemented();
+    notImplementedGdk();
 }
 
 void Frame::issueTransposeCommand()
 {
-    notImplemented();
+    notImplementedGdk();
 }
 
 void Frame::respondToChangedSelection(WebCore::Selection const&, bool)
 {
     // FIXME: If we want continous spell checking, we need to implement this.
+    notImplementedGdk();
 }
 
 void Frame::cleanupPlatformScriptObjects()
 {
+    notImplementedGdk();
 }
 
 bool Frame::isCharacterSmartReplaceExempt(UChar, bool)
@@ -287,6 +310,7 @@ bool Frame::isCharacterSmartReplaceExempt(UChar, bool)
 
 DragImageRef Frame::dragImageForSelection() 
 {
+    notImplementedGdk();
     return 0;
 }
 
