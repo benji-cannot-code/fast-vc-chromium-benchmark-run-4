8032a9d1f5ccc98bbb152b6ef6b58c4bb89cc991
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-24 02:55:24 +0000

    LayoutTests:
            Reviewed by Maciej.
    
            - updated test result for http://bugs.webkit.org/show_bug.cgi?id=12856
              REGRESSION (r13685): Canceling the suggestion popup does not revert the change
    
            * editing/deleting/delete-3800834-fix-expected.txt:
    
    WebCore:
    
            Reviewed by Maciej.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=12856
              REGRESSION (r13685): Canceling the suggestion popup does not revert the change
    
            * editing/ReplaceSelectionCommand.cpp:
            (WebCore::ReplaceSelectionCommand::doApply): If the fragment to be inserted
            is empty, first delete the selection, and only then bail out. 
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19837 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/ReplaceSelectionCommand.cpp
+++ third_party/WebKit/WebCore/editing/ReplaceSelectionCommand.cpp
@@ -480,9 +480,6 @@ void ReplaceSelectionCommand::doApply()
     Element* currentRoot = selection.rootEditableElement();
     ReplacementFragment fragment(document(), m_documentFragment.get(), m_matchStyle, selection);
     
-    if (fragment.isEmpty())
-        return;
-    
     if (m_matchStyle)
         m_insertionStyle = styleAtPosition(selection.start());
     
@@ -575,7 +572,7 @@ void ReplaceSelectionCommand::doApply()
     setTypingStyle(0);    
     
     // We're finished if there is nothing to add.
-    if (!fragment.firstChild())
+    if (fragment.isEmpty() || !fragment.firstChild())
         return;
     
     // 1) Insert the content.
