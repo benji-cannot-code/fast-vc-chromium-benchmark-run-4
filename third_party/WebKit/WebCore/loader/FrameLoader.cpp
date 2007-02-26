d599ca867c7a0ba52af4d621f871ff43d03534f3
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-26 21:11:42 +0000

    Reviewed by Geoff.
            <rdar://problem/4816376>
            REGRESSION: NetNewsWire 3.0 - Crashes in WebDocumentLoaderMac::attachToFrame() (12674)
            
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::reload):
            Remove call to setPolicyDocumentLoader here, it's set by the ::load function that we end up calling.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -2000,7 +2000,6 @@ void FrameLoader::reload()
         initialRequest = ResourceRequest(unreachableURL);
     
     RefPtr<DocumentLoader> loader = m_client->createDocumentLoader(initialRequest, SubstituteData());
-    setPolicyDocumentLoader(loader.get());
 
     ResourceRequest& request = loader->request();
 
