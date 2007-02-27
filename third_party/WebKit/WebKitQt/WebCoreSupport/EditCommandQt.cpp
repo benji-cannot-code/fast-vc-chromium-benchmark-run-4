a7138db0dd7aed04d24fdbbadc5be6c6031f9561
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-27 05:30:48 +0000

    WebCore:
            Reviewed by Kevin McCullough.
    
            - fix Qt build for earlier SVG changes.
    
            * platform/graphics/svg/qt/SVGResourceFilterQt.cpp:
    
    WebKitQt:
    
            Reviewed by Kevin McCullough.
    
            - fix Qt build for earlier SVG changes.
    
            * WebCoreSupport/EditCommandQt.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitQt/WebCoreSupport/EditCommandQt.cpp
+++ third_party/WebKit/WebKitQt/WebCoreSupport/EditCommandQt.cpp
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     Boston, MA 02111-1307, USA.
 */
 
+#include <wtf/Platform.h>
 #include "EditCommandQt.h"
 
 using namespace WebCore;
