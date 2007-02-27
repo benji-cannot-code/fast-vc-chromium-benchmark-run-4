28f4ce183f542e391a76857db88324ca1ed03afa
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-27 05:13:49 +0000

    2007-02-26  Mitz Pettel  <mitz@webkit.org>
            Reviewed by Antti Koivisto.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=12899
              Ordered lists not updating dynamically with new styles
    	  <rdar://problem/4889356>
    
            Test: fast/lists/list-style-type-dynamic-change.html
    
            * rendering/RenderListMarker.cpp:
            (WebCore::RenderListMarker::setStyle): Made changing the marker type trigger
            updating of the marker.
    
    2007-02-26  Mitz Pettel  <mitz@webkit.org>
    
            Reviewed by Antti Koivisto.
    
            - test for http://bugs.webkit.org/show_bug.cgi?id=12899
              Ordered lists not updating dynamically with new styles
    
            * fast/lists/list-style-type-dynamic-change-expected.checksum: Added.
            * fast/lists/list-style-type-dynamic-change-expected.png: Added.
            * fast/lists/list-style-type-dynamic-change-expected.txt: Added.
            * fast/lists/list-style-type-dynamic-change.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19875 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderListMarker.cpp
+++ third_party/WebKit/WebCore/rendering/RenderListMarker.cpp
@@ -483,7 +483,7 @@ RenderListMarker::~RenderListMarker()
 
 void RenderListMarker::setStyle(RenderStyle* s)
 {
-    if (s && style() && s->listStylePosition() != style()->listStylePosition())
+    if (s && style() && (s->listStylePosition() != style()->listStylePosition() || s->listStyleType() != style()->listStyleType()))
         setNeedsLayoutAndMinMaxRecalc();
     
     RenderBox::setStyle(s);
