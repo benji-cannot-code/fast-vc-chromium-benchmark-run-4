48328484fdc72b9a09d3760338031307c36faa7d
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-02-27 05:23:22 +0000

    2007-02-26  Oliver Hunt  <oliver@apple.com>
            Reviewed by Maciej.
    
            Fix for <rdar://problem/4827378>: Canvas with large height 
            uses lots of memory, computer almost stops responding
             
            Put cap on maximum area of canvas, size is similar too the 
            maximum size allowed by firefox (firefox seems to to cut off 
            at area == 32767 * 9358). 
    
            Also protect renderer against the possibility of a null context
            (this was triggering a CG warning)
    
            * html/HTMLCanvasElement.cpp:
            (WebCore::HTMLCanvasElement::createDrawingContext):
               Apply maximum canvas area
            (WebCore::HTMLCanvasElement::createPlatformImage):
               Protect against null CG Context
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19876 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLCanvasElement.cpp
@@ -50,6 +50,11 @@ using namespace HTMLNames;
 const int defaultWidth = 300;
 const int defaultHeight = 150;
 
+// Firefox limits width/height to 32767 pixels, but slows down dramatically before it 
+// reaches that limit we limit by area instead, giving us larger max dimensions, in exchange 
+// for reduce maximum canvas size.
+const float maxCanvasArea = 32768 * 8192; // Maximum canvas area in CSS pixels
+
 HTMLCanvasElement::HTMLCanvasElement(Document* doc)
     : HTMLElement(canvasTag, doc)
     , m_size(defaultWidth, defaultHeight)
@@ -179,7 +184,7 @@ void HTMLCanvasElement::createDrawingContext() const
     float wf = ceilf(unscaledWidth * pageScaleFactor);
     float hf = ceilf(unscaledHeight * pageScaleFactor);
     
-    if (!(wf > 0 && wf < UINT_MAX && hf > 0 && hf < UINT_MAX))
+    if (!(wf >= 1 && hf >= 1 && wf * hf <= maxCanvasArea))
         return;
         
     unsigned w = static_cast<unsigned>(wf);
@@ -215,8 +220,14 @@ CGImageRef HTMLCanvasElement::createPlatformImage() const
     GraphicsContext* context = drawingContext();
     if (!context)
         return 0;
-    CGContextFlush(context->platformContext());
-    return CGBitmapContextCreateImage(context->platformContext());
+    
+    CGContextRef contextRef = context->platformContext();
+    if (!contextRef)
+        return 0;
+    
+    CGContextFlush(contextRef);
+    
+    return CGBitmapContextCreateImage(contextRef);
 }
 
 #endif
