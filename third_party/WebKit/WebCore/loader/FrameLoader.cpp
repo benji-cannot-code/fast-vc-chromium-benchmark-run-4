b40402a659c571b026563212c2e595cd82d10f08
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-06 19:28:46 +0000

    Reviewed by Darin.
            - Rename a function to clarify its purpose.
    
            * WebCore.exp:
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::registerURLSchemeAsLocal):
            * loader/FrameLoader.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@19984 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -4299,7 +4299,7 @@ void FrameLoader::continueLoadWithData(SharedBuffer* buffer, const String& mimeT
     addData(buffer->data(), buffer->size());
 }
 
-void FrameLoader::registerSchemeAsLocal(const String& scheme)
+void FrameLoader::registerURLSchemeAsLocal(const String& scheme)
 {
     localSchemes().add(scheme);
 }
