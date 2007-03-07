a115523a7693c00a318d2ca0f96df5790e539d6c
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-07 23:32:39 +0000

    Fixing a comment.
    git-svn-id: svn://svn.chromium.org/blink/trunk@20036 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLAnchorElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLAnchorElement.cpp
@@ -288,7 +288,7 @@ void HTMLAnchorElement::parseMappedAttribute(MappedAttribute *attr)
 
 void HTMLAnchorElement::accessKeyAction(bool sendToAnyElement)
 {
-    // send the mouse button events iff the caller specified sendToAnyElement
+    // send the mouse button events if the caller specified sendToAnyElement
     dispatchSimulatedClick(0, sendToAnyElement);
 }
 
