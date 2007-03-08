973e428e32637c6609d77701d381c56fb9129ab5
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-08 03:31:33 +0000

    The change to go along with Anders' ChangeLog entry.
    git-svn-id: svn://svn.chromium.org/blink/trunk@20050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins/WebBaseNetscapePluginView.mm
+++ third_party/WebKit/WebKit/Plugins/WebBaseNetscapePluginView.mm
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebCore/FrameLoader.h> 
 #import <WebCore/FrameTree.h> 
 #import <WebCore/Page.h> 
+#import <WebCore/WebCoreObjCExtras.h>
 #import <WebKit/DOMPrivate.h>
 #import <WebKit/WebUIDelegate.h>
 #import <objc/objc-runtime.h>
