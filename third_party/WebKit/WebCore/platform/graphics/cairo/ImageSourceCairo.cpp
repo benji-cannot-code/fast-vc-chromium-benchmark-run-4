92552b37d846cf47183d86eeceebfcd94766e56a
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-08 13:57:12 +0000

    2007-03-09  Shrikant Gangoda  <shrikant.gangoda@celunite.com>
            Gdk build fix.
    
            * platform/graphics/cairo/ImageSourceCairo.cpp:
            (WebCore::ImageSource::~ImageSource):
            (WebCore::ImageSource::clear):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cairo/ImageSourceCairo.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cairo/ImageSourceCairo.cpp
@@ -91,7 +91,13 @@ ImageSource::ImageSource()
 
 ImageSource::~ImageSource()
 {
+    clear();
+}
+
+void ImageSource::clear()
+{
     delete m_decoder;
+    m_decoder = 0;
 }
 
 bool ImageSource::initialized() const
