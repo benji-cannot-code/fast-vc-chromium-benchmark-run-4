ce468fd30ac8518a21ce0350d2f75d7a4ef04ab0
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-09 02:25:25 +0000

    Reviewed by Adele.
            - <rdar://problem/4470381> multipart/form-data boundary security vulnerability
    
            By making the form data boundary a string with some random data in it, we reduce
            the possibility that anyone could take advantage of it by creating a file that
            intentionally has the boundary string in it.
    
            * html/HTMLFormElement.h: Removed boundary(), setBoundary(), and m_boundary.
            Marked a lot more stuff private.
            * html/HTMLFormElement.cpp:
            (WebCore::HTMLFormElement::HTMLFormElement): Removed code to initialize
            m_boundary.
            (WebCore::randomNumber): Added. Function that returns a random number, including
            seeding the random number generator the first time it's called. For now, usees the more
            random function random() on Mac OS X and the more-standard rand() on other platforms.
            (WebCore::HTMLFormElement::formData): Take a parameter with the form boundary string,
            and use that instead of m_boundary.
            (WebCore::getUniqueBoundaryString): Added. Makes a boundary string using random numbers
            and base 64 encoding.
            (WebCore::HTMLFormElement::submit): Call getUniqueBoundaryString and pass the boundary
            string into formData for multipart form posts.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLFormElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLFormElement.cpp
@@ -1,11 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * This file is part of the DOM implementation for KDE.
- *
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
- * Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2007 Apple Inc. All rights reserved.
  *           (C) 2006 Alexey Proskuryakov (ap@nypop.com)
  *
  * This library is free software; you can redistribute it and/or
@@ -28,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "HTMLFormElement.h"
 
+#include "Base64.h"
 #include "CString.h"
 #include "csshelper.h"
 #include "Event.h"
@@ -60,7 +59,6 @@ HTMLFormElement::HTMLFormElement(Document* doc)
     m_doingsubmit = false;
     m_inreset = false;
     m_enctype = "application/x-www-form-urlencoded";
-    m_boundary = "----------0xKhTmLbOuNdArY";
     m_malformed = false;
     m_preserveAcrossRemove = false;
 }
@@ -197,9 +195,28 @@ static DeprecatedCString encodeCString(const CString& cstr)
     return encoded;
 }
 
-PassRefPtr<FormData> HTMLFormElement::formData() const
+static int randomNumber()
 {
-    DeprecatedCString enc_string = ""; // used for non-multipart data
+    static bool randomSeeded = false;
+
+#if PLATFORM(DARWIN)
+    if (!randomSeeded) {
+        srandomdev();
+        randomSeeded = true;
+    }
+    return random();
+#else
+    if (!randomSeeded) {
+        srand(static_cast<unsigned>(time(0)));
+        randomSeeded = true;
+    }
+    return rand();
+#endif
+}
+
+PassRefPtr<FormData> HTMLFormElement::formData(const char* boundary) const
+{
+    DeprecatedCString enc_string = "";
 
     String str = m_acceptcharset;
     str.replace(',', ' ');
@@ -246,7 +263,7 @@ PassRefPtr<FormData> HTMLFormElement::formData() const
                 else
                 {
                     DeprecatedCString hstr("--");
-                    hstr += m_boundary.deprecatedString().latin1();
+                    hstr += boundary;
                     hstr += "\r\n";
                     hstr += "Content-Disposition: form-data; name=\"";
                     hstr += item.m_data.data();
@@ -295,8 +312,11 @@ PassRefPtr<FormData> HTMLFormElement::formData() const
     }
 
 
-    if (m_multipart)
-        enc_string = ("--" + m_boundary.deprecatedString() + "--\r\n").ascii();
+    if (m_multipart) {
+        enc_string = "--";
+        enc_string += boundary;
+        enc_string += "--\r\n";
+    }
 
     result->appendData(enc_string.data(), enc_string.length());
     return result;
@@ -314,11 +334,6 @@ void HTMLFormElement::parseEnctype(const String& type)
         m_enctype = "application/x-www-form-urlencoded";
         m_multipart = false;
     }
-}
-
-void HTMLFormElement::setBoundary( const String& bound )
-{
-    m_boundary = bound;
 }
 
 bool HTMLFormElement::prepareSubmit(Event* event)
@@ -344,6 +359,30 @@ bool HTMLFormElement::prepareSubmit(Event* event)
 void HTMLFormElement::submit()
 {
     submit(0, false);
+}
+
+// Returns a 0-terminated C string in the vector.
+static void getUniqueBoundaryString(Vector<char>& boundary)
+{
+    // Start with an informative prefix.
+    const char boundaryPrefix[] = "----WebKit-form-boundary-";
+    boundary.append(boundaryPrefix, strlen(boundaryPrefix));
+
+    // Append 16 characters of base 64 randomness.
+    Vector<char> randomBytes;
+    for (int i = 0; i < 3; ++i) {
+        int randomness = randomNumber();
+        randomBytes.append(static_cast<char>(randomness >> 24));
+        randomBytes.append(static_cast<char>(randomness >> 16));
+        randomBytes.append(static_cast<char>(randomness >> 8));
+        randomBytes.append(static_cast<char>(randomness));
+    }
+    Vector<char> randomBase64;
+    base64Encode(randomBytes, randomBase64);
+    boundary.append(randomBase64);
+
+    // Add a 0 at the end so we can use this as a C-style string.
+    boundary.append(0);
 }
 
 void HTMLFormElement::submit(Event* event, bool activateSubmitButton)
@@ -386,12 +425,19 @@ void HTMLFormElement::submit(Event* event, bool activateSubmitButton)
         firstSuccessfulSubmitButton->setActivatedSubmit(true);
 
     if (!m_post)
+
+    if (m_post) {
+        if (!m_multipart)
+            frame->loader()->submitForm("POST", m_url, formData(0), m_target, enctype(), String(), event);
+        else {
+            Vector<char> boundary;
+            getUniqueBoundaryString(boundary);
+            frame->loader()->submitForm("POST", m_url, formData(boundary.data()), m_target, enctype(), boundary.data(), event);
+        }
+    } else {
         m_multipart = false;
-    
-    if (m_post)
-        frame->loader()->submitForm("POST", m_url, formData(), m_target, enctype(), boundary(), event);
-    else
-        frame->loader()->submitForm("GET", m_url, formData(), m_target, String(), String(), event);
+        frame->loader()->submitForm("GET", m_url, formData(0), m_target, String(), String(), event);
+    }
 
     if (needButtonActivation && firstSuccessfulSubmitButton)
         firstSuccessfulSubmitButton->setActivatedSubmit(false);
--- third_party/WebKit/WebCore/html/HTMLFormElement.h
+++ third_party/WebKit/WebCore/html/HTMLFormElement.h
@@ -1,11 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * This file is part of the DOM implementation for KDE.
- *
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2000 Dirk Mueller (mueller@kde.org)
- * Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2007 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -62,9 +60,6 @@ public:
     String encoding() const { return m_enctype; }
     void setEncoding(const String& enctype) { setEnctype(enctype); }
 
-    String boundary() const { return m_boundary; }
-    void setBoundary(const String&);
-
     bool autoComplete() const { return m_autocomplete; }
 
     virtual void parseMappedAttribute(MappedAttribute*);
@@ -105,11 +100,18 @@ public:
     virtual String target() const;
     void setTarget(const String&);
 
+    // FIXME: Change this to be private after getting rid of all the clients.
+    Vector<HTMLGenericFormElement*> formElements;
+
+private:
+    void parseEnctype(const String&);
+    PassRefPtr<FormData> formData(const char* boundary) const;
+    unsigned formElementIndex(HTMLGenericFormElement*);
+
     friend class HTMLFormCollection;
 
     HTMLCollection::CollectionInfo* collectionInfo;
 
-    Vector<HTMLGenericFormElement*> formElements;
     Vector<HTMLImageElement*> imgElements;
     String m_url;
     String m_target;
@@ -124,12 +126,6 @@ public:
     bool m_inreset : 1;
     bool m_malformed : 1;
     bool m_preserveAcrossRemove : 1;
-
-private:
-    void parseEnctype(const String&);
-    PassRefPtr<FormData> formData() const;
-
-    unsigned formElementIndex(HTMLGenericFormElement*);
 
     String oldNameAttr;
 };
