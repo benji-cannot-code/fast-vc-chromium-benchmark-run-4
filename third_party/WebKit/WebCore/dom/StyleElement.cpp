0f8c7e5c196b684bd8ccf270e1287bff4f462b9e
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-10 22:43:14 +0000

    LayoutTests:
            Reviewed by Darin.
    
            - test for http://bugs.webkit.org/show_bug.cgi?id=12973
              REGRESSION: Reproducible assert while loading this test file if css is already in the cache
    
            * fast/dom/css-cached-import-rule-expected.checksum: Added.
            * fast/dom/css-cached-import-rule-expected.png: Added.
            * fast/dom/css-cached-import-rule-expected.txt: Added.
            * fast/dom/css-cached-import-rule.html: Added.
    
    WebCore:
    
            Reviewed by Darin.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=12973
              REGRESSION: Reproducible assert while loading this test file if css is already in the cache
    
            Test: fast/dom/css-cached-import-rule.html
    
            Replaced some direct calls to document->stylesheetLoaded() with calls to
            the sheet's checkLoaded(). The latter calls back to the element's sheetLoaded() --
            which notifies the document of the load -- and then updates the sheet's
            loadCompleted() flag, ensuring that it stays in sync with whether the stylesheet
            is still considered pending by the document.
    
            * dom/ProcessingInstruction.cpp:
            (WebCore::ProcessingInstruction::parseStyleSheet):
            * dom/StyleElement.cpp:
            (WebCore::StyleElement::childrenChanged):
            * html/HTMLLinkElement.cpp:
            (WebCore::HTMLLinkElement::process):
            (WebCore::HTMLLinkElement::setCSSStyleSheet):
            * ksvg2/svg/SVGStyleElement.cpp:
            (WebCore::SVGStyleElement::sheetLoaded):
            * ksvg2/svg/SVGStyleElement.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20098 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/StyleElement.cpp
+++ third_party/WebKit/WebCore/dom/StyleElement.cpp
@@ -89,8 +89,8 @@ void StyleElement::childrenChanged(Element* e)
         }
     }
 
-    if (!isLoading() && m_sheet)
-        document->stylesheetLoaded();
+    if (m_sheet)
+        m_sheet->checkLoaded();
 }
 
 }
