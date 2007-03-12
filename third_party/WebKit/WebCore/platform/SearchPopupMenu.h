efc08476a24ba43ece4dd7909454e268a44dbd57
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-12 20:43:14 +0000

    Reviewed by Oliver.
            Adding the ability to enable or disable a SearchPopupMenu.
    
            * platform/SearchPopupMenu.h:
            * platform/mac/SearchPopupMenuMac.mm: (WebCore::SearchPopupMenu::enabled):
            * rendering/RenderTextControl.cpp: (WebCore::RenderTextControl::showPopup):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20123 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/SearchPopupMenu.h
+++ third_party/WebKit/WebCore/platform/SearchPopupMenu.h
@@ -36,6 +36,8 @@ public:
 
     void saveRecentSearches(const AtomicString& name, const Vector<String>& searchItems);
     void loadRecentSearches(const AtomicString& name, Vector<String>& searchItems);
+
+    bool enabled();
     
 protected:
     SearchPopupMenu(PopupMenuClient* client);
