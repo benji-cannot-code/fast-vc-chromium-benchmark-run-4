dbd6dfe94774f1b2b4e0d72b260d74cdd17bd39f
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-15 03:23:56 +0000

    Reviewed by .
            - Fixed a build breakage.
    
            * kjs/DateMath.cpp:
            * kjs/date_object.cpp:
            (KJS::formatLocaleDate):
            (KJS::DateObjectImp::construct):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20204 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/DateMath.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/DateMath.cpp
@@ -53,6 +53,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <notify.h>
 #endif
 
+#if HAVE(SYS_TIMEB_H)
+#include <sys/timeb.h>
+#endif
+
 namespace KJS {
 
 /* Constants */
