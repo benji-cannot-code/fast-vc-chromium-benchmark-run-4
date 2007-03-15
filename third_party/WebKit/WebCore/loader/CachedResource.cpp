9c74ce27195ac9ef9c37d0f08778831e65f7a175
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-15 00:28:21 +0000

    Add asserts to help catch double refs and double derefs of CachedResources.
            Reviewed by mjs
    
            * loader/CachedResource.cpp:
            (WebCore::CachedResource::ref):
            (WebCore::CachedResource::deref):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20199 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/CachedResource.cpp
+++ third_party/WebKit/WebCore/loader/CachedResource.cpp
@@ -95,6 +95,7 @@ void CachedResource::setRequest(Request* request)
 
 void CachedResource::ref(CachedResourceClient *c)
 {
+    ASSERT(!m_clients.contains(c));
     if (!referenced() && inCache())
         cache()->addToLiveObjectSize(size());
     m_clients.add(c);
@@ -102,6 +103,7 @@ void CachedResource::ref(CachedResourceClient *c)
 
 void CachedResource::deref(CachedResourceClient *c)
 {
+    ASSERT(m_clients.contains(c));
     m_clients.remove(c);
     if (canDelete() && !inCache())
         delete this;
