243e5e7d6093a9e01a144a2d9b650eb7c9a760ec
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-15 13:26:45 +0000

    2007-03-16  Shrikant Gangoda  <shrikant.gangoda@celunite.com>
            Gdk build fix.
    
            * kjs/DateMath.cpp:  gettimeofday comes from <sys/time.h> on Linux.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20213 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/DateMath.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/DateMath.cpp
@@ -53,6 +53,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <notify.h>
 #endif
 
+#if HAVE(SYS_TIME_H)
+#include <sys/time.h>
+#endif
+
 #if HAVE(SYS_TIMEB_H)
 #include <sys/timeb.h>
 #endif
