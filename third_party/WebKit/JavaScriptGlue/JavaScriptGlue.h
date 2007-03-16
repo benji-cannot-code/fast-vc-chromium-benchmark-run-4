7aaf854878bb0aab75fc5b976af477467d11a047
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-16 00:37:22 +0000

    Yet another build fix. Sigh.
    git-svn-id: svn://svn.chromium.org/blink/trunk@20224 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptGlue/JavaScriptGlue.h
+++ third_party/WebKit/JavaScriptGlue/JavaScriptGlue.h
@@ -34,6 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <CoreServices/CoreServices.h>
 #endif
 
+#ifndef DEPRECATED_IN_MAC_OS_X_VERSION_10_5_AND_LATER
+#define DEPRECATED_IN_MAC_OS_X_VERSION_10_5_AND_LATER
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
