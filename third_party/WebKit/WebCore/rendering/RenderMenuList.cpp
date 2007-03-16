43974ed9f9b5adb51e4c955f68c111da554f1318
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-16 06:18:08 +0000

    Reviewed by Kevin Decker.
            Fix for <rdar://problem/4926179> Text in menulist control should never update if menu is open
    
            * rendering/RenderMenuList.cpp: (WebCore::RenderMenuList::updateFromElement):
              Only setTextFromOption if the menu isn't visible.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderMenuList.cpp
+++ third_party/WebKit/WebCore/rendering/RenderMenuList.cpp
@@ -141,10 +141,10 @@ void RenderMenuList::updateFromElement()
         m_optionsChanged = false;
     }
 
-    setTextFromOption(static_cast<HTMLSelectElement*>(node())->selectedIndex());
-
     if (m_popupIsVisible)
         m_popup->updateFromElement();
+    else
+        setTextFromOption(static_cast<HTMLSelectElement*>(node())->selectedIndex());
 }
 
 void RenderMenuList::setTextFromOption(int optionIndex)
