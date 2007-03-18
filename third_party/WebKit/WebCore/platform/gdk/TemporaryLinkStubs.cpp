2d5f4e4b1041cf313dbfb0cc0c4599cd670236be
kjk <kjk@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-18 00:04:47 +0000

    Not reviewed - gdk build fix.
            * platform/gdk/TemporaryLinkStubs.cpp:
            (Pasteboard::writeURL):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gdk/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/gdk/TemporaryLinkStubs.cpp
@@ -297,7 +297,7 @@ void Editor::markMisspellings(const Selection&) { notImplementedGdk(); }
 
 Pasteboard* Pasteboard::generalPasteboard() { notImplementedGdk(); return 0; }
 void Pasteboard::writeSelection(Range*, bool, Frame*) { notImplementedGdk(); }
-void Pasteboard::writeURL(const KURL&, const String&, Frame*, bool) { notImplementedGdk(); }
+void Pasteboard::writeURL(const KURL&, const String&, Frame*) { notImplementedGdk(); }
 void Pasteboard::writeImage(const HitTestResult&) { notImplementedGdk(); }
 void Pasteboard::clear() { notImplementedGdk(); }
 bool Pasteboard::canSmartReplace() { notImplementedGdk(); return false; }
