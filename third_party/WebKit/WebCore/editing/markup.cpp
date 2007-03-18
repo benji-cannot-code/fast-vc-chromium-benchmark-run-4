4557419c06e893a01bb82b6a4b24bc87473db99a
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-18 23:40:26 +0000

    2007-03-18  Mitz Pettel  <mitz@webkit.org>
            Reviewed by Tim Hatcher.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=13091
              REGRESSION (r20075): Wrong text style and pixel break in Apple Store Locator HUD
    
            Test: fast/innerHTML/additional-inline-style.html
    
            * editing/markup.cpp:
            (WebCore::startMarkup): Changed to add inline style based on CSS rules only
            in AnnotateForInterchange mode.
    
    2007-03-18  Mitz Pettel  <mitz@webkit.org>
    
            Reviewed by Tim Hatcher.
    
            - test and updated result for http://bugs.webkit.org/show_bug.cgi?id=13091
              REGRESSION (r20075): Wrong text style and pixel break in Apple Store Locator HUD
    
            * editing/pasteboard/paste-table-002-expected.txt:
            * fast/innerHTML/additional-inline-style-expected.txt: Added.
            * fast/innerHTML/additional-inline-style.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20297 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/markup.cpp
+++ third_party/WebKit/WebCore/editing/markup.cpp
@@ -191,7 +191,7 @@ static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnot
             const Element* el = static_cast<const Element*>(node);
             markup += el->nodeNamePreservingCase().deprecatedString();
             String additionalStyle;
-            if (el->isHTMLElement()) {
+            if (annotate && el->isHTMLElement()) {
                 RefPtr<CSSMutableStyleDeclaration> style = styleFromMatchedRulesForElement(const_cast<Element*>(el));
                 if (style->length() > 0)
                     additionalStyle = style->cssText();
@@ -202,7 +202,7 @@ static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnot
             for (unsigned int i = 0; i < length; i++) {
                 Attribute *attr = attrs->attributeItem(i);
                 String value = attr->value();
-                if (attr->name() == styleAttr && additionalStyle.length() > 0) {
+                if (annotate && attr->name() == styleAttr && additionalStyle.length()) {
                     value += "; " + additionalStyle;
                     additionalStyle = "";
                 }
@@ -214,7 +214,7 @@ static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnot
                 markup += "=\"" + escapeTextForMarkup(value.deprecatedString(), true) + "\"";
             }
             
-            if (additionalStyle.length() > 0)
+            if (annotate && additionalStyle.length())
                 // FIXME: Handle case where additionalStyle has illegal characters in it, like "
                 markup += " " +  styleAttr.localName().deprecatedString() + "=\"" + additionalStyle.deprecatedString() + "\"";
             
