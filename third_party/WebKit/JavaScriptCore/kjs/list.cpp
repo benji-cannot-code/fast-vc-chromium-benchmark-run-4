2625f26a0d3a3449dd6402e872f12e860eeb9557
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-19 23:50:43 +0000

    Reviewed by Geoff.
            - Changed list size threshold to 5 based on testing.
    
            I was testing the i-Bench JavaScript with the list statistics
            dumping on, and discovered that there were many 5-element lists.
            The fast case for lists was for 4 elements and fewer. By changing
            the threshold to 5 elements we get a measurable speedup. I believe
            this will help real web pages too, not just the benchmark.
    
            * kjs/list.cpp: Change constant from 4 to 5.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/list.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/list.cpp
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- *  This file is part of the KDE libraries
- *  Copyright (C) 2003 Apple Computer, Inc.
+ *  Copyright (C) 2003, 2004, 2005, 2006, 2007 Apple Inc. All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -34,7 +33,7 @@ namespace KJS {
 
 // tunable parameters
 const int poolSize = 512;
-const int inlineValuesSize = 4;
+const int inlineValuesSize = 5;
 
 enum ListImpState { unusedInPool = 0, usedInPool, usedOnHeap, immortal };
 
