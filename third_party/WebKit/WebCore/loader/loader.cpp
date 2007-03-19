03978ab0c4db79a310013aa6684cc552d9340f0b
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-19 00:37:19 +0000

    Reviewed by Hyatt
            http://bugs.webkit.org/show_bug.cgi?id=13111
            When stopped a load before it completes, partial images become the 
            broken image icon
    
            * loader/loader.cpp:
            (WebCore::Loader::didFail): Split off into a method that knows the 
              difference between failed and cancelled.  If a load is cancelled,
              don't call error() on the object - only remove it from the cache
            (WebCore::Loader::cancelRequests): Call didFail(loader, true)
            * loader/loader.h: Added didFail(SubresourceLoader*, bool cancelled)
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20299 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/loader.cpp
+++ third_party/WebKit/WebCore/loader/loader.cpp
@@ -114,7 +114,12 @@ void Loader::didFinishLoading(SubresourceLoader* loader)
     servePendingRequests();
 }
 
-void Loader::didFail(SubresourceLoader* loader, const ResourceError& error)
+void Loader::didFail(SubresourceLoader* loader, const ResourceError&)
+{
+    didFail(loader);
+}
+
+void Loader::didFail(SubresourceLoader* loader, bool cancelled)
 {
     RequestMap::iterator i = m_requestsLoading.find(loader);
     if (i == m_requestsLoading.end())
@@ -126,8 +131,11 @@ void Loader::didFail(SubresourceLoader* loader, const ResourceError& error)
     CachedResource* object = req->cachedResource();
     DocLoader* docLoader = req->docLoader();
 
-    docLoader->setLoadInProgress(true);
-    object->error();
+    if (!cancelled) {
+        docLoader->setLoadInProgress(true);
+        object->error();
+    }
+    
     docLoader->setLoadInProgress(false);
     cache()->remove(object);
 
@@ -222,7 +230,7 @@ void Loader::cancelRequests(DocLoader* dl)
 
     for (unsigned i = 0; i < loadersToCancel.size(); ++i) {
         SubresourceLoader* loader = loadersToCancel[i];
-        didFail(loader, loader->cancelledError());
+        didFail(loader, true);
     }
 }
 
