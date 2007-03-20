16ccc692efd9aa1a4e78028ff02f65bfeaeba8e9
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-20 00:27:55 +0000

    Rubberstamped by Oliver.
            * bridge/JavaScriptStatistics.cpp: Added missing header for
            correctness.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20332 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/JavaScriptStatistics.cpp
+++ third_party/WebKit/WebCore/bridge/JavaScriptStatistics.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <JavaScriptCore/JSLock.h>
 #include <JavaScriptCore/collector.h>
 #include <JavaScriptCore/interpreter.h>
+#include <pthread.h>
 
 namespace WebCore {
 
