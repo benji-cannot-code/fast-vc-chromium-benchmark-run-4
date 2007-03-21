31952a4a7d3ae90aa80d36876244cfc017847cb3
bdakin <bdakin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-21 07:24:22 +0000

    Fix for <rdar://problem/5072678> Crash in         RenderLayer::scrollRectToVisible with MallocScribble enabled
            I tried to fix this earlier today but my fix caused a regression
            scrolling through RSS pages. It turns out that sometimes scroll
            events need to propagate immediately, and sometimes they must be
            delayed or they risk deleting objects that are expected to be
            around after the event has propagated. Mitz's original fix made
            sheduleEvent() only delay events that happen during layout. This
            fix marks two other places in addition to layout where events also
            need to be delayed. These two places are places that were marked
            with FIXMEs that Mitz removed in his original patch. (There was a
            third FIXME in RenderLayer::updateScrollInfoAfterLayout() but that
            case is only called through layout and is covered by Mitz's
            original patch.)
    
            * page/FrameView.cpp:
            (WebCore::FrameView::~FrameView): Added assertion.
            (WebCore::FrameView::layout): Call new functions to increment and 
            decrement the queued events count.
            (WebCore::FrameView::pauseScheduledEvents):
            (WebCore::FrameView::resumeScheduledEvents): Decrement queued 
            events count and dispatch events if the count has zeroed.
            * page/FrameView.h:
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::scrollRectToVisible):
            (WebCore::Marquee::start):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -132,6 +132,7 @@ FrameView::~FrameView()
     resetScrollbars();
 
     ASSERT(m_refCount == 0);
+    ASSERT((!d->m_scheduledEvents || d->m_scheduledEvents.isEmpty()) && !d->m_enqueueEvents);
 
     if (m_frame) {
         ASSERT(m_frame->view() != this || !m_frame->document() || !m_frame->document()->renderer());
@@ -420,7 +421,7 @@ void FrameView::layout(bool allowSubtree)
         if (root->recalcMinMax())
             root->recalcMinMaxWidths();
     }
-    d->m_enqueueEvents++;
+    pauseScheduledEvents();
     root->layout();
     d->layoutRoot = 0;
 
@@ -466,9 +467,8 @@ void FrameView::layout(bool allowSubtree)
         updateOverflowStatus(visibleWidth() < contentsWidth(),
                              visibleHeight() < contentsHeight());
 
-    // Dispatch events scheduled during layout
-    dispatchScheduledEvents();
-    d->m_enqueueEvents--;
+    // Allow events scheduled during layout to fire
+    resumeScheduledEvents();
 }
 
 //
@@ -752,6 +752,20 @@ void FrameView::scheduleEvent(PassRefPtr<Event> event, PassRefPtr<EventTargetNod
     scheduledEvent->m_eventTarget = eventTarget;
     scheduledEvent->m_tempEvent = tempEvent;
     d->m_scheduledEvents.append(scheduledEvent);
+}
+
+void FrameView::pauseScheduledEvents()
+{
+    ASSERT(!d->m_scheduledEvents || d->m_scheduledEvents.isEmpty() || d->m_enqueueEvents);
+    d->m_enqueueEvents++;
+}
+
+void FrameView::resumeScheduledEvents()
+{
+    d->m_enqueueEvents--;
+    if (!d->m_enqueueEvents)
+        dispatchScheduledEvents();
+    ASSERT(!d->m_scheduledEvents || d->m_scheduledEvents.isEmpty() || d->m_enqueueEvents);
 }
 
 void FrameView::updateOverflowStatus(bool horizontalOverflow, bool verticalOverflow)
