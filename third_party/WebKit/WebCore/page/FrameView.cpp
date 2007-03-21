7ebd5ee74664f36b90c065215fa2d5a392e993ab
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-21 01:24:02 +0000

    Rubberstamped by Adele.
            Roll out r20348 because it was causing crashes.
    
            * page/FrameView.cpp:
            (WebCore::FrameViewPrivate::FrameViewPrivate):
            (WebCore::FrameView::layout):
            (WebCore::FrameView::scheduleEvent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20355 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -56,6 +56,7 @@ public:
     FrameViewPrivate(FrameView* view)
         : layoutTimer(view, &FrameView::layoutTimerFired)
         , m_mediaType("screen")
+        , m_enqueueEvents(0)
         , m_overflowStatusDirty(true)
         , m_viewportRenderer(0)
     {
@@ -108,6 +109,7 @@ public:
     
     String m_mediaType;
     
+    unsigned m_enqueueEvents;
     Vector<ScheduledEvent*> m_scheduledEvents;
     
     bool m_overflowStatusDirty;
@@ -418,6 +420,7 @@ void FrameView::layout(bool allowSubtree)
         if (root->recalcMinMax())
             root->recalcMinMaxWidths();
     }
+    d->m_enqueueEvents++;
     root->layout();
     d->layoutRoot = 0;
 
@@ -465,6 +468,7 @@ void FrameView::layout(bool allowSubtree)
 
     // Dispatch events scheduled during layout
     dispatchScheduledEvents();
+    d->m_enqueueEvents--;
 }
 
 //
@@ -737,6 +741,12 @@ void FrameView::setBaseBackgroundColor(Color bc)
 
 void FrameView::scheduleEvent(PassRefPtr<Event> event, PassRefPtr<EventTargetNode> eventTarget, bool tempEvent)
 {
+    if (!d->m_enqueueEvents) {
+        ExceptionCode ec = 0;
+        eventTarget->dispatchEvent(event, ec, tempEvent);
+        return;
+    }
+
     ScheduledEvent* scheduledEvent = new ScheduledEvent;
     scheduledEvent->m_event = event;
     scheduledEvent->m_eventTarget = eventTarget;
