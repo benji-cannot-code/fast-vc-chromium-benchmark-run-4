b558ba0c6a44f0e1b6a74a153368e4a66cb1025c
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-22 23:57:54 +0000

    Reviewed by Adele and John
            <rdar://5043528> - Prepare for new API
            
            * platform/network/ResourceHandle.cpp:
            (WebCore::ResourceHandle::clearAuthentication): Clear CF type
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20411 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceHandle.cpp
+++ third_party/WebKit/WebCore/platform/network/ResourceHandle.cpp
@@ -103,6 +103,8 @@ void ResourceHandle::clearAuthentication()
 {
 #if PLATFORM(MAC)
     d->m_currentMacChallenge = nil;
+#elif PLATFORM(CF)
+    d->m_currentCFChallenge = 0;
 #endif
     d->m_currentWebChallenge.nullify();
 }
