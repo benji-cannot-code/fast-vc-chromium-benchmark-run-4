cc1ec0cbe280d3e1ce464073bdadd9e2f797b633
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-22 09:21:11 +0000

    LayoutTests:
            Reviewed by Ada.
    
            Added a new test to make sure that eventSender.mouseClick works
            correctly.
    
            * fast/events/click-count-expected.txt: Added.
            * fast/events/click-count.html: Added.
    
            Updated results for some tests that depended on the broken version of
            eventSender.mouseClick.
    
            * fast/events/objc-event-api-expected.txt:
            * fast/forms/focus-selection-input-expected.txt:
            * fast/forms/focus-selection-textarea-expected.txt:
    
    WebKitTools:
    
            Reviewed by Ada.
    
            Make eventSender.mouseClick actually send a mouse up event.
    
            * DumpRenderTree/EventSendingController.m:
            (-[EventSendingController mouseClick]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20386 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/EventSendingController.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/EventSendingController.m
@@ -319,8 +319,8 @@ BOOL replayingSavedEvents;
                                                 eventNumber:++eventNumber
                                                  clickCount:clickCount
                                                    pressure:0.0];
-        [NSApp postEvent:mouseUpEvent atStart:NO];
         [subView mouseDown:mouseDownEvent];
+        [subView mouseUp:mouseUpEvent];
         lastClick = [mouseUpEvent timestamp];
     }
 }
