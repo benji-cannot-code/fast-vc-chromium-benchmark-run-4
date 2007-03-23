46487ecea85a308dc486b9124b87ae64c38f9195
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-23 22:26:42 +0000

    Reviewed/landed by Adam.
            Bug 13175: Make apollo mac project files for JavaScriptCore actually
            build something
            http://bugs.webkit.org/show_bug.cgi?id=13175
    
            - Changing apollo mac project files for JavaScriptCore such that they actually build
            JavaScriptCore source code.
    
            * JavaScriptCore.apolloproj/ForwardingSources/grammar.cpp: Added.
            * JavaScriptCore.apolloproj/mac/JavaScriptCore/JavaScriptCore.xcconfig:
            * JavaScriptCore.apolloproj/mac/JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20455 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/JavaScriptCore/JavaScriptCore.apolloproj/ForwardingSources/grammar.cpp
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// This file includes a derived source file from the build output tree.
+// Since the location of the derived sources depends on which configuration
+// we are building, we need to use the include path ( which can be
+// different for each configuration ) to reference the derived source file.
+#include <DerivedSources/grammar.cpp>
