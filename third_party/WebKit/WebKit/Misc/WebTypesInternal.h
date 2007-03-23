369f841ae92b536bf2a6b2ecb97ec594ae2a44ed
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-23 09:25:58 +0000

    2007-03-23  Mark Rowe  <mrowe@apple.com>
            Build fix for when BUILDING_ON_TIGER is not defined.
    
            * Misc/WebTypesInternal.h:
            * WebView/WebHTMLView.mm:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20427 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc/WebTypesInternal.h
+++ third_party/WebKit/WebKit/Misc/WebTypesInternal.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#if BUILDING_ON_TIGER
+#ifdef BUILDING_ON_TIGER
 typedef int WebNSInteger;
 typedef unsigned int WebNSUInteger;
 #else
