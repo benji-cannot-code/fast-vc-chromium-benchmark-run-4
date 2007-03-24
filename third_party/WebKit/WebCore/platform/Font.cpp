eb12d6c39223e2b21565f3ad1d72c352841a3297
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-24 06:24:59 +0000

    Fix Gmail spurious glyphs issue.
    git-svn-id: svn://svn.chromium.org/blink/trunk@20460 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Font.cpp
+++ third_party/WebKit/WebCore/platform/Font.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "Font.h"
 
+#include "CharacterNames.h"
 #include "FloatRect.h"
 #include "FontCache.h"
 #include "FontFallbackList.h"
@@ -410,7 +411,13 @@ const GlyphData& Font::glyphDataForCharacter(UChar32 c, const UChar* cluster, un
 
         if (node->isSystemFallback()) {
             // System fallback is character-dependent.
-            const FontData* characterFontData = FontCache::getFontDataForCharacters(*this, cluster, clusterLength);
+            // Convert characters that shouldn't render to zero width spaces when asking what font is
+            // appropriate.
+            const FontData* characterFontData;
+            if (clusterLength == 1 && Font::treatAsZeroWidthSpace(cluster[0]))
+                characterFontData = FontCache::getFontDataForCharacters(*this, &zeroWidthSpace, 1);
+            else
+                characterFontData = FontCache::getFontDataForCharacters(*this, cluster, clusterLength);
             if (smallCaps)
                 characterFontData = characterFontData->smallCapsFontData(m_fontDescription);
             if (characterFontData) {
