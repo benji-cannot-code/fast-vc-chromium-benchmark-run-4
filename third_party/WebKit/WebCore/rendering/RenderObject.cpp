b84ea38f2ee3412727ad416241f5f63eeeb83ece
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-24 07:07:24 +0000

    Fix for <rdar://problem/508328>, sluggish text entry in search field.  Make sure         our size actually changed before we decide to do a full repaint because of background/border         complexities.
            Reviewed by aroben
    
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::repaintAfterLayoutIfNeeded):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20461 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/WebCore/rendering/RenderObject.cpp
@@ -1744,10 +1744,12 @@ bool RenderObject::repaintAfterLayoutIfNeeded(const IntRect& oldBounds, const In
     IntRect newBounds = absoluteClippedOverflowRect();
     IntRect newOutlineBox;
 
-    bool fullRepaint = selfNeedsLayout() || mustRepaintBackgroundOrBorder();
+    bool fullRepaint = selfNeedsLayout();
     if (!fullRepaint) {
         newOutlineBox = absoluteOutlineBox();
         if (newOutlineBox.location() != oldOutlineBox.location())
+            fullRepaint = true;
+        else if (mustRepaintBackgroundOrBorder() && (newBounds != oldBounds || newOutlineBox != oldOutlineBox))
             fullRepaint = true;
     }
     if (fullRepaint) {
