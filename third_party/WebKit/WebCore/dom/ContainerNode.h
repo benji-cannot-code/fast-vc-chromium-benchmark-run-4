d6865697b78121280badaea55cc321dc0bbd06f1
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-25 08:39:27 +0000

    Reviewed by Darin.
            A partial fix for http://bugs.webkit.org/show_bug.cgi?id=13021
            XPath can be very slow
    
            * xml/XPathExpression.cpp:
            (WebCore::XPathExpression::evaluate): Reset a reference to the context node, as this may prevent the whole document
            from being destroyed in time.
    
            * dom/Attr.cpp:
            (WebCore::Attr::createTextChild): Instead of calling appendChild(), just do the few operations it really needs to perform.
            * dom/ContainerNode.h:
            (WebCore::ContainerNode::fastSetFirstChild):
            (WebCore::ContainerNode::fastSetLastChild):
            Added operations that let Attr hack internal ContainerNode data (evil, but fast!).
    
            * xml/XPathStep.cpp:
            (WebCore::XPath::Step::evaluate):
            (WebCore::XPath::Step::nodesInAxis):
            (WebCore::XPath::Step::nodeMatches):
            * xml/XPathStep.h:
            Merged node testing into axis enumeration. This saves a lot of Vector resizing and passing, and is necessary for future 
            optimizations (sometimes, we can just pick the single result node instead of enumerating and filtering the whole axis).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20487 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/ContainerNode.h
+++ third_party/WebKit/WebCore/dom/ContainerNode.h
@@ -66,7 +66,7 @@ public:
 
     Node* fastFirstChild() const { return m_firstChild; }
     Node* fastLastChild() const { return m_lastChild; }
-    
+
     void removeAllChildren();
     void removeChildren();
     void cloneChildNodes(Node* clone);
@@ -74,6 +74,9 @@ public:
 protected:
     static void queuePostAttachCallback(NodeCallback, Node*);
 
+    void fastSetFirstChild(Node* child) { m_firstChild = child; }
+    void fastSetLastChild(Node* child) { m_lastChild = child; }
+    
 private:
     Node* m_firstChild;
     Node* m_lastChild;
