6548008f5a34e855f43000aa8d74e248649b3a2d
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-27 22:55:03 +0000

    LayoutTests:
            Reviewed by darin
            
            <rdar://problem/5091898> REGRESSION: GMail Editor: A line of underlined text copied with Select All pastes with wrong font family
    
            Demonstrates the bug fixed:
            * editing/style/5091898-expected.checksum: Added.
            * editing/style/5091898-expected.png: Added.
            * editing/style/5091898-expected.txt: Added.
            * editing/style/5091898.html: Added.
            Demonstrates a related bug fixed (deleting
            the line break before a paragraph could remove
            its underlining):
            * editing/deleting/5091898-expected.checksum: Added.
            * editing/deleting/5091898-expected.png: Added.
            * editing/deleting/5091898-expected.txt: Added.
            * editing/deleting/5091898.html: Added.
            Demonstrates that a link now (correctly) exists, 
            instead of just underlined text:
            * editing/pasteboard/4840662-expected.txt:
    
    WebCore:
    
            Reviewed by darin
            
            <rdar://problem/5091898> REGRESSION: GMail Editor: A line of underlined text copied with Select All pastes with wrong font family
            
            createMarkup skipped elements if they were blocks
            when called from moveParagraphs (because that function 
            must receive only inline content).  This patch adds 
            code to inline these blocks instead of skipping them 
            so that we don't lose any of the style that they 
            contribute to the copied markup.
    
            * editing/markup.cpp:
            (WebCore::startMarkup): Add an inlineBlocks option.
            Make sure to overwrite display:block coming from
            a style sheet or the inline style declaration.
            (WebCore::createMarkup): Don't refuse to include a 
            specialCommonAncestor that's a block if we were asked 
            to include only inline content, since we can now inline 
            block elements in startMarkup.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20542 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/markup.cpp
+++ third_party/WebKit/WebCore/editing/markup.cpp
@@ -155,7 +155,7 @@ static void removeEnclosingMailBlockquoteStyle(CSSMutableStyleDeclaration* style
     blockquoteStyle->diff(style);
 }
 
-static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnotateForInterchange annotate)
+static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnotateForInterchange annotate, bool convertBlocksToInlines = false)
 {
     bool documentIsHTML = node->document()->isHTMLDocument();
     switch (node->nodeType()) {
@@ -190,10 +190,13 @@ static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnot
         case Node::ELEMENT_NODE: {
             DeprecatedString markup = DeprecatedChar('<');
             const Element* el = static_cast<const Element*>(node);
+            convertBlocksToInlines &= isBlock(const_cast<Node*>(node));
             markup += el->nodeNamePreservingCase().deprecatedString();
             String additionalStyle;
             if (annotate && el->isHTMLElement()) {
                 RefPtr<CSSMutableStyleDeclaration> style = styleFromMatchedRulesForElement(const_cast<Element*>(el));
+                if (convertBlocksToInlines)
+                    style->setProperty(CSS_PROP_DISPLAY, CSS_VAL_INLINE, true);
                 if (style->length() > 0)
                     additionalStyle = style->cssText();
             }
@@ -203,6 +206,11 @@ static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnot
             for (unsigned int i = 0; i < length; i++) {
                 Attribute *attr = attrs->attributeItem(i);
                 String value = attr->value();
+                if (attr->name() == styleAttr && convertBlocksToInlines) {
+                    RefPtr<CSSMutableStyleDeclaration> inlineStyle = static_cast<const HTMLElement*>(el)->getInlineStyleDecl()->copy();
+                    inlineStyle->setProperty(CSS_PROP_DISPLAY, CSS_VAL_INLINE, true);
+                    value = inlineStyle->cssText();
+                }
                 if (annotate && attr->name() == styleAttr && additionalStyle.length()) {
                     value += "; " + additionalStyle;
                     additionalStyle = "";
@@ -352,7 +360,7 @@ bool elementHasTextDecorationProperty(Node* node)
 
 // FIXME: Shouldn't we omit style info when annotate == DoNotAnnotateForInterchange? 
 // FIXME: At least, annotation and style info should probably not be included in range.markupString()
-DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotateForInterchange annotate, bool includeInlineSpecialAncestors)
+DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotateForInterchange annotate, bool convertBlocksToInlines)
 {
     if (!range || range->isDetached())
         return DeprecatedString();
@@ -487,9 +495,6 @@ DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotat
         
     if (Node *enclosingAnchor = enclosingNodeWithTag(specialCommonAncestor ? specialCommonAncestor : commonAncestor, aTag))
         specialCommonAncestor = enclosingAnchor;
-        
-    if (!annotate && includeInlineSpecialAncestors && isBlock(specialCommonAncestor))
-        specialCommonAncestor = 0;
     
     Node* body = enclosingNodeWithTag(commonAncestor, bodyTag);
     // FIXME: Only include markup for a fully selected root (and ancestors of lastClosed up to that root) if
@@ -502,9 +507,7 @@ DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotat
     if (specialCommonAncestor) {
         // Also include all of the ancestors of lastClosed up to this special ancestor.
         for (Node* ancestor = lastClosed->parentNode(); ancestor; ancestor = ancestor->parentNode()) {
-            if (!annotate && includeInlineSpecialAncestors && isBlock(ancestor))
-                continue;
-            if (ancestor == fullySelectedRoot) {
+            if (ancestor == fullySelectedRoot && !convertBlocksToInlines) {
                 RefPtr<CSSMutableStyleDeclaration> style = styleFromMatchedRulesAndInlineDecl(fullySelectedRoot);
                 
                 // Bring the background attribute over, but not as an attribute because a background attribute on a div
@@ -517,7 +520,7 @@ DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotat
                     markups.append("</div>");
                 }
             } else {
-                markups.prepend(startMarkup(ancestor, range, annotate));
+                markups.prepend(startMarkup(ancestor, range, annotate, convertBlocksToInlines));
                 markups.append(endMarkup(ancestor));
             }
             if (nodes)
