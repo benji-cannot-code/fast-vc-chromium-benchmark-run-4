de6843cf48fe2ad02b57a635cb04f0bb137bf73a
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-28 08:41:04 +0000

    2007-03-28  Oliver Hunt  <oliver@apple.com>
            rs=Hyatt.
    
            Rollout local changes :-/
    
            * loader/Cache.cpp:
            (WebCore::Cache::pruneAllResources):
            (WebCore::Cache::remove):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20551 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/Cache.cpp
+++ third_party/WebKit/WebCore/loader/Cache.cpp
@@ -43,7 +43,7 @@ using namespace std;
 
 namespace WebCore {
 
-const int cDefaultCacheSize = 1000;// 8192 * 1024;
+const int cDefaultCacheSize = 8192 * 1024;
 
 Cache* cache()
 {
@@ -155,7 +155,6 @@ void Cache::pruneLiveResources()
 
 void Cache::pruneAllResources()
 {
-    m_maximumSize = 1000;
     // No need to prune if all of our objects fit.
     if (m_currentSize <= m_maximumSize)
         return;
@@ -235,12 +234,12 @@ void Cache::remove(CachedResource* resource)
         if (resource->referenced())
             removeFromLiveResourcesList(resource);
         
-        // Notify all doc loaders that might be observing this object still that it has been 
-        // extracted from the set of resources. 
-        HashSet<DocLoader*>::iterator end = m_docLoaders.end(); 
-        for (HashSet<DocLoader*>::iterator itr = m_docLoaders.begin(); itr != end; ++itr) 
-            (*itr)->removeCachedResource(resource); 
-        
+        // Notify all doc loaders that might be observing this object still that it has been
+        // extracted from the set of resources.
+        HashSet<DocLoader*>::iterator end = m_docLoaders.end();
+        for (HashSet<DocLoader*>::iterator itr = m_docLoaders.begin(); itr != end; ++itr)
+            (*itr)->removeCachedResource(resource);
+
         // Subtract from our size totals.
         int delta = -resource->size();
         if (delta)
