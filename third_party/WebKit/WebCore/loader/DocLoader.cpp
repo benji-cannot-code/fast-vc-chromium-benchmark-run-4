9ef2d7b278df806b6ff89d1e3a5a714e246f2674
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-03-29 06:15:27 +0000

    2007-03-28  Oliver Hunt  <oliver@apple.com>
            Reviewed by Hyatt.
    
            Rename DocLoader::setPasteInProgress, etc to more generic 
            setAllowStaleResources, etc
    
            * editing/Editor.cpp:
            (WebCore::Editor::paste):
            * loader/DocLoader.cpp:
            (WebCore::DocLoader::DocLoader):
            (WebCore::DocLoader::checkForReload):
            * loader/DocLoader.h:
            (WebCore::DocLoader::setAllowStaleResources):
            * page/DragController.cpp:
            (WebCore::DragController::concludeDrag):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/DocLoader.cpp
+++ third_party/WebKit/WebCore/loader/DocLoader.cpp
@@ -49,7 +49,7 @@ DocLoader::DocLoader(Frame *frame, Document* doc)
     , m_doc(doc)
     , m_autoLoadImages(true)
     , m_loadInProgress(false)
-    , m_pasteInProgress(false)
+    , m_allowStaleResources(false)
 {
     m_cache->addDocLoader(this);
 }
@@ -66,7 +66,7 @@ void DocLoader::setExpireDate(time_t _expireDate)
 
 void DocLoader::checkForReload(const KURL& fullURL)
 {
-    if (m_pasteInProgress)
+    if (m_allowStaleResources)
         return; //Don't reload resources while pasting
     if (m_cachePolicy == CachePolicyVerify) {
        if (!m_reloadedURLs.contains(fullURL.url())) {
--- third_party/WebKit/WebCore/page/DragController.cpp
+++ third_party/WebKit/WebCore/page/DragController.cpp
@@ -352,12 +352,12 @@ bool DragController::concludeDrag(DragData* dragData, DragDestinationAction acti
     m_page->dragCaretController()->clear();
     RefPtr<Range> range = dragCaret.toRange();
     DocLoader* loader = range->ownerDocument()->docLoader();
-    loader->setPasteInProgress(true);
+    loader->setAllowStaleResources(true);
     if (dragIsMove(innerFrame->selectionController(), dragData) || dragCaret.isContentRichlyEditable()) { 
         bool chosePlainText = false;
         RefPtr<DocumentFragment> fragment = documentFragmentFromDragData(dragData, range, true, chosePlainText);
         if (!fragment || !innerFrame->editor()->shouldInsertFragment(fragment, range, EditorInsertActionDropped)) {
-            loader->setPasteInProgress(false);
+            loader->setAllowStaleResources(false);
             return false;
         }
         
@@ -374,7 +374,7 @@ bool DragController::concludeDrag(DragData* dragData, DragDestinationAction acti
     } else {
         String text = dragData->asPlainText();
         if (text.isEmpty() || !innerFrame->editor()->shouldInsertText(text, range.get(), EditorInsertActionDropped)) {
-            loader->setPasteInProgress(false);
+            loader->setAllowStaleResources(false);
             return false;
         }
         
@@ -382,7 +382,7 @@ bool DragController::concludeDrag(DragData* dragData, DragDestinationAction acti
         if (setSelectionToDragCaret(innerFrame, dragCaret, range, point))
             applyCommand(new ReplaceSelectionCommand(m_document, createFragmentFromText(range.get(), text), true, false, true)); 
     }
-    loader->setPasteInProgress(false);
+    loader->setAllowStaleResources(false);
 
     return true;
 }
