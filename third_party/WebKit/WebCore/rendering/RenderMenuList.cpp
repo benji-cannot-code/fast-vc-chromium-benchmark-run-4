600645b41475a9647a869be2efd67e18f42d2fdb
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-01 05:20:58 +0000

    Fix for bug 13234, layout of selected justified text is broken.  Rework TextRuns to avoid having         from/to members.  Let those be passed in only by the functions that need them (drawing and selection rect).
            Reviewed by mitz
    
            fast/text/justified-selection-at-edge.html
    
            * platform/Font.cpp:
            (WebCore::WidthIterator::WidthIterator):
            (WebCore::WidthIterator::advance):
            (WebCore::Font::canUseGlyphCache):
            (WebCore::Font::drawSimpleText):
            (WebCore::Font::drawText):
            (WebCore::Font::floatWidth):
            (WebCore::Font::floatWidthForSimpleText):
            (WebCore::Font::selectionRectForText):
            (WebCore::Font::selectionRectForSimpleText):
            (WebCore::Font::offsetForPositionForSimpleText):
            * platform/Font.h:
            (WebCore::TextRun::m_len):
            * platform/graphics/GraphicsContext.cpp:
            (WebCore::GraphicsContext::drawText):
            (WebCore::GraphicsContext::drawHighlightForText):
            * platform/graphics/GraphicsContext.h:
            * platform/mac/FontMac.mm:
            (WebCore::addDirectionalOverride):
            (WebCore::overrideLayoutOperation):
            (WebCore::ATSULayoutParameters::initialize):
            (WebCore::Font::selectionRectForComplexText):
            (WebCore::Font::drawComplexText):
            (WebCore::Font::floatWidthForComplexText):
            (WebCore::Font::offsetForPositionForComplexText):
            * platform/qt/FontQt.cpp:
            (WebCore::Font::drawComplexText):
            (WebCore::Font::floatWidthForComplexText):
            * platform/qt/TemporaryLinkStubs.cpp:
            (Font::selectionRectForComplexText):
            * rendering/EllipsisBox.cpp:
            (WebCore::EllipsisBox::paint):
            * rendering/InlineTextBox.cpp:
            (WebCore::InlineTextBox::selectionRect):
            (WebCore::InlineTextBox::paint):
            (WebCore::InlineTextBox::paintSelection):
            (WebCore::InlineTextBox::paintMarkedTextBackground):
            (WebCore::InlineTextBox::paintSpellingOrGrammarMarker):
            (WebCore::InlineTextBox::paintTextMatchMarker):
            (WebCore::InlineTextBox::offsetForPosition):
            (WebCore::InlineTextBox::positionForOffset):
            * rendering/RenderListMarker.cpp:
            (WebCore::RenderListMarker::paint):
            (WebCore::RenderListMarker::calcMinMaxWidth):
            (WebCore::RenderListMarker::getRelativeMarkerRect):
            * rendering/RenderMenuList.cpp:
            (WebCore::RenderMenuList::updateOptionsWidth):
            * rendering/RenderText.cpp:
            (WebCore::RenderText::widthFromCache):
            (WebCore::RenderText::width):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20646 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderMenuList.cpp
+++ third_party/WebKit/WebCore/rendering/RenderMenuList.cpp
@@ -122,7 +122,7 @@ void RenderMenuList::updateOptionsWidth()
         if (element->hasTagName(optionTag)) {
             String text = static_cast<HTMLOptionElement*>(element)->optionText();
             if (!text.isEmpty())
-                maxOptionWidth = max(maxOptionWidth, style()->font().floatWidth(text.impl(), textStyle));
+                maxOptionWidth = max(maxOptionWidth, style()->font().floatWidth(text, textStyle));
         }
     }
 
