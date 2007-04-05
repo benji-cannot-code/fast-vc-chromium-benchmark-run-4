2a0a674ffccf239bc31ebccbd59627ab6c2e9564
kjk <kjk@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-05 22:33:48 +0000

    Reviewed by darin.
            Coverity fix: in single-threaded case currentThreadIsMainThread is always true
            so the code in if (!currentThreadIsMainThread) cannot possibly be reached
            and Coverity complains about dead code.
    
            * kjs/collector.cpp:
            (KJS::Collector::collect):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20733 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/collector.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/collector.cpp
@@ -745,8 +745,10 @@ bool Collector::collect()
   markStackObjectsConservatively();
   markProtectedObjects();
   List::markProtectedLists();
+#if USE(MULTIPLE_THREADS)
   if (!currentThreadIsMainThread)
     markMainThreadOnlyObjects();
+#endif
 
 #ifndef NDEBUG
   fastMallocAllow();
