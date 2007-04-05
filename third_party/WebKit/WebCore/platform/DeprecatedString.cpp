8e4e097744921ff9bae5767e04717164cc0f739a
kjk <kjk@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-05 22:38:20 +0000

    Reviewed by darin.
            Coverity fix. Coverity says:
            "Event var_deref_model: Variable "unicode" tracked as NULL was passed to a 
            function that dereferences it."
    
            * platform/DeprecatedString.cpp:
            (WebCore::DeprecatedString::DeprecatedString):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/DeprecatedString.cpp
+++ third_party/WebKit/WebCore/platform/DeprecatedString.cpp
@@ -667,7 +667,7 @@ DeprecatedString::DeprecatedString(DeprecatedChar qc)
 
 DeprecatedString::DeprecatedString(const DeprecatedChar *unicode, unsigned length)
 {
-    if (!unicode && !length) {
+    if (!unicode || !length) {
         internalData.deref();
         dataHandle = makeSharedNullHandle();
         dataHandle[0]->ref();
