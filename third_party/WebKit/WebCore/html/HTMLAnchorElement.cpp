f5bd400f3ab1f44f03614b92642be051afd7a643
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-05 20:36:33 +0000

    Fixing typo that's crashing.
            * html/HTMLAnchorElement.cpp: (WebCore::HTMLAnchorElement::defaultEventHandler):
              Use evt instead of null variable k.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLAnchorElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLAnchorElement.cpp
@@ -132,7 +132,7 @@ void HTMLAnchorElement::defaultEventHandler(Event* evt)
             e = static_cast<MouseEvent*>(evt);
 
         KeyboardEvent* k = 0;
-        if (evt->type() == keydownEvent && k->isKeyboardEvent())
+        if (evt->type() == keydownEvent && evt->isKeyboardEvent())
             k = static_cast<KeyboardEvent*>(evt);
 
         if (e && e->button() == 2) {
