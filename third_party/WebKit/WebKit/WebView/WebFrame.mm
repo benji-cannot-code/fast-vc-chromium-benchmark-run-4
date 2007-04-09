62615eab786ad8e98bf8ea8fc52750818cb3425f
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-09 22:52:11 +0000

    Reviewed by Geoff, Ada and John.
            <rdar://problem/4600978> Would like a way to test whether a WebView is displaying a standalone image
    
            * WebView/WebFrame.mm:
            (-[WebFrame _isDisplayingStandaloneImage]):
            * WebView/WebFramePrivate.h:
            Add _isDisplayingStandaloneImage SPI.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20811 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebFrame.mm
+++ third_party/WebKit/WebKit/WebView/WebFrame.mm
@@ -646,6 +646,12 @@ static inline WebDataSource *dataSource(DocumentLoader* loader)
     [_private->bridge selectNSRange:range];
 }
 
+- (BOOL)_isDisplayingStandaloneImage
+{
+    Document* document = core(self)->document();
+    return document && document->isImageDocument();
+}
+
 @end
 
 @implementation WebFrame
