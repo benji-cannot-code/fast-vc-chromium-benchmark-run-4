b655117e08cc710ab982b60e78214489bfb61d99
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-10 00:00:21 +0000

    LayoutTests:
            Reviewed by Darin
    
            Layout test for the fix for <rdar://4921797> and http://bugs.webkit.org/show_bug.cgi?id=12005
    
            * http/tests/navigation/multiple-back-forward-entries-expected.txt: Added.
            * http/tests/navigation/multiple-back-forward-entries.html: Added.
            * http/tests/navigation/resources/slow-resource.pl: Added.
    
    WebCore:
    
            Reviewed by Darin
    
            Fixes <rdar://4921797> and http://bugs.webkit.org/show_bug.cgi?id=12005
    
            The original regression was to claim that more loads were the result of a "user gesture" than really
            were.  A lot of the ways a frame load could be kicked off didn't properly set up this flag, and it 
            wasn't properly propagated and respected where it should've been.
    
            This patch cleans much of that up.  One loose end is the "treatAsUserGesture" flag which is a stop
            gap measure to keep "slow redirects" working to create a new history item.  In the future, we need
            to cleanup the meaning and use of "userGesture" and "lockHistory."  This includes integrating them 
            in to FrameLoadRequest and being very clear of what their meaning actually is at different stages of
            the Frame load process.
    
            * dom/Document.cpp:
            (WebCore::Document::processHttpEquiv): Pass only the delay for the redirect
    
            * html/HTMLAnchorElement.cpp:
            (WebCore::HTMLAnchorElement::defaultEventHandler): Pass "lockHistory" false, "userGesture" true
    
            * ksvg2/svg/SVGAElement.cpp:
            (WebCore::SVGAElement::defaultEventHandler): Pass "lockHistory" false, "userGesture" true
    
            * loader/FrameLoader.cpp:
            (WebCore::ScheduledRedirection::ScheduledRedirection): Figure "lockHistory" and "userGesture" from the 
              delay here, instead of at 3 other different sites that call this method
            (WebCore::FrameLoader::changeLocation): Set userGesture correctly
            (WebCore::FrameLoader::urlSelected): Propagate userGesture down
            (WebCore::FrameLoader::requestFrame): 
            (WebCore::FrameLoader::receivedFirstData):
            (WebCore::FrameLoader::scheduleRedirection): Pass only the delay here
            (WebCore::FrameLoader::redirectionTimerFired): Set userGesture correctly
            (WebCore::FrameLoader::load):
            (WebCore::FrameLoader::updateHistoryForInternalLoad): Insteading of asserting we aren't a redirect,
              handle the case where we *are* a redirect by updating the previous history item
            * loader/FrameLoader.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20813 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/WebCore/loader/FrameLoader.h
@@ -265,8 +265,8 @@ namespace WebCore {
         void setDefersLoading(bool);
 
         void changeLocation(const String& URL, const String& referrer, bool lockHistory = true, bool userGesture = false);
-        void urlSelected(const ResourceRequest&, const String& target, Event*, bool lockHistory = false);
-        void urlSelected(const FrameLoadRequest&, Event*);
+        void urlSelected(const ResourceRequest&, const String& target, Event*, bool lockHistory, bool userGesture);
+        void urlSelected(const FrameLoadRequest&, Event*, bool userGesture);
       
         bool requestFrame(HTMLFrameOwnerElement*, const String& URL, const AtomicString& frameName);
         Frame* loadSubframe(HTMLFrameOwnerElement*, const KURL& URL, const String& name, const String& referrer);
@@ -288,7 +288,7 @@ namespace WebCore {
         String baseTarget() const;
         KURL dataURLBaseFromRequest(const ResourceRequest& request) const;
 
-        void scheduleRedirection(double delay, const String& URL, bool lockHistory = true);
+        void scheduleRedirection(double delay, const String& URL);
 
         void scheduleLocationChange(const String& URL, const String& referrer, bool lockHistory = true, bool userGesture = false);
         void scheduleRefresh(bool userGesture = false);
