1c3205546a6f330773d1cea2aac8f9e18e190717
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-11 18:22:20 +0000

    Reviewed by Antti
            <rdar://problem/5125648> and http://bugs.webkit.org/show_bug.cgi?id=13326
    
            Crash opening a link in a new window from Gmail
    
            Problem here is that the currentHistoryItem in the new window is null.  This *shouldn't* be the case, 
            and is the cause of a very long standing bug - 
            http://bugs.webkit.org/show_bug.cgi?id=3546 - Can't go back to first page in new window opened from gmail
    
            For now, adding a null check prevents this crash and restores the original, long standing regression
     
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::updateHistoryForInternalLoad): NULL check
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -4031,8 +4031,10 @@ void FrameLoader::updateHistoryForInternalLoad()
 #endif
     
     if (documentLoader()->isClientRedirect()) {
-        m_currentHistoryItem->setURL(documentLoader()->URL());
-        m_currentHistoryItem->setFormInfoFromRequest(documentLoader()->request());
+        if (m_currentHistoryItem) {
+            m_currentHistoryItem->setURL(documentLoader()->URL());
+            m_currentHistoryItem->setFormInfoFromRequest(documentLoader()->request());
+        }
     } else {
         // Add an item to the item tree for this frame
         Frame* parentFrame = m_frame->tree()->parent();
