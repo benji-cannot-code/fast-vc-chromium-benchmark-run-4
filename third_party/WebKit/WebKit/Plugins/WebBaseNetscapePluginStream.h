41a6011701fb67422b496cb517bd4b2524c1c767
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-13 04:32:51 +0000

    JavaScriptCore:
            Reviewed by Darin Adler.
    
            http://bugs.webkit.org/show_bug.cgi?id=13029
            rdar://problem/4994849
            Bug 13029: Permit NPAPI plug-ins to see HTTP response headers.
            This doesn't actually change JavaScriptCore, but that's where npapi.h is.
    
            * bindings/npapi.h:
            Add headers member to NPStream struct.  Also increase NP_VERSION_MINOR to 18.
            Increasing to >= 17 allows plug-ins to safely detect whether to look for
            NPStream::headers.  Increasing from 17 to 18 reflects presence of NPObject
            enumeration, which was added in a prior patch, and which has been agreed to
            constitute version 18 by the plugin-futures list.  Also add other missing
            bits of npapi.h to catch up from 14 to 18.  This includes features that are
            not implemented in WebKit, but those are safely stubbed.
    
    LayoutTests:
    
            Reviewed by Darin Adler.
    
            http://bugs.webkit.org/show_bug.cgi?id=13029
            rdar://problem/4994849
            Bug 13029: Permit NPAPI plug-ins to see HTTP response headers.
    
            * http/tests/plugins/npapi-response-headers-expected.txt: Added.
            * http/tests/plugins/npapi-response-headers.html: Added.
            * http/tests/plugins/resources/load-me-1.txt: Added.
            * http/tests/plugins/resources/load-me-2.txt: Added.
    
    WebKit:
    
            Reviewed by Darin Adler.
    
            http://bugs.webkit.org/show_bug.cgi?id=13029
            rdar://problem/4994849
            Bug 13029: Permit NPAPI plug-ins to see HTTP response headers.
    
            * Plugins/WebBaseNetscapePluginStream.h: declarations.
            * Plugins/WebBaseNetscapePluginStream.mm: main implementation.
            (-[WebBaseNetscapePluginStream dealloc]): cleanup.
            (-[WebBaseNetscapePluginStream finalize]): cleanup.
            (-[WebBaseNetscapePluginStream startStreamResponseURL:expectedContentLength:lastModifiedDate:MIMEType:headers:]):
            Pass headers along.
            (-[WebBaseNetscapePluginStream startStreamWithResponse:]):
            Main work is here.  Extract headers from NSHTTPURLResponse object into a byte sequence.
            See comments here about how it would be nice to have low-level access to the HTTP response.
            (-[WebBaseNetscapePluginStream _destroyStream]): cleanup.
            * Plugins/WebBaseNetscapePluginView.mm:
            (-[WebBaseNetscapePluginView evaluateJavaScriptPluginRequest:]):
            Conform to new startStream params.  Not applicable here, pass nil.
    
    WebKitTools:
    
            Reviewed by Darin Adler.
    
            http://bugs.webkit.org/show_bug.cgi?id=13029
            rdar://problem/4994849
            Bug 13029: Permit NPAPI plug-ins to see HTTP response headers.
            Changes in WebKitTools are only for the NPAPI test plugin.
    
            * DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.c: main test logic.
            (pluginInvoke): support null window argument for NPStream creation.
            (pluginAllocate): initialization.
            (pluginDeallocate): cleanup.
            (handleCallback): add second JS callback arg: header dump.
            (notifyStream): added; hook from NPP_NewStream to record headers.
            * DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.h: declarations.
            * DumpRenderTree/TestNetscapePlugIn.subproj/main.c: call new header hook.
            (NPP_NewStream): call new header hook.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins/WebBaseNetscapePluginStream.h
+++ third_party/WebKit/WebKit/Plugins/WebBaseNetscapePluginStream.h
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     char *path;
     BOOL sendNotification;
     void *notifyData;
+    char *headers;
     WebBaseNetscapePluginView *pluginView;
     NPReason reason;
     BOOL isTerminated;
@@ -81,7 +82,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)startStreamResponseURL:(NSURL *)theResponseURL
          expectedContentLength:(long long)expectedContentLength
               lastModifiedDate:(NSDate *)lastModifiedDate
-                      MIMEType:(NSString *)MIMEType;
+                      MIMEType:(NSString *)MIMEType
+                       headers:(NSData *)theHeaders;
 
 // cancelLoadWithError cancels the NSURLConnection and informs WebKit of the load error.
 // This method is overriden by subclasses.
