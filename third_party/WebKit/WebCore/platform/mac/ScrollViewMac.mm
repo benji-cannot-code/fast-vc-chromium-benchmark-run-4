112fe478d79e248640467bbba9cc57c06da18817
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-18 03:50:25 +0000

    Rolling out my previous checkin, as the compiler bug still exists on Leopard.
    git-svn-id: svn://svn.chromium.org/blink/trunk@20930 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/ScrollViewMac.mm
+++ third_party/WebKit/WebCore/platform/mac/ScrollViewMac.mm
@@ -335,7 +335,8 @@ void ScrollView::resizeContents(int w, int h)
         if (_h < 0)
             _h = 0;
             
-        [view setFrameSize:NSMakeSize(_w, _h)];
+        NSSize tempSize = { _w, _h }; // workaround for 4213314
+        [view setFrameSize:tempSize];
     } else {
         resize (_w, _h);
     }
@@ -392,7 +393,8 @@ IntPoint ScrollView::contentsToWindow(const IntPoint& contentsPoint) const
     if (docView)
         view = docView;
     
-    NSPoint np = [view convertPoint:contentsPoint toView: nil];
+    NSPoint tempPoint = { contentsPoint.x(), contentsPoint.y() }; // workaround for 4213314
+    NSPoint np = [view convertPoint:tempPoint toView: nil];
     return IntPoint(np);
 
     END_BLOCK_OBJC_EXCEPTIONS;
@@ -411,7 +413,8 @@ IntPoint ScrollView::windowToContents(const IntPoint& point) const
     if (docView)
         view = docView;
     
-    NSPoint np = [view convertPoint:point fromView: nil];
+    NSPoint tempPoint = { point.x(), point.y() }; // workaround for 4213314
+    NSPoint np = [view convertPoint:tempPoint fromView: nil];
 
     return IntPoint(np);
 
