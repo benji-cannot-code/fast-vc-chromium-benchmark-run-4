0ef87ce60aadd0f95c4bc706252c8d70d897dcc3
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-20 05:02:19 +0000

    Fix for bug 13208, implement word-break.  This patch produces a partial         implementation of word-break.  word-break: break-all is implemented.  In         addition, a custom value, word-break: break-word is added that is a hybrid         of word-wrap: break-word and word-break: break-all (and more useful than         either).
            Reviewed by beth
    
            Added fast/text/word-break.html
    
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
            * css/CSSPropertyNames.in:
            * css/CSSValueKeywords.in:
            * css/cssparser.cpp:
            (WebCore::CSSParser::parseValue):
            * css/cssstyleselector.cpp:
            (WebCore::CSSStyleSelector::applyProperty):
            * rendering/RenderStyle.cpp:
            (WebCore::StyleRareInheritedData::StyleRareInheritedData):
            (WebCore::StyleRareInheritedData::operator==):
            (WebCore::RenderStyle::diff):
            * rendering/RenderStyle.h:
            (WebCore::):
            (WebCore::RenderStyle::breakWords):
            (WebCore::RenderStyle::wordBreak):
            (WebCore::RenderStyle::setWordBreak):
            (WebCore::RenderStyle::initialWordBreak):
            (WebCore::RenderStyle::initialWordWrap):
            * rendering/RenderText.cpp:
            (WebCore::RenderText::calcMinMaxWidthInternal):
            * rendering/RenderTextControl.cpp:
            (WebCore::RenderTextControl::createInnerTextStyle):
            (WebCore::RenderTextControl::calcHeight):
            * rendering/bidi.cpp:
            (WebCore::RenderBlock::findNextLineBreak):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@20967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderStyle.cpp
+++ third_party/WebKit/WebCore/rendering/RenderStyle.cpp
@@ -525,7 +525,8 @@ StyleRareInheritedData::StyleRareInheritedData()
     , textShadow(0)
     , textSecurity(RenderStyle::initialTextSecurity())
     , userModify(READ_ONLY)
-    , wordWrap(WBNORMAL)
+    , wordBreak(RenderStyle::initialWordBreak())
+    , wordWrap(RenderStyle::initialWordWrap())
     , nbspMode(NBNORMAL)
     , khtmlLineBreak(LBNORMAL)
     , textSizeAdjust(RenderStyle::initialTextSizeAdjust())
@@ -542,6 +543,7 @@ StyleRareInheritedData::StyleRareInheritedData(const StyleRareInheritedData& o)
     , highlight(o.highlight)
     , textSecurity(o.textSecurity)
     , userModify(o.userModify)
+    , wordBreak(o.wordBreak)
     , wordWrap(o.wordWrap)
     , nbspMode(o.nbspMode)
     , khtmlLineBreak(o.khtmlLineBreak)
@@ -564,6 +566,7 @@ bool StyleRareInheritedData::operator==(const StyleRareInheritedData& o) const
         && highlight == o.highlight
         && textSecurity == o.textSecurity
         && userModify == o.userModify
+        && wordBreak == o.wordBreak
         && wordWrap == o.wordWrap
         && nbspMode == o.nbspMode
         && khtmlLineBreak == o.khtmlLineBreak
@@ -944,6 +947,7 @@ RenderStyle::Diff RenderStyle::diff(const RenderStyle* other) const
     if (rareInheritedData.get() != other->rareInheritedData.get()) {
         if (rareInheritedData->highlight != other->rareInheritedData->highlight ||
             rareInheritedData->textSizeAdjust != other->rareInheritedData->textSizeAdjust ||
+            rareInheritedData->wordBreak != other->rareInheritedData->wordBreak ||
             rareInheritedData->wordWrap != other->rareInheritedData->wordWrap ||
             rareInheritedData->nbspMode != other->rareInheritedData->nbspMode ||
             rareInheritedData->khtmlLineBreak != other->rareInheritedData->khtmlLineBreak ||
