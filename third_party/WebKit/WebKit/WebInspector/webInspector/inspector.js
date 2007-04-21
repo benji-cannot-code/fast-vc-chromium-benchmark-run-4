06e1813be02b865967f775a31f04c0207586e006
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-21 23:46:57 +0000

    Reviewed by Oliver.
            - fix some problems I ran into using the inspector
    
            * WebInspector/webInspector/inspector.js: Add some null checks.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21000 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebInspector/webInspector/inspector.js
+++ third_party/WebKit/WebKit/WebInspector/webInspector/inspector.js
@@ -737,7 +737,7 @@ function updateNodePane()
 
         updateElementAttributes();
 
-        if (focusedDOMNode.namespaceURI.length > 0) {
+        if (focusedDOMNode.namespaceURI && focusedDOMNode.namespaceURI.length > 0) {
             document.getElementById("nodeNamespace").textContent = focusedDOMNode.namespaceURI;
             document.getElementById("nodeNamespace").title = focusedDOMNode.namespaceURI;
             document.getElementById("nodeNamespaceRow").style.removeProperty("display");
@@ -816,7 +816,7 @@ function updateStylePane()
             }
         }
 
-        if (styleNode.style.length) {
+        if (styleNode.style && styleNode.style.length) {
             var inlineStyle = {
                 selectorText: "Inline Style Attribute",
                 style: styleNode.style,
