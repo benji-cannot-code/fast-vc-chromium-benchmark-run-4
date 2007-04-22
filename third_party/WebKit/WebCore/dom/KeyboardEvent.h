1abb6d63200af22e1433b823c58d8e7e2e22cee7
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-22 17:17:02 +0000

    LayoutTests:
            Reviewed by Adele.
    
            - test for <rdar://problem/5100240> REGRESSION: Control-O broken
    
            * editing/input/emacs-ctrl-o-expected.checksum: Added.
            * editing/input/emacs-ctrl-o-expected.png: Added.
            * editing/input/emacs-ctrl-o-expected.txt: Added.
            * editing/input/emacs-ctrl-o.html: Added.
    
    WebCore:
    
            Reviewed by Adele.
    
            - support for fix for <rdar://problem/5100240> REGRESSION: Control-O broken
    
            * dom/KeyboardEvent.h: Include a vector of command names in the KeypressCommand object
            instead of a single command name.
    
    WebKit:
    
            Reviewed by Adele.
    
            - fix for <rdar://problem/5100240> REGRESSION: Control-O broken
    
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView _interceptEditingKeyEvent:shouldSaveCommand:]): Update to handle a vector of
            command names instead of a single command.
            (-[WebHTMLView doCommandBySelector:]): Change logic so that we add the command to a vector
            and also so that the interpretKeyEvents parameters are still intact for a second call to
            doCommandBySelector:, since the key bindings mechanism can do more than one.
            (-[WebHTMLView insertText:]): Added comment.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/KeyboardEvent.h
+++ third_party/WebKit/WebCore/dom/KeyboardEvent.h
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * This file is part of the DOM implementation for KDE.
- *
  * Copyright (C) 2001 Peter Kelly (pmk@post.com)
  * Copyright (C) 2001 Tobias Anton (anton@stud.fbi.fh-darmstadt.de)
  * Copyright (C) 2006 Samuel Weinig (sam.weinig@gmail.com)
@@ -28,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define KeyboardEvent_h
 
 #include "UIEventWithKeyState.h"
+#include <wtf/Vector.h>
 
 namespace WebCore {
 
@@ -35,7 +34,7 @@ namespace WebCore {
 
 #if PLATFORM(MAC)
     struct KeypressCommand {
-        String name;
+        Vector<String> commandNames;
         String text;
     };
 #endif
