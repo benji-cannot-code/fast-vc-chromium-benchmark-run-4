d41cd9a746df85a50de25d63ed272b184302b40c
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-23 03:09:30 +0000

    Reviewed by Darin and Geoff.
            - move most of Window's data members into a separate private object, to avoid needing
            to use the oversize allocator for it
            
            It was the only remaining JSObject subclass to use the oversize
            allocator on 32-bit platforms, and having oversize objects around
            makes garbage collection slower so this would hurt performance
            with many tabs/windows open.
            
            No significant effect on JS iBench.
    
            * bindings/js/kjs_events.cpp:
            (KJS::JSUnprotectedEventListener::JSUnprotectedEventListener):
            (KJS::JSUnprotectedEventListener::~JSUnprotectedEventListener):
            (KJS::JSEventListener::JSEventListener):
            (KJS::JSEventListener::~JSEventListener):
            (KJS::JSLazyEventListener::parseCode):
            * bindings/js/kjs_window.cpp:
            (KJS::WindowPrivate::WindowPrivate):
            (KJS::Window::Window):
            (KJS::Window::~Window):
            (KJS::Window::location):
            (KJS::Window::selection):
            (KJS::Window::locationbar):
            (KJS::Window::menubar):
            (KJS::Window::personalbar):
            (KJS::Window::statusbar):
            (KJS::Window::toolbar):
            (KJS::Window::scrollbars):
            (KJS::Window::mark):
            (KJS::Window::getValueProperty):
            (KJS::Window::findJSEventListener):
            (KJS::Window::findJSUnprotectedEventListener):
            (KJS::Window::clearHelperObjectProperties):
            (KJS::Window::clear):
            (KJS::Window::setCurrentEvent):
            (KJS::Window::setReturnValueSlot):
            (KJS::Window::clearAllTimeouts):
            (KJS::Window::installTimeout):
            (KJS::Window::pauseTimeouts):
            (KJS::Window::resumeTimeouts):
            (KJS::Window::clearTimeout):
            (KJS::Window::timerFired):
            (KJS::Window::disconnectFrame):
            (KJS::Window::jsEventListeners):
            (KJS::Window::jsHTMLEventListeners):
            (KJS::Window::jsUnprotectedEventListeners):
            (KJS::Window::jsUnprotectedHTMLEventListeners):
            * bindings/js/kjs_window.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21014 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/kjs_events.cpp
+++ third_party/WebKit/WebCore/bindings/js/kjs_events.cpp
@@ -166,8 +166,8 @@ JSUnprotectedEventListener::JSUnprotectedEventListener(JSObject* _listener, Wind
   , win(_win)
 {
     if (_listener) {
-        Window::UnprotectedListenersMap& listeners = _html
-            ? _win->jsUnprotectedHTMLEventListeners : _win->jsUnprotectedEventListeners;
+        Window::UnprotectedListenersMap& listeners = _html 
+            ? _win->jsUnprotectedHTMLEventListeners() : _win->jsUnprotectedEventListeners();
         listeners.set(_listener, this);
     }
 }
@@ -176,7 +176,7 @@ JSUnprotectedEventListener::~JSUnprotectedEventListener()
 {
     if (listener && win) {
         Window::UnprotectedListenersMap& listeners = isHTMLEventListener()
-            ? win->jsUnprotectedHTMLEventListeners : win->jsUnprotectedEventListeners;
+            ? win->jsUnprotectedHTMLEventListeners() : win->jsUnprotectedEventListeners();
         listeners.remove(listener);
     }
 }
@@ -229,7 +229,7 @@ JSEventListener::JSEventListener(JSObject* _listener, Window* _win, bool _html)
 {
     if (_listener) {
         Window::ListenersMap& listeners = _html
-            ? _win->jsHTMLEventListeners : _win->jsEventListeners;
+            ? _win->jsHTMLEventListeners() : _win->jsEventListeners();
         listeners.set(_listener, this);
     }
 #ifndef NDEBUG
@@ -241,7 +241,7 @@ JSEventListener::~JSEventListener()
 {
     if (listener && win) {
         Window::ListenersMap& listeners = isHTMLEventListener()
-            ? win->jsHTMLEventListeners : win->jsEventListeners;
+            ? win->jsHTMLEventListeners() : win->jsEventListeners();
         listeners.remove(listener);
     }
 #ifndef NDEBUG
@@ -343,7 +343,7 @@ void JSLazyEventListener::parseCode() const
 
     if (listener) {
         Window::ListenersMap& listeners = isHTMLEventListener()
-            ? windowObj()->jsHTMLEventListeners : windowObj()->jsEventListeners;
+            ? windowObj()->jsHTMLEventListeners() : windowObj()->jsEventListeners();
         listeners.set(listener, const_cast<JSLazyEventListener*>(this));
     }
 }
