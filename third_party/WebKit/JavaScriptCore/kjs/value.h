81cc17405b8e22027eb153df4835cc9d2423a9e5
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-23 21:54:33 +0000

    Reviewed by Darin and Geoff.
            - move mark and collectOnMainThreadOnly bits into separate bitmaps
            
            This saves 4 bytes per cell, allowing shrink of cell size to 32,
            which leads to a .8% speed improvement on iBench.
            
            This is only feasible because of all the previous changes on the branch.
    
            * kjs/collector.cpp:
            (KJS::allocateBlock): Adjust for some renames of constants. 
            (KJS::Collector::markStackObjectsConservatively): Now that cells are 32 bytes (64 
            bytes on 64-bit) the cell alignment check can be made much more strict, and also
            obsoletes the need for a % sizeof(CollectorCell) check. Also, we can mask off the low
            bits of the pointer to have a potential block pointer to look for.
            (KJS::Collector::collectOnMainThreadOnly): Use bitmap.
            (KJS::Collector::markMainThreadOnlyObjects): Use bitmap.
            (KJS::Collector::collect): When sweeping, use bitmaps directly to find mark bits.
            * kjs/collector.h:
            (KJS::): Move needed constants and type declarations here.
            (KJS::CollectorBitmap::get): Bit twiddling to get a bitmap value.
            (KJS::CollectorBitmap::set): Bit twiddling to set a bitmap bit to true.
            (KJS::CollectorBitmap::clear): Bit twiddling to set a bitmap bit to false.
            (KJS::CollectorBitmap::clearAll): Clear whole bitmap at one go.
            (KJS::Collector::cellBlock): New operation, compute the block pointer for
            a cell by masking off low bits.
            (KJS::Collector::cellOffset): New operation, compute the cell offset for a
            cell by masking off high bits and dividing (actually a shift).
            (KJS::Collector::isCellMarked): Check mark bit in bitmap
            (KJS::Collector::markCell): Set mark bit in bitmap.
            * kjs/value.h:
            (KJS::JSCell::JSCell): No more bits.
            (KJS::JSCell::marked): Let collector handle it.
            (KJS::JSCell::mark): Let collector handle it.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/value.h
+++ third_party/WebKit/JavaScriptCore/kjs/value.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define KJS_VALUE_H
 
 #include "JSImmediate.h"
+#include "collector.h"
 #include "ustring.h"
 #include <stddef.h> // for size_t
 
@@ -155,10 +156,6 @@ public:
     void *operator new(size_t);
     virtual void mark();
     bool marked() const;
-
-private:
-    bool m_collectOnMainThreadOnly : 1;
-    bool m_marked : 1;
 };
 
 JSValue *jsNumberCell(double);
@@ -205,8 +202,6 @@ inline JSValue::~JSValue()
 }
 
 inline JSCell::JSCell()
-    : m_collectOnMainThreadOnly(false)
-    , m_marked(false)
 {
 }
 
@@ -231,12 +226,12 @@ inline bool JSCell::isObject() const
 
 inline bool JSCell::marked() const
 {
-    return m_marked;
+    return Collector::isCellMarked(this);
 }
 
 inline void JSCell::mark()
 {
-    m_marked = true;
+    return Collector::markCell(this);
 }
 
 inline JSCell *JSValue::downcast()
