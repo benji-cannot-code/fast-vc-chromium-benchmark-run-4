7da4d1ab401d52d3f768c2cb465a6f15dc3c049e
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-25 20:02:30 +0000

    Reviewed by Hyatt.
            * platform/AtomicString.cpp: (WebCore::AtomicString::add):
            Removed unnecessary use of strlen to check if a string is empty.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21095 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/AtomicString.cpp
+++ third_party/WebKit/WebCore/platform/AtomicString.cpp
@@ -1,8 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * This file is part of the DOM implementation for KDE.
- *
- * Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2007 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -84,10 +82,8 @@ StringImpl* AtomicString::add(const char* c)
 {
     if (!c)
         return 0;
-    int length = strlen(c);
-    if (length == 0)
-        return StringImpl::empty();
-    
+    if (!*c)
+        return StringImpl::empty();    
     return *stringTable->add<const char*, CStringTranslator>(c).first;
 }
 
