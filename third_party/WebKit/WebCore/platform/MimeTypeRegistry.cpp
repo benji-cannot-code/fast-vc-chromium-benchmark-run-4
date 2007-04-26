d7e4b50c6dda064dff3c942e9622a5b6937cb703
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-26 18:37:56 +0000

    Reviewed by Hyatt.
            <rdar://problem/4851944>
            REGRESSION: With TOT, BMP images can no longer be opened in the browser window
            
            On Tiger, the MIME type for the com.microsoft.bmp UTI isn't in the registry, so add it manually.
            
            * platform/MimeTypeRegistry.cpp:
            (WebCore::initialiseSupportedImageMIMETypes):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21117 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/MimeTypeRegistry.cpp
+++ third_party/WebKit/WebCore/platform/MimeTypeRegistry.cpp
@@ -38,12 +38,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore
 {
-static WTF::HashSet<String> *supportedImageResourceMIMETypes;
-static WTF::HashSet<String> *supportedImageMIMETypes;
-static WTF::HashSet<String> *supportedNonImageMIMETypes;
+static WTF::HashSet<String>* supportedImageResourceMIMETypes;
+static WTF::HashSet<String>* supportedImageMIMETypes;
+static WTF::HashSet<String>* supportedNonImageMIMETypes;
     
 #if PLATFORM(CG)
-extern String getMIMETypeForUTI(const String & uti);
+extern String getMIMETypeForUTI(const String& uti);
 #endif
 
 static void initialiseSupportedImageMIMETypes()
@@ -62,15 +62,19 @@ static void initialiseSupportedImageMIMETypes()
     }
     CFRelease(supportedTypes);
     
-    //We only get one MIME type per UTI, hence our need to add these manually
+    // On Tiger, com.microsoft.bmp doesn't have a MIME type in the registry.
+    supportedImageMIMETypes->add("image/bmp");
+    supportedImageResourceMIMETypes->add("image/bmp");
+    
+    //  We only get one MIME type per UTI, hence our need to add these manually
     supportedImageMIMETypes->add("image/pjpeg");
     supportedImageResourceMIMETypes->add("image/pjpeg");
     
-    //We don't want to try to treat all binary data as an image
+    //  We don't want to try to treat all binary data as an image
     supportedImageMIMETypes->remove("application/octet-stream");
     supportedImageResourceMIMETypes->remove("application/octet-stream");
     
-    //Don't treat pdf/postscript as images directly
+    //  Don't treat pdf/postscript as images directly
     supportedImageMIMETypes->remove("application/pdf");
     supportedImageMIMETypes->remove("application/postscript");
 
