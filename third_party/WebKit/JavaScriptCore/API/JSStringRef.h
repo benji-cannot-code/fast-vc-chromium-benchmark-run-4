51e4075fa006ba475e1fcf3fbc196eba782f042d
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-27 01:15:54 +0000

    Reviewed by Maciej Stachowiak, Adam Roben.
            Fixed 
            <rdar://problem/4885130> Remove #include of JSStringRefCF.h from JSStringRef.h
            <rdar://problem/4885123> JavaScriptCore is not cross-platform -- JSStringRef.h references CF datatypes
    
            * API/JSStringRef.h: Removed #include -- no clients need it anymore.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21130 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSStringRef.h
+++ third_party/WebKit/JavaScriptCore/API/JSStringRef.h
@@ -37,10 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 extern "C" {
 #endif
 
-#if defined(__APPLE__)
-#include <JavaScriptCore/JSStringRefCF.h>
-#endif
-
 /*!
 @typedef JSChar
 @abstract A Unicode character.
