66652f3f1881ed3b52ca8e7a0f73c3b8a020a566
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-04-27 08:10:54 +0000

    Reviewed by Mitz.
            <rdar://problem/5165755>
            View Source is broken; empty window is shown
            
            Return YES for applewebdata URLs.
            
            * WebView/WebView.mm:
            (+[WebView _canHandleRequest:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21141 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebView.mm
+++ third_party/WebKit/WebKit/WebView/WebView.mm
@@ -1017,6 +1017,10 @@ WebResourceDelegateImplementationCache WebViewGetResourceLoadDelegateImplementat
         return YES;
     }
 
+    // applewebdata URLs are used for data loads where no base URL has been specified.
+    if ([[[[request URL] scheme] lowercaseString] isEqualToString:@"applewebdata"])
+        return YES;
+    
     return [self _representationExistsForURLScheme:[[request URL] scheme]];
 }
 
