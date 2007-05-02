7cd647366ed5dba2bdb58817eceb9ee5f1858feb
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-02 01:40:31 +0000

    * rendering/RenderReplaced.cpp: Fix typo in a comment.
    git-svn-id: svn://svn.chromium.org/blink/trunk@21215 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderReplaced.cpp
+++ third_party/WebKit/WebCore/rendering/RenderReplaced.cpp
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  * Copyright (C) 2000 Dirk Mueller (mueller@kde.org)
- * Copyright (C) 2004, 2006,2 007 Apple Inc. All rights reserved.
+ * Copyright (C) 2004, 2006, 2007 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
