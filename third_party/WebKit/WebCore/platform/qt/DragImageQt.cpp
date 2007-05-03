82a705a6554eb3b06499a87339c87fd863c1c02c
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-03 04:39:37 +0000

    2007-05-03  Mark Rowe  <mrowe@apple.com>
            Qt build fix.  In r21185 the Qt version of WebCore::scaleDragImage was
            updated to have the incorrect signature.
    
            * platform/qt/DragImageQt.cpp:
            (WebCore::scaleDragImage):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/DragImageQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/DragImageQt.cpp
@@ -41,7 +41,7 @@ void deleteDragImage(DragImageRef)
 {
 }
 
-DragImageRef scaleDragImage(DragImageRef image, float, float)
+DragImageRef scaleDragImage(DragImageRef image, FloatSize)
 {
     return image;
 }
