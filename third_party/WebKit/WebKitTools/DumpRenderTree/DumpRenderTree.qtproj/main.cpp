3a8747ac8cb238a286a07bfb015837474ac9d2e1
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-07 13:56:23 +0000

    Change 5 results in css2.1 that were wrong due to a Qt bug.         Add css3 baseline.
            Fix the default font in DRT to 9pt Sans Serif.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21283 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.qtproj/main.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.qtproj/main.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qurl.h>
 #include <qdir.h>
 #include <qdebug.h>
+#include <qfont.h>
 
 #ifdef Q_WS_X11
 #include <qx11info_x11.h>
@@ -81,6 +82,11 @@ int main(int argc, char* argv[])
     FcConfigSetCurrent(config);
 #endif
     QApplication app(argc, argv);
+    QFont f("Sans Serif");
+    f.setPointSize(9);
+    f.setWeight(QFont::Normal);
+    f.setStyle(QFont::StyleNormal);
+    app.setFont(f);
 
 #ifdef Q_WS_X11
     QX11Info::setAppDpiY(0, 96);
