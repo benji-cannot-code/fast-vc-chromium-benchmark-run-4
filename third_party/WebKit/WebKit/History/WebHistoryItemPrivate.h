5c5a730a0fadd6d9a8108365a7647dbe1c91a31b
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-07 21:57:42 +0000

    Rubberstamped by Kevin (Sarge)
            Make _transientPropertyForKey: and _setTransientProperty:forKey: SPI
    
            * History/WebHistoryItemInternal.h:
            * History/WebHistoryItemPrivate.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21289 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/History/WebHistoryItemPrivate.h
+++ third_party/WebKit/WebKit/History/WebHistoryItemPrivate.h
@@ -53,4 +53,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This should not be called directly for WebHistoryItems that are already included
 // in WebHistory. Use -[WebHistory setLastVisitedTimeInterval:forItem:] instead.
 - (void)_setLastVisitedTimeInterval:(NSTimeInterval)time;
+// Transient properties may be of any ObjC type.  They are intended to be used to store state per back/forward list entry.
+// The properties will not be persisted; when the history item is removed, the properties will be lost.
+- (id)_transientPropertyForKey:(NSString *)key;
+- (void)_setTransientProperty:(id)property forKey:(NSString *)key;
+
 @end
