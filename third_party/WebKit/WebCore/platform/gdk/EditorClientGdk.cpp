50d0d9db5ec433cb5495eac480f278854eedd7eb
sfalken <sfalken@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-08 19:27:50 +0000

    2007-05-08  Steve Falkenburg  <sfalken@apple.com>
            Reviewed by Ada.
    
            Slight modification to last editor method fix.
    
            * WebCoreSupport/WebEditorClient.h:
            (WebEditorClient::updateSpellingUIWithGrammarString):
    
    2007-05-08  Steve Falkenburg  <sfalken@apple.com>
    
            Reviewed by Ada.
    
            Slight modification to last editor method fix.
    
            * bridge/EditorClient.h:
            * platform/gdk/EditorClientGdk.cpp:
            (WebCore::EditorClientGdk::updateSpellingUIWithGrammarString):
            * platform/gdk/EditorClientGdk.h:
            * platform/graphics/svg/SVGImageEmptyClients.h:
            (WebCore::SVGEmptyEditorClient::updateSpellingUIWithGrammarString):
    
    2007-05-08  Steve Falkenburg  <sfalken@apple.com>
    
            Reviewed by Ada.
            
            Slight modification to last editor method fix.
    
            * WebCoreSupport/EditorClientQt.cpp:
            (WebCore::EditorClientQt::updateSpellingUIWithGrammarString):
            * WebCoreSupport/EditorClientQt.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21311 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gdk/EditorClientGdk.cpp
+++ third_party/WebKit/WebCore/platform/gdk/EditorClientGdk.cpp
@@ -316,7 +316,7 @@ void EditorClientGdk::checkGrammarOfString(const UChar*, int, Vector<GrammarDeta
     notImplementedGdk();
 }
 
-void EditorClientGdk::updateSpellingUIWithGrammarString(const String&, const String&, const Vector<String>&)
+void EditorClientGdk::updateSpellingUIWithGrammarString(const String&, const GrammarDetail&)
 {
     notImplementedGdk();
 }
