4686e09739ccdccfd14ce15d5d9de4ab959bf824
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-08 04:59:48 +0000

    LayoutTests:
            Reviewed by Maciej.
    
            Test the behaviour of canvas methods called with inf/nan parameters
    
            * fast/canvas/canvas-with-incorrect-args-expected.txt: Added.
            * fast/canvas/canvas-with-incorrect-args.html: Added.
    
    WebCore:
    
            Reviewed by Maciej.
    
            Correcting comparisons to handle NaN in the same manner
            as other Canvas methods.
    
            * html/CanvasRenderingContext2D.cpp:
            (WebCore::CanvasRenderingContext2D::arcTo):
            (WebCore::CanvasRenderingContext2D::arc):
              Correcting arc/arcTo to match the WHAT WG draft.
            (WebCore::CanvasRenderingContext2D::rect):
            (WebCore::CanvasRenderingContext2D::clearRect):
            (WebCore::CanvasRenderingContext2D::fillRect):
            (WebCore::CanvasRenderingContext2D::strokeRect):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21294 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/WebCore/html/CanvasRenderingContext2D.cpp
@@ -396,7 +396,7 @@ void CanvasRenderingContext2D::bezierCurveTo(float cp1x, float cp1y, float cp2x,
 void CanvasRenderingContext2D::arcTo(float x0, float y0, float x1, float y1, float r, ExceptionCode& ec)
 {
     ec = 0;
-    if (r < 0) {
+    if (!(r > 0)) {
         ec = INDEX_SIZE_ERR;
         return;
     }
@@ -406,7 +406,7 @@ void CanvasRenderingContext2D::arcTo(float x0, float y0, float x1, float y1, flo
 void CanvasRenderingContext2D::arc(float x, float y, float r, float sa, float ea, bool clockwise, ExceptionCode& ec)
 {
     ec = 0;
-    if (r < 0) {
+    if (!(r > 0)) {
         ec = INDEX_SIZE_ERR;
         return;
     }
@@ -416,7 +416,7 @@ void CanvasRenderingContext2D::arc(float x, float y, float r, float sa, float ea
 void CanvasRenderingContext2D::rect(float x, float y, float width, float height, ExceptionCode& ec)
 {
     ec = 0;
-    if (width < 0 || height < 0) {
+    if (!(width >= 0 && height >= 0)) {
         ec = INDEX_SIZE_ERR;
         return;
     }
@@ -494,7 +494,7 @@ void CanvasRenderingContext2D::clip()
 void CanvasRenderingContext2D::clearRect(float x, float y, float width, float height, ExceptionCode& ec)
 {
     ec = 0;
-    if (width < 0 || height < 0) {
+    if (!(width >= 0 && height >= 0)) {
         ec = INDEX_SIZE_ERR;
         return;
     }
@@ -510,7 +510,7 @@ void CanvasRenderingContext2D::fillRect(float x, float y, float width, float hei
 {
     ec = 0;
 
-    if (width < 0 || height < 0) {
+    if (!(width >= 0 && height >= 0)) {
         ec = INDEX_SIZE_ERR;
         return;
     }
@@ -547,7 +547,7 @@ void CanvasRenderingContext2D::strokeRect(float x, float y, float width, float h
 {
     ec = 0;
 
-    if (width < 0 || height < 0 || lineWidth < 0) {
+    if (!(width >= 0 && height >= 0 && lineWidth >= 0)) {
         ec = INDEX_SIZE_ERR;
         return;
     }
