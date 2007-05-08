8e0edb31d4964ee18f4fa5f046da57255923efc1
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-08 05:41:48 +0000

    2007-05-07  Atul Mehrotra <atul.mehrotra@celunite.com>
            Reviewed by Maciej.
    
            GDK Build fix
    
            * platform/gdk/TemporaryLinkStubs.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gdk/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/gdk/TemporaryLinkStubs.cpp
@@ -74,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScrollBar.h"
 #include "SearchPopupMenu.h"
 #include "TextBoundaries.h"
+#include "TextBreakIteratorInternalICU.h"
 #include "Widget.h"
 #include <gtk/gtk.h>
 #include <stdio.h>
