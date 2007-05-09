7c791a0639c7bd6f7dcb489ab68a5c0bc1fd16d2
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-09 21:42:38 +0000

    2007-05-09  Holger Freyther <zecke@selfish.org>
            Reviewed by Mark Rowe.
    
            Gdk build fix with SVG enabled.  Stub out ImageBufferCairo.
    
            * WebCore.pro: Build ImageBufferCairo.cpp
            * platform/graphics/cairo/ImageBufferCairo.cpp: Added.
            (WebCore::ImageBuffer::create):
            (WebCore::ImageBuffer::~ImageBuffer):
            (WebCore::ImageBuffer::context):
    
    2007-05-09  Holger Freyther <zecke@selfish.org>
    
            Reviewed by Mark Rowe.
    
            * GdkLauncher/main.cpp: Always include config.h.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21340 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/GdkLauncher/main.cpp
+++ third_party/WebKit/WebKitTools/GdkLauncher/main.cpp
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#ifdef HAVE_CONFIG_H
 #include "config.h"
-#endif
 #include "Platform.h"
 #include "ChromeClientGdk.h"
 #include "ContextMenuClientGdk.h"
