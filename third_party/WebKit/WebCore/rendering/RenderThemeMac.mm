1c889ff58d5b8964e20ad7647ba788d491f000a8
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-12 00:17:38 +0000

    Make sure focus rings are always using the exterior style, regardless         of what the OS default is.
            Reviewed by beth
    
            * rendering/RenderThemeMac.mm:
            (WebCore::RenderThemeMac::checkbox):
            (WebCore::RenderThemeMac::radio):
            (WebCore::RenderThemeMac::popupButton):
            (WebCore::RenderThemeMac::search):
            (WebCore::RenderThemeMac::sliderThumbHorizontal):
            (WebCore::RenderThemeMac::sliderThumbVertical):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderThemeMac.mm
+++ third_party/WebKit/WebCore/rendering/RenderThemeMac.mm
@@ -1244,6 +1244,7 @@ NSButtonCell* RenderThemeMac::checkbox() const
         [m_checkbox.get() setButtonType:NSSwitchButton];
         [m_checkbox.get() setTitle:nil];
         [m_checkbox.get() setAllowsMixedState:YES];
+        [m_checkbox.get() setFocusRingType:NSFocusRingTypeExterior];
     }
     
     return m_checkbox.get();
@@ -1255,6 +1256,7 @@ NSButtonCell* RenderThemeMac::radio() const
         m_radio.adoptNS([[NSButtonCell alloc] init]);
         [m_radio.get() setButtonType:NSRadioButton];
         [m_radio.get() setTitle:nil];
+        [m_radio.get() setFocusRingType:NSFocusRingTypeExterior];
     }
     
     return m_radio.get();
@@ -1276,6 +1278,7 @@ NSPopUpButtonCell* RenderThemeMac::popupButton() const
     if (!m_popupButton) {
         m_popupButton.adoptNS([[NSPopUpButtonCell alloc] initTextCell:@"" pullsDown:NO]);
         [m_popupButton.get() setUsesItemFromMenu:NO];
+        [m_popupButton.get() setFocusRingType:NSFocusRingTypeExterior];
     }
     
     return m_popupButton.get();
@@ -1288,7 +1291,7 @@ NSSearchFieldCell* RenderThemeMac::search() const
         [m_search.get() setBezelStyle:NSTextFieldRoundedBezel];
         [m_search.get() setBezeled:YES];
         [m_search.get() setEditable:YES];
-        
+        [m_search.get() setFocusRingType:NSFocusRingTypeExterior];
         NSMenu* searchMenuTemplate = [[NSMenu alloc] initWithTitle:@""];
         [m_search.get() setSearchMenuTemplate:searchMenuTemplate];
         [searchMenuTemplate release];
@@ -1304,6 +1307,7 @@ NSSliderCell* RenderThemeMac::sliderThumbHorizontal() const
         [m_sliderThumbHorizontal.get() setTitle:nil];
         [m_sliderThumbHorizontal.get() setSliderType:NSLinearSlider];
         [m_sliderThumbHorizontal.get() setControlSize:NSSmallControlSize];
+        [m_sliderThumbHorizontal.get() setFocusRingType:NSFocusRingTypeExterior];
     }
     
     return m_sliderThumbHorizontal.get();
@@ -1316,6 +1320,7 @@ NSSliderCell* RenderThemeMac::sliderThumbVertical() const
         [m_sliderThumbVertical.get() setTitle:nil];
         [m_sliderThumbVertical.get() setSliderType:NSLinearSlider];
         [m_sliderThumbVertical.get() setControlSize:NSSmallControlSize];
+        [m_sliderThumbVertical.get() setFocusRingType:NSFocusRingTypeExterior];
     }
     
     return m_sliderThumbVertical.get();
