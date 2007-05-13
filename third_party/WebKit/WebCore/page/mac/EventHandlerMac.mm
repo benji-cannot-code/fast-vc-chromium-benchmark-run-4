bc226413b93b8e8f428cdad838f5a00425443166
rwlbuis <rwlbuis@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-13 14:00:18 +0000

    Reviewed by Darin.
            http://bugs.webkit.org/show_bug.cgi?id=13529
            DOM3 keyIdentifier text is incorrect
    
            Use 4 digits for the Unicode based key identifiers.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/mac/EventHandlerMac.mm
+++ third_party/WebKit/WebCore/page/mac/EventHandlerMac.mm
@@ -98,7 +98,7 @@ static inline bool isKeyboardOptionTab(KeyboardEvent* event)
     return event
     && (event->type() == keydownEvent || event->type() == keypressEvent)
     && event->altKey()
-    && event->keyIdentifier() == "U+000009";    
+    && event->keyIdentifier() == "U+0009";    
 }
 
 bool EventHandler::invertSenseOfTabsToLinks(KeyboardEvent* event) const
