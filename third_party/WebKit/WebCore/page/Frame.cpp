8d8d8f5c532d0f35769d9323f18ab2f8ceb2e857
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-15 06:15:21 +0000

    LayoutTests:
            Reviewed by ggaren
            
            Updated these expected results.  We now clear the
            selection inside a focused node *after* firing the
            mousedown event handler, which matches FF:
            * fast/forms/focus-selection-input-expected.txt:
            * fast/forms/focus-selection-textarea-expected.txt:
    
    WebCore:
    
            Reviewed by ggaren
            
            <http://bugs.webkit.org/show_bug.cgi?id=13716>
            REGRESSION: Three new layout test failures
            
            Two failures are correct.  Updated their expected results.
            
            In fast/events/frame-tab-focus.html, as we advance
            through focusable nodes, we descend into a subframe
            to focus a node and then ascend out of it into the
            main frame to focus the next.  When we focus the main 
            frame, the node in that frame that was previously 
            focused and contains an inactive selection is focused 
            momentarily because setCaretVisible tries to focus the 
            node containing the caret.
    
            * page/Frame.cpp:
            (WebCore::Frame::setCaretVisible): Don't focus the
            node containing the caret. FocusController will focus
            the previously focused node (which will contain the
            caret) if necessary when the frame gains focus.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21476 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -570,8 +570,6 @@ void Frame::setCaretVisible(bool flag)
     if (d->m_caretVisible == flag)
         return;
     clearCaretRectIfNeeded();
-    if (flag)
-        setFocusedNodeIfNeeded();
     d->m_caretVisible = flag;
     selectionLayoutChanged();
 }
