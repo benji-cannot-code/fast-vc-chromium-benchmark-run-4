fbd8ead9823dfc3417cfdf774a6b06feba716003
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-17 04:47:17 +0000

    Reviewed by Adam.
            <rdar://problem/5191024> Page never loads completely (emonome.vodpod.com)
            <rdar://problem/5205162> REGRESSION (r21367): In the Address URL field, typing a site name doesn't redirect to site after pressing return
            http://bugs.webkit.org/show_bug.cgi?id=13683
            <rdar://problem/5208951> REGRESSION: Progress bar never completes on link click that downloads (13694)
            http://bugs.webkit.org/show_bug.cgi?id=13694
            <rdar://problem/5201598> REGRESSION: After uploading file to iDisk at mac.com, "loading" status remains and file does not appear in the list
            
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::mainReceivedCompleteError): Make sure to always check whether the overall
            load completed, even if we think the current frame load is already complete.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -2246,7 +2246,6 @@ bool FrameLoader::frameHasLoaded() const
     return m_committedFirstRealDocumentLoad || isLoading() && !m_creatingInitialEmptyDocument; 
 }
 
-
 void FrameLoader::setDocumentLoader(DocumentLoader* loader)
 {
     if (!loader && !m_documentLoader)
@@ -4202,9 +4201,7 @@ void FrameLoader::mainReceivedCompleteError(DocumentLoader* loader, const Resour
     loader->setPrimaryLoadComplete(true);
     m_client->dispatchDidLoadMainResource(activeDocumentLoader());
     checkCompleted();
-#if PLATFORM(QT)
     checkLoadComplete();
-#endif
 }
 
 void FrameLoader::mainReceivedError(const ResourceError& error, bool isComplete)
