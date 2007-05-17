e1776cb279da83861e72972602b5b20d1e304caa
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-17 22:57:49 +0000

    Not reviewed, emergency test fix.
            - avoid crash on layout tests.
            
            Patched up my last change so that  widgets don't crash.
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::mainReceivedCompleteError):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21543 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -4201,7 +4201,8 @@ void FrameLoader::mainReceivedCompleteError(DocumentLoader* loader, const Resour
     loader->setPrimaryLoadComplete(true);
     m_client->dispatchDidLoadMainResource(activeDocumentLoader());
     checkCompleted();
-    checkLoadComplete();
+    if (m_frame->page())
+        checkLoadComplete();
 }
 
 void FrameLoader::mainReceivedError(const ResourceError& error, bool isComplete)
