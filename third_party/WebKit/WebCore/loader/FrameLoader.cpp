c6aa5495006ebc9f58192b87c7160ece9d1e0954
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-17 08:38:44 +0000

    WebCore:
            Reviewed by Darin.
    
            <rdar://problem/5207156> Hamachi test tool causes assertion in FormCompletionController in Safari
    
            This happens because the form delegate gets passed the wrong source frame during the second submit().
            If a frame with the given name is found, FrameLoader::load is called with the same parameters on the found frame.
            This causes the found frame to be used as the source frame, which is wrong.
            
            The fix is to create the form state object before checking what frame to load the new request in. 
            
            * WebCore.exp:
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::load):
            (WebCore::FrameLoader::checkNavigationPolicy):
            (WebCore::FrameLoader::continueAfterNavigationPolicy):
            * loader/FrameLoader.h:
            * manual-tests/submit-form-with-target-twice.html: Added.
    
    WebKit:
    
            Reviewed by Darin.
    
            <rdar://problem/5207156> Hamachi test tool causes assertion in FormCompletionController in Safari
    
            Update for WebCore changes.
            
            * WebView/WebFrame.mm:
            (-[WebFrame _loadURL:referrer:intoChild:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -1808,8 +1808,12 @@ void FrameLoader::load(const FrameLoadRequest& request, bool userGesture, Event*
         else
             loadType = FrameLoadTypeStandard;    
     
+        RefPtr<FormState> formState;
+        if (submitForm && !formValues.isEmpty())
+            formState = FormState::create(submitForm, formValues, m_frame);
+        
         load(request.resourceRequest().url(), referrer, loadType, 
-            request.frameName(), event, submitForm, formValues);
+            request.frameName(), event, formState.release());
     } else
         post(request.resourceRequest().url(), referrer, request.frameName(), 
             request.resourceRequest().httpBody(), request.resourceRequest().httpContentType(), event, submitForm, formValues);
@@ -1819,9 +1823,9 @@ void FrameLoader::load(const FrameLoadRequest& request, bool userGesture, Event*
 }
 
 void FrameLoader::load(const KURL& URL, const String& referrer, FrameLoadType newLoadType,
-    const String& frameName, Event* event, HTMLFormElement* form, const HashMap<String, String>& values)
+    const String& frameName, Event* event, PassRefPtr<FormState> formState)
 {
-    bool isFormSubmission = !values.isEmpty();
+    bool isFormSubmission = formState;
     
     ResourceRequest request(URL);
     if (!referrer.isEmpty())
@@ -1834,15 +1838,11 @@ void FrameLoader::load(const KURL& URL, const String& referrer, FrameLoadType ne
 
     NavigationAction action(URL, newLoadType, isFormSubmission, event);
 
-    RefPtr<FormState> formState;
-    if (form && !values.isEmpty())
-        formState = FormState::create(form, values, m_frame);
-    
     if (!frameName.isEmpty()) {
         if (Frame* targetFrame = m_frame->tree()->find(frameName))
-            targetFrame->loader()->load(URL, referrer, newLoadType, String(), event, form, values);
+            targetFrame->loader()->load(URL, referrer, newLoadType, String(), event, formState);
         else
-            checkNewWindowPolicy(action, request, formState.release(), frameName);
+            checkNewWindowPolicy(action, request, formState, frameName);
         return;
     }
 
@@ -1871,12 +1871,12 @@ void FrameLoader::load(const KURL& URL, const String& referrer, FrameLoadType ne
         
         oldDocumentLoader->setTriggeringAction(action);
         stopPolicyCheck();
-        checkNavigationPolicy(request, oldDocumentLoader.get(), formState.release(),
+        checkNavigationPolicy(request, oldDocumentLoader.get(), formState,
             callContinueFragmentScrollAfterNavigationPolicy, this);
     } else {
         // must grab this now, since this load may stop the previous load and clear this flag
         bool isRedirect = m_quickRedirectComing;
-        load(request, action, newLoadType, formState.release());
+        load(request, action, newLoadType, formState);
         if (isRedirect) {
             m_quickRedirectComing = false;
             if (m_provisionalDocumentLoader)
