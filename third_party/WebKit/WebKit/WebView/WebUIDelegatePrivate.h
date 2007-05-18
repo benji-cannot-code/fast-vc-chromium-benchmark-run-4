42cad0c69701e8497e3b2c92276b1af2590e8f39
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-18 17:05:30 +0000

    Reviewed by Kevin Decker
            WebKit part of fix for 5201499 (support for PDFKit UI for saving PDFs to disk)
            
            Also, the PDFKit mechanism for notifying clients about "Open File Externally" was
            changed from a notification to a delegate method. The notification was new to 
            Leopard, so removing it doesn't affect clients in the field.
    
            * WebView/WebUIDelegatePrivate.h:
            Declared new UI delegate method webView:saveFrameView:, analogous to the existing
            webView:printFrameView:
    
            * WebView/WebPDFView.mm:
            removed declaration of _webkit_PDFKitLaunchNotification
            (-[WebPDFView viewDidMoveToWindow]):
            don't observe _webkit_PDFKitLaunchNotification
            (-[WebPDFView viewWillMoveToWindow:]):
            ditto
            (-[WebPDFView PDFViewOpenPDFInNativeApplication:]):
            new PDFKit delegate method, replaces our use of _webkit_PDFKitLaunchNotification
            (-[WebPDFView PDFViewSavePDFToDownloadFolder:]):
            new PDFKit delegate method, calls through to new WebKit UI delegate method
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21569 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebUIDelegatePrivate.h
+++ third_party/WebKit/WebKit/WebView/WebUIDelegatePrivate.h
@@ -82,5 +82,6 @@ enum {
 - (BOOL)webViewShouldInterruptJavaScript:(WebView *)sender;
 - (void)webView:(WebView *)sender willPopupMenu:(NSMenu *)menu;
 - (void)webView:(WebView *)sender contextMenuItemSelected:(NSMenuItem *)item forElement:(NSDictionary *)element;
+- (void)webView:(WebView *)sender saveFrameView:(WebFrameView *)frameView;
 
 @end
