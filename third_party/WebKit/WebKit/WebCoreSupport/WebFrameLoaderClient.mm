e12ff8721ab54a2fd6a1c24b135322e3a47d6e55
tristan <tristan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-18 22:46:36 +0000

    Fixed potential crash with ASSERT in download due to previous commit.
    git-svn-id: svn://svn.chromium.org/blink/trunk@21582 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport/WebFrameLoaderClient.mm
+++ third_party/WebKit/WebKit/WebCoreSupport/WebFrameLoaderClient.mm
@@ -275,8 +275,7 @@ void WebFrameLoaderClient::download(ResourceHandle* handle, const ResourceReques
             originalURL = [currentItem URL];
     }
 
-    ASSERT(originalURL);
-    if ([download respondsToSelector:@selector(_setOriginatingURL:)])
+    if (originalURL && [download respondsToSelector:@selector(_setOriginatingURL:)])
         [download _setOriginatingURL:originalURL];
 }
 
