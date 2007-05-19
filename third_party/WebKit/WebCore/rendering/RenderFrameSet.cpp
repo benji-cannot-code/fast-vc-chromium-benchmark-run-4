c90852bc6c3347885e3d5cbd813d75d7f603c678
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-19 23:11:31 +0000

    2007-05-19  Mitz Pettel  <mitz@webkit.org>
            Reviewed by Dave Hyatt.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=13783
              REGRESSION (r21120): Frame resizers not repainted properly when dragged
    
            * rendering/RenderFrameSet.cpp:
            (WebCore::RenderFrameSet::layout):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21603 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderFrameSet.cpp
+++ third_party/WebKit/WebCore/rendering/RenderFrameSet.cpp
@@ -456,6 +456,11 @@ void RenderFrameSet::layout()
 {
     ASSERT(needsLayout());
 
+    bool doFullRepaint = selfNeedsLayout() && checkForRepaintDuringLayout();
+    IntRect oldBounds;
+    if (doFullRepaint)
+        oldBounds = absoluteClippedOverflowRect();
+
     if (!parent()->isFrameSet()) {
         FrameView* v = view()->frameView();
         m_width = v->visibleWidth();
@@ -479,6 +484,13 @@ void RenderFrameSet::layout()
     RenderContainer::layout();
 
     computeEdgeInfo();
+
+    if (doFullRepaint) {
+        view()->repaintViewRectangle(oldBounds);
+        IntRect newBounds = absoluteClippedOverflowRect();
+        if (newBounds != oldBounds)
+            view()->repaintViewRectangle(newBounds);
+    }
 
     setNeedsLayout(false);
 }
