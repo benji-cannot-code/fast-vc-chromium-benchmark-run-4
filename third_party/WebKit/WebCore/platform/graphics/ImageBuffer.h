effdba11bf8cbf90a17e5e0630a011371e81df27
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-20 17:45:02 +0000

    Add missing include for auto_ptr
    git-svn-id: svn://svn.chromium.org/blink/trunk@21609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/ImageBuffer.h
+++ third_party/WebKit/WebCore/platform/graphics/ImageBuffer.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "IntSize.h"
 #include <wtf/OwnPtr.h>
+#include <memory>
 
 #if PLATFORM(CG)
 typedef struct CGImage* CGImageRef;
