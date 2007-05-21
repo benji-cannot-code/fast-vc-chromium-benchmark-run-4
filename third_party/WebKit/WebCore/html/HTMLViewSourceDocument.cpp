c1d8cd2712c2b496a43831eecc6a39f8bf08842a
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-21 22:03:29 +0000

    Make a line gutter backdrop so that tiny view source docs don't look         bad.
            Reviewed by aroben
    
            * css/view-source.css:
            * html/HTMLViewSourceDocument.cpp:
            (WebCore::HTMLViewSourceDocument::createContainingTable):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLViewSourceDocument.cpp
+++ third_party/WebKit/WebCore/html/HTMLViewSourceDocument.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLHtmlElement.h"
 #include "HTMLAnchorElement.h"
 #include "HTMLBodyElement.h"
+#include "HTMLDivElement.h"
 #include "HTMLTableElement.h"
 #include "HTMLTableCellElement.h"
 #include "HTMLTableRowElement.h"
@@ -67,6 +68,17 @@ void HTMLViewSourceDocument::createContainingTable()
     Element* body = new HTMLBodyElement(this);
     html->addChild(body);
     body->attach();
+    
+    // Create a line gutter div that can be used to make sure the gutter extends down the height of the whole
+    // document.
+    Element* div = new HTMLDivElement(this);
+    Attribute* a = new MappedAttribute(classAttr, "webkit-line-gutter-backdrop");
+    NamedMappedAttrMap* attrs = new NamedMappedAttrMap(0);
+    attrs->insertAttribute(a, true);   
+    div->setAttributeMap(attrs);     
+    body->addChild(div);
+    div->attach();
+
     Element* table = new HTMLTableElement(this);
     body->addChild(table);
     table->attach();
