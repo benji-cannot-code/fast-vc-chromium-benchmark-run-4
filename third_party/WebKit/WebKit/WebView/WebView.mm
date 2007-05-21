71ee191414d5d187b0b96dd01bbcb9ee43e321c8
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-21 17:57:33 +0000

    Reviewed by Ada.
            <rdar://problem/5200816> REGRESSION: With Shiira 1.2.2 , I can't open embedded link in flash object by clicking (http:/www.adobe.com )
            
            Null check the request.
            
            * WebView/WebView.mm:
            (+[WebView _canHandleRequest:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21623 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebView.mm
+++ third_party/WebKit/WebKit/WebView/WebView.mm
@@ -1086,6 +1086,10 @@ WebFrameLoadDelegateImplementationCache WebViewGetFrameLoadDelegateImplementatio
 
 + (BOOL)_canHandleRequest:(NSURLRequest *)request
 {
+    // FIXME: If <rdar://problem/5217309> gets fixed, this check can be removed
+    if (!request)
+        return NO;
+    
     if ([NSURLConnection canHandleRequest:request]) {
         return YES;
     }
