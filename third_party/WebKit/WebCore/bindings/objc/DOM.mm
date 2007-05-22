bfb1974f0877cb4c92e67d576d3698a2709150b8
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-22 02:14:03 +0000

    Reviewed by Adam.
            <rdar://problem/5084400> -[DOMRange lineBoxRects] returns an empty array when the DOM nodes have just been created
    
            Do a updateLayoutIgnorePendingStylesheets before asking the Node or Range for it's box rects.
    
            * bindings/objc/DOM.mm:
            (-[DOMNode boundingBox]):
            (-[DOMNode lineBoxRects]):
            (-[DOMRange boundingBox]):
            (-[DOMRange lineBoxRects]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/DOM.mm
+++ third_party/WebKit/WebCore/bindings/objc/DOM.mm
@@ -400,6 +400,7 @@ static NSArray *kit(const Vector<IntRect>& rects)
 // If it was, we could even autogenerate.
 - (NSRect)boundingBox
 {
+    [self _node]->document()->updateLayoutIgnorePendingStylesheets();
     WebCore::RenderObject *renderer = [self _node]->renderer();
     if (renderer)
         return renderer->absoluteBoundingBoxRect();
@@ -410,6 +411,7 @@ static NSArray *kit(const Vector<IntRect>& rects)
 // If it was, we could even autogenerate.
 - (NSArray *)lineBoxRects
 {
+    [self _node]->document()->updateLayoutIgnorePendingStylesheets();
     WebCore::RenderObject *renderer = [self _node]->renderer();
     if (renderer) {
         Vector<WebCore::IntRect> rects;
@@ -425,12 +427,14 @@ static NSArray *kit(const Vector<IntRect>& rects)
 
 - (NSRect)boundingBox
 {
+    [self _range]->ownerDocument()->updateLayoutIgnorePendingStylesheets();
     return [self _range]->boundingBox();
 }
 
 - (NSArray *)lineBoxRects
 {
     Vector<WebCore::IntRect> rects;
+    [self _range]->ownerDocument()->updateLayoutIgnorePendingStylesheets();
     [self _range]->addLineBoxRects(rects);
     return kit(rects);
 }
