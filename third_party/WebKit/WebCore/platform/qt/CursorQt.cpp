66f078bb95bb4379bea96b3674bc7818bbf3c45a
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-24 03:28:50 +0000

    Fix the Qt build after r21685.
            * platform/qt/CursorQt.cpp:
            (WebCore::notAllowedCursor):
            (WebCore::zoomInCursor):
            (WebCore::zoomOutCursor):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21693 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/CursorQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/CursorQt.cpp
@@ -273,10 +273,19 @@ const Cursor& noneCursor()
 
 const Cursor& notAllowedCursor()
 {
-   //FIXME: Build fix -- what is correct here?
+   // FIXME: Build fix -- what is correct here?
    return Cursors::self()->BlankCursor;
 }
 
+const Cursor& zoomInCursor()
+{
+    return Cursors::self()->PointerCursor;
+}
+
+const Cursor& zoomOutCursor()
+{
+    return Cursors::self()->PointerCursor;
+}
 
 }
 
