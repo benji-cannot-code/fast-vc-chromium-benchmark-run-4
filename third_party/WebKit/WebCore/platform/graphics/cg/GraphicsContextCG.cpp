c006e6e8e8209032686dc912e7dca6155eb2a8be
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-24 17:45:27 +0000

    LayoutTests:
            Reviewed by Darin.
    
            - pixel test for http://bugs.webkit.org/show_bug.cgi?id=13837
              Separator in new <select> widget disappears when block element with border is in document.
    
            * fast/forms/menulist-separator-painting-expected.checksum: Added.
            * fast/forms/menulist-separator-painting-expected.png: Added.
            * fast/forms/menulist-separator-painting-expected.txt: Added.
            * fast/forms/menulist-separator-painting.html: Added.
    
    WebCore:
    
            Reviewed by Darin.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=13837
              Separator in new <select> widget disappears when block element with border is in document.
    
            Test: fast/forms/menulist-separator-painting.html
    
            * platform/graphics/cg/GraphicsContextCG.cpp:
            (WebCore::GraphicsContext::drawConvexPolygon): Changed this function to
            actually do both filling and stroking when needed, even though all existing
            callers set the stroke to none.
            * rendering/RenderThemeMac.mm:
            (WebCore::RenderThemeMac::paintMenuListButton): Added calls to set the stroke
            style and thickness when painting the arrows and the separator.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21719 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
@@ -370,7 +370,7 @@ void GraphicsContext::strokeArc(const IntRect& rect, int startAngle, int angleSp
 
 void GraphicsContext::drawConvexPolygon(size_t npoints, const FloatPoint* points, bool shouldAntialias)
 {
-    if (paintingDisabled())
+    if (paintingDisabled() || !fillColor().alpha() && (strokeThickness() <= 0 || strokeStyle() == NoStroke))
         return;
 
     if (npoints <= 1)
@@ -388,10 +388,12 @@ void GraphicsContext::drawConvexPolygon(size_t npoints, const FloatPoint* points
         CGContextAddLineToPoint(context, points[i].x(), points[i].y());
     CGContextClosePath(context);
 
-    if (fillColor().alpha())
-        CGContextEOFillPath(context);
-
-    if (strokeStyle() != NoStroke)
+    if (fillColor().alpha()) {
+        if (strokeStyle() != NoStroke)
+            CGContextDrawPath(context, kCGPathEOFillStroke);
+        else
+            CGContextEOFillPath(context);
+    } else
         CGContextStrokePath(context);
 
     CGContextRestoreGState(context);
