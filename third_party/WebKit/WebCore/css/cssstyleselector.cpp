d921d8f8f729986316e4588fb25d42fa958338ac
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-24 03:07:43 +0000

    Fix for <rdar://problem/5222611> REGRESSION: Paint ordering problems at         http://buymeasoda.com/dev/issues/image_onload/image_onload.html
            Clamp z-index so that values that exceed maxint (or that are below minint)
            are clamped to those values.
    
            Reviewed by darin
    
            * css/cssstyleselector.cpp:
            (WebCore::CSSStyleSelector::applyProperty):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21691 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/cssstyleselector.cpp
+++ third_party/WebKit/WebCore/css/cssstyleselector.cpp
@@ -3106,7 +3106,11 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
             style->setHasAutoZIndex();
             return;
         }
-        style->setZIndex((int)primitiveValue->getFloatValue());
+        
+        // FIXME: Should clamp all sorts of other integer properties too.
+        const double minIntAsDouble = INT_MIN;
+        const double maxIntAsDouble = INT_MAX;
+        style->setZIndex(max(minIntAsDouble, min(primitiveValue->getFloatValue(), maxIntAsDouble)));
         return;
     }
     case CSS_PROP_WIDOWS:
@@ -4027,7 +4031,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
         if (primitiveValue->getIdent() == CSS_VAL_INFINITE)
             style->setMarqueeLoopCount(-1); // -1 means repeat forever.
         else if (primitiveValue->primitiveType() == CSSPrimitiveValue::CSS_NUMBER)
-            style->setMarqueeLoopCount((int)(primitiveValue->getFloatValue()));
+            style->setMarqueeLoopCount((int)primitiveValue->getFloatValue());
         return;
     }
     case CSS_PROP__WEBKIT_MARQUEE_SPEED: {
