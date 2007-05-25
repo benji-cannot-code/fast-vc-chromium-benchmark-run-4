05e60dee655a08bb564b95a2f4afe5e481435922
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-25 13:37:22 +0000

    For the keyDown binding use QStringList instead of QList<QString>. The former is a default registered metatype. Also print out critical messages in dumprendertree's qt message handler.
    git-svn-id: svn://svn.chromium.org/blink/trunk@21752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.qtproj/main.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.qtproj/main.cpp
@@ -43,8 +43,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <signal.h>
 
-void messageHandler(QtMsgType, const char *)
+void messageHandler(QtMsgType type, const char *message)
 {
+    if (type == QtCriticalMsg) {
+        fprintf(stderr, "%s\n", message);
+        return;
+    }
     // do nothing
 }
 
