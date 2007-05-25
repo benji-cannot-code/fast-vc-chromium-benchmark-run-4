879141b21d089383c8d5195cdb3741498371651c
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-25 18:39:35 +0000

    Fix build.
            * DumpRenderTree/DumpRenderTree.m:
            * DumpRenderTree/FrameLoadDelegate.m:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21766 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebPreferences.h>
 #import <WebKit/WebPreferencesPrivate.h>
 #import <WebKit/WebResourceLoadDelegate.h>
-#import <WebCore/WebScriptObjectPendingPublic.h>
 #import <WebKit/WebViewPrivate.h>
 #import <JavaScriptCore/Assertions.h>
 #import <getopt.h>
--- third_party/WebKit/WebKitTools/DumpRenderTree/FrameLoadDelegate.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/FrameLoadDelegate.m
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKit.h>
 #import <WebKit/WebHTMLViewPrivate.h>
 #import <WebKit/WebFramePrivate.h>
-#import <WebCore/WebScriptObjectPendingPublic.h>
 
 @interface NSURL (DRTExtras)
 - (NSString *)_drt_descriptionSuitableForTestResult;
