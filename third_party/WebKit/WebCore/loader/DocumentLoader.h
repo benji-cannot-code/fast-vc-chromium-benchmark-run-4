ec1e5347fd26b47bc4c00a35577215946758a138
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-25 22:43:57 +0000

    Reviewed by Brady.
            <rdar://problem/5209536>
            http://bugs.webkit.org/show_bug.cgi?id=13757
            REGRESSION: Leaks seen on buildbot after r21497.
            
            Fix multipart data loading so it works like Tiger Safari. When one part of a multipart connection has been loaded,
            move the subresource loader from the subresource loaders set and into a special multipart subresource loader set. This set 
            is not considered when asking a document loader if it's currently loading. 
                    
            * loader/DocumentLoader.cpp:
            (WebCore::DocumentLoader::stopLoading):
            Always stop the multipart subresource loaders here.
            
            (WebCore::DocumentLoader::subresourceLoaderFinishedLoadingOnePart):
            New function that moves the loader from m_subresourceLoaders to m_multipartSubresourceLoaders and updates the document loader's state.
            
            * loader/ProgressTracker.cpp:
            (WebCore::ProgressTracker::incrementProgress):
            If a progress item already exists, reuse it. This can happen in some rare circumstances when dealing with multipart data.
            
            * loader/SubresourceLoader.cpp:
            (WebCore::SubresourceLoader::didReceiveResponse):
            Call subresourceLoaderFinishedLoadingOnePart if the response is multipart.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/DocumentLoader.h
+++ third_party/WebKit/WebCore/loader/DocumentLoader.h
@@ -149,6 +149,8 @@ namespace WebCore {
         void addPlugInStreamLoader(ResourceLoader*);
         void removePlugInStreamLoader(ResourceLoader*);
 
+        void subresourceLoaderFinishedLoadingOnePart(ResourceLoader*);
+        
     private:
         void setupForReplace();
         void commitIfReady();
@@ -161,6 +163,7 @@ namespace WebCore {
 
         RefPtr<MainResourceLoader> m_mainResourceLoader;
         ResourceLoaderSet m_subresourceLoaders;
+        ResourceLoaderSet m_multipartSubresourceLoaders;
         ResourceLoaderSet m_plugInStreamLoaders;
 
         RefPtr<SharedBuffer> m_mainResourceData;
