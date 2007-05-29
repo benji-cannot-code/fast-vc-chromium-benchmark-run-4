c077e5f5f47abc2b066e50f3061aa2cff91956ff
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-29 22:09:29 +0000

    Tweak my noscript fix to just always allow it inside the head.
    git-svn-id: svn://svn.chromium.org/blink/trunk@21866 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLHeadElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLHeadElement.cpp
@@ -26,9 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "HTMLHeadElement.h"
 
-#include "Document.h"
 #include "HTMLNames.h"
-#include "Settings.h"
 #include "Text.h"
 
 namespace WebCore {
@@ -65,13 +63,7 @@ bool HTMLHeadElement::childAllowed(Node* newChild)
 
 bool HTMLHeadElement::checkDTD(const Node* newChild)
 {
-    if (newChild->hasTagName(noscriptTag)) {
-        // Allow <noscript> inside the <head> when script is enabled, since the
-        // contents are just raw text that won't be displayed.
-        Settings* settings = document()->settings();
-        return settings && settings->isJavaScriptEnabled();
-    }
-    return newChild->hasTagName(titleTag) || newChild->hasTagName(isindexTag) ||
+    return newChild->hasTagName(noscriptTag) || newChild->hasTagName(titleTag) || newChild->hasTagName(isindexTag) ||
            newChild->hasTagName(baseTag) || newChild->hasTagName(scriptTag) ||
            newChild->hasTagName(styleTag) || newChild->hasTagName(metaTag) ||
            newChild->hasTagName(linkTag) || newChild->isTextNode();
