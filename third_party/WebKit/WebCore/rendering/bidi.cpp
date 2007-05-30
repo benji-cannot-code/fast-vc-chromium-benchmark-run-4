97ec6d536d246fb51a88af5bb30c0219a590c32f
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-30 05:32:40 +0000

    Fold the invalidateVerticalPositions crawl into the bidi iteration.         This makes it more correct (since we won't drill into elements that         did not need to be reset) and cuts out an extra walk of all our children.         We're still O(n^2) as new content streams in, but this at least cuts         out one separate O(n^2) crawl.
            Reviewed by olliej
    
            * rendering/RenderObject.cpp:
            * rendering/RenderObject.h:
            (WebCore::RenderObject::invalidateVerticalPosition):
            * rendering/bidi.cpp:
            (WebCore::RenderBlock::layoutInlineChildren):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21879 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/bidi.cpp
+++ third_party/WebKit/WebCore/rendering/bidi.cpp
@@ -1576,10 +1576,10 @@ void RenderBlock::layoutInlineChildren(bool relayoutChildren, int& repaintTop, i
 
     bool useRepaintBounds = false;
 
+    invalidateVerticalPosition();
+    
     m_overflowHeight = 0;
-    
-    invalidateVerticalPositions();
-    
+        
     m_height = borderTop() + paddingTop();
     int toAdd = borderBottom() + paddingBottom() + horizontalScrollbarHeight();
     
@@ -1604,9 +1604,10 @@ void RenderBlock::layoutInlineChildren(bool relayoutChildren, int& repaintTop, i
     if (firstChild()) {
         // layout replaced elements
         bool endOfInline = false;
-        RenderObject *o = bidiFirst(this, bidi, false);
+        RenderObject* o = bidiFirst(this, bidi, false);
         bool hasFloat = false;
         while (o) {
+            o->invalidateVerticalPosition();
             if (o->isReplaced() || o->isFloating() || o->isPositioned()) {
                 if (relayoutChildren || o->style()->width().isPercent() || o->style()->height().isPercent())
                     o->setChildNeedsLayout(true, false);
