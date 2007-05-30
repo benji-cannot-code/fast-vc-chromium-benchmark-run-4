99de71d0ea741e5d67b23ab55f2ee3a5f5b24310
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-05-30 21:08:16 +0000

    LayoutTests:
            Reviewed by Dr. Harrison.
    
            Patch for http://bugs.webkit.org/show_bug.cgi?id=13868
            REGRESSION: crash on accessing a new iframe's contentWindow property
    
            * fast/dom/frame-contentWindow-crash-expected.txt: Added.
            * fast/dom/frame-contentWindow-crash.html: Added.
            * fast/dom/iframe-contentWindow-crash-expected.txt: Added.
            * fast/dom/iframe-contentWindow-crash.html: Added.
    
    WebCore:
    
            Reviewed by Dr. Harrison.
    
            Patch for http://bugs.webkit.org/show_bug.cgi?id=13868
            REGRESSION: crash on accessing a new iframe's contentWindow property
    
            Adds a null check in for the contentWindow property for HTMLIFrameElement
            and HTMLFrameElement.
    
            * bindings/scripts/CodeGeneratorJS.pm:
            * html/HTMLFrameElement.idl:
            * html/HTMLIFrameElement.idl:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@21902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -908,6 +908,8 @@ sub GenerateImplementation
                         push(@implContent, "        }\n\n");
                         push(@implContent, "        return toJS(exec, obj.get());\n");
                         push(@implContent, "    }\n");
+                    } elsif ($attribute->signature->extendedAttributes->{"NullCheck"}) {
+                        push(@implContent, "        return imp->$name() ? $jsType : jsUndefined();\n");
                     } else {
                         push(@implContent, "        return $jsType;\n");
                     }
@@ -918,6 +920,8 @@ sub GenerateImplementation
         
                 if ($podType) {
                     push(@implContent, "        KJS::JSValue* result = " . NativeToJSValue($attribute->signature, "", "imp.$name(ec)") . ";\n");
+                } elsif ($attribute->signature->extendedAttributes->{"NullCheck"}) {
+                    push(@implContent, "        return imp->$name(ec) ? " . NativeToJSValue($attribute->signature, $implClassNameForValueConversion, "imp->$name(ec)") . " : jsUndefined();\n");
                 } else {
                     push(@implContent, "        KJS::JSValue* result = " . NativeToJSValue($attribute->signature, $implClassNameForValueConversion, "imp->$name(ec)") . ";\n");
                 }
