2df8c64a30a8326164fb43b4c8717e528878e687
rwlbuis <rwlbuis@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-04 13:41:08 +0000

    Fix the svg experimental build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@21967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGFEFloodElement.cpp
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGFEFloodElement.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGFEFloodElement.h"
 
 #include "Attr.h"
+#include "RenderStyle.h"
 #include "SVGNames.h"
 #include "SVGRenderStyle.h"
 #include "SVGResourceFilter.h"
