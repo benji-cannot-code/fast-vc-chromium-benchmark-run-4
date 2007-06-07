2f35648cc8154cabc75e1ebd93073ad035604a60
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-07 02:36:49 +0000

    Reviewed by Anders.
            Fix release build.
    
            * bindings/objc/DOM.mm:
            (-[DOMDocument createNodeIterator:whatToShow:filter:expandEntityReferences:]): Add missing null initialization.
            (-[DOMDocument createTreeWalker:whatToShow:filter:expandEntityReferences:]): Add missing null initialization.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@22036 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/DOM.mm
+++ third_party/WebKit/WebCore/bindings/objc/DOM.mm
@@ -701,7 +701,7 @@ short ObjCNodeFilterCondition::acceptNode(WebCore::Node* node) const
 
 - (DOMNodeIterator *)createNodeIterator:(DOMNode *)root whatToShow:(unsigned)whatToShow filter:(id <DOMNodeFilter>)filter expandEntityReferences:(BOOL)expandEntityReferences
 {
-    WebCore::NodeFilter* cppFilter;
+    WebCore::NodeFilter* cppFilter = 0;
     if (filter)
         cppFilter = new WebCore::NodeFilter(new ObjCNodeFilterCondition(filter));
     WebCore::ExceptionCode ec = 0;
@@ -712,7 +712,7 @@ short ObjCNodeFilterCondition::acceptNode(WebCore::Node* node) const
 
 - (DOMTreeWalker *)createTreeWalker:(DOMNode *)root whatToShow:(unsigned)whatToShow filter:(id <DOMNodeFilter>)filter expandEntityReferences:(BOOL)expandEntityReferences
 {
-    WebCore::NodeFilter* cppFilter;
+    WebCore::NodeFilter* cppFilter = 0;
     if (filter)
         cppFilter = new WebCore::NodeFilter(new ObjCNodeFilterCondition(filter));
     WebCore::ExceptionCode ec = 0;
