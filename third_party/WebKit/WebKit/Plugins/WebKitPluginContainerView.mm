f55afd0952bd380cefd8f59f6e347c8abb4b200f
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-10 07:14:52 +0000

    Fix for bug 14037, make sure respondsToSelector does the write thing         when invoked on a WebKit plugin's container view.
            Reviewed by Mark Rowe
    
            * Plugins/WebKitPluginContainerView.mm:
            (-[WebKitPluginContainerView respondsToSelector:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@22084 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins/WebKitPluginContainerView.mm
+++ third_party/WebKit/WebKit/Plugins/WebKitPluginContainerView.mm
@@ -55,6 +55,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return NSIntersectionRect([self convertRect:[_element _windowClipRect] fromView:nil], [super visibleRect]);
 }
 
+- (BOOL)respondsToSelector:(SEL)selector
+{
+    if (selector == @selector(objectForWebScript))
+        return [[[self subviews] objectAtIndex: 0] respondsToSelector:selector];
+    return [super respondsToSelector:selector];  
+}
+
 - (id)objectForWebScript
 {
     return [[[self subviews] objectAtIndex: 0] objectForWebScript];
