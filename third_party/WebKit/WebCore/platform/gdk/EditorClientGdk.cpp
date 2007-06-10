51946652297ddb9142d0e404d97dcd380baf180e
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-10 22:58:33 +0000

    2007-06-10  Holger Hans Peter Freyther  <zecke@selfish.org>
             Reviewed by Maciej.
     
             Disable whole-view editing for the Gdk port in order to improve the browsing experience.
     
             * platform/gdk/EditorClientGdk.cpp:
             (WebCore::EditorClientGdk::isEditable): Return false to disable whole-view editing
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@22088 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gdk/EditorClientGdk.cpp
+++ third_party/WebKit/WebCore/platform/gdk/EditorClientGdk.cpp
@@ -144,7 +144,7 @@ bool EditorClientGdk::selectWordBeforeMenuEvent()
 bool EditorClientGdk::isEditable()
 {
     notImplemented();
-    return true;
+    return false;
 }
 
 void EditorClientGdk::registerCommandForUndo(WTF::PassRefPtr<WebCore::EditCommand>)
