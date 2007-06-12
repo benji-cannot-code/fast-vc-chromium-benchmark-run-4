28a22c7013b70c1f8bc7cbdf929c719e78f30d5a
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-12 23:58:50 +0000

    WebKitTools:
            Land CygwinDownloader.
    
            Rubberstamped by Hyatt.
    
            * CygwinDownloader/cygwin-downloader.py: Added.
            * CygwinDownloader/cygwin-downloader.zip: Added.
            * CygwinDownloader/make-zip.sh: Added.
            * CygwinDownloader/setup.py: Added.
    
    WebKitSite:
    
            Update the location of cygwin-downloader.zip.
    
            Rubberstamped by Hyatt.
    
            * building/tools.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23466 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKitTools/CygwinDownloader/make-zip.sh
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+PYTHONEXE=$(cygpath -u "${SYSTEMDRIVE}\\Python25\\python.exe")
+ZIPNAME="cygwin-downloader.zip"
+
+if [[ ! -f "${PYTHONEXE}" ]]; then
+        echo "Couldn't find python.exe at ${PYTHONEXE}" 1>&2
+        exit 1
+fi
+
+"${PYTHONEXE}" setup.py py2exe || {
+        echo "Failed executing setup.py" 1>&2
+        exit 1
+}
+
+rm -f "${ZIPNAME}"
+
+cd dist
+
+zip -r ../"${ZIPNAME}" * || {
+        echo "Failed to create cygwin-downloader" 1>&2
+        exit 1
+}
+
+cd ..
+
+rm -rf build dist || {
+        echo "Failed to cleanup cygwin-downloader and build directories" 1>&2
+        exit 1
+}
---
+++ third_party/WebKit/WebKitTools/CygwinDownloader/setup.py
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from distutils.core import setup
+import py2exe
+
+setup(console=['cygwin-downloader.py'])
