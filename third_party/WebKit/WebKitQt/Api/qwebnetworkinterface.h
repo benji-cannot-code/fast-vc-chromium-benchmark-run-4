04335edee1a0b8e31039d84bfe2ce29dc925443f
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-13 13:05:01 +0000

    Added a QWebNetworkRequest convenience constructor.
    git-svn-id: svn://svn.chromium.org/blink/trunk@23498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitQt/Api/qwebnetworkinterface.h
+++ third_party/WebKit/WebKitQt/Api/qwebnetworkinterface.h
@@ -51,6 +51,7 @@ public:
     };
 
     QWebNetworkRequest();
+    explicit QWebNetworkRequest(const QUrl &url, Method method = Get, const QByteArray &postData = QByteArray());
     QWebNetworkRequest(const QWebNetworkRequest &other);
 
     QWebNetworkRequest &operator=(const QWebNetworkRequest &other);
--- third_party/WebKit/WebKitQt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKitQt/Api/qwebpage.cpp
@@ -132,9 +132,7 @@ QWebFrame *QWebPage::createFrame(QWebFrame *parentFrame, QWebFrameData *frameDat
 
 void QWebPage::open(const QUrl &url)
 {
-    QWebNetworkRequest request;
-    request.setUrl(url);
-    open(request);
+    open(QWebNetworkRequest(url));
 }
 
 void QWebPage::open(const QWebNetworkRequest &req)
