0b3b44f945c16fd33082d134cec70cc295a9d754
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-14 02:23:29 +0000

    Disable FastMalloc for the Qt build and make sure we         don't reimplement the global new/delete operators         when using the system malloc.
    git-svn-id: svn://svn.chromium.org/blink/trunk@23515 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/FastMalloc.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/FastMalloc.cpp
@@ -71,12 +71,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <pthread.h>
 #endif
 
-#ifndef USE_SYSTEM_MALLOC
-#ifndef NDEBUG
-#define USE_SYSTEM_MALLOC 1
+#if !defined(USE_SYSTEM_MALLOC) && defined(NDEBUG)
+#define FORCE_SYSTEM_MALLOC 0
 #else
-#define USE_SYSTEM_MALLOC 0
-#endif
+#define FORCE_SYSTEM_MALLOC 1
 #endif
 
 #ifndef NDEBUG
@@ -130,7 +128,7 @@ void fastMallocAllow()
 } // namespace WTF
 #endif // NDEBUG
 
-#if USE_SYSTEM_MALLOC
+#if FORCE_SYSTEM_MALLOC
 
 #include <stdlib.h>
 #if !PLATFORM(WIN_OS)
--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -198,4 +198,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_USE_CURL 1
 #endif
 
+#if PLATFORM(QT)
+#define USE_SYSTEM_MALLOC 1
+#endif
+
 #endif /* WTF_Platform_h */
