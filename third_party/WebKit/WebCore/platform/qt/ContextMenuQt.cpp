e406df0725e15fa1f1ae555755019031d4de3fbd
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-14 16:47:58 +0000

    Add missing null-init of pointer (will fix an upcoming crash)
    git-svn-id: svn://svn.chromium.org/blink/trunk@23527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/ContextMenuQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/ContextMenuQt.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 ContextMenu::ContextMenu(const HitTestResult& result)
-    : m_hitTestResult(result)
+    : m_hitTestResult(result), m_menu(0)
 {
 }
 
