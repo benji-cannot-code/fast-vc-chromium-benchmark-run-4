df77b11b621daa0b879a9daace6d432d0c2f7761
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-14 04:58:04 +0000

    JavaScriptCore:
            Reviewed by Mark Rowe.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=14132
              array sort with > 10000 elements sets elements > 10000 undefined
    
            Test: fast/js/sort-large-array.html
    
            * kjs/array_instance.h: Replaced pushUndefinedObjectsToEnd with
            compactForSorting, and removed ExecState parameters.
    
            * kjs/array_object.cpp:
            (ArrayInstance::sort): Changed to call compactForSorting.
            (ArrayInstance::compactForSorting): Do the get and delete of the
            properties directly on the property map instead of using public
            calls from JSObject. The public calls would just read the undefined
            values from the compacted sort results array!
    
    LayoutTests:
    
            Reviewed by Mark Rowe.
    
            - test for http://bugs.webkit.org/show_bug.cgi?id=14132
              array sort with > 10000 elements sets elements > 10000 undefined
    
            * fast/js/resources/sort-large-array.js: Added.
            * fast/js/sort-large-array-expected.txt: Added.
            * fast/js/sort-large-array.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23521 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_instance.h
+++ third_party/WebKit/JavaScriptCore/kjs/array_instance.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  *  This file is part of the KDE libraries
  *  Copyright (C) 1999-2000 Harri Porten (porten@kde.org)
- *  Copyright (C) 2003 Apple Computer, Inc.
+ *  Copyright (C) 2003, 2007 Apple Inc. All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -58,7 +58,7 @@ namespace KJS {
 
     void setLength(unsigned newLength, ExecState *exec);
     
-    unsigned pushUndefinedObjectsToEnd(ExecState *exec);
+    unsigned compactForSorting();
     
     void resizeStorage(unsigned);
 
