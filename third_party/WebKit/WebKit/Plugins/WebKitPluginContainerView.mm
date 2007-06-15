58fc4432850b70a099c6ed3df684b2a634147b9f
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-15 22:07:55 +0000

    Reviewed by Kevin.
            A better fix for <rdar://problem/5271774>. Only try to access the element if the
            view has an associated window. This also works with GC. (Fix suggested by Kevin.)
    
            * Plugins/WebKitPluginContainerView.mm:
            (-[WebKitPluginContainerView dealloc]):
            (-[WebKitPluginContainerView visibleRect]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23553 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Plugins/WebKitPluginContainerView.mm
+++ third_party/WebKit/WebKit/Plugins/WebKitPluginContainerView.mm
@@ -46,16 +46,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     [_element release];
     
-    // Calling [super dealloc] can end up calling visibleRect so we need to set
-    // the _element instance to 0 here so we can check for it in our visibleRect.
-    _element = 0;
-    
     [super dealloc];
 }
 
 - (NSRect)visibleRect
 {
-    if (!_element)
+    if (![self window])
         return [super visibleRect];
     
     // WebCore may impose an additional clip (via CSS overflow or clip properties).  Fetch
