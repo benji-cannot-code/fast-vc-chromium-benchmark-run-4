7df10e9d5e964f40734d7832341c03684ae5b923
kdecker <kdecker@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-19 00:01:37 +0000

    Reviewed by Tim Hatcher.
            Fixed: <rdar://problem/5276135> With Safari 3 Tiger Beta installed, a crash occurs in BBEdit while mousing down and dragging outside of HTML preview window
    
            * Carbon/HIWebView.m:
            (WindowHandler): Because the fix for 5051616 causes Tiger to crash in HIToolbox (but not on Leopard), I reverted back to using GetControlKind
            on Tiger only, instead of HIObjectIsOfClass.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Carbon/HIWebView.m
+++ third_party/WebKit/WebKit/Carbon/HIWebView.m
@@ -758,7 +758,12 @@ WindowHandler( EventHandlerCallRef inCallRef, EventRef inEvent, void* inUserData
 
                             HIViewRef view;
                             HIViewGetViewForMouseEvent(HIViewGetRoot(window), inEvent, &view);
+#ifndef BUILDING_ON_TIGER
                             if (HIObjectIsOfClass((HIObjectRef)view, kHIWebViewClassID))
+#else
+                            GetControlKind(view, &kind);
+                            if ( kind.signature == 'appl' && kind.kind == 'wbvw' )
+#endif
                                 result = SendEventToEventTargetWithOptions(inEvent, HIObjectGetEventTarget((HIObjectRef)view), kEventTargetDontPropagate);
                         }
                     }
@@ -770,7 +775,12 @@ WindowHandler( EventHandlerCallRef inCallRef, EventRef inEvent, void* inUserData
                     {
                         HIViewRef view;
                         HIViewGetViewForMouseEvent(HIViewGetRoot(window), inEvent, &view);
+#ifndef BUILDING_ON_TIGER
                         if (HIObjectIsOfClass((HIObjectRef)view, kHIWebViewClassID))
+#else
+                        GetControlKind(view, &kind);
+                        if ( kind.signature == 'appl' && kind.kind == 'wbvw' )
+#endif                        
                             result = SendEventToEventTargetWithOptions(inEvent, HIObjectGetEventTarget((HIObjectRef)view), kEventTargetDontPropagate);
                     }
                     break;
