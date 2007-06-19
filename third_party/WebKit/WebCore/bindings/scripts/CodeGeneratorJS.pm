d8121ea9f8eb2863ad8aef4132bd703928564604
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-19 19:28:14 +0000

    LayoutTests:
            Reviewed by Darin Adler.
    
            Tests for cross-frame access. <rdar://problem/5251309>.
    
            * http/tests/security/cross-frame-access-expected.txt: Added.
            * http/tests/security/cross-frame-access-first-time-expected.txt: Added.
            * http/tests/security/cross-frame-access-first-time.html: Added.
            * http/tests/security/cross-frame-access.html: Added.
            * http/tests/security/resources/cross-frame-iframe.html: Added.
    
    WebCore:
    
            Reviewed by Darin Adler.
    
            Fixed cross-frame access. <rdar://problem/5251309>. 
    
            * bindings/js/kjs_window.cpp:
            (KJS::Window::getValueProperty):
            * bindings/scripts/CodeGeneratorJS.pm:
            * page/DOMWindow.idl:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -532,8 +532,7 @@ sub GenerateHeader
 
 sub GenerateImplementation
 {
-    my $object = shift;
-    my $dataNode = shift;
+    my ($object, $dataNode) = @_;
 
     my $interfaceName = $dataNode->name;
     my $className = "JS$interfaceName";
@@ -865,6 +864,10 @@ sub GenerateImplementation
 
             if ($attribute->signature->extendedAttributes->{"Custom"}) {
                 push(@implContent, "    case " . WK_ucfirst($name) . "AttrNum:\n");
+                if ($dataNode->extendedAttributes->{"CheckDomainSecurity"} && !$attribute->signature->extendedAttributes->{"DoNotCheckDomainSecurity"}) {
+                    push(@implContent, "        if (!isSafeScript(exec))\n");
+                    push(@implContent, "            return jsUndefined();");
+                }
                 push(@implContent, "        return $name(exec);\n");
             } elsif ($attribute->signature->extendedAttributes->{"CheckFrameSecurity"}) {
                 push(@implContent, "    case " . WK_ucfirst($name) . "AttrNum:\n");
@@ -876,9 +879,17 @@ sub GenerateImplementation
                 $constructorType =~ s/Constructor$//;
 
                 push(@implContent, "    case " . $name . "ConstructorAttrNum:\n");
+                if ($dataNode->extendedAttributes->{"CheckDomainSecurity"} && !$attribute->signature->extendedAttributes->{"DoNotCheckDomainSecurity"}) {
+                    push(@implContent, "        if (!isSafeScript(exec))\n");
+                    push(@implContent, "            return jsUndefined();\n");
+                }
                 push(@implContent, "        return JS" . $constructorType . "::getConstructor(exec);\n");
             } elsif (!@{$attribute->getterExceptions}) {
                 push(@implContent, "    case " . WK_ucfirst($name) . "AttrNum:\n");
+                if ($dataNode->extendedAttributes->{"CheckDomainSecurity"} && !$attribute->signature->extendedAttributes->{"DoNotCheckDomainSecurity"}) {
+                    push(@implContent, "        if (!isSafeScript(exec))\n");
+                    push(@implContent, "            return jsUndefined();\n");
+                }
         
                 if ($podType) {
                     if ($podType eq "double") { # Special case for JSSVGNumber
@@ -914,6 +925,10 @@ sub GenerateImplementation
                 }
             } else {
                 push(@implContent, "    case " . WK_ucfirst($name) . "AttrNum: {\n");
+                if ($dataNode->extendedAttributes->{"CheckDomainSecurity"} && !$attribute->signature->extendedAttributes->{"DoNotCheckDomainSecurity"}) {
+                    push(@implContent, "        if (!isSafeScript(exec))\n");
+                    push(@implContent, "            return jsUndefined();\n");
+                }
                 push(@implContent, "        ExceptionCode ec = 0;\n");
         
                 if ($podType) {
