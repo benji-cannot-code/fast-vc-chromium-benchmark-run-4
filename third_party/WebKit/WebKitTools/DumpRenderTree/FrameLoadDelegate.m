d141c0a17f7ef0c04383026c261cd460cbc324dd
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-21 21:14:12 +0000

    LayoutTests:
            Reviewed by Geoff.
            
            Update test results.
    
            * http/tests/loading/bad-scheme-subframe-expected.txt:
            * http/tests/loading/bad-server-subframe-expected.txt:
            * http/tests/loading/basic-expected.txt:
            * http/tests/loading/empty-subframe-expected.txt:
            * http/tests/loading/simple-subframe-expected.txt:
            * webarchive/loading/test-loading-archive-expected.txt:
    
    WebKitTools:
    
            Reviewed by Geoff.
    
            Don't dump didFirstLayout callbacks, they happen intermittently.
            
            * DumpRenderTree/FrameLoadDelegate.m:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23723 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/FrameLoadDelegate.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/FrameLoadDelegate.m
@@ -298,14 +298,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
 }
 
-- (void)webView:(WebView *)sender didFirstLayoutInFrame:(WebFrame *)frame;
-{
-    if (shouldDumpFrameLoadCallbacks && !done) {
-        NSString *string = [NSString stringWithFormat:@"%@ - didFirstLayoutInFrame", [frame _drt_descriptionSuitableForTestResult]];
-        printf ("%s\n", [string UTF8String]);
-    }
-}
-
 - (void)webView:(WebView *)sender didFinishDocumentLoadForFrame:(WebFrame *)frame;
 {
     if (shouldDumpFrameLoadCallbacks && !done) {
