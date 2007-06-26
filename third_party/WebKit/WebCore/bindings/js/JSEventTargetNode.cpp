2dec0f88ed21407376e421cd4a03aa025db602ab
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-26 19:55:22 +0000

    LayoutTests:
            Reviewed by Geoff.
    
            Update tests for http://bugs.webkit.org/show_bug.cgi?id=14111
            Autogenerate Event JS binding
    
            * fast/dom/prototype-chain-expected.txt: DOMEventPrototype is no longer
            in the prototype chain.
            * fast/dom/xmlhttprequest-get-expected.txt:
    
    WebCore:
    
            Reviewed by Geoff.
    
            Patch for http://bugs.webkit.org/show_bug.cgi?id=14111
            Autogenerate Event JS binding
    
            - Moves the dataTransfer method into MouseEvent where it should be.
    
            * WebCore.xcodeproj/project.pbxproj:
            * bindings/js/JSEventCustom.cpp: Added.
            (WebCore::JSEvent::clipboardData):
            (WebCore::toJS):
            * bindings/js/JSEventTargetNode.cpp:
            * bindings/js/JSXMLHttpRequest.cpp:
            * bindings/js/kjs_events.cpp:
            * bindings/js/kjs_events.h:
            * bindings/objc/DOMUtility.mm:
            (KJS::createDOMWrapper):
            * bindings/scripts/CodeGeneratorJS.pm:
            * dom/Event.h:
            (WebCore::Event::returnValue):
            (WebCore::Event::setReturnValue):
            (WebCore::Event::clipboardData):
            * dom/Event.idl:
            * dom/MouseEvent.h:
            (WebCore::MouseEvent::dataTransfer):
            * dom/MouseEvent.idl:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSEventTargetNode.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSEventTargetNode.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EventListener.h"
 #include "EventNames.h"
 #include "EventTargetNode.h"
+#include "JSEvent.h"
 #include "Node.h"
 #include "kjs_events.h"
 #include "kjs_window.h"
--- third_party/WebKit/WebCore/bindings/js/JSXMLHttpRequest.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSXMLHttpRequest.cpp
@@ -27,15 +27,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoader.h"
 #include "HTMLDocument.h"
 #include "JSDocument.h"
+#include "JSEvent.h"
 #include "kjs_events.h"
 #include "kjs_window.h"
 #include "xmlhttprequest.h"
 
 #include "JSXMLHttpRequest.lut.h"
 
-using namespace WebCore;
-
 namespace KJS {
+
+using namespace WebCore;
 
 ////////////////////// JSXMLHttpRequest Object ////////////////////////
 
