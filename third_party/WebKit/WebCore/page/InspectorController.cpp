42aa94ceff6ec427af4f75c59b0b54f22b9045c4
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-26 00:51:43 +0000

    Reviewed by Adam Roben.
            Patch for http://bugs.webkit.org/show_bug.cgi?id=14257
            Web Inspector lists favicon.ico as "other" instead of "image"
    
            * page/InspectorController.cpp:
            (WebCore::InspectorResource::type):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/InspectorController.cpp
+++ third_party/WebKit/WebCore/page/InspectorController.cpp
@@ -122,6 +122,9 @@ struct InspectorResource : public Shared<InspectorResource> {
         if (!frameLoader)
             return Other;
 
+        if (request.url() == frameLoader->iconURL())
+            return Image;
+
         Document* doc = frameLoader->frame()->document();
         if (!doc)
             return Other;
