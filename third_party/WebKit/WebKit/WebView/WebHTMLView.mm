e349531951df60bdc2df69d638478613f3b1ec1e
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-27 08:19:53 +0000

    Reviewed by Maciej.
            Hopefully fix remainder of the IME issues on Mac.  
            We now assume that the IME silently consumes any event given
            to it during text composition, and only override this assumption
            if the NSTextInput or NSResponder callbacks are made.
    
            This prevents us from treating those events that the IME has 
            consumed internally (eg. candidate window navigation) as unhandled
            events that should be bubbled.
    
            This fixes:
              <rdar://problem/5107538> Major problems handling key press event with non-english Input Methods
              <rdar://problem/4196249> REGRESSION: Mail: Inputting space (U+0020) with IM deletes subsequent line breaks on Mail.app
              <rdar://problem/5015544> REGRESSION: Reverse conversion keyboard command does not work in Safari.
              <rdar://problem/5045121> REGRESSION: Inline is confirmed after press left/right arrow keys, happens in Mail but not in TextEdit.
              <rdar://problem/5076807> REGRESSION: Can't undo conversion of inline text (by hitting ESC)
              <rdar://problem/5085781> REGRESSION: Active input area lost "selected" highlight
              <rdar://problem/5094200> space key pressed to close the associated words candidate window gets inserted as text
              <rdar://problem/5228294> Candidate item for character matrix is sometimes skipped
    
            * WebKit.xcodeproj/project.pbxproj:
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView launchKeyEvent:]):
            (-[WebHTMLView keyDown:]):
            (-[WebHTMLView keyUp:]):
            (-[WebHTMLView _interceptEditingKeyEvent:shouldSaveCommand:]):
            (-[WebHTMLView unmarkText]):
            (-[WebHTMLView setMarkedText:selectedRange:]):
            (-[WebHTMLView doCommandBySelector:]):
            (-[WebHTMLView insertText:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23813 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebHTMLView.mm
+++ third_party/WebKit/WebKit/WebView/WebHTMLView.mm
@@ -302,6 +302,9 @@ struct WebHTMLViewInterpretKeyEventsParameters {
     KeyboardEvent* event;
     BOOL eventWasHandled;
     BOOL shouldSaveCommand;
+    // The Input Method may consume an event and not tell us, in
+    // which case we should not bubble the event up the DOM
+    BOOL consumedByIM;
 };
 
 @implementation WebHTMLViewPrivate
@@ -3272,9 +3275,8 @@ noPromisedData:
 
 - (void)keyDown:(NSEvent *)event
 {
+    RetainPtr<WebHTMLView> selfProtector = self;
     BOOL eventWasSentToWebCore = (_private->keyDownEvent == event);
-
-    [self retain];
 
     BOOL callSuper = NO;
 
@@ -3295,8 +3297,6 @@ noPromisedData:
         [super keyDown:event];
     else
         [NSCursor setHiddenUntilMouseMoves:YES];
-
-    [self release];
 }
 
 - (void)keyUp:(NSEvent *)event
@@ -5137,6 +5137,9 @@ static CGPoint coreGraphicsScreenPointForAppKitScreenPoint(NSPoint point)
     WebHTMLViewInterpretKeyEventsParameters parameters;
     parameters.eventWasHandled = false;
     parameters.shouldSaveCommand = shouldSave;
+    // If we're performing input composition assume that the IM has consumed the event, 
+    // and only change this assumption in one of the NSTextInput/Responder callbacks is used.
+    parameters.consumedByIM = [self hasMarkedText];
         
     if (const PlatformKeyboardEvent* platformEvent = event->keyEvent()) {
         NSEvent *macEvent = platformEvent->macEvent();
@@ -5160,7 +5163,7 @@ static CGPoint coreGraphicsScreenPointForAppKitScreenPoint(NSPoint point)
         }
         _private->interpretKeyEventsParameters = 0;
     }
-    return parameters.eventWasHandled;
+    return parameters.eventWasHandled || parameters.consumedByIM;
 }
 
 - (WebCore::CachedImage*)promisedDragTIFFDataSource 
@@ -5308,9 +5311,11 @@ BOOL isTextInput(Frame *coreFrame)
     WebHTMLViewInterpretKeyEventsParameters* parameters = _private->interpretKeyEventsParameters;
     _private->interpretKeyEventsParameters = 0;
 
-    if (parameters)
+    if (parameters) {
         parameters->eventWasHandled = YES;
-
+        parameters->consumedByIM = NO;
+    }
+    
     if (Frame* coreFrame = core([self _frame]))
         coreFrame->editor()->unmarkText();
 }
@@ -5361,9 +5366,11 @@ BOOL isTextInput(Frame *coreFrame)
     WebHTMLViewInterpretKeyEventsParameters* parameters = _private->interpretKeyEventsParameters;
     _private->interpretKeyEventsParameters = 0;
 
-    if (parameters)
+    if (parameters) {
         parameters->eventWasHandled = YES;
-
+        parameters->consumedByIM = NO;
+    }
+    
     Frame* coreFrame = core([self _frame]);
     if (!coreFrame)
         return;
@@ -5409,12 +5416,14 @@ BOOL isTextInput(Frame *coreFrame)
 
 - (void)doCommandBySelector:(SEL)selector
 {
-    if (selector == @selector(noop:))
-        return;
-
     // Use pointer to get parameters passed to us by the caller of interpretKeyEvents.
     // The same call to interpretKeyEvents can do more than one command.
     WebHTMLViewInterpretKeyEventsParameters* parameters = _private->interpretKeyEventsParameters;
+    if (parameters)
+        parameters->consumedByIM = NO;
+
+    if (selector == @selector(noop:))
+        return;
 
     KeyboardEvent* event = parameters ? parameters->event : 0;
     bool shouldSaveCommand = parameters && parameters->shouldSaveCommand;
@@ -5461,6 +5470,8 @@ BOOL isTextInput(Frame *coreFrame)
     // don't handle a mix of doCommandBySelector: and insertText:. 
     WebHTMLViewInterpretKeyEventsParameters* parameters = _private->interpretKeyEventsParameters;
     _private->interpretKeyEventsParameters = 0;
+    if (parameters)
+        parameters->consumedByIM = NO;
 
     // We don't support inserting an attributed string but input methods don't appear to require this.
     NSString *text;
