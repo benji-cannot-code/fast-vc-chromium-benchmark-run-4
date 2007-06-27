596fdc06919463e5cd66372e119c3d2e319fb246
sfalken <sfalken@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-27 17:28:39 +0000

    Prefast: Fix misplaced parenthesis.
            Reviewed by Darin.
    
            * WebView.cpp:
            (core):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23822 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -3616,7 +3616,7 @@ Page* core(IWebView* iWebView)
     Page* page = 0;
 
     WebView* webView = 0;
-    if (SUCCEEDED(iWebView->QueryInterface(CLSID_WebView, (void**)&webView) && webView)) {
+    if (SUCCEEDED(iWebView->QueryInterface(CLSID_WebView, (void**)&webView)) && webView) {
         page = webView->page();
         webView->Release();
     }
