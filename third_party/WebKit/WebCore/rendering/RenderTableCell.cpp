d0b766cce4fee564f1a495e8f8746fd3acd469ff
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-27 05:33:32 +0000

    2007-06-26  Mitz Pettel  <mitz@webkit.org>
            Reviewed by Hyatt.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=14406
              <colgroup>'s borders are ignored when it has <col> children
    
            Covered by existing tests.
    
            * rendering/RenderTableCell.cpp:
            (WebCore::RenderTableCell::collapsedLeftBorder):
            (WebCore::RenderTableCell::collapsedRightBorder):
            (WebCore::RenderTableCell::collapsedTopBorder):
            (WebCore::RenderTableCell::collapsedBottomBorder):
    
    2007-06-26  Mitz Pettel  <mitz@webkit.org>
    
            Reviewed by Hyatt.
    
            - updated results for http://bugs.webkit.org/show_bug.cgi?id=14406
              <colgroup>'s borders are ignored when it has <col> children
    
            * tables/mozilla/core/table_rules-expected.checksum: Added.
            * tables/mozilla/core/table_rules-expected.png: Added.
            * tables/mozilla/core/table_rules-expected.txt: Added.
            * tables/mozilla/core/table_rules.html: Copied from LayoutTests/tables/mozilla_expected_failures/core/table_rules.html.
            * tables/mozilla/marvin/x_table_rules_groups-expected.checksum: Added.
            * tables/mozilla/marvin/x_table_rules_groups-expected.png: Added.
            * tables/mozilla/marvin/x_table_rules_groups-expected.txt: Added.
            * tables/mozilla/marvin/x_table_rules_groups.xml: Copied from LayoutTests/tables/mozilla_expected_failures/marvin/x_table_rules_groups.xml.
            * tables/mozilla_expected_failures/core/table_rules-expected.checksum: Removed.
            * tables/mozilla_expected_failures/core/table_rules-expected.png: Removed.
            * tables/mozilla_expected_failures/core/table_rules-expected.txt: Removed.
            * tables/mozilla_expected_failures/core/table_rules.html: Removed.
            * tables/mozilla_expected_failures/marvin/backgr_border-table-column-group-expected.checksum:
            * tables/mozilla_expected_failures/marvin/backgr_border-table-column-group-expected.txt:
            * tables/mozilla_expected_failures/marvin/x_table_rules_groups-expected.checksum: Removed.
            * tables/mozilla_expected_failures/marvin/x_table_rules_groups-expected.png: Removed.
            * tables/mozilla_expected_failures/marvin/x_table_rules_groups-expected.txt: Removed.
            * tables/mozilla_expected_failures/marvin/x_table_rules_groups.xml: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTableCell.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTableCell.cpp
@@ -329,7 +329,7 @@ CollapsedBorderValue RenderTableCell::collapsedLeftBorder(bool rtl) const
             return result;
     }
     
-    // (5) Our column's left border.
+    // (5) Our column and column group's left borders.
     bool startColEdge;
     bool endColEdge;
     RenderTableCol* colElt = tableElt->colElement(col() + (rtl ? colSpan() - 1 : 0), &startColEdge, &endColEdge);
@@ -337,6 +337,11 @@ CollapsedBorderValue RenderTableCell::collapsedLeftBorder(bool rtl) const
         result = compareBorders(result, CollapsedBorderValue(&colElt->style()->borderLeft(), BCOL));
         if (!result.exists())
             return result;
+        if (colElt->parent()->isTableCol() && (!rtl ? !colElt->previousSibling() : !colElt->nextSibling())) {
+            result = compareBorders(result, CollapsedBorderValue(&colElt->parent()->style()->borderLeft(), BCOLGROUP));
+            if (!result.exists())
+                return result;
+        }
     }
     
     // (6) The right border of the column to the left.
@@ -392,7 +397,7 @@ CollapsedBorderValue RenderTableCell::collapsedRightBorder(bool rtl) const
             return result;
     }
     
-    // (5) Our column's right border.
+    // (5) Our column and column group's right borders.
     bool startColEdge;
     bool endColEdge;
     RenderTableCol* colElt = tableElt->colElement(col() + (rtl ? 0 : colSpan() - 1), &startColEdge, &endColEdge);
@@ -400,6 +405,11 @@ CollapsedBorderValue RenderTableCell::collapsedRightBorder(bool rtl) const
         result = compareBorders(result, CollapsedBorderValue(&colElt->style()->borderRight(), BCOL));
         if (!result.exists())
             return result;
+        if (colElt->parent()->isTableCol() && (!rtl ? !colElt->nextSibling() : !colElt->previousSibling())) {
+            result = compareBorders(result, CollapsedBorderValue(&colElt->parent()->style()->borderRight(), BCOLGROUP));
+            if (!result.exists())
+                return result;
+        }
     }
     
     // (6) The left border of the column to the right.
@@ -472,12 +482,17 @@ CollapsedBorderValue RenderTableCell::collapsedTopBorder() const
     }
     
     if (!currSection) {
-        // (8) Our column's top border.
+        // (8) Our column and column group's top borders.
         RenderTableCol* colElt = table()->colElement(col());
         if (colElt) {
             result = compareBorders(result, CollapsedBorderValue(&colElt->style()->borderTop(), BCOL));
             if (!result.exists())
                 return result;
+            if (colElt->parent()->isTableCol()) {
+                result = compareBorders(result, CollapsedBorderValue(&colElt->parent()->style()->borderTop(), BCOLGROUP));
+                if (!result.exists())
+                    return result;
+            }
         }
         
         // (9) The table's top border.
@@ -533,11 +548,16 @@ CollapsedBorderValue RenderTableCell::collapsedBottomBorder() const
     }
     
     if (!currSection) {
-        // (8) Our column's bottom border.
+        // (8) Our column and column group's bottom borders.
         RenderTableCol* colElt = table()->colElement(col());
         if (colElt) {
             result = compareBorders(result, CollapsedBorderValue(&colElt->style()->borderBottom(), BCOL));
             if (!result.exists()) return result;
+            if (colElt->parent()->isTableCol()) {
+                result = compareBorders(result, CollapsedBorderValue(&colElt->parent()->style()->borderBottom(), BCOLGROUP));
+                if (!result.exists())
+                    return result;
+            }
         }
         
         // (9) The table's bottom border.
