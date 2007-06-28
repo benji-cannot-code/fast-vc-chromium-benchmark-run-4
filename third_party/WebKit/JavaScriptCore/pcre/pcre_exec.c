e9f08b1e7148e287fd6a119d0b56b4212dccc4d6
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-28 06:53:45 +0000

    JavaScriptCore:
            Reviewed by Darin.
    
            - <rdar://problem/5271937> REGRESSION: Apparent WebKit JavaScript memory smasher when submitting comment to iWeb site (crashes in kjs_pcre_compile2)
            - Correctly evaluate the return value of _pcre_ucp_findchar.
    
            * pcre/pcre_compile.c:
            (compile_branch):
            * pcre/pcre_exec.c:
            (match):
    
    LayoutTests:
    
            Reviewed by Darin.
    
            - <rdar://problem/5271937> REGRESSION: Apparent WebKit JavaScript memory smasher when submitting comment to iWeb site (crashes in kjs_pcre_compile2)
            - Correctly evaluate the return value of _pcre_ucp_findchar.
    
            * fast/js/regexp-unicode-overflow-expected.txt: Added.
            * fast/js/regexp-unicode-overflow.html: Added.
            * fast/js/resources/regexp-unicode-overflow.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23846 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/pcre/pcre_exec.c
+++ third_party/WebKit/JavaScriptCore/pcre/pcre_exec.c
@@ -1947,7 +1947,7 @@ for (;;)
 #ifdef SUPPORT_UCP
           int chartype;
           int othercase;
-          if (_pcre_ucp_findchar(fc, &chartype, &othercase) < 0 || dc != othercase)
+          if (_pcre_ucp_findchar(fc, &chartype, &othercase) != ucp_L || dc != othercase)
 #endif
             RRETURN(MATCH_NOMATCH);
           }
@@ -2009,7 +2009,7 @@ for (;;)
         {
         int othercase;
         int chartype;
-        if ((ims & PCRE_CASELESS) == 0 || _pcre_ucp_findchar(fc, &chartype, &othercase) < 0)
+        if ((ims & PCRE_CASELESS) == 0 || _pcre_ucp_findchar(fc, &chartype, &othercase) != ucp_L)
           othercase = -1; /* Guaranteed to not match any character */
 
         for (i = 1; i <= min; i++)
@@ -2124,7 +2124,7 @@ for (;;)
         int othercase;
         int chartype;
         if ((ims & PCRE_CASELESS) != 0 &&
-             _pcre_ucp_findchar(fc, &chartype, &othercase) >= 0 &&
+             _pcre_ucp_findchar(fc, &chartype, &othercase) == ucp_L &&
              othercase > 0)
           oclength = _pcre_ord2utf8(othercase, occhars);
 #endif  /* SUPPORT_UCP */
