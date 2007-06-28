b5ea0e4cc0653f7a7362e92fdf820d989a1293b5
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-28 03:19:46 +0000

    LayoutTests:
            Reviewed by Adam Roben.
    
            Tests for http://bugs.webkit.org/show_bug.cgi?id=12045
            Crash under gmalloc at WTF::RefPtr<WebCore::HTMLSliderThumbElement>::operator->
            <rdar://problem/5286670>
    
            * fast/forms/thumbslider-crash-expected.checksum: Added.
            * fast/forms/thumbslider-crash-expected.png: Added.
            * fast/forms/thumbslider-crash-expected.txt: Added.
            * fast/forms/thumbslider-crash.html: Added.
            * fast/forms/thumbslider-no-parent-slider-expected.checksum: Added.
            * fast/forms/thumbslider-no-parent-slider-expected.png: Added.
            * fast/forms/thumbslider-no-parent-slider-expected.txt: Added.
            * fast/forms/thumbslider-no-parent-slider.html: Added.
    
    WebCore:
    
            Reviewed by Adam Roben.
    
            Patch for http://bugs.webkit.org/show_bug.cgi?id=12045
            Crash under gmalloc at WTF::RefPtr<WebCore::HTMLSliderThumbElement>::operator->
            <rdar://problem/5286670>
    
            Test: fast/forms/thumbslider-no-parent-slider.html
            Test: fast/forms/thumbslider-crash.html
    
            * rendering/RenderObject.h:
            (WebCore::RenderObject::isSlider):
            * rendering/RenderSlider.h:
            (WebCore::RenderSlider::isSlider):
            * rendering/RenderTheme.cpp:
            (WebCore::RenderTheme::paint): Only paint thumbslider
            if the parent renderer is a RenderSlider.
            * rendering/RenderThemeMac.mm:
            (WebCore::RenderThemeMac::paintSliderThumb):
            * rendering/RenderThemeSafari.cpp:
            (WebCore::RenderThemeSafari::paintSliderThumb):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderSlider.h
+++ third_party/WebKit/WebCore/rendering/RenderSlider.h
@@ -38,6 +38,7 @@ namespace WebCore {
         ~RenderSlider();
 
         virtual const char* renderName() const { return "RenderSlider"; }
+        virtual bool isSlider() const { return true; }
 
         virtual short baselinePosition( bool, bool ) const;
         virtual void calcPrefWidths();
