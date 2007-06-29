871b4359032647eb7ce18697e48125356f5cddf1
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-29 02:46:23 +0000

    LayoutTests:
            Reviewed by Beth.
    
            - test for http://bugs.webkit.org/show_bug.cgi?id=14118
              ASSERTION FAILED: !needsLayout() seen again
    
            * fast/dynamic/recursive-layout-expected.txt: Added.
            * fast/dynamic/recursive-layout.html: Added.
    
    WebCore:
    
            Reviewed by Beth.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=14118
              ASSERTION FAILED: !needsLayout() seen again
    
            Test: fast/dynamic/recursive-layout.html
    
            * page/FrameView.cpp:
            (WebCore::FrameView::layout): Removed the midLayout guards around
            the calls to invalidateSelection() and updateWidgetPositions(). Those
            calls may dirty the layout, but in that case they also update it, so
            they should be allowed to do it. Added an ASSERT to make sure that when
            they return, the root is not marked for layout.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23866 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -428,9 +428,7 @@ void FrameView::layout(bool allowSubtree)
         root->view()->popLayoutState();
     d->layoutRoot = 0;
 
-    d->midLayout = true;
     m_frame->invalidateSelection();
-    d->midLayout = false;
    
     d->layoutSchedulingEnabled=true;
 
@@ -441,11 +439,8 @@ void FrameView::layout(bool allowSubtree)
     layer->updateLayerPositions(d->doFullRepaint);
 
     // We update our widget positions right after doing a layout.
-    if (!subtree) {
-        d->midLayout = true;
+    if (!subtree)
         static_cast<RenderView*>(root)->updateWidgetPositions();
-        d->midLayout = false;
-    }
     
     // FIXME: Could optimize this and have objects removed from this list
     // if they ever do full repaints.
@@ -465,10 +460,8 @@ void FrameView::layout(bool allowSubtree)
     if (didFirstLayout)
         m_frame->loader()->didFirstLayout();
     
-    if (root->needsLayout()) {
-        scheduleRelayout();
-        return;
-    }
+    ASSERT(!root->needsLayout());
+
     setStaticBackground(useSlowRepaints());
 
     if (document->hasListenerType(Document::OVERFLOWCHANGED_LISTENER))
