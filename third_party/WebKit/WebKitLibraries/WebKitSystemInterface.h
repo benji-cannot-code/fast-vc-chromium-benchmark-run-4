13d04c604e8d95b3e916790d449aac92614e0358
antti <antti@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-06-29 08:18:35 +0000

    Reviewed by Maciej.
            Added WKQTMovieDataRate and WKQTMovieMaxTimeLoaded
    
            * WebKitSystemInterface.h:
            * libWebKitSystemInterface.a:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
+++ third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 #import <Carbon/Carbon.h>
 
+@class QTMovie;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -149,6 +151,9 @@ BOOL WKCGContextIsBitmapContext(CGContextRef context);
 void WKGetWheelEventDeltas(NSEvent *, float *deltaX, float *deltaY, BOOL *continuous);
 
 BOOL WKAppVersionCheckLessThan(NSString *, int, double);
+
+int WKQTMovieDataRate(QTMovie* movie);
+float WKQTMovieMaxTimeLoaded(QTMovie* movie);
 
 #ifdef __cplusplus
 }
