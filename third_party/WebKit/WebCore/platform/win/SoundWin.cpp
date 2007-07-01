279e8a8c039f0faf93703cf6f10ab195827fa6f3
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-01 04:52:21 +0000

    Make an int -> UINT conversion explicit that MSVC /W4 complains about
    Reviewed by John and Oliver.
    
    No regression test possible.
    
    * platform/win/SoundWin.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/SoundWin.cpp
+++ third_party/WebKit/WebCore/platform/win/SoundWin.cpp
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-void systemBeep() { MessageBeep(-1); }
+void systemBeep() { MessageBeep(static_cast<UINT>(-1)); }
 
 } // namespace WebCore
 
