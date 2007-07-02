15c2e4461be6659787d3c2d5760f292df36e1b45
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-02 20:47:40 +0000

    Rubberstamped by Darin.
            Rename FloatConversionUtilities.h to FloatConversion.h.
            Fix Win32 build.
    
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * platform/DeprecatedString.cpp:
            * platform/FloatConversion.h: Copied from platform/FloatConversionUtilities.h.
            * platform/FloatConversionUtilities.h: Removed.
            * platform/StringImpl.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23932 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/DeprecatedString.cpp
+++ third_party/WebKit/WebCore/platform/DeprecatedString.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DeprecatedString.h"
 
 #include "CString.h"
-#include "FloatConversionUtilities.h"
+#include "FloatConversion.h"
 #include "Logging.h"
 #include "PlatformString.h"
 #include "RegularExpression.h"
--- third_party/WebKit/WebCore/platform/StringImpl.cpp
+++ third_party/WebKit/WebCore/platform/StringImpl.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CString.h"
 #include "CharacterNames.h"
 #include "DeprecatedString.h"
-#include "FloatConversionUtilities.h"
+#include "FloatConversion.h"
 #include "Length.h"
 #include "StringHash.h"
 #include "TextBreakIterator.h"
