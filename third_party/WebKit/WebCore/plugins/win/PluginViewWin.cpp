b79594a093843c4212604e285309fdf5754b135a
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-02 00:44:15 +0000

    Fix a crash in ~PluginViewWin()
    This crash was introduced in r23827.
    
    Reviewed by Anders.
    
    Fixes dom/html/level2/html/HTMLAnchorElement01.html
    
    * plugins/win/PluginViewWin.cpp:
    (WebCore::PluginViewWin::~PluginViewWin): Allow m_plugin to be null.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23920 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
+++ third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
@@ -1158,7 +1158,9 @@ PluginViewWin::~PluginViewWin()
         DestroyWindow(m_window);
 
     m_parentFrame->cleanupScriptObjectsForPlugin(this);
-    m_plugin->unload();
+
+    if (m_plugin)
+        m_plugin->unload();
 }
 
 void PluginViewWin::disconnectStream(PluginStreamWin* stream)
