63b1afedcfb1bf2c0ea9b0cbbbe3ca58428003b1
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-04 10:56:08 +0000

    2007-07-04  Mitz Pettel  <mitz@webkit.org>
            Reviewed by Maciej.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=14495
              REGRESSION: Wrong cursor is displayed during drag and drop when text is selected
              <rdar://problem/5312384>
    
            No test possible because DumpRenderTree cannot query the current cursor.
    
            * page/EventHandler.cpp:
            (WebCore::EventHandler::selectCursor): Changed into a member function and
            added that if the last mouse down event could not have started a
            selection, then the cursor should not change to an I-beam unconditionally.
            (WebCore::EventHandler::handleMouseMoveEvent):
            * page/EventHandler.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23983 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/EventHandler.h
+++ third_party/WebKit/WebCore/page/EventHandler.h
@@ -49,6 +49,7 @@ namespace WebCore {
 
 class AtomicString;
 class Clipboard;
+class Cursor;
 class EventTargetNode;
 class Event;
 class FloatPoint;
@@ -187,6 +188,8 @@ private:
     bool handleMousePressEventTripleClick(const MouseEventWithHitTestResults&);
     bool handleMouseDraggedEvent(const MouseEventWithHitTestResults&);
     bool handleMouseReleaseEvent(const MouseEventWithHitTestResults&);
+
+    Cursor selectCursor(const MouseEventWithHitTestResults&, PlatformScrollbar*);
 
     void hoverTimerFired(Timer<EventHandler>*);
 
