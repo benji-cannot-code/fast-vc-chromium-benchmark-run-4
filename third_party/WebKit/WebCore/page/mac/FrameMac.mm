a9cb660fff86a510220c4b1c12e5a1caf9e21338
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-04 18:33:18 +0000

    Reenable some code for 64-bit now that <rdar://problem/5311640> and <rdar://problem/5311690> are resolved.
    git-svn-id: svn://svn.chromium.org/blink/trunk@23985 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/mac/FrameMac.mm
+++ third_party/WebKit/WebCore/page/mac/FrameMac.mm
@@ -513,10 +513,7 @@ void Frame::setUseSecureKeyboardEntry(bool enable)
     if (enable == IsSecureEventInputEnabled())
         return;
     if (enable) {
-#ifndef __LP64__
-        // FIXME: we need a new call on 64-bit <rdar://problem/5311690>
         EnableSecureEventInput();
-#endif
 #ifdef BUILDING_ON_TIGER
         KeyScript(enableRomanKeyboardsOnly);
 #else
@@ -525,10 +522,7 @@ void Frame::setUseSecureKeyboardEntry(bool enable)
         CFRelease(inputSources);
 #endif
     } else {
-#ifndef __LP64__
-        // FIXME: we need a new call on 64-bit <rdar://problem/5311690>
         DisableSecureEventInput();
-#endif
 #ifdef BUILDING_ON_TIGER
         KeyScript(smKeyEnableKybds);
 #else
