ee6ccf7c90279947d9a4e639e16c1d12fd3ae604
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-04 00:04:19 +0000

    LayoutTests:
            Reviewed by Tim Hatcher.
    
            - tests for <rdar://problem/5221297> Font family name in the cssText for a DOMCSSStyleDeclaration needs quotes
    
            * editing/style/font-family-with-space-expected.txt: Added.
            * editing/style/font-family-with-space.html: Added.
    
            * fast/inspector/style-expected.txt: Updated expected results to expect "Lucida Grande" in quote marks.
    
    WebCore:
    
            Reviewed by Tim Hatcher.
    
            - fix <rdar://problem/5221297> Font family name in the cssText for a DOMCSSStyleDeclaration needs quotes
    
            Test: editing/style/font-family-with-space.html
    
            * css/FontFamilyValue.cpp:
            (WebCore::isValidCSSIdentifier): Added. Implements the same rule that the CSS lexer does.
            (WebCore::quoteStringIfNeeded): Changed to call isValidCSSIdentifier instead of just
            checking for a leading "#" character.
            * editing/markup.cpp:
            (WebCore::escapeTextForMarkup): Changed to take a String parameter for better efficiency.
            (WebCore::renderedText): Changed to return a String for better efficiency.
            (WebCore::addNamespace): Updated to pass String to escapeTextForMarkup.
            (WebCore::startMarkup): Updated to pass String to escapeTextForMarkup. Added missing call
            to escapeTextForMarkup in the special case for the style property.
            (WebCore::createMarkup): Changed from single quotes to double quotes and also added missing
            call to escapeTextForMarkup in two special cases for the style property.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@23969 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/markup.cpp
+++ third_party/WebKit/WebCore/editing/markup.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2004, 2005, 2006, 2007 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -64,13 +64,13 @@ using namespace HTMLNames;
 
 static inline bool shouldSelfClose(const Node *node);
 
-static DeprecatedString escapeTextForMarkup(const DeprecatedString &in, bool isAttributeValue)
+static DeprecatedString escapeTextForMarkup(const String& in, bool isAttributeValue)
 {
     DeprecatedString s = "";
 
     unsigned len = in.length();
     for (unsigned i = 0; i < len; ++i) {
-        switch (in[i].unicode()) {
+        switch (in[i]) {
             case '&':
                 s += "&amp;";
                 break;
@@ -87,7 +87,7 @@ static DeprecatedString escapeTextForMarkup(const DeprecatedString &in, bool isA
                 }
                 // fall through
             default:
-                s += in[i];
+                s += DeprecatedChar(in[i]);
         }
     }
 
@@ -107,10 +107,10 @@ static String stringValueForRange(const Node *node, const Range *range)
     return str;
 }
 
-static DeprecatedString renderedText(const Node *node, const Range *range)
+static String renderedText(const Node* node, const Range* range)
 {
     if (!node->isTextNode())
-        return DeprecatedString();
+        return String();
 
     ExceptionCode ec;
     const Text* textNode = static_cast<const Text*>(node);
@@ -193,7 +193,7 @@ static String addNamespace(const AtomicString& prefix, const AtomicString& ns, H
     AtomicStringImpl* foundNS = namespaces.get(pre);
     if (foundNS != ns.impl()) {
         namespaces.set(pre, ns.impl());
-        return " xmlns" + (!prefix.isEmpty() ? ":" + prefix : "") + "=\"" + escapeTextForMarkup(ns.deprecatedString(), true) + "\"";
+        return " xmlns" + (!prefix.isEmpty() ? ":" + prefix : "") + "=\"" + escapeTextForMarkup(ns, true) + "\"";
     }
     
     return "";
@@ -213,7 +213,7 @@ static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnot
                     return stringValueForRange(node, range).deprecatedString();
             }
             bool useRenderedText = annotate && !enclosingNodeWithTag(const_cast<Node*>(node), selectTag);
-            DeprecatedString markup = useRenderedText ? escapeTextForMarkup(renderedText(node, range), false) : escapeTextForMarkup(stringValueForRange(node, range).deprecatedString(), false);
+            DeprecatedString markup = escapeTextForMarkup(useRenderedText ? renderedText(node, range) : stringValueForRange(node, range), false);
             return annotate ? convertHTMLTextToInterchangeFormat(markup, static_cast<const Text*>(node)) : markup;
         }
         case Node::COMMENT_NODE:
@@ -246,14 +246,11 @@ static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnot
                 // We'll handle the style attribute separately, below.
                 if (attr->name() == styleAttr && el->isHTMLElement() && (annotate || convertBlocksToInlines))
                     continue;
-                String value = attr->value();
-                // FIXME: Handle case where value has illegal characters in it, like "
                 if (documentIsHTML)
                     markup += " " + attr->name().localName().deprecatedString();
                 else
                     markup += " " + attr->name().toString().deprecatedString();
-                markup += "=\"" + escapeTextForMarkup(value.deprecatedString(), true) + "\"";
-
+                markup += "=\"" + escapeTextForMarkup(attr->value(), true) + "\"";
                 if (!documentIsHTML && namespaces && shouldAddNamespaceAttr(attr, *namespaces))
                     markup += addNamespace(attr->prefix(), attr->namespaceURI(), *namespaces).deprecatedString();
             }
@@ -268,7 +265,7 @@ static DeprecatedString startMarkup(const Node *node, const Range *range, EAnnot
                 if (convertBlocksToInlines)
                     style->setProperty(CSS_PROP_DISPLAY, CSS_VAL_INLINE, true);
                 if (style->length() > 0)
-                    markup += " " +  styleAttr.localName().deprecatedString() + "=\"" + style->cssText().deprecatedString() + "\"";
+                    markup += " style=\"" + escapeTextForMarkup(style->cssText(), true) + "\"";
             }
             
             if (shouldSelfClose(el)) {
@@ -563,7 +560,7 @@ DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotat
                     style->setProperty(CSS_PROP_BACKGROUND_IMAGE, "url('" + static_cast<Element*>(fullySelectedRoot)->getAttribute(backgroundAttr) + "')");
                 
                 if (style->length()) {
-                    markups.prepend("<div style='" + style->cssText().deprecatedString() + "'>");
+                    markups.prepend("<div style=\"" + escapeTextForMarkup(style->cssText(), true) + "\">");
                     markups.append("</div>");
                 }
             } else {
@@ -590,8 +587,7 @@ DeprecatedString createMarkup(const Range *range, Vector<Node*>* nodes, EAnnotat
         removeEnclosingMailBlockquoteStyle(style.get(), parentOfLastClosed);
         
         if (style->length() > 0) {
-            // FIXME: Handle case where style->cssText() has illegal characters in it, like "
-            DeprecatedString openTag = DeprecatedString("<span class=\"") + AppleStyleSpanClass + "\" style=\"" + style->cssText().deprecatedString() + "\">";
+            DeprecatedString openTag = DeprecatedString("<span class=\"") + AppleStyleSpanClass + "\" style=\"" + escapeTextForMarkup(style->cssText(), true) + "\">";
             markups.prepend(openTag);
             markups.append("</span>");
         }
