c70dd8dbade88caf2d93284090d3e34c6e8e29a6
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-05 06:08:56 +0000

    2007-07-04  Matt Perry  <mpComplete@gmail.com>
            Fix for http://bugs.webkit.org/show_bug.cgi?id=13400
            Bug 13400: REGRESSION (r20809-20814): No back entry created for navigations created by assigning to document.location
            <rdar://problem/5153025>
    
            Treat userGesture as always true for setLocation, forcing a back/forward
            entry to be created.
    
            * bindings/js/JSHTMLDocumentCustom.cpp:
            (WebCore::JSHTMLDocument::setLocation):
    
    2007-07-04  Matt Perry  <mpComplete@gmail.com>
    
            Test for http://bugs.webkit.org/show_bug.cgi?id=13400
            Bug 13400: REGRESSION (r20809-20814): No back entry created for navigations created by assigning to document.location
            <rdar://problem/5153025>
    
            Test that assigning to document.location in various ways adds an
            item to the back/forward list.
    
            * http/tests/navigation/document-location-click-expected.txt: Added.
            * http/tests/navigation/document-location-click-timeout-expected.txt: Added.
            * http/tests/navigation/document-location-click-timeout.html: Added.
            * http/tests/navigation/document-location-click.html: Added.
            * http/tests/navigation/document-location-mouseover-expected.txt: Added.
            * http/tests/navigation/document-location-mouseover.html: Added.
            * http/tests/navigation/document-location-onload-expected.txt: Added.
            * http/tests/navigation/document-location-onload.html: Added.
            * http/tests/navigation/resources/document-location.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/navigation/resources/document-location.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function start() {
+  if (window.layoutTestController) {
+    var n = document.location.search.substring(1);
+    if (!n) {
+      // page just opened
+      layoutTestController.dumpBackForwardList();
+      layoutTestController.dumpAsText();
+      layoutTestController.waitUntilDone();
+
+      runTest();
+    } else {
+      // loaded the ?1 navigation
+      layoutTestController.notifyDone();
+    }
+  }
+}
+
+function setLocation() {
+  document.location = "?1"
+}
+
