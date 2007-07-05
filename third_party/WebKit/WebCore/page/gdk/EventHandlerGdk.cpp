49d27d402fb56e8e953e26f436a685a781e473ea
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-05 05:21:28 +0000

    Mac, Qt, and Gdk build fixes.
    * page/EventHandler.h:
    * page/gdk/EventHandlerGdk.cpp:
    * page/mac/EventHandlerMac.mm:
    (WebCore::EventHandler::passSubframeEventToSubframe):
    * page/qt/EventHandlerQt.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24002 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/gdk/EventHandlerGdk.cpp
+++ third_party/WebKit/WebCore/page/gdk/EventHandlerGdk.cpp
@@ -92,7 +92,7 @@ bool EventHandler::eventActivatedView(const PlatformMouseEvent& event) const
     return false;
 }
 
-bool EventHandler::passSubframeEventToSubframe(MouseEventWithHitTestResults& event, Frame* subframe)
+bool EventHandler::passSubframeEventToSubframe(MouseEventWithHitTestResults& event, Frame* subframe, HitTestResult*)
 {
     notImplemented();
     return false;
--- third_party/WebKit/WebCore/page/qt/EventHandlerQt.cpp
+++ third_party/WebKit/WebCore/page/qt/EventHandlerQt.cpp
@@ -109,7 +109,7 @@ bool EventHandler::eventActivatedView(const PlatformMouseEvent&) const
     return false;
 }
 
-bool EventHandler::passSubframeEventToSubframe(MouseEventWithHitTestResults& event, Frame* subframe)
+bool EventHandler::passSubframeEventToSubframe(MouseEventWithHitTestResults& event, Frame* subframe, HitTestResult*)
 {
     notImplemented();
     return false;
