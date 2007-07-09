8b1f06489ed9b7ec2b1724d28a0ea0783e56eea9
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-09 22:10:15 +0000

    Reviewed by Geoff.
            Get rid of the node argument in our various script execution functions.
            
            * bindings/js/kjs_proxy.cpp:
            (WebCore::KJSProxy::evaluate):
            * bindings/js/kjs_proxy.h:
            * bindings/js/kjs_window.cpp:
            (KJS::ScheduledAction::execute):
            * dom/XMLTokenizer.cpp:
            (WebCore::XMLTokenizer::endElementNs):
            (WebCore::XMLTokenizer::notifyFinished):
            * html/HTMLScriptElement.cpp:
            (WebCore::HTMLScriptElement::evaluateScript):
            * html/HTMLTokenizer.cpp:
            (WebCore::HTMLTokenizer::scriptExecution):
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::changeLocation):
            (WebCore::FrameLoader::urlSelected):
            (WebCore::FrameLoader::submitForm):
            (WebCore::FrameLoader::replaceContentsWithScriptResult):
            (WebCore::FrameLoader::executeScript):
            * loader/FrameLoader.h:
            * page/mac/WebCoreFrameBridge.mm:
            (-[WebCoreFrameBridge stringByEvaluatingJavaScriptFromString:forceUserGesture:]):
            (-[WebCoreFrameBridge aeDescByEvaluatingJavaScriptFromString:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24126 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/mac/WebCoreFrameBridge.mm
+++ third_party/WebKit/WebCore/page/mac/WebCoreFrameBridge.mm
@@ -685,7 +685,7 @@ static HTMLFormElement *formElementFromDOMElement(DOMElement *element)
 - (NSString *)stringByEvaluatingJavaScriptFromString:(NSString *)string forceUserGesture:(BOOL)forceUserGesture
 {
     ASSERT(m_frame->document());
-    JSValue* result = m_frame->loader()->executeScript(0, string, forceUserGesture);
+    JSValue* result = m_frame->loader()->executeScript(string, forceUserGesture);
 
     // If the value returned isn't an object, we don't need an ExecState to convert it
     if (result && !result->isObject()) {
@@ -709,7 +709,7 @@ static HTMLFormElement *formElementFromDOMElement(DOMElement *element)
 {
     ASSERT(m_frame->document());
     ASSERT(m_frame == m_frame->page()->mainFrame());
-    JSValue* result = m_frame->loader()->executeScript(0, string, true);
+    JSValue* result = m_frame->loader()->executeScript(string, true);
     if (!result) // FIXME: pass errors
         return 0;
     JSLock lock;
