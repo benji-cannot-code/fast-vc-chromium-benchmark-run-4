bc280002a323ba33ce5304757e4b4c19b5c0f889
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-10 22:13:33 +0000

    Patch from Qing Zhao to draw focus rects on non-QStyle rendered nodes.  Needs a bit more work once we figure out how to use QPainterPathStroker in a more sophisticated manner.
    git-svn-id: svn://svn.chromium.org/blink/trunk@24168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/RenderThemeQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/RenderThemeQt.cpp
@@ -64,7 +64,7 @@ bool RenderThemeQt::supportsHover(const RenderStyle*) const
 
 bool RenderThemeQt::supportsFocusRing(const RenderStyle* style) const
 {
-    return true;
+    return supportsFocus(style->appearance());
 }
 
 short RenderThemeQt::baselinePosition(const RenderObject* o) const
@@ -401,6 +401,8 @@ bool RenderThemeQt::supportsFocus(EAppearance appearance) const
         case ButtonAppearance:
         case TextFieldAppearance:
         case MenulistAppearance:
+        case RadioAppearance:
+        case CheckboxAppearance:
             return true;
         default: // No for all others...
             return false;
