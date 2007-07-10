99c6c04f77547a130435c2d1f3c1657639601b7d
adachan <adachan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-10 21:53:19 +0000

    2007-07-10  Ada Chan  <adachan@apple.com>
            Fixed build.
    
            * WebView.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24166 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/BString.h>
 #include <WebCore/Cache.h>
 #include <WebCore/CommandByName.h>
+#include <WebCore/ContextMenu.h>
 #include <WebCore/ContextMenuController.h>
 #include <WebCore/CString.h>
 #include <WebCore/Cursor.h>
