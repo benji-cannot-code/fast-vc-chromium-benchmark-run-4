a744f968094d628033b0e926ba5e6077cafcf046
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-11 19:16:43 +0000

    Repair the build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@24207 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/ResourceHandleQt.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/ResourceHandleQt.cpp
@@ -66,7 +66,7 @@ bool ResourceHandle::start(Frame* frame)
         return false;
 
     // check for (probably) broken requests
-    if (method() != "GET" && method() != "POST") {
+    if (d->m_request.httpMethod() != "GET" && d->m_request.httpMethod() != "POST") {
         notImplemented();
         return false;
     }
