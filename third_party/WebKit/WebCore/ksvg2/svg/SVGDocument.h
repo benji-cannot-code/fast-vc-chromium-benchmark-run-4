2f3f015d40642a5c64da6649633c485b86fdc237
rwlbuis <rwlbuis@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-11 07:34:58 +0000

    Reviewed by Oliver.
            Forgot to commit this as part of commit r24146. Now we are sure
            createElement creates elements in the xhtml namespace only.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24184 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGDocument.h
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGDocument.h
@@ -43,8 +43,6 @@ namespace WebCore {
 
         SVGSVGElement* rootElement() const;
         
-        virtual PassRefPtr<Element> createElement(const String& tagName, ExceptionCode&);
-        
         void dispatchZoomEvent(float prevScale, float newScale);
         void dispatchScrollEvent();
 
