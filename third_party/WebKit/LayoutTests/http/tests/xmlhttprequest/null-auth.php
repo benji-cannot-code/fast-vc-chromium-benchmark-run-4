0dba861d00b4a811db8727bbd4c15c2e4ce1d75f
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-12 04:17:17 +0000

    Reviewed by Darin.
            http://bugs.webkit.org/show_bug.cgi?id=14584
            XMLHttpRequest treats null login/password incorrectly
    
            Test: http/tests/xmlhttprequest/null-auth.php
    
            * xml/xmlhttprequest.cpp:
            (WebCore::XMLHttpRequest::open):
            * xml/xmlhttprequest.h:
            Split the open() method into three to distinguish between missing and null credentials.
    
            * bindings/js/JSXMLHttpRequest.cpp:
            (KJS::JSXMLHttpRequestPrototypeFunction::callAsFunction): Call the appropriate open().
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/null-auth.php
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<p>Test that null values in XHR login/password parameters are treated correctly.</p>
+<p>No auth tokens should be sent with this request.</p>
+<pre id='syncResult'> </pre>
+<script>
+if (window.layoutTestController)
+  layoutTestController.dumpAsText();
+
+req = new XMLHttpRequest;
+req.open('POST', '<?php echo 'http://foo:bar@' . $_SERVER['HTTP_HOST'] . '/xmlhttprequest/resources/echo-auth.php' ?>', false, null, null);
+req.send();
+document.getElementById('syncResult').firstChild.nodeValue = req.responseText;
+</script>
