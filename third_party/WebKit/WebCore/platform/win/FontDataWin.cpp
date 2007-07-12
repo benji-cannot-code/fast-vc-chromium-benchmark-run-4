f51d5f9e7a95464ffc1048992053edf2e7d9ac13
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-12 04:34:48 +0000

    Reviewed by Maciej
            Fix for <rdar://problem/5329712> GMail crash when trying to compose new message
    
            * platform/win/FontDataWin.cpp:
            (WebCore::FontData::containsCharacters):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/FontDataWin.cpp
+++ third_party/WebKit/WebCore/platform/win/FontDataWin.cpp
@@ -150,12 +150,10 @@ bool FontData::containsCharacters(const UChar* characters, int length) const
 
     for (int i = 0; i < length; i++) {
         if (glyphBuffer[i] == 0xFFFFFFFF) {
-            delete []glyphBuffer;
             return false;
         }
     }
 
-    delete []glyphBuffer;
     return true;
 }
 
