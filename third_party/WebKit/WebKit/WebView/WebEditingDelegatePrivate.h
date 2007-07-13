f6177cef1c7da799965079ac881b27c46ad88e03
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-13 21:45:04 +0000

    Reviewed by Oliver Hunt.
            <rdar://problem/5333766> Can't include WebEditingDelegatePrivate.h
    
            * WebView/WebEditingDelegatePrivate.h: Changed the include to be a framework include,
            so other projects can use this header.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24275 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebEditingDelegatePrivate.h
+++ third_party/WebKit/WebKit/WebView/WebEditingDelegatePrivate.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "WebEditingDelegate.h"
+#import <WebKit/WebEditingDelegate.h>
 
 @class DOMHTMLElement;
 
