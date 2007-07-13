5f40a2a088b648b658706ad979fb84fcdfd8b49b
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-13 11:51:40 +0000

    2007-07-13  Mitz Pettel  <mitz@webkit.org>
            Reviewed by Dave Hyatt.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=14395
              When a <p> is inside <li> two <BR>s are rendered
    
            Test: fast/lists/marker-before-empty-inline.html
    
            * rendering/RenderBlock.h:
            * rendering/RenderListItem.cpp:
            (WebCore::getParentOfFirstLineBox): Changed to skip empty inline flows that
            do not actually generate any line boxes. Limited the nested lists quirk to
            the case where the list is a child of the list item, to match Firefox.
            * rendering/bidi.cpp:
            (WebCore::requiresLineBox): Added. Factored out of skipWhitespace.
            (WebCore::RenderBlock::generatesLineBoxesForInlineChild): Added. Checkes
            whether the child or any of its siblings following it will generate a line
            box in the flow.
            (WebCore::RenderBlock::skipWhitespace):
    
    2007-07-13  Mitz Pettel  <mitz@webkit.org>
    
            Reviewed by Dave Hyatt.
    
            - test and updated results for http://bugs.webkit.org/show_bug.cgi?id=14395
              When a <p> is inside <li> two <BR>s are rendered
    
            * editing/pasteboard/4861080-expected.checksum:
            * editing/pasteboard/4861080-expected.png:
            * editing/pasteboard/4861080-expected.txt:
            * fast/lists/marker-before-empty-inline-expected.checksum: Added.
            * fast/lists/marker-before-empty-inline-expected.png: Added.
            * fast/lists/marker-before-empty-inline-expected.txt: Added.
            * fast/lists/marker-before-empty-inline.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderListItem.cpp
+++ third_party/WebKit/WebCore/rendering/RenderListItem.cpp
@@ -136,7 +136,7 @@ bool RenderListItem::isEmpty() const
     return lastChild() == m_marker;
 }
 
-static RenderObject* getParentOfFirstLineBox(RenderObject* curr, RenderObject* marker)
+static RenderObject* getParentOfFirstLineBox(RenderBlock* curr, RenderObject* marker)
 {
     RenderObject* firstChild = curr->firstChild();
     if (!firstChild)
@@ -146,7 +146,7 @@ static RenderObject* getParentOfFirstLineBox(RenderObject* curr, RenderObject* m
         if (currChild == marker)
             continue;
 
-        if (currChild->isInline())
+        if (currChild->isInline() && (!currChild->isInlineFlow() || curr->generatesLineBoxesForInlineChild(currChild)))
             return curr;
 
         if (currChild->isFloating() || currChild->isPositioned())
@@ -155,11 +155,11 @@ static RenderObject* getParentOfFirstLineBox(RenderObject* curr, RenderObject* m
         if (currChild->isTable() || !currChild->isRenderBlock())
             break;
 
-        if (currChild->style()->htmlHacks() && currChild->element() &&
+        if (curr->isListItem() && currChild->style()->htmlHacks() && currChild->element() &&
             (currChild->element()->hasTagName(ulTag)|| currChild->element()->hasTagName(olTag)))
             break;
 
-        RenderObject* lineBox = getParentOfFirstLineBox(currChild, marker);
+        RenderObject* lineBox = getParentOfFirstLineBox(static_cast<RenderBlock*>(currChild), marker);
         if (lineBox)
             return lineBox;
     }
