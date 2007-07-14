50f65d7421ad66b83a53059fb1556815e162210a
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-14 17:04:03 +0000

    2007-07-14  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
            Reviewed by Darin.
    
            Fixes http://bugs.webkit.org/show_bug.cgi?id=13517,
                  http://bugs.webkit.org/show_bug.cgi?id=14237, and
                  the failure of test js1_5/Scope/regress-185485.js
    
            Bug 13517: DOM Exception 8 in finance.aol.com sub-page
            Bug 14237: Javascript "var" statement interprets initialization in the topmost function scope
    
            * kjs/nodes.cpp:
            (VarDeclNode::evaluate):
            * tests/mozilla/expected.html:
    
    2007-07-14  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
    
            Reviewed by Darin.
    
            Added tests for the following bugs:
            http://bugs.webkit.org/show_bug.cgi?id=13517
            http://bugs.webkit.org/show_bug.cgi?id=14237
    
            Bug 13517: DOM Exception 8 in finance.aol.com sub-page
            Bug 14237: Javascript "var" statement interprets initialization in the topmost function scope
    
            * fast/js/resources/vardecl-blocks-init.js: Added.
            * fast/js/resources/vardecl-preserve-arguments.js: Updated.
            * fast/js/vardecl-blocks-init-expected.txt: Added.
            * fast/js/vardecl-blocks-init.html: Added.
            * fast/js/vardecl-preserve-arguments-expected.txt: Updated result.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24287 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/vardecl-blocks-init.js
@@ -0,0 +1,57 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This test checks that variable declarations with initializers inside of catch and with blocks do not set values in a deeper scope."
+);
+
+function catchTest() {
+  var e = "foo";
+
+  try {
+    throw "bar";
+  } catch (e) {
+    var e = "baz";
+  }
+
+  return e;
+}
+
+function catchTest2() {
+  var e = "foo";
+
+  try {
+    throw "bar";
+  } catch (e) {
+    var e = "baz";
+
+    return e;
+  }
+}
+
+function withTest() {
+  var e = "foo"
+  var object = { 'e' : "bar" };
+
+  with (object) {
+    var e = "baz";
+  }
+
+  return e;
+}
+
+function withTest2() {
+  var e = "foo"
+  var object = { 'e' : "bar" };
+
+  with (object) {
+    var e = "baz";
+
+    return e;
+  }
+}
+
+shouldBe("catchTest()", "'foo'");
+shouldBe("catchTest2()", "'baz'");
+shouldBe("withTest()", "'foo'");
+shouldBe("withTest2()", "'baz'");
+
+var successfullyParsed = true;
