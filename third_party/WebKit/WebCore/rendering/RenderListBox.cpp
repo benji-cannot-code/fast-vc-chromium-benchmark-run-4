b5662667998c660031c1797757e2bd28a3f25bd3
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-16 21:46:58 +0000

    LayoutTests:
            Reviewed by Brady.
    
            Test for <rdar://problem/5321490> REGRESSION: visibility:hidden property doesn't hide parts of file input element
            http://bugs.webkit.org/show_bug.cgi?id=14565
    
            * fast/forms/hidden-input-file-expected.checksum: Added.
            * fast/forms/hidden-input-file-expected.png: Added.
            * fast/forms/hidden-input-file-expected.txt: Added.
            * fast/forms/hidden-input-file.html: Added.
            * fast/forms/hidden-listbox-expected.checksum
            * fast/forms/hidden-listbox-expected.png
            * fast/forms/hidden-listbox-expected.txt
            * fast/forms/hidden-listbox.html
    
    WebCore:
    
            Reviewed by Brady.
    
            Fix for <rdar://problem/5321490> REGRESSION: visibility:hidden property doesn't hide parts of file input element and listbox
            http://bugs.webkit.org/show_bug.cgi?id=14565
    
            Tests:
            fast/forms/hidden-input-file.html
            fast/forms/hidden-listbox.html
    
            * rendering/RenderFileUploadControl.cpp: (WebCore::RenderFileUploadControl::paintObject):
            Only paint if the control is supposed to be visible.
            * rendering/RenderListBox.cpp: (WebCore::RenderListBox::paintObject):
            ditto.  We already had checks to prevent drawing the text that considered the option's style too, but if visibility: hidden
            was just set on the listbox then a lot of extra work was being done, and the scrollbar was drawing too.  This change fixes that.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24327 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderListBox.cpp
+++ third_party/WebKit/WebCore/rendering/RenderListBox.cpp
@@ -249,6 +249,9 @@ IntRect RenderListBox::itemBoundingBoxRect(int tx, int ty, int index)
     
 void RenderListBox::paintObject(PaintInfo& paintInfo, int tx, int ty)
 {
+    if (style()->visibility() != VISIBLE)
+        return;
+    
     int listItemsSize = numItems();
 
     if (paintInfo.phase == PaintPhaseForeground) {
