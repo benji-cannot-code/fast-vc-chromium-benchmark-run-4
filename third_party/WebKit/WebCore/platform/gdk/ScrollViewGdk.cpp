c9b3623303a71e1bd142533debad3ab411f01cb7
zecke <zecke@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-16 20:21:22 +0000

    2007-07-16  Holger Hans Peter Freyther  <zecke@selfish.org>
            Reviewed by Maciej.
    
            Lazily update the scrollbars (e.g. on loading a page) to
            generate less expose events, which will lead to laying out less often
            and will fix http://bugs.webkit.org/show_bug.cgi?id=14020.
    
            * platform/gdk/ScrollViewGdk.cpp:
            (WebCore::ScrollView::ScrollViewPrivate::ScrollViewPrivate):
            (WebCore::ScrollView::update):
            (WebCore::ScrollView::resizeContents):
            (WebCore::ScrollView::updateScrollbars):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24321 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gdk/ScrollViewGdk.cpp
+++ third_party/WebKit/WebCore/platform/gdk/ScrollViewGdk.cpp
@@ -57,6 +57,7 @@ public:
         , layout(0)
         , horizontalAdjustment(0)
         , verticalAdjustment(0)
+        , scrollBarsNeedUpdate(false)
     { }
 
     bool hasStaticBackground;
@@ -68,6 +69,7 @@ public:
     GtkAdjustment *verticalAdjustment;
     IntSize contentsSize;
     IntSize viewPortSize;
+    bool scrollBarsNeedUpdate;
 };
 
 ScrollView::ScrollView()
@@ -98,7 +100,11 @@ void ScrollView::updateContents(const IntRect& updateRect, bool now)
 
 void ScrollView::update()
 {
-    notImplemented();
+    ASSERT(gtkWidget());
+    gtk_widget_queue_draw(gtkWidget());
+
+    if (m_data->scrollBarsNeedUpdate)
+        updateScrollbars();
 }
 
 int ScrollView::visibleWidth() const
@@ -140,7 +146,7 @@ void ScrollView::resizeContents(int w, int h)
     
     if (m_data->layout) {
         gtk_layout_set_size(m_data->layout, w, h);
-        updateScrollbars(); 
+        m_data->scrollBarsNeedUpdate = true;
     }
 }
 
@@ -303,6 +309,8 @@ void ScrollView::updateScrollbars()
     
     gtk_adjustment_changed(m_data->verticalAdjustment);
     gtk_adjustment_value_changed(m_data->verticalAdjustment);
+
+    m_data->scrollBarsNeedUpdate = false;
 }
 
 IntPoint ScrollView::windowToContents(const IntPoint& point) const
