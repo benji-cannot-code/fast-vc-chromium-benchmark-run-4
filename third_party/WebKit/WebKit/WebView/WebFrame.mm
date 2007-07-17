60f02caaa69d1457c5a4cea586ada237beb6701f
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-17 23:39:54 +0000

    Reviewed by Geoff.
            <rdar://problem/5336267> loadData:MIMEType:textEncodingName:baseURL: doesn't like relative URLs
    
            Get the absoluteURL from any user supplied NSURL before we pass it down to WebCore.
    
            * WebView/WebFrame.mm:
            (-[WebFrame _loadURL:referrer:intoChild:]):
            (-[WebFrame _loadData:MIMEType:textEncodingName:baseURL:unreachableURL:]):
            * WebView/WebView.mm:
            (-[WebView userAgentForURL:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebFrame.mm
+++ third_party/WebKit/WebKit/WebView/WebFrame.mm
@@ -322,7 +322,7 @@ WebView *getWebView(WebFrame *webFrame)
     if (archive)
         [childFrame loadArchive:archive];
     else
-        [childFrame _frameLoader]->load(URL, referrer, childLoadType,
+        [childFrame _frameLoader]->load([URL absoluteURL], referrer, childLoadType,
                                         String(), 0, 0);
 }
 
@@ -769,12 +769,12 @@ static NSURL *createUniqueWebDataURL()
         responseURL = createUniqueWebDataURL();
     }
     
-    ResourceRequest request(URL);
+    ResourceRequest request([URL absoluteURL]);
 
     // hack because Mail checks for this property to detect data / archive loads
     [NSURLProtocol setProperty:@"" forKey:@"WebDataRequest" inRequest:(NSMutableURLRequest *)request.nsURLRequest()];
 
-    SubstituteData substituteData(WebCore::SharedBuffer::wrapNSData(data), MIMEType, encodingName, unreachableURL, responseURL);
+    SubstituteData substituteData(WebCore::SharedBuffer::wrapNSData(data), MIMEType, encodingName, [unreachableURL absoluteURL], responseURL);
 
     [self _frameLoader]->load(request, substituteData);
 }
