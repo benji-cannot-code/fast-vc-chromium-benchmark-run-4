941ec32f4b85a28c8486a3365edddbdc7f1cdae3
zecke <zecke@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-17 21:28:08 +0000

    2007-07-17  Holger Hans Peter Freyther  <zecke@selfish.org>
            Blind build fix for Qt after r24366 by adding the additional
            WebCore::ResourceRequest& parameter to the download method.
    
            * WebCoreSupport/FrameLoaderClientQt.cpp:
            (WebCore::FrameLoaderClientQt::download):
            * WebCoreSupport/FrameLoaderClientQt.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24385 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitQt/WebCoreSupport/FrameLoaderClientQt.cpp
+++ third_party/WebKit/WebKitQt/WebCoreSupport/FrameLoaderClientQt.cpp
@@ -719,7 +719,7 @@ WTF::PassRefPtr<WebCore::DocumentLoader> FrameLoaderClientQt::createDocumentLoad
     return loader.release();
 }
 
-void FrameLoaderClientQt::download(WebCore::ResourceHandle*, const WebCore::ResourceRequest&, const WebCore::ResourceResponse&)
+void FrameLoaderClientQt::download(WebCore::ResourceHandle*, const WebCore::ResourceRequest&, const WebCore::ResourceRequest&, const WebCore::ResourceResponse&)
 {
     notImplemented();
 }
