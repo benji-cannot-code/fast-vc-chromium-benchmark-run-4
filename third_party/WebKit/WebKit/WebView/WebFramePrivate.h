8630e0a0855bebe66cc499693d2ae986fa67c13c
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-17 18:59:48 +0000

    Build fix.
            * WebView/WebFrame.mm:
            (-[WebFrame _loadURL:referrer:intoChild:]):
            * WebView/WebFramePrivate.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24369 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebFramePrivate.h
+++ third_party/WebKit/WebKit/WebView/WebFramePrivate.h
@@ -47,7 +47,7 @@ typedef enum {
     WebFrameLoadTypeReload,
     WebFrameLoadTypeReloadAllowingStaleData,
     WebFrameLoadTypeSame,               // user loads same URL again (but not reload button)
-    WebFrameLoadTypeInternal,
+    WebFrameLoadTypeInternal,           // maps to WebCore::FrameLoadTypeRedirectWithLockedHistory
     WebFrameLoadTypeReplace
 } WebFrameLoadType;
 
