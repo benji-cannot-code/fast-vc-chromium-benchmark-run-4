de8ffbf2969f2e221400c7ea86f6930e5db44d93
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-17 06:42:03 +0000

    Reviewed by Adam Roben.
            Move self, opener, parent, top, window, and frames from kjs_window
            to DOMWindow and auto-generate the bindings for them.
    
            * bindings/js/kjs_window.cpp:
            (KJS::Window::getValueProperty):
            * bindings/js/kjs_window.h:
            (KJS::Window::):
            * page/DOMWindow.cpp:
            (WebCore::DOMWindow::self):
            (WebCore::DOMWindow::opener):
            (WebCore::DOMWindow::parent):
            (WebCore::DOMWindow::top):
            * page/DOMWindow.h:
            (WebCore::DOMWindow::window):
            (WebCore::DOMWindow::frames):
            * page/DOMWindow.idl:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/kjs_window.h
+++ third_party/WebKit/WebCore/bindings/js/kjs_window.h
@@ -151,9 +151,8 @@ namespace KJS {
         ResizeBy, ResizeTo, ShowModalDialog,
 
         // Attributes
-        Crypto, Frames, Event_, Location_,
-        Navigator_, ClientInformation, Opener,
-        Parent, Self, Window_, Top,
+        Crypto, Event_, Location_, Navigator_,
+        ClientInformation,
 
         // Event Listeners
         Onabort, Onblur, Onchange, Onclick,
