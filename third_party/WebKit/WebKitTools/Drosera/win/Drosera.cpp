7b8e793d095ddb35c62a44f16436ba1059c07da2
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-17 00:44:23 +0000

    Reviewed by Darin.
            - Continued x-platform modifications.
    
            * Drosera/win/DebuggerDocumentWin.cpp: Copied from Drosera/win/DebuggerObjectCallbacks.cpp.
            * Drosera/win/DebuggerDocumentWin.h: Copied from Drosera/win/DebuggerObjectCallbacks.h.
            * Drosera/win/DebuggerObjectCallbacks.cpp: Removed.
            * Drosera/win/DebuggerObjectCallbacks.h: Removed.
            * Drosera/win/Drosera.cpp:
            * Drosera/win/Drosera.vcproj/Drosera.vcproj:
            * Drosera/win/stdafx.cpp: Removed.
            * Drosera/win/stdafx.h: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24339 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Drosera/win/Drosera.cpp
+++ third_party/WebKit/WebKitTools/Drosera/win/Drosera.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "Drosera.h"
 
-#include "DebuggerObjectCallbacks.h"
+#include "DebuggerDocumentWin.h"
 #include "HelperFunctions.h"
 #include "resource.h"
 #include <JavaScriptCore/JSStringRef.h>
