d6f71955105e0a99bec9e751ef5b2dd70faf8b61
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-17 21:17:00 +0000

    Initialize ContextMenuController::m_page
    Somehow this variable had never been initialized before.
    
    Reviewed by Tim.
    
    * page/ContextMenuController.cpp:
    (WebCore::ContextMenuController::ContextMenuController):
    * page/ContextMenuController.h:
    * page/Page.cpp:
    (WebCore::Page::Page):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24379 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/ContextMenuController.h
+++ third_party/WebKit/WebCore/page/ContextMenuController.h
@@ -40,7 +40,7 @@ namespace WebCore {
 
     class ContextMenuController : Noncopyable {
     public:
-        ContextMenuController(ContextMenuClient*);
+        ContextMenuController(Page*, ContextMenuClient*);
         ~ContextMenuController();
 
         ContextMenuClient* client() { return m_client; }
