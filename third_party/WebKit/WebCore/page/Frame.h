af6fa7f8a97e65db75447c2b4c591b7b70882629
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-18 05:40:54 +0000

    LayoutTests:
            Reviewed by Geoff Garen.
    
            Test for <rdar://problem/5333957>
    
            * http/tests/security/cross-frame-access-protocol.html: Fix a few typos.
            * http/tests/security/cross-frame-access-selection-expected.txt: Added.
            * http/tests/security/cross-frame-access-selection.html: Added.
            * http/tests/security/resources/foreign-domain-iframe-for-selection-test.html: Added.
    
    WebCore:
    
            Reviewed by Geoff Garen.
    
            Fix for <rdar://problem/5333957>
                    http://bugs.webkit.org/show_bug.cgi?id=14610
    
            Original patch by Feng Qian.
    
            - Clear the selection object when disconnecting a frame.
    
            Test: http/tests/security/cross-frame-access-selection.html
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::clear):
            * page/DOMWindow.cpp:
            (WebCore::DOMWindow::disconnectFrame):
            (WebCore::DOMWindow::clear):
            * page/DOMWindow.h:
            * page/Frame.cpp:
            (WebCore::Frame::clearScriptProxy):
            (WebCore::Frame::clearDOMWindow):
            * page/Frame.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24398 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.h
+++ third_party/WebKit/WebCore/page/Frame.h
@@ -233,6 +233,9 @@ public:
 
     void dashboardRegionsChanged();
 
+    void clearScriptProxy();
+    void clearDOMWindow();
+
     void cleanupScriptObjects();
     void cleanupScriptObjectsForPlugin(void*);
 
