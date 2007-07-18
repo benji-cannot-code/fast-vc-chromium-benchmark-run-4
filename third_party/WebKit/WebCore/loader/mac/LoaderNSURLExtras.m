benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 75516358279c82a7fa3153e9d04b0da9cb5a0bb7

Written by Darin, reviewed by me
        - fixed <rdar://problem/5249730> REGRESSION (Leopard, around 9A464): Safari opens the same local 
          documents more than once due to canonicalization change

        * loader/mac/LoaderNSURLExtras.m:
        (canonicalURL):
        pass URL through KURL so we get KURL's version of canonicalization as well as
        NSURLProtocol's version of canonicalization



git-svn-id: svn://svn.chromium.org/blink/trunk@23949 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebCore/loader/mac/LoaderNSURLExtras.m
@@ -179,7 +179,7 @@ NSURL *canonicalURL(NSURL *url)
 {
     if (!url)
         return nil;
-
+    
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
     Class concreteClass = wkNSURLProtocolClassForReqest(request);
     if (!concreteClass) {
@@ -187,13 +187,13 @@ NSURL *canonicalURL(NSURL *url)
         return url;
     }
     
-    NSURL *result = nil;
+    // Get NSURLProtocol's idea of canonical URL
     NSURLRequest *newRequest = [concreteClass canonicalRequestForRequest:request];
-    NSURL *newURL = [newRequest URL];
-    result = [[newURL retain] autorelease];
+    KURL newURL = [newRequest URL];
     [request release];
     
-    return result;
+    // pass through KURL to get KURL's idea of canonical URL
+    return newURL.getNSURL();
 }
 
 static bool vectorContainsString(Vector<String> vector, String string)

commit 05df1737935f391d6ffdaa2ecd9536f9312408bc

Reviewed by Maciej and Kevin Decker
        - fixed <rdar://problem/5315926> REGRESSION: ~.4% PLT regression from r23949 (canonicalization change)

        * loader/mac/LoaderNSURLExtras.m:
        (canonicalURL):
        Rolled out the fix for 5249730 that caused this regression. 5249730 is addressed a different way now.



git-svn-id: svn://svn.chromium.org/blink/trunk@24420 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebCore/loader/mac/LoaderNSURLExtras.m
@@ -187,13 +187,16 @@ NSURL *canonicalURL(NSURL *url)
         return url;
     }
     
-    // Get NSURLProtocol's idea of canonical URL
+    // This applies NSURL's concept of canonicalization, but not KURL's concept. It would
+    // make sense to apply both, but when we tried that it caused a performance degradation
+    // (see 5315926). It might make sense to apply only the KURL concept and not the NSURL
+    // concept, but it's too risky to make that change for WebKit 3.0.
     NSURLRequest *newRequest = [concreteClass canonicalRequestForRequest:request];
-    KURL newURL = [newRequest URL];
+    NSURL *newURL = [newRequest URL]; 
+    NSURL *result = [[newURL retain] autorelease]; 
     [request release];
     
-    // pass through KURL to get KURL's idea of canonical URL
-    return newURL.getNSURL();
+    return result;
 }
 
 static bool vectorContainsString(Vector<String> vector, String string)