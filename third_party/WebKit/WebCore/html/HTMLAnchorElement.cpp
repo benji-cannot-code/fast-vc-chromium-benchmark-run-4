081b1b0b7b9a6d4886287f062e045c99bfe88626
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-18 06:27:57 +0000

    Reviewed by Hyatt.
            http://bugs.webkit.org/show_bug.cgi?id=13618
            Don't set link targets to "_blank" on middle-click; rely on the policy
            delegate to do the right thing.
    
            * html/HTMLAnchorElement.cpp:
            (WebCore::HTMLAnchorElement::defaultEventHandler):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24401 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLAnchorElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLAnchorElement.cpp
@@ -186,10 +186,6 @@ void HTMLAnchorElement::defaultEventHandler(Event* evt)
 
         String url = parseURL(getAttribute(hrefAttr));
 
-        String target = getAttribute(targetAttr);
-        if (e && e->button() == MiddleButton)
-            target = "_blank";
-
         ASSERT(evt->target());
         ASSERT(evt->target()->toNode());
         if (evt->target()->toNode()->hasTagName(imgTag)) {
@@ -214,7 +210,7 @@ void HTMLAnchorElement::defaultEventHandler(Event* evt)
         }
 
         if (!evt->defaultPrevented() && document()->frame())
-            document()->frame()->loader()->urlSelected(document()->completeURL(url), target, evt, false, true);
+            document()->frame()->loader()->urlSelected(document()->completeURL(url), getAttribute(targetAttr), evt, false, true);
 
         evt->setDefaultHandled();
     } else if (m_isLink && isContentEditable()) {
