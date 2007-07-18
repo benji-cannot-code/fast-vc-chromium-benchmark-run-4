df8ad402db767f68f277bdac32ba85be0d6776cd
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-18 13:28:57 +0000

    Use contentDocument to get the source view's iframe document.         Also adds a null/undefined check for localVariableNames.
            * Drosera/debugger.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Drosera/debugger.js
+++ third_party/WebKit/WebKitTools/Drosera/debugger.js
@@ -72,6 +72,9 @@ ScriptCallFrame.prototype.loadVariables = function ()
     var variablesTable = document.getElementById("variablesTable");
     variablesTable.innerHTML = "";
 
+    if (!this.localVariableNames)
+        return;
+
     for(var i = 0; i < this.localVariableNames.length; i++) {
         var tr = document.createElement("tr");
         var td = document.createElement("td");
@@ -966,7 +969,7 @@ function switchFunction(index, shouldResetPopup)
     var selectedFunction = functionSelect.childNodes[index];
     var selection = sourcesFrame.getSelection();
     var currentFunction = selectedFunction.value;     
-    var currentFunctionElement = sourcesFrame.document.getElementById(currentFunction);
+    var currentFunctionElement = sourcesFrame.contentDocument.getElementById(currentFunction);
     
     functionSelect.blur();
     sourcesFrame.focus();
