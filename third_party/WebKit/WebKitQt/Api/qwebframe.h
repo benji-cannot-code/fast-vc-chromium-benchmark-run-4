973f1436ba405d7e00fc5e28a6ceb79e961c0f4a
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-18 12:22:29 +0000

    Reviewed by Zack & Simon
            Reallow setting of scrollbar policies on QWebFrame.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24406 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitQt/Api/qwebframe.h
+++ third_party/WebKit/WebKitQt/Api/qwebframe.h
@@ -64,7 +64,10 @@ public:
 
     QList<QWebFrame*> childFrames() const;
 
-    void suppressScrollbars(bool suppress);
+    Qt::ScrollBarPolicy verticalScrollBarPolicy() const;
+    void setVerticalScrollBarPolicy(Qt::ScrollBarPolicy);
+    Qt::ScrollBarPolicy horizontalScrollBarPolicy() const;
+    void setHorizontalScrollBarPolicy(Qt::ScrollBarPolicy);
 
     void render(QPainter *painter, const QRect &source);
 
