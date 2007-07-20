15d576b003c165dd0c2b43ea43173bc37b57d2d1
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-20 03:47:26 +0000

    Reviewed by Sam
            <rdar://problem/5134929> REGRESSION: Colon detected as delimiting an empty port is stripped
    
            The fix for this was in KURL::parse() which had a slightly changed rule that would strip the colon if there
            was no port.
    
            An additional canonicalization fix was made in window.location.host to match the expected behavior of leaving
            the colon in place - before this fix, if the colon was there but no port was there, we'd see the port as 0.
    
            * bindings/js/kjs_window.cpp:
            (KJS::Location::put): Call setHostAndPort instead of setHost and setPort seperately
    
            * manual-tests/location-host-canonicalization.html: Added.
    
            * platform/KURL.cpp:
            (WebCore::KURL::setHostAndPort): Set the host and port as one string component then reparse the URL
            (WebCore::KURL::parse): Change one line to consider the empty colon as part of the host name
            * platform/KURL.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24462 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/kjs_window.cpp
+++ third_party/WebKit/WebCore/bindings/js/kjs_window.cpp
@@ -1748,10 +1748,7 @@ void Location::put(ExecState *exec, const Identifier &p, JSValue *v, int attr)
       break;
     }
     case Host: {
-      DeprecatedString host = str.left(str.find(":"));
-      DeprecatedString port = str.mid(str.find(":")+1);
-      url.setHost(host);
-      url.setPort(port.toUInt());
+      url.setHostAndPort(str);
       break;
     }
     case Hostname:
