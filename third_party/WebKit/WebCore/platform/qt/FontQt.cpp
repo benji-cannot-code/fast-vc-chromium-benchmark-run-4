1fbcd3ee873b503adcc3cf61c7faca7f23332a7d
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-20 10:39:23 +0000

    make offsetForPosition and selectionRectForText work correctly with small caps.
    git-svn-id: svn://svn.chromium.org/blink/trunk@24473 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/FontQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/FontQt.cpp
@@ -321,7 +321,7 @@ int Font::offsetForPosition(const TextRun& run, const TextStyle& style, int posi
             int xe = w - components.at(i).offset;
             int xs = xe - components.at(i).width;
             if (position >= xs && position <= xe) {
-                QTextLayout layout(components.at(i).string, m_font);
+                QTextLayout layout(components.at(i).string, *components.at(i).font);
                 layout.beginLayout();
                 QTextLine l = layout.createLine();
                 if (!l.isValid())
@@ -340,7 +340,7 @@ int Font::offsetForPosition(const TextRun& run, const TextStyle& style, int posi
             int xs = components.at(i).offset;
             int xe = xs + components.at(i).width;
             if (position >= xs && position <= xe) {
-                QTextLayout layout(components.at(i).string, m_font);
+                QTextLayout layout(components.at(i).string, *components.at(i).font);
                 layout.beginLayout();
                 QTextLine l = layout.createLine();
                 if (!l.isValid())
@@ -359,8 +359,7 @@ int Font::offsetForPosition(const TextRun& run, const TextStyle& style, int posi
 }
 
 static float cursorToX(const Vector<TextRunComponent, 1024>& components, int width,
-                     const TextStyle& style,
-                     const QFont& font, int cursor)
+                     const TextStyle& style, int cursor)
 {
     int start = 0;
     for (int i = 0; i < components.size(); ++i) {
@@ -371,7 +370,7 @@ static float cursorToX(const Vector<TextRunComponent, 1024>& components, int wid
         int xs = components.at(i).offset;
         if (style.rtl())
             xs = width - xs - components.at(i).width;
-        QTextLayout layout(components.at(i).string, font);
+        QTextLayout layout(components.at(i).string, *components.at(i).font);
         layout.beginLayout();
         QTextLine l = layout.createLine();
         if (!l.isValid())
@@ -391,8 +390,8 @@ FloatRect Font::selectionRectForText(const TextRun& run, const TextStyle& style,
     Vector<TextRunComponent, 1024> components;
     int w = generateComponents(&components, *this, run, style);
 
-    float x1 = cursorToX(components, w, style, m_font, from);
-    float x2 = cursorToX(components, w, style, m_font, to);
+    float x1 = cursorToX(components, w, style, from);
+    float x2 = cursorToX(components, w, style, to);
     if (x2 < x1)
         qSwap(x1, x2);
 
