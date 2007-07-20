a95baf9994302be1af91f116ea3e702c5e831bc8
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-20 20:35:04 +0000

    LayoutTests:
            Reviewed by Beth Dakin.
    
            Computed style for border radii should return a CSSValueList instead 
            of CSSPrimitiveValue (with a Pair type) when the radii are not the same. 
    
            * fast/css/computed-style-expected.txt:
            * fast/css/computed-style-without-renderer-expected.txt:
            * fast/css/getComputedStyle-borderRadius-expected.txt: Copied from LayoutTests/fast/dom/getComputedStyle-borderRadius-expected.txt.
            * fast/css/getComputedStyle-borderRadius.html: Copied from LayoutTests/fast/dom/getComputedStyle-borderRadius.html.
            * fast/dom/getComputedStyle-borderRadius-expected.txt: Removed.
            * fast/dom/getComputedStyle-borderRadius.html: Removed.
    
    WebCore:
    
            Reviewed by Beth Dakin.
    
            Update for fix for http://bugs.webkit.org/show_bug.cgi?id=14646
            <rdar://problem/5340449>
    
            - Return a CSSValueList instead of CSSValue (with a Pair type) for
              computed values of border radii.
            - Add ability to CSSValueList to print out a space separated list
              instead of a comma.
            - Add border radii to list of computed styles that we create and can
              iterate over.
    
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::):
            (WebCore::getBorderRadiusCornerValue):
            * css/CSSValueList.cpp:
            (WebCore::CSSValueList::CSSValueList):
            (WebCore::CSSValueList::cssText):
            * css/CSSValueList.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24486 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSValueList.h
+++ third_party/WebKit/WebCore/css/CSSValueList.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * This file is part of the DOM implementation for KDE.
  *
  * (C) 1999-2003 Lars Knoll (knoll@kde.org)
- * Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2007 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -30,9 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class CSSValueList : public CSSValue
-{
+class CSSValueList : public CSSValue {
 public:
+    CSSValueList(bool isSpaceSeparated = false);
     virtual ~CSSValueList();
 
     unsigned length() const { return m_values.size(); }
@@ -47,8 +47,9 @@ public:
 
 protected:
     Vector<RefPtr<CSSValue> > m_values;
+    bool m_isSpaceSeparated;
 };
 
-} // namespace
+} // namespace WebCore
 
-#endif
+#endif // CSSValueList_h
