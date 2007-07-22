2309bb3c7c62424e77269e6140a242692c502074
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-22 22:47:25 +0000

    JavaScriptCore:
            Reviewed by Kevin Decker.
    
            - fix <rdar://problem/5126394> REGRESSION: Crash after clicking back button in test application (13250)
              http://bugs.webkit.org/show_bug.cgi?id=13250
    
            * bindings/objc/objc_utility.mm: (KJS::Bindings::convertObjcValueToValue):
            If the object returns 0 for _imp, convert that to "undefined", since callers
            can't cope with a JSValue of 0.
    
    WebCore:
    
            Reviewed by Kevin Decker.
    
            - fix <rdar://problem/5126394> REGRESSION: Crash after clicking back button in test application (13250)
              http://bugs.webkit.org/show_bug.cgi?id=13250
    
            * bindings/objc/WebScriptObject.mm:
            (_didExecute): Removed unnecessary check for isValid() since the _rootObject method already
            takes care of that check.
            (-[WebScriptObject _imp]): Made this return 0 if there is not a valid rootObject.
            (-[WebScriptObject _isSafeScript]): Made this always return false if there is not a valid
            rootObject, eliminating the need to check _rootObject for nil if the code is checking _isSafeScript.
            (-[WebScriptObject callWebScriptMethod:withArguments:]): Removed check of _rootObject, since it's
            immediately followed by a check of _isSafeScript. Removed awkward conversion of name from NSString *
            to KJS::Identifier and did it using WebCore::String instead -- makes it clear that there's no
            arbitrary JavaScript execution after _isSafeScript and before _imp, so we don't need to null-check
            the _imp pointer. Added a second check of _isSafeScript after calling get to get the function
            object, since arbitrary changes could occur in there, including navigation to a new frame.
            This also takes care of null checking the second call to _imp.
            (-[WebScriptObject evaluateWebScript:]): Removed check of _rootObject, since it's
            immediately followed by a check of _isSafeScript. Removed awkward conversion of script from NSString *
            to KJS::Identifier and did it using WebCore::String instead -- makes it clear that there's no
            arbitrary JavaScript execution after _isSafeScript and before _imp, so we don't need to null-check
            the _imp pointer.
            (-[WebScriptObject setValue:forKey:]): Removed check of _rootObject, since it's
            immediately followed by a check of _isSafeScript. Removed awkward conversion of key from NSString *
            to KJS::Identifier and did it using WebCore::String instead -- makes it clear that there's no
            arbitrary JavaScript execution after _isSafeScript and before _imp, so we don't need to null-check
            the _imp pointer.
            (-[WebScriptObject valueForKey:]): Ditto.
            (-[WebScriptObject removeWebScriptKey:]): Ditto.
            (-[WebScriptObject stringRepresentation]): Removed check of _rootObject, since it's
            immediately followed by a check of _isSafeScript. Removed unnecessary local variable
            and const_cast from the call to the _imp method.
            (-[WebScriptObject webScriptValueAtIndex:]): Removed check of _rootObject, since it's
            immediately followed by a check of _isSafeScript.
            (-[WebScriptObject setWebScriptValueAtIndex:value:]): Ditto.
            (-[WebScriptObject JSObject]): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/WebScriptObject.mm
+++ third_party/WebKit/WebCore/bindings/objc/WebScriptObject.mm
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "DOMInternal.h"
 #import "Frame.h"
+#import "PlatformString.h"
 #import "WebCoreObjCExtras.h"
 #import "WebCoreFrameBridge.h"
 #import <JavaScriptCore/context.h>
@@ -38,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace KJS;
 using namespace KJS::Bindings;
+using namespace WebCore;
 
 #define LOG_EXCEPTION(exec) \
     if (Interpreter::shouldPrintExceptions()) \
@@ -107,13 +109,14 @@ static void _didExecute(WebScriptObject *obj)
 {
     ASSERT(JSLock::lockCount() > 0);
     
-    if (![obj _rootObject] || ![obj _rootObject]->isValid())
+    RootObject* root = [obj _rootObject];
+    if (!root)
         return;
 
-    ExecState* exec = [obj _rootObject]->interpreter()->globalExec();
+    ExecState* exec = root->interpreter()->globalExec();
     KJSDidExecuteFunctionPtr func = Instance::didExecuteFunction();
     if (func)
-        func(exec, static_cast<JSObject*>([obj _rootObject]->interpreter()->globalObject()));
+        func(exec, static_cast<JSObject*>(root->interpreter()->globalObject()));
 }
 
 - (void)_setImp:(JSObject*)imp originRootObject:(PassRefPtr<RootObject>)originRootObject rootObject:(PassRefPtr<RootObject>)rootObject
@@ -145,13 +148,13 @@ static void _didExecute(WebScriptObject *obj)
     return self;
 }
 
-- (JSObject *)_imp
+- (JSObject*)_imp
 {
     // Associate the WebScriptObject with the JS wrapper for the ObjC DOM wrapper.
     // This is done on lazily, on demand.
     if (!_private->imp && _private->isCreatedByDOMWrapper)
         [self _initializeScriptDOMNodeImp];
-    return _private->imp;
+    return _private->rootObject && _private->rootObject->isValid() ? _private->imp : 0;
 }
 
 - (BOOL)_hasImp
@@ -171,12 +174,12 @@ static void _didExecute(WebScriptObject *obj)
 
 - (BOOL)_isSafeScript
 {
-    if (!_private->originRootObject || !_private->rootObject)
+    if (!_private->originRootObject)
         return true;
 
-    if (!_private->originRootObject->isValid() || !_private->rootObject->isValid())
+    if (!_private->originRootObject->isValid() || !_private->rootObject || !_private->rootObject->isValid())
         return false;
-        
+
     return _private->originRootObject->interpreter()->isSafeScript(_private->rootObject->interpreter());
 }
 
@@ -260,21 +263,16 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
 
 - (id)callWebScriptMethod:(NSString *)name withArguments:(NSArray *)args
 {
-    if (![self _rootObject])
-        return nil;
-
     if (![self _isSafeScript])
         return nil;
 
-    // Lookup the function object.
+    // Look up the function object.
     ExecState* exec = [self _rootObject]->interpreter()->globalExec();
     ASSERT(!exec->hadException());
 
     JSLock lock;
     
-    JSValue *v = convertObjcValueToValue(exec, &name, ObjcObjectType, [self _rootObject]);
-    Identifier identifier(v->toString(exec));
-    JSValue *func = [self _imp]->get(exec, identifier);
+    JSValue* func = [self _imp]->get(exec, String(name));
 
     if (!func || !func->isObject())
         // Maybe throw an exception here?
@@ -285,10 +283,13 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
     if (!funcImp->implementsCall())
         return 0;
 
-    JSObject *thisObj = const_cast<JSObject*>([self _imp]);
     List argList = listFromNSArray(exec, args, [self _rootObject]);
+
+    if (![self _isSafeScript])
+        return nil;
+
     [self _rootObject]->interpreter()->startTimeoutCheck();
-    JSValue *result = funcImp->call(exec, thisObj, argList);
+    JSValue *result = funcImp->call(exec, [self _imp], argList);
     [self _rootObject]->interpreter()->stopTimeoutCheck();
 
     if (exec->hadException()) {
@@ -307,9 +308,6 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
 
 - (id)evaluateWebScript:(NSString *)script
 {
-    if (![self _rootObject])
-        return nil;
-    
     if (![self _isSafeScript])
         return nil;
     
@@ -319,9 +317,8 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
     JSValue *result;
     JSLock lock;
     
-    JSValue *v = convertObjcValueToValue(exec, &script, ObjcObjectType, [self _rootObject]);
     [self _rootObject]->interpreter()->startTimeoutCheck();
-    Completion completion = [self _rootObject]->interpreter()->evaluate(UString(), 0, v->toString(exec));
+    Completion completion = [self _rootObject]->interpreter()->evaluate(UString(), 0, String(script));
     [self _rootObject]->interpreter()->stopTimeoutCheck();
     ComplType type = completion.complType();
     
@@ -347,9 +344,6 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
 
 - (void)setValue:(id)value forKey:(NSString *)key
 {
-    if (![self _rootObject])
-        return;
-
     if (![self _isSafeScript])
         return;
 
@@ -357,8 +351,7 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
     ASSERT(!exec->hadException());
 
     JSLock lock;
-    JSValue *v = convertObjcValueToValue(exec, &key, ObjcObjectType, [self _rootObject]);
-    [self _imp]->put(exec, Identifier(v->toString(exec)), convertObjcValueToValue(exec, &value, ObjcObjectType, [self _rootObject]));
+    [self _imp]->put(exec, String(key), convertObjcValueToValue(exec, &value, ObjcObjectType, [self _rootObject]));
 
     if (exec->hadException()) {
         LOG_EXCEPTION(exec);
@@ -370,9 +363,6 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
 
 - (id)valueForKey:(NSString *)key
 {
-    if (![self _rootObject])
-        return nil;
-        
     if (![self _isSafeScript])
         return nil;
 
@@ -380,8 +370,7 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
     ASSERT(!exec->hadException());
 
     JSLock lock;
-    JSValue *v = convertObjcValueToValue(exec, &key, ObjcObjectType, [self _rootObject]);
-    JSValue *result = [self _imp]->get(exec, Identifier(v->toString(exec)));
+    JSValue *result = [self _imp]->get(exec, String(key));
     
     if (exec->hadException()) {
         LOG_EXCEPTION(exec);
@@ -400,9 +389,6 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
 
 - (void)removeWebScriptKey:(NSString *)key
 {
-    if (![self _rootObject])
-        return;
-        
     if (![self _isSafeScript])
         return;
 
@@ -410,8 +396,7 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
     ASSERT(!exec->hadException());
 
     JSLock lock;
-    JSValue *v = convertObjcValueToValue(exec, &key, ObjcObjectType, [self _rootObject]);
-    [self _imp]->deleteProperty(exec, Identifier(v->toString(exec)));
+    [self _imp]->deleteProperty(exec, String(key));
 
     if (exec->hadException()) {
         LOG_EXCEPTION(exec);
@@ -423,32 +408,24 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
 
 - (NSString *)stringRepresentation
 {
-    if (![self _rootObject])
-        // This is a workaround for a gcc 3.3 internal compiler error.
-        return @"Undefined";
-
     if (![self _isSafeScript])
         // This is a workaround for a gcc 3.3 internal compiler error.
         return @"Undefined";
 
     JSLock lock;
-    JSObject *thisObj = const_cast<JSObject*>([self _imp]);
     ExecState* exec = [self _rootObject]->interpreter()->globalExec();
     
-    id result = convertValueToObjcValue(exec, thisObj, ObjcObjectType).objectValue;
+    id result = convertValueToObjcValue(exec, [self _imp], ObjcObjectType).objectValue;
 
-    id resultObj = [result description];
+    NSString *description = [result description];
 
     _didExecute(self);
 
-    return resultObj;
+    return description;
 }
 
 - (id)webScriptValueAtIndex:(unsigned)index
 {
-    if (![self _rootObject])
-        return nil;
-
     if (![self _isSafeScript])
         return nil;
 
@@ -473,9 +450,6 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
 
 - (void)setWebScriptValueAtIndex:(unsigned)index value:(id)value
 {
-    if (![self _rootObject])
-        return;
-
     if (![self _isSafeScript])
         return;
 
@@ -511,11 +485,8 @@ static List listFromNSArray(ExecState *exec, NSArray *array, RootObject* rootObj
 
 - (JSObjectRef)JSObject
 {
-    if (![self _rootObject])
-        return nil;
-
     if (![self _isSafeScript])
-        return nil;
+        return NULL;
 
     return toRef([self _imp]);
 }
