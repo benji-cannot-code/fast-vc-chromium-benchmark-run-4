dd7735fe4fac7f54f911906ccabd1725f05db2a1
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-23 05:30:22 +0000

    * platform/ContextMenu.cpp: Added missing include.
    git-svn-id: svn://svn.chromium.org/blink/trunk@24529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ContextMenu.cpp
+++ third_party/WebKit/WebCore/platform/ContextMenu.cpp
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceRequest.h"
 #include "SelectionController.h"
 #include "TextIterator.h"
+#include <memory>
 
 using namespace std;
 using namespace WTF;
