f438a6441c686ab766e64f2f3ca96dfe69b2a188
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-24 11:47:17 +0000

    Roll out r24583, it needs further consideration.
    git-svn-id: svn://svn.chromium.org/blink/trunk@24584 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/ContextMenuMac.mm
+++ third_party/WebKit/WebCore/platform/mac/ContextMenuMac.mm
@@ -115,7 +115,6 @@ void ContextMenu::appendItem(ContextMenuItem& item)
         setMenuItemTarget(platformItem);
 
     [m_platformDescription.get() addObject:platformItem];
-    [platformItem release];
 }
 
 void ContextMenu::insertItem(unsigned position, ContextMenuItem& item)
