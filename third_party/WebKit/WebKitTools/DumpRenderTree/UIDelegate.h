2240d7aa184840b2b6c63832a1e7610800be494c
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-26 21:59:05 +0000

    LayoutTests:
            Reviewed by Darin, Geoff and Sam.
    
            - <rdar://problem/5150461> Resizing the window larger than the screen causes no resize.
    
            - Tests to ensure we resize/move the window out of bounds correctly.
    
            * fast/dom/Window/window-resize-expected.txt: Added.
            * fast/dom/Window/window-resize.html: Added.
            * mac/leopard/Skipped:
    
    WebCore:
    
            Reviewed by Darin, Geoff and Sam.
    
            - <rdar://problem/5150461> Resizing the window larger than the screen causes no resize.
            - We now better match the behavior of other browsers.
    
            * bindings/js/kjs_window.cpp:
            (KJS::constrainToVisible):
            (KJS::WindowFunc::callAsFunction):
    
    WebKitTools:
    
            Reviewed by Darin, Geoff, Sam.
    
            - <rdar://problem/5150461> Resizing the window larger than the screen causes no resize.
    
            - Added delegate methods to intercept and fake the frame location so methods like resizeTo and moveTo can change the window location without actually making the window appear on-screen.
    
            * DumpRenderTree/UIDelegate.h:
            * DumpRenderTree/UIDelegate.m:
            (-[UIDelegate webView:setFrame:]):
            (-[UIDelegate webViewFrame:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24654 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/UIDelegate.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/UIDelegate.h
@@ -29,5 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  
 #import <Cocoa/Cocoa.h>
 
-@interface UIDelegate : NSObject
+@interface UIDelegate : NSObject {
+
+@private
+    NSRect m_frame;
+}
+
 @end
