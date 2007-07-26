1873ef230c1134347a54e316923a4401a16bd841
treat <treat@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-26 16:21:11 +0000

    Launch the context menu in the right place.
    git-svn-id: svn://svn.chromium.org/blink/trunk@24644 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/ContextMenuQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/ContextMenuQt.cpp
@@ -33,13 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <QAction>
 
-#include <qwebframe.h>
-#include <qwebpage.h>
-#include <FrameLoaderClientQt.h>
 #include <Document.h>
 #include <Frame.h>
-#include <FrameLoader.h>
-#include <FrameLoaderClient.h>
+#include <FrameView.h>
 
 namespace WebCore {
 
@@ -126,9 +122,8 @@ void ContextMenu::setPlatformDescription(PlatformMenuDescription menu)
 #ifndef QT_NO_MENU
     //qDebug("Switch menu(%p) %p to %p", this, (QMenu*)m_menu, menu);
     if (menu == 0) {
-        FrameLoaderClient *f = m_hitTestResult.innerNode()->document()->frame()->loader()->client();
-        QWidget *page = static_cast<FrameLoaderClientQt*>(f)->webFrame()->page();
-        m_menu->exec(page->mapToGlobal(m_hitTestResult.point()));
+        FrameView *v = m_hitTestResult.innerNode()->document()->frame()->view();
+        m_menu->exec(v->containingWindow()->mapToGlobal(v->contentsToWindow(m_hitTestResult.point())));
     }
     if (menu != m_menu) {
         delete m_menu;
