3ddb290e96a484d602b005032cbdd11d88190e21
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-27 01:12:14 +0000

    Reviewed by Kevin Decker
            - fixed <rdar://problem/5362989> Searching a pdf in some modes shows hilights for all instances 
              of the word, even those not on the current page
              
            It turns out that there were three different issues here, all contributing to incorrect display
            of multiple matches for PDF pages in certain display modes. (1) in non-continuous display modes,
            we weren't updating the match rects when the displayed page is changed with page up/down (e.g.);
            (2) the mechanism to update the match rects on scrolling was busted except for the first scroll
            away from 0,0; (3) the PDFKit API returns selection bounds for non-displayed pages in non-continuous
            modes just as if they were the displayed pages. This patch fixes all three issues.
    
            * WebView/WebPDFView.h:
            made ivar name even longer
            
            * WebView/WebPDFView.mm:
            (-[WebPDFView setPDFDocument:]):
            updated for ivar name change
            (-[WebPDFView viewDidMoveToWindow]):
            observe page-change notifications as well as the others; this is necessary because in the
            non-continuous modes the view can be completely updated without any scrolling involved (problem 1)
            (-[WebPDFView viewWillMoveToWindow:]):
            stop observing page-change notifications
            (-[WebPDFView rectsForTextMatches]):
            skip any pages that aren't visible; this avoids treating matches on non-displayed non-continous
            modes as if they were on the displayed page (problem 3)
            (-[WebPDFView _PDFDocumentViewMightHaveScrolled:]):
            after checking whether scroll position has changed since we last checked it, remember the new
            one (d'oh!) (problem 2)
    
            (-[WebPDFView _scaleOrDisplayModeOrPageChanged:]):
            renamed to include page changes
            
            (-[WebPDFView _visiblePDFPages]):
            new method, returns the set of pages that are at least partly visible
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24665 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebPDFView.h
+++ third_party/WebKit/WebKit/WebView/WebPDFView.h
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSString *path;
     id trackedFirstResponder;
     BOOL written;
-    BOOL _ignoreScaleAndDisplayModeNotifications;
+    BOOL _ignoreScaleAndDisplayModeAndPageNotifications;
     BOOL _willUpdatePreferencesSoon;
     PDFView *PDFSubviewProxy;
     WebDataSource *dataSource;
