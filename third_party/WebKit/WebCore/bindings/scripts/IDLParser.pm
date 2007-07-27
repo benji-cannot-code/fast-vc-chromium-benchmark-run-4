00f162d3caacacebf1f598b937fb2e6e781da8ad
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-27 08:29:11 +0000

    Fix build with moc as preprocessor. Don't rely the preprocessor preserving whitespace at the beginning of lines.
    git-svn-id: svn://svn.chromium.org/blink/trunk@24690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/IDLParser.pm
+++ third_party/WebKit/WebCore/bindings/scripts/IDLParser.pm
@@ -174,7 +174,7 @@ sub ParseInterface
     $data =~ /};/g;
     $data = substr($data, index($data, $sectionName), pos($data) - length($data));
 
-    $data =~ s/[\n\r]//g;
+    $data =~ s/[\n\r]/ /g;
 
     # Beginning of the regexp parsing magic
     if ($sectionName eq "exception") {
@@ -239,7 +239,7 @@ sub ParseInterface
             push(@$arrayRef, $line);
         }
 
-        $interfaceData =~ s/[\n\r]//g;
+        $interfaceData =~ s/[\n\r]/ /g;
         my @interfaceMethods = split(/;/, $interfaceData);
 
         foreach my $line (@interfaceMethods) {
