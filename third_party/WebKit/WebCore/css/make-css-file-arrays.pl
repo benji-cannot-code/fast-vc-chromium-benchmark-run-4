aa04c6cac7560f73d0f9627dc839362c00bfd1f6
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-27 08:28:48 +0000

    Make make-css-file-arrays.pl work on Windows by not calling /usr/bin/gcc as preprocessor for the Qt build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@24688 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/make-css-file-arrays.pl
+++ third_party/WebKit/WebCore/css/make-css-file-arrays.pl
@@ -22,6 +22,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Usage: make-css-file-arrays.pl <header> <output> <input> ...
 
 use strict;
+use Getopt::Long;
+
+my $preprocessor;
+
+GetOptions('preprocessor=s' => \$preprocessor);
+
+if (!$preprocessor) {
+    $preprocessor = "/usr/bin/gcc -E -P -x c++";
+}
 
 my $header = $ARGV[0];
 shift;
@@ -40,7 +49,7 @@ for my $in (@ARGV) {
     my $name = $1;
 
     # Slurp in the CSS file.
-    open IN, "-|", "/usr/bin/gcc", "-E", "-P", "-x", "c++", $in or die;
+    open IN, $preprocessor . " " . $in . "|" or die;
     my $text; { local $/; $text = <IN>; }
     close IN;
 
