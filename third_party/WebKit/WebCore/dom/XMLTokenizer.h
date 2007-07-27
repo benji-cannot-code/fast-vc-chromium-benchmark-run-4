ada1803ad5fd66ca1d770443dc8230a9301185e8
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-27 13:55:49 +0000

    Implement error handling.
    Add our copyrights
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/XMLTokenizer.h
+++ third_party/WebKit/WebCore/dom/XMLTokenizer.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Copyright (C) 2000 Peter Kelly (pmk@post.com)
  * Copyright (C) 2005, 2006 Apple Computer, Inc.
  * Copyright (C) 2007 Samuel Weinig (sam@webkit.org)
+ * Copyright (C) 2007 Trolltech ASA
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
