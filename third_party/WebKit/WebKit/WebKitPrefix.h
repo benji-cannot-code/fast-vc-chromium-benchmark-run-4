9ffefb0093b499b6d608135dcd68e645113b8292
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-27 20:40:05 +0000

    - fix build
            * WebKitPrefix.h: Removed the USING_WEBCORE_XXX definitions.
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView delete:]): Fixed this to use WebCore again; I accidentally revived a dead
            code path that didn't work in the last patch!
            (-[WebHTMLView deleteToMark:]): Ditto.
            (-[WebHTMLView copy:]): Removed the unused side of the ifdef.
            (-[WebHTMLView cut:]): Ditto.
            (-[WebHTMLView paste:]): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24745 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebKitPrefix.h
+++ third_party/WebKit/WebKit/WebKitPrefix.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2005, 2006, 2007 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -74,8 +74,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* Work around bug with C++ library that screws up Objective-C++ when exception support is disabled. */
 #undef try
 #undef catch
-
-#define USING_WEBCORE_CUT 1
-#define USING_WEBCORE_COPY 1
-//#define USING_WEBCORE_PASTE 1
-#define USING_WEBCORE_DELETE 1
