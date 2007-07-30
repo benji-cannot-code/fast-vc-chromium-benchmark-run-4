3bafc94b3f1d2ff1e6702e9294562866abb6e761
adele <adele@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-30 23:56:19 +0000

    Reviewed by Darin.
            Fix for <rdar://problem/5274937> Safari 3 breaks Missing Sync's WebKit code by crashing
    
            * loader/MainResourceLoader.cpp: (WebCore::MainResourceLoader::continueAfterContentPolicy): Nil check for the FrameLoader.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24786 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/MainResourceLoader.cpp
+++ third_party/WebKit/WebCore/loader/MainResourceLoader.cpp
@@ -249,7 +249,7 @@ void MainResourceLoader::continueAfterContentPolicy(PolicyAction policy)
 {
     ASSERT(m_waitingForContentPolicy);
     m_waitingForContentPolicy = false;
-    if (!frameLoader()->isStopping())
+    if (frameLoader() && !frameLoader()->isStopping())
         continueAfterContentPolicy(policy, m_response);
     deref(); // balances ref in didReceiveResponse
 }
