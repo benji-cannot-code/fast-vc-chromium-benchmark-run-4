81c898f834cd2765753c900c6589eed94e5274dc
treat <treat@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-07-31 03:44:13 +0000

    Fix build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@24791 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitQt/WebCoreSupport/EditorClientQt.h
+++ third_party/WebKit/WebKitQt/WebCoreSupport/EditorClientQt.h
@@ -66,6 +66,8 @@ public:
 
     virtual bool shouldApplyStyle(CSSStyleDeclaration*, Range*);
 
+    virtual bool shouldMoveRangeAfterDelete(Range*, Range*);
+
     virtual void didBeginEditing();
     virtual void respondToChangedContents();
     virtual void respondToChangedSelection();
