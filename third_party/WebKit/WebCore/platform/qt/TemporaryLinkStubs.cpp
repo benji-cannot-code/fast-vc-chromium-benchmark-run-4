9359a14591dcb4fe8caaea4af75e665b3b47d3f0
treat <treat@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-02 04:21:28 +0000

    Add an interface to manage global history for clients
    git-svn-id: svn://svn.chromium.org/blink/trunk@24815 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/qt/TemporaryLinkStubs.cpp
@@ -68,9 +68,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 
-
-bool WebCore::historyContains(DeprecatedString const&) { return false; }
-
 void Frame::setNeedsReapplyStyles() { notImplemented(); }
 
 void FrameView::updateBorder() { notImplemented(); }
