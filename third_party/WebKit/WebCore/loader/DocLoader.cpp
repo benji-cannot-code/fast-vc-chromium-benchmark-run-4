28346124945ca541cb63f6b6718fbe5d961d2d97
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-03 23:07:22 +0000

    Reviewed by Geoff.
            <rdar://problem/5374393> 
            Image change on disk not noticed by WebView; -[WebCache setDisabled:] used to cause a reload every time
            
            This is essentially a better fix for the crash in <rdar://problem/5362783>.
            
            * loader/DocLoader.cpp:
            (WebCore::DocLoader::requestResource):
            If the resource already exists in the m_docResources map, remove it and disassociate it from the doc loader.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@24861 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/DocLoader.cpp
+++ third_party/WebKit/WebCore/loader/DocLoader.cpp
@@ -139,10 +139,11 @@ CachedResource* DocLoader::requestResource(CachedResource::Type type, const Stri
     KURL fullURL = m_doc->completeURL(url.deprecatedString());
     
     if (cache()->disabled()) {
-        if (CachedResource* resource = m_docResources.get(fullURL.url())) {
-            if (type != resource->type())
-                return 0;
-            return resource;
+        HashMap<String, CachedResource*>::iterator it = m_docResources.find(fullURL.url());
+        
+        if (it != m_docResources.end()) {
+            it->second->setDocLoader(0);
+            m_docResources.remove(it);
         }
     }
                                                           
@@ -153,7 +154,6 @@ CachedResource* DocLoader::requestResource(CachedResource::Type type, const Stri
 
     CachedResource* resource = cache()->requestResource(this, type, fullURL, charset, skipCanLoadCheck, sendResourceLoadCallbacks);
     if (resource) {
-        ASSERT(!cache()->disabled() || !m_docResources.contains(resource->url()));
         m_docResources.set(resource->url(), resource);
         checkCacheObjectStatus(resource);
     }
