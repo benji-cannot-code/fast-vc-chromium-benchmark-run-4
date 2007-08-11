d07cc72f1e0293b791949102f357bb56d92c5b72
zecke <zecke@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-11 18:13:31 +0000

    2007-08-11 Holger Hans Peter Freyther <zecke@selfish.org>
            Reviewed by Anders.
    
            Implement passing events to a subframe. The code is copied from
            the windows port and passSubframeEventToSubframe was removed as it
            is not called and it is not avilable in the windows port as well.
    
            * page/gdk/EventHandlerGdk.cpp:
            (WebCore::EventHandler::passMousePressEventToSubframe):
            (WebCore::EventHandler::passMouseMoveEventToSubframe):
            (WebCore::EventHandler::passMouseReleaseEventToSubframe):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25012 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/gdk/EventHandlerGdk.cpp
+++ third_party/WebKit/WebCore/page/gdk/EventHandlerGdk.cpp
@@ -44,19 +44,9 @@ namespace WebCore {
 
 using namespace EventNames;
 
-static bool isKeyboardOptionTab(KeyboardEvent* event)
-{
-    return event
-        && (event->type() == keydownEvent || event->type() == keypressEvent)
-        && event->altKey()
-        && event->keyIdentifier() == "U+0009";
-}
-
 bool EventHandler::tabsToAllControls(KeyboardEvent* event) const
 {
-    bool handlingOptionTab = isKeyboardOptionTab(event);
-    
-    return handlingOptionTab;
+    return true;
 }
 
 void EventHandler::focusDocumentView()
@@ -86,16 +76,9 @@ bool EventHandler::passMouseDownEventToWidget(Widget* widget)
     return false;
 }
 
-bool EventHandler::eventActivatedView(const PlatformMouseEvent& event) const
+bool EventHandler::eventActivatedView(const PlatformMouseEvent&) const
 {
     notImplemented();
-    return false;
-}
-
-bool EventHandler::passSubframeEventToSubframe(MouseEventWithHitTestResults& event, Frame* subframe, HitTestResult*)
-{
-    notImplemented();
-    return false;
 }
 
 bool EventHandler::passWheelEventToWidget(PlatformWheelEvent&, Widget* widget)
@@ -112,17 +95,20 @@ Clipboard* EventHandler::createDraggingClipboard() const
 
 bool EventHandler::passMousePressEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
 {
-    return passSubframeEventToSubframe(mev, subframe);
+    subframe->eventHandler()->handleMousePressEvent(mev.event());
+    return true;
 }
 
 bool EventHandler::passMouseMoveEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe, HitTestResult* hoveredNode)
 {
-    return passSubframeEventToSubframe(mev, subframe, hoveredNode);
+    subframe->eventHandler()->handleMouseMoveEvent(mev.event(), hoveredNode);
+    return true;
 }
 
 bool EventHandler::passMouseReleaseEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
 {
-    return passSubframeEventToSubframe(mev, subframe);
+    subframe->eventHandler()->handleMouseReleaseEvent(mev.event());
+    return true;
 }
 
 bool EventHandler::passMousePressEventToScrollbar(MouseEventWithHitTestResults&, PlatformScrollbar* scrollbar)
