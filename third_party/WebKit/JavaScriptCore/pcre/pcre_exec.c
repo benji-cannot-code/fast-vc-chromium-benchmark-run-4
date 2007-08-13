ffe6c56a0a1aaae4b3f56b737f15c87e7d8cec57
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-13 02:42:17 +0000

    JavaScriptCore:
            Reviewed by Maciej.
    
            - fix http://bugs.webkit.org/show_bug.cgi?id=14931
              <rdar://problem/5403816> JavaScript regular expression non-participating capturing parentheses
              fail in 3 different ways
    
            Test: fast/js/regexp-non-capturing-groups.html
    
            * kjs/string_object.cpp:
            (KJS::replace): Add missing code to handle undefined backreferences; before we'd get the empty string
            instead of a JavaScript "undefined" value.
            (KJS::StringProtoFunc::callAsFunction): Implemented backreference support for split.
            * pcre/pcre_exec.c: (match): Made backreferences to undefined groups match the empty string instead
            of always failing. Only in JAVASCRIPT mode.
    
            * tests/mozilla/expected.html: Add a new expected test success, since this fixed one test result.
    
    LayoutTests:
    
            Reviewed by Maciej.
    
            - test for http://bugs.webkit.org/show_bug.cgi?id=14931
              JavaScript regular expression non-participating capturing groups behave incorrectly in edge cases
    
            * fast/js/regexp-non-capturing-groups-expected.txt: Added.
            * fast/js/regexp-non-capturing-groups.html: Added.
            * fast/js/resources/regexp-non-capturing-groups.js: Added.
    
            * fast/js/resources/js-test-pre.js: Updated to add a special case for array
            results, since there are some array results in the test. Also cut down on the
            verbosity of failures when the type of the actual result is the same as the type
            of the expected result. And get rid of the special case function just for NaN.
    
            * fast/js/regexp-unicode-overflow.html: Let the make-js-test-wrappers script
            regenerate this file (it removed one of the trailing newlines).
    
            * fast/js/kde/encode_decode_uri-expected.txt: This test now passes. I'm not sure
            why it was failing before.
    
            * fast/js/char-at-expected.txt: Removed all the periods at the end of NaN
            test PASS messages; they are now consistent with all the other tests.
            * fast/js/date-parse-comments-test-expected.txt: Ditto.
            * fast/js/date-parse-test-expected.txt: Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25026 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/pcre/pcre_exec.c
+++ third_party/WebKit/JavaScriptCore/pcre/pcre_exec.c
@@ -1467,7 +1467,6 @@ for (;;)
 
     case OP_REF:
       {
-      int tmplen;
       offset = GET2(ecode, 1) << 1;               /* Doubled ref number */
       ecode += 3;                                 /* Advance past item */
 
@@ -1476,9 +1475,12 @@ for (;;)
       can't just fail here, because of the possibility of quantifiers with zero
       minima. */
 
-      tmplen = INT_CAST(md->end_subject - eptr + 1);
       length = (offset >= offset_top || md->offset_vector[offset] < 0)?
-        tmplen :
+#if JAVASCRIPT
+        0 : /* in JavaScript these match the empty string */
+#else
+        INT_CAST(md->end_subject - eptr + 1) :
+#endif
         md->offset_vector[offset+1] - md->offset_vector[offset];
 
       /* Set up for repetition, or handle the non-repeated case */
