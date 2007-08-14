3d1c2c5e2781424408c9fdbd5e073293c71ca317
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-14 22:37:57 +0000

    One-line obvious fix for context menus: don't connect the signal once for each action, only once for the menu.
    git-svn-id: svn://svn.chromium.org/blink/trunk@25079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/ContextMenuQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/ContextMenuQt.cpp
@@ -46,6 +46,7 @@ ContextMenu::ContextMenu(const HitTestResult& result)
     m_menu = new QMenu;
     //qDebug("Create menu(%p) %p", this, (QMenu*)m_menu);
     m_proxy = new MenuEventProxy(this);
+    QObject::connect(m_menu, SIGNAL(triggered(QAction *)), m_proxy, SLOT(trigger(QAction *)));
 #endif
 }
 
@@ -92,7 +93,6 @@ void ContextMenu::insertItem(unsigned position, ContextMenuItem& item)
                 action = m_menu->addAction((QString)item.title());
                 m_menu->removeAction(action);
                 m_menu->insertAction(before, action);
-                QObject::connect(m_menu, SIGNAL(triggered(QAction *)), m_proxy, SLOT(trigger(QAction *)));
             }
             break;
         case SeparatorType:
