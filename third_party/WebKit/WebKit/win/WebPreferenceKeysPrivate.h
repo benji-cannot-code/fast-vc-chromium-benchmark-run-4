f1c31fb0ebaa22167f1cef38d95a529e4a543793
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-14 21:24:28 +0000

    Fix <rdar://problem/5214504> No way to change WebPreferences defaults (all preferences get written to disk)
            We now only write to disk values that have been explicitly set, and
            never write any default values.
    
            This is not enough, however, as anyone who has used WebKit prior to
            this change will have the default values sitting in their preferences
            plist on disk. To remedy this, we perform a one-time removal of any
            key-value pairs that match a pair in the defaults dictionary.
    
            Reviewed by Steve, John, Ada, and Darin.
    
            * WebPreferenceKeysPrivate.h: Added new key to ensure we only remove
            the defaults once.
            * WebPreferences.cpp:
            (WebPreferences::initializeDefaultSettings): Renamed from initialize, and
            made sure we only set up the defaults dictionary once. Now that the
            defaults dictionary is never modified after creation, we don't need to
            check whether the keys are already present.
            (WebPreferences::setStringValue): Don't modify the defaults dictionary.
            (WebPreferences::setIntegerValue): Ditto.
            (WebPreferences::setBoolValue): Ditto.
            (WebPreferences::save): Write out the user's preferences, not the defaults.
            (WebPreferences::load):
               - Removed gotos and early declarations now that we're using
                 RetainPtr everywhere.
               - Initialize m_privatePrefs with an empty dictionary if we didn't
                 read in a plist.
               - Call migrateDefaultSettingsFromSafari3Beta after loading.
            (WebPreferences::migrateDefaultSettingsFromSafari3Beta): Added.
            (WebPreferences::removeValuesMatchingDefaultSettings): Added.
            (WebPreferences::initWithIdentifier): Don't initialize m_privatePrefs
            -- load() does this now.
            * WebPreferences.h: Updated/added declarations.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebPreferenceKeysPrivate.h
+++ third_party/WebKit/WebKit/win/WebPreferenceKeysPrivate.h
@@ -94,3 +94,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define AllowContinuousSpellCheckingPreferenceKey "AllowContinuousSpellCheckingPreferenceKey" // default: true
 
 #define WebKitDOMPasteAllowedPreferenceKey "WebKitDOMPasteAllowedPreferenceKey" // default: false
+
+// If this key is present and has a value of true, we have already removed the default values from the user's preferences <rdar://problem/5214504>
+#define WebKitDidMigrateDefaultSettingsFromSafari3BetaPreferenceKey "WebKitDidMigrateDefaultSettingsFromSafari3BetaPreferenceKey"
