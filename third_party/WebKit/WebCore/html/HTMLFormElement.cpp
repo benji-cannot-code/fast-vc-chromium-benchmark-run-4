46f9e6996cf7b9275842a0263cfd67625bbfa350
honeycutt <honeycutt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-14 23:00:52 +0000

    2007-08-14  Jon Honeycutt  <jhoneycutt@apple.com>
            Reviewed by Steve.
    
            Build fix for Windows.
    
            * html/HTMLFormElement.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25080 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLFormElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLFormElement.cpp
@@ -43,6 +43,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MIMETypeRegistry.h"
 #include "RenderTextControl.h"
 
+#if PLATFORM(WIN_OS)
+#include <shlwapi.h>
+#endif
+
 namespace WebCore {
 
 using namespace EventNames;
