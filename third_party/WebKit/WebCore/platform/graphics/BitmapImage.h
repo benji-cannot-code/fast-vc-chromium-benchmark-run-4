94c294b105513e1de87336aaab46cd35e32d9c07
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-16 04:34:11 +0000

    2007-08-15  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Geoff.
    
            <rdar://problem/5389696> leak of 32-byte NSData object (and more?) in WebIconDatabase code path with each refresh of http://www.apple.com
            
            * platform/graphics/BitmapImage.h: Use RetainPtr for m_nsImage and m_tiffRep
            * platform/graphics/mac/ImageMac.mm:
            (WebCore::BitmapImage::initPlatformData): No need to do anything now
            (WebCore::BitmapImage::invalidatePlatformData): Simplify
            (WebCore::BitmapImage::getTIFFRepresentation): Use RetainPtr to avoid leaks
            (WebCore::BitmapImage::getNSImage): Use RetainPtr to avoid leaks
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/BitmapImage.h
+++ third_party/WebKit/WebCore/platform/graphics/BitmapImage.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IntSize.h"
 
 #if PLATFORM(MAC)
+#include <wtf/RetainPtr.h>
 #ifdef __OBJC__
 @class NSImage;
 #else
@@ -176,8 +177,8 @@ private:
     int m_repetitionsComplete;  // How many repetitions we've finished.
 
 #if PLATFORM(MAC)
-    mutable NSImage* m_nsImage; // A cached NSImage of frame 0. Only built lazily if someone actually queries for one.
-    mutable CFDataRef m_tiffRep; // Cached TIFF rep for frame 0.  Only built lazily if someone queries for one.
+    mutable RetainPtr<NSImage> m_nsImage; // A cached NSImage of frame 0. Only built lazily if someone actually queries for one.
+    mutable RetainPtr<CFDataRef> m_tiffRep; // Cached TIFF rep for frame 0.  Only built lazily if someone queries for one.
 #endif
 
     Color m_solidColor;  // If we're a 1x1 solid color, this is the color to use to fill.
