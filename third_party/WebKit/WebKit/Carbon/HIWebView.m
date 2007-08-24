22aa52b7c452546b047a7183ff909b989b488ec4
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-24 22:20:37 +0000

    Reviewed by John Sullivan.
            <rdar://problem/5410937> HIWebView in SimpleCarbonWeb doesn't seem to be getting adequate invalidation when window is resized
    
            This is a more localized fix for yesterday's change. Now explicitly call _web_layoutIfNeededRecursive inside the
            HIWebView Draw() function. Adds a FIXME to explain that we need to do layout before Carbon has decided what regions to draw.
            Doing layout in Draw() will potentially cause drawing to happen in two passes, but this has always been a problem in Carbon.
    
            * Carbon/HIWebView.m:
            (Draw): Call _web_layoutIfNeededRecursive on the main WebHTMLView.
            (SetFocusPart): Fix to work in ObjC++ (now that HIWebView.m is treated as a ObjC++ file.)
            * WebView/WebView.mm: Removes the 4 displayIfNeeded methods from yesterday's change.
            * WebKit.xcodeproj/project.pbxproj: Force the file type of HIWebView.m to ObjC++ so WebHTMLViewInternal.h can be included.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Carbon/HIWebView.m
+++ third_party/WebKit/WebKit/Carbon/HIWebView.m
@@ -29,15 +29,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifndef __LP64__
 
-#include "HIWebView.h"
+#import "HIWebView.h"
 
-#include "CarbonWindowAdapter.h"
-
-#include <WebKit/WebKit.h>
-#include "HIViewAdapter.h"
-#include <WebKitSystemInterface.h>
+#import "CarbonWindowAdapter.h"
+#import "HIViewAdapter.h"
+#import "WebHTMLViewInternal.h"
+#import "WebKit.h"
 
 #import <objc/objc-runtime.h>
+#import <WebKitSystemInterface.h>
 
 @interface NSWindow (AppKitSecretsHIWebViewKnows)
 - (void)_removeWindowRef;
@@ -46,6 +46,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface NSView (AppKitSecretsHIWebViewKnows)
 - (void)_clearDirtyRectsForTree;
 @end
+
+extern "C" void HIWebViewRegisterClass();
 
 @interface MenuItemProxy : NSObject <NSValidatedUserInterfaceItem>
 {
@@ -144,7 +146,6 @@ static const EventTypeSpec kEvents[] = {
 
 static HIWebView*		HIWebViewConstructor( HIViewRef inView );
 static void				HIWebViewDestructor( HIWebView* view );
-void                    HIWebViewRegisterClass( void );
 
 static OSStatus			HIWebViewEventHandler(
 								EventHandlerCallRef	inCallRef,
@@ -367,6 +368,15 @@ Draw( HIWebView* inView, RgnHandle limitRgn, CGContextRef inContext )
 
 //    printf( "Drawing: drawRect is (%g %g) (%g %g)\n", hiRect.origin.x, hiRect.origin.y,
 //            hiRect.size.width, hiRect.size.height );
+
+    // FIXME: We need to do layout before Carbon has decided what region needs drawn.
+    // In Cocoa we make sure to do layout and invalidate any new regions before draw, so everything
+    // can be drawn in one pass. Doing a layout here will cause new regions to be invalidated, but they
+    // will not all be drawn in this pass since we already have a fixed rect we are going to display.
+
+    NSView <WebDocumentView> *documentView = [[[inView->fWebView mainFrame] frameView] documentView];
+    if ([documentView isKindOfClass:[WebHTMLView class]])
+        [(WebHTMLView *)documentView _web_layoutIfNeededRecursive];
 
     if ( inView->fIsComposited )
         [inView->fWebView displayIfNeededInRect: *(NSRect*)&hiRect];
@@ -878,7 +888,10 @@ SetFocusPart(
 
         // Advance the keyboard focus, maybe right off of this view.  Maybe a subview of this one already has the keyboard focus, maybe not.
         freshlyMadeFirstResponderView = AdvanceFocus( view, goForward );
-        partCodeToReturn = freshlyMadeFirstResponderView ? desiredFocus : kControlFocusNoPart;
+        if (freshlyMadeFirstResponderView)
+            partCodeToReturn = desiredFocus;
+        else
+            partCodeToReturn = kControlFocusNoPart;
         //NSLog(freshlyMadeFirstResponderView ? @"Advanced the key focus." : @"Relinquished the key focus.");
     }
 	else
