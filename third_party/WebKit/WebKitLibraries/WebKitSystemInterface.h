c2ebc8d88ed66fd37818b353a6efd514ab884fb4
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-28 19:03:40 +0000

    Add WKSetNSURLRequestShouldContentSniff.
            * WebKitSystemInterface.h:
            * libWebKitSystemInterface.a:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25273 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
+++ third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
@@ -46,11 +46,13 @@ void WKSetNSURLConnectionDefersCallbacks(NSURLConnection *connection, BOOL defer
 float WKSecondsSinceLastInputEvent(void);
 
 void WKShowKeyAndMain(void);
+#ifndef __LP64__
 OSStatus WKSyncWindowWithCGAfterMove(WindowRef);
 unsigned WKCarbonWindowMask(void);
 void *WKGetNativeWindowFromWindowRef(WindowRef);
 OSType WKCarbonWindowPropertyCreator(void);
 OSType WKCarbonWindowPropertyTag(void);
+#endif
 
 typedef id WKNSURLConnectionDelegateProxyPtr;
 
@@ -59,6 +61,7 @@ WKNSURLConnectionDelegateProxyPtr WKCreateNSURLConnectionDelegateProxy(void);
 void WKDisableCGDeferredUpdates(void);
 
 Class WKNSURLProtocolClassForReqest(NSURLRequest *request);
+void WKSetNSURLRequestShouldContentSniff(NSMutableURLRequest *request, BOOL shouldContentSniff);
 
 unsigned WKGetNSAutoreleasePoolCount(void);
 
@@ -107,10 +110,12 @@ void WKDrawBezeledTextArea(NSRect, BOOL enabled);
 void WKPopupMenu(NSMenu*, NSPoint location, float width, NSView*, int selectedItem, NSFont*);
 
 void WKSendUserChangeNotifications(void);
+#ifndef __LP64__    
 BOOL WKConvertNSEventToCarbonEvent(EventRecord *carbonEvent, NSEvent *cocoaEvent);
 void WKSendKeyEventToTSM(NSEvent *theEvent);
 void WKCallDrawingNotification(CGrafPtr port, Rect *bounds);
-
+#endif
+    
 BOOL WKGetGlyphTransformedAdvances(NSFont *font, CGAffineTransform *m, ATSGlyphRef *glyph, CGSize *advance);
 CGFontRef WKGetCGFontFromNSFont(NSFont *font);
 void WKGetFontMetrics(NSFont *font, int *ascent, int *descent, int *lineGap, unsigned *unitsPerEm);
@@ -136,9 +141,11 @@ ATSLayoutRecord *WKGetGlyphVectorFirstRecord(WKGlyphVectorRef glyphVector);
 size_t WKGetGlyphVectorRecordSize(WKGlyphVectorRef glyphVector);
 ATSGlyphRef WKGetDefaultGlyphForChar(NSFont *font, UniChar c);
 
+#ifndef __LP64__
 NSEvent *WKCreateNSEventWithCarbonEvent(EventRef eventRef);
 NSEvent *WKCreateNSEventWithCarbonMouseMoveEvent(EventRef inEvent, NSWindow *window);
 NSEvent *WKCreateNSEventWithCarbonClickEvent(EventRef inEvent, WindowRef windowRef);
+#endif
 
 CGContextRef WKNSWindowOverrideCGContext(NSWindow *, CGContextRef);
 void WKNSWindowRestoreCGContext(NSWindow *, CGContextRef);
