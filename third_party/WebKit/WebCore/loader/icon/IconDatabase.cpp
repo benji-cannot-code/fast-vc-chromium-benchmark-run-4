aa434781403ac9377d0719ec0428ba266de6e4d4
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-08-28 20:12:29 +0000

    2007-08-28  Mark Rowe  <mrowe@apple.com>
            Reviewed by Darin Adler.
    
            <rdar://problem/5443453> Decreasing history expiration time from 1 year to 1 week can lead to long hang while icon database syncs
    
            Perform the sync inside a SQLite transaction.  This drops the time taken for the sync from over 90s to under half a second in the
            extreme case of over 90,000 URLs being pruned.
    
            * loader/icon/IconDatabase.cpp:
            (WebCore::IconDatabase::syncDatabase):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/icon/IconDatabase.cpp
+++ third_party/WebKit/WebCore/loader/icon/IconDatabase.cpp
@@ -839,6 +839,9 @@ void IconDatabase::syncDatabase()
     double timestamp = currentTime();
 #endif
 
+    SQLTransaction syncTransaction(*m_currentDB);
+    syncTransaction.begin();
+
     // First we'll do the pending additions
     // Starting with the IconDataCaches that need updating/insertion
     for (HashSet<IconDataCache*>::iterator i = m_iconDataCachesPendingUpdate.begin(), end = m_iconDataCachesPendingUpdate.end(); i != end; ++i) {
@@ -876,7 +879,8 @@ void IconDatabase::syncDatabase()
         LOG(IconDatabase, "Deleted icon %s", (*i).ascii().data());   
     }
     m_iconURLsPendingDeletion.clear();
-    
+    syncTransaction.commit();
+
     // If the timer was running to cause this update, we can kill the timer as its firing would be redundant
     m_updateTimer.stop();
     
