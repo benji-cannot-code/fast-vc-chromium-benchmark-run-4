9bd6b7b8afad573ce26625bb18a968c2b2e78eae
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-04 21:56:31 +0000

    Blind attempt at fixing Windows debug build
    git-svn-id: svn://svn.chromium.org/blink/trunk@25357 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/icon/SQLDatabase.h
+++ third_party/WebKit/WebCore/loader/icon/SQLDatabase.h
@@ -31,6 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Noncopyable.h>
 #include <wtf/Vector.h>
 
+#ifndef NDEBUG
+#include <pthread.h>
+#endif
 
 typedef struct sqlite3 sqlite3;
 
