7953dda21febed8ba48794a9a95f5b96a1d05cbf
andersca <andersca@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-07 22:46:19 +0000

    Reviewed by Oliver.
            <rdar://problem/5461487>
            Seed: Embedded media content (Flash Player 9) inside RSS reported as unknown MIME type.
            
            Don't return ObjectContentNone if the URL's extension can't be mapped to a MIME type. If the MIME type is empty, 
            we will try to display the content in a subframe instead, just like Mac WebKit.
            
            * WebFrame.cpp:
            (WebFrame::objectContentType):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25427 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebFrame.cpp
+++ third_party/WebKit/WebKit/win/WebFrame.cpp
@@ -2299,11 +2299,8 @@ Widget* WebFrame::createJavaAppletWidget(const IntSize&, Element* element, const
 ObjectContentType WebFrame::objectContentType(const KURL& url, const String& mimeTypeIn)
 {
     String mimeType = mimeTypeIn;
-    if (mimeType.isEmpty()) {
+    if (mimeType.isEmpty())
         mimeType = MIMETypeRegistry::getMIMETypeForExtension(url.path().mid(url.path().findRev('.')+1));
-        if(mimeType.isEmpty())
-            return WebCore::ObjectContentNone;
-    }
 
     if (mimeType.isEmpty())
         return ObjectContentFrame; // Go ahead and hope that we can display the content.
@@ -2317,7 +2314,7 @@ ObjectContentType WebFrame::objectContentType(const KURL& url, const String& mim
     if (MIMETypeRegistry::isSupportedNonImageMIMEType(mimeType))
         return WebCore::ObjectContentFrame;
 
-    return (ObjectContentType)0;
+    return WebCore::ObjectContentNone;
 }
 
 String WebFrame::overrideMediaType() const
