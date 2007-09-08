963f62bc5ca4f9c7d4cf062c27985e92aaa84c7c
weinig <weinig@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-08 22:45:55 +0000

    Add fix to the correct file.
            * loader/icon/SQLDatabase.h:
            * loader/icon/SQLStatement.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25451 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/icon/SQLDatabase.h
+++ third_party/WebKit/WebCore/loader/icon/SQLDatabase.h
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <pthread.h>
 #endif
 
+#if COMPILER(MSVC)
+#pragma warning(disable: 4800)
+#endif
+
 typedef struct sqlite3 sqlite3;
 
 namespace WebCore {
