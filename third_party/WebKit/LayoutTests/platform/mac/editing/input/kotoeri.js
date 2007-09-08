7add2fa84e21640813a94ffa34dc42f5de435b41
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-08 03:24:31 +0000

    Reviewed by Sam.
            Re-adding a few mac specific directories that accidentally got clobbered
            during my recent layout test rearranging
    
            * platform/mac/editing/input: Added.
            * platform/mac/editing/input/hangul-enter-confirms-and-sends-keypress-expected.txt: Added.
            * platform/mac/editing/input/hangul-enter-confirms-and-sends-keypress.html: Added.
            * platform/mac/editing/input/hangul.js: Added.
            * platform/mac/editing/input/kotoeri-enter-to-confirm-and-newline-expected.txt: Added.
            * platform/mac/editing/input/kotoeri-enter-to-confirm-and-newline.html: Added.
            * platform/mac/editing/input/kotoeri.js: Added.
            * platform/mac/editing/input/logger.js: Added.
            * platform/mac/fast/AppleScript: Added.
            * platform/mac/fast/AppleScript/001-expected.txt: Added.
            * platform/mac/fast/AppleScript/001.html: Added.
            * platform/mac/fast/AppleScript/array-expected.txt: Added.
            * platform/mac/fast/AppleScript/array.html: Added.
            * platform/mac/fast/AppleScript/date-expected.txt: Added.
            * platform/mac/fast/AppleScript/date.html: Added.
            * platform/mac/fast/objc: Added.
            * platform/mac/fast/objc/longlongTest-expected.txt: Added.
            * platform/mac/fast/objc/longlongTest.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25436 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/platform/mac/editing/input/kotoeri.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var kotoeriState = { "compositionString": null };
+function kotoeri(event){
+    if (event.characters[0] == '\n') {
+        if (kotoeriState.compositionString == null) {
+            textInputController.doCommand("insertNewline:");
+            return true;
+        }
+        textInputController.insertText(kotoeriState.compositionString);
+        kotoeriState.compositionString = null;
+        return true;
+    }        
+        
+    if (kotoeriState.compositionString == null) 
+         kotoeriState.compositionString = "";
+    kotoeriState.compositionString += event.characters;
+    var markedText = textInputController.makeAttributedString(kotoeriState.compositionString);
+    markedText.addAttribute("NSUnderline", 2);
+    textInputController.setMarkedText(markedText, markedText.length, 0);
+    return true;
+}
