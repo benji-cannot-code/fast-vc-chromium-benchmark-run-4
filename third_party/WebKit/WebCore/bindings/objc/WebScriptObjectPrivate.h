5c18355133eb0fa92d07df150e94f21d455cda5f
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-08 03:10:42 +0000

    WebCore:
            Reviewed by Timothy Hatcher.
    
            Housekeeping.  No test required.
    
            * bindings/objc/WebScriptObjectInternal.h: Removed unused file.
            * bindings/objc/WebScriptObjectPrivate.h: Updated copyright statement.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25435 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/WebScriptObjectPrivate.h
+++ third_party/WebKit/WebCore/bindings/objc/WebScriptObjectPrivate.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
-    Copyright (C) 2004 Apple Computer, Inc. All rights reserved.    
+    Copyright (C) 2004, 2005, 2006, 2007 Apple Inc. All rights reserved.    
 */
 #ifndef _WEB_SCRIPT_OBJECT_PRIVATE_H_
 #define _WEB_SCRIPT_OBJECT_PRIVATE_H_
