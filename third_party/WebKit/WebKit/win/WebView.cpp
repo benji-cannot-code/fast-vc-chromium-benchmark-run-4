8845941352b49b4c9d9ddca793b23373fb17da32
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-11 18:11:12 +0000

    Roll out accidentally-checked in change.
    git-svn-id: svn://svn.chromium.org/blink/trunk@25496 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -3909,7 +3909,6 @@ bool WebView::onIMEComposition(LPARAM lparam)
             goto cleanup;
         
         targetFrame->editor()->replaceMarkedText(compositionString);
-        targetFrame->editor()->unmarkText();
         RefPtr<Range> sourceRange = targetFrame->selectionController()->selection().toRange();
         setSelectionToEndOfRange(targetFrame, sourceRange.get());
     } else if (lparam) {
