876fed856712642e8d73e949bb815ff27861bab8
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-11 16:00:44 +0000

    Rubber-stamped by Mark Rowe.
            * Misc/WebNSPasteboardExtras.mm: Fix incorrect strings in my last check-in.
            The strings I checked in were wrong and were breaking layout tests too.
            These new ones match what WebKitSystemInterface was returning.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25490 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/Misc/WebNSPasteboardExtras.mm
+++ third_party/WebKit/WebKit/Misc/WebNSPasteboardExtras.mm
@@ -53,8 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 
-NSString *WebURLPboardType = @"CorePasteboardFlavorType 0x75726C20";
-NSString *WebURLNamePboardType = @"CorePasteboardFlavorType 0x75726C6E";
+NSString *WebURLPboardType = @"public.url";
+NSString *WebURLNamePboardType = @"public.url-name";
 
 @implementation NSPasteboard (WebExtras)
 
