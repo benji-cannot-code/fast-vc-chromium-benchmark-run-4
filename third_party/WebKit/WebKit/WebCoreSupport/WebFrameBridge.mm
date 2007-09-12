df033c0c7ca750a50b0128655961be27b7110dbc
ddkilzer <ddkilzer@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-12 22:23:51 +0000

    WebKit:
            Rubber-stamped by Darin and reviewed by Adam.
    
            Removed import of unused icon database headers.
    
            * WebCoreSupport/WebFrameBridge.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25519 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport/WebFrameBridge.mm
+++ third_party/WebKit/WebKit/WebCoreSupport/WebFrameBridge.mm
@@ -44,8 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebHTMLViewInternal.h"
 #import "WebHistoryItemInternal.h"
 #import "WebHistoryItemPrivate.h"
-#import "WebIconDatabase.h"
-#import "WebIconDatabasePrivate.h"
 #import "WebJavaPlugIn.h"
 #import "WebJavaScriptTextInputPanel.h"
 #import "WebKitErrorsPrivate.h"
