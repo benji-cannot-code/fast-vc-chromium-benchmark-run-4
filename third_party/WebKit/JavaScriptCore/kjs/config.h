b29886dac796390a5633359e9c3070ba99d41aec
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-13 18:52:29 +0000

    2007-09-12  Geoff Garen  <ggaren@apple.com>
            Reviewed by Sam Weinig.
    
            Fixed <rdar://problem/5429064> 141885 Safari JavaScript: Math.random() slightly less randomly distributed than on Safari / Mac
    
            Math.random was skewed slightly upward because it assumed that RAND_MAX was outside the range of
            values that rand() might return. This problem was particularly pronounced on Windows because
            the range of values returned by rand() on Windows is 2^16 smaller than the range of values
            return by rand() on Mac.
    
            Fixed by accounting for RAND_MAX return values. Also, switched Windows over to rand_s, which has
            a range that's equal to rand()'s range on Mac.
    
            * kjs/config.h:
    
            * kjs/math_object.cpp:
            (MathFuncImp::callAsFunction): Use the new new thing.
    
            * wtf/MathExtras.h: Platform abstraction for random numbers, to cover over differences on Windows.
            (wtf_random_init):
            (wtf_random):
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25541 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/config.h
+++ third_party/WebKit/JavaScriptCore/kjs/config.h
@@ -46,6 +46,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define max max
 #define min min
 
+// We need to define this before the first #include of stdlib.h or it won't contain rand_s.
+#define _CRT_RAND_S
+
 #define HAVE_FLOAT_H 1
 #define HAVE_FUNC__FINITE 1
 #define HAVE_SYS_TIMEB_H 1
