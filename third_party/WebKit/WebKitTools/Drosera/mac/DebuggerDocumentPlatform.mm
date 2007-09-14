e21dd63315d795080aa42f279248ad81c0fd75f4
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-14 18:02:52 +0000

    Reviewed by Adam.
            - Changing Win version of Drosera work with recent changes.
    
            * Drosera/DebuggerDocument.cpp:
            * Drosera/DebuggerDocument.h:
            * Drosera/mac/DebuggerDocumentPlatform.mm:
            (NSStringCreateWithJSStringRef):
            (JSValueRefCreateWithNSString):
            * Drosera/win/DebuggerClient.cpp:
            (DebuggerClient::stepInto):
            * Drosera/win/Drosera.vcproj/Drosera.vcproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Drosera/mac/DebuggerDocumentPlatform.mm
+++ third_party/WebKit/WebKitTools/Drosera/mac/DebuggerDocumentPlatform.mm
@@ -36,6 +36,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <JavaScriptCore/JSStringRefCF.h>
 #import <JavaScriptCore/RetainPtr.h>
 
+NSString* NSStringCreateWithJSStringRef(JSStringRef jsString)
+{
+    CFStringRef cfString = JSStringCopyCFString(kCFAllocatorDefault, jsString);
+    return (NSString *)cfString;
+}
+
+JSValueRef JSValueRefCreateWithNSString(JSContextRef context, NSString* nsString)
+{
+    JSRetainPtr<JSStringRef> jsString(Adopt, JSStringCreateWithCFString((CFStringRef)nsString));
+    return JSValueMakeString(context, jsString.get());
+}
+
 @interface NSString (WebScriptStringExtras)
 + (NSString *)stringOrNilFromWebScriptResult:(id)scriptResult;
 @end
