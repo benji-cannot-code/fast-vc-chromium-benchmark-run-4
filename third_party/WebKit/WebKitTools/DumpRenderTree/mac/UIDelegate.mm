ce14c3c265e4a7d67656b36ea362ee09aaeba56a
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-14 22:40:55 +0000

    Rubberstamped by Weinig
            You know, Sam, some of us use case-sensitve filesystems...
    
            * DumpRenderTree/DumpRenderTree.h:
            * DumpRenderTree/mac/UIDelegate.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/UIDelegate.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/UIDelegate.mm
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebFramePrivate.h>
 #import <WebKit/WebHTMLViewPrivate.h>
 #import <WebKit/WebView.h>
-#import <JavascriptCore/Assertions.h>
+#import <JavaScriptCore/Assertions.h>
 
 DumpRenderTreeDraggingInfo *draggingInfo = nil;
 
