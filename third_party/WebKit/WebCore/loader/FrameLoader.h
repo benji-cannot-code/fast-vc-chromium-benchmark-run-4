d58da8fd077a33075ed209b3039a45671e75896d
thatcher <thatcher@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-14 23:53:29 +0000

    WebCore:
            Reviewed by Sam.
    
            <rdar://problem/5472970> REGRESSION (r24276): TinyMCE popups show an empty window with no content
    
            Accessing the document of a window before the load finished would cause the window
            object to hold onto the initial empty document, and never switch over to the real document
            once the load finished. This regression was caused by r24276 which added a check to prevent
            clearing the window object when the load finished. The absence of this clear allowed the
            dialogArguments set with showModalDialog to persist on the window after the load. However,
            not clearing the window would keep other properties (and the empty document object) around.
    
            So the fix is to store away the dialog arguments that were passed to showModalDialog and
            put them back on the window object in the dialogArguments property each time
            the window is cleared.
    
            * bindings/js/kjs_window.cpp:
            (KJS::createWindow): No longer put dialogArguments on the window here.
            (KJS::showModalDialog): Put dialogArguments on the window and call
            setDialogArgumentsAndReturnValueSlot to remember the arguments.
            (KJS::Window::clear): Put m_dialogArguments back on the window as dialogArguments.
            (KJS::WindowFunc::callAsFunction): Call the new setDialogArgumentsAndReturnValue.
            (KJS::Window::setDialogArgumentsAndReturnValue): Store the arguments in m_dialogArguments.
            * bindings/js/kjs_window.h: Rename setReturnValueSlot to setDialogArgumentsAndReturnValueSlot.
            * manual-tests/modal-dialog-arguments.html: Confirmed that this test still passes.
    
            Reverted r24276 which was all the changes in FrameLoader.cpp and FrameLoader.h.
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::FrameLoader): Remove m_shouldClearWindowProperties.
            (WebCore::FrameLoader::createWindow): Remove the call to setShouldClearWindowProperties.
            (WebCore::FrameLoader::clear): No longer check m_shouldClearWindowProperties, clear the
            window whenever clearWindowProperties is set.
            (WebCore::FrameLoader::begin): Remove m_shouldClearWindowProperties.
            (WebCore::FrameLoader::open): Ditto.
            * loader/FrameLoader.h: Remove m_shouldClearWindowProperties.
    
    LayoutTests:
    
            Reviewed by Sam.
    
            <rdar://problem/5472970> REGRESSION (r24276): TinyMCE popups show an empty window with no content
    
            * fast/dom/Document/early-document-access.html: Added.
            * fast/dom/Document/resources: Added.
            * fast/dom/Document/resources/early-document-access-popup.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25576 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/WebCore/loader/FrameLoader.h
@@ -563,8 +563,6 @@ namespace WebCore {
         void dispatchDidFinishLoading(DocumentLoader*, unsigned long identifier);
         bool dispatchDidLoadResourceFromMemoryCache(DocumentLoader*, const ResourceRequest&, const ResourceResponse&, int length);
 
-        void setShouldClearWindowProperties(bool shouldClearWindowProperties) { m_shouldClearWindowProperties = shouldClearWindowProperties; }
-        
         Frame* m_frame;
         FrameLoaderClient* m_client;
 
@@ -617,7 +615,6 @@ namespace WebCore {
         OwnPtr<ScheduledRedirection> m_scheduledRedirection;
 
         bool m_needsClear;
-        bool m_shouldClearWindowProperties;
         bool m_receivedData;
 
         bool m_encodingWasChosenByUser;
