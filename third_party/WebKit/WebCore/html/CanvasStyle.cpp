de80b9728c144a800b590e6fdd5b4951d09fc3f1
staikos <staikos@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-18 20:58:59 +0000

    2007-09-18  Mike Fenton  <mike@staikos.net>
            Reviewed by George Staikos.
    
            Retain the pen properties when changing one aspect of the pen.
    
            * html/CanvasStyle.cpp:
            (WebCore::CanvasStyle::applyStrokeColor):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25621 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/CanvasStyle.cpp
+++ third_party/WebKit/WebCore/html/CanvasStyle.cpp
@@ -101,7 +101,9 @@ void CanvasStyle::applyStrokeColor(GraphicsContext* context)
                 (color & 0xFF) / 255.0f,
                 ((color >> 24) & 0xFF) / 255.0f);
 #elif PLATFORM(QT)
-            p->setPen(QPen(QColor(QRgb(color))));
+            QPen currentPen = p->pen();
+            currentPen.setColor((QColor(QRgb(color))));
+            p->setPen(currentPen);
 #endif
             break;
         }
@@ -115,9 +117,11 @@ void CanvasStyle::applyStrokeColor(GraphicsContext* context)
                 (color & 0xFF) / 255.0f,
                 m_alpha);
 #elif PLATFORM(QT)
+            QPen currentPen = p->pen();
             QColor clr = QColor(QRgb(color));
             clr.setAlphaF(m_alpha);
-            p->setPen(clr);
+            currentPen.setColor(clr);
+            p->setPen(currentPen);
 #endif
             break;
         }
@@ -127,8 +131,10 @@ void CanvasStyle::applyStrokeColor(GraphicsContext* context)
             CGContextSetGrayStrokeColor(context->platformContext(), m_grayLevel, m_alpha);
 #elif PLATFORM(QT)
             QColor clr;
+            QPen currentPen = p->pen();
             clr.setRgbF(m_grayLevel, m_grayLevel, m_grayLevel, m_alpha);
-            p->setPen(clr);
+            currentPen.setColor(clr);
+            p->setPen(currentPen);
 #endif
             break;
         }
