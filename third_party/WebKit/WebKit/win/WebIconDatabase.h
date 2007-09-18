e93144f946cdde0c346dc6c7529b2148a3d7cd78
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-18 23:12:55 +0000

    Reviewed by Ada and Geoff
            Part of <rdar://problem/5471308> - Get async Icon Database fully hooked up on Windows
            
            Hook up main thread delivery of Icon Database notifications from the secondary thread
            
            * WebIconDatabase.cpp:
            (WebIconDatabase::WebIconDatabase): Initialize m_deliveryRequested
            (WebIconDatabase::init): Set the IconDatabaseClient to the shared WebIconDatabase
            (WebIconDatabase::dispatchDidRemoveAllIcons): Queue the notification to be delivered on the main thread then
              ask for the delivery
            (WebIconDatabase::dispatchDidAddIconForPageURL): Ditto
            (WebIconDatabase::scheduleNotificationDelivery): If the m_deliveryRequested hasn't been set, then set it
              and perform the callOnMainThread() for WebIconDatabase::deliverNotifications
            (postDidRemoveAllIconsNotification):
            (postDidAddIconNotification):
            (WebIconDatabase::deliverNotifications): Deliver all notifications in the current queue
            * WebIconDatabase.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25631 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebIconDatabase.h
+++ third_party/WebKit/WebKit/win/WebIconDatabase.h
@@ -30,8 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IWebIconDatabase.h"
 
 #pragma warning(push, 0)
+#include <WebCore/IconDatabaseClient.h>
 #include <WebCore/IntSize.h>
 #include <WebCore/IntSizeHash.h>
+#include <WebCore/Threading.h>
 #pragma warning(pop)
 
 #include <WTF/HashMap.h>
@@ -45,7 +47,7 @@ namespace WebCore
 using namespace WebCore;
 using namespace WTF;
 
-class WebIconDatabase : public IWebIconDatabase
+class WebIconDatabase : public IWebIconDatabase, public WebCore::IconDatabaseClient
 {
 public:
     static WebIconDatabase* createInstance();
@@ -87,6 +89,9 @@ public:
     
     virtual HRESULT STDMETHODCALLTYPE allowDatabaseCleanup( void);
 
+    // IconDatabaseClient
+    virtual void dispatchDidRemoveAllIcons();
+    virtual void dispatchDidAddIconForPageURL(const WebCore::String&);
 protected:
     ULONG m_refCount;
     static WebIconDatabase* m_sharedWebIconDatabase;
@@ -97,6 +102,13 @@ protected:
     HBITMAP getOrCreateDefaultIconBitmap(LPSIZE size);
     HashMap<IntSize, HBITMAP> m_defaultIconMap;
     HashMap<IntSize, HBITMAP> m_sharedIconMap;
+
+    Mutex m_notificationMutex;
+    Vector<String> m_notificationQueue;
+    void scheduleNotificationDelivery();
+    bool m_deliveryRequested;
+
+    static void deliverNotifications();
 };
 
 #endif
