1e8fa986b2a7415a2f61c35999c102e282c18b1e
beidson <beidson@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-20 23:13:20 +0000

    WebCore:         Reviewed by Dave Hyatt
            <rdar://problem/5245981> - No favicon shows up for cnet.com
            
            CNet's favicon has two images in it - a 32x32 white square, and a 16x16 image that is their actual icon
            Till now on Windows we've always gone straight for the first image in an icon for the favicon, now we
            actually do proper size matching
    
            * platform/graphics/BitmapImage.h:
    
            * platform/graphics/Image.h:
            (WebCore::Image::getHBITMAPOfSize): 
            (WebCore::Image::drawFrameMatchingSourceSize): Search through the frames of the image for the correct size
    
            * platform/graphics/win/ImageWin.cpp:
            (WebCore::BitmapImage::getHBITMAP):
            (WebCore::BitmapImage::getHBITMAPOfSize):
            (WebCore::BitmapImage::drawFrameMatchingSourceSize):
    
    WebKit/win:
            Reviewed by Dave Hyatt
    
            <rdar://problem/5245981> - No favicon shows up for cnet.com
            
            * WebIconDatabase.cpp:
            (WebIconDatabase::iconForURL): Call getHBITMAPOfSize
            (WebIconDatabase::getOrCreateDefaultIconBitmap): Ditto
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebIconDatabase.cpp
+++ third_party/WebKit/WebKit/win/WebIconDatabase.cpp
@@ -187,7 +187,7 @@ HRESULT STDMETHODCALLTYPE WebIconDatabase::iconForURL(
     // Make sure we check for the case of an "empty image"
     if (icon && icon->width()) {
         *bitmap = (OLE_HANDLE)(ULONG64)getOrCreateSharedBitmap(size);
-        if (!icon->getHBITMAP((HBITMAP)(ULONG64)*bitmap)) {
+        if (!icon->getHBITMAPOfSize((HBITMAP)(ULONG64)*bitmap, size)) {
             LOG_ERROR("Failed to draw Image to HBITMAP");
             *bitmap = 0;
             return E_FAIL;
@@ -279,7 +279,7 @@ HBITMAP WebIconDatabase::getOrCreateDefaultIconBitmap(LPSIZE size)
         defaultIconImage = Image::loadPlatformResource("urlIcon");
     }
     m_defaultIconMap.set(*size, result);
-    if (!defaultIconImage->getHBITMAP(result)) {
+    if (!defaultIconImage->getHBITMAPOfSize(result, size)) {
         LOG_ERROR("Failed to draw Image to HBITMAP");
         return 0;
     }
