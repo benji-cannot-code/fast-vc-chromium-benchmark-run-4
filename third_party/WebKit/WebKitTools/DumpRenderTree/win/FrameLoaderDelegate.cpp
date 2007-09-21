cd706508708b130cf34b6190e33f24d93a7b2db3
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-21 20:12:26 +0000

    Reviewed by Oliver.
            - We need to initialize the ref count to 1 at initialization so that objects are not garbage collected. (actual patch written by Oliver)
    
            * DumpRenderTree/win/FrameLoaderDelegate.cpp:
            (FrameLoadDelegate::FrameLoadDelegate):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/win/FrameLoaderDelegate.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/win/FrameLoaderDelegate.cpp
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static FrameLoadDelegate* g_delegateWaitingOnTimer;
 
 FrameLoadDelegate::FrameLoadDelegate()
-    : m_refCount(0)
+    : m_refCount(1)
     , m_gcController(new GCController)
 {
 }
