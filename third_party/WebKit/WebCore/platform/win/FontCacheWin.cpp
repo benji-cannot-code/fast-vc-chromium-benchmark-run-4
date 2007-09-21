c769a28fe501eae263202586d6e519521145d50e
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-21 22:47:35 +0000

    Reviewed by Anders.
            - add QueryInterface capabilities to COMPtr
    
            * platform/win/COMPtr.h:
            (COMPtr::COMPtr): Added constructor that takes a Query tag and
            does an appropriate QueryInterface.
            (COMPtr::query): Added function to do a queryInterface.
            (COMPtr::copyQueryInterfaceRef): Added private helper used by
            both of the above. Note that when the query fails you get a 0,
            which is the same thing you get if a 0 pointer is passed in.
    
            * platform/win/FontCacheWin.cpp:
            (WebCore::FontCache::getFontLinkInterface): Convert to using
            the new query as a test case. Also eliminate the unnecessary
            second global variable and use COMPtr objects rather than just
            leaking references.
    
            * platform/win/WCDataObject.h: Removed unneeded include and
            using statements.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/FontCacheWin.cpp
+++ third_party/WebKit/WebCore/platform/win/FontCacheWin.cpp
@@ -28,19 +28,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include <winsock2.h>
 #include "FontCache.h"
+
+#include <winsock2.h>
 #include "FontData.h"
 #include "Font.h"
 #include <windows.h>
 #include <mlang.h>
 #include <ApplicationServices/ApplicationServices.h>
 #include <WebKitSystemInterface/WebKitSystemInterface.h>
+#include "COMPtr.h"
 
 using std::min;
 
-namespace WebCore
-{
+namespace WebCore {
 
 void FontCache::platformInit()
 {
@@ -49,19 +50,15 @@ void FontCache::platformInit()
 
 IMLangFontLink2* FontCache::getFontLinkInterface()
 {
-    static IMultiLanguage *multiLanguage;
-    if (!multiLanguage) {
+    static COMPtr<IMLangFontLink2> langFontLink;
+    if (!langFontLink) {
+        COMPtr<IMultiLanguage> multiLanguage;
         if (CoCreateInstance(CLSID_CMultiLanguage, 0, CLSCTX_ALL, IID_IMultiLanguage, (void**)&multiLanguage) != S_OK)
             return 0;
+        langFontLink.query(multiLanguage);
     }
 
-    static IMLangFontLink2* langFontLink;
-    if (!langFontLink) {
-        if (multiLanguage->QueryInterface(IID_IMLangFontLink2, (void**)&langFontLink) != S_OK)
-            return 0;
-    }
-
-    return langFontLink;
+    return langFontLink.get();
 }
 
 const FontData* FontCache::getFontDataForCharacters(const Font& font, const UChar* characters, int length)
--- third_party/WebKit/WebCore/platform/win/WCDataObject.h
+++ third_party/WebKit/WebCore/platform/win/WCDataObject.h
@@ -27,11 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WCDataObject_h
 #define WCDataObject_h
 
-#include "COMPtr.h"
 #include <wtf/Vector.h>
-
-using namespace std;
-
 #include <ShlObj.h>
 #include <objidl.h>
 
