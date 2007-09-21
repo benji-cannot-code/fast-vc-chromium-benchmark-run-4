a6e00462921c0c3490e5acbb9eda9d795d468577
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-21 07:02:55 +0000

    2007-09-20  Oliver Hunt  <oliver@apple.com>
            Reviewed by Steve.
    
            <rdar://problem/5487153> Korean characters are displayed as garbage with default encoding in some Korean web sites
    
            Adding support for per-localisation default character encodings on
            windows.
    
            * English.lproj/Localizable.strings:
            * WebPreferences.cpp:
            (WebPreferences::initializeDefaultSettings):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25680 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebPreferences.cpp
+++ third_party/WebKit/WebKit/win/WebPreferences.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/Font.h>
 #include <WebCore/PlatformString.h>
 #include <WebCore/StringHash.h>
+#include "WebLocalizableStrings.h"
 #pragma warning( pop )
 
 #include <CoreFoundation/CoreFoundation.h>
@@ -184,7 +185,8 @@ void WebPreferences::initializeDefaultSettings()
     CFDictionaryAddValue(defaults, CFSTR(WebKitMinimumLogicalFontSizePreferenceKey), CFSTR("9"));
     CFDictionaryAddValue(defaults, CFSTR(WebKitDefaultFontSizePreferenceKey), CFSTR("16"));
     CFDictionaryAddValue(defaults, CFSTR(WebKitDefaultFixedFontSizePreferenceKey), CFSTR("13"));
-    CFDictionaryAddValue(defaults, CFSTR(WebKitDefaultTextEncodingNamePreferenceKey), CFSTR("ISO-8859-1"));
+    WebCore::String defaultDefaultEncoding(LPCTSTR_UI_STRING("ISO-8859-1", "The default, default character encoding"));
+    CFDictionaryAddValue(defaults, CFSTR(WebKitDefaultTextEncodingNamePreferenceKey), defaultDefaultEncoding.createCFString());
 
     RetainPtr<CFStringRef> pageCacheSizeString(AdoptCF, CFStringCreateWithFormat(0, 0, CFSTR("%d"), pageCacheSize));
     CFDictionaryAddValue(defaults, CFSTR(WebKitPageCacheSizePreferenceKey), pageCacheSizeString.get());
