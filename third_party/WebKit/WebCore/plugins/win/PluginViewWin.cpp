f944057914218b1b388030fec9ca3863ca51f01a
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-09-27 01:24:47 +0000

    - fix Windows build
            * platform/win/ClipboardWin.cpp:
            (WebCore::writeURL): Add a check for empty URL. Remove the check for invalid URL.
            (WebCore::ClipboardWin::setData): Remove the check for invalid URL.
            * plugins/win/PluginViewWin.cpp:
            (WebCore::PluginViewWin::start): Check for empty URL instead of invalid URL.
            (WebCore::PluginViewWin::load): Same here.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25770 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
+++ third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
@@ -640,7 +640,7 @@ bool PluginViewWin::start()
 
     m_isStarted = true;
 
-    if (m_url.isValid() && !m_loadManually) {
+    if (!m_url.isEmpty() && !m_loadManually) {
         FrameLoadRequest frameLoadRequest;
         frameLoadRequest.resourceRequest().setHTTPMethod("GET");
         frameLoadRequest.resourceRequest().setURL(m_url);
@@ -799,7 +799,7 @@ NPError PluginViewWin::load(const FrameLoadRequest& frameLoadRequest, bool sendN
 
     KURL url = frameLoadRequest.resourceRequest().url();
     
-    if (!url.isValid())
+    if (url.isEmpty())
         return NPERR_INVALID_URL;
 
     String target = frameLoadRequest.frameName();
