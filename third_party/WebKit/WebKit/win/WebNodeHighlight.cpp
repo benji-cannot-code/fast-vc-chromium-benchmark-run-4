0db40c346bf408c92d85bda59bf2a93aa3c917ea
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-02 20:49:13 +0000

    JavaScriptCore:
            Reviewed by Adam.
    
            - add support for GDI objects to OwnPtr; I plan to use this
              to fix some GDI handle leaks
    
            * kjs/grammar.y: Change parser to avoid macros that conflict
            with macros defined in Windows system headers: THIS, DELETE,
            VOID, IN, and CONST. This is needed because OwnPtr.h will now
            include <windows.h>.
            * kjs/keywords.table: Ditto.
    
            * wtf/OwnPtr.h: For PLATFORM(WIN), add support so that OwnPtr can be
            a GDI handle, and it will call DeleteObject. Also change to use the
            RemovePointer technique used by RetainPtr, so you can say OwnPtr<HBITMAP>
            rather than having to pass in the type pointed to by HBITMAP.
    
            * wtf/OwnPtrWin.cpp: Added.
            (WebCore::deleteOwnedPtr): Put this in a separate file so that we
            don't have to include <windows.h> in OwnPtr.h.
    
            * JavaScriptCore.vcproj/WTF/WTF.vcproj: Added OwnPtrWin.cpp.
    
    WebCore:
    
            Reviewed by Adam.
    
            - add support for GDI objects to OwnPtr; I plan to use this
              to fix some GDI handle leaks
    
            * css/CSSGrammar.y: Change parser to avoid macro that conflicts
            with macro defined in Windows system headers: FLOAT. This is needed
            because OwnPtr.h will now include <windows.h>.
            * css/CSSParser.cpp: (WebCore::CSSParser::lex): Ditto.
            * css/tokenizer.flex: Ditto.
    
            * platform/win/CursorWin.cpp: (WebCore::Cursor::Cursor):
            Use OwnPtr for a few HBITMAP objects as a test case.
    
            * platform/win/COMPtr.h: (COMPtr::COMPtr): Fix so that we can use
            Query with other COMPtr. Before this would not compile if it was
            ever instantiated.
    
    win:
    
            Reviewed by Adam.
    
            - started using the new OwnPtr everywhere we do DeleteObject
    
            * WebNodeHighlight.cpp:
            (WebNodeHighlight::updateWindow):
            * WebView.cpp:
            (WebView::WebView):
            (WebView::deleteBackingStore):
            (WebView::ensureBackingStore):
            (WebView::addToDirtyRegion):
            (WebView::scrollBackingStore):
            (WebView::updateBackingStore):
            (WebView::paint):
            (WebView::paintIntoBackingStore):
            (WebView::paintIntoWindow):
            * WebView.h:
    
    Also set svn:eol-style on all .vcproj to CRLF.
    
    Also added svn:ignore of *.user for WebKitInitializer.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@25934 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebNodeHighlight.cpp
+++ third_party/WebKit/WebKit/win/WebNodeHighlight.cpp
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebNodeHighlight.h"
 
+#include <wtf/OwnPtr.h>
+
 #pragma warning(push, 0)
 #include <WebCore/Color.h>
 #include <WebCore/GraphicsContext.h>
@@ -122,14 +124,14 @@ void WebNodeHighlight::updateWindow()
     bitmapInfo.bmiHeader.biClrImportant  = 0;
 
     void* pixels = 0;
-    HBITMAP hbmp = ::CreateDIBSection(hdc, &bitmapInfo, DIB_RGB_COLORS, &pixels, 0, 0);
+    OwnPtr<HBITMAP> hbmp(::CreateDIBSection(hdc, &bitmapInfo, DIB_RGB_COLORS, &pixels, 0, 0));
     if (!hbmp) {
         DWORD error = ::GetLastError();
         error++;
         return;
     }
 
-    ::SelectObject(hdc, hbmp);
+    ::SelectObject(hdc, hbmp.get());
 
     GraphicsContext context(hdc);
 
@@ -159,7 +161,6 @@ void WebNodeHighlight::updateWindow()
 
     ::UpdateLayeredWindow(m_overlay, ::GetDC(0), &dstPoint, &size, hdc, &srcPoint, 0, &bf, ULW_ALPHA);
 
-    ::DeleteObject(hbmp);
     ::DeleteDC(hdc);
 }
 
