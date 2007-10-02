567db20efdb8d8e9e5b2142e1a915d0a26e92331
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-02 08:46:50 +0000

    The cookies() method should only return the value of the         cookie, not additional info such as the expiration date.         Fixes LayoutTests/fast/cookies/local-file-can-set-cookies.html
    git-svn-id: svn://svn.chromium.org/blink/trunk@25913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/CookieJarQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/CookieJarQt.cpp
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "config.h"
@@ -47,7 +47,11 @@ void setCookies(const KURL& url, const KURL& policyURL, const String& value)
 String cookies(const KURL& url)
 {
     QUrl u((QString)url.url());
-    return (String)QCookieJar::cookieJar()->cookies(u);
+    QString cookies = QCookieJar::cookieJar()->cookies(u);
+    int idx = cookies.indexOf(QLatin1Char(';'));
+    if (idx > 0)
+        cookies = cookies.left(idx);
+    return cookies;
 }
 
 bool cookiesEnabled()
