b3964a73a8ebd1f2f78794f51dd4758896b27d39
bdakin <bdakin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-05 20:04:49 +0000

    WebCore:
            Reviewed by John Sullivan.
    
            Fix for <rdar://problem/5441823> REGRESSION (r25142, Tiger only): 
            Vertical scroll bar not redrawn properly when going back in history 
            (15033)
    
            This fix if-defs r25142 to be Leopard-only since it causes 
            correctness issues on Tiger and does not seem to have any 
            performance impact on Tiger either.
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::transitionToCommitted):
    
    WebKit:
    
            Reviewed by John Sullivan.
    
            Fix for <rdar://problem/5441823> REGRESSION (r25142, Tiger only): 
            Vertical scroll bar not redrawn properly when going back in history 
            (15033)
    
            This fix if-defs r25142 to be Leopard-only since it causes 
            correctness issues on Tiger and does not seem to have any 
            performance impact on Tiger either.
    
            * WebView/WebDynamicScrollBarsView.m:
            (-[WebDynamicScrollBarsView setScrollBarsSuppressed:repaintOnUnsuppress:]):
            (-[WebDynamicScrollBarsView reflectScrolledClipView:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26070 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebView/WebDynamicScrollBarsView.m
+++ third_party/WebKit/WebKit/WebView/WebDynamicScrollBarsView.m
@@ -42,6 +42,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setScrollBarsSuppressed:(BOOL)suppressed repaintOnUnsuppress:(BOOL)repaint
 {
     suppressScrollers = suppressed;
+
+    // This code was originally changes for a Leopard performance imporvement. We decided to 
+    // ifdef it to fix correctness issues on Tiger documented in <rdar://problem/5441823>.
+#ifndef BUILDING_ON_TIGER
     if (suppressed) {
         [[self verticalScroller] setNeedsDisplay:NO];
         [[self horizontalScroller] setNeedsDisplay:NO];
@@ -49,6 +53,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         
     if (!suppressed && repaint)
         [super reflectScrolledClipView:[self contentView]];
+#else
+    if (suppressed || repaint) { 
+        [[self verticalScroller] setNeedsDisplay: !suppressed]; 
+        [[self horizontalScroller] setNeedsDisplay: !suppressed]; 
+    }
+#endif
 }
 
 - (void)updateScrollers
@@ -136,9 +146,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             [self updateScrollers];
     }
 
+    // This code was originally changed for a Leopard performance imporvement. We decided to 
+    // ifdef it to fix correctness issues on Tiger documented in <rdar://problem/5441823>.
+#ifndef BUILDING_ON_TIGER
     // Update the scrollers if they're not being suppressed.
     if (!suppressScrollers)
         [super reflectScrolledClipView:clipView];
+#else
+    [super reflectScrolledClipView:clipView]; 
+  
+    // Validate the scrollers if they're being suppressed. 
+    if (suppressScrollers) { 
+        [[self verticalScroller] setNeedsDisplay: NO]; 
+        [[self horizontalScroller] setNeedsDisplay: NO]; 
+    }
+#endif
 }
 
 - (void)setAllowsScrolling:(BOOL)flag
