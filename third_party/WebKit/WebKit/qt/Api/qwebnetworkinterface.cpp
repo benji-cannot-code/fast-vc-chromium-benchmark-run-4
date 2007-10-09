93a3b3dff0c9591c942dee63466bb1ab61287f1e
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-09 13:58:22 +0000

    Don't return a 404 status code for empty data: urls. Fixes fast/dom/HTMLHeadElement/head-check.html
    git-svn-id: svn://svn.chromium.org/blink/trunk@26165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebnetworkinterface.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebnetworkinterface.cpp
@@ -398,7 +398,7 @@ bool QWebNetworkManager::add(ResourceHandle *handle, QWebNetworkInterface *inter
         qWarning("REQUEST: [%s]\n", qPrintable(job->d->request.httpHeader.toString()));
     }
 
-    //DEBUG() << "QWebNetworkManager::add:" <<  job->d->request.httpHeader.toString();
+    DEBUG() << "QWebNetworkManager::add:" <<  job->d->request.httpHeader.toString();
 
     interface->addJob(job);
 
@@ -410,7 +410,7 @@ void QWebNetworkManager::cancel(ResourceHandle *handle)
     QWebNetworkJob *job = handle->getInternal()->m_job;
     if (!job)
         return;
-    //DEBUG() << "QWebNetworkManager::cancel:" <<  job->d->request.httpHeader.toString();
+    DEBUG() << "QWebNetworkManager::cancel:" <<  job->d->request.httpHeader.toString();
     job->d->resourceHandle = 0;
     job->d->connector = 0;
     job->d->interface->cancelJob(job);
@@ -428,8 +428,8 @@ void QWebNetworkManager::started(QWebNetworkJob *job)
         return;
     }
 
-    //DEBUG() << "ResourceHandleManager::receivedResponse:";
-    //DEBUG() << job->d->response.toString();
+    DEBUG() << "ResourceHandleManager::receivedResponse:";
+    DEBUG() << job->d->response.toString();
 
     QStringList cookies = job->d->response.allValues("Set-Cookie");
     KURL url(job->url().toString());
@@ -514,7 +514,7 @@ void QWebNetworkManager::data(QWebNetworkJob *job, const QByteArray &data)
     if (job->d->redirected)
         return; // don't emit the "Document has moved here" type of HTML
 
-    //DEBUG() << "receivedData" << job->d->request.url.path();
+    DEBUG() << "receivedData" << job->d->request.url.path();
     if (client)
         client->didReceiveData(job->d->resourceHandle, data.constData(), data.length(), data.length() /*FixMe*/);
     if (job->d->connector)
@@ -646,7 +646,7 @@ void QWebNetworkInterfacePrivate::parseDataUrl(QWebNetworkJob* job)
 
     if (header.isEmpty())
         header = "text/plain;charset=US-ASCII";
-    int statusCode = data.isEmpty() ? 404 : 200;
+    int statusCode = 200;
     QHttpResponseHeader response;
     response.setContentType(header);
     response.setContentLength(data.size());
