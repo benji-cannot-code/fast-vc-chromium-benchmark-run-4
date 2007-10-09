af6643888321537d74335653f152d39d3aeb77cc
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-09 13:57:39 +0000

    add a clear() method to QWebPageHistory.
    git-svn-id: svn://svn.chromium.org/blink/trunk@26158 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpagehistory.h
+++ third_party/WebKit/WebKit/qt/Api/qwebpagehistory.h
@@ -125,6 +125,8 @@ public:
     QWebPageHistory(const QWebPageHistory &other);
     QWebPageHistory &operator=(const QWebPageHistory &other);
     ~QWebPageHistory();
+
+    void clear();
     
     QList<QWebHistoryItem> items() const;
     QList<QWebHistoryItem> backItems(int maxItems) const;
