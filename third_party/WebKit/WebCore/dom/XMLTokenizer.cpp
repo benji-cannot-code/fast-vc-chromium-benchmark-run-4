30828feb2a6249caabf2c6e33324ba7b68544f2c
lars <lars@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-11 09:30:54 +0000

    Ensure that setting innerHTML on an element doesn't do weird things with the document. Fixes most issues with http://bugs.webkit.org/show_bug.cgi?id=15456
    git-svn-id: svn://svn.chromium.org/blink/trunk@26356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/XMLTokenizer.cpp
+++ third_party/WebKit/WebCore/dom/XMLTokenizer.cpp
@@ -599,10 +599,12 @@ bool XMLTokenizer::write(const SegmentedString& s, bool /*appendData*/)
                 String version = attrs.get("version");
                 String encoding = attrs.get("encoding");
                 ExceptionCode ec = 0;
-                if (!version.isEmpty())
-                    m_doc->setXMLVersion(version, ec);
-                if (!encoding.isEmpty())
-                    m_doc->setXMLEncoding(encoding);
+                if (!m_parsingFragment) {
+                    if (!version.isEmpty())
+                        m_doc->setXMLVersion(version, ec);
+                    if (!encoding.isEmpty())
+                        m_doc->setXMLEncoding(encoding);
+                }
             }
         }
         m_stream.addData(data);
@@ -928,7 +930,7 @@ void XMLTokenizer::comment(const xmlChar* s)
 void XMLTokenizer::startDocument(const xmlChar* version, const xmlChar* encoding, int standalone)
 {
     ExceptionCode ec = 0;
-    
+
     if (version)
         m_doc->setXMLVersion(toString(version), ec);
     m_doc->setXMLStandalone(standalone == 1, ec); // possible values are 0, 1, and -1
@@ -1242,7 +1244,8 @@ void XMLTokenizer::end()
     }
     
     setCurrentNode(0);
-    m_doc->finishedParsing();    
+    if (!m_parsingFragment)
+        m_doc->finishedParsing();    
 }
 
 void XMLTokenizer::finish()
@@ -1716,7 +1719,8 @@ void XMLTokenizer::startDocument()
     initializeParserContext();
     ExceptionCode ec = 0;
 
-    m_doc->setXMLStandalone(m_stream.isStandaloneDocument(), ec);
+    if (!m_parsingFragment)
+        m_doc->setXMLStandalone(m_stream.isStandaloneDocument(), ec);
 }
 
 void XMLTokenizer::parseStartElement()
@@ -1978,7 +1982,8 @@ void XMLTokenizer::parseDtd()
         || (publicId == "-//WAPFORUM//DTD XHTML Mobile 1.0//EN")) {
         setIsXHTMLDocument(true); // controls if we replace entities or not.
     }
-    m_doc->setDocType(new DocumentType(m_doc, name, publicId, systemId));
+    if (!m_parsingFragment)
+        m_doc->setDocType(new DocumentType(m_doc, name, publicId, systemId));
     
 }
 #endif
