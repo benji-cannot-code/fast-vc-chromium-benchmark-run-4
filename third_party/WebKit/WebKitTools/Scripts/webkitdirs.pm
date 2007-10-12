b41e8e9a49fa8e55587343d9203a0853e30495d0
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-12 15:33:50 +0000

    HackTop [26957:WebKitTools]% less ChangeLog                           [/Stuff/Projects/WebKit/WebKitTools] 2007-10-08  Eric Seidel  <eric@webkit.org>
            Reviewed by Mark Rowe.
    
            * Scripts/build-webkit: add --help, remove unused --color
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -629,9 +629,9 @@ sub qtMakeCommand()
     return "make";
 }
 
-sub buildQMakeProject($$)
+sub buildQMakeProject($)
 {
-    my ($project, $colorize) = @_;
+    my ($project) = @_;
 
     my @buildArgs = ("-r");
     my $make = qtMakeCommand();
@@ -694,9 +694,9 @@ sub buildQMakeProject($$)
     return $result;
 }
 
-sub buildQMakeGtkProject($$)
+sub buildQMakeGtkProject($)
 {
-    my ($project, $colorize) = @_;
+    my ($project) = @_;
 
     if ($project ne "WebKit") {
         die "The Gtk portbuilds JavaScriptCore/WebCore/WebKitQt in one shot! Only call it for 'WebKit'.\n";
