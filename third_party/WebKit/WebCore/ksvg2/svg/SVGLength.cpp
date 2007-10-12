e9ce05de7b8319a52011975dca7f6915f2be2611
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-12 13:28:04 +0000

    Reviewed by Oliver.
            Do not export some symbols for internal functions, saves some bytes.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26433 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGLength.cpp
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGLength.cpp
@@ -43,25 +43,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 // Helper functions
-inline unsigned int storeUnit(SVGLengthMode mode, SVGLengthType type)
+static inline unsigned int storeUnit(SVGLengthMode mode, SVGLengthType type)
 {
     return (mode << 4) | type;
 }
 
-inline SVGLengthMode extractMode(unsigned int unit)
+static inline SVGLengthMode extractMode(unsigned int unit)
 {
     unsigned int mode = unit >> 4;    
     return static_cast<SVGLengthMode>(mode);
 }
 
-inline SVGLengthType extractType(unsigned int unit)
+static inline SVGLengthType extractType(unsigned int unit)
 {
     unsigned int mode = unit >> 4;
     unsigned int type = unit ^ (mode << 4);
     return static_cast<SVGLengthType>(type);
 }
 
-inline String lengthTypeToString(SVGLengthType type)
+static inline String lengthTypeToString(SVGLengthType type)
 {
     switch (type) {
     case LengthTypeUnknown:
