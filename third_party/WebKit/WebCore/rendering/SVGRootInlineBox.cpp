2222af90c99b1ed5d489a23eba7c597fb723b58d
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-12 23:03:30 +0000

    Reviewed by Oliver Hunt.
            - fix float/int conversions that cause warnings on older versions
              of gcc (like the version currently being used on the PowerPC
              buildbot slave)
    
            * rendering/RenderPath.cpp:
            (WebCore::RenderPath::paint): Explicitly cast to int.
            (WebCore::RenderPath::addFocusRingRects): Use enclosingIntRect
            to convert a FloatRect to an IntRect.
            * rendering/SVGRootInlineBox.cpp:
            (WebCore::SVGRootInlineBoxPaintWalker::chunkPortionCallback):
            Use 0 for the initial value of an int, rather than 0.0f.
            (WebCore::svgTextStyleForInlineTextBox): Explicitly cast to int.
            (WebCore::applyTextAnchorToTextChunk): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26554 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/SVGRootInlineBox.cpp
+++ third_party/WebKit/WebCore/rendering/SVGRootInlineBox.cpp
@@ -267,7 +267,7 @@ struct SVGRootInlineBoxPaintWalker {
 
         int textDecorations = styleToUse->textDecorationsInEffect(); 
 
-        int textWidth = 0.0f;
+        int textWidth = 0;
         IntPoint decorationOrigin;
         SVGTextDecorationInfo info;
 
@@ -417,7 +417,7 @@ TextStyle svgTextStyleForInlineTextBox(RenderStyle* style, const InlineTextBox* 
     ASSERT(textBox);
     ASSERT(style);
 
-    return TextStyle(false, xPos, textBox->toAdd(), textBox->m_reversed, textBox->m_dirOverride || style->visuallyOrdered());
+    return TextStyle(false, static_cast<int>(xPos), textBox->toAdd(), textBox->m_reversed, textBox->m_dirOverride || style->visuallyOrdered());
 }
 
 static float cummulatedWidthOrHeightOfTextChunk(SVGTextChunk& chunk, bool calcWidthOnly)
@@ -541,9 +541,9 @@ static void applyTextAnchorToTextChunk(SVGTextChunk& chunk)
 
         // Move target box
         if (chunk.isVerticalText)
-            curBox->setYPos(curBox->yPos() + shift);
+            curBox->setYPos(curBox->yPos() + static_cast<int>(shift));
         else
-            curBox->setXPos(curBox->xPos() + shift);
+            curBox->setXPos(curBox->xPos() + static_cast<int>(shift));
     }
 }
 
