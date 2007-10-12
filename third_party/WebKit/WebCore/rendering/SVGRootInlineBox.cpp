37ff67a3634b157dff06806d2b450447a2f012be
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-12 12:22:35 +0000

    2007-06-15  Eric Seidel  <eric@webkit.org>
            Reviewed by Niko.
    
            Fix the release build.
    
            * rendering/SVGRootInlineBox.cpp:
            (WebCore::applyTextAnchorToTextChunk):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/SVGRootInlineBox.cpp
+++ third_party/WebKit/WebCore/rendering/SVGRootInlineBox.cpp
@@ -313,7 +313,6 @@ static void applyTextAnchorToTextChunk(SVGTextChunk& chunk)
 
         InlineBox* curBox = range.box;
         ASSERT(curBox->isInlineTextBox());
-
         ASSERT(curBox->parent() && (curBox->parent()->isRootInlineBox() || curBox->parent()->isInlineFlowBox()));
 
         // Move target box
