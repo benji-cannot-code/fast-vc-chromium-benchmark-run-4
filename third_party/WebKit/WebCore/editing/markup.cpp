8ecc3aadf5da94845a7f64821e903a1e528565a7
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-12 14:35:39 +0000

    Reviewed by Eric.
            - fixed "innerHTML updates layout every time (very bad if adding to it in a loop)"
            http://bugs.webkit.org/show_bug.cgi?id=15326
    
            * editing/markup.cpp:
            (WebCore::createMarkup): The Node version of this function currently never uses
            the render tree, so don't bother to update layout. This is the version used for
            innerHTML.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/markup.cpp
+++ third_party/WebKit/WebCore/editing/markup.cpp
@@ -730,7 +730,6 @@ DeprecatedString createMarkup(const Node* node, EChildrenOnly includeChildren,
         deleteButton->disable();
     }
 
-    document->updateLayoutIgnorePendingStylesheets();
     DeprecatedString result(markup(const_cast<Node*>(node), includeChildren, nodes));
 
     if (deleteButton)
