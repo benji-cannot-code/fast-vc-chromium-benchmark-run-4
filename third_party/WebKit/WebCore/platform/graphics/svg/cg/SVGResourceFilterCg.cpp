aad4c20d0a7ace8bfc65d3115f2548d9cb85e470
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-13 21:41:15 +0000

    Fixing a few testcases by enabling SVG_EXPERIMENTAL on windows, and making the required fixes and corrections.
    Reviewed by Maciej
    
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.vcproj/build-generated-files.sh:
            * bindings/scripts/CodeGeneratorJS.pm:
            * ksvg2/scripts/make_names.pl:
            * ksvg2/svg/SVGColor.cpp:
            (WebCore::SVGColor::colorFromRGBColorString):
            * page/DOMWindow.idl:
            * platform/graphics/svg/cg/SVGResourceFilterCg.cpp:
            * platform/win/WebCoreTextRenderer.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26569 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/svg/cg/SVGResourceFilterCg.cpp
+++ third_party/WebKit/WebCore/platform/graphics/svg/cg/SVGResourceFilterCg.cpp
@@ -52,8 +52,7 @@ void SVGResourceFilter::applyFilter(GraphicsContext*&, const FloatRect&)
     notImplemented();
 }
 
-
-SVGFilterEffect* SVGResourceFilter::createFilterEffect(const SVGFilterEffectType&)
+SVGFilterEffect* SVGResourceFilter::createFilterEffect(const SVGFilterEffectType&, SVGResourceFilter*)
 {
     return NULL;
 }
