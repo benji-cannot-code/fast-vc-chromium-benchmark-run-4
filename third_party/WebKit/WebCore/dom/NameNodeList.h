618d464ad57215af3455cae5cbf52922da7026ba
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-14 11:39:54 +0000

    2007-10-14  Eric Seidel  <eric@webkit.org>
            Reviewed by Maciej.
    
            REGRESSION: Javascript bug in getElementsByName
            http://bugs.webkit.org/show_bug.cgi?id=15274
    
            Test: fast/dom/getelementbyname-invalidation.html
    
            * dom/NameNodeList.h: remove empty rootNodeChildrenChanged override
    
    2007-10-14  Eric Seidel  <eric@webkit.org>
    
            Reviewed by Maciej.
    
            Test for http://bugs.webkit.org/show_bug.cgi?id=15274
            REGRESSION: Javascript bug in getElementsByName
    
            * fast/dom/getelementbyname-invalidation-expected.txt: Added.
            * fast/dom/getelementbyname-invalidation.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26585 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/NameNodeList.h
+++ third_party/WebKit/WebCore/dom/NameNodeList.h
@@ -44,7 +44,6 @@ public:
     virtual Node* item(unsigned index) const;
 
     // Other methods (not part of DOM)
-    virtual void rootNodeChildrenChanged() { }
     virtual void rootNodeAttributeChanged() { NodeList::rootNodeChildrenChanged(); }
 
 protected:
