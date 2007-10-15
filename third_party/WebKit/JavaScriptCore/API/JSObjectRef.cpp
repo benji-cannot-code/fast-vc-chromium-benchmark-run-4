88eac0ea8c9eec7afae958766002f0174b82ee23
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-15 20:58:28 +0000

    Removed unnecessary #include.
            * API/JSObjectRef.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26625 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSObjectRef.cpp
+++ third_party/WebKit/JavaScriptCore/API/JSObjectRef.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "identifier.h"
 #include "function.h"
-#include "nodes.h"
 #include "internal.h"
 #include "object.h"
 #include "PropertyNameArray.h"
