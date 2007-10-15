fd263bb30ead8da7d7b0edd5bf7fd42b5795e115
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-15 21:59:48 +0000

    * platform/mac/FontDataMac.mm:         D'oh! Added missing #import to fix build
    git-svn-id: svn://svn.chromium.org/blink/trunk@26630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/FontDataMac.mm
+++ third_party/WebKit/WebCore/platform/mac/FontDataMac.mm
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "config.h"
 #import "FontData.h"
 
+#import "BlockExceptions.h"
 #import "Color.h"
 #import "FloatRect.h"
 #import "Font.h"
