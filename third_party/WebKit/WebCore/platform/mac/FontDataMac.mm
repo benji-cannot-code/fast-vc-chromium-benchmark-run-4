24a91265e230f3435abeb6e535cb0ee873edaf11
sullivan <sullivan@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-15 21:41:36 +0000

    Reviewed by Geoff
            * platform/mac/FontDataMac.mm:
            (WebCore::FontData::smallCapsFontData):
            replace NS_DURING/NS_HANDLER with WebCore-style BEGIN/END_BLOCK_OBJC_EXCEPTIONS
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26628 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/FontDataMac.mm
+++ third_party/WebKit/WebCore/platform/mac/FontDataMac.mm
@@ -224,7 +224,7 @@ FontData* FontData::smallCapsFontData(const FontDescription& fontDescription) co
             smallCapsFontData.m_size = smallCapsFontData.m_size * smallCapsFontSizeMultiplier;
             m_smallCapsFontData = new FontData(smallCapsFontData, true, false);
         } else {
-            NS_DURING
+            BEGIN_BLOCK_OBJC_EXCEPTIONS;
             float size = [m_font.font() pointSize] * smallCapsFontSizeMultiplier;
             FontPlatformData smallCapsFont([[NSFontManager sharedFontManager] convertFont:m_font.font() toSize:size]);
             
@@ -247,10 +247,7 @@ FontData* FontData::smallCapsFontData(const FontDescription& fontDescription) co
 
                 m_smallCapsFontData = FontCache::getCachedFontData(&smallCapsFont);
             }
-
-            NS_HANDLER
-            NSLog(@"uncaught exception selecting font for small caps: %@", localException);
-            NS_ENDHANDLER
+            END_BLOCK_OBJC_EXCEPTIONS;
         }
     }
     return m_smallCapsFontData;
