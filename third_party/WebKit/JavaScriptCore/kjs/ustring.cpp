4d91728cb565f03aad24d1e03392dbcd097fd0fb
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-16 20:53:21 +0000

    - try to fix the GTK build
            * kjs/ustring.cpp: Include ASCIICType.h, not ASCIICtype.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26680 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/ustring.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/ustring.cpp
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <wtf/ASCIICtype.h>
+#include <wtf/ASCIICType.h>
 #include <wtf/Vector.h>
 
 #if HAVE(STRING_H)
