8480a667daef89be24ea25153de480774f75b067
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-17 03:47:35 +0000

    2007-10-16  Mark Rowe  <mrowe@apple.com>
            Gtk build fix.  Replace use of assert with ASSERT_NOT_REACHED.
    
            * platform/gtk/FontCacheGtk.cpp:
            (WebCore::FontCache::platformInit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26695 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/FontPlatformDataGtk.cpp
+++ third_party/WebKit/WebCore/platform/gtk/FontPlatformDataGtk.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cairo-ft.h"
 #include "cairo.h"
 #include <fontconfig/fcfreetype.h>
-#include <assert.h>
 
 namespace WebCore {
 
