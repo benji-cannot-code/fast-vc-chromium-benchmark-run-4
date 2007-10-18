3a7462e14c23f6410aaf297e341e30ea72369678
ap <ap@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-18 09:57:20 +0000

    Windows build fix.
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::applyProperty): Add braces around cases that have local variables now.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26744 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/WebCore/css/CSSStyleSelector.cpp
@@ -3475,6 +3475,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
         style->setOpacity(min(1.0f, max(0.0f, primitiveValue->getFloatValue())));
         return;
     case CSS_PROP__WEBKIT_BOX_ALIGN:
+    {
         HANDLE_INHERIT_AND_INITIAL(boxAlign, BoxAlign)
         if (!primitiveValue)
             return;
@@ -3482,6 +3483,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
         if (boxAlignment != BJUSTIFY)
             style->setBoxAlign(boxAlignment);
         return;
+    }
     case CSS_PROP_SRC: // Only used in @font-face rules.
         return;
     case CSS_PROP__WEBKIT_BOX_DIRECTION:
@@ -3500,13 +3502,15 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
             style->setBoxOrient(*primitiveValue);
         return;     
     case CSS_PROP__WEBKIT_BOX_PACK:
+    {
         HANDLE_INHERIT_AND_INITIAL(boxPack, BoxPack)
         if (!primitiveValue)
             return;
         EBoxAlignment boxPack = *primitiveValue;
         if (boxPack != BSTRETCH && boxPack != BBASELINE)
             style->setBoxPack(boxPack);
-        return;        
+        return;
+    }
     case CSS_PROP__WEBKIT_BOX_FLEX:
         HANDLE_INHERIT_AND_INITIAL(boxFlex, BoxFlex)
         if (!primitiveValue || primitiveValue->primitiveType() != CSSPrimitiveValue::CSS_NUMBER)
