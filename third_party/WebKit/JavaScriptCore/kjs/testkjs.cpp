979429652432ff2e44cd726e0b442e4e1aabac13
zimmermann <zimmermann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-19 21:25:53 +0000

    Reviewed by Anders. Try to fix Qt/Win build slave, by including windows.h also on Qt/Win.
    git-svn-id: svn://svn.chromium.org/blink/trunk@26783 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/time.h>
 #endif
 
-#if PLATFORM(WIN)
+#if PLATFORM(WIN_OS)
 #include <crtdbg.h>
 #include <windows.h>
 #endif
