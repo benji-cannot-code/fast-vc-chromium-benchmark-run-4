51f732974bf474c03f2d482fdfc25d7f12f99a36
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-19 14:46:28 +0000

    Qt/Windows build fix: Threading.h uses int32_t but doesn't include stdint.h.
    git-svn-id: svn://svn.chromium.org/blink/trunk@26772 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Threading.h
+++ third_party/WebKit/WebCore/platform/Threading.h
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <pthread.h>
 #endif
 
+#include <stdint.h>
+
 namespace WebCore {
 
 typedef uint32_t ThreadIdentifier;
