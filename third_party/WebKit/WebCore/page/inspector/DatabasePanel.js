a891ff4ffcaca1366568c6c1f59ec29261874733
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-20 21:00:53 +0000

    2007-10-20  Mark Rowe  <mrowe@apple.com>
            Reviewed by Tim Hatcher.
    
            Workaround for http://bugs.webkit.org/show_bug.cgi?id=15574
            Bug 15574: Web Inspector doesn't work with the new Database feature
    
            The executeSql callback is executed in the security domain of the web page that owns the database,
            while the inspector's window object is in the callback functions scope chain.  This is leading to a
            security violation when the callback attempts to access "document".  We can work around this by
            ensuring that "document" can be found in the scope chain before the window object.
    
            * page/inspector/DatabasePanel.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26830 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/DatabasePanel.js
+++ third_party/WebKit/WebCore/page/inspector/DatabasePanel.js
@@ -124,7 +124,11 @@ WebInspector.DatabasePanel = function(database)
     };
 }
 
-WebInspector.DatabasePanel.prototype = {
+// FIXME: The function and local variables are a workaround for http://bugs.webkit.org/show_bug.cgi?id=15574.
+WebInspector.DatabasePanel.prototype = (function() {
+var document = window.document;
+var Math = window.Math;
+return {
     show: function()
     {
         WebInspector.Panel.prototype.show.call(this);
@@ -507,5 +511,6 @@ WebInspector.DatabasePanel.prototype = {
         return table;
     }
 }
+})();
 
 WebInspector.DatabasePanel.prototype.__proto__ = WebInspector.Panel.prototype;
