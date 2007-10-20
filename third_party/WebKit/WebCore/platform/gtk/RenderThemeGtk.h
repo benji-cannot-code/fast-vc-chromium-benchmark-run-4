091a18e4b15cbac2bf325f5fc05c12c877550746
alp <alp@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-20 01:48:21 +0000

    2007-10-19  Alp Toker  <alp@atoker.com>
            Reviewed by Oliver.
    
            Use platform colors for text selection.
            Update the cache when the GTK style is changed.
    
            * platform/gtk/RenderThemeGtk.cpp:
            (WebCore::RenderThemeGtk::platformActiveSelectionBackgroundColor):
            (WebCore::RenderThemeGtk::platformInactiveSelectionBackgroundColor):
            (WebCore::RenderThemeGtk::platformActiveSelectionForegroundColor):
            (WebCore::RenderThemeGtk::platformInactiveSelectionForegroundColor):
            (WebCore::RenderThemeGtk::gtkStyleSet):
            (WebCore::RenderThemeGtk::gtkEntry):
            * platform/gtk/RenderThemeGtk.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/RenderThemeGtk.h
+++ third_party/WebKit/WebCore/platform/gtk/RenderThemeGtk.h
@@ -48,6 +48,12 @@ public:
     // A method asking if the theme's controls actually care about redrawing when hovered.
     virtual bool supportsHover(const RenderStyle* style) const { return true; }
 
+    // The platform selection color.
+    virtual Color platformActiveSelectionBackgroundColor() const;
+    virtual Color platformInactiveSelectionBackgroundColor() const;
+    virtual Color platformActiveSelectionForegroundColor() const;
+    virtual Color platformInactiveSelectionForegroundColor() const;
+
     // System fonts.
     virtual void systemFont(int propId, FontDescription&) const;
 
@@ -76,6 +82,7 @@ private:
 
     ThemeData getThemeData(RenderObject*);
 
+    static void gtkStyleSet(GtkWidget*, GtkStyle*, RenderTheme*);
 
     /*
      * hold the state
