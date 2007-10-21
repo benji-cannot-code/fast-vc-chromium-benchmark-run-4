6bde28f952be5f157c2bf33620e231586c7ea7b4
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-21 18:37:23 +0000

    - fix Windows build
            * kjs/array_instance.h: Removed unused ExecState parameter.
            * kjs/array_object.cpp:
            (KJS::ArrayInstance::put): Ditto.
            (KJS::ArrayInstance::setLength): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
@@ -191,7 +191,7 @@ void ArrayInstance::put(ExecState* exec, const Identifier& propertyName, JSValue
       throwError(exec, RangeError, "Invalid array length.");
       return;
     }
-    setLength(newLen, exec);
+    setLength(newLen);
     return;
   }
 
@@ -329,7 +329,7 @@ void ArrayInstance::resizeStorage(unsigned newLength)
     storageLength = newLength;
 }
 
-void ArrayInstance::setLength(unsigned newLength, ExecState *exec)
+void ArrayInstance::setLength(unsigned newLength)
 {
   if (newLength <= storageLength)
     resizeStorage(newLength);
