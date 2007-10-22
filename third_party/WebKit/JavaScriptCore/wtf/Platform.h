04f208d464033c2e6efb0b8a478a7b6bd45961ed
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-22 11:39:34 +0000

    Build fix for the non-qmake builds.
    git-svn-id: svn://svn.chromium.org/blink/trunk@26879 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -225,6 +225,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_ICONDATABASE 1
 #endif
 
+#if !defined(ENABLE_DATABASE)
+#define ENABLE_DATABASE 1
+#endif
+
 #if !defined(ENABLE_FTPDIR)
 #define ENABLE_FTPDIR 1
 #endif
