8aceeaeb82528e69c0cdf6e211b2bab7132b22b5
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-22 09:18:54 +0000

    2007-10-15 Holger Hans Peter Freyther <zecke@selfish.org>
            Implement the WebCore::fileSize function. The
            implementation assumes that QFileInfo will cache
            the result of the stat so that info.size() and
            info.exists() use the same stat result.
    
    Signed-off-by: Lars Knoll <lars@trolltech.com>
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26868 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/FileSystemQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/FileSystemQt.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2007 Staikos Computing Services Inc.
+ * Copyright (C) 2007 Holger Hans Peter Freyther
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <PlatformString.h>
 #include <QFile>
+#include <QFileInfo>
 #include <QDir>
 
 namespace WebCore {
@@ -42,6 +44,13 @@ bool fileExists(const String& path)
 bool deleteFile(const String& path)
 {
     return QFile::remove(path);
+}
+
+bool fileSize(const String& path, long long& result)
+{
+    QFileInfo info(path);
+    result = info.size();
+    return info.exists(); 
 }
 
 bool makeAllDirectories(const String& path)
