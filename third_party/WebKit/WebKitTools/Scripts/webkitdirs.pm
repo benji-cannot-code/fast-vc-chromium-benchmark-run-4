5ee78f46dd974acc5612d0b987cf90f1d0813771
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-22 14:54:05 +0000

    Wrap WEXITSTATUS with a little exitStatus() helper function that falls back to the use of $returnvalue >> 8 on Windows to determine the exit status on platforms without WEXITSTATUS.
    git-svn-id: svn://svn.chromium.org/blink/trunk@26885 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -31,6 +31,7 @@ use strict;
 use warnings;
 use FindBin;
 use File::Basename;
+use POSIX;
 
 BEGIN {
    use Exporter   ();
@@ -741,6 +742,15 @@ sub setPathForRunningWebKitApp
     return unless isCygwin();
 
     $env->{PATH} = join(':', productDir(), dirname(installedSafariPath()), $env->{PATH} || "");
+}
+
+sub exitStatus($)
+{
+    my ($returnvalue) = @_;
+    if ($^O eq "MSWin32") {
+        return $returnvalue >> 8;
+    }
+    return WEXITSTATUS($returnvalue);
 }
 
 1;
