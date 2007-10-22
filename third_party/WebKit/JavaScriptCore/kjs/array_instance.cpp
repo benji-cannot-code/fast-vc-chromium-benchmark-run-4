02b0eeb8b75af068b16b9fb661eb3f5e36a71024
bdash <bdash@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-22 13:56:45 +0000

    2007-10-22  Mark Rowe  <mrowe@apple.com>
            Gtk build fix.
    
            * kjs/array_instance.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26883 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_instance.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/array_instance.cpp
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PropertyNameArray.h"
 #include <wtf/Assertions.h>
 
+using std::min;
+
 namespace KJS {
 
 typedef HashMap<unsigned, JSValue*> SparseArrayValueMap;
