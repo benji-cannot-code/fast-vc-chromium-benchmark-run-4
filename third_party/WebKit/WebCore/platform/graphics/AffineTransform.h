71c1158da0098911e9453dad102ee5a121db2f82
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-23 15:52:26 +0000

    Get basic hit testing right for transforms.
            Reviewed by ollliej
    
            * platform/graphics/AffineTransform.cpp:
            (WebCore::AffineTransform::mapPoint):
            * platform/graphics/AffineTransform.h:
            * rendering/InlineBox.cpp:
            (WebCore::InlineBox::nodeAtPoint):
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::nodeAtPoint):
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::paintLayer):
            (WebCore::RenderLayer::hitTest):
            (WebCore::RenderLayer::hitTestLayer):
            * rendering/RenderLayer.h:
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::hitTest):
            * rendering/RenderObject.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26921 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/AffineTransform.h
+++ third_party/WebKit/WebCore/platform/graphics/AffineTransform.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+class IntPoint;
 class IntRect;
 class FloatRect;
 
@@ -54,6 +55,7 @@ public:
 
     void setMatrix(double a, double b, double c, double d, double e, double f);
     void map(double x, double y, double *x2, double *y2) const;
+    IntPoint mapPoint(const IntPoint&) const;
     IntRect mapRect(const IntRect&) const;
     FloatRect mapRect(const FloatRect&) const;
     
