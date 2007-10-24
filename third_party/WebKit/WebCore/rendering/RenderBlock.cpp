73d749151672eb21814fcf88c285217a118529e6
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-24 20:33:34 +0000

    Don't try to gap fill transformed selections.  Fix the invalidation of selection to use the clip to visible content         code path so that it works with multi-column layouts and transforms.
            Reviewed by aroben
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::isSelectionRoot):
            (WebCore::RenderBlock::fillSelectionGaps):
            * rendering/RenderView.cpp:
            (WebCore::RenderView::setSelection):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27000 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBlock.cpp
@@ -1748,7 +1748,7 @@ bool RenderBlock::isSelectionRoot() const
         return false;
         
     if (isBody() || isRoot() || hasOverflowClip() || isRelPositioned() ||
-        isFloatingOrPositioned() || isTableCell() || isInlineBlockOrInlineTable())
+        isFloatingOrPositioned() || isTableCell() || isInlineBlockOrInlineTable() || hasTransform())
         return true;
     
     if (view() && view()->selectionStart()) {
@@ -1798,8 +1798,8 @@ GapRects RenderBlock::fillSelectionGaps(RenderBlock* rootBlock, int blockX, int 
     if (!isBlockFlow()) // FIXME: Make multi-column selection gap filling work someday.
         return result;
 
-    if (m_hasColumns) {
-        // FIXME: We should learn how to gap fill multiple columns eventually.
+    if (m_hasColumns || hasTransform()) {
+        // FIXME: We should learn how to gap fill multiple columns and transforms eventually.
         lastTop = (ty - blockY) + height();
         lastLeft = leftSelectionOffset(rootBlock, height());
         lastRight = rightSelectionOffset(rootBlock, height());
