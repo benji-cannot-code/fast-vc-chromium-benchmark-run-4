276cc7ca0de59f0f8120285cd53dba711390ce71
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-24 23:00:26 +0000

    WebKitTools:
            Reviewed by Sam, Steve and Darin.
    
            - Stubbed out the WebScriptDebugListener functionality in the Server
            Connection class to prepare it for receiving those callbacks.
            - Also I changed the instantiation of DebuggerClient, DebuggerDocument
            and the ServerConnection to not need to be initialized with a server
            name, since that is not the way we connect to the WebKit server. 
    
            * Drosera/DebuggerDocument.cpp: Fixed a bug where I was always logging
            no exception.  Now it only loggs when there is an exception
            (DebuggerDocument::willExecuteStatement):
            (DebuggerDocument::didEnterCallFrame):
            (DebuggerDocument::willLeaveCallFrame):
            (DebuggerDocument::windowScriptObjectAvailable):
            (DebuggerDocument::callFunctionOnObject):
            * Drosera/win/DebuggerClient.cpp: No longer needs the ServerConnection
            to be instantiated with a server's name.
            (DebuggerClient::DebuggerClient):
            (DebuggerClient::didFinishLoadForFrame):
            * Drosera/win/DebuggerClient.h: Removed unsued variable.
            * Drosera/win/Drosera.cpp: Client no longer needs to be initialized with
            a server name.
            (Drosera::init):
            (Drosera::initServer):
            * Drosera/win/Drosera.h: No longer need the ServerConnection to be
            instantiated with a server's name.
            * Drosera/win/ServerConnection.cpp: 
            (ServerConnection::ServerConnection): No longer needs a server name
            (ServerConnection::~ServerConnection): Only release the global context
            if there is one.
            (ServerConnection::serverConnectionDidDie): Stub for
            IWebScriptDebugListener
            (ServerConnection::QueryInterface): ditto
            (ServerConnection::AddRef): ditto
            (ServerConnection::Release): ditto
            (ServerConnection::didLoadMainResourceForDataSource): ditto
            (ServerConnection::didParseSource): ditto
            (ServerConnection::failedToParseSource): ditto
            (ServerConnection::didEnterCallFrame): ditto
            (ServerConnection::willExecuteStatement): ditto
            (ServerConnection::willLeaveCallFrame): ditto
            (ServerConnection::exceptionWasRaised): ditto
            * Drosera/win/ServerConnection.h: Stubbed out the
            IWebScriptDebugListener functions, and this class no longer needs to be
            instantiated with a server name.
    
    win:
    
            Reviewed by Sam, Steve and Darin.
    
            - Added stubs for what will be neede to let Drosera attach to the
            WebKit process and debug it.
    
            * Interfaces/IWebScriptCallFrame.idl: Added.
            * Interfaces/IWebScriptDebugListener.idl: Added.
            * Interfaces/WebKit.idl:
            * WebKit.vcproj/Interfaces.vcproj:
            * WebKit.vcproj/WebKitGUID.vcproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27003 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Drosera/win/ServerConnection.cpp
+++ third_party/WebKit/WebKitTools/Drosera/win/ServerConnection.cpp
@@ -39,18 +39,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <JavaScriptCore/RetainPtr.h>
 
 // FIXME: Some of the below functionality cannot be implemented until the WebScriptDebug Server works on windows.
-
-ServerConnection* ServerConnection::initWithServerName(const std::wstring& serverName)
+ServerConnection::ServerConnection()
+    : m_globalContext(0)
 {
-    ServerConnection* server = new ServerConnection();
-    server->switchToServerNamed(serverName);
-
-    return server;
 }
 
 ServerConnection::~ServerConnection()
 {
-    JSGlobalContextRelease(m_globalContext);
+    if (m_globalContext)
+        JSGlobalContextRelease(m_globalContext);
 }
 
 void ServerConnection::setGlobalContext(JSGlobalContextRef globalContextRef)
@@ -70,10 +67,6 @@ void ServerConnection::stepInto()
 {
 }
 
-void ServerConnection::switchToServerNamed(const std::wstring& /*name*/)
-{
-}
-
 // Connection Handling
 
 void ServerConnection::applicationTerminating()
@@ -82,7 +75,6 @@ void ServerConnection::applicationTerminating()
 
 void ServerConnection::serverConnectionDidDie()
 {
-    switchToServerNamed(L"");
 }
 
 // Stack & Variables
@@ -92,3 +84,99 @@ WebScriptCallFrame* ServerConnection::currentFrame() const
     return m_currentFrame;
 }
 
+
+// IUnknown --------------------------------------------------
+HRESULT STDMETHODCALLTYPE ServerConnection::QueryInterface(REFIID riid, void** ppvObject)
+{
+    *ppvObject = 0;
+    if (IsEqualGUID(riid, IID_IUnknown))
+        *ppvObject = this;
+    else if (IsEqualGUID(riid, IID_IWebScriptDebugListener))
+        *ppvObject = static_cast<IWebScriptDebugListener*>(this);
+    else
+        return E_NOINTERFACE;
+
+    AddRef();
+    return S_OK;
+}
+
+ULONG STDMETHODCALLTYPE ServerConnection::AddRef(void)
+{
+    // COM ref-counting isn't useful to us because we're in charge of the lifetime of the WebView.
+    return 1;
+}
+
+ULONG STDMETHODCALLTYPE ServerConnection::Release(void)
+{
+    // COM ref-counting isn't useful to us because we're in charge of the lifetime of the WebView.
+    return 1;
+}
+// IWebScriptDebugListener -----------------------------------
+HRESULT STDMETHODCALLTYPE ServerConnection::didLoadMainResourceForDataSource(
+    /* [in] */ IWebView* /*view*/,
+    /* [in] */ IWebDataSource* /*dataSource*/)
+{
+    return S_OK;
+}
+
+HRESULT STDMETHODCALLTYPE ServerConnection::didParseSource(
+    /* [in] */ IWebView* /*view*/,
+    /* [in] */ BSTR /*sourceCode*/,
+    /* [in] */ UINT /*baseLineNumber*/,
+    /* [in] */ BSTR /*url*/,
+    /* [in] */ int /*sourceID*/,
+    /* [in] */ IWebFrame* /*forWebFrame*/)
+{
+    return S_OK;
+}
+
+HRESULT STDMETHODCALLTYPE ServerConnection::failedToParseSource(
+    /* [in] */ IWebView* /*view*/,
+    /* [in] */ BSTR /*sourceCode*/,
+    /* [in] */ UINT /*baseLineNumber*/,
+    /* [in] */ BSTR /*url*/,
+    /* [in] */ BSTR /*error*/,
+    /* [in] */ IWebFrame* /*forWebFrame*/)
+{
+    return S_OK;
+}
+
+HRESULT STDMETHODCALLTYPE ServerConnection::didEnterCallFrame(
+    /* [in] */ IWebView* /*view*/,
+    /* [in] */ IWebScriptCallFrame* /*frame*/,
+    /* [in] */ int /*sourceID*/,
+    /* [in] */ int /*lineNumber*/,
+    /* [in] */ IWebFrame* /*forWebFrame*/)
+{
+    return S_OK;
+}
+
+HRESULT STDMETHODCALLTYPE ServerConnection::willExecuteStatement(
+    /* [in] */ IWebView* /*view*/,
+    /* [in] */ IWebScriptCallFrame* /*frame*/,
+    /* [in] */ int /*sourceID*/,
+    /* [in] */ int /*lineNumber*/,
+    /* [in] */ IWebFrame* /*forWebFrame*/)
+{
+    return S_OK;
+}
+
+HRESULT STDMETHODCALLTYPE ServerConnection::willLeaveCallFrame(
+    /* [in] */ IWebView* /*view*/,
+    /* [in] */ IWebScriptCallFrame* /*frame*/,
+    /* [in] */ int /*sourceID*/,
+    /* [in] */ int /*lineNumber*/,
+    /* [in] */ IWebFrame* /*forWebFrame*/)
+{
+    return S_OK;
+}
+
+HRESULT STDMETHODCALLTYPE ServerConnection::exceptionWasRaised(
+    /* [in] */ IWebView* /*view*/,
+    /* [in] */ IWebScriptCallFrame* /*frame*/,
+    /* [in] */ int /*sourceID*/,
+    /* [in] */ int /*lineNumber*/,
+    /* [in] */ IWebFrame* /*forWebFrame*/)
+{
+    return S_OK;
+}
