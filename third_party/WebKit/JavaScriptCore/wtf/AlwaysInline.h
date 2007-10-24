d3d23023ee802ca0a791a79aa1adbb2e2e3229b9
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-24 18:04:38 +0000

    Reviewed by Sam Weinig.
            Disable ALWAYS_INLINE in debug builds, since it drives the debugger
            crazy.
    
            * wtf/AlwaysInline.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@26995 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/AlwaysInline.h
+++ third_party/WebKit/JavaScriptCore/wtf/AlwaysInline.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #ifndef ALWAYS_INLINE
-#if COMPILER(GCC)
+#if COMPILER(GCC) && defined(NDEBUG)
 #define ALWAYS_INLINE inline __attribute__ ((__always_inline__))
 #else
 #define ALWAYS_INLINE inline
