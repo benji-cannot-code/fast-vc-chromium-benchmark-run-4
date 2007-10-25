aa19ff1cf97f476da72e2cfbaf553029144b716b
alp <alp@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-25 12:36:54 +0000

    2007-10-25  Alp Toker  <alp@atoker.com>
            Reviewed by Mark Rowe.
    
            Add support for list box theme colors and styled menu list buttons.
    
            Remove obsolete FIXMEs.
    
            * platform/gtk/RenderThemeGtk.cpp:
            (WebCore::RenderThemeGtk::RenderThemeGtk):
            (WebCore::RenderThemeGtk::paintCheckbox):
            (WebCore::RenderThemeGtk::paintRadio):
            (WebCore::RenderThemeGtk::paintButton):
            (WebCore::RenderThemeGtk::adjustMenuListStyle):
            (WebCore::RenderThemeGtk::paintMenuList):
            (WebCore::RenderThemeGtk::activeListBoxSelectionBackgroundColor):
            (WebCore::RenderThemeGtk::inactiveListBoxSelectionBackgroundColor):
            (WebCore::RenderThemeGtk::activeListBoxSelectionForegroundColor):
            (WebCore::RenderThemeGtk::inactiveListBoxSelectionForegroundColor):
            (WebCore::RenderThemeGtk::gtkTreeView):
            * platform/gtk/RenderThemeGtk.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27037 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/RenderThemeGtk.h
+++ third_party/WebKit/WebCore/platform/gtk/RenderThemeGtk.h
@@ -54,6 +54,12 @@ public:
     virtual Color platformActiveSelectionForegroundColor() const;
     virtual Color platformInactiveSelectionForegroundColor() const;
 
+    // List Box selection color
+    virtual Color activeListBoxSelectionBackgroundColor() const;
+    virtual Color activeListBoxSelectionForegroundColor() const;
+    virtual Color inactiveListBoxSelectionBackgroundColor() const;
+    virtual Color inactiveListBoxSelectionForegroundColor() const;
+
     // System fonts.
     virtual void systemFont(int propId, FontDescription&) const;
 
@@ -71,6 +77,9 @@ protected:
     virtual bool paintTextField(RenderObject*, const RenderObject::PaintInfo&, const IntRect&);
 
     virtual bool paintTextArea(RenderObject*, const RenderObject::PaintInfo&, const IntRect&);
+
+    virtual void adjustMenuListStyle(CSSStyleSelector*, RenderStyle*, Element*) const;
+    virtual bool paintMenuList(RenderObject*, const RenderObject::PaintInfo&, const IntRect&);
 
 private:
     void addIntrinsicMargins(RenderStyle*) const;
@@ -92,6 +101,7 @@ private:
     GtkWidget* gtkRadioButton() const;
     GtkWidget* gtkEntry() const;
     GtkWidget* gtkEditable() const;
+    GtkWidget* gtkTreeView() const;
 
     /*
      * unmapped GdkWindow having a container. This is holding all
@@ -105,6 +115,7 @@ private:
     mutable GtkWidget* m_gtkRadioButton;
     mutable GtkWidget* m_gtkEntry;
     mutable GtkWidget* m_gtkEditable;
+    mutable GtkWidget* m_gtkTreeView;
 
     mutable GtkWidget* m_unmappedWindow;
     mutable GtkWidget* m_container;
