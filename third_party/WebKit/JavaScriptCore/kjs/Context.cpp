1e68bfc6aa696f113d27366f524b8e7c97a34256
ggaren <ggaren@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-25 21:37:45 +0000

    Reviewed by Oliver Hunt.
            Fixed http://bugs.webkit.org/show_bug.cgi?id=15694
            Shrink the size of an activation object by 1 word
            
            This is in preparation for adding a symbol table to the activation 
            object.
            
            The basic strategy here is to rely on the mutual exclusion between
            the arguments object pointer and the function pointer (you only need
            the latter in order to create the former), and store them in the same 
            place. The LazyArgumentsObject class encapsulates this strategy.
            
            Also inlined the ArgumentsImp constructor, for good measure.
            
            SunSpider reports no regression. Regression tests pass.
    
            * JavaScriptCore.xcodeproj/project.pbxproj:
            * kjs/Context.cpp:
            (KJS::Context::~Context):
            * kjs/function.cpp:
            (KJS::ActivationImp::LazyArgumentsObject::createArgumentsObject):
            (KJS::ActivationImp::LazyArgumentsObject::mark):
            (KJS::):
            (KJS::ActivationImp::argumentsGetter):
            (KJS::ActivationImp::mark):
            * kjs/function.h:
            (KJS::ActivationImp::LazyArgumentsObject::LazyArgumentsObject):
            (KJS::ActivationImp::LazyArgumentsObject::getOrCreate):
            (KJS::ActivationImp::LazyArgumentsObject::resetArguments):
            (KJS::ActivationImp::LazyArgumentsObject::setArgumentsObject):
            (KJS::ActivationImp::LazyArgumentsObject::argumentsObject):
            (KJS::ActivationImp::LazyArgumentsObject::setFunction):
            (KJS::ActivationImp::LazyArgumentsObject::function):
            (KJS::ActivationImp::LazyArgumentsObject::createdArgumentsObject):
            (KJS::ActivationImp::LazyArgumentsObject::):
            (KJS::ActivationImp::ActivationImp::ActivationImp):
            (KJS::ActivationImp::resetArguments):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/Context.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/Context.cpp
@@ -88,7 +88,7 @@ Context::~Context()
     // This prevents lists of Lists from building up, waiting to be garbage collected
     ActivationImp* activation = static_cast<ActivationImp*>(m_activation);
     if (activation)
-        activation->releaseArguments();
+        activation->resetArguments();
 }
 
 void Context::mark()
