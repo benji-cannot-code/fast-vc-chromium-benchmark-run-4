34a8394a85972f01f8a8e49041e7948d0e226172
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-25 05:23:20 +0000

    Fix <rdar://5549919> Add font database initialization code to WebCore and call it from WebKit
     WebCore:
    
             Add font database initialization code to WebCore
    
             Reviewed by Ada.
    
             * WebCore.vcproj/WebCore.vcproj: Added FontDatabase.{cpp,h}.
             * platform/win/FontDatabase.cpp: Added.
             (WebCore::systemFontsDirectory):
             (WebCore::fontsPlistPath):
             (WebCore::systemHasFontsNewerThanFontsPlist):
             (WebCore::readFontPlist):
             (WebCore::populateFontDatabaseFromPlist):
             (WebCore::populateFontDatabaseFromFileSystem):
             (WebCore::writeFontDatabaseToPlist):
             (WebCore::populateFontDatabase): This is the only function callable
             from outside this file. It populates the font database once, either
             from the fonts plist, or from the filesystem (and then saves a new
             fonts plist).
             * platform/win/FontDatabase.h: Added.
    
     WebKitTools:
    
             Remove now-unnecessary call to InitializeCoreGraphics
    
             WebKit takes care of this now.
    
             Reviewed by Ada.
    
             * DumpRenderTree/win/DumpRenderTree.cpp:
             (initialize):
    
     WebKit/win:
    
             Fix <rdar://5549919> Initialize the font database before any font code is invoked
    
             We initialize the database in two places:
                1. When instantiating WebKitClassFactory, which is guaranteed to
                happen before any WebView is instantiated.
                2. When making a WebCore::Font in WebKitGraphics.
    
             This ensures that the font database will be populated before any font
             code is invoked. We rely on WebCore to only populate the database
             once.
    
             Reviewed by Ada.
    
             * WebKitClassFactory.cpp:
             (WebKitClassFactory::WebKitClassFactory): Populate the font database.
             * WebKitGraphics.cpp:
             (makeFont): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27020 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebKitClassFactory.cpp
+++ third_party/WebKit/WebKit/win/WebKitClassFactory.cpp
@@ -51,6 +51,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebDebugProgram.h"
 #include "WebView.h"
 #include <SafariTheme/SafariTheme.h>
+#pragma warning(push, 0)
+#include <WebCore/FontDatabase.h>
+#pragma warning(pop)
 
 // WebKitClassFactory ---------------------------------------------------------
 
@@ -67,6 +70,8 @@ WebKitClassFactory::WebKitClassFactory(CLSID targetClass)
                 stInit();
         didInitializeSafariTheme = true;
     }
+
+    WebCore::populateFontDatabase();
 
     gClassCount++;
 }
