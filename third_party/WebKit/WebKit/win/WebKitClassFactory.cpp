19966cbca3afb9325ecf9e687208f859c0eef3e1
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-25 05:22:56 +0000

    Add [I]WebTextRenderer
     WebKitTools:
    
             Use WebTextRenderer in DRT
    
             Reviewed by Ada.
    
             * DumpRenderTree/win/DumpRenderTree.cpp:
             (initialize): Use WebTextRenderer instead of using
             InitializeCoreGraphics/AddFontResourceEx.
    
     WebKit/win:
    
             Add [I]WebTextRenderer
    
             Right now this class can only be used to add private fonts for use by
             the running process, but will eventually be the home of the
             WebKitGraphics functions.
    
             Reviewed by Ada.
    
             * ForEachCoClass.h: Added WebTextRenderer.
             * Interfaces/WebKit.idl: Ditto.
             * WebKit.vcproj/Interfaces.vcproj: Ditto.
             * WebKit.vcproj/WebKit.vcproj: Ditto.
             * WebKit.vcproj/WebKitGUID.vcproj: Ditto.
             * WebKitClassFactory.cpp: Ditto.
             * WebTextRenderer.cpp: Added.
             (WebTextRenderer::createInstance):
             (WebTextRenderer::WebTextRenderer):
             (WebTextRenderer::~WebTextRenderer):
             (WebTextRenderer::QueryInterface):
             (WebTextRenderer::AddRef):
             (WebTextRenderer::Release):
             (WebTextRenderer::registerPrivateFont):
             * WebTextRenderer.h: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27019 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebKitClassFactory.cpp
+++ third_party/WebKit/WebKit/win/WebKitClassFactory.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebMutableURLRequest.h"
 #include "WebNotificationCenter.h"
 #include "WebPreferences.h"
+#include "WebTextRenderer.h"
 #include "WebURLCredential.h"
 #include "WebURLProtectionSpace.h"
 #include "WebURLResponse.h"
