807d995faeda92d4f76aeb47031e547457e49f99
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-25 21:03:41 +0000

    WebCore:
            Fix for bug 15672, background images don't tile properly inside transforms.  This patch fixes background
            tiling to work in the presence of transforms and fixes bugs in both SVG and CSS transforms.
    
            Reviewed by aroben and mitz
    
            * WebCore.base.exp:
            * platform/graphics/Image.cpp:
            (WebCore::Image::setData):
            * platform/graphics/cg/ImageCG.cpp:
            (WebCore::ImageInfo::ImageInfo):
            (WebCore::Image::drawPatternCallback):
            (WebCore::Image::drawPattern):
            * platform/graphics/mac/GraphicsContextMac.mm:
            (WebCore::GraphicsContext::drawLineForMisspellingOrBadGrammar):
            * platform/mac/WebCoreSystemInterface.h:
            * platform/mac/WebCoreSystemInterface.mm:
    
    WebKit:
    
            Fix for bug 15672, backgrounds don't tile properly inside transforms.  This patch fixes tiling
            of backgrounds inside CSS transforms and also of HTML content with background images inside SVG
            transforms.
    
            Reviewed by aroben and mmitz
    
            * WebCoreSupport/WebSystemInterface.m:
            (InitWebCoreSystemInterface):
            * WebKit.xcodeproj/project.pbxproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/WebCoreSupport/WebSystemInterface.m
+++ third_party/WebKit/WebKit/WebCoreSupport/WebSystemInterface.m
@@ -77,6 +77,7 @@ void InitWebCoreSystemInterface(void)
     INIT(SetDragImage);
     INIT(SetNSURLConnectionDefersCallbacks);
     INIT(SetNSURLRequestShouldContentSniff);
+    INIT(SetPatternBaseCTM);
     INIT(SetPatternPhaseInUserSpace);
     INIT(SetUpFontCache);
     INIT(SignalCFReadStreamEnd);
