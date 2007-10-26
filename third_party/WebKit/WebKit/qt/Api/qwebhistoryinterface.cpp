a59866ad9500172763ef4e4d68770338e3b80c93
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-26 12:22:37 +0000

    2007-10-26  Mark Rowe  <mrowe@apple.com>
            Build fix.  Add missing #include of Platform.h.
    
            * Api/qwebhistoryinterface.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebhistoryinterface.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebhistoryinterface.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <QCoreApplication>
 
+#include <wtf/Platform.h>
 #include "DeprecatedString.h"
 
 namespace WebCore {
