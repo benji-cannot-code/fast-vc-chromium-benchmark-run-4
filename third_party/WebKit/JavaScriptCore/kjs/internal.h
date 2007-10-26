db5d2036a8d804513811b418febb0ba4505bcb15
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-26 03:58:18 +0000

    Reviewed by Oliver.
            - fold together toPrimitive() and toNumber() conversions for 0.5% gain on SunSpider
    
            * kjs/nodes.cpp:
            (KJS::SubNode::evaluate): Subtract directly, since toPrimitive() is not
            adding any value over toNumber() here.
            (KJS::valueForReadModifyAssignment): Ditto.
            (KJS::lessThan): Use new getPrimitiveNumber() method to avoid some virtual calls
            and branches.
            (KJS::lessThanEq): Ditto.
            * JavaScriptCore.exp: Export new functions as needed.
            * kjs/value.h:
            (KJS::JSValue::toPrimitive): Fixed formatting.
            (KJS::JSValue::getPrimitiveNumber): New method - this simultaneously converts
            to number and tells you whether a toPrimitive() conversion with a Number hint
            would have given a string.
            * kjs/internal.cpp:
            (KJS::StringImp::getPrimitiveNumber): Implemented. 
            (KJS::NumberImp::getPrimitiveNumber): ditto
            (KJS::GetterSetterImp::getPrimitiveNumber): ditto
            (KJS::StringImp::toPrimitive): Fixed formatting.
            (KJS::NumberImp::toPrimitive): ditto
            (KJS::GetterSetterImp::toPrimitive): ditto
            * kjs/internal.h:
            * kjs/object.cpp:
            (KJS::JSObject::getPrimitiveNumber): Implemented.
            * kjs/object.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/internal.h
+++ third_party/WebKit/JavaScriptCore/kjs/internal.h
@@ -54,7 +54,8 @@ namespace KJS {
 
     JSType type() const { return StringType; }
 
-    JSValue *toPrimitive(ExecState *exec, JSType preferred = UnspecifiedType) const;
+    JSValue* toPrimitive(ExecState*, JSType preferred = UnspecifiedType) const;
+    bool getPrimitiveNumber(ExecState*, double& number) const;
     bool toBoolean(ExecState *exec) const;
     double toNumber(ExecState *exec) const;
     UString toString(ExecState *exec) const;
@@ -72,7 +73,8 @@ namespace KJS {
 
     JSType type() const { return NumberType; }
 
-    JSValue *toPrimitive(ExecState *exec, JSType preferred = UnspecifiedType) const;
+    JSValue* toPrimitive(ExecState*, JSType preferred = UnspecifiedType) const;
+    bool getPrimitiveNumber(ExecState*, double& number) const;
     bool toBoolean(ExecState *exec) const;
     double toNumber(ExecState *exec) const;
     UString toString(ExecState *exec) const;
