d8101503e957dd1be5b1cbf0657551e37fc519ba
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-27 01:37:04 +0000

    Reviewed by Adam.
            - Made JSStringCreateWithBSTR capable of handling null BSTRs.
    
            * API/JSStringRefCOM.cpp:
            (JSStringCreateWithBSTR):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27135 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSStringRefCOM.cpp
+++ third_party/WebKit/JavaScriptCore/API/JSStringRefCOM.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 JSStringRef JSStringCreateWithBSTR(BSTR string)
 {
-    return JSStringCreateWithCharacters(string, SysStringLen(string));
+    return JSStringCreateWithCharacters(string ? string : L"", string ? SysStringLen(string) : 0);
 }
 
 BSTR JSStringCopyBSTR(const JSStringRef string)
