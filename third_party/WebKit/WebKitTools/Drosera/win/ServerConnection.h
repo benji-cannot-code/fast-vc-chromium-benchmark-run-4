5fa83d91a0fb213e02598671ac95215c47ad914c
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-27 01:38:57 +0000

    Reviewed by Adam.
            - Implemented the WebScriptDebugListener functions now that the
            WebScriptDebugServer exists
    
            * Drosera/win/ServerConnection.cpp: Implemented WebScriptDebugListener
            functions.
            (ServerConnection::currentFrame):
            (ServerConnection::didLoadMainResourceForDataSource):
            (ServerConnection::didParseSource):
            (ServerConnection::failedToParseSource):
            (ServerConnection::didEnterCallFrame):
            (ServerConnection::willExecuteStatement):
            (ServerConnection::willLeaveCallFrame):
            (ServerConnection::exceptionWasRaised):
            * Drosera/win/ServerConnection.h: Removed unused arguments from the
            members arguments list.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27136 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Drosera/win/ServerConnection.h
+++ third_party/WebKit/WebKitTools/Drosera/win/ServerConnection.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebKit/IWebScriptDebugServer.h>
 
 class DebuggerClient;
-class WebScriptCallFrame;
+interface IWebScriptCallFrame;
 
 typedef struct OpaqueJSContext* JSGlobalContextRef;
 
@@ -53,7 +53,7 @@ public:
 
     void applicationTerminating();
     void serverConnectionDidDie();
-    WebScriptCallFrame* currentFrame() const;
+    IWebScriptCallFrame* currentFrame() const;
 
     // IUnknown
     HRESULT STDMETHODCALLTYPE QueryInterface(
@@ -65,57 +65,58 @@ public:
 
     // IWebScriptDebugListener
     HRESULT STDMETHODCALLTYPE didLoadMainResourceForDataSource(
-        /* [in] */ IWebView* view,
+        /* [in] */ IWebView*,
         /* [in] */ IWebDataSource* dataSource);
 
     HRESULT STDMETHODCALLTYPE didParseSource(
-        /* [in] */ IWebView* view,
+        /* [in] */ IWebView*,
         /* [in] */ BSTR sourceCode,
         /* [in] */ UINT baseLineNumber,
         /* [in] */ BSTR url,
         /* [in] */ int sourceID,
-        /* [in] */ IWebFrame* forWebFrame);
+        /* [in] */ IWebFrame* webFrame);
 
     HRESULT STDMETHODCALLTYPE failedToParseSource(
-        /* [in] */ IWebView* view,
+        /* [in] */ IWebView*,
         /* [in] */ BSTR sourceCode,
         /* [in] */ UINT baseLineNumber,
         /* [in] */ BSTR url,
         /* [in] */ BSTR error,
-        /* [in] */ IWebFrame* forWebFrame);
+        /* [in] */ IWebFrame*);
 
     HRESULT STDMETHODCALLTYPE didEnterCallFrame(
-        /* [in] */ IWebView* view,
+        /* [in] */ IWebView*,
         /* [in] */ IWebScriptCallFrame* frame,
         /* [in] */ int sourceID,
         /* [in] */ int lineNumber,
-        /* [in] */ IWebFrame* forWebFrame);
+        /* [in] */ IWebFrame*);
 
     HRESULT STDMETHODCALLTYPE willExecuteStatement(
-        /* [in] */ IWebView* view,
-        /* [in] */ IWebScriptCallFrame* frame,
+        /* [in] */ IWebView*,
+        /* [in] */ IWebScriptCallFrame*,
         /* [in] */ int sourceID,
         /* [in] */ int lineNumber,
-        /* [in] */ IWebFrame* forWebFrame);
+        /* [in] */ IWebFrame*);
 
     HRESULT STDMETHODCALLTYPE willLeaveCallFrame(
-        /* [in] */ IWebView* view,
+        /* [in] */ IWebView*,
         /* [in] */ IWebScriptCallFrame* frame,
         /* [in] */ int sourceID,
         /* [in] */ int lineNumber,
-        /* [in] */ IWebFrame* forWebFrame);
+        /* [in] */ IWebFrame*);
 
     HRESULT STDMETHODCALLTYPE exceptionWasRaised(
-        /* [in] */ IWebView* view,
-        /* [in] */ IWebScriptCallFrame* frame,
+        /* [in] */ IWebView*,
+        /* [in] */ IWebScriptCallFrame*,
         /* [in] */ int sourceID,
         /* [in] */ int lineNumber,
-        /* [in] */ IWebFrame* forWebFrame);
+        /* [in] */ IWebFrame*);
 
 private:
     std::wstring m_currentServerName;
 
-    WebScriptCallFrame* m_currentFrame;
+    // FIXME: make this a COMPtr when the Interface exists and the destructor can be called.
+    IWebScriptCallFrame* m_currentFrame;
     COMPtr<IWebScriptDebugServer> m_server;
     JSGlobalContextRef m_globalContext;
 };
