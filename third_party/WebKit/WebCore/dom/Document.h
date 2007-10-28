53b00e1ca24c01d57ae907597bbc881da42a7a77
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-28 07:32:15 +0000

    Reviewed by Adam.
            Pass the Document's domain to InspectorController:didOpenDatabase. This matches
            what the Inspector shows for other resources.
    
            * storage/Database.cpp:
            (WebCore::Database::openDatabase):
    
            * dom/Document.h: Remove KURL.h, no longer needed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.h
+++ third_party/WebKit/WebCore/dom/Document.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DocumentMarker.h"
 #include "HTMLCollection.h"
 #include "HTMLFormElement.h"
-#include "KURL.h"
 #include "SecurityOrigin.h"
 #include "StringHash.h"
 #include "Timer.h"
