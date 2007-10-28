3951aeb3067564f7f7b1f239e2fc785329f37519
darin <darin@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-28 17:31:11 +0000

    * platform/graphics/svg/mac/SVGResourceFilterPlatformDataMac.h:         We don't include "config.h" in headers. Hope this desn't break         the non-SVG build again.
    git-svn-id: svn://svn.chromium.org/blink/trunk@27182 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/svg/mac/SVGResourceFilterPlatformDataMac.h
+++ third_party/WebKit/WebCore/platform/graphics/svg/mac/SVGResourceFilterPlatformDataMac.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SVGResourceFilterPlatformDataMac_h
 #define SVGResourceFilterPlatformDataMac_h
 
-#include "config.h"
 #if ENABLE(SVG) && ENABLE(SVG_EXPERIMENTAL_FEATURES)
 
 #include "SVGResourceFilter.h"
@@ -65,4 +64,3 @@ namespace WebCore {
 #endif // #if ENABLE(SVG) && ENABLE(SVG_EXPERIMENTAL_FEATURES)
 
 #endif // SVGResourceFilterPlatformDataMac_h
-
