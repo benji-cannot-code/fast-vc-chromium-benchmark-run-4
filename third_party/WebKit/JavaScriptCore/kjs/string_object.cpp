20ea3cb8e56f521abd145e274db6c7ae22749f04
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-29 03:00:16 +0000

    2007-10-28  Mark Rowe  <mrowe@apple.com>
            Gtk build fix.  Add include of MathExtras.h.
    
            * kjs/string_object.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/string_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/string_object.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "operations.h"
 #include "PropertyNameArray.h"
 #include "regexp_object.h"
+#include <wtf/MathExtras.h>
 #include <wtf/unicode/Unicode.h>
 
 #if PLATFORM(CF)
