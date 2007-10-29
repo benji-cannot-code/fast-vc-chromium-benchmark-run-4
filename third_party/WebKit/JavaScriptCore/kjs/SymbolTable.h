25dfbc3f6e92baef47e8d965218d7ae0c7918997
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-29 01:56:05 +0000

    2007-10-28  Mark Rowe  <mrowe@apple.com>
            Reviewed by Maciej.
    
            Fix "AllInOneFile.o has a global initializer in it".
    
            Some versions of gcc generate a global initializer for std::numeric_limits<size_t>::max().
            We can avoid this by moving it inside an inline function.
    
            * kjs/SymbolTable.h:
            (KJS::missingSymbolMarker):
            * kjs/function.cpp:
            (KJS::ActivationImp::getOwnPropertySlot):
            (KJS::ActivationImp::put):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/SymbolTable.h
+++ third_party/WebKit/JavaScriptCore/kjs/SymbolTable.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SymbolTable_h
 
 #include "property_map.h"
+#include "AlwaysInline.h"
 
 namespace KJS {
 
@@ -42,12 +43,12 @@ namespace KJS {
         static const bool safeToCompareToEmptyOrDeleted = true;
     };
 
-    static const size_t missingSymbolMarker = std::numeric_limits<size_t>::max();
+    static ALWAYS_INLINE size_t missingSymbolMarker() { return std::numeric_limits<size_t>::max(); }
 
     struct SymbolTableIndexHashTraits {
         typedef size_t TraitType;
         typedef SymbolTableIndexHashTraits StorageTraits;
-        static size_t emptyValue() { return missingSymbolMarker; }
+        static size_t emptyValue() { return missingSymbolMarker(); }
         static const bool emptyValueIsZero = false;
         static const bool needsDestruction = false;
         static const bool needsRef = false;
