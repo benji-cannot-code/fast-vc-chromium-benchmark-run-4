a58f816cc8fdb6d1922bc80fdac89e68ab8f4a82
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-29 01:29:48 +0000

    JavaScriptCore:
            Reviewed by Mark.
            
            - Added assertions to protect against adding empty or deleted keys to a HashTable
    
            * wtf/HashTable.h:
            (WTF::HashTable::lookup):
            (WTF::HashTable::lookupForWriting):
            (WTF::HashTable::fullLookupForWriting):
            (WTF::HashTable::add):
    
    WebCore:
    
            Reviewed by Mark.
    
            - fixed REGRESSION(r27176): Reproducible crash while trying to order dinner makes bdash sad
            http://bugs.webkit.org/show_bug.cgi?id=15731
    
            * bindings/js/kjs_window.cpp:
            (KJS::Window::installTimeout): Avoid putting in or accessing empty or deleted keys.
            (KJS::Window::clearTimeout): ditto
            * manual-tests/bad-clearTimeout-crash.html: Added. Automated test not possible.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/HashTable.h
+++ third_party/WebKit/JavaScriptCore/wtf/HashTable.h
@@ -403,6 +403,12 @@ namespace WTF {
     inline Value* HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits>::lookup(const T& key)
     {
         ASSERT(m_table);
+#ifndef ASSERT_DISABLED
+        if (HashFunctions::safeToCompareToEmptyOrDeleted) {
+            ASSERT(!HashTranslator::equal(KeyTraits::emptyValue(), key));
+            ASSERT(!HashTranslator::equal(KeyTraits::deletedValue(), key));
+        }
+#endif
 
         int k = 0;
         int sizeMask = m_tableSizeMask;
@@ -447,6 +453,12 @@ namespace WTF {
     inline typename HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits>::LookupType HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits>::lookupForWriting(const T& key)
     {
         ASSERT(m_table);
+#ifndef ASSERT_DISABLED
+        if (HashFunctions::safeToCompareToEmptyOrDeleted) {
+            ASSERT(!HashTranslator::equal(KeyTraits::emptyValue(), key));
+            ASSERT(!HashTranslator::equal(KeyTraits::deletedValue(), key));
+        }
+#endif
 
         int k = 0;
         ValueType* table = m_table;
@@ -498,6 +510,12 @@ namespace WTF {
     inline typename HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits>::FullLookupType HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits>::fullLookupForWriting(const T& key)
     {
         ASSERT(m_table);
+#ifndef ASSERT_DISABLED
+        if (HashFunctions::safeToCompareToEmptyOrDeleted) {
+            ASSERT(!HashTranslator::equal(KeyTraits::emptyValue(), key));
+            ASSERT(!HashTranslator::equal(KeyTraits::deletedValue(), key));
+        }
+#endif
 
         int k = 0;
         ValueType* table = m_table;
@@ -548,6 +566,13 @@ namespace WTF {
     template<typename T, typename Extra, typename HashTranslator>
     inline pair<typename HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits>::iterator, bool> HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits>::add(const T& key, const Extra& extra)
     {
+#ifndef ASSERT_DISABLED
+        if (HashFunctions::safeToCompareToEmptyOrDeleted) {
+            ASSERT(!HashTranslator::equal(KeyTraits::emptyValue(), key));
+            ASSERT(!HashTranslator::equal(KeyTraits::deletedValue(), key));
+        }
+#endif
+
         invalidateIterators();
 
         if (!m_table)
