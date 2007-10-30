940ad2e2c33df722d5d1aa059ed5958c3b2f89f4
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-30 02:18:09 +0000

    Not reviewed, build fix.
            - Include Vector.h in a way that actually works.
    
            * kjs/LocalStorage.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27249 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/LocalStorage.h
+++ third_party/WebKit/JavaScriptCore/kjs/LocalStorage.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef KJS_LOCAL_STORAGE_H
 #define KJS_LOCAL_STORAGE_H
 
-#include <Vector.h>
+#include <wtf/Vector.h>
 
 namespace KJS {
     class JSValue;
