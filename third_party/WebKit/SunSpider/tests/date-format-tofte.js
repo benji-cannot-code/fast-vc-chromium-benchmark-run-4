5d7c7d685388762cf2dab862dfee3ce9f722a3cc
mjs <mjs@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-30 01:37:42 +0000

    Reviewed by Darin.
            - fix Date tests to work in command-line SpiderMonkey
    
            * tests/date-format-tofte.js:
            * tests/date-format-xparb.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27243 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/SunSpider/tests/date-format-tofte.js
+++ third_party/WebKit/SunSpider/tests/date-format-tofte.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-Array.prototype.exists = function (x) {
-    for (var i = 0; i < this.length; i++) {
-        if (this[i] == x) return true;
+function arrayExists(array, x) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == x) return true;
     }
     return false;
 }
@@ -277,7 +277,7 @@ Date.prototype.formatDate = function (input,time) {
             // this is our way of allowing users to escape stuff
             ia.splice(ij,1);
         } else {
-            if (switches.exists(ia[ij])) {
+            if (arrayExists(switches,ia[ij])) {
                 ia[ij] = eval(ia[ij] + "()");
             }
         }
@@ -293,8 +293,8 @@ Date.prototype.formatDate = function (input,time) {
 var date = new Date("1/1/2007 1:11:11");
 
 for (i = 0; i < 500; ++i) {
-    var short = date.formatDate("Y-m-d");
-    var long = date.formatDate("l, F d, Y g:i:s A");
+    var shortFormat = date.formatDate("Y-m-d");
+    var longFormat = date.formatDate("l, F d, Y g:i:s A");
     date.setTime(date.getTime() + 84266956);
 }
 
