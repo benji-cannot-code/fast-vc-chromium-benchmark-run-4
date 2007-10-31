b05d8a95fe58a80d5356ddfb93243fa5e5866073
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-31 01:27:54 +0000

    WebCore:
            Reviewed by Darin Adler.
    
            <rdar://problem/5549929> CrashTracer: [USER] 35 crashes at WebCore::CharacterData::insertData
            
            We were trying to insert a tab into a br, after the br incorrectly ended up inside
            a tab span.
            
            * editing/DeleteButtonController.cpp: 
            (WebCore::isDeletableElement): Changed to take in a const Node* instead of a Node*.
            * editing/DeleteSelectionCommand.cpp:
            (WebCore::isTableRow): Ditto.
            * editing/IndentOutdentCommand.cpp:
            (WebCore::isIndentBlockquote): Ditto.
            (WebCore::isListOrIndentBlockquote): Ditto.
            * editing/InsertLineBreakCommand.cpp:
            (WebCore::InsertLineBreakCommand::shouldUseBreakElement): Added, moved code from
            doApply here.
            (WebCore::InsertLineBreakCommand::doApply): 
            Don't upstream() the insertion position.  upstream()ing it will only have an effect
            when the insertion position is the first in its paragraph (since we canonicalize
            VisiblePositions to the upstream() candidate).  In this start of paragraph case, 
            upstream() can move outside inline elements like tab spans or elements that might
            have a different whitespace mode (added two test cases to cover these).
            Moved code to decide whether to insert a br or a '\n' to its own method.
            Removed special case code for inserting at a position inside a tab span.  We instead
            adjust the insertion position before insertion if it is inside a tab span and
            handle insertion in the appropriate if-block.  This fixes a bug where we would 
            only insert one line break when two were needed (added a testcase).
            Removed special case code for inserting before and after tables and horizontal
            rules.  We handle these insertions in the appropriate if-block.
            * editing/InsertLineBreakCommand.h:
            * editing/ReplaceSelectionCommand.cpp:
            (WebCore::isMailPasteAsQuotationNode): Change to take in a const Node*.
            * editing/htmlediting.cpp:
            (WebCore::isContentEditable): Ditto.
            (WebCore::isBlock): Ditto.
            (WebCore::enclosingNodeOfType): Changed to take a function pointer to a function
            that takes in a const Node*.
            (WebCore::isTabSpanTextNode): Check to see that the node actually a text node,
            and not, say, a br.
            * editing/htmlediting.h:
            * editing/markup.cpp:
            (WebCore::styleFromMatchedRulesAndInlineDecl): Changed to take in a const Node*.
            (WebCore::elementHasTextDecorationProperty): Ditto.
    
    LayoutTests:
    
            Reviewed by Darin Adler.
    
            <rdar://problem/5549929> CrashTracer: [USER] 35 crashes at WebCore::CharacterData::insertData
    
            * editing/inserting/5549929-1-expected.txt: Added.
            * editing/inserting/5549929-1.html: Added.
            * editing/inserting/5549929-2.html: Added.
            * editing/inserting/5549929-3.html: Added.
            * platform/mac/editing/inserting/5549929-2-expected.checksum: Added.
            * platform/mac/editing/inserting/5549929-2-expected.png: Added.
            * platform/mac/editing/inserting/5549929-2-expected.txt: Added.
            * platform/mac/editing/inserting/5549929-3-expected.checksum: Added.
            * platform/mac/editing/inserting/5549929-3-expected.png: Added.
            * platform/mac/editing/inserting/5549929-3-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27280 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/DeleteButtonController.cpp
+++ third_party/WebKit/WebCore/editing/DeleteButtonController.cpp
@@ -62,7 +62,7 @@ DeleteButtonController::DeleteButtonController(Frame* frame)
 {
 }
 
-static bool isDeletableElement(Node* node)
+static bool isDeletableElement(const Node* node)
 {
     if (!node || !node->isHTMLElement() || !node->inDocument() || !node->isContentEditable())
         return false;
--- third_party/WebKit/WebCore/editing/htmlediting.cpp
+++ third_party/WebKit/WebCore/editing/htmlediting.cpp
@@ -191,7 +191,7 @@ Element* editableRootForPosition(const Position& p)
     return node->rootEditableElement();
 }
 
-bool isContentEditable(Node* node)
+bool isContentEditable(const Node* node)
 {
     return node->isContentEditable();
 }
@@ -287,7 +287,7 @@ VisiblePosition lastEditablePositionBeforePositionInRoot(const Position& positio
 }
 
 // Whether or not content before and after this node will collapse onto the same line as it.
-bool isBlock(Node* node)
+bool isBlock(const Node* node)
 {
     return node && node->renderer() && !node->renderer()->isInline();
 }
@@ -578,7 +578,7 @@ Node* enclosingNodeWithTag(Node* node, const QualifiedName& tagName)
     return 0;
 }
 
-Node* enclosingNodeOfType(Node* node, bool (*nodeIsOfType)(Node*))
+Node* enclosingNodeOfType(Node* node, bool (*nodeIsOfType)(const Node*))
 {
     if (!node)
         return 0;
@@ -787,7 +787,7 @@ bool isTabSpanNode(const Node *node)
 
 bool isTabSpanTextNode(const Node *node)
 {
-    return (node && node->parentNode() && isTabSpanNode(node->parentNode()));
+    return node && node->isTextNode() && node->parentNode() && isTabSpanNode(node->parentNode());
 }
 
 Node *tabSpanNode(const Node *node)
