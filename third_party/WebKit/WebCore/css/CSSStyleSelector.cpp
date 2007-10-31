04dfd2ab1564132ae598091838a66dc82f1cc50a
hyatt <hyatt@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-31 02:30:40 +0000

    Make sure CSS transforms can be animated using the CSS transition property.
            Reviewed by Dan and Antti
    
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::applyProperty):
            * page/AnimationController.cpp:
            (WebCore::blendFunc):
            (WebCore::ImplicitAnimation::animate):
            * rendering/Length.h:
            (WebCore::Length::blend):
            * rendering/RenderStyle.cpp:
            (WebCore::StyleTransformData::operator==):
            (WebCore::TransformOperations::operator==):
            (WebCore::blendLengths):
            (WebCore::ScaleTransformOperation::blend):
            (WebCore::RotateTransformOperation::blend):
            (WebCore::SkewTransformOperation::blend):
            (WebCore::TranslateTransformOperation::blend):
            (WebCore::MatrixTransformOperation::blend):
            * rendering/RenderStyle.h:
            (WebCore::TransformOperations::operator!=):
            (WebCore::TransformOperations::isEmpty):
            (WebCore::TransformOperations::size):
            (WebCore::TransformOperations::operator[]):
            (WebCore::TransformOperations::append):
            (WebCore::RenderStyle::transform):
            (WebCore::RenderStyle::setTransform):
            (WebCore::RenderStyle::initialTransform):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27283 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/WebCore/css/CSSStyleSelector.cpp
@@ -3869,7 +3869,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
     }
     case CSS_PROP__WEBKIT_TRANSFORM: {
         HANDLE_INHERIT_AND_INITIAL(transform, Transform);
-        Vector<RefPtr<TransformOperation> > operations;
+        TransformOperations operations;
         if (!value->isPrimitiveValue()) {
             CSSValueList* list = static_cast<CSSValueList*>(value);
             unsigned size = list->length();
