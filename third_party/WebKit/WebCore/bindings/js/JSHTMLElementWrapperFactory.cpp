04171b06171881e18b7e87aaf05ef768f4503c38
antti <antti@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-31 02:11:25 +0000

    Another Qt/GTK build fix.
            * bindings/js/JSHTMLElementWrapperFactory.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSHTMLElementWrapperFactory.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSHTMLElementWrapperFactory.cpp
@@ -85,7 +85,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSHTMLAnchorElement.h"
 #include "JSHTMLAppletElement.h"
 #include "JSHTMLAreaElement.h"
-#include "JSHTMLAudioElement.h"
 #include "JSHTMLBRElement.h"
 #include "JSHTMLBaseElement.h"
 #include "JSHTMLBaseFontElement.h"
@@ -129,7 +128,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSHTMLQuoteElement.h"
 #include "JSHTMLScriptElement.h"
 #include "JSHTMLSelectElement.h"
-#include "JSHTMLSourceElement.h"
 #include "JSHTMLStyleElement.h"
 #include "JSHTMLTableCaptionElement.h"
 #include "JSHTMLTableCellElement.h"
@@ -140,7 +138,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSHTMLTextAreaElement.h"
 #include "JSHTMLTitleElement.h"
 #include "JSHTMLUListElement.h"
+
+#if ENABLE(VIDEO)
+#include "JSHTMLAudioElement.h"
+#include "JSHTMLSourceElement.h"
 #include "JSHTMLVideoElement.h"
+#endif
 
 #include "kjs_html.h"
 
