e44449966697dbff0f0a19db7e8ed990886cc6a6
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-10-31 11:02:36 +0000

    Reviewed by NOBODY (qt buildfix)
    * kjs/nodes.h: include OwnPtr.h
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27309 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/nodes.h
+++ third_party/WebKit/JavaScriptCore/kjs/nodes.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "internal.h"
 #include "SymbolTable.h"
 #include <wtf/ListRefPtr.h>
+#include <wtf/OwnPtr.h>
 #include <wtf/Vector.h>
 
 #if PLATFORM(X86) && COMPILER(GCC)
