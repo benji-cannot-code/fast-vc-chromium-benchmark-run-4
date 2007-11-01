19c74600524d4a7580039d194166332eb76e4cb3
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-01 21:15:22 +0000

    Reviewed by Anders
            Renamed a flag inside of SQLiteTransaction and added an accessor (for future work)
    
            * platform/sql/SQLiteTransaction.cpp:
            (WebCore::SQLiteTransaction::SQLiteTransaction):
            (WebCore::SQLiteTransaction::~SQLiteTransaction):
            (WebCore::SQLiteTransaction::begin):
            (WebCore::SQLiteTransaction::commit):
            (WebCore::SQLiteTransaction::rollback):
            * platform/sql/SQLiteTransaction.h:
            (WebCore::SQLiteTransaction::inProgress):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27362 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/sql/SQLiteTransaction.cpp
+++ third_party/WebKit/WebCore/platform/sql/SQLiteTransaction.cpp
@@ -33,31 +33,31 @@ namespace WebCore {
 
 SQLiteTransaction::SQLiteTransaction(SQLiteDatabase& db)
     : m_db(db)
-    , m_began(false)
+    , m_inProgress(false)
 {
 }
 
 SQLiteTransaction::~SQLiteTransaction()
 {
-    if (m_began) 
+    if (m_inProgress) 
         rollback();
 }
     
 void SQLiteTransaction::begin()
 {
-    if (!m_began) {
+    if (!m_inProgress) {
         ASSERT(!m_db.m_transactionInProgress);
-        m_began = m_db.executeCommand("BEGIN;");
+        m_inProgress = m_db.executeCommand("BEGIN;");
         m_db.m_transactionInProgress = true;
     }
 }
 
 void SQLiteTransaction::commit()
 {
-    if (m_began) {
+    if (m_inProgress) {
         ASSERT(m_db.m_transactionInProgress);
         if (m_db.executeCommand("COMMIT;")) {
-            m_began = false;
+            m_inProgress = false;
             m_db.m_transactionInProgress = false;
         }
     }
@@ -65,10 +65,10 @@ void SQLiteTransaction::commit()
 
 void SQLiteTransaction::rollback()
 {
-    if (m_began) {
+    if (m_inProgress) {
         ASSERT(m_db.m_transactionInProgress);
         if (m_db.executeCommand("ROLLBACK;")) {
-            m_began = false;
+            m_inProgress = false;
             m_db.m_transactionInProgress = false;
         }
     }
--- third_party/WebKit/WebCore/platform/sql/SQLiteTransaction.h
+++ third_party/WebKit/WebCore/platform/sql/SQLiteTransaction.h
@@ -43,9 +43,10 @@ public:
     void commit();
     void rollback();
     
+    bool inProgress() const { return m_inProgress; }
 private:
     SQLiteDatabase& m_db;
-    bool m_began;
+    bool m_inProgress;
 
 };
 
