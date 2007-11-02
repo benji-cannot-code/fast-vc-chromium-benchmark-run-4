73dbe29360dd4ed63869f5a7bdf805909b6a0699
alp <alp@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-02 21:51:49 +0000

    Reviewed by Darin.
    Include Cairo headers properly
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27377 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/AffineTransform.h
+++ third_party/WebKit/WebCore/platform/graphics/AffineTransform.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif PLATFORM(QT)
 #include <QMatrix>
 #elif PLATFORM(CAIRO)
-#include "cairo.h"
+#include <cairo.h>
 #elif PLATFORM(WX) && USE(WXGC)
 #include <wx/defs.h>
 #include <wx/graphics.h>
--- third_party/WebKit/WebCore/platform/gtk/FontPlatformDataGtk.cpp
+++ third_party/WebKit/WebCore/platform/gtk/FontPlatformDataGtk.cpp
@@ -32,8 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CString.h"
 #include "PlatformString.h"
 #include "FontDescription.h"
-#include "cairo-ft.h"
-#include "cairo.h"
+
+#include <cairo-ft.h>
+#include <cairo.h>
 #include <fontconfig/fcfreetype.h>
 
 namespace WebCore {
