4fdc55b458c260de98461b38c4eb2eaeef3b5218
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-03 16:42:26 +0000

    2007-11-03  Mark Rowe  <mrowe@apple.com>
            Gtk build fix.
    
            * kjs/nodes.cpp:  Add missing include.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "function_object.h"
 #include "lexer.h"
 #include "operations.h"
+#include "regexp_object.h"
 #include <math.h>
 #include <wtf/Assertions.h>
 #include <wtf/HashCountedSet.h>
