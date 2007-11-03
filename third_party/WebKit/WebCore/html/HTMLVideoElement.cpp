8dc550154cce95bf9c889f7890a6b146840c5179
antti <antti@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-03 04:17:47 +0000

    WebCore:
            Reviewed by Adam.
            
            Add video width/height DOM and content attributes from latest HTML5 draft.
    
            Test: media/video-width-height.html
    
            * html/HTMLVideoElement.cpp:
            (WebCore::HTMLVideoElement::parseMappedAttribute):
            (WebCore::HTMLVideoElement::width):
            (WebCore::HTMLVideoElement::setWidth):
            (WebCore::HTMLVideoElement::height):
            (WebCore::HTMLVideoElement::setHeight):
            * html/HTMLVideoElement.h:
            * html/HTMLVideoElement.idl:
    
    LayoutTests:
    
            Reviewed by Adam.
            
            Test for video width/height DOM and content attributes. 
    
            * media/video-width-height-expected.txt: Added.
            * media/video-width-height.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27390 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLVideoElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLVideoElement.cpp
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(VIDEO)
 #include "HTMLVideoElement.h"
 
+#include "CSSHelper.h"
+#include "CSSPropertyNames.h"
 #include "HTMLNames.h"
 
 namespace WebCore {
@@ -38,6 +40,17 @@ using namespace HTMLNames;
 HTMLVideoElement::HTMLVideoElement(Document* doc)
     : HTMLMediaElement(HTMLNames::videoTag, doc)
 {
+}
+    
+void HTMLVideoElement::parseMappedAttribute(MappedAttribute* attr)
+{
+    const QualifiedName& attrName = attr->name();
+    if (attrName == widthAttr)
+        addCSSLength(attr, CSS_PROP_WIDTH, attr->value());
+    else if (attrName == heightAttr)
+        addCSSLength(attr, CSS_PROP_HEIGHT, attr->value());
+    else
+        HTMLMediaElement::parseMappedAttribute(attr);
 }
 
 int HTMLVideoElement::videoWidth() const
@@ -54,5 +67,29 @@ int HTMLVideoElement::videoHeight() const
     return m_movie->naturalSize().height();
 }
 
+int HTMLVideoElement::width() const
+{
+    bool ok;
+    int w = getAttribute(widthAttr).toInt(&ok);
+    return ok ? w : 0;
+}
+    
+void HTMLVideoElement::setWidth(int value)
+{
+    setAttribute(widthAttr, String::number(value));
+}
+    
+int HTMLVideoElement::height() const
+{
+    bool ok;
+    int h = getAttribute(heightAttr).toInt(&ok);
+    return ok ? h : 0;
+}
+    
+void HTMLVideoElement::setHeight(int value)
+{
+    setAttribute(heightAttr, String::number(value));
+}
+    
 }
 #endif
