c048d07c821d59c3661aebea03a6981e7c69f1e4
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-05 23:05:03 +0000

    2007-11-05  Mark Rowe  <mrowe@apple.com>
            Gtk build fix.
    
            * kjs/list.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/list.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/list.cpp
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "list.h"
 
+using std::min;
+
 namespace KJS {
 
 void List::getSlice(int startIndex, List& result) const
