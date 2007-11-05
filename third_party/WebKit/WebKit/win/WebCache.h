c492eb9be9fd27bbcaecccacf58dd59c2a721c0c
aroben <aroben@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-05 18:57:30 +0000

    Add IWebCache::disabled
             This matches the Mac WebCache class.
    
             Reviewed by Sam.
    
             * Interfaces/IWebCache.idl:
             * WebCache.cpp:
             (WebCache::disabled):
             * WebCache.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebCache.h
+++ third_party/WebKit/WebKit/win/WebCache.h
@@ -53,6 +53,9 @@ public:
     virtual HRESULT STDMETHODCALLTYPE setDisabled( 
         /* [in] */ BOOL disabled);
 
+    virtual HRESULT STDMETHODCALLTYPE disabled(
+        /* [out][retval] */ BOOL*);
+
 protected:
     ULONG m_refCount;
 };
