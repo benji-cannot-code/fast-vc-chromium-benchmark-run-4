4d6e03a9677af636b4518d0c7d0a4c0b93156aa8
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-06 18:43:38 +0000

    Reviewed by Adam and Steve.
            - added hooks needed to implement showModalDialog on Windows
    
            * Interfaces/IWebUIDelegate.idl: Added canRunModal, createModalDialog,
            and runModal functions to the end of IWebUIDelegate3.
    
            * WebChromeClient.h: Added uiDelegate and uiDelegate2 helper functions,
            so it's easier to write client functions.
            * WebChromeClient.cpp:
            (WebChromeClient::createWindow): Implemented dialog case. Calls IWebUIDelegate3.
            (WebChromeClient::canRunModal): Implemented. Calls IWebUIDelegate3.
            (WebChromeClient::runModal): Ditto.
            (WebChromeClient::uiDelegate): Added.
            (WebChromeClient::uiDelegate2): Added.
            (WebChromeClient::uiDelegate3): Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27481 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebChromeClient.h
+++ third_party/WebKit/WebKit/win/WebChromeClient.h
@@ -25,10 +25,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include <WebCore/ChromeClient.h>
+#include <WebCore/COMPtr.h>
 #include <WebCore/FocusDirection.h>
 #include <wtf/Forward.h>
 
 class WebView;
+
+interface IWebUIDelegate;
+interface IWebUIDelegate2;
+interface IWebUIDelegate3;
 
 class WebChromeClient : public WebCore::ChromeClient {
 public:
@@ -95,6 +100,11 @@ public:
     virtual void print(WebCore::Frame*);
 
     virtual bool runDatabaseSizeLimitPrompt(WebCore::Frame*, const WebCore::String& origin);
+
 private:
+    COMPtr<IWebUIDelegate> uiDelegate();
+    COMPtr<IWebUIDelegate2> uiDelegate2();
+    COMPtr<IWebUIDelegate3> uiDelegate3();
+
     WebView* m_webView;
 };
