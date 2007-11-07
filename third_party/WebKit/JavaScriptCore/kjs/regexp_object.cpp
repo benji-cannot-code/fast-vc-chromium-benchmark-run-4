8113c8592602c86dc4bc3100cc5edb80ec05d401
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 17:54:12 +0000

    2007-11-07  Mark Rowe  <mrowe@apple.com>
            Gtk build fix.
    
            * kjs/regexp_object.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/regexp_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/regexp_object.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "regexp.h"
 #include "types.h"
 #include "value.h"
+#include "UnusedParam.h"
 
 #include <stdio.h>
 
