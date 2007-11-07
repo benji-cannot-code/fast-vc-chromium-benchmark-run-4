1bb48cd2faf3f5f62485945716cee50a7c12d8e5
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 14:30:38 +0000

    Changed the webAction() accessor to not be a slot but just a public function.
    git-svn-id: svn://svn.chromium.org/blink/trunk@27547 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.h
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.h
@@ -167,6 +167,8 @@ public:
     quint64 totalBytes() const;
     quint64 bytesReceived() const;
 
+    QAction *webAction(WebAction action) const;
+
 public slots:
     /**
      * Stops loading of the page, if loading.
@@ -185,8 +187,6 @@ public slots:
     void copy();
     void paste();
     // ### should we have execCommand() or something similar?
-
-    QAction *webAction(WebAction action) const;
 
 signals:
     /**
