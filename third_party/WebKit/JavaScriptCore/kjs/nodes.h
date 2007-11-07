6ecbd65ae0c6996d35b3166c224171ce781aae7e
oliver <oliver@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 06:03:20 +0000

    Avoid unnecessarily boxing the result from post inc/decrement for 0.3% gain in sunspider
    Reviewed by Maciej
    
    We now convert the common 'for (...; ...; <var>++) ...' to the semantically identical
    'for (...; ...; ++<var>) ...'.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/nodes.h
+++ third_party/WebKit/JavaScriptCore/kjs/nodes.h
@@ -143,6 +143,9 @@ namespace KJS {
     // Used for iterative, depth-first traversal of the node tree. Does not cross function call boundaries.
     virtual void optimizeVariableAccess(FunctionBodyNode*, DeclarationStacks::NodeStack&) KJS_FAST_CALL { }
 
+    // Used to optimize those nodes that do extra work when returning a result, even if the result has no semantic relevance
+    virtual void optimizeForUnnecessaryResult() { }
+      
   protected:
     Completion createErrorCompletion(ExecState *, ErrorType, const char *msg) KJS_FAST_CALL;
     Completion createErrorCompletion(ExecState *, ErrorType, const char *msg, const Identifier &) KJS_FAST_CALL;
@@ -539,13 +542,27 @@ namespace KJS {
     RefPtr<ArgumentsNode> args;
   };
 
-  class PostIncResolveNode : public Node {
+  class PrePostResolveNode : public Node {
   public:
-    PostIncResolveNode(const Identifier& i) KJS_FAST_CALL : m_ident(i) {}
-
-    PostIncResolveNode(PlacementNewAdoptType) KJS_FAST_CALL 
+    PrePostResolveNode(const Identifier& i) KJS_FAST_CALL : m_ident(i) {}
+      
+    PrePostResolveNode(PlacementNewAdoptType) KJS_FAST_CALL 
         : Node(PlacementNewAdopt)
         , m_ident(PlacementNewAdopt)
+    {
+    }
+      
+  protected:
+      Identifier m_ident;
+      size_t m_index; // Used by LocalVarPostfixNode.      
+  };
+    
+  class PostIncResolveNode : public PrePostResolveNode {
+  public:
+    PostIncResolveNode(const Identifier& i) KJS_FAST_CALL : PrePostResolveNode(i) {}
+
+    PostIncResolveNode(PlacementNewAdoptType) KJS_FAST_CALL 
+        : PrePostResolveNode(PlacementNewAdopt)
     {
     }
 
@@ -553,10 +570,8 @@ namespace KJS {
     JSValue* evaluate(ExecState*) KJS_FAST_CALL;
     virtual void streamTo(SourceStream&) const KJS_FAST_CALL;
     virtual Precedence precedence() const { return PrecPostfix; }
+    virtual void optimizeForUnnecessaryResult();
 
-  protected:
-    Identifier m_ident;
-    size_t index; // Used by LocalVarPostfixNode.
   };
 
   class PostIncLocalVarNode : public PostIncResolveNode {
@@ -565,19 +580,19 @@ namespace KJS {
         : PostIncResolveNode(PlacementNewAdopt)
     {
         ASSERT(i != missingSymbolMarker());
-        index = i;
+        m_index = i;
     }
 
     JSValue* evaluate(ExecState*) KJS_FAST_CALL;
+    virtual void optimizeForUnnecessaryResult();
   };
 
-  class PostDecResolveNode : public Node {
+  class PostDecResolveNode : public PrePostResolveNode {
   public:
-    PostDecResolveNode(const Identifier& i) KJS_FAST_CALL : m_ident(i) {}
+    PostDecResolveNode(const Identifier& i) KJS_FAST_CALL : PrePostResolveNode(i) {}
 
     PostDecResolveNode(PlacementNewAdoptType) KJS_FAST_CALL 
-        : Node(PlacementNewAdopt)
-        , m_ident(PlacementNewAdopt)
+        : PrePostResolveNode(PlacementNewAdopt)
     {
     }
 
@@ -585,10 +600,7 @@ namespace KJS {
     JSValue* evaluate(ExecState*) KJS_FAST_CALL;
     virtual void streamTo(SourceStream&) const KJS_FAST_CALL;
     virtual Precedence precedence() const { return PrecPostfix; }
-
-  protected:
-    Identifier m_ident;
-    size_t index; // Used by LocalVarPostfixNode.
+    virtual void optimizeForUnnecessaryResult();
   };
 
   class PostDecLocalVarNode : public PostDecResolveNode {
@@ -597,10 +609,11 @@ namespace KJS {
         : PostDecResolveNode(PlacementNewAdopt)
     {
         ASSERT(i != missingSymbolMarker());
-        index = i;
+        m_index = i;
     }
 
     JSValue* evaluate(ExecState*) KJS_FAST_CALL;
+    virtual void optimizeForUnnecessaryResult();
   };
 
   class PostfixBracketNode : public Node {
@@ -792,16 +805,15 @@ namespace KJS {
     RefPtr<Node> m_expr;
   };
 
-  class PreIncResolveNode : public Node {
+  class PreIncResolveNode : public PrePostResolveNode {
   public:
     PreIncResolveNode(const Identifier &s) KJS_FAST_CALL 
-        : m_ident(s) 
+        : PrePostResolveNode(s) 
     { 
     }
     
     PreIncResolveNode(PlacementNewAdoptType) KJS_FAST_CALL 
-        : Node(PlacementNewAdopt)
-        , m_ident(PlacementNewAdopt) 
+        : PrePostResolveNode(PlacementNewAdopt)
     {
     }
     
@@ -810,10 +822,6 @@ namespace KJS {
     JSValue* evaluate(ExecState*) KJS_FAST_CALL;
     virtual void streamTo(SourceStream&) const KJS_FAST_CALL;
     virtual Precedence precedence() const { return PrecUnary; }
-
-  protected:
-    Identifier m_ident;
-    size_t m_index; // Used by LocalVarPrefixNode.
   };
 
   class PreIncLocalVarNode : public PreIncResolveNode {
@@ -828,16 +836,15 @@ namespace KJS {
     JSValue* evaluate(ExecState*) KJS_FAST_CALL;
   };
   
-  class PreDecResolveNode : public Node {
+  class PreDecResolveNode : public PrePostResolveNode {
   public:
     PreDecResolveNode(const Identifier &s) KJS_FAST_CALL 
-        : m_ident(s) 
+        : PrePostResolveNode(s) 
     { 
     }
     
     PreDecResolveNode(PlacementNewAdoptType) KJS_FAST_CALL 
-        : Node(PlacementNewAdopt)
-        , m_ident(PlacementNewAdopt) 
+        : PrePostResolveNode(PlacementNewAdopt)
     {
     }
     
@@ -846,10 +853,6 @@ namespace KJS {
     JSValue* evaluate(ExecState*) KJS_FAST_CALL;
     virtual void streamTo(SourceStream&) const KJS_FAST_CALL;
     virtual Precedence precedence() const { return PrecUnary; }
-
-  protected:
-    Identifier m_ident;
-    size_t m_index; // Used by LocalVarPrefixNode.
   };
 
   class PreDecLocalVarNode : public PreDecResolveNode {
@@ -1562,7 +1565,13 @@ namespace KJS {
   class ForNode : public StatementNode {
   public:
     ForNode(Node* e1, Node* e2, Node* e3, StatementNode* s) KJS_FAST_CALL :
-      expr1(e1), expr2(e2), expr3(e3), statement(s) { m_mayHaveDeclarations = true; }
+      expr1(e1), expr2(e2), expr3(e3), statement(s)
+    {
+        m_mayHaveDeclarations = true; 
+        if (expr3)
+            expr3->optimizeForUnnecessaryResult();
+    }
+
     virtual void optimizeVariableAccess(FunctionBodyNode*, DeclarationStacks::NodeStack&) KJS_FAST_CALL;
     virtual Completion execute(ExecState*) KJS_FAST_CALL;
     virtual void streamTo(SourceStream&) const KJS_FAST_CALL;
