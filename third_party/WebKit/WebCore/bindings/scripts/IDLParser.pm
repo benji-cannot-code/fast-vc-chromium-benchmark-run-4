ebf8359e982de832962d19646d5530f8a2dab16d
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 10:32:07 +0000

    Fix compilation on Windows with non-cygwin perl.
    git-svn-id: svn://svn.chromium.org/blink/trunk@27508 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/IDLParser.pm
+++ third_party/WebKit/WebCore/bindings/scripts/IDLParser.pm
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package IDLParser;
 
+use IPC::Open2;
 use IDLStructure;
 
 use constant MODE_UNDEF    => 0; # Default mode.
@@ -73,10 +74,10 @@ sub Parse
 
     print " | *** Starting to parse $fileName...\n |\n" unless $beQuiet;
 
-    open FILE, "-|", split(' ', $preprocessor), (map { "-D$_" } split(' ', $defines)), $fileName
-        or die "Could not open $fileName: $!";
-    my @documentContent = <FILE>;
-    close FILE;
+    open2(\*PP_OUT, \*PP_IN, split(' ', $preprocessor), (map { "-D$_" } split(' ', $defines)), $fileName);
+    close PP_IN;
+    my @documentContent = <PP_OUT>;
+    close PP_OUT;
 
     my $dataAvailable = 0;
 
