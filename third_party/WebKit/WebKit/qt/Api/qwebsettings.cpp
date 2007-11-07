c183d381f7cf037c8c584b2684633a11fa1175ba
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 14:30:43 +0000

    Changed the icondatabase accessor to be a static function because it doesn't change the QWebSettings object.
    git-svn-id: svn://svn.chromium.org/blink/trunk@27548 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebsettings.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebsettings.cpp
@@ -147,7 +147,7 @@ void QWebSettings::setIconDatabaseEnabled(bool enabled, const QString &location)
     }
 }
 
-bool QWebSettings::iconDatabaseEnabled() const
+bool QWebSettings::iconDatabaseEnabled()
 {
     return WebCore::iconDatabase()->isEnabled() && WebCore::iconDatabase()->isOpen();
 }
