2d544af9c54dfe07a831844ab927e664965690b5
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 15:52:09 +0000

    For safety provide a default constructor for WindowFeatures(). ContextMenuController.cpp: createNewWindow as well as QWebPage need to create a default initialized WindowFeatures object on the fly.
    git-svn-id: svn://svn.chromium.org/blink/trunk@27568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/WindowFeatures.h
+++ third_party/WebKit/WebCore/bridge/WindowFeatures.h
@@ -30,6 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 struct WindowFeatures {
+    inline WindowFeatures()
+        : xSet(false), ySet(false), widthSet(false), heightSet(false),
+          menuBarVisible(true), statusBarVisible(true), toolBarVisible(true),
+          locationBarVisible(true), scrollbarsVisible(true), resizable(true),
+          fullscreen(false), dialog(false)
+    {}
+
     float x;
     bool xSet;
     float y;
