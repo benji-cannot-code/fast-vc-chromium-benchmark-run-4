8d49aabe8dd2b1a794d9af72f25ed6817504ca93
justing <justing@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 01:13:47 +0000

    WebCore:
            Reviewed by Dan Bernstein.
            
            <rdar://problem/5583387> ASSERTION FAILED: !refChild->hasTagName(bodyTag) when pasting newline in plain text into rich text Mail
            <rdar://problem/5583362> REGRESSION (5523.10.3-TOT): Newlines stripped when pasting plain text in Mail
    
            * editing/markup.cpp:
            (WebCore::createFragmentFromText): Put paragraphs of text into clones of the block
            that encloses the input context, unless that block is the body, which shouldn't
            be cloned.  In that case, use regular divs, as we did before r27369.
    
    LayoutTests:
    
            Reviewed by Dan Bernstein.
    
            <rdar://problem/5583387> ASSERTION FAILED: !refChild->hasTagName(bodyTag) when pasting newline in plain text into rich text Mail
            <rdar://problem/5583362> REGRESSION (5523.10.3-TOT): Newlines stripped when pasting plain text in Mail
    
            * editing/pasteboard/5583362.html: Added.
            * editing/resources/plaintext-pasteboard-data.dat: Added.
            * platform/mac/editing/pasteboard/5583362-expected.checksum: Added.
            * platform/mac/editing/pasteboard/5583362-expected.png: Added.
            * platform/mac/editing/pasteboard/5583362-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/markup.cpp
+++ third_party/WebKit/WebCore/editing/markup.cpp
@@ -972,8 +972,6 @@ PassRefPtr<DocumentFragment> createFragmentFromText(Range* context, const String
 {
     if (!context)
         return 0;
-        
-    Node* block = enclosingBlock(context->startNode());
 
     Node* styleNode = context->startNode();
     if (!styleNode) {
@@ -1015,6 +1013,9 @@ PassRefPtr<DocumentFragment> createFragmentFromText(Range* context, const String
     }
 
     // Break string into paragraphs. Extra line breaks turn into empty paragraphs.
+    Node* block = enclosingBlock(context->startNode());
+    bool useClonesOfEnclosingBlock = !block->hasTagName(bodyTag);
+    
     DeprecatedStringList list = DeprecatedStringList::split('\n', string, true); // true gets us empty strings in the list
     while (!list.isEmpty()) {
         DeprecatedString s = list.first();
@@ -1027,7 +1028,7 @@ PassRefPtr<DocumentFragment> createFragmentFromText(Range* context, const String
             ASSERT(ec == 0);
             element->setAttribute(classAttr, AppleInterchangeNewline);            
         } else {
-            element = static_cast<Element *>(block->cloneNode(false).get());
+            element = useClonesOfEnclosingBlock ? static_cast<Element*>(block->cloneNode(false).get()) : createDefaultParagraphElement(document);
             fillContainerFromString(element.get(), s);
         }
         fragment->appendChild(element.release(), ec);
