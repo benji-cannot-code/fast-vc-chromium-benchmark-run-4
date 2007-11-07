362998e3a4590f1e1fda0343e4fca90df5825671
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 14:32:16 +0000

    Changed QWebPageHistory::goToItem to take a value instead of a pointer.
    git-svn-id: svn://svn.chromium.org/blink/trunk@27564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpagehistory.h
+++ third_party/WebKit/WebKit/qt/Api/qwebpagehistory.h
@@ -131,7 +131,7 @@ public:
 
     void goBack();
     void goForward();
-    void goToItem(QWebHistoryItem *item);
+    void goToItem(const QWebHistoryItem &item);
 
     QWebHistoryItem backItem() const;
     QWebHistoryItem currentItem() const;
