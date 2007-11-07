980c5027ec94d3503d572ee896b77f6923bb26ff
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 13:45:44 +0000

    Implement Pasteboard::writeURL() * The URL currently gets written as text/plain and text/uri-list.   The win and mac port have some more types which we currently do   not support. When supporting them we can use the 'titleString'   as well. * As with writeSelection we only copy into the Clipboard. We could   consider copying into the Selection as well.
    Signed-off-by: Lars Knoll <lars@trolltech.com>
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27517 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/PasteboardQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/PasteboardQt.cpp
@@ -1,6 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006 Zack Rusin <zack@kde.org>
+ * Copyright (C) 2006, 2007 Apple Inc.  All rights reserved.
+ * Copyright (C) 2007 Trolltech ASA
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qclipboard.h>
 #include <qmimedata.h>
 #include <qapplication.h>
+#include <qurl.h>
 
 #define methodDebug() qDebug() << "PasteboardQt: " << __FUNCTION__;
 
@@ -97,9 +100,15 @@ PassRefPtr<DocumentFragment> Pasteboard::documentFragment(Frame* frame, PassRefP
     return 0;
 }
 
-void Pasteboard::writeURL(const KURL&, const String&, Frame*)
+void Pasteboard::writeURL(const KURL& _url, const String&, Frame*)
 {
-    notImplemented();
+    ASSERT(!_url.isEmpty());
+
+    QMimeData* md = new QMimeData;
+    QString url = _url.url();
+    md->setText(url);
+    md->setUrls(QList<QUrl>() << QUrl(url));
+    QApplication::clipboard()->setMimeData(md, QClipboard::Clipboard);
 }
 
 void Pasteboard::writeImage(Node*, const KURL&, const String&)
