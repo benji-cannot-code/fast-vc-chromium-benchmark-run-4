7d4b997acd8916bdf9198561c41ba38fc41b24f4
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 17:32:11 +0000

    Reviewed by Beth Dakin.
            Eliminated a bogus (though compiled-out) branch in the collector.
    
            * kjs/collector.cpp:
            (KJS::Collector::heapAllocate):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/collector.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/collector.cpp
@@ -218,7 +218,7 @@ template <Collector::HeapType heapType> void* Collector::heapAllocate(size_t s)
   if (heapType == PrimaryHeap && heap.extraCost > ALLOCATIONS_PER_COLLECTION) {
       size_t numLiveObjectsAtLastCollect = heap.numLiveObjectsAtLastCollect;
       size_t numNewObjects = numLiveObjects - numLiveObjectsAtLastCollect;
-      const size_t newCost = heapType == PrimaryHeap ? numNewObjects + heap.extraCost : numNewObjects;
+      const size_t newCost = heapType == numNewObjects + heap.extraCost;
       if (newCost >= ALLOCATIONS_PER_COLLECTION && newCost >= numLiveObjectsAtLastCollect)
           goto collect;
   }
@@ -245,6 +245,7 @@ scan:
     }
     heap.firstBlockWithPossibleSpace = i;
   } else {
+
 collect:
     size_t numLiveObjectsAtLastCollect = heap.numLiveObjectsAtLastCollect;
     size_t numNewObjects = numLiveObjects - numLiveObjectsAtLastCollect;
