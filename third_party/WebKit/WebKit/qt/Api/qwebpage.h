841ce904fdd143ed172f652478ccb23659037833
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-07 14:31:26 +0000

    Removed a bunch of slots/functions that are now available through the new actions API.
    git-svn-id: svn://svn.chromium.org/blink/trunk@27555 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.h
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.h
@@ -157,11 +157,6 @@ public:
     QNetworkProxy networkProxy() const;
 #endif
 
-    // #### remove after adding editor actions
-    bool canCut() const;
-    bool canCopy() const;
-    bool canPaste() const;
-
     quint64 totalBytes() const;
     quint64 bytesReceived() const;
 
@@ -175,17 +170,8 @@ public slots:
      */
     void stop();
 
-    // #### should these be actions?
-    void goBack();
-    void goForward();
-    void goToHistoryItem(const QWebHistoryItem &item);
-
     // ### should be a signal: void geometryChangeRequest(const QRect &geom);
     virtual void setWindowGeometry(const QRect& geom);
-
-    void cut();
-    void copy();
-    void paste();
 
 signals:
     /**
