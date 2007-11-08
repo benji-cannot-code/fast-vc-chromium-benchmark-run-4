2a19e0a647679054956848a582abfa14156eb82f
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-08 19:49:23 +0000

    2007-11-08  Mark Rowe  <mrowe@apple.com>
            Build fix for JavaScriptGlue.
    
            * wtf/MathExtras.h: Include stdlib.h for srand and RAND_MAX.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27605 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/MathExtras.h
+++ third_party/WebKit/JavaScriptCore/wtf/MathExtras.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_MathExtras_h
 
 #include <math.h>
+#include <stdlib.h>
 #include <time.h>
 
 #if COMPILER(MSVC)
