51cf78039806899c2b132cbc9a0fc9cc922c55dc
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-08 20:22:38 +0000

    Windows build fix
            Reviewed by Sam and Ada.
    
            * wtf/MathExtras.h: Get rid of a circular #include dependency to fix
            the build.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27607 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/MathExtras.h
+++ third_party/WebKit/JavaScriptCore/wtf/MathExtras.h
@@ -34,13 +34,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if COMPILER(MSVC)
 
 #include "kjs/operations.h"
-#include "kjs/value.h"
 #include <xmath.h>
 #include <limits>
 
 #if HAVE(FLOAT_H)
 #include <float.h>
 #endif
+
+namespace KJS {
+    extern const double NaN;
+}
 
 #endif
 
