569b0c8136112363b01c00b7efd419fe54df1e67
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-08 15:42:46 +0000

    WebCore:
            Reviewed by Adam.
    
            - fix <rdar://problem/5552943> accesskey does not focus <button> elements
    
            Test: fast/forms/access-key.html
    
            * html/HTMLButtonElement.cpp: (WebCore::HTMLButtonElement::accessKeyAction):
            Added a call to focus.
    
    LayoutTests:
    
            Reviewed by Adam.
    
            - test changes for <rdar://problem/5552943> accesskey does not focus <button> elements
    
            * fast/forms/access-key.html: Check for both focus and click events on all elements, but
            resisted the urge to switch to the American spelling for "focused".
    
            * fast/forms/access-key-expected.txt: Updated to reflect the bug fix ("1 button focussed"),
            the fact that <input type=button>, <input type=checkbox>, <input type=submit>, and
            <input type=reset> are all both focused and clicked ("2 input type button clicked",
            "3 input type checkbox clicked", "5 input type submit focussed", and
            "6 input type reset focussed"). Unfortunately this now also demonstrates that we don't
            send a click to <input type=text> and Gecko does. And there are still quite a few types
            that are not covered.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLButtonElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLButtonElement.cpp
@@ -139,6 +139,7 @@ bool HTMLButtonElement::appendFormData(FormDataList& formData, bool)
 
 void HTMLButtonElement::accessKeyAction(bool sendToAnyElement)
 {   
+    focus();
     // send the mouse button events iff the caller specified sendToAnyElement
     dispatchSimulatedClick(0, sendToAnyElement);
 }
