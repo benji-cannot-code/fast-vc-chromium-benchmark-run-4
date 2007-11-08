513178984028ca6144b0a04fb3f8f7450914f286
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-08 16:50:03 +0000

    2007-11-08  Mark Rowe  <mrowe@apple.com>
            Not reviewed.  Fix two instances of includes using the wrong case in the filename.
    
            * platform/graphics/mac/MoviePrivateQTKit.mm:
            * platform/wx/MimeTypeRegistryWx.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/MoviePrivateQTKit.mm
+++ third_party/WebKit/WebCore/platform/graphics/mac/MoviePrivateQTKit.mm
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "IntRect.h"
 #import "KURL.h"
 #import <limits>
-#import "MimeTypeRegistry.h"
+#import "MIMETypeRegistry.h"
 #import "Movie.h"
 #import <QTKit/QTKit.h>
 #import "ScrollView.h"
