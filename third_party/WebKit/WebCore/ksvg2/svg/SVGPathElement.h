5b354db7d690835b49cf374fe7f62be03e1de361
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-08 08:03:19 +0000

    2007-11-08  Eric Seidel  <eric@webkit.org>
            No review.
    
            * ksvg2/svg/SVGPathElement.h: remove old comment
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGPathElement.h
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGPathElement.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGAnimatedPathData.h"
 #include "SVGExternalResourcesRequired.h"
 #include "SVGLangSpace.h"
-//#include "SVGParserUtilities.h"
 #include "SVGStyledTransformableElement.h"
 #include "SVGTests.h"
 
