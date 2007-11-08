5100eed48881527542e4464e20414067e9b8697a
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-08 17:18:22 +0000

    2007-11-08  Mark Rowe  <mrowe@apple.com>
            Fix the Gtk, Qt and Wx builds.
    
            * platform/gtk/TemporaryLinkStubs.cpp:
            * platform/qt/TemporaryLinkStubs.cpp:
            * platform/wx/TemporaryLinkStubs.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/gtk/TemporaryLinkStubs.cpp
@@ -52,8 +52,6 @@ Vector<char> loadResourceIntoArray(const char* resourceName)
     return resource;
 }
 
-void FrameView::updateBorder() { notImplemented(); }
-
 
 /********************************************************/
 /* Completely empty stubs (mostly to allow DRT to run): */
--- third_party/WebKit/WebCore/platform/qt/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/qt/TemporaryLinkStubs.cpp
@@ -69,8 +69,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 
-void FrameView::updateBorder() { notImplemented(); }
-
 namespace WebCore {
 
 Vector<String> supportedKeySizes() { notImplemented(); return Vector<String>(); }
