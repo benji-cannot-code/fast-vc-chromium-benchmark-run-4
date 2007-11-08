e51addbe6dfca4892b4f50fdf79eb2ed98df07a9
hausmann <hausmann@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-08 08:20:33 +0000

    Implement WebCore::callOnMainThread
    Implemented using a global QObject, that is moved to the main thread
    and then sending a custom event to it.
    Picked a number below QEvent::User but above any other documented value.
    
    Signed-off-by: Lars Knoll <lars@trolltech.com>
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27593 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/ThreadingQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/ThreadingQt.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2007 Staikos Computing Services Inc.
+ * Copyright (C) 2007 Trolltech ASA
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,14 +28,57 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "NotImplemented.h"
+#include <QtCore/QObject>
+#include <QtCore/QCoreApplication>
+
 
 namespace WebCore {
 
-void callOnMainThread(void (*f)()) {
-    notImplemented();
+class PerformFunctionEvent : public QEvent {
+public:
+    static const int EventType = 723;
+
+    PerformFunctionEvent(void (*_function)());
+    void (*function)();
+};
+
+class MainThreadInvoker : public QObject {
+    Q_OBJECT
+public:
+    MainThreadInvoker();
+
+protected:
+    bool event(QEvent*);
+};
+
+PerformFunctionEvent::PerformFunctionEvent(void (*_function)())
+    : QEvent(static_cast<QEvent::Type>(EventType))
+    , function(_function)
+{}
+
+MainThreadInvoker::MainThreadInvoker()
+{
+    moveToThread(QCoreApplication::instance()->thread());
 }
 
+bool MainThreadInvoker::event(QEvent* event)
+{
+    if (event->type() == PerformFunctionEvent::EventType)
+        static_cast<PerformFunctionEvent*>(event)->function();
+
+    return QObject::event(event);
+}
+
+Q_GLOBAL_STATIC(MainThreadInvoker, webkit_main_thread_invoker)
+
+
+void callOnMainThread(void (*functionToPerform)()) {
+    if (!functionToPerform)
+        return;
+
+    QCoreApplication::postEvent(webkit_main_thread_invoker(), new PerformFunctionEvent(functionToPerform));
+}
 
 }
 
+#include "ThreadingQt.moc"
