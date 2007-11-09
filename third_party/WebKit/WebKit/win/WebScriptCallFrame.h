a08db6b7e19c63a7ef639c86cbad31a3bf584faa
kmccullo <kmccullo@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-09 08:29:20 +0000

    Reviewed by Adam.
            - This patch does two main things.
            1) It adds pragma warning guards around WebCore includes in WebKit files
            that were previously overlooked.
            2) It implements almost the entireity of WebScriptDebugger.  Only one
            function remains and that implementation is dependent on finishing the
            implementation of WebScriptScope.
    
            * WebScriptCallFrame.h:
            * WebScriptDebugServer.h:
            * WebScriptDebugger.cpp:
            (WebScriptDebugger::WebScriptDebugger):
            (WebScriptDebugger::sourceParsed):
            (WebScriptDebugger::callEvent):
            (WebScriptDebugger::atStatement):
            (WebScriptDebugger::returnEvent):
            (WebScriptDebugger::exception):
            (WebScriptDebugger::enterFrame):
            (WebScriptDebugger::leaveFrame):
            * WebScriptDebugger.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27640 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebScriptCallFrame.h
+++ third_party/WebKit/WebKit/win/WebScriptCallFrame.h
@@ -33,7 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IWebScriptCallFrame.h"
 
 #include <JavaScriptCore/ExecState.h>
+#pragma warning(push, 0)
 #include <WebCore/COMPtr.h>
+#pragma warning(pop)
 
 class WebScriptCallFrame : public IWebScriptCallFrame {
 public:
--- third_party/WebKit/WebKit/win/WebScriptDebugServer.h
+++ third_party/WebKit/WebKit/win/WebScriptDebugServer.h
@@ -31,7 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "IWebScriptDebugListener.h"
 #include <wtf/HashSet.h>
+#pragma warning(push, 0)
 #include <WebCore/COMPtr.h>
+#pragma warning(pop)
 
 interface IWebView;
 
