c47b4d52cc8f4fda7e8f03750bbfa393d36e5734
sfalken <sfalken@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-09 02:03:40 +0000

    Bump versions for submit
    git-svn-id: svn://svn.chromium.org/blink/trunk@27628 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/ProgIDMacros.h
+++ third_party/WebKit/WebKit/win/ProgIDMacros.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define STRINGIFIED_VERSION(version) STRINGIFY(version)
 #define STRINGIFY(s) L###s
 
-#define CURRENT_PROGID_VERSION 523
+#define CURRENT_PROGID_VERSION 525
 #define VERSION_INDEPENDENT_PRODUCTION_PROGID(className) L##"WebKit." L###className
 #define VERSION_INDEPENDENT_OPENSOURCE_PROGID(className) L##"OpenSource" VERSION_INDEPENDENT_PRODUCTION_PROGID(className)
 #define VERSIONED_PROGID(versionIndependentProgID, version) versionIndependentProgID L##"." STRINGIFIED_VERSION(version)
