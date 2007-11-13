33a2441140e6bd54619e59008efe8b0ebb279c75
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-13 09:49:50 +0000

    2007-11-13  Alp Toker  <alp@atoker.com>
            Build fix for breakage to non-Mac builds introduced in r27746.
    
            * kjs/ustring.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/ustring.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/ustring.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/ASCIICType.h>
 #include <wtf/MathExtras.h>
 #include <wtf/Vector.h>
+#include <wtf/unicode/UTF8.h>
 
 #if HAVE(STRING_H)
 #include <string.h>
@@ -50,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using namespace WTF;
+using namespace WTF::Unicode;
 using namespace std;
 
 namespace KJS {
