3924a383e77490ba09653cb44a0cb661c9886c98
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-14 03:04:11 +0000

    Build fix: changed Shared to RefCounted.
            * WebCoreSupport/ChromeClientQt.h:
            * WebCoreSupport/ContextMenuClientQt.cpp:
            * WebCoreSupport/ContextMenuClientQt.h:
            * WebCoreSupport/EditorClientQt.h:
            * WebCoreSupport/FrameLoaderClientQt.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27780 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/ContextMenuClientQt.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/ContextMenuClientQt.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ContextMenu.h"
 #include "HitTestResult.h"
 #include "KURL.h"
-#include "Shared.h"
+#include "RefCounted.h"
 #include "NotImplemented.h"
 
 #include <stdio.h>
--- third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KURL.h"
 #include "Frame.h"
 #include "FrameLoader.h"
-#include "Shared.h"
+#include "RefCounted.h"
 #include "ResourceResponse.h"
 class QWebFrame;
 
