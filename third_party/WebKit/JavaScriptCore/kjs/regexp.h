10bcfb19fe18dd374c672df4cbfc55dcdad44cd4
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-14 00:58:41 +0000

    Build fix
            Reviewed by Geoff.
    
            * kjs/regexp.h: Added a missing #include.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/regexp.h
+++ third_party/WebKit/JavaScriptCore/kjs/regexp.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <pcre.h>
 #include <sys/types.h>
 #include <wtf/OwnArrayPtr.h>
+#include <wtf/Shared.h>
 
 namespace KJS {
 
