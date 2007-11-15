9871f23a77957734948505c2b2d20d684442d2a3
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-15 19:49:25 +0000

    Reviewed by Brady.
            Get rid of SQLiteStatement::bindText and rename bindText16 to bindText.
            Remove the copy parameter from bindText and bindBlob and always copy passed in data instead.
    
            * loader/icon/IconDatabase.cpp:
            (WebCore::IconDatabase::setIconIDForPageURLInSQLDatabase):
            (WebCore::IconDatabase::removePageURLFromSQLDatabase):
            (WebCore::IconDatabase::getIconIDForIconURLFromSQLDatabase):
            (WebCore::IconDatabase::addIconURLToSQLDatabase):
            (WebCore::IconDatabase::getImageDataForIconURLFromSQLDatabase):
            (WebCore::IconDatabase::writeIconSnapshotToSQLDatabase):
            * platform/sql/SQLiteStatement.cpp:
            (WebCore::SQLiteStatement::prepare):
            * platform/sql/SQLiteStatement.h:
            * storage/Database.cpp:
            (WebCore::setTextValueInDatabase):
            * storage/DatabaseTracker.cpp:
            (WebCore::DatabaseTracker::fullPathForDatabase):
            (WebCore::DatabaseTracker::databaseNamesForOrigin):
            (WebCore::DatabaseTracker::addDatabase):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27813 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/Database.cpp
+++ third_party/WebKit/WebCore/storage/Database.cpp
@@ -222,7 +222,7 @@ static bool setTextValueInDatabase(SQLiteDatabase& db, const String& query, cons
         return false;
     }
 
-    statement.bindText16(1, value);
+    statement.bindText(1, value);
 
     result = statement.step();
     if (result != SQLResultDone) {
