0e653d0607ad866020e84c5c3c0008481183baaa
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-16 11:20:56 +0000

    Reviewed by Eric.
    Fixes: http://bugs.webkit.org/show_bug.cgi?id=12290
    
    Implement all SVGTextContentElement DOM methods.
    This is the last missing SVG text feature. SVG fonts is next.
    
    Added testcase: svg/custom/text-dom-01-f.svg (tests all new methods)
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/ksvg2/svg/SVGTextContentElement.h
+++ third_party/WebKit/WebCore/ksvg2/svg/SVGTextContentElement.h
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifndef SVGTextContentElement_h
 #define SVGTextContentElement_h
-#if ENABLE(SVG)
 
+#if ENABLE(SVG)
 #include "SVGExternalResourcesRequired.h"
 #include "SVGLangSpace.h"
 #include "SVGStyledElement.h"
@@ -54,13 +54,13 @@ namespace WebCore {
         // 'SVGTextContentElement' functions
         long getNumberOfChars() const;
         float getComputedTextLength() const;
-        float getSubStringLength(unsigned long charnum, unsigned long nchars, ExceptionCode&) const;
-        FloatPoint getStartPositionOfChar(unsigned long charnum, ExceptionCode&) const;
-        FloatPoint getEndPositionOfChar(unsigned long charnum, ExceptionCode&) const;
-        FloatRect getExtentOfChar(unsigned long charnum, ExceptionCode&) const;
-        float getRotationOfChar(unsigned long charnum, ExceptionCode&) const;
+        float getSubStringLength(long charnum, unsigned long nchars, ExceptionCode&) const;
+        FloatPoint getStartPositionOfChar(long charnum, ExceptionCode&) const;
+        FloatPoint getEndPositionOfChar(long charnum, ExceptionCode&) const;
+        FloatRect getExtentOfChar(long charnum, ExceptionCode&) const;
+        float getRotationOfChar(long charnum, ExceptionCode&) const;
         long getCharNumAtPosition(const FloatPoint&) const;
-        void selectSubString(unsigned long charnum, unsigned long nchars, ExceptionCode&) const;
+        void selectSubString(long charnum, long nchars, ExceptionCode&) const;
 
         virtual void parseMappedAttribute(MappedAttribute*);
 
