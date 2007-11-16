2efc5b44620d2a8e1bc08d4eb412790da910f95d
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-16 22:27:03 +0000

    2007-11-16  Doug Turner  <dougt@meer.net>
            Reviewed by Alp.
    
            http://bugs.webkit.org/show_bug.cgi?id=16018
            build bustage when building on debian 4.0
    
            Fix build bustage on GTK+ with older versions of Pango.
    
            Don't cache the return value since the docs say it can change.
    
            * platform/gtk/Language.cpp:
            (WebCore::defaultLanguage):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/Language.cpp
+++ third_party/WebKit/WebCore/platform/gtk/Language.cpp
@@ -30,21 +30,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CString.h"
 #include "PlatformString.h"
 
+#include <gtk/gtk.h>
 #include <pango/pango.h>
 
 namespace WebCore {
 
 String defaultLanguage()
 {
-    static String defaultLanguage;
-
-    if (!defaultLanguage.isEmpty())
-        return defaultLanguage;
-
-    PangoLanguage* pangoLanguage = pango_language_get_default();
-    defaultLanguage = String(pango_language_to_string(pangoLanguage));
-
-    return defaultLanguage;
+    return pango_language_to_string(gtk_get_default_language());
 }
 
 }
