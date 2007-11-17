4acaa5d8d50f5732bfdc0aa0ef39da573bc55ae6
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-17 08:58:24 +0000

    Reviewed by Darin.
            http://bugs.webkit.org/show_bug.cgi?id=15969
            Eliminate Editor::deleteRange()
    
            No change in functionality.
    
    WebCore:
            * editing/CompositeEditCommand.h: Removed unimplemented deleteKeyPressed().
    
            * editing/Editor.cpp:
            (WebCore::Editor::deleteWithDirection):
            (WebCore::Editor::cut):
            (WebCore::Editor::performDelete):
            * editing/Editor.h:
            Moved relevant Editor::deleteRange() functionality to its callers.
            Removed deleteSelectionWithSmartDelete(void), which was more confusing than helpful
            in my opinion.
    
            * editing/mac/EditorMac.mm:
            (WebCore::initializeKillRingIfNeeded):
            (WebCore::Editor::addToKillRing):
            (WebCore::Editor::yank):
            (WebCore::Editor::yankAndSelect):
            (WebCore::Editor::setMark):
            (WebCore::unionDOMRanges):
            (WebCore::Editor::deleteToMark):
            (WebCore::Editor::selectToMark):
            (WebCore::Editor::swapWithMark):
            Pushed kill ring handling down from WebCore.
    
            * page/mac/WebCoreFrameBridge.h:
            * page/mac/WebCoreFrameBridge.mm:
            Removed setMarkDOMRange (the corresponding getter is still needed).
    
            * WebCore.base.exp: No longer need to export Editor::deleteRange() and Editor::insertTextWithoutSendingTextEvent().
            Export the new kill ring functions.
    
    WebKit:
            * WebView/WebHTMLView.mm:
            (+[WebHTMLView initialize]):
            (-[WebHTMLView yank:]):
            (-[WebHTMLView yankAndSelect:]):
            (-[WebHTMLView setMark:]):
            (-[WebHTMLView deleteToMark:]):
            (-[WebHTMLView selectToMark:]):
            (-[WebHTMLView swapWithMark:]):
            Pushed all kill ring methods to WebCore. They were guilty of using Editor::deleteRange()!
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27873 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/mac/WebCoreFrameBridge.h
+++ third_party/WebKit/WebCore/page/mac/WebCoreFrameBridge.h
@@ -166,7 +166,6 @@ enum WebScrollGranularity {
 - (void)scrollDOMRangeToVisible:(DOMRange *)range;
 
 // Emacs-style-editing "mark"
-- (void)setMarkDOMRange:(DOMRange *)range;
 - (DOMRange *)markDOMRange;
 
 - (NSFont *)fontForSelection:(BOOL *)hasMultipleFonts;
