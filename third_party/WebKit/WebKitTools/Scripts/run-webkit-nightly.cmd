331b846c0e606480eb9586e176e9aa77301ea59d
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-17 20:49:33 +0000

    Make it easy to run Safari in the debugger on Windows
     WebKitSite:
    
             Update Windows debugging instructions
    
             Reviewed by Mark Rowe.
    
             * building/debug.html:
    
     WebKitTools:
    
             Make it easy to run Safari in the debugger on Windows
    
             I've added a new script, debug-safari, which launches Safari in the
             debugger. On OS X it just calls gdb-safari.
    
             Reviewed by Mark Rowe.
    
             * FindSafari/FindSafari.cpp:
             (_tmain): Added a /debugger flag, which in combination with
             /printSafariLauncher will print a script that launches Safari in the
             debugger.
             * Scripts/debug-safari: Added.
             * Scripts/run-safari: Changed to call runSafari().
             * Scripts/run-webkit-nightly.cmd: Prepends the launcher script with
             vsvars32.bat, which will let us find VS/VC++ Express, and passes the
             first argument along to FindSafari.
             * Scripts/webkitdirs.pm:
             (sub runSafari): Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27878 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/run-webkit-nightly.cmd
+++ third_party/WebKit/WebKitTools/Scripts/run-webkit-nightly.cmd
@@ -1,5 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @echo off
 set script="%TMP%\run-webkit-nightly2.cmd"
-FindSafari.exe /printSafariLauncher > %script%
+set vsvars="%VS80COMNTOOLS%\vsvars32.bat"
+if exist %vsvars% (
+    copy %vsvars% "%script%"
+) else (
+    del "%script%"
+)
+FindSafari.exe %1 /printSafariLauncher >> "%script%"
 call %script%
