62736cdb80b59f52fc727d9699337a7d45187fca
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-20 08:50:44 +0000

    * Build in release mode
    git-svn-id: svn://svn.chromium.org/blink/trunk@27916 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/NotImplemented.h
+++ third_party/WebKit/WebCore/platform/NotImplemented.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if PLATFORM(QT)
 
     #include <qglobal.h>
+    #include <qbytearray.h>
     #define notImplemented() \
         if (qgetenv("DISABLE_NI_WARNING").isEmpty()) \
             qDebug("FIXME: UNIMPLEMENTED: %s:%d (%s)", __FILE__, __LINE__, WTF_PRETTY_FUNCTION)
