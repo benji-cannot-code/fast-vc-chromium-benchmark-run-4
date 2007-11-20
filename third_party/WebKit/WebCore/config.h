5dd05547dcf3d7e010722de23a19fb889c259e05
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-20 16:06:23 +0000

    2007-11-20  Mark Rowe  <mrowe@apple.com>
            Reviewed by Alp Toker.
    
            * WebKit.pri: Add in -Wno-unused-parameter to silence warnings in WebCore.
    
    2007-11-20  Mark Rowe  <mrowe@apple.com>
    
            Reviewed by Alp Toker.
    
            * config.h: Change #if to #ifdef to silence warnings on non-Apple platforms.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/config.h
+++ third_party/WebKit/WebCore/config.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define MOBILE 0
 
-#if __APPLE__
+#ifdef __APPLE__
 #define HAVE_FUNC_USLEEP 1
 #endif /* __APPLE__ */
 
