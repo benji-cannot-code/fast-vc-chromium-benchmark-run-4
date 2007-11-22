b425ab05cd5145af1a5dc6dd565840a5a5db3e16
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-22 03:28:14 +0000

    2007-11-21  Alp Toker  <alp@atoker.com>
            Reviewed by Mark Rowe.
    
            http://bugs.webkit.org/show_bug.cgi?id=16071
            Curl backend handles EINTR incorrectly
    
            Defer timers during select() to avoid interruption by timer signals.
    
            * platform/network/curl/ResourceHandleManager.cpp:
            (WebCore::ResourceHandleManager::downloadTimerCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/curl/ResourceHandleManager.cpp
+++ third_party/WebKit/WebCore/platform/network/curl/ResourceHandleManager.cpp
@@ -187,7 +187,10 @@ void ResourceHandleManager::downloadTimerCallback(Timer<ResourceHandleManager>* 
     timeout.tv_sec = 0;
     timeout.tv_usec = selectTimeoutMS * 1000;       // select waits microseconds
 
+    // Temporarily disable timers since signals may interrupt select(), raising EINTR errors on some platforms
+    setDeferringTimers(true);
     int rc = ::select(maxfd + 1, &fdread, &fdwrite, &fdexcep, &timeout);
+    setDeferringTimers(false);
 
     if (-1 == rc) {
 #ifndef NDEBUG
@@ -197,10 +200,7 @@ void ResourceHandleManager::downloadTimerCallback(Timer<ResourceHandleManager>* 
     }
 
     int runningHandles = 0;
-    CURLMcode curlCode = CURLM_CALL_MULTI_PERFORM;
-    while (CURLM_CALL_MULTI_PERFORM == curlCode) {
-        curlCode = curl_multi_perform(m_curlMultiHandle, &runningHandles);
-    }
+    while (curl_multi_perform(m_curlMultiHandle, &runningHandles) == CURLM_CALL_MULTI_PERFORM) { }
 
     // check the curl messages indicating completed transfers
     // and free their resources
