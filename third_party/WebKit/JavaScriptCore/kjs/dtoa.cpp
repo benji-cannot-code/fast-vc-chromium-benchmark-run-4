c0a3e9866998907b529972d60899b06d1d7ce8e8
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-22 03:08:41 +0000

    Fix <rdar://problem/5602936> Need to resolve new GCC 4.2 warnings.
    Reviewed by Tim Hatcher.
    
    Fix all warnings emitted by GCC 4.2 when building JavaScriptCore.  This allows builds with
    -Werror to succeed.  At present they will crash when executed due to code that is not safe
    under strict aliasing (<rdar://problem/5536806>).
    
    This required some format strings to be modified in WebCore and WebKit as their format
    specifiers did not match the argument type.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27947 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/dtoa.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/dtoa.cpp
@@ -2975,7 +2975,7 @@ dtoa
 					if (dval(d) > 0.5 + dval(eps))
 						goto bump_up;
 					else if (dval(d) < 0.5 - dval(eps)) {
-						while(*--s == '0');
+						while (*--s == '0') { }
 						s++;
 						goto ret1;
 						}
@@ -3294,7 +3294,7 @@ dtoa
 #ifdef Honor_FLT_ROUNDS
 trimzeros:
 #endif
-		while(*--s == '0');
+		while (*--s == '0') { }
 		s++;
 		}
  ret:
