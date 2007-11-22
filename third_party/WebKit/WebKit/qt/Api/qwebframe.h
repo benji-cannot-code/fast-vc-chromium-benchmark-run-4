0623c2c5f48e6641c74843dc1cadb239c6d196bf
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-22 10:12:42 +0000

    Use Q_SIGNALS/Q_SLOTS in the public API
    git-svn-id: svn://svn.chromium.org/blink/trunk@27966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.h
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.h
@@ -79,7 +79,7 @@ public:
 public Q_SLOTS:
     QString evaluateJavaScript(const QString& scriptSource);
 
-signals:
+Q_SIGNALS:
     void cleared();
     void loadDone(bool ok);
     void provisionalLoad();
