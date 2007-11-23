120ee6a0b1f096711b90dd773bf3b3c491424810
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-23 22:03:38 +0000

    2007-11-22  Alp Toker  <alp@atoker.com>
            Reviewed by Mark Rowe.
    
            Implement and document several WebKitFrame functions, some of which
            are necessary to support DRT.
    
            Correct NULL handling and improve run-time checks.
    
            * Api/webkitgtkframe.cpp:
            * Api/webkitgtkframe.h:
            * Api/webkitgtkprivate.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27991 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/Api/webkitgtkframe.h
+++ third_party/WebKit/WebKit/gtk/Api/webkitgtkframe.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2007 Holger Hans Peter Freyther
+ * Copyright (C) 2007 Alp Toker <alp@atoker.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gdk/gdk.h>
 
 #include "webkitgtkdefines.h"
+
+typedef struct OpaqueJSContext* JSGlobalContextRef;
+typedef const struct OpaqueJSContext* JSContextRef;
+typedef struct OpaqueJSValue* JSObjectRef;
 
 G_BEGIN_DECLS
 
@@ -99,6 +104,24 @@ webkit_frame_get_rectangle (WebKitFrame* frame);
 
 WEBKIT_API WebKitPage*
 webkit_frame_get_page (WebKitFrame* frame);
+
+WEBKIT_API WebKitFrame*
+webkit_frame_get_parent (WebKitFrame* frame);
+
+WEBKIT_API void
+webkit_frame_load_request (WebKitFrame* frame, WebKitNetworkRequest* request);
+
+WEBKIT_API void
+webkit_frame_stop_loading (WebKitFrame* frame);
+
+WEBKIT_API void
+webkit_frame_reload (WebKitFrame* frame);
+
+WEBKIT_API WebKitFrame*
+webkit_frame_find_frame (WebKitFrame* frame, const gchar* name);
+
+WEBKIT_API JSGlobalContextRef
+webkit_frame_get_global_context (WebKitFrame* frame);
 
 G_END_DECLS
 
