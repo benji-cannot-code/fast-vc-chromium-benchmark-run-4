c655b3dfbfd54a040d2f2558bd7614c986345233
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-23 10:19:17 +0000

    Reviewed by Maciej.
            <rdar://problem/5539306> REGRESSION: redirect fails when subframe's document is opened but
            not closed (affects digg.com)
    
            Tests: fast/loader/meta-refresh-vs-open.html
                   fast/loader/redirect-with-open-subframe-2.html
                   fast/loader/redirect-with-open-subframe.html
                   http/tests/loading/onload-vs-immediate-refresh.pl
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::scheduleRedirection): Only check whether the load is complete for
            HTTP redirects - JavaScript-initiated ones are effective immediately.
    
    WebKitTools:
            * DumpRenderTree/mac/DumpRenderTree.mm: (runTest): Replace the current document with a blank
            one after finishing with a test to avoid having its delayed onload handler fired when
            starting the next test. This is ugly and still unreliable (see LayoutTests ChangeLog),
            but it helps somewhat.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@27986 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -1809,7 +1809,9 @@ void FrameLoader::scheduleRedirection(ScheduledRedirection* redirection)
 {
     stopRedirectionTimer();
     m_scheduledRedirection.set(redirection);
-    if (m_isComplete)
+    if (!m_isComplete && redirection->type != ScheduledRedirection::redirection)
+        completed();
+    if (m_isComplete || redirection->type != ScheduledRedirection::redirection)
         startRedirectionTimer();
 }
 
