fd8544efd8f5a0b18e2b83ed772f94512c0217c0
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-27 01:08:58 +0000

    WebCore:
            Build fix.
    
            * WebCore.vcproj/WebCore.make:
            * WebCore.vcproj/migrate-idls.sh:
    
    WebKit/win:
    
            Build fix.
    
            * WebKit.vcproj/build-generated-files.sh:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebKit.vcproj/build-generated-files.sh
+++ third_party/WebKit/WebKit/win/WebKit.vcproj/build-generated-files.sh
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 NUMCPUPATH="../../../WebKitTools/Scripts/num-cpus"
-if [ -x $NUMCPUPATH ]
+if [ -x $NUMCPUPATH ]; then
     NUMCPUS=`$NUMCPUPATH`
 else
     NUMCPUS=1
