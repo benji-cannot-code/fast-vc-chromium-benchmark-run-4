13027fb9ab0e0b359bceab39a5d9ad4f5213282e
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-27 01:35:05 +0000

    Reviewed by Adele.
            - some middle-mouse-button-related fixes
    
            These don't affect Safari since it maps the middle mouse button to the command key,
            but that might not always be the case for future versions.
    
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView otherMouseDown:]): Pass through middle mouse down events to WebCore.
            (-[WebHTMLView otherMouseDragged:]): Ditto, for drag events.
            (-[WebHTMLView otherMouseUp:]): Ditto, for up events.
    
            * WebView/WebPolicyDelegate.h: Fixed inaccurate documentation of WebActionButtonKey.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
@@ -4784,6 +4784,30 @@ NSStrokeColorAttributeName        /* NSColor, default nil: same as foreground co
     [super _windowChangedKeyState];
 }
 
+- (void)otherMouseDown:(NSEvent *)event
+{
+    if ([event buttonNumber] == 2)
+        [self mouseDown:event];
+    else
+        [super otherMouseDown:event];
+}
+
+- (void)otherMouseDragged:(NSEvent *)event
+{
+    if ([event buttonNumber] == 2)
+        [self mouseDragged:event];
+    else
+        [super otherMouseDragged:event];
+}
+
+- (void)otherMouseUp:(NSEvent *)event
+{
+    if ([event buttonNumber] == 2)
+        [self mouseUp:event];
+    else
+        [super otherMouseUp:event];
+}
+
 @end
 
 @implementation WebHTMLView (WebTextSizing)
--- third_party/WebKit/WebKit/mac/WebView/WebPolicyDelegate.h
+++ third_party/WebKit/WebKit/mac/WebView/WebPolicyDelegate.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2003, 2004, 2005 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2003, 2004, 2005, 2007 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class WebFrame;
 @class WebPolicyPrivate;
 
-
 /*!
     @enum WebNavigationType
     @abstract The type of action that triggered a possible navigation.
@@ -57,13 +56,11 @@ typedef enum {
     WebNavigationTypeOther
 } WebNavigationType;
 
-
 extern NSString *WebActionNavigationTypeKey; // NSNumber (WebNavigationType)
 extern NSString *WebActionElementKey; // NSDictionary of element info
-extern NSString *WebActionButtonKey;  // NSEventType
+extern NSString *WebActionButtonKey; // NSNumber (0 for left button, 1 for middle button, 2 for right button)
 extern NSString *WebActionModifierFlagsKey; // NSNumber (unsigned)
 extern NSString *WebActionOriginalURLKey; // NSURL
-
 
 /*!
     @protocol WebPolicyDecisionListener
