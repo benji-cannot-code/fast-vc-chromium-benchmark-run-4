c17043f38909bf9630aeefe0846b0f4e222435d3
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-28 09:04:15 +0000

    Reviewed by Maciej Stachowiak.
            - fix <rdar://problem/5596160> fast/events/objc-event-api.html fails when run alone (or first)
    
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView setDataSource:]): This method calls addMouseMovedObserver
            because addMouseMovedObserver returns early if the dataSource
            is not nil. But if the dataSource is already set (which happens when
            a WebHTMLView is being reused) then addMouseMovedObserver must not
            be called again.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28104 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
@@ -3286,11 +3286,15 @@ noPromisedData:
     ASSERT(dataSource);
     if (_private->dataSource != dataSource) {
         ASSERT(!_private->closed);
+        BOOL hadDataSource = _private->dataSource != nil;
+
         [dataSource retain];
         [_private->dataSource release];
         _private->dataSource = dataSource;
         [_private->pluginController setDataSource:dataSource];
-        [self addMouseMovedObserver];
+
+        if (!hadDataSource)
+            [self addMouseMovedObserver];
     }
 }
 
