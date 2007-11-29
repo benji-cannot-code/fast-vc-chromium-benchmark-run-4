benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit c3283739e127100368cd4cde47f1dc5c30f5a1ca

2007-11-24  Eric Seidel  <eric@webkit.org>
        Reviewed by Sam.

        reformat is_anchored

        * pcre/pcre_compile.cpp:
        (is_anchored):


git-svn-id: svn://svn.chromium.org/blink/trunk@28151 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/JavaScriptCore/pcre/pcre_compile.cpp
@@ -2058,41 +2058,35 @@ Arguments:
 Returns:     true or false
 */
 
-static bool
-is_anchored(const uschar *code, int options, unsigned int bracket_map,
-  unsigned int backref_map)
+static bool is_anchored(const uschar* code, int options, unsigned int bracket_map, unsigned int backref_map)
 {
-do {
-   const uschar *scode =
-     first_significant_code(code + 1+LINK_SIZE, false);
-   int op = *scode;
-
-   /* Capturing brackets */
-
-   if (op > OP_BRA)
-     {
-     int new_map;
-     op -= OP_BRA;
-     if (op > EXTRACT_BASIC_MAX) op = GET2(scode, 2+LINK_SIZE);
-     new_map = bracket_map | ((op < 32)? (1 << op) : 1);
-     if (!is_anchored(scode, options, new_map, backref_map)) return false;
-     }
-
-   /* Other brackets */
-
-   else if (op == OP_BRA || op == OP_ASSERT || op == OP_ONCE)
-     {
-     if (!is_anchored(scode, options, bracket_map, backref_map)) return false;
-     }
-
-   /* Check for explicit anchoring */
-
-   else if (((options & PCRE_MULTILINE) != 0 || op != OP_CIRC))
-     return false;
-   code += GET(code, 1);
-   }
-while (*code == OP_ALT);   /* Loop for each alternative */
-return true;
+    do {
+        const uschar *scode =
+        first_significant_code(code + 1 + LINK_SIZE, false);
+        int op = *scode;
+        
+        /* Capturing brackets */
+        if (op > OP_BRA) {
+            op -= OP_BRA;
+            if (op > EXTRACT_BASIC_MAX)
+                op = GET2(scode, 2 + LINK_SIZE);
+            int new_map = bracket_map | ((op < 32)? (1 << op) : 1);
+            if (!is_anchored(scode, options, new_map, backref_map))
+                return false;
+        }
+        
+        /* Other brackets */
+        else if (op == OP_BRA || op == OP_ASSERT || op == OP_ONCE)
+            if (!is_anchored(scode, options, bracket_map, backref_map))
+                return false;
+        
+        /* Check for explicit anchoring */
+        
+        else if ((options & PCRE_MULTILINE) || op != OP_CIRC)
+            return false;
+        code += GET(code, 1);
+    } while (*code == OP_ALT);   /* Loop for each alternative */
+    return true;
 }
 
 

commit 08f84e4e4de2f39e9437ecd665eef87f90c0c890

2007-11-29  Eric Seidel  <eric@webkit.org>
        Reviewed by Mark Rowe.

        Fix for --guard crash of fast/js/regexp-charclass-crash introduced by r28151.

        * pcre/pcre_compile.cpp:
        (is_anchored):


git-svn-id: svn://svn.chromium.org/blink/trunk@28186 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/JavaScriptCore/pcre/pcre_compile.cpp
@@ -2072,13 +2072,13 @@ static bool is_anchored(const uschar* code, int options, unsigned bracket_map, u
         }
         
         /* Other brackets */
-        else if (op == OP_BRA || op == OP_ASSERT || op == OP_ONCE)
+        else if (op == OP_BRA || op == OP_ASSERT || op == OP_ONCE) {
             if (!is_anchored(scode, options, bracket_map, backref_map))
                 return false;
         
         /* Check for explicit anchoring */
         
-        else if ((options & MatchAcrossMultipleLinesOption) || op != OP_CIRC)
+        } else if ((options & MatchAcrossMultipleLinesOption) || op != OP_CIRC)
             return false;
         code += getOpcodeValueAtOffset(code, 1);
     } while (*code == OP_ALT);   /* Loop for each alternative */