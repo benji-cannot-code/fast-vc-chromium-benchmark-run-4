bbc3a232d6a679521c2c1a212dd2c14634e26edc
justin.garcia@apple.com <justin.garcia@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-29 01:33:52 +0000

    WebCore:
            Reviewed by Darin Adler.
    
            <rdar://problem/5573879> GMail Beta: Crash in when undoing Remove Formating
            
            In the new beta, Google has added some custom code on top of our RemoveFormat
            to work around a bug where fully selected lists aren't removed.  This code corrupts
            the undo stack and causes crashes.  This change fixes two problems with RemoveFormat
            so that Google doesn't have to have any custom code. After checking this in I'll 
            work on bulletproofing the undo stack
    
            * WebCore.xcodeproj/project.pbxproj: 
            * editing/CompositeEditCommand.cpp:
            (WebCore::CompositeEditCommand::insertLineBreak): Added this convenience method.
            (WebCore::CompositeEditCommand::inputText): Added code to select all inserted text, not
            just the last paragraph.  Added code to support an input string with '\n's.
            * editing/CompositeEditCommand.h:
            * editing/Editor.cpp:
            (WebCore::Editor::removeFormattingAndStyle): Moved code to its own EditCommand.
            * editing/RemoveFormatCommand.cpp: Added. Moved code from removeFormattingAndStyle here.
            (WebCore::RemoveFormatCommand::RemoveFormatCommand):
            (WebCore::RemoveFormatCommand::doApply): Added code to remove fully selected lists.
            * editing/RemoveFormatCommand.h: Added.
            (WebCore::RemoveFormatCommand::editingAction):
    
    LayoutTests:
    
            Reviewed by Darin Adler.
            
            <rdar://problem/5573879> GMail Beta: Crash in when undoing Remove Formating
    
            Added to demonstrate fixes:
            * editing/execCommand/5573879.html: Added.
            * platform/mac/editing/execCommand/5573879-expected.checksum: Added.
            * platform/mac/editing/execCommand/5573879-expected.png: Added.
            * platform/mac/editing/execCommand/5573879-expected.txt: Added.
            
            Fixes (RemoveFormat now preserves the selection correctly):
            * platform/mac/editing/execCommand/4920742-1-expected.checksum:
            * platform/mac/editing/execCommand/4920742-1-expected.png:
            * platform/mac/editing/execCommand/4920742-1-expected.txt:
            * platform/mac/editing/execCommand/remove-formatting-2-expected.checksum:
            * platform/mac/editing/execCommand/remove-formatting-2-expected.png:
            * platform/mac/editing/execCommand/remove-formatting-2-expected.txt:
            * platform/mac/editing/execCommand/remove-formatting-expected.checksum:
            * platform/mac/editing/execCommand/remove-formatting-expected.png:
            * platform/mac/editing/execCommand/remove-formatting-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28126 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/CompositeEditCommand.cpp
+++ third_party/WebKit/WebCore/editing/CompositeEditCommand.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLNames.h"
 #include "InlineTextBox.h"
 #include "InsertIntoTextNodeCommand.h"
+#include "InsertLineBreakCommand.h"
 #include "InsertNodeBeforeCommand.h"
 #include "InsertParagraphSeparatorCommand.h"
 #include "InsertTextCommand.h"
@@ -121,6 +122,11 @@ void CompositeEditCommand::removeStyledElement(Element* element)
 void CompositeEditCommand::insertParagraphSeparator(bool useDefaultParagraphElement)
 {
     applyCommandToComposite(new InsertParagraphSeparatorCommand(document(), useDefaultParagraphElement));
+}
+
+void CompositeEditCommand::insertLineBreak()
+{
+    applyCommandToComposite(new InsertLineBreakCommand(document()));
 }
 
 void CompositeEditCommand::insertNodeBefore(Node* insertChild, Node* refChild)
@@ -263,9 +269,29 @@ void CompositeEditCommand::joinTextNodes(Text *text1, Text *text2)
 
 void CompositeEditCommand::inputText(const String &text, bool selectInsertedText)
 {
-    RefPtr<InsertTextCommand> command = new InsertTextCommand(document());
-    applyCommandToComposite(command);
-    command->input(text, selectInsertedText);
+    int offset = 0;
+    int length = text.length();
+    RefPtr<Range> startRange = new Range(document(), Position(document()->documentElement(), 0), endingSelection().start());
+    int startIndex = TextIterator::rangeLength(startRange.get());
+    int newline;
+    do {
+        newline = text.find('\n', offset);
+        if (newline != offset) {
+            RefPtr<InsertTextCommand> command = new InsertTextCommand(document());
+            applyCommandToComposite(command);
+            int substringLength = newline == -1 ? length - offset : newline - offset;
+            command->input(text.substring(offset, substringLength), false);
+        }
+        if (newline != -1)
+            insertLineBreak();
+            
+        offset = newline + 1;
+    } while (newline != -1 && offset != length);
+    
+    if (selectInsertedText) {
+        RefPtr<Range> selectedRange = TextIterator::rangeFromLocationAndLength(document()->documentElement(), startIndex, length);
+        setEndingSelection(Selection(selectedRange.get()));
+    }
 }
 
 void CompositeEditCommand::insertTextIntoNode(Text *node, int offset, const String &text)
