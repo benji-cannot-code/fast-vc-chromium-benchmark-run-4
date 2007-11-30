ad37153fc68690cce6607febce78c2d4555f26bb
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-11-30 20:15:11 +0000

    Other wx port fixes needed to restore the build bot.
    git-svn-id: svn://svn.chromium.org/blink/trunk@28250 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/wx/WebKitSupport/ChromeClientWx.cpp
+++ third_party/WebKit/WebKit/wx/WebKitSupport/ChromeClientWx.cpp
@@ -303,13 +303,13 @@ void ChromeClientWx::print(Frame*)
     notImplemented();
 }
 
-unsigned long long ChromeClient::requestQuotaIncreaseForNewDatabase(Frame*, const SecurityOriginData&, const String&, unsigned long long)
+unsigned long long ChromeClientWx::requestQuotaIncreaseForNewDatabase(Frame*, const SecurityOriginData&, const String&, unsigned long long)
 {
     notImplemented();
     return 0;
 }
 
-unsigned long long ChromeClient::requestQuotaIncreaseForDatabaseOperation(Frame*, const SecurityOriginData&, const String&, unsigned long long)
+unsigned long long ChromeClientWx::requestQuotaIncreaseForDatabaseOperation(Frame*, const SecurityOriginData&, const String&, unsigned long long)
 {
     notImplemented();
     return 0;
