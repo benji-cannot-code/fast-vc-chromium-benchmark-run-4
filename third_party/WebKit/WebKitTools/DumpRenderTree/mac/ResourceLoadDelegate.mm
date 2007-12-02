14bd2df78ed56cbeb49ea6cba811fcd27bc9e1ae
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-02 23:40:21 +0000

    Rubber stamped by Anders.
            Use [NSURL absoluteString] instead of [NSURL description] in order to get
            more uniform results cross platform.
    
            * DumpRenderTree/mac/ResourceLoadDelegate.mm:
            (-[NSURL _drt_descriptionSuitableForTestResult]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28324 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/ResourceLoadDelegate.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/ResourceLoadDelegate.mm
@@ -70,7 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSString *)_drt_descriptionSuitableForTestResult 
 {
     if (![self isFileURL])
-        return [self description];
+        return [self absoluteString];
 
     WebDataSource *dataSource = [mainFrame dataSource];
     if (!dataSource)
