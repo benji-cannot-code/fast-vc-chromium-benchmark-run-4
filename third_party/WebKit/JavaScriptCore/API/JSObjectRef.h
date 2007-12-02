5caf6b09d528ba3915a4758f22d18a713290f479
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-02 05:24:25 +0000

    2007-12-01  Alp Toker  <alp@atoker.com>
            Reviewed by Adam Roben.
    
            http://bugs.webkit.org/show_bug.cgi?id=16228
            kJSClassDefinitionEmpty is not exported with JS_EXPORT
    
            Add JS_EXPORT to kJSClassDefinitionEmpty.
    
            Make the gcc compiler check take precedence over the WIN32||_WIN32
            check to ensure that symbols are exported on Windows when using gcc.
    
            Add a TODO referencing the bug about JS_EXPORT in the Win build
            (http://bugs.webkit.org/show_bug.cgi?id=16227)
    
            Don't define JS_EXPORT as 'extern' when the compiler is unknown since
            it would result in the incorrect expansion:
    
              extern extern const JSClassDefinition kJSClassDefinitionEmpty;
    
            (This was something we inherited from CFBase.h that doesn't make sense
            for JSBase.h)
    
            * API/JSBase.h:
            * API/JSObjectRef.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28311 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSObjectRef.h
+++ third_party/WebKit/JavaScriptCore/API/JSObjectRef.h
@@ -369,7 +369,7 @@ typedef struct {
 JSClassDefinition definition = kJSClassDefinitionEmpty;
 definition.finalize = Finalize;
 */
-extern const JSClassDefinition kJSClassDefinitionEmpty;
+JS_EXPORT extern const JSClassDefinition kJSClassDefinitionEmpty;
 
 /*!
 @function
