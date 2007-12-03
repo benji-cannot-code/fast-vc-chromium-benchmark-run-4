2b37d068d59729fc9d491d899bf64363b16eda7a
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-03 09:25:45 +0000

    2007-12-03  Mark Rowe  <mrowe@apple.com>
            Speculative Windows build fix.
    
            * WebScriptCallFrame.cpp:
            (WebScriptCallFrame::valueByEvaluatingJavaScriptFromString):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28340 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebScriptCallFrame.cpp
+++ third_party/WebKit/WebKit/win/WebScriptCallFrame.cpp
@@ -174,7 +174,7 @@ JSValue* WebScriptCallFrame::valueByEvaluatingJavaScriptFromString(BSTR script)
 {
     ExecState* state = m_state;
     Interpreter* interp  = state->dynamicInterpreter();
-    JSObject* globObj = interp->globalObject();
+    JSGlobalObject* globObj = interp->globalObject();
 
     // find "eval"
     JSObject* eval = 0;
