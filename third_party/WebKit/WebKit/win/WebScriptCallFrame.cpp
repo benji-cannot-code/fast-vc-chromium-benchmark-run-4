7b746a9d21442d6ceb299d35bc0cd1f2a9813af9
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-04 18:27:48 +0000

    Windows build fix
            * WebScriptCallFrame.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebScriptCallFrame.cpp
+++ third_party/WebKit/WebKit/win/WebScriptCallFrame.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebScriptCallFrame.h"
 
 #include "COMEnumVariant.h"
-#include "IWebScriptScope.h"
 #include "Function.h"
 
 #include <JavaScriptCore/JSGlobalObject.h>
