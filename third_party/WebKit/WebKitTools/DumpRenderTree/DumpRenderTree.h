2ec1385ee0a73f060f09f37c09f5d9cf6f30c440
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-04 22:28:06 +0000

    2007-12-04  Alp Toker  <alp@atoker.com>
            Prospective Win DRT build fix.
    
            * DumpRenderTree/DumpRenderTree.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28409 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.h
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DumpRenderTreeGtk.h"
 #endif
 
-#if PLATFORM(CF)
+#if PLATFORM(CF) || PLATFORM(MAC) || PLATFORM(WIN)
 #include <CoreFoundation/CoreFoundation.h>
 extern CFRunLoopTimerRef waitToDumpWatchdog;
 #endif
