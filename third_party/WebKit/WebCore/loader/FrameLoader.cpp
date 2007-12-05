2b3b994d07b3015eefe9a6657f4f0aa31fa4bf92
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-05 22:58:41 +0000

    Reviewed by Adam Roben.
            - fix http://bugs.webkit.org/show_bug.cgi?id=16306
              Assertion failed in WebCore/loader/FrameLoader.cpp:4642
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::dispatchDidCommitLoad): Check m_creatingInitialEmptyDocument
            and don't deliver the client call in that case.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28461 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -508,7 +508,7 @@ void FrameLoader::submitFormAgain()
 void FrameLoader::submitForm(const char* action, const String& url, PassRefPtr<FormData> formData,
     const String& target, const String& contentType, const String& boundary, Event* event)
 {
-    ASSERT(formData.get());
+    ASSERT(formData);
     
     KURL u = completeURL(url.isNull() ? "" : url);
     // FIXME: Do we really need to special-case an empty URL?
@@ -4648,6 +4648,9 @@ bool FrameLoader::shouldTreatSchemeAsLocal(const String& scheme)
 
 void FrameLoader::dispatchDidCommitLoad()
 {
+    if (m_creatingInitialEmptyDocument)
+        return;
+
 #ifndef NDEBUG
     ASSERT(!m_didDispatchDidCommitLoad);
     m_didDispatchDidCommitLoad = true;
