2bfb1eb2738a9121d9ab55bc4f1de4a175173d8e
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-06 03:11:50 +0000

    Build fix: added some #includes.
            * kjs/JSImmediate.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/JSImmediate.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/JSImmediate.cpp
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSImmediate.h"
 
 #include "JSGlobalObject.h"
+#include "boolean_object.h"
+#include "number_object.h"
 #include "object.h"
 
 namespace KJS {
