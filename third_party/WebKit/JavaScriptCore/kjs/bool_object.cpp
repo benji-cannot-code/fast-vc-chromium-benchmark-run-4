88f1681c25dc94be5875fad89402a20aaa04faf4
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-06 02:41:09 +0000

    Build fix: added an #include.
            * kjs/bool_object.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/bool_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/bool_object.cpp
@@ -24,8 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "bool_object.h"
 
-#include "operations.h"
+#include "JSGlobalObject.h"
 #include "error_object.h"
+#include "operations.h"
 #include <wtf/Assertions.h>
 
 using namespace KJS;
