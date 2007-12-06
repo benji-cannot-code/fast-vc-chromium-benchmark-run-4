9f63a0ae55e0813886b0a8baa1ca70438393061a
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-06 06:25:24 +0000

    Reviewed by Maciej.
            - fix http://bugs.webkit.org/show_bug.cgi?id=16266
              <rdar://problem/5625279> REGRESSION: crash loading CNN.com at
              Interpreter::createObjectsForGlobalObjectProperties()
    
            I don't know how to reproduce this in a test.
    
            * bindings/js/JSDocumentCustom.cpp: (WebCore::JSDocument::mark):
            Call JSEventTargetNode::mark instead of DOMObject::mark. There's no difference
            right now, but it's best to call your base class, not your base's base.
    
            * bindings/js/kjs_events.h: Removed unneeded virtual from the
            JSUnprotectedEventListener::mark() function; it neither derives from or is
            derived from anyone else with a mark() function so there's no need for virtual.
    
            * bindings/js/kjs_window.cpp: (KJS::Window::mark): Call JSGlobalObject::mark,
            not JSObject::mark. This is the actual bug fix.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28482 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDocumentCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDocumentCustom.cpp
@@ -42,7 +42,7 @@ using namespace KJS;
 
 void JSDocument::mark()
 {
-    DOMObject::mark();
+    JSEventTargetNode::mark();
     ScriptInterpreter::markDOMNodesForDocument(static_cast<Document*>(impl()));
 }
 
--- third_party/WebKit/WebCore/bindings/js/kjs_window.cpp
+++ third_party/WebKit/WebCore/bindings/js/kjs_window.cpp
@@ -287,7 +287,7 @@ Location *Window::location() const
 // reference our special objects during garbage collection
 void Window::mark()
 {
-  JSObject::mark();
+  JSGlobalObject::mark();
   if (d->loc && !d->loc->marked())
     d->loc->mark();
 }
