f5c3832e769720c646d11cd392e6ef0089a485ed
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-07 23:22:32 +0000

    Tried to fix build by suppressing warning.
            * WebScriptCallFrame.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebScriptCallFrame.cpp
+++ third_party/WebKit/WebKit/win/WebScriptCallFrame.cpp
@@ -34,13 +34,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "COMEnumVariant.h"
 #include "Function.h"
 
+#pragma warning(push, 0)
 #include <JavaScriptCore/Interpreter.h>
 #include <JavaScriptCore/JSGlobalObject.h>
 #include <JavaScriptCore/JSStringRefBSTR.h>
 #include <JavaScriptCore/JSValueRef.h>
 #include <JavaScriptCore/PropertyNameArray.h>
 
-#pragma warning(push, 0)
 #include <WebCore/BString.h>
 #include <WebCore/PlatformString.h>
 #pragma warning(pop)
