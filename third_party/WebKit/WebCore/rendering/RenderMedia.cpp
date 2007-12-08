a11664d3d4b6b8b9807002180b30fb2f5c55d686
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-08 12:11:34 +0000

    Mac Tiger build fix.
            Use the wtf prefix when including MathExtras.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28562 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderMedia.cpp
+++ third_party/WebKit/WebCore/rendering/RenderMedia.cpp
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLInputElement.h"
 #include "HTMLMediaElement.h"
 #include "HTMLNames.h"
-#include "MathExtras.h"
+#include <wtf/MathExtras.h>
 #include "MouseEvent.h"
 #include "MediaPlayer.h"
 #include "RenderSlider.h"
