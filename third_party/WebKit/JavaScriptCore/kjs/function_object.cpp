08bf3f5c7da05d3ecffec8694f681e1e0a3a9ee4
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-08 00:24:51 +0000

    Build fix: added #include.
            * kjs/function_object.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28546 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/function_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/function_object.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "function_object.h"
 
 #include "JSGlobalObject.h"
+#include "Parser.h"
 #include "array_object.h"
 #include "debugger.h"
 #include "function.h"
