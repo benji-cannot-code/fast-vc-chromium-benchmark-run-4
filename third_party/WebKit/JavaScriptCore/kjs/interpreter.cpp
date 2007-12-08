71d4e7be5fc59b94a2d89da17ba6615069948147
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-08 00:35:08 +0000

    Build fix: added #include.
            * kjs/interpreter.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28548 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/interpreter.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/interpreter.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ExecState.h"
 #include "JSGlobalObject.h"
+#include "Parser.h"
 #include "SavedBuiltins.h"
 #include "array_object.h"
 #include "bool_object.h"
