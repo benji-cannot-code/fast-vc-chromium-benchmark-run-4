75d5fba200ccfe6dd6bd776765a5bee62fce20d7
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-08 00:22:27 +0000

    - fix Windows build
            * bridge/win/GlobalHistoryWin.cpp:
            (WebCore::historyContains): Missed a rename.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28544 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/win/GlobalHistoryWin.cpp
+++ third_party/WebKit/WebCore/bridge/win/GlobalHistoryWin.cpp
@@ -34,7 +34,7 @@ namespace WebCore {
 bool historyContains(const UChar* characters, unsigned length)
 {
     WebCoreHistoryProvider* provider = WebCoreHistory::historyProvider();
-    return provider && provider->containsItemForURLUnicode(characters, length);
+    return provider && provider->containsURL(characters, length);
 }
 
 } // namespace WebCore
