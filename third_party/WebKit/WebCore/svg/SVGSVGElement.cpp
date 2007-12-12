c063295721a21f74afd0047f44bed7b10da0f502
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-12 21:24:23 +0000

    Reviewed by Oliver.
            Fix for <rdar://problem/5643770> REGRESSION: Free-standing SVGs 
            with width and height 100% clip to 300 x 150
    
            Though it was correct in an earlier iteration of my patch, it is 
            not sufficient in the final, committed version to ask if the 
            relativeWidthValue() or relativeHeightValue() is greater than 0 
            just to determine if one has been set, for, they are now 
            initialized to 300 and 150 respectively! This patch instead adds a 
            bool to keep track of whether a container size has been set, and 
            only used the relative value if it has.
    
            * rendering/RenderSVGRoot.cpp:
            (WebCore::RenderSVGRoot::calcViewport):
            * svg/SVGSVGElement.cpp:
            (WebCore::SVGSVGElement::SVGSVGElement):
            * svg/SVGSVGElement.h:
            (WebCore::SVGSVGElement::setContainerSize):
            (WebCore::SVGSVGElement::hasSetContainerSize):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGSVGElement.cpp
+++ third_party/WebKit/WebCore/svg/SVGSVGElement.cpp
@@ -73,6 +73,7 @@ SVGSVGElement::SVGSVGElement(const QualifiedName& tagName, Document* doc)
     , m_timeScheduler(new TimeScheduler(doc))
     , m_viewSpec(0)
     , m_containerSize(300, 150)
+    , m_hasSetContainerSize(false)
 {
     setWidthBaseValue(SVGLength(this, LengthModeWidth, "100%"));
     setHeightBaseValue(SVGLength(this, LengthModeHeight, "100%"));
