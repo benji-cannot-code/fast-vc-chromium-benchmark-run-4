a47293a437511f9d722f2978b36b3072fbebd85f
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-12 08:00:41 +0000

    WebCore:
            Reviewed by Maciej Stachowiak.
    
            - allow non-integer font sizes on Windows for small caps
    
            * platform/graphics/win/FontDataWin.cpp:
            (WebCore::FontData::smallCapsFontData):
            * platform/graphics/win/FontPlatformData.h:
            (WebCore::FontPlatformData::size):
            (WebCore::FontPlatformData::setSize):
            * platform/graphics/win/FontPlatformDataWin.cpp:
            (WebCore::FontPlatformData::FontPlatformData):
    
    LayoutTests:
    
            Reviewed by Maciej Stachowiak.
    
            - removed now-passing tests
    
            * platform/win/Skipped:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28641 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/FontDataWin.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/FontDataWin.cpp
@@ -138,7 +138,7 @@ void FontData::platformDestroy()
 FontData* FontData::smallCapsFontData(const FontDescription& fontDescription) const
 {
     if (!m_smallCapsFontData) {
-        int smallCapsHeight = lroundf(cSmallCapsFontSizeMultiplier * m_font.size());
+        float smallCapsHeight = cSmallCapsFontSizeMultiplier * m_font.size();
         if (isCustomFont()) {
             FontPlatformData smallCapsFontData(m_font);
             smallCapsFontData.setSize(smallCapsHeight);
@@ -146,7 +146,7 @@ FontData* FontData::smallCapsFontData(const FontDescription& fontDescription) co
         } else {
             LOGFONT winfont;
             GetObject(m_font.hfont(), sizeof(LOGFONT), &winfont);
-            winfont.lfHeight = -smallCapsHeight * 32;
+            winfont.lfHeight = -lroundf(smallCapsHeight * 32);
             HFONT hfont = CreateFontIndirect(&winfont);
             m_smallCapsFontData = new FontData(FontPlatformData(hfont, smallCapsHeight, fontDescription.bold(), fontDescription.italic()));
         }
