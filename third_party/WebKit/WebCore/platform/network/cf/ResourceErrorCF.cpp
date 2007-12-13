919488e619179b0f5e54416e648a9914720ed673
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-13 19:36:57 +0000

    - build fix
            * platform/network/cf/ResourceErrorCF.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28691 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/ResourceErrorCF.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/ResourceErrorCF.cpp
@@ -30,7 +30,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if USE(CFNETWORK)
 
+// FIXME: Once <rdar://problem/5050881> is fixed in open source we
+// can remove this extern "C"
+extern "C" { 
 #include <CFNetwork/CFNetworkErrors.h>
+}
+
 #include <CoreFoundation/CFError.h>
 #include <WTF/RetainPtr.h>
 
