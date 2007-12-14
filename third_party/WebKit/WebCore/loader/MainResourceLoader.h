599e79f76b06585db6c9a470e26757a5fb3cbe25
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-14 23:42:32 +0000

    Reviewed by Adele.
            - fix http://bugs.webkit.org/show_bug.cgi?id=16442
              navigation policy delegate gets called twice for each load
    
            * loader/MainResourceLoader.cpp:
            (WebCore::MainResourceLoader::willSendRequest): Removed call to checkNavigationPolicy.
            That's handled by FrameLoader.
    
            * loader/MainResourceLoader.h: Removed callContinueAfterNavigationPolicy
            and continueAfterNavigationPolicy.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28726 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/MainResourceLoader.h
+++ third_party/WebKit/WebCore/loader/MainResourceLoader.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005, 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2005, 2006, 2007 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -74,9 +74,6 @@ namespace WebCore {
         ResourceError interruptionForPolicyChangeError() const;
         void stopLoadingForPolicyChange();
         bool isPostOrRedirectAfterPost(const ResourceRequest& newRequest, const ResourceResponse& redirectResponse);
-
-        static void callContinueAfterNavigationPolicy(void*, const ResourceRequest&, PassRefPtr<FormState>, bool shouldContinue);
-        void continueAfterNavigationPolicy(const ResourceRequest&, bool shouldContinue);
 
         static void callContinueAfterContentPolicy(void*, PolicyAction);
         void continueAfterContentPolicy(PolicyAction);
