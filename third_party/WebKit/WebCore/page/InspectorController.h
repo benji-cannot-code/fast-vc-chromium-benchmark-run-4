2ff622721958c071d60447bb5f969304a9e9917b
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-14 03:32:18 +0000

    Reviewed by Mark Rowe.
            Fix typos and rename InspectorController::moveByUnrestricted to InspectorController::moveWindowBy.
    
            * page/InspectorController.cpp:
            (WebCore::moveByUnrestricted):
            (WebCore::InspectorController::windowScriptObjectAvailable):
            (WebCore::InspectorController::moveWindowBy):
            * page/InspectorController.h:
            * page/inspector/inspector.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28708 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/InspectorController.h
+++ third_party/WebKit/WebCore/page/InspectorController.h
@@ -118,7 +118,7 @@ public:
 
     const ResourcesMap& resources() const { return m_resources; }
 
-    void moveByUnrestricted(float x, float y) const;
+    void moveWindowBy(float x, float y) const;
 
 private:
     void focusNode();
