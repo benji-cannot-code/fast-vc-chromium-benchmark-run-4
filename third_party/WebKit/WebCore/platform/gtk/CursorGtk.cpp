abf18b0308fc74a14eb247bcf8dd63418630930e
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-15 01:03:59 +0000

    2007-12-14  Alp Toker  <alp@atoker.com>
            Reviewed by Maciej.
    
            http://bugs.webkit.org/show_bug.cgi?id=16432
            [GTK] Update license headers
    
            Consent has been given by the authors of these files to change license
            to the LGPL as outlined in the bug report.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/CursorGtk.cpp
+++ third_party/WebKit/WebCore/platform/gtk/CursorGtk.cpp
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
  * Copyright (C) 2006 Michael Emmel mike.emmel@gmail.com 
  * Copyright (C) 2007 Christian Dywan <christian@twotoasts.de>
  * All rights reserved.
