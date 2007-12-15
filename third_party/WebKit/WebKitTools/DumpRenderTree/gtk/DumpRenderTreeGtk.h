7e0ceaa78811bfe4a5be5ec81bfc8690ceabf960
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-15 12:39:48 +0000

    Add a watchdog timer to Gtk DumpRenderTree, and implement alert/prompt/confirm. This prevents many layout tests from hanging while waiting on user responses to dialogs.
    Reviewed by Alp Toker.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/gtk/DumpRenderTreeGtk.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/gtk/DumpRenderTreeGtk.h
@@ -34,5 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 extern WebKitWebFrame* mainFrame;
 extern WebKitWebFrame* topLoadingFrame;
+extern guint waitToDumpWatchdog;
 
 #endif // DumpRenderTreeGtk_h 
