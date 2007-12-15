a88c2f23b1715ced9c76901502ac0b57e69bd771
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-15 04:21:40 +0000

    2007-12-14  Alp Toker  <alp@atoker.com>
            Build fix for GTK+/Qt and ports that don't use AllInOneFile.cpp.
    
            Include UnusedParam.h.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/TCSystemAlloc.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/TCSystemAlloc.cpp
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Assertions.h"
 #include "TCSystemAlloc.h"
 #include "TCSpinLock.h"
+#include "UnusedParam.h"
 
 #ifndef MAP_ANONYMOUS
 #define MAP_ANONYMOUS MAP_ANON
