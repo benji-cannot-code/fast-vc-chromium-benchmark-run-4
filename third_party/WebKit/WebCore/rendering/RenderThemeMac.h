0282cd2258ba947b18d3f5851800388e430e083e
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-16 19:26:02 +0000

    WebCore:
            Reviewed by Darin.
            
            http://bugs.webkit.org/show_bug.cgi?id=6129
            Incomplete implementation of CSS 2.1 system colors
    
            Test: fast/css/css2-system-color.html
            
            Based on original patch by Rob Buis.
            
            System colors are retrieved from NSColor as appropriate. If the color is a pattern color
            (and therefore NSColor won't let us retrieve a color from it) we draw a 1x1 image of the
            color and sample that to get a solid color.
    
            * css/CSSStyleSelector.cpp:
            (WebCore::):
            (WebCore::colorForCSSValue):
            * rendering/RenderTheme.cpp:
            (WebCore::RenderTheme::systemColor):
            * rendering/RenderTheme.h:
            * rendering/RenderThemeMac.h:
            * rendering/RenderThemeMac.mm:
            (WebCore::getSystemColor):
            (WebCore::RenderThemeMac::platformColorsDidChange):
            (WebCore::RenderThemeMac::systemColor):
    
    LayoutTests:
    
            Reviewed by Darin.
            
            http://bugs.webkit.org/show_bug.cgi?id=6129
            Incomplete implementation of CSS 2.1 system colors
            
            Add layout test for this bug and update tests for new system-derived colors
    
            * fast/css/css2-system-color.html: Added.
            * platform/mac/fast/css/css2-system-color-expected.checksum: Added.
            * platform/mac/fast/css/css2-system-color-expected.png: Added.
            * platform/mac/fast/css/css2-system-color-expected.txt: Added.
            * platform/mac/fast/css/css2-system-fonts-expected.checksum:
            * platform/mac/tables/mozilla/bugs/bug103533-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28775 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderThemeMac.h
+++ third_party/WebKit/WebCore/rendering/RenderThemeMac.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RenderThemeMac_h
 
 #import "RenderTheme.h"
+#import <wtf/HashMap.h>
 #import <wtf/RetainPtr.h>
 
 #ifdef __OBJC__
@@ -63,9 +64,11 @@ public:
     virtual Color platformActiveSelectionBackgroundColor() const;
     virtual Color platformInactiveSelectionBackgroundColor() const;
     virtual Color activeListBoxSelectionBackgroundColor() const;
+    
+    virtual void platformColorsDidChange();
 
     // System fonts.
-    virtual void systemFont(int propId, FontDescription&) const;
+    virtual void systemFont(int cssValueId, FontDescription&) const;
 
     virtual int minimumMenuListSize(RenderStyle*) const;
 
@@ -77,6 +80,8 @@ public:
     virtual int popupInternalPaddingBottom(RenderStyle*) const;
     
     virtual bool paintCapsLockIndicator(RenderObject*, const RenderObject::PaintInfo&, const IntRect&);
+
+    virtual Color systemColor(int cssValueId) const;
 
 protected:
     // Methods for each appearance value.
@@ -190,6 +195,8 @@ private:
 
     bool m_isSliderThumbHorizontalPressed;
     bool m_isSliderThumbVerticalPressed;
+
+    mutable HashMap<int, RGBA32> m_systemColorCache;
 
     RetainPtr<WebCoreRenderThemeNotificationObserver> m_notificationObserver;
 };
