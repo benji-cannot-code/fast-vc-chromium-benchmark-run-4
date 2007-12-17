f49c788f3e633574d5185463fef715d29aa9e189
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-17 17:53:23 +0000

    Reviewed by Mark Rowe.
            - fix http://bugs.webkit.org/show_bug.cgi?id=16468
              REGRESSION(r28781): Crash running storage/transaction_callback_exception_crash.html
    
            * storage/DatabaseThread.cpp:
            (WebCore::DatabaseThread::dispatchNextTaskIdentifier): Use a RefPtr for the database
            because there's no guarantee it won't lose its last reference otherwise.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28811 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/DatabaseThread.cpp
+++ third_party/WebKit/WebCore/storage/DatabaseThread.cpp
@@ -156,7 +156,7 @@ void* DatabaseThread::databaseThread()
 
 bool DatabaseThread::dispatchNextTaskIdentifier()
 {
-    Database* workDatabase = 0;
+    RefPtr<Database> workDatabase;
     RefPtr<DatabaseTask> task;
 
     {
@@ -180,7 +180,7 @@ bool DatabaseThread::dispatchNextTaskIdentifier()
     if (task) {
         ASSERT(workDatabase);
         workDatabase->resetAuthorizer();
-        task->performTask(workDatabase);
+        task->performTask(workDatabase.get());
         return true;
     }
 
