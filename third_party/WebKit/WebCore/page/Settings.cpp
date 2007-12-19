0f06adbf738955b4cfe89ee47b56795ef0d07a7f
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-19 20:14:07 +0000

    Fix unsigned/enum problem on Win32. r=darin.
    git-svn-id: svn://svn.chromium.org/blink/trunk@28869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Settings.cpp
+++ third_party/WebKit/WebCore/page/Settings.cpp
@@ -325,7 +325,7 @@ void Settings::setFontRenderingMode(FontRenderingMode mode)
 
 FontRenderingMode Settings::fontRenderingMode() const
 {
-    return m_fontRenderingMode;
+    return static_cast<FontRenderinMode>(m_fontRenderingMode);
 }
 
 } // namespace WebCore
