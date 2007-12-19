5f1c54d31c3c49e68ec3f31915715d94f3bba32a
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-19 23:22:11 +0000

    2007-12-19  Alp Toker  <alp@atoker.com>
            Build fix for Pango < 1.18.0 breakage introduced in r28864. Use Fc and
            the Pango backend API in these cases.
    
            * platform/graphics/gtk/FontPlatformDataGtk.cpp:
            (WebCore::FontPlatformData::FontPlatformData):
            (WebCore::FontPlatformData::~FontPlatformData):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28876 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gtk/FontPlatformDataGtk.cpp
+++ third_party/WebKit/WebCore/platform/graphics/gtk/FontPlatformDataGtk.cpp
@@ -27,6 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
+// Use the Pango backend API for compatibility with older Pango versions.
+#define PANGO_ENABLE_BACKEND
+
 #include "config.h"
 #include "FontPlatformData.h"
 
@@ -35,6 +38,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FontDescription.h"
 #include <cairo.h>
 #include <assert.h>
+
+#include <pango/pango.h>
+#include <pango/pangocairo.h>
+
+// Use cairo-ft if a recent enough Pango version isn't available.
+#if !PANGO_VERSION_CHECK(1,18,0)
+#include <cairo-ft.h>
+#include <pango/pangofc-fontmap.h>
+#endif
 
 namespace WebCore {
 
@@ -88,8 +100,31 @@ FontPlatformData::FontPlatformData(const FontDescription& fontDescription, const
     }
 
     // FIXME: should we set some default font?
+#if PANGO_VERSION_CHECK(1,18,0)
     if (m_font)
-        m_scaledFont = pango_cairo_font_get_scaled_font(PANGO_CAIRO_FONT(m_font));
+        m_scaledFont = cairo_scaled_font_reference(pango_cairo_font_get_scaled_font(PANGO_CAIRO_FONT(m_font)));
+#else
+    // This compatibility code for older versions of Pango is not well-tested.
+    if (m_font) {
+        PangoFcFont* fcfont = PANGO_FC_FONT(m_font);
+        cairo_font_face_t* face = cairo_ft_font_face_create_for_pattern(fcfont->font_pattern);
+        double size;
+        if (FcPatternGetDouble(fcfont->font_pattern, FC_PIXEL_SIZE, 0, &size) != FcResultMatch)
+            size = 12.0;
+        cairo_matrix_t fontMatrix;
+        cairo_matrix_init_scale(&fontMatrix, size, size);
+        cairo_font_options_t* fontOptions;
+        if (pango_cairo_context_get_font_options(m_context))
+            fontOptions = cairo_font_options_copy(pango_cairo_context_get_font_options(m_context));
+        else
+            fontOptions = cairo_font_options_create();
+        cairo_matrix_t ctm;
+        cairo_matrix_init_identity(&ctm);
+        m_scaledFont = cairo_scaled_font_create(face, &fontMatrix, &ctm, fontOptions);
+        cairo_font_options_destroy(fontOptions);
+        cairo_font_face_destroy(face);
+    }
+#endif
 
     pango_font_description_free(description);
 }
@@ -124,6 +159,8 @@ bool FontPlatformData::init()
 
 FontPlatformData::~FontPlatformData()
 {
+    if (m_scaledFont)
+        cairo_scaled_font_destroy(m_scaledFont);
 }
 
 bool FontPlatformData::isFixedPitch()
