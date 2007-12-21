60172dfb0056ef1a2f9ec535aa69faca022a0e29
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-21 06:57:52 +0000

    2007-12-20  Jon Honeycutt  <jhoneycutt@apple.com>
            Reviewed by Kevin Decker.
    
            <rdar://problem/5655797> REGRESSION(304-306A10) Safari does not find
            shockwave plugin on freshly installed OS
    
            Look for Director plugin in its native directory.
    
            * plugins/win/PluginDatabaseWin.cpp:
            (WebCore::PluginDatabaseWin::getPluginsInPaths): The Director plugin
            doesn't start with "np," so special case its filename.
            (WebCore::addMacromediaPluginPaths): Add Flash and "Shockwave 10"
            directories to the plugin paths.
            (WebCore::PluginDatabaseWin::defaultPluginPaths):
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/win/PluginDatabaseWin.cpp
+++ third_party/WebKit/WebCore/plugins/win/PluginDatabaseWin.cpp
@@ -189,7 +189,8 @@ PluginSet PluginDatabaseWin::getPluginsInPaths() const
                 continue;
 
             String filename = String(findFileData.cFileName, wcslen(findFileData.cFileName));
-            if (!filename.startsWith("np", false) || !filename.endsWith(".dll", false))
+            if ((!filename.startsWith("np", false) || !filename.endsWith("dll", false)) &&
+                (!equalIgnoringCase(filename, "Plugin.dll") || !it->endsWith("Shockwave 10", false)))
                 continue;
 
             String fullPath = *it + "\\" + filename;
@@ -406,18 +407,20 @@ exit:
     return pluginsPath;
 }
 
-static inline void addFlashPluginPath(Vector<String>& paths)
+static inline void addMacromediaPluginPaths(Vector<String>& paths)
 {
     WCHAR systemDirectoryStr[MAX_PATH];
 
     if (GetSystemDirectory(systemDirectoryStr, _countof(systemDirectoryStr)) == 0)
         return;
 
-    WCHAR flashDirectoryStr[MAX_PATH];
+    WCHAR macromediaDirectoryStr[MAX_PATH];
 
-    PathCombine(flashDirectoryStr, systemDirectoryStr, TEXT("macromed\\Flash"));
+    PathCombine(macromediaDirectoryStr, systemDirectoryStr, TEXT("macromed\\Flash"));
+    paths.append(macromediaDirectoryStr);
 
-    paths.append(flashDirectoryStr);
+    PathCombine(macromediaDirectoryStr, systemDirectoryStr, TEXT("macromed\\Shockwave 10"));
+    paths.append(macromediaDirectoryStr);
 }
 
 Vector<String> PluginDatabaseWin::defaultPluginPaths()
@@ -431,7 +434,7 @@ Vector<String> PluginDatabaseWin::defaultPluginPaths()
     addAdobeAcrobatPluginPath(paths);
     addMozillaPluginPaths(paths);
     addWindowsMediaPlayerPluginPath(paths);
-    addFlashPluginPath(paths);
+    addMacromediaPluginPaths(paths);
 
     return paths;
 }
