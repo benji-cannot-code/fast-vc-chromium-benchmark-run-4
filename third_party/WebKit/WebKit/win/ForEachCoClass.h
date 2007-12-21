592d3709979f86c47603d12b21af296f998dc576
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-21 10:55:28 +0000

    Build fix: cannot use framework-style include for ProgIDMacros.h,         because the file is only created on a leter step.
            * ForEachCoClass.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28932 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/ForEachCoClass.h
+++ third_party/WebKit/WebKit/win/ForEachCoClass.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ForEachCoClass_h
 #define ForEachCoClass_h
 
-#include <WebKit/ProgIDMacros.h>
+#include "ProgIDMacros.h"
 
 #define FOR_EACH_COCLASS(macro) \
     macro(CFDictionaryPropertyBag) \
