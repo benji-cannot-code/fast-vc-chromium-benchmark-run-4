bdec35cfa9eb530ff4a2779e77e0a89a2e34b301
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-22 21:39:37 +0000

    Reviewed by Oliver.
    Fixes: http://bugs.webkit.org/show_bug.cgi?id=15966 (Crash in SVGRootInlineBox::walkTextChunks() on mouse hover)
    
    Add new isSVGRootInlineBox() function to InlineBox to be able to differentiate between RootInlineBox/SVGRootInlineBox.
    SVG assumed it's root inline box (for <text> elements) is always of type SVGRootInlineBox, which is not the case
    for HTML text in <foreignObject>. Text selection doesn't work so far as line box offsets are wrong - which is visible
    in Web Inspector.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@28962 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/SVGRenderTreeAsText.cpp
+++ third_party/WebKit/WebCore/rendering/SVGRenderTreeAsText.cpp
@@ -359,8 +359,6 @@ static inline bool containsInlineTextBox(SVGTextChunk& chunk, SVGInlineTextBox* 
 static inline void writeSVGInlineTextBox(TextStream& ts, SVGInlineTextBox* textBox, int indent)
 {
     SVGRootInlineBox* rootBox = textBox->svgRootInlineBox();
-    ASSERT(rootBox);
-
     if (!rootBox)
         return;
 
