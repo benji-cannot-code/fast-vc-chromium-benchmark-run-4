55cbd70f744d26083de8dd5afd1f109091aece93
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2007-12-30 16:21:36 +0000

    2007-12-30  Mark Rowe  <mrowe@apple.com>
            Unreviewed.  Update WordPress to 2.3.2, which includes security fixes.
    
            * blog/wp-admin/admin.php:
            * blog/wp-admin/includes/file.php:
            * blog/wp-admin/install.php:
            * blog/wp-admin/setup-config.php:
            * blog/wp-app.php:
            * blog/wp-includes/formatting.php:
            * blog/wp-includes/functions.php:
            * blog/wp-includes/pluggable.php:
            * blog/wp-includes/post.php:
            * blog/wp-includes/query.php:
            * blog/wp-includes/taxonomy.php:
            * blog/wp-includes/version.php:
            * blog/wp-includes/wp-db.php:
            * blog/wp-mail.php:
            * blog/wp-settings.php:
            * blog/xmlrpc.php:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29038 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitSite/blog/wp-admin/admin.php
+++ third_party/WebKit/WebKitSite/blog/wp-admin/admin.php
@@ -1,5 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
+define('WP_ADMIN', TRUE);
+
 if ( defined('ABSPATH') )
 	require_once( ABSPATH . 'wp-config.php');
 else
--- third_party/WebKit/WebKitSite/blog/wp-includes/query.php
+++ third_party/WebKit/WebKitSite/blog/wp-includes/query.php
@@ -33,9 +33,9 @@ function wp_reset_query() {
  */
 
 function is_admin () {
-	global $wp_query;
-
-	return ($wp_query->is_admin || (stripos($_SERVER['REQUEST_URI'], 'wp-admin/') !== false));
+	if ( defined('WP_ADMIN') ) 
+		return WP_ADMIN;
+	return false;
 }
 
 function is_archive () {
@@ -643,7 +643,7 @@ class WP_Query {
 		if ('' != $qv['preview'])
 			$this->is_preview = true;
 
-		if ( strpos($_SERVER['PHP_SELF'], 'wp-admin/') !== false )
+		if ( is_admin() )
 			$this->is_admin = true;
 
 		if ( false !== strpos($qv['feed'], 'comments-') ) {
