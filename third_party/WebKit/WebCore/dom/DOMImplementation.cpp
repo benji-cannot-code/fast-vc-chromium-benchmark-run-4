41e918f08dc005ea63a3e3c6ec9f0ebd301692e6
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-02 06:59:10 +0000

    Scripting MIME Types application/ecmascript, application/javascript not viewable         <http://bugs.webkit.org/show_bug.cgi?id=11063>
            Reviewed by Darin.
    
            This patch consolidates the list of acceptable MIME types for JavaScript
            source into the MIMETypeRegistry class, and replaces checks for these
            types with a call to MIMETypeRegistry::isSupportedJavaScriptMIMEType().
    
            No tests added since viewing JavaScript source is not testable.
    
            * dom/DOMImplementation.cpp:
            (WebCore::DOMImplementation::isTextMIMEType): Use
            MIMETypeRegistry::isSupportedJavaScriptMIMEType() instead of a single
            hard-coded MIME type, "application/x-javascript".
    
            * html/HTMLScriptElement.cpp:
            (WebCore::HTMLScriptElement::shouldExecuteAsJavaScript): Moved list of
            JavaScript MIME types from here to
            MIMETypeRegistry::initialiseSupportedJavaScriptMIMETypes().
    
            * platform/MIMETypeRegistry.cpp:
            (WebCore::initialiseSupportedJavaScriptMIMETypes): Added. List of
            MIME types came from HTMLScriptElement::shouldExecuteAsJavaScript().
            (WebCore::initialiseSupportedNonImageMimeTypes): Remove single
            hard-coded MIME type, "application/x-javascript", from the list.
            (WebCore::initialiseMIMETypeRegistry): Initialise
            supportedJavaScriptMIMETypes, then pre-populate supportedNonImageMIMETypes
            with values in supportedJavaScriptMIMETypes.
            (WebCore::MIMETypeRegistry::isSupportedJavaScriptMIMEType): Added.
    
            * platform/MIMETypeRegistry.h: Added isSupportedJavaScriptMIMEType().
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29077 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/DOMImplementation.cpp
+++ third_party/WebKit/WebCore/dom/DOMImplementation.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Image.h"
 #include "ImageDocument.h"
 #include "MediaList.h"
+#include "MIMETypeRegistry.h"
 #include "Page.h"
 #include "PluginDocument.h"
 #include "PlugInInfoStore.h"
@@ -340,11 +341,11 @@ bool DOMImplementation::isXMLMIMEType(const String& mimeType)
 
 bool DOMImplementation::isTextMIMEType(const String& mimeType)
 {
-    if (mimeType == "application/x-javascript" ||
+    if (MIMETypeRegistry::isSupportedJavaScriptMIMEType(mimeType) ||
         (mimeType.startsWith("text/") && mimeType != "text/html" &&
          mimeType != "text/xml" && mimeType != "text/xsl"))
         return true;
-    
+
     return false;
 }
 
