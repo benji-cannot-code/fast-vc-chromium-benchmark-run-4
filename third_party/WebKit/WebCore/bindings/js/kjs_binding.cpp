5b89b58989fb8e784557aee229ac1ac7c79f07c7
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-02 07:03:21 +0000

    - fix release build
            * bindings/js/kjs_binding.cpp:
            (KJS::setDOMException): Initialize to avoid uninitialized variable warning.
            Removed default so we get a warning if there's a missing case.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29078 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/kjs_binding.cpp
+++ third_party/WebKit/WebCore/bindings/js/kjs_binding.cpp
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  *  Copyright (C) 1999-2001 Harri Porten (porten@kde.org)
- *  Copyright (C) 2004, 2005, 2006, 2007 Apple Inc. All rights reserved.
+ *  Copyright (C) 2004, 2005, 2006, 2007, 2008 Apple Inc. All rights reserved.
  *  Copyright (C) 2007 Samuel Weinig <sam@webkit.org>
  *
  *  This library is free software; you can redistribute it and/or
@@ -287,7 +287,7 @@ void setDOMException(ExecState* exec, ExceptionCode ec)
     ExceptionCodeDescription description;
     getExceptionCodeDescription(ec, description);
 
-    JSValue* errorObject;
+    JSValue* errorObject = 0;
     switch (description.type) {
         case DOMExceptionType:
             errorObject = toJS(exec, new DOMCoreException(description));
@@ -311,10 +311,9 @@ void setDOMException(ExecState* exec, ExceptionCode ec)
             errorObject = toJS(exec, new XPathException(description));
             break;
 #endif
-        default:
-            ASSERT_NOT_REACHED();
     }
 
+    ASSERT(errorObject);
     exec->setException(errorObject);
 }
 
