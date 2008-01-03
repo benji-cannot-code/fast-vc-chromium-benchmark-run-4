3edababa824fa25840756d62b9b8abb925b454a4
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-03 02:00:26 +0000

    - fix buffer overruns seen on buildbot
            * platform/text/StringImpl.cpp:
            (WebCore::countCharacter): Added this.
            (WebCore::StringImpl::toCoordsArray): Use countCharacter instead of incorrect find expression.
            (WebCore::StringImpl::toLengthArray): Ditto.
            (WebCore::StringImpl::replace): Added assertions.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29103 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/StringImpl.cpp
+++ third_party/WebKit/WebCore/platform/text/StringImpl.cpp
@@ -201,6 +201,15 @@ Length StringImpl::toLength()
     return parseLength(m_data, m_length);
 }
 
+static int countCharacter(StringImpl* string, UChar character)
+{
+    int count = 0;
+    int length = string->length();
+    for (int i = 0; i < length; ++i)
+            count += (*string)[i] == character;
+    return count;
+}
+
 Length* StringImpl::toCoordsArray(int& len)
 {
     Vector<UChar> spacified(m_length);
@@ -215,7 +224,7 @@ Length* StringImpl::toCoordsArray(int& len)
 
     str = str->simplifyWhiteSpace();
 
-    len = (str->find(' ') >= 0) + 1;
+    len = countCharacter(str.get(), ' ') + 1;
     Length* r = new Length[len];
 
     int i = 0;
@@ -228,6 +237,8 @@ Length* StringImpl::toCoordsArray(int& len)
     }
     r[i] = parseLength(str->characters() + pos, str->length() - pos);
 
+    ASSERT(i == len - 1);
+
     return r;
 }
 
@@ -239,7 +250,7 @@ Length* StringImpl::toLengthArray(int& len)
         return 0;
     }
 
-    len = (str->find(',') >= 0) + 1;
+    len = countCharacter(str.get(), ',') + 1;
     Length* r = new Length[len];
 
     int i = 0;
@@ -250,6 +261,8 @@ Length* StringImpl::toLengthArray(int& len)
         r[i++] = parseLength(str->characters() + pos, pos2 - pos);
         pos = pos2+1;
     }
+
+    ASSERT(i == len - 1);
 
     /* IE Quirk: If the last comma is the last char skip it and reduce len by one */
     if (str->length()-pos > 0)
@@ -810,6 +823,8 @@ PassRefPtr<StringImpl> StringImpl::replace(UChar pattern, StringImpl* replacemen
     srcSegmentLength = m_length - srcSegmentStart;
     memcpy(data.data() + dstOffset, m_data + srcSegmentStart, srcSegmentLength * sizeof(UChar));
 
+    ASSERT(dstOffset + srcSegmentLength == static_cast<int>(data.size()));
+
     return adopt(data);
 }
 
@@ -855,6 +870,8 @@ PassRefPtr<StringImpl> StringImpl::replace(StringImpl* pattern, StringImpl* repl
 
     srcSegmentLength = m_length - srcSegmentStart;
     memcpy(data.data() + dstOffset, m_data + srcSegmentStart, srcSegmentLength * sizeof(UChar));
+
+    ASSERT(dstOffset + srcSegmentLength == static_cast<int>(data.size()));
 
     return adopt(data);
 }
