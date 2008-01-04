792c860fa644720207e31966197dc6c4614b4b6b
alice.liu@apple.com <alice.liu@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-04 01:28:27 +0000

    WebKitTools:
            Reviewed by Darin, Mitz.
    
            fixed <rdar://5130762> mousedowns in different locations increase the clickcount incorrectly
    
            the clickcount should not increase if the last click was at a different location.  it's a new click.
    
            * DumpRenderTree/mac/DumpRenderTree.mm:
            (runTest):
            * DumpRenderTree/mac/EventSendingController.h:
            * DumpRenderTree/mac/EventSendingController.mm:
            (-[EventSendingController mouseDown]):
            (-[EventSendingController mouseUp]):
    
    LayoutTests:
    
            Reviewed by Darin, Mitz.
    
            updating expected results after fixing clickcount for 
            <rdar://5130762> mousedowns in different locations increase the clickcount incorrectly
    
            * fast/forms/focus-selection-input-expected.txt:
            * fast/forms/focus-selection-textarea-expected.txt:
            * platform/mac/fast/events/objc-event-api-expected.txt:
            * platform/mac/fast/inline/dirtyLinesForInline-expected.txt:
            * platform/mac/fast/text/textIteratorNilRenderer-expected.txt:
            * platform/mac/svg/custom/pointer-events-image-expected.txt:
            * platform/mac/svg/custom/pointer-events-path-expected.txt:
            * platform/mac/svg/custom/pointer-events-text-expected.txt:
            * platform/mac/svg/custom/text-hit-test-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/DumpRenderTree.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/DumpRenderTree.mm
@@ -835,7 +835,8 @@ static void runTest(const char *pathOrURL)
 
     if ([WebHistory optionalSharedHistory])
         [WebHistory setOptionalSharedHistory:nil];
-    lastMousePosition = NSMakePoint(0, 0);
+    lastMousePosition = NSZeroPoint;
+    lastClickPosition = NSZeroPoint;
 
     if (currentTest != nil)
         CFRelease(currentTest);
--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/EventSendingController.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/EventSendingController.mm
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 extern "C" void _NSNewKillRingSequence();
 
 NSPoint lastMousePosition;
+NSPoint lastClickPosition;
 NSArray *webkitDomEventNames;
 NSMutableArray *savedMouseEvents; // mouse events sent between mouseDown and mouseUp are stored here, and then executed at once.
 BOOL replayingSavedEvents;
@@ -183,7 +184,8 @@ BOOL replayingSavedEvents;
 - (void)mouseDown
 {
     [[[mainFrame frameView] documentView] layout];
-    if ([self currentEventTime] - lastClick >= 1)
+    if (([self currentEventTime] - lastClick >= 1) ||
+        !NSEqualPoints(lastMousePosition, lastClickPosition))
         clickCount = 1;
     else
         clickCount++;
@@ -247,6 +249,7 @@ BOOL replayingSavedEvents;
     [targetView mouseUp:event];
     down = NO;
     lastClick = [event timestamp];
+    lastClickPosition = lastMousePosition;
     if (draggingInfo) {
         WebView *webView = [mainFrame webView];
         
