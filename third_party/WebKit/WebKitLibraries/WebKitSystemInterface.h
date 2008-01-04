4202066d15314ad890824b903bf8ee914e3138d9
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-04 04:03:46 +0000

    Update libraries for <rdar://problem/4106190> Include "Where from" metadata in drag-and-dropped images
            * WebKitSystemInterface.h:
            * libWebKitSystemInterfaceLeopard.a:
            * libWebKitSystemInterfaceTiger.a:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29141 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
+++ third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
@@ -40,6 +40,7 @@ CFStringEncoding WKGetWebDefaultCFStringEncoding(void);
 float WKSecondsSinceLastInputEvent(void);
 CFStringRef WKPreferRGB32Key(void);
 
+void WKSetMetadataURL(NSString *URLString, NSString *referrer, NSString *path);
 void WKSetNSURLConnectionDefersCallbacks(NSURLConnection *connection, BOOL defers);
 float WKSecondsSinceLastInputEvent(void);
 
