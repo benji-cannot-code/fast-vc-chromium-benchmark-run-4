4ea005cefde1d7db9f3bd1ecaa107276df6bf26f
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-04 14:49:50 +0000

    fix DRT after the changes to QWebPage.
    The great thing is that we actually don't have
    any regressions from the QWebPage change :)
    
    Signed-off-by: Simon
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29167 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/qt/DumpRenderTree.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/qt/DumpRenderTree.cpp
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <qwebpage.h>
 #include <qwebframe.h>
+#include <qwebview.h>
 #include <qwebsettings.h>
 
 #include <unistd.h>
@@ -69,6 +70,7 @@ public:
 
 private:
     DumpRenderTree *m_drt;
+    QWidget *m_view;
 };
 
 WebPage::WebPage(QWidget *parent, DumpRenderTree *drt)
@@ -113,9 +115,13 @@ DumpRenderTree::DumpRenderTree()
     m_controller = new LayoutTestController(this);
     connect(m_controller, SIGNAL(done()), this, SLOT(dump()), Qt::QueuedConnection);
 
-    m_page = new WebPage(0, this);
+    QWebView *view = new QWebView(0);
+    view->resize(QSize(maxViewWidth, maxViewHeight));
+    m_page = new WebPage(view, this);
+    view->setPage(m_page);
     connect(m_page, SIGNAL(frameCreated(QWebFrame *)), this, SLOT(connectFrame(QWebFrame *)));
-    m_page->setViewportSize(QSize(maxViewWidth, maxViewHeight));
+    connectFrame(m_page->mainFrame());
+    
     m_page->mainFrame()->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     m_page->mainFrame()->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     connect(m_page->mainFrame(), SIGNAL(titleChanged(const QString&)),
@@ -127,7 +133,7 @@ DumpRenderTree::DumpRenderTree()
     QObject::connect(this, SIGNAL(quit()), qApp, SLOT(quit()), Qt::QueuedConnection);
     qt_drt_run(true);
     QFocusEvent event(QEvent::FocusIn, Qt::ActiveWindowFocusReason);
-    QApplication::sendEvent(m_page, &event);
+    QApplication::sendEvent(view, &event);
 }
 
 DumpRenderTree::~DumpRenderTree()
