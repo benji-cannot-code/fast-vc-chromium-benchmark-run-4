80aeedbc9d06a39160b6633dd00b0f9c3a1f9d83
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-05 00:15:29 +0000

    Fix another stringImpl leak.
            * platform/text/StringImpl.cpp: remove an extra allocation
            (WebCore::StringImpl::StringImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29182 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/StringImpl.cpp
+++ third_party/WebKit/WebCore/platform/text/StringImpl.cpp
@@ -97,7 +97,6 @@ StringImpl::StringImpl(const StringImpl& str, WithTerminatingNullCharacter)
 
 StringImpl::StringImpl(const char* str, unsigned len)
     : m_length(len)
-    , m_data(newUCharVector(len))
     , m_hash(0)
     , m_inTable(false)
     , m_hasTerminatingNullCharacter(false)
