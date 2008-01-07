10c73653ffa007303a4d0e1abdcb9a669b56fd93
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-07 13:29:00 +0000

    Build fix for the Windows build. MSVC wants to see the full declaration of arguments even when just passing them through.
    git-svn-id: svn://svn.chromium.org/blink/trunk@29226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QPainter>
 #if QT_VERSION >= 0x040400
 #include <QNetworkAccessManager>
+#include <QNetworkRequest>
 #endif
 
 using namespace WebCore;
