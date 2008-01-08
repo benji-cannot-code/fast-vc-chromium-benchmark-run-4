a5ab08c254cad2f14e563722b51ffdc8eb3ede8a
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-08 23:51:28 +0000

    Reviewed by Adam Roben.
            <rdar://problem/5676515> List of scripts and images missing when opening Web Inspector from new window (16567)
    
            InspectorController::didLoadResourceFromMemoryCache was not being called for
            resources that loaded from the memory cache that the FrameLoader's client
            has already been notified about. This fix always calls the InspectorController
            when loading a memory cached resource. No test possible for the Web Inspector.
    
            * loader/DocLoader.cpp:
            (WebCore::DocLoader::checkCacheObjectStatus): Moved most of the logic to
            FrameLoader::loadedResourceFromMemoryCache so the InspectorController can always be notified.
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::didTellClientAboutLoad): Renamed from didTellBridgeAboutLoad.
            (WebCore::FrameLoader::haveToldClientAboutLoad): Renamed from haveToldBridgeAboutLoad.
            (WebCore::FrameLoader::loadResourceSynchronously): Call the renamed didTellClientAboutLoad.
            (WebCore::FrameLoader::loadedResourceFromMemoryCache): Only takes a CachedResource now.
            Always call InspectorController. If the resource's sendResourceLoadCallbacks is false or
            didTellClientAboutLoad is true, do an early return. Otherwise call the client and call
            didTellClientAboutLoad.
            (WebCore::FrameLoader::dispatchDidLoadResourceFromMemoryCache): Removed, work now done
            in FrameLoader::loadedResourceFromMemoryCache.
            * loader/FrameLoader.h: Renamed {didTell,haveTold}BridgeAboutLoad to {didTell,haveTold}ClientAboutLoad.
            Made loadedResourceFromMemoryCache only take a CachedResource. Renamed m_urlsBridgeKnowsAbout to 
            m_urlsClientKnowsAbout.
            * loader/SubresourceLoader.cpp:
            (WebCore::SubresourceLoader::load): Call the renamed didTellClientAboutLoad.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29322 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/SubresourceLoader.cpp
+++ third_party/WebKit/WebCore/loader/SubresourceLoader.cpp
@@ -78,7 +78,7 @@ SubresourceLoader::~SubresourceLoader()
 
 bool SubresourceLoader::load(const ResourceRequest& r)
 {
-    m_frame->loader()->didTellBridgeAboutLoad(r.url().string());
+    m_frame->loader()->didTellClientAboutLoad(r.url().string());
     
     return ResourceLoader::load(r);
 }
