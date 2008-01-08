4ee841b7692440d8b9a2d0fadc430735a49ea590
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-08 01:32:30 +0000

    Build fix.
            * platform/graphics/qt/SimpleFontDataQt.cpp:
            (WebCore::SimpleFontData::SimpleFontData):
            (WebCore::SimpleFontData::~SimpleFontData):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29267 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/SimpleFontDataQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/SimpleFontDataQt.cpp
@@ -25,12 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-FontData::FontData(const FontPlatformData& font, bool customFont, bool loading)
+SimpleFontData::SimpleFontData(const FontPlatformData& font, bool customFont, bool loading)
     : m_font(font), m_isCustomFont(customFont), m_isLoading(loading)
 {
 }
 
-FontData::~FontData()
+SimpleFontData::~SimpleFontData()
 {
 }
 
