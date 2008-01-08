3c798b6a12baac2e15b69aba737ba33e81a08059
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-08 01:53:17 +0000

    Build fix.
            * platform/graphics/qt/GlyphPageTreeNodeQt.cpp:
            (WebCore::GlyphPageTreeNode::pruneTreeCustomFontData):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29273 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GlyphPageTreeNodeQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GlyphPageTreeNodeQt.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-void GlyphPageTreeNode::pruneTreeCustomFontData(const SimpleFontData* fontData)
+void GlyphPageTreeNode::pruneTreeCustomFontData(const FontData* fontData)
 {
     QFontDatabase::removeApplicationFont(fontData->platformData().handle);
 }
