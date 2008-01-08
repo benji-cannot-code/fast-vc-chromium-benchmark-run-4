69cfeca24616f55a1d6ebee835371f84716a5f11
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-08 01:37:21 +0000

    2008-01-07  Jon Honeycutt  <jhoneycutt@apple.com>
            Reviewed by Hyatt.
    
            <rdar://problem/5673489> Safari does not render windowless plugins in an
            iframe when opacity < 1.0
    
            Plugins in transparency layers handle their own world transforms, so
            only apply the horizontal/vertical transform if we are not in a
            transparency layer.
    
            * platform/graphics/GraphicsContext.h: Add a Windows-platform-only
            inTransparencyLayer() function
            * platform/win/GraphicsContextWin.cpp:
            (WebCore::GraphicsContext::getWindowsContext): Use inTransparencyLayer()
            (WebCore::GraphicsContext::inTransparencyLayer):
            (WebCore::GraphicsContext::releaseWindowsContext): Use
            inTransparencyLayer()
            * plugins/win/PluginViewWin.cpp:
            (WebCore::PluginViewWin::paint): When retrieving the HDC, use the rect
            relative to the window. Pass m_isTransparent to
            get/releaseWindowsContext(). Only set the world transform if we are not
            in a transparency layer.
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29270 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/GraphicsContextWin.cpp
+++ third_party/WebKit/WebCore/platform/win/GraphicsContextWin.cpp
@@ -80,7 +80,7 @@ GraphicsContext::GraphicsContext(HDC hdc)
 
 HDC GraphicsContext::getWindowsContext(const IntRect& dstRect, bool supportAlphaBlend)
 {
-    if (m_data->m_transparencyCount) {
+    if (inTransparencyLayer()) {
         if (dstRect.isEmpty())
             return 0;
 
@@ -135,9 +135,11 @@ HDC GraphicsContext::getWindowsContext(const IntRect& dstRect, bool supportAlpha
     return m_data->m_hdc;
 }
 
+bool GraphicsContext::inTransparencyLayer() const { return m_data->m_transparencyCount; }
+
 void GraphicsContext::releaseWindowsContext(HDC hdc, const IntRect& dstRect, bool supportAlphaBlend)
 {
-    if (hdc && m_data->m_transparencyCount) {
+    if (hdc && inTransparencyLayer()) {
         if (dstRect.isEmpty())
             return;
 
