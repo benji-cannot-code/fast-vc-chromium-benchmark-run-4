1a5c9c3ecc7ea8076870b5d2c1ba6ef4e5b87a6b
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-08 13:31:39 +0000

    Reviewed by Mark.
            - remove duplicate definition of getElementById from HTMLDocument IDL (and ObjC bindings)
    
            * bindings/objc/PublicDOMInterfaces.h:
            * html/HTMLDocument.idl:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/PublicDOMInterfaces.h
+++ third_party/WebKit/WebCore/bindings/objc/PublicDOMInterfaces.h
@@ -364,7 +364,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)close;
 - (void)write:(NSString *)text;
 - (void)writeln:(NSString *)text;
-- (DOMElement *)getElementById:(NSString *)elementId;
 @end
 
 @interface DOMHTMLElement : DOMElement
