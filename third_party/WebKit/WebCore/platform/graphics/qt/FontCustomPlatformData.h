6ce5b261af295970be552d77e91d246566efe580
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-08 03:48:52 +0000

    Build fix.
            * platform/graphics/qt/FontCustomPlatformData.cpp:
            (WebCore::FontCustomPlatformData::~FontCustomPlatformData): Added.
            Calls QFontDatabase::removeApplicationFont().
            * platform/graphics/qt/FontCustomPlatformData.h:
            * platform/graphics/qt/GlyphPageTreeNodeQt.cpp:
            (WebCore::GlyphPageTreeNode::pruneTreeCustomFontData): Removed the
            implementation because Qt does not use the WebCore glyph cache.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29283 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/FontCustomPlatformData.h
+++ third_party/WebKit/WebCore/platform/graphics/qt/FontCustomPlatformData.h
@@ -31,6 +31,8 @@ class SharedBuffer;
 class FontPlatformData;
 
 struct FontCustomPlatformData : Noncopyable {
+    ~FontCustomPlatformData();
+
     int handle; // for use with QFontDatabase::addApplicationFont/removeApplicationFont
 
     FontPlatformData fontPlatformData(int size, bool bold, bool italic);
--- third_party/WebKit/WebCore/platform/graphics/qt/GlyphPageTreeNodeQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GlyphPageTreeNodeQt.cpp
@@ -23,15 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "GlyphPageTreeNode.h"
 
-#include "FontPlatformData.h"
-#include "SimpleFontData.h"
-#include <QFontDatabase>
-
 namespace WebCore {
 
 void GlyphPageTreeNode::pruneTreeCustomFontData(const FontData* fontData)
 {
-    QFontDatabase::removeApplicationFont(fontData->platformData().handle);
 }
 
 }
