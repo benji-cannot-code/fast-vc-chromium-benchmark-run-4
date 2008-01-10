b08aa7b70fe5c61cea9ea29053bc3797475fafe1
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-10 16:38:33 +0000

    Documentation for QWebFrame.
    git-svn-id: svn://svn.chromium.org/blink/trunk@29356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.h
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.h
@@ -55,7 +55,9 @@ class QWebFrameData;
 class QWEBKIT_EXPORT QWebFrame : public QObject
 {
     Q_OBJECT
-protected:
+    Q_PROPERTY(Qt::ScrollBarPolicy verticalScrollBarPolicy READ verticalScrollBarPolicy WRITE setVerticalScrollBarPolicy)
+    Q_PROPERTY(Qt::ScrollBarPolicy horizontalScrollBarPolicy READ horizontalScrollBarPolicy WRITE setHorizontalScrollBarPolicy)
+private:
     QWebFrame(QWebPage *parent, QWebFrameData *frameData);
     QWebFrame(QWebFrame *parent, QWebFrameData *frameData);
     ~QWebFrame();
@@ -86,6 +88,7 @@ public:
     
     QString name() const;
 
+    QWebFrame *parentFrame() const;
     QList<QWebFrame*> childFrames() const;
 
     Qt::ScrollBarPolicy verticalScrollBarPolicy() const;
@@ -93,7 +96,7 @@ public:
     Qt::ScrollBarPolicy horizontalScrollBarPolicy() const;
     void setHorizontalScrollBarPolicy(Qt::ScrollBarPolicy);
 
-    void render(QPainter *painter, const QRegion &source);
+    void render(QPainter *painter, const QRegion &clip);
     void layout();
 
     QPoint pos() const;
