95314154524af8fdf471072e939eb7e718fd8731
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-11 12:40:32 +0000

    Qt build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@29403 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/qt/TemporaryLinkStubs.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 
 #include "AXObjectCache.h"
+#include "CString.h"
 #include "CachedResource.h"
 #include "CookieJar.h"
 #include "Cursor.h"
