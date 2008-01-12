4f9c02c26576b0d752a35ff42b1f5f102a47a5f6
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-12 18:05:44 +0000

    - try to fix Qt build
            * kjs/function.cpp: Added include of scope_chain_mark.h.
            * kjs/scope_chain_mark.h: Added multiple-include guards.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29439 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/function.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/function.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "lexer.h"
 #include "nodes.h"
 #include "operations.h"
+#include "scope_chain_mark.h"
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
--- third_party/WebKit/JavaScriptCore/kjs/scope_chain_mark.h
+++ third_party/WebKit/JavaScriptCore/kjs/scope_chain_mark.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- *  Copyright (C) 2003, 2006 Apple Inc.
+ *  Copyright (C) 2003, 2006, 2008 Apple Inc. All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -19,9 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
-#include "scope_chain.h"
+#ifndef scope_chain_mark_h
+#define scope_chain_mark_h
+
 #include "Activation.h"
-#include "object.h"
+#include "scope_chain.h"
 
 namespace KJS {
 
@@ -44,3 +46,5 @@ namespace KJS {
     }
 
 } // namespace KJS
+
+#endif
