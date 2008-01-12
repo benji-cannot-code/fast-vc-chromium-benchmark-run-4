9cd3d32bc836d72d201f9d630e73d1a6b406dda1
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-12 08:37:20 +0000

    Reviewed by Sam.
            - fixed <rdar://problem/5556374> REGRESSION: cross-domain error when one URL uses an explicit port number and another doesn't
            
            * platform/SecurityOrigin.cpp:
            (WebCore::isDefaultPortForProtocol):
            (WebCore::SecurityOrigin::SecurityOrigin):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29430 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/SecurityOrigin.cpp
+++ third_party/WebKit/WebCore/platform/SecurityOrigin.cpp
@@ -39,6 +39,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+static bool isDefaultPortForProtocol(unsigned short port, String protocol)
+{
+    if (protocol.isEmpty())
+        return false;
+
+    static HashMap<String, unsigned> defaultPorts;
+    if (defaultPorts.isEmpty()) {
+        defaultPorts.set("http", 80);
+        defaultPorts.set("https", 443);
+        defaultPorts.set("ftp", 21);
+        defaultPorts.set("ftps", 990);
+    }
+    return defaultPorts.get(protocol) == port;
+}
+
 SecurityOrigin::SecurityOrigin(const String& protocol, const String& host, unsigned short port)
     : m_protocol(protocol.lower())
     , m_host(host.lower())
@@ -54,6 +69,12 @@ SecurityOrigin::SecurityOrigin(const String& protocol, const String& host, unsig
     // data: URLs are not allowed access to anything other than themselves.
     if (m_protocol == "data")
         m_noAccess = true;
+
+
+    if (isDefaultPortForProtocol(m_port, m_protocol)) {
+        m_port = 0;
+        m_portSet = false;
+    }   
 }
 
 bool SecurityOrigin::isEmpty() const
