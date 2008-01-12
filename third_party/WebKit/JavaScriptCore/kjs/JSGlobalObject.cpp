200f451106d6128d7cdbc0d76cb69f39c378b93c
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-12 14:06:33 +0000

    2008-01-12  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
            Reviewed by Mark Rowe.
    
            Fixes leaking of ActivationStackNode objects.
    
            * kjs/JSGlobalObject.cpp:
            (KJS::JSGlobalObject::deleteActivationStack):
            (KJS::JSGlobalObject::~JSGlobalObject):
            (KJS::JSGlobalObject::init):
            (KJS::JSGlobalObject::reset):
            * kjs/JSGlobalObject.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29435 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/JSGlobalObject.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/JSGlobalObject.cpp
@@ -91,6 +91,12 @@ static inline unsigned getCurrentTime() {
 
 JSGlobalObject* JSGlobalObject::s_head = 0;
 
+void JSGlobalObject::deleteActivationStack()
+{
+    for (ActivationStackNode* currentNode = d()->activations; currentNode; currentNode = currentNode->prev)
+        delete currentNode;
+}
+
 JSGlobalObject::~JSGlobalObject()
 {
     ASSERT(JSLock::currentThreadIsHoldingLock());
@@ -103,6 +109,8 @@ JSGlobalObject::~JSGlobalObject()
     s_head = d()->next;
     if (s_head == this)
         s_head = 0;
+    
+    deleteActivationStack();
     
     delete d();
 }
@@ -128,6 +136,8 @@ void JSGlobalObject::init()
     d()->currentExec = 0;
     d()->recursion = 0;
     d()->debugger = 0;
+    
+    d()->activations = 0;
     
     reset(prototype());
 }
@@ -203,7 +213,11 @@ void JSGlobalObject::reset(JSValue* prototype)
 
     ExecState* exec = &d()->globalExec;
 
-    d()->activations = new ActivationStackNode;
+    deleteActivationStack();
+    
+    ActivationStackNode* newStackNode = new ActivationStackNode;
+    newStackNode->prev = 0;    
+    d()->activations = newStackNode;
     d()->activationCount = 0;
 
     // Prototypes
