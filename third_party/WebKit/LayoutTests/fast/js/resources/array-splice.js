cd5818a966880b57853b4c0537a5d066960c521d
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-14 09:12:18 +0000

    Reviewed by David Kilzer.
            http://bugs.webkit.org/show_bug.cgi?id=16787
            array.splice() with 1 element not working
    
            Test: fast/js/array-splice.html
    
            * kjs/array_object.cpp:
            (KJS::ArrayProtoFuncSplice::callAsFunction): Implement this Mozilla extension, and fix
            some other edge cases.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/array-splice.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This tests array.splice behavior."
+);
+
+var arr = ['a','b','c','d'];
+shouldBe("arr", "['a','b','c','d']");
+shouldBe("arr.splice(2)", "['c','d']");
+shouldBe("arr", "['a','b']");
+shouldBe("arr.splice(0)", "['a','b']");
+shouldBe("arr", "[]")
+
+arr = ['a','b','c','d'];
+shouldBe("arr.splice()", "undefined")
+shouldBe("arr", "['a','b','c','d']");
+shouldBe("arr.splice(undefined)", "['a','b','c','d']")
+shouldBe("arr", "[]");
+
+arr = ['a','b','c','d'];
+shouldBe("arr.splice(null)", "['a','b','c','d']")
+shouldBe("arr", "[]");
+
+arr = ['a','b','c','d'];
+shouldBe("arr.splice(100)", "[]")
+shouldBe("arr", "['a','b','c','d']");
+shouldBe("arr.splice(-1)", "['d']")
+shouldBe("arr", "['a','b','c']");
+
+shouldBe("arr.splice(2, undefined)", "[]")
+shouldBe("arr.splice(2, null)", "[]")
+shouldBe("arr.splice(2, -1)", "[]")
+shouldBe("arr", "['a','b','c']");
+shouldBe("arr.splice(2, 100)", "['c']")
+shouldBe("arr", "['a','b']");
+
+var successfullyParsed = true;
