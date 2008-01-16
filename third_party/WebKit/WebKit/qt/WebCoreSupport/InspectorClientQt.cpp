a5cc53cefec6a0f8af85bbf620a4f4bfa0438ce0
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-16 09:37:48 +0000

    * Make the InspectorClientView inherit from QWebView instead of QWidget. This   way paintEvent, mouse{Press,Release}Event and other events get forwarded to   the QWebPage/WebInspector automatically.
    git-svn-id: svn://svn.chromium.org/blink/trunk@29513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/InspectorClientQt.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/InspectorClientQt.cpp
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "qwebpage.h"
 #include "qwebpage_p.h"
+#include "qwebview.h"
+
 #include <QtCore/QCoreApplication>
 
 #include "InspectorController.h"
@@ -47,27 +49,25 @@ class InspectorClientWebPage : public QWebPage
 public:
     QWebPage* createWindow()
     {
-        QWidget *w = new QWidget(0);
+        QWidget *w = new QWebView(0);
         QWebPage *page = new QWebPage(w);
         page->setView(w);
         connect(page, SIGNAL(destroyed()), w, SLOT(deleteLater()));
         return page;
     }
 };
-    
 
-class InspectorClientView : public QWidget {
+
+class InspectorClientView : public QWebView {
 public:
     InspectorClientView(InspectorController* controller)
-        : QWidget(0)
+        : QWebView(0)
         , m_controller(controller)
-        , m_page(new InspectorClientWebPage)
     {
-        m_page->setView(this);
-        connect(m_page, SIGNAL(destroyed()), SLOT(deleteLater()));
+        setPage(new InspectorClientWebPage);
+        connect(page(), SIGNAL(destroyed()), SLOT(deleteLater()));
     }
 
-    QWebPage* page() const { return m_page; }
 protected:
     void hideEvent(QHideEvent* ev)
     {
@@ -83,7 +83,6 @@ protected:
 
 private:
     InspectorController* m_controller;
-    QWebPage* m_page;
 };
 
 
