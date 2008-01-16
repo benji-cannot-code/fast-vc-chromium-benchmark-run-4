d3695cb980471826d476e4e6eb0995f314018205
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-16 21:56:43 +0000

    WebCore:
            Fix for http://bugs.webkit.org/show_bug.cgi?id=14846, cell padding can't be changed dynamically.
    
            Reviewed by Eric Seidel
    
            Added fast/table/dynamic-cellpadding.html
    
            * html/HTMLTableElement.cpp:
            (WebCore::HTMLTableElement::parseMappedAttribute):
            * rendering/RenderTable.cpp:
            (WebCore::RenderTable::setCellPadding):
            * rendering/RenderTable.h:
    
    LayoutTests:
    
            Reviewed by Eric Seidel
    
            http://bugs.webkit.org/show_bug.cgi?id=14846
    
            * fast/table/dynamic-cellpadding.html: Added.
            * platform/mac/fast/table/dynamic-cellpadding-expected.checksum: Added.
            * platform/mac/fast/table/dynamic-cellpadding-expected.png: Added.
            * platform/mac/fast/table/dynamic-cellpadding-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29539 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTable.h
+++ third_party/WebKit/WebCore/rendering/RenderTable.h
@@ -87,7 +87,7 @@ public:
     const Color& bgColor() const { return style()->backgroundColor(); }
 
     unsigned cellPadding() const { return m_padding; }
-    void setCellPadding(unsigned p) { m_padding = p; }
+    void setCellPadding(unsigned);
 
     int outerBorderTop() const;
     int outerBorderBottom() const;
