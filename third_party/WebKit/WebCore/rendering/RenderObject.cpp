d684c82567d2a0824aebf4551f6e34c15714e74a
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-17 06:20:16 +0000

    WebCore:
            Fix for http://bugs.webkit.org/show_bug.cgi?id=16611
    
            Make sure vertical-align length values are offset from their parents rather than being absolute to the
            line.
    
            Reviewed by olliej
    
            Added fast/css/vertical-align-lengths.html
    
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::getVerticalPosition):
    
    LayoutTests:
    
           Fix for http://bugs.webkit.org/show_bug.cgi?id=16611
    
           Reviewed by olliej
    
            * ChangeLog:
            * fast/css/vertical-align-lengths.html: Added.
            * platform/mac/fast/css/vertical-align-lengths-expected.checksum: Added.
            * platform/mac/fast/css/vertical-align-lengths-expected.png: Added.
            * platform/mac/fast/css/vertical-align-lengths-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29553 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/WebCore/rendering/RenderObject.cpp
@@ -2688,8 +2688,6 @@ short RenderObject::getVerticalPosition(bool firstLine) const
         vpos = PositionTop;
     else if (va == BOTTOM)
         vpos = PositionBottom;
-    else if (va == LENGTH)
-        vpos = -style()->verticalAlignLength().calcValue(lineHeight(firstLine));
     else {
         bool checkParent = parent()->isInline() && !parent()->isInlineBlockOrInlineTable() && parent()->style()->verticalAlign() != TOP && parent()->style()->verticalAlign() != BOTTOM;
         vpos = checkParent ? parent()->verticalPositionHint(firstLine) : 0;
@@ -2714,6 +2712,8 @@ short RenderObject::getVerticalPosition(bool firstLine) const
                 vpos -= style(firstLine)->font().descent();
         } else if (va == BASELINE_MIDDLE)
             vpos += -lineHeight(firstLine) / 2 + baselinePosition(firstLine);
+        else if (va == LENGTH)
+            vpos -= style()->verticalAlignLength().calcValue(lineHeight(firstLine));
     }
 
     return vpos;
