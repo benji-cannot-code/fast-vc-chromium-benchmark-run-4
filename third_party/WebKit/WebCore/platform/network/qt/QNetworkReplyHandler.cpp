f6073e30b4ace22b308fd49274abf314821253c0
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-17 13:01:35 +0000

    Re-enable gzip compression as accepted encoding, now that the bug is fixed in Qt 4.4's network module.
    git-svn-id: svn://svn.chromium.org/blink/trunk@29569 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
@@ -59,9 +59,6 @@ QNetworkReplyHandler::QNetworkReplyHandler(ResourceHandle *handle)
 
     m_request = r.toNetworkRequest();
 
-    // ### temporary workaround for gzip decoding bug in the new QHttp
-    m_request.setRawHeader("Accept-Encoding", "identity");
-
     start();
 }
 
