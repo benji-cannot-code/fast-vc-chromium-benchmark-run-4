bd14bc52d11ad9069c27cfc92bbe0c20382d7f85
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-17 19:06:55 +0000

    Qt/GTK+ build fix.
            * platform/gtk/ScrollViewGtk.cpp:
            (WebCore::ScrollView::ScrollViewPrivate::isActive):
            * platform/qt/ScrollViewQt.cpp:
            (WebCore::ScrollView::ScrollViewPrivate::isActive):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29586 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/ScrollViewGtk.cpp
+++ third_party/WebKit/WebCore/platform/gtk/ScrollViewGtk.cpp
@@ -94,6 +94,7 @@ public:
 
     virtual void valueChanged(Scrollbar*);
     virtual IntRect windowClipRect() const;
+    virtual bool isActive() const;
 
     static void adjustmentChanged(GtkAdjustment*, gpointer);
 
@@ -240,6 +241,12 @@ void ScrollView::ScrollViewPrivate::valueChanged(Scrollbar* bar)
 IntRect ScrollView::ScrollViewPrivate::windowClipRect() const
 {
     return static_cast<const FrameView*>(view)->windowClipRect(false);
+}
+
+bool ScrollView::ScrollViewPrivate::isActive() const
+{
+    Page* page = static_cast<const FrameView*>(m_view)->frame()->page();
+    return page && page->focusController()->isActive();
 }
 
 ScrollView::ScrollView()
