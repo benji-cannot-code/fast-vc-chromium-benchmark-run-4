afdbc396af0ce7c358f05cf7cd2f0f831022209b
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-18 21:05:16 +0000

    JavaScriptCore:
            Reviewed by Geoff.
    
            - Correctly report cost of appended strings to trigger GC.
    
            * kjs/ustring.cpp:
            (KJS::):
            (KJS::UString::Rep::create):
            (KJS::UString::UString): Don't create unnecssary objects.
            (KJS::UString::cost): Report cost if necessary but also keep track of 
            reported cost.
            * kjs/ustring.h:
    
    LayoutTests:
    
            Reviewed by Geoff.
    
            - Correctly report cost of appended strings to trigger GC.
    
            * fast/js/garbage-collect-after-string-appends-expected.txt: Added.
            * fast/js/garbage-collect-after-string-appends.html: Added.
            * fast/js/resources/garbage-collect-after-string-appends.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29639 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/garbage-collect-after-string-appends.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This test checks whether the GC collects after string appends."
+);
+
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
+if (window.GCController)
+    GCController.collect();
+
+
+// str has 150 chars in it (which is greater than the limit of the GC to ignore which I believe is at 128).
+var str = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+var count = 500;
+for (var i = 0; i < count; ++i) {
+    str += "b";
+}
+
+// str has 128 chars in it.
+str = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+count = 10;
+for (var i = 0; i < count; ++i) {
+    str += str;
+}
+
+var jsObjCount = 0;
+if (window.GCController)
+    jsObjCount = GCController.getJSObjectCount();
+
+if (jsObjCount <= 500 && jsObjCount > 0)
+    testPassed("Garbage Collector triggered")
+else
+    testFailed("Garbage Collector NOT triggered. Number of JSObjects: " + jsObjCount);
+
+var successfullyParsed = true;
