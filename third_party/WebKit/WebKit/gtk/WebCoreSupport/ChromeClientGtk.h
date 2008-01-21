79265171c3655c0aaa3def02cbd3f2b39b08ed60
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-21 22:43:52 +0000

    2008-01-21  Alp Toker  <alp@atoker.com>
            GTK+ build fix for breakage introduced in r29698.
    
            * WebCoreSupport/ChromeClientGtk.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29699 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/WebCoreSupport/ChromeClientGtk.h
+++ third_party/WebKit/WebKit/gtk/WebCoreSupport/ChromeClientGtk.h
@@ -94,7 +94,7 @@ namespace WebKit {
 
         virtual void print(WebCore::Frame*);
 
-        virtual void exceededDatabaseQuota(Frame*, const String&);
+        virtual void exceededDatabaseQuota(WebCore::Frame*, const WebCore::String&);
 
     private:
         WebKitWebView* m_webView;
