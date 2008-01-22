5cd05e6e3aee55ec1c4d162e8be75dab02ac5e27
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-22 10:03:40 +0000

    Lars Knoll <lars@trolltech.com>
    cleanup the CookieJarQt implementation and implement cookiesEnabled.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29715 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/CookieJarQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/CookieJarQt.cpp
@@ -46,21 +46,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+#if QT_VERSION >= 0x040400
+static QNetworkCookieJar *cookieJar(const Document *document)
+{
+    Frame *frame = document->frame();
+    if (!frame)
+        return 0;
+    FrameLoader *loader = frame->loader();
+    if (!loader)
+        return 0;
+    QWebFrame* webFrame = static_cast<FrameLoaderClientQt*>(loader->client())->webFrame();
+    QWebPage* page = webFrame->page();
+    QNetworkAccessManager* manager = page->networkAccessManager();
+    QNetworkCookieJar* jar = manager->cookieJar();
+    return jar;
+}
+#endif
+
 void setCookies(Document* document, const KURL& url, const KURL& policyURL, const String& value)
 {
     QUrl u(url);
     QUrl p(policyURL);
 #if QT_VERSION >= 0x040400
-    Frame *frame = document->frame();
-    if (!frame)
+    QNetworkCookieJar* jar = cookieJar(document);
+    if (!jar)
         return;
-    FrameLoader *loader = frame->loader();
-    if (!loader)
-        return;
-    QWebFrame* webFrame = static_cast<FrameLoaderClientQt*>(loader->client())->webFrame();
-    QWebPage* page = webFrame->page();
-    QNetworkAccessManager* manager = page->networkAccessManager();
-    QNetworkCookieJar* jar = manager->cookieJar();
 
     QList<QNetworkCookie> cookies = QNetworkCookie::parseCookies(QString(value).toAscii());
     jar->setCookiesFromUrl(cookies, p);
@@ -73,16 +83,9 @@ String cookies(const Document* document, const KURL& url)
 {
     QUrl u(url);
 #if QT_VERSION >= 0x040400
-    Frame *frame = document->frame();
-    if (!frame)
+    QNetworkCookieJar* jar = cookieJar(document);
+    if (!jar)
         return String();
-    FrameLoader *loader = frame->loader();
-    if (!loader)
-        return String();
-    QWebFrame* webFrame = static_cast<FrameLoaderClientQt*>(loader->client())->webFrame();
-    QWebPage* page = webFrame->page();
-    QNetworkAccessManager* manager = page->networkAccessManager();
-    QNetworkCookieJar* jar = manager->cookieJar();
 
     QList<QNetworkCookie> cookies = jar->cookiesForUrl(u);
     if (cookies.isEmpty())
@@ -105,8 +108,8 @@ String cookies(const Document* document, const KURL& url)
 bool cookiesEnabled(const Document* document)
 {
 #if QT_VERSION >= 0x040400
-    // ###
-    return true;
+    QNetworkCookieJar* jar = cookieJar(document);
+    return (jar != 0);
 #else
     return QCookieJar::cookieJar()->isEnabled();
 #endif
