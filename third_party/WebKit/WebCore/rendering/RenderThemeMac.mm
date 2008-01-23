940ce6992d299c3910c6da4830ef1c1355930e71
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-23 04:01:09 +0000

    Reviewed by Adele Peterson.
            - fix http://bugs.webkit.org/show_bug.cgi?id=16905
              <rdar://problem/5692407> REGRESSION (3.0.4-TOT): "menu" pseudocolor is badly chosen
    
            The regression test for this is fast/css/css2-system-color.html which
            is currently disabled.
    
            * rendering/RenderThemeMac.mm:
            (WebCore::menuBackgroundColor): Added. Uses HIThemeDrawMenuBackground
            to draw the menu item background into a bitmap graphics context and
            returns the color of the pixel at (0, 0).
            (WebCore::RenderThemeMac::systemColor): Changed to call
            menuBackgroundColor for the CSS2 menu color.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderThemeMac.mm
+++ third_party/WebKit/WebCore/rendering/RenderThemeMac.mm
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "RenderView.h"
 #import "SharedBuffer.h"
 #import "WebCoreSystemInterface.h"
+#import <Carbon/Carbon.h>
 #import <Cocoa/Cocoa.h>
 #import <wtf/RetainPtr.h>
 #import <math.h>
@@ -241,6 +242,34 @@ static RGBA32 convertNSColorToColor(NSColor *color)
     return makeRGB(pixel[0], pixel[1], pixel[2]);
 }
 
+static RGBA32 menuBackgroundColor()
+{
+    NSBitmapImageRep *offscreenRep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:nil
+                                                                             pixelsWide:1
+                                                                             pixelsHigh:1
+                                                                          bitsPerSample:8
+                                                                        samplesPerPixel:4
+                                                                               hasAlpha:YES
+                                                                               isPlanar:NO
+                                                                         colorSpaceName:NSCalibratedRGBColorSpace
+                                                                            bytesPerRow:4
+                                                                           bitsPerPixel:32];
+
+    CGContextRef context = static_cast<CGContextRef>([[NSGraphicsContext graphicsContextWithBitmapImageRep:offscreenRep] graphicsPort]);
+    CGRect rect = CGRectMake(0, 0, 1, 1);
+    HIThemeMenuDrawInfo drawInfo;
+    drawInfo.version =  0;
+    drawInfo.menuType = kThemeMenuTypePopUp;
+    HIThemeDrawMenuBackground(&rect, &drawInfo, context, kHIThemeOrientationInverted);
+
+    NSUInteger pixel[4];
+    [offscreenRep getPixel:pixel atX:0 y:0];
+
+    [offscreenRep release];
+
+    return makeRGB(pixel[0], pixel[1], pixel[2]);
+}
+
 void RenderThemeMac::platformColorsDidChange()
 {
     m_systemColorCache.clear();
@@ -309,7 +338,7 @@ Color RenderThemeMac::systemColor(int cssValueId) const
             color = convertNSColorToColor([NSColor textColor]);
             break;
         case CSS_VAL_MENU:
-            color = convertNSColorToColor([NSColor selectedMenuItemColor]);
+            color = menuBackgroundColor();
             break;
         case CSS_VAL_MENUTEXT:
             color = convertNSColorToColor([NSColor selectedMenuItemTextColor]);
