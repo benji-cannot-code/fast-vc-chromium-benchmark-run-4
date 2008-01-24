2c0a2b45b08b56fc43226fa507e542f45960a045
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-24 21:49:25 +0000

    http://bugs.webkit.org/show_bug.cgi?id=16982
            Make sure to make <head> the current block if it is created before a <body> already exists.
    
            Reviewed by Dan
    
            * html/HTMLParser.cpp:
            (WebCore::HTMLParser::createHead):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29775 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLParser.cpp
+++ third_party/WebKit/WebCore/html/HTMLParser.cpp
@@ -1381,6 +1381,12 @@ void HTMLParser::createHead()
     document->documentElement()->insertBefore(head, body, ec);
     if (ec)
         head = 0;
+        
+    // If the body does not exist yet, then the <head> should be pushed as the current block.
+    if (head && !body) {
+        pushBlock(head->localName(), head->tagPriority());
+        setCurrent(head);
+    }
 }
 
 PassRefPtr<Node> HTMLParser::handleIsindex(Token* t)
