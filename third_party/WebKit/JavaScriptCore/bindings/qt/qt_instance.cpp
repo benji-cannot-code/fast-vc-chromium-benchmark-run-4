5b26cc8ac87b72f80ea02408e43ed23ef19c50fa
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-24 13:41:14 +0000

    Michael Goddard <michael.goddard@trolltech.com>
    Code style cleanups.
    Remove spaces and unneeded declared parameter names.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bindings/qt/qt_instance.cpp
+++ third_party/WebKit/JavaScriptCore/bindings/qt/qt_instance.cpp
@@ -46,14 +46,14 @@ static InstanceJSObjectMap cachedObjects;
 // Derived RuntimeObject
 class QtRuntimeObjectImp : public RuntimeObjectImp {
     public:
-        QtRuntimeObjectImp(Instance *instance);
+        QtRuntimeObjectImp(Instance*);
         ~QtRuntimeObjectImp();
         virtual void invalidate();
     protected:
         void removeFromCache();
 };
 
-QtRuntimeObjectImp::QtRuntimeObjectImp(Instance *instance)
+QtRuntimeObjectImp::QtRuntimeObjectImp(Instance* instance)
     : RuntimeObjectImp(instance)
 {
 }
@@ -72,7 +72,7 @@ void QtRuntimeObjectImp::invalidate()
 void QtRuntimeObjectImp::removeFromCache()
 {
     JSLock lock;
-    QtInstance *key = cachedObjects.key(this);
+    QtInstance* key = cachedObjects.key(this);
     if (key)
         cachedObjects.remove(key);
 }
