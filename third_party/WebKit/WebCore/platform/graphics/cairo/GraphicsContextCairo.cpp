2be4c73f01ae10221b2318205c8841e69f256e6f
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-28 08:18:43 +0000

    Gtk build fix
    git-svn-id: svn://svn.chromium.org/blink/trunk@29828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cairo/GraphicsContextCairo.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cairo/GraphicsContextCairo.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if PLATFORM(CAIRO)
 
+#include "AffineTransform.h"
 #include "CairoPath.h"
 #include "FloatRect.h"
 #include "Font.h"
