bf262001d9c2865bf2a7f649a1ffdc51df235e73
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-30 19:40:05 +0000

    2008-01-30  Jon Honeycutt  <jhoneycutt@apple.com>
            Reviewed by Darin.
    
            <rdar://problem/5669317> Crash closing pop up window with Real Player
            content
    
            Some RealPlayer versions hang on unload. To prevent this hang, don't
            unload versions since the last known unloadable version.
            <rdar://problem/5713147> tracks revisiting this when the bug in
            RealPlayer is fixed and we can again unload the plug-in.
    
            * plugins/win/PluginPackageWin.cpp:
            (WebCore::PluginPackageWin::compareFileVersion): Compare the plug-in's
            file version to the passed file version, returning -1, 0, or 1 if the
            plug-in's version is less than, equal to, or greater than the version
            passed.
            (WebCore::PluginPackageWin::isPluginBlacklisted): Use
            compareFileVersion()
            * plugins/win/PluginPackageWin.h:
            * plugins/win/PluginViewWin.cpp:
            (WebCore::PluginViewWin::determineQuirks): If the plug-in version is
            newer than the last-known unloadable version, add the DontUnloadPlugin
            quirk
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29871 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/win/PluginPackageWin.h
+++ third_party/WebKit/WebCore/plugins/win/PluginPackageWin.h
@@ -62,6 +62,8 @@ namespace WebCore {
         void unloadWithoutShutdown();
 
         const NPPluginFuncs* pluginFuncs() const { return &m_pluginFuncs; }
+
+        int compareFileVersion(unsigned compareVersionMS, unsigned compareVersionLS) const;
     private:
         PluginPackageWin(const String& path, const FILETIME& lastModified);
         bool fetchInfo();
