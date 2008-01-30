19aefe1fddac19c33d1106b9e72c761a16d393f6
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-30 00:28:40 +0000

    Reviewed by Dave Hyatt.
            - make Font instances cache their primary font
    
            * WebCore.base.exp:
            * platform/graphics/Font.cpp:
            (WebCore::Font::Font):
            (WebCore::Font::operator=):
            (WebCore::Font::cachePrimaryFont):
            (WebCore::Font::update):
            * platform/graphics/Font.h:
            (WebCore::Font::primaryFont):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29857 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/Font.cpp
+++ third_party/WebKit/WebCore/platform/graphics/Font.cpp
@@ -276,6 +276,7 @@ UChar32 WidthIterator::normalizeVoicingMarks(int currentCharacter)
 
 Font::Font()
     : m_pageZero(0)
+    , m_cachedPrimaryFont(0)
     , m_letterSpacing(0)
     , m_wordSpacing(0)
     , m_isPlatformFont(false)
@@ -285,6 +286,7 @@ Font::Font()
 Font::Font(const FontDescription& fd, short letterSpacing, short wordSpacing) 
     : m_fontDescription(fd)
     , m_pageZero(0)
+    , m_cachedPrimaryFont(0)
     , m_letterSpacing(letterSpacing)
     , m_wordSpacing(wordSpacing)
     , m_isPlatformFont(false)
@@ -294,6 +296,7 @@ Font::Font(const FontDescription& fd, short letterSpacing, short wordSpacing)
 Font::Font(const FontPlatformData& fontData, bool isPrinterFont)
     : m_fontList(new FontFallbackList)
     , m_pageZero(0)
+    , m_cachedPrimaryFont(0)
     , m_letterSpacing(0)
     , m_wordSpacing(0)
     , m_isPlatformFont(true)
@@ -307,6 +310,7 @@ Font::Font(const Font& other)
     , m_fontList(other.m_fontList)
     , m_pages(other.m_pages)
     , m_pageZero(other.m_pageZero)
+    , m_cachedPrimaryFont(other.m_cachedPrimaryFont)
     , m_letterSpacing(other.m_letterSpacing)
     , m_wordSpacing(other.m_wordSpacing)
     , m_isPlatformFont(other.m_isPlatformFont)
@@ -319,6 +323,7 @@ Font& Font::operator=(const Font& other)
     m_fontList = other.m_fontList;
     m_pages = other.m_pages;
     m_pageZero = other.m_pageZero;
+    m_cachedPrimaryFont = other.m_cachedPrimaryFont;
     m_letterSpacing = other.m_letterSpacing;
     m_wordSpacing = other.m_wordSpacing;
     m_isPlatformFont = other.m_isPlatformFont;
@@ -469,10 +474,11 @@ const GlyphData& Font::glyphDataForCharacter(UChar32 c, bool mirror, bool forceS
     return data;
 }
 
-const SimpleFontData* Font::primaryFont() const
+void Font::cachePrimaryFont() const
 {
     ASSERT(m_fontList);
-    return m_fontList->primaryFont(this)->fontDataForCharacter(' ');
+    ASSERT(!m_cachedPrimaryFont);
+    m_cachedPrimaryFont = m_fontList->primaryFont(this)->fontDataForCharacter(' ');
 }
 
 const FontData* Font::fontDataAt(unsigned index) const
@@ -497,6 +503,7 @@ void Font::update(PassRefPtr<FontSelector> fontSelector) const
     if (!m_fontList)
         m_fontList = new FontFallbackList();
     m_fontList->invalidate(fontSelector);
+    m_cachedPrimaryFont = 0;
     m_pageZero = 0;
     m_pages.clear();
 }
