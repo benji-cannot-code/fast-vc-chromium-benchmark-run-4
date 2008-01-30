b2a3146605dda8522ed8bd92cb381072830a75de
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-30 00:18:41 +0000

    WebKitTools:
            Reviewed by Sam Weinig. Sam Weinig.
            
            Fixed <rdar://problem/5692566> fast/encoding/mailto-always-utf-8.html
            fails when run after fast/dom/Window/window-property-shadowing.html (16902)
    
            Modified our policy delegates not to output window.name. Since a
            previous test may have explicitly set window.name, we can't rely on its
            implicit value in test results.
    
            * DumpRenderTree/mac/PolicyDelegate.mm:
            (-[PolicyDelegate webView:decidePolicyForNavigationAction:request:frame:decisionListener:]):
    
            * DumpRenderTree/win/PolicyDelegate.cpp:
            (PolicyDelegate::decidePolicyForNavigationAction):
    
    LayoutTests:
    
            Reviewed by Sam Weinig. Sam Weinig.
            
            Fixed <rdar://problem/5692566> fast/encoding/mailto-always-utf-8.html
            fails when run after fast/dom/Window/window-property-shadowing.html (16902)
    
            These layout tests demonstrate the expected behavior of window.name,
            including the fact that one test can change the window.name value seen
            by another test:
    
            * fast/dom/Window/window-property-shadowing-name-expected.txt: Added.
            * fast/dom/Window/window-property-shadowing-name.html: Added.
            * fast/dom/Window/window-property-clearing-expected.txt:
            * fast/dom/Window/window-property-clearing.html:
            
            Modified these expected results not to depend on window.name. Since a
            previous test may have explicitly set window.name, we can't rely on its
            implicit value:
    
            * fast/encoding/mailto-always-utf-8-expected.txt:
            * fast/forms/mailto/advanced-get-expected.txt:
            * fast/forms/mailto/advanced-put-expected.txt:
            * fast/forms/mailto/get-multiple-items-expected.txt:
            * fast/forms/mailto/get-multiple-items-text-plain-expected.txt:
            * fast/forms/mailto/get-multiple-items-x-www-form-urlencoded-expected.txt:
            * fast/forms/mailto/get-non-ascii-always-utf-8-expected.txt:
            * fast/forms/mailto/get-non-ascii-expected.txt:
            * fast/forms/mailto/get-non-ascii-text-plain-expected.txt:
            * fast/forms/mailto/get-non-ascii-text-plain-latin-1-expected.txt:
            * fast/forms/mailto/get-overwrite-query-expected.txt:
            * fast/forms/mailto/post-append-query-expected.txt:
            * fast/forms/mailto/post-multiple-items-expected.txt:
            * fast/forms/mailto/post-multiple-items-multipart-form-data-expected.txt:
            * fast/forms/mailto/post-multiple-items-text-plain-expected.txt:
            * fast/forms/mailto/post-multiple-items-x-www-form-urlencoded-expected.txt:
            * fast/forms/mailto/post-text-plain-expected.txt:
            * fast/forms/mailto/post-text-plain-with-accept-charset-expected.txt:
            * http/tests/security/feed-urls-from-remote-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29856 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/PolicyDelegate.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/PolicyDelegate.mm
@@ -39,11 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                                              frame:(WebFrame *)frame
                                                   decisionListener:(id<WebPolicyDecisionListener>)listener
 {
-    NSString *frameName = [frame name];
-    NSString *requestURL = [[request URL] absoluteString];
-    printf("Frame %s attempted to load %s\n", [frameName UTF8String], [requestURL UTF8String]);
+    printf("Policy delegate: attempt to load %s\n", [[[request URL] absoluteString] UTF8String]);
     [listener ignore];
 }
-
 
 @end
