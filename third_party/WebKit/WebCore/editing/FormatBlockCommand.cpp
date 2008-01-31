27f496dfdd18f0ad533bb96d6fc3ec3de8ecf349
justin.garcia@apple.com <justin.garcia@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-31 00:16:43 +0000

    WebCore:
            Reviewed by Darin Adler.
    
            <rdar://problem/5700414> REGRESSION (Adama-ToT): Selecting "Header 1" style in Leopard Server wiki inserts newline
    
            * editing/FormatBlockCommand.cpp:
            (WebCore::FormatBlockCommand::doApply): If the selected paragraph was empty,
            we may still need to call moveParagrah to remove the line break that holds that
            paragraph open because the new block of the requested type needs to to replace it.
    
    LayoutTests:
    
            Reviewed by Darin Adler.
            
            <rdar://problem/5700414> REGRESSION (Adama-ToT): Selecting "Header 1" style in Leopard Server wiki inserts newline
    
            * editing/execCommand/5700414-1.html: Added.
            * editing/execCommand/5700414-2.html: Added.
            * platform/mac/editing/execCommand/5700414-1-expected.checksum: Added.
            * platform/mac/editing/execCommand/5700414-1-expected.png: Added.
            * platform/mac/editing/execCommand/5700414-1-expected.txt: Added.
            * platform/mac/editing/execCommand/5700414-2-expected.checksum: Added.
            * platform/mac/editing/execCommand/5700414-2-expected.png: Added.
            * platform/mac/editing/execCommand/5700414-2-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/FormatBlockCommand.cpp
+++ third_party/WebKit/WebCore/editing/FormatBlockCommand.cpp
@@ -124,7 +124,7 @@ void FormatBlockCommand::doApply()
     appendNode(placeholder.get(), blockNode.get());
     
     VisiblePosition destination(Position(placeholder.get(), 0));
-    if (paragraphStart == paragraphEnd) {
+    if (paragraphStart == paragraphEnd && !lineBreakExistsAtPosition(paragraphStart)) {
         setEndingSelection(destination);
         return;
     }
