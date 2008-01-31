870092e7f3660e5b486b93e13c4956bad9a20133
justin.garcia@apple.com <justin.garcia@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-01-31 19:01:15 +0000

    WebCore:
            Reviewed by Darin Adler.
            
            <rdar://problem/5708115> REGRESSION: Words selected with a double click and copied won't paste into Mail
    
            * page/mac/WebCoreFrameBridge.h: Re-exposed smartInsertForString:, it's used by a WebKit method used by Mail.
    
    WebKit/mac:
    
            Reviewed by Darin Adler.
            
            <rdar://problem/5708115> REGRESSION: Words selected with a double click and copied won't paste into Mail
    
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView _smartInsertForString:replacingRange:beforeString:afterString:]): Brought 
            this back, it's used by Mail.
            (-[WebHTMLView _canSmartReplaceWithPasteboard:]): This WebInternal method is
            also used by Mail.  Moved to WebPrivate.
            * WebView/WebHTMLViewPrivate.h: Expose two methods that Mail uses here, so that we don't
            accidently remove them in the future.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@29889 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLViewPrivate.h
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLViewPrivate.h
@@ -121,4 +121,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Typically this would be called from inside an override of -[NSView knowsPageRange:].
 - (void)_layoutForPrinting;
 
+- (BOOL)_canSmartReplaceWithPasteboard:(NSPasteboard *)pasteboard;
+
+
 @end
