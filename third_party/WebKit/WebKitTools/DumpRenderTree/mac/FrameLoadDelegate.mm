58f169eb398cd44d31de8ac3ead0c8c7c9ebddf9
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-05 20:47:31 +0000

    WebKitTools/ 2008-02-05  Holger Freyther  <zecke@selfish.org>
            Reviewed by Darin.
    
            In http://bugs.webkit.org/show_bug.cgi?id=16853 it was identified that the
            the output of the willCloseFrame and didClearWindowObject FrameLoadDelegate
            depends on the order the tests are executed.
    
            Propose to remove willCloseFrame and didlCearWindowObject output from the
            FrameLoadDelegate and update the test results. The output of didClearWindowObject
            is not interesting for any of the current tests and we have other ways to find
            out if a frame was closed or not.
    
            * DumpRenderTree/mac/FrameLoadDelegate.mm:
            * DumpRenderTree/win/FrameLoadDelegate.cpp:
            (FrameLoadDelegate::didClearWindowObject):
            * DumpRenderTree/win/FrameLoadDelegate.h:
    
    LayoutTest:
    2008-02-05  Holger Freyther  <zecke@selfish.org>
    
            Reviewed by Darin.
    
            In http://bugs.webkit.org/show_bug.cgi?id=16853 it was identified that the
            the output of the willCloseFrame and didClearWindowObject FrameLoadDelegate
            depends on the order the tests are executed.
    
            Propose to remove willCloseFrame and didlCearWindowObject output from the
            FrameLoadDelegate and update the test results. The output of didClearWindowObject
            is not interesting for any of the current tests and we have other ways to find
            out if a frame was closed or not.
    
            * http/tests/loading/bad-scheme-subframe-expected.txt:
            * http/tests/loading/bad-server-subframe-expected.txt:
            * http/tests/loading/basic-expected.txt:
            * http/tests/loading/empty-subframe-expected.txt:
            * http/tests/loading/gmail-assert-on-load-expected.txt:
            * http/tests/loading/onload-vs-immediate-refresh-expected.txt:
            * http/tests/loading/slow-parsing-subframe-expected.txt:
            * platform/mac/http/tests/loading/simple-subframe-expected.txt:
            * webarchive/loading/test-loading-archive-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30014 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/FrameLoadDelegate.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/FrameLoadDelegate.mm
@@ -229,11 +229,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)webView:(WebView *)sender didClearWindowObject:(WebScriptObject *)obj forFrame:(WebFrame *)frame
 {
-    if (!done && layoutTestController->dumpFrameLoadCallbacks()) {
-        NSString *string = [NSString stringWithFormat:@"%@ - didClearWindowObjectForFrame", [frame _drt_descriptionSuitableForTestResult]];
-        printf ("%s\n", [string UTF8String]);
-    }
-        
     ASSERT(obj == [frame windowObject]);
     ASSERT([obj JSObject] == JSContextGetGlobalObject([frame globalContext]));
 
@@ -316,14 +311,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {
     if (!done && layoutTestController->dumpFrameLoadCallbacks()) {
         NSString *string = [NSString stringWithFormat:@"%@ - didCancelClientRedirectForFrame", [frame _drt_descriptionSuitableForTestResult]];
-        printf ("%s\n", [string UTF8String]);
-    }
-}
-
-- (void)webView:(WebView *)sender willCloseFrame:(WebFrame *)frame;
-{
-    if (!done && layoutTestController->dumpFrameLoadCallbacks()) {
-        NSString *string = [NSString stringWithFormat:@"%@ - willCloseFrame", [frame _drt_descriptionSuitableForTestResult]];
         printf ("%s\n", [string UTF8String]);
     }
 }
