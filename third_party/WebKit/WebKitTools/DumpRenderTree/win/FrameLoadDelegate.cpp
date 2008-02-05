6220e803931a8516bba92601be8a69e8359ebf9a
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-05 22:28:35 +0000

    Build fix for Windows. willCloseFrame is a pure virtual and we are forced to have an implementation.
    This partially reverts r30014.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30026 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/win/FrameLoadDelegate.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/win/FrameLoadDelegate.cpp
@@ -245,6 +245,13 @@ HRESULT STDMETHODCALLTYPE FrameLoadDelegate::didFailLoadWithError(
     return S_OK;
 }
 
+HRESULT STDMETHODCALLTYPE FrameLoadDelegate::willCloseFrame( 
+        /* [in] */ IWebView *webView,
+        /* [in] */ IWebFrame *frame)
+{
+    return E_NOTIMPL;
+}
+
 HRESULT STDMETHODCALLTYPE FrameLoadDelegate::didClearWindowObject( 
         /* [in] */ IWebView*webView,
         /* [in] */ JSContextRef context,
