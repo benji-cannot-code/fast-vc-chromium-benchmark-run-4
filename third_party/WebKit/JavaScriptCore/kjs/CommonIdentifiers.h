fa681dc0c33116a7414b4548b739673d35f7742c
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-06 17:33:07 +0000

    JavaScriptCore:
            Reviewed by Sam.
    
            - replace calls to put to set up properties with calls to putDirect, to
              prepare for a future change where put won't take attributes any more,
              and for a slight performance boost
    
            * API/JSObjectRef.cpp:
            (JSObjectMakeConstructor): Use putDirect instead of put.
            * kjs/CommonIdentifiers.h: Removed lastIndex.
            * kjs/JSGlobalObject.cpp:
            (KJS::JSGlobalObject::reset): Use putDirect instead of put.
            * kjs/array_object.cpp:
            (KJS::arrayProtoFuncConcat): Took out extra call to get length (unused).
            (KJS::ArrayObjectImp::ArrayObjectImp): Use putDirect instead of put.
            * kjs/error_object.cpp:
            (KJS::ErrorPrototype::ErrorPrototype): Use putDirect instead of put.
            * kjs/function.cpp:
            (KJS::Arguments::Arguments): Use putDirect instead of put.
            (KJS::PrototypeFunction::PrototypeFunction): Use putDirect instead of put.
            * kjs/function_object.cpp:
            (KJS::FunctionObjectImp::construct): Use putDirect instead of put.
            * kjs/nodes.cpp:
            (KJS::FuncDeclNode::makeFunction): Use putDirect instead of put.
            (KJS::FuncExprNode::evaluate): Use putDirect instead of put.
            * kjs/regexp_object.cpp:
            (KJS::regExpProtoFuncCompile): Use setLastIndex instead of put(lastIndex).
            (KJS::RegExpImp::match): Get and set lastIndex by using m_lastIndex instead of
            calling get and put.
            * kjs/regexp_object.h:
            (KJS::RegExpImp::setLastIndex): Added.
            * kjs/string_object.cpp:
            (KJS::stringProtoFuncMatch): Use setLastIndex instead of put(lastIndex).
    
    WebCore:
    
            Reviewed by Sam.
    
            - replace calls to put to set up properties with calls to putDirect, to
              prepare for a future change where put won't take attributes any more,
              and for a slight performance boost
    
            * bindings/js/JSAudioConstructor.cpp:
            (WebCore::JSAudioConstructor::JSAudioConstructor): Use putDirect instead of put.
            * bindings/js/JSEventTargetBase.h:
            (WebCore::JSEventTargetPrototype::self): Ditto.
            * bindings/js/JSHTMLOptionElementConstructor.cpp:
            (WebCore::JSHTMLOptionElementConstructor::JSHTMLOptionElementConstructor): Ditto.
            * bindings/js/JSSQLResultSetRowListCustom.cpp:
            (WebCore::JSSQLResultSetRowList::item): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30040 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/CommonIdentifiers.h
+++ third_party/WebKit/JavaScriptCore/kjs/CommonIdentifiers.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     macro(ignoreCase) \
     macro(index) \
     macro(input) \
-    macro(lastIndex) \
     macro(length) \
     macro(message) \
     macro(multiline) \
