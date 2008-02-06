fd65ef22454e1667fa527b6e802ea995187f9d0c
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-06 02:13:41 +0000

    Reviewed by Oliver. Add new dynamic update tests, for SVGClipPathElement.
    git-svn-id: svn://svn.chromium.org/blink/trunk@30033 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/svg/dynamic-updates/resources/SVGClipPathElement-dom-clipPathUnits-attr.js
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// [Name] SVGClipPathElement-dom-clipPathUnits-attr.js
+// [Expected rendering result] green circle - and a series of PASS mesages
+
+description("Tests dynamic updates of the 'clipPathUnits' attribute of the SVGClipPathElement object")
+createSVGTestCase();
+
+var defsElement = createSVGElement("defs");
+rootSVGElement.appendChild(defsElement);
+
+var clipPathElement = createSVGElement("clipPath");
+clipPathElement.setAttribute("id", "clipper");
+clipPathElement.setAttribute("clipPathUnits", "objectBoundingBox");
+
+var circleElement = createSVGElement("circle");
+circleElement.setAttribute("cx", "150");
+circleElement.setAttribute("cy", "150");
+circleElement.setAttribute("r", "150");
+clipPathElement.appendChild(circleElement);
+
+defsElement.appendChild(clipPathElement);;
+
+var rectElement = createSVGElement("rect");
+rectElement.setAttribute("width", "300");
+rectElement.setAttribute("height", "300");
+rectElement.setAttribute("fill", "green");
+rectElement.setAttribute("clip-path", "url(#clipper)");
+rootSVGElement.appendChild(rectElement);
+
+shouldBeEqualToString("clipPathElement.getAttribute('clipPathUnits')", "objectBoundingBox")
+
+function executeTest() {
+    clipPathElement.setAttribute("clipPathUnits", "userSpaceOnUse");
+    shouldBeEqualToString("clipPathElement.getAttribute('clipPathUnits')", "userSpaceOnUse");
+
+    waitForClickEvent(rectElement);
+    triggerUpdate();
+}
+
+window.setTimeout("executeTest()", 0);
+var successfullyParsed = true;
