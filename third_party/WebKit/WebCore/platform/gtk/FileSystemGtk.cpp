217db5d55b212ba90d2f6ec6c06b756cc2861ec9
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-07 23:29:54 +0000

    Qt and GTK+ build fixes
            * platform/gtk/FileSystemGtk.cpp: Added a missing #include.
            * platform/qt/FileSystemQt.cpp: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30080 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/FileSystemGtk.cpp
+++ third_party/WebKit/WebCore/platform/gtk/FileSystemGtk.cpp
@@ -21,8 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-
 #include "FileSystem.h"
+
+#include "NotImplemented.h"
 #include "PlatformString.h"
 #include "CString.h"
 
--- third_party/WebKit/WebCore/platform/qt/FileSystemQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/FileSystemQt.cpp
@@ -29,7 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <PlatformString.h>
+#include "NotImplemented.h"
+#include "PlatformString.h"
 #include <QFile>
 #include <QFileInfo>
 #include <QDir>
