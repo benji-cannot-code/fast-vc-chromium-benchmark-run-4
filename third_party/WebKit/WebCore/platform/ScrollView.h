c7b7f0524cab6c070c5a3345324398b46ab8aec9
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-07 04:12:36 +0000

    Switch from directly handling wx scroll wheel events to handling the PlatformWheelEvent instead to give JS, etc. a chance to handle it. http://bugs.webkit.org/show_bug.cgi?id=17179
    git-svn-id: svn://svn.chromium.org/blink/trunk@30060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ScrollView.h
+++ third_party/WebKit/WebCore/platform/ScrollView.h
@@ -231,6 +231,7 @@ namespace WebCore {
 
     private:
         void adjustScrollbars(int x = -1, int y = -1, bool refresh = true);
+        IntSize maximumScroll() const;
 
         class ScrollViewPrivate;
         ScrollViewPrivate* m_data;
