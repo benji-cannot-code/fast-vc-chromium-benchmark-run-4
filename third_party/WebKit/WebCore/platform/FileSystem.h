5c91b74f86be98912fcbc9b3dce1c08b739aaa37
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-08 23:35:51 +0000

    wx port build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@30100 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/FileSystem.h
+++ third_party/WebKit/WebCore/platform/FileSystem.h
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gmodule.h>
 #endif
 
+#include <time.h>
+
 #include <wtf/Platform.h>
 
 typedef const struct __CFData* CFDataRef;
