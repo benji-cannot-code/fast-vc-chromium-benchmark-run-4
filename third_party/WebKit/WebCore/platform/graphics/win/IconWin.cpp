b7e5ff8a055d25f101d001ea344d6c854a74048c
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-11 04:17:24 +0000

    - try to fix Win, Qt, GTK, and WX builds
            * platform/Cursor.h:
            (WebCore::SharedCursor::SharedCursor): Added explicit RefCounted constructor.
            * platform/graphics/win/IconWin.cpp:
            (WebCore::Icon::Icon): Ditto.
            * platform/win/FileChooserWin.cpp:
            (WebCore::FileChooser::FileChooser): Ditto.
            * platform/win/PopupMenuWin.cpp:
            (WebCore::PopupMenu::PopupMenu): Ditto.
            * platform/wx/TemporaryLinkStubs.cpp:
            (FileChooser::FileChooser): Ditto.
            (PopupMenu::PopupMenu): Ditto.
            (Icon::Icon): Ditto.
            * plugins/PluginStream.cpp:
            (WebCore::PluginStream::PluginStream): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30135 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/IconWin.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/IconWin.cpp
@@ -29,12 +29,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 Icon::Icon()
-    : m_hIcon(0)
+    : RefCounted<Icon>(0)
+    , m_hIcon(0)
 {
 }
 
 Icon::Icon(HICON icon)
-    : m_hIcon(icon)
+    : RefCounted<Icon>(0)
+    , m_hIcon(icon)
 {
 }
 
