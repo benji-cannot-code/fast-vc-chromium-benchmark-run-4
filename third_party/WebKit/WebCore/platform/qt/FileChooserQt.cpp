a8f254d743389c9f7d72b964e941ea8ece297eaf
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-11 07:38:36 +0000

    - another Qt build fix (wish the Qt bot wouldn't stop on the first error)
            * platform/qt/FileChooserQt.cpp:
            (WebCore::FileChooser::FileChooser): Added explicit RefCounted constructor.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/FileChooserQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/FileChooserQt.cpp
@@ -33,7 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 FileChooser::FileChooser(FileChooserClient* client, const String& filename)
-    : m_client(client)
+    : RefCounted<FileChooser>(0)
+    , m_client(client)
     , m_filename(filename)
     , m_icon(chooseIcon(filename))
 {
