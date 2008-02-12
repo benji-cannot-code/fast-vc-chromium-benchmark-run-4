33aaa47fe087d74e61b2d5184041aa108a488b12
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-12 20:11:55 +0000

    Release build fix
            * storage/DatabaseTask.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30173 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/DatabaseTask.cpp
+++ third_party/WebKit/WebCore/storage/DatabaseTask.cpp
@@ -99,10 +99,12 @@ void DatabaseOpenTask::doPerformTask()
     m_success = database()->performOpenAndVerify(m_code);
 }
 
+#ifndef NDEBUG
 const char* DatabaseOpenTask::debugTaskName() const
 {
     return "DatabaseOpenTask";
 }
+#endif
 
 // *** DatabaseTransactionTask ***
 // Starts a transaction that will report its results via a callback.
@@ -126,10 +128,12 @@ void DatabaseTransactionTask::doPerformTask()
     }
 }
 
+#ifndef NDEBUG
 const char* DatabaseTransactionTask::debugTaskName() const
 {
     return "DatabaseTransactionTask";
 }
+#endif
 
 // *** DatabaseTableNamesTask ***
 // Retrieves a list of all tables in the database - for WebInspector support.
@@ -144,10 +148,11 @@ void DatabaseTableNamesTask::doPerformTask()
     m_tableNames = database()->performGetTableNames();
 }
 
+#ifndef NDEBUG
 const char* DatabaseTableNamesTask::debugTaskName() const
 {
     return "DatabaseTableNamesTask";
 }
-
+#endif
 
 } // namespace WebCore
