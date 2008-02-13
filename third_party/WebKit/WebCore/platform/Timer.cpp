80ef5aad59b35f6ed8674c56d2173adf43115b78
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-13 12:27:46 +0000

    Reviewed by Darin.
            - fix http://bugs.webkit.org/show_bug.cgi?id=17340
              WebCore/platform/Timer.cpp fails to build with gcc 4.3
    
            * platform/Timer.cpp: Add include of <limits.h> since this file uses UINT_MAX.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Timer.cpp
+++ third_party/WebKit/WebCore/platform/Timer.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2006, 2008 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,8 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "SharedTimer.h"
 #include "SystemTime.h"
-#include <math.h>
+#include <limits.h>
 #include <limits>
+#include <math.h>
 #include <wtf/HashSet.h>
 #include <wtf/Vector.h>
 
