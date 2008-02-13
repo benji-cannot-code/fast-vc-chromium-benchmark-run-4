85c576ea33b11ddf4a4a72bfbd6d7ddd34a1c907
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-13 06:04:19 +0000

    Reviewed by Brady Eidson.
            <rdar://problem/5652560> Can't delete database if the website that
            uses it has been opened in this session
    
            Close the Database on the database thread before deleting the file.
            Tested and works on Windows and Mac.
    
            * platform/sql/SQLiteDatabase.cpp:
            (WebCore::SQLiteDatabase::close): Assert we are on the opening thread.
            * storage/Database.cpp:
            (WebCore::Database::markAsDeletedAndClose): Unschedule any pending
            Database tasks, and start and imediate DatabaseCloseTask.
            (WebCore::Database::close): Close the SQLDatabase.
            * storage/Database.h: Renamed markAsDeleted to markAsDeletedAndClose.
            * storage/DatabaseTask.cpp:
            (WebCore::DatabaseCloseTask::DatabaseCloseTask): New task.
            (WebCore::DatabaseCloseTask::doPerformTask): Call close on the Database.
            (WebCore::DatabaseCloseTask::debugTaskName): Return "DatabaseCloseTask".
            * storage/DatabaseTask.h: Add DatabaseCloseTask.
            * storage/DatabaseTracker.cpp:
            (WebCore::DatabaseTracker::deleteDatabaseFile): Call the renamed
              markAsDeletedAndClose.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30189 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/DatabaseTracker.cpp
+++ third_party/WebKit/WebCore/storage/DatabaseTracker.cpp
@@ -745,7 +745,7 @@ bool DatabaseTracker::deleteDatabaseFile(SecurityOrigin* origin, const String& n
                     // We have some database open with this name. Mark them as deleted.
                     DatabaseSet::const_iterator end = databaseSet->end();
                     for (DatabaseSet::const_iterator it = databaseSet->begin(); it != end; ++it)
-                        (*it)->markAsDeleted();
+                        (*it)->markAsDeletedAndClose();
                 }
             }
         }
