2a1c88fa88903b4ed1df61b03a5aca6fdc35ddcd
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-13 21:49:41 +0000

    Reviewed by Oliver.
    Actually fix the manual-tests/svg-cursor-changes.svg testcase.
    I only reran layout tests when fixing the last issues with the patch,
    instead of trying the manual-test :( Fixed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30211 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSCursorImageValue.h
+++ third_party/WebKit/WebCore/css/CSSCursorImageValue.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSImageValue.h"
 #include "IntPoint.h"
 
+#include <wtf/HashSet.h>
+
 namespace WebCore {
 
 class Element;
@@ -45,7 +47,7 @@ private:
     IntPoint m_hotspot;
 
 #if ENABLE(SVG)
-    SVGElement* m_referencedElement;
+    HashSet<SVGElement*> m_referencedElements;
 #endif
 };
 
