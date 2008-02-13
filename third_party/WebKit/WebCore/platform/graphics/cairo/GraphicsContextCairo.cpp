4e304cd51e8d6e79a246a322a4b95a5becc93101
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-13 04:22:25 +0000

    Endeavour to fix qt and gtk builds
    git-svn-id: svn://svn.chromium.org/blink/trunk@30188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cairo/GraphicsContextCairo.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cairo/GraphicsContextCairo.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CairoPath.h"
 #include "FloatRect.h"
 #include "Font.h"
+#include "ImageBuffer.h"
 #include "IntRect.h"
 #include "NotImplemented.h"
 #include "Path.h"
--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Path.h"
 #include "Color.h"
 #include "GraphicsContext.h"
+#include "ImageBuffer.h"
 #include "Font.h"
 #include "Pen.h"
 #include "NotImplemented.h"
