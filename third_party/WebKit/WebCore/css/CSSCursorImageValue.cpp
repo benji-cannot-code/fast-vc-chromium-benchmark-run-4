66255b19f2081d71748cb0fea727f88204c908dc
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-13 21:19:31 +0000

    Windows/GTK+ build fixes
    WebCore:
    
            Windows build fix
    
            * css/CSSCursorImageValue.cpp: #include MathExtras.h to get roundf.
    
    WebKitTools:
    
            Windows/GTK+ build fix
    
            * DumpRenderTree/LayoutTestController.cpp: #include MathExtras.h to
            get isnan.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30209 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSCursorImageValue.cpp
+++ third_party/WebKit/WebCore/css/CSSCursorImageValue.cpp
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "CSSCursorImageValue.h"
 
+#include <wtf/MathExtras.h>
+
 #if ENABLE(SVG)
 #include "CachedImage.h"
 #include "DocLoader.h"
--- third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <JavaScriptCore/JSObjectRef.h>
 #include <JavaScriptCore/JSRetainPtr.h>
 #include <wtf/Assertions.h>
+#include <wtf/MathExtras.h>
 
 LayoutTestController::LayoutTestController(bool testRepaintDefault, bool testRepaintSweepHorizontallyDefault)
     : m_dumpAsText(false)
