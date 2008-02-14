f9253edb43215c738e7f12f81acf44fc7a3d0748
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-14 12:26:44 +0000

    * Make sure that the WebCore::Frame is living as long as the WebKitWebFrame   wrapped around it. All current calls to WebCore::Frame from within the   WebKitWebFrame are safe, even in the case where the WebCore::Frame is already   detached from the WebCore::Page. * For the mainFrame the WebKitWebView is holding the initial reference and   will unref on destruction. * For sub-frames the FramerLoaderClientGtk.cpp will do the unrefing when the   frame gets detached from the parent. * Make sure FrameLoader::detachFromParent gets called by calling FrameLoader::detachChildren
    git-svn-id: svn://svn.chromium.org/blink/trunk@30226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
+++ third_party/WebKit/WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
@@ -453,7 +453,9 @@ void FrameLoaderClient::detachedFromParent3()
 
 void FrameLoaderClient::detachedFromParent4()
 {
-    notImplemented();
+    ASSERT(m_frame);
+    g_object_unref(m_frame);
+    m_frame = 0;
 }
 
 void FrameLoaderClient::loadedFromCachedPage()
