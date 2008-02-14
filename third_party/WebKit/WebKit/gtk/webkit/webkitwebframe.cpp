d1eb19f45d9e54ea33b16c780d136dc71bb4c4dc
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-14 12:27:16 +0000

    * Make the handling of the coreFrame consistent. We construct the coreFrame   at the beginning and it will live until the end. Change the g_return_(val_)if   to ASSERTs as having a 0 coreFrame can never happen.
    git-svn-id: svn://svn.chromium.org/blink/trunk@30227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebframe.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebframe.cpp
@@ -319,7 +319,7 @@ const gchar* webkit_web_frame_get_name(WebKitWebFrame* frame)
         return priv->name;
 
     Frame* coreFrame = core(frame);
-    g_return_val_if_fail(coreFrame, NULL);
+    ASSERT(coreFrame);
 
     String string = coreFrame->tree()->name();
     priv->name = g_strdup(string.utf8().data());
@@ -339,7 +339,7 @@ WebKitWebFrame* webkit_web_frame_get_parent(WebKitWebFrame* frame)
     g_return_val_if_fail(WEBKIT_IS_WEB_FRAME(frame), NULL);
 
     Frame* coreFrame = core(frame);
-    g_return_val_if_fail(coreFrame, NULL);
+    ASSERT(coreFrame);
 
     return kit(coreFrame->tree()->parent());
 }
@@ -361,7 +361,7 @@ void webkit_web_frame_load_request(WebKitWebFrame* frame, WebKitNetworkRequest* 
     g_return_if_fail(WEBKIT_IS_NETWORK_REQUEST(request));
 
     Frame* coreFrame = core(frame);
-    g_return_if_fail(coreFrame);
+    ASSERT(coreFrame);
 
     // TODO: Use the ResourceRequest carried by WebKitNetworkRequest when it is implemented.
     DeprecatedString string = DeprecatedString::fromUtf8(webkit_network_request_get_uri(request));
@@ -379,7 +379,7 @@ void webkit_web_frame_stop_loading(WebKitWebFrame* frame)
     g_return_if_fail(WEBKIT_IS_WEB_FRAME(frame));
 
     Frame* coreFrame = core(frame);
-    g_return_if_fail(coreFrame);
+    ASSERT(coreFrame);
 
     coreFrame->loader()->stopAllLoaders();
 }
@@ -395,7 +395,7 @@ void webkit_web_frame_reload(WebKitWebFrame* frame)
     g_return_if_fail(WEBKIT_IS_WEB_FRAME(frame));
 
     Frame* coreFrame = core(frame);
-    g_return_if_fail(coreFrame);
+    ASSERT(coreFrame);
 
     coreFrame->loader()->reload();
 }
@@ -423,7 +423,7 @@ WebKitWebFrame* webkit_web_frame_find_frame(WebKitWebFrame* frame, const gchar* 
     g_return_val_if_fail(name, NULL);
 
     Frame* coreFrame = core(frame);
-    g_return_val_if_fail(coreFrame, NULL);
+    ASSERT(coreFrame);
 
     String nameString = String::fromUTF8(name);
     return kit(coreFrame->tree()->find(AtomicString(nameString)));
@@ -443,7 +443,7 @@ JSGlobalContextRef webkit_web_frame_get_global_context(WebKitWebFrame* frame)
     g_return_val_if_fail(WEBKIT_IS_WEB_FRAME(frame), NULL);
 
     Frame* coreFrame = core(frame);
-    g_return_val_if_fail(coreFrame, NULL);
+    ASSERT(coreFrame);
 
     return toGlobalRef(coreFrame->scriptProxy()->globalObject()->globalExec());
 }
@@ -458,9 +458,10 @@ GSList* webkit_web_frame_get_children(WebKitWebFrame* frame)
 {
     g_return_val_if_fail(WEBKIT_IS_WEB_FRAME(frame), NULL);
 
-    GSList* children = NULL;
     Frame* coreFrame = core(frame);
+    ASSERT(coreFrame);
 
+    GSList* children = NULL;
     for (Frame* child = coreFrame->tree()->firstChild(); child; child = child->tree()->nextSibling()) {
         FrameLoader* loader = child->loader();
         WebKit::FrameLoaderClient* client = static_cast<WebKit::FrameLoaderClient*>(loader->client());
@@ -482,6 +483,8 @@ gchar* webkit_web_frame_get_inner_text(WebKitWebFrame* frame)
     g_return_val_if_fail(WEBKIT_IS_WEB_FRAME(frame), NULL);
 
     Frame* coreFrame = core(frame);
+    ASSERT(coreFrame);
+
     FrameView* view = coreFrame->view();
 
     if (view->layoutPending())
@@ -650,6 +653,8 @@ void webkit_web_frame_print(WebKitWebFrame* frame)
         topLevel = NULL;
 
     Frame* coreFrame = core(frame);
+    ASSERT(coreFrame);
+
     PrintContext printContext(coreFrame);
 
     GtkPrintOperation* op = gtk_print_operation_new();
