0ed09d3d8468ce3eb2bd5fefb5919b9e4d989449
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-16 06:44:46 +0000

    Build fix for Qt and Cairo builds
    git-svn-id: svn://svn.chromium.org/blink/trunk@30337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
@@ -930,7 +930,7 @@ void GraphicsContext::paintBuffer(ImageBuffer* buffer, const IntRect& r)
     painter->setBackground(currentBackground);
 }
 
-void GraphicsContext::drawImage(ImageBuffer* buffer, const FloatRect& srcRect, const FloatRect& destRect)
+void GraphicsContext::drawImage(ImageBuffer* buffer, const FloatRect& srcRect, const FloatRect& dstRect)
 {
     QPainter* painter = static_cast<QPainter*>(platformContext());
     QPixmap px = *buffer->pixmap();
