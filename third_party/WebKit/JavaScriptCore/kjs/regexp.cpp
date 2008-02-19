e4674d1be6fd116d6a92454c0969dc147d4b211d
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-19 19:16:42 +0000

    JavaScriptCore:
            Rubber stamped by Anders.
    
            - removed explicit initialization to 1 for RefCounted; that's now the default
    
            * kjs/regexp.cpp:
            (KJS::RegExp::RegExp): Removed RefCounted initializer.
    
    WebCore:
    
            Rubber stamped by Anders.
    
            - removed explicit initialization to 1 for RefCounted; that's now the default
    
            * loader/ResourceLoader.cpp:
            (WebCore::ResourceLoader::ResourceLoader): Removed RefCounted initializer.
            * platform/network/ResourceHandle.cpp:
            (WebCore::ResourceHandle::ResourceHandle): Ditto.
            * platform/text/StringImpl.cpp:
            (WebCore::StringImpl::StringImpl): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/regexp.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/regexp.cpp
@@ -34,8 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace KJS {
 
 inline RegExp::RegExp(const UString& pattern)
-  : RefCounted<RegExp>(1)
-  , m_pattern(pattern)
+  : m_pattern(pattern)
   , m_flagBits(0)
   , m_constructionError(0)
   , m_numSubpatterns(0)
@@ -50,8 +49,7 @@ PassRefPtr<RegExp> RegExp::create(const UString& pattern)
 }
 
 inline RegExp::RegExp(const UString& pattern, const UString& flags)
-  : RefCounted<RegExp>(1)
-  , m_pattern(pattern)
+  : m_pattern(pattern)
   , m_flags(flags)
   , m_flagBits(0)
   , m_constructionError(0)
