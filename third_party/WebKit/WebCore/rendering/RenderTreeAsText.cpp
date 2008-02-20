20a554abe4096a8e337f806a6b8f17791ed88d4c
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-20 02:07:18 +0000

    - fix build when SVG is not enabled
            * rendering/RenderTreeAsText.cpp: Added include of "TextStream.h".
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30420 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTreeAsText.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTreeAsText.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderView.h"
 #include "RenderWidget.h"
 #include "SelectionController.h"
+#include "TextStream.h"
 #include <wtf/Vector.h>
 
 #if ENABLE(SVG)
