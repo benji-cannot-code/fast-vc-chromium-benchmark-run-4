30a9466e0bd27c2165c6987476017b5d4c4475e2
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-21 03:42:35 +0000

    Reviewed, tweaked and landed by Sam.
            - make HTMLInterchange return a String instead of a DeprecatedString
    
            * editing/HTMLInterchange.cpp:
            (WebCore::): Return a String from convertedSpaceString.
            (WebCore::convertHTMLTextToInterchangeFormat): Use a Vector instead of
            a DeprecatedString to build up the return String.
            * editing/HTMLInterchange.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/HTMLInterchange.cpp
+++ third_party/WebKit/WebCore/editing/HTMLInterchange.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2004 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2004, 2008 Apple Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLInterchange.h"
 
 #include "CharacterNames.h"
-#include "Document.h"
 #include "Text.h"
 #include "TextIterator.h"
 
@@ -36,14 +35,14 @@ namespace WebCore {
 
 namespace {
 
-DeprecatedString convertedSpaceString() 
+String convertedSpaceString()
 {
-    static DeprecatedString convertedSpaceString;
-    if (convertedSpaceString.length() == 0) {
+    static String convertedSpaceString;
+    if (convertedSpaceString.isNull()) {
         convertedSpaceString = "<span class=\"";
         convertedSpaceString += AppleConvertedSpace;
         convertedSpaceString += "\">";
-        convertedSpaceString += DeprecatedChar(noBreakSpace);
+        convertedSpaceString.append(noBreakSpace);
         convertedSpaceString += "</span>";
     }
     return convertedSpaceString;
@@ -51,67 +50,63 @@ DeprecatedString convertedSpaceString()
 
 } // end anonymous namespace
 
-DeprecatedString convertHTMLTextToInterchangeFormat(const DeprecatedString& in, const Text* node)
+String convertHTMLTextToInterchangeFormat(const String& in, const Text* node)
 {
     // Assume all the text comes from node.
     if (node->renderer() && node->renderer()->style()->preserveNewline())
         return in;
-        
-    DeprecatedString s;
 
-    unsigned int i = 0;
-    unsigned int consumed = 0;
+    Vector<UChar> s;
+
+    unsigned i = 0;
+    unsigned consumed = 0;
     while (i < in.length()) {
         consumed = 1;
-        if (isCollapsibleWhitespace(in[i].unicode())) {
+        if (isCollapsibleWhitespace(in[i])) {
             // count number of adjoining spaces
-            unsigned int j = i + 1;
-            while (j < in.length() && isCollapsibleWhitespace(in[j].unicode()))
+            unsigned j = i + 1;
+            while (j < in.length() && isCollapsibleWhitespace(in[j]))
                 j++;
-            unsigned int count = j - i;
+            unsigned count = j - i;
             consumed = count;
             while (count) {
-                unsigned int add = count % 3;
+                unsigned add = count % 3;
                 switch (add) {
                     case 0:
-                        s += convertedSpaceString();
-                        s += ' ';
-                        s += convertedSpaceString();
+                        append(s, convertedSpaceString());
+                        s.append(' ');
+                        append(s, convertedSpaceString());
                         add = 3;
                         break;
                     case 1:
                         if (i == 0 || i + 1 == in.length()) // at start or end of string
-                            s += convertedSpaceString();
+                            append(s, convertedSpaceString());
                         else
-                            s += ' ';
+                            s.append(' ');
                         break;
                     case 2:
                         if (i == 0) {
                              // at start of string
-                            s += convertedSpaceString();
-                            s += ' ';
-                        }
-                        else if (i + 2 == in.length()) {
+                            append(s, convertedSpaceString());
+                            s.append(' ');
+                        } else if (i + 2 == in.length()) {
                              // at end of string
-                            s += convertedSpaceString();
-                            s += convertedSpaceString();
-                        }
-                        else {
-                            s += convertedSpaceString();
-                            s += ' ';
+                            append(s, convertedSpaceString());
+                            append(s, convertedSpaceString());
+                        } else {
+                            append(s, convertedSpaceString());
+                            s.append(' ');
                         }
                         break;
                 }
                 count -= add;
             }
-        }
-        else {
-            s += in[i];
-        }
+        } else
+            s.append(in[i]);
         i += consumed;
     }
 
-    return s;
+    return String::adopt(s);
 }
 
-}
+} // namespace WebCore
--- third_party/WebKit/WebCore/editing/HTMLInterchange.h
+++ third_party/WebKit/WebCore/editing/HTMLInterchange.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2004 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2004, 2008 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class DeprecatedString;
+class String;
 class Text;
 
 #define AppleInterchangeNewline   "Apple-interchange-newline"
@@ -40,7 +40,7 @@ class Text;
 
 enum EAnnotateForInterchange { DoNotAnnotateForInterchange, AnnotateForInterchange };
 
-DeprecatedString convertHTMLTextToInterchangeFormat(const DeprecatedString&, const Text*);
+String convertHTMLTextToInterchangeFormat(const String&, const Text*);
 
 }
 
