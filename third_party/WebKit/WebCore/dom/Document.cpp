9270848eace3556c01ea2b39692d3e013277df69
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-21 04:05:37 +0000

    Fix the layout test failure that never should have passed in the first         place by making changes to media lists actually result in the recomputation         of the style selector.  Now it passes for the right reasons and not because         of dumb luck.
            Reviewed by Sam Weinig
    
            * css/MediaList.cpp:
            (WebCore::MediaList::deleteMedium):
            (WebCore::MediaList::setMediaText):
            (WebCore::MediaList::appendMedium):
            (WebCore::MediaList::notifyChanged):
            * css/MediaList.h:
            * dom/Document.cpp:
            (WebCore::Document::attach):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -1214,7 +1214,6 @@ void Document::attach()
             matchAuthorAndUserStyles = docSettings->authorAndUserStylesEnabled();
         m_styleSelector = new CSSStyleSelector(this, userStyleSheet(), m_styleSheets.get(), m_mappedElementSheet.get(), !inCompatMode(), matchAuthorAndUserStyles);
         m_styleSelector->setEncodedURL(m_url);
-        m_didCalculateStyleSelector = true;
     }
 
     recalcStyle(Force);
