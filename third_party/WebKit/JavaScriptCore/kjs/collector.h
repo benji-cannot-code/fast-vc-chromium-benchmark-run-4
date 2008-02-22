9d5a8f835adbf3438c51f096fbc8056705c4ba11
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-22 23:16:40 +0000

    JavaScriptCore:
            Reviewed by Sam Weinig.
            
            Partial fix for <rdar://problem/5744037> Gmail out of memory (17455)
            
            I'm removing KJS_MEM_LIMIT for the following reasons:
            
            - We have a few reports of KJS_MEM_LIMIT breaking important web
            applications, like GMail and Google Reader. (For example, if you
            simply open 12 GMail tabs, tab #12 will hit the limit.)
    
            - Firefox has no discernable JS object count limit, so any limit, even
            a large one, is a potential compatibility problem.
            
            - KJS_MEM_LIMIT does not protect against malicious memory allocation,
            since there are many ways to maliciously allocate memory without
            increasing the JS object count.
            
            - KJS_MEM_LIMIT is already mostly broken, since it only aborts the
            script that breaches the limit, not any subsequent scripts.
            
            - We've never gotten bug reports about websites that would have
            benefited from an unbroken KJS_MEM_LIMIT. The initial check-in of
            KJS_MEM_LIMIT (KJS revision 80061) doesn't mention a website that
            needed it.
            
            - Any website that brings you anywhere close to crashing due to the
            number of live JS objects will almost certainly put up the "slow
            script" dialog at least 20 times beforehand.
    
            * kjs/collector.cpp:
            (KJS::Collector::collect):
            * kjs/collector.h:
            * kjs/nodes.cpp:
            (KJS::TryNode::execute):
    
    LayoutTests:
    
            Reviewed by Sam Weinig.
            
            Removing the test for KJS_MEM_LIMIT, since I removed KJS_MEM_LIMIT.
    
            * fast/js/out-of-memory-expected.txt: Removed.
            * fast/js/out-of-memory.html: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30492 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/collector.h
+++ third_party/WebKit/JavaScriptCore/kjs/collector.h
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 #include <wtf/HashCountedSet.h>
 
-#define KJS_MEM_LIMIT 500000
-
 namespace KJS {
 
   class JSCell;
@@ -48,7 +46,6 @@ namespace KJS {
     static void reportExtraMemoryCost(size_t cost);
 
     static size_t size();
-    static bool isOutOfMemory() { return memoryFull; }
 
     static void protect(JSValue*);
     static void unprotect(JSValue*);
