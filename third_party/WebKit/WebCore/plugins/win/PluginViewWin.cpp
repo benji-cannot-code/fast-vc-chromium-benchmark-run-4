3b940a6cfd334063c400900e0d81bd7099ca05e3
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-22 21:39:27 +0000

    Reviewed and tweaked by Jon Honeycutt. Reviewed and landed by Sam.
            - Remove uses of DeprecatedString in Windows plugin code.
    
            * plugins/win/PluginPackageWin.cpp:
            (WebCore::getVersionInfo): Cleanup formatting.
            (WebCore::PluginPackage::freeLibraryTimerFired): Remove un-needed variable name.
            (WebCore::PluginPackage::storeFileVersion): Move casts.
            (WebCore::PluginPackage::fetchInfo): Use OwnArrayPtr and switch to more efficient
            use of Vectors.
            (WebCore::PluginPackage::load): Fix whitespace.
            (WebCore::PluginPackage::hash): Make the hashCodes const
            * plugins/win/PluginViewWin.cpp:
            (WebCore::makeURL): Use String instead of DeprecatedString.
            (WebCore::parseRFC822HeaderFields): Ditto.
            (WebCore::PluginView::handlePost): Ditto.
            (WebCore::PluginView::status): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30488 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
+++ third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
@@ -963,9 +963,9 @@ NPError PluginView::load(const FrameLoadRequest& frameLoadRequest, bool sendNoti
 
 static KURL makeURL(const KURL& baseURL, const char* relativeURLString)
 {
-    DeprecatedString urlString = DeprecatedString::fromLatin1(relativeURLString);
+    String urlString = relativeURLString;
 
-    // Strip return characters
+    // Strip return characters.
     urlString.replace('\n', "");
     urlString.replace('\r', "");
 
@@ -1109,7 +1109,7 @@ static inline HTTPHeaderMap parseRFC822HeaderFields(const Vector<char>& buffer, 
             } else {
                 // Merge the continuation of the previous header
                 String currentValue = headerFields.get(lastKey);
-                String newValue = DeprecatedString::fromLatin1(line, lineLength);
+                String newValue(line, lineLength);
 
                 headerFields.set(lastKey, currentValue + newValue);
             }
@@ -1123,7 +1123,7 @@ static inline HTTPHeaderMap parseRFC822HeaderFields(const Vector<char>& buffer, 
                 // malformed header; ignore it and continue
                 continue;
             else {
-                lastKey = capitalizeRFC822HeaderFieldName(DeprecatedString::fromLatin1(line, colon - line));
+                lastKey = capitalizeRFC822HeaderFieldName(String(line, colon - line));
                 String value;
 
                 for (colon++; colon != eol; colon++) {
@@ -1133,7 +1133,7 @@ static inline HTTPHeaderMap parseRFC822HeaderFields(const Vector<char>& buffer, 
                 if (colon == eol)
                     value = "";
                 else
-                    value = DeprecatedString::fromLatin1(colon, eol - colon);
+                    value = String(colon, eol - colon);
 
                 String oldValue = headerFields.get(lastKey);
                 if (!oldValue.isNull()) {
@@ -1162,7 +1162,7 @@ NPError PluginView::handlePost(const char* url, const char* target, uint32 len, 
     Vector<char> buffer;
     
     if (file) {
-        String filename = DeprecatedString::fromLatin1(buf, len);
+        String filename(buf, len);
 
         if (filename.startsWith("file:///"))
             filename = filename.substring(8);
@@ -1280,10 +1280,8 @@ const char* PluginView::userAgent()
 
 void PluginView::status(const char* message)
 {
-    String s = DeprecatedString::fromLatin1(message);
-
     if (Page* page = m_parentFrame->page())
-        page->chrome()->setStatusbarText(m_parentFrame, s);
+        page->chrome()->setStatusbarText(m_parentFrame, String(message));
 }
 
 NPError PluginView::getValue(NPNVariable variable, void* value)
