c1f76d370e2d18559f9b00952347da4adbfbc1a6
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-23 01:27:17 +0000

    Fix Qt build.
            * Api/qwebhistoryinterface.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30507 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebhistoryinterface.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebhistoryinterface.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <QCoreApplication>
 
+#include "PlatformString.h"
 #include <wtf/Platform.h>
 
 // FIXME: It's not correct to just implement a WebCore function in WebKit!
