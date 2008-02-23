34843da39397f740f9075cf75357e069f90f6074
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-23 03:31:12 +0000

    Once more with the cairo/qt/wx build fixing
    git-svn-id: svn://svn.chromium.org/blink/trunk@30512 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cairo/ImageBufferCairo.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cairo/ImageBufferCairo.cpp
@@ -29,8 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ImageBuffer.h"
 
 #include "GraphicsContext.h"
-#include <cairo.h>
+#include "ImageData.h"
 #include "NotImplemented.h"
+
+#include <cairo.h>
 
 using namespace std;
 
--- third_party/WebKit/WebCore/platform/graphics/wx/ImageBufferWx.cpp
+++ third_party/WebKit/WebCore/platform/graphics/wx/ImageBufferWx.cpp
@@ -26,7 +26,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "ImageBuffer.h"
+
 #include "GraphicsContext.h"
+#include "ImageData.h"
 #include "NotImplemented.h"
 
 namespace WebCore {
