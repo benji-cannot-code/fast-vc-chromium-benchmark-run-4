53aced4f706b1d7300007f85649646bc4aa1bdf2
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-23 03:09:41 +0000

    Windows build fix.  Add missing include of DeprecatedCString.h and sort existing includes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@30511 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/PasteboardWin.cpp
+++ third_party/WebKit/WebCore/platform/win/PasteboardWin.cpp
@@ -27,10 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "Pasteboard.h"
 
-#include "ClipboardUtilitiesWin.h"
 #include "CString.h"
-#include "DocumentFragment.h"
+#include "ClipboardUtilitiesWin.h"
+#include "DeprecatedCString.h"
 #include "Document.h"
+#include "DocumentFragment.h"
 #include "Element.h"
 #include "Frame.h"
 #include "HitTestResult.h"
