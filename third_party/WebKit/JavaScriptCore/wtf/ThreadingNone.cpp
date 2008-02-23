8d797acfd1fe71e5fb4874522ef4f3e52c5325e1
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-23 20:29:58 +0000

    Qt/Wx build fix - this file was still in a wrong namespace, too.
            * wtf/ThreadingNone.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/ThreadingNone.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/ThreadingNone.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "Threading.h"
 
-namespace WebCore {
+namespace WTF {
 
 ThreadIdentifier createThread(ThreadFunction, void*) { return 0; }
 int waitForThreadCompletion(ThreadIdentifier, void**) { return 0; }
