5c20c9442f7b159e61c7f06f41c0206523c656e9
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-25 16:52:30 +0000

    Move two more methods to WebFrameLoaderClient
             Reviewed by Sam.
    
             * WebCoreSupport/WebFrameLoaderClient.cpp:
             (WebFrameLoaderClient::hasFrameView):
             (WebFrame::forceLayout):
             * WebCoreSupport/WebFrameLoaderClient.h:
             * WebFrame.cpp:
             * WebFrame.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebFrame.cpp
+++ third_party/WebKit/WebKit/win/WebFrame.cpp
@@ -1204,19 +1204,9 @@ WebHistory* WebFrame::webHistory()
     return webHistory;
 }
 
-bool WebFrame::hasFrameView() const
-{
-    return !!d->frameView();
-}
-
 void WebFrame::makeRepresentation(DocumentLoader*)
 {
     notImplemented();
-}
-
-void WebFrame::forceLayout()
-{
-    core(this)->forceLayout(true);
 }
 
 void WebFrame::forceLayoutForNonHTML()
