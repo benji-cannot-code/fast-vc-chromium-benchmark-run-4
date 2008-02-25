e2a721d308cc534a63de1851a0268e4756aaae74
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-25 07:11:40 +0000

    Reviewed by Mark Rowe.
            http://bugs.webkit.org/show_bug.cgi?id=17529
            Add support for reading from stdin from testkjs
    
            * kjs/testkjs.cpp:
            (GlobalObject::GlobalObject): Add readline function to global object.
            (functionReadline): Added. Reads characters from stdin until a '\n' or
            EOF is encountered. The input is returned as a String to the caller.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30560 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
@@ -63,6 +63,7 @@ static JSValue* functionGC(ExecState*, JSObject*, const List&);
 static JSValue* functionVersion(ExecState*, JSObject*, const List&);
 static JSValue* functionRun(ExecState*, JSObject*, const List&);
 static JSValue* functionLoad(ExecState*, JSObject*, const List&);
+static JSValue* functionReadline(ExecState*, JSObject*, const List&);
 static JSValue* functionQuit(ExecState*, JSObject*, const List&);
 
 class StopWatch {
@@ -137,6 +138,7 @@ GlobalObject::GlobalObject(Vector<UString>& arguments)
     putDirectFunction(new PrototypeFunction(globalExec(), functionPrototype(), 1, "version", functionVersion));
     putDirectFunction(new PrototypeFunction(globalExec(), functionPrototype(), 1, "run", functionRun));
     putDirectFunction(new PrototypeFunction(globalExec(), functionPrototype(), 1, "load", functionLoad));
+    putDirectFunction(new PrototypeFunction(globalExec(), functionPrototype(), 0, "readline", functionReadline));
 
     JSObject* array = arrayConstructor()->construct(globalExec(), globalExec()->emptyList());
     for (size_t i = 0; i < arguments.size(); ++i)
@@ -197,6 +199,20 @@ JSValue* functionLoad(ExecState* exec, JSObject*, const List& args)
     Interpreter::evaluate(exec->dynamicGlobalObject()->globalExec(), fileName, 0, script.data());
 
     return jsUndefined();
+}
+
+JSValue* functionReadline(ExecState*, JSObject*, const List&)
+{
+    Vector<char, 256> line;
+    int c;
+    while ((c = getchar()) != EOF) {
+        // FIXME: Should we also break on \r? 
+        if (c == '\n')
+            break;
+        line.append(c);
+    }
+    line.append('\0');
+    return jsString(line.data());
 }
 
 JSValue* functionQuit(ExecState*, JSObject*, const List&)
