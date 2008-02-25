84c763e74df5a122b12eced10d5112b453737c52
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-25 06:48:07 +0000

    - quick follow-up to that last check-in
            * platform/graphics/cg/ImageSourceCG.cpp:
            (WebCore::ImageSource::createFrameAtIndex): Use CFEqual instead of
            WebCore::String's == for speed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/ImageSourceCG.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cg/ImageSourceCG.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2004, 2005, 2006, 2008 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,14 +26,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "ImageSource.h"
-#include "SharedBuffer.h"
 
 #if PLATFORM(CG)
 
 #include "IntSize.h"
+#include "SharedBuffer.h"
 #include <ApplicationServices/ApplicationServices.h>
 
 namespace WebCore {
+
+static const CFStringRef kCGImageSourceShouldPreferRGB32 = CFSTR("kCGImageSourceShouldPreferRGB32");
 
 ImageSource::ImageSource()
     : m_decoder(0)
@@ -53,15 +55,13 @@ void ImageSource::clear()
     }
 }
 
-const CFStringRef kCGImageSourceShouldPreferRGB32 = CFSTR("kCGImageSourceShouldPreferRGB32");
-
 CFDictionaryRef imageSourceOptions()
 {
     static CFDictionaryRef options;
     
     if (!options) {
-        const void *keys[2] = { kCGImageSourceShouldCache, kCGImageSourceShouldPreferRGB32 };
-        const void *values[2] = { kCFBooleanTrue, kCFBooleanTrue };
+        const void* keys[2] = { kCGImageSourceShouldCache, kCGImageSourceShouldPreferRGB32 };
+        const void* values[2] = { kCFBooleanTrue, kCFBooleanTrue };
         options = CFDictionaryCreate(NULL, keys, values, 2, 
             &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
     }
@@ -156,8 +156,8 @@ size_t ImageSource::frameCount() const
 CGImageRef ImageSource::createFrameAtIndex(size_t index)
 {
     CGImageRef image = CGImageSourceCreateImageAtIndex(m_decoder, index, imageSourceOptions());
-    String imageUTI = CGImageSourceGetType(m_decoder);
-    if ("public.xbitmap-image" != imageUTI)
+    CFStringRef imageUTI = CGImageSourceGetType(m_decoder);
+    if (!imageUTI || !CFEqual(imageUTI, CFSTR("public.xbitmap-image")))
         return image;
     
     // If it is an xbm image, mask out all the white areas to render them transparent.
