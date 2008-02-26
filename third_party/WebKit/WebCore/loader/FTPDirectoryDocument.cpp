c7f868fc4aeafa9021a4af94f6a0b877c8f781fd
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-26 06:20:09 +0000

    Windows build fix for wx port.
    git-svn-id: svn://svn.chromium.org/blink/trunk@30589 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FTPDirectoryDocument.cpp
+++ third_party/WebKit/WebCore/loader/FTPDirectoryDocument.cpp
@@ -40,6 +40,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SharedBuffer.h"
 #include "Text.h"
 
+// On Win, the threadsafe *_r functions need to be gotten from pthreads.  
+#if COMPILER(MSVC) && USE(PTHREADS) 
+#include <pthread.h> 
+#endif 
+
 #if PLATFORM(QT)
 #include <QDateTime>
 #endif
