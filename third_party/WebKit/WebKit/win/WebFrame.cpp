86b7b84ef83114524d028d4cb8ccf4ee442a7a19
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-26 01:01:00 +0000

    Remove WebFrame::detachedFromParent1
             This method was removed from FrameLoaderClient in r19042, but was
             never removed from WebFrame.
    
             Reviewed by Anders.
    
             * WebFrame.cpp:
             * WebFrame.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30579 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebFrame.cpp
+++ third_party/WebKit/WebKit/win/WebFrame.cpp
@@ -1210,11 +1210,6 @@ void WebFrame::setCopiesOnScroll()
     notImplemented();
 }
 
-void WebFrame::detachedFromParent1()
-{
-    notImplemented();
-}
-
 void WebFrame::detachedFromParent2()
 {
     notImplemented();
