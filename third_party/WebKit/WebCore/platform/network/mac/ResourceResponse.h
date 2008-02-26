88c84521c3111b9a158df6b38eefa9703ebb4ebb
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-26 08:56:02 +0000

    Reviewed by eseidel.  Landed by eseidel
            Fix improperly initialized m_isUpToDate member variable.
            Fixs 50+ layout tests when run on Tiger.
            http://bugs.webkit.org/show_bug.cgi?id=17549
    
            * platform/network/cf/ResourceResponse.h:
            (WebCore::ResourceResponse::ResourceResponse):
            * platform/network/mac/ResourceResponse.h:
            (WebCore::ResourceResponse::ResourceResponse):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/mac/ResourceResponse.h
+++ third_party/WebKit/WebCore/platform/network/mac/ResourceResponse.h
@@ -55,6 +55,7 @@ public:
     
     ResourceResponse(const KURL& url, const String& mimeType, long long expectedLength, const String& textEncodingName, const String& filename)
         : ResourceResponseBase(url, mimeType, expectedLength, textEncodingName, filename)
+        , m_isUpToDate(true)
     {
     }
 
