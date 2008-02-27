79d16dd750596c350a6aa5ed2fff64786d8cfc49
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-27 00:49:43 +0000

    Move WebHistory-related methods to WebFrameLoaderClient
             Reviewed by Anders.
    
             * WebCoreSupport/WebFrameLoaderClient.cpp:
             (WebFrameLoaderClient::updateGlobalHistory):
             (WebFrameLoaderClient::shouldGoToHistoryItem):
             (WebFrameLoaderClient::setTitle): Changed some nested ifs into early
             returns.
             (WebFrameLoaderClient::webHistory): Changed to return a COMPtr to
             make the reference management foolproof.
             * WebCoreSupport/WebFrameLoaderClient.h:
             * WebFrame.cpp:
             * WebFrame.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebCoreSupport/WebFrameLoaderClient.cpp
+++ third_party/WebKit/WebKit/win/WebCoreSupport/WebFrameLoaderClient.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebDocumentLoader.h"
 #include "WebError.h"
 #include "WebFrame.h"
+#include "WebHistory.h"
 #include "WebNotificationCenter.h"
 #include "WebView.h"
 #pragma warning(push, 0)
@@ -284,6 +285,19 @@ void WebFrameLoaderClient::finishedLoading(DocumentLoader* loader)
     m_hasSentResponseToPlugin = false;
 }
 
+void WebFrameLoaderClient::updateGlobalHistory(const KURL& url)
+{
+    COMPtr<WebHistory> history = webHistory();
+    if (!history)
+        return;
+    history->addItemForURL(BString(url.string()), 0);                 
+}
+
+bool WebFrameLoaderClient::shouldGoToHistoryItem(HistoryItem*) const
+{
+    return true;
+}
+
 PassRefPtr<DocumentLoader> WebFrameLoaderClient::createDocumentLoader(const ResourceRequest& request, const SubstituteData& substituteData)
 {
     RefPtr<WebDocumentLoader> loader = new WebDocumentLoader(request, substituteData);
@@ -292,6 +306,31 @@ PassRefPtr<DocumentLoader> WebFrameLoaderClient::createDocumentLoader(const Reso
 
     loader->setDataSource(dataSource.get());
     return loader.release();
+}
+
+void WebFrameLoaderClient::setTitle(const String& title, const KURL& url)
+{
+    BOOL privateBrowsingEnabled = FALSE;
+    COMPtr<IWebPreferences> preferences;
+    if (SUCCEEDED(m_webFrame->webView()->preferences(&preferences)))
+        preferences->privateBrowsingEnabled(&privateBrowsingEnabled);
+    if (privateBrowsingEnabled)
+        return;
+
+    // update title in global history
+    COMPtr<WebHistory> history = webHistory();
+    if (!history)
+        return;
+
+    COMPtr<IWebHistoryItem> item;
+    if (FAILED(history->itemForURL(BString(url.string()), &item)))
+        return;
+
+    COMPtr<IWebHistoryItemPrivate> itemPrivate(Query, item);
+    if (!itemPrivate)
+        return;
+
+    itemPrivate->setTitle(BString(title));
 }
 
 void WebFrameLoaderClient::savePlatformDataToCachedPage(CachedPage* cachedPage)
@@ -515,4 +554,16 @@ void WebFrameLoaderClient::redirectDataToPlugin(Widget* pluginWidget)
     // Ideally, this function shouldn't be necessary, see <rdar://problem/4852889>
 
     m_pluginView = static_cast<PluginView*>(pluginWidget);
+}
+
+COMPtr<WebHistory> WebFrameLoaderClient::webHistory() const
+{
+    if (m_webFrame != m_webFrame->webView()->topLevelFrame())
+        return 0;
+
+    IWebHistoryPrivate* historyInternal = WebHistory::optionalSharedHistoryInternal(); // does not add a ref
+    if (!historyInternal)
+        return 0;
+
+    return COMPtr<WebHistory>(Query, historyInternal);
 }
