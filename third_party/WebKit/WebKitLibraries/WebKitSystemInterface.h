f488813a2e717db9d9e218a6fafce1318705ef5e
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-28 00:42:34 +0000

    Reviewed by Mark Rowe
            Removed some unused methods:
            WKGetNSURLResponseCalculatedExpiration
            WKGetNSURLResponseMustRevalidate
    
            * WebKitSystemInterface.h:
            * libWebKitSystemInterfaceLeopard.a:
            * libWebKitSystemInterfaceTiger.a:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30637 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
+++ third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
@@ -32,8 +32,6 @@ NSString *WKGetMIMETypeForExtension(NSString *extension);
 
 NSDate *WKGetNSURLResponseLastModifiedDate(NSURLResponse *response);
 NSTimeInterval WKGetNSURLResponseFreshnessLifetime(NSURLResponse *response);
-NSTimeInterval WKGetNSURLResponseCalculatedExpiration(NSURLResponse *response);
-BOOL WKGetNSURLResponseMustRevalidate(NSURLResponse *response);
 
 CFStringEncoding WKGetWebDefaultCFStringEncoding(void);
 
