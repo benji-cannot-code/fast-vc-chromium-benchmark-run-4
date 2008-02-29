093437d63f0bfd82cfeabb66222d5a2159ca8602
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-02-29 23:48:42 +0000

    Remove unused DebuggerImp::abort and DebuggerImp::aborted
            Reviewed by Tim and Sam.
    
            * kjs/function_object.cpp:
            (KJS::FunctionObjectImp::construct):
            * kjs/internal.h:
            (KJS::DebuggerImp::DebuggerImp):
            * kjs/nodes.cpp:
            (KJS::Node::handleException):
            (KJS::FunctionBodyNodeWithDebuggerHooks::execute):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/internal.h
+++ third_party/WebKit/JavaScriptCore/kjs/internal.h
@@ -104,14 +104,9 @@ namespace KJS {
 
     DebuggerImp() {
       globalObjects = 0;
-      isAborted = false;
     }
 
-    void abort() { isAborted = true; }
-    bool aborted() const { return isAborted; }
-
     AttachedGlobalObject* globalObjects;
-    bool isAborted;
   };
 
 #ifndef NDEBUG
