f8a6dc8f8607a20bfe1ed474bb8f4d92aebc8d1e
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-01 00:13:29 +0000

    Add WebPreferences::setValueForKey
            This is just a small refactoring of some duplicated logic into a
            shared method.
    
            Reviewed by Sam.
    
            * WebPreferences.cpp:
            (WebPreferences::setValueForKey): Added.
            (WebPreferences::setStringValue): Call setValueForKey.
            (WebPreferences::setIntegerValue): Ditto.
            (WebPreferences::setBoolValue): Ditto.
            (WebPreferences::setLongLongValue): Ditto.
            * WebPreferences.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30682 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebPreferences.cpp
+++ third_party/WebKit/WebKit/win/WebPreferences.cpp
@@ -207,6 +207,13 @@ const void* WebPreferences::valueForKey(CFStringRef key)
     return value;
 }
 
+void WebPreferences::setValueForKey(CFStringRef key, CFPropertyListRef value)
+{
+    CFDictionarySetValue(m_privatePrefs.get(), key, value);
+    if (m_autoSaves)
+        save();
+}
+
 BSTR WebPreferences::stringValueForKey(CFStringRef key)
 {
     CFStringRef str = (CFStringRef)valueForKey(key);
@@ -312,9 +319,7 @@ void WebPreferences::setStringValue(CFStringRef key, LPCTSTR value)
     
     RetainPtr<CFStringRef> valueRef(AdoptCF,
         CFStringCreateWithCharactersNoCopy(0, (UniChar*)_wcsdup(value), (CFIndex)_tcslen(value), kCFAllocatorMalloc));
-    CFDictionarySetValue(m_privatePrefs.get(), key, valueRef.get());
-    if (m_autoSaves)
-        save();
+    setValueForKey(key, valueRef.get());
 
     postPreferencesChangesNotification();
 }
@@ -325,9 +330,7 @@ void WebPreferences::setIntegerValue(CFStringRef key, int value)
         return;
 
     RetainPtr<CFNumberRef> valueRef(AdoptCF, CFNumberCreate(0, kCFNumberSInt32Type, &value));
-    CFDictionarySetValue(m_privatePrefs.get(), key, valueRef.get());
-    if (m_autoSaves)
-        save();
+    setValueForKey(key, valueRef.get());
 
     postPreferencesChangesNotification();
 }
@@ -337,9 +340,7 @@ void WebPreferences::setBoolValue(CFStringRef key, BOOL value)
     if (boolValueForKey(key) == value)
         return;
 
-    CFDictionarySetValue(m_privatePrefs.get(), key, value ? kCFBooleanTrue : kCFBooleanFalse);
-    if (m_autoSaves)
-        save();
+    setValueForKey(key, value ? kCFBooleanTrue : kCFBooleanFalse);
 
     postPreferencesChangesNotification();
 }
@@ -350,9 +351,7 @@ void WebPreferences::setLongLongValue(CFStringRef key, LONGLONG value)
         return;
 
     RetainPtr<CFNumberRef> valueRef(AdoptCF, CFNumberCreate(0, kCFNumberLongLongType, &value));
-    CFDictionarySetValue(m_privatePrefs.get(), key, valueRef.get());
-    if (m_autoSaves)
-        save();
+    setValueForKey(key, valueRef.get());
 
     postPreferencesChangesNotification();
 }
--- third_party/WebKit/WebKit/win/WebPreferences.h
+++ third_party/WebKit/WebKit/win/WebPreferences.h
@@ -316,6 +316,7 @@ public:
     HRESULT postPreferencesChangesNotification();
 
 protected:
+    void setValueForKey(CFStringRef key, CFPropertyListRef value);
     const void* valueForKey(CFStringRef key);
     BSTR stringValueForKey(CFStringRef key);
     int integerValueForKey(CFStringRef key);
