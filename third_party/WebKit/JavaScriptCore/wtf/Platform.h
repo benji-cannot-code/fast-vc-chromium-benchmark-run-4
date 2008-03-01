1f5b749ed2d5396e90e9e1c40fa875bdc05960c2
pewtermoose@webkit.org <pewtermoose@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-01 00:29:22 +0000

    2008-02-29  Brent Fulgham  <bfulgham@gmail.com> JavaScriptCore:
            http://bugs.webkit.org/show_bug.cgi?id=17483
            Implement scrollbars on Windows (Cairo)
    
            Reviewed by Adam Roben.
    
            * wtf/Platform.h:
    
    WebCore:
    
            http://bugs.webkit.org/show_bug.cgi?id=17483
            Implement scrollbars on Windows (Cairo)
    
            Reviewed by Adam Roben.
    
            * platform/win/PlatfromScrollBarWin.cpp: Duplicate implementation
              from PlatformScrollBarWinSafari.cpp, then modify to use the
              native Windows theme engine.  Use SOFT_LINK.  Use platform
              'GetSystemMetrics' call to decide size of scrollbars and buttons.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -113,6 +113,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_PLATFORM_CAIRO 1
 #endif
 
+#if PLATFORM(WIN)&& PLATFORM(CG)
+#define WTF_USE_SAFARI_THEME 1
+#endif
 
 #ifdef __S60__
 // we are cross-compiling, it is not really windows
