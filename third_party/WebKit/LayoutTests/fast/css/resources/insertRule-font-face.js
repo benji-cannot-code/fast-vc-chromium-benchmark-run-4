05828893b245d651643f0dceb1bed73b75ce7115
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-03 19:45:21 +0000

    WebCore:
            Dynamically inserting CSS rule with @media query fails with DOM Exception 12
            <http://bugs.webkit.org/show_bug.cgi?id=15986>
    
            Reviewed by Darin.
    
            Allow any valid rule (@font-face, @import, @media, @page or style) to be
            parsed when using CSSStyleSheet.insertRule().  Previously only import and
            style rules were allowed.  Note that @page rules always throw an exception
            because they're not implemented yet, so no test case was added for them.
    
            Tests: fast/css/insertRule-font-face.html
                   fast/css/insertRule-media.html
    
            * css/CSSGrammar.y: Extracted 'valid_rule' out of 'rule'.  Renamed
            'ruleset_or_import' to 'valid_rule_or_import' and changed its definition.
    
    LayoutTests:
    
            Dynamically inserting CSS rule with @media query fails with DOM Exception 12
            <http://bugs.webkit.org/show_bug.cgi?id=15986>
    
            Reviewed by Darin.
    
            * fast/css/insertRule-font-face.html: Added.
            * fast/css/insertRule-media.html: Added.
            * fast/css/resources/insertRule-font-face.js: Added.
            * fast/css/resources/insertRule-media.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30715 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/css/resources/insertRule-font-face.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+'Test parsing of @font-face rule using insertRule().  <a href="http://bugs.webkit.org/show_bug.cgi?id=15986">http://bugs.webkit.org/show_bug.cgi?id=15986</a>'
+);
+
+var rule = "@font-face { }";
+shouldBe("document.styleSheets[0].insertRule(rule, 0)", "0");
+shouldBe("document.styleSheets[0].rules[0].cssText", "rule");
+
+var successfullyParsed = true;
---
+++ third_party/WebKit/LayoutTests/fast/css/resources/insertRule-media.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+'Test parsing of @media rule using insertRule().  <a href="http://bugs.webkit.org/show_bug.cgi?id=15986">http://bugs.webkit.org/show_bug.cgi?id=15986</a>'
+);
+
+var rule = "@media all and (min-width: 0px) { \n  #greenid { color: green; }\n}";
+shouldBe("document.styleSheets[0].insertRule(rule, 0)", "0");
+shouldBe("document.styleSheets[0].rules[0].cssText", "rule");
+
+var successfullyParsed = true;
