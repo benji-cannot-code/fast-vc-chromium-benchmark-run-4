2a32410084eddb5caa033190d665a0bc4ad71c5c
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-03 21:18:22 +0000

    WebCore:
            Full page zoom plumbing.  Add a notion of whether or not a zoom is text only or a full zoom to the
            Frame.  setTextSizeMultiplier does a text only zoom.
    
            Reviewed by Tim H.
    
            * page/Frame.cpp:
            (WebCore::Frame::zoomFactor):
            (WebCore::Frame::isZoomFactorTextOnly):
            (WebCore::Frame::setZoomFactor):
            (WebCore::FramePrivate::FramePrivate):
            * page/Frame.h:
            * page/FramePrivate.h:
            * page/mac/WebCoreFrameBridge.mm:
            (-[WebCoreFrameBridge setTextSizeMultiplier:]):
            * svg/SVGSVGElement.cpp:
            (WebCore::SVGSVGElement::setCurrentScale):
    
    WebKit/win:
    
            Full page zoom work.  Make setting of a zoom factor take a boolean saying whether it is a text only
            or full page zoom.
    
            Reviewed by Tim H.
    
            * WebFrame.cpp:
            (WebFrame::setTextSizeMultiplier):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30719 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FramePrivate.h
+++ third_party/WebKit/WebCore/page/FramePrivate.h
@@ -85,6 +85,7 @@ namespace WebCore {
         String m_kjsDefaultStatusBarText;
 
         int m_zoomFactor;
+        bool m_zoomFactorIsTextOnly;
 
         TextGranularity m_selectionGranularity;
 
