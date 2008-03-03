6354b3f1bc1702b6e32ac4e77ed9f5233153cbde
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-03 23:08:53 +0000

    wx build fixes after recent Frame-related changes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@30725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/wx/PasteboardWx.cpp
+++ third_party/WebKit/WebCore/platform/wx/PasteboardWx.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Pasteboard.h"
 #include "DocumentFragment.h"
 #include "Editor.h"
+#include "Frame.h"
 #include "KURL.h"
 #include "markup.h"
 #include "PlatformString.h"
