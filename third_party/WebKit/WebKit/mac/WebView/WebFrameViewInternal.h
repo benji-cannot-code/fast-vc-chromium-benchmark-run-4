750a32420397fd396a4921598e40e98972bd48ee
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-04 15:21:42 +0000

    WebCore:
            Reviewed by Adam.
    
            - remove WebCoreFrameBridge installInFrame: method
    
            * WebCore.base.exp: Export a few symbols.
            * WebCore.xcodeproj/project.pbxproj: Export a few files.
            * page/mac/WebCoreFrameBridge.h: Remove installInFrame:.
            * page/mac/WebCoreFrameBridge.mm: Ditto.
    
    WebKit/mac:
    
            Reviewed by Adam.
    
            - remove WebCoreFrameBridge installInFrame: method
    
            * WebCoreSupport/WebFrameLoaderClient.mm:
            (WebFrameLoaderClient::transitionToCommittedForNewPage): Call
            -[WebFrameView _install] instead of -[WebCoreFrameBridge installInFrame:].
            * WebView/WebFrameView.mm:
            (-[WebFrameView _install]): Added. Has code from -[WebCoreFrameBridge installInFrame:].
            (-[WebFrameView _setCustomScrollViewClass:]): Used early return idiom so the entire
            method isn't nested inside an if statement. Call
            -[WebFrameView _install] instead of -[WebCoreFrameBridge installInFrame:].
            * WebView/WebFrameViewInternal.h: Added declaration of _install method so it can be
            used in WebFrameLoaderClient.mm.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30743 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebFrameViewInternal.h
+++ third_party/WebKit/WebKit/mac/WebView/WebFrameViewInternal.h
@@ -47,5 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (Class)_viewClassForMIMEType:(NSString *)MIMEType;
 + (BOOL)_canShowMIMETypeAsHTML:(NSString *)MIMEType;
 - (WebDynamicScrollBarsView *)_scrollView;
+- (void)_install;
 
 @end
