bc2e51dc6fcb12f45b19ca28380a9416a9818057
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-04 08:11:50 +0000

    Build fix.
            * bindings/js/kjs_events.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30736 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/kjs_events.cpp
+++ third_party/WebKit/WebCore/bindings/js/kjs_events.cpp
@@ -38,8 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "kjs_window.h"
 #include <kjs/function_object.h>
 
-#include "kjs_events.lut.h"
-
 using namespace KJS;
 
 namespace WebCore {
