e4991ea7317ced5a8ae5e97a9320681a71c45b22
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-04 22:35:27 +0000

    Reviewed by Darin.
            Fix crash that happens when trying to load a page with a Java applet.
            
            * WebCoreSupport/WebFrameLoaderClient.mm:
            Don't release the names and values array - the kit method returns an autoreleased array.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30765 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebFrameLoaderClient.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebFrameLoaderClient.mm
@@ -1431,9 +1431,6 @@ Widget* WebFrameLoaderClient::createJavaAppletWidget(const IntSize& size, Elemen
                 [values addObject:[NSString stringWithFormat:@"%d", size.width()]];
             }
             view = pluginView(m_webFrame.get(), (WebPluginPackage *)pluginPackage, names, values, baseURL, kit(element), NO);
-            [names release];
-            [values release];
-            
         } 
 #ifndef __LP64__
         else if ([pluginPackage isKindOfClass:[WebNetscapePluginPackage class]]) {
