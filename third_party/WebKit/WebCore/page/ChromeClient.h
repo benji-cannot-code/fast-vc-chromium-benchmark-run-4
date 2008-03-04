38b6e04d16de5c6e6f516580fd991c3cdad00e66
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-04 16:49:48 +0000

    2008-03-04  Alp Toker  <alp@atoker.com>
            GTK+ build fix suggested by aroben. Remove an unused Mac-specific
            included introduced in r30740.
    
            * page/ChromeClient.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30748 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/ChromeClient.h
+++ third_party/WebKit/WebCore/page/ChromeClient.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ChromeClient_h
 
 #include "FocusDirection.h"
-#include "WebCoreKeyboardUIMode.h"
 #include <wtf/Forward.h>
 #include <wtf/Vector.h>
 
