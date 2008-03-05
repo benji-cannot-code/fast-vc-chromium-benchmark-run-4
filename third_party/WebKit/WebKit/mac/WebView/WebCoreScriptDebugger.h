477fb12ddaa18b48158f0698324b4fa33fa49a56
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-05 19:34:29 +0000

    Move -scopeChain to WebScriptCallFrame
             Reviewed by Darin.
    
             * WebView/WebCoreScriptDebugger.h:
               - Added declarations of -globalObject and -_convertValueToObjcValue:
                 to WebCoreScriptCallFrame
               - Removed declaration of -scopeChain.
             * WebView/WebCoreScriptDebugger.mm: Moved -_convertValueToObjcValue
             within the main WebCoreScriptCallFrame implementation.
             (-[WebCoreScriptCallFrame globalObject]): Added.
             * WebView/WebScriptDebugDelegate.mm:
             (-[WebScriptCallFrame scopeChain]): Added. Code came from
             -[WebCoreScriptCallFrame scopeChain].
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebCoreScriptDebugger.h
+++ third_party/WebKit/WebKit/mac/WebView/WebCoreScriptDebugger.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class WebCoreScriptDebuggerImp;
 namespace KJS {
     class ExecState;
+    class JSValue;
     class UString;
 }
 
@@ -86,11 +87,12 @@ NSString *toNSString(const KJS::UString&);
 }
 
 - (id)wrapper;
+- (WebScriptObject *)globalObject;
 - (WebCoreScriptCallFrame *)caller;
 - (KJS::ExecState*)state;
 
-- (NSArray *)scopeChain;
 - (id)exception;
 - (id)evaluateWebScript:(NSString *)script;
+- (id)_convertValueToObjcValue:(KJS::JSValue*)value;
 
 @end
