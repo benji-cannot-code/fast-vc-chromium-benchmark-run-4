1374ad3dd4ae038a588f5673f831a620332d3b4d
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-05 06:42:15 +0000

    Qt build fix.
            * page/qt/FrameQt.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30788 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/qt/FrameQt.cpp
+++ third_party/WebKit/WebCore/page/qt/FrameQt.cpp
@@ -66,7 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSLock.h"
 #include "runtime_root.h"
 #include "runtime.h"
-#include "JSDOMWindow"
+#include "JSDOMWindow.h"
 #include <QScrollArea>
 #include "NotImplemented.h"
 
