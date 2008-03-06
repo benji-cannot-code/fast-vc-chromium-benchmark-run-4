d9ea87eaa17308945867d02c1eb67dea044410d2
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-06 20:58:35 +0000

    Tiger build fix...?
            * DumpRenderTree/mac/WatchdogMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30854 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/WatchdogMac.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/WatchdogMac.mm
@@ -30,6 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#import <sys/types.h>
+#import <unistd.h>
+
 void WatchdogMac::handleHang()
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
