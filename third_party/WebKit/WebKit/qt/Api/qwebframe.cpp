a4202c980f87dfd2efb4ff0262437acc792afca0
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-07 13:09:09 +0000

    Fix the Qt build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@30870 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
@@ -53,8 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 #include "HitTestResult.h"
 
-#include "bindings/runtime.h"
-#include "bindings/runtime_root.h"
+#include "runtime.h"
+#include "runtime_root.h"
 #include "JSDOMWindow.h"
 #include "kjs_proxy.h"
 #include "kjs_binding.h"
