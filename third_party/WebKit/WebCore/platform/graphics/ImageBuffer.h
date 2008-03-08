750a6602bede1e904a9e1b9d58b61ea6e593365b
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-08 03:08:48 +0000

    WebCore:
            Reviewed by Oliver Hunt.
    
            Fix for http://bugs.webkit.org/show_bug.cgi?id=16673
            <canvas> lacks toDataURL()
    
            Tests: fast/canvas/toDataURL-noData.html
                   fast/canvas/toDataURL-supportedTypes.html
    
            * html/CanvasRenderingContext2D.cpp:
            (WebCore::CanvasRenderingContext2D::printSecurityExceptionMessage): Make message non-global.
            * html/HTMLCanvasElement.cpp:
            (WebCore::HTMLCanvasElement::printSecurityExceptionMessage): Log error message indicating that toDataURL has
            been called on a tainted canvas. 
            (WebCore::HTMLCanvasElement::toDataURL): This does the tainted canvas check, ensures that the canvas has pixel
            data, checks to see if the MIME type is supported.  Unsupported MIME types and the null string (which is made
            by passing no argument/null/undefined in JavaScript) are treated as being PNG, as per the spec.  The actual 
            toDataURL logic is actually done in the ImageBuffer class.
            * html/HTMLCanvasElement.h: Add function declarations.
            * html/HTMLCanvasElement.idl: Add function declaration.
    
            * platform/MIMETypeRegistry.cpp:
            (WebCore::initializeSupportedImageMIMETypes):
            (WebCore::initializeSupportedImageMIMETypesForEncoding):
            (WebCore::initializeSupportedJavaScriptMIMETypes):
            (WebCore::initializeSupportedNonImageMimeTypes):
            (WebCore::initializeSupportedMediaMIMETypes):
            (WebCore::initializeMIMETypeRegistry):
            (WebCore::MIMETypeRegistry::getMIMETypeForPath):
            (WebCore::MIMETypeRegistry::isSupportedImageMIMEType):
            (WebCore::MIMETypeRegistry::isSupportedImageResourceMIMEType):
            (WebCore::MIMETypeRegistry::isSupportedImageMIMETypeForEncoding):
            (WebCore::MIMETypeRegistry::isSupportedJavaScriptMIMEType):
            (WebCore::MIMETypeRegistry::isSupportedNonImageMIMEType):
            (WebCore::MIMETypeRegistry::isSupportedMediaMIMEType):
            (WebCore::MIMETypeRegistry::isJavaAppletMIMEType):
            (WebCore::MIMETypeRegistry::getSupportedImageMIMETypes):
            (WebCore::MIMETypeRegistry::getSupportedImageResourceMIMETypes):
            (WebCore::MIMETypeRegistry::getSupportedImageMIMETypesForEncoding):
            (WebCore::MIMETypeRegistry::getSupportedNonImageMIMETypes):
            (WebCore::MIMETypeRegistry::getSupportedMediaMIMETypes):
            * platform/MIMETypeRegistry.h:
            Clean up and add new supportedImageMIMETypesForEncoding set which contains the set of
            MIME types the platform knows how to encode. Currently only implemented for CG and Qt.
    
            * platform/graphics/ImageBuffer.h: Add toDataURL method.
            * platform/graphics/cairo/ImageBufferCairo.cpp:
            (WebCore::ImageBuffer::toDataURL): Add stub.
            * platform/graphics/cg/ImageBufferCG.cpp:
            (WebCore::ImageBuffer::create):
            (WebCore::ImageBuffer::getImageData):
            (WebCore::ImageBuffer::putImageData):
            (WebCore::ImageBuffer::toDataURL):
            Converts the current context to a data: url of the specified MIME type.  This method
            unfortunately has to flip the context, resulting in less than optimal code.
    
            * platform/graphics/qt/ImageBufferQt.cpp:
            (WebCore::ImageBuffer::toDataURL): Add stub.
            * platform/graphics/wx/ImageBufferWx.cpp:
            (WebCore::ImageBuffer::toDataURL): Add stub.
    
    LayoutTests:
    
            Reviewed by Oliver Hunt.
    
            Tests for http://bugs.webkit.org/show_bug.cgi?id=16673
            <canvas> lacks toDataURL()
    
            * fast/canvas/toDataURL-noData-expected.txt: Added.
            * fast/canvas/toDataURL-noData.html: Added.
            * fast/canvas/toDataURL-supportedTypes-expected.txt: Added.
            * fast/canvas/toDataURL-supportedTypes.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30888 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/ImageBuffer.h
+++ third_party/WebKit/WebCore/platform/graphics/ImageBuffer.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006 Nikolas Zimmermann <zimmermann@kde.org>
- * Copyright (C) 2007 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -53,6 +53,7 @@ namespace WebCore {
     class IntPoint;
     class IntRect;
     class RenderObject;
+    class String;
 
     class ImageBuffer : Noncopyable {
     public:
@@ -73,6 +74,8 @@ namespace WebCore {
         PassRefPtr<ImageData> getImageData(const IntRect& rect) const;
         void putImageData(ImageData* source, const IntRect& sourceRect, const IntPoint& destPoint);
 
+        String toDataURL(const String& mimeType) const;
+
     private:
         void* m_data;
         IntSize m_size;
@@ -91,6 +94,7 @@ namespace WebCore {
         mutable cairo_surface_t* m_surface;
 #endif
     };
-}
 
-#endif
+} // namespace WebCore
+
+#endif // ImageBuffer_h
