41f5dea681454cabfae78166ccbdd54497addf84
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-09 01:57:17 +0000

    2008-03-08  Julien Chaffraix  <julien.chaffraix@gmail.com>
            Reviewed by Mark Rowe.
    
            DRT build fix for Tiger.
    
            * DumpRenderTree/mac/DumpRenderTreeMac.h: Define BUILDING_ON_TIGER.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30903 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/DumpRenderTreeMac.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/DumpRenderTreeMac.h
@@ -33,6 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // FIXME: we should add a config.h file for DumpRenderTree.
 #define WTF_PLATFORM_CF 1
 
+#ifdef MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_4
+#define BUILDING_ON_TIGER 1
+#endif
+
 @class DumpRenderTreeDraggingInfo;
 @class NavigationController;
 @class PolicyDelegate;
