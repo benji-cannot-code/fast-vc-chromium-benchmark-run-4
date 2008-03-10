eac559ec7ca95a215ca5f8bc8ae7f5d7f942fbb3
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-10 00:49:15 +0000

    2008-03-09  J?rg Billeter  <j@bitron.ch>
            Reviewed by Alp Toker.
    
            Conditionalise ICU for Unicode in the GTK+ port.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -204,11 +204,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_USE_MULTIPLE_THREADS 1
 #endif
 
-/* for Unicode, KDE uses Qt, everything else uses ICU */
+/* for Unicode, KDE uses Qt */
 #if PLATFORM(KDE) || PLATFORM(QT)
 #define WTF_USE_QT4_UNICODE 1
 #elif PLATFORM(SYMBIAN)
 #define WTF_USE_SYMBIAN_UNICODE 1
+#elif PLATFORM(GTK)
+/* The GTK+ Unicode backend is configurable */
 #else
 #define WTF_USE_ICU_UNICODE 1
 #endif
