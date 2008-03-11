2582adca7faac01fa732b4c3cc374e381e52804a
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-11 19:59:04 +0000

    Stop including pthread.h in WebCore on Windows
            Reviewed by Darin.
    
            * WebCore.vcproj/WebCore.vcproj:
              - Changed the include path to not contain pthreads/
              - Let VS remove a duplicate FileConfiguration element
              - Let VS have its way with the file ordering
            * WebCorePrefix.h: Made the #include of pthread.h be compiled out on
            Windows.
            * loader/FTPDirectoryDocument.cpp: Added a Windows-specific #define of
            localtime_r (pthread.h was providing a macro that substituted plain
            old localtime before, but localtime_s is "more secure", so I went with
            that).
            * loader/FTPDirectoryParser.cpp: Ditto for gmtime_r.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30965 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCorePrefix.h
+++ third_party/WebKit/WebCore/WebCorePrefix.h
@@ -58,11 +58,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define min min
 #endif
 
+#else
+#include <pthread.h>
 #endif // defined(WIN32) || defined(_WIN32)
 
 #include <sys/types.h>
 #include <fcntl.h>
-#include <pthread.h>
 #if defined(__APPLE__)
 #include <regex.h>
 #endif
