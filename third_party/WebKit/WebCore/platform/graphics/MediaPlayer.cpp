ec2af0c1a352b4521eba744713c99cb2ce803d11
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-11 14:59:58 +0000

    2008-03-11  Tor Arne Vestbø  <tavestbo@trolltech.com>
            Reviewed by Darin.
    
            Implemented HTML media element support for QtWebKit, using Phonon.
    
            http://bugs.webkit.org/show_bug.cgi?id=17766
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@30960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/MediaPlayer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/MediaPlayer.cpp
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MediaPlayerPrivateQuickTimeWin.h"
 #elif PLATFORM(GTK)
 #include "MediaPlayerPrivateGStreamer.h"
+#elif PLATFORM(QT)
+#include "MediaPlayerPrivatePhonon.h"
 #endif
 
 namespace WebCore {
