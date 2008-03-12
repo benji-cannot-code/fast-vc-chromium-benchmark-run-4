bb1f8fd3f74e59b53ee201e66473de09139d7f16
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-12 19:40:15 +0000

    2008-03-12  Tommi Komulainen  <tommi.komulainen@iki.fi>
            Reviewed by Alp Toker.
    
            http://bugs.webkit.org/show_bug.cgi?id=17799
            WebView signals should use more specific types
    
            * webkit/webkitwebview.cpp: Use WEBKIT_TYPE_WEB_FRAME argument
            type in "script-alert", "script-confirm", "script-script-prompt"
            signals so that it gets more clearly documented rather than just
            plain GObject.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
@@ -877,7 +877,7 @@ static void webkit_web_view_class_init(WebKitWebViewClass* webViewClass)
             NULL,
             webkit_marshal_BOOLEAN__OBJECT_STRING,
             G_TYPE_BOOLEAN, 2,
-            G_TYPE_OBJECT, G_TYPE_STRING);
+            WEBKIT_TYPE_WEB_FRAME, G_TYPE_STRING);
 
     /**
      * WebKitWebView::script-confirm:
@@ -897,7 +897,7 @@ static void webkit_web_view_class_init(WebKitWebViewClass* webViewClass)
             NULL,
             webkit_marshal_BOOLEAN__OBJECT_STRING_BOOLEAN,
             G_TYPE_BOOLEAN, 3,
-            G_TYPE_OBJECT, G_TYPE_STRING, G_TYPE_BOOLEAN);
+            WEBKIT_TYPE_WEB_FRAME, G_TYPE_STRING, G_TYPE_BOOLEAN);
 
     /**
      * WebKitWebView::script-prompt:
@@ -918,7 +918,7 @@ static void webkit_web_view_class_init(WebKitWebViewClass* webViewClass)
             NULL,
             webkit_marshal_BOOLEAN__OBJECT_STRING_STRING_STRING,
             G_TYPE_BOOLEAN, 4,
-            G_TYPE_OBJECT, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_POINTER);
+            WEBKIT_TYPE_WEB_FRAME, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_POINTER);
 
     /**
      * WebKitWebView::select-all:
