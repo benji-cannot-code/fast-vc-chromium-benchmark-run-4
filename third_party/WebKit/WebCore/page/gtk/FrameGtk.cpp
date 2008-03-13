54701eb4d5917517f6d53dd64bc1e709b447d96c
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-13 05:40:12 +0000

    Further Gtk and Qt build fixes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@31021 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/gtk/FrameGtk.cpp
+++ third_party/WebKit/WebCore/page/gtk/FrameGtk.cpp
@@ -29,7 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "Frame.h"
+
 #include "NotImplemented.h"
+#include "runtime.h"
 
 
 namespace WebCore {
