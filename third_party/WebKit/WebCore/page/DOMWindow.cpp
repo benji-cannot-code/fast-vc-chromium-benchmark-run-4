5283819860d6936fcaba85c8f2a95785fedcf662
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-13 03:40:47 +0000

    Reviewed by Anders Carlsson.
            Don't go through the Document just to get the frame Element.
    
            * page/DOMWindow.cpp:
            (WebCore::DOMWindow::frameElement):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31013 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/DOMWindow.cpp
+++ third_party/WebKit/WebCore/page/DOMWindow.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2006, 2007 Apple Inc. All rights reserved.
+ * Copyright (C) 2006, 2007, 2008 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoader.h"
 #include "FrameTree.h"
 #include "FrameView.h"
+#include "HTMLFrameOwnerElement.h"
 #include "History.h"
 #include "MessageEvent.h"
 #include "Navigator.h"
@@ -252,13 +253,7 @@ Element* DOMWindow::frameElement() const
     if (!m_frame)
         return 0;
 
-    Document* doc = m_frame->document();
-    ASSERT(doc);
-    if (!doc)
-        return 0;
-
-    // FIXME: could this use m_frame->ownerElement() instead of going through the Document.
-    return doc->ownerElement();
+    return m_frame->ownerElement();
 }
 
 void DOMWindow::focus()
