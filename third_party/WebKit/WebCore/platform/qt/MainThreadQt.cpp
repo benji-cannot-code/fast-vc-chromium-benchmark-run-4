bc8a73dc0d2f1438084357552fd4bfcf6d660c69
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-18 15:20:49 +0000

    Fix the Qt build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@31126 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/MainThreadQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/MainThreadQt.cpp
@@ -43,8 +43,8 @@ class MainThreadInvoker : public QObject {
 public:
     MainThreadInvoker();
 
-protected:
-    bool event(QEvent*);
+private Q_SLOTS:
+    void dispatch();
 };
 
 MainThreadInvoker::MainThreadInvoker()
@@ -52,7 +52,7 @@ MainThreadInvoker::MainThreadInvoker()
     moveToThread(QCoreApplication::instance()->thread());
 }
 
-bool MainThreadInvoker::event(QEvent*)
+void MainThreadInvoker::dispatch()
 {
     dispatchFunctionsFromMainThread();
 }
@@ -62,7 +62,7 @@ Q_GLOBAL_STATIC(MainThreadInvoker, webkit_main_thread_invoker)
 
 void scheduleDispatchFunctionsOnMainThread()
 {
-    QCoreApplication::postEvent(webkit_main_thread_invoker(), 0);
+    QMetaObject::invokeMethod(webkit_main_thread_invoker(), "dispatch", Qt::QueuedConnection);
 }
 
 }
