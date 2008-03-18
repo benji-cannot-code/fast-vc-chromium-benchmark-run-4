ed8dfa6b535b955e3b13224dd04e8cc2d3a4ad32
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-18 21:10:30 +0000

    2008-03-18  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Oliver.
            
            - inline ActivationImp::init for 0.8% SunSpider speedup
    
            * kjs/Activation.h:
            (KJS::ActivationImp::init): Moved here from function.cpp
            * kjs/function.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31136 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/Activation.h
+++ third_party/WebKit/JavaScriptCore/kjs/Activation.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ExecState.h"
 #include "JSVariableObject.h"
 #include "object.h"
+#include "function.h"
 
 namespace KJS {
 
@@ -81,6 +82,14 @@ namespace KJS {
         void createArgumentsObject(ExecState*);
         ActivationData* d() const { return static_cast<ActivationData*>(JSVariableObject::d); }
     };
+
+    inline void ActivationImp::init(ExecState* exec)
+    {
+        d()->exec = exec;
+        d()->function = exec->function();
+        d()->symbolTable = &exec->function()->body->symbolTable();
+        d()->argumentsObject = 0;
+    }
 
     const size_t activationStackNodeSize = 32;
 
