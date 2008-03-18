b0f3ae820eb41a0497e51aa97d0315f8ed76eca6
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-18 21:25:56 +0000

    2008-03-18  Sam Weinig  <sam@webkit.org>
            Reviewed by Darin Adler.
    
            - Speed up JavaScript prototype and constructor object creation
              using a static Identifier in the self() methods to avoid the
              cost of creating one from a c-string each time.
    
            5% speedup for Acid3 test 26
    
            * bindings/scripts/CodeGeneratorJS.pm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31137 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -737,7 +737,8 @@ sub GenerateImplementation
     } else {
         push(@implContent, "JSObject* ${className}Prototype::self(ExecState* exec)\n");
         push(@implContent, "{\n");
-        push(@implContent, "    return KJS::cacheGlobalObject<${className}Prototype>(exec, \"[[${className}.prototype]]\");\n");
+        push(@implContent, "    static const Identifier* prototypeIdentifier = new Identifier(\"[[${className}.prototype]]\");\n");
+        push(@implContent, "    return KJS::cacheGlobalObject<${className}Prototype>(exec, *prototypeIdentifier);\n");
         push(@implContent, "}\n\n");
     }
     if ($numConstants > 0 || $numFunctions > 0) {
@@ -1116,7 +1117,8 @@ sub GenerateImplementation
 
     if ($dataNode->extendedAttributes->{"GenerateConstructor"}) {
         push(@implContent, "JSValue* ${className}::getConstructor(ExecState* exec)\n{\n");
-        push(@implContent, "    return KJS::cacheGlobalObject<${className}Constructor>(exec, \"[[${interfaceName}.constructor]]\");\n");
+        push(@implContent, "    static const Identifier* constructorIdentifier = new Identifier(\"[[${interfaceName}.constructor]]\");\n");
+        push(@implContent, "    return KJS::cacheGlobalObject<${className}Constructor>(exec, *constructorIdentifier);\n");
         push(@implContent, "}\n\n");
     }
 
