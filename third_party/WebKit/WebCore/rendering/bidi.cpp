f8146b6771e30f07724c232e8dbde40e5f9cd481
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-18 04:36:51 +0000

    2008-03-17  Dan Bernstein  <mitz@apple.com>
            Rubber-stamped by Dave Hyatt.
    
            - FloatingObject cleanup
    
            Renamed FloatingObject's data members as follows: node -> m_renderer,
            startY -> m_top, endY -> m_bottom, left -> m_left, width -> m_width,
            and noPaint -> !m_shouldPaint, reversing the meaning of the flag.
    
            Also addressed the FIXME in RenderBlock::containsFloat().
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::repaintOverhangingFloats):
            (WebCore::RenderBlock::paintFloats):
            (WebCore::RenderBlock::insertFloatingObject):
            (WebCore::RenderBlock::removeFloatingObject):
            (WebCore::RenderBlock::positionNewFloats):
            (WebCore::RenderBlock::leftRelOffset):
            (WebCore::RenderBlock::rightRelOffset):
            (WebCore::RenderBlock::nextFloatBottomBelow):
            (WebCore::RenderBlock::floatBottom):
            (WebCore::RenderBlock::floatRect):
            (WebCore::RenderBlock::lowestPosition):
            (WebCore::RenderBlock::rightmostPosition):
            (WebCore::RenderBlock::leftmostPosition):
            (WebCore::RenderBlock::leftBottom):
            (WebCore::RenderBlock::rightBottom):
            (WebCore::RenderBlock::clearFloats):
            (WebCore::RenderBlock::addOverhangingFloats):
            (WebCore::RenderBlock::addIntrudingFloats):
            (WebCore::RenderBlock::containsFloat): Changed to return false if the
            floats lists exists but is empty, since line layout code no longer
            relies on the buggy behavior.
            (WebCore::RenderBlock::nodeAtPoint):
            (WebCore::RenderBlock::adjustForBorderFit):
            * rendering/RenderBlock.h:
            (WebCore::RenderBlock::containsFloats):
            (WebCore::RenderBlock::FloatingObject::FloatingObject):
            * rendering/bidi.cpp:
            (WebCore::RenderBlock::layoutInlineChildren):
            (WebCore::RenderBlock::matchedEndLine):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31117 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/bidi.cpp
+++ third_party/WebKit/WebCore/rendering/bidi.cpp
@@ -946,10 +946,10 @@ void RenderBlock::layoutInlineChildren(bool relayoutChildren, int& repaintTop, i
                 } else
                     m_floatingObjects->first();
                 for (FloatingObject* f = m_floatingObjects->current(); f; f = m_floatingObjects->next()) {
-                    lastRootBox()->floats().append(f->node);
-                    ASSERT(f->node == floats[floatIndex].object);
+                    lastRootBox()->floats().append(f->m_renderer);
+                    ASSERT(f->m_renderer == floats[floatIndex].object);
                     // If a float's geometry has changed, give up on syncing with clean lines.
-                    if (floats[floatIndex].rect != IntRect(f->left, f->startY, f->width, f->endY - f->startY))
+                    if (floats[floatIndex].rect != IntRect(f->m_left, f->m_top, f->m_width, f->m_bottom - f->m_top))
                         endLine = 0;
                     floatIndex++;
                 }
@@ -1008,7 +1008,7 @@ void RenderBlock::layoutInlineChildren(bool relayoutChildren, int& repaintTop, i
                 } else
                     m_floatingObjects->first();
                 for (FloatingObject* f = m_floatingObjects->current(); f; f = m_floatingObjects->next())
-                    lastRootBox()->floats().append(f->node);
+                    lastRootBox()->floats().append(f->m_renderer);
                 lastFloat = m_floatingObjects->last();
             }
         }
@@ -1210,7 +1210,7 @@ bool RenderBlock::matchedEndLine(const BidiState& start, const BidiIterator& end
         int bottom = lastLine->blockHeight() + abs(delta);
 
         for (FloatingObject* f = m_floatingObjects->first(); f; f = m_floatingObjects->next()) {
-            if (f->endY >= top && f->endY < bottom)
+            if (f->m_bottom >= top && f->m_bottom < bottom)
                 return false;
         }
 
@@ -1244,7 +1244,7 @@ bool RenderBlock::matchedEndLine(const BidiState& start, const BidiIterator& end
                 int bottom = lastLine->blockHeight() + abs(delta);
 
                 for (FloatingObject* f = m_floatingObjects->first(); f; f = m_floatingObjects->next()) {
-                    if (f->endY >= top && f->endY < bottom)
+                    if (f->m_bottom >= top && f->m_bottom < bottom)
                         return false;
                 }
             }
