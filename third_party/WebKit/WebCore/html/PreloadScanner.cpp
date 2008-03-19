6b7c046b3ceb4780f94852185b0b16ce573e7276
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-19 09:37:58 +0000

    2008-03-19  Mark Rowe  <mrowe@apple.com>
            Reviewed by Oliver Hunt.
    
            Use WTF::Unicode abstraction rather than using ICU functions directly.
    
            * html/PreloadScanner.cpp:
            (WebCore::PreloadScanner::tokenize):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31152 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/PreloadScanner.cpp
+++ third_party/WebKit/WebCore/html/PreloadScanner.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLLinkElement.h"
 #include "HTMLNames.h"
 #include "SystemTime.h"
+#include <wtf/unicode/Unicode.h>
 
 #ifdef __GNUC__
 // The main tokenizer includes this too so we are getting two copies of the data. However, this way the code gets inlined.
@@ -57,6 +58,8 @@ const struct Entity* findEntity(register const char* str, register unsigned int 
 #endif
 
 #define PRELOAD_DEBUG 0
+
+using namespace WTF;
 
 namespace WebCore {
     
@@ -372,7 +375,7 @@ void PreloadScanner::tokenize(const SegmentedString& source)
                 UChar tmpChar = 0;
                 bool match = true;
                 for (unsigned n = 0; n < m_lastStartTag.length() + 1; n++) {
-                    tmpChar = u_tolower(*m_source);
+                    tmpChar = Unicode::toLower(*m_source);
                     if (n < m_lastStartTag.length() && tmpChar != m_lastStartTag[n])
                         match = false;
                     tmpString.append(tmpChar);
