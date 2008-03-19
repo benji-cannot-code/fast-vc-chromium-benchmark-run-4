5bd65606a88831cd74bc5d9db5e38bdfcf87d73e
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-19 20:39:18 +0000

    Make clicking anywhere in a row in the DOM tree select that row's node
            We now have mousedown and dblclick event listeners on the root of the
            tree that forward the event to the node on the row the mouse is over.
    
            Reviewed by Tim Hatcher.
    
            * page/inspector/DocumentPanel.js:
            (WebInspector.DocumentPanel): Added a dblclick and mousedown event
            listeners to the root of the tree.
            (WebInspector.DocumentPanel._treeElementFromEvent): Added. Finds the
            tree element for the row underneath the mouse.
            (WebInspector.DocumentPanel._ondblclick): Added. Sends the dblclick
            event on to the tree element in the current row.
            (WebInspector.DocumentPanel._onmousedown): Added. Selects the tree
            element in the current row.
            * page/inspector/treeoutline.js:
            (TreeOutline.treeElementFromPoint): Added.
            (TreeElement.treeElementSelected): Changed to call
            TreeElement.isEventWithinDisclosureTriangle, and added an early return.
            (TreeElement.treeElementToggled): Ditto.
            (TreeElement.isEventWithinDisclosureTriangle): Added.
            * page/inspector/utilities.js:
            (Node.enclosingNodeOrSelfWithNodeNameInArray): Added.
            (Node.enclosingNodeOrSelfWithNodeName): Now just calls
            enclosingNodeOrSelfWithNodeNameInArray.
            (Elemnt.get totalOffsetLeft): Added.
            (Elemnt.get totalOffsetTop): Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31159 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/treeoutline.js
+++ third_party/WebKit/WebCore/page/inspector/treeoutline.js
@@ -286,6 +286,13 @@ TreeOutline.prototype.findTreeElement = function(representedObject, isAncestor, 
     return item;
 }
 
+TreeOutline.prototype.treeElementFromPoint = function(x, y)
+{
+    var node = this._childrenListNode.ownerDocument.elementFromPoint(x, y);
+    var listNode = node.enclosingNodeOrSelfWithNodeNameInArray(["ol", "li"]);
+    return listNode.parentTreeElement || listNode.treeElement;
+}
+
 TreeOutline.prototype.handleKeyEvent = function(event)
 {
     if (!this.selectedTreeElement || event.shiftKey || event.metaKey || event.ctrlKey)
@@ -512,8 +519,10 @@ TreeElement.treeElementSelected = function(event)
     if (!element || !element.treeElement || !element.treeElement.selectable)
         return;
 
-    if (event.offsetX > element.treeElement.arrowToggleWidth || !element.treeElement.hasChildren)
-        element.treeElement.select();
+    if (element.treeElement.isEventWithinDisclosureTriangle(event))
+        return;
+
+    element.treeElement.select();
 }
 
 TreeElement.treeElementToggled = function(event)
@@ -522,18 +531,19 @@ TreeElement.treeElementToggled = function(event)
     if (!element || !element.treeElement)
         return;
 
-    if (event.offsetX <= element.treeElement.arrowToggleWidth && element.treeElement.hasChildren) {
-        if (element.treeElement.expanded) {
-            if (event.altKey)
-                element.treeElement.collapseRecursively();
-            else
-                element.treeElement.collapse();
-        } else {
-            if (event.altKey)
-                element.treeElement.expandRecursively();
-            else
-                element.treeElement.expand();
-        }
+    if (!element.treeElement.isEventWithinDisclosureTriangle(event))
+        return;
+
+    if (element.treeElement.expanded) {
+        if (event.altKey)
+            element.treeElement.collapseRecursively();
+        else
+            element.treeElement.collapse();
+    } else {
+        if (event.altKey)
+            element.treeElement.expandRecursively();
+        else
+            element.treeElement.expand();
     }
 }
 
@@ -726,4 +736,10 @@ TreeElement.prototype.traversePreviousTreeElement = function(skipHidden, dontPop
         return null;
 
     return this.parent;
+}
+
+TreeElement.prototype.isEventWithinDisclosureTriangle = function(event)
+{
+    var left = this._listItemNode.totalOffsetLeft;
+    return event.pageX >= left && event.pageX <= left + this.arrowToggleWidth && this.hasChildren;
 }
