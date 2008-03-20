5552fd322662022557982ffdf0bca67fcf880389
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-20 22:59:09 +0000

    Highlight nodes in the page when you hover over their DOM breadcrumbs
            Part of Bug 16532: Inspector should highlight nodes in page when
            hovering over nodes in Inspector's interface
    
            <http://bugs.webkit.org/show_bug.cgi?id=16532>
            <rdar://problem/5712896>
    
            Reviewed by Tim Hatcher.
    
            * page/inspector/DocumentPanel.js:
            (WebInspector.DocumentPanel): Add an mouseout event listener to the
            breadcrumbs element to clear the highlighted node.
            (WebInspector.DocumentPanel.updateBreadcrumbs): Change the mouseover
            event listener to highlight the node represented by the hovered
            breadcrumb.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/DocumentPanel.js
+++ third_party/WebKit/WebCore/page/inspector/DocumentPanel.js
@@ -65,6 +65,7 @@ WebInspector.DocumentPanel = function(resource, views)
 
     domView.crumbsElement = document.createElement("div");
     domView.crumbsElement.className = "crumbs";
+    domView.crumbsElement.addEventListener("mouseout", clearNodeHighlight.bind(domView.crumbsElement), false);
 
     domView.innerCrumbsElement = document.createElement("div");
     domView.crumbsElement.appendChild(domView.innerCrumbsElement);
@@ -266,6 +267,7 @@ WebInspector.DocumentPanel.prototype = {
 
         var mouseOverCrumbFunction = function(event) {
             panel.mouseOverCrumb = true;
+            InspectorController.highlightDOMNode(this.representedObject);
 
             if ("mouseOutTimeout" in panel) {
                 clearTimeout(panel.mouseOutTimeout);
@@ -304,7 +306,7 @@ WebInspector.DocumentPanel.prototype = {
             crumb.className = "crumb";
             crumb.representedObject = current;
             crumb.addEventListener("mousedown", selectCrumbFunction, false);
-            crumb.addEventListener("mouseover", mouseOverCrumbFunction, false);
+            crumb.addEventListener("mouseover", mouseOverCrumbFunction.bind(crumb), false);
             crumb.addEventListener("mouseout", mouseOutCrumbFunction, false);
 
             var crumbTitle;
