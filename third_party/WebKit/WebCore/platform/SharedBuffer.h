917601f647cb60f5d788a3bb21d8264794df12b2
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-20 04:51:43 +0000

    2008-03-19  Stephanie Lewis <slewis@apple.com>
            Rubber-stamped by Anders.
    
            Fix Windows Build
    
            * platform/SharedBuffer.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31170 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/SharedBuffer.h
+++ third_party/WebKit/WebCore/platform/SharedBuffer.h
@@ -31,9 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Forward.h>
 #include <wtf/Vector.h>
 
-#if PLATFORM(MAC)
+#if PLATFORM(CF)
 #include <wtf/RetainPtr.h>
+#endif
 
+#if PLATFORM(MAC)
 #ifdef __OBJC__
 @class NSData;
 #else
