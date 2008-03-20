7078c05d38e8e3029e75a5977746291c1b185539
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-20 06:36:26 +0000

    Avoid heap allocating the root scope chain node for eval and closure free functions
    Reviewed by Maciej
    
    Maciej suggested using an inline ScopeChainNode for functions that don't use eval
    or closures as they are unable to ever capture the scope chain.  This gives us a 2.4%
    win in sunspider, a 15% win in controlflow-recursive, and big (>5%) wins in a number
    of other tests.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31172 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/ExecState.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/ExecState.cpp
@@ -53,6 +53,7 @@ inline ExecState::ExecState(JSGlobalObject* globalObject)
     , m_arguments(0)
     , m_activation(0)
     , m_localStorage(&globalObject->localStorage())
+    , m_inlineScopeChainNode(0, 0)
     , m_variableObject(globalObject)
     , m_thisValue(globalObject)
     , m_iterationDepth(0)
@@ -73,6 +74,7 @@ inline ExecState::ExecState(JSGlobalObject* globalObject, JSObject* /*thisObject
     , m_arguments(0)
     , m_activation(0)
     , m_localStorage(&globalObject->localStorage())
+    , m_inlineScopeChainNode(0, 0)
     , m_variableObject(globalObject)
     , m_thisValue(globalObject)
     , m_iterationDepth(0)
@@ -98,6 +100,7 @@ inline ExecState::ExecState(JSGlobalObject* globalObject, JSObject* thisObject, 
     , m_activation(0)
     , m_localStorage(callingExec->m_localStorage)
     , m_scopeChain(scopeChain)
+    , m_inlineScopeChainNode(0, 0)
     , m_variableObject(variableObject)
     , m_thisValue(thisObject)
     , m_iterationDepth(0)
@@ -119,6 +122,7 @@ inline ExecState::ExecState(JSGlobalObject* globalObject, JSObject* thisObject,
     , m_function(func)
     , m_arguments(&args)
     , m_scopeChain(func->scope())
+    , m_inlineScopeChainNode(0, 0)
     , m_thisValue(thisObject)
     , m_iterationDepth(0)
     , m_switchDepth(0) 
@@ -130,7 +134,14 @@ inline ExecState::ExecState(JSGlobalObject* globalObject, JSObject* thisObject,
     m_activation = activation;
     m_localStorage = &activation->localStorage();
     m_variableObject = activation;
-    m_scopeChain.push(activation);
+    if (functionBodyNode->usesEval() || functionBodyNode->needsClosure())
+        m_scopeChain.push(activation);
+    else {
+        m_inlineScopeChainNode.object = activation;
+        // The ScopeChain will ref this node itself, so we don't need to worry about
+        // anything trying to delete our scopenode
+        m_scopeChain.push(&m_inlineScopeChainNode);
+    }
 }
 
 inline ExecState::~ExecState()
