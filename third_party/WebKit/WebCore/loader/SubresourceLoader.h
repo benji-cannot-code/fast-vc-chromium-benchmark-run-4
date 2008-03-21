3725988969bb38c35291045cdc7f28d6cd564acd
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-21 01:00:58 +0000

    2008-03-20  Anders Carlsson  <andersca@apple.com>
            Get rid of a couple of unnecessary class declarations.
            
            * loader/SubresourceLoader.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31201 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/SubresourceLoader.h
+++ third_party/WebKit/WebCore/loader/SubresourceLoader.h
@@ -34,12 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceLoader.h"
 #include <wtf/PassRefPtr.h>
  
-#ifndef __OBJC__
-class NSArray;
-class NSDictionary;
-class NSMutableURLRequest;
-#endif
- 
 namespace WebCore {
 
     class FormData;
