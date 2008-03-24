3345348ec34e3dafeb36533204b14de63fecf1b7
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-24 20:43:13 +0000

    2008-03-24  Brady Eidson  <beidson@apple.com>
            Reviewed by Darin
    
            Some style cleanup and add a logging channel for upcoming work
    
            * platform/Logging.cpp:
            * platform/Logging.h:
    
            * platform/MIMETypeRegistry.cpp:
            (WebCore::initializeSupportedImageMIMETypes):
            (WebCore::initializeSupportedNonImageMimeTypes):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31251 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Logging.h
+++ third_party/WebKit/WebCore/platform/Logging.h
@@ -55,6 +55,7 @@ namespace WebCore {
     extern WTFLogChannel LogStorageAPI;
     extern WTFLogChannel LogMedia;
     extern WTFLogChannel LogPlugin;
+    extern WTFLogChannel LogArchives;
 
     void InitializeLoggingChannelsIfNecessary();
 }
