db7d259ae38973d138c5940d3e8dc123357c79b2
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-25 19:12:33 +0000

    2008-03-25  Darin Adler  <darin@apple.com>
            Reviewed by Anders.
    
            - avoid converting UString to String and then later to AtomicString
    
            1% speedup on Acid3 test 26
    
            This is inefficient for the case where there's already an AtomicString.
            By first creating and destroying a String we waste time.
    
            * bindings/scripts/CodeGeneratorJS.pm: Use const UString& for string
            arguments rather than using String; this uses the object as it's produced
            from toString rather than creating a new temporary object that might not
            be of the right type. The type conversion happens at the callsite instead.
            Also eliminate the ability to use the type AtomicString from IDL.
    
            * dom/KeyboardEvent.idl: AtomicString -> DOMString.
            * dom/MouseEvent.idl: Ditto.
            * dom/MutationEvent.idl: Ditto.
            * dom/UIEvent.idl: Ditto.
            * svg/SVGStyleElement.idl: Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31286 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -1350,9 +1350,8 @@ sub GetNativeTypeFromSignature
 }
 
 my %nativeType = (
-    "AtomicString" => "AtomicString",
     "CompareHow" => "Range::CompareHow",
-    "DOMString" => "String",
+    "DOMString" => "const UString&",
     "EventTarget" => "EventTargetNode*",
     "SVGLength" => "SVGLength",
     "SVGMatrix" => "AffineTransform",
@@ -1380,7 +1379,6 @@ sub GetNativeType
 }
 
 my %typeCanFailConversion = (
-    "AtomicString" => 0,
     "Attr" => 1,
     "CompareHow" => 0,
     "DOMString" => 0,
@@ -1444,7 +1442,6 @@ sub JSValueToNative
     return "static_cast<Range::CompareHow>($value->toInt32(exec))" if $type eq "CompareHow";
     return "static_cast<SVGPaint::SVGPaintType>($value->toInt32(exec))" if $type eq "SVGPaintType";
 
-    return "$value->toString(exec)" if $type eq "AtomicString";
     if ($type eq "DOMString") {
         return "valueToStringWithNullCheck(exec, $value)" if $signature->extendedAttributes->{"ConvertNullToNullString"};
         return "valueToStringWithUndefinedOrNullCheck(exec, $value)" if $signature->extendedAttributes->{"ConvertUndefinedOrNullToNullString"};
