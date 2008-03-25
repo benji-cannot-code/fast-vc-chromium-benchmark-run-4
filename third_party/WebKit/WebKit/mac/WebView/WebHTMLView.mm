4641e1ca2ff3885825b6830fc582deca5effd01d
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-25 22:17:45 +0000

    Reviewed by Darin Adler.
            - fix http://bugs.webkit.org/show_bug.cgi?id=17933
              Reopen All Windows From Last Session causes crash
    
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView layoutToMinimumPageWidth:maximumPageWidth:adjustingViewSize:]): Added null
            check.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31292 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
@@ -2703,6 +2703,11 @@ static void _updateFocusedAndActiveStateTimerCallback(CFRunLoopTimerRef timer, v
     LOG(View, "%@ doing layout", self);
 
     Frame* coreFrame = core([self _frame]);
+    if (!coreFrame) {
+        _private->needsLayout = NO;
+        return;
+    }
+
     if (minPageWidth > 0.0)
         coreFrame->forceLayoutWithPageWidthRange(minPageWidth, maxPageWidth, adjustViewSize);
     else {
