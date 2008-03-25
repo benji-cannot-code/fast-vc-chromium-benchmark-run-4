a8863d462fb1d795cb979ab5439dd60ce45ed736
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-25 21:57:00 +0000

    2008-03-25  Brady Eidson  <beidson@apple.com>
            Reviewed by Jon Honeycutt's rubberstamp
    
            Fix a leak with the new WebArchive setup
    
            * WebView/WebArchive.mm:
            (-[WebArchivePrivate setCoreArchive:]): Deref() the old WebArchive
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebArchive.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebArchive.mm
@@ -101,6 +101,7 @@ static NSString * const WebSubframeArchivesKey = @"WebSubframeArchives";
 {
     ASSERT(coreArchive);
     ASSERT(newCoreArchive);
+    coreArchive->deref();
     coreArchive = newCoreArchive.releaseRef();
 }
 
