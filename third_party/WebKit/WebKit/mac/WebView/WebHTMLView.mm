2d437a7f4359bbbc10b7459456f038f1b0a69364
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-25 04:35:30 +0000

    Bug 18030: REGRESSION(r31236): Space bar fails to scroll down page <http://bugs.webkit.org/show_bug.cgi?id=18030>
    Reviewed by Mark Rowe
    
    Rollout keyDown changes from r31236 -- fix for keyDown behaviour is
    tracked by Bug 18057: keyDown incorrectly propagates up the frame tree
    <http://bugs.webkit.org/show_bug.cgi?id=18057>
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
@@ -3534,8 +3534,7 @@ noPromisedData:
 
     BOOL completionPopupWasOpen = _private->compController && [_private->compController popupWindowIsOpen];
     Frame* coreFrame = core([self _frame]);
-    if (!eventWasSentToWebCore && coreFrame) {
-        coreFrame->eventHandler()->keyEvent(event);
+    if (!eventWasSentToWebCore && coreFrame && coreFrame->eventHandler()->keyEvent(event)) {
         // WebCore processed a key event, bail on any preexisting complete: UI
         if (completionPopupWasOpen)
             [_private->compController endRevertingChange:YES moveLeft:NO];
