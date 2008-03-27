5ed06660fe913d033b24909c66b019fbdf1a41e8
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-27 22:14:08 +0000

    Fix compilation against Qt 4.4 and one missing SVG_FONTS #ifdef.
    git-svn-id: svn://svn.chromium.org/blink/trunk@31376 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSSVGElementWrapperFactory.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSSVGElementWrapperFactory.cpp
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSSVGElementWrapperFactory.h"
 
 #include "JSSVGAElement.h"
+#if ENABLE(SVG_FONTS)
 #include "JSSVGAltGlyphElement.h"
+#endif
 #include "JSSVGAnimateColorElement.h"
 #include "JSSVGAnimateElement.h"
 #include "JSSVGAnimateTransformElement.h"
