b7a81135abad39b0749614fa61c25cdf3b842f3e
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-27 01:43:54 +0000

    Turn on SVG animation on Windows
    JavaScriptCore:
    
            Force JSC headers to be copied by touching a file
    
            * kjs/array_instance.cpp:
            (KJS::ArrayInstance::getPropertyNames):
    
    WebCore:
    
            Turn on SVG animation on Windows
    
            Also touched some files to force things to rebuild/regenerate.
    
            * WebCore.vcproj/WebCore.vcproj: Added ENABLE_SVG_ANIMATION.
            * WebCore.vcproj/build-generated-files.sh: Ditto.
            * bindings/scripts/CodeGenerator.pm: Touched.
            * config.h: Touched.
            * svg/svgtags.in: Touched.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/config.h
+++ third_party/WebKit/WebCore/config.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WINVER 0x0500
 #endif
 
-// If we don't define these, they get defined in windef.h. 
+// If we don't define these, they get defined in windef.h.
 // We want to use std::min and std::max.
 #ifndef max
 #define max max
