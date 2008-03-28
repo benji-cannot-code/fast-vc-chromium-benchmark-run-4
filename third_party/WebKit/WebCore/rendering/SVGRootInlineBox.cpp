cc7036d1ca47ccb101489dc0c9303c1841ff8063
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-28 03:37:09 +0000

    2008-03-27  Maciej Stachowiak  <mjs@apple.com>
            Rubber stamped by Oliver.
    
            - re-fix Acid3, some code was incorrectly moved when fixing the Qt build
    
            * rendering/SVGRootInlineBox.cpp:
            (WebCore::SVGRootInlineBox::buildLayoutInformationForTextBox):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31381 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/SVGRootInlineBox.cpp
+++ third_party/WebKit/WebCore/rendering/SVGRootInlineBox.cpp
@@ -1307,7 +1307,6 @@ void SVGRootInlineBox::buildLayoutInformationForTextBox(SVGCharacterLayoutInfo& 
         }
 
         double kerning = 0.0;
-        lastGlyph.isValid = false;
 #if ENABLE(SVG_FONTS)
         SVGFontElement* svgFont = 0;
         if (style->font().isSVGFont())
@@ -1323,7 +1322,8 @@ void SVGRootInlineBox::buildLayoutInformationForTextBox(SVGCharacterLayoutInfo& 
             lastGlyph.unicode = unicodeStr;
             lastGlyph.glyphName = glyphName;
             lastGlyph.isValid = true;
-        }
+        } else
+            lastGlyph.isValid = false;
 #endif
 
         svgChar.x -= (float)kerning;
