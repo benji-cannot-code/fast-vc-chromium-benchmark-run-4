2c21cb5b0a2238e7f40b7ee45b6723c4c12475fd
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-31 08:50:23 +0000

    Fix the Qt build.
    ResourceHandleInternal.h references ResourceHandle::fireFailure, so include
    ResourceHandle.h.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31457 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceHandleInternal.h
+++ third_party/WebKit/WebCore/platform/network/ResourceHandleInternal.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ResourceHandleInternal_h
 #define ResourceHandleInternal_h
 
+#include "ResourceHandle.h"
 #include "ResourceRequest.h"
 #include "AuthenticationChallenge.h"
 #include "Timer.h"
