adc854f420d3e32dbe5f0c42695ca8e78f7f75bf
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-31 06:13:26 +0000

    Reviewed by Oliver.
            Make AMatthews life easier -- fix the SVG_FILTERS build
    
            Build fix only, no tests.
    
            * svg/SVGComponentTransferFunctionElement.cpp:
            (WebCore::SVGComponentTransferFunctionElement::SVGComponentTransferFunctionElement):
            * svg/SVGFEColorMatrixElement.cpp:
            (WebCore::SVGFEColorMatrixElement::SVGFEColorMatrixElement):
            * svg/SVGFEDiffuseLightingElement.cpp:
            * svg/SVGFEGaussianBlurElement.cpp:
            * svg/SVGFEImageElement.cpp:
            (WebCore::SVGFEImageElement::SVGFEImageElement):
            * svg/SVGFESpecularLightingElement.cpp:
            * svg/SVGFETurbulenceElement.cpp:
            * svg/SVGFilterElement.cpp:
            * svg/SVGFilterPrimitiveStandardAttributes.cpp:
            * svg/graphics/filters/SVGFEBlend.cpp:
            * svg/graphics/filters/SVGFEComponentTransfer.cpp:
            * svg/graphics/filters/SVGFEComposite.cpp:
            * svg/graphics/filters/SVGFEDiffuseLighting.cpp:
            * svg/graphics/filters/SVGFEGaussianBlur.cpp:
            * svg/graphics/filters/SVGFEImage.cpp:
            * svg/graphics/filters/SVGFEMerge.cpp:
            * svg/graphics/filters/SVGFEMorphology.cpp:
            * svg/graphics/filters/SVGFEOffset.cpp:
            * svg/graphics/filters/SVGFESpecularLighting.cpp:
            * svg/graphics/filters/SVGFETurbulence.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31451 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/graphics/filters/SVGFEComposite.cpp
+++ third_party/WebKit/WebCore/svg/graphics/filters/SVGFEComposite.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(SVG) && ENABLE(SVG_FILTERS)
 #include "SVGFEComposite.h"
+#include "TextStream.h"
 
 namespace WebCore {
 
--- third_party/WebKit/WebCore/svg/graphics/filters/SVGFEDiffuseLighting.cpp
+++ third_party/WebKit/WebCore/svg/graphics/filters/SVGFEDiffuseLighting.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(SVG) && ENABLE(SVG_FILTERS)
 #include "SVGLightSource.h"
 #include "SVGFEDiffuseLighting.h"
+#include "TextStream.h"
 
 namespace WebCore {
 
