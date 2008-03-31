8398b1a1088c2e089b47cfebf4583e6d86d09fcf
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-03-31 17:59:50 +0000

    WebCore:
    2008-03-31  Brady Eidson  <beidson@apple.com>
    
            Reviewed by Darin and Mitz's rubber stamp
    
            Remove dataForArchivedSelection(WebCore::Frame*) from the EditorClient - only usage is now directly in WebCore
    
            * page/EditorClient.h:
            * svg/graphics/SVGImageEmptyClients.h:
    
            * platform/mac/PasteboardMac.mm:
            (WebCore::Pasteboard::writeSelection): Make the archive and get the data directly
    
    WebKit/mac:
    
    2008-03-31  Brady Eidson  <beidson@apple.com>
    
            Reviewed by Darin and Mitz's rubber stamp
    
            Remove dataForArchivedSelection(WebCore::Frame*) from the EditorClient - only usage is now directly in WebCore
    
            * WebCoreSupport/WebEditorClient.mm:
            * WebCoreSupport/WebEditorClient.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31468 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/PasteboardMac.mm
+++ third_party/WebKit/WebCore/platform/mac/PasteboardMac.mm
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "HitTestResult.h"
 #import "Image.h"
 #import "KURL.h"
+#import "LegacyWebArchive.h"
 #import "LoaderNSURLExtras.h"
 #import "MIMETypeRegistry.h"
 #import "RenderImage.h"
@@ -160,7 +161,9 @@ void Pasteboard::writeSelection(NSPasteboard* pasteboard, Range* selectedRange, 
     
     // Put HTML on the pasteboard.
     if ([types containsObject:WebArchivePboardType]) {
-        [pasteboard setData:frame->editor()->client()->dataForArchivedSelection(frame) forType:WebArchivePboardType];
+        RefPtr<LegacyWebArchive> archive = LegacyWebArchive::createFromSelection(frame);
+        RetainPtr<CFDataRef> data = archive ? archive->rawDataRepresentation() : 0;
+        [pasteboard setData:(NSData *)data.get() forType:WebArchivePboardType];
     }
     
     // Put the attributed string on the pasteboard (RTF/RTFD format).
