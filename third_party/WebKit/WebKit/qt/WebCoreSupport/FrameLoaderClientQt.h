dfb9fdafd79712eaa392ed3ad6d1bb9e740c0d09
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-01 08:47:04 +0000

    Holger Hans Peter Freyther <holger.freyther@trolltech.com>
    * Get the RefCounting of WebCore::Frame right and make sure that both QWebFrame
      and WebCore::Frame get destroyed if we leave site with subframes and on the
      end of the application.
    * Use adoptRef in FrameLoaderClientQt::createFrame to get the refs right for subframes. We
      do not want to add an extra reference. Without this we are leaking subframes.
    * Assume that the lifetime of a Frame and FrameLoader are the same, when the Frame is gone
      we want to destroy the QWebFrame (e.g. on a page with subframes). Add delete m_webFrame
      in the frameLoaderDestroyed method to do that.
    * If we happen to delete the QWebFrame before the FrameLoaderClientQt we set m_webFrame to
      zero in the FrameLoaderClientQt to avoid bad things.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31521 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006 Zack Rusin <zack@kde.org>
  * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2007-2008 Trolltech ASA
  *
  * All rights reserved.
  *
@@ -55,6 +56,7 @@ namespace WebCore {
     class FrameLoaderClientQt : public QObject, public FrameLoaderClient {
         Q_OBJECT
 
+        friend class ::QWebFrame;
         void callPolicyFunction(FramePolicyFunction function, PolicyAction action);
     private slots:
         void slotCallPolicyFunction(int);
