e2b63e014875dbfb2f7723e0eea927237e296121
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-02 13:49:12 +0000

    Fix compilation on case-sensitive file systems.
    git-svn-id: svn://svn.chromium.org/blink/trunk@31567 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGGradientElement.h
+++ third_party/WebKit/WebCore/svg/SVGGradientElement.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGExternalResourcesRequired.h"
 #include "SVGPaintServerGradient.h"
 #include "SVGStyledElement.h"
-#include "SVGTransformlist.h"
+#include "SVGTransformList.h"
 #include "SVGURIReference.h"
 
 namespace WebCore {
