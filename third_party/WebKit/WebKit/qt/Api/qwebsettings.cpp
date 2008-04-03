2705f0b0fabfaf64ce20af8ad2be862884a131d9
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-03 20:17:59 +0000

    2008-04-03  Holger Hans Peter Freyther  <holger.freyther@trolltech.com>
            Reviewed by Simon.
    
            * Change IconDatabase::iconForPageURL to return more information. E.g. if
            an image has been found, or if the loading of an image has been scheduled.
            * Update FrameLoader to use another method to trigger reading from disk
            * Update the QWebFrame and QWebSettings
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebsettings.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebsettings.cpp
@@ -316,8 +316,9 @@ void QWebSettings::clearIconDatabase()
 */
 QPixmap QWebSettings::iconForUrl(const QUrl &url)
 {
-    WebCore::Image* image = WebCore::iconDatabase()->iconForPageURL(WebCore::KURL(url).string(),
-                                WebCore::IntSize(16, 16));
+    WebCore::Image* image = 0;
+    WebCore::iconDatabase()->iconForPageURL(WebCore::KURL(url).string(),
+                                            WebCore::IntSize(16, 16), &image);
     if (!image) {
         return QPixmap();
     }
