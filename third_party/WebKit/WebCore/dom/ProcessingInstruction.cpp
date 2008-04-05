44660a376b6773f0978fb3280c283399021697a8
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-05 20:43:48 +0000

    Reviewed by Eric.
            http://bugs.webkit.org/show_bug.cgi?id=17129
            Incorrect style resolution in CSS styled SVG, with multiple style sheets
            Fix the style resolution on the svg by ensuring xml-stylesheet pseudo
            attributes title and alternate are transferred to the stylesheet
            created from the ProcessingInstruction.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31664 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/ProcessingInstruction.cpp
+++ third_party/WebKit/WebCore/dom/ProcessingInstruction.cpp
@@ -39,6 +39,7 @@ ProcessingInstruction::ProcessingInstruction(Document* doc)
     : ContainerNode(doc)
     , m_cachedSheet(0)
     , m_loading(false)
+    , m_alternate(false)
 #if ENABLE(XSLT)
     , m_isXSL(false)
 #endif
@@ -51,6 +52,7 @@ ProcessingInstruction::ProcessingInstruction(Document* doc, const String& target
     , m_data(data)
     , m_cachedSheet(0)
     , m_loading(false)
+    , m_alternate(false)
 #if ENABLE(XSLT)
     , m_isXSL(false)
 #endif
@@ -135,6 +137,9 @@ bool ProcessingInstruction::checkStyleSheet()
             return true;
 
         String href = attrs.get("href");
+        String alternate = attrs.get("alternate");
+        m_alternate = alternate == "yes";
+        m_title = attrs.get("title");
 
         if (href.length() > 1) {
             if (href[0] == '#') {
@@ -207,6 +212,8 @@ void ProcessingInstruction::setCSSStyleSheet(const String& url, const String& ch
 #endif
     m_sheet = new CSSStyleSheet(this, url, charset);
     parseStyleSheet(sheet->sheetText());
+    m_sheet->setTitle(m_title);
+    m_sheet->setDisabled(m_alternate);
 }
 
 #if ENABLE(XSLT)
@@ -244,6 +251,8 @@ void ProcessingInstruction::setCSSStyleSheet(CSSStyleSheet* sheet)
     ASSERT(!m_cachedSheet);
     ASSERT(!m_loading);
     m_sheet = sheet;
+    m_sheet->setTitle(m_title);
+    m_sheet->setDisabled(m_alternate);
 }
 
 bool ProcessingInstruction::offsetInCharacters() const
