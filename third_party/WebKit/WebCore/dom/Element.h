6e2c93e00caefe2e6416e9ef5177fce55823b084
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-09 18:36:49 +0000

    2008-04-09  Antti Koivisto  <antti@apple.com>
            Reviewed by Mitz.
    
            Avoid unnecessary calls to virtual updateStyleAttributeIfNeeded() method.
            - do the the checking in call sites instead of inside the function
            - rename to updateStyleAttribute()
            - use namedAttrMap directly (for getting id attribute) in insertedIntoDocument/removedFromDocument 
              to avoid calling attributes() which may trigger style attribute update.
            
            * dom/Element.cpp:
            (WebCore::Element::attributes):
            (WebCore::Element::getAttribute):
            (WebCore::Element::hasAttributes):
            (WebCore::Element::insertedIntoDocument):
            (WebCore::Element::removedFromDocument):
            * dom/Element.h:
            (WebCore::Element::updateStyleAttribute):
            * dom/NamedAttrMap.cpp:
            (WebCore::NamedAttrMap::addAttribute):
            * dom/StyledElement.cpp:
            (WebCore::StyledElement::updateStyleAttribute):
            * dom/StyledElement.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Element.h
+++ third_party/WebKit/WebCore/dom/Element.h
@@ -201,7 +201,7 @@ private:
 
     virtual void createAttributeMap() const;
 
-    virtual void updateStyleAttributeIfNeeded() const {}
+    virtual void updateStyleAttribute() const {}
     
     void updateFocusAppearanceSoonAfterAttach();
     void cancelFocusAppearanceUpdate();
--- third_party/WebKit/WebCore/dom/StyledElement.h
+++ third_party/WebKit/WebCore/dom/StyledElement.h
@@ -66,7 +66,7 @@ public:
     void createInlineStyleDecl();
     void destroyInlineStyleDecl();
     void invalidateStyleAttribute();
-    virtual void updateStyleAttributeIfNeeded() const;
+    virtual void updateStyleAttribute() const;
     
     const ClassNames& classNames() const { ASSERT(hasClass()); ASSERT(mappedAttributes()); return mappedAttributes()->classNames(); }
 
