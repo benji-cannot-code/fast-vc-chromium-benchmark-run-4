4fcee897472d35e707785f97c42d332dc65be6eb
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-09 15:18:47 +0000

    Build fix
            * wtf/AlwaysInline.h: Make sure to #include Platform.h before using
            the macros it defines.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/AlwaysInline.h
+++ third_party/WebKit/JavaScriptCore/wtf/AlwaysInline.h
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
+#include "Platform.h"
+
 #ifndef ALWAYS_INLINE
 #if COMPILER(GCC) && defined(NDEBUG)
 #define ALWAYS_INLINE inline __attribute__ ((__always_inline__))
