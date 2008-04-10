12bee0291958016b35296d9a5a100196ea8cd0b3
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-10 17:42:13 +0000

    2008-04-10  Brady Eidson  <beidson@apple.com>
            Reviewed by Mitz Pettel
    
            DOM Storage layout test cleanup
    
            * storage/domstorage/sessionstorage/iframe-events.html: Check for the layoutTestController in a manner that
              won't confuse Firefox
            * storage/domstorage/sessionstorage/resources/clearSessionStorage.js: Don't mutate while iterating!  Clea
              the sessionStorage properly
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/storage/domstorage/sessionstorage/resources/clearSessionStorage.js
+++ third_party/WebKit/LayoutTests/storage/domstorage/sessionstorage/resources/clearSessionStorage.js
@@ -1,9 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function clearSessionStorage()
 {
-    var i = 0;
-    for (;i < sessionStorage.length; ++i)
-        sessionStorage.removeItem(sessionStorage.key(i));
+    var keys = new Array();
+    for (key in sessionStorage)
+        keys.push(key);
+                
+    for (key in keys)
+        sessionStorage.removeItem(keys[key]);
 }
 
 if (window.sessionStorage)
