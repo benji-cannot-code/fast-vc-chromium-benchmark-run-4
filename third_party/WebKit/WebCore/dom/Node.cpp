d9972a4ab68ad5aec04e0e02bdd2bcde171afdaa
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-10 00:55:51 +0000

    2008-04-09  Antti Koivisto  <antti@apple.com>
            Reviewed by Mitz.
    
            Remove a less-than-critical bit from Node and replace it with a hash. Those bits are valuable!
    
            Also moved setting of the guard to dispatchSimulatedClick for better consistency. 
            No one else calls dispatchSimulatedMouseEvent() currently and this is really a click() specific behavior.
    
            * dom/EventTargetNode.cpp:
            (WebCore::EventTargetNode::dispatchSimulatedMouseEvent):
            (WebCore::EventTargetNode::dispatchSimulatedClick):
            * dom/Node.cpp:
            (WebCore::Node::Node):
            * dom/Node.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31767 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Node.cpp
+++ third_party/WebKit/WebCore/dom/Node.cpp
@@ -138,7 +138,6 @@ Node::Node(Document *doc)
       m_hovered(false),
       m_inActiveChain(false),
       m_inDetach(false),
-      m_dispatchingSimulatedEvent(false),
       m_inSubtreeMark(false)
 {
 #ifndef NDEBUG
