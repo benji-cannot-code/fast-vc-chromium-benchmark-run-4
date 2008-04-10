2108e1305636a05ee1f2b9ab0e10566727656e54
adachan@apple.com <adachan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-10 22:30:49 +0000

    Added WebCoreStatistics that provides stats on Javascript objects         and IconDatabase.
            Changed WebCache::statistics() to additionally return xsl stylesheets data
            and data on live sizes and decoded sizes.
    
            Reviewed by Steve.
    
            * ForEachCoClass.h:
            * Interfaces/IWebCoreStatistics.idl: Added.
            * Interfaces/WebKit.idl:
            * WebCache.cpp:
            (WebCache::statistics):
            * WebCoreStatistics.cpp: Added.
            (WebCoreStatistics::WebCoreStatistics):
            (WebCoreStatistics::~WebCoreStatistics):
            (WebCoreStatistics::createInstance):
            (WebCoreStatistics::QueryInterface):
            (WebCoreStatistics::AddRef):
            (WebCoreStatistics::Release):
            (WebCoreStatistics::javaScriptObjectsCount):
            (WebCoreStatistics::javaScriptGlobalObjectsCount):
            (WebCoreStatistics::javaScriptProtectedObjectsCount):
            (WebCoreStatistics::javaScriptProtectedGlobalObjectsCount):
            (WebCoreStatistics::iconPageURLMappingCount):
            (WebCoreStatistics::iconRetainedPageURLCount):
            (WebCoreStatistics::iconRecordCount):
            (WebCoreStatistics::iconsWithDataCount):
            * WebCoreStatistics.h: Added.
            * WebKit.vcproj/WebKit.vcproj:
            * WebKitClassFactory.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31786 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/ForEachCoClass.h
+++ third_party/WebKit/WebKit/win/ForEachCoClass.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     macro(CFDictionaryPropertyBag) \
     macro(WebArchive) \
     macro(WebCache) \
+    macro(WebCoreStatistics) \
     macro(WebDatabaseManager) \
     macro(WebDownload) \
     macro(WebError) \
