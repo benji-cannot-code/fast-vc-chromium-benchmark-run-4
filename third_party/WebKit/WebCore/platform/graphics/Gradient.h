6a1f5b714cd69aa1902f56457bf5d89056ca7458
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-10 19:35:45 +0000

    2008-04-10  David Hyatt  <hyatt@apple.com>
            Gradient improvements.  Support for adding color stops by Color and not just by String.  Add the ability to mark
            stops as already being sorted.
    
            Reviewed by aroben
    
            * platform/graphics/Gradient.cpp:
            (WebCore::Gradient::addColorStop):
            * platform/graphics/Gradient.h:
            (WebCore::Gradient::setStopsSorted):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31781 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/Gradient.h
+++ third_party/WebKit/WebCore/platform/graphics/Gradient.h
@@ -47,6 +47,7 @@ typedef void* PlatformGradient;
 
 namespace WebCore {
 
+    class Color;
     class String;
 
     class Gradient : public Noncopyable {
@@ -55,7 +56,8 @@ namespace WebCore {
         Gradient(const FloatPoint& p0, float r0, const FloatPoint& p1, float r1);
         ~Gradient();
 
-        void addColorStop(float, const String& color);
+        void addColorStop(float, const String&);
+        void addColorStop(float, const Color&);
 
         void getColor(float value, float* r, float* g, float* b, float* a) const;
 
@@ -71,6 +73,8 @@ namespace WebCore {
             ColorStop() : stop(0), red(0), green(0), blue(0), alpha(0) { }
             ColorStop(float s, float r, float g, float b, float a) : stop(s), red(r), green(g), blue(b), alpha(a) { }
         };
+
+        void setStopsSorted(bool s) { m_stopsSorted = s; }
 
     private:
         void platformInit() { m_gradient = 0; }
