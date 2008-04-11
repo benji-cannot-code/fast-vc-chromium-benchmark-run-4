228027a46ebeaa5b221c9b386dfad0101f0e1223
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-11 07:41:38 +0000

    2008-04-11  Antti Koivisto  <antti@apple.com>
            Try to fix Qt build by including some headers.
    
            * ChangeLog:
            * platform/graphics/UnitBezier.h:
            * svg/SVGAnimationElement.cpp:
            * svg/animation/SMILTime.h:
            * svg/animation/SVGSMILElement.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31808 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/UnitBezier.h
+++ third_party/WebKit/WebCore/platform/graphics/UnitBezier.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UnitBezier_h
 #define UnitBezier_h
 
+#include <math.h>
+
 namespace WebCore {
 
     struct UnitBezier {
--- third_party/WebKit/WebCore/svg/animation/SVGSMILElement.cpp
+++ third_party/WebKit/WebCore/svg/animation/SVGSMILElement.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGURIReference.h"
 #include "SMILTimeContainer.h"
 #include "XLinkNames.h"
+#include <math.h>
 #include <wtf/MathExtras.h>
 #include <wtf/Vector.h>
 
