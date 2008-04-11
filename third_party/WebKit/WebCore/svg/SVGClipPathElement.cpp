0d58db26ee9dba888e391a0ad41d7ec4075ffe7a
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-11 07:00:15 +0000

    Reviewed by Eric.
            http://bugs.webkit.org/show_bug.cgi?id=18340
            Elements with display="none" in a <clipPath> still contribute to clipping path
    
            Skip elements in clip-path container that have display=none specified.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31805 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGClipPathElement.cpp
+++ third_party/WebKit/WebCore/svg/SVGClipPathElement.cpp
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
     Copyright (C) 2004, 2005, 2007, 2008 Nikolas Zimmermann <zimmermann@kde.org>
-                  2004, 2005, 2006, 2007 Rob Buis <buis@kde.org>
+                  2004, 2005, 2006, 2007, 2008 Rob Buis <buis@kde.org>
 
     This file is part of the KDE project
 
@@ -106,11 +106,13 @@ SVGResource* SVGClipPathElement::canvasResource()
         if (n->isSVGElement() && static_cast<SVGElement*>(n)->isStyledTransformable()) {
             SVGStyledTransformableElement* styled = static_cast<SVGStyledTransformableElement*>(n);
             RenderStyle* pathStyle = document()->styleSelector()->styleForElement(styled, clipPathStyle);
-            Path pathData = styled->toClipPath();
-            // FIXME: How do we know the element has done a layout?
-            pathData.transform(styled->animatedLocalTransform());
-            if (!pathData.isEmpty())
-                m_clipper->addClipData(pathData, pathStyle->svgStyle()->clipRule(), bbox);
+            if (pathStyle->display() != NONE) {
+                Path pathData = styled->toClipPath();
+                // FIXME: How do we know the element has done a layout?
+                pathData.transform(styled->animatedLocalTransform());
+                if (!pathData.isEmpty())
+                    m_clipper->addClipData(pathData, pathStyle->svgStyle()->clipRule(), bbox);
+            }
             pathStyle->deref(document()->renderArena());
         }
     }
