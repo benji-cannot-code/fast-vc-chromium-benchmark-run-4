62bdea9a1976a58da32ec548dc5a54020a6c480c
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-16 18:41:54 +0000

    JavaScriptCore:
    2008-04-16  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Darin Adler.
    
            - Remove kjs_ prefix from strtod, dtoa, and freedtoa and put it
              in the KJS namespace.
            - Make strtod, dtoa, and freedtoa c++ functions instead of extern "C".
            - Remove mode switching from dtoa.  ~2% improvement on test 26. 
            - Removes all unnecessary #defines from dtoa code.
    
            * JavaScriptCore.exp:
            * kjs/dtoa.cpp:
            (KJS::ulp):
            (KJS::b2d):
            (KJS::d2b):
            (KJS::ratio):
            (KJS::):
            (KJS::strtod):
            (KJS::freedtoa):
            (KJS::dtoa):
            * kjs/dtoa.h:
            * kjs/function.cpp:
            (KJS::parseInt):
            * kjs/lexer.cpp:
            (KJS::Lexer::lex):
            * kjs/number_object.cpp:
            (KJS::integer_part_noexp):
            (KJS::numberProtoFuncToExponential):
            * kjs/ustring.cpp:
            (KJS::UString::from):
            (KJS::UString::toDouble):
    
    WebCore:
    
    2008-04-16  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Darin Adler.
    
            Rename kjs_strtod to KJS::strtod.
    
            * platform/text/String.cpp:
            (WebCore::charactersToDouble):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/dtoa.h
+++ third_party/WebKit/JavaScriptCore/kjs/dtoa.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- *  Copyright (C) 2003 Apple Computer, Inc.
+ *  Copyright (C) 2003, 2008 Apple Inc. All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -27,12 +27,13 @@ namespace WTF {
 }
 
 namespace KJS {
-    extern WTF::Mutex* s_dtoaP5Mutex;
-}
 
-extern "C" double kjs_strtod(const char* s00, char** se);
-extern "C" char* kjs_dtoa(double d, int mode, int ndigits,
-                          int* decpt, int* sign, char** rve);
-extern "C" void kjs_freedtoa(char* s);
+    extern WTF::Mutex* s_dtoaP5Mutex;
+
+    double strtod(const char* s00, char** se);
+    char* dtoa(double d, int ndigits, int* decpt, int* sign, char** rve);
+    void freedtoa(char* s);
+
+} // namespace KJS
 
 #endif /* KJS_dtoa_h */
