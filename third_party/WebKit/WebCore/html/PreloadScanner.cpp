944f8b1ea62853a14ba9de6cc69202c9991a9ccb
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-16 21:41:12 +0000

    WebCore:
    2008-04-16  Antti Koivisto  <antti@apple.com>
    
            Reviewed by Alexey.
            
            Fix <rdar://problem/5863728>
            REGRESSION: Garbage character shown in temperatures in the Weather widget (encoding issue with degree symbol)
            
            Parse the encoding attribute in PreloadScanner.
    
            Test: fast/encoding/script-preload-encoding.html
    
            * html/PreloadScanner.cpp:
            (WebCore::PreloadScanner::processAttribute):
            (WebCore::PreloadScanner::emitTag):
            (WebCore::PreloadScanner::emitCSSRule):
            * html/PreloadScanner.h:
            * loader/DocLoader.cpp:
            (WebCore::DocLoader::preload):
            * loader/DocLoader.h:
    
    LayoutTests:
    
    2008-04-16  Antti Koivisto  <antti@apple.com>
    
            Reviewed by Alexey.
            
            Test for <rdar://problem/5863728>
            REGRESSION: Garbage character shown in temperatures in the Weather widget (encoding issue with degree symbol)
    
            * fast/encoding/resources/utf-8.css: Added.
            * fast/encoding/resources/utf-8.js: Added.
            * fast/encoding/preload-encoding-expected.txt: Added.
            * fast/encoding/preload-encoding.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@31964 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/PreloadScanner.cpp
+++ third_party/WebKit/WebCore/html/PreloadScanner.cpp
@@ -111,7 +111,8 @@ void PreloadScanner::reset()
     m_attributeValue.clear();
     m_lastStartTag = AtomicString();
     
-    m_urlToLoad = "";
+    m_urlToLoad = String();
+    m_charset = String();
     m_linkIsStyleSheet = false;
     m_lastCharacterIndex = 0;
     clearLastCharacters();
@@ -682,6 +683,8 @@ void PreloadScanner::processAttribute()
     if (tag == scriptTag || tag == imgTag) {
         if (attribute == srcAttr && m_urlToLoad.isEmpty())
             m_urlToLoad = parseURL(value);
+        else if (attribute == charsetAttr)
+            m_charset = value;
     } else if (tag == linkTag) {
         if (attribute == hrefAttr && m_urlToLoad.isEmpty())
             m_urlToLoad = parseURL(value);
@@ -691,7 +694,8 @@ void PreloadScanner::processAttribute()
             bool icon = false;
             HTMLLinkElement::tokenizeRelAttribute(value, styleSheet, alternate, icon);
             m_linkIsStyleSheet = styleSheet && !alternate && !icon;
-        }
+        } else if (attribute == charsetAttr)
+            m_charset = value;
     }
 }
     
@@ -810,13 +814,14 @@ void PreloadScanner::emitTag()
     }
     
     if (tag == scriptTag)
-        m_document->docLoader()->preload(CachedResource::Script, m_urlToLoad);
+        m_document->docLoader()->preload(CachedResource::Script, m_urlToLoad, m_charset);
     else if (tag == imgTag) 
-        m_document->docLoader()->preload(CachedResource::ImageResource, m_urlToLoad);
+        m_document->docLoader()->preload(CachedResource::ImageResource, m_urlToLoad, String());
     else if (tag == linkTag && m_linkIsStyleSheet) 
-        m_document->docLoader()->preload(CachedResource::CSSStyleSheet, m_urlToLoad);
+        m_document->docLoader()->preload(CachedResource::CSSStyleSheet, m_urlToLoad, m_charset);
 
     m_urlToLoad = String();
+    m_charset = String();
     m_linkIsStyleSheet = false;
 }
     
@@ -827,7 +832,7 @@ void PreloadScanner::emitCSSRule()
         String value(m_cssRuleValue.data(), m_cssRuleValue.size());
         String url = parseURL(value);
         if (!url.isEmpty())
-            m_document->docLoader()->preload(CachedResource::CSSStyleSheet, url);
+            m_document->docLoader()->preload(CachedResource::CSSStyleSheet, url, String());
     }
     m_cssRule.clear();
     m_cssRuleValue.clear();
