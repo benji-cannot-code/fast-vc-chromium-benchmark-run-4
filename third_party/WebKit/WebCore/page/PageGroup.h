c84638b66f1bcb6a899903bc81463627c1b528a5
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-18 10:00:56 +0000

    Build fix for Qt and gcc 4.3.
    git-svn-id: svn://svn.chromium.org/blink/trunk@32199 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/PageGroup.h
+++ third_party/WebKit/WebCore/page/PageGroup.h
@@ -29,12 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/HashSet.h>
 #include <wtf/Noncopyable.h>
+#include "LocalStorage.h"
 #include "StringHash.h"
 
 namespace WebCore {
 
     class KURL;
-    class LocalStorage;
     class Page;
 
     class PageGroup : Noncopyable {
