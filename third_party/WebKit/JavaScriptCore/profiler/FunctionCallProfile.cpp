e6921c83b5f2bbf271afb2f9228f0b937c3ab243
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-18 09:31:58 +0000

    Build fix for gcc 4.3 and Gtk.
    git-svn-id: svn://svn.chromium.org/blink/trunk@32198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/profiler/FunctionCallProfile.cpp
+++ third_party/WebKit/JavaScriptCore/profiler/FunctionCallProfile.cpp
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Profiler.h"
 #include "DateMath.h"
 
+#include <stdio.h>
+
 namespace KJS {
 
 FunctionCallProfile::FunctionCallProfile(const UString& name)
--- third_party/WebKit/JavaScriptCore/profiler/Profiler.cpp
+++ third_party/WebKit/JavaScriptCore/profiler/Profiler.cpp
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <kjs/ExecState.h>
 #include <kjs/function.h>
 
+#include <stdio.h>
+
 namespace KJS {
 
 static Profiler* sharedProfiler = 0;
