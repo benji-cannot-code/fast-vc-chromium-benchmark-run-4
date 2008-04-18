5a70c4eaa1d05de1bccbc9415a34b37e0113cac4
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-18 01:02:29 +0000

    2008-04-17  Jon Honeycutt  <jhoneycutt@apple.com>
            Reviewed by aliu.
    
            * page/AccessibilityObject.cpp:
            (WebCore::AccessibilityObject::layoutCount): Check isRenderView() before
            casting to RenderView.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32041 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/AccessibilityObject.cpp
+++ third_party/WebKit/WebCore/page/AccessibilityObject.cpp
@@ -680,6 +680,8 @@ bool AccessibilityObject::isLoaded() const
 
 int AccessibilityObject::layoutCount() const
 {
+    if (!m_renderer->isRenderView())
+        return 0;
     return static_cast<RenderView*>(m_renderer)->frameView()->layoutCount();
 }
 
