a576967b9f61348950bf4ea4edc1a98a4cd96c82
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-18 20:14:58 +0000

    Fixed marquee suspension on wrong document. Passes current document into Frame::clearTimers.
    Reviewed by hyatt.
    
    * history/CachedPage.cpp:
    (WebCore::CachedPage::clear):
    * page/Frame.cpp:
    (WebCore::Frame::clearTimers):
    * page/Frame.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32223 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -1459,12 +1459,11 @@ void Frame::sendScrollEvent()
     doc->dispatchHTMLEvent(scrollEvent, true, false);
 }
 
-void Frame::clearTimers(FrameView *view)
+void Frame::clearTimers(FrameView *view, Document *document)
 {
     if (view) {
         view->unscheduleRelayout();
         if (view->frame()) {
-            Document* document = view->frame()->document();
             if (document && document->renderer() && document->renderer()->hasLayer())
                 document->renderer()->layer()->suspendMarquees();
             view->frame()->animationController()->suspendAnimations();
@@ -1474,7 +1473,7 @@ void Frame::clearTimers(FrameView *view)
 
 void Frame::clearTimers()
 {
-    clearTimers(d->m_view.get());
+    clearTimers(d->m_view.get(), document());
 }
 
 RenderStyle *Frame::styleForSelectionStart(Node *&nodeToRemove) const
