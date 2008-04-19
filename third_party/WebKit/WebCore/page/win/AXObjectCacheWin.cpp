9eb1d89795816076b1e842c1f9d34fd16b7e8d4e
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-19 03:52:04 +0000

    2008-04-17  Jon Honeycutt  <jhoneycutt@apple.com>
            Reviewed by Darin, Alice.
    
            Implement AXObjectCache::detachWrapper() on Windows.
    
            * page/win/AXObjectCacheWin.cpp:
            (WebCore::AXObjectCache::detachWrapper): If this object has a wrapper,
            call its detach() method.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32249 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/win/AXObjectCacheWin.cpp
+++ third_party/WebKit/WebCore/page/win/AXObjectCacheWin.cpp
@@ -32,12 +32,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-void AXObjectCache::detachWrapper(AccessibilityObject*)
+void AXObjectCache::detachWrapper(AccessibilityObject* obj)
 {
+    // On Windows, AccessibilityObjects are created when get_accChildCount is
+    // called, but they are not wrapped until get_accChild is called, so this
+    // object may not have a wrapper.
+    if (AccessibilityObjectWrapper* wrapper = obj->wrapper())
+        wrapper->detach();
 }
 
 void AXObjectCache::attachWrapper(AccessibilityObject*)
 {
+    // On Windows, AccessibilityObjects are wrapped when the accessibility
+    // software requests them via get_accChild.
 }
 
 void AXObjectCache::postNotification(RenderObject*, const String&)
