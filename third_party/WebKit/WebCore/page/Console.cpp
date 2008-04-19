4836802d4ed0119d4cb95ec94845f796027ce9b1
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-19 02:50:57 +0000

    Gtk build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@32244 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Console.cpp
+++ third_party/WebKit/WebCore/page/Console.cpp
@@ -36,8 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorController.h"
 #include "Page.h"
 #include "PlatformString.h"
-#include <kjs/Profiler.h>
 #include <kjs/list.h>
+#include <profiler/Profiler.h>
 
 using namespace KJS;
 
