5fe5c80d3d93a346ddd65042dd16ea4074f60bde
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-19 01:34:03 +0000

    2008-04-18  Antti Koivisto  <antti@apple.com>
            Reviewed by Oliver.
    
            Implement path animation for <animateMotion>.
            
            SVG test suite tests
            animate-elem-07-t.svg
            animate-elem-08-t.svg
            animate-elem-24-t.svg
            animate-elem-30-t.svg
    
            * svg/SVGAnimateMotionElement.cpp:
            (WebCore::SVGAnimateMotionElement::SVGAnimateMotionElement):
            (WebCore::SVGAnimateMotionElement::hasValidTarget):
            (WebCore::SVGAnimateMotionElement::parseMappedAttribute):
            (WebCore::SVGAnimateMotionElement::rotateMode):
            (WebCore::SVGAnimateMotionElement::animationPath):
            (WebCore::SVGAnimateMotionElement::calculateAnimatedValue):
            (WebCore::SVGAnimateMotionElement::applyResultsToTarget):
            * svg/SVGAnimateMotionElement.h:
            (WebCore::SVGAnimateMotionElement::):
            * svg/SVGAnimationElement.cpp:
            (WebCore::SVGAnimationElement::animationMode):
            (WebCore::SVGAnimationElement::startedActiveInterval):
            * svg/SVGAnimationElement.h:
            (WebCore::SVGAnimationElement::):
            (WebCore::SVGAnimationElement::calculateDistance):
            (WebCore::SVGAnimationElement::animationPath):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32243 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGAnimationElement.h
+++ third_party/WebKit/WebCore/svg/SVGAnimationElement.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(SVG_ANIMATION)
 
 #include "ElementTimeControl.h"
+#include "Path.h"
 #include "SMILTime.h"
 #include "SVGSMILElement.h"
 #include "SVGExternalResourcesRequired.h"
@@ -77,7 +78,7 @@ protected:
         String byValue() const;
         String fromValue() const;
         
-        enum AnimationMode { NoAnimation, ToAnimation, ByAnimation, ValuesAnimation, FromToAnimation, FromByAnimation };
+        enum AnimationMode { NoAnimation, ToAnimation, ByAnimation, ValuesAnimation, FromToAnimation, FromByAnimation, PathAnimation };
         AnimationMode animationMode() const;
 
         virtual bool hasValidTarget() const;
@@ -99,10 +100,10 @@ private:
         virtual bool calculateFromAndToValues(const String& fromString, const String& toString) = 0;
         virtual bool calculateFromAndByValues(const String& fromString, const String& byString) = 0;
         virtual void calculateAnimatedValue(float percentage, unsigned repeat, SVGSMILElement* resultElement) = 0;
+        virtual float calculateDistance(const String& fromString, const String& toString) { return -1.f; }
+        virtual Path animationPath() const { return Path(); }
         
         void currentValuesForValuesAnimation(float percent, float& effectivePercent, String& from, String& to);
-
-        virtual float calculateDistance(const String& fromString, const String& toString) { return -1.f; }
 
         void calculateKeyTimesForCalcModePaced();
     
