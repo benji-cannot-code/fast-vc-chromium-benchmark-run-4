8d402c2e498450678d3d1133d4d1cd66c74b19e3
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-19 05:39:32 +0000

    wx build fixes. Rename LocalStorage.h to LocalStorageEntry.h to avoid header detection issues between WebCore/storage/LocalStorage.h and it, and add $(PROFILER_SOURCES) to the wx JSCore build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@32259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/ExecState.h
+++ third_party/WebKit/JavaScriptCore/kjs/ExecState.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ExecState_h
 
 #include "LabelStack.h"
-#include "LocalStorage.h"
+#include "LocalStorageEntry.h"
 #include "completion.h"
 #include "list.h"
 #include "scope_chain.h"
--- third_party/WebKit/JavaScriptCore/kjs/function.h
+++ third_party/WebKit/JavaScriptCore/kjs/function.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define KJS_FUNCTION_H
 
 #include "JSVariableObject.h"
-#include "LocalStorage.h"
+#include "LocalStorageEntry.h"
 #include "SymbolTable.h"
 #include "nodes.h"
 #include "object.h"
