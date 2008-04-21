b66cb24d579145e6db9041e226f8b0dc84718300
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-21 10:46:47 +0000

    2008-04-21  Holger Hans Peter Freyther  <holger.freyther@trolltech.com>
            Reviewed by Simon.
    
            * Remove the special Qt ctor of BitmapImage as it did not work. The Bitmap returned
            0 as width and after intialising drawing would fail. BitmapImage relies on a proper
            ImageSource.
            * Instead of adding more special cases to BitmapImage create a new class called StillImage
            which is getting one QPixmap and is implementing Image::draw and Image::size. This is
            working well for the images created using Image::loadPlatformResource.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32302 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/ImageQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/ImageQt.cpp
@@ -51,6 +51,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <math.h>
 
+namespace WebCore {
+class StillImage : public Image {
+public:
+    StillImage(const QPixmap& pixmap);
+
+    virtual IntSize size() const;
+    virtual QPixmap* getPixmap() const;
+    virtual void draw(GraphicsContext*, const FloatRect& dstRect, const FloatRect& srcRect, CompositeOperator);
+
+private:
+    QPixmap m_pixmap;
+};
+}
+
 // This function loads resources into WebKit
 static QPixmap loadResourcePixmap(const char *name)
 {
@@ -88,8 +102,7 @@ void FrameData::clear()
 
 Image* Image::loadPlatformResource(const char* name)
 {
-    BitmapImage* img = new BitmapImage(loadResourcePixmap(name));
-    return img;
+    return new StillImage(loadResourcePixmap(name));
 }
 
     
@@ -99,35 +112,12 @@ void Image::drawPattern(GraphicsContext* ctxt, const FloatRect& tileRect, const 
     notImplemented();
 }
 
-BitmapImage::BitmapImage(const QPixmap &pixmap, ImageObserver *observer)
-    : Image(observer)
-    , m_currentFrame(0)
-    , m_frames(0)
-    , m_frameTimer(0)
-    , m_repetitionCount(0)
-    , m_repetitionsComplete(0)
-    , m_isSolidColor(false)
-    , m_animatingImageType(true)
-    , m_animationFinished(false)
-    , m_allDataReceived(false)
-    , m_haveSize(false)
-    , m_sizeAvailable(false)
-    , m_decodedSize(0)
-    , m_haveFrameCount(false)
-    , m_frameCount(0)
-{
-    m_pixmap = new QPixmap(pixmap);
-}
-
 void BitmapImage::initPlatformData()
 {
-    m_pixmap = 0;
 }
 
 void BitmapImage::invalidatePlatformData()
 {
-    delete m_pixmap;
-    m_pixmap = 0;
 }
     
 // Drawing Routines
@@ -201,10 +191,34 @@ void BitmapImage::checkForSolidColor()
 
 QPixmap* BitmapImage::getPixmap() const
 {
-    if (!m_pixmap)
-      return const_cast<BitmapImage*>(this)->frameAtIndex(0);
-    else
-      return m_pixmap;
+    return const_cast<BitmapImage*>(this)->frameAtIndex(0);
+}
+
+StillImage::StillImage(const QPixmap& pixmap)
+    : m_pixmap(pixmap)
+{}
+
+IntSize StillImage::size() const
+{
+    return IntSize(m_pixmap.width(), m_pixmap.height());
+}
+
+QPixmap* StillImage::getPixmap() const
+{
+    return const_cast<QPixmap*>(&m_pixmap);
+}
+
+void StillImage::draw(GraphicsContext* ctxt, const FloatRect& dst,
+                      const FloatRect& src, CompositeOperator op)
+{
+    if (m_pixmap.isNull())
+        return;
+
+    ctxt->save();
+    ctxt->setCompositeOperation(op);
+    QPainter* painter(ctxt->platformContext());
+    painter->drawPixmap(dst, m_pixmap, src);
+    ctxt->restore();
 }
 
 }
