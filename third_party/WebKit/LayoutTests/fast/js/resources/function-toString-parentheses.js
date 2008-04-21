c01873afd40ebe6f0b2b6a521490d720acd86306
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-21 15:34:28 +0000

    Stamped by Anders.
            Make test work in Opera.  Fix bug in results serialization.
    
            * fast/js/resources/function-toString-parentheses.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/resources/function-toString-parentheses.js
+++ third_party/WebKit/LayoutTests/fast/js/resources/function-toString-parentheses.js
@@ -11,6 +11,7 @@ function compileAndSerialize(expression)
     serializedString = serializedString.replace(/[ \t\r\n]+/g, " ");
     serializedString = serializedString.replace("function () { return ", "");
     serializedString = serializedString.replace("; }", "");
+    serializedString = serializedString.replace(/^\s*|\s*$/g, ''); // Opera adds whitespace
     return serializedString;
 }
 
@@ -21,6 +22,7 @@ function compileAndSerializeLeftmostTest(expression)
     serializedString = serializedString.replace(/[ \t\r\n]+/g, " ");
     serializedString = serializedString.replace("function () { ", "");
     serializedString = serializedString.replace("; }", "");
+    serializedString = serializedString.replace(/^\s*|\s*$/g, ''); // Opera adds whitespace
     return serializedString;
 }
 
@@ -59,7 +61,7 @@ function testHigherFirst(opHigher, opLower)
     shouldBe("compileAndSerialize('(a " + opHigher + " b) " + opLower + " c')",
         removesExtraParentheses
             ? "'a " + opHigher + " b " + opLower + " c'"
-            : "'a " + opHigher + " (b " + opLower + " c)'"
+            : "'(a " + opHigher + " b) " + opLower + " c'"
     );
     shouldBe("compileAndSerialize('a " + opHigher + " (b " + opLower + " c)')",
         "'a " + opHigher + " (b " + opLower + " c)'");
