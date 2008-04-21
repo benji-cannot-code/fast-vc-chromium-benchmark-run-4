71d5c7896af4d033923781e081829908bed18d4e
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-21 23:22:06 +0000

    Fix Windows bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@32355 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSParser.cpp
+++ third_party/WebKit/WebCore/css/CSSParser.cpp
@@ -1628,7 +1628,7 @@ bool CSSParser::parseFillShorthand(int propId, const int* properties, int numPro
     ShorthandScope scope(this, propId);
 
     bool parsedProperty[cMaxFillProperties] = { false };
-    RefPtr<CSSValue> values[numProperties];
+    RefPtr<CSSValue> values[cMaxFillProperties];
     RefPtr<CSSValue> positionYValue;
     int i;
 
