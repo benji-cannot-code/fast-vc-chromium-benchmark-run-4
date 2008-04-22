8f15f118bb84a46a6c77702842496484955bb929
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-22 08:31:10 +0000

    Simon Hausmann <hausmann@webkit.org>
    Don't crash if an input method query is done without a page.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32379 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebview.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebview.cpp
@@ -617,7 +617,9 @@ bool QWebView::focusNextPrevChild(bool next)
 */
 QVariant QWebView::inputMethodQuery(Qt::InputMethodQuery property) const
 {
-    return d->page->inputMethodQuery(property);
+    if (d->page)
+        return d->page->inputMethodQuery(property);
+    return QVariant();
 }
 
 /*!\reimp
