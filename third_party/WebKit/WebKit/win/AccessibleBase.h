e345c51ddf0cb6171ae86b4ad38c3080e68d887f
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-22 06:45:31 +0000

    2008-04-18  Jon Honeycutt  <jhoneycutt@apple.com>
            Reviewed by Darin.
    
            Implement get_accName(), get_accValue(), get_accDescription(). Added
            virtual methods that accessible objects can override to customize the
            result.
    
            * AccessibleBase.cpp:
            (AccessibleBase::get_accName): Report result of name().
            (AccessibleBase::get_accValue): Report result of value().
            (AccessibleBase::get_accDescription): Report result of description().
            (AccessibleBase::name): Added; returns AccessibilityObject::title().
            (AccessibleBase::value): Added; returns
            AccessibilityObject::stringValue().
            (AccessibleBase::description): Added; returns
            AccessibilityObject::accessibilityDescription().
            * AccessibleBase.h: Added declarations for name(), value(), and
            description().
            * ChangeLog: Fix my misuse of "definition."
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32369 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/AccessibleBase.h
+++ third_party/WebKit/WebKit/win/AccessibleBase.h
@@ -31,11 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/AccessibilityObject.h>
 #include <WebCore/AccessibilityObjectWrapperWin.h>
 
-using WebCore::AccessibilityObject;
-
 class AccessibleBase : public IAccessible, public WebCore::AccessibilityObjectWrapper {
 public:
-    static AccessibleBase* createInstance(AccessibilityObject*);
+    static AccessibleBase* createInstance(WebCore::AccessibilityObject*);
 
     // IUnknown
     virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppvObject);
@@ -93,12 +91,16 @@ public:
     }
 
 protected:
-    AccessibleBase(AccessibilityObject*);
+    AccessibleBase(WebCore::AccessibilityObject*);
     virtual ~AccessibleBase();
 
-    HRESULT getAccessibilityObjectForChild(VARIANT vChild, AccessibilityObject*&) const;
+    virtual WebCore::String name() const;
+    virtual WebCore::String value() const;
+    virtual WebCore::String description() const;
 
-    static AccessibleBase* wrapper(AccessibilityObject*);
+    HRESULT getAccessibilityObjectForChild(VARIANT vChild, WebCore::AccessibilityObject*&) const;
+
+    static AccessibleBase* wrapper(WebCore::AccessibilityObject*);
 
     int m_refCount;
 
