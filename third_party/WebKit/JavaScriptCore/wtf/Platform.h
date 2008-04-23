345b38940fbcb6f1f4e70757502e99d17a3bf74f
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-23 13:58:56 +0000

    Holger Hans Peter Freyther  <zecke@selfish.org>
    Removed the #define for USE_SYSTEM_MALLOC that we set in WebKit.pri
    already.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32431 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -237,10 +237,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_USE_PTHREADS 1
 #endif
 
-#if PLATFORM(QT)
-#define USE_SYSTEM_MALLOC 1
-#endif
-
 #if PLATFORM(MAC) || PLATFORM(WIN)
 #define HAVE_ACCESSIBILITY 1
 #endif
