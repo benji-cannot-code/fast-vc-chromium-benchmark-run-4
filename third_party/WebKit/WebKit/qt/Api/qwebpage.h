d0ba0327e011be2b3a91f54c10262abc0c86af36
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-23 14:36:10 +0000

    Simon Hausmann <hausmann@webkit.org>
    Fix crashes on window.close().
    
    We should not delete the QWebPage object in the ChromeClient but leave it up to
    the application when and whether to delete a browser window. For this we now
    emit the windowCloseRequested() signal.
    
    Done with Tor Arne.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32433 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.h
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.h
@@ -246,6 +246,7 @@ Q_SIGNALS:
     void geometryChangeRequested(const QRect& geom);
     void repaintRequested(const QRect& dirtyRect);
     void scrollRequested(int dx, int dy, const QRect& scrollViewRect);
+    void windowCloseRequested();
     void linkClicked(const QUrl &url);
 
     void toolBarVisibilityChangeRequested(bool visible);
