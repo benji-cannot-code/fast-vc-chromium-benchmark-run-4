4e12780ee095675357a8650249dae50ea22d9a5f
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-24 07:55:22 +0000

    Tor Arne Vestbø  <tavestbo@trolltech.com>
    Cleaned up copyright headers in the Qt port (removed misplaced class
    descriptions and fixed inconsistent whitespace and indentation).
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32464 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.h
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.h
@@ -17,9 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     along with this library; see the file COPYING.LIB.  If not, write to
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA 02110-1301, USA.
-
-    This class provides all functionality needed for loading images, style sheets and html
-    pages from the web. It has a memory cache for these objects.
 */
 
 #ifndef QWEBPAGE_H
--- third_party/WebKit/WebKit/qt/Api/qwebview.h
+++ third_party/WebKit/WebKit/qt/Api/qwebview.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA 02110-1301, USA.
 */
+
 #ifndef QWEBVIEW_H
 #define QWEBVIEW_H
 
