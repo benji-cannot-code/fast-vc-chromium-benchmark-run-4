7f49c4c23f5b4e4963220265470112453503cbaa
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-24 18:57:58 +0000

    ../mac:
    2008-04-24  Brady Eidson  <beidson@apple.com>
    
            Reviewed by Darin
    
            Rework the Settings population again.
    
            * WebView/WebView.mm:
            (-[WebView _updateSettingsFromPreferences:]): This method is called both from _preferencesChangedNotification
              and directly from WebView's common init function.
            (-[WebView _preferencesChangedNotification:]):
            (-[WebView _commonInitializationWithFrameName:groupName:]): Call _updateSettingsFromPreferences immediately
              after creating the new Page
    
    ../win:
    
    2008-04-24  Brady Eidson  <beidson@apple.com>
    
            Reviewed by Darin
    
            Revert my previous change to WebKit/Win until I can make it act more like the new Mac code
    
            * WebView.cpp:
            (WebView::initWithFrame):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32505 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -2083,10 +2083,6 @@ HRESULT STDMETHODCALLTYPE WebView::initWithFrame(
     WebKitSetWebDatabasesPathIfNecessary();
 
     m_page = new Page(new WebChromeClient(this), new WebContextMenuClient(this), new WebEditorClient(this), new WebDragClient(this), new WebInspectorClient(this));
-
-    IWebNotificationCenter* notifyCenter = WebNotificationCenter::defaultCenterInternal();
-    notifyCenter->addObserver(this, WebPreferences::webPreferencesChangedNotification(), static_cast<IWebPreferences*>(m_preferences.get()));
-    m_preferences->postPreferencesChangesNotification();
     
     if (m_uiDelegate) {
         COMPtr<IWebUIDelegate2> uiDelegate2;
@@ -2116,6 +2112,10 @@ HRESULT STDMETHODCALLTYPE WebView::initWithFrame(
 
     initializeToolTipWindow();
     windowAncestryDidChange();
+
+    IWebNotificationCenter* notifyCenter = WebNotificationCenter::defaultCenterInternal();
+    notifyCenter->addObserver(this, WebPreferences::webPreferencesChangedNotification(), static_cast<IWebPreferences*>(m_preferences.get()));
+    m_preferences->postPreferencesChangesNotification();
 
     setSmartInsertDeleteEnabled(TRUE);
     return hr;
