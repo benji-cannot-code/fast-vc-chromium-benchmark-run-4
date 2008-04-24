68487612b4e2404a0a4a094f26f642f2a9756272
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-24 11:20:22 +0000

    Holger Hans Peter Freyther <zecke@selfish.org>
    * Set an expiration date for local files as we do not know what will happen
      to these files (in contrast to a http server with max-age and other headers)
    * Remember the time when we started the job and use that for the expiration date
    * QNetworkManager does not handle expiration at all, we might want to set the
      expiration for all downloads but this will be a performance nightmare.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32482 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceHandleInternal.h"
 #include "ResourceResponse.h"
 #include "ResourceRequest.h"
+#include <QDateTime>
 #include <QFile>
 #include <QNetworkReply>
 #include <QNetworkCookie>
@@ -133,6 +134,7 @@ QNetworkReplyHandler::QNetworkReplyHandler(ResourceHandle *handle)
     , m_reply(0)
     , m_redirected(false)
     , m_responseSent(false)
+    , m_startTime(0)
 {
     const ResourceRequest &r = m_resourceHandle->request();
 
@@ -253,6 +255,9 @@ void QNetworkReplyHandler::sendResponseIfNeeded()
         response.setHTTPHeaderField(QString::fromAscii(headerName), QString::fromAscii(m_reply->rawHeader(headerName)));
     }
 
+    if (isLocalFileReply)
+        response.setExpirationDate(m_startTime);
+
     QUrl redirection = m_reply->attribute(QNetworkRequest::RedirectionTargetAttribute).toUrl();
     if (redirection.isValid()) {
         QUrl newUrl = m_reply->url().resolved(redirection);
@@ -307,6 +312,8 @@ void QNetworkReplyHandler::start()
     if (m_method == QNetworkAccessManager::PostOperation
         && (!url.toLocalFile().isEmpty() || url.scheme() == QLatin1String("data")))
         m_method = QNetworkAccessManager::GetOperation;
+
+    m_startTime = QDateTime::currentDateTime().toTime_t();
 
     switch (m_method) {
         case QNetworkAccessManager::GetOperation:
--- third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.h
+++ third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.h
@@ -64,6 +64,7 @@ private:
     bool m_responseSent;
     QNetworkAccessManager::Operation m_method;
     QNetworkRequest m_request;
+    uint   m_startTime;
 };
 
 // Self destructing QIODevice for FormData
