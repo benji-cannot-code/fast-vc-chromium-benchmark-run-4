57ea24628bb5191eb1bf21ff737fa21be9fe5a28
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-24 08:53:44 +0000

    Tor Arne Vestbø <tavestbo@trolltech.com>
    Render text areas using Qt (ensures proper style).
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32476 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/RenderThemeQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/RenderThemeQt.cpp
@@ -254,6 +254,7 @@ bool RenderThemeQt::paintTextField(RenderObject* o, const RenderObject::PaintInf
     QStyleOptionFrameV2 panel;
     if (p.widget)
         panel.initFrom(p.widget);
+
     panel.rect = r;
     panel.lineWidth = p.style->pixelMetric(QStyle::PM_DefaultFrameWidth, &panel, p.widget);
     panel.state |= QStyle::State_Sunken;
@@ -261,7 +262,9 @@ bool RenderThemeQt::paintTextField(RenderObject* o, const RenderObject::PaintInf
 
     // Get the correct theme data for a button
     EAppearance appearance = applyTheme(panel, o);
-    Q_ASSERT(appearance == TextFieldAppearance || appearance == SearchFieldAppearance);
+    Q_ASSERT(appearance == TextFieldAppearance
+            || appearance == SearchFieldAppearance
+            || appearance == TextAreaAppearance);
 
     // Now paint the text field.
     p.drawPrimitive(QStyle::PE_PanelLineEdit, panel);
@@ -271,6 +274,17 @@ bool RenderThemeQt::paintTextField(RenderObject* o, const RenderObject::PaintInf
 
 void RenderThemeQt::adjustTextFieldStyle(CSSStyleSelector*, RenderStyle* style, Element*) const
 {
+    style->setBackgroundColor(Color::transparent);
+}
+
+bool RenderThemeQt::paintTextArea(RenderObject* o, const RenderObject::PaintInfo& i, const IntRect& r)
+{
+    return paintTextField(o, i, r);
+}
+
+void RenderThemeQt::adjustTextAreaStyle(CSSStyleSelector* selector, RenderStyle* style, Element* element) const
+{
+    adjustTextFieldStyle(selector, style, element);
 }
 
 void RenderThemeQt::adjustMenuListStyle(CSSStyleSelector*, RenderStyle* style, Element*) const
@@ -407,6 +421,7 @@ bool RenderThemeQt::supportsFocus(EAppearance appearance) const
         case PushButtonAppearance:
         case ButtonAppearance:
         case TextFieldAppearance:
+        case TextAreaAppearance:
         case MenulistAppearance:
         case RadioAppearance:
         case CheckboxAppearance:
