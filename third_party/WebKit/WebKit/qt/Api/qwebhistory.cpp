12bb6ae9aae4d6c6155fa93551225ec850b68d20
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-25 12:37:01 +0000

    Denis Dzyubenko <denis.dzyubenko@trolltech.com>
    Fixed the way QWebHistory works - when you call back(), forward() or goToItem() functions it changes the current item in history *and* loads the corresponding page.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32570 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebhistory.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebhistory.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "PlatformString.h"
 #include "Image.h"
+#include "Page.h"
 
 #include <QSharedData>
 
@@ -270,35 +271,41 @@ bool QWebHistory::canGoForward() const
 }
 
 /*!
-  Set the current item to be the previous item in the history; i.e., goes back
-  one history item.
+  Set the current item to be the previous item in the history and goes to the page;
+  i.e., goes back one history item.
 
   \sa forward(), goToItem()
 */
 void QWebHistory::back()
 {
     d->lst->goBack();
+    WebCore::Page* page = d->lst->page();
+    page->goToItem(currentItem().d->item, WebCore::FrameLoadTypeIndexedBackForward);
 }
 
 /*!
-  Sets the current item to be the next item in the history; i.e., goes forward
-  one history item.
+  Sets the current item to be the next item in the history and goes to the page;
+  i.e., goes forward one history item.
 
   \sa back(), goToItem()
 */
 void QWebHistory::forward()
 {
     d->lst->goBack();
+    WebCore::Page* page = d->lst->page();
+    page->goToItem(currentItem().d->item, WebCore::FrameLoadTypeIndexedBackForward);
 }
 
 /*!
-  Sets the current item to be the specified \a item in the history.
+  Sets the current item to be the specified \a item in the history and goes to the page.
 
   \sa back(), forward()
 */
 void QWebHistory::goToItem(const QWebHistoryItem &item)
 {
     d->lst->goToItem(item.d->item);
+    WebCore::Page* page = d->lst->page();
+    page->goToItem(currentItem().d->item, WebCore::FrameLoadTypeIndexedBackForward);
 }
 
 /*!
