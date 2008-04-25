48c549314d6c96195f140b09ea0810920046aebb
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-25 00:26:00 +0000

    2008-04-24  Brady Eidson  <beidson@apple.com>
            Reviewed by Teh Mitzpettel
    
            Make it so PageGroups know what their own name is.
    
            * WebCore.base.exp:
    
            * page/Page.cpp:
            (WebCore::Page::setGroupName):
            (WebCore::Page::groupName):
            * page/Page.h:
    
            * page/PageGroup.cpp: Move the PageGroupMap into the PageGroup class.
            (WebCore::PageGroup::PageGroup): Added a c'tor that takes the group name as an argument instead of
              the initial Page.
            (WebCore::PageGroup::pageGroup): Handles creating and mapping names to groups
            * page/PageGroup.h:
            (WebCore::PageGroup::name):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/PageGroup.h
+++ third_party/WebKit/WebCore/page/PageGroup.h
@@ -39,8 +39,11 @@ namespace WebCore {
 
     class PageGroup : Noncopyable {
     public:
+        PageGroup(const String& name);
         PageGroup(Page*);
 
+        static PageGroup* pageGroup(const String& groupName);
+        
         const HashSet<Page*>& pages() const { return m_pages; }
 
         void addPage(Page*);
@@ -55,6 +58,7 @@ namespace WebCore {
         static void setShouldTrackVisitedLinks(bool);
         static void removeAllVisitedLinks();
 
+        const String& name() { return m_name; }
         unsigned identifier() { return m_identifier; }
 
 #if ENABLE(DOM_STORAGE)
@@ -63,6 +67,8 @@ namespace WebCore {
 
     private:
         void addVisitedLink(unsigned stringHash);
+
+        String m_name;
 
         HashSet<Page*> m_pages;
         HashSet<unsigned, AlreadyHashed> m_visitedLinkHashes;
