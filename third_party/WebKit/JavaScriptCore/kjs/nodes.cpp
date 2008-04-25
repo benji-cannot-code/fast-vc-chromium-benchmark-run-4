benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 1842ce0c18d546a611ea95b93b044a1c272c801c

Reviewed by Geoff.
        - Wait until local variable data is fully constructed before notifying the debugger of entering
        or leaving a call frame.

        * kjs/function.cpp:
        (KJS::FunctionImp::callAsFunction):
        * kjs/nodes.cpp:
        (KJS::FunctionBodyNode::execute):



git-svn-id: svn://svn.chromium.org/blink/trunk@28458 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
@@ -4580,7 +4580,28 @@ UString FunctionBodyNode::paramString() const
 Completion FunctionBodyNode::execute(ExecState* exec)
 {
     processDeclarations(exec);
-    return BlockNode::execute(exec);
+
+    if (Debugger* dbg = exec->dynamicInterpreter()->debugger()) {
+        if (!dbg->callEvent(exec, sourceId(), lineNo(), exec->function(), *exec->arguments())) {
+            dbg->imp()->abort();
+            return Completion(Interrupted, jsUndefined());
+        }
+    }    
+    
+    Completion completion = BlockNode::execute(exec);
+    
+    if (Debugger* dbg = exec->dynamicInterpreter()->debugger()) {
+        if (completion.complType() == Throw)
+            exec->setException(completion.value());
+
+        if (!dbg->returnEvent(exec, sourceId(), lineNo(), exec->function())) {
+            dbg->imp()->abort();
+            return Completion(Interrupted, jsUndefined());
+        }
+    }
+
+    
+    return completion;
 }
 
 // ------------------------------ FuncDeclNode ---------------------------------

third_party/WebKit/JavaScriptCore/kjs/function.cpp
@@ -78,43 +78,11 @@ JSValue* FunctionImp::callAsFunction(ExecState* exec, JSObject* thisObj, const L
   if (exec->hadException())
     newExec.setException(exec->exception());
 
-  Debugger* dbg = exec->dynamicInterpreter()->debugger();
-  int sourceId = -1;
-  int lineNo = -1;
-  if (dbg) {
-    sourceId = body->sourceId();
-    lineNo = body->firstLine();
-
-    bool cont = dbg->callEvent(&newExec, sourceId, lineNo, this, args);
-    if (!cont) {
-      dbg->imp()->abort();
-      return jsUndefined();
-    }
-  }
-
   Completion comp = execute(&newExec);
 
   // if an exception occured, propogate it back to the previous execution object
   if (newExec.hadException())
     comp = Completion(Throw, newExec.exception());
-
-  // The debugger may have been deallocated by now if the WebFrame
-  // we were running in has been destroyed, so refetch it.
-  // See http://bugs.webkit.org/show_bug.cgi?id=9477
-  dbg = exec->dynamicInterpreter()->debugger();
-
-  if (dbg) {
-    lineNo = body->lastLine();
-
-    if (comp.complType() == Throw)
-        newExec.setException(comp.value());
-
-    int cont = dbg->returnEvent(&newExec, sourceId, lineNo, this);
-    if (!cont) {
-      dbg->imp()->abort();
-      return jsUndefined();
-    }
-  }
 
   if (comp.complType() == Throw) {
     exec->setException(comp.value());

commit 0f0dfd8a611a9d525f07aed13cf18975ef4e3165

<rdar://problem/5819422> REGRESSION: Wrong line number passed to -willLeaveCallFrame
        Patch by George Dicker and Michael Kahl.  Reviewed by Darin.

        When -[NSObject(WebScriptDebugDelegate) webView:willLeaveCallFrame:sourceId:line:forWebFrame:]
        is invoked, the first line number of the function is returned instead of the last
        line number.  This regressed in r28458.

        * kjs/nodes.cpp:
        (KJS::FunctionBodyNodeWithDebuggerHooks::execute): Pass lastLine() instead of lineNo()
        when calling Debugger::returnEvent().



git-svn-id: svn://svn.chromium.org/blink/trunk@32578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
@@ -4910,7 +4910,7 @@ JSValue* FunctionBodyNodeWithDebuggerHooks::execute(ExecState* exec)
     if (Debugger* dbg = exec->dynamicGlobalObject()->debugger()) {
         if (exec->completionType() == Throw)
             exec->setException(result);
-        if (!dbg->returnEvent(exec, sourceId(), lineNo(), exec->function()))
+        if (!dbg->returnEvent(exec, sourceId(), lastLine(), exec->function()))
             return exec->setInterruptedCompletion();
     }
 