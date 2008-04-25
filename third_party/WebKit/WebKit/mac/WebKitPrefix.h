747bec1334de2ee87b2747a40fb37e9e43482e73
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-25 01:29:21 +0000

    Add a definition of BUILDING_ON_LEOPARD to complement BUILDING_ON_TIGER.
    Reviewed by Sam Weinig.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebKitPrefix.h
+++ third_party/WebKit/WebKit/mac/WebKitPrefix.h
@@ -62,6 +62,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 #if MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_4
 #define BUILDING_ON_TIGER 1
+#elif MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_5
+#define BUILDING_ON_LEOPARD 1
 #endif
 #endif
 
