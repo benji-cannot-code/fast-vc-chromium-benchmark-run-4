986b84325ce322759354aa55b7a7db6fc9164114
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-25 07:52:37 +0000

    Simon Hausmann <hausmann@webkit.org>
    When we encounter a new/unknown HTTP request type report it back to WebCore as loading error.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32541 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qwebpage.h>
 
 #include <QDebug>
+#include <QCoreApplication>
 
 namespace WebCore {
 
@@ -340,8 +341,17 @@ void QNetworkReplyHandler::start()
             putDevice->setParent(m_reply);
             break;
         }
-        case QNetworkAccessManager::UnknownOperation:
-            break; // eh?
+        case QNetworkAccessManager::UnknownOperation: {
+            m_reply = 0;
+            ResourceHandleClient* client = m_resourceHandle->client();
+            if (client) {
+                ResourceError error(url.host(), 400 /*bad request*/,
+                                    url.toString(),
+                                    QCoreApplication::translate("QWebPage", "Bad HTTP request"));
+                client->didFail(m_resourceHandle, error);
+            }
+            return;
+        }
     }
 
     m_reply->setParent(this);
