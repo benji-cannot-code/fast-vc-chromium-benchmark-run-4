92d01ceb63758c55ab84d0e50fe351933ff81105
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-25 12:37:32 +0000

    Tor Arne Vestbø <tavestbo@trolltech.com>
    Respect antialiasing hint when drawing focus rects.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
@@ -571,6 +571,8 @@ void GraphicsContext::drawFocusRing(const Color& color)
         return;
 
     QPainter *p = m_data->p();
+    const bool antiAlias = p->testRenderHint(QPainter::Antialiasing);
+    p->setRenderHint(QPainter::Antialiasing, m_data->antiAliasingForRectsAndLines);
 
     const QPen oldPen = p->pen();
     const QBrush oldBrush = p->brush();
@@ -594,6 +596,8 @@ void GraphicsContext::drawFocusRing(const Color& color)
 #endif
     p->setPen(oldPen);
     p->setBrush(oldBrush);
+
+    p->setRenderHint(QPainter::Antialiasing, antiAlias);
 }
 
 void GraphicsContext::drawLineForText(const IntPoint& origin, int width, bool printing)
@@ -822,7 +826,7 @@ void GraphicsContext::clipOut(const IntRect& rect)
 {
     if (paintingDisabled())
         return;
-        
+
     QPainter *p = m_data->p();
     QRectF clipBounds = p->clipPath().boundingRect();
     QPainterPath newClip;
@@ -837,7 +841,7 @@ void GraphicsContext::clipOutEllipseInRect(const IntRect& rect)
 {
     if (paintingDisabled())
         return;
-    
+
     QPainter *p = m_data->p();
     QRectF clipBounds = p->clipPath().boundingRect();
     QPainterPath newClip;
@@ -899,7 +903,7 @@ void GraphicsContext::setPlatformStrokeColor(const Color& color)
 }
 
 void GraphicsContext::setPlatformStrokeStyle(const StrokeStyle& strokeStyle)
-{   
+{
     if (paintingDisabled())
         return;
     QPainter *p = m_data->p();
