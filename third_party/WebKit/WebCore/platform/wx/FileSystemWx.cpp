90bb16085209fae2daf8cdc6f71c0b39dad79132
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-27 00:28:25 +0000

    wx build fix. Add needed wx includes for compilation. https://bugs.webkit.org/show_bug.cgi?id=18758
    git-svn-id: svn://svn.chromium.org/blink/trunk@32594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/wx/FileSystemWx.cpp
+++ third_party/WebKit/WebCore/platform/wx/FileSystemWx.cpp
@@ -34,6 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NotImplemented.h"
 #include "PlatformString.h"
 
+#include <wx/wx.h>
+#include <wx/filename.h>
+
 namespace WebCore {
 
 bool fileExists(const String& path)
