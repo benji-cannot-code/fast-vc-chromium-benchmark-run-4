14105e1297a1320dfff29070f5c08ea36d73eed5
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-28 13:44:24 +0000

    David Boddie <dboddie@trolltech.com>
    Minor documentation fix.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebhistory.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebhistory.cpp
@@ -272,8 +272,8 @@ bool QWebHistory::canGoForward() const
 }
 
 /*!
-  Set the current item to be the previous item in the history and goes to the page;
-  i.e., goes back one history item.
+  Set the current item to be the previous item in the history and goes to the
+  corresponding page; i.e., goes back one history item.
 
   \sa forward(), goToItem()
 */
@@ -285,8 +285,8 @@ void QWebHistory::back()
 }
 
 /*!
-  Sets the current item to be the next item in the history and goes to the page;
-  i.e., goes forward one history item.
+  Sets the current item to be the next item in the history and goes to the
+  corresponding page; i.e., goes forward one history item.
 
   \sa back(), goToItem()
 */
