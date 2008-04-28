a88ebf35109274978e2cc88f65d56a8fc0fbcbca
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-28 18:58:11 +0000

    Reviewed by Geoff Garen.
            <rdar://problem/5825192> Replace use of Gestalt with BSD sysctl
    
            * platform/graphics/mac/MediaPlayerPrivateQTKit.mm:
            (WebCore::MediaPlayerPrivate::isAvailable): There is no need to check QuickTime version post-Tiger,
            as it will never be below 7.3 again. This change may make library loading happen earlier in
            some cases, hopefully, that's not a big deal.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32653 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/MediaPlayerPrivateQTKit.mm
+++ third_party/WebKit/WebCore/platform/graphics/mac/MediaPlayerPrivateQTKit.mm
@@ -133,8 +133,11 @@ using namespace std;
 namespace WebCore {
 
 static const float endPointTimerInterval = 0.020f;
+
+#ifdef BUILDING_ON_TIGER
 static const long minimumQuickTimeVersion = 0x07300000; // 7.3
-    
+#endif
+
 MediaPlayerPrivate::MediaPlayerPrivate(MediaPlayer* player)
     : m_player(player)
     , m_objcObserver(AdoptNS, [[WebCoreMovieObserver alloc] initWithCallback:this])
@@ -670,9 +673,9 @@ void MediaPlayerPrivate::getSupportedTypes(HashSet<String>& types)
     
 bool MediaPlayerPrivate::isAvailable()
 {
+#ifdef BUILDING_ON_TIGER
     SInt32 version;
     OSErr result;
-    // This Carbon API is available in 64 bit too
     result = Gestalt(gestaltQuickTime, &version);
     if (result != noErr) {
         LOG_ERROR("No QuickTime available. Disabling <video> and <audio> support.");
@@ -683,6 +686,10 @@ bool MediaPlayerPrivate::isAvailable()
         return false;
     }
     return true;
+#else
+    // On 10.5 and higher, QuickTime will always be new enough for <video> and <audio> support, so we just check that the framework can be loaded.
+    return QTKitLibrary();
+#endif
 }
     
 void MediaPlayerPrivate::disableUnsupportedTracks(unsigned& enabledTrackCount)
