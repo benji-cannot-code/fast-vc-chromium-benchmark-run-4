e83a94b8974ba8945fe47165d9dda56f8d80fcbd
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-28 15:09:22 +0000

    Kavindra Devi Palaraja  <kdpalara@trolltech.com>
    Added more documentation for QWebSettings, QWebPluginFactory and QWebFrame
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32641 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpluginfactory.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpluginfactory.cpp
@@ -24,14 +24,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*!
     \class QWebPluginFactory
     \since 4.4
-    \brief The QWebPluginFactory class creates plugins to be embedded into web pages.
+    \brief The QWebPluginFactory class creates plugins to be embedded into web
+    pages.
 
-    QWebPluginFactory is a factory for creating plugins for QWebPage. A plugin factory can be
-    installed on a QWebPage using QWebPage::setPluginFactory().
+    QWebPluginFactory is a factory for creating plugins for QWebPage. A plugin
+    factory can be installed on a QWebPage using QWebPage::setPluginFactory().
 
-    You can provide a QWebPluginFactory by implementing the plugins() and the create() method. For
-    plugins() it is necessary to describe the plugins the factory can create, including a
-    description and the supported mime types. The mime types are matched with the mime type
+    You can provide a QWebPluginFactory by implementing the plugins() and the
+    create() method. For plugins() it is necessary to describe the plugins the
+    factory can create, including a description and the supported mime types. The mime types are matched with the mime type
     specified in the HTML object tag. The create() method is called if the requested mime type is
     supported. The implementation has to return a new instance of the plugin requested for the given
     mime type and the specified url.
@@ -102,7 +103,11 @@ QWebPluginFactory::~QWebPluginFactory()
 /*!
     \fn QList<Plugin> QWebPluginFactory::plugins() const = 0
 
-    Implemented in subclasses to return a list of supported plugins the factory can create.
+    This function is reimplemented in subclasses to return a list of
+    supported plugins the factory can create.
+
+    \note Currently, this function is only called when JavaScript programs
+    access the global \c plugins or \c mimetypes objects. 
 */
 
 /*!
@@ -114,7 +119,8 @@ void QWebPluginFactory::refreshPlugins()
 }
 
 /*!
-    \fn QObject *QWebPluginFactory::create(const QString &mimeType, const QUrl &url, const QStringList &argumentNames, const QStringList &argumentValues) const = 0
+    \fn QObject *QWebPluginFactory::create(const QString &mimeType, const QUrl &url,
+    const QStringList &argumentNames, const QStringList &argumentValues) const = 0
 
     Implemented in subclasses to create a new plugin that can display content of type \a mimeType.
     The url of the content is provided in \a url.
@@ -146,6 +152,8 @@ void QWebPluginFactory::refreshPlugins()
     \endtable
 
     The returned object should be a QWidget.
+
+    \note Ownership of the returned object will be transferred to the caller.
 */
 
 /*!
