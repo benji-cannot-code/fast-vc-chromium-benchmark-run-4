bf325286d001795af5deb3e0d67b30427e03f38f
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-29 08:51:42 +0000

    Ariya Hidayat <ariya.hidayat@trolltech.com>
    Ensure that relative URL is converted to absolute URL.
    
    This is necessary because loading a relative URL is not really supported
    (the web page may load, but the subsequent URLs for images and links will
    not be resolved properly).
    This also fixes https://bugs.webkit.org/show_bug.cgi?id=18484
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <qdebug.h>
 #include <qevent.h>
+#include <qfileinfo.h>
 #include <qpainter.h>
 #if QT_VERSION >= 0x040400
 #include <qnetworkrequest.h>
@@ -272,6 +273,14 @@ QString QWebFrame::title() const
     else return QString();
 }
 
+static inline QUrl ensureAbsoluteUrl(const QUrl &url)
+{
+    if (!url.isRelative())
+        return url;
+
+    return QUrl::fromLocalFile(QFileInfo(url.toLocalFile()).absoluteFilePath());
+}
+
 /*!
     \property QWebFrame::url
     \brief the url of the frame currently viewed
@@ -281,9 +290,9 @@ QString QWebFrame::title() const
 
 void QWebFrame::setUrl(const QUrl &url)
 {
-    d->frame->loader()->begin(url);
+    d->frame->loader()->begin(ensureAbsoluteUrl(url));
     d->frame->loader()->end();
-    load(url);
+    load(ensureAbsoluteUrl(url));
 }
 
 QUrl QWebFrame::url() const
@@ -329,9 +338,9 @@ QWebPage *QWebFrame::page() const
 void QWebFrame::load(const QUrl &url)
 {
 #if QT_VERSION < 0x040400
-    load(QWebNetworkRequest(url));
+    load(QWebNetworkRequest(ensureAbsoluteUrl(url)));
 #else
-    load(QNetworkRequest(url));
+    load(QNetworkRequest(ensureAbsoluteUrl(url)));
 #endif
 }
 
@@ -346,7 +355,7 @@ void QWebFrame::load(const QWebNetworkRequest &req)
     if (d->parentFrame())
         d->page->d->insideOpenCall = true;
 
-    QUrl url = req.url();
+    QUrl url = ensureAbsoluteUrl(req.url());
     QHttpRequestHeader httpHeader = req.httpHeader();
     QByteArray postData = req.postData();
 
@@ -390,7 +399,7 @@ void QWebFrame::load(const QNetworkRequest &req,
     if (d->parentFrame())
         d->page->d->insideOpenCall = true;
 
-    QUrl url = req.url();
+    QUrl url = ensureAbsoluteUrl(req.url());
 
     WebCore::ResourceRequest request(url);
 
