ee552e7ba2bef2d84b700bc9937cd2fdaed36c1a
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-29 08:47:12 +0000

    Holger Hans Peter Freyther  <zecke@selfish.org>
    Use the WebCore facility to do the scrolling. Move some code around.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32678 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage_p.h
+++ third_party/WebKit/WebKit/qt/Api/qwebpage_p.h
@@ -105,6 +105,7 @@ public:
 
     void shortcutOverrideEvent(QKeyEvent*);
     void leaveEvent(QEvent *);
+    bool handleScrolling(QKeyEvent*);
 
     WebCore::ChromeClientQt *chromeClient;
     WebCore::ContextMenuClientQt *contextMenuClient;
