208fcbb0ee732442480bccb258e8a7fa2d7a4b63
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-04-30 08:12:15 +0000

    2008-04-29  David Hyatt  <hyatt@apple.com>
            Improve the performance of masks by adding code to set more precise clips on the transparency layers
            used to paint the content under the mask, and then also avoid creating transparency layers for the masks
            themselves in common cases.
    
            Reviewed by Dan Bernstein
    
            * platform/graphics/GraphicsContext.cpp:
            (WebCore::GraphicsContext::drawTiledImage):
            Fix a bug in drawTiledImage where the composite mode got lost in the double stretch case for nine piece
            images.
    
            * rendering/InlineFlowBox.cpp:
            (WebCore::InlineFlowBox::paintFillLayers):
            (WebCore::InlineFlowBox::paintFillLayer):
            (WebCore::InlineFlowBox::paintMask):
            Optimize to only push a transparency layer for the mask if multiple images are combining.  Add a new
            composite operator argument for use when painting fill layers to do direct destination-in compositing while
            painting images.
    
    
            * rendering/InlineFlowBox.h:
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::paintMask):
            (WebCore::RenderBox::paintMaskImages):
            Optimize to only push a transparency layer for the mask if multiple images are combining.
    
            (WebCore::RenderBox::maskClipRect):
            A new method that attempts to compute a precise clip rect for the mask images.
    
            (WebCore::RenderBox::paintFillLayers):
            (WebCore::RenderBox::paintFillLayer):
            (WebCore::RenderBox::paintFillLayerExtended):
            * rendering/RenderBox.h:
            Added the composite operator argument to the paintFillLayer methods.
    
            * rendering/RenderFieldset.cpp:
            (WebCore::RenderFieldset::paintMask):
            Call the new base class paintMaskImages method for optimized mask painting.
    
            * rendering/RenderLayer.cpp:
            (WebCore::transparencyClipBox):
            Fix a coordinate space issue with transparencyClipBox when masks were used.  The border box was in the
            wrong coordinate space.  Moved the code into the layer's boundingBox() method instead.
    
            (WebCore::RenderLayer::paintLayer):
            Removed the code that always pushed a transparency layer before painting masks.
    
            (WebCore::RenderLayer::boundingBox):
            Call maskClipRect to shrink the bounding box of the layer to fit the mask clip.
    
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::paintNinePieceImage):
            Added a composite operator argument for direct destination-in painting of nine piece images.
    
            * rendering/RenderObject.h:
            (WebCore::RenderObject::maskClipRect):
            (WebCore::RenderObject::paintFillLayerExtended):
            Base class stubs for RenderBox-implemented methods.
    
            * rendering/RenderTable.cpp:
            (WebCore::RenderTable::paintMask):
            * rendering/RenderTableCell.cpp:
            (WebCore::RenderTableCell::paintMask):
            Modified to call the new paintMaskImages function.
    
            * svg/graphics/SVGImage.cpp:
            (WebCore::SVGImage::draw):
            SVG images now respect composite modes by pushing a transparency layer when the composite mode
            is anything other than source-over.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32722 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBox.h
+++ third_party/WebKit/WebCore/rendering/RenderBox.h
@@ -143,7 +143,7 @@ public:
     virtual IntRect caretRect(InlineBox*, int caretOffset, int* extraWidthToEndOfLine = 0);
 
     virtual void paintFillLayerExtended(const PaintInfo&, const Color&, const FillLayer*, int clipY, int clipHeight,
-                                        int tx, int ty, int width, int height, InlineFlowBox* = 0);
+                                        int tx, int ty, int width, int height, InlineFlowBox* = 0, CompositeOperator = CompositeSourceOver);
     IntSize calculateBackgroundSize(const FillLayer*, int scaledWidth, int scaledHeight) const;
 
     virtual int staticX() const;
@@ -166,9 +166,13 @@ public:
         setNeedsLayout(false);
     }
 
+    virtual IntRect maskClipRect();
+    
 protected:
-    void paintFillLayer(const PaintInfo&, const Color&, const FillLayer*, int clipY, int clipHeight, int tx, int ty, int width, int height);
-    void paintFillLayers(const PaintInfo&, const Color&, const FillLayer*, int clipY, int clipHeight, int tx, int ty, int width, int height);
+    void paintFillLayer(const PaintInfo&, const Color&, const FillLayer*, int clipY, int clipHeight, int tx, int ty, int width, int height, CompositeOperator = CompositeSourceOver);
+    void paintFillLayers(const PaintInfo&, const Color&, const FillLayer*, int clipY, int clipHeight, int tx, int ty, int width, int height, CompositeOperator = CompositeSourceOver);
+
+    void paintMaskImages(const PaintInfo&, int clipY, int clipHeight, int tx, int ty, int width, int height);
 
 #if PLATFORM(MAC)
     void paintCustomHighlight(int tx, int ty, const AtomicString& type, bool behindText);
