0f0360d265e0b2cdf3e79176d55c6d4c4399b751
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-01 16:32:32 +0000

    2008-05-01  Kevin McCullough  <kmccullough@apple.com>
            Reviewed by Darin.
    
            <rdar://problem/5770054> JavaScript profiler (10928)
            - Fix "sample" output so that it can be imported into Instruments
            - Also keep track of number of times a function is profiled.
    
            * JavaScriptCore.xcodeproj/project.pbxproj: Add StrHash.h which needed
            to be pulled out of identifier.cpp so that it could be used by the
            profiler and identifiers.
            * kjs/identifier.cpp: Ditto.
            * profiler/FunctionCallProfile.cpp:
            (KJS::FunctionCallProfile::printDataInspectorStyle): Inspector style
            printing should show microseconds.
            (KJS::FunctionCallProfile::printDataSampleStyle): Sample style printing
            now counts the number of times a function is in the stack tree and does
            not print microseconds since that does not make sense for a sampler.
            * profiler/FunctionCallProfile.h: Keep track of number of times a
            function is profiled.
            (KJS::FunctionCallProfile::numberOfCalls):
            * profiler/Profiler.cpp:
            (KJS::functionNameCountPairComparator): Comparator for sort function in
            printDataSampleStyle.
            (KJS::Profiler::printDataSampleStyle): Print the number of times that a
            function is listed in the stack tree in order of most times listed.
            * wtf/HashCountedSet.h: Added copyToVector since it didn't exist and is
            a more standard way to copy a HashSet to a Vector. I added on variant
            that takes a pair as the Vector's type and so the HashCountedSet simply
            fills in that pair with its internal pair, and another variant that
            takes a Vector of the type of the HashCountedSet and only fills in the
            Vector with the first element of the pair.
            (WTF::copyToVector):
            * wtf/StrHash.h: Added.
            (WTF::):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32760 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/identifier.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/identifier.cpp
@@ -29,27 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Assertions.h>
 #include <wtf/FastMalloc.h>
 #include <wtf/HashSet.h>
+//#include <wtf/StrHash.h>
 #if USE(MULTIPLE_THREADS)
 #include <wtf/ThreadSpecific.h>
 using namespace WTF;
 #endif
-
-namespace WTF {
-
-    template<typename T> struct DefaultHash;
-    template<typename T> struct StrHash;
-
-    template<> struct StrHash<KJS::UString::Rep *> {
-        static unsigned hash(const KJS::UString::Rep *key) { return key->hash(); }
-        static bool equal(const KJS::UString::Rep *a, const KJS::UString::Rep *b) { return KJS::Identifier::equal(a, b); }
-        static const bool safeToCompareToEmptyOrDeleted = false;
-    };
-
-    template<> struct DefaultHash<KJS::UString::Rep *> {
-        typedef StrHash<KJS::UString::Rep *> Hash;
-    };
-
-}
 
 namespace KJS {
 
--- third_party/WebKit/JavaScriptCore/wtf/HashCountedSet.h
+++ third_party/WebKit/JavaScriptCore/wtf/HashCountedSet.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Assertions.h"
 #include "HashMap.h"
+#include "Vector.h"
 
 namespace WTF {
 
@@ -171,6 +172,33 @@ namespace WTF {
     {
         m_impl.clear(); 
     }
+    
+    template<typename Value, typename HashFunctions, typename Traits, typename VectorType>
+    inline void copyToVector(const HashCountedSet<Value, HashFunctions, Traits>& collection, VectorType& vector)
+    {
+        typedef typename HashCountedSet<Value, HashFunctions, Traits>::const_iterator iterator;
+        
+        vector.resize(collection.size());
+        
+        iterator it = collection.begin();
+        iterator end = collection.end();
+        for (unsigned i = 0; it != end; ++it, ++i)
+            vector[i] = *it;
+    }
+
+    template<typename Value, typename HashFunctions, typename Traits>
+    inline void copyToVector(const HashCountedSet<Value, HashFunctions, Traits>& collection, Vector<Value>& vector)
+    {
+        typedef typename HashCountedSet<Value, HashFunctions, Traits>::const_iterator iterator;
+        
+        vector.resize(collection.size());
+        
+        iterator it = collection.begin();
+        iterator end = collection.end();
+        for (unsigned i = 0; it != end; ++it, ++i)
+            vector[i] = (*it).first;
+    }
+
 
 } // namespace khtml
 
