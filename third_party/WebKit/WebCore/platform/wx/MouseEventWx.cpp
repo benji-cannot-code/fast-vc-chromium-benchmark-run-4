5db6cad3273fcf87bb81ede0738f6dd9057d3762
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-02 05:16:42 +0000

    Reviewed by Eric Seidel.
    Make sure we properly set the button for all mouse events, not just mouse down, set the click count to 0 for non-click events, and finally set the timestamp.
            
    https://bugs.webkit.org/show_bug.cgi?id=18464
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32798 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/wx/MouseEventWx.cpp
+++ third_party/WebKit/WebCore/platform/wx/MouseEventWx.cpp
@@ -25,7 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+#include "Assertions.h"
 #include "PlatformMouseEvent.h"
+#include "SystemTime.h"
 
 #include <wx/defs.h>
 #include <wx/event.h>
@@ -35,7 +37,6 @@ namespace WebCore {
 PlatformMouseEvent::PlatformMouseEvent(const wxMouseEvent& event, const wxPoint& globalPoint)
     : m_position(event.GetPosition())
     , m_globalPosition(globalPoint)
-    , m_clickCount(event.ButtonDClick() ? 2 : 1)
     , m_shiftKey(event.ShiftDown())
     , m_ctrlKey(event.CmdDown())
     , m_altKey(event.AltDown())
@@ -54,12 +55,22 @@ PlatformMouseEvent::PlatformMouseEvent(const wxMouseEvent& event, const wxPoint&
     else if (type == wxEVT_MOTION)
         m_eventType = MouseEventMoved;
 
-    if (event.LeftIsDown())
+    if (event.Button(wxMOUSE_BTN_LEFT))
         m_button = LeftButton;
-    else if (event.RightIsDown())
+    else if (event.Button(wxMOUSE_BTN_RIGHT))
         m_button = RightButton;
-    else if (event.MiddleIsDown())
+    else if (event.Button(wxMOUSE_BTN_MIDDLE))
         m_button = MiddleButton;
+    else if (!m_eventType == MouseEventMoved)
+        ASSERT_NOT_REACHED();
+
+
+    if (m_eventType == MouseEventMoved)
+        m_clickCount = 0;
+    else
+        m_clickCount = event.ButtonDClick() ? 2 : 1;
+
+    m_timestamp = WebCore::currentTime();
 }
 
 }
