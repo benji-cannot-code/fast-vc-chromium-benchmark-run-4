c44193560d7e8c18e1df8c48f1400c2afc1fe582
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-02 06:24:18 +0000

    Reviewed by Kevin Ollivier.
    Implement popup menu support for wx port.
            
    https://bugs.webkit.org/show_bug.cgi?id=18776
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/PopupMenu.h
+++ third_party/WebKit/WebCore/platform/PopupMenu.h
@@ -51,6 +51,13 @@ typedef struct _GtkMenuItem GtkMenuItem;
 typedef struct _GtkWidget GtkWidget;
 #include <wtf/HashMap.h>
 #include <glib.h>
+#elif PLATFORM(WX)
+#ifdef __WXMSW__
+#include <wx/msw/winundef.h>
+#endif
+class wxMenu;
+#include <wx/defs.h>
+#include <wx/event.h>
 #endif
 
 namespace WebCore {
@@ -61,6 +68,9 @@ class PlatformScrollbar;
 class PopupMenu : public RefCounted<PopupMenu>
 #if PLATFORM(WIN)
                 , private ScrollbarClient
+#endif
+#if PLATFORM(WX)
+                , public wxEvtHandler
 #endif
 {
 public:
@@ -162,6 +172,9 @@ private:
     static void menuUnmapped(GtkWidget*, PopupMenu*);
     static void menuPositionFunction(GtkMenu*, gint*, gint*, gboolean*, PopupMenu*);
     static void menuRemoveItem(GtkWidget*, PopupMenu*);
+#elif PLATFORM(WX)
+    wxMenu* m_menu;
+    void OnMenuItemSelected(wxCommandEvent&);
 #endif
 
 };
