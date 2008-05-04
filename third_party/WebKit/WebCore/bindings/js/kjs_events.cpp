1d88ec1d54134bf4d57e1b84bef73d41526b1c04
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-04 00:48:40 +0000

    2008-05-03  Sam Weinig  <sam@webkit.org>
            Build fix.
    
            * bindings/js/kjs_events.cpp:
            (WebCore::JSAbstractEventListener::handleEvent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32846 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/kjs_events.cpp
+++ third_party/WebKit/WebCore/bindings/js/kjs_events.cpp
@@ -94,7 +94,7 @@ void JSAbstractEventListener::handleEvent(Event* ele, bool isWindowEvent)
         } else {
             JSObject* thisObj;
             if (isWindowEvent)
-                thisObj = window->wrapper();
+                thisObj = window->shell();
             else
                 thisObj = static_cast<JSObject*>(toJS(exec, event->currentTarget()));
             window->startTimeoutCheck();
