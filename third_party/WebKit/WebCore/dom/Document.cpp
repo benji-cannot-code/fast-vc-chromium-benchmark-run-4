3d9f375794603532f5c532f4a06ba2308136d621
justin.garcia@apple.com <justin.garcia@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-05 22:50:56 +0000

    WebCore:
    2008-05-05  Justin Garcia  <justin.garcia@apple.com>
    
            Reviewed by Darin.
    
            <rdar://problem/5865171> REGRESSION: Creating a new quote places caret at beginning of quote instead of the end
            
            Disable Range mutation on document modifications in Tiger and Leopard Mail,
            since they do their own mutation which interferes with ours.
    
            * WebCore.base.exp:
            * dom/Document.cpp:
            (WebCore::Document::nodeChildrenChanged):
            (WebCore::Document::nodeWillBeRemoved):
            (WebCore::Document::textInserted):
            (WebCore::Document::textRemoved):
            (WebCore::Document::textNodesMerged):
            (WebCore::Document::textNodeSplit):
            * page/Settings.cpp:
            (WebCore::Settings::Settings):
            (WebCore::Settings::disableRangeMutationForOldAppleMail):
            * page/Settings.h:
            (WebCore::Settings::rangeMutationDisabledForOldAppleMail):
    
    WebKit/mac:
    
    2008-05-05  Justin Garcia  <justin.garcia@apple.com>
    
            Reviewed by Darin.
            
            <rdar://problem/5865171> REGRESSION: Creating a new quote places caret at beginning of quote instead of the end
    
            * WebView/WebView.mm:
            (-[WebView _updateSettingsFromPreferences:]): Disable Range mutation on changes to
            the document for Tiger and Leopard Mail.  There is code in Mail that does it, and
            the two interfere.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32883 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -2495,9 +2495,11 @@ void Document::detachNodeIterator(NodeIterator *ni)
 
 void Document::nodeChildrenChanged(ContainerNode* container)
 {
-    HashSet<Range*>::const_iterator end = m_ranges.end();
-    for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != end; ++it)
-        (*it)->nodeChildrenChanged(container);
+    if (!page()->settings()->rangeMutationDisabledForOldAppleMail()) {
+        HashSet<Range*>::const_iterator end = m_ranges.end();
+        for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != end; ++it)
+            (*it)->nodeChildrenChanged(container);
+    }
 }
 
 void Document::nodeWillBeRemoved(Node* n)
@@ -2506,9 +2508,11 @@ void Document::nodeWillBeRemoved(Node* n)
     for (HashSet<NodeIterator*>::const_iterator it = m_nodeIterators.begin(); it != nodeIteratorsEnd; ++it)
         (*it)->nodeWillBeRemoved(n);
 
-    HashSet<Range*>::const_iterator rangesEnd = m_ranges.end();
-    for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != rangesEnd; ++it)
-        (*it)->nodeWillBeRemoved(n);
+    if (!page()->settings()->rangeMutationDisabledForOldAppleMail()) {
+        HashSet<Range*>::const_iterator rangesEnd = m_ranges.end();
+        for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != rangesEnd; ++it)
+            (*it)->nodeWillBeRemoved(n);
+    }
 
     if (Frame* frame = this->frame()) {
         frame->selectionController()->nodeWillBeRemoved(n);
@@ -2518,9 +2522,11 @@ void Document::nodeWillBeRemoved(Node* n)
 
 void Document::textInserted(Node* text, unsigned offset, unsigned length)
 {
-    HashSet<Range*>::const_iterator end = m_ranges.end();
-    for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != end; ++it)
-        (*it)->textInserted(text, offset, length);
+    if (!page()->settings()->rangeMutationDisabledForOldAppleMail()) {
+        HashSet<Range*>::const_iterator end = m_ranges.end();
+        for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != end; ++it)
+            (*it)->textInserted(text, offset, length);
+    }
 
     // Update the markers for spelling and grammar checking.
     shiftMarkers(text, offset, length);
@@ -2528,9 +2534,11 @@ void Document::textInserted(Node* text, unsigned offset, unsigned length)
 
 void Document::textRemoved(Node* text, unsigned offset, unsigned length)
 {
-    HashSet<Range*>::const_iterator end = m_ranges.end();
-    for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != end; ++it)
-        (*it)->textRemoved(text, offset, length);
+    if (!page()->settings()->rangeMutationDisabledForOldAppleMail()) {
+        HashSet<Range*>::const_iterator end = m_ranges.end();
+        for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != end; ++it)
+            (*it)->textRemoved(text, offset, length);
+    }
 
     // Update the markers for spelling and grammar checking.
     removeMarkers(text, offset, length);
@@ -2539,19 +2547,23 @@ void Document::textRemoved(Node* text, unsigned offset, unsigned length)
 
 void Document::textNodesMerged(Text* oldNode, unsigned offset)
 {
-    NodeWithIndex oldNodeWithIndex(oldNode);
-    HashSet<Range*>::const_iterator end = m_ranges.end();
-    for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != end; ++it)
-        (*it)->textNodesMerged(oldNodeWithIndex, offset);
+    if (!page()->settings()->rangeMutationDisabledForOldAppleMail()) {
+        NodeWithIndex oldNodeWithIndex(oldNode);
+        HashSet<Range*>::const_iterator end = m_ranges.end();
+        for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != end; ++it)
+            (*it)->textNodesMerged(oldNodeWithIndex, offset);
+    }
 
     // FIXME: This should update markers for spelling and grammar checking.
 }
 
 void Document::textNodeSplit(Text* oldNode)
 {
-    HashSet<Range*>::const_iterator end = m_ranges.end();
-    for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != end; ++it)
-        (*it)->textNodeSplit(oldNode);
+    if (!page()->settings()->rangeMutationDisabledForOldAppleMail()) {
+        HashSet<Range*>::const_iterator end = m_ranges.end();
+        for (HashSet<Range*>::const_iterator it = m_ranges.begin(); it != end; ++it)
+            (*it)->textNodeSplit(oldNode);
+    }
 
     // FIXME: This should update markers for spelling and grammar checking.
 }
--- third_party/WebKit/WebCore/page/Settings.cpp
+++ third_party/WebKit/WebCore/page/Settings.cpp
@@ -77,6 +77,7 @@ Settings::Settings(Page* page)
     , m_webArchiveDebugModeEnabled(false)
     , m_inApplicationChromeMode(false)
     , m_offlineWebApplicationCacheEnabled(false)
+    , m_rangeMutationDisabledForOldAppleMail(false)
 {
     // A Frame may not have been created yet, so we initialize the AtomicString 
     // hash before trying to use it.
@@ -339,6 +340,11 @@ void Settings::setWebArchiveDebugModeEnabled(bool enabled)
 void Settings::setLocalStorageDatabasePath(const String& path)
 {
     m_localStorageDatabasePath = path;
+}
+
+void Settings::disableRangeMutationForOldAppleMail(bool disable)
+{
+    m_rangeMutationDisabledForOldAppleMail = disable;
 }
 
 void Settings::setApplicationChromeMode(bool mode)
