d02511400175efcd53586d06648016abdb7323d2
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-06 23:32:49 +0000

    2008-05-06  Brady Eidson  <beidson@apple.com>
            Reviewed by Darin, Sam Weinig, and Anders
    
            Preparation for upcoming work making LocalStorage persistent.
    
            The final step before code that actually does storage and retrieval of LocalStorage items.
    
            The LocalStorage set is responsible for controlling the path and filenames that individual
            LocalStorageAreas will use for their persistent store.  This adds the ability to return that
            filename.
    
            Also, add the scheduling methods that LocalStorageArea will use for importing and syncing 
            it's persistent items.
    
            * storage/LocalStorage.cpp:
            (WebCore::LocalStorage::storageArea):  Add some comments re: the future direction of this
              method once we actually do quota tracking.
            (WebCore::LocalStorage::fullDatabaseFilename):
            (WebCore::LocalStorage::scheduleImport):
            (WebCore::LocalStorage::scheduleSync):
            * storage/LocalStorage.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/LocalStorage.h
+++ third_party/WebKit/WebCore/storage/LocalStorage.h
@@ -34,20 +34,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
+#include <wtf/Threading.h>
 
 namespace WebCore {
 
     class PageGroup;
     class StorageArea;
 
-    class LocalStorage : public RefCounted<LocalStorage> {
+    class LocalStorage : public ThreadSafeShared<LocalStorage> {
     public:
         static PassRefPtr<LocalStorage> create(PageGroup* group, const String& path) { return adoptRef(new LocalStorage(group, path)); }
 
         PassRefPtr<StorageArea> storageArea(Frame* sourceFrame, SecurityOrigin*);
 
-        void performImport();
-        void performSync();
+        void scheduleImport(PassRefPtr<LocalStorageArea>);
+        void scheduleSync(PassRefPtr<LocalStorageArea>);
 
         void close();
 
@@ -60,7 +61,19 @@ namespace WebCore {
         PageGroup* m_group;
         RefPtr<LocalStorageThread> m_thread;
 
+    // The following members are subject to thread synchronization issues
+    public:
+        // To be called from the background thread:
+        String fullDatabaseFilename(SecurityOrigin*);
+
+        void performImport();
+        void performSync();
+
+    private:
         String m_path;
+
+        typedef HashMap<RefPtr<SecurityOrigin>, unsigned long long, SecurityOriginHash> SecurityOriginQuoteMap;
+        SecurityOriginQuoteMap m_securityOriginQuoteMap;
     };
 
 } // namespace WebCore
