833d3b4ce1271a2fbf189eaf36678b554635b365
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-07 09:36:39 +0000

    2008-05-07  Ariya Hidayat  <ariya.hidayat@trolltech.com>
            Rubber-stamped by Oliver Hunt.
    
            Fix building without accessibility.
    
            * page/AXObjectCache.cpp:
            (WebCore::AXObjectCache::selectedChildrenChanged): proper #if
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32947 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/AXObjectCache.cpp
+++ third_party/WebKit/WebCore/page/AXObjectCache.cpp
@@ -181,9 +181,11 @@ void AXObjectCache::childrenChanged(RenderObject* renderer)
         obj->childrenChanged();
 }
 
+#if HAVE(ACCESSIBILITY)
 void AXObjectCache::selectedChildrenChanged(RenderObject* renderer)
 {
     postNotificationToElement(renderer, "AXSelectedChildrenChanged");
 }
+#endif
 
 } // namespace WebCore
