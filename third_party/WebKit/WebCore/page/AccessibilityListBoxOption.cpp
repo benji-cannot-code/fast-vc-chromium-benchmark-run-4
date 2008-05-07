de75ca0775def5907a3de960d04c658a50f430b4
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-07 01:21:22 +0000

    2008-05-06  Alp Toker  <alp@nuanti.com>
            Partial GTK+ build fix. Add files from r32925 to the build and replace
            'nil' return with 0.
    
            * GNUmakefile.am:
            * page/AccessibilityListBoxOption.cpp:
            (WebCore::AccessibilityListBoxOption::parentObject):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32932 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/AccessibilityListBoxOption.cpp
+++ third_party/WebKit/WebCore/page/AccessibilityListBoxOption.cpp
@@ -134,7 +134,7 @@ AccessibilityObject* AccessibilityListBoxOption::parentObject() const
 {
     HTMLSelectElement* parentNode = listBoxOptionParentNode();
     if (!parentNode)
-        return nil;
+        return 0;
     
     return m_optionElement->document()->axObjectCache()->get(parentNode->renderer());
 }
