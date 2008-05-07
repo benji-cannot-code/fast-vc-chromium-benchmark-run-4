bcba4c05fc1b7e173e4cf4eec089f79a35693482
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-07 01:27:53 +0000

    2008-05-06  Anders Carlsson  <andersca@apple.com>
            Reviewed by Brady.
    
            Support reading back app caches from the datbase.
        
            * loader/appcache/ApplicationCache.cpp:
            (WebCore::ApplicationCache::ApplicationCache):
            Initialize m_storageID to 0.
            
            * loader/appcache/ApplicationCacheGroup.cpp:
            (WebCore::ApplicationCacheGroup::cacheDestroyed):
            If the cache being destroyed is not the newest cache, it should no longer be 
            stored in the database. Remove it.
            
            (WebCore::ApplicationCacheGroup::setNewestCache):
            Don't store the cache here.
            
            (WebCore::ApplicationCacheGroup::checkIfLoadIsComplete):
            Store it here instead.
            
            * loader/appcache/ApplicationCacheStorage.cpp:
            (WebCore::ApplicationCacheStorage::loadCacheGroup):
            New method that loads a cache group with a given manifest URL (or returns 0 if the load fails).
            
            (WebCore::ApplicationCacheStorage::findOrCreateCacheGroup):
            Search for the group in the database.
            
            (WebCore::ApplicationCacheStorage::loadManifestHostHashes):
            New method that loads the host hashes from the database.
            
            (WebCore::ApplicationCacheStorage::cacheGroupForURL):
            Search in the database for a cache that contains the resource.
            
            (WebCore::ApplicationCacheStorage::loadCache):
            New method that loads a cache with a given ID.
            
            (WebCore::ApplicationCacheStorage::remove):
            New method that removes a cache.
            
            * loader/appcache/ApplicationCacheStorage.h:
            Add definitions.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32933 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/appcache/ApplicationCacheGroup.cpp
+++ third_party/WebKit/WebCore/loader/appcache/ApplicationCacheGroup.cpp
@@ -250,6 +250,9 @@ void ApplicationCacheGroup::cacheDestroyed(ApplicationCache* cache)
     
     m_caches.remove(cache);
     
+    if (cache != newestCache())
+        cacheStorage().remove(cache);
+
     if (m_caches.isEmpty())
         delete this;
 }
@@ -263,9 +266,6 @@ void ApplicationCacheGroup::setNewestCache(PassRefPtr<ApplicationCache> newestCa
     m_newestCache = newestCache; 
     m_caches.add(m_newestCache.get());
     m_newestCache->setGroup(this);
-
-    // Store the cache 
-    cacheStorage().storeNewestCache(this);
 }
 
 void ApplicationCacheGroup::update(Frame* frame)
@@ -529,6 +529,9 @@ void ApplicationCacheGroup::checkIfLoadIsComplete()
     }
     
     setNewestCache(m_cacheBeingUpdated.release());
+        
+    // Store the cache 
+    cacheStorage().storeNewestCache(this);
     
     callListeners(isUpgradeAttempt ? &DOMApplicationCache::callUpdateReadyListener : &DOMApplicationCache::callCachedListener, 
                   documentLoaders);
