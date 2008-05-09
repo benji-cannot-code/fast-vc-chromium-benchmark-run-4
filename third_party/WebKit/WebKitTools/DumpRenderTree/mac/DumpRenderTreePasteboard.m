842c742ad4bbace62d1797ad3fdf731dfda83d38
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-09 06:16:11 +0000

    Another attempt at a Tiger build fix.
    Use DumpRenderTreeMac.h rather than DumpRenderTree.h as DumpRenderTreePasteboard is not an Obj-C++ file.
    
    * DumpRenderTree/mac/DumpRenderTreePasteboard.m:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@32997 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/DumpRenderTreePasteboard.m
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/DumpRenderTreePasteboard.m
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "DumpRenderTree.h"
+#import "DumpRenderTreeMac.h"
 #import "DumpRenderTreePasteboard.h"
 
 #import <WebKit/WebTypesInternal.h>
