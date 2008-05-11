51ab327f930afdbd4092d0f31d3ea4495d073ed1
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-11 16:36:23 +0000

    2008-05-11  Julien Chaffraix  <jchaffraix@webkit.org>
            Debug build fix.
    
            * loader/appcache/ApplicationCache.cpp: Include stdio.h
            as we are using printf in ApplicationCache::dump().
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33032 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/appcache/ApplicationCache.cpp
+++ third_party/WebKit/WebCore/loader/appcache/ApplicationCache.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ApplicationCacheResource.h"
 #include "ApplicationCacheStorage.h"
 #include "ResourceRequest.h"
+#include <stdio.h>
 
 namespace WebCore {
  
