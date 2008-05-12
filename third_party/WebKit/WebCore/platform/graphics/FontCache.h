8e5fdd3d2fc89c165a4a6c1523ae74ad516f4ccf
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-12 22:23:04 +0000

    2008-05-12  Alp Toker  <alp@nuanti.com>
            GTK+ build fix for breakage in r33056. Include limits.h for INT_MAX.
    
            * platform/graphics/FontCache.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/FontCache.h
+++ third_party/WebKit/WebCore/platform/graphics/FontCache.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef FontCache_h
 #define FontCache_h
 
+#include <limits.h>
 #include <wtf/unicode/Unicode.h>
 
 #if PLATFORM(WIN)
