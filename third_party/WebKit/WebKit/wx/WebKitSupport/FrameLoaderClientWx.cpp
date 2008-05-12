d9d13f4a3d06a58dd99c342bf86d8cb2892ad30c
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-12 05:14:53 +0000

    Previous commit made bdash sad. Restore happiness state by fixing missed style issue.
    git-svn-id: svn://svn.chromium.org/blink/trunk@33037 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/wx/WebKitSupport/FrameLoaderClientWx.cpp
+++ third_party/WebKit/WebKit/wx/WebKitSupport/FrameLoaderClientWx.cpp
@@ -869,9 +869,9 @@ void FrameLoaderClientWx::transitionToCommittedForNewPage()
     m_frame->setView(0);
 
     FrameView* frameView;
-    if (isMainFrame) {
+    if (isMainFrame)
         frameView = new FrameView(m_frame, IntRect(m_webView->GetRect()).size());
-    } else
+    else
         frameView = new FrameView(m_frame);
 
     ASSERT(frameView);
