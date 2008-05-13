1edf77507dc26f6e93b49ec11a6f85154ede63f9
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-13 22:31:22 +0000

    Fixes the assertion and crash that would happen when inspecting a element from a frame. This change makes JSInspectedObjectWrapper pass unwrapped objects around for global objects that share the same page group identifier. Also returns jsUndefined() instead of 0 to prevent crashing in release builds if the page groups don't match.
    Passes all the tests in: manual-tests/inspector-wrappers
    
    Reviewed by Adam Roben.
    
    * bindings/js/JSInspectedObjectWrapper.cpp:
    (WebCore::JSInspectedObjectWrapper::prepareIncomingValue): Return jsUndefined() instead of 0.
    Call allowsUnwrappedAccessFrom instead of unwrappedExecStateMatches.
    * bindings/js/JSQuarantinedObjectWrapper.cpp:
    (WebCore::JSQuarantinedObjectWrapper::allowsUnwrappedAccessFrom): Renamed from unwrappedExecStateMatches.
    Return true if the pageGroupIdentifier of both wrappers match.
    (WebCore::JSQuarantinedObjectWrapper::callAsFunction): Return jsUndefined() instead of 0.
    * bindings/js/JSQuarantinedObjectWrapper.h: Renamed unwrappedExecStateMatches to allowsUnwrappedAccessFrom.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33414 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSQuarantinedObjectWrapper.h
+++ third_party/WebKit/WebCore/bindings/js/JSQuarantinedObjectWrapper.h
@@ -40,7 +40,7 @@ namespace WebCore {
         KJS::JSObject* unwrappedObject() const { return m_unwrappedObject; }
         KJS::ExecState* unwrappedExecState() const;
 
-        bool unwrappedExecStateMatches(const KJS::ExecState*) const;
+        bool allowsUnwrappedAccessFrom(const KJS::ExecState*) const;
 
         virtual bool getOwnPropertySlot(KJS::ExecState*, const KJS::Identifier&, KJS::PropertySlot&);
         virtual bool getOwnPropertySlot(KJS::ExecState*, unsigned, KJS::PropertySlot&);
