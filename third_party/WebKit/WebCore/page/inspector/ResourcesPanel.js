616952a9e26a19c316b339312879bfe9b224b1dd
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-13 22:30:05 +0000

    Factored most of SourceView out into SourceFrame so it can be shared by the new ScriptView. Added the ScriptView class to be used for scripts that arn't Resources (like eval code.) Added a simple Script object that hold the data from the debugger parsedSource hooks. A ScriptView holds a Script object, and uses it for source data.
    Added breakpoint and execution line support to the SourceFrame
    where they are visually represented in the source.
    
    Reviewed by Kevin McCullough.
    
    * page/inspector/inspector.js:
    (WebInspector.performSearch): Change the caller of sourceFrameForResource
    to use the SourceFrame result's element property.
    * page/inspector/ResourcesPanel.js: Use the new SourceFrame.
    * page/inspector/Script.js: Added.
    * page/inspector/ScriptView.js: Added.
    * page/inspector/SourceFrame.js: Added.
    * page/inspector/SourceView.js: Use the new SourceFrame.
    * WebCore.vcproj/WebCore.vcproj: Add new files.
    * page/inspector/WebKit.qrc: Ditto.
    * page/inspector/inspector.html: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/ResourcesPanel.js
+++ third_party/WebKit/WebCore/page/inspector/ResourcesPanel.js
@@ -315,7 +315,7 @@ WebInspector.ResourcesPanel.prototype = {
 
         resource._resourcesTreeElement.updateErrorsAndWarnings();
 
-        var view = this._resourceView(resource);
+        var view = this.resourceViewForResource(resource);
         if (view.addMessage)
             view.addMessage(msg);
     },
@@ -389,15 +389,15 @@ WebInspector.ResourcesPanel.prototype = {
 
         resource._resourcesTreeElement.updateErrorsAndWarnings();
 
+        var oldView = resource._resourcesView;
+
         resource._resourcesView.detach();
         delete resource._resourcesView;
 
         resource._resourcesView = newView;
 
-        if (resource !== this.visibleResource)
-            return;
-
-        newView.show();
+        if (oldView.visible && oldView.element.parentNode)
+            newView.show(oldView.element.parentNode);
     },
 
     showResource: function(resource, line)
@@ -410,11 +410,11 @@ WebInspector.ResourcesPanel.prototype = {
         if (this.visibleResource && this.visibleResource._resourcesView)
             this.visibleResource._resourcesView.hide();
 
-        var view = this._resourceView(resource);
-        view.show();
+        var view = this.resourceViewForResource(resource);
+        view.show(this.resourceViews);
 
-        if (line && view.showLine)
-            view.showLine(line);
+        if (line && view.revealLine)
+            view.revealLine(line);
 
         if (resource._resourcesTreeElement) {
             resource._resourcesTreeElement.reveal();
@@ -441,9 +441,18 @@ WebInspector.ResourcesPanel.prototype = {
         this._updateSidebarWidth();
     },
 
+    resourceViewForResource: function(resource)
+    {
+        if (!resource)
+            return null;
+        if (!resource._resourcesView)
+            resource._resourcesView = this._createResourceView(resource);
+        return resource._resourcesView;
+    },
+
     sourceFrameForResource: function(resource)
     {
-        var view = this._resourceView(resource);
+        var view = this.resourceViewForResource(resource);
         if (!view)
             return null;
 
@@ -455,7 +464,7 @@ WebInspector.ResourcesPanel.prototype = {
             this.attach();
 
         view.setupSourceFrameIfNeeded();
-        return view.frameElement;
+        return view.sourceFrame;
     },
 
     handleKeyEvent: function(event)
@@ -968,15 +977,6 @@ WebInspector.ResourcesPanel.prototype = {
     {
         var selectedOption = this.sortingSelectElement[this.sortingSelectElement.selectedIndex];
         this.sortingFunction = selectedOption.sortingFunction;
-    },
-
-    _resourceView: function(resource)
-    {
-        if (!resource)
-            return null;
-        if (!resource._resourcesView)
-            resource._resourcesView = this._createResourceView(resource);
-        return resource._resourcesView;
     },
 
     _createResourceView: function(resource)
