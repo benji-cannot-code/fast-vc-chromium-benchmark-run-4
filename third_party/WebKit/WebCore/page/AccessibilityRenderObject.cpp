7a1b48401a26adebf66acac6c1370fd841158cc0
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-13 23:55:41 +0000

    <rdar://problem/5932677> AX: Submit buttons aren't returning AXTitle
    git-svn-id: svn://svn.chromium.org/blink/trunk@33420 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/AccessibilityRenderObject.cpp
+++ third_party/WebKit/WebCore/page/AccessibilityRenderObject.cpp
@@ -623,9 +623,6 @@ String AccessibilityRenderObject::title() const
     if (!ariaLabel.isEmpty())
         return ariaLabel;
     
-    if (roleValue() == ButtonRole)
-        return textUnderElement();
-    
     bool isInputTag = node->hasTagName(inputTag);
     if (isInputTag) {
         HTMLInputElement* input = static_cast<HTMLInputElement*>(node);
@@ -638,6 +635,9 @@ String AccessibilityRenderObject::title() const
         if (label)
             return label->innerText();
     }
+    
+    if (roleValue() == ButtonRole)
+        return textUnderElement();
     
     if (isLink()) {
         Element* element = static_cast<Element*>(node);    
