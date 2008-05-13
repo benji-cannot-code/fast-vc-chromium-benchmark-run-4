cb88d65af733ee96808d663b94ad95b3752b79d4
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-13 22:29:19 +0000

    Implements more debugger APIs on JavaScriptDebugServer and reduces the number of callbacks to JavaScriptDebugListeners. These changes will better facilitate debugger optimizations when SquirrelFish merges.
    Reviewed by Kevin McCullough.
    
    * page/InspectorController.cpp:
    (WebCore::InspectorController::didParseSource): Removed the ExecState.
    (WebCore::InspectorController::failedToParseSource): Ditto.
    (WebCore::InspectorController::didPause): Added.
    * page/InspectorController.h: Changed the JavaScriptDebugListener functions.
    * page/JavaScriptDebugListener.h: Removed some callbacks and added didPause.
    * page/JavaScriptDebugServer.cpp:
    (WebCore::JavaScriptDebugServer::JavaScriptDebugServer): Initialize new data members.
    (WebCore::JavaScriptDebugServer::~JavaScriptDebugServer): Delete all values of m_breakpoints.
    (WebCore::JavaScriptDebugServer::removeListener): Call resume if the last listener was removed.
    (WebCore::JavaScriptDebugServer::hasListenersInterestedInPage): Returns true if there are any
    global listeners or a listener for the page.
    (WebCore::JavaScriptDebugServer::addBreakpoint): Adds and entry to m_breakpoints.
    (WebCore::JavaScriptDebugServer::removeBreakpoint): Removes a entry in m_breakpoints.
    (WebCore::JavaScriptDebugServer::hasBreakpoint):  Checks if there is a breakpoint for the
    sourceID and line.
    (WebCore::JavaScriptDebugServer::clearBreakpoints): Removed all breakpoints.
    (WebCore::JavaScriptDebugServer::pauseOnNextStatement): Sets m_pauseOnNextStatement to true.
    (WebCore::JavaScriptDebugServer::resume): Sets m_paused to false.
    (WebCore::JavaScriptDebugServer::stepIntoStatement): Calls resume and sets
    m_pauseOnNextStatement to true.
    (WebCore::JavaScriptDebugServer::stepOverStatement): Calls resume and sets m_pauseOnExecState to
    the current call frame's ExecState.
    (WebCore::JavaScriptDebugServer::stepOutOfFunction): Calls resume and sets m_pauseOnExecState to
    the current call frame's caller ExecState.
    (WebCore::JavaScriptDebugServer::currentCallFrame): Returns m_currentCallFrame if paused.
    (WebCore::dispatchDidParseSource): Removed the ExecState argument.
    (WebCore::dispatchFailedToParseSource): Ditto.
    (WebCore::JavaScriptDebugServer::sourceParsed): Doesn't pass the ExecState to dispatchDidParseSource
    or dispatchFailedToParseSource.
    (WebCore::dispatchFunctionToListeners):
    (WebCore::JavaScriptDebugServer::dispatchFunctionToListeners): Removes all the arguments passed
    to the callback.
    (WebCore::JavaScriptDebugServer::setJavaScriptPaused): Various overloaded functions
    to pause parts of WebCore to prevent JavaScript execution while paused.
    (WebCore::JavaScriptDebugServer::pauseIfNeeded): Decides if the debugger should pause based
    on the passed in ExecState, source ID and line number. This checks for breakpoints, stepping, etc.
    Calls didPause on all the listeners and spins a EventLoop until resume is called.
    (WebCore::JavaScriptDebugServer::callEvent): Call pauseIfNeeded.
    (WebCore::JavaScriptDebugServer::atStatement): Ditto.
    (WebCore::JavaScriptDebugServer::returnEvent): Ditto.
    (WebCore::JavaScriptDebugServer::exception): Ditto.
    * page/JavaScriptDebugServer.h: Added new functions.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33404 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/JavaScriptDebugServer.h
+++ third_party/WebKit/WebCore/page/JavaScriptDebugServer.h
@@ -34,11 +34,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
+#include <wtf/RefPtr.h>
 
 namespace WebCore {
 
     class Frame;
+    class FrameView;
     class Page;
+    class PageGroup;
+    class PausedTimeouts;
+    class JavaScriptCallFrame;
     class JavaScriptDebugListener;
 
     class JavaScriptDebugServer : KJS::Debugger {
@@ -51,18 +56,39 @@ namespace WebCore {
         void addListener(JavaScriptDebugListener*, Page*);
         void removeListener(JavaScriptDebugListener*, Page*);
 
+        void addBreakpoint(int sourceID, unsigned lineNumber);
+        void removeBreakpoint(int sourceID, unsigned lineNumber);
+        bool hasBreakpoint(int sourceID, unsigned lineNumber) const;
+        void clearBreakpoints();
+
+        void pauseOnNextStatement();
+        void resume();
+
+        void stepIntoStatement();
+        void stepOverStatement();
+        void stepOutOfFunction();
+
+        JavaScriptCallFrame* currentCallFrame();
+
         void pageCreated(Page*);
 
         typedef HashSet<JavaScriptDebugListener*> ListenerSet;
-        typedef void (JavaScriptDebugListener::*JavaScriptExecutionCallback)(KJS::ExecState*, int sourceID, int lineNumber);
+        typedef void (JavaScriptDebugListener::*JavaScriptExecutionCallback)();
 
     private:
         JavaScriptDebugServer();
         ~JavaScriptDebugServer();
 
         bool hasListeners() const { return !m_listeners.isEmpty() || !m_pageListenersMap.isEmpty(); }
+        bool hasListenersInterestedInPage(Page*);
 
-        void dispatchFunctionToListeners(JavaScriptExecutionCallback, KJS::ExecState*, int sourceID, int lineNumber);
+        void setJavaScriptPaused(const PageGroup&, bool paused);
+        void setJavaScriptPaused(Page*, bool paused);
+        void setJavaScriptPaused(Frame*, bool paused);
+        void setJavaScriptPaused(FrameView*, bool paused);
+
+        void dispatchFunctionToListeners(JavaScriptExecutionCallback, KJS::ExecState*);
+        void pauseIfNeeded(KJS::ExecState* exec, int sourceID, int lineNumber);
 
         virtual bool sourceParsed(KJS::ExecState*, int sourceID, const KJS::UString& sourceURL, const KJS::UString& source, int startingLineNumber, int errorLine, const KJS::UString& errorMsg);
         virtual bool callEvent(KJS::ExecState*, int sourceID, int lineNumber, KJS::JSObject* function, const KJS::List& args);
@@ -74,6 +100,12 @@ namespace WebCore {
         PageListenersMap m_pageListenersMap;
         ListenerSet m_listeners;
         bool m_callingListeners;
+        bool m_pauseOnNextStatement;
+        bool m_paused;
+        KJS::ExecState* m_pauseOnExecState;
+        RefPtr<JavaScriptCallFrame> m_currentCallFrame;
+        HashMap<RefPtr<Frame>, PausedTimeouts*> m_pausedTimeouts;
+        HashMap<int, HashSet<unsigned>*> m_breakpoints;
     };
 
 } // namespace WebCore
