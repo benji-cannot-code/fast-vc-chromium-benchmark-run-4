7cf67cf8cbf99a5ec73db95581793e1ed953dbf9
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-13 22:29:31 +0000

    Add debugger related functions to the InspectorController and expose them to the JavaScript side of InspectorController.
    Reviewed by Geoff Garen.
    
    * page/InspectorController.cpp:
    (WebCore::jsStringRef): Added helper to convert a UString to a JSStringRef.
    (WebCore::currentCallFrame): Call InspectorController::currentCallFrame.
    (WebCore::pauseInDebugger): Call InspectorController::pauseInDebugger.
    (WebCore::resumeDebugger): Call InspectorController::resumeDebugger.
    (WebCore::stepOverStatementInDebugger): Call InspectorController::stepOverStatementInDebugger.
    (WebCore::stepIntoStatementInDebugger): Call InspectorController::stepIntoStatementInDebugger.
    (WebCore::stepOutOfFunctionInDebugger): Call InspectorController::stepOutOfFunctionInDebugger.
    (WebCore::addBreakpoint): Call InspectorController::addBreakpoint.
    (WebCore::removeBreakpoint): Call InspectorController::removeBreakpoint.
    (WebCore::InspectorController::windowScriptObjectAvailable): Add the new functions to the
    InspectorController JavaScript class.
    (WebCore::InspectorController::startDebuggingAndReloadInspectedPage): Clear breakpoints.
    (WebCore::InspectorController::currentCallFrame): Return the currentCallFrame from the
    JavaScriptDebugServer.
    (WebCore::InspectorController::pauseInDebugger): Call JavaScriptDebugServer's pauseOnNextStatement.
    (WebCore::InspectorController::resumeDebugger): Call JavaScriptDebugServer's resume.
    (WebCore::InspectorController::stepOverStatementInDebugger):  Call JavaScriptDebugServer's stepOverStatement.
    (WebCore::InspectorController::stepIntoStatementInDebugger): Call JavaScriptDebugServer's stepIntoStatement.
    (WebCore::InspectorController::stepOutOfFunctionInDebugger): Call JavaScriptDebugServer's stepOutOfFunction.
    (WebCore::InspectorController::addBreakpoint): Call JavaScriptDebugServer's addBreakpoint.
    (WebCore::InspectorController::removeBreakpoint): Call JavaScriptDebugServer's removeBreakpoint.
    (WebCore::InspectorController::didParseSource): Call into the Inspector's JavaScript.
    (WebCore::InspectorController::failedToParseSource): Ditto.
    (WebCore::InspectorController::didPause): Ditto.
    * page/InspectorController.h: Add new functions.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33405 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/InspectorController.h
+++ third_party/WebKit/WebCore/page/InspectorController.h
@@ -48,6 +48,7 @@ class Database;
 class DocumentLoader;
 class GraphicsContext;
 class InspectorClient;
+class JavaScriptCallFrame;
 class Node;
 class Page;
 class ResourceResponse;
@@ -134,6 +135,18 @@ public:
     void startDebuggingAndReloadInspectedPage();
     void stopDebugging();
     bool debuggerAttached() const { return m_debuggerAttached; }
+
+    JavaScriptCallFrame* currentCallFrame() const;
+
+    void addBreakpoint(int sourceID, unsigned lineNumber);
+    void removeBreakpoint(int sourceID, unsigned lineNumber);
+
+    void pauseInDebugger();
+    void resumeDebugger();
+
+    void stepOverStatementInDebugger();
+    void stepIntoStatementInDebugger();
+    void stepOutOfFunctionInDebugger();
 
     void drawNodeHighlight(GraphicsContext&) const;
 
