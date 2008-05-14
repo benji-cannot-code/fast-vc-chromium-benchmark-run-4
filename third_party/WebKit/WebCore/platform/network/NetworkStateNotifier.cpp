7f9d39546022e875904fce0cc981199cd12e6050
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-14 22:35:52 +0000

    Windows build fix
            * platform/network/NetworkStateNotifier.cpp: Added a missing #include.
            * platform/network/NetworkStateNotifier.h:
            (WebCore::NetworkStateNotifier::NetworkStateNotifier): Removed 'void'
            return type from the constructor, and fixed the spelling of a member
            variable.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33461 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/NetworkStateNotifier.cpp
+++ third_party/WebKit/WebCore/platform/network/NetworkStateNotifier.cpp
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "NetworkStateNotifier.h"
 
+#include <wtf/Assertions.h>
+
 namespace WebCore {
 
 NetworkStateNotifier& networkStateNotifier()
