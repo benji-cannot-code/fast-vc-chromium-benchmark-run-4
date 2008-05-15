f99f563776dd163975b781e25c69406a74d48772
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-15 17:02:40 +0000

    Tiger build fix.
            * Misc/WebNSAttributedStringExtras.mm: Import WebTypesInternal.h for NSUInteger.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33490 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Misc/WebNSAttributedStringExtras.mm
+++ third_party/WebKit/WebKit/mac/Misc/WebNSAttributedStringExtras.mm
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebDataSourcePrivate.h"
 #import "WebFrame.h"
 #import "WebFrameInternal.h"
+#import "WebTypesInternal.h"
 #import <WebCore/BlockExceptions.h>
 #import <WebCore/ColorMac.h>
 #import <WebCore/CSSHelper.h>
