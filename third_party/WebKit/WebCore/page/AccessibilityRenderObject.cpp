3e35f48adb4e8a0f987b32b434f9d74425c229ed
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-17 00:28:11 +0000

    <rdar://problem/5582483> Mail compose area needs to allow AXValue to be settable
    git-svn-id: svn://svn.chromium.org/blink/trunk@33542 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/AccessibilityRenderObject.cpp
+++ third_party/WebKit/WebCore/page/AccessibilityRenderObject.cpp
@@ -292,6 +292,19 @@ bool AccessibilityRenderObject::isMultiSelect() const
 bool AccessibilityRenderObject::isReadOnly() const
 {
     ASSERT(m_renderer);
+    
+    if (isWebArea()) {
+        Document* document = m_renderer->document();
+        if (!document)
+            return true;
+        
+        Frame* frame = document->frame();
+        if (!frame)
+            return true;
+        
+        return !frame->isContentEditable();
+    }
+
     return !m_renderer->node() || !m_renderer->node()->isContentEditable();
 }
 
@@ -1853,6 +1866,9 @@ bool AccessibilityRenderObject::canSetFocusAttribute() const
 
 bool AccessibilityRenderObject::canSetValueAttribute() const
 {
+    if (isWebArea()) 
+        return !isReadOnly();
+
     return isTextControl() || isProgressIndicator();
 }
 
