e90821075b89cc53e2bd87223a2802247f76f7ff
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-20 15:40:56 +0000

    wx build fixes for PluginViewWx.cpp and WebKit/wx/WebView.cpp
    git-svn-id: svn://svn.chromium.org/blink/trunk@33932 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/wx/WebView.cpp
+++ third_party/WebKit/WebKit/wx/WebView.cpp
@@ -274,7 +274,7 @@ wxString wxWebView::GetPageSource()
         WebCore::Document* doc = m_impl->frame->document();
         
         if (doc) {
-            wxString source = doc->toString();
+            wxString source = createMarkup(doc);
             return source;
         }
     }
