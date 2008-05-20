5b5ba9b4e10a415544d39b3af12e61c40b73be0e
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-20 17:12:10 +0000

    Rename sortFileName{Ascending,Descending} to sortFunctionName{Ascending,Descending}.
    Reviewed by Kevin McCullough.
    
    * JavaScriptCore.exp:
    * kjs/config.h:
    * profiler/Profile.h:
    * profiler/ProfileNode.cpp:
    (KJS::functionNameDescendingComparator):
    (KJS::ProfileNode::sortFunctionNameDescending):
    (KJS::functionNameAscendingComparator):
    (KJS::ProfileNode::sortFunctionNameAscending):
    * profiler/ProfileNode.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33935 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/profiler/ProfileNode.h
+++ third_party/WebKit/JavaScriptCore/profiler/ProfileNode.h
@@ -70,8 +70,8 @@ namespace KJS {
         void sortSelfTimeAscending();
         void sortCallsDescending();
         void sortCallsAscending();
-        void sortFileNameDescending();
-        void sortFileNameAscending();
+        void sortFunctionNameDescending();
+        void sortFunctionNameAscending();
 
         void endAndRecordCall();
 
