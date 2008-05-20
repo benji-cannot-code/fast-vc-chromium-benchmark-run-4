376780d753aea05f6168d2f784e4a14e47a36314
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-20 23:05:21 +0000

    2008-05-20  Kevin McCullough  <kmccullough@apple.com>
            Reviewed by Adam.
    
            <rdar://problem/5950538> JSProfiler: Keep track of non-JS execution time
            We now have an extra node that represents the excess non-JS time.
            - Also changed "SCRIPT" and "anonymous function" to be more consistent
            with the debugger.
    
            * profiler/ProfileNode.cpp:
            (KJS::ProfileNode::stopProfiling): If this ProfileNode is the head node
            create a new child that has the excess execution time.
            (KJS::ProfileNode::calculatePercentages): Moved calculation of the
            percentages into a function since it's called from multiple places.
            * profiler/ProfileNode.h: Add the newly needed functions used above.
            (KJS::ProfileNode::setTotalTime):
            (KJS::ProfileNode::setSelfTime):
            (KJS::ProfileNode::setNumberOfCalls):
            * profiler/Profiler.cpp: renamed "SCRIPT" and "anonymous function" to be
            consistent with the debugger and use constants that can be localized
            more easily.
            (KJS::getCallIdentifiers):
            (KJS::getCallIdentifierFromFunctionImp):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/profiler/ProfileNode.cpp
+++ third_party/WebKit/JavaScriptCore/profiler/ProfileNode.cpp
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace KJS {
 
+static const char* NonJSExecution = "(non-JavaScript)";
+
 ProfileNode::ProfileNode(const CallIdentifier& callIdentifier)
     : m_callIdentifier(callIdentifier)
     , m_totalTime (0.0)
@@ -113,8 +115,19 @@ void ProfileNode::stopProfiling(double totalProfileTime, bool headProfileNode)
     ASSERT(m_selfTime <= m_totalTime);
     m_selfTime = m_totalTime - m_selfTime;
 
-    m_totalPercent = (m_totalTime / totalProfileTime) * 100.0;
-    m_selfPercent = (m_selfTime / totalProfileTime) * 100.0;
+    if (headProfileNode && m_selfTime) {
+        RefPtr<ProfileNode> idleNode = ProfileNode::create(CallIdentifier(NonJSExecution, 0, 0));
+
+        idleNode->setTotalTime(m_selfTime);
+        idleNode->setSelfTime(m_selfTime);
+        idleNode->setNumberOfCalls(0);
+        idleNode->calculatePercentages(totalProfileTime);
+
+        addChild(idleNode.release());
+        m_selfTime = 0.0;
+    }
+
+    calculatePercentages(totalProfileTime);
 }
 
 #pragma mark Sorting methods
@@ -230,6 +243,13 @@ void ProfileNode::endAndRecordCall()
 
     ++m_numberOfCalls;
 }
+
+void ProfileNode::calculatePercentages(double totalProfileTime)
+{
+    m_totalPercent = (m_totalTime / totalProfileTime) * 100.0;
+    m_selfPercent = (m_selfTime / totalProfileTime) * 100.0;
+}
+
 
 void ProfileNode::printDataInspectorStyle(int indentLevel) const
 {
