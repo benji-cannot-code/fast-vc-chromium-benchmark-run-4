a323e635aa25972d580e638361aacfc7e98782aa
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-22 03:44:21 +0000

    2008-05-21  Alp Toker  <alp@nuanti.com>
            GTK+/UNIX testkjs build fix. Include signal.h.
    
            * kjs/testkjs.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@33994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
@@ -45,6 +45,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/time.h>
 #endif
 
+#if PLATFORM(UNIX)
+#include <signal.h>
+#endif
+
 #if PLATFORM(WIN_OS)
 #include <crtdbg.h>
 #include <windows.h>
