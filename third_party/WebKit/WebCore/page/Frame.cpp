8052f003fadfbc2e2d5d3497013dbc3f951f88d1
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-22 09:36:19 +0000

    2008-05-22  Stephanie Lewis  <slewis@apple.com>
            Reviewed by Dan.
    
            Fix <rdar://problem/5952405>.  The unbeforeunload count cannot be cleared when it is dispatched because it is possible to be called again.  Clear it after the unload event is dispatched.
    
            Test: fast/events/onunload-clears-onbeforeunload.html
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::stopLoading):
            * page/Frame.cpp:
            (WebCore::Frame::shouldClose):
    
            implement the beforeUnload UI delegate so that DRT will dispatch beforeunload events.
    
            * DumpRenderTree/mac/UIDelegate.mm:
            (-[UIDelegate webView:runBeforeUnloadConfirmPanelWithMessage:initiatedByFrame:]):
            * DumpRenderTree/win/UIDelegate.cpp:
            (SearchableWebViewHost::runBeforeUnloadConfirmPanelWithMessage):
    
            add test to test for beforeunload event handlers being removed after they have been fired.
    
            * fast/events/onunload-clears-onbeforeunload-expected.txt: Added.
            * fast/events/onunload-clears-onbeforeunload.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -1805,9 +1805,6 @@ bool Frame::shouldClose()
     RefPtr<BeforeUnloadEvent> beforeUnloadEvent = new BeforeUnloadEvent;
     beforeUnloadEvent->setTarget(doc);
     doc->handleWindowEvent(beforeUnloadEvent.get(), false);
-    
-    if (eventHandler()->pendingFrameBeforeUnloadEventCount())
-        eventHandler()->clearPendingFrameBeforeUnloadEventCount();
 
     if (!beforeUnloadEvent->defaultPrevented() && doc)
         doc->defaultEventHandler(beforeUnloadEvent.get());
