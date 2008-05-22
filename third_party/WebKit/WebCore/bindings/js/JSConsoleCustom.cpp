978f56f74d57d68989d7b4e9a4fec1d29cfd3457
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-22 05:58:51 +0000

    Fix Bug 19153: Inspector should support console.debug
    WebCore:
    
            Fix Bug 19153: Inspector should support console.debug
    
            <https://bugs.webkit.org/show_bug.cgi?id=19153>
            <rdar://problem/5950856>
    
            Reviewed by Tim Hatcher.
    
            Test: manual-tests/inspector/console-log-formatting.html
    
            * bindings/js/JSConsoleCustom.cpp:
            (WebCore::JSConsole::debug): Added.
            * manual-tests/inspector/console-log-formatting.html: Changed to test
            console.debug as well.
            * page/Console.cpp:
            (WebCore::Console::debug): Added. Just calls through to log().
            * page/Console.h:
            * page/Console.idl: Added debug() declaration.
    
    LayoutTests:
    
            Update window-properties results for new console functions
    
            Reviewed by Tim Hatcher.
    
            * fast/dom/Window/window-properties-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34010 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSConsoleCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSConsoleCustom.cpp
@@ -33,6 +33,12 @@ using namespace KJS;
 
 namespace WebCore {
 
+JSValue* JSConsole::debug(ExecState* exec, const List& arguments)
+{
+    impl()->debug(exec, arguments);
+    return jsUndefined();
+}
+
 JSValue* JSConsole::error(ExecState* exec, const List& arguments)
 {
     impl()->error(exec, arguments);
