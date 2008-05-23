edae443c5f37f38f60d1268e85f89247f845125d
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-23 16:38:45 +0000

    2008-05-22  Anders Carlsson  <andersca@apple.com>
            Reviewed by Adam.
    
            Add IWebIconFetcher interface and implementation.
            
            * Interfaces/IWebFramePrivate.idl:
            * Interfaces/IWebIconFetcher.idl: Added.
            * Interfaces/WebKit.idl:
            * WebFrame.cpp:
            * WebFrame.h:
            * WebIconFetcher.cpp: Added.
            * WebIconFetcher.h: Added.
            * WebKit.vcproj/WebKit.vcproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34078 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebFrame.cpp
+++ third_party/WebKit/WebKit/win/WebFrame.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebEditorClient.h"
 #include "WebFramePolicyListener.h"
 #include "WebHistory.h"
+#include "WebIconFetcher.h"
 #include "WebKit.h"
 #include "WebKitStatisticsPrivate.h"
 #include "WebNotificationCenter.h"
@@ -800,6 +801,29 @@ HRESULT STDMETHODCALLTYPE WebFrame::pendingFrameUnloadEventCount(
         return E_FAIL;
 
     *result = coreFrame->eventHandler()->pendingFrameUnloadEventCount();
+    return S_OK;
+}
+
+HRESULT STDMETHODCALLTYPE WebFrame::fetchApplicationIcon( 
+    /* [in] */ IWebIconFetcherDelegate *delegate,
+    /* [retval][out] */ IWebIconFetcher **result)
+{
+    if (!result)
+        return E_POINTER;
+
+    *result = 0;
+
+    if (!delegate)
+        return E_FAIL;
+
+    Frame* coreFrame = core(this);
+    if (!coreFrame)
+        return E_FAIL;
+
+    *result = WebIconFetcher::fetchApplicationIcon(coreFrame, delegate);
+    if (!*result)
+        return E_FAIL;
+
     return S_OK;
 }
 
