e8e2e4ca37e269c66ba9df1458a7f4fe228d8156
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-23 00:41:49 +0000

    WebCore:
    2008-05-22  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Anders Carlsson.
    
            <rdar://problem/5838772> Support cross-site XMLHttpRequest
    
            - Implement support for cross-site non-GET requests using the HTTP header
              Access-control mechanism.
    
            Tests: http/tests/xmlhttprequest/access-control-basic-allow-async.html
                   http/tests/xmlhttprequest/access-control-basic-not-get-allow-async.html
                   http/tests/xmlhttprequest/access-control-basic-not-get-allow.html
    
            * xml/XMLHttpRequest.cpp:
            (WebCore::XMLHttpRequest::XMLHttpRequest):
            (WebCore::XMLHttpRequest::send):
            Don't continue send if inside the method check preflight (only happens for async).
            
            (WebCore::XMLHttpRequest::crossSiteAccessRequest):
            Add support for non-GET requests, starting off a method check preflight.
    
            (WebCore::XMLHttpRequest::handleAsynchronousMethodCheckResult):
            Handler for finishing up after an asynchronous method check has completed.
    
            (WebCore::XMLHttpRequest::didFinishLoading):
            (WebCore::XMLHttpRequest::didFinishLoadingMethodCheck):
            (WebCore::XMLHttpRequest::didReceiveResponse):
            (WebCore::XMLHttpRequest::didReceiveResponseMethodCheck):
            (WebCore::XMLHttpRequest::didReceiveData):
            Do a limited set of access control when in the method check preflight.
            * xml/XMLHttpRequest.h:
    
    LayoutTests:
    
    2008-05-22  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Anders Carlsson.
    
            <rdar://problem/5838772> Support cross-site XMLHttpRequest
    
            - Add more tests for cross-domain XHR.
    
            * http/tests/xmlhttprequest/access-control-basic-allow-access-control-origin-header.html:
            * http/tests/xmlhttprequest/access-control-basic-allow-async-expected.txt: Added.
            * http/tests/xmlhttprequest/access-control-basic-allow-async.html: Added.
            * http/tests/xmlhttprequest/access-control-basic-denied.html:
            * http/tests/xmlhttprequest/access-control-basic-exclude.html:
            * http/tests/xmlhttprequest/access-control-basic-not-get-allow-async-expected.txt: Added.
            * http/tests/xmlhttprequest/access-control-basic-not-get-allow-async.html: Added.
            * http/tests/xmlhttprequest/access-control-basic-not-get-allow-expected.txt: Added.
            * http/tests/xmlhttprequest/access-control-basic-not-get-allow.html: Added.
            * http/tests/xmlhttprequest/resources/access-control-basic-non-get-allow.cgi: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34041 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/xml/XMLHttpRequest.h
+++ third_party/WebKit/WebCore/xml/XMLHttpRequest.h
@@ -104,6 +104,10 @@ private:
     virtual void didFinishLoading(SubresourceLoader*);
     virtual void receivedCancellation(SubresourceLoader*, const AuthenticationChallenge&);
 
+    // Special versions for the method check preflight
+    void didReceiveResponseMethodCheck(SubresourceLoader*, const ResourceResponse&);
+    void didFinishLoadingMethodCheck(SubresourceLoader*);
+
     void processSyncLoadResults(const Vector<char>& data, const ResourceResponse&, ExceptionCode&);
     void updateAndDispatchOnProgress(unsigned int len);
 
@@ -124,6 +128,8 @@ private:
 
     void loadRequestSynchronously(ResourceRequest&, ExceptionCode&);
     void loadRequestAsynchronously(ResourceRequest&);
+
+    void handleAsynchronousMethodCheckResult();
 
     void genericError();
     void networkError();
@@ -165,6 +171,7 @@ private:
 
     bool m_sameOriginRequest;
     bool m_allowAccess;
+    bool m_inMethodCheck;
 
     // FIXME: Add support for AccessControlList in a PI in an XML document in addition to the http header.
     OwnPtr<AccessControlList> m_httpAccessControlList;
