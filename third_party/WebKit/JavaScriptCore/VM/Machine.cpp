6a94799d648c2195fddd994f381d64fd052007ad
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-23 23:44:40 +0000

    2008-05-23  Anders Carlsson  <andersca@apple.com>
            Reviewed by Geoff.
    
            <rdar://problem/5959886> REGRESSION: Assertion failure in JSImmediate::toString when loading GMail (19217)
            
            Change List to store a JSValue*** pointer + an offset instead of a JSValue** pointer to protect against the case where 
            a register file changes while a list object points to its buffer.
            
            * VM/Machine.cpp:
            (KJS::Machine::privateExecute):
            * kjs/JSActivation.cpp:
            (KJS::JSActivation::createArgumentsObject):
            * kjs/list.cpp:
            (KJS::List::getSlice):
            * kjs/list.h:
            (KJS::List::List):
            (KJS::List::at):
            (KJS::List::append):
            (KJS::List::begin):
            (KJS::List::end):
            (KJS::List::buffer):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34095 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/Machine.cpp
+++ third_party/WebKit/JavaScriptCore/VM/Machine.cpp
@@ -1941,7 +1941,7 @@ JSValue* Machine::privateExecute(ExecutionFlag flag, ExecState* exec, RegisterFi
             r[argv].u.jsValue = base == missingThisObjectMarker() ? exec->globalThisValue() : (r[base].u.jsValue)->toObject(exec); // "this" value
             JSObject* thisObj = static_cast<JSObject*>(r[argv].u.jsValue);
 
-            List args(&r[argv + 1].u.jsValue, argc - 1);
+            List args(reinterpret_cast<JSValue***>(registerBase), registerOffset + argv + 1, argc - 1);
 
             registerFile->setSafeForReentry(true);
             JSValue* returnValue = static_cast<JSObject*>(v)->callAsFunction(exec, thisObj, args);
@@ -2059,7 +2059,8 @@ JSValue* Machine::privateExecute(ExecutionFlag flag, ExecState* exec, RegisterFi
 
             int registerOffset = r - (*registerBase);
 
-            List args(&r[argv + 1].u.jsValue, argc - 1);
+            List args(reinterpret_cast<JSValue***>(registerBase), registerOffset + argv + 1, argc - 1);
+
             registerFile->setSafeForReentry(true);
             JSValue* returnValue = constructor->construct(exec, args);
             registerFile->setSafeForReentry(false);
