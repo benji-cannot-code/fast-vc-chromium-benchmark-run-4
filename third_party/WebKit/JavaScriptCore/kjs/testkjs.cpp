1751ea07e862394bfda03c8b1145addc9ba55be1
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-24 12:43:02 +0000

    Rubber-stamped, tweaked and landed by Alexey.
            Build fix for gcc 4.3. Added missing standard includes.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <wtf/Assertions.h>
 #include <wtf/HashTraits.h>
 
--- third_party/WebKit/WebCore/page/Console.cpp
+++ third_party/WebKit/WebCore/page/Console.cpp
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <kjs/interpreter.h>
 #include <kjs/list.h>
 #include <profiler/Profiler.h>
+#include <stdio.h>
 
 using namespace KJS;
 
