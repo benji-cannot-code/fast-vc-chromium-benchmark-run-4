44929f276167eb21c751fa5cb0ad6c8b03933c65
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-25 09:55:28 +0000

    2008-05-25  Sriram Neelakandan  <sriram.neelakandan@gmail.com>
            Reviewed by Alp Toker.
    
            http://bugs.webkit.org/show_bug.cgi?id=18935
            [Gtk] Plugin Load crashes with NP_FULL mode
    
            Fix crash due to uninitialized variable.
    
            * WebCoreSupport/FrameLoaderClientGtk.cpp:
            (WebKit::FrameLoaderClient::FrameLoaderClient):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34121 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
+++ third_party/WebKit/WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
@@ -60,6 +60,7 @@ namespace WebKit {
 FrameLoaderClient::FrameLoaderClient(WebKitWebFrame* frame)
     : m_frame(frame)
     , m_pluginView(0)
+    , m_hasSentResponseToPlugin(false)
     , m_userAgent("")
 {
     ASSERT(m_frame);
@@ -318,7 +319,9 @@ PassRefPtr<Frame> FrameLoaderClient::createFrame(const KURL& url, const String& 
 
 void FrameLoaderClient::redirectDataToPlugin(Widget* pluginWidget)
 {
+    ASSERT(!m_pluginView);
     m_pluginView = static_cast<PluginView*>(pluginWidget);
+    m_hasSentResponseToPlugin = false;
 }
 
 Widget* FrameLoaderClient::createJavaAppletWidget(const IntSize&, Element*, const KURL& baseURL,
