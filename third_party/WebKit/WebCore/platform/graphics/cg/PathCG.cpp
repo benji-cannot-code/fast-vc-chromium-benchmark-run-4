7f29dafba9ce1e6fd4cbc1262ea04c71aee5dcea
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-25 07:11:04 +0000

    Reviewed by Eric.
            https://bugs.webkit.org/show_bug.cgi?id=18927
            Unify path data debug output
    
            Implement unified path data output for Mac svg tests.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34117 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/PathCG.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cg/PathCG.cpp
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2003, 2006 Apple Computer, Inc.  All rights reserved.
- *                     2006 Rob Buis <buis@kde.org>
+ *                     2006, 2008 Rob Buis <buis@kde.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -190,17 +190,17 @@ static void CGPathToCFStringApplierFunction(void* info, const CGPathElement *ele
     CGPoint* points = element->points;
     switch (element->type) {
     case kCGPathElementMoveToPoint:
-        CFStringAppendFormat(string, 0, CFSTR("M%.2f,%.2f"), points[0].x, points[0].y);
+        CFStringAppendFormat(string, 0, CFSTR("M%.2f,%.2f "), points[0].x, points[0].y);
         break;
     case kCGPathElementAddLineToPoint:
-        CFStringAppendFormat(string, 0, CFSTR("L%.2f,%.2f"), points[0].x, points[0].y);
+        CFStringAppendFormat(string, 0, CFSTR("L%.2f,%.2f "), points[0].x, points[0].y);
         break;
     case kCGPathElementAddQuadCurveToPoint:
-        CFStringAppendFormat(string, 0, CFSTR("Q%.2f,%.2f,%.2f,%.2f"),
+        CFStringAppendFormat(string, 0, CFSTR("Q%.2f,%.2f,%.2f,%.2f "),
                 points[0].x, points[0].y, points[1].x, points[1].y);
         break;
     case kCGPathElementAddCurveToPoint:
-        CFStringAppendFormat(string, 0, CFSTR("C%.2f,%.2f,%.2f,%.2f,%.2f,%.2f"),
+        CFStringAppendFormat(string, 0, CFSTR("C%.2f,%.2f,%.2f,%.2f,%.2f,%.2f "),
                 points[0].x, points[0].y, points[1].x, points[1].y,
                 points[2].x, points[2].y);
         break;
@@ -216,6 +216,8 @@ static CFStringRef CFStringFromCGPath(CGPathRef path)
 
     CFMutableStringRef string = CFStringCreateMutable(NULL, 0);
     CGPathApply(path, string, CGPathToCFStringApplierFunction);
+    CFStringTrimWhitespace(string);
+
 
     return string;
 }
