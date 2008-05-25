cff9b9e51904f9f66ed784f407cc3136652f8d5b
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-25 07:15:42 +0000

    Reviewed by Darin.
            Changed regular expression matching result array to be lazily filled, because many callers
            only care about it being non-null.
    
            2% improvement on Acid3 test 26.
    
            * kjs/array_instance.cpp: Added a void* member to ArrayStorage for ArrayInstance subclasses
            to use.
            * kjs/array_instance.h:
            (KJS::ArrayInstance::lazyCreationData):
            (KJS::ArrayInstance::setLazyCreationData):
            Added methods to access it from subclasses.
    
            * kjs/regexp_object.cpp:
            (KJS::RegExpMatchesArray::RegExpMatchesArray):
            (KJS::RegExpMatchesArray::getOwnPropertySlot):
            (KJS::RegExpMatchesArray::put):
            (KJS::RegExpMatchesArray::deleteProperty):
            (KJS::RegExpMatchesArray::getPropertyNames):
            (KJS::RegExpMatchesArray::fillArrayInstanceIfNeeded):
            (KJS::RegExpMatchesArray::~RegExpMatchesArray):
            (KJS::RegExpObjectImp::arrayOfMatches):
            RegExpMatchesArray is a subclass of ArrayInstance that isn't filled until
            accessed for the first time.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34118 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_instance.h
+++ third_party/WebKit/JavaScriptCore/kjs/array_instance.h
@@ -54,6 +54,10 @@ namespace KJS {
     void sort(ExecState*);
     void sort(ExecState*, JSObject* compareFunction);
 
+  protected:
+    void* lazyCreationData();
+    void setLazyCreationData(void*);
+
   private:
     static JSValue* lengthGetter(ExecState*, JSObject*, const Identifier&, const PropertySlot&);
     bool inlineGetOwnPropertySlot(ExecState*, unsigned propertyName, PropertySlot&);
