f6b3b1c0244aa32d3215b95bcb3cfe6afe450acb
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-26 02:29:58 +0000

    <rdar://problem/5840884> _recursive_resumeNullEventsForAllNetscapePlugins and _pauseNullEvents not defined
    Follow-up for r33052.  _recursive_resumeNullEventsForAllNetscapePlugins and _recursive_pauseNullEventsForAllNetscapePlugins
    need to be declared in WebFramePrivate.h rather than WebFrameInternal.h so they can be used from outside of WebKit.
    
    Reviewed by Darin Adler.
    
    * WebView/WebFrame.mm:
    (-[WebFrame _recursive_resumeNullEventsForAllNetscapePlugins]):
    (-[WebFrame _recursive_pauseNullEventsForAllNetscapePlugins]):
    * WebView/WebFrameInternal.h:
    * WebView/WebFramePrivate.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34130 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebFramePrivate.h
+++ third_party/WebKit/WebKit/mac/WebView/WebFramePrivate.h
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebFrame.h>
 #import <JavaScriptCore/JSBase.h>
 
+#if !defined(ENABLE_NETSCAPE_PLUGIN_API)
+#define ENABLE_NETSCAPE_PLUGIN_API 1
+#endif
+
 @class WebIconFetcher;
 @class WebScriptObject;
 
@@ -72,5 +76,10 @@ typedef enum {
 
 - (WebIconFetcher *)fetchApplicationIcon:(id)target
                                 selector:(SEL)selector;
+
+#if ENABLE_NETSCAPE_PLUGIN_API
+- (void)_recursive_resumeNullEventsForAllNetscapePlugins;
+- (void)_recursive_pauseNullEventsForAllNetscapePlugins;
+#endif
 
 @end
