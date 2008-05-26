98eac6dc622e42f4f78ca77d6955d7e2af812ff2
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-26 21:13:03 +0000

    2008-05-26  Anders Carlsson  <andersca@apple.com>
            Reviewed by Sam.
    
            This patch does two things:
            
            * It adds a toJSNewlyCreated function for creating JS wrappers for nodes where we know
            that the node doesn't have a wrapper. This avoids a hash lookup.
            
            * It adds toJSNewlyCreated methods for JSElement and JSText. 
            
            All in all this is a 4.3% speedup on Acid 3 test 26.
            
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * bindings/js/JSElementCustom.cpp:
            (WebCore::toJSNewlyCreated):
            * bindings/js/JSNodeCustom.cpp:
            (WebCore::createWrapper):
            (WebCore::toJSNewlyCreated):
            (WebCore::toJS):
            * bindings/js/JSTextCustom.cpp: Added.
            (WebCore::toJSNewlyCreated):
            * bindings/scripts/CodeGeneratorJS.pm:
            * dom/Document.idl:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34141 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSElementCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSElementCustom.cpp
@@ -36,6 +36,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLFrameElementBase.h"
 #include "HTMLNames.h"
 #include "JSAttr.h"
+#include "JSHTMLElementWrapperFactory.h"
+
+#if ENABLE(SVG)
+#include "JSSVGElementWrapperFactory.h"
+#include "SVGElement.h"
+#endif
 
 using namespace KJS;
 
@@ -120,4 +126,29 @@ JSValue* JSElement::setAttributeNodeNS(ExecState* exec, const List& args)
     return result;
 }
 
+    
+JSValue* toJSNewlyCreated(ExecState* exec, Element* element)
+{
+    if (!element)
+        return jsNull();
+
+    ASSERT(!ScriptInterpreter::getDOMNodeForDocument(element->document(), element));
+    
+    Document* doc = element->document();
+    JSNode* ret = 0;
+    
+    if (element->isHTMLElement())
+        ret = createJSHTMLWrapper(exec, static_cast<HTMLElement*>(element));
+#if ENABLE(SVG)
+    else if (element->isSVGElement())
+        ret = createJSSVGWrapper(exec, static_cast<SVGElement*>(element));
+#endif
+    else
+        ret = new JSElement(JSElementPrototype::self(exec), element);
+
+    ScriptInterpreter::putDOMNodeForDocument(doc, element, ret);
+
+    return ret;    
+}
+    
 } // namespace WebCore
