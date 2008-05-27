d7820ddaf5711394d38b71b27c59a04d7fc09dc2
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-27 05:04:48 +0000

    2008-05-26  Stephanie Lewis  <slewis@apple.com>
            Windows build fix.
    
            * kjs/testkjs.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34145 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/testkjs.cpp
@@ -38,9 +38,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <wtf/Assertions.h>
 #include <wtf/HashTraits.h>
+
+#if !PLATFORM(WIN_OS)
+#include <unistd.h>
+#endif
 
 #if HAVE(SYS_TIME_H)
 #include <sys/time.h>
