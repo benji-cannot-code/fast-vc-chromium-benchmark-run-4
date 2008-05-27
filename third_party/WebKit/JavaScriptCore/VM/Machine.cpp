891ba8365b9e829504cfad72c79557e411339198
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-27 14:43:36 +0000

    2008-05-27  Darin Adler  <darin@apple.com>
            Reviewed by Tim Hatcher.
    
            - https://bugs.webkit.org/show_bug.cgi?id=19180
              speed up SunSpider by optimizing immediate number cases
    
            Add immediate number cases for the &, |, and ^ operators.
            Makes standalone SunSpider 1.010x faster.
    
            * VM/Machine.cpp:
            (KJS::Machine::privateExecute): Add areBothImmediateNumbers special cases
            for the &, |, and ^ operators.
    
            * kjs/JSImmediate.h:
            (KJS::JSImmediate::xorImmediateNumbers): Added.
            (KJS::JSImmediate::orImmediateNumbers): Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34149 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/Machine.cpp
+++ third_party/WebKit/JavaScriptCore/VM/Machine.cpp
@@ -1309,8 +1309,15 @@ JSValue* Machine::privateExecute(ExecutionFlag flag, ExecState* exec, RegisterFi
         int dst = (++vPC)->u.operand;
         int src1 = (++vPC)->u.operand;
         int src2 = (++vPC)->u.operand;
-        JSValue* result = jsNumber((r[src1].u.jsValue->toInt32(exec)) & (r[src2].u.jsValue->toInt32(exec)));
-        VM_CHECK_EXCEPTION();
+        JSValue* v1 = r[src1].u.jsValue;
+        JSValue* v2 = r[src2].u.jsValue;
+        JSValue* result;
+        if (JSImmediate::areBothImmediateNumbers(v1, v2))
+            result = JSImmediate::andImmediateNumbers(v1, v2);
+        else {
+            result = jsNumber(v1->toInt32(exec) & v2->toInt32(exec));
+            VM_CHECK_EXCEPTION();
+        }
         r[dst].u.jsValue = result;
         
         ++vPC;
@@ -1326,8 +1333,15 @@ JSValue* Machine::privateExecute(ExecutionFlag flag, ExecState* exec, RegisterFi
         int dst = (++vPC)->u.operand;
         int src1 = (++vPC)->u.operand;
         int src2 = (++vPC)->u.operand;
-        JSValue* result = jsNumber((r[src1].u.jsValue->toInt32(exec)) ^ (r[src2].u.jsValue->toInt32(exec)));
-        VM_CHECK_EXCEPTION();
+        JSValue* v1 = r[src1].u.jsValue;
+        JSValue* v2 = r[src2].u.jsValue;
+        JSValue* result;
+        if (JSImmediate::areBothImmediateNumbers(v1, v2))
+            result = JSImmediate::xorImmediateNumbers(v1, v2);
+        else {
+            result = jsNumber(v1->toInt32(exec) ^ v2->toInt32(exec));
+            VM_CHECK_EXCEPTION();
+        }
         r[dst].u.jsValue = result;
         
         ++vPC;
@@ -1343,8 +1357,15 @@ JSValue* Machine::privateExecute(ExecutionFlag flag, ExecState* exec, RegisterFi
         int dst = (++vPC)->u.operand;
         int src1 = (++vPC)->u.operand;
         int src2 = (++vPC)->u.operand;
-        JSValue* result = jsNumber((r[src1].u.jsValue->toInt32(exec)) | (r[src2].u.jsValue->toInt32(exec)));
-        VM_CHECK_EXCEPTION();
+        JSValue* v1 = r[src1].u.jsValue;
+        JSValue* v2 = r[src2].u.jsValue;
+        JSValue* result;
+        if (JSImmediate::areBothImmediateNumbers(v1, v2))
+            result = JSImmediate::orImmediateNumbers(v1, v2);
+        else {
+            result = jsNumber(v1->toInt32(exec) | v2->toInt32(exec));
+            VM_CHECK_EXCEPTION();
+        }
         r[dst].u.jsValue = result;
         
         ++vPC;
