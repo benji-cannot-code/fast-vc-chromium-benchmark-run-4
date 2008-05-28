3ac4624e958bda1925f4307387bd24d4fb70bb72
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-28 23:20:17 +0000

    2008-05-28  Anders Carlsson  <andersca@apple.com>
            Build fix.
            
            * storage/LocalStorage.cpp:
            (WebCore::LocalStorage::scheduleImport):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/LocalStorage.cpp
+++ third_party/WebKit/WebCore/storage/LocalStorage.cpp
@@ -128,7 +128,7 @@ void LocalStorage::close()
     }
 }
 
-void LocalStorage::scheduleImport(PassRefPtr<LocalStorageArea> area)
+bool LocalStorage::scheduleImport(PassRefPtr<LocalStorageArea> area)
 {
     ASSERT(isMainThread());
 
