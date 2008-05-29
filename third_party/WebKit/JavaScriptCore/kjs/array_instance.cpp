d68a58e95235366ad9a41e8fa9da9e363b4f8af1
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-29 17:31:43 +0000

    2008-05-29  Steve Falkenburg  <sfalken@apple.com>
            Build fix.
    
            * kjs/array_instance.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34205 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_instance.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/array_instance.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Assertions.h>
 #include <wtf/AVLTree.h>
 
-using std::min;
+using namespace std;
 
 namespace KJS {
 
