4e1c3be0e078f341d6cdef4e8753e6e85a25a191
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-05-30 10:08:31 +0000

    Reviewed by Darin.
            http://bugs.webkit.org/show_bug.cgi?id=7466
            <rdar://problem/4657563> Use of Ctrl as access key modifier conflicts with Mac OS X
            emacs-style keybindings
    
    WebCore:
            * page/EventHandler.cpp:
            (WebCore::EventHandler::handleAccessKey): Use Ctrl+Option for access keys on Mac OS X.
    
    LayoutTests:
            * fast/events/access-key-self-destruct.html:
            * fast/forms/access-key.html:
            * fast/forms/focus-selection-input-expected.txt:
            * fast/forms/focus-selection-input.html:
            * fast/forms/legend-access-key.html:
            * fast/forms/select-accesskey.html:
            Updated tests to keep passing.
    
            * fast/forms/select-accesskey-expected.txt: Added.
            * fast/forms/select-accesskey.html:
            * platform/mac/fast/forms/select-accesskey-expected.checksum: Removed.
            * platform/mac/fast/forms/select-accesskey-expected.png: Removed.
            * platform/mac/fast/forms/select-accesskey-expected.txt: Removed.
            Updated to keep passing, made text-only.
    
            * fast/forms/focus-selection-textarea.html:
            * fast/forms/focus-selection-textarea-expected.txt:
            Added missing platform-specific logic.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/mac/EventHandlerMac.mm
+++ third_party/WebKit/WebCore/page/mac/EventHandlerMac.mm
@@ -48,7 +48,7 @@ namespace WebCore {
 
 using namespace EventNames;
 
-unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::CtrlKey;
+unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::CtrlKey | PlatformKeyboardEvent::AltKey;
 
 static RetainPtr<NSEvent>& currentEvent()
 {
