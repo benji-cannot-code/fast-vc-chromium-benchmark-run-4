4595e63419ff3432fd00ed655739345c79dd67d9
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-02 19:58:02 +0000

    Build fix for non-AllInOne builds
            * kjs/array_object.cpp: Added a missing #include.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/array_object.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "array_object.h"
 #include "array_object.lut.h"
 
+#include "Machine.h"
 #include "error_object.h"
 #include "lookup.h"
 #include "operations.h"
