1628a05b095f572c83c4fdf692e23a13fd5e06bb
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-02 17:26:04 +0000

    WebCore:
    2008-06-02  Darin Adler  <darin@apple.com>
    
            Reviewed by Mitz.
    
            - fix https://bugs.webkit.org/show_bug.cgi?id=19358
              <rdar://problem/5951986> first line break in <pre> elements ignored
              (chess boards at http://rankzero.de/)
    
            Test: fast/parser/pre-first-line-break.html
    
            * editing/markup.cpp:
            (WebCore::appendStartMarkup): Fix handling of <listing>. Not part of the
            bug mentioned above, but uncovered by the regression test.
    
            * html/HTMLTokenizer.cpp:
            (WebCore::HTMLTokenizer::write): Clear the "discardLF" flag when
            processing a new tag.
    
    LayoutTests:
    
    2008-06-02  Darin Adler  <darin@apple.com>
    
            Reviewed by Mitz.
    
            - test for https://bugs.webkit.org/show_bug.cgi?id=19358
              <rdar://problem/5951986> first line break in <pre> elements ignored
              (chess boards at http://rankzero.de/)
    
            * fast/parser/pre-first-line-break-expected.txt: Added.
            * fast/parser/pre-first-line-break.html: Added.
            * fast/parser/resources/TEMPLATE.html: Copied from fast/dom/resources/TEMPLATE.html.
            * fast/parser/resources/pre-first-line-break.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34311 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
+++ third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
@@ -1736,6 +1736,7 @@ bool HTMLTokenizer::write(const SegmentedString& str, bool appendData)
             tagStartLineno = m_lineNumber;
             src.advancePastNonNewline();
             state.setStartTag(true);
+            state.setDiscardLF(false);
         } else if (cc == '\n' || cc == '\r') {
             if (state.discardLF())
                 // Ignore this LF
