ac5ba1f038bcdb89738c0be998311efcf8f67621
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-03 18:29:03 +0000

    Fix build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@34345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/AccessibilityRenderObject.cpp
+++ third_party/WebKit/WebCore/page/AccessibilityRenderObject.cpp
@@ -2023,8 +2023,6 @@ bool AccessibilityRenderObject::ariaRoleHasPresentationalChildren() const
     case ProgressIndicatorRole:
     //case SeparatorRole:
         return true;
-    default:
-        return false;
     }
     ASSERT_NOT_REACHED();
     return false;
