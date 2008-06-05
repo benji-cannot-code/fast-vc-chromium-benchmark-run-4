ff08cab333414ea9e5b0bae3d8310525b1fd04d6
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-05 22:17:56 +0000

    <rdar://problem/5979998> AXList controls should have AXSelectedChildren and its children's AXSelected attribute writable
    git-svn-id: svn://svn.chromium.org/blink/trunk@34384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/AccessibilityObject.cpp
+++ third_party/WebKit/WebCore/page/AccessibilityObject.cpp
@@ -283,6 +283,10 @@ void AccessibilityObject::setValue(const String& string)
 {
 }
 
+void AccessibilityObject::setSelected(bool)
+{
+}
+
 bool AccessibilityObject::press() const
 {
     Element* actionElem = actionElement();
@@ -299,7 +303,7 @@ AXObjectCache* AccessibilityObject::axObjectCache() const
     return 0;
 }
 
-void AccessibilityObject::getDocumentLinks(Vector<RefPtr<AccessibilityObject> >& result) const
+void AccessibilityObject::getDocumentLinks(AccessibilityChildrenVector& result) const
 {
 }
 
--- third_party/WebKit/WebCore/page/AccessibilityRenderObject.cpp
+++ third_party/WebKit/WebCore/page/AccessibilityRenderObject.cpp
@@ -1371,7 +1371,7 @@ AXObjectCache* AccessibilityRenderObject::axObjectCache() const
     return m_renderer->document()->axObjectCache();
 }
 
-void AccessibilityRenderObject::getDocumentLinks(Vector<RefPtr<AccessibilityObject> >& result) const
+void AccessibilityRenderObject::getDocumentLinks(AccessibilityChildrenVector& result) const
 {
     Document* document = m_renderer->document();
     RefPtr<HTMLCollection> coll = document->links();
@@ -2139,7 +2139,7 @@ void AccessibilityRenderObject::childrenChanged()
     }
 }
 
-const Vector<RefPtr<AccessibilityObject> >& AccessibilityRenderObject::children()
+const AccessibilityObject::AccessibilityChildrenVector& AccessibilityRenderObject::children()
 {
     if (!m_haveChildren)
         addChildren();
@@ -2163,7 +2163,7 @@ void AccessibilityRenderObject::addChildren()
         if (obj->accessibilityIsIgnored()) {
             if (!obj->hasChildren())
                 obj->addChildren();
-            Vector<RefPtr<AccessibilityObject> >children = obj->children();
+            AccessibilityChildrenVector children = obj->children();
             unsigned length = children.size();
             for (unsigned i = 0; i < length; ++i)
                 m_children.append(children[i]);
@@ -2189,7 +2189,7 @@ void AccessibilityRenderObject::addChildren()
     }
 }
 
-void AccessibilityRenderObject::ariaListboxSelectedChildren(Vector<RefPtr<AccessibilityObject> >& result)
+void AccessibilityRenderObject::ariaListboxSelectedChildren(AccessibilityChildrenVector& result)
 {
     AccessibilityObject* child = firstChild();
     bool isMultiselectable = false;
@@ -2222,7 +2222,7 @@ void AccessibilityRenderObject::ariaListboxSelectedChildren(Vector<RefPtr<Access
     }
 }
 
-void AccessibilityRenderObject::selectedChildren(Vector<RefPtr<AccessibilityObject> >& result)
+void AccessibilityRenderObject::selectedChildren(AccessibilityChildrenVector& result)
 {
     ASSERT(result.isEmpty());
 
@@ -2234,7 +2234,7 @@ void AccessibilityRenderObject::selectedChildren(Vector<RefPtr<AccessibilityObje
     return ariaListboxSelectedChildren(result);
 }
 
-void AccessibilityRenderObject::ariaListboxVisibleChildren(Vector<RefPtr<AccessibilityObject> >& result)      
+void AccessibilityRenderObject::ariaListboxVisibleChildren(AccessibilityChildrenVector& result)      
 {
     if (!hasChildren())
         addChildren();
@@ -2246,7 +2246,7 @@ void AccessibilityRenderObject::ariaListboxVisibleChildren(Vector<RefPtr<Accessi
     }
 }
 
-void AccessibilityRenderObject::visibleChildren(Vector<RefPtr<AccessibilityObject> >& result)
+void AccessibilityRenderObject::visibleChildren(AccessibilityChildrenVector& result)
 {
     ASSERT(result.isEmpty());
         
