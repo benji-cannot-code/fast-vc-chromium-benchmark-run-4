a9d2237936796b15f04972f8678316939d04a773
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-05 20:03:43 +0000

    Re-landing an independent part of a previously rolled out threading patch.
            * wtf/ThreadSpecific.h: Make sure to initialize POD thread-specific varaibles, too
            (replaced "new T" with "new T()").
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34380 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/ThreadSpecific.h
+++ third_party/WebKit/JavaScriptCore/wtf/ThreadSpecific.h
@@ -116,6 +116,7 @@ inline void ThreadSpecific<T>::destroy(void* ptr)
 
 template<typename T>
 inline ThreadSpecific<T>::ThreadSpecific()
+    : m_data()
 {
 }
 
@@ -142,7 +143,7 @@ inline ThreadSpecific<T>::operator T*()
 {
     T* ptr = static_cast<T*>(get());
     if (!ptr) {
-        ptr = new T;
+        ptr = new T();
         set(ptr);
     }
     return ptr;
