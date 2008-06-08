cb97e5c23e9f87a4d1ec2e45314a4594858e663e
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-08 19:41:25 +0000

    WebCore:
    2008-06-08  Vincent Ricard  <magic@magicninja.org>
    
            Reviewed by Darin.
    
            Bug 18041: DOMNodeRemoved events are sent twice
    
            Test: fast/dom/Node/DOMNodeRemovedEvent.html
    
            * dom/ContainerNode.cpp:
            (WebCore::ContainerNode::removeChild):
    
    LayoutTests:
    
    2008-06-08  Vincent Ricard  <magic@magicninja.org>
    
            Reviewed by Darin.
    
            Test case for
            Bug 18041: DOMNodeRemoved events are sent twice
    
            * fast/dom/Node/DOMNodeRemovedEvent-expected.txt: Added.
            * fast/dom/Node/DOMNodeRemovedEvent.html: Added.
            * fast/dom/Node/resources/DOMNodeRemovedEvent.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/Node/resources/DOMNodeRemovedEvent.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("This test checks that DOMNodeRemovedEvent is emitted once (and only once).");
+
+var div = document.createElement("div");
+document.body.appendChild(div);
+
+var count = 0;
+document.body.addEventListener("DOMNodeRemoved", function () { count++; }, false);
+document.body.removeChild(div);
+
+shouldBe("count", "1");
+
+var successfullyParsed = true;
