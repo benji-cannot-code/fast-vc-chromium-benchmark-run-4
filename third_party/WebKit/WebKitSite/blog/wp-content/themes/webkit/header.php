b12fe8071337bddb8f1dacc780351728df13f866
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-10 14:22:10 +0000

    2008-06-10  Maciej Stachowiak  <mjs@apple.com>
            Rubber stamped by Adele.
    
            - add short title and multisize icon
            
            * blog/wp-content/themes/webkit/header.php:
            * images/surfin-safari.icns: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitSite/blog/wp-content/themes/webkit/header.php
+++ third_party/WebKit/WebKitSite/blog/wp-content/themes/webkit/header.php
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 
-    <title><?php bloginfo('name'); if ( is_single() ) { ?> - Blog Archive <?php } wp_title(); ?></title>
+    <title><?php bloginfo('name'); if ( is_single() ) { ?> - Blog Archive <?php } else { ?> - The WebKit Blog <?php } wp_title(); ?></title>
+
+    <meta name="application-name" content="<?php bloginfo('name') ?>">
 
     <meta name="generator" content="WordPress <?php bloginfo('version'); ?>">
 
@@ -26,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>">
     <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+    <link rel="icon" sizes="16x16 32x32 128x128 256x256" type="image/x-icns" href="/images/surfin-safari.icns">
+    <link rel="SHORTCUT ICON" type="image/x-icon" href="/favicon.ico">
+
 
     <?php wp_head(); ?>
 </head>
