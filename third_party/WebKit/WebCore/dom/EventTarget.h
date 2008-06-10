a65a45a14a6d3efe62e87273c307ccd8f6298bed
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-10 21:06:54 +0000

    Reviewed by mjs.
            Fix disconnected event listener crasher
            m_disconnectedNodesWithEventListeners not updated when nodes adopted
            https://bugs.webkit.org/show_bug.cgi?id=19451
    
            I failed to find a way to make this crash DRT, I can crash Debug Safari consistently.
    
            * dom/EventTarget.cpp:
            (WebCore::EventTarget::willMoveToNewOwnerDocument):
            (WebCore::EventTarget::didMoveToNewOwnerDocument):
            * dom/EventTarget.h:
            * dom/EventTargetNode.cpp:
            (WebCore::EventTargetNode::willMoveToNewOwnerDocument):
            (WebCore::EventTargetNode::didMoveToNewOwnerDocument):
            * dom/EventTargetNode.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34485 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/EventTarget.h
+++ third_party/WebKit/WebCore/dom/EventTarget.h
@@ -87,6 +87,8 @@ namespace WebCore {
 
         void insertedIntoDocument(EventTargetNode* referenceNode);
         void removedFromDocument(EventTargetNode* referenceNode);
+        void willMoveToNewOwnerDocument(EventTargetNode* referenceNode);
+        void didMoveToNewOwnerDocument(EventTargetNode* referenceNode);
 
         void handleLocalEvents(EventTargetNode* referenceNode, Event*, bool useCapture);
 
