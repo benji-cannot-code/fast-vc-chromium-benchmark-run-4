6d2e1510d8b7be03e6803f7993925f606b8cf35c
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-13 22:32:13 +0000

    2008-06-13  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
            Reviewed by Darin.
    
            Add a test for existing loader behaviour in preparation for:
    
            Bug 19422: Distinct redirects from the same link do not create distinct history items
            <https://bugs.webkit.org/show_bug.cgi?id=19422>
    
            * http/tests/navigation/redirect-cycle-expected.txt: Added.
            * http/tests/navigation/redirect-cycle.html: Added.
            * http/tests/navigation/resources/redirect-cycle-1.pl: Added.
            * http/tests/navigation/resources/redirect-cycle-2.pl: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/navigation/resources/redirect-cycle-2.pl
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/perl
+
+print "Status: 302 Moved Temporarily\r\n";
+print "Location: redirect-cycle-1.pl\r\n";
+print "Content-type: text/html\r\n";
+print "\r\n";
+print "<html>";
+print "<body>";
+print "<div>Page 2</div>";
+print "</body>";
+print "</html>";
