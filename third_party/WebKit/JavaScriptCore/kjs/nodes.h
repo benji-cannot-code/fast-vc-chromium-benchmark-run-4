2ca1773168b1e345dc6ab35af62b1bfa22164f2f
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-14 04:40:45 +0000

    2008-06-13  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
            Reviewed by Maciej.
    
            Eliminate the use of temporaries to store the left hand side of an
            expression when the right hand side is a constant. This slightly
            improves the generated bytecode for a few SunSpider tests, but it is
            mostly in preparation for fixing
    
            Bug 19484: More instructions needs to use temporary registers
            <https://bugs.webkit.org/show_bug.cgi?id=19484>
    
            * VM/CodeGenerator.h:
            (KJS::CodeGenerator::leftHandSideNeedsCopy):
            (KJS::CodeGenerator::emitNodeForLeftHandSide):
            * kjs/nodes.cpp:
            (KJS::BracketAccessorNode::emitCode):
            (KJS::ReadModifyResolveNode::emitCode):
            (KJS::AssignDotNode::emitCode):
            (KJS::ReadModifyDotNode::emitCode):
            (KJS::AssignBracketNode::emitCode):
            (KJS::ReadModifyBracketNode::emitCode):
            * kjs/nodes.h:
            (KJS::ExpressionNode::):
            (KJS::FalseNode::):
            (KJS::TrueNode::):
            (KJS::NumberNode::):
            (KJS::StringNode::):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34528 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/nodes.h
+++ third_party/WebKit/JavaScriptCore/kjs/nodes.h
@@ -209,6 +209,7 @@ namespace KJS {
         }
 
         virtual bool isNumber() const KJS_FAST_CALL { return false; }
+        virtual bool isConstant() const KJS_FAST_CALL { return false; }        
         virtual bool isLocation() const KJS_FAST_CALL { return false; }
         virtual bool isResolveNode() const KJS_FAST_CALL { return false; }
         virtual bool isBracketAccessorNode() const KJS_FAST_CALL { return false; }
@@ -260,6 +261,7 @@ namespace KJS {
 
         virtual RegisterID* emitCode(CodeGenerator&, RegisterID* = 0) KJS_FAST_CALL;
 
+        virtual bool isConstant() const KJS_FAST_CALL { return true; }
         virtual void streamTo(SourceStream&) const KJS_FAST_CALL;
         virtual Precedence precedence() const { return PrecPrimary; }
     };
@@ -273,6 +275,7 @@ namespace KJS {
 
         virtual RegisterID* emitCode(CodeGenerator&, RegisterID* = 0) KJS_FAST_CALL;
 
+        virtual bool isConstant() const KJS_FAST_CALL { return true; }
         virtual void streamTo(SourceStream&) const KJS_FAST_CALL;
         virtual Precedence precedence() const { return PrecPrimary; }
     };
@@ -302,6 +305,7 @@ namespace KJS {
         virtual Precedence precedence() const { return signbit(m_double) ? PrecUnary : PrecPrimary; }
 
         virtual bool isNumber() const KJS_FAST_CALL { return true; }
+        virtual bool isConstant() const KJS_FAST_CALL { return true; }
         double value() const KJS_FAST_CALL { return m_double; }
         virtual void setValue(double d) KJS_FAST_CALL { m_double = d; }
 
@@ -334,6 +338,7 @@ namespace KJS {
 
         virtual RegisterID* emitCode(CodeGenerator&, RegisterID* = 0) KJS_FAST_CALL;
 
+        virtual bool isConstant() const KJS_FAST_CALL { return true; }
         virtual void streamTo(SourceStream&) const KJS_FAST_CALL;
         virtual Precedence precedence() const { return PrecPrimary; }
 
