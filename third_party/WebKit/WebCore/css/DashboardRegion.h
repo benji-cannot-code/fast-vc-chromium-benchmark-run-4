eec1c9baed59cb0533f729eed84c6a844a0e2243
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-15 04:40:44 +0000

    2008-06-14  Darin Adler  <darin@apple.com>
            Reviewed by Sam.
    
            - fix https://bugs.webkit.org/show_bug.cgi?id=19544
              DashboardRegion pieces are leaking
    
            My recent change introduced a leak of objects owned by each DashboardRegion,
            because I made ~Rect non-virtual. By using a separate RefCounted class for
            each of Rect and DashboardRegion, the leak is gone. It's not clear if the
            DashboardRegion class should really exist -- it doesn't have a DOM binding,
            so it could conceivably be removed some day.
    
            * css/DashboardRegion.h: Derive from RectBase instead of from Rect.
            * css/Rect.h: Added RectBase. Changed Rect to derive from RectBase.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34552 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/DashboardRegion.h
+++ third_party/WebKit/WebCore/css/DashboardRegion.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * (C) 1999-2003 Lars Knoll (knoll@kde.org)
- * Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2008 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class DashboardRegion : public Rect {
+class DashboardRegion : public RectBase, public RefCounted<DashboardRegion> {
 public:
     static PassRefPtr<DashboardRegion> create() { return adoptRef(new DashboardRegion); }
 
--- third_party/WebKit/WebCore/css/Rect.h
+++ third_party/WebKit/WebCore/css/Rect.h
@@ -27,10 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    class Rect : public RefCounted<Rect> {
+    class RectBase {
     public:
-        static RefPtr<Rect> create() { return adoptRef(new Rect); }
-
         CSSPrimitiveValue* top() const { return m_top.get(); }
         CSSPrimitiveValue* right() const { return m_right.get(); }
         CSSPrimitiveValue* bottom() const { return m_bottom.get(); }
@@ -42,13 +40,22 @@ namespace WebCore {
         void setLeft(PassRefPtr<CSSPrimitiveValue> left) { m_left = left; }
 
     protected:
-        Rect() { }
+        RectBase() { }
+        ~RectBase() { }
 
     private:
         RefPtr<CSSPrimitiveValue> m_top;
         RefPtr<CSSPrimitiveValue> m_right;
         RefPtr<CSSPrimitiveValue> m_bottom;
         RefPtr<CSSPrimitiveValue> m_left;
+    };
+
+    class Rect : public RectBase, public RefCounted<Rect> {
+    public:
+        static PassRefPtr<Rect> create() { return adoptRef(new Rect); }
+
+    private:
+        Rect() { }
     };
 
 } // namespace WebCore
