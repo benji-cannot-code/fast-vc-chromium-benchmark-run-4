749d3b926cfb6587e666e3c476a3c03ef707b720
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-15 04:59:15 +0000

    2008-06-14  Darin Adler  <darin@apple.com>
            - more build fixing
    
            * platform/gtk/DragDataGtk.cpp: Added missing header.
            * platform/wx/DragDataWx.cpp: Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34554 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/wx/DragDataWx.cpp
+++ third_party/WebKit/WebCore/platform/wx/DragDataWx.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007 Apple Inc.  All rights reserved.
+ * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "DragData.h"
+
+#include "Clipboard.h"
 #include "Document.h"
 #include "DocumentFragment.h"
 
