103eea57dda993c78b7633ac65819f9bf181ad0c
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-17 14:59:19 +0000

    2008-06-17  Ariya Hidayat  <ariya.hidayat@trolltech.com>
            Rubber stamped by Adam Robben.
    
            Include JSGlobalObject.h to fix the build.
    
            * kjs/ScopeChain.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34622 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/ScopeChain.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/ScopeChain.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "PropertyNameArray.h"
 #include "JSObject.h"
+#include "JSGlobalObject.h"
 #include <stdio.h>
 
 namespace KJS {
