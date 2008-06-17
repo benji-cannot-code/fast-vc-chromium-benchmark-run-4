da0ab7bad0418f99c522fc6d5c36e3e8bf5b964f
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-17 03:45:51 +0000

    Trying to fix Windows build.
            * kjs/PropertyNameArray.h:
            * kjs/identifier.cpp:
            Include ExecState.h
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34611 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/PropertyNameArray.h
+++ third_party/WebKit/JavaScriptCore/kjs/PropertyNameArray.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PropertyNameArray_h
 #define PropertyNameArray_h
 
+#include "ExecState.h"
 #include "identifier.h"
 #include <wtf/HashSet.h>
 #include <wtf/Vector.h>
--- third_party/WebKit/JavaScriptCore/kjs/identifier.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/identifier.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "identifier.h"
 
+#include "ExecState.h"
 #include "JSLock.h"
 #include <new> // for placement new
 #include <string.h> // for strlen
