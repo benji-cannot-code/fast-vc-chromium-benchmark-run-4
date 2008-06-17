1a11bcca80fdd8c626193355c46009724cf85da1
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-17 03:49:28 +0000

    2008-06-16  Alp Toker  <alp@nuanti.com>
            Rubber-stamped by Geoff.
    
            Change c++ to c in minidom and testapi emacs mode line comments.
    
            * API/Node.h:
            * API/NodeList.c:
            * API/NodeList.h:
            * API/testapi.c:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34612 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/NodeList.h
+++ third_party/WebKit/JavaScriptCore/API/NodeList.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/* -*- mode: c++; c-basic-offset: 4 -*- */
+/* -*- mode: c; c-basic-offset: 4 -*- */
 /*
  * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
  *
