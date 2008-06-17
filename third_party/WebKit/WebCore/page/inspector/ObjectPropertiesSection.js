cd6dc9099f7e9c02aa7f71e25715bb9333ce8aa5
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-17 22:32:56 +0000

    Remember the expanded state of objects in the Scope Variables pane when stepping.         https://bugs.webkit.org/show_bug.cgi?id=19584
            Reviewed by Adam Roben.
    
            * page/inspector/ObjectPropertiesSection.js:
            (WebInspector.ObjectPropertiesSection): Add an optional argument
            that is the treeElementConstructor to use when making TreeElements.
            (WebInspector.ObjectPropertiesSection.prototype.onpopulate): Use
            the treeElementConstructor to create TreeElements.
            (WebInspector.ObjectPropertyTreeElement.prototype.onpopulate): Use
            the treeElementConstructor to create TreeElements.
            * page/inspector/ScopeChainSidebarPane.js:
            (WebInspector.ScopeChainSidebarPane.prototype.update): Create an
            _expandedProperties object on the callframe if one doesn't exist yet.
            Pass WebInspector.ScopeVariableTreeElement to the ObjectPropertiesSection
            as the TreeElement constructor we want to use.
            (WebInspector.ScopeVariableTreeElement): Added.
            (WebInspector.ScopeVariableTreeElement.prototype.onattach): Expand if the
            propertyIdentifier is in the pane's _expandedProperties.
            (WebInspector.ScopeVariableTreeElement.prototype.onexpand): Add the
            propertyIdentifier to the pane's _expandedProperties.
            (WebInspector.ScopeVariableTreeElement.prototype.oncollapse): Remove the
            propertyIdentifier from the pane's _expandedProperties.
            (WebInspector.ScopeVariableTreeElement.prototype.get propertyIdentifier):
            Return an identifier that has the section title, subtile and propertyPath concatenated.
            (WebInspector.ScopeVariableTreeElement.prototype.get propertyPath): Return a string
            that has the propertyNames up to the root ancestor concatenated with a period.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/ObjectPropertiesSection.js
+++ third_party/WebKit/WebCore/page/inspector/ObjectPropertiesSection.js
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-WebInspector.ObjectPropertiesSection = function(object, title, subtitle, emptyPlaceholder, ignoreHasOwnProperty, extraProperties)
+WebInspector.ObjectPropertiesSection = function(object, title, subtitle, emptyPlaceholder, ignoreHasOwnProperty, extraProperties, treeElementConstructor)
 {
     if (!title) {
         title = Object.describe(object);
@@ -39,6 +39,7 @@ WebInspector.ObjectPropertiesSection = function(object, title, subtitle, emptyPl
     this.object = object;
     this.ignoreHasOwnProperty = ignoreHasOwnProperty;
     this.extraProperties = extraProperties;
+    this.treeElementConstructor = treeElementConstructor || WebInspector.ObjectPropertyTreeElement;
 
     WebInspector.PropertiesSection.call(this, title, subtitle);
 }
@@ -63,7 +64,7 @@ WebInspector.ObjectPropertiesSection.prototype = {
                 continue;
             if (!this.ignoreHasOwnProperty && "hasOwnProperty" in object && !object.hasOwnProperty(propertyName))
                 continue;
-            this.propertiesTreeOutline.appendChild(new WebInspector.ObjectPropertyTreeElement(object, propertyName));
+            this.propertiesTreeOutline.appendChild(new this.treeElementConstructor(object, propertyName));
         }
 
         if (!this.propertiesTreeOutline.children.length) {
@@ -124,7 +125,7 @@ WebInspector.ObjectPropertyTreeElement.prototype = {
             var propertyName = properties[i];
             if (propertyName === "__treeElementIdentifier")
                 continue;
-            this.appendChild(new WebInspector.ObjectPropertyTreeElement(childObject, propertyName));
+            this.appendChild(new this.treeOutline.section.treeElementConstructor(childObject, propertyName));
         }
     }
 }
