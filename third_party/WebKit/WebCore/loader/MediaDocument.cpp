ceb6fa39660d2c69417c08db3241d440ed34339d
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-18 01:00:33 +0000

    2008-06-17  Adele Peterson  <adele@apple.com>
            Attempt to fix builds by wrapping new code in #if ENABLE(VIDEO)
    
            * loader/MediaDocument.cpp:
            * loader/MediaDocument.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34639 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/MediaDocument.cpp
+++ third_party/WebKit/WebCore/loader/MediaDocument.cpp
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+
+#if ENABLE(VIDEO)
 #include "MediaDocument.h"
 
 #include "Element.h"
@@ -129,3 +131,4 @@ Tokenizer* MediaDocument::createTokenizer()
 }
     
 }
+#endif
--- third_party/WebKit/WebCore/loader/MediaDocument.h
+++ third_party/WebKit/WebCore/loader/MediaDocument.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MediaDocument_h
 #define MediaDocument_h
 
+#if ENABLE(VIDEO)
+
 #include "HTMLDocument.h"
 
 namespace WebCore {
@@ -47,4 +49,5 @@ private:
     
 }
 
-#endif // ImageDocument_h
+#endif
+#endif
