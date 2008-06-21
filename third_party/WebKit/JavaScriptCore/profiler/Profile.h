e0456a2fc4ae0b8be493c5e5e978d21cdc46efe2
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-21 04:39:23 +0000

    Fixed an ASSERT(m_actualSelfTime <= m_actualTotalTime) when starting         and stopping a profile from the Develop menu. Also prevents         inserting an incorrect parent node as the new head after profiling         is stopped from the Develop menu.
            Reviewed by Dan Bernstein.
    
            * profiler/Profile.cpp:
            (KJS::Profile::stopProfiling): If the current node is already the head
            then there is no more need to record future nodes in didExecute.
            (KJS::Profile::didExecute): Move the code of setupCurrentNodeAsStopped
            into here since this was the only caller. When setting the total time
            keep any current total time while adding the self time of the head.
            (KJS::Profile::setupCurrentNodeAsStopped): Removed.
            * profiler/Profile.h: Removed setupCurrentNodeAsStopped.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34710 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/profiler/Profile.h
+++ third_party/WebKit/JavaScriptCore/profiler/Profile.h
@@ -79,7 +79,6 @@ namespace KJS {
         Profile(const UString& title, ExecState* originatingGlobalExec, unsigned pageGroupIdentifier, ProfilerClient*);
         void removeProfileStart();
         void removeProfileEnd();
-        void setupCurrentNodeAsStopped();
 
         UString m_title;
         ExecState* m_originatingGlobalExec;
