0376877113a2a95ff398624e6f29801e7e79510b
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-23 16:24:24 +0000

    2008-06-23  Anders Carlsson  <andersca@apple.com>
            Reviewed by Geoff.
    
            Make changedDocuments a HashSet.
    
            * dom/Document.cpp:
            (WebCore::Document::setDocumentChanged):
            (WebCore::Document::updateDocumentsRendering):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34739 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -256,7 +256,7 @@ static bool acceptsEditingFocus(Node *node)
     return frame->editor()->shouldBeginEditing(rangeOfContents(root).get());
 }
 
-static DeprecatedPtrList<Document>* changedDocuments = 0;
+static HashSet<Document*>* changedDocuments = 0;
 
 Document::Document(Frame* frame, bool isXHTML)
     : ContainerNode(0)
@@ -1064,8 +1064,8 @@ void Document::setDocumentChanged(bool b)
     if (b) {
         if (!m_docChanged) {
             if (!changedDocuments)
-                changedDocuments = new DeprecatedPtrList<Document>;
-            changedDocuments->append(this);
+                changedDocuments = new HashSet<Document*>;
+            changedDocuments->add(this);
         }
         if (m_accessKeyMapValid) {
             m_accessKeyMapValid = false;
@@ -1175,7 +1175,11 @@ void Document::updateDocumentsRendering()
     if (!changedDocuments)
         return;
 
-    while (Document* doc = changedDocuments->take()) {
+    while (changedDocuments->size()) {
+        HashSet<Document*>::iterator it = changedDocuments->begin();
+        Document* doc = *it;
+        changedDocuments->remove(it);
+        
         doc->m_docChanged = false;
         doc->updateRendering();
     }
