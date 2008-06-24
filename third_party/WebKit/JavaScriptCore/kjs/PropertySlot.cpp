a536c94d65913e38f84a617f95a4e32a225f5fb2
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-24 10:27:40 +0000

    Fix the Qt build, adapt to various JSCore API changes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@34762 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/PropertySlot.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/PropertySlot.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "PropertySlot.h"
 
+#include "JSFunction.h"
 #include "JSGlobalObject.h"
 #include "JSObject.h"
 #include "RegisterFileStack.h"
--- third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HitTestResult.h"
 
 #include "runtime.h"
+#include "runtime_object.h"
 #include "runtime_root.h"
 #include "JSDOMWindow.h"
 #include "qt_instance.h"
