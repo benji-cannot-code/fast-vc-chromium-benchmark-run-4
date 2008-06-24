9527ca45910119fa09fe369384ddafb35fe7bbce
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-24 11:28:55 +0000

    2008-06-24  Jonathon Jongsma  <jonathon.jongsma@collabora.co.uk>
            Reviewed by Darin.
    
            https://bugs.webkit.org/show_bug.cgi?id=19727
    
            Return bool from GraphicsContext::getShadow() so the tests aren't duplicated so
            many times in Cairo and Qt ports.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34765 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/GraphicsContext.h
+++ third_party/WebKit/WebCore/platform/graphics/GraphicsContext.h
@@ -198,7 +198,7 @@ namespace WebCore {
         void endTransparencyLayer();
 
         void setShadow(const IntSize&, int blur, const Color&);
-        void getShadow(IntSize&, int&, Color&);
+        bool getShadow(IntSize&, int&, Color&) const;
         void clearShadow();
 
         void initFocusRing(int width, int offset);
