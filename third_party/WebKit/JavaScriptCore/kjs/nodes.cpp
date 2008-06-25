9e34b11afee34947fa7708e67d849c93469169bf
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-25 00:41:40 +0000

    2008-06-24  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
            Reviewed by Maciej.
    
            Make the conversion of the pair (less, jtrue) to jless use register
            reference counting information for safety instead of requiring callers
            to decide whether it is safe.
    
            No changes on SunSpider codegen.
    
            * VM/CodeGenerator.cpp:
            (KJS::CodeGenerator::emitJumpIfTrue):
            * VM/CodeGenerator.h:
            * kjs/nodes.cpp:
            (KJS::DoWhileNode::emitCode):
            (KJS::WhileNode::emitCode):
            (KJS::ForNode::emitCode):
            (KJS::CaseBlockNode::emitCodeForBlock):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34784 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
@@ -1201,7 +1201,7 @@ RegisterID* DoWhileNode::emitCode(CodeGenerator& generator, RegisterID* dst)
     
     generator.emitLabel(continueTarget.get());
     RegisterID* cond = generator.emitNode(m_expr.get());
-    generator.emitJumpIfTrueMayCombine(cond, topOfLoop.get());
+    generator.emitJumpIfTrue(cond, topOfLoop.get());
     generator.emitLabel(breakTarget.get());
     return result.get();
 }
@@ -1223,7 +1223,7 @@ RegisterID* WhileNode::emitCode(CodeGenerator& generator, RegisterID* dst)
 
     generator.emitLabel(continueTarget.get());
     RegisterID* cond = generator.emitNode(m_expr.get());
-    generator.emitJumpIfTrueMayCombine(cond, topOfLoop.get());
+    generator.emitJumpIfTrue(cond, topOfLoop.get());
 
     generator.emitLabel(breakTarget.get());
     
@@ -1255,7 +1255,7 @@ RegisterID* ForNode::emitCode(CodeGenerator& generator, RegisterID* dst)
     generator.emitLabel(beforeCondition.get());
     if (m_expr2) {
         RegisterID* cond = generator.emitNode(m_expr2.get());
-        generator.emitJumpIfTrueMayCombine(cond, topOfLoop.get());
+        generator.emitJumpIfTrue(cond, topOfLoop.get());
     } else {
         generator.emitJump(topOfLoop.get());
     }
@@ -1423,14 +1423,14 @@ RegisterID* CaseBlockNode::emitCodeForBlock(CodeGenerator& generator, RegisterID
         RegisterID* clauseVal = generator.emitNode(list->getClause()->expr());
         generator.emitBinaryOp(op_stricteq, clauseVal, clauseVal, switchExpression);
         labelVector.append(generator.newLabel());
-        generator.emitJumpIfTrueMayCombine(clauseVal, labelVector[labelVector.size() - 1].get());
+        generator.emitJumpIfTrue(clauseVal, labelVector[labelVector.size() - 1].get());
     }
 
     for (ClauseListNode* list = m_list2.get(); list; list = list->getNext()) {
         RegisterID* clauseVal = generator.emitNode(list->getClause()->expr());
         generator.emitBinaryOp(op_stricteq, clauseVal, clauseVal, switchExpression);
         labelVector.append(generator.newLabel());
-        generator.emitJumpIfTrueMayCombine(clauseVal, labelVector[labelVector.size() - 1].get());
+        generator.emitJumpIfTrue(clauseVal, labelVector[labelVector.size() - 1].get());
     }
 
     RefPtr<LabelID> defaultLabel;
