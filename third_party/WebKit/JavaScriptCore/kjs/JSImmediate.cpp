386f1c30d1eab83f2b2c5122ee9af6b0e6262a3d
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-27 04:44:43 +0000

    Speculative fix for the Windows build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@34825 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/JSImmediate.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/JSImmediate.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSImmediate.h"
 
 #include "BooleanObject.h"
+#include "JSGlobalObject.h"
 #include "JSNotAnObject.h"
 #include "NumberObject.h"
 
