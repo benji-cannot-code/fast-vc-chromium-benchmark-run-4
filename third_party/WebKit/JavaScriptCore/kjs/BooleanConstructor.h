85b0267b35acbe15ac74283398031f3f74bcf540
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-28 05:20:06 +0000

    2008-06-27  Sam Weinig  <sam@webkit.org>
            Reviewed by Oliver Hunt.
    
            Fix platforms that don't use AllInOne.cpp
    
            * kjs/BooleanConstructor.h:
            * kjs/BooleanPrototype.h:
            * kjs/FunctionPrototype.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34844 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/BooleanConstructor.h
+++ third_party/WebKit/JavaScriptCore/kjs/BooleanConstructor.h
@@ -22,9 +22,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BooleanConstructor_h
 #define BooleanConstructor_h
 
-#include "FunctionPrototype.h"
+#include "JSFunction.h"
 
 namespace KJS {
+
+    class BooleanPrototype;
+    class FunctionPrototype;
 
     /**
      * @internal
--- third_party/WebKit/JavaScriptCore/kjs/FunctionPrototype.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/FunctionPrototype.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSGlobalObject.h"
 #include "Parser.h"
 #include "debugger.h"
+#include "JSArray.h"
 #include "JSFunction.h"
 #include "JSString.h"
 #include "lexer.h"
