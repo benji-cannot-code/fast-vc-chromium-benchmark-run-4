e1ca9b4205425b6e68423475ebf3a52c97f83ed1
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-29 20:25:04 +0000

    2008-06-29  Sam Weinig  <sam@webkit.org>
            Fix Tiger build.
    
            * DumpRenderTree/mac/EventSendingController.mm:
            (eventTypeForMouseButtonAndAction):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34874 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/EventSendingController.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/EventSendingController.mm
@@ -205,7 +205,7 @@ BOOL replayingSavedEvents;
 
 static NSEventType eventTypeForMouseButtonAndAction(int button, MouseAction action)
 {
-    switch(button) {
+    switch (button) {
         case LeftMouseButton:
             switch (action) {
                 case MouseDown:
@@ -235,7 +235,7 @@ static NSEventType eventTypeForMouseButtonAndAction(int button, MouseAction acti
             }
     }
     assert(0);
-    return 0;
+    return static_cast<NSEventType>(0);
 }
 
 - (void)updateClickCountForButton:(int)buttonNumber
