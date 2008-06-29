aace7a5fdef4fd6dd59281016931b29fc81c280c
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-29 22:18:38 +0000

    2008-06-29  Sam Weinig  <sam@webkit.org>
            Build fix for Qt.
    
            * kjs/DateMath.cpp:
            * kjs/DatePrototype.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/DateMath.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/DateMath.cpp
@@ -52,6 +52,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/MathExtras.h>
 #include <wtf/StringExtras.h>
 
+#if HAVE(ERRNO_H)
+#include <errno.h>
+#endif
+
 #if PLATFORM(DARWIN)
 #include <notify.h>
 #endif
--- third_party/WebKit/JavaScriptCore/kjs/DatePrototype.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/DatePrototype.cpp
@@ -37,10 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/StringExtras.h>
 #include <wtf/UnusedParam.h>
 
-#if HAVE(ERRNO_H)
-#include <errno.h>
-#endif
-
 #if HAVE(SYS_PARAM_H)
 #include <sys/param.h>
 #endif
