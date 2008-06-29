7947e6b209d08d97217e59be5abfb2f1f29faa89
jmalonzo@webkit.org <jmalonzo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-29 08:19:00 +0000

    2008-06-29  Jan Michael Alonzo  <jmalonzo@webkit.org>
            Rubber-stamped by Cameron Zwarich
    
            Fix Gtk non-AllInOne build
    
            * GNUmakefile.am: include JSVariableObject.cpp
            * kjs/RegExpConstructor.cpp: include RegExpObject.h
            * kjs/RegExpObject.h: forward declare RegExpPrototype
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34870 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/RegExpObject.h
+++ third_party/WebKit/JavaScriptCore/kjs/RegExpObject.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace KJS {
 
+    class RegExpPrototype;
+
     class RegExpObject : public JSObject {
     public:
         enum { Global, IgnoreCase, Multiline, Source, LastIndex };
