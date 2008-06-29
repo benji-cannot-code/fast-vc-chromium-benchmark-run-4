b37888e91b2de407c43d01057aae79af9c2e8b18
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-29 19:24:59 +0000

    Reviewed by darin.
            Add multi-button mouseevent support to DRT
            https://bugs.webkit.org/show_bug.cgi?id=15173
    
            It's now possible to specify the mouse button with:
            eventSender.mouseDown(1); eventSender.mouseUp(1); etc.
    
            * DumpRenderTree/DumpRenderTree.xcodeproj/project.pbxproj:
            * DumpRenderTree/mac/EventSendingController.h:
            * DumpRenderTree/mac/EventSendingController.mm:
            (+[EventSendingController isSelectorExcludedFromWebScript:]):
            (+[EventSendingController webScriptNameForSelector:]):
            (eventTypeForMouseButtonAndAction):
            (-[EventSendingController updateClickCountForButton:]):
            (-[EventSendingController mouseDown:]):
            (-[EventSendingController mouseUp:]):
            (-[EventSendingController mouseMoveToX:Y:]):
            (-[EventSendingController contextClick]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34871 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/EventSendingController.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/EventSendingController.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface EventSendingController : NSObject <DOMEventListener>
 {
-    BOOL down;
+    BOOL leftMouseButtonDown;
     BOOL dragMode;
     int clickCount;
     NSTimeInterval lastClick;
