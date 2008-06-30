6b9edcf9d776ce7696558b1660e77e7b3ce91b9a
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-06-30 08:09:28 +0000

    2008-06-30  Simon Hausmann  <hausmann@webkit.org>
            Fix the Qt build.
    
            Added missing includes and changed getItem array calls to use get()
            instead.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34884 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
+++ third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
@@ -21,10 +21,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "qt_runtime.h"
 #include "qt_instance.h"
+#include "JSGlobalObject.h"
 #include "JSObject.h"
 #include "JSArray.h"
 #include "date_object.h"
+#include "DatePrototype.h"
 #include "DateMath.h"
+#include "ObjectPrototype.h"
+#include "RegExpConstructor.h"
 #include "RegExpObject.h"
 #include <runtime.h>
 #include <runtime_object.h>
@@ -328,7 +332,7 @@ QVariant convertValueToQVariant(ExecState* exec, JSValue* value, QMetaType::Type
                 int objdist = 0;
                 qConvDebug() << "converting a " << len << " length Array";
                 for (int i = 0; i < len; ++i) {
-                    JSValue *val = array->getItem(i);
+                    JSValue *val = array->get(exec, i);
                     result.append(convertValueToQVariant(exec, val, QMetaType::Void, &objdist, visitedObjects));
                     if (objdist == -1) {
                         qConvDebug() << "Failed converting element at index " << i;
@@ -376,7 +380,7 @@ QVariant convertValueToQVariant(ExecState* exec, JSValue* value, QMetaType::Type
                 QStringList result;
                 int len = array->getLength();
                 for (int i = 0; i < len; ++i) {
-                    JSValue* val = array->getItem(i);
+                    JSValue* val = array->get(exec, i);
                     UString ustring = val->toString(exec);
                     QString qstring = QString::fromUtf16((const ushort*)ustring.rep()->data(),ustring.size());
 
@@ -598,7 +602,7 @@ QVariant convertValueToQVariant(ExecState* exec, JSValue* value, QMetaType::Type
                     QObjectList result;
                     int len = array->getLength();
                     for (int i = 0; i < len; ++i) {
-                        JSValue* val = array->getItem(i);
+                        JSValue* val = array->get(exec, i);
                         int itemdist = -1;
                         QVariant item = convertValueToQVariant(exec, val, QMetaType::QObjectStar, &itemdist, visitedObjects);
                         if (itemdist >= 0)
@@ -649,7 +653,7 @@ QVariant convertValueToQVariant(ExecState* exec, JSValue* value, QMetaType::Type
                     QList<int> result;
                     int len = array->getLength();
                     for (int i = 0; i < len; ++i) {
-                        JSValue* val = array->getItem(i);
+                        JSValue* val = array->get(exec, i);
                         int itemdist = -1;
                         QVariant item = convertValueToQVariant(exec, val, QMetaType::Int, &itemdist, visitedObjects);
                         if (itemdist >= 0)
