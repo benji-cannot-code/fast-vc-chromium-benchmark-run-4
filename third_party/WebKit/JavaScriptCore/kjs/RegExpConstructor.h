7ed73db0e00051d348dff3ad2fa14aa896938777
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-01 07:07:06 +0000

    Build fixes, include OwnPtr.h and DateInstance.h
    git-svn-id: svn://svn.chromium.org/blink/trunk@34908 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/RegExpConstructor.h
+++ third_party/WebKit/JavaScriptCore/kjs/RegExpConstructor.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RegExpConstructor_h
 
 #include "InternalFunction.h"
+#include <wtf/OwnPtr.h>
 
 namespace KJS {
 
