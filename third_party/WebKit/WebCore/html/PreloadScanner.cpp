f9cff597e73c3e5a023c051619e82cde0cc33c6f
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-01 12:01:38 +0000

    Reviewed by Antti.
            <rdar://problem/6033080> REGRESSION: Some CH characters display as garbage on webpage for
            the first time.
    
            Test: fast/encoding/preload-encoding.html
    
            * html/PreloadScanner.cpp:
            (WebCore::PreloadScanner::tokenize): Reset charset when entering a tag, not just when
            starting a load.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34914 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/PreloadScanner.cpp
+++ third_party/WebKit/WebCore/html/PreloadScanner.cpp
@@ -346,11 +346,13 @@ void PreloadScanner::tokenize(const SegmentedString& source)
                     m_state = CloseTagOpen;
                 else if (cc >= 'A' && cc <= 'Z') {
                     m_tagName.clear();
+                    m_charset = String();
                     m_tagName.append(cc + 0x20);
                     m_closeTag = false;
                     m_state = TagName;
                 } else if (cc >= 'a' && cc <= 'z') {
                     m_tagName.clear();
+                    m_charset = String();
                     m_tagName.append(cc);
                     m_closeTag = false;
                     m_state = TagName;
@@ -390,11 +392,13 @@ void PreloadScanner::tokenize(const SegmentedString& source)
             }
             if (cc >= 'A' && cc <= 'Z') {
                 m_tagName.clear();
+                m_charset = String();
                 m_tagName.append(cc + 0x20);
                 m_closeTag = true;
                 m_state = TagName;
             } else if (cc >= 'a' && cc <= 'z') {
                 m_tagName.clear();
+                m_charset = String();
                 m_tagName.append(cc);
                 m_closeTag = true;
                 m_state = TagName;
