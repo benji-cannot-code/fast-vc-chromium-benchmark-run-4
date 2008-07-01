40f2ed36690c3610bd88ff256b82601cc6ca614e
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-01 17:39:46 +0000

    2008-07-01  Sam Weinig  <sam@webkit.org>
            Fix non-AllInOne builds.
    
            * kjs/DateMath.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34922 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/DateMath.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/DateMath.cpp
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "DateMath.h"
 
-#include "JSValue.h"
+#include "JSNumberCell.h"
 #include <math.h>
 #include <stdint.h>
 #include <time.h>
