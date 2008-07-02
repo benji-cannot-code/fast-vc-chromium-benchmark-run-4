16258368d1c59e2349928670a07eade43511ba43
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-02 07:33:38 +0000

    Non-AllInOne build fix.
            * kjs/interpreter.cpp: Include JSLock.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/interpreter.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/interpreter.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ExecState.h"
 #include "JSGlobalObject.h"
+#include "JSLock.h"
 #include "Machine.h"
 #include "Parser.h"
 #include "completion.h"
