0014b4aa31ec8eb8f8aff2711a0e2e96981471a4
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-02 07:57:56 +0000

    Another non-AllInOne build fix.
            * kjs/JSGlobalObject.cpp: Include JSLock.h here, too.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34950 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/JSGlobalObject.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/JSGlobalObject.cpp
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ErrorPrototype.h"
 #include "FunctionConstructor.h"
 #include "FunctionPrototype.h"
+#include "JSLock.h"
 #include "Machine.h"
 #include "MathObject.h"
 #include "NativeErrorConstructor.h"
