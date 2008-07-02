bef657f1913fda43699e5b3096644937e6cdecaa
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-02 02:12:59 +0000

    Update the path to the configuration file.
    git-svn-id: svn://svn.chromium.org/blink/trunk@34941 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitSite/blog/wp-config.php
+++ third_party/WebKit/WebKitSite/blog/wp-config.php
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-require('/var/opendarwin/secrets/webkit-blog-config.php');
-/* Stop editing */
+require('/www/auth/webkit-blog-config.php');
 
 define('WP_CACHE', true); //Added by WP-Cache Manager
 define('ABSPATH', dirname(__FILE__).'/');
