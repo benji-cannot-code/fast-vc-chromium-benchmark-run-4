1c383d0ceb18ab7612175783b64878e79ae4c853
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-03 08:06:24 +0000

    2008-07-03  Simon Hausmann  <hausmann@webkit.org>
            Reviewed by Alexey Proskuryakov.
    
            Fix the non-threaded build.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34979 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/JSGlobalData.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/JSGlobalData.cpp
@@ -153,8 +153,8 @@ JSGlobalData*& JSGlobalData::threadInstanceInternal()
     static ThreadSpecific<DataInstance> threadInstance;
     return *threadInstance;
 #else
-    static JSGlobalData threadInstance;
-    return &threadInstance;
+    static JSGlobalData* threadInstance;
+    return threadInstance;
 #endif
 }
 
