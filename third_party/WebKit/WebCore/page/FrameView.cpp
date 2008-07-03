1f92cafd62cba0e58d055a21ec233083ccfb20a6
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-03 19:35:40 +0000

    2008-07-03  David Hyatt  <hyatt@apple.com>
            Revise Dan's fix for an assert on Windows, since layoutIfNeededRecursive doesn't exist on the
            Mac.  Revert updateControlTints and add the layout call outside of it in the already-existing
            !PLATFORM(MAC) ifdef in FocusController's setActive method.
    
            Reviewed by ggaren, weinig
    
            * page/FocusController.cpp:
            (WebCore::FocusController::setActive):
            * page/FrameView.cpp:
            (WebCore::FrameView::updateControlTints):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34987 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -1106,7 +1106,8 @@ void FrameView::updateControlTints()
         return;
     
     if (theme()->supportsControlTints() && m_frame->contentRenderer()) {
-        layoutIfNeededRecursive();
+        if (needsLayout())
+            layout();
         PlatformGraphicsContext* const noContext = 0;
         GraphicsContext context(noContext);
         context.setUpdatingControlTints(true);
