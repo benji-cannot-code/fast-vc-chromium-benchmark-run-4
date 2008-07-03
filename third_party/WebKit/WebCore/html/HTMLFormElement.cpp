6b1cc428319654aec53a218e518be9727f57116c
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-03 19:13:37 +0000

    Reviewed by Darin.
            https://bugs.webkit.org/show_bug.cgi?id=19884
            Locating a store at www.366.ru doesn't work
    
            Test: fast/forms/submit-with-base.html
    
            * html/HTMLFormElement.cpp: (WebCore::HTMLFormElement::submit): Treat empty action the same
            as missing one - ignore base URL.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@34986 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLFormElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLFormElement.cpp
@@ -462,7 +462,7 @@ void HTMLFormElement::submit(Event* event, bool activateSubmitButton)
     if (needButtonActivation && firstSuccessfulSubmitButton)
         firstSuccessfulSubmitButton->setActivatedSubmit(true);
     
-    if (!m_url)
+    if (m_url.isEmpty())
         m_url = document()->url().string();
 
     if (m_post) {
