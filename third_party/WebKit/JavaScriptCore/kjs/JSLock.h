c01cf91b5f5f21fda6d47234bea5b0a56626a761
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-04 00:06:11 +0000

    Follow-up to the 64-bit build fix.  Use intptr_t rather than ssize_t as the latter is non-standard and does not exist on Windows.
    git-svn-id: svn://svn.chromium.org/blink/trunk@34991 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/JSLock.h
+++ third_party/WebKit/JavaScriptCore/kjs/JSLock.h
@@ -85,7 +85,7 @@ namespace KJS {
         static void lock(ExecState*);
         static void unlock(ExecState*);
 
-        static ssize_t lockCount();
+        static intptr_t lockCount();
         static bool currentThreadIsHoldingLock();
 
         static void registerThread();
@@ -99,7 +99,7 @@ namespace KJS {
             ~DropAllLocks();
             
         private:
-            ssize_t m_lockCount;
+            intptr_t m_lockCount;
             bool m_lockingForReal;
         };
     };
