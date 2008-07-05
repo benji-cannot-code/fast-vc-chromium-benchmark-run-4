834c561f80dfbc267a16ce853460bd16771fe1a6
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-05 05:59:02 +0000

    2008-07-04  Sam Weinig  <sam@webkit.org>
            Fix Qt build.
    
            * bridge/qt/qt_instance.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35010 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_instance.cpp
+++ third_party/WebKit/WebCore/bridge/qt/qt_instance.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PropertyNameArray.h"
 #include "runtime_object.h"
 #include "ObjectPrototype.h"
+#include "Error.h"
 
 #include <qmetaobject.h>
 #include <qdebug.h>
