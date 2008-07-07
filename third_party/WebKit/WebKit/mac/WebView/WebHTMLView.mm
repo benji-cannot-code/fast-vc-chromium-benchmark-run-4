6e4b0eba0a651576c41692a89da467db3a20fda6
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-07 22:02:19 +0000

    2008-07-07  Anders Carlsson  <andersca@apple.com>
            Reviewed by Darin.
    
            Speculative fix for     
            <rdar://problem/5839800>
            CrashTracer: [USER] 5802 crashes in Safari at com.apple.WebKit: -[WebHTMLView(WebPrivate) _updateMouseoverWithFakeEvent] + 389
            
            Set _private->closed to YES before calling -[WebPluginController destroyAllPlugins].
            
            My theory is that the plug-in destruction callbacks could end up rescheduling timers or re-adding notifications.
            This is usually protected by _private->closed, but in this case it might still be false.
    
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView close]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35043 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
@@ -1835,6 +1835,9 @@ static void _updateMouseoverTimerCallback(CFRunLoopTimerRef timer, void *info)
     // out the archived WebHTMLView and recreating a new one if needed. So close doesn't need to do anything in that case.
     if (!_private || _private->closed)
         return;
+
+    _private->closed = YES;
+
     [self _cancelUpdateMouseoverTimer];
     [self _cancelUpdateFocusedAndActiveStateTimer];
     [self _clearLastHitViewIfSelf];
@@ -1845,7 +1848,7 @@ static void _updateMouseoverTimerCallback(CFRunLoopTimerRef timer, void *info)
     // remove tooltips before clearing _private so removeTrackingRect: will work correctly
     [self removeAllToolTips];
     [_private clear];
-    _private->closed = YES;
+    
     Page* page = core([self _webView]);
     if (page)
         page->dragController()->setDraggingImageURL(KURL());
