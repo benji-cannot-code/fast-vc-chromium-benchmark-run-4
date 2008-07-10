aca3582efb3d819b7df14648cdb2d4458b0989b2
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-10 23:21:15 +0000

    WebCore:
    2008-07-10  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Dave Hyatt and Darin Adler.
    
            Add support for calling querySelector and querySelectorAll on DocumentFragments
    
            - Fixes bug where nodes not in the document tree would not match based on ID due
              to over optimization.
    
            Test: fast/dom/SelectorAPI/detached-element.html
    
            * GNUmakefile.am:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * WebCoreSources.bkl:
            * bindings/js/JSDocumentFragmentCustom.cpp: Added.
            (WebCore::JSDocumentFragment::querySelector): Add custom code matching JSElement
            and JSDocument that checks for a 2nd arguments and throws an exception indicating
            we do not currently support the optional NSResolver part of the Selectors API spec.
            (WebCore::JSDocumentFragment::querySelectorAll): Ditto.
            * dom/DocumentFragment.idl: Add querySelector and querySelectorAll declarations.
    
            * dom/Node.cpp:
            (WebCore::Node::querySelector): Make the CSS parser parse the selector
            according to the strictness of the document so that mixed case ID selectors
            match in quirks mode.  Also, don't use the fast ID path if the root is not
            in the DOM tree, as it won't work.
            (WebCore::Node::querySelectorAll): Ditto.
            * dom/SelectorNodeList.cpp:
            (WebCore::createSelectorNodeList): Don't use the fast ID path if the root is not
            in the DOM tree, as it won't work.
    
    LayoutTests:
    
    2008-07-10  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Dave Hyatt and Darin Adler,
    
            Add support for calling querySelector and querySelectorAll on DocumentFragments
    
            - Add test for nodes not in the document tree.
    
            * fast/dom/SelectorAPI/detached-element-expected.txt: Added.
            * fast/dom/SelectorAPI/detached-element.html: Added.
            * fast/dom/SelectorAPI/dumpNodeList-expected.txt:
            * fast/dom/SelectorAPI/dumpNodeList.html:
            * fast/dom/SelectorAPI/not-supported-NSResolver-expected.txt:
            * fast/dom/SelectorAPI/not-supported-NSResolver.html:
            * fast/dom/SelectorAPI/resources/detached-element.js: Added.
            * fast/dom/Window/window-properties-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35108 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/SelectorAPI/resources/detached-element.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This tests that querySelector and querySelectorAll work with elements that are not in a document yet."
+);
+
+var root = document.createElement('div');
+var correctNode = document.createElement('div');
+correctNode.setAttribute("id", "testId")
+root.appendChild(correctNode);
+var noChild = document.createElement('div');
+
+shouldBe("root.querySelector('div')", "correctNode");
+shouldBe("root.querySelector('#testId')", "correctNode");
+
+shouldBe("root.querySelectorAll('div').length", "1");
+shouldBe("root.querySelectorAll('div').item(0)", "correctNode");
+shouldBe("root.querySelectorAll('#testId').length", "1");
+shouldBe("root.querySelectorAll('#testId').item(0)", "correctNode");
+
+shouldBeNull("noChild.querySelector('div')");
+shouldBe("noChild.querySelectorAll('div').length", "0");
+
+var successfullyParsed = true;
