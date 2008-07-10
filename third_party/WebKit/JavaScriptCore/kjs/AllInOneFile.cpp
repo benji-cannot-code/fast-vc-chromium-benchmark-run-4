dd24e61f840da2a68de2a8421d7d5f8ee0ae78fc
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-10 17:55:26 +0000

    Reviewed by Mark Rowe.
            Eliminate CollectorHeapIntrospector.
    
            CollectorHeapIntrospector was added primarily in the hopes to improve leaks tool output,
            a result that it didn't deliver. Also, it helped by labeling JSC heap regions as reported by
            vmmap tool, but at the same time, it made them mislabeled as malloc'd ones - the correct
            way to label mapped regions is to use a VM tag.
    
            So, it makes more sense to remove it completely than to make it work with multiple heaps.
    
            * JavaScriptCore.exp:
            * JavaScriptCore.xcodeproj/project.pbxproj:
            * kjs/AllInOneFile.cpp:
            * kjs/InitializeThreading.cpp:
            (KJS::initializeThreading):
            * kjs/collector.cpp:
            * kjs/collector.h:
            * kjs/CollectorHeapIntrospector.cpp: Removed.
            * kjs/CollectorHeapIntrospector.h: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35097 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/AllInOneFile.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/AllInOneFile.cpp
@@ -41,9 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "BooleanObject.cpp"
 #include "BooleanPrototype.cpp"
 #include "collector.cpp"
-#if PLATFORM(DARWIN)
-#include "CollectorHeapIntrospector.cpp"
-#endif
 #include "CommonIdentifiers.cpp"
 #include "DateConstructor.cpp"
 #include "DateMath.cpp"
--- third_party/WebKit/JavaScriptCore/kjs/collector.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/collector.cpp
@@ -50,8 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <mach/thread_act.h>
 #include <mach/vm_map.h>
 
-#include "CollectorHeapIntrospector.h"
-
 #elif PLATFORM(WIN_OS)
 
 #include <windows.h>
@@ -118,8 +116,9 @@ Heap::~Heap()
 
 static NEVER_INLINE CollectorBlock* allocateBlock()
 {
-#if PLATFORM(DARWIN)    
+#if PLATFORM(DARWIN)
     vm_address_t address = 0;
+    // FIXME: tag the region as a JavaScriptCore heap when we get a registered VM tag: <rdar://problem/6054788>.
     vm_map(current_task(), &address, BLOCK_SIZE, BLOCK_OFFSET_MASK, VM_FLAGS_ANYWHERE, MEMORY_OBJECT_NULL, 0, FALSE, VM_PROT_DEFAULT, VM_PROT_DEFAULT, VM_INHERIT_DEFAULT);
 #elif PLATFORM(WIN_OS)
      // windows virtual address granularity is naturally 64k
@@ -489,13 +488,6 @@ void Heap::registerThread()
     }
 }
 
-#if PLATFORM(DARWIN)
-void Heap::initializeHeapIntrospector()
-{
-    ASSERT(pthread_main_np());
-    CollectorHeapIntrospector::init(&primaryHeap, &numberHeap);
-}
-#endif
 #endif
 
 #define IS_POINTER_ALIGNED(p) (((intptr_t)(p) & (sizeof(char*) - 1)) == 0)
