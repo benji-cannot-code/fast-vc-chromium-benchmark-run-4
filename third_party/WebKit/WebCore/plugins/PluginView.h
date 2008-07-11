c4da6e85ef0fbb0c8714c033376fd3a53f6fd9e8
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-11 11:28:16 +0000

    2008-07-11  Ariya Hidayat  <ariya.hidayat@trolltech.com>
            Reviewed by Adam Roben.
    
            Fix compile with MinGW since it does not like friend static function.
    
            * plugins/PluginView.h:
            * plugins/win/PluginViewWin.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35124 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/PluginView.h
+++ third_party/WebKit/WebCore/plugins/PluginView.h
@@ -57,7 +57,7 @@ namespace WebCore {
     class KeyboardEvent;
     class MouseEvent;
     class KURL;
-#if PLATFORM(WIN)
+#if PLATFORM(WIN_OS)
     class PluginMessageThrottlerWin;
 #endif
     class PluginPackage;
@@ -92,10 +92,6 @@ namespace WebCore {
     };
 
     class PluginView : public Widget, private PluginStreamClient {
-#if PLATFORM(WIN)
-    friend static LRESULT CALLBACK PluginViewWndProc(HWND, UINT, WPARAM, LPARAM);
-#endif
-
     public:
         static PluginView* create(Frame* parentFrame, const IntSize&, Element*, const KURL&, const Vector<String>& paramNames, const Vector<String>& paramValues, const String& mimeType, bool loadManually);
         virtual ~PluginView();
@@ -157,7 +153,8 @@ namespace WebCore {
 
         virtual bool isPluginView() const { return true; }
 
-#if PLATFORM(WIN)
+#if PLATFORM(WIN_OS)
+        static LRESULT CALLBACK PluginViewWndProc(HWND, UINT, WPARAM, LPARAM);
         LRESULT wndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
         WNDPROC pluginWndProc() const { return m_pluginWndProc; }
 #endif
@@ -239,7 +236,7 @@ namespace WebCore {
         bool m_needsXEmbed;
 #endif
 
-#if PLATFORM(WIN)
+#if PLATFORM(WIN_OS)
         OwnPtr<PluginMessageThrottlerWin> m_messageThrottler;
         WNDPROC m_pluginWndProc;
         unsigned m_lastMessage;
