dfae06471b93f3ca1a81955b4938def0e3f805d0
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-11 22:34:42 +0000

    2008-07-11  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dave Hyatt
    
            Bug 18885: RenderLayer::enclosingPositionedAncestor() should
            look for transforms, since transforms create containing
            blocks.
            <https://bugs.webkit.org/show_bug.cgi?id=18885>
    
            Test: fast/transforms/transform-positioned-ancestor.html
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::stackingContext):
            (WebCore::RenderLayer::enclosingPositionedAncestor):
            (WebCore::RenderLayer::enclosingTransformedAncestor):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35138 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayer.cpp
@@ -426,15 +426,15 @@ RenderLayer *RenderLayer::stackingContext() const
     RenderLayer* curr = parent();
     for ( ; curr && !curr->m_object->isRenderView() && !curr->m_object->isRoot() &&
           curr->m_object->style()->hasAutoZIndex();
-          curr = curr->parent()) { };
+          curr = curr->parent()) { }
     return curr;
 }
 
 RenderLayer* RenderLayer::enclosingPositionedAncestor() const
 {
     RenderLayer* curr = parent();
-    for ( ; curr && !curr->m_object->isRenderView() && !curr->m_object->isPositioned() && !curr->m_object->isRelPositioned();
-         curr = curr->parent()) { };
+    for ( ; curr && !curr->m_object->isRenderView() && !curr->m_object->isPositioned() && !curr->m_object->isRelPositioned() && !curr->m_object->hasTransform();
+         curr = curr->parent()) { }
     return curr;
 }
 
@@ -442,7 +442,7 @@ RenderLayer* RenderLayer::enclosingTransformedAncestor() const
 {
     RenderLayer* curr = parent();
     for ( ; curr && !curr->m_object->isRenderView() && !curr->transform(); curr = curr->parent())
-        { };
+        { }
     return curr;
 }
 
