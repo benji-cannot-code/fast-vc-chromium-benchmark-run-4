c57fbd0379aa8a7c52a786cb74a0040ce245ed22
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-11 00:28:03 +0000

    <rdar://problem/6067408> AX: internal anchors broken
    Fixes a broken accessibility test
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35111 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/KURL.h
+++ third_party/WebKit/WebCore/platform/KURL.h
@@ -144,7 +144,8 @@ public:
     void setQuery(const String&);
 
     void setRef(const String&);
-
+    void removeRef();
+    
     friend bool equalIgnoringRef(const KURL&, const KURL&);
 
     friend bool protocolHostAndPortAreEqual(const KURL&, const KURL&);
