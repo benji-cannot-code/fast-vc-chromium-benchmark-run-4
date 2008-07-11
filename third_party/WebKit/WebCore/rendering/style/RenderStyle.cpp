b0b65e90cfe1de00eb29bb16c7a2acfd7d546ec1
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-11 22:40:56 +0000

    2008-07-11  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dave Hyatt
    
            StyleRareNonInheritedData needs to initialize and compare
            m_maskBoxImage.
            <https://bugs.webkit.org/show_bug.cgi?id=20005>
    
            * rendering/style/RenderStyle.cpp:
            (WebCore::StyleRareNonInheritedData::StyleRareNonInheritedData):
            (WebCore::StyleRareNonInheritedData::operator==):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/style/RenderStyle.cpp
+++ third_party/WebKit/WebCore/rendering/style/RenderStyle.cpp
@@ -814,6 +814,7 @@ StyleRareNonInheritedData::StyleRareNonInheritedData(const StyleRareNonInherited
     , m_boxReflect(o.m_boxReflect)
     , m_transition(o.m_transition ? new Transition(*o.m_transition) : 0)
     , m_mask(o.m_mask)
+    , m_maskBoxImage(o.m_maskBoxImage)
 #if ENABLE(XBL)
     , bindingURI(o.bindingURI ? o.bindingURI->copy() : 0)
 #endif
@@ -867,6 +868,7 @@ bool StyleRareNonInheritedData::operator==(const StyleRareNonInheritedData& o) c
         && reflectionDataEquivalent(o)
         && transitionDataEquivalent(o)
         && m_mask == o.m_mask
+        && m_maskBoxImage == o.m_maskBoxImage
 #if ENABLE(XBL)
         && bindingsEquivalent(o)
 #endif
