264dfd2e26dd33fb09a2975bb08d74e400046f77
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-14 19:21:52 +0000

    Windows build fixes
    JavaScriptCore:
    
            Windows build fix
    
            * JavaScriptCore.vcproj/jsc/jsc.vcproj: Added include/pthreads to the
            include path.
    
    WebCore:
    
            Windows build fixes
    
            * WebCore.vcproj/WebCore.vcproj: Add include/pthreads to the include
            path.
            * bindings/scripts/CodeGeneratorCOM.pm: Touched this to force COM
            bindings to regenerate.
            * loader/FTPDirectoryDocument.cpp:
            * loader/FTPDirectoryParser.cpp:
            Fixed some duplicate macro definitions now that we're including
            pthread.h again.
    
    WebKit/win:
    
            Windows build fix
    
            * WebJavaScriptCollector.cpp:
            (WebJavaScriptCollector::objectCount): Updated to match
            -[WebCoreStatistics javaScriptsObjectCount].
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebJavaScriptCollector.cpp
+++ third_party/WebKit/WebKit/win/WebJavaScriptCollector.cpp
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <JavaScriptCore/JSGlobalData.h>
 #include <JavaScriptCore/collector.h>
 #include <WebCore/GCController.h>
+#include <WebCore/JSDOMWindow.h>
+#include <kjs/JSLock.h>
 #pragma warning(pop)
 
 using namespace KJS;
@@ -110,6 +112,7 @@ HRESULT STDMETHODCALLTYPE WebJavaScriptCollector::objectCount(
         return E_POINTER;
     }
 
-    *count = (UINT)JSGlobalData::threadInstance().heap->size();
+    JSLock lock(false);
+    *count = (UINT)JSDOMWindow::commonJSGlobalData()->heap->size();
     return S_OK;
 }
