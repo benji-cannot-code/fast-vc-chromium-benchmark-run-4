f4c637209ee84d417ee0b3ab8373ba9da3d8f465
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-14 21:16:57 +0000

    WebCore:
    2008-07-14  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Geoff.
    
            <rdar://problem/6073974>
            https://bugs.webkit.org/show_bug.cgi?id=18106
            The "onscroll" event bubbles, which is inconsistent with Firefox and IE
    
            Don't bubble the scroll event.
            
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::scrollToOffset):
            * rendering/RenderListBox.cpp:
            (WebCore::RenderListBox::valueChanged):
    
    LayoutTests:
    
    2008-07-14  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Geoff.
    
            <rdar://problem/6073974>
            https://bugs.webkit.org/show_bug.cgi?id=18106
            The "onscroll" event bubbles, which is inconsistent with Firefox and IE
            
            Add test.
            
            * fast/events/scroll-event-does-not-bubble-expected.txt: Added.
            * fast/events/scroll-event-does-not-bubble.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderListBox.cpp
+++ third_party/WebKit/WebCore/rendering/RenderListBox.cpp
@@ -538,7 +538,7 @@ void RenderListBox::valueChanged(Scrollbar*)
         m_indexOffset = newOffset;
         repaint();
         // Fire the scroll DOM event.
-        EventTargetNodeCast(node())->dispatchHTMLEvent(scrollEvent, true, false);
+        EventTargetNodeCast(node())->dispatchHTMLEvent(scrollEvent, false, false);
     }
 }
 
