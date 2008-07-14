5b2f60eb7162b84284fcdbf037d1571752d5ee9b
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-14 23:15:35 +0000

    Don't include autoversion.h in WebKitDLL.cpp.         autoversion.h changes for every build, so we don't want to include it unless necessary.
            Rubber stamped by Ada Chan.
    
            * WebKitDLL.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35175 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebKitDLL.cpp
+++ third_party/WebKit/WebKit/win/WebKitDLL.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebKitDLL.h"
 
-#include "autoversion.h"
 #include "ForEachCoClass.h"
 #include "resource.h"
 #include "WebKit.h"
