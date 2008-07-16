93f35e20a19667e5f428801c5c5c456f02305bc8
adachan@apple.com <adachan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-16 21:41:14 +0000

    Fixed build.
            * kjs/JSGlobalObject.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/JSGlobalObject.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/JSGlobalObject.cpp
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NumberPrototype.h"
 #include "ObjectConstructor.h"
 #include "ObjectPrototype.h"
+#include "Profiler.h"
 #include "PrototypeFunction.h"
 #include "RegExpConstructor.h"
 #include "RegExpPrototype.h"
