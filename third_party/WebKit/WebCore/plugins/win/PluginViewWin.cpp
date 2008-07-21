f57c9b7f57c054e05cd4467e861758171445d750
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-21 13:46:03 +0000

    2008-07-21  Simon Hausmann  <hausmann@webkit.org>
            Reviewed by Holger.
    
            Fix windowHandleForPlatformWidget to return 0 as HWND if we don't have
            a QWidget. This can happen on page/view destruction for example.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35262 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
+++ third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
@@ -74,6 +74,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static inline HWND windowHandleForPlatformWidget(PlatformWidget widget)
 {
 #if PLATFORM(QT)
+    if (!widget)
+        return 0;
     return widget->winId();
 #else
     return widget;
