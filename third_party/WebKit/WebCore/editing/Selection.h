755e9ab1d6ef4ef2e4a765d7ea0fe8f655c17df3
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-22 05:52:09 +0000

    Reviewed by Dan Bernstein.
            https://bugs.webkit.org/show_bug.cgi?id=20117
            setBaseAndExtent fails to reverse the current selection
    
            Test: editing/selection/setBaseAndExtent-revert-selection.html
    
            * editing/Selection.h: (WebCore::operator==): Compare direction, too.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/Selection.h
+++ third_party/WebKit/WebCore/editing/Selection.h
@@ -116,7 +116,7 @@ private:
 
 inline bool operator==(const Selection& a, const Selection& b)
 {
-    return a.start() == b.start() && a.end() == b.end() && a.affinity() == b.affinity() && a.granularity() == b.granularity();
+    return a.start() == b.start() && a.end() == b.end() && a.affinity() == b.affinity() && a.granularity() == b.granularity() && a.isBaseFirst() == b.isBaseFirst();
 }
 
 inline bool operator!=(const Selection& a, const Selection& b)
