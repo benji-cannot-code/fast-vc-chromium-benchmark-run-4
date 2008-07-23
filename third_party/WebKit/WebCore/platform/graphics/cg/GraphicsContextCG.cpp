1aa2ccd376b8ccbd2a9f0b99d5aa6af7d1baefe5
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-23 23:15:04 +0000

    build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@35306 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
@@ -819,7 +819,7 @@ void GraphicsContext::setImageInterpolationQuality(InterpolationQuality mode)
     if (paintingDisabled())
         return;
     
-    CGInterpolationQuality quality;
+    CGInterpolationQuality quality = kCGInterpolationDefault;
     switch (mode) {
         case InterpolationDefault:
             quality = kCGInterpolationDefault;
