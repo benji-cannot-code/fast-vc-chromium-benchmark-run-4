9d71de948e6549a97cffa0be93057c30aa0ab363
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-24 21:27:17 +0000

    Windows build fix for wx. wx doesn't implement pan scrolling yet.
    git-svn-id: svn://svn.chromium.org/blink/trunk@35330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/WebCore/page/EventHandler.cpp
@@ -565,7 +565,7 @@ void EventHandler::handleAutoscroll(RenderObject* renderer)
 
     setAutoscrollRenderer(renderer);
 
-#if PLATFORM(WIN) || (PLATFORM(WX) && PLATFORM(WIN_OS))
+#if PLATFORM(WIN)
     if (m_panScrollInProgress) {
         m_panScrollStartPos = currentMousePosition();
         m_frame->view()->printPanScrollIcon(m_panScrollStartPos);
@@ -598,7 +598,7 @@ void EventHandler::autoscrollTimerFired(Timer<EventHandler>*)
             stopAutoscrollTimer();
             return;
         }
-#if PLATFORM(WIN) || (PLATFORM(WX) && PLATFORM(WIN_OS))
+#if PLATFORM(WIN)
         r->panScroll(m_panScrollStartPos);
 #endif
     }
@@ -676,7 +676,7 @@ void EventHandler::stopAutoscrollTimer(bool rendererIsBeingDestroyed)
     if (autoscrollRenderer()) {
         if (!rendererIsBeingDestroyed && (m_autoscrollInProgress || m_panScrollInProgress))
             autoscrollRenderer()->stopAutoscroll();
-#if PLATFORM(WIN) || (PLATFORM(WX) && PLATFORM(WIN_OS))
+#if PLATFORM(WIN)
         if (m_panScrollInProgress) {
             m_frame->view()->removePanScrollIcon();
         }
@@ -939,7 +939,7 @@ bool EventHandler::handleMousePressEvent(const PlatformMouseEvent& mouseEvent)
         return true;
     }
  
-#if PLATFORM(WIN) || (PLATFORM(WX) && PLATFORM(WIN_OS))
+#if PLATFORM(WIN)
     if (m_frame->page()->mainFrame()->eventHandler()->panScrollInProgress() || m_autoscrollInProgress) {
         stopAutoscrollTimer();
     } else if (mouseEvent.button() == MiddleButton && !mev.isOverLink()) {
