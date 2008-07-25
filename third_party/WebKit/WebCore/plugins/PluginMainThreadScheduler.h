c05e975433e3c6ffcc1bf17bb61a20f0a2c5fc29
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-25 10:55:59 +0000

    2008-07-25  Simon Hausmann  <hausmann@webkit.org>
            Rubber-stamped by Lars.
    
            Fix the Qt build by adapting to the latest ArgList API changes and
            replacing the inclusion of npapi.h in MainThreadScheduler with a forward
            declaration of NPP to avoid an include conflict. npapi.h on X11 includes
            X headers, which include wonderful defines such as "#define Status" that
            cause conflicts.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35336 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/PluginMainThreadScheduler.h
+++ third_party/WebKit/WebCore/plugins/PluginMainThreadScheduler.h
@@ -31,7 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/HashMap.h>
 #include <wtf/MainThread.h>
 #include <wtf/Threading.h>
-#include "npapi.h"
+
+typedef struct _NPP NPP_t;
+typedef NPP_t* NPP;
 
 namespace WebCore {
 
