99c1bb5694b8502dbc7704b5364220fc7493b556
initial.commit <initial.commit@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-26 22:42:52 +0000

    Add net to the repository.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/base/net_resources.h
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// TODO(tc): Come up with a way to automate the generation of these
+// IDs so they don't collide with other rc files.
+#define IDR_DIR_HEADER_HTML            400
+#define IDR_EFFECTIVE_TLD_NAMES        401
