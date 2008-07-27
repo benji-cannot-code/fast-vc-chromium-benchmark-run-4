a2dfc9f952d4d88fe7fdae8376fbad2ed34483ab
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-27 05:44:30 +0000

    2008-07-26  Keishi Hattori  <casey.hattori@gmail.com>
            Reviewed by Adam Roben.
    
            Add console.time/timeEnd.
            https://bugs.webkit.org/show_bug.cgi?id=19159
    
            * manual-tests/inspector/console-time.html: Added. Test cases for console.time/timeEnd.
            * page/Console.cpp:
            (WebCore::Console::time): Added.
            (WebCore::Console::timeEnd): Added.
            * page/Console.h:
            * page/Console.idl: Added console.time/timeEnd.
            * page/InspectorController.cpp:
            (WebCore::InspectorController::startTiming): Added.
            (WebCore::InspectorController::stopTiming): Added.
            * page/InspectorController.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/InspectorController.h
+++ third_party/WebKit/WebCore/page/InspectorController.h
@@ -170,6 +170,9 @@ public:
     void stepOutOfFunctionInDebugger();
 
     void drawNodeHighlight(GraphicsContext&) const;
+    
+    void startTiming(const KJS::UString& title);
+    bool stopTiming(const KJS::UString& title, double& elapsed);
 
 private:
     void focusNode();
@@ -221,6 +224,7 @@ private:
     FrameResourcesMap m_frameResources;
     Vector<ConsoleMessage*> m_consoleMessages;
     Vector<RefPtr<KJS::Profile> > m_profiles;
+    HashMap<String, double> m_times;
 #if ENABLE(DATABASE)
     DatabaseResourcesSet m_databaseResources;
 #endif
