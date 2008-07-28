0c74b8a96f6127edf9fb7ac64d34954ad5a96183
rahulk@google.com <rahulk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-28 23:27:49 +0000

    Rename uninstall link from Chrome to localized "Google Chrome" string (value of reg key - DisplayName). Keep the uninstall key itself as Google Chrome in all languages as it is not visible to the user and helps in deleting the key in case user changes the system locale after installing Chrome.
    BUG=1293755
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/setup/install.cc
+++ chrome/installer/setup/install.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/create_reg_key_work_item.h"
 #include "chrome/installer/util/delete_tree_work_item.h"
 #include "chrome/installer/util/install_util.h"
+#include "chrome/installer/util/l10n_string_util.h"
 #include "chrome/installer/util/logging_installer.h"
 #include "chrome/installer/util/google_update_constants.h"
 #include "chrome/installer/util/set_reg_value_work_item.h"
@@ -47,6 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/util_constants.h"
 #include "chrome/installer/util/version.h"
 #include "chrome/installer/util/work_item_list.h"
+
+#include "setup_strings.h"
 
 namespace {
 std::wstring AppendPath(const std::wstring parent_path,
@@ -75,11 +78,11 @@ void AddUninstallShortcutWorkItems(HKEY reg_root,
   // Create DisplayName, UninstallString and InstallLocation keys
   install_list->AddCreateRegKeyWorkItem(reg_root,
                                         installer_util::kUninstallRegPath);
+  const std::wstring& product_name =
+      installer_util::GetLocalizedString(IDS_PRODUCT_NAME_BASE);
   install_list->AddSetRegValueWorkItem(
-      reg_root,
-      installer_util::kUninstallRegPath,
-      installer_util::kUninstallDisplayNameField,
-      installer_util::kChrome, true);
+      reg_root, installer_util::kUninstallRegPath,
+      installer_util::kUninstallDisplayNameField, product_name, true);
   install_list->AddSetRegValueWorkItem(reg_root,
                                        installer_util::kUninstallRegPath,
                                        installer_util::kUninstallStringField,
