4c334d922553a2cd3f1fece022837041ff24815c
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-28 23:34:32 +0000

    WebCore:
    2008-07-28  Brady Eidson  <beidson@apple.com>
    
            Reviewed by Sam Weinig
    
            Test: security/autocomplete-cleared-on-back.html
    
            <rdar://problem/6093281> - autocomplete="off" should work when going back
    
            Taken care of in two ways:
            1 - Listening for the page cache notification to clear the form, in cases where the page
                is being restored from the page cache
            2 - Not saving such form elements when doing the normal "save state" to a history item,
                for cases with no page cache
    
            * html/HTMLFormElement.cpp:
            (WebCore::HTMLFormElement::~HTMLFormElement):
            (WebCore::HTMLFormElement::parseMappedAttribute):
            (WebCore::HTMLFormElement::didRestoreFromCache):
            (WebCore::HTMLFormElement::willMoveToNewOwnerDocument):
            (WebCore::HTMLFormElement::didMoveToNewOwnerDocument):
            * html/HTMLFormElement.h:
    
            * html/HTMLInputElement.cpp:
            (WebCore::HTMLInputElement::~HTMLInputElement):
            (WebCore::HTMLInputElement::setInputType):
            (WebCore::HTMLInputElement::saveState):
            (WebCore::HTMLInputElement::parseMappedAttribute):
            (WebCore::HTMLInputElement::needsCacheCallback):
            (WebCore::HTMLInputElement::registerForCacheCallbackIfNeeded): Only register if the type or attribute
              indicate we should.
            (WebCore::HTMLInputElement::unregisterForCacheCallbackIfNeeded): Only unregister if both the type
              and attribute are clear of needing registration.
            (WebCore::HTMLInputElement::didRestoreFromCache):
            (WebCore::HTMLInputElement::willMoveToNewOwnerDocument):
            (WebCore::HTMLInputElement::didMoveToNewOwnerDocument):
            * html/HTMLInputElement.h:
    
    LayoutTests:
    
    2008-07-28  Brady Eidson  <beidson@apple.com>
    
            Reviewed by Sam Weinig
    
            <rdar://problem/6093281> - autocomplete="off" should work when going back
    
            * security/autocomplete-cleared-on-back-expected.txt: Added.
            * security/autocomplete-cleared-on-back.html: Added.
            * security/resources: Added.
            * security/resources/autocomplete-cleared-on-back2.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLFormElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLFormElement.cpp
@@ -82,6 +82,9 @@ HTMLFormElement::HTMLFormElement(Document* doc)
 
 HTMLFormElement::~HTMLFormElement()
 {
+    if (!m_autocomplete)
+        document()->unregisterForCacheCallbacks(this);
+
     delete m_elementAliases;
     delete collectionInfo;
 
@@ -541,9 +544,13 @@ void HTMLFormElement::parseMappedAttribute(MappedAttribute* attr)
         m_acceptcharset = attr->value();
     else if (attr->name() == acceptAttr) {
         // ignore this one for the moment...
-    } else if (attr->name() == autocompleteAttr)
+    } else if (attr->name() == autocompleteAttr) {
         m_autocomplete = !equalIgnoringCase(attr->value(), "off");
-    else if (attr->name() == onsubmitAttr)
+        if (!m_autocomplete)
+            document()->registerForCacheCallbacks(this);    
+        else
+            document()->unregisterForCacheCallbacks(this);
+    } else if (attr->name() == onsubmitAttr)
         setHTMLEventListener(submitEvent, attr);
     else if (attr->name() == onresetAttr)
         setHTMLEventListener(resetEvent, attr);
@@ -714,6 +721,26 @@ void HTMLFormElement::getNamedElements(const AtomicString& name, Vector<RefPtr<N
     // name has been accessed, remember it
     if (namedItems.size() && aliasElem != namedItems.first())
         addElementAlias(static_cast<HTMLFormControlElement*>(namedItems.first().get()), name);        
+}
+
+void HTMLFormElement::didRestoreFromCache()
+{
+    ASSERT(!m_autocomplete);
+    
+    for (unsigned i = 0; i < formElements.size(); ++i)
+        formElements[i]->reset();
+}
+
+void HTMLFormElement::willMoveToNewOwnerDocument()
+{
+    if (!m_autocomplete)
+        document()->unregisterForCacheCallbacks(this);
+}
+
+void HTMLFormElement::didMoveToNewOwnerDocument()
+{
+    if(m_autocomplete)
+        document()->registerForCacheCallbacks(this);
 }
 
 void HTMLFormElement::CheckedRadioButtons::addButton(HTMLFormControlElement* element)
--- third_party/WebKit/WebCore/html/HTMLFormElement.h
+++ third_party/WebKit/WebCore/html/HTMLFormElement.h
@@ -122,6 +122,12 @@ public:
     
     CheckedRadioButtons& checkedRadioButtons() { return m_checkedRadioButtons; }
     
+    virtual void didRestoreFromCache();
+
+protected:
+    virtual void willMoveToNewOwnerDocument();
+    virtual void didMoveToNewOwnerDocument();
+
 private:
     void parseEnctype(const String&);
     bool isMailtoForm() const;
