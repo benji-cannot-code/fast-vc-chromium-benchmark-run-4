7cb6f8eef56b97f9b20fe510cfca83d36a62cafb
nsylvain@google.com <nsylvain@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-28 15:45:13 +0000

    Fix compile errors due to missing includes.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics_log_unittest.cc
+++ chrome/browser/metrics_log_unittest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/time.h"
 #include "chrome/browser/metrics_log.h"
+#include "googleurl/src/gurl.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
--- chrome/browser/safe_browsing/safe_browsing_database_unittest.cc
+++ chrome/browser/safe_browsing/safe_browsing_database_unittest.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "chrome/browser/safe_browsing/protocol_parser.h"
 #include "chrome/browser/safe_browsing/safe_browsing_database.h"
+#include "googleurl/src/gurl.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
