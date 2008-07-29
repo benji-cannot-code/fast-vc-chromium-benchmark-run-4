a36b1d3cc82e7ccc9dffa71815f299853e4f038f
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-29 00:21:49 +0000

    Update the JSONValueSerializer to have a flag to allow trailing commas.  By default, we don't support this when deserializing JSON, but the user can enable this.
    BUG=1295713
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/json_value_serializer_unittest.cc
+++ chrome/common/json_value_serializer_unittest.cc
@@ -192,7 +192,7 @@ namespace {
 
 void ValidateJsonList(const std::string& json) {
   Value* root = NULL;
-  ASSERT_TRUE(JSONReader::Read(json, &root));
+  ASSERT_TRUE(JSONReader::Read(json, &root, false));
   ASSERT_TRUE(root && root->IsType(Value::TYPE_LIST));
   ListValue* list = static_cast<ListValue*>(root);
   ASSERT_EQ(1, list->GetSize());
@@ -216,7 +216,7 @@ TEST(JSONValueSerializerTest, JSONReaderComments) {
 
   Value* root = NULL;
   // It's ok to have a comment in a string.
-  ASSERT_TRUE(JSONReader::Read("[\"// ok\\n /* foo */ \"]", &root));
+  ASSERT_TRUE(JSONReader::Read("[\"// ok\\n /* foo */ \"]", &root, false));
   ASSERT_TRUE(root && root->IsType(Value::TYPE_LIST));
   ListValue* list = static_cast<ListValue*>(root);
   ASSERT_EQ(1, list->GetSize());
@@ -229,10 +229,10 @@ TEST(JSONValueSerializerTest, JSONReaderComments) {
 
   root = NULL;
   // You can't nest comments.
-  ASSERT_FALSE(JSONReader::Read("/* /* inner */ outer */ [ 1 ]", &root));
+  ASSERT_FALSE(JSONReader::Read("/* /* inner */ outer */ [ 1 ]", &root, false));
 
   // Not a open comment token.
-  ASSERT_FALSE(JSONReader::Read("/ * * / [1]", &root));
+  ASSERT_FALSE(JSONReader::Read("/ * * / [1]", &root, false));
 }
 
 namespace {
