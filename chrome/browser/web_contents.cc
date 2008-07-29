bff20c1beeb60e8fad6ecec0f27dfb5f77356756
nsylvain@google.com <nsylvain@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-29 20:57:35 +0000

    Reverting revision 70, 66 and 65 to fix crashes.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_contents.cc
+++ chrome/browser/web_contents.cc
@@ -861,7 +861,7 @@ SiteInstance* WebContents::GetSiteInstanceForEntry(
   if (entry.site_instance())
     return entry.site_instance();
 
-  // (UGLY) HEURISTIC, process-per-site only:
+  // (UGLY) HEURISTIC:
   //
   // If this navigation is generated, then it probably corresponds to a search
   // query.  Given that search results typically lead to users navigating to
@@ -871,8 +871,7 @@ SiteInstance* WebContents::GetSiteInstanceForEntry(
   // NOTE: This can be removed once we have a way to transition between
   //       RenderViews in response to a link click.
   //
-  if (CommandLine().HasSwitch(switches::kProcessPerSite) &&
-      entry.GetTransitionType() == PageTransition::GENERATED)
+  if (entry.GetTransitionType() == PageTransition::GENERATED)
     return curr_instance;
 
   const GURL& dest_url = entry.GetURL();
