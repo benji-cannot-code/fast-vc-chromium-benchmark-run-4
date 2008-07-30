73f3d314e360602b25924bbcbd969bf4b0d6580b
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-30 23:14:37 +0000

    Reviewed by Oliver.
            MSVC warns when structs are called classes or vice versa.
            Make all the source refer to JSGlobalData as a class.
    
            * kjs/CommonIdentifiers.h:
            * kjs/JSGlobalData.h:
            * kjs/Parser.h:
            * kjs/lexer.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35466 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/JSGlobalData.h
+++ third_party/WebKit/JavaScriptCore/kjs/JSGlobalData.h
@@ -53,7 +53,8 @@ namespace KJS {
     class UString;
     struct HashTable;
 
-    struct JSGlobalData : public RefCounted<JSGlobalData> {
+    class JSGlobalData : public RefCounted<JSGlobalData> {
+    public:
         static PassRefPtr<JSGlobalData> create();
         ~JSGlobalData();
 
