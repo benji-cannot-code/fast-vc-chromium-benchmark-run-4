f5e9f7ffef7c40aece24d4a82cc6133bb9f2b98c
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-30 05:55:57 +0000

    Reviewed by Geoff Garen.
            Fix a leak of ThreadRegistrar objects.
    
            As the heap is usually deleted when registered threads still exist, ThreadSpecific doesn't
            have a chance to clean up per-thread object. Switched to native pthread calls, storing a
            plain pointer that doesn't require cleanup.
    
            * kjs/collector.cpp:
            (KJS::PlatformThread::PlatformThread):
            (KJS::Heap::Thread::Thread):
            (KJS::Heap::Heap):
            (KJS::Heap::~Heap):
            (KJS::Heap::registerThread):
            (KJS::Heap::unregisterThread):
            * kjs/collector.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35440 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/collector.h
+++ third_party/WebKit/JavaScriptCore/kjs/collector.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/OwnPtr.h>
 #include <wtf/Threading.h>
 #if USE(MULTIPLE_THREADS)
-#include <wtf/ThreadSpecific.h>
+#include <pthread.h>
 #endif
 
 namespace KJS {
@@ -132,20 +132,12 @@ namespace KJS {
         HashSet<ArgList*>* m_markListSet;
 
 #if USE(MULTIPLE_THREADS)
+        static void unregisterThread(void*);
         void unregisterThread();
-
-        class ThreadRegistrar : Noncopyable {
-        public:
-            ThreadRegistrar(Heap* heap) : m_heap(heap) {}
-            ~ThreadRegistrar() { m_heap->unregisterThread(); }
-
-        private:
-            Heap* m_heap;
-        };
 
         Mutex m_registeredThreadsMutex;
         Thread* m_registeredThreads;
-        WTF::ThreadSpecific<OwnPtr<ThreadRegistrar> > m_currentThreadRegistrar;
+        pthread_key_t m_currentThreadRegistrar;
 #endif
 
         JSGlobalData* m_globalData;
