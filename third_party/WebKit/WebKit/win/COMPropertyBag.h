cdf8e3fc5c6ce26367e346eb9371bd5189894d7f
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-31 01:11:54 +0000

    2008-07-30  Anders Carlsson  <andersca@apple.com>
            Fix copyright years.
            
            * COMPropertyBag.h:
            * COMVariantSetter.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/COMPropertyBag.h
+++ third_party/WebKit/WebKit/win/COMPropertyBag.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007 Apple Inc. All Rights Reserved.
+ * Copyright (C) 2008 Apple Inc. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
