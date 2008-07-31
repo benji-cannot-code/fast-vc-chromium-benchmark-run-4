48ddcd36deefd21cdeff565fe0ec348058b5ae9a
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-07-31 17:20:00 +0000

    2008-07-31  Erik Bunce  <elbunce@thehive.com>
            Reviewed by Simon.
    
            https://bugs.webkit.org/show_bug.cgi?id=20224
    
            Include the Writing Direction sub-menu in the context menu of all
            platforms but GTK. This fixes a regression introduced by revision
            34426.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35486 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ContextMenu.cpp
+++ third_party/WebKit/WebCore/platform/ContextMenu.cpp
@@ -154,7 +154,9 @@ static void createAndAppendSpeechSubMenu(const HitTestResult& result, ContextMen
 
     speechMenuItem.setSubMenu(&speechMenu);
 }
-
+#endif
+ 
+#if !PLATFORM(GTK)
 static void createAndAppendWritingDirectionSubMenu(const HitTestResult& result, ContextMenuItem& writingDirectionMenuItem)
 {
     ContextMenu writingDirectionMenu(result);
@@ -401,6 +403,8 @@ void ContextMenu::populate()
                 contextMenuItemTagSpeechMenu());
             createAndAppendSpeechSubMenu(m_hitTestResult, SpeechMenuItem);
             appendItem(SpeechMenuItem);
+#endif
+#if !PLATFORM(GTK)
             ContextMenuItem WritingDirectionMenuItem(SubmenuType, ContextMenuItemTagWritingDirectionMenu, 
                 contextMenuItemTagWritingDirectionMenu());
             createAndAppendWritingDirectionSubMenu(m_hitTestResult, WritingDirectionMenuItem);
