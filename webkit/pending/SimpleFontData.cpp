f7f6955e0c8a1fa79cf7a7699bae4a3686110c42
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-31 22:24:53 +0000

    This adds an additional condition on my previous patch in r183 which tried to fix box characters appearing in some pages, such as Gmail.
    The problem is that the newline character is treated as a zero-width space in the old patch, but apparently WebKit relies on this turning into a space (or something, I don't totally understand). I just removed control characters from this condition since we don't actually want to change their behavior in this respect.
    
    webkit=https://bugs.webkit.org/show_bug.cgi?id=20237
    BUG=1166024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/pending/SimpleFontData.cpp
+++ webkit/pending/SimpleFontData.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "SimpleFontData.h"
 
+#include "Font.h"
 #include "FontMetrics.h"
 
 #if ENABLE(SVG_FONTS)
@@ -149,6 +150,21 @@ float SimpleFontData::widthForGlyph(UChar32 c, Glyph glyph) const
         ASSERT((cGlyphWidthUnknown == width) || (actual_width == width));
     }
 #endif
+
+    // Some characters should be zero width and we want to ignore whatever
+    // crazy stuff the font may have (or not defined). If the font doesn't
+    // define it, we don't want to measure the width of the "invalid character"
+    // box, for example.
+    //
+    // Note that we have to exempt control characters, which
+    // treatAsZeroWidthSpace would normally return true for. This is primarily
+    // for \n since it will be rendered as a regular space in HTML.
+    //
+    // TODO(brettw): we should have Font::treatAsZeroWidthSpace return true for
+    // zero width spaces (U+200B) just like Font::treatAsSpace will return true
+    // for spaces. Then the additional OR is not necessary.
+    if (c > ' ' && (Font::treatAsZeroWidthSpace(c) || c == 0x200b))
+      return 0.0f;
 
     if (width != cGlyphWidthUnknown)
         return width;
