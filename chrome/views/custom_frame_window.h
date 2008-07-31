0393ed6028f84165e0d75559f19cd734358a8c29
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-31 04:44:59 +0000

    Should not be able to construct a CustomFrameWindow directly. Should use CreateChromeWindow instead.
    Move CustomFrameWindow's two constructors to the protected section. They should be called by subclasses only.
    
    B=1293984
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/custom_frame_window.h
+++ chrome/views/custom_frame_window.h
@@ -51,9 +51,6 @@ class NonClientView;
 ////////////////////////////////////////////////////////////////////////////////
 class CustomFrameWindow : public Window {
  public:
-  explicit CustomFrameWindow(WindowDelegate* window_delegate);
-  CustomFrameWindow(WindowDelegate* window_delegate,
-                    NonClientView* non_client_view);
   virtual ~CustomFrameWindow();
 
   // Executes the specified SC_command.
@@ -70,6 +67,12 @@ class CustomFrameWindow : public Window {
   virtual void UpdateWindowTitle();
 
  protected:
+  // Cannot construct one of these directly - use Window::CreateChromeWindow.
+  // Subclasses _must_ call one of these constructors.
+  explicit CustomFrameWindow(WindowDelegate* window_delegate);
+  CustomFrameWindow(WindowDelegate* window_delegate,
+                    NonClientView* non_client_view);
+
   // Overridden from Window:
   virtual void SizeWindowToDefault();
   virtual void EnableClose(bool enable);
