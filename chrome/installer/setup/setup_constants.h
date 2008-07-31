bfc6dd1bfeb84f1e3373bb851066776eb326ca8a
rahulk@google.com <rahulk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-31 18:26:15 +0000

    My earlier change to blindly copy all the files under Application\Chrome breaks updates if any dictionary is currently open. Fix it to copy the default dictionaries of installer only if there aren't any present already.
    BUG=1302580
    TEST=Run 149.1 version of Chrome and do an operation that requires dictionary to be open (write in a text area). Simultaneously try to upgrade Chrome and it should not fail.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/setup/setup_constants.h
+++ chrome/installer/setup/setup_constants.h
@@ -38,6 +38,7 @@ namespace installer {
 extern const wchar_t kChromeOldExe[];
 extern const wchar_t kChromeNewExe[];
 extern const wchar_t kWowHelperExe[];
+extern const wchar_t kDictionaries[];
 extern const wchar_t kChromeArchive[];
 extern const wchar_t kChromePatchArchive[];
 extern const wchar_t kChromeCompressedArchive[];
