a9c29369844ca1bf4d0b77afffba6b874d19b595
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-01 17:36:48 +0000

    Adds a backend for the bookmarks bar that reads/writes to a JSON file. These files aren't being built or used yet (though a test case exists that is ifdef'd out).
    BUG=1256202
    TEST=none
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmark_bar_model.h
+++ chrome/browser/bookmark_bar_model.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/include/SkBitmap.h"
 
 class BookmarkBarModel;
+class BookmarkCodec;
 class Profile;
 
 // BookmarkBarNode ------------------------------------------------------------
@@ -51,6 +52,7 @@ class Profile;
 //
 class BookmarkBarNode : public ChromeViews::TreeNode<BookmarkBarNode> {
   friend class BookmarkBarModel;
+  friend class BookmarkCodec;
 
  public:
   explicit BookmarkBarNode(BookmarkBarModel* model);
@@ -65,6 +67,8 @@ class BookmarkBarNode : public ChromeViews::TreeNode<BookmarkBarNode> {
   const GURL& GetURL() const { return url_; }
 
   // Returns the start ID corresponding to this node.
+  // TODO(sky): bug 1256202, make this an ever increasing integer assigned on
+  // reading, but not archived. Best to set it automatically in the constructor.
   history::StarID GetStarID() const { return star_id_; }
 
   // Returns the type of this node.
@@ -74,6 +78,7 @@ class BookmarkBarNode : public ChromeViews::TreeNode<BookmarkBarNode> {
   history::StarredEntry GetEntry();
 
   // Returns the ID of group.
+  // TODO(sky): bug 1256202, nuke this.
   history::UIStarID GetGroupID() { return group_id_; }
 
   // Called when the favicon becomes invalid.
@@ -81,6 +86,13 @@ class BookmarkBarNode : public ChromeViews::TreeNode<BookmarkBarNode> {
     loaded_favicon_ = false;
     favicon_ = SkBitmap();
   }
+
+  // Returns the time the bookmark/group was added.
+  Time date_added() const { return date_added_; }
+
+  // Returns the last time the group was modified. This is only maintained
+  // for folders (including the bookmark and other folder).
+  Time date_group_modified() const { return date_group_modified_; }
 
  private:
   // Resets the properties of the node from the supplied entry.
@@ -109,6 +121,7 @@ class BookmarkBarNode : public ChromeViews::TreeNode<BookmarkBarNode> {
   GURL url_;
 
   // Type of node.
+  // TODO(sky): bug 1256202, convert this into a type defined here.
   history::StarredEntry::Type type_;
 
   // Group ID.
