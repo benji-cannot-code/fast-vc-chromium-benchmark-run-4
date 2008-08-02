bc17f340afb6ad42976ce991ff6dab932c6c62f5
cpu@google.com <cpu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-02 00:20:46 +0000

    I think this is the crasher - I want to test this on the bots.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271 0039d316-1c4b-4281-b951-d872f2087c98

--- base/gfx/native_theme.cc
+++ base/gfx/native_theme.cc
@@ -91,7 +91,8 @@ NativeTheme::NativeTheme()
 
 NativeTheme::~NativeTheme() {
   if (theme_dll_) {
-    CloseHandles();
+    // todo (cpu): fix this soon.
+    // CloseHandles();
     FreeLibrary(theme_dll_);
   }
 }
