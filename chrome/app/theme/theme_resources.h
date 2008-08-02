56ef70738b2b8b48107e34f0bf2ae1c6ede335d2
glen@google.com <glen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-02 00:44:22 +0000

    Make our distributor logo light on XP (and leave it dark on Vista).
    Also conveniently fixes accessibility test 'failures' introduced by the last distributor logo change.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/theme/theme_resources.h
+++ chrome/app/theme/theme_resources.h
@@ -312,3 +312,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDR_OTR_ICON_STANDALONE                          9380
 #define IDR_PRODUCT_LOGO                                 9381
 #define IDR_DISTRIBUTOR_LOGO                             9382
+#define IDR_DISTRIBUTOR_LOGO_LIGHT                       9383
--- chrome/test/accessibility/constants.h
+++ chrome/test/accessibility/constants.h
@@ -63,10 +63,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Chrome Client chidren.
 #define BROWSER_VIEW_ACC_INDEX          (0)
 #define TABSTRIP_ACC_INDEX              (1)
-#define CHROME_MIN_ACC_INDEX            (3)
-#define CHROME_MAX_ACC_INDEX            (4)
-#define CHROME_RESTORE_ACC_INDEX        (5)
-#define CHROME_CLOSE_ACC_INDEX          (6)
+#define CHROME_MIN_ACC_INDEX            (4)
+#define CHROME_MAX_ACC_INDEX            (5)
+#define CHROME_RESTORE_ACC_INDEX        (6)
+#define CHROME_CLOSE_ACC_INDEX          (7)
 
 // Browser View children.
 #define TOOLBAR_ACC_INDEX               (0)
