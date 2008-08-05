0746b16b95c1402e06665dd284c85eefba5c945b
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-05 23:43:39 +0000

    ../../../../../../WebCore:
    2008-08-05  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Darin.
    
            Move the main thread deallocator here from WebKit.
            
            * WebCore.base.exp:
            * WebCore.xcodeproj/project.pbxproj:
            * platform/mac/WebCoreObjCExtras.c:
            (method_getImplementation):
            (deallocCallback):
            (WebCoreObjCScheduleDeallocateOnMainThread):
            * platform/mac/WebCoreObjCExtras.h:
    
    ../../../../../../WebKit:
    
    2008-08-05  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Darin.
    
            Remove MainThreadObjectDeallocator.{h|mm}.
            
            * WebKit.xcodeproj/project.pbxproj:
    
    ../../../../../../WebKit/mac:
    
    2008-08-05  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Darin.
    
            Remove MainThreadObjectDeallocator.{h|mm}.
    
            * WebView/MainThreadObjectDeallocator.h: Removed.
            * WebView/MainThreadObjectDeallocator.mm: Removed.
            
            * WebView/WebView.mm:
            (-[WebView dealloc]):
            Call WebCoreObjCScheduleDeallocateOnMainThread instead.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebViewInternal.h"
 
 #import "DOMRangeInternal.h"
-#import "MainThreadObjectDeallocator.h"
 #import "WebBackForwardListInternal.h"
 #import "WebBaseNetscapePluginView.h"
 #import "WebChromeClient.h"
@@ -2070,7 +2069,7 @@ static void WebKitInitializeApplicationCachePathIfNecessary()
 
 - (void)dealloc
 {
-    if (scheduleDeallocateOnMainThread([WebView class], self))
+    if (WebCoreObjCScheduleDeallocateOnMainThread([WebView class], self))
         return;
 
     // call close to ensure we tear-down completely
