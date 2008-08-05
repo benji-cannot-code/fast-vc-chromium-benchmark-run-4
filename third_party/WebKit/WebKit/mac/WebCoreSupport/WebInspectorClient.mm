11ba27ffb764b0071a2dd86b7c2b6a03fdc48875
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-05 02:02:36 +0000

    Adopt the formal protocols where necessary. Final part of fix for <rdar://problem/5853147>.
    Reviewed by Kevin Decker.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35547 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebInspectorClient.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebInspectorClient.mm
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 
-@interface WebInspectorWindowController : NSWindowController {
+@interface WebInspectorWindowController : NSWindowController <NSWindowDelegate> {
 @private
     WebView *_inspectedWebView;
     WebView *_webView;
