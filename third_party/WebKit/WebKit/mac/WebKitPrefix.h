7a0f5cf1743455894af0bee6d13926f40b1e09ac
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-05 02:02:33 +0000

    Declare empty protocols when using versions of AppKit that do not use formal protocols for delegates and data sources. Part one of fix for <rdar://problem/5853147>.
    Reviewed by Darin Adler.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35546 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebKitPrefix.h
+++ third_party/WebKit/WebKit/mac/WebKitPrefix.h
@@ -67,6 +67,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif
 
+#include "EmptyProtocolDefinitions.h"
+
 #include <wtf/Platform.h>
 
 #ifdef __cplusplus
