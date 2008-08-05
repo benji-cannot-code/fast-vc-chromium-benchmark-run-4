f0d1e068e87cd39d013c2fee5f0705d0cb88aef6
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-05 23:17:53 +0000

    Convert more consumers of ML::WatchObject to ObjectWatcher.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@408 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_process_host.h
+++ chrome/browser/plugin_process_host.h
@@ -28,14 +28,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef CHROME_BROWSER_PLUGIN_PROCESS_HOST_H__
-#define CHROME_BROWSER_PLUGIN_PROCESS_HOST_H__
+#ifndef CHROME_BROWSER_PLUGIN_PROCESS_HOST_H_
+#define CHROME_BROWSER_PLUGIN_PROCESS_HOST_H_
 
 #include <vector>
 
 #include "base/basictypes.h"
 #include "base/id_map.h"
-#include "base/message_loop.h"
+#include "base/object_watcher.h"
 #include "base/process.h"
 #include "base/scoped_ptr.h"
 #include "base/task.h"
@@ -60,7 +60,7 @@ class GURL;
 // the renderer and plugin processes.
 class PluginProcessHost : public IPC::Channel::Listener,
                           public IPC::Message::Sender,
-                          public MessageLoop::Watcher {
+                          public base::ObjectWatcher::Delegate {
  public:
   PluginProcessHost(PluginService* plugin_service);
   ~PluginProcessHost();
@@ -76,7 +76,7 @@ class PluginProcessHost : public IPC::Channel::Listener,
   // IPC::Message::Sender implementation:
   virtual bool Send(IPC::Message* msg);
 
-  // MessageLoop watcher callback
+  // ObjectWatcher::Delegate implementation:
   virtual void OnObjectSignaled(HANDLE object);
 
   // IPC::Channel::Listener implementation:
@@ -160,6 +160,9 @@ class PluginProcessHost : public IPC::Channel::Listener,
   // The handle to our plugin process.
   Process process_;
 
+  // Used to watch the plugin process handle.
+  base::ObjectWatcher watcher_;
+
   // true while we're waiting the channel to be opened.  In the meantime,
   // plugin instance requests will be buffered.
   bool opening_channel_;
@@ -180,4 +183,4 @@ class PluginProcessHost : public IPC::Channel::Listener,
   DISALLOW_EVIL_CONSTRUCTORS(PluginProcessHost);
 };
 
-#endif  // CHROME_BROWSER_PLUGIN_PROCESS_HOST_H__
+#endif  // CHROME_BROWSER_PLUGIN_PROCESS_HOST_H_
