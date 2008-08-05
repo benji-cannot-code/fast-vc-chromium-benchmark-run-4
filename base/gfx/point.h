880760aec52bf6cfc60b596532f356fcecef6403
avi@google.com <avi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-05 18:31:45 +0000

    Fix for missing build info. Review URL: http://chrome-reviews.prom.corp.google.com/1128
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@383 0039d316-1c4b-4281-b951-d872f2087c98

--- base/gfx/point.h
+++ base/gfx/point.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_GFX_POINT_H__
 #define BASE_GFX_POINT_H__
 
+#include "build/build_config.h"
+
 #ifdef UNIT_TEST
 #include <iostream>
 #endif
