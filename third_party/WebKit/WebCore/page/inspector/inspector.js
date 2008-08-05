af645d6831f37dea52b6fa8f3daacaf04b75d4ee
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-05 16:47:22 +0000

    Changes the editing behavior in the Inspector to commit changes         when focus is lost, instead of reverting the changes.
            https://bugs.webkit.org/show_bug.cgi?id=20279
    
            Reviewed by Adam Roben.
    
            * page/inspector/inspector.js:
            (WebInspector.startEditing): Call editingCommitted in blurEventListener
            instead of editingCancelled.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35562 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/inspector.js
+++ third_party/WebKit/WebCore/page/inspector/inspector.js
@@ -1143,7 +1143,7 @@ WebInspector.startEditing = function(element, committedCallback, cancelledCallba
         element.tabIndex = 0;
 
     function blurEventListener() {
-        editingCancelled.call(element);
+        editingCommitted.call(element);
     }
 
     function cleanUpAfterEditing() {
