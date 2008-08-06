a8eb01c11abd0bf0a5246e4f30e876e9a35d88af
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-06 21:27:02 +0000

    just some hopefully non-contentious stuff to get out of the way before doing the real MessageLoop changes.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@459 0039d316-1c4b-4281-b951-d872f2087c98

--- base/id_map.h
+++ base/id_map.h
@@ -31,14 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_ID_MAP_H__
 #define BASE_ID_MAP_H__
 
-// hash map is common (GCC4 and Dinkumware also support it, for example), but
-// is not strictly part of the C++ standard. MS puts it into a funny namespace,
-// although most other vendors seem to use std. This may have to change if
-// other platforms are supported.
-#include <hash_map>
-using stdext::hash_map;
-
 #include "base/basictypes.h"
+#include "base/hash_tables.h"
 #include "base/logging.h"
 
 // This object maintains a list of IDs that can be quickly converted to
@@ -52,7 +46,7 @@ using stdext::hash_map;
 template<class T>
 class IDMap {
  private:
-  typedef hash_map<int32, T*> HashTable;
+  typedef base::hash_map<int32, T*> HashTable;
 
  public:
   IDMap() : next_id_(1) {
