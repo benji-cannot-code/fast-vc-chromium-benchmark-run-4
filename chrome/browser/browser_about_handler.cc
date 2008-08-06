1bd9c1ee8b0e1b2263f326c10aba2701fedc3f85
erikkay@google.com <erikkay@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-06 22:33:53 +0000

    * add webkit version and V8 version to about:version * reformat page * use new product image rather than an http link to google.com
    BUG=1300256
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@465 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -63,6 +63,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/about_handler.h"
 #include "googleurl/src/gurl.h"
 #include "webkit/glue/webkit_glue.h"
+#ifdef CHROME_V8
+#include "v8/public/v8.h"
+#endif
 
 #include "generated_resources.h"
 
@@ -255,9 +258,23 @@ std::string BrowserAboutHandler::AboutVersion() {
     DLOG(ERROR) << "Unable to create FileVersionInfo object";
     return std::string();
   }
+
+  std::wstring webkit_version = UTF8ToWide(webkit_glue::GetWebKitVersion());
+#ifdef CHROME_V8
+  const char* v8_vers = v8::V8::GetVersion();
+  std::wstring js_version = UTF8ToWide(v8_vers);
+  std::wstring js_engine = L"V8";
+#else
+  std::wstring js_version = webkit_version;
+  std::wstring js_engine = L"JavaScriptCore";
+#endif
+
   localized_strings.SetString(L"name",
       l10n_util::GetString(IDS_PRODUCT_NAME));
   localized_strings.SetString(L"version", version_info->file_version());
+  localized_strings.SetString(L"js_engine", js_engine);
+  localized_strings.SetString(L"js_version", js_version);
+  localized_strings.SetString(L"webkit_version", webkit_version);
   localized_strings.SetString(L"company",
       l10n_util::GetString(IDS_ABOUT_VERSION_COMPANY_NAME));
   localized_strings.SetString(L"copyright",
