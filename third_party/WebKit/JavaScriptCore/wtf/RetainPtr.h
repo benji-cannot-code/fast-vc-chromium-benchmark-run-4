614e51cb7ae6c2b4f985972d5b42457b3e4872e6
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-06 11:17:21 +0000

    2008-08-06  Ariya Hidayat  <ariya.hidayat@trolltech.com>
           Fix non-Mac build.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/RetainPtr.h
+++ third_party/WebKit/JavaScriptCore/wtf/RetainPtr.h
@@ -25,7 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RetainPtr_h
 
 #include <algorithm>
+
+#if PLATFORM(MAC)
 #include <CoreFoundation/CoreFoundation.h>
+#endif
 
 #ifdef __OBJC__
 #import <Foundation/Foundation.h>
