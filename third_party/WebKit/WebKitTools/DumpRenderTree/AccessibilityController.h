a76925eeb0dc0c2aa4d42e39532176b01315a2a4
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-06 20:01:35 +0000

    Reviewed by darin.
            Fix leaks seen on build-bot by fixing memory management of AccessibilityUIElement.
    
            I've made AccessibilityUIElement a stack object for now.  It could be
            made RefCounted, but I figured that might be overkill for the moment.
            Essentially it's just a RefPtr itself. :)
    
            I also fixed a few typos, such as AccessibilityUIElement() instead of ~AccessibilityUIElement()
            which was causing additional leaks. :)
    
            I added toAXElement to remove a bunch of copy/paste code.
    
            * DumpRenderTree/AccessibilityController.h:
            * DumpRenderTree/AccessibilityUIElement.cpp:
            (toAXElement):
            (allAttributesCallback):
            (attributesOfLinkedUIElementsCallback):
            (attributesOfChildrenCallback):
            (lineForIndexCallback):
            (boundsForRangeCallback):
            (childAtIndexCallback):
            (getRoleCallback):
            (getTitleCallback):
            (getDescriptionCallback):
            (getWidthCallback):
            (getHeightCallback):
            (getIntValueCallback):
            (getMinValueCallback):
            (getMaxValueCallback):
            (getInsertionPointLineNumberCallback):
            (finalize):
            * DumpRenderTree/AccessibilityUIElement.h:
            (AccessibilityUIElement::platformUIElement):
            * DumpRenderTree/mac/AccessibilityControllerMac.mm:
            (AccessibilityController::focusedElement):
            * DumpRenderTree/mac/AccessibilityUIElementMac.mm:
            (AccessibilityUIElement::AccessibilityUIElement):
            (AccessibilityUIElement::getLinkedUIElements):
            (AccessibilityUIElement::getChildren):
            (AccessibilityUIElement::getChildAtIndex):
            (AccessibilityUIElement::attributesOfLinkedUIElements):
            (AccessibilityUIElement::attributesOfChildren):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35608 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/AccessibilityController.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/AccessibilityController.h
@@ -39,8 +39,8 @@ public:
     void makeWindowObject(JSContextRef context, JSObjectRef windowObject, JSValueRef* exception);
 
     // Controller Methods - platfrom independant implementations
-    AccessibilityUIElement* rootElement();
-    AccessibilityUIElement* focusedElement();
+    AccessibilityUIElement rootElement();
+    AccessibilityUIElement focusedElement();
 
 private:
     static JSClassRef getJSClass();
