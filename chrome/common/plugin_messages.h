f0cfd709b6533526251557c8e9e6a791002292dc
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-06 19:00:35 +0000

    fix build (take 2)
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@445 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/plugin_messages.h
+++ chrome/common/plugin_messages.h
@@ -455,7 +455,7 @@ struct ParamTraits<NPIdentifier_Param> {
       l->append(UTF8ToWide(str));
       NPN_MemFree(str);
     } else {
-      l->append(IntToString(NPN_IntFromIdentifier(p.identifier)));
+      l->append(IntToWString(NPN_IntFromIdentifier(p.identifier)));
     }
   }
 };
