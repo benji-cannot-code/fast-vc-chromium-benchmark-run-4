6d2c5eac3bd1181257e5a8d9ad5472ce12d40025
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-06 10:17:05 +0000

    Build fix only, no review.
            Attempt to fix the Linux and --no-svg builds.
    
            * bindings/js/ScriptController.h: wrap RetainPtr include in PLATFORM(MAC)
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue): add missing CSSProperties to switch
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/ScriptController.h
+++ third_party/WebKit/WebCore/bindings/js/ScriptController.h
@@ -26,9 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSDOMWindowShell.h"
 #include <kjs/protect.h>
 #include <wtf/RefPtr.h>
-#include <wtf/RetainPtr.h>
 
 #if PLATFORM(MAC)
+#include <wtf/RetainPtr.h>
+
 #ifdef __OBJC__
 @class WebScriptObject;
 #else
