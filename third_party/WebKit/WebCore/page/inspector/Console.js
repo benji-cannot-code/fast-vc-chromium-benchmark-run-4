0720fdff05f263ddf1f2173e571fe945b1ca88e6
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-06 18:47:43 +0000

    2008-08-06  Kevin McCullough  <kmccullough@apple.com>
            Reviewed by Tim and Darin.
    
            <rdar://problem/6126160> URLs in the console should be clickable.
            - Implemented and now use linkifyString() to parse console messages for
            URLs.
    
            * page/inspector/Console.js:
            * page/inspector/inspector.js:
            * page/inspector/utilities.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35607 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/Console.js
+++ third_party/WebKit/WebCore/page/inspector/Console.js
@@ -405,8 +405,10 @@ WebInspector.Console.prototype = {
 
     _formatstring: function(str, elem, plainText)
     {
-        // Honor the plainText argument, if the textContent output doesn't make sense.
-        elem.appendChild(document.createTextNode("\"" + str + "\""));
+        if (plainText)
+            elem.appendChild(document.createTextNode("\"" + str + "\""));
+        else
+            elem.insertAdjacentHTML("beforeEnd", "\"" + WebInspector.linkifyString(str) + "\"");
     },
 
     _formatregexp: function(re, elem, plainText)
@@ -490,7 +492,7 @@ WebInspector.ConsoleMessage = function(source, level, line, url, groupLevel)
 
 WebInspector.ConsoleMessage.prototype = {
     _format: function(parameters, plainText)
-    {
+    {    
         var formattedResult = document.createElement("span");
 
         if (!parameters.length)
@@ -514,8 +516,9 @@ WebInspector.ConsoleMessage.prototype = {
             function append(a, b)
             {
                 if (!(b instanceof Node))
-                    b = document.createTextNode(b);
-                a.appendChild(b);
+                    a.insertAdjacentHTML("beforeEnd", WebInspector.linkifyString(b.toString()));
+                else
+                    a.appendChild(b);
                 return a;
             }
 
@@ -528,7 +531,7 @@ WebInspector.ConsoleMessage.prototype = {
 
         for (var i = 0; i < parameters.length; ++i) {
             if (typeof parameters[i] === "string")
-                formattedResult.appendChild(document.createTextNode(parameters[i]));
+                formattedResult.insertAdjacentHTML("beforeEnd", WebInspector.linkifyString(parameters[i]));
             else
                 formattedResult.appendChild(formatForConsole(parameters[i]));
             if (i < parameters.length - 1)
