9a22f3dbc500e69829763350ce011b43919e7bc1
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-07 07:24:18 +0000

    rollback r498 and r500 to repair test bustage
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@503 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run.cc
+++ chrome/browser/first_run.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_util.h"
 #include "base/logging.h"
+#include "base/object_watcher.h"
 #include "base/path_service.h"
 #include "base/process_util.h"
 #include "base/string_util.h"
@@ -198,7 +199,7 @@ namespace {
 // process has ended and what was the result of the operation as reported by
 // the process exit code. This class executes in the context of the main chrome
 // process.
-class ImportProcessRunner : public MessageLoop::Watcher {
+class ImportProcessRunner : public base::ObjectWatcher::Delegate {
  public:
   // The constructor takes the importer process to watch and then it does a
   // message loop blocking wait until the process ends. This object now owns
@@ -206,7 +207,7 @@ class ImportProcessRunner : public MessageLoop::Watcher {
   explicit ImportProcessRunner(ProcessHandle import_process)
       : import_process_(import_process),
         exit_code_(ResultCodes::NORMAL_EXIT) {
-    MessageLoop::current()->WatchObject(import_process, this);
+    watcher_.StartWatching(import_process, this);
     MessageLoop::current()->Run();
   }
   virtual ~ImportProcessRunner() {
@@ -219,7 +220,6 @@ class ImportProcessRunner : public MessageLoop::Watcher {
   }
   // The child process has terminated. Find the exit code and quit the loop.
   virtual void OnObjectSignaled(HANDLE object) {
-    MessageLoop::current()->WatchObject(object, NULL);
     DCHECK(object == import_process_);
     if (!::GetExitCodeProcess(import_process_, &exit_code_)) {
       NOTREACHED();
@@ -228,6 +228,7 @@ class ImportProcessRunner : public MessageLoop::Watcher {
   }
 
  private:
+  base::ObjectWatcher watcher_;
   ProcessHandle import_process_;
   DWORD exit_code_;
 };
--- chrome/browser/render_process_host.h
+++ chrome/browser/render_process_host.h
@@ -28,15 +28,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef CHROME_BROWSER_RENDER_PROCESS_HOST_H_
-#define CHROME_BROWSER_RENDER_PROCESS_HOST_H_
+#ifndef CHROME_BROWSER_RENDER_PROCESS_HOST_H__
+#define CHROME_BROWSER_RENDER_PROCESS_HOST_H__
 
-#include <set>
 #include <vector>
 #include <windows.h>
 
 #include "base/id_map.h"
-#include "base/object_watcher.h"
+#include "base/message_loop.h"
 #include "base/process.h"
 #include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
@@ -63,8 +62,8 @@ class WebContents;
 // are correlated with IDs. This way, the Views and the corresponding ViewHosts
 // communicate through the two process objects.
 class RenderProcessHost : public IPC::Channel::Listener,
+                          public MessageLoop::Watcher,
                           public IPC::Channel::Sender,
-                          public base::ObjectWatcher::Delegate,
                           public NotificationObserver {
  public:
   // Returns the RenderProcessHost given its ID.  Returns NULL if the ID does
@@ -157,7 +156,7 @@ class RenderProcessHost : public IPC::Channel::Listener,
   virtual void OnMessageReceived(const IPC::Message& msg);
   virtual void OnChannelConnected(int32 peer_pid);
 
-  // ObjectWatcher::Delegate
+  // MessageLoop watcher callback
   virtual void OnObjectSignaled(HANDLE object);
 
   // IPC::Channel::Sender callback
@@ -246,9 +245,6 @@ class RenderProcessHost : public IPC::Channel::Listener,
   // Our renderer process.
   Process process_;
 
-  // Used to watch the renderer process handle.
-  base::ObjectWatcher watcher_;
-
   // The profile associated with this renderer process.
   Profile* profile_;
 
@@ -294,4 +290,4 @@ inline std::wstring GenerateRandomChannelID(void* instance) {
 }
 
 
-#endif  // CHROME_BROWSER_RENDER_PROCESS_HOST_H_
+#endif  // CHROME_BROWSER_RENDER_PROCESS_HOST_H__
