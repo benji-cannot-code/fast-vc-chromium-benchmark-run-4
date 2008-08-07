733f836a72156c109c5888b9b65d8765d481eb38
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-07 16:04:57 +0000

    Delete backing stores before 5 second WM_TIMER fires if more than 2 are already queued.
            WM_TIMER-based timers can be starved if enough other events are keeping
            our message loop busy. This change prevents many backing store deletes from stacking
            up in this case.
            
            Reviewed by Geoff Garen.
    
            * WebView.cpp:
            (WebView::deleteBackingStore):
            (WebView::deleteBackingStoreSoon):
            (WebView::cancelDeleteBackingStoreSoon):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -116,6 +116,7 @@ using std::min;
 using std::max;
 
 static HMODULE accessibilityLib;
+static HashSet<WebView*> pendingDeleteBackingStoreSet;
 
 WebView* kit(Page* page)
 {
@@ -636,6 +637,8 @@ void WebView::close()
 
 void WebView::deleteBackingStore()
 {
+    pendingDeleteBackingStoreSet.remove(this);
+
     if (m_deleteBackingStoreTimerActive) {
         KillTimer(m_viewWindow, DeleteBackingStoreTimer);
         m_deleteBackingStoreTimerActive = false;
@@ -2726,6 +2729,17 @@ void WebView::updateActiveStateSoon() const
 
 void WebView::deleteBackingStoreSoon()
 {
+    if (pendingDeleteBackingStoreSet.size() > 2) {
+        Vector<WebView*> views;
+        HashSet<WebView*>::iterator end = pendingDeleteBackingStoreSet.end();
+        for (HashSet<WebView*>::iterator it = pendingDeleteBackingStoreSet.begin(); it != end; ++it)
+            views.append(*it);
+        for (int i = 0; i < views.size(); ++i)
+            views[i]->deleteBackingStore();
+        ASSERT(pendingDeleteBackingStoreSet.isEmpty());
+    }
+
+    pendingDeleteBackingStoreSet.add(this);
     m_deleteBackingStoreTimerActive = true;
     SetTimer(m_viewWindow, DeleteBackingStoreTimer, delayBeforeDeletingBackingStoreMsec, 0);
 }
@@ -2734,6 +2748,7 @@ void WebView::cancelDeleteBackingStoreSoon()
 {
     if (!m_deleteBackingStoreTimerActive)
         return;
+    pendingDeleteBackingStoreSet.remove(this);
     m_deleteBackingStoreTimerActive = false;
     KillTimer(m_viewWindow, DeleteBackingStoreTimer);
 }
