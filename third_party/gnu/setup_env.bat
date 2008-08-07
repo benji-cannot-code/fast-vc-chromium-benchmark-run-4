adc62d1e453239a142f3b1c0dafc41eb53096f87
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-07 22:00:12 +0000

    (Part of last commit, but gcl doesn't like deleting directories
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@540 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/gnu/setup_env.bat
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-:: This script must not rely on any external tools or PATH values.
-@echo OFF
-
-:: Let advanced users checkout the tools in just one P4 enlistment
-if "%SETUP_ENV_GNU_TOOLS%"=="done" goto :EOF
-set  SETUP_ENV_GNU_TOOLS=done
-
-set PATH=%PATH%;%~dp0\files
