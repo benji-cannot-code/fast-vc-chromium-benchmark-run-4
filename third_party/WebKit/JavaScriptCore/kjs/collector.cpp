a4a79abe67471ee6138245b27d668637b8c59a4c
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-08 09:48:10 +0000

    Improve performance of arithmetic operators
    Reviewed by Cameron Zwarich
    
    Added a fast (non-virtual) mechanism to determine if a non-immediate JSValue*
    is a JSNumberCell.  We then use this to allow improved specialisation in many
    arithmetic operators.  SunSpider reports a 2.5% progression overall, with greater
    than 10% progressions on a number of arithmetic heavy tests.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35639 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/collector.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/collector.cpp
@@ -160,6 +160,7 @@ Heap::~Heap()
 #endif
 }
 
+template <Heap::HeapType heapType>
 static NEVER_INLINE CollectorBlock* allocateBlock()
 {
 #if PLATFORM(DARWIN)
@@ -200,7 +201,7 @@ static NEVER_INLINE CollectorBlock* allocateBlock()
     address += adjust;
     memset(reinterpret_cast<void*>(address), 0, BLOCK_SIZE);
 #endif
-
+    reinterpret_cast<CollectorBlock*>(address)->type = heapType;
     return reinterpret_cast<CollectorBlock*>(address);
 }
 
@@ -347,7 +348,7 @@ collect:
             heap.blocks = static_cast<CollectorBlock**>(fastRealloc(heap.blocks, numBlocks * sizeof(CollectorBlock*)));
         }
 
-        targetBlock = (Block*)allocateBlock();
+        targetBlock = (Block*)allocateBlock<heapType>();
         targetBlock->freeList = targetBlock->cells;
         targetBlock->heap = this;
         targetBlockUsedCells = 0;
--- third_party/WebKit/JavaScriptCore/kjs/collector.h
+++ third_party/WebKit/JavaScriptCore/kjs/collector.h
@@ -106,7 +106,7 @@ namespace KJS {
         HashSet<ArgList*>& markListSet() { if (!m_markListSet) m_markListSet = new HashSet<ArgList*>; return *m_markListSet; }
 
         JSGlobalData* globalData() const { return m_globalData; }
-
+        static bool fastIsNumber(JSCell*);
     private:
         template <Heap::HeapType heapType> void* heapAllocate(size_t);
         template <Heap::HeapType heapType> size_t sweep();
@@ -203,6 +203,7 @@ namespace KJS {
         CollectorCell* freeList;
         CollectorBitmap marked;
         Heap* heap;
+        Heap::HeapType type;
     };
 
     class SmallCellCollectorBlock {
@@ -212,7 +213,14 @@ namespace KJS {
         SmallCollectorCell* freeList;
         CollectorBitmap marked;
         Heap* heap;
+        Heap::HeapType type;
     };
+    
+    inline bool Heap::fastIsNumber(JSCell* cell)
+    {
+        CollectorBlock* block = Heap::cellBlock(cell);
+        return block && block->type == NumberHeap;
+    }
 
     inline const CollectorBlock* Heap::cellBlock(const JSCell* cell)
     {
