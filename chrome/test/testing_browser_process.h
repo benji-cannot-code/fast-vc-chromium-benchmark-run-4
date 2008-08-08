341fbc4429da305d0b82cae5b9dd2a6b9d67b3df
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-08 17:52:58 +0000

    fix bustage
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@574 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/testing_browser_process.h
+++ chrome/test/testing_browser_process.h
@@ -144,6 +144,8 @@ class TestingBrowserProcess : public BrowserProcess {
 
   virtual SuspendController* suspend_controller() { return NULL; }
 
+  virtual bool IsUsingNewFrames() { return false; }
+
  private:
   NotificationService notification_service_;
   DISALLOW_EVIL_CONSTRUCTORS(TestingBrowserProcess);
