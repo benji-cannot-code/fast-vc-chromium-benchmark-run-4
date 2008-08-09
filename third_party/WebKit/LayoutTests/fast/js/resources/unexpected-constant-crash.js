eebeb8d9aaf3b8296d6ea1c26181162580c9994d
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-09 03:56:46 +0000

    2008-08-08  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
            Reviewed by Oliver.
    
            Bug 20330: JSCore crash loading any filehurricane media page
            <https://bugs.webkit.org/show_bug.cgi?id=20330>
    
            Fix a typo in the constant loading patch. Also, add a case for
            op_unexpected_load to CodeBlock::dump().
    
            JavaScriptCore:
    
            * VM/CodeBlock.cpp:
            (KJS::CodeBlock::dump):
            * VM/CodeGenerator.cpp:
            (KJS::CodeGenerator::addUnexpectedConstant):
    
            LayoutTests:
    
            * fast/js/resources/unexpected-constant-crash.js: Added.
            * fast/js/unexpected-constant-crash-expected.txt: Added.
            * fast/js/unexpected-constant-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35651 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/unexpected-constant-crash.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This test checks that the regexp and unexpected constant counters are not confused with each other. It will fail with an assertion failure in a debug build if this is the case."
+);
+
+var r = / /;
+var s;
+delete s;
+
+var successfullyParsed = true;
