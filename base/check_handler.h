2b15d7477b1e66d2ddd5658091a555bcdcf2cb0f
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-11 23:36:16 +0000

    bustage fix, include build_config for platform defines.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@684 0039d316-1c4b-4281-b951-d872f2087c98

--- base/check_handler.h
+++ base/check_handler.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_CHECK_HANDLER_H__
 #define BASE_CHECK_HANDLER_H__
 
+#include "build/build_config.h"
+
 #if defined(OS_WIN)
 #include <windows.h>
 #endif
