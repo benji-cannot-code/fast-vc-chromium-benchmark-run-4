60b77a69fed4a269638956c0403a70960da99d3a
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-11 18:04:46 +0000

    WebCore:
            Reviewed by Darin Adler.
    
            - fix <rdar://problem/6131096> Reproducible crash in CounterNode::isReset under guard malloc
    
            Test: fast/css/counters/invalidate-cached-counter-node.html
    
            * rendering/RenderContainer.cpp:
            (WebCore::RenderContainer::invalidateCounters): Added. Invalidates all
            RenderCounters in :before and :after content.
            * rendering/RenderContainer.h:
            * rendering/RenderCounter.cpp:
            (WebCore::RenderCounter::isCounter): Renamed isRenderCounter() to this
            to match the RenderObject method.
            (WebCore::RenderCounter::invalidate): Added. Resets the cached
            CounterNode and invalidates the object's layout and preferred widths.
            (WebCore::destroyCounterNodeChildren): Added a call to
            invalidateCounters().
            * rendering/RenderCounter.h:
            * rendering/RenderObject.h:
            (WebCore::RenderObject::invalidateCounters):
    
    LayoutTests:
    
            Reviewed by Darin Adler.
    
            - test for <rdar://problem/6131096> Reproducible crash in CounterNode::isReset under guard malloc
    
            * fast/css/counters/invalidate-cached-counter-node.html: Added.
            * platform/mac/fast/css/counters/invalidate-cached-counter-node-expected.checksum: Added.
            * platform/mac/fast/css/counters/invalidate-cached-counter-node-expected.png: Added.
            * platform/mac/fast/css/counters/invalidate-cached-counter-node-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35667 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderCounter.cpp
+++ third_party/WebKit/WebCore/rendering/RenderCounter.cpp
@@ -220,7 +220,7 @@ const char* RenderCounter::renderName() const
     return "RenderCounter";
 }
 
-bool RenderCounter::isRenderCounter() const
+bool RenderCounter::isCounter() const
 {
     return true;
 }
@@ -264,6 +264,12 @@ void RenderCounter::calcPrefWidths(int lead)
     RenderText::calcPrefWidths(lead);
 }
 
+void RenderCounter::invalidate()
+{
+    m_counterNode = 0;
+    setNeedsLayoutAndPrefWidthsRecalc();
+}
+
 static void destroyCounterNodeChildren(AtomicStringImpl* identifier, CounterNode* node)
 {
     CounterNode* previous;
@@ -272,6 +278,7 @@ static void destroyCounterNodeChildren(AtomicStringImpl* identifier, CounterNode
         child->parent()->removeChild(child);
         ASSERT(counterMaps().get(child->renderer())->get(identifier) == child);
         counterMaps().get(child->renderer())->remove(identifier);
+        child->renderer()->invalidateCounters();
         delete child;
     }
 }
--- third_party/WebKit/WebCore/rendering/RenderObject.h
+++ third_party/WebKit/WebCore/rendering/RenderObject.h
@@ -406,6 +406,7 @@ public:
     void setNeedsPositionedMovementLayout();
     void setPrefWidthsDirty(bool, bool markParents = true);
     void invalidateContainerPrefWidths();
+    virtual void invalidateCounters() { }
     
     void setNeedsLayoutAndPrefWidthsRecalc()
     {
