9bae3fa1cea5960783d8cc4d1d6f9de18a5c0ddb
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-11 04:51:38 +0000

    Reviewed by Eric Seidel.
            - fix https://bugs.webkit.org/show_bug.cgi?id=20339
              REGRESSION (r35531-r35615): Acid3 crashes on Windows in CachedFont::getSVGFontById
    
            * dom/make_names.pl: Removed "using namespace WebCore::*Names"
            from *ElementFactory.cpp files.        
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35661 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/make_names.pl
+++ third_party/WebKit/WebCore/dom/make_names.pl
@@ -216,7 +216,7 @@ sub printConstructors
 
         print F "$parameters{'namespace'}Element* ${name}Constructor(Document* doc, bool createdByParser)\n";
         print F "{\n";
-        print F "    return new $parameters{'namespace'}${ucName}Element(${name}Tag, doc);\n";
+        print F "    return new $parameters{'namespace'}${ucName}Element($parameters{'namespace'}Names::${name}Tag, doc);\n";
         print F "}\n\n";
     }
     print F "#endif\n" if $parameters{'guardFactoryWith'};
@@ -226,7 +226,7 @@ sub printFunctionInits
 {
     my ($F, $namesRef) = @_;
     for my $name (sort keys %$namesRef) {
-        print F "    gFunctionMap->set(${name}Tag.localName().impl(), ${name}Constructor);\n";
+        print F "    gFunctionMap->set($parameters{'namespace'}Names::${name}Tag.localName().impl(), ${name}Constructor);\n";
     }
 }
 
@@ -517,7 +517,6 @@ print F <<END
 #include <wtf/HashMap.h>
 
 using namespace WebCore;
-using namespace $parameters{'cppNamespace'}::$parameters{'namespace'}Names;
 
 typedef $parameters{'namespace'}Element* (*ConstructorFunc)(Document* doc, bool createdByParser);
 typedef WTF::HashMap<AtomicStringImpl*, ConstructorFunc> FunctionMap;
