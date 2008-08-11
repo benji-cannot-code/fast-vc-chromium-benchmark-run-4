827c3ee6597d1d72512d4d4f4c41398fabfd9c81
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-11 12:24:57 +0000

    HashTable is a dependent name, since it's based on the template parameter T.  Use typename for derived types, this will now build on GCC.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@643 0039d316-1c4b-4281-b951-d872f2087c98

--- base/id_map.h
+++ base/id_map.h
@@ -84,7 +84,7 @@ class IDMap {
   }
 
   void Remove(int32 id) {
-    HashTable::iterator i = data_.find(id);
+    typename HashTable::iterator i = data_.find(id);
     if (i == data_.end()) {
       NOTREACHED() << "Attempting to remove an item not in the list";
       return;
@@ -97,7 +97,7 @@ class IDMap {
   }
 
   T* Lookup(int32 id) const {
-    HashTable::const_iterator i = data_.find(id);
+    typename HashTable::const_iterator i = data_.find(id);
     if (i == data_.end())
       return NULL;
     return i->second;
