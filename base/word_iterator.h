bd2b16b76fe92709b05e8cc7e0456688576fe0a1
avi@google.com <avi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-11 21:26:52 +0000

    Fixes word_iterator to actually compile. Next time, never just paste code in that _should_ fix things without actually compiling :(
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@672 0039d316-1c4b-4281-b951-d872f2087c98

--- base/word_iterator.h
+++ base/word_iterator.h
@@ -32,9 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_WORD_ITERATOR_H__
 
 #include <string>
+#include <vector>
+
+#include "unicode/uchar.h"
 
 #include "base/basictypes.h"
-#include "unicode/umachine.h"  // Needed for U_WCHAR_IS_UTF16.
 
 // The WordIterator class iterates through the words and word breaks
 // in a string.  (In the string " foo bar! ", the word breaks are at the
@@ -95,7 +97,7 @@ class WordIterator {
  private:
   // ICU iterator.
   void* iter_;
-#ifndef U_WCHAR_IS_UTF16
+#if !defined(WCHAR_T_IS_UTF16)
   std::vector<UChar> chars_;
 #endif
 
