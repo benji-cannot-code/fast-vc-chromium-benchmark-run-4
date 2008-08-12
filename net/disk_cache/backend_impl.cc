62b7e3fb5748496c9c94590dec6dc0a12e3f67d7
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-12 23:57:07 +0000

    Re submitting the histograms change (735)
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@763 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_impl.cc
+++ net/disk_cache/backend_impl.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/disk_cache/backend_impl.h"
 
 #include "base/file_util.h"
+#include "base/histogram.h"
 #include "base/message_loop.h"
 #include "base/scoped_handle.h"
 #include "base/string_util.h"
@@ -326,6 +327,7 @@ bool BackendImpl::OpenEntry(const std::string& key, Entry** entry) {
   if (disabled_)
     return false;
 
+  Time start = Time::Now();
   uint32 hash = Hash(key);
 
   EntryImpl* cache_entry = MatchEntry(key, hash, false);
@@ -337,6 +339,7 @@ bool BackendImpl::OpenEntry(const std::string& key, Entry** entry) {
   DCHECK(entry);
   *entry = cache_entry;
 
+  UMA_HISTOGRAM_TIMES(L"DiskCache.OpenTime", Time::Now() - start);
   stats_.OnEvent(Stats::OPEN_HIT);
   return true;
 }
@@ -345,6 +348,7 @@ bool BackendImpl::CreateEntry(const std::string& key, Entry** entry) {
   if (disabled_ || key.empty())
     return false;
 
+  Time start = Time::Now();
   uint32 hash = Hash(key);
 
   scoped_refptr<EntryImpl> parent;
@@ -407,6 +411,7 @@ bool BackendImpl::CreateEntry(const std::string& key, Entry** entry) {
   *entry = NULL;
   cache_entry.swap(reinterpret_cast<EntryImpl**>(entry));
 
+  UMA_HISTOGRAM_TIMES(L"DiskCache.CreateTime", Time::Now() - start);
   stats_.OnEvent(Stats::CREATE_HIT);
   Trace("create entry hit ");
   return true;
@@ -1035,6 +1040,7 @@ void BackendImpl::TrimCache(bool empty) {
   if (disabled_)
     return;
 
+  Time start = Time::Now();
   Rankings::ScopedRankingsBlock node(&rankings_);
   Rankings::ScopedRankingsBlock next(&rankings_, rankings_.GetPrev(node.get()));
   DCHECK(next.get());
@@ -1054,6 +1060,9 @@ void BackendImpl::TrimCache(bool empty) {
       if (node->Data()->pointer) {
         entry = EntryImpl::Update(entry);
       }
+      static Histogram counter(L"DiskCache.TrimAge", 1, 10000, 50);
+      counter.SetFlags(kUmaTargetedHistogramFlag);
+      counter.Add((Time::Now() - entry->GetLastUsed()).InHours());
       entry->Doom();
       entry->Release();
       if (!empty)
@@ -1061,6 +1070,7 @@ void BackendImpl::TrimCache(bool empty) {
     }
   }
 
+  UMA_HISTOGRAM_TIMES(L"DiskCache.TotalTrimTime", Time::Now() - start);
   Trace("*** Trim Cache end ***");
   return;
 }
