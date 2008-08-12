90bc13f83f4011f677cc3c722fa0ada2a019c258
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-12 17:45:47 +0000

    Remove the call to WKNSWindowMakeBottomCornersSquare on the         Web Inspector's window. This isn't needed anymore since the         window style masks used always have square bottom corners.
            Reviewed by Adam Roben.
    
            * WebCoreSupport/WebInspectorClient.mm:
            (-[WebInspectorWindowController window]): Remove the call
            to WKNSWindowMakeBottomCornersSquare.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35688 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebInspectorClient.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebInspectorClient.mm
@@ -49,8 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/DOMExtensions.h>
 #import <WebKit/WebUIDelegate.h>
 
-#import <WebKitSystemInterface.h>
-
 using namespace WebCore;
 
 @interface WebInspectorWindowController : NSWindowController <NSWindowDelegate> {
@@ -237,8 +235,6 @@ void WebInspectorClient::updateWindowTitle() const
 #ifndef BUILDING_ON_TIGER
     [window setAutorecalculatesContentBorderThickness:NO forEdge:NSMaxYEdge];
     [window setContentBorderThickness:55. forEdge:NSMaxYEdge];
-
-    WKNSWindowMakeBottomCornersSquare(window);
 #endif
 
     [self setWindow:window];
