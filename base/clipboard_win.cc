51451f3aa685307d51f71871a2235fceb8e728ca
avi@google.com <avi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-12 16:25:23 +0000

    Basic implementation of the clipboard on the Mac.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@717 0039d316-1c4b-4281-b951-d872f2087c98

--- base/clipboard_win.cc
+++ base/clipboard_win.cc
@@ -175,7 +175,7 @@ Clipboard::~Clipboard() {
   clipboard_owner_ = NULL;
 }
 
-void Clipboard::Clear() const {
+void Clipboard::Clear() {
   // Acquire the clipboard.
   ClipboardLock lock;
   if (!lock.Acquire(clipboard_owner_))
@@ -184,7 +184,7 @@ void Clipboard::Clear() const {
   ::EmptyClipboard();
 }
 
-void Clipboard::WriteText(const std::wstring& text) const {
+void Clipboard::WriteText(const std::wstring& text) {
   ClipboardLock lock;
   if (!lock.Acquire(clipboard_owner_))
     return;
@@ -195,7 +195,7 @@ void Clipboard::WriteText(const std::wstring& text) const {
 }
 
 void Clipboard::WriteHTML(const std::wstring& markup,
-                          const std::string& url) const {
+                          const std::string& url) {
   // Acquire the clipboard.
   ClipboardLock lock;
   if (!lock.Acquire(clipboard_owner_))
@@ -211,7 +211,7 @@ void Clipboard::WriteHTML(const std::wstring& markup,
 }
 
 void Clipboard::WriteBookmark(const std::wstring& title,
-                              const std::string& url) const {
+                              const std::string& url) {
   // Acquire the clipboard.
   ClipboardLock lock;
   if (!lock.Acquire(clipboard_owner_))
@@ -228,7 +228,7 @@ void Clipboard::WriteBookmark(const std::wstring& title,
 }
 
 void Clipboard::WriteHyperlink(const std::wstring& title,
-                               const std::string& url) const {
+                               const std::string& url) {
   // Write as a bookmark.
   WriteBookmark(title, url);
 
@@ -243,7 +243,7 @@ void Clipboard::WriteHyperlink(const std::wstring& title,
   WriteHTML(link, std::string());
 }
 
-void Clipboard::WriteWebSmartPaste() const {
+void Clipboard::WriteWebSmartPaste() {
   // Acquire the clipboard.
   ClipboardLock lock;
   if (!lock.Acquire(clipboard_owner_))
@@ -252,7 +252,7 @@ void Clipboard::WriteWebSmartPaste() const {
   SetClipboardData(ClipboardUtil::GetWebKitSmartPasteFormat()->cfFormat, NULL);
 }
 
-void Clipboard::WriteBitmap(const void* pixels, const gfx::Size& size) const {
+void Clipboard::WriteBitmap(const void* pixels, const gfx::Size& size) {
   HDC dc = ::GetDC(NULL);
 
   // This doesn't actually cost us a memcpy when the bitmap comes from the
@@ -288,7 +288,7 @@ void Clipboard::WriteBitmap(const void* pixels, const gfx::Size& size) const {
 }
 
 void Clipboard::WriteBitmapFromSharedMemory(const SharedMemory& bitmap,
-                                            const gfx::Size& size) const {
+                                            const gfx::Size& size) {
   // TODO(darin): share data in gfx/bitmap_header.cc somehow
   BITMAPINFO bm_info = {0};
   bm_info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
@@ -315,7 +315,7 @@ void Clipboard::WriteBitmapFromSharedMemory(const SharedMemory& bitmap,
 }
 
 void Clipboard::WriteBitmapFromHandle(HBITMAP source_hbitmap,
-                                      const gfx::Size& size) const {
+                                      const gfx::Size& size) {
   // Acquire the clipboard.
   ClipboardLock lock;
   if (!lock.Acquire(clipboard_owner_))
@@ -364,13 +364,13 @@ void Clipboard::WriteBitmapFromHandle(HBITMAP source_hbitmap,
 // Write a file or set of files to the clipboard in HDROP format. When the user
 // invokes a paste command (in a Windows explorer shell, for example), the files
 // will be copied to the paste location.
-void Clipboard::WriteFile(const std::wstring& file) const {
+void Clipboard::WriteFile(const std::wstring& file) {
   std::vector<std::wstring> files;
   files.push_back(file);
   WriteFiles(files);
 }
 
-void Clipboard::WriteFiles(const std::vector<std::wstring>& files) const {
+void Clipboard::WriteFiles(const std::vector<std::wstring>& files) {
   ClipboardLock lock;
   if (!lock.Acquire(clipboard_owner_))
     return;
