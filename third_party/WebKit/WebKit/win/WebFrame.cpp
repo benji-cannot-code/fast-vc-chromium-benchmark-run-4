a71007d3c6b1e2a337637f42d638492feb9eac9c
adachan@apple.com <adachan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-13 19:03:27 +0000

    Implemented DOMHTMLElement::idName() and WebFrame::frameElement().         Added IWebFrame::paintDocumentRectToContext() so we can paint a frame's         content into a device context.  Changed WebView::paintDocumentRectToContext()         to call that WebFrame method.
            Reviewed by Adam Roben.
    
            * DOMHTMLClasses.cpp:
            (DOMHTMLElement::idName):
            * Interfaces/IWebFramePrivate.idl:
            * WebFrame.cpp:
            (WebFrame::paintDocumentRectToContext):
            (WebFrame::frameElement):
            * WebFrame.h:
            * WebView.cpp:
            (WebView::paintDocumentRectToContext):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35723 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebFrame.cpp
+++ third_party/WebKit/WebKit/win/WebFrame.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "COMPropertyBag.h"
 #include "DefaultPolicyDelegate.h"
 #include "DOMCoreClasses.h"
+#include "HTMLFrameOwnerElement.h"
 #include "MarshallingHelpers.h"
 #include "WebActionPropertyBag.h"
 #include "WebChromeClient.h"
@@ -285,6 +286,37 @@ HRESULT STDMETHODCALLTYPE WebFrame::setIsDisconnected(
     return E_FAIL;
 }
 
+HRESULT STDMETHODCALLTYPE WebFrame::paintDocumentRectToContext(
+    /* [in] */ RECT rect,
+    /* [in] */ OLE_HANDLE deviceContext)
+{
+    Frame* coreFrame = core(this);
+    if (!coreFrame)
+        return E_FAIL;
+
+    FrameView* view = coreFrame->view();
+    if (!view)
+        return E_FAIL;
+
+    // We can't paint with a layout still pending.
+    view->layoutIfNeededRecursive();
+
+    HDC dc = (HDC)(ULONG64)deviceContext;
+    GraphicsContext gc(dc);
+    gc.save();
+    LONG width = rect.right - rect.left;
+    LONG height = rect.bottom - rect.top;
+    FloatRect dirtyRect;
+    dirtyRect.setWidth(width);
+    dirtyRect.setHeight(height);
+    gc.clip(dirtyRect);
+    gc.translate(-rect.left, -rect.top);
+    coreFrame->paint(&gc, rect);
+    gc.restore();
+
+    return S_OK;
+}
+
 // IUnknown -------------------------------------------------------------------
 
 HRESULT STDMETHODCALLTYPE WebFrame::QueryInterface(REFIID riid, void** ppvObject)
@@ -377,10 +409,21 @@ HRESULT STDMETHODCALLTYPE WebFrame::DOMDocument(
 }
 
 HRESULT STDMETHODCALLTYPE WebFrame::frameElement( 
-    /* [retval][out] */ IDOMHTMLElement** /*frameElement*/)
+    /* [retval][out] */ IDOMHTMLElement** frameElement)
 {
-    ASSERT_NOT_REACHED();
-    return E_NOTIMPL;
+    if (!frameElement)
+        return E_POINTER;
+
+    *frameElement = 0;
+    Frame* coreFrame = core(this);
+    if (!coreFrame)
+        return E_FAIL;
+
+    COMPtr<IDOMElement> domElement(AdoptCOM, DOMElement::createInstance(coreFrame->ownerElement()));
+    COMPtr<IDOMHTMLElement> htmlElement(Query, domElement);
+    if (!htmlElement)
+        return E_FAIL;
+    return htmlElement.copyRefTo(frameElement);
 }
 
 HRESULT STDMETHODCALLTYPE WebFrame::currentForm( 
