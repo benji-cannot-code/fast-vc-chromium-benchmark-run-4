c47820b80c71afd791ea8b7e8ffc9501e37e1bc1
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-13 21:51:45 +0000

    Tests for KeywordEditorView.
    BUG=1324185
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@828 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/testing_profile.h
+++ chrome/test/testing_profile.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_prefs.h"
 #include "chrome/browser/history/history.h"
 #include "chrome/browser/profile.h"
+#include "chrome/browser/template_url_model.h"
 #include "chrome/common/pref_service.h"
 
 class TestingProfile : public Profile {
@@ -51,6 +52,9 @@ class TestingProfile : public Profile {
   // Creates the BookmkarBarModel. If not invoked the bookmark bar model is
   // NULL.
   void CreateBookmarkBarModel();
+
+  // Creates a TemplateURLModel. If not invoked the TemplateURLModel is NULL.
+  void CreateTemplateURLModel();
 
   virtual std::wstring GetPath() {
     return std::wstring();
@@ -91,7 +95,7 @@ class TestingProfile : public Profile {
     return prefs_.get();
   }
   virtual TemplateURLModel* GetTemplateURLModel() {
-    return NULL;
+    return template_url_model_.get();
   }
   virtual TemplateURLFetcher* GetTemplateURLFetcher() {
     return NULL;
@@ -183,6 +187,9 @@ class TestingProfile : public Profile {
 
   // The BookmarkBarModel. Only created if CreateBookmarkBarModel is invoked.
   scoped_ptr<BookmarkBarModel> bookmark_bar_model_;
+
+  // The TemplateURLFetcher. Only created if CreateTemplateURLModel is invoked.
+  scoped_ptr<TemplateURLModel> template_url_model_;
 
   // Do we have a history service? This defaults to the value of
   // history_service, but can be explicitly set.
