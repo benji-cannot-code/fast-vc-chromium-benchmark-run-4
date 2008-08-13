2c2d53a6327bdf1fc626404f2ddbff5b3df3d8ba
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-13 23:20:03 +0000

    Fix for mac and linux. Fix gcc warning about class with virtual functions and no virtual dtor.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@842 0039d316-1c4b-4281-b951-d872f2087c98

--- base/platform_thread.h
+++ base/platform_thread.h
@@ -49,6 +49,9 @@ class PlatformThread {
  public:
   // Gets the current thread.
   static PlatformThread Current();
+  
+  // Gets the current thread id
+  static int CurrentId();
 
   // Yield the current thread so another thread can be scheduled.
   static void YieldCurrentThread();
