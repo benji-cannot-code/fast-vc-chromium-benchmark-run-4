b64fa524e59ab90d15954c8d3bd27a3cc1fa93e0
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-13 18:11:01 +0000

    Remember the docked state of the Web Inspector, so it can be reopened docked if it was last docked.
    WebCore:
    
    2008-08-13  Timothy Hatcher  <timothy@apple.com>
    
            Changed the InspectorController so it can be notified when the
            attached state of the Inspector changes in WebKit.
    
            Reviewed by Kevin McCullough.
    
            * WebCore.base.exp: Updated the symbol for setWindowVisible.
            * page/InspectorController.cpp:
            (WebCore::InspectorController::setWindowVisible): Added an attached argument,
            that defaults to false.Call setAttachedWindow with the attached argument.
            (WebCore::InspectorController::setAttachedWindow): Call the script version
            of setAttachedWindow.
            * page/InspectorController.h:
            * page/inspector/inspector.js:
            (WebInspector.setAttachedWindow): Set the attached property.
    
    WebKit/mac:
    
    2008-08-13  Timothy Hatcher  <timothy@apple.com>
    
            Remember the docked state of the Web Inspector, so it can be
            reopened docked if it was last docked.
    
            https://bugs.webkit.org/show_bug.cgi?id=14271
    
            Reviewed by Kevin McCullough.
    
            * WebCoreSupport/WebInspectorClient.mm:
            (-[WebInspectorWindowController init]): Initialize _shouldAttach
            to the value stored in the user defaults. If there has never
            been a value stored, default to being attached.
            (-[WebInspectorWindowController showWindow:]): Pass the attached
            state to InspectorController::setWindowVisible.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35722 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/inspector.js
+++ third_party/WebKit/WebCore/page/inspector/inspector.js
@@ -398,6 +398,11 @@ WebInspector.focusChanged = function(event)
     this.currentFocusElement = event.target;
 }
 
+WebInspector.setAttachedWindow = function(attached)
+{
+    this.attached = attached;
+}
+
 WebInspector.close = function(event)
 {
     InspectorController.closeWindow();
