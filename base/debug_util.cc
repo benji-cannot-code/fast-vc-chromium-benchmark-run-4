af9c24ce574733853fc46cb129e45dd5c87f55cc
mmentovai@google.com <mmentovai@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-13 13:18:34 +0000

    Fix the Mac project to build debug_util since r785 added it.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@792 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug_util.cc
+++ base/debug_util.cc
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug_util.h"
 
-#include <windows.h>
-
 #include "base/platform_thread.h"
 
 bool DebugUtil::WaitForDebugger(int wait_seconds, bool silent) {
