25967c29f5f53d8f8fc8804a26c6991362c9d2c0
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-13 15:21:53 +0000

    Kill check handler.  It seems to be unused, and is implemented using ugly SEH handlers.  If we want this we could reimplement it cleanly.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@802 0039d316-1c4b-4281-b951-d872f2087c98

--- base/condition_variable_test.cc
+++ base/condition_variable_test.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <vector>
 
-#include "base/check_handler.h"
 #include "base/condition_variable.h"
 #include "base/logging.h"
 #include "base/scoped_ptr.h"
