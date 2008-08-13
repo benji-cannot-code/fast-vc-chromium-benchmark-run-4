b0c0e9164434f0935d941bcfdfbf55f0126eebf7
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-13 22:55:45 +0000

    Turn on most tests for posix.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@839 0039d316-1c4b-4281-b951-d872f2087c98

--- base/time_unittest.cc
+++ base/time_unittest.cc
@@ -28,10 +28,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <process.h>
 #include <time.h>
 
+#if defined(OS_WIN)
+#include <process.h>
+#endif
+
 #include "base/time.h"
+#include "base/platform_thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 // Test conversions to/from time_t and exploding/unexploding.
@@ -39,7 +43,11 @@ TEST(Time, TimeT) {
   // C library time and exploded time.
   time_t now_t_1 = time(NULL);
   struct tm tms;
+#if defined(OS_WIN)
   localtime_s(&tms, &now_t_1);
+#elif defined(OS_POSIX)
+  localtime_r(&now_t_1, &tms);
+#endif
 
   // Convert to ours.
   Time our_time_1 = Time::FromTimeT(now_t_1);
@@ -94,13 +102,17 @@ TEST(Time, UTCExplode) {
 
 TEST(TimeTicks, Deltas) {
   TimeTicks ticks_start = TimeTicks::Now();
-  Sleep(10);
+  PlatformThread::Sleep(10);
   TimeTicks ticks_stop = TimeTicks::Now();
   TimeDelta delta = ticks_stop - ticks_start;
   EXPECT_GE(delta.InMilliseconds(), 10);
   EXPECT_GE(delta.InMicroseconds(), 10000);
   EXPECT_EQ(delta.InSeconds(), 0);
 }
+
+#if defined(OS_WIN)
+
+// TODO(pinkerton): Need to find a way to mock this for non-windows.
 
 namespace {
 
@@ -194,3 +206,5 @@ TEST(TimeTicks, Rollover) {
     MockTimeTicks::UninstallTicker();
   }
 }
+
+#endif
