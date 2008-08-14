b903f3f76d180cd8d563fcda3225f33f19b830a0
christian@webkit.org <christian@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-14 19:14:47 +0000

    Gtk+/ Cairo build fix, patch by Dirk Schulze.
    git-svn-id: svn://svn.chromium.org/blink/trunk@35757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/CanvasStyle.cpp
+++ third_party/WebKit/WebCore/html/CanvasStyle.cpp
@@ -155,9 +155,6 @@ void CanvasStyle::applyStrokeColor(GraphicsContext* context)
     }
 }
 
-// Cairo's graphics model allows us to share a single code path for
-// stroke and fill.
-#if !PLATFORM(CAIRO)
 void CanvasStyle::applyFillColor(GraphicsContext* context)
 {
     if (!context)
@@ -203,6 +200,5 @@ void CanvasStyle::applyFillColor(GraphicsContext* context)
             break;
     }
 }
-#endif
 
 }
