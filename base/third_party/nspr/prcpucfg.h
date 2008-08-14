3b4cbfa73e2bafa83dc0dc8242b3ffe6f60905db
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-14 11:17:51 +0000

    Implement nspr time on Linux.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@863 0039d316-1c4b-4281-b951-d872f2087c98

--- base/third_party/nspr/prcpucfg.h
+++ base/third_party/nspr/prcpucfg.h
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/third_party/nspr/prcpucfg_win.h"
 #elif defined(__APPLE__)
 #include "base/third_party/nspr/prcpucfg_mac.h"
+#elif defined(__linux__)
+#include "base/third_party/nspr/prcpucfg_linux.h"
 #else
 #error Provide a prcpucfg.h appropriate for your platform
 #endif
