f00f60912cbf196677cc3691512271452d86f2f0
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-15 00:17:50 +0000

    Make more cross-platform
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@917 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin.h
+++ webkit/glue/webplugin.h
@@ -33,12 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 #include <vector>
-#include <windows.h>
 
 #include "base/basictypes.h"
 #include "base/gfx/rect.h"
 
 typedef struct HWND__* HWND;
+typedef void* HANDLE;
 
 class GURL;
 class WebFrame;
