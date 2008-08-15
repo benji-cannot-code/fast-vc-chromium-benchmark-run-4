432f1bc6218ee584c0f1d50e83f39f225a797f35
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-15 01:05:11 +0000

    Fix more compiler warnings.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@922 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_escape_unittest.cc
+++ base/string_escape_unittest.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 TEST(StringEscapeTest, JavascriptDoubleQuote) {
   static const char* kToEscape          = "\b\001aZ\"\\wee";
   static const char* kEscaped           = "\\b\\x01aZ\\\"\\\\wee";
-  static const char* kEscapedQuoted     = "\"\\b\\x01aZ\\\"\\\\wee\"";
   static const wchar_t* kUToEscape      = L"\b\x0001" L"a\x123fZ\"\\wee";
   static const char* kUEscaped          = "\\b\\x01a\\u123FZ\\\"\\\\wee";
   static const char* kUEscapedQuoted    = "\"\\b\\x01a\\u123FZ\\\"\\\\wee\"";
