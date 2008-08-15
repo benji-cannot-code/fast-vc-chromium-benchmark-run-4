c923c39bb1d5f9162ba5ff1ccf1ddb8764bcf8ca
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-15 18:12:34 +0000

    2008-08-15  Håvard Wall  <hwall@trolltech.com>
            Reviewed by Simon.
    
            Fixes: compile with QT_NO_STYLE_STYLESHEET
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
@@ -473,7 +473,7 @@ void FrameLoaderClientQt::postProgressEstimateChangedNotification()
 
 void FrameLoaderClientQt::postProgressFinishedNotification()
 {
-    // send a mousemove event to 
+    // send a mousemove event to
     // (1) update the cursor to change according to whatever is underneath the mouse cursor right now
     // (2) display the tool tip if the mouse hovers a node which has a tool tip
     if (m_frame && m_frame->eventHandler() && m_webFrame->page()) {
@@ -1050,6 +1050,7 @@ Widget* FrameLoaderClientQt::createPlugin(const IntSize& pluginSize, Element* el
 
     if (mimeType == "application/x-qt-plugin" || mimeType == "application/x-qt-styled-widget") {
         object = m_webFrame->page()->createPlugin(classid, qurl, params, values);
+#ifndef QT_NO_STYLE_STYLESHEET
         QWidget *widget = qobject_cast<QWidget *>(object);
         if (widget && mimeType == "application/x-qt-styled-widget") {
 
@@ -1068,6 +1069,8 @@ Widget* FrameLoaderClientQt::createPlugin(const IntSize& pluginSize, Element* el
 
             widget->setStyleSheet(styleSheet);
         }
+#endif // QT_NO_STYLE_STYLESHEET
+    }
 
 #if QT_VERSION >= 0x040400
         if (!object) {
@@ -1088,7 +1091,6 @@ Widget* FrameLoaderClientQt::createPlugin(const IntSize& pluginSize, Element* el
             }
             // FIXME: make things work for widgetless plugins as well
             delete object;
-        }
     } else { // NPAPI Plugins
         PluginView* pluginView = PluginView::create(m_frame, pluginSize, element, url,
             paramNames, paramValues, mimeType, loadManually);
