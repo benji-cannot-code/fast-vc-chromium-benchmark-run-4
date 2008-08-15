cffeee0104792117a52ed5f04cf10276e99a05c8
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-15 18:10:57 +0000

    2008-08-15  David Boddie  <dboddie@trolltech.com>
            Reviewed by Simon.
    
            Doc: Added documentation for default property values.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -785,6 +785,8 @@ void QWebPagePrivate::leaveEvent(QEvent *ev)
     \brief the page's palette
 
     The background brush of the palette is used to draw the background of the main frame.
+
+    By default, this property contains the application's default palette.
 */
 void QWebPage::setPalette(const QPalette &pal)
 {
@@ -1407,6 +1409,9 @@ QSize QWebPage::viewportSize() const
 
     The size affects for example the visibility of scrollbars
     if the document is larger than the viewport.
+
+    By default, for a newly-created Web page, this property contains a size with
+    zero width and height.
 */
 void QWebPage::setViewportSize(const QSize &size) const
 {
@@ -1464,6 +1469,8 @@ bool QWebPage::acceptNavigationRequest(QWebFrame *frame, const QWebNetworkReques
 /*!
     \property QWebPage::selectedText
     \brief the text currently selected
+
+    By default, this property contains an empty string.
 
     \sa selectionChanged()
 */
@@ -1648,6 +1655,8 @@ QAction *QWebPage::action(WebAction action) const
 /*!
     \property QWebPage::modified
     \brief whether the page contains unsubmitted form data
+
+    By default, this property is false.
 */
 bool QWebPage::isModified() const
 {
