af5def4fcc5e0387c7faf085dd581420c5d72c90
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-17 21:18:42 +0000

    2008-08-17  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
            Reviewed by Dan Bernstein.
    
            Fix a typo in r35807 that is also causing build failures for
            non-AllInOne builds.
    
            * kjs/NumberConstructor.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35809 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/NumberConstructor.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/NumberConstructor.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace KJS {
 
-ASSERT_CLASS_FITS_IN_CELL(NativeErrorConstructor);
+ASSERT_CLASS_FITS_IN_CELL(NumberConstructor);
 
 const ClassInfo NumberConstructor::info = { "Function", &InternalFunction::info, 0, ExecState::numberTable };
 
