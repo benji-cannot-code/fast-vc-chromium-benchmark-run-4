44e18ecc6bc77c264df72f0f67ef06e6e84801c9
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-17 23:38:36 +0000

    <rdar://problem/6150322> In Gmail, a crash occurs at KJS::Machine::privateExecute() when applying list styling to text after a quote had been removed <https://bugs.webkit.org/show_bug.cgi?id=20386>
    Reviewed by Cameron Zwarich.
    
    This crash was caused by "depth()" incorrectly determining the scope depth
    of a 0 depth function without a full scope chain.  Because such a function
    would not have an activation the depth function would return the scope depth
    of the parent frame, thus triggering an incorrect unwind.  Any subsequent
    look up that walked the scope chain would result in incorrect behaviour,
    leading to a crash or incorrect variable resolution.  This can only actually
    happen in try...finally statements as that's the only path that can result in
    the need to unwind the scope chain, but not force the function to need a
    full scope chain.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35812 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/exception-try-finally-scope-error.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('This test makes sure stack unwinding works correctly when confronted with a 0-depth scope chain without an activation');
+var result;
+function runTest() {
+    var test = "outer scope";
+    with({test:"inner scope"})
+       (function () { try { throw ""; } finally { result = test; shouldBe("result", '"inner scope"'); return;}})()
+}
+runTest();
+
+var successfullyParsed = true;
