3aa9399a2d69c7cf63efddb999f6c6f7920ad661
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-18 12:36:07 +0000

    Return 0 instead of NULL for a non-pointer type.  Eliminates a warning in GCC.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@981 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/sgl/SkTypeface_fake.cpp
+++ skia/sgl/SkTypeface_fake.cpp
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 uint32_t SkTypeface::UniqueID(const SkTypeface* face)
 {
-    return NULL;
+    return 0;
 }
 
 void SkTypeface::serialize(SkWStream* stream) const {
