3260647a82842b841b8cce5b9603f8bb3b7cf507
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-18 20:21:57 +0000

    2008-08-18  Kevin McCullough  <kmccullough@apple.com>
            Reviewed by Tim.
    
            <rdar://problem/6150593> JSProfiler: Empty profiles disappear when there
            is another profile.
    
            * page/inspector/ProfilesPanel.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35821 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/ProfilesPanel.js
+++ third_party/WebKit/WebCore/page/inspector/ProfilesPanel.js
@@ -118,7 +118,7 @@ WebInspector.ProfilesPanel.prototype = {
         this.sidebarTree.removeChildren();
         this.profileViews.removeChildren();
 
-        this._shouldPopulateProfiles = true;
+        this.populateInterface();
     },
 
     handleKeyEvent: function(event)
