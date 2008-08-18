4f11d3edd7c5a9320fd040bd75debb75f0d19da2
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-18 20:40:04 +0000

    Make some net unittests compile and pass on linux.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@991 0039d316-1c4b-4281-b951-d872f2087c98

--- base/base_paths_linux.cc
+++ base/base_paths_linux.cc
@@ -32,12 +32,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <unistd.h>
 
+#include "base/file_util.h"
 #include "base/logging.h"
+#include "base/path_service.h"
 #include "base/sys_string_conversions.h"
 
 namespace base {
 
 bool PathProviderLinux(int key, std::wstring* result) {
+  std::wstring cur;
   switch (key) {
     case base::FILE_EXE:
     case base::FILE_MODULE: { // TODO(evanm): is this correct?
@@ -51,6 +54,14 @@ bool PathProviderLinux(int key, std::wstring* result) {
       *result = base::SysNativeMBToWide(bin_dir);
       return true;
     }
+    case base::DIR_SOURCE_ROOT:
+      // On linux, unit tests execute two levels deep from the source root.
+      // For example:  chrome/{Debug|Hammer}/net_unittest
+      PathService::Get(base::DIR_EXE, &cur);
+      file_util::UpOneDirectory(&cur);
+      file_util::UpOneDirectory(&cur);
+      *result = cur;
+      return true;
   }
   return false;
 }
