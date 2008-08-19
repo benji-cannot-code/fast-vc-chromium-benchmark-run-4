609e830602b4e55d33d12c2783b1267d62630f71
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-19 17:52:18 +0000

    Add mime_util and http_chunked_decoder to mac project.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1041 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mime_util.cc
+++ net/base/mime_util.cc
@@ -64,7 +64,7 @@ class MimeUtil : public PlatformMimeUtil {
                        const std::string &mime_type) const;
 
 private:
-  friend DefaultSingletonTraits<MimeUtil>;
+  friend struct DefaultSingletonTraits<MimeUtil>;
   MimeUtil() {
     InitializeMimeTypeMaps();
   }
