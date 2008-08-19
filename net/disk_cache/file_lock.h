5ea8583dbaeeff10fb1f879c8c482989ed86288b
avi@google.com <avi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-19 19:47:42 +0000

    Fix up warnings in net/
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1048 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/file_lock.h
+++ net/disk_cache/file_lock.h
@@ -55,7 +55,7 @@ namespace disk_cache {
 class FileLock {
  public:
   explicit FileLock(BlockFileHeader* header);
-  ~FileLock() {
+  virtual ~FileLock() {
     Unlock();
   }
   // Virtual to make sure the compiler never inlines the calls.
