34ccb70a06f0393e60efb28d23d791e4e44808fa
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-19 17:31:19 +0000

    Make the otr avatar icon not show in detached OTR popup windows.
    B=1031854
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1038 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/opaque_frame.cc
+++ chrome/browser/views/frame/opaque_frame.cc
@@ -40,8 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // OpaqueFrame, public:
 
 OpaqueFrame::OpaqueFrame(BrowserView2* browser_view)
-    : CustomFrameWindow(browser_view, new OpaqueNonClientView(
-          this, browser_view, browser_view->IsOffTheRecord())),
+    : CustomFrameWindow(browser_view, new OpaqueNonClientView(this,
+                                                              browser_view)),
       browser_view_(browser_view) {
   browser_view_->set_frame(this);
 }
