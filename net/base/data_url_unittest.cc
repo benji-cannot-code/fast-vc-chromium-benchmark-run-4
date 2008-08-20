d97afc3cfce80a42b49e9545c2b2b76466634884
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-20 00:15:50 +0000

    Add more files to the net module compile list.  Had to make some small changes to how static struct arrays were declared so arraysize() would work with gcc.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1074 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/data_url_unittest.cc
+++ net/base/data_url_unittest.cc
@@ -35,19 +35,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+  struct ParseTestData {
+    const char* url;
+    bool is_valid;
+    const char* mime_type;
+    const char* charset;
+    const char* data;
+  };
+
 class DataURLTest : public testing::Test {
 };
 
 }
 
 TEST(DataURLTest, Parse) {
-  const struct {
-    const char* url;
-    bool is_valid;
-    const char* mime_type;
-    const char* charset;
-    const char* data;
-  } tests[] = {
+  const ParseTestData tests[] = {
     { "data:",
        false,
        "",
