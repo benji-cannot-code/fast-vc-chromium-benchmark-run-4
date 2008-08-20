a6cd11bc670ffd290725440cf9e82a3d06489878
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-20 04:06:48 +0000

    Portability changes to http_response_headers. The parsing changes will also behave differently for these cases (added as unit-tests):
    "Content-Length: +10\n"
      Before: 10
      After: -1
    
    "Content-Length: 40000000000000000000\n"
      Before: 9223372036854775807
      After: -1
    
    "Content-Length: \v10\n"
      Before: 10
      After: -1
    
    "Content-Length: \f10\n"
      Before: 10
      After: -1
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1083 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_headers.cc
+++ net/http/http_response_headers.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_response_headers.h"
 
 #include <algorithm>
-#include <hash_map>
 
 #include "base/logging.h"
 #include "base/pickle.h"
@@ -266,7 +265,7 @@ void HttpResponseHeaders::GetNormalizedHeaders(string* output) const {
   // be a web app, we cannot be certain of the semantics of commas despite the
   // fact that RFC 2616 says that they should be regarded as value separators.
   //
-  typedef stdext::hash_map<string, size_t> HeadersMap;
+  typedef base::hash_map<string, size_t> HeadersMap;
   HeadersMap headers_map;
   HeadersMap::iterator iter = headers_map.end();
 
@@ -910,6 +909,8 @@ bool HttpResponseHeaders::IsKeepAlive() const {
   return keep_alive;
 }
 
+// From RFC 2616:
+// Content-Length = "Content-Length" ":" 1*DIGIT
 int64 HttpResponseHeaders::GetContentLength() const {
   void* iter = NULL;
   string content_length_val;
@@ -919,16 +920,12 @@ int64 HttpResponseHeaders::GetContentLength() const {
   if (content_length_val.empty())
     return -1;
 
-  // NOTE: We do not use StringToInt64 here since we want to know if
-  // parsing failed.
-
-  char* end;
-  int64 result = _strtoi64(content_length_val.c_str(), &end, 10);
-
-  if (result < 0)
+  if (content_length_val[0] == '+')
     return -1;
 
-  if (end != content_length_val.c_str() + content_length_val.length())
+  int64 result;
+  bool ok = StringToInt64(content_length_val, &result);
+  if (!ok || result < 0) 
     return -1;
 
   return result;
