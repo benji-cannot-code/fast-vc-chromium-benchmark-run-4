46764193e856e1d7495df1d6744a1c16598c9324
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-20 22:54:52 +0000

    TrackedObjects assumes you can use a "TLS slot" of -1 to indicate uninitialized.  This isn't true for the pthread_key_t type, which is unsigned on Linux and reportedly a struct on Macs.  This change modifies the Slot type to be a struct containing an "initialized" flag.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1122 0039d316-1c4b-4281-b951-d872f2087c98

--- base/tracked_objects.cc
+++ base/tracked_objects.cc
@@ -36,9 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace tracked_objects {
 
-// a TLS index to the TrackRegistry for the current thread.
+// A TLS slot to the TrackRegistry for the current thread.
 // static
-TLSSlot ThreadData::tls_index_ = -1;
+TLSSlot ThreadData::tls_index_(base::LINKER_INITIALIZED);
 
 //------------------------------------------------------------------------------
 // Death data tallies durations when a death takes place.
@@ -105,15 +105,14 @@ Lock ThreadData::list_lock_;
 // static
 ThreadData::Status ThreadData::status_ = ThreadData::UNINITIALIZED;
 
-ThreadData::ThreadData() :  message_loop_(MessageLoop::current()) {}
+ThreadData::ThreadData() : message_loop_(MessageLoop::current()) {}
 
 // static
 ThreadData* ThreadData::current() {
-  if (-1 == tls_index_)
-    return NULL;  // not yet initialized.
+  if (!tls_index_.initialized())
+    return NULL;
 
-  ThreadData* registry =
-      static_cast<ThreadData*>(ThreadLocalStorage::Get(tls_index_));
+  ThreadData* registry = static_cast<ThreadData*>(tls_index_.Get());
   if (!registry) {
     // We have to create a new registry for ThreadData.
     bool too_late_to_create = false;
@@ -133,7 +132,7 @@ ThreadData* ThreadData::current() {
       delete registry;
       registry = NULL;
     } else {
-      ThreadLocalStorage::Set(tls_index_, registry);
+      tls_index_.Set(registry);
     }
   }
   return registry;
@@ -345,11 +344,9 @@ bool ThreadData::StartTracking(bool status) {
     status_ = SHUTDOWN;
     return true;
   }
-  TLSSlot tls_index = ThreadLocalStorage::Alloc();
   AutoLock lock(list_lock_);
   DCHECK(status_ == UNINITIALIZED);
-  tls_index_ = tls_index;
-  CHECK(-1 != tls_index_);
+  CHECK(tls_index_.Initialize(NULL));
   status_ = ACTIVE;
   return true;
 }
@@ -402,9 +399,9 @@ void ThreadData::ShutdownSingleThreadedCleanup() {
     delete next_thread_data;  // Includes all Death Records.
   }
 
-  CHECK(-1 != tls_index_);
-  ThreadLocalStorage::Free(tls_index_);
-  tls_index_ = -1;
+  CHECK(tls_index_.initialized());
+  tls_index_.Free();
+  DCHECK(!tls_index_.initialized());
   status_ = UNINITIALIZED;
 }
 
--- chrome/common/notification_service.h
+++ chrome/common/notification_service.h
@@ -37,8 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 
-#include "base/thread_local_storage.h"
 #include "base/observer_list.h"
+#include "base/thread_local_storage.h"
 #include "base/values.h"
 #include "chrome/common/notification_details.h"
 #include "chrome/common/notification_source.h"
@@ -51,8 +51,7 @@ class NotificationService {
   // Returns the NotificationService object for the current thread, or NULL if
   // none.
   static NotificationService* current() {
-    return static_cast<NotificationService *>(
-        ThreadLocalStorage::Get(tls_index_));
+    return static_cast<NotificationService *>(tls_index_.Get());
   }
 
   // Normally instantiated when the thread is created.  Not all threads have
@@ -120,7 +119,7 @@ class NotificationService {
 
   // The thread local storage index, used for getting the current thread's
   // instance.
-  static int tls_index_;
+  static TLSSlot tls_index_;
 
 #ifndef NDEBUG
   // Used to check to see that AddObserver and RemoveObserver calls are
