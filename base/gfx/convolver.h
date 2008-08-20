2afc5bb6d0bbf13ab046ac8232fcc5b7460d6995
awalker@google.com <awalker@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-20 19:34:30 +0000

    Merges in updated versions of Mac graphics platform classes, adds generic typedefs for use by higher level classes that just need to hold a reference (though does not yet refactor those classes).
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1099 0039d316-1c4b-4281-b951-d872f2087c98

--- base/gfx/convolver.h
+++ base/gfx/convolver.h
@@ -35,6 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 
+// avoid confusion with Mac OS X's math library (Carbon)
+#if defined(OS_MACOSX)
+#undef FloatToFixed
+#endif
+
 namespace gfx {
 
 // Represents a filter in one dimension. Each output pixel has one entry in this
