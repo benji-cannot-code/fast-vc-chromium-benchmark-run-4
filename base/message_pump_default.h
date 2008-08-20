4b32601c43d6e81dcc3d23842e55e878beaffc75
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-20 21:29:17 +0000

    fix hammer build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1108 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_pump_default.h
+++ base/message_pump_default.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_MESSAGE_PUMP_DEFAULT_H_
 
 #include "base/message_pump.h"
+#include "base/time.h"
 #include "base/waitable_event.h"
 
 namespace base {
