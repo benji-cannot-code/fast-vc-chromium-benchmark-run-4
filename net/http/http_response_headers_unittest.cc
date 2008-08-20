9ffe628b8254a323e71d5db7a0cd3d5ed25a9760
avi@google.com <avi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-20 23:11:34 +0000

    More of net compiling on the Mac.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1125 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_headers_unittest.cc
+++ net/http/http_response_headers_unittest.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/pickle.h"
 #include "base/time.h"
-#include "net/base/net_util.h"
 #include "net/http/http_response_headers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -364,7 +363,7 @@ TEST(HttpResponseHeadersTest, Persist) {
     },
   };
 
-  for (size_t i = 0; i < arraysize(tests); ++i) {
+  for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
     std::string headers = tests[i].raw_headers;
     HeadersToRaw(&headers);
     scoped_refptr<HttpResponseHeaders> parsed1 =
@@ -678,7 +677,7 @@ TEST(HttpResponseHeadersTest, RequiresValidation) {
   Time::FromString(L"Wed, 28 Nov 2007 00:40:12 GMT", &response_time);
   Time::FromString(L"Wed, 28 Nov 2007 00:45:20 GMT", &current_time);
 
-  for (size_t i = 0; i < arraysize(tests); ++i) {
+  for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
     string headers(tests[i].headers);
     HeadersToRaw(&headers);
     scoped_refptr<HttpResponseHeaders> parsed = new HttpResponseHeaders(headers);
@@ -730,7 +729,7 @@ TEST(HttpResponseHeadersTest, Update) {
     },
   };
 
-  for (size_t i = 0; i < arraysize(tests); ++i) {
+  for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
     string orig_headers(tests[i].orig_headers);
     HeadersToRaw(&orig_headers);
     scoped_refptr<HttpResponseHeaders> parsed =
@@ -776,7 +775,7 @@ TEST(HttpResponseHeadersTest, EnumerateHeaderLines) {
       "Foo: 1, 2, 3\n"
     },
   };
-  for (size_t i = 0; i < arraysize(tests); ++i) {
+  for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
     string headers(tests[i].headers);
     HeadersToRaw(&headers);
     scoped_refptr<HttpResponseHeaders> parsed =
@@ -860,7 +859,7 @@ TEST(HttpResponseHeadersTest, IsRedirect) {
       true
     },
   };
-  for (size_t i = 0; i < arraysize(tests); ++i) {
+  for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
     string headers(tests[i].headers);
     HeadersToRaw(&headers);
     scoped_refptr<HttpResponseHeaders> parsed =
@@ -946,7 +945,7 @@ TEST(HttpResponseHeadersTest, GetContentLength) {
       -1
     },
   };
-  for (size_t i = 0; i < arraysize(tests); ++i) {
+  for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
     string headers(tests[i].headers);
     HeadersToRaw(&headers);
     scoped_refptr<HttpResponseHeaders> parsed =
@@ -996,7 +995,7 @@ TEST(HttpResponseHeadersTest, IsKeepAlive) {
       false
     },
   };
-  for (size_t i = 0; i < arraysize(tests); ++i) {
+  for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
     string headers(tests[i].headers);
     HeadersToRaw(&headers);
     scoped_refptr<HttpResponseHeaders> parsed =
