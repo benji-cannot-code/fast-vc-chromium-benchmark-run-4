52c5413b2adc780993f7ecbea57362cbdcb97734
jnd@google.com <jnd@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-21 02:58:09 +0000

    As Brett pointed it out, We normally use 8-bit for encoding names since they're always ASCII. Plus, wstrings will also be 32-bit characters on Linux and Mac, which is expensive. So I make this CL by using std::string instead of std::wstring every time you have an encoding name.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/navigation_controller_unittest.cc
+++ chrome/browser/navigation_controller_unittest.cc
@@ -158,7 +158,7 @@ class NavigationControllerTest : public testing::Test,
                               const GURL&,
                               WindowOpenDisposition,
                               PageTransition::Type,
-                              const std::wstring& override_encoding) {}
+                              const std::string& override_encoding) {}
   virtual void NavigationStateChanged(const TabContents*,
                                       unsigned flags) {}
   virtual void ReplaceContents(TabContents* source,
--- chrome/browser/views/constrained_window_impl.h
+++ chrome/browser/views/constrained_window_impl.h
@@ -82,7 +82,7 @@ class ConstrainedWindowImpl : public ConstrainedWindow,
                               const GURL& url,
                               WindowOpenDisposition disposition,
                               PageTransition::Type transition,
-                              const std::wstring& override_encoding);
+                              const std::string& override_encoding);
 
   // Overridden from TabContentsDelegate:
   virtual void NavigationStateChanged(const TabContents* source,
