4c3646f07c64dbf38d88afbb795266c20fac39ae
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-21 20:12:47 +0000

    Make the Inspector's Metrics sidebar pane editable.
            https://bugs.webkit.org/show_bug.cgi?id=17218
            rdar://problem/5732818
    
            Reviewed by Kevin McCullough.
    
            * page/inspector/ElementsPanel.js:
            (WebInspector.ElementsPanel): Add an event listener for
            the "metrics edited" event, so the Styles pane is updated.
            * page/inspector/MetricsSidebarPane.js:
            (WebInspector.MetricsSidebarPane.prototype.update): Remember the node
            so future updates work. Add a double click event listener for the
            metric values to start editing.
            (WebInspector.MetricsSidebarPane.prototype.startEditing):
            Call WebInspector.startEditing with some context.
            (WebInspector.MetricsSidebarPane.prototype.editingCancelled):
            (WebInspector.MetricsSidebarPane.prototype.editingCommitted):
            Set the user input on the elements inline style. Fire the
            "metrics edited" event.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35876 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/ElementsPanel.js
+++ third_party/WebKit/WebCore/page/inspector/ElementsPanel.js
@@ -62,6 +62,8 @@ WebInspector.ElementsPanel = function()
 
     this.sidebarPanes.styles.expanded = true;
 
+    this.sidebarPanes.metrics.addEventListener("metrics edited", this._metricsPaneEdited, this);
+
     this.sidebarElement = document.createElement("div");
     this.sidebarElement.id = "elements-sidebar";
 
@@ -410,6 +412,12 @@ WebInspector.ElementsPanel.prototype = {
             InspectorController.highlightDOMNode(this._hoveredDOMNode);
         else
             InspectorController.hideDOMNodeHighlight();
+    },
+
+    _metricsPaneEdited: function()
+    {
+        this.sidebarPanes.styles.needsUpdate = true;
+        this.updateStyles(true);
     },
 
     _focusedNodeChanged: function(forceUpdate)
