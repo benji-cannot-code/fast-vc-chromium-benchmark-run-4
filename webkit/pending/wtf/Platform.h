1570cb64338b26d0c389cc85d4953c3c1e487bdb
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-22 19:36:38 +0000

    Introduce PLATFORM(CHROME) and turn on Skia for mac if we're building that platform.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1235 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/pending/wtf/Platform.h
+++ webkit/pending/wtf/Platform.h
@@ -80,6 +80,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* Operating environments */
 
+/* define a platform for chrome, requires |BUILDING_CHROME__| to be defined */
+#if defined(BUILDING_CHROME__)
+#define WTF_PLATFORM_CHROME 1
+#endif
+
 /* PLATFORM(QT) */
 /* PLATFORM(GTK) */
 /* PLATFORM(MAC) */
@@ -109,6 +114,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if PLATFORM(MAC)
 #define WTF_PLATFORM_CG 1
 #define WTF_PLATFORM_CI 1
+#if PLATFORM(CHROME)
+#define WTF_PLATFORM_SKIA 1
+#endif
 #elif !PLATFORM(QT) && !PLATFORM(WX)
 #define WTF_PLATFORM_CAIRO 1
 #endif
