ab12c5b21e885eaf061bf7a3d53928bc13f32321
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-23 07:16:41 +0000

    WebCore:
            Reviewed by Dave Hyatt.
    
            - fix <rdar://problem/6065143> Reflowed image in first line of table doesn't affect baseline
    
            Test: fast/table/vertical-align-baseline.html
    
            * rendering/RenderTable.cpp:
            (WebCore::RenderTable::getBaselineOfFirstLineBox): Added. Returns the
            baseline of the first table row, if there is one, -1 otherwise.
            * rendering/RenderTable.h:
            * rendering/RenderTableCell.cpp:
            (WebCore::RenderTableCell::baselinePosition): Changed to follow the
            CSS2.1 definition of the baseline of a table cell.
            * rendering/RenderTableSection.cpp:
            (WebCore::RenderTableSection::getBaselineOfFirstLineBox): Added. Returns
            the baseline of the first row in the section.
            * rendering/RenderTableSection.h:
    
    LayoutTests:
    
            Reviewed by Dave Hyatt.
    
            - test and updated results for <rdar://problem/6065143> Reflowed image in first line of table doesn't affect baseline
    
            * fast/table/vertical-align-baseline.html: Added.
            * platform/mac/fast/table/form-with-table-style-expected.checksum:
            * platform/mac/fast/table/form-with-table-style-expected.png:
            * platform/mac/fast/table/form-with-table-style-expected.txt:
            * platform/mac/fast/table/vertical-align-baseline-expected.checksum: Added.
            * platform/mac/fast/table/vertical-align-baseline-expected.png: Added.
            * platform/mac/fast/table/vertical-align-baseline-expected.txt: Added.
            * platform/mac/tables/mozilla/bugs/bug3037-1-expected.txt:
            * platform/mac/tables/mozilla_expected_failures/bugs/bug2479-5-expected.checksum:
            * platform/mac/tables/mozilla_expected_failures/bugs/bug2479-5-expected.png:
            * platform/mac/tables/mozilla_expected_failures/bugs/bug2479-5-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35899 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTable.h
+++ third_party/WebKit/WebCore/rendering/RenderTable.h
@@ -103,6 +103,8 @@ public:
     virtual void layout();
     virtual void calcPrefWidths();
 
+    virtual int getBaselineOfFirstLineBox() const;
+
     virtual RenderBlock* firstLineBlock() const;
     virtual void updateFirstLetter();
     
