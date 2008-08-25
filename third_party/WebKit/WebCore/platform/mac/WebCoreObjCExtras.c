f663e2f01164f42642a68b8e05d84eb55e98ae6e
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-25 20:56:21 +0000

    2008-08-25  Anders Carlsson  <andersca@apple.com>
            Reviewed by Mark.
    
            <rdar://problem/6149787> crash @ com.apple.JavaScriptCore: WTF::callOnMainThread + 37.
            
            Initialize threading in the call to WebCoreObjCFinalizeOnMainThread. We currently assume 
            (safely) that any class that needs finalization on the main thread will also need to be deallocated
            on the main thread, and calling initializeThreading from WebCoreObjCFinalizeOnMainThread instead of
            calling it from WebCoreObjCScheduleDeallocateOnMainThread seems safer.
    
            * platform/mac/WebCoreObjCExtras.c:
            (WebCoreObjCFinalizeOnMainThread):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35922 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/WebCoreObjCExtras.c
+++ third_party/WebKit/WebCore/platform/mac/WebCoreObjCExtras.c
@@ -34,12 +34,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <objc/objc-runtime.h>
 #include <wtf/Assertions.h>
 #include <wtf/MainThread.h>
+#include <wtf/Threading.h>
 
 void WebCoreObjCFinalizeOnMainThread(Class cls)
 {
 #if !defined(BUILDING_ON_TIGER) && !defined(DONT_FINALIZE_ON_MAIN_THREAD)
     objc_finalizeOnMainThread(cls);
 #endif
+    
+    // The reason we call initializeThreading here is that we'd like to have 
+    // threading initialized early, otherwise WebCoreObjCScheduleDeallocateOnMainThread
+    // will crash
+    WTF::initializeThreading();
 }
 
 #ifdef BUILDING_ON_TIGER
