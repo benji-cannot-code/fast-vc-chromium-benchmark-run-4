447bc0074d295d6517b322439f7e5f08c4be6c3f
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-25 01:27:53 +0000

    2008-08-24  Beth Dakin  <bdakin@apple.com>
            Reviewed by John Sullivan.
    
            Provide the excludeFromTextSearch SPI on Windows as well.
    
            * Interfaces/IWebFrame.idl:
            * WebFrame.cpp:
            (WebFrame::setExcludeFromTextSearch):
            * WebFrame.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35909 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebFrame.h
+++ third_party/WebKit/WebKit/win/WebFrame.h
@@ -214,6 +214,9 @@ public:
     virtual HRESULT STDMETHODCALLTYPE setIsDisconnected(
         /* [in] */ BOOL flag);
 
+    virtual HRESULT STDMETHODCALLTYPE setExcludeFromTextSearch(
+        /* [in] */ BOOL flag);
+
     virtual HRESULT STDMETHODCALLTYPE paintDocumentRectToContext(
         /* [in] */ RECT rect,
         /* [in] */ OLE_HANDLE deviceContext);
