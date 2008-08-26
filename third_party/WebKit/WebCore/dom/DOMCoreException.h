d97fe1d0a9c5c2b5e3099a0bd3e1ede08bb7fa9e
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-08-26 21:21:26 +0000

    2008-08-26  Sam Weinig  <sam@webkit.org>
            Reviewed by Beth Dakin.
    
            Fix typo.
    
            * dom/DOMCoreException.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@35936 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/DOMCoreException.h
+++ third_party/WebKit/WebCore/dom/DOMCoreException.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #ifndef DOMCoreException_h
-#define DOMCodeException_h
+#define DOMCoreException_h
 
 #include "ExceptionBase.h"
 
