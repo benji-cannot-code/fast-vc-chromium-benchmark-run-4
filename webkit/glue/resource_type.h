30760d3ad54b9297d854abc9ade462450d68ca84
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-27 18:40:06 +0000

    include basictypes for mac/linux
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1453 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/resource_type.h
+++ webkit/glue/resource_type.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_GLUE_RESOURCE_TYPE_H__
 #define WEBKIT_GLUE_RESOURCE_TYPE_H__
 
+#include "base/basictypes.h"
 
 class ResourceType {
  public:
