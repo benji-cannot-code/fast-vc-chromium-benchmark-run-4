e17b7afc9ebaa9819396174020455ea724f570a2
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-27 13:22:06 +0000

    make reference parameters const.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1436 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webtextinput_impl.h
+++ webkit/glue/webtextinput_impl.h
@@ -21,9 +21,9 @@ class WebTextInputImpl : public WebTextInput {
   virtual ~WebTextInputImpl();
 
   // WebTextInput methods
-  virtual void InsertText(std::string& text);
-  virtual void DoCommand(std::string& command);
-  virtual void SetMarkedText(std::string& text,
+  virtual void InsertText(const std::string& text);
+  virtual void DoCommand(const std::string& command);
+  virtual void SetMarkedText(const std::string& text,
                              int32_t location, int32_t length);
   virtual void UnMarkText();
   virtual bool HasMarkedText();
@@ -40,7 +40,7 @@ class WebTextInputImpl : public WebTextInput {
   virtual void FirstRectForCharacterRange(int32_t location,
                                           int32_t length);
   virtual void CharacterIndexForPoint(double x, double y);
-  virtual void MakeAttributedString(std::string& str);
+  virtual void MakeAttributedString(const std::string& str);
 
  private:
   WebCore::Frame* GetFrame();
