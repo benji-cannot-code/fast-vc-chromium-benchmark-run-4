a828d22161fb0003931b0e9a6519c21ee0415cd5
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-27 05:29:40 +0000

    rollback r1427 to attempt to fix test failures
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1429 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop.cc
+++ base/message_loop.cc
@@ -342,6 +342,7 @@ void MessageLoop::ReloadWorkQueue() {
 }
 
 void MessageLoop::DeletePendingTasks() {
+  /* Comment this out as it's causing crashes.
   while (!work_queue_.Empty()) {
     Task* task = work_queue_.Pop();
     if (task->is_owned_by_message_loop())
@@ -353,6 +354,7 @@ void MessageLoop::DeletePendingTasks() {
     if (task->is_owned_by_message_loop())
       delete task;
   }
+  */
 }
 
 void MessageLoop::DidChangeNextTimerExpiry() {
