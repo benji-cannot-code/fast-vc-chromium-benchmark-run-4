9cb07692847c7830773b1e4183bbf1f10877ffad
mmentovai@google.com <mmentovai@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-27 20:37:52 +0000

    Some more passing tests in base for Linux
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1460 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_posix.cc
+++ base/file_util_posix.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <libgen.h>
 #include <sys/errno.h>
 #include <sys/stat.h>
-#include <sys/syslimits.h>
 #include <time.h>
 
 #include <fstream>
