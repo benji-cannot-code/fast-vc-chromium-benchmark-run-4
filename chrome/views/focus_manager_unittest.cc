88692b292fcb8cf9162b30af23d01ae941112598
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-27 06:33:18 +0000

    Explicitly call RunAllPending in the Teardown phase a few more unit tests to prevent Purify from complaining about these pending tasks as MLKs.
    TBR=erikkay
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1431 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/focus_manager_unittest.cc
+++ chrome/views/focus_manager_unittest.cc
@@ -522,6 +522,9 @@ void FocusManagerTest::SetUp() {
 
 void FocusManagerTest::TearDown() {
   test_window_->CloseNow();
+
+  // Flush the message loop to make Purify happy.
+  message_loop_.RunAllPending();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
