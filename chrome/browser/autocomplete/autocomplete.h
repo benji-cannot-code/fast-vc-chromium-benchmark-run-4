f96020c92449df64c8f6cca5131675f720a6eebf
nsylvain@google.com <nsylvain@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-27 14:24:43 +0000

    Fix header guard
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1440 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete.h
+++ chrome/browser/autocomplete/autocomplete.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_H__
-#define CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_H__
+#ifndef CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_H_
+#define CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_H_
 
 #include <map>
 #include <string>
@@ -777,5 +777,5 @@ struct AutocompleteLog {
   const AutocompleteResult& result;
 };
 
-#endif  // CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_H__
+#endif  // CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_H_
 
