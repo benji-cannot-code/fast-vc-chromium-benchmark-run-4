8402df590c4b448eb9d4dad17186b7349c552676
nsylvain@google.com <nsylvain@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-27 15:46:17 +0000

    Fix header guard
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1444 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_popup.h
+++ chrome/browser/autocomplete/autocomplete_popup.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_POPUP_H__
-#define CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_POPUP_H__
+#ifndef CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_POPUP_H_
+#define CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_POPUP_H_
 
 #include <atlbase.h>
 #include <atlapp.h>
@@ -399,5 +399,5 @@ class AutocompletePopup
   scoped_ptr<MirroringContext> mirroring_context_;
 };
 
-#endif  // CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_POPUP_H__
+#endif  // CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_POPUP_H_
 
