ee4e29dbb6d175c4eb7f4fff7ddab4538d52927b
mmentovai@google.com <mmentovai@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-28 17:40:59 +0000

    WorkerPool implementation for Mac, using NSOperationQueue
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1493 0039d316-1c4b-4281-b951-d872f2087c98

--- base/worker_pool_unittest.cc
+++ base/worker_pool_unittest.cc
@@ -3,12 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/platform_test.h"
 #include "base/task.h"
 #include "base/waitable_event.h"
 #include "base/worker_pool.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::WaitableEvent;
+
+typedef PlatformTest WorkerPoolTest;
 
 namespace {
 
@@ -25,7 +28,7 @@ class PostTaskTestTask : public Task {
   WaitableEvent* event_;
 };
 
-TEST(WorkerPoolTest, PostTask) {
+TEST_F(WorkerPoolTest, PostTask) {
   WaitableEvent test_event(false, false);
   WaitableEvent long_test_event(false, false);
   bool signaled;
