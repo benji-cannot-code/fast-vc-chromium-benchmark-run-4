f5e5277925b55cb89c11599fd0c8011087d87192
maruel@google.com <maruel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-29 19:21:29 +0000

    Fix two tests that expected en-US locale and were failing otherwise.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1543 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/accessibility/accessibility_tests.cc
+++ chrome/test/accessibility/accessibility_tests.cc
@@ -5,14 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <Objbase.h>
 #include <Oleacc.h>
+
+#include "base/command_line.h"
+#include "base/file_util.h"
+#include "base/win_util.h"
+#include "chrome/app/chrome_dll_resource.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/test/accessibility/accessibility_util.h"
 #include "chrome/test/ui/ui_test.h"
 #include "chrome/test/automation/browser_proxy.h"
 #include "chrome/test/automation/window_proxy.h"
 #include "chrome/test/automation/tab_proxy.h"
-#include "chrome/app/chrome_dll_resource.h"
-#include "base/win_util.h"
-#include "base/file_util.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_util.h"
 
@@ -26,6 +29,9 @@ class AccessibilityTest : public UITest {
   AccessibilityTest() {
     show_window_ = true;
     CoInitialize(NULL);
+    CommandLine::AppendSwitchWithValue(&launch_arguments_,
+                                       switches::kLang,
+                                       L"en-us");
   }
   ~AccessibilityTest() {
     CoUninitialize();
