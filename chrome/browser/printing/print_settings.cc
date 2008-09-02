1c0f819536993c534949c59d8e884692742ffb95
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-02 13:11:46 +0000

    Add a LinkerInitialized constructor to AtomicSequenceNumber.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1636 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_settings.cc
+++ chrome/browser/printing/print_settings.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace printing {
 
 // Global SequenceNumber used for generating unique cookie values.
-static base::AtomicSequenceNumber cookie_seq;
+static base::AtomicSequenceNumber cookie_seq(base::LINKER_INITIALIZED);
 
 PrintSettings::PrintSettings()
     : min_shrink(1.25),
