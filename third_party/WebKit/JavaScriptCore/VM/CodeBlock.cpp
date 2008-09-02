e0b62f743d5221941ff7a0dbb22f0a85e7ffe202
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-02 15:15:21 +0000

    Windows build fixes
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj: Add
            StructureID.{cpp,h} to the project. Also let VS reorder this file.
            * VM/CodeBlock.cpp: Include StringExtras so that snprintf will be
            defined on Windows.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36024 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/CodeBlock.cpp
+++ third_party/WebKit/JavaScriptCore/VM/CodeBlock.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Machine.h"
 #include "debugger.h"
 #include <stdio.h>
+#include <wtf/StringExtras.h>
 
 namespace KJS {
 
