47e745aa8ac0c4c686e1d32dc4d684f61f44d6ed
maruel@google.com <maruel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-02 13:56:12 +0000

    Unit tests can't access the outside world.
    TBR=wtc
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1638 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/ssl_client_socket_unittest.cc
+++ net/base/ssl_client_socket_unittest.cc
@@ -22,7 +22,8 @@ class SSLClientSocketTest : public testing::Test {
 
 //-----------------------------------------------------------------------------
 
-TEST_F(SSLClientSocketTest, Connect) {
+// bug 1354783
+TEST_F(SSLClientSocketTest, DISABLED_Connect) {
   net::AddressList addr;
   net::HostResolver resolver;
   TestCompletionCallback callback;
@@ -49,7 +50,8 @@ TEST_F(SSLClientSocketTest, Connect) {
   EXPECT_FALSE(sock.IsConnected());
 }
 
-TEST_F(SSLClientSocketTest, Read) {
+// bug 1354783
+TEST_F(SSLClientSocketTest, DISABLED_Read) {
   net::AddressList addr;
   net::HostResolver resolver;
   TestCompletionCallback callback;
@@ -94,7 +96,8 @@ TEST_F(SSLClientSocketTest, Read) {
   }
 }
 
-TEST_F(SSLClientSocketTest, Read_SmallChunks) {
+// bug 1354783
+TEST_F(SSLClientSocketTest, DISABLED_Read_SmallChunks) {
   net::AddressList addr;
   net::HostResolver resolver;
   TestCompletionCallback callback;
@@ -136,7 +139,8 @@ TEST_F(SSLClientSocketTest, Read_SmallChunks) {
   }
 }
 
-TEST_F(SSLClientSocketTest, Read_Interrupted) {
+// bug 1354783
+TEST_F(SSLClientSocketTest, DISABLED_Read_Interrupted) {
   net::AddressList addr;
   net::HostResolver resolver;
   TestCompletionCallback callback;
