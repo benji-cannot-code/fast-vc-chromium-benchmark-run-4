11255059a994301d5646c5010b5cbbbdffcdff9a
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-03 00:25:12 +0000

    GCC 4.3 build fixes.
    BUG=241
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1663 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/pending/Page.h
+++ webkit/pending/Page.h
@@ -34,12 +34,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef struct HINSTANCE__* HINSTANCE;
 #endif
 
-typedef enum TextCaseSensitivity {
+enum TextCaseSensitivity {
     TextCaseSensitive,
     TextCaseInsensitive
 };
 
-typedef enum FindDirection {
+enum FindDirection {
     FindDirectionForward,
     FindDirectionBackward
 };
