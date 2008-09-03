71237fd61d0744ac89d641642aa04a54f7352829
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-03 17:13:23 +0000

    Fix a typo that broke the posix build.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1681 0039d316-1c4b-4281-b951-d872f2087c98

--- base/thread_local_posix.cc
+++ base/thread_local_posix.cc
@@ -25,7 +25,7 @@ void ThreadLocalPlatform::FreeSlot(SlotType& slot) {
 
 // static
 void* ThreadLocalPlatform::GetValueFromSlot(SlotType& slot) {
-  return pthread_getspecific(slot_);
+  return pthread_getspecific(slot);
 }
 
 // static
