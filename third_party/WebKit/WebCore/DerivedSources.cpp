eed305684f331f8135594a4e74657300a9b42848
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-03 20:30:59 +0000

    Windows build fix
            * DerivedSources.cpp: Add JSTextMetrics.cpp to fix the build.
            * WebCore.vcproj/WebCore.vcproj: Add JSTextMetrics.h for
            convenience/consistency.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36065 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/DerivedSources.cpp
+++ third_party/WebKit/WebCore/DerivedSources.cpp
@@ -306,6 +306,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSSVGZoomEvent.cpp"
 #include "JSText.cpp"
 #include "JSTextEvent.cpp"
+#include "JSTextMetrics.cpp"
 #include "JSTimeRanges.cpp"
 #include "JSTreeWalker.cpp"
 #include "JSUIEvent.cpp"
