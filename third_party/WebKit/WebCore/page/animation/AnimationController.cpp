9d3634a82586b268703a202ce117b22c756a067b
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-03 07:05:48 +0000

    2008-09-03  Dirk Schulze  <vbs85@gmx.de>
            Reviewed and tweaked by Mark Rowe.
    
            WebKitGtk build fix.
    
            * GNUmakefile.am:
            * page/animation/AnimationController.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36052 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/AnimationController.cpp
+++ third_party/WebKit/WebCore/page/animation/AnimationController.cpp
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "AnimationController.h"
-#include "CompositeAnimation.h"
 
+#include "CompositeAnimation.h"
 #include "Frame.h"
 #include "Timer.h"
 
