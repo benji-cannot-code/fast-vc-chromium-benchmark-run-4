32714fc5aaf2994c42bb83f5c9c7f098983f65d5
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-03 16:47:37 +0000

    Add ThreadLocalPointer and ThreadLocalBoolean abstractions, that will deprecate the old ThreadLocalStorage / TLSSlot APIs.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1678 0039d316-1c4b-4281-b951-d872f2087c98

--- base/thread.h
+++ base/thread.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop.h"
 #include "base/platform_thread.h"
-#include "base/thread_local_storage.h"
 
 namespace base {
 
