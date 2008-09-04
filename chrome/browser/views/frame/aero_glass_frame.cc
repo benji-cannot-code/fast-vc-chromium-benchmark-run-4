dd09ff658e19c1f6d714cfe200a3fa0a4001780e
maruel@google.com <maruel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-04 01:03:30 +0000

    Fix the include order in aero_glass_frame.cc. This was causing issues with WPO enabled. Review URL: http://codereview.chromium.org/430
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1703 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/aero_glass_frame.cc
+++ chrome/browser/views/frame/aero_glass_frame.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <dwmapi.h>
-
 #include "chrome/browser/views/frame/aero_glass_frame.h"
+
+#include <dwmapi.h>
 
 #include "chrome/app/theme/theme_resources.h"
 #include "chrome/browser/frame_util.h"
