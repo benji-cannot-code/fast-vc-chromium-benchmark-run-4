2ef09aa85cabf961ac42cfd855afcb66a1088eb3
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-04 11:44:17 +0000

    2008-09-04  Tor Arne Vestbø  <tavestbo@trolltech.com>
            Reviewed by Simon.
    
            Re-enable support for user stylesheets in QtWebKit
    
            QtWebKit now follows the FRAME_LOADS_USER_STYLESHEET
            code path, which allows us to keep API support for
            loading user style sheets from remote URLs.
    
            As part of the change UserStyleSheetLoader.cpp/h was
            moved from WebCore/loader/mac to WebCore/loader.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36088 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.h
+++ third_party/WebKit/WebCore/dom/Document.h
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // FIXME: We should move Mac off of the old Frame-based user stylesheet loading
 // code and onto the new code in Page. We can't do that until the code in Page
 // supports non-file: URLs, however.
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) || PLATFORM(QT)
 #define FRAME_LOADS_USER_STYLESHEET 1
 #else
 #define FRAME_LOADS_USER_STYLESHEET 0
