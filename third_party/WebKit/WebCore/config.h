3ae5d6f206b62821a3bffe7a6ff52214216ae342
jmalonzo@webkit.org <jmalonzo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-04 20:26:16 +0000

    2008-09-04  Marco Barisione  <marco.barisione@collabora.co.uk>
            Reviewed by Eric Seidel.
    
            http://bugs.webkit.org/show_bug.cgi?id=20380
            [GTK][AUTOTOOLS] Include autotoolsconfig.h from config.h
    
            * kjs/config.h: Include the configuration header generated by
            autotools if available.
    
    2008-09-04  Marco Barisione  <marco.barisione@collabora.co.uk>
    
            Reviewed by Eric Seidel.
    
            http://bugs.webkit.org/show_bug.cgi?id=20380
            [GTK][AUTOTOOLS] Include autotoolsconfig.h from config.h
    
            * configure.ac: Rename the configuration header from aconfig.h to autotoolsconfig.h.
    
    2008-09-04  Marco Barisione  <marco.barisione@collabora.co.uk>
    
            Reviewed by Eric Seidel.
    
            http://bugs.webkit.org/show_bug.cgi?id=20380
            [GTK][AUTOTOOLS] Include autotoolsconfig.h from config.h
    
            * config.h: Include the configuration header generated by autotools if
            available.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36098 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/config.h
+++ third_party/WebKit/WebCore/config.h
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
+#if HAVE_CONFIG_H
+#include "autotoolsconfig.h"
+#endif
+
 #include <wtf/Platform.h>
 
 #define MOBILE 0
