4c0b2ff9061ce07d4a4d66aae83b6e335891a240
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-04 01:32:51 +0000

    Reviewed by Sam.
            Clean up Platform.h and add PLATFORM(CHROMIUM), PLATFORM(SKIA) and USE(V8_BINDINGS)
    
            * Configurations/WebCore.xcconfig: add missing ENABLE_*
            * config.h: add rules for V8_BINDINGS
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/ASCIICType.h
+++ third_party/WebKit/JavaScriptCore/wtf/ASCIICType.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WTF_ASCIICType_h
 #define WTF_ASCIICType_h
 
+#include <wtf/Assertions.h>
 #include <wtf/Platform.h>
 
 // The behavior of many of the functions in the <ctype.h> header is dependent
