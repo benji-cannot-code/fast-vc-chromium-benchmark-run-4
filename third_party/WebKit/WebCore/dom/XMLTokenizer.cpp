7b9edb7f60c9a2618696b8824a5ec80c2866127e
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-04 20:44:12 +0000

    Build fix only, no review.
            * dom/XMLTokenizer.cpp: Fix the Chromium merge build by adding a missing header (the Mac files must include it somewhere).
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/XMLTokenizer.cpp
+++ third_party/WebKit/WebCore/dom/XMLTokenizer.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLTokenizer.h"
 #include "ScriptController.h"
 #include "ScriptElement.h"
+#include "SecurityOrigin.h"
 #include "ProcessingInstruction.h"
 #include "ResourceError.h"
 #include "ResourceHandle.h"
