d12e1545835b4bf054f5bcfed764449f3eff9763
aa@google.com <aa@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-05 17:47:58 +0000

    Try again to begin compiling the files that implemen the HTML5 database API.
    This time, I added a project dependency from WebCore to sqlite
    on every solution that includes WebCore.
    
    Review URL: http://codereview.chromium.org/466
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1785 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/page/inspector/InspectorController.cpp
+++ webkit/port/page/inspector/InspectorController.cpp
@@ -90,9 +90,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include <wtf/RefCounted.h>
 
+// TODO(aa): Implement database inspection and remove this macro.
+#define ENABLE_DATABASE_INSPECTION 0
+
 #if ENABLE(DATABASE)
 #include "Database.h"
+#if ENABLE_DATABASE_INSPECTION
 #include "JSDatabase.h"
+#endif
 #endif
 
 #if USE(JAVASCRIPTCORE_BINDINGS)
@@ -478,6 +483,7 @@ protected:
 
 #if ENABLE(DATABASE)
 struct InspectorDatabaseResource : public RefCounted<InspectorDatabaseResource> {
+#if ENABLE_DATABASE_INSPECTION
     static PassRefPtr<InspectorDatabaseResource> create(Database* database, const String& domain, const String& name, const String& version)
     {
         // Apple changed the default refcount to 1: http://trac.webkit.org/changeset/30406
@@ -519,6 +525,7 @@ private:
         , scriptObject(0)
     {
     }
+#endif
 };
 #endif
 
@@ -909,6 +916,7 @@ void InspectorController::search(Node* node, const String& target) {
 #endif
 
 #if ENABLE(DATABASE)
+#if ENABLE_DATABASE_INSPECTION
 static JSValueRef databaseTableNames(JSContextRef ctx, JSObjectRef /*function*/, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception)
 {
     InspectorController* controller = reinterpret_cast<InspectorController*>(JSObjectGetPrivate(thisObject));
@@ -962,6 +970,7 @@ static JSValueRef databaseTableNames(JSContextRef ctx, JSObjectRef /*function*/,
 
     return result;
 }
+#endif
 #endif
 
 #if USE(JAVASCRIPTCORE_BINDINGS)
@@ -1448,7 +1457,9 @@ void InspectorController::windowScriptObjectAvailable()
         { "detach", detach, kJSPropertyAttributeNone },
         { "search", search, kJSPropertyAttributeNone },
 #if ENABLE(DATABASE)
+#if ENABLE_DATABASE_INSPECTION
         { "databaseTableNames", databaseTableNames, kJSPropertyAttributeNone },
+#endif
 #endif
         { "inspectedWindow", inspectedWindow, kJSPropertyAttributeNone },
         { "localizedStringsURL", localizedStrings, kJSPropertyAttributeNone },
@@ -2194,9 +2205,11 @@ void InspectorController::populateScriptObjects()
         addScriptConsoleMessage(m_consoleMessages[i]);
 
 #if ENABLE(DATABASE)
+#if ENABLE_DATABASE_INSPECTION
     DatabaseResourcesSet::iterator databasesEnd = m_databaseResources.end();
     for (DatabaseResourcesSet::iterator it = m_databaseResources.begin(); it != databasesEnd; ++it)
         addDatabaseScriptResource((*it).get());
+#endif
 #endif
 }
 #elif USE(V8_BINDING)
@@ -2213,6 +2226,7 @@ void InspectorController::populateScriptObjects()
 #endif
 
 #if ENABLE(DATABASE)
+#if ENABLE_DATABASE_INSPECTION
 JSObjectRef InspectorController::addDatabaseScriptResource(InspectorDatabaseResource* resource)
 {
     ASSERT_ARG(resource, resource);
@@ -2306,6 +2320,7 @@ void InspectorController::removeDatabaseScriptResource(InspectorDatabaseResource
     JSObjectCallAsFunction(m_scriptContext, removeDatabaseFunction, m_scriptObject, 1, arguments, &exception);
     HANDLE_EXCEPTION(exception);
 }
+#endif
 #endif
 
 #if USE(JAVASCRIPTCORE_BINDINGS)
@@ -2435,11 +2450,13 @@ void InspectorController::resetScriptObjects()
     }
 
 #if ENABLE(DATABASE)
+#if ENABLE_DATABASE_INSPECTION
     DatabaseResourcesSet::iterator databasesEnd = m_databaseResources.end();
     for (DatabaseResourcesSet::iterator it = m_databaseResources.begin(); it != databasesEnd; ++it) {
         InspectorDatabaseResource* resource = (*it).get();
         resource->setScriptObject(0, 0);
     }
+#endif
 #endif
 
 #if USE(JAVASCRIPTCORE_BINDINGS)
@@ -2487,7 +2504,9 @@ void InspectorController::didCommitLoad(DocumentLoader* loader)
         m_consoleMessages.clear();
 
 #if ENABLE(DATABASE)
+#if ENABLE_DATABASE_INSPECTION
         m_databaseResources.clear();
+#endif
 #endif
 
         if (windowVisible()) {
@@ -2730,6 +2749,7 @@ void InspectorController::resourceRetrievedByXMLHttpRequest(unsigned long identi
 #if ENABLE(DATABASE)
 void InspectorController::didOpenDatabase(Database* database, const String& domain, const String& name, const String& version)
 {
+#if ENABLE_DATABASE_INSPECTION
     if (!enabled())
         return;
 
@@ -2739,6 +2759,7 @@ void InspectorController::didOpenDatabase(Database* database, const String& doma
 
     if (windowVisible())
         addDatabaseScriptResource(resource.get());
+#endif
 }
 #endif
 
