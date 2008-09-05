7cb80b5243fd540dca6abc27ffb7cb8170e739f9
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-05 17:24:37 +0000

    back out part of last change, better fix upcoming.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1784 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webframe_impl.cc
+++ webkit/glue/webframe_impl.cc
@@ -127,7 +127,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gfx/bitmap_platform_device_win.h"
 #endif
 #include "base/gfx/rect.h"
-#include "base/gfx/platform_canvas.h"
+#include "base/gfx/platform_canvas_win.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/stats_counters.h"
