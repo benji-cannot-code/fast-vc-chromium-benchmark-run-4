d5736740273bd119c20726c6a61ffde76419771f
cpu@google.com <cpu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-05 02:25:32 +0000

    Same CL as 448. Had to redo it.
    Change the default download path if needed, in Vista it could be set tothe
    destkop which is a risky location.- Remove reading the download location from IE
    in vista.BUG=b/1355855
    
    This was already LGTmed
    
    
    Review URL: http://codereview.chromium.org/462
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1753 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -113,6 +113,7 @@ extern const wchar_t kMemoryCacheSize[];
 
 extern const wchar_t kDownloadDefaultDirectory[];
 extern const wchar_t kDownloadExtensionsToOpen[];
+extern const wchar_t kDownloadDirUpgraded[];
 
 extern const wchar_t kSaveFileDefaultDirectory[];
 
