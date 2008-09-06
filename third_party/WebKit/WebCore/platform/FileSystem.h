8cac19ffa764ed0e6b0f984325d8cc3fe86f287d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-06 00:21:58 +0000

    Reviewed by Adam Roben.
            Build fix for WebKitWin and Chromium
    
            * platform/FileSystem.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36121 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/FileSystem.h
+++ third_party/WebKit/WebCore/platform/FileSystem.h
@@ -51,19 +51,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef const struct __CFData* CFDataRef;
 
+#if PLATFORM(WIN)
+// These are to avoid including <winbase.h> in a header for Chromium
+typedef void *HANDLE;
+// Assuming STRICT
+typedef struct HINSTANCE__* HINSTANCE;
+typedef HINSTANCE HMODULE;
+#endif
+
 namespace WebCore {
 
 class CString;
 
 #if PLATFORM(WIN)
-typedef void *HANDLE;
-// Assuming STRICT
-typedef struct HINSTANCE__* HINSTANCE;
-typedef HINSTANCE HMODULE;
-
 typedef HANDLE PlatformFileHandle;
 typedef HMODULE PlatformModule;
-const PlatformFileHandle invalidPlatformFileHandle = INVALID_HANDLE_VALUE;
+// FIXME: -1 is INVALID_HANDLE_VALUE, defined in <winbase.h>. Chromium tries to
+// avoid using Windows headers in headers.  We'd rather move this into the .cpp.
+const PlatformFileHandle invalidPlatformFileHandle = reinterpret_cast<HANDLE>(-1);
 
 struct PlatformModuleVersion {
     unsigned leastSig;
