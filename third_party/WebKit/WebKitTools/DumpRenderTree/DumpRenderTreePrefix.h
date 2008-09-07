08a315859fcb8736e2e2a45988bbd93dd8636009
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-07 07:12:45 +0000

    2008-09-07  Stephanie Lewis  <slewis@apple.com>
            Reviewed by Mark Rowe.
    
            Fix DRT build
    
            * DumpRenderTree/DumpRenderTreePrefix.h:
            * DumpRenderTree/win/DumpRenderTree.vcproj:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36248 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTreePrefix.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTreePrefix.h
@@ -32,3 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 
 #endif
+
+// If we don't define these, they get defined in windef.h. 
+// We want to use std::min and std::max
+#define max max
+#define min min
