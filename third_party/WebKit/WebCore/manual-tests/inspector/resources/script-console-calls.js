59c6927dabd8d1e82052b97d9e200e708b0c8d59
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-07 16:48:51 +0000

    Add a manual-test I forgot to commit in r36029.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebCore/manual-tests/inspector/resources/script-console-calls.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+console.info("Test console.info");
+console.log("Test console.log");
+console.warn("Test console.warn");
+console.error("Test console.error");
+console.time("Test console.time");
+console.timeEnd("Test console.time");
+console.count("Test console.count");
+console.assert(false, "Test console.assert");
