170255124b56edc36b3298b925d0860497089c22
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-07 23:57:40 +0000

    Fix SSL state in the URL bar being incorrect. Going to an EV site like https://www.verisign.com/ would not should the EV name in the URL bar unless you did something like switch tabs away and back because in my cleanup I removed the notification that this was depending on.
    This patch adds a new NOTIFY_SSL_STATE_CHANGED notification which is broadcast by the various SSL components when they update the flags. The browser now listens for this notification and will update the URL bar.
    
    BUG=1359547
    TEST=see repro steps in bug
    
    Review URL: http://codereview.chromium.org/436
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1831 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ssl_policy.cc
+++ chrome/browser/ssl_policy.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_contents.h"
 #include "chrome/common/jstemplate_builder.h"
 #include "chrome/common/l10n_util.h"
+#include "chrome/common/notification_service.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_service.h"
 #include "chrome/common/resource_bundle.h"
@@ -285,21 +286,27 @@ class DefaultPolicy : public SSLPolicy {
                                       error->request_url().host());
 
     switch (judgment) {
-    case net::X509Certificate::Policy::ALLOWED:
-      // We've been told to allow this certificate.
-      error->manager()->SetMaxSecurityStyle(
-          SECURITY_STYLE_AUTHENTICATION_BROKEN);
-      error->ContinueRequest();
-      break;
-    case net::X509Certificate::Policy::DENIED:
-      // For now we handle the DENIED as the UNKNOWN, which means a blocking
-      // page is shown to the user every time he comes back to the page.
-    case net::X509Certificate::Policy::UNKNOWN:
-      // We don't know how to handle this error.  Ask our sub-policies.
-      sub_policies_[index]->OnCertError(main_frame_url, error);
-      break;
-    default:
-      NOTREACHED();
+      case net::X509Certificate::Policy::ALLOWED:
+        // We've been told to allow this certificate.
+        if (error->manager()->SetMaxSecurityStyle(
+                SECURITY_STYLE_AUTHENTICATION_BROKEN)) {
+          NotificationService::current()->Notify(
+              NOTIFY_SSL_STATE_CHANGED,
+              Source<NavigationController>(error->manager()->controller()),
+              Details<NavigationEntry>(
+                  error->manager()->controller()->GetActiveEntry()));
+        }
+        error->ContinueRequest();
+        break;
+      case net::X509Certificate::Policy::DENIED:
+        // For now we handle the DENIED as the UNKNOWN, which means a blocking
+        // page is shown to the user every time he comes back to the page.
+      case net::X509Certificate::Policy::UNKNOWN:
+        // We don't know how to handle this error.  Ask our sub-policies.
+        sub_policies_[index]->OnCertError(main_frame_url, error);
+        break;
+      default:
+        NOTREACHED();
     }
   }
 
@@ -386,6 +393,7 @@ void SSLPolicy::OnRequestStarted(SSLManager* manager, const GURL& url,
   }
 
   NavigationEntry::SSLStatus& ssl = entry->ssl();
+  bool changed = false;
   if (!entry->url().SchemeIsSecure() ||  // Current page is not secure.
       resource_type == ResourceType::MAIN_FRAME ||  // Main frame load.
       net::IsCertStatusError(ssl.cert_status())) {  // There is already
@@ -406,6 +414,7 @@ void SSLPolicy::OnRequestStarted(SSLManager* manager, const GURL& url,
     if (net::IsCertStatusError(ssl_cert_status)) {
       // The resource is unsafe.
       if (!ssl.has_unsafe_content()) {
+        changed = true;
         ssl.set_has_unsafe_content();
         manager->SetMaxSecurityStyle(SECURITY_STYLE_AUTHENTICATION_BROKEN);
       }
@@ -420,12 +429,23 @@ void SSLPolicy::OnRequestStarted(SSLManager* manager, const GURL& url,
 
   // Now check for mixed content.
   if (entry->url().SchemeIsSecure() && !url.SchemeIsSecure()) {
-    ssl.set_has_mixed_content();
+    if (!ssl.has_mixed_content()) {
+      changed = true;
+      ssl.set_has_mixed_content();
+    }
     const std::wstring& msg = l10n_util::GetStringF(
         IDS_MIXED_CONTENT_LOG_MESSAGE,
         UTF8ToWide(entry->url().spec()),
         UTF8ToWide(url.spec()));
     manager->AddMessageToConsole(msg, MESSAGE_LEVEL_WARNING);
+  }
+
+  if (changed) {
+    // Only send the notification when something actually changed.
+    NotificationService::current()->Notify(
+        NOTIFY_SSL_STATE_CHANGED,
+        Source<NavigationController>(manager->controller()),
+        Details<NavigationEntry>(entry));
   }
 }
 
