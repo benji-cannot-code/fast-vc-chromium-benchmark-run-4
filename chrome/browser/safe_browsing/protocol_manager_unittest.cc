51f6dbe0478ae9aaf9db85530cc63d07eeef6526
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-08 18:09:33 +0000

    Disable a flakey test. BUG=http://code.google.com/p/chromium/issues/detail?id=1880 Review URL: http://codereview.chromium.org/1816
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1852 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/protocol_manager_unittest.cc
+++ chrome/browser/safe_browsing/protocol_manager_unittest.cc
@@ -81,7 +81,8 @@ TEST(SafeBrowsingProtocolManagerTest, TestChunkStrings) {
   EXPECT_EQ(pm.FormatList(phish, true), "goog-phish-shavar;mac\n");
 }
 
-TEST(SafeBrowsingProtocolManagerTest, TestGetHashBackOffTimes) {
+// Flakey, see http://code.google.com/p/chromium/issues/detail?id=1880
+TEST(SafeBrowsingProtocolManagerTest, DISABLED_TestGetHashBackOffTimes) {
   SafeBrowsingProtocolManager pm(NULL, NULL, "", "");
 
   // No errors or back off time yet.
@@ -132,4 +133,3 @@ TEST(SafeBrowsingProtocolManagerTest, TestGetHashBackOffTimes) {
   EXPECT_EQ(pm.gethash_error_count_, 7);
   EXPECT_TRUE(pm.next_gethash_time_== now + TimeDelta::FromMinutes(480));
 }
-
