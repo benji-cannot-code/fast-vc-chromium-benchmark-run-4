71751152dc72f51300a49c3f56a5bff81ab38871
nsylvain@google.com <nsylvain@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-08 22:33:45 +0000

    Change the external protocol handler to look at the command that will be run before warning the user. If the command is empty, there is no reason to warn.
    I removed a dcheck. This dcheck is good, but unfortunately
    we can't use g_browser_process in the io_thread.
    Review URL: http://codereview.chromium.org/1632
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1871 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/edit_drop_target.h
+++ chrome/browser/autocomplete/edit_drop_target.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_AUTOCOMPLETE_EDIT_DROP_TARGET_H__
-#define CHROME_BROWSER_AUTOCOMPLETE_EDIT_DROP_TARGET_H__
+#ifndef CHROME_BROWSER_AUTOCOMPLETE_EDIT_DROP_TARGET_H_
+#define CHROME_BROWSER_AUTOCOMPLETE_EDIT_DROP_TARGET_H_
 
 #include "base/base_drop_target.h"
 
@@ -56,5 +56,5 @@ class EditDropTarget : public BaseDropTarget {
   DISALLOW_EVIL_CONSTRUCTORS(EditDropTarget);
 };
 
-#endif  // CHROME_BROWSER_AUTOCOMPLETE_EDIT_DROP_TARGET_H__
+#endif  // CHROME_BROWSER_AUTOCOMPLETE_EDIT_DROP_TARGET_H_
 
