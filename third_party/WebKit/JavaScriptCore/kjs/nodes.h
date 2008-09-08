725841767e812887d1ba53388e6e8738862ac9a0
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-08 20:42:46 +0000

    - build fix
            * kjs/nodes.h:
            (JSC::StatementNode::):
            (JSC::BlockNode::):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/nodes.h
+++ third_party/WebKit/JavaScriptCore/kjs/nodes.h
@@ -234,7 +234,7 @@ namespace JSC {
         virtual Precedence precedence() const { ASSERT_NOT_REACHED(); return PrecExpression; }
         virtual bool isEmptyStatement() const JSC_FAST_CALL { return false; }
 
-        virtual bool isBlock() const KJS_FAST_CALL { return false; }
+        virtual bool isBlock() const JSC_FAST_CALL { return false; }
     protected:
         LabelStack m_labelStack;
 
@@ -1828,7 +1828,7 @@ namespace JSC {
 
         StatementVector& children() { return m_children; }
 
-        virtual bool isBlock() const KJS_FAST_CALL { return true; }
+        virtual bool isBlock() const JSC_FAST_CALL { return true; }
     protected:
         StatementVector m_children;
     };
