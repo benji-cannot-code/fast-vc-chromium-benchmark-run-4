96473bcf7c679a58729ddc4ba77d4bf178e05660
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-08 18:01:31 +0000

    A couple of quick TODO and other cleanups.
    B=1031854
    
    Review URL: http://codereview.chromium.org/1624
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1850 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/aero_glass_frame.cc
+++ chrome/browser/views/frame/aero_glass_frame.cc
@@ -64,10 +64,6 @@ gfx::Rect AeroGlassFrame::GetWindowBoundsForClientBounds(
   return gfx::Rect(rect);
 }
 
-void AeroGlassFrame::SizeToContents(const gfx::Rect& contents_bounds) {
-  // TODO(beng): implement me.
-}
-
 gfx::Rect AeroGlassFrame::GetBoundsForTabStrip(TabStrip* tabstrip) const {
   return GetAeroGlassNonClientView()->GetBoundsForTabStrip(tabstrip);
 }
@@ -81,7 +77,6 @@ ChromeViews::Window* AeroGlassFrame::GetWindow() {
 
 void AeroGlassFrame::UpdateWindowIcon() {
   Window::UpdateWindowIcon();
-  // TODO(beng): do something in the non-client view when this builds on Vista.
 }
 
 ///////////////////////////////////////////////////////////////////////////////
