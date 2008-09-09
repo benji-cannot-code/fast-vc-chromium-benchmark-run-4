f45d77ee3cbbe9e198f8cb32c8796ddc66e82cf8
mmoss@google.com <mmoss@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-09 22:35:16 +0000

    Make CreateDirectory consistent on Win and Posix when directory exists. Review URL: http://codereview.chromium.org/1681
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1941 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_win.cc
+++ base/file_util_win.cc
@@ -420,6 +420,8 @@ bool CreateNewTempDirectory(const std::wstring& prefix,
 }
 
 bool CreateDirectory(const std::wstring& full_path) {
+  if (PathExists(full_path))
+    return true;
   int err = SHCreateDirectoryEx(NULL, full_path.c_str(), NULL);
   return err == ERROR_SUCCESS;
 }
@@ -644,4 +646,3 @@ std::wstring FileEnumerator::Next() {
 }
 
 }  // namespace file_util
-
