b9ec5821b3546e9bb2a56da8545126570a05245f
mmentovai@google.com <mmentovai@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-09 19:50:18 +0000

    Apply xcconfigs to Mac TestShell, fix warnings Review URL: http://codereview.chromium.org/1668
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1917 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/mac/test_shell_webview.mm
+++ webkit/tools/test_shell/mac/test_shell_webview.mm
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <Cocoa/Cocoa.h>
+#import "test_shell_webview.h"
 
-#include "test_shell_webview.h"
+#import <Cocoa/Cocoa.h>
 
 #include "base/gfx/rect.h"
 #include "base/scoped_ptr.h"
