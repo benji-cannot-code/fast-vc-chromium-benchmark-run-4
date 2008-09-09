1c5435055d8a2a7fadf78ef09560e8569103fe8e
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-09 18:56:30 +0000

    fix win bustage
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1912 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/event_conversion.h
+++ webkit/glue/event_conversion.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_GLUE_EVENT_CONVERSION_H__
 #define WEBKIT_GLUE_EVENT_CONVERSION_H__
 
+#include "base/basictypes.h"
+
 #pragma warning(push, 0)
 #include "PlatformKeyboardEvent.h"
 #include "PlatformMouseEvent.h"
