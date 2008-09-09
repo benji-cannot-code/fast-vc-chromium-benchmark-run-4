f3d19518bafb5c704dec7af134106be22f68cd99
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-09 20:27:19 +0000

    remove reference in mac ifdef to a function not in the shared RendeTheme header Review URL: http://codereview.chromium.org/1674
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1922 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webview_impl.cc
+++ webkit/glue/webview_impl.cc
@@ -60,8 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PluginInfoStore.h"
 #if defined(OS_WIN)
 #include "RenderThemeWin.h"
-#elif defined(OS_MACOSX)
-#include "RenderThemeMac.h"
 #endif
 #include "ResourceHandle.h"
 #include "SelectionController.h"
@@ -1130,8 +1128,6 @@ void WebViewImpl::SetPreferences(const WebPreferences& preferences) {
   // RenderTheme is a singleton that needs to know the default font size to
   // draw some form controls.  We let it know each time the size changes.
   WebCore::RenderThemeWin::setDefaultFontSize(preferences.default_font_size);
-#elif defined(OS_MACOSX)
-  WebCore::RenderThemeMac::setDefaultFontSize(preferences.default_font_size);
 #endif
 
   // Used to make sure if the frameview needs layout, layout is triggered
