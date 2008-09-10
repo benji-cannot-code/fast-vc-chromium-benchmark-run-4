ed822675d811cace676f13baad819b1502fe837e
finnur@google.com <finnur@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-10 22:35:34 +0000

    Fixing 1669: Renderer crashes on FindNext http://code.google.com/p/chromium/issues/detail?id=1669
    Re. Finding: When the SelectionController in WebKit doesn't give us a proper selection for the match found or a proper bounding box for the same we would not be able to set the Active tickmark nor mark that frame as a frame that contains the active tickmark. The latter causes the crash on Google Reader described in issue 1669 and also the crash with Malayalam unicode content found internally (bug 1341577). In the case of Reader, the Selection returned is not empty, but has a start and end set to 0.
    
    When fixing this I noticed that if the first match we find specifies "-webkit-user-select: none" then no further matches will be found on the page. This is because we were changing the flag |found| to false in Find which causes the scoping effort to not start. What we should do instead is set the active rect to a default rect so that the scoping effort knows that we don't know what the active rect is. It will then mark the first match found on the page as active and continue scoping.
    
    To recap on how Find works (since I know it has been a while since you looked at this problem):
    
    Find will try to find the first match from a given point in the page. When something is found it will stop the find operation and start the scoping effort in the background for each frame. Scoping starts from the top of the frame, finds all matches and builds the tickmark vector per frame - in the process marking which tickmark is active. FindNext used to rely on the Scoping to mark the active frame, but now it is done in Find instead.
    
    BUG=1341577
    
    Review URL: http://codereview.chromium.org/1847
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2028 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webframe_impl.h
+++ webkit/glue/webframe_impl.h
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef WEBKIT_GLUE_WEBFRAME_IMPL_H__
-#define WEBKIT_GLUE_WEBFRAME_IMPL_H__
+#ifndef WEBKIT_GLUE_WEBFRAME_IMPL_H_
+#define WEBKIT_GLUE_WEBFRAME_IMPL_H_
 
 #include <string>
 
@@ -139,7 +139,7 @@ class WebFrameImpl : public WebFrame {
   virtual void Redo();
   virtual void ClearSelection();
 
-  virtual void SetInViewSourceMode(bool);
+  virtual void SetInViewSourceMode(bool enable);
 
   virtual bool GetInViewSourceMode() const;
 
@@ -154,7 +154,7 @@ class WebFrameImpl : public WebFrame {
 
   virtual bool ExecuteCoreCommandByName(const std::string& name, const std::string& value);
 
-  virtual void AddMessageToConsole(const std::wstring& msg, 
+  virtual void AddMessageToConsole(const std::wstring& msg,
                                    ConsoleMessageLevel level);
 
   virtual void ClosePage();
@@ -333,14 +333,21 @@ class WebFrameImpl : public WebFrame {
   // The index of the active tickmark for the current frame.
   size_t active_tickmark_;
 
+  // This flag is used by the scoping effort to determine if we need to figure
+  // out which rectangle is the active tickmark. Once we find the active
+  // rectangle we clear this flag.
+  bool locating_active_rect_;
+
   // This rectangle is used during the scoping effort to figure out what rect
   // got selected during the Find operation. In other words, first the Find
   // operation iterates to the next match and then scoping will happen for all
   // matches. When we encounter this rectangle during scoping we mark that
   // tickmark as active (see active_tickmark_). This avoids having to iterate
   // through a potentially very large tickmark vector to see which hit is
-  // active. Once we find the active rectangle we clear this rectangle to
-  // indicate that we are done determining what the active match is.
+  // active. An empty rect means that we don't know the rectangle for the
+  // selection (ie. because the selection controller couldn't tell us what the
+  // bounding box for it is) and the scoping effort should mark the first
+  // match it finds as the active rectangle.
   WebCore::IntRect active_selection_rect_;
 
   // This range represents the range that got selected during the Find or
@@ -427,7 +434,7 @@ class WebFrameImpl : public WebFrame {
   // For each printed page, the view of the document in pixels.
   Vector<WebCore::IntRect> pages_;
 
-  DISALLOW_EVIL_CONSTRUCTORS(WebFrameImpl);
+  DISALLOW_COPY_AND_ASSIGN(WebFrameImpl);
 };
 
-#endif  // WEBKIT_GLUE_WEBFRAME_IMPL_H__
+#endif  // WEBKIT_GLUE_WEBFRAME_IMPL_H_
