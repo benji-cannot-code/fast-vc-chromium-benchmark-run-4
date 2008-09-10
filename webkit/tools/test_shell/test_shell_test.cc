d5d58347031991406a9b2447a113200d03f24f28
avi@google.com <avi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-10 21:21:51 +0000

    Bring test shell test into the mac project
    Review URL: http://codereview.chromium.org/1705
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2017 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_test.cc
+++ webkit/tools/test_shell/test_shell_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/tools/test_shell/test_shell_test.h"
 
+#include "base/basictypes.h"
 #include "base/file_util.h"
 #include "base/message_loop.h"
 #include "base/path_service.h"
@@ -18,7 +19,9 @@ std::wstring TestShellTest::GetTestURL(std::wstring test_case_path,
 
 void TestShellTest::SetUp() {
   // Make a test shell for use by the test.
+#if defined(OS_WIN)
   TestShell::RegisterWindowClass();
+#endif
   CreateEmptyWindow();
   test_shell_->Show(test_shell_->webView(), NEW_WINDOW);
 
@@ -32,7 +35,7 @@ void TestShellTest::SetUp() {
 void TestShellTest::TearDown() {
   // Loading a blank url clears the memory in the current page.
   test_shell_->LoadURL(L"about:blank");
-  DestroyWindow(test_shell_->mainWnd());
+  test_shell_->DestroyWindow(test_shell_->mainWnd());
   LayoutTestController::ClearShell();
   
   // Flush the MessageLoop of any residual tasks.
