d4a557f53de314ed4fa9bac0a8ebc800d36658f5
fqian@google.com <fqian@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-10 04:09:48 +0000

    Patch for bug 1994:
    Make sure that customized properties set on window.location and window.navigator survive
    GC. Matches Safari and Firefox's behaviors.
    
    Review URL: http://codereview.chromium.org/1884
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1973 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/page/Navigator.h
+++ webkit/port/page/Navigator.h
@@ -244,6 +244,8 @@ class Navigator : public RefCounted<Navigator> {
     return m_frame->settings()->isJavaEnabled();
   }
 
+  Frame* frame() { return m_frame; }
+
   void disconnectFrame() { m_frame = NULL; }
 
  private:
