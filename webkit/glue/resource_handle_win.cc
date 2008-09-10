329ca590e7a7a8bfb8e1c214f05268ad44a46dda
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-10 14:28:45 +0000

    make more cross platform, use process_utils to get proc id. remove unused header. Review URL: http://codereview.chromium.org/1857
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1985 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/resource_handle_win.cc
+++ webkit/glue/resource_handle_win.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceError.h"
 #include "ResourceHandle.h"
 #include "ResourceHandleClient.h"
-#include "ResourceHandleWin.h"  // for Platform{Response,Data}Struct
 #include "ResourceRequest.h"
 #include "ResourceResponse.h"
 #pragma warning(pop)
@@ -43,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #undef LOG
 #include "base/logging.h"
 #include "base/message_loop.h"
+#include "base/process_util.h"
 #include "base/time.h"
 #include "base/string_util.h"
 #include "base/string_tokenizer.h"
@@ -297,7 +297,7 @@ bool ResourceHandleInternal::Start(
       request_.frame() ? WebFrameImpl::FromFrame(request_.frame()) : NULL;
 
   CString method = request_.httpMethod().latin1();
-  GURL referrer(webkit_glue::StringToStdWString(request_.httpReferrer()));
+  GURL referrer(webkit_glue::StringToStdString(request_.httpReferrer()));
 
   // Compute the URL of the load.
   GURL url = webkit_glue::KURLToGURL(request_.url());
@@ -320,7 +320,7 @@ bool ResourceHandleInternal::Start(
   GURL policy_url;
   if (request_.resourceType() != ResourceType::MAIN_FRAME &&
       request_.frame() && request_.frame()->document()) {
-    policy_url = GURL(webkit_glue::StringToStdWString(
+    policy_url = GURL(webkit_glue::StringToStdString(
         request_.frame()->document()->policyBaseURL()));
   }
 
@@ -335,8 +335,8 @@ bool ResourceHandleInternal::Start(
   if (!headerMap.contains("accept"))
     request_.addHTTPHeaderField("Accept", "*/*");
 
-  const String crlf(L"\r\n");
-  const String sep(L": ");
+  const String crlf("\r\n");
+  const String sep(": ");
   for (HTTPHeaderMap::const_iterator it = headerMap.begin();
        it != headerMap.end(); ++it) {
     // Skip over referrer headers found in the header map because we already
@@ -378,7 +378,7 @@ bool ResourceHandleInternal::Start(
   // have a origin_pid. Find a better place to set this.
   int origin_pid = request_.originPid();
   if (origin_pid == 0)
-    origin_pid = ::GetCurrentProcessId();
+    origin_pid = process_util::GetCurrentProcId();
 
   bool mixed_content =
       webkit_glue::KURLToGURL(request_.mainDocumentURL()).SchemeIsSecure() &&
