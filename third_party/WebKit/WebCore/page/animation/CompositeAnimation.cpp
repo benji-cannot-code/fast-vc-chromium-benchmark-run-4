12d76989ed1b25fb20ef70e076d8566cfe11f854
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-11 21:59:08 +0000

    2008-09-11  Dean Jackson  <dino@apple.com>
            Reviewed by Dan Bernstein.
    
            Fix assertion on transition property "none"
            https://bugs.webkit.org/show_bug.cgi?id=20751
    
            Test: transitions/transition-end-event-set-none.html
    
            * page/animation/CompositeAnimation.cpp:
            (WebCore::CompositeAnimation::updateTransitions):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/CompositeAnimation.cpp
+++ third_party/WebKit/WebCore/page/animation/CompositeAnimation.cpp
@@ -60,6 +60,10 @@ void CompositeAnimation::updateTransitions(RenderObject* renderer, const RenderS
             continue;
          
         int prop = anim->property();
+
+        if (prop == cAnimateNone)
+            continue;
+        
         bool all = prop == cAnimateAll;
         
         // Handle both the 'all' and single property cases. For the single prop case, we make only one pass
