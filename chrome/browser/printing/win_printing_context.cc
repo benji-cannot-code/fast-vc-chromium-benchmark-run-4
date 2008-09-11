67e22b3e4efbfcdcaa7be4e336bc793247977fd9
maruel@google.com <maruel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-11 21:25:07 +0000

    Two minor fixes. Fix the hard coded desired dpi and remove an unneeded include. Review URL: http://codereview.chromium.org/1954
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2084 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/win_printing_context.cc
+++ chrome/browser/printing/win_printing_context.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time_format.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/printing/print_job_manager.h"
-#include "chrome/common/gfx/emf.h"
 
 namespace {
 
