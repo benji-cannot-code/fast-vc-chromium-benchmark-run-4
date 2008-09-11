fbffe08783db97d787d69bb164f6e27285f59ab3
wtc@google.com <wtc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-11 00:24:09 +0000

    Remove all but one place in this file where we return ERR_FAILED.
    In the remaining place, log the original Win32 error
    code to help diagnose a failure in the field.
    
    R=darin
    Review URL: http://codereview.chromium.org/2401
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2044 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_transaction_winhttp.cc
+++ net/http/http_transaction_winhttp.cc
@@ -82,6 +82,8 @@ static int TranslateOSError(DWORD error) {
 
     default:
       DCHECK(error != ERROR_IO_PENDING);  // WinHTTP doesn't use this error.
+      LOG(WARNING) << "Unknown error " << error
+                   << " mapped to net::ERR_FAILED";
       return ERR_FAILED;
   }
 }
@@ -1241,7 +1243,7 @@ int HttpTransactionWinHttp::SendRequest() {
       // TODO(darin): no way to support >4GB uploads w/ WinHttp?
       if (upload_len > static_cast<uint64>(DWORD(-1))) {
         NOTREACHED() << "upload length is too large";
-        return ERR_FAILED;
+        return ERR_FILE_TOO_BIG;
       }
 
       total_size = static_cast<DWORD>(upload_len);
@@ -1757,7 +1759,7 @@ void HttpTransactionWinHttp::HandleStatusCallback(DWORD status,
                                                   DWORD_PTR result,
                                                   DWORD error,
                                                   DWORD secure_failure) {
-  int rv = ERR_FAILED;
+  int rv;
 
   switch (status) {
     case WINHTTP_CALLBACK_STATUS_REQUEST_ERROR:
@@ -1778,6 +1780,8 @@ void HttpTransactionWinHttp::HandleStatusCallback(DWORD status,
       break;
     default:
       NOTREACHED() << "unexpected status code";
+      rv = ERR_UNEXPECTED;
+      break;
   }
 
   if (rv == ERR_IO_PENDING) {
