d4b367289906f2e757f5d5604b6053c1fbcb6e17
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-12 18:22:07 +0000

    Reviewed by Sam Weinig.
            - https://bugs.webkit.org/show_bug.cgi?id=20793 clean up page/animation
    
            * page/animation/AnimationController.cpp:
            * page/animation/AnimationController.h:
            * page/animation/CompositeAnimation.cpp:
            * page/animation/CompositeAnimation.h:
            * page/animation/ImplicitAnimation.cpp:
            * page/animation/ImplicitAnimation.h:
            * page/animation/KeyframeAnimation.cpp:
            * page/animation/KeyframeAnimation.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36351 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/AnimationController.h
+++ third_party/WebKit/WebCore/page/animation/AnimationController.h
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef AnimationController_h
 #define AnimationController_h
 
-#include <stdint.h>
-
 namespace WebCore {
 
 class AnimationControllerPrivate;
@@ -40,35 +38,40 @@ class Frame;
 class RenderObject;
 class RenderStyle;
 
-class AnimationController
-{
+class AnimationController {
 public:
     AnimationController(Frame*);
     ~AnimationController();
-    
+
     void cancelAnimations(RenderObject*);
     RenderStyle* updateAnimations(RenderObject*, RenderStyle* newStyle);
-    
-    void setAnimationStartTime(RenderObject* obj, double t);
-    void setTransitionStartTime(RenderObject* obj, int property, double t);
-    
-    bool isAnimatingPropertyOnRenderer(RenderObject* obj, int property, bool isRunningNow) const;
-    
-    void suspendAnimations(Document* document);
-    void resumeAnimations(Document* document);
+
+    void setAnimationStartTime(RenderObject*, double t);
+    void setTransitionStartTime(RenderObject*, int property, double t);
+
+    bool isAnimatingPropertyOnRenderer(RenderObject*, int property, bool isRunningNow) const;
+
+    void suspendAnimations(Document*);
+    void resumeAnimations(Document*);
     void updateAnimationTimer();
-    
+
     void startUpdateRenderingDispatcher();
-    
+
     void styleAvailable();
-    
-    void setWaitingForStyleAvailable(bool waiting) { if (waiting) m_numStyleAvailableWaiters++; else m_numStyleAvailableWaiters--; }
-    
+
+    void setWaitingForStyleAvailable(bool waiting)
+    {
+        if (waiting)
+            m_numStyleAvailableWaiters++;
+        else
+            m_numStyleAvailableWaiters--;
+    }
+
 private:
     AnimationControllerPrivate* m_data;
-    uint32_t m_numStyleAvailableWaiters;    
+    unsigned m_numStyleAvailableWaiters;    
 };
 
-}
+} // namespace WebCore
 
-#endif
+#endif // AnimationController_h
