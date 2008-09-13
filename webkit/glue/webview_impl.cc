bdf531148b7dc08a7749a88df5e0ae39086e96f6
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-13 02:06:26 +0000

    Fix string conversions for Mac build bustage Review URL: http://codereview.chromium.org/2824
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2162 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webview_impl.cc
+++ webkit/glue/webview_impl.cc
@@ -1163,7 +1163,7 @@ std::wstring WebViewImpl::GetMainFrameEncodingName() {
     return std::wstring(L"");
 
   String encoding_name = main_frame_->frame()->loader()->encoding();
-  return std::wstring(encoding_name.charactersWithNullTermination());
+  return webkit_glue::StringToStdWString(encoding_name);
 }
 
 void WebViewImpl::MakeTextLarger() {
