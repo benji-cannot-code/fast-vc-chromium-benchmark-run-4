b943c9fe1b1653d67b58a3f65db2e0121e88cbce
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-13 02:59:00 +0000

    Fix Mac build.
            * css/CSSStyleSelector.cpp:
            (WebCore::getTransformOperationType):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36367 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/WebCore/css/CSSStyleSelector.cpp
@@ -5606,6 +5606,7 @@ TransformOperation::OperationType getTransformOperationType(WebKitCSSTransformVa
         case WebKitCSSTransformValue::SkewXTransformOperation:          return TransformOperation::SKEW_X;
         case WebKitCSSTransformValue::SkewYTransformOperation:          return TransformOperation::SKEW_Y;
         case WebKitCSSTransformValue::MatrixTransformOperation:         return TransformOperation::MATRIX;
+        case WebKitCSSTransformValue::UnknownTransformOperation:        return TransformOperation::NONE;
     }
     return TransformOperation::NONE;
 }
