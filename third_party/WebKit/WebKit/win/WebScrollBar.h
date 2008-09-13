ed6420099c64bf46f0511e1c788e53b3c6d67bad
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-13 18:41:47 +0000

    more scrollbar refactoring.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36370 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebScrollBar.h
+++ third_party/WebKit/WebKit/win/WebScrollBar.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma warning(push, 0)
 #include <WebCore/COMPtr.h>
 #include <WebCore/ScrollBar.h>
+#include <WebCore/ScrollbarClient.h>
 #pragma warning(pop)
 
 namespace WebCore {
