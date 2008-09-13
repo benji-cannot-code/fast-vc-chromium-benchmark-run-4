3d4040512be0dcb42983c0e89f96104ad4683f61
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-13 01:19:59 +0000

    Forgot to commit these two files.
    B=2205
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2157 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/importer_lock_view.cc
+++ chrome/browser/views/importer_lock_view.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/importer_lock_view.h"
 
 #include "chrome/app/locales/locale_settings.h"
-#include "chrome/browser/importer.h"
+#include "chrome/browser/importer/importer.h"
 #include "chrome/browser/standard_layout.h"
 #include "chrome/common/l10n_util.h"
 #include "chrome/views/label.h"
--- chrome/browser/views/importing_progress_view.h
+++ chrome/browser/views/importing_progress_view.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_VIEWS_IMPORTING_PROGRESS_VIEW_H_
 #define CHROME_BROWSER_VIEWS_IMPORTING_PROGRESS_VIEW_H_
 
-#include "chrome/browser/importer.h"
+#include "chrome/browser/importer/importer.h"
 #include "chrome/views/dialog_delegate.h"
 #include "chrome/views/view.h"
 #include "chrome/views/window.h"
