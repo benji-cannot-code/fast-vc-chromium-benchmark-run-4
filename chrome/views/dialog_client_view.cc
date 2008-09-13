3aa3d0ef5603da324a3733e17246b2738a4fb99a
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-13 20:07:03 +0000

    fix build bustage
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2170 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/dialog_client_view.cc
+++ chrome/views/dialog_client_view.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/views/dialog_client_view.h"
 
 #include "base/gfx/native_theme.h"
-#include "chrome/browser/standard_layout.h"
+#include "chrome/browser/views/standard_layout.h"
 #include "chrome/common/gfx/chrome_canvas.h"
 #include "chrome/common/gfx/chrome_font.h"
 #include "chrome/common/l10n_util.h"
