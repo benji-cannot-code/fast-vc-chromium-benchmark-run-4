55f3804255abd9e9ae65b7c1ae53d234d837a7a7
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-14 22:15:28 +0000

    2008-09-14  Sam Weinig  <sam@webkit.org>
            Qt build fix.
    
            * platform/qt/PlatformScrollBarQt.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/PlatformScrollBarQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/PlatformScrollBarQt.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007 Apple Inc.  All rights reserved.
+ * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
  * Copyright (C) 2007 Staikos Computing Services Inc. <info@staikos.net>
  * Copyright (C) 2007 Trolltech ASA
  *
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-
 #include "PlatformScrollBar.h"
 
 #include "EventHandler.h"
@@ -36,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 #include "IntRect.h"
 #include "PlatformMouseEvent.h"
+#include "ScrollbarTheme.h"
 
 #include <QApplication>
 #include <QDebug>
