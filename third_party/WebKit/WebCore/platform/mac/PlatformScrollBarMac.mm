9671301554940ea5adc5ca3769fb8ac3686c83f5
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-14 01:06:57 +0000

    Remove setEnabled from all Scrollbar subclasses that were just using their Widget base class methods.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36395 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/PlatformScrollBarMac.mm
+++ third_party/WebKit/WebCore/platform/mac/PlatformScrollBarMac.mm
@@ -193,11 +193,6 @@ void PlatformScrollbar::setRect(const IntRect& rect)
     setFrameGeometry(rect);
 }
 
-void PlatformScrollbar::setEnabled(bool enabled)
-{
-    Widget::setEnabled(enabled);
-}
-
 void PlatformScrollbar::paint(GraphicsContext* graphicsContext, const IntRect& damageRect)
 {
     Widget::paint(graphicsContext, damageRect);
