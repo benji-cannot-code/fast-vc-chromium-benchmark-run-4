fca35c6199433708e9b573cfeba0f0a9adcf029a
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-14 20:07:29 +0000

    Include the area to the right of the tabstrip underneath the window controls in the window caption area.
    B=1031854
    Review URL: http://codereview.chromium.org/2840
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2202 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_view2.cc
+++ chrome/browser/views/frame/browser_view2.cc
@@ -21,10 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/toolbar_view.h"
 #include "chrome/common/drag_drop_types.h"
 #include "chrome/common/l10n_util.h"
+#include "chrome/common/notification_service.h"
 #include "chrome/common/os_exchange_data.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/resource_bundle.h"
 #include "chrome/common/win_util.h"
+#include "chrome/views/hwnd_notification_source.h"
 #include "chrome/views/view.h"
 #include "chrome/views/window.h"
 
@@ -678,12 +680,9 @@ bool BrowserView2::CanClose() const {
   }
 
   // Empty TabStripModel, it's now safe to allow the Window to be closed.
-  /*
-  // TODO(beng): for some reason, this won't compile. Figure it out.
   NotificationService::current()->Notify(
       NOTIFY_WINDOW_CLOSED, Source<HWND>(frame_->GetWindow()->GetHWND()),
       NotificationService::NoDetails());
-      */
   return true;
 }
 
@@ -730,6 +729,15 @@ int BrowserView2::NonClientHitTest(const gfx::Point& point) {
   bounds.top += toolbar_->GetY();
   if (gfx::Rect(bounds).Contains(point.x(), point.y()))
     return HTCLIENT;
+
+  // If the point's y coordinate is above the top of the toolbar, but not in
+  // the tabstrip (per previous checking in this function), then we consider it
+  // in the window caption (e.g. the area to the right of the tabstrip
+  // underneath the window controls).
+  GetBounds(&bounds);
+  bounds.bottom = GetY() + toolbar_->GetY();
+  if (gfx::Rect(bounds).Contains(point.x(), point.y()))
+    return HTCAPTION;
 
   // If the point is somewhere else, delegate to the default implementation.
   return ClientView::NonClientHitTest(point);
