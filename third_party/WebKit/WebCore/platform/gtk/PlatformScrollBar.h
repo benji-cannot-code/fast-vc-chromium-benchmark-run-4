9d436fe9542967f3e2a4b8e6f8535169aa522cf8
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-14 00:50:24 +0000

    Remove now unused methods from GTK scrollbars.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36393 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/PlatformScrollBar.h
+++ third_party/WebKit/WebCore/platform/gtk/PlatformScrollBar.h
@@ -48,9 +48,6 @@ public:
     virtual void setEnabled(bool);
     virtual void paint(GraphicsContext*, const IntRect& damageRect);
 
-    static int horizontalScrollbarHeight();
-    static int verticalScrollbarWidth();
-
 protected:
     PlatformScrollbar(ScrollbarClient*, ScrollbarOrientation, ScrollbarControlSize);
 
