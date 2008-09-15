410eb275de106518aa1ea7dff1ecefc1a4d3cd3b
dsmith@webkit.org <dsmith@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-15 08:45:35 +0000

    2008-09-15  David Smith  <catfish.man@gmail.com>
            Reviewed by Dave Hyatt.
    
            https://bugs.webkit.org/show_bug.cgi?id=20180
            
            >2x speedup on querySelector[All] with :nth-child by removing the collectRulesOnly argument on SelectorChecker.
            With collectRulesOnly set to true (the default) an optimization is turned off, and no callers were intentionally leaving it true.
            querySelector[All] assumed that the default was probably the right way to go, and so ended up being slow.
            
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::CSSStyleSelector):
            (WebCore::CSSStyleSelector::SelectorChecker::SelectorChecker):
            * css/CSSStyleSelector.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36432 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/WebCore/css/CSSStyleSelector.cpp
@@ -375,7 +375,7 @@ static const MediaQueryEvaluator& printEval()
 
 CSSStyleSelector::CSSStyleSelector(Document* doc, const String& userStyleSheet, StyleSheetList* styleSheets, CSSStyleSheet* mappedElementSheet, bool strictParsing, bool matchAuthorAndUserStyles)
     : m_backgroundData(BackgroundFillLayer)
-    , m_checker(doc, strictParsing, false)
+    , m_checker(doc, strictParsing)
     , m_fontSelector(CSSFontSelector::create(doc))
 {
     init();
@@ -850,10 +850,10 @@ static inline const AtomicString* linkAttribute(Node* node)
     return 0;
 }
 
-CSSStyleSelector::SelectorChecker::SelectorChecker(Document* document, bool strictParsing, bool collectRulesOnly)
+CSSStyleSelector::SelectorChecker::SelectorChecker(Document* document, bool strictParsing)
     : m_document(document)
     , m_strictParsing(strictParsing)
-    , m_collectRulesOnly(collectRulesOnly)
+    , m_collectRulesOnly(false)
     , m_pseudoStyle(RenderStyle::NOPSEUDO)
     , m_documentIsHTML(document->isHTMLDocument())
 {
