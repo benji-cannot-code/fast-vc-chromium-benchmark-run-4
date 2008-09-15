0b24b685e272f56e0bf5f011677b1ef2756a9480
zork@google.com <zork@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-15 23:43:42 +0000

    Adding sync plugin messages, and plugin broadcasts to the renderer processes
    Review URL: http://codereview.chromium.org/2411
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2246 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_thread.cc
+++ chrome/renderer/render_thread.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/shared_memory.h"
 #include "base/thread_local.h"
+#include "chrome/common/chrome_plugin_lib.h"
 #include "chrome/common/ipc_logging.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/plugin/plugin_channel.h"
@@ -162,11 +163,23 @@ void RenderThread::OnMessageReceived(const IPC::Message& msg) {
       IPC_MESSAGE_HANDLER(ViewMsg_SetCacheCapacities, OnSetCacheCapacities)
       IPC_MESSAGE_HANDLER(ViewMsg_GetCacheResourceStats,
                           OnGetCacheResourceStats)
+      IPC_MESSAGE_HANDLER(ViewMsg_PluginMessage, OnPluginMessage)
       // send the rest to the router
       IPC_MESSAGE_UNHANDLED(router_.OnMessageReceived(msg))
     IPC_END_MESSAGE_MAP()
   } else {
     router_.OnMessageReceived(msg);
+  }
+}
+
+void RenderThread::OnPluginMessage(const std::wstring& dll_path,
+                                   const std::vector<uint8>& data) {
+  CHECK(ChromePluginLib::IsPluginThread());
+  ChromePluginLib *chrome_plugin = ChromePluginLib::Find(dll_path);
+  if (chrome_plugin) {
+    void *data_ptr = const_cast<void*>(reinterpret_cast<const void*>(&data[0]));
+    uint32 data_len = static_cast<uint32>(data.size());
+    chrome_plugin->functions().on_message(data_ptr, data_len);
   }
 }
 
