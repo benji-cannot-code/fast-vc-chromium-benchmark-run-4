acdc7df0d64cda75d26efc30286e016ccb795773
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-15 15:16:15 +0000

    Build fix
            * kjs/StructureID.cpp: Removed a stray semicolon.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/StructureID.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/StructureID.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "identifier.h"
 #include "JSObject.h"
-#include "PropertyNameArray.h";
+#include "PropertyNameArray.h"
 #include <wtf/RefPtr.h>
 
 using namespace std;
