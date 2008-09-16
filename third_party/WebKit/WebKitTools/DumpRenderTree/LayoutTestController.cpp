330056e5d5c2c2ad413d7f5f9185a04f664a193a
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-16 17:27:39 +0000

    2008-09-16  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Eric Seidel
    
            Protect against tests that call layoutTestController.notifyDone()
            more than once, which would lead to memory corruption, by nulling
            out the LayoutTestController on the JSObjectRef on destruction. Also
            add a finalize callback on the class so that if the LTC outlives
            the JSObjectRef, the JSObjectRef backpointer on the LTC can be
            nulled out.
    
            https://bugs.webkit.org/show_bug.cgi?id=20875
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36512 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.cpp
@@ -60,6 +60,7 @@ LayoutTestController::LayoutTestController(bool testRepaintDefault, bool testRep
     , m_waitToDump(false)
     , m_windowIsKey(true)
     , m_globalFlag(false)
+    , m_selfJSObject(0)
 {
 }
 
@@ -338,8 +339,13 @@ static JSValueRef notifyDoneCallback(JSContextRef context, JSObjectRef function,
     // Has mac & windows implementation
     // May be able to be made platform independant by using shared WorkQueue
     LayoutTestController* controller = reinterpret_cast<LayoutTestController*>(JSObjectGetPrivate(thisObject));
-    controller->notifyDone();
-
+    if (controller)
+        controller->notifyDone();
+    else {
+        const char* msg = "FAIL: Null layoutTestController in notifyDone(). Did a test call notifyDone() twice?\n";
+        fprintf(stderr, msg);
+        fprintf(stdout, msg);
+    }
     return JSValueMakeUndefined(context);
 }
 
@@ -641,13 +647,20 @@ static bool setGlobalFlagCallback(JSContextRef context, JSObjectRef thisObject, 
     return true;
 }
 
+static void layoutTestControllerObjectFinalize(JSObjectRef object)
+{
+    LayoutTestController* controller = reinterpret_cast<LayoutTestController*>(JSObjectGetPrivate(object));
+    if (controller)
+        controller->setJSObject(0);
+}
+
 // Object Creation
 
 void LayoutTestController::makeWindowObject(JSContextRef context, JSObjectRef windowObject, JSValueRef* exception)
 {
     JSRetainPtr<JSStringRef> layoutTestContollerStr(Adopt, JSStringCreateWithUTF8CString("layoutTestController"));
-    JSValueRef layoutTestContollerObject = JSObjectMake(context, getJSClass(), this);
-    JSObjectSetProperty(context, windowObject, layoutTestContollerStr.get(), layoutTestContollerObject, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete, exception);
+    setJSObject(JSObjectMake(context, getJSClass(), this));
+    JSObjectSetProperty(context, windowObject, layoutTestContollerStr.get(), jsObject(), kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete, exception);
 }
 
 JSClassRef LayoutTestController::getJSClass()
@@ -659,7 +672,7 @@ JSClassRef LayoutTestController::getJSClass()
         JSStaticFunction* staticFunctions = LayoutTestController::staticFunctions();
         JSClassDefinition classDefinition = {
             0, kJSClassAttributeNone, "LayoutTestController", 0, staticValues, staticFunctions,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            0, layoutTestControllerObjectFinalize, 0, 0, 0, 0, 0, 0, 0, 0, 0
         };
 
         layoutTestControllerClass = JSClassCreate(&classDefinition);
