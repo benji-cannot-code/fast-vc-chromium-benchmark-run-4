2d91a5f1293ca59a2d7213027925bb71a7fffb3d
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-16 09:17:35 +0000

    Fix Qt build bustage in ScrollbarThemeQt.cpp
    git-svn-id: svn://svn.chromium.org/blink/trunk@36494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/ScrollbarThemeQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/ScrollbarThemeQt.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScrollbarThemeQt.h"
 
 #include "GraphicsContext.h"
+#include "PlatformMouseEvent.h"
 #include "ScrollBar.h"
 
 #include <QApplication>
