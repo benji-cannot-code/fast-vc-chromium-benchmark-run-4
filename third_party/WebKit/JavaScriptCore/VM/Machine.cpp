810c5a489a0752899c7e25a4cc19631bdf206884
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-16 03:46:58 +0000

    2008-09-15  Sam Weinig  <sam@webkit.org>
            Roll out r36462.  It broke document.all.
    
            * VM/CTI.cpp:
            (JSC::CTI::privateCompileMainPass):
            (JSC::CTI::privateCompileSlowCases):
            * VM/CTI.h:
            * VM/Machine.cpp:
            (JSC::Machine::Machine):
            (JSC::Machine::cti_op_eq_null):
            (JSC::Machine::cti_op_neq_null):
            * VM/Machine.h:
            (JSC::Machine::isJSString):
            * kjs/JSCell.h:
            * kjs/JSWrapperObject.h:
            * kjs/StringObject.h:
            * kjs/StringObjectThatMasqueradesAsUndefined.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/Machine.cpp
+++ third_party/WebKit/JavaScriptCore/VM/Machine.cpp
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "debugger.h"
 #include "operations.h"
 #include "SamplingTool.h"
-#include "StringObjectThatMasqueradesAsUndefined.h"
 #include <stdio.h>
 
 #if PLATFORM(DARWIN)
@@ -572,10 +571,6 @@ Machine::Machine()
     JSArray* jsArray = new (storage) JSArray(StructureID::create(jsNull()));
     m_jsArrayVptr = jsArray->vptr();
     static_cast<JSCell*>(jsArray)->~JSCell();
-
-    StringObjectThatMasqueradesAsUndefined* jsStringObjectThatMasqueradesAsUndefined = new (storage) StringObjectThatMasqueradesAsUndefined(StringObjectThatMasqueradesAsUndefined::VPtrStealingHack);
-    m_jsStringObjectThatMasqueradesAsUndefinedVptr = jsStringObjectThatMasqueradesAsUndefined->vptr();
-    static_cast<JSCell*>(jsStringObjectThatMasqueradesAsUndefined)->~JSCell();
 
     JSString* jsString = new (storage) JSString(JSString::VPtrStealingHack);
     m_jsStringVptr = jsString->vptr();
@@ -5428,11 +5423,10 @@ void Machine::cti_op_debug(CTI_ARGS)
 JSValue* Machine::cti_op_eq_null(CTI_ARGS)
 {
     JSValue* src = ARG_src1;
-    
     if (src->isUndefinedOrNull())
         return jsBoolean(true);
 
-    return jsBoolean(ARG_exec->machine()->doesMasqueradesAsUndefined(src));
+    return jsBoolean(!JSImmediate::isImmediate(src) && static_cast<JSCell*>(src)->masqueradeAsUndefined());
 }
 
 JSValue* Machine::cti_op_neq_null(CTI_ARGS)
@@ -5441,7 +5435,7 @@ JSValue* Machine::cti_op_neq_null(CTI_ARGS)
     if (src->isUndefinedOrNull())
         return jsBoolean(false);
 
-    return jsBoolean(!ARG_exec->machine()->doesMasqueradesAsUndefined(src));
+    return jsBoolean(JSImmediate::isImmediate(src) || !static_cast<JSCell*>(src)->masqueradeAsUndefined());
 }
 
 void* Machine::cti_vm_throw(CTI_ARGS)
