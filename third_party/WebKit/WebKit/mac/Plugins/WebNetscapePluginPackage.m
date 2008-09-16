70cd750ead41fdfb0d3f3d087598112c587cb8a5
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-16 09:15:38 +0000

    2008-09-16  Anders Carlsson  <andersca@apple.com>
            Reviewed by Dave Hyatt.
    
            Instead of storing a pointer to NPP method individually, just store a pointer to the NPNetscapeFuncs vtable.
            
            * Plugins/WebBaseNetscapePluginStream.h:
            * Plugins/WebBaseNetscapePluginStream.mm:
            (-[WebBaseNetscapePluginStream setPlugin:]):
            (-[WebBaseNetscapePluginStream startStreamResponseURL:expectedContentLength:lastModifiedDate:MIMEType:headers:]):
            (-[WebBaseNetscapePluginStream wantsAllStreams]):
            (-[WebBaseNetscapePluginStream _destroyStream]):
            (-[WebBaseNetscapePluginStream _deliverData]):
            * Plugins/WebNetscapePluginPackage.h:
            * Plugins/WebNetscapePluginPackage.m:
            (-[WebNetscapePluginPackage pluginFuncs]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36492 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginPackage.m
+++ third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginPackage.m
@@ -679,6 +679,11 @@ abort:
     return NPP_Print;
 }
 
+- (NPPluginFuncs *)pluginFuncs
+{
+    return &pluginFuncs;
+}
+
 - (void)wasRemovedFromPluginDatabase:(WebPluginDatabase *)database
 {
     [super wasRemovedFromPluginDatabase:database];
