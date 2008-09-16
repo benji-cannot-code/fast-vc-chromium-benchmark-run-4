d0abf10cd910a033b07588f24d85c081767d9e10
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-16 14:32:44 +0000

    Add an HMAC implementation using NSS.  Include centralized NSS initialization, based on code from phajdan.jr.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2260 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/nss_init.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/nss_init.h"
+
+#include "base/logging.h"
+#include "base/singleton.h"
+
+// Include this header last, since NSS will define "Lock" in an enum.
+//   https://bugzilla.mozilla.org/show_bug.cgi?id=455424
+#include <nss.h>
+
+namespace base {
+
+namespace {
+
+class NSSInitSingleton {
+ public:
+  NSSInitSingleton() {
+    CHECK(NSS_NoDB_Init(".") == SECSuccess);
+  }
+
+  ~NSSInitSingleton() {
+    NSS_Shutdown();
+  }
+};
+
+}  // namespace
+
+void EnsureNSSInit() {
+  Singleton<NSSInitSingleton>::get();
+}
+
+}  // namespace base
