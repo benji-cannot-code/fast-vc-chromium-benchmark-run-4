8e5176a1e6911f053b811cac0c1da2bec05507d8
avi@google.com <avi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-16 19:17:54 +0000

    More linkage (stubbing out willCacheResponse()) for now
    Review URL: http://codereview.chromium.org/2900
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2271 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webframeloaderclient_impl.h
+++ webkit/glue/webframeloaderclient_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoaderClient.h"
 #pragma warning(pop)
 
+#include "build/build_config.h"
 #include "googleurl/src/gurl.h"
 #include "webkit/glue/webview_delegate.h"
 #include "webkit/glue/window_open_disposition.h"
@@ -200,7 +201,7 @@ class WebFrameLoaderClient : public WebCore::FrameLoaderClient {
 
   virtual void unloadListenerChanged();
 
-#if defined(__APPLE__)
+#if defined(OS_MACOSX)
 // The above should have && !defined(BUILDING_CHROMIUM__) at the end but can't
 // for now, since we need to add that extra define all the way down to the
 // WebCore core. TODO(avi): Get that define into WebCore.
