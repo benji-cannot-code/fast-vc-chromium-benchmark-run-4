a6202a9a248109053e7988dba1868110cc0ccfda
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-17 05:50:48 +0000

    Fix gtk bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/ScrollbarGtk.h
+++ third_party/WebKit/WebCore/platform/gtk/ScrollbarGtk.h
@@ -36,6 +36,8 @@ namespace WebCore {
 
 class ScrollbarGtk : public Scrollbar {
 public:
+    friend class Scrollbar;
+
     virtual ~ScrollbarGtk();
 
     virtual void setFrameGeometry(const IntRect&);
