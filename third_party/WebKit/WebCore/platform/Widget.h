b61ac4eb9346ae0b233387a4cb7ba10ee3af7413
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-17 22:07:50 +0000

    Fix wx bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36562 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Widget.h
+++ third_party/WebKit/WebCore/platform/Widget.h
@@ -211,12 +211,6 @@ public:
     IntRect convertToContainingWindow(const IntRect&) const;
 #endif
 
-#if PLATFORM(WX)
-    Widget(wxWindow*);
-    wxWindow* nativeWindow() const;
-    virtual void setNativeWindow(wxWindow*);
-#endif
-
 private:
     void init(); // Must be called by all Widget constructors to initialize cross-platform data.
 
