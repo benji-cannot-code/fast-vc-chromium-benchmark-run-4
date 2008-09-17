f6ac3f2cabc51a9761c3df2db8ef40eccf8741cd
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-17 06:32:23 +0000

    Fix Qt bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36537 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QPainter>
 #include <QClipboard>
 #include <QSslSocket>
+#include <QStyle>
 #include <QSysInfo>
 #if QT_VERSION >= 0x040400
 #include <QNetworkAccessManager>
