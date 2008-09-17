921ebb70425d04047bacd3425583ea204bc44df9
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-17 22:21:48 +0000

    Fix Qt bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
@@ -1092,7 +1092,7 @@ Widget* FrameLoaderClientQt::createPlugin(const IntSize& pluginSize, Element* el
             if (widget && view) {
                 widget->setParent(view);
                 Widget* w= new Widget();
-                w->setNativeWidget(widget);
+                w->setPlatformWidget(widget);
                 return w;
             }
             // FIXME: make things work for widgetless plugins as well
