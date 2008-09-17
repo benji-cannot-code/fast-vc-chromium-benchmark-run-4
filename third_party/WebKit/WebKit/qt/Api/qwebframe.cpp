23bb6ea59b501abd278d477977715fcbd63d00d3
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-17 05:06:36 +0000

    2008-09-16  Alp Toker  <alp@nuanti.com>
            Prospective Qt build fix. ScrollBar.h -> Scrollbar.h
    
            * Api/qwebframe.cpp:
            * Api/qwebpage.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36535 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceRequest.h"
 #include "RenderView.h"
 #include "SelectionController.h"
-#include "ScrollBar.h"
+#include "Scrollbar.h"
 #include "PrintContext.h"
 #include "SubstituteData.h"
 
