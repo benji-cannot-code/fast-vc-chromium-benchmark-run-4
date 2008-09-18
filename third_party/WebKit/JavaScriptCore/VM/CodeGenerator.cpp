1db23e28c79370ed694fbaef0e0adbb7941bf7a4
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-18 06:20:42 +0000

    2008-09-17  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
            Reviewed by Maciej Stachowiak.
    
            Bug 20876: REGRESSION (r36417, r36427): fast/js/exception-expression-offset.html fails
            <https://bugs.webkit.org/show_bug.cgi?id=20876>
    
            r36417 and r36427 caused an get_by_id opcode to be emitted before the
            instanceof and construct opcodes, in order to enable inline caching of
            the prototype property. Unfortunately, this regressed some tests dealing
            with exceptions thrown by 'instanceof' and the 'new' operator. We fix
            these problems by detecting whether an "is not an object" exception is
            thrown before op_instanceof or op_construct, and emit the proper
            exception in those cases.
    
            * VM/CodeGenerator.cpp:
            (JSC::CodeGenerator::emitConstruct):
            * VM/CodeGenerator.h:
            * VM/ExceptionHelpers.cpp:
            (JSC::createInvalidParamError):
            (JSC::createNotAConstructorError):
            (JSC::createNotAnObjectError):
            * VM/ExceptionHelpers.h:
            * VM/Machine.cpp:
            (JSC::Machine::getOpcode):
            (JSC::Machine::privateExecute):
            * VM/Machine.h:
            * kjs/nodes.cpp:
            (JSC::NewExprNode::emitCode):
            (JSC::InstanceOfNode::emitCode):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36604 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/CodeGenerator.cpp
+++ third_party/WebKit/JavaScriptCore/VM/CodeGenerator.cpp
@@ -1110,7 +1110,7 @@ RegisterID* CodeGenerator::emitUnaryNoDstOp(OpcodeID opcode, RegisterID* src)
     return src;
 }
 
-RegisterID* CodeGenerator::emitConstruct(RegisterID* dst, RegisterID* func, ArgumentsNode* argumentsNode)
+RegisterID* CodeGenerator::emitConstruct(RegisterID* dst, RegisterID* func, ArgumentsNode* argumentsNode, unsigned divot, unsigned startOffset, unsigned endOffset)
 {
     ASSERT(func->refCount());
 
@@ -1130,8 +1130,10 @@ RegisterID* CodeGenerator::emitConstruct(RegisterID* dst, RegisterID* func, Argu
         emitNode(argv.last().get(), n);
     }
 
+    emitExpressionInfo(divot, startOffset, endOffset);
     emitGetById(funcProto.get(), func, globalExec()->propertyNames().prototype);
 
+    emitExpressionInfo(divot, startOffset, endOffset);
     emitOpcode(op_construct);
     instructions().append(dst->index());
     instructions().append(func->index());
