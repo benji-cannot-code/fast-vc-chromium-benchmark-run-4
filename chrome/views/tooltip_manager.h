19b13d95ecac3db04b136c099d7e5dfb9004a27e
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-19 15:25:12 +0000

    Fixes bug in tooltip manager. Mouse coordinates for NC events are in screen coordinates and need to be converted.
    BUG=2523
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/3160
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2406 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/tooltip_manager.h
+++ chrome/views/tooltip_manager.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_VIEWS_TOOLTIP_MANAGER_H__
-#define CHROME_VIEWS_TOOLTIP_MANAGER_H__
+#ifndef CHROME_VIEWS_TOOLTIP_MANAGER_H_
+#define CHROME_VIEWS_TOOLTIP_MANAGER_H_
 
 #include <windows.h>
 #include <string>
@@ -161,5 +161,4 @@ class TooltipManager {
 
 } // namespace ChromeViews
 
-#endif // CHROME_VIEWS_TOOLTIP_MANAGER_H__
-
+#endif // CHROME_VIEWS_TOOLTIP_MANAGER_H_
