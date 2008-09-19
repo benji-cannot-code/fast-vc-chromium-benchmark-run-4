b52c861720c7c7cc9fef1a24ae45e755645d2c74
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-19 22:36:33 +0000

    Stop exposing manually_selected_match_ outside the AutocompletePopupModel.  The main goal of this is to be a first step towards divorcing the popup and the edit from each other.
    To do this, I changed the behavior of manual selections.  They now do not persist once the user types more characters, hits esc, etc.  Our old behavior, which Brett and I designed long ago, turns out to have been a mistake; users who arrowed to an item and then typed more weren't expecting "stickiness" on their previous choice, and it led to user mistakes.
    
    This also required changing how we do the "keyword UI" persistence in the case where the user switches into keyword UI, but then deletes all his text.  Previously, we used manually_selected_match_ with a provider affinity to the keyword provider in order to accomplish this.  Now we stick another flag on the AutocompleteInput, which, when set, biases the keyword provider to return the best results.  The user-visible effect of this is that when in keyword UI mode with no query string, the selected entry in the popup will be the first, rather than third, entry.  This is a small win.
    
    While here I fixed the bug where editing a string and transforming it into a keyword search would avoid switching into keyword UI (as expected), but also delete the keyword off the visible string (oops).  I also made us lock the popup once the user changes the manually_selected_match_, in order to give a little more stability to it.
    
    I'm sorry this makes so many behavioral changes at once.  All this code is tangled together and untangling it is hard :(
    
    The keyword-related variables in the AutocompleteEditModel seem a mess.  They are probably worse now than before this change; I think I need a followup change at some point to make them all more sane.  It seems like we have three variables and complex conditionals where two, and simpler ones, would do.
    
    BUG=997976,1201974,1204173
    Review URL: http://codereview.chromium.org/3172
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2426 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete.h
+++ chrome/browser/autocomplete/autocomplete.h
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // UNKNOWN input type:
 // --------------------------------------------------------------------|-----
-// Keyword (non-substituting, exact match)                             | 1500
+// Keyword (non-substituting or in keyword UI mode, exact match)       | 1500
 // HistoryURL (exact or inline autocomplete match)                     | 1400
 // Search (what you typed)                                             | 1300
 // HistoryURL (what you typed)                                         | 1200
@@ -60,7 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // REQUESTED_URL input type:
 // --------------------------------------------------------------------|-----
-// Keyword (non-substituting, exact match)                             | 1500
+// Keyword (non-substituting or in keyword UI mode, exact match)       | 1500
 // HistoryURL (exact or inline autocomplete match)                     | 1400
 // HistoryURL (what you typed)                                         | 1300
 // Search (what you typed)                                             | 1200
@@ -77,7 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // URL input type:
 // --------------------------------------------------------------------|-----
-// Keyword (non-substituting, exact match)                             | 1500
+// Keyword (non-substituting or in keyword UI mode, exact match)       | 1500
 // HistoryURL (exact or inline autocomplete match)                     | 1400
 // HistoryURL (what you typed)                                         | 1200
 // Keyword (substituting, exact match)                                 | 1100
@@ -90,7 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // QUERY input type:
 // --------------------------------------------------------------------|-----
-// Keyword (non-substituting, exact match)                             | 1500
+// Keyword (non-substituting or in keyword UI mode, exact match)       | 1500
 // Keyword (substituting, exact match)                                 | 1400
 // Search (what you typed)                                             | 1300
 // Search (past query in history)                                      | 1250--
@@ -150,10 +150,16 @@ class AutocompleteInput {
     FORCED_QUERY,  // Input forced to be a query by an initial '?'
   };
 
-  AutocompleteInput() : type_(INVALID), prevent_inline_autocomplete_(false) {}
+  AutocompleteInput()
+      : type_(INVALID),
+        prevent_inline_autocomplete_(false),
+        prefer_keyword_(false) {
+  }
+  
   AutocompleteInput(const std::wstring& text,
                     const std::wstring& desired_tld,
-                    bool prevent_inline_autocomplete);
+                    bool prevent_inline_autocomplete,
+                    bool prefer_keyword);
 
   // Parses |text| and returns the type of input this will be interpreted as.
   // The components of the input are stored in the output parameter |parts|.
@@ -188,6 +194,10 @@ class AutocompleteInput {
     return prevent_inline_autocomplete_;
   }
 
+  // Returns whether, given an input string consisting solely of a substituting
+  // keyword, we should score it like a non-substituting keyword.
+  const bool prefer_keyword() const { return prefer_keyword_; }
+
   // operator==() by another name.
   bool Equals(const AutocompleteInput& other) const;
 
@@ -204,6 +214,7 @@ class AutocompleteInput {
   std::wstring scheme_;
   std::wstring desired_tld_;
   bool prevent_inline_autocomplete_;
+  bool prefer_keyword_;
 };
 
 // AutocompleteMatch ----------------------------------------------------------
@@ -572,14 +583,14 @@ class AutocompleteResult {
 
   // Adds a single match. The match is inserted at the appropriate position
   // based on relevancy and display order. This is ONLY for use after
-  // SortAndCull has been invoked.
+  // SortAndCull() has been invoked, and preserves default_match_.
   void AddMatch(const AutocompleteMatch& match);
 
-  // Adds a new set of matches to the set of results.
+  // Adds a new set of matches to the set of results.  Does not re-sort.
   void AppendMatches(const ACMatches& matches);
 
   // Removes duplicates, puts the list in sorted order and culls to leave only
-  // the best kMaxMatches results.
+  // the best kMaxMatches results.  Sets the default match to the best match.
   void SortAndCull();
 
   // Vector-style accessors/operators.
@@ -599,14 +610,6 @@ class AutocompleteResult {
   // Get the default match for the query (not necessarily the first).  Returns
   // end() if there is no default match.
   const_iterator default_match() const { return default_match_; }
-
-  // Sets the default match to the best result.  When a particular URL is
-  // desired, we return that if available; otherwise, if a provider affinity is
-  // specified, we pick the most relevant match from that provider; otherwise,
-  // we return the best match overall.
-  // Returns true if the selection specified an exact match and we were able to
-  // find and use it.
-  bool SetDefaultMatch(const Selection& selection);
 
   // Given some input and a particular match in this result set, returns the
   // "alternate navigation URL", if any, for that match.  This is a URL to try
@@ -680,7 +683,6 @@ class AutocompleteController : public ACProviderListener {
                          const ACProviders& providers)
       : listener_(listener),
         providers_(providers),
-        keyword_provider_(NULL),
         history_contents_provider_(NULL) {
   }
 #endif
@@ -722,8 +724,6 @@ class AutocompleteController : public ACProviderListener {
   // From AutocompleteProvider::Listener
   virtual void OnProviderUpdate(bool updated_matches);
 
-  KeywordProvider* keyword_provider() const { return keyword_provider_; }
-
  private:
   // Returns true if all providers have finished processing the query.
   bool QueryComplete() const;
@@ -744,8 +744,6 @@ class AutocompleteController : public ACProviderListener {
 
   // A list of all providers.
   ACProviders providers_;
-
-  KeywordProvider* keyword_provider_;
 
   HistoryContentsProvider* history_contents_provider_;
 
