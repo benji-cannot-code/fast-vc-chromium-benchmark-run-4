dc75ae9fbf3722b9e76f79850d321f9b2141aa20
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-19 04:30:53 +0000

    2008-09-18  David Hyatt  <hyatt@apple.com>
            Eliminate addToSuperview from Widget, since it was only called
            by ScrollViewMac's addChild method.  Just shift the original body
            of addToSuperView into addChild.
    
            Reviewed by Sam Weinig
    
            * platform/Widget.h:
            * platform/mac/ScrollViewMac.mm:
            (WebCore::ScrollView::addChild):
            * platform/mac/WidgetMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36651 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Widget.h
+++ third_party/WebKit/WebCore/platform/Widget.h
@@ -181,7 +181,6 @@ public:
     static void beforeMouseDown(NSView*, Widget*);
     static void afterMouseDown(NSView*, Widget*);
 
-    void addToSuperview(NSView* superview);
     void removeFromSuperview();
 #endif
 
