0b0dbe6eed83d505f5bae9a24d069cd7791d4f26
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-20 00:54:40 +0000

    Fix build bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36688 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/ScrollbarThemeWin.cpp
+++ third_party/WebKit/WebCore/platform/win/ScrollbarThemeWin.cpp
@@ -250,7 +250,7 @@ void ScrollbarThemeWin::paintButton(GraphicsContext* context, Scrollbar* scrollb
 {
     checkAndInitScrollbarTheme();
 
-    bool start = (part == BackButtonPart);
+    bool start = (part == BackButtonStartPart);
     int xpState = 0;
     int classicState = 0;
     if (scrollbar->orientation() == HorizontalScrollbar)
