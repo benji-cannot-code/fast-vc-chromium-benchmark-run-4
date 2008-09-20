82b92608ff16224b5b086c2550dd6500b2d5d5e4
paulg@google.com <paulg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-20 02:03:08 +0000

    Create a SafeBrowsing database interface to more easily facilitate alternate implementations.
    The current SafeBrowsingDatabase code is moved to *_impl files.
    Adding a new implementation can be done via command line flags
    in the SafeBrowsingDatabase::Create factory method.
    Review URL: http://codereview.chromium.org/3162
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2434 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/bloom_filter.h
+++ chrome/browser/safe_browsing/bloom_filter.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // A simple bloom filter.  It's currently limited to four hashing functions,
 // which are calculated from the item's hash.
 
+#ifndef CHROME_BROWSER_SAFE_BROWSING_BLOOM_FILTER_H__
+#define CHROME_BROWSER_SAFE_BROWSING_BLOOM_FILTER_H__
+
 #include "base/scoped_ptr.h"
 #include "base/basictypes.h"
 
@@ -33,3 +36,4 @@ class BloomFilter {
   scoped_array<char> data_;
 };
 
+#endif  // CHROME_BROWSER_SAFE_BROWSING_BLOOM_FILTER_H__
--- chrome/browser/safe_browsing/protocol_manager.cc
+++ chrome/browser/safe_browsing/protocol_manager.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/safe_browsing/protocol_parser.h"
-#include "chrome/browser/safe_browsing/safe_browsing_database.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/common/env_util.h"
 #include "chrome/common/env_vars.h"
