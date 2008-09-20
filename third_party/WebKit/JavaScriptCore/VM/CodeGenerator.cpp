b85c17b3e8a58c1fbc5152a154c44c1024d0dc55
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-20 03:00:43 +0000

    Improve peformance of local variable initialisation.
    Reviewed by Maciej Stachowiak
    
    Pull local and constant initialisation out of slideRegisterWindowForCall
    and into its own opcode.  This allows the JIT to generate the initialisation
    code for a function directly into the instruction stream and so avoids a few
    branches on function entry.
    
    Results a 1% progression in SunSpider, particularly in a number of the bitop
    tests where the called functions are very fast.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36695 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/CodeGenerator.cpp
+++ third_party/WebKit/JavaScriptCore/VM/CodeGenerator.cpp
@@ -269,6 +269,7 @@ CodeGenerator::CodeGenerator(FunctionBodyNode* functionBody, const Debugger* deb
     , m_globalData(&scopeChain.globalObject()->globalExec()->globalData())
     , m_lastOpcodeID(op_end)
 {
+    emitOpcode(op_initialise_locals);
     codeBlock->globalData = m_globalData;
 
     m_codeBlock->numConstants = functionBody->neededConstants();
@@ -317,6 +318,7 @@ CodeGenerator::CodeGenerator(EvalNode* evalNode, const Debugger* debugger, const
     , m_globalData(&scopeChain.globalObject()->globalExec()->globalData())
     , m_lastOpcodeID(op_end)
 {
+    emitOpcode(op_initialise_locals);
     codeBlock->globalData = m_globalData;
 
     m_codeBlock->numConstants = evalNode->neededConstants();
