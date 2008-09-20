f3e32f2fa8173a6c22ea4ac2a68b44d7cbb1506b
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-20 02:23:42 +0000

    2008-09-19  Sam Weinig  <sam@webkit.org>
            Reviewed by Darin Adler.
    
            Patch for https://bugs.webkit.org/show_bug.cgi?id=20928
            Speed up JS property enumeration by caching entire PropertyNameArray
    
            1.3% speedup on Sunspider, 30% on string-fasta.
    
            * JavaScriptCore.exp:
            * VM/JSPropertyNameIterator.cpp:
            (JSC::JSPropertyNameIterator::~JSPropertyNameIterator):
            (JSC::JSPropertyNameIterator::invalidate):
            * VM/JSPropertyNameIterator.h:
            (JSC::JSPropertyNameIterator::JSPropertyNameIterator):
            (JSC::JSPropertyNameIterator::create):
            * kjs/JSObject.cpp:
            (JSC::JSObject::getPropertyNames):
            * kjs/PropertyMap.cpp:
            (JSC::PropertyMap::getEnumerablePropertyNames):
            * kjs/PropertyMap.h:
            * kjs/PropertyNameArray.cpp:
            (JSC::PropertyNameArray::add):
            * kjs/PropertyNameArray.h:
            (JSC::PropertyNameArrayData::create):
            (JSC::PropertyNameArrayData::propertyNameVector):
            (JSC::PropertyNameArrayData::setCachedPrototypeChain):
            (JSC::PropertyNameArrayData::cachedPrototypeChain):
            (JSC::PropertyNameArrayData::begin):
            (JSC::PropertyNameArrayData::end):
            (JSC::PropertyNameArrayData::PropertyNameArrayData):
            (JSC::PropertyNameArray::PropertyNameArray):
            (JSC::PropertyNameArray::addKnownUnique):
            (JSC::PropertyNameArray::size):
            (JSC::PropertyNameArray::operator[]):
            (JSC::PropertyNameArray::begin):
            (JSC::PropertyNameArray::end):
            (JSC::PropertyNameArray::setData):
            (JSC::PropertyNameArray::data):
            (JSC::PropertyNameArray::releaseData):
            * kjs/ScopeChain.cpp:
            (JSC::ScopeChainNode::print):
            * kjs/StructureID.cpp:
            (JSC::structureIDChainsAreEqual):
            (JSC::StructureID::getEnumerablePropertyNames):
            (JSC::StructureID::clearEnumerationCache):
            (JSC::StructureID::createCachedPrototypeChain):
            * kjs/StructureID.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36694 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/JSPropertyNameIterator.cpp
+++ third_party/WebKit/JavaScriptCore/VM/JSPropertyNameIterator.cpp
@@ -36,7 +36,6 @@ ASSERT_CLASS_FITS_IN_CELL(JSPropertyNameIterator);
 
 JSPropertyNameIterator::~JSPropertyNameIterator()
 {
-    invalidate();
 }
 
 JSValue* JSPropertyNameIterator::toPrimitive(ExecState*, PreferredPrimitiveType) const
@@ -84,13 +83,8 @@ void JSPropertyNameIterator::mark()
 
 void JSPropertyNameIterator::invalidate()
 {
-    if (m_propertyNames) {
-        for (Identifier* p = m_propertyNames; p != m_end; ++p)
-            p->~Identifier();
-        fastFree(m_propertyNames);
-    }
     m_object = 0;
-    m_propertyNames = 0;
+    m_data.clear();
 }
 
 } // namespace JSC
--- third_party/WebKit/JavaScriptCore/kjs/PropertyMap.h
+++ third_party/WebKit/JavaScriptCore/kjs/PropertyMap.h
@@ -31,6 +31,7 @@ namespace JSC {
 
     class JSObject;
     class JSValue;
+    class PropertyNameArray;
 
     typedef JSValue** PropertyStorage;
 
@@ -94,7 +95,7 @@ namespace JSC {
         size_t getOffset(const Identifier& propertyName);
         size_t getOffset(const Identifier& propertyName, unsigned& attributes);
 
-        void getEnumerablePropertyNames(Vector<UString::Rep*>&) const;
+        void getEnumerablePropertyNames(PropertyNameArray&) const;
 
         bool hasGetterSetterProperties() const { return m_getterSetterFlag; }
         void setHasGetterSetterProperties(bool f) { m_getterSetterFlag = f; }
