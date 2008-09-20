8b4854b6e51ac29c6b1a474c3435acaa0b9fd7cd
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-20 04:35:33 +0000

    2008-09-19  Sam Weinig  <sam@webkit.org>
            Roll r36694 back in.  It did not cause the crash.
    
            * JavaScriptCore.exp:
            * VM/JSPropertyNameIterator.cpp:
            (JSC::JSPropertyNameIterator::~JSPropertyNameIterator):
            (JSC::JSPropertyNameIterator::invalidate):
            * VM/JSPropertyNameIterator.h:
            (JSC::JSPropertyNameIterator::JSPropertyNameIterator):
            (JSC::JSPropertyNameIterator::create):
            * kjs/JSObject.cpp:
            (JSC::JSObject::getPropertyNames):
            * kjs/PropertyMap.cpp:
            (JSC::PropertyMap::getEnumerablePropertyNames):
            * kjs/PropertyMap.h:
            * kjs/PropertyNameArray.cpp:
            (JSC::PropertyNameArray::add):
            * kjs/PropertyNameArray.h:
            (JSC::PropertyNameArrayData::create):
            (JSC::PropertyNameArrayData::propertyNameVector):
            (JSC::PropertyNameArrayData::setCachedPrototypeChain):
            (JSC::PropertyNameArrayData::cachedPrototypeChain):
            (JSC::PropertyNameArrayData::begin):
            (JSC::PropertyNameArrayData::end):
            (JSC::PropertyNameArrayData::PropertyNameArrayData):
            (JSC::PropertyNameArray::PropertyNameArray):
            (JSC::PropertyNameArray::addKnownUnique):
            (JSC::PropertyNameArray::size):
            (JSC::PropertyNameArray::operator[]):
            (JSC::PropertyNameArray::begin):
            (JSC::PropertyNameArray::end):
            (JSC::PropertyNameArray::setData):
            (JSC::PropertyNameArray::data):
            (JSC::PropertyNameArray::releaseData):
            * kjs/StructureID.cpp:
            (JSC::structureIDChainsAreEqual):
            (JSC::StructureID::getEnumerablePropertyNames):
            (JSC::StructureID::clearEnumerationCache):
            (JSC::StructureID::createCachedPrototypeChain):
            * kjs/StructureID.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36701 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/PropertyMap.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/PropertyMap.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PropertyMap.h"
 
 #include "JSObject.h"
+#include "PropertyNameArray.h"
 #include "protect.h"
 #include <algorithm>
 #include <wtf/Assertions.h>
@@ -473,7 +474,7 @@ static int comparePropertyMapEntryIndices(const void* a, const void* b)
     return 0;
 }
 
-void PropertyMap::getEnumerablePropertyNames(Vector<UString::Rep*>& propertyNames) const
+void PropertyMap::getEnumerablePropertyNames(PropertyNameArray& propertyNames) const
 {
     if (!m_table)
         return;
@@ -492,9 +493,14 @@ void PropertyMap::getEnumerablePropertyNames(Vector<UString::Rep*>& propertyName
                 ++i;
             }
         }
-        propertyNames.reserveCapacity(i);
-        for (int k = 0; k < i; ++k)
-            propertyNames.append(a[k]->key);
+        if (!propertyNames.size()) {
+            for (int k = 0; k < i; ++k)
+                propertyNames.addKnownUnique(a[k]->key);
+        } else {
+            for (int k = 0; k < i; ++k)
+                propertyNames.add(a[k]->key);
+        }
+
         return;
     }
 
@@ -515,9 +521,13 @@ void PropertyMap::getEnumerablePropertyNames(Vector<UString::Rep*>& propertyName
     sortedEnumerables.resize(enumerableCount);
 
     // Put the keys of the sorted entries into the list.
-    propertyNames.reserveCapacity(sortedEnumerables.size());
-    for (size_t i = 0; i < sortedEnumerables.size(); ++i)
-        propertyNames.append(sortedEnumerables[i]->key);
+    if (!propertyNames.size()) {
+        for (size_t i = 0; i < sortedEnumerables.size(); ++i)
+            propertyNames.addKnownUnique(sortedEnumerables[i]->key);
+    } else {
+        for (size_t i = 0; i < sortedEnumerables.size(); ++i)
+            propertyNames.add(sortedEnumerables[i]->key);
+    }
 }
 
 #if DO_PROPERTYMAP_CONSTENCY_CHECK
