18996275b0cd93860b58f9bf530bc5f32dac6031
pewtermoose@webkit.org <pewtermoose@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-22 21:59:45 +0000

    2008-09-22  Matt Lilek  <webkit@mattlilek.com>
            Not reviewed, Windows build fix.
    
            * kjs/Arguments.cpp:
            * kjs/FunctionPrototype.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/Arguments.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/Arguments.cpp
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSFunction.h"
 #include "JSGlobalObject.h"
 
+using namespace std;
+
 namespace JSC {
 
 ASSERT_CLASS_FITS_IN_CELL(Arguments);
