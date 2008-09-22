77f9f76dd101c13ebd5a6357207120a85c128d14
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-22 00:12:14 +0000

    Fix the non-AllInOneFile build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/Arguments.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/Arguments.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Arguments.h"
 
 #include "JSActivation.h"
+#include "JSFunction.h"
 #include "JSGlobalObject.h"
 
 namespace JSC {
