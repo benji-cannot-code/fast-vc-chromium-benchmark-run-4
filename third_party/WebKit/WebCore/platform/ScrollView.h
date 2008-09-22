cad7d129ed9c11b97dc5408beb5f279618fb988a
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-22 08:53:06 +0000

    2008-09-22  Alp Toker  <alp@nuanti.com>
            Suggested by David Hyatt.
    
            Build fix: ScrollView::update() is still used by Document.cpp on !MAC
            so make it public.
    
            * platform/ScrollView.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36761 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ScrollView.h
+++ third_party/WebKit/WebCore/platform/ScrollView.h
@@ -143,6 +143,7 @@ namespace WebCore {
     protected:
         void updateContents(const IntRect&, bool now = false);
         void updateWindowRect(const IntRect&, bool now = false);
+    public:
         void update();
 
 #if PLATFORM(MAC) && defined __OBJC__
