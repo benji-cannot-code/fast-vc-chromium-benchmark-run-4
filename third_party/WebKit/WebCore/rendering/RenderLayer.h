6be6e2ebd633b16c75c5f59b8178f6f0ec2bd75c
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-22 00:33:19 +0000

    2008-09-20  David Hyatt  <hyatt@apple.com>
            Make sure transformed scrollbars in overflow sections position
            properly.  This patch mimics the same behavior that works for
            iframes, namely making sure that the same code that dynamically
            adjusts iframe widget positions at paint time for fixed positioning
            and transforms also applies to scrollbars.  (This is as simple as passing
            in the current translation factor at paint time rather than crawling
            up the layer tree to compute a "false" absolute position.)
    
            An existing transform test covers this (although only a pixel result
            reveals the correct rendering).
    
            Reviewed by Darin Adler
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::updateLayerPositions):
            (WebCore::RenderLayer::positionOverflowControls):
            (WebCore::RenderLayer::paintOverflowControls):
            (WebCore::RenderLayer::paintLayer):
            * rendering/RenderLayer.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.h
+++ third_party/WebKit/WebCore/rendering/RenderLayer.h
@@ -229,7 +229,7 @@ public:
     int verticalScrollbarWidth() const;
     int horizontalScrollbarHeight() const;
 
-    void positionOverflowControls();
+    void positionOverflowControls(int tx, int ty);
     bool isPointInResizeControl(const IntPoint&);
     bool hitTestOverflowControls(HitTestResult&);
     IntSize offsetFromResizeCorner(const IntPoint&) const;
