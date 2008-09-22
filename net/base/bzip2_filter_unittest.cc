c5de47a4b64ab8a5433bf8e8a57b311cc6fb4c20
jar@google.com <jar@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-22 17:24:28 +0000

    Experimental integration of delta compression content encoding
    The command line option "sdch-enable" enables support of sdch
    and automtic lazy download of dictionaries.  Optionally it
    can select a singular domain to work from.
    
    By default, all domains are enabled when the flag is used.
    "-sdch-enable=".google.com"  Enables it only for Google.
    
    When the switch is not set on the command line, all this
    code is completely disabled.
    
    Still TBD:
    a) Finish implementation of security details (much of it is in place)
    b) Add tests for security details.
    
    
    r=huanr,ajenjo,kmixter
    
    Review URL: http://codereview.chromium.org/461
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2443 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/bzip2_filter_unittest.cc
+++ net/base/bzip2_filter_unittest.cc
@@ -93,8 +93,8 @@ class BZip2FilterUnitTest : public PlatformTest {
   // Encoded_source and encoded_source_len are compressed data and its size.
   // Output_buffer_size specifies the size of buffer to read out data from
   // filter.
-  // get_extra_data specifies whether get the extra data because maybe some server
-  // might send extra data after finish sending compress data
+  // get_extra_data specifies whether get the extra data because maybe some
+  // server might send extra data after finish sending compress data.
   void DecodeAndCompareWithFilter(Filter* filter,
                                   const char* source,
                                   int source_len,
@@ -131,7 +131,7 @@ class BZip2FilterUnitTest : public PlatformTest {
       while (1) {
         int decode_data_len = std::min(decode_avail_size, output_buffer_size);
 
-        code = filter->ReadFilteredData(decode_next, &decode_data_len);
+        code = filter->ReadData(decode_next, &decode_data_len);
         decode_next += decode_data_len;
         decode_avail_size -= decode_data_len;
 
@@ -165,11 +165,13 @@ class BZip2FilterUnitTest : public PlatformTest {
                                            int* dest_len) {
     memcpy(filter->stream_buffer(), source, source_len);
     filter->FlushStreamBuffer(source_len);
-    return filter->ReadFilteredData(dest, dest_len);
+    return filter->ReadData(dest, dest_len);
   }
 
   const char* source_buffer() const { return source_buffer_.data(); }
-  int source_len() const { return static_cast<int>(source_buffer_.size()) - kExtraDataBufferSize; }
+  int source_len() const {
+    return static_cast<int>(source_buffer_.size()) - kExtraDataBufferSize;
+  }
 
   std::string source_buffer_;
 
@@ -181,8 +183,10 @@ class BZip2FilterUnitTest : public PlatformTest {
 // Basic scenario: decoding bzip2 data with big enough buffer.
 TEST_F(BZip2FilterUnitTest, DecodeBZip2) {
   // Decode the compressed data with filter
+  std::vector<std::string> filters;
+  filters.push_back("bzip2");
   scoped_ptr<Filter> filter(
-      Filter::Factory("bzip2", kApplicationOctetStream, kDefaultBufferSize));
+      Filter::Factory(filters, kApplicationOctetStream, kDefaultBufferSize));
   ASSERT_TRUE(filter.get());
   memcpy(filter->stream_buffer(), bzip2_encode_buffer_, bzip2_encode_len_);
   filter->FlushStreamBuffer(bzip2_encode_len_);
@@ -190,7 +194,7 @@ TEST_F(BZip2FilterUnitTest, DecodeBZip2) {
   char bzip2_decode_buffer[kDefaultBufferSize];
   int bzip2_decode_size = kDefaultBufferSize;
   Filter::FilterStatus result =
-    filter->ReadFilteredData(bzip2_decode_buffer, &bzip2_decode_size);
+      filter->ReadData(bzip2_decode_buffer, &bzip2_decode_size);
   ASSERT_EQ(Filter::FILTER_DONE, result);
 
   // Compare the decoding result with source data
@@ -202,8 +206,10 @@ TEST_F(BZip2FilterUnitTest, DecodeBZip2) {
 // To do that, we create a filter with a small buffer that can not hold all
 // the input data.
 TEST_F(BZip2FilterUnitTest, DecodeWithSmallInputBuffer) {
+  std::vector<std::string> filters;
+  filters.push_back("bzip2");
   scoped_ptr<Filter> filter(
-      Filter::Factory("bzip2", kApplicationOctetStream, kSmallBufferSize));
+      Filter::Factory(filters, kApplicationOctetStream, kSmallBufferSize));
   ASSERT_TRUE(filter.get());
   DecodeAndCompareWithFilter(filter.get(), source_buffer(), source_len(),
                              bzip2_encode_buffer_, bzip2_encode_len_,
@@ -212,8 +218,10 @@ TEST_F(BZip2FilterUnitTest, DecodeWithSmallInputBuffer) {
 
 // Tests we can decode when caller has small buffer to read out from filter.
 TEST_F(BZip2FilterUnitTest, DecodeWithSmallOutputBuffer) {
+  std::vector<std::string> filters;
+  filters.push_back("bzip2");
   scoped_ptr<Filter> filter(
-        Filter::Factory("bzip2", kApplicationOctetStream, kDefaultBufferSize));
+        Filter::Factory(filters, kApplicationOctetStream, kDefaultBufferSize));
   ASSERT_TRUE(filter.get());
   DecodeAndCompareWithFilter(filter.get(), source_buffer(), source_len(),
                              bzip2_encode_buffer_, bzip2_encode_len_,
@@ -225,8 +233,10 @@ TEST_F(BZip2FilterUnitTest, DecodeWithSmallOutputBuffer) {
 // header correctly. (2) Sometimes the filter will consume input without
 // generating output. Verify filter can handle it correctly.
 TEST_F(BZip2FilterUnitTest, DecodeWithOneByteInputBuffer) {
+  std::vector<std::string> filters;
+  filters.push_back("bzip2");
   scoped_ptr<Filter> filter(
-        Filter::Factory("bzip2", kApplicationOctetStream, 1));
+        Filter::Factory(filters, kApplicationOctetStream, 1));
   ASSERT_TRUE(filter.get());
   DecodeAndCompareWithFilter(filter.get(), source_buffer(), source_len(),
                              bzip2_encode_buffer_, bzip2_encode_len_,
@@ -236,8 +246,10 @@ TEST_F(BZip2FilterUnitTest, DecodeWithOneByteInputBuffer) {
 // Tests we can still decode with just 1 byte buffer in the filter and just 1
 // byte buffer in the caller.
 TEST_F(BZip2FilterUnitTest, DecodeWithOneByteInputAndOutputBuffer) {
+  std::vector<std::string> filters;
+  filters.push_back("bzip2");
   scoped_ptr<Filter> filter(
-      Filter::Factory("bzip2", kApplicationOctetStream, 1));
+      Filter::Factory(filters, kApplicationOctetStream, 1));
   ASSERT_TRUE(filter.get());
   DecodeAndCompareWithFilter(filter.get(), source_buffer(), source_len(),
                              bzip2_encode_buffer_, bzip2_encode_len_, 1, false);
@@ -253,8 +265,10 @@ TEST_F(BZip2FilterUnitTest, DecodeCorruptedData) {
   int corrupt_decode_size = kDefaultBufferSize;
 
   // Decode the correct data with filter
+  std::vector<std::string> filters;
+  filters.push_back("bzip2");
   scoped_ptr<Filter> filter1(
-      Filter::Factory("bzip2", kApplicationOctetStream, kDefaultBufferSize));
+      Filter::Factory(filters, kApplicationOctetStream, kDefaultBufferSize));
   ASSERT_TRUE(filter1.get());
 
   Filter::FilterStatus code = DecodeAllWithFilter(filter1.get(),
@@ -268,7 +282,7 @@ TEST_F(BZip2FilterUnitTest, DecodeCorruptedData) {
 
   // Decode the corrupted data with filter
   scoped_ptr<Filter> filter2(
-      Filter::Factory("bzip2", kApplicationOctetStream, kDefaultBufferSize));
+      Filter::Factory(filters, kApplicationOctetStream, kDefaultBufferSize));
   ASSERT_TRUE(filter2.get());
 
   int pos = corrupt_data_len / 2;
@@ -296,8 +310,10 @@ TEST_F(BZip2FilterUnitTest, DecodeMissingData) {
   --corrupt_data_len;
 
   // Decode the corrupted data with filter
+  std::vector<std::string> filters;
+  filters.push_back("bzip2");
   scoped_ptr<Filter> filter(
-      Filter::Factory("bzip2", kApplicationOctetStream, kDefaultBufferSize));
+      Filter::Factory(filters, kApplicationOctetStream, kDefaultBufferSize));
   ASSERT_TRUE(filter.get());
   char corrupt_decode_buffer[kDefaultBufferSize];
   int corrupt_decode_size = kDefaultBufferSize;
@@ -320,8 +336,10 @@ TEST_F(BZip2FilterUnitTest, DecodeCorruptedHeader) {
   corrupt_data[2] = !corrupt_data[2];
 
   // Decode the corrupted data with filter
+  std::vector<std::string> filters;
+  filters.push_back("bzip2");
   scoped_ptr<Filter> filter(
-      Filter::Factory("bzip2", kApplicationOctetStream, kDefaultBufferSize));
+      Filter::Factory(filters, kApplicationOctetStream, kDefaultBufferSize));
   ASSERT_TRUE(filter.get());
   char corrupt_decode_buffer[kDefaultBufferSize];
   int corrupt_decode_size = kDefaultBufferSize;
@@ -345,11 +363,14 @@ TEST_F(BZip2FilterUnitTest, DecodeWithExtraDataAndSmallOutputBuffer) {
   memcpy(more_data, bzip2_encode_buffer_, bzip2_encode_len_);
   memcpy(more_data + bzip2_encode_len_, kExtraData, kExtraDataBufferSize);
 
+  std::vector<std::string> filters;
+  filters.push_back("bzip2");
   scoped_ptr<Filter> filter(
-      Filter::Factory("bzip2", kApplicationOctetStream, kDefaultBufferSize));
+      Filter::Factory(filters, kApplicationOctetStream, kDefaultBufferSize));
   ASSERT_TRUE(filter.get());
   DecodeAndCompareWithFilter(filter.get(),
-                             source_buffer(), source_len() + kExtraDataBufferSize,
+                             source_buffer(),
+                             source_len() + kExtraDataBufferSize,
                              more_data,
                              more_data_len,
                              kSmallBufferSize,
@@ -362,11 +383,14 @@ TEST_F(BZip2FilterUnitTest, DecodeWithExtraDataAndSmallInputBuffer) {
   memcpy(more_data, bzip2_encode_buffer_, bzip2_encode_len_);
   memcpy(more_data + bzip2_encode_len_, kExtraData, kExtraDataBufferSize);
 
+  std::vector<std::string> filters;
+  filters.push_back("bzip2");
   scoped_ptr<Filter> filter(
-      Filter::Factory("bzip2", kApplicationOctetStream, kSmallBufferSize));
+      Filter::Factory(filters, kApplicationOctetStream, kSmallBufferSize));
   ASSERT_TRUE(filter.get());
   DecodeAndCompareWithFilter(filter.get(),
-                             source_buffer(), source_len() + kExtraDataBufferSize,
+                             source_buffer(),
+                             source_len() + kExtraDataBufferSize,
                              more_data,
                              more_data_len,
                              kDefaultBufferSize,
--- net/url_request/url_request_http_job.h
+++ net/url_request/url_request_http_job.h
@@ -3,8 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H__
-#define NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H__
+#ifndef NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H_
+#define NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H_
+
+#include <string>
+#include <vector>
 
 #include "net/base/completion_callback.h"
 #include "net/http/http_request_info.h"
@@ -25,7 +28,7 @@ class URLRequestHttpJob : public URLRequestJob {
   virtual ~URLRequestHttpJob();
 
  protected:
-  URLRequestHttpJob(URLRequest* request);
+  explicit URLRequestHttpJob(URLRequest* request);
 
   // URLRequestJob methods:
   virtual void SetUpload(net::UploadData* upload);
@@ -39,7 +42,7 @@ class URLRequestHttpJob : public URLRequestJob {
   virtual void GetResponseInfo(net::HttpResponseInfo* info);
   virtual bool GetResponseCookies(std::vector<std::string>* cookies);
   virtual int GetResponseCode();
-  virtual bool GetContentEncoding(std::string* encoding_type);
+  virtual bool GetContentEncodings(std::vector<std::string>* encoding_type);
   virtual bool IsRedirectResponse(GURL* location, int* http_status_code);
   virtual bool IsSafeRedirect(const GURL& location);
   virtual bool NeedsAuth();
@@ -78,12 +81,11 @@ class URLRequestHttpJob : public URLRequestJob {
 
   bool read_in_progress_;
 
-  // Keep a reference to the url request context to be sure it's not
-  // deleted before us.
+  // Keep a reference to the url request context to be sure it's not deleted
+  // before us.
   scoped_refptr<URLRequestContext> context_;
 
-  DISALLOW_EVIL_CONSTRUCTORS(URLRequestHttpJob);
+  DISALLOW_COPY_AND_ASSIGN(URLRequestHttpJob);
 };
 
-#endif  // NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H__
-
+#endif  // NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H_
