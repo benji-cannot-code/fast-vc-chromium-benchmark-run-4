f3f3e60abe4df7d19b52ae69a920c4a74bc4b1f3
jar@google.com <jar@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-22 18:21:11 +0000

    Rollback 2444 and 2443 to get linux and mac prep
    Also need to augment sln dependencies
    
    tbr
    Review URL: http://codereview.chromium.org/3188
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2446 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_http_job.h
+++ net/url_request/url_request_http_job.h
@@ -3,11 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H_
-#define NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H_
-
-#include <string>
-#include <vector>
+#ifndef NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H__
+#define NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H__
 
 #include "net/base/completion_callback.h"
 #include "net/http/http_request_info.h"
@@ -28,7 +25,7 @@ class URLRequestHttpJob : public URLRequestJob {
   virtual ~URLRequestHttpJob();
 
  protected:
-  explicit URLRequestHttpJob(URLRequest* request);
+  URLRequestHttpJob(URLRequest* request);
 
   // URLRequestJob methods:
   virtual void SetUpload(net::UploadData* upload);
@@ -42,7 +39,7 @@ class URLRequestHttpJob : public URLRequestJob {
   virtual void GetResponseInfo(net::HttpResponseInfo* info);
   virtual bool GetResponseCookies(std::vector<std::string>* cookies);
   virtual int GetResponseCode();
-  virtual bool GetContentEncodings(std::vector<std::string>* encoding_type);
+  virtual bool GetContentEncoding(std::string* encoding_type);
   virtual bool IsRedirectResponse(GURL* location, int* http_status_code);
   virtual bool IsSafeRedirect(const GURL& location);
   virtual bool NeedsAuth();
@@ -81,11 +78,12 @@ class URLRequestHttpJob : public URLRequestJob {
 
   bool read_in_progress_;
 
-  // Keep a reference to the url request context to be sure it's not deleted
-  // before us.
+  // Keep a reference to the url request context to be sure it's not
+  // deleted before us.
   scoped_refptr<URLRequestContext> context_;
 
-  DISALLOW_COPY_AND_ASSIGN(URLRequestHttpJob);
+  DISALLOW_EVIL_CONSTRUCTORS(URLRequestHttpJob);
 };
 
-#endif  // NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H_
+#endif  // NET_URL_REQUEST_URL_REQUEST_HTTP_JOB_H__
+
