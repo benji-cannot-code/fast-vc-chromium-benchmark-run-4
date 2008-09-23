d0f6ab9afc7545b30a42d18f087068f27553e546
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-23 18:37:18 +0000

    2008-09-23  Kevin McCullough  <kmccullough@apple.com>
            Reviewed by Darin.
    
            Bug 21030: The JS debugger breaks on the do of a do-while not the while
            (where the conditional statement is)
            https://bugs.webkit.org/show_bug.cgi?id=21030
            Now the statementListEmitCode detects if a do-while node is being
            emited and emits the debug hook on the last line instead of the first.
    
            This change had no effect on sunspider.
    
            * kjs/nodes.cpp:
            (JSC::statementListEmitCode):
            * kjs/nodes.h:
            (JSC::StatementNode::isDoWhile):
            (JSC::DoWhileNode::isDoWhile):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36809 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/nodes.h
+++ third_party/WebKit/JavaScriptCore/kjs/nodes.h
@@ -235,6 +235,7 @@ namespace JSC {
         virtual bool isEmptyStatement() const JSC_FAST_CALL { return false; }
 
         virtual bool isBlock() const JSC_FAST_CALL { return false; }
+        virtual bool isDoWhile() const JSC_FAST_CALL { return false; }
     protected:
         LabelStack m_labelStack;
 
@@ -1937,6 +1938,7 @@ namespace JSC {
         virtual RegisterID* emitCode(CodeGenerator&, RegisterID* = 0) JSC_FAST_CALL;
         virtual void streamTo(SourceStream&) const JSC_FAST_CALL;
 
+        virtual bool isDoWhile() const JSC_FAST_CALL { return true; }
     private:
         RefPtr<StatementNode> m_statement;
         RefPtr<ExpressionNode> m_expr;
