82a2970ee430c5fdd50c3085dbf0727cba12fc30
amanda@chromium.org <amanda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-23 19:48:17 +0000

    SkRect.fTop should be set to the y coordinate of a CGRect, not the x Patch contributed by James Vega <vega.james@gmail.com>. Original review: http://codereview.chromium.org/3198 Review URL: http://codereview.chromium.org/4230
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2513 0039d316-1c4b-4281-b951-d872f2087c98

--- base/gfx/skia_utils_mac.cc
+++ base/gfx/skia_utils_mac.cc
@@ -13,7 +13,7 @@ namespace gfx {
 SkIRect CGRectToSkIRect(const CGRect& rect) {
   SkIRect sk_rect = {
     SkScalarRound(rect.origin.x),
-    SkScalarRound(rect.origin.x),
+    SkScalarRound(rect.origin.y),
     SkScalarRound(rect.origin.x + rect.size.width),
     SkScalarRound(rect.origin.y + rect.size.height)
   };
@@ -23,7 +23,7 @@ SkIRect CGRectToSkIRect(const CGRect& rect) {
 SkRect CGRectToSkRect(const CGRect& rect) {
   SkRect sk_rect = {
     rect.origin.x,
-    rect.origin.x,
+    rect.origin.y,
     rect.origin.x + rect.size.width,
     rect.origin.y + rect.size.height,
   };
