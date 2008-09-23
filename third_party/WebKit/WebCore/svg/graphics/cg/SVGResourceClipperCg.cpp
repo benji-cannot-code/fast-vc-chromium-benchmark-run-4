790ec4099da00710ed57136ddf7424bdffc50607
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-23 07:23:14 +0000

    No review, build fix only.
            Another blind stab in the dark.
    
            * svg/graphics/cg/SVGResourceClipperCg.cpp: Add missing header.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/graphics/cg/SVGResourceClipperCg.cpp
+++ third_party/WebKit/WebCore/svg/graphics/cg/SVGResourceClipperCg.cpp
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AffineTransform.h"
 #include "GraphicsContext.h"
 
+#include <ApplicationServices/ApplicationServices.h>
+
 namespace WebCore {
 
 void SVGResourceClipper::applyClip(GraphicsContext* context, const FloatRect& boundingBox) const
