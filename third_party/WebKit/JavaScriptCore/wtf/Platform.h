06c9bd252da6a654bf6675c98c388357f4c68ece
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-24 01:52:08 +0000

    2008-09-23  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Darin Adler.
    
            * wtf/Platform.h: Removed duplicate #if.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36825 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -336,10 +336,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_NETSCAPE_PLUGIN_API 1
 #endif
 
-#if !defined(ENABLE_DASHBOARD_SUPPORT)
-#define ENABLE_DASHBOARD_SUPPORT 0
-#endif
-
 #if !defined(ENABLE_SAMPLING_TOOL)
 #define ENABLE_SAMPLING_TOOL 0
 #endif
