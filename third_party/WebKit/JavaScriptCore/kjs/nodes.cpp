dce65b94909515778f10df31ff704f957a446964
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-24 22:24:00 +0000

    2008-09-24  Kevin McCullough  <kmccullough@apple.com>
            Style change.
    
            * kjs/nodes.cpp:
            (JSC::statementListEmitCode):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36872 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/nodes.cpp
@@ -1085,7 +1085,7 @@ static inline RegisterID* statementListEmitCode(StatementVector& statements, Cod
     StatementVector::iterator end = statements.end();
     for (StatementVector::iterator it = statements.begin(); it != end; ++it) {
         StatementNode* n = it->get();
-        if(!n->isLoop())
+        if (!n->isLoop())
             generator.emitDebugHook(WillExecuteStatement, n->firstLine(), n->lastLine());
         generator.emitNode(dst, n);
     }
