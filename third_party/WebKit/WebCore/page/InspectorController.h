d39b96bdd142ede55ceb30c6d4e4d70de1cad109
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-24 00:05:34 +0000

    2008-09-23  Kevin McCullough  <kmccullough@apple.com>
            Reviewed by Time and Oliver.
    
            Bug 20949: Catch repeated messages in Inspector Controller to limit
            memory usage
            - Store the repeat count in the Console Message object, in the 
            Inspector Controller and JS ConsoleMessage object.
    
            * page/InspectorController.cpp:
            (WebCore::ConsoleMessage::ConsoleMessage):
            (WebCore::ConsoleMessage::operator==):
            (WebCore::InspectorController::InspectorController):
            (WebCore::InspectorController::addConsoleMessage):
            (WebCore::InspectorController::addScriptConsoleMessage):
            * page/InspectorController.h:
            * page/inspector/Console.js:
            * page/inspector/Resource.js:
            * page/inspector/ResourcesPanel.js:
            * page/inspector/SourceFrame.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36817 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/InspectorController.h
+++ third_party/WebKit/WebCore/page/InspectorController.h
@@ -256,6 +256,7 @@ private:
     RefPtr<Node> m_highlightedNode;
     unsigned m_groupLevel;
     bool m_searchingForNode;
+    ConsoleMessage* m_previousMessage;
 };
 
 } // namespace WebCore
--- third_party/WebKit/WebCore/page/inspector/Resource.js
+++ third_party/WebKit/WebCore/page/inspector/Resource.js
@@ -466,9 +466,6 @@ WebInspector.Resource.prototype = {
         if (this._errors === x)
             return;
 
-        var difference = x - this._errors;
-        WebInspector.errors += difference;
-
         this._errors = x;
     },
 
@@ -483,9 +480,6 @@ WebInspector.Resource.prototype = {
     {
         if (this._warnings === x)
             return;
-
-        var difference = x - this._warnings;
-        WebInspector.warnings += difference;
 
         this._warnings = x;
     },
