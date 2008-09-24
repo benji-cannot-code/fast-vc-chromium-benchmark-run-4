9b1e1c83e9504982d6a3e11c1a0577fa9715579b
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-24 02:23:24 +0000

    Reviewed by Eric.
    Fixes: https://bugs.webkit.org/show_bug.cgi?id=21046 (Several LayoutTests crash)
    
    Fix missing negation in EventTargetNode::insertedIntoDocument.
    Made handleLocalEvents() virtual again, HTMLFormElement overrides it.
    Remove code, that wasn't supposed to go in in dispatchGenericEvent().
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/EventTargetNode.cpp
+++ third_party/WebKit/WebCore/dom/EventTargetNode.cpp
@@ -75,7 +75,7 @@ EventTargetNode::~EventTargetNode()
 
 void EventTargetNode::insertedIntoDocument()
 {
-    if (m_regdListeners && m_regdListeners->isEmpty())
+    if (m_regdListeners && !m_regdListeners->isEmpty())
         document()->unregisterDisconnectedNodeWithEventListeners(this);
 
     Node::insertedIntoDocument();
@@ -211,10 +211,6 @@ bool EventTargetNode::dispatchGenericEvent(PassRefPtr<Event> e, ExceptionCode& e
 
     if (inDocument()) {
         for (Node* n = this; n; n = n->eventParentNode()) {
-            // <use> shadow trees are owned by an invisible root element, skip it
-            if (n->isShadowNode())
-                continue;
-
             n->ref();
             nodeChain.prepend(n);
         }
