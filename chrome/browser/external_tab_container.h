adf589ea7b283d8176ab5b5c8e2874c0f11fb39d
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-24 21:21:48 +0000

    Remove DidNavigate from the tab contents delegate and all the related plumbing. I added additional information to the regular load commit notification so all interested parties can listen for that instead.
    I removed the old navigation type enum, and replaced it with the enum from
    the NavigationController, so it's now public.
    Review URL: http://codereview.chromium.org/3112
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2573 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/external_tab_container.h
+++ chrome/browser/external_tab_container.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "chrome/browser/tab_contents_delegate.h"
 #include "chrome/common/chrome_constants.h"
+#include "chrome/common/notification_registrar.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/views/focus_manager.h"
 #include "chrome/views/root_view.h"
@@ -78,8 +79,6 @@ class ExternalTabContainer : public TabContentsDelegate,
   virtual void UpdateTargetURL(TabContents* source, const GURL& url);
   virtual void ContentsZoomChange(bool zoom_in);
   virtual void ToolbarSizeChanged(TabContents* source, bool is_animating);
-  virtual void DidNavigate(NavigationType nav_type,
-                           int relative_navigation_offet);
   virtual void ForwardMessageToExternalHost(const std::string& receiver,
                                             const std::string& message);
 
@@ -133,6 +132,9 @@ class ExternalTabContainer : public TabContentsDelegate,
  protected:
   TabContents *tab_contents_;
   AutomationProvider* automation_;
+
+  NotificationRegistrar registrar_;
+
   // Root view
   ChromeViews::RootView root_view_;
   // The accelerator table of the external host.
