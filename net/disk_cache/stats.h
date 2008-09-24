0e5061739bb34715ec6c8634be2e51877706ac0e
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-24 23:51:25 +0000

    Use histograms to send interesting parts of the disk cache statistics.
    Most of this CL deals with a derived implementation of histograms that just
    queries the size stats already generated by the disk cache. The exact number
    of buckets, and their distribution, is controlled directly by the new class and
    the disk cache stats code.
    
    Review URL: http://codereview.chromium.org/3069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2580 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/stats.h
+++ net/disk_cache/stats.h
@@ -3,13 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_DISK_CACHE_STATS_H__
-#define NET_DISK_CACHE_STATS_H__
+#ifndef NET_DISK_CACHE_STATS_H_
+#define NET_DISK_CACHE_STATS_H_
 
 #include <string>
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/scoped_ptr.h"
+#include "net/disk_cache/stats_histogram.h"
 
 namespace disk_cache {
 
@@ -58,6 +60,11 @@ class Stats {
 
   void GetItems(StatsItems* items);
 
+  // Support for StatsHistograms. Together, these methods allow StatsHistograms
+  // to take a snapshot of the data_sizes_ as the histogram data.
+  int GetBucketRange(size_t i) const;
+  void Snapshot(StatsHistogram::StatsSamples* samples) const;
+
  private:
   int GetStatsBucket(int32 size);
 
@@ -65,11 +72,12 @@ class Stats {
   uint32 storage_addr_;
   int data_sizes_[kDataSizesLength];
   int64 counters_[MAX_COUNTER];
+  scoped_ptr<StatsHistogram> size_histogram_;
 
-  DISALLOW_EVIL_CONSTRUCTORS(Stats);
+  DISALLOW_COPY_AND_ASSIGN(Stats);
 };
 
 }  // namespace disk_cache
 
-#endif  // NET_DISK_CACHE_STATS_H__
+#endif  // NET_DISK_CACHE_STATS_H_
 
