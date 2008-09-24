e9740a797babff00aa522e889a3ac37ff51adb3c
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-24 02:08:32 +0000

    Rename some properties of ResourcesPanel and ScriptsPanel to be the same,         so future code can be shared.
            https://bugs.webkit.org/show_bug.cgi?id=21005
    
            Reviewed by Oliver Hunt.
    
            * page/inspector/ResourcesPanel.js: Renamed resourceViews to viewsContainerElement.
            And visibleResourceView to visibleView.
            * page/inspector/ScriptsPanel.js: Renamed scriptResourceViews to viewsContainerElement.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/inspector/ResourcesPanel.js
+++ third_party/WebKit/WebCore/page/inspector/ResourcesPanel.js
@@ -34,9 +34,9 @@ WebInspector.ResourcesPanel = function()
 
     this.element.addStyleClass("resources");
 
-    this.resourceViews = document.createElement("div");
-    this.resourceViews.id = "resource-views";
-    this.element.appendChild(this.resourceViews);
+    this.viewsContainerElement = document.createElement("div");
+    this.viewsContainerElement.id = "resource-views";
+    this.element.appendChild(this.viewsContainerElement);
 
     this.containerElement = document.createElement("div");
     this.containerElement.id = "resources-container";
@@ -162,10 +162,10 @@ WebInspector.ResourcesPanel.prototype = {
         this._updateSidebarWidth();
         this.refreshIfNeeded();
 
-        var visibleResourceView = this.visibleResourceView;
-        if (visibleResourceView) {
-            visibleResourceView.headersVisible = true;
-            visibleResourceView.show(this.resourceViews);
+        var visibleView = this.visibleView;
+        if (visibleView) {
+            visibleView.headersVisible = true;
+            visibleView.show(this.viewsContainerElement);
         }
     },
 
@@ -173,12 +173,12 @@ WebInspector.ResourcesPanel.prototype = {
     {
         this._updateGraphDividersIfNeeded();
 
-        var visibleResourceView = this.visibleResourceView;
-        if (visibleResourceView && "resize" in visibleResourceView)
-            visibleResourceView.resize();
+        var visibleView = this.visibleView;
+        if (visibleView && "resize" in visibleView)
+            visibleView.resize();
     },
 
-    get visibleResourceView()
+    get visibleView()
     {
         if (this.visibleResource)
             return this.visibleResource._resourcesView;
@@ -273,7 +273,7 @@ WebInspector.ResourcesPanel.prototype = {
         this._staleResources = [];
 
         this.resourcesTreeElement.removeChildren();
-        this.resourceViews.removeChildren();
+        this.viewsContainerElement.removeChildren();
         this.resourcesGraphsElement.removeChildren();
 
         this._updateGraphDividersIfNeeded(true);
@@ -300,7 +300,7 @@ WebInspector.ResourcesPanel.prototype = {
 
     removeResource: function(resource)
     {
-        if (this.visibleResourceView === resource._resourcesView)
+        if (this.visibleView === resource._resourcesView)
             this.closeVisibleResource();
 
         this._resources.remove(resource, true);
@@ -424,7 +424,7 @@ WebInspector.ResourcesPanel.prototype = {
 
         var view = this.resourceViewForResource(resource);
         view.headersVisible = true;
-        view.show(this.resourceViews);
+        view.show(this.viewsContainerElement);
 
         if (line) {
             if (view.revealLine)
@@ -1053,14 +1053,14 @@ WebInspector.ResourcesPanel.prototype = {
         }
 
         this.containerContentElement.style.left = width + "px";
-        this.resourceViews.style.left = width + "px";
+        this.viewsContainerElement.style.left = width + "px";
         this.sidebarResizeElement.style.left = (width - 3) + "px";
 
         this._updateGraphDividersIfNeeded();
 
-        var visibleResourceView = this.visibleResourceView;
-        if (visibleResourceView && "resize" in visibleResourceView)
-            visibleResourceView.resize();
+        var visibleView = this.visibleView;
+        if (visibleView && "resize" in visibleView)
+            visibleView.resize();
     }
 }
 
--- third_party/WebKit/WebCore/page/inspector/ScriptsPanel.js
+++ third_party/WebKit/WebCore/page/inspector/ScriptsPanel.js
@@ -110,8 +110,8 @@ WebInspector.ScriptsPanel = function()
     this.debuggerStatusElement.id = "scripts-debugger-status";
     this.sidebarButtonsElement.appendChild(this.debuggerStatusElement);
 
-    this.scriptResourceViews = document.createElement("div");
-    this.scriptResourceViews.id = "script-resource-views";
+    this.viewsContainerElement = document.createElement("div");
+    this.viewsContainerElement.id = "script-resource-views";
 
     this.sidebarElement = document.createElement("div");
     this.sidebarElement.id = "scripts-sidebar";
@@ -156,7 +156,7 @@ WebInspector.ScriptsPanel = function()
     this.attachOverlayElement.appendChild(attachButton);
 
     this.element.appendChild(this.attachOverlayElement);
-    this.element.appendChild(this.scriptResourceViews);
+    this.element.appendChild(this.viewsContainerElement);
     this.element.appendChild(this.sidebarElement);
     this.element.appendChild(this.sidebarResizeElement);
 
@@ -201,7 +201,7 @@ WebInspector.ScriptsPanel.prototype = {
         if (this.visibleView) {
             if (this.visibleView instanceof WebInspector.ResourceView)
                 this.visibleView.headersVisible = false;
-            this.visibleView.show(this.scriptResourceViews);
+            this.visibleView.show(this.viewsContainerElement);
         }
     },
 
@@ -360,7 +360,7 @@ WebInspector.ScriptsPanel.prototype = {
         this._scriptsForURLsInFilesSelect = {};
         this.filesSelectElement.removeChildren();
         this.functionsSelectElement.removeChildren();
-        this.scriptResourceViews.removeChildren();
+        this.viewsContainerElement.removeChildren();
 
         if (this._sourceIDMap) {
             for (var sourceID in this._sourceIDMap) {
@@ -389,7 +389,7 @@ WebInspector.ScriptsPanel.prototype = {
         this._visibleView = x;
 
         if (x)
-            x.show(this.scriptResourceViews);
+            x.show(this.viewsContainerElement);
     },
 
     canShowResource: function(resource)
@@ -585,7 +585,7 @@ WebInspector.ScriptsPanel.prototype = {
 
         this.sidebarElement.style.width = newWidth + "px";
         this.sidebarButtonsElement.style.width = newWidth + "px";
-        this.scriptResourceViews.style.right = newWidth + "px";
+        this.viewsContainerElement.style.right = newWidth + "px";
         this.sidebarResizeWidgetElement.style.right = newWidth + "px";
         this.sidebarResizeElement.style.right = (newWidth - 3) + "px";
 
