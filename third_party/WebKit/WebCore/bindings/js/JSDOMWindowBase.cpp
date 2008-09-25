495b3673721219d54827e6b7f23e3d1616d0df51
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-25 00:39:11 +0000

    JavaScriptCore:
    2008-09-24  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Maciej Stachowiak.
    
            Remove staticFunctionGetter.  There is only one remaining user of
            staticFunctionGetter and it can be converted to use setUpStaticFunctionSlot.
    
            * JavaScriptCore.exp:
            * kjs/lookup.cpp:
            * kjs/lookup.h:
    
    WebCore:
    
    2008-09-24  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Maciej Stachowiak.
    
            Remove staticFunctionGetter.  There is only one remaining user of
            staticFunctionGetter and it can be converted to use setUpStaticFunctionSlot.
    
            * bindings/js/JSDOMWindowBase.cpp:
            (WebCore::JSDOMWindowBase::getOwnPropertySlot):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDOMWindowBase.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDOMWindowBase.cpp
@@ -590,7 +590,7 @@ bool JSDOMWindowBase::getOwnPropertySlot(ExecState* exec, const Identifier& prop
                     return false;
             }
             if (allowsAccessFrom(exec))
-                slot.setStaticEntry(this, entry, staticFunctionGetter);
+                setUpStaticFunctionSlot(exec, entry, this, propertyName, slot);
             else
                 slot.setUndefined();
         } else
