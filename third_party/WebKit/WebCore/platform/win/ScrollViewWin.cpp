3a4f233cf8c7b2ab15288e3fc53a4bd36bfa1df5
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-26 21:45:49 +0000

    Fix Win bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36969 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/ScrollViewWin.cpp
+++ third_party/WebKit/WebCore/platform/win/ScrollViewWin.cpp
@@ -107,7 +107,7 @@ const int panIconSizeLength = 20;
 void ScrollView::ScrollViewPrivate::setHasHorizontalScrollbar(bool hasBar)
 {
     if (hasBar && !m_view->m_horizontalScrollbar) {
-        m_horizontalScrollbar = Scrollbar::createNativeScrollbar(this, HorizontalScrollbar, RegularScrollbar);
+        m_view->m_horizontalScrollbar = Scrollbar::createNativeScrollbar(this, HorizontalScrollbar, RegularScrollbar);
         m_view->addChild(m_view->m_horizontalScrollbar.get());
     } else if (!hasBar && m_view->m_horizontalScrollbar) {
         m_view->removeChild(m_view->m_horizontalScrollbar.get());
@@ -119,7 +119,7 @@ void ScrollView::ScrollViewPrivate::setHasVerticalScrollbar(bool hasBar)
 {
     if (hasBar && !m_view->m_verticalScrollbar) {
         m_view->m_verticalScrollbar = Scrollbar::createNativeScrollbar(this, VerticalScrollbar, RegularScrollbar);
-        m_view->addChild(m_verticalScrollbar.get());
+        m_view->addChild(m_view->m_verticalScrollbar.get());
     } else if (!hasBar && m_view->m_verticalScrollbar) {
         m_view->removeChild(m_view->m_verticalScrollbar.get());
         m_view->m_verticalScrollbar = 0;
@@ -193,7 +193,7 @@ void ScrollView::ScrollViewPrivate::setAllowsScrolling(bool flag)
     else if (!flag)
         m_view->m_verticalScrollbarMode = ScrollbarAlwaysOff;
 
-    if (flag && m_horizontalScrollbarMode == ScrollbarAlwaysOff)
+    if (flag && m_view->m_horizontalScrollbarMode == ScrollbarAlwaysOff)
         m_view->m_horizontalScrollbarMode = ScrollbarAuto;
     else if (!flag)
         m_view->m_horizontalScrollbarMode = ScrollbarAlwaysOff;
@@ -204,7 +204,7 @@ void ScrollView::ScrollViewPrivate::setAllowsScrolling(bool flag)
 bool ScrollView::ScrollViewPrivate::allowsScrolling() const
 {
     // Return YES if either horizontal or vertical scrolling is allowed.
-    return m_horizontalScrollbarMode != ScrollbarAlwaysOff || m_verticalScrollbarMode != ScrollbarAlwaysOff;
+    return m_view->m_horizontalScrollbarMode != ScrollbarAlwaysOff || m_view->m_verticalScrollbarMode != ScrollbarAlwaysOff;
 }
 
 IntRect ScrollView::ScrollViewPrivate::windowClipRect() const
