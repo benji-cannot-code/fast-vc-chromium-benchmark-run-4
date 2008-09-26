3410f91d57098d1e1029e5500ed577f28a88c1c9
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-26 23:19:25 +0000

    Fix the mac compile by removing the unnecessary setDeviceOffset function. Review URL: http://codereview.chromium.org/5020
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2652 0039d316-1c4b-4281-b951-d872f2087c98

--- base/gfx/bitmap_platform_device_mac.h
+++ base/gfx/bitmap_platform_device_mac.h
@@ -58,7 +58,6 @@ class BitmapPlatformDeviceMac : public PlatformDeviceMac {
 
   virtual CGContextRef GetBitmapContext();
   virtual void SetTransform(const SkMatrix& matrix);
-  virtual void SetDeviceOffset(int x, int y);
 
   // This currently only supports extremely simple clip rects.
   virtual void SetClipRegion(const SkRegion& region);
