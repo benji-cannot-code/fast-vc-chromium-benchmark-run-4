08dce6209052f88ad3ddf57619b2332040a5754d
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-26 09:36:28 +0000

    2008-09-26  Håvard Wall  <hwall@trolltech.com>
            Reviewed by Simon.
    
            Fix compilation with QT_NO_MESSAGEBOX
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36947 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -1246,7 +1246,9 @@ void QWebPage::javaScriptConsoleMessage(const QString& message, int lineNumber, 
 */
 void QWebPage::javaScriptAlert(QWebFrame *frame, const QString& msg)
 {
+#ifndef QT_NO_MESSAGEBOX
     QMessageBox::information(d->view, mainFrame()->title(), msg, QMessageBox::Ok);
+#endif
 }
 
 /*!
@@ -1257,7 +1259,11 @@ void QWebPage::javaScriptAlert(QWebFrame *frame, const QString& msg)
 */
 bool QWebPage::javaScriptConfirm(QWebFrame *frame, const QString& msg)
 {
+#ifdef QT_NO_MESSAGEBOX
+    return true;
+#else
     return QMessageBox::Yes == QMessageBox::information(d->view, mainFrame()->title(), msg, QMessageBox::Yes, QMessageBox::No);
+#endif
 }
 
 /*!
