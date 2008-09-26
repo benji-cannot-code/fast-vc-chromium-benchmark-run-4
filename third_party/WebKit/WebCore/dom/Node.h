b83b8c513d12319b605f5dc6671819fdf3e82b78
dsmith@webkit.org <dsmith@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-26 04:05:57 +0000

    2008-09-25  David Smith  <catfish.man@gmail.com>
            Reviewed by Dave Hyatt.
    
            Fix a nasty regression I introduced in the previous commit, which caused infinite recursion on facebook.
    
            * dom/Element.cpp:
            (WebCore::Element::rareData):
            * dom/Element.h:
            * dom/Node.cpp:
            (WebCore::Node::rareData):
            * dom/Node.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Node.h
+++ third_party/WebKit/WebCore/dom/Node.h
@@ -502,8 +502,7 @@ protected:
     
     bool hasRareData() const { return m_hasRareData; }
     
-    NodeRareData* rareData();
-    const NodeRareData* rareData() const;
+    NodeRareData* rareData() const;
     NodeRareData* ensureRareData();
     
     virtual bool virtualHasTagName(const QualifiedName&) const;
