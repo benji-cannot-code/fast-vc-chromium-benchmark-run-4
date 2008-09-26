82478ed4052000a6c67fbea7fb05e10bac30203c
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-26 06:44:08 +0000

    Fix Qt bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/ScrollViewQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/ScrollViewQt.cpp
@@ -556,8 +556,8 @@ void ScrollView::paint(GraphicsContext* context, const IntRect& rect)
     context->translate(x(), y());
     documentDirtyRect.move(-x(), -y());
 
-    context->translate(-contentsX(), -contentsY());
-    documentDirtyRect.move(contentsX(), contentsY());
+    context->translate(-scrollX(), -scrollY());
+    documentDirtyRect.move(scrollX(), scrollY());
 
     documentDirtyRect.intersect(enclosingIntRect(visibleContentRect()));
     context->clip(documentDirtyRect);
@@ -627,7 +627,7 @@ void ScrollView::wheelEvent(PlatformWheelEvent& e)
         (deltaY > 0 && scrollOffset().height() > 0)) {
 
         e.accept();
-        scrollBy(int(-deltaX * LINE_STEP), int(-deltaY * LINE_STEP));
+        scrollBy(IntSize(int(-deltaX * LINE_STEP), int(-deltaY * LINE_STEP)));
     }
 }
 
