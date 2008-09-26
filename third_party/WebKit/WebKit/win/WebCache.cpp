aadd2f84fe0038eca143fed7173f736b9c7767d4
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-26 20:47:58 +0000

    Fix Mac build with XPATH and XSLT disabled
    WebCore:
    
            Fix Mac build with XPATH and XSLT disabled
    
            Reviewed by Alexey.
    
            * bindings/objc/DOMUtility.mm:
            (JSC::createDOMWrapper): Conditionalize code with ENABLE(XPATH).
            * dom/XMLTokenizerLibxml2.cpp:
            (WebCore::errorFunc): Conditionalize method with ENABLE(XSLT) to
            prevent an unused code warning.
    
    WebKit/mac:
    
            Fix Mac build with XSLT disabled
    
            Reviewed by Alexey.
    
            * Misc/WebCache.mm:
            (+[WebCache statistics]): Populate xslStyleSheet statistics with
            zeros if XSLT is disabled.
    
    WebKit/win:
    
            Speculative Windows build fix with XSLT disabled
    
            Reviewed by Alexey.
    
            * WebCache.cpp:
            (WebCache::statistics): Populate xslStyleSheet statistics with
            zeros if XSLT is disabled.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36961 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebCache.cpp
+++ third_party/WebKit/WebKit/win/WebCache.cpp
@@ -105,6 +105,9 @@ HRESULT STDMETHODCALLTYPE WebCache::statistics(
     static CFStringRef stylesheetsKey = CFSTR("style sheets");
     static CFStringRef xslKey = CFSTR("xsl");
     static CFStringRef scriptsKey = CFSTR("scripts");
+#if !ENABLE(XSLT)
+    const int zero = 0;
+#endif
 
     RetainPtr<CFMutableDictionaryRef> dictionary(AdoptCF,
         CFDictionaryCreateMutable(0, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks));
@@ -115,7 +118,11 @@ HRESULT STDMETHODCALLTYPE WebCache::statistics(
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.cssStyleSheets.count));
     CFDictionaryAddValue(dictionary.get(), stylesheetsKey, value.get());
     
+#if ENABLE(XSLT)
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.xslStyleSheets.count));
+#else
+    value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &zero));
+#endif
     CFDictionaryAddValue(dictionary.get(), xslKey, value.get());
 
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.scripts.count));
@@ -133,7 +140,11 @@ HRESULT STDMETHODCALLTYPE WebCache::statistics(
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.cssStyleSheets.size));
     CFDictionaryAddValue(dictionary.get(), stylesheetsKey, value.get());
     
+#if ENABLE(XSLT)
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.xslStyleSheets.size));
+#else
+    value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &zero));
+#endif
     CFDictionaryAddValue(dictionary.get(), xslKey, value.get());
 
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.scripts.size));
@@ -151,7 +162,11 @@ HRESULT STDMETHODCALLTYPE WebCache::statistics(
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.cssStyleSheets.liveSize));
     CFDictionaryAddValue(dictionary.get(), stylesheetsKey, value.get());
     
+#if ENABLE(XSLT)
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.xslStyleSheets.liveSize));
+#else
+    value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &zero));
+#endif
     CFDictionaryAddValue(dictionary.get(), xslKey, value.get());
 
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.scripts.liveSize));
@@ -169,7 +184,11 @@ HRESULT STDMETHODCALLTYPE WebCache::statistics(
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.cssStyleSheets.decodedSize));
     CFDictionaryAddValue(dictionary.get(), stylesheetsKey, value.get());
     
+#if ENABLE(XSLT)
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.xslStyleSheets.decodedSize));
+#else
+    value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &zero));
+#endif
     CFDictionaryAddValue(dictionary.get(), xslKey, value.get());
 
     value.adoptCF(CFNumberCreate(0, kCFNumberIntType, &stat.scripts.decodedSize));
