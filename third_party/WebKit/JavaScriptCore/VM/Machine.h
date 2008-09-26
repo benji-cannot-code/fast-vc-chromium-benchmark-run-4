d6d9e3c644cfe2e04309d5fe9e99b5a2f821797c
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-26 22:43:16 +0000

    2008-09-26  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Darin Adler.
            
            Reverted the part of r36614 that used static data because static data
            is not thread-safe.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/Machine.h
+++ third_party/WebKit/JavaScriptCore/VM/Machine.h
@@ -75,7 +75,14 @@ namespace JSC {
         
         RegisterFile& registerFile() { return m_registerFile; }
         
-        static Opcode getOpcode(OpcodeID id);
+        Opcode getOpcode(OpcodeID id)
+        {
+            #if HAVE(COMPUTED_GOTO)
+                return m_opcodeTable[id];
+            #else
+                return id;
+            #endif
+        }
 
         OpcodeID getOpcodeID(Opcode opcode)
         {
@@ -290,7 +297,7 @@ namespace JSC {
         void* m_jsFunctionVptr;
 
 #if HAVE(COMPUTED_GOTO)
-        static Opcode s_opcodeTable[numOpcodeIDs]; // Maps OpcodeID => Opcode for compiling
+        Opcode m_opcodeTable[numOpcodeIDs]; // Maps OpcodeID => Opcode for compiling
         HashMap<Opcode, OpcodeID> m_opcodeIDTable; // Maps Opcode => OpcodeID for decompiling
 #endif
     };
