9eabf3a4121505fb6748dbd25506878c156b2c53
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-26 09:36:57 +0000

    2008-09-26  Ariya Hidayat  <ahidayat@trolltech.com>
            Reviewed by Simon.
    
            Fixed potential crash when deleting QWebView instance.
    
            When deleting the view, sets the page's view to NULL.
            The reason is that the page is smart and its destructor might lead to a call
            that operates on a view (which is in the process of being deleted).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@36949 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.h
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.h
@@ -300,6 +300,7 @@ private:
 
     friend class QWebFrame;
     friend class QWebPagePrivate;
+    friend class QWebView;
     friend class WebCore::ChromeClientQt;
     friend class WebCore::EditorClientQt;
     friend class WebCore::FrameLoaderClientQt;
--- third_party/WebKit/WebKit/qt/Api/qwebview.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebview.cpp
@@ -177,6 +177,9 @@ QWebView::QWebView(QWidget *parent)
 */
 QWebView::~QWebView()
 {
+    if (d->page)
+        d->page->d->view = 0;
+
     if (d->page && d->page->parent() == this)
         delete d->page;
     delete d;
