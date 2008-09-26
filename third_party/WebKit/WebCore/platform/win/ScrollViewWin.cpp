fce991e3119fa0decc2ae2421a6980edde4e8ded
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-26 06:33:30 +0000

    Fix Win bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36937 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/ScrollViewWin.cpp
+++ third_party/WebKit/WebCore/platform/win/ScrollViewWin.cpp
@@ -650,7 +650,7 @@ void ScrollView::wheelEvent(PlatformWheelEvent& e)
         adjustDeltaForPageScrollMode(deltaX, e.isPageXScrollModeEnabled(), visibleWidth());
         adjustDeltaForPageScrollMode(deltaY, e.isPageYScrollModeEnabled(), visibleHeight());
 
-        scrollBy(-deltaX * LINE_STEP_WIN, -deltaY * LINE_STEP_WIN);
+        scrollBy(IntSize(-deltaX * LINE_STEP_WIN, -deltaY * LINE_STEP_WIN));
     }
 }
 
