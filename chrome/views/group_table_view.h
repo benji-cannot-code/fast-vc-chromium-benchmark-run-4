5f1036e16dba5be83286c30c08c125c9723eae22
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-26 17:45:36 +0000

    Adds the ability to sort TableView. Contrary to what we spoke about the other day I ended up doing the sorting in tableview. This makes it a heck of lot easier than having every model have to deal with it. As part of this I removed the optional non-caching logic from TableView, which was never used. Sadly though, this means there are coordinate transformations.
    I've only enabled sorting in the keyword editor, I have to make sure
    all the other places that use TableView can deal with it. For example,
    task manager can't deal with it currently as it expects the getters to
    be called only once where as when sorting they may be called multiple
    times.
    
    BUG=2790
    TEST=This enables sorting ONLY in the keyword editor. Make sure there
      aren't any problems in adding/removing/changing entries in the
      keyword editor after this.
    
    Review URL: http://codereview.chromium.org/4276
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2631 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/group_table_view.h
+++ chrome/views/group_table_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_VIEWS_GROUP_TABLE_VIEW_H__
-#define CHROME_VIEWS_GROUP_TABLE_VIEW_H__
+#ifndef CHROME_VIEWS_GROUP_TABLE_VIEW_H_
+#define CHROME_VIEWS_GROUP_TABLE_VIEW_H_
 
 #include "base/task.h"
 #include "chrome/views/table_view.h"
@@ -44,11 +44,11 @@ class GroupTableView : public TableView {
  protected:
   // Notification from the ListView that the selected state of an item has
   // changed.
-  void OnSelectedStateChanged(int item, bool is_selected);
+  void OnSelectedStateChanged(int model_row, bool is_selected);
 
   // Extra-painting required to draw the separator line between groups.
   virtual bool ImplementPostPaint() { return true; }
-  virtual void PostPaint(int row, int column, bool selected,
+  virtual void PostPaint(int model_row, int column, bool selected,
                          const CRect& bounds, HDC device_context);
 
   // In order to make keyboard navigation possible (using the Up and Down
@@ -57,19 +57,27 @@ class GroupTableView : public TableView {
   // needs to be set on a group item when a group is selected.
   virtual void OnKeyDown(unsigned short virtual_keycode);
 
+  // Overriden to make sure rows in the same group stay grouped together.
+  virtual int CompareRows(int model_row1, int model_row2);
+
+  // Updates model_index_to_range_start_map_ from the model.
+  virtual void PrepareForSort();
+
  private:
   // Make the selection of group consistent.
   void SyncSelection();
 
-  GroupTableModel *model_;
+  GroupTableModel* model_;
 
   // A factory to make the selection consistent among groups.
   ScopedRunnableMethodFactory<GroupTableView> sync_selection_factory_;
 
-  DISALLOW_EVIL_CONSTRUCTORS(GroupTableView);
+  // Maps from model row to start of group.
+  std::map<int,int> model_index_to_range_start_map_;
+
+  DISALLOW_COPY_AND_ASSIGN(GroupTableView);
 };
 
 }  // namespace ChromeViews
 
-#endif  // CHROME_VIEWS_GROUP_TABLE_VIEW_H__
-
+#endif  // CHROME_VIEWS_GROUP_TABLE_VIEW_H_
