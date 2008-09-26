17c4e29dc3807f9d25e16fe4830677b34d9e2a96
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-26 06:47:47 +0000

    Fix Win, Qt, Gtk bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36940 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/ScrollViewWin.cpp
+++ third_party/WebKit/WebCore/platform/win/ScrollViewWin.cpp
@@ -640,7 +640,7 @@ void ScrollView::wheelEvent(PlatformWheelEvent& e)
         return;
 
     // Determine how much we want to scroll.  If we can move at all, we will accept the event.
-    IntSize maxScrollDelta = maximumScroll();
+    IntSize maxScrollDelta = maximumScrollPosition() - scrollPosition();
     if ((e.deltaX() < 0 && maxScrollDelta.width() > 0) ||
         (e.deltaX() > 0 && scrollOffset().width() > 0) ||
         (e.deltaY() < 0 && maxScrollDelta.height() > 0) ||
