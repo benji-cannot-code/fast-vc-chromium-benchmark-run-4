1d52b33f9fe709550db94cf27f3c0bca5d0deeed
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-27 22:18:35 +0000

    Added support for console.trace.
    JavaScriptCore:
    
    2008-09-27  Keishi Hattori  <casey.hattori@gmail.com>
    
            Added Machine::retrieveCaller to the export list.
    
            Reviewed by Kevin McCullough and Tim Hatcher.
    
            * JavaScriptCore.exp: Added Machine::retrieveCaller.
    
    WebCore:
    
    2008-09-27  Keishi Hattori  <casey.hattori@gmail.com>
    
            Added support for console.trace.
    
            https://bugs.webkit.org/show_bug.cgi?id=19157
    
            Reviewed by Kevin McCullough and Tim Hatcher.
    
            * bindings/js/JSConsoleCustom.cpp:
            (WebCore::JSConsole::trace):
            * page/Console.cpp:
            (WebCore::Console::trace): Calls Machine::retrieveCaller to
            get a stack trace.
            * page/Console.h:
            (WebCore::): Added TraceMessageLevel.
            * page/Console.idl: Added console.trace.
            * page/inspector/Console.js:
            (ConsoleMessage): Added case for TraceMessageLevel.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37012 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Console.h
+++ third_party/WebKit/WebCore/page/Console.h
@@ -62,6 +62,7 @@ namespace WebCore {
         ErrorMessageLevel,
         ObjectMessageLevel,
         NodeMessageLevel,
+        TraceMessageLevel,
         StartGroupMessageLevel,
         EndGroupMessageLevel
     };
@@ -82,6 +83,7 @@ namespace WebCore {
         void warn(JSC::ExecState*, const JSC::ArgList&);
         void dir(JSC::ExecState*, const JSC::ArgList&);
         void dirxml(JSC::ExecState*, const JSC::ArgList& arguments);
+        void trace(JSC::ExecState*);
         void assertCondition(bool condition, JSC::ExecState*, const JSC::ArgList&);
         void count(JSC::ExecState*, const JSC::ArgList&);
         void profile(JSC::ExecState*, const JSC::ArgList&);
