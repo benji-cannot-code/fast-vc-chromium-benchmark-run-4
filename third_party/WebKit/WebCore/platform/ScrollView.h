bd71928a679a4b0d819e21618784d347b3488b8a
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-27 05:24:16 +0000

    Fix WIndows bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@36984 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ScrollView.h
+++ third_party/WebKit/WebCore/platform/ScrollView.h
@@ -255,9 +255,6 @@ public:
     virtual void show();
     virtual void hide();
     virtual void setParentVisible(bool);
-
-    void setAllowsScrolling(bool);
-    bool allowsScrolling() const;
 #endif
 
 #if PLATFORM(QT)
