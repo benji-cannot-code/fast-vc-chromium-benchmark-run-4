03e8e53a401bf8340f6463876be6e96a30b544f4
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-28 01:10:26 +0000

    One more simple change to trigger the buildbots.
    This is just fixing the header guard
    Review URL: http://codereview.chromium.org/4319
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2662 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/src/restricted_token.h
+++ sandbox/src/restricted_token.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SANDBOX_SRC_RESTRICTED_TOKEN_H__
-#define SANDBOX_SRC_RESTRICTED_TOKEN_H__
+#ifndef SANDBOX_SRC_RESTRICTED_TOKEN_H_
+#define SANDBOX_SRC_RESTRICTED_TOKEN_H_
 
 #include <windows.h>
 #include <vector>
@@ -196,5 +196,5 @@ class RestrictedToken {
 
 }  // namespace sandbox
 
-#endif  // SANDBOX_SRC_RESTRICTED_TOKEN_H__
+#endif  // SANDBOX_SRC_RESTRICTED_TOKEN_H_
 
