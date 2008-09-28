1a6c7b712f276a075426722779381aacc3bcc164
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-28 00:37:10 +0000

    I'm testing the buildbots, I needed a small change to trigger them. Review URL: http://codereview.chromium.org/4318
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2661 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/src/sid.h
+++ sandbox/src/sid.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SANDBOX_SRC_SID_H__
-#define SANDBOX_SRC_SID_H__
+#ifndef SANDBOX_SRC_SID_H_
+#define SANDBOX_SRC_SID_H_
 
 #include <windows.h>
 
@@ -27,5 +27,5 @@ class Sid {
 
 }  // namespace sandbox
 
-#endif  // SANDBOX_SRC_SID_H__
+#endif  // SANDBOX_SRC_SID_H_
 
