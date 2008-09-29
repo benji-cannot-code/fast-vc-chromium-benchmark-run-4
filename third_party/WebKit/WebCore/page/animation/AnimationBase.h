c13c75e2633b0ec676edf8cd7d0e6e34381e4523
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-29 21:18:07 +0000

    2008-09-29  Chris Marrin  <cmarrin@apple.com>
            Reviewed by Sam Weinig
    
            https://bugs.webkit.org/show_bug.cgi?id=20921
            -webkit-animation-timing-function: inside of keyframes is ignored
    
            Test: animations/keyframe-timing-functions.html
    
            * page/animation/AnimationBase.cpp:
            (WebCore::AnimationBase::progress):
            * page/animation/AnimationBase.h:
            * page/animation/ImplicitAnimation.cpp:
            (WebCore::ImplicitAnimation::animate):
            * page/animation/KeyframeAnimation.cpp:
            (WebCore::KeyframeAnimation::animate):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/AnimationBase.h
+++ third_party/WebKit/WebCore/page/animation/AnimationBase.h
@@ -46,6 +46,7 @@ class KeyframeAnimation;
 class Node;
 class RenderObject;
 class RenderStyle;
+class TimingFunction;
 
 class AnimationTimerBase {
 public:
@@ -210,7 +211,7 @@ public:
     void setAnimating(bool inAnimating = true) { m_animating = inAnimating; }
     bool animating() const { return m_animating; }
 
-    double progress(double scale, double offset) const;
+    double progress(double scale, double offset, const TimingFunction*) const;
 
     virtual void animate(CompositeAnimation*, RenderObject*, const RenderStyle* currentStyle, 
                          const RenderStyle* targetStyle, RenderStyle*& animatedStyle) { }
