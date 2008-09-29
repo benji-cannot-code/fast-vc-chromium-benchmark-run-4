2e425f1abe40f7450be000edea7e2bad33c69da8
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-29 20:09:59 +0000

    2008-09-29  David Hyatt  <hyatt@apple.com>
            https://bugs.webkit.org/show_bug.cgi?id=21218
    
            Accidentally removed a check to see if the scrollbar modes were equal.  This caused a crash on Windows.
            It's silly that the code was this fragile, but for now just put the check back in.
    
            Reviewed by Dan Bernstein
    
            * platform/ScrollView.cpp:
            (WebCore::ScrollView::setScrollbarModes):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37069 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ScrollView.cpp
+++ third_party/WebKit/WebCore/platform/ScrollView.cpp
@@ -68,6 +68,8 @@ void ScrollView::removeChild(Widget* child)
 
 void ScrollView::setScrollbarModes(ScrollbarMode horizontalMode, ScrollbarMode verticalMode)
 {
+    if (horizontalMode == horizontalScrollbarMode() && verticalMode == verticalScrollbarMode())
+        return;
     m_horizontalScrollbarMode = horizontalMode;
     m_verticalScrollbarMode = verticalMode;
     if (platformWidget())
