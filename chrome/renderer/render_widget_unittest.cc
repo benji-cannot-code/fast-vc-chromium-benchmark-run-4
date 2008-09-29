56e4d2442b92b56873c833e3e27735d7898158fe
cpu@google.com <cpu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-29 18:45:09 +0000

    Disable tests because it is leaking memory
    BUG=2954
    
    TBR=erikkay
    
    Review URL: http://codereview.chromium.org/4333
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2683 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_widget_unittest.cc
+++ chrome/renderer/render_widget_unittest.cc
@@ -117,7 +117,7 @@ class MockRenderThread : public RenderThreadBase {
   IPC::MessageReplyDeserializer* reply_deserializer_;
 };
 
-TEST(RenderWidgetTest, CreateAndCloseWidget) {
+TEST(RenderWidgetTest, DISABLED_CreateAndCloseWidget) {
   MessageLoop msg_loop;
   MockRenderThread render_thread;
 
