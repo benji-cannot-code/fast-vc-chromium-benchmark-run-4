55a31c5536924fc425d473581289e78bb9965251
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-29 21:04:54 +0000

    Windows build fix, take 3
    git-svn-id: svn://svn.chromium.org/blink/trunk@37072 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ScrollView.cpp
+++ third_party/WebKit/WebCore/platform/ScrollView.cpp
@@ -378,8 +378,9 @@ void ScrollView::platformSetScrollPosition(const IntPoint&)
 {
 }
 
-bool platformScroll(ScrollDirection, ScrollGranularity)
+bool ScrollView::platformScroll(ScrollDirection, ScrollGranularity)
 {
+    return true;
 }
 #endif
 
