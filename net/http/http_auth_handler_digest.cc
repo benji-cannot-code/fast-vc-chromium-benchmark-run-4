0c35d36b961066d5092e25d5e0484fbec958870d
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-30 01:06:38 +0000

    Use base::RandInt() in place of rand(), now that rand_util has been moved from chrome/ to base/.
    Review URL: http://codereview.chromium.org/5036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2707 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_handler_digest.cc
+++ net/http/http_auth_handler_digest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_auth_handler_digest.h"
 
 #include "base/md5.h"
+#include "base/rand_util.h"
 #include "base/string_util.h"
 #include "net/base/net_util.h"
 #include "net/http/http_auth.h"
@@ -48,9 +49,8 @@ std::string HttpAuthHandlerDigest::GenerateNonce() {
   static const char domain[] = "0123456789abcdef";
   std::string cnonce;
   cnonce.reserve(16);
-  // TODO(eroman): use rand_util::RandIntSecure()
   for (int i = 0; i < 16; ++i)
-    cnonce.push_back(domain[rand() % 16]);
+    cnonce.push_back(domain[base::RandInt(0, 15)]);
   return cnonce;
 }
 
