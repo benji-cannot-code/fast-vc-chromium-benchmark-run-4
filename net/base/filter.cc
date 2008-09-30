60bcfd15a4487492df71f2b3e4d0e912ef9f345e
sgk@google.com <sgk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-30 01:37:05 +0000

    Get open-vcdiff building on Linux and Mac (in SCons) using captured values for generating a config.h file.
    Builds base/sdch_{filter,manager}.cc on all platforms and links
    the 'sdch' library.
    
    Add sdch to the SCons configuration loaded on Mac and Linux.
    
    Removes platform #ifs in net/base/filter.cc initialization code
    (reverting r2740).
    
    B=2662
    Review URL: http://codereview.chromium.org/5204
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2710 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/filter.cc
+++ net/base/filter.cc
@@ -8,10 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "net/base/gzip_filter.h"
 #include "net/base/bzip2_filter.h"
-#if defined(OS_WIN) || defined(OS_MACOSX)
-// TODO(port): remove #ifdef when sdch works on all platforms.
 #include "net/base/sdch_filter.h"
-#endif
 
 namespace {
 
@@ -86,11 +83,8 @@ Filter* Filter::SingleFilter(const std::string& filter_type,
   } else if (LowerCaseEqualsASCII(filter_type, kBZip2) ||
              LowerCaseEqualsASCII(filter_type, kXBZip2)) {
     type_id = FILTER_TYPE_BZIP2;
-#if defined(OS_WIN) || defined(OS_MACOSX)
-    // TODO(port): remove #ifdef when sdch works on all platforms.
   } else if (LowerCaseEqualsASCII(filter_type, kSdch)) {
     type_id = FILTER_TYPE_SDCH;
-#endif
   } else {
     // Note we also consider "identity" and "uncompressed" UNSUPPORTED as
     // filter should be disabled in such cases.
@@ -117,8 +111,6 @@ Filter* Filter::SingleFilter(const std::string& filter_type,
       }
       break;
     }
-#if defined(OS_WIN) || defined(OS_MACOSX)
-    // TODO(port): remove #ifdef when sdch works on all platforms.
     case FILTER_TYPE_SDCH: {
       scoped_ptr<SdchFilter> sdch_filter(new SdchFilter());
       if (sdch_filter->InitBuffer(buffer_size)) {
@@ -128,7 +120,6 @@ Filter* Filter::SingleFilter(const std::string& filter_type,
       }
       break;
     }
-#endif
     default: {
       break;
     }
