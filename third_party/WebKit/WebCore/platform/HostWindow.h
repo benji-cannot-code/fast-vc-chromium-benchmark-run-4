eb68fa75644fdcb1a3a660770b3cd54422e0c339
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-30 21:30:56 +0000

    Eliminate the ScrollView update() method.
    git-svn-id: svn://svn.chromium.org/blink/trunk@37113 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/HostWindow.h
+++ third_party/WebKit/WebCore/platform/HostWindow.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HostWindow_h
 
 #include <wtf/Noncopyable.h>
+#include "IntRect.h"
 
 namespace WebCore {
 
@@ -42,6 +43,9 @@ public:
     // contentChanged boolean indicates whether or not the Web page content actually changed (or if a repaint
     // of unchanged content is being requested).
     virtual void repaint(const IntRect&, bool contentChanged, bool immediate = false) = 0;
+
+    // The paint method just causes a synchronous update of the window to happen for platforms that need it (Windows).
+    void paint() { repaint(IntRect(), false, true); }
 };
 
 } // namespace WebCore
--- third_party/WebKit/WebCore/platform/win/ScrollViewWin.cpp
+++ third_party/WebKit/WebCore/platform/win/ScrollViewWin.cpp
@@ -216,11 +216,6 @@ void ScrollView::updateWindowRect(const IntRect& rect, bool now)
         ::UpdateWindow(containingWindowHandle);
 }
 
-void ScrollView::update()
-{
-    ::UpdateWindow(containingWindow());
-}
-
 void ScrollView::setFrameRect(const IntRect& newGeometry)
 {
     IntRect oldGeometry = frameRect();
