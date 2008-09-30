61ed339709b34bb46d29f3fc8866c53babcba402
jar@google.com <jar@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-30 01:18:09 +0000

    Transition to pulling open-vcdiff rather than using a private snapshot pull
    I can't include all the svn deletions (all of sdch/open_vcdiff/*) in this
    change list, as it will break reitveld.  I'll do them piecemeal after a get
    an LGTM.
    
    Also, by temporarilly leaving in the old subtree, I can support mac until
    the move to use this new sub-tree.
    
    r=nsylvain,sgk
    Review URL: http://codereview.chromium.org/5402
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2709 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/sdch_filter.cc
+++ net/base/sdch_filter.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/sdch_filter.h"
 #include "net/base/sdch_manager.h"
 
-#include "sdch/open_vcdiff/depot/opensource/open-vcdiff/src/google/vcdecoder.h"
+#include "sdch/open-vcdiff/src/google/vcdecoder.h"
 
 SdchFilter::SdchFilter()
     : decoding_status_(DECODING_UNINITIALIZED),
