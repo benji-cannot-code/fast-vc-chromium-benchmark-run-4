c9e03bbb82761f0874c5289f680ff374a0a26d95
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-09-30 20:56:17 +0000

    Fix qt bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@37109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/ScrollViewQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/ScrollViewQt.cpp
@@ -188,7 +188,7 @@ void ScrollView::update()
         native->update();
         return;
     }
-    updateContents(IntRect(0, 0, width(), height()));
+    repaintContentRectangle(IntRect(0, 0, width(), height()));
 }
 
 void ScrollView::setFrameRect(const IntRect& newGeometry)
