c06358f49c80fd27053ecce43a68cff700873079
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-01 17:26:06 +0000

    Fixes bug where JSON reader errored on reading vertical tabs (\v). The writer properly escapes a vertical tab, but the readed instead errored on the vertical this. This resulted in lost bookmarks:(
    BUG=3031
    TEST=covered by unit tests
    
    Review URL: http://codereview.chromium.org/5628
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2759 0039d316-1c4b-4281-b951-d872f2087c98

--- base/json_reader.cc
+++ base/json_reader.cc
@@ -362,6 +362,7 @@ JSONReader::Token JSONReader::ParseStringToken() {
         case 'n':
         case 'r':
         case 't':
+        case 'v':
         case '"':
           break;
         default:
@@ -406,6 +407,9 @@ bool JSONReader::DecodeString(const Token& token, Value** node) {
           break;
         case 't':
           decoded_str.push_back('\t');
+          break;
+        case 'v':
+          decoded_str.push_back('\v');
           break;
 
         case 'x':
