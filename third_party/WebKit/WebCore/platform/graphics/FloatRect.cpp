c6093ea7444bcade9b5fb165c61b23059ec6242d
brettw@chromium.org <brettw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-02 19:33:42 +0000

    Reviewed by Sam Weinig.
    https://bugs.webkit.org/show_bug.cgi?id=21292
    
    Revert the changes to enclosingIntRect made in r12530 since the new code
    doesn't round correctly.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/FloatRect.cpp
+++ third_party/WebKit/WebCore/platform/graphics/FloatRect.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FloatConversion.h"
 #include "IntRect.h"
 #include <algorithm>
+#include <math.h>
 
 using std::max;
 using std::min;
@@ -112,14 +113,10 @@ void FloatRect::scale(float s)
 
 IntRect enclosingIntRect(const FloatRect& rect)
 {
-    int l = static_cast<int>(rect.x());
-    int t = static_cast<int>(rect.y());
-    // FIXME: These two need to be a "ceiling" operation, not rounding.
-    // We changed them to do "+ 0.5f" to compile on Win32 where there's
-    // no ceilf, but they should be changed back to "ceiling" at some point
-    // and we should provide an implementation of ceilf for Win32.
-    int r = static_cast<int>(rect.right() + 0.5f);
-    int b = static_cast<int>(rect.bottom() + 0.5f);
+    int l = static_cast<int>(floorf(rect.x()));
+    int t = static_cast<int>(floorf(rect.y()));
+    int r = static_cast<int>(ceilf(rect.right()));
+    int b = static_cast<int>(ceilf(rect.bottom()));
     return IntRect(l, t, r - l, b - t);
 }
 
