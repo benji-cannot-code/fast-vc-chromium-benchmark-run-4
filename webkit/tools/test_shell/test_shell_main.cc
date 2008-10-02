d873451a73dc84c0a810e1c6c2d9f7454b7afc77
patrick@chromium.org <patrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-02 22:57:33 +0000

    Fix indentation. Review URL: http://codereview.chromium.org/5636
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2816 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_main.cc
+++ webkit/tools/test_shell/test_shell_main.cc
@@ -197,8 +197,8 @@ int main(int argc, char* argv[]) {
   bool interactive = !layout_test_mode;
   TestShell::InitializeTestShell(interactive);
 
-    if (parsed_command_line.HasSwitch(test_shell::kAllowScriptsToCloseWindows))
-      TestShell::SetAllowScriptsToCloseWindows();
+  if (parsed_command_line.HasSwitch(test_shell::kAllowScriptsToCloseWindows))
+    TestShell::SetAllowScriptsToCloseWindows();
 
   // Disable user themes for layout tests so pixel tests are consistent.
   if (!interactive)
