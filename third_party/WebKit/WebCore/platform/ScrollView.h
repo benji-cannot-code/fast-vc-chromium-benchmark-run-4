91e0f3785249b3b2fd513697a323f54acb25dc20
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-03 08:50:33 +0000

    2008-10-03  David Hyatt  <hyatt@apple.com>
            https://bugs.webkit.org/show_bug.cgi?id=21330
    
            Fix Gtk adjustments so that they are properly checked again before creating horizontal/vertical
            scrollbars.
            
            Reviewed by Oliver Hunt
    
            * platform/ScrollView.cpp:
            (WebCore::ScrollView::setHasHorizontalScrollbar):
            (WebCore::ScrollView::setHasVerticalScrollbar):
            (WebCore::ScrollView::platformHasHorizontalAdjustment):
            (WebCore::ScrollView::platformHasVerticalAdjustment):
            * platform/ScrollView.h:
            * platform/gtk/ScrollViewGtk.cpp:
            (WebCore::ScrollView::adjustmentChanged):
            (WebCore::ScrollView::ScrollView):
            (WebCore::ScrollView::~ScrollView):
            (WebCore::ScrollView::setGtkAdjustments):
            (WebCore::ScrollView::platformHandleHorizontalAdjustment):
            (WebCore::ScrollView::platformHandleVerticalAdjustment):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ScrollView.h
+++ third_party/WebKit/WebCore/platform/ScrollView.h
@@ -138,7 +138,10 @@ public:
     
     // This method scrolls by lines, pages or pixels.
     bool scroll(ScrollDirection, ScrollGranularity);
-    
+        
+    // Scroll the actual contents of the view (either blitting or invalidating as needed).
+    void scrollContents(const IntSize& scrollDelta);
+
     // This gives us a means of blocking painting on our scrollbars until the first layout has occurred.
     void setScrollbarsSuppressed(bool suppressed, bool repaintOnUnsuppress = false);
     bool scrollbarsSuppressed() const { return m_scrollbarsSuppressed; }
@@ -243,9 +246,6 @@ private:
     // Called to update the scrollbars to accurately reflect the state of the view.
     void updateScrollbars(const IntSize& desiredOffset);
 
-    // Scroll the actual contents of the view (either blitting or invalidating as needed).
-    void scrollContents(const IntSize& scrollDelta);
-
     // These methods are used to create/destroy scrollbars.
     void setHasHorizontalScrollbar(bool);
     void setHasVerticalScrollbar(bool);
@@ -267,6 +267,8 @@ private:
     bool platformIsOffscreen() const;
     bool platformHandleHorizontalAdjustment(const IntSize&);
     bool platformHandleVerticalAdjustment(const IntSize&);
+    bool platformHasHorizontalAdjustment() const;
+    bool platformHasVerticalAdjustment() const;
 
 #if PLATFORM(MAC) && defined __OBJC__
 public:
@@ -274,14 +276,6 @@ public:
 
 private:
     NSScrollView<WebCoreFrameScrollView>* scrollView() const;
-#endif
-
-#if !PLATFORM(MAC) && !PLATFORM(WIN) && !PLATFORM(QT)
-// FIXME: ScrollViewPrivate will eventually be completely gone.
-    class ScrollViewPrivate;
-    ScrollViewPrivate* m_data;
-
-    friend class ScrollViewPrivate; // FIXME: Temporary.
 #endif
 
 #if !PLATFORM(MAC) && !PLATFORM(WX)
@@ -300,6 +294,9 @@ private:
 #if PLATFORM(GTK)
 public:
     void setGtkAdjustments(GtkAdjustment* hadj, GtkAdjustment* vadj);
+    GtkAdjustment* m_horizontalAdjustment;
+    GtkAdjustment* m_verticalAdjustment;
+    void setScrollOffset(const IntSize& offset) { m_scrollOffset = offset; }
 #endif
 
 #if PLATFORM(WX)
@@ -308,6 +305,8 @@ public:
 
 private:
     void adjustScrollbars(int x = -1, int y = -1, bool refresh = true);
+    class ScrollViewPrivate;
+    ScrollViewPrivate* m_data;
 #endif
 
 }; // class ScrollView
