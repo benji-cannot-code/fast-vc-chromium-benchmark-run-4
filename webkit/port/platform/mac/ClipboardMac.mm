3b51146a40c3faa8c3fe0c9c08920d8fc0ef144d
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-03 21:26:09 +0000

    Drag FrameMac.mm through the mud to get it merged.  Do the same with ClipboardMac.mm, which was a piece of cake by comparison. Review URL: http://codereview.chromium.org/6465
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2852 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/mac/ClipboardMac.mm
+++ webkit/port/platform/mac/ClipboardMac.mm
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "Pasteboard.h"
 #import "Range.h"
 #import "RenderImage.h"
-#import "WebCoreFrameBridge.h"
 #import "WebCoreSystemInterface.h"
 
 @class WebArchive;
