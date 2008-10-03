d171ed60cafeaaf6600217912a762b9d0dacb0cf
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-03 03:05:29 +0000

    Fixes a handful of broken tests after the merge that resulted from us not compiling some new binding classes. Note that I'm removing the expected output for variable-iteration-test as it was wrong. We're not passing this one for a different reason (no idea what that is now).
    BUG=3010
    TEST=covered by tests
    
    Review URL: http://codereview.chromium.org/6228
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2824 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/v8/v8_index.cpp
+++ webkit/port/bindings/v8/v8_index.cpp
@@ -59,6 +59,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8CSSValue.h"
 #include "V8CSSValueList.h"
 #include "V8CSSStyleSheet.h"
+#include "V8CSSVariablesDeclaration.h"
+#include "V8CSSVariablesRule.h"
 #include "V8Document.h"
 #include "V8DocumentFragment.h"
 #include "V8DocumentType.h"
