d1a4fbf4b291233634356a1817a7d25fb161b064
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-03 21:32:24 +0000

    Update include headers for SkGraphicsContextMac.cpp
    Review URL: http://codereview.chromium.org/6467
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2854 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/graphics/SkGraphicsContextMac.cpp
+++ webkit/port/platform/graphics/SkGraphicsContextMac.cpp
@@ -3,13 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "config.h"
 #include "SkGraphicsContext.h"
 
 #include "base/gfx/platform_canvas_mac.h"
 #include "base/gfx/image_operations.h"
 #include "base/gfx/skia_utils_mac.h"
 #include "base/logging.h"
-#include "GraphicsContextPrivate.h"
+#include "GraphicsContextPlatformPrivate.h"
 #include "SkBitmap.h"
 #include "NativeImageSkia.h"
 #include "SkiaUtils.h"
