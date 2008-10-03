d3fc5053428ec73b44da4b8718dcaffae9f9d00a
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-03 19:05:46 +0000

    Use the full src-relative path to gtest.h
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2841 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_path_unittest.cc
+++ base/file_path_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 
 #include "base/basictypes.h"
-#include "gtest/gtest.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 // This macro helps avoid wrapped lines in the test structs.
 #define FPL(x) FILE_PATH_LITERAL(x)
