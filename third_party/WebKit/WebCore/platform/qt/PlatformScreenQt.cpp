0fd9e11e3bdb8810f16ae80fc200665be5ed7538
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-04 01:09:37 +0000

    2008-10-03  David Hyatt  <hyatt@apple.com>
            Fix Qt bustage from missing include.
    
            * platform/qt/PlatformScreenQt.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37273 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/PlatformScreenQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/PlatformScreenQt.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FloatRect.h"
 #include "Frame.h"
 #include "FrameView.h"
+#include "HostWindow.h"
 #include "Widget.h"
 #include <QApplication>
 #include <QDesktopWidget>
