97593d56c18bf2000033ca030c12550d38078535
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-05 02:21:53 +0000

    Upgrade to WordPress 2.6.2.
    * blog:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37309 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitSite/blog/wp-includes/version.php
+++ third_party/WebKit/WebKitSite/blog/wp-includes/version.php
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  * @global string $wp_version
  */
-$wp_version = '2.6.1';
+$wp_version = '2.6.2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
