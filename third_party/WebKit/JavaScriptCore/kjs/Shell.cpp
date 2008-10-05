9bc2f24d2727c59a567a684f018740cb3aebaa59
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-05 17:38:32 +0000

    2008-10-05  Gavin Barraclough  <barraclough@apple.com>
            Reviewed by Cameron Zwarich.
    
            Fix for bug #21387 - using SamplingTool with CTI.
    
            (1) A repatch offset offset changes due to an additional instruction to update SamplingTool state.
            (2) Fix an incusion order problem due to ExecState changes.
            (3) Change to a MACHINE_SAMPLING macro, use of exec should now be accessing global data.
    
            * VM/CTI.h:
            (JSC::CTI::execute):
            * VM/SamplingTool.h:
            (JSC::SamplingTool::privateExecuteReturned):
            * kjs/Shell.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/Shell.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/Shell.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSArray.h"
 #include "JSLock.h"
 #include "PrototypeFunction.h"
+#include "SamplingTool.h"
 #include "completion.h"
 #include "interpreter.h"
 #include <math.h>
