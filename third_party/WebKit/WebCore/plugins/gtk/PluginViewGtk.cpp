b6517acad7b77b8d11021dece779e7db56a06dca
jmalonzo@webkit.org <jmalonzo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-05 21:57:05 +0000

    2008-10-05  Luke Kenneth Casson Leighton  <lkcl@lkcl.net>
            Reviewed by David Hyatt.
    
            GTK_WINDOW_HWND not GTK_WINDOWING_HWND
            https://bugs.webkit.org/show_bug.cgi?id=20725
    
            Updated to ToT by Jan Alonzo.
    
            * plugins/gtk/PluginViewGtk.cpp:
            (WebCore::PluginView::getValue):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37319 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/gtk/PluginViewGtk.cpp
+++ third_party/WebKit/WebCore/plugins/gtk/PluginViewGtk.cpp
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gdk/gdkx.h>
 #endif
 #ifdef GDK_WINDOWING_WIN32
+#include "PluginMessageThrottlerWin.h"
 #include <gdk/gdkwin32.h>
 #endif
 
@@ -437,13 +438,13 @@ NPError PluginView::getValue(NPNVariable variable, void* value)
 #endif
 
         case NPNVnetscapeWindow: {
-            void* w = reinterpret_cast<void*>(value);
-
 #if PLATFORM(X11)
+            void* w = reinterpret_cast<void*>(value);
             *((XID *)w) = GDK_WINDOW_XWINDOW(m_parentFrame->view()->hostWindow()->platformWindow()->window);
 #endif
 #ifdef GDK_WINDOWING_WIN32
-            *((HWND *)w) = GDK_WINDOWING_HWND(m_parentFrame->view()->hostWindow()->platformWindow()->window);
+            HGDIOBJ* w = reinterpret_cast<HGDIOBJ*>(value);
+            *w = GDK_WINDOW_HWND(m_parentFrame->view()->hostWindow()->platformWindow()->window);
 #endif
             return NPERR_NO_ERROR;
         }
