a364abee08059e425d76de64264e0465eb66cfb4
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-06 20:02:07 +0000

    2008-10-06  Anders Carlsson  <andersca@apple.com>
            Reviewed by David Hyatt.
    
            Convert more methods over to C++.
            
            * Plugins/WebBaseNetscapePluginStream.h:
            * Plugins/WebBaseNetscapePluginStream.mm:
            (WebNetscapePluginStream::reasonForError):
            (WebNetscapePluginStream::destroyStreamWithReason):
            (WebNetscapePluginStream::cancelLoadWithError):
            (-[WebBaseNetscapePluginStream cancelLoadWithError:]):
            (WebNetscapePluginStream::destroyStreamWithError):
            (-[WebBaseNetscapePluginStream destroyStreamWithError:]):
            (-[WebBaseNetscapePluginStream cancelLoadAndDestroyStreamWithError:]):
            (-[WebBaseNetscapePluginStream _deliverDataToFile:]):
            (-[WebBaseNetscapePluginStream finishedLoading]):
            * Plugins/WebBaseNetscapePluginView.mm:
            (-[WebBaseNetscapePluginView webFrame:didFinishLoadWithError:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37339 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebBaseNetscapePluginView.mm
+++ third_party/WebKit/WebKit/mac/Plugins/WebBaseNetscapePluginView.mm
@@ -2056,9 +2056,8 @@ static inline void getNPRect(const NSRect& nr, NPRect& npr)
 - (void)webFrame:(WebFrame *)webFrame didFinishLoadWithError:(NSError *)error
 {
     NPReason reason = NPRES_DONE;
-    if (error != nil) {
-        reason = [WebBaseNetscapePluginStream reasonForError:error];
-    }    
+    if (error != nil)
+        reason = WebNetscapePluginStream::reasonForError(error);
     [self webFrame:webFrame didFinishLoadWithReason:reason];
 }
 
