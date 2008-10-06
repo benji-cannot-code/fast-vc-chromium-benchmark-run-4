24204db1d8bee135d0ae18e4c5e0d9f5472c4479
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-06 06:05:19 +0000

    2008-10-05  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Cameron Zwarich.
            
            - fix releas-only test failures caused by the fix to bug 21375
    
            * VM/Machine.cpp:
            (JSC::Machine::unwindCallFrame): Update ExecState while unwinding call frames;
            it now matters more to have a still-valid ExecState, since dynamicGlobalObject
            will make use of the ExecState's scope chain.
            * VM/Machine.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/Machine.cpp
+++ third_party/WebKit/JavaScriptCore/VM/Machine.cpp
@@ -756,7 +756,7 @@ bool Machine::isOpcode(Opcode opcode)
 
 //#endif
 
-NEVER_INLINE bool Machine::unwindCallFrame(ExecState* exec, JSValue* exceptionValue, const Instruction*& vPC, CodeBlock*& codeBlock, Register*& r)
+NEVER_INLINE bool Machine::unwindCallFrame(ExecState*& exec, JSValue* exceptionValue, const Instruction*& vPC, CodeBlock*& codeBlock, Register*& r)
 {
     CodeBlock* oldCodeBlock = codeBlock;
     ScopeChainNode* scopeChain = this->scopeChain(r);
@@ -793,6 +793,7 @@ NEVER_INLINE bool Machine::unwindCallFrame(ExecState* exec, JSValue* exceptionVa
     
     void* returnPC = r[RegisterFile::ReturnPC].v();
     r = r[RegisterFile::CallerRegisters].r();
+    exec = CallFrame::create(r);
     if (isHostCallFrame(r))
         return false;
 
