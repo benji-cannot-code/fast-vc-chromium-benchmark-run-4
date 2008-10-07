da4ce73b117bf501b519ae89138de2822448a736
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-07 20:25:51 +0000

    Provide webcoresysteminterface; link testshell against it and libWebKitSystemInterfaceLeopard. Review URL: http://codereview.chromium.org/6551
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2958 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/webkit.xcodeproj/webcoresysteminterface_prebuild.sh
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+# Copyright (c) 2008 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -ex
+GENERATED_DIR="${CONFIGURATION_TEMP_DIR}/generated"
+
+# Allow framework-style #include of <WebCore/WebCoreSystemInterface.h>
+FORWARDING_DIR="${GENERATED_DIR}/webcoresysteminterface"
+mkdir -p "${FORWARDING_DIR}"
+cd "${FORWARDING_DIR}"
+ln -fhs "../../../../../third_party/WebKit/WebCore/platform/mac" "WebCore"
