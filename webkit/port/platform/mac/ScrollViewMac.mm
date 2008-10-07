e8f4a1db571ff835010fc378f106426f6f902d3d
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-07 21:23:27 +0000

    Provide a do-nothing constructor and destructor for the Mac ScrollView. Review URL: http://codereview.chromium.org/6318
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2965 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/mac/ScrollViewMac.mm
+++ webkit/port/platform/mac/ScrollViewMac.mm
@@ -50,6 +50,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+ScrollView::ScrollView() {
+}
+
+ScrollView::~ScrollView() {
+}
+
 int ScrollView::visibleWidth() const {
   return frameGeometry().width();
 }
