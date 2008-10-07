5a9e7afd5321da1e6b8038599581872c1532b0ed
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-07 18:17:37 +0000

    2008-10-07  Cameron Zwarich  <zwarich@apple.com>
            Rubber-stamped by Mark Rowe.
    
            Roll out r37370.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37381 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/PropertyMap.h
+++ third_party/WebKit/JavaScriptCore/kjs/PropertyMap.h
@@ -43,13 +43,11 @@ namespace JSC {
 
     struct PropertyMapEntry {
         UString::Rep* key;
-        unsigned offset;
         unsigned attributes;
         unsigned index;
 
         PropertyMapEntry(UString::Rep* k, int a)
             : key(k)
-            , offset(0)
             , attributes(a)
             , index(0)
         {
@@ -97,10 +95,11 @@ namespace JSC {
 
         bool isEmpty() { return !m_table; }
 
-        size_t get(const Identifier& propertyName);
-        size_t get(const Identifier& propertyName, unsigned& attributes);
-        size_t put(const Identifier& propertyName, unsigned attributes);
-        size_t remove(const Identifier& propertyName);
+        size_t put(const Identifier& propertyName, JSValue*, unsigned attributes, bool checkReadOnly, JSObject* slotBase, PutPropertySlot&, PropertyStorage&);
+        void remove(const Identifier& propertyName, PropertyStorage&);
+
+        size_t getOffset(const Identifier& propertyName);
+        size_t getOffset(const Identifier& propertyName, unsigned& attributes);
 
         void getEnumerablePropertyNames(PropertyNameArray&) const;
 
@@ -116,17 +115,16 @@ namespace JSC {
         typedef PropertyMapEntry Entry;
         typedef PropertyMapHashTable Table;
 
-        void expand();
-        void rehash();
-        void rehash(unsigned newTableSize);
-        void createTable();
+        void expand(PropertyStorage&);
+        void rehash(PropertyStorage&);
+        void rehash(unsigned newTableSize, PropertyStorage&);
+        void createTable(PropertyStorage&);
 
-        void insert(const Entry&);
+        void insert(const Entry&, JSValue*, PropertyStorage&);
 
-        void checkConsistency();
+        void checkConsistency(PropertyStorage&);
 
         Table* m_table;
-        Vector<unsigned> m_deletedOffsets;
         bool m_getterSetterFlag : 1;
     };
 
@@ -136,7 +134,7 @@ namespace JSC {
     {
     }
 
-    inline size_t PropertyMap::get(const Identifier& propertyName)
+    inline size_t PropertyMap::getOffset(const Identifier& propertyName)
     {
         ASSERT(!propertyName.isNull());
 
@@ -156,7 +154,7 @@ namespace JSC {
             return WTF::notFound;
 
         if (rep == m_table->entries()[entryIndex - 1].key)
-            return m_table->entries()[entryIndex - 1].offset;
+            return entryIndex - 2;
 
 #if DUMP_PROPERTYMAP_STATS
         ++numCollisions;
@@ -176,7 +174,7 @@ namespace JSC {
                 return WTF::notFound;
 
             if (rep == m_table->entries()[entryIndex - 1].key)
-                return m_table->entries()[entryIndex - 1].offset;
+                return entryIndex - 2;
         }
     }
 
