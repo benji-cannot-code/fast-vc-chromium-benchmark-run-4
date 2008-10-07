8a0c0c2e3cf6fc12459865e6468b71299ce963c3
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-07 01:11:38 +0000

    2008-10-06  Mark Mentovai  <mark@moxienet.com>
            Reviewed by Sam Weinig.
    
            * platform/network/mac/FormDataStreamMac.mm: #import <wtf/Threading.h>
            to get the declaration for isMainThread().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/mac/FormDataStreamMac.mm
+++ third_party/WebKit/WebCore/platform/network/mac/FormDataStreamMac.mm
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <wtf/Assertions.h>
 #import <wtf/HashMap.h>
 #import <wtf/MainThread.h>
+#import <wtf/Threading.h>
 
 namespace WebCore {
 
