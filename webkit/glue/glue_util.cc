b987a6d11b1d868faef01d445adee3e16854d678
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-08 20:40:42 +0000

    base/logging.h defines LOG when webkit is about to so undef it
    Review URL: http://codereview.chromium.org/6204
    Patch from icefox.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3042 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/glue_util.cc
+++ webkit/glue/glue_util.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 
 #pragma warning(push, 0)
+#undef LOG
 #include "CString.h"
 #include "PlatformString.h"
 #pragma warning(pop)
