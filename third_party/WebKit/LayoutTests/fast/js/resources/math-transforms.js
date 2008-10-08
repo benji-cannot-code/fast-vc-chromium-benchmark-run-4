c574565491c7a2d6d61f76293fe08c7e309af90e
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-08 10:27:08 +0000

    2008-10-08  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Oliver Hunt.
            
            - update regression testing system to be able to tell 0 from -0
            
            * fast/js/resources/js-test-pre.js:
    
            Update tests and results appropriately.
    
            * fast/js/resources/constant-folding.js:
            * fast/js/constant-folding-expected.txt:
            * fast/js/math-transforms-expected.txt:
            * fast/js/resources/math-transforms.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37416 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/resources/math-transforms.js
+++ third_party/WebKit/LayoutTests/fast/js/resources/math-transforms.js
@@ -28,10 +28,10 @@ for (var name in values) {
     var numForStr = numberForString[name] ? numberForString[name] : name;
 
     shouldBe("values." + name + " * 1", "+values." + name);
-    shouldBe("values." + name + " * 1", "" + values[numForStr]);
+    shouldBe("values." + name + " * 1", stringify(values[numForStr]));
 
     shouldBe("1 * values." + name, "+values." + name);
-    shouldBe("1 * values." + name, "" + values[numForStr]);
+    shouldBe("1 * values." + name, stringify(values[numForStr]));
 }
 
 for (var name1 in values) {
@@ -40,25 +40,25 @@ for (var name1 in values) {
         var numForStr2 = numberForString[name2] ? numberForString[name2] : name2;
 
         shouldBe("+values." + name1 + " * values." + name2, "values." + name1 + " * values." + name2);
-        shouldBe("+values." + name1 + " * values." + name2, "" + (values[name1] * values[name2]));
+        shouldBe("+values." + name1 + " * values." + name2, stringify(values[name1] * values[name2]));
         shouldBe("values." + name1 + " * +values." + name2, "values." + name1 + " * values." + name2);
-        shouldBe("values." + name1 + " * +values." + name2, "" + (values[name1] * values[name2]));
+        shouldBe("values." + name1 + " * +values." + name2, stringify(values[name1] * values[name2]));
         shouldBe("+values." + name1 + " * +values." + name2, "values." + name1 + " * values." + name2);
-        shouldBe("+values." + name1 + " * +values." + name2, "" + (values[name1] * values[name2]));
+        shouldBe("+values." + name1 + " * +values." + name2, stringify(values[name1] * values[name2]));
 
         shouldBe("+values." + name1 + " / values." + name2, "values." + name1 + " / values." + name2);
-        shouldBe("+values." + name1 + " / values." + name2, "" + (values[name1] / values[name2]));
+        shouldBe("+values." + name1 + " / values." + name2, stringify(values[name1] / values[name2]));
         shouldBe("values." + name1 + " / +values." + name2, "values." + name1 + " / values." + name2);
-        shouldBe("values." + name1 + " / +values." + name2, "" + (values[name1] / values[name2]));
+        shouldBe("values." + name1 + " / +values." + name2, stringify(values[name1] / values[name2]));
         shouldBe("+values." + name1 + " / +values." + name2, "values." + name1 + " / values." + name2);
-        shouldBe("+values." + name1 + " / +values." + name2, "" + (values[name1] / values[name2]));
+        shouldBe("+values." + name1 + " / +values." + name2, stringify(values[name1] / values[name2]));
 
         shouldBe("+values." + name1 + " - values." + name2, "values." + name1 + " - values." + name2);
-        shouldBe("+values." + name1 + " - values." + name2, "" + (values[name1] - values[name2]));
+        shouldBe("+values." + name1 + " - values." + name2, stringify(values[name1] - values[name2]));
         shouldBe("values." + name1 + " - +values." + name2, "values." + name1 + " - values." + name2);
-        shouldBe("values." + name1 + " - +values." + name2, "" + (values[name1] - values[name2]));
+        shouldBe("values." + name1 + " - +values." + name2, stringify(values[name1] - values[name2]));
         shouldBe("+values." + name1 + " - +values." + name2, "values." + name1 + " - values." + name2);
-        shouldBe("+values." + name1 + " - +values." + name2, "" + (values[name1] - values[name2]));
+        shouldBe("+values." + name1 + " - +values." + name2, stringify(values[name1] - values[name2]));
     }
 }
 
