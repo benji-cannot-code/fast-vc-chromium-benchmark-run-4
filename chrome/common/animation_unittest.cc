d729f14248c5605b049d46875acfca384badf7c9
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-08 21:27:02 +0000

    Build some code and unit tests inside chrome.  This depended on sqlite, so adding that too. Review URL: http://codereview.chromium.org/6361
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3055 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/animation_unittest.cc
+++ chrome/common/animation_unittest.cc
@@ -10,11 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace std;
 
-namespace {
-  class AnimationTest: public testing::Test {
-   private:
-    MessageLoopForUI message_loop_;
-  };
+class AnimationTest: public testing::Test {
+ private:
+  MessageLoopForUI message_loop_;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
