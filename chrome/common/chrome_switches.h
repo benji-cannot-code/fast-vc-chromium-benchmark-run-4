357844dd53b07a35d3c170dfea7c1f57cb2c9c5b
maruel@google.com <maruel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-08 17:01:47 +0000

    Adds an --incognito switch that can be used to launch Chrome in Incognito mode. You can combine this with --app= to launch an app in Chrome that has no effect on the user's profile.
    Note that this is not the same thing as launching chrome.exe
    --user-data-dir=/dev/null, which would give you an completely empty profile
    instead of loading the user's profile as read-only.
    
    BUG=1790,2012
    
    Patch by Yarin Kaul <yarin.kaul@gmail.com>
    Review URL: http://codereview.chromium.org/6584
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3013 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -129,6 +129,7 @@ extern const wchar_t kEnableP13n[];
 
 extern const wchar_t kSdchFilter[];
 
+extern const wchar_t kIncognito[];
 extern const wchar_t kUseNewSafeBrowsing[];
 
 }  // namespace switches
