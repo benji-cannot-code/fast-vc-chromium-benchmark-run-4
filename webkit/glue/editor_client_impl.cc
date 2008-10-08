9e4f5ef8b590aba2763dbe7a9489edca1774a5a0
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-08 20:51:21 +0000

    Remove unused static function
    Review URL: http://codereview.chromium.org/6216
    Patch from icefox.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3044 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/editor_client_impl.cc
+++ webkit/glue/editor_client_impl.cc
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // unbounded memory.  This is the maximum number of distinct undoable
 // actions -- unbroken stretches of typed characters are coalesced
 // into a single action.
-static const int kMaximumUndoStackDepth = 1000;
+static const size_t kMaximumUndoStackDepth = 1000;
 
 namespace {
 
@@ -337,17 +337,6 @@ void EditorClientImpl::redo() {
     // reapply will call us back to push this command onto the undo stack.
     in_redo_ = false;
   }
-}
-
-// Get the distance we should go (in pixels) when doing a pageup/pagedown.
-static int GetVerticalPageDistance(WebCore::KeyboardEvent* event) {
-  if (event->target()) {
-    WebCore::Node* node = event->target()->toNode();
-    if (node && node->renderer())
-      return node->renderer()->contentHeight();
-  }
-
-  return 0;
 }
 
 // The below code was adapted from the WebKit file webview.cpp
