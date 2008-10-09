aff7dca38a9223d9817594744ff50bc54d2328cd
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-09 20:40:32 +0000

    Roll back this section of the previous change to see if it is necessary.
    http://crbug.com/3264
    Review URL: http://codereview.chromium.org/6394
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3126 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/custom_frame_window.h
+++ chrome/views/custom_frame_window.h
@@ -49,7 +49,6 @@ class CustomFrameWindow : public Window {
   virtual void DisableInactiveRendering(bool disable);
 
   // Overridden from HWNDViewContainer:
-  virtual void OnEnterIdle(UINT reason, HWND window);
   virtual void OnInitMenu(HMENU menu);
   virtual void OnInitMenuPopup(HMENU menu, UINT position, BOOL is_system_menu);
   virtual void OnMouseLeave();
