67c44c48efa7171618b4b4e1ce7e4b81c0af18af
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-09 14:26:27 +0000

    2008-10-09  Ariya Hidayat  <ariya.hidayat@trolltech.com>
            Reviewed by Simon.
    
            Build fix for MinGW.
    
            * wtf/AlwaysInline.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/AlwaysInline.h
+++ third_party/WebKit/JavaScriptCore/wtf/AlwaysInline.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Platform.h"
 
 #ifndef ALWAYS_INLINE
-#if COMPILER(GCC) && defined(NDEBUG)
+#if COMPILER(GCC) && defined(NDEBUG) && !COMPILER(MINGW)
 #define ALWAYS_INLINE inline __attribute__ ((__always_inline__))
 #elif COMPILER(MSVC) && defined(NDEBUG)
 #define ALWAYS_INLINE __forceinline
