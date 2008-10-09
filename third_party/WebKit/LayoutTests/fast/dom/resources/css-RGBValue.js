ed0150eac1c44e9b9a0b6208814843685bf1e253
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-09 00:14:50 +0000

    WebCore:
    2008-10-08  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Cameron Zwarich.
    
            Fix for https://bugs.webkit.org/show_bug.cgi?id=21241
            REGRESSION (r36977): getRGBColorValue().red returning incorrect value
    
            Update JSRGBColor to use the new static function per getter approach.
    
            Test: fast/dom/css-RGBValue.html
    
            * bindings/js/JSRGBColor.cpp:
            (WebCore::):
            (jsRGBColorRed):
            (jsRGBColorGreen):
            (jsRGBColorBlue):
            * bindings/js/JSRGBColor.h:
    
    LayoutTests:
    
    2008-10-08  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Cameron Zwarich.
    
            Test for https://bugs.webkit.org/show_bug.cgi?id=21241
            REGRESSION (r36977): getRGBColorValue().red returning incorrect value
    
            * fast/dom/css-RGBValue.html: Added.
            * fast/dom/resources/css-RGBValue.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37432 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/resources/css-RGBValue.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("This test ensures that objects RGBColor objects return the correct values.");
+
+var element = document.createElement('div');
+document.documentElement.appendChild(element);
+element.style.color = "rgb(10, 20, 30)";
+
+shouldBe("window.getComputedStyle(element, '').getPropertyCSSValue('color').getRGBColorValue().red.getFloatValue(CSSPrimitiveValue.CSS_NUMBER)", "10");
+shouldBe("window.getComputedStyle(element, '').getPropertyCSSValue('color').getRGBColorValue().green.getFloatValue(CSSPrimitiveValue.CSS_NUMBER)", "20");
+shouldBe("window.getComputedStyle(element, '').getPropertyCSSValue('color').getRGBColorValue().blue.getFloatValue(CSSPrimitiveValue.CSS_NUMBER)", "30");
+
+successfullyParsed = true;
