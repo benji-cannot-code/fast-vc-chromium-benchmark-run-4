205c77a2a9b5173b139e24da14c3a12b8c293556
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-09 18:02:59 +0000

    Compile fix on Linux GURL constructor takes std::string or UTF16String not std::wstring
        Remove the windows function _wtoi and use the cross platform function
        StringToInt from string_util.h for converting the string to an int
    
    Review URL: http://codereview.chromium.org/5202
    Patch from icefox.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3109 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/dom_operations.cc
+++ webkit/glue/dom_operations.cc
@@ -315,7 +315,6 @@ static bool FindFormInputElements(WebCore::HTMLFormElement* fe,
                                   const FormData& data,
                                   FormElements* result) {
   Vector<RefPtr<WebCore::Node> > temp_elements;
-  bool found_elements = true;
   // Loop through the list of elements we need to find on the form in
   // order to autofill it. If we don't find any one of them, abort 
   // processing this form; it can't be the right one.
@@ -363,7 +362,7 @@ static void FindFormElements(WebView* view,
     if (!doc->isHTMLDocument()) 
       continue;
 
-    GURL full_origin(StringToStdWString(doc->documentURI()));
+    GURL full_origin(StringToStdString(doc->documentURI()));
     if (data.origin != full_origin.ReplaceComponents(rep))
       continue;
 
@@ -655,7 +654,10 @@ static int ParseSingleIconSize(const std::wstring& text) {
     if (!(text[i] >= L'0' && text[i] <= L'9'))
       return 0;
   }
-  return _wtoi(text.c_str());
+  int output;
+  if (!StringToInt(text, &output))
+    return 0;
+  return output;
 }
 
 // Parses an icon size. An icon size must match the following regex:
@@ -700,7 +702,7 @@ static void AddInstallIcon(WebCore::HTMLLinkElement* link,
   if (href.isEmpty() || href.isNull())
     return;
 
-  GURL url(webkit_glue::StringToStdWString(href));
+  GURL url(webkit_glue::StringToStdString(href));
   if (!url.is_valid())
     return;
 
@@ -753,7 +755,7 @@ void GetApplicationInfo(WebView* view, WebApplicationInfo* app_info) {
           app_info->description =
               webkit_glue::StringToStdWString(meta->content());
         } else if (meta->name() == String("application-url")) {
-          std::wstring url = webkit_glue::StringToStdWString(meta->content());
+          std::string url = webkit_glue::StringToStdString(meta->content());
           GURL main_url = main_frame->GetURL();
           app_info->app_url = main_url.is_valid() ?
               main_url.Resolve(url) : GURL(url);
