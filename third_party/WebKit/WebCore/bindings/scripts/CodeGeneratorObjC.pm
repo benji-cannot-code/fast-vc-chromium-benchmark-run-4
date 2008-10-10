9859db94bde8e70ba3bca12d170518b124b3cb75
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-10 04:34:30 +0000

    Tiger build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@37471 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorObjC.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorObjC.pm
@@ -799,10 +799,15 @@ sub GenerateHeader
 
             my $publicInterfaceKey = $property . ";";
 
-            my $declarationSuffix = ";\n";
+            my $availabilityMacro = "";
             if (defined $publicInterfaces{$publicInterfaceKey} and length $publicInterfaces{$publicInterfaceKey}) {
-                $declarationSuffix = " " . $publicInterfaces{$publicInterfaceKey} . $declarationSuffix;
+                $availabilityMacro = $publicInterfaces{$publicInterfaceKey};
             }
+
+            $availabilityMacro = "WEBKIT_PROTOCOL_METHOD_ANNOTATION($availabilityMacro)" if $isProtocol and length $availabilityMacro;
+
+            my $declarationSuffix = ";\n";
+            $declarationSuffix = " $availabilityMacro;\n" if length $availabilityMacro;
 
             my $public = (defined $publicInterfaces{$publicInterfaceKey} or $newPublicClass);
             delete $publicInterfaces{$publicInterfaceKey};
@@ -894,10 +899,16 @@ sub GenerateHeader
                 $fatalError = 1;
             }
 
-            my $declarationSuffix = ";\n";
+            my $availabilityMacro = "";
             if (defined $publicInterfaces{$publicInterfaceKey} and length $publicInterfaces{$publicInterfaceKey}) {
-                $declarationSuffix = " " . $publicInterfaces{$publicInterfaceKey} . ";\n";
+                $availabilityMacro = $publicInterfaces{$publicInterfaceKey};
             }
+
+            $availabilityMacro = "WEBKIT_PROTOCOL_METHOD_ANNOTATION($availabilityMacro)" if $isProtocol and length $availabilityMacro;
+
+            my $functionDeclaration = $functionSig;
+            $functionDeclaration .= " " . $availabilityMacro if length $availabilityMacro;
+            $functionDeclaration .= ";\n";
 
             my $public = (defined $publicInterfaces{$publicInterfaceKey} or $newPublicClass);
             delete $publicInterfaces{$publicInterfaceKey};
@@ -907,8 +918,6 @@ sub GenerateHeader
                 AddForwardDeclarationsForType($type, 1) if $needsDeprecatedVersion;
                 AddForwardDeclarationsForType($type, $public) unless $public and $needsDeprecatedVersion;
             }
-
-            my $functionDeclaration = $functionSig . $declarationSuffix;
 
             push(@headerFunctions, $functionDeclaration) if $public;
             push(@privateHeaderFunctions, $functionDeclaration) unless $public;
@@ -926,6 +935,7 @@ sub GenerateHeader
                 }
 
                 $availabilityMacro = "WEBKIT_CATEGORY_METHOD_ANNOTATION($availabilityMacro)" unless $isProtocol;
+                $availabilityMacro = "WEBKIT_PROTOCOL_METHOD_ANNOTATION($availabilityMacro)" if $isProtocol;
 
                 $functionDeclaration = "$deprecatedFunctionSig $availabilityMacro;\n";
 
