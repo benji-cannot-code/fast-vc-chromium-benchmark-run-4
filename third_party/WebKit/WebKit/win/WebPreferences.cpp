01acf5b1d9c0a9ed18597c613424fdc65b3f6555
adachan@apple.com <adachan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-10 20:43:01 +0000

    Bug 21526: The hashmap webPreferencesInstances in WebPreferences.cpp can         potentially hold onto stale instances of WebPreferences         https://bugs.webkit.org/show_bug.cgi?id=21526
            webPreferencesInstances now keeps a reference to all the instances it holds.
            Fix up callers of WebPreferences::removeReferenceForIdentifier() to call 
            it after they have released their references to the WebPreferences objects.
    
            Reviewed by Adam Roben.
    
            * WebPreferences.cpp:
            (WebPreferences::getInstanceForIdentifier):
            (WebPreferences::removeReferenceForIdentifier):
            * WebView.cpp:
            (WebView::close):
            (WebView::setPreferences):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37489 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebPreferences.cpp
+++ third_party/WebKit/WebKit/win/WebPreferences.cpp
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKit.h"
 #include "WebKitDLL.h"
 #include "WebPreferences.h"
-#include "WebKit.h"
 
+#include "COMPtr.h"
 #include "WebNotificationCenter.h"
 #include "WebPreferenceKeysPrivate.h"
 
@@ -100,7 +100,7 @@ static bool booleanValueForPreferencesValue(CFPropertyListRef value)
 
 static CFDictionaryRef defaultSettings;
 
-static HashMap<WebCore::String, WebPreferences*> webPreferencesInstances;
+static HashMap<WebCore::String, COMPtr<WebPreferences> > webPreferencesInstances;
 
 WebPreferences* WebPreferences::sharedStandardPreferences()
 {
@@ -153,7 +153,7 @@ WebPreferences* WebPreferences::getInstanceForIdentifier(BSTR identifier)
         return sharedStandardPreferences();
 
     WebCore::String identifierString(identifier, SysStringLen(identifier));
-    return webPreferencesInstances.get(identifierString);
+    return webPreferencesInstances.get(identifierString).get();
 }
 
 void WebPreferences::setInstance(WebPreferences* instance, BSTR identifier)
@@ -170,7 +170,7 @@ void WebPreferences::removeReferenceForIdentifier(BSTR identifier)
         return;
 
     WebCore::String identifierString(identifier, SysStringLen(identifier));
-    WebPreferences* webPreference = webPreferencesInstances.get(identifierString);
+    WebPreferences* webPreference = webPreferencesInstances.get(identifierString).get();
     if (webPreference && webPreference->m_refCount == 1)
         webPreferencesInstances.remove(identifierString);
 }
