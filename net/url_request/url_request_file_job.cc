21461c534c163cde91e0a9a31094985d1351b19a
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-10 19:07:49 +0000

    add file:// handling to mac and linux builds Review URL: http://codereview.chromium.org/7230
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3232 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_file_job.cc
+++ net/url_request/url_request_file_job.cc
@@ -29,7 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/base/net_util.h"
 #include "net/url_request/url_request.h"
+#if defined(OS_WIN)
 #include "net/url_request/url_request_file_dir_job.h"
+#endif
 
 #if defined(OS_WIN)
 class URLRequestFileJob::AsyncResolver :
@@ -75,8 +77,10 @@ URLRequestJob* URLRequestFileJob::Factory(
   std::wstring file_path;
   if (net::FileURLToFilePath(request->url(), &file_path)) {
     if (file_path[file_path.size() - 1] == file_util::kPathSeparator) {
+#if defined(OS_WIN)
       // Only directories have trailing slashes.
       return new URLRequestFileDirJob(request, file_path);
+#endif
     }
   }
 
@@ -101,20 +105,22 @@ URLRequestFileJob::~URLRequestFileJob() {
 }
 
 void URLRequestFileJob::Start() {
+#if defined(OS_WIN)
   // Resolve UNC paths on a background thread.
   if (!file_path_.compare(0, 2, L"\\\\")) {
     DCHECK(!async_resolver_);
     async_resolver_ = new AsyncResolver(this);
     WorkerPool::PostTask(FROM_HERE, NewRunnableMethod(
         async_resolver_.get(), &AsyncResolver::Resolve, file_path_), true);
-  } else {
-    file_util::FileInfo file_info;
-    bool exists = file_util::GetFileInfo(file_path_, &file_info);
-
-    // Continue asynchronously.
-    MessageLoop::current()->PostTask(FROM_HERE, NewRunnableMethod(
-        this, &URLRequestFileJob::DidResolve, exists, file_info));
+    return;
   }
+#endif
+  file_util::FileInfo file_info;
+  bool exists = file_util::GetFileInfo(file_path_, &file_info);
+
+  // Continue asynchronously.
+  MessageLoop::current()->PostTask(FROM_HERE, NewRunnableMethod(
+      this, &URLRequestFileJob::DidResolve, exists, file_info));
 }
 
 void URLRequestFileJob::Kill() {
--- net/url_request/url_request_job_manager.cc
+++ net/url_request/url_request_job_manager.cc
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "net/url_request/url_request_about_job.h"
 #include "net/url_request/url_request_error_job.h"
-#if defined(OS_WIN)
 #include "net/url_request/url_request_file_job.h"
+#if defined(OS_WIN)
 #include "net/url_request/url_request_ftp_job.h"
 #else
 // TODO(playmobil): Implement on non-windows platforms.
@@ -34,8 +34,8 @@ struct SchemeToFactory {
 static const SchemeToFactory kBuiltinFactories[] = {
   { "http", URLRequestHttpJob::Factory },
   { "https", URLRequestHttpJob::Factory },
-#if defined(OS_WIN)
   { "file", URLRequestFileJob::Factory },
+#if defined(OS_WIN)
   { "ftp", URLRequestFtpJob::Factory },
 #else
 // TODO(playmobil): Implement on non-windows platforms.
