2ffd3610622fc293e8c01fd5eb493f8f4abf67fa
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-10 07:04:38 +0000

    WebCore:
    2008-10-09  David Hyatt  <hyatt@apple.com>
    
            Add support for scrollbar orientation as a pseudoclass.  Add support for whether or not the scrollbar
            is active as well.
    
            Reviewed by Tim Hatcher
    
            Added scrollbars/scrollbar-orientation.html
    
            * css/CSSSelector.cpp:
            (WebCore::CSSSelector::extractPseudoType):
            * css/CSSSelector.h:
            (WebCore::CSSSelector::):
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::SelectorChecker::checkScrollbarPseudoClass):
            * platform/Scrollbar.cpp:
            (WebCore::Scrollbar::isWindowActive):
            * platform/Scrollbar.h:
            * rendering/RenderScrollbar.cpp:
            (WebCore::RenderScrollbar::paint):
            (WebCore::RenderScrollbar::partForStyleResolve):
            * rendering/RenderScrollbar.h:
    
    LayoutTests:
    
    2008-10-09  David Hyatt  <hyatt@apple.com>
    
            Add layout test for scrollbars honoring orientation and window active state.
    
            Reviewed by Tim Hatcher
    
            * platform/mac/scrollbars/scrollbar-orientation-expected.checksum: Added.
            * platform/mac/scrollbars/scrollbar-orientation-expected.png: Added.
            * platform/mac/scrollbars/scrollbar-orientation-expected.txt: Added.
            * scrollbars/scrollbar-orientation.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Scrollbar.cpp
+++ third_party/WebKit/WebCore/platform/Scrollbar.cpp
@@ -419,6 +419,11 @@ void Scrollbar::setEnabled(bool e)
     invalidate();
 }
 
+bool Scrollbar::isWindowActive() const
+{
+    return m_client->isActive();
+}
+ 
 void Scrollbar::invalidateRect(const IntRect& rect)
 {
     if (suppressInvalidation())
