3850407c33ba876d8a140d31d5da8451416103ff
dglazkov@google.com <dglazkov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-10 17:30:52 +0000

    Implement cross-platform ExceptionContext (2/2)
    First part #6400 committed as r3216.
    
    Review URL: http://codereview.chromium.org/7081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3219 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bridge/ExceptionContextV8.cpp
+++ webkit/port/bridge/ExceptionContextV8.cpp
@@ -35,13 +35,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-ExceptionContext::ExceptionContext()
-    : m_exception()
-    , m_exceptionCatcher(0)
+// Unlike JSC, which stores exceptions in ExecState that is accessible from
+// ScriptController that is retrievable from Node*, V8 uses static chain of
+// handlers (encapsulated as v8::TryCatch and here as ExceptionCatcher)
+// to track exceptions, so it has no need for Node*.
+ExceptionContext::ExceptionContext(Node* node)
 {
 }
 
-ExceptionContext::~ExceptionContext()
+ExceptionContext::ExceptionContext()
+    : m_exception()
+    , m_exceptionCatcher(0)
 {
 }
 
@@ -61,16 +65,12 @@ bool ExceptionContext::hadException()
     return !m_exception.IsEmpty();
 }
 
-ExceptionContext* ExceptionContext::createFromNode(Node*)
+JSException ExceptionContext::exception() const
 {
-    // Unlike JSC, which stores exceptions in ExecState that is accessible from
-    // ScriptController that is retrievable from Node*, V8 uses static chain of
-    // handlers (encapsulated as v8::TryCatch and here as ExceptionCatcher)
-    // to track exceptions, so it has no need for Node*.
-    return new ExceptionContext();
+    return m_exception;
 }
 
-JSException ExceptionContext::NoException()
+JSException ExceptionContext::noException()
 {
     return v8::Local<v8::Value>();
 }
@@ -94,7 +94,7 @@ void ExceptionCatcher::updateContext()
     if (m_catcher.HasCaught())
         m_context->setException(m_catcher.Exception());
     else
-        m_context->setException(ExceptionContext::NoException());
+        m_context->setException(ExceptionContext::noException());
 }
 
 ExceptionCatcher::~ExceptionCatcher()
