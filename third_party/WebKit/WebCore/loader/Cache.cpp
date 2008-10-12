a0107813da3cc9798512bd50c52b649a7f03fda8
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-12 04:56:20 +0000

    Rubber-stamped by Sam Weinig.
            - change all internal callers to use Cache::evict() instead of remove()
    
            * loader/Cache.cpp:
            (WebCore::Cache::revalidateResource):
            (WebCore::Cache::revalidationSucceeded):
            (WebCore::Cache::pruneDeadResources):
            (WebCore::Cache::setDisabled):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37519 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/Cache.cpp
+++ third_party/WebKit/WebCore/loader/Cache.cpp
@@ -197,13 +197,13 @@ void Cache::revalidateResource(CachedResource* resource, DocLoader* docLoader)
     if (resource->resourceToRevalidate())
         return;
     if (!resource->canUseCacheValidator()) {
-        remove(resource);
+        evict(resource);
         return;
     }
     const String& url = resource->url();
     CachedResource* newResource = createResource(resource->type(), KURL(url), resource->encoding());
     newResource->setResourceToRevalidate(resource);
-    remove(resource);
+    evict(resource);
     m_resources.set(url, newResource);
     newResource->setInCache(true);
     resourceAccessed(newResource);
@@ -217,7 +217,7 @@ void Cache::revalidationSucceeded(CachedResource* revalidatingResource, const Re
     ASSERT(!resource->inCache());
     ASSERT(resource->isLoaded());
     
-    remove(revalidatingResource);
+    evict(revalidatingResource);
 
     ASSERT(!m_resources.get(resource->url()));
     m_resources.set(resource->url(), resource);
@@ -339,7 +339,7 @@ void Cache::pruneDeadResources()
             CachedResource* prev = current->m_prevInAllResourcesList;
             if (!current->hasClients() && !current->isPreloaded()) {
                 evict(current);
-                // If remove() caused pruneDeadResources() to be re-entered, bail out. This can happen when removing an
+                // If evict() caused pruneDeadResources() to be re-entered, bail out. This can happen when removing an
                 // SVG CachedImage that has subresources.
                 if (!m_inPruneDeadResources)
                     return;
@@ -693,7 +693,7 @@ void Cache::setDisabled(bool disabled)
         CachedResourceMap::iterator i = m_resources.begin();
         if (i == m_resources.end())
             break;
-        remove(i->second);
+        evict(i->second);
     }
 }
 
