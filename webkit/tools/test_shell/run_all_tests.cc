4bcf8855ddcbee43682d8154b3942f2462e301e8
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-13 15:45:20 +0000

    Minimal change to make test_shell_tests link and run zero tests on Linux.
    Review URL: http://codereview.chromium.org/1603
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3288 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/run_all_tests.cc
+++ webkit/tools/test_shell/run_all_tests.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Run all of our test shell tests.  This is just an entry point
 // to kick off gTest's RUN_ALL_TESTS().
 
+#if defined(OS_WIN)
 #include <windows.h>
 #include <commctrl.h>
+#endif
 
 #include "base/at_exit.h"
 #include "base/icu_util.h"
@@ -33,6 +35,7 @@ int main(int argc, char* argv[]) {
   // the AtExitManager or else we will leak objects.
   base::AtExitManager at_exit_manager;  
 
+#if defined(OS_WIN)
   TestShell::InitLogging(true);  // suppress error dialogs
 
   // Initialize test shell in non-interactive mode, which will let us load one
@@ -44,6 +47,7 @@ int main(int argc, char* argv[]) {
   // so just set it to be a really large number.  This is necessary because
   // when running under Purify, we were hitting those timeouts.
   TestShell::SetFileTestTimeout(USER_TIMER_MAXIMUM);
+#endif
 
   // Allocate a message loop for this thread.  Although it is not used
   // directly, its constructor sets up some necessary state.
@@ -52,18 +56,23 @@ int main(int argc, char* argv[]) {
   // Load ICU data tables
   icu_util::Initialize();
 
+#if defined(OS_WIN)
   INITCOMMONCONTROLSEX InitCtrlEx;
 
   InitCtrlEx.dwSize = sizeof(INITCOMMONCONTROLSEX);
   InitCtrlEx.dwICC  = ICC_STANDARD_CLASSES;
   InitCommonControlsEx(&InitCtrlEx);
+#endif
 
   // Run the actual tests
   testing::InitGoogleTest(&argc, argv);
   int result = RUN_ALL_TESTS();
 
+#if defined(OS_WIN)
   TestShell::ShutdownTestShell();
   TestShell::CleanupLogging();
+#endif
+
   return result;
 }
 
