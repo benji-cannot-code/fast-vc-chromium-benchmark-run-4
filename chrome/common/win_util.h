25d38455a3c42c4c40d3e814c850ee61befae549
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-13 19:56:42 +0000

    Use system title bar font for app windows, detached popups, constrained popups.
    http://crbug.com/3376
    
    Review URL: http://codereview.chromium.org/7274
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3307 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/win_util.h
+++ chrome/common/win_util.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fix_wp64.h"
 #include "base/gfx/rect.h"
 #include "base/scoped_handle.h"
+#include "chrome/common/gfx/chrome_font.h"
 
 namespace win_util {
 
@@ -228,6 +229,9 @@ int MessageBox(HWND hwnd,
                const std::wstring& text,
                const std::wstring& caption,
                UINT flags);
+
+// Returns the system set window title font.
+ChromeFont GetWindowTitleFont();
 
 }  // namespace win_util
 
