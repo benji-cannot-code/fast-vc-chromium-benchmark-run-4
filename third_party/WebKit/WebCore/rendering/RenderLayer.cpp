b88862d2e13e848dda936b832ec1d838a2e2d845
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-13 21:53:07 +0000

    2008-10-13  David Hyatt  <hyatt@apple.com>
            In order to always match the :corner-present pseudo-class properly, delay initial updating of
            scrollbar styles until the layer has the scrollbars in its member variables.
    
            Reviewed by Tim Hatcher
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::setHasHorizontalScrollbar):
            (WebCore::RenderLayer::setHasVerticalScrollbar):
            * rendering/RenderScrollbar.cpp:
            (WebCore::RenderScrollbar::RenderScrollbar):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayer.cpp
@@ -1202,6 +1202,8 @@ void RenderLayer::setHasHorizontalScrollbar(bool hasScrollbar)
         destroyScrollbar(HorizontalScrollbar);
 
     // Destroying or creating one bar can cause our scrollbar corner to come and go.  We need to update the opposite scrollbar's style.
+    if (m_hBar)
+        m_hBar->styleChanged();
     if (m_vBar)
         m_vBar->styleChanged();
 
@@ -1225,6 +1227,8 @@ void RenderLayer::setHasVerticalScrollbar(bool hasScrollbar)
      // Destroying or creating one bar can cause our scrollbar corner to come and go.  We need to update the opposite scrollbar's style.
     if (m_hBar)
         m_hBar->styleChanged();
+    if (m_vBar)
+        m_vBar->styleChanged();
 
 #if ENABLE(DASHBOARD_SUPPORT)
     // Force an update since we know the scrollbars have changed things.
