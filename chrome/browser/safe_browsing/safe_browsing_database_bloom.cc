40f9a98176cdb5ace367f356aaf390b5c894ebf6
paulg@google.com <paulg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-13 21:03:51 +0000

    Minor code clean up. Review URL: http://codereview.chromium.org/7271
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3314 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_database_bloom.cc
+++ chrome/browser/safe_browsing/safe_browsing_database_bloom.cc
@@ -311,6 +311,7 @@ void SafeBrowsingDatabaseBloom::InsertChunks(const std::string& list_name,
   std::deque<SBChunk>::iterator i = chunks->begin();
   for (; i != chunks->end(); ++i) {
     SBChunk& chunk = (*i);
+    chunk.list_id = list_id;
     std::deque<SBChunkHost>::iterator j = chunk.hosts.begin();
     for (; j != chunk.hosts.end(); ++j) {
       j->entry->set_list_id(list_id);
@@ -334,8 +335,7 @@ void SafeBrowsingDatabaseBloom::ProcessChunks() {
 
   while (!pending_chunks_.empty()) {
     std::deque<SBChunk>* chunks = pending_chunks_.front();
-    // The entries in one chunk are all either adds or subs.
-    if (chunks->front().hosts.front().entry->IsAdd()) {
+    if (chunks->front().is_add) {
       ProcessAddChunks(chunks);
     } else {
       ProcessSubChunks(chunks);
@@ -352,7 +352,7 @@ void SafeBrowsingDatabaseBloom::ProcessChunks() {
 void SafeBrowsingDatabaseBloom::ProcessAddChunks(std::deque<SBChunk>* chunks) {
   while (!chunks->empty()) {
     SBChunk& chunk = chunks->front();
-    int list_id = chunk.hosts.front().entry->list_id();
+    int list_id = chunk.list_id;
     int chunk_id = chunk.chunk_number;
 
     // The server can give us a chunk that we already have because it's part of
@@ -512,7 +512,7 @@ void SafeBrowsingDatabaseBloom::CreateChunkCaches() {
 void SafeBrowsingDatabaseBloom::ProcessSubChunks(std::deque<SBChunk>* chunks) {
   while (!chunks->empty()) {
     SBChunk& chunk = chunks->front();
-    int list_id = chunk.hosts.front().entry->list_id();
+    int list_id = chunk.list_id;
     int chunk_id = chunk.chunk_number;
 
     if (!ChunkExists(list_id, SUB_CHUNK, chunk_id)) {
