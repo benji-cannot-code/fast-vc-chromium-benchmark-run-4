8fb5554b321affa9e026a9612f58915502deed5f
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-14 16:52:52 +0000

    Fix the build by including string. Review URL: http://codereview.chromium.org/7303
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3347 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_contents_view.h
+++ chrome/browser/web_contents_view.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
+#include <string>
+
 #include "base/basictypes.h"
 #include "base/gfx/rect.h"
 #include "base/gfx/size.h"
