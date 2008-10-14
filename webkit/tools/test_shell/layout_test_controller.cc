628806b16161cb6a43c730e2d935974ed75d015b
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-14 23:08:05 +0000

    Add this file I forgot in my previous CL. Review URL: http://codereview.chromium.org/7330
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3380 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/layout_test_controller.cc
+++ webkit/tools/test_shell/layout_test_controller.cc
@@ -336,7 +336,7 @@ void LayoutTestController::objCIdentityIsEqual(
 
 void LayoutTestController::Reset() {
   if (shell_) {
-    shell_->webView()->MakeTextStandardSize();
+    shell_->webView()->ResetZoom();
     shell_->webView()->SetTabKeyCyclesThroughElements(true);
   }
   dump_as_text_ = false;
