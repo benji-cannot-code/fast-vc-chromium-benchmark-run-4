db4bf43b2d7c3b4b6a73770d377867fa033e1902
adachan@apple.com <adachan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-14 22:16:30 +0000

    Fix windows build.
            Reviewed by Sam Weinig.
    
            * DumpRenderTree/cg/PixelDumpSupportCG.cpp:
            * DumpRenderTree/config.h:
            * DumpRenderTree/win/DumpRenderTreeWin.h:
            * DumpRenderTree/win/LayoutTestControllerWin.cpp:
            * DumpRenderTree/win/UIDelegate.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/cg/PixelDumpSupportCG.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/cg/PixelDumpSupportCG.cpp
@@ -29,9 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "DumpRenderTree.h"
+#include "config.h"
 #include "PixelDumpSupportCG.h"
 
+#include "DumpRenderTree.h"
 #include "LayoutTestController.h"
 #include <ImageIO/CGImageDestination.h>
 #include <wtf/Assertions.h>
