672f1e0437f9749d9a45e3055de0e169a984f605
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-15 21:37:46 +0000

    Add a check in Chrome to not run user level mode if machine level Chrome is already installed.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3423 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/install_util.cc
+++ chrome/installer/util/install_util.cc
@@ -18,6 +18,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/google_update_constants.h"
 
 
+std::wstring InstallUtil::GetChromeUninstallCmd(bool system_install) {
+  HKEY root = system_install ? HKEY_LOCAL_MACHINE : HKEY_CURRENT_USER;
+  BrowserDistribution* dist = BrowserDistribution::GetDistribution();
+  RegKey key(root, dist->GetUninstallRegPath().c_str());
+  std::wstring uninstall_cmd;
+  key.ReadValue(installer_util::kUninstallStringField, &uninstall_cmd);
+  return uninstall_cmd;
+}
+
 installer::Version* InstallUtil::GetChromeVersion(bool system_install) {
   RegKey key;
   std::wstring version_str;
--- chrome/installer/util/install_util.h
+++ chrome/installer/util/install_util.h
@@ -21,12 +21,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // independently.
 class InstallUtil {
  public:
+  // Reads the uninstall command for Chromium from registry and returns it.
+  // If system_install is true the command is read from HKLM, otherwise
+  // from HKCU.
+  static std::wstring GetChromeUninstallCmd(bool system_install);
+
   // Find the version of Chrome installed on the system by checking the
   // Google Update registry key. Returns the version or NULL if no version is
   // found.
   // system_install: if true, looks for version number under the HKLM root,
   //                 otherwise looks under the HKCU.
-  static installer::Version * GetChromeVersion(bool system_install);
+  static installer::Version* GetChromeVersion(bool system_install);
 
   // This function checks if the current OS is supported for Chromium.
   static bool IsOSSupported();
