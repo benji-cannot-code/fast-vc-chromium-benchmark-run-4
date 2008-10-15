9bebf71756767407863f94c1dfb3bbe407cafd43
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-15 00:19:45 +0000

    Link in WebView to the gtk test shell, and organize all the gtk files into their own dir.
    Review URL: http://codereview.chromium.org/7334
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3386 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/tools/test_shell/gtk/webview_host.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtk/gtk.h>
+
+#include "webkit/tools/test_shell/webview_host.h"
+
+#include "base/gfx/platform_canvas.h"
+#include "base/gfx/rect.h"
+#include "base/gfx/size.h"
+#include "webkit/glue/webinputevent.h"
+#include "webkit/glue/webview.h"
+
+/*static*/
+WebViewHost* WebViewHost::Create(GtkWidget* parent_window,
+                                 WebViewDelegate* delegate,
+                                 const WebPreferences& prefs) {
+  WebViewHost* host = new WebViewHost();
+
+  // TODO(erg):
+  // - Set "host->view_"
+  // - Call "host->webwidget_->Resize"
+  host->webwidget_ = WebView::Create(delegate, prefs);
+
+  return host;
+}
+
+WebView* WebViewHost::webview() const {
+  return static_cast<WebView*>(webwidget_);
+}
