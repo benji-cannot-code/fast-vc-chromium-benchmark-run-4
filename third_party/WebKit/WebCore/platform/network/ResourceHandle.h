ab96bc95eedebc144160873cef1a6c96c926da84
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-15 21:53:46 +0000

    2008-10-15  Adele Peterson  <adele@apple.com>
            Reviewed by Sam Weinig.
    
            Implement didSendBodyData delegate method, and use older code path when that delegate doesn't exist. 
    
            * platform/network/ResourceHandle.h:
            * platform/network/mac/FormDataStreamMac.mm: (WebCore::formRead):
            * platform/network/mac/ResourceHandleMac.mm:
            (WebCore::ResourceHandle::didSendBodyDataDelegateExists):
            (WebCore::ResourceHandle::start):
            (WebCore::ResourceHandle::cancel):
            (-[WebCoreResourceHandleAsDelegate connection:willSendRequest:redirectResponse:]):
            (-[WebCoreResourceHandleAsDelegate connection:didSendBodyData:totalBytesWritten:totalBytesExpectedToWrite:]):
            (-[WebCoreResourceHandleAsDelegate connectionDidFinishLoading:]):
            (-[WebCoreResourceHandleAsDelegate connection:didFailWithError:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37616 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceHandle.h
+++ third_party/WebKit/WebCore/platform/network/ResourceHandle.h
@@ -104,7 +104,10 @@ public:
 
     static void loadResourceSynchronously(const ResourceRequest&, ResourceError&, ResourceResponse&, Vector<char>& data, Frame* frame);
     static bool willLoadFromCache(ResourceRequest&);
-    
+#if PLATFORM(MAC)
+    static bool didSendBodyDataDelegateExists();
+#endif
+
     ~ResourceHandle();
 
 #if PLATFORM(MAC) || USE(CFNETWORK) || USE(CURL)
