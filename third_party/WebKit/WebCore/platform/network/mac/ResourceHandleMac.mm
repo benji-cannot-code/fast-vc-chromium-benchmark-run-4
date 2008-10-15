a59fd4c3bc9c7ae6aafa79d3bfa390e0fbb2e184
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-15 22:10:25 +0000

    2008-10-15  Adele Peterson  <adele@apple.com>
            Attempt to fix the Tiger build.
    
            * platform/network/mac/ResourceHandleMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37620 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/mac/ResourceHandleMac.mm
+++ third_party/WebKit/WebCore/platform/network/mac/ResourceHandleMac.mm
@@ -43,6 +43,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "SubresourceLoader.h"
 #import "WebCoreSystemInterface.h"
 
+#ifdef BUILDING_ON_TIGER
+typedef int NSInteger;
+#endif
+
 using namespace WebCore;
 
 @interface WebCoreResourceHandleAsDelegate : NSObject <NSURLAuthenticationChallengeSender>
