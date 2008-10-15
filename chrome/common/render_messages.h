de648380255d7f461b3c7fd225b05f93eb9b2317
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-15 17:45:38 +0000

    The is_content_filtered parameter of the ViewHostMsg_FrameNavigate_Params was not serialized/deserialized.
    BUG=2742 (partially)
    Review URL: http://codereview.chromium.org/7351
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3395 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/render_messages.h
+++ chrome/common/render_messages.h
@@ -836,6 +836,7 @@ struct ParamTraits<ViewHostMsg_FrameNavigate_Params> {
     WriteParam(m, p.gesture);
     WriteParam(m, p.contents_mime_type);
     WriteParam(m, p.is_post);
+    WriteParam(m, p.is_content_filtered);
   }
   static bool Read(const Message* m, void** iter, param_type* p) {
     return
@@ -852,7 +853,8 @@ struct ParamTraits<ViewHostMsg_FrameNavigate_Params> {
       ReadParam(m, iter, &p->security_info) &&
       ReadParam(m, iter, &p->gesture) &&
       ReadParam(m, iter, &p->contents_mime_type) &&
-      ReadParam(m, iter, &p->is_post);
+      ReadParam(m, iter, &p->is_post) &&
+      ReadParam(m, iter, &p->is_content_filtered);
   }
   static void Log(const param_type& p, std::wstring* l) {
     l->append(L"(");
@@ -883,6 +885,8 @@ struct ParamTraits<ViewHostMsg_FrameNavigate_Params> {
     LogParam(p.contents_mime_type, l);
     l->append(L", ");
     LogParam(p.is_post, l);
+    l->append(L", ");
+    LogParam(p.is_content_filtered, l);
     l->append(L")");
   }
 };
