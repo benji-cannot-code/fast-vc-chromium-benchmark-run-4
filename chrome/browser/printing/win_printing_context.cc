30a7686895badda7baecc138de96b7376ca1eb8d
maruel@google.com <maruel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-15 17:56:40 +0000

    Change #ifdef _DEBUG test to #ifndef NDEBUG test. Review URL: http://codereview.chromium.org/7404
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3398 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/win_printing_context.cc
+++ chrome/browser/printing/win_printing_context.cc
@@ -136,7 +136,7 @@ class PrintingContext::CallbackHandler
 
 PrintingContext::PrintingContext()
     : hdc_(NULL),
-#ifdef _DEBUG
+#ifndef NDEBUG
       page_number_(-1),
 #endif
       dialog_box_(NULL),
@@ -243,7 +243,7 @@ void PrintingContext::ResetSettings() {
   settings_.Clear();
   in_print_job_ = false;
 
-#ifdef _DEBUG
+#ifndef NDEBUG
   page_number_ = -1;
 #endif
 }
@@ -298,7 +298,7 @@ PrintingContext::Result PrintingContext::NewDocument(
   if (StartDoc(hdc_, &di) <= 0)
     return OnErrror();
 
-#ifdef _DEBUG
+#ifndef NDEBUG
   page_number_ = 0;
 #endif
   return OK;
@@ -313,7 +313,7 @@ PrintingContext::Result PrintingContext::NewPage() {
   if (StartPage(hdc_) <= 0)
     return OnErrror();
 
-#ifdef _DEBUG
+#ifndef NDEBUG
   ++page_number_;
 #endif
 
