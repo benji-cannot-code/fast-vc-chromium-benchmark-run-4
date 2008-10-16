ffaea1337b61d19d6ad2c9b9fa70fddc2877c507
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-16 01:05:12 +0000

    <rdar://problem/6272508> Crash occurs after loading flash content at http://www.macrumors.com/
    Restore some code related to the CoreGraphics drawing model that was misplaced in r37131.
    
    Reviewed by Dan Bernstein.
    
    * Plugins/WebBaseNetscapePluginView.mm:
    (-[WebBaseNetscapePluginView invalidateRegion:]):
    (-[WebBaseNetscapePluginView setVariable:value:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37623 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebBaseNetscapePluginView.mm
+++ third_party/WebKit/WebKit/mac/Plugins/WebBaseNetscapePluginView.mm
@@ -2408,7 +2408,12 @@ static inline void getNPRect(const NSRect& nr, NPRect& npr)
         break;
 #endif /* NP_NO_QUICKDRAW */
         
-        case NPDrawingModelCoreGraphics:    
+        case NPDrawingModelCoreGraphics:
+        {
+            CGRect cgRect = CGPathGetBoundingBox((NPCGRegion)invalidRegion);
+            invalidRect = *(NSRect*)&cgRect;
+            break;
+        }
         default:
             ASSERT_NOT_REACHED();
         break;
@@ -2564,7 +2569,11 @@ static NPBrowserTextInputFuncs *browserTextInputFuncs()
                 // Supported drawing models:
 #ifndef NP_NO_QUICKDRAW
                 case NPDrawingModelQuickDraw:
-#endif                
+#endif
+                case NPDrawingModelCoreGraphics:
+                    drawingModel = newDrawingModel;
+                    return NPERR_NO_ERROR;
+
                 // Unsupported (or unknown) drawing models:
                 default:
                     LOG(Plugins, "Plugin %@ uses unsupported drawing model: %d", pluginPackage, drawingModel);
