7be9b2289672f4e8d4eca10093fc1d15a9a98c71
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-16 20:36:09 +0000

    Misc style changes. (trying to address previous review comments).
    Review URL: http://codereview.chromium.org/7447
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3482 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction_unittest.cc
+++ net/http/http_network_transaction_unittest.cc
@@ -224,6 +224,12 @@ class HttpNetworkTransactionTest : public PlatformTest {
     mock_sockets_index = 0;
   }
 
+  virtual void TearDown() {
+    // Empty the current queue.
+    MessageLoop::current()->RunAllPending();
+    PlatformTest::TearDown();
+  }
+
  protected:
   void KeepAliveConnectionResendRequestTest(const MockRead& read_failure);
 };
@@ -269,9 +275,6 @@ SimpleGetHelperResult SimpleGetHelper(MockRead data_reads[]) {
   EXPECT_EQ(net::OK, rv);
 
   trans->Destroy();
-
-  // Empty the current queue.
-  MessageLoop::current()->RunAllPending();
 
   return out;
 }
@@ -435,9 +438,6 @@ TEST_F(HttpNetworkTransactionTest, ReuseConnection) {
     EXPECT_EQ(kExpectedResponseData[i], response_data);
 
     trans->Destroy();
-
-    // Empty the current queue.
-    MessageLoop::current()->RunAllPending();
   }
 }
 
@@ -483,9 +483,6 @@ TEST_F(HttpNetworkTransactionTest, Ignores100) {
   EXPECT_EQ("hello world", response_data);
 
   trans->Destroy();
-
-  // Empty the current queue.
-  MessageLoop::current()->RunAllPending();
 }
 
 // read_failure specifies a read failure that should cause the network
@@ -545,9 +542,6 @@ void HttpNetworkTransactionTest::KeepAliveConnectionResendRequestTest(
     EXPECT_EQ(kExpectedResponseData[i], response_data);
 
     trans->Destroy();
-
-    // Empty the current queue.
-    MessageLoop::current()->RunAllPending();
   }
 }
 
@@ -593,9 +587,6 @@ TEST_F(HttpNetworkTransactionTest, NonKeepAliveConnectionReset) {
   EXPECT_TRUE(response == NULL);
 
   trans->Destroy();
-
-  // Empty the current queue.
-  MessageLoop::current()->RunAllPending();
 }
 
 // What do various browsers do when the server closes a non-keepalive
@@ -615,7 +606,7 @@ TEST_F(HttpNetworkTransactionTest, NonKeepAliveConnectionEOF) {
     MockRead(false, net::OK),
   };
   SimpleGetHelperResult out = SimpleGetHelper(data_reads);
-  EXPECT_EQ(out.rv, net::ERR_EMPTY_RESPONSE);
+  EXPECT_EQ(net::ERR_EMPTY_RESPONSE, out.rv);
 }
 
 // Test the request-challenge-retry sequence for basic auth.
@@ -701,9 +692,6 @@ TEST_F(HttpNetworkTransactionTest, BasicAuth) {
   EXPECT_EQ(100, response->headers->GetContentLength());
 
   trans->Destroy();
-
-  // Empty the current queue.
-  MessageLoop::current()->RunAllPending();
 }
 
 // Test the flow when both the proxy server AND origin server require
@@ -837,7 +825,4 @@ TEST_F(HttpNetworkTransactionTest, BasicAuthProxyThenServer) {
   EXPECT_EQ(100, response->headers->GetContentLength());
 
   trans->Destroy();
-
-  // Empty the current queue.
-  MessageLoop::current()->RunAllPending();
 }
