e001c6122da3087ac09d2ce9fff028806daa7382
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-17 20:20:30 +0000

    fix release bustage :-(
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3555 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/view.h
+++ chrome/views/view.h
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_VIEWS_VIEW_H_
 #define CHROME_VIEWS_VIEW_H_
 
+#include <atlbase.h>	 
+#include <atlapp.h>	 
+#include <atlmisc.h>	 
+
 #include <map>
-#include <oleacc.h>
 #include <vector>
 
 #include "base/gfx/rect.h"
