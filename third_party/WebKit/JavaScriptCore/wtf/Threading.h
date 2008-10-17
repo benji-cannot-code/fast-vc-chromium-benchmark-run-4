bdb1aa148b6a48471764154be184b688e27f7072
jmalonzo@webkit.org <jmalonzo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-17 21:16:01 +0000

    2008-10-17  Marco Barisione  <marco.barisione@collabora.co.uk>
            Reviewed by Sam Weinig. Landed by Jan Alonzo.
    
            https://bugs.webkit.org/show_bug.cgi?id=21603
            [GTK] Minor fixes to GOwnPtr
    
            * wtf/GOwnPtr.cpp:
            (WTF::GError):
            (WTF::GList):
            (WTF::GCond):
            (WTF::GMutex):
            (WTF::GPatternSpec):
            (WTF::GDir):
            * wtf/GOwnPtr.h:
            (WTF::freeOwnedGPtr):
            (WTF::GOwnPtr::~GOwnPtr):
            (WTF::GOwnPtr::outPtr):
            (WTF::GOwnPtr::set):
            (WTF::GOwnPtr::clear):
            * wtf/Threading.h:
    
    2008-10-17  Marco Barisione  <marco.barisione@collabora.co.uk>
    
            Reviewed by Sam Weinig. Landed by Jan Alonzo.
    
            https://bugs.webkit.org/show_bug.cgi?id=21603
            [GTK] Minor fixes to GOwnPtr
    
            * platform/graphics/gtk/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::mediaPlayerPrivateErrorCallback):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37660 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Threading.h
+++ third_party/WebKit/JavaScriptCore/wtf/Threading.h
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Assertions.h>
 #include <wtf/Locker.h>
 #include <wtf/Noncopyable.h>
+
 #if PLATFORM(GTK)
 #include <wtf/GOwnPtr.h>
 #endif
