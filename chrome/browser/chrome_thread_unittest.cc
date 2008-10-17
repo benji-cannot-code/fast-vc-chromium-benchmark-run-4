d606f5db4e3e202b47c25b1c7c5ffed8a30ca08e
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-17 21:16:13 +0000

    Make ChromeThreadTest and TextDatabaseTest inherit from PlatformTest for autorelease pool management on the Mac. Review URL: http://codereview.chromium.org/7640
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3563 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_thread_unittest.cc
+++ chrome/browser/chrome_thread_unittest.cc
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/platform_test.h"
 #include "chrome/browser/chrome_thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-TEST(ChromeThreadTest, Get) {
+typedef PlatformTest ChromeThreadTest;
+
+TEST_F(ChromeThreadTest, Get) {
   scoped_ptr<ChromeThread> io_thread;
   scoped_ptr<ChromeThread> file_thread;
   scoped_ptr<ChromeThread> db_thread;
@@ -76,4 +79,3 @@ TEST(ChromeThreadTest, Get) {
   EXPECT_TRUE(ChromeThread::GetMessageLoop(ChromeThread::DB) == NULL);
   EXPECT_TRUE(ChromeThread::GetMessageLoop(ChromeThread::HISTORY) == NULL);
 }
-
--- chrome/browser/history/text_database_unittest.cc
+++ chrome/browser/history/text_database_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_util.h"
 #include "base/path_service.h"
+#include "base/platform_test.h"
 #include "base/scoped_ptr.h"
 #include "base/string_util.h"
 #include "chrome/browser/history/text_database.h"
@@ -77,13 +78,14 @@ bool ResultsHaveURL(const std::vector<TextDatabase::Match>& results,
 
 }  // namespace
 
-class TextDatabaseTest : public testing::Test {
+class TextDatabaseTest : public PlatformTest {
  public:
   TextDatabaseTest() : db_(NULL) {
   }
 
  protected:
   void SetUp() {
+    PlatformTest::SetUp();
     PathService::Get(base::DIR_TEMP, &temp_path_);
   }
 
@@ -91,6 +93,7 @@ class TextDatabaseTest : public testing::Test {
     for (size_t i = 0; i < opened_files_.size(); i++)
       file_util::Delete(opened_files_[i], false);
     file_util::Delete(file_name_, false);
+    PlatformTest::TearDown();
   }
 
   // Create databases with this function, which will ensure that the files are
