25f98dc727cc08f32a260ddfee1dba89705d913f
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-17 21:22:10 +0000

    We want these symbols on all platforms.
    Review URL: http://codereview.chromium.org/7491
    Patch from Torchmobile Inc..
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3564 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/localized_strings.cc
+++ webkit/glue/localized_strings.cc
@@ -53,7 +53,6 @@ inline String GetLocalizedString(int message_id) {
   return webkit_glue::StdWStringToString(str);
 }
 
-#if defined(OS_WIN)
 String WebCore::searchableIndexIntroduction() {
   return GetLocalizedString(IDS_SEARCHABLE_INDEX_INTRO);
 }
@@ -99,7 +98,6 @@ String WebCore::AXImageMapText() {
 String WebCore::AXHeadingText() {
   return GetLocalizedString(IDS_AX_ROLE_HEADING);
 }
-#endif  // OS_WIN
 String WebCore::AXButtonActionVerb() {
   return GetLocalizedString(IDS_AX_BUTTON_ACTION_VERB);
 }
@@ -154,7 +152,6 @@ String imageTitle(const String& filename, const IntSize& size) {
 
 } //namespace WebCore
 
-#if defined(OS_WIN)
 // We don't use these strings, so they return an empty String. We can't just
 // make them asserts because webcore still calls them.
 String WebCore::contextMenuItemTagOpenLinkInNewWindow() { return String(); }
@@ -193,4 +190,3 @@ String WebCore::contextMenuItemTagLeftToRight() { return String(); }
 String WebCore::contextMenuItemTagRightToLeft() { return String(); }
 String WebCore::contextMenuItemTagInspectElement() { return String(); }
 String WebCore::contextMenuItemTagShowSpellingPanel(bool show) { return String(); }
-#endif  // OS_WIN
