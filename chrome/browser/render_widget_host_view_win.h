6c2409a73960215714b063809af61cf62a1582d0
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-17 15:29:03 +0000

    Separate out most view creation from WebContents. This adds a new sub-delegate to RenderViewHostDelegate which is implemented by WebContentsView. I did a lot of plumbing and moving around as a result. Review URL: http://codereview.chromium.org/6608
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3527 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/render_widget_host_view_win.h
+++ chrome/browser/render_widget_host_view_win.h
@@ -58,7 +58,8 @@ class RenderWidgetHostViewWin :
                      RenderWidgetHostHWNDTraits>,
   public RenderWidgetHostView {
  public:
-  explicit RenderWidgetHostViewWin(RenderWidgetHost* render_widget_host);
+  // The view will associate itself with the given widget.
+  explicit RenderWidgetHostViewWin(RenderWidgetHost* widget);
   virtual ~RenderWidgetHostViewWin();
 
   void set_close_on_deactivate(bool close_on_deactivate) {
@@ -114,6 +115,7 @@ class RenderWidgetHostViewWin :
   END_MSG_MAP()
 
   // Implementation of RenderWidgetHostView:
+  virtual RenderWidgetHost* GetRenderWidgetHost() const;
   virtual void DidBecomeSelected();
   virtual void WasHidden();
   virtual void SetSize(const gfx::Size& size);
