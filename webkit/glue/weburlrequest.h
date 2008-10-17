e7e0f834c47e2c2d94685b52264abed869c555e2
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-17 00:00:40 +0000

    Currently, there is no way to access or modify the upload data and/or complete list of request headers associated with a WebRequest object.  The intent is to access this information from WebViewDelegate::DispositionForNavigationAction().
    Remove the HasFormData() method that checked for upload (form) data on the
    history item. The HasFormData() method is not currently being called from
    anywhere, so changes elsewhere are not required.
    
    Add HasUploadData(), GetUploadData() and SetUploadData() methods to test, get
    and set the underlying request upload data as a net::UploadData object. Add
    GetHttpHeaders() and SetHttpHeaders() methods to get and set all request headers
    using a map of values.  Add the SetHttpHeaderValue() method to set the value for
    a particular request header.
    
    Patch by Marshall Greenblatt
    
    R=darin
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3497 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/weburlrequest.h
+++ webkit/glue/weburlrequest.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_GLUE_WEBURLREQUEST_H__
 #define WEBKIT_GLUE_WEBURLREQUEST_H__
 
+#include <map>
 #include <string>
 
 #include "base/basictypes.h"
@@ -18,10 +19,16 @@ enum WebRequestCachePolicy {
   WebRequestReturnCacheDataDontLoad
 };
 
+namespace net {
+class UploadData;
+}
+
 class GURL;
 
 class WebRequest {
  public:
+  typedef std::map<std::string, std::string> HeaderMap;
+
   // Extra information that is associated with a request. The embedder derives
   // from this REFERENCE COUNTED class to associated data with a request and
   // get it back when the page loads.
@@ -72,6 +79,19 @@ class WebRequest {
   // given header was not set in the request, the empty string is returned.
   virtual std::wstring GetHttpHeaderValue(const std::wstring& field) const = 0;
 
+  // Set a value for a header in the request.
+  virtual void SetHttpHeaderValue(const std::wstring& field,
+      const std::wstring& value) = 0;
+
+  // Fills a map with all header name/value pairs set in the request.
+  virtual void GetHttpHeaders(HeaderMap* headers) const = 0;
+
+  // Sets the header name/value pairs for the request from a map. Values set
+  // using this method replace any pre-existing values with the same name.
+  // Passing in a blank value will result in a header with a blank value being
+  // sent as part of the request.
+  virtual void SetHttpHeaders(const HeaderMap& headers) = 0;
+
   // Helper function for GetHeaderValue to retrieve the referrer. This
   // referrer is generated automatically by WebKit when navigation events
   // occur. If there was no referrer (for example, the browser instructed
@@ -95,8 +115,15 @@ class WebRequest {
   virtual std::string GetSecurityInfo() const = 0;
   virtual void SetSecurityInfo(const std::string& info) = 0;
 
-  // Returns true if this request contains history state that has form data.
-  virtual bool HasFormData() const = 0;
+  // Returns true if the request has upload data.
+  virtual bool HasUploadData() const = 0;
+
+  // Returns the request upload data. This object is temporary and should be
+  // deleted after use.
+  virtual void GetUploadData(net::UploadData* data) const = 0;
+
+  // Set the request upload data.
+  virtual void SetUploadData(const net::UploadData& data) = 0;
 
   virtual ~WebRequest() { }
 };
