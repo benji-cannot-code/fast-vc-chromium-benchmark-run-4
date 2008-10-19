d0b382bcf5107cbd1048ac722bb26787e84ec987
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-19 04:06:01 +0000

    2008-10-18  Darin Adler  <darin@apple.com>
            - fix non-all-in-one build
    
            * kjs/ConstructData.h: Add include of JSImmedate.h (for now).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37692 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/ConstructData.h
+++ third_party/WebKit/JavaScriptCore/kjs/ConstructData.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ConstructData_h
 #define ConstructData_h
 
+#include "JSImmediate.h" // temporary until JSValuePtr becomes a class we can forward-declare
+
 namespace JSC {
 
     class ArgList;
