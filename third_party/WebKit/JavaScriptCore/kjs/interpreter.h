f21562b163e70f57c2267a64ab8bb970113e0c9d
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-19 04:21:12 +0000

    2008-10-18  Darin Adler  <darin@apple.com>
            - fix build
    
            * kjs/interpreter.h: Include JSValue.h instead of JSImmediate.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37695 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/interpreter.h
+++ third_party/WebKit/JavaScriptCore/kjs/interpreter.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef KJS_Interpreter_h
 #define KJS_Interpreter_h
 
-#include <kjs/JSImmediate.h>
+#include <kjs/JSValue.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/unicode/Unicode.h>
 
