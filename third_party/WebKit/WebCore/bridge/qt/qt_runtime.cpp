c2b32b7f01516e1ee1b170a05b8bc8348ee7498d
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-20 15:29:04 +0000

    2008-10-20  Darin Adler  <darin@apple.com>
            - try to fix Qt build
    
            * bridge/qt/qt_runtime.cpp:
            (JSC::Bindings::QtRuntimeMetaMethod::connectGetter): Add asObject calls.
            (JSC::Bindings::QtRuntimeMetaMethod::disconnectGetter): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
+++ third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
@@ -1349,7 +1349,7 @@ JSValuePtr QtRuntimeMetaMethod::lengthGetter(ExecState* exec, const Identifier&,
 
 JSValuePtr QtRuntimeMetaMethod::connectGetter(ExecState* exec, const Identifier& ident, const PropertySlot& slot)
 {
-    QtRuntimeMetaMethod* thisObj = static_cast<QtRuntimeMetaMethod*>(slot.slotBase());
+    QtRuntimeMetaMethod* thisObj = static_cast<QtRuntimeMetaMethod*>(asObject(slot.slotBase()));
     QW_DS(QtRuntimeMetaMethod, thisObj);
 
     if (!d->m_connect)
@@ -1359,7 +1359,7 @@ JSValuePtr QtRuntimeMetaMethod::connectGetter(ExecState* exec, const Identifier&
 
 JSValuePtr QtRuntimeMetaMethod::disconnectGetter(ExecState* exec, const Identifier& ident, const PropertySlot& slot)
 {
-    QtRuntimeMetaMethod* thisObj = static_cast<QtRuntimeMetaMethod*>(slot.slotBase());
+    QtRuntimeMetaMethod* thisObj = static_cast<QtRuntimeMetaMethod*>(asObject(slot.slotBase()));
     QW_DS(QtRuntimeMetaMethod, thisObj);
 
     if (!d->m_disconnect)
