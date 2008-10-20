73fbe55b627ee0233fe740c3c1116dc2fc17a887
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-20 23:22:57 +0000

    Fix KJS build.  Strangely, the PlatformWidget type is not expected to be defined in the WebCore namespace (it isn't in any of the other ports). Review URL: http://codereview.chromium.org/7680
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3636 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/chromium/PlatformWidget.h
+++ webkit/port/platform/chromium/PlatformWidget.h
@@ -33,10 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gfx/native_widget_types.h"
 
-namespace WebCore {
-
 typedef gfx::ViewHandle PlatformWidget;
-
-}
 
 #endif
