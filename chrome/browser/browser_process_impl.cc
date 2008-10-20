821e89eb7f8150cf538760677d1648c181f2deee
jar@google.com <jar@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-20 17:43:59 +0000

    Disable FieldTrial code to repair UI reliability test regression
    r=mbelshe
    Review URL: http://codereview.chromium.org/7520
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3608 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -118,6 +118,7 @@ BrowserProcessImpl::BrowserProcessImpl(CommandLine& command_line)
         memory_model_ = MEDIUM_MEMORY_MODEL;
     }
   } else {
+#if 0  // TODO(jar): Comment out setting of fix UI reliability regression.
     // Randomly choose what memory model to use.
     const double probability = 0.5;
     FieldTrial* trial(new FieldTrial(BrowserTrial::kMemoryModelFieldTrial,
@@ -127,6 +128,7 @@ BrowserProcessImpl::BrowserProcessImpl(CommandLine& command_line)
       memory_model_ = HIGH_MEMORY_MODEL;
     else
       memory_model_ = MEDIUM_MEMORY_MODEL;
+#endif  // 0.
   }
 
   suspend_controller_ = new SuspendController();
