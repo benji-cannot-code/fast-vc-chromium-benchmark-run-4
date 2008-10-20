b38f266f78d9b75105bab20272bc04e7ca758595
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-20 21:25:46 +0000

    This fixes http://code.google.com/p/chromium/issues/detail?id=205, which was an issue with a windowed flash instance not rendering content at times.The bug occurs as a result of the following:-1. The flash plugin executes a script via GetURLNotify. This script calls window.open with the target as self, which shows up as a new tab in the browser. This causes a new RenderView object to be instantiated (See RenderView::CreateWebView).2. RenderView::CreateWebView sends over the ViewHostMsg_CreateWindow IPC message to the browser. The handler in the browser sends over an ack for this message with the window handle. This is used as the parent window for any plugins instantiated in the page.3. At times, the newly created view starts receiving data which is processed before the ViewMsg_CreatingNew_ACK message is received and processed by the view. This causes the plugin to be instantiated without a parent window thus ending up as a top level window.The fix is to queue up resource messages and process them after we receive the ack for the ViewHostMsg_CreateWindow IPC.
    Tests :- Covered by UI tests.
    
    R=jam
    
    
    Review URL: http://codereview.chromium.org/7514
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3631 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/resource_dispatcher.h
+++ chrome/common/resource_dispatcher.h
@@ -69,6 +69,10 @@ class ResourceDispatcher : public base::RefCounted<ResourceDispatcher> {
     message_sender_ = NULL;
   }
 
+  // Returns true if the message passed in is a resource related
+  // message.
+  bool IsResourceMessage(const IPC::Message& message) const;
+
  private:
   friend class ResourceDispatcherTest;
 
