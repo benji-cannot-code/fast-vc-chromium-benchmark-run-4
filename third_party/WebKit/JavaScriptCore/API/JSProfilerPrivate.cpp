b23651a08150b3f6d17256c49f45d6f8a37cdc04
pkasting@chromium.org <pkasting@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-21 21:09:15 +0000

    2008-10-21  Peter Kasting  <pkasting@google.com>
            Reviewed by Maciej Stachowiak.
    
            Fix an include of a non-public header to use "" instead of <>.
    
            * API/JSProfilerPrivate.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37762 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSProfilerPrivate.cpp
+++ third_party/WebKit/JavaScriptCore/API/JSProfilerPrivate.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "APICast.h"
 #include "OpaqueJSString.h"
-#include <JavaScriptCore/Profiler.h>
+#include "Profiler.h"
 
 using namespace JSC;
 
