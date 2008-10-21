e8a596b5a13498d13cde414cf59f0892e798a894
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-21 23:45:09 +0000

    Bring in CursorChromium to Linux build.
    For some reason gcc wasn't happy with these implicit conversions:
    webkit/port/platform/chromium/CursorChromium.cpp:88: error: conversion from 'WebCursor::Type' to non-scalar type 'WebCore::Cursor' requested
    
    But making them explicit works.
    
    Review URL: http://codereview.chromium.org/8028
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3704 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/chromium/PlatformCursor.h
+++ webkit/port/platform/chromium/PlatformCursor.h
@@ -36,10 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 struct PlatformCursor : WebCursor {
-  PlatformCursor(int type = 0)
+  explicit PlatformCursor(int type = 0)
       : WebCursor(static_cast<WebCursor::Type>(type)) {
   }
-  PlatformCursor(const WebCursor& c)
+  explicit PlatformCursor(const WebCursor& c)
       : WebCursor(c) {
   }
 };
