2020ac7302bf41a7880eee0140bb9df5acf4b083
finnur@google.com <finnur@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-21 23:51:23 +0000

    Fixing various layout issues with the download shelf:
    1) Bug 3424: If you download an EXE which takes a while to start and you switch tabs before it does, then when you come back to the tab it would truncate the dangerous download on the right because we have never cached the size of the label. We now perform a SizeLabelToMinWidth() in the constructor of DownloadItem for dangerous downloads to make sure the calculation has taken place when needed.
    
    2) Bug 3424: Once I fixed the problem above, I also noticed that the Save and Discard buttons would not show since the cached button sizes are 0 because the view animation took place while the shelf was hidden (while the shelf had no parent). By triggering a Layout of the shelf after setting the bounds, this triggers the buttons to draw appropriately.
    
    3) Bug 3459: The remaining work was to fix the truncation on the bottom of the dangerous download view by taking the size of the progress icon for non-dangerous downloads into account. Ideally, we should increase the size of a non-dangerous download view if the warning icon or the buttons on the dangerous download view cause the size to increase, but I find that unlikely and it should be easy to fix if we come across that.
    
    Review URL: http://codereview.chromium.org/7843
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3705 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/download_item_view.cc
+++ chrome/browser/views/download_item_view.cc
@@ -26,12 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(paulg): These may need to be adjusted when download progress
 //              animation is added, and also possibly to take into account
 //              different screen resolutions.
-static const int kTextWidth = 140;           // Pixels
-static const int kDangerousTextWidth = 200;  // Pixels
-static const int kHorizontalTextPadding = 2; // Pixels
-static const int kVerticalPadding = 3;       // Pixels
-static const int kVerticalTextSpacer = 2;    // Pixels
-static const int kVerticalTextPadding = 2;   // Pixels
+static const int kTextWidth = 140;            // Pixels
+static const int kDangerousTextWidth = 200;   // Pixels
+static const int kHorizontalTextPadding = 2;  // Pixels
+static const int kVerticalPadding = 3;        // Pixels
+static const int kVerticalTextSpacer = 2;     // Pixels
+static const int kVerticalTextPadding = 2;    // Pixels
 
 // The maximum number of characters we show in a file name when displaying the
 // dangerous download message.
@@ -42,10 +42,10 @@ static const int kFileNameMaxLength = 20;
 static const int kLeftPadding = 0;  // Pixels.
 
 // The space between the Save and Discard buttons when prompting for a dangerous
-// donwload.
+// download.
 static const int kButtonPadding = 5;  // Pixels.
 
-// The space on the left and right side of the dangerous donwload label.
+// The space on the left and right side of the dangerous download label.
 static const int kLabelPadding = 4;  // Pixels.
 
 static const SkColor kFileNameColor = SkColorSetRGB(87, 108, 149);
@@ -209,9 +209,10 @@ DownloadItemView::DownloadItemView(DownloadItem* download,
         views::Label::ALIGN_LEFT);
     dangerous_download_label_->SetColor(kFileNameColor);
     AddChildView(dangerous_download_label_);
+    SizeLabelToMinWidth();
   }
 
-  // Set up our animation
+  // Set up our animation.
   StartDownloadProgress();
 }
 
@@ -221,7 +222,7 @@ DownloadItemView::~DownloadItemView() {
   download_->RemoveObserver(this);
 }
 
-// Progress animation handlers
+// Progress animation handlers.
 
 void DownloadItemView::UpdateDownloadProgress() {
   progress_angle_ = (progress_angle_ +
@@ -242,7 +243,7 @@ void DownloadItemView::StopDownloadProgress() {
   progress_timer_.Stop();
 }
 
-// DownloadObserver interface
+// DownloadObserver interface.
 
 // Update the progress graphic on the icon and our text status label
 // to reflect our current bytes downloaded, time remaining.
@@ -295,7 +296,6 @@ void DownloadItemView::OnDownloadUpdated(DownloadItem* download) {
 // In dangerous mode we have to layout our buttons.
 void DownloadItemView::Layout() {
   if (IsDangerousMode()) {
-    SizeLabelToMinWidth();
     int x = kLeftPadding + dangerous_mode_body_image_set_.top_left->width() +
       warning_icon_->width() + kLabelPadding;
     int y = (height() - dangerous_download_label_->height()) / 2;
@@ -480,7 +480,7 @@ void DownloadItemView::Paint(ChromeCanvas* canvas) {
       }
     }
 
-    // Draw the icon image
+    // Draw the icon image.
     canvas->DrawBitmapInt(*icon,
                           download_util::kSmallProgressIconOffset,
                           download_util::kSmallProgressIconOffset);
@@ -525,7 +525,7 @@ void DownloadItemView::ClearDangerousMode() {
   body_state_ = NORMAL;
   drop_down_state_ = NORMAL;
 
-  // Remove the views used by the dangerours mode.
+  // Remove the views used by the dangerous mode.
   RemoveChildView(save_button_);
   delete save_button_;
   save_button_ = NULL;
@@ -546,27 +546,30 @@ void DownloadItemView::ClearDangerousMode() {
 
 gfx::Size DownloadItemView::GetPreferredSize() {
   int width, height;
+
+  // First, we set the height to the height of two rows or text plus margins.
+  height = 2 * kVerticalPadding + 2 * font_.height() + kVerticalTextPadding;
+  // Then we increase the size if the progress icon doesn't fit.
+  height = std::max<int>(height, download_util::kSmallProgressIconSize);
+
   if (IsDangerousMode()) {
     width = kLeftPadding + dangerous_mode_body_image_set_.top_left->width();
     width += warning_icon_->width() + kLabelPadding;
     width += dangerous_download_label_->width() + kLabelPadding;
     gfx::Size button_size = GetButtonSize();
+    // Make sure the button fits.
+    height = std::max<int>(height, 2 * kVerticalPadding + button_size.height());
+    // Then we make sure the warning icon fits.
+    height = std::max<int>(height, 2 * kVerticalPadding +
+                                   warning_icon_->height());
     width += button_size.width() * 2 + kButtonPadding;
     width += dangerous_mode_body_image_set_.top_right->width();
-    height = std::max<int>(2 * kVerticalPadding +  2 * font_.height() +
-                             kVerticalTextPadding,
-                           2 * kVerticalPadding + warning_icon_->height());
-    height = std::max<int>(height,
-                           2 * kVerticalPadding + button_size.height());
   } else {
     width = kLeftPadding + normal_body_image_set_.top_left->width();
     width += download_util::kSmallProgressIconSize;
     width += kTextWidth;
     width += normal_body_image_set_.top_right->width();
     width += normal_drop_down_image_set_.top->width();
-    height = std::max<int>(2 * kVerticalPadding +  2 * font_.height() +
-                             kVerticalTextPadding,
-                           download_util::kSmallProgressIconSize);
   }
   return gfx::Size(width, height);
 }
@@ -744,7 +747,7 @@ gfx::Size DownloadItemView::GetButtonSize() {
   return size;
 }
 
-// This method computes the miminum width of the label for diplaying its text
+// This method computes the minimum width of the label for displaying its text
 // on 2 lines.  It just breaks the string in 2 lines on the spaces and keeps the
 // configuration with minimum width.
 void DownloadItemView::SizeLabelToMinWidth() {
@@ -766,10 +769,10 @@ void DownloadItemView::SizeLabelToMinWidth() {
     text.replace(sp_index, 1, L"\n");
     dangerous_download_label_->SetText(text);
     size = dangerous_download_label_->GetPreferredSize();
-    
+
     if (min_width == -1)
       min_width = size.width();
- 
+
     // If the width is growing again, it means we passed the optimal width spot.
     if (size.width() > min_width)
       break;
