1f842ab9b6efc9f5513a30f95ad4cbc0e4854b27
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-21 01:22:51 +0000

    2008-10-20  Antti Koivisto  <antti@apple.com>
            Reviewed by Adele Peterson.
    
            Fix https://bugs.webkit.org/show_bug.cgi?id=21763
            REGRESSION (r36108-r36113): Can't load picture content on wired.com galleries
            <rdar://problem/6285025
            
            If we get 304 response we need to explicitly check if that completes the frame load.
            
            No test case, simulating the condition is pretty difficult.
    
            * loader/loader.cpp:
            (WebCore::Loader::Host::didReceiveResponse):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/loader.cpp
+++ third_party/WebKit/WebCore/loader/loader.cpp
@@ -384,6 +384,10 @@ void Loader::Host::didReceiveResponse(SubresourceLoader* loader, const ResourceR
 
             // Existing resource is ok, just use it updating the expiration time.
             cache()->revalidationSucceeded(resource, response);
+            
+            if (request->docLoader()->frame())
+                request->docLoader()->frame()->loader()->checkCompleted();
+
             delete request;
 
             servePendingRequests();
