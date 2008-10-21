95f488d9fa52c969b3b265ad569180d5b60f7287
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-21 18:55:13 +0000

    When on Linux compile these strings in also.
    Review URL: http://codereview.chromium.org/7817
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3670 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/localized_strings.cc
+++ webkit/glue/localized_strings.cc
@@ -53,7 +53,7 @@ inline String GetLocalizedString(int message_id) {
   return webkit_glue::StdWStringToString(str);
 }
 
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
 String WebCore::searchableIndexIntroduction() {
   return GetLocalizedString(IDS_SEARCHABLE_INDEX_INTRO);
 }
@@ -99,7 +99,7 @@ String WebCore::AXImageMapText() {
 String WebCore::AXHeadingText() {
   return GetLocalizedString(IDS_AX_ROLE_HEADING);
 }
-#endif  // OS_WIN
+#endif  // OS_WIN || OS_LINUX
 String WebCore::AXButtonActionVerb() {
   return GetLocalizedString(IDS_AX_BUTTON_ACTION_VERB);
 }
