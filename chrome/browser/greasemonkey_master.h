fb2eb6eacac3807fef88dd8e1f1035764a6e84ae
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-21 00:49:49 +0000

    Show the path to the Greasemonkey script that caused an error in the console.
    Review URL: http://codereview.chromium.org/7533
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3643 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/greasemonkey_master.h
+++ chrome/browser/greasemonkey_master.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_GREASEMONKEY_MASTER_H__
 #define CHROME_BROWSER_GREASEMONKEY_MASTER_H__
 
-#include <vector>
-
 #include "base/process.h"
 #include "base/scoped_ptr.h"
 #include "base/shared_memory.h"
