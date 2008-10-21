444673663614dbe8d747e40dcbc09d26311fd888
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-21 18:08:09 +0000

    Fix a timing problem that caused the find bar's coordinates to be incorrect. The visible flag was getting set twice from two different sources, one of which was wrong. Review URL: http://codereview.chromium.org/7820
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3666 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.cc
+++ chrome/browser/automation/automation_provider.cc
@@ -1747,7 +1747,7 @@ void AutomationProvider::GetFindWindowVisibility(const IPC::Message& message,
   bool visible = false;
   WebContents* web_contents = GetWebContentsForHandle(handle, NULL);
   if (web_contents)
-    visible = web_contents->view()->GetFindBarWindowInfo(&position, &visible);
+    web_contents->view()->GetFindBarWindowInfo(&position, &visible);
 
   Send(new AutomationMsg_FindWindowVisibilityResponse(message.routing_id(),
                                                       visible));
@@ -1759,7 +1759,7 @@ void AutomationProvider::HandleFindWindowLocationRequest(
   bool visible = false;
   WebContents* web_contents = GetWebContentsForHandle(handle, NULL);
   if (web_contents)
-    visible = web_contents->view()->GetFindBarWindowInfo(&position, &visible);
+    web_contents->view()->GetFindBarWindowInfo(&position, &visible);
 
   Send(new AutomationMsg_FindWindowLocationResponse(message.routing_id(),
                                                     position.x(),
