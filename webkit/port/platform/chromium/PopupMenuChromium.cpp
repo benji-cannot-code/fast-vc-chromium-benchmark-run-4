e6d35af348313de5f96169ecb4898ba6c2ce744c
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-22 21:39:50 +0000

    Fix Linux build bustage
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3771 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/chromium/PopupMenuChromium.cpp
+++ webkit/port/platform/chromium/PopupMenuChromium.cpp
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Widget.h"
 #include "WidgetClientChromium.h"
 
-#if !PLATFORM(WIN_OS)
+#if PLATFORM(DARWIN)
 #include "KeyboardCodes.h"
 #endif
 
