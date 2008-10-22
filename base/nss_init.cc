438b08f6e3a504bfbc736857975c7de733a8a80d
dkegel@google.com <dkegel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-22 19:50:58 +0000

    Port SSLClientSocket to Linux Passes tests (once you enable them by removing DISABLED_). Probably want to add a mock https server so we can leave those tests enabled when we check in.
    Had to add full duplex support to TCPClientSocket on Linux
    to avoid kludgy plumbing issues.
    Also had to add dummy implementation of 
    X509Certificate::~X509Certificate to prevent link error.
    
    Rediffed to current trunk, addressed all review issues. 
    
    Review URL: http://codereview.chromium.org/4049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3751 0039d316-1c4b-4281-b951-d872f2087c98

--- base/nss_init.cc
+++ base/nss_init.cc
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <nss.h>
 
+// Work around https://bugzilla.mozilla.org/show_bug.cgi?id=455424
+// until NSS 3.12.2 comes out and we update to it.
+#define Lock FOO_NSS_Lock
+#include <ssl.h>
+#undef Lock
+
 #include "base/logging.h"
 #include "base/singleton.h"
 
@@ -16,9 +22,16 @@ class NSSInitSingleton {
  public:
   NSSInitSingleton() {
     CHECK(NSS_NoDB_Init(".") == SECSuccess);
+    // Enable ciphers
+    NSS_SetDomesticPolicy();
+    // Enable SSL
+    SSL_OptionSetDefault(SSL_SECURITY, PR_TRUE);
   }
 
   ~NSSInitSingleton() {
+    // Have to clear the cache, or NSS_Shutdown fails with SEC_ERROR_BUSY
+    SSL_ClearSessionCache();
+
     SECStatus status = NSS_Shutdown();
     DCHECK(status == SECSuccess);
   }
--- net/base/client_socket.h
+++ net/base/client_socket.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_BASE_CLIENT_SOCKET_H_
 #define NET_BASE_CLIENT_SOCKET_H_
 
+#include "base/logging.h"
 #include "net/base/socket.h"
+#include "net/base/net_errors.h"
 
 namespace net {
 
@@ -41,6 +43,16 @@ class ClientSocket : public Socket {
   // Called to test if the connection is still alive.  Returns false if a
   // connection wasn't established or the connection is dead.
   virtual bool IsConnected() const = 0;
+
+#if defined(OS_LINUX)
+  // Identical to posix system call getpeername().
+  // Needed by ssl_client_socket_nss.
+  virtual int GetPeerName(struct sockaddr *name, socklen_t *namelen) {
+    // Default implementation just permits some unit tests to link.
+    NOTREACHED();
+    return ERR_UNEXPECTED;
+  }
+#endif
 };
 
 }  // namespace net
