ce94fb9b871f1b221c45cf3839034d5cb9d5775e
ojan@google.com <ojan@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-22 22:46:22 +0000

    Plugin changes to make JSC build work. Review URL: http://codereview.chromium.org/7883
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3783 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_impl.h
+++ webkit/glue/webplugin_impl.h
@@ -67,6 +67,10 @@ class WebPluginContainer : public WebCore::Widget {
   virtual void attachToWindow();
   virtual void detachFromWindow();
 
+#if USE(JSC)
+  virtual bool isPluginView() const;
+#endif
+
   // Returns window-relative rectangles that should clip this widget.
   // Only rects that intersect the given bounds are relevant.
   // Use this to implement iframe shim behavior.
