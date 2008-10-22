2f10366ece0cb205864a9d71b1d29888baa0174d
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-22 17:32:55 +0000

    oops, didn't mean PLATFORM.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3742 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/chromium/DragImageRef.h
+++ webkit/port/platform/chromium/DragImageRef.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if PLATFORM(OS_WIN)
+#if defined(OS_WIN)
 typedef struct HBITMAP__* HBITMAP;
 #elif defined(OS_MACOSX)
 #if __OBJC__ 
