50c7cb647151fbf01a75a303e7ccf8c3b6d40d13
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-22 19:53:00 +0000

    2008-10-22  Ariya Hidayat  <ariya.hidayat@trolltech.com>
            Reviewed by Simon Hausmann.
    
            For public API, use the term boundingRect instead of boundingBox.
    
            * Api/qwebframe.cpp:
            (QWebHitTestResultPrivate::QWebHitTestResultPrivate):
            (QWebHitTestResult::boundingRect):
            * Api/qwebframe.h:
            * Api/qwebframe_p.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37787 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.h
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.h
@@ -68,7 +68,7 @@ public:
     bool isNull() const;
 
     QPoint pos() const;
-    QRect boundingBox() const;
+    QRect boundingRect() const;
     QString title() const;
 
     QString linkText() const;
--- third_party/WebKit/WebKit/qt/Api/qwebframe_p.h
+++ third_party/WebKit/WebKit/qt/Api/qwebframe_p.h
@@ -101,7 +101,7 @@ public:
     QWebHitTestResultPrivate(const WebCore::HitTestResult &hitTest);
 
     QPoint pos;
-    QRect boundingBox;
+    QRect boundingRect;
     QString title;
     QString linkText;
     QUrl linkUrl;
