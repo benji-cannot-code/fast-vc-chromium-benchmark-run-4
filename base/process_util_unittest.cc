919415dca4ed3e2a1f6b04e4c0bb9933fc67f30a
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-22 01:15:47 +0000

    Added linux process utilities and tests.
    Review URL: http://codereview.chromium.org/7202
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3715 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_unittest.cc
+++ base/process_util_unittest.cc
@@ -5,10 +5,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define _CRT_SECURE_NO_WARNINGS
 
-#include "testing/gtest/include/gtest/gtest.h"
+#include "base/multiprocess_test.h"
 #include "base/process_util.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
-TEST(ProcessUtilTest, EnableLFH) {
+#if defined(OS_WIN)
+#include <windows.h>
+#elif defined(OS_LINUX)
+#include <dlfcn.h>
+#endif
+
+namespace {
+class ProcessUtilTest : public MultiProcessTest {
+};
+}
+
+extern "C" int DYNAMIC_EXPORT SimpleChildProcess() {
+  return 0;
+}
+
+TEST_F(ProcessUtilTest, SpawnChild) {
+  ProcessHandle handle = this->SpawnChild(L"SimpleChildProcess");
+
+  ASSERT_NE(static_cast<ProcessHandle>(NULL), handle);
+  EXPECT_TRUE(process_util::WaitForSingleProcess(handle, 1000));
+}
+
+// TODO(estade): if possible, port these 2 tests.
+#if defined(OS_WIN)
+TEST_F(ProcessUtilTest, EnableLFH) {
   ASSERT_TRUE(process_util::EnableLowFragmentationHeap());
   if (IsDebuggerPresent()) {
     // Under these conditions, LFH can't be enabled. There's no point to test
@@ -39,7 +64,7 @@ TEST(ProcessUtilTest, EnableLFH) {
   }
 }
 
-TEST(ProcessUtilTest, CalcFreeMemory) {
+TEST_F(ProcessUtilTest, CalcFreeMemory) {
   process_util::ProcessMetrics* metrics =
     process_util::ProcessMetrics::CreateProcessMetrics(::GetCurrentProcess());
   ASSERT_TRUE(NULL != metrics);
@@ -73,4 +98,5 @@ TEST(ProcessUtilTest, CalcFreeMemory) {
   delete[] alloc;
   delete metrics;
 }
+#endif  // defined(OS_WIN)
 
