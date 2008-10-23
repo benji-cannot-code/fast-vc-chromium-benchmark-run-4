71b2a0630445e547a4bc7240169c2f8a50f5ca49
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-23 21:42:07 +0000

    Port:  - net/base/dns_resolution_observer.cc  - net/http/http_network_transaction.cc  - net/url_request/url_request_filter.cc Add to net library the above and:  - net/base/dns_resolution_observer.cc  - net/base/upload_data_stream.cc  - net/http/http_network_layer.cc  - net/proxy/proxy_resolver_fixed.cc  - net/proxy/proxy_service.cc  - net/url_request/url_request_job_manager.cc Add to net_unittests:  - net/http/http_network_transaction_unittest.cc  - net/http/http_transaction_unittest.cc  - net/http/http_util_unittest.cc
    Patch by Paweł Hajdan jr <phajdan.jr@gmail.com>
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3867 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/dns_resolution_observer.cc
+++ net/base/dns_resolution_observer.cc
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file supports network stack independent notification of progress
 // towards resolving a hostname.
 
-#include <windows.h>
-#include <string>
-
 #include "net/base/dns_resolution_observer.h"
+
+#include <string>
 
 #include "base/logging.h"
 
--- net/url_request/url_request_filter.cc
+++ net/url_request/url_request_filter.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
-#include "net/url_request/url_request_inet_job.h"
-
 URLRequestFilter* URLRequestFilter::shared_instance_ = NULL;
 
 /* static */
