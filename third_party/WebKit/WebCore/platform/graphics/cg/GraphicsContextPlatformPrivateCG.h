d581b3f132e8376582dc82b4788465f012320d35
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-23 21:17:27 +0000

    Reviewed by Sam Weinig.
            Un-break the Darwin build.
    
            * platform/graphics/cg/GraphicsContextPlatformPrivateCG.h:
            * platform/graphics/cg/ImageCG.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37825 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/GraphicsContextPlatformPrivateCG.h
+++ third_party/WebKit/WebCore/platform/graphics/cg/GraphicsContextPlatformPrivateCG.h
@@ -46,7 +46,7 @@ public:
         CGContextRelease(m_cgContext);
     }
 
-#if PLATFORM(DARWIN)
+#if PLATFORM(MAC) || PLATFORM(CHROMIUM)
     // These methods do nothing on Mac.
     void save() {}
     void restore() {}
--- third_party/WebKit/WebCore/platform/graphics/cg/ImageCG.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cg/ImageCG.cpp
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PlatformString.h"
 #include <ApplicationServices/ApplicationServices.h>
 
-#if PLATFORM(DARWIN)
+#if PLATFORM(MAC) || PLATFORM(CHROMIUM)
 #include "WebCoreSystemInterface.h"
 #endif
 
