a3bb24346f185ae7776f6e0b285fb94a2300b89c
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-23 22:29:54 +0000

    2008-10-23  Gavin Barraclough  <barraclough@apple.com>
            Reviewed by Oliver Hunt.
    
            Fix hideous pathological case performance when looking up repatch info, bug #21727.
    
            When repatching JIT code to optimize we look up records providing information about
            the generated code (also used to track recsources used in linking to be later released). 
            The lookup was being performed using a linear scan of all such records.
    
            (1) Split up the different types of reptach information.  This means we can search them
                separately, and in some cases should reduce their size.
            (2) In the case of property accesses, search with a binary chop over the data.
            (3) In the case of calls, pass a pointer to the repatch info into the relink function.
    
            * VM/CTI.cpp:
            (JSC::CTI::CTI):
            (JSC::CTI::compileOpCall):
            (JSC::CTI::privateCompileMainPass):
            (JSC::CTI::privateCompileSlowCases):
            (JSC::CTI::privateCompile):
            (JSC::CTI::unlinkCall):
            (JSC::CTI::linkCall):
            * VM/CTI.h:
            * VM/CodeBlock.cpp:
            (JSC::CodeBlock::dump):
            (JSC::CodeBlock::~CodeBlock):
            (JSC::CodeBlock::unlinkCallers):
            (JSC::CodeBlock::derefStructureIDs):
            * VM/CodeBlock.h:
            (JSC::StructureStubInfo::StructureStubInfo):
            (JSC::CallLinkInfo::CallLinkInfo):
            (JSC::CallLinkInfo::setUnlinked):
            (JSC::CallLinkInfo::isLinked):
            (JSC::getStructureStubInfoReturnLocation):
            (JSC::binaryChop):
            (JSC::CodeBlock::addCaller):
            (JSC::CodeBlock::getStubInfo):
            * VM/CodeGenerator.cpp:
            (JSC::CodeGenerator::emitResolve):
            (JSC::CodeGenerator::emitGetById):
            (JSC::CodeGenerator::emitPutById):
            (JSC::CodeGenerator::emitCall):
            (JSC::CodeGenerator::emitConstruct):
            * VM/Machine.cpp:
            (JSC::Machine::cti_vm_lazyLinkCall):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37831 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/CTI.h
+++ third_party/WebKit/JavaScriptCore/VM/CTI.h
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ARG_instr4 static_cast<Instruction*>(ARGS[4])
 #define ARG_instr5 static_cast<Instruction*>(ARGS[5])
 #define ARG_instr6 static_cast<Instruction*>(ARGS[6])
+#define ARG_linkInfo2 static_cast<CallLinkInfo*>(ARGS[2])
 
 #define CTI_RETURN_ADDRESS_SLOT (ARGS[-1])
 
@@ -95,9 +96,9 @@ namespace JSC {
     class StringJumpTable;
     class StructureIDChain;
 
+    struct CallLinkInfo;
     struct Instruction;
     struct OperandTypes;
-    struct StructureStubInfo;
 
     typedef JSValue* (SFX_CALL *CTIHelper_j)(CTI_ARGS);
     typedef JSObject* (SFX_CALL *CTIHelper_o)(CTI_ARGS);
@@ -335,8 +336,8 @@ namespace JSC {
             return cti.privateCompilePatchGetArrayLength(returnAddress);
         }
 
-        static void linkCall(CodeBlock* callerCodeBlock, JSFunction* callee, CodeBlock* calleeCodeBlock, void* ctiCode, void* returnAddress, int callerArgCount);
-        static void unlinkCall(StructureStubInfo*);
+        static void linkCall(JSFunction* callee, CodeBlock* calleeCodeBlock, void* ctiCode, CallLinkInfo* callLinkInfo, int callerArgCount);
+        static void unlinkCall(CallLinkInfo*);
 
         inline static JSValuePtr execute(void* code, RegisterFile* registerFile, CallFrame* callFrame, JSGlobalData* globalData, JSValuePtr* exception)
         {
@@ -434,7 +435,8 @@ namespace JSC {
 
         Vector<CallRecord> m_calls;
         Vector<X86Assembler::JmpDst> m_labels;
-        Vector<StructureStubCompilationInfo> m_structureStubCompilationInfo;
+        Vector<StructureStubCompilationInfo> m_propertyAccessCompilationInfo;
+        Vector<StructureStubCompilationInfo> m_callStructureStubCompilationInfo;
         Vector<JmpTable> m_jmpTable;
 
         struct JSRInfo {
--- third_party/WebKit/JavaScriptCore/VM/CodeGenerator.cpp
+++ third_party/WebKit/JavaScriptCore/VM/CodeGenerator.cpp
@@ -966,7 +966,7 @@ RegisterID* CodeGenerator::emitResolve(RegisterID* dst, const Identifier& proper
     }
 
     if (globalObject) {
-        m_codeBlock->structureIDInstructions.append(instructions().size());
+        m_codeBlock->globalResolveInstructions.append(instructions().size());
         emitOpcode(op_resolve_global);
         instructions().append(dst->index());
         instructions().append(globalObject);
@@ -1046,7 +1046,7 @@ RegisterID* CodeGenerator::emitResolveFunction(RegisterID* baseDst, RegisterID* 
 
 RegisterID* CodeGenerator::emitGetById(RegisterID* dst, RegisterID* base, const Identifier& property)
 {
-    m_codeBlock->structureIDInstructions.append(instructions().size());
+    m_codeBlock->propertyAccessInstructions.append(instructions().size());
 
     emitOpcode(op_get_by_id);
     instructions().append(dst->index());
@@ -1061,7 +1061,7 @@ RegisterID* CodeGenerator::emitGetById(RegisterID* dst, RegisterID* base, const 
 
 RegisterID* CodeGenerator::emitPutById(RegisterID* base, const Identifier& property, RegisterID* value)
 {
-    m_codeBlock->structureIDInstructions.append(instructions().size());
+    m_codeBlock->propertyAccessInstructions.append(instructions().size());
 
     emitOpcode(op_put_by_id);
     instructions().append(base->index());
@@ -1220,7 +1220,7 @@ RegisterID* CodeGenerator::emitCall(OpcodeID opcodeID, RegisterID* dst, Register
     }
 
     emitExpressionInfo(divot, startOffset, endOffset);
-    m_codeBlock->structureIDInstructions.append(instructions().size());
+    m_codeBlock->callLinkInfos.append(CallLinkInfo());
     emitOpcode(opcodeID);
     instructions().append(dst->index());
     instructions().append(func->index());
@@ -1284,7 +1284,7 @@ RegisterID* CodeGenerator::emitConstruct(RegisterID* dst, RegisterID* func, Argu
         callFrame.append(newTemporary());
 
     emitExpressionInfo(divot, startOffset, endOffset);
-    m_codeBlock->structureIDInstructions.append(instructions().size());
+    m_codeBlock->callLinkInfos.append(CallLinkInfo());
     emitOpcode(op_construct);
     instructions().append(dst->index());
     instructions().append(func->index());
