0e0d4cae24dd130d58ae9eadc86361b7f3408b5d
dsmith@webkit.org <dsmith@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-23 00:49:27 +0000

    WebCore:
    2008-10-22  David Smith  <catfish.man@gmail.com>
    
            Reviewed by andersca.
                   
            https://bugs.webkit.org/show_bug.cgi?id=19974
            getElementsByClassName not live enough
    
            Test: fast/dom/getElementsByClassName/015.html
    
            * dom/StyledElement.cpp:
            (WebCore::StyledElement::classAttributeChanged): call dispatchSubtreeModifiedEvent()
    
    LayoutTests:
    
    2008-10-22  David Smith  <catfish.man@gmail.com>
    
            Reviewed by andersca.
            
            https://bugs.webkit.org/show_bug.cgi?id=19974
            getElementsByClassName not live enough
    
            * fast/dom/getElementsByClassName/015-expected.txt: Added.
            * fast/dom/getElementsByClassName/015.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/StyledElement.cpp
+++ third_party/WebKit/WebCore/dom/StyledElement.cpp
@@ -228,6 +228,7 @@ void StyledElement::classAttributeChanged(const AtomicString& newClassString)
             mappedAttributes()->clearClass();
     }
     setChanged();
+    dispatchSubtreeModifiedEvent();
 }
 
 void StyledElement::parseMappedAttribute(MappedAttribute *attr)
