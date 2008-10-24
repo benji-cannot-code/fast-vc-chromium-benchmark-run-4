c7388116664b1e9bfeb6e1bcecbd274596840a3d
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-24 17:44:42 +0000

    In the upstream bug, we realized that the only caller of this function has a CG call too, which explains why this is PLATFORM(CG).  I've added PLATFORM(DARWIN) just to clarify that the implementation is Mac-only. Review URL: http://codereview.chromium.org/7975
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3925 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/chromium/MimeTypeRegistryChromium.cpp
+++ webkit/port/platform/chromium/MimeTypeRegistryChromium.cpp
@@ -37,10 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore 
 {
 
-// From MIMETypeRegistryMac.mm.  PLATFORM(CG) seems wrong, this should
-// probably be PLATFORM(CF), but the caller uses PLATFORM(CG).  This is
-// filed upstream at https://bugs.webkit.org/show_bug.cgi?id=21847.
-#if PLATFORM(CG)
+// From MIMETypeRegistryMac.mm.
+#if PLATFORM(DARWIN) && PLATFORM(CG)
 String getMIMETypeForUTI(const String & uti)
 {
     CFStringRef utiref = uti.createCFString();
