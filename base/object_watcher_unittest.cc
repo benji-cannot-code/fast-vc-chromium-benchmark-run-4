83c18c177bcea3df52a8ca3c857023e7110266f2
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-24 19:21:13 +0000

    Make IPC::SyncChannel not duplicate the underlying  MessageLoop implementation by pumping messages on its own.  This fixes the problem of windowless plugins not painting on right click, and generally makes this class almost ported, other than using a generic version of events/locks.Through this change I've also cleaned up the class and hopefully made it more understandable. Review URL: http://codereview.chromium.org/8001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3934 0039d316-1c4b-4281-b951-d872f2087c98

--- base/object_watcher_unittest.cc
+++ base/object_watcher_unittest.cc
@@ -35,6 +35,7 @@ void RunTest_BasicSignal(MessageLoop::Type message_loop_type) {
   MessageLoop message_loop(message_loop_type);
 
   base::ObjectWatcher watcher;
+  EXPECT_EQ(NULL, watcher.GetWatchedObject());
 
   // A manual-reset event that is not yet signaled.
   HANDLE event = CreateEvent(NULL, TRUE, FALSE, NULL);
@@ -42,11 +43,13 @@ void RunTest_BasicSignal(MessageLoop::Type message_loop_type) {
   QuitDelegate delegate;
   bool ok = watcher.StartWatching(event, &delegate);
   EXPECT_TRUE(ok);
+  EXPECT_EQ(event, watcher.GetWatchedObject());
   
   SetEvent(event);
 
   MessageLoop::current()->Run();
 
+  EXPECT_EQ(NULL, watcher.GetWatchedObject());
   CloseHandle(event);
 }
 
