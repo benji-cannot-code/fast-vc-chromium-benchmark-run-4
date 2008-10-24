8379551e68715cf8bc703f4744b55db76a145842
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-24 01:33:52 +0000

    Resolve undefined symbols with "mime" in their names resulting from PLATFORM(MAC) killing Review URL: http://codereview.chromium.org/8137
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3894 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/mac/PasteboardMac.mm
+++ webkit/port/platform/mac/PasteboardMac.mm
@@ -283,6 +283,7 @@ void Pasteboard::writeURL(const KURL& url, const String& titleStr, Frame* frame)
 #endif
 }
 
+#if 0
 static NSFileWrapper* fileWrapperForImage(CachedResource* resource, NSURL *url)
 {
     SharedBuffer* coreData = resource->data();
@@ -297,7 +298,6 @@ static NSFileWrapper* fileWrapperForImage(CachedResource* resource, NSURL *url)
     return wrapper;
 }
 
-#if 0
 void Pasteboard::writeFileWrapperAsRTFDAttachment(NSFileWrapper* wrapper)
 {
     NSTextAttachment *attachment = [[NSTextAttachment alloc] initWithFileWrapper:wrapper];
