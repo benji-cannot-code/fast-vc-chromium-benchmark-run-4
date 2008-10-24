3b5a93e04afa14bb53530c97fb619cc2aab66da2
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-24 23:46:30 +0000

    Reviewed by Sam Weinig.
            Generate missing constructors and add test to prevent missing ones in the future
            https://bugs.webkit.org/show_bug.cgi?id=21846
    
            fast/dom/wrapper-classes-expected.txt results were incorrectly platform-specific
            that test used to be platform-dependent, but isn't anymore
    
            * fast/dom/Window/window-properties-expected.txt:
            * fast/dom/wrapper-classes-expected.txt:
            * fast/js/global-constructors.html: Added.
            * fast/js/resources/global-constructors.js: Added.
            * platform/mac/fast/dom/wrapper-classes-expected.txt: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37875 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/global-constructors.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("This test documents our set of global constructors we expose on the window object (FF and Opera don't expose them on the window, btw).  This also checks to make sure than any constructor attribute we expose has the expected constructor type.")
+
+var constructorNames = [];
+
+for (var name in window) {
+    var value = window[name];
+    var re = new RegExp("Constructor]$");
+    var isConstructor = re.exec(value);
+    if (isConstructor)
+        constructorNames.push(name);
+}
+
+constructorNames.sort();
+
+for (var x in constructorNames) {
+    var name = constructorNames[x];
+    var expectedConstructorName = "'[object " + name + "Constructor]'";
+    if (name == "XMLDocument")
+        // Gecko exposes an "XMLDocument" constructor, but we just use Document for XML documents instead of a custom sub-type
+        expectedConstructorName = "'[object DocumentConstructor]'";
+
+    shouldBe("" + name + ".toString()", expectedConstructorName);
+}
+
+var successfullyParsed = true;
