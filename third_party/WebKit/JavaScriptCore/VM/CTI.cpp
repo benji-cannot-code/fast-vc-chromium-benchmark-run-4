376a4fb0e2a3d77f3c6302d133c1808d5e776e82
jmalonzo@webkit.org <jmalonzo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-25 09:37:56 +0000

    2008-10-25  Jan Michael Alonzo  <jmalonzo@webkit.org>
            JSC Build fix, not reviewed.
    
            * VM/CTI.cpp: add missing include stdio.h for debug builds
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37888 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/CTI.cpp
+++ third_party/WebKit/JavaScriptCore/VM/CTI.cpp
@@ -36,6 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wrec/WREC.h"
 #include "ResultType.h"
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 using namespace std;
 
 namespace JSC {
