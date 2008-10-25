02748d9e4d3703b2b6ca91965f19d5650b3eadc5
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-25 06:38:40 +0000

    2008-10-24  Sam Weinig  <sam@webkit.org>
            Another windows build fix.
    
            * WebCoreSupport/WebChromeClient.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37884 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebCoreSupport/WebChromeClient.cpp
+++ third_party/WebKit/WebKit/win/WebCoreSupport/WebChromeClient.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma warning(push, 0)
 #include <WebCore/BString.h>
 #include <WebCore/ContextMenu.h>
+#include <WebCore/FileChooser.h>
 #include <WebCore/FloatRect.h>
 #include <WebCore/FrameLoadRequest.h>
 #include <WebCore/FrameView.h>
