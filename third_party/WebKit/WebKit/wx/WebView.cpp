4c3e592fc1807785d051557915c5f1a47cff9db2
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-27 22:15:11 +0000

    Reviewed by Kevin Ollivier.
    Update the active state as well as the focused state as both need to be true
    for the caret to be drawn.
            
    https://bugs.webkit.org/show_bug.cgi?id=21900
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/wx/WebView.cpp
+++ third_party/WebKit/WebKit/wx/WebView.cpp
@@ -565,8 +565,10 @@ void wxWebView::OnSetFocus(wxFocusEvent& event)
     if (m_mainFrame)
         frame = m_mainFrame->GetFrame();
         
-    if (frame)
+    if (frame) {
+        m_impl->page->focusController()->setActive(true);
         frame->selection()->setFocused(true);
+    }
 
     event.Skip();
 }
@@ -577,9 +579,10 @@ void wxWebView::OnKillFocus(wxFocusEvent& event)
     if (m_mainFrame)
         frame = m_mainFrame->GetFrame();
         
-    if (frame)
+    if (frame) {
+        m_impl->page->focusController()->setActive(false);
         frame->selection()->setFocused(false);
-
+    }
     event.Skip();
 }
 
