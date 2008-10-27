4f6178179a339be68b50618be93655aea02c0027
jar@google.com <jar@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-27 18:54:18 +0000

    Lint message loop code (added explicit to a class definition)
    Incidental lint of code noted the need for explicit... so I tossed it in.
    
    r=mbelshe
    Review URL: http://codereview.chromium.org/8623
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4014 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop.h
+++ base/message_loop.h
@@ -98,10 +98,10 @@ class MessageLoop : public base::MessagePump::Delegate {
   //
   // NOTE: These methods may be called on any thread.  The Task will be invoked
   // on the thread that executes MessageLoop::Run().
-  
+
   void PostTask(
       const tracked_objects::Location& from_here, Task* task);
-  
+
   void PostDelayedTask(
       const tracked_objects::Location& from_here, Task* task, int delay_ms);
 
@@ -248,7 +248,7 @@ class MessageLoop : public base::MessagePump::Delegate {
 
   class AutoRunState : RunState {
    public:
-    AutoRunState(MessageLoop* loop);
+    explicit AutoRunState(MessageLoop* loop);
     ~AutoRunState();
    private:
     MessageLoop* loop_;
@@ -265,7 +265,7 @@ class MessageLoop : public base::MessagePump::Delegate {
     PendingTask(Task* task, bool nestable)
         : task(task), sequence_num(0), nestable(nestable) {
     }
-    
+
     // Used to support sorting.
     bool operator<(const PendingTask& other) const;
   };
@@ -355,7 +355,7 @@ class MessageLoop : public base::MessagePump::Delegate {
   // A list of tasks that need to be processed by this instance.  Note that
   // this queue is only accessed (push/pop) by our current thread.
   TaskQueue work_queue_;
-  
+
   // Contains delayed tasks, sorted by their 'delayed_run_time' property.
   DelayedTaskQueue delayed_work_queue_;
 
@@ -477,7 +477,7 @@ class MessageLoopForIO : public MessageLoop {
   typedef base::MessagePumpLibevent::Watcher Watcher;
 
   // Please see MessagePumpLibevent for definitions of these methods.
-  void WatchSocket(int socket, short interest_mask, 
+  void WatchSocket(int socket, short interest_mask,
                    struct event* e, Watcher* watcher);
   void UnwatchSocket(struct event* e);
 #endif  // defined(OS_POSIX)
