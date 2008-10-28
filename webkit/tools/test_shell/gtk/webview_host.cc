4f79d89bd195e71304d2a7245b71671327d44ea0
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-28 17:49:06 +0000

    * Revert "Start writing the GTK code for test_shell." This reverts commit 54dfed3ab933cea7492ae1ed2f35cdbfe822e8a4.
    Review URL: http://codereview.chromium.org/8839
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4072 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/gtk/webview_host.cc
+++ webkit/tools/test_shell/gtk/webview_host.cc
@@ -14,19 +14,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/webview.h"
 
 /*static*/
-WebViewHost* WebViewHost::Create(GtkWidget* box,
+WebViewHost* WebViewHost::Create(GtkWidget* parent_window,
                                  WebViewDelegate* delegate,
                                  const WebPreferences& prefs) {
-  // TODO(agl):
-  // /usr/local/google/agl/src/chrome/src/webkit/tools/test_shell/gtk/webview_host.cc:19: error: no matching function for call to 'WebWidgetHost::Create(GtkWidget*&, WebViewDelegate*&)'
-  WebViewHost* host = reinterpret_cast<WebViewHost *>(WebWidgetHost::Create(box, NULL));
+  WebViewHost* host = new WebViewHost();
 
   // TODO(erg):
   // - Set "host->view_"
   // - Call "host->webwidget_->Resize"
   host->webwidget_ = WebView::Create(delegate, prefs);
-  host->webwidget_->Resize(gfx::Size(640, 480));
-  host->webwidget_->Layout();
 
   return host;
 }
