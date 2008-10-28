07a97dad7f64d4b33b8613bc1506dec0fa71f05d
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-28 21:59:28 +0000

    2008-10-28  Alp Toker  <alp@nuanti.com>
            Reviewed by Mark Rowe.
    
            Fix GTK DRT hang when running the tests.
    
            Update output from the DRT tool to print an additional '#EOF' to match
            breaking changes that were made in r37434.
    
            * DumpRenderTree/gtk/DumpRenderTree.cpp:
            (dump):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37944 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/gtk/DumpRenderTree.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/gtk/DumpRenderTree.cpp
@@ -181,7 +181,7 @@ void dump()
         }
 
         if (printSeparators) {
-            puts("#EOF");
+            puts("#EOF"); // terminate the content block
             fputs("#EOF\n", stderr);
             fflush(stdout);
             fflush(stderr);
@@ -192,11 +192,14 @@ void dump()
         if (!gLayoutTestController->dumpAsText() && !gLayoutTestController->dumpDOMAsWebArchive() && !gLayoutTestController->dumpSourceAsWebArchive()) {
             // FIXME: Add support for dumping pixels
         }
-
-        fflush(stdout);
     }
 
     // FIXME: call displayWebView here when we support --paint
+
+    puts("#EOF"); // terminate the (possibly empty) pixels block
+
+    fflush(stdout);
+    fflush(stderr);
 
     done = true;
 }
