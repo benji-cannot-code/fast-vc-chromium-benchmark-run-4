9fec65b88394264e2d6c6d56dd33bc0f33c22ffd
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-28 17:25:25 +0000

    - Make user script loading asynchronous on the file thread. - Automatically reload scripts when the directory changes. - Add a unit test for the GreasemonkeyMaster. Review URL: http://codereview.chromium.org/7472
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4069 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profile.cc
+++ chrome/browser/profile.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 
 #include "base/command_line.h"
+#include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/lock.h"
 #include "base/path_service.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_list.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/download/download_manager.h"
+#include "chrome/browser/greasemonkey_master.h"
 #include "chrome/browser/history/history.h"
 #include "chrome/browser/navigation_controller.h"
 #include "chrome/browser/profile_manager.h"
@@ -379,6 +381,10 @@ class OffTheRecordProfileImpl : public Profile,
     return profile_->GetVisitedLinkMaster();
   }
 
+  virtual GreasemonkeyMaster* GetGreasemonkeyMaster() {
+    return profile_->GetGreasemonkeyMaster();
+  }
+
   virtual HistoryService* GetHistoryService(ServiceAccessType sat) {
     if (sat == EXPLICIT_ACCESS) {
       return profile_->GetHistoryService(sat);
@@ -669,6 +675,19 @@ VisitedLinkMaster* ProfileImpl::GetVisitedLinkMaster() {
   }
 
   return visited_link_master_.get();
+}
+
+GreasemonkeyMaster* ProfileImpl::GetGreasemonkeyMaster() {
+  if (!greasemonkey_master_.get()) {
+    std::wstring script_dir_str;
+    PathService::Get(chrome::DIR_USER_SCRIPTS, &script_dir_str);
+    FilePath script_dir(script_dir_str);
+    greasemonkey_master_ =
+        new GreasemonkeyMaster(g_browser_process->file_thread()->message_loop(),
+                               script_dir);
+  }
+
+  return greasemonkey_master_.get();
 }
 
 PrefService* ProfileImpl::GetPrefs() {
--- chrome/browser/profile.h
+++ chrome/browser/profile.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class BookmarkModel;
 class DownloadManager;
+class GreasemonkeyMaster;
 class HistoryService;
 class NavigationController;
 class PrefService;
@@ -98,6 +99,11 @@ class Profile {
   // profile.  The VisitedLinkMaster is lazily created the first time
   // that this method is called.
   virtual VisitedLinkMaster* GetVisitedLinkMaster() = 0;
+
+  // Retrieves a pointer to the GreasemonkeyMaster associated with this
+  // profile.  The GreasemonkeyMaster is lazily created the first time
+  // that this method is called.
+  virtual GreasemonkeyMaster* GetGreasemonkeyMaster() = 0;
 
   // Retrieves a pointer to the HistoryService associated with this
   // profile.  The HistoryService is lazily created the first time
@@ -236,6 +242,7 @@ class ProfileImpl : public Profile {
   virtual Profile* GetOffTheRecordProfile();
   virtual Profile* GetOriginalProfile();
   virtual VisitedLinkMaster* GetVisitedLinkMaster();
+  virtual GreasemonkeyMaster* GetGreasemonkeyMaster();
   virtual HistoryService* GetHistoryService(ServiceAccessType sat);
   virtual WebDataService* GetWebDataService(ServiceAccessType sat);
   virtual PrefService* GetPrefs();
@@ -283,6 +290,7 @@ class ProfileImpl : public Profile {
   std::wstring path_;
   bool off_the_record_;
   scoped_ptr<VisitedLinkMaster> visited_link_master_;
+  scoped_refptr<GreasemonkeyMaster> greasemonkey_master_;
   scoped_ptr<PrefService> prefs_;
   scoped_ptr<TemplateURLFetcher> template_url_fetcher_;
   scoped_ptr<TemplateURLModel> template_url_model_;
