8f27089b4279150827894cdd0f84ab6626fb02e7
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-28 17:48:33 +0000

    2008-10-28  Kevin McCullough  <kmccullough@apple.com>
            Reviewed by Dan Bernstein.
    
            -Removed unused includes.
            Apparent .4% speedup in Sunspider
    
            * kjs/JSObject.cpp:
            * kjs/interpreter.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@37929 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/JSObject.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/JSObject.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "nodes.h"
 #include "operations.h"
 #include <math.h>
-#include <profiler/Profiler.h>
 #include <wtf/Assertions.h>
 
 #define JSOBJECT_MARK_TRACING 0
--- third_party/WebKit/JavaScriptCore/kjs/interpreter.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/interpreter.cpp
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Parser.h"
 #include "completion.h"
 #include "debugger.h"
-#include <profiler/Profiler.h>
 #include <stdio.h>
 
 #if !PLATFORM(WIN_OS)
