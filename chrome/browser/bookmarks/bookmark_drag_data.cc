1e4cbb6b91f77f39d09a45279139a04e5b085d65
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-28 00:53:22 +0000

    Make it so the user can't drag javascript: links to the desktop from web content or the bookmarks bar.  Maintain the ability to drag javascript: links from web content to the bookmarks bar or to drag the javascript: link as plain text.
    BUG=3431
    Review URL: http://codereview.chromium.org/8638
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4050 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_drag_data.cc
+++ chrome/browser/bookmarks/bookmark_drag_data.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_drag_data.h"
 
 #include "base/pickle.h"
+#include "base/string_util.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/profile.h"
 #include "chrome/common/os_exchange_data.h"
@@ -83,8 +84,13 @@ void BookmarkDragData::Write(Profile* profile, OSExchangeData* data) const {
 
   // If there is only one element and it is a URL, write the URL to the
   // clipboard.
-  if (elements.size() == 1 && elements[0].is_url)
-    data->SetURL(elements[0].url, elements[0].title);
+  if (elements.size() == 1 && elements[0].is_url) {
+    if (elements[0].url.SchemeIs("javascript")) {
+      data->SetString(ASCIIToWide(elements[0].url.spec()));
+    } else {
+      data->SetURL(elements[0].url, elements[0].title);
+    }
+  }
 
   Pickle data_pickle;
   data_pickle.WriteWString(profile->GetPath());
