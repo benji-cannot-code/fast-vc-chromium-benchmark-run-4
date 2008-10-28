dc7ad5a87d920073215bd67d9a442d551f89371b
petersont@google.com <petersont@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-28 22:09:49 +0000

    Added a class with AutofillForm factory method.
    Review URL: http://codereview.chromium.org/8402
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4095 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/autofill_form.h
+++ webkit/glue/autofill_form.h
@@ -9,10 +9,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "webkit/glue/autofill_form.h"
+
+namespace WebCore {
+  class HTMLFormElement;
+}
+
 // The AutofillForm struct represents a single HTML form together with the
 // values entered in the fields.
 
-struct AutofillForm {
+class AutofillForm {
+ public:
   // Struct for storing name/value pairs.
   struct Element {
     Element() {}
@@ -23,6 +30,8 @@ struct AutofillForm {
     std::wstring name;
     std::wstring value;
   };
+
+  static AutofillForm* CreateAutofillForm(WebCore::HTMLFormElement* form);
 
   // A vector of all the input fields in the form.
   std::vector<Element> elements;
