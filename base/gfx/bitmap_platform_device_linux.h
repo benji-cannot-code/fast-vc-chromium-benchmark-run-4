bf75819f897c71e7f56b12cb341a03d5fe80f368
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-29 18:17:02 +0000

    System includes need to use angle brackets for check_deps
    TBR=tc
    
    Review URL: http://codereview.chromium.org/8706
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4141 0039d316-1c4b-4281-b951-d872f2087c98

--- base/gfx/bitmap_platform_device_linux.h
+++ base/gfx/bitmap_platform_device_linux.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gfx/platform_device_linux.h"
 #include "base/ref_counted.h"
 
-#include "gdk-pixbuf/gdk-pixbuf.h"
+#include <gdk-pixbuf/gdk-pixbuf.h>
 
 namespace gfx {
 
