077d96c6e859e4e496657cc56339084d45ca9baa
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-29 20:13:27 +0000

    wx build fixes after addition of runtime and ImageBuffer changes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@37976 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/wx/ImageBufferData.h
+++ third_party/WebKit/WebCore/platform/graphics/wx/ImageBufferData.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007 Kevin Ollivier <kevino@theolliviers.com>
+ * Copyright (C) 2008 Kevin Ollivier. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -21,60 +21,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "config.h"
-#include "ImageBuffer.h"
+#ifndef ImageBufferData_h
+#define ImageBufferData_h
 
-#include "GraphicsContext.h"
-#include "ImageData.h"
-#include "NotImplemented.h"
+
+#include "OwnPtr.h"
 
 namespace WebCore {
 
-ImageBufferData::ImageBufferData(const IntSize&)
-{
-}
+class IntSize;
 
-ImageBuffer::ImageBuffer(const IntSize&, bool grayScale, bool& success) : 
-    m_data(IntSize())
-{
-    notImplemented();
-    success = false;
-}
+class ImageBufferData {
+public:
+    ImageBufferData(const IntSize&);
+};
 
-ImageBuffer::~ImageBuffer()
-{
-}
+}  // namespace WebCore
 
-GraphicsContext* ImageBuffer::context() const
-{
-    notImplemented();
-    return 0;
-}
-
-PassRefPtr<ImageData> ImageBuffer::getImageData(const IntRect&) const
-{
-    notImplemented();
-    return 0;
-}
-
-void ImageBuffer::putImageData(ImageData*, const IntRect&, const IntPoint&)
-{
-    notImplemented();
-}
-
-String ImageBuffer::toDataURL(const String&) const
-{
-    notImplemented();
-    return String();
-}
-
-Image* ImageBuffer::image() const
-{
-    notImplemented();
-    return 0;
-}
-
-} // namespace WebCore
+#endif  // ImageBufferData_h
