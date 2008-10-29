d6ba17715d1db9131bf201c2384e2260629685c5
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-29 17:09:15 +0000

    Add fieldnames to SQL statements for better forward-compatibility.  Had we had this code in place already, I wouldn't have needed to rev the compatible version number in my upcoming database change >:(
    I checked the rest of our sourcebase's INSERT and SELECT statements, but these were the only ones that needed to have fieldnames added (in a few cases SELECT * was appropriate).
    Review URL: http://codereview.chromium.org/8684
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4130 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/cookie_monster_sqlite.cc
+++ chrome/common/net/cookie_monster_sqlite.cc
@@ -150,7 +150,9 @@ void SQLitePersistentCookieStore::Backend::Commit() {
     return;
 
   SQLITE_UNIQUE_STATEMENT(add_smt, *cache_,
-                          "INSERT INTO cookies VALUES (?,?,?,?,?,?,?,?)");
+      "INSERT INTO cookies (creation_utc, host_key, name, value, path, "
+      "expires_utc, secure, httponly) "
+      "VALUES (?,?,?,?,?,?,?,?)");
   if (!add_smt.is_valid()) {
     NOTREACHED();
     return;
@@ -183,6 +185,7 @@ void SQLitePersistentCookieStore::Backend::Commit() {
           NOTREACHED() << "Could not add a cookie to the DB.";
         }
         break;
+
       case PendingOperation::COOKIE_DELETE:
         del_smt->reset();
         del_smt->bind_int64(0, po->cc().CreationDate().ToInternalValue());
@@ -190,6 +193,7 @@ void SQLitePersistentCookieStore::Backend::Commit() {
           NOTREACHED() << "Could not delete a cookie from the DB.";
         }
         break;
+
       default:
         NOTREACHED();
         break;
@@ -285,7 +289,9 @@ bool SQLitePersistentCookieStore::Load(
 
   // Slurp all the cookies into the out-vector.
   SQLStatement smt;
-  if (smt.prepare(db, "SELECT * FROM cookies") != SQLITE_OK) {
+  if (smt.prepare(db,
+      "SELECT creation_utc, host_key, name, value, path, expires_utc, secure, "
+      "httponly FROM cookies") != SQLITE_OK) {
     NOTREACHED() << "select statement prep failed";
     sqlite3_close(db);
     return false;
