a833661841b39b00a80451ec09df93a1ea9887e5
glen@chromium.org <glen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-29 22:20:40 +0000

    Fix for Save Page downloads not notifying their observers on completion.
    BUG=3417
    
    Review URL: http://codereview.chromium.org/8804
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4177 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_manager.cc
+++ chrome/browser/download/download_manager.cc
@@ -215,6 +215,7 @@ void DownloadItem::Cancel(bool update_history) {
 void DownloadItem::Finished(int64 size) {
   state_ = COMPLETE;
   UpdateSize(size);
+  UpdateObservers();
   StopProgressTimer();
 }
 
