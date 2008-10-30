3ee3a0fbb93553af2cfd739a94ec060184cfac80
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-30 18:12:31 +0000

    2008-10-29  Anders Carlsson  <andersca@apple.com>
            Reviewed by Kevin Decker.
    
            * Plugins/WebBaseNetscapePluginStream.h:
            * Plugins/WebBaseNetscapePluginStream.mm:
            (WebNetscapePluginStream::WebNetscapePluginStream):
            (WebNetscapePluginStream::~WebNetscapePluginStream):
            (WebNetscapePluginStream::start):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38002 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebBaseNetscapePluginStream.h
+++ third_party/WebKit/WebKit/mac/Plugins/WebBaseNetscapePluginStream.h
@@ -120,8 +120,8 @@ private:
     bool m_newStreamSuccessful;
     
     WebCore::FrameLoader* m_frameLoader;
-    WebCore::NetscapePlugInStreamLoader* m_loader;
-    NSURLRequest *m_request;
+    RefPtr<WebCore::NetscapePlugInStreamLoader> m_loader;
+    RetainPtr<NSMutableURLRequest> m_request;
     NPPluginFuncs *m_pluginFuncs;
 
     void deliverDataTimerFired(WebCore::Timer<WebNetscapePluginStream>* timer);
