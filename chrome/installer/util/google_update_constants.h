5f1f7de87cdf5f4caa4113d825f003b138b0f399
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-31 16:15:38 +0000

    Consolidate same constant defined in three different places. No change in chrome.exe size.
    Review URL: http://codereview.chromium.org/8409
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4284 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/google_update_constants.h
+++ chrome/installer/util/google_update_constants.h
@@ -5,25 +5,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Defines all the command-line switches used with Google Update.
 
-#ifndef CHROME_INSTALLER_UTIL_GOOGLE_UPDATE_CONSTANTS_H__
-#define CHROME_INSTALLER_UTIL_GOOGLE_UPDATE_CONSTANTS_H__
+#ifndef CHROME_INSTALLER_UTIL_GOOGLE_UPDATE_CONSTANTS_H_
+#define CHROME_INSTALLER_UTIL_GOOGLE_UPDATE_CONSTANTS_H_
 
 namespace google_update {
 
 extern const wchar_t kChromeGuid[];
-
 // Strictly speaking Google Update doesn't care about this GUID but it is still
 // related to install as it is used by MSI to identify Gears.
 extern const wchar_t kGearsUpgradeCode[];
 
 extern const wchar_t kRegPathClients[];
 extern const wchar_t kRegPathClientState[];
-extern const wchar_t kRegApFieldName[];
-extern const wchar_t kRegNameField[];
-extern const wchar_t kRegVersionField[];
-extern const wchar_t kRegLastCheckedField[];
 
+extern const wchar_t kRegApField[];
+extern const wchar_t kRegBrowserField[];
+extern const wchar_t kRegDidRunField[];
+extern const wchar_t kRegLangField[];
+extern const wchar_t kRegLastCheckedField[];
+extern const wchar_t kRegNameField[];
+extern const wchar_t kRegRLZBrandField[];
+extern const wchar_t kRegUsageStatsField[];
+extern const wchar_t kRegVersionField[];
 }  // namespace google_update
 
-#endif  // CHROME_INSTALLER_UTIL_GOOGLE_UPDATE_CONSTANTS_H__
+#endif  // CHROME_INSTALLER_UTIL_GOOGLE_UPDATE_CONSTANTS_H_
 
