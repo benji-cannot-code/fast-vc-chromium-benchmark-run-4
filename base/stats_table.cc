41e3ad363ccf4f71d4c3b0709acc3093306ec7be
dkegel@google.com <dkegel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-31 19:21:34 +0000

    Port last remaining test case in base/stats_table_unittest.cc, and fix the bug it exposes on posix in StatsTable, i.e. UnregisterThread() breaks when called inside SlotReturnFunction() on posix because posix clears tls data before calling destructor.
    Review URL: http://codereview.chromium.org/8751
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4309 0039d316-1c4b-4281-b951-d872f2087c98

--- base/stats_table.cc
+++ base/stats_table.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/platform_thread.h"
+#include "base/process_util.h"
 #include "base/shared_memory.h"
 #include "base/string_util.h"
 #include "base/thread_local_storage.h"
@@ -91,16 +92,6 @@ struct StatsTableTLSData {
   StatsTable* table;
   int slot;
 };
-
-// The SlotReturnFunction is called at thread exit for each thread
-// which used the StatsTable.
-static void SlotReturnFunction(void* data) {
-  StatsTableTLSData* tls_data = static_cast<StatsTableTLSData*>(data);
-  if (tls_data) {
-    DCHECK(tls_data->table);
-    tls_data->table->UnregisterThread();
-  }
-}
 
 }  // namespace
 
@@ -314,12 +305,7 @@ int StatsTable::RegisterThread(const std::wstring& name) {
     base::wcslcpy(impl_->thread_name(slot), thread_name.c_str(),
                   kMaxThreadNameLength);
     *(impl_->thread_tid(slot)) = PlatformThread::CurrentId();
-    // TODO(pinkerton): these should go into process_utils when it's ported
-#if defined(OS_WIN)
-    *(impl_->thread_pid(slot)) = GetCurrentProcessId();
-#elif defined(OS_POSIX)
-    *(impl_->thread_pid(slot)) = getpid();
-#endif  
+    *(impl_->thread_pid(slot)) = process_util::GetCurrentProcId();
   }
 
   // Set our thread local storage.
@@ -342,7 +328,10 @@ StatsTableTLSData* StatsTable::GetTLSData() const {
 }
 
 void StatsTable::UnregisterThread() {
-  StatsTableTLSData* data = GetTLSData();
+  UnregisterThread(GetTLSData());
+}
+
+void StatsTable::UnregisterThread(StatsTableTLSData* data) {
   if (!data)
     return;
   DCHECK(impl_);
@@ -354,6 +343,14 @@ void StatsTable::UnregisterThread() {
   // Remove the calling thread's TLS so that it cannot use the slot.
   tls_index_.Set(NULL);
   delete data;
+}
+
+void StatsTable::SlotReturnFunction(void* data) {
+  StatsTableTLSData* tls_data = static_cast<StatsTableTLSData*>(data);
+  if (tls_data) {
+    DCHECK(tls_data->table);
+    tls_data->table->UnregisterThread(tls_data);
+  }
 }
 
 int StatsTable::CountThreadsRegistered() const {
