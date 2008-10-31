b36de3cefbc8d9510be198c6e141321657a2ae21
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-10-31 21:59:03 +0000

    A couple wxMSW build fixes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@38053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/wx/WebKitSupport/ChromeClientWx.cpp
+++ third_party/WebKit/WebKit/wx/WebKitSupport/ChromeClientWx.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "ChromeClientWx.h"
+#include "FileChooser.h"
 #include "FloatRect.h"
 #include "FrameLoadRequest.h"
 #include "NotImplemented.h"
