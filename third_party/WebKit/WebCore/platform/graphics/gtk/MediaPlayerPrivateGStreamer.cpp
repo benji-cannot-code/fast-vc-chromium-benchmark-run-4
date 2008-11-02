6617f7c7a83bfc661f3aa2d8b160bed1c4b37b0a
christian@webkit.org <christian@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-02 20:23:14 +0000

    2008-11-02  Xan Lopez  <xan@gnome.org>
            Reviewed by Holger Freyther
    
            https://bugs.webkit.org/show_bug.cgi?id=22009
            HTML5 Video with GStreamer pulls gnome-vfs without using it.
    
            * configure.ac:
            Remove gnome-vfs from configure.ac, it's unused.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38067 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gtk/MediaPlayerPrivateGStreamer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/gtk/MediaPlayerPrivateGStreamer.cpp
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gst/interfaces/mixer.h>
 #include <gst/interfaces/xoverlay.h>
 #include <gst/video/video.h>
-#include <libgnomevfs/gnome-vfs.h>
 #include <limits>
 #include <math.h>
 
