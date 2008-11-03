ae20c39bdc19114439b9238db68c51d997b4d7da
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-03 20:47:20 +0000

    2008-11-03  Alp Toker  <alp@nuanti.com>
            Reviewed by Geoffrey Garen.
    
            Fix warning when building on Darwin without JSC_MULTIPLE_THREADS
            enabled.
    
            * kjs/InitializeThreading.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38077 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/kjs/InitializeThreading.cpp
+++ third_party/WebKit/JavaScriptCore/kjs/InitializeThreading.cpp
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace JSC {
 
-#if PLATFORM(DARWIN)
+#if PLATFORM(DARWIN) && ENABLE(JSC_MULTIPLE_THREADS)
 static pthread_once_t initializeThreadingKeyOnce = PTHREAD_ONCE_INIT;
 #endif
 
