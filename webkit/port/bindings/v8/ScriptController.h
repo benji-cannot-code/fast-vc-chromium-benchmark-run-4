85d23397346590d238107d2557bfe5f76c62bcfb
fqian@google.com <fqian@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-03 18:19:36 +0000

    Rename ScriptController::notifyDOMWindowReady() to windowObjectAvailable().
    Remove unnecessary calls to dispatchWindowObjectReady. FrameLoader calls the function.
    
    Review URL: http://codereview.chromium.org/9247
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4460 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/v8/ScriptController.h
+++ webkit/port/bindings/v8/ScriptController.h
@@ -233,7 +233,7 @@ public:
 
     void clearWindowShell();
     void updateDocument();
-    void notifyDOMWindowReady() { m_proxy->domWindowReady(); }
+    void windowObjectAvailable() { m_proxy->domWindowReady(); }
 
     void pauseTimeouts(OwnPtr<PausedTimeouts>&);
     void resumeTimeouts(OwnPtr<PausedTimeouts>&);
