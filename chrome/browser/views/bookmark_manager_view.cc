16d40596a4188deae1acd6668aa3cc32695f1c11
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-04 04:44:43 +0000

    Fixes two bookmark manager bugs. Use Activate instead of MoveToFront so that we restore the window if minimized/iconified and give focus to the search field.
    BUG=4038 4044
    TEST=see bugs
    
    Review URL: http://codereview.chromium.org/9069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4557 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/bookmark_manager_view.cc
+++ chrome/browser/views/bookmark_manager_view.cc
@@ -120,7 +120,7 @@ void BookmarkManagerView::Show(Profile* profile) {
     return;
 
   if (open_window != NULL) {
-    open_window->MoveToFront(true);
+    open_window->Activate();
     return;
   }
 
@@ -133,6 +133,9 @@ void BookmarkManagerView::Show(Profile* profile) {
   manager->PrepareForShow();
   // And show it.
   open_window->Show();
+
+  // Give initial focus to the search field.
+  manager->search_tf_->RequestFocus();
 }
 
 // static
