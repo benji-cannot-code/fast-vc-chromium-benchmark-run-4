798ba43182029e683bb32584caffc6a9b2fa876b
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-04 08:00:40 +0000

    Set the thread name for the sandbox event thread. Review URL: http://codereview.chromium.org/9263
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4581 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/src/broker_services.cc
+++ sandbox/src/broker_services.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/src/broker_services.h"
 
 #include "base/logging.h"
+#include "base/platform_thread.h"
 #include "sandbox/src/sandbox_policy_base.h"
 #include "sandbox/src/sandbox.h"
 #include "sandbox/src/target_process.h"
@@ -133,6 +134,8 @@ void BrokerServicesBase::FreeResources(JobTracker* tracker) {
 DWORD WINAPI BrokerServicesBase::TargetEventsThread(PVOID param) {
   if (NULL == param)
     return 1;
+
+  PlatformThread::SetName("BrokerEventThread");
 
   BrokerServicesBase* broker = reinterpret_cast<BrokerServicesBase*>(param);
   HANDLE port = broker->job_port_;
