4545ba684d5818d4e5732ffd825c7f49c18c9f6b
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-04 07:33:36 +0000

    2008-11-03  Cameron Zwarich  <zwarich@apple.com>
            Not reviewed.
    
            Fix the wxWindows build.
    
            * bridge/c/c_instance.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38088 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/c/c_instance.cpp
+++ third_party/WebKit/WebCore/bridge/c/c_instance.cpp
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <runtime/ExecState.h>
 #include <runtime/JSLock.h>
 #include <runtime/JSNumberCell.h>
-#include <kjs/PropertyNameArray.h>
+#include <runtime/PropertyNameArray.h>
 #include <wtf/Assertions.h>
 #include <wtf/StringExtras.h>
 #include <wtf/Vector.h>
