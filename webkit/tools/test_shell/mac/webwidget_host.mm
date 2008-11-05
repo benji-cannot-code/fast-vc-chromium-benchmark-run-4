23878687d65ec1a85643e643d3caabedb6f259e5
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-05 18:34:25 +0000

    make sure the host gets notified of invalidates so we only paint what changes Review URL: http://codereview.chromium.org/9419
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4790 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/mac/webwidget_host.mm
+++ webkit/tools/test_shell/mac/webwidget_host.mm
@@ -91,6 +91,8 @@ void WebWidgetHost::DidInvalidateRect(const gfx::Rect& damaged_rect) {
   paint_rect_ = paint_rect_.Union(damaged_rect);
 
   NSRect r = NSRectFromCGRect(damaged_rect.ToCGRect());
+  // flip to cocoa coordinates
+  r.origin.y = [view_ frame].size.height - r.size.height - r.origin.y;
   [view_ setNeedsDisplayInRect:r];
 }
 
@@ -111,6 +113,8 @@ void WebWidgetHost::DidScrollRect(int dx, int dy, const gfx::Rect& clip_rect) {
   scroll_dy_ = dy;
 
   NSRect r = NSRectFromCGRect(clip_rect.ToCGRect());
+  // flip to cocoa coordinates
+  r.origin.y = [view_ frame].size.height - r.size.height - r.origin.y;
   [view_ setNeedsDisplayInRect:r];
 }
 
@@ -176,7 +180,7 @@ void WebWidgetHost::Paint() {
   // first time we call it.  This is necessary because some WebCore rendering
   // objects update their layout only when painted.
   for (int i = 0; i < 2; ++i) {
-    paint_rect_ = client_rect;//.Intersect(paint_rect_);
+    paint_rect_ = client_rect.Intersect(paint_rect_);
     if (!paint_rect_.IsEmpty()) {
       gfx::Rect rect(paint_rect_);
       paint_rect_ = gfx::Rect();
@@ -202,9 +206,6 @@ void WebWidgetHost::Paint() {
     
     [view_ unlockFocus];
   }
-
-  // Draw children
-  // UpdateWindow(hwnd_);
 }
 
 void WebWidgetHost::Resize(const gfx::Rect& rect) {
