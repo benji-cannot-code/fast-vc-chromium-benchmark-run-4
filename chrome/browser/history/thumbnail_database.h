845c1340411c49c9b5cd36f68d63cabe003c0dae
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-05 23:39:02 +0000

    Whitespace typo fix from CL 8722
    Review URL: http://codereview.chromium.org/9611
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4839 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/thumbnail_database.h
+++ chrome/browser/history/thumbnail_database.h
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 struct sqlite3;
 struct ThumbnailScore;
-  namespace base {
-class Time;
+namespace base {
+  class Time;
 }
 
 namespace history {
