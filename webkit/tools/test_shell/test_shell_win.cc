db8eda17514d7da28f993f71ba9198884ad9b7c1
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-05 23:17:24 +0000

    revert 9391
    Review URL: http://codereview.chromium.org/9607
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4837 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_win.cc
+++ webkit/tools/test_shell/test_shell_win.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/webpreferences.h"
 #include "webkit/glue/webview.h"
 #include "webkit/glue/plugins/plugin_list.h"
+#include "webkit/tools/test_shell/simple_resource_loader_bridge.h"
 #include "webkit/tools/test_shell/test_navigation_controller.h"
 
 #define MAX_LOADSTRING 100
@@ -73,6 +74,13 @@ void TestShell::InitializeTestShell(bool interactive) {
   web_prefs_ = new WebPreferences;
 
   ResetWebPreferences();
+}
+
+void TestShell::ShutdownTestShell() {
+  delete window_list_;
+  SimpleResourceLoaderBridge::Shutdown();
+  delete TestShell::web_prefs_;
+  OleUninitialize();
 }
 
 bool TestShell::CreateNewWindow(const std::wstring& startingURL,
