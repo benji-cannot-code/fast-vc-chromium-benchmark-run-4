73ed0314a9dc912f96de1077e6f6c02bbffa781e
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-06 08:53:06 +0000

    2008-11-05  Cameron Zwarich  <zwarich@apple.com>
            Rubber-stamped by Sam Weinig.
    
            Move create_hash_table and jsc.pro out of the kjs directory and into the
            root directory of JavaScriptCore.
    
            JavaScriptCore:
    
            * DerivedSources.make:
            * JavaScriptCore.pri:
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj:
            * JavaScriptCore.xcodeproj/project.pbxproj:
            * create_hash_table: Copied from kjs/create_hash_table.
            * jsc.pro: Copied from kjs/jsc.pro.
            * kjs/create_hash_table: Removed.
            * kjs/jsc.pro: Removed.
            * make-generated-sources.sh:
    
            * WebKit.pro:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38155 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/DerivedSources.make
+++ third_party/WebKit/JavaScriptCore/DerivedSources.make
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 VPATH = \
+    $(JavaScriptCore) \
     $(JavaScriptCore)/kjs \
     $(JavaScriptCore)/VM \
     $(JavaScriptCore)/pcre \
--- third_party/WebKit/JavaScriptCore/make-generated-sources.sh
+++ third_party/WebKit/JavaScriptCore/make-generated-sources.sh
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 export SRCROOT=$PWD
 export WebCore=$PWD
-export CREATE_HASH_TABLE="$SRCROOT/kjs/create_hash_table"
+export CREATE_HASH_TABLE="$SRCROOT/create_hash_table"
 
 mkdir -p DerivedSources/JavaScriptCore
 cd DerivedSources/JavaScriptCore
