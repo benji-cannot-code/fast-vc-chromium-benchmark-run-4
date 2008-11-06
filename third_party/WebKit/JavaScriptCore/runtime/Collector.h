efa53ec63fba52bd4c8094c79ed15bd70966dfde
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-06 17:42:31 +0000

    2008-11-06  Kristian Amlie  <kristian.amlie@nokia.com>
            Reviewed by Simon Hausmann.
    
            Add missing stddef.h header needed for compilation in Symbian.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38171 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/Collector.h
+++ third_party/WebKit/JavaScriptCore/runtime/Collector.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define KJSCOLLECTOR_H_
 
 #include "JSImmediate.h"
+#include <stddef.h>
 #include <string.h>
 #include <wtf/HashCountedSet.h>
 #include <wtf/HashSet.h>
