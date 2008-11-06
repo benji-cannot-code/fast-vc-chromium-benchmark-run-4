6ab2f4fc3d933f599b2477a9858f566a1b997676
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-06 18:38:31 +0000

    2008-11-06  Kristian Amlie  <kristian.amlie@nokia.com>
            Reviewed by Simon Hausmann.
    
            Compile fix for Symbian.
            The snprintf function is defined in stdio.h, the inclusion
            of snprintf.h inside this PLATFORM(SYMBIAN) block does not
            compile.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38180 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/config.h
+++ third_party/WebKit/WebCore/config.h
@@ -128,7 +128,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define U_HAVE_INTTYPES_H 0
 
 #include <stdio.h>
-#include <snprintf.h>
 #include <limits.h>
 #include <wtf/MathExtras.h>
 #endif
