3015d8981fbe2fac08c6268343d46a912a265042
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-06 09:22:54 +0000

    2008-11-06  Cameron Zwarich  <zwarich@apple.com>
            Not reviewed.
    
            Fix the WebCore build for some platforms by changing build scripts to
            account for the recent move of the create_hash_table script from the kjs
            subdirectory of JavaScriptCore to the root directory.
    
            * WebCore.pro:
            * make-generated-sources.sh:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38158 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/make-generated-sources.sh
+++ third_party/WebKit/WebCore/make-generated-sources.sh
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 export SOURCE_ROOT=$PWD
 export SRCROOT=$PWD
 export WebCore=$PWD
-export CREATE_HASH_TABLE="$SRCROOT/../JavaScriptCore/kjs/create_hash_table"
+export CREATE_HASH_TABLE="$SRCROOT/../JavaScriptCore/create_hash_table"
 
 mkdir -p DerivedSources/WebCore &&
 make -C DerivedSources/WebCore -f ../../DerivedSources.make
