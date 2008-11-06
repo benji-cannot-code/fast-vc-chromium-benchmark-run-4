0dfcfbcde6029f9824da5e5dad588f4d2e9c3a51
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-06 18:03:23 +0000

    2008-11-06  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Simon Hausmann.
    
            Include strings.h for strncasecmp().
            This is needed for compilation inside Symbian and it is also
            confirmed by the man-page on Linux.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38176 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/DateMath.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/DateMath.cpp
@@ -68,6 +68,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/timeb.h>
 #endif
 
+#if HAVE(STRINGS_H)
+#include <strings.h>
+#endif
+
 using namespace WTF;
 
 namespace JSC {
