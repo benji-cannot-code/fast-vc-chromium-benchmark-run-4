c1158dca49b1b7f6be7128403e2b1a5dd69c4231
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-07 17:48:38 +0000

    2008-11-07  Alp Toker  <alp@nuanti.com>
            More grammar.cpp -> Grammar.cpp build fixes.
    
            * AllInOneFile.cpp:
            * GNUmakefile.am:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38225 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/AllInOneFile.cpp
+++ third_party/WebKit/JavaScriptCore/AllInOneFile.cpp
@@ -57,7 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "runtime/ErrorConstructor.cpp"
 #include "runtime/FunctionConstructor.cpp"
 #include "runtime/FunctionPrototype.cpp"
-#include "grammar.cpp"
+#include "Grammar.cpp"
 #include "runtime/Identifier.cpp"
 #include "runtime/JSString.cpp"
 #include "runtime/JSNumberCell.cpp"
