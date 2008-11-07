7e952e285b5b6968f0eb01ef9e49510a8b5d3d5a
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-07 21:34:02 +0000

    2008-11-07  Anders Carlsson  <andersca@apple.com>
            Reviewed by Kevin Decker
    
            Explicitly initialize the NPDrawingModelCoreAnimation.
    
            * bridge/npapi.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38231 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/npapi.h
+++ third_party/WebKit/WebCore/bridge/npapi.h
@@ -422,7 +422,7 @@ typedef enum {
 #endif
     NPDrawingModelCoreGraphics = 1,
     NPDrawingModelOpenGL = 2,
-    NPDrawingModelCoreAnimation
+    NPDrawingModelCoreAnimation = 3
 } NPDrawingModel;
 
 /*
