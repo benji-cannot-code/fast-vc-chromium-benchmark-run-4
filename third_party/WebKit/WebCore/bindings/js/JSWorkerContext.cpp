1b1171f49e2d2013aa9d55b2eba44e180c45cc6f
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-07 05:54:49 +0000

    Reviewed by Darin Adler.
            https://bugs.webkit.org/show_bug.cgi?id=22097
            Expose MessageEvent constructor on WorkerContext
    
            * bindings/js/JSWorkerContext.cpp:
            (WebCore::jsWorkerContextMessageEvent):
            (WebCore::setJSWorkerContextMessageEvent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38212 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSWorkerContext.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSWorkerContext.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "JSDOMBinding.h"
 #include "JSMessageChannelConstructor.h"
+#include "JSMessageEvent.h"
 #include "JSMessagePort.h"
 #include "JSWorkerLocation.h"
 #include "WorkerContext.h"
@@ -46,6 +47,8 @@ JSValue* jsWorkerContextLocation(ExecState*, const Identifier&, const PropertySl
 JSValue* jsWorkerContextMessageChannel(ExecState*, const Identifier&, const PropertySlot&);
 void setJSWorkerContextMessageChannel(ExecState*, JSObject*, JSValue*);
 JSValue* jsWorkerContextMessagePort(ExecState*, const Identifier&, const PropertySlot&);
+void setJSWorkerContextMessageEvent(ExecState*, JSObject*, JSValue*);
+JSValue* jsWorkerContextMessageEvent(ExecState*, const Identifier&, const PropertySlot&);
 void setJSWorkerContextMessagePort(ExecState*, JSObject*, JSValue*);
 JSValue* jsWorkerContextWorkerLocation(ExecState*, const Identifier&, const PropertySlot&);
 void setJSWorkerContextWorkerLocation(ExecState*, JSObject*, JSValue*);
@@ -66,6 +69,7 @@ void setJSWorkerContextWorkerLocation(ExecState*, JSObject*, JSValue*);
 #  onconnect                     jsWorkerContextOnconnect           DontDelete
 #  onmessage and other MessagePort attributes?
   MessageChannel                jsWorkerContextMessageChannel                DontDelete
+  MessageEvent                  jsWorkerContextMessageEvent                  DontDelete
   MessagePort                   jsWorkerContextMessagePort                   DontDelete
   WorkerLocation                jsWorkerContextWorkerLocation                DontDelete
 #  XMLHttpRequest                jsWorkerContextXMLHttpRequest                DontDelete
@@ -140,6 +144,11 @@ JSValue* jsWorkerContextMessageChannel(ExecState* exec, const Identifier&, const
     return getDOMConstructor<JSMessageChannelConstructor>(exec, static_cast<JSWorkerContext*>(asObject(slot.slotBase())));
 }
 
+JSValue* jsWorkerContextMessageEvent(ExecState* exec, const Identifier&, const PropertySlot& slot)
+{
+    return JSMessageEvent::getConstructor(exec);
+}
+
 JSValue* jsWorkerContextMessagePort(ExecState* exec, const Identifier&, const PropertySlot&)
 {
     return JSMessagePort::getConstructor(exec);
@@ -170,6 +179,11 @@ bool JSWorkerContextPrototype::getOwnPropertySlot(ExecState* exec, const Identif
 void setJSWorkerContextMessageChannel(ExecState*, JSObject*, JSValue*)
 {
     // FIXME: Do we need to override put for global constructors, like JSDOMWindowBase does?
+    ASSERT_NOT_REACHED();
+}
+
+void setJSWorkerContextMessageEvent(ExecState*, JSObject*, JSValue*)
+{
     ASSERT_NOT_REACHED();
 }
 
