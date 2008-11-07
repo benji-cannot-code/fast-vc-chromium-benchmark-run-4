81ca4e3587f71154033afbbd3fd802f1c9c7542b
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-07 23:46:03 +0000

    * fix system level install bug
    Review URL: http://codereview.chromium.org/10224
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5038 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/setup/main.cc
+++ chrome/installer/setup/main.cc
@@ -534,7 +534,7 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance,
   } else {
     install_status = InstallChrome(parsed_command_line,
                                    installed_version.get(),
-                                   system_install);
+                                   options);
   }
 
   CoUninitialize();
