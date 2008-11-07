a02f19277774eb7105e32942901e6bf73e40d7f1
jar@google.com <jar@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-07 00:49:11 +0000

    Insert histograms to support latency experiments with SDCH
    Gather data to show the duration of time between the first
    byte sent, to last byte read.  Use a second histogram to 
    look at latency when the content-encoding is not really 
    sdch (due to server side experiment, or proxy interference).
    
    r=huanr,wtc
    Review URL: http://codereview.chromium.org/9448
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4941 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/filter.h
+++ net/base/filter.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/scoped_ptr.h"
+#include "base/time.h"
 #include "googleurl/src/gurl.h"
 #include "testing/gtest/include/gtest/gtest_prod.h"
 
@@ -121,6 +122,8 @@ class Filter {
   void SetMimeType(const std::string& mime_type);
   const std::string& mime_type() const { return mime_type_; }
 
+  void SetConnectTime(const base::Time& time);
+
   // Translate the text of a filter name (from Content-Encoding header) into a
   // FilterType.
   static FilterType ConvertEncodingToType(const std::string& filter_type);
@@ -169,6 +172,8 @@ class Filter {
 
   FilterStatus last_status() const { return last_status_; }
 
+  base::Time connect_time() const { return connect_time_; }
+
   // Buffer to hold the data to be filtered.
   scoped_array<char> stream_buffer_;
 
@@ -185,6 +190,10 @@ class Filter {
   // This is used by SDCH filters which need to restrict use of a dictionary to
   // a specific URL or path.
   GURL url_;
+
+  // To facilitate histogramming by individual filters, we store the connect
+  // time for the corresponding HTTP transaction.
+  base::Time connect_time_;
 
   // To facilitate error recovery in SDCH filters, allow filter to know if
   // content is text/html by checking within this mime type (SDCH filter may
