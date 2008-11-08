891f717d668e28cca9f3f0a6ad0a5cdcd2271bd3
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-08 00:11:31 +0000

    Roll this change back until I understand the crashes.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5046 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/frame_util.cc
+++ chrome/browser/frame_util.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/frame_util.h"
 
-#include "base/base_switches.h"
-#include "base/command_line.h"
 #include "base/message_loop.h"
 #include "base/win_util.h"
 #include "chrome/app/result_codes.h"
@@ -182,3 +180,4 @@ void FrameUtil::EndSession() {
   // down. If any messages are processed we'll likely crash. Exit now.
   ExitProcess(ResultCodes::NORMAL_EXIT);
 }
+
