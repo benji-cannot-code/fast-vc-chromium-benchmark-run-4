a20e0892afe28ea908738c5372108178a20b1854
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-09 15:50:32 +0000

    2008-11-09  Darin Adler  <darin@apple.com>
            Reviewed by Sam Weinig.
    
            - fix https://bugs.webkit.org/show_bug.cgi?id=22142
              a few fast/canvas tests use non-standard HTML wrappers but don't need to
    
            * fast/canvas/gradient-addColorStop-with-invalid-color-expected.txt:
            Fixed spelling error: "gradieints".
    
            * fast/canvas/gradient-addColorStop-with-invalid-color.html: Regenerated with
            make-js-test-wrappers script; removes the canvas element that was in here.
            * fast/canvas/linearGradient-infinite-values.html: Ditto.
            * fast/canvas/radialGradient-infinite-values.html: Ditto.
    
            * fast/canvas/resources/gradient-addColorStop-with-invalid-color.js:
            Fixed spelling error and changed code to use createElement to make a canvas
            element rather than using getElementById to find an existing canvas element.
            * fast/canvas/resources/linearGradient-infinite-values.js: Ditto.
            * fast/canvas/resources/radialGradient-infinite-values.js: Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/canvas/resources/gradient-addColorStop-with-invalid-color.js
+++ third_party/WebKit/LayoutTests/fast/canvas/resources/gradient-addColorStop-with-invalid-color.js
@@ -1,10 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description(
-'This test checks invalid colors on gradieints'
+'This test checks invalid colors on gradients.'
 );
 
-var ctx = document.getElementById('canvas').getContext('2d');
-var gradient = ctx.createLinearGradient(0, 0, 150, 0);
+var gradient = document.createElement('canvas').getContext('2d').createLinearGradient(0, 0, 150, 0);
 
 shouldThrow("gradient.addColorStop(0, '')", "'Error: SYNTAX_ERR: DOM Exception 12'");
 shouldThrow("gradient.addColorStop(0, '#cc')", "'Error: SYNTAX_ERR: DOM Exception 12'");
@@ -12,5 +11,3 @@ shouldThrow("gradient.addColorStop(0, 'rgb(257, 0)')", "'Error: SYNTAX_ERR: DOM 
 shouldThrow("gradient.addColorStop(0, 'rgb(257, 0, 5, 0)')", "'Error: SYNTAX_ERR: DOM Exception 12'");
 
 var successfullyParsed = true;
-
-
