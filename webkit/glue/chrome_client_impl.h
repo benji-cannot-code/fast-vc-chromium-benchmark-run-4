3bef9c3fecd1ad5f1038b01dde6a387ae98f5bca
erikkay@google.com <erikkay@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-10 19:34:43 +0000

    Change old code from "unloadListnerChanged" model to new WebKit EnableSuddenTermination, unforking a bunch of code.
    BUG=http://crbug.com/4064
    
    
    Review URL: http://codereview.chromium.org/9746
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5116 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/chrome_client_impl.h
+++ webkit/glue/chrome_client_impl.h
@@ -121,6 +121,9 @@ public:
 
   void SetCursor(const WebCursor& cursor);
 
+  virtual void enableSuddenTermination();
+  virtual void disableSuddenTermination();
+
 private:
   WebViewImpl* webview_;  // weak pointer
   bool toolbars_visible_;
--- webkit/glue/webview_delegate.h
+++ webkit/glue/webview_delegate.h
@@ -455,8 +455,10 @@ class WebViewDelegate : virtual public WebWidgetDelegate {
                                        const AutofillForm& form) {
   }
 
-  //
-  virtual void OnUnloadListenerChanged(WebView* webview, WebFrame* webframe) {
+  virtual void EnableSuddenTermination() {
+  }
+
+  virtual void DisableSuddenTermination() {
   }
 
   // Queries the browser for suggestions to be shown for the form text field
