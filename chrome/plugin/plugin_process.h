227cc4aa8d8d675c806ae3471aa8755449dc2354
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-11 14:22:48 +0000

    NO CODE CHANGE. Just force a build.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5171 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/plugin/plugin_process.h
+++ chrome/plugin/plugin_process.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_PLUGIN_PLUGIN_PROCESS_H__
-#define CHROME_PLUGIN_PLUGIN_PROCESS_H__
+#ifndef CHROME_PLUGIN_PLUGIN_PROCESS_H_
+#define CHROME_PLUGIN_PLUGIN_PROCESS_H_
 
 #include "chrome/common/child_process.h"
 #include "chrome/plugin/plugin_thread.h"
@@ -49,5 +49,4 @@ class PluginProcess : public ChildProcess {
   DISALLOW_EVIL_CONSTRUCTORS(PluginProcess);
 };
 
-#endif  // CHROME_PLUGIN_PLUGIN_PROCESS_H__
-
+#endif  // CHROME_PLUGIN_PLUGIN_PROCESS_H_
