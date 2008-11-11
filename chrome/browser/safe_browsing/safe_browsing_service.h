84345902458e2d6bedffe6c76a11d3e4c2b77a37
paulg@google.com <paulg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-11 21:49:56 +0000

    Handle the possibility of failure when starting a SafeBrowsing update transaction.
    If we fail to start the transaction, we report the error
    to the protocol manager which aborts the update process.
    Review URL: http://codereview.chromium.org/9778
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5207 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_service.h
+++ chrome/browser/safe_browsing/safe_browsing_service.h
@@ -137,8 +137,7 @@ class SafeBrowsingService
   void HandleChunkDelete(std::vector<SBChunkDelete>* chunk_deletes);
   void GetAllChunks();
 
-  // Methods called to indicate the beginning and end of a complete update.
-  void UpdateStarted();
+  // Called when a complete update cycle has finished.
   void UpdateFinished(bool update_succeeded);
 
   // The blocking page on the UI thread has completed.
@@ -210,7 +209,6 @@ class SafeBrowsingService
 
   void NotifyClientBlockingComplete(Client* client, bool proceed);
 
-  void DatabaseUpdateStarted();
   void DatabaseUpdateFinished(bool update_succeeded);
 
   void Start();
