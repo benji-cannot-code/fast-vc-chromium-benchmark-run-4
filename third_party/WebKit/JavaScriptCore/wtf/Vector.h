d3a4e27e77a0971a84f9f7e2b2b9b14f7ee555ba
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-11 03:51:24 +0000

    2008-11-10  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Antti Koivisto.
            
            - Make Vector::clear() release the Vector's memory (1MB savings on membuster)
            https://bugs.webkit.org/show_bug.cgi?id=22170
    
            * wtf/Vector.h:
            (WTF::VectorBufferBase::deallocateBuffer): Set capacity to 0 as
            well as size, otherwise shrinking capacity to 0 can fail to reset
            the capacity and thus cause a future crash.
            (WTF::Vector::~Vector): Shrink size not capacity; we only need
            to call destructors, the buffer will be freed anyway.
            (WTF::Vector::clear): Change this to shrinkCapacity(0), not just shrink(0).
            (WTF::::shrinkCapacity): Use shrink() instead of resize() for case where
            the size is greater than the new capacity, to work with types that have no
            default constructor.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Vector.h
+++ third_party/WebKit/JavaScriptCore/wtf/Vector.h
@@ -280,8 +280,10 @@ namespace WTF {
 
         void deallocateBuffer(T* bufferToDeallocate)
         {
-            if (m_buffer == bufferToDeallocate)
+            if (m_buffer == bufferToDeallocate) {
                 m_buffer = 0;
+                m_capacity = 0;
+            }
             fastFree(bufferToDeallocate);
         }
 
@@ -443,7 +445,7 @@ namespace WTF {
 
         ~Vector()
         {
-            clear();
+            if (m_size) shrink(0);
         }
 
         Vector(const Vector&);
@@ -494,7 +496,7 @@ namespace WTF {
         void reserveCapacity(size_t newCapacity);
         void shrinkCapacity(size_t newCapacity);
 
-        void clear() { if (m_size) shrink(0); }
+        void clear() { shrinkCapacity(0); }
 
         template<typename U> void append(const U*, size_t);
         template<typename U> void append(const U&);
@@ -726,7 +728,8 @@ namespace WTF {
         if (newCapacity >= capacity())
             return;
 
-        resize(min(m_size, newCapacity));
+        if (newCapacity < size()) 
+            shrink(newCapacity);
 
         T* oldBuffer = begin();
         if (newCapacity > 0) {
