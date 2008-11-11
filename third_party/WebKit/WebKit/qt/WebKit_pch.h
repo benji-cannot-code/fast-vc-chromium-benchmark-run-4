ae13c81072609c287e523a5315e6516625363210
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-11 23:39:15 +0000

    2008-11-11  Cameron Zwarich  <zwarich@apple.com>
            Reviewed by Geoff Garen.
    
            Remove pointless dependencies on the now-deleted kjs directory and
            mentions of it in comments that are no longer relevant.
    
            JavaScriptCore:
    
            * ForwardingHeaders/JavaScriptCore/JSLock.h: Removed.
    
            WebCore:
    
            * WebCore.pro:
            * webcore-base.bkl:
    
            WebKit/qt:
    
            * WebKit_pch.h:
    
            WebKit/wx:
    
            * presets/wxwebkit.bkl:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebKit_pch.h
+++ third_party/WebKit/WebKit/qt/WebKit_pch.h
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #endif
 
-#include "../../JavaScriptCore/kjs/config.h"
+#include "../../JavaScriptCore/config.h"
 
 #include <math.h>
 #include <stdio.h>
@@ -80,5 +80,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/GetPtr.h>
 
 #include "../../WebCore/bindings/js/JSDOMBinding.h"
-#include "../../JavaScriptCore/kjs/MathObject.h"
+#include "../../JavaScriptCore/runtime/MathObject.h"
 #endif
