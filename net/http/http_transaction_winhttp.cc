ef2b2f40875cecd310fd4924022793841713d73f
wtc@google.com <wtc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-12 01:18:55 +0000

    Notify the WinHttpRequestThrottle of the completion of a request when we have read the entire response, rather than waiting until the destructor.  This is what we do in the new HTTP stack (see HttpNetworkTransaction::DoReadBodyComplete). This allows us to unblock the queued requests in the WinHttpRequestThrottle sooner.
    After this change, the name of the request_submitted_
    member is no longer accurate.  Since we'll replace the
    WinHTTP code soon, I just update the comment rather than
    renaming the member.
    
    R=darin
    BUG=4302
    Review URL: http://codereview.chromium.org/10613
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5245 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_transaction_winhttp.cc
+++ net/http/http_transaction_winhttp.cc
@@ -728,7 +728,7 @@ void HttpTransactionWinHttp::StatusCallback(HINTERNET handle,
     // Successfully found the IP address of the server.
     case WINHTTP_CALLBACK_STATUS_NAME_RESOLVED:
       DidFinishDnsResolutionWithStatus(true,
-                                       GURL(),  // null referer URL.
+                                       GURL(),  // null referrer URL.
                                        reinterpret_cast<void*>(context));
       break;
   }
@@ -1434,6 +1434,14 @@ int HttpTransactionWinHttp::DidReadData(DWORD num_bytes) {
     // tolerate this situation, and therefore so must we.
     if (content_length_remaining_ < 0)
       content_length_remaining_ = 0;
+  }
+
+  // We have read the entire response.  Mark the request done to unblock a
+  // queued request.
+  if (rv == 0 || content_length_remaining_ == 0) {
+    DCHECK(request_submitted_);
+    request_submitted_ = false;
+    session_->request_throttle()->NotifyRequestDone(connect_peer_);
   }
 
   return rv;
