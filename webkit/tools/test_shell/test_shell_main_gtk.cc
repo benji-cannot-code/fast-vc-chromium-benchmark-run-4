80d5ce516c799ea919a511fc4dcb79ceded7f48f
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-12 19:23:27 +0000

    Pass through V8 flags from test shell.
    Review URL: http://codereview.chromium.org/10834
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5273 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_main_gtk.cc
+++ webkit/tools/test_shell/test_shell_main_gtk.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/string_util.h"
 #include "base/message_loop.h"
+#include "webkit/glue/webkit_glue.h"
 #include "webkit/tools/test_shell/test_shell.h"
 #include "webkit/tools/test_shell/test_shell_switches.h"
 
@@ -57,6 +58,12 @@ int main(int argc, char* argv[]) {
         parsed_command_line.GetLooseValuesBegin();
     uri = *iter;
   }
+
+  std::wstring js_flags =
+    parsed_command_line.GetSwitchValue(test_shell::kJavaScriptFlags);
+  // Test shell always exposes the GC.
+  CommandLine::AppendSwitch(&js_flags, L"expose-gc");
+  webkit_glue::SetJavaScriptFlags(js_flags);
 
   MessageLoopForUI main_message_loop;
 
