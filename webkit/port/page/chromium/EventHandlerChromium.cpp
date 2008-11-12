3476cd4f0d761e5e54da121819aa207d8ba89f27
dglazkov@google.com <dglazkov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-12 18:13:58 +0000

    Landing 38097:38305 merge (src/webkit, DEPS)
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5265 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/page/chromium/EventHandlerChromium.cpp
+++ webkit/port/page/chromium/EventHandlerChromium.cpp
@@ -46,8 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::AltKey;
-
 const double EventHandler::TextDragDelay = 0.0;
 
 bool EventHandler::passMousePressEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
@@ -144,6 +142,11 @@ void EventHandler::focusDocumentView()
 bool EventHandler::passWidgetMouseDownEventToWidget(RenderWidget* renderWidget)
 {
     return passMouseDownEventToWidget(renderWidget->widget());
+}
+
+unsigned EventHandler::accessKeyModifiers()
+{
+    return PlatformKeyboardEvent::AltKey;
 }
 
 }
