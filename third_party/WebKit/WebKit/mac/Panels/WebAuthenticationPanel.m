fa776a574706c54ef17aa88ecc5579eab2641d99
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-13 07:17:17 +0000

    2008-11-12  Stephanie Lewis  <slewis@apple.com>
            Fix Mac build.        
    
            * Panels/WebAuthenticationPanel.m:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38363 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Panels/WebAuthenticationPanel.m
+++ third_party/WebKit/WebKit/mac/Panels/WebAuthenticationPanel.m
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/NSURLAuthenticationChallenge.h>
 #import <Foundation/NSURLProtectionSpace.h>
 #import <Foundation/NSURLCredential.h>
+#import <WebKit/WebKitNSStringExtras.h>
 #import <WebKit/WebLocalizableStrings.h>
 #import <WebKit/WebNSURLExtras.h>
 #import <wtf/Assertions.h>
