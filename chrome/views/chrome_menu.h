11b2694e1c0e0bba41a3492765e2c956bf6a95fa
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-13 23:09:49 +0000

    Three menu bugs: . If a context menu was shown from a menu, then hidden we did not keep   mouse capture and bad things happened (menu wouldn't go away when    clicking else where, crash ...). . We only update menu sizes on first show of a menu. That way if a   context menu doesn't have icons things don't shift around. . If nothing was selected in the menu pressing the context menu   showed the context menu for the root. It doesn't make sense for the   root menu to have a context menu.
    BUG=4364
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/10706
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5397 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/chrome_menu.h
+++ chrome/views/chrome_menu.h
@@ -636,7 +636,7 @@ class MenuController : public MessageLoopForUI::Dispatcher {
                     bool update_immediately);
 
   // Cancels the current Run. If all is true, any nested loops are canceled
-  // as well. This immediatley hides all menus.
+  // as well. This immediately hides all menus.
   void Cancel(bool all);
 
   // An alternative to Cancel(true) that can be used with a OneShotTimer.
@@ -950,4 +950,3 @@ class MenuController : public MessageLoopForUI::Dispatcher {
 }  // namespace views
 
 #endif  // CHROME_VIEWS_CHROME_MENU_H__
-
