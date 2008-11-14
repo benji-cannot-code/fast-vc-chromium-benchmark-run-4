0995a4596f1aaf5b860563932505b4b6af0e9333
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-14 22:17:25 +0000

    Review URL: http://codereview.chromium.org/10958
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5509 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/container_win.h
+++ chrome/views/container_win.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <atlcrack.h>
 
 #include "base/message_loop.h"
-#include "base/system_monitor.h"
 #include "chrome/views/focus_manager.h"
 #include "chrome/views/layout_manager.h"
 #include "chrome/views/container.h"
@@ -418,9 +417,6 @@ class ContainerWin : public Container,
   virtual LRESULT OnNotify(int w_param, NMHDR* l_param);
   virtual void OnPaint(HDC dc);
   virtual LRESULT OnPowerBroadcast(DWORD power_event, DWORD data) {
-    base::SystemMonitor* monitor = base::SystemMonitor::Get();
-    if (monitor)
-      monitor->ProcessWmPowerBroadcastMessage(power_event);
     SetMsgHandled(FALSE);
     return 0;
   }
