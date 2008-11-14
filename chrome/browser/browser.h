38722811445d12a1a450159aa678601d3aecdc5b
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-14 19:24:31 +0000

    Rid most of the Browser object of HWND dependencies.
    Review URL: http://codereview.chromium.org/10929
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.h
+++ chrome/browser/browser.h
@@ -96,11 +96,6 @@ class Browser : public TabStripModelDelegate,
   bool SupportsCommand(int id) const;
   bool IsCommandEnabled(int id) const;
 
-  // DEPRECATED DEPRECATED DEPRECATED /////////////////////////////////////////
-
-  // Returns the HWND of the top-level system window for this Browser.
-  HWND GetTopLevelHWND() const;
-
   // State Storage and Retrieval for UI ///////////////////////////////////////
 
   // Save and restore the window position.
@@ -297,7 +292,7 @@ class Browser : public TabStripModelDelegate,
   // Overridden from TabStripModelDelegate:
   virtual GURL GetBlankTabURL() const;
   virtual void CreateNewStripWithContents(TabContents* detached_contents,
-                                          const gfx::Point& drop_point);
+                                          const gfx::Rect& window_bounds);
   virtual int GetDragActions() const;
   // Construct a TabContents for a given URL, profile and transition type.
   // If instance is not null, its process will be used to render the tab.
@@ -361,7 +356,7 @@ class Browser : public TabStripModelDelegate,
                                  bool proceed, 
                                  bool* proceed_to_fire_unload);
   virtual void ShowHtmlDialog(HtmlDialogContentsDelegate* delegate,
-                              HWND parent_hwnd);
+                              void* parent_window);
 
   // Overridden from SelectFileDialog::Listener:
   virtual void FileSelected(const std::wstring& path, void* params);
