76de59b6badce197293a1a99fd3fb825ee94defb
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-14 19:07:32 +0000

    Change to integrate SystemMonitor changes into chrome. This enables the dynamic detection of battery status changes so that we can turn on/off the hi-res timer.
    Review URL: http://codereview.chromium.org/10264
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5489 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/testing_browser_process.h
+++ chrome/test/testing_browser_process.h
@@ -119,8 +119,6 @@ class TestingBrowserProcess : public BrowserProcess {
 
   virtual MemoryModel memory_model() { return HIGH_MEMORY_MODEL; }
 
-  virtual SuspendController* suspend_controller() { return NULL; }
-
   virtual HANDLE shutdown_event() { return shutdown_event_; }
 
  private:
