d380f1d1e068efc779313a48c2477f61a8fb9104
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-14 18:53:39 +0000

    2008-11-14  Darin Fisher  <darin@chromium.org>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=22251
            HTMLCanvasElement.cpp unnecessarily includes runtime/Interpreter.h
    
            * html/HTMLCanvasElement.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38399 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLCanvasElement.cpp
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Page.h"
 #include "RenderHTMLCanvas.h"
 #include "Settings.h"
-#include <runtime/Interpreter.h>
 #include <math.h>
 #include <stdio.h>
 
