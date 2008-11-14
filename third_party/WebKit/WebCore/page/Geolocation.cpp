848ddfbf15f7ce76a32af534470157328c50d758
bolsinga@apple.com <bolsinga@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-14 23:48:26 +0000

    Reviewed by Darin Adler.
            https://bugs.webkit.org/show_bug.cgi?id=22264
            Need to update to latest Geolocation spec (13 November 2008)
            
            Update to new error code constants and values.
    
            * page/Geolocation.cpp: Use new named ErrorCode enums
            (WebCore::Geolocation::GeoNotifier::timerFired):
            (WebCore::Geolocation::getCurrentPosition):
            (WebCore::Geolocation::watchPosition):
            * page/PositionError.h: Have ErrorCode enum names match new error constant names and values
            (WebCore::PositionError::):
            * page/PositionError.idl: Add new named error constants
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38409 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Geolocation.cpp
+++ third_party/WebKit/WebCore/page/Geolocation.cpp
@@ -48,7 +48,7 @@ void Geolocation::GeoNotifier::timerFired(Timer<GeoNotifier>*)
     
     m_timer.stop();
 
-    RefPtr<PositionError> error = PositionError::create(PositionError::TIMEOUT_ERROR, "Timed out");
+    RefPtr<PositionError> error = PositionError::create(PositionError::TIMEOUT, "Timed out");
     m_errorCallback->handleEvent(error.get());
 }
 
@@ -74,7 +74,7 @@ void Geolocation::getCurrentPosition(PassRefPtr<PositionCallback> successCallbac
 
     if (!m_service->startUpdating(options)) {
         if (notifier->m_errorCallback) {
-            RefPtr<PositionError> error = PositionError::create(PositionError::PERMISSION_ERROR, "Unable to Start");
+            RefPtr<PositionError> error = PositionError::create(PositionError::PERMISSION_DENIED, "Unable to Start");
             notifier->m_errorCallback->handleEvent(error.get());
         }
         return;
@@ -89,7 +89,7 @@ int Geolocation::watchPosition(PassRefPtr<PositionCallback> successCallback, Pas
 
     if (!m_service->startUpdating(options)) {
         if (notifier->m_errorCallback) {
-            RefPtr<PositionError> error = PositionError::create(PositionError::PERMISSION_ERROR, "Unable to Start");
+            RefPtr<PositionError> error = PositionError::create(PositionError::PERMISSION_DENIED, "Unable to Start");
             notifier->m_errorCallback->handleEvent(error.get());
         }
         return 0;
