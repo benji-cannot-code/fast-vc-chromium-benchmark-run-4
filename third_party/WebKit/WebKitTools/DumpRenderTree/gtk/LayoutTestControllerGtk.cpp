11cc062babee83d4801d2298c47c89e8bd72db3a
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-14 22:58:44 +0000

    2008-11-14  Alp Toker  <alp@nuanti.com>
            GTK DRT build fix for gcc 4.4 snapshot. Add missing stdio include.
    
            Fixes Debian bug #505723
            http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=505723
    
            * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38406 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/gtk/LayoutTestControllerGtk.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/gtk/LayoutTestControllerGtk.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <JavaScriptCore/JSRetainPtr.h>
 #include <JavaScriptCore/JSStringRef.h>
 
+#include <stdio.h>
 #include <glib.h>
 #include <webkit/webkit.h>
 
