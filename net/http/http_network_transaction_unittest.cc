a5b381f9d0c9b57da893c4a593d5ff25db1231e6
wtc@google.com <wtc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-14 00:00:22 +0000

    Add more info to the comment about www.yahoo.com's padding of the chunk-size field.
    Fix miscellaneous nits reported by cpplint.py.
    
    Use ASCIItoWide instead of UTF8ToWide to convert the username
    and password specified in a URL.  Those two components of the
    URL have to be ASCII.  Carry over a TODO comment about unescaping
    username/password from http_transaction_winhttp.cc.
    
    R=eroman
    Review URL: http://codereview.chromium.org/10864
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5410 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction_unittest.cc
+++ net/http/http_network_transaction_unittest.cc
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <math.h> // ceil
+#include <math.h>  // ceil
+
 #include "base/compiler_specific.h"
 #include "base/platform_test.h"
 #include "net/base/client_socket_factory.h"
@@ -900,14 +901,14 @@ TEST_F(HttpNetworkTransactionTest, DontRecycleTCPSocketForSSLTunnel) {
               "Host: www.google.com\r\n\r\n"),
   };
 
-  // The proxy responds to the connect with a 404, using a persistent 
+  // The proxy responds to the connect with a 404, using a persistent
   // connection. Usually a proxy would return 501 (not implemented),
   // or 200 (tunnel established).
   MockRead data_reads1[] = {
     MockRead("HTTP/1.1 404 Not Found\r\n"),
     MockRead("Content-Length: 10\r\n\r\n"),
     MockRead("0123456789"),
-    MockRead(false, net::ERR_UNEXPECTED), // Should not be reached.
+    MockRead(false, net::ERR_UNEXPECTED),  // Should not be reached.
   };
 
   MockSocket data1;
@@ -931,7 +932,7 @@ TEST_F(HttpNetworkTransactionTest, DontRecycleTCPSocketForSSLTunnel) {
   EXPECT_EQ(404, response->headers->response_code());
   EXPECT_EQ(10, response->headers->GetContentLength());
   EXPECT_TRUE(net::HttpVersion(1, 1) == response->headers->GetHttpVersion());
-  
+
   std::string response_data;
   rv = ReadTransaction(trans.get(), &response_data);
   EXPECT_STREQ("0123456789", response_data.c_str());
