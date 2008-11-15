c075cc0ae9ac03f5562880b8150394b568c156ef
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-15 20:48:41 +0000

    2008-11-15  Dirk Schulze  <vbs85@gmx.de>
            Reviewed by Darin Adler.
    
            Removed platform dependent code on SVGPaintServerSolid.
    
            * GNUmakefile.am:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * svg/graphics/SVGPaintServerSolid.cpp:
            (WebCore::SVGPaintServerSolid::setup):
            (WebCore::SVGPaintServerSolid::renderPath):
            * svg/graphics/SVGPaintServerSolid.h:
            * svg/graphics/cairo/SVGPaintServerSolidCairo.cpp: Removed.
            * svg/graphics/cg/SVGPaintServerSolidCg.cpp: Removed.
            * svg/graphics/qt/SVGPaintServerSolidQt.cpp: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/graphics/SVGPaintServerSolid.h
+++ third_party/WebKit/WebCore/svg/graphics/SVGPaintServerSolid.h
@@ -47,6 +47,7 @@ namespace WebCore {
         virtual TextStream& externalRepresentation(TextStream&) const;
 
         virtual bool setup(GraphicsContext*&, const RenderObject*, SVGPaintTargetType, bool isPaintingText) const;
+        virtual void renderPath(GraphicsContext*&, const RenderObject*, SVGPaintTargetType) const;
 
     private:
         SVGPaintServerSolid();
