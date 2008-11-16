0e1a1cd053ead2696e6306515c75ed99000f96d9
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-16 19:43:00 +0000

    - Stop rlz from constantly "rebuilding". - Changes dependencies from rlz   browser   interactive_ui_tests   unit_tests
    to
    
    rlz
      interactive_ui_tests
      mini_installer
      unit_tests
    Review URL: http://codereview.chromium.org/10760
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5548 0039d316-1c4b-4281-b951-d872f2087c98

--- rlz/copy_files.bat
+++ rlz/copy_files.bat
@@ -7,8 +7,4 @@ setlocal
 set InputPath=%~1
 set OutDir=%~2
 
-if NOT "%CHROMIUM_BUILD%" == "_google_chrome" goto END
-
-xcopy /R /C /Y %InputPath% %OutDir%
-
-:END
+if exist "%InputPath%" xcopy /R /C /Y "%InputPath%" "%OutDir%"
