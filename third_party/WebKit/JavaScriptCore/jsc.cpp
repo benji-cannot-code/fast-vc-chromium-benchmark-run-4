365e2f26bee5b6bb3dc047a7841f6076fc51af33
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-16 06:04:12 +0000

    2008-11-15  Sam Weinig  <sam@webkit.org>
            Reviewed by Cameron Zwarich.
    
            Cleanup jsc command line code a little.
    
            * jsc.cpp:
            (functionQuit):
            (main): Use standard exit status macros
            (cleanupGlobalData): Factor out cleanup code into this function.
            (printUsageStatement): Use standard exit status macros.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/jsc.cpp
+++ third_party/WebKit/JavaScriptCore/jsc.cpp
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
-#include "Completion.h"
 #include "CodeGenerator.h"
+#include "Completion.h"
 #include "InitializeThreading.h"
 #include "Interpreter.h"
 #include "JSArray.h"
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SamplingTool.h"
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #if !PLATFORM(WIN_OS)
@@ -65,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using namespace JSC;
 using namespace WTF;
 
+static void cleanupGlobalData(JSGlobalData*);
 static bool fillBufferWithContentsOfFile(const UString& fileName, Vector<char>& buffer);
 
 static JSValue* functionPrint(ExecState*, JSObject*, JSValue*, const ArgList&);
@@ -254,14 +256,8 @@ JSValue* functionReadline(ExecState* exec, JSObject*, JSValue*, const ArgList&)
 
 JSValue* functionQuit(ExecState* exec, JSObject*, JSValue*, const ArgList&)
 {
-    {
-        JSLock lock(false);
-        JSGlobalData& globalData = exec->globalData();
-        globalData.heap.destroy();
-        globalData.deref();
-    }
-
-    exit(0);
+    cleanupGlobalData(&exec->globalData());
+    exit(EXIT_SUCCESS);
 #if !COMPILER(MSVC)
     // MSVC knows that exit(0) never returns, so it flags this return statement as unreachable.
     return jsUndefined();
@@ -306,12 +302,15 @@ int main(int argc, char** argv)
         res = jscmain(argc, argv, globalData);
     EXCEPT(res = 3)
 
-    JSLock::lock(false);
-    globalData->heap.destroy();
-    JSLock::unlock(false);
-
-    globalData->deref();
+    cleanupGlobalData(globalData);
     return res;
+}
+
+static void cleanupGlobalData(JSGlobalData* globalData)
+{
+    JSLock lock(false);
+    globalData->heap.destroy();
+    globalData->deref();
 }
 
 static bool runWithScripts(GlobalObject* globalObject, const Vector<UString>& fileNames, bool dump)
@@ -401,7 +400,7 @@ static void printUsageStatement()
     fprintf(stderr, "  -h|--help  Prints this help message\n");
     fprintf(stderr, "  -i         Enables interactive mode (default if no files are specified)\n");
     fprintf(stderr, "  -s         Installs signal handlers that exit on a crash (Unix platforms only)\n");
-    exit(-1);
+    exit(EXIT_FAILURE);
 }
 
 static void parseArguments(int argc, char** argv, Options& options)
