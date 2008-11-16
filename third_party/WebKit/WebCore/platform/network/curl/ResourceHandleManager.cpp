ed042f75e9b581ae0abd22906e302ffd5c0576ef
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-16 21:53:28 +0000

    Do not spit Curl errors into the stdout as this is used by DRT
    We have failing tests results due the curl errors printed to stdout,
    move them to stderr to be able to pass those tests.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38455 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/curl/ResourceHandleManager.cpp
+++ third_party/WebKit/WebCore/platform/network/curl/ResourceHandleManager.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TextEncoding.h"
 
 #include <errno.h>
+#include <stdio.h>
 #include <wtf/Vector.h>
 
 #if PLATFORM(GTK)
@@ -586,7 +587,7 @@ void ResourceHandleManager::startJob(ResourceHandle* job)
     // timeout will occur and do curl_multi_perform
     if (ret && ret != CURLM_CALL_MULTI_PERFORM) {
 #ifndef NDEBUG
-        printf("Error %d starting job %s\n", ret, encodeWithURLEscapeSequences(job->request().url().string()).latin1().data());
+        fprintf(stderr, "Error %d starting job %s\n", ret, encodeWithURLEscapeSequences(job->request().url().string()).latin1().data());
 #endif
         job->cancel();
         return;
