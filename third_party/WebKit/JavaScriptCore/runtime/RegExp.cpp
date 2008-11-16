947020b656f311a74c9af1f948d535b4e40d005a
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-16 03:58:43 +0000

    2008-11-15  Darin Adler  <darin@apple.com>
            - fix non-WREC build
    
            * runtime/RegExp.cpp: Put "using namespace WREC" inside #if ENABLE(WREC).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/RegExp.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/RegExp.cpp
@@ -32,7 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Assertions.h>
 #include <wtf/OwnArrayPtr.h>
 
+#if ENABLE(WREC)
 using namespace WREC;
+#endif
 
 namespace JSC {
 
