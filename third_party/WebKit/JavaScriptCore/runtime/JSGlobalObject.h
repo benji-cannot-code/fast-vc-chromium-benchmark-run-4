d14e7aad1c408434456c4850576d54d01ff4ab4b
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-16 10:05:23 +0000

    Reviewed by Dan Bernstein.
            https://bugs.webkit.org/show_bug.cgi?id=22290
            Remove cross-heap GC and MessagePort multi-threading support
    
            It is broken (and may not be implementable at all), and no longer needed, as we
            don't use MessagePorts for communication with workers any more.
    
    JavaScriptCore:
            * JavaScriptCore.exp:
            * runtime/Collector.cpp:
            (JSC::Heap::collect):
            * runtime/JSGlobalObject.cpp:
            * runtime/JSGlobalObject.h:
            Remove hooks for cross-heap GC.
    
    WebCore:
            * bindings/js/JSDOMBinding.cpp:
            * bindings/js/JSDOMBinding.h:
            * bindings/js/JSDOMWindowBase.cpp:
            * bindings/js/JSDOMWindowBase.h:
            Removed cross-heap GC implementation.
    
            * dom/MessagePort.cpp:
            (WebCore::MessagePort::hasPendingActivity):
            * dom/MessagePort.h:
            Made objects RefCounted instead of ThreadSafeShared, added FIXME comments for code that is
            unnecessarily complicated for single threaded case.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38444 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSGlobalObject.h
+++ third_party/WebKit/JavaScriptCore/runtime/JSGlobalObject.h
@@ -159,7 +159,6 @@ namespace JSC {
         virtual ~JSGlobalObject();
 
         virtual void mark();
-        virtual void markCrossHeapDependentObjects();
 
         virtual bool getOwnPropertySlot(ExecState*, const Identifier&, PropertySlot&);
         virtual bool getOwnPropertySlot(ExecState*, const Identifier&, PropertySlot&, bool& slotIsWriteable);
--- third_party/WebKit/WebCore/bindings/js/JSDOMWindowBase.h
+++ third_party/WebKit/WebCore/bindings/js/JSDOMWindowBase.h
@@ -63,8 +63,6 @@ namespace WebCore {
 
         void disconnectFrame();
 
-        virtual void markCrossHeapDependentObjects();
-
         virtual bool getOwnPropertySlot(JSC::ExecState*, const JSC::Identifier&, JSC::PropertySlot&);
         virtual void put(JSC::ExecState*, const JSC::Identifier& propertyName, JSC::JSValue*, JSC::PutPropertySlot&);
 
