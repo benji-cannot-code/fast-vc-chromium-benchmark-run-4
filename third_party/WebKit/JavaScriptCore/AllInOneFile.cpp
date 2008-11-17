a2fdee26d048ea5b869d43d1502c739436695272
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-17 21:29:41 +0000

    2008-11-17  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Sam Weinig.
            
            Moved VM/RegisterFile.cpp => interpreter/RegisterFile.cpp.
    
            * AllInOneFile.cpp:
            * GNUmakefile.am:
            * JavaScriptCore.pri:
            * JavaScriptCore.scons:
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj:
            * JavaScriptCore.xcodeproj/project.pbxproj:
            * JavaScriptCoreSources.bkl:
            * VM/RegisterFile.cpp: Removed.
            * interpreter/RegisterFile.cpp: Copied from VM/RegisterFile.cpp.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38520 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/AllInOneFile.cpp
+++ third_party/WebKit/JavaScriptCore/AllInOneFile.cpp
@@ -103,4 +103,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/FastMalloc.cpp"
 #include "wtf/TCSystemAlloc.cpp"
 #include "bytecompiler/BytecodeGenerator.cpp"
-#include "VM/RegisterFile.cpp"
+#include "interpreter/RegisterFile.cpp"
