9abbcab0bb54e0e7d1b27ee9e338dc6dbca375c4
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-17 04:09:07 +0000

    2008-11-16  Geoffrey Garen  <ggaren@apple.com>
            Not reviewed.
            
            Try to fix gtk build.
    
            * VM/CTI.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/CTI.cpp
+++ third_party/WebKit/JavaScriptCore/VM/CTI.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSArray.h"
 #include "JSFunction.h"
 #include "Machine.h"
-#include "wrec/WREC.h"
 #include "ResultType.h"
 #include "SamplingTool.h"
 
