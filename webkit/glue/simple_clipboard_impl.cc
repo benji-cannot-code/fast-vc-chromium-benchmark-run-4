af265f7b1ba5fe8cfd1beb87cda8bf20331820df
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-17 23:34:21 +0000

    Lazy-initialize Clipboard, since we can't use it until we have an X connection.
    Review URL: http://codereview.chromium.org/11429
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5583 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/simple_clipboard_impl.cc
+++ webkit/glue/simple_clipboard_impl.cc
@@ -8,8 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/clipboard.h"
-#include "webkit/glue/scoped_clipboard_writer_glue.h"
+#include "base/lazy_instance.h"
 #include "googleurl/src/gurl.h"
+#include "webkit/glue/scoped_clipboard_writer_glue.h"
 
 #include "SkBitmap.h"
 
@@ -29,27 +30,27 @@ ScopedClipboardWriterGlue::~ScopedClipboardWriterGlue() {
 
 namespace webkit_glue {
 
-Clipboard clipboard;
+base::LazyInstance<Clipboard> clipboard(base::LINKER_INITIALIZED);
 
 Clipboard* ClipboardGetClipboard() {
-  return &clipboard;
+  return clipboard.Pointer();
 }
 
 bool ClipboardIsFormatAvailable(Clipboard::FormatType format) {
-  return clipboard.IsFormatAvailable(format);
+  return ClipboardGetClipboard()->IsFormatAvailable(format);
 }
 
 void ClipboardReadText(std::wstring* result) {
-  clipboard.ReadText(result);
+  ClipboardGetClipboard()->ReadText(result);
 }
 
 void ClipboardReadAsciiText(std::string* result) {
-  clipboard.ReadAsciiText(result);
+  ClipboardGetClipboard()->ReadAsciiText(result);
 }
 
 void ClipboardReadHTML(std::wstring* markup, GURL* url) {
   std::string url_str;
-  clipboard.ReadHTML(markup, url ? &url_str : NULL);
+  ClipboardGetClipboard()->ReadHTML(markup, url ? &url_str : NULL);
   if (url)
     *url = GURL(url_str);
 }
