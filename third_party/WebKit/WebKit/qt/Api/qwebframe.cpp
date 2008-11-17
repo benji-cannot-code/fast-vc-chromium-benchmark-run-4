9c2846073cc10a915661a00a6aa6e5cf44ddd202
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-17 23:37:02 +0000

    2008-11-17  Geoffrey Garen  <ggaren@apple.com>
            Not reviewed.
            
            Try to fix qt build.
    
            * Api/qwebframe.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38537 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
@@ -62,7 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qt_instance.h"
 #include "ScriptController.h"
 #include "JSDOMBinding.h"
-#include "ExecState.h"
+#include "CallFrame.h"
 #include "JSLock.h"
 #include "JSObject.h"
 #include "qt_runtime.h"
