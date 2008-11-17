2e3cb08381597978fe8e4552b71fd18cdc09e526
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-17 05:03:40 +0000

    2008-11-16  Geoffrey Garen  <ggaren@apple.com>
            Not reviewed.
            
            Fixed tyop.
    
            * VM/CTI.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38480 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/VM/CTI.cpp
+++ third_party/WebKit/JavaScriptCore/VM/CTI.cpp
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define __ m_assembler. 
 
-#if PlATFORM(WIN)
+#if PLATFORM(WIN)
 #undef FIELD_OFFSET // Fix conflict with winnt.h.
 #endif
 
