72e6ae6895d792f253bc59abf68b9751137ca5fb
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-18 00:59:04 +0000

    Fix an inaccurate comment.
    Review URL: http://codereview.chromium.org/11221
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5588 0039d316-1c4b-4281-b951-d872f2087c98

--- base/logging.h
+++ base/logging.h
@@ -89,7 +89,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace logging {
 
 // Where to record logging output? A flat file and/or system debug log via
-// OutputDebugString. Defaults to LOG_ONLY_TO_FILE.
+// OutputDebugString. Defaults on Windows to LOG_ONLY_TO_FILE, and on
+// POSIX to LOG_ONLY_TO_SYSTEM_DEBUG_LOG (aka stderr).
 enum LoggingDestination { LOG_NONE,
                           LOG_ONLY_TO_FILE,
                           LOG_ONLY_TO_SYSTEM_DEBUG_LOG,
