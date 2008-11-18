1e6b4d4ec1ac55b240d8c5d5c3c586b6aacf0f17
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-18 23:16:59 +0000

    2008-11-18  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Sam Weinig.
    
            https://bugs.webkit.org/show_bug.cgi?id=22342
            Remove unused JSLock.h include
    
            * dom/Document.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -115,7 +115,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "XMLTokenizer.h"
 #include "JSDOMBinding.h"
 #include "ScriptController.h"
-#include <runtime/JSLock.h>
 #include <wtf/StdLibExtras.h>
 
 #if ENABLE(DATABASE)
