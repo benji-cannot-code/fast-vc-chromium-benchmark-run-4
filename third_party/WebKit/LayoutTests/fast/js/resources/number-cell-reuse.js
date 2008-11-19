d69c29004e791845be437981c412864dcc020160
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-19 04:04:01 +0000

    2008-11-18  Cameron Zwarich  <zwarich@apple.com>
            Reviewed by Maciej Stachowiak and Geoff Garen.
    
            Bug 22287: ASSERTION FAILED: Not enough jumps linked in slow case codegen in CTI::privateCompileSlowCases())
            <https://bugs.webkit.org/show_bug.cgi?id=22287>
    
            Fix a typo in the number cell reuse code where the first and second
            operands are sometimes confused.
    
            JavaScriptcore:
    
            * jit/JIT.cpp:
            (JSC::JIT::compileBinaryArithOpSlowCase):
    
            LayoutTests:
    
            * fast/js/number-cell-reuse-expected.txt: Added.
            * fast/js/number-cell-reuse.html: Added.
            * fast/js/resources/number-cell-reuse.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/number-cell-reuse.js
@@ -0,0 +1,66 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This test checks corner cases of the number cell reuse code. In particular, it checks for known cases where code generation for number cell reuse caused assertions to fail."
+);
+
+function leftConstantRightSimple(a)
+{
+    return 0.1 * (a * a);
+}
+
+shouldBe("leftConstantRightSimple(2)", "0.4");
+
+function leftConstantRightComplex(a)
+{
+    return 0.1 * (a * a + a);
+}
+
+shouldBe("leftConstantRightComplex(1)", "0.2");
+
+function leftSimpleRightConstant(a)
+{
+    return (a * a) * 0.1;
+}
+
+shouldBe("leftSimpleRightConstant(2)", "0.4");
+
+function leftComplexRightConstant(a)
+{
+    return (a * a + a) * 0.1;
+}
+
+shouldBe("leftComplexRightConstant(1)", "0.2");
+
+function leftThisRightSimple(a)
+{
+    return this * (a * a);
+}
+
+shouldBeNaN("leftThisRightSimple(2)");
+shouldBe("leftThisRightSimple.call(2, 2)", "8");
+
+function leftThisRightComplex(a)
+{
+    return this * (a * a + a);
+}
+
+shouldBeNaN("leftThisRightComplex(2)");
+shouldBe("leftThisRightComplex.call(2, 2)", "12");
+
+function leftSimpleRightThis(a)
+{
+    return (a * a) * this;
+}
+
+shouldBeNaN("leftSimpleRightThis(2)");
+shouldBe("leftSimpleRightThis.call(2, 2)", "8");
+
+function leftComplexRightThis(a)
+{
+    return (a * a + a) * this;
+}
+
+shouldBeNaN("leftComplexRightThis(2)");
+shouldBe("leftComplexRightThis.call(2, 2)", "12");
+
+var successfullyParsed = true;
