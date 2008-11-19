0bd072c60d6cb0da1490d1f5adedc694e2674efc
mmoss@google.com <mmoss@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-19 22:26:05 +0000

    Make http_server.sh mutli-platform.
    Review URL: http://codereview.chromium.org/11498
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5718 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/layout_tests/layout_package/http_server.sh
+++ webkit/tools/layout_tests/layout_package/http_server.sh
@@ -1,10 +1,20 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/sh
 
-system_root=`cygpath "$SYSTEMROOT"`
-export PATH="/usr/bin:$system_root/system32:$system_root:$system_root/system32/WBEM"
-
 exec_dir=$(dirname $0)
 
-"$exec_dir/../../../../third_party/python_24/python.exe" \
-    "$exec_dir/http_server.py" "$@"
+if [ "$OSTYPE" = "cygwin" ]; then
+  system_root=`cygpath "$SYSTEMROOT"`
+  PATH="/usr/bin:$system_root/system32:$system_root:$system_root/system32/WBEM"
+  export PATH
+  python_prog="$exec_dir/../../../third_party/python_24/python.exe"
+else
+  python_prog=python
+  # When not using the included python, we don't get automatic site.py paths.
+  # Specifically, run_webkit_tests needs the paths in:
+  # third_party/python_24/Lib/site-packages/google.pth
+  PYTHONPATH="$(readlink -f ${exec_dir}/../../../../tools/python):$PYTHONPATH"
+  export PYTHONPATH
+fi
+
+"$python_prog" "$exec_dir/http_server.py" "$@"
