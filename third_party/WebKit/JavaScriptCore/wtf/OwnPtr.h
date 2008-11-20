166fd88323407767eec0ea949d1256188a638af1
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-20 21:25:35 +0000

    2008-11-20  Sam Weinig  <sam@webkit.org>
            Reviewed by Darin Adler.
    
            Add OwnPtr constructor and OwnPtr::adopt that take an auto_ptr.
    
            * wtf/OwnPtr.h:
            (WTF::OwnPtr::OwnPtr):
            (WTF::OwnPtr::adopt):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38628 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/OwnPtr.h
+++ third_party/WebKit/JavaScriptCore/wtf/OwnPtr.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_OwnPtr_h
 
 #include <algorithm>
+#include <memory>
 #include <wtf/Assertions.h>
 #include <wtf/Noncopyable.h>
 
@@ -67,12 +68,17 @@ namespace WTF {
         typedef ValueType* PtrType;
 
         explicit OwnPtr(PtrType ptr = 0) : m_ptr(ptr) { }
+        OwnPtr(std::auto_ptr<ValueType> autoPtr) : m_ptr(autoPtr.release()) { }
         ~OwnPtr() { deleteOwnedPtr(m_ptr); }
 
         PtrType get() const { return m_ptr; }
         PtrType release() { PtrType ptr = m_ptr; m_ptr = 0; return ptr; }
 
+        // FIXME: This should be renamed to adopt. 
         void set(PtrType ptr) { ASSERT(!ptr || m_ptr != ptr); deleteOwnedPtr(m_ptr); m_ptr = ptr; }
+
+        void adopt(std::auto_ptr<ValueType> autoPtr) { ASSERT(!autoPtr.get() || m_ptr != autoPtr.get()); deleteOwnedPtr(m_ptr); m_ptr = autoPtr.release(); }
+
         void clear() { deleteOwnedPtr(m_ptr); m_ptr = 0; }
 
         ValueType& operator*() const { ASSERT(m_ptr); return *m_ptr; }
