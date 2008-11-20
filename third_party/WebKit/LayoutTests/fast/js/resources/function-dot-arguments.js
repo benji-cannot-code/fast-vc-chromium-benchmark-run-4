1c08fd32761df16f622281bec964000c4abe1e4c
pam@chromium.org <pam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-20 23:31:08 +0000

    2008-11-20  Pamela Greene  <pam@chromium.org>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=22369
            Add test cases covering more basic operations.
    
            * fast/js/function-dot-arguments-expected.txt:
            * fast/js/resources/function-dot-arguments.js:
            (basicTest): Added
            (lengthTest): Added
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38637 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/resources/function-dot-arguments.js
+++ third_party/WebKit/LayoutTests/fast/js/resources/function-dot-arguments.js
@@ -1,7 +1,22 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description(
-"This test checks corner cases of 'f.arguments'."
+"This test checks basic and corner cases of 'f.arguments'."
 );
+
+function basicTest(args)
+{
+    return basicTest.arguments;
+}
+shouldBe("basicTest('one')[0]", "'one'");
+shouldBeUndefined("basicTest('one')[2]");
+shouldBe("basicTest('one', 'two', 'three')[1]", "'two'");
+
+function lengthTest()
+{
+    return "" + lengthTest.arguments.length;
+}
+shouldBe("lengthTest()", "'0'");
+shouldBe("lengthTest('From', '%E5%8C%97%E4%BA%AC', 360, '/', 'webkit.org')", "'5'");
 
 function assignTest()
 {
