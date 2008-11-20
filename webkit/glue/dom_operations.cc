85e34faa2c8d714b132e2292f82e81bacceb025d
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-20 02:04:41 +0000

    Review URL: http://codereview.chromium.org/11306
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5747 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/dom_operations.cc
+++ webkit/glue/dom_operations.cc
@@ -28,6 +28,10 @@ MSVC_PUSH_WARNING_LEVEL(0);
 MSVC_POP_WARNING();
 #undef LOG
 
+// Brings in more WebKit headers and #undefs LOG again, so this needs to come
+// first.
+#include "webkit/glue/autocomplete_input_listener.h"
+
 #include "base/string_util.h"
 #include "webkit/glue/dom_operations.h"
 #include "webkit/glue/form_data.h"
@@ -446,7 +450,7 @@ void FillPasswordForm(WebView* view,
         static_cast<WebFrameLoaderClient*>(username_element->document()->
                                            frame()->loader()->client());
     WebFrameImpl* webframe_impl = frame_loader_client->webframe();
-    webframe_impl->RegisterPasswordListener(
+    webframe_impl->GetAutocompleteListener()->AddInputListener(
         username_element,
         new PasswordAutocompleteListener(
             new HTMLInputDelegate(username_element),
