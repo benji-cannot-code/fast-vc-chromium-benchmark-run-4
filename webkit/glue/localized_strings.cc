fcb8f4f60f5c7e7d81a14c916cf12f13c9ac28d4
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-20 23:54:20 +0000

    Shim in test_shell string resources needed for http:// tests. We eventually need to figure out our localization / resources on Linux, but this will unblock some parallel work for now.
    Review URL: http://codereview.chromium.org/11553
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5801 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/localized_strings.cc
+++ webkit/glue/localized_strings.cc
@@ -39,12 +39,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "webkit/glue/glue_util.h"
 
-#if defined(OS_WIN)
-#include "webkit_strings.h"
-#else
+#if defined(OS_MACOSX)
 // TODO:(pinkerton): only windows has the GRIT machinery, so we've created a
 // temporary generated header until we can figure out the l10n strategy.
 #include "bogus_webkit_strings.h"
+#else
+#include "webkit_strings.h"
 #endif
 
 using namespace WebCore;
