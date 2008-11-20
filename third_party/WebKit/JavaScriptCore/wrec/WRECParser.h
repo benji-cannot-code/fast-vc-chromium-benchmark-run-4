799a6e0a7bee1b559657feb12fe477abfe3c9de3
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-20 17:38:50 +0000

    2008-11-20  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Sam Weinig.
            
            A little more WREC refactoring.
            
            Removed the "Register" suffix from register names in WREC, and renamed:
                currentPosition => index
                currentValue => character
                quantifierCount => repeatCount
                
            Added a top-level parsePattern function to the WREC parser, which
            allowed me to remove the error() and atEndOfPattern() accessors.
            
            Factored out an MSVC customization into a constant.
            
            Renamed nextLabel => beginPattern.
    
            * wrec/WREC.cpp:
            (JSC::WREC::compileRegExp):
            * wrec/WRECGenerator.cpp:
            (JSC::WREC::Generator::generateBacktrack1):
            (JSC::WREC::Generator::generateBacktrackBackreference):
            (JSC::WREC::Generator::generateBackreferenceQuantifier):
            (JSC::WREC::Generator::generateNonGreedyQuantifier):
            (JSC::WREC::Generator::generateGreedyQuantifier):
            (JSC::WREC::Generator::generatePatternCharacter):
            (JSC::WREC::Generator::generateCharacterClassInvertedRange):
            (JSC::WREC::Generator::generateCharacterClassInverted):
            (JSC::WREC::Generator::generateCharacterClass):
            (JSC::WREC::Generator::generateParentheses):
            (JSC::WREC::Generator::generateParenthesesResetTrampoline):
            (JSC::WREC::Generator::generateAssertionBOL):
            (JSC::WREC::Generator::generateAssertionEOL):
            (JSC::WREC::Generator::generateAssertionWordBoundary):
            (JSC::WREC::Generator::generateBackreference):
            (JSC::WREC::Generator::generateDisjunction):
            (JSC::WREC::Generator::terminateDisjunction):
            * wrec/WRECGenerator.h:
            * wrec/WRECParser.h:
            (JSC::WREC::Parser::parsePattern):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38621 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wrec/WRECParser.h
+++ third_party/WebKit/JavaScriptCore/wrec/WRECParser.h
@@ -76,8 +76,14 @@ namespace JSC { namespace WREC {
             while (parseTerm(failures)) { }
         }
 
-        void parseDisjunction(JmpSrcVector& failures);
+        bool parsePattern(JmpSrcVector& failures)
+        {
+            parseDisjunction(failures);
 
+            // Parsing the pattern should fully consume it.
+            return peek() == EndOfPattern && m_error == NoError;
+        }
+        void parseDisjunction(JmpSrcVector& failures);
         bool parseTerm(JmpSrcVector& failures);
         bool parseEscape(JmpSrcVector& failures);
         bool parseOctalEscape(JmpSrcVector& failures);
@@ -150,13 +156,6 @@ namespace JSC { namespace WREC {
             return n;
         }
 
-        bool atEndOfPattern()
-        {
-            return peek() == EndOfPattern;
-        }
-        
-        Error error() { return m_error; }
-        
         void recordSubpattern() { ++m_numSubpatterns; }
         unsigned numSubpatterns() { return m_numSubpatterns; }
         
