16e4c84b10b7949689131e92bf6213bfb4aae14b
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-21 00:48:46 +0000

    2008-11-20  Dean Jackson  <dino@apple.com>
            Reviewed by Antti Koivisto
    
            https://bugs.webkit.org/show_bug.cgi?id=21247
    
            Make transition shorthand property accept transition-delay.
    
            Test: transitions/transition-shorthand-delay.html
    
            * css/CSSParser.cpp:
            (WebCore::CSSParser::parseTransitionShorthand):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSParser.cpp
+++ third_party/WebKit/WebCore/css/CSSParser.cpp
@@ -1795,7 +1795,8 @@ bool CSSParser::parseTransitionShorthand(bool important)
 {
     const int properties[] = { CSSPropertyWebkitTransitionProperty,
                                CSSPropertyWebkitTransitionDuration,
-                               CSSPropertyWebkitTransitionTimingFunction };
+                               CSSPropertyWebkitTransitionTimingFunction,
+                               CSSPropertyWebkitTransitionDelay };
     const int numProperties = sizeof(properties) / sizeof(properties[0]);
     
     ShorthandScope scope(this, CSSPropertyWebkitTransition);
