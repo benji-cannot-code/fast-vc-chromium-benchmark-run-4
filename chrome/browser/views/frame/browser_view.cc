af6e6c95e6d5b61fcd382d7f4f82c9f259c091ea
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-21 22:27:24 +0000

    Beginnings of a new InfoBar system.
    This implements AlertInfoBar and InfoBarContainer. It also makes the crashed plugin/js oom infobars use this new system.
    
    Design Doc: http://dev.chromium.org/developers/design-documents/info-barshttp://crbug.com/4620
    
    Review URL: http://codereview.chromium.org/11318
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5856 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_view.cc
+++ chrome/browser/views/frame/browser_view.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/frame/browser_frame.h"
 #include "chrome/browser/views/html_dialog_view.h"
 #include "chrome/browser/views/importer_view.h"
+#include "chrome/browser/views/infobars/infobar_container.h"
 #include "chrome/browser/views/keyword_editor_view.h"
 #include "chrome/browser/views/password_manager_view.h"
 #include "chrome/browser/views/status_bubble.h"
@@ -339,6 +340,9 @@ void BrowserView::Init() {
   toolbar_->Init(browser_->profile());
   toolbar_->SetAccessibleName(l10n_util::GetString(IDS_ACCNAME_TOOLBAR));
 
+  infobar_container_ = new InfoBarContainer(this);
+  AddChildView(infobar_container_);
+
   contents_container_ = new TabContentsContainerView;
   set_contents_view(contents_container_);
   AddChildView(contents_container_);
@@ -646,7 +650,9 @@ void BrowserView::TabSelectedAt(TabContents* old_contents,
   if (old_contents)
     old_contents->StoreFocus();
 
-  // Tell the frame what happened so that the TabContents gets resized, etc.
+  // Update various elements that are interested in knowing the current
+  // TabContents.
+  infobar_container_->ChangeTabContents(new_contents);
   contents_container_->SetTabContents(new_contents);
   // TODO(beng): This should be called automatically by SetTabContents, but I
   //             am striving for parity now rather than cleanliness. This is
@@ -1056,7 +1062,7 @@ int BrowserView::LayoutBookmarkAndInfoBars(int top) {
     // the Bookmark bar isn't visible on all tabs, then we need to show the
     // Info bar _above_ the Bookmark bar, since the Bookmark bar is styled to
     // look like it's part of the New Tab Page...
-    if (active_info_bar_ && active_bookmark_bar_ &&
+    if (active_bookmark_bar_ &&
         bookmark_bar_view_->IsNewTabPage() &&
         !bookmark_bar_view_->IsAlwaysShown()) {
       top = LayoutInfoBar(top);
@@ -1079,13 +1085,21 @@ int BrowserView::LayoutBookmarkBar(int top) {
   return top;
 }
 int BrowserView::LayoutInfoBar(int top) {
-  if (SupportsWindowFeature(FEATURE_INFOBAR) && active_info_bar_) {
-    gfx::Size ps = active_info_bar_->GetPreferredSize();
-    active_info_bar_->SetBounds(0, top, width(), ps.height());
+  if (SupportsWindowFeature(FEATURE_INFOBAR)) {
+    // Layout the new infobar.
+    gfx::Size ps = infobar_container_->GetPreferredSize();
+    infobar_container_->SetBounds(0, top, width(), ps.height());
     top += ps.height();
-    if (SupportsWindowFeature(FEATURE_BOOKMARKBAR) && active_bookmark_bar_ &&
-        !show_bookmark_bar_pref_.GetValue()) {
-      top -= kSeparationLineHeight;
+
+    if (active_info_bar_) {
+      // Layout the old infobar.
+      ps = active_info_bar_->GetPreferredSize();
+      active_info_bar_->SetBounds(0, top, width(), ps.height());
+      top += ps.height();
+      if (SupportsWindowFeature(FEATURE_BOOKMARKBAR) && active_bookmark_bar_ &&
+          !show_bookmark_bar_pref_.GetValue()) {
+        top -= kSeparationLineHeight;
+      }
     }
   }
   return top;
@@ -1132,7 +1146,10 @@ bool BrowserView::MaybeShowInfoBar(TabContents* contents) {
   if (contents && contents->AsWebContents() &&
       contents->AsWebContents()->view()->IsInfoBarVisible())
     new_info_bar = contents->AsWebContents()->view()->GetInfoBarView();
-  return UpdateChildViewAndLayout(new_info_bar, &active_info_bar_);
+  UpdateChildViewAndLayout(new_info_bar, &active_info_bar_);
+
+  // TODO(beng): remove this function once the InfoBar rejiggering is complete.
+  return true;
 }
 
 bool BrowserView::MaybeShowDownloadShelf(TabContents* contents) {
