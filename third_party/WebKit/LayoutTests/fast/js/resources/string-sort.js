57f689c24d29613ddb803c74c5352cc266921a32
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-21 21:26:49 +0000

    2008-11-21  Cameron Zwarich  <zwarich@apple.com>
            Reviewed by Sam Weinig.
    
            Add a test for <rdar://problem/4268912>, which was fixed in r13294 (!!).
    
            * fast/js/resources/string-sort.js:
            * fast/js/string-sort-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/resources/string-sort.js
+++ third_party/WebKit/LayoutTests/fast/js/resources/string-sort.js
@@ -16,5 +16,6 @@ shouldBeInThisOrder(["_", "a"]);
 shouldBeInThisOrder(["0", "A"]);
 shouldBeInThisOrder(["-", "0"]);
 shouldBeInThisOrder([" ", "-"]);
+shouldBeInThisOrder(["A", "ABC"]);
 
 var successfullyParsed = true;
