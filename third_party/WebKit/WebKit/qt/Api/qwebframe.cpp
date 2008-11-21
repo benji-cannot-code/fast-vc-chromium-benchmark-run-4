4d990706bee4d197e6ba6b8eca934d489b32b29f
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-21 07:16:50 +0000

    2008-11-20  Sam Weinig  <sam@webkit.org>
            Fix Qt build.
    
            * Api/qwebframe.cpp:
            (QWebFrame::evaluateJavaScript):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38655 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
@@ -55,18 +55,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 #include "HitTestResult.h"
 
+#include "CallFrame.h"
+#include "JSDOMBinding.h"
+#include "JSDOMWindow.h"
+#include "JSLock.h"
+#include "JSObject.h"
+#include "qt_instance.h"
+#include "qt_runtime.h"
 #include "runtime.h"
 #include "runtime_object.h"
 #include "runtime_root.h"
-#include "JSDOMWindow.h"
-#include "qt_instance.h"
 #include "ScriptController.h"
 #include "ScriptValue.h"
-#include "JSDOMBinding.h"
-#include "CallFrame.h"
-#include "JSLock.h"
-#include "JSObject.h"
-#include "qt_runtime.h"
+#include "StringSourceProvider.h"
 
 #include "wtf/HashMap.h"
 
@@ -935,7 +936,7 @@ QVariant QWebFrame::evaluateJavaScript(const QString& scriptSource)
     ScriptController *proxy = d->frame->script();
     QVariant rc;
     if (proxy) {
-        JSC::JSValue* v = proxy->evaluate(String(), 1, scriptSource).jsValue();
+        JSC::JSValue* v = proxy->evaluate(makeSource(scriptSource)).jsValue();
         if (v) {
             int distance = 0;
             rc = JSC::Bindings::convertValueToQVariant(proxy->globalObject()->globalExec(), v, QMetaType::Void, &distance);
