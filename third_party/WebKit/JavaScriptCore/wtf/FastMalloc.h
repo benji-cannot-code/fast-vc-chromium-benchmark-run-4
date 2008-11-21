ca6dd068dfe035a041645701544be2b864b4b619
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-21 20:09:40 +0000

    Reverted fix for bug 22042 (Replace abort() with CRASH()), because it was breaking         FOR_EACH_OPCODE_ID macro somehow, making Safari crash.
    git-svn-id: svn://svn.chromium.org/blink/trunk@38672 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/FastMalloc.h
+++ third_party/WebKit/JavaScriptCore/wtf/FastMalloc.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-    // These functions call CRASH() if an allocation fails.
+    // These functions call abort() if an allocation fails.
     void* fastMalloc(size_t n);
     void* fastZeroedMalloc(size_t n);
     void* fastCalloc(size_t n_elements, size_t element_size);
--- third_party/WebKit/JavaScriptCore/wtf/TCSpinLock.h
+++ third_party/WebKit/JavaScriptCore/wtf/TCSpinLock.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 #include <sys/types.h>
 #endif
+#include <stdlib.h>     /* for abort() */
 
 #if PLATFORM(WIN_OS)
 #ifndef WIN32_LEAN_AND_MEAN
@@ -199,16 +200,16 @@ struct TCMalloc_SpinLock {
   pthread_mutex_t private_lock_;
 
   inline void Init() {
-    if (pthread_mutex_init(&private_lock_, NULL) != 0) CRASH();
+    if (pthread_mutex_init(&private_lock_, NULL) != 0) abort();
   }
   inline void Finalize() {
-    if (pthread_mutex_destroy(&private_lock_) != 0) CRASH();
+    if (pthread_mutex_destroy(&private_lock_) != 0) abort();
   }
   inline void Lock() {
-    if (pthread_mutex_lock(&private_lock_) != 0) CRASH();
+    if (pthread_mutex_lock(&private_lock_) != 0) abort();
   }
   inline void Unlock() {
-    if (pthread_mutex_unlock(&private_lock_) != 0) CRASH();
+    if (pthread_mutex_unlock(&private_lock_) != 0) abort();
   }
 };
 
