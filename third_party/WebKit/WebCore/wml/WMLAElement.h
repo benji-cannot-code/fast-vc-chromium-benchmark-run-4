136fb3342e043f8015667171df3cf8b527540ff9
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-21 15:25:26 +0000

    Reviewed by Alexey Proskuryakov.
    Fix some coding style/formatting issues in WMLAElement.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38659 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/wml/WMLAElement.h
+++ third_party/WebKit/WebCore/wml/WMLAElement.h
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-
 /**
  * Copyright (C) 2008 Torch Mobile Inc. All rights reserved.
  *               http://www.torchmobile.com/
