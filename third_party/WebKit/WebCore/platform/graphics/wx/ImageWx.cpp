8ff325f06372f0c1a34f8cbec2d857548658619d
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-22 03:08:15 +0000

    Reviewed by Kevin Ollivier.
    Improve wx image drawing performance considerably when using wxGraphicsContext
    by avoiding unnecessary copies and drawing.
            
    https://bugs.webkit.org/show_bug.cgi?id=22404
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38685 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/wx/ImageWx.cpp
+++ third_party/WebKit/WebCore/platform/graphics/wx/ImageWx.cpp
@@ -96,6 +96,7 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& dst, const FloatR
 
 #if USE(WXGC)
     wxGCDC* context = (wxGCDC*)ctxt->platformContext();
+    wxGraphicsContext* gc = context->GetGraphicsContext();
 #else
     wxWindowDC* context = ctxt->platformContext();
 #endif
@@ -115,7 +116,26 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& dst, const FloatR
 
     // Set the compositing operation.
     ctxt->setCompositeOperation(op);
+    
+#if USE(WXGC)
+    float scaleX = src.width() / dst.width();
+    float scaleY = src.height() / dst.height();
 
+    FloatRect adjustedDestRect = dst;
+    FloatSize selfSize = currentFrameSize();
+    
+    if (src.size() != selfSize) {
+        adjustedDestRect.setLocation(FloatPoint(dst.x() - src.x() / scaleX, dst.y() - src.y() / scaleY));
+        adjustedDestRect.setSize(FloatSize(selfSize.width() / scaleX, selfSize.height() / scaleY));
+    }
+    
+    // If the image is only partially loaded, then shrink the destination rect that we're drawing into accordingly.
+    int currHeight = bitmap->GetHeight(); 
+    if (currHeight < selfSize.height())
+        adjustedDestRect.setHeight(adjustedDestRect.height() * currHeight / selfSize.height());
+
+    gc->DrawBitmap(*bitmap, adjustedDestRect.x(), adjustedDestRect.y(), adjustedDestRect.width(), adjustedDestRect.height());
+#else
     IntRect srcIntRect(src);
     IntRect dstIntRect(dst);
     bool rescaling = false;
@@ -125,7 +145,8 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& dst, const FloatR
         wxImage img = bitmap->ConvertToImage();
         img.Rescale(dstIntRect.width(), dstIntRect.height());
         bitmap = new wxBitmap(img);
-    }   
+    } 
+    
     wxMemoryDC mydc; 
     ASSERT(bitmap->GetRefData());
     mydc.SelectObject(*bitmap); 
@@ -144,6 +165,8 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& dst, const FloatR
         delete bitmap;
         bitmap = NULL;
     }
+#endif
+
     ctxt->restore();
 }
 
@@ -170,23 +193,33 @@ void BitmapImage::drawPattern(GraphicsContext* ctxt, const FloatRect& srcRect, c
 #if USE(WXGC)
     wxGraphicsContext* gc = context->GetGraphicsContext();
     gc->ConcatTransform(patternTransform);
-#endif
-
+#else
     wxMemoryDC mydc;
     mydc.SelectObject(*bitmap);
+#endif
 
-    while ( currentW < dstRect.width() ) {
-        while ( currentH < dstRect.height() ) {
+    wxPoint origin(context->GetDeviceOrigin());
+    wxSize clientSize(context->GetSize());
+
+    while ( currentW < dstRect.width()  && currentW < clientSize.x - origin.x ) {
+        while ( currentH < dstRect.height() && currentH < clientSize.y - origin.y) {
+#if USE(WXGC)
+            gc->DrawBitmap(*bitmap, (wxDouble)dstRect.x() + currentW, (wxDouble)dstRect.y() + currentH, (wxDouble)srcRect.width(), (wxDouble)srcRect.height());
+#else
             context->Blit((wxCoord)dstRect.x() + currentW, (wxCoord)dstRect.y() + currentH,  
                             (wxCoord)srcRect.width(), (wxCoord)srcRect.height(), &mydc, 
                             (wxCoord)srcRect.x(), (wxCoord)srcRect.y(), wxCOPY, true); 
+#endif
             currentH += srcRect.height();
         }
         currentW += srcRect.width();
         currentH = 0;
     }
     ctxt->restore();
+
+#if !USE(WXGC)
     mydc.SelectObject(wxNullBitmap);
+#endif    
     
     // NB: delete is causing crashes during page load, but not during the deletion
     // itself. It occurs later on when a valid bitmap created in frameAtIndex
