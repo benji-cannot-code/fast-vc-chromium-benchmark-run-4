283bc0b467f6e9d1b9fa206a1452e3607f4ff271
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-22 00:11:28 +0000

    2008-11-21  Darin Fisher  <darin@chromium.org>
            Reviewed by Geoffrey Garen.
    
            https://bugs.webkit.org/show_bug.cgi?id=22390
            Abstract away JSC:: usage in WebCore/xml
    
            * bindings/js/ScriptString.h: Added.
            (WebCore::ScriptString::ScriptString):
            (WebCore::ScriptString::operator JSC::UString):
            (WebCore::ScriptString::isNull):
            (WebCore::ScriptString::size):
            (WebCore::ScriptString::operator=):
            (WebCore::ScriptString::operator+=):
            * inspector/InspectorController.cpp:
            (WebCore::XMLHttpRequestResource::XMLHttpRequestResource):
            (WebCore::InspectorResource::setXMLHttpRequestProperties):
            (WebCore::InspectorController::resourceRetrievedByXMLHttpRequest):
            * inspector/InspectorController.h:
            * xml/XMLHttpRequest.cpp:
            (WebCore::XMLHttpRequest::responseText):
            (WebCore::XMLHttpRequest::clearResponse):
            (WebCore::XMLHttpRequest::dropProtection):
            (WebCore::XMLHttpRequest::didFinishLoading):
            (WebCore::XMLHttpRequest::didReceiveData):
            * xml/XMLHttpRequest.h:
            (WebCore::XMLHttpRequest::setLastSendURL):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38680 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorController.cpp
+++ third_party/WebKit/WebCore/inspector/InspectorController.cpp
@@ -218,7 +218,7 @@ struct ConsoleMessage {
 // XMLHttpRequestResource Class
 
 struct XMLHttpRequestResource {
-    XMLHttpRequestResource(JSC::UString& sourceString)
+    XMLHttpRequestResource(const JSC::UString& sourceString)
     {
         JSC::JSLock lock(false);
         this->sourceString = sourceString.rep();
@@ -303,7 +303,7 @@ struct InspectorResource : public RefCounted<InspectorResource> {
             JSValueProtect(context, newScriptObject);
     }
 
-    void setXMLHttpRequestProperties(JSC::UString& data)
+    void setXMLHttpRequestProperties(const JSC::UString& data)
     {
         xmlHttpRequestResource.set(new XMLHttpRequestResource(data));
     }
@@ -2499,7 +2499,7 @@ void InspectorController::didFailLoading(DocumentLoader* loader, unsigned long i
     }
 }
 
-void InspectorController::resourceRetrievedByXMLHttpRequest(unsigned long identifier, JSC::UString& sourceString)
+void InspectorController::resourceRetrievedByXMLHttpRequest(unsigned long identifier, const JSC::UString& sourceString)
 {
     if (!enabled())
         return;
--- third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
+++ third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoader.h"
 #include "HTTPParsers.h"
 #include "InspectorController.h"
-#include "JSDOMBinding.h"
-#include "JSDOMWindow.h"
 #include "KURL.h"
 #include "KURLHash.h"
 #include "Page.h"
@@ -48,8 +46,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "XMLHttpRequestProgressEvent.h"
 #include "XMLHttpRequestUpload.h"
 #include "markup.h"
-#include <runtime/JSLock.h>
 #include <wtf/StdLibExtras.h>
+
+#if USE(JSC)
+#include "JSDOMWindow.h"
+#endif
 
 namespace WebCore {
 
@@ -207,7 +208,7 @@ XMLHttpRequest::State XMLHttpRequest::readyState() const
     return m_state;
 }
 
-const JSC::UString& XMLHttpRequest::responseText() const
+const ScriptString& XMLHttpRequest::responseText() const
 {
     return m_responseText;
 }
@@ -779,10 +780,7 @@ void XMLHttpRequest::internalAbort()
 void XMLHttpRequest::clearResponse()
 {
     m_response = ResourceResponse();
-    {
-        JSC::JSLock lock(false);
-        m_responseText = "";
-    }
+    m_responseText = "";
     m_createdDocument = false;
     m_responseXML = 0;
 }
@@ -827,6 +825,7 @@ void XMLHttpRequest::abortError()
 
 void XMLHttpRequest::dropProtection()        
 {
+#if USE(JSC)
     // The XHR object itself holds on to the responseText, and
     // thus has extra cost even independent of any
     // responseText or responseXML objects it has handed
@@ -838,6 +837,7 @@ void XMLHttpRequest::dropProtection()
         if (JSC::JSValue* wrapper = getCachedDOMObjectWrapper(*window->globalData(), this))
             JSC::Heap::heap(wrapper)->reportExtraMemoryCost(m_responseText.size() * 2);
     }
+#endif
 
     unsetPendingActivity(this);
 }
@@ -1053,11 +1053,8 @@ void XMLHttpRequest::didFinishLoading(SubresourceLoader* loader)
     if (m_state < HEADERS_RECEIVED)
         changeState(HEADERS_RECEIVED);
 
-    {
-        JSC::JSLock lock(false);
-        if (m_decoder)
-            m_responseText += m_decoder->flush();
-    }
+    if (m_decoder)
+        m_responseText += m_decoder->flush();
 
     if (Frame* frame = document()->frame()) {
         if (Page* page = frame->page()) {
@@ -1253,12 +1250,7 @@ void XMLHttpRequest::didReceiveData(SubresourceLoader*, const char* data, int le
     if (len == -1)
         len = strlen(data);
 
-    String decoded = m_decoder->decode(data, len);
-
-    {
-        JSC::JSLock lock(false);
-        m_responseText += decoded;
-    }
+    m_responseText += m_decoder->decode(data, len);
 
     if (!m_error) {
         updateAndDispatchOnProgress(len);
