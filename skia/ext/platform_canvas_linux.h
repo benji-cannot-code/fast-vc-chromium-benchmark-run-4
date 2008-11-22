f003cc87c989166d4aa120bd9898df7a35b8c6b4
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-22 00:17:42 +0000

    Fix a header rename I missed on the last step. Review URL: http://codereview.chromium.org/11585
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5876 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/platform_canvas_linux.h
+++ skia/ext/platform_canvas_linux.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SKIA_EXT_PLATFORM_CANVAS_LINUX_H_
 #define SKIA_EXT_PLATFORM_CANVAS_LINUX_H_
 
-#include "PlatformDeviceLinux.h"
+#include "skia/ext/platform_device_linux.h"
 
 namespace gfx {
 
