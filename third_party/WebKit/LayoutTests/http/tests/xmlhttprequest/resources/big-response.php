e040150d29219a457d274607cc8de60c03913c0b
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-24 14:42:52 +0000

    Reviewed by Mark Rowe.
            https://bugs.webkit.org/show_bug.cgi?id=22428
            xmlhttprequest-onProgress-open-should-zero-length.html fails intermittently
    
            * http/tests/xmlhttprequest/resources/big-response.php: Reduce the size of big response
            to make the test faster, but practically guarantee that the response will be delivered in
            many chunks.
    
            * http/tests/xmlhttprequest/xmlhttprequest-onProgress-open-should-zero-length-expected.txt:
            * http/tests/xmlhttprequest/xmlhttprequest-onProgress-open-should-zero-length.html:
            Made the test more reliable by comparing final response position with known resource size.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38709 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/resources/big-response.php
+++ third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/resources/big-response.php
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-for ($i = 0; $i < 1024 * 1024; $i++) {
+for ($i = 0; $i < 128 * 1024; $i++) {
     echo "Chunk $i";
     if (!$i % 200)
         flush();
