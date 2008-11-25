3d6592b668a755a417a74748d5bfa0901f979a28
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-25 02:20:27 +0000

    2008-11-24  Darin Fisher  <darin@chromium.org>
            Fix bustage.
    
            * History/WebHistory.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38740 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/History/WebHistory.mm
+++ third_party/WebKit/WebKit/mac/History/WebHistory.mm
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebHistoryItemInternal.h"
 #import "WebKitLogging.h"
 #import "WebNSURLExtras.h"
+#import "WebTypesInternal.h"
 #import <WebCore/HistoryItem.h>
 #import <WebCore/PageGroup.h>
 
