c532d9c073b9f5176b2417708967ce6a70ab38f6
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-25 23:41:44 +0000

    ScopedHandle is not in a namespace.
    Review URL: http://codereview.chromium.org/12663
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6010 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test_file_util_win.cc
+++ base/test_file_util_win.cc
@@ -10,12 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/file_util.h"
+#include "base/scoped_handle.h"
 
 namespace file_util {
 
 bool EvictFileFromSystemCache(const wchar_t* file) {
   // Request exclusive access to the file and overwrite it with no buffering.
-  win_util::ScopedHandle hfile(
+  ScopedHandle hfile(
       CreateFile(file, GENERIC_READ | GENERIC_WRITE, 0, NULL,
                  OPEN_EXISTING, FILE_FLAG_NO_BUFFERING,
                  NULL));
