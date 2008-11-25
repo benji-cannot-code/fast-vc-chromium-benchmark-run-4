b5aae7f628fa38eb8550f6016ef2cb207fd639ea
jungshik@google.com <jungshik@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-25 00:15:14 +0000

    Get rid of ISO-8859-9 in the character encoding menu because it's now treated as an alias to its superset, windows-1254.
    BUG=4746
    TEST=visit http://www.sabah.com.tr (ISO-8859-9 page) and make sure that 'Turkish' is checked in the encoding menu
    
    
    Review URL: http://codereview.chromium.org/11811
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5948 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_dll_resource.h
+++ chrome/app/chrome_dll_resource.h
@@ -124,13 +124,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_ENCODING_ISO885910          32920
 #define IDC_ENCODING_ISO885914          32921
 #define IDC_ENCODING_ISO885916          32922
-#define IDC_ENCODING_ISO88599           32923
-#define IDC_ENCODING_WINDOWS1254        32924
-#define IDC_ENCODING_ISO88596           32925
-#define IDC_ENCODING_WINDOWS1256        32926
-#define IDC_ENCODING_ISO88598           32927
-#define IDC_ENCODING_WINDOWS1255        32928
-#define IDC_ENCODING_WINDOWS1258        32929
+#define IDC_ENCODING_WINDOWS1254        32923
+#define IDC_ENCODING_ISO88596           32924
+#define IDC_ENCODING_WINDOWS1256        32925
+#define IDC_ENCODING_ISO88598           32926
+#define IDC_ENCODING_WINDOWS1255        32927
+#define IDC_ENCODING_WINDOWS1258        32928
+// Free space 32929
 // Spell-check suggestions.
 // If more suggestions have to be inserted, insert them before the _LAST
 // These have to be consecutive.
