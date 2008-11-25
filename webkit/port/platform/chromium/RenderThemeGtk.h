4bf8f69279a829b518845d7a60a938fd48e3227b
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-25 18:44:00 +0000

    Use the default handling for input styles. Right now we're trying to match the GTK theme, using code with a bunch of FIXMEs because it's not correct.  Let's use the default code path which will style us the same as Windows.
    Review URL: http://codereview.chromium.org/12435
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5980 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/chromium/RenderThemeGtk.h
+++ webkit/port/platform/chromium/RenderThemeGtk.h
@@ -65,7 +65,6 @@ public:
 
     virtual bool paintButton(RenderObject*, const RenderObject::PaintInfo&, const IntRect&);
 
-    virtual void adjustTextFieldStyle(CSSStyleSelector*, RenderStyle*, Element*) const;
     virtual bool paintTextField(RenderObject*, const RenderObject::PaintInfo&, const IntRect&);
 
     virtual void adjustTextAreaStyle(CSSStyleSelector*, RenderStyle*, Element*) const;
