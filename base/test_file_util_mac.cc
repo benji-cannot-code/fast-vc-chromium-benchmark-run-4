c404848e5a10e7ad0812ab4d1bbc10b55dfa34f0
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-25 23:10:46 +0000

    Un-duplicate EvictFileFromSystemCache.
    Change required for http://codereview.chromium.org/9639 .
    
    Review URL: http://codereview.chromium.org/12407
    Patch from Pawel Hajdan Jr.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6006 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/test_file_util_mac.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test_file_util.h"
+
+#include "base/logging.h"
+
+namespace file_util {
+
+bool EvictFileFromSystemCache(const wchar_t* file) {
+  // TODO(port): Implement.
+  NOTIMPLEMENTED();
+  return false;
+}
+
+}  // namespace file_util
--- chrome/tools/perf/flush_cache/flush_cache.cc
+++ chrome/tools/perf/flush_cache/flush_cache.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_piece.h"
 #include "base/process_util.h"
 #include "base/sys_string_conversions.h"
-#include "chrome/test/test_file_util.h"
+#include "base/test_file_util.h"
 
 int main(int argc, const char* argv[]) {
   base::EnableTerminationOnHeapCorruption();
