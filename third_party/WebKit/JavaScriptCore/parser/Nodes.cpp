d0bd9ed1cdcd37e74781d058cda324cd25a6004d
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-25 07:23:44 +0000

    2008-11-24  Cameron Zwarich  <zwarich@apple.com>
            Reviewed by Geoff Garen.
    
            Bug 13790: Function declarations are not treated as statements (used to affect starcraft2.com)
            <https://bugs.webkit.org/show_bug.cgi?id=13790>
    
            Modify the parser to treat function declarations as statements,
            simplifying the grammar in the process. Technically, according to the
            grammar in the ECMA spec, function declarations are not statements and
            can not be used everywhere that statements can, but it is not worth the
            possibility compatibility issues just to stick to the spec in this case.
    
            JavaScriptCore:
    
            * parser/Grammar.y:
            * parser/Nodes.cpp:
            (JSC::FuncDeclNode::emitBytecode): Avoid returning ignoredResult()
            as a result, because it causes a crash in DoWhileNode::emitBytecode().
    
            LayoutTests:
    
            * fast/js/function-declaration-statement-expected.txt: Added.
            * fast/js/function-declaration-statement.html: Added.
            * fast/js/resources/function-declaration-statement.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/parser/Nodes.cpp
+++ third_party/WebKit/JavaScriptCore/parser/Nodes.cpp
@@ -2633,6 +2633,8 @@ JSFunction* FuncDeclNode::makeFunction(ExecState* exec, ScopeChainNode* scopeCha
 
 RegisterID* FuncDeclNode::emitBytecode(BytecodeGenerator&, RegisterID* dst)
 {
+    if (dst == ignoredResult())
+        dst = 0;
     return dst;
 }
 
