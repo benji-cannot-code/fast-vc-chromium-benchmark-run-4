e5d8cd2951f7413f119e00907aa01aa6e0909c5a
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-26 19:51:29 +0000

    Remove dependencies on base/ from MIMETypeRegistry.cpp
    Patch by phajdan.jr@gmail.com
    R=darin
    
    Review URL: http://codereview.chromium.org/12004
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6043 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/MIMETypeRegistry.cpp
+++ webkit/port/platform/MIMETypeRegistry.cpp
@@ -25,14 +25,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+#include "ChromiumBridge.h"
 #include "CString.h"
 #include "MIMETypeRegistry.h"
 #include "MediaPlayer.h"
 #include "StringHash.h"
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
-
-#include "net/base/mime_util.h"
 
 namespace WebCore
 {
@@ -75,7 +74,7 @@ String MIMETypeRegistry::getMIMETypeForPath(const String& path)
 bool MIMETypeRegistry::isSupportedImageMIMEType(const String& mimeType)
 { 
     return !mimeType.isEmpty()
-        && net::IsSupportedImageMimeType(mimeType.latin1().data()); 
+        && ChromiumBridge::isSupportedImageMIMEType(mimeType.latin1().data());
 }
 
 bool MIMETypeRegistry::isSupportedImageMIMETypeForEncoding(const String& mimeType)
@@ -86,8 +85,9 @@ bool MIMETypeRegistry::isSupportedImageMIMETypeForEncoding(const String& mimeTyp
 
 bool MIMETypeRegistry::isSupportedJavaScriptMIMEType(const String& mimeType)
 {
+    const char* data = mimeType.latin1().data();
     return !mimeType.isEmpty()
-        && net::IsSupportedJavascriptMimeType(mimeType.latin1().data()); 
+        && ChromiumBridge::isSupportedJavascriptMIMEType(data);
 }
 
 bool MIMETypeRegistry::isSupportedImageResourceMIMEType(const String& mimeType)
@@ -98,7 +98,7 @@ bool MIMETypeRegistry::isSupportedImageResourceMIMEType(const String& mimeType)
 bool MIMETypeRegistry::isSupportedNonImageMIMEType(const String& mimeType)
 {
    return !mimeType.isEmpty()
-       && net::IsSupportedNonImageMimeType(mimeType.latin1().data()); 
+       && ChromiumBridge::isSupportedNonImageMIMEType(mimeType.latin1().data());
 }
 
 #if ENABLE(VIDEO)
