af4c09498e977a3d5e79f9657dea74d67f80363c
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-26 22:23:33 +0000

    Reviewed by Sam Weinig. Fix obvious problem in WMLPageState constructor, m_page was not set to the passed Page pointer.
    git-svn-id: svn://svn.chromium.org/blink/trunk@38790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/wml/WMLPageState.cpp
+++ third_party/WebKit/WebCore/wml/WMLPageState.cpp
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 WMLPageState::WMLPageState(Page* page)
-    : m_page(0)
+    : m_page(page)
     , m_historyLength(0)
     , m_activeCard(0)
     , m_hasDeckAccess(false)
