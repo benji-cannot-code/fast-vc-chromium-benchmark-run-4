37de3f41efb3a65533ea72901837c01da18a63ad
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-26 19:31:45 +0000

    The autofill menu does not accept the selection on abandon, such as when pressing ESC. I had to introduce another acceptOnAbandon member, though the name I came up with is pretty lame.
    BUG=4803
    TEST=Type some text in a from input text, in the autofill popup menu, use the arrow keys to select something then press ESC. The text input content should not change. Also, make sure the select input still does keep the selection when ESC is pressed.
    Review URL: http://codereview.chromium.org/12667
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6042 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webview_impl.cc
+++ webkit/glue/webview_impl.cc
@@ -432,7 +432,6 @@ bool WebViewImpl::KeyEvent(const WebKeyboardEvent& event) {
 #endif
       return true;
     }
-    return false;
   }
 
   // A new key being pressed should hide the popup.
@@ -1517,6 +1516,7 @@ void WebViewImpl::AutofillSuggestionsForNode(
           WebCore::PopupContainer::create(autocomplete_popup_client_.get(),
                                           false);
       autocomplete_popup_->setTextOnIndexChange(false);
+      autocomplete_popup_->setAcceptOnAbandon(false);
       autocomplete_popup_->show(focused_node->getRect(), 
                                 page_->mainFrame()->view(), 0);
     }
