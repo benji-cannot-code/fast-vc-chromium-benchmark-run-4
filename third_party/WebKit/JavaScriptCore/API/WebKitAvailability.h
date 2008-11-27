3f4d8e7f9516b197d573647f64d6f71f35af29f6
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-27 12:14:09 +0000

    2008-11-27  Alp Toker  <alp@nuanti.com>
            Remove C++ comment from JavaScriptCore API headers (introduced r35449).
            Fixes build for ANSI C applications using the public API.
    
            * API/WebKitAvailability.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38817 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/WebKitAvailability.h
+++ third_party/WebKit/JavaScriptCore/API/WebKitAvailability.h
@@ -44,7 +44,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifdef __APPLE__
 #import <AvailabilityMacros.h>
 #else
-// For non-Mac platforms, require the newest version.
+/*
+ * For non-Mac platforms, require the newest version.
+ */
 #define WEBKIT_VERSION_MIN_REQUIRED WEBKIT_VERSION_LATEST
 /*
  * only certain compilers support __attribute__((deprecated))
