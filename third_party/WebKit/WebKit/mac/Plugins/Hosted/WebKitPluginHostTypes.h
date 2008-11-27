0f99d0a2dca3b2d009a630b35975172fa36ae26b
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-27 01:23:23 +0000

    2008-11-26  Anders Carlsson  <andersca@apple.com>
            Reviewed by Dan Bernstein.
            
            * Plugins/Hosted/WebKitPluginHost.defs:
            Add copyright headers.
            
            * Plugins/Hosted/WebKitPluginHostTypes.h: Added.
            Forgot to add this.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit/mac/Plugins/Hosted/WebKitPluginHostTypes.h
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#ifndef WebKitPluginHostTypes_h
+#define WebKitPluginHostTypes_h
+
+typedef uint8_t * plist_bytes_t;
+typedef uint8_t * application_name_t;
+
+typedef char * data_t;
+
+#endif // WebKitPluginHostTypes_h
