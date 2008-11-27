79ae6b7dfd3bd4eaea2b289fb1070ffb82ef6c8b
alp@webkit.org <alp@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-27 13:07:08 +0000

    2008-11-27  Alp Toker  <alp@nuanti.com>
            Change recently introduced C++ comments in Platform.h to C comments to
            fix the minidom build with traditional C.
    
            Build GtkLauncher and minidom with the '-ansi' compiler flag to detect
            API header breakage at build time.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38819 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -80,7 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined (__S60__) || defined (__SYMBIAN32__)
-// we are cross-compiling, it is not really windows
+/* we are cross-compiling, it is not really windows */
 #undef WTF_PLATFORM_WIN_OS
 #undef WTF_PLATFORM_WIN
 #undef WTF_PLATFORM_CAIRO
@@ -422,12 +422,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_GEOLOCATION 0
 #endif
 
-// CTI only supports x86 at the moment, and has only been tested on Mac and Windows.
+/* CTI only supports x86 at the moment, and has only been tested on Mac and Windows. */
 #if !defined(ENABLE_JIT) && PLATFORM(X86) && (PLATFORM(MAC) || PLATFORM(WIN))
 #define ENABLE_JIT 1
 #endif
 
-// WREC only supports x86 at the moment, and has only been tested on Mac and Windows.
+/* WREC only supports x86 at the moment, and has only been tested on Mac and Windows. */
 #if !defined(ENABLE_WREC) && ENABLE(JIT) && PLATFORM(X86) && (PLATFORM(MAC) || PLATFORM(WIN))
 #define ENABLE_WREC 1
 #endif
@@ -447,7 +447,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif
 
-// Use "fastcall" calling convention on MSVC
+/* Use "fastcall" calling convention on MSVC */
 #if COMPILER(MSVC)
 #define WTF_USE_FAST_CALL_CTI_ARGUMENT 1
 #define WTF_USE_CTI_ARGUMENT 1
