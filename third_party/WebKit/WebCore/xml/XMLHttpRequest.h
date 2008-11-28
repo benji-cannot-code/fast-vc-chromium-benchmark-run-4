f01ab6bea002281a40e7eec067d02fae2078fa90
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-28 10:23:41 +0000

    2008-11-28  David Levin  <levin@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            http://bugs.webkit.org/show_bug.cgi?id=22524
    
            Made static initializations in XMLHttpRequest.cpp thread-safe in preparation for usage
            of XMLHttpRequest by Workers (on threads).
    
            No observable change in behavior, so no test.
    
            * xml/XMLHttpRequest.cpp:
            (WebCore::XMLHttpRequestStaticData::XMLHttpRequestStaticData):
            (WebCore::PreflightResultCacheItem::allowsCrossSiteMethod):
            (WebCore::PreflightResultCacheItem::allowsCrossSiteHeaders):
            (WebCore::PreflightResultCacheItem::allowsRequest):
            (WebCore::createXMLHttpRequestStaticData):
            (WebCore::initializeXMLHttpRequestStaticData):
            (WebCore::XMLHttpRequest::XMLHttpRequest):
            (WebCore::XMLHttpRequest::isSafeRequestHeader):
            (WebCore::XMLHttpRequest::isOnAccessControlResponseHeaderWhitelist):
            * xml/XMLHttpRequest.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/xml/XMLHttpRequest.h
+++ third_party/WebKit/WebCore/xml/XMLHttpRequest.h
@@ -141,6 +141,7 @@ private:
 
     String getRequestHeader(const String& name) const;
     void setRequestHeaderInternal(const String& name, const String& value);
+    bool isSafeRequestHeader(const String&) const;
 
     void changeState(State newState);
     void callReadyStateChangeListener();
@@ -160,6 +161,8 @@ private:
 
     void loadRequestSynchronously(ResourceRequest&, ExceptionCode&);
     void loadRequestAsynchronously(ResourceRequest&);
+
+    bool isOnAccessControlResponseHeaderWhitelist(const String&) const;
 
     bool isSimpleCrossSiteAccessRequest() const;
     String accessControlOrigin() const;
