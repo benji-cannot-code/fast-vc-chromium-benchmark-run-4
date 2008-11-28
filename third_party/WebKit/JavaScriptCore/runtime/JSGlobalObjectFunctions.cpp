3d04932debce2b8465922a086575ebae1c1e5087
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-11-28 15:18:45 +0000

    2008-11-28  Simon Hausmann  <hausmann@webkit.org>
            Rubber-stamped by Tor Arne Vestbø.
    
            Removed unnecessary inclusion of errno.h, which also fixes compilation on Windows CE.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSGlobalObjectFunctions.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSGlobalObjectFunctions.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "dtoa.h"
 #include "Lexer.h"
 #include "Nodes.h"
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
