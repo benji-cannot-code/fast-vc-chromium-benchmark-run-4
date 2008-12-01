e91eddbff63bc920d1a0aa9e3359ac19651bc974
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-01 22:23:29 +0000

    2008-12-01  Antti Koivisto  <antti@apple.com>
            Reviewed by Maciej Stachowiak.
            
            Restore inline buffer after vector is shrunk back below its inline capacity.
    
            * wtf/Vector.h:
            (WTF::):
            (WTF::VectorBuffer::restoreInlineBufferIfNeeded):
            (WTF::::shrinkCapacity):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38875 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Vector.h
+++ third_party/WebKit/JavaScriptCore/wtf/Vector.h
@@ -349,6 +349,8 @@ namespace WTF {
             std::swap(m_buffer, other.m_buffer);
             std::swap(m_capacity, other.m_capacity);
         }
+        
+        void restoreInlineBufferIfNeeded() { }
 
         using Base::allocateBuffer;
         using Base::deallocateBuffer;
@@ -395,6 +397,14 @@ namespace WTF {
             if (bufferToDeallocate == inlineBuffer())
                 return;
             Base::deallocateBuffer(bufferToDeallocate);
+        }
+        
+        void restoreInlineBufferIfNeeded()
+        {
+            if (m_buffer)
+                return;
+            m_buffer = inlineBuffer();
+            m_capacity = inlineCapacity;
         }
 
         using Base::buffer;
@@ -741,6 +751,7 @@ namespace WTF {
         }
 
         m_buffer.deallocateBuffer(oldBuffer);
+        m_buffer.restoreInlineBufferIfNeeded();
     }
 
     // Templatizing these is better than just letting the conversion happen implicitly,
