87295f808b4c44b026aa9db5fe14898a4f47789f
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-01 14:19:35 +0000

    2008-12-01  David Levin  <levin@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            https://bugs.webkit.org/show_bug.cgi?id=22567
            Make HashTable work as expected with respect to threads. Specifically, it has class-level
            thread safety and constant methods work on constant objects without synchronization.
            
            No observable change in behavior, so no test.
    
            * wtf/HashTable.cpp:
            (WTF::hashTableStatsMutex):
            (WTF::HashTableStats::~HashTableStats):
            (WTF::HashTableStats::recordCollisionAtCount):
            Guarded variable access with a mutex.
    
            * wtf/HashTable.h:
            (WTF::::lookup):
            (WTF::::lookupForWriting):
            (WTF::::fullLookupForWriting):
            (WTF::::add):
            (WTF::::reinsert):
            (WTF::::remove):
            (WTF::::rehash):
            Changed increments of static variables to use atomicIncrement.
    
            (WTF::::invalidateIterators):
            (WTF::addIterator):
            (WTF::removeIterator):
            Guarded mutable access with a mutex.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38859 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/HashTable.h
+++ third_party/WebKit/JavaScriptCore/wtf/HashTable.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2005, 2006, 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2008 David Levin <levin@chromium.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FastMalloc.h"
 #include "HashTraits.h"
 #include <wtf/Assertions.h>
+#include <wtf/Threading.h>
 
 namespace WTF {
 
@@ -43,13 +45,19 @@ namespace WTF {
 
     struct HashTableStats {
         ~HashTableStats();
+        // All of the variables are accessed in ~HashTableStats when the static struct is destroyed.
+
+        // The following variables are all atomically incremented when modified.
         static int numAccesses;
-        static int numCollisions;
-        static int collisionGraph[4096];
-        static int maxCollisions;
         static int numRehashes;
         static int numRemoves;
         static int numReinserts;
+
+        // The following variables are only modified in the recordCollisionAtCount method within a mutex.
+        static int maxCollisions;
+        static int numCollisions;
+        static int collisionGraph[4096];
+
         static void recordCollisionAtCount(int count);
     };
 
@@ -202,6 +210,8 @@ namespace WTF {
 
 #if CHECK_HASHTABLE_ITERATORS
     public:
+        // Any modifications of the m_next or m_previous of an iterator that is in a linked list of a HashTable::m_iterator,
+        // should be guarded with m_table->m_mutex.
         mutable const HashTableType* m_table;
         mutable const_iterator* m_next;
         mutable const_iterator* m_previous;
@@ -398,7 +408,9 @@ namespace WTF {
 
 #if CHECK_HASHTABLE_ITERATORS
     public:
+        // All access to m_iterators should be guarded with m_mutex.
         mutable const_iterator* m_iterators;
+        mutable Mutex m_mutex;
 #endif
     };
 
@@ -467,7 +479,7 @@ namespace WTF {
             return 0;
 
 #if DUMP_HASHTABLE_STATS
-        ++HashTableStats::numAccesses;
+        atomicIncrement(&HashTableStats::numAccesses);
         int probeCount = 0;
 #endif
 
@@ -512,7 +524,7 @@ namespace WTF {
         int i = h & sizeMask;
 
 #if DUMP_HASHTABLE_STATS
-        ++HashTableStats::numAccesses;
+        atomicIncrement(&HashTableStats::numAccesses);
         int probeCount = 0;
 #endif
 
@@ -564,7 +576,7 @@ namespace WTF {
         int i = h & sizeMask;
 
 #if DUMP_HASHTABLE_STATS
-        ++HashTableStats::numAccesses;
+        atomicIncrement(&HashTableStats::numAccesses);
         int probeCount = 0;
 #endif
 
@@ -624,7 +636,7 @@ namespace WTF {
         int i = h & sizeMask;
 
 #if DUMP_HASHTABLE_STATS
-        ++HashTableStats::numAccesses;
+        atomicIncrement(&HashTableStats::numAccesses);
         int probeCount = 0;
 #endif
 
@@ -739,7 +751,7 @@ namespace WTF {
         ASSERT(!lookupForWriting(Extractor::extract(entry)).second);
         ASSERT(!isDeletedBucket(*(lookupForWriting(Extractor::extract(entry)).first)));
 #if DUMP_HASHTABLE_STATS
-        ++HashTableStats::numReinserts;
+        atomicIncrement(&HashTableStats::numReinserts);
 #endif
 
         Mover<ValueType, Traits::needsDestruction>::move(entry, *lookupForWriting(Extractor::extract(entry)).first);
@@ -802,7 +814,7 @@ namespace WTF {
     void HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits>::remove(ValueType* pos)
     {
 #if DUMP_HASHTABLE_STATS
-        ++HashTableStats::numRemoves;
+        atomicIncrement(&HashTableStats::numRemoves);
 #endif
 
         deleteBucket(*pos);
@@ -888,7 +900,7 @@ namespace WTF {
 
 #if DUMP_HASHTABLE_STATS
         if (oldTableSize != 0)
-            ++HashTableStats::numRehashes;
+            atomicIncrement(&HashTableStats::numRehashes);
 #endif
 
         m_tableSize = newTableSize;
@@ -1017,6 +1029,7 @@ namespace WTF {
     template<typename Key, typename Value, typename Extractor, typename HashFunctions, typename Traits, typename KeyTraits>
     void HashTable<Key, Value, Extractor, HashFunctions, Traits, KeyTraits>::invalidateIterators()
     {
+        MutexLocker lock(m_mutex);
         const_iterator* next;
         for (const_iterator* p = m_iterators; p; p = next) {
             next = p->m_next;
@@ -1038,6 +1051,7 @@ namespace WTF {
         if (!table) {
             it->m_next = 0;
         } else {
+            MutexLocker lock(table->m_mutex);
             ASSERT(table->m_iterators != it);
             it->m_next = table->m_iterators;
             table->m_iterators = it;
@@ -1059,6 +1073,7 @@ namespace WTF {
             ASSERT(!it->m_next);
             ASSERT(!it->m_previous);
         } else {
+            MutexLocker lock(it->m_table->m_mutex);
             if (it->m_next) {
                 ASSERT(it->m_next->m_previous == it);
                 it->m_next->m_previous = it->m_previous;
