d1a5a063b83af058fef62da219c6f837a0754bfa
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-02 20:25:26 +0000

    Reviewed by Darin Adler.
            Wrap a JSC-only hack in a USE(JSC) block to fix the v8 build.
            https://bugs.webkit.org/show_bug.cgi?id=21951
    
            * svg/SVGElementInstance.cpp:
            (WebCore::SVGElementInstance::forgetWrapper):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38909 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGElementInstance.cpp
+++ third_party/WebKit/WebCore/svg/SVGElementInstance.cpp
@@ -32,11 +32,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EventListener.h"
 #include "EventNames.h"
 #include "FrameView.h"
-#include "GCController.h"
 #include "SVGElementInstanceList.h"
 #include "SVGUseElement.h"
 
 #include <wtf/RefCountedLeakCounter.h>
+
+#if USE(JSC)
+#include "GCController.h"
+#endif
 
 namespace WebCore {
 
@@ -89,6 +92,7 @@ void SVGElementInstance::setShadowTreeElement(SVGElement* element)
 
 void SVGElementInstance::forgetWrapper()
 {
+#if USE(JSC)
     // FIXME: This is fragile, as discussed with Sam. Need to find a better solution.
     // Think about the case where JS explicitely holds "var root = useElement.instanceRoot;".
     // We still have to recreate this wrapper somehow. The gc collection below, won't catch it.
@@ -96,7 +100,8 @@ void SVGElementInstance::forgetWrapper()
     // If the use shadow tree has been rebuilt, just the JSSVGElementInstance objects
     // are still holding RefPtrs of SVGElementInstance objects, which prevent us to
     // be deleted (and the shadow tree is not destructed as well). Force JS GC.
-    gcController().garbageCollectNow();    
+    gcController().garbageCollectNow();
+#endif
 }
 
 void SVGElementInstance::appendChild(PassRefPtr<SVGElementInstance> child)
