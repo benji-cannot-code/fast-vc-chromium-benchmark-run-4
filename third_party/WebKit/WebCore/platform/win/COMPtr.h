e06680678008f3817049bf242413354998d22571
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-02 15:13:44 +0000

    Build fix for Windows projects with NOMINMAX defined globally
            * platform/win/COMPtr.h: Don't redefine NOMINMAX if it's already
            defined.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/COMPtr.h
+++ third_party/WebKit/WebCore/platform/win/COMPtr.h
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPtr_h
 #define COMPtr_h
 
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 
 #include <guiddef.h>
 #include <unknwn.h>
