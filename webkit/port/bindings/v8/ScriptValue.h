d5e8e062f4cb85f1d3906e0a0a33a527aa6c3334
fqian@google.com <fqian@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-03 21:06:11 +0000

    Some cleanup of ScriptValue.
    Review URL: http://codereview.chromium.org/13102
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6310 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/v8/ScriptValue.h
+++ webkit/port/bindings/v8/ScriptValue.h
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "v8.h"
 
+#ifndef NDEBUG
+#include "v8_proxy.h"  // for register and unregister global handles.
+#endif
+
 namespace WebCore {
 
 class String;
@@ -45,12 +49,22 @@ public:
 
     ScriptValue(v8::Handle<v8::Value> value) 
     {
-        m_value = v8::Persistent<v8::Value>::New(value);
+        if (!value.IsEmpty()) {
+            m_value = v8::Persistent<v8::Value>::New(value);
+#ifndef NDEBUG
+            V8Proxy::RegisterGlobalHandle(SCRIPTVALUE, this, m_value);
+#endif
+        }
     }
 
     ScriptValue(const ScriptValue& value) 
     {
-        m_value = v8::Persistent<v8::Value>::New(value.m_value);
+        if (!value.m_value.IsEmpty()) {
+            m_value = v8::Persistent<v8::Value>::New(value.m_value);
+#ifndef NDEBUG
+            V8Proxy::RegisterGlobalHandle(SCRIPTVALUE, this, m_value);
+#endif
+        }
     }
 
     ScriptValue& operator=(const ScriptValue& value) 
@@ -58,15 +72,30 @@ public:
         if (this == &value) 
             return *this;
 
-        m_value.Dispose();
-        m_value = v8::Persistent<v8::Value>::New(value.m_value);
+        clear();
+        if (!value.m_value.IsEmpty()) { 
+            m_value = v8::Persistent<v8::Value>::New(value.m_value);
+#ifndef NDEBUG
+            V8Proxy::RegisterGlobalHandle(SCRIPTVALUE, this, m_value);
+#endif
+        }
+
         return *this;
     }
 
+    void clear() {
+        if (!m_value.IsEmpty()) {
+#ifndef NDEBUG
+            V8Proxy::UnregisterGlobalHandle(this, m_value);
+#endif
+            m_value.Dispose();
+            m_value.Clear();
+        }
+    }
+
     ~ScriptValue() 
-    { 
-        m_value.Dispose();
-        m_value.Clear();
+    {
+        clear();
     }
 
     bool getString(String& result) const;
