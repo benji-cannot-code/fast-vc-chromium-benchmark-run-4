b3314386c90a2e54d148c9d4358988e75787cdd3
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-03 23:48:26 +0000

    Reviewed by George Staikos.
    Fixes: https://bugs.webkit.org/show_bug.cgi?id=22636
    
    Implement WML variable substitution & validation.
    Created a framework for scriptable WML layout tests, that are able to test variable substiution & validation.
    
    Tests: wml/variable-reference-invalid-character.html
           wml/variable-reference-valid.html
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38974 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/wml/WMLVariables.h
+++ third_party/WebKit/WebCore/wml/WMLVariables.h
@@ -35,8 +35,8 @@ namespace WebCore {
         WMLVariableEscapingUnescape
     };
 
-    bool isValidVariableName(const String&, bool isReference);
-    bool containsVariableReference(const String&);
+    bool isValidVariableName(const String&);
+    bool containsVariableReference(const String&, bool& isValid);
 
     String substituteVariableReferences(const String& variableReference,
                                         Document*,
