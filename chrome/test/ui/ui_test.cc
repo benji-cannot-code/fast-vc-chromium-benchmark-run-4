477f15543221076ef68606c8977f961403796859
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-03 00:14:09 +0000

    Add a note about a test that's expected to fail if you're not running crash_service.exe. Review URL: http://codereview.chromium.org/12882
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6254 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/ui_test.cc
+++ chrome/test/ui/ui_test.cc
@@ -33,6 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::TimeTicks;
 
+const wchar_t UITest::kFailedNoCrashService[] =
+    L"NOTE: This test is expected to fail if crash_service.exe is not "
+    L"running. Start it manually before running this test (see the build "
+    L"output directory).";
 bool UITest::in_process_renderer_ = false;
 bool UITest::in_process_plugins_ = false;
 bool UITest::no_sandbox_ = false;
@@ -120,10 +124,10 @@ void UITest::TearDown() {
     file_util::CountFilesCreatedAfter(crash_dump_path, test_start_time_) / 2;
   std::wstring error_msg =
       L"Encountered an unexpected crash in the program during this test.";
-  if (expected_crashes_ > 0 && actual_crashes == 0)
-    error_msg += L"  NOTE: This test is expected to fail if crash_service.exe "
-                 L"is not running. Start it manually before running this "
-                 L"test (see the build output directory).";
+  if (expected_crashes_ > 0 && actual_crashes == 0) {
+    error_msg += L"  ";
+    error_msg += kFailedNoCrashService;
+  }
   EXPECT_EQ(expected_crashes_, actual_crashes) << error_msg;
 }
 
