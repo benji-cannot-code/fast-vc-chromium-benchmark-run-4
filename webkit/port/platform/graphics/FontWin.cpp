4276ee9d42a19b0dbe7f49e9e0951b4501fb8e22
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-03 21:38:42 +0000

    Fix include path in FontWin.cpp Review URL: http://codereview.chromium.org/13106
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6313 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/graphics/FontWin.cpp
+++ webkit/port/platform/graphics/FontWin.cpp
@@ -36,8 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SkiaUtils.h"
 #include "UniscribeHelperTextRun.h"
 
-#include "base/gfx/skia_utils.h"  // TODO(brettw) remove this dependency.
 #include "skia/ext/platform_canvas_win.h"
+#include "skia/ext/skia_utils_win.h"  // TODO(brettw) remove this dependency.
 
 namespace WebCore {
 
