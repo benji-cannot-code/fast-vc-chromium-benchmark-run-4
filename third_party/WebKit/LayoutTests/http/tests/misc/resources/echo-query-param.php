5d83920abc04cea2fc73a3ee4fcf961aa6b3d6ed
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-04 07:27:27 +0000

    Reviewed by Darin Adler.
            Add missing test files (utf-16,32 files) and two php files
            for bug 21635 and enabled utf-7 tests (bug 22492). The patch
            for bug 21635 was landed in http://trac.webkit.org/changeset/38755,
            but binary test files (utf-16/32) were omitted in the changeset.
    
            https://bugs.webkit.org/show_bug.cgi?id=21635
            https://bugs.webkit.org/show_bug.cgi?id=22492
    
            * http/tests/misc/resources/check-query-param.php: Added.
            * http/tests/misc/resources/echo-query-param.php: Added.
            * http/tests/misc/submit-get-in-utf16be.html: Added.
            * http/tests/misc/submit-get-in-utf16le.html: Added.
            * http/tests/misc/submit-get-in-utf32be.html: Added.
            * http/tests/misc/submit-get-in-utf32le.html: Added.
            * http/tests/misc/submit-get-in-utf7.html: Copied from http/tests/misc/submit-get-in-utf7.html-disabled.
            * http/tests/misc/submit-get-in-utf7.html-disabled: Removed.
            * http/tests/misc/submit-post-in-utf16be.html: Added.
            * http/tests/misc/submit-post-in-utf16le.html: Added.
            * http/tests/misc/submit-post-in-utf32be.html: Added.
            * http/tests/misc/submit-post-in-utf32le.html: Added.
            * http/tests/misc/submit-post-in-utf7.html: Copied from http/tests/misc/submit-post-in-utf7.html-disabled.
            * http/tests/misc/submit-post-in-utf7.html-disabled: Removed.
            * http/tests/misc/url-in-utf16be.html: Added.
            * http/tests/misc/url-in-utf16le.html: Added.
            * http/tests/misc/url-in-utf32be.html: Added.
            * http/tests/misc/url-in-utf32le.html: Added.
            * http/tests/misc/url-in-utf7.html: Copied from http/tests/misc/url-in-utf7.html-disabled.
            * http/tests/misc/url-in-utf7.html-disabled: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@38988 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/misc/resources/echo-query-param.php
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+echo "<html><body><div id='output'>";
+echo $_GET{'q'};
+echo "</div></body></html>";
+?>
