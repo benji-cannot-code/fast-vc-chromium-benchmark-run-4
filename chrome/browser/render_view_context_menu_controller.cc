364aee44a4f9ef8cddf3cf992a3cd3a05393be53
sidchat@google.com <sidchat@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-04 18:53:16 +0000

    Add support in spell check options sub-context menu to pop up fonts and languages options menu. Review URL: http://codereview.chromium.org/12908
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6364 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/render_view_context_menu_controller.cc
+++ chrome/browser/render_view_context_menu_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_clipboard_writer.h"
 #include "base/string_util.h"
 #include "chrome/app/chrome_dll_resource.h"
+#include "chrome/browser/views/options/fonts_languages_window_view.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_service.h"
@@ -185,11 +186,11 @@ bool RenderViewContextMenuController::IsCommandEnabled(int id) const {
       return true;
     case IDS_CONTENT_CONTEXT_ADD_TO_DICTIONARY:
       return !params_.misspelled_word.empty();
+    case IDS_CONTENT_CONTEXT_LANGUAGE_SETTINGS:
+      return true;
     case IDS_CONTENT_CONTEXT_VIEWPAGEINFO:
       return (source_web_contents_->controller()->GetActiveEntry() != NULL);
     case IDS_CONTENT_CONTEXT_VIEWFRAMEINFO:
-      return true;
-    case IDS_CONTENT_CONTEXT_LANGUAGE_SETTINGS:
       return true;
     case IDS_CONTENT_CONTEXT_SAVEFRAMEAS:
     case IDS_CONTENT_CONTEXT_PRINTFRAME:
@@ -445,6 +446,14 @@ void RenderViewContextMenuController::ExecuteCommand(int id) {
     case IDS_CONTENT_CONTEXT_ADD_TO_DICTIONARY:
       source_web_contents_->render_view_host()->AddToDictionary(
           params_.misspelled_word);
+      break;
+
+    case IDS_CONTENT_CONTEXT_LANGUAGE_SETTINGS:
+      views::Window::CreateChromeWindow(
+          source_web_contents_->GetContentHWND(),
+          gfx::Rect(),
+          new FontsLanguagesWindowView(
+              source_web_contents_->profile()))->Show();
       break;
 
     case IDS_CONTENT_CONTEXT_ADDSEARCHENGINE:  // Not implemented.
