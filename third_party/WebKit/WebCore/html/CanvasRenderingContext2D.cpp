4327055215cb88f89b9b0cf3aa9fbe16212b656f
brettw@chromium.org <brettw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-04 20:37:36 +0000

    Reviewed by Darin Adler.
    Remove unused Completion.h include.
    
    * html/CanvasRenderingContext2D.cpp: Removed Completion.h include.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39000 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/WebCore/html/CanvasRenderingContext2D.cpp
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SecurityOrigin.h"
 #include "Settings.h"
 #include "TextMetrics.h"
-#include <runtime/Completion.h>
 #include <stdio.h>
 #include <wtf/MathExtras.h>
 
