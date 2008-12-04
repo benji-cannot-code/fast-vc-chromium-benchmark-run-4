085365bf4ca0a01b856c5ecd4b83a07a3c2157b3
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-04 23:28:02 +0000

    Don't cancel the original manual data stream request when the plugin invokes NPN_RequestRead to initiate seekable byte range requests on the stream. While the contract on the plugins part is not fully clear from the mozilla docs, this is what Firefox does.
    This also improves performance greatly as the manual data stream already has most of the data which the plugin requests anyway. I verified this with large PDF files around 14-15MB. We are twice as faster with this fix.
    
    This fixes http://code.google.com/p/chromium/issues/detail?id=5009, which is an issue with the PDF file not loading at times. The problem also occurs at times in Firefox when we set breakpoints and thus slow down the operation.
    The Reader plugin displays its UI in child windows on a separate thread in the plugin process. These are parented to the plugin window which lives on the plugin thread. The plugin thread unfortunately is treated as an IO thread by
    the plugin and it uses the SendMessage API to send messages to the plugin thread and back. If the plugin fails to receive data for the PDF file in a reasonable time,
    it just destroys the PDF window hierarchy causing this issue.
    
    Handing the data off the manual data stream along with the byte range data speeds up the whole operation and thus avoids this issue to a great extent.
    
    Bug=5009
    R=jam
    
    Review URL: http://codereview.chromium.org/12960
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6396 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin.h
+++ webkit/glue/webplugin.h
@@ -130,9 +130,6 @@ class WebPlugin {
                                 bool notify, const char* url,
                                 void* notify_data, bool popups_allowed) = 0;
 
-  // Cancels document load.
-  virtual void CancelDocumentLoad() = 0;
-
   // Initiates a HTTP range request.
   virtual void InitiateHTTPRangeRequest(const char* url,
                                         const char* range_info,
