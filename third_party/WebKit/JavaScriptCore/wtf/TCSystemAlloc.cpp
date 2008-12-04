075e23f6fb6921cc0549e93df2786327b107e80e
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-04 21:01:44 +0000

    2008-12-04  Simon Hausmann  <simon.hausmann@nokia.com>
            Reviewed by Holger Freyther.
    
            Fix crashes in the Qt build on Linux/i386 with non-executable memory
            by enabling TCSystemAlloc and the PROT_EXEC flag for mmap.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/TCSystemAlloc.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/TCSystemAlloc.cpp
@@ -54,7 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if HAVE(MMAP)
 static const int cProtFlags = PROT_READ | PROT_WRITE
-#if ENABLE(ASSEMBLER) && (PLATFORM(GTK) || (PLATFORM(MAC) && PLATFORM(X86_64)))
+#if ENABLE(ASSEMBLER) && (PLATFORM(GTK) || (PLATFORM(MAC) && PLATFORM(X86_64)) || PLATFORM(QT))
                               | PROT_EXEC
 #endif
                               ;
