6e5513e074ceb01e5d984fd6719bba170cefbc0d
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-05 00:32:38 +0000

    * Do not try to update distribution other than Google Chrome.
    BUG=5109
    
    Review URL: http://codereview.chromium.org/13140
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6400 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_update.cc
+++ chrome/browser/google_update.cc
@@ -23,10 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_update_idl_i.c"
 
 namespace {
-// Check if the currently running Chrome can be updated by Google Update by
-// checking if it is running from the standard location. Return true if running
-// from the standard location, otherwise return false.
+// Check if the currently running instance can be updated by Google Update.
+// Returns true only if the instance running is a Google Chrome
+// distribution installed in a standard location.
 bool CanUpdateCurrentChrome(const std::wstring& chrome_exe_path) {
+#if !defined(GOOGLE_CHROME_BUILD)
+  return false;
+#else
   std::wstring user_exe_path = installer::GetChromeInstallPath(false);
   std::wstring machine_exe_path = installer::GetChromeInstallPath(true);
   std::transform(user_exe_path.begin(), user_exe_path.end(),
@@ -43,6 +46,7 @@ bool CanUpdateCurrentChrome(const std::wstring& chrome_exe_path) {
   }
 
   return true;
+#endif
 }
 
 // Creates an instance of a COM Local Server class using either plain vanilla
@@ -320,4 +324,3 @@ bool GoogleUpdate::ReportFailure(HRESULT hr, GoogleUpdateErrorCode error_code,
       &GoogleUpdate::ReportResults, UPGRADE_ERROR, error_code));
   return false;
 }
-
