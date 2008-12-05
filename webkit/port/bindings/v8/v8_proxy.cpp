8f4cb2a9a5c92d385f88260a36b90eeed367683d
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-05 19:31:09 +0000

    Same as CL 12831, create a new one to get rid of the eol-style commit error.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6439 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/v8/v8_proxy.cpp
+++ webkit/port/bindings/v8/v8_proxy.cpp
@@ -99,6 +99,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KeyboardEvent.h"
 #include "Location.h"
 #include "MediaList.h"
+#include "MediaPlayer.h"
 #include "MessageChannel.h"
 #include "MessageEvent.h"
 #include "MessagePort.h"
@@ -2583,8 +2584,7 @@ bool V8Proxy::IsWrapperOfType(v8::Handle<v8::Value> value,
   macro(textarea, TEXTAREA)                      \
   macro(title, TITLE)                            \
   macro(ul, ULIST)                               \
-  macro(xmp, PRE)                                \
-  FOR_EACH_VIDEO_TAG(macro)
+  macro(xmp, PRE)
 
 V8ClassIndex::V8WrapperType V8Proxy::GetHTMLElementType(HTMLElement* element)
 {
@@ -2593,6 +2593,15 @@ V8ClassIndex::V8WrapperType V8Proxy::GetHTMLElementType(HTMLElement* element)
 #define ADD_TO_HASH_MAP(tag, name) \
     map.set(#tag, V8ClassIndex::HTML##name##ELEMENT);
 FOR_EACH_TAG(ADD_TO_HASH_MAP)
+#if ENABLE(VIDEO)
+    // If MediaPlayer::isAvailable() == false, WebCore::videoContructor
+    // creates a HTMLelement object for <video> tag rather than HTMLVideoElement
+    // so only add the mappings if media player is available, or we will get
+    // the wrong element type. Same thing for <audio> and <source>.
+    if (MediaPlayer::isAvailable()) {
+FOR_EACH_VIDEO_TAG(ADD_TO_HASH_MAP)
+    }
+#endif
 #undef ADD_TO_HASH_MAP
   }
 
