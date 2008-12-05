117e3907c20b3c009158efc672b2a3cf434295eb
brettw@chromium.org <brettw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-05 23:22:06 +0000

    Merge branch 'ownptr' into ownptr_commit
    git-svn-id: svn://svn.chromium.org/blink/trunk@39048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSSelector.h
+++ third_party/WebKit/WebCore/css/CSSSelector.h
@@ -26,8 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CSSSelector_h
 
 #include "QualifiedName.h"
-#include "OwnPtr.h"
 #include <wtf/Noncopyable.h>
+#include <wtf/OwnPtr.h>
 
 namespace WebCore {
 
