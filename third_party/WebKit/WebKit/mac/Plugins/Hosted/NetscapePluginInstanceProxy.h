0b403657879af056044a3c54a760e874f2dcea19
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-05 00:10:52 +0000

    ..:
    2008-12-04  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Sam Weinig.
    
            Add HostedNetscapePluginStream.
            
            * WebKit.xcodeproj/project.pbxproj:
    
    ../mac:
    
    2008-12-04  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Sam Weinig.
    
            More work on streams.
            
            * Plugins/Hosted/HostedNetscapePluginStream.h: Added.
            (WebKit::HostedNetscapePluginStream::create):
            * Plugins/Hosted/HostedNetscapePluginStream.mm: Added.
            (WebKit::HostedNetscapePluginStream::HostedNetscapePluginStream):
            (WebKit::HostedNetscapePluginStream::startStreamWithResponse):
            (WebKit::HostedNetscapePluginStream::startStream):
            (WebKit::HostedNetscapePluginStream::didReceiveData):
            (WebKit::HostedNetscapePluginStream::didFinishLoading):
            (WebKit::HostedNetscapePluginStream::didReceiveResponse):
            * Plugins/Hosted/NetscapePluginInstanceProxy.h:
            (WebKit::NetscapePluginInstanceProxy::pluginView):
            (WebKit::NetscapePluginInstanceProxy::hostProxy):
            * Plugins/Hosted/NetscapePluginInstanceProxy.mm:
            (WebKit::NetscapePluginInstanceProxy::PluginRequest::PluginRequest):
            (WebKit::NetscapePluginInstanceProxy::PluginRequest::requestID):
            (WebKit::NetscapePluginInstanceProxy::PluginRequest::request):
            (WebKit::NetscapePluginInstanceProxy::PluginRequest::frameName):
            (WebKit::NetscapePluginInstanceProxy::PluginRequest::didStartFromUserGesture):
            (WebKit::NetscapePluginInstanceProxy::NetscapePluginInstanceProxy):
            (WebKit::NetscapePluginInstanceProxy::performRequest):
            (WebKit::NetscapePluginInstanceProxy::evaluateJavaScript):
            (WebKit::NetscapePluginInstanceProxy::requestTimerFired):
            (WebKit::NetscapePluginInstanceProxy::loadRequest):
            * Plugins/Hosted/WebKitPluginHost.defs:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/Hosted/NetscapePluginInstanceProxy.h
+++ third_party/WebKit/WebKit/mac/Plugins/Hosted/NetscapePluginInstanceProxy.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/RetainPtr.h>
 
 @class WebHostedNetscapePluginView;
-@class WebPluginRequest;
 
 namespace WebKit {
 
@@ -54,7 +53,9 @@ public:
     uint32_t pluginID() const { return m_pluginID; }
     uint32_t renderContextID() const { return m_renderContextID; }
     bool useSoftwareRenderer() const { return m_useSoftwareRenderer; }
-    
+    WebHostedNetscapePluginView *pluginView() const { return m_pluginView; }
+    NetscapePluginHostProxy* hostProxy() const { return m_pluginHostProxy; }
+
     void pluginHostDied();
     
     void resize(NSRect size, NSRect clipRect);
@@ -68,11 +69,14 @@ public:
     void stopTimers();
     
     void status(const char* message);
-    NPError loadURL(const char* url, const char* target, bool post, const char* postData, uint32_t postDataLength, bool postDataIsFile, bool currentEventIsUserGesture, uint32_t& streamID);
+    NPError loadURL(const char* url, const char* target, bool post, const char* postData, uint32_t postDataLength, bool postDataIsFile, bool currentEventIsUserGesture, uint32_t& requestID);
     
 private:
     NPError loadRequest(NSURLRequest *, const char* cTarget, bool currentEventIsUserGesture, uint32_t& streamID);
-    void performRequest(WebPluginRequest*);
+    
+    class PluginRequest;
+    void performRequest(PluginRequest*);
+    void evaluateJavaScript(PluginRequest*);
     
     NetscapePluginInstanceProxy(NetscapePluginHostProxy*, WebHostedNetscapePluginView *, uint32_t pluginID, uint32_t renderContextID, boolean_t useSoftwareRenderer);
 
@@ -81,7 +85,9 @@ private:
 
     void requestTimerFired(WebCore::Timer<NetscapePluginInstanceProxy>*);
     WebCore::Timer<NetscapePluginInstanceProxy> m_requestTimer;
-    Deque<RetainPtr<WebPluginRequest> > m_pluginRequests;
+    Deque<PluginRequest*> m_pluginRequests;
+    
+    uint32_t m_currentRequestID;
     
     uint32_t m_pluginID;
     uint32_t m_renderContextID;
