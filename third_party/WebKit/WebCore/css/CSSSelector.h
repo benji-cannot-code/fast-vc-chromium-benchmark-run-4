bd713e20e8acd3097b459c61a111449ac168eded
brettw@chromium.org <brettw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-05 23:13:10 +0000

    Reviewed by Eric Seidel.
    Add a missing include for OwnPtr to make CSSSelector compile without
    precompiled headers.
    
    * css/CSSSelector.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSSelector.h
+++ third_party/WebKit/WebCore/css/CSSSelector.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CSSSelector_h
 
 #include "QualifiedName.h"
+#include "OwnPtr.h"
 #include <wtf/Noncopyable.h>
 
 namespace WebCore {
