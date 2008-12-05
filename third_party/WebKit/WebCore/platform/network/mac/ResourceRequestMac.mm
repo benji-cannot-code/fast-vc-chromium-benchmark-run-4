68270cf474d2d5512096804becec884ebd6b0ae3
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-05 16:33:52 +0000

    Tiger build fix.
            * platform/network/mac/ResourceRequestMac.mm: Define NSUInteger.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/mac/ResourceRequestMac.mm
+++ third_party/WebKit/WebCore/platform/network/mac/ResourceRequestMac.mm
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#ifdef BUILDING_ON_TIGER
+typedef unsigned NSUInteger;
+#endif
+
 @interface NSURLRequest (WebCoreContentDispositionEncoding)
 - (NSArray *)contentDispositionEncodingFallbackArray;
 @end
