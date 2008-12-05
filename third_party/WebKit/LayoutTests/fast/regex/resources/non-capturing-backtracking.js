4c5d3df93d6ccd28bd694038b37794efa710e33a
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-05 20:56:33 +0000

    2008-12-05  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Sam Weinig.
            
            Added a test for a bug I discovered while working on
            https://bugs.webkit.org/show_bug.cgi?id=22685.
    
            * fast/regex/non-capturing-backtracking-expected.txt: Added.
            * fast/regex/non-capturing-backtracking.html: Added.
            * fast/regex/resources/non-capturing-backtracking.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39040 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/regex/resources/non-capturing-backtracking.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This page tests for proper backtracking with greedy quantifiers and non-capturing parentheses."
+);
+
+var re = /(?:a*)a/;
+shouldBe("re.exec('a')", "['a']");
+
+var successfullyParsed = true;
