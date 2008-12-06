6265afb98a466b744af367866122a2a6cb1c18ba
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-06 00:24:55 +0000

    Revert "Original CL: http://codereview.chromium.org/12831, fix the build error for http://codereview.chromium.org/13181."
    This reverts commit r6463.
    
    Review URL: http://codereview.chromium.org/13225
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6468 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/v8/v8_proxy.cpp
+++ webkit/port/bindings/v8/v8_proxy.cpp
@@ -98,9 +98,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorController.h"
 #include "KeyboardEvent.h"
 #include "Location.h"
-#include "MediaError.h"
 #include "MediaList.h"
-#include "MediaPlayer.h"
 #include "MessageChannel.h"
 #include "MessageEvent.h"
 #include "MessagePort.h"
@@ -130,7 +128,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "StyleSheetList.h"
 #include "TextEvent.h"
 #include "TextMetrics.h"
-#include "TimeRanges.h"
 #include "TreeWalker.h"
 #include "WebKitCSSTransformValue.h"
 #include "XMLHttpRequest.h"
@@ -2586,7 +2583,8 @@ bool V8Proxy::IsWrapperOfType(v8::Handle<v8::Value> value,
   macro(textarea, TEXTAREA)                      \
   macro(title, TITLE)                            \
   macro(ul, ULIST)                               \
-  macro(xmp, PRE)
+  macro(xmp, PRE)                                \
+  FOR_EACH_VIDEO_TAG(macro)
 
 V8ClassIndex::V8WrapperType V8Proxy::GetHTMLElementType(HTMLElement* element)
 {
@@ -2595,15 +2593,6 @@ V8ClassIndex::V8WrapperType V8Proxy::GetHTMLElementType(HTMLElement* element)
 #define ADD_TO_HASH_MAP(tag, name) \
     map.set(#tag, V8ClassIndex::HTML##name##ELEMENT);
 FOR_EACH_TAG(ADD_TO_HASH_MAP)
-#if ENABLE(VIDEO)
-    // If MediaPlayer::isAvailable() == false, WebCore::videoConstructor
-    // created a HTMLElement object for <video> tag rather than HTMLVideoElement
-    // so only add the mappings if the media player is available, or we will get
-    // the wrong element type. Same thing for <audio> and <source>.
-    if (MediaPlayer::isAvailable()) {
-FOR_EACH_VIDEO_TAG(ADD_TO_HASH_MAP)
-    }
-#endif
 #undef ADD_TO_HASH_MAP
   }
 
