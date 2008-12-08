33424c526c7a2b610a8a0c37e52c11835bc4631e
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-08 13:38:51 +0000

    2008-12-08  Trenton Schulz <trenton.schulz@nokia.com>
            Rubber-stamped by Tor Arne Vestbø.
    
            Fix build warning on Mac
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39094 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/mac/StringImplMac.mm
+++ third_party/WebKit/WebCore/platform/text/mac/StringImplMac.mm
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "StringImpl.h"
 
+#include <Foundation/Foundation.h>
+
 namespace WebCore {
 
 StringImpl::operator NSString *()
