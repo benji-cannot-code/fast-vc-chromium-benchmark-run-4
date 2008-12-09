f763532cfcf130d1410ede9eeded5da1436ecec8
cpu@google.com <cpu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-09 23:16:58 +0000

    First run is going into infinite recursion if started from master prefs - keeps creating importer processes - the issue is the creation of the first run sentinel file needs to   happen before starting the new process.
    BUG=1535947
    
    Review URL: http://codereview.chromium.org/13312
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6651 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run.cc
+++ chrome/browser/first_run.cc
@@ -206,6 +206,12 @@ bool FirstRun::ProcessMasterPreferences(
   // required state given that FirstRunView is not going to be called.
   FirstRun::SetShowFirstRunBubblePref();
 
+  // We need to be able to create the first run sentinel or else we cannot
+  // proceed because ImportSettings will launch the importer process which
+  // would end up here if the sentinel is not present.
+  if (!FirstRun::CreateSentinel())
+    return false;
+
   if (parse_result & installer_util::MASTER_PROFILE_SHOW_WELCOME)
     FirstRun::SetShowWelcomePagePref();
 
@@ -223,7 +229,6 @@ bool FirstRun::ProcessMasterPreferences(
     }
   }
 
-  FirstRun::CreateSentinel();
   return false;
 }
 
