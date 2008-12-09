1372d863b2494e5929cabb9d3b4f95857800948e
wtc@google.com <wtc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-09 20:52:58 +0000

    Now we know the delete iter->second call in RemovePendingRequest causes a request to be removed from pending_requests_.  Add the removing_pending_request_ member and some DCHECKS to catch that in action.
    R=eroman
    BUG=4749
    Review URL: http://codereview.chromium.org/13270
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6622 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resource_dispatcher_host.cc
+++ chrome/browser/resource_dispatcher_host.cc
@@ -1430,11 +1430,13 @@ ResourceDispatcherHost::ResourceDispatcherHost(MessageLoop* io_loop)
       request_id_(-1),
       plugin_service_(PluginService::GetInstance()),
       method_runner_(this),
-      is_shutdown_(false) {
+      is_shutdown_(false),
+      removing_pending_request_(0) {
 }
 
 ResourceDispatcherHost::~ResourceDispatcherHost() {
   AsyncEventHandler::GlobalCleanup();
+  CHECK(!removing_pending_request_);
   STLDeleteValues(&pending_requests_);
 }
 
@@ -1467,6 +1469,7 @@ void ResourceDispatcherHost::Shutdown() {
 void ResourceDispatcherHost::OnShutdown() {
   DCHECK(MessageLoop::current() == io_loop_);
   is_shutdown_ = true;
+  CHECK(!removing_pending_request_);
   STLDeleteValues(&pending_requests_);
   // Make sure we shutdown the timer now, otherwise by the time our destructor
   // runs if the timer is still running the Task is deleted twice (once by
@@ -1934,6 +1937,8 @@ void ResourceDispatcherHost::RemovePendingRequest(int render_process_host_id,
 
 void ResourceDispatcherHost::RemovePendingRequest(
     const PendingRequestList::iterator& iter) {
+  CHECK(!removing_pending_request_);
+  removing_pending_request_ = 1;
   size_t num_requests_before = pending_requests_.size();
 
   // Notify the login handler that this request object is going away.
@@ -1952,6 +1957,7 @@ void ResourceDispatcherHost::RemovePendingRequest(
     update_load_states_timer_.Stop();
 
   CHECK(pending_requests_.size() == num_requests_before - 1);
+  removing_pending_request_ = 0;
 }
 
 // URLRequest::Delegate -------------------------------------------------------
