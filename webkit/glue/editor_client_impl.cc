9ca97157cf601144632a9109ffdd5950b7487a9a
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-09 19:22:17 +0000

    Linux: the WebKeyboardEvent key_code field is the windows key code, not the native key code.
    Also match windows behavior for setting m_text field of platform event equal to key_code.
    
    update: fixes at least 7 layout tests.
    
    Review URL: http://codereview.chromium.org/12981
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6607 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/editor_client_impl.cc
+++ webkit/glue/editor_client_impl.cc
@@ -604,7 +604,6 @@ void EditorClientImpl::handleKeyboardEvent(WebCore::KeyboardEvent* evt) {
 }
 
 void EditorClientImpl::handleInputMethodKeydown(WebCore::KeyboardEvent* keyEvent) {
-  NOTIMPLEMENTED();
 }
 
 void EditorClientImpl::textFieldDidBeginEditing(WebCore::Element*) {
