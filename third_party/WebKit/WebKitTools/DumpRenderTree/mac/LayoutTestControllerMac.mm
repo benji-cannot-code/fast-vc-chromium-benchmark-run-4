13e6b14666264432342436015f326eafeeb5f1de
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-09 05:34:57 +0000

    2008-12-08  Stephanie Lewis  <slewis@apple.com>
            Fix Tiger build.
    
            * DumpRenderTree/mac/LayoutTestControllerMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39124 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/LayoutTestControllerMac.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/LayoutTestControllerMac.mm
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebPreferences.h>
 #import <WebKit/WebPreferencesPrivate.h>
 #import <WebKit/WebSecurityOriginPrivate.h>
+#import <WebKit/WebTypesInternal.h>
 #import <WebKit/WebView.h>
 #import <WebKit/WebViewPrivate.h>
 #import <wtf/RetainPtr.h>
