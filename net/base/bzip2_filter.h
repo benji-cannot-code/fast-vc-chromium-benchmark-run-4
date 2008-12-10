d911fd5410e00299ac14d9550cb499882bbb8eb7
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-10 05:53:18 +0000

    no-op style change.
    TBR=wtc@chromium.org
    
    Review URL: http://codereview.chromium.org/13335
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6694 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/bzip2_filter.h
+++ net/base/bzip2_filter.h
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // BZip2Filter is also a subclass of Filter. See the latter's header file filter.h
 // for sample usage.
 
-#ifndef NET_BASE_BZIP2_FILTER_H__
-#define NET_BASE_BZIP2_FILTER_H__
+#ifndef NET_BASE_BZIP2_FILTER_H_
+#define NET_BASE_BZIP2_FILTER_H_
 
 #include "base/scoped_ptr.h"
 #include "net/base/filter.h"
@@ -78,8 +78,8 @@ class BZip2Filter : public Filter {
   // ReadFilteredData.
   scoped_ptr<bz_stream> bzip2_data_stream_;
 
-  DISALLOW_EVIL_CONSTRUCTORS(BZip2Filter);
+  DISALLOW_COPY_AND_ASSIGN(BZip2Filter);
 };
 
-#endif  // NET_BASE_BZIP2_FILTER_H__
+#endif  // NET_BASE_BZIP2_FILTER_H_
 
