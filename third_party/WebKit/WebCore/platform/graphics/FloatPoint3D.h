32f1e9481168b7a4f07be3aeeee1488ff4d08316
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-10 20:57:09 +0000

    2008-12-10  Simon Fraser  <simon.fraser@apple.com>
            Potential build fix. The forward declaration of FloatPoint should
            be inside the WebCore namespace.
    
            * platform/graphics/FloatPoint3D.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39183 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/FloatPoint3D.h
+++ third_party/WebKit/WebCore/platform/graphics/FloatPoint3D.h
@@ -25,9 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(SVG)
 
-class FloatPoint;
-
 namespace WebCore {
+
+class FloatPoint;
 
 class FloatPoint3D {
 public:
