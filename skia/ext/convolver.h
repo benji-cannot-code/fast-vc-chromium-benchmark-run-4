0f88e309508da2da4ca18a54f45954548891c1ec
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-10 15:01:10 +0000

    Remove base dependency from image_operations and associated unittests. I also renamed the namespace from "gfx" to "skia".
    This also adds back the image_operations_unittest to a project (this had gotten lost in my move).
    Review URL: http://codereview.chromium.org/13143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6706 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/convolver.h
+++ skia/ext/convolver.h
@@ -8,14 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "base/basictypes.h"
-
 // avoid confusion with Mac OS X's math library (Carbon)
 #if defined(OS_MACOSX)
 #undef FloatToFixed
 #endif
 
-namespace gfx {
+namespace skia {
 
 // Represents a filter in one dimension. Each output pixel has one entry in this
 // object for the filter values contributing to it. You build up the filter
@@ -34,10 +32,10 @@ class ConvolusionFilter1D {
   }
 
   // Convert between floating point and our fixed point representation.
-  static inline int16 FloatToFixed(float f) {
-    return static_cast<int16>(f * (1 << kShiftBits));
+  static short FloatToFixed(float f) {
+    return static_cast<short>(f * (1 << kShiftBits));
   }
-  static inline unsigned char FixedToChar(int16 x) {
+  static unsigned char FixedToChar(short x) {
     return static_cast<unsigned char>(x >> kShiftBits);
   }
 
@@ -66,7 +64,7 @@ class ConvolusionFilter1D {
 
   // Same as the above version, but the input is already fixed point.
   void AddFilter(int filter_offset,
-                 const int16* filter_values,
+                 const short* filter_values,
                  int filter_length);
 
   // Retrieves a filter for the given |value_offset|, a position in the output
@@ -74,7 +72,7 @@ class ConvolusionFilter1D {
   // filter values are put into the corresponding out arguments (see AddFilter
   // above for what these mean), and a pointer to the first scaling factor is
   // returned. There will be |filter_length| values in this array.
-  inline const int16* FilterForValue(int value_offset,
+  inline const short* FilterForValue(int value_offset,
                                      int* filter_offset,
                                      int* filter_length) const {
     const FilterInstance& filter = filters_[value_offset];
@@ -101,7 +99,7 @@ class ConvolusionFilter1D {
   // We store all the filter values in this flat list, indexed by
   // |FilterInstance.data_location| to avoid the mallocs required for storing
   // each one separately.
-  std::vector<int16> filter_values_;
+  std::vector<short> filter_values_;
 
   // The maximum size of any filter we've added.
   int max_filter_;
@@ -125,14 +123,14 @@ class ConvolusionFilter1D {
 //
 // The layout in memory is assumed to be 4-bytes per pixel in B-G-R-A order
 // (this is ARGB when loaded into 32-bit words on a little-endian machine).
-void BGRAConvolve2D(const uint8* source_data,
+void BGRAConvolve2D(const unsigned char* source_data,
                     int source_byte_row_stride,
                     bool source_has_alpha,
                     const ConvolusionFilter1D& xfilter,
                     const ConvolusionFilter1D& yfilter,
-                    uint8* output);
+                    unsigned char* output);
 
-}  // namespace gfx
+}  // namespace skia
 
 #endif  // SKIA_EXT_CONVOLVER_H_
 
--- skia/ext/image_operations.h
+++ skia/ext/image_operations.h
@@ -6,12 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SKIA_EXT_IMAGE_OPERATIONS_H_
 #define SKIA_EXT_IMAGE_OPERATIONS_H_
 
-#include "base/basictypes.h"
-#include "base/gfx/rect.h"
-
 class SkBitmap;
+struct SkIRect;
 
-namespace gfx {
+namespace skia {
 
 class ImageOperations {
  public:
@@ -38,14 +36,14 @@ class ImageOperations {
   // The destination subset must be smaller than the destination image.
   static SkBitmap Resize(const SkBitmap& source,
                          ResizeMethod method,
-                         const Size& dest_size,
-                         const Rect& dest_subset);
+                         int dest_width, int dest_height,
+                         const SkIRect& dest_subset);
 
   // Alternate version for resizing and returning the entire bitmap rather than
   // a subset.
   static SkBitmap Resize(const SkBitmap& source,
                          ResizeMethod method,
-                         const Size& dest_size);
+                         int dest_width, int dest_height);
 
 
   // Create a bitmap that is a blend of two others. The alpha argument
@@ -58,7 +56,6 @@ class ImageOperations {
   ImageOperations();  // Class for scoping only.
 };
 
-}  // namespace gfx
+}  // namespace skia
 
-#endif  // SKIA_EXT_IMAGE_OPERATIONS_H__
-
+#endif  // SKIA_EXT_IMAGE_OPERATIONS_H_
