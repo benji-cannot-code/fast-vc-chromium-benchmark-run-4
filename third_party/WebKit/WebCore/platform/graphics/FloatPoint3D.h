ecd2075c38232356d6b753320244be1ef2c7f4be
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-10 20:03:47 +0000

    2008-12-10  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Sam Weinig
    
            https://bugs.webkit.org/show_bug.cgi?id=22793
    
            Cleanup FloatPoint3D: inline the getters and setters,
            fix a potential divide-by-zero in normalize(), and add
            a FloatPoint constructor.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/FloatPoint3D.h
+++ third_party/WebKit/WebCore/platform/graphics/FloatPoint3D.h
@@ -25,21 +25,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(SVG)
 
+class FloatPoint;
+
 namespace WebCore {
 
 class FloatPoint3D {
 public:
     FloatPoint3D();
     FloatPoint3D(float x, float y, float z);
+    FloatPoint3D(const FloatPoint&);
 
-    float x() const;
-    void setX(float x);
+    float x() const { return m_x; }
+    void setX(float x) { m_x = x; }
 
-    float y() const;
-    void setY(float y);
+    float y() const { return m_y; }
+    void setY(float y) { m_y = y; }
 
-    float z() const;
-    void setZ(float z);
+    float z() const { return m_z; }
+    void setZ(float z) { m_z = z; }
 
     void normalize();
 
