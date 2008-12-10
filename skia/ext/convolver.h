81beb2223a386dd65b4cda04572d5b8cea8263f1
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-10 15:25:18 +0000

    Use crappy apple define instead of OS_ defines since those don't exist for Skia. Review URL: http://codereview.chromium.org/13709
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6709 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/convolver.h
+++ skia/ext/convolver.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 // avoid confusion with Mac OS X's math library (Carbon)
-#if defined(OS_MACOSX)
+#if defined(__APPLE__)
 #undef FloatToFixed
 #endif
 
