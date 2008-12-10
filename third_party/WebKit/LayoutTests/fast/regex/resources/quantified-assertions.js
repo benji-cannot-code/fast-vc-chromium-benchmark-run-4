c725f0c085829f4f12912bf54d75630adb364b5f
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-10 07:54:04 +0000

    JavaScriptCore:
    2008-12-09  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Oliver Hunt.
    
            Added WREC support for an assertion followed by a quantifier. Fixed
            PCRE to match.
    
            * wrec/WRECParser.cpp:
            (JSC::WREC::Parser::parseParentheses): Throw away the quantifier, since
            it's meaningless. (Firefox does the same.)
    
            * pcre/pcre_compile.cpp:
            (compileBranch): ditto.
    
    LayoutTests:
    
    2008-12-09  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Oliver Hunt.
    
            Added a test for an assertion followed by a quantifier.
    
            * fast/regex/quantified-assertions-expected.txt: Added.
            * fast/regex/quantified-assertions.html: Added.
            * fast/regex/resources/quantified-assertions.js: Copied from fast/regex/resources/alternative-length-miscalculation.js.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39164 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/regex/resources/quantified-assertions.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This page tests assertions followed by quantifiers."
+);
+
+var regexp;
+
+regexp = /(?=a){0}/gm;
+debug("\nTesting regexp: " + regexp);
+shouldBeTrue("regexp.test('a')");
+shouldBe("regexp.lastIndex", "0");
+
+regexp = /(?=a){1}/gm;
+debug("\nTesting regexp: " + regexp);
+shouldBeTrue("regexp.test('a')");
+shouldBe("regexp.lastIndex", "0");
+
+regexp = /(?!a){0}/gm;
+debug("\nTesting regexp: " + regexp);
+shouldBeTrue("regexp.test('b')");
+shouldBe("regexp.lastIndex", "0");
+
+regexp = /(?!a){1}/gm;
+debug("\nTesting regexp: " + regexp);
+shouldBeTrue("regexp.test('b')");
+shouldBe("regexp.lastIndex", "0");
+
+var successfullyParsed = true;
