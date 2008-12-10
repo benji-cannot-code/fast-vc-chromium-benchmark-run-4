d74be2f0b208c5b0665c807f4c80150b13c322dc
finnur@google.com <finnur@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-10 18:53:17 +0000

    Now that we've synced to my WebKit changes I can remove the use of highlevel WebCore types in the scrollbar code. I also found some code that had been ifdef-ed that I missed deleting during my last pass.
    Review URL: http://codereview.chromium.org/13713
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6723 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/chromium/ScrollbarThemeChromium.cpp
+++ webkit/port/platform/chromium/ScrollbarThemeChromium.cpp
@@ -29,9 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ChromiumBridge.h"
 #include "PlatformMouseEvent.h"
-#include "Frame.h"
-#include "FrameView.h"
 #include "Scrollbar.h"
+#include "ScrollbarClient.h"
 #include "ScrollbarThemeComposite.h"
 
 // -----------------------------------------------------------------------------
@@ -123,16 +122,10 @@ void ScrollbarThemeChromium::paintTickmarks(GraphicsContext* context, Scrollbar*
     if (rect.height() <= 0 || rect.width() <= 0)
         return;  // nothing to draw on.
 
-    // Get the frameview.
-    // FIXME: Stop relying on high-level WebCore types such as Frame and FrameView.
-    if (!scrollbar->parent()->isFrameView())
-        return;
-    FrameView* frameView = static_cast<FrameView*>(scrollbar->parent());
-    Document* doc = frameView->frame()->document();
-
-    // Get the text markers for the frameview.
-    Vector<IntRect> markers = doc->renderedRectsForMarkers(DocumentMarker::TextMatch);
-    if (!markers.size())
+    // Get the tickmarks for the frameview.
+    Vector<IntRect> tickmarks;
+    scrollbar->client()->getTickmarks(tickmarks);
+    if (!tickmarks.size())
         return;
 
     // Get the image for the tickmarks.
@@ -144,7 +137,7 @@ void ScrollbarThemeChromium::paintTickmarks(GraphicsContext* context, Scrollbar*
 
     context->save();
 
-    for (Vector<IntRect>::const_iterator i = markers.begin(); i != markers.end(); ++i) {
+    for (Vector<IntRect>::const_iterator i = tickmarks.begin(); i != tickmarks.end(); ++i) {
         // Calculate how far down (in %) the tick-mark should appear.
         const float percent = static_cast<float>(i->y()) / scrollbar->totalSize();
 
