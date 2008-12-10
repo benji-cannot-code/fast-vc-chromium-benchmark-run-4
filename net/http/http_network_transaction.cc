1939f998ee9b4770cb3ba3b7a496dcad5dec8de2
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-10 09:03:15 +0000

    Add a ProxyScriptFetcher class for doing asynch downloads of PAC scripts.This object will be owned by ProxyService. It will be used to manage the fetching of PAC scripts (on the IO thread, using the primary URLRequestContext).BUG=74,2764 (partial) Review URL: http://codereview.chromium.org/13251
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6699 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction.cc
+++ net/http/http_network_transaction.cc
@@ -391,6 +391,11 @@ int HttpNetworkTransaction::DoResolveProxy() {
 
   next_state_ = STATE_RESOLVE_PROXY_COMPLETE;
 
+  if (request_->load_flags & LOAD_BYPASS_PROXY) {
+    proxy_info_.UseDirect();
+    return OK;
+  }
+
   return session_->proxy_service()->ResolveProxy(
       request_->url, &proxy_info_, &io_callback_, &pac_request_);
 }
@@ -1027,6 +1032,10 @@ int HttpNetworkTransaction::ReconsiderProxyAfterError(int error) {
       break;
     default:
       return error;
+  }
+
+  if (request_->load_flags & LOAD_BYPASS_PROXY) {
+    return error;
   }
 
   int rv = session_->proxy_service()->ReconsiderProxyAfterError(
