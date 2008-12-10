089fd66e6d7b6e738117d50e1bb2826de431e405
pam@chromium.org <pam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-10 01:35:03 +0000

    2008-12-09  David Levin  <levin@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=22512
    
            Add a test to check for checks when attaching an event to a document created by
            'document.implementation.createDocument'.
    
            * fast/events/create-document-crash-on-attach-event-expected.txt: Added.
            * fast/events/create-document-crash-on-attach-event.html: Added.
            * fast/events/resources/create-document-crash-on-attach-event.js: Added.
            (doc.onload):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39159 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/events/resources/create-document-crash-on-attach-event.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('This test checks for crashes in setting an event handler on a document element created by '
+            + 'document.implementation.createDocument.');
+
+var doc = document.implementation.createDocument('', '', null);
+doc.onload = function() { };
+testPassed('Attached onload event handler to created document.');
+
+var successfullyParsed = true;
