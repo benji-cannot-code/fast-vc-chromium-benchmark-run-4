167b8d353762b9df66f3590500d9fb7299ef119c
jungshik@google.com <jungshik@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-11 20:30:49 +0000

    A follow-up to a recent unforking of WebCore/platform/text/TextCodecICU.cpp (r6540). Up to now, we canonicalized to EUC-KR, TIS-620 and GB2312, but with unforking, we'd better canonicalize to windows-949, windows-874 and GBK.
    Another change is necessary in Webkit (Document.cpp), which I'll do it while unforking it. 
    
    BUG=3703
    
    Review URL: http://codereview.chromium.org/13645
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6817 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_dll_resource.h
+++ chrome/app/chrome_dll_resource.h
@@ -96,7 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_ENCODING_UTF16LE            35008
 #define IDC_ENCODING_ISO88591           35009
 #define IDC_ENCODING_WINDOWS1252        35010
-#define IDC_ENCODING_GB2312             35011
+#define IDC_ENCODING_GBK                35011
 #define IDC_ENCODING_GB18030            35012
 #define IDC_ENCODING_BIG5               35013
 #define IDC_ENCODING_BIG5HKSCS          35014
