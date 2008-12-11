cfb070be934974bdf0d4dc24fa044f5e411003ba
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-11 06:27:36 +0000

    2008-12-10  Cameron Zwarich  <zwarich@apple.com>
            Reviewed by Oliver Hunt.
    
            Bug 22734: Debugger crashes when stepping into a function call in a return statement
            <https://bugs.webkit.org/show_bug.cgi?id=22734>
            <rdar://problem/6426796>
    
            * bytecompiler/BytecodeGenerator.cpp:
            (JSC::BytecodeGenerator::BytecodeGenerator): The DebuggerCallFrame uses
            the 'this' value stored in a callFrame, so op_convert_this should be
            emitted at the beginning of a function body when generating bytecode
            with debug hooks.
            * debugger/DebuggerCallFrame.cpp:
            (JSC::DebuggerCallFrame::thisObject): The assertion inherent in the call
            to asObject() here is valid, because any 'this' value should have been
            converted to a JSObject*.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/debugger/DebuggerCallFrame.cpp
+++ third_party/WebKit/JavaScriptCore/debugger/DebuggerCallFrame.cpp
@@ -61,7 +61,6 @@ JSObject* DebuggerCallFrame::thisObject() const
     if (!m_callFrame->codeBlock())
         return 0;
 
-    // FIXME: Why is it safe to assume this is an object?
     return asObject(m_callFrame->thisValue());
 }
 
