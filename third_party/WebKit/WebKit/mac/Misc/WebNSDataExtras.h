5aa0508e86468cb3759ff0aca223fcccec67eb75
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-11 21:27:39 +0000

    2008-12-11  Anders Carlsson  <andersca@apple.com>
            Fix Tiger build.
    
            * Misc/WebNSDataExtras.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39216 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Misc/WebNSDataExtras.h
+++ third_party/WebKit/WebKit/mac/Misc/WebNSDataExtras.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import "WebTypesInternal.h"
+
 #define WEB_GUESS_MIME_TYPE_PEEK_LENGTH 1024
 
 @interface NSData (WebNSDataExtras)
