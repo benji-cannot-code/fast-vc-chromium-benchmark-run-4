afbdde7c3685997468e823cfe5feb155ebfa4912
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-11 19:55:18 +0000

    Improve sleep resolution in process_util_linux.
    Review URL: http://codereview.chromium.org/13750
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6814 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_linux.cc
+++ base/process_util_linux.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_util.h"
 #include "base/logging.h"
+#include "base/platform_thread.h"
 #include "base/string_tokenizer.h"
 #include "base/string_util.h"
 #include "base/time.h"
@@ -258,8 +259,7 @@ bool WaitForProcessesToExit(const std::wstring& executable_name,
       result = true;
       break;
     }
-    // TODO(port): Improve resolution
-    sleep(1);
+    PlatformThread::Sleep(100);
   } while ((base::Time::Now() - end_time) > base::TimeDelta());
 
   return result;
