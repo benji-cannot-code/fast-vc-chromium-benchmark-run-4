6fcd5928a61284f65136c81d2e0ee457aeb1d253
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-11 20:14:13 +0000

    Reverting 6815. Review URL: http://codereview.chromium.org/14006
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6816 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test_file_util_linux.cc
+++ base/test_file_util_linux.cc
@@ -13,8 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace file_util {
 
-bool EvictFileFromSystemCache(const FilePath& file) {
-  int fd = open(file.value().c_str(), O_RDONLY);
+bool EvictFileFromSystemCache(const wchar_t* file) {
+  FilePath fpath = FilePath::FromWStringHack(file);
+  int fd = open(fpath.value().c_str(), O_RDONLY);
   if (fd < 0)
     return false;
   if (fdatasync(fd) != 0)
--- chrome/browser/visitedlink_perftest.cc
+++ chrome/browser/visitedlink_perftest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/perftimer.h"
 #include "base/shared_memory.h"
@@ -145,8 +144,7 @@ TEST_F(VisitedLink, TestLoad) {
   for (int i = 0; i < load_count; i++)
   {
     // make sure the file has to be re-loaded
-    file_util::EvictFileFromSystemCache(
-        FilePath::FromWStringHack(std::wstring(db_name_)));
+    file_util::EvictFileFromSystemCache(db_name_.c_str());
 
     // cold load (no OS cache, hopefully)
     {
