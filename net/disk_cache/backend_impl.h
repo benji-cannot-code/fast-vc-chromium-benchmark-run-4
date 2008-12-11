baf90ebbb09dd8f558903624606ce488ccb63820
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-11 23:49:43 +0000

    Disk cache: Set up an experiment to measure the effect of increasing the maximum cache size.
    There will be three groups on the experiment, each group with
    10% of the users (Dev Channel). Each group will have the max
    cache size increased by a factor from 2 to 4 times the current
    size, and we'll measure the age of evicted entries.
    
    I'm also adding a few metrics of reliability, to detect how
    often we're not getting to do proper cleanup.
    
    Review URL: http://codereview.chromium.org/14013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6847 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_impl.h
+++ net/disk_cache/backend_impl.h
@@ -100,6 +100,9 @@ class BackendImpl : public Backend {
   // Reports a critical error (and disables the cache).
   void CriticalError(int error);
 
+  // Reports an uncommon, recoverable error.
+  void ReportError(int error);
+
   // Called when an interesting event should be logged (counted).
   void OnEvent(Stats::Counters an_event);
 
@@ -134,6 +137,7 @@ class BackendImpl : public Backend {
 
   // Deletes the cache and starts again.
   void RestartCache();
+  void PrepareForRestart();
 
   // Creates a new entry object and checks to see if it is dirty. Returns zero
   // on success, or a disk_cache error on failure.
@@ -154,6 +158,7 @@ class BackendImpl : public Backend {
   // If empty is true, the whole cache will be trimmed, regardless of being in
   // use.
   void TrimCache(bool empty);
+  void ReportTrimTimes(EntryImpl* entry);
   
   // Handles the used storage count.
   void AddStorageSize(int32 bytes);
