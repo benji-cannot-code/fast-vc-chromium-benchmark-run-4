df01a019efaab46b04f92fe20efffff0bdff602a
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-11 00:36:47 +0000

    Build ipc_tests on linux.
    Review URL: http://codereview.chromium.org/13366
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6762 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/ipc_channel_posix.cc
+++ chrome/common/ipc_channel_posix.cc
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/ipc_channel_posix.h"
 
+#include <errno.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <stddef.h>
-#include <sys/un.h>
 
 #include "base/logging.h"
 #include "base/process_util.h"
@@ -18,6 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "chrome/common/chrome_counters.h"
 #include "chrome/common/ipc_message_utils.h"
+
+#if defined(OS_LINUX)
+#include <linux/un.h>
+#elif defined(OS_MACOSX)
+#include <sys/un.h>
+#endif
 
 namespace IPC {
 
