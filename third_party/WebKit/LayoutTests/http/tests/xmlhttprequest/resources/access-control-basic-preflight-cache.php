0e6cc307ff9b32b77ebd1dd4cfbf708d21d64a60
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-11 10:20:57 +0000

    2008-12-08  Dean McNamee  <deanm@chromium.org>
            Reviewed by Darin Adler.
    
            Always append a trailing slash when using PHP's sys_get_temp_dir().
    
            This allows these tests to run on Linux, where this API returns the
            directory without a trailing slash.
    
            * http/tests/xmlhttprequest/resources/access-control-basic-preflight-cache-invalidation.php:
            * http/tests/xmlhttprequest/resources/access-control-basic-preflight-cache-timeout.php:
            * http/tests/xmlhttprequest/resources/access-control-basic-preflight-cache.php:
            * http/tests/xmlhttprequest/resources/portabilityLayer.php:
            * http/tests/xmlhttprequest/resources/reset-temp-file.php:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/resources/access-control-basic-preflight-cache.php
+++ third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/resources/access-control-basic-preflight-cache.php
@@ -2,8 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
 require_once 'portabilityLayer.php';
 
-$tempDir = sys_get_temp_dir();
-$tmpFile = $tempDir . $_GET['filename'];
+$tmpFile = sys_get_temp_dir() . '/' . $_GET['filename'];
 
 function fail()
 {
--- third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/resources/reset-temp-file.php
+++ third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/resources/reset-temp-file.php
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
 require_once 'portabilityLayer.php';
 
-$tempDir = sys_get_temp_dir();
-$tmpFile = $tempDir . $_GET['filename'];
+$tmpFile = sys_get_temp_dir() . '/' . $_GET['filename'];
 unlink($tmpFile)
 ?>
