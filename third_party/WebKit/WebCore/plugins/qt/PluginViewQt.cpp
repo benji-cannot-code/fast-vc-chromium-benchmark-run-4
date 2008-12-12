684f412375f72fe1b285fab3653191786b3d05c5
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-12 13:18:30 +0000

    2008-12-12  Tor Arne Vestbø  <tavestbo@trolltech.com>
            Rubber-stamped by Oliver Hunt.
    
            Share PluginView::paintMissingPluginIcon() between ports
    
            Also, enable this feature for Qt/X11, Qt/Mac and GTK
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39238 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/qt/PluginViewQt.cpp
+++ third_party/WebKit/WebCore/plugins/qt/PluginViewQt.cpp
@@ -129,8 +129,7 @@ void PluginView::hide()
 void PluginView::paint(GraphicsContext* context, const IntRect& rect)
 {
     if (!m_isStarted) {
-        // Draw the "missing plugin" image
-        //paintMissingPluginIcon(context, rect);
+        paintMissingPluginIcon(context, rect);
         return;
     }
 
