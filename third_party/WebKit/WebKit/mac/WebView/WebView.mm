7de839fb094015e6d8532798bf0738b5141266d7
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-12 01:15:17 +0000

    2008-12-11  Stephanie Lewis  <slewis@apple.com>
            Fix build.
    
            * WebView/WebView.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39224 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "DOMRangeInternal.h"
 #import "WebBackForwardListInternal.h"
+#import "WebCache.h"
 #import "WebChromeClient.h"
 #import "WebContextMenuClient.h"
 #import "WebDOMOperationsPrivate.h"
