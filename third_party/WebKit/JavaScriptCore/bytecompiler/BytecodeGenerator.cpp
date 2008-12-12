351f03ef05f39aa07d24f7c8815b535bcc4e3cf6
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-12 22:48:53 +0000

    2008-12-12  Cameron Zwarich  <zwarich@apple.com>
            Reviewed by Sam Weinig.
    
            Bug 22828: Do not inspect bytecode instruction stream for op_get_by_id exception information
            <https://bugs.webkit.org/show_bug.cgi?id=22828>
    
            In order to remove the bytecode instruction stream after generating
            native code, all inspection of bytecode instructions at runtime must
            be removed. One particular instance of this is the special handling of
            exceptions thrown by the op_get_by_id emitted directly before an
            op_construct or an op_instanceof. This patch moves that information to
            an auxiliary data structure in CodeBlock.
    
            * bytecode/CodeBlock.cpp:
            (JSC::CodeBlock::getByIdExceptionInfoForBytecodeOffset):
            * bytecode/CodeBlock.h:
            (JSC::CodeBlock::addGetByIdExceptionInfo):
            * bytecompiler/BytecodeGenerator.cpp:
            (JSC::BytecodeGenerator::emitConstruct):
            * bytecompiler/BytecodeGenerator.h:
            (JSC::BytecodeGenerator::emitGetByIdExceptionInfo):
            * parser/Nodes.cpp:
            (JSC::InstanceOfNode::emitBytecode):
            * runtime/ExceptionHelpers.cpp:
            (JSC::createNotAnObjectError):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
+++ third_party/WebKit/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
@@ -1334,6 +1334,7 @@ RegisterID* BytecodeGenerator::emitConstruct(RegisterID* dst, RegisterID* func, 
 
     // Load prototype.
     emitExpressionInfo(divot, startOffset, endOffset);
+    emitGetByIdExceptionInfo(op_construct);
     emitGetById(funcProto.get(), func, globalData()->propertyNames->prototype);
 
     // Reserve space for call frame.
