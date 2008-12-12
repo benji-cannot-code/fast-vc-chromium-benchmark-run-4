663d8292bc96dbb737574d91d172072ce458b8bf
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-12 19:09:47 +0000

    message_pump_libevent refactor: * Unify WatchSocket & WatchFileHandle. * Better encapsulate libevent. * Fix a bug with blocking writes in ipc_posix.cc
    Review URL: http://codereview.chromium.org/13757
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6911 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/listen_socket.h
+++ net/base/listen_socket.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_BASE_SOCKET_H_
 #define NET_BASE_SOCKET_H_
 
+#include <string>
 #if defined(OS_WIN)
 #include <winsock2.h>
 #include "base/object_watcher.h"
@@ -19,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "net/base/net_util.h"
 #include "net/base/net_errors.h"
-#include "third_party/libevent/event.h"
-#include "base/message_pump_libevent.h"
 #endif
 
 #include "base/basictypes.h"
@@ -36,7 +35,7 @@ class ListenSocket : public base::RefCountedThreadSafe<ListenSocket>,
 #if defined(OS_WIN)
                      public base::ObjectWatcher::Delegate
 #elif defined(OS_POSIX)
-                     public base::MessagePumpLibevent::Watcher
+                     public MessageLoopForIO::Watcher
 #endif
 {
  public:
@@ -81,11 +80,11 @@ class ListenSocket : public base::RefCountedThreadSafe<ListenSocket>,
     NOT_WAITING      = 0,
     WAITING_ACCEPT   = 1,
     WAITING_READ     = 3,
-    WAITING_CLOSE    = 4	
+    WAITING_CLOSE    = 4
   };
-  // Pass any value in case of Windows, because in Windows 
-  // we are not using state. 
-  void WatchSocket(WaitState state); 
+  // Pass any value in case of Windows, because in Windows
+  // we are not using state.
+  void WatchSocket(WaitState state);
   void UnwatchSocket();
 
 #if defined(OS_WIN)
@@ -96,17 +95,17 @@ class ListenSocket : public base::RefCountedThreadSafe<ListenSocket>,
 #elif defined(OS_POSIX)
   WaitState wait_state_;
   // The socket's libevent wrapper
-  scoped_ptr<event> event_;
+  MessageLoopForIO::FileDescriptorWatcher watcher_;
   // Called by MessagePumpLibevent when the socket is ready to do I/O
-  void OnSocketReady(short flags);
+  void OnFileCanReadWithoutBlocking(int fd);
+  void OnFileCanWriteWithoutBlocking(int fd);
 #endif
 
-  SOCKET socket_; 
+  SOCKET socket_;
   ListenSocketDelegate *socket_delegate_;
 
  private:
   DISALLOW_EVIL_CONSTRUCTORS(ListenSocket);
 };
 
-#endif // NET_BASE_SOCKET_H_
-
+#endif  // NET_BASE_SOCKET_H_
