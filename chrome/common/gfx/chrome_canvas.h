f512fce97585a67d9dfdb40a8a34081a0ed98c3f
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-12 01:52:03 +0000

    ...
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6869 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/gfx/chrome_canvas.h
+++ chrome/common/gfx/chrome_canvas.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_COMMON_GFX_CHROME_CANVAS_H_
 #define CHROME_COMMON_GFX_CHROME_CANVAS_H_
 
-#include <windows.h>
 #include <string>
 
 #include "base/basictypes.h"
