a2be846c9ac26fa151c0836ef17769f9fb5bc08a
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-12 12:00:40 +0000

    Port flush_cache tool and perf_tests.
    BUG=4160,4263
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=6815
    Review URL: http://codereview.chromium.org/9639
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6886 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test_file_util_mac.cc
+++ base/test_file_util_mac.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace file_util {
 
-bool EvictFileFromSystemCache(const wchar_t* file) {
+bool EvictFileFromSystemCache(const FilePath& file) {
   // TODO(port): Implement.
   NOTIMPLEMENTED();
   return false;
