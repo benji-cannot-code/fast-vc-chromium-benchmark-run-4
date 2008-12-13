b1884e4792d98153404373fdd60b0df78691467b
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-13 01:47:34 +0000

    2008-12-12  Cameron Zwarich  <zwarich@apple.com>
            Reviewed by Oliver Hunt.
    
            Bug 22835: Crash during bytecode generation when comparing to null
            <https://bugs.webkit.org/show_bug.cgi?id=22835>
            <rdar://problem/6286749>
    
            Change the special cases in bytecode generation for comparison to null
            to use tempDestination().
    
            JavaScriptCore:
    
            * parser/Nodes.cpp:
            (JSC::BinaryOpNode::emitBytecode):
            (JSC::EqualNode::emitBytecode):
    
            LayoutTests:
    
            * fast/js/ignored-result-null-comparison-crash-expected.txt: Added.
            * fast/js/ignored-result-null-comparison-crash.html: Added.
            * fast/js/resources/ignored-result-null-comparison-crash.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39263 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/ignored-result-null-comparison-crash.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This tests that bytecode generation doesn't crash on a comparison to null with an ignored result."
+);
+
+function equalToNullTest(a)
+{
+    a == null;
+    return true;
+}
+
+shouldBeTrue("equalToNullTest()");
+
+function notEqualToNullTest(a)
+{
+    a != null;
+    return true;
+}
+
+shouldBeTrue("notEqualToNullTest()");
+
+var successfullyParsed = true;
