678bd96dd50e19d182125be74984cc89b422bc66
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-13 06:06:33 +0000

    Update Windows cache sizes to match recent Mac change.
            Reviewed by Stephanie Lewis, Sam Weinig.
    
            * WebView.cpp:
            (WebView::setCacheModel):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -417,14 +417,12 @@ void WebView::setCacheModel(WebCacheModel cacheModel)
 
         // Object cache capacities (in bytes)
         if (memSize >= 2048)
-            cacheTotalCapacity = 128 * 1024 * 1024;
+            cacheTotalCapacity = 96 * 1024 * 1024;
         else if (memSize >= 1536)
-            cacheTotalCapacity = 86 * 1024 * 1024;
-        else if (memSize >= 1024)
             cacheTotalCapacity = 64 * 1024 * 1024;
-        else if (memSize >= 512)
+        else if (memSize >= 1024)
             cacheTotalCapacity = 32 * 1024 * 1024;
-        else if (memSize >= 256)
+        else if (memSize >= 512)
             cacheTotalCapacity = 16 * 1024 * 1024; 
 
         cacheMinDeadCapacity = 0;
@@ -451,14 +449,12 @@ void WebView::setCacheModel(WebCacheModel cacheModel)
 
         // Object cache capacities (in bytes)
         if (memSize >= 2048)
-            cacheTotalCapacity = 128 * 1024 * 1024;
+            cacheTotalCapacity = 96 * 1024 * 1024;
         else if (memSize >= 1536)
-            cacheTotalCapacity = 86 * 1024 * 1024;
-        else if (memSize >= 1024)
             cacheTotalCapacity = 64 * 1024 * 1024;
-        else if (memSize >= 512)
+        else if (memSize >= 1024)
             cacheTotalCapacity = 32 * 1024 * 1024;
-        else if (memSize >= 256)
+        else if (memSize >= 512)
             cacheTotalCapacity = 16 * 1024 * 1024; 
 
         cacheMinDeadCapacity = cacheTotalCapacity / 8;
@@ -505,14 +501,12 @@ void WebView::setCacheModel(WebCacheModel cacheModel)
         // browsing pattern. Even growth above 128MB can have substantial 
         // value / MB for some content / browsing patterns.)
         if (memSize >= 2048)
-            cacheTotalCapacity = 256 * 1024 * 1024;
-        else if (memSize >= 1536)
-            cacheTotalCapacity = 172 * 1024 * 1024;
-        else if (memSize >= 1024)
             cacheTotalCapacity = 128 * 1024 * 1024;
-        else if (memSize >= 512)
+        else if (memSize >= 1536)
+            cacheTotalCapacity = 96 * 1024 * 1024;
+        else if (memSize >= 1024)
             cacheTotalCapacity = 64 * 1024 * 1024;
-        else if (memSize >= 256)
+        else if (memSize >= 512)
             cacheTotalCapacity = 32 * 1024 * 1024; 
 
         cacheMinDeadCapacity = cacheTotalCapacity / 4;
