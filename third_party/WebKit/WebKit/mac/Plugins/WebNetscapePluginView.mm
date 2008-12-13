a4e0e5f8074479d43538530272931e98b367ddfb
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-13 00:01:15 +0000

    2008-12-12  Anders Carlsson  <andersca@apple.com>
            Reviewed by Tim Hatcher.
    
            Don't try to free the dummy "1" port state.
            
            * Plugins/WebNetscapePluginView.mm:
            (-[WebNetscapePluginView sendEvent:isDrawRect:]):
            (-[WebNetscapePluginView updateAndSetWindow]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39257 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginView.mm
+++ third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginView.mm
@@ -664,7 +664,8 @@ static inline void getNPRect(const NSRect& nr, NPRect& npr)
     if (portState) {
         if ([self currentWindow])
             [self restorePortState:portState];
-        free(portState);
+        if (portState != (PortState)1)
+            free(portState);
     }
 
     if (!wasDeferring)
@@ -918,7 +919,8 @@ static inline void getNPRect(const NSRect& nr, NPRect& npr)
     if (portState) {
         [self setWindowIfNecessary];
         [self restorePortState:portState];
-        free(portState);
+        if (portState != (PortState)1)
+            free(portState);
     }   
     if (didLockFocus)
         [self unlockFocus];
