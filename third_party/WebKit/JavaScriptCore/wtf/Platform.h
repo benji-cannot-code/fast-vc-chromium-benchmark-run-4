9e59fa55d872627f3d56914dc75695e4c5339e2e
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-14 07:19:40 +0000

    2008-12-13  Gavin Barraclough  <barraclough@apple.com>
            Reviewed by Cameron Zwarich.
    
            Re-enable WREC on 64-bit.
            Implements one of the MacroAssembler::jnzPtr methods, previously only implemented for 32-bit x86.
    
            https://bugs.webkit.org/show_bug.cgi?id=22849
    
            * assembler/MacroAssembler.h:
            (JSC::MacroAssembler::testImm64):
            (JSC::MacroAssembler::jnzPtr):
            * assembler/X86Assembler.h:
            (JSC::X86Assembler::testq_i32r):
            (JSC::X86Assembler::testq_rr):
            * wtf/Platform.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39287 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -432,8 +432,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_JIT_OPTIMIZE_ARITHMETIC 1
 #endif
 
-/* WREC only supports x86 at the moment, and has only been tested on Mac and Windows. */
-#if !defined(ENABLE_WREC) && PLATFORM(X86) && (PLATFORM(MAC) || PLATFORM(WIN))
+/* WREC supports x86 & x86-64, and has been tested on Mac and Windows ('cept on 64-bit on Mac). */
+#if (!defined(ENABLE_WREC) && PLATFORM(X86) && PLATFORM(MAC)) \
+ || (!defined(ENABLE_WREC) && PLATFORM(X86_64) && PLATFORM(MAC)) \
+ || (!defined(ENABLE_WREC) && PLATFORM(X86) && PLATFORM(WIN))
 #define ENABLE_WREC 1
 #endif
 
