35ac2c17ab7f28b0d8c674b58aaf8baf43284de0
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-15 12:03:41 +0000

    Reviewed by Oliver Hunt.
            <rdar://problem/6444455> Worker Thread crash running multiple workers for a moderate amount of time
    
    WebCore:
            * dom/WorkerThread.cpp: (WebCore::WorkerThread::workerThread):
            Detach the thread. Without this, one page of its stack was never unmmaped, and fragmentation
            made RegisterFile allocaiton fail after a while.
    
    JavaScriptCore:
            * interpreter/RegisterFile.h: (JSC::RegisterFile::RegisterFile):
            Improve error handling: if mmap fails, crash immediately, and print out the reason.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39297 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/interpreter/RegisterFile.h
+++ third_party/WebKit/JavaScriptCore/interpreter/RegisterFile.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Register.h"
 #include "Collector.h"
 #if HAVE(MMAP)
+#include <errno.h>
 #include <sys/mman.h>
 #endif
 #include <wtf/Noncopyable.h>
@@ -122,7 +123,10 @@ namespace JSC {
             size_t bufferLength = (capacity + maxGlobals) * sizeof(Register);
 #if HAVE(MMAP)
             m_buffer = static_cast<Register*>(mmap(0, bufferLength, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, -1, 0));
-            ASSERT(reinterpret_cast<intptr_t>(m_buffer) != -1);
+            if (m_buffer == MAP_FAILED) {
+                fprintf(stderr, "Could not allocate register file: %d\n", errno);
+                CRASH();
+            }
 #elif HAVE(VIRTUALALLOC)
             // FIXME: Use VirtualAlloc, and commit pages as we go.
             m_buffer = static_cast<Register*>(fastMalloc(bufferLength));
