9b4180cc73a00d777b4c36314bc59837866fb019
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-15 00:41:18 +0000

    WebKit/mac:
            Reviewed by Darin Adler.
    
            - fix <rdar://problem/3258561> WebHistoryAllItemsRemovedNotification
              should add items to userInfo
    
            * History/WebHistory.mm:
            (-[WebHistoryPrivate allItems]): Added this helper method, which returns
            all values in the _entriesByURL dictionary.
            (-[WebHistory removeAllItems]): Changed to send the array of all items
            in the notification.
    
    WebKit/win:
    
            Reviewed by Darin Adler.
    
            - Windows equivalent of <rdar://problem/3258561>
              WebHistoryAllItemsRemovedNotification should add items to userInfo
    
            * WebHistory.cpp:
            (WebHistory::removeAllItems): Changed to create an array of all items
            and send it in the notification.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/History/WebHistory.mm
+++ third_party/WebKit/WebKit/mac/History/WebHistory.mm
@@ -80,6 +80,7 @@ NSString *DatesArrayKey = @"WebHistoryDates";
 - (BOOL)containsURL:(NSURL *)URL;
 - (WebHistoryItem *)itemForURL:(NSURL *)URL;
 - (WebHistoryItem *)itemForURLString:(NSString *)URLString;
+- (NSArray *)allItems;
 
 - (BOOL)loadFromURL:(NSURL *)URL collectDiscardedItemsInto:(NSMutableArray *)discardedItems error:(NSError **)error;
 - (BOOL)saveToURL:(NSURL *)URL error:(NSError **)error;
@@ -410,6 +411,11 @@ WebHistoryDateKey timeIntervalForBeginningOfDay(NSTimeInterval interval)
     return [self itemForURLString:[URL _web_originalDataAsString]];
 }
 
+- (NSArray *)allItems
+{
+    return [_entriesByURL allValues];
+}
+
 #pragma mark ARCHIVING/UNARCHIVING
 
 - (void)setHistoryAgeInDaysLimit:(int)limit
@@ -683,11 +689,9 @@ WebHistoryDateKey timeIntervalForBeginningOfDay(NSTimeInterval interval)
 
 - (void)removeAllItems
 {
-    if ([_historyPrivate removeAllItems]) {
-        [[NSNotificationCenter defaultCenter]
-            postNotificationName:WebHistoryAllItemsRemovedNotification
-                          object:self];
-    }
+    NSArray *entries = [_historyPrivate allItems];
+    if ([_historyPrivate removeAllItems])
+        [self _sendNotification:WebHistoryAllItemsRemovedNotification entries:entries];
 }
 
 - (void)addItems:(NSArray *)newEntries
