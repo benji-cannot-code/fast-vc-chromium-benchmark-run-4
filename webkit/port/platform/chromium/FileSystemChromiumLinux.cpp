de04491db29150e2acc7798e6b3276d608333002
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-15 19:32:55 +0000

    Remove FileSystemPosix.cpp since it is actually unnecessary.  This makes the Linux port more like the Windows and Mac ports.
    R=evanm
    
    Review URL: http://codereview.chromium.org/14431
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6997 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/chromium/FileSystemChromiumLinux.cpp
+++ webkit/port/platform/chromium/FileSystemChromiumLinux.cpp
@@ -30,14 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "FileSystem.h"
 
-#import <Foundation/NSFileManager.h>
-#include "PlatformString.h"
-
 namespace WebCore {
 
 String pathGetFileName(const String& path)
 {
-    return [[NSFileManager defaultManager] displayNameAtPath:path];
+    return path.substring(path.reverseFind('/') + 1);
 }
 
 }
