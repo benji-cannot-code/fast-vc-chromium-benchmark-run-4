a04f0c05f2c843f34d794fb1ab57b678b5f91b48
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-16 20:11:48 +0000

    fuzzymatch needed libtiff to compile for me.
    Review URL: http://codereview.chromium.org/14164
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7080 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/fuzzymatch/fuzzymatch.c
+++ third_party/fuzzymatch/fuzzymatch.c
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Otherwise the binary exists with code 0.
  *
  * This requires leptonica to be installed. On Ubuntu do
- *   # apt-get install libleptonica libleptonica-dev
+ *   # apt-get install libleptonica libleptonica-dev libtiff4-dev
  *
  * Build with:
  *   % gcc -o fuzzymatch fuzzymatch.c -llept -ljpeg -ltiff -lpng -lz -lm -Wall -O2
