e8982f02c90951779b66070c413031e771585b00
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-16 16:02:46 +0000

    2008-12-16 Yael Aharon <yael.aharon@nokia.com>
            Reviewed by Tor Arne Vestbø.
    
            Qt/Win build fix
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39335 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/SystemTimeWin.cpp
+++ third_party/WebKit/WebCore/platform/win/SystemTimeWin.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <DateMath.h>
 #include <windows.h>
 
-#if COMPILER(MINGW)
+#if COMPILER(MINGW) || (PLATFORM(QT) && COMPILER(MSVC))
 #include <float.h>
 #define FLOAT_MAX FLT_MAX
 #endif
