92c697759dc971783d80ac5e0174443bd8fed31e
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-17 01:08:33 +0000

    Make stop work again (I broke it).  Rather than revert to the previous working-but-lame code, this cleans up the code a little further by actually enabling/disabling the stop command at the right times.
    BUG=5542
    Review URL: http://codereview.chromium.org/14498
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7119 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -308,11 +308,6 @@ bool Browser::IsCommandEnabled(int id) const {
   if (!GetSelectedTabContents())
     return false;
 
-  // TODO(pkasting): Should maybe enable/disable this in
-  // NavigationStateChanged()?
-  if (id == IDC_STOP)
-    return IsCurrentPageLoading();
-
   return controller_.IsCommandEnabled(id);
 }
 
@@ -633,7 +628,7 @@ void Browser::Go() {
 
 void Browser::Stop() {
   UserMetrics::RecordAction(L"Stop", profile_);
-  GetSelectedTabContents()->AsWebContents()->Stop();
+  GetSelectedTabContents()->Stop();
 }
 
 void Browser::NewWindow() {
@@ -1430,12 +1425,13 @@ void Browser::TabSelectedAt(TabContents* old_contents,
   UpdateToolbar(true);
 
   // Force the go/stop button to change.
-  GetGoButton()->ChangeMode(
-      (new_contents->AsWebContents() && new_contents->is_loading()) ?
+  bool is_loading = new_contents->is_loading();
+  GetGoButton()->ChangeMode(is_loading ?
       GoButton::MODE_STOP : GoButton::MODE_GO);
 
   // Update commands to reflect current state.
   UpdateCommandsForTabState();
+  controller_.UpdateCommandEnabled(IDC_STOP, is_loading);
 
   // Reset the status bubble.
   GetStatusBubble()->Hide();
@@ -1687,10 +1683,13 @@ void Browser::LoadingStateChanged(TabContents* source) {
   window_->UpdateLoadingAnimations(tabstrip_model_.TabsAreLoading());
   window_->UpdateTitleBar();
 
-  // Let the go button know that it should change appearance if possible.
   if (source == GetSelectedTabContents()) {
-    GetGoButton()->ScheduleChangeMode(
-      source->is_loading() ? GoButton::MODE_STOP : GoButton::MODE_GO);
+    // Let the go button know that it should change appearance if possible.
+    bool is_loading = source->is_loading();
+    GetGoButton()->ScheduleChangeMode(is_loading ?
+        GoButton::MODE_STOP : GoButton::MODE_GO);
+
+    controller_.UpdateCommandEnabled(IDC_STOP, is_loading);
 
     GetStatusBubble()->SetStatus(GetSelectedTabContents()->GetStatusText());
   }
