953c86ab819b85a04b337822d65000a497b9c27d
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-17 06:30:17 +0000

    2008-12-16  Sam Weinig  <sam@webkit.org>
            Reviewed by Geoffrey Garen.
    
            Fix for https://bugs.webkit.org/show_bug.cgi?id=22838
            Remove dependency on the bytecode Instruction buffer in Interpreter::throwException
            Part of <rdar://problem/6428342>
    
            * bytecode/CodeBlock.cpp:
            (JSC::CodeBlock::functionRegisterForBytecodeOffset): Added. Function to get
            a function Register index in a callFrame for a bytecode offset.
            (JSC::CodeBlock::shrinkToFit): Shrink m_getByIdExceptionInfo and m_functionRegisterInfos.
            * bytecode/CodeBlock.h:
            (JSC::FunctionRegisterInfo::FunctionRegisterInfo): Added.
            (JSC::CodeBlock::addFunctionRegisterInfo):
            * bytecompiler/BytecodeGenerator.cpp:
            (JSC::BytecodeGenerator::emitCall):
            * interpreter/Interpreter.cpp:
            (JSC::Interpreter::throwException): Use functionRegisterForBytecodeOffset in JIT
            mode.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bytecode/CodeBlock.cpp
+++ third_party/WebKit/JavaScriptCore/bytecode/CodeBlock.cpp
@@ -1428,6 +1428,32 @@ bool CodeBlock::getByIdExceptionInfoForBytecodeOffset(unsigned bytecodeOffset, O
     return true;
 }
 
+#if ENABLE(JIT)
+bool CodeBlock::functionRegisterForBytecodeOffset(unsigned bytecodeOffset, int& functionRegisterIndex)
+{
+    ASSERT(bytecodeOffset < m_instructions.size());
+
+    if (!m_rareData || !m_rareData->m_functionRegisterInfos.size())
+        return false;
+
+    int low = 0;
+    int high = m_rareData->m_functionRegisterInfos.size();
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (m_rareData->m_functionRegisterInfos[mid].bytecodeOffset <= bytecodeOffset)
+            low = mid + 1;
+        else
+            high = mid;
+    }
+
+    if (!low || m_rareData->m_functionRegisterInfos[low - 1].bytecodeOffset != bytecodeOffset)
+        return false;
+
+    functionRegisterIndex = m_rareData->m_functionRegisterInfos[low - 1].functionRegisterIndex;
+    return true;
+}
+#endif
+
 void CodeBlock::shrinkToFit()
 {
     m_instructions.shrinkToFit();
@@ -1444,6 +1470,7 @@ void CodeBlock::shrinkToFit()
 
     m_expressionInfo.shrinkToFit();
     m_lineInfo.shrinkToFit();
+    m_getByIdExceptionInfo.shrinkToFit();
 
     m_identifiers.shrinkToFit();
     m_functionExpressions.shrinkToFit();
@@ -1457,6 +1484,9 @@ void CodeBlock::shrinkToFit()
         m_rareData->m_immediateSwitchJumpTables.shrinkToFit();
         m_rareData->m_characterSwitchJumpTables.shrinkToFit();
         m_rareData->m_stringSwitchJumpTables.shrinkToFit();
+#if ENABLE(JIT)
+        m_rareData->m_functionRegisterInfos.shrinkToFit();
+#endif
     }
 }
 
--- third_party/WebKit/JavaScriptCore/bytecode/CodeBlock.h
+++ third_party/WebKit/JavaScriptCore/bytecode/CodeBlock.h
@@ -129,6 +129,17 @@ namespace JSC {
         bool isLinked() { return callee; }
     };
 
+    struct FunctionRegisterInfo {
+        FunctionRegisterInfo(unsigned bytecodeOffset, int functionRegisterIndex)
+            : bytecodeOffset(bytecodeOffset)
+            , functionRegisterIndex(functionRegisterIndex)
+        {
+        }
+
+        unsigned bytecodeOffset;
+        int functionRegisterIndex;
+    };
+
     struct GlobalResolveInfo {
         GlobalResolveInfo()
             : structure(0)
@@ -291,6 +302,8 @@ namespace JSC {
         {
             return binaryChop<PC, void*, getNativePC>(m_pcVector.begin(), m_pcVector.size(), nativePC)->bytecodeIndex;
         }
+
+        bool functionRegisterForBytecodeOffset(unsigned bytecodeOffset, int& functionRegisterIndex);
 #endif
 
         Vector<Instruction>& instructions() { return m_instructions; }
@@ -350,6 +363,8 @@ namespace JSC {
         size_t numberOfCallLinkInfos() const { return m_callLinkInfos.size(); }
         void addCallLinkInfo() { m_callLinkInfos.append(CallLinkInfo()); }
         CallLinkInfo& callLinkInfo(int index) { return m_callLinkInfos[index]; }
+
+        void addFunctionRegisterInfo(unsigned bytecodeOffset, int functionIndex) { createRareDataIfNecessary(); m_rareData->m_functionRegisterInfos.append(FunctionRegisterInfo(bytecodeOffset, functionIndex)); }
 
         Vector<PC>& pcVector() { return m_pcVector; }
 #endif
@@ -481,6 +496,10 @@ namespace JSC {
             Vector<StringJumpTable> m_stringSwitchJumpTables;
 
             EvalCodeCache m_evalCodeCache;
+
+#if ENABLE(JIT)
+            Vector<FunctionRegisterInfo> m_functionRegisterInfos;
+#endif
         };
 
         OwnPtr<RareData> m_rareData;
