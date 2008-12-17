3a6ac074a660ae945ce6a6b1e66479748aa4916d
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-17 23:08:24 +0000

    Add beginnings of a custom FTP implementation.  This is built but not used or tested yet.  This is just skeleton code.
    R=wtc
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7179 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/ftp/ftp_network_session.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2008 The Chromium Authors. All rights reserved.  Use of this
+// source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
+
+#ifndef NET_FTP_FTP_NETWORK_SESSION_H_
+#define NET_FTP_FTP_NETWORK_SESSION_H_
+
+#include "base/ref_counted.h"
+#include "net/base/auth_cache.h"
+
+namespace net {
+
+// This class holds session objects used by FtpNetworkTransaction objects.
+class FtpNetworkSession : public base::RefCounted<FtpNetworkSession> {
+ public:
+  FtpNetworkSession() {}
+
+  AuthCache* auth_cache() { return &auth_cache_; }
+
+ private:
+  AuthCache auth_cache_;
+};
+
+}  // namespace net
+
+#endif  // NET_FTP_FTP_NETWORK_SESSION_H_
