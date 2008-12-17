f3fbf2e90934ab381ddad9faaafad033e2aac54e
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-17 19:01:41 +0000

    Rubber-stamped by Timothy Hatcher.
    Fix wml/ tests, by doing s/</\&lt;/ and s/>/\&gt;/.
    Not sure why this problem wasn't visible before, something has changed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/wml/resources/go-task-animation.js
+++ third_party/WebKit/LayoutTests/wml/resources/go-task-animation.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// [Name] go-task-animation.js
 
-createWMLTestCase("Tests <timer> and <go> element combinations", false, "resources/animation.wml");
+createWMLTestCase("Tests &lt;timer&gt; and &lt;go&gt; element combinations", false, "resources/animation.wml");
 
 var counter = 0;
 
