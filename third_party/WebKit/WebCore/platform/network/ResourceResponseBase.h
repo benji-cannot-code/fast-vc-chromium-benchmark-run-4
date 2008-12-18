29105b7d8110a541c14815bc46df822dfc96215f
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-18 23:58:53 +0000

    2008-12-18  Darin Adler  <darin@apple.com>
            Reviewed by Sam Weinig.
    
            - fix <rdar://problem/6449841> reduce memory use of ResourceResponseBase by removing two maps
    
            We were parsing the cache-control and pragma header field values into maps.
            I changed that so instead we only cache two bits with the data we were actually
            using. Later we might want to move this responsibility out of this class entirely;
            we can cache it at the higher level instead.
    
            * loader/CachedResource.cpp:
            (WebCore::CachedResource::mustRevalidate): Changed to call a specific API to get at
            the bits in quest instead of a general "cache control directives" API.
    
            * platform/network/ResourceResponseBase.cpp:
            (WebCore::ResourceResponseBase::setHTTPHeaderField): Remove the logic for the
            pragma header field since no one is using this for now.
            (WebCore::ResourceResponseBase::parseCacheControlDirectives): Eliminated the return
            value and made this function have side effects only. Changed it so it's the caller's
            responsibility to check m_haveParsedCacheControl. Set m_cacheControlContainsNoCache
            and m_cacheControlContainsMustRevalidate rather than keeping a map around.
    
            * platform/network/ResourceResponseBase.h:
            (WebCore::ResourceResponseBase::cacheControlContainsNoCache): Added.
            (WebCore::ResourceResponseBase::cacheControlContainsMustRevalidate): Added.
            (WebCore::ResourceResponseBase::ResourceResponseBase): Updated since I removed
            m_haveParsedCacheControl and renamed m_haveParsedCacheControlHeader to remove
            the imprecise use of the term "header".
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceResponseBase.h
+++ third_party/WebKit/WebCore/platform/network/ResourceResponseBase.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2006, 2008 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,15 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-typedef HashMap<String, HashSet<String>, CaseFoldingHash> CacheControlDirectiveMap;
-typedef HashMap<String, String, CaseFoldingHash> PragmaDirectiveMap;
-
 class ResourceResponse;
 
 // Do not use this class directly, use the class ResponseResponse instead
 class ResourceResponseBase {
- public:
-
+public:
     bool isNull() const { return m_isNull; }
     bool isHTTP() const;
 
@@ -70,9 +66,6 @@ class ResourceResponseBase {
     void setHTTPHeaderField(const AtomicString& name, const String& value);
     const HTTPHeaderMap& httpHeaderFields() const;
 
-    const PragmaDirectiveMap& parsePragmaDirectives() const;
-    const CacheControlDirectiveMap& parseCacheControlDirectives() const;
-
     bool isMultipart() const { return mimeType() == "multipart/x-mixed-replace"; }
 
     bool isAttachment() const;
@@ -83,17 +76,29 @@ class ResourceResponseBase {
     void setLastModifiedDate(time_t);
     time_t lastModifiedDate() const;
 
+    bool cacheControlContainsNoCache() const
+    {
+        if (!m_haveParsedCacheControl)
+            parseCacheControlDirectives();
+        return m_cacheControlContainsMustRevalidate;
+    }
+    bool cacheControlContainsMustRevalidate() const
+    {
+        if (!m_haveParsedCacheControl)
+            parseCacheControlDirectives();
+        return m_cacheControlContainsMustRevalidate;
+    }
+
     static bool compare(const ResourceResponse& a, const ResourceResponse& b);
 
- protected:
+protected:
     ResourceResponseBase()  
         : m_expectedContentLength(0)
         , m_httpStatusCode(0)
         , m_expirationDate(0)
         , m_lastModifiedDate(0)
         , m_isNull(true)
-        , m_haveParsedCacheControlHeader(false)
-        , m_haveParsedPragmaHeader(false)
+        , m_haveParsedCacheControl(false)
     {
     }
 
@@ -107,15 +112,14 @@ class ResourceResponseBase {
         , m_expirationDate(0)
         , m_lastModifiedDate(0)
         , m_isNull(false)
-        , m_haveParsedCacheControlHeader(false)
-        , m_haveParsedPragmaHeader(false)
+        , m_haveParsedCacheControl(false)
     {
     }
 
     void lazyInit() const;
 
     // The ResourceResponse subclass may "shadow" this method to lazily initialize platform specific fields
-    void platformLazyInit() {}
+    void platformLazyInit() { }
 
     // The ResourceResponse subclass may "shadow" this method to compare platform specific fields
     static bool platformCompare(const ResourceResponse& a, const ResourceResponse& b) { return true; }
@@ -130,11 +134,14 @@ class ResourceResponseBase {
     HTTPHeaderMap m_httpHeaderFields;
     time_t m_expirationDate;
     time_t m_lastModifiedDate;
-    bool m_isNull:1;
-    mutable bool m_haveParsedCacheControlHeader:1;
-    mutable bool m_haveParsedPragmaHeader:1;
-    mutable CacheControlDirectiveMap m_cacheControlDirectiveMap;
-    mutable PragmaDirectiveMap m_pragmaDirectiveMap;
+    bool m_isNull : 1;
+
+private:
+    void parseCacheControlDirectives() const;
+
+    mutable bool m_haveParsedCacheControl : 1;
+    mutable bool m_cacheControlContainsMustRevalidate : 1;
+    mutable bool m_cacheControlContainsNoCache : 1;
 };
 
 inline bool operator==(const ResourceResponse& a, const ResourceResponse& b) { return ResourceResponseBase::compare(a, b); }
