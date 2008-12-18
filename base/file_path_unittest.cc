e97dc5b42ae71858903383448694a735a2e823f7
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-18 09:25:21 +0000

    fix merge failure
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7209 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_path_unittest.cc
+++ base/file_path_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/path_service.h"
-#include "chrome/common/chrome_paths.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
