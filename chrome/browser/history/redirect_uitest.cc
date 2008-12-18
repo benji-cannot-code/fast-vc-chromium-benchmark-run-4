090d8b234691721d119af3d97c18ae294a474eba
paulg@google.com <paulg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-18 19:11:38 +0000

    Make some more ui_test Purify friendly. Review URL: http://codereview.chromium.org/15027
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7242 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/redirect_uitest.cc
+++ chrome/browser/history/redirect_uitest.cc
@@ -60,7 +60,7 @@ TEST_F(RedirectTest, Client) {
   // We need the sleep for the client redirects, because it appears as two
   // page visits in the browser.
   NavigateToURL(first_url);
-  Sleep(kWaitForActionMsec);
+  Sleep(action_timeout_ms());
 
   scoped_ptr<TabProxy> tab_proxy(GetActiveTab());
   ASSERT_TRUE(tab_proxy.get());
@@ -107,7 +107,7 @@ TEST_F(RedirectTest, ClientCancelled) {
   GURL first_url = net::FilePathToFileURL(first_path);
 
   NavigateToURL(first_url);
-  Sleep(kWaitForActionMsec);
+  Sleep(action_timeout_ms());
 
   scoped_ptr<TabProxy> tab_proxy(GetActiveTab());
   ASSERT_TRUE(tab_proxy.get());
