115cf7cf77745dc28cf5f63e3d60617823ab2a8b
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-18 08:38:16 +0000

    Rather than create a TODO that will never be done, I went ahead and implemented FilePath::Contains().
    Review URL: http://codereview.chromium.org/14827
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7208 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_path.cc
+++ base/file_path.cc
@@ -4,11 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/file_path.h"
+#include "base/file_util.h"
 #include "base/logging.h"
 
 // These includes are just for the *Hack functions, and should be removed
 // when those functions are removed.
 #include "base/string_piece.h"
+#include "base/string_util.h"
 #include "base/sys_string_conversions.h"
 
 #if defined(FILE_PATH_USES_WIN_SEPARATORS)
@@ -177,6 +179,31 @@ FilePath FilePath::Append(const FilePath& component) const {
 
 bool FilePath::IsAbsolute() const {
   return IsPathAbsolute(path_);
+}
+
+bool FilePath::Contains(const FilePath &other) const {
+  FilePath parent = FilePath(*this);
+  FilePath child = FilePath(other);
+
+  if (!file_util::AbsolutePath(&parent) || !file_util::AbsolutePath(&child))
+    return false;
+
+#if defined(OS_WIN)
+  // file_util::AbsolutePath() does not flatten case on Windows, so we must do
+  // a case-insensitive compare.
+  if (!StartsWith(child.value(), parent.value(), false))
+#else
+  if (!StartsWithASCII(child.value(), parent.value(), true))
+#endif
+    return false;
+
+  // file_util::AbsolutePath() normalizes '/' to '\' on Windows, so we only need
+  // to check kSeparators[0].
+  if (child.value().length() <= parent.value().length() ||
+      child.value()[parent.value().length()] != kSeparators[0])
+    return false;
+
+  return true;
 }
 
 #if defined(OS_POSIX)
