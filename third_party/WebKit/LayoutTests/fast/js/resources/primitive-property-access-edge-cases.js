ce8a98f3b6f38574f9af45c5205da9a952788098
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-18 17:45:44 +0000

    JavaScriptCore:
    2008-12-17  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Gavin Barraclough.
            
            Fixed https://bugs.webkit.org/show_bug.cgi?id=22393
            Segfault when caching property accesses to primitive cells.
            
            Changed some asObject casts to asCell casts in cases where a primitive
            value may be a cell and not an object.
            
            Re-enabled property caching for primitives in cases where it had been
            disabled because of this bug.
            
            Updated a comment to better explain something Darin thought needed
            explaining in an old patch review.
    
            * interpreter/Interpreter.cpp:
            (JSC::countPrototypeChainEntriesAndCheckForProxies):
            (JSC::Interpreter::tryCacheGetByID):
            (JSC::Interpreter::tryCTICacheGetByID):
            (JSC::Interpreter::cti_op_get_by_id_self_fail):
            (JSC::Interpreter::cti_op_get_by_id_proto_list):
    
    LayoutTests:
    
    2008-12-17  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Gavin Barraclough.
            
            Added a test for https://bugs.webkit.org/show_bug.cgi?id=22393
            Segfault when caching property accesses to primitive cells.
    
            * fast/js/primitive-property-access-edge-cases-expected.txt: Added.
            * fast/js/primitive-property-access-edge-cases.html: Added.
            * fast/js/resources/primitive-property-access-edge-cases.js: Added.
            ():
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39374 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/primitive-property-access-edge-cases.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This page tests for assertion failures in edge cases of property lookup on primitive values."
+);
+
+var didNotCrash = true;
+
+(function () {
+    delete String.prototype.constructor;
+    for (var i = 0; i < 3; ++i)
+        "".replace;
+})();
+
+(function () {
+    String.prototype.__proto__ = { x: 1, y: 1 };
+    delete String.prototype.__proto__.x;
+    for (var i = 0; i < 3; ++i)
+        "".y;
+})();
+
+(function () {
+    function f(x) {
+        x.y;
+    }
+    
+    String.prototype.x = 1;
+    String.prototype.y = 1;
+    delete String.prototype.x;
+
+    Number.prototype.x = 1;
+    Number.prototype.y = 1;
+    delete Number.prototype.x;
+
+    for (var i = 0; i < 3; ++i)
+        f("");
+
+    for (var i = 0; i < 3; ++i)
+        f(.5);
+})();
+
+shouldBeTrue("didNotCrash");
+
+var successfullyParsed = true;
