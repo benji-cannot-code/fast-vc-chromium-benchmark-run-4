82507ae51c0c812ca7c899787886f42dfe2f4c52
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-19 10:41:57 +0000

    2008-12-19  Jade Han <jade.han@nokia.com>
            Reviewed by Tor Arne Vestbø.
    
            [Qt] Allow conversion of JavaScript Number and Boolean types to Qt types
    
            https://bugs.webkit.org/show_bug.cgi?id=22880
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39399 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
+++ third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <runtime.h>
 #include <runtime_array.h>
 #include <runtime_object.h>
+#include "BooleanObject.h"
 
 // QtScript has these
 Q_DECLARE_METATYPE(QObjectList);
@@ -180,7 +181,12 @@ QVariant convertValueToQVariant(ExecState* exec, JSValue* value, QMetaType::Type
                 hint = QMetaType::QRegExp;
                 break;
             case Object:
-                hint = QMetaType::QVariantMap;
+                if (object->inherits(&NumberObject::info))
+                    hint = QMetaType::Double;
+                else if (object->inherits(&BooleanObject::info))
+                    hint = QMetaType::Bool;
+                else
+                    hint = QMetaType::QVariantMap;
                 break;
             case QObj:
                 hint = QMetaType::QObjectStar;
@@ -208,7 +214,10 @@ QVariant convertValueToQVariant(ExecState* exec, JSValue* value, QMetaType::Type
     int dist = -1;
     switch (hint) {
         case QMetaType::Bool:
-            ret = QVariant(value->toBoolean(exec));
+            if (type == Object && object->inherits(&BooleanObject::info))
+                ret = QVariant(asBooleanObject(value)->internalValue()->toBoolean(exec));
+            else
+                ret = QVariant(value->toBoolean(exec));
             if (type == Boolean)
                 dist = 0;
             else
@@ -700,7 +709,12 @@ QVariant convertValueToQVariant(ExecState* exec, JSValue* value, QMetaType::Type
                         *distance = 1;
                     return QVariant();
                 } else {
-                    // Well.. we can do anything... just recurse with the autodetect flag
+                    if (type == Object) {
+                        // Since we haven't really visited this object yet, we remove it
+                        visitedObjects->remove(object);
+                    }
+
+                    // And then recurse with the autodetect flag
                     ret = convertValueToQVariant(exec, value, QMetaType::Void, distance, visitedObjects);
                     dist = 10;
                 }
