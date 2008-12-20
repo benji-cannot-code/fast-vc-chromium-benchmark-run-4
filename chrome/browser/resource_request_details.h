49c971bf3ff040deb39c2815dd382cbebb8c5c4d
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-20 01:18:59 +0000

    This CL moves the different resource handlers from resource_dispatcher_host.cc into their own files, into a new renderer_host directory. Their is no functionality change in this CL.
    The motivation for doing that was:
    - resource_dispatcher_host.cc has become very long and more than 1/3 of the code is for the resource handler.
    - having the resource handler in their own file allows having unit tests for them.
    
    BUG=none
    TEST=run all unit tests and UI tests.
    Review URL: http://codereview.chromium.org/14487
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7336 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resource_request_details.h
+++ chrome/browser/resource_request_details.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "chrome/browser/cert_store.h"
-#include "chrome/browser/resource_dispatcher_host.h"
+#include "chrome/browser/renderer_host/resource_dispatcher_host.h"
 #include "googleurl/src/gurl.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_status.h"
