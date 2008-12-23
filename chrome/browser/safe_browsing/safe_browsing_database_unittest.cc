410e30096d1c7b4c5d1f0bd37ce22d3adbe8e0cc
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-23 20:30:19 +0000

    * Bring up GreaseMonkeySlave unittest on OS X. * Fix missing Autoreleasepool warning in SafeBrowsing Unittest.
    Review URL: http://codereview.chromium.org/16243
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7432 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_database_unittest.cc
+++ chrome/browser/safe_browsing/safe_browsing_database_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "googleurl/src/gurl.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "testing/platform_test.h"
 
 using base::Time;
 
@@ -96,8 +97,11 @@ namespace {
 
 }  // namespace
 
+class SafeBrowsingDatabasePlatformTest : public PlatformTest {
+};
+
 // Tests retrieving list name information.
-TEST(SafeBrowsingDatabase, ListName) {
+TEST_F(SafeBrowsingDatabasePlatformTest, ListName) {
   SafeBrowsingDatabase* database = SetupTestDatabase();
 
   // Insert some malware add chunks.
