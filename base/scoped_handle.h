abb616ad5c521744055995e157d3fd5efb542aba
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-23 21:33:27 +0000

    Add stdio to this file becasue we use FILE.
    It starts failing with FILE, identifier not found, when
    you remove the include for logging.h, which is included
    in scoped_handle_win.h
    
    Review URL: http://codereview.chromium.org/16461
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7441 0039d316-1c4b-4281-b951-d872f2087c98

--- base/scoped_handle.h
+++ base/scoped_handle.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_SCOPED_HANDLE_H_
 #define BASE_SCOPED_HANDLE_H_
 
+#include <stdio.h>
+
 #include "base/basictypes.h"
 
 #if defined(OS_WIN)
