95a757898c9f42bd3566e375244a8b763e22b428
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-23 22:58:32 +0000

    Kill PlatformScreenMac.mm
    Replace it with an implementation of GetScreenInfoHelper like
    the other platforms.  This webkit glue function is implemented
    in webkit_glue_mac.mm.
    
    Now that we have a GetScreenInfoHelper implemented for each
    platform, I moved the implementation of GetScreenInfo into
    test_shell.cc.
    
    R=dglazkov
    
    Review URL: http://codereview.chromium.org/16470
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7453 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_win.cc
+++ webkit/tools/test_shell/test_shell_win.cc
@@ -769,8 +769,4 @@ bool DownloadUrl(const std::string& url, HWND caller_window) {
   return false;
 }
 
-ScreenInfo GetScreenInfo(gfx::NativeView window) {
-  return GetScreenInfoHelper(window);
-}
-
 }  // namespace webkit_glue
