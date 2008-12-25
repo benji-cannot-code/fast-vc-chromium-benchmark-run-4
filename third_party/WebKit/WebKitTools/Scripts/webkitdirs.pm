0c59db2f7d958ef5d1e89206de91dc93a0cb9eb7
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-25 05:26:33 +0000

    Move the guts of determineCurrentSVNRevision to VCSUtils as svnRevisionForDirectory, and make it work for git too.
    Reviewed by Timothy Hatcher.
    
    * Scripts/VCSUtils.pm:
    * Scripts/webkitdirs.pm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39471 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -213,9 +213,7 @@ sub determineCurrentSVNRevision
 {
     return if defined $currentSVNRevision;
     determineSourceDir();
-    my $svnInfo = `LC_ALL=C svn info $sourceDir | grep Revision:`;
-    ($currentSVNRevision) = ($svnInfo =~ m/Revision: (\d+).*/g);
-    die "Unable to determine current SVN revision in $sourceDir" unless (defined $currentSVNRevision);
+    $currentSVNRevision = svnRevisionForDirectory($sourceDir);
     return $currentSVNRevision;
 }
 
