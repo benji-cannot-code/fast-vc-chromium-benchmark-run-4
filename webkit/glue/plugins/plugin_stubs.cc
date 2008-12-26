910f3c70ae67c4294ed0ab5a6c2a0876ff191081
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-26 01:09:24 +0000

    Stub out some plugin functions to make the dynamic linker happy. With this change, test_shell_tests can link with dynamic libraries.
    Review URL: http://codereview.chromium.org/16476
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7474 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/glue/plugins/plugin_stubs.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file stubs out some functions needed to make the linker happy
+// without linking in all the plugin code.  It should be removed once
+// we have plugins working on all platforms.
+
+// TODO(port): remove this file.
+
+#include "base/logging.h"
+#include "webkit/glue/plugins/plugin_instance.h"
+#include "webkit/glue/plugins/plugin_stream.h"
+
+namespace NPAPI {
+
+PluginStream::~PluginStream() {
+  NOTIMPLEMENTED();
+}
+
+bool PluginStream::Close(NPReason reason) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void PluginInstance::NPP_StreamAsFile(NPStream*, const char*) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace NPAPI
