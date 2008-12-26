143538695590ec719ee4a3e582ddabad346831f7
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-26 21:31:20 +0000

    2008-12-26  David Levin  <levin@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            https://bugs.webkit.org/show_bug.cgi?id=22950
    
            Remove ensureTrailingSlash from appcache test.
    
            * http/tests/appcache/resources/fail-on-update.php:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39485 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/appcache/resources/fail-on-update.php
+++ third_party/WebKit/LayoutTests/http/tests/appcache/resources/fail-on-update.php
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
 require_once '../../xmlhttprequest/resources/portabilityLayer.php';
 
-$tmpFile = ensureTrailingSlash(sys_get_temp_dir()) . "abort_update_state";
+$tmpFile = sys_get_temp_dir() . "/" . "abort_update_state";
 
 function setState($newState, $file)
 {
