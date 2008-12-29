000cc38e6823fefc29dbbc1ca29cbe0054f84660
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2008-12-29 21:42:33 +0000

    2008-12-29  Adele Peterson  <adele@apple.com>
            Reviewed by Brady Eidson.
    
            Fix for https://bugs.webkit.org/show_bug.cgi?id=21797
            <rdar://problem/6310682> REGRESSION: Crash in CFHTTPCookieStorageCopy beneath WebCore::cookies() when 
            running fast/dom/document-attribute-js-null.html and http/tests/security/cookies/create-document.html
            
            Return early if the document is trying to get or set a cookie with an empty cookie url.
    
            * dom/Document.cpp:
            (WebCore::Document::cookie):
            (WebCore::Document::setCookie):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -2903,7 +2903,11 @@ String Document::cookie() const
     if (page() && !page()->cookieEnabled())
         return String();
 
-    return cookies(this, cookieURL());
+    KURL cookieURL = this->cookieURL();
+    if (cookieURL.isEmpty())
+        return String();
+
+    return cookies(this, cookieURL);
 }
 
 void Document::setCookie(const String& value)
@@ -2911,7 +2915,11 @@ void Document::setCookie(const String& value)
     if (page() && !page()->cookieEnabled())
         return;
 
-    setCookies(this, cookieURL(), policyBaseURL(), value);
+    KURL cookieURL = this->cookieURL();
+    if (cookieURL.isEmpty())
+        return;
+
+    setCookies(this, cookieURL, policyBaseURL(), value);
 }
 
 String Document::referrer() const
