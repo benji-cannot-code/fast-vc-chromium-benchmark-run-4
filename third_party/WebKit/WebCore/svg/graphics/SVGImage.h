7b0ac3cbe8263d907221dcbbbefce69bdcd90e5e
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-02 21:22:29 +0000

    2009-01-02  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Darin Adler
    
            Use an OwnPtr for m_chromeClient.
    
            * svg/graphics/SVGImage.cpp:
            (WebCore::SVGImage::~SVGImage):
            (WebCore::SVGImage::dataChanged):
            * svg/graphics/SVGImage.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/graphics/SVGImage.h
+++ third_party/WebKit/WebCore/svg/graphics/SVGImage.h
@@ -73,7 +73,7 @@ private:
         virtual NativeImagePtr nativeImageForCurrentFrame();
         
         SVGDocument* m_document;
-        SVGImageChromeClient* m_chromeClient;
+        OwnPtr<SVGImageChromeClient> m_chromeClient;
         OwnPtr<Page> m_page;
         RefPtr<Frame> m_frame;
         RefPtr<FrameView> m_frameView;
