0543aa6ef307fbd40949502fa05d88e945744a42
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-03 14:50:51 +0000

    g++ 4.3.x doesn't seem to like forward declarations with namespace::classname so declare the forward declaration inside the appropriate namespace.
    Review URL: http://codereview.chromium.org/16508
    Patch from Craig Schlenter.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7539 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/async_resource_handler.h
+++ chrome/browser/renderer_host/async_resource_handler.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/resource_dispatcher_host.h"
 #include "chrome/browser/renderer_host/resource_handler.h"
 
-class base::SharedMemory;
+namespace base {
+class SharedMemory;
+}
 
 // Used to complete an asynchronous resource request in response to resource
 // load events from the resource dispatcher host.
