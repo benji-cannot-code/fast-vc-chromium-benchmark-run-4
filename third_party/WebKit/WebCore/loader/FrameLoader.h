030f99c20f68c5cbb959c4d494a62ee9ec3d49d3
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-03 08:21:52 +0000

    Build fix.
            * loader/FrameLoader.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/WebCore/loader/FrameLoader.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define FrameLoader_h
 
 #include "CachePolicy.h"
+#include "FormState.h"
 #include "FrameLoaderTypes.h"
 #include "ResourceRequest.h"
 #include "Timer.h"
@@ -51,7 +52,6 @@ namespace WebCore {
     class Element;
     class Event;
     class FormData;
-    class FormState;
     class Frame;
     class FrameLoaderClient;
     class HistoryItem;
