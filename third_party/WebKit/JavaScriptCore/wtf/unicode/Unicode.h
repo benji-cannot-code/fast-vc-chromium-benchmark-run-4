388e20773497068e500ae8ea39052dae2fa889f9
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-04 18:00:58 +0000

    2009-01-04  Darin Adler  <darin@apple.com>
            Reviewed by David Kilzer.
    
            Bug 15114: Provide compile-time assertions for sizeof(UChar), sizeof(DeprecatedChar), etc.
            https://bugs.webkit.org/show_bug.cgi?id=15114
    
            * wtf/unicode/Unicode.h: Assert size of UChar. There is no DeprecatedChar any more.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39585 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/unicode/Unicode.h
+++ third_party/WebKit/JavaScriptCore/wtf/unicode/Unicode.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WTF_UNICODE_H
 #define WTF_UNICODE_H
 
-#include <wtf/Platform.h>
+#include <wtf/Assertions.h>
 
 #if USE(QT4_UNICODE)
 #include "qt4/UnicodeQt4.h"
@@ -32,5 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 #error "Unknown Unicode implementation"
 #endif
+
+COMPILE_ASSERT(sizeof(UChar) == 2, UCharIsTwoBytes);
 
 #endif // WTF_UNICODE_H
