1a8455661f6e0606df2d95fb71953d981624bb09
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-05 19:45:05 +0000

    Fix the Qt build
    git-svn-id: svn://svn.chromium.org/blink/trunk@39608 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/runtime_object.cpp
+++ third_party/WebKit/WebCore/bridge/runtime_object.cpp
@@ -173,7 +173,7 @@ bool RuntimeObjectImp::getOwnPropertySlot(ExecState *exec, const Identifier& pro
     return false;
 }
 
-void RuntimeObjectImp::put(ExecState* exec, const Identifier& propertyName, JSValue* value, PutPropertySlot&)
+void RuntimeObjectImp::put(ExecState* exec, const Identifier& propertyName, JSValue* value, PutPropertySlot& slot)
 {
     if (!instance) {
         throwInvalidAccessError(exec);
@@ -191,7 +191,7 @@ void RuntimeObjectImp::put(ExecState* exec, const Identifier& propertyName, JSVa
         instance->setValueOfUndefinedField(exec, propertyName, value);
 #if PLATFORM(QT)
     else if (instance->getBindingLanguage() == Instance::QtLanguage)
-        JSObject::put(exec,  propertyName, value, slot);
+        JSObject::put(exec, propertyName, value, slot);
 #endif
 
     instance->end();
