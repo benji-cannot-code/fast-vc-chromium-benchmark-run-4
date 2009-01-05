02dea80e215783d12948d0943e653be0a6d096fc
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-05 23:36:51 +0000

    2009-01-05  Adele Peterson  <adele@apple.com>
            Windows build fix.
    
            * WebCoreSupport/WebChromeClient.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebCoreSupport/WebChromeClient.cpp
+++ third_party/WebKit/WebKit/win/WebCoreSupport/WebChromeClient.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/FrameLoadRequest.h>
 #include <WebCore/FrameView.h>
 #include <WebCore/LocalizedStrings.h>
+#include <WebCore/NotImplemented.h>
 #include <WebCore/Page.h>
 #include <WebCore/WindowFeatures.h>
 #pragma warning(pop)
