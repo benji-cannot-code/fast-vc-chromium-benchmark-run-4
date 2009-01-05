b690609f5f58b81a89d5f2b5c9875fda45a9c57c
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-05 23:18:35 +0000

    Blind mac build fix
    git-svn-id: svn://svn.chromium.org/blink/trunk@39621 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebChromeClient.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebChromeClient.mm
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebCore/Page.h>
 #import <WebCore/PlatformScreen.h>
 #import <WebCore/PlatformString.h>
+#import <WebCore/NotImplemented.h>
 #import <WebCore/ResourceRequest.h>
 #import <WebCore/Widget.h>
 #import <WebCore/WindowFeatures.h>
