fd3952efb4fbac6fd0230c154b87ffcfd6023bc6
alice.liu@apple.com <alice.liu@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-05 00:18:53 +0000

    2009-01-04  Alice Liu  <alice.liu@apple.com>
            <rdar://problem/6341776> Merge m_transitionCount and m_offset in Structure.
    
            Reviewed by Darin Adler.
    
            * runtime/Structure.cpp:
            (JSC::Structure::Structure): Remove m_transitionCount
            (JSC::Structure::addPropertyTransitionToExistingStructure): No need to wait until after the assignment to offset to assert if it's notFound; move it up.
            (JSC::Structure::addPropertyTransition): Use method for transitionCount instead of m_transitionCount. Remove line that maintains the m_transitionCount.
            (JSC::Structure::changePrototypeTransition): Remove line that maintains the m_transitionCount.
            (JSC::Structure::getterSetterTransition): Remove line that maintains the m_transitionCount.
            * runtime/Structure.h:
            Changed s_maxTransitionLength and m_offset from size_t to signed char.  m_offset will never become greater than 64 
            because the structure transitions to a dictionary at that time.
            (JSC::Structure::transitionCount): method to replace the data member
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39593 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/Structure.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/Structure.cpp
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using namespace std;
-using WTF::doubleHash;
+using namespace WTF;
 
 namespace JSC {
 
@@ -127,10 +127,9 @@ Structure::Structure(JSValue* prototype, const TypeInfo& typeInfo)
     , m_cachedPrototypeChain(0)
     , m_previous(0)
     , m_nameInPrevious(0)
-    , m_transitionCount(0)
     , m_propertyTable(0)
     , m_propertyStorageCapacity(JSObject::inlineStorageCapacity)
-    , m_offset(WTF::notFound)
+    , m_offset(noOffset)
     , m_isDictionary(false)
     , m_isPinnedPropertyTable(false)
     , m_hasGetterSetterProperties(false)
@@ -238,7 +237,7 @@ static unsigned nextPowerOf2(unsigned v)
 
 static unsigned sizeForKeyCount(size_t keyCount)
 {
-    if (keyCount == WTF::notFound)
+    if (keyCount == notFound)
         return newTableSize;
 
     if (keyCount < 8)
@@ -362,14 +361,14 @@ PassRefPtr<Structure> Structure::addPropertyTransitionToExistingStructure(Struct
     if (structure->m_usingSingleTransitionSlot) {
         Structure* existingTransition = structure->m_transitions.singleTransition;
         if (existingTransition && existingTransition->m_nameInPrevious.get() == propertyName.ustring().rep() && existingTransition->m_attributesInPrevious == attributes) {
+            ASSERT(structure->m_transitions.singleTransition->m_offset != noOffset);
             offset = structure->m_transitions.singleTransition->m_offset;
-            ASSERT(offset != WTF::notFound);
             return existingTransition;
         }
     } else {
         if (Structure* existingTransition = structure->m_transitions.table->get(make_pair(propertyName.ustring().rep(), attributes))) {
+            ASSERT(existingTransition->m_offset != noOffset);
             offset = existingTransition->m_offset;
-            ASSERT(offset != WTF::notFound);
             return existingTransition;
         }
     }
@@ -383,7 +382,7 @@ PassRefPtr<Structure> Structure::addPropertyTransition(Structure* structure, con
     ASSERT(structure->typeInfo().type() == ObjectType);
     ASSERT(!Structure::addPropertyTransitionToExistingStructure(structure, propertyName, attributes, offset));
 
-    if (structure->m_transitionCount > s_maxTransitionLength) {
+    if (structure->transitionCount() > s_maxTransitionLength) {
         RefPtr<Structure> transition = toDictionaryTransition(structure);
         offset = transition->put(propertyName, attributes);
         if (transition->propertyStorageSize() > transition->propertyStorageCapacity())
@@ -396,7 +395,6 @@ PassRefPtr<Structure> Structure::addPropertyTransition(Structure* structure, con
     transition->m_previous = structure;
     transition->m_nameInPrevious = propertyName.ustring().rep();
     transition->m_attributesInPrevious = attributes;
-    transition->m_transitionCount = structure->m_transitionCount + 1;
     transition->m_propertyStorageCapacity = structure->m_propertyStorageCapacity;
     transition->m_hasGetterSetterProperties = structure->m_hasGetterSetterProperties;
 
@@ -451,7 +449,6 @@ PassRefPtr<Structure> Structure::changePrototypeTransition(Structure* structure,
 {
     RefPtr<Structure> transition = create(prototype, structure->typeInfo());
 
-    transition->m_transitionCount = structure->m_transitionCount + 1;
     transition->m_propertyStorageCapacity = structure->m_propertyStorageCapacity;
     transition->m_hasGetterSetterProperties = structure->m_hasGetterSetterProperties;
 
@@ -467,7 +464,6 @@ PassRefPtr<Structure> Structure::changePrototypeTransition(Structure* structure,
 PassRefPtr<Structure> Structure::getterSetterTransition(Structure* structure)
 {
     RefPtr<Structure> transition = create(structure->storedPrototype(), structure->typeInfo());
-    transition->m_transitionCount = structure->m_transitionCount + 1;
     transition->m_propertyStorageCapacity = structure->m_propertyStorageCapacity;
     transition->m_hasGetterSetterProperties = transition->m_hasGetterSetterProperties;
 
@@ -615,7 +611,7 @@ size_t Structure::get(const Identifier& propertyName, unsigned& attributes)
 
     materializePropertyMapIfNecessary();
     if (!m_propertyTable)
-        return WTF::notFound;
+        return notFound;
 
     UString::Rep* rep = propertyName._ustring.rep();
 
@@ -627,7 +623,7 @@ size_t Structure::get(const Identifier& propertyName, unsigned& attributes)
 
     unsigned entryIndex = m_propertyTable->entryIndices[i & m_propertyTable->sizeMask];
     if (entryIndex == emptyEntryIndex)
-        return WTF::notFound;
+        return notFound;
 
     if (rep == m_propertyTable->entries()[entryIndex - 1].key) {
         attributes = m_propertyTable->entries()[entryIndex - 1].attributes;
@@ -649,7 +645,7 @@ size_t Structure::get(const Identifier& propertyName, unsigned& attributes)
 
         entryIndex = m_propertyTable->entryIndices[i & m_propertyTable->sizeMask];
         if (entryIndex == emptyEntryIndex)
-            return WTF::notFound;
+            return notFound;
 
         if (rep == m_propertyTable->entries()[entryIndex - 1].key) {
             attributes = m_propertyTable->entries()[entryIndex - 1].attributes;
@@ -661,7 +657,7 @@ size_t Structure::get(const Identifier& propertyName, unsigned& attributes)
 size_t Structure::put(const Identifier& propertyName, unsigned attributes)
 {
     ASSERT(!propertyName.isNull());
-    ASSERT(get(propertyName) == WTF::notFound);
+    ASSERT(get(propertyName) == notFound);
 
     checkConsistency();
 
@@ -756,7 +752,7 @@ size_t Structure::remove(const Identifier& propertyName)
     UString::Rep* rep = propertyName._ustring.rep();
 
     if (!m_propertyTable)
-        return WTF::notFound;
+        return notFound;
 
 #if DUMP_PROPERTYMAP_STATS
     ++numProbes;
@@ -771,7 +767,7 @@ size_t Structure::remove(const Identifier& propertyName)
     while (1) {
         entryIndex = m_propertyTable->entryIndices[i & m_propertyTable->sizeMask];
         if (entryIndex == emptyEntryIndex)
-            return WTF::notFound;
+            return notFound;
 
         key = m_propertyTable->entries()[entryIndex - 1].key;
         if (rep == key)
--- third_party/WebKit/JavaScriptCore/runtime/Structure.h
+++ third_party/WebKit/JavaScriptCore/runtime/Structure.h
@@ -107,7 +107,7 @@ namespace JSC {
         bool hasGetterSetterProperties() const { return m_hasGetterSetterProperties; }
         void setHasGetterSetterProperties(bool hasGetterSetterProperties) { m_hasGetterSetterProperties = hasGetterSetterProperties; }
 
-        bool isEmpty() const { return m_propertyTable ? !m_propertyTable->keyCount : m_offset == WTF::notFound; }
+        bool isEmpty() const { return m_propertyTable ? !m_propertyTable->keyCount : m_offset == noOffset; }
 
     private:
         Structure(JSValue* prototype, const TypeInfo&);
@@ -140,9 +140,17 @@ namespace JSC {
             return &m_refCount;
         }
 
+        signed char transitionCount() const
+        {
+            // Since the number of transitions is always the same as m_offset, we keep the size of Structure down by not storing both.
+            return m_offset == noOffset ? 0 : m_offset + 1;
+        }
+
         static const unsigned emptyEntryIndex = 0;
     
-        static const size_t s_maxTransitionLength = 64;
+        static const signed char s_maxTransitionLength = 64;
+
+        static const signed char noOffset = -1;
 
         TypeInfo m_typeInfo;
 
@@ -152,7 +160,6 @@ namespace JSC {
         RefPtr<Structure> m_previous;
         RefPtr<UString::Rep> m_nameInPrevious;
 
-        size_t m_transitionCount;
         union {
             Structure* singleTransition;
             StructureTransitionTable* table;
@@ -163,7 +170,7 @@ namespace JSC {
         PropertyMapHashTable* m_propertyTable;
 
         size_t m_propertyStorageCapacity;
-        size_t m_offset;
+        signed char m_offset;
 
         bool m_isDictionary : 1;
         bool m_isPinnedPropertyTable : 1;
