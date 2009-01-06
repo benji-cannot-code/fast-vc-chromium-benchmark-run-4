d94849d0b08a94b30b429f166b84b4353e396f1e
finnur@google.com <finnur@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-06 21:29:26 +0000

    Applying patch for Mohamed Mansour (reviewed by pkasting): http://codereview.chromium.org/16246
    CRASH at Tab::OnMouseReleased
    
    Some strange thing is happening that it crashes for view::HitTest, it can never
    ENTER that function. It seems that the tab is being destroyed and as pkasting
    stated on IRC: 
    "Part of this may be because our retarded Views designs can't distinguish which
    buttons are being held versus clicked in these sorts of cases (I filed a bug on
    this about a year and a half ago, internally)"
    
    When a tab has ended dragging (EndDrag), if something was being dragged and you
    end it, it actually cleans the TabDelegate and assigns its value to 'freed
    memory' which is 0xfeeefeee. Therefore it crashes while dragging, because the
    object no longer exists. I couldn't do if (delegate_) cause that always returns
    true since delegate has 0xfeeefeee. 
    
    So I just changed the return type from void to bool for underlying EndDragImpl
    and pumped it to tab.cc. That way, we can know if a tab is destroyed or not.
    
    BUG=5819 (http://crbug.com/5819)
    
    TEST=Dragging tabs around, closing while dragging, and closing tabs.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7615 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/dragged_tab_controller.cc
+++ chrome/browser/views/tabs/dragged_tab_controller.cc
@@ -356,8 +356,8 @@ void DraggedTabController::Drag() {
   }
 }
 
-void DraggedTabController::EndDrag(bool canceled) {
-  EndDragImpl(canceled ? CANCELED : NORMAL);
+bool DraggedTabController::EndDrag(bool canceled) {
+  return EndDragImpl(canceled ? CANCELED : NORMAL);
 }
 
 Tab* DraggedTabController::GetDragSourceTabForContents(
@@ -922,7 +922,7 @@ Tab* DraggedTabController::GetTabMatchingDraggedContents(
   return index == TabStripModel::kNoTab ? NULL : tabstrip->GetTabAt(index);
 }
 
-void DraggedTabController::EndDragImpl(EndDragType type) {
+bool DraggedTabController::EndDragImpl(EndDragType type) {
   bring_to_front_timer_.Stop();
 
   // Hide the current dock controllers.
@@ -960,6 +960,8 @@ void DraggedTabController::EndDragImpl(EndDragType type) {
   // If we're not destroyed now, we'll be destroyed asynchronously later.
   if (destroy_now)
     source_tabstrip_->DestroyDragController();
+
+  return destroy_now;
 }
 
 void DraggedTabController::RevertDrag() {
