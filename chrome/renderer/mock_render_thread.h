46d50a1523b496e9f43b238f6c726705211d8a7d
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-06 19:18:02 +0000

    Fix a leak in the RenderView tests. We were responsible for deleting the reply deserializer for sync messages, which wasn't happening for all cases.
    BUG=6037
    Review URL: http://codereview.chromium.org/17206
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7597 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/mock_render_thread.h
+++ chrome/renderer/mock_render_thread.h
@@ -109,7 +109,7 @@ class MockRenderThread : public RenderThreadBase {
   IPC::Channel::Listener* widget_;
 
   // The last known good deserializer for sync messages.
-  IPC::MessageReplyDeserializer* reply_deserializer_;
+  scoped_ptr<IPC::MessageReplyDeserializer> reply_deserializer_;
 
   std::vector<MessagePair> messages_;
 };
