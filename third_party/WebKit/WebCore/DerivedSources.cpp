8ace6da331fd1c39eb06c25398785cbdb205c202
alice.liu@apple.com <alice.liu@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-06 01:50:47 +0000

    Build fix from 39625
    git-svn-id: svn://svn.chromium.org/blink/trunk@39633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/DerivedSources.cpp
+++ third_party/WebKit/WebCore/DerivedSources.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSBarInfo.cpp"
 #include "JSCanvasGradient.cpp"
 #include "JSCanvasPattern.cpp"
-#include "JSCanvasPixelArray.cpp"
 #include "JSCanvasRenderingContext2D.cpp"
 #include "JSCDATASection.cpp"
 #include "JSCharacterData.cpp"
