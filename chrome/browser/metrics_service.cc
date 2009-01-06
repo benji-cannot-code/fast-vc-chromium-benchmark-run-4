54f622f265cd6ba01f4660160f491707b3e83c45
avi@google.com <avi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-06 21:39:04 +0000

    Change "dll" to "plugin" where appropriate.
    Review URL: http://codereview.chromium.org/16543
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7617 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics_service.cc
+++ chrome/browser/metrics_service.cc
@@ -82,7 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // are:
 //
 //    INITIALIZED,            // Constructor was called.
-//    PLUGIN_LIST_REQUESTED,  // Waiting for DLL list to be loaded.
+//    PLUGIN_LIST_REQUESTED,  // Waiting for plugin list to be loaded.
 //    PLUGIN_LIST_ARRIVED,    // Waiting for timer to send initial log.
 //    INITIAL_LOG_READY,      // Initial log generated, and waiting for reply.
 //    SEND_OLD_INITIAL_LOGS,  // Sending unsent logs from previous session.
@@ -95,7 +95,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The MS has been constructed, but has taken no actions to compose the
 // initial log.
 //
-//    PLUGIN_LIST_REQUESTED,  // Waiting for DLL list to be loaded.
+//    PLUGIN_LIST_REQUESTED,  // Waiting for plugin list to be loaded.
 // Typically about 30 seconds after startup, a task is sent to a second thread
 // to get the list of plugins.  That task will (when complete) make an async
 // callback (via a Task) to indicate the completion.
@@ -731,7 +731,7 @@ void MetricsService::AddOrRemoveObserver(NotificationObserver* observer,
 
 void MetricsService::PushPendingLogsToUnsentLists() {
   if (state_ < INITIAL_LOG_READY)
-    return;  // We didn't and still don't have time to get DLL list etc.
+    return;  // We didn't and still don't have time to get plugin list etc.
 
   if (pending_log()) {
     PreparePendingLogText();
