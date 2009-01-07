5d900a02477174fe7075af06dc788d5ce0c31f4a
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-07 18:40:46 +0000

    Integrate grit into mac build. Add more files that are building in scons to the xcode project Review URL: http://codereview.chromium.org/17237
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7671 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/chrome.xcodeproj/browser_prebuild.sh
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -ex
+GENERATED_DIR="${CONFIGURATION_TEMP_DIR}/generated"
+GRIT_DIR="${GENERATED_DIR}/grit"
+mkdir -p "${GRIT_DIR}"
+
+python "${PROJECT_DIR}/../tools/grit/grit.py" \
+    -i "${PROJECT_DIR}/app/generated_resources.grd" build \
+    -o "${GRIT_DIR}"
+
+python "${PROJECT_DIR}/../tools/grit/grit.py" \
+    -i "${PROJECT_DIR}/app/chromium_strings.grd" build \
+    -o "${GRIT_DIR}"
