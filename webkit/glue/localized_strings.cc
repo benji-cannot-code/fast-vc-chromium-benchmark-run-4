c6a15541322d018a3a95c32ee2ce6c46a0465df1
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-08 16:16:27 +0000

    Replace stub bogus_webkit_strings.h with real generated header via grit Review URL: http://codereview.chromium.org/17267
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7733 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/localized_strings.cc
+++ webkit/glue/localized_strings.cc
@@ -38,14 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "build/build_config.h"
 #include "webkit/glue/glue_util.h"
-
-#if defined(OS_MACOSX)
-// TODO:(pinkerton): only windows has the GRIT machinery, so we've created a
-// temporary generated header until we can figure out the l10n strategy.
-#include "bogus_webkit_strings.h"
-#else
 #include "webkit_strings.h"
-#endif
 
 using namespace WebCore;
 
--- webkit/tools/test_shell/test_shell.cc
+++ webkit/tools/test_shell/test_shell.cc
@@ -38,12 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/tools/test_shell/simple_resource_loader_bridge.h"
 #include "webkit/tools/test_shell/test_navigation_controller.h"
 #include "webkit/tools/test_shell/test_shell_switches.h"
-
-#if defined(OS_MACOSX)
-#include "webkit/glue/bogus_webkit_strings.h"
-#elif defined(OS_WIN) || defined(OS_LINUX)
 #include "webkit_strings.h"
-#endif
 
 #include "SkBitmap.h"
 
