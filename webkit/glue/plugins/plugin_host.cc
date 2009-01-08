506d1ea040f8342a0b85f13daad17b2494e46867
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-08 00:37:53 +0000

    Stub out NPN_Construct.
    Review URL: http://codereview.chromium.org/16564
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7707 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/plugin_host.cc
+++ webkit/glue/plugins/plugin_host.cc
@@ -862,14 +862,5 @@ void NPN_PluginThreadAsyncCall(NPP id,
   }
 }
 
-bool NPN_Construct(NPP npp,
-                   NPObject* obj,
-                   const NPVariant *args,
-                   uint32_t argCount,
-                   NPVariant *result) {
-  NOTREACHED();
-  return false;
-}
-
 } // extern "C"
 
