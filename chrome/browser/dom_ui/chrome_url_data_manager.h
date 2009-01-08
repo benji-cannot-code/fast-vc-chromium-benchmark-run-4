a4da0b9444262ae4990fa31c111452576e40f912
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-08 12:07:23 +0000

    Porting in chrome/ (add 6 files to Linux SCons build)
    Review URL: http://codereview.chromium.org/17052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7725 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/chrome_url_data_manager.h
+++ chrome/browser/dom_ui/chrome_url_data_manager.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class GURL;
 class MessageLoop;
 class URLRequest;
+class URLRequestChromeJob;
 class URLRequestJob;
 
 // To serve dynamic data off of chrome: URLs, implement the
--- chrome/browser/ssl_manager.h
+++ chrome/browser/ssl_manager.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_registrar.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/render_messages.h"
-#include "chrome/views/link.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_errors.h"
 #include "net/base/ssl_info.h"
