2ce322fb684b15732d7cbb5d31425731aba608f9
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-11 08:06:58 +0000

    2009-01-11  Dmitry Titov  <dimich@chromium.org>
            Reviewed by Darin Adler.
    
            Make ScheduledAction (Timeout callback) able to execute the callback under WorkerContext.
            Part of 'Timers in Workers' work.
            https://bugs.webkit.org/show_bug.cgi?id=23223
    
            * bindings/js/ScheduledAction.cpp:
            (WebCore::ScheduledAction::execute): new method that takes WorkerContext.
            (WebCore::ScheduledAction::executeFunctionInContext): Executes a JSFunction in proper context with 'this' object.
            * bindings/js/ScheduledAction.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39783 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/ScheduledAction.h
+++ third_party/WebKit/WebCore/bindings/js/ScheduledAction.h
@@ -25,10 +25,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <runtime/Protect.h>
 #include <wtf/Vector.h>
 
+namespace JSC {
+    class JSGlobalObject;
+}
+
 namespace WebCore {
 
-    class JSDOMWindowShell;
+    class Document;
     class ScriptExecutionContext;
+    class WorkerContext;
 
    /* An action (either function or string) to be executed after a specified
     * time interval, either once or repeatedly. Used for window.setTimeout()
@@ -41,11 +46,15 @@ namespace WebCore {
             : m_code(code)
         {
         }
-         
+
         void execute(ScriptExecutionContext*);
 
     private:
-        void execute(JSDOMWindowShell*);
+        void executeFunctionInContext(JSC::JSGlobalObject*, JSC::JSValuePtr thisValue);
+        void execute(Document*);
+#if ENABLE(WORKERS)        
+        void execute(WorkerContext*);
+#endif
 
         JSC::ProtectedJSValuePtr m_function;
         Vector<JSC::ProtectedJSValuePtr> m_args;
