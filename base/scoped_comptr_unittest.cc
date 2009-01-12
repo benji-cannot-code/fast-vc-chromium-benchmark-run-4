0c36cccf6c7e0a89be7c9f98aa0506b203f3e63a
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-12 21:47:45 +0000

    Removing unnecessary include that broke the dependency tests.
    TBR=maruel
    
    Review URL: http://codereview.chromium.org/17622
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7897 0039d316-1c4b-4281-b951-d872f2087c98

--- base/scoped_comptr_unittest.cc
+++ base/scoped_comptr_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/scoped_comptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "chrome/common/win_util.h"
 
 #if defined(OS_WIN)
 #include <shlobj.h>
