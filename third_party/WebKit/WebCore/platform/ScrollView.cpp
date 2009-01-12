a6475a68411b5e9712f0508595546465615fcf9e
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-12 22:15:18 +0000

    2009-01-12  David Hyatt  <hyatt@apple.com>
            Fix 0.6% regression in PLT.  Make sure not to waste time updating NSScrollView's can blit on scroll state
            in the ScrollView constructor, since this is always going to be properly updated by FrameView anyway.
    
            Reviewed by Oliver Hunt
    
            * platform/ScrollView.cpp:
            (WebCore::ScrollView::ScrollView):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ScrollView.cpp
+++ third_party/WebKit/WebCore/platform/ScrollView.cpp
@@ -51,8 +51,6 @@ ScrollView::ScrollView()
     , m_useFixedLayout(false)
 {
     platformInit();
-    if (platformWidget())
-        platformSetCanBlitOnScroll(true);
 }
 
 ScrollView::~ScrollView()
