41753d04803ca639566b70d70b9bdde9da082180
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-12 20:08:50 +0000

    Hook up skia's assertions to our logging system. Review URL: http://codereview.chromium.org/17285
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7889 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/include/corecg/SkPostConfig.h
+++ skia/include/corecg/SkPostConfig.h
@@ -97,11 +97,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // Chrome already defines WIN32_LEAN_AND_MEAN so no need to define it here.
 
     #include <windows.h>
-    // End Chrome-specific changes
 
+    #include <stdio.h>
     #ifndef SK_DEBUGBREAK
-        #define SK_DEBUGBREAK(cond)     do { if (!(cond)) DebugBreak(); } while (false)
+        #define SK_DEBUGBREAK(cond) do { if (!(cond)) { \
+            SkDebugf("%s:%d: failed assertion \"%s\"\n", \
+            __FILE__, __LINE__, #cond); SK_CRASH(); } } while (false)
     #endif
+
+    // End Chrome-specific changes
 
     #ifdef SK_BUILD_FOR_WIN32
         #define strcasecmp(a, b)        stricmp(a, b)
