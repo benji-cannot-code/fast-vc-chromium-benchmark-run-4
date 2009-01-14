fc481986cbefe5aa4ef7c883e7fe26d1c134fe14
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-14 03:05:08 +0000

    disable failing media unit tests
    these don't work on my machine either
    
    TBR=cpu
    
    Review URL: http://codereview.chromium.org/18039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8000 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/audio_output_win_unittest.cc
+++ media/audio/win/audio_output_win_unittest.cc
@@ -178,7 +178,7 @@ TEST(WinAudioTest, PCMWaveStreamGetAndClose) {
 }
 
 // Test that it can be opened and closed.
-TEST(WinAudioTest, PCMWaveStreamOpenAndClose) {
+TEST(WinAudioTest, DISABLED_PCMWaveStreamOpenAndClose) {
   AudioManager* audio_man = AudioManager::GetAudioManager();
   ASSERT_TRUE(NULL != audio_man);
   AudioOutputStream* oas =
@@ -190,7 +190,7 @@ TEST(WinAudioTest, PCMWaveStreamOpenAndClose) {
 
 // Test that it uses the double buffers correctly. Because it uses the actual
 // audio device, you might hear a short pop noise for a short time.
-TEST(WinAudioTest, PCMWaveStreamDoubleBuffer) {
+TEST(WinAudioTest, DISABLED_PCMWaveStreamDoubleBuffer) {
   AudioManager* audio_man = AudioManager::GetAudioManager();
   ASSERT_TRUE(NULL != audio_man);
   AudioOutputStream* oas =
@@ -210,7 +210,7 @@ TEST(WinAudioTest, PCMWaveStreamDoubleBuffer) {
 // This test produces actual audio for 1.5 seconds on the default wave
 // device at 44.1K s/sec. Parameters have been chosen carefully so you should
 // not hear pops or noises while the sound is playing.
-TEST(WinAudioTest, PCMWaveStreamPlay200HzTone44Kss) {
+TEST(WinAudioTest, DISABLED_PCMWaveStreamPlay200HzTone44Kss) {
   AudioManager* audio_man = AudioManager::GetAudioManager();
   ASSERT_TRUE(NULL != audio_man);
   AudioOutputStream* oas =
@@ -232,7 +232,7 @@ TEST(WinAudioTest, PCMWaveStreamPlay200HzTone44Kss) {
 // This test produces actual audio for for 1.5 seconds on the default wave
 // device at 22K s/sec. Parameters have been chosen carefully so you should
 // not hear pops or noises while the sound is playing.
-TEST(WinAudioTest, PCMWaveStreamPlay200HzTone22Kss) {
+TEST(WinAudioTest, DISABLED_PCMWaveStreamPlay200HzTone22Kss) {
   AudioManager* audio_man = AudioManager::GetAudioManager();
   ASSERT_TRUE(NULL != audio_man);
   AudioOutputStream* oas =
