74d5ee41e82dcc1cdd19b9c644311ff2a064f929
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-14 17:58:58 +0000

    Reviewed by Darin Adler.
            https://bugs.webkit.org/show_bug.cgi?id=23316
            Application cache updating always fails with an assertion
    
            Test: http/tests/appcache/update-cache.html
    
            * loader/appcache/ApplicationCacheGroup.cpp:
            (WebCore::ApplicationCacheGroup::documentLoaderDestroyed): Correctly handle the case where
            multiple caches survive the last DocumentLoader destruction, preventing a crash in release
            builds.
            (WebCore::ApplicationCacheGroup::setNewestCache): Removed a bogus assertion.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39891 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/appcache/resources/versioned-manifest.php
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+require_once '../../resources/portabilityLayer.php';
+
+$tmpFile = sys_get_temp_dir() . "/" . "appcache_manifest_counter";
+
+function getCount($file)
+{
+    if (!file_exists($file)) {
+        file_put_contents($file, "0");
+        return "0";
+    }
+    return file_get_contents($file);
+}
+
+function stepCounter($file)
+{
+    if (file_exists($file)) {
+        $value = getCount($file);
+        file_put_contents($file, ++$value);
+    }
+}
+
+header("Expires: Thu, 01 Dec 2003 16:00:00 GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Content-Type: text/cache-manifest");
+
+if ("step" == $_GET['command'])
+    stepCounter($tmpFile);
+
+print("CACHE MANIFEST\n");
+print("# version " . getCount($tmpFile) . "\n");
+print("counter.php\n");
+print("NETWORK:\n");
+print("versioned-manifest.php?command=\n");
+?>
