bb27b2deedfbee85d51fa0688cd362345a723166
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-14 17:49:12 +0000

    Build fix: altbase.h inclusion in hang_monitor
    r8020 tickled an issue by removing altbase.h from a header file where it
    shouldn't have been needed.
    
    Review URL: http://codereview.chromium.org/18228
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8022 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/hang_monitor/hung_window_detector.cc
+++ chrome/browser/hang_monitor/hung_window_detector.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/hang_monitor/hung_window_detector.h"
 
 #include <windows.h>
+#include <atlbase.h>
 
 #include "base/logging.h"
 #include "chrome/app/result_codes.h"
