e2addb84e3729be69befca9e2ba868e80b1fad76
wtc@google.com <wtc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-15 01:55:25 +0000

    Add a bucket in the connection type histograms for intermediate CA certificates containing MD5 signatures.
    Change copyright year of new files to 2009.
    
    R=jar
    BUG=6102
    Review URL: http://codereview.chromium.org/18063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8070 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/connection_type_histograms.h
+++ net/base/connection_type_histograms.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -24,6 +24,8 @@ enum ConnectionType {
                            // the certificate chain (excluding root)
   CONNECTION_SSL_MD4 = 4,  // An SSL connection with an MD4 certificate in
                            // the certificate chain (excluding root)
+  CONNECTION_SSL_MD5_CA = 5,  // An SSL connection with an MD5 CA certificate
+                              // in the certificate chain (excluding root)
   NUM_OF_CONNECTION_TYPES
 };
 
