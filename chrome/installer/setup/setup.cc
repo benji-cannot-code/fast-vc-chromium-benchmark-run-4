84e20da49168a673fb57a8b876c526e238e7c537
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-15 00:51:16 +0000

    Write installer success result to the registry even for user level installs.
    BUG=1549439
    
    Review URL: http://codereview.chromium.org/18249
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8056 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/setup/setup.cc
+++ chrome/installer/setup/setup.cc
@@ -199,9 +199,6 @@ installer_util::InstallStatus installer::InstallOrUpdateChrome(
   std::wstring install_path(GetChromeInstallPath(system_install));
   if (install_path.empty()) {
     LOG(ERROR) << "Could not get installation destination path.";
-    InstallUtil::WriteInstallerResult(system_install,
-                                      installer_util::INSTALL_FAILED,
-                                      IDS_INSTALL_FAILED_BASE, NULL);
     return installer_util::INSTALL_FAILED;
   } else {
     LOG(INFO) << "install destination path: " << install_path;
@@ -218,9 +215,6 @@ installer_util::InstallStatus installer::InstallOrUpdateChrome(
   installer_util::InstallStatus result;
   if (!install_success) {
     LOG(ERROR) << "Install failed.";
-    InstallUtil::WriteInstallerResult(system_install,
-                                     installer_util::INSTALL_FAILED,
-                                     IDS_INSTALL_FAILED_BASE, NULL);
     result = installer_util::INSTALL_FAILED;
   } else {
     if (!installed_version) {
