26bec508f8b9c6dc8c056488d77ea334b51709d9
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-15 23:24:55 +0000

    Fix current working directory problem in chrome_tests.sh.
    Review URL: http://codereview.chromium.org/18292
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8133 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/chrome_tests.sh
+++ tools/valgrind/chrome_tests.sh
@@ -2,4 +2,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/sh
 
 export THISDIR=`dirname $0`
-PYTHONPATH=$THISDIR/../../webkit/tools/layout_tests/:$THISDIR/../purify:$THISDIR/../python "./chrome_tests.py" "$@"
+PYTHONPATH=$THISDIR/../../webkit/tools/layout_tests/:$THISDIR/../purify:$THISDIR/../python "$THISDIR/chrome_tests.py" "$@"
