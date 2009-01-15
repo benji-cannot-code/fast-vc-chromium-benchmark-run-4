ce573e7d6be9cd98f370290e99078e684b29af6b
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-15 21:31:59 +0000

    2009-01-15  Craig Schlenter  <craig.schlenter@gmail.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=23347
            Compilation of JavaScriptCore/wtf/ThreadingPthreads.cpp fails on Linux
    
            * wtf/ThreadingPthreads.cpp: included limits.h as INT_MAX is defined there.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/ThreadingPthreads.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/ThreadingPthreads.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RandomNumberSeed.h"
 
 #include <errno.h>
+#include <limits.h>
 #include <sys/time.h>
 
 namespace WTF {
