280f3035d0069dbac614f61fc6df4eafe76d029a
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-15 21:15:24 +0000

    Bug 23225: REGRESSION: Assertion failure in reparseInPlace() (m_sourceElements) at sfgate.com <https://bugs.webkit.org/show_bug.cgi?id=23225> <rdar://problem/6487432>
    Reviewed by Geoff Garen
    
    Character position for open and closing brace was incorrectly referencing m_position to
    record their position in a source document, however this is unsafe as BOMs may lead to
    m_position being an arbitrary position from the real position of the current character.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39942 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/bom-in-file-retains-correct-offset.js
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("This ensures that BOM's scattered through a source file do not break parsing");
+function f(){successfullyReparsed=true;shouldBeTrue('successfullyReparsed');successfullyParsed=true}﻿f();
