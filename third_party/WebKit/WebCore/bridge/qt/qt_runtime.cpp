e72384cdd32c2e01b6669a017ff8d099e85fcf18
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-15 23:48:25 +0000

    2009-01-15  Anders Carlsson  <andersca@apple.com>
            Reviewed by Darin Adler.
    
            Add QtInstance::getInstance and start using it.
    
            * bridge/qt/qt_instance.cpp:
            (JSC::Bindings::QtRuntimeObjectImp::mark):
            (JSC::Bindings::QtRuntimeObjectImp::classInfo):
            (JSC::Bindings::):
            (JSC::Bindings::QtInstance::getInstance):
            * bridge/qt/qt_instance.h:
            * bridge/qt/qt_runtime.cpp:
            (JSC::Bindings::convertValueToQVariant):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39950 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
+++ third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
@@ -549,7 +549,7 @@ QVariant convertValueToQVariant(ExecState* exec, JSValuePtr value, QMetaType::Ty
 
         case QMetaType::QObjectStar:
             if (type == QObj) {
-                QtInstance* qtinst = static_cast<QtInstance*>(Instance::getInstance(object, Instance::QtLanguage));
+                QtInstance* qtinst = QtInstance::getInstance(object);
                 if (qtinst) {
                     if (qtinst->getObject()) {
                         qConvDebug() << "found instance, with object:" << (void*) qtinst->getObject();
@@ -573,7 +573,7 @@ QVariant convertValueToQVariant(ExecState* exec, JSValuePtr value, QMetaType::Ty
 
         case QMetaType::VoidStar:
             if (type == QObj) {
-                QtInstance* qtinst = static_cast<QtInstance*>(Instance::getInstance(object, Instance::QtLanguage));
+                QtInstance* qtinst = QtInstance::getInstance(object);
                 if (qtinst) {
                     if (qtinst->getObject()) {
                         qConvDebug() << "found instance, with object:" << (void*) qtinst->getObject();
