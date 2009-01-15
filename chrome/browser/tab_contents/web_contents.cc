eff28108cd5732806ee65deed0b4407a334e1392
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-15 01:45:09 +0000

    fix bustage
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8067 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/web_contents.cc
+++ chrome/browser/tab_contents/web_contents.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/load_from_memory_cache_details.h"
 #include "chrome/browser/load_notification_details.h"
 #include "chrome/browser/modal_html_dialog_delegate.h"
-#include "chrome/browser/password_manager.h"
+#include "chrome/browser/password_manager/password_manager.h"
 #include "chrome/browser/plugin_installer.h"
 #include "chrome/browser/plugin_service.h"
 #include "chrome/browser/printing/print_job.h"
