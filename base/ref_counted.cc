2fbec394e2737d3fdfa0ea07bbb343ca0eac1a1e
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-16 18:06:09 +0000

    Reverting 8180. Review URL: http://codereview.chromium.org/18319
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8186 0039d316-1c4b-4281-b951-d872f2087c98

--- base/ref_counted.cc
+++ base/ref_counted.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ref_counted.h"
 
 #include "base/logging.h"
-#include "base/thread_collision_warner.h"
 
 namespace base {
 
@@ -25,9 +24,6 @@ RefCountedBase::~RefCountedBase() {
 }
 
 void RefCountedBase::AddRef() {
-  // TODO(maruel): Add back once it doesn't assert 500 times/sec.
-  // Current thread books the critical section "AddRelease" without release it.
-  // DFAKE_SCOPED_LOCK_THREAD_LOCKED(add_release_);
 #ifndef NDEBUG
   DCHECK(!in_dtor_);
 #endif
@@ -35,9 +31,6 @@ void RefCountedBase::AddRef() {
 }
 
 bool RefCountedBase::Release() {
-  // TODO(maruel): Add back once it doesn't assert 500 times/sec.
-  // Current thread books the critical section "AddRelease" without release it.
-  // DFAKE_SCOPED_LOCK_THREAD_LOCKED(add_release_);
 #ifndef NDEBUG
   DCHECK(!in_dtor_);
 #endif
