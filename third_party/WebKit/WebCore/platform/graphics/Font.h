735f03159dbd3207f34f913960f1bcbd95ff8b60
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-16 23:39:00 +0000

    Fix the Qt build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@39994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/Font.h
+++ third_party/WebKit/WebCore/platform/graphics/Font.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SimpleFontData.h"
 #include <wtf/HashMap.h>
 
+#include <math.h>
+
 #if PLATFORM(QT)
 #include <QFont>
 #endif
