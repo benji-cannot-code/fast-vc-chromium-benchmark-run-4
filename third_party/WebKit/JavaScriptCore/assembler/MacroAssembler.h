a28675f0e629413301de37dfe1b8ce7256b047eb
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-16 06:51:20 +0000

    2009-01-15  Gavin Barraclough  <barraclough@apple.com>
            Reviewed by Oliver Hunt.
    
            Add support for integer addition, subtraction and multiplication
            in JIT code on x86-64.
    
            * assembler/MacroAssembler.h:
            (JSC::MacroAssembler::mul32):
            (JSC::MacroAssembler::sub32):
            (JSC::MacroAssembler::joMul32):
            (JSC::MacroAssembler::joSub32):
            * jit/JIT.cpp:
            (JSC::JIT::privateCompileMainPass):
            (JSC::JIT::privateCompileSlowCases):
            * jit/JIT.h:
            * jit/JITArithmetic.cpp:
            (JSC::JIT::compileFastArith_op_add):
            (JSC::JIT::compileFastArithSlow_op_add):
            (JSC::JIT::compileFastArith_op_mul):
            (JSC::JIT::compileFastArithSlow_op_mul):
            (JSC::JIT::compileFastArith_op_sub):
            (JSC::JIT::compileFastArithSlow_op_sub):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/assembler/MacroAssembler.h
+++ third_party/WebKit/JavaScriptCore/assembler/MacroAssembler.h
@@ -575,6 +575,11 @@ public:
         m_assembler.idivl_r(divisor);
     }
 
+    void mul32(RegisterID src, RegisterID dest)
+    {
+        m_assembler.imull_rr(src, dest);
+    }
+    
     void mul32(Imm32 imm, RegisterID src, RegisterID dest)
     {
         m_assembler.imull_i32r(src, imm.m_value, dest);
@@ -704,6 +709,11 @@ public:
 #endif
     }
 
+    void sub32(RegisterID src, RegisterID dest)
+    {
+        m_assembler.subl_rr(src, dest);
+    }
+    
     void sub32(Imm32 imm, RegisterID dest)
     {
         m_assembler.subl_ir(imm.m_value, dest);
@@ -1793,9 +1803,21 @@ public:
         return Jump(m_assembler.jo());
     }
     
+    Jump joMul32(RegisterID src, RegisterID dest)
+    {
+        mul32(src, dest);
+        return Jump(m_assembler.jo());
+    }
+    
     Jump joMul32(Imm32 imm, RegisterID src, RegisterID dest)
     {
         mul32(imm, src, dest);
+        return Jump(m_assembler.jo());
+    }
+    
+    Jump joSub32(RegisterID src, RegisterID dest)
+    {
+        sub32(src, dest);
         return Jump(m_assembler.jo());
     }
     
