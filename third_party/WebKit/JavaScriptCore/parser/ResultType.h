03fb7236dd7d0c84c589be40a00467478360a4e7
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-17 00:39:01 +0000

    2009-01-16  Gavin Barraclough  <barraclough@apple.com>
            Reviewed by Oliver Hunt.
    
            Add support to x86-64 JIT for inline double precision arithmetic ops.
            +5/6% on x86-64, JIT enabled, sunspider.
    
            * assembler/MacroAssembler.h:
            (JSC::MacroAssembler::addPtr):
            * assembler/X86Assembler.h:
            (JSC::X86Assembler::movq_rr):
            * jit/JIT.h:
            * jit/JITArithmetic.cpp:
            (JSC::JIT::compileFastArith_op_pre_inc):
            (JSC::JIT::compileBinaryArithOp):
            (JSC::JIT::compileBinaryArithOpSlowCase):
            (JSC::JIT::compileFastArith_op_add):
            (JSC::JIT::compileFastArithSlow_op_add):
            (JSC::JIT::compileFastArith_op_mul):
            (JSC::JIT::compileFastArithSlow_op_mul):
            (JSC::JIT::compileFastArith_op_sub):
            (JSC::JIT::compileFastArithSlow_op_sub):
            * parser/ResultType.h:
            (JSC::ResultType::isReusable):
            (JSC::ResultType::isInt32):
            (JSC::ResultType::definitelyIsNumber):
            (JSC::ResultType::mightBeNumber):
            (JSC::ResultType::isNotNumber):
            (JSC::ResultType::unknownType):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@39998 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/parser/ResultType.h
+++ third_party/WebKit/JavaScriptCore/parser/ResultType.h
@@ -42,6 +42,8 @@ namespace JSC {
         static const Type TypeMaybeBool   = 0x20;
         static const Type TypeMaybeOther  = 0x40;
 
+        static const Type TypeBits = TypeMaybeNumber | TypeMaybeString | TypeMaybeNull | TypeMaybeBool | TypeMaybeOther;
+
         explicit ResultType(Type type)
             : m_type(type)
         {
@@ -49,29 +51,29 @@ namespace JSC {
         
         bool isReusable()
         {
-            return (m_type & TypeReusable);
+            return m_type & TypeReusable;
         }
 
         bool isInt32()
         {
-            return (m_type & TypeInt32);
+            return m_type & TypeInt32;
         }
 
         bool definitelyIsNumber()
         {
-            return ((m_type & ~TypeReusable) == TypeMaybeNumber);
-        }
-        
-        bool isNotNumber()
-        {
-            return !(m_type & TypeMaybeNumber);
+            return (m_type & TypeBits) == TypeMaybeNumber;
         }
         
         bool mightBeNumber()
         {
-            return !isNotNumber();
+            return m_type & TypeMaybeNumber;
         }
 
+        bool isNotNumber()
+        {
+            return !mightBeNumber();
+        }
+        
         static ResultType nullType()
         {
             return ResultType(TypeMaybeNull);
@@ -109,7 +111,7 @@ namespace JSC {
         
         static ResultType unknownType()
         {
-            return ResultType(TypeMaybeNumber | TypeMaybeString | TypeMaybeNull | TypeMaybeBool | TypeMaybeOther);
+            return ResultType(TypeBits);
         }
         
         static ResultType forAdd(ResultType op1, ResultType op2)
