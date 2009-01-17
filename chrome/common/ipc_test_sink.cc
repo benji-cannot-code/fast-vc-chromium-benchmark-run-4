9e984653b0d4cf8ffbd34b27c8fe4c207aebc9d6
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-17 05:41:48 +0000

    Factor out the message sink from MockRenderThread to a separate class. I will be using this in a RenderView test in the future. Review URL: http://codereview.chromium.org/18326
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8255 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/ipc_test_sink.cc
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/ipc_test_sink.h"
+
+namespace IPC {
+
+TestSink::TestSink() {
+}
+
+TestSink::~TestSink() {
+}
+
+void TestSink::OnMessageReceived(const Message& msg) {
+  messages_.push_back(Message(msg));
+}
+
+void TestSink::ClearMessages() {
+  messages_.clear();
+}
+
+const Message* TestSink::GetMessageAt(size_t index) const {
+  if (index >= messages_.size())
+    return NULL;
+  return &messages_[index];
+}
+
+const Message* TestSink::GetFirstMessageMatching(uint16 id) const {
+  for (size_t i = 0; i < messages_.size(); i++) {
+    if (messages_[i].type() == id)
+      return &messages_[i];
+  }
+  return NULL;
+}
+
+const Message* TestSink::GetUniqueMessageMatching(uint16 id) const {
+  size_t found_index = 0;
+  size_t found_count = 0;
+  for (size_t i = 0; i < messages_.size(); i++) {
+    if (messages_[i].type() == id) {
+      found_count++;
+      found_index = i;
+    }
+  }
+  if (found_count != 1)
+    return NULL;  // Didn't find a unique one.
+  return &messages_[found_index];
+}
+
+}  // namespace IPC
