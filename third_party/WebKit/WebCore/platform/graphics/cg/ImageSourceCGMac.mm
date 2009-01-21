f47e8e5cbaeaaed22f6ea305e63273893f863081
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-21 07:39:02 +0000

    2009-01-20  Darin Adler  <darin@apple.com>
            Reviewed by Alexey Proskuryakov.
    
            Bug 23448: memory leak in Mac version of preferredExtensionForImageSourceType
            https://bugs.webkit.org/show_bug.cgi?id=23448
    
            * platform/graphics/cg/ImageSourceCGMac.mm:
            (WebCore::preferredExtensionForImageSourceType): Use RetainPtr to avoid the leak.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40084 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/ImageSourceCGMac.mm
+++ third_party/WebKit/WebCore/platform/graphics/cg/ImageSourceCGMac.mm
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2008 Apple Inc. All Rights Reserved.
+ * Copyright (C) 2008, 2009 Apple Inc. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -42,7 +42,8 @@ String MIMETypeForImageSourceType(const String& uti)
 String preferredExtensionForImageSourceType(const String& uti)
 {
     RetainPtr<CFStringRef> type(AdoptCF, uti.createCFString());
-    return UTTypeCopyPreferredTagWithClass(type.get(), kUTTagClassFilenameExtension);
+    RetainPtr<CFStringRef> extension(AdoptCF, UTTypeCopyPreferredTagWithClass(type.get(), kUTTagClassFilenameExtension));
+    return extension.get();
 }
 
 } // namespace WebCore
