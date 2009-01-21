6c279dc5017c33474e561afc76e72c04b065bb3a
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-21 01:58:53 +0000

    Fix BrowserTest.WindowsSessionEnd by moving a virtual call from the constructor to the Init() function().
    The virtual call in the constructor of the base class doesn't
    get dispatched to the child class since we haven't started to
    construct it yet.  Instead, defer the call until after we've
    finished creating the object.
    
    Review URL: http://codereview.chromium.org/18601
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8354 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/window.cc
+++ chrome/views/window.cc
@@ -266,10 +266,6 @@ Window::Window(WindowDelegate* window_delegate)
   // behavior before calling Init.
   set_window_style(0);
   set_window_ex_style(0);
-  if (!IsAppWindow()) {
-    NotificationService::current()->AddObserver(
-        this, NOTIFY_ALL_APPWINDOWS_CLOSED, NotificationService::AllSources());
-  }
 }
 
 void Window::Init(HWND parent, const gfx::Rect& bounds) {
@@ -300,6 +296,11 @@ void Window::Init(HWND parent, const gfx::Rect& bounds) {
   SetClientView(window_delegate_->CreateClientView(this));
   SetInitialBounds(bounds);
   InitAlwaysOnTopState();
+
+  if (!IsAppWindow()) {
+    NotificationService::current()->AddObserver(
+        this, NOTIFY_ALL_APPWINDOWS_CLOSED, NotificationService::AllSources());
+  }
 }
 
 void Window::SetClientView(ClientView* client_view) {
