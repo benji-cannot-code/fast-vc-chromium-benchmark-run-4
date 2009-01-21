edd347df107e509256a96102dbecedb8335df038
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-21 06:38:55 +0000

    2009-01-20  Darin Adler  <darin@apple.com>
            Try to fix Wx build.
    
            * platform/wx/PasteboardWx.cpp:
            (WebCore::Pasteboard::writeSelection): Removed code that replaces backslash
            with backslashAsCurrencySymbol. I'm pretty sure this is not needed when writing
            to the clipboard. Other platforms don't do it. If it was needed, the new function
            to use would be displayStringModifiedByEncoding.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40082 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/wx/PasteboardWx.cpp
+++ third_party/WebKit/WebCore/platform/wx/PasteboardWx.cpp
@@ -55,10 +55,7 @@ void Pasteboard::writeSelection(Range* selectedRange, bool canSmartCopyOrDelete,
 {
     if (wxTheClipboard->Open())
     {
-        String text = frame->selectedText();
-        text.replace('\\', frame->backslashAsCurrencySymbol());
-        
-        wxTheClipboard->SetData( new wxTextDataObject(text) );
+        wxTheClipboard->SetData( new wxTextDataObject(frame->selectedText()) );
         wxTheClipboard->Close();
     }
 }
