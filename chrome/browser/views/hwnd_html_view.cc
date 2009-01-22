0868e13e3327e7a0593ffb799cedf03d83a0ab57
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-22 18:33:54 +0000

    Fix messed up include path. Review URL: http://codereview.chromium.org/18512
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8479 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/hwnd_html_view.cc
+++ chrome/browser/views/hwnd_html_view.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/views/hwnd_html_view.h"
 
-#include "chrome/browser/render_view_host.h"
-#include "chrome/browser/render_widget_host_view_win.h"
-#include "chrome/browser/render_view_host_delegate.h"
+#include "chrome/browser/renderer_host/render_view_host.h"
+#include "chrome/browser/renderer_host/render_widget_host_view_win.h"
+#include "chrome/browser/renderer_host/render_view_host_delegate.h"
 #include "chrome/browser/tab_contents/site_instance.h"
 #include "chrome/views/widget.h"
 
