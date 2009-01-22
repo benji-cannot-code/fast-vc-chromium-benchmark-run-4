78d91fe8b89d51d14a6a485336930f524f57690d
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-22 20:10:48 +0000

    Add back convolver_unittest.cc and image_operations_unittest.cc. Fix a TODO in platform_canvas_unittest.cc. Make vector_canvas_unittest.cc slightly less dependent on base/. Remove Windows 2000 checks.
    (Same as r8458 with platform_canvas_unittest.cc and SConscript fixed)
    BUG=5113
    Review URL: http://codereview.chromium.org/18511
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8498 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/platform_canvas_unittest.cc
+++ skia/ext/platform_canvas_unittest.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if defined(OS_WIN)
-#include <windows.h>
-#else
+#if !defined(OS_WIN)
 #include <unistd.h>
 #endif
 
@@ -18,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 #include "SkColor.h"
+
+// TODO(maruel): Removes once notImplemented() is not necessary anymore.
+#include "NotImplemented.h"
 
 namespace skia {
 
@@ -51,8 +52,7 @@ bool VerifyRect(const PlatformCanvas& canvas,
 
 // Checks whether there is a white canvas with a black square at the given
 // location in pixels (not in the canvas coordinate system).
-// TODO(ericroman): rename Square to Rect
-bool VerifyBlackSquare(const PlatformCanvas& canvas, int x, int y, int w, int h) {
+bool VerifyBlackRect(const PlatformCanvas& canvas, int x, int y, int w, int h) {
   return VerifyRect(canvas, SK_ColorWHITE, SK_ColorBLACK, x, y, w, h);
 }
 
@@ -89,7 +89,7 @@ void DrawNativeRect(PlatformCanvas& canvas, int x, int y, int w, int h) {
 }
 #else
 void DrawNativeRect(PlatformCanvas& canvas, int x, int y, int w, int h) {
-  NOTIMPLEMENTED();
+  notImplemented();
 }
 #endif
 
@@ -117,7 +117,9 @@ class LayerSaver {
   }
 
   ~LayerSaver() {
+#if defined(OS_WIN)
     canvas_.getTopPlatformDevice().fixupAlphaBeforeCompositing();
+#endif
     canvas_.restore();
   }
 
@@ -162,7 +164,7 @@ TEST(PlatformCanvas, SkLayer) {
     LayerSaver layer(canvas, kLayerX, kLayerY, kLayerW, kLayerH);
     canvas.drawColor(SK_ColorBLACK);
   }
-  EXPECT_TRUE(VerifyBlackSquare(canvas, kLayerX, kLayerY, kLayerW, kLayerH));
+  EXPECT_TRUE(VerifyBlackRect(canvas, kLayerX, kLayerY, kLayerW, kLayerH));
 }
 
 // Test native clipping.
@@ -176,7 +178,9 @@ TEST(PlatformCanvas, ClipRegion) {
   // with a black rectangle.
   // Note: Don't use LayerSaver, since internally it sets a clip region.
   DrawNativeRect(canvas, 0, 0, 16, 16);
+#if defined(OS_WIN)
   canvas.getTopPlatformDevice().fixupAlphaBeforeCompositing();
+#endif
   EXPECT_TRUE(VerifyCanvasColor(canvas, SK_ColorBLACK));
 
   // Test that intersecting disjoint clip rectangles sets an empty clip region
@@ -203,7 +207,7 @@ TEST(PlatformCanvas, FillLayer) {
     LayerSaver layer(canvas, kLayerX, kLayerY, kLayerW, kLayerH);
     DrawNativeRect(canvas, 0, 0, 100, 100);
   }
-  EXPECT_TRUE(VerifyBlackSquare(canvas, kLayerX, kLayerY, kLayerW, kLayerH));
+  EXPECT_TRUE(VerifyBlackRect(canvas, kLayerX, kLayerY, kLayerW, kLayerH));
 
   // Make a layer and fill it partially to make sure the translation is correct.
   canvas.drawColor(SK_ColorWHITE);
@@ -211,7 +215,7 @@ TEST(PlatformCanvas, FillLayer) {
     LayerSaver layer(canvas, kLayerX, kLayerY, kLayerW, kLayerH);
     DrawNativeRect(canvas, kInnerX, kInnerY, kInnerW, kInnerH);
   }
-  EXPECT_TRUE(VerifyBlackSquare(canvas, kInnerX, kInnerY, kInnerW, kInnerH));
+  EXPECT_TRUE(VerifyBlackRect(canvas, kInnerX, kInnerY, kInnerW, kInnerH));
 
   // Add a clip on the layer and fill to make sure clip is correct.
   canvas.drawColor(SK_ColorWHITE);
@@ -222,7 +226,7 @@ TEST(PlatformCanvas, FillLayer) {
     DrawNativeRect(canvas, 0, 0, 100, 100);
     canvas.restore();
   }
-  EXPECT_TRUE(VerifyBlackSquare(canvas, kInnerX, kInnerY, kInnerW, kInnerH));
+  EXPECT_TRUE(VerifyBlackRect(canvas, kInnerX, kInnerY, kInnerW, kInnerH));
 
   // Add a clip and then make the layer to make sure the clip is correct.
   canvas.drawColor(SK_ColorWHITE);
@@ -233,7 +237,7 @@ TEST(PlatformCanvas, FillLayer) {
     DrawNativeRect(canvas, 0, 0, 100, 100);
   }
   canvas.restore();
-  EXPECT_TRUE(VerifyBlackSquare(canvas, kInnerX, kInnerY, kInnerW, kInnerH));
+  EXPECT_TRUE(VerifyBlackRect(canvas, kInnerX, kInnerY, kInnerW, kInnerH));
 }
 
 // Test that translation + make layer works properly.
@@ -251,8 +255,8 @@ TEST(PlatformCanvas, TranslateLayer) {
     DrawNativeRect(canvas, 0, 0, 100, 100);
   }
   canvas.restore();
-  EXPECT_TRUE(VerifyBlackSquare(canvas, kLayerX + 1, kLayerY + 1,
-                                kLayerW, kLayerH));
+  EXPECT_TRUE(VerifyBlackRect(canvas, kLayerX + 1, kLayerY + 1,
+                              kLayerW, kLayerH));
 
   // Translate then make the layer.
   canvas.drawColor(SK_ColorWHITE);
@@ -263,8 +267,8 @@ TEST(PlatformCanvas, TranslateLayer) {
     DrawNativeRect(canvas, kInnerX, kInnerY, kInnerW, kInnerH);
   }
   canvas.restore();
-  EXPECT_TRUE(VerifyBlackSquare(canvas, kInnerX + 1, kInnerY + 1,
-                                kInnerW, kInnerH));
+  EXPECT_TRUE(VerifyBlackRect(canvas, kInnerX + 1, kInnerY + 1,
+                              kInnerW, kInnerH));
 
   // Make the layer then translate.
   canvas.drawColor(SK_ColorWHITE);
@@ -275,8 +279,8 @@ TEST(PlatformCanvas, TranslateLayer) {
     DrawNativeRect(canvas, kInnerX, kInnerY, kInnerW, kInnerH);
   }
   canvas.restore();
-  EXPECT_TRUE(VerifyBlackSquare(canvas, kInnerX + 1, kInnerY + 1,
-                                kInnerW, kInnerH));
+  EXPECT_TRUE(VerifyBlackRect(canvas, kInnerX + 1, kInnerY + 1,
+                              kInnerW, kInnerH));
 
   // Translate both before and after, and have a clip.
   canvas.drawColor(SK_ColorWHITE);
@@ -289,9 +293,8 @@ TEST(PlatformCanvas, TranslateLayer) {
     DrawNativeRect(canvas, 0, 0, 100, 100);
   }
   canvas.restore();
-  EXPECT_TRUE(VerifyBlackSquare(canvas, kInnerX + 2, kInnerY + 2,
-                                kInnerW, kInnerH));
+  EXPECT_TRUE(VerifyBlackRect(canvas, kInnerX + 2, kInnerY + 2,
+                              kInnerW, kInnerH));
 }
 
 }  // namespace skia
-
