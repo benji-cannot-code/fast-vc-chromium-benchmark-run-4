83140a09ac8d14343f63b271afb2ee97d4d4fd94
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-22 20:12:47 +0000

    2009-01-22  Anders Carlsson  <andersca@apple.com>
            Reviewed by Sam Weinig.
            
            Clean up the test plug-in code. We now always use the CG drawing model and the
            Cocoa event model. It is however possible to revert to the old Carbon event model by
            specifying forcecarbon=true in the embed/object tag.
            
            * DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.h:
            * DumpRenderTree/TestNetscapePlugIn.subproj/main.cpp:
            (NPP_New):
            (handleEventCarbon):
            (handleEventCocoa):
            (NPP_HandleEvent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40128 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.h
@@ -30,11 +30,13 @@ extern NPNetscapeFuncs *browser;
 
 typedef struct {
     NPObject header;
+
     NPP npp;
     NPBool eventLogging;
     NPBool logSetWindow;
     NPBool logDestroy;
     NPBool returnErrorFromNewStream;
+    NPEventModel eventModel;
     NPObject* testObject;
     NPStream* stream;
     char* onStreamLoad;
