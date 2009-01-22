348e2fbe7717e0a31d851a76b154946976841eaf
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-22 08:58:16 +0000

    2009-01-22  David Levin  <levin@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            Bug 22720: Make XMLHttpRequest work in Workers
            <https://bugs.webkit.org/show_bug.cgi?id=22720>
    
            Add copy/adopt for HTTPHeaderMap to allow the data to be passed across threads.
    
            No observable change in behavior, so no test.
    
            * GNUmakefile.am:
            * WebCore.pro:
            * WebCore.scons:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * WebCoreSources.bkl:
            * platform/network/HTTPHeaderMap.cpp: Added.
            (WebCore::HTTPHeaderMap::copyData):
            (WebCore::HTTPHeaderMap::adopt):
            * platform/network/HTTPHeaderMap.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40123 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/HTTPHeaderMap.h
+++ third_party/WebKit/WebCore/platform/network/HTTPHeaderMap.h
@@ -30,11 +30,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AtomicString.h"
 #include "AtomicStringHash.h"
 #include "StringHash.h"
+#include <memory>
+#include <utility>
 #include <wtf/HashMap.h>
+#include <wtf/Vector.h>
 
 namespace WebCore {
 
-    typedef HashMap<AtomicString, String, CaseFoldingHash> HTTPHeaderMap;
+    typedef Vector<std::pair<String, String> > HTTPHeaderMapData;
+
+    class HTTPHeaderMap : public HashMap<AtomicString, String, CaseFoldingHash> {
+    public:
+        // Gets a copy of the data suitable for passing to another thread.
+        std::auto_ptr<HTTPHeaderMapData> copyData() const;
+
+        void adopt(std::auto_ptr<HTTPHeaderMapData>);
+    };
 
 } // namespace WebCore
 
