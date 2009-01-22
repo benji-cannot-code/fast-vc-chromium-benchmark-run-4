7be3ed6171cc775061619060d3d6437f19d60c4b
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-22 18:55:42 +0000

    Not reviewed. Fix WML build problem in RenderBox.cpp, WMLNames is undefined.
    git-svn-id: svn://svn.chromium.org/blink/trunk@40126 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBox.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBox.cpp
@@ -47,6 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <math.h>
 
+#if ENABLE(WML)
+#include "WMLNames.h"
+#endif
+
 using namespace std;
 
 namespace WebCore {
