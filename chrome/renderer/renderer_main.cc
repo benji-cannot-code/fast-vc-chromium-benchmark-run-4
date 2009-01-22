d2cce19dc452d168b1e10af75b897104d1d0f7b0
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-22 01:23:11 +0000

    POSIX: Get render_process_host to build.
    This is an adopted CL from Evan. Original:
      http://codereview.chromium.org/14504
    
    (see original for review comments etc)
    
    Review URL: http://codereview.chromium.org/16814
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8430 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/renderer_main.cc
+++ chrome/renderer/renderer_main.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/logging_chrome.h"
 #include "chrome/common/main_function_params.h"
 #include "chrome/common/resource_bundle.h"
+#include "chrome/common/win_util.h"
 #include "chrome/renderer/render_process.h"
 #include "chrome/test/injection_test_dll.h"
 #include "sandbox/src/sandbox.h"
