56245948afd32a08588bbb8263037c16e67a45c3
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-22 21:16:25 +0000

    WebCore:
    2009-01-22  Eric Roman  <eroman@chromium.org>
    
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=20806
            Send referrer for history navigations.
    
            Test: http/tests/navigation/back-send-referrer.html
    
            Consolidates HistoryItem::rssFeedReferrer and HistoryItem::formReferrer into a single HistoryItem::referrer property.
    
            * history/HistoryItem.cpp:
            (WebCore::HistoryItem::HistoryItem):
            (WebCore::HistoryItem::referrer):
            (WebCore::HistoryItem::setReferrer):
            (WebCore::HistoryItem::formReferrer):
            (WebCore::HistoryItem::rssFeedReferrer):
            (WebCore::HistoryItem::setRSSFeedReferrer):
            (WebCore::HistoryItem::setFormInfoFromRequest):
            * history/HistoryItem.h:
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::loadItem):
    
    WebKit/win:
    
    2009-01-22  Eric Roman  <eroman@chromium.org>
    
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=20806
            Deprecate RSSFeedReferrer() and setRSSFeedReferrer().
    
            * WebHistoryItem.cpp:
            (WebHistoryItem::RSSFeedReferrer):
            (WebHistoryItem::setRSSFeedReferrer):
    
    WebKit/mac:
    
    2009-01-22  Eric Roman  <eroman@chromium.og>
    
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=20806
            Deprecate RSSFeedReferrer() and setRSSFeedReferrer().
    
            * History/WebHistoryItem.mm:
            (-[WebHistoryItem RSSFeedReferrer]):
            (-[WebHistoryItem setRSSFeedReferrer:]):
    
    LayoutTests:
    
    2009-01-22  Eric Roman  <eroman@chromium.org>
    
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=20806
            "Referer" header not sent for history navigation.
    
            * http/tests/navigation/back-send-referrer-expected.txt: Added.
            * http/tests/navigation/back-send-referrer.html: Added.
            * http/tests/navigation/resources/back-send-referrer-helper.php: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40132 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/navigation/resources/back-send-referrer-helper.php
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+    // Prevent from being cached.
+    header("Cache-Control: no-cache, private, max-age=0");
+    header("Content-Type: text/html");
+?>
+
+<html>
+
+<script>
+    window.name = parseInt(window.name) + 1;
+</script>
+
+Referrer: <?php echo $_SERVER['HTTP_REFERER']; ?>
+<br/>
+window.name: <script>document.write(window.name)</script>
+
+<form name=loopback action="" method=GET></form>
+
+<script>
+    if (window.name == 1) {
+        // Navigate once more to add a history entry.
+        document.loopback.submit();
+    } else if (window.name == 2) {
+        history.go(-1);
+    } else {
+        if (window.layoutTestController)
+            window.layoutTestController.notifyDone();
+    }
+</script>
+
+</html>
