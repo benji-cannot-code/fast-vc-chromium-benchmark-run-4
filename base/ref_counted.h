c161b5a409f2562c9697199d6616176c3cc3d2e0
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-22 20:29:43 +0000

    Reverting 8499. Review URL: http://codereview.chromium.org/18687
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8504 0039d316-1c4b-4281-b951-d872f2087c98

--- base/ref_counted.h
+++ base/ref_counted.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/atomic_ref_count.h"
 #include "base/basictypes.h"
-#include "base/thread_collision_warner.h"
 
 namespace base {
 
@@ -29,8 +28,6 @@ class RefCountedBase {
 #ifndef NDEBUG
   bool in_dtor_;
 #endif
-
-  DFAKE_MUTEX(add_release_);
 
   DISALLOW_COPY_AND_ASSIGN(RefCountedBase);
 };
