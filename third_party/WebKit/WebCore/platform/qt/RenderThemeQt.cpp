b1d05d3fdaec6bb6787625ab6467494a83fc64aa
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-22 15:29:25 +0000

    Fix the Qt build after RenderBox changes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@40125 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/RenderThemeQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/RenderThemeQt.cpp
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 #include "HTMLMediaElement.h"
 #include "HTMLNames.h"
+#include "RenderBox.h"
 
 namespace WebCore {
 
@@ -160,7 +161,7 @@ int RenderThemeQt::baselinePosition(const RenderObject* o) const
 
     if (o->style()->appearance() == CheckboxPart ||
         o->style()->appearance() == RadioPart)
-        return o->marginTop() + RenderBox::toRenderBox(o)->height() - 2; // Same as in old khtml
+        return o->marginTop() + RenderBox::toConstRenderBox(o)->height() - 2; // Same as in old khtml
     return RenderTheme::baselinePosition(o);
 }
 
