69058365573a3f3c52b0268d7565c72e59a46e60
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-23 18:38:13 +0000

    Move security style into the tab_contents directory. This is the last part of the "Tab Contents (old)" group! Review URL: http://codereview.chromium.org/18686
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8564 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/navigation_entry.h
+++ chrome/browser/tab_contents/navigation_entry.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_ptr.h"
 #include "base/string_util.h"
 #include "chrome/app/theme/theme_resources.h"
-#include "chrome/browser/security_style.h"
+#include "chrome/browser/tab_contents/security_style.h"
 #include "chrome/browser/tab_contents/site_instance.h"
 #include "chrome/browser/tab_contents/tab_contents_type.h"
 #include "chrome/common/page_transition_types.h"
--- chrome/browser/tab_contents/security_style.h
+++ chrome/browser/tab_contents/security_style.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_SECURITY_STYLE_H__
-#define CHROME_BROWSER_SECURITY_STYLE_H__
+#ifndef CHROME_BROWSER_TAB_CONTENTS_SECURITY_STYLE_H_
+#define CHROME_BROWSER_TAB_CONTENTS_SECURITY_STYLE_H_
 
 // Various aspects of the UI change their appearance according to the security
 // context in which they are displayed.  For example, the location bar displays
@@ -32,5 +32,4 @@ enum SecurityStyle {
   SECURITY_STYLE_AUTHENTICATED,
 };
 
-#endif // CHROME_BROWSER_SECURITY_STYLE_H__
-
+#endif  // CHROME_BROWSER_TAB_CONTENTS_SECURITY_STYLE_H_
