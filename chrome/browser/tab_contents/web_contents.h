78d3441458c57cd0c3252eb9fdbf75886b3ac486
brg@chromium.com <brg@chromium.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-23 00:02:44 +0000

    Changes to insure that when in app-mode, links open in the default browser.  This change should have no affect on Chrome when not in app-mode.Tested against Gmail.  UITests added as app_mode_navigation_uitest. Review URL: http://codereview.chromium.org/18093
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8523 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/web_contents.h
+++ chrome/browser/tab_contents/web_contents.h
@@ -481,6 +481,10 @@ class WebContents : public TabContents,
   void GenerateKeywordIfNecessary(
       const ViewHostMsg_FrameNavigate_Params& params);
 
+  // Helper function to launch the external browser.
+  void OpenUrlInDefaultBrowserAndClosePage(const GURL& url,
+                                           RenderViewHost* rvh);
+
   // Data ----------------------------------------------------------------------
 
   // The corresponding view.
