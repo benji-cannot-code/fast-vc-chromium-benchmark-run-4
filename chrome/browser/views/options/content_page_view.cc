08c16db48aabc82c9bc2f946c45870b89be3cd15
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-23 03:32:44 +0000

    BUG=1007
    Added a dialog for removing exceptions of password manager
    Added the PasswordManagerExceptionsView class for removing exception of password manager added by "Never for this site" button. Also added a button in "Minor Tweaks" tab for openning the dialog.
    
    Review URL: http://codereview.chromium.org/18494
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8544 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/options/content_page_view.cc
+++ chrome/browser/views/options/content_page_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/options/fonts_languages_window_view.h"
 #include "chrome/browser/views/options/options_group_view.h"
 #include "chrome/browser/views/password_manager_view.h"
+#include "chrome/browser/views/password_manager_exceptions_view.h"
 #include "chrome/browser/views/standard_layout.h"
 #include "chrome/common/gfx/chrome_canvas.h"
 #include "chrome/common/l10n_util.h"
@@ -162,6 +163,7 @@ ContentPageView::ContentPageView(Profile* profile)
       download_browse_button_(NULL),
       download_ask_for_save_location_checkbox_(NULL),
       select_file_dialog_(SelectFileDialog::Create(this)),
+      passwords_exceptions_button_(NULL),
       passwords_group_(NULL),
       passwords_asktosave_radio_(NULL),
       passwords_neversave_radio_(NULL),
@@ -224,7 +226,10 @@ void ContentPageView::ButtonPressed(views::NativeButton* sender) {
                               profile()->GetPrefs());
     }
     ask_to_save_passwords_.SetValue(enabled);
-  } else if (sender == passwords_show_passwords_button_) {
+  } else if (sender == passwords_exceptions_button_) {
+    UserMetricsRecordAction(L"Options_ShowPasswordManagerExceptions", NULL);
+    PasswordManagerExceptionsView::Show(profile());
+  }else if (sender == passwords_show_passwords_button_) {
     UserMetricsRecordAction(L"Options_ShowPasswordManager", NULL);
     PasswordManagerView::Show(profile());
   } else if (sender == form_autofill_checkbox_) {
@@ -397,6 +402,9 @@ void ContentPageView::InitPasswordSavingGroup() {
   passwords_show_passwords_button_ = new views::NativeButton(
       l10n_util::GetString(IDS_OPTIONS_PASSWORDS_SHOWPASSWORDS));
   passwords_show_passwords_button_->SetListener(this);
+  passwords_exceptions_button_ = new views::NativeButton(
+      l10n_util::GetString(IDS_OPTIONS_PASSWORDS_EXCEPTIONS));
+  passwords_exceptions_button_->SetListener(this);
 
   using views::GridLayout;
   using views::ColumnSet;
@@ -410,14 +418,23 @@ void ContentPageView::InitPasswordSavingGroup() {
   column_set->AddColumn(GridLayout::LEADING, GridLayout::CENTER, 1,
                         GridLayout::USE_PREF, 0, 0);
 
+  const int double_column_view_set_id = 0;
+  column_set = layout->AddColumnSet(double_column_view_set_id);
+  column_set->AddColumn(GridLayout::LEADING, GridLayout::CENTER, 0,
+                        GridLayout::USE_PREF, 0, 0);
+  column_set->AddPaddingColumn(0, kRelatedControlHorizontalSpacing);
+  column_set->AddColumn(GridLayout::LEADING, GridLayout::CENTER, 0,
+                        GridLayout::USE_PREF, 0, 0);
+
   layout->StartRow(0, single_column_view_set_id);
   layout->AddView(passwords_asktosave_radio_);
   layout->AddPaddingRow(0, kRelatedControlVerticalSpacing);
   layout->StartRow(0, single_column_view_set_id);
   layout->AddView(passwords_neversave_radio_);
   layout->AddPaddingRow(0, kUnrelatedControlVerticalSpacing);
-  layout->StartRow(0, single_column_view_set_id);
+  layout->StartRow(0, double_column_view_set_id);
   layout->AddView(passwords_show_passwords_button_);
+  layout->AddView(passwords_exceptions_button_);
 
   passwords_group_ = new OptionsGroupView(
       contents, l10n_util::GetString(IDS_OPTIONS_PASSWORDS_GROUP_NAME), L"",
