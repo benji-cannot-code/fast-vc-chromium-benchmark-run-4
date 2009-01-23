782dd5f9e394a9b10d1ad420478339d722fa8752
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-23 23:33:28 +0000

    2009-01-23  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Adele Peterson
            
            Fix for https://bugs.webkit.org/show_bug.cgi?id=23407
    
            Implement QTMovieWin::hasVideo so the controller on an audio-only <video> element 
            is never hidden (as with an <audio> element).
    
            Test: media/video-controls-visible-audio-only.html
    
            * platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp:
            (WebCore::MediaPlayerPrivate::hasVideo):
            * platform/graphics/win/QTMovieWin.cpp:
            (QTMovieWin::hasVideo):
            * platform/graphics/win/QTMovieWin.h:
    
    2009-01-23  Eric Carlson  <eric.carlson@apple.com>
    
            Reviewed by Adele Peterson
    
            Test for behavior of <video> element controller when a media file has
            only audio.
            https://bugs.webkit.org/show_bug.cgi?id=23407
    
            * media/video-controls-visible-audio-only.html: Added.
            * platform/mac/media/video-controls-visible-audio-only-expected.checksum: Added.
            * platform/mac/media/video-controls-visible-audio-only-expected.png: Added.
            * platform/mac/media/video-controls-visible-audio-only-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/QTMovieWin.h
+++ third_party/WebKit/WebCore/platform/graphics/win/QTMovieWin.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2007, 2008, 2009 Apple, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -86,6 +86,8 @@ public:
     void paint(HDC, int x, int y);
 
     void disableUnsupportedTracks(unsigned& enabledTrackCount);
+
+    bool hasVideo() const;
 
     static unsigned countSupportedTypes();
     static void getSupportedType(unsigned index, const UChar*& str, unsigned& len);
