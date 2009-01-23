c7c2631d4bbc785401980019dc31f263704f2bd1
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-23 20:37:44 +0000

    2009-01-23  Anders Carlsson  <andersca@apple.com>
            * Configurations/Base.xcconfig:
            GCC 4.0 build fix.
            
            * runtime/JSNumberCell.h:
            64-bit build fix.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSNumberCell.h
+++ third_party/WebKit/JavaScriptCore/runtime/JSNumberCell.h
@@ -36,6 +36,8 @@ namespace JSC {
     extern const double NaN;
     extern const double Inf;
 
+    JSValuePtr jsNumberCell(ExecState*, double);
+
 #if !USE(ALTERNATE_JSIMMEDIATE)
 
     class Identifier;
@@ -46,8 +48,6 @@ namespace JSC {
 
     struct ClassInfo;
     struct Instruction;
-
-    JSValuePtr jsNumberCell(ExecState*, double);
 
     class JSNumberCell : public JSCell {
         friend class JIT;
