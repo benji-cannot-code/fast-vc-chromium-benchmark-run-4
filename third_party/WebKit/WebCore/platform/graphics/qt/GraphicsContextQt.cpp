a49b0cca55d213e752617aec8fee0ecd68d62a47
ariya@webkit.org <ariya@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-23 12:38:47 +0000

    2009-01-23  Ariya Hidayat  <ariya.hidayat@trolltech.com>
            Reviewed by Simon Hausmann.
    
            [Qt] Simplify the assertion.
    
            * platform/graphics/qt/GraphicsContextQt.cpp:
            (WebCore::GraphicsContext::getWindowsContext):
            (WebCore::GraphicsContext::releaseWindowsContext):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40155 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
@@ -1107,8 +1107,8 @@ void GraphicsContext::setPlatformShouldAntialias(bool enable)
 
 HDC GraphicsContext::getWindowsContext(const IntRect& dstRect, bool supportAlphaBlend, bool mayCreateBitmap)
 {
-    // painting through native HDC is only supported for plugin, where mayCreateBitmap is always TRUE
-    Q_ASSERT(mayCreateBitmap == TRUE);
+    // painting through native HDC is only supported for plugin, where mayCreateBitmap is always true
+    Q_ASSERT(mayCreateBitmap);
 
     if (dstRect.isEmpty())
         return 0;
@@ -1166,8 +1166,8 @@ HDC GraphicsContext::getWindowsContext(const IntRect& dstRect, bool supportAlpha
 
 void GraphicsContext::releaseWindowsContext(HDC hdc, const IntRect& dstRect, bool supportAlphaBlend, bool mayCreateBitmap)
 {
-    // painting through native HDC is only supported for plugin, where mayCreateBitmap is always TRUE
-    Q_ASSERT(mayCreateBitmap == TRUE);
+    // painting through native HDC is only supported for plugin, where mayCreateBitmap is always true
+    Q_ASSERT(mayCreateBitmap);
 
     if (hdc) {
 
