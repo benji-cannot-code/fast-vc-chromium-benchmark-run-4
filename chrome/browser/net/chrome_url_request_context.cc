dc3041fc13c60678c1dadd0243d408e5c2821f20
jungshik@google.com <jungshik@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-23 00:39:47 +0000

    Add q-values to languages in Accept-Language HTTP header to be compatible with Apache. Add q-values to charsets in Accept-Charset header in the same way as Firefox does.
    BUG=5899
    TEST=HttpUtilTest.Accept*  (net_unittest)
    
    
    Review URL: http://codereview.chromium.org/17340
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8527 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_url_request_context.cc
+++ chrome/browser/net/chrome_url_request_context.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "net/http/http_cache.h"
+#include "net/http/http_util.h"
 #include "net/proxy/proxy_service.h"
 #include "webkit/glue/webkit_glue.h"
 
@@ -96,12 +97,11 @@ ChromeURLRequestContext::ChromeURLRequestContext(Profile* profile)
       is_off_the_record_(profile->IsOffTheRecord()) {
   user_agent_ = webkit_glue::GetUserAgent();
 
-  // set up Accept-Language and Accept-Charset header values
-  // TODO(jungshik) : This may slow down http requests. Perhaps,
-  // we have to come up with a better way to set up these values.
-  accept_language_ = WideToASCII(prefs_->GetString(prefs::kAcceptLanguages));
-  accept_charset_ = WideToASCII(prefs_->GetString(prefs::kDefaultCharset));
-  accept_charset_ += ",*,utf-8";
+  // Set up Accept-Language and Accept-Charset header values
+  accept_language_ = net::HttpUtil::GenerateAcceptLanguageHeader(
+      WideToASCII(prefs_->GetString(prefs::kAcceptLanguages)));
+  accept_charset_ = net::HttpUtil::GenerateAcceptCharsetHeader(
+      WideToASCII(prefs_->GetString(prefs::kDefaultCharset)));
 
   cookie_policy_.SetType(net::CookiePolicy::FromInt(
       prefs_->GetInteger(prefs::kCookieBehavior)));
@@ -184,7 +184,8 @@ FilePath ChromeURLRequestContext::GetPathForExtension(const std::string& id) {
 void ChromeURLRequestContext::OnAcceptLanguageChange(std::string accept_language) {
   DCHECK(MessageLoop::current() ==
          ChromeThread::GetMessageLoop(ChromeThread::IO));
-  accept_language_ = accept_language;
+  accept_language_ =
+      net::HttpUtil::GenerateAcceptLanguageHeader(accept_language);
 }
 
 void ChromeURLRequestContext::OnCookiePolicyChange(net::CookiePolicy::Type type) {
