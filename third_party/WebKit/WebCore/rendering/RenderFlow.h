6a436c4011cfa70ab9563dd24db8744cb3e1e810
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-23 04:07:34 +0000

    2009-01-22  David Hyatt  <hyatt@apple.com>
            Move the m_widthChanged bit on RenderTableCell into RenderFlow's set of bits.  Let intrinsicPaddingTop and
            intrinsicPaddingBottom just be normal non-bitfield values.
    
            Reviewed by Oliver Hunt
    
            * rendering/RenderFlow.h:
            (WebCore::RenderFlow::RenderFlow):
            * rendering/RenderTableCell.cpp:
            (WebCore::RenderTableCell::RenderTableCell):
            (WebCore::RenderTableCell::updateWidth):
            (WebCore::RenderTableCell::layout):
            * rendering/RenderTableCell.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40149 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderFlow.h
+++ third_party/WebKit/WebCore/rendering/RenderFlow.h
@@ -53,6 +53,7 @@ public:
         , m_selectionState(SelectionNone)
         , m_hasColumns(false)
         , m_isContinuation(false)
+        , m_cellWidthChanged(false)
     {
     }
 #ifndef NDEBUG
@@ -135,6 +136,9 @@ protected:
     
     // from RenderInline
     bool m_isContinuation : 1; // Whether or not we're a continuation of an inline.
+    
+    // from RenderTableCell
+    bool m_cellWidthChanged : 1;
 };
 
 #ifdef NDEBUG
