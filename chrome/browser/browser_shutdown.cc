1921c1cccccfc61f386c99b9d3c0521947a97e90
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-23 18:28:38 +0000

    Adds call to unload plugins on shutdown.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/18715
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8562 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_shutdown.cc
+++ chrome/browser/browser_shutdown.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_service.h"
+#include "chrome/common/chrome_plugin_lib.h"
 #include "chrome/common/resource_bundle.h"
 #include "chrome/browser/plugin_service.h"
 #include "net/dns_global.h"
@@ -82,6 +83,12 @@ std::wstring GetShutdownMsPath() {
 }
 
 void Shutdown() {
+  // Unload plugins. This needs to happen on the IO thread.
+  if (g_browser_process->io_thread()) {
+    g_browser_process->io_thread()->message_loop()->PostTask(FROM_HERE,
+        NewRunnableFunction(&ChromePluginLib::UnloadAllPlugins));
+  }
+
   // WARNING: During logoff/shutdown (WM_ENDSESSION) we may not have enough
   // time to get here. If you have something that *must* happen on end session,
   // consider putting it in BrowserProcessImpl::EndSession.
@@ -190,4 +197,3 @@ void ReadLastShutdownInfo() {
 }
 
 }  // namespace browser_shutdown
-
