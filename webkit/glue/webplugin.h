b420ecb6764038119917aa5e68bbaf4e944fc14d
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-24 04:30:16 +0000

    No functional change.  Delete trailing whitespace and word-wrap to 80 columns.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8609 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin.h
+++ webkit/glue/webplugin.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/gfx/rect.h"
 
+// TODO(port): type typedefs are obviously incorrect on non-Windows
+// platforms, but now a lot of code now accidentally depends on them
+// existing.  #ifdef out these declarations and fix all the users.
 typedef struct HWND__* HWND;
 typedef void* HANDLE;
 
@@ -124,7 +127,7 @@ class WebPlugin {
 
   // Handles GetURL/GetURLNotify/PostURL/PostURLNotify requests initiated
   // by plugins.
-  virtual void HandleURLRequest(const char *method, 
+  virtual void HandleURLRequest(const char *method,
                                 bool is_javascript_url,
                                 const char* target, unsigned int len,
                                 const char* buf, bool is_file_data,
@@ -158,7 +161,7 @@ class WebPluginResourceClient {
                                   uint32 last_modified,
                                   bool request_is_seekable,
                                   bool* cancel) = 0;
-  virtual void DidReceiveData(const char* buffer, int length, 
+  virtual void DidReceiveData(const char* buffer, int length,
                               int data_offset) = 0;
   virtual void DidFinishLoading() = 0;
   virtual void DidFail() = 0;
