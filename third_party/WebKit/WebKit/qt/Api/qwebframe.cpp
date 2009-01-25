131bc9442798182726942e9e381a790d3014a3cb
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-25 00:34:58 +0000

    Oops, fix the Qt build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@40220 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
@@ -784,7 +784,10 @@ void QWebFrame::render(QPainter *painter, const QRegion &clip)
 */
 void QWebFrame::render(QPainter *painter)
 {
-    d->renderPrivate(painter, QRegion(view->frameRect()));
+    if (!d->frame->view())
+        return;
+
+    d->renderPrivate(painter, QRegion(d->frame->view()->frameRect()));
 }
 
 /*!
