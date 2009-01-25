ac28f8472ddfe46cb8462086508be1d96bd6ae27
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-25 20:25:34 +0000

    2009-01-25  Darin Adler  <darin@apple.com>
            Reviewed by Sam Weinig.
    
            Bug 23352: Turn on more compiler warnings in the Mac build
            https://bugs.webkit.org/show_bug.cgi?id=23352
    
            Third patch: Use the noreturn attribute on functions that don't
            return to prepare for the use of the -Wmissing-noreturn warning.
    
            * jit/JITCall.cpp:
            (JSC::unreachable): Added NO_RETURN.
            * jsc.cpp:
            (functionQuit): Ditto.
            (printUsageStatement): Ditto.
            * wtf/AlwaysInline.h: Added definition of NO_RETURN.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40230 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/jit/JITCall.cpp
+++ third_party/WebKit/JavaScriptCore/jit/JITCall.cpp
@@ -188,7 +188,7 @@ void JIT::compileOpCallSlowCase(Instruction* instruction, Vector<SlowCaseEntry>:
 
 #else
 
-static void unreachable()
+static NO_RETURN void unreachable()
 {
     ASSERT_NOT_REACHED();
     exit(1);
--- third_party/WebKit/JavaScriptCore/wtf/AlwaysInline.h
+++ third_party/WebKit/JavaScriptCore/wtf/AlwaysInline.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifndef ALWAYS_INLINE
 #if COMPILER(GCC) && defined(NDEBUG) && !COMPILER(MINGW)
-#define ALWAYS_INLINE inline __attribute__ ((__always_inline__))
+#define ALWAYS_INLINE inline __attribute__((__always_inline__))
 #elif COMPILER(MSVC) && defined(NDEBUG)
 #define ALWAYS_INLINE __forceinline
 #else
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifndef NEVER_INLINE
 #if COMPILER(GCC)
-#define NEVER_INLINE __attribute__ ((__noinline__))
+#define NEVER_INLINE __attribute__((__noinline__))
 #else
 #define NEVER_INLINE
 #endif
@@ -52,5 +52,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define LIKELY(x) __builtin_expect((x), 1)
 #else
 #define LIKELY(x) (x)
+#endif
+#endif
+
+#ifndef NO_RETURN
+#if COMPILER(GCC)
+#define NO_RETURN __attribute((__noreturn__))
+#else
+#define NO_RETURN
 #endif
 #endif
