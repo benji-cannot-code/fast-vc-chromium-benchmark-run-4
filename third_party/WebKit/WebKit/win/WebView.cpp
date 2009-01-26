467131819bb95bdcf18aaa8c632acc27b3df92f3
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-26 07:26:29 +0000

    2009-01-25  Darin Adler  <darin@apple.com>
            Try to fix Windows build.
    
            * WebView.cpp: Added FloatQuad.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40244 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/EventNames.h>
 #include <WebCore/FileSystem.h>
 #include <WebCore/FocusController.h>
+#include <WebCore/FloatQuad.h>
 #include <WebCore/FrameLoader.h>
 #include <WebCore/FrameTree.h>
 #include <WebCore/FrameView.h>
