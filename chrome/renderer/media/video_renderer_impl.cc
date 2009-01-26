d511a0e4b80c5de8d679411c5c75ba0cb37e7c54
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-26 19:53:21 +0000

    Checking in stubbed out ChromeMediaPlayer filter implementations.
    Review URL: http://codereview.chromium.org/18538
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8652 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/renderer/media/video_renderer_impl.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.  Use of this
+// source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
+
+#include "chrome/renderer/media/video_renderer_impl.h"
+
+VideoRendererImpl::VideoRendererImpl() {
+}
+
+VideoRendererImpl::~VideoRendererImpl() {
+}
+
+void VideoRendererImpl::Stop() {
+  // TODO(scherkus): implement Stop.
+  NOTIMPLEMENTED();
+}
+
+bool VideoRendererImpl::Initialize(media::VideoDecoder* decoder) {
+  // TODO(scherkus): implement Initialize.
+  NOTIMPLEMENTED();
+  return false;
+}
--- chrome/renderer/webmediaplayer_delegate_impl.cc
+++ chrome/renderer/webmediaplayer_delegate_impl.cc
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
+#include "chrome/renderer/media/audio_renderer_impl.h"
+#include "chrome/renderer/media/data_source_impl.h"
+#include "chrome/renderer/media/video_renderer_impl.h"
 #include "chrome/renderer/webmediaplayer_delegate_impl.h"
 
 WebMediaPlayerDelegateImpl::WebMediaPlayerDelegateImpl()
@@ -23,6 +26,15 @@ WebMediaPlayerDelegateImpl::WebMediaPlayerDelegateImpl()
       web_media_player_(NULL),
       width_(0) {
   // TODO(hclam): initialize the media player here
+
+  // TODO(scherkus): remove these when actually in use -- right now I declare
+  // these to force compiler/linker errors to reveal themselves.
+  scoped_refptr<AudioRendererImpl> audio_renderer;
+  audio_renderer = new AudioRendererImpl();
+  scoped_refptr<DataSourceImpl> data_source;
+  data_source = new DataSourceImpl();
+  scoped_refptr<VideoRendererImpl> video_renderer;
+  video_renderer = new VideoRendererImpl();
 }
 
 WebMediaPlayerDelegateImpl::~WebMediaPlayerDelegateImpl() {
