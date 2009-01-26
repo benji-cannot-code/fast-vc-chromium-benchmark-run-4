4f3f8a6cd542b96c829917c892ae311c1c7a1336
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-26 22:52:13 +0000

    2009-01-26  Brady Eidson  <beidson@apple.com>
            Reviewed by Sam Weinig
    
            Remove some historical CachedPage time stamp fluff.
    
            * history/CachedPage.cpp:
            (WebCore::CachedPage::CachedPage):
            * history/CachedPage.h:
            (WebCore::CachedPage::timeStamp):
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::cachePageForHistoryItem):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40267 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -4240,7 +4240,6 @@ void FrameLoader::cachePageForHistoryItem(HistoryItem* item)
 {
     if (Page* page = m_frame->page()) {
         RefPtr<CachedPage> cachedPage = CachedPage::create(page);
-        cachedPage->setTimeStampToNow();
         m_client->savePlatformDataToCachedPage(cachedPage.get());
 
         pageCache()->add(item, cachedPage.release());
