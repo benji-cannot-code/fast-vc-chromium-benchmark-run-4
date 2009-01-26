c1a62acaaa790cb8a23bbb3fb50e4e80fc346d5c
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-26 22:48:10 +0000

    Case sensitive directory names on linux
    Review URL: http://codereview.chromium.org/18786
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8668 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/v8/extensions/Interval.cpp
+++ webkit/port/bindings/v8/extensions/Interval.cpp
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "Interval.h"
-#include "WTF/CurrentTime.h"
+#include "wtf/CurrentTime.h"
 
 namespace WebCore {
 
