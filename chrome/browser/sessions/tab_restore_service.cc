e41eb28fee9f3a3b10f8a84dbaa188fa7457c2fd
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-26 18:32:54 +0000

    Fixes possible crash in restoring a previously closed window. The crash would occur if we pruned a tab and the index of selected tab was > tab_count - pruned_count.
    BUG=none
    TEST=make sure no problems with restoring closed windows.
    
    Review URL: http://codereview.chromium.org/18737
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8640 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/tab_restore_service.cc
+++ chrome/browser/sessions/tab_restore_service.cc
@@ -158,6 +158,9 @@ void TabRestoreService::BrowserClosing(Browser* browser) {
     delete window;
     window = NULL;
   } else {
+    window->selected_tab_index =
+        std::min(static_cast<int>(window->tabs.size() - 1),
+                 window->selected_tab_index);
     AddEntry(window, true, true);
   }
 }
