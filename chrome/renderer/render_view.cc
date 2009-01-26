c471507fe48eea85c9f36def7c160cd0ea46d728
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-26 20:41:33 +0000

    Link Doctor is expecting the query param connectionfailure, not connectionerror.
    BUG=1599954
    
    Review URL: http://codereview.chromium.org/18811
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8659 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -2158,7 +2158,7 @@ GURL RenderView::GetAlternateErrorPageURL(const GURL& failedURL,
       break;
 
     case CONNECTION_ERROR:
-      params.append("connectionerror");
+      params.append("connectionfailure");
       break;
 
     default:
