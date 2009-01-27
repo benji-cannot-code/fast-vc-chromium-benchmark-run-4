66a3612b1574a85d9df9176e2a323586db4bb572
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-27 01:26:16 +0000

    Moved Init() before startup_data_->event.Signal() because derived classes may not be safe to use until Init() has been called.  As an example, RenderThread() creates it's IPC::SyncChannel in Init(), so it isn't safe to call Send() method until after.
    Update use of a Thread (hit with ui_tests) to no longer require Init()
    be called after the startup event is signaled (required old thread
    behavior).
    
    Ran a bunch more tests on Windows to try harder to make sure I didn't break anything.
    Review URL: http://codereview.chromium.org/18824
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8695 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/default_plugin/plugin_install_job_monitor.h
+++ webkit/default_plugin/plugin_install_job_monitor.h
@@ -10,12 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/thread.h"
+#include "base/ref_counted.h"
 
 // Provides the plugin installation job monitoring functionality.
 // The PluginInstallationJobMonitorThread class represents a background
 // thread which monitors the install job completion port which is associated
 // with the job when an instance of this class is initialized.
-class PluginInstallationJobMonitorThread : public base::Thread {
+class PluginInstallationJobMonitorThread :
+    public base::Thread,
+    public base::RefCountedThreadSafe<PluginInstallationJobMonitorThread> {
+
  public:
   PluginInstallationJobMonitorThread();
   virtual ~PluginInstallationJobMonitorThread();
@@ -47,10 +51,16 @@ class PluginInstallationJobMonitorThread : public base::Thread {
   bool AssignProcessToJob(HANDLE process_handle);
 
  protected:
+  // Installs a task on our thread to call WaitForJobThread().  We
+  // can't call it directly since we would deadlock (thread which
+  // creates me blocks until Start() returns, and Start() doesn't
+  // return until Init() does).
+  virtual void Init();
+
   // Blocks on the plugin installation job completion port by invoking the
   // GetQueuedCompletionStatus API.
   // We return from this function when the job monitoring thread is stopped.
-  virtual void Init();
+  virtual void WaitForJobThread();
 
  private:
   // The install job completion port. Created in Init.
