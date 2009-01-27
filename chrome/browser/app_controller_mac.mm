681da6ba8613a283775a66d1792b1df7dfd86f9a
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-27 16:28:44 +0000

    Unimportant cleanup.
    Review URL: http://codereview.chromium.org/18847
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8718 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/app_controller_mac.mm
+++ chrome/browser/app_controller_mac.mm
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [super dealloc];
 }
 
-// We can't use the standard terminate: method because it will abrubptly exit
+// We can't use the standard terminate: method because it will abruptly exit
 // the app and leave things on the stack in an unfinalized state. We need to
 // post a quit message to our run loop so the stack can gracefully unwind.
 - (IBAction)quit:(id)sender {  
