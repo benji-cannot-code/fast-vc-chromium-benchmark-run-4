d50d6b34adf647cee54502e71d5c431b1d721fba
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-27 23:20:12 +0000

    Modifies shutdown/startup in the following manner:
    . Before registering the scheme 'chrome' with GURL we make sure it
      isn't already registered. I need this as I'm going to invoke
      BrowserMain multiple times and didn't want the list of schemes to
      have n-chromes.
    . Removes ResourceBundle::CleanupSharedInstance from
      browser_shutdown. We do this from BrowserMain already, so there is
      no need in doing it here.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/18861
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8768 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_shutdown.cc
+++ chrome/browser/browser_shutdown.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/waitable_event.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/dom_ui/chrome_url_data_manager.h"
 #include "chrome/browser/first_run.h"
 #include "chrome/browser/jankometer.h"
 #include "chrome/browser/metrics/metrics_service.h"
@@ -147,6 +148,8 @@ void Shutdown() {
     std::wstring shutdown_ms_file = GetShutdownMsPath();
     file_util::WriteFile(shutdown_ms_file, shutdown_ms.c_str(), len);
   }
+
+  UnregisterURLRequestChromeJob();
 }
 
 void ReadLastShutdownInfo() {
