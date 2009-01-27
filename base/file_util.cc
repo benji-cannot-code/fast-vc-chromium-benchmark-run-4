c9146883369ac1156cae72267ba1258f41b65c5c
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-27 17:23:12 +0000

    Fix Windows build.
    Review URL: http://codereview.chromium.org/19022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8722 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util.cc
+++ base/file_util.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_util.h"
 
+#if defined(OS_WIN)
+#include <io.h>
+#endif
 #include <stdio.h>
 
 #include <fstream>
