3b6f0b74c2c0e8a6ddb3285da705bcc9c7e7ad87
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-27 22:26:11 +0000

    Extend the popup-mode location bar out into the nonclient view.  This makes popup mode pixel perfect except for the off-by-one sizing regression caught by the interactive UI test, which I haven't figured out yet.
    BUG=5054
    Review URL: http://codereview.chromium.org/18862
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8763 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/opaque_non_client_view.cc
+++ chrome/browser/views/frame/opaque_non_client_view.cc
@@ -64,6 +64,10 @@ enum {
   FRAME_CLIENT_EDGE_BOTTOM_LEFT,
   FRAME_CLIENT_EDGE_LEFT,
 
+  // Popup-mode toolbar edges.
+  FRAME_TOOLBAR_POPUP_EDGE_LEFT,
+  FRAME_TOOLBAR_POPUP_EDGE_RIGHT,
+
   FRAME_PART_BITMAP_COUNT  // Must be last.
 };
 
@@ -99,6 +103,7 @@ class ActiveWindowResources : public views::WindowResources {
             IDR_CONTENT_TOP_RIGHT_CORNER, IDR_CONTENT_RIGHT_SIDE,
             IDR_CONTENT_BOTTOM_RIGHT_CORNER, IDR_CONTENT_BOTTOM_CENTER,
             IDR_CONTENT_BOTTOM_LEFT_CORNER, IDR_CONTENT_LEFT_SIDE,
+        IDR_LOCATIONBG_POPUPMODE_LEFT, IDR_LOCATIONBG_POPUPMODE_RIGHT,
         0
       };
 
@@ -150,6 +155,7 @@ class InactiveWindowResources : public views::WindowResources {
             IDR_CONTENT_TOP_RIGHT_CORNER, IDR_CONTENT_RIGHT_SIDE,
             IDR_CONTENT_BOTTOM_RIGHT_CORNER, IDR_CONTENT_BOTTOM_CENTER,
             IDR_CONTENT_BOTTOM_LEFT_CORNER, IDR_CONTENT_LEFT_SIDE,
+        IDR_LOCATIONBG_POPUPMODE_LEFT, IDR_LOCATIONBG_POPUPMODE_RIGHT,
         0
       };
 
@@ -201,6 +207,7 @@ class OTRActiveWindowResources : public views::WindowResources {
             IDR_CONTENT_TOP_RIGHT_CORNER, IDR_CONTENT_RIGHT_SIDE,
             IDR_CONTENT_BOTTOM_RIGHT_CORNER, IDR_CONTENT_BOTTOM_CENTER,
             IDR_CONTENT_BOTTOM_LEFT_CORNER, IDR_CONTENT_LEFT_SIDE,
+        IDR_LOCATIONBG_POPUPMODE_LEFT, IDR_LOCATIONBG_POPUPMODE_RIGHT,
         0
       };
 
@@ -252,6 +259,7 @@ class OTRInactiveWindowResources : public views::WindowResources {
             IDR_CONTENT_TOP_RIGHT_CORNER, IDR_CONTENT_RIGHT_SIDE,
             IDR_CONTENT_BOTTOM_RIGHT_CORNER, IDR_CONTENT_BOTTOM_CENTER,
             IDR_CONTENT_BOTTOM_LEFT_CORNER, IDR_CONTENT_LEFT_SIDE,
+        IDR_LOCATIONBG_POPUPMODE_LEFT, IDR_LOCATIONBG_POPUPMODE_RIGHT,
         0
       };
 
@@ -862,28 +870,30 @@ void OpaqueNonClientView::PaintTitleBar(ChromeCanvas* canvas) {
 }
 
 void OpaqueNonClientView::PaintToolbarBackground(ChromeCanvas* canvas) {
-  if (!browser_view_->IsToolbarVisible() ||
-      !browser_view_->IsToolbarDisplayModeNormal())
+  if (!browser_view_->IsToolbarVisible())
     return;
-
+  
   gfx::Rect toolbar_bounds(browser_view_->GetToolbarBounds());
   gfx::Point toolbar_origin(toolbar_bounds.origin());
   View::ConvertPointToView(frame_->client_view(), this, &toolbar_origin);
   toolbar_bounds.set_origin(toolbar_origin);
 
-  SkBitmap* toolbar_left =
-      resources()->GetPartBitmap(FRAME_CLIENT_EDGE_TOP_LEFT);
+  bool normal_mode = browser_view_->IsToolbarDisplayModeNormal();
+  SkBitmap* toolbar_left = resources()->GetPartBitmap(normal_mode ?
+      FRAME_CLIENT_EDGE_TOP_LEFT : FRAME_TOOLBAR_POPUP_EDGE_LEFT);
   canvas->DrawBitmapInt(*toolbar_left,
                         toolbar_bounds.x() - toolbar_left->width(),
                         toolbar_bounds.y());
 
-  SkBitmap* toolbar_center =
-      resources()->GetPartBitmap(FRAME_CLIENT_EDGE_TOP);
-  canvas->TileImageInt(*toolbar_center, toolbar_bounds.x(), toolbar_bounds.y(),
-                       toolbar_bounds.width(), toolbar_center->height());
+  if (normal_mode) {
+    SkBitmap* toolbar_center =
+        resources()->GetPartBitmap(FRAME_CLIENT_EDGE_TOP);
+    canvas->TileImageInt(*toolbar_center, toolbar_bounds.x(),
+        toolbar_bounds.y(), toolbar_bounds.width(), toolbar_center->height());
+  }
 
-  canvas->DrawBitmapInt(
-      *resources()->GetPartBitmap(FRAME_CLIENT_EDGE_TOP_RIGHT),
+  canvas->DrawBitmapInt(*resources()->GetPartBitmap(normal_mode ?
+      FRAME_CLIENT_EDGE_TOP_RIGHT : FRAME_TOOLBAR_POPUP_EDGE_RIGHT),
       toolbar_bounds.right(), toolbar_bounds.y());
 }
 
