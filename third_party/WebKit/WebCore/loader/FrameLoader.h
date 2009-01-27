e5d352cae7acf3676a733f3eb910e2f1be9fdb69
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-27 03:49:43 +0000

    2009-01-26  Brady Eidson  <beidson@apple.com>
            Reviewed by Darin Adler
    
            -Make the recently added back/forward cache logging much better by actually
             walking the entire frame tree and indenting the resulting output.
            -Fix a null-termination bug in HistoryItem tree logging 
    
            * history/HistoryItem.cpp:
            (WebCore::HistoryItem::showTreeWithIndent):
    
            * loader/FrameLoader.cpp:
            (WebCore::pageCacheLogPrefix):
            (WebCore::pageCacheLog):
            (WebCore::FrameLoader::logCanCachePageDecision):
            (WebCore::FrameLoader::logCanCacheFrameDecision):
            * loader/FrameLoader.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40286 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/WebCore/loader/FrameLoader.h
@@ -461,7 +461,7 @@ namespace WebCore {
         bool canCachePageContainingThisFrame();
 #ifndef NDEBUG
         void logCanCachePageDecision();
-        bool logCanCacheFrameDecision();
+        bool logCanCacheFrameDecision(int indentLevel);
 #endif
 
         void addBackForwardItemClippedAtTarget(bool doClip);
