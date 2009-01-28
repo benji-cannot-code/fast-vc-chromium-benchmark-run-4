b49d0db6c1cfbd396313aa3aca821f896f189469
erikkay@google.com <erikkay@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-28 00:22:46 +0000

    Add FilePath version of ReadFileToString.
    Review URL: http://codereview.chromium.org/19401
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8774 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util.cc
+++ base/file_util.cc
@@ -250,7 +250,7 @@ bool ContentsEqual(const FilePath& filename1, const FilePath& filename2) {
   return true;
 }
 
-bool ReadFileToString(const std::wstring& path, std::string* contents) {
+bool ReadFileToString(const FilePath& path, std::string* contents) {
   FILE* file = OpenFile(path, "rb");
   if (!file) {
     return false;
@@ -349,6 +349,10 @@ bool MemoryMappedFile::IsValid() {
 }
 
 // Deprecated functions ----------------------------------------------------
+
+bool ReadFileToString(const std::wstring& path, std::string* contents) {
+  return ReadFileToString(FilePath::FromWStringHack(path), contents);
+}
 
 bool AbsolutePath(std::wstring* path_str) {
   FilePath path(FilePath::FromWStringHack(*path_str));
