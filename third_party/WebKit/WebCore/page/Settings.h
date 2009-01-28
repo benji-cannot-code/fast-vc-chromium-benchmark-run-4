e4d87646b5a0b60673240f26724bd9873d648cea
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-28 19:19:32 +0000

    2009-01-28  Zachary Kuznia  <zork@chromiuum.org>
            Reviewed by Sam Weinig.
    
            https://bugs.webkit.org/show_bug.cgi?id=22070
            Add an option to allow scripts to close windows.
    
            * page/Settings.cpp:
            (WebCore::Settings::Settings):
            (WebCore::Settings::setAllowScriptsToCloseWindows):
            * page/Settings.h:
            (WebCore::Settings::allowScriptsToCloseWindows):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40315 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Settings.h
+++ third_party/WebKit/WebCore/page/Settings.h
@@ -200,6 +200,9 @@ namespace WebCore {
         static bool shouldPaintNativeControls() { return gShouldPaintNativeControls; }
 #endif
 
+        void setAllowScriptsToCloseWindows(bool);
+        bool allowScriptsToCloseWindows() const { return m_allowScriptsToCloseWindows; }
+
     private:
         Page* m_page;
         
@@ -251,6 +254,7 @@ namespace WebCore {
         bool m_zoomsTextOnly : 1;
         bool m_enforceCSSMIMETypeInStrictMode : 1;
         size_t m_maximumDecodedImageSize;
+        bool m_allowScriptsToCloseWindows : 1;
 
 #if USE(SAFARI_THEME)
         static bool gShouldPaintNativeControls;
