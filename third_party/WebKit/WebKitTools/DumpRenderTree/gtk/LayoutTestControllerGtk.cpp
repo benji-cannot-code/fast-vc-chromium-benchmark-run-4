8bc2092a3cf7881f6c1af4096a05e15f88cdfb1d
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-28 22:43:10 +0000

    2009-01-28  Geoffrey Garen  <ggaren@apple.com>
            Build fix for GTK.
    
            * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
            (LayoutTestController::webHistoryItemCount):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/gtk/LayoutTestControllerGtk.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/gtk/LayoutTestControllerGtk.cpp
@@ -98,6 +98,12 @@ void LayoutTestController::keepWebHistory()
     // FIXME: implement
 }
 
+size_t LayoutTestController::webHistoryItemCount()
+{
+    // FIXME: implement
+    return 0;
+}
+
 void LayoutTestController::notifyDone()
 {
     if (m_waitToDump && !topLoadingFrame && !WorkQueue::shared()->count())
