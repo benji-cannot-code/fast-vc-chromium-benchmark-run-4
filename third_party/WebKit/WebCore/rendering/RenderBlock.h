7c45286792f4854ac491de21e588e951a2180b11
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-28 23:18:56 +0000

    2009-01-28  David Hyatt  <hyatt@apple.com>
            Move localCaretRect from RenderFlow into RenderBlock, since it only applied to blocks anyway and
            was misplaced from the start.
    
            Reviewed by anttik
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::localCaretRect):
            * rendering/RenderBlock.h:
            * rendering/RenderFlow.cpp:
            * rendering/RenderFlow.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40333 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBlock.h
+++ third_party/WebKit/WebCore/rendering/RenderBlock.h
@@ -317,6 +317,8 @@ public:
     RenderInline* inlineContinuation() const { return m_inlineContinuation; }
     void setInlineContinuation(RenderInline* c) { m_inlineContinuation = c; }
 
+    virtual IntRect localCaretRect(InlineBox*, int caretOffset, int* extraWidthToEndOfLine = 0);
+
 private:
     void adjustPointToColumnContents(IntPoint&) const;
     void adjustForBorderFit(int x, int& left, int& right) const; // Helper function for borderFitAdjust
