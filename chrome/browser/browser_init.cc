9ef00a30027814913a96edd0431afd79c7f71f56
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-28 09:22:44 +0000

    Remove extraneous uses of the --enable-user-script switch. It's better to check this at one chokepoint (initialization) and let the rest of the code work the same, but end up as no-ops.
    Review URL: http://codereview.chromium.org/18401
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8800 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_init.cc
+++ chrome/browser/browser_init.cc
@@ -223,8 +223,6 @@ bool BrowserInit::LaunchWithProfile::Launch(Profile* profile,
     }
   }
 
-  profile->InitExtensions();
-
   return true;
 }
 
@@ -386,6 +384,10 @@ bool BrowserInit::ProcessCommandLine(const CommandLine& parsed_command_line,
     CreateAutomationProvider<AutomationProvider>(automation_channel_id,
                                                  profile, expected_tabs);
   }
+
+  // Start up the extensions service.
+  profile->InitExtensions();
+
   // If we don't want to launch a new browser window or tab (in the case
   // of an automation request), we are done here.
   if (!silent_launch) {
