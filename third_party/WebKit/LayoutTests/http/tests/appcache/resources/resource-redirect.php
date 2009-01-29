77acf612140973ffaed52ac7739832d3e9a07db7
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-29 20:40:00 +0000

    Reviewed by Anders Carlsson.
            https://bugs.webkit.org/show_bug.cgi?id=23620
            Crash in appcache/resource-redirect.html on Tiger
    
            There was a bug in the test case - it didn't send a Location header, and on Tiger, this
            resulted in a failure callback being sent. It turned out that proper redirects weren't
            caught at all.
    
            Tests: http/tests/appcache/manifest-redirect-2.html
                   http/tests/appcache/resource-redirect-2.html
    
            * loader/appcache/ApplicationCacheGroup.cpp:
            (WebCore::ApplicationCacheGroup::didFail): Fixed this function to not crash if
            didReceiveResponse() wasn't called for this request.
            (WebCore::ApplicationCacheGroup::didReceiveResponse): Fixed to check for redirects correctly.
            (WebCore::ApplicationCacheGroup::didReceiveManifestResponse): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40368 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/appcache/resources/resource-redirect.php
+++ third_party/WebKit/LayoutTests/http/tests/appcache/resources/resource-redirect.php
@@ -4,5 +4,5 @@ header("Expires: Thu, 01 Dec 2003 16:00:00 GMT");
 header("Cache-Control: no-cache, must-revalidate");
 header("Pragma: no-cache");
 header('HTTP/1.1 307 Temporary Redirect');
-header('simple.txt');
+header('Location: simple.txt');
 ?>
