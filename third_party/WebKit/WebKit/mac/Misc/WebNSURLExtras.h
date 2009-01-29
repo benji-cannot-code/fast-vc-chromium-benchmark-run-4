08e5929add50188f09cbbe3a100001f36bf35605
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-29 03:53:12 +0000

    Add missing declaration for -[NSURL(WebNSURLExtras) _webkit_isFileURL]
            Reviewed by Dan Bernstein.
    
            * Misc/WebNSURLExtras.h:
            (-[NSURL(WebNSURLExtras) _webkit_isFileURL]): Added missing
            declaration after the implementation was added in r9258.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40346 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Misc/WebNSURLExtras.h
+++ third_party/WebKit/WebKit/mac/Misc/WebNSURLExtras.h
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSURL *)_webkit_URLByRemovingResourceSpecifier;
 
 - (BOOL)_webkit_isJavaScriptURL;
+- (BOOL)_webkit_isFileURL;
 - (NSString *)_webkit_scriptIfJavaScriptURL;
 - (BOOL)_webkit_isFTPDirectoryURL;
 
