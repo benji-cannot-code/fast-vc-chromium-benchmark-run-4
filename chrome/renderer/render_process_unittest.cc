70138aa178d6ba27d1b9f3103c061bf21cfb7fae
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-29 01:04:15 +0000

    Fix purify failure. http://code.google.com/p/chromium/issues/detail?id=7144
    Review URL: http://codereview.chromium.org/19642
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8850 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_process_unittest.cc
+++ chrome/renderer/render_process_unittest.cc
@@ -8,29 +8,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/render_process.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+namespace {
 
 class RenderProcessTest : public testing::Test {
  public:
   virtual void SetUp() {
-    // Must have a message loop to create a RenderThread()
-    message_loop_ = new MessageLoop(MessageLoop::TYPE_DEFAULT);
+    RenderProcess::GlobalInit(L"render_process_unittest");
   }
 
   virtual void TearDown() {
-    delete message_loop_;
+    RenderProcess::GlobalCleanup();
   }
 
- private:
-  MessageLoop *message_loop_;
+  private:
+   MessageLoopForIO message_loop_;
 };
 
 
 TEST_F(RenderProcessTest, TestSharedMemoryAllocOne) {
-  RenderProcess::GlobalInit(ASCIIToWide("hi mom"));
   size_t size = base::SysInfo::VMAllocationGranularity();
   base::SharedMemory* mem = RenderProcess::AllocSharedMemory(size);
   ASSERT_TRUE(mem);
   RenderProcess::FreeSharedMemory(mem);
-  RenderProcess::GlobalCleanup();
 }
 
+}  // namespace
