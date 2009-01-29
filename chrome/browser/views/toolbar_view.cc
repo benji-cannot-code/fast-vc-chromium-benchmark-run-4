39482acd6c3682fe192aa1fb02ab666bdb190466
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-29 17:51:45 +0000

    Move kClientEdgeThickness from BrowserView to NonClientView (which makes more sense anyway) so I'll be able to use it in custom_frame_window.cc.
    Also eliminate an unnecessarily-repeated set of resource IDs from the opaque nonclient view, and move a comment to match what I ended up doing (but haven't yet sent up for review) in other places.  Seems I couldn't make up my mind on that one.
    Review URL: http://codereview.chromium.org/19458
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8891 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/toolbar_view.cc
+++ chrome/browser/views/toolbar_view.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/navigation_entry.h"
 #include "chrome/browser/user_data_manager.h"
 #include "chrome/browser/views/dom_view.h"
-#include "chrome/browser/views/frame/browser_view.h"
 #include "chrome/browser/views/go_button.h"
 #include "chrome/browser/views/location_bar_view.h"
 #include "chrome/browser/views/theme_helpers.h"
@@ -41,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/views/button_dropdown.h"
 #include "chrome/views/hwnd_view.h"
 #include "chrome/views/label.h"
+#include "chrome/views/non_client_view.h"
 #include "chrome/views/tooltip_manager.h"
 #include "chrome/views/widget.h"
 #include "net/base/net_util.h"
@@ -488,7 +488,7 @@ gfx::Size BrowserToolbarView::GetPreferredSize() {
   // prevents the content area from being off by |kClientEdgeThickness| px.
   int client_edge_height =
       (browser_->window() && browser_->window()->IsMaximized()) ?
-      0 : BrowserView::kClientEdgeThickness;
+      0 : views::NonClientView::kClientEdgeThickness;
   return gfx::Size(0,
       location_bar_->GetPreferredSize().height() + client_edge_height);
 }
--- chrome/views/non_client_view.cc
+++ chrome/views/non_client_view.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
+const int NonClientView::kClientEdgeThickness = 1;
+
 int NonClientView::GetHTComponentForFrame(const gfx::Point& point,
                                           int top_resize_border_height,
                                           int resize_border_width,
