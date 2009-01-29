1fafbf505af5fc150444f0563b3012186addc3cf
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-29 23:32:06 +0000

    Create a stub chrome executable that crashes when run.
    This is mostly making sure we have all the symbols we need.
    
    Review URL: http://codereview.chromium.org/19683
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8930 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/browser_main_gtk.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/command_line.h"
+#include "chrome/app/result_codes.h"
+#include "chrome/browser/browser_main_win.h"
+
+// From browser_main_win.h, stubs until we figure out the right thing...
+
+int DoUninstallTasks() {
+  return ResultCodes::NORMAL_EXIT;
+}
+
+bool DoUpgradeTasks(const CommandLine& command_line) {
+  return ResultCodes::NORMAL_EXIT;
+}
+
+bool CheckForWin2000() {
+  return false;
+}
+
+int HandleIconsCommands(const CommandLine &parsed_command_line) {
+  return 0;
+}
+
+bool CheckMachineLevelInstall() {
+  return false;
+}
+
+void PrepareRestartOnCrashEnviroment(const CommandLine &parsed_command_line) {
+}
+
+void RecordBreakpadStatusUMA(MetricsService* metrics) {
+}
