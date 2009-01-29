8d0dfc619839af45a399ee078175ce4b9259f32a
mpcomplete@google.com <mpcomplete@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-29 21:29:15 +0000

    Re-enable the NoScript unittest.
    Turns out we need to broadcast the NOTIFY_USER_SCRIPTS_LOADED message even if
    there are no scripts.  I just removed the DCHECK that expects scripts.
    Review URL: http://codereview.chromium.org/19475
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8913 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -760,7 +760,6 @@ void BrowserRenderProcessHost::Observe(NotificationType type,
     case NOTIFY_USER_SCRIPTS_LOADED: {
       base::SharedMemory* shared_memory =
           Details<base::SharedMemory>(details).ptr();
-      DCHECK(shared_memory);
       if (shared_memory) {
         SendUserScriptsUpdate(shared_memory);
       }
