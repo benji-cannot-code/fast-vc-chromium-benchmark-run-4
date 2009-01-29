65ade9a9f8419950c8f5d21013d886fbabca3638
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-29 19:21:07 +0000

    Fix Bug 23623: Windowed Flash instances aren't captured when a WebView receives a WM_PRINTCLIENT message
    WebCore:
    
            Fix Bug 23623: Windowed Flash instances aren't captured when a WebView
            receives a WM_PRINTCLIENT message
    
            <https://bugs.webkit.org/show_bug.cgi?id=23623>
            <rdar://problem/6513921>
            <rdar://problem/6536874>
    
            Flash doesn't seem to respond to WM_PRINTCLIENT, so we change
            WM_PRINTCLIENT messages into WM_PAINT messages and hook into the
            BeginPaint/EndPaint APIs to return the HDC we received from
            WM_PRINTCLIENT. This causes the plugin to paint into the
            WM_PRINTCLIENT HDC via its normal WM_PAINT handler. This technique
            came from <http://www.fengyuan.com/article/wmprint.html>.
    
            No test possible.
    
            Reviewed by Darin Adler.
    
            * platform/graphics/GraphicsContext.h: Added
            [set]ShouldIncludeChildWindows on Windows.
    
            * platform/graphics/cg/GraphicsContextPlatformPrivateCG.h: Added
            m_shouldIncludeChildWindows.
            (WebCore::GraphicsContextPlatformPrivate::GraphicsContextPlatformPrivate):
            Initialize the new member.
    
            * platform/graphics/win/GraphicsContextCGWin.cpp:
            (WebCore::GraphicsContext::setShouldIncludeChildWindows):
            (WebCore::GraphicsContext::shouldIncludeChildWindows):
            Added simple setter and getter.
    
            * plugins/PluginView.cpp:
            (WebCore::PluginView::PluginView): Initialize the new member.
    
            * plugins/PluginView.h: Added m_wmPrintHDC and various functions
            required for making WM_PRINTCLIENT work.
    
            * plugins/win/PluginViewWin.cpp:
            (WebCore::PluginView::hookedBeginPaint):
            (WebCore::PluginView::hookedEndPaint):
            (WebCore::hook):
            (WebCore::setUpOffscreenPaintingHooks):
            Added. Code was modified from code available on fengyuan.com.
    
            (WebCore::PluginView::wndProc): Turn WM_PRINTCLIENT messages into
            WM_PAINT messages and save off the HDC that we're supposed to paint
            into.
    
            (WebCore::PluginView::paintWindowedPluginIntoContext): Added. Gets an
            HDC from the GraphicsContext, translates it into client coordinates,
            gets the plugin to draw into the HDC by sending a WM_PRINTCLIENT
            message, and returns the HDC to the GraphicsContext.
    
            (WebCore::PluginView::paint): If we're trying to paint a windowed
            plugin into the GraphicsContext, call paintWindowedPluginIntoContext.
    
            (WebCore::PluginView::init): Call setUpOffscreenPaintingHooks to make
            our WM_PRINTCLIENT trick work.
    
    WebKit/win:
    
            Fix Bug 23623: Windowed Flash instances aren't captured when a WebView
            receives a WM_PRINTCLIENT message
    
            <https://bugs.webkit.org/show_bug.cgi?id=23623>
            <rdar://problem/6513921>
            <rdar://problem/6536874>
    
            Reviewed by Darin Adler.
    
            * WebFrame.cpp:
            (WebFrame::paintDocumentRectToContext):
            (WebFrame::spoolPages):
            Call GraphicsContext::setShouldIncludeChildWindows so that child
            windows will be painted into the GraphicsContext rather than painted
            directly to the screen.
    
            * WebView.cpp:
            (WebView::updateBackingStore): Added a windowsToPaint parameter, which
            we pass along to paintIntoBackingStore.
    
            (WebView::paint): Tell updateBackingStore to paint child windows if
            we're not painting to the screen.
    
            (WebView::paintIntoBackingStore): Added a windowsToPaint paramter,
            which we use to tell our GraphicsContext whether or not to include
            child windows.
    
            * WebView.h: Added windowsToPaint parameters to paintIntoBackingStore
            and updateBackingStore (which I also made private).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/GraphicsContextPlatformPrivateCG.h
+++ third_party/WebKit/WebCore/platform/graphics/cg/GraphicsContextPlatformPrivateCG.h
@@ -35,6 +35,7 @@ public:
 #if PLATFORM(WIN)
         , m_hdc(0)
         , m_transparencyCount(0)
+        , m_shouldIncludeChildWindows(false)
 #endif
         , m_userToDeviceTransformKnownToBeIdentity(false)
     {
@@ -75,6 +76,7 @@ public:
 
     HDC m_hdc;
     unsigned m_transparencyCount;
+    bool m_shouldIncludeChildWindows;
 #endif
 
     CGContextRef m_cgContext;
