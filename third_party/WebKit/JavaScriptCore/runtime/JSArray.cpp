4b7f4e5c5ddf3fa7cd84e617cfd723cc79fe8225
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-29 00:26:28 +0000

    2009-01-28  Sam Weinig  <sam@webkit.org>
            Reviewed by Geoff Garen.
    
            Initial patch by Mark Rowe.
    
            <rdar://problem/6519356>
            REGRESSION (r36006): "out of memory" alert running dromaeo on Windows
    
            Report the cost of the ArrayStorage vector more accurately/often.
    
            * runtime/JSArray.cpp:
            (JSC::JSArray::JSArray): Report the extra cost even for a filled array
            because JSString using the single character optimization and immediates
            wont increase the cost themselves. 
            (JSC::JSArray::putSlowCase): Update the cost when increasing the size of
            the array.
            (JSC::JSArray::increaseVectorLength): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40339 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSArray.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSArray.cpp
@@ -176,8 +176,7 @@ JSArray::JSArray(ExecState* exec, PassRefPtr<Structure> structure, const ArgList
 
     m_storage = storage;
 
-    // When the array is created non-empty, its cells are filled, so it's really no worse than
-    // a property map. Therefore don't report extra memory cost.
+    Heap::heap(this)->reportExtraMemoryCost(storageSize(length));
 
     checkConsistency();
 }
@@ -350,6 +349,9 @@ NEVER_INLINE void JSArray::putSlowCase(ExecState* exec, unsigned i, JSValuePtr v
     }
 
     unsigned vectorLength = storage->m_vectorLength;
+
+    Heap::heap(this)->reportExtraMemoryCost(storageSize(newVectorLength) - storageSize(vectorLength));
+
     if (newNumValuesInVector == storage->m_numValuesInVector + 1) {
         for (unsigned j = vectorLength; j < newVectorLength; ++j)
             storage->m_vector[j] = noValue();
@@ -463,6 +465,7 @@ bool JSArray::increaseVectorLength(unsigned newLength)
     if (!storage)
         return false;
 
+    Heap::heap(this)->reportExtraMemoryCost(storageSize(newVectorLength) - storageSize(vectorLength));
     storage->m_vectorLength = newVectorLength;
 
     for (unsigned i = vectorLength; i < newVectorLength; ++i)
