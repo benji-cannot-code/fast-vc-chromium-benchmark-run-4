536ca21398eb625a42b39ad1949c0bab3297866f
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-30 23:11:17 +0000

    2009-01-30  Sam Weinig  <sam@webkit.org>
            Commit files I forgot to in the previous commit.
    
            * page/AccessibilityRenderObject.cpp:
            (WebCore::AccessibilityRenderObject::layoutCount):
            * page/Frame.cpp:
            (WebCore::Frame::contentRenderer):
            (WebCore::Frame::forceLayoutWithPageWidthRange):
            * page/FrameView.cpp:
            (WebCore::FrameView::layout):
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::nodeAtPoint):
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::repaintUsingContainer):
            (WebCore::RenderObject::repaint):
            (WebCore::RenderObject::repaintRectangle):
            (WebCore::RenderObject::view):
            (WebCore::RenderObject::scheduleRelayout):
            (WebCore::RenderObject::maximalOutlineSize):
            * rendering/SVGRenderSupport.cpp:
            (WebCore::clampImageBufferSizeToViewport):
            * svg/SVGLength.cpp:
            (WebCore::SVGLength::PercentageOfViewport):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40425 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/WebCore/rendering/RenderObject.cpp
@@ -1656,7 +1656,7 @@ RenderBox* RenderObject::containerForRepaint() const
 void RenderObject::repaintUsingContainer(RenderBox* repaintContainer, const IntRect& r, bool immediate)
 {
     if (!repaintContainer || repaintContainer->isRenderView()) {
-        RenderView* v = repaintContainer ? static_cast<RenderView*>(repaintContainer) : view();
+        RenderView* v = repaintContainer ? toRenderView(repaintContainer) : view();
         v->repaintViewRectangle(r, immediate);
     } else {
         // Handle container-relative repaints eventually.
@@ -1673,7 +1673,7 @@ void RenderObject::repaint(bool immediate)
     if (!o->isRenderView())
         return;
 
-    RenderView* view = static_cast<RenderView*>(o);
+    RenderView* view = toRenderView(o);
     if (view->printing())
         return; // Don't repaint if we're printing.
 
@@ -1690,7 +1690,7 @@ void RenderObject::repaintRectangle(const IntRect& r, bool immediate)
     if (!o->isRenderView())
         return;
 
-    RenderView* view = static_cast<RenderView*>(o);
+    RenderView* view = toRenderView(o);
     if (view->printing())
         return; // Don't repaint if we're printing.
 
@@ -2205,7 +2205,7 @@ IntRect RenderObject::localCaretRect(InlineBox*, int, int* extraWidthToEndOfLine
 
 RenderView* RenderObject::view() const
 {
-    return static_cast<RenderView*>(document()->renderer());
+    return toRenderView(document()->renderer());
 }
 
 bool RenderObject::hasOutlineAnnotation() const
@@ -2483,7 +2483,7 @@ int RenderObject::baselinePosition(bool firstLine, bool isRootLineBox) const
 void RenderObject::scheduleRelayout()
 {
     if (isRenderView()) {
-        FrameView* view = static_cast<RenderView*>(this)->frameView();
+        FrameView* view = toRenderView(this)->frameView();
         if (view)
             view->scheduleRelayout();
     } else if (parent()) {
@@ -2733,7 +2733,7 @@ int RenderObject::maximalOutlineSize(PaintPhase p) const
 {
     if (p != PaintPhaseOutline && p != PaintPhaseSelfOutline && p != PaintPhaseChildOutlines)
         return 0;
-    return static_cast<RenderView*>(document()->renderer())->maximalOutlineSize();
+    return toRenderView(document()->renderer())->maximalOutlineSize();
 }
 
 int RenderObject::caretMinOffset() const
