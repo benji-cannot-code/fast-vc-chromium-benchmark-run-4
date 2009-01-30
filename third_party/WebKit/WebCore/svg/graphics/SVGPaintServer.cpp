1110e979e265adb8d8aa1147bad7cf581556fc5a
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-30 00:39:43 +0000

    2009-01-29  Darin Fisher  <darin@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=23627
            Fix the PLATFORM(SKIA) build.
    
            * svg/graphics/SVGPaintServer.cpp:
            (WebCore::SVGPaintServer::teardown):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40385 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/graphics/SVGPaintServer.cpp
+++ third_party/WebKit/WebCore/svg/graphics/SVGPaintServer.cpp
@@ -38,6 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGStyledElement.h"
 #include "SVGURIReference.h"
 
+#if PLATFORM(SKIA)
+#include "PlatformContextSkia.h"
+#endif
+
 namespace WebCore {
 
 SVGPaintServer::SVGPaintServer()
@@ -180,9 +184,9 @@ void SVGPaintServer::renderPath(GraphicsContext*& context, const RenderObject* p
         context->strokePath();
 }
 
-void SVGPaintServer::teardown(GraphicsContext*&, const RenderObject*, SVGPaintTargetType, bool) const
-{
 #if PLATFORM(SKIA)
+void SVGPaintServer::teardown(GraphicsContext*& context, const RenderObject*, SVGPaintTargetType, bool) const
+{
     // FIXME: Move this into the GraphicsContext
     // WebKit implicitly expects us to reset the path.
     // For example in fillAndStrokePath() of RenderPath.cpp the path is 
@@ -191,8 +195,12 @@ void SVGPaintServer::teardown(GraphicsContext*&, const RenderObject*, SVGPaintTa
     context->beginPath();
     context->platformContext()->setGradient(0);
     context->platformContext()->setPattern(0);
-#endif
 }
+#else
+void SVGPaintServer::teardown(GraphicsContext*&, const RenderObject*, SVGPaintTargetType, bool) const
+{
+}
+#endif
 
 DashArray dashArrayFromRenderingStyle(const RenderStyle* style)
 {
