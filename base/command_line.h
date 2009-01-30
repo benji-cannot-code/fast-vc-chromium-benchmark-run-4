81ae1181e43921202801580a0c66ab002ec97e69
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-30 00:40:43 +0000

    Provides the ability to write a unit test that brings up a browser. As a proof of concept I converted FindInPageControllerTest.FindInPageFrames over to this.
    See the description in InProcessBrowserTest for how it all works.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/19644
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8934 0039d316-1c4b-4281-b951-d872f2087c98

--- base/command_line.h
+++ base/command_line.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/scoped_ptr.h"
 
+class InProcessBrowserTest;
+
 class CommandLine {
  public:
 #if defined(OS_WIN)
@@ -114,7 +116,15 @@ class CommandLine {
                        bool include_program);
 
  private:
+  friend class InProcessBrowserTest;
+
   CommandLine() {}
+
+  // Used by InProcessBrowserTest.
+  static CommandLine* ForCurrentProcessMutable() {
+    DCHECK(current_process_commandline_);
+    return current_process_commandline_;
+  }
 
   // The singleton CommandLine instance representing the current process's
   // command line.
