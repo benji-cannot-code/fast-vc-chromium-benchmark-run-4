7bf7947fd0cf3e20a2fa8cdf0d5f57b25fcb2a80
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-31 01:19:57 +0000

    Make sure that Clipboard operations that require dispatching of windows messages are performed on the UI thread.
    SetClipboardData requires the clipboard to be open with a handle
    to a window that will be notified when the contents are going to
    change again. If Windows messages are not processed, any other app
    writing to the clipboard will be locked while we acknowledge their
    request (by processing the message).
    
    The IO thread doesn't pump windows messages anymore so write
    clipboard operations cannot be performed from that thread and have
    to be posted to another thread.
    
    BUG=5823
    
    Review URL: http://codereview.chromium.org/19733
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9003 0039d316-1c4b-4281-b951-d872f2087c98

--- base/clipboard_unittest.cc
+++ base/clipboard_unittest.cc
@@ -7,12 +7,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/clipboard.h"
+#include "base/message_loop.h"
 #include "base/scoped_clipboard_writer.h"
 #include "base/string_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
+#if defined(OS_WIN)
+class ClipboardTest : public PlatformTest {
+ protected:  
+  virtual void SetUp() {
+    message_loop_.reset(new MessageLoopForUI());
+  }
+  virtual void TearDown() {
+  }
+
+ private:
+  scoped_ptr<MessageLoop> message_loop_;
+};
+#elif defined(OS_POSIX)
 typedef PlatformTest ClipboardTest;
+#endif  // defined(OS_WIN)
 
 TEST_F(ClipboardTest, ClearTest) {
   Clipboard clipboard;
