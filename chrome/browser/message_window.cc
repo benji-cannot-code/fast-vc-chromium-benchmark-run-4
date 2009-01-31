8bcada214a6303c66e1f2fe5499e9016a09fd9e8
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-31 00:21:27 +0000

    Rollback 8998
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8999 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/message_window.cc
+++ chrome/browser/message_window.cc
@@ -210,7 +210,8 @@ LRESULT MessageWindow::OnCopyData(HWND hwnd, const COPYDATASTRUCT* cds) {
       NOTREACHED();
       return TRUE;
     }
-    BrowserInit::ProcessCommandLine(cur_dir, prefs, false, profile, NULL);
+    BrowserInit::ProcessCommandLine(parsed_command_line, cur_dir, prefs, false,
+                                    profile, NULL);
     return TRUE;
   }
   return TRUE;
