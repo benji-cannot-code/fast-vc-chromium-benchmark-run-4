4363917538be0027eccd071a84affebf8a926033
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-31 02:31:32 +0000

    2009-01-30  Darin Adler  <darin@apple.com>
            Reviewed by Mark Rowe.
    
            Bug 23622: create CFString and NSString objects from WebCore::String without copying the characters
            https://bugs.webkit.org/show_bug.cgi?id=23622
    
            * platform/text/cf/StringImplCF.cpp:
            (WebCore::StringWrapperCFAllocator::allocator): Added. Returns the allocator.
            (WebCore::StringWrapperCFAllocator::retain): Added. Callback for allocator.
            (WebCore::StringWrapperCFAllocator::release): Ditto.
            (WebCore::StringWrapperCFAllocator::copyDescription): Ditto.
            (WebCore::StringWrapperCFAllocator::allocate): Ditto.
            (WebCore::StringWrapperCFAllocator::reallocate): Ditto.
            (WebCore::StringWrapperCFAllocator::deallocate): Ditto.
            (WebCore::StringWrapperCFAllocator::preferredSize): Ditto.
            (WebCore::StringWrapperCFAllocator::create): Added. Creates the allocator, but
            returns 0 if garbage collection is enabled.
            (WebCore::StringImpl::createCFString): Use StringWrapperCFAllocator if possible.
    
            * platform/text/mac/StringImplMac.mm:
            (WebCore::StringImpl::operator NSString *): Use CFString and toll-free bridging,
            rather than using NSString directly. This lets NSString benefit from the above.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/mac/StringImplMac.mm
+++ third_party/WebKit/WebCore/platform/text/mac/StringImplMac.mm
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/**
- * Copyright (C) 2006 Apple Computer, Inc.
+/*
+ * Copyright (C) 2006, 2009 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -22,13 +22,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "StringImpl.h"
 
-#include <Foundation/Foundation.h>
+#include "FoundationExtras.h"
 
 namespace WebCore {
 
 StringImpl::operator NSString *()
 {
-    return [NSString stringWithCharacters:m_data length:m_length];
+    return HardAutorelease(createCFString());
 }
 
 }
