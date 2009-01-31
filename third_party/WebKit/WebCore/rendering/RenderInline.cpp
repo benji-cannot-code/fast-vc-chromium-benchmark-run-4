e99b55afefb74c903c4a771925bb5d87501c3e19
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-31 18:07:37 +0000

    2009-01-31  Adam Treat  <adam.treat@torchmobile.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=23663
            Add a void toRenderBox() method which catches unnecessary casts at compile
            time and also fix the four cases where this was happening.
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::determineHorizontalPosition):
            * rendering/RenderBox.h:
            * rendering/RenderFlexibleBox.cpp:
            (WebCore::RenderFlexibleBox::layoutHorizontalBox):
            * rendering/RenderInline.cpp:
            (WebCore::RenderInline::updateHitTestResult):
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::updateLayerPosition):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40452 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderInline.cpp
+++ third_party/WebKit/WebCore/rendering/RenderInline.cpp
@@ -621,7 +621,7 @@ void RenderInline::updateHitTestResult(HitTestResult& result, const IntPoint& po
             RenderBlock* firstBlock = node->renderer()->containingBlock();
             
             // Get our containing block.
-            RenderBox* block = toRenderBox(this);
+            RenderBox* block = this;
             if (isInline())
                 block = containingBlock();
         
