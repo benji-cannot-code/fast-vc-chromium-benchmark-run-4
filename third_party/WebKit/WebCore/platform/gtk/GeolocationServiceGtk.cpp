784cd2c65cfe274085bf0e2a2ab64ebf6b879cfd
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-01-31 03:51:13 +0000

    [GTK] Logic fixes and build fixes
    https://bugs.webkit.org/show_bug.cgi?id=22022
    
    Catch up with the internal GeoLocation API and fix the logic of
    the previous version.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/GeolocationServiceGtk.cpp
+++ third_party/WebKit/WebCore/platform/gtk/GeolocationServiceGtk.cpp
@@ -85,7 +85,7 @@ bool GeolocationServiceGtk::startUpdating(PositionOptions* options)
     g_object_unref(master);
 
     if (!client) {
-        setError(PositionError::LOCATION_PROVIDER_ERROR, "Could not connect to location provider.");
+        setError(PositionError::UNKNOWN_ERROR, "Could not connect to location provider.");
         return false;
     }
 
@@ -100,14 +100,14 @@ bool GeolocationServiceGtk::startUpdating(PositionOptions* options)
                                                              true, GEOCLUE_RESOURCE_ALL, &error.outPtr());
 
     if (!result) {
-        setError(PositionError::LOCATION_PROVIDER_ERROR, error->message);
+        setError(PositionError::UNKNOWN_ERROR, error->message);
         g_object_unref(client);
         return false;
     }
 
     m_geocluePosition = geoclue_master_client_create_position(client, &error.outPtr());
     if (!m_geocluePosition) {
-        setError(PositionError::LOCATION_PROVIDER_ERROR, error->message);
+        setError(PositionError::UNKNOWN_ERROR, error->message);
         g_object_unref(client);
         return false;
     }
@@ -118,7 +118,7 @@ bool GeolocationServiceGtk::startUpdating(PositionOptions* options)
     m_geoclueClient = client;
     updateLocationInformation();
 
-    return result;
+    return true;
 }
 
 void GeolocationServiceGtk::stopUpdating()
@@ -167,18 +167,14 @@ void GeolocationServiceGtk::updateLocationInformation()
                                                                  &m_altitude, &accuracy.outPtr(),
                                                                  &error.outPtr());
     if (error) {
-        setError(PositionError::POSITION_NOT_FOUND_ERROR, error->message);
+        setError(PositionError::POSITION_UNAVAILABLE, error->message);
         return;
-    } else if (fields & GEOCLUE_POSITION_FIELDS_LATITUDE && fields & GEOCLUE_POSITION_FIELDS_LONGITUDE) {
-        setError(PositionError::POSITION_NOT_FOUND_ERROR, "Position could not be determined.");
+    } else if (!(fields & GEOCLUE_POSITION_FIELDS_LATITUDE && fields & GEOCLUE_POSITION_FIELDS_LONGITUDE)) {
+        setError(PositionError::POSITION_UNAVAILABLE, "Position could not be determined.");
         return;
     }
 
 
-    // The m_altitudeAccuracy accuracy is likely to be wrong
-    GeoclueAccuracyLevel level;
-    geoclue_accuracy_get_details(accuracy.get(), &level, &m_accuracy, &m_altitudeAccuracy);
-    updatePosition();
 }
 
 void GeolocationServiceGtk::updatePosition()
@@ -192,8 +188,8 @@ void GeolocationServiceGtk::updatePosition()
 
 void GeolocationServiceGtk::position_changed(GeocluePosition*, GeocluePositionFields fields, int timestamp, double latitude, double longitude, double altitude, GeoclueAccuracy* accuracy, GeolocationServiceGtk* that)
 {
-    if (fields & GEOCLUE_POSITION_FIELDS_LATITUDE && fields & GEOCLUE_POSITION_FIELDS_LONGITUDE) {
-        that->setError(PositionError::POSITION_NOT_FOUND_ERROR, "Position could not be determined.");
+    if (!(fields & GEOCLUE_POSITION_FIELDS_LATITUDE && fields & GEOCLUE_POSITION_FIELDS_LONGITUDE)) {
+        that->setError(PositionError::POSITION_UNAVAILABLE, "Position could not be determined.");
         return;
     }
 
