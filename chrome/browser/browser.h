a06cd93e8031a335e29648181c884afa855701cd
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-01 18:46:53 +0000

    More fixes.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9026 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.h
+++ chrome/browser/browser.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/pref_member.h"
 #include "base/gfx/rect.h"
+#include "base/task.h"
 #include "skia/include/SkBitmap.h"
 
 #if defined(OS_WIN)
