872e2e61719e33671c670881ffa4e2e9e54afbca
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-01 19:06:19 +0000

    More bustage fixes
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9029 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_uitest.cc
+++ chrome/browser/browser_uitest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_util.h"
 #include "base/string_util.h"
+#include "base/values.h"
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/l10n_util.h"
--- chrome/views/root_view_gtk.cc
+++ chrome/views/root_view_gtk.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/views/root_view.h"
 
+#include "base/logging.h"
+
 namespace views {
 
 // TODO(port): Port GetScheduledPaintRectConstrainedToSize() to not use RECT.
