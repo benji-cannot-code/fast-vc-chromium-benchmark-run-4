420d7aae4f74a98fdd30430d07e814684cadb060
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-02 15:29:53 +0000

    2009-02-02  Adam Treat  <adam.treat@torchmobile.com>
            Fix the Qt build to call forceLayout on the view instead.
    
            * Api/qwebpage.cpp:
            (QWebPage::setFixedLayoutSize):
            (QWebPage::setUseFixedLayout):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40474 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -1581,7 +1581,7 @@ void QWebPage::setFixedLayoutSize(const QSize &size) const
     if (frame->d->frame && frame->d->frame->view()) {
         WebCore::FrameView* view = frame->d->frame->view();
         view->setFixedLayoutSize(size);
-        frame->d->frame->forceLayout();
+        view->forceLayout();
     }
 }
 
@@ -1603,7 +1603,7 @@ void QWebPage::setUseFixedLayout(bool useFixedLayout)
     if (frame->d->frame && frame->d->frame->view()) {
         WebCore::FrameView* view = frame->d->frame->view();
         view->setUseFixedLayout(useFixedLayout);
-        frame->d->frame->forceLayout();
+        view->forceLayout();
     }
 }
 
