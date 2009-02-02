cf01f07720ad9c0fc2b8a4abfd5410b47a52aeb2
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-02 20:55:02 +0000

    2009-02-02  Darin Adler  <darin@apple.com>
            Try to fix Wx build.
    
            * html/HTMLAreaElement.cpp: Added missing include of "Path.h".
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40487 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLAreaElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLAreaElement.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLNames.h"
 #include "HitTestResult.h"
 #include "Length.h"
+#include "Path.h"
 #include "RenderObject.h"
 
 using namespace std;
