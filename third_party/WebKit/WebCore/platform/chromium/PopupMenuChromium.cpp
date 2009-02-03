c341ae52db3587a27eb1d952e41fea61c6558153
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-03 00:45:05 +0000

    2009-02-02  Darin Fisher  <darin@chromium.org>
            Reviewed by Eric Seidel (and Ojan Vafai).
    
            https://bugs.webkit.org/show_bug.cgi?id=23696
            Select element doesn't show new value when focus is switched in
            onchange event.
    
            Fix PopupMenuChromium to hide itself before calling valueChanged.  This
            better matches the behavior of the other ports.
    
            * platform/chromium/PopupMenuChromium.cpp:
            (WebCore::PopupListBox::create):
            (WebCore::PopupContainer::PopupContainer):
            (WebCore::PopupContainer::showPopup):
            (WebCore::PopupContainer::hidePopup):
            (WebCore::PopupListBox::abandon):
            (WebCore::PopupListBox::acceptIndex):
            (WebCore::PopupMenu::show):
            (WebCore::PopupMenu::hide):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40504 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/PopupMenuChromium.cpp
+++ third_party/WebKit/WebCore/platform/chromium/PopupMenuChromium.cpp
@@ -83,6 +83,11 @@ static const PopupContainerSettings dropDownSettings = {
 // box ("combobox" on Windows).
 class PopupListBox : public FramelessScrollView, public RefCounted<PopupListBox> {
 public:
+    static PassRefPtr<PopupListBox> create(PopupMenuClient* client, const PopupContainerSettings& settings)
+    {
+        return adoptRef(new PopupListBox(client, settings));
+    }
+
     // FramelessScrollView
     virtual void paint(GraphicsContext*, const IntRect&);
     virtual bool handleMouseDownEvent(const PlatformMouseEvent&);
@@ -310,13 +315,9 @@ PassRefPtr<PopupContainer> PopupContainer::create(PopupMenuClient* client,
 }
 
 PopupContainer::PopupContainer(PopupMenuClient* client, const PopupContainerSettings& settings)
-    : m_listBox(new PopupListBox(client, settings))
+    : m_listBox(PopupListBox::create(client, settings))
     , m_settings(settings)
 {
-    // FrameViews are created with a refcount of 1 so it needs releasing after we
-    // assign it to a RefPtr.
-    m_listBox->deref();
-
     setScrollbarModes(ScrollbarAlwaysOff, ScrollbarAlwaysOff);
 }
 
@@ -348,11 +349,11 @@ void PopupContainer::showPopup(FrameView* view)
         chromeClient->popupOpened(this, widgetRect, m_settings.focusOnShow);
     }
 
-    // Must get called after we have a client and containingWindow.
-    addChild(m_listBox.get());
+    if (!m_listBox->parent())
+        addChild(m_listBox.get());
 
-    // Enable scrollbars after the listbox is inserted into the hierarchy, so
-    // it has a proper WidgetClient.
+    // Enable scrollbars after the listbox is inserted into the hierarchy,
+    // so it has a proper WidgetClient.
     m_listBox->setVerticalScrollbarMode(ScrollbarAuto);
 
     m_listBox->scrollToRevealSelection();
@@ -362,12 +363,6 @@ void PopupContainer::showPopup(FrameView* view)
 
 void PopupContainer::hidePopup()
 {
-    invalidate();
-
-    m_listBox->disconnectClient();
-    removeChild(m_listBox.get());
-    m_listBox = 0;
-    
     if (client())
         client()->popupClosed(this);
 }
@@ -829,12 +824,10 @@ void PopupListBox::abandon()
 
     m_selectedIndex = m_originalIndex;
 
+    m_popupClient->hidePopup();
+
     if (m_willAcceptOnAbandon)
         m_popupClient->valueChanged(m_selectedIndex);
-
-    // valueChanged may have torn down the popup!
-    if (m_popupClient)
-        m_popupClient->hidePopup();
 }
 
 int PopupListBox::pointToRowIndex(const IntPoint& point)
@@ -866,12 +859,11 @@ void PopupListBox::acceptIndex(int index)
     if (isSelectableItem(index)) {
         RefPtr<PopupListBox> keepAlive(this);
 
-        // Tell the <select> PopupMenuClient what index was selected, and hide ourself.
-        m_popupClient->valueChanged(index);
+        // Hide ourselves first since valueChanged may have numerous side-effects.
+        m_popupClient->hidePopup();
 
-        // valueChanged may have torn down the popup!
-        if (m_popupClient)
-            m_popupClient->hidePopup();
+        // Tell the <select> PopupMenuClient what index was selected.
+        m_popupClient->valueChanged(index);
     }
 }
 
@@ -1131,16 +1123,15 @@ PopupMenu::~PopupMenu()
 
 void PopupMenu::show(const IntRect& r, FrameView* v, int index) 
 {
-    p.popup = PopupContainer::create(client(), dropDownSettings);
+    if (!p.popup)
+        p.popup = PopupContainer::create(client(), dropDownSettings);
     p.popup->show(r, v, index);
 }
 
 void PopupMenu::hide()
 {
-    if (p.popup) {
+    if (p.popup)
         p.popup->hidePopup();
-        p.popup = 0;
-    }
 }
 
 void PopupMenu::updateFromElement()
