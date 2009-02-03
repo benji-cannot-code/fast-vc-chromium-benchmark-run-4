2ab7b20b7272e12c03c19088cc9a6b0a1273e0e2
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-03 17:36:49 +0000

    Factor out platform specific chrome_paths calls into platform specific files.
    Review URL: http://codereview.chromium.org/19760
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9088 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/chrome_paths_linux.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/chrome_paths_internal.h"
+
+#include "base/logging.h"
+
+namespace chrome {
+
+bool GetDefaultUserDataDirectory(FilePath* result) {
+  // TODO(port): Decide what to do on linux.
+  NOTIMPLEMENTED();
+  return false;
+}
+
+bool GetUserDocumentsDirectory(FilePath* result) {
+  // TODO(port): Get the path (possibly using xdg-user-dirs)
+  // or decide we don't need it on other platforms.
+  NOTIMPLEMENTED();
+  return false;
+}
+
+bool GetUserDesktop(FilePath* result) {
+  // TODO(port): Get the path (possibly using xdg-user-dirs)
+  // or decide we don't need it on other platforms.
+  NOTIMPLEMENTED();
+  return false;
+}
+
+}  // namespace chrome
