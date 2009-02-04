c62aef7804d480e0086b61c828e5a43a6af8185d
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-04 23:16:29 +0000

    Add a very simple test for renderer_main, in the interests of bringup. Also bring up ipc_sync_channel_unittest on OS X.
    Review URL: http://codereview.chromium.org/21060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9184 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/ipc_sync_channel_unittest.cc
+++ chrome/common/ipc_sync_channel_unittest.cc
@@ -30,11 +30,6 @@ using base::WaitableEvent;
 
 namespace {
 
-class IPCSyncChannelTest : public testing::Test {
- private:
-  MessageLoop message_loop_;
-};
-
 // SyncChannel should only be used in child processes as we don't want to hang
 // the browser.  So in the unit test we need to have a ChildProcess object.
 class TestProcess : public ChildProcess {
@@ -235,6 +230,11 @@ void RunTest(std::vector<Worker*> workers) {
 }
 
 }  // namespace
+
+class IPCSyncChannelTest : public testing::Test {
+ private:
+  MessageLoop message_loop_;
+};
 
 //-----------------------------------------------------------------------------
 
--- chrome/renderer/renderer_main_platform_delegate_mac.cc
+++ chrome/renderer/renderer_main_platform_delegate_mac.cc
@@ -40,6 +40,5 @@ bool RendererMainPlatformDelegate::EnableSandbox() {
 }
 
 void RendererMainPlatformDelegate::RunSandboxTests() {
-  // Should run sandbox unit tests.
-  NOTIMPLEMENTED();
+  // TODO(port): Run sandbox unit test here.
 }
