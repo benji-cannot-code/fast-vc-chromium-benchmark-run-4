3a94ef14ef3303d74b81c657545b18f944f781df
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-04 18:20:17 +0000

    Revert "Make chrome/renderer/render_main.cc cross-platform + OS X bringup." This reverts commit r9141.
    TBR=jeremy
    
    Review URL: http://codereview.chromium.org/21049
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9145 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/sandbox_init_wrapper.cc
+++ chrome/common/sandbox_init_wrapper.cc
@@ -8,6 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "chrome/common/chrome_switches.h"
 
+#if defined(OS_MACOSX)
+extern "C" {
+#include <sandbox.h>
+}
+#endif
+
 #if defined(OS_WIN)
 
 void SandboxInitWrapper::SetServices(sandbox::SandboxInterfaceInfo* info) {
@@ -32,8 +38,13 @@ void SandboxInitWrapper::InitializeSandbox(const CommandLine& command_line,
 #if defined(OS_WIN)
       target_services_->Init();
 #elif defined(OS_MACOSX)
-      // Nothing to do here for OS X, see renderer_main_platform_delegate_mac.cc
-      // For Sandbox initialization.
+      // TODO(pinkerton): note, this leaks |error_buff|. What do we want to
+      // do with the error? Pass it back to main?
+      char* error_buff;
+      int error = sandbox_init(kSBXProfilePureComputation, SANDBOX_NAMED,
+                               &error_buff);
+      if (error)
+        exit(-1);
 #endif
     }
   }
