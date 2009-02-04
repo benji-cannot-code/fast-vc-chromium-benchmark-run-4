ee2fa4518d7560cadd554920309f064ede2c58cd
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-04 14:04:04 +0000

    2009-02-04  Holger Hans Peter Freyther  <zecke@selfish.org>
            Rubber stamped by Mark Rowe.
    
            FramePrivate was removed in r40024 remove the forward
            declaration as well.
    
            * page/Frame.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40611 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.h
+++ third_party/WebKit/WebCore/page/Frame.h
@@ -66,7 +66,6 @@ class Editor;
 class EventHandler;
 class FrameLoader;
 class FrameLoaderClient;
-class FramePrivate;
 class FrameTree;
 class HTMLFrameOwnerElement;
 class HTMLTableCellElement;
@@ -126,8 +125,6 @@ public:
     void createView(const IntSize&, const Color&, bool, const IntSize &, bool,
                     ScrollbarMode = ScrollbarAuto, ScrollbarMode = ScrollbarAuto);
 
-
-    friend class FramePrivate;
 
 private:
     Frame(Page*, HTMLFrameOwnerElement*, FrameLoaderClient*);
