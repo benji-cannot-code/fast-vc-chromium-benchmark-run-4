dd9ecfb8ad7b26d8096adbfa05865e4c9588315b
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-04 23:28:11 +0000

    WebKit/mac:
    2009-02-04  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Mark Rowe.
            
            Part I of <rdar://problem/6552272>.
    
            Clear the redirectURLs entry when first visiting a site, so sites that
            only redirect you the first time you visit them can later learn that
            they don't redirect.
    
            * History/WebHistory.mm:
            (-[WebHistory _visitedURL:withTitle:method:wasFailure:serverRedirectURL:isClientRedirect:]):
    
    WebKit/win:
    
    2009-02-04  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Mark Rowe.
            
            Part I of <rdar://problem/6552272>.
    
            Clear the redirectURLs entry when first visiting a site, so sites that
            only redirect you the first time you visit them can later learn that
            they don't redirect.
    
            * WebHistory.cpp:
            (WebHistory::visitedURL):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40637 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebHistory.cpp
+++ third_party/WebKit/WebKit/win/WebHistory.cpp
@@ -729,6 +729,11 @@ void WebHistory::visitedURL(const KURL& url, const String& title, const String& 
         item->historyItem()->addRedirectURL(serverRedirectURL);
     }
 
+    if (serverRedirectURL.isEmpty() && !isClientRedirect) {
+        COMPtr<WebHistoryItem> item(Query, entry);
+        item->setRedirectURLs(std::auto_ptr<Vector<String> >());
+    }
+
     CFDictionaryPropertyBag* userInfo = createUserInfoFromHistoryItem(
         getNotificationString(kWebHistoryItemsAddedNotification), entry);
     postNotification(kWebHistoryItemsAddedNotification, userInfo);
