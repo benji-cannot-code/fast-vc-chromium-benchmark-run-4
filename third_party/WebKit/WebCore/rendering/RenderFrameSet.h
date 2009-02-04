ff225a1628046e19a2022f9f5a15eb56ae922133
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-04 04:59:39 +0000

    2009-02-03  Brad Garcia  <bgarcia@google.com> Remove call to canResize when trying to determine if pointer is within a frame.
    git-svn-id: svn://svn.chromium.org/blink/trunk@40605 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderFrameSet.h
+++ third_party/WebKit/WebCore/rendering/RenderFrameSet.h
@@ -94,7 +94,6 @@ private:
 
     inline HTMLFrameSetElement* frameSet() const;
 
-    bool canResize(const IntPoint&) const;
     void setIsResizing(bool);
 
     void layOutAxis(GridAxis&, const Length*, int availableSpace);
