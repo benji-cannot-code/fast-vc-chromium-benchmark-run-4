7d54c04dc95adf0485ec1b88fab8c8f4e23e20da
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-05 21:13:39 +0000

    Linux: get us to the point where we crash at browser->window()->Show()
    Review URL: http://codereview.chromium.org/20098
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9257 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_dll_main.cc
+++ chrome/app/chrome_dll_main.cc
@@ -37,9 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/logging_chrome.h"
 #include "chrome/common/main_function_params.h"
-#if defined(OS_WIN) || defined(OS_LINUX)
 #include "chrome/common/resource_bundle.h"
-#endif
 #include "chrome/common/sandbox_init_wrapper.h"
 #if defined(OS_WIN)
 #include "sandbox/src/sandbox.h"
