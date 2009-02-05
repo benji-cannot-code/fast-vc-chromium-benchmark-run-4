be3255e9dc025477127d09425a7f0d81b5973b77
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-05 18:42:30 +0000

    Add support for the V8Custom attribute in advance of merging in http://trac.webkit.org/changeset/40617
    This change allows us to remove some of the #ifdefs in Document.idl
    
    R=levin
    
    Review URL: http://codereview.chromium.org/21075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9229 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/scripts/CodeGeneratorV8.pm
+++ webkit/port/bindings/scripts/CodeGeneratorV8.pm
@@ -742,8 +742,14 @@ sub GenerateNewFunctionTemplate
   my $interfaceName = $dataNode->name;
   my $name = $function->signature->name;
 
-  if ($function->signature->extendedAttributes->{"Custom"}) {
-    my $customFunc = $function->signature->extendedAttributes->{"Custom"};
+  if ($function->signature->extendedAttributes->{"Custom"} ||
+      $function->signature->extendedAttributes->{"V8Custom"}) {
+    if ($function->signature->extendedAttributes->{"Custom"} &&
+        $function->signature->extendedAttributes->{"V8Custom"}) {
+      die "Custom and V8Custom should be mutually exclusive!"
+    }
+    my $customFunc = $function->signature->extendedAttributes->{"Custom"} ||
+                     $function->signature->extendedAttributes->{"V8Custom"};
     if ($customFunc eq 1) {
       $customFunc = $interfaceName . WK_ucfirst($name);
     }
@@ -883,7 +889,7 @@ sub GenerateBatchedAttributeData
     $accessControl = "static_cast<v8::AccessControl>(" . $accessControl . ")";
 
 
-    my $customAccessor = $attrExt->{"Custom"} || $attrExt->{"CustomSetter"} || $attrExt->{"CustomGetter"} || "";
+    my $customAccessor = $attrExt->{"Custom"} || $attrExt->{"V8Custom"} || $attrExt->{"CustomSetter"} || $attrExt->{"CustomGetter"} || "";
     if ($customAccessor eq 1) {
       # use the naming convension, interface + (capitalize) attr name
       $customAccessor = $interfaceName . WK_ucfirst($attrName);
@@ -925,7 +931,7 @@ sub GenerateBatchedAttributeData
       }
 
     # Custom Getter and Setter
-    } elsif ($attrExt->{"Custom"}) {
+    } elsif ($attrExt->{"Custom"} || $attrExt->{"V8Custom"}) {
       $getter = "V8Custom::v8${customAccessor}AccessorGetter";
       $setter = "V8Custom::v8${customAccessor}AccessorSetter";
       
@@ -1051,7 +1057,8 @@ sub GenerateImplementation
       # Do not generate accessor if this is a custom attribute.  The
       # call will be forwarded to a hand-written accessor
       # implementation.
-      if ($attribute->signature->extendedAttributes->{"Custom"}) {
+      if ($attribute->signature->extendedAttributes->{"Custom"} ||
+          $attribute->signature->extendedAttributes->{"V8Custom"}) {
         $implIncludes{"v8_custom.h"} = 1;
         next;
       }
@@ -1080,7 +1087,8 @@ sub GenerateImplementation
     foreach my $function (@{$dataNode->functions}) {
       # hack for addEventListener/RemoveEventListener
       # TODO(fqian): avoid naming conflict
-      if ($function->signature->extendedAttributes->{"Custom"}) {
+      if ($function->signature->extendedAttributes->{"Custom"} ||
+          $function->signature->extendedAttributes->{"V8Custom"}) {
         $implIncludes{"v8_custom.h"} = 1;
 
       } else {
@@ -1830,7 +1838,8 @@ sub RequiresCustomSignature
 {
     my $function = shift;
     # No signature needed for Custom function
-    if ($function->signature->extendedAttributes->{"Custom"}) {
+    if ($function->signature->extendedAttributes->{"Custom"} ||
+        $function->signature->extendedAttributes->{"V8Custom"}) {
       return 0;
     }
 
