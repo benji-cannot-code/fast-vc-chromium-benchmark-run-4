eae4823e714cb41b009bb92a967438b4f366fbe0
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-05 03:43:03 +0000

    Fix mac/linux build break.
    Review URL: http://codereview.chromium.org/21073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9210 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.h
+++ chrome/common/temp_scaffolding_stubs.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/page_transition_types.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_service.h"
+#include "chrome/common/render_messages.h"
 #include "googleurl/src/gurl.h"
 #include "skia/include/SkBitmap.h"
 #include "webkit/glue/password_form.h"
