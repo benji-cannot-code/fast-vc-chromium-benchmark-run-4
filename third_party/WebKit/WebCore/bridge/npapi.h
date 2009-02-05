cad52a48ee991ee80ad7e9fde2e25e2ccb37f92d
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-05 20:58:34 +0000

    2009-02-05  Anders Carlsson  <andersca@apple.com>
            Reviewed by Kevin Decker.
            
            Update to match Gecko.
            
            * bridge/npapi.h:
            * plugins/npfunctions.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40676 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/npapi.h
+++ third_party/WebKit/WebCore/bridge/npapi.h
@@ -109,9 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*----------------------------------------------------------------------*/
 
 #define NP_VERSION_MAJOR 0
-#define NP_VERSION_MINOR 20
-
-
+#define NP_VERSION_MINOR 23
 
 /*----------------------------------------------------------------------*/
 /*             Definition of Basic Types                                */
@@ -344,6 +342,8 @@ typedef enum {
      */
     NPPVpluginWantsAllNetworkStreams = 18,
 
+    NPPVpluginPrivateModeBool = 19,
+    
 #ifdef XP_MACOSX
     /* Used for negotiating drawing models */
     NPPVpluginDrawingModel = 1000,
@@ -380,7 +380,9 @@ typedef enum {
     /* Get the NPObject wrapper for the plugins DOM element. */
     NPNVPluginElementNPObject = 16,
 
-    NPNVSupportsWindowless = 17
+    NPNVSupportsWindowless = 17,
+    
+    NPNVprivateModeBool = 18
 
 #ifdef XP_MACOSX
     , NPNVpluginDrawingModel = 1000 /* The NPDrawingModel specified by the plugin */
@@ -400,6 +402,11 @@ typedef enum {
     , NPNVbrowserTextInputFuncs = 1002 /* The browser text input vtable */
 #endif /* XP_MACOSX */
 } NPNVariable;
+
+typedef enum {
+   NPNURLVCookie = 501,
+   NPNURLVProxy
+} NPNURLVariable;
 
 /*
  * The type of a NPWindow - it specifies the type of the data structure
@@ -727,7 +734,11 @@ typedef struct NP_Port
 #define NPVERS_HAS_RESPONSE_HEADERS       17
 #define NPVERS_HAS_NPOBJECT_ENUM          18
 #define NPVERS_HAS_PLUGIN_THREAD_ASYNC_CALL 19
-#define NPVERS_MACOSX_HAS_EVENT_MODELS    20
+#define NPVERS_HAS_ALL_NETWORK_STREAMS    20
+#define NPVERS_HAS_URL_AND_AUTH_INFO      21
+#define NPVERS_HAS_PRIVATE_MODE           22
+#define NPVERS_MACOSX_HAS_EVENT_MODELS    23
+
 
 /*----------------------------------------------------------------------*/
 /*             Function Prototypes                */
