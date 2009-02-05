75fae8b87e5f23133108f2caa60043999d95081a
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-05 20:41:51 +0000

    Fix linker error on Linux & Mac by stubbing out a function. Review URL: http://codereview.chromium.org/20100
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9247 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.cc
+++ chrome/common/temp_scaffolding_stubs.cc
@@ -318,6 +318,9 @@ void ResourceMessageFilter::OnResolveProxyCompleted(
   NOTIMPLEMENTED();
 }
 
+void RunRepostFormWarningDialog(NavigationController*) {
+}
+
 #if defined(OS_MACOSX)
 ResourceBundle* ResourceBundle::g_shared_instance_ = NULL;
 
