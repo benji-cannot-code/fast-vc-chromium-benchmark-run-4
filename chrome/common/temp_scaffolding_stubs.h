d8af4ff4031b43288fc10dcefe0517cd289edbbd
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-05 19:17:19 +0000

    Take out unnecessary include that I added in a failed effort to fix the linux/mac builds.
    Review URL: http://codereview.chromium.org/20089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9236 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.h
+++ chrome/common/temp_scaffolding_stubs.h
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/page_transition_types.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_service.h"
-#include "chrome/common/render_messages.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/load_states.h"
 #include "skia/include/SkBitmap.h"
