0de9a23139eb82658dcf08fff91b5442ab5f08b9
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-05 01:02:58 +0000

    2009-02-04  Geoffrey Garen  <ggaren@apple.com>
            Build fix.
    
            * WebHistory.cpp:
            (WebHistory::visitedURL):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40652 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebHistory.cpp
+++ third_party/WebKit/WebKit/win/WebHistory.cpp
@@ -731,7 +731,7 @@ void WebHistory::visitedURL(const KURL& url, const String& title, const String& 
 
     if (serverRedirectURL.isEmpty() && !isClientRedirect) {
         COMPtr<WebHistoryItem> item(Query, entry);
-        item->setRedirectURLs(std::auto_ptr<Vector<String> >());
+        item->historyItem()->setRedirectURLs(std::auto_ptr<Vector<String> >());
     }
 
     CFDictionaryPropertyBag* userInfo = createUserInfoFromHistoryItem(
