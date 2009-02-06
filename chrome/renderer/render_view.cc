ba20c454ab67772834af43d8adfef3e0aa91d704
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-06 22:29:03 +0000

    Clean up unnecessary includes in render_view.cc Review URL: http://codereview.chromium.org/24005
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9347 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_piece.h"
 #include "base/string_util.h"
 #include "build/build_config.h"
-#include "chrome/app/theme/theme_resources.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/gfx/favicon_size.h"
 #include "chrome/common/gfx/color_utils.h"
@@ -51,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/webresponse.h"
 #include "webkit/glue/weburlrequest.h"
 #include "webkit/glue/webview.h"
-//#include "webkit/port/platform/graphics/PlatformContextSkia.h"
 
 #include "generated_resources.h"
 
