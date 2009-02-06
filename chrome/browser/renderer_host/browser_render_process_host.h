9202e7fb334b93d5f7cf0356006569f8eadaa5cf
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-06 20:19:05 +0000

    Janitorial: browser_render_process_host header/lint cleanup. Remove lots of header files it doesn't need and add the ones it does use. Review URL: http://codereview.chromium.org/21135
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9332 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.h
+++ chrome/browser/renderer_host/browser_render_process_host.h
@@ -8,17 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if defined(OS_WIN)
-#include <windows.h>
-#endif
+#include <string>
 
-#include <limits>
-#include <set>
-#include <vector>
-
-#include "base/id_map.h"
 #include "base/process.h"
-#include "base/rand_util.h"
 #include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
 #include "base/shared_memory.h"
@@ -31,10 +23,6 @@ class GURL;
 class PrefService;
 class RenderWidgetHelper;
 class WebContents;
-
-namespace base {
-class Thread;
-}
 
 namespace gfx {
 class Size;
@@ -123,9 +111,9 @@ class BrowserRenderProcessHost : public RenderProcessHost,
 
   // Handles actually spawning the renderer process with the appropriate options
   // for each platform.
-  bool SpawnChild(const CommandLine& command_line, 
+  bool SpawnChild(const CommandLine& command_line,
       IPC::SyncChannel* channel, base::ProcessHandle* process_handle);
-    
+
   // Sends the renderer process a new set of user scripts.
   void SendUserScriptsUpdate(base::SharedMemory* shared_memory);
 
@@ -157,4 +145,3 @@ class BrowserRenderProcessHost : public RenderProcessHost,
 std::wstring GenerateRandomChannelID(void* instance);
 
 #endif  // CHROME_BROWSER_RENDERER_HOST_BROWSER_RENDER_PROCESS_HOST_H_
-
