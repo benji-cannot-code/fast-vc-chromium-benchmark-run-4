b2cd241380cca5e805cd91ee4e39710a83165e80
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-06 18:39:18 +0000

    Fix linux build bustage.
    tbr=pinkerton
    
    Review URL: http://codereview.chromium.org/20130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9321 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/test_render_view_host.h
+++ chrome/browser/renderer_host/test_render_view_host.h
@@ -17,10 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 #if defined(OS_WIN)
-#include "chrome/browser/renderer_host/render_widget_host_view.h"
 #include "chrome/browser/tab_contents/navigation_controller.h"
 #elif defined(OS_POSIX)
 #include "chrome/common/temp_scaffolding_stubs.h"
+#endif
+
+#if !defined(OS_MACOSX)
+#include "chrome/browser/renderer_host/render_widget_host_view.h"
 #endif
 
 class TestWebContents;
