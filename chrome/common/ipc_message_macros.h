0a9607d05dc747734a4feffee2ad2d36a8662598
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-06 01:35:30 +0000

    Commit my changes that simplify automation message unpacking.
    TBR=amit
    Review URL: http://codereview.chromium.org/24002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9299 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/ipc_message_macros.h
+++ chrome/common/ipc_message_macros.h
@@ -85,7 +85,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #undef IPC_MESSAGE_ROUTED4
 #undef IPC_MESSAGE_ROUTED5
 #undef IPC_MESSAGE_ROUTED6
-#undef IPC_MESSAGE_EMPTY
 #undef IPC_SYNC_MESSAGE_CONTROL0_0
 #undef IPC_SYNC_MESSAGE_CONTROL0_1
 #undef IPC_SYNC_MESSAGE_CONTROL0_2
@@ -177,9 +176,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   msg_class##__ID,
 
 #define IPC_MESSAGE_ROUTED6(msg_class, type1, type2, type3, type4, type5, type6) \
-  msg_class##__ID,
-
-#define IPC_MESSAGE_EMPTY(msg_class) \
   msg_class##__ID,
 
 #define IPC_SYNC_MESSAGE_CONTROL0_0(msg_class) \
@@ -426,9 +422,6 @@ void class_name::OnMessageReceived(const IPC::Message& msg) \
   IPC_MESSAGE_LOG(msg_class)
 
 #define IPC_MESSAGE_ROUTED6(msg_class, type1, type2, type3, type4, type5, type6) \
-  IPC_MESSAGE_LOG(msg_class)
-
-#define IPC_MESSAGE_EMPTY(msg_class) \
   IPC_MESSAGE_LOG(msg_class)
 
 #define IPC_SYNC_MESSAGE_CONTROL0_0(msg_class) \
@@ -682,14 +675,6 @@ void class_name::OnMessageReceived(const IPC::Message& msg) \
       : IPC::MessageWithTuple< Tuple6<type1, type2, type3, type4, type5, \
       type6> >(                                                         \
           routing_id, ID, MakeTuple(arg1, arg2, arg3, arg4, arg5, arg6)) {} \
-  };
-
-// Dummy class for now, just to give us the ID field.
-#define IPC_MESSAGE_EMPTY(msg_class) \
-  class msg_class { \
-   public: \
-    enum { ID = msg_class##__ID }; \
-    static void Log(const IPC::Message* msg, std::wstring* l) {} \
   };
 
 #define IPC_SYNC_MESSAGE_CONTROL0_0(msg_class) \
