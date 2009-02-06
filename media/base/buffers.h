4619428291f7c5b5fc9ca4c9750751d9264e1d27
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-06 02:40:42 +0000

    Added missing header include to buffers.h.
    We use DCHECK inside buffers.h, but weren't including base/logging.h.
    
    Review URL: http://codereview.chromium.org/21120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9302 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/buffers.h
+++ media/base/buffers.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MEDIA_BASE_BUFFERS_H_
 
 #include "base/basictypes.h"
+#include "base/logging.h"
 #include "base/ref_counted.h"
 #include "base/time.h"
 
