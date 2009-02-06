b837f51f95ca286f6afddd3945534905179cbc4d
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-06 20:33:33 +0000

    2009-02-06  Anders Carlsson  <andersca@apple.com>
            Build fix.
            
            * bridge/npapi.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/npapi.h
+++ third_party/WebKit/WebCore/bridge/npapi.h
@@ -109,7 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*----------------------------------------------------------------------*/
 
 #define NP_VERSION_MAJOR 0
-#define NP_VERSION_MINOR 23
+#define NP_VERSION_MINOR 24
 
 /*----------------------------------------------------------------------*/
 /*             Definition of Basic Types                                */
@@ -344,6 +344,9 @@ typedef enum {
 
     NPPVpluginPrivateModeBool = 19,
     
+    /* Checks to see if the plug-in would like the browser to load the "src" attribute. */
+    NPPVpluginCancelSrcStream = 20,
+
 #ifdef XP_MACOSX
     /* Used for negotiating drawing models */
     NPPVpluginDrawingModel = 1000,
@@ -738,7 +741,7 @@ typedef struct NP_Port
 #define NPVERS_HAS_URL_AND_AUTH_INFO      21
 #define NPVERS_HAS_PRIVATE_MODE           22
 #define NPVERS_MACOSX_HAS_EVENT_MODELS    23
-
+#define NPVERS_HAS_CANCEL_SRC_STREAM      24
 
 /*----------------------------------------------------------------------*/
 /*             Function Prototypes                */
