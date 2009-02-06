fdd5c7d9c240ff6e0e3d82f2f8e2f6067ee94e55
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-06 19:29:30 +0000

    Make cert_store.cc compile on Mac and Linux. Review URL: http://codereview.chromium.org/20135
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9326 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/resource_dispatcher_host.cc
+++ chrome/browser/renderer_host/resource_dispatcher_host.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/scoped_ptr.h"
 #include "base/time.h"
+#include "chrome/browser/cert_store.h"
 #include "chrome/browser/cross_site_request_manager.h"
 #include "chrome/browser/renderer_host/async_resource_handler.h"
 #include "chrome/browser/renderer_host/buffered_resource_handler.h"
@@ -37,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_POSIX)
 #include "chrome/common/temp_scaffolding_stubs.h"
 #elif defined(OS_WIN)
-#include "chrome/browser/cert_store.h"
 #include "chrome/browser/download/download_file.h"
 #include "chrome/browser/download/download_manager.h"
 #include "chrome/browser/download/download_request_manager.h"
