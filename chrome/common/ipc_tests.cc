6fb8ef42a3f61764913c2ac409d438b672408fec
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-07 00:57:45 +0000

    Mac build fix: Mac needs more includes than Linux
    Review URL: http://codereview.chromium.org/20158
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9372 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/ipc_tests.cc
+++ chrome/common/ipc_tests.cc
@@ -5,7 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include <windows.h>
+#elif defined(OS_POSIX)
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #endif
+
 #include <stdio.h>
 #include <iostream>
 #include <string>
