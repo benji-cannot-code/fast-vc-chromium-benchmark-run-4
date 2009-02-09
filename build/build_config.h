8406700589e81db8e37895a2609130baad2c6e42
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-09 15:42:08 +0000

    WIN32 is defined by windows.h. _WIN32 is defined by the compiler. Fix the check to use _WIN32 since it is defined 100% of the time. Review URL: http://codereview.chromium.org/20108
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9387 0039d316-1c4b-4281-b951-d872f2087c98

--- build/build_config.h
+++ build/build_config.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define OS_MACOSX 1
 #elif defined(__linux__)
 #define OS_LINUX 1
-#elif defined(WIN32)
+#elif defined(_WIN32)
 #define OS_WIN 1
 #else
 #error Please add support for your platform in build/build_config.h
