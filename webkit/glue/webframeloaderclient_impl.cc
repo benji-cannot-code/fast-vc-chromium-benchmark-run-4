6b177dbc33d6031d74a6acbc0652f52754d91885
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-09 22:16:51 +0000

    WebKit merge 40722:40785 (part 2)
    Account for the renaming of toRange to toNormalizedRange done by Eric Seidel.
    
    R=amanda
    Review URL: http://codereview.chromium.org/20192
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9427 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webframeloaderclient_impl.cc
+++ webkit/glue/webframeloaderclient_impl.cc
@@ -1113,7 +1113,7 @@ void WebFrameLoaderClient::finishedLoading(DocumentLoader* dl) {
 void WebFrameLoaderClient::updateGlobalHistory() {
 }
 
-void WebFrameLoaderClient::updateGlobalHistoryForRedirectWithoutHistoryItem() {
+void WebFrameLoaderClient::updateGlobalHistoryRedirectLinks() {
 }
 
 bool WebFrameLoaderClient::shouldGoToHistoryItem(HistoryItem*) const {
--- webkit/glue/webframeloaderclient_impl.h
+++ webkit/glue/webframeloaderclient_impl.h
@@ -121,7 +121,8 @@ class WebFrameLoaderClient : public WebCore::FrameLoaderClient {
   virtual void finishedLoading(WebCore::DocumentLoader*);
 
   virtual void updateGlobalHistory();
-  virtual void updateGlobalHistoryForRedirectWithoutHistoryItem();
+  virtual void updateGlobalHistoryRedirectLinks();
+
   virtual bool shouldGoToHistoryItem(WebCore::HistoryItem*) const;
 
   virtual WebCore::ResourceError blockedError(const WebCore::ResourceRequest&);
