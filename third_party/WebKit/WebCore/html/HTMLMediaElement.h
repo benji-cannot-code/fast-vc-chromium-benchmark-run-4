62cc67f3a433f4638387b8086a708e657130cd07
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-09 19:48:27 +0000

    2009-02-09  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Antti Koivisto
            
            https://bugs.webkit.org/show_bug.cgi?id=23737
            Make it possible to control media element playback without knowing so many
            internal implementation details.
    
            * html/HTMLMediaElement.cpp:
            (WebCore::HTMLMediaElement::togglePlayState): New, toggle between playing and paused state.
            (WebCore::HTMLMediaElement::beginScrubbing): New, pause as necessary for scrubbing mode.
            (WebCore::HTMLMediaElement::endScrubbing): New, resume playback if paused for scrubbing mode.
            * html/HTMLMediaElement.h:
    
            (WebCore::HTMLMediaElement::hasVideo): New, added so clients don't need to access MediaPlayer directly.
            * html/HTMLVideoElement.h:
    
            (WebCore::HTMLVideoElement::hasVideo): New.
    
            * rendering/MediaControlElements.cpp:
            (WebCore::MediaControlPlayButtonElement::defaultEventHandler): Use new media element 
            togglePlayState method.
            (WebCore::MediaControlTimelineElement::defaultEventHandler): Tell media element when scrubbing
            begins and ends so it can deal with pausing logic. Don't call setCurrentTime unless the time
            will change.
    
            * rendering/RenderMedia.cpp:
            (WebCore::RenderMedia::updateControls): Ask media element if it is able to play instead of including
            internal logic here.
            (WebCore::RenderMedia::updateControlVisibility): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLMediaElement.h
+++ third_party/WebKit/WebCore/html/HTMLMediaElement.h
@@ -59,6 +59,7 @@ public:
     MediaPlayer* player() const { return m_player.get(); }
     
     virtual bool isVideo() const { return false; }
+    virtual bool hasVideo() const { return false; }
     
     void scheduleLoad();
     
@@ -127,6 +128,9 @@ public:
     void setVolume(float, ExceptionCode&);
     bool muted() const;
     void setMuted(bool);
+    void togglePlayState(ExceptionCode& ec);
+    void beginScrubbing();
+    void endScrubbing();
 
     bool canPlay() const;
 
