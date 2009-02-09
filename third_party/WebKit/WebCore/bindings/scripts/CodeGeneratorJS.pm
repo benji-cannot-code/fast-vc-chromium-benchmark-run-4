fce0716d8e863a6db5703a8966521b592fb11796
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-09 22:32:27 +0000

    2009-02-06  Darin Fisher  <darin@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=23809
            Return CanvasRenderingContext2D instead of DOMObject in IDL to avoid V8 #ifdefs
    
            * bindings/scripts/CodeGeneratorJS.pm:
            * dom/Document.idl:
            * html/HTMLCanvasElement.idl:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40797 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -1700,9 +1700,7 @@ sub NativeToJSValue
         $implIncludes{"NameNodeList.h"} = 1;
     }
 
-    if ($type eq "DOMObject") {
-        $implIncludes{"JSCanvasRenderingContext2D.h"} = 1;
-    } elsif ($type =~ /SVGPathSeg/) {
+    if ($type =~ /SVGPathSeg/) {
         $implIncludes{"JS$type.h"} = 1;
         $joinedName = $type;
         $joinedName =~ s/Abs|Rel//;
