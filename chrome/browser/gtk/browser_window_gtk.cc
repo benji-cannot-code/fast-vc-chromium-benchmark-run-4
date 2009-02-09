73c9440d9500ba5f51b5317519512ffbf0e37a13
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-09 22:02:55 +0000

    Porting the toolbar to GTK.
    Quite a bit of TODOs in the code, but it displays and properly
    enables/disables buttons and routes mouse clicks.
    
    This also moves all the gtk files into the gtk/ folder, at Ben's request.
    
    Review URL: http://codereview.chromium.org/21176
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9421 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_window_gtk.cc
+++ chrome/browser/gtk/browser_window_gtk.cc
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/browser_window_gtk.h"
+#include "chrome/browser/gtk/browser_window_gtk.h"
 
 #include "base/logging.h"
 #include "chrome/browser/browser.h"
+#include "chrome/browser/gtk/browser_toolbar_view_gtk.h"
 
 namespace {
 
@@ -59,6 +60,14 @@ void BrowserWindowGtk::Init() {
   g_signal_connect(G_OBJECT(window_), "window-state-event",
                    G_CALLBACK(MainWindowStateChanged), this);
   bounds_ = GetInitialWindowBounds(window_);
+
+  GtkWidget* vbox = gtk_vbox_new(FALSE, 0);
+
+  toolbar_.reset(new BrowserToolbarGtk(browser_.get()));
+  toolbar_->Init(browser_->profile());
+  toolbar_->AddToolbarToBox(vbox);
+
+  gtk_container_add(GTK_CONTAINER(window_), vbox);
 }
 
 void BrowserWindowGtk::Show() {
--- chrome/browser/gtk/browser_window_gtk.h
+++ chrome/browser/gtk/browser_window_gtk.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+ // Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_ptr.h"
 #include "chrome/browser/browser_window.h"
 
+class BrowserToolbarGtk;
 
 // An implementation of BrowserWindow for GTK.
 // Cross-platform code will interact with this object when
@@ -65,11 +66,14 @@ class BrowserWindowGtk : public BrowserWindow {
  protected:
   virtual void DestroyBrowser();
   GtkWindow* window_;
+
   scoped_ptr<Browser> browser_;
 
  private:
   gfx::Rect bounds_;
   GdkWindowState state_;
+
+  scoped_ptr<BrowserToolbarGtk> toolbar_;
 };
 
 #endif  // CHROME_BROWSER_WINDOW_GTK_H_
