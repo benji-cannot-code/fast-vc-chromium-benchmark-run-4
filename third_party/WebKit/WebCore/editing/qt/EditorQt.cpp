604e8e2b60fea71fab9c993a46e29cd4043d1900
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-10 14:27:06 +0000

    2009-02-10  Adam Treat  <adam.treat@torchmobile.com>
            Fix Qt build following r40793.
    
            * editing/qt/EditorQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40815 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/qt/EditorQt.cpp
+++ third_party/WebKit/WebCore/editing/qt/EditorQt.cpp
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ClipboardQt.h"
 #include "Document.h"
 #include "Element.h"
-#include "Selection.h"
+#include "VisibleSelection.h"
 #include "SelectionController.h"
 #include "TextIterator.h"
 #include "htmlediting.h"
