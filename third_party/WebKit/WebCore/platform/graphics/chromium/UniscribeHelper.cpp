8ff3f73458aaabe3849a56d8eac0c287ebea8cf1
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-10 08:34:15 +0000

    2009-02-10  Sverrir Berg  <sverrir@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=23630
            Add complex font rendering using Skia instead of Windows ScriptTextOut.
            This adds support for enhanced webkit styles when drawing complex
            glyphs.
    
            * platform/graphics/chromium/FontChromiumWin.cpp:
            (WebCore::Font::drawGlyphs):
            (WebCore::Font::drawComplexText):
            * platform/graphics/chromium/UniscribeHelper.cpp:
            (WebCore::containsMissingGlyphs):
            (WebCore::UniscribeHelper::draw):
            (WebCore::UniscribeHelper::shape):
            * platform/graphics/chromium/UniscribeHelper.h:
            * platform/graphics/skia/SkiaFontWin.cpp:
            (WebCore::windowsCanHandleTextDrawing):
            (WebCore::skiaDrawText):
            (WebCore::paintSkiaText):
            * platform/graphics/skia/SkiaFontWin.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40812 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/chromium/UniscribeHelper.cpp
+++ third_party/WebKit/WebCore/platform/graphics/chromium/UniscribeHelper.cpp
@@ -35,6 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 
 #include "FontUtilsChromiumWin.h"
+#include "PlatformContextSkia.h"
+#include "SkiaFontWin.h"
+#include "SkPoint.h"
 #include <wtf/Assertions.h>
 
 namespace WebCore {
@@ -59,7 +62,7 @@ static bool containsMissingGlyphs(WORD *glyphs,
                                   SCRIPT_FONTPROPERTIES* properties)
 {
     for (int i = 0; i < length; ++i) {
-        if (glyphs[i] == properties->wgDefault 
+        if (glyphs[i] == properties->wgDefault
             || (glyphs[i] == properties->wgInvalid
             && glyphs[i] != properties->wgBlank))
             return true;
@@ -288,11 +291,14 @@ int UniscribeHelper::xToCharacter(int x) const
     return 0;
 }
 
-void UniscribeHelper::draw(HDC dc, int x, int y, int from, int to)
+void UniscribeHelper::draw(GraphicsContext* graphicsContext,
+                           HDC dc, int x, int y, int from, int to)
 {
     HGDIOBJ oldFont = 0;
     int curX = x;
     bool firstRun = true;
+    bool useWindowsDrawing = windowsCanHandleTextDrawing(graphicsContext);
+    PlatformGraphicsContext* context = graphicsContext->platformContext();
 
     for (size_t screenIndex = 0; screenIndex < m_runs.size(); screenIndex++) {
         int itemIndex = m_screenOrder[screenIndex];
@@ -363,7 +369,7 @@ void UniscribeHelper::draw(HDC dc, int x, int y, int from, int to)
         // Actually draw the glyphs we found.
         int glyphCount = afterGlyph - fromGlyph;
         if (fromGlyph >= 0 && glyphCount > 0) {
-            // Account for the preceeding space we need to add to this run. We
+            // Account for the preceding space we need to add to this run. We
             // don't need to count for the following space because that will be
             // counted in advanceForItem below when we move to the next run.
             innerOffset += shaping.m_prePadding;
@@ -380,30 +386,44 @@ void UniscribeHelper::draw(HDC dc, int x, int y, int from, int to)
             // Fonts with different ascents can be used to render different
             // runs.  'Across-runs' y-coordinate correction needs to be
             // adjusted for each font.
-            HRESULT hr = S_FALSE;
+            bool textOutOk = false;
             for (int executions = 0; executions < 2; ++executions) {
-                hr = ScriptTextOut(dc, shaping.m_scriptCache,
-                                   curX + innerOffset,
-                                   y - shaping.m_ascentOffset,
-                                   0, 0, &item.a, 0, 0,
-                                   &shaping.m_glyphs[fromGlyph],
-                                   glyphCount,
-                                   &shaping.m_advance[fromGlyph],
-                                   justify,
-                                   &shaping.m_offsets[fromGlyph]);
-                if (S_OK != hr && 0 == executions) {
-                    // If this ScriptTextOut is called from the renderer it
-                    // might fail because the sandbox is preventing it from
-                    // opening the font files.  If we are running in the
-                    // renderer, TryToPreloadFont is overridden to ask the
-                    // browser to preload the font for us so we can access it.
+                if (useWindowsDrawing) {
+                    HRESULT hr = ScriptTextOut(dc, shaping.m_scriptCache,
+                                               curX + innerOffset,
+                                               y - shaping.m_ascentOffset,
+                                               0, 0, &item.a, 0, 0,
+                                               &shaping.m_glyphs[fromGlyph],
+                                               glyphCount,
+                                               &shaping.m_advance[fromGlyph],
+                                               justify,
+                                               &shaping.m_offsets[fromGlyph]);
+                    ASSERT(S_OK == hr);
+                    textOutOk = (hr == S_OK);
+                } else {
+                    SkPoint origin;
+                    origin.fX = curX + + innerOffset;
+                    origin.fY = y + m_ascent - shaping.m_ascentOffset;
+                    textOutOk = paintSkiaText(context,
+                                              shaping.m_hfont,
+                                              glyphCount,
+                                              &shaping.m_glyphs[fromGlyph],
+                                              &shaping.m_advance[fromGlyph],
+                                              &shaping.m_offsets[fromGlyph],
+                                              &origin);
+                }
+
+                if (!textOutOk && 0 == executions) {
+                    // If TextOut is called from the renderer it might fail
+                    // because the sandbox is preventing it from opening the
+                    // font files.  If we are running in the renderer,
+                    // TryToPreloadFont is overridden to ask the browser to
+                    // preload the font for us so we can access it.
                     tryToPreloadFont(shaping.m_hfont);
                     continue;
                 }
                 break;
             }
-
-            ASSERT(S_OK == hr);
         }
 
         curX += advanceForItem(itemIndex);
@@ -563,7 +583,7 @@ bool UniscribeHelper::shape(const UChar* input,
         //     PurifyMarkAsInitialized(
         //         &shaping.m_glyphs[0],
         //         sizeof(shaping.m_glyphs[0] * generatedGlyphs);
-        
+
         ZeroMemory(&shaping.m_glyphs[0],
                    sizeof(shaping.m_glyphs[0]) * shaping.m_glyphs.size());
 #endif
