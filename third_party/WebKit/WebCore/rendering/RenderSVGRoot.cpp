9b97524a38ba7025cce8454a60b478aa06272a8c
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-10 17:00:12 +0000

    2009-02-10  Adam Treat  <adam.treat@torchmobile.com>
            Reviewed by George Staikos.
    
            Attempt to fix the Qt build after r40791.
    
            * rendering/RenderSVGRoot.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40823 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderSVGRoot.cpp
+++ third_party/WebKit/WebCore/rendering/RenderSVGRoot.cpp
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGSVGElement.h"
 #include "SVGStyledElement.h"
 
+#if ENABLE(SVG_FILTERS)
+#include "SVGResourceFilter.h"
+#endif
+
 using namespace std;
 
 namespace WebCore {
