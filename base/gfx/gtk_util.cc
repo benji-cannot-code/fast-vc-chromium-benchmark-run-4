32b913e42be81d97a5c7c004ba4c7931c27f7306
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-11 18:05:07 +0000

    Add a gtk_utils (like gdi_utils), with region rectangle subtraction.
    This will be used like the gdi functions, for constructing clipping regions.
    
    Add ToGdkRectangle() on gfx::Rect, similar to the win32 and CG methods.
    
    Make the scons file better reflect which files are platform specific.
    
    Review URL: http://codereview.chromium.org/20259
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9574 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/gfx/gtk_util.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/gfx/gtk_util.h"
+
+#include <gdk/gdk.h>
+
+#include "base/gfx/rect.h"
+
+namespace gfx {
+
+void SubtractRectanglesFromRegion(GdkRegion* region,
+                                  const std::vector<gfx::Rect>& cutouts) {
+  for (size_t i = 0; i < cutouts.size(); ++i) {
+    GdkRectangle rect = cutouts[i].ToGdkRectangle();
+    GdkRegion* rect_region = gdk_region_rectangle(&rect);
+    gdk_region_subtract(region, rect_region);
+    // TODO(deanm): It would be nice to be able to reuse the GdkRegion here.
+    gdk_region_destroy(rect_region);
+  }
+}
+
+}  // namespace gfx
