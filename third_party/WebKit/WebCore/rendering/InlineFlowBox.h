82ebd542ad3d5632c059fec96c44ec6153586017
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-11 06:03:33 +0000

    2009-02-10  David Hyatt  <hyatt@apple.com>
            Fix for pixel test regression in fast/text.  Make sure not to add in
            the borderTop and paddingTop of the block to the baseline when painting
            strict mode text decorations.
            
            Reviewed by Mark Rowe
        
            * rendering/InlineFlowBox.cpp:
            (WebCore::InlineFlowBox::paintTextDecorations):
            * rendering/InlineFlowBox.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/InlineFlowBox.h
+++ third_party/WebKit/WebCore/rendering/InlineFlowBox.h
@@ -122,8 +122,6 @@ public:
         m_includeRightEdge = includeRight;
     }
 
-    int baseline() const { return renderer()->style(m_firstLine)->font().ascent() + borderTop() + paddingTop(); }
-
     // Helper functions used during line construction and placement.
     void determineSpacingForFlowBoxes(bool lastLine, RenderObject* endObject);
     int getFlowSpacingWidth();
