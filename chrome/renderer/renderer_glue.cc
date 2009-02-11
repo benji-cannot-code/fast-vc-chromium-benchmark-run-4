be7938325f31eb59665c471159c502c61357965a
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-11 19:13:17 +0000

    Media player availability now determined by SetMediaPlayerAvailable.
    Instead of relying on the command line, RenderProcess will attempt to load and enable the media player if possible.  This is the first in a few changes where I'll be adding in hooks to load media DLLs at runtime if present.
    
    Review URL: http://codereview.chromium.org/20253
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9584 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/renderer_glue.cc
+++ chrome/renderer/renderer_glue.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #include "base/clipboard.h"
-#include "base/command_line.h"
 #include "base/scoped_clipboard_writer.h"
 #include "base/string_util.h"
 #include "chrome/renderer/net/render_dns_master.h"
@@ -145,8 +144,16 @@ ScopedClipboardWriterGlue::~ScopedClipboardWriterGlue() {
 
 namespace webkit_glue {
 
+// Global variable set during RenderProcess::GlobalInit if video was enabled
+// and our media libraries were successfully loaded.
+static bool g_media_player_available = false;
+
+void SetMediaPlayerAvailable(bool value) {
+  g_media_player_available = value;
+}
+
 bool IsMediaPlayerAvailable() {
-  return CommandLine::ForCurrentProcess()->HasSwitch(switches::kEnableVideo);
+  return g_media_player_available;
 }
 
 void PrefetchDns(const std::string& hostname) {
