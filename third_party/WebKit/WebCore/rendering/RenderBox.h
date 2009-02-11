e97e1bbce7359c898ef30f6570f5556d270a8226
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-11 20:52:37 +0000

    2009-02-11  Sam Weinig  <sam@webkit.org>
            Reviewed by David Hyatt.
    
            Remove unneeded ASSERTS.
    
            * rendering/RenderBox.h:
            (WebCore::RenderBox::width):
            (WebCore::RenderBox::height):
            (WebCore::RenderBox::size):
            (WebCore::RenderBox::frameRect):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40864 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBox.h
+++ third_party/WebKit/WebCore/rendering/RenderBox.h
@@ -42,8 +42,8 @@ public:
 
     int x() const { return m_frameRect.x(); }
     int y() const { return m_frameRect.y(); }
-    int width() const { ASSERT(!isRenderInline()); return m_frameRect.width(); }
-    int height() const { ASSERT(!isRenderInline()); return m_frameRect.height(); }
+    int width() const { return m_frameRect.width(); }
+    int height() const { return m_frameRect.height(); }
     
     void setX(int x) { m_frameRect.setX(x); }
     void setY(int y) { m_frameRect.setY(y); }
@@ -51,7 +51,7 @@ public:
     void setHeight(int height) { m_frameRect.setHeight(height); }
     
     IntPoint location() const { return m_frameRect.location(); }
-    IntSize size() const { ASSERT(!isRenderInline()); return m_frameRect.size(); }
+    IntSize size() const { return m_frameRect.size(); }
 
     void setLocation(const IntPoint& location) { m_frameRect.setLocation(location); }
     void setLocation(int x, int y) { setLocation(IntPoint(x, y)); }
@@ -59,7 +59,7 @@ public:
     void setSize(const IntSize& size) { m_frameRect.setSize(size); }
     void move(int dx, int dy) { m_frameRect.move(dx, dy); }
 
-    IntRect frameRect() const { ASSERT(!isRenderInline()); return m_frameRect; }
+    IntRect frameRect() const { return m_frameRect; }
     void setFrameRect(const IntRect& rect) { m_frameRect = rect; }
 
     IntRect borderBoxRect() const { return IntRect(0, 0, width(), height()); }
