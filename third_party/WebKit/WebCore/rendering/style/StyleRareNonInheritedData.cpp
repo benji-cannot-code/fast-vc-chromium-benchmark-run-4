113b1c6d019af265f3cd0c5b576910bcf6942e7c
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-11 23:18:56 +0000

    2009-02-11  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dave Hyatt
    
            https://bugs.webkit.org/show_bug.cgi?id=23862
    
            Add a bit on RenderStyle that gets set when running accelerated
            transitions of transform or opacity. These ensure that styles
            compare as different during the transition, so that interruption
            can be detected reliably.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40876 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/style/StyleRareNonInheritedData.cpp
+++ third_party/WebKit/WebCore/rendering/style/StyleRareNonInheritedData.cpp
@@ -43,6 +43,9 @@ StyleRareNonInheritedData::StyleRareNonInheritedData()
     , matchNearestMailBlockquoteColor(RenderStyle::initialMatchNearestMailBlockquoteColor())
     , m_appearance(RenderStyle::initialAppearance())
     , m_borderFit(RenderStyle::initialBorderFit())
+#if USE(ACCELERATED_COMPOSITING)
+    , m_runningAcceleratedAnimation(false)
+#endif
     , m_boxShadow(0)
     , m_animations(0)
     , m_transitions(0)
@@ -70,6 +73,9 @@ StyleRareNonInheritedData::StyleRareNonInheritedData(const StyleRareNonInherited
     , matchNearestMailBlockquoteColor(o.matchNearestMailBlockquoteColor)
     , m_appearance(o.m_appearance)
     , m_borderFit(o.m_borderFit)
+#if USE(ACCELERATED_COMPOSITING)
+    , m_runningAcceleratedAnimation(o.m_runningAcceleratedAnimation)
+#endif
     , m_boxShadow(o.m_boxShadow ? new ShadowData(*o.m_boxShadow) : 0)
     , m_boxReflect(o.m_boxReflect)
     , m_animations(o.m_animations ? new AnimationList(*o.m_animations) : 0)
@@ -118,6 +124,9 @@ bool StyleRareNonInheritedData::operator==(const StyleRareNonInheritedData& o) c
         && matchNearestMailBlockquoteColor == o.matchNearestMailBlockquoteColor
         && m_appearance == o.m_appearance
         && m_borderFit == o.m_borderFit
+#if USE(ACCELERATED_COMPOSITING)
+        && !m_runningAcceleratedAnimation && !o.m_runningAcceleratedAnimation
+#endif
         && shadowDataEquivalent(o)
         && reflectionDataEquivalent(o)
         && animationDataEquivalent(o)
