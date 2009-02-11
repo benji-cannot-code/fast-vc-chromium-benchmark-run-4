a6bf8acd2b750cef6ded14f36e103418e9d0e093
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-11 12:04:27 +0000

    Trying to fix wx build.
            * bytecode/JumpTable.h: Include "MacroAssembler.h", not <MacroAssembler.h>.
            * jscore.bkl: Added assembler directory to search paths.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40854 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bytecode/JumpTable.h
+++ third_party/WebKit/JavaScriptCore/bytecode/JumpTable.h
@@ -31,9 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef JumpTable_h
 #define JumpTable_h
 
+#include "MacroAssembler.h"
 #include "UString.h"
 #include <wtf/HashMap.h>
-#include <MacroAssembler.h>
 #include <wtf/Vector.h>
 
 namespace JSC {
