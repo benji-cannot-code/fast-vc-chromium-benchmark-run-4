c40fc41ad8f58206aadbc1a34c00380bd2af1ab5
sullivan@apple.com <sullivan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-11 06:28:29 +0000

    2009-02-10  John Sullivan  <sullivan@apple.com>
            Reviewed by Dan Bernstein
            
            <https://bugs.webkit.org/show_bug.cgi?id=23891>
            [WebHistoryItem _setVisitCount:] is unused and should be removed
    
            * History/WebHistoryItem.mm:
            (-[WebHistoryItem _setVisitCount:]):
            removed this unused method, which is a synonym for setVisitCount: that was introduced
            recently and abandoned even more recently
    
            * History/WebHistoryItemInternal.h:
            removed declaration of _setVisitCount:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/History/WebHistoryItem.mm
+++ third_party/WebKit/WebKit/mac/History/WebHistoryItem.mm
@@ -414,11 +414,6 @@ static WebWindowWatcher *_windowWatcher = nil;
     core(_private)->visited(title, [NSDate timeIntervalSinceReferenceDate]);
 }
 
-- (void)_setVisitCount:(int)count
-{
-    core(_private)->setVisitCount(count);
-}
-
 - (void)_recordInitialVisit
 {
     core(_private)->recordInitialVisit();
