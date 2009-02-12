c29c2638994989556d02ac234e9570249f56f8cf
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-12 18:04:26 +0000

    Revert "Add chrome/browser/ssl/ssl_manager.cc to the build." This reverts commit 9656.
    Review URL: http://codereview.chromium.org/20308
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9672 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.cc
+++ chrome/common/temp_scaffolding_stubs.cc
@@ -341,6 +341,20 @@ void RunBeforeUnloadDialog(WebContents* web_contents,
   NOTIMPLEMENTED();
 }
 
+bool SSLManager::DeserializeSecurityInfo(const std::string&, int*, int*, int*) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void SSLManager::OnSSLCertificateError(
+    ResourceDispatcherHost* resource_dispatcher,
+    URLRequest* request,
+    int cert_error,
+    net::X509Certificate* cert,
+    MessageLoop* ui_loop) {
+  NOTIMPLEMENTED();
+}
+
 //--------------------------------------------------------------------------
 
 void RunRepostFormWarningDialog(NavigationController*) {
@@ -417,11 +431,6 @@ void DebuggerShell::ProcessCommand(const std::wstring& data) {
   NOTIMPLEMENTED();
 }
 #endif  // !CHROME_DEBUGGER_DISABLED
-
-SSLPolicy* SSLPolicy::GetDefaultPolicy() {
-  NOTIMPLEMENTED();
-  return NULL;
-}
 
 void HistoryURLProvider::ExecuteWithDB(history::HistoryBackend*,
                                        history::URLDatabase*,
