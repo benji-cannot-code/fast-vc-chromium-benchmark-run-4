0130ffba72edd1ed89c364ff18e13c339ebc3690
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-12 23:14:02 +0000

    2009-02-12  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Simon Fraser.
    
            https://bugs.webkit.org/show_bug.cgi?id=23923
            Implement mediaPlayerVolumeChanged so a media engine can report autonomous volume changes.
            Supporting this requires that we know when we are processing a media engine callback so
            we don't turn around and tell the media player to change the volume when it is reporting
            a volume change.
            
            * html/HTMLMediaElement.cpp:
            (WebCore::HTMLMediaElement::HTMLMediaElement): initialize m_processingMediaPlayerCallback
            (WebCore::HTMLMediaElement::mediaPlayerNetworkStateChanged): call begin/endProcessingMediaPlayerCallback
            (WebCore::HTMLMediaElement::setNetworkState): logic split out of mediaPlayerNetworkStateChanged 
            to make it easier to maintain m_processingMediaPlayerCallback.
            (WebCore::HTMLMediaElement::mediaPlayerReadyStateChanged): call begin/endProcessingMediaPlayerCallback
            (WebCore::HTMLMediaElement::mediaPlayerTimeChanged): Ditto.
            (WebCore::HTMLMediaElement::mediaPlayerRepaint): Ditto.
            (WebCore::HTMLMediaElement::mediaPlayerVolumeChanged): New
            (WebCore::HTMLMediaElement::updateVolume): Don't change media engine volume if we are processing
            a callback from the engine.
            * html/HTMLMediaElement.h:
            (WebCore::HTMLMediaElement::processingMediaPlayerCallback): New, return true if (m_processingMediaPlayerCallback>0)
            (WebCore::HTMLMediaElement::beginProcessingMediaPlayerCallback): New, increment m_processingMediaPlayerCallback
            (WebCore::HTMLMediaElement::endProcessingMediaPlayerCallback): New, decrement m_processingMediaPlayerCallback
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40943 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLMediaElement.cpp
@@ -86,6 +86,7 @@ HTMLMediaElement::HTMLMediaElement(const QualifiedName& tagName, Document* doc)
     , m_inActiveDocument(true)
     , m_player(0)
     , m_loadRestrictions(NoLoadRestriction)
+    , m_processingMediaPlayerCallback(0)
     , m_sendProgressEvents(true)
 {
     document()->registerForDocumentActivationCallbacks(this);
@@ -377,14 +378,23 @@ end:
     m_loadNestingLevel--;
 }
 
+
 void HTMLMediaElement::mediaPlayerNetworkStateChanged(MediaPlayer*)
 {
-    if (!m_begun || m_networkState == EMPTY)
+    if (!m_begun)
+        return;
+    
+    beginProcessingMediaPlayerCallback();
+    setNetworkState(m_player->networkState());
+    endProcessingMediaPlayerCallback();
+}
+
+void HTMLMediaElement::setNetworkState(MediaPlayer::NetworkState state)
+{
+    if (m_networkState == EMPTY)
         return;
     
     m_terminateLoadBelowNestingLevel = m_loadNestingLevel;
-
-    MediaPlayer::NetworkState state = m_player->networkState();
     
     // 3.14.9.4. Loading the media resource
     // 14
@@ -464,8 +474,12 @@ void HTMLMediaElement::mediaPlayerNetworkStateChanged(MediaPlayer*)
 
 void HTMLMediaElement::mediaPlayerReadyStateChanged(MediaPlayer*)
 {
+    beginProcessingMediaPlayerCallback();
+
     MediaPlayer::ReadyState state = m_player->readyState();
     setReadyState((ReadyState)state);
+
+    endProcessingMediaPlayerCallback();
 }
 
 void HTMLMediaElement::setReadyState(ReadyState state)
@@ -942,6 +956,8 @@ void HTMLMediaElement::checkIfSeekNeeded()
 
 void HTMLMediaElement::mediaPlayerTimeChanged(MediaPlayer*)
 {
+    beginProcessingMediaPlayerCallback();
+
     if (readyState() >= CAN_PLAY)
         m_seeking = false;
     
@@ -959,12 +975,23 @@ void HTMLMediaElement::mediaPlayerTimeChanged(MediaPlayer*)
     }
 
     updatePlayState();
+
+    endProcessingMediaPlayerCallback();
 }
 
 void HTMLMediaElement::mediaPlayerRepaint(MediaPlayer*)
 {
+    beginProcessingMediaPlayerCallback();
     if (renderer())
         renderer()->repaint();
+    endProcessingMediaPlayerCallback();
+}
+
+void HTMLMediaElement::mediaPlayerVolumeChanged(MediaPlayer*)
+{
+    beginProcessingMediaPlayerCallback();
+    updateVolume();
+    endProcessingMediaPlayerCallback();
 }
 
 PassRefPtr<TimeRanges> HTMLMediaElement::buffered() const
@@ -1032,10 +1059,13 @@ void HTMLMediaElement::updateVolume()
     if (!m_player)
         return;
 
-    Page* page = document()->page();
-    float volumeMultiplier = page ? page->mediaVolume() : 1;
-
-    m_player->setVolume(m_muted ? 0 : m_volume * volumeMultiplier);
+    // Avoid recursion when the player reports volume changes.
+    if (!processingMediaPlayerCallback()) {
+        Page* page = document()->page();
+        float volumeMultiplier = page ? page->mediaVolume() : 1;
+    
+        m_player->setVolume(m_muted ? 0 : m_volume * volumeMultiplier);
+    }
     
     if (renderer())
         renderer()->updateFromElement();
