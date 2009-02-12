c7447bc8b64db6e275ac9446fed017613248b2ee
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-12 02:30:38 +0000

    WebKit/mac:
    2009-02-11  Brady Eidson  <beidson@apple.com>
    
            Reviewed by Mark Rowe
    
            <rdar://problem/6570573> Some visit counts in History.plist have insanely high values, can roll over to negative
    
            Remove the item from the date caches before registering the visit.  Otherwise it might not be successfully removed 
            and when we add it back later it will exist in the list twice.  This will cause the entry to be written out twice,
            which would lead to doubling (or more!) the visit count on next launch when these multiple items are merged.
    
            * History/WebHistory.mm:
            (-[WebHistoryPrivate visitedURL:withTitle:]): Swap the removeItemFromDateCaches and visitedWithTitle calls.
            (-[WebHistoryPrivate addItem:discardDuplicate:]): Add a mode that allows the entry being added to be discarded
              if an entry for the URL already exists.  Use that mode when reading the History.plist so only the most
              recent entry for a given URL will be used.
            (-[WebHistoryPrivate addItems:]):
            (-[WebHistoryPrivate loadHistoryGutsFromURL:savedItemsCount:collectDiscardedItemsInto:error:]): 
    
    WebKit/win:
    
    2009-02-11  Brady Eidson  <beidson@apple.com>
    
            Reviewed by Mark Rowe
    
            <rdar://problem/6570573> Some visit counts in History.plist have insanely high values, can roll over to negative
    
            Remove the item from the date caches before registering the visit.  Otherwise it might not be successfully removed 
            and when we add it back later it will exist in the list twice.  This will cause the entry to be written out twice,
            which would lead to doubling (or more!) the visit count on next launch when these multiple items are merged.
    
            * WebHistory.cpp:
            (WebHistory::loadHistoryGutsFromURL):
            (WebHistory::addItems):
            (WebHistory::addItem): Add a mode that allows the entry being added to be discarded if an entry for the URL already 
              exists.  Use that mode when reading the History.plist so only the most recent entry for a given URL will be used.
            (WebHistory::visitedURL): Swap the removeItemFromDateCaches and visitedWithTitle calls.
            * WebHistory.h:
    
            * WebHistoryItem.cpp:
            (WebHistoryItem::initFromDictionaryRepresentation):  Add the negative-visit-count-correction-logic as implemented
              for Mac in http://trac.webkit.org/changeset/40851.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40882 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/History/WebHistory.mm
+++ third_party/WebKit/WebKit/mac/History/WebHistory.mm
@@ -69,7 +69,7 @@ NSString *DatesArrayKey = @"WebHistoryDates";
 
 - (WebHistoryItem *)visitedURL:(NSURL *)url withTitle:(NSString *)title;
 
-- (void)addItem:(WebHistoryItem *)entry;
+- (BOOL)addItem:(WebHistoryItem *)entry discardDuplicate:(BOOL)discardDuplicate;
 - (void)addItems:(NSArray *)newEntries;
 - (BOOL)removeItem:(WebHistoryItem *)entry;
 - (BOOL)removeItems:(NSArray *)entries;
@@ -267,8 +267,15 @@ static WebHistoryDateKey timeIntervalForBeginningOfDay(NSTimeInterval interval)
 
     if (entry) {
         LOG(History, "Updating global history entry %@", entry);
-        [entry _visitedWithTitle:title];
+        // Remove the item from date caches before changing its last visited date.  Otherwise we might get duplicate entries
+        // as seen in <rdar://problem/6570573>.
+#if ASSERT_UNUSED
         [self removeItemFromDateCaches:entry];
+#else
+        BOOL itemWasInDateCaches = [self removeItemFromDateCaches:entry];
+        ASSERT(itemWasInDateCaches);
+#endif
+        [entry _visitedWithTitle:title];
     } else {
         LOG(History, "Adding new global history entry for %@", url);
         entry = [[WebHistoryItem alloc] initWithURLString:URLString title:title lastVisitedTimeInterval:[NSDate timeIntervalSinceReferenceDate]];
@@ -282,7 +289,7 @@ static WebHistoryDateKey timeIntervalForBeginningOfDay(NSTimeInterval interval)
     return entry;
 }
 
-- (void)addItem:(WebHistoryItem *)entry
+- (BOOL)addItem:(WebHistoryItem *)entry discardDuplicate:(BOOL)discardDuplicate
 {
     ASSERT_ARG(entry, entry);
     ASSERT_ARG(entry, [entry lastVisitedTimeInterval] != 0);
@@ -291,6 +298,9 @@ static WebHistoryDateKey timeIntervalForBeginningOfDay(NSTimeInterval interval)
 
     WebHistoryItem *oldEntry = [_entriesByURL objectForKey:URLString];
     if (oldEntry) {
+        if (discardDuplicate)
+            return NO;
+
         // The last reference to oldEntry might be this dictionary, so we hold onto a reference
         // until we're done with oldEntry.
         [oldEntry retain];
@@ -304,6 +314,8 @@ static WebHistoryDateKey timeIntervalForBeginningOfDay(NSTimeInterval interval)
 
     [self addItemToDateCaches:entry];
     [_entriesByURL setObject:entry forKey:URLString];
+    
+    return YES;
 }
 
 - (BOOL)removeItem:(WebHistoryItem *)entry
@@ -360,7 +372,7 @@ static WebHistoryDateKey timeIntervalForBeginningOfDay(NSTimeInterval interval)
     // faster (fewer compares) by inserting them from oldest to newest.
     NSEnumerator *enumerator = [newEntries reverseObjectEnumerator];
     while (WebHistoryItem *entry = [enumerator nextObject])
-        [self addItem:entry];
+        [self addItem:entry discardDuplicate:NO];
 }
 
 #pragma mark DATE-BASED RETRIEVAL
@@ -545,8 +557,8 @@ static WebHistoryDateKey timeIntervalForBeginningOfDay(NSTimeInterval interval)
             if (ageLimitPassed || itemLimitPassed)
                 [discardedItems addObject:item];
             else {
-                [self addItem:item];
-                ++(*numberOfItemsLoaded);
+                if ([self addItem:item discardDuplicate:YES])
+                    ++(*numberOfItemsLoaded);
                 if (*numberOfItemsLoaded == itemCountLimit)
                     itemLimitPassed = YES;
 
