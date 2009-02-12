9ef89c99415e4d4d90e558aae930943e0c042d82
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-12 18:43:56 +0000

    IB outlets only get filled in during awakening, not initialization. D'oh!
    Review URL: http://codereview.chromium.org/20302
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9681 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_contents_controller.mm
+++ chrome/browser/cocoa/tab_contents_controller.mm
@@ -76,7 +76,7 @@ class LocationBarBridge : public LocationBar {
     if (commands_)
       observer_ = new TabContentsCommandObserver(self, commands);
     locationBarBridge_ = new LocationBarBridge(self);
-    [contentsBox_ setContentView:contents->GetNativeView()];
+    contents_ = contents;
   }
   return self;
 }
@@ -90,6 +90,8 @@ class LocationBarBridge : public LocationBar {
 }
 
 - (void)awakeFromNib {
+  [contentsBox_ setContentView:contents_->GetNativeView()];
+  
   // Provide a starting point since we won't get notifications if the state
   // doesn't change between tabs.
   [self updateToolbarCommandStatus];
