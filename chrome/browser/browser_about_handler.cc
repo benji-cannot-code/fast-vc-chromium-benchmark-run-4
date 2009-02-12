1281eec96303f5f7597ee8392302bfcfbbe5854b
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-12 19:23:05 +0000

    Port browser_about_handler. Some parts are still NOTIMPLEMENTED.
    Review URL: http://codereview.chromium.org/20300
    Patch from Craig Schlenter <craig.schlenter@gmail.com>.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9685 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 #include "chrome/browser/profile_manager.h"
 #include "chrome/browser/renderer_host/render_view_host.h"
-#include "chrome/browser/views/about_ipc_dialog.h"
 #include "chrome/common/jstemplate_builder.h"
 #include "chrome/common/l10n_util.h"
 #include "chrome/common/pref_names.h"
@@ -38,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_WIN)
+#include "chrome/browser/views/about_ipc_dialog.h"
 #include "chrome/browser/views/about_network_dialog.h"
 #endif
 
@@ -112,7 +112,7 @@ void AboutSource::StartDataRequest(const std::string& path_raw,
                                    int request_id) {
   std::string path = path_raw;
   std::string info;
-  if (path.find("/") != -1) {
+  if (path.find("/") != std::string::npos) {
     size_t pos = path.find("/");
     info = path.substr(pos + 1, path.length() - (pos + 1));
     path = path.substr(0, pos);
@@ -173,8 +173,13 @@ bool BrowserAboutHandler::MaybeHandle(GURL* url,
   }
 
   if (LowerCaseEqualsASCII(url->path(), "network")) {
+#if defined(OS_WIN)
     // Run the dialog. This will re-use the existing one if it's already up.
     AboutNetworkDialog::RunDialog();
+#else
+    NOTIMPLEMENTED();
+    // TODO(port) Implement this.
+#endif
 
     // Navigate the renderer to about:blank. This is kind of stupid but is the
     // easiest thing to do in this situation without adding a lot of complexity
@@ -185,8 +190,13 @@ bool BrowserAboutHandler::MaybeHandle(GURL* url,
 
 #ifdef IPC_MESSAGE_LOG_ENABLED
   if (LowerCaseEqualsASCII(url->path(), "ipc")) {
+#if defined(OS_WIN)
     // Run the dialog. This will re-use the existing one if it's already up.
     AboutIPCDialog::RunDialog();
+#else
+    NOTIMPLEMENTED();
+    // TODO(port) Implement this.
+#endif
     *url = GURL("about:blank");
     return false;
   }
@@ -391,7 +401,7 @@ std::string BrowserAboutHandler::AboutStats() {
 
     // JSON doesn't allow '.' in names.
     size_t pos;
-    while ((pos = name.find(".")) != -1)
+    while ((pos = name.find(".")) != std::string::npos)
       name.replace(pos, 1, ":");
 
     // Try to see if this name already exists.
