3d96eb411478122d22d48280121e9ff3eb0463ea
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-13 02:40:46 +0000

    POSIX: fix Mac build
    (It worked on the try bot, what's different with the builders?)
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9737 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/file_descriptor_set_unittest.cc
+++ chrome/common/file_descriptor_set_unittest.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This test is POSIX only.
 
+#include <unistd.h>
+#include <stdio.h>
+
 #include "base/basictypes.h"
 #include "chrome/common/file_descriptor_set_posix.h"
 #include "testing/gtest/include/gtest/gtest.h"
