8b83120051e8073f6a2b7736f238e3f03eb021f2
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-13 17:08:03 +0000

    Hack to get CG functions working in the OS X sandbox.
    Review URL: http://codereview.chromium.org/21324
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9758 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/renderer_main_platform_delegate_mac.cc
+++ chrome/renderer/renderer_main_platform_delegate_mac.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug_util.h"
 
+#include <ApplicationServices/ApplicationServices.h>
 extern "C" {
 #include <sandbox.h>
 }
@@ -25,6 +26,20 @@ RendererMainPlatformDelegate::~RendererMainPlatformDelegate() {
 void RendererMainPlatformDelegate::PlatformInitialize() {
   // Load WebKit system interfaces.
   InitWebCoreSystemInterface();
+
+  // Warmup CG - without these calls these two functions won't work in the
+  // sandbox.
+  CGColorSpaceRef rgb_colorspace =
+      CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
+
+  // Allocate a 1 byte image.
+  char data[8];
+  CGContextRef tmp = CGBitmapContextCreate(data, 1, 1, 8, 1*8,
+                                           rgb_colorspace,
+                                           kCGImageAlphaPremultipliedFirst |
+                                           kCGBitmapByteOrder32Host);
+  CGColorSpaceRelease(rgb_colorspace);
+  CGContextRelease(tmp);
 }
 
 void RendererMainPlatformDelegate::PlatformUninitialize() {
