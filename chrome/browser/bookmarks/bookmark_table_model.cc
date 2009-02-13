dc64f01cbf625d61f2114da736892dc05ff04026
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-13 01:52:48 +0000

    Add TableModel to the scaffold, so we don't depend on views. Review URL: http://codereview.chromium.org/21315
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9730 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_table_model.cc
+++ chrome/browser/bookmarks/bookmark_table_model.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time_format.h"
 #include "grit/theme_resources.h"
 #include "chrome/browser/bookmarks/bookmark_utils.h"
+#include "chrome/common/l10n_util.h"
 #include "chrome/common/resource_bundle.h"
 #include "googleurl/src/gurl.h"
 
--- chrome/browser/bookmarks/bookmark_table_model.h
+++ chrome/browser/bookmarks/bookmark_table_model.h
@@ -7,7 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_BOOKMARKS_BOOKMARK_TABLE_MODEL_H_
 
 #include "chrome/browser/bookmarks/bookmark_model.h"
+
+#include "build/build_config.h"
+
+#if defined(OS_WIN)
 #include "chrome/views/table_view.h"
+#elif defined(OS_POSIX)
+#include "chrome/common/temp_scaffolding_stubs.h"
+#endif
 
 // BookmarkTableModel provides a view of the BookmarkModel as a TableModel.
 // Three variations are provided:
