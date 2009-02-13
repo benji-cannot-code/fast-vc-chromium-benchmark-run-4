816eca7f2aa8e8859584402afe3b82af5d134c3b
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-13 12:02:04 +0000

    2009-02-13  Martin Smith  <msmith@trolltech.com>
            Reviewed by Simon Hausmann.
    
            Corrected some minor qdoc errors.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40971 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebsettings.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebsettings.cpp
@@ -711,9 +711,10 @@ qint64 QWebSettings::offlineStorageDefaultQuota()
     return QWebSettings::globalSettings()->d->offlineStorageDefaultQuota;
 }
 
-/*!
+/*
     \internal
-
+    \relates QWebSettings
+    
     Sets the path for HTML5 offline web application cache storage to \a path.
 
     \a path must point to an existing directory where the cache is stored.
@@ -729,9 +730,10 @@ void QWEBKIT_EXPORT qt_websettings_setOfflineWebApplicationCachePath(const QStri
 #endif
 }
 
-/*!
+/*
     \internal
-
+    \relates QWebSettings
+    
     Returns the path of the HTML5 offline web application cache storage
     or an empty string if the feature is disabled.
 
@@ -748,6 +750,7 @@ QString QWEBKIT_EXPORT qt_websettings_offlineWebApplicationCachePath()
 
 /*
     \since 4.5
+    \relates QWebSettings
 
     Sets the path for HTML5 local storage databases to \a path.
 
@@ -757,7 +760,6 @@ QString QWEBKIT_EXPORT qt_websettings_offlineWebApplicationCachePath()
 
     \sa localStorageDatabasePath()
 */
-
 void QWEBKIT_EXPORT qt_websettings_setLocalStorageDatabasePath(QWebSettings* settings, const QString& path)
 {
     QWebSettingsPrivate *d = settings->handle();
@@ -767,6 +769,7 @@ void QWEBKIT_EXPORT qt_websettings_setLocalStorageDatabasePath(QWebSettings* set
 
 /*
     \since 4.5
+    \relates QWebSettings
 
     Returns the path for HTML5 local storage databases
     or an empty string if the feature is disabled.
