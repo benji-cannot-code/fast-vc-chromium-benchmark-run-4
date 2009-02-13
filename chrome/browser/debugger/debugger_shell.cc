70e0150451fa7b815b24212720110550f2247eed
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-13 19:25:45 +0000

    Add debugger_shell to the POSIX build.
    Reviewed in issue 20321. Got reverted, but was not the cause of the debugger test failure.
    Review URL: http://codereview.chromium.org/21355
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9776 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/debugger_shell.cc
+++ chrome/browser/debugger/debugger_shell.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/debugger/debugger_shell.h"
 
+#include "build/build_config.h"
+
 #include "base/file_util.h"
 #include "base/path_service.h"
 #include "base/string_util.h"
@@ -16,9 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/debugger/debugger_node.h"
 #include "chrome/browser/debugger/resources/debugger_resources.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
-#include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/resource_bundle.h"
+
+#if defined(OS_WIN)
+#include "chrome/browser/tab_contents/tab_contents.h"
+#elif defined(OS_POSIX)
+#include "chrome/common/temp_scaffolding_stubs.h"
+#endif
 
 DebuggerShell::DebuggerShell(DebuggerInputOutput* io) : io_(io),
                                                         debugger_ready_(true) {
@@ -410,6 +417,3 @@ v8::Handle<v8::Value> DebuggerShell::CompileAndRun(
   }
   return v8::Undefined();
 }
-
-
-
