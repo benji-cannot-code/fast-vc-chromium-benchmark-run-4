fff1f4780c01eeb39c7a25e29f1a210e57eb18d8
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-13 18:07:44 +0000

    Register Metrics prefs for Mac and Linux.
    http://codereview.chromium.org/20350
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9763 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_prefs.cc
+++ chrome/browser/browser_prefs.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser.h"
 #include "chrome/browser/cache_manager_host.h"
+#include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/browser/renderer_host/browser_render_process_host.h"
 #include "chrome/browser/search_engines/template_url_prepopulate_data.h"
 #include "chrome/browser/google_url_tracker.h"
@@ -19,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/dns_global.h"
 #include "chrome/browser/download/download_manager.h"
 #include "chrome/browser/external_protocol_handler.h"
-#include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/browser/password_manager/password_manager.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/spellchecker.h"
@@ -42,13 +42,13 @@ void RegisterAllPrefs(PrefService* user_prefs, PrefService* local_state) {
   BrowserRenderProcessHost::RegisterPrefs(local_state);
   CacheManagerHost::RegisterPrefs(local_state);
   SafeBrowsingService::RegisterPrefs(local_state);
+  MetricsLog::RegisterPrefs(local_state);
+  MetricsService::RegisterPrefs(local_state);
 #if defined(OS_WIN)  // TODO(port): whittle this down as we port
   BookmarkManagerView::RegisterPrefs(local_state);
   BrowserView::RegisterBrowserViewPrefs(local_state);
   browser_shutdown::RegisterPrefs(local_state);
   chrome_browser_net::RegisterPrefs(local_state);
-  MetricsLog::RegisterPrefs(local_state);
-  MetricsService::RegisterPrefs(local_state);
   PageInfoWindow::RegisterPrefs(local_state);
   TaskManager::RegisterPrefs(local_state);
   ExternalProtocolHandler::RegisterPrefs(local_state);
