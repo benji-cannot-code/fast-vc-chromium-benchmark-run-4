0910ef245d30576e28259d66564f23f5bd647c29
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-13 19:04:03 +0000

    2009-02-13  Scott Violet  <sky@google.com>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=23934
            Skia platform doesn't render text decoration shadows.
    
            Makes Skia render text decoration shadows correctly. We weren't
            preparing the SkPaint correctly and didn't have a couple of checks
            CG has. Additionally makes the fillColor/strokeColor methods
            consistent.
    
            This behavior is covered by existing layout tests (see bug for list).
    
            * platform/graphics/chromium/FontChromiumWin.cpp:
            (WebCore::Font::drawGlyphs):
            (WebCore::Font::drawComplexText):
            * platform/graphics/skia/GraphicsContextSkia.cpp:
            (WebCore::GraphicsContext::drawLineForText):
            * platform/graphics/skia/PlatformContextSkia.cpp:
            (PlatformContextSkia::effectiveFillColor):
            (PlatformContextSkia::effectiveStrokeColor):
            * platform/graphics/skia/PlatformContextSkia.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@40982 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
+++ third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
@@ -648,6 +648,9 @@ void GraphicsContext::drawLineForText(const IntPoint& pt,
     if (paintingDisabled())
         return;
 
+    if (width <= 0)
+        return;
+
     int thickness = SkMax32(static_cast<int>(strokeThickness()), 1);
     SkRect r;
     r.fLeft = SkIntToScalar(pt.x());
@@ -656,7 +659,9 @@ void GraphicsContext::drawLineForText(const IntPoint& pt,
     r.fBottom = r.fTop + SkIntToScalar(thickness);
 
     SkPaint paint;
-    paint.setColor(strokeColor().rgb());
+    platformContext()->setupPaintForFilling(&paint);
+    // Text lines are drawn using the stroke color.
+    paint.setColor(platformContext()->effectiveStrokeColor());
     platformContext()->canvas()->drawRect(r, paint);
 }
 
