44a5e0fbdebd3bb0c0efaa47b6190739ff9886f7
munjal@chromium.org <munjal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-13 01:46:20 +0000

    Add ability to set a flag in the Label view class that tells it to return a preferred size of 0, 0 when the label is not visible.
    Review URL: http://codereview.chromium.org/21313
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9725 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/link.h
+++ chrome/views/link.h
@@ -86,6 +86,8 @@ class Link : public Label {
 
   // The color when the link is neither highlighted nor disabled.
   SkColor normal_color_;
+
+  DISALLOW_COPY_AND_ASSIGN(Link);
 };
 
 }  // namespace views
