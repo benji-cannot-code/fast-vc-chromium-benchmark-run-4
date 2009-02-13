8f15bcd6ce9eec0ed63cda89ba8173213889c532
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-13 18:40:32 +0000

    WebKit merge 40847-40875 Chrome side. Review URL: http://codereview.chromium.org/21351
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9767 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/inspector_client_impl.h
+++ webkit/glue/inspector_client_impl.h
@@ -21,6 +21,7 @@ public:
 
   virtual WebCore::Page* createPage();
   virtual WebCore::String localizedStringsURL();
+  virtual WebCore::String hiddenPanels();
   virtual void showWindow();
   virtual void closeWindow();
   virtual bool windowVisible();
