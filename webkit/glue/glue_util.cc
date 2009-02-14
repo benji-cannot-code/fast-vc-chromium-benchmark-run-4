8b3ed5e0789cab54d8a17100d818a9e7dc69608d
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-14 01:25:05 +0000

    Fix linux build bustage
    tbr=jhawkins
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9810 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/glue_util.cc
+++ webkit/glue/glue_util.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/glue_util.h"
 #include "base/compiler_specific.h"
 #include "base/gfx/rect.h"
-#include "base/string_part.h"
+#include "base/string_piece.h"
 #include "base/string_util.h"
 #include "base/sys_string_conversions.h"
 
