ae55eb65bcd4eb61a7b6efe26b9ac26fbaf939dc
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-14 16:39:31 +0000

    2009-02-14  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=23955
            V8HTMLPlugInCustom returns undefined, which fools interceptors.
    
            * bindings/v8/custom/V8HTMLPlugInElementCustom.cpp:
            (WebCore::NAMED_PROPERTY_GETTER): Use deferToInterceptor() return value.
            (WebCore::NAMED_PROPERTY_SETTER): Ditto.
            (WebCore::INDEXED_PROPERTY_GETTER): Ditto.
            (WebCore::INDEXED_PROPERTY_SETTER): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41017 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8HTMLPlugInElementCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8HTMLPlugInElementCustom.cpp
@@ -49,11 +49,11 @@ NAMED_PROPERTY_GETTER(HTMLPlugInElement)
     HTMLPlugInElement* imp = V8Proxy::DOMWrapperToNode<HTMLPlugInElement>(info.Holder());
     ScriptInstance scriptInstance = imp->getInstance();
     if (!scriptInstance)
-        return v8::Undefined();
+        return deferToInterceptor();
 
     v8::Local<v8::Object> instance = v8::Local<v8::Object>::New(scriptInstance->instance());
     if (instance.IsEmpty())
-        return v8::Undefined();
+        return deferToInterceptor();
 
     return NPObjectGetNamedProperty(instance, name);
 }
@@ -64,11 +64,11 @@ NAMED_PROPERTY_SETTER(HTMLPlugInElement)
     HTMLPlugInElement* imp = V8Proxy::DOMWrapperToNode<HTMLPlugInElement>(info.Holder());
     ScriptInstance scriptInstance = imp->getInstance();
     if (!scriptInstance)
-        return v8::Undefined();
+        return deferToInterceptor();
 
     v8::Local<v8::Object> instance = v8::Local<v8::Object>::New(scriptInstance->instance());
     if (instance.IsEmpty())
-        return v8::Undefined();
+        return deferToInterceptor();
 
     return NPObjectSetNamedProperty(instance, name, value);
 }
@@ -85,11 +85,11 @@ INDEXED_PROPERTY_GETTER(HTMLPlugInElement)
     HTMLPlugInElement* imp = V8Proxy::DOMWrapperToNode<HTMLPlugInElement>(info.Holder());
     ScriptInstance scriptInstance = imp->getInstance();
     if (!scriptInstance)
-        return v8::Undefined();
+        return deferToInterceptor();
 
     v8::Local<v8::Object> instance = v8::Local<v8::Object>::New(scriptInstance->instance());
     if (instance.IsEmpty())
-        return v8::Undefined();
+        return deferToInterceptor();
 
     return NPObjectGetIndexedProperty(instance, index);
 }
@@ -100,11 +100,11 @@ INDEXED_PROPERTY_SETTER(HTMLPlugInElement)
     HTMLPlugInElement* imp = V8Proxy::DOMWrapperToNode<HTMLPlugInElement>(info.Holder());
     ScriptInstance scriptInstance = imp->getInstance();
     if (!scriptInstance)
-        return v8::Undefined();
+        return deferToInterceptor();
 
     v8::Local<v8::Object> instance = v8::Local<v8::Object>::New(scriptInstance->instance());
     if (instance.IsEmpty())
-        return v8::Undefined();
+        return deferToInterceptor();
 
     return NPObjectSetIndexedProperty(instance, index, value);
 }
