ce492c158306eba6558db7411f397bc4965a042a
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-14 04:09:51 +0000

    Add chrome-extension to the list of "web-safe" schemes for resource loading. This makes web pages able to refer to resources inside extensions.
    Review URL: http://codereview.chromium.org/20371
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9821 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/renderer_security_policy.cc
+++ chrome/browser/renderer_host/renderer_security_policy.cc
@@ -83,6 +83,7 @@ RendererSecurityPolicy::RendererSecurityPolicy() {
   RegisterWebSafeScheme("ftp");
   RegisterWebSafeScheme("data");
   RegisterWebSafeScheme("feed");
+  RegisterWebSafeScheme("chrome-extension");
 
   // We know about the following psuedo schemes and treat them specially.
   RegisterPseudoScheme("about");
