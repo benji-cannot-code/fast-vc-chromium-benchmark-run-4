d2d9de5354d28ec03b727f3cf1ba2026d97c486c
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-15 00:09:42 +0000

    Clean up some includes in sessions/ directory.
    Review URL: http://codereview.chromium.org/21379
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9830 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/base_session_service.cc
+++ chrome/browser/sessions/base_session_service.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "build/build_config.h"
-
 #include "chrome/browser/sessions/base_session_service.h"
 
 #include "base/pickle.h"
@@ -15,11 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sessions/session_types.h"
 #include "chrome/browser/tab_contents/navigation_entry.h"
 #include "chrome/common/stl_util-inl.h"
-
-// TODO(port): Get rid of this section and finish porting.
-#if defined(OS_WIN)
-#include "chrome/browser/tab_contents/tab_contents.h"
-#endif
 
 // InternalGetCommandsRequest -------------------------------------------------
 
--- chrome/browser/sessions/session_restore.cc
+++ chrome/browser/sessions/session_restore.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "build/build_config.h"
-
 #include "chrome/browser/sessions/session_restore.h"
 
 #include <vector>
@@ -19,10 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/navigation_controller.h"
 #include "chrome/common/notification_registrar.h"
 #include "chrome/common/notification_service.h"
-
-#if defined(OS_WIN)
-#include "chrome/browser/tab_contents/tab_contents.h"
-#endif
 
 namespace {
 
