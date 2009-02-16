c523b17e1ef280344b71ab1e4c952922cbe86b05
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-16 05:28:06 +0000

    Make browser_shudown.cc compile on Posix.  This is r9835 with a fix for the mac build. Review URL: http://codereview.chromium.org/20393
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9848 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_shutdown.cc
+++ chrome/browser/browser_shutdown.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_shutdown.h"
 
+#include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/histogram.h"
 #include "base/path_service.h"
@@ -16,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/first_run.h"
 #include "chrome/browser/jankometer.h"
 #include "chrome/browser/metrics/metrics_service.h"
-#include "chrome/browser/plugin_process_host.h"
-#include "chrome/browser/plugin_service.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
 #include "chrome/browser/renderer_host/render_view_host.h"
 #include "chrome/browser/renderer_host/render_widget_host.h"
@@ -27,8 +26,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_service.h"
 #include "chrome/common/chrome_plugin_lib.h"
 #include "chrome/common/resource_bundle.h"
-#include "chrome/browser/plugin_service.h"
 #include "net/dns_global.h"
+
+// TODO(port): Get rid of this section and finish porting.
+#if defined(OS_WIN)
+#include "chrome/browser/plugin_process_host.h"
+#include "chrome/browser/plugin_service.h"
+#else
+#include "chrome/common/temp_scaffolding_stubs.h"
+#endif
+
 
 using base::Time;
 using base::TimeDelta;
@@ -42,7 +49,7 @@ int shutdown_num_processes_slow_;
 
 bool delete_resources_on_shutdown = true;
 
-const wchar_t* const kShutdownMsFile = L"chrome_shutdown_ms.txt";
+const char* const kShutdownMsFile = "chrome_shutdown_ms.txt";
 
 void RegisterPrefs(PrefService* local_state) {
   local_state->RegisterIntegerPref(prefs::kShutdownType, NOT_VALID);
@@ -79,11 +86,10 @@ void OnShutdownStarting(ShutdownType type) {
   }
 }
 
-std::wstring GetShutdownMsPath() {
-  std::wstring shutdown_ms_file;
+FilePath GetShutdownMsPath() {
+  FilePath shutdown_ms_file;
   PathService::Get(base::DIR_TEMP, &shutdown_ms_file);
-  file_util::AppendToPath(&shutdown_ms_file, kShutdownMsFile);
-  return shutdown_ms_file;
+  return shutdown_ms_file.AppendASCII(kShutdownMsFile);
 }
 
 void Shutdown() {
@@ -153,7 +159,7 @@ void Shutdown() {
     TimeDelta shutdown_delta = Time::Now() - shutdown_started_;
     std::string shutdown_ms = Int64ToString(shutdown_delta.InMilliseconds());
     int len = static_cast<int>(shutdown_ms.length()) + 1;
-    std::wstring shutdown_ms_file = GetShutdownMsPath();
+    FilePath shutdown_ms_file = GetShutdownMsPath();
     file_util::WriteFile(shutdown_ms_file, shutdown_ms.c_str(), len);
   }
 
@@ -161,12 +167,12 @@ void Shutdown() {
 }
 
 void ReadLastShutdownInfo() {
-  std::wstring shutdown_ms_file = GetShutdownMsPath();
+  FilePath shutdown_ms_file = GetShutdownMsPath();
   std::string shutdown_ms_str;
   int64 shutdown_ms = 0;
   if (file_util::ReadFileToString(shutdown_ms_file, &shutdown_ms_str))
     shutdown_ms = StringToInt64(shutdown_ms_str);
-  DeleteFile(shutdown_ms_file.c_str());
+  file_util::Delete(shutdown_ms_file, false);
 
   PrefService* prefs = g_browser_process->local_state();
   ShutdownType type =
