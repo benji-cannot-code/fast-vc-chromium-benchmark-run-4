ab5f77fadd745a63647dfc11c86e3b4da6e1a1c7
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-16 06:32:45 +0000

    Register PasswordManager prefs for Mac and Linux.
    Review URL: http://codereview.chromium.org/20392
    Patch from Craig Schlenter <craig.schlenter@gmail.com>.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9849 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_prefs.cc
+++ chrome/browser/browser_prefs.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser.h"
 #include "chrome/browser/cache_manager_host.h"
 #include "chrome/browser/metrics/metrics_service.h"
+#include "chrome/browser/password_manager/password_manager.h"
 #include "chrome/browser/renderer_host/browser_render_process_host.h"
 #include "chrome/browser/search_engines/template_url_prepopulate_data.h"
 #include "chrome/browser/google_url_tracker.h"
@@ -20,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/dns_global.h"
 #include "chrome/browser/download/download_manager.h"
 #include "chrome/browser/external_protocol_handler.h"
-#include "chrome/browser/password_manager/password_manager.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/spellchecker.h"
 #include "chrome/browser/ssl/ssl_manager.h"
@@ -57,12 +57,12 @@ void RegisterAllPrefs(PrefService* user_prefs, PrefService* local_state) {
   // User prefs
   SessionStartupPref::RegisterUserPrefs(user_prefs);
   Browser::RegisterUserPrefs(user_prefs);
+  PasswordManager::RegisterUserPrefs(user_prefs);
 #if defined(OS_WIN)  // TODO(port): whittle this down as we port
   BookmarkBarView::RegisterUserPrefs(user_prefs);
   BookmarkTableView::RegisterUserPrefs(user_prefs);
   chrome_browser_net::RegisterUserPrefs(user_prefs);
   DownloadManager::RegisterUserPrefs(user_prefs);
-  PasswordManager::RegisterUserPrefs(user_prefs);
   SSLManager::RegisterUserPrefs(user_prefs);
   AutofillManager::RegisterUserPrefs(user_prefs);
 #endif
