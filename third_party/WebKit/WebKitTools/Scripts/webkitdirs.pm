76c6231cb3caa290fab1c1a7ad5f5dfdbc43daa5
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-17 02:59:29 +0000

    2009-02-16  Gustavo Noronha Silva  <gns@gnome.org>
            Reviewed by Adam Roben
    
            Actually use the installation prefix defined in WebKitInstallationPrefix,
            in autotools builds.
    
            * Scripts/webkitdirs.pm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41033 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -977,6 +977,8 @@ sub buildAutotoolsProject($@)
     my $config = passedConfiguration() || configuration();
     my $prefix = $ENV{"WebKitInstallationPrefix"};
 
+    push @buildArgs, "--prefix=" . $prefix if defined($prefix);
+
     # check if configuration is Debug
     if ($config =~ m/debug/i) {
         push @buildArgs, "--enable-debug";
