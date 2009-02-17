20d9255426222b11157cfde6e42fc247c83e3e3b
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-17 20:19:48 +0000

    Registering the AutoFill manager because someone added it to the build without registering its prefs.
    Review URL: http://codereview.chromium.org/20431
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9899 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_prefs.cc
+++ chrome/browser/browser_prefs.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_prefs.h"
 
+#include "chrome/browser/autofill_manager.h"
 #include "chrome/browser/browser.h"
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/cache_manager_host.h"
@@ -17,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/web_contents.h"
 
 #if defined(OS_WIN)  // TODO(port): whittle this down as we port
-#include "chrome/browser/autofill_manager.h"
 #include "chrome/browser/net/dns_global.h"
 #include "chrome/browser/download/download_manager.h"
 #include "chrome/browser/external_protocol_handler.h"
@@ -64,8 +64,8 @@ void RegisterAllPrefs(PrefService* user_prefs, PrefService* local_state) {
   chrome_browser_net::RegisterUserPrefs(user_prefs);
   DownloadManager::RegisterUserPrefs(user_prefs);
   SSLManager::RegisterUserPrefs(user_prefs);
-  AutofillManager::RegisterUserPrefs(user_prefs);
 #endif
+  AutofillManager::RegisterUserPrefs(user_prefs);
   TabContents::RegisterUserPrefs(user_prefs);
   TemplateURLPrepopulateData::RegisterUserPrefs(user_prefs);
   WebContents::RegisterUserPrefs(user_prefs);
