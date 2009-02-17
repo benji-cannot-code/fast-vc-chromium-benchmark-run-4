6b5c8f1ec8ad58411f17d130818c9d04d3a7b674
jungshik@google.com <jungshik@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-17 20:44:34 +0000

    Change the encoding for tapuz.co.il to UTF-8 from windows-1255 and update the version to 20.
    BUG=7271
    TEST=Start Chrome in Hebrew Windows or with cmdline switch "--country=IL --lang=he" and set the default 
    search engine to tapuz (it's in Hebrew) and type a keyword in the omnibox and select search with 'tapuz'.
    Search result should come up with multiple hits. 
    
    Review URL: http://codereview.chromium.org/20315
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9901 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_prepopulate_data.cc
+++ chrome/browser/search_engines/template_url_prepopulate_data.cc
@@ -1633,7 +1633,7 @@ const PrepopulatedEngine tapuz = {
   L"tapuz.co.il",
   "http://www.tapuz.co.il/favicon.ico",
   L"http://www.tapuz.co.il/search/search.asp?q={searchTerms}",
-  "windows-1255",
+  "UTF-8",
   NULL,
   57,
 };
@@ -3187,7 +3187,7 @@ void RegisterUserPrefs(PrefService* prefs) {
 }
 
 int GetDataVersion() {
-  return 19;  // Increment this if you change the above data in ways that mean
+  return 20;  // Increment this if you change the above data in ways that mean
               // users with existing data should get a new version.
 }
 
