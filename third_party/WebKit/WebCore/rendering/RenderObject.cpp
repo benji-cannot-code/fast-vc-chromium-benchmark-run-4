a2713885c5d924d26b9b9029925dca91a8a9eae5
brettw@chromium.org <brettw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-17 18:14:35 +0000

    Reviewed by Simon Fraser.
    https://bugs.webkit.org/attachment.cgi?id=27666
    Fix Chromium build build: forgotten include in RenderObject, sync
    RenderTheme to the recent changes in RenderObject.
    
    * rendering/RenderObject.cpp:
    * rendering/RenderThemeChromiumMac.mm:
      (WebCore::RenderThemeChromiumMac::updatePressedState):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41040 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/WebCore/rendering/RenderObject.cpp
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <stdio.h>
 #include <wtf/RefCountedLeakCounter.h>
+#include <wtf/UnusedParam.h>
 
 #if USE(ACCELERATED_COMPOSITING)
 #include "RenderLayerCompositor.h"
