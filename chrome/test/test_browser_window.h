4f98023b5166ed31b67259888f297d8d12471426
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-17 17:33:13 +0000

    When internal functions try to focus the location bar, check whether it's focusable, and clear the focus if it's not.  This shouldn't kick in in any cases today (I don't think), but it will matter in fullscreen mode, where creating a new tab should not attempt to focus the (unfocusable) location bar, but should not do nothing either, since these leaves focus in a hosed state.
    There are other choices here.  I could make View::RequestFocus() try and clear the focus if the view is not focusable.  That seems likely to cause side-effects unless I restore a lot of the calls to IsFocusable() that I previously removed.  I also don't know whether sticking this on BrowserWindow was the right approach; if there's a better spot architecturally, speak up.
    Review URL: http://codereview.chromium.org/21368
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9882 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/test_browser_window.h
+++ chrome/test/test_browser_window.h
@@ -39,6 +39,7 @@ class TestBrowserWindow : public BrowserWindow {
   virtual LocationBar* GetLocationBar() const {
     return const_cast<TestLocationBar*>(&location_bar_);
   }
+  virtual void SetFocusToLocationBar() {}
   virtual void UpdateStopGoState(bool is_loading) {}
   virtual void UpdateToolbar(TabContents* contents,
                              bool should_restore_state) {}
