dd449037394d32c1e95c908b8fbbedf72884a43c
dimich@chromium.org <dimich@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-18 00:21:21 +0000

    WebCore: 2009-02-17  Peter Abrahamsen  <rainhead@gmail.com>
            Reviewed by Sam Weinig.
    
            https://bugs.webkit.org/show_bug.cgi?id=23958
            <rdar://problem/6587815>
    
            Updated XMLHttpRequest with new header names from the latest Access
            Control draft: http://www.w3.org/TR/access-control/
            - Access-Control-Origin becomes Access-Control-Allow-Origin
            - Access-Control-Credentials becomes Access-Control-Allow-Credentials
    
            * xml/XMLHttpRequest.cpp:
            (WebCore::XMLHttpRequest::accessControlCheck):
    
    LayoutTests:
    2009-02-17  Peter Abrahamsen  <rainhead@gmail.com>
    
            Reviewed by Sam Weinig.
    
            https://bugs.webkit.org/show_bug.cgi?id=23958
            <rdar://problem/6587815>
    
            Updated XMLHttpRequests tests to match the newest Access Control draft.
    
            * http/tests/security/originHeader/resources/print-origin.cgi:
            * http/tests/xmlhttprequest/resources/access-control-basic-allow-access-control-origin-header.cgi:
            * http/tests/xmlhttprequest/resources/access-control-basic-allow-print-headers.cgi:
            * http/tests/xmlhttprequest/resources/access-control-basic-allow-star.cgi:
            * http/tests/xmlhttprequest/resources/access-control-basic-allow.cgi:
            * http/tests/xmlhttprequest/resources/access-control-basic-get-fail-non-simple.cgi:
            * http/tests/xmlhttprequest/resources/access-control-basic-non-get-allow.cgi:
            * http/tests/xmlhttprequest/resources/access-control-basic-preflight-cache-invalidation.php:
            * http/tests/xmlhttprequest/resources/access-control-basic-preflight-cache-timeout.php:
            * http/tests/xmlhttprequest/resources/access-control-basic-preflight-cache.php:
            * http/tests/xmlhttprequest/resources/access-control-basic-whitelist-response-headers.cgi:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/security/originHeader/resources/print-origin.cgi
+++ third_party/WebKit/LayoutTests/http/tests/security/originHeader/resources/print-origin.cgi
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 use strict;
 
 print "Content-Type: text/html\n";
-print "Access-Control-Origin: *\n";
+print "Access-Control-Allow-Origin: *\n";
 print "Cache-Control: no-store\n\n";
 
 print "HTTP_ORIGIN: " . $ENV{"HTTP_ORIGIN"} . "\n";
--- third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
+++ third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
@@ -1232,7 +1232,7 @@ void XMLHttpRequest::didSendData(unsigned long long bytesSent, unsigned long lon
 
 bool XMLHttpRequest::accessControlCheck(const ResourceResponse& response)
 {
-    const String& accessControlOriginString = response.httpHeaderField("Access-Control-Origin");
+    const String& accessControlOriginString = response.httpHeaderField("Access-Control-Allow-Origin");
     if (accessControlOriginString == "*" && !m_includeCredentials)
         return true;
 
@@ -1241,7 +1241,7 @@ bool XMLHttpRequest::accessControlCheck(const ResourceResponse& response)
         return false;
 
     if (m_includeCredentials) {
-        const String& accessControlCredentialsString = response.httpHeaderField("Access-Control-Credentials");
+        const String& accessControlCredentialsString = response.httpHeaderField("Access-Control-Allow-Credentials");
         if (accessControlCredentialsString != "true")
             return false;
     }
