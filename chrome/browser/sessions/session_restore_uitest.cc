da9e952f26a3f9e30e9a230bf2019bf88c9ac682
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-18 23:30:19 +0000

    Renables the session restore ui tests in hopes of finding out why they are flakey. I'll watch the bot to see where they are failing after landing. Also fixes leaks in the tests I noticed when run under Purify.
    BUG=6495
    TEST=none
    
    Review URL: http://codereview.chromium.org/20477
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9991 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_restore_uitest.cc
+++ chrome/browser/sessions/session_restore_uitest.cc
@@ -118,7 +118,8 @@ TEST_F(SessionRestoreUITest, RestoresForwardAndBackwardNavs) {
   NavigateToURL(url2);
   NavigateToURL(url3);
 
-  ASSERT_TRUE(GetActiveTab()->GoBack());
+  scoped_ptr<TabProxy> active_tab(GetActiveTab());
+  ASSERT_TRUE(active_tab->GoBack());
 
   QuitBrowserAndRestore();
 
@@ -155,7 +156,8 @@ TEST_F(SessionRestoreUITest, RestoresCrossSiteForwardAndBackwardNavs) {
   NavigateToURL(cross_site_url);
   NavigateToURL(url2);
 
-  ASSERT_TRUE(GetActiveTab()->GoBack());
+  scoped_ptr<TabProxy> active_tab(GetActiveTab());
+  ASSERT_TRUE(active_tab->GoBack());
 
   QuitBrowserAndRestore();
 
@@ -242,7 +244,8 @@ TEST_F(SessionRestoreUITest, ClosedTabStaysClosed) {
 
   browser_proxy->AppendTab(url2);
 
-  browser_proxy->GetActiveTab()->Close(true);
+  scoped_ptr<TabProxy> active_tab(browser_proxy->GetActiveTab());
+  active_tab->Close(true);
 
   QuitBrowserAndRestore();
   browser_proxy.reset();
@@ -429,4 +432,3 @@ TEST_F(SessionRestoreUITest, DISABLED_ShareProcessesOnRestore) {
 
   ASSERT_EQ(expected_process_count, GetBrowserProcessCount());
 }
-
