6c91340cfe5e5b5217dd68203fd5d499959ca7c6
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-18 21:59:02 +0000

    Include what you use in plugin_list_mac.mm. Review URL: http://codereview.chromium.org/21469
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9976 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/plugin_list_mac.mm
+++ webkit/glue/plugins/plugin_list_mac.mm
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/glue/plugins/plugin_list.h"
 
+#import <Foundation/Foundation.h>
+
 #include "base/file_util.h"
 #include "base/mac_util.h"
 #include "webkit/glue/plugins/plugin_lib.h"
