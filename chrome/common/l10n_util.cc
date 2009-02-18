a80de1f948414fdf561db1a5778c4aeb4578626e
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-18 21:47:54 +0000

    load strings and display them on linux
    Review URL: http://codereview.chromium.org/21465
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9972 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/l10n_util.cc
+++ chrome/common/l10n_util.cc
@@ -18,9 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/gfx/chrome_canvas.h"
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
 // TODO(port): re-enable.
 #include "chrome/common/resource_bundle.h"
+#endif
+#if defined(OS_WIN)
 #include "chrome/views/view.h"
 #endif  // defined(OS_WIN)
 #include "unicode/coll.h"
@@ -326,7 +328,7 @@ std::wstring GetLocalName(const std::wstring& locale_code_wstr,
 }
 
 std::wstring GetString(int message_id) {
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
   ResourceBundle &rb = ResourceBundle::GetSharedInstance();
   return rb.GetLocalizedString(message_id);
 #else
@@ -342,7 +344,7 @@ static std::wstring GetStringF(int message_id,
                                const std::wstring& c,
                                const std::wstring& d,
                                std::vector<size_t>* offsets) {
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
 // TODO(port): re-enable.
   const std::wstring& format_string = GetString(message_id);
   std::wstring formatted = ReplaceStringPlaceholders(format_string, a, b, c,
