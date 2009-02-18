69dab3dc77a54d495ef54f0a0a2837dd7859fba7
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-18 21:59:07 +0000

    Include what you use in webcursor_mac.mm. Review URL: http://codereview.chromium.org/20468
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9977 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webcursor_mac.mm
+++ webkit/glue/webcursor_mac.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/glue/webcursor.h"
 
+#import <AppKit/AppKit.h>
+
 #include "config.h"
 #include "PlatformCursor.h"
 #include "RetainPtr.h"
