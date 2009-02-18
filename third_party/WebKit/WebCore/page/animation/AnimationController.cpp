a046a49f552d5795e1c2ac4ba436008ad4724592
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-18 19:20:08 +0000

    2009-02-18  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=24005
            Add an include to fix Chromium build.
    
            * page/animation/AnimationController.cpp: Add UnusedParam.h include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/AnimationController.cpp
+++ third_party/WebKit/WebCore/page/animation/AnimationController.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EventNames.h"
 #include "Frame.h"
 #include <wtf/CurrentTime.h>
+#include <wtf/UnusedParam.h>
 
 namespace WebCore {
 
