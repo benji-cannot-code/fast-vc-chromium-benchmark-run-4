6874833be67395a4e14467d80d6fbfb1841073a3
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-18 21:43:00 +0000

    Make interstitial_page.cc compile on Posix. Review URL: http://codereview.chromium.org/21467
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9971 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.h
+++ chrome/common/temp_scaffolding_stubs.h
@@ -468,20 +468,6 @@ class FaviconStatus {
   GURL url_;
 };
 
-class InterstitialPage {
- public:
-  InterstitialPage(WebContents* tab, bool new_navigation, const GURL& url) {
-    NOTIMPLEMENTED();
-  }
-  virtual void DontProceed() { NOTIMPLEMENTED(); }
-  virtual void Proceed() { NOTIMPLEMENTED(); }
-  virtual void Show() { NOTIMPLEMENTED(); }
-  WebContents* tab() const {
-    NOTIMPLEMENTED();
-    return NULL;
-  }
-};
-
 class TabContents : public PageNavigator, public NotificationObserver {
  public:
   enum InvalidateTypes {
