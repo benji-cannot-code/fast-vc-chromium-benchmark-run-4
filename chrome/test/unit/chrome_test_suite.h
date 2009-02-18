a07822df2757a5d53f12631f33e2383713f2c3be
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-18 22:34:36 +0000

    Try to land strings on linux again (r9972).  This time, enable the resource bundle on linux unittests and make the unit_tests executable require the various .pak files.
    Review URL: http://codereview.chromium.org/20471
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9984 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/unit/chrome_test_suite.h
+++ chrome/test/unit/chrome_test_suite.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
 // TODO(port): Remove the #ifdef when ResourceBundle is ported.
 #include "chrome/common/resource_bundle.h"
 #endif
@@ -50,7 +50,7 @@ protected:
     if (!user_data_dir.empty())
       PathService::Override(chrome::DIR_USER_DATA, user_data_dir);
 
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
     // TODO(port): Remove the #ifdef when ResourceBundle is ported.
     //
     // Force unittests to run using en-us so if we test against string
@@ -68,7 +68,7 @@ protected:
   }
 
   virtual void Shutdown() {
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
     // TODO(port): Remove the #ifdef when ResourceBundle is ported.
     ResourceBundle::CleanupSharedInstance();
 #endif
