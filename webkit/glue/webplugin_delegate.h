a115a9796691c17ec43413d07947976aab33751e
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-19 02:49:36 +0000

    Add #include <string>
    Review URL: http://codereview.chromium.org/21498
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10002 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_delegate.h
+++ webkit/glue/webplugin_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_GLUE_WEBPLUGIN_DELEGATE_H_
 #define WEBKIT_GLUE_WEBPLUGIN_DELEGATE_H_
 
+#include <string>
 #include <vector>
 
 #include "base/gfx/native_widget_types.h"
