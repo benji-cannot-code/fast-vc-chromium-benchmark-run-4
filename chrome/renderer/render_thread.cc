29dc463566c4f11dfb7fe4e634b365b7fa0c5e1c
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-19 21:54:02 +0000

    Remove some NOTIMPLMENTEDs which are apparently no longer necessary.
    Review URL: http://codereview.chromium.org/21520
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10036 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_thread.cc
+++ chrome/renderer/render_thread.cc
@@ -244,7 +244,7 @@ void RenderThread::OnCreateNewView(gfx::NativeViewId parent_hwnd,
 void RenderThread::OnSetCacheCapacities(size_t min_dead_capacity,
                                         size_t max_dead_capacity,
                                         size_t capacity) {
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
   CacheManager::SetCapacities(min_dead_capacity, max_dead_capacity, capacity);
 #else
   // TODO(port)
@@ -253,7 +253,7 @@ void RenderThread::OnSetCacheCapacities(size_t min_dead_capacity,
 }
 
 void RenderThread::OnGetCacheResourceStats() {
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
   CacheManager::ResourceTypeStats stats;
   CacheManager::GetResourceTypeStats(&stats);
   Send(new ViewHostMsg_ResourceTypeStats(stats));
@@ -264,7 +264,7 @@ void RenderThread::OnGetCacheResourceStats() {
 }
 
 void RenderThread::InformHostOfCacheStats() {
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
   CacheManager::UsageStats stats;
   CacheManager::GetUsageStats(&stats);
   Send(new ViewHostMsg_UpdatedCacheStats(stats));
