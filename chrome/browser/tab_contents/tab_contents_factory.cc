0a707d0975686711393094573a7cd81e989131d0
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-19 21:56:31 +0000

    Port TabContentsFactory to posix.
    Enable back_forward_menu_model_unittest on linux.
    Review URL: http://codereview.chromium.org/21522
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10041 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents_factory.cc
+++ chrome/browser/tab_contents/tab_contents_factory.cc
@@ -7,17 +7,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_about_handler.h"
 #include "chrome/browser/browser_url_handler.h"
 #include "chrome/browser/dom_ui/dom_ui_contents.h"
-#include "chrome/browser/dom_ui/html_dialog_contents.h"
 #include "chrome/browser/dom_ui/new_tab_ui.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
 #include "chrome/browser/debugger/debugger_contents.h"
-#include "chrome/browser/tab_contents/native_ui_contents.h"
-#include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/tab_contents/tab_contents_factory.h"
 #include "chrome/browser/tab_contents/view_source_contents.h"
 #include "chrome/browser/tab_contents/web_contents.h"
 #include "net/base/net_util.h"
+
+#if defined(OS_WIN)
+// TODO(port): port these headers to posix.
+#include "chrome/browser/dom_ui/html_dialog_contents.h"
+#include "chrome/browser/tab_contents/native_ui_contents.h"
+#include "chrome/browser/tab_contents/tab_contents.h"
+#elif defined(OS_POSIX)
+#include "chrome/common/temp_scaffolding_stubs.h"
+#endif
 
 typedef std::map<TabContentsType, TabContentsFactory*> TabContentsFactoryMap;
 static TabContentsFactoryMap* g_extra_types;  // Only allocated if needed.
@@ -34,16 +40,19 @@ TabContentsType TabContentsFactory::NextUnusedType() {
   return static_cast<TabContentsType>(type + 1);
 }
 
-/*static*/
+// static
 TabContents* TabContents::CreateWithType(TabContentsType type,
                                          Profile* profile,
                                          SiteInstance* instance) {
-  TabContents* contents;
+  TabContents* contents = NULL;
 
   switch (type) {
     case TAB_CONTENTS_WEB:
       contents = new WebContents(profile, instance, NULL, MSG_ROUTING_NONE, NULL);
       break;
+// TODO(port): remove this platform define, either by porting the tab contents
+// types or removing them completely.
+#if defined(OS_WIN)
     case TAB_CONTENTS_NEW_TAB_UI:
       contents = new NewTabUIContents(profile, instance, NULL);
       break;
@@ -65,6 +74,7 @@ TabContents* TabContents::CreateWithType(TabContentsType type,
     case TAB_CONTENTS_DOM_UI:
       contents = new DOMUIContents(profile, instance, NULL);
       break;
+#endif  // defined(OS_WIN)
     default:
       if (g_extra_types) {
         TabContentsFactoryMap::const_iterator it = g_extra_types->find(type);
@@ -83,7 +93,7 @@ TabContents* TabContents::CreateWithType(TabContentsType type,
   return contents;
 }
 
-/*static*/
+// static
 TabContentsType TabContents::TypeForURL(GURL* url) {
   DCHECK(url);
   if (g_extra_types) {
@@ -94,6 +104,8 @@ TabContentsType TabContents::TypeForURL(GURL* url) {
     }
   }
 
+// TODO(port): port the rest of this function.
+#if defined(OS_WIN)
   // Try to handle as a browser URL. If successful, |url| will end up
   // containing the real url being loaded (browser url's are just an alias).
   TabContentsType type(TAB_CONTENTS_UNKNOWN_TYPE);
@@ -117,12 +129,15 @@ TabContentsType TabContents::TypeForURL(GURL* url) {
     *url = GURL(url->path());
     return TAB_CONTENTS_VIEW_SOURCE;
   }
+#elif defined(OS_POSIX)
+  NOTIMPLEMENTED();
+#endif
 
   // NOTE: Even the empty string can be loaded by a WebContents.
   return TAB_CONTENTS_WEB;
 }
 
-/*static*/
+// static
 TabContentsFactory* TabContents::RegisterFactory(TabContentsType type,
                                                  TabContentsFactory* factory) {
   if (!g_extra_types)
