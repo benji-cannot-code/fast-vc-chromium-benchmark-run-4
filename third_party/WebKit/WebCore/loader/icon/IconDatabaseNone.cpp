2f26c92c67e9330388759d01984a3997a8c94c4f
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-20 19:27:13 +0000

    Make IconDatabaseNone.cpp compile with -Wunused and pass check-for-exit-time-destructors
            Reviewed by Sam Weinig.
    
            * loader/icon/IconDatabaseNone.cpp:
            (WebCore::IconDatabase::defaultDatabaseFilename): Use DEFINE_STATIC_LOCAL().
            (WebCore::IconDatabase::open): Commented out unused parameter.
            (WebCore::IconDatabase::setPrivateBrowsingEnabled): Ditto.
            (WebCore::IconDatabase::iconForPageURL): Ditto.
            (WebCore::IconDatabase::iconURLForPageURL): Ditto.
            (WebCore::IconDatabase::defaultIcon): Ditto.
            (WebCore::IconDatabase::retainIconForPageURL): Ditto.
            (WebCore::IconDatabase::releaseIconForPageURL): Ditto.
            (WebCore::IconDatabase::setIconDataForIconURL): Ditto.
            (WebCore::IconDatabase::setIconURLForPageURL): Ditto.
            (WebCore::IconDatabase::setEnabled): Ditto.
            (WebCore::IconDatabase::pageURLMappingCount): Added stub.
            (WebCore::IconDatabase::retainedPageURLCount): Ditto.
            (WebCore::IconDatabase::iconRecordCount): Ditto.
            (WebCore::IconDatabase::iconRecordCountWithData): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/icon/IconDatabaseNone.cpp
+++ third_party/WebKit/WebCore/loader/icon/IconDatabaseNone.cpp
@@ -26,7 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "IconDatabase.h"
+
+#include "PlatformString.h"
 #include "SharedBuffer.h"
+#include <wtf/StdLibExtras.h>
 
 namespace WebCore {
 
@@ -48,8 +51,8 @@ const int updateTimerDelay = 5;
 
 String IconDatabase::defaultDatabaseFilename()
 {
-    static String defaultDatabaseFilename = "Icons.db";
-    return defaultDatabaseFilename;
+    DEFINE_STATIC_LOCAL(String, defaultDatabaseFilename, ("Icons.db"));
+    return defaultDatabaseFilename.copy();
 }
 
 IconDatabase* iconDatabase()
@@ -63,7 +66,7 @@ IconDatabase::IconDatabase()
 {
 }
 
-bool IconDatabase::open(const String& databasePath)
+bool IconDatabase::open(const String& /*databasePath*/)
 {
     return false;
 }
@@ -86,7 +89,7 @@ void IconDatabase::removeAllIcons()
 {
 }
 
-void IconDatabase::setPrivateBrowsingEnabled(bool flag)
+void IconDatabase::setPrivateBrowsingEnabled(bool /*flag*/)
 {
 }
 
@@ -100,7 +103,7 @@ void IconDatabase::readIconForPageURLFromDisk(const String&)
 
 }
 
-Image* IconDatabase::iconForPageURL(const String& pageURL, const IntSize& size)
+Image* IconDatabase::iconForPageURL(const String& /*pageURL*/, const IntSize& size)
 {
     return defaultIcon(size);
 }
@@ -116,33 +119,33 @@ bool IconDatabase::iconDataKnownForIconURL(const String&)
     return false;
 }
 
-String IconDatabase::iconURLForPageURL(const String& pageURL)
+String IconDatabase::iconURLForPageURL(const String& /*pageURL*/)
 {
     return String();
 }
 
-Image* IconDatabase::defaultIcon(const IntSize& size)
+Image* IconDatabase::defaultIcon(const IntSize& /*size*/)
 {
     return 0;
 }
 
-void IconDatabase::retainIconForPageURL(const String& pageURL)
+void IconDatabase::retainIconForPageURL(const String& /*pageURL*/)
 {
 }
 
-void IconDatabase::releaseIconForPageURL(const String& pageURL)
+void IconDatabase::releaseIconForPageURL(const String& /*pageURL*/)
 {
 }
 
-void IconDatabase::setIconDataForIconURL(PassRefPtr<SharedBuffer> data, const String& iconURL)
+void IconDatabase::setIconDataForIconURL(PassRefPtr<SharedBuffer> /*data*/, const String& /*iconURL*/)
 {
 }
 
-void IconDatabase::setIconURLForPageURL(const String& iconURL, const String& pageURL)
+void IconDatabase::setIconURLForPageURL(const String& /*iconURL*/, const String& /*pageURL*/)
 {
 }
 
-void IconDatabase::setEnabled(bool enabled)
+void IconDatabase::setEnabled(bool /*enabled*/)
 {
 }
 
@@ -166,6 +169,26 @@ void IconDatabase::delayDatabaseCleanup()
 
 void IconDatabase::allowDatabaseCleanup()
 {
+}
+
+size_t IconDatabase::pageURLMappingCount()
+{
+    return 0;
+}
+
+size_t IconDatabase::retainedPageURLCount()
+{
+    return 0;
+}
+
+size_t IconDatabase::iconRecordCount()
+{
+    return 0;
+}
+
+size_t IconDatabase::iconRecordCountWithData()
+{
+    return 0;
 }
 
 void IconDatabase::setClient(IconDatabaseClient*)
