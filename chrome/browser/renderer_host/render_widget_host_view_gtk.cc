d4657057750d18e5415a0f794688145a5fc0403d
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-20 17:43:03 +0000

    Implement RenderWidgetHostViewGtk::Destroy.
    This follows the same style as on Windows, destroy the native window in Destroy(), not in the destructor.
    
    Review URL: http://codereview.chromium.org/20538
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10104 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_gtk.cc
+++ chrome/browser/renderer_host/render_widget_host_view_gtk.cc
@@ -156,7 +156,6 @@ RenderWidgetHostViewGtk::RenderWidgetHostViewGtk(RenderWidgetHost* widget_host)
 }
 
 RenderWidgetHostViewGtk::~RenderWidgetHostViewGtk() {
-  gtk_widget_destroy(view_);
 }
 
 void RenderWidgetHostViewGtk::DidBecomeSelected() {
@@ -268,7 +267,7 @@ void RenderWidgetHostViewGtk::RendererGone() {
 }
 
 void RenderWidgetHostViewGtk::Destroy() {
-  NOTIMPLEMENTED();
+  gtk_widget_destroy(view_);
 }
 
 void RenderWidgetHostViewGtk::SetTooltipText(const std::wstring& tooltip_text) {
