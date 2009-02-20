edebbe931b43d03c1ddfa839a2f34856600ee0fb
jnd@chromium.org <jnd@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-20 05:17:28 +0000

    I forgot to add #include <functional> to l10n_util.h for using binary_function
    BUG=7647
    
    Review URL: http://codereview.chromium.org/21533
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10079 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/l10n_util.h
+++ chrome/common/l10n_util.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #endif
 #include <algorithm>
+#include <functional>
 #include <string>
 #include <vector>
 
