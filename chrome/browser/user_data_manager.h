a582fda4133279afe5fc38f7e140eaada6513206
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-20 03:33:11 +0000

    Remove a forward declaration that is not being used. Review URL: http://codereview.chromium.org/20528
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10075 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/user_data_manager.h
+++ chrome/browser/user_data_manager.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/ref_counted.h"
 
-class CommandLine;
 class MessageLoop;
 
 // Provides an abstraction of profiles on top of the user data directory
