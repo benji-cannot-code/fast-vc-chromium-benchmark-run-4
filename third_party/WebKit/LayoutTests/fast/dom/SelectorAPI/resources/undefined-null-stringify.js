346eb74a73f7b60517635d99846289bfe905499b
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-20 00:38:17 +0000

    WebCore:
    2009-02-19  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Beth Dakin.
    
            Patch for https://bugs.webkit.org/show_bug.cgi?id=24044
            Update querySelector/querySelectorAll to match the latest spec
    
            Update querySelector and querySelectorAll to match the latest version
            of the Selectors API spec.  We now stringify null and undefined to "null"
            and "undefined" respectively instead of to "".
    
            Test: fast/dom/SelectorAPI/undefined-null-stringify.html
    
            * dom/Document.idl:
            * dom/DocumentFragment.idl:
            * dom/Element.idl:
    
    LayoutTests:
    
    2009-02-19  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Beth Dakin.
    
            Tests for https://bugs.webkit.org/show_bug.cgi?id=24044
            Update querySelector/querySelectorAll to match the latest spec
    
            * fast/dom/SelectorAPI/dumpNodeList-almost-strict-expected.txt:
            * fast/dom/SelectorAPI/dumpNodeList-expected.txt:
            * fast/dom/SelectorAPI/resig-SelectorsAPI-test-expected.txt:
            * fast/dom/SelectorAPI/resig-SelectorsAPI-test.xhtml: Updated.  We now fail 17 tests
            due to not throwing an exception when no arguments are passed to querySelector
            or querySelectorAll.
            * fast/dom/SelectorAPI/resources/undefined-null-stringify.js: Added.
            * fast/dom/SelectorAPI/undefined-null-stringify-expected.txt: Added.
            * fast/dom/SelectorAPI/undefined-null-stringify.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41093 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/SelectorAPI/resources/undefined-null-stringify.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This tests that the querySelector and querySelectorAll correctly stringify null and undefined to \"null\" and \"undefined\"."
+);
+
+var root = document.createElement('div');
+var nullNode = document.createElement('null');
+root.appendChild(nullNode);
+var undefinedNode = document.createElement('undefined');
+root.appendChild(undefinedNode);
+document.body.appendChild(root);
+
+shouldBe("document.querySelector(null)", "nullNode");
+shouldBe("document.querySelector(undefined)", "undefinedNode");
+
+shouldBe("document.querySelectorAll(null).length", "1");
+shouldBe("document.querySelectorAll(null).item(0)", "nullNode");
+shouldBe("document.querySelectorAll(undefined).length", "1");
+shouldBe("document.querySelectorAll(undefined).item(0)", "undefinedNode");
+
+var successfullyParsed = true;
