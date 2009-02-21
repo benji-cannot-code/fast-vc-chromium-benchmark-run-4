a2c5e07bdc56c429bc90e3c637c87faebaba7137
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-21 21:06:31 +0000

    Mac rendering fix
    My change r10071 broke rendering on the Mac. Thanks to eseidel for
    letting me use his Mac Pro to debug it.
    
    Sometimes I still manage to stun myself with my own stupidity :(
    
    TBR=pinkerton
    
    Review URL: http://codereview.chromium.org/28018
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10152 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/transport_dib_mac.cc
+++ chrome/common/transport_dib_mac.cc
@@ -56,7 +56,7 @@ void* TransportDIB::memory() const {
 }
 
 TransportDIB::Id TransportDIB::id() const {
-  return false;
+  return shared_memory_.id();
 }
 
 TransportDIB::Handle TransportDIB::handle() const {
