58f8fcd5aee08af4e808fccca882b1681d98e028
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-23 23:53:17 +0000

    Make plugin_installer.cc compile on Posix. Review URL: http://codereview.chromium.org/31001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10222 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.h
+++ chrome/common/temp_scaffolding_stubs.h
@@ -80,7 +80,6 @@ class SessionCommand;
 class SessionID;
 class SiteInstance;
 class SpellChecker;
-class TabContents;
 class TabContentsDelegate;
 class TabContentsFactory;
 class TabNavigation;
@@ -423,6 +422,7 @@ class TabContents : public PageNavigator, public NotificationObserver {
   static TabContentsFactory* RegisterFactory(TabContentsType type,
                                              TabContentsFactory* factory);
   void OnStartDownload(DownloadItem* download) { NOTIMPLEMENTED(); }
+  void RemoveInfoBar(InfoBarDelegate* delegate) { NOTIMPLEMENTED(); }
  protected:
   typedef std::vector<ConstrainedWindow*> ConstrainedWindowList;
   ConstrainedWindowList child_windows_;
@@ -578,11 +578,6 @@ class PrintViewManager {
   }
 };
 }
-
-class PluginInstaller {
- public:
-  PluginInstaller(WebContents*) { }
-};
 
 class HungRendererWarning {
  public:
