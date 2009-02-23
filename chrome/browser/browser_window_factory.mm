e05ee17b4a8650fb0d313aa24ae5ca9fe248eabf
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-23 21:54:26 +0000

    Update header ref to new location.
    Review URL: http://codereview.chromium.org/27063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10215 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_window_factory.mm
+++ chrome/browser/browser_window_factory.mm
@@ -4,17 +4,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/browser_window.h"
-#include "chrome/browser/browser_window_controller.h"
+#include "chrome/browser/cocoa/browser_window_controller.h"
 
-// Create the controller for the Browser, which handles loading the browser 
+// Create the controller for the Browser, which handles loading the browser
 // window from the nib. The controller takes ownership of |browser|.
 // static
 BrowserWindow* BrowserWindow::CreateBrowserWindow(Browser* browser) {
   // TODO(pinkerton): figure out ownership model. If BrowserList keeps track
   // of the browser windows, it will probably tell us when it needs to go
-  // away, and it seems we need to feed back to that when we get a 
+  // away, and it seems we need to feed back to that when we get a
   // performClose: from the UI.
-  BrowserWindowController* controller = 
+  BrowserWindowController* controller =
       [[BrowserWindowController alloc] initWithBrowser:browser];
   return [controller browserWindow];
 }
