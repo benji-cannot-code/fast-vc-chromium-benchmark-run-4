616247dcd004dfcce784a95909a6481ecaa64943
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-23 23:54:02 +0000

    Obey the XDG base directory spec and use ~/.config/chromium on Linux.
    As discussed here:
    http://groups.google.com/group/chromium-dev/browse_thread/thread/41e9154311779e1a
    
    Review URL: http://codereview.chromium.org/27069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/process_singleton.h
+++ chrome/browser/process_singleton.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Implementation notes:
 // - the Windows implementation uses an invisible global message window;
-// - the Linux implementation uses a Unix domain socket in ~/.chromium.
+// - the Linux implementation uses a Unix domain socket in the user data dir.
 
 class ProcessSingleton {
  public:
