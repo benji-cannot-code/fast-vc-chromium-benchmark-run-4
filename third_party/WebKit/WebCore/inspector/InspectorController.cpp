6abaa14072813035169249ad385459afd0c3c840
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-23 23:20:33 +0000

    2009-02-23  Adam Treat  <adam.treat@torchmobile.com>
            Reviewed by Timothy Hatcher.
    
            https://bugs.webkit.org/show_bug.cgi?id=24106
            The Qt port is crashing on exit because the tear down procedure involves
            the WebCore::InspectorController trying to access the JS execution context
            for a page that is being deleted.  This patch amends the inspector so
            that it does not try and access the execution context of the WebCore::Page
            in the midst of deletion.
    
            * inspector/InspectorController.cpp:
            (WebCore::InspectorController::inspectedPageDestroyed):
            (WebCore::InspectorController::stopUserInitiatedProfiling):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41158 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorController.cpp
+++ third_party/WebKit/WebCore/inspector/InspectorController.cpp
@@ -1221,10 +1221,10 @@ InspectorController::~InspectorController()
 
 void InspectorController::inspectedPageDestroyed()
 {
-    close();
-
     ASSERT(m_inspectedPage);
     m_inspectedPage = 0;
+
+    close();
 }
 
 bool InspectorController::enabled() const
@@ -1826,10 +1826,12 @@ void InspectorController::stopUserInitiatedProfiling()
     title += ".";
     title += UString::from(m_currentUserInitiatedProfileNumber);
 
-    ExecState* exec = toJSDOMWindow(m_inspectedPage->mainFrame())->globalExec();
-    RefPtr<Profile> profile = Profiler::profiler()->stopProfiling(exec, title);
-    if (profile)
-        addProfile(profile, 0, UString());
+    if (m_inspectedPage) {
+        ExecState* exec = toJSDOMWindow(m_inspectedPage->mainFrame())->globalExec();
+        RefPtr<Profile> profile = Profiler::profiler()->stopProfiling(exec, title);
+        if (profile)
+            addProfile(profile, 0, UString());
+    }
 
     toggleRecordButton(false);
 }
