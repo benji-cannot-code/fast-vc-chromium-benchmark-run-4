4e7b60d2d854ce1f0e95ed1f59002703e276172c
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-24 19:56:16 +0000

    Get shared link working again.
    1) The spell checker works, but it was still in temp_scaffolding_stubs.
    This meant different files had a different view of the class, causing
    memory corruption on startup.  (Valgrind caught this.  We need a Valgrind
    builder!)
    2) The net library provides gtest-related bits and must depend on gtest.
    
    Review URL: http://codereview.chromium.org/27036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10280 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.cc
+++ chrome/common/temp_scaffolding_stubs.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/cache_manager_host.h"
 #include "chrome/browser/debugger/debugger_shell.h"
+#include "chrome/browser/dom_ui/dom_ui.h"
 #include "chrome/browser/download/download_request_dialog_delegate.h"
 #include "chrome/browser/download/download_request_manager.h"
 #include "chrome/browser/first_run.h"
@@ -468,3 +469,6 @@ DownloadRequestDialogDelegate* DownloadRequestDialogDelegate::Create(
   return NULL;
 }
 
+DOMMessageHandler::~DOMMessageHandler() {
+  NOTIMPLEMENTED();
+}
