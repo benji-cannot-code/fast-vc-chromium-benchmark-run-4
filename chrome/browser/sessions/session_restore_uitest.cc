69da5e32e3af7a5c189b16c8e55ee842239f5cb7
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-24 17:22:43 +0000

    Attempt at fixing flakeyness when running session restore ui tests in purify. I believe the ui test isn't waiting for the session restore to complete, so that when the ui test does various asserts the state of the browser is wrong.
    BUG=6495
    TEST=covered by tests
    
    Review URL: http://codereview.chromium.org/28051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10268 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_restore_uitest.cc
+++ chrome/browser/sessions/session_restore_uitest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/scoped_ptr.h"
+#include "base/string_util.h"
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
@@ -34,12 +35,13 @@ class SessionRestoreUITest : public UITest {
         path_prefix.AppendASCII("bot3.html"));
   }
 
-  virtual void QuitBrowserAndRestore() {
+  virtual void QuitBrowserAndRestore(int expected_tab_count) {
     UITest::TearDown();
 
     clear_profile_ = false;
 
-    launch_arguments_.AppendSwitch(switches::kRestoreLastSession);
+    launch_arguments_.AppendSwitchWithValue(switches::kRestoreLastSession,
+                                            IntToWString(expected_tab_count));
     UITest::SetUp();
   }
 
@@ -97,7 +99,7 @@ TEST_F(SessionRestoreUITest, Basic) {
   NavigateToURL(url1);
   NavigateToURL(url2);
 
-  QuitBrowserAndRestore();
+  QuitBrowserAndRestore(1);
 
   // NOTE: Don't use GetActiveWindow here, when run with the screen locked
   // active windows returns NULL.
@@ -121,7 +123,7 @@ TEST_F(SessionRestoreUITest, RestoresForwardAndBackwardNavs) {
   scoped_ptr<TabProxy> active_tab(GetActiveTab());
   ASSERT_TRUE(active_tab->GoBack());
 
-  QuitBrowserAndRestore();
+  QuitBrowserAndRestore(1);
 
   // NOTE: Don't use GetActiveWindow here, when run with the screen locked
   // active windows returns NULL.
@@ -159,7 +161,7 @@ TEST_F(SessionRestoreUITest, RestoresCrossSiteForwardAndBackwardNavs) {
   scoped_ptr<TabProxy> active_tab(GetActiveTab());
   ASSERT_TRUE(active_tab->GoBack());
 
-  QuitBrowserAndRestore();
+  QuitBrowserAndRestore(1);
 
   // NOTE: Don't use GetActiveWindow here, when run with the screen locked
   // active windows returns NULL.
@@ -202,7 +204,7 @@ TEST_F(SessionRestoreUITest, TwoTabsSecondSelected) {
 
   ASSERT_TRUE(browser_proxy->AppendTab(url2));
 
-  QuitBrowserAndRestore();
+  QuitBrowserAndRestore(2);
   browser_proxy.reset();
 
   ASSERT_TRUE(automation()->GetBrowserWindowCount(&window_count) &&
@@ -247,7 +249,7 @@ TEST_F(SessionRestoreUITest, ClosedTabStaysClosed) {
   scoped_ptr<TabProxy> active_tab(browser_proxy->GetActiveTab());
   active_tab->Close(true);
 
-  QuitBrowserAndRestore();
+  QuitBrowserAndRestore(1);
   browser_proxy.reset();
   tab_proxy.reset();
 
@@ -329,7 +331,7 @@ TEST_F(SessionRestoreUITest, DISABLED_TwoWindowsCloseOneRestoreOnlyOne) {
 
   // Restart and make sure we have only one window with one tab and the url
   // is url1.
-  QuitBrowserAndRestore();
+  QuitBrowserAndRestore(1);
 
   AssertOneWindowWithOneTab();
 
@@ -413,7 +415,7 @@ TEST_F(SessionRestoreUITest, DISABLED_ShareProcessesOnRestore) {
   // Restart.
   browser_proxy.reset();
   last_tab.reset();
-  QuitBrowserAndRestore();
+  QuitBrowserAndRestore(3);
 
   // Wait for each tab to finish being restored, then make sure the process
   // count matches.
