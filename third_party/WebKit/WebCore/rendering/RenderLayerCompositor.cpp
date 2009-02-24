555481dcd433f4ca8bdbd052726373d5d8722c0a
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-24 22:57:56 +0000

    2009-02-24  Simon Fraser  <simon.fraser@apple.com>
            Fix build when ACCELERATED_COMPOSITING is turned on
            (overflowList() -> normalFlowList()).
    
            * rendering/RenderLayerBacking.cpp:
            (WebCore::RenderLayerBacking::hasNonCompositingContent):
            (WebCore::RenderLayerBacking::paintIntoLayer):
            * rendering/RenderLayerCompositor.cpp:
            (WebCore::RenderLayerCompositor::calculateCompositedBounds):
            (WebCore::RenderLayerCompositor::computeCompositingRequirements):
            (WebCore::RenderLayerCompositor::rebuildCompositingLayerTree):
            (WebCore::RenderLayerCompositor::recursiveRepaintLayerRect):
            (WebCore::RenderLayerCompositor::layerHas3DContent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41192 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayerCompositor.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayerCompositor.cpp
@@ -234,9 +234,9 @@ IntRect RenderLayerCompositor::calculateCompositedBounds(const RenderLayer* laye
         }
     }
 
-    Vector<RenderLayer*>* overflowList = layer->overflowList();
-    if (overflowList) {
-        for (Vector<RenderLayer*>::iterator it = overflowList->begin(); it != overflowList->end(); ++it) {
+    Vector<RenderLayer*>* normalFlowList = layer->normalFlowList();
+    if (normalFlowList) {
+        for (Vector<RenderLayer*>::iterator it = normalFlowList->begin(); it != normalFlowList->end(); ++it) {
             RenderLayer* curLayer = (*it);
             if (!curLayer->isComposited()) {
                 IntRect curAbsBounds = calculateCompositedBounds(curLayer, layer);
@@ -315,7 +315,7 @@ void RenderLayerCompositor::computeCompositingRequirements(RenderLayer* layer, s
 {
     layer->updateLayerPosition();
     layer->updateZOrderLists();
-    layer->updateOverflowList();
+    layer->updateNormalFlowList();
     
     // Clear the flag
     layer->setHasCompositingDescendant(false);
@@ -356,10 +356,10 @@ void RenderLayerCompositor::computeCompositingRequirements(RenderLayer* layer, s
         }
     }
     
-    ASSERT(!layer->m_overflowListDirty);
-    Vector<RenderLayer*>* overflowList = layer->overflowList();
-    if (overflowList && overflowList->size() > 0) {
-        for (Vector<RenderLayer*>::const_iterator it = overflowList->begin(); it != overflowList->end(); ++it) {
+    ASSERT(!layer->m_normalFlowListDirty);
+    Vector<RenderLayer*>* normalFlowList = layer->normalFlowList();
+    if (normalFlowList && normalFlowList->size() > 0) {
+        for (Vector<RenderLayer*>::const_iterator it = normalFlowList->begin(); it != normalFlowList->end(); ++it) {
             RenderLayer* curLayer = (*it);
             computeCompositingRequirements(curLayer, childState);
         }
@@ -494,10 +494,10 @@ void RenderLayerCompositor::rebuildCompositingLayerTree(RenderLayer* layer, stru
         }
     }
 
-    ASSERT(!layer->m_overflowListDirty);
-    Vector<RenderLayer*>* overflowList = layer->overflowList();
-    if (overflowList && overflowList->size() > 0) {
-        for (Vector<RenderLayer*>::iterator it = overflowList->begin(); it != overflowList->end(); ++it) {
+    ASSERT(!layer->m_normalFlowListDirty);
+    Vector<RenderLayer*>* normalFlowList = layer->normalFlowList();
+    if (normalFlowList && normalFlowList->size() > 0) {
+        for (Vector<RenderLayer*>::iterator it = normalFlowList->begin(); it != normalFlowList->end(); ++it) {
             RenderLayer* curLayer = (*it);
             rebuildCompositingLayerTree(curLayer, childState);
             if (curLayer->isComposited())
@@ -553,9 +553,9 @@ void RenderLayerCompositor::recursiveRepaintLayerRect(RenderLayer* layer, const 
             }
         }
         
-        Vector<RenderLayer*>* overflowList = layer->overflowList();
-        if (overflowList) {
-            for (Vector<RenderLayer*>::iterator it = overflowList->begin(); it != overflowList->end(); ++it) {
+        Vector<RenderLayer*>* normalFlowList = layer->normalFlowList();
+        if (normalFlowList) {
+            for (Vector<RenderLayer*>::iterator it = normalFlowList->begin(); it != normalFlowList->end(); ++it) {
                 RenderLayer* curLayer = (*it);
                 int x = 0, y = 0;
                 curLayer->convertToLayerCoords(layer, x, y);
@@ -780,9 +780,9 @@ bool RenderLayerCompositor::layerHas3DContent(const RenderLayer* layer) const
         }
     }
 
-    Vector<RenderLayer*>* overflowList = layer->overflowList();
-    if (overflowList) {
-        for (Vector<RenderLayer*>::iterator it = overflowList->begin(); it != overflowList->end(); ++it) {
+    Vector<RenderLayer*>* normalFlowList = layer->normalFlowList();
+    if (normalFlowList) {
+        for (Vector<RenderLayer*>::iterator it = normalFlowList->begin(); it != normalFlowList->end(); ++it) {
             RenderLayer* curLayer = (*it);
             if (layerHas3DContent(curLayer))
                 return true;
