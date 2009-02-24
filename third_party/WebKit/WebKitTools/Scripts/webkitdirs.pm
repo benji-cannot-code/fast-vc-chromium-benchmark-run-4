9790521824bd69c5d328d708238c4c790200f632
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-24 16:23:47 +0000

    2009-02-24  Adam Treat  <adam.treat@torchmobile.com>
            Reviewed by Cameron Zwarich.
    
            The Qt port does not support these yet nor does the nm check work with
            QMake based build.
    
            * Scripts/webkitdirs.pm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -469,8 +469,8 @@ sub checkWebCoreSVGSupport
 
 sub hasAcceleratedCompositingSupport
 {
-    return 0 if isCygwin();
-    
+    return 0 if isCygwin() || isQt();
+
     my $path = shift;
 
     my $useAcceleratedCompositing = 0;
@@ -498,8 +498,8 @@ sub checkWebCoreAcceleratedCompositingSupport
 
 sub has3DTransformsSupport
 {
-    return 0 if isCygwin();
-    
+    return 0 if isCygwin() || isQt();
+
     my $path = shift;
 
     my $has3DTransformsSupport = 0;
