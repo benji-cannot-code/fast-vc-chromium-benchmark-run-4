5c05e5e8b9a1b4e4f9ca850f102421afa5bbfa94
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-25 19:50:12 +0000

    Partial Windows build fix.
            * DerivedSources.cpp:
            * WebCore.vcproj/WebCore.vcproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/DerivedSources.cpp
+++ third_party/WebKit/WebCore/DerivedSources.cpp
@@ -319,6 +319,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSWebKitCSSKeyframesRule.cpp"
 #include "JSWebKitCSSTransformValue.cpp"
 #include "JSWebKitCSSMatrix.cpp"
+#include "JSWebKitPoint.cpp"
 #include "JSWebKitTransitionEvent.cpp"
 #include "JSWheelEvent.cpp"
 #include "JSWorker.cpp"
