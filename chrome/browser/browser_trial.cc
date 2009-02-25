e51de8d4cd68f4d248f0bbd289cbfdf229620843
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-25 18:26:29 +0000

    Remove memory model experiment
    We've already removed the histograms (during cleanup of other files) so
    this just discards the remaining uses of the constants, along with the
    experiment establishment portion.
    
    r=mbelshe
    Review URL: http://codereview.chromium.org/27032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10352 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_trial.cc
+++ chrome/browser/browser_trial.cc
@@ -5,5 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_trial.h"
 
-// A test to determine the impact of using HIGH vs MEDIUM memory models.
-const wchar_t* BrowserTrial::kMemoryModelFieldTrial = L"memory_model";
+// The following is a sample of the lines that should be listed here.
+
+// // A test to determine the impact of using HIGH vs MEDIUM memory models.
+// const wchar_t* BrowserTrial::kMemoryModelFieldTrial = L"memory_model";
--- chrome/browser/browser_trial.h
+++ chrome/browser/browser_trial.h
@@ -15,7 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // define current and past trials.
 class BrowserTrial {
  public:
-  static const wchar_t* kMemoryModelFieldTrial;
+
+  // The following is a sample line for what should be listed in this file.
+  // static const wchar_t* kMemoryModelFieldTrial;
+
  private:
   DISALLOW_COPY_AND_ASSIGN(BrowserTrial);
 };
