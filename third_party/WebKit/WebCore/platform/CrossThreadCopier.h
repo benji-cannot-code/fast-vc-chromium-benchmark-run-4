288be181446dd3e6c0f9da52b3cae47f76fa16cd
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-25 18:06:47 +0000

    2009-02-25  Jian Li  <jianli@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            Remove "#if ENABLE(WORKERS)" wrap from CrossThreadCopier files.
            https://bugs.webkit.org/show_bug.cgi?id=24145
    
            * platform/CrossThreadCopier.cpp:
            * platform/CrossThreadCopier.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/CrossThreadCopier.h
+++ third_party/WebKit/WebCore/platform/CrossThreadCopier.h
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CrossThreadCopier_h
 #define CrossThreadCopier_h
 
-#if ENABLE(WORKERS)
-
 #include <memory>
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefPtr.h>
@@ -108,7 +106,5 @@ namespace WebCore {
     };
 
 } // namespace WebCore
-
-#endif // ENABLE(WORKERS)
 
 #endif // CrossThreadCopier_h
