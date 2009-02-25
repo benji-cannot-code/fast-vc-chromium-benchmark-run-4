e88e8bac258cca6cce237282c560abaad033fd74
jmalonzo@webkit.org <jmalonzo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-25 20:39:59 +0000

    2009-02-25  Jan Michael Alonzo  <jmalonzo@webkit.org>
            Gtk build fix. Not reviewed.
    
            Add files to the build per r41218.
            Rename WebkitPoint.h to WebKitPoint.h in DOMWindow.cpp
    
            * GNUmakefile.am:
            * page/DOMWindow.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/DOMWindow.cpp
+++ third_party/WebKit/WebCore/page/DOMWindow.cpp
@@ -58,7 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Screen.h"
 #include "SecurityOrigin.h"
 #include "Settings.h"
-#include "WebkitPoint.h"
+#include "WebKitPoint.h"
 #include <algorithm>
 #include <wtf/MathExtras.h>
 
