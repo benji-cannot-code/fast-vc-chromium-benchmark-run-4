d2b2ec20c3375ada02fdb841123d3e1bf938d529
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-26 20:10:38 +0000

    Attempting backout of r10495 due to  ui_tests: ResourceDispatcherTest.CrossSiteAfterCrash PageLoadTest.Reliability CrashRecoveryUITest.Reload CrashRecoveryUITest.LoadInNewTab Review URL: http://codereview.chromium.org/28208
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10505 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/site_instance_unittest.cc
+++ chrome/browser/site_instance_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/navigation_entry.h"
 #include "chrome/browser/tab_contents/web_contents.h"
 #include "chrome/common/render_messages.h"
-#include "chrome/common/url_constants.h"
 #include "chrome/test/testing_profile.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
