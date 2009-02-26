21b60fbf59c86402382191efbb75e1f34d85b2b3
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-26 02:30:12 +0000

    Linux: add splash screen
    In preparation for an internal launch, this adds a splash screen as
    the startup URL for Linux which has a few FAQs.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10442 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -62,6 +62,7 @@ static const char kStatsPath[] = "stats";
 static const char kVersionPath[] = "version";
 static const char kCreditsPath[] = "credits";
 static const char kTermsPath[] = "terms";
+static const char kLinuxSplash[] = "linux-splash";
 
 class AboutSource : public ChromeURLDataManager::DataSource {
  public:
@@ -142,6 +143,12 @@ void AboutSource::StartDataRequest(const std::string& path_raw,
   } else if (path == kTermsPath) {
     response = BrowserAboutHandler::AboutTerms();
   }
+#if defined(OS_LINUX)
+  else if (path == kLinuxSplash) {
+    response = BrowserAboutHandler::AboutLinuxSplash();
+  }
+#endif
+
   FinishDataRequest(response, request_id);
 }
 
@@ -302,6 +309,15 @@ std::string BrowserAboutHandler::AboutCredits() {
           IDR_CREDITS_HTML);
 
   return credits_html;
+}
+
+// static
+std::string BrowserAboutHandler::AboutLinuxSplash() {
+  static const std::string linux_splash_html =
+      ResourceBundle::GetSharedInstance().GetDataResource(
+          IDR_LINUX_SPLASH_HTML);
+
+  return linux_splash_html;
 }
 
 // static
