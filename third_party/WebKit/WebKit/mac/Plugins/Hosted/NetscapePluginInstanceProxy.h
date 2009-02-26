0d9e7b2f4484ae44496870d22bcc7a25171aa28c
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-26 20:07:39 +0000

    2009-02-25  Anders Carlsson  <andersca@apple.com>
            Reviewed by Kevin Decker.
    
            Fix <rdar://problem/6623697>.
            
            * Plugins/Hosted/NetscapePluginInstanceProxy.h:
            * Plugins/Hosted/NetscapePluginInstanceProxy.mm:
            (WebKit::NetscapePluginInstanceProxy::print):
            Ask the plug-in host to print, create a CGImage of the returned bytes and draw
            the image into the passed in context.
            
            * Plugins/Hosted/WebHostedNetscapePluginView.mm:
            (-[WebHostedNetscapePluginView drawRect:]):
            When printing, call NetscapePluginInstanceProxy::print.
            
            * Plugins/Hosted/WebKitPluginHost.defs:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/Hosted/NetscapePluginInstanceProxy.h
+++ third_party/WebKit/WebKit/mac/Plugins/Hosted/NetscapePluginInstanceProxy.h
@@ -95,6 +95,8 @@ public:
     void keyEvent(NSView *pluginView, NSEvent *, NPCocoaEventType);
     void insertText(NSString *);
     
+    void print(CGContextRef, unsigned width, unsigned height);
+    
     void startTimers(bool throttleTimers);
     void stopTimers();
     
