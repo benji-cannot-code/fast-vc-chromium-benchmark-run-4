95cd37891d023b6d42c15d23df8f8c6eb8386d6a
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-26 16:21:27 +0000

    2009-02-26  Xan Lopez  <xan@gnome.org>
            Reviewed by Holger Freyther.
    
            https://bugs.webkit.org/show_bug.cgi?id=17585
            [gtk] get|set encoding api
    
            Add functions to get and set a custom encoding an a view.
    
            * webkit/webkitprivate.h:
            * webkit/webkitwebview.cpp:
            (webkit_web_view_set_custom_encoding):
            (webkit_web_view_get_custom_encoding):
            * webkit/webkitwebview.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41247 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ContextMenuController.h"
 #include "Cursor.h"
 #include "Document.h"
+#include "DocumentLoader.h"
 #include "DragClientGtk.h"
 #include "Editor.h"
 #include "EditorClientGtk.h"
@@ -865,6 +866,16 @@ static void webkit_web_view_dispose(GObject* object)
     G_OBJECT_CLASS(webkit_web_view_parent_class)->dispose(object);
 }
 
+static void webkit_web_view_finalize(GObject* object)
+{
+    WebKitWebView* webView = WEBKIT_WEB_VIEW(object);
+    WebKitWebViewPrivate* priv = webView->priv;
+
+    g_free(priv->customEncoding);
+
+    G_OBJECT_CLASS(webkit_web_view_parent_class)->finalize(object);
+}
+
 static gboolean webkit_create_web_view_request_handled(GSignalInvocationHint* ihint, GValue* returnAccu, const GValue* handlerReturn, gpointer dummy)
 {
     gpointer newWebView = g_value_get_object(handlerReturn);
@@ -1397,6 +1408,7 @@ static void webkit_web_view_class_init(WebKitWebViewClass* webViewClass)
 
     GObjectClass* objectClass = G_OBJECT_CLASS(webViewClass);
     objectClass->dispose = webkit_web_view_dispose;
+    objectClass->finalize = webkit_web_view_finalize;
     objectClass->get_property = webkit_web_view_get_property;
     objectClass->set_property = webkit_web_view_set_property;
 
@@ -2757,4 +2769,47 @@ SoupSession* webkit_get_default_session ()
     return ResourceHandle::defaultSession();
 }
 
+}
+
+/**
+ * webkit_web_view_set_custom_encoding:
+ * @web_view: a #WebKitWebView
+ * @encoding: the new encoding, or %NULL to restore the default encoding
+ *
+ * Sets the current #WebKitWebView encoding, without modifying the default one,
+ * and reloads the page.
+ *
+ * Since: 1.1.1
+ */
+void webkit_web_view_set_custom_encoding(WebKitWebView* webView, const char* encoding)
+{
+    g_return_if_fail(WEBKIT_IS_WEB_VIEW(webView));
+
+    core(webView)->mainFrame()->loader()->reloadWithOverrideEncoding(String::fromUTF8(encoding));
+}
+
+/**
+ * webkit_web_view_get_custom_encoding:
+ * @web_view: a #WebKitWebView
+ *
+ * Returns the current encoding of the #WebKitWebView, not the default-encoding
+ * of WebKitWebSettings.
+ *
+ * Return value: a string containing the current custom encoding for @web_view, or %NULL if there's none set.
+ *
+ * Since: 1.1.1
+ */
+const char* webkit_web_view_get_custom_encoding(WebKitWebView* webView)
+{
+    g_return_val_if_fail(WEBKIT_IS_WEB_VIEW(webView), NULL);
+
+    String overrideEncoding = core(webView)->mainFrame()->loader()->documentLoader()->overrideEncoding();
+
+    if (!overrideEncoding.isEmpty()) {
+        WebKitWebViewPrivate* priv = webView->priv;
+        g_free (priv->customEncoding);
+        priv->customEncoding = g_strdup(overrideEncoding.utf8().data());
+        return priv->customEncoding;
+    } else
+      return NULL;
 }
