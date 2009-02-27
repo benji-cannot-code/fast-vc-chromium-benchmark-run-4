f7bac08091900e9b6908e8641f7de0b7807de6fa
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-27 07:39:37 +0000

    Remove unnecessary config.h files now that the Glue Xcode target no longer uses WebCorePrefix.h.
    R=mark
    
    Review URL: http://codereview.chromium.org/27245
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10596 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/simple_webmimeregistry_impl.cc
+++ webkit/glue/simple_webmimeregistry_impl.cc
@@ -3,10 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
-// TODO(darin): This include should not be necessary, but for some reason the
-// Mac build requires it.
-#include "config.h"
-
 #include "webkit/glue/simple_webmimeregistry_impl.h"
 
 #include "WebString.h"
