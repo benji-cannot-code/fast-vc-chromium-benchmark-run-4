281b7ed51fb139ea9ff57fc9bc3ce2bf9ca9c840
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-27 17:19:18 +0000

    Add a centralized error reporter to the extensions sytem. This can be called from any component on any thread. The hope is that this will encourage more thorough error handling.
    Review URL: http://codereview.chromium.org/27165
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10611 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profile.cc
+++ chrome/browser/profile.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_list.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/download/download_manager.h"
+#include "chrome/browser/extensions/extension_error_reporter.h"
 #include "chrome/browser/extensions/extensions_service.h"
 #include "chrome/browser/extensions/user_script_master.h"
 #include "chrome/browser/history/history.h"
@@ -368,6 +369,7 @@ void ProfileImpl::InitExtensions() {
     script_dir = script_dir.Append(chrome::kUserScriptsDirname);
   }
 
+  ExtensionErrorReporter::Init(true);  // allow noisy errors.
   user_script_master_ = new UserScriptMaster(
       g_browser_process->file_thread()->message_loop(), script_dir);
   extensions_service_ = new ExtensionsService(
