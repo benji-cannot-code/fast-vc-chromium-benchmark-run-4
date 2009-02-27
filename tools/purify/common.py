548ff4da4bd228e9ea3b5c5bb72dc6631c12cab0
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-27 01:03:41 +0000

    Submitting nirnimesh's changes. LGTMed by Elliot Glaysher. See http://codereview.chromium.org/27079
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10552 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/purify/common.py
+++ tools/purify/common.py
@@ -129,8 +129,9 @@ def RunSubprocess(proc, timeout=0, detach=False):
   elif not detach:
     for line in p.stdout.readlines():
       _print_line(line, False)
-    logging.info("flushing stdout")
-    p.stdout.flush()
+    if sys.platform != 'darwin':   # stdout flush fails on Mac
+      logging.info("flushing stdout")
+      p.stdout.flush()
 
   logging.info("collecting result code")
   result = p.poll()
@@ -332,6 +333,3 @@ class Rational(object):
             except:
               logging.warning("unable to delete file %s: %s" % (file, 
                               sys.exc_info()[0]))
-
-
-
