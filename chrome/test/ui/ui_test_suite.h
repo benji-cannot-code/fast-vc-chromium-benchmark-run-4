58191ae93664f063d56392e4e9a8b9b5f0e32fbe
patrick@chromium.org <patrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-27 17:54:30 +0000

    Add --log-level to the list of flags accepted by UI tests. Review URL: http://codereview.chromium.org/27264
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10614 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/ui_test_suite.h
+++ chrome/test/ui/ui_test_suite.h
@@ -50,6 +50,11 @@ class UITestSuite : public ChromeTestSuite {
     if (!js_flags.empty()) {
       UITest::set_js_flags(js_flags);
     }
+    std::wstring log_level =
+      parsed_command_line.GetSwitchValue(switches::kLoggingLevel);
+    if (!log_level.empty()) {
+      UITest::set_log_level(log_level);
+    }
   }
 
   virtual void SuppressErrorDialogs() {
