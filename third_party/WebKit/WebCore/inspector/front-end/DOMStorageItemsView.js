885052464fa03113a7fa3d21b1fb7fe473b3bb22
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-02-27 18:56:06 +0000

    Added a refresh button to storage views.
            https://bugs.webkit.org/show_bug.cgi?id=24040
    
            Reviewed by Timothy Hatcher.
    
            * inspector/front-end/DOMStorageItemsView.js:
            (WebInspector.DOMStorageItemsView):
            (WebInspector.DOMStorageItemsView.prototype.get statusBarItems):
            (WebInspector.DOMStorageItemsView.prototype.update):
            (WebInspector.DOMStorageItemsView.prototype._refreshButtonClicked):
            * inspector/front-end/DatabaseTableView.js:
            (WebInspector.DatabaseTableView):
            (WebInspector.DatabaseTableView.prototype.get statusBarItems):
            (WebInspector.DatabaseTableView.prototype._refreshButtonClicked):
            * inspector/front-end/DatabasesPanel.js:
            (WebInspector.DatabasesPanel):
            (WebInspector.DatabasesPanel.prototype.get statusBarItems):
            (WebInspector.DatabasesPanel.prototype.reset):
            (WebInspector.DatabasesPanel.prototype.showDatabase):
            (WebInspector.DatabasesPanel.prototype.showDOMStorage):
            (WebInspector.DatabasesPanel.prototype._updateSidebarWidth):
            * inspector/front-end/inspector.css:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41289 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/DOMStorageItemsView.js
+++ third_party/WebKit/WebCore/inspector/front-end/DOMStorageItemsView.js
@@ -32,17 +32,22 @@ WebInspector.DOMStorageItemsView = function(domStorage)
 
     this.element.addStyleClass("storage-view");
     this.element.addStyleClass("table");
-    
+
     this.deleteButton = document.createElement("button");
     this.deleteButton.title = WebInspector.UIString("Delete");
     this.deleteButton.className = "delete-storage-status-bar-item status-bar-item hidden";
     this.deleteButton.addEventListener("click", this._deleteButtonClicked.bind(this), false);
+
+    this.refreshButton = document.createElement("button");
+    this.refreshButton.title = WebInspector.UIString("Refresh");
+    this.refreshButton.className = "refresh-storage-status-bar-item status-bar-item";
+    this.refreshButton.addEventListener("click", this._refreshButtonClicked.bind(this), false);
 }
 
 WebInspector.DOMStorageItemsView.prototype = {
     get statusBarItems()
     {
-        return [this.deleteButton];
+        return [this.refreshButton, this.deleteButton];
     },
 
     show: function(parentElement)
@@ -63,7 +68,7 @@ WebInspector.DOMStorageItemsView.prototype = {
         var hasDOMStorage = this.domStorage;
         if (hasDOMStorage)
             hasDOMStorage = this.domStorage.domStorage;
-        
+
         if (hasDOMStorage) {
             var dataGrid = WebInspector.panels.databases.dataGridForDOMStorage(this.domStorage.domStorage);
             if (!dataGrid)
@@ -74,6 +79,7 @@ WebInspector.DOMStorageItemsView.prototype = {
                 this.deleteButton.removeStyleClass("hidden");
             }
         }
+
         if (!hasDOMStorage) {
             var emptyMsgElement = document.createElement("div");
             emptyMsgElement.className = "storage-table-empty";
@@ -84,7 +90,7 @@ WebInspector.DOMStorageItemsView.prototype = {
             this.deleteButton.addStyleClass("hidden");
         }
     },
-    
+
     _deleteButtonClicked: function(event)
     {
         if (this._dataGrid) {
@@ -92,6 +98,11 @@ WebInspector.DOMStorageItemsView.prototype = {
             
             this.show();
         }
+    },
+
+    _refreshButtonClicked: function(event)
+    {
+        this.update();
     }
 }
 
--- third_party/WebKit/WebCore/inspector/front-end/DatabaseTableView.js
+++ third_party/WebKit/WebCore/inspector/front-end/DatabaseTableView.js
@@ -33,6 +33,11 @@ WebInspector.DatabaseTableView = function(database, tableName)
 
     this.element.addStyleClass("storage-view");
     this.element.addStyleClass("table");
+
+    this.refreshButton = document.createElement("button");
+    this.refreshButton.title = WebInspector.UIString("Refresh");
+    this.refreshButton.className = "refresh-storage-status-bar-item status-bar-item";
+    this.refreshButton.addEventListener("click", this._refreshButtonClicked.bind(this), false);
 }
 
 WebInspector.DatabaseTableView.prototype = {
@@ -40,6 +45,11 @@ WebInspector.DatabaseTableView.prototype = {
     {
         WebInspector.View.prototype.show.call(this, parentElement);
         this.update();
+    },
+
+    get statusBarItems()
+    {
+        return [this.refreshButton];
     },
 
     update: function()
@@ -78,6 +88,10 @@ WebInspector.DatabaseTableView.prototype = {
         this.element.appendChild(errorMsgElement);
     },
 
+    _refreshButtonClicked: function(event)
+    {
+        this.update();
+    }
 }
 
 WebInspector.DatabaseTableView.prototype.__proto__ = WebInspector.View.prototype;
