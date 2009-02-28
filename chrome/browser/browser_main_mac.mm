72f007fcb1ea5bd306f0b71a9805438006fa9c7c
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-28 01:41:26 +0000

    Adapt (and move) mark@chromium.org's Keystone integration to Chromium. Call it from the Browser.  (no-op if not packaged for branding). Add new build target "app_packaging" to package (if possible). (app_packaging target intentionally left out of "All").
    For convenience, here is the app_packaging script embedded in the xcodeproj:
    
      PACKAGER="${PROJECT_DIR}/tools/mac/package_chrome.sh"
      if [ -f "${PACKAGER}" ]; then
        "${PACKAGER}"
      fi
    
    Mark, I know this is not in gyp... I am happy to have this klobbered
    and redo it (in gyp) at the appropriate time.
    
    Review URL: http://codereview.chromium.org/27293
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10668 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main_mac.mm
+++ chrome/browser/browser_main_mac.mm
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <crt_externs.h>
 #include <Cocoa/Cocoa.h>
 #include "base/command_line.h"
 #include "chrome/app/result_codes.h"
 #include "chrome/browser/app_controller_mac.h"
 #include "chrome/browser/browser_main_win.h"
-#include <crt_externs.h>
+#include "chrome/app/keystone_glue.h"
 
 namespace Platform {
 
@@ -25,6 +26,10 @@ namespace Platform {
 void WillInitializeMainMessageLoop(const CommandLine & command_line) {
   [NSApplication sharedApplication];
   [NSBundle loadNibNamed:@"MainMenu" owner:NSApp];
+
+  // Doesn't need to be in a GOOGLE_CHROME_BUILD since this references
+  // a framework only distributed with Google Chrome.
+  [KeystoneGlue registerWithKeystone];
 
   // TODO(port): Use of LSUIElement=1 is a temporary fix.  The right
   // answer is to fix the renderer to not use Cocoa.
