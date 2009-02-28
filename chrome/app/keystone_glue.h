2dd3954a27ba7e4ee1228cfdaea3aea896ae2671
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-28 01:52:06 +0000

    Fix guard to reflect new position in tree
    Review URL: http://codereview.chromium.org/28293
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10670 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/keystone_glue.h
+++ chrome/app/keystone_glue.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef WEBKIT_TOOLS_TEST_SHELL_MAC_KEYSTONE_GLUE_H_
-#define WEBKIT_TOOLS_TEST_SHELL_MAC_KEYSTONE_GLUE_H_
+#ifndef CHROME_APP_KEYSTONE_GLUE_H_
+#define CHROME_APP_KEYSTONE_GLUE_H_
 
 #import <Foundation/Foundation.h>
 
@@ -31,4 +31,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-#endif  // WEBKIT_TOOLS_TEST_SHELL_MAC_KEYSTONE_GLUE_H_
+#endif  // CHROME_APP_KEYSTONE_GLUE_H_
