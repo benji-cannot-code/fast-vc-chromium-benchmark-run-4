24dc176bba888c21d50259e0decee76afae2c65c
jmalonzo@webkit.org <jmalonzo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-01 19:22:13 +0000

    2009-03-01  Jan Michael Alonzo  <jmalonzo@webkit.org>
            Reviewed by Holger Freyther.
    
             [Gtk] get the HTTP layout tests going
             https://bugs.webkit.org/show_bug.cgi?id=24259
    
            Added API to get the response mime type from a frame. We need this
            so we can decide if we need to dump the frame as text or its
            render tree
    
            * webkit/webkitprivate.h:
            * webkit/webkitwebframe.cpp:
    
    2009-03-01  Jan Michael Alonzo  <jmalonzo@webkit.org>
    
            Reviewed by Holger Freyther.
    
             [Gtk] get the HTTP layout tests going
             https://bugs.webkit.org/show_bug.cgi?id=24259
    
            Determine the frame's response and decide whether to dump as text
            or the render tree
    
            * DumpRenderTree/gtk/DumpRenderTree.cpp:
            (dump):
    
    2009-02-28  Jan Michael Alonzo  <jmalonzo@webkit.org>
    
            Reviewed by Holger Freyther.
    
            [Gtk] get the HTTP layout tests going
            https://bugs.webkit.org/show_bug.cgi?id=24259
    
            Enable http tests, excluding tests/folders that fail or untested
    
            * platform/gtk/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebframe.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebframe.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Copyright (C) 2008 Christian Dywan <christian@imendio.com>
  * Copyright (C) 2008 Collabora Ltd.
  * Copyright (C) 2008 Nuanti Ltd.
+ * Copyright (C) 2009 Jan Alonzo <jmalonzo@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AnimationController.h"
 #include "CString.h"
+#include "DocumentLoader.h"
 #include "FrameLoader.h"
 #include "FrameLoaderClientGtk.h"
 #include "FrameTree.h"
@@ -716,6 +718,14 @@ unsigned int webkit_web_frame_number_of_active_animations(WebKitWebFrame* frame)
         return 0;
 
     return controller->numberOfActiveAnimations();
+}
+
+gchar* webkit_web_frame_get_response_mime_type(WebKitWebFrame* frame)
+{
+    Frame* coreFrame = core(frame);
+    DocumentLoader* docLoader = coreFrame->loader()->documentLoader();
+    String mimeType = docLoader->responseMIMEType();
+    return g_strdup(mimeType.utf8().data());
 }
 
 }
