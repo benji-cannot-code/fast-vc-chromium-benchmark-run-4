d165c2350ad9eace65db41e2a7eb75668ad72a63
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-01 16:22:15 +0000

    2009-03-01  Gustavo Noronha Silva  <gns@gnome.org>
            Reviewed by Holger Freyther.
    
            Avoid leaking errors when reporting GIO errors.
    
            * platform/network/soup/ResourceHandleSoup.cpp:
            (WebCore::readCallback):
            (WebCore::openCallback):
            (WebCore::queryInfoCallback):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41333 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/soup/ResourceHandleSoup.cpp
+++ third_party/WebKit/WebCore/platform/network/soup/ResourceHandleSoup.cpp
@@ -697,6 +697,7 @@ static void readCallback(GObject* source, GAsyncResult* res, gpointer)
     nread = g_input_stream_read_finish(d->m_input_stream, res, &error);
     if (error) {
         ResourceError resourceError = networkErrorForFile(d->m_gfile, error);
+        g_error_free(error);
         cleanupGioOperation(d);
         client->didFail(handle.get(), resourceError);
         return;
@@ -738,6 +739,7 @@ static void openCallback(GObject* source, GAsyncResult* res, gpointer)
     in = g_file_read_finish(G_FILE(source), res, &error);
     if (error) {
         ResourceError resourceError = networkErrorForFile(d->m_gfile, error);
+        g_error_free(error);
         cleanupGioOperation(d);
         client->didFail(handle, resourceError);
         return;
@@ -785,6 +787,7 @@ static void queryInfoCallback(GObject* source, GAsyncResult* res, gpointer)
         // for a while).
 
         ResourceError resourceError = networkErrorForFile(d->m_gfile, error);
+        g_error_free(error);
         cleanupGioOperation(d);
         client->didFail(handle, resourceError);
         return;
@@ -794,7 +797,7 @@ static void queryInfoCallback(GObject* source, GAsyncResult* res, gpointer)
         // FIXME: what if the URI points to a directory? Should we
         // generate a listing? How? What do other backends do here?
 
-        ResourceError resourceError = networkErrorForFile(d->m_gfile, error);
+        ResourceError resourceError = networkErrorForFile(d->m_gfile, 0);
         cleanupGioOperation(d);
         client->didFail(handle, resourceError);
         return;
