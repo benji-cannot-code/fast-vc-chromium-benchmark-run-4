2f2e807c9d4fbc904c203816ec78ea596c3d3a33
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-02 19:12:05 +0000

    Remove includes to the newly removed edit_drop_target.h header.
    Review URL: http://codereview.chromium.org/28319
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10715 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit.cc
+++ chrome/browser/autocomplete/autocomplete_edit.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/autocomplete_accessibility.h"
 #include "chrome/browser/autocomplete/autocomplete_popup_model.h"
 #include "chrome/browser/autocomplete/autocomplete_popup_view_win.h"
-#include "chrome/browser/autocomplete/edit_drop_target.h"
 #include "chrome/browser/autocomplete/keyword_provider.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/command_updater.h"
