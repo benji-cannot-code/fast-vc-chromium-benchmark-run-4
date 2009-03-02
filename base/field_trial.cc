9dd8cb437cd649566512589102ea0203d3ebfde8
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-02 19:21:07 +0000

    Fix linux tree (move static const definition out of class and into cc file)
    TBR=mbelshe
    Review URL: http://codereview.chromium.org/27341
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10716 0039d316-1c4b-4281-b951-d872f2087c98

--- base/field_trial.cc
+++ base/field_trial.cc
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::Time;
 
+// static
+const int FieldTrial::kNotParticipating = -1;
+
 //------------------------------------------------------------------------------
 // FieldTrial methods and members.
 
--- base/field_trial.h
+++ base/field_trial.h
@@ -79,7 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class FieldTrial : public base::RefCounted<FieldTrial> {
  public:
-  static const int kNotParticipating = -1;
+  static const int kNotParticipating;
 
   typedef int Probability;  // Use scaled up probability.
 
