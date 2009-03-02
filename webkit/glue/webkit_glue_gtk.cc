d7a0a7b2a763addf9ec30500a3729e727c9c1290
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-02 23:38:42 +0000

    Fix build bustage.
    TBR=mark
    
    Review URL: http://codereview.chromium.org/28342
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10748 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkit_glue_gtk.cc
+++ webkit/glue/webkit_glue_gtk.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <gtk/gtk.h>
 
+#include "webkit/glue/screen_info.h"
+
 namespace webkit_glue {
 
 ScreenInfo GetScreenInfoHelper(gfx::NativeView window) {
--- webkit/glue/webkit_glue_mac.mm
+++ webkit/glue/webkit_glue_mac.mm
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <AppKit/AppKit.h>
 
+#include "webkit/glue/screen_info.h"
+
 namespace webkit_glue {
 
 static NSScreen *ScreenForWindow(NSWindow *window) {
