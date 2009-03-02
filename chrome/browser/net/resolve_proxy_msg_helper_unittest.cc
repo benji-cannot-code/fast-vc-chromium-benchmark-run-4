fef50c36eab76174a7955ab775de239df342f960
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-02 22:51:02 +0000

    Add file that got missing from main checkin.
    TBR=wtc@chromium.org
    Review URL: http://codereview.chromium.org/27357
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10744 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/resolve_proxy_msg_helper_unittest.cc
+++ chrome/browser/net/resolve_proxy_msg_helper_unittest.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/waitable_event.h"
 #include "net/base/net_errors.h"
+#include "net/proxy/proxy_config_service.h"
+#include "net/proxy/proxy_resolver.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 // This ProxyConfigService always returns "http://pac" as the PAC url to use.
