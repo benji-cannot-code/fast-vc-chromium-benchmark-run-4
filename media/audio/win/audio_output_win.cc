26799a4ccb8b8dd00808f06cb191a36b2dd53bb4
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-02 15:17:47 +0000

    Random change to force a build. Fix style compliance. In theory, shouldn't break the build.
    TBR=cpy
    Review URL: http://codereview.chromium.org/27331
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10701 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/audio_output_win.cc
+++ media/audio/win/audio_output_win.cc
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "media/audio/audio_output.h"
+
 #include <windows.h>
 #include <mmsystem.h>
 
 #include "base/at_exit.h"
 #include "base/basictypes.h"
-#include "media/audio/audio_output.h"
 #include "media/audio/win/audio_manager_win.h"
 #include "media/audio/win/waveout_output_win.h"
 
@@ -150,4 +151,3 @@ AudioManager* AudioManager::GetAudioManager() {
   }
   return g_audio_manager;
 }
-
