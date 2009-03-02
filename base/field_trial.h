b1d76a2b2384b7440e616c6667445215f699cce1
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-02 22:50:07 +0000

    Improve comments in FieldTrial class.
    tbr=mbelshe
    Review URL: http://codereview.chromium.org/28334
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10743 0039d316-1c4b-4281-b951-d872f2087c98

--- base/field_trial.h
+++ base/field_trial.h
@@ -34,20 +34,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // // Note, FieldTrials are reference counted, and persist automagically until
 // // process teardown, courtesy of their automatic registration in
 // // FieldTrialList.
-// trial = new FieldTrial("MemoryExperiment", 1000);
-// group1 = trial->AppendGroup("_high_mem", 20);  // 2% this _high_mem group.
-// group2 = trial->AppendGroup("_low_mem", 20);   // 2% this _low_mem group.
+// scoped_refptr<FieldTrial> trial = new FieldTrial("MemoryExperiment", 1000);
+// int group1 = trial->AppendGroup("_high_mem", 20);  // 2% this _high_mem group.
+// int group2 = trial->AppendGroup("_low_mem", 20);   // 2% this _low_mem group.
 // // Take action depending of which group we randomly land in.
-// switch (trial->group()) {
-//   case group1:
-//          ... do something
-//          break;
-//   case group2:
-//          ....
-//          break;
-//   default:
-//          ...
-// }
+// if (trial->group() == group1)
+//   SetMemoryModel(HIGH);  // Sample setting of browser state.
+// else if (trial->group() == group2)
+//   SetMemoryModel(LOW);  // Sample alternate setting.
 
 // We then modify any histograms we wish to correlate with our experiment to
 // have slighly different names, depending on what group the trial instance
