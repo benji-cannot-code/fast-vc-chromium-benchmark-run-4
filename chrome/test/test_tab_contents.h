20439bd054437dbf488302dbc96ec79807c90e84
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-02 21:00:53 +0000

    Remove unneeded macros that required WTL includes.
    TBR=deanm
    Review URL: http://codereview.chromium.org/27348
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10723 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/test_tab_contents.h
+++ chrome/test/test_tab_contents.h
@@ -13,9 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TabContents typed created by TestTabContentsFactory.
 class TestTabContents : public TabContents {
  public:
-  BEGIN_MSG_MAP(TestTabContents)
-  END_MSG_MAP()
-
   explicit TestTabContents(TabContentsType type);
 
   // Sets the site instance used by *ALL* TestTabContents.
