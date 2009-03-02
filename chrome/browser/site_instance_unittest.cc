a07dbaf70e9e8f6c198c5a2b57cce7f466fa7fd8
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-02 21:39:29 +0000

    Add site_instance_unittest.cc to the Posix build. Review URL: http://codereview.chromium.org/27299
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10726 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/site_instance_unittest.cc
+++ chrome/browser/site_instance_unittest.cc
@@ -11,12 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/testing_profile.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace {
-
 class SiteInstanceTest : public testing::Test {
  private:
   MessageLoopForUI message_loop_;
 };
+
+namespace {
 
 class TestBrowsingInstance : public BrowsingInstance {
  public:
