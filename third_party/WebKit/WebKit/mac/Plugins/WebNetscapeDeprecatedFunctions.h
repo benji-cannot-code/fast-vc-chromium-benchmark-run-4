ad31a772d67b3b858b79bc018e968a5e36e8d463
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-03 00:00:46 +0000

    2009-03-02  Anders Carlsson  <andersca@apple.com>
            Build fix.
    
            * Plugins/WebNetscapeDeprecatedFunctions.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41371 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebNetscapeDeprecatedFunctions.h
+++ third_party/WebKit/WebKit/mac/Plugins/WebNetscapeDeprecatedFunctions.h
@@ -28,9 +28,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <CoreServices/CoreServices.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern OSErr WebGetDiskFragment(const FSSpec *fileSpec, UInt32 offset, UInt32 length, ConstStr63Param fragName, CFragLoadOptions options, CFragConnectionID *connID, Ptr *mainAddr, Str255 errMessage);
 extern OSErr WebCloseConnection(CFragConnectionID *connID);
 extern SInt16 WebLMGetCurApRefNum(void);
 extern void WebLMSetCurApRefNum(SInt16 value);
 
+#ifdef __cplusplus
+}
+#endif
+        
 #endif /* ENABLE(NETSCAPE_PLUGIN_API) && !defined(__LP64__) */
