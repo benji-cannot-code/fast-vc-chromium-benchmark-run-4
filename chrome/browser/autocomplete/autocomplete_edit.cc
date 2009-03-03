44d21998ce47112bb1a7c6a5416e24c60aefb6f7
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-03 12:40:12 +0000

    Cleanup the autocomplete model includes.  Move from a deprecated std::wstring to utf8 url fixer upper function.
    These files should now compile on Linux.
    
    Review URL: http://codereview.chromium.org/28323
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10788 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit.cc
+++ chrome/browser/autocomplete/autocomplete_edit.cc
@@ -5,34 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/autocomplete/autocomplete_edit.h"
 
-#include <locale>
-
-// TODO(deanm): Cleanup these includes.
 #include "base/basictypes.h"
-#include "base/ref_counted.h"
-#include "base/scoped_clipboard_writer.h"
 #include "base/string_util.h"
-#include "chrome/app/chrome_dll_resource.h"
-#include "chrome/browser/autocomplete/autocomplete_accessibility.h"
+#include "chrome/browser/autocomplete/autocomplete_edit_view.h"
 #include "chrome/browser/autocomplete/autocomplete_popup_model.h"
-#include "chrome/browser/autocomplete/autocomplete_popup_view_win.h"
 #include "chrome/browser/autocomplete/keyword_provider.h"
-#include "chrome/browser/browser_process.h"
-#include "chrome/browser/command_updater.h"
-#include "chrome/browser/drag_utils.h"
 #include "chrome/browser/metrics/user_metrics.h"
 #include "chrome/browser/net/url_fixer_upper.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/search_engines/template_url.h"
 #include "chrome/browser/search_engines/template_url_model.h"
-#include "chrome/browser/tab_contents/tab_contents.h"
-#include "chrome/browser/views/location_bar_view.h"
-#include "chrome/common/clipboard_service.h"
 #include "chrome/common/notification_service.h"
-#include "chrome/common/os_exchange_data.h"
-#include "chrome/common/win_util.h"
+#include "googleurl/src/gurl.h"
 #include "googleurl/src/url_util.h"
-#include "grit/generated_resources.h"
 #include "skia/include/SkBitmap.h"
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -165,7 +150,7 @@ bool AutocompleteEditModel::GetURLForText(const std::wstring& text,
   if (type != AutocompleteInput::URL)
     return false;
     
-  *url = GURL(URLFixerUpper::FixupURL(text, std::wstring()));
+  *url = GURL(URLFixerUpper::FixupURL(WideToUTF8(text), std::string()));
   return true;
 }
 
