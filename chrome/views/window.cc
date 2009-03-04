9e61f8b92057bba1c42db33f529512373a598994
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-04 01:07:56 +0000

    Fix other release bustage.  Gah
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10850 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/window.cc
+++ chrome/views/window.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/views/window.h"
 
+#include <shellapi.h>
+
 #include "base/win_util.h"
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/common/gfx/chrome_canvas.h"
