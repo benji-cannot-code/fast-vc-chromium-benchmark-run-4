79d6c317824de1ef5fa51c86604ce47616f2a620
glen@chromium.org <glen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-04 04:07:23 +0000

    * Make the download page focus the input field onload.* Fix spacing between controls (Pause/Cancel).* Help deblock download UI creation (timeout in downloads.html, kMaxDownloads in downloads_ui.cc)* Allow resume for paused downloads BUG=8271,8270,8130,8330 Review URL: http://codereview.chromium.org/40047
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10863 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/downloads_ui.cc
+++ chrome/browser/dom_ui/downloads_ui.cc
@@ -26,6 +26,10 @@ using base::Time;
 // DownloadsUI is accessible from chrome-ui://downloads.
 static const char kDownloadsHost[] = "downloads";
 
+// Maximum number of downloads to show. TODO(glen): Remove this and instead
+// stuff the downloads down the pipe slowly.
+static const int kMaxDownloads = 150;
+
 ///////////////////////////////////////////////////////////////////////////////
 //
 // DownloadsHTMLSource
@@ -74,6 +78,10 @@ void DownloadsUIHTMLSource::StartDataRequest(const std::string& path,
   localized_strings.SetString(L"control_resume",
       l10n_util::GetString(IDS_DOWNLOAD_LINK_RESUME));
 
+  localized_strings.SetString(L"textdirection",
+      (l10n_util::GetTextDirection() == l10n_util::RIGHT_TO_LEFT) ?
+       L"rtl" : L"ltr");
+
   static const StringPiece downloads_html(
       ResourceBundle::GetSharedInstance().GetRawDataResource(
           IDR_DOWNLOADS_HTML));
@@ -99,7 +107,7 @@ class DownloadItemSorter : public std::binary_function<DownloadItem*,
                                                        bool> {
  public:
   bool operator()(const DownloadItem* lhs, const DownloadItem* rhs) {
-    return lhs->start_time() < rhs->start_time();
+    return lhs->start_time() > rhs->start_time();
   }
 };
 
@@ -121,7 +129,9 @@ DownloadsDOMHandler::DownloadsDOMHandler(DOMUI* dom_ui, DownloadManager* dlm)
     NewCallback(this, &DownloadsDOMHandler::HandleDiscardDangerous));
   dom_ui_->RegisterMessageCallback("show",
     NewCallback(this, &DownloadsDOMHandler::HandleShow));
-  dom_ui_->RegisterMessageCallback("pause",
+  dom_ui_->RegisterMessageCallback("togglepause",
+    NewCallback(this, &DownloadsDOMHandler::HandlePause));
+  dom_ui_->RegisterMessageCallback("resume",
     NewCallback(this, &DownloadsDOMHandler::HandlePause));
   dom_ui_->RegisterMessageCallback("cancel",
     NewCallback(this, &DownloadsDOMHandler::HandleCancel));
@@ -180,6 +190,9 @@ void DownloadsDOMHandler::SetDownloads(
   // Scan for any in progress downloads and add ourself to them as an observer.
   for (OrderedDownloads::iterator it = download_items_.begin();
        it != download_items_.end(); ++it) {
+    if (static_cast<int>(it - download_items_.begin()) > kMaxDownloads)
+      break;
+
     DownloadItem* download = *it;
     if (download->state() == DownloadItem::IN_PROGRESS) {
       // We want to know what happens as the download progresses.
@@ -254,11 +267,12 @@ void DownloadsDOMHandler::HandleCancel(const Value* value) {
 
 void DownloadsDOMHandler::SendCurrentDownloads() {
   ListValue results_value;
-
   for (OrderedDownloads::iterator it = download_items_.begin();
-    it != download_items_.end(); ++it) {
-      results_value.Append(CreateDownloadItemValue(*it,
-        static_cast<int>(it - download_items_.begin())));
+      it != download_items_.end(); ++it) {
+    int index = static_cast<int>(it - download_items_.begin());
+    if (index > kMaxDownloads)
+      break;
+    results_value.Append(CreateDownloadItemValue(*it,index));
   }
 
   dom_ui_->CallJavascriptFunction(L"downloadsList", results_value);
@@ -268,7 +282,7 @@ DictionaryValue* DownloadsDOMHandler::CreateDownloadItemValue(
     DownloadItem* download, int id) {
   DictionaryValue* file_value = new DictionaryValue();
 
-  file_value->SetInteger(L"time",
+  file_value->SetInteger(L"started",
     static_cast<int>(download->start_time().ToTimeT()));
   file_value->SetInteger(L"id", id);
   file_value->SetString(L"file_path", download->full_path().ToWStringHack());
