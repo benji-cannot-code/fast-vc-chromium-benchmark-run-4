8b84f3c798ded7ada667dd4179259118eb213699
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-04 04:45:14 +0000

    Test reverting to WebKit's implementation of WTF::currentTime() to see if it explains the intl1 page cycler regression.
    This is a windows only change.
    
    TBR=dglazkov
    
    Review URL: http://codereview.chromium.org/40096
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10867 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkitclient_impl.cc
+++ webkit/glue/webkitclient_impl.cc
@@ -7,6 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop.h"
 
+// HACK for testing purposes only.  Just trying a change on the buildbots.
+#if defined(OS_WIN)
+namespace WTF { double currentTime(); }
+#endif
+
 namespace webkit_glue {
 
 WebKitClientImpl::WebKitClientImpl()
@@ -19,7 +24,11 @@ WebKit::WebClipboard* WebKitClientImpl::clipboard() {
 }
 
 double WebKitClientImpl::currentTime() {
+#if defined(OS_WIN)
+  return WTF::currentTime();
+#else
   return base::Time::Now().ToDoubleT();
+#endif
 }
 
 void WebKitClientImpl::setSharedTimerFiredFunction(void (*func)()) {
