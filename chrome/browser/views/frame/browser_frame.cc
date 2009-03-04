aee5a9456d562f1c52e65a3d58cbe30fa7ef37cf
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-04 00:50:14 +0000

    Fix release-only build break.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10848 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_frame.cc
+++ chrome/browser/views/frame/browser_frame.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/frame/browser_frame.h"
 
 #include <dwmapi.h>
+#include <shellapi.h>
 
 #include "chrome/browser/browser_list.h"
 #include "chrome/browser/views/frame/browser_view.h"
