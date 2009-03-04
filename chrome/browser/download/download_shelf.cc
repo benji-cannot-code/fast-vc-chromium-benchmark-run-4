5f2c51be1f1d68310038a78764b5df9f42fae1c2
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-04 22:19:41 +0000

    Basic download shelf implementation on linux.
    It shows and hides at appropriate times, and that's about it.
    
    TEST=navigate to a savable page and select "save page as" from the page menu. Click the x. Save the page again. The shelf should pop up, go away, pop up again.
    
    Review URL: http://codereview.chromium.org/38004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10927 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_shelf.cc
+++ chrome/browser/download/download_shelf.cc
@@ -15,12 +15,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 void DownloadShelf::ShowAllDownloads() {
+#if defined(OS_WIN)
   Profile* profile = tab_contents_->profile();
   if (profile)
     UserMetrics::RecordAction(L"ShowDownloads", profile);
   GURL url = DownloadsUI::GetBaseURL();
   tab_contents_->OpenURL(url, GURL(), NEW_FOREGROUND_TAB,
       PageTransition::AUTO_BOOKMARK);
+#else
+  // TODO(port): After we port DownloadsUI, enable this function.
+  NOTIMPLEMENTED();
+#endif
 }
 
 void DownloadShelf::ChangeTabContents(TabContents* old_contents,
--- chrome/common/temp_scaffolding_stubs.h
+++ chrome/common/temp_scaffolding_stubs.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/cache_manager_host.h"
 #include "chrome/browser/cancelable_request.h"
+#include "chrome/browser/download/download_shelf.h"
 #include "chrome/browser/download/save_types.h"
 #include "chrome/browser/history/download_types.h"
 #include "chrome/browser/history/history.h"
@@ -64,7 +65,6 @@ class CPCommandInterface;
 class DOMUIHost;
 class DownloadItem;
 class DownloadManager;
-class DownloadShelf;
 class HistoryService;
 class LoginHandler;
 class MetricsService;
@@ -489,24 +489,25 @@ class TabContents : public PageNavigator, public NotificationObserver {
   static void RegisterUserPrefs(PrefService* prefs) {
     prefs->RegisterBooleanPref(prefs::kBlockPopups, false);
   }
-  static void MigrateShelf(TabContents* from, TabContents* to) {
-    NOTIMPLEMENTED();
-  }
   virtual void CreateView() {}
   virtual gfx::NativeView GetNativeView() const { return NULL; }
   static TabContentsFactory* RegisterFactory(TabContentsType type,
                                              TabContentsFactory* factory);
-  void OnStartDownload(DownloadItem* download) { NOTIMPLEMENTED(); }
   void RemoveInfoBar(InfoBarDelegate* delegate) { NOTIMPLEMENTED(); }
   virtual bool ShouldDisplayURL() { return true; }
   void ToolbarSizeChanged(bool is_animating);
-  DownloadShelf* GetDownloadShelf() { NOTIMPLEMENTED(); return NULL; }
+  void OnStartDownload(DownloadItem* download);
+  DownloadShelf* GetDownloadShelf();
+  static void MigrateShelf(TabContents* from, TabContents* to);
+  void MigrateShelfFrom(TabContents* tab_contents);
  protected:
   typedef std::vector<ConstrainedWindow*> ConstrainedWindowList;
   ConstrainedWindowList child_windows_;
  private:
+  virtual void ReleaseDownloadShelf();
   friend class AutomationProvider;
 
+  scoped_ptr<DownloadShelf> download_shelf_;
   TabContentsType type_;
   bool is_crashed_;
   bool is_active_;
