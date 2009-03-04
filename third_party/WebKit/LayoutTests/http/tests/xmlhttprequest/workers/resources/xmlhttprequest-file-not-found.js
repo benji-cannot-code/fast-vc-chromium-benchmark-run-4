2043e8c6332e69aa54b685ae262c14d8def7f1e6
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-04 22:29:54 +0000

    WebCore:
    2009-03-04  David Levin  <levin@chromium.org>
    
            Reviewed by Alexey Proskuryakov.
    
            Bug 24337: Assert when doing sync XHR in a worker for a cacheable response.
            <https://bugs.webkit.org/show_bug.cgi?id=24337>
    
            Tests: http/tests/xmlhttprequest/access-control-basic-denied-preflight-cache.html
                   http/tests/xmlhttprequest/workers/xmlhttprequest-file-not-found.html
    
            * loader/mac/ResourceLoaderMac.mm:
            (WebCore::ResourceLoader::willCacheResponse):
            An identifier is only asssigned if resource load callbacks are done.  So don't send
            willCacheResponse which uses identifier if resource load callbacks aren't being sent.
    
    LayoutTests:
    
    2009-03-04  David Levin  <levin@chromium.org>
    
            Reviewed by Alexey Proskuryakov.
    
            Bug 24337: Assert when doing sync XHR in a worker for a cacheable response.
            <https://bugs.webkit.org/show_bug.cgi?id=24337>
    
            * http/tests/xmlhttprequest/access-control-basic-denied-preflight-cache-expected.txt: Added.
            * http/tests/xmlhttprequest/access-control-basic-denied-preflight-cache.html: Added.
            * http/tests/xmlhttprequest/workers/resources/xmlhttprequest-file-not-found.js: Added.
            (log):
            (done):
            * http/tests/xmlhttprequest/workers/xmlhttprequest-file-not-found-expected.txt: Added.
            * http/tests/xmlhttprequest/workers/xmlhttprequest-file-not-found.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41434 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/workers/resources/xmlhttprequest-file-not-found.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function log(message)
+{
+    postMessage("log " + message);
+}
+
+function done()
+{
+    postMessage("DONE");
+}
+
+req = new XMLHttpRequest;
+try {
+    req.open("GET", "missing-file", false);
+    req.send();
+} catch (e) {
+    log("Exception received.");
+}
+done();
