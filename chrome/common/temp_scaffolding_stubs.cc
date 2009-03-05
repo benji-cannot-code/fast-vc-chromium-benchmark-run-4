809f631b4c1f864ee9de2c3e9d9ca4a3344816bf
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-05 18:29:56 +0000

    Reverting 11002. Review URL: http://codereview.chromium.org/39203
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11008 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.cc
+++ chrome/common/temp_scaffolding_stubs.cc
@@ -428,7 +428,6 @@ bool IsPluginProcess() {
 //--------------------------------------------------------------------------
 
 void RunJavascriptMessageBox(WebContents* web_contents,
-                             const GURL& url,
                              int dialog_flags,
                              const std::wstring& message_text,
                              const std::wstring& default_prompt_text,
@@ -438,7 +437,6 @@ void RunJavascriptMessageBox(WebContents* web_contents,
 }
 
 void RunBeforeUnloadDialog(WebContents* web_contents,
-                           const GURL& url,
                            const std::wstring& message_text,
                            IPC::Message* reply_msg) {
   NOTIMPLEMENTED();
