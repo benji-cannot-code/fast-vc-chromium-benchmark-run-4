e59c738bc1bb383fe63d06eb351a71bf270a2ff4
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-05 17:34:38 +0000

    2009-03-05  Dimitri Glazkov  <dglazkov@chromium.org>
            GTK Build fix.
    
            * inspector/ConsoleMessage.h: Use proper header.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/ConsoleMessage.h
+++ third_party/WebKit/WebCore/inspector/ConsoleMessage.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Console.h"
 
-#include <JavaScriptCore/Protect.h>
+#include <runtime/Protect.h>
 #include <wtf/Vector.h>
 
 namespace JSC {
