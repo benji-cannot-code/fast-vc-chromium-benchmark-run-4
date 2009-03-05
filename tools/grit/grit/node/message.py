0cbadc53685b0db1b56440a0ff54557d48eec52c
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-05 01:32:47 +0000

    Use grit to generate locale rc files and remove the old locale rc files from the tree.
    I made a small change to grit so we can use our resource ID as the translation ID making it a bit easier to read.
    
    The old locale_settings*.rc files would #include the google_strings.rc or chromium_strings.rc.  Instead, I added it to the vcproj file using $(CHROMIUM_BUILD) to grab the right file.  This required changing the output names in the grd files.
    
    TBR=deanm
    
    Review URL: http://codereview.chromium.org/39152
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10955 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/grit/grit/node/message.py
+++ tools/grit/grit/node/message.py
@@ -55,7 +55,7 @@ class MessageNode(base.ContentNode):
   def _IsValidAttribute(self, name, value):
     if name not in ['name', 'offset', 'translateable', 'desc', 'meaning',
                     'internal_comment', 'shortcut_groups', 'custom_type',
-                    'validation_expr']:
+                    'validation_expr', 'use_name_for_id']:
       return False
     if name == 'translateable' and value not in ['true', 'false']:
       return False
@@ -73,6 +73,7 @@ class MessageNode(base.ContentNode):
       'shortcut_groups' : '',
       'custom_type' : '',
       'validation_expr' : '',
+      'use_name_for_id' : 'false',
     }
 
   def GetTextualIds(self):
@@ -140,10 +141,14 @@ class MessageNode(base.ContentNode):
     description_or_id = self.attrs['desc']
     if description_or_id == '' and 'name' in self.attrs:
       description_or_id = 'ID: %s' % self.attrs['name']
-    
+
+    assigned_id = None
+    if self.attrs['use_name_for_id'] == 'true':
+      assigned_id = self.attrs['name']
     message = tclib.Message(text=text, placeholders=placeholders,
                             description=description_or_id,
-                            meaning=self.attrs['meaning'])
+                            meaning=self.attrs['meaning'],
+                            assigned_id=assigned_id)
     self.clique = self.UberClique().MakeClique(message, self.IsTranslateable())
     for group in self.shortcut_groups_:
       self.clique.AddToShortcutGroup(group)
