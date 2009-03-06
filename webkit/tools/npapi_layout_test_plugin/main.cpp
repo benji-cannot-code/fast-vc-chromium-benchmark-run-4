c2019af519aee10a26a577ac8a1b9dc25b0b4569
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-06 21:14:54 +0000

    Fixes the following layout tests.
    1. LayoutTests\plugins\netscape-plugin-setwindow-size.html
    2. LayoutTests\plugins\destroy-stream-twice.html.
    
    The fix for the first test was to get our NPP_SetWindow implementation in the layout test plugin on par with webkit's implementation. I also added a fflush(stdout) to make sure that the printf shows up. If we don't do this the test fails often.
    
    The fix for the second test is to get our NPP_DestroyStream implementation in the layout test plugin on par with webkit's implementation. With the latest webkit update, there is a destroyNullStream test which has been added in this layout test. The layout test plugin had to be updated with this new method on the same lines as its counterpart in  webkit.
    
    
    Review URL: http://codereview.chromium.org/39242
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11157 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/npapi_layout_test_plugin/main.cpp
+++ webkit/tools/npapi_layout_test_plugin/main.cpp
@@ -139,15 +139,12 @@ NPError NPP_Destroy(NPP instance, NPSavedData **save)
 
 NPError NPP_SetWindow(NPP instance, NPWindow *window)
 {
-    if (window->window == NULL) {
-        return NPERR_NO_ERROR;
-    }
-
     PluginObject* obj = static_cast<PluginObject*>(instance->pdata);
 
     if (obj) {
         if (obj->logSetWindow) {
             printf("PLUGIN: NPP_SetWindow: %d %d\n", (int)window->width, (int)window->height);
+            fflush(stdout);
             obj->logSetWindow = false;
         }
     }
@@ -191,9 +188,9 @@ NPError NPP_NewStream(NPP instance, NPMIMEType type, NPStream *stream, NPBool se
 NPError NPP_DestroyStream(NPP instance, NPStream *stream, NPReason reason)
 {
     PluginObject* obj = static_cast<PluginObject*>(instance->pdata);
+
     if (obj->onStreamDestroy)
         executeScript(obj, obj->onStreamDestroy);
-    obj->stream = 0;
 
     return NPERR_NO_ERROR;
 }
