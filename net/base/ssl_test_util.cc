d9423dc57932d8dd3b02e0d20cf7ee2a683fd44e
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-06 00:37:52 +0000

    Add performance tests for the proxy resolver implementations.
    I downloaded the test data "no-ads.pac" from <http://ap.coova.org/wpad.dat>.
    
    BUG=2764
    Review URL: http://codereview.chromium.org/40149
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11066 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/ssl_test_util.cc
+++ net/base/ssl_test_util.cc
@@ -38,6 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/test_completion_callback.h"
 #include "testing/platform_test.h"
 
+#if defined(OS_WIN)
+#pragma comment(lib, "crypt32.lib")
+#endif
+
 namespace {
 
 #if defined(OS_LINUX)
