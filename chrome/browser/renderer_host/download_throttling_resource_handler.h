5dc48a6eabe69834b299436e5bcd67c512785512
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-07 00:19:38 +0000

    This CL adds a way to specify the security info when canceling a URLRequest. This allows to tag a request on the renderer side with its security info.  It is useful for the "frame info" dialog. When showing that dialog for blocked frames, the security info can be retrieved and users can see the cert details for the blocked frame.
    TEST=Open a page containing a frame served over bad HTTPS.  The frame is blocked (replaced with a warning message). Right-click, select "Frame info". The dialog should have a "show cert" button which when pressed should show the frame's bad cert.
    BUG=2853
    Review URL: http://codereview.chromium.org/7276
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11179 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/download_throttling_resource_handler.h
+++ chrome/browser/renderer_host/download_throttling_resource_handler.h
@@ -47,7 +47,8 @@ class DownloadThrottlingResourceHandler
                           int min_size);
   virtual bool OnReadCompleted(int request_id, int* bytes_read);
   virtual bool OnResponseCompleted(int request_id,
-                                   const URLRequestStatus& status);
+                                   const URLRequestStatus& status,
+                                   const std::string& security_info);
 
   // DownloadRequestManager::Callback implementation:
   void CancelDownload();
