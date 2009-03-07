f17ab4851d6fae21ece3c8eb63e0f95592700458
treat@webkit.org <treat@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-07 18:42:26 +0000

    2009-03-07  Adam Treat  <adam.treat@torchmobile.com>
            Reviewed by Cameron Zwarich.
    
            These methods are clearly returning the wrong values as the two were
            returning swapped information.
    
            * Api/qwebpage.cpp:
            (QWebPage::totalBytes):
            (QWebPage::bytesReceived):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -2602,7 +2602,7 @@ void QWebPagePrivate::_q_onLoadProgressChanged(int) {
     \sa bytesReceived()
 */
 quint64 QWebPage::totalBytes() const {
-    return d->m_bytesReceived;
+    return d->m_totalBytes;
 }
 
 
@@ -2612,7 +2612,7 @@ quint64 QWebPage::totalBytes() const {
     \sa totalBytes()
 */
 quint64 QWebPage::bytesReceived() const {
-    return d->m_totalBytes;
+    return d->m_bytesReceived;
 }
 
 /*!
