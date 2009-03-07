96d88d4e0568c62f4370b37f42f7794c02dd3477
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-07 07:47:52 +0000

    Landing again the CL that adds security info to canceled requests (it was breaking NPAPI tests on Vista due to an erroneuous commented line).
    TBR=darin
    
    
    Review URL: http://codereview.chromium.org/39309
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11197 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ssl/ssl_manager.h
+++ chrome/browser/ssl/ssl_manager.h
@@ -50,6 +50,8 @@ class WebContents;
 
 class SSLManager : public NotificationObserver {
  public:
+  class CertError;
+
   // An ErrorHandler carries information from the IO thread to the UI thread
   // and is dispatched to the appropriate SSLManager when it arrives on the
   // UI thread.  Subclasses should override the OnDispatched/OnDispatchFailed
@@ -63,6 +65,8 @@ class SSLManager : public NotificationObserver {
   class ErrorHandler : public base::RefCountedThreadSafe<ErrorHandler> {
    public:
     virtual ~ErrorHandler() { }
+
+    virtual CertError* AsCertError() { return NULL; }
 
     // Find the appropriate SSLManager for the URLRequest and begin handling
     // this error.
@@ -168,7 +172,7 @@ class SSLManager : public NotificationObserver {
     bool request_has_been_notified_;  // A flag to make sure we notify the
                                       // URLRequest exactly once.
 
-    DISALLOW_EVIL_CONSTRUCTORS(ErrorHandler);
+    DISALLOW_COPY_AND_ASSIGN(ErrorHandler);
   };
 
   // A CertError represents an error that occurred with the certificate in an
@@ -176,6 +180,9 @@ class SSLManager : public NotificationObserver {
   // thread and allows us to cancel/continue a request it is associated with.
   class CertError : public ErrorHandler {
    public:
+
+    virtual CertError* AsCertError() { return this; }
+
     // These accessors are available on either thread
     const net::SSLInfo& ssl_info() const { return ssl_info_; }
     int cert_error() const { return cert_error_; }
@@ -207,7 +214,7 @@ class SSLManager : public NotificationObserver {
     // that error.
     ResourceType::Type resource_type_;
 
-    DISALLOW_EVIL_CONSTRUCTORS(CertError);
+    DISALLOW_COPY_AND_ASSIGN(CertError);
   };
 
   // The MixedContentHandler class is used to query what to do with
@@ -225,7 +232,7 @@ class SSLManager : public NotificationObserver {
     virtual void OnDispatched() { manager()->OnMixedContent(this); }
 
    private:
-    DISALLOW_EVIL_CONSTRUCTORS(MixedContentHandler);
+    DISALLOW_COPY_AND_ASSIGN(MixedContentHandler);
   };
 
   // The SSLManager will ask its delegate to decide how to handle events
