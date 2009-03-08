b081429b8deb9a9b7175f011764e4175f181864e
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-08 19:35:54 +0000

    2009-03-08  Gustavo Noronha Silva  <gns@gnome.org>
            Reviewed by Holger Freyther.
    
            Implement proper logging for the GTK+ port, inspired by the one in
            Qt. We use the WEBKIT_DEBUG environment variable which was already
            being used to decide whether to setup a logger for soup or not.
    
            * platform/gtk/LoggingGtk.cpp:
            (WebCore::getChannelFromName):
            (WebCore::InitializeLoggingChannelsIfNecessary):
            * platform/network/soup/ResourceHandleSoup.cpp:
            (WebCore::ensureSessionIsInitialized):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41520 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/soup/ResourceHandleSoup.cpp
+++ third_party/WebKit/WebCore/platform/network/soup/ResourceHandleSoup.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DocLoader.h"
 #include "Frame.h"
 #include "HTTPParsers.h"
+#include "Logging.h"
 #include "MIMETypeRegistry.h"
 #include "NotImplemented.h"
 #include "Page.h"
@@ -402,9 +403,7 @@ static void ensureSessionIsInitialized(SoupSession* session)
     else
         setDefaultCookieJar(jar);
 
-    const char* webkitDebug = g_getenv("WEBKIT_DEBUG");
-    if (!soup_session_get_feature(session, SOUP_TYPE_LOGGER)
-        && webkitDebug && !strcmp(webkitDebug, "network")) {
+    if (!soup_session_get_feature(session, SOUP_TYPE_LOGGER) && LogNetwork.state == WTFLogChannelOn) {
         SoupLogger* logger = soup_logger_new(static_cast<SoupLoggerLogLevel>(SOUP_LOGGER_LOG_BODY), -1);
         soup_logger_attach(logger, session);
         g_object_unref(logger);
