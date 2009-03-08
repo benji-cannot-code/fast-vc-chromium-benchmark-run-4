faca26e5ce7a5e7b99fe01d63b2f2b76afb3d784
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-08 07:41:31 +0000

    Landing again the CL that adds security info to canceled requests (last time it was causing sync XMLHttpRequests to hang, breaking many layout tests).
    TBR=darin
    Review URL: http://codereview.chromium.org/39321
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11221 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_plugin_host.cc
+++ chrome/renderer/chrome_plugin_host.cc
@@ -87,7 +87,8 @@ class PluginRequestHandlerProxy
     }
   }
 
-  virtual void OnCompletedRequest(const URLRequestStatus& status) {
+  virtual void OnCompletedRequest(const URLRequestStatus& status,
+                                  const std::string& security_info) {
     completed_ = true;
 
     if (!status.is_success()) {
