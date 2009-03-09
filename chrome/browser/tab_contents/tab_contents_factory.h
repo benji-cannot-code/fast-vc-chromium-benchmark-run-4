92dadbc663f400815e787c60151a9058b2dc56f1
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-09 21:11:36 +0000

    Enable download_request_manager_unittest on linux.
    Review URL: http://codereview.chromium.org/41002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11288 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents_factory.h
+++ chrome/browser/tab_contents/tab_contents_factory.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include "chrome/browser/tab_contents/tab_contents_type.h"
 
+class GURL;
 class TabContents;
 
 // Extend from this class to implement a custom tab contents type.  See
