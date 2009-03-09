8168f0ff18cbe19af07a35635d0bd7f4599d6ccb
markus@chromium.org <markus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-09 22:31:39 +0000

    Adds support for BlockTillLoaded to Linux. Review URL: http://codereview.chromium.org/39298
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11300 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_model.h
+++ chrome/browser/bookmarks/bookmark_model.h
@@ -10,9 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/lock.h"
 #include "base/observer_list.h"
-#if defined(OS_WIN)
-#include "base/scoped_handle.h"
-#endif  // defined(OS_WIN)
+#include "base/waitable_event.h"
 #include "chrome/browser/bookmarks/bookmark_service.h"
 #include "chrome/browser/bookmarks/bookmark_storage.h"
 #include "chrome/browser/cancelable_request.h"
@@ -431,12 +429,7 @@ class BookmarkModel : public NotificationObserver, public BookmarkService {
   // doesn't exist and the history service hasn't finished loading.
   bool waiting_for_history_load_;
 
-#if defined(OS_WIN)
-  // TODO(port): Implement for other platforms.
-
-  // Handle to event signaled when loading is done.
-  ScopedHandle loaded_signal_;
-#endif
+  base::WaitableEvent loaded_signal_;
 
   DISALLOW_COPY_AND_ASSIGN(BookmarkModel);
 };
