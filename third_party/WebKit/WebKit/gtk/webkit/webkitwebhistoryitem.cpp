e16665c8b771aee2b4bd67c3ba85f8d68d0ee777
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-10 17:34:32 +0000

    2009-03-10  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Alexey Proskuryakov.
    
            https://bugs.webkit.org/show_bug.cgi?id=24493
            [GTK] Misc patches for WebKitWebHistoryItem
    
            Use 'if (foo)' instead of 'if (foo != NULL)', per coding style
            guidelines.
    
            * webkit/webkitwebhistoryitem.cpp:
            (_WebKitWebHistoryItemPrivate::webkit_history_item_remove):
            (_WebKitWebHistoryItemPrivate::webkit_web_history_item_get_title):
            (_WebKitWebHistoryItemPrivate::webkit_web_history_item_get_alternate_title):
            (_WebKitWebHistoryItemPrivate::webkit_web_history_item_get_uri):
            (_WebKitWebHistoryItemPrivate::webkit_web_history_item_get_original_uri):
            (_WebKitWebHistoryItemPrivate::webkit_web_history_item_get_last_visited_time):
            (_WebKitWebHistoryItemPrivate::webkit_web_history_item_get_target):
            (_WebKitWebHistoryItemPrivate::webkit_web_history_item_is_target_item):
            (_WebKitWebHistoryItemPrivate::webkit_web_history_item_get_children):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41558 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebhistoryitem.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebhistoryitem.cpp
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Boston, MA 02110-1301, USA.
  */
 
-
 #include "config.h"
 
 #include "webkitwebhistoryitem.h"
@@ -100,7 +99,7 @@ static void webkit_history_item_remove(WebCore::HistoryItem* historyItem)
     GHashTable* table = webkit_history_items();
     WebKitWebHistoryItem* webHistoryItem = (WebKitWebHistoryItem*) g_hash_table_lookup(table, historyItem);
 
-    g_return_if_fail(webHistoryItem != NULL);
+    g_return_if_fail(webHistoryItem);
 
     g_hash_table_remove(table, historyItem);
     g_object_unref(webHistoryItem);
@@ -332,7 +331,7 @@ G_CONST_RETURN gchar* webkit_web_history_item_get_title(WebKitWebHistoryItem* we
 
     WebCore::HistoryItem* item = core(webHistoryItem);
 
-    g_return_val_if_fail(item != NULL, NULL);
+    g_return_val_if_fail(item, NULL);
 
     WebKitWebHistoryItemPrivate* priv = webHistoryItem->priv;
     priv->title = item->title().utf8();
@@ -354,7 +353,7 @@ G_CONST_RETURN gchar* webkit_web_history_item_get_alternate_title(WebKitWebHisto
 
     WebCore::HistoryItem* item = core(webHistoryItem);
 
-    g_return_val_if_fail(item != NULL, NULL);
+    g_return_val_if_fail(item, NULL);
 
     WebKitWebHistoryItemPrivate* priv = webHistoryItem->priv;
     priv->alternateTitle = item->alternateTitle().utf8();
@@ -394,7 +393,7 @@ G_CONST_RETURN gchar* webkit_web_history_item_get_uri(WebKitWebHistoryItem* webH
 
     WebCore::HistoryItem* item = core(WEBKIT_WEB_HISTORY_ITEM(webHistoryItem));
 
-    g_return_val_if_fail(item != NULL, NULL);
+    g_return_val_if_fail(item, NULL);
 
     WebKitWebHistoryItemPrivate* priv = webHistoryItem->priv;
     priv->uri = item->urlString().utf8();
@@ -416,7 +415,7 @@ G_CONST_RETURN gchar* webkit_web_history_item_get_original_uri(WebKitWebHistoryI
 
     WebCore::HistoryItem* item = core(WEBKIT_WEB_HISTORY_ITEM(webHistoryItem));
 
-    g_return_val_if_fail(item != NULL, NULL);
+    g_return_val_if_fail(item, NULL);
 
     WebKitWebHistoryItemPrivate* priv = webHistoryItem->priv;
     priv->originalUri = item->originalURLString().utf8();
@@ -438,7 +437,7 @@ gdouble webkit_web_history_item_get_last_visited_time(WebKitWebHistoryItem* webH
 
     WebCore::HistoryItem* item = core(WEBKIT_WEB_HISTORY_ITEM(webHistoryItem));
 
-    g_return_val_if_fail(item != NULL, 0);
+    g_return_val_if_fail(item, 0);
 
     return item->lastVisitedTime();
 }
@@ -451,7 +450,7 @@ G_CONST_RETURN gchar* webkit_web_history_item_get_target(WebKitWebHistoryItem* w
 
     WebCore::HistoryItem* item = core(webHistoryItem);
 
-    g_return_val_if_fail(item != NULL, NULL);
+    g_return_val_if_fail(item, NULL);
 
     WebCore::CString t = item->target().utf8();
     return g_strdup(t.data());
@@ -463,7 +462,7 @@ gboolean webkit_web_history_item_is_target_item(WebKitWebHistoryItem* webHistory
 
     WebCore::HistoryItem* item = core(webHistoryItem);
 
-    g_return_val_if_fail(item != NULL, false);
+    g_return_val_if_fail(item, false);
 
     return item->isTargetItem();
 }
@@ -474,7 +473,7 @@ GList* webkit_web_history_item_get_children(WebKitWebHistoryItem* webHistoryItem
 
     WebCore::HistoryItem* item = core(webHistoryItem);
 
-    g_return_val_if_fail(item != NULL, NULL);
+    g_return_val_if_fail(item, NULL);
 
     const WebCore::HistoryItemVector& children = item->children();
     if (!children.size())
@@ -501,7 +500,7 @@ WebCore::HistoryItem* WebKit::core(WebKitWebHistoryItem* webHistoryItem)
 
 WebKitWebHistoryItem* WebKit::kit(PassRefPtr<WebCore::HistoryItem> historyItem)
 {
-    g_return_val_if_fail(historyItem != NULL, NULL);
+    g_return_val_if_fail(historyItem, NULL);
 
     RefPtr<WebCore::HistoryItem> item = historyItem;
 
