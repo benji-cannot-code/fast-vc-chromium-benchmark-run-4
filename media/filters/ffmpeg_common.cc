78bc97b4a9f625661e809332c0ff0b5d0c6c6c60
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-10 19:09:28 +0000

    Checking in media::FFmpegGlue, media::FFmpegDemuxer and tests.
    Fixes build break by including FFmpeg dependency for media project in chrome.sln.
    
    This is a second attempt at committing the following changelists:
    http://codereview.chromium.org/39295
    http://codereview.chromium.org/28165
    
    TBR=darin
    
    Review URL: http://codereview.chromium.org/42029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11356 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/filters/ffmpeg_common.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/filters/ffmpeg_common.h"
+
+namespace media {
+
+const char kFFmpegCodecID[] = "FFmpegCodecID";
+
+namespace mime_type {
+
+const char kFFmpegAudio[] = "audio/x-ffmpeg";
+const char kFFmpegVideo[] = "video/x-ffmpeg";
+
+}  // namespace mime_type
+
+}  // namespace media
