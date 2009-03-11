235ad6d67155d536bd47162533762f3e59f0a1dd
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-11 21:40:49 +0000

    2009-03-11  Darin Fisher  <darin@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            https://bugs.webkit.org/show_bug.cgi?id=24521
            Add some missing includes for the non-JSC build.
    
            * platform/text/PlatformString.h: added OwnPtr.h
            * xml/XMLHttpRequest.cpp: added SecurityOrigin.h
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
+++ third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTTPParsers.h"
 #include "ResourceError.h"
 #include "ResourceRequest.h"
+#include "SecurityOrigin.h"
 #include "Settings.h"
 #include "TextResourceDecoder.h"
 #include "ThreadableLoader.h"
