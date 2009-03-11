754d49df67eefa5329e7d1af365ae65152b4ca27
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-11 20:55:54 +0000

    Minor nits in IO buffer
    While doing restructuring work on filters, I read some of the IObuffer code,
    and it scared me a bit that there was a public constructor taking a data
    buffer that was then destroyed.  After searching around for how this was used,
    I realized it is only used by the derived class, and thought that making it
    protected would save other folks from wondering/searching/checking.
    
    Since the size type in other constuctory was int rather than size_t,
    I also added a DCHECK (mostly out of paranoia).
    
    r=rvargas
    Review URL: http://codereview.chromium.org/43086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11479 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/io_buffer.cc
+++ net/base/io_buffer.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 IOBuffer::IOBuffer(int buffer_size) {
-  DCHECK(buffer_size);
+  DCHECK(buffer_size > 0);
   data_ = new char[buffer_size];
 }
 
