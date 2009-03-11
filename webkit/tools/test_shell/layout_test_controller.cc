0cc6d8450e826b55d3e5a06210745d644921ec5b
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-11 00:54:45 +0000

    Reverting 11396.
    failing layout tests on linux
    Review URL: http://codereview.chromium.org/43058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11409 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/layout_test_controller.cc
+++ webkit/tools/test_shell/layout_test_controller.cc
@@ -190,7 +190,7 @@ void LayoutTestController::WorkQueue::AddWork(WorkItem* work) {
 }
 
 void LayoutTestController::dumpAsText(const CppArgumentList& args,
-                                      CppVariant* result) {
+                                                   CppVariant* result) {
   dump_as_text_ = true;
   result->SetNull();
 }
@@ -280,7 +280,7 @@ void LayoutTestController::notifyDone(
 
 class WorkItemBackForward : public LayoutTestController::WorkItem {
  public:
-  explicit WorkItemBackForward(int distance) : distance_(distance) {}
+  WorkItemBackForward(int distance) : distance_(distance) {}
   void Run(TestShell* shell) {
     shell->GoBackOrForward(distance_);
   }
@@ -317,7 +317,7 @@ void LayoutTestController::queueReload(
 
 class WorkItemScript : public LayoutTestController::WorkItem {
  public:
-  explicit WorkItemScript(const string& script) : script_(script) {}
+  WorkItemScript(const string& script) : script_(script) {}
   void Run(TestShell* shell) {
     wstring url = L"javascript:" + UTF8ToWide(script_);
     shell->LoadURL(url.c_str());
@@ -664,7 +664,6 @@ void LayoutTestController::dumpSelectionRect(
 
 void LayoutTestController::display(
     const CppArgumentList& args, CppVariant* result) {
-  shell_->webViewHost()->DisplayForRepaint();
   result->SetNull();
 }
 
