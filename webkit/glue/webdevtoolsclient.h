7afe37fcdb1c293505ea46241d5115cae2a731af
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-12 13:03:51 +0000

    Initial WebDevToolsAgent implementation contains two agent objects: Dom agent and Net agent.
    Dom agent provides API for querying for DOM nodes and receiving notifications
    on
    Dom updates. It has some logic in and this logic is covered with the unit
    tests.
    
    Net agent pushes an initial set of request/response-related events to the
    client. It is to be filled with more data later on. It currently caches loaders
    for all the requests which is Ok for the case when this agent is turned ON (at
    least for now).
    
    Note that this code is not yet wired to the dev tools agent (this is by design).
    The plan is to start enrolling the dev tools agent glue that connects these
    sub-agents with the IPC transport once this CL is in.
    
    Original CL: http://codereview.chromium.org/41008/show
    
    Review URL: http://codereview.chromium.org/43128
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11531 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/glue/webdevtoolsclient.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBKIT_GLUE_WEBDEVTOOLSCLIENT_H_
+#define WEBKIT_GLUE_WEBDEVTOOLSCLIENT_H_
+
+#include <string>
+#include "base/basictypes.h"
+
+class WebDevToolsClientDelegate;
+class WebView;
+
+// WebDevToolsClient represents DevTools client sitting in the Glue. It provides
+// direct and delegate Apis to the host.
+class WebDevToolsClient {
+ public:
+  static WebDevToolsClient* Create(
+      WebView* view,
+      WebDevToolsClientDelegate* delegate);
+
+  WebDevToolsClient() {}
+  virtual ~WebDevToolsClient() {}
+
+  virtual void DispatchMessageFromAgent(const std::string& raw_msg) = 0;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(WebDevToolsClient);
+};
+
+#endif  // WEBKIT_GLUE_WEBDEVTOOLSCLIENT_H_
---
+++ webkit/glue/webdevtoolsclient_delegate.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBKIT_GLUE_WEBDEVTOOLSCLIENT_DELEGATE_H_
+#define WEBKIT_GLUE_WEBDEVTOOLSCLIENT_DELEGATE_H_
+
+#include <string>
+#include "base/basictypes.h"
+
+class WebDevToolsClientDelegate {
+ public:
+  WebDevToolsClientDelegate() {}
+  virtual ~WebDevToolsClientDelegate() {}
+
+  virtual void SendMessageToAgent(const std::string& raw_msg) = 0;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(WebDevToolsClientDelegate);
+};
+
+#endif  // WEBKIT_GLUE_WEBDEVTOOLSAGENT_DELEGATE_H_
