036033edcf9588cc3cb2b09c4006465cfb7d3b84
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-12 16:35:23 +0000

    Add a TabController class to manage a TabView (with corresponding xib). Rewrite strip controller to create and manage TabControllers instead of NSButtons. Review URL: http://codereview.chromium.org/43137
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11541 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/cocoa/tab_view.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_COCOA_TAB_VIEW_H_
+#define CHROME_BROWSER_COCOA_TAB_VIEW_H_
+
+#import <Cocoa/Cocoa.h>
+
+@class TabController;
+
+// A view that handles the event tracking (clicking and dragging) for a tab
+// on the tab strip. Relies on an associated TabController to provide a
+// target/action for selecting the tab.
+
+@interface TabView : NSView {
+ @private
+  IBOutlet TabController* controller_;
+}
+
+@end
+
+#endif  // CHROME_BROWSER_COCOA_TAB_VIEW_H_
