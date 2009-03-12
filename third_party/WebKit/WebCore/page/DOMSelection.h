6fb320a45e1f74646f083464b3a4630b1e0f4f04
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-12 23:05:52 +0000

    Reviewed by Justin Garcia.
            Clean up DOMSelection to use some helper functions
            making the code smaller, and less error-prone.
            https://bugs.webkit.org/show_bug.cgi?id=19221
    
            I tried moving DOMSelection off of rangeCompliantEquivalent
            but failed.  VisibleSelection holds positions like (table, 1) to mean
            "after the table".
    
            * page/DOMSelection.cpp:
            (WebCore::DOMSelection::visibleSelection):
            (WebCore::anchorPosition):
            (WebCore::focusPosition):
            (WebCore::basePosition):
            (WebCore::extentPosition):
            (WebCore::DOMSelection::anchorNode):
            (WebCore::DOMSelection::anchorOffset):
            (WebCore::DOMSelection::focusNode):
            (WebCore::DOMSelection::focusOffset):
            (WebCore::DOMSelection::baseNode):
            (WebCore::DOMSelection::baseOffset):
            (WebCore::DOMSelection::extentNode):
            (WebCore::DOMSelection::extentOffset):
            * page/DOMSelection.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41657 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/DOMSelection.h
+++ third_party/WebKit/WebCore/page/DOMSelection.h
@@ -41,6 +41,7 @@ namespace WebCore {
     class Range;
     class Node;
     class String;
+    class VisibleSelection;
 
     typedef int ExceptionCode;
 
@@ -92,7 +93,10 @@ namespace WebCore {
 
     private:
         DOMSelection(Frame*);
-        
+
+        // Convenience method for accessors, does not NULL check m_frame.
+        const VisibleSelection& visibleSelection() const;
+
         Frame* m_frame;
     };
 
