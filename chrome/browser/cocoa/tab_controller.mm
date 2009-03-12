cf177581934fda5f3141c8a574677ef0052430e9
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-12 20:38:45 +0000

    Don't rely on subview ordering to be equivalent to tab ordering. Allows us to correctly set z-order on tabs. Add animation to tabs closing. Fix issue where new tab button would not draw correctly by pulling it from the nib. Fix memory leak in TabController. Fix console output about missing image. Review URL: http://codereview.chromium.org/43144
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11575 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_controller.mm
+++ chrome/browser/cocoa/tab_controller.mm
@@ -27,6 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
+- (void)dealloc {
+  [image_ release];
+  [super dealloc];
+}
+
 // Called when the tab's nib is done loading and all outlets are hooked up.
 - (void)awakeFromNib {
   [[self view] addSubview:backgroundButton_
