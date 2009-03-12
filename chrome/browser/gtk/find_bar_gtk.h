189f98279debd8c573a6fd6e792912ca94ef2f23
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-12 17:14:31 +0000

    fix linux build.  we need to include basictypes.h to get DISALLOW_COPY_AND_ASSIGN
    TBR=finnur
    
    Review URL: http://codereview.chromium.org/43140
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11545 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/find_bar_gtk.h
+++ chrome/browser/gtk/find_bar_gtk.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_GTK_FIND_BAR_GTK_H_
 #define CHROME_BROWSER_GTK_FIND_BAR_GTK_H_
 
+#include "base/basictypes.h"
 #include "chrome/browser/find_bar.h"
 
 #include <gtk/gtk.h>
