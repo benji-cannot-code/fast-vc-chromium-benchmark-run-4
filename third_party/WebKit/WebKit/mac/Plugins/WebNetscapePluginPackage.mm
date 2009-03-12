ef835a40dedf17f11ef490449e200f5529db9cd8
kdecker@apple.com <kdecker@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-12 18:30:11 +0000

    Reviewed by Anders Carlsson.
            * Plugins/WebNetscapePluginPackage.mm:
            (-[WebNetscapePluginPackage _unloadWithShutdown:]): Simply a small SUPPORT_CFM code block.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginPackage.mm
+++ third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginPackage.mm
@@ -742,11 +742,7 @@ TransitionVector tVectorForFunctionPointer(FunctionPointer fp)
         [self closeResourceFile:resourceRef];
 
 #ifdef SUPPORT_CFM
-    if (isBundle)
-#endif
-        
-#ifdef SUPPORT_CFM
-    else
+    if (!isBundle)
         WebCloseConnection(&connID);
 #endif
 
