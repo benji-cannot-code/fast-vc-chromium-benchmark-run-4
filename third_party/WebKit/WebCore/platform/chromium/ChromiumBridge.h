7827c52036ea2cf68504837be27b1613ec549182
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-12 20:39:01 +0000

    2009-03-12  Darin Fisher  <darin@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            https://bugs.webkit.org/show_bug.cgi?id=24554
            Remove some unused functions from ChromiumBridge.
    
            * platform/chromium/ChromiumBridge.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41646 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/ChromiumBridge.h
+++ third_party/WebKit/WebCore/platform/chromium/ChromiumBridge.h
@@ -134,7 +134,6 @@ namespace WebCore {
         // StatsCounters ------------------------------------------------------
         static void decrementStatsCounter(const char* name);
         static void incrementStatsCounter(const char* name);
-        static void initV8CounterFunction();
 
         // SystemTime ---------------------------------------------------------
         static double currentTime();
@@ -158,9 +157,6 @@ namespace WebCore {
         // Trace Event --------------------------------------------------------
         static void traceEventBegin(const char* name, void* id, const char* extra);
         static void traceEventEnd(const char* name, void* id, const char* extra);
-
-        // URL ----------------------------------------------------------------
-        static KURL inspectorURL();
 
         // Visited links ------------------------------------------------------
         static LinkHash visitedLinkHash(const UChar* url, unsigned length);
