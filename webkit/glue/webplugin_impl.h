d31d26bd0ec4dac1895c11a58b55fb4b58014d07
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-13 00:25:26 +0000

    Checkin files I missed.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11602 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_impl.h
+++ webkit/glue/webplugin_impl.h
@@ -187,7 +187,6 @@ class WebPluginImpl : public WebPlugin,
 
   NPObject* GetWindowScriptNPObject();
   NPObject* GetPluginElement();
-  virtual WebFrame* GetWebFrame() { return webframe_; }
 
   void SetCookie(const GURL& url,
                  const GURL& policy_url,
