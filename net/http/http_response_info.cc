09e07a4cb95476acac5d41852b0cf5bb3ee98e49
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-13 20:06:57 +0000

    Highlights of changes: 1. Added entry to ResourceResponseHead so that it contains    either a base::PlatformFile (OS_WIN) or    base::FileDescriptor (OS_POSIX) for passing the file    handle from browser to renderer process. 2. Also added IPC messages for reporting download progress    and ACK message for it. ResourceLoaderBridge::Peer::OnDownloadProgress    is added so that the peer is notified of the download    progress in the renderer process. 3. Load flag to kick start the resource loading for media    files. LOAD_MEDIA_RESOURCE is added so that    ResourceDispatcherHost knows how to use a different    ResourceHandler for handling media resource request.
    Review URL: http://codereview.chromium.org/27168
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11661 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_info.cc
+++ net/http/http_response_info.cc
@@ -4,12 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "net/http/http_response_info.h"
-
 #include "net/http/http_response_headers.h"
 
 namespace net {
 
-HttpResponseInfo::HttpResponseInfo() {
+HttpResponseInfo::HttpResponseInfo()
+    : response_data_file(base::kInvalidPlatformFileValue) {
 }
 
 HttpResponseInfo::~HttpResponseInfo() {
