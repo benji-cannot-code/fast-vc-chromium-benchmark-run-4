cf045a306b5c7da9e199d6a236a8145f47a39ed1
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-13 23:12:09 +0000

    Remove unneeded includes of gtest_prod.h. Reduce usage from 689 to 601. Review URL: http://codereview.chromium.org/42189
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11680 0039d316-1c4b-4281-b951-d872f2087c98

--- base/time.h
+++ base/time.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <time.h>
 
 #include "base/basictypes.h"
-#include "testing/gtest/include/gtest/gtest_prod.h"
 
 #if defined(OS_WIN)
 // For FILETIME in FromFileTime, until it moves to a new converter class.
--- chrome/browser/history/in_memory_history_backend.h
+++ chrome/browser/history/in_memory_history_backend.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_ptr.h"
 #include "chrome/browser/history/history_notifications.h"
 #include "chrome/common/notification_observer.h"
+#include "testing/gtest/include/gtest/gtest_prod.h"
 
 class HistoryDatabase;
 class Profile;
