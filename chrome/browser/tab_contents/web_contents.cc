5cd356bb2e55eb5cf75ff95886ade5246957aaae
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-13 17:03:07 +0000

    RSS feed support (part 1)
    Part 1 is RSS feed auto-discovery. 
    
    This will parse the web page header to find the feeds in the document and notify
    the browser to display the RSS icon in the toolbar. You can click on the icon,
    but it will just navigate to the first feed on the page, which (unless it has
    been designed to be browser friendly) will just dump XML as text on the user.
    
    For this reason I have disabled the code that makes the RSS icon appear and
    intend to enable it when we have a good landing page to display the XML.
    
    Review URL: http://codereview.chromium.org/43109
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11640 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/web_contents.cc
+++ chrome/browser/tab_contents/web_contents.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/mime_util.h"
 #include "net/base/net_errors.h"
 #include "net/base/registry_controlled_domain.h"
+#include "webkit/glue/feed.h"
 #include "webkit/glue/webkit_glue.h"
 
 #if defined(OS_WIN)
@@ -806,6 +807,25 @@ void WebContents::UpdateTitle(RenderViewHost* rvh,
   // Broadcast notifications when the UI should be updated.
   if (entry == controller()->GetEntryAtOffset(0))
     NotifyNavigationStateChanged(INVALIDATE_TITLE);
+}
+
+void WebContents::UpdateFeedList(
+    RenderViewHost* rvh, const ViewHostMsg_UpdateFeedList_Params& params) {
+  if (!controller())
+    return;
+
+  DCHECK(rvh == render_view_host());
+  NavigationEntry* entry = controller()->GetEntryWithPageID(type(),
+                                                            GetSiteInstance(),
+                                                            params.page_id);
+  if (!entry)
+    return;
+
+  entry->set_feedlist(params.feedlist);
+
+  // Broadcast notifications when the UI should be updated.
+  if (entry == controller()->GetEntryAtOffset(0))
+    NotifyNavigationStateChanged(INVALIDATE_FEEDLIST);
 }
 
 void WebContents::UpdateEncoding(RenderViewHost* render_view_host,
