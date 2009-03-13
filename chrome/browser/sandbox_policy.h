29ffa3cafb7539fd4c6dba55a55bd96d9da7958c
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-13 14:36:40 +0000

    Remove unneeded includes. Review URL: http://codereview.chromium.org/43091
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11629 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sandbox_policy.h
+++ chrome/browser/sandbox_policy.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_path.h"
 #include "sandbox/src/sandbox.h"
-#include "webkit/activex_shim/activex_shared.h"
 
 // Adds the generic policy rules to a sandbox TargetPolicy.
 bool AddGenericPolicy(sandbox::TargetPolicy* policy);
--- webkit/glue/glue_serialize.h
+++ webkit/glue/glue_serialize.h
@@ -11,12 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_GLUE_GLUE_SERIALIZE_H__
 #define WEBKIT_GLUE_GLUE_SERIALIZE_H__
 
-#include "PassRefPtr.h"
-#include "RefPtr.h"
-
 namespace WebCore {
 class String;
 class HistoryItem;
+}
+namespace WTF {
+template<typename T> class PassRefPtr;
 }
 
 namespace webkit_glue {
@@ -24,13 +24,13 @@ namespace webkit_glue {
 // of 0, so the first RefPtr it is assigned to will take ownership.  The empty
 // string corresponds with a NULL HistoryItem.
 void HistoryItemToString(
-    PassRefPtr<WebCore::HistoryItem> item, std::string* serialized_item);
-PassRefPtr<WebCore::HistoryItem> HistoryItemFromString(
+    WTF::PassRefPtr<WebCore::HistoryItem> item, std::string* serialized_item);
+WTF::PassRefPtr<WebCore::HistoryItem> HistoryItemFromString(
     const std::string& serialized_item);
 
 // For testing purposes only.
 void HistoryItemToVersionedString(
-    PassRefPtr<WebCore::HistoryItem> item, int version,
+    WTF::PassRefPtr<WebCore::HistoryItem> item, int version,
     std::string* serialized_item);
 }
 
