5b19cbf797eaf0843c2535b56231f09b8ed7bee8
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-13 19:52:10 +0000

    2009-03-13  Anders Carlsson  <andersca@apple.com>
            Another attempt at fixing the build.
            
            * Plugins/WebNetscapePluginEventHandlerCocoa.mm:
            (WebNetscapePluginEventHandlerCocoa::WebNetscapePluginEventHandlerCocoa):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41680 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginEventHandlerCocoa.mm
+++ third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginEventHandlerCocoa.mm
@@ -34,7 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 WebNetscapePluginEventHandlerCocoa::WebNetscapePluginEventHandlerCocoa(WebNetscapePluginView* pluginView)
     : WebNetscapePluginEventHandler(pluginView)
+#ifndef __LP64__
     , m_keyEventHandler(0)
+#endif
 {
 }
 
