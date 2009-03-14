a10582c4320fcab643da5d43416a1e9fb1069a13
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-14 04:45:58 +0000

    Autocomplete popup should show up when clicking or pressing the up/down key in a text-field, even if the text-field is not empty.
    BUG=6437
    TEST=Open a form with several fields. Start entering text in a text field to bring-up the autocomplete popup. Click somewhere else (so the popup is closed). Click again in the text-field (at the end). On the 2nd click the autocomplete popup should show.
    
    
    Review URL: http://codereview.chromium.org/42198
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11698 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/editor_client_impl.cc
+++ webkit/glue/editor_client_impl.cc
@@ -673,8 +673,7 @@ void EditorClientImpl::ShowAutofillForNode(WebCore::Node* node) {
     if (element->hasLocalName(WebCore::HTMLNames::inputTag)) {
       WebCore::HTMLInputElement* input_element =
           static_cast<WebCore::HTMLInputElement*>(element);
-      if (input_element->value().isEmpty())
-        Autofill(input_element, true);
+      Autofill(input_element, true);
     }
   }
 }
