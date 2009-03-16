e3a3fe8de7494ae378ab5f66c5a6cab35a2f6b12
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-16 22:07:28 +0000

    2009-03-16  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Eric Seidel
    
            Clean up a few issues in the Animation code:
    
            * page/animation/AnimationBase.cpp:
            (WebCore::AnimationBase::updateStateMachine):
            Whitespace
    
            (WebCore::AnimationBase::willNeedService):
            Don't round to float, use std::max
    
            * page/animation/AnimationController.cpp:
            (WebCore::AnimationControllerPrivate::startTimeResponse):
            Fix erroneously copied line to null out m_lastResponseWaiter.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41745 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/AnimationController.cpp
+++ third_party/WebKit/WebCore/page/animation/AnimationController.cpp
@@ -411,7 +411,7 @@ void AnimationControllerPrivate::startTimeResponse(double t)
     }
     
     m_responseWaiters = 0;
-    m_responseWaiters = 0;
+    m_lastResponseWaiter = 0;
 }
 
 AnimationController::AnimationController(Frame* frame)
