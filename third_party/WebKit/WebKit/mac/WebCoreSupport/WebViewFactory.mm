7c3df6cc2827473a51be66e2c68b4a3056febd66
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-16 23:30:18 +0000

    WebKit:
    2009-03-16  Darin Adler  <darin@apple.com>
    
            Reviewed by Dan Bernstein.
    
            * English.lproj/Localizable.strings: Updated.
            * StringsNotToBeLocalized.txt: Updated for recent changes.
    
    WebKit/mac:
    
    2009-03-16  Darin Adler  <darin@apple.com>
    
            Reviewed by Dan Bernstein.
    
            <rdar://problem/6577174> Rename the text directionality submenus to “Paragraph Direction” and “Selection Direction”
    
            * WebCoreSupport/WebViewFactory.mm:
            (-[WebViewFactory contextMenuItemTagParagraphDirectionMenu]): Changed string here, but only
            post-Leopard, since we want this to match the Mac OS X menu on Tiger and Leopard.
            (-[WebViewFactory contextMenuItemTagSelectionDirectionMenu]): Changed string here.
    
    WebKit/win:
    
    2009-03-16  Darin Adler  <darin@apple.com>
    
            Reviewed by Dan Bernstein.
    
            <rdar://problem/6577174> Rename the text directionality submenus to “Paragraph Direction” and “Selection Direction”
    
            For now, change only the localized strings, not the terminology in the code.
    
            * WebCoreLocalizedStrings.cpp:
            (WebCore::contextMenuItemTagWritingDirectionMenu): Changed string here.
            (WebCore::contextMenuItemTagTextDirectionMenu): And here.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebViewFactory.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebViewFactory.mm
@@ -342,12 +342,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSString *)contextMenuItemTagWritingDirectionMenu
 {
+#if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD)
+    return UI_STRING("Paragraph Direction", "Paragraph direction context sub-menu item");
+#else
     return UI_STRING("Writing Direction", "Writing direction context sub-menu item");
+#endif
 }
 
 - (NSString *)contextMenuItemTagTextDirectionMenu
 {
-    return UI_STRING("Text Direction", "Text direction context sub-menu item");
+    return UI_STRING("Selection Direction", "Selection direction context sub-menu item");
 }
 
 - (NSString *)contextMenuItemTagDefaultDirection
