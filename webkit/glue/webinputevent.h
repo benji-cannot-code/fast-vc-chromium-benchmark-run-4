e27d66fcff07abf1fb7f5eb0b79be4bd2ace7cdd
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-16 23:33:24 +0000

    DOM wheel scroll delta fix, Chromium side.  This depends on the WebKit side changes and can't be committed without first landing them and adding DEPS to this. Review URL: http://codereview.chromium.org/48037
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11801 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webinputevent.h
+++ webkit/glue/webinputevent.h
@@ -134,6 +134,8 @@ class WebMouseWheelEvent : public WebMouseEvent {
  public:
   float delta_x;
   float delta_y;
+  float wheel_ticks_x;
+  float wheel_ticks_y;
   bool scroll_by_page;
 
   WebMouseWheelEvent() {}
