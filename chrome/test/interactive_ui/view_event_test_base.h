6118997ad498df3f8309ecdf61a60cc6f1a939a7
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 02:21:19 +0000

    fix bustage - file moved
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11834 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/interactive_ui/view_event_test_base.h
+++ chrome/test/interactive_ui/view_event_test_base.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop.h"
 #include "base/thread.h"
-#include "chrome/views/window_delegate.h"
+#include "chrome/views/window/window_delegate.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 class Task;
