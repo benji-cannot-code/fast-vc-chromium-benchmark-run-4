0630a2d75af4901c66e43d98a138b6b17a72cb04
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 01:08:29 +0000

    fix bustage - moved file
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11817 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run.cc
+++ chrome/browser/first_run.cc
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/master_preferences.h"
 #include "chrome/installer/util/shell_util.h"
 #include "chrome/installer/util/util_constants.h"
-#include "chrome/views/accelerator_handler.h"
+#include "chrome/views/widget/accelerator_handler.h"
 #include "chrome/views/window.h"
 
 #include "google_update_idl.h"
