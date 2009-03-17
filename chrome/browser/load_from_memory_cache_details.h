72c6992af1d5e0fcb421c54cc261365fcbba6f32
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 06:11:08 +0000

    SSL Fix: Step 4.
    Plumb the security origin information for memory cache loads too.
    
    R=wtc
    BUG=8706
    
    
    Review URL: http://codereview.chromium.org/42263
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11850 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/load_from_memory_cache_details.h
+++ chrome/browser/load_from_memory_cache_details.h
@@ -11,20 +11,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class LoadFromMemoryCacheDetails {
  public:
-  LoadFromMemoryCacheDetails(const GURL& url, int cert_id, int cert_status)
-      : url_(url),
-        cert_id_(cert_id),
-        cert_status_(cert_status)
+   LoadFromMemoryCacheDetails(
+       const GURL& url,
+       const std::string& frame_origin,
+       const std::string& main_frame_origin,
+       int cert_id,
+       int cert_status)
+       : url_(url),
+         frame_origin_(frame_origin),
+         main_frame_origin_(main_frame_origin_),
+         cert_id_(cert_id),
+         cert_status_(cert_status)
   { }
 
   ~LoadFromMemoryCacheDetails() { }
 
   const GURL& url() const { return url_; }
+  const std::string& frame_origin() const { return frame_origin_; }
+  const std::string& main_frame_origin() const { return main_frame_origin_; }
   int ssl_cert_id() const { return cert_id_; }
   int ssl_cert_status() const { return cert_status_; }
 
 private:
   GURL url_;
+  std::string frame_origin_;
+  std::string main_frame_origin_;
   int cert_id_;
   int cert_status_;
 
