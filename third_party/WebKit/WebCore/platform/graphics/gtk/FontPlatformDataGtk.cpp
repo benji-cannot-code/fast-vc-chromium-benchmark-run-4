2dc9faaf14fd52ee24dbbb57fc846c5eeaf77453
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-17 14:43:32 +0000

    2009-03-16  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Holger Freyther.
    
            https://bugs.webkit.org/show_bug.cgi?id=24592
            [GTK] Crash in FcPatternHash
    
            Sanitize memory management in gtk fonts.
    
            Release memory allocated by FontPlatformDataGtk in its own
            destructor instead of doing it from other classes, and add copy
            constructor and '=' operator to be able to track referenced
            objects properly.
    
            * platform/graphics/gtk/FontPlatformData.h:
            * platform/graphics/gtk/FontPlatformDataGtk.cpp:
            (WebCore::FontPlatformData::operator=):
            (WebCore::FontPlatformData::FontPlatformData):
            (WebCore::FontPlatformData::~FontPlatformData):
            * platform/graphics/gtk/SimpleFontDataGtk.cpp:
            (WebCore::SimpleFontData::platformDestroy):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41762 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gtk/FontPlatformDataGtk.cpp
+++ third_party/WebKit/WebCore/platform/graphics/gtk/FontPlatformDataGtk.cpp
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Copyright (C) 2006 Michael Emmel mike.emmel@gmail.com
  * Copyright (C) 2007, 2008 Alp Toker <alp@atoker.com>
  * Copyright (C) 2007 Holger Hans Peter Freyther
+ * Copyright (C) 2009 Igalia S.L.
  * All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
@@ -162,6 +163,45 @@ FontPlatformData::FontPlatformData(cairo_font_face_t* fontFace, int size, bool b
     m_scaledFont = cairo_scaled_font_create(fontFace, &fontMatrix, &ctm, options);
 }
 
+FontPlatformData& FontPlatformData::operator=(const FontPlatformData& other)
+{
+    // Check for self-assignment.
+    if (this == &other)
+        return *this;
+
+    m_size = other.m_size;
+    m_syntheticBold = other.m_syntheticBold;
+    m_syntheticOblique = other.m_syntheticOblique;
+
+    if (other.m_scaledFont)
+        cairo_scaled_font_reference (other.m_scaledFont);
+    if (m_scaledFont)
+        cairo_scaled_font_destroy(m_scaledFont);
+    m_scaledFont = other.m_scaledFont;
+
+    if (other.m_pattern)
+        FcPatternReference(other.m_pattern);
+    if (m_pattern)
+        FcPatternDestroy(m_pattern);
+    m_pattern = other.m_pattern;
+
+    if (m_fallbacks) {
+        FcFontSetDestroy(m_fallbacks);
+        // This will be re-created on demand.
+        m_fallbacks = 0;
+    }
+
+    return *this;
+}
+
+FontPlatformData::FontPlatformData(const FontPlatformData& other)
+    : m_pattern(0)
+    , m_fallbacks(0)
+    , m_scaledFont(0)
+{
+    *this = other;
+}
+
 bool FontPlatformData::init()
 {
     static bool initialized = false;
@@ -177,6 +217,20 @@ bool FontPlatformData::init()
 
 FontPlatformData::~FontPlatformData()
 {
+    if (m_pattern && ((FcPattern*)-1 != m_pattern)) {
+        FcPatternDestroy(m_pattern);
+        m_pattern = 0;
+    }
+
+    if (m_fallbacks) {
+        FcFontSetDestroy(m_fallbacks);
+        m_fallbacks = 0;
+    }
+
+    if (m_scaledFont) {
+        cairo_scaled_font_destroy(m_scaledFont);
+        m_scaledFont = 0;
+    }
 }
 
 bool FontPlatformData::isFixedPitch()
