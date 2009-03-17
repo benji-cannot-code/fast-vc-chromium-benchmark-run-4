f124248345687b8ca40c0b2f3e5c031eb850042f
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 20:32:34 +0000

    Some upstream code assumes that the executable path from the path service is standardized, but NSBundle makes not such promise, so we standardize it ourselves.
    While here, remove the app data dir support since it seems the installer and
    windows sandbox are the only things that need it.  Moved that code upstream
    to the chrome paths...
    
    Directly handle the user data dir w/in the chrome path service call back, return
    different things for chrome vs. chromium.
    
    Review URL: http://codereview.chromium.org/48089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11910 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_paths_mac.mm
+++ chrome/common/chrome_paths_mac.mm
@@ -15,9 +15,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome {
 
 bool GetDefaultUserDataDirectory(FilePath* result) {
-  if (!PathService::Get(base::DIR_LOCAL_APP_DATA, result))
-    return false;
-  return true;
+  bool success = false;
+  NSArray* dirs =
+      NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory,
+                                          NSUserDomainMask, YES);
+  if ([dirs count] && result) {
+    NSString* base = [dirs objectAtIndex:0];
+#if defined(GOOGLE_CHROME_BUILD)
+    base = [base stringByAppendingPathComponent@"Google"];
+    NSString* tail = @"Chrome";
+#else
+    NSString* tail = @"Chromium";
+#endif
+    NSString* path = [base stringByAppendingPathComponent:tail];
+    *result = FilePath([path fileSystemRepresentation]);
+    success = true;
+  }
+  return success;
 }
 
 bool GetUserDocumentsDirectory(FilePath* result) {
