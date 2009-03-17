a63089048713f04974d05ce84072af14a3aa7ff9
klink@chromium.org <klink@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 21:36:52 +0000

    Fixes build breakage on Linux.
    TBR=estade (fixing broken Linux build)
    
    Review URL: http://codereview.chromium.org/42307
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11915 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/view_gtk.cc
+++ chrome/views/view_gtk.cc
@@ -18,7 +18,7 @@ void View::DoDrag(const MouseEvent& e, int press_x, int press_y) {
   NOTIMPLEMENTED();
 }
 
-AccessibleWrapper* View::GetAccessibleWrapper() {
+ViewAccessibilityWrapper* View::GetViewAccessibilityWrapper() {
   NOTIMPLEMENTED();
   return NULL;
 }
