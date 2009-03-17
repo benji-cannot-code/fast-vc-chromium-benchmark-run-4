29a8bfffabf01147b222f0cd1bb8b3053ef079d6
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 01:55:27 +0000

    bustage fix - file moved
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11826 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_window_win.cc
+++ chrome/browser/debugger/devtools_window_win.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/debugger/devtools_manager.h"
 #include "chrome/browser/debugger/devtools_view.h"
-#include "chrome/views/window.h"
+#include "chrome/views/window/window.h"
 
 
 // static
