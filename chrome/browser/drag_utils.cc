26a8cb82cbe9fe96aeadd057bd1c736b680695d5
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 05:02:55 +0000

    Forgot these two files.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11842 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/drag_utils.cc
+++ chrome/browser/drag_utils.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/os_exchange_data.h"
 #include "chrome/common/resource_bundle.h"
 #include "chrome/common/win_util.h"
-#include "chrome/views/text_button.h"
+#include "chrome/views/controls/button/text_button.h"
 #include "googleurl/src/gurl.h"
 #include "grit/theme_resources.h"
 
