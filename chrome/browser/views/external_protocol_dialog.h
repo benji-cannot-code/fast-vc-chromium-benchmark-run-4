ce9df4064f7300f8df0814b80fc53aa724ad99b2
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 01:57:59 +0000

    bustage fix - file moved
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11827 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/external_protocol_dialog.h
+++ chrome/browser/views/external_protocol_dialog.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTERNAL_PROTOCOL_DIALOG_H__
 #define CHROME_BROWSER_EXTERNAL_PROTOCOL_DIALOG_H__
 
-#include "chrome/views/dialog_delegate.h"
+#include "chrome/views/window/dialog_delegate.h"
 #include "googleurl/src/gurl.h"
 
 class MessageBoxView;
--- chrome/browser/views/importing_progress_view.h
+++ chrome/browser/views/importing_progress_view.h
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_VIEWS_IMPORTING_PROGRESS_VIEW_H_
 
 #include "chrome/browser/importer/importer.h"
-#include "chrome/views/dialog_delegate.h"
 #include "chrome/views/view.h"
-#include "chrome/views/window.h"
+#include "chrome/views/window/dialog_delegate.h"
+#include "chrome/views/window/window.h"
 
 namespace views {
 class CheckmarkThrobber;
