f131892015b52f5a659b3c1b20217d153238c14f
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 05:07:50 +0000

    doh, release build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11843 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/controls/scrollbar/native_scroll_bar.cc
+++ chrome/views/controls/scrollbar/native_scroll_bar.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/views/controls/scrollbar/native_scroll_bar.h"
 
+#include <atlbase.h>
+#include <atlapp.h>
+#include <atlwin.h>
+
 #include "base/message_loop.h"
 #include "chrome/views/controls/hwnd_view.h"
 #include "chrome/views/widget/widget.h"
