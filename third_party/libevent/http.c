d6f0473539f23e1a03913e6dc246c6a591007b68
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 01:53:22 +0000

    Linux (libevent): Supply sys/queue.h replacement macros when needed.
    Not all build platforms have a sufficiently up-to-date sys/queue.h
    header. For those platforms which don't, we select libevent's replace
    macros and augment them where we are still missing some.
    
    Review URL: http://codereview.chromium.org/48048
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11823 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libevent/http.c
+++ third_party/libevent/http.c
@@ -89,6 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "evutil.h"
 #include "log.h"
 #include "http-internal.h"
+#include "event-internal.h"
 
 #ifdef WIN32
 #define strcasecmp _stricmp
