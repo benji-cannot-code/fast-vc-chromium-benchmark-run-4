a9d49199c7ab2b4d78061db04dc3e9a07b37f6dc
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-18 20:30:06 +0000

    WebCore:
    2009-03-18  Beth Dakin  <bdakin@apple.com>
    
            Reviewed by Dave Hyatt.
    
            Fix for <rdar://problem/6636747> REGRESSION (Safari 4 PB-r41326): 
            Popup menu appears at the wrong location on page at http://
            www.signonsandiego.com/
    
            This was a regression from http://trac.webkit.org/changeset/40769, 
            which changed the base class of RenderInline to 
            RenderBoxModelObject rather than RenderBox.
    
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::offsetParent): offsetParent should return a 
            RenderBoxModelObject rather than just a RenderBox, which is more 
            restrictive. 
    
    LayoutTests:
    
    2009-03-18  Beth Dakin  <bdakin@apple.com>
    
            Reviewed by Dave Hyatt.
    
            Test for <rdar://problem/6636747> REGRESSION (Safari 4 PB-r41326): 
            Popup menu appears at the wrong location on page at http://
            www.signonsandiego.com/
    
            * fast/dom/offset-parent-positioned-and-inline-expected.txt: Added.
            * fast/dom/offset-parent-positioned-and-inline.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41814 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/WebCore/rendering/RenderObject.cpp
@@ -2291,7 +2291,7 @@ RenderBoxModelObject* RenderObject::offsetParent() const
         currZoom = newZoom;
         curr = curr->parent();
     }
-    return curr && curr->isBox() ? toRenderBox(curr) : 0;
+    return curr && curr->isBoxModelObject() ? toRenderBoxModelObject(curr) : 0;
 }
 
 #if ENABLE(SVG)
