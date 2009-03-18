08589f0c913a860a8c14815eff19607957f155d0
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-18 23:47:54 +0000

    Fix single-process ui test failures from my previous change.  The problem was that the wrong pid was being sent to RDH from RMF.  It needs to be the fake pid for the in-process renderer.
    BUG=8890
    Review URL: http://codereview.chromium.org/42366
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12040 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/child_process_info.h
+++ chrome/common/child_process_info.h
@@ -31,7 +31,7 @@ class ChildProcessInfo {
   // Getter to the process handle.
   base::ProcessHandle handle() const { return process_.handle(); }
 
-  int pid() const {
+  virtual int GetProcessId() const {
     if (pid_ != -1)
       return pid_;
 
@@ -96,8 +96,6 @@ class ChildProcessInfo {
   ChildProcessInfo(ProcessType type);
 
  private:
-  friend class ResourceDispatcherHostTest;
-
   ProcessType type_;
   std::wstring name_;
   mutable int pid_;  // Cache of the process id.
