a4f8d11cd7b54dbe653d9a9df0b76801304a21cc
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-18 05:46:29 +0000

    A tricky fix for Issue 1845.
    This change is a very tricky fix for Issue 1845 in chromium: cant alignt text to the right using right shift and right ctrl.This change consists of two parts listed below.
    
    1. Emulating the implementation of Safari that changes the text-direction of an input element.
    
    Safari uses context menus to change the text direction. This change adds an IPC message 'ViewMsg_SetTextDirection', which notifies the new text direction. Also, it adds two functions: RenderWidgetHost::UpdateTextDirection() and RenderWidgetHost::NotifyTextDirection(). They encapsulate the new IPC message so that we can use them both when we presses a set of keys and when we add context-menu items which change the text direction.
    
    2. Calling the above interface when pressing right-shift and right-control keys, or when left-shift and left-control keys.
    
    This modifies the RenderWidgetHostViewWin::OnKeyEvent() function and call the above text-direction interfaces when a user finishes pressing the keys. As you can imagine, if we send an IPC message every time when we receive a WM_KEYDOWN event, we continue sending IPC messages while a user is pressing the keys.
    
    BUG=1845
    Review URL: http://codereview.chromium.org/39252
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11953 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webwidget_impl.cc
+++ webkit/glue/webwidget_impl.cc
@@ -203,6 +203,9 @@ bool WebWidgetImpl::ImeUpdateStatus(bool* enable_ime,
   return false;
 }
 
+void WebWidgetImpl::SetTextDirection(WebTextDirection direction) {
+}
+
 //-----------------------------------------------------------------------------
 // WebCore::HostWindow
 
