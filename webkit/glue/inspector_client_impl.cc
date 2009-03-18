7d007524beb1bba9b41eb6bcce2a9c4778f546ca
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-18 16:08:26 +0000

    Unfork WebInspector frontend take 2: New DEPS and webkit/...
    Review URL: http://codereview.chromium.org/48132
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11967 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/inspector_client_impl.cc
+++ webkit/glue/inspector_client_impl.cc
@@ -167,8 +167,8 @@ String WebInspectorClient::localizedStringsURL() {
 }
 
 String WebInspectorClient::hiddenPanels() {
-  NOTIMPLEMENTED();
-  return String();
+  // Enumerate tabs that are currently disabled.
+  return "scripts,profiles,databases";
 }
 
 void WebInspectorClient::populateSetting(
