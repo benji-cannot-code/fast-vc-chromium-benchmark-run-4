96bd9003dea1e6337ae8f3197fd2496c679eac34
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-18 14:07:37 +0000

    Change the stub for file_util::EvictFileFromSystemCache
    I don't think there is a way to get something out of the UBC once it's in,
    so for now I'm just making this api return true so it won't fail unittests.
    I have email out to Amit incase he has any ideas.
    
    Review URL: http://codereview.chromium.org/42333
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11961 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test_file_util_mac.cc
+++ base/test_file_util_mac.cc
@@ -10,9 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace file_util {
 
 bool EvictFileFromSystemCache(const FilePath& file) {
-  // TODO(port): Implement.
-  NOTIMPLEMENTED();
-  return false;
+  // There is no way that we can think of to dump something from the UBC.  You
+  // can add something so when you open it, it doesn't go in, but that won't
+  // work here.
+  return true;
 }
 
 }  // namespace file_util
