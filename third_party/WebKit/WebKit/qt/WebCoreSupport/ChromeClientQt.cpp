9f73a4ebca3088ecac9e0a5f9db6fa2ca1157c3a
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-19 15:16:07 +0000

    2009-03-19  Paul Olav Tvete  <paul.tvete@nokia.com>
            Reviewed by Simon Hausmann.
    
            Properly escape tooltip text
    
            ManualTest: http://xkcd.com/554/
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41829 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/ChromeClientQt.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/ChromeClientQt.cpp
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qwebsecurityorigin_p.h"
 
 #include <qtooltip.h>
+#include <qtextdocument.h>
 
 namespace WebCore
 {
@@ -374,7 +375,7 @@ void ChromeClientQt::setToolTip(const String &tip)
         view->setToolTip(QString());
         QToolTip::hideText();
     } else {
-        QString dtip = QLatin1String("<p>") + tip + QLatin1String("</p>");
+        QString dtip = QLatin1String("<p>") + Qt::escape(tip) + QLatin1String("</p>");
         view->setToolTip(dtip);
     }
 #else
