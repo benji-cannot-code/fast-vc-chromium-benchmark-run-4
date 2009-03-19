5d7b9a31da92f84864ee045422b506f498aa10f9
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-19 19:56:39 +0000

    2009-03-19  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Darin Fisher.
    
            https://bugs.webkit.org/show_bug.cgi?id=24702
            Upstream miscellaneous bindings changes.
    
            * bindings/v8/ScheduledAction.cpp:
            (WebCore::ScheduledAction::execute): Changed to call lower-case evaluate.
            * bindings/v8/ScriptCallStack.h: Added an extra include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41841 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/ScriptCallStack.h
+++ third_party/WebKit/WebCore/bindings/v8/ScriptCallStack.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ScriptCallStack_h
 
 #include "ScriptCallFrame.h"
+#include "ScriptState.h"
 #include "ScriptValue.h"
 #include <wtf/Noncopyable.h>
 
