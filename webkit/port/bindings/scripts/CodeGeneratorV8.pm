0e0bc3da633faf762c25755f9491978ac9b9b054
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-19 22:18:12 +0000

    Enable IDL attribute [V8ReadOnly] to have the same semantics as having specified an attribute as readonly.
    This allows us to unfork IDL differences where V8 wants
    readonly and JSC does not by using the V8ReadOnly 
    tag.
    
    
    Review URL: http://codereview.chromium.org/50037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12165 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/scripts/CodeGeneratorV8.pm
+++ webkit/port/bindings/scripts/CodeGeneratorV8.pm
@@ -898,7 +898,7 @@ sub GenerateBatchedAttributeData
       $accessControl = "v8::ALL_CAN_WRITE";
     } elsif ($attrExt->{"DoNotCheckDomainSecurity"}) {
       $accessControl = "v8::ALL_CAN_READ";
-       if (!($attribute->type =~ /^readonly/)) {
+       if (!($attribute->type =~ /^readonly/) && !($attrExt->{"V8ReadOnly"})) {
          $accessControl .= "|v8::ALL_CAN_WRITE";
        }
     }
@@ -990,7 +990,7 @@ sub GenerateBatchedAttributeData
     }
 
     # Read only attributes
-    if ($attribute->type =~ /^readonly/) {
+    if ($attribute->type =~ /^readonly/ || $attrExt->{"V8ReadOnly"}) {
       $setter = "0";
     }
 
@@ -1106,7 +1106,8 @@ sub GenerateImplementation
       } elsif ($attribute->signature->extendedAttributes->{"Replaceable"}) {
         $dataNode->extendedAttributes->{"ExtendsDOMGlobalObject"} || die "Replaceable attribute can only be used in interface that defines ExtendsDOMGlobalObject attribute!";
 #        GenerateReplaceableAttrSetter($implClassName);
-      } elsif ($attribute->type !~ /^readonly/) {
+      } elsif ($attribute->type !~ /^readonly/ && 
+               !$attribute->signature->extendedAttributes->{"V8ReadOnly"}) {
         GenerateNormalAttrSetter($attribute, $dataNode, $classIndex, $implClassName);
       }
     }
