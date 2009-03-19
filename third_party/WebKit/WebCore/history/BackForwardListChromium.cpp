b12879f1e9946fcaeac77c7a0bf671ed19bf9859
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-19 06:18:16 +0000

    2009-03-18  Marc-Antoine Ruel  <maruel@chromium.org>
            Reviewed by Darin Fisher.
    
            https://bugs.webkit.org/show_bug.cgi?id=24398
            Fix a crash when loading a svg file in Chromium's test_shell and
            then reloading the page.
    
            * history/BackForwardListChromium.cpp:
            (WebCore::BackForwardList::BackForwardList):
            (WebCore::BackForwardList::close):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41824 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/history/BackForwardListChromium.cpp
+++ third_party/WebKit/WebCore/history/BackForwardListChromium.cpp
@@ -39,6 +39,7 @@ static const unsigned NoCurrentItemIndex = UINT_MAX;
 
 BackForwardList::BackForwardList(Page* page)
     : m_page(page)
+    , m_client(0)
     , m_capacity(DefaultCapacity)
     , m_closed(true)
     , m_enabled(true)
@@ -129,7 +130,8 @@ HistoryItemVector& BackForwardList::entries()
 
 void BackForwardList::close()
 {
-    m_client->close();
+    if (m_client)
+        m_client->close();
     m_page = 0;
     m_closed = true;
 }
