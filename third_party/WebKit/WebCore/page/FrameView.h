e0cbb7a247980582ed78ea91815e319b761488fd
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-20 00:16:02 +0000

    2009-03-19  Anders Carlsson  <andersca@apple.com>
            Reviewed by Dan Bernstein.
    
            <rdar://problem/6682554> Flash content not being rendered (Shockwave Flash 10.0 r22)
            
            If calling updateWidget for some reason resulted in another widget being added to m_widgetUpdateSet, then
            that object would never be updated.
            
            * page/FrameView.cpp:
            (WebCore::FrameView::updateWidgets):
            Factor the widget updating code out into this method. Return true if the update set is empty.
            
            (WebCore::FrameView::performPostLayoutTasks):
            Loop over the update set multiple times until all widgets have been updated or until we reach the cap.
            
            * page/FrameView.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.h
+++ third_party/WebKit/WebCore/page/FrameView.h
@@ -212,6 +212,8 @@ private:
     void updateDeferredRepaintDelay();
     double adjustedDeferredRepaintDelay() const;
 
+    bool updateWidgets();
+    
     static double sCurrentPaintTimeStamp; // used for detecting decoded resource thrash in the cache
 
     unsigned m_refCount;
