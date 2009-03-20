061c0aaec52aab691b841b31d8b4557ca28bb909
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-20 21:49:00 +0000

    Disk cache: More instrumentation for the cache.
    Now we separate data between before and after the cache
    is full. Also, reduce the rate at which a particular
    client sends data to be only once a week. The effect is
    that the histogram data will only have one value per client,
    and the same client will not be "voting" on more than one
    version (release) at the same time.
    
    
    Review URL: http://codereview.chromium.org/50063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12218 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/stats.h
+++ net/disk_cache/stats.h
@@ -43,6 +43,8 @@ class Stats {
     OPEN_RANKINGS,  // An entry has to be read just to modify rankings.
     GET_RANKINGS,  // We got the ranking info without reading the whole entry.
     FATAL_ERROR,
+    LAST_REPORT,  // Time of the last time we sent a report.
+    LAST_REPORT_TIMER,  // Timer count since last report.
     MAX_COUNTER
   };
 
@@ -60,6 +62,12 @@ class Stats {
   int64 GetCounter(Counters counter) const;
 
   void GetItems(StatsItems* items);
+  int GetHitRatio() const;
+  int GetResurrectRatio() const;
+  void ResetRatios();
+
+  // Returns the lower bound of the space used by entries bigger than 512 KB.
+  int GetLargeEntriesSize();
 
   // Saves the stats to disk.
   void Store();
@@ -71,6 +79,7 @@ class Stats {
 
  private:
   int GetStatsBucket(int32 size);
+  int GetRatio(Counters hit, Counters miss) const;
 
   BackendImpl* backend_;
   uint32 storage_addr_;
