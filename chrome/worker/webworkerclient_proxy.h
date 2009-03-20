be167fd67d4581a60fcdefd3584e9da2ee907c95
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-20 23:17:09 +0000

    Fix threading and ownership issues in WebWorkerClientProxy. Review URL: http://codereview.chromium.org/42469
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12230 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/worker/webworkerclient_proxy.h
+++ chrome/worker/webworkerclient_proxy.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_WORKER_WEBWORKERCLIENT_PROXY_H_
 
 #include "base/basictypes.h"
-#include "base/scoped_ptr.h"
+#include "base/ref_counted.h"
 #include "chrome/common/ipc_channel.h"
 #include "googleurl/src/gurl.h"
 #include "webkit/glue/webworkerclient.h"
@@ -20,10 +20,10 @@ class WebWorker;
 // IPCs that are sent to the renderer, where they're converted back to function
 // calls by WebWorkerProxy.
 class WebWorkerClientProxy : public WebWorkerClient,
-                             public IPC::Channel::Listener {
+                             public IPC::Channel::Listener,
+                             public base::RefCounted<WebWorkerClientProxy> {
  public:
   WebWorkerClientProxy (const GURL& url, int route_id);
-  ~WebWorkerClientProxy ();
 
   // WebWorkerClient implementation.
   void PostMessageToWorkerObject(const string16& message);
@@ -46,6 +46,9 @@ class WebWorkerClientProxy : public WebWorkerClient,
   void OnMessageReceived(const IPC::Message& message);
 
  private:
+  friend class base::RefCounted<WebWorkerClientProxy>;
+  ~WebWorkerClientProxy ();
+
   bool Send(IPC::Message* message);
 
   // The source url for this worker.
@@ -53,7 +56,7 @@ class WebWorkerClientProxy : public WebWorkerClient,
 
   int route_id_;
 
-  scoped_ptr<WebWorker> impl_;
+  WebWorker* impl_;
 
   DISALLOW_COPY_AND_ASSIGN(WebWorkerClientProxy);
 };
