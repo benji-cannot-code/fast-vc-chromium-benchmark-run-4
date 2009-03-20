90d70f6f4e4af789114cd69d52c13c719b37b402
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-20 17:59:26 +0000

    Implement context menus on Mac. Review URL: http://codereview.chromium.org/42448
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12198 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/web_contents_view_mac.mm
+++ chrome/browser/tab_contents/web_contents_view_mac.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/cocoa/sad_tab_view.h"
 #include "chrome/browser/renderer_host/render_widget_host.h"
 #include "chrome/browser/renderer_host/render_widget_host_view_mac.h"
+#include "chrome/browser/tab_contents/render_view_context_menu_mac.h"
 #include "chrome/browser/tab_contents/web_contents.h"
 
 #include "chrome/common/temp_scaffolding_stubs.h"
@@ -181,7 +182,9 @@ void WebContentsViewMac::OnFindReply(int request_id,
 }
 
 void WebContentsViewMac::ShowContextMenu(const ContextMenuParams& params) {
-  NOTIMPLEMENTED();
+  RenderViewContextMenuMac menu(web_contents_,
+                                params,
+                                GetNativeView());
 }
 
 WebContents* WebContentsViewMac::CreateNewWindowInternal(
