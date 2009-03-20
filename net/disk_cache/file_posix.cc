24f7b79e9f92a39b572434011a526dfa99e79145
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-20 17:29:09 +0000

    initialize mixed_ in the File(PlatformFile) constructor variant.
    Review URL: http://codereview.chromium.org/42419
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12193 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/file_posix.cc
+++ net/disk_cache/file_posix.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace disk_cache {
 
 File::File(base::PlatformFile file)
-    : init_(true), platform_file_(file) {
+    : init_(true), mixed_(true), platform_file_(file) {
 }
 
 bool File::Init(const std::wstring& name) {
