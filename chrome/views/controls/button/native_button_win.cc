5a6c75ad2682e148c704a3677373899fda2c76b7
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-21 00:50:26 +0000

    Add support for radio buttons. The code implementing checked/group was copied from the old radio button.
    Review URL: http://codereview.chromium.org/50080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12235 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/controls/button/native_button_win.cc
+++ chrome/views/controls/button/native_button_win.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "chrome/views/controls/button/checkbox2.h"
 #include "chrome/views/controls/button/native_button2.h"
+#include "chrome/views/controls/button/radio_button2.h"
 #include "chrome/views/widget/widget.h"
 
 namespace views {
