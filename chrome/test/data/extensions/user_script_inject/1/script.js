7435d8ca1526deb36d084a9edf41e675bc236ad3
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-21 16:32:40 +0000

    Committing issue 39299: Test for extension user script load and inject.
    This add a test that a user script actually gets injected into
    pages and runs. Before we only had unit tests for the various
    classes and weren't testing the system end-to-end.
    
    Review URL: http://codereview.chromium.org/51003
    Patch from Steve Krulewitz <skrulx@gmail.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12247 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/user_script_inject/1/script.js
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Modify the document title to indicate that this script has run.
+document.title = 'user script executed.';
