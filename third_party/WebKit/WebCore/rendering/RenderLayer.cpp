4f8c047e45da12e40fba4e8474e1f27c9383201f
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-22 18:43:32 +0000

    2009-03-22  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dan Bernstein
    
            https://bugs.webkit.org/show_bug.cgi?id=24743
    
            Fix hit testing regression from r41840. We need to pass the temporary
            HitTestResult when testing sublayers, then only copy to 'result' when
            the layer is known to have been hit.
    
            Test: fast/layers/zindex-hit-test.html
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::hitTestLayer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayer.cpp
@@ -2346,7 +2346,7 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
                 continue;
 
             HitTestResult tempResult(result.point());
-            RenderLayer* hitLayer = currLayer->hitTestLayer(rootLayer, this, request, result, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
+            RenderLayer* hitLayer = currLayer->hitTestLayer(rootLayer, this, request, tempResult, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
             if (isHitCandidate(hitLayer, depthSortDescendants, zOffset, unflattenedTransformState.get())) {
                 result = tempResult;
                 if (!depthSortDescendants)
@@ -2375,7 +2375,7 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
     if (m_negZOrderList) {
         for (int i = m_negZOrderList->size() - 1; i >= 0; --i) {
             HitTestResult tempResult(result.point());
-            RenderLayer* hitLayer = m_negZOrderList->at(i)->hitTestLayer(rootLayer, this, request, result, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
+            RenderLayer* hitLayer = m_negZOrderList->at(i)->hitTestLayer(rootLayer, this, request, tempResult, hitTestRect, hitTestPoint, false, localTransformState.get(), zOffsetForDescendantsPtr);
             if (isHitCandidate(hitLayer, depthSortDescendants, zOffset, unflattenedTransformState.get())) {
                 result = tempResult;
                 if (!depthSortDescendants)
