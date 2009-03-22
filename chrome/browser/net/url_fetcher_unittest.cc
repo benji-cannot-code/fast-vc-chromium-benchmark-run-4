bb4f27f02de6f83395539773586d32ce59c3a0d8
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-22 20:41:40 +0000

    Free unit tests for Mac: BookmarkHTMLWriterTest, BookmarkUtilsTest, DownloadRequestManagerTest, URLFetcher*Test, TimeFormat, RenderWidgetTest
    Review URL: http://codereview.chromium.org/50081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12269 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/url_fetcher_unittest.cc
+++ chrome/browser/net/url_fetcher_unittest.cc
@@ -334,8 +334,6 @@ void URLFetcherCancelTest::TestContextReleased() {
   io_loop_.PostTask(FROM_HERE, new MessageLoop::QuitTask());
 }
 
-}  // namespace.
-
 TEST_F(URLFetcherTest, SameThreadsTest) {
   // Create the fetcher on the main thread.  Since IO will happen on the main
   // thread, this will test URLFetcher's ability to do everything on one
@@ -463,3 +461,5 @@ TEST_F(URLFetcherCancelTest, ReleasesContext) {
 
   MessageLoop::current()->Run();
 }
+
+}  // namespace.
