cd2faa9d5592a59668ce5780141d7fe88bf78897
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-23 18:54:18 +0000

    2009-03-23  Glen Murphy  <glen@chromium.org>
            Reviewed by Darin Fisher.
    
            https://bugs.webkit.org/show_bug.cgi?id=24657
    
            Fix Skia drawing of highly scaled bitmaps; the conversion to
            IntRect produced visible layout test failures in highly scaled 
            coordinate systems.
    
            Test: svg/custom/image-small-width-height.svg
    
            * WebCore\platform\graphics\skia\ImageSkia.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/skia/ImageSkia.cpp
+++ third_party/WebKit/WebCore/platform/graphics/skia/ImageSkia.cpp
@@ -428,7 +428,7 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& dstRect,
     paintSkBitmap(ctxt->platformContext(),
                   *bm,
                   enclosingIntRect(normSrcRect),
-                  enclosingIntRect(normDstRect),
+                  normDstRect,
                   WebCoreCompositeToSkiaComposite(compositeOp));
 }
 
@@ -448,7 +448,7 @@ void BitmapImageSingleFrameSkia::draw(GraphicsContext* ctxt,
     paintSkBitmap(ctxt->platformContext(),
                   m_nativeImage,
                   enclosingIntRect(normSrcRect),
-                  enclosingIntRect(normDstRect),
+                  normDstRect,
                   WebCoreCompositeToSkiaComposite(compositeOp));
 }
 
