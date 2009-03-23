afef6cacfc05f827dec46f40362d75aa44ffcca7
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-23 23:36:56 +0000

    Disable the mini installer tests because they started failing at revision 12317.
    Review URL: http://codereview.chromium.org/49012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12325 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/mini_installer_test/test.cc
+++ chrome/test/mini_installer_test/test.cc
@@ -42,6 +42,8 @@ TEST_F(MiniInstallTest, MiniInstallerOverChromeMetaInstallerTest) {
   installer.OverInstall();
 }
 
+// These 2 tests started failing at revision 12317.
+#if 0
 TEST_F(MiniInstallTest, MiniInstallerSystemInstallTest) {
   if (win_util::GetWinVersion() < win_util::WINVERSION_VISTA) {
     ChromeMiniInstaller installer(mini_installer_constants::kSystemInstall);
@@ -55,6 +57,8 @@ TEST_F(MiniInstallTest, MiniInstallerUserInstallTest) {
   installer.InstallMiniInstaller(false,
       mini_installer_constants::kChromeMiniInstallerExecutable);
 }
+
+#endif
 
 TEST(InstallUtilTests, MiniInstallTestValidWindowsVersion) {
   // We run the tests on all supported OSes.
