810a983882efcea1d8225b5f90c09e101c703c99
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-23 22:15:58 +0000

    2009-03-23  Darin Adler  <darin@apple.com>
            Reviewed by Sam Weinig.
    
            * platform/KURL.h: Removed now-incorrect comments.
            None of the parts include the separator characters any more, now that
            query doesn't include the "?", so the comments explaining which do and do
            not are no-longer helpful.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41921 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/KURL.h
+++ third_party/WebKit/WebCore/platform/KURL.h
@@ -120,8 +120,8 @@ public:
     String pass() const;
     String path() const;
     String lastPathComponent() const;
-    String query() const; // Includes the "?".
-    String ref() const; // Does *not* include the "#".
+    String query() const;
+    String ref() const;
     bool hasRef() const;
 
     String prettyURL() const;
