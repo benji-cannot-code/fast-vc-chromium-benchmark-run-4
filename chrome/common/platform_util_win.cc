e578539408db691b20aa1f172baa08b0666fe78e
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-24 20:50:08 +0000

    fix win build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12396 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/platform_util_win.cc
+++ chrome/common/platform_util_win.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/platform_util.h"
 
 #include <atlbase.h>
+#include <shellapi.h>
 
 #include "base/file_path.h"
 #include "base/file_util.h"
