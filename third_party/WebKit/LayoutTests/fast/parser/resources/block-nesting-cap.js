31d4056ed838d30eb23ce855253bf63e63fa31ec
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-24 16:24:25 +0000

    WebCore:
            Reviewed by Darin Adler.
    
            - fix <rdar://problem/6107874> by capping the nesting depth of
              "block-level" elements generated by the parser
    
            Test: fast/parser/block-nesting-cap.html
    
            * html/HTMLParser.cpp:
            (WebCore::HTMLParser::HTMLParser):
            (WebCore::HTMLParser::insertNode):
            (WebCore::HTMLParser::pushBlock):
            (WebCore::HTMLParser::popOneBlockCommon):
            (WebCore::HTMLParser::freeBlock):
            * html/HTMLParser.h:
    
    LayoutTests:
    
            Reviewed by Darin Adler.
    
            - test for <rdar://problem/6107874>
    
            Test that the nesting depth of "block-level" elements generated by
            the parser is capped
    
            * fast/parser/block-nesting-cap-expected.txt: Added.
            * fast/parser/block-nesting-cap.html: Added.
            * fast/parser/resources/block-nesting-cap.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/parser/resources/block-nesting-cap.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('Test that the HTML parser does not allow the nesting depth of "block-level" elements to exceed 4096.');
+
+var depth = 4100;
+var markup = "";
+var i;
+for (i = 0; i < depth; ++i)
+    markup += "<div id='d" + i + "'>";
+var doc = document.implementation.createHTMLDocument();
+doc.body.innerHTML = markup;
+
+var d4094 = doc.getElementById("d4094");
+var d4095 = doc.getElementById("d4095");
+var d4096 = doc.getElementById("d4096");
+
+shouldBe("d4096.parentNode === d4094", "true");
+shouldBe("d4095.parentNode === d4094", "true");
+shouldBe("d4096.previousSibling === d4095", "true");
+
+var successfullyParsed = true;
