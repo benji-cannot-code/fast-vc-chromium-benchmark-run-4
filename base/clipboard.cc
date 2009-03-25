be1ca6a35479b3435332b01c035ac338f58a1080
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-25 19:46:55 +0000

    Validate clipboard bitmap size data.
    Review URL: http://codereview.chromium.org/53040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12484 0039d316-1c4b-4281-b951-d872f2087c98

--- base/clipboard.cc
+++ base/clipboard.cc
@@ -7,6 +7,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 
+namespace {
+
+// A compromised renderer could send us bad data, so validate it.
+bool IsBitmapSafe(const Clipboard::ObjectMapParams& params) {
+  const gfx::Size* size =
+      reinterpret_cast<const gfx::Size*>(&(params[1].front()));
+  return params[0].size() ==
+      static_cast<size_t>(size->width() * size->height() * 4);
+}
+
+}
+
 void Clipboard::DispatchObject(ObjectType type, const ObjectMapParams& params) {
   switch (type) {
     case CBF_TEXT:
@@ -41,6 +53,8 @@ void Clipboard::DispatchObject(ObjectType type, const ObjectMapParams& params) {
 
 #if defined(OS_WIN) || defined(OS_LINUX)
     case CBF_BITMAP:
+      if (!IsBitmapSafe(params))
+        return;
       WriteBitmap(&(params[0].front()), &(params[1].front()));
       break;
 #endif  // defined(OS_WIN) || defined(OS_LINUX)
