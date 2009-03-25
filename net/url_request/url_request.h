7940287bc98c4e17eb92b92a6d6369db2e2e5f08
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-25 18:14:29 +0000

    Prioritize which HTTP requests get a socket first by adding a priority level to various methods and classes.  Fix lint errors along the way.
    R=darin,wtc
    BUG=8993
    Review URL: http://codereview.chromium.org/42541
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12470 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request.h
+++ net/url_request/url_request.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/logging.h"
 #include "base/ref_counted.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/load_states.h"
@@ -16,16 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_status.h"
 
 namespace base {
-  class Time;
-}
+class Time;
+}  // namespace base
 
 namespace net {
-
 class IOBuffer;
 class UploadData;
 class X509Certificate;
-
-}
+}  // namespace net
 
 class URLRequestContext;
 class URLRequestJob;
@@ -432,12 +431,20 @@ class URLRequest {
   // Returns the expected content size if available
   int64 GetExpectedContentSize() const;
 
+  // Returns the priority level for this request.  A larger value indicates
+  // higher priority.  Negative values are not used.
+  int priority() const { return priority_; }
+  void set_priority(int priority) {
+    DCHECK_GE(priority, 0);
+    priority_ = priority;
+  }
+
  protected:
   // Allow the URLRequestJob class to control the is_pending() flag.
   void set_is_pending(bool value) { is_pending_ = value; }
 
   // Allow the URLRequestJob class to set our status too
-  void set_status(const URLRequestStatus &value) { status_ = value; }
+  void set_status(const URLRequestStatus& value) { status_ = value; }
 
   // Allow the URLRequestJob to redirect this request.  Returns net::OK if
   // successful, otherwise an error code is returned.
@@ -505,6 +512,10 @@ class URLRequest {
   // Cached value for use after we've orphaned the job handling the
   // first transaction in a request involving redirects.
   uint64 final_upload_progress_;
+
+  // The priority level for this request.  Objects like ClientSocketPool use
+  // this to determine which URLRequest to allocate sockets to first.
+  int priority_;
 
   DISALLOW_COPY_AND_ASSIGN(URLRequest);
 };
