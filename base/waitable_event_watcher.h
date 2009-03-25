b701863f109efbc9ddb3587bd783c9a954459250
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-25 17:16:22 +0000

    Revert "POSIX: allow WaitableEvents to be deleted while watching them."
    This reverts commit r12459 - it broke the world.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12462 0039d316-1c4b-4281-b951-d872f2087c98

--- base/waitable_event_watcher.h
+++ base/waitable_event_watcher.h
@@ -12,11 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/object_watcher.h"
 #else
 #include "base/message_loop.h"
-#include "base/waitable_event.h"
 #endif
 
 namespace base {
 
+class WaitableEvent;
 class Flag;
 class AsyncWaiter;
 class AsyncCallbackTask;
@@ -52,9 +52,6 @@ class AsyncCallbackTask;
 // occurs just before a WaitableEventWatcher is deleted. There is currently no
 // safe way to stop watching an automatic reset WaitableEvent without possibly
 // missing a signal.
-//
-// NOTE: you /are/ allowed to delete the WaitableEvent while still waiting on
-// it with a Watcher. It will act as if the event was never signaled.
 // -----------------------------------------------------------------------------
 
 class WaitableEventWatcher
@@ -144,7 +141,6 @@ class WaitableEventWatcher
   scoped_refptr<Flag> cancel_flag_;
   AsyncWaiter* waiter_;
   AsyncCallbackTask* callback_task_;
-  scoped_refptr<WaitableEvent::WaitableEventKernel> kernel_;
 #endif
 };
 
