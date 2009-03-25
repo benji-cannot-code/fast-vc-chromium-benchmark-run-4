fd297fadced2d04b97648b5409a53a8dfa13341d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-25 22:14:57 +0000

    Reviewed by Darin Adler.
            Move ASSERT(foo) to before where foo-> is used
            https://bugs.webkit.org/show_bug.cgi?id=24684
    
            * platform/chromium/PasteboardChromium.cpp:
            (WebCore::Pasteboard::writeImage):
              Move ASSERTs to the top of the function, code was using "node" before ASSERT(node), once I moved
              ASSERT(node) to the top of the function it made sense to move the rest of them there too.
            * rendering/SVGRenderSupport.cpp:
            (WebCore::prepareToRenderSVGContent):
              move ASSERT(object) to *before* we use "object" :)
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@41990 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/SVGRenderSupport.cpp
+++ third_party/WebKit/WebCore/rendering/SVGRenderSupport.cpp
@@ -47,9 +47,9 @@ void prepareToRenderSVGContent(RenderObject* object, RenderObject::PaintInfo& pa
     UNUSED_PARAM(rootFilter);
 #endif
 
+    ASSERT(object);
     SVGElement* svgElement = static_cast<SVGElement*>(object->node());
     ASSERT(svgElement && svgElement->document() && svgElement->isStyled());
-    ASSERT(object);
 
     SVGStyledElement* styledElement = static_cast<SVGStyledElement*>(svgElement);
     const RenderStyle* style = object->style();
