d5d99536e72eb96a01937c5661e9b7295f7ac914
sky@google.com <sky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-25 19:42:39 +0000

    Fixes crash that occurred because we weren't correctly disposing of the native HWND for buttons.
    BUG=9301
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/42609
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12481 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/controls/native_control_win.cc
+++ chrome/views/controls/native_control_win.cc
@@ -28,6 +28,14 @@ NativeControlWin::NativeControlWin() : HWNDView() {
 }
 
 NativeControlWin::~NativeControlWin() {
+  HWND hwnd = GetHWND();
+  if (hwnd) {
+    // Destroy the hwnd if it still exists. Otherwise we won't have shut things
+    // down correctly, leading to leaking and crashing if another message
+    // comes in for the hwnd.
+    Detach();
+    DestroyWindow(hwnd);
+  }
 }
 
 LRESULT NativeControlWin::ProcessMessage(UINT message, WPARAM w_param,
