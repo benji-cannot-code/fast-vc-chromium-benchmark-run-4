b771198a694914af6171f084d1249f2053c0a555
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-25 20:55:42 +0000

    Revert "Fix/punt on a bunch of NOTIMPLEMENTEDs."
    This reverts commit r12489.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_window_gtk.cc
+++ chrome/browser/gtk/browser_window_gtk.cc
@@ -365,9 +365,7 @@ void BrowserWindowGtk::UpdateLoadingAnimations(bool should_animate) {
 }
 
 void BrowserWindowGtk::SetStarredState(bool is_starred) {
-  // Hide our current lack of starring support from NOTIMPLEMENTED spew.
-  if (is_starred)
-    NOTIMPLEMENTED();
+  NOTIMPLEMENTED();
 }
 
 gfx::Rect BrowserWindowGtk::GetNormalBounds() const {
