aafc45b5849407a6c28d4dda5ebd211508a40009
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-26 18:43:26 +0000

    Hook up ctrl+tab and ctrl+shift+tab for switching tabs in linux
    We can't just add accelerators for these because GDK_Tab and GDK_ISO_Left_Tab are not valid accelerator keys:
    http://www.google.com/codesearch/p?hl=en#ycarM8Ghiog/gtk+-2.4.0/gtk/gtkaccelgroup.c&q=gtk_accel_groups_activate&l=805
    
    Instead, handle these keys in keypress and forward the event on to BrowserWindowGtk::OnAccelerator directly.  We stash a pointer to BrowserWindowGtk on the GtkWindow.
    
    Review URL: http://codereview.chromium.org/42611
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12581 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_window_gtk.h
+++ chrome/browser/gtk/browser_window_gtk.h
@@ -33,6 +33,9 @@ class BrowserWindowGtk : public BrowserWindow,
   explicit BrowserWindowGtk(Browser* browser);
   virtual ~BrowserWindowGtk();
 
+  // Process a keyboard input and try to find an accelerator for it.
+  void HandleAccelerator(guint keyval, GdkModifierType modifier);
+
   // Overridden from BrowserWindow
   virtual void Show();
   virtual void SetBounds(const gfx::Rect& bounds);
@@ -109,11 +112,11 @@ class BrowserWindowGtk : public BrowserWindow,
   static gboolean OnContentAreaExpose(GtkWidget* widget, GdkEventExpose* e,
                                       BrowserWindowGtk* window);
 
-  static gboolean OnAccelerator(GtkAccelGroup* accel_group,
-                                GObject* acceleratable,
-                                guint keyval,
-                                GdkModifierType modifier,
-                                BrowserWindowGtk* browser_window);
+  static gboolean OnGtkAccelerator(GtkAccelGroup* accel_group,
+                                   GObject* acceleratable,
+                                   guint keyval,
+                                   GdkModifierType modifier,
+                                   BrowserWindowGtk* browser_window);
 
   // A small shim for browser_->ExecuteCommand.
   void ExecuteBrowserCommand(int id);
