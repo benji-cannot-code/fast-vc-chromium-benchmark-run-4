ef268296cf3d244a43f752cb380f997432127190
idanan@chromium.org <idanan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-26 19:58:51 +0000

    Issue 21268: The RemakeSee http://codereview.chromium.org/21268 for the original issue which was partially reverted due to a possible regression. Issue 8819 was created to track the part of the original fix which was reverted.A double-click come as an event which follows a single-click. The core of this change is to makesure that a double-click arrives at the object which handled the single-click which preceeded it.If it does not, then the double-click is transformed into a single-click at the dispatch level.The tricky part is that a single-click and the double-click which follows it may not both be clientor non-client. So, all double-clicks (client or non-client) are now centrally handled and replacedwhen needed by second single-clicks.Note that the behavior which was reverted was to discard a double-click rather than reducing it toa single-click. The bug filed as a regression involved using a double-middle-click as two single-clicks when destroying compressed tabs sequentially.BUG=8819 Review URL: http://codereview.chromium.org/50017
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12590 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/widget/widget_win.cc
+++ chrome/views/widget/widget_win.cc
@@ -521,7 +521,7 @@ void WidgetWin::OnKeyUp(TCHAR c, UINT rep_cnt, UINT flags) {
 }
 
 void WidgetWin::OnLButtonDown(UINT flags, const CPoint& point) {
-  ProcessMousePressed(point, flags | MK_LBUTTON, false);
+  ProcessMousePressed(point, flags | MK_LBUTTON, false, false);
 }
 
 void WidgetWin::OnLButtonUp(UINT flags, const CPoint& point) {
@@ -529,11 +529,11 @@ void WidgetWin::OnLButtonUp(UINT flags, const CPoint& point) {
 }
 
 void WidgetWin::OnLButtonDblClk(UINT flags, const CPoint& point) {
-  ProcessMousePressed(point, flags | MK_LBUTTON, true);
+  ProcessMousePressed(point, flags | MK_LBUTTON, true, false);
 }
 
 void WidgetWin::OnMButtonDown(UINT flags, const CPoint& point) {
-  ProcessMousePressed(point, flags | MK_MBUTTON, false);
+  ProcessMousePressed(point, flags | MK_MBUTTON, false, false);
 }
 
 void WidgetWin::OnMButtonUp(UINT flags, const CPoint& point) {
@@ -541,7 +541,7 @@ void WidgetWin::OnMButtonUp(UINT flags, const CPoint& point) {
 }
 
 void WidgetWin::OnMButtonDblClk(UINT flags, const CPoint& point) {
-  ProcessMousePressed(point, flags | MK_MBUTTON, true);
+  ProcessMousePressed(point, flags | MK_MBUTTON, true, false);
 }
 
 LRESULT WidgetWin::OnMouseActivate(HWND window, UINT hittest_code,
@@ -583,11 +583,11 @@ LRESULT WidgetWin::OnMouseRange(UINT msg, WPARAM w_param, LPARAM l_param) {
 }
 
 void WidgetWin::OnNCLButtonDblClk(UINT flags, const CPoint& point) {
-  SetMsgHandled(FALSE);
+  SetMsgHandled(ProcessMousePressed(point, flags | MK_LBUTTON, true, true));
 }
 
 void WidgetWin::OnNCLButtonDown(UINT flags, const CPoint& point) {
-  SetMsgHandled(FALSE);
+  SetMsgHandled(ProcessMousePressed(point, flags | MK_LBUTTON, false, true));
 }
 
 void WidgetWin::OnNCLButtonUp(UINT flags, const CPoint& point) {
@@ -595,11 +595,11 @@ void WidgetWin::OnNCLButtonUp(UINT flags, const CPoint& point) {
 }
 
 void WidgetWin::OnNCMButtonDblClk(UINT flags, const CPoint& point) {
-  SetMsgHandled(FALSE);
+  SetMsgHandled(ProcessMousePressed(point, flags | MK_MBUTTON, true, true));
 }
 
 void WidgetWin::OnNCMButtonDown(UINT flags, const CPoint& point) {
-  SetMsgHandled(FALSE);
+  SetMsgHandled(ProcessMousePressed(point, flags | MK_MBUTTON, false, true));
 }
 
 void WidgetWin::OnNCMButtonUp(UINT flags, const CPoint& point) {
@@ -624,11 +624,11 @@ LRESULT WidgetWin::OnNCMouseMove(UINT flags, const CPoint& point) {
 }
 
 void WidgetWin::OnNCRButtonDblClk(UINT flags, const CPoint& point) {
-  SetMsgHandled(FALSE);
+  SetMsgHandled(ProcessMousePressed(point, flags | MK_RBUTTON, true, true));
 }
 
 void WidgetWin::OnNCRButtonDown(UINT flags, const CPoint& point) {
-  SetMsgHandled(FALSE);
+  SetMsgHandled(ProcessMousePressed(point, flags | MK_RBUTTON, false, true));
 }
 
 void WidgetWin::OnNCRButtonUp(UINT flags, const CPoint& point) {
@@ -654,7 +654,7 @@ void WidgetWin::OnPaint(HDC dc) {
 }
 
 void WidgetWin::OnRButtonDown(UINT flags, const CPoint& point) {
-  ProcessMousePressed(point, flags | MK_RBUTTON, false);
+  ProcessMousePressed(point, flags | MK_RBUTTON, false, false);
 }
 
 void WidgetWin::OnRButtonUp(UINT flags, const CPoint& point) {
@@ -662,7 +662,7 @@ void WidgetWin::OnRButtonUp(UINT flags, const CPoint& point) {
 }
 
 void WidgetWin::OnRButtonDblClk(UINT flags, const CPoint& point) {
-  ProcessMousePressed(point, flags | MK_RBUTTON, true);
+  ProcessMousePressed(point, flags | MK_RBUTTON, true, false);
 }
 
 void WidgetWin::OnSettingChange(UINT flags, const wchar_t* section) {
@@ -713,16 +713,21 @@ void WidgetWin::TrackMouseEvents(DWORD mouse_tracking_flags) {
   }
 }
 
-bool WidgetWin::ProcessMousePressed(const CPoint& point, UINT flags,
-                                    bool dbl_click) {
+bool WidgetWin::ProcessMousePressed(const CPoint& point,
+                                    UINT flags,
+                                    bool dbl_click,
+                                    bool non_client) {
   last_mouse_event_was_move_ = false;
   // Windows gives screen coordinates for nonclient events, while the RootView
   // expects window coordinates; convert if necessary.
   gfx::Point converted_point(point);
+  if (non_client)
+    View::ConvertPointToView(NULL, root_view_.get(), &converted_point);
   MouseEvent mouse_pressed(Event::ET_MOUSE_PRESSED,
                            converted_point.x(),
                            converted_point.y(),
                            (dbl_click ? MouseEvent::EF_IS_DOUBLE_CLICK : 0) |
+                           (non_client ? MouseEvent::EF_IS_NON_CLIENT : 0) |
                            Event::ConvertWindowsFlags(flags));
   if (root_view_->OnMousePressed(mouse_pressed)) {
     is_mouse_down_ = true;
