0cb99ccc446b92331b2bfa855517209bcb497fed
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-26 18:18:38 +0000

    Remove SafariThemeConstants.h, which is now provided by WebKitSupportLibrary
            * win/include/SafariTheme: Removed.
            * win/include/SafariTheme/SafariThemeConstants.h: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42008 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitLibraries/win/include/SafariTheme/SafariThemeConstants.h
+++
@@ -1,2 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// This is a placeholder header used in WebCore/rendering/RenderThemeSafari.h until a version of WebKitSupportLibrary containing this header is released
