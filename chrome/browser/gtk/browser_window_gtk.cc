9e211e8d571d6218e0ad513fc1b6d621dba19611
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-26 23:10:06 +0000

    More NOTIMPLEMENTEDs into bugs.
    Review URL: http://codereview.chromium.org/42668
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12615 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_window_gtk.cc
+++ chrome/browser/gtk/browser_window_gtk.cc
@@ -387,7 +387,8 @@ void BrowserWindowGtk::UpdateTitleBar() {
 }
 
 void BrowserWindowGtk::UpdateLoadingAnimations(bool should_animate) {
-  NOTIMPLEMENTED();
+  // Need to implement loading animations.
+  // http://code.google.com/p/chromium/issues/detail?id=9380
 }
 
 void BrowserWindowGtk::SetStarredState(bool is_starred) {
@@ -424,7 +425,8 @@ void BrowserWindowGtk::SetFocusToLocationBar() {
 }
 
 void BrowserWindowGtk::UpdateStopGoState(bool is_loading) {
-  NOTIMPLEMENTED();
+  // Need to implement stop/go button.
+  // http://code.google.com/p/chromium/issues/detail?id=9381
 }
 
 void BrowserWindowGtk::UpdateToolbar(TabContents* contents,
