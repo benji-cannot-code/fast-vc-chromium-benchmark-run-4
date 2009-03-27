788d65d29bef4cd95baa47e1aa7d3980fa12f0cc
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-27 20:36:21 +0000

    Remove redundant attributes and functions from IDL files that have the         same attributes and functions defined in the super class.
            Reviewed by Simon Fraser.
    
            * WebCore.xcodeproj/project.pbxproj: Remove DOMHTMLBodyElementPrivate.h.
            * bindings/objc/PublicDOMInterfaces.h: Remove focus and blur from DOMHTMLElement.
            * html/HTMLBodyElement.idl: Remove scrollLeft, scrollTop, scrollWidth
            and scrollHeight.
            * html/HTMLElement.idl: Remove blur and focus.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/PublicDOMInterfaces.h
+++ third_party/WebKit/WebCore/bindings/objc/PublicDOMInterfaces.h
@@ -475,8 +475,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(readonly) BOOL isContentEditable;
 @property(readonly, copy) NSString *titleDisplayString AVAILABLE_WEBKIT_VERSION_3_0_AND_LATER;
 @property int tabIndex;
-- (void)blur;
-- (void)focus;
 @end
 
 @interface DOMHTMLEmbedElement : DOMHTMLElement WEBKIT_VERSION_1_3
