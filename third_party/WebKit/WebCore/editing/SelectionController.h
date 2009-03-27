4b9af647a2abf065be9a5940725c7e85a812c255
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-27 23:23:09 +0000

    2009-03-27  Xiaomei Ji  <xji@chromium.org>
            Reviewed by Dan Bernstein.
    
            Fix https://bugs.webkit.org/show_bug.cgi?id=24303
            Using keyboard select RTL text, Highlights goes to opposite direction from Firefox and IE.
    
            Test: editing/selection/extend-selection.html
    
            * editing/SelectionController.cpp:
            (WebCore::SelectionController::directionOfEnclosingBlock): Added
            (WebCore::SelectionController::modifyExtendingRight): Added. Currenctly 
            implemented for character and word granularity, all other granularities
            are treated as "forward".
            (WebCore::SelectionController::modifyExtendingForward): Renamed 
            modifyExtendingRightForward() to this.
            (WebCore::SelectionController::modifyExtendingLeft): Added. Currenctly
            implemented for character and word granularity, all other granularities
            are treated as "backward".
            (WebCore::SelectionController::modifyExtendingBackward): Renamed
            modifyExtendingLeftBackward() to this.
            (WebCore::SelectionController::modify): Change to call either the 
            left/right or backward/forward methods depending on the 'dir' argument
            for extends.
            * editing/SelectionController.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42054 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/SelectionController.h
+++ third_party/WebKit/WebCore/editing/SelectionController.h
@@ -131,10 +131,14 @@ public:
 private:
     enum EPositionType { START, END, BASE, EXTENT };
 
-    VisiblePosition modifyExtendingRightForward(TextGranularity);
+    TextDirection directionOfEnclosingBlock();
+
+    VisiblePosition modifyExtendingRight(TextGranularity);
+    VisiblePosition modifyExtendingForward(TextGranularity);
     VisiblePosition modifyMovingRight(TextGranularity);
     VisiblePosition modifyMovingForward(TextGranularity);
-    VisiblePosition modifyExtendingLeftBackward(TextGranularity);
+    VisiblePosition modifyExtendingLeft(TextGranularity);
+    VisiblePosition modifyExtendingBackward(TextGranularity);
     VisiblePosition modifyMovingLeft(TextGranularity);
     VisiblePosition modifyMovingBackward(TextGranularity);
 
@@ -188,3 +192,4 @@ void showTree(const WebCore::SelectionController*);
 #endif
 
 #endif // SelectionController_h
+
