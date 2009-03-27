3a1b7d4754f2a45e6e0968de695e241ca9f56cc3
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-27 16:55:39 +0000

    Fix interactive_ui_tests failures by temporarily allowing direct dns lookup.
    Review URL: http://codereview.chromium.org/56010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12662 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_focus_uitest.cc
+++ chrome/browser/browser_focus_uitest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/message_loop.h"
+#include "base/ref_counted.h"
 #include "chrome/browser/automation/ui_controls.h"
 #include "chrome/browser/browser.h"
 #include "chrome/browser/dom_operation_notification_details.h"
@@ -21,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/views/window/window.h"
 #include "chrome/test/in_process_browser_test.h"
 #include "chrome/test/ui_test_utils.h"
-
+#include "net/base/host_resolver_unittest.h"
 
 namespace {
 
@@ -36,9 +37,19 @@ const wchar_t kTypicalPage[] = L"files/focus/typical_page.html";
 class BrowserFocusTest : public InProcessBrowserTest {
  public:
   BrowserFocusTest() {
+    host_mapper_ = new net::RuleBasedHostMapper();
+    // TODO(phajdan.jr): Don't make a real dns lookup here.
+    // page_with_focus.html has a reference to google.com.
+    host_mapper_->AllowDirectLookup("*.google.com");
+    scoped_host_mapper_.Init(host_mapper_.get());
+
     set_show_window(true);
     EnableDOMAutomation();
   }
+
+private:
+  scoped_refptr<net::RuleBasedHostMapper> host_mapper_;
+  net::ScopedHostMapper scoped_host_mapper_;
 };
 
 class JavaScriptRunner : public NotificationObserver {
