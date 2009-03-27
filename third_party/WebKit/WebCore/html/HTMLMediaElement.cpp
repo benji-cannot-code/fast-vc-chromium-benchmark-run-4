59736f2cc392ab0591c651bdde44a82b3bbadf5f
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-27 04:56:56 +0000

    2009-03-26  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Simon Fraser.
    
            https://bugs.webkit.org/show_bug.cgi?id=24874
            24874: HTMLMediaElement: 'duration' defaults to NaN, deal with it
            
            The 'duration' attribute is NaN when no media is available, so the
            media element should take care when comparing with duration().
    
            * html/HTMLMediaElement.cpp:
            (WebCore::HTMLMediaElement::mediaPlayerTimeChanged): Don't compare with duration when
            it is NaN.
            (WebCore::HTMLMediaElement::endedPlayback): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLMediaElement.cpp
@@ -1128,7 +1128,7 @@ void HTMLMediaElement::mediaPlayerTimeChanged(MediaPlayer*)
     
     float now = currentTime();
     float dur = duration();
-    if (now >= dur) {
+    if (!isnan(dur) && dur && now >= dur) {
         if (loop()) {
             ExceptionCode ignoredException;
             m_sentEndEvent = false;
@@ -1225,7 +1225,11 @@ bool HTMLMediaElement::potentiallyPlaying() const
 
 bool HTMLMediaElement::endedPlayback() const
 {
-    return m_player && m_readyState >= HAVE_METADATA && currentTime() >= duration() && !loop();
+    if (!m_player || m_readyState < HAVE_METADATA)
+        return false;
+    
+    float dur = duration();
+    return !isnan(dur) && currentTime() >= dur && !loop();
 }
 
 bool HTMLMediaElement::stoppedDueToErrors() const
