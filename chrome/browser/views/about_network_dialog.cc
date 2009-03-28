388f4252dd011afa183be4d3e32f4bb89a2831e8
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-28 22:06:33 +0000

    Fix crash that occurs when the about:network window is closed while still tracking network jobs.
    BUG=9446
    R=brettw
    
    Review URL: http://codereview.chromium.org/45072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12752 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/about_network_dialog.cc
+++ chrome/browser/views/about_network_dialog.cc
@@ -274,6 +274,7 @@ AboutNetworkDialog::AboutNetworkDialog() : tracking_(false) {
 
 AboutNetworkDialog::~AboutNetworkDialog() {
   active_dialog = NULL;
+  tracker->StopTracking();
   tracker->Release();
   tracker = NULL;
 }
