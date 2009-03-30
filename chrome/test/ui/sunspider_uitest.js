922b1f54b192a6fae41194312da3b1b2f2a879c6
patrick@chromium.org <patrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-30 23:28:32 +0000

    - Add UI test for the V8 Benchmark Suite. - Modify the V8 Benchmark Suite to work with the UI test framework. - Add utils for JavaScript UI tests. - Move automation.js files for SunSpider and V8 Benchmark to the   src/chrome/test/ui directory. - Overload UITest::PrintResult for convenience.
    BUG=9255
    Review URL: http://codereview.chromium.org/42628
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12823 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/sunspider_uitest.js
+++ chrome/test/ui/sunspider_uitest.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   found in the LICENSE file.
 */
 
-// Automation utilities.
+// Automation utilities for running SunSpider as a UI test.
 
 function Automation() {
 }
