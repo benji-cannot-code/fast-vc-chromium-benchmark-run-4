fadd3a73d59cb76f8a68b0693b99f06905816927
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-30 18:47:53 +0000

    The NativeButton HWND would not be created yet when we were setting the initial focus in the first run bubble view. This CL makes sure to set the focus when everything is set-up.
    BUG=9387
    TEST=Run chrome to show the first run bubble (you can use the --first-run parameter). When Chrome comes-up, it shows the "Search from here" info bubble. The focus should be on the "Keep Google..." button.
    Review URL: http://codereview.chromium.org/55019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12790 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/first_run_bubble.cc
+++ chrome/browser/views/first_run_bubble.cc
@@ -100,6 +100,10 @@ class FirstRunBubbleView : public views::View,
     AddChildView(change_button_);
   }
 
+  void BubbleShown() {
+    keep_button_->RequestFocus();
+  }
+
   // Overridden from ButtonListener.
   virtual void ButtonPressed(views::Button* sender) {
     bubble_window_->Close();
@@ -152,11 +156,6 @@ class FirstRunBubbleView : public views::View,
     keep_button_->SetBounds(change_button_->x() - pref_size.width() -
                             kRelatedButtonHSpacing, change_button_->y(),
                             pref_size.width(), pref_size.height());
-  }
-
-  virtual void ViewHierarchyChanged(bool is_add, View* parent, View* child) {
-    if (keep_button_)
-      keep_button_->RequestFocus();
   }
 
   // Overridden from views::View.
@@ -238,6 +237,7 @@ FirstRunBubble* FirstRunBubble::Show(Profile* profile, HWND parent_hwnd,
   views::FocusManager* focus_manager =
       views::FocusManager::GetFocusManager(window->GetNativeView());
   focus_manager->AddFocusChangeListener(view);
+  view->BubbleShown();
   return window;
 }
 
