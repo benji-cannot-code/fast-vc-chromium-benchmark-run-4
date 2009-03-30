5d909b9bb15b84517c8ea46e77c858c21220832a
yurys@google.com <yurys@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-30 12:37:08 +0000

    1. Added 'scripts' command response handler that populates Scripts panel with script sources. 2. Modify WebDevToolsClientImpl::JsAddSourceToFrame so that it returns false when fails. Review URL: http://codereview.chromium.org/56058
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12769 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/inspector_controller_impl.js
+++ webkit/glue/devtools/js/inspector_controller_impl.js
@@ -46,8 +46,7 @@ devtools.InspectorControllerImpl.prototype.addSourceToFrame =
   if (!element.id) {
     element.id = "f" + this.frame_element_id_++; 
   }
-  DevToolsHost.addSourceToFrame(mimeType, source, element.id);
-  return true;
+  return DevToolsHost.addSourceToFrame(mimeType, source, element.id);
 };
 
 
