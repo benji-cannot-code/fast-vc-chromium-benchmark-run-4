89f300366bbc8cc1fce4ef0b207889be16037a7d
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-30 12:29:29 +0000

    DevTools: support null callbacks in the frontend, encapsulate node ids.
    Review URL: http://codereview.chromium.org/57004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12768 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/inspector_controller_impl.js
+++ webkit/glue/devtools/js/inspector_controller_impl.js
@@ -78,7 +78,7 @@ devtools.InspectorControllerImpl.prototype.hideDOMNodeHighlight = function() {
  */
 devtools.InspectorControllerImpl.prototype.highlightDOMNode =
     function(hoveredNode) {
-  RemoteToolsAgent.HighlightDOMNode(hoveredNode.id);
+  RemoteToolsAgent.HighlightDOMNode(hoveredNode.id_);
 };
 
 
