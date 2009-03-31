56beb2e3dfd153f380839241a1a56895a464d89b
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-03-31 20:24:49 +0000

    2009-03-31  Darin Adler  <darin@apple.com>
            Reviewed by Adele Peterson.
    
            <rdar://problem/6740581> REGRESSION (r41793): Page Down and Page Up don’t work in Leopard Mail
    
            * WebView/WebHTMLView.mm:
            (-[WebResponderChainSink tryToPerform:with:]): Added. Without this we would think we had
            handled an event when we actually hadn't. Specifically, when -[WebFrameView scrollPageDown:]
            calls tryToPerform on the next responder.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42140 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
@@ -6099,4 +6099,10 @@ static void extractUnderlines(NSAttributedString *string, Vector<CompositionUnde
     _receivedUnhandledCommand = YES;
 }
 
+- (BOOL)tryToPerform:(SEL)action with:(id)object
+{
+    _receivedUnhandledCommand = YES;
+    return YES;
+}
+
 @end
