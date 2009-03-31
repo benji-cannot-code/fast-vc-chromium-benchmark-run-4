f1660da54cc984edc19961e199d250e04fe22e23
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-31 20:18:17 +0000

    OCD fixing of style for the initializer list of ExtensionView. Review URL: http://codereview.chromium.org/56113
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12901 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_view.cc
+++ chrome/browser/extensions/extension_view.cc
@@ -13,9 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/browser_resources.h"
 #include "grit/generated_resources.h"
 
-ExtensionView::ExtensionView(
-    Extension* extension, const GURL& url, Profile* profile) :
-    HWNDHtmlView(url, this, false), extension_(extension), profile_(profile) {
+ExtensionView::ExtensionView(Extension* extension,
+                             const GURL& url,
+                             Profile* profile)
+    : HWNDHtmlView(url, this, false),
+      extension_(extension),
+      profile_(profile) {
   // TODO(mpcomplete): query this from the renderer somehow?
   set_preferred_size(gfx::Size(100, 100));
 }
