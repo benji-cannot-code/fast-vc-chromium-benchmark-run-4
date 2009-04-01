6595a26bdd9c86d9ad094ad75b6008564a016fa2
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-01 07:36:58 +0000

    Trying to fix flaky error in CopyTreeWorkItemTest.NewNameAndCopyTest
    BUG=9086
    
    Review URL: http://codereview.chromium.org/56145
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12944 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/copy_tree_work_item_unittest.cc
+++ chrome/installer/util/copy_tree_work_item_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/logging.h"
 #include "base/path_service.h"
+#include "base/platform_thread.h"
 #include "base/process_util.h"
 #include "base/scoped_ptr.h"
 #include "base/string_util.h"
@@ -60,6 +61,19 @@ namespace {
     ASSERT_TRUE(file.is_open());
     file << contents;
     file.close();
+  }
+
+  bool IsFileInUse(std::wstring path) {
+    if (!file_util::PathExists(path))
+      return false;
+
+    HANDLE handle = ::CreateFile(path.c_str(), FILE_ALL_ACCESS,
+                                 NULL, NULL, OPEN_EXISTING, NULL, NULL);
+    if (handle  == INVALID_HANDLE_VALUE)
+      return true;
+
+    CloseHandle(handle);
+    return false;
   }
 
   // Simple function to read text from a file.
@@ -447,6 +461,10 @@ TEST_F(CopyTreeWorkItemTest, NewNameAndCopyTest) {
   work_item.reset(WorkItem::CreateCopyTreeWorkItem(
       file_name_from, file_name_to, temp_dir_, WorkItem::NEW_NAME_IF_IN_USE,
       alternate_to));
+  if (IsFileInUse(file_name_to))
+    PlatformThread::Sleep(2000);
+  // If file is still in use, the rest of the test will fail.
+  ASSERT_FALSE(IsFileInUse(file_name_to));
   EXPECT_TRUE(work_item->Do());
 
   EXPECT_TRUE(file_util::PathExists(file_name_from));
