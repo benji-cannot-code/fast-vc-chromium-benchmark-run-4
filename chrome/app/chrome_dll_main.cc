a36222a66b7827b4d8a676994518b7e26ef4e492
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-02 15:02:35 +0000

    Don't fatal error when GTK fails to load a 64-bit IME.
    This prevents crash on startup if you have IMEs configured.
    
    Patch from Fumitoshi Ukai.
    
    BUG=9643
    
    Review URL: http://codereview.chromium.org/56176
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13011 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_dll_main.cc
+++ chrome/app/chrome_dll_main.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_LINUX)
+#include <string.h>
 #include <gtk/gtk.h>
 #endif
 
@@ -160,16 +161,24 @@ bool IncorrectChromeHtmlArguments(const std::wstring& command_line) {
 #endif  // OS_WIN
 
 #if defined(OS_LINUX)
-static void GLibFatalLogHandler(const gchar* log_domain,
-                                GLogLevelFlags log_level,
-                                const gchar* message,
-                                gpointer userdata) {
+static void GLibLogHandler(const gchar* log_domain,
+                           GLogLevelFlags log_level,
+                           const gchar* message,
+                           gpointer userdata) {
   if (!log_domain)
     log_domain = "<unknown>";
   if (!message)
     message = "<no message>";
 
-  LOG(FATAL) << log_domain << ": " << message;
+  // http://code.google.com/p/chromium/issues/detail?id=9643
+  // Until we have a real 64-bit build or all of these 32-bit package issues
+  // are sorted out, don't fatal on ELF 32/64-bit mismatch warnings.
+  if (strstr(message, "Loading IM context type") ||
+      strstr(message, "wrong ELF class: ELFCLASS64")) {
+    LOG(ERROR) << "Bug 9643: " << log_domain << ": " << message;
+  } else {
+    LOG(FATAL) << log_domain << ": " << message;
+  }
 }
 
 static void SetUpGLibLogHandler() {
@@ -182,7 +191,7 @@ static void SetUpGLibLogHandler() {
                                                   G_LOG_LEVEL_ERROR |
                                                   G_LOG_LEVEL_CRITICAL |
                                                   G_LOG_LEVEL_WARNING),
-                      GLibFatalLogHandler,
+                      GLibLogHandler,
                       NULL);
   }
 }
