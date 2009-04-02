d2e87a4240d03aa6564ab9d0eedd16e449ba8c8d
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-02 20:07:15 +0000

    Bug 2220 lists two cases where the inactive highlighting doesn't reappear on FindNext. This is a simple fix for one of those cases (user presses Refresh after Find).
    BUG=2220
    TEST=Open google.com, press Find, press e, press Refresh, press F3 (or FindNext button in UI). Then make sure the inactive matches are highlighted (not just the active match).
    Review URL: http://codereview.chromium.org/56193
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13028 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/web_contents.h
+++ chrome/browser/tab_contents/web_contents.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -211,6 +211,11 @@ class WebContents : public TabContents,
   bool find_ui_active() const { return find_ui_active_; }
   void set_find_ui_active(bool find_ui_active) {
       find_ui_active_ = find_ui_active;
+  }
+
+  // Setter for find_op_aborted_.
+  void set_find_op_aborted(bool find_op_aborted) {
+    find_op_aborted_ = find_op_aborted;
   }
 
   // Used _only_ by testing to set the current request ID, since it calls
@@ -682,7 +687,9 @@ class WebContents : public TabContents,
 
   // True if a Find operation was aborted. This can happen if the Find box is
   // closed or if the search term inside the Find box is erased while a search
-  // is in progress.
+  // is in progress. This can also be set if a page has been reloaded, and will
+  // on FindNext result in a full Find operation so that the highlighting for
+  // inactive matches can be repainted.
   bool find_op_aborted_;
 
   // Each time a search request comes in we assign it an id before passing it
