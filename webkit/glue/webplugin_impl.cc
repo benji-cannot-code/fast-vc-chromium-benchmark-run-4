e5adfafd3a849e01013b5e5b8bee657841e23891
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-03 18:28:05 +0000

    WebKit merge 42132:42199 (Chrome side)
    Account for a FrameLoader method that was renamed.
    
    R=amanda
    
    Review URL: http://codereview.chromium.org/62011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13095 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_impl.cc
+++ webkit/glue/webplugin_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EventNames.h"
 #include "FloatPoint.h"
 #include "FormData.h"
+#include "FormState.h"
 #include "FocusController.h"
 #include "Frame.h"
 #include "FrameLoader.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameTree.h"
 #include "FrameView.h"
 #include "GraphicsContext.h"
+#include "HTMLFormElement.h"
 #include "HTMLNames.h"
 #include "HTMLPlugInElement.h"
 #include "IntRect.h"
@@ -515,13 +517,12 @@ RoutingStatus WebPluginImpl::RouteToFrame(const char *method,
   WebCore::FrameLoader *loader = frame()->loader();
   // we actually don't know whether usergesture is true or false,
   // passing true since all we can do is assume it is okay.
-  loader->loadFrameRequestWithFormAndValues(
+  loader->loadFrameRequest(
       load_request,
       false,  // lock history
       false,  // lock back forward list
       0,      // event
-      0,      // form element
-      HashMap<WebCore::String, WebCore::String>());
+      0);     // form state
 
   // load() can cause the frame to go away.
   if (webframe_) {
