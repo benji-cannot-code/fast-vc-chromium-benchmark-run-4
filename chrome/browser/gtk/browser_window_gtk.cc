e5f1ed57ad721fc2a3d97b7831ed3b0926f4ba87
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-03 21:11:41 +0000

    Gtk: Insert a border at the bottom of the toolbar.
    Review URL: http://codereview.chromium.org/62021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13109 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_window_gtk.cc
+++ chrome/browser/gtk/browser_window_gtk.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_paths_linux.h"
 #include "base/command_line.h"
+#include "base/gfx/gtk_util.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/path_service.h"
@@ -33,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/theme_resources.h"
 
 namespace {
+
+const GdkColor kBorderColor = GDK_COLOR_RGB(0xbe, 0xc8, 0xd4);
 
 class DummyButtonListener : public views::ButtonListener {
  public:
@@ -185,6 +188,13 @@ BrowserWindowGtk::BrowserWindowGtk(Browser* browser)
   toolbar_.reset(new BrowserToolbarGtk(browser_.get()));
   toolbar_->Init(browser_->profile(), window_);
   toolbar_->AddToolbarToBox(content_vbox_);
+
+  // Insert a border between the toolbar and the web contents.
+  GtkWidget* border = gtk_event_box_new();
+  gtk_widget_set_size_request(border, -1, 1);
+  gtk_widget_modify_bg(border, GTK_STATE_NORMAL, &kBorderColor);
+  gtk_box_pack_start(GTK_BOX(content_vbox_), border, FALSE, FALSE, 0);
+  gtk_widget_show(border);
 
   FindBarGtk* find_bar_gtk = new FindBarGtk();
   find_bar_controller_.reset(new FindBarController(find_bar_gtk));
