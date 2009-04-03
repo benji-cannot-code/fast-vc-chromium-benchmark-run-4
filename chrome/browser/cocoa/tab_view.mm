38b8994dc21c8485251115e6de8e397b1b59c5df
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-03 20:36:21 +0000

    Add some hysterisis to tab dragging on Mac.
    http://code.google.com/p/chromium/issues/detail?id=9670
    
    Review URL: http://codereview.chromium.org/60072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13104 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_view.mm
+++ chrome/browser/cocoa/tab_view.mm
@@ -74,6 +74,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   BOOL dragging = YES;
   BOOL moved = NO;
 
+  // Do not start dragging until the user has "torn" the tab off by
+  // moving more than 3 pixels.
+  BOOL torn = NO;
+  static const double kDragStartDistance = 3.0;
+
   NSDate* targetDwellDate = nil; // The date this target was first chosen
   NSMutableArray* targets = [NSMutableArray array];
 
@@ -86,6 +91,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                            untilDate:[NSDate distantFuture]
                               inMode:NSDefaultRunLoopMode dequeue:YES];
     NSPoint thisPoint = [NSEvent mouseLocation];
+
+    // TODO(alcor): Pinkerton indicated that cole is adding a more
+    // formal concept of "magnetism" to tab detachment, which would
+    // be an alternative solution.
+    if (!torn) {
+      double dx = thisPoint.x - lastPoint.x;
+      double dy = thisPoint.y - lastPoint.y;
+
+      if (dx * dx + dy * dy < kDragStartDistance * kDragStartDistance
+	  && [theEvent type] == NSLeftMouseDragged) {
+	continue;
+      }
+      torn = YES;
+    }
 
     // Find all the windows that could be a target. It has to be of the
     // appropriate class, and visible (obviously).
