fd07b72464d58165e26f3c5cb4095556e4f2c607
kdecker@apple.com <kdecker@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-03 18:16:05 +0000

    Reviewed by Darin.
            *  loader/ProgressTracker.cpp: Included DocumentLoader.h
            (WebCore::ProgressTracker::incrementProgress): Limit the 50%-clamp rule to 
            documents that use WebCore's layout system.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42204 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/ProgressTracker.cpp
+++ third_party/WebKit/WebCore/loader/ProgressTracker.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "ProgressTracker.h"
 
+#include "DocumentLoader.h"
 #include "Frame.h"
 #include "FrameLoader.h"
 #include "FrameLoaderClient.h"
@@ -197,8 +198,11 @@ void ProgressTracker::incrementProgress(unsigned long identifier, const char*, i
     else
         percentOfRemainingBytes = 1.0;
     
-    // Treat the first layout as the half-way point.
-    double maxProgressValue = m_originatingProgressFrame->loader()->firstLayoutDone() ? finalProgressValue : .5;
+    // For documents that use WebCore's layout system, treat first layout as the half-way point.
+    // FIXME: The hasHTMLView function is a sort of roundabout way of asking "do you use WebCore's layout system".
+    bool useClampedMaxProgress = m_originatingProgressFrame->loader()->client()->hasHTMLView()
+        && !m_originatingProgressFrame->loader()->firstLayoutDone();
+    double maxProgressValue = useClampedMaxProgress ? 0.5 : finalProgressValue;
     increment = (maxProgressValue - m_progressValue) * percentOfRemainingBytes;
     m_progressValue += increment;
     m_progressValue = min(m_progressValue, maxProgressValue);
