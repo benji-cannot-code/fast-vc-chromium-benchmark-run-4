43276d598de8ee58692a422a762e72de40aabce0
dimich@chromium.org <dimich@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-03 22:24:39 +0000

    2009-04-03  Jian Li  <jianli@chromium.org>
            Reviewed by Darin Fisher.
    
            https://bugs.webkit.org/show_bug.cgi?id=25009
            Upstream changes to WorkerContextExecutionProxy for V8 bindings in order to use V8EventListenerList as container.
    
            * bindings/v8/WorkerContextExecutionProxy.cpp:
            (WebCore::WorkerContextExecutionProxy::WorkerContextExecutionProxy):
            (WebCore::WorkerContextExecutionProxy::dispose):
            (WebCore::WorkerContextExecutionProxy::FindOrCreateEventListener):
            (WebCore::WorkerContextExecutionProxy::RemoveEventListener):
            * bindings/v8/WorkerContextExecutionProxy.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42215 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/WorkerContextExecutionProxy.h
+++ third_party/WebKit/WebCore/bindings/v8/WorkerContextExecutionProxy.h
@@ -36,7 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(WORKERS)
 
 #include <v8.h>
+#include "V8EventListenerList.h"
 #include "V8Index.h"
+#include <wtf/OwnPtr.h>
 #include <wtf/Vector.h>
 
 namespace WebCore {
@@ -96,7 +98,7 @@ namespace WebCore {
         v8::Persistent<v8::Context> m_context;
         int m_recursion;
 
-        Vector<V8WorkerContextEventListener*> m_listeners;
+        OwnPtr<V8EventListenerList> m_listeners;
         Vector<Event*> m_events;
     };
 
