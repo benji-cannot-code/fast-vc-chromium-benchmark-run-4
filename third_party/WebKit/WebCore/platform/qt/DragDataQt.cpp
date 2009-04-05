adcc7208b4d690a5be7cfe7f62986180e1b596c3
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-05 17:26:41 +0000

    2009-04-05  Erik L. Bunce  <elbunce@xendom.com>
            Reviewed by Simon Hausmann.
    
            https://bugs.webkit.org/show_bug.cgi?id=25050
    
            Fix an assert failure when dropping an 'empty' text/uri-list on a QWebView.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42234 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/DragDataQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/DragDataQt.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008, 2009 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -127,6 +127,10 @@ String DragData::asURL(String* title) const
     if (!m_platformDragData)
         return String();
     QList<QUrl> urls = m_platformDragData->urls();
+
+    if (urls.isEmpty())
+        return String();
+
     return urls.first().toString();
 }
     
