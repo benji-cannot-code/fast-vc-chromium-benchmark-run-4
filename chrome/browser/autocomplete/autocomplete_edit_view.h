df8d8f96cdae6f320e0ec2da86c288863af41e12
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-06 22:51:11 +0000

    Initial implemention of Mac Omnibox.
    AutocompletePopupViewMac implements AutocompletePopupView in terms of a bare NSWindow containing an NSTableView.  AutocompleteTableTarget implements an Obj-C class to bridge from appkit callbacks back to the popup view (and from there to the model which contains the data it needs).
    
    AutocompleteEditViewMac implements AutocompleteEditView in terms of an NSTextField, which is passed down from a nib owner.  It works with the popup view to make sure the popup is positioned correctly.  AutocompleteFieldDelegate is an internal Obj-C class to bridge from appkit callbacks back to the edit view (and then the edit model).
    
    LocationBarViewMac implements LocationBar for interacting with the rest of the browser, and AutocompleteEditController for managing the edit and popup views.  It is mostly placeholder code stolen from the gtk implementation.
    
    ---
    
    I've tried to implement an amount of code which worked and was useful, but which didn't drag on and on into the future.  So no tab to search or hints or anything, sometimes ugly, selection may be funky, etc.
    
    Review URL: http://codereview.chromium.org/50074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13201 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view.h
+++ chrome/browser/autocomplete/autocomplete_edit_view.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // This file defines the interface class AutocompleteEditView.  Each toolkit
-// will implement the edit view differently, so that code is inheriently
+// will implement the edit view differently, so that code is inherently
 // platform specific.  However, the AutocompleteEditModel needs to do some
 // communication with the view.  Since the model is shared between platforms,
 // we need to define an interface that all view implementations will share.
--- chrome/browser/cocoa/tab_contents_controller.h
+++ chrome/browser/cocoa/tab_contents_controller.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class BookmarkModel;
 class CommandUpdater;
 class LocationBar;
+class LocationBarViewMac;
 class TabContents;
 class TabContentsCommandObserver;
 class TabStripModel;
@@ -38,7 +39,7 @@ class ToolbarModel;
  @private
   CommandUpdater* commands_;  // weak, may be nil
   TabContentsCommandObserver* observer_;  // nil if |commands_| is nil
-  LocationBar* locationBarBridge_;
+  LocationBarViewMac* locationBarView_;
   TabContents* contents_;  // weak
 
   ToolbarModel* toolbarModel_;  // weak, one per window
@@ -103,6 +104,8 @@ class ToolbarModel;
 // Called to update the loading state. Handles updating the go/stop button
 // state.
 - (void)setIsLoading:(BOOL)isLoading;
+
+- (void)defocusLocationBar;
 
 // Make the location bar the first responder, if possible.
 - (void)focusLocationBar;
