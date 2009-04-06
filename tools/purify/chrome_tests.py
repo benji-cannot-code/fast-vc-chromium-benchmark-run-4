df4ab1d51d93d82c20dfdf2db30fcfc90776b80a
bradnelson@google.com <bradnelson@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-06 04:52:27 +0000

    Redirecting buildbots to use v8_shell.exe instead of v8_shell_sample.exe
    Review URL: http://codereview.chromium.org/62049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13151 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/purify/chrome_tests.py
+++ tools/purify/chrome_tests.py
@@ -125,7 +125,7 @@ class ChromeTests:
       self._data_dir = os.path.join(module_dir, "test", "data", "purify")
     else:
       self._data_dir = os.path.join(module_dir, "data", "purify")
-      
+
     cmd = list(self._command_preamble)
     cmd.append("--data_dir=%s" % self._data_dir)
     cmd.append("--report_dir=%s" % self._report_dir)
@@ -345,7 +345,7 @@ class ChromeTests:
                              multi=True)
 
   def TestV8(self):
-    shell = "v8_shell_sample.exe"
+    shell = "v8_shell.exe"
     # We need to compute _build_dir early to in order to pass in the
     # shell path as an argument to the test script.
     self.ComputeBuildDir("chrome", shell)
@@ -403,4 +403,3 @@ def _main(argv):
 if __name__ == "__main__":
   ret = _main(sys.argv)
   sys.exit(ret)
-
