47cd6b5035baa45e260aac6f3dbc7a8b15c5aa5e
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-07 17:57:11 +0000

    2009-04-07  Anders Carlsson  <andersca@apple.com>
            Try to fix the Tiger build.
    
            * Plugins/WebBaseNetscapePluginView.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42272 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebBaseNetscapePluginView.mm
+++ third_party/WebKit/WebKit/mac/Plugins/WebBaseNetscapePluginView.mm
@@ -644,9 +644,9 @@ using namespace WebCore;
 @end
 
 
-#ifndef BUILDING_ON_TIGER
 namespace WebKit {
 
+#ifndef BUILDING_ON_TIGER
 CString proxiesForURL(NSURL *url)
 {
     RetainPtr<CFDictionaryRef> systemProxies(AdoptCF, CFNetworkCopySystemProxySettings());
@@ -704,6 +704,7 @@ CString proxiesForURL(NSURL *url)
     
     return proxies.utf8();
 }
+#endif
 
 bool getAuthenticationInfo(const char* protocolStr, const char* hostStr, int32_t port, const char* schemeStr, const char* realmStr,
                            CString& username, CString& password)
@@ -748,7 +749,6 @@ bool getAuthenticationInfo(const char* protocolStr, const char* hostStr, int32_t
 }
 
 } // namespace WebKit
-#endif
 
 #endif //  ENABLE(NETSCAPE_PLUGIN_API)
 
