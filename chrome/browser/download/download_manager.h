439af7f2ea0052fcd665896cbb1b729e327b8cb3
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-07 21:21:12 +0000

    Cloned from 62030
    Add a "Clear All" link to the downloads page.
    
    Fixes jankiness of the blue bar in the download page as it loads.
    
    Improve rendering time of the downloads page by chunking the number of
    download items to do before scheduling the timeout.
    
    BUG=9033
    
    Review URL: http://codereview.chromium.org/62115
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13285 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_manager.h
+++ chrome/browser/download/download_manager.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -355,6 +355,10 @@ class DownloadManager : public base::RefCountedThreadSafe<DownloadManager>,
   // the same or more recent than |remove_begin|. The number of downloads
   // deleted is returned back to the caller.
   int RemoveDownloads(const base::Time remove_begin);
+
+  // Remove all downloads will delete all downloads. The number of downloads
+  // deleted is returned back to the caller.
+  int RemoveAllDownloads();
 
   // Download the object at the URL. Used in cases such as "Save Link As..."
   void DownloadUrl(const GURL& url,
