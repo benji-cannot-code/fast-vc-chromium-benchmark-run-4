d2545343567a2fd2979808a8ca0c9b97c05b4835
cpu@google.com <cpu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-08 00:20:32 +0000

    Greatly reduce the race window in ProcessSingleton - fix only for windows - Anantha has a good test to be commited RSN
    BUG=9593
    
    Review URL: http://codereview.chromium.org/57082
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13313 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/process_singleton_win.cc
+++ chrome/browser/process_singleton_win.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Checks the visiblilty of the enumerated window and signals once a visible
+// Checks the visibility of the enumerated window and signals once a visible
 // window has been found.
 BOOL CALLBACK BrowserWindowEnumeration(HWND window, LPARAM param) {
   bool* result = reinterpret_cast<bool*>(param);
@@ -34,14 +34,15 @@ BOOL CALLBACK BrowserWindowEnumeration(HWND window, LPARAM param) {
 
 }  // namespace
 
+// Look for a Chrome instance that uses the same profile directory.
 ProcessSingleton::ProcessSingleton(const FilePath& user_data_dir)
-    : window_(NULL),
-      locked_(false) {
-  // Look for a Chrome instance that uses the same profile directory:
-  remote_window_ = FindWindowEx(HWND_MESSAGE,
-                                NULL,
-                                chrome::kMessageWindowClass,
+    : window_(NULL), locked_(false) {
+  // FindWindoEx and Create() should be one atomic operation in order to not
+  // have a race condition.
+  remote_window_ = FindWindowEx(HWND_MESSAGE, NULL, chrome::kMessageWindowClass,
                                 user_data_dir.ToWStringHack().c_str());
+  if (!remote_window_)
+    Create();
 }
 
 ProcessSingleton::~ProcessSingleton() {
@@ -127,9 +128,14 @@ bool ProcessSingleton::NotifyOtherProcess() {
   return false;
 }
 
+// For windows, there is no need to call Create() since the call is made in
+// the constructor but to avoid having more platform specific code in
+// browser_main.cc we tolerate a second call which will do nothing.
 void ProcessSingleton::Create() {
-  DCHECK(!window_);
   DCHECK(!remote_window_);
+  if (window_)
+    return;
+
   HINSTANCE hinst = GetModuleHandle(NULL);
 
   WNDCLASSEX wc = {0};
