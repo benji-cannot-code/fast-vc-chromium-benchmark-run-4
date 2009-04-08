9bd9a9f1c636b684e08ceee1e6535bd44e13e647
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-08 07:02:06 +0000

    Reviewed by Maciej Stachowiak.
            https://bugs.webkit.org/show_bug.cgi?id=24957
            REGRESSION: Google calendar widget no longer shows upcoming events
    
            Test: http/tests/xmlhttprequest/authorization-header.html
    
            * xml/XMLHttpRequest.cpp:
            (WebCore::XMLHttpRequestStaticData::XMLHttpRequestStaticData): Removed Authorization from
            the list of forbidden headers.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42314 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
+++ third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
@@ -67,7 +67,6 @@ XMLHttpRequestStaticData::XMLHttpRequestStaticData()
     m_forbiddenRequestHeaders.add("accept-encoding");
     m_forbiddenRequestHeaders.add("access-control-request-headers");
     m_forbiddenRequestHeaders.add("access-control-request-method");
-    m_forbiddenRequestHeaders.add("authorization");
     m_forbiddenRequestHeaders.add("connection");
     m_forbiddenRequestHeaders.add("content-length");
     m_forbiddenRequestHeaders.add("content-transfer-encoding");
