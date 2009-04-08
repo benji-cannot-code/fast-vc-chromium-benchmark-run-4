44f1e64d0d9b53c48efb7a508e3cfc333e82c33b
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-08 05:29:28 +0000

    Land the current build.webkit.org configuration for safekeeping.
    * BuildSlaveSupport/build.webkit.org-config/README: Added.
    * BuildSlaveSupport/build.webkit.org-config/Makefile:
    * BuildSlaveSupport/build.webkit.org-config/buildbot.tac:
    * BuildSlaveSupport/build.webkit.org-config/config.json: Added.
    * BuildSlaveSupport/build.webkit.org-config/master.cfg:
    * BuildSlaveSupport/build.webkit.org-config/public_html/buildbot.css: Renamed from BuildSlaveSupport/build.webkit.org-config/buildbot.css.
    * BuildSlaveSupport/build.webkit.org-config/public_html/index.html: Added.
    * BuildSlaveSupport/build.webkit.org-config/public_html/robots.txt: Added.
    * BuildSlaveSupport/build.webkit.org-config/webkit/__init__.py: Removed.
    * BuildSlaveSupport/build.webkit.org-config/webkit/auth.py: Removed.
    * BuildSlaveSupport/build.webkit.org-config/webkit/basesteps.py: Removed.
    * BuildSlaveSupport/build.webkit.org-config/webkit/builders.py: Removed.
    * BuildSlaveSupport/build.webkit.org-config/webkit/factories.py: Removed.
    * BuildSlaveSupport/build.webkit.org-config/webkit/schedulers.py: Removed.
    * BuildSlaveSupport/build.webkit.org-config/webkit/status.py: Removed.
    * BuildSlaveSupport/build.webkit.org-config/webkit/steps.py: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/BuildSlaveSupport/build.webkit.org-config/webkit/__init__.py
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-from webkit import auth, status, builders, schedulers
-reload(auth)
-reload(status)
-reload(builders)
-reload(schedulers)
