5b2bd776cdc4f7d54dba49791ed8bbd88fcac6aa
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-09 21:36:05 +0000

    Use two CustomDrawButtonBases to draw ToolbarStarToggleGtk.
    Review URL: http://codereview.chromium.org/67012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13463 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/toolbar_star_toggle_gtk.h
+++ chrome/browser/gtk/toolbar_star_toggle_gtk.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gtk/gtk.h>
 
 #include "base/basictypes.h"
+#include "chrome/browser/gtk/custom_button.h"
+#include "chrome/common/owned_widget_gtk.h"
 
 class GURL;
 
@@ -23,7 +25,7 @@ class ToolbarStarToggleGtk {
 
   void SetStarred(bool starred);
 
-  GtkWidget* widget() const { return widget_; }
+  GtkWidget* widget() const { return widget_.get(); }
 
  private:
   // Callback for expose, used to draw the custom graphics.
@@ -31,16 +33,13 @@ class ToolbarStarToggleGtk {
                            ToolbarStarToggleGtk* obj);
 
   // The actual button widget.
-  GtkWidget* widget_;
+  OwnedWidgetGtk widget_;
 
   // Whether we show the yellow star.
-  bool starred_;
+  bool is_starred_;
 
-  // Unstarred images
-  GdkPixbuf* unstarred_pixbuf_[GTK_STATE_INSENSITIVE + 1];
-
-  // Starred images
-  GdkPixbuf* starred_pixbuf_[GTK_STATE_INSENSITIVE + 1];
+  CustomDrawButtonBase unstarred_;
+  CustomDrawButtonBase starred_;
 
   DISALLOW_EVIL_CONSTRUCTORS(ToolbarStarToggleGtk);
 };
