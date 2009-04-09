7415f975e1fba232bd34c556e88d10e6904f6772
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-09 22:40:05 +0000

    WebCore:
    2009-04-09  Darin Adler  <darin@apple.com>
    
            Reviewed by Anders Carlsson and Sam Weinig.
    
            Part of <rdar://problem/5438063> Saving history containing 100,000 entries causes pauses of 2s while browsing
    
            Longer term solution is to change the design so Safari doesn't read and write all of history.
            This patch is step one: Do the serializing, which is done on the main thread, much faster.
    
            * WebCore.base.exp: Added new entry points.
            * WebCore.xcodeproj/project.pbxproj: Added new source files.
    
            * history/cf: Added.
    
            * history/cf/HistoryPropertyList.cpp: Added.
            * history/cf/HistoryPropertyList.h: Added. Code to write history files. In the future we'll also
            have code for reading here too.
    
            * platform/cf/BinaryPropertyList.cpp: Added.
            * platform/cf/BinaryPropertyList.h: Added. Code to write binary property list files.
    
    WebKit/mac:
    
    2009-04-09  Darin Adler  <darin@apple.com>
    
            Reviewed by Anders Carlsson and Sam Weinig.
    
            Part of <rdar://problem/5438063> Saving history containing 100,000 entries causes pauses of 2s while browsing
    
            Longer term solution is to change the design so Safari doesn't read and write all of history.
            This patch is step one: Do the serializing, which is done on the main thread, much faster.
    
            * History/WebHistory.mm:
            (-[WebHistoryPrivate data]): Added. Returns the NSData object containing serialized history.
            For creating new SPI so you can get the data in memory instead of on disk. Uses WebHistoryWriter.
            (-[WebHistoryPrivate saveToURL:error:]): Changed to call [self data
            (-[WebHistory _data]): Added.
            (WebHistoryWriter::WebHistoryWriter): Added.
            (WebHistoryWriter::writeHistoryItems): Added.
    
            * History/WebHistoryPrivate.h: Added a new _data method.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42372 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/History/WebHistoryPrivate.h
+++ third_party/WebKit/WebKit/mac/History/WebHistoryPrivate.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2005, 2008, 2009 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -47,5 +47,12 @@ extern NSString *WebHistoryItemsDiscardedWhileLoadingNotification;
     @result Returns an array of all WebHistoryItems in WebHistory, in an undefined order.
 */
 - (NSArray *)allItems;
+
+/*!
+    @method _data
+    @result A data object with the entire history in the same format used by the saveToURL:error: method.
+*/
+- (NSData *)_data;
+
 
 @end
