e1c9aff9b172ecee7efc06b196d38e25ae99db58
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-09 14:39:32 +0000

    DevTools: Cache resource metainfo even when debugger agent is not attached. - Cache 200 resources - Hide agent functionality behind the flag Review URL: http://codereview.chromium.org/62183
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13419 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/net_agent.js
+++ webkit/glue/devtools/js/net_agent.js
@@ -22,8 +22,6 @@ devtools.NetAgent = function() {
       goog.bind(this.didReceiveResponse, this);
   RemoteNetAgent.DidFinishLoading =
       goog.bind(this.didFinishLoading, this);
-  RemoteNetAgent.DidFailLoading =
-      goog.bind(this.didFailLoading, this);
 };
 
 
@@ -139,20 +137,5 @@ devtools.NetAgent.prototype.didFinishLoading = function(identifier, value) {
   }
   resource.endTime = value.endTime;
   resource.finished = true;
-  resource.failed = false;
-};
-
-
-/**
- * @see NetAgentDelegate.
- * {@inheritDoc}.
- */
-devtools.NetAgent.prototype.didFailLoading = function(identifier, value) {
-  var resource = this.resources_[identifier];
-  if (!resource) {
-    return;
-  }
-  resource.endTime = value.endTime;
-  resource.finished = false;
-  resource.failed = true;
+  resource.failed = !!value.errorCode;
 };
