cb63b01f0bd3e0f36da45c11060ced6fef49926e
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-09 16:55:37 +0000

    Skia: Fix skia compile error with newer gcc versions.
    http://codereview.chromium.org/63048
    
    (by Craig Schlenter)
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13428 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ports/SkFontHost_FreeType.cpp
+++ skia/ports/SkFontHost_FreeType.cpp
@@ -269,6 +269,7 @@ static void unref_ft_face(FT_Face face) {
     SkASSERT("shouldn't get here, face not in list");
 }
 
+#ifdef SKIA_HARFBUZZ
 void SkFaceRec::setupShaper(HB_ShaperItem* item) {
     if (!fHBFace) {
         fHBFace = HB_NewFace(fFace, hb_freetype_table_sfnt_get);
@@ -289,6 +290,7 @@ void SkFaceRec::setupShaper(HB_ShaperItem* item) {
 void SkFaceRec::releaseShaper(HB_ShaperItem* item) {
     fRefCnt--;
 }
+#endif
 
 ///////////////////////////////////////////////////////////////////////////
 
