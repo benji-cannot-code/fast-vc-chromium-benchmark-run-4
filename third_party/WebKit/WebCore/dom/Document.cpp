1af51bca50ba4fb3b5bb6e5988ca6a24808a6c81
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-09 22:08:57 +0000

    Removed duplicate header include.
    git-svn-id: svn://svn.chromium.org/blink/trunk@42370 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -100,7 +100,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderView.h"
 #include "RenderWidget.h"
 #include "ScriptController.h"
-#include "ScriptController.h"
 #include "SecurityOrigin.h"
 #include "SegmentedString.h"
 #include "SelectionController.h"
