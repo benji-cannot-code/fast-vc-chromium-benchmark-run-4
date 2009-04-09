91ad1aa5e6cf01cc5b83ab41951bfb1e887a32b1
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-09 20:40:06 +0000

    This CL adds a confirmation box when closing the browser with in-progress downloads.
    BUG=1028
    TEST=Start several bug downloads, close the browser. A dialog shows up to warn you are about to lose the downloads. Try the wait and continue button. Try again with several windows, and in Incognito mode.
    Review URL: http://codereview.chromium.org/62131
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13453 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_window_cocoa.h
+++ chrome/browser/cocoa/browser_window_cocoa.h
@@ -66,6 +66,7 @@ class BrowserWindowCocoa : public BrowserWindow {
   virtual void ShowPasswordManager();
   virtual void ShowSelectProfileDialog();
   virtual void ShowNewProfileDialog();
+  virtual void ConfirmBrowserCloseWithPendingDownloads();
   virtual void ShowHTMLDialog(HtmlDialogUIDelegate* delegate,
                               void* parent_window);
 
