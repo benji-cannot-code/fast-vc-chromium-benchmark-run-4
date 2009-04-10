a5cbfd4cca77c72e40426d1ae6ac7e262b420c49
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-10 21:44:48 +0000

    Fixed https://bugs.webkit.org/show_bug.cgi?id=25134
            This both fixes a bug where I was not sending in the right time
            to continue the animation and was not recomputing current time
            when coming out of suspend.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42406 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/AnimationController.cpp
+++ third_party/WebKit/WebCore/page/animation/AnimationController.cpp
@@ -205,6 +205,8 @@ bool AnimationControllerPrivate::isAnimatingPropertyOnRenderer(RenderObject* ren
 
 void AnimationControllerPrivate::suspendAnimations(Document* document)
 {
+    setBeginAnimationUpdateTime(cBeginAnimationUpdateTimeNotSet);
+    
     RenderObjectAnimationMap::const_iterator animationsEnd = m_compositeAnimations.end();
     for (RenderObjectAnimationMap::const_iterator it = m_compositeAnimations.begin(); it != animationsEnd; ++it) {
         RenderObject* renderer = it->first;
@@ -218,6 +220,8 @@ void AnimationControllerPrivate::suspendAnimations(Document* document)
 
 void AnimationControllerPrivate::resumeAnimations(Document* document)
 {
+    setBeginAnimationUpdateTime(cBeginAnimationUpdateTimeNotSet);
+    
     RenderObjectAnimationMap::const_iterator animationsEnd = m_compositeAnimations.end();
     for (RenderObjectAnimationMap::const_iterator it = m_compositeAnimations.begin(); it != animationsEnd; ++it) {
         RenderObject* renderer = it->first;
