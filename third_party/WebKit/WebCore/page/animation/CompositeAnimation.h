2b361d1db03ca931e479d4d91d9ec53127b044f3
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-10 23:19:00 +0000

    2009-04-10  Chris Marrin  <cmarrin@apple.com>
            Reviewed by Dan Bernstein
    
            https://bugs.webkit.org/show_bug.cgi?id=25108
    
            If you remove a class with a transition while that transition is running
            the animation timer will continue to fire after the transition is finished.
            This has no visual indication, but it does drain the processor. And in some
            cases it might even cause a glitch in future animations. Unfortunately there
            is no way to test this without putting in printfs.
    
            This happens because the animation logic is never traversed after a transition
            is removed, so we never get a chance to cleanup. So I added cleanup in the logic
            that fires the dispatch of the last style change when the animation finishes.
    
            Test: transitions/remove-transition-style.html
    
            * page/animation/AnimationController.cpp:
            (WebCore::AnimationControllerPrivate::updateStyleIfNeededDispatcherFired):
            * page/animation/CompositeAnimation.cpp:
            (WebCore::CompositeAnimation::updateTransitions):
            (WebCore::CompositeAnimation::animate):
            (WebCore::CompositeAnimation::cleanupFinishedAnimations):
            * page/animation/CompositeAnimation.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/CompositeAnimation.h
+++ third_party/WebKit/WebCore/page/animation/CompositeAnimation.h
@@ -75,7 +75,7 @@ public:
     
     PassRefPtr<KeyframeAnimation> getAnimationForProperty(int property) const;
 
-    void cleanupFinishedAnimations(RenderObject*);
+    void cleanupFinishedAnimations();
 
     void overrideImplicitAnimations(int property);
     void resumeOverriddenImplicitAnimations(int property);
