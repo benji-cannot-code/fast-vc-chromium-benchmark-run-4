df216fca1fbf8808cbbee294c49d7426491364a5
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-10 01:17:25 +0000

    Windows part 1 of <rdar://problem/5438063> Saving history containing 100,000 entries causes pauses of 2s while browsing
    WebCore:
    
            Start compiling HistoryPropertyList on Windows
    
            Reviewed by Darin Adler.
    
            * WebCore.vcproj/WebCore.vcproj: Added HistoryPropertyList and its
            dependencies to the project. Copy headers from history/cf to
            $WebKitOutputDir.
            * history/cf/HistoryPropertyList.cpp: Added StringExtras.h so that
            this file will compile on Windows.
    
    WebKit/win:
    
            Windows part 1 of <rdar://problem/5438063> Saving history containing
            100,000 entries causes pauses of 2s while browsing
    
            Use HistoryPropertyListWriter to implement IWebHistory::saveToURL.
    
            Reviewed by Darin Adler.
    
            * WebHistory.cpp:
            (WebHistoryWriter::WebHistoryWriter): Added.
            (WebHistoryWriter::writeHistoryItems): Added. Loop backwards through
            the entries for each date, and pass the corresponding
            WebCore::HistoryItem to writeHistoryItem.
            (WebHistory::saveHistoryGuts): Use WebHistoryWriter to serialize
            m_entriesByDate, then write the serialized data to disk using
            CFWriteStream.
    
            * WebHistory.h: Removed datesArray.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42381 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/history/cf/HistoryPropertyList.cpp
+++ third_party/WebKit/WebCore/history/cf/HistoryPropertyList.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HistoryPropertyList.h"
 
 #include "HistoryItem.h"
+#include <wtf/StringExtras.h>
 
 namespace WebCore {
 
