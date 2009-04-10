34e86165041a08eedfbbf0b0334f82699bde02e7
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-10 16:38:19 +0000

    Let WebKit draw the active highlight for FindInPage, instead of using the SelectionController.
    This fixes a couple of bugs, such as: 
    
    Active match highlighting disappears when mouse selection
    is made within the page and selection color is orange (6145).
    
    and 
    
    Selection color becomes orange in some cases on tabs that
    don't have a Find box open.
    
    and
    
    Gmail jumping to the first match if on FindNext if Find box
    is closed and reopened (9795).
    
    NOTE: This patch depends on my patch to WebKit that just
    got accepted. See:
    https://bugs.webkit.org/show_bug.cgi?id=25102
    
    BUG=6145, 9795
    TEST=Find something on a page that results in multiple
    matches being found. Highlight another word that is not
    currently highlighted. You should see both active and 
    inactive highlights (orange and yellow) and a blue colored
    selection highlight.
    
    Review URL: http://codereview.chromium.org/66016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13510 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webframe_impl.cc
+++ webkit/glue/webframe_impl.cc
@@ -971,18 +971,26 @@ bool WebFrameImpl::Find(int request_id,
 
   if (!options.findNext)
     frame()->page()->unmarkAllTextMatches();
+  else
+    SetMarkerActive(active_match_.get(), false);  // Active match is changing.
 
   // Starts the search from the current selection.
   bool start_in_selection = true;
+
+  // If the user has selected something since the last Find operation we want
+  // to start from there. Otherwise, we start searching from where the last Find
+  // operation left off (either a Find or a FindNext operation).
+  VisibleSelection selection(frame()->selection()->selection());
+  if (selection.isNone() && active_match_) {
+    selection = VisibleSelection(active_match_.get());
+    frame()->selection()->setSelection(selection);
+  }
 
   DCHECK(frame() && frame()->view());
   bool found = frame()->findString(webcore_string, options.forward,
                                    options.matchCase, wrap_within_frame,
                                    start_in_selection);
   if (found) {
-#if defined(OS_WIN)
-    WebCore::RenderThemeChromiumWin::setFindInPageMode(true);
-#endif
     // Store which frame was active. This will come in handy later when we
     // change the active match ordinal below.
     WebFrameImpl* old_active_frame = main_frame_impl->active_match_frame_;
@@ -1004,6 +1012,8 @@ bool WebFrameImpl::Find(int request_id,
     } else {
       active_match_ = new_selection.toNormalizedRange();
       curr_selection_rect = active_match_->boundingBox();
+      SetMarkerActive(active_match_.get(), true);  // Active.
+      ClearSelection();  // WebKit draws the highlighting for all matches.
     }
 
     if (!options.findNext) {
@@ -1114,7 +1124,7 @@ void WebFrameImpl::InvalidateIfNecessary() {
   }
 }
 
-void WebFrameImpl::AddMarker(WebCore::Range* range) {
+void WebFrameImpl::AddMarker(WebCore::Range* range, bool active_match) {
   // Use a TextIterator to visit the potentially multiple nodes the range
   // covers.
   TextIterator markedText(range);
@@ -1126,7 +1136,8 @@ void WebFrameImpl::AddMarker(WebCore::Range* range) {
         WebCore::DocumentMarker::TextMatch,
         textPiece->startOffset(exception),
         textPiece->endOffset(exception),
-        "" };
+        "",
+        active_match };
 
     if (marker.endOffset > marker.startOffset) {
       // Find the node to add a marker to and add it.
@@ -1145,6 +1156,13 @@ void WebFrameImpl::AddMarker(WebCore::Range* range) {
           range->boundingBox());
     }
   }
+}
+
+void WebFrameImpl::SetMarkerActive(WebCore::Range* range, bool active) {
+  if (!range)
+    return;
+
+  frame()->document()->setMarkersActive(range, active);
 }
 
 void WebFrameImpl::ScopeStringMatches(int request_id,
@@ -1243,8 +1261,6 @@ void WebFrameImpl::ScopeStringMatches(int request_id,
     if (frame()->editor()->insideVisibleArea(result_range.get())) {
       ++match_count;
 
-      AddMarker(result_range.get());
-
       setStart(search_range.get(), new_start);
       Node* shadow_tree_root = search_range->shadowTreeRootNode();
       if (search_range->collapsed(ec) && shadow_tree_root)
@@ -1265,9 +1281,11 @@ void WebFrameImpl::ScopeStringMatches(int request_id,
       // match was found in active_selection_rect_ on the current frame. If we
       // find this rect during scoping it means we have found the active
       // tickmark.
+      bool found_active_match = false;
       if (locating_active_rect_ && (active_selection_rect == result_bounds)) {
         // We have found the active tickmark frame.
         main_frame_impl->active_match_frame_ = this;
+        found_active_match = true;
         // We also know which tickmark is active now.
         active_match_index_ = match_count - 1;
         // To stop looking for the active tickmark, we set this flag.
@@ -1285,6 +1303,8 @@ void WebFrameImpl::ScopeStringMatches(int request_id,
             request_id);
 #endif
       }
+
+      AddMarker(result_range.get(), found_active_match);
     }
 
     resume_scoping_from_range_ = result_range;
@@ -1389,10 +1409,6 @@ void WebFrameImpl::StopFinding(bool clear_selection) {
   if (!clear_selection)
     SetFindEndstateFocusAndSelection();
   CancelPendingScopingEffort();
-
-#if defined(OS_WIN)
-  WebCore::RenderThemeChromiumWin::setFindInPageMode(false);
-#endif
 
   // Remove all markers for matches found and turn off the highlighting.
   if (this == static_cast<WebFrameImpl*>(GetView()->GetMainFrame()))
