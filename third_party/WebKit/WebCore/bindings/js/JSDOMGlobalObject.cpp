09419584473f01cf5897eb6af6a97a611be9283f
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-11 00:10:39 +0000

    2009-04-10  Grace Kloba  <klobag@gmail.com>
            Reviewed by Darin Adler.
    
            Move WORKERS header files inside ENABLE(WORKERS).
            So if WORKERS is not enabled and the generated files are not generated, the build is still working.
    
            * bindings/js/JSDOMGlobalObject.cpp:
            * bindings/js/JSMessageChannelConstructor.cpp:
            * bindings/js/ScheduledAction.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42413 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDOMGlobalObject.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDOMGlobalObject.cpp
@@ -31,8 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "JSDOMWindow.h"
 #include "JSEventListener.h"
+
+#if ENABLE(WORKERS)
 #include "JSWorkerContext.h"
 #include "WorkerContext.h"
+#endif
 
 using namespace JSC;
 
--- third_party/WebKit/WebCore/bindings/js/ScheduledAction.cpp
+++ third_party/WebKit/WebCore/bindings/js/ScheduledAction.cpp
@@ -32,14 +32,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoader.h"
 #include "JSDOMBinding.h"
 #include "JSDOMWindow.h"
-#include "JSWorkerContext.h"
 #include "ScriptController.h"
 #include "ScriptExecutionContext.h"
 #include "ScriptSourceCode.h"
 #include "ScriptValue.h"
+#include <runtime/JSLock.h>
+
+#if ENABLE(WORKERS)
+#include "JSWorkerContext.h"
 #include "WorkerContext.h"
 #include "WorkerThread.h"
-#include <runtime/JSLock.h>
+#endif
 
 using namespace JSC;
 
