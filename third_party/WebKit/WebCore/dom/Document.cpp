6cb0c37dc65e261fc7c0e18ecfb0585d2f4892f9
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-13 17:14:44 +0000

    2009-04-13  David Hyatt  <hyatt@apple.com>
            Reviewed by Darin Adler and Dan Bernstein.
    
            Assertion in updateStyleForAllDocuments.  Make sure to unscheduleStyleRecalc when an object goes into
            the page cache.  Disallow scheduling of style recalcs on a document that is already in the page cache.
            Schedule a style recalc if needed when an object comes out of the page cache.
    
            * dom/Document.cpp:
            (WebCore::Document::scheduleStyleRecalc):
            (WebCore::Document::setInPageCache):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42439 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -1077,7 +1077,7 @@ PassRefPtr<TreeWalker> Document::createTreeWalker(Node *root, unsigned whatToSho
 
 void Document::scheduleStyleRecalc()
 {
-    if (m_styleRecalcTimer.isActive())
+    if (m_styleRecalcTimer.isActive() || inPageCache())
         return;
 
     ASSERT(childNeedsStyleRecalc());
@@ -3161,11 +3161,14 @@ void Document::setInPageCache(bool flag)
         m_savedRenderer = renderer();
         if (FrameView* v = view())
             v->resetScrollbars();
+        unscheduleStyleRecalc();
     } else {
         ASSERT(renderer() == 0 || renderer() == m_savedRenderer);
         ASSERT(m_renderArena);
         setRenderer(m_savedRenderer);
         m_savedRenderer = 0;
+        if (childNeedsStyleRecalc())
+            scheduleStyleRecalc();
     }
 }
 
