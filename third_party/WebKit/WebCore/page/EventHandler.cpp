4a4902f1c23033909786560b51124ac5df55c452
justin.garcia@apple.com <justin.garcia@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-13 20:38:03 +0000

    WebCore:
    2009-04-13  Justin Garcia  <justin.garcia@apple.com>
    
            Reviewed by Adele Peterson.
    
            https://bugs.webkit.org/show_bug.cgi?id=25153
            Can't place the caret into text field that scrolls the window on focus
            
            We refetch the target node in the shadow node case, and when we do so the window coordinate for the mouse event may
            be invalid because of scrolling that the focus handler did.  Cache the document point (that we derived from the window
            coordinate) and use that any time we refetch.
    
            * page/EventHandler.cpp:
            (WebCore::documentPointForWindowPoint):
            (WebCore::EventHandler::handleMousePressEvent):
            (WebCore::EventHandler::prepareMouseEvent):
    
    LayoutTests:
    
    2009-04-13  Justin Garcia  <justin.garcia@apple.com>
    
            Reviewed by Adele Peterson.
            
            <rdar://problem/6748324> REGRESSION (Kirkwood): Can't type in Netflix search field
    
            * fast/forms/25153-expected.txt: Added.
            * fast/forms/25153.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42456 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/WebCore/page/EventHandler.cpp
@@ -1040,6 +1040,14 @@ Cursor EventHandler::selectCursor(const MouseEventWithHitTestResults& event, Scr
     }
     return pointerCursor();
 }
+    
+IntPoint documentPointForWindowPoint(Frame* frame, const IntPoint& windowPoint)
+{
+    FrameView* view = frame->view();
+    // FIXME: Is it really OK to use the wrong coordinates here when view is 0?
+    // Historically the code would just crash; this is clearly no worse than that.
+    return view ? view->windowToContents(windowPoint) : windowPoint;
+}
 
 bool EventHandler::handleMousePressEvent(const PlatformMouseEvent& mouseEvent)
 {
@@ -1061,7 +1069,8 @@ bool EventHandler::handleMousePressEvent(const PlatformMouseEvent& mouseEvent)
     m_mouseDownWasInSubframe = false;
 
     HitTestRequest request(HitTestRequest::Active);
-    MouseEventWithHitTestResults mev = prepareMouseEvent(request, mouseEvent);
+    IntPoint documentPoint = documentPointForWindowPoint(m_frame, mouseEvent.pos());
+    MouseEventWithHitTestResults mev = m_frame->document()->prepareMouseEvent(request, documentPoint, mouseEvent);
 
     if (!mev.targetNode()) {
         invalidateClick();
@@ -1140,7 +1149,7 @@ bool EventHandler::handleMousePressEvent(const PlatformMouseEvent& mouseEvent)
     if (mev.scrollbar()) {
         const bool wasLastScrollBar = mev.scrollbar() == m_lastScrollbarUnderMouse.get();
         HitTestRequest request(HitTestRequest::ReadOnly | HitTestRequest::Active);
-        mev = prepareMouseEvent(request, mouseEvent);
+        mev = m_frame->document()->prepareMouseEvent(request, documentPoint, mouseEvent);
         if (wasLastScrollBar && mev.scrollbar() != m_lastScrollbarUnderMouse.get())
             m_lastScrollbarUnderMouse = 0;
     }
@@ -1156,7 +1165,7 @@ bool EventHandler::handleMousePressEvent(const PlatformMouseEvent& mouseEvent)
         // event target node can't still be the shadow node.
         if (mev.targetNode()->isShadowNode() && mev.targetNode()->shadowParentNode()->hasTagName(inputTag)) {
             HitTestRequest request(HitTestRequest::ReadOnly | HitTestRequest::Active);
-            mev = prepareMouseEvent(request, mouseEvent);
+            mev = m_frame->document()->prepareMouseEvent(request, documentPoint, mouseEvent);
         }
 
         FrameView* view = m_frame->view();
@@ -1500,12 +1509,8 @@ MouseEventWithHitTestResults EventHandler::prepareMouseEvent(const HitTestReques
 {
     ASSERT(m_frame);
     ASSERT(m_frame->document());
-
-    FrameView* view = m_frame->view();
-    // FIXME: Is it really OK to use the wrong coordinates here when view is 0?
-    // Historically the code would just crash; this is clearly no worse than that.
-    IntPoint documentPoint = view ? view->windowToContents(mev.pos()) : mev.pos();
-    return m_frame->document()->prepareMouseEvent(request, documentPoint, mev);
+    
+    return m_frame->document()->prepareMouseEvent(request, documentPointForWindowPoint(m_frame, mev.pos()), mev);
 }
 
 #if ENABLE(SVG)
