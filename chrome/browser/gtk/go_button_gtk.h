b655a45c5bd61fc871fb7e472709d573a7333ab4
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-13 22:23:18 +0000

    Implement tooltips for the Go/Stop button in Linux. BUG=9381
    Review URL: http://codereview.chromium.org/67064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13618 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/go_button_gtk.h
+++ chrome/browser/gtk/go_button_gtk.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/owned_widget_gtk.h"
 
 class Browser;
+class LocationBarViewGtk;
 class Task;
 
 class GoButtonGtk {
@@ -21,7 +22,7 @@ class GoButtonGtk {
   enum Mode { MODE_GO = 0, MODE_STOP };
   enum ButtonState { BS_NORMAL = 0, BS_HOT };
 
-  explicit GoButtonGtk(Browser* browser);
+  GoButtonGtk(LocationBarViewGtk* location_bar, Browser* browser);
   ~GoButtonGtk();
 
   GtkWidget* widget() const { return widget_.get(); }
@@ -47,8 +48,13 @@ class GoButtonGtk {
   static gboolean OnLeave(GtkButton* widget, GoButtonGtk* button);
   static gboolean OnClicked(GtkButton* widget, GoButtonGtk* button);
 
+  void SetToggled();
+
   Task* CreateButtonTimerTask();
   void OnButtonTimer();
+  void SetTooltip();
+
+  LocationBarViewGtk* const location_bar_;
 
   // Keep a pointer to the Browser object to execute commands on it.
   Browser* const browser_;
