13e40d23789e958426d090c6917dad3672add860
bolsinga@apple.com <bolsinga@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-13 17:07:26 +0000

    2009-04-13  Greg Bolsinga  <bolsinga@apple.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=24770
            
            Add custom code for Coordinates that returns null when required by the
            Geolocation spec.
            
            http://dev.w3.org/geo/api/spec-source.html#coordinates
    
            * WebCore.xcodeproj/project.pbxproj:
            * bindings/js/JSCoordinatesCustom.cpp: Added.
            (WebCore::JSCoordinates::altitude): returns null if the value cannot be provided.
            (WebCore::JSCoordinates::altitudeAccuracy): Ditto
            (WebCore::JSCoordinates::heading): Ditto
            (WebCore::JSCoordinates::speed): Ditto
            * page/Coordinates.h:
            (WebCore::Coordinates::create): Add 'canProvide' parameters and values
            (WebCore::Coordinates::canProvideAltitude):
            (WebCore::Coordinates::canProvideAltitudeAccuracy):
            (WebCore::Coordinates::canProvideHeading):
            (WebCore::Coordinates::canProvideSpeed):
            (WebCore::Coordinates::Coordinates):
            * page/Coordinates.idl: specify custom getters
            * platform/mac/GeolocationServiceMac.mm: Set the values correctly if they cannot be provided.
            (-[WebCoreCoreLocationObserver locationManager:didUpdateToLocation:fromLocation:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42437 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Coordinates.h
+++ third_party/WebKit/WebCore/page/Coordinates.h
@@ -37,7 +37,7 @@ typedef int ExceptionCode;
 
 class Coordinates : public RefCounted<Coordinates> {
 public:
-    static PassRefPtr<Coordinates> create(double latitude, double longitude, double altitude, double accuracy, double altitudeAccuracy, double heading, double speed) { return adoptRef(new Coordinates(latitude, longitude, altitude, accuracy, altitudeAccuracy, heading, speed)); }
+    static PassRefPtr<Coordinates> create(double latitude, double longitude, bool providesAltitude, double altitude, double accuracy, bool providesAltitudeAccuracy, double altitudeAccuracy, bool providesHeading, double heading, bool providesSpeed, double speed) { return adoptRef(new Coordinates(latitude, longitude, providesAltitude, altitude, accuracy, providesAltitudeAccuracy, altitudeAccuracy, providesHeading, heading, providesSpeed, speed)); }
     
     double latitude() const { return m_latitude; }
     double longitude() const { return m_longitude; }
@@ -47,10 +47,15 @@ public:
     double heading() const { return m_heading; }
     double speed() const { return m_speed; }
 
+    bool canProvideAltitude() const { return m_canProvideAltitude; }
+    bool canProvideAltitudeAccuracy() const { return m_canProvideAltitudeAccuracy; }
+    bool canProvideHeading() const { return m_canProvideHeading; }
+    bool canProvideSpeed() const { return m_canProvideSpeed; }
+    
     String toString() const;
 
 private:
-    Coordinates(double latitude, double longitude, double altitude, double accuracy, double altitudeAccuracy, double heading, double speed)
+    Coordinates(double latitude, double longitude, bool providesAltitude, double altitude, double accuracy, bool providesAltitudeAccuracy, double altitudeAccuracy, bool providesHeading, double heading, bool providesSpeed, double speed)
         : m_latitude(latitude)
         , m_longitude(longitude)
         , m_altitude(altitude)
@@ -58,6 +63,10 @@ private:
         , m_altitudeAccuracy(altitudeAccuracy)
         , m_heading(heading)
         , m_speed(speed)
+        , m_canProvideAltitude(providesAltitude)
+        , m_canProvideAltitudeAccuracy(providesAltitudeAccuracy)
+        , m_canProvideHeading(providesHeading)
+        , m_canProvideSpeed(providesSpeed)
     {
     }
 
@@ -68,6 +77,11 @@ private:
     double m_altitudeAccuracy;
     double m_heading;
     double m_speed;
+    
+    bool m_canProvideAltitude;
+    bool m_canProvideAltitudeAccuracy;
+    bool m_canProvideHeading;
+    bool m_canProvideSpeed;
 };
     
 } // namespace WebCore
