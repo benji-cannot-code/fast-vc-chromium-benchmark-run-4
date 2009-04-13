97ef048b50041d337c6a0e4cd6eb9b157aa69d6e
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-13 18:22:22 +0000

    Add a comment to the hwnd view about proper usage. I was using it incorrectly in when trying to do some ui demos.
    Add a hook for the frame window test I'm doing.
    Review URL: http://codereview.chromium.org/58013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13605 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_init.cc
+++ chrome/browser/browser_init.cc
@@ -48,6 +48,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win_util.h"
 #endif
 
+#ifdef FRAME_WINDOW
+#include "frame_window/frame_window.h"
+#endif
+
 namespace {
 
 // A delegate for the InfoBar shown when the previous session has crashed. The
@@ -535,6 +539,11 @@ bool BrowserInit::LaunchBrowserImpl(const CommandLine& command_line,
                                     bool process_startup,
                                     int* return_code) {
   DCHECK(profile);
+
+#ifdef FRAME_WINDOW
+  FrameWindow::Show(profile);
+  return true;
+#endif
 
   // Continue with the off-the-record profile from here on if --incognito
   if (command_line.HasSwitch(switches::kIncognito))
