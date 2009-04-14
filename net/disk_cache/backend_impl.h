d0aadb09bdb304756ad8427741eec7b8aad08790
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-14 18:48:15 +0000

    Disk cache: Enable the code that sets the max size automatically. (still limiting the ceiling).
    Review URL: http://codereview.chromium.org/68007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13684 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_impl.h
+++ net/disk_cache/backend_impl.h
@@ -248,6 +248,9 @@ class BackendImpl : public Backend {
   DISALLOW_EVIL_CONSTRUCTORS(BackendImpl);
 };
 
+// Returns the prefered max cache size given the available disk space.
+int PreferedCacheSize(int64 available);
+
 }  // namespace disk_cache
 
 #endif  // NET_DISK_CACHE_BACKEND_IMPL_H_
