f9848e221df622ba438e10e87d5e3c896fae4af8
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-14 06:10:31 +0000

    Add JsonSchema-based validation for the tab APIs.
    Arv: can you take json_schema.js and json_schema_test.js.
    Matt: you take the rest.
    
    Review URL: http://codereview.chromium.org/66006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13649 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_view_unittest.cc
+++ chrome/browser/extensions/extension_view_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/site_instance.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/test/in_process_browser_test.h"
 #include "chrome/test/ui_test_utils.h"
 
