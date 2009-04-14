1d690c47a900b59417ac7820281d8b8e99ab8d74
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-14 07:19:34 +0000

    speculative 'doze build fix
    git-svn-id: svn://svn.chromium.org/blink/trunk@42482 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/RegExp.h
+++ third_party/WebKit/JavaScriptCore/runtime/RegExp.h
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ExecutableAllocator.h"
 #include <wtf/Forward.h>
 #include <wtf/RefCounted.h>
-#include "RegexJIT.h"
-#include "RegexInterpreter.h"
+#include "yarr/RegexJIT.h"
+#include "yarr/RegexInterpreter.h"
 
 struct JSRegExp;
 
