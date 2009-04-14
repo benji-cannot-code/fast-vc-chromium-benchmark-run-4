906c690f02a0dfe09ebb68f55f5db679c79fb225
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-14 15:07:03 +0000

    2009-04-14  Kenneth Rohde Christiansen  <kenneth.christiansen@openbossa.org>
            Reviewed by Tor Arne Vestbø.
    
            Rename QWebElement::setHtml and html to setXml and toXml respectivily.
    
            Also add a mean to define the scope (inner or other).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/tests/qwebelement/tst_qwebelement.cpp
+++ third_party/WebKit/WebKit/qt/tests/qwebelement/tst_qwebelement.cpp
@@ -130,7 +130,7 @@ void tst_QWebElement::textHtml()
     QCOMPARE(body.toPlainText(), QString("test"));
     QCOMPARE(body.toPlainText(), m_mainFrame->toPlainText());
 
-    QCOMPARE(body.html(), html);
+    QCOMPARE(body.toXml(QWebElement::InnerXml), html);
 }
 
 void tst_QWebElement::simpleSelection()
@@ -331,7 +331,7 @@ void tst_QWebElement::foreachManipulation()
     QWebElement body = m_mainFrame->documentElement();
 
     foreach(QWebElement p, body.findAll("p")) {
-        p.setHtml("<div>foo</div><div>bar</div>");
+        p.setXml(QWebElement::InnerXml, "<div>foo</div><div>bar</div>");
     }
 
     QCOMPARE(body.findAll("div").count(), 4);
