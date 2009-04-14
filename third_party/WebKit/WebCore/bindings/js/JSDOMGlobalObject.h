b830f29b0cc22be356872c1d3f665cec7728a7fe
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-14 22:54:25 +0000

    2009-04-14  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Sam Weinig.
    
            Nixed some now-dead code related to protected event listeners.
    
            * bindings/js/JSDOMGlobalObject.cpp:
            (WebCore::JSDOMGlobalObject::~JSDOMGlobalObject):
            * bindings/js/JSDOMGlobalObject.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDOMGlobalObject.h
+++ third_party/WebKit/WebCore/bindings/js/JSDOMGlobalObject.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
     class Event;
-    class JSProtectedEventListener;
     class JSEventListener;
     class ScriptExecutionContext;
 
@@ -54,23 +53,14 @@ namespace WebCore {
 
         virtual ScriptExecutionContext* scriptExecutionContext() const = 0;
 
-        // Finds a wrapper of a JS EventListener, returns 0 if no existing one.
-        JSProtectedEventListener* findJSProtectedEventListener(JSC::JSValuePtr, bool isInline = false);
-
-        // Finds or creates a wrapper of a JS EventListener. JS EventListener object is GC-protected.
-        PassRefPtr<JSProtectedEventListener> findOrCreateJSProtectedEventListener(JSC::JSValuePtr, bool isInline = false);
-
         // Finds a wrapper of a GC-unprotected JS EventListener, returns 0 if no existing one.
         JSEventListener* findJSEventListener(JSC::JSValuePtr, bool isInline = false);
 
         // Finds or creates a wrapper of a JS EventListener. JS EventListener object is *NOT* GC-protected.
         PassRefPtr<JSEventListener> findOrCreateJSEventListener(JSC::JSValuePtr, bool isInline = false);
 
-        typedef HashMap<JSC::JSObject*, JSProtectedEventListener*> ProtectedListenersMap;
         typedef HashMap<JSC::JSObject*, JSEventListener*> JSListenersMap;
 
-        ProtectedListenersMap& jsProtectedEventListeners();
-        ProtectedListenersMap& jsProtectedInlineEventListeners();
         JSListenersMap& jsEventListeners();
         JSListenersMap& jsInlineEventListeners();
 
@@ -86,8 +76,6 @@ namespace WebCore {
             JSDOMStructureMap structures;
             JSDOMConstructorMap constructors;
 
-            JSDOMGlobalObject::ProtectedListenersMap jsProtectedEventListeners;
-            JSDOMGlobalObject::ProtectedListenersMap jsProtectedInlineEventListeners;
             JSDOMGlobalObject::JSListenersMap jsEventListeners;
             JSDOMGlobalObject::JSListenersMap jsInlineEventListeners;
 
