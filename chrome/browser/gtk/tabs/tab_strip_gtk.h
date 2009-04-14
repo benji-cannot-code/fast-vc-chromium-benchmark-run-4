598f28f3f3dd6c867df83c30176bbab4b29bcb05
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-14 22:40:03 +0000

    Implement context menu handling for the Linux tab strip. Review URL: http://codereview.chromium.org/73053
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13714 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/tabs/tab_strip_gtk.h
+++ chrome/browser/gtk/tabs/tab_strip_gtk.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/gtk/tabs/tab_gtk.h"
 #include "chrome/browser/tabs/tab_strip_model.h"
 #include "chrome/common/owned_widget_gtk.h"
-#include "skia/include/SkBitmap.h"
 
 class NewTabButton;
 
