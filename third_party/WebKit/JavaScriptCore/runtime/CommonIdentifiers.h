2069ac808ef9ca611081808ead0067f9d7510dc3
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-14 06:24:43 +0000

    BUG 25171: It should be possible to manually set the name of an anonymous function <https://bugs.webkit.org/show_bug.cgi?id=25171>
    Submitted by Francisco Tolmasky <francisco@280north.com>
    Reviewed by Oliver Hunt.
    
    This change adds the displayName property to functions, which when set overrides the
    normal name when appearing in the console.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42478 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/CommonIdentifiers.h
+++ third_party/WebKit/JavaScriptCore/runtime/CommonIdentifiers.h
@@ -64,7 +64,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     macro(toPrecision) \
     macro(toString) \
     macro(UTC) \
-    macro(valueOf)
+    macro(valueOf) \
+    macro(displayName)
 
 namespace JSC {
 
