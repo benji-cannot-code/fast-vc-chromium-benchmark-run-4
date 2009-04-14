1d8fc93295a61898889e5844620f21816c2c84f9
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-14 06:39:22 +0000

    Missed code from last patch
    git-svn-id: svn://svn.chromium.org/blink/trunk@42480 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/InternalFunction.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/InternalFunction.cpp
@@ -49,7 +49,7 @@ const UString& InternalFunction::name(JSGlobalData* globalData)
     return asString(getDirect(globalData->propertyNames->name))->value();
 }
 
-const UString& InternalFunction::displayName(JSGlobalData* globalData)
+const UString InternalFunction::displayName(JSGlobalData* globalData)
 {
     JSValuePtr displayName = getDirect(globalData->propertyNames->displayName);
     
@@ -59,9 +59,9 @@ const UString& InternalFunction::displayName(JSGlobalData* globalData)
     return UString::null();
 }
 
-const UString& InternalFunction::calculatedDisplayName(JSGlobalData* globalData)
+const UString InternalFunction::calculatedDisplayName(JSGlobalData* globalData)
 {
-    const UString& explicitName = displayName(globalData);
+    const UString explicitName = displayName(globalData);
     
     if (!explicitName.isEmpty())
         return explicitName;
