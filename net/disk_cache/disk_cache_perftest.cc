08427b9d7cf79499fcea4bad589e63847d27a355
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-14 23:18:46 +0000

    Fix strict aliasing warning in disk_cache_perftest. Review URL: http://codereview.chromium.org/67144
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13717 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/disk_cache_perftest.cc
+++ net/disk_cache/disk_cache_perftest.cc
@@ -218,10 +218,7 @@ TEST_F(DiskCacheTest, BlockFilesPerformance) {
   srand(seed);
 
   const int kNumEntries = 60000;
-  int32 buffer[kNumEntries];
-  memset(buffer, 0, sizeof(buffer));
-  disk_cache::Addr* address = reinterpret_cast<disk_cache::Addr*>(buffer);
-  ASSERT_EQ(sizeof(*address), sizeof(*buffer));
+  disk_cache::Addr* address = new disk_cache::Addr[kNumEntries];
 
   PerfTimeLogger timer1("Fill three block-files");
 
@@ -246,4 +243,5 @@ TEST_F(DiskCacheTest, BlockFilesPerformance) {
 
   timer2.Done();
   MessageLoop::current()->RunAllPending();
+  delete address;
 }
