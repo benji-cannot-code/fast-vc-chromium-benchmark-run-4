32a645f2d4a36ca4f6d6b3211ea4f50c8a02c831
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-15 01:42:27 +0000

    2009-04-14  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by NOBODY (OOPS!).
            
            Fix <rdar://problem/6755724> <audio> and <video> elements can reference local 
            file:/// URLs from remote in Safari
    
            Tests: http/tests/security/local-video-poster-from-remote.html
                   http/tests/security/local-video-source-from-remote.html
                   http/tests/security/local-video-src-from-remote.html
    
            * html/HTMLMediaElement.cpp:
            (WebCore::HTMLMediaElement::loadResource): Don't pass url to media engine if loader->canLoad()
            says it should not be loaded.
    
    2009-04-14  Eric Carlson  <eric.carlson@apple.com>
    
            Reviewed by NOBODY (OOPS!).
    
            Fix <rdar://problem/6755724> <audio> and <video> elements can reference local 
            file:/// URLs from remote in Safari
    
            Test cases to ensure that local 'src', 'poster', and <source> are not loaded.
    
            * http/tests/security/local-video-poster-from-remote-expected.txt: Added.
            * http/tests/security/local-video-poster-from-remote.html: Added.
            * http/tests/security/local-video-source-from-remote-expected.txt: Added.
            * http/tests/security/local-video-source-from-remote.html: Added.
            * http/tests/security/local-video-src-from-remote-expected.txt: Added.
            * http/tests/security/local-video-src-from-remote.html: Added.
            * http/tests/security/resources/load-media.cgi: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLMediaElement.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSHelper.h"
 #include "CSSPropertyNames.h"
 #include "CSSValueKeywords.h"
+#include "DocLoader.h"
 #include "Event.h"
 #include "EventNames.h"
 #include "ExceptionCode.h"
@@ -469,8 +470,24 @@ void HTMLMediaElement::loadNextSourceChild()
 
 void HTMLMediaElement::loadResource(String url, ContentType& contentType)
 {
-    // The resource fetch algorithm 
+    Frame* frame = document()->frame();
+    FrameLoader* loader = frame ? frame->loader() : 0;
 
+    // don't allow remote to local urls
+    if (!loader || !loader->canLoad(KURL(KURL(), url), String(), document())) {
+        FrameLoader::reportLocalLoadFailed(frame, url);
+
+        // If we rejected the url from a <source> element and there are more <source> children, schedule
+        // the next one without reporting an error
+        if (m_loadState == LoadingFromSourceElement && havePotentialSourceChild())
+            scheduleLoad();
+        else
+            noneSupported();
+
+        return;
+    }
+
+    // The resource fetch algorithm 
     m_networkState = NETWORK_LOADING;
 
     m_currentSrc = url;
