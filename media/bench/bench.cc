96651d7d7ae121c7a73f227e144976887b476cd2
fbarchard@chromium.org <fbarchard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-15 19:03:18 +0000

    improvements to media_bench (ffmpeg benchmarking application) -format output into columns -add -fast2 and -skip flags
    Review URL: http://codereview.chromium.org/69004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13771 0039d316-1c4b-4281-b951-d872f2087c98

--- media/bench/bench.cc
+++ media/bench/bench.cc
@@ -8,8 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // options.  We also use this tool to measure performance regressions when
 // testing newer builds of FFmpeg from trunk.
 
-#include <iostream>
 #include <windows.h>
+#include <iomanip>
+#include <iostream>
 
 #include "base/at_exit.h"
 #include "base/basictypes.h"
@@ -22,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 const wchar_t kStream[]                 = L"stream";
 const wchar_t kVideoThreads[]           = L"video-threads";
+const wchar_t kFast2[]                  = L"fast2";
+const wchar_t kSkip[]                   = L"skip";
 }  // namespace switches
 
 int main(int argc, const char** argv) {
@@ -36,7 +39,11 @@ int main(int argc, const char** argv) {
               << "  --stream=[audio|video]          "
               << "Benchmark either the audio or video stream\n"
               << "  --video-threads=N               "
-              << "Decode video using N threads" << std::endl;
+              << "Decode video using N threads\n"
+              << "  --fast2                         "
+              << "Enable fast2 flag\n"
+              << "  --skip=[1|2|3]                  "
+              << "1=loop nonref, 2=loop, 3= frame nonref" << std::endl;
     return 1;
   }
 
@@ -62,6 +69,19 @@ int main(int argc, const char** argv) {
   std::wstring threads(cmd_line->GetSwitchValue(switches::kVideoThreads));
   if (!threads.empty() && !StringToInt(threads, &video_threads)) {
     video_threads = 0;
+  }
+
+  bool fast2 = false;
+  if (cmd_line->HasSwitch(switches::kFast2)) {
+    fast2 = true;
+  }
+
+  int skip = 0;
+  if (cmd_line->HasSwitch(switches::kSkip)) {
+    std::wstring skip_opt(cmd_line->GetSwitchValue(switches::kSkip));
+    if (!StringToInt(skip_opt, &skip)) {
+      skip = 0;
+    }
   }
 
   // Register FFmpeg and attempt to open file.
@@ -107,6 +127,18 @@ int main(int argc, const char** argv) {
   AVPacket packet;
   AVCodecContext* codec_context = format_context->streams[target_stream]->codec;
   AVCodec* codec = avcodec_find_decoder(codec_context->codec_id);
+
+  if (skip == 1) {
+    codec_context->skip_loop_filter = AVDISCARD_NONREF;
+  } else if (skip == 2) {
+    codec_context->skip_loop_filter = AVDISCARD_ALL;
+  } else if (skip == 3) {
+    codec_context->skip_loop_filter = AVDISCARD_ALL;
+    codec_context->skip_frame = AVDISCARD_NONREF;
+  }
+  if (fast2) {
+    codec_context->flags2 |= CODEC_FLAG2_FAST;
+  }
 
   // Initialize threaded decode.
   if (target_codec == CODEC_TYPE_VIDEO && video_threads > 0) {
@@ -191,12 +223,19 @@ int main(int argc, const char** argv) {
   // Calculate the standard deviation (jitter).
   double stddev = sqrt(squared_sum / decode_times.size());
 
-  // Print our results.
+    // Print our results.
+  std::cout.setf(std::ios::fixed);
+  std::cout.precision(3);
   std::cout << std::endl;
-  std::cout << "     Frames: " << decode_times.size() << std::endl;
-  std::cout << "      Total: " << total.InMillisecondsF() << "ms" << std::endl;
-  std::cout << "  Summation: " << sum << "ms" << std::endl;
-  std::cout << "    Average: " << average << "ms" << std::endl;
-  std::cout << "     StdDev: " << stddev << "ms" << std::endl;
+  std::cout << "     Frames:" << std::setw(10) << decode_times.size()
+            << std::endl;
+  std::cout << "      Total:" << std::setw(10) << total.InMillisecondsF()
+            << " ms" << std::endl;
+  std::cout << "  Summation:" << std::setw(10) << sum
+            << " ms" << std::endl;
+  std::cout << "    Average:" << std::setw(10) << average
+            << " ms" << std::endl;
+  std::cout << "     StdDev:" << std::setw(10) << stddev
+            << " ms" << std::endl;
   return 0;
 }
