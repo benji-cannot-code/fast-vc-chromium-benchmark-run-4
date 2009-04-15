a7a4ad89df6b9d0336e45e3dccb92b5c52806ca5
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-15 23:46:30 +0000

    Fix Windows build of DumpRenderTree.
            * DumpRenderTree/win/DumpRenderTree.cpp:
            (sharedCFURLCache):
            (main):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/win/DumpRenderTree.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/win/DumpRenderTree.cpp
@@ -1041,6 +1041,25 @@ IWebView* createWebViewAndOffscreenWindow(HWND* webViewWindow)
     return webView;
 }
 
+RetainPtr<CFURLCacheRef> sharedCFURLCache()
+{
+    HMODULE module = GetModuleHandle(TEXT("CFNetwork_debug.dll"));
+    if (!module)
+        module = GetModuleHandle(TEXT("CFNetwork.dll"));
+    if (!module)
+        return 0;
+
+    typedef CFURLCacheRef (*CFURLCacheCopySharedURLCacheProcPtr)(void);
+    if (CFURLCacheCopySharedURLCacheProcPtr copyCache = reinterpret_cast<CFURLCacheCopySharedURLCacheProcPtr>(GetProcAddress(module, "CFURLCacheCopySharedURLCache")))
+        return RetainPtr<CFURLCacheRef>(AdoptCF, copyCache());
+
+    typedef CFURLCacheRef (*CFURLCacheSharedURLCacheProcPtr)(void);
+    if (CFURLCacheSharedURLCacheProcPtr sharedCache = reinterpret_cast<CFURLCacheSharedURLCacheProcPtr>(GetProcAddress(module, "CFURLCacheSharedURLCache")))
+        return sharedCache();
+
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
     leakChecking = false;
@@ -1096,7 +1115,8 @@ int main(int argc, char* argv[])
     if (FAILED(webView->mainFrame(&frame)))
         return -1;
 
-    CFURLCacheRemoveAllCachedResponses(CFURLCacheSharedURLCache());
+    RetainPtr<CFURLCacheRef> urlCache = sharedCFURLCache();
+    CFURLCacheRemoveAllCachedResponses(urlCache.get());
 
 #ifdef _DEBUG
     _CrtMemState entryToMainMemCheckpoint;
