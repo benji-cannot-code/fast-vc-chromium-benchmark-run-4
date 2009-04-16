6639e479b417644577f4ca2414b3902467e53ad4
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-16 17:24:03 +0000

    Reviewed by Geoffrey Garen.
            Add filterBoundingBox() to RenderSVGModelObject to share a tiny bit of code
    
            RenderSVGModelObject has much more interesting code it can share,
            but I'm just trying to get the commits out of my local branch and into
            the repository in any order I can. :)  This one was small.
    
            This will be used by the unified RenderSVGModelObject
            clippedOverflowRectForRepaint patch coming soon.
    
            * rendering/RenderPath.cpp:
            (WebCore::RenderPath::clippedOverflowRectForRepaint):
            * rendering/RenderSVGContainer.cpp:
            (WebCore::RenderSVGContainer::clippedOverflowRectForRepaint):
            * rendering/RenderSVGModelObject.cpp:
            (WebCore::RenderSVGModelObject::filterBoundingBox):
            * rendering/RenderSVGModelObject.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderSVGModelObject.h
+++ third_party/WebKit/WebCore/rendering/RenderSVGModelObject.h
@@ -50,6 +50,10 @@ public:
     RenderSVGModelObject(SVGStyledElement*);
 
     virtual bool requiresLayer() const { return false; }
+
+protected:
+    // Returns the bounding box for the filter associated with this object (if any)
+    FloatRect filterBoundingBox() const;
 };
 
 }
