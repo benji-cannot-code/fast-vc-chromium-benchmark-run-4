fb0643e78bb7110c672421e786ef4c0cb433ebb5
brg@chromium.com <brg@chromium.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-16 22:59:02 +0000

    Re-introducing the toolbar importer code.This is the same code that was removed in CL http://codereview.chromium.org/18550.The UI will be reworked in a CL immediately following review/submission of this code.Please contact Nick Baum for information regarding this change. Review URL: http://codereview.chromium.org/66065
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13895 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/importer.h
+++ chrome/browser/importer/importer.h
@@ -33,6 +33,7 @@ enum ProfileType {
 #endif
   FIREFOX2,
   FIREFOX3,
+  GOOGLE_TOOLBAR5,
   // Identifies a 'bookmarks.html' file.
   BOOKMARKS_HTML
 };
@@ -285,6 +286,7 @@ class ImporterHost : public base::RefCounted<ImporterHost>,
   void DetectIEProfiles();
 #endif
   void DetectFirefoxProfiles();
+  void DetectGoogleToolbarProfiles();
 
   // The list of profiles with the default one first.
   std::vector<ProfileInfo*> source_profiles_;
