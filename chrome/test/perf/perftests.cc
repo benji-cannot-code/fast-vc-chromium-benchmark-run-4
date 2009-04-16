8832615956ce783a2398ac88c87695c88642f41b
dkegel@google.com <dkegel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-16 23:23:09 +0000

    Fix error [FATAL:at_exit.cc(40)] Check failed: false. Tried to RegisterCallback without an AtExitManager in (possibly unused) binary perf_tests
    Review URL: http://codereview.chromium.org/60103
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13899 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/perf/perftests.cc
+++ chrome/test/perf/perftests.cc
@@ -8,8 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.cc"
 
 int main(int argc, char **argv) {
+  PerfTestSuite suite(argc, argv);
   chrome::RegisterPathProvider();
   MessageLoop main_message_loop;
 
-  return PerfTestSuite(argc, argv).Run();
+  return suite.Run();
 }
