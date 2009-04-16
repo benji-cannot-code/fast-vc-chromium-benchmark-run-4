dabb8187ede8edeab57531440ea504c53ee9be4e
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-16 03:51:55 +0000

    2009-04-15  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Sam Weinig.
            
            More fix for https://bugs.webkit.org/show_bug.cgi?id=21260
            Unbounded memory growth when churning elements with anonymous event handler functions
    
            Also fixed <rdar://problem/6792909> WebInspector crashes when reloading
            a page with breakpoints set
            
            * bindings/js/JSEventListener.cpp:
            (WebCore::JSEventListener::clearJSFunctionInline):
            (WebCore::JSEventListener::markJSFunction):
            * bindings/js/JSEventListener.h: Actually clear our function and global
            object pointers when our client instructs us to. (Oops!) Also, mark
            our global object while we still intend to use it.
    
            * bindings/js/JSSVGElementInstanceCustom.cpp:
            (WebCore::JSSVGElementInstance::mark):
            (WebCore::toJS):
            * bindings/scripts/CodeGeneratorJS.pm:
            * svg/SVGElementInstance.idl: For every SVGElementInstance wrapper, ensure
            that the corresponding element also has a wrapper, to keep its event
            listeners alive.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42569 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSSVGElementInstanceCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSSVGElementInstanceCustom.cpp
@@ -27,11 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 
 #if ENABLE(SVG)
-#include "SVGElementInstance.h"
 #include "JSSVGElementInstance.h"
 
-#include "JSEventListener.h"
 #include "JSDOMWindow.h"
+#include "JSEventListener.h"
+#include "JSSVGElement.h"
+#include "SVGElementInstance.h"
 
 using namespace JSC;
 
@@ -39,9 +40,13 @@ namespace WebCore {
 
 void JSSVGElementInstance::mark()
 {
-    Base::mark();
+    DOMObject::mark();
 
-    markEventListeners(impl()->eventListeners());
+    // Mark the wrapper for our corresponding element, so it can mark its event handlers.
+    JSNode* correspondingWrapper = getCachedDOMNodeWrapper(impl()->correspondingElement()->document(), impl()->correspondingElement());
+    ASSERT(correspondingWrapper);
+    if (!correspondingWrapper->marked())
+        correspondingWrapper->mark();
 }
 
 JSValuePtr JSSVGElementInstance::addEventListener(ExecState* exec, const ArgList& args)
@@ -72,6 +77,17 @@ void JSSVGElementInstance::pushEventHandlerScope(ExecState*, ScopeChain&) const
 {
 }
 
+JSC::JSValuePtr toJS(JSC::ExecState* exec, SVGElementInstance* object)
+{
+    JSValuePtr result = getDOMObjectWrapper<JSSVGElementInstance>(exec, object);
+
+    // Ensure that our corresponding element has a JavaScript wrapper to keep its event handlers alive.
+    if (object)
+        toJS(exec, object->correspondingElement());
+
+    return result;
 }
 
-#endif
+} // namespace WebCore
+
+#endif // ENABLE(SVG)
