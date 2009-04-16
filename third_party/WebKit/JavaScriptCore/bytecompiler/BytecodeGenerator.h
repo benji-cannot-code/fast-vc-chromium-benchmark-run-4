9bfbc2fe8d90d0d5064d14b0bbd15ad4b93b8f39
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-16 11:36:07 +0000

    Improve performance of read-write-modify operators
    Reviewed by Gavin Barraclough
    
    Implement cross scope optimisation for read-write-modify
    operators, to avoid unnecessary calls to property resolve
    helper functions.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bytecompiler/BytecodeGenerator.h
+++ third_party/WebKit/JavaScriptCore/bytecompiler/BytecodeGenerator.h
@@ -244,6 +244,7 @@ namespace JSC {
         RegisterID* emitLoad(RegisterID* dst, JSValuePtr);
         RegisterID* emitUnexpectedLoad(RegisterID* dst, bool);
         RegisterID* emitUnexpectedLoad(RegisterID* dst, double);
+        RegisterID* emitLoadGlobalObject(RegisterID* dst, JSObject* globalObject);
 
         RegisterID* emitUnaryOp(OpcodeID, RegisterID* dst, RegisterID* src);
         RegisterID* emitBinaryOp(OpcodeID, RegisterID* dst, RegisterID* src1, RegisterID* src2, OperandTypes);
@@ -462,6 +463,7 @@ namespace JSC {
         int m_nextGlobalIndex;
         int m_nextParameterIndex;
         int m_nextConstantIndex;
+        unsigned m_globalConstantIndex;
 
         int m_globalVarStorageOffset;
 
