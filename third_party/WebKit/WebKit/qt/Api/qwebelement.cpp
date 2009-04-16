0267cb873d3c8c49830704eb726ce097de4abf52
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-16 22:25:58 +0000

    2009-04-16  Holger Hans Peter Freyther  <zecke@selfish.org>
            Rubber-stamped by Simon Hausmann.
    
            Make the operator== and operator!= non inline to
            allow looking at the d-pointer in the future.
    
            * Api/qwebelement.cpp:
            (QWebElement::operator==):
            (QWebElement::operator!=):
            * Api/qwebelement.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42596 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebelement.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebelement.cpp
@@ -123,6 +123,16 @@ QWebElement::~QWebElement()
         m_element->deref();
 }
 
+bool QWebElement::operator==(const QWebElement& o) const
+{
+    return m_element == o.m_element;
+}
+
+bool QWebElement::operator!=(const QWebElement& o) const
+{
+    return m_element != o.m_element;
+}
+
 /*!
     Returns true if the element is a null element; false otherwise.
 */
