3f8b22a3db7373c09c1d7f80734434903864d069
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-16 01:27:13 +0000

    ad an argument to MoveTabContentsAt() that switches whether the moved tab is set as the selected tab, or if the selected tab is left selected (but possibly moved).
    Review URL: http://codereview.chromium.org/63153
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13823 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/dragged_tab_controller.cc
+++ chrome/browser/views/tabs/dragged_tab_controller.cc
@@ -585,7 +585,7 @@ void DraggedTabController::MoveTab(const gfx::Point& screen_point) {
       to_index = NormalizeIndexToAttachedTabStrip(to_index);
       if (from_index != to_index) {
         last_move_screen_x_ = screen_point.x();
-        attached_model->MoveTabContentsAt(from_index, to_index);
+        attached_model->MoveTabContentsAt(from_index, to_index, true);
       }
     }
   }
@@ -924,7 +924,8 @@ void DraggedTabController::RevertDrag() {
     } else {
       // The Tab was moved within the TabStrip where the drag was initiated.
       // Move it back to the starting location.
-      source_tabstrip_->model()->MoveTabContentsAt(index, source_model_index_);
+      source_tabstrip_->model()->MoveTabContentsAt(index, source_model_index_,
+          true);
     }
   } else {
     // TODO(beng): (Cleanup) seems like we should use Attach() for this
