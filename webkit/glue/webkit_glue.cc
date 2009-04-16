182c6457837fb3349256932c07915083cde5137a
dkegel@google.com <dkegel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-16 00:44:53 +0000

    Per Mark's request, add back the CPU type on Linux the way Firefox does it.
    While I've got your attention: how bad an idea would
    it be to (for nonofficial builds only)
    append 'r' and the svn revision number to the chrome version?
    Review URL: http://codereview.chromium.org/67146
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13819 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkit_glue.cc
+++ webkit/glue/webkit_glue.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include <objidl.h>
 #include <mlang.h>
+#elif defined(OS_LINUX)
+#include <sys/utsname.h>
 #endif
 
 #include "BackForwardList.h"
@@ -327,6 +329,20 @@ std::string BuildOSCpuInfo() {
   base::SysInfo::OperatingSystemVersionNumbers(&os_major_version,
                                                &os_minor_version,
                                                &os_bugfix_version);
+#else
+  // Should work on any Posix system
+  // Get CPU type and operating system name
+  struct utsname unixinfo;
+  uname(&unixinfo);
+
+  std::string cputype;
+  // special case for biarch systems
+  if (strcmp(unixinfo.machine, "x86_64") == 0 &&
+      sizeof(void *) == sizeof(int32)) {
+    cputype.assign("i686 (x86_64)");
+  } else {
+    cputype.assign(unixinfo.machine);
+  }
 #endif
 
   StringAppendF(
@@ -340,8 +356,10 @@ std::string BuildOSCpuInfo() {
       os_major_version,
       os_minor_version,
       os_bugfix_version
-#elif defined(OS_LINUX)
-      "Linux"            // Firefox also puts CPU info here
+#else
+      "%s %s",
+      unixinfo.sysname, // e.g. Linux
+      cputype.c_str()   // e.g. i686
 #endif
   );
 
