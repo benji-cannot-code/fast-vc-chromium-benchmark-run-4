941bc8d62199a53644fc2702b7997e82bd5bd87e
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-16 23:39:09 +0000

    Split out the contents view into its own header. rename it to AutocompletePopupContentsView from AutocompletePopupViewContents too.
    No other changes.
    
    Review URL: http://codereview.chromium.org/79032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13901 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/autocomplete/autocomplete_popup_win.h
+++ chrome/browser/views/autocomplete/autocomplete_popup_win.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class AutocompleteEditModel;
 class AutocompleteEditViewWin;
-class AutocompletePopupViewContents;
+class AutocompletePopupContentsView;
 class Profile;
 
 // Interface to retrieve the position of the popup.
@@ -70,7 +70,7 @@ class AutocompletePopupWin : public views::WidgetWin,
   AutocompletePopupPositioner* popup_positioner_;
 
   // The view that holds the result views.
-  AutocompletePopupViewContents* contents_;
+  AutocompletePopupContentsView* contents_;
 
   DISALLOW_COPY_AND_ASSIGN(AutocompletePopupWin);
 };
