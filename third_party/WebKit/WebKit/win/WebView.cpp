e6bdf1d7861d3343e5526e7f0186839dcfd1cfea
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-16 22:17:01 +0000

    2009-04-16  Xan Lopez  <xlopez@igalia.com>
            Unreviewed build fix.
    
            * WebView.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/IntRect.h>
 #include <WebCore/KeyboardEvent.h>
 #include <WebCore/Language.h>
+#include <WebCore/Logging.h>
 #include <WebCore/MIMETypeRegistry.h>
 #include <WebCore/Page.h>
 #include <WebCore/PageCache.h>
