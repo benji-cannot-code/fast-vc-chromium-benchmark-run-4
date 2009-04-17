9683d1c08652c685b31e715a230532162bae7d68
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-17 17:21:28 +0000

    Add missing <string> include for release build.
    TBR=jcampan
    
    Review URL: http://codereview.chromium.org/67255
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13947 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui_test_utils.h
+++ chrome/test/ui_test_utils.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_TEST_UI_TEST_UTILS_H_
 #define CHROME_TEST_UI_TEST_UTILS_H_
 
+#include <string>
+
 #include "base/basictypes.h"
 #include "chrome/common/notification_observer.h"
 
