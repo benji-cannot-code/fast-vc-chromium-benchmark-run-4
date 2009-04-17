01a0dff6602273417f212c5c9c26ba8a60926fb8
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-17 04:55:08 +0000

    added windows.getWindows() and tabs.moveTab()
    Review URL: http://codereview.chromium.org/79026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13919 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_module.h
+++ chrome/browser/extensions/extension_tabs_module.h
@@ -8,14 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_function.h"
 
+class Browser;
 class TabContents;
 
 class ExtensionTabUtil {
  public:
+  static int GetWindowId(const Browser* browser);
   static int GetTabId(const TabContents* tab_contents);
   static int GetWindowIdOfTab(const TabContents* tab_contents);
 };
 
+class GetWindowsFunction : public SyncExtensionFunction {
+  virtual bool RunImpl();
+};
 class GetTabsForWindowFunction : public SyncExtensionFunction {
   virtual bool RunImpl();
 };
@@ -26,6 +31,9 @@ class GetTabFunction : public SyncExtensionFunction {
   virtual bool RunImpl();
 };
 class UpdateTabFunction : public SyncExtensionFunction {
+  virtual bool RunImpl();
+};
+class MoveTabFunction : public SyncExtensionFunction {
   virtual bool RunImpl();
 };
 class RemoveTabFunction : public SyncExtensionFunction {
