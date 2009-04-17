dabf6d61137cc251a693ac7813fcab6089547e35
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-17 00:24:43 +0000

    Reorganize fullscreen mode handling.  Now nearly everything is in WindowWin.  This also includes work area change notifications, which only really apply to Windows (and not pure Widgets) anyway.  Some parts (namely, the browser-specific things) are still in BrowserView.This allows me to fix the bug where unlocking your screen resized fullscreen mode windows incorrectly.There are probably one or two other small bits tangled in here -- this got a bit messy.  I moved the exit bubble out of browser/ because it wasn't browser-specific; in theory, any WindowWin can now go into fullscreen mode.BUG=8159 Review URL: http://codereview.chromium.org/78002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13908 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/opaque_browser_frame_view.cc
+++ chrome/browser/views/frame/opaque_browser_frame_view.cc
@@ -464,7 +464,7 @@ gfx::Point OpaqueBrowserFrameView::GetSystemMenuPoint() const {
   gfx::Point system_menu_point(
       MirroredXCoordinateInsideView(FrameBorderThickness()),
       NonClientTopBorderHeight() + browser_view_->GetTabStripHeight() -
-      (browser_view_->IsFullscreen() ? 0 : kClientEdgeThickness));
+      (frame_->IsFullscreen() ? 0 : kClientEdgeThickness));
   ConvertPointToScreen(this, &system_menu_point);
   return system_menu_point;
 }
@@ -504,7 +504,7 @@ void OpaqueBrowserFrameView::GetWindowMask(const gfx::Size& size,
                                            gfx::Path* window_mask) {
   DCHECK(window_mask);
 
-  if (!browser_view_->CanCurrentlyResize())
+  if (frame_->IsMaximized() || frame_->IsFullscreen())
     return;
 
   // Redefine the window visible region for the new size.
@@ -541,7 +541,7 @@ void OpaqueBrowserFrameView::ResetWindowControls() {
 // OpaqueBrowserFrameView, views::View overrides:
 
 void OpaqueBrowserFrameView::Paint(ChromeCanvas* canvas) {
-  if (browser_view_->IsFullscreen())
+  if (frame_->IsFullscreen())
     return;  // Nothing is visible, so don't bother to paint.
 
   if (frame_->IsMaximized())
@@ -648,7 +648,8 @@ SkBitmap OpaqueBrowserFrameView::GetFavIconForTabIconView() {
 // OpaqueBrowserFrameView, private:
 
 int OpaqueBrowserFrameView::FrameBorderThickness() const {
-  return browser_view_->CanCurrentlyResize() ? kFrameBorderThickness : 0;
+  return (frame_->IsMaximized() || frame_->IsFullscreen()) ?
+      0 : kFrameBorderThickness;
 }
 
 int OpaqueBrowserFrameView::TopResizeHeight() const {
@@ -658,7 +659,8 @@ int OpaqueBrowserFrameView::TopResizeHeight() const {
 int OpaqueBrowserFrameView::NonClientBorderThickness() const {
   // When we fill the screen, we don't show a client edge.
   return FrameBorderThickness() +
-      (browser_view_->CanCurrentlyResize() ? kClientEdgeThickness : 0);
+      ((frame_->IsMaximized() || frame_->IsFullscreen()) ?
+       0 : kClientEdgeThickness);
 }
 
 int OpaqueBrowserFrameView::NonClientTopBorderHeight() const {
@@ -667,8 +669,9 @@ int OpaqueBrowserFrameView::NonClientTopBorderHeight() const {
     return TitleCoordinates(&title_top_spacing, &title_thickness);
   }
 
-  return FrameBorderThickness() + (browser_view_->CanCurrentlyResize() ?
-      kNonClientRestoredExtraThickness : 0);
+  return FrameBorderThickness() +
+      ((frame_->IsMaximized() || frame_->IsFullscreen()) ?
+       0 : kNonClientRestoredExtraThickness);
 }
 
 int OpaqueBrowserFrameView::UnavailablePixelsAtBottomOfNonClientHeight() const {
