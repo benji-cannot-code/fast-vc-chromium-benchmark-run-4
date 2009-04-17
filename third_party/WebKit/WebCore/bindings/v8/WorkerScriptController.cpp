f84d68227a60c5734d75f3d0be1ce86e2f6ed78b
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-17 04:23:00 +0000

    2009-04-16  Eric Roman  <eroman@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            https://bugs.webkit.org/show_bug.cgi?id=25196
            Add a missing V8DOMMap include.
    
            * bindings/v8/WorkerScriptController.cpp: Included V8DOMMap.h.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42604 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/WorkerScriptController.cpp
+++ third_party/WebKit/WebCore/bindings/v8/WorkerScriptController.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScriptSourceCode.h"
 #include "ScriptValue.h"
 #include "DOMTimer.h"
+#include "V8DOMMap.h"
 #include "WorkerContext.h"
 #include "WorkerContextExecutionProxy.h"
 #include "WorkerObjectProxy.h"
