be79a5a7dc548cf1b662fa81f47c3af57a28ad7f
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-17 09:20:35 +0000

    2009-04-17  Simon Hausmann  <simon.hausmann@nokia.com>
            Unreviewed build fix.
    
            Fix the build with enabled SVG filters
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderSVGModelObject.cpp
+++ third_party/WebKit/WebCore/rendering/RenderSVGModelObject.cpp
@@ -30,9 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+
 #if ENABLE(SVG)
 #include "RenderSVGModelObject.h"
 
+#if ENABLE(SVG_FILTERS)
+#include "SVGResourceFilter.h"
+#endif
 #include "SVGStyledElement.h"
 
 namespace WebCore {
