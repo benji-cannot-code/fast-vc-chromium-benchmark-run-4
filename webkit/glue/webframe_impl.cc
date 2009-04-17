9d9ca93570dc43c440c1b97e0c8bc2692210d836
mpcomplete@google.com <mpcomplete@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-17 18:45:44 +0000

    Rename AllowCrossOriginAccessHack to GrantUniversalAccess, and move the HACK warnings to the callsite. Review URL: http://codereview.chromium.org/79028
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13953 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webframe_impl.cc
+++ webkit/glue/webframe_impl.cc
@@ -879,7 +879,7 @@ void WebFrameImpl::CallJSGC() {
 #endif
 }
 
-void WebFrameImpl::AllowCrossOriginAccessHack() {
+void WebFrameImpl::GrantUniversalAccess() {
   DCHECK(frame_ && frame_->document());
   if (frame_ && frame_->document()) {
     frame_->document()->securityOrigin()->grantUniversalAccess();
