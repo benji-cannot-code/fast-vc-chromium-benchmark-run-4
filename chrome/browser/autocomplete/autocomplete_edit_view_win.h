f85a3c532812db07d894d06c0b869c2e3ab83a3f
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-17 21:15:30 +0000

    Move the implementation of AutocompletePopupView from AutocompletePopupWin to AutocompletePopupContentsView, a more central location.
    Review URL: http://codereview.chromium.org/77012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13967 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view_win.h
+++ chrome/browser/autocomplete/autocomplete_edit_view_win.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/autocomplete.h"
 #include "chrome/browser/autocomplete/autocomplete_edit_view.h"
 #include "chrome/browser/toolbar_model.h"
-#include "chrome/browser/views/autocomplete/autocomplete_popup_win.h"
+#include "chrome/browser/views/autocomplete/autocomplete_popup_contents_view.h"
 #include "chrome/common/gfx/chrome_font.h"
 #include "chrome/common/page_transition_types.h"
 #include "chrome/views/controls/menu/menu.h"
--- chrome/browser/views/toolbar_view.h
+++ chrome/browser/views/toolbar_view.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/encoding_menu_controller_delegate.h"
 #include "chrome/browser/user_data_manager.h"
-#include "chrome/browser/views/autocomplete/autocomplete_popup_win.h"
+#include "chrome/browser/views/autocomplete/autocomplete_popup_contents_view.h"
 #include "chrome/browser/views/dom_view.h"
 #include "chrome/browser/views/go_button.h"
 #include "chrome/browser/views/location_bar_view.h"
