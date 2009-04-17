5da5e1d5e3162aa07db0207c48ee7c98b049107c
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-17 23:11:24 +0000

    Fix a couple of style issues.
    TBR=darin
    Review URL: http://codereview.chromium.org/67259
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13978 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/app_cache/app_cache_context_impl.cc
+++ chrome/common/app_cache/app_cache_context_impl.cc
@@ -40,7 +40,7 @@ void AppCacheContextImpl::Initialize(ContextType context_type,
 
   sender_->Send(new AppCacheMsg_ContextCreated(context_type,
                                                context_id_,
-                                               parent ? parent->context_id()
+                                               parent ? parent->GetContextID()
                                                       : kNoAppCacheContextId));
 }
 
