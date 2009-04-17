04b0e10cd1e810407834ad9597cdaf2da4db6534
adachan@apple.com <adachan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-17 22:54:05 +0000

    <rdar://problem/6803905> Lots of ASSERTs firing when trying to clear history
            Reviewed by Steve Falkenburg.
    
            * WebHistory.cpp:
            (WebHistory::data): Return null if there are no history items.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebHistory.cpp
+++ third_party/WebKit/WebKit/win/WebHistory.cpp
@@ -566,7 +566,11 @@ HRESULT WebHistory::data(IStream** stream)
 
     *stream = 0;
 
-    COMPtr<MemoryStream> result(AdoptCOM, MemoryStream::createInstance(SharedBuffer::wrapCFData(data().get())));
+    RetainPtr<CFDataRef> historyData = data();
+    if (!historyData)
+        return S_OK;
+
+    COMPtr<MemoryStream> result(AdoptCOM, MemoryStream::createInstance(SharedBuffer::wrapCFData(historyData.get())));
     return result.copyRefTo(stream);
 }
 
@@ -949,6 +953,9 @@ void WebHistory::addVisitedLinksToPageGroup(PageGroup& group)
 
 RetainPtr<CFDataRef> WebHistory::data() const
 {
+    if (!CFArrayGetCount(m_entriesByDate.get()))
+        return 0;
+
     WebHistoryWriter writer(m_entriesByDate.get());
     writer.writePropertyList();
     return writer.releaseData();
