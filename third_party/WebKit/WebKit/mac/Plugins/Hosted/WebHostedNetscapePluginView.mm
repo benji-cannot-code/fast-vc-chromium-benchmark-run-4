ecc7f50ce3597de99bf75ab6baec8371fa714b2f
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-21 21:35:00 +0000

    2009-04-21  Anders Carlsson  <andersca@apple.com>
            Reviewed by Darin Adler and Kevin Decker.
    
            WebKit side of <rdar://problem/6781642>.
            
            When we call resize with an actual changed size, block until the plug-in host is done.
            
            * Plugins/Hosted/NetscapePluginInstanceProxy.h:
            * Plugins/Hosted/NetscapePluginInstanceProxy.mm:
            (WebKit::NetscapePluginInstanceProxy::resize):
            * Plugins/Hosted/WebHostedNetscapePluginView.h:
            * Plugins/Hosted/WebHostedNetscapePluginView.mm:
            (-[WebHostedNetscapePluginView updateAndSetWindow]):
            * Plugins/Hosted/WebKitPluginHost.defs:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/Hosted/WebHostedNetscapePluginView.mm
+++ third_party/WebKit/WebKit/mac/Plugins/Hosted/WebHostedNetscapePluginView.mm
@@ -144,7 +144,10 @@ extern "C" {
     boundsInWindow.origin.y = borderViewHeight - NSMaxY(boundsInWindow);
     visibleRectInWindow.origin.y = borderViewHeight - NSMaxY(visibleRectInWindow);
 
-    _proxy->resize(boundsInWindow, visibleRectInWindow);
+    BOOL sizeChanged = !NSEqualSizes(_previousSize, boundsInWindow.size);
+    _previousSize = boundsInWindow.size;
+    
+    _proxy->resize(boundsInWindow, visibleRectInWindow, sizeChanged);
 }
 
 - (void)windowFocusChanged:(BOOL)hasFocus
