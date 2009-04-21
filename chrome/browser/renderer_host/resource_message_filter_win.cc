42e7793b4a16a106698d32b49b5001bfa5f728bb
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-21 01:48:32 +0000

    I managed to break test_shell. Reverting. I'll fix tomorrow.
    Reverts r14075
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14080 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/resource_message_filter_win.cc
+++ chrome/browser/renderer_host/resource_message_filter_win.cc
@@ -4,30 +4,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/renderer_host/resource_message_filter.h"
-#include "chrome/common/render_messages.h"
 
 // We get null window_ids passed into the two functions below; please see
 // http://crbug.com/9060 for more details.
 
 void ResourceMessageFilter::OnGetWindowRect(gfx::NativeViewId window_id,
-                                            IPC::Message* reply_msg) {
+                                            gfx::Rect* rect) {
   HWND window = gfx::NativeViewFromId(window_id);
   RECT window_rect = {0};
   GetWindowRect(window, &window_rect);
-  gfx::Rect rect(window_rect);
-
-  ViewHostMsg_GetWindowRect::WriteReplyParams(reply_msg, rect);
-  Send(reply_msg);
+  *rect = window_rect;
 }
 
 void ResourceMessageFilter::OnGetRootWindowRect(gfx::NativeViewId window_id,
-                                                IPC::Message* reply_msg) {
+                                                gfx::Rect* rect) {
   HWND window = gfx::NativeViewFromId(window_id);
   RECT window_rect = {0};
   HWND root_window = ::GetAncestor(window, GA_ROOT);
   GetWindowRect(root_window, &window_rect);
-  gfx::Rect rect(window_rect);
-
-  ViewHostMsg_GetRootWindowRect::WriteReplyParams(reply_msg, rect);
-  Send(reply_msg);
+  *rect = window_rect;
 }
