e00ef2d57cc2647fcc1ec50ae68432894977ba46
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-21 18:09:24 +0000

    Fix running run-safari and run-webkit-tests from the paths recommended on webkit.org
            Reviewed by Steve Falkenburg and Sam Weinig.
    
            * Scripts/run-webkit-nightly.cmd: Moved quotes from variable
            definitions to variable uses. Otherwise cmd.exe can get confused about
            the quoting.
            * Scripts/run-webkit-tests: Changed to use an absolute path to
            execAppWithEnv so that run-webkit-tests will work when run from
            outside the source tree.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42719 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/run-webkit-nightly.cmd
+++ third_party/WebKit/WebKitTools/Scripts/run-webkit-nightly.cmd
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @echo off
-set script="%TMP%\run-webkit-nightly2.cmd"
-set vsvars="%VS80COMNTOOLS%\vsvars32.bat"
-if exist %vsvars% (
-    copy %vsvars% "%script%"
+set script=%TMP%\run-webkit-nightly2.cmd
+set vsvars=%VS80COMNTOOLS%\vsvars32.bat
+if exist "%vsvars%" (
+    copy "%vsvars%" "%script%"
 ) else (
     del "%script%"
 )
 FindSafari.exe %1 /printSafariLauncher >> "%script%"
-call %script%
+call "%script%"
