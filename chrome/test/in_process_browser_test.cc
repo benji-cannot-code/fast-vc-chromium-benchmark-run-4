7a4026b104a9275072be8dee3674bbb432af5fca
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-21 22:01:01 +0000

    Stop using and remove deprecated file_util::TrimFilename(). Review URL: http://codereview.chromium.org/67267
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14142 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/in_process_browser_test.cc
+++ chrome/test/in_process_browser_test.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/in_process_browser_test.h"
 
 #include "base/command_line.h"
+#include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/path_service.h"
 #include "chrome/browser/browser.h"
@@ -115,7 +116,8 @@ void InProcessBrowserTest::SetUp() {
   // try to use unit_test.exe.
   std::wstring renderer_path;
   PathService::Get(base::FILE_EXE, &renderer_path);
-  file_util::TrimFilename(&renderer_path);
+  FilePath fp_renderer_path = FilePath::FromWStringHack(renderer_path);
+  renderer_path = fp_renderer_path.DirName().ToWStringHack();
   file_util::AppendToPath(&renderer_path,
                           chrome::kBrowserProcessExecutableName);
   command_line->AppendSwitchWithValue(switches::kRendererPath, renderer_path);
