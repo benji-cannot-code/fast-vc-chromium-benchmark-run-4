233abf16025baa6a99b5bfd62d9e8ac8aababe76
ariya@webkit.org <ariya@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-22 12:08:55 +0000

    2009-04-22  Tamas Szirbucz  <szirbucz.tamas@stud.u-szeged.hu>
            Reviewed by Ariya Hidayat.
    
            https://bugs.webkit.org/show_bug.cgi?id=25023
            Delete reply in QNetworkReplyHandler::abort() to avoid leak.
    
            * platform/network/qt/QNetworkReplyHandler.cpp:
            (WebCore::QNetworkReplyHandler::abort):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
@@ -175,6 +175,7 @@ void QNetworkReplyHandler::abort()
     if (m_reply) {
         QNetworkReply* reply = release();
         reply->abort();
+        reply->deleteLater();
         deleteLater();
     }
 }
