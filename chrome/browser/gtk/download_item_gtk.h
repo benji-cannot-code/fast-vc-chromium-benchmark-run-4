d40519c3b4fece3d4046f8a51370d740c6925326
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-22 18:54:11 +0000

    Add new item animation to download shelf.
    Also, fix duration and tween type for download shelf open animation.
    
    Review URL: http://codereview.chromium.org/88064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14220 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/download_item_gtk.h
+++ chrome/browser/gtk/download_item_gtk.h
@@ -10,12 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/scoped_ptr.h"
 #include "chrome/browser/download/download_manager.h"
+#include "chrome/common/animation.h"
 
 class BaseDownloadItemModel;
 class DownloadShelfContextMenuGtk;
 class NineBox;
+class SlideAnimation;
 
-class DownloadItemGtk : DownloadItem::Observer {
+class DownloadItemGtk : public DownloadItem::Observer,
+                        public AnimationDelegate {
  public:
   // DownloadItemGtk takes ownership of |download_item_model|.
   DownloadItemGtk(BaseDownloadItemModel* download_item_model,
@@ -25,9 +28,12 @@ class DownloadItemGtk : DownloadItem::Observer {
   // destroying its children. Hence we do nothing in the destructor.
   ~DownloadItemGtk();
 
-  // DownloadItem::Observer implementation
+  // DownloadItem::Observer implementation.
   virtual void OnDownloadUpdated(DownloadItem* download);
   virtual void OnDownloadOpened(DownloadItem* download) { }
+
+  // AnimationDelegate implementation.
+  virtual void AnimationProgressed(const Animation* animation);
 
  private:
   static void InitNineBoxes();
@@ -60,6 +66,9 @@ class DownloadItemGtk : DownloadItem::Observer {
   // animation.
   GtkWidget* body_;
 
+  // The GtkLabel that holds the download title text.
+  GtkWidget* name_label_;
+
   // The GtkLabel that holds the status text.
   GtkWidget* status_label_;
 
@@ -79,6 +88,9 @@ class DownloadItemGtk : DownloadItem::Observer {
   // This is the leftmost widget on |parent_shelf_| that is not a download item.
   // We do not want to overlap it.
   GtkWidget* bounding_widget_;
+
+  // The animation when this item is first added to the shelf.
+  scoped_ptr<SlideAnimation> new_item_animation_;
 };
 
 #endif  // CHROME_BROWSER_GTK_DOWNLOAD_ITEM_GTK_H_
