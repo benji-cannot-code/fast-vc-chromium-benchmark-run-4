6f9388facf0af18d78a20956e5de2968dba8840e
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-22 22:28:45 +0000

    implement remaining tab events (except for onTabUpdated).
    Review URL: http://codereview.chromium.org/88053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14255 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_browser_event_router.h
+++ chrome/browser/extensions/extension_browser_event_router.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_EXTENSIONS_EXTENSION_BROWSER_EVENT_ROUTER_H_
 
 #include <vector>
+#include <set>
 #include <string>
 
 #include "base/basictypes.h"
@@ -49,6 +50,10 @@ class ExtensionBrowserEventRouter : public TabStripModelObserver,
   friend struct DefaultSingletonTraits<ExtensionBrowserEventRouter>;
 
   bool initialized_;
+
+  // Maintain set of known tab ids, so we can distinguish between tab creation
+  // and tab insertion. Also used to not send tab-detached after tab-removed.
+  std::set<int> tab_ids_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionBrowserEventRouter);
 };
