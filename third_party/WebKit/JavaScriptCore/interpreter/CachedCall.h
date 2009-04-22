d401f236626bd83b694a06e3881c72616bb14bc9
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-22 07:22:19 +0000

    Build fix
    git-svn-id: svn://svn.chromium.org/blink/trunk@42745 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/interpreter/CachedCall.h
+++ third_party/WebKit/JavaScriptCore/interpreter/CachedCall.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CachedCall_h
 
 #include "CallFrameClosure.h"
+#include "JSFunction.h"
+#include "JSGlobalObject.h"
 #include "Interpreter.h"
 
 namespace JSC {
