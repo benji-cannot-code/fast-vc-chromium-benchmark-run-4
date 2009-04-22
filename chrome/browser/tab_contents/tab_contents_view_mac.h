80848e806838a7d4a6dfc8270b113f1ab6447ec3
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-22 22:45:44 +0000

    Don't send resize messages until the renderer has been initialized.
    1) Moves renderer_initialized_ from RenderWidgetView into RenderWidgetHost (its parent).  Should be identical semantics to before.
    2) Test renderer_initialized_ in RWH::WasResized().
    
    This also reverts r13725, which was another attempt at fixing this problem.
    
    BUG=9830,10659
    TEST=From the Linux start page, click a link then click back -- should not get a
    gray page.  From Google reader, click a link (which spawns a new tab within the
    same process) -- should not get a gray page.
    
    Review URL: http://codereview.chromium.org/93038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14260 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents_view_mac.h
+++ chrome/browser/tab_contents/tab_contents_view_mac.h
@@ -59,7 +59,6 @@ class TabContentsViewMac : public TabContentsView,
   virtual void SetInitialFocus();
   virtual void StoreFocus();
   virtual void RestoreFocus();
-  virtual void SetChildSize(RenderWidgetHostView* rwh_view);
 
   // Backend implementation of RenderViewHostDelegate::View.
   virtual RenderWidgetHostView* CreateNewWidgetInternal(int route_id,
