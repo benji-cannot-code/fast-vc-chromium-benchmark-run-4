c4ed0fe17adb501a1da165fdc91b6b855242d7e1
dimich@google.com <dimich@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-22 00:36:16 +0000

    Move test_worker dll files in hierarchy to reflect the fact that this dll is only used by the test_shell. Also inserted a stub WebKitClient implementation into test_worker_main to remove dependency of webkit on chrome. Review URL: http://codereview.chromium.org/87056
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14168 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_worker/test_webworker.cc
+++ webkit/tools/test_shell/test_worker/test_webworker.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(WORKERS)
 
-#include "chrome/test/worker/test_webworker.h"
+#include "webkit/tools/test_shell/test_worker/test_webworker.h"
 
 #include "base/compiler_specific.h"
 #include "base/task.h"
