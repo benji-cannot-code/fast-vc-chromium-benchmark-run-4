ffc565000a9b24acef988264f8bd30aee1049967
ariya@webkit.org <ariya@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-23 13:53:10 +0000

    2009-04-23  Antonio Gomes   <antonio.gomes@openbossa.org>
            Reviewed by Ariya Hidayat.
    
            [Qt] Added QWebElement::computedStyleProperty method.
    
            * Api/qwebelement.cpp:
            (QWebElement::computedStyleProperty):
            * Api/qwebelement.h:
            * tests/qwebelement/tst_qwebelement.cpp:
            (tst_QWebElement::computedStyle):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42777 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebelement.h
+++ third_party/WebKit/WebKit/qt/Api/qwebelement.h
@@ -127,6 +127,8 @@ public:
     QString styleProperty(const QString &name) const;
     void setStyleProperty(const QString &name, const QString &value);
 
+    QString computedStyleProperty(const QString &name) const;
+
 private:
     QWebElement(WebCore::Element *domElement);
 
