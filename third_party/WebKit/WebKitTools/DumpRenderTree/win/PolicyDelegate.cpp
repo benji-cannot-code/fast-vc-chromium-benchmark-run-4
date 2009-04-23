5c6bddbb2563706c433c47391dbeeed5865d7f07
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-23 07:10:51 +0000

    2009-04-23  Brady Eidson  <beidson@apple.com>
            Fix Windows DRT build.
    
            * DumpRenderTree/win/PolicyDelegate.cpp:
            (PolicyDelegate::unableToImplementPolicyWithError):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42774 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/win/PolicyDelegate.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/win/PolicyDelegate.cpp
@@ -169,7 +169,7 @@ HRESULT STDMETHODCALLTYPE PolicyDelegate::unableToImplementPolicyWithError(
     /*[in]*/ IWebFrame* frame)
 {
     BSTR domainStr;
-    error->domain(&errorStr);
+    error->domain(&domainStr);
     wstring domainMessage = domainStr;
 
     int code;
