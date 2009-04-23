97a96cc9bd41670f87b572458a973a56bcf068e0
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-23 23:59:37 +0000

    WebCore:
    2009-04-23  Kevin McCullough  <kmccullough@apple.com>
    
            Reviewed by Adam Roben.
    
            <rdar://problem/6808109> "Deletion UI" is not available for many
            portions of HTML content
    
            This patch makes the deletion UI show up in some new situations:
            1) If a block's background color is different from its parent's
            2) If a block has a background image.
            3) If a block has a single visible border.
            However the block must now not only be at least a minimum width and
            height, but also exceed a minimum area.  In practice this has led to
            much better element selection. 
    
            * editing/DeleteButtonController.cpp:
            (WebCore::isDeletableElement):
    
    LayoutTests:
    
    2009-04-23  Kevin McCullough  <kmccullough@apple.com>
    
            Reviewed by Adam Roben.
    
            <rdar://problem/6808109> "Deletion UI" is not available for many
            portions of HTML content
    
            This patch makes the deletion UI show up in some new situations:
            1) If a block's background color is different from its parent's
            2) If a block has a background image.
            3) If a block has a single visible border.
            However the block must now not only be at least a minimum width and
            height, but also exceed a minimum area.  In practice this has led to
            much better element selection.
    
            * editing/deleting/deletionUI-borders.html: Added.
            * editing/deleting/deletionUI-differing-background.html: Added.
            * editing/deleting/deletionUI-minimum-size.html: Added.
            * editing/deleting/resources: Added.
            * editing/deleting/resources/deletionUI-helpers.js: Added.
            (debug):
            (determineDeletionUIExistence):
            * platform/mac/editing/deleting/deletionUI-borders-expected.txt: Added.
            * platform/mac/editing/deleting/deletionUI-differing-background-expected.txt: Added.
            * platform/mac/editing/deleting/deletionUI-minimum-size-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42798 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/editing/deleting/resources/deletionUI-helpers.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
+function debug(msg)
+{
+    var span = document.createElement("span");
+    document.getElementById("console").appendChild(span); // insert it first so XHTML knows the namespace
+    span.innerHTML = msg + '<br />';
+}
+
+function determineDeletionUIExistence(id) {
+    var sel = window.getSelection();
+    var selElement = document.getElementById(id);
+    sel.setPosition(selElement, 0);
+    var deleteButton = document.getElementById("WebKit-Editing-Delete-Button");
+    debug(id + (deleteButton ? " HAS " : " has NO ") + "deletion UI");
+}
