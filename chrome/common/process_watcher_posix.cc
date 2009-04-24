c1a1ca5dcdba6ff37e831078ed899deb583ff52e
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-24 22:47:45 +0000

    Mac: build fix.
    Different header paths it seems, although the tryservers were ok with
    it.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14493 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/process_watcher_posix.cc
+++ chrome/common/process_watcher_posix.cc
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/process_watcher.h"
 
 #include <errno.h>
+#include <signal.h>
 #include <sys/types.h>
-#include <sys/signal.h>
 #include <sys/wait.h>
 
 #include "base/platform_thread.h"
