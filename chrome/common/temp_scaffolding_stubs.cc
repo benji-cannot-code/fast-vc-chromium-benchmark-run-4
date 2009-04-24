13a2c545f997ad77f4461252784bf938ec2955eb
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-24 23:27:11 +0000

    Revert "POSIX: Don't spawn zombies." (r14488)
    Something else is trying to reap children in the ui_tests and causing
    a mess. Reverting since it's a Friday night.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14499 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.cc
+++ chrome/common/temp_scaffolding_stubs.cc
@@ -233,6 +233,11 @@ LoginHandler* CreateLoginPrompt(net::AuthChallengeInfo* auth_info,
   return NULL;
 }
 
+void ProcessWatcher::EnsureProcessTerminated(int) {
+  NOTIMPLEMENTED();
+}
+
+
 //--------------------------------------------------------------------------
 namespace webkit_glue {
 
