260fdc76f4b4302f3f54b1abc544d9f6f93b8d32
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-24 21:22:28 +0000

    Do not send stats from Chromium builds.
    BUG=1435533
    
    Review URL: http://codereview.chromium.org/93109
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14474 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/browser_distribution.cc
+++ chrome/installer/util/browser_distribution.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -8,8 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // specific branding, we will need to extend this class with a custom
 // implementation.
 
-#include "base/registry.h"
 #include "chrome/installer/util/browser_distribution.h"
+
+#include "base/registry.h"
 #include "chrome/installer/util/google_chrome_distribution.h"
 
 BrowserDistribution* BrowserDistribution::GetDistribution() {
@@ -56,6 +57,14 @@ int BrowserDistribution::GetInstallReturnCode(
 
 std::wstring BrowserDistribution::GetStateKey() {
   return L"Software\\Chromium";
+}
+
+std::wstring BrowserDistribution::GetStateMediumKey() {
+  return L"Software\\Chromium";
+}
+
+std::wstring BrowserDistribution::GetStatsServerURL() {
+  return L"";
 }
 
 std::wstring BrowserDistribution::GetDistributionData(RegKey* key) {
