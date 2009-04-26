4f7a5d607dce7e703c4af64ba3efd4dfd4b0f366
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-26 23:35:22 +0000

    Add some histograms to see how often users click through blocking pages.
    R=jar
    
    Review URL: http://codereview.chromium.org/99020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14569 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_blocking_page.cc
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/safe_browsing/safe_browsing_blocking_page.h"
 
+#include "base/histogram.h"
 #include "base/string_util.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/dom_operation_notification_details.h"
@@ -50,6 +51,21 @@ static const char* const kLearnMoreCommand = "learnMore";
 static const char* const kProceedCommand = "proceed";
 static const char* const kTakeMeBackCommand = "takeMeBack";
 
+namespace {
+
+enum SafeBrowsingBlockingPageEvent {
+  SHOW,
+  PROCEED,
+  DONT_PROCEED,
+};
+
+void RecordSafeBrowsingBlockingPageStats(SafeBrowsingBlockingPageEvent event) {
+  static LinearHistogram histogram("interstial.safe_browsing", 0, 2, 4);
+  histogram.SetFlags(kUmaTargetedHistogramFlag);
+  histogram.Add(event);
+}
+
+}  // namespace
 // static
 SafeBrowsingBlockingPageFactory* SafeBrowsingBlockingPage::factory_ = NULL;
 
@@ -84,6 +100,7 @@ SafeBrowsingBlockingPage::SafeBrowsingBlockingPage(
       sb_service_(sb_service),
       is_main_frame_(IsMainPage(unsafe_resources)),
       unsafe_resources_(unsafe_resources) {
+  RecordSafeBrowsingBlockingPageStats(SHOW);
   if (!is_main_frame_) {
     navigation_entry_index_to_remove_ =
         tab()->controller().last_committed_entry_index();
@@ -363,6 +380,8 @@ void SafeBrowsingBlockingPage::CommandReceived(const std::string& cmd) {
 }
 
 void SafeBrowsingBlockingPage::Proceed() {
+  RecordSafeBrowsingBlockingPageStats(PROCEED);
+
   NotifySafeBrowsingService(sb_service_, unsafe_resources_, true);
 
   // Check to see if some new notifications of unsafe resources have been
@@ -388,6 +407,8 @@ void SafeBrowsingBlockingPage::Proceed() {
 }
 
 void SafeBrowsingBlockingPage::DontProceed() {
+  RecordSafeBrowsingBlockingPageStats(DONT_PROCEED);
+
   NotifySafeBrowsingService(sb_service_, unsafe_resources_, false);
 
   // The user does not want to proceed, clear the queued unsafe resources
