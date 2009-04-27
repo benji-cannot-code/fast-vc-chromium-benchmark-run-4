cf4076c9bb3ca4853a08f3ac9dcf071f83272374
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-27 23:56:20 +0000

    Disable a test that fails on mac.  I enabled this in r14684, but it's not ready yet for mac.
    TBR=evan
    
    Review URL: http://codereview.chromium.org/100083
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14686 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automation/automation_proxy_uitest.cc
+++ chrome/test/automation/automation_proxy_uitest.cc
@@ -402,6 +402,8 @@ class AutomationProxyTest2 : public AutomationProxyVisibleTest {
   std::wstring document2_;
 };
 
+// TODO(port): Port to mac.
+#if defined(OS_WIN) || defined(OS_LINUX)
 TEST_F(AutomationProxyTest2, GetActiveTabIndex) {
   scoped_ptr<BrowserProxy> window(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(window.get());
@@ -417,8 +419,6 @@ TEST_F(AutomationProxyTest2, GetActiveTabIndex) {
   ASSERT_EQ(at_index, active_tab_index);
 }
 
-// TODO(port): Port to mac.
-#if defined(OS_WIN) || defined(OS_LINUX)
 TEST_F(AutomationProxyTest2, GetTabTitle) {
   scoped_ptr<BrowserProxy> window(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(window.get());
