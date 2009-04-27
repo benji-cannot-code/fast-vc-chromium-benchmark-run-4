8f06d943a8912d3724aff6dde6646df71dd8dae4
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-27 23:12:07 +0000

    2009-04-27  Sam Weinig  <sam@webkit.org>
            Reviewed by Geoffrey Garen.
    
            Fix for <rdar://problem/6772944> REGRESSION (r42223): PLT 2% slower, i-Bench 3% slower
    
            Be slightly less eager in collecting after page close/navigation by increasing
            the time before collection from 0 seconds to .5 seconds.
    
            3% speedup on the PLT.
    
            * bindings/js/GCController.cpp:
            (WebCore::GCController::garbageCollectSoon):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/GCController.cpp
+++ third_party/WebKit/WebCore/bindings/js/GCController.cpp
@@ -66,7 +66,7 @@ GCController::GCController()
 void GCController::garbageCollectSoon()
 {
     if (!m_GCTimer.isActive())
-        m_GCTimer.startOneShot(0);
+        m_GCTimer.startOneShot(0.5);
 }
 
 void GCController::gcTimerFired(Timer<GCController>*)
