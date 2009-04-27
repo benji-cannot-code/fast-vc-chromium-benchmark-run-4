6a8dc754db1880051056c833ee3d647b385344bd
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-27 21:15:40 +0000

    Update selected line in model before accepting input when the user clicks in the popup. BUG=10932
    Review URL: http://codereview.chromium.org/93130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14659 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_popup_view_mac.mm
+++ chrome/browser/autocomplete/autocomplete_popup_view_mac.mm
@@ -182,9 +182,10 @@ const std::wstring& AutocompletePopupViewMac::ResultDescriptionAt(size_t i) {
   return model_->result().match_at(i).description;
 }
 
-void AutocompletePopupViewMac::AcceptInput(
-    WindowOpenDisposition disposition, bool for_drop) {
-  edit_view_->AcceptInput(disposition, for_drop);
+void AutocompletePopupViewMac::AcceptInput() {
+  NSTableView* table = [popup_ contentView];
+  model_->SetSelectedLine([table selectedRow], false);
+  edit_view_->AcceptInput(CURRENT_TAB, false);
 }
 
 @implementation AutocompleteTableTarget
@@ -250,7 +251,7 @@ objectValueForTableColumn:(NSTableColumn*)aTableColumn
 
 - (void)select:sender {
   DCHECK(popup_view_);
-  popup_view_->AcceptInput(CURRENT_TAB, false);
+  popup_view_->AcceptInput();
 }
 
 @end
