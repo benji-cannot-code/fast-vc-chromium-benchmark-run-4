b33277ff9de381d94faa8df6c27ed77d1d1a4703
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-27 20:12:04 +0000

    2009-04-27  Dimitri Glazkov  <dglazkov@chromium.org>
            Build fix.
    
            https://bugs.webkit.org/show_bug.cgi?id=25382
            Remove direct reference to CallFrame.h include to pacify Win build.
    
            * bindings/js/JSDOMBinding.h: Removed ScriptState.h include.
            * bindings/js/ScriptState.h: Replaced CallFrame.h with JSDOMBinding.h
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42903 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDOMBinding.h
+++ third_party/WebKit/WebCore/bindings/js/JSDOMBinding.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <runtime/Completion.h>
 #include <runtime/Lookup.h>
 #include <runtime/JSFunction.h>
-#include "ScriptState.h"
 #include <wtf/Noncopyable.h>
 
 namespace JSC {
