0f11117e303b49afbc807cfa800fd004f062f0ad
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-28 03:39:20 +0000

    2009-04-27  Eric Roman  <eroman@chromium.org>
            Reviewed by Darin Fisher.
    
            Fix a compile breakage.
            <https://bugs.webkit.org/show_bug.cgi?id=25384>
    
            * bindings/v8/custom/V8DOMWindowCustom.cpp:
            (WebCore::CALLBACK_FUNC_DECL):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42919 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
@@ -130,7 +130,7 @@ CALLBACK_FUNC_DECL(DOMWindowAddEventListener)
     if (listener) {
         String eventType = toWebCoreString(args[0]);
         bool useCapture = args[2]->BooleanValue();
-        doc->addWindowEventListener(eventType, listener, useCapture);
+        imp->addEventListener(eventType, listener, useCapture);
     }
 
     return v8::Undefined();
@@ -161,7 +161,7 @@ CALLBACK_FUNC_DECL(DOMWindowRemoveEventListener)
     if (listener) {
         String eventType = toWebCoreString(args[0]);
         bool useCapture = args[2]->BooleanValue();
-        doc->removeWindowEventListener(eventType, listener.get(), useCapture);
+        imp->removeEventListener(eventType, listener.get(), useCapture);
     }
 
     return v8::Undefined();
