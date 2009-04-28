adde474795a55bcfbd71ad384f4c2ddd89909a1d
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-28 23:49:14 +0000

    Fix mac build break.
    Review URL: http://codereview.chromium.org/100139
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14801 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_strip_controller.mm
+++ chrome/browser/cocoa/tab_strip_controller.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tabs/tab_strip_model.h"
 #include "chrome/common/l10n_util.h"
 #include "grit/chromium_strings.h"
+#include "grit/generated_resources.h"
 
 @implementation TabStripController
 
--- chrome/common/platform_util_mac.mm
+++ chrome/common/platform_util_mac.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/cocoa/tab_window_controller.h"
 #include "chrome/common/l10n_util.h"
 #include "grit/chromium_strings.h"
+#include "grit/generated_resources.h"
 
 namespace platform_util {
 
