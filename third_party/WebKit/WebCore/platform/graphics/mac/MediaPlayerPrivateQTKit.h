948728e0123275972c4d7202c9b4c7641d7fc884
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-28 01:01:00 +0000

    2009-04-27  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Darin Adler.
    
            <rdar://problem/6822344> Use of QTMovieCurrentSizeAttribute generates exception and will crash
    
            QTMovieCurrentSizeAttribute generates an exception with some versions of QTKit, so calculate a 
            multiplier to scale from natural size to current size when a movie is opened and use that to 
            return the correct value from the naturalSize() method.
    
            * platform/graphics/mac/MediaPlayerPrivateQTKit.h:
            * platform/graphics/mac/MediaPlayerPrivateQTKit.mm:
            (WebCore::MediaPlayerPrivate::MediaPlayerPrivate): Initialize m_scaleFactor.
            (WebCore::MediaPlayerPrivate::naturalSize): Return naturalSize transformed by initial scale.
            (WebCore::MediaPlayerPrivate::cacheMovieScale): New, calculate difference between initial
            size and natural size so naturalSize() accounts for non-identity movie matrix.
            (WebCore::MediaPlayerPrivate::updateStates): Call cacheMovieScale when load state reaches
            QTMovieLoadStateLoaded for the first time.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42918 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/MediaPlayerPrivateQTKit.h
+++ third_party/WebKit/WebCore/platform/graphics/mac/MediaPlayerPrivateQTKit.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "MediaPlayerPrivate.h"
 #include "Timer.h"
+#include "FloatSize.h"
 #include <wtf/RetainPtr.h>
 
 #ifdef __OBJC__
@@ -129,7 +130,7 @@ private:
     void disableUnsupportedTracks();
     
     void sawUnsupportedTracks();
-
+    void cacheMovieScale();
     bool metaDataAvailable() const { return m_qtMovie && m_readyState >= MediaPlayer::HaveMetadata; }
 
     MediaPlayer* m_player;
@@ -145,6 +146,7 @@ private:
     bool m_isStreaming;
     bool m_visible;
     IntRect m_rect;
+    FloatSize m_scaleFactor;
     unsigned m_enabledTrackCount;
     unsigned m_totalTrackCount;
     bool m_hasUnsupportedTracks;
