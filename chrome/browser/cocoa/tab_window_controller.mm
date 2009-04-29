771565f9a51a4e618b45bf8a8df7877242bbd5c7
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-29 22:08:04 +0000

    Move window chrome into drag overlay so they stand out more during a drag out of a window. Review URL: http://codereview.chromium.org/99206
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14898 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_window_controller.mm
+++ chrome/browser/cocoa/tab_window_controller.mm
@@ -44,7 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (NSArray*)viewsToMoveToOverlay {
-  return [NSArray arrayWithObject:[self tabStripView]];
+  return [NSArray arrayWithObjects:[self tabStripView],
+            [self tabContentArea], nil];
 }
 
 // if |useOverlay| is true, we're moving views into the overlay's content
