b10564d7ebcdc671fa287e0d6aa2ab44f42e2e1e
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-29 00:17:07 +0000

    Rename -[DOMRange lineBoxRects] to -[DOMRange textRects] and change how it         collects the individual rects, making sure to only include RenderText.
            <rdar://problem/6810061>
    
            Reviewed by Sam Weinig.
    
            * bindings/objc/DOM.mm:
            (-[DOMNode textRects]): Added. Make a Range and call textRects.
            (-[DOMNode lineBoxRects]): Call textRects method.
            (-[DOMRange textRects]): Call Range::textRects.
            (-[DOMRange lineBoxRects]): Call textRects method.
            * bindings/objc/DOMPrivate.h: Add the new methods and a comment
            about lineBoxRects being deprecated.
            * dom/Range.cpp:
            (WebCore::Range::boundingBox): Call textRects (renamed from addLineBoxRects).
            (WebCore::Range::textRects): Renamed from addLineBoxRects. Iterate over the
            nodes instead of the RenderObjects to correctly account for offsets. Also
            only call absoluteRectsForRange on RenderText.
            * dom/Range.h: Renamed addLineBoxRects to textRects.
            * page/Frame.cpp:
            (WebCore::Frame::selectionTextRects): Call textRects (renamed from addLineBoxRects).
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::absoluteRectsForRange): Remove if conditions that made this
            function very specific for the lineBoxRects API. These functions are still used
            by the InspectorController, and this change improves what the Inspector shows.
            (WebCore::RenderObject::absoluteQuadsForRange): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42974 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/DOMPrivate.h
+++ third_party/WebKit/WebCore/bindings/objc/DOMPrivate.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface DOMNode (DOMNodeExtensionsPendingPublic)
 - (NSImage *)renderedImage;
+- (NSArray *)textRects;
 @end
 
 // FIXME: this should be removed as soon as all internal Apple uses of it have been replaced with
@@ -45,7 +46,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface DOMRange (DOMRangeExtensions)
 - (NSRect)boundingBox;
-- (NSArray *)lineBoxRects;
+- (NSArray *)lineBoxRects; // Deprecated. Use textRects instead.
+- (NSArray *)textRects;
 @end
 
 @interface DOMElement (WebPrivate)
