2a66c28d1e3e8ad65435c88526dc092edf7125ef
ariya@webkit.org <ariya@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-29 13:55:38 +0000

    2009-04-29  Ariya Hidayat  <ariya.hidayat@nokia.com>
            Reviewed by Simon Hausmann.
    
            Implement QWebElement::evaluateScript.
    
            * Api/qwebelement.cpp:
            (setupScriptContext):
            (QWebElement::evaluateScript):
            * Api/qwebelement.h:
            * tests/qwebelement/tst_qwebelement.cpp:
            (tst_QWebElement::evaluateScript):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@42995 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebelement.h
+++ third_party/WebKit/WebKit/qt/Api/qwebelement.h
@@ -119,6 +119,8 @@ public:
     void removeFromDocument();
     void removeChildren();
 
+    QVariant evaluateScript(const QString& scriptSource);
+
     QVariant callFunction(const QString &functionName, const QVariantList &arguments = QVariantList());
     QStringList functions() const;
 
