1bed53612461026f6f882853d3129cd57f689654
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-29 20:12:54 +0000

    2009-04-29  John Abd-El-Malek  <jam@chromium.org>
            Reviewed by Darin Adler.
    
            Expose sudden termination to Chromium port.
            https://bugs.webkit.org/show_bug.cgi?id=25457
    
            * platform/SuddenTermination.h:
            * platform/chromium/ChromiumBridge.h:
            * platform/chromium/SuddenTerminationChromium.cpp: Added.
            (WebCore::disableSuddenTermination):
            (WebCore::enableSuddenTermination):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/ChromiumBridge.h
+++ third_party/WebKit/WebCore/platform/chromium/ChromiumBridge.h
@@ -136,6 +136,9 @@ namespace WebCore {
         static void decrementStatsCounter(const char* name);
         static void incrementStatsCounter(const char* name);
 
+        // Sudden Termination
+        static void suddenTerminationChanged(bool enabled);
+
         // SystemTime ---------------------------------------------------------
         static double currentTime();
 
