a2acbb4fae959db4b16f58b7b9324859f709aa42
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-29 21:38:14 +0000

    2009-03-27  Benjamin C Meyer  <benjamin.meyer@torchmobile.com>
            Reviewed by Simon Hausmann.
    
            Update the page actions when a finishing loading even if the frame is
            not the top frame such as when browsing inside of a website with frames.
            https://bugs.webkit.org/show_bug.cgi?id=24890
    
            * WebCoreSupport/FrameLoaderClientQt.cpp:
            (WebCore::FrameLoaderClientQt::dispatchDidFinishLoad):
            * tests/qwebpage/frametest/frame_a.html: Added.
            * tests/qwebpage/frametest/index.html: Added.
            * tests/qwebpage/tst_qwebpage.cpp:
            (tst_QWebPage::backActionUpdate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43015 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
@@ -388,7 +388,7 @@ void FrameLoaderClientQt::dispatchDidFinishLoad()
 
     m_loadSucceeded = true;
 
-    if (m_frame->tree()->parent() || !m_webFrame)
+    if (!m_webFrame)
         return;
     m_webFrame->page()->d->updateNavigationActions();
 }
