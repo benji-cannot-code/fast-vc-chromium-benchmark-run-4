fb5473367a1ab057e29dc2120119105431fa4355
ariya@webkit.org <ariya@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-30 10:04:53 +0000

    2009-04-30  Ariya Hidayat  <ariya.hidayat@nokia.com>
            Unreviewed build fix, after r43035.
    
            Temporarily use Position::deprecatedEditingOffset().
    
            * Api/qwebpage.cpp:
            (QWebPage::inputMethodQuery):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -1071,7 +1071,7 @@ QVariant QWebPage::inputMethodQuery(Qt::InputMethodQuery property) const
         if (frame) {
             VisibleSelection selection = frame->selection()->selection();
             if (selection.isCaret()) {
-                return QVariant(selection.start().m_offset);
+                return QVariant(selection.start().deprecatedEditingOffset());
             }
         }
         return QVariant();
