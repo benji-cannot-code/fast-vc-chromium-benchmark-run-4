3b735aa9bbbd02af5b815bd763771d58681f0093
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-30 15:24:37 +0000

    Implement a mostly working InfoBubble with a shim BookmarkBubble.
    This ended up being implemented as a toplevel instead of a popup, to handle
    things like virtual desktop switching.  I imagine there are some other problems
    we might hit, like the window getting decorated, etc.  Although I think the
    shape mask might prevent decorations from being visible.  This is not pixel
    perfect with Windows, since we're not anti-aliasing the frame border.
    
    Review URL: http://codereview.chromium.org/100203
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14941 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_window_gtk.cc
+++ chrome/browser/gtk/browser_window_gtk.cc
@@ -488,7 +488,7 @@ void BrowserWindowGtk::ShowBookmarkManager() {
 
 void BrowserWindowGtk::ShowBookmarkBubble(const GURL& url,
                                           bool already_bookmarked) {
-  NOTIMPLEMENTED();
+  toolbar_->star()->ShowStarBubble(url, !already_bookmarked);
 }
 
 void BrowserWindowGtk::ShowReportBugDialog() {
--- chrome/browser/gtk/toolbar_star_toggle_gtk.cc
+++ chrome/browser/gtk/toolbar_star_toggle_gtk.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/gtk/toolbar_star_toggle_gtk.h"
 
+#include "base/gfx/rect.h"
+#include "chrome/browser/gtk/bookmark_bubble_gtk.h"
 #include "chrome/common/resource_bundle.h"
 #include "grit/theme_resources.h"
 
@@ -26,6 +28,22 @@ ToolbarStarToggleGtk::ToolbarStarToggleGtk()
 
 ToolbarStarToggleGtk::~ToolbarStarToggleGtk() {
   widget_.Destroy();
+}
+
+void ToolbarStarToggleGtk::ShowStarBubble(const GURL& url,
+                                          bool newly_bookmarked) {
+  GtkWidget* widget = widget_.get();
+  gint x, y;
+  gdk_window_get_origin(widget->window, &x, &y);
+  x += widget->allocation.x;
+  y += widget->allocation.y;
+  gint width = widget->allocation.width;
+  gint height = widget->allocation.height;
+
+  BookmarkBubbleGtk::Show(gfx::Rect(x, y, width, height),
+                          NULL,
+                          url,
+                          newly_bookmarked);
 }
 
 void ToolbarStarToggleGtk::SetStarred(bool starred) {
