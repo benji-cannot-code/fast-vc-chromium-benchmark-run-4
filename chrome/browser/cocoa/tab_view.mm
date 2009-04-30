bae8aa986833bbf0e8283976498fcd402464c6fa
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-30 20:58:19 +0000

    Changes the way we calculate hit rects in TabView to reduce overlap between tabs.
    TEST=Open a few tabs, try clicking on the right half of the close button that's
    to the left of the selected tab.
    
    Review URL: http://codereview.chromium.org/99225
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14990 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_view.mm
+++ chrome/browser/cocoa/tab_view.mm
@@ -33,8 +33,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // view or our child close button.
 - (NSView *)hitTest:(NSPoint)aPoint {
   NSPoint viewPoint = [self convertPoint:aPoint fromView:[self superview]];
+  NSRect frame = [self frame];
+
+  // Reduce the width of the hit rect slightly to remove the overlap
+  // between adjacent tabs.  The drawing code in TabCell has the top
+  // corners of the tab inset by height*2/3, so we inset by half of
+  // that here.  This doesn't completely eliminate the overlap, but it
+  // works well enough.
+  NSRect hitRect = NSInsetRect(frame, frame.size.height / 3.0f, 0);
   if (NSPointInRect(viewPoint, [closeButton_ frame])) return closeButton_;
-  if (NSPointInRect(aPoint, [self frame])) return self;
+  if (NSPointInRect(aPoint, hitRect)) return self;
   return nil;
 }
 
