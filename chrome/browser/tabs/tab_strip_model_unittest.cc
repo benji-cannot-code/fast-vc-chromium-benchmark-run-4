67dde59cf1eb5c52e4575972b10654271a65e875
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-30 16:30:38 +0000

    Closing the last tab with a download in-progress would cause the tab to be closed and become unusable if the user decides not to proceed with the browser shutdown. This is because we check for in-progress downloads when the browser is closed, and the tab is closed before that, leaving the tab-strip in a bad state.
    This CL ensures we also bring-up the confirmation dialog when the last tab is
    closed.
    
    BUG=10680
    TEST=Start downloading a big file. While the file is downloading, close all
    tabs. When closing the last tab, the in-progress download dialog should be
    shown. Select the 'Wait for downloads', the download tab should be shown and
    the previous tab should still be displayed and functional.
    Close all tabs again, this time select the 'Close and cancel downloads'
    option, the browser should be closed.
    
    
    Review URL: http://codereview.chromium.org/99195
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14948 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tabs/tab_strip_model_unittest.cc
+++ chrome/browser/tabs/tab_strip_model_unittest.cc
@@ -23,8 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class TabStripDummyDelegate : public TabStripModelDelegate {
  public:
   explicit TabStripDummyDelegate(TabContents* dummy)
-      : dummy_contents_(dummy) {}
+      : dummy_contents_(dummy), can_close_(true) {}
   virtual ~TabStripDummyDelegate() {}
+
+  void set_can_close(bool value) { can_close_ = value; }
 
   // Overridden from TabStripModelDelegate:
   virtual TabContents* AddBlankTab(bool foreground) { return NULL; }
@@ -57,11 +59,15 @@ class TabStripDummyDelegate : public TabStripModelDelegate {
   }
   virtual bool CanRestoreTab() { return false; }
   virtual void RestoreTab() {}
+  virtual bool CanCloseContentsAt(int index) { return can_close_ ; }
 
  private:
   // A dummy TabContents we give to callers that expect us to actually build a
   // Destinations tab for them.
   TabContents* dummy_contents_;
+
+  // Whether tabs can be closed.
+  bool can_close_;
 
   DISALLOW_EVIL_CONSTRUCTORS(TabStripDummyDelegate);
 };
@@ -312,7 +318,15 @@ TEST_F(TabStripModelTest, TestBasicAPI) {
 
   // Test CloseTabContentsAt
   {
-    tabstrip.CloseTabContentsAt(2);
+    // Let's test nothing happens when the delegate veto the close.
+    delegate.set_can_close(false);
+    EXPECT_FALSE(tabstrip.CloseTabContentsAt(2));
+    EXPECT_EQ(3, tabstrip.count());
+    EXPECT_EQ(0, observer.GetStateCount());
+
+    // Now let's close for real.
+    delegate.set_can_close(true);
+    EXPECT_TRUE(tabstrip.CloseTabContentsAt(2));
     EXPECT_EQ(2, tabstrip.count());
 
     EXPECT_EQ(3, observer.GetStateCount());
