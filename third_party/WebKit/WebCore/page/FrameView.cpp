84891f1f61c667bef5cbd4dbe49a2c8c109601bd
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-04-30 02:02:23 +0000

    2009-04-29  David Hyatt  <hyatt@apple.com>
            Reviewed by Dan Bernstein.
    
            For now just drop the deferred layout on the floor, since it causes an infinite hang in mail.  Even
            trying to schedule a relayout for later leaves you in a state where you hit the needsLayout painting
            assertions.
    
            Basically what Mail is doing is crazy, and we can't support it other than to just drop the last layout
            and not do it (which is basically what was happening before).
    
            * page/FrameView.cpp:
            (WebCore::FrameView::setContentsSize):
            (WebCore::FrameView::needsLayout):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43040 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -353,8 +353,8 @@ void FrameView::setContentsSize(const IntSize& size)
     
     m_deferSetNeedsLayouts--;
     
-    if (!m_deferSetNeedsLayouts && m_setNeedsLayoutWasDeferred)
-        layout();
+    if (!m_deferSetNeedsLayouts)
+        m_setNeedsLayoutWasDeferred = false; // FIXME: Find a way to make the deferred layout actually happen.
 }
 
 void FrameView::adjustViewSize()
@@ -988,7 +988,7 @@ bool FrameView::needsLayout() const
         || m_layoutRoot
         || (document && document->childNeedsStyleRecalc()) // can occur when using WebKit ObjC interface
         || m_frame->needsReapplyStyles()
-        || m_setNeedsLayoutWasDeferred;
+        || (m_deferSetNeedsLayouts && m_setNeedsLayoutWasDeferred);
 }
 
 void FrameView::setNeedsLayout()
