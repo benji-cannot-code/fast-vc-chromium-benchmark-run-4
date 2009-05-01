f0d8192ac5ab76c8cacec69b145ce7791bba9478
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-01 23:13:32 +0000

    If Chrome is not the default browser, tell the user, unless: - it is the first run - the user already said not to warn him/her about it - an info-bar is already showing.
    BUG=9049
    TEST=Run a new install of chrome, proceed through the first run flow, don't
         make Chrome your default browser.  No info-bar warning about Chrome not
         being the default browser should be shown.
         Restart Chrome, such an info-bar should be shown. Click the x on the
         info-bar to close it. Restart Chrome. The info-bar should be shown.
         Select "Set as default". Restart Chrome, the info-bar should not be shown.
         Start IE, make it your default browser (Tools menu, 'Internet option',
         Programs tab). Restart Chrome, it should show the default browser info-bar.
         Select "Don't ask me again". Restart Chrome, the info-bar should not be
         shown.
    
    Review URL: http://codereview.chromium.org/99301
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15115 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -1175,6 +1175,7 @@ void Browser::RegisterUserPrefs(PrefService* prefs) {
   prefs->RegisterBooleanPref(prefs::kDeletePasswords, false);
   prefs->RegisterBooleanPref(prefs::kDeleteFormData, true);
   prefs->RegisterIntegerPref(prefs::kDeleteTimePeriod, 0);
+  prefs->RegisterBooleanPref(prefs::kCheckDefaultBrowser, true);
 }
 
 // static
