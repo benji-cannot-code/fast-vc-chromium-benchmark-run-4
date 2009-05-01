b399e82ae2337582bea48c6e46695141bfa65011
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-01 04:15:42 +0000

    2009-04-30  Beth Dakin  <bdakin@apple.com>
            Reviewed by Oliver Hunt.
    
            Fix for <rdar://problem/6841106> REGRESSION: Crash drag selecting 
            To Do's in a Mail note (excessive recursion in mouse drag tracking)
    
            Oliver's recent change to dragging seems to have revealed a long-
            time bug in passSubframeEventToSubframe where we did not set 
            m_sendingEventToSubview to true before calling 
            handleMouseMoveEvent() in the NSMouseMoved case. This patch fixes 
            that and adds ASSERTions around all of the places where 
            m_sendingEventToSubview is set to true and then false to make sure 
            we are not trampling its state.
    
            * page/mac/EventHandlerMac.mm:
            (WebCore::EventHandler::eventLoopHandleMouseDragged):
            (WebCore::EventHandler::eventLoopHandleMouseUp):
            (WebCore::EventHandler::passSubframeEventToSubframe):
            (WebCore::EventHandler::passWheelEventToWidget):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43111 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/mac/EventHandlerMac.mm
+++ third_party/WebKit/WebCore/page/mac/EventHandlerMac.mm
@@ -353,6 +353,7 @@ bool EventHandler::eventLoopHandleMouseDragged(const MouseEventWithHitTestResult
         return false;
     
     if (!m_mouseDownWasInSubframe) {
+        ASSERT(!m_sendingEventToSubview);
         m_sendingEventToSubview = true;
         BEGIN_BLOCK_OBJC_EXCEPTIONS;
         [view mouseDragged:currentEvent().get()];
@@ -379,6 +380,7 @@ bool EventHandler::eventLoopHandleMouseUp(const MouseEventWithHitTestResults&)
         return false;
     
     if (!m_mouseDownWasInSubframe) {
+        ASSERT(!m_sendingEventToSubview);
         m_sendingEventToSubview = true;
         BEGIN_BLOCK_OBJC_EXCEPTIONS;
         [view mouseUp:currentEvent().get()];
@@ -407,7 +409,10 @@ bool EventHandler::passSubframeEventToSubframe(MouseEventWithHitTestResults& eve
             // Since we're passing in currentEvent() here, we can call
             // handleMouseMoveEvent() directly, since the save/restore of
             // currentEvent() that mouseMoved() does would have no effect.
+            ASSERT(!m_sendingEventToSubview);
+            m_sendingEventToSubview = true;
             subframe->eventHandler()->handleMouseMoveEvent(currentEvent().get(), hoveredNode);
+            m_sendingEventToSubview = false;
             return true;
         
         case NSLeftMouseDown: {
@@ -491,6 +496,7 @@ bool EventHandler::passWheelEventToWidget(PlatformWheelEvent&, Widget* widget)
         // We probably hit the border of a RenderWidget
         return false;
 
+    ASSERT(!m_sendingEventToSubview);
     m_sendingEventToSubview = true;
     // Work around <rdar://problem/6806810> which can cause -[NSScrollView scrollWheel:] to
     // crash if the NSScrollView is released during timer or network callback dispatch
