c1c89cb5c5028e10292336c3f5daf1c61fbd1c19
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-01 00:24:50 +0000

    Disabling the EmbeddedMP3 plugin test as it needs a functional sound device on the machine to succeed. Disabling the MediaPlayerNew test as an ASSERT in Webkit is firing causing the renderer to crash. Will debug this more and log a bug.
    TBR=nsylvain
    
    Review URL: http://codereview.chromium.org/100236
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15024 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/plugin/plugin_test.cpp
+++ chrome/test/plugin/plugin_test.cpp
@@ -139,7 +139,7 @@ TEST_F(PluginTest, Quicktime) {
   TestPlugin(L"quicktime.html", kShortWaitTimeout);
 }
 
-TEST_F(PluginTest, MediaPlayerNew) {
+TEST_F(PluginTest, DISABLED_MediaPlayerNew) {
   TestPlugin(L"wmp_new.html", kShortWaitTimeout);
 }
 
@@ -226,7 +226,7 @@ TEST_F(ActiveXTest, DISABLED_CustomScripting) {
   TestActiveX(L"activex_custom_scripting.html", kShortWaitTimeout, true);
 }
 
-TEST_F(ActiveXTest, EmbeddedMP3) {
+TEST_F(ActiveXTest, DISABLED_EmbeddedMP3) {
   TestActiveX(L"mp3_test.html", kLongWaitTimeout, false);
 }
 
