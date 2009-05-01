f7b8aa71b660c4c29afc6cc84efd9b9a5a1629f4
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-01 17:24:49 +0000

    Release the autocomplete popup panel when not visible.
    Review URL: http://codereview.chromium.org/100260
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15061 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_popup_view_mac.mm
+++ chrome/browser/autocomplete/autocomplete_popup_view_mac.mm
@@ -112,6 +112,14 @@ void AutocompletePopupViewMac::UpdatePopupAppearance() {
   if (result.empty()) {
     [[popup_ parentWindow] removeChildWindow:popup_];
     [popup_ orderOut:nil];
+
+    // Break references to table_target_ releasing popup_.
+    NSTableView* table = [popup_ contentView];
+    [table setTarget:nil];
+    [table setDataSource:nil];
+
+    popup_.reset(nil);
+
     return;
   }
 
