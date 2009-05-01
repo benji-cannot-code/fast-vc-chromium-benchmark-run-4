ae751ff853a76978690204cb7bc9797ecefbd18e
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-01 22:37:12 +0000

    Fix a bug in session restore on linux/mac.  We were pickling the page title as a wstring, but reading it back as a string16.  This allows the SessionRestoreUITests to pass.
    Also enable the inspector ui test which is currently disabled, but the code compiles fine on linux/mac.
    
    BUG=10516
    
    Review URL: http://codereview.chromium.org/99297
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15109 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_service.cc
+++ chrome/browser/sessions/session_service.cc
@@ -29,12 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_service.h"
 #include "chrome/common/scoped_vector.h"
 
-// TODO(port): Get rid of this section and finish porting.
-#if defined(OS_WIN)
-#include "chrome/browser/tab_contents/tab_contents.h"
-#include "chrome/common/win_util.h"
-#endif
-
 using base::Time;
 
 // Identifier for commands written to file.
