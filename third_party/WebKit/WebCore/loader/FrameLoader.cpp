d2ebff967e11852b00dc654399070a366d7ae249
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-02 19:58:43 +0000

    WebCore:
    2009-05-02  Darin Adler  <darin@apple.com>
    
            Reviewed by Brady Eidson.
    
            Bug 25491: WebFrame leak when a subframe removes itself
            https://bugs.webkit.org/show_bug.cgi?id=25491
            rdar://problem/6833859
    
            Test: fast/loading/subframe-removes-itself.html
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::checkLoadCompleteForThisFrame): When isStopping is true,
            treat load as complete, even if isLoadingInAPISense is still returning true.
    
    LayoutTests:
    
    2009-05-02  Darin Adler  <darin@apple.com>
    
            Reviewed by Brady Eidson (except for the non-fast/loading test fixes).
    
            Bug 25491: WebFrame leak when a subframe removes itself
            https://bugs.webkit.org/show_bug.cgi?id=25491
            rdar://problem/6833859
    
            * fast/loading: Added.
            * fast/loading/resources: Added.
            * fast/loading/resources/subframe-that-removes-itself.html: Added.
            * fast/loading/subframe-removes-itself-expected.txt: Added.
            * fast/loading/subframe-removes-itself.html: Added.
    
            * fast/frames/location-put-after-removal-expected.txt: Updated.
            For some reason, fixing the loader sequence results in output that has one more blank line.
    
            * http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-in-body.html:
            * http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-parent-same-origin-deny.html:
            * http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag.html:
            Updated tests so they don't terminate when the partial load of the subframe completes, but
            rather wait until the subsequent load of about:blank occurs.
    
            * platform/gtk/Skipped: Skip loading tests.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43148 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -3293,7 +3293,7 @@ void FrameLoader::checkLoadCompleteForThisFrame()
                     item = m_currentHistoryItem;
                 
             bool shouldReset = true;
-            if (!pdl->isLoadingInAPISense()) {
+            if (!(pdl->isLoadingInAPISense() && !pdl->isStopping())) {
                 m_delegateIsHandlingProvisionalLoadError = true;
                 m_client->dispatchDidFailProvisionalLoad(error);
                 m_delegateIsHandlingProvisionalLoadError = false;
@@ -3325,7 +3325,7 @@ void FrameLoader::checkLoadCompleteForThisFrame()
         
         case FrameStateCommittedPage: {
             DocumentLoader* dl = m_documentLoader.get();            
-            if (!dl || dl->isLoadingInAPISense())
+            if (!dl || (dl->isLoadingInAPISense() && !dl->isStopping()))
                 return;
 
             markLoadComplete();
