abf20a316c09954f3704950210daa446e9c4c3ec
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-02 04:52:36 +0000

    JavaScriptCore:
    2009-05-01  Sam Weinig  <sam@webkit.org>
    
            Roll out JavaScriptCore API number marshaling.
    
            * API/APICast.h:
            (toJS):
            (toRef):
            * API/JSBase.cpp:
            (JSEvaluateScript):
            (JSCheckScriptSyntax):
            * API/JSCallbackConstructor.cpp:
            (JSC::constructJSCallback):
            * API/JSCallbackFunction.cpp:
            (JSC::JSCallbackFunction::call):
            * API/JSCallbackObjectFunctions.h:
            (JSC::::getOwnPropertySlot):
            (JSC::::put):
            (JSC::::deleteProperty):
            (JSC::::construct):
            (JSC::::hasInstance):
            (JSC::::call):
            (JSC::::toNumber):
            (JSC::::toString):
            (JSC::::staticValueGetter):
            (JSC::::callbackGetter):
            * API/JSObjectRef.cpp:
            (JSObjectMakeFunction):
            (JSObjectMakeArray):
            (JSObjectMakeDate):
            (JSObjectMakeError):
            (JSObjectMakeRegExp):
            (JSObjectGetPrototype):
            (JSObjectSetPrototype):
            (JSObjectGetProperty):
            (JSObjectSetProperty):
            (JSObjectGetPropertyAtIndex):
            (JSObjectSetPropertyAtIndex):
            (JSObjectDeleteProperty):
            (JSObjectCallAsFunction):
            (JSObjectCallAsConstructor):
            * API/JSValueRef.cpp:
            (JSValueGetType):
            (JSValueIsUndefined):
            (JSValueIsNull):
            (JSValueIsBoolean):
            (JSValueIsNumber):
            (JSValueIsString):
            (JSValueIsObject):
            (JSValueIsObjectOfClass):
            (JSValueIsEqual):
            (JSValueIsStrictEqual):
            (JSValueIsInstanceOfConstructor):
            (JSValueMakeUndefined):
            (JSValueMakeNull):
            (JSValueMakeBoolean):
            (JSValueMakeNumber):
            (JSValueMakeString):
            (JSValueToBoolean):
            (JSValueToNumber):
            (JSValueToStringCopy):
            (JSValueToObject):
            (JSValueProtect):
            (JSValueUnprotect):
            * JavaScriptCore.exp:
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def:
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore_debug.def:
            * runtime/JSNumberCell.cpp:
            * runtime/JSNumberCell.h:
            * runtime/JSValue.h:
    
    WebCore:
    
    2009-05-01  Sam Weinig  <sam@webkit.org>
    
            Roll out JavaScriptCore API number marshaling.
    
            * bindings/js/ScriptValue.cpp:
            (WebCore::ScriptValue::isEqual):
            * inspector/JavaScriptProfile.cpp:
            (WebCore::getHeadCallback):
            * inspector/JavaScriptProfileNode.cpp:
            (WebCore::getChildren):
            (WebCore::getParent):
            (WebCore::getHead):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43138 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSCallbackObjectFunctions.h
+++ third_party/WebKit/JavaScriptCore/API/JSCallbackObjectFunctions.h
@@ -129,9 +129,9 @@ bool JSCallbackObject<Base>::getOwnPropertySlot(ExecState* exec, const Identifie
             JSLock::DropAllLocks dropAllLocks(exec);
             JSValueRef exception = 0;
             JSValueRef value = getProperty(ctx, thisRef, propertyNameRef.get(), &exception);
-            exec->setException(toJS(exec, exception));
+            exec->setException(toJS(exception));
             if (value) {
-                slot.setValue(toJS(exec, value));
+                slot.setValue(toJS(value));
                 return true;
             }
             if (exception) {
@@ -170,7 +170,7 @@ void JSCallbackObject<Base>::put(ExecState* exec, const Identifier& propertyName
     JSContextRef ctx = toRef(exec);
     JSObjectRef thisRef = toRef(this);
     RefPtr<OpaqueJSString> propertyNameRef;
-    JSValueRef valueRef = toRef(exec, value);
+    JSValueRef valueRef = toRef(value);
     
     for (JSClassRef jsClass = classRef(); jsClass; jsClass = jsClass->parentClass) {
         if (JSObjectSetPropertyCallback setProperty = jsClass->setProperty) {
@@ -179,7 +179,7 @@ void JSCallbackObject<Base>::put(ExecState* exec, const Identifier& propertyName
             JSLock::DropAllLocks dropAllLocks(exec);
             JSValueRef exception = 0;
             bool result = setProperty(ctx, thisRef, propertyNameRef.get(), valueRef, &exception);
-            exec->setException(toJS(exec, exception));
+            exec->setException(toJS(exception));
             if (result || exception)
                 return;
         }
@@ -194,7 +194,7 @@ void JSCallbackObject<Base>::put(ExecState* exec, const Identifier& propertyName
                     JSLock::DropAllLocks dropAllLocks(exec);
                     JSValueRef exception = 0;
                     bool result = setProperty(ctx, thisRef, propertyNameRef.get(), valueRef, &exception);
-                    exec->setException(toJS(exec, exception));
+                    exec->setException(toJS(exception));
                     if (result || exception)
                         return;
                 } else
@@ -229,7 +229,7 @@ bool JSCallbackObject<Base>::deleteProperty(ExecState* exec, const Identifier& p
             JSLock::DropAllLocks dropAllLocks(exec);
             JSValueRef exception = 0;
             bool result = deleteProperty(ctx, thisRef, propertyNameRef.get(), &exception);
-            exec->setException(toJS(exec, exception));
+            exec->setException(toJS(exception));
             if (result || exception)
                 return true;
         }
@@ -283,11 +283,11 @@ JSObject* JSCallbackObject<Base>::construct(ExecState* exec, JSObject* construct
             int argumentCount = static_cast<int>(args.size());
             Vector<JSValueRef, 16> arguments(argumentCount);
             for (int i = 0; i < argumentCount; i++)
-                arguments[i] = toRef(exec, args.at(i));
+                arguments[i] = toRef(args.at(i));
             JSLock::DropAllLocks dropAllLocks(exec);
             JSValueRef exception = 0;
             JSObject* result = toJS(callAsConstructor(execRef, constructorRef, argumentCount, arguments.data(), &exception));
-            exec->setException(toJS(exec, exception));
+            exec->setException(toJS(exception));
             return result;
         }
     }
@@ -306,8 +306,8 @@ bool JSCallbackObject<Base>::hasInstance(ExecState* exec, JSValue value, JSValue
         if (JSObjectHasInstanceCallback hasInstance = jsClass->hasInstance) {
             JSLock::DropAllLocks dropAllLocks(exec);
             JSValueRef exception = 0;
-            bool result = hasInstance(execRef, thisRef, toRef(exec, value), &exception);
-            exec->setException(toJS(exec, exception));
+            bool result = hasInstance(execRef, thisRef, toRef(value), &exception);
+            exec->setException(toJS(exception));
             return result;
         }
     }
@@ -338,11 +338,11 @@ JSValue JSCallbackObject<Base>::call(ExecState* exec, JSObject* functionObject, 
             int argumentCount = static_cast<int>(args.size());
             Vector<JSValueRef, 16> arguments(argumentCount);
             for (int i = 0; i < argumentCount; i++)
-                arguments[i] = toRef(exec, args.at(i));
+                arguments[i] = toRef(args.at(i));
             JSLock::DropAllLocks dropAllLocks(exec);
             JSValueRef exception = 0;
-            JSValue result = toJS(exec, callAsFunction(execRef, functionRef, thisObjRef, argumentCount, arguments.data(), &exception));
-            exec->setException(toJS(exec, exception));
+            JSValue result = toJS(callAsFunction(execRef, functionRef, thisObjRef, argumentCount, arguments.data(), &exception));
+            exec->setException(toJS(exception));
             return result;
         }
     }
@@ -406,10 +406,10 @@ double JSCallbackObject<Base>::toNumber(ExecState* exec) const
             
             JSValueRef exception = 0;
             JSValueRef value = convertToType(ctx, thisRef, kJSTypeNumber, &exception);
-            exec->setException(toJS(exec, exception));
+            exec->setException(toJS(exception));
             if (value) {
                 double dValue;
-                return toJS(exec, value).getNumber(dValue) ? dValue : NaN;
+                return toJS(value).getNumber(dValue) ? dValue : NaN;
             }
         }
             
@@ -429,10 +429,10 @@ UString JSCallbackObject<Base>::toString(ExecState* exec) const
             {
                 JSLock::DropAllLocks dropAllLocks(exec);
                 value = convertToType(ctx, thisRef, kJSTypeString, &exception);
-                exec->setException(toJS(exec, exception));
+                exec->setException(toJS(exception));
             }
             if (value)
-                return toJS(exec, value).getString();
+                return toJS(value).getString();
             if (exception)
                 return "";
         }
@@ -476,15 +476,12 @@ JSValue JSCallbackObject<Base>::staticValueGetter(ExecState* exec, const Identif
                 if (JSObjectGetPropertyCallback getProperty = entry->getProperty) {
                     if (!propertyNameRef)
                         propertyNameRef = OpaqueJSString::create(propertyName.ustring());
+                    JSLock::DropAllLocks dropAllLocks(exec);
                     JSValueRef exception = 0;
-                    JSValueRef value;
-                    {
-                        JSLock::DropAllLocks dropAllLocks(exec);
-                        value = getProperty(toRef(exec), thisRef, propertyNameRef.get(), &exception);
-                    }
-                    exec->setException(toJS(exec, exception));
+                    JSValueRef value = getProperty(toRef(exec), thisRef, propertyNameRef.get(), &exception);
+                    exec->setException(toJS(exception));
                     if (value)
-                        return toJS(exec, value);
+                        return toJS(value);
                     if (exception)
                         return jsUndefined();
                 }
@@ -533,9 +530,9 @@ JSValue JSCallbackObject<Base>::callbackGetter(ExecState* exec, const Identifier
 
             JSValueRef exception = 0;
             JSValueRef value = getProperty(toRef(exec), thisRef, propertyNameRef.get(), &exception);
-            exec->setException(toJS(exec, exception));
+            exec->setException(toJS(exception));
             if (value)
-                return toJS(exec, value);
+                return toJS(value);
             if (exception)
                 return jsUndefined();
         }
