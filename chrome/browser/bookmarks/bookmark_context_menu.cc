fea8ad202fdfeb94603d55c07790791d1bcf4d35
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-04 02:08:27 +0000

    Make the header include of views_strings.h here windows-only, temporarily (we duplicate the strings in generated_resources.h)
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15181 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_context_menu.cc
+++ chrome/browser/bookmarks/bookmark_context_menu.cc
@@ -19,13 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_service.h"
 #include "grit/generated_resources.h"
-#include "grit/views_strings.h"
 
 // TODO(port): Port these files.
 #if defined(OS_WIN)
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/views/bookmark_manager_view.h"
 #include "chrome/views/window/window.h"
+// TODO(beng): move this stuff to a common location and move it out of this block
+#include "grit/views_strings.h"
 #endif
 
 namespace {
