7987686d6d5142331474d80986fcd6e274161375
sidchat@google.com <sidchat@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-04 17:44:25 +0000

    Change in enable-spellcheck checkbox in Languages Options Tab should not be considered unless OK is clicked.
    Issue=11013
    Review URL: http://codereview.chromium.org/99095
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15219 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/options/languages_page_view.cc
+++ chrome/browser/views/options/languages_page_view.cc
@@ -481,6 +481,7 @@ LanguagesPageView::LanguagesPageView(Profile* profile)
       OptionsPageView(profile),
       language_table_edited_(false),
       language_warning_shown_(false),
+      enable_spellcheck_checkbox_clicked_(false),
       spellcheck_language_index_selected_(-1),
       ui_language_index_selected_(-1),
       starting_ui_language_index_(-1) {
@@ -512,10 +513,7 @@ void LanguagesPageView::ButtonPressed(views::Button* sender) {
         new AddLanguageWindowView(this, profile()))->Show();
     language_table_edited_ = true;
   } else if (sender == enable_spellchecking_checkbox_) {
-    if (enable_spellchecking_checkbox_->checked())
-      enable_spellcheck_.SetValue(true);
-    else
-      enable_spellcheck_.SetValue(false);
+      enable_spellcheck_checkbox_clicked_ = true;
   }
 }
 
@@ -845,4 +843,7 @@ void LanguagesPageView::SaveChanges() {
     dictionary_language_.SetValue(ASCIIToWide(dictionary_language_model_->
         GetLocaleFromIndex(spellcheck_language_index_selected_)));
   }
+
+  if (enable_spellcheck_checkbox_clicked_)
+    enable_spellcheck_.SetValue(enable_spellchecking_checkbox_->checked());
 }
