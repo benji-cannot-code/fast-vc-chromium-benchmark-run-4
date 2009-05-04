a312fab46fc79ada557a5d152aa651788cce7303
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-04 09:22:32 +0000

    DevTools: Support XmlHTTPRequests sniffing.
    Review URL: http://codereview.chromium.org/100262
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15198 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/net_agent_impl.cc
+++ webkit/glue/devtools/net_agent_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceError.h"
 #include "ResourceRequest.h"
 #include "ResourceResponse.h"
+#include "ScriptString.h"
 #include "TextEncoding.h"
 #include <wtf/CurrentTime.h>
 #undef LOG
@@ -156,6 +157,7 @@ void NetAgentImpl::DidFinishLoading(
   // Start removing resources from the cache once there are too many of them.
   if (finished_resources_.size() > 200) {
     for (int i = 0; i < 50; ++i) {
+      xml_http_sources_.remove(finished_resources_[i].first);
       delete finished_resources_[i].second;
     }
     finished_resources_.remove(0, 50);
@@ -188,6 +190,12 @@ void NetAgentImpl::DidLoadResourceFromMemoryCache(
   int identifier = last_cached_identifier_--;
 }
 
+void NetAgentImpl::DidLoadResourceByXMLHttpRequest(
+    int identifier,
+    const WebCore::ScriptString& source) {
+  xml_http_sources_.set(identifier, source);
+}
+
 void NetAgentImpl::GetResourceContent(
     int call_id,
     int identifier,
@@ -198,7 +206,10 @@ void NetAgentImpl::GetResourceContent(
 
   String source;
 
-  if (main_loader_.get() && main_loader_->requestURL() == url) {
+  WebCore::ScriptString script = xml_http_sources_.get(identifier);
+  if (!script.isNull()) {
+    source = String(script);
+  } else if (main_loader_.get() && main_loader_->requestURL() == url) {
     RefPtr<SharedBuffer> buffer = main_loader_->mainResourceData();
     String text_encoding_name = document_->inputEncoding();
     if (buffer) {
