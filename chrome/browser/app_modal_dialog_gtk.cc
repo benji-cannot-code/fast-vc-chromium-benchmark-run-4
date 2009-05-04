7889e3ed02ef35fe399579e1d35fde52f258968f
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-04 08:15:04 +0000

    Fix Linux build. Review URL: http://codereview.chromium.org/99339
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15196 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/app_modal_dialog_gtk.cc
+++ chrome/browser/app_modal_dialog_gtk.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/string_util.h"
-#include "chrome/browser/tab_contents/web_contents.h"
+#include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/tab_contents/tab_contents_view.h"
 #include "chrome/common/l10n_util.h"
 #include "chrome/common/message_box_flags.h"
