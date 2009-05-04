5dcf1d955666fd471aecb72a309ab3133fc5d505
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-04 16:44:29 +0000

    Get rid of the static next_id_ counter, to make construction of ProxyConfig on different threads safe.
    BUG=11323
    Review URL: http://codereview.chromium.org/102023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15209 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_config.cc
+++ net/proxy/proxy_config.cc
@@ -10,13 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-// static
-ProxyConfig::ID ProxyConfig::last_id_ = ProxyConfig::INVALID_ID;
-
 ProxyConfig::ProxyConfig()
     : auto_detect(false),
       proxy_bypass_local_names(false),
-      id_(++last_id_) {
+      id_(INVALID_ID) {
 }
 
 bool ProxyConfig::Equals(const ProxyConfig& other) const {
@@ -141,6 +138,7 @@ std::ostream& operator<<(std::ostream& out, const net::ProxyConfig& config) {
     out << "  }\n";
   }
 
-  out << "}";
+  out << "  id: " << config.id() << "\n"
+      << "}";
   return out;
 }
