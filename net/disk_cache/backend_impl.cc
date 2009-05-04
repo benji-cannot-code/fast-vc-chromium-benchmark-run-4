1548cc72125f083519ed700cf5f3a70ba3609211
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-04 20:23:50 +0000

    Fix a browser crash when reading the disk cache experiment data after failing to map the index file.
    BUG=11414
    TEST=none
    
    Review URL: http://codereview.chromium.org/100349
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15235 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_impl.cc
+++ net/disk_cache/backend_impl.cc
@@ -261,7 +261,7 @@ bool BackendImpl::Init() {
   }
 
   init_ = true;
-  if (data_ && !InitExperiment(&data_->header.experiment))
+  if (!InitExperiment(&data_->header.experiment))
     return false;
 
   if (data_->header.experiment > 6)
@@ -938,6 +938,10 @@ bool BackendImpl::InitBackingStore(bool* file_created) {
 
   index_ = new MappedFile();
   data_ = reinterpret_cast<Index*>(index_->Init(index_name, 0));
+  if (!data_) {
+    LOG(ERROR) << "Unable to map Index file";
+    return false;
+  }
   return true;
 }
 
@@ -1403,10 +1407,7 @@ void BackendImpl::UpgradeTo2_1() {
 }
 
 bool BackendImpl::CheckIndex() {
-  if (!data_) {
-    LOG(ERROR) << "Unable to map Index file";
-    return false;
-  }
+  DCHECK(data_);
 
   size_t current_size = index_->GetLength();
   if (current_size < sizeof(Index)) {
