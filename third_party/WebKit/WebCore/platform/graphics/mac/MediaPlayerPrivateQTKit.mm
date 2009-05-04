e1e987e0abb7491608a3127b3f84ad12879017b2
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-04 15:56:17 +0000

    2009-05-04  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Adam Roben.
            
            https://bugs.webkit.org/show_bug.cgi?id=25545
            Bug 25545: HTMLMediaElement: display anamorphic video correctly
            
            Tell QuickTime to use clean aperture mode so movies with non-square pixels are sized
            and displayed correctly.
    
            * platform/graphics/mac/MediaPlayerPrivateQTKit.mm:
            (WebCore::MediaPlayerPrivate::createQTMovie): Ask QuickTime to use clean aperture mode.
            * platform/graphics/win/QTMovieWin.cpp:
            (QTMovieWin::load): Ditto. Add ASSERT to ensure that static movie property array isn't 
            filled beyond capacity.
    
    2009-05-04  Eric Carlson  <eric.carlson@apple.com>
    
            Reviewed by Adam Roben.
    
            https://bugs.webkit.org/show_bug.cgi?id=25545
            Bug 25545: HTMLMediaElement: display anamorphic video correctly
            
            Added 720x576i PAL anamorphic movie, extended video-size-intrinsic-scale
            test to check that it is sized correctly.
    
            * media/content/720x576i-anamorphic.mov: Added.
            * media/video-size-intrinsic-scale-expected.txt:
            * media/video-size-intrinsic-scale.html:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43182 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/MediaPlayerPrivateQTKit.mm
+++ third_party/WebKit/WebCore/platform/graphics/mac/MediaPlayerPrivateQTKit.mm
@@ -93,6 +93,10 @@ SOFT_LINK_POINTER(QTKit, QTMovieURLAttribute, NSString *)
 SOFT_LINK_POINTER(QTKit, QTMovieVolumeDidChangeNotification, NSString *)
 SOFT_LINK_POINTER(QTKit, QTSecurityPolicyNoCrossSiteAttribute, NSString *)
 SOFT_LINK_POINTER(QTKit, QTVideoRendererWebKitOnlyNewImageAvailableNotification, NSString *)
+#ifndef BUILDING_ON_TIGER
+SOFT_LINK_POINTER(QTKit, QTMovieApertureModeClean, NSString *)
+SOFT_LINK_POINTER(QTKit, QTMovieApertureModeAttribute, NSString *)
+#endif
 
 #define QTMovie getQTMovieClass()
 #define QTMovieView getQTMovieViewClass()
@@ -122,6 +126,10 @@ SOFT_LINK_POINTER(QTKit, QTVideoRendererWebKitOnlyNewImageAvailableNotification,
 #define QTMovieVolumeDidChangeNotification getQTMovieVolumeDidChangeNotification()
 #define QTSecurityPolicyNoCrossSiteAttribute getQTSecurityPolicyNoCrossSiteAttribute()
 #define QTVideoRendererWebKitOnlyNewImageAvailableNotification getQTVideoRendererWebKitOnlyNewImageAvailableNotification()
+#ifndef BUILDING_ON_TIGER
+#define QTMovieApertureModeClean getQTMovieApertureModeClean()
+#define QTMovieApertureModeAttribute getQTMovieApertureModeAttribute()
+#endif
 
 // Older versions of the QTKit header don't have these constants.
 #if !defined QTKIT_VERSION_MAX_ALLOWED || QTKIT_VERSION_MAX_ALLOWED <= QTKIT_VERSION_7_0
@@ -230,6 +238,9 @@ void MediaPlayerPrivate::createQTMovie(const String& url)
                                      [NSNumber numberWithBool:YES], QTSecurityPolicyNoCrossSiteAttribute,
                                      [NSNumber numberWithBool:NO], QTMovieAskUnresolvedDataRefsAttribute,
                                      [NSNumber numberWithBool:YES], @"QTMovieOpenForPlaybackAttribute",     // FIXME: Use defined attribute when required version of QT supports this attribute
+#ifndef BUILDING_ON_TIGER
+                                     QTMovieApertureModeClean, QTMovieApertureModeAttribute,
+#endif
                                      nil];
     
     NSError *error = nil;
