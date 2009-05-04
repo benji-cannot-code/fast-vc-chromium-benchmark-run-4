02b0824d882b3faf863c97c8ff9038304483cab4
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-04 08:15:01 +0000

    Fix the mac build from my WebContents rename. Review URL: http://codereview.chromium.org/100329
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15195 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.cc
+++ chrome/browser/automation/automation_provider.cc
@@ -2619,7 +2619,6 @@ TabContents* AutomationProvider::GetTabContentsForHandle(
     int handle, NavigationController** tab) {
   if (tab_tracker_->ContainsHandle(handle)) {
     NavigationController* nav_controller = tab_tracker_->GetResource(handle);
-    TabContents* tab_contents = nav_controller->tab_contents();
     if (tab)
       *tab = nav_controller;
     return nav_controller->tab_contents();
