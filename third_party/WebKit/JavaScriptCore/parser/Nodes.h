705a61bf0a9511de7b016823b46559c0c61f5e73
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-05 12:16:41 +0000

    More build fixes
    git-svn-id: svn://svn.chromium.org/blink/trunk@43227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/parser/Nodes.h
+++ third_party/WebKit/JavaScriptCore/parser/Nodes.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NODES_H_
 
 #include "Error.h"
+#include "JIT.h"
 #include "JITCode.h"
 #include "Opcode.h"
 #include "ResultType.h"
