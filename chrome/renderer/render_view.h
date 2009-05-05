0a64b4b6922b0cbaac9ef72f98c2e34f091d0128
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-05 00:10:06 +0000

    Various trivial clean-up bits: * Fix an 80-column issue * Remove an unused param * Remove an unnecessary setter * Fix a variable capitalization style violation Review URL: http://codereview.chromium.org/100361
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15254 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.h
+++ chrome/renderer/render_view.h
@@ -301,7 +301,7 @@ class RenderView : public RenderWidget,
   virtual void ReportFindInPageSelection(int request_id,
                                          int active_match_ordinal,
                                          const WebKit::WebRect& selection);
-  virtual bool WasOpenedByUserGesture(WebView* webview) const;
+  virtual bool WasOpenedByUserGesture() const;
   virtual void SpellCheck(const std::wstring& word, int& misspell_location,
                           int& misspell_length);
   virtual void SetInputMethodState(bool enabled);
@@ -597,10 +597,6 @@ class RenderView : public RenderWidget,
   // Creates DevToolsClient and sets up JavaScript bindings for developer tools
   // UI that is going to be hosted by this RenderView.
   void CreateDevToolsClient();
-
-  void set_opened_by_user_gesture(bool value) {
-    opened_by_user_gesture_ = value;
-  }
 
   // Called by RenderWidget after it paints.
   virtual void DidPaint();
