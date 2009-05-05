46441208fec3a57b725ece28430c2e5e733ce895
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-05 22:00:14 +0000

    2009-05-05  Xan Lopez  <xlopez@igalia.com> and Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
            Reviewed by Oliver Hunt.
    
            Disable the NativeFunctionWrapper for all non-Mac ports for now,
            as it is also crashing on Linux/x86.
    
            * runtime/NativeFunctionWrapper.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43253 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/NativeFunctionWrapper.h
+++ third_party/WebKit/JavaScriptCore/runtime/NativeFunctionWrapper.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NativeFunctionWrapper_h
 
 namespace JSC {
-#if ENABLE(JIT) && !PLATFORM(WIN)
+#if ENABLE(JIT) && PLATFORM(MAC)
     class JSFunction;
     typedef JSFunction NativeFunctionWrapper;
 #else
