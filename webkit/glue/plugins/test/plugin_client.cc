53a419a873a5df27d56746226b6e1f5bd172aa36
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-05 07:49:19 +0000

    Fix Flash window in Analytics being incorrectly visible.
    The bug was that we accidentally marked a plugin widget as visible if it's parent was visible, even if it was explicitly set as hidden.
    
    BUG=8927
    TEST=regression test included, also can verify that analytics doesn't display the gray rectangle per the bug
    Review URL: http://codereview.chromium.org/106008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15285 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/test/plugin_client.cc
+++ webkit/glue/plugins/test/plugin_client.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/plugins/test/plugin_npobject_lifetime_test.h"
 #include "webkit/glue/plugins/test/plugin_npobject_proxy_test.h"
 #include "webkit/glue/plugins/test/plugin_window_size_test.h"
+#include "webkit/glue/plugins/test/plugin_windowed_test.h"
 #include "webkit/glue/plugins/test/plugin_windowless_test.h"
 #include "third_party/npapi/bindings/npapi.h"
 #include "third_party/npapi/bindings/npruntime.h"
@@ -154,6 +155,12 @@ NPError NPP_New(NPMIMEType pluginType, NPP instance, uint16 mode,
   } else if (test_name == "private") {
     new_test = new NPAPIClient::PrivateTest(instance,
       NPAPIClient::PluginClient::HostFunctions());
+#if defined(OS_WIN)
+  // TODO(port): plugin_windowed_test.*.
+  } else if (test_name == "hidden_plugin") {
+    new_test = new NPAPIClient::WindowedPluginTest(instance,
+        NPAPIClient::PluginClient::HostFunctions());
+#endif
   } else {
     // If we don't have a test case for this, create a
     // generic one which basically never fails.
