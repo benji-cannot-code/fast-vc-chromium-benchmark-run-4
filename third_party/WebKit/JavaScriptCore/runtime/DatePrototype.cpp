d07d65a78ba7a63cbdde51b07a1cd915fcc9f4aa
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-05 12:28:09 +0000

    Yet more windows build fixes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@43229 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/DatePrototype.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/DatePrototype.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DatePrototype.h"
 
 #include "DateMath.h"
+#include "Error.h"
 #include "JSString.h"
 #include "ObjectPrototype.h"
 #include "DateInstance.h"
