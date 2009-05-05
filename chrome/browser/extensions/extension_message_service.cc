593839b901f13ebb10a9b3955fe5a486131c9b2d
mpcomplete@google.com <mpcomplete@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-05 00:50:53 +0000

    Add a RENDERER_PROCESS_CLOSED notification that gets sent when we close the RenderProcessHost's process_.  This is similar to RENDER_PROCESS_TERMINATED, but the latter is not always sent since the process could get reused.  It also takes the place of RENDERER_PROCESS_CRASHED, where this one contains a boolean Details indicating whether the process crashed (true) or just exited (false). Review URL: http://codereview.chromium.org/100277
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15261 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_message_service.cc
+++ chrome/browser/extensions/extension_message_service.cc
@@ -82,7 +82,7 @@ void ExtensionMessageService::Init() {
       NotificationType::RENDERER_PROCESS_TERMINATED,
       NotificationService::AllSources());
   NotificationService::current()->AddObserver(this,
-      NotificationType::RENDERER_PROCESS_CRASHED,
+      NotificationType::RENDERER_PROCESS_CLOSED,
       NotificationService::AllSources());
 }
 
@@ -215,7 +215,8 @@ void ExtensionMessageService::Observe(NotificationType type,
   DCHECK_EQ(MessageLoop::current()->type(), MessageLoop::TYPE_UI);
 
   DCHECK(type.value == NotificationType::RENDERER_PROCESS_TERMINATED ||
-         type.value == NotificationType::RENDERER_PROCESS_CRASHED);
+         type.value == NotificationType::RENDERER_PROCESS_CLOSED);
+  
   RenderProcessHost* renderer = Source<RenderProcessHost>(source).ptr();
 
   {
