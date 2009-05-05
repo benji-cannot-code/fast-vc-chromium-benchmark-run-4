873e0d25de4bc94ac4fa787ffac184525c1f30ec
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-05 01:18:52 +0000

    2009-05-04  Brady Eidson  <beidson@apple.com>
            Reviewed by Sam 'The Belly' Weinig
    
            <rdar://problem/6828894> - Facebook photo uploader applet broken on Leopard only
    
            In r41568 we started adding a default codebase attribute to <applet> tags if they didn't specify one
            themselves.  Leopard's Java plug-in mishandles this case and fails to load code for the applet.
    
            The spirit of r41568 can be maintained by removing the "default codebase" piece and only performing
            the canLoad() check if a codebase was actually set on the applet tag.
    
            * html/HTMLAppletElement.cpp:
            (WebCore::HTMLAppletElement::createRenderer): Don't create a default codebase attribute.
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::createJavaAppletWidget): Only perform the canLoad() check if the codebase was set.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43200 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -5110,10 +5110,12 @@ Widget* FrameLoader::createJavaAppletWidget(const IntSize& size, HTMLAppletEleme
         paramValues.append(it->second);
     }
 
-    KURL codeBaseURL = completeURL(codeBaseURLString);
-    if (!canLoad(codeBaseURL, String(), element->document())) {
-        FrameLoader::reportLocalLoadFailed(m_frame, codeBaseURL.string());
-        return 0;
+    if (!codeBaseURLString.isEmpty()) {
+        KURL codeBaseURL = completeURL(codeBaseURLString);
+        if (!canLoad(codeBaseURL, String(), element->document())) {
+            FrameLoader::reportLocalLoadFailed(m_frame, codeBaseURL.string());
+            return 0;
+        }
     }
 
     if (baseURLString.isEmpty())
