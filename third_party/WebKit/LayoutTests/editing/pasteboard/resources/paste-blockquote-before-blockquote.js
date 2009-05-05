1bf6461132bd2756676d943a040f07b81c51b03e
kmccullough@apple.com <kmccullough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-05 22:32:29 +0000

    WebCore:
    2009-05-05  Kevin McCullough  <kmccullough@apple.com>
    
            Reviewed by Dan Bernstein.
    
            <rdar://problem/6552129> Select a quote line and paste elsewhere, you
            get that line and an extra quoted blank line
            <rdar://problem/6609308> Triple-click a quoted header line, copy, paste
            onto an empty line makes an extra quoted line
    
            - When pasting a blockquote with a newline, make sure we put the newline
            outside of the blockquote so that it is not quoted.
    
            * editing/InsertParagraphSeparatorCommand.cpp:
            (WebCore::InsertParagraphSeparatorCommand::doApply):
    
    LayoutTests:
    
    2009-05-05  Kevin McCullough  <kmccullough@apple.com>
    
            Reviewed by Dan Bernstein.
    
            <rdar://problem/6552129> Select a quote line and paste elsewhere, you
            get that line and an extra quoted blank line
            <rdar://problem/6609308> Triple-click a quoted header line, copy, paste
            onto an empty line makes an extra quoted line
    
            - Testing different scenarios where we paste quoted text into a non-quoted area and we
            do not want a trailing newline to be quoted.
    
            * editing/pasteboard/paste-blockquote-before-blockquote-expected.txt: Added.
            * editing/pasteboard/paste-blockquote-before-blockquote.html: Added.
            * editing/pasteboard/paste-double-nested-blockquote-before-blockquote-expected.txt: Added.
            * editing/pasteboard/paste-double-nested-blockquote-before-blockquote.html: Added.
            * editing/pasteboard/resources/paste-blockquote-before-blockquote.js: Added.
            (copyAndPasteNode):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/editing/pasteboard/resources/paste-blockquote-before-blockquote.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+function copyAndPasteNode(nodeName) {
+    var range = document.createRange();
+
+    // Test with a single blockquote
+    var nodeToCopy = document.getElementById(nodeName);
+    range.setStartBefore(nodeToCopy);
+    range.setEndAfter(nodeToCopy);
+
+    var sel = window.getSelection();
+    sel.addRange(range);
+    document.execCommand("Copy");
+
+    var pasteDiv = document.getElementById("pasteDiv");
+    sel.setPosition(pasteDiv, 0);
+    document.execCommand("Paste");
+
+    console.log(pasteDiv.children[1] == undefined
+        ? "FAILED: pasting " + nodeName + " DID keep the newline within the blockquote"
+        : "SUCCESS: pasting " + nodeName + " DID NOT keep the newline within the blockquote.");
+}
+
