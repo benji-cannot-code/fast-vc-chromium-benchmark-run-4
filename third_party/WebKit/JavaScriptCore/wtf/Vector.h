7177395d3b290bf1c52dae7eb8b674d15a2569f9
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-05 05:37:50 +0000

    2009-05-04  Maciej Stachowiak  <mjs@apple.com>
            Rubber stamped by Gavin.
    
            - inline Vector::resize for a ~1.5% speedup on string-tagcloud
    
            * wtf/Vector.h:
            (WTF::Vector::resize): Inline
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43205 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Vector.h
+++ third_party/WebKit/JavaScriptCore/wtf/Vector.h
@@ -693,7 +693,7 @@ namespace WTF {
     }
 
     template<typename T, size_t inlineCapacity>
-    void Vector<T, inlineCapacity>::resize(size_t size)
+    inline void Vector<T, inlineCapacity>::resize(size_t size)
     {
         if (size <= m_size)
             TypeOperations::destruct(begin() + size, end());
