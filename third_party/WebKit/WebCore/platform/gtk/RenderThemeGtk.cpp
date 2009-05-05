57ac0ef810af3152ddc0ea321eea536c214e536a
jmalonzo@webkit.org <jmalonzo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-05 20:16:25 +0000

    2009-05-05  Jan Michael Alonzo  <jmalonzo@webkit.org>
            Reviewed by Xan Lopez.
    
            Call moz_gtk_shutdown on RenderThemeGtk destruction.
    
            * platform/gtk/RenderThemeGtk.cpp:
            (WebCore::RenderThemeGtk::~RenderThemeGtk):
            * platform/gtk/RenderThemeGtk.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43250 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/RenderThemeGtk.cpp
+++ third_party/WebKit/WebCore/platform/gtk/RenderThemeGtk.cpp
@@ -55,6 +55,14 @@ RenderThemeGtk::RenderThemeGtk()
     }
 }
 
+RenderThemeGtk::~RenderThemeGtk()
+{
+    if (mozGtkInitialized) {
+        moz_gtk_shutdown();
+        mozGtkInitialized = false;
+    }
+}
+
 static bool supportsFocus(ControlPart appearance)
 {
     switch (appearance) {
