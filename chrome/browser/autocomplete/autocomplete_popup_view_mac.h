18c5662fabd9ad7a19a6ebb7a4c232f39bc1c31f
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-05 19:33:39 +0000

    Convert autocomplete to use a custom matrix and button cell.
    On the user-visible side this change enables mouse hover (the item
    under the mouse is highlighted), and lays out the text omnibox v2
    style.  Only hover really needed the control change, but I have
    another change queued up to style the popup text.  It uses different
    colors and shades, and really looked horrible with the NSTableView
    highlighting.  [Which is why this part of the CL is coming first.]
    
    TEST=Omnibox popup works and shows highlighting under the mouse in addition to the keyboard-selected item.
    
    Review URL: http://codereview.chromium.org/99310
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15321 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_popup_view_mac.h
+++ chrome/browser/autocomplete/autocomplete_popup_view_mac.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class AutocompletePopupModel;
 class AutocompleteEditModel;
 class AutocompleteEditViewMac;
-@class AutocompleteTableTarget;
+@class AutocompleteMatrixTarget;
 class Profile;
 
 // Implements AutocompletePopupView using a raw NSWindow containing an
@@ -58,7 +58,7 @@ class AutocompletePopupViewMac : public AutocompletePopupView {
   }
   virtual void UpdatePopupAppearance();
   virtual void OnHoverEnabledOrDisabled(bool disabled) { NOTIMPLEMENTED(); }
- 
+
   // This is only called by model in SetSelectedLine() after updating
   // everything.  Popup should already be visible.
   virtual void PaintUpdatesNow();
@@ -66,12 +66,9 @@ class AutocompletePopupViewMac : public AutocompletePopupView {
   // Returns the popup's model.
   virtual AutocompletePopupModel* GetModel();
 
-  // Helpers which forward to model_, otherwise our Objective-C helper
-  // object would need model_ to be public:.
-  size_t ResultRowCount();
-  const std::wstring& ResultContentsAt(size_t i);
-  bool ResultStarredAt(size_t i);
-  const std::wstring& ResultDescriptionAt(size_t i);
+  // Updates model_'s sense of selected line from the UI before
+  // calling edit_view_'s AcceptInput().  Used by internal Objective-C
+  // helper object.
   void AcceptInput();
 
  private:
@@ -83,7 +80,7 @@ class AutocompletePopupViewMac : public AutocompletePopupView {
 
   NSTextField* field_;  // owned by tab controller
 
-  scoped_nsobject<AutocompleteTableTarget> table_target_;
+  scoped_nsobject<AutocompleteMatrixTarget> matrix_target_;
   // TODO(shess): Before checkin review implementation to make sure
   // that popup_'s object hierarchy doesn't keep references to
   // destructed objects.
