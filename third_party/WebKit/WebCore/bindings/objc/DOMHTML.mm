4559923d4d5434061ed4843ca53f249622cfe38c
bolsinga@apple.com <bolsinga@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-06 00:54:41 +0000

    WebCore:
    2009-05-05  Greg Bolsinga  <bolsinga@apple.com>
    
            Reviewed by Sam Weinig.
    
            https://bugs.webkit.org/show_bug.cgi?id=25554
    
            Support HTML5 text control input types: email, number, tel, url
    
            Test: fast/html/text-field-input-types.html
    
            * bindings/objc/DOMHTML.mm:
            (-[DOMHTMLInputElement _isTextField]): Call HTMLInputElement::isTextField directly.
            * html/HTMLInputElement.cpp: Use the new types where appropriate.
            (WebCore::HTMLInputElement::setInputType):
            (WebCore::HTMLInputElement::type):
            (WebCore::HTMLInputElement::saveState):
            (WebCore::HTMLInputElement::restoreState):
            (WebCore::HTMLInputElement::accessKeyAction):
            (WebCore::HTMLInputElement::rendererIsNeeded):
            (WebCore::HTMLInputElement::createRenderer):
            (WebCore::HTMLInputElement::appendFormData):
            (WebCore::HTMLInputElement::valueWithDefault):
            (WebCore::HTMLInputElement::storesValueSeparateFromAttribute):
            (WebCore::HTMLInputElement::defaultEventHandler):
            * html/HTMLInputElement.h: Ditto.
            (WebCore::HTMLInputElement::):
            (WebCore::HTMLInputElement::isTextField):
    
    LayoutTests:
    
    2009-05-05  Greg Bolsinga  <bolsinga@apple.com>
    
            Reviewed by Sam Weinig.
    
            https://bugs.webkit.org/show_bug.cgi?id=25554
    
            Support HTML5 text control input types: email, number, tel, url
    
            * fast/html/text-field-input-types-expected.txt: Added.
            * fast/html/text-field-input-types.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43267 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/objc/DOMHTML.mm
+++ third_party/WebKit/WebCore/bindings/objc/DOMHTML.mm
@@ -105,33 +105,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (BOOL)_isTextField
 {
-    // We could make this public API as-is, or we could change it into a method that returns whether
-    // the element is a text field or a button or ... ?
-    static NSArray *textInputTypes = nil;
-#ifndef NDEBUG
-    static NSArray *nonTextInputTypes = nil;
-#endif
-    
-    NSString *fieldType = [self type];
-    
-    // No type at all is treated as text type
-    if ([fieldType length] == 0)
-        return YES;
-    
-    if (textInputTypes == nil)
-        textInputTypes = [[NSSet alloc] initWithObjects:@"text", @"password", @"search", @"isindex", nil];
-    
-    BOOL isText = [textInputTypes containsObject:[fieldType lowercaseString]];
-    
-#ifndef NDEBUG
-    if (nonTextInputTypes == nil)
-        nonTextInputTypes = [[NSSet alloc] initWithObjects:@"checkbox", @"radio", @"submit", @"reset", @"file", @"hidden", @"image", @"button", @"range", nil];
-    
-    // Catch cases where a new input type has been added that's not in these lists.
-    ASSERT(isText || [nonTextInputTypes containsObject:[fieldType lowercaseString]]);
-#endif    
-    
-    return isText;
+    return core(self)->isTextField();
 }
 
 - (NSRect)_rectOnScreen
--- third_party/WebKit/WebCore/html/HTMLInputElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLInputElement.cpp
@@ -231,6 +231,14 @@ void HTMLInputElement::setInputType(const String& t)
         newType = SEARCH;
     else if (equalIgnoringCase(t, "range"))
         newType = RANGE;
+    else if (equalIgnoringCase(t, "email"))
+        newType = EMAIL;
+    else if (equalIgnoringCase(t, "number"))
+        newType = NUMBER;
+    else if (equalIgnoringCase(t, "tel"))
+        newType = TELEPHONE;
+    else if (equalIgnoringCase(t, "url"))
+        newType = URL;
     else
         newType = TEXT;
 
@@ -314,6 +322,10 @@ const AtomicString& HTMLInputElement::type() const
             DEFINE_STATIC_LOCAL(const AtomicString, checkbox, ("checkbox"));
             return checkbox;
         }
+        case EMAIL: {
+            DEFINE_STATIC_LOCAL(const AtomicString, email, ("email"));
+            return email;
+        }
         case FILE: {
             DEFINE_STATIC_LOCAL(const AtomicString, file, ("file"));
             return file;
@@ -328,6 +340,10 @@ const AtomicString& HTMLInputElement::type() const
         }
         case ISINDEX:
             return emptyAtom;
+        case NUMBER: {
+            DEFINE_STATIC_LOCAL(const AtomicString, number, ("number"));
+            return number;
+        }
         case PASSWORD: {
             DEFINE_STATIC_LOCAL(const AtomicString, password, ("password"));
             return password;
@@ -352,9 +368,17 @@ const AtomicString& HTMLInputElement::type() const
             DEFINE_STATIC_LOCAL(const AtomicString, submit, ("submit"));
             return submit;
         }
+        case TELEPHONE: {
+            DEFINE_STATIC_LOCAL(const AtomicString, telephone, ("tel"));
+            return telephone;
+        }
         case TEXT: {
             DEFINE_STATIC_LOCAL(const AtomicString, text, ("text"));
             return text;
+        }
+        case URL: {
+            DEFINE_STATIC_LOCAL(const AtomicString, url, ("url"));
+            return url;
         }
     }
     return emptyAtom;
@@ -367,15 +391,19 @@ bool HTMLInputElement::saveState(String& result) const
 
     switch (inputType()) {
         case BUTTON:
+        case EMAIL:
         case FILE:
         case HIDDEN:
         case IMAGE:
         case ISINDEX:
+        case NUMBER:
         case RANGE:
         case RESET:
         case SEARCH:
         case SUBMIT:
+        case TELEPHONE:
         case TEXT:
+        case URL:
             result = value();
             return true;
         case CHECKBOX:
@@ -394,15 +422,19 @@ void HTMLInputElement::restoreState(const String& state)
     ASSERT(inputType() != PASSWORD); // should never save/restore password fields
     switch (inputType()) {
         case BUTTON:
+        case EMAIL:
         case FILE:
         case HIDDEN:
         case IMAGE:
         case ISINDEX:
+        case NUMBER:
         case RANGE:
         case RESET:
         case SEARCH:
         case SUBMIT:
+        case TELEPHONE:
         case TEXT:
+        case URL:
             setValue(state);
             break;
         case CHECKBOX:
@@ -498,10 +530,14 @@ void HTMLInputElement::accessKeyAction(bool sendToAnyElement)
         case HIDDEN:
             // a no-op for this type
             break;
+        case EMAIL:
         case ISINDEX:
+        case NUMBER:
         case PASSWORD:
         case SEARCH:
+        case TELEPHONE:
         case TEXT:
+        case URL:
             // should never restore previous selection here
             focus(false);
             break;
@@ -634,16 +670,20 @@ bool HTMLInputElement::rendererIsNeeded(RenderStyle *style)
     switch (inputType()) {
         case BUTTON:
         case CHECKBOX:
+        case EMAIL:
         case FILE:
         case IMAGE:
         case ISINDEX:
+        case NUMBER:
         case PASSWORD:
         case RADIO:
         case RANGE:
         case RESET:
         case SEARCH:
         case SUBMIT:
+        case TELEPHONE:
         case TEXT:
+        case URL:
             return HTMLFormControlElementWithState::rendererIsNeeded(style);
         case HIDDEN:
             return false;
@@ -670,10 +710,14 @@ RenderObject *HTMLInputElement::createRenderer(RenderArena *arena, RenderStyle *
             return new (arena) RenderImage(this);
         case RANGE:
             return new (arena) RenderSlider(this);
+        case EMAIL:
         case ISINDEX:
+        case NUMBER:
         case PASSWORD:
         case SEARCH:
+        case TELEPHONE:
         case TEXT:
+        case URL:
             return new (arena) RenderTextControlSingleLine(this);
     }
     ASSERT(false);
@@ -752,12 +796,16 @@ bool HTMLInputElement::appendFormData(FormDataList& encoding, bool multipart)
         return false;
 
     switch (inputType()) {
+        case EMAIL:
         case HIDDEN:
         case ISINDEX:
+        case NUMBER:
         case PASSWORD:
         case RANGE:
         case SEARCH:
+        case TELEPHONE:
         case TEXT:
+        case URL:
             // always successful
             encoding.appendData(name(), value());
             return true;
@@ -907,15 +955,19 @@ String HTMLInputElement::valueWithDefault() const
         switch (inputType()) {
             case BUTTON:
             case CHECKBOX:
+            case EMAIL:
             case FILE:
             case HIDDEN:
             case IMAGE:
             case ISINDEX:
+            case NUMBER:
             case PASSWORD:
             case RADIO:
             case RANGE:
             case SEARCH:
+            case TELEPHONE:
             case TEXT:
+            case URL:
                 break;
             case RESET:
                 v = resetButtonDefaultLabel();
@@ -1003,12 +1055,16 @@ bool HTMLInputElement::storesValueSeparateFromAttribute() const
         case RESET:
         case SUBMIT:
             return false;
+        case EMAIL:
         case FILE:
         case ISINDEX:
+        case NUMBER:
         case PASSWORD:
         case RANGE:
         case SEARCH:
+        case TELEPHONE:
         case TEXT:
+        case URL:
             return true;
     }
     return false;
@@ -1174,12 +1230,16 @@ void HTMLInputElement::defaultEventHandler(Event* evt)
         if (charCode == '\r') {
             switch (inputType()) {
                 case CHECKBOX:
+                case EMAIL:
                 case HIDDEN:
                 case ISINDEX:
+                case NUMBER:
                 case PASSWORD:
                 case RANGE:
                 case SEARCH:
+                case TELEPHONE:
                 case TEXT:
+                case URL:
                     // Simulate mouse click on the default form button for enter for these types of elements.
                     clickDefaultFormButton = true;
                     break;
@@ -1297,12 +1357,16 @@ void HTMLInputElement::defaultEventHandler(Event* evt)
                     if (!checked())
                         clickElement = true;
                     break;
+                case EMAIL:
                 case HIDDEN:
                 case ISINDEX:
+                case NUMBER:
                 case PASSWORD:
                 case RANGE:
                 case SEARCH:
+                case TELEPHONE:
                 case TEXT:
+                case URL:
                     break;
             }
         }
