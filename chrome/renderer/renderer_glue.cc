c5fe4086cce1850e32ab32e50f5a9733989ee0ad
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-06 17:37:21 +0000

    Add an extra colon to make inspector work again.
    This was broken by http://codereview.chromium.org/101026.
    
    
    R=glen
    BUG=11532
    TESt=open inspector. Inspector shouldn't show a blank page.
    
    Review URL: http://codereview.chromium.org/113034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15423 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/renderer_glue.cc
+++ chrome/renderer/renderer_glue.cc
@@ -210,7 +210,7 @@ void ClipboardReadHTML(string16* markup, GURL* url) {
 
 GURL GetInspectorURL() {
   return GURL(std::string(chrome::kChromeUIScheme) +
-              "//inspector/inspector.html");
+              "://inspector/inspector.html");
 }
 
 std::string GetUIResourceProtocol() {
