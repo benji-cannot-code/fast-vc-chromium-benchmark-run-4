f9a786b38440cb13421a26b4d529b8c53b8676c6
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-06 04:47:23 +0000

    2009-05-05  Darin Fisher  <darin@chromium.org>
            Fixing build bustage.
    
            Add some missing includes to fix the Chromium build.
    
            * bindings/v8/custom/V8DOMWindowCustom.cpp:
            * bindings/v8/custom/V8HTMLDocumentCustom.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43275 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Frame.h"
 #include "FrameLoadRequest.h"
 #include "FrameView.h"
+#include "HTMLCollection.h"
 #include "Page.h"
 #include "PlatformScreen.h"
 #include "ScriptSourceCode.h"
