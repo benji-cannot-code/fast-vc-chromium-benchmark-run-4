96ffe7637641a1b40f48ad7c7fe440dc3a5e9f8c
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-06 00:47:16 +0000

    Fix invisible mac bustage - this must have been skipped in my earlier CL. Correct include line.
    TBR=rohitrao
    Review URL: http://codereview.chromium.org/113014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15363 0039d316-1c4b-4281-b951-d872f2087c98

--- app/resource_bundle_mac.mm
+++ app/resource_bundle_mac.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#include "app/l10n_util.h"
 #include "base/base_paths.h"
 #include "base/data_pack.h"
 #include "base/file_path.h"
@@ -18,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/gfx/chrome_font.h"
-#include "chrome/common/l10n_util.h"
 
 
 ResourceBundle::~ResourceBundle() {
