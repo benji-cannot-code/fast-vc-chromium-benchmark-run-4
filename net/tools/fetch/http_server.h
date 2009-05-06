d9eb7613bdd61cea85a24269f6fc968af76994b7
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-06 00:17:50 +0000

    A utility driver for doing client/server HTTP transaction tests.
    This is an initial codebase - there is a lot of work to do.
    But I wanted to get an initial version checked in.
    
    http://crbug.com/6754
    Review URL: http://codereview.chromium.org/99333
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15360 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/tools/fetch/http_server.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_BASE_TOOLS_HTTP_SERVER_H_
+#define NET_BASE_TOOLS_HTTP_SERVER_H_
+
+#include "base/basictypes.h"
+#include "net/tools/fetch/http_session.h"
+
+// Implements a simple, single-threaded HttpServer.
+// Right now, this class implements no functionality above and beyond
+// the HttpSession.
+class HttpServer {
+public:
+  HttpServer(std::string ip, int port);
+  ~HttpServer();
+
+private:
+  scoped_ptr<HttpSession> session_;
+  DISALLOW_EVIL_CONSTRUCTORS(HttpServer);
+};
+
+#endif // NET_BASE_TOOLS_HTTP_SERVER_H_
+
---
+++ net/tools/fetch/http_server_request_info.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_BASE_TOOLS_HTTP_SERVER_REQUEST_INFO_H_
+#define NET_BASE_TOOLS_HTTP_SERVER_REQUEST_INFO_H_
+
+#include <string>
+
+#include "net/http/http_request_info.h"
+
+// Meta information about an HTTP request.
+// This is geared toward servers in that it keeps a map of the headers and
+// values rather than just a list of header strings (which net::HttpRequestInfo
+// does).
+class HttpServerRequestInfo : public net::HttpRequestInfo {
+ public:
+  HttpServerRequestInfo() : net::HttpRequestInfo() {}
+
+  // A map of the names -> values for HTTP headers.
+  std::map<std::string, std::string> headers;
+};
+
+#endif  // NET_BASE_TOOLS_HTTP_SERVER_REQUEST_INFO_H_
