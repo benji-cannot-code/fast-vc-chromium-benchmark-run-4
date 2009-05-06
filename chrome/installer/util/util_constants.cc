3665b9d46d7b792b6d8efcc84fa144ce7413aef2
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-06 16:20:18 +0000

    Re-re-write the logic to elevate uninstaller.
    BUG=7178
    TEST=Test install/uninstall in various combinations as described below and make sure that files or registry keys are not left behind:
    Distribution: {Chromium|Google Chrome}
    Install Level: {system|user}
    Delete Profile Option: {Selected|Not selected}
    Default Browser: {set|not set}
    
    Review URL: http://codereview.chromium.org/109037
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15412 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/util_constants.cc
+++ chrome/installer/util/util_constants.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -57,6 +57,9 @@ const wchar_t kRegisterChromeBrowser[] = L"register-chrome-browser";
 // Renames chrome.exe to old_chrome.exe and renames new_chrome.exe to chrome.exe
 // to support in-use updates. Also deletes opv key.
 const wchar_t kRenameChromeExe[] = L"rename-chrome-exe";
+
+// Removes Chrome registration from current machine. Requires admin rights.
+const wchar_t kRemoveChromeRegistration[] = L"remove-chrome-registration";
 
 // When we try to relaunch setup.exe as admin on Vista, we append this command
 // line flag so that we try the launch only once.
--- chrome/installer/util/util_constants.h
+++ chrome/installer/util/util_constants.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -81,6 +81,7 @@ extern const wchar_t kLogFile[];
 extern const wchar_t kMakeChromeDefault[];
 extern const wchar_t kRegisterChromeBrowser[];
 extern const wchar_t kRenameChromeExe[];
+extern const wchar_t kRemoveChromeRegistration[];
 extern const wchar_t kRunAsAdmin[];
 extern const wchar_t kSystemLevel[];
 extern const wchar_t kUninstall[];
