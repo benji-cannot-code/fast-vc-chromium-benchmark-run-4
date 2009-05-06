dfa7c11f485bd3a0b68c630442f11c95f56cef04
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-06 17:07:25 +0000

    Remove unused chrome switches.
    I also looked at the test_shell switches, but they are all used.
    
    Review URL: http://codereview.chromium.org/108036
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15421 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -21,7 +21,6 @@ extern const wchar_t kBrowserCrashTest[];
 extern const wchar_t kRendererCrashTest[];
 extern const wchar_t kRendererStartupDialog[];
 extern const wchar_t kPluginStartupDialog[];
-extern const wchar_t kTestShellStartupDialog[];
 extern const wchar_t kPluginLauncher[];
 
 extern const wchar_t kProcessChannelID[];
@@ -45,7 +44,6 @@ extern const wchar_t kDiskCacheDir[];
 extern const wchar_t kEnableUserDataDirProfiles[];
 extern const wchar_t kParentProfile[];
 extern const wchar_t kApp[];
-extern const wchar_t kAppUploadFile[];
 extern const wchar_t kDomAutomationController[];
 extern const wchar_t kPluginPath[];
 extern const wchar_t kUserAgent[];
