b9059f5290e1636159ad51c55002490ef9dbfd5c
jungshik@google.com <jungshik@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-06 00:11:57 +0000

    Make the width of the reset options confirmation dialog localizable.It's currently hard-coded to be 400, but it needs to be localizable.I made it localizable and set the value to 80 (which makes it exactly the same width as the current one) for most locales except for Malayalam for which it's too narrow. Once reviewed and landed in the trunk, this need to be merged to the branch. BUG=11267 TEST=Run CHrome with '--lang=ml', go to Options | Advanced and click on 'Restore the defaul settings' button at the bottom. The dialog box popping up should not have any truncated strings due to the dimension of the dialog being too small Review URL: http://codereview.chromium.org/100008
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15359 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/options/advanced_page_view.cc
+++ chrome/browser/views/options/advanced_page_view.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/views/window/window.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
+#include "grit/locale_settings.h"
 
 namespace {
 
@@ -65,13 +66,14 @@ class ResetDefaultsConfirmBox : public views::DialogDelegate {
  private:
   ResetDefaultsConfirmBox(HWND parent_hwnd, AdvancedPageView* page_view)
       : advanced_page_view_(page_view) {
-    const int kDialogWidth = 400;
+    int dialog_width = views::Window::GetLocalizedContentsWidth(
+        IDS_OPTIONS_RESET_CONFIRM_BOX_WIDTH_CHARS);
     // Also deleted when the window closes.
     message_box_view_ = new MessageBoxView(
         MessageBoxFlags::kFlagHasMessage | MessageBoxFlags::kFlagHasOKButton,
         l10n_util::GetString(IDS_OPTIONS_RESET_MESSAGE).c_str(),
         std::wstring(),
-        kDialogWidth);
+        dialog_width);
     views::Window::CreateChromeWindow(parent_hwnd, gfx::Rect(), this)->Show();
   }
   virtual ~ResetDefaultsConfirmBox() { }
