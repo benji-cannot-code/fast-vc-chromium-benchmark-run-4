480b2b566273b7ac7c7072e9ea5b7d141479ec61
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-06 18:57:30 +0000

    2009-05-06  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dave Hyatt, Dan Bernstein
    
            <rdar://problem/6860197> Reduce the size of FontFallbackList in 64-bit
    
            Re-order the data members of FontFallbackList to save 8 bytes when building 64-bit.
    
            * platform/graphics/FontFallbackList.cpp:
            (WebCore::FontFallbackList::FontFallbackList):
            * platform/graphics/FontFallbackList.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/FontFallbackList.cpp
+++ third_party/WebKit/WebCore/platform/graphics/FontFallbackList.cpp
@@ -37,10 +37,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 FontFallbackList::FontFallbackList()
-    : m_familyIndex(0)
+    : m_fontSelector(0)
+    , m_familyIndex(0)
     , m_pitch(UnknownPitch)
     , m_loadingCustomFonts(false)
-    , m_fontSelector(0)
     , m_generation(fontCache()->generation())
 {
 }
