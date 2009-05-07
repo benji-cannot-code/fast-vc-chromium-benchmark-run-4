93b2fc93362bac39da8257a35e75afabb7d768de
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-07 11:04:17 +0000

    Don't link-depend on views/ in chrome/browser/gtk
    This is a preparation for removing even more dependencies on views/
    on Linux, but further parts are more complicated.
    
    http://crbug.com/11066
    
    Review URL: http://codereview.chromium.org/115078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15538 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_window_gtk.cc
+++ chrome/browser/gtk/browser_window_gtk.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "app/resource_bundle.h"
 #include "base/base_paths_linux.h"
-#include "base/command_line.h"
 #include "base/gfx/gtk_util.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
@@ -34,11 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/render_widget_host_view_gtk.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/tab_contents/tab_contents_view.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_service.h"
-#include "chrome/views/controls/button/text_button.h"
 #include "grit/theme_resources.h"
 
 namespace {
@@ -47,13 +44,6 @@ namespace {
 const int kLoadingAnimationFrameTimeMs = 30;
 
 const GdkColor kBorderColor = GDK_COLOR_RGB(0xbe, 0xc8, 0xd4);
-
-class DummyButtonListener : public views::ButtonListener {
- public:
-  virtual void ButtonPressed(views::Button* sender) {
-    DLOG(ERROR) << "Button Pressed!";
-  }
-};
 
 gboolean MainWindowConfigured(GtkWindow* window, GdkEventConfigure* event,
                               BrowserWindowGtk* browser_win) {
@@ -286,20 +276,6 @@ BrowserWindowGtk::BrowserWindowGtk(Browser* browser)
   gtk_widget_set_double_buffered(content_vbox_, FALSE);
   g_signal_connect(G_OBJECT(content_vbox_), "expose-event",
                    G_CALLBACK(&OnContentAreaExpose), this);
-
-  // Temporary hack hidden behind a command line option to add one of the
-  // experimental ViewsGtk objects to the Gtk hierarchy.
-  const CommandLine& parsed_command_line = *CommandLine::ForCurrentProcess();
-  if (parsed_command_line.HasSwitch(switches::kViewsGtk)) {
-    experimental_widget_.reset(new views::WidgetGtk());
-    experimental_widget_->Init(gfx::Rect(), false);
-    experimental_widget_->SetContentsView(
-        new views::TextButton(new DummyButtonListener, L"Button"));
-
-    gtk_box_pack_start(GTK_BOX(content_vbox_),
-                       experimental_widget_->GetNativeView(),
-                       false, false, 2);
-  }
 
   toolbar_.reset(new BrowserToolbarGtk(browser_.get()));
   toolbar_->Init(browser_->profile(), window_);
