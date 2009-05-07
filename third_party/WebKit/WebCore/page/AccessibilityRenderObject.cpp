6660b9e007972c00f38d6cf30616dc0326f8c5f9
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-07 16:01:03 +0000

    Bug 25598: AX: if a radio button has a label and a title, the label is not exposed https://bugs.webkit.org/show_bug.cgi?id=25598
    git-svn-id: svn://svn.chromium.org/blink/trunk@43345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/AccessibilityRenderObject.cpp
+++ third_party/WebKit/WebCore/page/AccessibilityRenderObject.cpp
@@ -1131,6 +1131,18 @@ void AccessibilityRenderObject::linkedUIElements(AccessibilityChildrenVector& li
         addRadioButtonGroupMembers(linkedUIElements);
 }
 
+bool AccessibilityRenderObject::exposesTitleUIElement() const
+{
+    if (!isControl())
+        return false;
+
+    // checkbox or radio buttons don't expose the title ui element unless it has a title already
+    if (isCheckboxOrRadio() && getAttribute(titleAttr).isEmpty())
+        return false;
+    
+    return true;
+}
+    
 AccessibilityObject* AccessibilityRenderObject::titleUIElement() const
 {
     if (!m_renderer)
@@ -1140,8 +1152,7 @@ AccessibilityObject* AccessibilityRenderObject::titleUIElement() const
     if (isFieldset())
         return axObjectCache()->getOrCreate(static_cast<RenderFieldset*>(m_renderer)->findLegend());
     
-    // checkbox and radio hide their labels. Only controls get titleUIElements for now
-    if (isCheckboxOrRadio() || !isControl())
+    if (!exposesTitleUIElement())
         return 0;
     
     Node* element = m_renderer->node();
@@ -1174,7 +1185,7 @@ bool AccessibilityRenderObject::accessibilityIsIgnored() const
         HTMLElement* correspondingControl = labelElement->correspondingControl();
         if (correspondingControl && correspondingControl->renderer()) {
             AccessibilityObject* controlObject = axObjectCache()->getOrCreate(correspondingControl->renderer());
-            if (controlObject->isCheckboxOrRadio())
+            if (!controlObject->exposesTitleUIElement())
                 return true;
         }
     }
