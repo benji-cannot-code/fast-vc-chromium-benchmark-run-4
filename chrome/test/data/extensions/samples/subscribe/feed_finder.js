700b37793bfb1261ad528af6ad6d3a1b8edd82b6
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-07 01:12:01 +0000

    Fix an  occurence of 'chromium' that didn't get changed to 'chrome'
    Review URL: http://codereview.chromium.org/115049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15486 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/samples/subscribe/feed_finder.js
+++ chrome/test/data/extensions/samples/subscribe/feed_finder.js
@@ -15,6 +15,6 @@ function find() {
     while (item = result.iterateNext())
       feeds.push(item.href);
 
-    chromium.extension.connect().postMessage(feeds);
+    chrome.extension.connect().postMessage(feeds);
   }
 }
