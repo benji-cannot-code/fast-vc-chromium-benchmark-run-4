27a04a628aa9c195b7711e12fa0a6816797a7ff3
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-07 07:26:27 +0000

    Change the ViewStorage API back to the way it was (void for StoreView) since returning an int is just a test optimization.
    Expose a view_count() accessor on ViewStorage for unit tests to discern the number of views stored in it.
    
    Review URL: http://codereview.chromium.org/113091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15534 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/focus/view_storage.cc
+++ chrome/views/focus/view_storage.cc
@@ -53,7 +53,7 @@ int ViewStorage::CreateStorageID() {
   return view_storage_next_id_++;
 }
 
-int ViewStorage::StoreView(int storage_id, View* view) {
+void ViewStorage::StoreView(int storage_id, View* view) {
   DCHECK(view);
   std::map<int, ViewLocationInfo*>::iterator iter =
       id_to_view_location_.find(storage_id);
@@ -91,7 +91,6 @@ int ViewStorage::StoreView(int storage_id, View* view) {
     ids = id_iter->second;
   }
   ids->push_back(storage_id);
-  return storage_id;
 }
 
 View* ViewStorage::RetrieveView(int storage_id) {
