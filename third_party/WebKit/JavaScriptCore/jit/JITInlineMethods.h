cb03f2b33890f10fe4c71e0bcf1cb8c5d7ba3de0
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-07 23:33:00 +0000

    2009-05-07  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Cameron Zwarich.
            
            Replaced two more macros with references to the JITStackFrame structure.
    
            * jit/JIT.cpp:
            (JSC::JIT::privateCompileMainPass):
            * jit/JITInlineMethods.h:
            (JSC::JIT::restoreArgumentReference):
            * jit/JITStubs.cpp:
            (JSC::):
            * jit/JITStubs.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43374 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/jit/JITInlineMethods.h
+++ third_party/WebKit/JavaScriptCore/jit/JITInlineMethods.h
@@ -207,7 +207,7 @@ ALWAYS_INLINE JIT::Call JIT::emitNakedCall(void* function)
 ALWAYS_INLINE void JIT::restoreArgumentReference()
 {
     move(stackPointerRegister, firstArgumentRegister);
-    emitPutCTIParam(callFrameRegister, STUB_ARGS_callFrame);
+    emitPutCTIParam(callFrameRegister, offsetof(struct JITStackFrame, callFrame) / sizeof (void*));
 }
 ALWAYS_INLINE void JIT::restoreArgumentReferenceForTrampoline()
 {
@@ -221,13 +221,13 @@ ALWAYS_INLINE void JIT::restoreArgumentReferenceForTrampoline()
 ALWAYS_INLINE void JIT::restoreArgumentReference()
 {
     poke(stackPointerRegister);
-    emitPutCTIParam(callFrameRegister, STUB_ARGS_callFrame);
+    emitPutCTIParam(callFrameRegister, offsetof(struct JITStackFrame, callFrame) / sizeof (void*));
 }
 ALWAYS_INLINE void JIT::restoreArgumentReferenceForTrampoline() {}
 #else // JIT_STUB_ARGUMENT_VA_LIST
 ALWAYS_INLINE void JIT::restoreArgumentReference()
 {
-    emitPutCTIParam(callFrameRegister, STUB_ARGS_callFrame);
+    emitPutCTIParam(callFrameRegister, offsetof(struct JITStackFrame, callFrame) / sizeof (void*));
 }
 ALWAYS_INLINE void JIT::restoreArgumentReferenceForTrampoline() {}
 #endif
