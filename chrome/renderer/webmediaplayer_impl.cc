a76c80e543abac9b246be725e7ed89f6b493bb15
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-07 03:12:40 +0000

    Use ifdef to take away the paint method for media player from mac Since mac doesn't use skia, there's no WebCanvas, so disable everything using WebCanvas for mac.
    TBR=scherkus, darin
    
    Review URL: http://codereview.chromium.org/113076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15498 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/webmediaplayer_impl.cc
+++ chrome/renderer/webmediaplayer_impl.cc
@@ -289,6 +289,8 @@ void WebMediaPlayerImpl::setSize(const WebSize& size) {
   }
 }
 
+// TODO(hclam): enable this for mac.
+#if WEBKIT_USING_SKIA
 void WebMediaPlayerImpl::paint(skia::PlatformCanvas* canvas,
                                const WebRect& rect) {
   DCHECK(main_loop_ && MessageLoop::current() == main_loop_);
@@ -297,6 +299,7 @@ void WebMediaPlayerImpl::paint(skia::PlatformCanvas* canvas,
     video_renderer_->Paint(canvas, rect);
   }
 }
+#endif
 
 void WebMediaPlayerImpl::WillDestroyCurrentMessageLoop() {
   pipeline_.Stop();
