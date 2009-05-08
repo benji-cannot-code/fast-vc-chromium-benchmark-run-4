7a66f66611d290fcedf3cf2bfe5accf50e578502
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-08 22:53:29 +0000

    Reviewed by Kevin Ollivier.
    Make sure the path's refdata isn't deleted before we're done with the object.
            
    https://bugs.webkit.org/show_bug.cgi?id=25652
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43418 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/wx/PathWx.cpp
+++ third_party/WebKit/WebCore/platform/graphics/wx/PathWx.cpp
@@ -67,11 +67,12 @@ Path::Path()
 
 Path::~Path()
 {
+    clear();
 }
 
 Path::Path(const Path& path)
 { 
-    m_path = (PlatformPath*)&path.m_path;
+    m_path = new wxGraphicsPath(*path.m_path);
 }
 
 bool Path::contains(const FloatPoint& point, const WindRule rule) const
@@ -90,7 +91,7 @@ bool Path::contains(const FloatPoint& point, const WindRule rule) const
 
 void Path::translate(const FloatSize&)
 { 
-    notImplemented(); 
+    notImplemented();
 }
 
 FloatRect Path::boundingRect() const 
