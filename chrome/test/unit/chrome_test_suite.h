12c16814b5144d59b64a2eb4953f84c151a7bcfe
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-08 01:46:46 +0000

    Revert r15607 r15607 broke the build TBR=brg, thestig
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15608 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/unit/chrome_test_suite.h
+++ chrome/test/unit/chrome_test_suite.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "app/resource_bundle.h"
 #include "base/stats_table.h"
-#include "base/file_util.h"
+#include "base/file_path.h"
 #if defined(OS_MACOSX)
 #include "base/mac_util.h"
 #endif
@@ -82,9 +82,8 @@ class ChromeTestSuite : public TestSuite {
         CommandLine::ForCurrentProcess()->GetSwitchValue(
             switches::kUserDataDir));
     if (user_data_dir.empty() &&
-        file_util::CreateNewTempDirectory("chrome_test_", &user_data_dir)) {
+      PathService::Get(base::DIR_EXE, &user_data_dir))
       user_data_dir = user_data_dir.AppendASCII("test_user_data");
-    }
     if (!user_data_dir.empty())
       PathService::Override(chrome::DIR_USER_DATA,
                             user_data_dir.ToWStringHack());
@@ -124,13 +123,6 @@ class ChromeTestSuite : public TestSuite {
     StatsTable::set_current(NULL);
     delete stats_table_;
 
-    // Delete the test_user_data dir recursively
-    FilePath user_data_dir;
-    if (PathService::Get(chrome::DIR_USER_DATA, &user_data_dir) &&
-        !user_data_dir.empty()) {
-      file_util::Delete(user_data_dir, true);
-      file_util::Delete(user_data_dir.DirName(), false);
-    }
     TestSuite::Shutdown();
   }
 
