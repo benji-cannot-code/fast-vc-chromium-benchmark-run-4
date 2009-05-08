ae3d298a11961dad4c38938ad074b1d2c05466f3
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-08 10:17:58 +0000

    DevTools: Activate inspector window on break / exception.
    Review URL: http://codereview.chromium.org/113100
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15632 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/debugger_agent.js
+++ webkit/glue/devtools/js/debugger_agent.js
@@ -495,6 +495,7 @@ devtools.DebuggerAgent.prototype.handleBacktraceResponse_ = function(msg) {
   this.currentCallFrame_ = f;
   
   WebInspector.pausedScript();
+  DevToolsHost.activateWindow();
 };
 
 
