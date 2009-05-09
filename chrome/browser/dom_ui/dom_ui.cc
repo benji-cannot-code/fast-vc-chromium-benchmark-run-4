0236b1ec7ee063f2ba6d7a9d44e5064ba1b3f374
glen@chromium.org <glen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-09 01:30:19 +0000

    Remove unnecessary win_util from dom_ui.
    TBR=beng
    
    Review URL: http://codereview.chromium.org/113175
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15706 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/dom_ui.cc
+++ chrome/browser/dom_ui/dom_ui.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util-inl.h"
 #include "base/string_util.h"
 #include "base/values.h"
-#include "base/win_util.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/tab_contents/tab_contents_view.h"
