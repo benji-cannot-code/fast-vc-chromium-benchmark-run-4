888ed5fcc7f70aa5f13c41226d580f001ff5a140
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-10 17:53:22 +0000

    Reviewed by Dan Bernstein.
            <rdar://problem/6867598> REGRESSION (r42483): Have to enter credentials twice when trying
            to view protected MobileMe video
    
            Add a temporary workaround.
    
            * platform/network/ResourceHandleInternal.h:
            (WebCore::ResourceHandleInternal::ResourceHandleInternal):
            Added an m_useSiteSpecificQuirks boolean (Mac-only). A Frame pointer is only available when
            starting a request, so we need to store this data for later use.
    
            * platform/network/mac/ResourceHandleMac.mm:
            (WebCore::ResourceHandle::start): Initialize m_useSiteSpecificQuirks.
            (WebCore::ResourceHandle::receivedCredential): Use per-session credentials with gallery.me.com.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43462 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceHandleInternal.h
+++ third_party/WebKit/WebCore/platform/network/ResourceHandleInternal.h
@@ -128,6 +128,7 @@ namespace WebCore {
 #endif
 #if PLATFORM(MAC)
             , m_startWhenScheduled(false)
+            , m_needsSiteSpecificQuirks(false)
             , m_currentMacChallenge(nil)
 #elif USE(CFNETWORK)
             , m_currentCFChallenge(0)
@@ -163,6 +164,7 @@ namespace WebCore {
         RetainPtr<WebCoreResourceHandleAsDelegate> m_delegate;
         RetainPtr<id> m_proxy;
         bool m_startWhenScheduled;
+        bool m_needsSiteSpecificQuirks;
 #endif
 #if USE(WININET)
         HANDLE m_fileHandle;
