b10660b738c5e6b91df1f2b0de6ca9ec4f8719a7
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-10 20:43:05 +0000

    fix build bustage
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15741 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webworkerclient_impl.cc
+++ webkit/glue/webworkerclient_impl.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/api/public/WebKit.h"
 #include "webkit/api/public/WebKitClient.h"
 #include "webkit/api/public/WebString.h"
+#include "webkit/api/public/WebURL.h"
 #include "webkit/api/public/WebWorker.h"
 #include "webkit/glue/glue_util.h"
 #include "webkit/glue/webframeloaderclient_impl.h"
