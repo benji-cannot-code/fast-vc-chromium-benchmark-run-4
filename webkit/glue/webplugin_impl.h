6790df1dbfeeaeb99aa71edcdc0d1dac278b4aaf
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-10 05:29:32 +0000

    Fix plugin window sticking around when parent became invisible.  Added test for all different scenarios.
    Note I grabbed the plugin hwnd and manually checked it instead of looking for callbacks from the plugin's SetWindow since the latter isn't called if the visibility changes.
    
    BUG=1842096
    TEST=regression test included, test http://chromedashboard per bug
    Review URL: http://codereview.chromium.org/115169
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15732 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_impl.h
+++ webkit/glue/webplugin_impl.h
@@ -230,11 +230,6 @@ class WebPluginImpl : public WebPlugin,
   // Notifies the plugin about focus changes.
   void setFocus();
 
-  // Called by WebPluginContainer::show/hide, which overrides Widget show/hide.
-  // This allows us to control the visible state of the plugin window.
-  void show();
-  void hide();
-
   // Handle widget events.
   void handleEvent(WebCore::Event* event);
   void handleMouseEvent(WebCore::MouseEvent* event);
@@ -339,7 +334,6 @@ class WebPluginImpl : public WebPlugin,
   WebFrameImpl* webframe_;
 
   WebPluginDelegate* delegate_;
-  bool visible_;
 
   WebPluginContainer* widget_;
 
