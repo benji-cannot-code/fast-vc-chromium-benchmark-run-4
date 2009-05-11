c18eb7fa12548bf1477a3b731f3d4a315162700a
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-11 17:28:42 +0000

    2009-05-11  Brady Eidson  <beidson@apple.com>
            Reviewed by Darin Adler.
    
            Remove unused header that declared one function which was never defined anywhere.
    
            * WebCore.vcproj/WebCore.vcproj:
            * platform/network/cf/ResourceResponseCFNet.cpp: Change to include ResourceResponse.h (which really means
              platform/network/cf/ResourceResponse.h)
            * platform/network/cf/ResourceResponseCFNet.h: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/ResourceResponseCFNet.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/ResourceResponseCFNet.cpp
@@ -25,11 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "ResourceResponseCFNet.h"
+#include "ResourceResponse.h"
 
 #include "HTTPParsers.h"
 #include "MIMETypeRegistry.h"
-#include "ResourceResponse.h"
 #include <CFNetwork/CFURLResponsePriv.h>
 #include <wtf/RetainPtr.h>
 
