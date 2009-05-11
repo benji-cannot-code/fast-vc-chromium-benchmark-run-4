ef5c26e7b461a977149121962862e4e844d2e72f
dkegel@google.com <dkegel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-11 19:16:16 +0000

    Move timeout to right spot in tools/valgrind/chrome_tests.py.  Fixes thinko committed in issue 114012.
    Review URL: http://codereview.chromium.org/115191
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15770 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/chrome_tests.py
+++ tools/valgrind/chrome_tests.py
@@ -241,10 +241,10 @@ class ChromeTests:
   def TestUI(self):
     return self.SimpleTest("chrome", "ui_tests",
                            valgrind_test_args=[
+                            "--timeout=120000",
                             "--trace_children",
                             "--indirect"],
                            cmd_args=[
-                            "--timeout=120000",
                             "--ui-test-timeout=120000",
                             "--ui-test-action-timeout=80000",
                             "--ui-test-action-max-timeout=180000"])
