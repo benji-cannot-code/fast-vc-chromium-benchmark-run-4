709647b5682628ede8464e2b05ebf75556318b33
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-11 00:27:40 +0000

    Touch Nodes.cpp to try to fix Windows build
            * parser/Nodes.cpp: Removed whitespace.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/parser/Nodes.cpp
+++ third_party/WebKit/JavaScriptCore/parser/Nodes.cpp
@@ -100,7 +100,7 @@ RegisterID* ThrowableExpressionData::emitThrowError(BytecodeGenerator& generator
     generator.emitThrow(exception);
     return exception;
 }
-    
+
 // ------------------------------ StatementNode --------------------------------
 
 void StatementNode::setLoc(int firstLine, int lastLine)
