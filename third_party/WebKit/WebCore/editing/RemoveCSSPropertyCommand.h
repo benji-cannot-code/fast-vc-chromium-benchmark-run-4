e8ce6cb608b5d05b6f227e08f2507f92ec87c86e
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-11 23:54:27 +0000

    2009-05-11  Norbert Leser  <norbert.leser@nokia.com>
            Reviewed by Darin Adler.
    
            The Symbian tools can't compile and link this without this include.
            It's not clear why this is different from other toolsets or why it affects
            only this header file, but adding the include for now.
    
            * editing/RemoveCSSPropertyCommand.h: Added include of CSSMutableStyleDeclaration.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43523 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/RemoveCSSPropertyCommand.h
+++ third_party/WebKit/WebCore/editing/RemoveCSSPropertyCommand.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RemoveCSSPropertyCommand_h
 
 #include "EditCommand.h"
+#include "CSSMutableStyleDeclaration.h"
 #include "CSSPropertyNames.h"
 
 namespace WebCore {
