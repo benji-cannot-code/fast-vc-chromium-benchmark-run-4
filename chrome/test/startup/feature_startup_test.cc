840a90af84b72472d9745eca42fdd9d5641244ab
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-12 00:44:36 +0000

    The version of gcc on mac (4.2) doesn't like inheriting from a test in an anonymous namespace.  Just move the actual test cases into the namespace so everything is in the same translation unit.
    TBR=evan
    
    Review URL: http://codereview.chromium.org/113240
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15826 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/startup/feature_startup_test.cc
+++ chrome/test/startup/feature_startup_test.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/perftimer.h"
 #include "base/time.h"
-#include "build/build_config.h"
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/automation/browser_proxy.h"
@@ -90,12 +89,7 @@ class NewTabUIStartupTest : public UITest {
   }
 };
 
-}  // namespace
-
 // TODO(pamg): run these tests with a reference build?
-
-// TODO(tc): Fix this.
-#if !defined(OS_MACOSX)
 TEST_F(NewTabUIStartupTest, PerfCold) {
   RunStartupTest("tab_cold", false /* not cold */, true /* important */);
 }
@@ -103,4 +97,5 @@ TEST_F(NewTabUIStartupTest, PerfCold) {
 TEST_F(NewTabUIStartupTest, DISABLED_PerfWarm) {
   RunStartupTest("tab_warm", true /* cold */, false /* not important */);
 }
-#endif
+
+}  // namespace
