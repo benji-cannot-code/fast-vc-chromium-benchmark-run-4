3ae2e5be96e42a1c4edcaa8cd4a6af6b60e59c56
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-12 20:16:21 +0000

    Reviewed by Darin Adler.
            - declare a forgotten method
    
            * DOM/WebDOMOperationsPrivate.h: Declare -[DOMNode markupString] in the
            WebDOMNodeOperationsPendingPublic category.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/DOM/WebDOMOperationsPrivate.h
+++ third_party/WebKit/WebKit/mac/DOM/WebDOMOperationsPrivate.h
@@ -32,3 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface DOMDocument (WebDOMDocumentOperationsPrivate)
 - (NSArray *)_focusableNodes;
 @end
+
+@interface DOMNode (WebDOMNodeOperationsPendingPublic)
+- (NSString *)markupString;
+@end
