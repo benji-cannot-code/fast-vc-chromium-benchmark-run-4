3a485a0478bdb24d4b26b589dd44c204dde91599
jmalonzo@webkit.org <jmalonzo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-13 14:22:00 +0000

    Revert "2009-05-12  Jan Michael Alonzo  <jmalonzo@webkit.org>"
    This reverts commit Revert r43562 - [Gtk] WTF_USE_JSC is already defined in
    WebCore/config.h.
    
    * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -337,7 +337,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if PLATFORM(GTK)
-#define WTF_USE_JSC 1
 #if HAVE(PTHREAD_H)
 #define WTF_USE_PTHREADS 1
 #endif
