ee0f461436f7349620ce15d754c761c3af92b3af
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-13 19:31:47 +0000

    Reverting 15986. Review URL: http://codereview.chromium.org/113359
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15987 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/webplugin_delegate_proxy.h
+++ chrome/renderer/webplugin_delegate_proxy.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_RENDERER_WEBPLUGIN_DELEGATE_PROXY_H__
 
 #include <string>
-#include <vector>
 
 #include "base/gfx/rect.h"
 #include "base/gfx/native_widget_types.h"
@@ -21,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class GURL;
 struct NPObject;
 class NPObjectStub;
-struct NPVariant_Param;
 struct PluginHostMsg_URLRequest_Params;
 class RenderView;
 class SkBitmap;
@@ -128,10 +126,6 @@ class WebPluginDelegateProxy : public WebPluginDelegate,
   void OnShowModalHTMLDialog(const GURL& url, int width, int height,
                              const std::string& json_arguments,
                              std::string* json_retval);
-  void OnGetDragData(const NPVariant_Param& event, bool add_data,
-                     std::vector<NPVariant_Param>* values, bool* success);
-  void OnSetDropEffect(const NPVariant_Param& event, int effect,
-                       bool* success);
   void OnMissingPluginStatus(int status);
   void OnGetCPBrowsingContext(uint32* context);
   void OnCancelDocumentLoad();
