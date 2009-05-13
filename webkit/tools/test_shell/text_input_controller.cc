4b88e5743937042bf177470215d5e8a84181a769
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-13 21:50:53 +0000

    Properly convert from UTF8 to UTF16 in WebTextInputImpl.
    String util changes by darin@chromium.org.
    
    BUG=11699
    Review URL: http://codereview.chromium.org/115215
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16006 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/text_input_controller.cc
+++ webkit/tools/test_shell/text_input_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/tools/test_shell/text_input_controller.h"
 
+#include "base/string_util.h"
 #include "webkit/glue/webview.h"
 #include "webkit/glue/webframe.h"
 #include "webkit/glue/webtextinput.h"
@@ -51,7 +52,7 @@ void TextInputController::insertText(
     return;
 
   if (args.size() >= 1 && args[0].isString()) {
-    text_input->InsertText(args[0].ToString());
+    text_input->InsertText(UTF8ToUTF16(args[0].ToString()));
   }
 }
 
@@ -64,7 +65,7 @@ void TextInputController::doCommand(
     return;
 
   if (args.size() >= 1 && args[0].isString()) {
-    text_input->DoCommand(args[0].ToString());
+    text_input->DoCommand(UTF8ToUTF16(args[0].ToString()));
   }
 }
 
@@ -78,7 +79,7 @@ void TextInputController::setMarkedText(
 
   if (args.size() >= 3 && args[0].isString()
       && args[1].isNumber() && args[2].isNumber()) {
-    text_input->SetMarkedText(args[0].ToString(),
+    text_input->SetMarkedText(UTF8ToUTF16(args[0].ToString()),
                               args[1].ToInt32(),
                               args[2].ToInt32());
   }
