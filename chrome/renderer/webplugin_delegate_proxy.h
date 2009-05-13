2140972ee68c50294d7f7f423cf09338258abaae
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-13 19:22:56 +0000

    CPAPI gears drag drop and renderer IPC....
    CPAPI (0.10) functions for gears drag drop; one to extract the
    drag type/data given an NPObject *event, one to override the
    drop effect (drag cursor).
    
    Gears drag drop API receives a browser event as an NPObject* so
    the event is untrusted.  Provide IPC calls to the renderer so
    gears can pass the event to renderer/V8 for checking, prior to
    drag type/data extraction, or the setting of the drop effect.
    
    V8 event checking is a TODO(noel), http://mondrian/10947778 for
    the CPAPI (0.10) change submitted to gears.
    
    BUG=7995
    
    Original patch by Noel Gordon via:
    http://codereview.chromium.org/99240
    Review URL: http://codereview.chromium.org/115280
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15986 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/webplugin_delegate_proxy.h
+++ chrome/renderer/webplugin_delegate_proxy.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_RENDERER_WEBPLUGIN_DELEGATE_PROXY_H__
 
 #include <string>
+#include <vector>
 
 #include "base/gfx/rect.h"
 #include "base/gfx/native_widget_types.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class GURL;
 struct NPObject;
 class NPObjectStub;
+struct NPVariant_Param;
 struct PluginHostMsg_URLRequest_Params;
 class RenderView;
 class SkBitmap;
@@ -126,6 +128,10 @@ class WebPluginDelegateProxy : public WebPluginDelegate,
   void OnShowModalHTMLDialog(const GURL& url, int width, int height,
                              const std::string& json_arguments,
                              std::string* json_retval);
+  void OnGetDragData(const NPVariant_Param& event, bool add_data,
+                     std::vector<NPVariant_Param>* values, bool* success);
+  void OnSetDropEffect(const NPVariant_Param& event, int effect,
+                       bool* success);
   void OnMissingPluginStatus(int status);
   void OnGetCPBrowsingContext(uint32* context);
   void OnCancelDocumentLoad();
