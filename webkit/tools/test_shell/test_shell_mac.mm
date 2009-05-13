969edaf2daf3dfbc55c96df429e9317c04edf618
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-13 14:04:11 +0000

    Fix the mac build break with skia move. Review URL: http://codereview.chromium.org/115294
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15951 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_mac.mm
+++ webkit/tools/test_shell/test_shell_mac.mm
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/tools/test_shell/simple_resource_loader_bridge.h"
 #include "webkit/tools/test_shell/test_navigation_controller.h"
 
-#import "skia/include/SkBitmap.h"
+#include "SkBitmap.h"
 
 #import "mac/DumpRenderTreePasteboard.h"
 
