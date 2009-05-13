85881fbc793f3ce8fe5b57a3374eade6e056e59f
ariya@webkit.org <ariya@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-13 17:13:45 +0000

    2009-05-13  Ariya Hidayat  <ariya.hidayat@nokia.com>
            Unreviewed build fix, as suggested by Yael Aharon <yael.aharon@nokia.com>.
    
            * wtf/qt/ThreadingQt.cpp:
            (WTF::waitForThreadCompletion): renamed IsValid to isValid.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43637 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/qt/ThreadingQt.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/qt/ThreadingQt.cpp
@@ -121,7 +121,7 @@ void setThreadNameInternal(const char*)
 
 int waitForThreadCompletion(ThreadIdentifier threadID, void** result)
 {
-    ASSERT(threadID.IsValid());
+    ASSERT(threadID.isValid());
 
     QThread* thread = threadID.platformId();
 
