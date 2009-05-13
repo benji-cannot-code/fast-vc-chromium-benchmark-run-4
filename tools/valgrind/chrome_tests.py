f65e8dba3c7d0bf58aa483dc4b5fb4073ddd026c
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-13 20:06:50 +0000

    valgrind: print the gtest suppression list on startup.
    This is in the docs, but there are a lot of docs, and all the other files
    it's pulling flags from are printed during startup.
    
    Review URL: http://codereview.chromium.org/113358
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15990 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/chrome_tests.py
+++ tools/valgrind/chrome_tests.py
@@ -177,6 +177,7 @@ class ChromeTests:
     for directory in self._data_dirs:
       filename = os.path.join(directory, name + ".gtest.txt")
       if os.path.exists(filename):
+        logging.info("reading gtest filters from %s" % filename)
         f = open(filename, 'r')
         for line in f.readlines():
           if line.startswith("#") or line.startswith("//") or line.isspace():
