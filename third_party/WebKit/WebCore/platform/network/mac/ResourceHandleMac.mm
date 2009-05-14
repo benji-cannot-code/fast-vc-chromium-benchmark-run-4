35b1ad981d3dbfa5c1564591fbc931003c290196
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-14 16:45:39 +0000

    Reviewed by Darin Adler.
            https://bugs.webkit.org/show_bug.cgi?id=25784
            Leaks seen on HTTP tests
    
            We should change ResourceHandleMac.mm to use smart pointers some day, but this is a minimal fix.
    
            * platform/network/mac/ResourceHandleMac.mm:
            (WebCore::ResourceHandle::loadResourceSynchronously): Release mutable request copy made for
            disabling content sniffing, matching the normal code path.
            (-[WebCoreSynchronousLoader connection:willSendRequest:redirectResponse:]): Release m_user
            and m_pass before overwriting.
            (-[WebCoreSynchronousLoader connection:didReceiveAuthenticationChallenge:]): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43709 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/mac/ResourceHandleMac.mm
+++ third_party/WebKit/WebCore/platform/network/mac/ResourceHandleMac.mm
@@ -353,7 +353,7 @@ void ResourceHandle::loadResourceSynchronously(const ResourceRequest& request, S
     
     NSURLRequest *nsRequest;
     if (!shouldContentSniffURL(request.url())) {
-        NSMutableURLRequest *mutableRequest = [request.nsURLRequest() mutableCopy];
+        NSMutableURLRequest *mutableRequest = [[request.nsURLRequest() mutableCopy] autorelease];
         wkSetNSURLRequestShouldContentSniff(mutableRequest, NO);
         nsRequest = mutableRequest;
     } else
@@ -799,6 +799,8 @@ void ResourceHandle::receivedCancellation(const AuthenticationChallenge& challen
 
     if (redirectResponse) {
         // Take user/pass out of the URL.
+        [m_user release];
+        [m_pass release];
         m_user = [[m_url user] copy];
         m_pass = [[m_url password] copy];
         if (m_user || m_pass) {
@@ -830,6 +832,8 @@ void ResourceHandle::receivedCancellation(const AuthenticationChallenge& challen
         WebCoreCredentialStorage::set(credential, [challenge protectionSpace]);
         [[challenge sender] useCredential:credential forAuthenticationChallenge:challenge];
         [credential release];
+        [m_user release];
+        [m_pass release];
         m_user = 0;
         m_pass = 0;
         return;
