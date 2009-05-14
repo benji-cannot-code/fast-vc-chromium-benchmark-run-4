5110b27434d213d6217a8d2832a2436dfbd5fdf4
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-14 14:23:11 +0000

    2009-05-14  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=25325
    
            Make sure pthread_self() is declared before it gets called in Collector.cpp
    
            * runtime/Collector.cpp: Include pthread.h in most Unix-like platforms
            (not just for OPENBSD)
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/Collector.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/Collector.cpp
@@ -60,9 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if PLATFORM(SOLARIS)
 #include <thread.h>
-#endif
-
-#if PLATFORM(OPENBSD)
+#else
 #include <pthread.h>
 #endif
 
