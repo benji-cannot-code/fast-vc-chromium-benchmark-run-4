c252610d9a848d2aca37be56cdc29618c69775d4
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-14 01:05:27 +0000

    Retry r16006.
    Review URL: http://codereview.chromium.org/113369
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16025 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webtextinput.h
+++ webkit/glue/webtextinput.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 #include "base/basictypes.h"
+#include "base/string16.h"
 
 class WebTextInput {
  public:
@@ -15,13 +16,13 @@ class WebTextInput {
   virtual ~WebTextInput() {}
 
   // Inserts text to the associated frame.
-  virtual void InsertText(const std::string& text) = 0;
+  virtual void InsertText(const string16& text) = 0;
 
   // Executes the given editing command on the frame.
-  virtual void DoCommand(const std::string& command) = 0;
+  virtual void DoCommand(const string16& command) = 0;
 
   // Sets marked text region on the frame.
-  virtual void SetMarkedText(const std::string& text,
+  virtual void SetMarkedText(const string16& text,
                              int32_t location, int32_t length) = 0;
 
   // Clears the marked text region on the frame.
