8eaeaf3617cb079aacedf9a46c6403c23f4d3177
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-14 16:11:25 +0000

    2009-05-14  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            Remove a JSC-specific include that is already added by including
            ScriptController.
    
            No change in behavior, so no tests.
    
            * dom/Document.cpp: Removed JSDOMBinding include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43705 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -81,7 +81,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HitTestResult.h"
 #include "ImageLoader.h"
 #include "InspectorController.h"
-#include "JSDOMBinding.h"
 #include "ScriptEventListener.h"
 #include "KeyboardEvent.h"
 #include "Logging.h"
