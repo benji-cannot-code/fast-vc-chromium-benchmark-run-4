446ca86d4fdb9cbb9d6cafe6bfa6f9b09e901a7f
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-14 06:26:32 +0000

    Minor cleanup of SSL UI code.
    TBR=jcampan
    TEST=No functionality change.
    
    Review URL: http://codereview.chromium.org/113392
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16044 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ssl/ssl_policy.h
+++ chrome/browser/ssl/ssl_policy.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_SSL_POLICY_H_
-#define CHROME_BROWSER_SSL_POLICY_H_
+#ifndef CHROME_BROWSER_SSL_SSL_POLICY_H_
+#define CHROME_BROWSER_SSL_SSL_POLICY_H_
 
 #include "base/singleton.h"
 #include "chrome/browser/ssl/ssl_blocking_page.h"
@@ -57,4 +57,4 @@ class SSLPolicy : public SSLManager::Delegate,
   DISALLOW_COPY_AND_ASSIGN(SSLPolicy);
 };
 
-#endif  // CHROME_BROWSER_SSL_POLICY_H_
+#endif  // CHROME_BROWSER_SSL_SSL_POLICY_H_
