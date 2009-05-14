339fb59ae8dc7738d6fe22d5365e98a0957231e1
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-14 20:47:56 +0000

    2009-05-14  David Hyatt  <hyatt@apple.com>
            Reviewed by Tim Hatcher.
    
            Fix for <rdar://problem/6886217> REGRESSION (S4 beta-ToT): Adium chat window contents no longer resize.
    
            Technically this is a bug in Adium.  It appears that Adium has subclassed the WebView and implemented
            viewDidMoveToWindow in its subclass improperly.  It doesn't call up to the base class WebView like it
            should and so our boundsChanged notification never gets added.
    
            Reduce the dependence on viewDidMoveToWindow by moving the registration of observers into
            viewWillMoveToWindow instead.
    
            * WebView/WebView.mm:
            (-[WebView addSizeObserversForWindow:]):
            (-[WebView removeWindowObservers]):
            (-[WebView addWindowObserversForWindow:]):
            (-[WebView viewWillMoveToWindow:]):
            (-[WebView viewDidMoveToWindow]):
            (-[WebView viewDidMoveToSuperview]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43727 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -2710,11 +2710,11 @@ static bool needsWebViewInitThreadWorkaround()
     }
 }
 
-- (void)addSizeObservers
+- (void)addSizeObserversForWindow:(NSWindow *)window
 {
     // -addSizeObservers can be called from -viewDidMoveToSuperview: below -[NSView initWithCoder:], before
     // we've had a chance to initialize _private
-    if (_private && [self window]) {
+    if (_private && window) {
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_boundsChanged) 
             name:NSViewFrameDidChangeNotification object:self];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_boundsChanged) 
@@ -2723,9 +2723,8 @@ static bool needsWebViewInitThreadWorkaround()
     }
 }
 
-- (void)addWindowObservers
+- (void)addWindowObserversForWindow:(NSWindow *)window
 {
-    NSWindow *window = [self window];
     if (!_private->useDocumentViews && window) {
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowDidBecomeKey:)
             name:NSWindowDidBecomeKeyNotification object:nil];
@@ -2754,10 +2753,7 @@ static bool needsWebViewInitThreadWorkaround()
     // Don't do anything if the WebView isn't initialized.
     // This happens when decoding a WebView in a nib.
     // FIXME: What sets up the observer of NSWindowWillCloseNotification in this case?
-    if (!_private)
-        return;
-
-    if (_private->closed)
+    if (!_private || _private->closed)
         return;
     
     if ([self window] && [self window] != [self hostWindow])
@@ -2771,11 +2767,16 @@ static bool needsWebViewInitThreadWorkaround()
         // and over, so do them when we move into a window.
         [window setAcceptsMouseMovedEvents:YES];
         WKSetNSWindowShouldPostEventNotifications(window, YES);
-        
-        [self removeWindowObservers];
-        [self removeSizeObservers];
     } else
         _private->page->willMoveOffscreen();
+        
+    if (window != [self window]) {
+        [self removeSizeObservers];
+        [self removeWindowObservers];
+
+        [self addSizeObserversForWindow:window];
+        [self addWindowObserversForWindow:window];
+    }
 }
 
 - (void)viewDidMoveToWindow
@@ -2787,11 +2788,8 @@ static bool needsWebViewInitThreadWorkaround()
     if (!_private || _private->closed)
         return;
         
-    if ([self window]) {
-        [self addWindowObservers];
-        [self addSizeObservers];
+    if ([self window])
         _private->page->didMoveOnscreen();
-    }
 }
 
 - (void)_updateFocusedAndActiveState
@@ -3151,7 +3149,7 @@ static bool needsWebViewInitThreadWorkaround()
 - (void)viewDidMoveToSuperview
 {
     if ([self superview] != nil)
-        [self addSizeObservers];
+        [self addSizeObserversForWindow:[self window]];
 }
 
 - (void)setApplicationNameForUserAgent:(NSString *)applicationName
