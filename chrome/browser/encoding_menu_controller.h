e00c8fe8985218deefecb58c81968a0f1022af0d
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-14 20:02:42 +0000

    Add #include <string> to fix building with gcc 4.3. Review URL: http://codereview.chromium.org/115372
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16095 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/encoding_menu_controller.h
+++ chrome/browser/encoding_menu_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHORME_BROWSER_ENCODING_MENU_CONTROLLER_H_
 #define CHORME_BROWSER_ENCODING_MENU_CONTROLLER_H_
 
+#include <string>
 #include <vector>
 
 #include "base/basictypes.h"  // For DISALLOW_IMPLICIT_CONSTRUCTORS
