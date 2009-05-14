82bd87d0f15b9db2e7e5bbd79bd0507b0c94f40c
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-14 21:49:13 +0000

    Fix bustage... some more traces of always-on-top I forgot to remove.
    TBR=jcampan
    Review URL: http://codereview.chromium.org/114028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16108 0039d316-1c4b-4281-b951-d872f2087c98

--- views/window/window_gtk.cc
+++ views/window/window_gtk.cc
@@ -163,7 +163,6 @@ gfx::NativeWindow WindowGtk::GetNativeWindow() const {
 WindowGtk::WindowGtk(WindowDelegate* window_delegate)
     : WidgetGtk(TYPE_WINDOW),
       is_modal_(false),
-      is_always_on_top_(false),
       window_delegate_(window_delegate),
       non_client_view_(new NonClientView(this)) {
   window_delegate_->window_.reset(this);
@@ -177,7 +176,6 @@ void WindowGtk::Init(const gfx::Rect& bounds) {
     // TODO(erg): Fix once modality works.
     // BecomeModal();
   }
-  is_always_on_top_ = window_delegate_->IsAlwaysOnTop();
 
   WidgetGtk::Init(bounds, true);
 
@@ -189,7 +187,6 @@ void WindowGtk::Init(const gfx::Rect& bounds) {
   UpdateWindowTitle();
 
   //  SetInitialBounds(bounds);
-  //  InitAlwaysOnTopState();
 
   // if (!IsAppWindow()) {
   //   notification_registrar_.Add(
