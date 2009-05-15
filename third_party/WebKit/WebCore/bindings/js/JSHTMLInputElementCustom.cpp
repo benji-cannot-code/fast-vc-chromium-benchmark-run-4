4423b04b465a7dfc82ac17429d790d807b744fd9
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-15 20:09:36 +0000

    2009-05-15  Sam Weinig  <sam@webkit.org>
            Reviewed by Beth Dakin.
    
            Fix for https://bugs.webkit.org/show_bug.cgi?id=25826
            <rdar://problem/6884742> 
            REGRESSION: In Gmail's Edit Link dialog, I can't type in the Link To: field (due to <input type=url> support)
    
            Added a site specific quirk for mail.google.com which returns "text" when getting the type of an <input type=url>
    
            * bindings/js/JSHTMLInputElementCustom.cpp:
            (WebCore::needsGmailQuirk):
            (WebCore::JSHTMLInputElement::type):
            * html/HTMLInputElement.idl:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43779 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSHTMLInputElementCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSHTMLInputElementCustom.cpp
@@ -27,11 +27,45 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "JSHTMLInputElement.h"
 
+#include "Document.h"
 #include "HTMLInputElement.h"
+#include "Settings.h"
 
 using namespace JSC;
 
 namespace WebCore {
+
+static bool needsGmailQuirk(HTMLInputElement* input)
+{
+    Document* document = input->document();
+
+    const KURL& url = document->url();
+    if (url.host() != "mail.google.com")
+        return false;
+
+    // As with other site-specific quirks, allow website developers to turn this off.
+    // In theory, this allows website developers to check if their fixes are effective.
+    Settings* settings = document->settings();
+    if (!settings)
+        return false;
+    if (!settings->needsSiteSpecificQuirks())
+        return false;
+
+    return true;
+}
+
+JSValue JSHTMLInputElement::type(ExecState* exec) const
+{
+    HTMLInputElement* input = static_cast<HTMLInputElement*>(impl());
+    const AtomicString& type = input->type();
+
+    DEFINE_STATIC_LOCAL(const AtomicString, url, ("url"));
+    DEFINE_STATIC_LOCAL(const AtomicString, text, ("text"));
+
+    if (type == url && needsGmailQuirk(input))
+        return jsString(exec, text);
+    return jsString(exec, type);
+}
 
 JSValue JSHTMLInputElement::selectionStart(ExecState* exec) const
 {
