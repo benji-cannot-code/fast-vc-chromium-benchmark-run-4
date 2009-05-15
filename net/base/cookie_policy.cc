ac6de1d0b71b1a3a3598340853a0d28383c2dc82
idanan@chromium.org <idanan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-15 15:24:46 +0000

    Privacy option added for all cookies to become session cookies.
    BUG=10502
    Review URL: http://codereview.chromium.org/87047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16158 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/cookie_policy.cc
+++ net/base/cookie_policy.cc
@@ -17,6 +17,8 @@ bool CookiePolicy::CanGetCookies(const GURL& url, const GURL& policy_url) {
       return true;
     case CookiePolicy::BLOCK_THIRD_PARTY_COOKIES:
       return true;
+    case CookiePolicy::SESSION_COOKIES:
+      return true;
     case CookiePolicy::BLOCK_ALL_COOKIES:
       return false;
     default:
@@ -34,6 +36,8 @@ bool CookiePolicy::CanSetCookie(const GURL& url, const GURL& policy_url) {
         return true;  // Empty policy URL should indicate a first-party request
       return net::RegistryControlledDomainService::SameDomainOrHost(url,
                                                                     policy_url);
+    case CookiePolicy::SESSION_COOKIES:
+      return true;
     case CookiePolicy::BLOCK_ALL_COOKIES:
       return false;
     default:
