95363af71093f829fd3aafa5d7f84e74f8cf205c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-15 05:38:48 +0000

    2009-05-14  Rahul Kuchhal  <kuchhal@chromium.org>
            Reviewed by Eric Seidel.
    
            Initialize m_spreadMethod in the second constructor too. Not sure if
            a test case can be written, so no test case.
            https://bugs.webkit.org/show_bug.cgi?id=25814
    
            * platform/graphics/Gradient.cpp:
            (WebCore::Gradient::Gradient):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/Gradient.cpp
+++ third_party/WebKit/WebCore/platform/graphics/Gradient.cpp
@@ -53,6 +53,7 @@ Gradient::Gradient(const FloatPoint& p0, float r0, const FloatPoint& p1, float r
     , m_r1(r1)
     , m_stopsSorted(false)
     , m_lastStop(0)
+    , m_spreadMethod(SpreadMethodPad)
 {
     platformInit();
 }
