8b864a2cbeffb65b5971c9fcd091db2b90b97c60
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-16 21:51:42 +0000

    WebKit:
    2009-05-16  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Sam Weinig and Dan Bernstein.
    
            Fix <rdar://problem/6889644>.
            
            * English.lproj/Localizable.strings:
    
    WebKit/mac:
    
    2009-05-16  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Sam Weinig and Dan Bernstein.
    
            Fix <rdar://problem/6889644>
            
            * Plugins/Hosted/NetscapePluginHostManager.mm:
            (WebKit::NetscapePluginHostManager::spawnPluginHost):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/Hosted/NetscapePluginHostManager.mm
+++ third_party/WebKit/WebKit/mac/Plugins/Hosted/NetscapePluginHostManager.mm
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "NetscapePluginHostProxy.h"
 #import "NetscapePluginInstanceProxy.h"
+#import "WebLocalizableStrings.h"
 #import "WebKitSystemInterface.h"
 #import "WebNetscapePluginPackage.h"
 #import <mach/mach_port.h>
@@ -135,7 +136,10 @@ bool NetscapePluginHostManager::spawnPluginHost(WebNetscapePluginPackage *packag
         return false;
     }
     
-    NSString *visibleName = [NSString stringWithFormat:@"%@ - %@", [package filename], [[NSProcessInfo processInfo] processName]];
+    NSString *visibleName = [NSString stringWithFormat:UI_STRING("%@ (%@ Internet plug-in)",
+                                                                 "visible name of the plug-in host process. The first argument is the plug-in name "
+                                                                 "and the second argument is the application name."),
+                             [[package filename] stringByDeletingPathExtension], [[NSProcessInfo processInfo] processName]];
     
     NSDictionary *hostProperties = [[NSDictionary alloc] initWithObjectsAndKeys:
                                     visibleName, @"visibleName",
