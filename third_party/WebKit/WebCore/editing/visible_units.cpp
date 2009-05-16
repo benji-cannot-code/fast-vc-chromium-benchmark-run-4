d30737fcc7f285033c5f4a9dc2090a9f33aabd61
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-16 00:06:07 +0000

    WebCore:
            Reviewed by Sam Weinig.
    
            - fix <rdar://problem/6888244> REGRESSION: Using the Up or Down keyboard
              arrows, I can't navigate caret between To Do items in a Note message
    
            Test: editing/selection/move-by-line-004.html
    
            * editing/visible_units.cpp:
            (WebCore::enclosingNodeWithNonInlineRenderer): Added this helper
            function.
            (WebCore::previousLinePosition): When advancing to the next candidate
            node, stop when leaving the closest ancestor of the original node that
            is not an inline. The code, incorrectly, was trying to exit the closest
            ancestor of that type which was editable, and therefore missing other
            blocks in the same editable root.
            (WebCore::nextLinePosition): Ditto.
    
    LayoutTests:
    
            Reviewed by Sam Weinig.
    
            - test for <rdar://problem/6888244> REGRESSION: Using the Up or Down
              keyboard arrows, I can't navigate caret between To Do items in a Note
              message
    
            * editing/selection/move-by-line-004-expected.txt: Added.
            * editing/selection/move-by-line-004.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43792 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/visible_units.cpp
+++ third_party/WebKit/WebCore/editing/visible_units.cpp
@@ -540,6 +540,15 @@ static Node* previousLeafWithSameEditability(Node* node)
     return 0;
 }
 
+static Node* enclosingNodeWithNonInlineRenderer(Node* n)
+{
+    for (Node* p = n; p; p = p->parentNode()) {
+        if (p->renderer() && !p->renderer()->isInline())
+            return p;
+    }
+    return 0;
+}
+
 VisiblePosition previousLinePosition(const VisiblePosition &visiblePosition, int x)
 {
     Position p = visiblePosition.deepEquivalent();
@@ -569,9 +578,9 @@ VisiblePosition previousLinePosition(const VisiblePosition &visiblePosition, int
         // This containing editable block does not have a previous line.
         // Need to move back to previous containing editable block in this root editable
         // block and find the last root line box in that block.
-        Node* startBlock = enclosingBlock(node);
+        Node* startBlock = enclosingNodeWithNonInlineRenderer(node);
         Node* n = previousLeafWithSameEditability(node);
-        while (n && startBlock == enclosingBlock(n))
+        while (n && startBlock == enclosingNodeWithNonInlineRenderer(n))
             n = previousLeafWithSameEditability(n);
         while (n) {
             if (highestEditableRoot(Position(n, 0)) != highestRoot)
@@ -671,9 +680,9 @@ VisiblePosition nextLinePosition(const VisiblePosition &visiblePosition, int x)
         // This containing editable block does not have a next line.
         // Need to move forward to next containing editable block in this root editable
         // block and find the first root line box in that block.
-        Node* startBlock = enclosingBlock(node);
+        Node* startBlock = enclosingNodeWithNonInlineRenderer(node);
         Node* n = nextLeafWithSameEditability(node, p.deprecatedEditingOffset());
-        while (n && startBlock == enclosingBlock(n))
+        while (n && startBlock == enclosingNodeWithNonInlineRenderer(n))
             n = nextLeafWithSameEditability(n);
         while (n) {
             if (highestEditableRoot(Position(n, 0)) != highestRoot)
