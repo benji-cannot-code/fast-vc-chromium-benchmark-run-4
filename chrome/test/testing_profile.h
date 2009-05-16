9233b46650ea8aff020bcebe5be074e07980f72f
glen@chromium.org <glen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-16 07:04:42 +0000

    Attempt to fix leaks in DOMUISources test
    TBR=arv
    
    Review URL: http://codereview.chromium.org/115441
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/testing_profile.h
+++ chrome/test/testing_profile.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/browser_prefs.h"
+#include "chrome/browser/browser_theme_provider.h"
 #include "chrome/browser/history/history.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/search_engines/template_url_model.h"
@@ -52,6 +53,9 @@ class TestingProfile : public Profile {
 
   // Creates a TemplateURLModel. If not invoked the TemplateURLModel is NULL.
   void CreateTemplateURLModel();
+
+  // Creates a ThemeProvider. If not invoked the ThemeProvider is NULL.
+  void CreateThemeProvider();
 
   virtual FilePath GetPath() {
     return path_;
@@ -124,7 +128,7 @@ class TestingProfile : public Profile {
   virtual void SetTheme(Extension* extension) { }
   virtual void ClearTheme() { }
   virtual ThemeProvider* GetThemeProvider() {
-    return NULL;
+    return theme_provider_.get();
   }
   virtual URLRequestContext* GetRequestContext() {
     return NULL;
@@ -228,6 +232,9 @@ class TestingProfile : public Profile {
 
   // The SessionService. Defaults to NULL, but can be set using the setter.
   scoped_refptr<SessionService> session_service_;
+
+  // The theme provider. Only created if CreateThemeProvider is invoked.
+  scoped_refptr<BrowserThemeProvider> theme_provider_;
 
   // Do we have a history service? This defaults to the value of
   // history_service, but can be explicitly set.
