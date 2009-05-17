7f225e916755ed49bb9059c5f9a105725617f8c3
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-17 16:50:21 +0000

    2009-05-17  Chris Fleizach  <cfleizach@apple.com>
            Reviewed by Darin Adler.
    
            Bug 25769: Fix accessibility warnings on GTK
            https://bugs.webkit.org/show_bug.cgi?id=25769
    
            * page/AccessibilityTable.cpp:
            (WebCore::AccessibilityTable::columnCount): Get rid of excess const.
            (WebCore::AccessibilityTable::rowCount): Ditto.
            * page/AccessibilityTable.h: Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/AccessibilityTable.h
+++ third_party/WebKit/WebCore/page/AccessibilityTable.h
@@ -64,8 +64,8 @@ public:
     AccessibilityChildrenVector& columns();
     AccessibilityChildrenVector& rows();
     
-    const unsigned columnCount();
-    const unsigned rowCount();
+    unsigned columnCount();
+    unsigned rowCount();
     
     virtual String title() const;
     
