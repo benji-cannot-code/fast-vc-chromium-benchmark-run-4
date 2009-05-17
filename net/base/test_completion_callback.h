e99a364670ac6eaa4cc652bbeca8ae0ce9f7f754
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-17 15:24:38 +0000

    Add connected socket function to ClientSocketPool and ClientSocketHandle. Provide a new api in ClientSocketPool and ClientSocketHandle for clients (notably HttpNetworkTransaction) to request connected sockets, rather than having to allocate and Connect() sockets themselves.  Add a bunch of tests for it.  Have not begun switching clients over to using this.
    Review URL: http://codereview.chromium.org/99143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16260 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/test_completion_callback.h
+++ net/base/test_completion_callback.h
@@ -39,7 +39,6 @@ class TestCompletionCallback : public CallbackRunner< Tuple1<int> > {
     return result_;
   }
 
- private:
   virtual void RunWithParams(const Tuple1<int>& params) {
     result_ = params.a;
     have_result_ = true;
@@ -47,6 +46,7 @@ class TestCompletionCallback : public CallbackRunner< Tuple1<int> > {
       MessageLoop::current()->Quit();
   }
 
+ private:
   int result_;
   bool have_result_;
   bool waiting_for_result_;
