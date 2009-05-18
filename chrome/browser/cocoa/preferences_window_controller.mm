7b15bdc0779922f3dec8972162d7d91f9a9f107f
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-18 12:47:28 +0000

    Fix for crash in empty home page field. Ensure pref gets set back to NTP. BUG=11852. Review URL: http://codereview.chromium.org/113363
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16274 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/preferences_window_controller.mm
+++ chrome/browser/cocoa/preferences_window_controller.mm
@@ -28,7 +28,8 @@ NSString* const kUserDoneEditingPrefsNotification =
 
 namespace {
 std::wstring GetNewTabUIURLString() {
-  return UTF8ToWide(chrome::kChromeUINewTabURL);
+  std::wstring temp = UTF8ToWide(chrome::kChromeUINewTabURL);
+  return URLFixerUpper::FixupURL(temp, std::wstring());
 }
 }  // namespace
 
@@ -314,6 +315,8 @@ enum { kHomepageNewTabPage, kHomepageURL };
   // If the text field contains a valid URL, sync it to prefs. We run it
   // through the fixer upper to allow input like "google.com" to be converted
   // to something valid ("http://google.com").
+  if (!urlString)
+    urlString = [NSString stringWithFormat:@"%s", chrome::kChromeUINewTabURL];
   std::wstring temp = base::SysNSStringToWide(urlString);
   std::wstring fixedString = URLFixerUpper::FixupURL(temp, std::wstring());
   if (GURL(WideToUTF8(fixedString)).is_valid())
