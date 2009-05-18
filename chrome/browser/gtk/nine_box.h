c6f5db3d5f61f6c0c8648a6fd723e17b42ebed06
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-18 22:56:11 +0000

    Linux: Just enough toolbar theming to make everything look normal again.
    http://crbug.com/11679
    
    Review URL: http://codereview.chromium.org/115438
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16339 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/nine_box.h
+++ chrome/browser/gtk/nine_box.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <gtk/gtk.h>
 
+class ThemeProvider;
+
 // A NineBox manages a set of source images representing a 3x3 grid, where
 // non-corner images can be tiled to make a larger image.  It's used to
 // use bitmaps for constructing image-based resizable widgets like buttons.
@@ -25,6 +27,12 @@ class NineBox {
   // ids that will be passed to the resource bundle.  Use 0 for no image.
   NineBox(int top_left, int top, int top_right, int left, int center, int right,
           int bottom_left, int bottom, int bottom_right);
+
+  // Same as above, but use themed images.
+  NineBox(ThemeProvider* theme_provider,
+          int top_left, int top, int top_right, int left, int center, int right,
+          int bottom_left, int bottom, int bottom_right);
+
   ~NineBox();
 
   // Render the NineBox to |dst|.
