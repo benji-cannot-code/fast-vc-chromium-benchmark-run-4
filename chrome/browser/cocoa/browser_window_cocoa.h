7a307257ed53961a6acc5d95dee82ab99de69be4
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-18 18:26:58 +0000

    Switch to a real API for putting titles in docked windows.
    Review URL: http://codereview.chromium.org/113536
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16305 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_window_cocoa.h
+++ chrome/browser/cocoa/browser_window_cocoa.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Browser;
 @class BrowserWindowController;
 @class FindBarCocoaController;
-@class NSString;
 @class NSWindow;
 @class NSMenu;
 
@@ -83,8 +82,6 @@ class BrowserWindowCocoa : public BrowserWindow,
   virtual void DestroyBrowser();
 
  private:
-  void SetMinimizedWindowTitle(NSWindow* window, NSString* title);
-
   NSWindow* window_;  // weak, owned by controller
   Browser* browser_;  // weak, owned by controller
   BrowserWindowController* controller_;  // weak, owns us
--- chrome/browser/cocoa/browser_window_cocoa.mm
+++ chrome/browser/cocoa/browser_window_cocoa.mm
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gfx/rect.h"
 #include "base/logging.h"
-#include "base/scoped_cftyperef.h"
 #include "base/sys_string_conversions.h"
 #include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/cocoa/browser_window_cocoa.h"
@@ -15,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_service.h"
 #include "chrome/browser/profile.h"
-
-#include <ApplicationServices/ApplicationServices.h>
 
 BrowserWindowCocoa::BrowserWindowCocoa(Browser* browser,
                                        BrowserWindowController* controller,
@@ -98,9 +95,8 @@ void BrowserWindowCocoa::UpdateTitleBar() {
   // Window menu
   [NSApp changeWindowsItem:window_ title:newTitle filename:NO];
 
-  // Dock (if applicable)
-  if ([window_ isMiniaturized])
-    SetMinimizedWindowTitle(window_, newTitle);
+  // Dock
+  [window_ setMiniwindowTitle:newTitle];
 }
 
 void BrowserWindowCocoa::UpdateLoadingAnimations(bool should_animate) {
@@ -258,23 +254,4 @@ void BrowserWindowCocoa::DestroyBrowser() {
 
   // at this point the controller is dead (autoreleased), so
   // make sure we don't try to reference it any more.
-}
-
-void BrowserWindowCocoa::SetMinimizedWindowTitle(NSWindow* window,
-                                                 NSString* title) {
-  typedef OSStatus (*CoreDockSetItemTitlePtr)(CGWindowID wid,
-                                              CFStringRef title);
-
-  scoped_cftyperef<CFBundleRef> hi_services(
-      CFBundleGetBundleWithIdentifier(CFSTR("com.apple.HIServices")));
-  if (!hi_services)
-    return;
-
-  CoreDockSetItemTitlePtr CoreDockSetItemTitle =
-      (CoreDockSetItemTitlePtr)
-      CFBundleGetFunctionPointerForName(hi_services,
-                                        CFSTR("CoreDockSetItemTitle"));
-
-  if (CoreDockSetItemTitle)
-    CoreDockSetItemTitle([window windowNumber], (CFStringRef)title);
 }
