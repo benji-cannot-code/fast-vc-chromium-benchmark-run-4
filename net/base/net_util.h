5a9c2a1c58f64ccc543822a1678c6d931771973f
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-18 16:47:32 +0000

    Include C system headers before C++ system headers.
    Document that GetHostAndPort returns an IPv6 literal
    address with square brackets.
    
    Document that the host parameter to the ProxyServer
    constructor takes an IPv6 literal address with square
    brackets.
    
    R=eroman
    BUG=N/A
    TEST=N/A
    Review URL: http://codereview.chromium.org/113494
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16287 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_util.h
+++ net/base/net_util.h
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#include <string>
-
 #ifdef OS_WIN
 #include <windows.h>
 #endif
+
+#include <string>
 
 #include "base/basictypes.h"
 
@@ -40,7 +40,8 @@ bool FileURLToFilePath(const GURL& url, FilePath* file_path);
 // Saves the result into |*host| and |*port|. If the input did not have
 // the optional port, sets |*port| to -1.
 // Returns true if the parsing was successful, false otherwise.
-// The returned host is NOT canonicalized, and may be invalid.
+// The returned host is NOT canonicalized, and may be invalid. If <host> is
+// an IPv6 literal address, the returned host includes the square brackets.
 bool GetHostAndPort(std::string::const_iterator host_and_port_begin,
                     std::string::const_iterator host_and_port_end,
                     std::string* host,
