63e2be6b607a6334ac10d6fa70f3acca59f287c2
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-19 06:06:22 +0000

    2009-05-18  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Gavin Barraclough.
            
            - for polymorphic prototype lookups, increase the number of slots from 4 to 8
            
            ~4% faster on v8 raytrace benchmark
    
            * bytecode/Instruction.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/bytecode/Instruction.h
+++ third_party/WebKit/JavaScriptCore/bytecode/Instruction.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Structure.h"
 #include <wtf/VectorTraits.h>
 
-#define POLYMORPHIC_LIST_CACHE_SIZE 4
+#define POLYMORPHIC_LIST_CACHE_SIZE 8
 
 namespace JSC {
 
