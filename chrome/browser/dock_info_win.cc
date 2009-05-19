98b9d7997c9c4c68b38ce82e4264066a590bf5a3
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-19 15:02:13 +0000

    Fix the Windows release build by adding some missing includes. Review URL: http://codereview.chromium.org/115511
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16375 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dock_info_win.cc
+++ chrome/browser/dock_info_win.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/dock_info.h"
 
+#include <atlbase.h>
+#include <atlapp.h>
+#include <atlmisc.h>
+
 #include "base/basictypes.h"
 #include "base/logging.h"
 #include "chrome/browser/browser.h"
