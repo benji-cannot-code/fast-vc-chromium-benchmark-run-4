02db71b22677d1c56293cbd2a7a887b8d4362899
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-19 21:31:27 +0000

    comment out test in linux
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16417 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/child_process_security_policy_unittest.cc
+++ chrome/browser/child_process_security_policy_unittest.cc
@@ -275,6 +275,8 @@ TEST_F(ChildProcessSecurityPolicyTest, RemoveRace) {
   EXPECT_FALSE(p->HasDOMUIBindings(kRendererID));
 }
 
+#if defined(OS_WIN)
+
 class ChildProcessSecurityPolicyInProcessBrowserTest
     : public InProcessBrowserTest {
  public:
@@ -312,3 +314,5 @@ IN_PROC_BROWSER_TEST_F(ChildProcessSecurityPolicyInProcessBrowserTest, NoLeak) {
   EXPECT_EQ(
       ChildProcessSecurityPolicy::GetInstance()->security_state_.size(), 1U);
 }
+
+#endif
