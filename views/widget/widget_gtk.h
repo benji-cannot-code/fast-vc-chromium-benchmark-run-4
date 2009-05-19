76ed7994b55e42aa890f71ca4fe2fea6961a0d9e
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-19 23:26:44 +0000

    Fixes delivery of mouse events in WidgetGtk. Also fixes GetRootWidget and GetWidget to match windows.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/113603
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16431 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/widget_gtk.h
+++ views/widget/widget_gtk.h
@@ -21,7 +21,7 @@ class View;
 class WindowGtk;
 
 // Widget implementation for GTK.
-class WidgetGtk : public Widget {
+class WidgetGtk : public Widget, public MessageLoopForUI::Observer {
  public:
   // Type of widget.
   enum Type {
@@ -65,6 +65,10 @@ class WidgetGtk : public Widget {
   virtual Window* GetWindow();
   virtual const Window* GetWindow() const;
 
+  // MessageLoopForUI::Observer.
+  virtual void WillProcessEvent(GdkEvent* event) {}
+  virtual void DidProcessEvent(GdkEvent* event);
+
  protected:
   virtual void OnSizeAllocate(GtkWidget* widget, GtkAllocation* allocation);
   virtual void OnPaint(GtkWidget* widget, GdkEventExpose* event);
@@ -88,10 +92,19 @@ class WidgetGtk : public Widget {
                                       GdkEventVisibility* event) {
     return false;
   }
+  virtual gboolean OnGrabBrokeEvent(GtkWidget* widget, GdkEvent* event);
+  virtual void OnGrabNotify(GtkWidget* widget, gboolean was_grabbed);
+
+  // Returns whether capture should be released on mouse release. The default
+  // is true.
+  virtual bool ReleaseCaptureOnMouseReleased() { return true; }
 
   // Sets and retrieves the WidgetGtk in the userdata section of the widget.
   static WindowGtk* GetWindowForNative(GtkWidget* widget);
   static void SetWindowForNative(GtkWidget* widget, WindowGtk* window);
+
+  // Are we a subclass of WindowGtk?
+  bool is_window_;
 
  private:
   virtual RootView* CreateRootView();
@@ -122,11 +135,16 @@ class WidgetGtk : public Widget {
   static gboolean CallScroll(GtkWidget* widget, GdkEventScroll* event);
   static gboolean CallVisibilityNotify(GtkWidget* widget,
                                        GdkEventVisibility* event);
+  static gboolean CallGrabBrokeEvent(GtkWidget* widget, GdkEvent* event);
+  static void CallGrabNotify(GtkWidget* widget, gboolean was_grabbed);
 
+  // Returns the first ancestor of |widget| that is a window.
   static Window* GetWindowImpl(GtkWidget* widget);
 
   // Creates the GtkWidget.
   void CreateGtkWidget();
+
+  void HandleGrabBroke();
 
   const Type type_;
 
@@ -141,6 +159,9 @@ class WidgetGtk : public Widget {
 
   // If true, the mouse is currently down.
   bool is_mouse_down_;
+
+  // Have we done a mouse grab?
+  bool has_capture_;
 
   // The following are used to detect duplicate mouse move events and not
   // deliver them. Displaying a window may result in the system generating
