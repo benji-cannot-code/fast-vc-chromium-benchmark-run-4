171d6bbc768e82024ca060ccc840b6894808c03c
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-19 06:10:26 +0000

    Fix Linux Bustage from new CL where it is missing string import
    Patch by Mohamed Mansour.
    
    R=abarth
    BUG=6646
    TEST=compiles
    
    http://codereview.chromium.org/113573
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16360 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_index.h
+++ chrome/browser/bookmarks/bookmark_index.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 #include <map>
 #include <set>
+#include <string>
 #include <vector>
 
 #include "base/basictypes.h"
