f53c6f7355cd247e66f56cfb240a70ae94ba4f35
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-19 23:39:03 +0000

    Cleanup code to keep flag showing "was cached" only in URLRequestHttpJob
    r=wtc
    Review URL: http://codereview.chromium.org/113535
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16435 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/filter.cc
+++ net/base/filter.cc
@@ -40,7 +40,7 @@ const char kTextHtml[]             = "text/html";
 
 Filter* Filter::Factory(const std::vector<FilterType>& filter_types,
                         const FilterContext& filter_context) {
-  DCHECK(filter_context.GetInputStreamBufferSize() > 0);
+  DCHECK_GT(filter_context.GetInputStreamBufferSize(), 0);
   if (filter_types.empty() || filter_context.GetInputStreamBufferSize() <= 0)
     return NULL;
 
@@ -264,7 +264,7 @@ Filter::~Filter() {}
 
 bool Filter::InitBuffer() {
   int buffer_size = filter_context_.GetInputStreamBufferSize();
-  DCHECK(buffer_size > 0);
+  DCHECK_GT(buffer_size, 0);
   if (buffer_size <= 0 || stream_buffer())
     return false;
 
