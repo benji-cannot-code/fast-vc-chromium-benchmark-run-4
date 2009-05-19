131e7276ea4d8f96e566a251574e5982993b753d
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-19 15:23:12 +0000

    Gets dragged_tab_view to compile on linux.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/115495
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16377 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/dragged_tab_controller.cc
+++ chrome/browser/views/tabs/dragged_tab_controller.cc
@@ -432,6 +432,7 @@ void DraggedTabController::Observe(NotificationType type,
 ///////////////////////////////////////////////////////////////////////////////
 // DraggedTabController, MessageLoop::Observer implementation:
 
+#if defined(OS_WIN)
 void DraggedTabController::WillProcessMessage(const MSG& msg) {
 }
 
@@ -443,6 +444,12 @@ void DraggedTabController::DidProcessMessage(const MSG& msg) {
   if (msg.message == WM_KEYDOWN && msg.wParam == VK_ESCAPE)
     EndDrag(true);
 }
+
+#else
+void DraggedTabController::WillProcessEvent(GdkEvent* event) {
+  NOTIMPLEMENTED();
+}
+#endif
 
 ///////////////////////////////////////////////////////////////////////////////
 // DraggedTabController, private:
--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -1593,6 +1593,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 ['include', '^browser/dock_info_gtk.cc'],
                 ['include', '^browser/dock_info.cc'],
                 ['include', '^browser/dock_info.h'],
+                ['include', '^browser/views/tabs/dragged_tab_view.cc'],
+                ['include', '^browser/views/tabs/dragged_tab_view.h'],
                 ['include', '^browser/views/tabs/tab.cc'],
                 ['include', '^browser/views/tabs/tab.h'],
                 ['include', '^browser/views/tabs/tab_renderer.cc'],
