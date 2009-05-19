e0ceb6929fc1fc4a00a9bd48016246fe449260c8
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-19 21:57:01 +0000

    WebCore:
    2009-05-19  Antti Koivisto  <antti@apple.com>
    
            Reviewed by Darin Adler).
            
            <rdar://problem/6902674> REGRESSION: Query parameters are sometimes incorrect in URL (23135)
            
            Push characters back in correct order when entity parsing fails after 3 characters.
    
            Test: fast/tokenizer/preload-scanner-entities.html
    
            * html/PreloadScanner.cpp:
            (WebCore::PreloadScanner::consumeEntity):
    
    LayoutTests:
    
    2009-05-19  Antti Koivisto  <antti@apple.com>
    
            Reviewed by Darin Adler.
            
            <rdar://problem/6902674> REGRESSION: Query parameters are sometimes incorrect in URL (23135)
    
            * fast/tokenizer/preload-scanner-entities-expected.txt: Added.
            * fast/tokenizer/preload-scanner-entities.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43876 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/PreloadScanner.cpp
+++ third_party/WebKit/WebCore/html/PreloadScanner.cpp
@@ -227,8 +227,8 @@ unsigned PreloadScanner::consumeEntity(SegmentedString& source, bool& notEnoughC
             else if (cc >= 'A' && cc <= 'F')
                 result = 10 + cc - 'A';
             else {
-                source.push(seenChars[1]);
                 source.push('#');
+                source.push(seenChars[1]);
                 return 0;
             }
             entityState = Hex;
@@ -282,8 +282,8 @@ unsigned PreloadScanner::consumeEntity(SegmentedString& source, bool& notEnoughC
             if (seenChars.size() == 2)
                 source.push(seenChars[0]);
             else if (seenChars.size() == 3) {
-                source.push(seenChars[1]);
                 source.push(seenChars[0]);
+                source.push(seenChars[1]);
             } else
                 source.prepend(SegmentedString(String(seenChars.data(), seenChars.size() - 1)));
             return 0;
