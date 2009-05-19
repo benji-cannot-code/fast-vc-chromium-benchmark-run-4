3e24f7e9fe1eca8f137830354251bc54d0c18ffd
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-19 22:56:52 +0000

    Add a skeleton browser_frame implementation for GTK, make browser_view (which it needs) compile on GTK. Review URL: http://codereview.chromium.org/113589
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16426 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_view.cc
+++ chrome/browser/views/frame/browser_view.cc
@@ -484,6 +484,7 @@ SkBitmap BrowserView::GetOTRAvatarIcon() {
   return otr_avatar_;
 }
 
+#if defined(OS_WIN)
 void BrowserView::PrepareToRunSystemMenu(HMENU menu) {
   for (int i = 0; i < arraysize(kMenuLayout); ++i) {
     int command = kMenuLayout[i].command;
@@ -507,6 +508,7 @@ void BrowserView::PrepareToRunSystemMenu(HMENU menu) {
     }
   }
 }
+#endif
 
 // static
 void BrowserView::RegisterBrowserViewPrefs(PrefService* prefs) {
@@ -572,8 +574,8 @@ bool BrowserView::IsActive() const {
   return frame_->GetWindow()->IsActive();
 }
 
-#if defined(OS_WIN)
 void BrowserView::FlashFrame() {
+#if defined(OS_WIN)
   FLASHWINFO fwi;
   fwi.cbSize = sizeof(fwi);
   fwi.hwnd = frame_->GetWindow()->GetNativeWindow();
@@ -581,8 +583,10 @@ void BrowserView::FlashFrame() {
   fwi.uCount = 4;
   fwi.dwTimeout = 0;
   FlashWindowEx(&fwi);
-}
+#else
+  NOTIMPLEMENTED();
 #endif
+}
 
 gfx::NativeWindow BrowserView::GetNativeHandle() {
   return GetWidget()->GetNativeView();
@@ -642,8 +646,8 @@ bool BrowserView::IsMaximized() const {
   return frame_->GetWindow()->IsMaximized();
 }
 
-#if defined(OS_WIN)
 void BrowserView::SetFullscreen(bool fullscreen) {
+#if defined(OS_WIN)
   if (IsFullscreen() == fullscreen)
     return;  // Nothing to do.
 
@@ -697,8 +701,10 @@ void BrowserView::SetFullscreen(bool fullscreen) {
   ignore_layout_ = false;
   Layout();
   frame_->GetWindow()->PopForceHidden();
-}
+#else
+  NOTIMPLEMENTED();
 #endif
+}
 
 bool BrowserView::IsFullscreen() const {
   return frame_->GetWindow()->IsFullscreen();
@@ -874,17 +880,19 @@ void BrowserView::ConfirmBrowserCloseWithPendingDownloads() {
                                     delegate)->Show();
 }
 
-#if defined(OS_WIN)
 void BrowserView::ShowHTMLDialog(HtmlDialogUIDelegate* delegate,
                                  void* parent_window) {
+#if defined(OS_WIN)
   HWND parent_hwnd = reinterpret_cast<HWND>(parent_window);
   parent_hwnd = parent_hwnd ? parent_hwnd : GetWidget()->GetNativeView();
   HtmlDialogView* html_view = new HtmlDialogView(browser_.get(), delegate);
   views::Window::CreateChromeWindow(parent_hwnd, gfx::Rect(), html_view);
   html_view->InitDialog();
   html_view->window()->Show();
-}
+#else
+  NOTIMPLEMENTED();
 #endif
+}
 
 void BrowserView::UserChangedTheme() {
   frame_->GetWindow()->GetNonClientView()->SetUseNativeFrame(false);
@@ -1136,8 +1144,8 @@ bool BrowserView::CanClose() const {
   return true;
 }
 
-#if defined(OS_WIN)
 int BrowserView::NonClientHitTest(const gfx::Point& point) {
+#if defined(OS_WIN)
   // Since the TabStrip only renders in some parts of the top of the window,
   // the un-obscured area is considered to be part of the non-client caption
   // area of the window. So we need to treat hit-tests in these regions as
@@ -1214,8 +1222,10 @@ int BrowserView::NonClientHitTest(const gfx::Point& point) {
 
   // If the point is somewhere else, delegate to the default implementation.
   return views::ClientView::NonClientHitTest(point);
-}
+#else
+  NOTIMPLEMENTED();
 #endif
+}
 
 gfx::Size BrowserView::GetMinimumSize() {
   // TODO: In theory the tabstrip width should probably be
