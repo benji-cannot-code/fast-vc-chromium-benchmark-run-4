79d447bba7b6c9750847ff6727e344615ac84f04
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-20 19:30:56 +0000

    Add generated files to svn:ignore list for views subdir.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16508 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/menu/chrome_menu.cc
+++ views/controls/menu/chrome_menu.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task.h"
 #include "base/timer.h"
 #include "base/win_util.h"
-#include "grit/generated_resources.h"
+#include "grit/app_strings.h"
 #include "skia/ext/skia_utils_win.h"
 #include "views/border.h"
 #include "views/drag_utils.h"
@@ -789,7 +789,7 @@ class EmptyMenuMenuItem : public MenuItemView {
 
   explicit EmptyMenuMenuItem(MenuItemView* parent) :
       MenuItemView(parent, 0, NORMAL) {
-    SetTitle(l10n_util::GetString(IDS_MENU_EMPTY_SUBMENU));
+    SetTitle(l10n_util::GetString(IDS_APP_MENU_EMPTY_SUBMENU));
     // Set this so that we're not identified as a normal menu item.
     SetID(kEmptyMenuItemViewID);
     SetEnabled(false);
