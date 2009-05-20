86c0f551aa3c3fe0404db32a32970f51f3a6971e
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-20 20:47:25 +0000

    Hook up more of extension uninstall.
    Also removed all external dependencies from ExtensionsService.
    It now only sends out notifications, which other services
    consume. This should allow us to unit test the
    ExtensionsService frontend, but I haven't added that yet.
    
    Review URL: http://codereview.chromium.org/113493
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16529 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_service.h
+++ chrome/browser/plugin_service.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/singleton.h"
 #include "base/waitable_event_watcher.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/common/notification_observer.h"
 #include "webkit/glue/webplugininfo.h"
 
 #if defined(OS_WIN)
@@ -37,7 +38,9 @@ class ResourceMessageFilter;
 
 // This can be called on the main thread and IO thread.  However it must
 // be created on the main thread.
-class PluginService : base::WaitableEventWatcher::Delegate {
+class PluginService
+    : public base::WaitableEventWatcher::Delegate,
+      public NotificationObserver {
  public:
   // Returns the PluginService singleton.
   static PluginService* GetInstance();
@@ -117,7 +120,11 @@ class PluginService : base::WaitableEventWatcher::Delegate {
   ~PluginService();
 
   // base::WaitableEventWatcher::Delegate implementation.
-  void OnWaitableEventSignaled(base::WaitableEvent* waitable_event);
+  virtual void OnWaitableEventSignaled(base::WaitableEvent* waitable_event);
+
+  // NotificationObserver implementation
+  virtual void Observe(NotificationType type, const NotificationSource& source,
+                       const NotificationDetails& details);
 
   // mapping between plugin path and PluginProcessHost
   typedef base::hash_map<FilePath, PluginProcessHost*> PluginMap;
