19599c40dd4a51b5f39996c58a9fa91c626bcc1f
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-20 18:03:48 +0000

    Fix the webkit.org build of chromium.
    http://build.chromium.org/buildbot/waterfall.fyi/waterfall?builder=Webkit%20%28webkit.org%29
    
    This is the same code that was there upstream before these methods
    became pure virtual.
    
    TBR=dglazkov
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16492 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/chrome_client_impl.h
+++ webkit/glue/chrome_client_impl.h
@@ -108,6 +108,7 @@ class ChromeClientImpl : public WebCore::ChromeClientChromium {
   virtual PlatformWidget platformWindow() const;
   virtual void contentsSizeChanged(WebCore::Frame*,
                                    const WebCore::IntSize&) const;
+  virtual void scrollRectIntoView(const WebCore::IntRect&, const WebCore::ScrollView*) const { }
 
   virtual void mouseDidMoveOverElement(const WebCore::HitTestResult& result,
                                        unsigned modifierFlags);
@@ -119,8 +120,11 @@ class ChromeClientImpl : public WebCore::ChromeClientChromium {
   virtual void exceededDatabaseQuota(WebCore::Frame*,
                                      const WebCore::String& databaseName);
 
+  virtual void requestGeolocationPermissionForFrame(WebCore::Frame*, WebCore::Geolocation*) { }
+
   virtual void runOpenPanel(WebCore::Frame*,
                             PassRefPtr<WebCore::FileChooser>);
+  virtual bool setCursor(WebCore::PlatformCursorHandle) { return false; }
   virtual void popupOpened(WebCore::PopupContainer* popup_container,
                            const WebCore::IntRect& bounds,
                            bool activatable,
