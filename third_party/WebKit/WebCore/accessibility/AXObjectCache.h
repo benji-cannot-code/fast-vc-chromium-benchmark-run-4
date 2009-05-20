5f0595499f5d0d2d40d4fb52a7623e6b0688efb8
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-20 16:59:40 +0000

    2009-05-20  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Gustavo Noronha.
    
            Implement AtkObject::focus-event and
            AtkObject::state-changed:focused signal emission.
    
            Adds a new handleFocusedUIElementChangedWithRenderers, only in the
            GTK port, called at the same point than
            handleFocusedUIElementChanged but with two parameters, the old and
            the newly focused RenderObjects. We need this, since the ATK
            signals require us to pass the objects involved in the focus
            change as arguments.
    
            * accessibility/AXObjectCache.h:
            * accessibility/gtk/AXObjectCacheAtk.cpp:
            (WebCore::AXObjectCache::handleFocusedUIElementChangedWithRenderers):
            * dom/Document.cpp:
            (WebCore::Document::setFocusedNode):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43920 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AXObjectCache.h
+++ third_party/WebKit/WebCore/accessibility/AXObjectCache.h
@@ -81,6 +81,9 @@ namespace WebCore {
         void handleActiveDescendantChanged(RenderObject*);
         void handleAriaRoleChanged(RenderObject*);
         void handleFocusedUIElementChanged();
+#if PLATFORM(GTK)
+        void handleFocusedUIElementChangedWithRenderers(RenderObject*, RenderObject*);
+#endif
         static void enableAccessibility() { gAccessibilityEnabled = true; }
         static void enableEnhancedUserInterfaceAccessibility() { gAccessibilityEnhancedUserInterfaceEnabled = true; }
         
@@ -111,6 +114,9 @@ namespace WebCore {
     inline void AXObjectCache::selectedChildrenChanged(RenderObject*) { }
     inline void AXObjectCache::postNotification(RenderObject*, const String&) { }
     inline void AXObjectCache::postNotificationToElement(RenderObject*, const String&) { }
+#if PLATFORM(GTK)
+    inline void AXObjectCache::handleFocusedUIElementChangedWithRenderers(RenderObject*, RenderObject*) { }
+#endif
 #endif
 
 }
