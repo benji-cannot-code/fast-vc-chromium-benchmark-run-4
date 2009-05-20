e62e14a3c1a08a79625ff5ac30955d23ade4da56
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-20 11:38:12 +0000

    2009-05-20  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Eric Seidel.
    
            Fix GTK debug build.
    
            The function dumpDisjunction, compiled with debug enabled, uses
            printf, which needs stdio.h to be included.
    
            * yarr/RegexInterpreter.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/yarr/RegexInterpreter.cpp
+++ third_party/WebKit/JavaScriptCore/yarr/RegexInterpreter.cpp
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RegexCompiler.h"
 #include "RegexPattern.h"
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 #if ENABLE(YARR)
 
 using namespace WTF;
