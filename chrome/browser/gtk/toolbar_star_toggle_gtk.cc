10f64b2abb220a8b711c9807f9d0d810ac92a585
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-20 01:04:10 +0000

    Temporarily disabling the implementation of the bookmark bubble. http://crbug.com/12259
    Review URL: http://codereview.chromium.org/113609
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16450 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/toolbar_star_toggle_gtk.cc
+++ chrome/browser/gtk/toolbar_star_toggle_gtk.cc
@@ -33,6 +33,9 @@ ToolbarStarToggleGtk::~ToolbarStarToggleGtk() {
 
 void ToolbarStarToggleGtk::ShowStarBubble(const GURL& url,
                                           bool newly_bookmarked) {
+// TODO(erg): Temporarily disabling the implementation of the bookmark bubble.
+// http://crbug.com/12259
+#if 0
   GtkWidget* widget = widget_.get();
   gint x, y;
   gdk_window_get_origin(widget->window, &x, &y);
@@ -45,6 +48,7 @@ void ToolbarStarToggleGtk::ShowStarBubble(const GURL& url,
                           NULL,
                           url,
                           newly_bookmarked);
+#endif
 }
 
 void ToolbarStarToggleGtk::SetStarred(bool starred) {
