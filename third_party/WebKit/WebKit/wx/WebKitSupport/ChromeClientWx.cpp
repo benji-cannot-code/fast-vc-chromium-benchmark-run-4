3181b2115e46b6f1bd4a47e520cbc1a0d14ccb25
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-20 16:04:05 +0000

    [architecture] Make ChromeClient a interface again
    https://bugs.webkit.org/show_bug.cgi?id=25834
    
    With recent additions to ChromeClient.h empty defaults were
    added. This is bad for porters as these changes go unnoticed
    and at runtime no notImplemented warning is logged and grepping
    for notImplemented will not show anything. Change this Client
    to be like the other Clients again and always have pure virtuals
    (but for stuff inside #ifdef PLATFORM(MAC)).
    
    Update the various WebKit/* implementations to compile again.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43918 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/wx/WebKitSupport/ChromeClientWx.cpp
+++ third_party/WebKit/WebKit/wx/WebKitSupport/ChromeClientWx.cpp
@@ -394,4 +394,16 @@ void ChromeClientWx::runOpenPanel(Frame*, PassRefPtr<FileChooser>)
     notImplemented();
 }
 
+bool ChromeClientWx::setCursor(PlatformCursorHandle)
+{
+    notImplemented();
+    return false;
+}
+
+void ChromeClientWx::requestGeolocationPermissionForFrame(Frame*, Geolocation*)
+{
+    // See the comment in WebCore/page/ChromeClient.h
+    notImplemented();
+}
+
 }
