5acfcfeb4449206788d5043fa99432ea58a4699a
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-20 04:14:21 +0000

    Remove TRACK_HWND_CREATION/TRACK_HWND_DESTRUCTION macro since the bug it was created for has long been closed.  This code causes crashes when the browser is closed with the task manager open (in debug builds at the very least). Review URL: http://codereview.chromium.org/115553
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16462 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_process_host.cc
+++ chrome/browser/plugin_process_host.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "app/win_util.h"
-#include "base/win_util.h"
 #include "chrome/browser/sandbox_policy.h"
 #include "sandbox/src/sandbox.h"
 #endif
--- views/controls/native_control.cc
+++ views/controls/native_control.cc
@@ -80,10 +80,7 @@ class NativeControlContainer : public CWindowImpl<NativeControlContainer,
  private:
 
   LRESULT OnCreate(LPCREATESTRUCT create_struct) {
-    TRACK_HWND_CREATION(m_hWnd);
-
     control_ = parent_->CreateNativeControl(m_hWnd);
-    TRACK_HWND_CREATION(control_);
 
     FocusManager::InstallFocusSubclass(control_, parent_);
 
@@ -126,7 +123,6 @@ class NativeControlContainer : public CWindowImpl<NativeControlContainer,
   void OnDestroy() {
     if (parent_)
       parent_->OnDestroy();
-    TRACK_HWND_DESTRUCTION(m_hWnd);
   }
 
   void OnContextMenu(HWND window, const WTL::CPoint& location) {
@@ -377,7 +373,6 @@ LRESULT CALLBACK NativeControl::NativeControlWndProc(HWND window, UINT message,
                             reinterpret_cast<WNDPROC>(original_handler));
     RemoveProp(window, kHandlerKey);
     RemoveProp(window, kNativeControlKey);
-    TRACK_HWND_DESTRUCTION(window);
   }
 
   return CallWindowProc(reinterpret_cast<WNDPROC>(original_handler), window,
