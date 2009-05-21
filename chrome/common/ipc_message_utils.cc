be626a88a928cb1604641a4b34b9ca958af1e349
amit@chromium.org <amit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-21 15:40:49 +0000

    Move declaration of kMaxRecursionDepth out of #ifdef EXCLUDE_SKIA_DEPENDENCIES block This will cause compile failures if EXCLUDE_SKIA_DEPENDENCIES is defined.
    TBR=mpcomplete
    
    Review URL: http://codereview.chromium.org/113705
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16603 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/ipc_message_utils.cc
+++ chrome/common/ipc_message_utils.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace IPC {
 
+const int kMaxRecursionDepth = 100;
+
 #ifndef EXCLUDE_SKIA_DEPENDENCIES
 
 namespace {
@@ -55,8 +57,6 @@ struct SkBitmap_Data {
     return true;
   }
 };
-
-const int kMaxRecursionDepth = 100;
 
 }  // namespace
 
