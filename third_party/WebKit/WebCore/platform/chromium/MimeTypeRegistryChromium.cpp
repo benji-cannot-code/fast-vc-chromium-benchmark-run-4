cedca15f77302add32c89c5b5bbf273762b5345f
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-21 21:19:26 +0000

    2009-05-21  David Levin  <levin@chromium.org>
            Reviewed by Darin Fisher.
    
            Bug 25946: Chromium needs MIMETypeRegistry::getMediaMIMETypeForExtension implementation.
            https://bugs.webkit.org/show_bug.cgi?id=25946
    
            A very simple implementation to keep it working.  A bug has been filed in the chromium db
            (http://code.google.com/p/chromium/issues/detail?id=12441) to investigate doing something more
            complete.
    
            * platform/chromium/MimeTypeRegistryChromium.cpp:
            (WebCore::MIMETypeRegistry::getMediaMIMETypeForExtension):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43981 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/MimeTypeRegistryChromium.cpp
+++ third_party/WebKit/WebCore/platform/chromium/MimeTypeRegistryChromium.cpp
@@ -128,6 +128,11 @@ bool MIMETypeRegistry::isJavaAppletMIMEType(const String& mimeType)
         || mimeType.startsWith("application/x-java-vm", false);
 }
 
+String MIMETypeRegistry::getMediaMIMETypeForExtension(const String&)
+{
+    return String();
+}
+
 static HashSet<String>& dummyHashSet()
 {
     ASSERT_NOT_REACHED();
