4a01bac564e681cef7e6e53cb043528d7207c35a
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-21 02:43:19 +0000

    [GTK] Implement Image::loadPlatformResource.
    Install the four resources we are using. For the icons
    where an icon name is specified by freedeskop.org try to
    map the WebCore name to that name and fallback to the
    internal icon. For everything else use the internal icon
    name. Remove the temporary link stub as this is not
    required any more.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43955 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/TemporaryLinkStubs.cpp
+++ third_party/WebKit/WebCore/platform/gtk/TemporaryLinkStubs.cpp
@@ -39,20 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 
-// This function loads resources from WebKit
-// This does not belong here and I'm not sure where
-// it should go
-// I don't know what the plans or design is
-// for none code resources
-Vector<char> loadResourceIntoArray(const char* resourceName)
-{
-    Vector<char> resource;
-    //if (strcmp(resourceName,"missingImage") == 0) {
-    //}
-    return resource;
-}
-
-
 /********************************************************/
 /* Completely empty stubs (mostly to allow DRT to run): */
 /********************************************************/
