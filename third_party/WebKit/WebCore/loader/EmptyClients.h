6c07fb0a135a4b5d481043b1ec52475f6923c979
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-21 21:02:28 +0000

    2009-05-20  Siddhartha Chattopadhyaya  <sidchat@google.com>
            Reviewed by Justin Garcia.
            
            Add automatic spell correction support in WebKit
    
            https://bugs.webkit.org/show_bug.cgi?id=24953
    
            * editing/Editor.cpp:
            (WebCore::findFirstMisspellingInRange):
            (WebCore::Editor::advanceToNextMisspelling):
            (WebCore::Editor::markMisspellingsAfterTypingToPosition):
            (WebCore::markAllMisspellingsInRange):
            (WebCore::markMisspellingsOrBadGrammar):
            (WebCore::Editor::markMisspellings):
            (WebCore::Editor::markBadGrammar):
            (WebCore::Editor::markMisspellingsAndBadGrammar):
            * editing/Editor.h:
            * loader/EmptyClients.h:
            (WebCore::EmptyEditorClient::getAutoCorrectSuggestionForMisspelledWord):
            * page/EditorClient.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43980 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/EmptyClients.h
+++ third_party/WebKit/WebCore/loader/EmptyClients.h
@@ -376,6 +376,7 @@ public:
     virtual void ignoreWordInSpellDocument(const String&) { }
     virtual void learnWord(const String&) { }
     virtual void checkSpellingOfString(const UChar*, int, int*, int*) { }
+    virtual String getAutoCorrectSuggestionForMisspelledWord(const String&) { return String(); }
     virtual void checkGrammarOfString(const UChar*, int, Vector<GrammarDetail>&, int*, int*) { }
 #if PLATFORM(MAC) && !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD)
     virtual void checkTextOfParagraph(const UChar*, int, uint64_t, Vector<TextCheckingResult>&) { };
@@ -454,3 +455,4 @@ public:
 }
 
 #endif // EmptyClients_h
+
