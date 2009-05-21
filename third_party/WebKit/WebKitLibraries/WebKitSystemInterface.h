9f18e3ab1e9adb33264e41a6a5a15937ec02b491
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-21 09:30:05 +0000

    Rubber-stamped by Mark Rowe.
            - correct a copyright header accidently reverted in r43964
    
            * WebKitSystemInterface.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@43965 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
+++ third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*      
     WebKitSystemInterface.h
-    Copyright (C) 2005, 2006, 2007, 2008 Apple Inc. All rights reserved.
+    Copyright (C) 2005, 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
 
     Public header file.
 */
