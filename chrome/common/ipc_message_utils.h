4dbc635e80845ac62b86c779c56fb8ccfd2e2796
mpcomplete@google.com <mpcomplete@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-21 01:01:59 +0000

    Add IPC serialization for DictionaryValue and ListValue, with unit tests. Review URL: http://codereview.chromium.org/115491
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16562 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/ipc_message_utils.h
+++ chrome/common/ipc_message_utils.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Forward declarations.
 class GURL;
 class SkBitmap;
+class DictionaryValue;
+class ListValue;
 
 namespace gfx {
 class Point;
@@ -356,6 +358,22 @@ struct ParamTraits<SkBitmap> {
   // r->SetConfig() and r->SetPixels() are called.
   static bool Read(const Message* m, void** iter, param_type* r);
 
+  static void Log(const param_type& p, std::wstring* l);
+};
+
+template <>
+struct ParamTraits<DictionaryValue> {
+  typedef DictionaryValue param_type;
+  static void Write(Message* m, const param_type& p);
+  static bool Read(const Message* m, void** iter, param_type* r);
+  static void Log(const param_type& p, std::wstring* l);
+};
+
+template <>
+struct ParamTraits<ListValue> {
+  typedef ListValue param_type;
+  static void Write(Message* m, const param_type& p);
+  static bool Read(const Message* m, void** iter, param_type* r);
   static void Log(const param_type& p, std::wstring* l);
 };
 
