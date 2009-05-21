0a75b5d8be2490f5ac00883858f40d40270b7aba
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-21 00:34:44 +0000

    Make the NotificationRegistrar safe for use in Singletons, which may outlive the NotificationService instances, by checking whether the service exists before calling RemoveObserver() on it.
    Also add comments in NotificationService telling people to use NotificationRegistrar.
    
    BUG=2381
    Review URL: http://codereview.chromium.org/115601
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16554 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/notification_registrar.h
+++ chrome/common/notification_registrar.h
@@ -36,6 +36,9 @@ class NotificationRegistrar {
   // Unregisters all notifications.
   void RemoveAll();
 
+  // Returns true if no notifications are registered.
+  bool IsEmpty() const;
+
  private:
   struct Record;
 
