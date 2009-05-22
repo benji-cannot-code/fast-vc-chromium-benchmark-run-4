f038b34c9d359052b10bb60c343c173e02f70136
robertshield@google.com <robertshield@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-22 18:08:17 +0000

    Re-do of r16741 (http://codereview.chromium.org/113482).
    Moved the new automation message in automation_messages_internal.h to the END of the enum as per the large scary warning that I skillfully missed last time around.
    
    TBR=amit
    
    Review URL: http://codereview.chromium.org/115711
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16754 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/automation_constants.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/automation_constants.h"
+
+namespace automation {
+// JSON value labels for proxy settings that are passed in via
+// AutomationMsg_SetProxyConfig.
+const wchar_t kJSONProxyAutoconfig[] = L"proxy.autoconfig";
+const wchar_t kJSONProxyNoProxy[] = L"proxy.no_proxy";
+const wchar_t kJSONProxyPacUrl[] = L"proxy.pac_url";
+const wchar_t kJSONProxyBypassList[] = L"proxy.bypass_list";
+const wchar_t kJSONProxyServer[] = L"proxy.server";
+}
