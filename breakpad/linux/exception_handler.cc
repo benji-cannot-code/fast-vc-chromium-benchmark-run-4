eb2044e0b3c4e895aec08293915bde72743819f4
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-22 19:33:15 +0000

    Fix a missing include in exception_handler.cc, fix compilation on Jaunty. Review URL: http://codereview.chromium.org/113774
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16773 0039d316-1c4b-4281-b951-d872f2087c98

--- breakpad/linux/exception_handler.cc
+++ breakpad/linux/exception_handler.cc
@@ -66,17 +66,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "breakpad/linux/exception_handler.h"
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/mman.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <linux/limits.h>
+#include <sched.h>
 #include <signal.h>
+#include <sys/mman.h>
 #include <sys/signal.h>
+#include <sys/syscall.h>
 #include <sys/ucontext.h>
 #include <sys/user.h>
-#include <sys/syscall.h>
 #include <sys/wait.h>
-#include <sched.h>
+#include <unistd.h>
 
 #include "breakpad/linux/linux_syscall_support.h"
 #include "breakpad/linux/memory.h"
