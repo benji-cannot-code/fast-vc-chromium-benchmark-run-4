a40340eac5a2c09fae09162c571d0e34573207c6
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-22 12:05:43 +0000

    DevTools: remove node highlight on dev tools window close. BUG=11766 Review URL: http://codereview.chromium.org/115655
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16737 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webdevtoolsagent_impl.cc
+++ webkit/glue/webdevtoolsagent_impl.cc
@@ -101,6 +101,7 @@ void WebDevToolsAgentImpl::Attach() {
 }
 
 void WebDevToolsAgentImpl::Detach() {
+  HideDOMNodeHighlight();
   debugger_agent_impl_.set(NULL);
   dom_agent_impl_.set(NULL);
   net_agent_impl_->Detach();
@@ -186,7 +187,9 @@ void WebDevToolsAgentImpl::HighlightDOMNode(int node_id) {
 
 void WebDevToolsAgentImpl::HideDOMNodeHighlight() {
   Page* page = web_view_impl_->page();
-  page->inspectorController()->hideHighlight();
+  if (page) {
+    page->inspectorController()->hideHighlight();
+  }
 }
 
 void WebDevToolsAgentImpl::EvaluateJavaScript(int call_id, const String& js) {
