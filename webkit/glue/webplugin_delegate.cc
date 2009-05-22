4696567966c739a0632db5b7f5b119ed04cfab09
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-22 03:29:57 +0000

    linux: add quirk for windowless flash drawing glitches
    Review URL: http://codereview.chromium.org/115685
    Patch from Antoine Labour <piman@google.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16718 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_delegate.cc
+++ webkit/glue/webplugin_delegate.cc
@@ -28,10 +28,8 @@ const gfx::Rect& WebPluginDelegate::GetClipRect() const {
   return GetRect();
 }
 
-#if defined(OS_WIN)
 // Returns a combinaison of PluginQuirks.
 int WebPluginDelegate::GetQuirks() const {
   NOTREACHED();
   return 0;
 }
-#endif
