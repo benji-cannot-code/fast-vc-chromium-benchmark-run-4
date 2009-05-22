48bb6f29558dc945c9c12b77e33fdfa170e440af
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-22 20:28:12 +0000

    Fix bug 12468 where F3 (FindNext) in a brand new tab was not using prepopulated search (what you have searched for in other tabs).
    TEST=Covered by in-process-browser test now. To test manually: open a tab, search for something in FindInPage, Press Ctrl+T and then F3. It should search for the same thing in the newly opened tab. 
    BUG=12468
    
    Review URL: http://codereview.chromium.org/115714
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16786 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -2611,7 +2611,8 @@ void Browser::FindInPage(bool find_next, bool forward_direction) {
   ShowFindBar();
   if (find_next) {
     GetSelectedTabContents()->StartFinding(string16(),
-                                           forward_direction);
+                                           forward_direction,
+                                           false);  // Not case sensitive.
   }
 }
 
