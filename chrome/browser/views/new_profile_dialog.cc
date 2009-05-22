a67065e47751a78d484fb774c66de0d613ea5fb2
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-22 21:29:22 +0000

    Move more dialog showing routines to browser_dialogs.h to reduce dependencies and make this file easier to port. Review URL: http://codereview.chromium.org/113778
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16798 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/new_profile_dialog.cc
+++ chrome/browser/views/new_profile_dialog.cc
@@ -20,6 +20,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/view.h"
 #include "views/window/window.h"
 
+namespace browser {
+
+// Declared in browser_dialogs.h so others don't have to depend on our header.
+void ShowNewProfileDialog() {
+  NewProfileDialog::RunDialog();
+}
+
+}  // namespace browser
+
 // static
 void NewProfileDialog::RunDialog() {
   NewProfileDialog* dlg = new NewProfileDialog();
