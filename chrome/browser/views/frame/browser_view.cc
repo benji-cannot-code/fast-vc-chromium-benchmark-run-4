5f6671fdaad4381f6c986c533405cef5b8eb4ee0
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-22 21:36:50 +0000

    Fix the Windows build by adding some missing headers.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16799 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_view.cc
+++ chrome/browser/views/frame/browser_view.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/find_bar.h"
 #include "chrome/browser/find_bar_controller.h"
 #include "chrome/browser/view_ids.h"
-#include "chrome/browser/views/about_chrome_view.h"
 #include "chrome/browser/views/bookmark_bar_view.h"
 #include "chrome/browser/views/browser_bubble.h"
 #include "chrome/browser/views/browser_dialogs.h"
@@ -57,7 +56,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/fill_layout.h"
 #include "views/view.h"
 #include "views/widget/root_view.h"
+#include "views/window/dialog_delegate.h"
 #include "views/window/non_client_view.h"
+#include "views/window/window.h"
 
 using base::TimeDelta;
 
