40e9d96ce42dcb5f0a3ac395da3f42c6e45726cf
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-22 16:08:58 +0000

    Fix the call to editingCancelled to be this._editingCancelled, since         editingCancelled doesn't exist and throws an exception.
            Reviewed by Timothy Hatcher.
    
            * inspector/front-end/ElementsTreeOutline.js:
            (WebInspector.ElementsTreeElement.prototype._attributeEditingCommitted):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ElementsTreeOutline.js
+++ third_party/WebKit/WebCore/inspector/front-end/ElementsTreeOutline.js
@@ -572,7 +572,7 @@ WebInspector.ElementsTreeElement.prototype = {
         parseContainerElement.innerHTML = "<span " + newText + "></span>";
         var parseElement = parseContainerElement.firstChild;
         if (!parseElement || !parseElement.hasAttributes()) {
-            editingCancelled(element, context);
+            this._editingCancelled(element, context);
             return;
         }
 
