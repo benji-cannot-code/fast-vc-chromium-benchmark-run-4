45da157fd4d265eba794eb1b0fbb08f25637b335
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-22 00:28:22 +0000

    <rdar://problem/6910264> REGRESSION: Cached DOM global object property access fails in browser (25921) <https://bugs.webkit.org/show_bug.cgi?id=25921>
    Reviewed by Maciej Stachowiak.
    
    When caching properties on the global object we need to ensure that we're
    not attempting to cache through a shell object.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44016 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/global-function-resolve.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test to make sure cached lookups on the global object are performed correctly.");
+
+var funcNames = [];
+var cachedFunctions = [];
+
+for (var i in this) {
+    funcNames.push(i);
+    cachedFunctions.push(new Function("return " + i));
+}
+
+for (var i = 0; i < funcNames.length; i++) {
+    shouldBe("cachedFunctions["+i+"]()", funcNames[i]);
+    shouldBe("cachedFunctions["+i+"]()", funcNames[i]);
+}
+
+successfullyParsed = true;
