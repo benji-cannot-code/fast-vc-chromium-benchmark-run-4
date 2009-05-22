147e230bdf32731eaff7eb3d6af0472010ce3a23
huanr@chromium.org <huanr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-22 18:40:21 +0000

    Fix some incorrect timeout settings in ui test.
    Review URL: http://codereview.chromium.org/113673
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16766 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/ui_test.cc
+++ chrome/test/ui/ui_test.cc
@@ -89,7 +89,7 @@ bool UITest::DieFileDie(const FilePath& file, bool recurse) {
   for (int i = 0; i < 10; ++i) {
     if (file_util::Delete(file, recurse))
       return true;
-    PlatformThread::Sleep(action_max_timeout_ms() / 10);
+    PlatformThread::Sleep(sleep_timeout_ms() / 10);
   }
   return false;
 }
@@ -215,7 +215,7 @@ void UITest::LaunchBrowserAndServer() {
   if (wait_for_initial_loads_)
     ASSERT_TRUE(server_->WaitForInitialLoads());
   else
-    PlatformThread::Sleep(2000);
+    PlatformThread::Sleep(sleep_timeout_ms());
 
   automation()->SetFilteredInet(true);
 }
@@ -494,10 +494,10 @@ void UITest::NavigateToURL(const GURL& url) {
 }
 
 bool UITest::WaitForDownloadShelfVisible(TabProxy* tab) {
-  const int kCycles = 20;
+  const int kCycles = 10;
   for (int i = 0; i < kCycles; i++) {
     // Give it a chance to catch up.
-    PlatformThread::Sleep(action_max_timeout_ms() / kCycles);
+    PlatformThread::Sleep(sleep_timeout_ms() / kCycles);
 
     bool visible = false;
     if (!tab->IsShelfVisible(&visible))
@@ -514,7 +514,7 @@ bool UITest::WaitForDownloadShelfVisible(TabProxy* tab) {
 #if defined(OS_WIN)
 bool UITest::WaitForFindWindowVisibilityChange(BrowserProxy* browser,
                                                bool wait_for_open) {
-  const int kCycles = 20;
+  const int kCycles = 10;
   for (int i = 0; i < kCycles; i++) {
     bool visible = false;
     if (!browser->IsFindWindowFullyVisible(&visible))
@@ -530,7 +530,7 @@ bool UITest::WaitForFindWindowVisibilityChange(BrowserProxy* browser,
 
 bool UITest::WaitForBookmarkBarVisibilityChange(BrowserProxy* browser,
                                                 bool wait_for_open) {
-  const int kCycles = 20;
+  const int kCycles = 10;
   for (int i = 0; i < kCycles; i++) {
     bool visible = false;
     bool animating = true;
@@ -860,7 +860,7 @@ bool UITest::EvictFileFromSystemCacheWrapper(const FilePath& path) {
   for (int i = 0; i < 10; i++) {
     if (file_util::EvictFileFromSystemCache(path))
       return true;
-    PlatformThread::Sleep(1000);
+    PlatformThread::Sleep(sleep_timeout_ms() / 10);
   }
   return false;
 }
