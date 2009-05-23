ae8e62cc482fcc2bd94a49a53041566788600789
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-23 22:08:53 +0000

    Reformat WebCore::requiresContextForWordBoundary()
            * platform/text/TextBoundaries.h:
            (WebCore::requiresContextForWordBoundary):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44102 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/TextBoundaries.h
+++ third_party/WebKit/WebCore/platform/text/TextBoundaries.h
@@ -31,8 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    inline bool requiresContextForWordBoundary(UChar32 ch) { return WTF::Unicode::hasLineBreakingPropertyComplexContext(
-ch); }
+    inline bool requiresContextForWordBoundary(UChar32 ch)
+    {
+        return WTF::Unicode::hasLineBreakingPropertyComplexContext(ch);
+    }
 
     void findWordBoundary(const UChar*, int len, int position, int* start, int* end);
     int findNextWordFromIndex(const UChar*, int len, int position, bool forward);
