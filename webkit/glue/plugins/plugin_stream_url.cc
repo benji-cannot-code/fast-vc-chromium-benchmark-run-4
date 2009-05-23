06b65ee9fa8bca5110a2850b368d424b79105c11
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-23 01:30:33 +0000

    Make sure that seekable streams are removed from the list of resource clients maintained by WebPluginProxy.
    The PluginStreamUrl object ensures that it notifies the webplugin object that it is being deleted in its destructor. 
    
    The other change is to remove the call to CancelRequest in WebPluginDelegateImpl::CreateResourceClient for
    seekable streams, as this call does not do anything for manual streams created with a resource id of -1.
    In any case for byte range requests, receiving a new response does not mean that earlier requests should
    be cancelled.
    
    This fixes bug http://code.google.com/p/chromium/issues/detail?id=12445
    
    BUG=12445
    
    
    Review URL: http://codereview.chromium.org/113772
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16828 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/plugin_stream_url.cc
+++ webkit/glue/plugins/plugin_stream_url.cc
@@ -27,6 +27,9 @@ PluginStreamUrl::PluginStreamUrl(
 }
 
 PluginStreamUrl::~PluginStreamUrl() {
+  if (instance() && instance()->webplugin()) {
+    instance()->webplugin()->ResourceClientDeleted(AsResourceClient());
+  }
 }
 
 bool PluginStreamUrl::Close(NPReason reason) {
--- webkit/glue/plugins/webplugin_delegate_impl.cc
+++ webkit/glue/plugins/webplugin_delegate_impl.cc
@@ -1206,8 +1206,6 @@ WebPluginResourceClient* WebPluginDelegateImpl::CreateResourceClient(
     NPAPI::PluginStream* plugin_stream =
         reinterpret_cast<NPAPI::PluginStream*>(existing_stream);
 
-    plugin_stream->CancelRequest();
-
     return plugin_stream->AsResourceClient();
   }
 
