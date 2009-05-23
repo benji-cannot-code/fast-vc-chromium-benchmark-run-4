797838190ceb13aef0b28acc26e3061cb0ea2d97
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-23 09:06:07 +0000

    Add tests to cover exceptions thrown from non-object results of get_by_id
    Reviewed by Maciej Stachowiak
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44087 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/exception-for-nonobject.js
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test for correct handling of exceptions from instanceof and 'new' expressions");
+
+shouldThrow("new {}.undefined");
+shouldThrow("1 instanceof {}.undefined");
+
+successfullyParsed = true;
