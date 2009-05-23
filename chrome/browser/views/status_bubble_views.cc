4e772b2c246dab2428cfc119f50225a14b01aeed
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-23 01:01:57 +0000

    Forces window to show and hide with native system calls at the beginning and end of animation.  This allows the system bubble to hide and display in systems that don't respect opacity, such as the Parallels desktop.
    BUG= http://crbug.com/622
    TEST=Start Chrome on a system running in a Parallels window.  Hover over
    a status bubble, and move mouse away.  Status bubble should disappear.
    
    (NOTE: This was reviewed at http://codereview.chromium.org/113747.  It is in a
    different review here because that code was uploaded from another machine,
    before I had committer access.)
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=16802
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16824 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/status_bubble_views.cc
+++ chrome/browser/views/status_bubble_views.cc
@@ -186,6 +186,7 @@ void StatusBubbleViews::StatusView::Show() {
   Stop();
   CancelTimer();
   SetOpacity(1.0);
+  popup_->Show();
   stage_ = BUBBLE_SHOWN;
   PaintNow();
 }
@@ -195,6 +196,7 @@ void StatusBubbleViews::StatusView::Hide() {
   CancelTimer();
   SetOpacity(0.0);
   text_.clear();
+  popup_->Hide();
   stage_ = BUBBLE_HIDDEN;
 }
 
@@ -267,6 +269,7 @@ void StatusBubbleViews::StatusView::StartHiding() {
 
 void StatusBubbleViews::StatusView::StartShowing() {
   if (stage_ == BUBBLE_HIDDEN) {
+    popup_->Show();
     stage_ = BUBBLE_SHOWING_TIMER;
     StartTimer(kShowDelay);
   } else if (stage_ == BUBBLE_HIDING_TIMER) {
@@ -310,6 +313,7 @@ void StatusBubbleViews::StatusView::AnimationEnded(
 
   if (stage_ == BUBBLE_HIDING_FADE) {
     stage_ = BUBBLE_HIDDEN;
+    popup_->Hide();
   } else if (stage_ == BUBBLE_SHOWING_FADE) {
     stage_ = BUBBLE_SHOWN;
   }
