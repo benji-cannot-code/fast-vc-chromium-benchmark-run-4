c0bb0140dd1dbfbbc979360636823871d3b1dc03
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-25 07:32:31 +0000

    Update webkit to r44123
    This required dealing with a few changes in WebKit:
    
    1- ImageBuffer::create() now returns a PassOwnPtr instead of an auto_ptr.
    2- ChromeClient::createHTMLParserQuirks() now returns a PassOwnPtr instead of a raw pointer.
    3- HTMLFormControlElement::onChange was renamed dispatchFormControlChangeEvent.
    4- dom/SelectElement.{h,cpp} were added to the tree.
    
    TEST=none
    R=ukai
    BUG=none
    
    Review URL: http://codereview.chromium.org/115751
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16849 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/password_autocomplete_listener.cc
+++ webkit/glue/password_autocomplete_listener.cc
@@ -39,7 +39,7 @@ void HTMLInputDelegate::OnFinishedAutocompleting() {
   // having a yellow background.
   element_->setAutofilled(true);
   // Notify any changeEvent listeners.
-  element_->onChange();
+  element_->dispatchFormControlChangeEvent();
 }
 
 PasswordAutocompleteListener::PasswordAutocompleteListener(
