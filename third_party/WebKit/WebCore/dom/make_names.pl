8bb0d34120f654ed94482ff3ebb15546d44571e7
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-25 12:39:15 +0000

    2009-05-25  Simon Hausmann  <simon.hausmann@nokia.com>
            Reviewed by Tor Arne Vestbø.
    
            https://bugs.webkit.org/show_bug.cgi?id=25612
    
            Support conditionals in the tag factories, by placing
            feature #ifdefs around the individual factory functions
            and the registration.
    
            Made the Audio and Video elements conditional in the tag factories
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44128 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/make_names.pl
+++ third_party/WebKit/WebCore/dom/make_names.pl
@@ -96,7 +96,8 @@ sub initializeTagPropertyHash
             # By default, the JSInterfaceName is the same as the interfaceName.
             'JSInterfaceName' => defaultInterfaceName($_[0]),
             'mapToTagName' => '',
-            'wrapperOnlyIfMediaIsAvailable' => 0);
+            'wrapperOnlyIfMediaIsAvailable' => 0,
+            'conditional' => 0);
 }
 
 sub initializeAttrPropertyHash
@@ -313,8 +314,18 @@ sub printConstructors
 
         $uniqueTags{$interfaceName} = '1';
 
+        my $conditional = $tags{$tagName}{"conditional"};
+        if ($conditional) {
+            my $conditionalString = "ENABLE(" . join(") && ENABLE(", split(/&/, $conditional)) . ")";
+            print F "#if ${conditionalString}\n\n";
+        }
+
         printConstructorSignature($F, $tagName, $tagConstructorMap{$tagName}, "tagName");
         printConstructorInterior($F, $tagName, $interfaceName, "tagName");
+
+        if ($conditional) {
+            print F "#endif\n\n";
+        }
     }
 
     # Mapped tag name uses a special wrapper to keep their prefix and namespaceURI while using the mapped localname.
@@ -335,10 +346,21 @@ sub printFunctionInits
     my %tagConstructorMap = %$tagConstructorMap;
 
     for my $tagName (sort keys %tagConstructorMap) {
+
+        my $conditional = $tags{$tagName}{"conditional"};
+        if ($conditional) {
+            my $conditionalString = "ENABLE(" . join(") && ENABLE(", split(/&/, $conditional)) . ")";
+            print F "#if ${conditionalString}\n";
+        }
+
         if ($tags{$tagName}{'mapToTagName'}) {
             print F "    addTag(${tagName}Tag, $tags{$tagName}{'mapToTagName'}To${tagName}Constructor);\n";
         } else {
             print F "    addTag(${tagName}Tag, $tagConstructorMap{$tagName}Constructor);\n";
+        }
+
+        if ($conditional) {
+            print F "#endif\n\n";
         }
     }
 }
@@ -847,6 +869,12 @@ sub printWrapperFunctions
         next if defined($tagsSeen{$JSInterfaceName}) || usesDefaultJSWrapper($tagName);
         $tagsSeen{$JSInterfaceName} = 1;
 
+        my $conditional = $tags{$tagName}{"conditional"};
+        if ($conditional) {
+            my $conditionalString = "ENABLE(" . join(") && ENABLE(", split(/&/, $conditional)) . ")";
+            print F "#if ${conditionalString}\n\n";
+        }
+
         # Hack for the media tags
         if ($tags{$tagName}{"wrapperOnlyIfMediaIsAvailable"}) {
             print F <<END
@@ -868,6 +896,9 @@ static JSNode* create${JSInterfaceName}Wrapper(ExecState* exec, PassRefPtr<$para
 
 END
 ;
+        }
+        if ($conditional) {
+            print F "#endif\n\n";
         }
     }
 }
@@ -921,8 +952,18 @@ END
         # Do not add the name to the map if it does not have a JS wrapper constructor or uses the default wrapper.
         next if usesDefaultJSWrapper($tag, \%tags);
 
+        my $conditional = $tags{$tag}{"conditional"};
+        if ($conditional) {
+            my $conditionalString = "ENABLE(" . join(") && ENABLE(", split(/&/, $conditional)) . ")";
+            print F "#if ${conditionalString}\n";
+        }
+
         my $ucTag = $tags{$tag}{"JSInterfaceName"};
         print F "       map.set(${tag}Tag.localName().impl(), create${ucTag}Wrapper);\n";
+
+        if ($conditional) {
+            print F "#endif\n";
+        }
     }
 
     print F <<END
