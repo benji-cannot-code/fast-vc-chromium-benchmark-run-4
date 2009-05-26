91cfb475e4ccc9b036e422ae424f865d786f3b3b
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-26 20:17:50 +0000

    First phase of Mac Keychain integration. For the overall plan, see the design doc linked from the bug. This implements the ability to read existing Keychain passwords, but no storage/updating yet, and no adding of our own metadata as passwords are used; all that will be done in follow-up patches.
    This also includes a Keychain wrapper for the functionality necessary so far, and a mock Keychain sufficient to unit-test essentially all of the code.
    
    This patch deliberately excludes the step of instantiating a PasswordStoreMac and hooking it up to the profile, because a bug in autocomplete itself prevents passwords we load from actually being used, and we don't want to trigger Keychain UI for passwords that will be ignored. So this won't be used in builds yet, but it will be unit tested.
    
    BUG=11745
    TEST=none (no user-visible effect yet)
    Review URL: http://codereview.chromium.org/115658
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16897 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/password_manager/password_store_mac.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_STORE_MAC_H_
+#define CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_STORE_MAC_H_
+
+#include "base/scoped_ptr.h"
+#include "chrome/browser/password_manager/password_store.h"
+
+class MacKeychain;
+
+class PasswordStoreMac : public PasswordStore {
+ public:
+  // Takes ownership of |keychain|, which must not be NULL.
+  explicit PasswordStoreMac(MacKeychain* keychain);
+  virtual ~PasswordStoreMac() {}
+
+ private:
+  void AddLoginImpl(const PasswordForm& form);
+  void UpdateLoginImpl(const PasswordForm& form);
+  void RemoveLoginImpl(const PasswordForm& form);
+  void GetLoginsImpl(GetLoginsRequest* request);
+
+  scoped_ptr<MacKeychain> keychain_;
+
+  DISALLOW_COPY_AND_ASSIGN(PasswordStoreMac);
+};
+
+#endif  // CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_STORE_MAC_H_
