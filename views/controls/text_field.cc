09cf0b140590a5f633a6c744ec9e3b497eded5a5
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-26 22:16:19 +0000

    A quick fix for Issue 11863 (Take 2). This change is exactly the same change as <http://codereview.chromium.org/115353>. I would like to re-send this change because the PC that I created this original change has been dead and I have not been able to commit.
    This issue is caused by a stupid mistake of mine that I forgot adding a WM_IME_ENDCOMPOSITION handler to update the find results when a composition is finished (or canceled). This change adds a handler for WM_IME_ENDCOMPOSITION messages to update the find results.
    
    BUG=11863 "Korean find-in-page: Backspace all the way, Chrome will highlight all the characters that contain the first consonant"
    TEST=Open a Korean page, type Control+F keys, Switch the input language to Korean, type a right-alt key, type a 'w' key, type a 'k' key, type a 'a' key, type a backspace key, and verify there are not any highlited characters. 
    Review URL: http://codereview.chromium.org/113847
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16934 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/text_field.cc
+++ views/controls/text_field.cc
@@ -76,6 +76,7 @@ class TextField::Edit
     MESSAGE_HANDLER_EX(WM_IME_CHAR, OnImeChar)
     MESSAGE_HANDLER_EX(WM_IME_STARTCOMPOSITION, OnImeStartComposition)
     MESSAGE_HANDLER_EX(WM_IME_COMPOSITION, OnImeComposition)
+    MESSAGE_HANDLER_EX(WM_IME_ENDCOMPOSITION, OnImeEndComposition)
     MSG_WM_KEYDOWN(OnKeyDown)
     MSG_WM_LBUTTONDBLCLK(OnLButtonDblClk)
     MSG_WM_LBUTTONDOWN(OnLButtonDown)
@@ -124,6 +125,7 @@ class TextField::Edit
   LRESULT OnImeChar(UINT message, WPARAM wparam, LPARAM lparam);
   LRESULT OnImeStartComposition(UINT message, WPARAM wparam, LPARAM lparam);
   LRESULT OnImeComposition(UINT message, WPARAM wparam, LPARAM lparam);
+  LRESULT OnImeEndComposition(UINT message, WPARAM wparam, LPARAM lparam);
   void OnKeyDown(TCHAR key, UINT repeat_count, UINT flags);
   void OnLButtonDblClk(UINT keys, const CPoint& point);
   void OnLButtonDown(UINT keys, const CPoint& point);
@@ -503,6 +505,20 @@ LRESULT TextField::Edit::OnImeComposition(UINT message,
 
   OnAfterPossibleChange();
   return result;
+}
+
+LRESULT TextField::Edit::OnImeEndComposition(UINT message,
+                                             WPARAM wparam,
+                                             LPARAM lparam) {
+  // Bug 11863: Korean IMEs send a WM_IME_ENDCOMPOSITION message without
+  // sending any WM_IME_COMPOSITION messages when a user deletes all
+  // composition characters, i.e. a composition string becomes empty. To handle
+  // this case, we need to update the find results when a composition is
+  // finished or canceled.
+  parent_->SyncText();
+  if (parent_->GetController())
+    parent_->GetController()->ContentsChanged(parent_, GetText());
+  return DefWindowProc(message, wparam, lparam);
 }
 
 void TextField::Edit::OnKeyDown(TCHAR key, UINT repeat_count, UINT flags) {
