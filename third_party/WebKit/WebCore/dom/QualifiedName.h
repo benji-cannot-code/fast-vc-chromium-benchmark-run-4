f09a8346cf8f45fd0335d46376aa64a4065a4d0c
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-26 06:20:53 +0000

    Reviewed by Darin Adler.
            <rdar://problem/6918671> REGRESSION (r41700): Crashes in debug DumpRenderTree at RefCountedBase::ref().
    
            * dom/QualifiedName.cpp: (WebCore::QualifiedName::QualifiedName):
            * dom/QualifiedName.h: (WebCore::QualifiedName::QualifiedNameImpl::QualifiedNameImpl):
            Adjust empty namespace before QNameComponentsTranslator black magic, not after.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44147 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/QualifiedName.h
+++ third_party/WebKit/WebCore/dom/QualifiedName.h
@@ -50,8 +50,9 @@ public:
         QualifiedNameImpl(const AtomicString& prefix, const AtomicString& localName, const AtomicString& namespaceURI)
             : m_prefix(prefix)
             , m_localName(localName)
-            , m_namespace(namespaceURI.isEmpty() ? nullAtom : namespaceURI)
+            , m_namespace(namespaceURI)
         {
+            ASSERT(!namespaceURI.isEmpty() || namespaceURI.isNull());
         }        
     };
 
