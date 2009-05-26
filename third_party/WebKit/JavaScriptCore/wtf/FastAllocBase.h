fabbccd9f0593031b7d8aaddac071c331f4ca054
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-26 23:06:42 +0000

    2009-05-26  Brent Fulgham  <bfulgham@webkit.org>
            Build fix to correct r44161.
    
            * wtf/FastAllocBase.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/FastAllocBase.h
+++ third_party/WebKit/JavaScriptCore/wtf/FastAllocBase.h
@@ -80,9 +80,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include "FastMalloc.h"
+#include "TypeTraits.h"
 #include <wtf/Assertions.h>
-#include <wtf/FastMalloc.h>
-#include <wtf/TypeTraits.h>
 
 namespace WTF {
 
