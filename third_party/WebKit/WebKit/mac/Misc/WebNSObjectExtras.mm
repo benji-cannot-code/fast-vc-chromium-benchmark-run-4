27f40e9367588a6df9af95b4523dcf2dcda57f2a
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-26 18:26:16 +0000

    2009-05-26  David Hyatt  <hyatt@apple.com>
            Back out the workaround for Mail crashing.  Darin is working on the real fix.
    
            * Misc/WebNSObjectExtras.mm:
            (-[WebMainThreadInvoker forwardInvocation:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44153 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Misc/WebNSObjectExtras.mm
+++ third_party/WebKit/WebKit/mac/Misc/WebNSObjectExtras.mm
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)forwardInvocation:(NSInvocation *)invocation
 {
     [invocation setTarget:target];
-    [invocation retainArguments];
     [invocation performSelectorOnMainThread:@selector(_webkit_invokeAndHandleException:) withObject:self waitUntilDone:YES];
     if (exception) {
         id exceptionToThrow = [exception autorelease];
