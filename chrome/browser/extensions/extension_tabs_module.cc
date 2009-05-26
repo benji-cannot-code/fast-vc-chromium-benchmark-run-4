c075cef187657c205a1420493006cd40abe3b4bf
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-26 22:55:28 +0000

    Add a single monitor implementation of WindowSizer for Linux. (try 2)
    BUG=10005
    Review URL: http://codereview.chromium.org/115723
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16940 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_module.cc
+++ chrome/browser/extensions/extension_tabs_module.cc
@@ -13,15 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extensions_service.h"
 #include "chrome/browser/renderer_host/render_view_host_delegate.h"
 #include "chrome/browser/tab_contents/navigation_entry.h"
+#include "chrome/browser/window_sizer.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_error_utils.h"
 
-// TODO(port): Port these files.
-#if defined(OS_WIN) || defined(OS_MACOSX)
-#include "chrome/browser/window_sizer.h"
-#else
-#include "chrome/common/temp_scaffolding_stubs.h"
-#endif
 
 namespace keys = extension_tabs_module_constants;
 
