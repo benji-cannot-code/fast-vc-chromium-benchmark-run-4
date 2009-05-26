3d1cb7c21a46e93f4c8c8f01b0d887bbdd3223c2
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-26 21:34:08 +0000

    HttpResponseHeaders::GetContentRange() to handle implied LWS There was a bug in HttpResponseHeaders::GetContentRange() that it didn't respect implied LWS rule, i.e. there can be arbitrary number of spaces in the header value between tokens.
    TEST=HttpResponseHeaders.GetContentRange
    Review URL: http://codereview.chromium.org/113675
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16921 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_headers_unittest.cc
+++ net/http/http_response_headers_unittest.cc
@@ -1110,6 +1110,13 @@ TEST(HttpResponseHeaders, GetContentRange) {
       51
     },
     { "HTTP/1.1 206 Partial Content\n"
+      "Content-Range: bytes\t0-50/51",
+      false,
+      -1,
+      -1,
+      -1
+    },
+    { "HTTP/1.1 206 Partial Content\n"
       "Content-Range:     bytes 0-50/51",
       true,
       0,
@@ -1117,7 +1124,22 @@ TEST(HttpResponseHeaders, GetContentRange) {
       51
     },
     { "HTTP/1.1 206 Partial Content\n"
-      "Content-Range: bytes    0    -   50   /   51",
+      "Content-Range:     bytes    0    -   50  \t / \t51",
+      true,
+      0,
+      50,
+      51
+    },
+    { "HTTP/1.1 206 Partial Content\n"
+      "Content-Range: bytes 0\t-\t50\t/\t51\t",
+      true,
+      0,
+      50,
+      51
+    },
+
+    { "HTTP/1.1 206 Partial Content\n"
+      "Content-Range: \t   bytes \t  0    -   50   /   5   1",
       false,
       -1,
       -1,
@@ -1137,8 +1159,22 @@ TEST(HttpResponseHeaders, GetContentRange) {
       -1,
       -1
     },
+    { "HTTP/1.1 416 Requested range not satisfiable\n"
+      "Content-Range: bytes *   /    *   ",
+      true,
+      -1,
+      -1,
+      -1
+    },
     { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: bytes 0-50/*",
+      true,
+      0,
+      50,
+      -1
+    },
+    { "HTTP/1.1 206 Partial Content\n"
+      "Content-Range: bytes 0-50  /    * ",
       true,
       0,
       50,
