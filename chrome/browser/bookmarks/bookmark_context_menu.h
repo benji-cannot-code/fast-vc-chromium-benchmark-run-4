018cdd4738585bcf9e76f3ab6e79644cba190b32
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-26 21:55:21 +0000

    Fix more compilation errors in browser_view.cc and an error I introduced in a previous change that used the wrong parent variable when showing HTML dialogs. Review URL: http://codereview.chromium.org/113864
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16929 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_context_menu.h
+++ chrome/browser/bookmarks/bookmark_context_menu.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model.h"
 
 // TODO(port): Port this file.
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(TOOLKIT_VIEWS)
 #include "views/controls/menu/chrome_menu.h"
 #elif defined(OS_LINUX)
 #include "chrome/browser/gtk/menu_gtk.h"
@@ -28,7 +28,7 @@ class PageNavigator;
 // bookmark bar, items on the bookmark bar, submenus of the bookmark bar and
 // the bookmark manager.
 class BookmarkContextMenu : public BookmarkModelObserver,
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(TOOLKIT_VIEWS)
                             public views::MenuDelegate
 #elif defined(OS_LINUX)
                             public MenuGtk::Delegate
@@ -140,7 +140,7 @@ class BookmarkContextMenu : public BookmarkModelObserver,
   BookmarkModel* model_;
   ConfigurationType configuration_;
 
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(TOOLKIT_VIEWS)
   scoped_ptr<views::MenuItemView> menu_;
 #elif defined(OS_LINUX)
   scoped_ptr<MenuGtk> menu_;
