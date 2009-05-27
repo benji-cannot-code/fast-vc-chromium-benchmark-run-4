9a408c5e9b9a4841c6bf533ff6cf4fe310f6d3b9
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-27 18:32:52 +0000

    Work around linux resolver problem where changes to resolv.conf go unnoticed in some distributions. BUG=11380
    TEST=Start chrome with empty resolv.conf, fix resolv.conf; chrome should
    be able to resolve sites.
    
    Review URL: http://codereview.chromium.org/113904
    Patch from Craig Schlenter <craig.schlenter@gmail.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17005 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_resolver.cc
+++ net/base/host_resolver.cc
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <netdb.h>
 #include <sys/socket.h>
 #endif
+#if defined(OS_LINUX)
+#include <resolv.h>
+#endif
 
 #include "base/message_loop.h"
 #include "base/string_util.h"
@@ -73,6 +76,13 @@ static int HostResolverProc(
   hints.ai_socktype = SOCK_STREAM;
 
   int err = getaddrinfo(host.c_str(), port.c_str(), &hints, out);
+#if defined(OS_LINUX)
+  // If we fail, re-initialise the resolver just in case there have been any
+  // changes to /etc/resolv.conf and retry. See http://crbug.com/11380 for info.
+  if (err && !res_init())
+    err = getaddrinfo(host.c_str(), port.c_str(), &hints, out);
+#endif
+
   return err ? ERR_NAME_NOT_RESOLVED : OK;
 }
 
