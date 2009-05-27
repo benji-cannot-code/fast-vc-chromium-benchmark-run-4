cd53f8750dd7bfc22f2335bd9246011ed8f71403
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-27 19:36:55 +0000

    Add stubs for a variety of dialogs under GTK-Views. Review URL: http://codereview.chromium.org/115824
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17010 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/browser_dialogs.h
+++ chrome/browser/views/browser_dialogs.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Browser;
 class BrowserView;
+class FindBar;
 class GURL;
 class HtmlDialogUIDelegate;
 class InfoBubbleDelegate;
---
+++ chrome/browser/views/dialog_stubs_gtk.cc
@@ -0,0 +1,83 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file contains stub implementations of the functions declared in
+// browser_dialogs.h that are currently unimplemented in GTK-views.
+
+#include "base/logging.h"
+#include "chrome/browser/views/browser_dialogs.h"
+
+namespace browser {
+
+void ShowBugReportView(views::Widget* parent,
+                       Profile* profile,
+                       TabContents* tab) {
+  NOTIMPLEMENTED();
+}
+
+void ShowClearBrowsingDataView(views::Widget* parent,
+                               Profile* profile) {
+  NOTIMPLEMENTED();
+}
+
+void ShowSelectProfileDialog() {
+  NOTIMPLEMENTED();
+}
+
+void ShowImporterView(views::Widget* parent,
+                      Profile* profile) {
+  NOTIMPLEMENTED();
+}
+
+void ShowBookmarkBubbleView(views::Window* parent,
+                            const gfx::Rect& bounds,
+                            InfoBubbleDelegate* delegate,
+                            Profile* profile,
+                            const GURL& url,
+                            bool newly_bookmarked) {
+  NOTIMPLEMENTED();
+}
+
+void HideBookmarkBubbleView() {
+  NOTIMPLEMENTED();
+}
+
+bool IsBookmarkBubbleViewShowing() {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void ShowBookmarkManagerView(Profile* profile) {
+  NOTIMPLEMENTED();
+}
+
+void ShowAboutChromeView(views::Widget* parent,
+                         Profile* profile) {
+  NOTIMPLEMENTED();
+}
+
+void ShowHtmlDialogView(gfx::NativeWindow parent, Browser* browser,
+                        HtmlDialogUIDelegate* delegate) {
+  NOTIMPLEMENTED();
+}
+
+FindBar* CreateFindBar(BrowserView* browser_view) {
+  NOTIMPLEMENTED();
+  return NULL;
+}
+
+void ShowPasswordsExceptionsWindowView(Profile* profile) {
+  NOTIMPLEMENTED();
+}
+
+void ShowKeywordEditorView(Profile* profile) {
+  NOTIMPLEMENTED();
+}
+
+void ShowNewProfileDialog() {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace browser
