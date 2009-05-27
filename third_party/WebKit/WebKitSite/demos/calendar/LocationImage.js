87929631083fbbeaf0fb1642fa323575cbd0e5c8
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-27 20:04:40 +0000

    2009-05-27  Maciej Stachowiak  <mjs@apple.com>
            Not reviewed, demo content.
            
            - add Calendar demo.
    
            * demos/calendar: Added.
            * demos/calendar/Boom.aiff: Added.
            * demos/calendar/Calendar.css: Added.
            * demos/calendar/Calendar.html: Added.
            * demos/calendar/Calendar.js: Added.
            * demos/calendar/Calendar.manifest: Added.
            * demos/calendar/CalendarApp.icns: Added.
            * demos/calendar/Images: Added.
            * demos/calendar/Images/AirPort4.png: Added.
            * demos/calendar/Images/AirPortError.png: Added.
            * demos/calendar/Images/disclosureTriangleSmallDown.png: Added.
            * demos/calendar/Images/disclosureTriangleSmallRight.png: Added.
            * demos/calendar/Images/statusbarBackground.png: Added.
            * demos/calendar/Images/statusbarResizerVertical.png: Added.
            * demos/calendar/LocationImage.js: Added.
            * demos/calendar/Utilities.js: Added.
            * demos/calendar/favicon.ico: Added.
            * demos/calendar/mime.types: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKitSite/demos/calendar/LocationImage.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function LocationImage() 
+{
+
+}
+
+LocationImage.prototype.requestLocationImage = function(address, callback)
+{
+
+}
+
+
+LocationImage.prototype.geocodeResultReceived = function(point, callback)
+{
+
+}
