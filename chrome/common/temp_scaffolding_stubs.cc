01dcbc27406a51fb309db654e984d176555ee112
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-28 03:56:03 +0000

    Hopefully fix the linux build.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17063 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.cc
+++ chrome/common/temp_scaffolding_stubs.cc
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/download_shelf.h"
 #endif
 
+#if defined(OS_LINUX)
+#include "chrome/browser/dock_info.h"
+#endif
+
 #if defined(TOOLKIT_VIEWS)
 #include "views/controls/menu/chrome_menu.h"
 #endif
@@ -316,11 +320,15 @@ void ShowOptionsWindow(OptionsPage page,
 }
 #endif
 
-#if defined(OS_MACOSX)
+#if !defined(TOOLKIT_VIEWS)
 bool DockInfo::GetNewWindowBounds(gfx::Rect* new_window_bounds,
                                   bool* maximize_new_window) const {
   NOTIMPLEMENTED();
   return true;
+}
+
+void DockInfo::AdjustOtherWindowBounds() const {
+  NOTIMPLEMENTED();
 }
 #endif
 
--- chrome/common/temp_scaffolding_stubs.h
+++ chrome/common/temp_scaffolding_stubs.h
@@ -344,7 +344,7 @@ class FaviconStatus {
 class DockInfo {
  public:
   bool GetNewWindowBounds(gfx::Rect*, bool*) const;
-  void AdjustOtherWindowBounds() const { NOTIMPLEMENTED(); }
+  void AdjustOtherWindowBounds() const;
 };
 #else
 
