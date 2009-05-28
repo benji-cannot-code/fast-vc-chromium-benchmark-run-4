670834f0714d8df929a0a3ed13c50c1bdd5935bb
brettw@chromium.org <brettw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-28 21:05:51 +0000

    2009-05-28  Brett Wilson  <brettw@chromium.org>
        Unreviewed, build fix.
    
        https://bugs.webkit.org/show_bug.cgi?id=26067
    
        Add casts for scale function to make more explicit what is happening
        and fix a compiler warning.
    
        * platform/graphics/IntSize.h:
          (WebCore::IntSize::scale):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44252 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/IntSize.h
+++ third_party/WebKit/WebCore/platform/graphics/IntSize.h
@@ -76,8 +76,8 @@ public:
     
     void scale(float scale)
     {
-        m_width *= scale;
-        m_height *= scale;
+        m_width = static_cast<int>(static_cast<float>(m_width) * scale);
+        m_height = static_cast<int>(static_cast<float>(m_height) * scale);
     }
     
     IntSize expandedTo(const IntSize& other) const
