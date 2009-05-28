da9baefebbe1bad548ff50375f565623243b9307
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-28 19:09:45 +0000

    Removes some NOTIMPLEMENTED now that TooltipManager exists on linux.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/113955
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17092 0039d316-1c4b-4281-b951-d872f2087c98

--- views/view.cc
+++ views/view.cc
@@ -22,10 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/layout_manager.h"
 #include "views/views_delegate.h"
 #include "views/widget/root_view.h"
+#include "views/widget/tooltip_manager.h"
 #include "views/widget/widget.h"
 #include "views/window/window.h"
 #if defined(OS_WIN)
-#include "views/widget/tooltip_manager.h"
 #include "views/accessibility/view_accessibility_wrapper.h"
 #endif
 
@@ -1223,27 +1223,15 @@ bool View::GetTooltipTextOrigin(int x, int y, gfx::Point* loc) {
 }
 
 void View::TooltipTextChanged() {
-#if defined(OS_WIN)
   Widget* widget = GetWidget();
   if (widget && widget->GetTooltipManager())
     widget->GetTooltipManager()->TooltipTextChanged(this);
-#else
-  // TODO(port): Not actually windows specific; I just haven't ported this part
-  // yet.
-  NOTIMPLEMENTED();
-#endif
 }
 
 void View::UpdateTooltip() {
-#if defined(OS_WIN)
   Widget* widget = GetWidget();
   if (widget && widget->GetTooltipManager())
     widget->GetTooltipManager()->UpdateTooltip();
-#else
-  // TODO(port): Not actually windows specific; I just haven't ported this part
-  // yet.
-  NOTIMPLEMENTED();
-#endif
 }
 
 void View::SetParentOwned(bool f) {
