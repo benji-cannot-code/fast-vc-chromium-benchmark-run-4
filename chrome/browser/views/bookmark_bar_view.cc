cad90297b8686796e60dbd290158d0c26ffb55b6
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-28 19:58:56 +0000

    Gets bookmark_bar_view to compile on Linux.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/115872
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17098 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/bookmark_bar_view.cc
+++ chrome/browser/views/bookmark_bar_view.cc
@@ -12,9 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util.h"
 #include "app/os_exchange_data.h"
 #include "app/resource_bundle.h"
-#include "app/win_util.h"
 #include "base/string_util.h"
-#include "base/base_drag_source.h"
 #include "chrome/browser/bookmarks/bookmark_context_menu.h"
 #include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/browser.h"
@@ -45,6 +43,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/widget/tooltip_manager.h"
 #include "views/widget/widget.h"
 #include "views/window/window.h"
+
+#if defined(OS_WIN)
+#include "app/win_util.h"
+#include "base/base_drag_source.h"
+#endif
 
 using views::CustomButton;
 using views::DropTargetEvent;
@@ -147,8 +150,13 @@ static std::wstring CreateToolTipForURLAndTitle(const gfx::Point& screen_loc,
                                                 const GURL& url,
                                                 const std::wstring& title,
                                                 const std::wstring& languages) {
-  const gfx::Rect monitor_bounds = win_util::GetMonitorBoundsForRect(
+#if defined(OS_WIN)
+  gfx::Rect monitor_bounds = win_util::GetMonitorBoundsForRect(
       gfx::Rect(screen_loc.x(), screen_loc.y(), 1, 1));
+#else
+  gfx::Rect monitor_bounds(0, 0, 10000, 10000);
+  NOTIMPLEMENTED();
+#endif
   gfx::Font tt_font = views::TooltipManager::GetDefaultFont();
   std::wstring result;
 
@@ -273,7 +281,7 @@ class BookmarkFolderButton : public views::MenuButton {
 // Tracks drops on the BookmarkBarView.
 
 struct BookmarkBarView::DropInfo {
-  DropInfo() : drop_index(-1), is_menu_showing(false), valid(false) {}
+  DropInfo() : valid(false), drop_index(-1), is_menu_showing(false) {}
 
   // Whether the data is valid.
   bool valid;
@@ -363,7 +371,6 @@ static const SkBitmap& GetGroupIcon() {
 
 BookmarkBarView::BookmarkBarView(Profile* profile, Browser* browser)
     : profile_(NULL),
-      browser_(browser),
       page_navigator_(NULL),
       model_(NULL),
       bookmark_menu_(NULL),
@@ -374,6 +381,7 @@ BookmarkBarView::BookmarkBarView(Profile* profile, Browser* browser)
       overflow_button_(NULL),
       instructions_(NULL),
       bookmarks_separator_view_(NULL),
+      browser_(browser),
       throbbing_view_(NULL) {
   SetID(VIEW_ID_BOOKMARK_BAR);
   Init();
@@ -1269,6 +1277,10 @@ void BookmarkBarView::Observe(NotificationType type,
       StopThrobbing(false);
       bubble_url_ = GURL();
       break;
+
+    default:
+      NOTREACHED();
+      break;
   }
 }
 
@@ -1349,10 +1361,13 @@ void BookmarkBarView::StartShowFolderDropMenuTimer(BookmarkNode* node) {
   }
   DCHECK(!show_folder_drop_menu_task_);
   show_folder_drop_menu_task_ = new ShowFolderDropMenuTask(this, node);
+#if defined(OS_WIN)
   static DWORD delay = 0;
-  if (!delay && !SystemParametersInfo(SPI_GETMENUSHOWDELAY, 0, &delay, 0)) {
+  if (!delay && !SystemParametersInfo(SPI_GETMENUSHOWDELAY, 0, &delay, 0))
     delay = kShowFolderDropMenuDelay;
-  }
+#else
+  int delay = kShowFolderDropMenuDelay;
+#endif
   MessageLoop::current()->PostDelayedTask(FROM_HERE,
                                           show_folder_drop_menu_task_, delay);
 }
--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -1688,6 +1688,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 ['include', '^browser/dock_info_gtk.cc'],
                 ['include', '^browser/dock_info.cc'],
                 ['include', '^browser/dock_info.h'],
+                ['include', '^browser/views/bookmark_bar_view.cc'],
+                ['include', '^browser/views/bookmark_bar_view.h'],
                 ['include', '^browser/views/bookmark_menu_button.cc'],
                 ['include', '^browser/views/bookmark_menu_button.h'],
                 ['include', '^browser/views/bookmark_menu_controller_views.cc'],
