57def58da65dfa9a9fbb46971a39aa0c9ff27245
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-28 16:55:04 +0000

    WebCore:
    2009-05-28  Simon Fraser  <simon.fraser@apple.com>
    
            Reviewed by Oliver Hunt.
            
            https://bugs.webkit.org/show_bug.cgi?id=8736
    
            Tests: fast/borders/border-radius-constraints.html
                   fast/borders/border-radius-split-inline.html
                   
            When the sum of the corner radii on a side exceed the length of the side,
            reduce the radii according to CSS 3 rules.
            
            Add RenderStyle::getBorderRadiiForRect() to fetch corner radii, applying
            the constraints. Use that for painting borders, box-shadow, clipping replaced
            elements
    
            * platform/graphics/IntSize.h:
            (WebCore::IntSize::scale):
            Add a scale method that scales by a float (using C rounding rules, like IntRect::scale()).
    
            * platform/graphics/Path.cpp:
            Make the QUARTER const static.
    
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::pushContentsClip):
            Use getBorderRadiiForRect to fetch border radii.
    
            * rendering/RenderBoxModelObject.cpp:
            (WebCore::RenderBoxModelObject::paintFillLayerExtended):
            Use getBorderRadiiForRect to fetch border radii.
    
            (WebCore::RenderBoxModelObject::paintBorder):
            Use getBorderRadiiForRect to fetch border radii, and fix a bug when drawing
            borders for split inlines, which used to apply the radii for each segment,
            and no longer does.
    
            (WebCore::RenderBoxModelObject::paintBoxShadow):
            Use getBorderRadiiForRect to fetch border radii.
    
            * rendering/RenderReplaced.cpp:
            (WebCore::RenderReplaced::paint):
            Use getBorderRadiiForRect to fetch border radii for clipping.
    
            * rendering/RenderWidget.cpp:
            (WebCore::RenderWidget::paint):
            Use getBorderRadiiForRect to fetch border radii for clipping.
    
            * rendering/style/RenderStyle.h:
            * rendering/style/RenderStyle.cpp:
            (WebCore::RenderStyle::getBorderRadiiForRect):
            New bottleneck method to fetch corner radiil given a rect, applying the constraint
            rules.
    
    LayoutTests:
    
    2009-05-28  Simon Fraser  <simon.fraser@apple.com>
    
            Reviewed by Oliver Hunt.
            
            https://bugs.webkit.org/show_bug.cgi?id=8736
    
            * fast/borders/border-radius-constraints.html: Added.
            * platform/mac/fast/borders/border-radius-constraints-expected.checksum: Added.
            * platform/mac/fast/borders/border-radius-constraints-expected.png: Added.
            * platform/mac/fast/borders/border-radius-constraints-expected.txt: Added.
            
            Test that corner radii are constrained according to CSS3 rules.
    
            * fast/borders/border-radius-huge-assert.html:
            * platform/mac/fast/borders/border-radius-huge-assert-expected.checksum:
            * platform/mac/fast/borders/border-radius-huge-assert-expected.png:
            * platform/mac/fast/borders/border-radius-huge-assert-expected.txt:
            
            Now that we constrain border-radius, update the text of the test, and the
            expected result.
            
            * fast/borders/border-radius-split-inline.html: Added.
            * platform/mac/fast/borders/border-radius-split-inline-expected.checksum: Added.
            * platform/mac/fast/borders/border-radius-split-inline-expected.png: Added.
            * platform/mac/fast/borders/border-radius-split-inline-expected.txt: Added.
            
            New test for border-radius on a split inline.
            
            * fast/box-shadow/border-radius-big.html:
            * platform/mac/fast/box-shadow/border-radius-big-expected.checksum:
            * platform/mac/fast/box-shadow/border-radius-big-expected.png:
            * platform/mac/fast/box-shadow/border-radius-big-expected.txt:
    
            Now that we constrain border-radius, update the text of the test, and the
            expected result.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44235 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/Path.cpp
+++ third_party/WebKit/WebCore/platform/graphics/Path.cpp
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>
 #include <wtf/MathExtras.h>
 
-const float QUARTER = 0.552f; // approximation of control point positions on a bezier
+static const float QUARTER = 0.552f; // approximation of control point positions on a bezier
                               // to simulate a quarter of a circle.
 namespace WebCore {
 
