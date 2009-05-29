4f3e4934b5f9fb08479f1b3b5328507f26290e47
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-29 20:24:02 +0000

    linux: Bookmark bar slide animation.
    also improve the look of the instructions label.
    Review URL: http://codereview.chromium.org/112070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17216 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/bookmark_bar_gtk.h
+++ chrome/browser/gtk/bookmark_bar_gtk.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "app/slide_animation.h"
 #include "chrome/common/owned_widget_gtk.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
 
@@ -22,7 +23,8 @@ class NineBox;
 class PageNavigator;
 class Profile;
 
-class BookmarkBarGtk : public BookmarkModelObserver {
+class BookmarkBarGtk : public AnimationDelegate,
+                       public BookmarkModelObserver {
  public:
   explicit BookmarkBarGtk(Profile* proifle, Browser* browser);
   virtual ~BookmarkBarGtk();
@@ -58,6 +60,10 @@ class BookmarkBarGtk : public BookmarkModelObserver {
 
   // Returns true if the bookmarks bar preference is set to 'always show'.
   bool IsAlwaysShown();
+
+  // AnimationDelegate implementation ------------------------------------------
+  virtual void AnimationProgressed(const Animation* animation);
+  virtual void AnimationEnded(const Animation* animation);
 
  private:
   // Helper function that sets visual properties of GtkButton |button| to the
@@ -226,6 +232,8 @@ class BookmarkBarGtk : public BookmarkModelObserver {
 
   // Paints the background for our bookmark bar.
   scoped_ptr<NineBox> background_ninebox_;
+
+  scoped_ptr<SlideAnimation> slide_animation_;
 };
 
 #endif  // CHROME_BROWSER_GTK_BOOKMARK_BAR_GTK_H_
