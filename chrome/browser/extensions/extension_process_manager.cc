1bf04b02ee5fa4d845b8c58cb743bcb1fba21504
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-29 06:32:43 +0000

    Fix unit test bustage by making sure ExtensionView::ViewHierarchyChanged calls the base class impl first.
    Fix compile bustage by not compiling windows-specific code on other platforms.
    
    TBR=brettw
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/115912
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17172 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_process_manager.cc
+++ chrome/browser/extensions/extension_process_manager.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browsing_instance.h"
 #include "chrome/browser/extensions/extension_host.h"
+#if defined(OS_WIN)
 #include "chrome/browser/extensions/extension_view.h"
+#endif
 #include "chrome/browser/extensions/extensions_service.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/tab_contents/site_instance.h"
@@ -41,12 +43,14 @@ ExtensionProcessManager::~ExtensionProcessManager() {
     delete *iter;
 }
 
+#if defined(OS_WIN)
 ExtensionView* ExtensionProcessManager::CreateView(Extension* extension,
                                                    const GURL& url,
                                                    Browser* browser) {
   return new ExtensionView(
       new ExtensionHost(extension, GetSiteInstanceForURL(url)), browser, url);
 }
+#endif
 
 void ExtensionProcessManager::CreateBackgroundHost(Extension* extension,
                                                    const GURL& url) {
