a780e39b3cd66a702dda8a0f9f31e6748602991e
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-29 21:28:29 +0000

    Better seeking for <video> and <audio> There two problems surrouding the seek feature for <video> and <audio>: 1. After each seek, the time goes back and forth until    stable, so user has to "fight" the time bar for    seeking. 2. When playing an audio file, sounds plays a litte bit    and then the play position rewinded and plays again.
    The cause of above problems:
    1. There are demuxed packets and decoded buffers inside
       decoders and renderers. When seek is requested only
       demuxer flush its buffers, decoders and renderers
       still plays a little bit of the old buffer, thus
       updating the time incorrectly.
    2. When playing a media file, WebKit does:
       1. load()
       2. pause()
       3. seek(0.0f)
       4. play()
       Since load() does prerolling internally, there were
       some decoded buffers in the renderers, and when
       seek(0.0f) is fired the play position reset to time
       0 and decode starts from there again, the internal
       buffer queue in the renderer would then look like:
    
       | 0.0s | | 0.1s | | 0.2s | | 0.0s | | 0.1s | ...
    
       Thus playback at the beginning goes back and forth.
    
    To solve the seek problems, here's what is done in this CL:
    1. All decoders and renderers should receive the seek signal.
    2. When seek signal is received, discard all buffers and
       schedule read again.
    
    With this CL, we can now scrub an <audio> tag with the timebar.
    The downside of this fix is that we don't have prerolling
    (notice that due to how WebKit starts playing back we didn't
    preroll anyway...), this should be fixed in another CL.
    
    Review URL: http://codereview.chromium.org/113891
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17242 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/media_posix.cc
+++ media/base/media_posix.cc
@@ -71,6 +71,11 @@ AVCodec* avcodec_find_decoder(enum CodecID id) {
   return avcodec_find_decoder_ptr(id);
 }
 
+void (*avcodec_flush_buffers_ptr)(AVCodecContext *avctx) = NULL;
+void avcodec_flush_buffers(AVCodecContext *avctx) {
+  avcodec_flush_buffers_ptr(avctx);
+}
+
 void (*avcodec_init_ptr)(void) = NULL;
 void avcodec_init(void) {
   avcodec_init_ptr();
@@ -231,6 +236,9 @@ bool InitializeMediaLibrary(const FilePath& module_dir) {
   avcodec_find_decoder_ptr =
       reinterpret_cast<AVCodec* (*)(enum CodecID)>(
           dlsym(libs[FILE_LIBAVCODEC], "avcodec_find_decoder"));
+  avcodec_flush_buffers_ptr =
+      reinterpret_cast<void (*)(AVCodecContext*)>(
+          dlsym(libs[FILE_LIBAVCODEC], "avcodec_flush_buffers"));
   avcodec_init_ptr =
       reinterpret_cast<void (*)(void)>(
           dlsym(libs[FILE_LIBAVCODEC], "avcodec_init"));
@@ -283,6 +291,7 @@ bool InitializeMediaLibrary(const FilePath& module_dir) {
       avcodec_decode_audio3_ptr &&
       avcodec_decode_video2_ptr &&
       avcodec_find_decoder_ptr &&
+      avcodec_flush_buffers_ptr &&
       avcodec_init_ptr &&
       avcodec_open_ptr &&
       avcodec_thread_init_ptr &&
