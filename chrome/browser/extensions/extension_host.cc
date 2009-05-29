228411c6c821be1c5c06189b050d50d17fb71a37
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-29 06:41:16 +0000

    Fixing more bustage by ifdefing out Windows-only code. TBR=brettw BUG=none TEST=none Review URL: http://codereview.chromium.org/115914
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17173 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_host.cc
+++ chrome/browser/extensions/extension_host.cc
@@ -30,7 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/context_menu.h"
 
 ExtensionHost::ExtensionHost(Extension* extension, SiteInstance* site_instance)
-    : extension_(extension), view_(NULL), did_stop_loading_(false) {
+    : extension_(extension),
+#if defined(OS_WIN)
+      view_(NULL),
+#endif
+      did_stop_loading_(false) {
   render_view_host_ = new RenderViewHost(
       site_instance, this, MSG_ROUTING_NONE, NULL);
   render_view_host_->AllowExtensionBindings();
@@ -53,8 +57,10 @@ void ExtensionHost::CreateRenderView(const GURL& url,
 }
 
 void ExtensionHost::UpdatePreferredWidth(int pref_width) {
+#if defined(OS_WIN)
   if (view_)
     view_->DidContentsPreferredWidthChange(pref_width);
+#endif
 }
 
 WebPreferences ExtensionHost::GetWebkitPrefs() {
@@ -87,8 +93,10 @@ void ExtensionHost::DidStopLoading(RenderViewHost* render_view_host) {
   render_view_host->WasResized();
   did_stop_loading_ = true;
 
+#if defined(OS_WIN)
   if (view_)
     view_->ShowIfCompletelyLoaded();
+#endif
 }
 
 ExtensionFunctionDispatcher* ExtensionHost::
@@ -159,18 +167,24 @@ void ExtensionHost::HandleKeyboardEvent(const NativeWebKeyboardEvent& event) {
 }
 
 void ExtensionHost::HandleMouseEvent() {
+#if defined(OS_WIN)
   if (view_)
     view_->HandleMouseEvent();
+#endif
 }
 
 void ExtensionHost::HandleMouseLeave() {
+#if defined(OS_WIN)
   if (view_)
     view_->HandleMouseLeave();
+#endif
 }
 
 Browser* ExtensionHost::GetBrowser() {
+#if defined(OS_WIN)
   if (view_)
     return view_->browser();
+#endif
   Browser* browser = BrowserList::GetLastActiveWithProfile(
       render_view_host()->process()->profile());
   // TODO(mpcomplete): what this verifies doesn't actually happen yet.
