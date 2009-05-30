76fc4d9e712f730fbe13f90cacf0f2f6bfbbac87
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-30 16:50:23 +0000

    2009-05-30  Holger Hans Peter Freyther  <zecke@selfish.org>
            Unreviewed build fix for AXObjectCache.
    
            For !HAVE(ACCESSIBILITY) postNotification was defined twice. Move
            that into the #ifdef.
    
            * accessibility/AXObjectCache.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AXObjectCache.cpp
+++ third_party/WebKit/WebCore/accessibility/AXObjectCache.cpp
@@ -279,6 +279,7 @@ void AXObjectCache::notificationPostTimerFired(Timer<AXObjectCache>*)
     m_notificationsToPost.clear();
 }
     
+#if HAVE(ACCESSIBILITY)
 void AXObjectCache::postNotification(RenderObject* renderer, const String& message, bool postToElement)
 {
     // Notifications for text input objects are sent to that object.
@@ -312,7 +313,6 @@ void AXObjectCache::postNotification(RenderObject* renderer, const String& messa
         m_notificationPostTimer.startOneShot(0);
 }
 
-#if HAVE(ACCESSIBILITY)
 void AXObjectCache::selectedChildrenChanged(RenderObject* renderer)
 {
     postNotification(renderer, "AXSelectedChildrenChanged", true);
