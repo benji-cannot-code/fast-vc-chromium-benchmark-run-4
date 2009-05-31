b6c5b203c1c09c9c2c46194425ec8896d6e76514
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-05-31 03:26:30 +0000

    Build fix for platforms without plugins support.
    git-svn-id: svn://svn.chromium.org/blink/trunk@44295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/PluginViewNone.cpp
+++ third_party/WebKit/WebCore/plugins/PluginViewNone.cpp
@@ -85,6 +85,12 @@ const char* PluginView::userAgent()
     return 0;
 }
 
+const char* PluginView::userAgentStatic()
+{
+    notImplemented();
+    return 0;
+}
+
 NPError PluginView::handlePostReadFile(Vector<char>&, uint32, const char*)
 {
     notImplemented();
@@ -93,6 +99,12 @@ NPError PluginView::handlePostReadFile(Vector<char>&, uint32, const char*)
 }
 
 NPError PluginView::getValue(NPNVariable, void*)
+{
+    notImplemented();
+    return 0;
+}
+
+NPError PluginView::getValueStatic(NPNVariable variable, void* value)
 {
     notImplemented();
     return 0;
