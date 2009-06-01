ed6f1cdaa32824b927ca98aab5ee324a8c5fb6c8
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-01 23:09:43 +0000

    Reviewed by Eric Seidel.
    Fix assertion error in --filters enabled debug builds.
    Instead of using RefPtr<AtomicStringImpl> as keys for the hash maps in SVGFilterBuilder, just use AtomicString objects.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44338 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/graphics/filters/SVGFilterBuilder.h
+++ third_party/WebKit/WebCore/svg/graphics/filters/SVGFilterBuilder.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 
 #if ENABLE(SVG) && ENABLE(FILTERS)
+#include "AtomicStringHash.h"
 #include "FilterEffect.h"
 #include "PlatformString.h"
 
@@ -44,8 +45,8 @@ namespace WebCore {
         void clearEffects();
 
     private:
-        HashMap<RefPtr<AtomicStringImpl>, RefPtr<FilterEffect> > m_builtinEffects;
-        HashMap<RefPtr<AtomicStringImpl>, RefPtr<FilterEffect> > m_namedEffects;
+        HashMap<AtomicString, RefPtr<FilterEffect> > m_builtinEffects;
+        HashMap<AtomicString, RefPtr<FilterEffect> > m_namedEffects;
 
         RefPtr<FilterEffect> m_lastEffect;
     };
