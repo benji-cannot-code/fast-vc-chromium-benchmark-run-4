f8f7cc69b6ce1642920445671b49105dcd633aa1
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-01 18:19:23 +0000

    Have the find bar show number of matches on linux.
    We needed to enable some code in webkit/glue/webframe_impl.cc and
    add a line to clear the results in find_bar_gtk.cc.
    
    BUG=12955
    
    Review URL: http://codereview.chromium.org/115960
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17335 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/find_bar_gtk.cc
+++ chrome/browser/gtk/find_bar_gtk.cc
@@ -216,6 +216,7 @@ void FindBarGtk::SetFocusAndSelection() {
 }
 
 void FindBarGtk::ClearResults(const FindNotificationDetails& results) {
+  UpdateUIForFindResult(results, string16());
 }
 
 void FindBarGtk::StopAnimation() {
@@ -327,6 +328,8 @@ void FindBarGtk::FindEntryTextInContents(bool forward_search) {
   } else {
     // The textbox is empty so we reset.
     tab_contents->StopFinding(true);  // true = clear selection on page.
+    UpdateUIForFindResult(find_bar_controller_->tab_contents()->find_result(),
+                          string16());
   }
 }
 
