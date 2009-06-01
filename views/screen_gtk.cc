0d898a431564b7653726602da8a2ff1a442c0783
erikkay@google.com <erikkay@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-01 16:58:02 +0000

    A helper class for screen related queries.  Currently only has a single method for getting the current cursor position in screen coordinates. Review URL: http://codereview.chromium.org/112072
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17322 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ views/screen_gtk.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "views/screen.h"
+
+#include <gtk/gtk.h>
+
+namespace views {
+
+// static
+gfx::Point Screen::GetCursorScreenPoint() {
+  gint x, y;
+  gdk_display_get_pointer(NULL, NULL, &x, &y, NULL);
+  return gfx::Point(x, y);
+}
+
+}  // namespace
+
---
+++ views/screen_win.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "views/screen.h"
+
+#include <windows.h>
+
+namespace views {
+
+// static
+gfx::Point Screen::GetCursorScreenPoint() {
+  POINT pt;
+  GetCursorPos(&pt);
+  return gfx::Point(pt);
+}
+
+}  // namespace
+
