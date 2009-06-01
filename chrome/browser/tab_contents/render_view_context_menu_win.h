702f1d3e7f40d7b90a7deb8d0f6ffc054167ec03
xji@chromium.org <xji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-01 21:19:30 +0000

    This CL fixes issue 10827 -- Hebrew: pop-up menu positioned to right of mouse-click in content area
    context menu should be positioned using the mouse-click point as top right point in RTL locales.
    
    BUG=http://crbug.com/10827
    TEST=Open Hebrew Chrome, Right-click on the content area, on a link, or on an image, The pop-up menu should be positioned to the left of the mouse-click.
    Review URL: http://codereview.chromium.org/118043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17353 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_context_menu_win.h
+++ chrome/browser/tab_contents/render_view_context_menu_win.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_TAB_CONTENTS_RENDER_VIEW_CONTEXT_MENU_WIN_H_
 #define CHROME_BROWSER_TAB_CONTENTS_RENDER_VIEW_CONTEXT_MENU_WIN_H_
 
+#include "base/scoped_ptr.h"
 #include "chrome/browser/tab_contents/render_view_context_menu.h"
 #include "views/accelerator.h"
 #include "views/controls/menu/menu_win.h"
@@ -44,7 +45,7 @@ class RenderViewContextMenuWin : public RenderViewContextMenu,
                   const std::wstring& label,
                   views::Menu::MenuItemType type);
 
-  views::MenuWin menu_;
+  scoped_ptr<views::MenuWin> menu_;
   views::Menu* sub_menu_;
 };
 
