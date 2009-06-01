ce439c7146f04f282a9a5b45f5d00d69600b9a1d
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-01 20:26:42 +0000

    Moved bug to external tracker.
    TBR=nsylvain
    Review URL: http://codereview.chromium.org/118085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17347 0039d316-1c4b-4281-b951-d872f2087c98

--- base/pickle.cc
+++ base/pickle.cc
@@ -97,8 +97,8 @@ bool Pickle::ReadInt(void** iter, int* result) const {
   if (!IteratorHasRoomFor(*iter, sizeof(*result)))
     return false;
 
-  // TODO(jar) bug 1129285: Pickle should be cleaned up, and not dependent on
-  // alignment.
+  // TODO(jar): http://crbug.com/13108 Pickle should be cleaned up, and not
+  // dependent on alignment.
   // Next line is otherwise the same as: memcpy(result, *iter, sizeof(*result));
   *result = *reinterpret_cast<int*>(*iter);
 
@@ -114,8 +114,8 @@ bool Pickle::ReadLong(void** iter, long* result) const {
   if (!IteratorHasRoomFor(*iter, sizeof(*result)))
     return false;
 
-  // TODO(jar) bug 1129285: Pickle should be cleaned up, and not dependent on
-  // alignment.
+  // TODO(jar): http://crbug.com/13108 Pickle should be cleaned up, and not
+  // dependent on alignment.
   memcpy(result, *iter, sizeof(*result));
 
   UpdateIter(iter, sizeof(*result));
@@ -136,8 +136,8 @@ bool Pickle::ReadSize(void** iter, size_t* result) const {
   if (!IteratorHasRoomFor(*iter, sizeof(*result)))
     return false;
 
-  // TODO(jar) bug 1129285: Pickle should be cleaned up, and not dependent on
-  // alignment.
+  // TODO(jar): http://crbug.com/13108 Pickle should be cleaned up, and not
+  // dependent on alignment.
   // Next line is otherwise the same as: memcpy(result, *iter, sizeof(*result));
   *result = *reinterpret_cast<size_t*>(*iter);
 
