411cbd282fd059e83017cd460a46aa09f0aa7561
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-01 08:18:11 +0000

    2009-06-01  Darin Adler  <darin@apple.com>
            * WebView/WebTextCompletionController.mm: Fix Tiger build by adding import
            of WebTypesInternal.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebTextCompletionController.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebTextCompletionController.mm
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "DOMRangeInternal.h"
 #import "WebFrameInternal.h"
 #import "WebHTMLViewInternal.h"
+#import "WebTypesInternal.h"
 #import <WebCore/Frame.h>
 
 @interface NSWindow (WebNSWindowDetails)
