327ea46e9704a780cdc1382c630b3f70165a81cb
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-02 19:01:34 +0000

    2009-06-02  Brent Fulgham  <bfulgham@webkit.org>
            Reviewed by Adam Roben.
    
            Update WinLauncher for WebKitCreateInstance.
            https://bugs.webkit.org/show_bug.cgi?id=26136
    
            * WinLauncher/WinLauncher.cpp: Revise implementation to
              use WebKitCreateInstance (rather than CoCreateInstance).
            * WinLauncher/WinLauncher.h: Modify include path for
              WebKit.h to match DumpRenderTree style.
            * WinLauncher/WinLauncher.vcproj: Remove unnecessary
              WebKit.tlb and EmbeddedIDL flags from build.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44363 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/WinLauncher/WinLauncher.cpp
+++ third_party/WebKit/WebKitTools/WinLauncher/WinLauncher.cpp
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "stdafx.h"
 #include "WinLauncher.h"
-#include "WebKit.h"
+#include <WebKit/WebKitCOMAPI.h>
 
 #include <commctrl.h>
 #include <objbase.h>
@@ -178,7 +178,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     SetWindowLong(hURLBarWnd, GWL_WNDPROC,(long)MyEditProc);
     SetFocus(hURLBarWnd);
 
-    HRESULT hr = CoCreateInstance(CLSID_WebView, 0, CLSCTX_ALL, IID_IWebView, (void**)&gWebView);
+    HRESULT hr = WebKitCreateInstance(CLSID_WebView, 0, IID_IWebView, (void**)&gWebView);
     if (FAILED(hr))
         goto exit;
 
@@ -202,9 +202,8 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     hr = gWebView->mainFrame(&frame);
     if (FAILED(hr))
         goto exit;
-    static BSTR defaultHTML = 0;
-    if (!defaultHTML)
-        defaultHTML = SysAllocString(TEXT("<p style=\"background-color: #00FF00\">Testing</p><img src=\"http://webkit.org/images/icon-gold.png\" alt=\"Face\"><div style=\"border: solid blue\" contenteditable=\"true\">div with blue border</div><ul><li>foo<li>bar<li>baz</ul>"));
+
+    static BSTR defaultHTML = SysAllocString(TEXT("<p style=\"background-color: #00FF00\">Testing</p><img src=\"http://webkit.org/images/icon-gold.png\" alt=\"Face\"><div style=\"border: solid blue\" contenteditable=\"true\">div with blue border</div><ul><li>foo<li>bar<li>baz</ul>"));
     frame->loadHTMLString(defaultHTML, 0);
     frame->Release();
 
@@ -212,6 +211,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     hr = gWebView->QueryInterface(IID_IWebViewPrivate, (void**)&viewExt);
     if (FAILED(hr))
         goto exit;
+
     hr = viewExt->viewWindow((OLE_HANDLE*) &gViewWindow);
     viewExt->Release();
     if (FAILED(hr) || !gViewWindow)
@@ -242,7 +242,7 @@ exit:
     // Shut down COM.
     OleUninitialize();
     
-    return (int) msg.wParam;
+    return static_cast<int>(msg.wParam);
 }
 
 ATOM MyRegisterClass(HINSTANCE hInstance)
@@ -325,7 +325,7 @@ LRESULT CALLBACK MyEditProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lPara
 {
     switch (message) {
         case WM_CHAR:
-            if( wParam == 13 ) { // Enter Key
+            if (wParam == 13) { // Enter Key
                 wchar_t strPtr[MAX_URL_LENGTH];
                 *((LPWORD)strPtr) = MAX_URL_LENGTH; 
                 int strLen = SendMessage(hDlg, EM_GETLINE, 0, (LPARAM)strPtr);
@@ -368,10 +368,9 @@ static void loadURL(BSTR urlBStr)
 {
     IWebFrame* frame = 0;
     IWebMutableURLRequest* request = 0;
-    static BSTR methodBStr = 0;
 
-    if (!methodBStr)
-        methodBStr = SysAllocString(TEXT("GET"));
+
+    static BSTR methodBStr = SysAllocString(TEXT("GET"));
 
     if (urlBStr && urlBStr[0] && (PathFileExists(urlBStr) || PathIsUNC(urlBStr))) {
         TCHAR fileURL[INTERNET_MAX_URL_LENGTH];
@@ -384,7 +383,7 @@ static void loadURL(BSTR urlBStr)
     if (FAILED(hr))
         goto exit;
 
-    hr = CoCreateInstance(CLSID_WebMutableURLRequest, 0, CLSCTX_ALL, IID_IWebMutableURLRequest, (void**)&request);
+    hr = WebKitCreateInstance(CLSID_WebMutableURLRequest, 0, IID_IWebMutableURLRequest, (void**)&request);
     if (FAILED(hr))
         goto exit;
 
--- third_party/WebKit/WebKitTools/WinLauncher/WinLauncher.h
+++ third_party/WebKit/WebKitTools/WinLauncher/WinLauncher.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "resource.h"
-#include "WebKit.h"
+#include <WebKit/WebKit.h>
 
 class WinLauncherWebHost : public IWebFrameLoadDelegate
 {
