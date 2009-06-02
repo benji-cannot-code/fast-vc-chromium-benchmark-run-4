6ea8c58b707083ebd3acca40ca7896348f28c448
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-02 09:17:00 +0000

    Revert 44354.  Test does not pass.
    git-svn-id: svn://svn.chromium.org/blink/trunk@44355 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLOptionElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLOptionElement.cpp
@@ -162,7 +162,7 @@ void HTMLOptionElement::childrenChanged(bool changedByParser, Node* beforeChange
 HTMLSelectElement* HTMLOptionElement::ownerSelectElement() const
 {
     Node* select = parentNode();
-    while (select && !(select->hasTagName(selectTag) || select->hasTagName(keygenTag)))
+    while (select && !select->hasTagName(selectTag))
         select = select->parentNode();
 
     if (!select)
