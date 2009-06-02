26aa09155ff22486c247bce24eb69c106f1226c1
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-02 08:55:41 +0000

    Disable this test since I can't run it without trashing my profile.
    TBR=anantha
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17397 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/mini_installer_test/test.cc
+++ chrome/test/mini_installer_test/test.cc
@@ -101,7 +101,7 @@ TEST_F(MiniInstallTest, DISABLED_MiniInstallerOverChromeMetaInstallerTest) {
   installer.OverInstall();
 }
 
-TEST_F(MiniInstallTest, MiniInstallerSystemInstallTest) {
+TEST_F(MiniInstallTest, DISABLED_MiniInstallerSystemInstallTest) {
   if (win_util::GetWinVersion() < win_util::WINVERSION_VISTA) {
     ChromeMiniInstaller installer(mini_installer_constants::kSystemInstall,
                                   mini_installer_constants::kDevChannelBuild);
