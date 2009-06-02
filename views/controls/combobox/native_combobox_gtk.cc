321fcbdb3f7bd6f34163b0b8e13b3ed465e8c7f5
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-02 20:03:43 +0000

    Gets ComboBox to compile on linux.
    BUG=none
    TEST=none
    TBR=ben
    
    Review URL: http://codereview.chromium.org/118123
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17422 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/combobox/native_combobox_gtk.cc
+++ views/controls/combobox/native_combobox_gtk.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "views/controls/combobox/native_combobox_gtk.h"
 
+#include "base/logging.h"
 #include "views/controls/combobox/combobox.h"
 
 namespace views {
