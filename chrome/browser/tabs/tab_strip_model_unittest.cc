19882537b2adb4e89d4539bbc35daf40747ebf9c
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-02 20:18:49 +0000

    Fix mac build.
    TBR=erg
    
    Review URL: http://codereview.chromium.org/114068
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17426 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tabs/tab_strip_model_unittest.cc
+++ chrome/browser/tabs/tab_strip_model_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/path_service.h"
 #include "base/stl_util-inl.h"
-#include "chrome/browser/dock_info.h"
 #include "chrome/browser/dom_ui/new_tab_ui.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/profile_manager.h"
