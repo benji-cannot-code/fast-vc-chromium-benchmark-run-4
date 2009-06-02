0a203ac947fa6c5e934d9369c82cd506ae3f8397
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-02 16:57:56 +0000

    2009-06-02  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Sam Weinig.
    
            Use C-style comments in Platform.h so it can be included from C
            files.
    
            * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44359 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -544,8 +544,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(JIT) || ENABLE(YARR_JIT)
 #define ENABLE_ASSEMBLER 1
 #endif
-// Setting this flag prevents the assembler from using RWX memory; this may improve
-// security but currectly comes at a significant performance cost.
+/* Setting this flag prevents the assembler from using RWX memory; this may improve
+   security but currectly comes at a significant performance cost. */
 #define ENABLE_ASSEMBLER_WX_EXCLUSIVE 0
 
 #if !defined(ENABLE_PAN_SCROLLING) && PLATFORM(WIN_OS)
