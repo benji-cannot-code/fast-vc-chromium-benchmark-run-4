d20b2bff9af8c75f224a182913052963126ccd36
dglazkov@google.com <dglazkov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-03 22:05:18 +0000

    Always disconnect event listeners when clearing for navigation, even if the script context is empty.
    R=mbelshe
    BUG=13266
    TEST=gmail shouldn't crash anymore.
    
    Review URL: http://codereview.chromium.org/119109
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17546 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/v8/v8_proxy.cpp
+++ webkit/port/bindings/v8/v8_proxy.cpp
@@ -1828,6 +1828,9 @@ void V8Proxy::clearForClose()
 
 void V8Proxy::clearForNavigation()
 {
+    // disconnect all event listeners
+    DisconnectEventListeners();
+
     if (!m_context.IsEmpty()) {
         v8::HandleScope handle;
         ClearDocumentWrapper();
@@ -1839,9 +1842,6 @@ void V8Proxy::clearForNavigation()
             LookupDOMWrapper(V8ClassIndex::DOMWINDOW, m_global);
         ASSERT(!wrapper.IsEmpty());
         wrapper->TurnOnAccessCheck();
-
-        // disconnect all event listeners
-        DisconnectEventListeners();
 
         // Separate the context from its global object.
         m_context->DetachGlobal();
