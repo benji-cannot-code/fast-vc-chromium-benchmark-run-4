2dfaf5f756d4cb2dcf2b9713989e974a8b409264
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-03 19:16:59 +0000

    Implement more of the BookmarkBubble, connecting it to the bookmark system.
    This allows you to edit the bookmark title, and remove the bookmark.
    
    BUG=11738
    TEST=Click the bookmark star, you should be able to edit the bookmark title
         and remove the bookmark.
    
    Review URL: http://codereview.chromium.org/119079
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17514 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/toolbar_star_toggle_gtk.cc
+++ chrome/browser/gtk/toolbar_star_toggle_gtk.cc
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/resource_bundle.h"
 #include "base/gfx/rect.h"
 #include "chrome/browser/gtk/bookmark_bubble_gtk.h"
+#include "chrome/browser/gtk/browser_toolbar_gtk.h"
 #include "grit/theme_resources.h"
 
-ToolbarStarToggleGtk::ToolbarStarToggleGtk()
-    : widget_(gtk_button_new()),
+ToolbarStarToggleGtk::ToolbarStarToggleGtk(BrowserToolbarGtk* host)
+    : host_(host),
+      widget_(gtk_button_new()),
       is_starred_(false),
       unstarred_(IDR_STAR, IDR_STAR_P, IDR_STAR_H, IDR_STAR_D),
       starred_(IDR_STARRED, IDR_STARRED_P, IDR_STARRED_H, 0) {
@@ -43,7 +45,7 @@ void ToolbarStarToggleGtk::ShowStarBubble(const GURL& url,
 
   BookmarkBubbleGtk::Show(GTK_WINDOW(gtk_widget_get_toplevel(widget)),
                           gfx::Rect(x, y, width, height),
-                          NULL,
+                          host_->profile(),
                           url,
                           newly_bookmarked);
 }
