3bc40bb22f3910afb0d6a7dc96c8c669dc883840
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-03 20:32:14 +0000

    2009-06-03  David Levin  <levin@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            v8's ScriptController::evaluate should protect the Frame like the jsc version.
            https://bugs.webkit.org/show_bug.cgi?id=26172
    
            This change is simply copying protections done for Frame in the method
            WebCore::ScriptController::evaluate in the file js/ScriptController.cpp.
    
            * bindings/v8/ScriptController.cpp:
            (WebCore::ScriptController::evaluate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/ScriptController.cpp
+++ third_party/WebKit/WebCore/bindings/v8/ScriptController.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2008, 2009 Google Inc. All rights reserved.
+ * Copyright (C) 2009 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -192,7 +193,14 @@ ScriptValue ScriptController::evaluate(const ScriptSourceCode& sourceCode)
         return ScriptValue();
 
     v8::Context::Scope scope(context);
+
+    RefPtr<Frame> protect(m_frame);
+
     v8::Local<v8::Value> object = m_proxy->evaluate(sourceCode, 0);
+
+    // Evaluating the JavaScript could cause the frame to be deallocated
+    // so we start the keep alive timer here.
+    m_frame->keepAlive();
 
     if (object.IsEmpty() || object->IsUndefined())
         return ScriptValue();
