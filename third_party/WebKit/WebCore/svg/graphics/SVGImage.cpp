58b10d3253ca4887628004b6583c25919fbd8f49
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-03 15:29:06 +0000

    Windows build fix after r44379
            * svg/graphics/SVGImage.cpp: Move EmptyClients.h back down below the
            other #includes to fix a compiler warning on Windows.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/graphics/SVGImage.cpp
+++ third_party/WebKit/WebCore/svg/graphics/SVGImage.cpp
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CachedPage.h"
 #include "DocumentLoader.h"
-#include "EmptyClients.h"
 #include "FloatRect.h"
 #include "Frame.h"
 #include "FrameLoader.h"
@@ -48,6 +47,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGRenderSupport.h"
 #include "SVGSVGElement.h"
 #include "Settings.h"
+
+// Moving this #include above FrameLoader.h causes the Windows build to fail due to warnings about
+// alignment in Timer<FrameLoader>. It seems that the definition of EmptyFrameLoaderClient is what
+// causes this (removing that definition fixes the warnings), but it isn't clear why.
+#include "EmptyClients.h"
 
 namespace WebCore {
 
