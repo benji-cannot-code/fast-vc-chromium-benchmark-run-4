92beac3589386f5ec1f81e8dd7644e8e8b71bb8d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-04 20:44:39 +0000

    2009-06-04  Albert J. Wong  <ajwong@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=26148
            Adding in empty files to stage the extract of RenderThemeChromiumSkia
            from RenderThemeChromiumLinux and RenderThemeChromiumWindows.
    
            * rendering/RenderThemeChromiumSkia.cpp: Added.
            * rendering/RenderThemeChromiumSkia.h: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebCore/rendering/RenderThemeChromiumSkia.cpp
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// FIXME: This is a placeholder to avoid some issues with webkit.gyp in the chromium tree. Having this placeholder until we can factor out
+// RenderThemeChromiumSkia from RenderThemeChromiumLinux will keep the HEAD of both trees compatible.
