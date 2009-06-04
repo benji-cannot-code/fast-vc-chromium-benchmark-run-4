6c1d0abf1c411fb90f69b97f4a9a6593979f3484
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-04 20:39:05 +0000

    2009-06-04  Darin Adler  <darin@apple.com>
            Reviewed by Tim Hatcher.
    
            Fix crashes due to unbalanced start/stop observer calls.
    
            * DumpRenderTree/mac/DumpRenderTreeWindow.h: Added observingWebView.
            * DumpRenderTree/mac/DumpRenderTreeWindow.mm:
            (-[DumpRenderTreeWindow startObservingWebView]): Call stopObservingWebView
            in case we're already observing some other WebView. Set observingWebView.
            (-[DumpRenderTreeWindow stopObservingWebView]): Do nothing if
            observingWebView is already NO. Clear observingWebView.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44425 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/DumpRenderTreeWindow.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/DumpRenderTreeWindow.h
@@ -34,6 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class WebView;
 
 @interface DumpRenderTreeWindow : NSWindow
+{
+    BOOL observingWebView;
+}
+
 // I'm not sure why we can't just use [NSApp windows]
 + (NSArray *)openWindows;
 
