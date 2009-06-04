b69dedc1f46f4f28da20cbc189b9d0eb1916a222
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-04 08:22:24 +0000

    Speculative Tiger build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@44410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebDelegateImplementationCaching.h
+++ third_party/WebKit/WebKit/mac/WebView/WebDelegateImplementationCaching.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This header contains WebView declarations that can be used anywhere in WebKit, but are neither SPI nor API.
 
+#import "WebTypesInternal.h"
+
 @class WebView;
 
 struct WebResourceDelegateImplementationCache {
