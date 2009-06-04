82629869f70fef0fc58cb78d6acdafd00d3f7274
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-04 02:37:45 +0000

    Reland my ClientSocketPool refactor. Fixed a bug where we were double compensating for active_socket_count in CancelRequest and OnIOComplete. Added ClientSocketPoolTest_TwoRequestsCancelOne to catch this.  Triggered a DCHECK in debug mode before I removed the extra code in OnIOComplete.
    BUG=http://www.crbug.com/13289
    
    Review URL: http://codereview.chromium.org/118172
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17588 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/test_completion_callback.h
+++ net/base/test_completion_callback.h
@@ -41,7 +41,6 @@ class TestCompletionCallback : public CallbackRunner< Tuple1<int> > {
 
   bool have_result() const { return have_result_; }
 
- private:
   virtual void RunWithParams(const Tuple1<int>& params) {
     result_ = params.a;
     have_result_ = true;
@@ -49,6 +48,7 @@ class TestCompletionCallback : public CallbackRunner< Tuple1<int> > {
       MessageLoop::current()->Quit();
   }
 
+ private:
   int result_;
   bool have_result_;
   bool waiting_for_result_;
