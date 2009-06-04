8e2c1a6cedbaaafef351ed5b54cac19ff23d2253
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-04 18:34:23 +0000

    2009-06-04  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=26180
            Add a fast path for SecurityOrigin::equal. If "other == this" (where
            other is the other security origin), then we really don't need to do
            all the other (expensive) comparisons. We know it's equal.
    
            * page/SecurityOrigin.cpp:
            (WebCore::SecurityOrigin::equal):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44420 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/SecurityOrigin.cpp
+++ third_party/WebKit/WebCore/page/SecurityOrigin.cpp
@@ -278,6 +278,9 @@ String SecurityOrigin::databaseIdentifier() const
 
 bool SecurityOrigin::equal(const SecurityOrigin* other) const 
 {
+    if (other == this)
+        return true;
+    
     if (!isSameSchemeHostPort(other))
         return false;
 
