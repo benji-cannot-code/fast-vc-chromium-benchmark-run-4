49cf7e553df710010824c00064f38c6c4f02ff2c
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-04 22:08:13 +0000

    Fix: Alt+Tab not sending FocusWillChange events.
    This causes a DCHECK in FindBarWin if the Find box is open when the user switches to another app with Alt+Tab.
    
    The Focus manager now calls ClearFocus everywhere it used to set focused_view_ = NULL, to make sure the event reaches all listeners for FocusWillChange.
    
    BUG=13350
    TEST=Open Chrome, press Ctrl+F then Alt+Tab. Swith focus back to Chrome and press Esc multiple times and you should not see the Find box flash briefly.
    
    Review URL: http://codereview.chromium.org/118267
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17678 0039d316-1c4b-4281-b951-d872f2087c98

--- views/focus/focus_manager.cc
+++ views/focus/focus_manager.cc
@@ -351,9 +351,8 @@ bool FocusManager::OnPostActivate(HWND window,
 
 void FocusManager::ValidateFocusedView() {
   if (focused_view_) {
-    if (!ContainsView(focused_view_)) {
-      focused_view_ = NULL;
-    }
+    if (!ContainsView(focused_view_))
+      ClearFocus();
   }
 }
 
@@ -555,7 +554,7 @@ void FocusManager::StoreFocusedView() {
   view_storage->StoreView(stored_focused_view_storage_id_, focused_view_);
 
   View* v = focused_view_;
-  focused_view_ = NULL;
+  ClearFocus();
 
   if (v)
     v->SchedulePaint();  // Remove focus border.
@@ -717,7 +716,7 @@ bool FocusManager::IsTabTraversalKeyEvent(const KeyEvent& key_event) {
 
 void FocusManager::ViewRemoved(View* parent, View* removed) {
   if (focused_view_ && focused_view_ == removed)
-    focused_view_ = NULL;
+    ClearFocus();
 }
 
 void FocusManager::AddKeystrokeListener(KeystrokeListener* listener) {
