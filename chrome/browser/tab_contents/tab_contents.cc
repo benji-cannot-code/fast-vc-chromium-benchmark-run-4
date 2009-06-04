46d9234a429cefcb474ebdcb0f0d255c7d4cd065
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-04 19:30:29 +0000

    Add atlapp.h. Wasn't neccessary on my local windows machine but builders want it.
    TBR=beng
    
    Review URL: http://codereview.chromium.org/119194
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17648 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 // For CRect
+#include <atlapp.h>
 #include <atlmisc.h>
 // TODO(port): some of these headers should be ported.
 #include "chrome/browser/modal_html_dialog_delegate.h"
