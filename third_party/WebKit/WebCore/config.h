4b7e2b2cd5fedd7c374a08546b494c8bfd8138a9
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-04 00:11:51 +0000

    - Windows build fix
            Rolled out apparently-accidental changes to config.h from r44398. These
            were not part of the patch as reviewed.
    
            * config.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44405 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/config.h
+++ third_party/WebKit/WebCore/config.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/Platform.h>
 
-#if PLATFORM(WIN_OS) && !defined(BUILDING_WX__)
+#if PLATFORM(WIN_OS) && !defined(BUILDING_WX__) && !COMPILER(GCC)
 #if defined(BUILDING_JavaScriptCore) || defined(BUILDING_WTF)
 #define JS_EXPORTDATA __declspec(dllexport)
 #else
@@ -67,11 +67,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 // CURL needs winsock, so don't prevent inclusion of it
-//#if !USE(CURL)
+#if !USE(CURL)
 #ifndef _WINSOCKAPI_
 #define _WINSOCKAPI_ // Prevent inclusion of winsock.h in windows.h
 #endif
-//#endif
+#endif
 
 #endif /* PLATFORM(WIN_OS) */
 
@@ -108,20 +108,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if PLATFORM(WIN)
-#define WTF_PLATFORM_CF 1
-#if 0
 #define WTF_PLATFORM_CG 1
 #undef WTF_PLATFORM_CAIRO
 #define WTF_USE_CFNETWORK 1
-#undef WTF_USE_CURL
-#else
-#undef WTF_PLATFORM_CG
-#define WTF_PLATFORM_CAIRO 1
-#undef WTF_USE_CFNETWORK
-#define WTF_USE_CURL 1
-#endif
-#undef WTF_USE_PTHREADS
 #undef WTF_USE_WININET
+#define WTF_PLATFORM_CF 1
+#define WTF_USE_PTHREADS 0
 #endif
 
 #if PLATFORM(MAC)
