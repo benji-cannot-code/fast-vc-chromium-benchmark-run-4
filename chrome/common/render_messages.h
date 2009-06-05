62ffe10d3f8dc2db7a5e4e5d55d01334609e4531
klink@chromium.org <klink@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-05 21:56:48 +0000

    Fixes an issue where the MSAA information was not reported back correctly to the AT, upon the HWND containing the web content first receiving focus. Note that this is not a full fix for the bug below, but one more piece to resolving it.BUG=13291TEST=none Review URL: http://codereview.chromium.org/118206
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17778 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/render_messages.h
+++ chrome/common/render_messages.h
@@ -521,7 +521,6 @@ struct ParamTraits<webkit_glue::WebAccessibility::InParams> {
     WriteParam(m, p.object_id);
     WriteParam(m, p.function_id);
     WriteParam(m, p.child_id);
-    WriteParam(m, p.direct_descendant);
     WriteParam(m, p.input_long1);
     WriteParam(m, p.input_long2);
   }
@@ -530,7 +529,6 @@ struct ParamTraits<webkit_glue::WebAccessibility::InParams> {
       ReadParam(m, iter, &p->object_id) &&
       ReadParam(m, iter, &p->function_id) &&
       ReadParam(m, iter, &p->child_id) &&
-      ReadParam(m, iter, &p->direct_descendant) &&
       ReadParam(m, iter, &p->input_long1) &&
       ReadParam(m, iter, &p->input_long2);
   }
@@ -541,8 +539,6 @@ struct ParamTraits<webkit_glue::WebAccessibility::InParams> {
     LogParam(p.function_id, l);
     l->append(L", ");
     LogParam(p.child_id, l);
-    l->append(L", ");
-    LogParam(p.direct_descendant, l);
     l->append(L", ");
     LogParam(p.input_long1, l);
     l->append(L", ");
