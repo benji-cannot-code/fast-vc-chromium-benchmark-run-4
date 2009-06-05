1d588af5b418e0abc2529f1311b9f10a7626e15c
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-05 10:58:16 +0000

    2009-06-05  Gavin Barraclough  <barraclough@apple.com>
            Reviewed by Oliver Hunt.
    
            Remove DoubleNotEqual floating point comparison condition for now -
            it is not used, and it is unclear the semantics are correct (I think
            this comparison would actually give you not-equal-or-unordered, which
            might be what is wanted... we can revisit this interface & get it
            right when required).
    
            Also, fix asserts in branchArith32 ops.  All adds & subs can check
            for Signed, multiply only sets OF so can only check for overflow.
    
            * assembler/MacroAssemblerX86Common.h:
            (JSC::MacroAssemblerX86Common::):
            (JSC::MacroAssemblerX86Common::branchAdd32):
            (JSC::MacroAssemblerX86Common::branchMul32):
            (JSC::MacroAssemblerX86Common::branchSub32):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44461 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/assembler/MacroAssemblerX86Common.h
+++ third_party/WebKit/JavaScriptCore/assembler/MacroAssemblerX86Common.h
@@ -58,7 +58,6 @@ public:
 
     enum DoubleCondition {
         DoubleEqual = X86Assembler::ConditionE,
-        DoubleNotEqual = X86Assembler::ConditionNE,
         DoubleGreaterThan = X86Assembler::ConditionA,
         DoubleGreaterThanOrEqual = X86Assembler::ConditionAE,
         DoubleLessThan = X86Assembler::ConditionB,
@@ -602,35 +601,35 @@ public:
 
     Jump branchAdd32(Condition cond, Imm32 imm, RegisterID dest)
     {
-        ASSERT((cond == Overflow) || (cond == Zero) || (cond == NonZero));
+        ASSERT((cond == Overflow) || (cond == Signed) || (cond == Zero) || (cond == NonZero));
         add32(imm, dest);
         return Jump(m_assembler.jCC(x86Condition(cond)));
     }
     
     Jump branchMul32(Condition cond, RegisterID src, RegisterID dest)
     {
-        ASSERT((cond == Overflow) || (cond == Zero) || (cond == NonZero));
+        ASSERT(cond == Overflow);
         mul32(src, dest);
         return Jump(m_assembler.jCC(x86Condition(cond)));
     }
     
     Jump branchMul32(Condition cond, Imm32 imm, RegisterID src, RegisterID dest)
     {
-        ASSERT((cond == Overflow) || (cond == Zero) || (cond == NonZero));
+        ASSERT(cond == Overflow);
         mul32(imm, src, dest);
         return Jump(m_assembler.jCC(x86Condition(cond)));
     }
     
     Jump branchSub32(Condition cond, RegisterID src, RegisterID dest)
     {
-        ASSERT((cond == Overflow) || (cond == Zero) || (cond == NonZero));
+        ASSERT((cond == Overflow) || (cond == Signed) || (cond == Zero) || (cond == NonZero));
         sub32(src, dest);
         return Jump(m_assembler.jCC(x86Condition(cond)));
     }
     
     Jump branchSub32(Condition cond, Imm32 imm, RegisterID dest)
     {
-        ASSERT((cond == Overflow) || (cond == Zero) || (cond == NonZero));
+        ASSERT((cond == Overflow) || (cond == Signed) || (cond == Zero) || (cond == NonZero));
         sub32(imm, dest);
         return Jump(m_assembler.jCC(x86Condition(cond)));
     }
