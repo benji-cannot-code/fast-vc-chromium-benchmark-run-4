e690993aedba982e83daccedcf5328985732a3d3
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-05 23:09:04 +0000

    Move ConvertAcceleratorsFromWindowsStyle into chrome/common/gtk_util. Use it on the custom startup url buttons.
    BUG=11507
    
    Review URL: http://codereview.chromium.org/118327
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17796 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/gtk_util.h
+++ chrome/common/gtk_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_COMMON_GTK_UTIL_H_
 
 #include <gtk/gtk.h>
+#include <string>
 
 #include "base/gfx/point.h"
 #include "base/gfx/rect.h"
@@ -50,6 +51,10 @@ void InitRCStyles();
 // otherwise expand to fill the vertical space of the hbox (e.g. buttons).
 void CenterWidgetInHBox(GtkWidget* hbox, GtkWidget* widget, bool pack_at_end,
                         int padding);
+
+// Change windows accelerator style to GTK style. (GTK uses _ for
+// accelerators.  Windows uses & with && as an escape for &.)
+std::string ConvertAcceleratorsFromWindowsStyle(const std::string& label);
 
 }  // namespace gtk_util
 
