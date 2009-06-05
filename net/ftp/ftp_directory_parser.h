05f03b01c0086a6d33b0a11325c4fd68ce18e02c
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-05 02:14:16 +0000

    Check in the fourth Portable FTP CL from Ibrar Ahmed <ibrar.ahmad@gmail.com>.
    Now we can browse some ftp sites and can download files.
    There are some TODO yet to be fixed.
    
    Original review: http://codereview.chromium.org/115291
    
    R=wtc
    BUG=http://crbug.com/4965
    TEST=none
    Review URL: http://codereview.chromium.org/118274
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17703 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_directory_parser.h
+++ net/ftp/ftp_directory_parser.h
@@ -49,13 +49,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 struct ListState {
-  int64         now_time;             // needed for year determination.
-  struct tm     now_tm;               // needed for year determination.
-  int           lstyle;               // LISTing style.
-  int           parsed_one;           // returned anything yet?
-  char          carry_buf[84];        // for VMS multiline.
-  unsigned int  carry_buf_len;        // length of name in carry_buf.
-  unsigned int  numlines;             // number of lines seen.
+  void*         magic;          // to determine if previously initialized
+  int64         now_time;       // needed for year determination.
+  struct tm     now_tm;         // needed for year determination.
+  int           lstyle;         // LISTing style.
+  int           parsed_one;     // returned anything yet?
+  char          carry_buf[84];  // for VMS multiline.
+  unsigned int  carry_buf_len;  // length of name in carry_buf.
+  unsigned int  numlines;       // number of lines seen.
 };
 
 enum LineType {
@@ -74,6 +75,7 @@ struct ListResult {
   unsigned int  fe_lnlen;        // length of symlink name
   char          fe_size[40];     // size of file in bytes (<= (2^128 - 1))
   int           fe_cinfs;        // file system is definitely case insensitive
+  // TODO(ibrar): We should use "base::Time::Exploded" instead of "tm"
   struct tm     fe_time;         // last-modified time
 };
 
