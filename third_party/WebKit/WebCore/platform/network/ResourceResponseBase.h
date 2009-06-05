c001404cb999fec558fedecf34e2257d2b7e0bcb
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-05 07:36:17 +0000

    2009-06-05  Antti Koivisto  <antti@apple.com>
            Try to fix Windows (and possibly other platforms) build.
    
            Restore ResourceResponseBase::lastModifiedDate() and setLastModifiedDate() removed in previous commit.
            for now since PluginStream used on some platforms expects them and calculations differ from plain 
            Last-modified header value.
            
            Also include <wtf/MathExtras.h> to get isfinite().
    
            * platform/network/ResourceResponseBase.cpp:
            (WebCore::ResourceResponseBase::adopt):
            (WebCore::ResourceResponseBase::copyData):
            (WebCore::ResourceResponseBase::setLastModifiedDate):
            (WebCore::ResourceResponseBase::lastModifiedDate):
            * platform/network/ResourceResponseBase.h:
            * platform/network/cf/ResourceResponseCFNet.cpp:
            (WebCore::ResourceResponse::platformLazyInit):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceResponseBase.h
+++ third_party/WebKit/WebCore/platform/network/ResourceResponseBase.h
@@ -78,6 +78,11 @@ public:
     bool isMultipart() const { return mimeType() == "multipart/x-mixed-replace"; }
 
     bool isAttachment() const;
+    
+    // FIXME: These are used by PluginStream on some platforms. Calculations may differ from just returning plain Last-odified header.
+    // Leaving it for now but this should go away in favor of generic solution.
+    void setLastModifiedDate(time_t);
+    time_t lastModifiedDate() const; 
 
     // These functions return parsed values of the corresponding response headers.
     // NaN means that the header was not present or had invalid value.
@@ -118,6 +123,7 @@ protected:
     int m_httpStatusCode;
     String m_httpStatusText;
     HTTPHeaderMap m_httpHeaderFields;
+    time_t m_lastModifiedDate;
 
     bool m_isNull : 1;
     
@@ -152,6 +158,7 @@ struct CrossThreadResourceResponseData {
     int m_httpStatusCode;
     String m_httpStatusText;
     OwnPtr<CrossThreadHTTPHeaderMapData> m_httpHeaders;
+    time_t m_lastModifiedDate;
 };
 
 } // namespace WebCore
