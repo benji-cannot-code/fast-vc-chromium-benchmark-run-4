76810d046e96d9f9eeee9099f291c52dba9220fa
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-05 10:13:44 +0000

    DevTools: Make reloading inspector lead to the complete DevTools Client Window reopening.
    BUG=12956
    Review URL: http://codereview.chromium.org/119178
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17725 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_manager.h
+++ chrome/browser/debugger/devtools_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 
+#include "base/ref_counted.h"
 #include "chrome/browser/debugger/devtools_client_host.h"
 
 namespace IPC {
@@ -19,7 +20,8 @@ class RenderViewHost;
 
 // This class is a singleton that manages DevToolsClientHost instances and
 // routes messages between developer tools clients and agents.
-class DevToolsManager : public DevToolsClientHost::CloseListener {
+class DevToolsManager : public DevToolsClientHost::CloseListener,
+                        public base::RefCounted<DevToolsManager> {
  public:
   DevToolsManager();
   virtual ~DevToolsManager();
@@ -70,6 +72,8 @@ private:
 
   void EnableDevToolsInPrefs(RenderViewHost* inspected_rvh);
 
+  void ForceReopenWindow();
+
   // These two maps are for tracking dependencies between inspected tabs and
   // their DevToolsClientHosts. They are usful for routing devtools messages
   // and allow us to have at most one devtools client host per tab. We use
@@ -85,6 +89,7 @@ private:
   typedef std::map<DevToolsClientHost*, RenderViewHost*>
       ClientHostToInspectedRvhMap;
   ClientHostToInspectedRvhMap client_host_to_inspected_rvh_;
+  RenderViewHost* inspected_rvh_for_reopen_;
 
   DISALLOW_COPY_AND_ASSIGN(DevToolsManager);
 };
