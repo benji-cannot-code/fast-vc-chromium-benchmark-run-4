c61516153ef538d5ef190c00996933f48512a5c8
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-05 14:00:20 +0000

    2009-06-03  Ben Murdoch  <benm@google.com>
            <https://bugs.webkit.org/show_bug.cgi?id=25710> HTML5 Database stops executing transactions if the URL hash changes while a transaction is open and an XHR is in progress.
    
            Reviewed by Alexey Proskuryakov.
    
            Fix a bug that causes database transactions to fail if a history navigation to a hash fragment of the same document is made while resources (e.g. an XHR) are loading
            https://bugs.webkit.org/show_bug.cgi?id=25710
    
            Test: storage/hash-change-with-xhr.html
    
            * loader/DocumentLoader.cpp:
            (WebCore::DocumentLoader::stopLoading):
            * loader/DocumentLoader.h:
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::stopLoading):
            (WebCore::FrameLoader::stopAllLoaders):
            * loader/FrameLoader.h:
            * loader/FrameLoaderTypes.h:
            (WebCore::):
            * page/Page.cpp:
            (WebCore::Page::goToItem):
            * WebCore.base.exp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44468 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoaderTypes.h
+++ third_party/WebKit/WebCore/loader/FrameLoaderTypes.h
@@ -67,6 +67,11 @@ namespace WebCore {
         NavigationTypeOther
     };
 
+    enum DatabasePolicy {
+        DatabasePolicyStop,    // The database thread should be stopped and database connections closed.
+        DatabasePolicyContinue
+    };
+
     enum ObjectContentType {
         ObjectContentNone,
         ObjectContentImage,
