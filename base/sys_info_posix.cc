73e778c10ca2f6686711784f19d554a44ed4ef16
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-05 01:21:09 +0000

    sysconf(_SC_NPROCESSORS_ONLN) is not available on some POSIX systems, use sysctl(3) for OpenBSD
    Review URL: http://codereview.chromium.org/118289
    Patch from Peter Valchev <pvalchev@google.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17695 0039d316-1c4b-4281-b951-d872f2087c98

--- base/sys_info_posix.cc
+++ base/sys_info_posix.cc
@@ -17,12 +17,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <mach/mach_init.h>
 #endif
 
+#if defined(OS_OPENBSD)
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#endif
+
 #include "base/logging.h"
 #include "base/string_util.h"
 
 namespace base {
 
 int SysInfo::NumberOfProcessors() {
+#if defined(OS_OPENBSD)
+  int mib[] = { CTL_HW, HW_NCPU };
+  int ncpu;
+  size_t size = sizeof(ncpu);
+  if (sysctl(mib, 2, &ncpu, &size, NULL, 0) == -1) {
+    NOTREACHED();
+    return 1;
+  }
+  return ncpu;
+#else
   // It seems that sysconf returns the number of "logical" processors on both
   // mac and linux.  So we get the number of "online logical" processors.
   static long res = sysconf(_SC_NPROCESSORS_ONLN);
@@ -32,6 +47,7 @@ int SysInfo::NumberOfProcessors() {
   }
 
   return static_cast<int>(res);
+#endif
 }
 
 // static
