e5fba8a7d75b96fa016045169087a89947f7a712
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-08 17:45:11 +0000

    Remove a header that is no longer being used after r17818.
    TBR=thestig
    Review URL: http://codereview.chromium.org/118389
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17874 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/tabs/dragged_tab_gtk.h
+++ chrome/browser/gtk/tabs/dragged_tab_gtk.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gfx/size.h"
 #include "base/scoped_ptr.h"
 #include "base/task.h"
-#include "chrome/common/owned_widget_gtk.h"
 
 class ChromeCanvas;
 class TabContents;
