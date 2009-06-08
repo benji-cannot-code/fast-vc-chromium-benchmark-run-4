ba2c477adce8f8470b89d35bde5dc711deda4119
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-08 23:33:23 +0000

    Moving the Windows specific activation code in the FocusManager to the widget_win.
    BUG=None
    TEST=Test that the focus in the browser works well: that windows remember which view had focus when deactivated/reactivated. Make sure focus traversal works as expected.
    Review URL: http://codereview.chromium.org/118406
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17911 0039d316-1c4b-4281-b951-d872f2087c98

--- views/focus/focus_manager.cc
+++ views/focus/focus_manager.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "views/focus/focus_manager.h"
+
 #include <algorithm>
 
 #include "build/build_config.h"
@@ -14,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/histogram.h"
 #include "base/logging.h"
 #include "views/accelerator.h"
-#include "views/focus/focus_manager.h"
 #include "views/focus/view_storage.h"
 #include "views/view.h"
 #include "views/widget/root_view.h"
@@ -79,20 +80,6 @@ static LRESULT CALLBACK FocusWindowCallback(HWND window, UINT message,
         if (!focus_manager->OnNCDestroy(window))
           return 0;
         break;
-      case WM_ACTIVATE: {
-        // We call the DefWindowProc before calling OnActivate as some of our
-        // windows need the OnActivate notifications.  The default activation on
-        // the window causes it to focus the main window, and since
-        // FocusManager::OnActivate attempts to restore the focused view, it
-        // needs to be called last so the focus it is setting does not get
-        // overridden.
-        LRESULT result = CallWindowProc(original_handler, window, WM_ACTIVATE,
-                                        wParam, lParam);
-        if (!focus_manager->OnPostActivate(window,
-                                           LOWORD(wParam), HIWORD(wParam)))
-          return 0;
-        return result;
-      }
       default:
         break;
     }
@@ -335,17 +322,6 @@ bool FocusManager::OnKeyUp(HWND window, UINT message, WPARAM wparam,
   }
 
   return true;
-}
-
-bool FocusManager::OnPostActivate(HWND window,
-                                  int activation_state,
-                                  int minimized_state) {
-  if (WA_INACTIVE == LOWORD(activation_state)) {
-    StoreFocusedView();
-  } else {
-    RestoreFocusedView();
-  }
-  return false;
 }
 #endif
 
