cd70a130c9282bc4c27a9d7a3d0cafaa4cb59eff
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-08 17:26:01 +0000

    GTK bookmark manager: organize menu and aesthetic improvements.
    Review URL: http://codereview.chromium.org/118388
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17868 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_utils.cc
+++ chrome/browser/bookmarks/bookmark_utils.cc
@@ -365,7 +365,6 @@ void CopyToClipboard(BookmarkModel* model,
 #else
   // TODO(port): Clipboard integration.  Don't we have clipboard
   // implemented somewhere else?
-  NOTIMPLEMENTED();
 #endif
 
   if (remove_nodes) {
@@ -397,7 +396,6 @@ void PasteFromClipboard(BookmarkModel* model,
   bookmark_utils::CloneDragData(model, bookmark_data.elements, parent, index);
 #else
   // TODO(port): Clipboard integration.
-  NOTIMPLEMENTED();
 #endif
 }
 
@@ -415,7 +413,6 @@ bool CanPasteFromClipboard(BookmarkNode* node) {
   return bookmark_data.Read(data_wrapper);
 #else
   // TODO(port): Clipboard integration.
-  NOTIMPLEMENTED();
   return false;
 #endif
 }
