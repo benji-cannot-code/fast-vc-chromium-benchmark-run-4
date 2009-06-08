c282e7d95ed5c6a14fccd1d6e8de246aef068e95
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-08 20:47:43 +0000

    2009-06-07  David Kilzer  <ddkilzer@apple.com>
            Make JavaScriptCore compile for iPhone and iPhone Simulator
    
            Reviewed by Gavin Barraclough.
    
            * Configurations/Base.xcconfig: Split GCC_ENABLE_OBJC_GC on
            $(REAL_PLATFORM_NAME).  Added $(ARCHS_UNIVERSAL_IPHONE_OS) to
            VALID_ARCHS.  Added REAL_PLATFORM_NAME_iphoneos,
            REAL_PLATFORM_NAME_iphonesimulator, HAVE_DTRACE_iphoneos and
            HAVE_DTRACE_iphonesimulator variables.
            * Configurations/DebugRelase.xcconfig: Split ARCHS definition on
            $(REAL_PLATFORM_NAME).
            * Configurations/JavaScriptCore.xcconfig: Added
            EXPORTED_SYMBOLS_FILE_armv6 and EXPORTED_SYMBOLS_FILE_armv7
            variables.  Split OTHER_LDFLAGS into OTHER_LDFLAGS_BASE and
            OTHER_LDFLAGS_$(REAL_PLATFORM_NAME) since CoreServices.framework
            is only linked to on Mac OS X.
            * JavaScriptCore.xcodeproj/project.pbxproj: Removed references
            to CoreServices.framework since it's linked using OTHER_LDFLAGS
            in JavaScriptCore.xcconfig.
            * profiler/ProfilerServer.mm: Added #import for iPhone
            Simulator.
            (-[ProfilerServer init]): Conditionalize use of
            NSDistributedNotificationCenter to non-iPhone or iPhone
            Simulator.
            * wtf/FastMalloc.cpp:
            (WTF::TCMallocStats::): Build fix for iPhone and iPhone
            Simulator.
            * wtf/Platform.h: Defined PLATFORM(IPHONE) and
            PLATFORM(IPHONE_SIMULATOR).
            * wtf/ThreadingPthreads.cpp:
            (WTF::setThreadNameInternal): Build fix for iPhone and iPhone
            Simulator.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44504 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/profiler/ProfilerServer.mm
+++ third_party/WebKit/JavaScriptCore/profiler/ProfilerServer.mm
@@ -29,8 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "JSProfilerPrivate.h"
 #import "JSRetainPtr.h"
-
 #import <Foundation/Foundation.h>
+
+#if PLATFORM(IPHONE_SIMULATOR)
+#import <Foundation/NSDistributedNotificationCenter.h>
+#endif
 
 @interface ProfilerServer : NSObject {
 @private
@@ -63,16 +66,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if ([defaults boolForKey:@"EnableJSProfiling"])
         [self startProfiling];
 
+#if !PLATFORM(IPHONE) || PLATFORM(IPHONE_SIMULATOR)
+    // FIXME: <rdar://problem/6546135>
     // The catch-all notifications
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(startProfiling) name:@"ProfilerServerStartNotification" object:nil];
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(stopProfiling) name:@"ProfilerServerStopNotification" object:nil];
+#endif
 
     // The specific notifications
     NSProcessInfo *processInfo = [NSProcessInfo processInfo];
     _serverName = [[NSString alloc] initWithFormat:@"ProfilerServer-%d", [processInfo processIdentifier]];
 
+#if !PLATFORM(IPHONE) || PLATFORM(IPHONE_SIMULATOR)
+    // FIXME: <rdar://problem/6546135>
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(startProfiling) name:[_serverName stringByAppendingString:@"-Start"] object:nil];
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(stopProfiling) name:[_serverName stringByAppendingString:@"-Stop"] object:nil];
+#endif
 
     [pool drain];
 
