788c37252d42c97683bc81c61f8b776e73ba5399
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-08 17:27:10 +0000

    Crash in a CHECK if we fail to load a data pack file.  Running w/o our resources is crazy talk.
    We've had 20 crashes here from 7 users, hopefully this will gives us some more info.
    
    Review URL: http://codereview.chromium.org/118326
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17869 0039d316-1c4b-4281-b951-d872f2087c98

--- base/data_pack.cc
+++ base/data_pack.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/data_pack.h"
 
+#include <errno.h>
+
 #include "base/file_util.h"
 #include "base/logging.h"
 #include "base/string_piece.h"
@@ -49,8 +51,8 @@ DataPack::~DataPack() {
 bool DataPack::Load(const FilePath& path) {
   mmap_.reset(new file_util::MemoryMappedFile);
   if (!mmap_->Initialize(path)) {
-    mmap_.reset();
-    return false;
+    CHECK(false) << "Failed to mmap " << path.value() << " errno: " <<
+        strerror(errno);
   }
 
   // Parse the header of the file.
