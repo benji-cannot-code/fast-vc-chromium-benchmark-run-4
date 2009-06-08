03116ffe511dd16d4683ce22b74e6b39ab02f1c8
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-08 23:16:04 +0000

    2009-06-08  Dimitri Glazkov  <dglazkov@chromium.org>
            Unreviewed, projectile-fixing Windows build.
    
            * runtime/DateConversion.cpp: Added StringExtras include.
            * wtf/DateMath.cpp: Replaced math with algorithm include (looking for std::min def for Windows).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/DateConversion.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/DateConversion.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "UString.h"
 #include <wtf/DateMath.h>
+#include <wtf/StringExtras.h>
 
 using namespace WTF;
 
--- third_party/WebKit/JavaScriptCore/wtf/DateMath.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/DateMath.cpp
@@ -50,8 +50,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MathExtras.h"
 #include "StringExtras.h"
 
+#include <algorithm>
 #include <limits>
-#include <math.h>
 #include <stdint.h>
 #include <time.h>
 
