09300d174da40ee0db9c73c6fae788645ad0a988
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-09 09:00:20 +0000

    2009-06-09  Simon Hausmann  <simon.hausmann@nokia.com>
            Fix the Qt build, add missing function declaration.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44530 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/qt/jsobjects.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/qt/jsobjects.h
@@ -95,6 +95,7 @@ public slots:
     bool pauseAnimationAtTimeOnElementWithId(const QString &animationName, double time, const QString &elementId);
     bool pauseTransitionAtTimeOnElementWithId(const QString &propertyName, double time, const QString &elementId);
     unsigned numberOfActiveAnimations() const;
+    void dispatchPendingLoadRequests();
 
 private slots:
     void processWork();
