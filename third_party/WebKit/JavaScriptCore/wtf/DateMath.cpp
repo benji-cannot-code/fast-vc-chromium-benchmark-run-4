3c92ac4c77bcdfea2d2228660e06807bdb4310fd
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-09 04:48:45 +0000

    Speculative GTK build fix.
    * wtf/DateMath.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44518 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/DateMath.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/DateMath.cpp
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "StringExtras.h"
 
 #include <algorithm>
+#include <limits.h>
 #include <limits>
 #include <stdint.h>
 #include <time.h>
