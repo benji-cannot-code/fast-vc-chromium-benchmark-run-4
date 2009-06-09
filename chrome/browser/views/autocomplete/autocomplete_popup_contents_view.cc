49152fbc84e32abf7d08302df0ce7a0d869d0d1b
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-09 02:13:07 +0000

    Fix weird stretchy text rendering in O2 popup. Apparently the tile flags were angering the skia gods.
    http://crbug.com/13277
    TEST=size window to various sizes, type stuff in omnibox, text shouldn't be stretched or scrunched.
    Review URL: http://codereview.chromium.org/118414
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17921 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/autocomplete/autocomplete_popup_contents_view.cc
+++ chrome/browser/views/autocomplete/autocomplete_popup_contents_view.cc
@@ -795,8 +795,8 @@ void AutocompletePopupContentsView::PaintChildren(gfx::Canvas* canvas) {
 
   SkShader* shader = SkShader::CreateBitmapShader(
       contents_canvas.getDevice()->accessBitmap(false),
-      SkShader::kRepeat_TileMode,
-      SkShader::kRepeat_TileMode);
+      SkShader::kClamp_TileMode,
+      SkShader::kClamp_TileMode);
   paint.setShader(shader);
   shader->unref();
 
