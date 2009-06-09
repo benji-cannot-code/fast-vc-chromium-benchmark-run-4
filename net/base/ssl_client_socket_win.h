22012ee4a7ebbdc9d1bdb80f5eb7492de327c0d8
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-09 01:37:27 +0000

    We don't handle certificate errors during SSL renegotiation. In the common case, the server sends the same certificate during renegotiation.  Since the certificate has been verified, we can assume the certificate is good or has been accepted by the user. If the server sends a different certificate that has an error, we need to return an error code that won't trigger our certificate error handling code, which doesn't handle this case correctly.  Add the ERR_CERT_ERROR_IN_SSL_RENEGOTIATION error for this purpose.
    R=rvargas
    BUG=http://crbug.com/13226
    TEST=See http://crbug.com/13226 comment 9
    Review URL: http://codereview.chromium.org/118410
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17919 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/ssl_client_socket_win.h
+++ net/base/ssl_client_socket_win.h
@@ -67,6 +67,7 @@ class SSLClientSocketWin : public SSLClientSocket {
 
   int DidCallInitializeSecurityContext();
   int DidCompleteHandshake();
+  void DidCompleteRenegotiation(int result);
   void LogConnectionTypeMetrics() const;
   void SetNextStateForRead();
   void FreeSendBuffer();
