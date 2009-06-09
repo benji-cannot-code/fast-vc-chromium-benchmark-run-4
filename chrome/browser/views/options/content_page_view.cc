25079a748452f292543136826c441d30af88242f
mhm@chromium.org <mhm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-09 05:55:19 +0000

    Implement different radio groups for Options > Personal Stuff
    Passwords and Form autofill share the same group, they need to be treated separately. It did not work correctly, when "password" radio option was chosen,two of 
    "form autofill" radio option both change to blank(not selected). This patch will properly create different radio groups for passwords and autofill.
    
    BUG=13222 (http://crbug.com/13222)
    
    TEST=Radio buttons now function propertly
    
    TBR=ben
    Review URL: http://codereview.chromium.org/119347
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17932 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/options/content_page_view.cc
+++ chrome/browser/views/options/content_page_view.cc
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kPopupBlockingRadioGroup = 1;
-const int kPasswordSavingRadioGroup = 2;
+const int kPasswordSavingRadioGroup = 1;
+const int kFormAutofillRadioGroup = 2;
 }  // namespace
 
 ContentPageView::ContentPageView(Profile* profile)
@@ -76,7 +76,7 @@ void ContentPageView::ButtonPressed(views::Button* sender) {
       UserMetricsRecordAction(L"Options_FormAutofill_Disable",
                               profile()->GetPrefs());
     }
-    form_autofill_.SetValue(enabled);
+    ask_to_save_form_autofill_.SetValue(enabled);
   } else if (sender == passwords_exceptions_button_) {
     UserMetricsRecordAction(L"Options_ShowPasswordsExceptions", NULL);
     PasswordsExceptionsWindowView::Show(profile());
@@ -134,7 +134,8 @@ void ContentPageView::InitControlLayout() {
   // Init member prefs so we can update the controls if prefs change.
   ask_to_save_passwords_.Init(prefs::kPasswordManagerEnabled,
                               profile()->GetPrefs(), this);
-  form_autofill_.Init(prefs::kFormAutofillEnabled, profile()->GetPrefs(), this);
+  ask_to_save_form_autofill_.Init(prefs::kFormAutofillEnabled,
+                                  profile()->GetPrefs(), this);
 }
 
 void ContentPageView::NotifyPrefChanged(const std::wstring* pref_name) {
@@ -146,7 +147,7 @@ void ContentPageView::NotifyPrefChanged(const std::wstring* pref_name) {
     }
   }
   if (!pref_name || *pref_name == prefs::kFormAutofillEnabled) {
-    if (form_autofill_.GetValue()) {
+    if (ask_to_save_form_autofill_.GetValue()) {
       form_autofill_asktosave_radio_->SetChecked(true);
     } else {
       form_autofill_neversave_radio_->SetChecked(true);
@@ -216,12 +217,12 @@ void ContentPageView::InitPasswordSavingGroup() {
 void ContentPageView::InitFormAutofillGroup() {
   form_autofill_asktosave_radio_ = new views::RadioButton(
       l10n_util::GetString(IDS_OPTIONS_AUTOFILL_SAVE),
-      kPasswordSavingRadioGroup);
+      kFormAutofillRadioGroup);
   form_autofill_asktosave_radio_->set_listener(this);
   form_autofill_asktosave_radio_->SetMultiLine(true);
   form_autofill_neversave_radio_ = new views::RadioButton(
       l10n_util::GetString(IDS_OPTIONS_AUTOFILL_NEVERSAVE),
-      kPasswordSavingRadioGroup);
+      kFormAutofillRadioGroup);
   form_autofill_neversave_radio_->set_listener(this);
   form_autofill_neversave_radio_->SetMultiLine(true);
 
