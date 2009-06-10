319a749903d31cb912e1447623523d5dbeab26d6
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-10 21:19:58 +0000

    Fix a crash in linux if the renderer process dies too fast.  On mac, we just don't get the sad tab page.
    If the renderer dies before it sends a ViewHostMsg_RenderViewReady message,
    we never get the TAB_CONTENTS_CONNECTED, which in turn means that
    TAB_CONTENTS_DISCONNECTED doesn't fire.  If TAB_CONTENTS_DISCONNECTED
    doesn't fire, we never initialize sad_tab_ in tab_contents_view_gtk.cc and
    crash the browser process.
    
    I found this crash on the crash server:
    http://crash/reportdetail?
    reportid=fb3a7bc60c67eb1e&product=Chrome_Linux&version=3.0.183.1&date=&signa
    ture=TabContentsViewGtk::Invalidate()-534CC7
    
    BUG=13715
    
    Review URL: http://codereview.chromium.org/119395
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18087 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents_view_gtk.cc
+++ chrome/browser/tab_contents/tab_contents_view_gtk.cc
@@ -106,8 +106,6 @@ TabContentsViewGtk::TabContentsViewGtk(TabContents* tab_contents)
   gtk_widget_show(fixed_.get());
   registrar_.Add(this, NotificationType::TAB_CONTENTS_CONNECTED,
                  Source<TabContents>(tab_contents));
-  registrar_.Add(this, NotificationType::TAB_CONTENTS_DISCONNECTED,
-                 Source<TabContents>(tab_contents));
 }
 
 TabContentsViewGtk::~TabContentsViewGtk() {
@@ -194,8 +192,12 @@ void TabContentsViewGtk::SetPageTitle(const std::wstring& title) {
     gdk_window_set_title(content_view->window, WideToUTF8(title).c_str());
 }
 
-void TabContentsViewGtk::Invalidate() {
-  gtk_widget_queue_draw(sad_tab_->widget());
+void TabContentsViewGtk::OnTabCrashed() {
+  if (!sad_tab_.get()) {
+    sad_tab_.reset(new SadTabGtk);
+    InsertIntoContentArea(sad_tab_->widget());
+    gtk_widget_show(sad_tab_->widget());
+  }
 }
 
 void TabContentsViewGtk::SizeContents(const gfx::Size& size) {
@@ -272,12 +274,6 @@ void TabContentsViewGtk::Observe(NotificationType type,
       // the new RenderWidgetHostViewGtk instance already removed all the
       // vbox's children.
       sad_tab_.reset();
-      break;
-    }
-    case NotificationType::TAB_CONTENTS_DISCONNECTED: {
-      sad_tab_.reset(new SadTabGtk);
-      InsertIntoContentArea(sad_tab_->widget());
-      gtk_widget_show(sad_tab_->widget());
       break;
     }
     default:
