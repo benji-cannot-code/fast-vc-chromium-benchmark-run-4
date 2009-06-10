d9ba96250e5104d1328d35c5ee1bce72caf4e811
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-10 16:36:26 +0000

    Try2 at disabling the thumbnail store unit tests. The previous try didn't work because the friend declarations depended on the exact name of the tests, which I changed when I marked them disabled. Review URL: http://codereview.chromium.org/119426
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18064 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/thumbnail_store_unittest.cc
+++ chrome/browser/thumbnail_store_unittest.cc
@@ -115,7 +115,10 @@ void ThumbnailStoreTest::PrintPixelDiff(SkBitmap* image_a, SkBitmap* image_b) {
             << maxv[3] << ")" << std::endl;
 }
 
-TEST_F(ThumbnailStoreTest, DISABLED_RetrieveFromCache) {
+// TODO(meelapshah) fix the leak in these tests and re-enable.
+#if 0
+
+TEST_F(ThumbnailStoreTest, RetrieveFromCache) {
   RefCountedBytes* read_image = NULL;
   scoped_refptr<ThumbnailStore> store = new ThumbnailStore;
 
@@ -139,7 +142,7 @@ TEST_F(ThumbnailStoreTest, DISABLED_RetrieveFromCache) {
   read_image->Release();
 }
 
-TEST_F(ThumbnailStoreTest, DISABLED_RetrieveFromDisk) {
+TEST_F(ThumbnailStoreTest, RetrieveFromDisk) {
   scoped_refptr<RefCountedBytes> read_image = new RefCountedBytes;
   scoped_refptr<ThumbnailStore> store = new ThumbnailStore;
 
@@ -160,3 +163,4 @@ TEST_F(ThumbnailStoreTest, DISABLED_RetrieveFromDisk) {
   EXPECT_EQ(0, memcmp(&read_image->data[0], &jpeg_image_->data[0],
                       jpeg_image_->data.size()));
 }
+#endif
