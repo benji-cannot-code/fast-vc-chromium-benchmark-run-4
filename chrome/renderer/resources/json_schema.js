e434fa9e7fa003fe4668747039158f911e5acf59
erikkay@google.com <erikkay@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-10 15:10:55 +0000

    Clean up bookmark API to match style of other extension APIs
    BUG=11823
    TEST=--load-extension test/data/extensions/samples/bookmarks
    TEST=unit_tests.exe --gtest_filter=ExtensionAPIClientTest.*
    
    Review URL: http://codereview.chromium.org/118209
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18056 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/json_schema.js
+++ chrome/renderer/resources/json_schema.js
@@ -377,7 +377,7 @@ chrome.JSONSchemaValidator.prototype.addError = function(path, key,
     return {
       choice: [
         type,
-        { type: "array",  item: type }
+        { type: "array",  item: type,  minItems: 1 }
       ]
     };
   };
