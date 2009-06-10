2a8532407a6c678ce28ca1d1717679cbaf2727e4
mpcomplete@google.com <mpcomplete@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-10 00:06:47 +0000

    Change the extension callback system to work more like events, where we track contexts rather than frames.
    Also change the way we call through to javascript, to avoid a v8::Compile.
    This is so we don't skew the histogram stats on our script cache.
    
    BUG=?
    TEST=none
    Review URL: http://codereview.chromium.org/119369
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18001 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/bindings_utils.h
+++ chrome/renderer/extensions/bindings_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/resource_bundle.h"
 #include "base/singleton.h"
 #include "base/string_piece.h"
+#include "v8/include/v8.h"
 
 #include <string>
 
@@ -32,5 +33,12 @@ const char* GetStringResource() {
 // Returns the current RenderView, based on which V8 context is current.  It is
 // an error to call this when not in a V8 context.
 RenderView* GetRenderViewForCurrentContext();
+
+// Call the named javascript function with the given arguments in a context.
+// The function name should be reachable from the global object, and can be a
+// sub-property like "chrome.handleResponse_".
+void CallFunctionInContext(v8::Handle<v8::Context> context,
+                           const std::string& function_name, int argc,
+                           v8::Handle<v8::Value>* argv);
 
 #endif  // CHROME_RENDERER_EXTENSIONS_BINDINGS_UTILS_H_
