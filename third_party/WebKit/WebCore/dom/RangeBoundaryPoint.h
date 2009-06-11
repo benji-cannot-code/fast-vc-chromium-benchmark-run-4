a179885f26aecb4427b9dd4cac522ccdec70817d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-11 21:20:01 +0000

    Speculative fix
    git-svn-id: svn://svn.chromium.org/blink/trunk@44617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/RangeBoundaryPoint.h
+++ third_party/WebKit/WebCore/dom/RangeBoundaryPoint.h
@@ -34,7 +34,6 @@ namespace WebCore {
 
 class RangeBoundaryPoint {
 public:
-    RangeBoundaryPoint();
     explicit RangeBoundaryPoint(PassRefPtr<Node> container);
 
     const Position toPosition() const;
@@ -64,17 +63,12 @@ private:
     Node* m_childBeforeBoundary;
 };
 
-inline RangeBoundaryPoint::RangeBoundaryPoint()
-    : m_offsetInContainer(0)
-    , m_childBeforeBoundary(0)
-{
-}
-
 inline RangeBoundaryPoint::RangeBoundaryPoint(PassRefPtr<Node> container)
     : m_containerNode(container)
     , m_offsetInContainer(0)
     , m_childBeforeBoundary(0)
 {
+    ASSERT(m_containerNode);
 }
 
 inline Node* RangeBoundaryPoint::container() const
@@ -117,6 +111,7 @@ inline void RangeBoundaryPoint::clear()
 
 inline void RangeBoundaryPoint::set(PassRefPtr<Node> container, int offset, Node* childBefore)
 {
+    ASSERT(container);
     ASSERT(offset >= 0);
     ASSERT(childBefore == (offset ? container->childNode(offset - 1) : 0));
     m_containerNode = container;
