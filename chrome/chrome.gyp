e7a2cac2898390f4be773b36f434d0cc2e4b1700
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-11 21:25:12 +0000

    Compile sunspider on mac and linux. Review URL: http://codereview.chromium.org/126003
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18201 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -3091,7 +3091,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'test/accessibility/tab_impl.cc',
             'test/ui/npapi_uitest.cc',
             'test/ui/sandbox_uitests.cc',
-            'test/ui/sunspider_uitest.cc',
             'worker/worker_uitest.cc',
           ],
         }],
--- chrome/test/ui/sunspider_uitest.cc
+++ chrome/test/ui/sunspider_uitest.cc
@@ -126,13 +126,17 @@ class SunSpiderReferenceTest : public SunSpiderTest {
     FilePath dir;
     PathService::Get(chrome::DIR_TEST_TOOLS, &dir);
     dir = dir.AppendASCII("reference_build");
+#if defined(OS_WIN)
     dir = dir.AppendASCII("chrome");
+#elif defined(OS_LINUX)
+    dir = dir.AppendASCII("chrome_linux");
+#elif defined(OS_MACOSX)
+    dir = dir.AppendASCII("chrome_mac");
+#endif
     browser_directory_ = dir;
     UITest::SetUp();
   }
 };
-
-}  // namespace
 
 TEST_F(SunSpiderTest, Perf) {
   if (!CommandLine::ForCurrentProcess()->HasSwitch(kRunSunSpider))
@@ -147,3 +151,5 @@ TEST_F(SunSpiderReferenceTest, Perf) {
 
   RunTest();
 }
+
+}  // namespace
