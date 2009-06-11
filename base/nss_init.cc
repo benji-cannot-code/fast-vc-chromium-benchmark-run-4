2065fcc97ca2116e5a408d2bc8e1961ea7688875
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-11 18:48:29 +0000

    Initialize an empty password for NSS databases (so that the databases won't require login) if the password hasn't been initialized yet.
    R=ukai
    BUG=http://crbug.com/13699
    TEST=none
    Review URL: http://codereview.chromium.org/119381
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18185 0039d316-1c4b-4281-b951-d872f2087c98

--- base/nss_init.cc
+++ base/nss_init.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Work around https://bugzilla.mozilla.org/show_bug.cgi?id=455424
 // until NSS 3.12.2 comes out and we update to it.
 #define Lock FOO_NSS_Lock
+#include <pk11pub.h>
 #include <secmod.h>
 #include <ssl.h>
 #undef Lock
@@ -77,6 +78,16 @@ class NSSInitSingleton {
         PR_GetErrorText(buffer);
 
       NOTREACHED() << "Error initializing NSS: " << buffer;
+    }
+
+    // If we haven't initialized the password for the NSS databases,
+    // initialize an empty-string password so that we don't need to
+    // log in.
+    PK11SlotInfo* slot = PK11_GetInternalKeySlot();
+    if (slot) {
+      if (PK11_NeedUserInit(slot))
+        PK11_InitPin(slot, NULL, NULL);
+      PK11_FreeSlot(slot);
     }
 
     root_ = InitDefaultRootCerts();
