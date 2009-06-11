a4d3cf165e6bf231c4c8396b5c9c15b23a854b70
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-11 21:53:55 +0000

    Change DnsMaster to only access HostResolver only on the IO thread. And plumb through an (unused) HostResolver dependency into DnsMaster.
    This is in preparation for <http://codereview.chromium.org/118100> which turns HostResolver into a (non-thread-safe) shared dependency of the URLRequestContext.
    
    After CL 118100, DnsMaster will be issuing host resolves through the main URLRequestContext's HostResolver (now on the IO thread). This will have the effect of warming the shared HostResolver's internal cache.
    
    BUG=13163
    TEST=existing unit tests
    
    Review URL: http://codereview.chromium.org/122028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18212 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/dns_global.h
+++ chrome/browser/net/dns_global.h
@@ -18,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class PrefService;
 
+namespace net {
+class HostResolver;
+}
+
 namespace chrome_browser_net {
 
 // Initialize dns prefetching subsystem. Must be called before any other
@@ -31,6 +35,14 @@ void EnsureDnsPrefetchShutdown();
 // Free all resources allocated by InitDnsPrefetch. After that you must not call
 // any function from this file.
 void FreeDnsPrefetchResources();
+
+// Lazily allocates a HostResolver to be used by the DNS prefetch system, on
+// the IO thread. Must be matched by a call to FreeGlobalHostResolver().
+net::HostResolver* GetGlobalHostResolver();
+
+// Frees the HostResolver allocated by GetGlobalHostResolver(). Must be called
+// on the IO thread.
+void FreeGlobalHostResolver();
 
 //------------------------------------------------------------------------------
 // Global APIs relating to Prefetching in browser
@@ -63,7 +75,7 @@ class DnsPrefetcherInit {
   // OS cache before all resource loading is complete.  This is the default.
   static const size_t kMaxConcurrentLookups;
 
-  explicit DnsPrefetcherInit(size_t max_concurrent, PrefService* user_prefs) {
+  DnsPrefetcherInit(size_t max_concurrent, PrefService* user_prefs) {
     InitDnsPrefetch(max_concurrent, user_prefs);
   }
 
