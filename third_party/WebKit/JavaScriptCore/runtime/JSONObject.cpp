1c9df15a54277a055cbccbd70595f57d4e1ef8fb
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-11 18:47:13 +0000

    Lower stringify recursion limit to deal with small windows stack.
    git-svn-id: svn://svn.chromium.org/blink/trunk@44610 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSONObject.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSONObject.cpp
@@ -81,7 +81,7 @@ class Stringifier {
     typedef UString StringBuilder;
     // <https://bugs.webkit.org/show_bug.cgi?id=26276> arbitrary limits for recursion
     // are bad
-    enum { MaxObjectDepth = 1600 };
+    enum { MaxObjectDepth = 512 };
 public:
     Stringifier(ExecState* exec, JSValue replacer, const UString& gap)
         : m_exec(exec)
