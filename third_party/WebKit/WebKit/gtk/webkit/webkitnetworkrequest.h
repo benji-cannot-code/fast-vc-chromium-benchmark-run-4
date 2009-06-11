f51c0f94ac3f5fbea6ca5f2a04807b60d610a588
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-11 02:48:29 +0000

    2009-06-10  Gustavo Noronha Silva  <gns@gnome.org>
            Reviewed by Xan Lopez.
    
            Add unit tests for our WebKitNetworkRequest object.
    
            * GNUmakefile.am:
    
    WebKit/gtk
    
    2009-06-10  Gustavo Noronha Silva  <gns@gnome.org>
    
            Reviewed by Xan Lopez.
    
            Make WebKitNetworkRequest a proper GObject, making URI and
            SoupMessage properties. Also adding unit tests for creation and
            destruction.
    
            * tests/testnetworkrequest.c: Added.
            (test_network_request_create_destroy):
            (test_network_request_properties):
            (main):
            * webkit/webkitnetworkrequest.cpp:
            (webkit_network_request_get_property):
            (webkit_network_request_set_property):
            (webkit_network_request_class_init):
            (webkit_network_request_init):
            (webkit_network_request_constructor):
            (webkit_network_request_new_with_core_request):
            (webkit_network_request_new):
            (webkit_network_request_set_uri):
            (webkit_network_request_get_uri):
            * webkit/webkitnetworkrequest.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44598 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitnetworkrequest.h
+++ third_party/WebKit/WebKit/gtk/webkit/webkitnetworkrequest.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WEBKIT_NETWORK_REQUEST_H
 
 #include <glib-object.h>
+#include <libsoup/soup.h>
 
 #include <webkit/webkitdefines.h>
 
@@ -65,6 +66,9 @@ webkit_network_request_set_uri  (WebKitNetworkRequest *request,
 
 WEBKIT_API G_CONST_RETURN gchar *
 webkit_network_request_get_uri  (WebKitNetworkRequest *request);
+
+WEBKIT_API SoupMessage *
+webkit_network_request_get_message(WebKitNetworkRequest* request);
 
 G_END_DECLS
 
