20cdadb1b27c73b9ca5ccf5d80065c6fbed08bf0
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-12 14:56:41 +0000

    Return 88 instead of -88 from the purify scripts since the magic value in buildbot changed.
    TEST=Buildbot will turn orange if there is a warning
    BUG=None
    Review URL: http://codereview.chromium.org/126048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18270 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/purify/purify_analyze.py
+++ tools/purify/purify_analyze.py
@@ -861,7 +861,7 @@ class PurifyAnalyze:
       if fixes:
         logging.warning("%d total errors unexpectedly fixed" % fixes)
         # magic return code to turn the builder orange (via ReturnCodeCommand)
-        return -88
+        return 88
     return 0
 
 
