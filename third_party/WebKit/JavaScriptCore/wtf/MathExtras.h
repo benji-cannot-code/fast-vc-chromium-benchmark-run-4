ccabf20c2e6504acf77f08d176ba4fd25cd7c218
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-12 22:41:49 +0000

    2009-06-12  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=26191
            Remove xmath include in MathExtras.h, because it is not needed and also
            breaks VS2008 builds with TR1 turned on.
    
            * wtf/MathExtras.h: Removed xmath include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/MathExtras.h
+++ third_party/WebKit/JavaScriptCore/wtf/MathExtras.h
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if COMPILER(MSVC)
 #if PLATFORM(WIN_CE)
 #include <stdlib.h>
-#else
-#include <xmath.h>
 #endif
 #include <limits>
 
