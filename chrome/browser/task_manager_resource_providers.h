1b8a0f12eaf8c76918f8459797b81b8eea220a55
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-12 17:36:50 +0000

    This makes the network column in the task manager not display NA as network usage is available.
    BUG=http://crbug.com/13958
    TEST=Open the task manager, the network column for the browser process should be showing 0. Type something in the location bar, the value should change.
    Review URL: http://codereview.chromium.org/125056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18290 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager_resource_providers.h
+++ chrome/browser/task_manager_resource_providers.h
@@ -245,13 +245,8 @@ class TaskManagerBrowserProcessResource : public TaskManager::Resource {
   SkBitmap GetIcon() const;
   base::ProcessHandle GetProcess() const;
 
-  bool SupportNetworkUsage() const {
-    return network_usage_support_;
-  }
-
-  void SetSupportNetworkUsage() {
-    network_usage_support_ = true;
-  }
+  bool SupportNetworkUsage() const { return true; }
+  void SetSupportNetworkUsage() { NOTREACHED(); }
 
   // Returns the pid of the browser process.
   int process_id() const { return pid_; }
@@ -259,7 +254,6 @@ class TaskManagerBrowserProcessResource : public TaskManager::Resource {
  private:
   base::ProcessHandle process_;
   int pid_;
-  bool network_usage_support_;
   mutable std::wstring title_;
 
   static SkBitmap* default_icon_;
