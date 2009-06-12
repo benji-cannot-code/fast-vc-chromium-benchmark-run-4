9ef2ed039c3c385a0eb5ef803e9b15a965724c8b
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-12 23:00:05 +0000

    Copy login-related section of WebDatabase into a new class for use in PasswordStore.
    The platform-specific inheritence here is because the current Windows implementation of PasswordStore is a temporary shim, and will eventually need to use its own database instead; since we'll need almost all the fields on the Mac for the bag-on-the-side anyway, it makes sense to share as much of this implementation as possible.
    
    The code and unit tests are pretty much unchanged from WebDatabase (except for the inheritence stuff); the duplication should be temporary as the WebDatabase copy will be removed once Windows switches (and migrates their passwords).
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/119392
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18330 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/password_manager/login_database_mac.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/password_manager/login_database_mac.h"
+
+std::string LoginDatabaseMac::EncryptedString(const std::wstring& plain_text)
+    const {
+  return std::string();
+}
+
+std::wstring LoginDatabaseMac::DecryptedString(const std::string& cipher_text)
+    const {
+  return std::wstring();
+}
