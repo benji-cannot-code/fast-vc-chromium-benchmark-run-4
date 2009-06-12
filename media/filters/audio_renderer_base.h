5916ab7d419f3a325c47514c61f94ac3494ae55c
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-12 06:33:18 +0000

    Trust the asynchronous read protocol and stop posting tasks to the pipeline.
    Review URL: http://codereview.chromium.org/118458
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18257 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/audio_renderer_base.h
+++ media/filters/audio_renderer_base.h
@@ -78,6 +78,11 @@ class AudioRendererBase : public AudioRenderer {
                                int* sample_bits_out);
 
  private:
+  // Helper method that schedules an asynchronous read from the decoder.
+  //
+  // Safe to call from any thread.
+  void ScheduleRead();
+
   // Audio decoder.
   AudioDecoder* decoder_;
 
@@ -101,12 +106,6 @@ class AudioRendererBase : public AudioRenderer {
   // Audio time at end of last call to FillBuffer().
   // TODO(ralphl): Update this value after seeking.
   base::TimeDelta last_fill_buffer_time_;
-
-  // Posts a task on the pipeline thread to read a sample from the decoder.  The
-  // resulting buffer will be placed in the queue.
-  //
-  // Safe to call on any thread.
-  void ScheduleRead();
 
   DISALLOW_COPY_AND_ASSIGN(AudioRendererBase);
 };
