060995bd1ef451f14fa465f7db1b1e1864a81826
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-13 18:49:25 +0000

    Include sys/types.h for ssize_t.  This fixes compliation with gcc-4.3.
    Patch contributed by Joel Stanley
    R=deanm
    
    Review URL: http://codereview.chromium.org/125097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18356 0039d316-1c4b-4281-b951-d872f2087c98

--- base/unix_domain_socket_posix.cc
+++ base/unix_domain_socket_posix.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <unistd.h>
 #include <sys/uio.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 
 #include "base/eintr_wrapper.h"
