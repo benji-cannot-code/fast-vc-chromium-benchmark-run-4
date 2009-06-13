b256917c646391c9e8f597e082d4c46d97874597
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-13 17:52:44 +0000

    2009-06-12  Dave Hyatt  <hyatt@apple.com>
            Reviewed by Anders Carlsson.
    
            https://bugs.webkit.org/show_bug.cgi?id=26373
    
            Add a new class to Threading in wtf called ReadWriteLock that handles single writer/multiple reader locking.
            Provide a pthreads-only implementation of the lock for now, as this class is only going to be used
            on Snow Leopard at first.
    
            * wtf/Threading.h:
            (WTF::ReadWriteLock::impl):
            * wtf/ThreadingPthreads.cpp:
            (WTF::ReadWriteLock::ReadWriteLock):
            (WTF::ReadWriteLock::~ReadWriteLock):
            (WTF::ReadWriteLock::readLock):
            (WTF::ReadWriteLock::tryReadLock):
            (WTF::ReadWriteLock::writeLock):
            (WTF::ReadWriteLock::tryWriteLock):
            (WTF::ReadWriteLock::unlock):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44651 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Threading.h
+++ third_party/WebKit/JavaScriptCore/wtf/Threading.h
@@ -127,18 +127,22 @@ void detachThread(ThreadIdentifier);
 
 #if USE(PTHREADS)
 typedef pthread_mutex_t PlatformMutex;
+typedef pthread_rwlock_t PlatformReadWriteLock;
 typedef pthread_cond_t PlatformCondition;
 #elif PLATFORM(GTK)
 typedef GOwnPtr<GMutex> PlatformMutex;
+typedef void* PlatformReadWriteLock; // FIXME: Implement.
 typedef GOwnPtr<GCond> PlatformCondition;
 #elif PLATFORM(QT)
 typedef QT_PREPEND_NAMESPACE(QMutex)* PlatformMutex;
+typedef void* PlatformReadWriteLock; // FIXME: Implement.
 typedef QT_PREPEND_NAMESPACE(QWaitCondition)* PlatformCondition;
 #elif PLATFORM(WIN_OS)
 struct PlatformMutex {
     CRITICAL_SECTION m_internalMutex;
     size_t m_recursionCount;
 };
+typedef void* PlatformReadWriteLock; // FIXME: Implement.
 struct PlatformCondition {
     size_t m_waitersGone;
     size_t m_waitersBlocked;
@@ -152,6 +156,7 @@ struct PlatformCondition {
 };
 #else
 typedef void* PlatformMutex;
+typedef void* PlatformReadWriteLock;
 typedef void* PlatformCondition;
 #endif
     
@@ -171,6 +176,23 @@ private:
 };
 
 typedef Locker<Mutex> MutexLocker;
+
+class ReadWriteLock : Noncopyable {
+public:
+    ReadWriteLock();
+    ~ReadWriteLock();
+
+    void readLock();
+    bool tryReadLock();
+
+    void writeLock();
+    bool tryWriteLock();
+    
+    void unlock();
+
+private:
+    PlatformReadWriteLock m_readWriteLock;
+};
 
 class ThreadCondition : Noncopyable {
 public:
