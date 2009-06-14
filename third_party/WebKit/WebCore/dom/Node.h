b578e161f191352872bd24c4cd25ab9979fff245
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-14 21:27:10 +0000

    2009-06-14  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Darin Adler
    
            https://bugs.webkit.org/show_bug.cgi?id=26362
    
            Fix an issue noted on quirksmode.com where dynamically adding siblings does not
            update :nth-child style correctly.
    
            Test: fast/css/nth-child-dynamic.html
    
            * dom/Node.h:
            Change diff() to take const RenderStyle*
            * dom/Node.cpp:
            (WebCore::Node::diff):
            When comparing styles to decide whether to detach and reattach, we
            need to look at whether the pseudoclass-related data in the style
            changed (since RenderStyle::diff doesn't check this).
    
            * rendering/style/RenderStyle.cpp:
            (WebCore::RenderStyle::getCachedPseudoStyle):
            (WebCore::RenderStyle::inheritedNotEqual):
            Make these methods const.
            * rendering/style/RenderStyle.h:
            (WebCore::InheritedFlags::styleType):
            (WebCore::InheritedFlags::pseudoClassStateEquivalent):
            Make methods const.
            Add new method that compares the pseudoclass-related members.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44671 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Node.h
+++ third_party/WebKit/WebCore/dom/Node.h
@@ -107,7 +107,7 @@ public:
     static void dumpStatistics();
 
     enum StyleChange { NoChange, NoInherit, Inherit, Detach, Force };    
-    static StyleChange diff(RenderStyle*, RenderStyle*);
+    static StyleChange diff(const RenderStyle*, const RenderStyle*);
 
     Node(Document*, bool isElement = false, bool isContainer = false, bool isText = false);
     virtual ~Node();
