8f6a17f3321b93830ead86ce4e8525c03eedc875
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-14 04:24:28 +0000

    Build fix: add missing header.
    git-svn-id: svn://svn.chromium.org/blink/trunk@44665 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/SecurityOrigin.cpp
+++ third_party/WebKit/WebCore/page/SecurityOrigin.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CString.h"
 #include "KURL.h"
 #include "PlatformString.h"
+#include "StringHash.h"
 #include <wtf/HashSet.h>
 #include <wtf/StdLibExtras.h>
 
