e7cd6c03abc74036b2128f844e8e8ff94d70c935
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-15 17:18:31 +0000

    Popuplate bookmark menus on 1st launch.
    BUG=13794
    
    TEST=launch Chrome.  Before doing anything (e.g. changing focus or
    creating new windows), make sure the bookmark menu is populated.  If
    you have no bookmarks you'll need to add them ('star' some URLs) then
    restart Chrome.
    
    Review URL: http://codereview.chromium.org/125094
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18394 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_menu_bridge.mm
+++ chrome/browser/cocoa/bookmark_menu_bridge.mm
@@ -15,9 +15,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile_manager.h"
 
 BookmarkMenuBridge::BookmarkMenuBridge()
-    : controller_([[BookmarkMenuCocoaController alloc] initWithBridge:this]),
-      observing_(true) {
-  BrowserList::AddObserver(this);
+    : controller_([[BookmarkMenuCocoaController alloc] initWithBridge:this]) {
+  // Depending on when this is constructed, we may or may not have a
+  // browser yet.  If we do, start watching the bookmarks.  If we do
+  // not, delay watching until we see a SetLastActive().  At this time
+  // (6/12/09), the BookmarkMenuBridge object is constructed AFTER the
+  // first SetLastActive in -[AppController applicationDidFinishLaunching:].
+  Browser* browser = BrowserList::GetLastActive();
+  if (browser) {
+    observing_ = false;  // not observing browser activation
+    ObserveBookmarkModel();
+  } else {
+    observing_ = true;
+    BrowserList::AddObserver(this);
+  }
 }
 
 BookmarkMenuBridge::~BookmarkMenuBridge() {
@@ -105,6 +116,11 @@ void BookmarkMenuBridge::OnBrowserRemoving(const Browser* browser) {
 void BookmarkMenuBridge::OnBrowserSetLastActive(const Browser* browser) {
   BrowserList::RemoveObserver(this);
   observing_ = false;
+  ObserveBookmarkModel();
+}
+
+// Watch for changes.
+void BookmarkMenuBridge::ObserveBookmarkModel() {
   BookmarkModel* model = GetBookmarkModel();
   model->AddObserver(this);
   if (model->IsLoaded())
