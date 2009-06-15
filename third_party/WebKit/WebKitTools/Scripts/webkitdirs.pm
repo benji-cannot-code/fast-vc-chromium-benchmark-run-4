25f6ec645505769ac72cb5aaff48da217bfaf5ef
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-15 08:39:27 +0000

    2009-06-12  Tor Arne Vestbø  <tor.arne.vestbo@nokia.com>
            Reviewed by Mark Rowe.
            Reviewed by Simon Hausmann.
            Reviewed by Xan Lopez.
    
            Refactor handling of options in the build-webkit script
    
            Options are now defined in one place, and then reused when creating
            the usage help text, the arguments to GetOptions(), and when passing
            the options on to the underlying port-dependent build systems.
    
            This allows the Qt port to read the defaults for the options from the
            pro file (dynamically), and to pass the options on to qmake at build.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44681 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -56,6 +56,7 @@ my $sourceDir;
 my $currentSVNRevision;
 my $osXVersion;
 my $isQt;
+my %qtFeatureDefaults;
 my $isGtk;
 my $isWx;
 my @wxArgs;
@@ -680,6 +681,25 @@ sub isQt()
 {
     determineIsQt();
     return $isQt;
+}
+
+sub qtFeatureDefaults()
+{
+    determineQtFeatureDefaults();
+    return %qtFeatureDefaults;
+}
+
+sub determineQtFeatureDefaults()
+{
+    return if %qtFeatureDefaults;
+    my $originalCwd = getcwd();
+    chdir File::Spec->catfile(sourceDir(), "WebCore");
+    my $defaults = `qmake CONFIG+=compute_defaults 2>&1`;
+    chdir $originalCwd;
+
+    while ($defaults =~ m/(\S*?)=(.*?)( |$)/gi) {
+        $qtFeatureDefaults{$1}=$2;
+    }
 }
 
 sub checkForArgumentAndRemoveFromARGV
