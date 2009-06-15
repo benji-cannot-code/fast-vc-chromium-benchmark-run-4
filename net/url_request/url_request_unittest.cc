020afe5554ba35f093d4648270e93488ff1a161c
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-15 21:32:18 +0000

    Revert "Make TCPClientSocketPool own the ConnectingSockets." This reverts r18414. Broke reliability bot.
    Review URL: http://codereview.chromium.org/128001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18436 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -46,7 +46,10 @@ namespace {
 class URLRequestHttpCacheContext : public URLRequestContext {
  public:
   URLRequestHttpCacheContext() {
-    host_resolver_ = new net::HostResolver;
+    // TODO(eroman): we turn off host caching to see if synchronous
+    // host resolving interacts poorly with client socket pool. [experiment]
+    // http://crbug.com/13952
+    host_resolver_ = new net::HostResolver(0, 0);
     proxy_service_ = net::ProxyService::CreateNull();
     http_transaction_factory_ =
         new net::HttpCache(
@@ -316,7 +319,8 @@ TEST_F(HTTPSRequestTest, MAYBE_HTTPSExpiredTest) {
   }
 }
 
-TEST_F(URLRequestTest, CancelTest) {
+// http://crbug.com/13952
+TEST_F(URLRequestTest, DISABLED_CancelTest) {
   TestDelegate d;
   {
     TestURLRequest r(GURL("http://www.google.com/"), &d);
@@ -339,7 +343,8 @@ TEST_F(URLRequestTest, CancelTest) {
 #endif
 }
 
-TEST_F(URLRequestTest, CancelTest2) {
+// http://crbug.com/13952
+TEST_F(URLRequestTest, DISABLED_CancelTest2) {
   scoped_refptr<HTTPTestServer> server =
       HTTPTestServer::CreateServer(L"", NULL);
   ASSERT_TRUE(NULL != server.get());
@@ -368,7 +373,8 @@ TEST_F(URLRequestTest, CancelTest2) {
 #endif
 }
 
-TEST_F(URLRequestTest, CancelTest3) {
+// http://crbug.com/13952
+TEST_F(URLRequestTest, DISABLED_CancelTest3) {
   scoped_refptr<HTTPTestServer> server =
       HTTPTestServer::CreateServer(L"", NULL);
   ASSERT_TRUE(NULL != server.get());
@@ -396,7 +402,8 @@ TEST_F(URLRequestTest, CancelTest3) {
 #endif
 }
 
-TEST_F(URLRequestTest, CancelTest4) {
+// http://crbug.com/13952
+TEST_F(URLRequestTest, DISABLED_CancelTest4) {
   scoped_refptr<HTTPTestServer> server =
       HTTPTestServer::CreateServer(L"", NULL);
   ASSERT_TRUE(NULL != server.get());
