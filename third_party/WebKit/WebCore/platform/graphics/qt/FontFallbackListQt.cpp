885eee9c7d12a8fba9d8bd7f79472edcf20db6be
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-15 15:38:14 +0000

    2009-06-15  Simon Hausmann  <simon.hausmann@nokia.com>
            Reviewed by and done with Tor Arne Vestbø.
    
            Fix initialization of uninitialized members in FontFallbackListQt, leading
            to random crashes when rendering text.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/FontFallbackListQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/FontFallbackListQt.cpp
@@ -33,16 +33,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 FontFallbackList::FontFallbackList()
-    : m_familyIndex(0)
+    : m_pageZero(0)
+    , m_cachedPrimarySimpleFontData(0)
+    , m_fontSelector(0)
+    , m_familyIndex(0)
     , m_pitch(UnknownPitch)
     , m_loadingCustomFonts(false)
-    , m_fontSelector(0)
     , m_generation(0)
 {
 }
 
 void FontFallbackList::invalidate(WTF::PassRefPtr<WebCore::FontSelector> fontSelector)
 {
+    releaseFontData();
+    m_fontList.clear();
+    m_pageZero = 0;
+    m_pages.clear();
+    m_cachedPrimarySimpleFontData = 0;
     m_familyIndex = 0;
     m_pitch = UnknownPitch;
     m_loadingCustomFonts = false;
