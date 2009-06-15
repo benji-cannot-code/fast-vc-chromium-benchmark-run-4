70479025dbae661883fde5145ce41ef6fc27849f
sidchat@google.com <sidchat@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-15 22:18:40 +0000

    Add Compact Language Detection (CLD) library to Chrome. This works in Windows only currently.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/122007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18445 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ third_party/cld/bar/toolbar/cld/i18n/encodings/compact_lang_det/win/cld_unilib_google3.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2009 Google Inc.  All Rights Reserved.
+// Author: alekseys@google.com (Aleksey Shlyapnikov)
+
+// This code is not actually used, it was copied here for the reference only.
+// See cld_htmlutils_windows.cc for Windows version of this code.
+
+#include "cld/bar/toolbar/cld/i18n/encodings/compact_lang_det/win/cld_unilib.h"
+
+#include "cld/util/utf8/unilib.h"
+
+namespace cld_UniLib {
+
+int OneCharLen(const char* src) {
+  return UniLib::OneCharLen(src);
+}
+
+}  // namespace cld_UniLib
---
+++ third_party/cld/bar/toolbar/cld/i18n/encodings/compact_lang_det/win/cld_utf.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BAR_TOOLBAR_CLD_I18N_ENCODINGS_COMPACT_LANG_DET_WIN_CLD_UTF_H_
+#define BAR_TOOLBAR_CLD_I18N_ENCODINGS_COMPACT_LANG_DET_WIN_CLD_UTF_H_
+
+#if !defined(CLD_WINDOWS)
+
+//#include "third_party/utf/utf.h"
+
+#else
+
+enum {
+  UTFmax        = 4,            // maximum bytes per rune
+  Runesync      = 0x80,         // cannot represent part of a UTF sequence (<)
+  Runeself      = 0x80,         // rune and UTF sequences are the same (<)
+  Runeerror     = 0xFFFD,       // decoding error in UTF
+  Runemax       = 0x10FFFF,     // maximum rune value
+};
+
+#endif
+
+#endif  // BAR_TOOLBAR_CLD_I18N_ENCODINGS_COMPACT_LANG_DET_WIN_CLD_UTF_H_
