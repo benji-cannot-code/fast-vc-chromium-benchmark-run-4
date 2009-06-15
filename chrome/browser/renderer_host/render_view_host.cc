c79c2ada76e829d7210a20da01176591abaf48b9
yuzo@chromium.org <yuzo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-15 08:20:17 +0000

    Consider a redirect following user gesture as user-initiated in maintaining navigation entries. Also, ignore redirect- or machine-initiated- new subframe navigations.
    The current code treats all redirects as machine-initiated in processing
    navigation to a new page (to fix Bugs 9663 and 10531). This is not always
    appropriate, because some sites, e.g., www.google.com/ig, use redirect to
    implement user-initiated navigation (Bug 11896).
    
    This change assumes that a machine-initiated redirect happens within 300ms
    since the last document load was completed, while  a user-initiated one
    happens later.
    
    This assumption is not always correct, e.g., a user may cause transition within
    300ms. But I cannot think of any better ways to tell if a redirect is machine-
    initiated or user-initiated.
    
    I believe this change works good enough, at least better than the status quo.
    
    Review URL: http://codereview.chromium.org/115919
    
    TEST=Open http://www.hp.com and observe it redirects to
    http://www.hp.com/#Product . Hit Back button and observe
    the former URL is not visited. Open http://www.google.com/ig and
    click tabs inside the page, and try hitting Back and Forward to see if the
    navigation is right. Open http://www.google.com/codesearch, search for
    something, click on a result item, and try hitting Back.
    
    BUG=11896,12820
    
    
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18373 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_view_host.cc
+++ chrome/browser/renderer_host/render_view_host.cc
@@ -774,6 +774,8 @@ void RenderViewHost::OnMessageReceived(const IPC::Message& msg) {
                         OnMsgDOMUISend)
     IPC_MESSAGE_HANDLER(ViewHostMsg_ForwardMessageToExternalHost,
                         OnMsgForwardMessageToExternalHost)
+    IPC_MESSAGE_HANDLER(ViewHostMsg_DocumentLoadedInFrame,
+                        OnMsgDocumentLoadedInFrame)
     IPC_MESSAGE_HANDLER(ViewHostMsg_GoToEntryAtOffset,
                         OnMsgGoToEntryAtOffset)
     IPC_MESSAGE_HANDLER(ViewHostMsg_SetTooltipText, OnMsgSetTooltipText)
@@ -1149,6 +1151,10 @@ void RenderViewHost::OnMsgForwardMessageToExternalHost(
     const std::string& message, const std::string& origin,
     const std::string& target) {
   delegate_->ProcessExternalHostMessage(message, origin, target);
+}
+
+void RenderViewHost::OnMsgDocumentLoadedInFrame() {
+  delegate_->DocumentLoadedInFrame();
 }
 
 void RenderViewHost::DisassociateFromPopupCount() {
