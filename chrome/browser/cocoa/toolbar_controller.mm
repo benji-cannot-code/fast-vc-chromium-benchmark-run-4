06d60b0632c6bca9325265ac1a6e15bd6d3219d1
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-15 20:20:49 +0000

    Fix http://crbug.com/13971:	OSX: Text copied from Omnibox is styled
    Provide a custom field editor for the location bar that overrides cut/copy to only write plain text to the clipboard.
    
    BUG=13971
    TEST=Type a URL in the Omnibox, select all and copy url, open Textedit and paste the url into it.  Expected results: url should be unstyled previous behavior: styled text (as it appeared in the Omnibox was pasted)
    
    Review URL: http://codereview.chromium.org/126075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18425 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/toolbar_controller.mm
+++ chrome/browser/cocoa/toolbar_controller.mm
@@ -15,6 +15,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static NSString* const kStarImageName = @"star";
 static NSString* const kStarredImageName = @"starred";
 
+@implementation LocationBarFieldEditor
+- (void)copy:(id)sender {
+  NSPasteboard* pb = [NSPasteboard generalPasteboard];
+  [self performCopy:pb];
+}
+
+- (void)cut:(id)sender {
+  NSPasteboard* pb = [NSPasteboard generalPasteboard];
+  [self performCut:pb];
+}
+
+- (void)performCopy:(NSPasteboard*)pb {
+  [pb declareTypes:[NSArray array] owner:nil];
+  [self writeSelectionToPasteboard:pb types:
+      [NSArray arrayWithObject:NSStringPboardType]];
+}
+
+- (void)performCut:(NSPasteboard*)pb {
+  [self performCopy:pb];
+  [self delete:nil];
+}
+
+@end
+
 @interface ToolbarController(Private)
 - (void)initCommandStatus:(CommandUpdater*)commands;
 @end
@@ -50,10 +74,6 @@ static NSString* const kStarredImageName = @"starred";
   locationBarView_.reset(new LocationBarViewMac(locationBar_, commands_,
                                                 toolbarModel_, profile_));
   [locationBar_ setFont:[NSFont systemFontOfSize:[NSFont systemFontSize]]];
-}
-
-- (void)dealloc {
-  [super dealloc];
 }
 
 - (LocationBar*)locationBar {
@@ -120,6 +140,23 @@ static NSString* const kStarredImageName = @"starred";
   }
   [goButton_ setImage:[NSImage imageNamed:imageName]];
   [goButton_ setTag:tag];
+}
+
+- (id)customFieldEditorForObject:(id)obj {
+  if (obj == locationBar_) {
+    // Lazilly construct Field editor, Cocoa UI code always runs on the
+    // same thread, so there shoudn't be a race condition here.
+    if (locationBarFieldEditor_.get() == nil) {
+      locationBarFieldEditor_.reset([[LocationBarFieldEditor alloc] init]);
+    }
+
+    // This needs to be called every time, otherwise notifications
+    // aren't sent correctly.
+    DCHECK(locationBarFieldEditor_.get());
+    [locationBarFieldEditor_.get() setFieldEditor:YES];
+    return locationBarFieldEditor_.get();
+  }
+  return nil;
 }
 
 // Returns an array of views in the order of the outlets above.
--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -3284,6 +3284,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/cocoa/find_bar_view_unittest.mm',
         'browser/cocoa/location_bar_cell_unittest.mm',
         'browser/cocoa/location_bar_view_mac_unittest.mm',
+        'browser/cocoa/location_bar_fieldeditor_unittest.mm',
         'browser/cocoa/gradient_button_cell_unittest.mm',
         'browser/cocoa/grow_box_view_unittest.mm',
         'browser/cocoa/preferences_window_controller_unittest.mm',
