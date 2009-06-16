d2b48619c31e1555e7a6b08fb69277ac0ad8fdd4
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-16 17:25:47 +0000

    Fix Windows release compile by adding missing build_config.h include.
    TBR=deanm
    
    Review URL: http://codereview.chromium.org/126211
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18502 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/external_protocol_handler.cc
+++ chrome/browser/external_protocol_handler.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/external_protocol_handler.h"
 
+#include "build/build_config.h"
+
 #if defined(OS_WIN)
 #include <windows.h>
 #include <shellapi.h>
