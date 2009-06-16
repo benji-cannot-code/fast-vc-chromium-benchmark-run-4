4b21e1014fbd9110f029c6b7ac5555d69b5eb525
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-16 22:28:21 +0000

    2009-06-16  Jian Li  <jianli@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Bug 26456: Hook up V8 bindings for Worker's importScripts functionality.
            https://bugs.webkit.org/show_bug.cgi?id=26456
    
            * bindings/v8/custom/V8WorkerContextCustom.cpp:
            (WebCore::CALLBACK_FUNC_DECL):
            * bindings/v8/custom/V8WorkerCustom.cpp: Fixed missing exception code
              handling in Worker constructor for V8 bindings.
            (WebCore::CALLBACK_FUNC_DECL):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44744 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8WorkerContextCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8WorkerContextCustom.cpp
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ExceptionCode.h"
 #include "DOMTimer.h"
-#include "NotImplemented.h"
 #include "ScheduledAction.h"
 #include "V8Binding.h"
 #include "V8CustomBinding.h"
@@ -130,7 +129,29 @@ v8::Handle<v8::Value> SetTimeoutOrInterval(const v8::Arguments& args, bool singl
 CALLBACK_FUNC_DECL(WorkerContextImportScripts)
 {
     INC_STATS(L"DOM.WorkerContext.importScripts()");
-    notImplemented();
+    if (!args.Length())
+        return v8::Undefined();
+
+    String callerURL = V8Proxy::GetSourceName();
+    int callerLine = V8Proxy::GetSourceLineNumber() + 1;
+
+    Vector<String> urls;
+    for (int i = 0; i < args.Length(); i++) {
+        v8::TryCatch tryCatch;
+        v8::Handle<v8::String> scriptUrl = args[i]->ToString();
+        if (tryCatch.HasCaught() || scriptUrl.IsEmpty())
+            return v8::Undefined();
+        urls.append(toWebCoreString(scriptUrl));
+    }
+
+    WorkerContext* workerContext = V8Proxy::ToNativeObject<WorkerContext>(V8ClassIndex::WORKERCONTEXT, args.Holder());
+
+    ExceptionCode ec = 0;
+    workerContext->importScripts(urls, callerURL, callerLine, ec);
+
+    if (ec)
+        return throwError(ec);
+
     return v8::Undefined();
 }
 
