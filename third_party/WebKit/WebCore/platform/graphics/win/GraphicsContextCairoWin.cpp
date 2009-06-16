a992407d6e9434b684db21f948958c791d67d47a
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-16 20:27:16 +0000

    009-06-16  Brent Fulgham  <bfulgham@webkit.org>
            Reviewed by Xan Lopez.
    
            Fixes https://bugs.webkit.org/show_bug.cgi?id=26453.
            Null Cairo contextwill crash Windows Cairo build.
    
            Test: fast/multicol/columns-shorthand-parsing.html 
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/GraphicsContextCairoWin.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/GraphicsContextCairoWin.cpp
@@ -212,6 +212,9 @@ void GraphicsContextPlatformPrivate::concatCTM(const TransformationMatrix& trans
 
 void GraphicsContextPlatformPrivate::syncContext(PlatformGraphicsContext* cr)
 {
+    if (!cr)
+       return;
+
     cairo_surface_t* surface = cairo_get_target(cr);
     m_hdc = cairo_win32_surface_get_dc(surface);   
 
