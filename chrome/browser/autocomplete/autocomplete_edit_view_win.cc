0a871a54b00326a6eb3edbd0d20ff34648447d10
hamaji@chromium.org <hamaji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-16 00:48:32 +0000

    Add comments on magic number 0xbb. They are VK_OEM_PLUS. I guess the original author of this code considered the case where VK_OEM_PLUS isn't defined. For example, it seems that 0xbb is VK_EQUAL for embeded environments and VK_OEM_PLUS is never defined. Review URL: http://codereview.chromium.org/119154
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18467 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view_win.cc
+++ chrome/browser/autocomplete/autocomplete_edit_view_win.cc
@@ -908,7 +908,8 @@ bool AutocompleteEditViewWin::SkipDefaultKeyEventProcessing(
       return !e.IsAltDown() && e.IsControlDown();
 
     case VK_BACK:
-    case 0xbb:
+    case 0xbb:  // We don't use VK_OEM_PLUS in case the macro isn't defined.
+                // (e.g., we don't have this symbol in embeded environment).
       return true;
 
     default:
@@ -1816,6 +1817,8 @@ bool AutocompleteEditViewWin::OnKeyDownOnlyWritable(TCHAR key,
     }
 
     case 0xbb:  // Ctrl-'='.  Triggers subscripting (even in plain text mode).
+                // We don't use VK_OEM_PLUS in case the macro isn't defined.
+                // (e.g., we don't have this symbol in embeded environment).
       return true;
 
     default:
