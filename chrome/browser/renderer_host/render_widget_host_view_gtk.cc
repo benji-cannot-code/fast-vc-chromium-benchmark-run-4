984b6eadbdc6b135a49093a385a6efb170a75bf9
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-16 00:36:00 +0000

    Limit the size of popup menus on linux.
    This fixes <http://crbug.com/13805>.
    
    I've filed http://crbug.com/14142 for a more graceful solution.
    
    Review URL: http://codereview.chromium.org/126171
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18464 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_gtk.cc
+++ chrome/browser/renderer_host/render_widget_host_view_gtk.cc
@@ -25,6 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/api/public/gtk/WebInputEventFactory.h"
 #include "webkit/glue/webcursor_gtk_data.h"
 
+static const int kMaxPopupWidth = 2000;
+static const int kMaxPopupHeight = 2000;
+
 using WebKit::WebInputEventFactory;
 
 // This class is a simple convenience wrapper for Gtk functions. It has only
@@ -275,7 +278,9 @@ void RenderWidgetHostViewGtk::InitAsPopup(
     parent_host_view->Blur();
   }
 
-  gtk_widget_set_size_request(view_.get(), pos.width(), pos.height());
+  gtk_widget_set_size_request(view_.get(),
+                              std::min(pos.width(), kMaxPopupWidth),
+                              std::min(pos.height(), kMaxPopupHeight));
 
   gtk_window_set_default_size(GTK_WINDOW(popup), -1, -1);
   // Don't allow the window to be resized. This also forces the window to
