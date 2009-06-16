ed7f280e0fad262c04ad873030a8903ac5efc496
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-16 21:30:38 +0000

    Since the introduction of PushSource, there are two buffering layers in the browser process, the hardware buffer used in AudioOutputStream and transportation buffer in PushSource. Together with the latency in the IPC audio layer we have a serious AV sync problem.
    To compensate the delay and latency introduced by these three factors
    two parameters are added in RequestAudioPacket message that include
    the buffer fill level and timestamp of the request. These two parameters
    are used to determine the playback delay to be used by the audio
    renderer to update the pipeline with the time delta.
    
    So we have three parameters we need to care about:
    1. Hardware buffer in AudioOutputStream
    2. Buffered data in PushSource
    3. IPC latency
    
    We have accurate values for 2 and 3 but not 1. We currently don't have the
    API in AudioOutputStream to query the remaining buffer in the hardware
    buffer. But usually there is a large amount of data in it, e.g. on Windows
    400ms worth of data. Since we now detached the hardware buffer request of
    OnMoreData() from the actual packet request of IPC (by the introduction of
    PushSource), it is really critical to know the buffer level in the hardware.
    I made a guess of this buffer level by using the amount of last buffer copy.
    
    Review URL: http://codereview.chromium.org/122020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18536 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/audio_renderer_host.h
+++ chrome/browser/renderer_host/audio_renderer_host.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // AudioRendererHost::IPCAudioSource is a container of AudioOutputStream and
 // provide audio packets to the associated AudioOutputStream through IPC. It
 // performs the logic for buffering and controlling the AudioOutputStream.
-// 
+//
 // Here is a state diagram for the IPCAudioSource:
 //
 //          .--------->  [ Stopped ]  <--------.
@@ -311,10 +311,17 @@ class AudioRendererHost : public base::RefCountedThreadSafe<AudioRendererHost> {
     AudioOutputStream::State state_;
     base::SharedMemory shared_memory_;
     PushSource push_source_;
+
+    // Flag that indicates there is an outstanding request.
     bool outstanding_request_;
+    base::Time outstanding_request_time_;
+
+    // Number of bytes copied in the last OnMoreData call.
+    size_t last_copied_bytes_;
 
     // Protects:
     // - |outstanding_requests_|
+    // - |last_copied_bytes_|
     // - |push_source_|
     Lock lock_;
   };
