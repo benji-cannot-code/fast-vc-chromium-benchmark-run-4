c15d1f2985b398b448a4468b3662148bccd1efb8
erikkay@google.com <erikkay@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-16 00:54:59 +0000

    Fix a bug where ExtensionHost and ExtensionView weren't getting closed properly when a browser window was closed.
    BUG=13975
    TEST=see bug for repro steps
    
    
    Review URL: http://codereview.chromium.org/125164
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18470 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_view.h
+++ chrome/browser/extensions/extension_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 #include "base/scoped_ptr.h"
+#include "chrome/common/notification_registrar.h"
 #include "googleurl/src/gurl.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "views/controls/native/native_view_host.h"
@@ -29,7 +30,8 @@ class ExtensionContainer {
 };
 
 // This handles the display portion of an ExtensionHost.
-class ExtensionView : public views::NativeViewHost {
+class ExtensionView : public views::NativeViewHost,
+                      public NotificationObserver {
  public:
   ExtensionView(ExtensionHost* host, Browser* browser);
   ~ExtensionView();
@@ -56,6 +58,11 @@ class ExtensionView : public views::NativeViewHost {
                                const gfx::Rect& current);
   virtual void ViewHierarchyChanged(bool is_add,
                                     views::View *parent, views::View *child);
+
+  // NotificationObserver:
+  virtual void Observe(NotificationType type,
+                       const NotificationSource& source,
+                       const NotificationDetails& details);
 
  private:
   friend class ExtensionHost;
@@ -84,6 +91,9 @@ class ExtensionView : public views::NativeViewHost {
   // The container this view is in (not necessarily its direct superview).
   // Note: the view does not own its container.
   ExtensionContainer* container_;
+
+  // So that we can track browser window closing.
+  NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionView);
 };
