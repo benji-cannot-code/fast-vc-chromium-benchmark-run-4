397fd5d005b31b2b074411d4053ab69253f684f2
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-17 19:33:21 +0000

    WebCore:
    2009-06-17  David Hyatt  <hyatt@apple.com>
    
            Reviewed by Adam Roben and Anders Carlsson.
    
            Stub out the HTMLDataGridElement.
    
            * DerivedSources.make:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * WebCoreSources.bkl:
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::adjustRenderStyle):
            * editing/htmlediting.cpp:
            (WebCore::canHaveChildrenForEditing):
            * html/HTMLDataGridElement.h: Added.
            (WebCore::HTMLDataGridElement::HTMLDataGridElement):
            (WebCore::HTMLDataGridElement::tagPriority):
            * html/HTMLDataGridElement.idl: Added.
            * html/HTMLElement.cpp:
            (WebCore::inlineTagList):
            * html/HTMLParser.cpp:
            (WebCore::HTMLParser::isAffectedByResidualStyle):
            * html/HTMLTagNames.in:
    
    LayoutTests:
    
    2009-06-17  David Hyatt  <hyatt@apple.com>
    
            Reviewed by Anders Carlsson.
    
            Update window properties layout test for HTMLDataGridElement addition.
    
            * fast/dom/Window/window-properties-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/DerivedSources.make
+++ third_party/WebKit/WebCore/DerivedSources.make
@@ -111,6 +111,7 @@ DOM_CLASSES = \
     HTMLButtonElement \
     HTMLCanvasElement \
     HTMLCollection \
+    HTMLDataGridElement \
     HTMLDListElement \
     HTMLDirectoryElement \
     HTMLDivElement \
--- third_party/WebKit/WebCore/html/HTMLElementsAllInOne.cpp
+++ third_party/WebKit/WebCore/html/HTMLElementsAllInOne.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLBodyElement.cpp"
 #include "HTMLButtonElement.cpp"
 #include "HTMLCanvasElement.cpp"
+#include "HTMLDataGridElement.cpp"
 #include "HTMLDListElement.cpp"
 #include "HTMLDirectoryElement.cpp"
 #include "HTMLDivElement.cpp"
