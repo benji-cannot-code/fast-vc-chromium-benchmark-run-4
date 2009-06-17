9a2c7fe6b757f4bc4ce1f0bd8e13af81715f5871
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-17 19:06:26 +0000

    Linux: Enable metrics_service_uitest.cc. Add support for zygotes to call DidProcessCrash on renderers. TEST=metrics_server_uitest.cc
    Review URL: http://codereview.chromium.org/126264
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18641 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process.h
+++ base/process.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_PROCESS_H_
 
 #include "base/basictypes.h"
+#include "build/build_config.h"
 
 #include <sys/types.h>
 #ifdef OS_WIN
