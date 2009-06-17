965295f8dcdd8f559ac98140aeb6e81dff2c5b52
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-17 23:19:56 +0000

    Pass a gfk::NativeWindow instead of gfx::NativeView for TemplateURLFetcher. Use cross-platform method to get the NativeWindow.
    BUG=13326
    
    Review URL: http://codereview.chromium.org/126302
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18666 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -2020,11 +2020,7 @@ void TabContents::PageHasOSDD(RenderViewHost* render_view_host,
 
   // Download the OpenSearch description document. If this is successful a
   // new keyword will be created when done.
-#if defined(OS_WIN)
-  gfx::NativeView ancestor = GetAncestor(view_->GetNativeView(), GA_ROOT);
-#else
-  gfx::NativeView ancestor = NULL;
-#endif
+  gfx::NativeWindow ancestor = view_->GetTopLevelNativeWindow();
   profile()->GetTemplateURLFetcher()->ScheduleDownload(
       keyword,
       url,
