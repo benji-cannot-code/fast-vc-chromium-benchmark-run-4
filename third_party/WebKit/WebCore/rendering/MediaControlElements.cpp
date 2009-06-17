4d7eee2f0bc3938fd5fcd4205ce1e0db95198758
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-17 21:16:43 +0000

    2009-06-17  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Simon Fraser.
    
            <rdar://problem/6981193> Crash in MediaControlInputElement::attachToParent
    
            * rendering/MediaControlElements.cpp:
            (WebCore::MediaTextDisplayElement::attachToParent): NULL check element renderer or parent renderer.
            (WebCore::MediaControlInputElement::attachToParent): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/MediaControlElements.cpp
+++ third_party/WebKit/WebCore/rendering/MediaControlElements.cpp
@@ -94,7 +94,7 @@ MediaTextDisplayElement::MediaTextDisplayElement(Document* doc, PseudoId pseudo,
 void MediaTextDisplayElement::attachToParent(Element* parent)
 {
     parent->addChild(this);
-    if (renderer())
+    if (renderer() && parent->renderer())
         parent->renderer()->addChild(renderer());
 }
 
@@ -139,7 +139,8 @@ MediaControlInputElement::MediaControlInputElement(Document* doc, PseudoId pseud
 void MediaControlInputElement::attachToParent(Element* parent)
 {
     parent->addChild(this);
-    parent->renderer()->addChild(renderer());
+    if (renderer() && parent->renderer())
+        parent->renderer()->addChild(renderer());
 }
 
 void MediaControlInputElement::update()
