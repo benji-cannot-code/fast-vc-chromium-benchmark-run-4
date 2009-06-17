63585d37c11b0ed55389042f0df6e99ed1495033
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-17 04:56:16 +0000

    WebCore:
    2009-06-16  David Levin  <levin@chromium.org>
    
            Reviewed by David Hyatt.
    
            REGRESSION: When the main page (ScrollView) has a custom scrollbar, it crashes on destruction.
            https://bugs.webkit.org/show_bug.cgi?id=26326
    
            Test: scrollbars/scrollbar-crash-on-refresh.html
    
            * dom/Document.cpp:
            (WebCore::Document::detach):
            Gives the FrameView a change to do any necessary cleanup on
            Document::detach() which is where the renderArena gets detroyed.
            * page/FrameView.cpp:
            (WebCore::FrameView::detachCustomScrollbars):
            Gets rid of any custom scrollbars (if the docment supplied them).
            * page/FrameView.h:
            * platform/Scrollbar.h:
            (WebCore::Scrollbar::isCustomScrollbar):
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::destroy):
            Removed the check for document()->frame().  If frame() is 0 in this code,
            then the call to animation() is also incorrect (since it does document()->frame()->animation()).
            * rendering/RenderScrollbar.h:
            (WebCore::RenderScrollbar::isCustomScrollbar):
    
    LayoutTests:
    
    2009-06-16  David Levin  <levin@chromium.org>
    
            Reviewed by David Hyatt.
    
            REGRESSION: When the main page (ScrollView) has a custom scrollbar, it crashes on destruction.
            https://bugs.webkit.org/show_bug.cgi?id=26326
    
            Test that refreshing an iframe with custom scrollbars works without crashing.
    
            * scrollbars/resources/page-with-custom-scrollbars.html: Added.
            * scrollbars/scrollbar-crash-on-refresh-expected.txt: Added.
            * scrollbars/scrollbar-crash-on-refresh.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44751 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderScrollbar.h
+++ third_party/WebKit/WebCore/rendering/RenderScrollbar.h
@@ -71,6 +71,7 @@ public:
     IntRect trackPieceRectWithMargins(ScrollbarPart, const IntRect&);
 
     int minimumThumbLength();
+    virtual bool isCustomScrollbar() const { return true; }
 
 private:
     PassRefPtr<RenderStyle> getScrollbarPseudoStyle(ScrollbarPart, PseudoId);
