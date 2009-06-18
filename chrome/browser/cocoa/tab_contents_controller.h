c97746120dc3a9d1b5ffaac2946bf1648e8a609c
avi@google.com <avi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-18 13:58:34 +0000

    Quick spelling fix.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/132016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18703 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_contents_controller.h
+++ chrome/browser/cocoa/tab_contents_controller.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_COCOA_TAB_COTNENTS_CONTROLLER_H_
-#define CHROME_BROWSER_COCOA_TAB_COTNENTS_CONTROLLER_H_
+#ifndef CHROME_BROWSER_COCOA_TAB_CONTENTS_CONTROLLER_H_
+#define CHROME_BROWSER_COCOA_TAB_CONTENTS_CONTROLLER_H_
 
 #include <Cocoa/Cocoa.h>
 
@@ -50,4 +50,4 @@ class TabStripModel;
 
 @end
 
-#endif  // CHROME_BROWSER_COCOA_TAB_COTNENTS_CONTROLLER_H_
+#endif  // CHROME_BROWSER_COCOA_TAB_CONTENTS_CONTROLLER_H_
