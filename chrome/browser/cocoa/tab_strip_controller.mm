eab5f65f306038b6f7fe68e3262cc1a9f3bc2d38
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-18 21:11:59 +0000

    Fix an uninitialized variable that valgrind complains about.
    BUG=none
    TEST=Tab dragging on the Mac should still work.
    Review URL: http://codereview.chromium.org/131055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18749 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_strip_controller.mm
+++ chrome/browser/cocoa/tab_strip_controller.mm
@@ -65,6 +65,7 @@ NSString* const kTabStripNumberOfTabsChanged = @"kTabStripNumberOfTabsChanged";
     dragBlockingView_.reset([[TabStripControllerDragBlockingView alloc]
                               initWithFrame:NSZeroRect]);
     [view addSubview:dragBlockingView_];
+    newTabTargetFrame_ = NSMakeRect(0, 0, 0, 0);
   }
   return self;
 }
@@ -253,8 +254,9 @@ NSString* const kTabStripNumberOfTabsChanged = @"kTabStripNumberOfTabsChanged";
       
       // Check the frame by identifier to avoid redundant calls to animator.
       NSValue *identifier = [NSValue valueWithPointer:[tab view]];
-      NSRect oldTarget = [[targetFrames_ objectForKey:identifier] rectValue];
-      if (!NSEqualRects(oldTarget, tabFrame)) {
+      NSValue *oldTargetValue = [targetFrames_ objectForKey:identifier];
+      if (!oldTargetValue ||
+          !NSEqualRects([oldTargetValue rectValue], tabFrame)) {
         [frameTarget setFrame:tabFrame];
         [targetFrames_ setObject:[NSValue valueWithRect:tabFrame]
                           forKey:identifier];        
