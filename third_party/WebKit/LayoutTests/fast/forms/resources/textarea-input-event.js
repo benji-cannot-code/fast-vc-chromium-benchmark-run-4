f29fd7440819bea979c3f10f2e0261ffb87cabd2
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-18 18:58:02 +0000

    2009-06-17  Erik Arvidsson  <arv@chromium.org>
            Reviewed by Adele Peterson.
    
            https://bugs.webkit.org/show_bug.cgi?id=15189
            Adds the HTML5 input event support for textarea.
            
            Also, moves the oninput attribute parse handling to HTMLElement so that
            it can be set on any HTML element so that bubbling events can be handled
            using HTML attribute handlers.
    
            Test: fast/forms/textarea-input-event.html
    
            * html/HTMLElement.cpp:
            (WebCore::HTMLElement::parseMappedAttribute):
            * html/HTMLInputElement.cpp:
            (WebCore::HTMLInputElement::parseMappedAttribute):
            * rendering/RenderTextControlMultiLine.cpp:
            (WebCore::RenderTextControlMultiLine::subtreeHasChanged):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44811 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/forms/resources/textarea-input-event.js
@@ -0,0 +1,47 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('Test for <a href="https://bugs.webkit.org/show_bug.cgi?id=15189">https://bugs.webkit.org/show_bug.cgi?id=15189</a><br>');
+
+var handleTextareaOnInputCallCount = 0;
+var handleDivOnInputCallCount = 0;
+var successfullyParsed = false;
+
+function handleDivOnInput(e) {
+    if (e.target !== ta) {
+        testFailed('Wrong target');
+    } else {
+        handleDivOnInputCallCount++;
+        shouldBeEqualToString('ta.value', 'x');
+    }
+}
+
+function handleTextareaOnInput(e) {
+    if (e.target !== ta) {
+        testFailed('Wrong target');
+    } else {
+        handleTextareaOnInputCallCount++;
+        shouldBeEqualToString('ta.value', 'x');
+    }
+}
+
+var ta = document.getElementById('ta');
+ta.setAttribute('oninput', 'handleTextareaOnInput(event)');
+
+// Change the value before focusing.
+ta.value = '';
+
+ta.focus();
+
+if (window.eventSender) {
+    eventSender.keyDown('x', []);
+    shouldEvaluateTo('handleTextareaOnInputCallCount', 1);
+    shouldEvaluateTo('handleDivOnInputCallCount', 1);
+    
+    // Change programmatically
+    ta.value = 'programmatically';
+    
+    // Should not have triggered the events.
+    shouldEvaluateTo('handleTextareaOnInputCallCount', 1);
+    shouldEvaluateTo('handleDivOnInputCallCount', 1);
+    
+    successfullyParsed = true;
+}
--- third_party/WebKit/WebCore/rendering/RenderTextControlMultiLine.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTextControlMultiLine.cpp
@@ -49,6 +49,9 @@ void RenderTextControlMultiLine::subtreeHasChanged()
     if (!node()->focused())
         return;
 
+    // Fire the "input" DOM event
+    node()->dispatchEvent(eventNames().inputEvent, true, false);
+
     if (Frame* frame = document()->frame())
         frame->textDidChangeInTextArea(static_cast<Element*>(node()));
 }
