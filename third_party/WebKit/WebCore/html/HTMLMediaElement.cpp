213e768b7c54f5c8327b48ee5ef60237c003741a
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-18 01:49:27 +0000

    2009-06-17  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=26499
    
            First step to making video rendering be hardware-accelerated:
            make <video> elements get self-painting RenderLayers, and add
            an isVideo() virtual method to RenderObject.
    
            * html/HTMLMediaElement.cpp:
            (WebCore::HTMLMediaElement::setReadyState):
            (WebCore::HTMLMediaElement::mediaPlayerDurationChanged):
            (WebCore::HTMLMediaElement::mediaPlayerSizeChanged):
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::shouldBeNormalFlowOnly):
            (WebCore::RenderLayer::isSelfPaintingLayer):
            * rendering/RenderObject.h:
            (WebCore::RenderObject::isVideo):
            * rendering/RenderVideo.h:
            (WebCore::RenderVideo::requiresLayer):
            (WebCore::RenderVideo::isVideo):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLMediaElement.cpp
@@ -711,7 +711,7 @@ void HTMLMediaElement::setReadyState(MediaPlayer::ReadyState state)
         scheduleEvent(eventNames().loadedmetadataEvent);
 
 #if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-        if (renderer() && !renderer()->isImage()) {
+        if (renderer() && renderer()->isVideo()) {
             static_cast<RenderVideo*>(renderer())->videoSizeChanged();
         }
 #endif        
@@ -1257,7 +1257,7 @@ void HTMLMediaElement::mediaPlayerDurationChanged(MediaPlayer*)
 #if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
     if (renderer()) {
         renderer()->updateFromElement();
-        if (!renderer()->isImage())
+        if (renderer()->isVideo())
             static_cast<RenderVideo*>(renderer())->videoSizeChanged();
     }
 #endif        
@@ -1277,7 +1277,7 @@ void HTMLMediaElement::mediaPlayerSizeChanged(MediaPlayer*)
 {
     beginProcessingMediaPlayerCallback();
 #if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    if (renderer() && !renderer()->isImage())
+    if (renderer() && renderer()->isVideo())
         static_cast<RenderVideo*>(renderer())->videoSizeChanged();
 #endif        
     endProcessingMediaPlayerCallback();
