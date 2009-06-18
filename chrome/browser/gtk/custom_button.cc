1f3cb4553c745aa343beee54ae90c86a2327aff6
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-18 21:11:23 +0000

    Paint the custom frame border and round the corners.
    We apply the theme background color to the window and use a NineBox
    to draw the theme images around the border.
    
    BUG=13430
    
    Review URL: http://codereview.chromium.org/131049
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18748 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/custom_button.cc
+++ chrome/browser/gtk/custom_button.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "app/resource_bundle.h"
 #include "base/basictypes.h"
-#include "chrome/browser/gtk/nine_box.h"
 
 #include "grit/theme_resources.h"
 
