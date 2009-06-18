a9f7dc0cc3e4621ea0d093dcc07858132d491b93
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-18 22:51:03 +0000

    Implement a max worker count of 16 per tab and 64 total.  Any workers created after that are queued.
    Review URL: http://codereview.chromium.org/125242
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18763 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/worker_host/worker_process_host.h
+++ chrome/browser/worker_host/worker_process_host.h
@@ -15,30 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class WorkerProcessHost : public ChildProcessHost {
  public:
-  WorkerProcessHost(ResourceDispatcherHost* resource_dispatcher_host_);
-  ~WorkerProcessHost();
-
-  // Starts the process.  Returns true iff it succeeded.
-  bool Init();
-
-  // Creates a worker object in the process.
-  void CreateWorker(const GURL& url,
-                    int renderer_process_id,
-                    int render_view_route_id,
-                    int worker_route_id,
-                    IPC::Message::Sender* sender,
-                    int sender_pid,
-                    int sender_route_id);
-
-  // Returns true iff the given message from a renderer process was forwarded to
-  // the worker.
-  bool FilterMessage(const IPC::Message& message, int sender_pid);
-
-  void SenderShutdown(IPC::Message::Sender* sender);
-
- protected:
-  friend class WorkerService;
-
   // Contains information about each worker instance, needed to forward messages
   // between the renderer and worker processes.
   struct WorkerInstance {
@@ -50,6 +26,24 @@ class WorkerProcessHost : public ChildProcessHost {
     int sender_pid;
     int sender_route_id;
   };
+
+  WorkerProcessHost(ResourceDispatcherHost* resource_dispatcher_host_);
+  ~WorkerProcessHost();
+
+  // Starts the process.  Returns true iff it succeeded.
+  bool Init();
+
+  // Creates a worker object in the process.
+  void CreateWorker(const WorkerInstance& instance);
+
+  // Returns true iff the given message from a renderer process was forwarded to
+  // the worker.
+  bool FilterMessage(const IPC::Message& message, int sender_pid);
+
+  void SenderShutdown(IPC::Message::Sender* sender);
+
+ protected:
+  friend class WorkerService;
 
   typedef std::list<WorkerInstance> Instances;
   const Instances& instances() const { return instances_; }
@@ -71,6 +65,7 @@ class WorkerProcessHost : public ChildProcessHost {
   void OnCreateDedicatedWorker(const GURL& url,
                                int render_view_route_id,
                                int* route_id);
+  void OnCancelCreateDedicatedWorker(int route_id);
   void OnForwardToWorker(const IPC::Message& message);
 
   Instances instances_;
--- chrome/browser/worker_host/worker_service.h
+++ chrome/browser/worker_host/worker_service.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/singleton.h"
+#include "chrome/browser/worker_host/worker_process_host.h"
 #include "chrome/common/ipc_message.h"
 #include "chrome/common/notification_registrar.h"
 #include "googleurl/src/gurl.h"
@@ -35,6 +36,9 @@ class WorkerService : public NotificationObserver {
                              int sender_pid,
                              int sender_route_id);
 
+  // Cancel creation of a dedicated worker that hasn't started yet.
+  void CancelCreateDedicatedWorker(int sender_pid, int sender_route_id);
+
   // Called by the worker creator when a message arrives that should be
   // forwarded to the worker process.
   void ForwardMessage(const IPC::Message& message, int sender_pid);
@@ -44,11 +48,22 @@ class WorkerService : public NotificationObserver {
                const NotificationSource& source,
                const NotificationDetails& details);
 
-  void NotifySenderShutdown(IPC::Message::Sender* sender);
+  // Notifies us that a process that's talking to a worker has shut down.
+  void OnSenderShutdown(IPC::Message::Sender* sender);
+
+  // Notifies us that a worker process has closed.
+  void OnWorkerProcessDestroyed(WorkerProcessHost* process);
 
   MessageLoop* ui_loop() { return ui_loop_; }
 
   int next_worker_route_id() { return ++next_worker_route_id_; }
+
+  // Used when multiple workers can run in the same process.
+  static const int kMaxWorkerProcessesWhenSharing;
+
+  // Used when we run each worker in a separate process.
+  static const int kMaxWorkersWhenSeparate;
+  static const int kMaxWorkersPerTabWhenSeparate;
 
  private:
   friend struct DefaultSingletonTraits<WorkerService>;
@@ -68,10 +83,16 @@ class WorkerService : public NotificationObserver {
   // number of worker instance running.
   WorkerProcessHost* GetLeastLoadedWorker();
 
+  // Checks if we can create a worker process based on the process limit when
+  // we're using a strategy of one process per core.
+  bool CanCreateWorkerProcess(const WorkerProcessHost::WorkerInstance& instance);
+
   NotificationRegistrar registrar_;
   int next_worker_route_id_;
   ResourceDispatcherHost* resource_dispatcher_host_;
   MessageLoop* ui_loop_;
+
+  WorkerProcessHost::Instances queued_workers_;
 
   DISALLOW_COPY_AND_ASSIGN(WorkerService);
 };
