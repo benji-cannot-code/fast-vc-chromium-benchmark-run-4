4124d1f7630923cff83ed3e70459e3f11406e28f
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-18 22:04:41 +0000

    2009-06-17  Brent Fulgham  <bfulgham@webkit.org>
            Reviewed by Eric Seidel.
    
            Final cleanups in this refactoring:
            (1) Move WindowsBitmap implementation from CG-specific file to
                the platform-common GraphicsContextWin.cpp, since it is
                equally useful on both platforms.
            (2) Revise the TransformationMatrix logic as suggested by
                Adam Roben in his review comments to Part #2 of this
                refactoring.
    
            * platform/graphics/win/GraphicsContextCGWin.cpp:
            * platform/graphics/win/GraphicsContextWin.cpp:
            (WebCore::GraphicsContext::WindowsBitmap::WindowsBitmap):
            (WebCore::GraphicsContext::WindowsBitmap::~WindowsBitmap):
            (WebCore::GraphicsContext::createWindowsBitmap):
            (WebCore::GraphicsContext::getWindowsContext):
            (WebCore::GraphicsContextPlatformPrivate::scale):
            (WebCore::GraphicsContextPlatformPrivate::rotate):
            (WebCore::GraphicsContextPlatformPrivate::translate):
            (WebCore::GraphicsContextPlatformPrivate::concatCTM):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44829 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/GraphicsContextCGWin.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/GraphicsContextCGWin.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "GraphicsContext.h"
 
-#include "BitmapInfo.h"
 #include "TransformationMatrix.h"
 #include "Path.h"
 
@@ -112,41 +111,6 @@ void GraphicsContext::releaseWindowsContext(HDC hdc, const IntRect& dstRect, boo
     }
 
     m_data->restore();
-}
-
-GraphicsContext::WindowsBitmap::WindowsBitmap(HDC hdc, IntSize size)
-    : m_hdc(0)
-    , m_size(size)
-{
-    BitmapInfo bitmapInfo = BitmapInfo::create(m_size);
-
-    m_bitmap = CreateDIBSection(0, &bitmapInfo, DIB_RGB_COLORS, reinterpret_cast<void**>(&m_bitmapBuffer), 0, 0);
-    if (!m_bitmap)
-        return;
-
-    m_hdc = CreateCompatibleDC(hdc);
-    SelectObject(m_hdc, m_bitmap);
-
-    BITMAP bmpInfo;
-    GetObject(m_bitmap, sizeof(bmpInfo), &bmpInfo);
-    m_bytesPerRow = bmpInfo.bmWidthBytes;
-    m_bitmapBufferLength = bmpInfo.bmWidthBytes * bmpInfo.bmHeight;
-
-    SetGraphicsMode(m_hdc, GM_ADVANCED);
-}
-
-GraphicsContext::WindowsBitmap::~WindowsBitmap()
-{
-    if (!m_bitmap)
-        return;
-
-    DeleteDC(m_hdc);
-    DeleteObject(m_bitmap);
-}
-
-GraphicsContext::WindowsBitmap* GraphicsContext::createWindowsBitmap(IntSize size)
-{
-    return new WindowsBitmap(m_data->m_hdc, size);
 }
 
 void GraphicsContext::drawWindowsBitmap(WindowsBitmap* image, const IntPoint& point)
