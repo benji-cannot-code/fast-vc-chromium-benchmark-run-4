693f8e033e4ac301a5d46e7bd9c5d85fbb4ee342
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-18 04:58:34 +0000

    Improve chunked encoding parsing.
    R=abarth
    BUG=14508
    TEST=HttpChunkedDecoderTest.ExcessiveChunkLen
    
    Patch contributed by Chris Evans.
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18687 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_chunked_decoder_unittest.cc
+++ net/http/http_chunked_decoder_unittest.cc
@@ -267,3 +267,10 @@ TEST(HttpChunkedDecoderTest, InvalidConsecutiveCRLFs) {
   };
   RunTestUntilFailure(inputs, arraysize(inputs), 1);
 }
+
+TEST(HttpChunkedDecoderTest, ExcessiveChunkLen) {
+  const char* inputs[] = {
+    "c0000000\r\nhello\r\n"
+  };
+  RunTestUntilFailure(inputs, arraysize(inputs), 0);
+}
