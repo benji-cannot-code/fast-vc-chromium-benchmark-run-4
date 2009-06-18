6fed96124bf8d22b487a8b5bcfa6f349e75781aa
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-18 19:06:08 +0000

    2009-06-18  Simon Fraser  <simon.fraser@apple.com>
            Fix the Leopard build where USE(ACCELERATED_COMPOSITING) is not defined.
    
            * platform/graphics/mac/MediaPlayerPrivateQTKit.mm:
            (WebCore::MediaPlayerPrivate::currentRenderingMode):
            (WebCore::MediaPlayerPrivate::setUpVideoRendering):
            (WebCore::MediaPlayerPrivate::tearDownVideoRendering):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44812 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/MediaPlayerPrivateQTKit.mm
+++ third_party/WebKit/WebCore/platform/graphics/mac/MediaPlayerPrivateQTKit.mm
@@ -456,8 +456,10 @@ MediaPlayerPrivate::MediaRenderingMode MediaPlayerPrivate::currentRenderingMode(
     if (m_qtVideoRenderer)
         return MediaRenderingSoftwareRenderer;
     
+#if USE(ACCELERATED_COMPOSITING)
     if (m_qtVideoLayer)
         return MediaRenderingMovieLayer;
+#endif
     
     return MediaRenderingNone;
 }
@@ -497,10 +499,12 @@ void MediaPlayerPrivate::setUpVideoRendering()
     case MediaRenderingSoftwareRenderer:
         createQTVideoRenderer();
         break;
+#if USE(ACCELERATED_COMPOSITING)
     case MediaRenderingMovieLayer:
         createQTMovieLayer();
         break;
     }
+#endif
 }
 
 void MediaPlayerPrivate::tearDownVideoRendering()
@@ -509,8 +513,10 @@ void MediaPlayerPrivate::tearDownVideoRendering()
         detachQTMovieView();
     else if (m_qtVideoRenderer)
         destroyQTVideoRenderer();
+#if USE(ACCELERATED_COMPOSITING)
     else
         destroyQTMovieLayer();
+#endif
 }
 
 bool MediaPlayerPrivate::hasSetUpVideoRendering() const
