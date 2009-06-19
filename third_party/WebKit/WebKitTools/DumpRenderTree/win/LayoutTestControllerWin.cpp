18b477a12995378c06ecf54196f7d56fcb204722
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-19 08:29:41 +0000

    2009-06-19  Adam Barth  <abarth@webkit.org>
            Unreviewed attempt to fix Windows build.
    
            * DumpRenderTree/win/DumpRenderTree.cpp:
            (resetWebViewToConsistentStateBeforeTesting):
            * DumpRenderTree/win/LayoutTestControllerWin.cpp:
            (LayoutTestController::setXSSAuditorEnabled):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/win/LayoutTestControllerWin.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/win/LayoutTestControllerWin.cpp
@@ -319,7 +319,11 @@ void LayoutTestController::setXSSAuditorEnabled(bool enabled)
     if (FAILED(webView->preferences(&preferences)))
         return;
 
-    preferences->setXSSAuditorEnabled(enabled);
+    COMPtr<IWebPreferencesPrivate> prefsPrivate(Query, preferences);
+    if (!prefsPrivate)
+        return;
+
+    prefsPrivate->setXSSAuditorEnabled(enabled);
 }
 
 void LayoutTestController::setPopupBlockingEnabled(bool enabled)
