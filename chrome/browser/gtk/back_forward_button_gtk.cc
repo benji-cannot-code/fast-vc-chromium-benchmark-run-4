ae72d9b469442979b6bb00e24703799becd61e63
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-19 03:28:11 +0000

    Implement window open disposition for (some) navigation buttons.
    still need to do similar for link buttons and such.
    
    BUG=14518
    
    Review URL: http://codereview.chromium.org/131071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18786 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/back_forward_button_gtk.cc
+++ chrome/browser/gtk/back_forward_button_gtk.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser.h"
 #include "chrome/browser/gtk/back_forward_menu_model_gtk.h"
 #include "chrome/browser/gtk/menu_gtk.h"
+#include "chrome/common/gtk_util.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
 
@@ -22,6 +23,7 @@ static const int kMenuTimerDelay = 500;
 BackForwardButtonGtk::BackForwardButtonGtk(Browser* browser, bool is_forward)
     : browser_(browser),
       is_forward_(is_forward),
+      last_release_event_flags_(0),
       show_menu_factory_(this) {
   int normal, active, highlight, depressed, tooltip;
   if (is_forward) {
@@ -50,6 +52,8 @@ BackForwardButtonGtk::BackForwardButtonGtk(Browser* browser, bool is_forward)
                    G_CALLBACK(OnClick), this);
   g_signal_connect(widget(), "button-press-event",
                    G_CALLBACK(OnButtonPress), this);
+  g_signal_connect(widget(), "button-release-event",
+                   G_CALLBACK(OnButtonRelease), this);
   gtk_widget_add_events(widget(), GDK_POINTER_MOTION_MASK);
   g_signal_connect(widget(), "motion-notify-event",
                    G_CALLBACK(OnMouseMove), this);
@@ -58,6 +62,8 @@ BackForwardButtonGtk::BackForwardButtonGtk(Browser* browser, bool is_forward)
   // default of right aligned.
   g_object_set_data(G_OBJECT(widget()), "left-align-popup",
                     reinterpret_cast<void*>(true));
+
+  gtk_util::SetButtonTriggersNavigation(widget());
 }
 
 BackForwardButtonGtk::~BackForwardButtonGtk() {
@@ -84,13 +90,18 @@ void BackForwardButtonGtk::OnClick(GtkWidget* widget,
                                    BackForwardButtonGtk* button) {
   button->show_menu_factory_.RevokeAll();
 
-  button->browser_->ExecuteCommand(button->is_forward_ ?
-                                   IDC_FORWARD : IDC_BACK);
+  DCHECK(button->last_release_event_flags_ != 0);
+  button->browser_->ExecuteCommandWithDisposition(
+      button->is_forward_ ? IDC_FORWARD : IDC_BACK,
+      event_utils::DispositionFromEventFlags(
+          button->last_release_event_flags_));
 }
 
 // static
 gboolean BackForwardButtonGtk::OnButtonPress(GtkWidget* widget,
     GdkEventButton* event, BackForwardButtonGtk* button) {
+  button->last_release_event_flags_ = 0;
+
   if (event->button != 1)
     return FALSE;
 
@@ -99,6 +110,13 @@ gboolean BackForwardButtonGtk::OnButtonPress(GtkWidget* widget,
       button->show_menu_factory_.NewRunnableMethod(
           &BackForwardButtonGtk::ShowBackForwardMenu),
       kMenuTimerDelay);
+  return FALSE;
+}
+
+// static
+gboolean BackForwardButtonGtk::OnButtonRelease(GtkWidget* widget,
+    GdkEventButton* event, BackForwardButtonGtk* button) {
+  button->last_release_event_flags_ = event->state;
   return FALSE;
 }
 
