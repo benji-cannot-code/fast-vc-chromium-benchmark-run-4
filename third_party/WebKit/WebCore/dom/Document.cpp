72e85ad131330910a2c2017d8433bd6347c8a097
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-19 18:25:42 +0000

    2009-06-19  Adam Barth  <abarth@webkit.org>
            Reviewed by Dimitri Glazkov.
    
            https://bugs.webkit.org/show_bug.cgi?id=26536
    
            Fix external-script-URL-location.html and
            write-external-script-open.html LayoutTests.
    
            implicitOpen is called from several places.  We need to setXSSAuditor
            each time, so I moved the call inside implicitOpen.
    
            * dom/Document.cpp:
            (WebCore::Document::open):
            (WebCore::Document::implicitOpen):
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::begin):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44861 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -1478,7 +1478,7 @@ void Document::open(Document* ownerDocument)
         if (m_frame->loader()->state() == FrameStateProvisional)
             m_frame->loader()->stopAllLoaders();
     }
-    
+
     implicitOpen();
 
     if (m_frame)
@@ -1505,6 +1505,9 @@ void Document::implicitOpen()
     clear();
     m_tokenizer = createTokenizer();
     setParsing(true);
+
+    if (m_frame)
+        m_tokenizer->setXSSAuditor(m_frame->script()->xssAuditor());
 
     // If we reload, the animation controller sticks around and has
     // a stale animation time. We need to update it here.
