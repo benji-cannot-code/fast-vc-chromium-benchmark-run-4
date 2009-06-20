ea36b8e389d2671ae313faa7ecaabc1da4b54579
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-20 06:21:17 +0000

    2009-06-19  Kenneth Rohde Christiansen  <kenneth.christiansen@openbossa.org>
            Reviewed by Steve Falkenburg.
    
            https://bugs.webkit.org/show_bug.cgi?id=26494
            Bug 26494: RenderTheme::themeForPage reads from Settings before it has
            been initialized by WebKit
    
            Get the shouldPaintNativeControls preference and update the settings
            before initializing the page, as the page depends on it after the
            themeForPage change.
    
            * WebView.cpp:
            (WebView::initWithFrame):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -2315,6 +2315,12 @@ HRESULT STDMETHODCALLTYPE WebView::initWithFrame(
 #endif
     WebKitSetApplicationCachePathIfNecessary();
     
+#if USE(SAFARI_THEME)
+    BOOL shouldPaintNativeControls;
+    if (SUCCEEDED(m_preferences->shouldPaintNativeControls(&shouldPaintNativeControls)))
+        Settings::setShouldPaintNativeControls(shouldPaintNativeControls);
+#endif
+
     m_page = new Page(new WebChromeClient(this), new WebContextMenuClient(this), new WebEditorClient(this), new WebDragClient(this), new WebInspectorClient(this));
 
     BSTR localStoragePath;
