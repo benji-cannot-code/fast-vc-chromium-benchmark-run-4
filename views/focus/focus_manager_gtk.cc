ecf1c34ab6176d97fa1a513f70554928ac04f48c
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-20 04:22:44 +0000

    Relanding focus manager refactoring with build fix, see:http://codereview.chromium.org/125148BUG=NoneTEST=NoneTBR=ben Review URL: http://codereview.chromium.org/141013
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18889 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ views/focus/focus_manager_gtk.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "views/focus/focus_manager.h"
+
+#include "base/logging.h"
+
+namespace views {
+
+void FocusManager::ClearNativeFocus() {
+  NOTIMPLEMENTED();
+}
+
+void FocusManager::FocusNativeView(gfx::NativeView native_view) {
+  NOTIMPLEMENTED();
+}
+
+// static
+FocusManager* FocusManager::GetFocusManagerForNativeView(
+    gfx::NativeView native_view) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace views
