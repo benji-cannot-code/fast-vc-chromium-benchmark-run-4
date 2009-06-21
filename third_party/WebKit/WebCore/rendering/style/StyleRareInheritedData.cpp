c74cc22fe70c667549218886e55568f79775698e
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-21 20:16:59 +0000

    WebCore:
            Reviewed by Sam Weinig.
    
            - fix https://bugs.webkit.org/show_bug.cgi?id=9694
              resize value not compared
    
            Test: fast/css/resize-value-compared.html
    
            * rendering/style/StyleRareInheritedData.cpp:
            (WebCore::StyleRareInheritedData::operator==): Compare the resize
            member.
    
    LayoutTests:
    
            Reviewed by Sam Weinig.
    
            - test for https://bugs.webkit.org/show_bug.cgi?id=9694
              resize value not compared
    
            * fast/css/resize-value-compared-expected.txt: Added.
            * fast/css/resize-value-compared.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44912 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/style/StyleRareInheritedData.cpp
+++ third_party/WebKit/WebCore/rendering/style/StyleRareInheritedData.cpp
@@ -81,6 +81,7 @@ bool StyleRareInheritedData::operator==(const StyleRareInheritedData& o) const
         && nbspMode == o.nbspMode
         && khtmlLineBreak == o.khtmlLineBreak
         && textSizeAdjust == o.textSizeAdjust
+        && resize == o.resize
         && userSelect == o.userSelect;
 }
 
