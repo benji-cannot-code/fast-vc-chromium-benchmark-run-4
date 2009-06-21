1c6510bab43e97affa79f8c76f217996b4a87749
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-21 23:02:13 +0000

    Bug 26587: Support JSON.parse <https://bugs.webkit.org/show_bug.cgi?id=26587>
    Reviewed by Darin Adler and Cameron Zwarich.
    
    Extend the LiteralParser to support the full strict JSON
    grammar, fix a few places where the grammar was incorrectly
    lenient.   Doesn't yet support the JSON.parse reviver function
    but that does not block the JSON.parse functionality itself.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44923 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/interpreter/Interpreter.cpp
+++ third_party/WebKit/JavaScriptCore/interpreter/Interpreter.cpp
@@ -351,7 +351,7 @@ NEVER_INLINE JSValue Interpreter::callEval(CallFrame* callFrame, RegisterFile* r
 
     UString programSource = asString(program)->value();
 
-    LiteralParser preparser(callFrame, programSource);
+    LiteralParser preparser(callFrame, programSource, LiteralParser::NonStrictJSON);
     if (JSValue parsedObject = preparser.tryLiteralParse())
         return parsedObject;
     
--- third_party/WebKit/JavaScriptCore/runtime/JSONObject.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSONObject.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Error.h"
 #include "ExceptionHelpers.h"
 #include "JSArray.h"
+#include "LiteralParser.h"
 #include "PropertyNameArray.h"
 #include <wtf/MathExtras.h>
 
@@ -37,6 +38,7 @@ namespace JSC {
 
 ASSERT_CLASS_FITS_IN_CELL(JSONObject);
 
+static JSValue JSC_HOST_CALL JSONProtoFuncParse(ExecState*, JSObject*, JSValue, const ArgList&);
 static JSValue JSC_HOST_CALL JSONProtoFuncStringify(ExecState*, JSObject*, JSValue, const ArgList&);
 
 }
@@ -563,6 +565,7 @@ const ClassInfo JSONObject::info = { "JSON", 0, 0, ExecState::jsonTable };
 
 /* Source for JSONObject.lut.h
 @begin jsonTable
+  parse         JSONProtoFuncParse             DontEnum|Function 1
   stringify     JSONProtoFuncStringify         DontEnum|Function 1
 @end
 */
@@ -583,6 +586,24 @@ bool JSONObject::getOwnPropertySlot(ExecState* exec, const Identifier& propertyN
 void JSONObject::markStringifiers(Stringifier* stringifier)
 {
     stringifier->mark();
+}
+
+// ECMA-262 v5 15.12.3
+JSValue JSC_HOST_CALL JSONProtoFuncParse(ExecState* exec, JSObject*, JSValue, const ArgList& args)
+{
+    if (args.isEmpty())
+        return throwError(exec, GeneralError, "JSON.parse requires at least one parameter");
+    JSValue value = args.at(0);
+    UString source = value.toString(exec);
+    if (exec->hadException())
+        return jsNull();
+    
+    LiteralParser jsonParser(exec, source, LiteralParser::StrictJSON);
+    JSValue parsedObject = jsonParser.tryLiteralParse();
+    if (!parsedObject)
+        return throwError(exec, SyntaxError, "Unable to parse JSON string");
+    
+    return parsedObject;
 }
 
 // ECMA-262 v5 15.12.3
