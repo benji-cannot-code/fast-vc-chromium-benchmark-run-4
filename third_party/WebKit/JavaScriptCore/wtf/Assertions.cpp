771290a03d00e7a9daf02d3157eb442df11398c2
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-21 22:46:47 +0000

    2009-06-21  David Levin  <levin@chromium.org>
            Reviewed by NOBODY (speculative build fix for windows).
    
            Simply removed some whitespace form this file to make windows build wtf and
            hopefully copy the new MessageQueque.h so that WebCore picks it up.
    
            * wtf/Assertions.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44922 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Assertions.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/Assertions.cpp
@@ -55,7 +55,7 @@ static void vprintf_stderr_common(const char* format, va_list args)
     if (strstr(format, "%@")) {
         CFStringRef cfFormat = CFStringCreateWithCString(NULL, format, kCFStringEncodingUTF8);
         CFStringRef str = CFStringCreateWithFormatAndArguments(NULL, NULL, cfFormat, args);
-        
+
         int length = CFStringGetMaximumSizeForEncoding(CFStringGetLength(str), kCFStringEncodingUTF8);
         char* buffer = (char*)malloc(length + 1);
 
