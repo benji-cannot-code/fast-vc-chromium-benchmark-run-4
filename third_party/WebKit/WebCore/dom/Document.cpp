b67b68a04528759a3cf0460fb881679c9090aadf
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-22 17:20:03 +0000

    WebCore:
    2009-06-22  David Levin  <levin@chromium.org>
    
            Reviewed by David Hyatt and Eric Seidel.
    
            REGRESSION: When the main page (ScrollView) has a custom scrollbar, it crashes on destruction.
            https://bugs.webkit.org/show_bug.cgi?id=26326
    
            Test: scrollbars/scrollbar-crash-on-refresh.html
    
            * dom/Document.cpp:
            (WebCore::Document::detach):
            Gives the FrameView a change to do any necessary cleanup on
            Document::detach() which is where the renderArena gets detroyed.
            * page/FrameView.cpp:
            (WebCore::FrameView::detachCustomScrollbars):
            Gets rid of any custom scrollbars (if the docment supplied them).
            * page/FrameView.h:
            * platform/Scrollbar.h:
            (WebCore::Scrollbar::isCustomScrollbar):
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::destroy):
            Removed the check for document()->frame().  If frame() is 0 in this code,
            then the call to animation() is also incorrect (since it does document()->frame()->animation()).
            * rendering/RenderScrollbar.h:
            (WebCore::RenderScrollbar::isCustomScrollbar):
    
    LayoutTests:
    
    2009-06-22  David Levin  <levin@chromium.org>
    
            Reviewed by David Hyatt and Eric Seidel.
    
            REGRESSION: When the main page (ScrollView) has a custom scrollbar, it crashes on destruction.
            https://bugs.webkit.org/show_bug.cgi?id=26326
    
            Test that refreshing an iframe with custom scrollbars works without crashing.
    
            * scrollbars/resources/page-with-custom-scrollbars.html: Added.
            * scrollbars/scrollbar-crash-on-refresh-expected.txt: Added.
            * scrollbars/scrollbar-crash-on-refresh.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44940 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *           (C) 2006 Alexey Proskuryakov (ap@webkit.org)
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
  * Copyright (C) 2008, 2009 Torch Mobile Inc. All rights reserved. (http://www.torchmobile.com/)
- * Copyright (C) 2008 David Levin (levin@chromium.org)
+ * Copyright (C) 2008, 2009 Google Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -1339,6 +1339,12 @@ void Document::detach()
     // in order to stop media elements
     documentWillBecomeInactive();
     
+    if (m_frame) {
+        FrameView* view = m_frame->view();
+        if (view)
+            view->detachCustomScrollbars();
+    }
+
     // indicate destruction mode,  i.e. attached() but renderer == 0
     setRenderer(0);
 
