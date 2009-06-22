dfc8dd366a29992cbe1e42090c509ee22e2d82b8
arv@google.com <arv@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-22 23:53:09 +0000

    Update the New New Tab Page. There are still a lot of things that are not implemented but it is time to get this submitted and iron out the remaining issues.
    BUG=13362
    
    TEST=Start chrome with --new-new-tab-page and look at play arouind with
    the new tab page.
    
    Review URL: http://codereview.chromium.org/132027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18989 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/new_tab_ui.cc
+++ chrome/browser/dom_ui/new_tab_ui.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/dom_ui/dom_ui_theme_source.h"
 #include "chrome/browser/dom_ui/downloads_dom_handler.h"
 #include "chrome/browser/dom_ui/history_ui.h"
+#include "chrome/browser/dom_ui/shown_sections_handler.h"
 #include "chrome/browser/dom_ui/web_resource_handler.h"
 #include "chrome/browser/history/page_usage_data.h"
 #include "chrome/browser/metrics/user_metrics.h"
@@ -295,6 +296,38 @@ void NewTabHTMLSource::StartDataRequest(const std::string& path,
       l10n_util::GetString(IDS_NEW_TAB_ATTRIBUTION_INTRO));
   localized_strings.SetString(L"resourcecache",
       l10n_util::GetString(IDS_NEW_TAB_WEB_RESOURCE_CACHE));
+  localized_strings.SetString(L"editthumbnail",
+    l10n_util::GetString(IDS_NEW_TAB_EDIT_THUMBNAIL));
+  localized_strings.SetString(L"recentactivities",
+    l10n_util::GetString(IDS_NEW_TAB_RECENT_ACTIVITIES));
+  localized_strings.SetString(L"downloads",
+    l10n_util::GetString(IDS_NEW_TAB_DOWNLOADS));
+  localized_strings.SetString(L"viewfullhistory",
+    l10n_util::GetString(IDS_NEW_TAB_VIEW_FULL_HISTORY));
+  localized_strings.SetString(L"viewalldownloads",
+    l10n_util::GetString(IDS_NEW_TAB_VIEW_ALL_DOWNLOADS));
+  localized_strings.SetString(L"showthumbnails",
+    l10n_util::GetString(IDS_NEW_TAB_SHOW_THUMBNAILS));
+  localized_strings.SetString(L"hidethumbnails",
+    l10n_util::GetString(IDS_NEW_TAB_HIDE_THUMBNAILS));
+  localized_strings.SetString(L"showlist",
+    l10n_util::GetString(IDS_NEW_TAB_SHOW_LIST));
+  localized_strings.SetString(L"hidelist",
+    l10n_util::GetString(IDS_NEW_TAB_HIDE_LIST));
+  localized_strings.SetString(L"showrecent",
+    l10n_util::GetString(IDS_NEW_TAB_SHOW_RECENT));
+  localized_strings.SetString(L"hiderecent",
+    l10n_util::GetString(IDS_NEW_TAB_HIDE_RECENT));
+  localized_strings.SetString(L"showrecommendations",
+    l10n_util::GetString(IDS_NEW_TAB_SHOW_RECOMMENDATIONS));
+  localized_strings.SetString(L"hiderecommendations",
+    l10n_util::GetString(IDS_NEW_TAB_HIDE_RECOMMENDATIONS));
+  localized_strings.SetString(L"thumbnailremovednotification",
+    l10n_util::GetString(IDS_NEW_TAB_THUMBNAIL_REMOVED_NOTIFICATION));
+  localized_strings.SetString(L"undothumbnailremove",
+    l10n_util::GetString(IDS_NEW_TAB_UNDO_THUMBNAIL_REMOVE));
+  localized_strings.SetString(L"otrmessage",
+    l10n_util::GetString(IDS_NEW_TAB_OTR_MESSAGE));
 
   SetFontAndTextDirection(&localized_strings);
 
@@ -1377,11 +1410,10 @@ NewTabUI::NewTabUI(TabContents* contents)
       DownloadManager* dlm = GetProfile()->GetDownloadManager();
       DownloadsDOMHandler* downloads_handler =
           new DownloadsDOMHandler(this, dlm);
-
       AddMessageHandler(downloads_handler);
-      AddMessageHandler(new BrowsingHistoryHandler(this));
-
       downloads_handler->Init();
+
+      AddMessageHandler(new ShownSectionsHandler(this));
     }
 
     if (EnableWebResources())
@@ -1444,6 +1476,8 @@ void NewTabUI::RegisterUserPrefs(PrefService* prefs) {
   MostVisitedHandler::RegisterUserPrefs(prefs);
   if (NewTabUI::EnableWebResources())
     WebResourceHandler::RegisterUserPrefs(prefs);
+  if (NewTabUI::EnableNewNewTabPage())
+    ShownSectionsHandler::RegisterUserPrefs(prefs);
 }
 
 // static
