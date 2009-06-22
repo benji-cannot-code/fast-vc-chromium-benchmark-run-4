c04f8592fbc2ec4fedd87d15dfdf56d168b45fa0
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-22 21:53:52 +0000

    TBR: Fix dead tree.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18964 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service.h
+++ chrome/browser/extensions/extensions_service.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/command_line.h"
 #include "base/file_path.h"
 #include "base/linked_ptr.h"
 #include "base/ref_counted.h"
@@ -74,7 +73,6 @@ class ExtensionsService
   };
 
   ExtensionsService(Profile* profile,
-                    const CommandLine* command_line,
                     MessageLoop* frontend_loop,
                     MessageLoop* backend_loop);
   ~ExtensionsService();
@@ -135,13 +133,12 @@ class ExtensionsService
   // The name of the file that the current active version number is stored in.
   static const char* kCurrentVersionFileName;
 
-  void SetExtensionsEnabled(bool enabled);
-  bool extensions_enabled() { return extensions_enabled_; }
-
+  void set_extensions_enabled(bool enabled) { extensions_enabled_ = enabled; }
   void set_show_extensions_prompts(bool enabled) {
     show_extensions_prompts_ = enabled;
   }
 
+  bool extensions_enabled() { return extensions_enabled_; }
   bool show_extensions_prompts() {
     return show_extensions_prompts_;
   }
@@ -213,12 +210,9 @@ class ExtensionsServiceBackend
   ExtensionsServiceBackend(const FilePath& install_directory,
                            ResourceDispatcherHost* rdh,
                            MessageLoop* frontend_loop,
-                           DictionaryValue* extension_prefs,
-                           bool extensions_enabled);
+                           DictionaryValue* extension_prefs);
 
   virtual ~ExtensionsServiceBackend();
-
-  void set_extensions_enabled(bool enabled) { extensions_enabled_ = enabled; }
 
   // Loads the installed extensions.
   // Errors are reported through ExtensionErrorReporter. On completion,
@@ -405,10 +399,6 @@ class ExtensionsServiceBackend
 
   // The message loop to use to call the frontend.
   MessageLoop* frontend_loop_;
-
-  // Whether non-theme extensions are enabled (themes and externally registered
-  // extensions are always enabled).
-  bool extensions_enabled_;
 
   // A map of all external extension providers.
   typedef std::map<Extension::Location,
