ff8b56e1b575986f92c167010781265f4b9529a9
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-22 23:00:17 +0000

    Pass correct value to setShouldPaintNativeControls.
            Rubber stamped by Mark Rowe.
    
            * DumpRenderTree/win/DumpRenderTree.cpp:
            (main):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44962 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/win/DumpRenderTree.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/win/DumpRenderTree.cpp
@@ -1064,6 +1064,8 @@ RetainPtr<CFURLCacheRef> sharedCFURLCache()
 
 int main(int argc, char* argv[])
 {
+    Sleep(10000);
+
     leakChecking = false;
 
     _setmode(1, _O_BINARY);
@@ -1113,7 +1115,7 @@ int main(int argc, char* argv[])
     COMPtr<IWebPreferencesPrivate> standardPreferencesPrivate;
     if (FAILED(standardPreferences->QueryInterface(&standardPreferencesPrivate)))
         return -1;
-    standardPreferencesPrivate->setShouldPaintNativeControls(TRUE);
+    standardPreferencesPrivate->setShouldPaintNativeControls(FALSE);
 
     COMPtr<IWebView> webView(AdoptCOM, createWebViewAndOffscreenWindow(&webViewWindow));
     if (!webView)
