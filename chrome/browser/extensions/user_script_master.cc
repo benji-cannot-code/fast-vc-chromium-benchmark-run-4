fde7129fe9d1055bd87259f212d139c4b44b472a
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-22 15:23:10 +0000

    Fix bug where content scripts are not run if they are in extensions that are loaded after EXTENSIONS_READY.
    Review URL: http://codereview.chromium.org/140065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18903 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/user_script_master.cc
+++ chrome/browser/extensions/user_script_master.cc
@@ -352,7 +352,7 @@ void UserScriptMaster::Observe(NotificationType type,
           lone_scripts_.push_back(*iter);
         }
       }
-      if (!extensions_service_ready_)
+      if (extensions_service_ready_)
         StartScan();
       break;
     }
