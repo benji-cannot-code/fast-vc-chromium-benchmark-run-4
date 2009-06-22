daf49b291532688dbdc4d126d0adae731835a487
paul@chromium.org <paul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-22 22:22:38 +0000

    WebKit merge 44928:44943.
    Test=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/144007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18969 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/v8/v8_index.h
+++ webkit/port/bindings/v8/v8_index.h
@@ -69,6 +69,7 @@ typedef v8::Persistent<v8::FunctionTemplate> (*FunctionTemplateFactory)();
   V(HTMLCANVASELEMENT, HTMLCanvasElement)                               \
   V(HTMLDATAGRIDCOLELEMENT, HTMLDataGridColElement)                     \
   V(HTMLDATAGRIDELEMENT, HTMLDataGridElement)                           \
+  V(HTMLDATAGRIDROWELEMENT, HTMLDataGridRowElement)                     \
   V(HTMLDIRECTORYELEMENT, HTMLDirectoryElement)                         \
   V(HTMLDIVELEMENT, HTMLDivElement)                                     \
   V(HTMLDLISTELEMENT, HTMLDListElement)                                 \
@@ -275,7 +276,6 @@ typedef v8::Persistent<v8::FunctionTemplate> (*FunctionTemplateFactory)();
   V(DOMIMPLEMENTATION, DOMImplementation)                               \
   V(DOMPARSER, DOMParser)                                               \
   V(DOMSELECTION, DOMSelection)                                         \
-  V(DOMSTRINGLIST, DOMStringList)                                       \
   V(DOMWINDOW, DOMWindow)                                               \
   V(EVENT, Event)                                                       \
   V(EVENTEXCEPTION, EventException)                                     \
