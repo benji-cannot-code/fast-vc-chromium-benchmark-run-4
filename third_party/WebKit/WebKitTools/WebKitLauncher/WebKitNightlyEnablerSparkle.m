b0bd12d79840a30d0f719ea96c464e637671dace
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-22 00:03:43 +0000

    Set a sensible user agent string for the HTTP requests that Sparkle makes (checking for and downloading updates).
    Reviewed by Sam Weinig.
    
    * WebKitLauncher/WebKitNightlyEnablerSparkle.m:
    (userAgentStringForSparkle):
    (initializeSparkle):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44925 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/WebKitLauncher/WebKitNightlyEnablerSparkle.m
+++ third_party/WebKit/WebKitTools/WebKitLauncher/WebKitNightlyEnablerSparkle.m
@@ -123,6 +123,17 @@ static void setMethodImplementation(Method m, IMP imp)
 
 #endif
 
+static NSString *userAgentStringForSparkle()
+{
+    NSBundle *safariBundle = [NSBundle mainBundle];
+    NSString *safariVersion = [[safariBundle localizedInfoDictionary] valueForKey:@"CFBundleShortVersionString"];
+    NSString *safariBuild = [[[safariBundle infoDictionary] valueForKey:(NSString *)kCFBundleVersionKey] substringFromIndex:1];
+    NSString *webKitRevision = [[webKitLauncherBundle() infoDictionary] valueForKey:(NSString *)kCFBundleVersionKey];
+    NSString *applicationName = [NSString stringWithFormat:@"Version/%@ Safari/%@ WebKitRevision/%@", safariVersion, safariBuild, webKitRevision];
+    Class WebView = objc_lookUpClass("WebView");
+    return objc_msgSend(WebView, @selector(_standardUserAgentWithApplicationName:), applicationName);
+}
+
 void initializeSparkle()
 {
     // Override some Sparkle behaviour
@@ -136,6 +147,7 @@ void initializeSparkle()
     setMethodImplementation(methodToPatch, (IMP)updateAlertInitForAlertPanel);
 
     SUUpdater *updater = [SUUpdater updaterForBundle:webKitLauncherBundle()];
+    [updater setUserAgentString:userAgentStringForSparkle()];
 
     // Find the first separator on the Safari menu…
     NSMenu *applicationSubmenu = [[[NSApp mainMenu] itemAtIndex:0] submenu];
