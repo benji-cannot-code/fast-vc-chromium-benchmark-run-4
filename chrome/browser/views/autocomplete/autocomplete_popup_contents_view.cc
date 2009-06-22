4b7e6f59c1fc8df6b7e972bc3e418715f56e482b
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-22 20:25:10 +0000

    More CHECKs in my continuing effort to figure out what is going on.  Neither Ben nor I can get this one.
    BUG=13500
    TEST=none
    Review URL: http://codereview.chromium.org/140009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18947 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/autocomplete/autocomplete_popup_contents_view.cc
+++ chrome/browser/views/autocomplete/autocomplete_popup_contents_view.cc
@@ -280,6 +280,7 @@ AutocompleteResultView::AutocompleteResultView(
       hot_(false),
       font_(font),
       mirroring_context_(new MirroringContext()) {
+  CHECK(model_index >= 0);
   InitClass();
 }
 
