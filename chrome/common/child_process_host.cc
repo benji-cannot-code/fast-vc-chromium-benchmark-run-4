f4d544864205567da2b7cf5685ce4d05a22ca2f2
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-22 19:40:53 +0000

    Cancel all pending requests when a worker goes away.
    BUG=14753
    
    Review URL: http://codereview.chromium.org/140039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18944 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/child_process_host.cc
+++ chrome/common/child_process_host.cc
@@ -61,6 +61,8 @@ ChildProcessHost::ChildProcessHost(
 ChildProcessHost::~ChildProcessHost() {
   Singleton<ChildProcessList>::get()->remove(this);
 
+  resource_dispatcher_host_->CancelRequestsForProcess(GetProcessId());
+
   if (handle()) {
     watcher_.StopWatching();
     ProcessWatcher::EnsureProcessTerminated(handle());
