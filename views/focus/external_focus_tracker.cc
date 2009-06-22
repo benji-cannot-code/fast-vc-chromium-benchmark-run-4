7f96003bf5b52cf12e93db79fcfd39dd9a374624
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-22 15:27:45 +0000

    Revert :
    Changed by: jcampan@chromium.org
    Changed at: Fri 19 Jun 2009 21:22:47
    Branch: src
    Revision: 18889
    Comments:
    Relanding focus manager refactoring with build fix, see:http://codereview.chromium.org/125148BUG=NoneTEST=NoneTBR=ben
    Review URL: http://codereview.chromium.org/141013
    
    Because it creates hundreds of new reliability crashes.
    
    TBR:jcampan
    
    Review URL: http://codereview.chromium.org/140064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18904 0039d316-1c4b-4281-b951-d872f2087c98

--- views/focus/external_focus_tracker.cc
+++ views/focus/external_focus_tracker.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "views/focus/external_focus_tracker.h"
 
-#include "base/logging.h"
 #include "views/view.h"
 #include "views/focus/view_storage.h"
 
@@ -15,8 +14,6 @@ ExternalFocusTracker::ExternalFocusTracker(View* parent_view,
                                            FocusManager* focus_manager)
     : focus_manager_(focus_manager),
       parent_view_(parent_view) {
-  DCHECK(focus_manager);
-  DCHECK(parent_view);
   view_storage_ = ViewStorage::GetSharedInstance();
   last_focused_view_storage_id_ = view_storage_->CreateStorageID();
   // Store the view which is focused when we're created.
--- views/view_win.cc
+++ views/view_win.cc
@@ -18,6 +18,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
+FocusManager* View::GetFocusManager() {
+  Widget* widget = GetWidget();
+  if (!widget)
+    return NULL;
+
+  HWND hwnd = widget->GetNativeView();
+  if (!hwnd)
+    return NULL;
+
+  return FocusManager::GetFocusManager(hwnd);
+}
+
 void View::DoDrag(const MouseEvent& e, int press_x, int press_y) {
   int drag_operations = GetDragOperations(press_x, press_y);
   if (drag_operations == DragDropTypes::DRAG_NONE)
@@ -44,7 +56,7 @@ void View::Focus() {
   // messages.
   FocusManager* focus_manager = GetFocusManager();
   if (focus_manager)
-    focus_manager->FocusNativeView(GetRootView()->GetWidget()->GetNativeView());
+    focus_manager->FocusHWND(GetRootView()->GetWidget()->GetNativeView());
 }
 
 int View::GetHorizontalDragThreshold() {
