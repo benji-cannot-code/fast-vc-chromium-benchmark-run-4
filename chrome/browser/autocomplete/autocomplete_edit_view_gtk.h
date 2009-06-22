f571a1dfd3d8104cb4c423ded23d3c6b19e03981
mhm@chromium.org <mhm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-22 22:19:34 +0000

    Paste and Go was missing on the omnibar this CL will append a menu item to the current Autocomplete edit box and by using Gtk clipboard, it will paste and search it.
    BUG=14800 (http://crbug.com/14800)
    
    TEST=Selected text from view and right clicked copied. Went to the omnibox and right clicked > Paste and Go. It navigated to the page successfully.
    
    Review URL: http://codereview.chromium.org/140050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18967 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view_gtk.h
+++ chrome/browser/autocomplete/autocomplete_edit_view_gtk.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/scoped_ptr.h"
+#include "base/string_util.h"
 #include "chrome/browser/autocomplete/autocomplete_edit_view.h"
 #include "chrome/browser/toolbar_model.h"
 #include "chrome/common/owned_widget_gtk.h"
@@ -183,6 +184,23 @@ class AutocompleteEditViewGtk : public AutocompleteEditView {
         HandleEditSearchEngines();
   }
   void HandleEditSearchEngines();
+
+  static void HandlePasteAndGoThunk(GtkMenuItem* menuitem,
+                                    AutocompleteEditViewGtk* self) {
+    self->HandlePasteAndGo();
+  }
+  void HandlePasteAndGo();
+
+  static void HandlePasteAndGoReceivedTextThunk(GtkClipboard* clipboard,
+                                                const gchar* text,
+                                                gpointer self) {
+    // If there is nothing to paste (|text| is NULL), do nothing.
+    if (!text)
+      return;
+    reinterpret_cast<AutocompleteEditViewGtk*>(self)->
+        HandlePasteAndGoReceivedText(UTF8ToWide(text));
+  }
+  void HandlePasteAndGoReceivedText(const std::wstring& text);
 
   // Get the character indices of the current selection.  This honors
   // direction, cp_max is the insertion point, and cp_min is the bound.
