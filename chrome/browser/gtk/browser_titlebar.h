9e750d176edbb180be53774ff8de674dd377bd91
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-22 20:56:15 +0000

    Move the titlebar background drawing from the titlebar to the browser window.  This fixes a visual bug where the border looked disconnected with the background.  This matches what Windows does.
    BUG=14884
    
    Review URL: http://codereview.chromium.org/140078
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18954 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_titlebar.h
+++ chrome/browser/gtk/browser_titlebar.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/scoped_ptr.h"
 #include "chrome/browser/gtk/menu_gtk.h"
-#include "chrome/browser/gtk/nine_box.h"
 
 class BrowserWindowGtk;
 class CustomDrawButton;
@@ -52,11 +51,6 @@ class BrowserTitlebar : public MenuGtk::Delegate {
                                         int image_hot, GtkWidget* box,
                                         int tooltip);
 
-  // Callback for when the titlebar (include the background of the tab strip)
-  // needs to be redrawn.
-  static gboolean OnExpose(GtkWidget* widget, GdkEventExpose* e,
-                           BrowserTitlebar* window);
-
   // Callback for changes to window state.  This includes
   // maximizing/restoring/minimizing the window.
   static gboolean OnWindowStateChanged(GtkWindow* window,
@@ -91,10 +85,6 @@ class BrowserTitlebar : public MenuGtk::Delegate {
   scoped_ptr<CustomDrawButton> maximize_button_;
   scoped_ptr<CustomDrawButton> restore_button_;
   scoped_ptr<CustomDrawButton> close_button_;
-
-  // The background of the title bar and tab strip.
-  scoped_ptr<NineBox> titlebar_background_;
-  scoped_ptr<NineBox> titlebar_background_otr_;
 
   // The context menu.
   scoped_ptr<MenuGtk> context_menu_;
