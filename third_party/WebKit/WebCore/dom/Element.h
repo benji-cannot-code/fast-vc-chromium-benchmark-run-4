ff4e3d0312b16cd438ef2050a9cbbd07e721fa02
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-23 01:04:58 +0000

    2009-06-22  Darin Adler  <darin@apple.com>
            Reviewed by Sam Weinig.
    
            Bug 25425: DOM attribute getter/setter functions should use const AtomicString& type
            https://bugs.webkit.org/show_bug.cgi?id=25425
    
            * bindings/scripts/CodeGeneratorJS.pm: Added handling for ReflectURL.
            * bindings/scripts/CodeGeneratorObjC.pm: Ditto.
    
            * dom/Element.cpp:
            (WebCore::Element::getURLAttribute): Added. For use implementing getters
            for ReflectURL.
            * dom/Element.h: Added getURLAttribute.
    
            * html/HTMLImageElement.idl: Use Reflect and ReflectURL.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Element.h
+++ third_party/WebKit/WebCore/dom/Element.h
@@ -62,7 +62,7 @@ public:
     void setAttribute(const AtomicString& name, const AtomicString& value, ExceptionCode&);
     void setAttributeNS(const AtomicString& namespaceURI, const AtomicString& qualifiedName, const AtomicString& value, ExceptionCode&);
 
-    void scrollIntoView (bool alignToTop = true);
+    void scrollIntoView(bool alignToTop = true);
     void scrollIntoViewIfNeeded(bool centerIfNeeded = true);
 
     void scrollByUnits(int units, ScrollGranularity);
@@ -159,6 +159,7 @@ public:
     virtual void accessKeyAction(bool /*sendToAnyEvent*/) { }
 
     virtual bool isURLAttribute(Attribute*) const;
+    KURL getURLAttribute(const QualifiedName&) const;
     virtual const QualifiedName& imageSourceAttributeName() const;
     virtual String target() const { return String(); }
 
