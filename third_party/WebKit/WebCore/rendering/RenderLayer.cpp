1e79690d7c54a2a4ca0179a72835c8a6d3edbc96
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-23 21:48:15 +0000

    2009-06-23  Simon Fraser  <simon.fraser@apple.com>
            Build fix.
    
            Wrap shouldDoSoftwarePaint() in #if USE(ACCELERATED_COMPOSITING) to avoid warning when
            ACCELERATED_COMPOSITING is not defined.
    
            * rendering/RenderLayer.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayer.cpp
@@ -1931,10 +1931,12 @@ static void performOverlapTests(RenderObject::OverlapTestRequestMap& overlapTest
         overlapTestRequests.remove(overlappedRequestClients[i]);
 }
 
+#if USE(ACCELERATED_COMPOSITING)
 static bool shouldDoSoftwarePaint(const RenderLayer* layer, bool paintingReflection)
 {
     return paintingReflection && !layer->has3DTransform();
 }
+#endif
 
 void RenderLayer::paintLayer(RenderLayer* rootLayer, GraphicsContext* p,
                         const IntRect& paintDirtyRect, PaintRestriction paintRestriction,
