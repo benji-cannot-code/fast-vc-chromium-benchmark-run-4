bc906676a143d76c4e88e85383b5097c5fe250ca
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-23 23:19:56 +0000

    linux: Fix windowed plugin resize
    The plugin window was not resized properly if its size depends on the page size and that gets changed (exposed by some O3D samples).
    This CL does basically 2 things:
    - when resizing the plugin, queue a resize to signal the parent that the size request has changed.
    - explicitly store the size, to avoid relying on the allocation, that could be changed by the parent.
    
    Review URL: http://codereview.chromium.org/144023
    Patch from Antoine Labour <piman@google.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19079 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/gtk_plugin_container.cc
+++ webkit/glue/plugins/gtk_plugin_container.cc
@@ -7,16 +7,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <gtk/gtk.h>
 
+#include "base/basictypes.h"
+
 namespace {
 
-// This class has no members/methods, and is only used for namespacing purposes.
-class GtkPluginContainer {
+// NOTE: This class doesn't have constructors/destructors, it is created
+// through GLib's object management.
+class GtkPluginContainer : public GtkSocket {
  public:
   static GtkWidget* CreateNewWidget() {
     GtkWidget* container = GTK_WIDGET(g_object_new(GetType(), NULL));
     g_signal_connect(GTK_SOCKET(container), "plug-removed",
                      G_CALLBACK(OnPlugRemoved), NULL);
     return container;
+  }
+
+  // Sets the requested size of the widget.
+  void set_size(int width, int height) {
+    width_ = width;
+    height_ = height;
+  }
+
+  // Casts a widget into a GtkPluginContainer, after checking the type.
+  template <class T>
+  static GtkPluginContainer *CastChecked(T *instance) {
+    return G_TYPE_CHECK_INSTANCE_CAST(instance, GetType(), GtkPluginContainer);
   }
 
  private:
@@ -30,7 +45,7 @@ class GtkPluginContainer {
         static_cast<GClassInitFunc>(&ClassInit),
         NULL, NULL,
         sizeof(GtkSocket),  // We are identical to a GtkSocket.
-        0, NULL,
+        0, &InstanceInit,
       };
       type = g_type_register_static(GTK_TYPE_SOCKET,
                                     "GtkPluginContainer",
@@ -46,11 +61,18 @@ class GtkPluginContainer {
     widget_class->size_request = &HandleSizeRequest;
   }
 
+  // Implementation of the instance initializer (constructor).
+  static void InstanceInit(GTypeInstance *instance, gpointer klass) {
+    GtkPluginContainer *container = CastChecked(instance);
+    container->set_size(0, 0);
+  }
+
   // Report our allocation size during size requisition.
   static void HandleSizeRequest(GtkWidget* widget,
                                 GtkRequisition* requisition) {
-    requisition->width = widget->allocation.width;
-    requisition->height = widget->allocation.height;
+    GtkPluginContainer *container = CastChecked(widget);
+    requisition->width = container->width_;
+    requisition->height = container->height_;
   }
 
   static gboolean OnPlugRemoved(GtkSocket* socket) {
@@ -58,6 +80,10 @@ class GtkPluginContainer {
     // We return TRUE to indicate that we don't want to destroy our side.
     return TRUE;
   }
+
+  int width_;
+  int height_;
+  DISALLOW_IMPLICIT_CONSTRUCTORS(GtkPluginContainer);
 };
 
 }  // anonymous namespace
@@ -65,4 +91,10 @@ class GtkPluginContainer {
 // Create a new instance of our GTK widget object.
 GtkWidget* gtk_plugin_container_new() {
   return GtkPluginContainer::CreateNewWidget();
+}
+
+void gtk_plugin_container_set_size(GtkWidget *widget, int width, int height) {
+  GtkPluginContainer::CastChecked(widget)->set_size(width, height);
+  // Signal the parent that the size request has changed.
+  gtk_widget_queue_resize_no_redraw(widget);
 }
--- webkit/glue/plugins/gtk_plugin_container.h
+++ webkit/glue/plugins/gtk_plugin_container.h
@@ -11,9 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // directly, so we need a subclass of GtkSocket that sidesteps the
 // size_request handler.
 //
-// The custom size_request handler just reports the allocation, so it's
-// the owner's responsibility to call gtk_widget_size_allocate() with the
-// proper size.
+// The custom size_request handler just reports the size set by
+// gtk_plugin_container_set_size.
 
 typedef struct _GtkWidget GtkWidget;
 
@@ -21,5 +20,8 @@ typedef struct _GtkWidget GtkWidget;
 // Intentionally GTK-style here since we're creating a custom GTK widget.
 // This is a GtkSocket subclass; see its documentation for available methods.
 GtkWidget* gtk_plugin_container_new();
+
+// Sets the size of the GtkPluginContainer.
+void gtk_plugin_container_set_size(GtkWidget *widget, int width, int height);
 
 #endif  // WEBKIT_GLUE_PLUGINS_GTK_PLUGIN_CONTAINER_H_
