b691b76b2359c6625e320126f049f943a75fed21
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-23 18:03:12 +0000

    WebCore:
    2009-06-23  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Darin Adler.
    
            WebCore side of <rdar://problem/6946094>.
            
            * platform/win/ClipboardWin.cpp:
            (WebCore::ClipboardWin::setExternalDataObject):
            * platform/win/ClipboardWin.h:
    
    WebKit/win:
    
    2009-06-23  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Darin Adler.
    
            WebKit side of <rdar://problem/6946094>.
    
            * Interfaces/IWebUIDelegate.idl:
            Add a newPasteboard out parameter to willPerformDragSourceAction.
            
            * WebCoreSupport/WebDragClient.cpp:
            (WebDragClient::willPerformDragSourceAction):
            Implement this. If the client returns a new pasteboard, use it for the drag.
            
            * WebCoreSupport/WebInspectorDelegate.h:
            (WebInspectorDelegate::willPerformDragSourceAction):
            Add the new parameter.
    
    WebKitTools:
    
    2009-06-23  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Darin Adler.
            
            Update for WebKit changes.
            
            * DumpRenderTree/win/UIDelegate.h:
            (UIDelegate::willPerformDragSourceAction):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44995 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/ClipboardWin.h
+++ third_party/WebKit/WebCore/platform/win/ClipboardWin.h
@@ -72,6 +72,8 @@ namespace WebCore {
 
         COMPtr<IDataObject> dataObject() { return m_dataObject; }
 
+        void setExternalDataObject(IDataObject *dataObject);
+
     private:
         ClipboardWin(bool isForDragging, IDataObject*, ClipboardAccessPolicy);
         ClipboardWin(bool isForDragging, WCDataObject*, ClipboardAccessPolicy);
