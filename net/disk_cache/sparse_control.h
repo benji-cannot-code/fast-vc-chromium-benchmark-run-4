466c6d24e3e51d3f0f266ce7ed9348f62220cb30
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-23 22:07:36 +0000

    Disk Cache: Implement GetAvailableRange for the regular disk cache.
    This is required to enable sparse caching.
    
    BUG=12258
    TEST=unittest
    
    Review URL: http://codereview.chromium.org/146005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19069 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/sparse_control.h
+++ net/disk_cache/sparse_control.h
@@ -35,7 +35,8 @@ class SparseControl {
   enum SparseOperation {
     kNoOperation,
     kReadOperation,
-    kWriteOperation
+    kWriteOperation,
+    kGetRangeOperation
   };
 
   explicit SparseControl(EntryImpl* entry)
@@ -110,6 +111,9 @@ class SparseControl {
   // work.
   bool DoChildIO();
 
+  // Performs the required work for GetAvailableRange for one child.
+  int DoGetAvailableRange();
+
   // Performs the required work after a single IO operations finishes.
   void DoChildIOCompleted(int result);
 
@@ -125,6 +129,7 @@ class SparseControl {
   bool pending_;  // True if any child IO operation returned pending.
   bool finished_;
   bool init_;
+  bool range_found_;  // True if GetAvailableRange found something.
 
   SparseHeader sparse_header_;  // Data about the children of entry_.
   Bitmap children_map_;  // The actual bitmap of children.
