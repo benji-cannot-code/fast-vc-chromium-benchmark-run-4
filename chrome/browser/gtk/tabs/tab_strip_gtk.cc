acf991f49e2a159d28e4f706baa3f44e88994b25
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-23 22:43:14 +0000

    Syncs to latest messages and makes tab overview button work.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/147047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19073 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/tabs/tab_strip_gtk.cc
+++ chrome/browser/gtk/tabs/tab_strip_gtk.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/slide_animation.h"
 #include "base/gfx/gtk_util.h"
 #include "base/gfx/point.h"
-#include "chrome/browser/browser.h"
 #include "chrome/browser/browser_theme_provider.h"
 #include "chrome/browser/gtk/custom_button.h"
 #include "chrome/browser/gtk/dnd_registry.h"
@@ -21,6 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/gtk_util.h"
 #include "grit/app_resources.h"
 #include "grit/theme_resources.h"
+
+#if defined(LINUX2)
+#include "chrome/browser/browser.h"
+#include "chrome/browser/browser_list.h"
+#include "chrome/browser/gtk/browser_window_gtk.h"
+#include "chrome/browser/views/tabs/tab_overview_types.h"
+#include "chrome/common/x11_util.h"
+#endif
 
 namespace {
 
@@ -1582,6 +1589,14 @@ CustomDrawButton* TabStripGtk::MakeTabOverviewButton() {
 // static
 void TabStripGtk::OnTabOverviewButtonClicked(GtkWidget* widget,
                                              TabStripGtk* tabstrip) {
-  // TODO(sky): implement me.
+  Browser* browser = BrowserList::GetLastActive();
+  DCHECK(browser);  // In order for the user to click on the tab there should
+                    // be an active browser.
+  TabOverviewTypes::Message message;
+  message.set_type(TabOverviewTypes::Message::WM_SWITCH_TO_OVERVIEW_MODE);
+  GtkWidget* browser_widget = GTK_WIDGET(
+      static_cast<BrowserWindowGtk*>(browser->window())->GetNativeHandle());
+  message.set_param(0, x11_util::GetX11WindowFromGtkWidget(browser_widget));
+  TabOverviewTypes::instance()->SendMessage(message);
 }
 #endif
