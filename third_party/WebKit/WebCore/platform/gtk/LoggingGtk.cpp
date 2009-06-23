ea68d81f7c63998ebd12511c6038b0cc06f72669
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-23 12:43:47 +0000

    2009-06-23  Tor Arne Vestbø  <tor.arne.vestbo@nokia.com>
            Reviewed by Simon Hausmann.
    
            Remove duplicated code in Qt & GTK logging initialization
    
            We now share the getChannelFromName() function in Logging.cpp
    
            * platform/gtk/LoggingGtk.cpp:
            * platform/qt/LoggingQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@44989 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/LoggingGtk.cpp
+++ third_party/WebKit/WebCore/platform/gtk/LoggingGtk.cpp
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "Logging.h"
+#include "PlatformString.h"
 
 #include <glib.h>
 #include <string.h>
@@ -28,55 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 // Inspired by the code used by the Qt port
-
-static WTFLogChannel* getChannelFromName(const char* channelName)
-{
-    if (strlen(channelName) < 3)
-        return 0;
-
-    if (!g_ascii_strcasecmp(channelName, "BackForward"))
-        return &LogBackForward;
-    if (!g_ascii_strcasecmp(channelName, "Editing"))
-        return &LogEditing;
-    if (!g_ascii_strcasecmp(channelName, "Events"))
-        return &LogEvents;
-    if (!g_ascii_strcasecmp(channelName, "Frames"))
-        return &LogFrames;
-    if (!g_ascii_strcasecmp(channelName, "FTP"))
-        return &LogFTP;
-    if (!g_ascii_strcasecmp(channelName, "History"))
-        return &LogHistory;
-    if (!g_ascii_strcasecmp(channelName, "IconDatabase"))
-        return &LogIconDatabase;
-    if (!g_ascii_strcasecmp(channelName, "Loading"))
-        return &LogLoading;
-    if (!g_ascii_strcasecmp(channelName, "Media"))
-        return &LogMedia;
-    if (!g_ascii_strcasecmp(channelName, "Network"))
-        return &LogNetwork;
-    if (!g_ascii_strcasecmp(channelName, "NotYetImplemented"))
-        return &LogNotYetImplemented;
-    if (!g_ascii_strcasecmp(channelName, "PageCache"))
-        return &LogPageCache;
-    if (!g_ascii_strcasecmp(channelName, "PlatformLeaks"))
-        return &LogPlatformLeaks;
-    if (!g_ascii_strcasecmp(channelName, "Plugins"))
-        return &LogPlugins;
-    if (!g_ascii_strcasecmp(channelName, "PopupBlocking"))
-        return &LogPopupBlocking;
-    if (!g_ascii_strcasecmp(channelName, "SpellingAndGrammar"))
-        return &LogSpellingAndGrammar;
-    if (!g_ascii_strcasecmp(channelName, "SQLDatabase"))
-        return &LogSQLDatabase;
-    if (!g_ascii_strcasecmp(channelName, "StorageAPI"))
-        return &LogStorageAPI;
-    if (!g_ascii_strcasecmp(channelName, "TextConversion"))
-        return &LogTextConversion;
-    if (!g_ascii_strcasecmp(channelName, "Threading"))
-        return &LogThreading;
-
-    return 0;
-}
 
 void InitializeLoggingChannelsIfNecessary()
 {
@@ -99,10 +51,8 @@ void InitializeLoggingChannelsIfNecessary()
     char** logv = g_strsplit(logEnv, " ", -1);
 
     for (int i = 0; logv[i]; i++) {
-        WTFLogChannel* channel = getChannelFromName(logv[i]);
-        if (!channel)
-            continue;
-        channel->state = WTFLogChannelOn;
+        if (WTFLogChannel* channel = getChannelFromName(logv[i]))
+            channel->state = WTFLogChannelOn;
     }
 
     g_strfreev(logv);
