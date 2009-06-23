c4142d354f5ed3b3d76dc349bfce60e6e74926b4
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-23 21:25:11 +0000

    2009-06-23  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dave Hyatt.
    
            https://bugs.webkit.org/show_bug.cgi?id=24863
    
            Fix reflection painting on elements with compositing layers.
    
            Test: compositing/reflections/reflection-on-composited.html
    
            * rendering/RenderLayer.h:
            Add a new PaintLayerPaintingReflection flag which is set while painting inside
            reflections.
            (WebCore::RenderLayer::setPaintingInsideReflection):
            Add a setter so that RenderLayerBacking can call it.
    
            * rendering/RenderLayer.cpp:
            (WebCore::shouldDoSoftwarePaint):
            Utility to clarify the logic of when a composited layer should do a software paint.
    
            (WebCore::RenderLayer::paintLayer):
            Use shouldDoSoftwarePaint() to determine when to software-paint a reflected layer.
            Remove transform-related tests from the reflection-painting if test because they appear
            to be unnecessary.
    
            * rendering/RenderLayerBacking.cpp:
            (WebCore::RenderLayerBacking::paintIntoLayer):
            Add the code to paint reflections to the composited layer painting method.
    
            * rendering/RenderReplica.cpp:
            (WebCore::RenderReplica::paint):
            Set the PaintLayerPaintingReflection flag.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayer.cpp
@@ -1931,6 +1931,11 @@ static void performOverlapTests(RenderObject::OverlapTestRequestMap& overlapTest
         overlapTestRequests.remove(overlappedRequestClients[i]);
 }
 
+static bool shouldDoSoftwarePaint(const RenderLayer* layer, bool paintingReflection)
+{
+    return paintingReflection && !layer->has3DTransform();
+}
+
 void RenderLayer::paintLayer(RenderLayer* rootLayer, GraphicsContext* p,
                         const IntRect& paintDirtyRect, PaintRestriction paintRestriction,
                         RenderObject* paintingRoot, RenderObject::OverlapTestRequestMap* overlapTestRequests,
@@ -1942,7 +1947,7 @@ void RenderLayer::paintLayer(RenderLayer* rootLayer, GraphicsContext* p,
         // but we need to ensure that we don't cache clip rects computed with the wrong root in this case.
         if (p->updatingControlTints())
             paintFlags |= PaintLayerTemporaryClipRects;
-        else if (!backing()->paintingGoesToWindow()) {
+        else if (!backing()->paintingGoesToWindow() && !shouldDoSoftwarePaint(this, paintFlags & PaintLayerPaintingReflection)) {
             // If this RenderLayer should paint into its backing, that will be done via RenderLayerBacking::paintIntoLayer().
             return;
         }
@@ -2013,10 +2018,10 @@ void RenderLayer::paintLayer(RenderLayer* rootLayer, GraphicsContext* p,
     bool haveTransparency = localPaintFlags & PaintLayerHaveTransparency;
 
     // Paint the reflection first if we have one.
-    if (m_reflection && !m_paintingInsideReflection && (!m_transform || (paintFlags & PaintLayerAppliedTransform))) {
+    if (m_reflection && !m_paintingInsideReflection) {
         // Mark that we are now inside replica painting.
         m_paintingInsideReflection = true;
-        reflectionLayer()->paintLayer(rootLayer, p, paintDirtyRect, paintRestriction, paintingRoot, overlapTestRequests, localPaintFlags);
+        reflectionLayer()->paintLayer(rootLayer, p, paintDirtyRect, paintRestriction, paintingRoot, overlapTestRequests, localPaintFlags | PaintLayerPaintingReflection);
         m_paintingInsideReflection = false;
     }
 
