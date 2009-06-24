37e7e2df625613923795bf42dc6dd0b369b6431e
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-24 22:14:23 +0000

    Fix extension unpacking on linux/mac
    Add a unittest that pretends to be the UtilityProcessHost and launches a chrome utility process for unpacking a theme.
    
    Review URL: http://codereview.chromium.org/147001
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19181 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -158,6 +158,8 @@ extern const wchar_t kTestName[];
 
 extern const wchar_t kRendererCmdPrefix[];
 
+extern const wchar_t kUtilityCmdPrefix[];
+
 extern const wchar_t kNewFtp[];
 
 extern const wchar_t kIPCUseFIFO[];
