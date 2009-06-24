53d9de2b2699f020c06b99e7dc0c95db5a00e918
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-24 18:15:13 +0000

    perfbot BUG=none TEST=none
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19146 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_strip_controller.mm
+++ chrome/browser/cocoa/tab_strip_controller.mm
@@ -472,7 +472,9 @@ NSString* const kTabStripNumberOfTabsChanged = @"kTabStripNumberOfTabsChanged";
     }
     else if (!contents->is_loading()) {
       // Set everything back to normal, we're done loading.
+#if 0
       [tabController setIconView:[self favIconImageViewForContents:contents]];
+#endif
       [tabController setWaiting:NO];
       [tabController setLoading:NO];
     }
