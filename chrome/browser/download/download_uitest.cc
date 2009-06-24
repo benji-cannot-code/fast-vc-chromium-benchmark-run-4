75eb66a95db39bc8cdc2f8983502335dd959da6e
jshin@chromium.org <jshin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-24 17:08:21 +0000

    Disable PerWindowShelf test on Linux because it's flaky
    BUG=15211 (http://crbug.com/15211 )
    TEST=PerWindowShelf test does not show up as a failure on Linux bot
    TBR=thakis
    Review URL: http://codereview.chromium.org/146091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19135 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_uitest.cc
+++ chrome/browser/download/download_uitest.cc
@@ -31,9 +31,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_LINUX)
 #define MAYBE_UnknownSize DISABLED_UnknownSize
 #define MAYBE_IncognitoDownload DISABLED_IncognitoDownload
+// It's flaky on Linux (http://crbug.com/15211)
+#define MAYBE_PerWindowShelf DISABLED_PerWindowShelf
 #else
 #define MAYBE_UnknownSize UnknownSize
 #define MAYBE_IncognitoDownload IncognitoDownload
+#define MAYBE_PerWindowShelf PerWindowShelf
 #endif
 
 namespace {
@@ -235,7 +238,7 @@ TEST_F(DownloadTest, ContentDisposition) {
 // Test that the download shelf is per-window by starting a download in one
 // tab, opening a second tab, closing the shelf, going back to the first tab,
 // and checking that the shelf is closed.
-TEST_F(DownloadTest, PerWindowShelf) {
+TEST_F(DownloadTest, MAYBE_PerWindowShelf) {
   FilePath file(FILE_PATH_LITERAL("download-test3.gif"));
   FilePath download_file(FILE_PATH_LITERAL("download-test3-attachment.gif"));
 
