3e18d5a806741697f58a391acdef856df879732d
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-24 16:21:27 +0000

    perfbot
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19127 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_strip_controller.mm
+++ chrome/browser/cocoa/tab_strip_controller.mm
@@ -470,6 +470,7 @@ NSString* const kTabStripNumberOfTabsChanged = @"kTabStripNumberOfTabsChanged";
 #else
       NSView* throbber = [[[NSView alloc] initWithFrame:frame] autorelease];
 #endif
+      throbber = nil;
 #if 0
       [tabController setIconView:throbber];
 #endif
