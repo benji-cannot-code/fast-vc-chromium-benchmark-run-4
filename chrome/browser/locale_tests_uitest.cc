c18dd4f0d2022de3a9b65966ffb26518e9452ced
jshin@chromium.org <jshin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-24 23:28:44 +0000

    Change zh-tw to zh-TW when it's used to find the locale data pack on Linux (case-sensitive file system).
    BUG=8647
    TEST=Pass ui test on Linux
    TBR=tony
    
    Review URL: http://codereview.chromium.org/146117
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19197 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/locale_tests_uitest.cc
+++ chrome/browser/locale_tests_uitest.cc
@@ -22,7 +22,7 @@ class LocaleTestsHe : public UITest {
 class LocaleTestsZhTw : public UITest {
  public:
   LocaleTestsZhTw() : UITest() {
-    launch_arguments_.AppendSwitchWithValue(L"lang", L"zh-tw");
+    launch_arguments_.AppendSwitchWithValue(L"lang", L"zh-TW");
   }
 };
 
