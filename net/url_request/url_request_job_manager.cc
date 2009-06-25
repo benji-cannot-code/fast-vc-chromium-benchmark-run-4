c8ded2bf2f240f2c8979be088f666156af5987bc
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-25 17:32:14 +0000

    [Second attempt]
    Use the new portable FTP implementation on non-Windows platforms.
    Note: the new FTP is still work in progress.
    
    Original review URL: http://codereview.chromium.org/119342
    
    R=dank
    BUG=http://crbug.com/4965
    TEST=none
    Review URL: http://codereview.chromium.org/146044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19258 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_job_manager.cc
+++ net/url_request/url_request_job_manager.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "net/url_request/url_request_ftp_job.h"
 #else
-// TODO(playmobil): Implement on non-windows platforms.
+#include "net/url_request/url_request_new_ftp_job.h"
 #endif
 #include "net/url_request/url_request_http_job.h"
 #include "net/url_request/url_request_view_cache_job.h"
@@ -39,7 +39,7 @@ static const SchemeToFactory kBuiltinFactories[] = {
 #if defined(OS_WIN)
   { "ftp", URLRequestFtpJob::Factory },
 #else
-// TODO(playmobil): Implement on non-windows platforms.
+  { "ftp", URLRequestNewFtpJob::Factory },
 #endif
   { "about", URLRequestAboutJob::Factory },
   { "view-cache", URLRequestViewCacheJob::Factory },
