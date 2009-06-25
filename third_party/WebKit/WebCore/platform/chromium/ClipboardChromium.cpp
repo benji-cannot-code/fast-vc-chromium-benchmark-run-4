e051a186859ed6f463c6d24cbc6a1df81e1cb873
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-25 15:50:51 +0000

    2009-06-25  Dimitri Glazkov  <dglazkov@chromium.org>
            Unreviewed, build fix.
    
            Add FileList.h include to fix Chromium build.
    
            * platform/chromium/ClipboardChromium.cpp: Added FileList.h include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45176 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/ClipboardChromium.cpp
+++ third_party/WebKit/WebCore/platform/chromium/ClipboardChromium.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ClipboardUtilitiesChromium.h"
 #include "Document.h"
 #include "Element.h"
+#include "FileList.h"
 #include "Frame.h"
 #include "HTMLNames.h"
 #include "NamedAttrMap.h"
