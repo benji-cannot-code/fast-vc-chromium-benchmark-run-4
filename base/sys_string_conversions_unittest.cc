7669179d3c5e9cc9063068be103438a11e5cd306
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-25 12:05:07 +0000

    Only run the UTF-8 assuming sys conversion tests on Linux.
    Review URL: http://codereview.chromium.org/147146
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19240 0039d316-1c4b-4281-b951-d872f2087c98

--- base/sys_string_conversions_unittest.cc
+++ base/sys_string_conversions_unittest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/basictypes.h"
 #include "base/string_piece.h"
 #include "base/string_util.h"
 #include "base/sys_string_conversions.h"
@@ -70,6 +71,7 @@ TEST(SysStrings, SysUTF8ToWide) {
 }
 
 // We assume the test is running in a UTF8 locale.
+#if defined(OS_LINUX)
 TEST(SysStrings, SysWideToNativeMB) {
   using base::SysWideToNativeMB;
   EXPECT_EQ("Hello, world", SysWideToNativeMB(L"Hello, world"));
@@ -177,3 +179,4 @@ TEST(SysStrings, SysNativeMBAndWide) {
     EXPECT_EQ(wide, trip);
   }
 }
+#endif  // OS_LINUX
