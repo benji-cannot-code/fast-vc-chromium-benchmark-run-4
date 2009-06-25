7c3730bc37c0f3000aa7a77c37198d1f4a6563cb
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-25 13:58:34 +0000

    Add favicons to tabs on the Mac. Also moved SkBitmapToNSImage() to skia/ext/skia_utils_mac.h and removed chrome/browser/cocoa/cocoa_utils.h.
    Patch by rsesek.
    
    BUG=13565
    
    Review URL: http://codereview.chromium.org/131018
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19244 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/skia_utils_mac.mm
+++ skia/ext/skia_utils_mac.mm
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_cftyperef.h"
 #include "base/scoped_ptr.h"
 #include "skia/ext/bitmap_platform_device_mac.h"
+#include "third_party/skia/include/utils/mac/SkCGUtils.h"
+#include "third_party/skia/include/core/SkColorPriv.h"
 
 namespace gfx {
 
@@ -164,6 +166,19 @@ SkBitmap NSImageToSkBitmap(NSImage* image, NSSize size, bool is_opaque) {
   [NSGraphicsContext setCurrentContext:current_context];
 
   return bitmap;
+}
+
+NSImage* SkBitmapToNSImage(const SkBitmap& skiaBitmap) {
+  // First convert SkBitmap to CGImageRef.
+  CGImageRef cgimage = SkCreateCGImageRef(skiaBitmap);
+
+  // Now convert to NSImage.
+  NSBitmapImageRep* bitmap = [[[NSBitmapImageRep alloc]
+                                   initWithCGImage:cgimage] autorelease];
+  CFRelease(cgimage);
+  NSImage* image = [[[NSImage alloc] init] autorelease];
+  [image addRepresentation:bitmap];
+  return image;
 }
 
 }  // namespace gfx
