62320817cfd4988ac263d4e7306f062c7c54f71d
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-25 23:49:39 +0000

    Remove unused num_rows argument to OptionsLayoutBuilderGtk
    BUG=none
    
    Review URL: http://codereview.chromium.org/149037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19312 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/options/options_layout_gtk.h
+++ chrome/browser/gtk/options/options_layout_gtk.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class OptionsLayoutBuilderGtk {
  public:
-  explicit OptionsLayoutBuilderGtk(int num_rows);
+  explicit OptionsLayoutBuilderGtk();
 
   GtkWidget* get_page_widget() {
     return page_;
