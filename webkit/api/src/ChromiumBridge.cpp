2e4da62063ca5dd431212d9fe2e962255f873a1b
dpranke@google.com <dpranke@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-25 00:02:52 +0000

    implement WebSandboxSupport stub to prevent a null ptr deref
    If FontChromiumWin's Font::drawGlyphs() fails for some reason, we
    may attempt to reload the font using ChromiumBridge::ensureFontLoaded().
    This requires a sandbox routine, SandboxSupport::ensureFontLoaded() to be
    implemented, and in test_shell, it wasn't, causing a null ptr deref and
    weird things to happen.
    
    Note that there is no easy way to provoke this codepath once the other
    bugs causing the crash in #10977 are fixed.
    
    R=darin
    BUG=http://crbug.org/10977
    TEST=none
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19200 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/src/ChromiumBridge.cpp
+++ webkit/api/src/ChromiumBridge.cpp
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2009 Google Inc. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *     * Neither the name of Google Inc. nor the names of its
  * contributors may be used to endorse or promote products derived from
  * this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -149,7 +149,11 @@ void ChromiumBridge::prefetchDNS(const String& hostname)
 #if defined(OS_WIN)
 bool ChromiumBridge::ensureFontLoaded(HFONT font)
 {
-    return webKitClient()->sandboxSupport()->ensureFontLoaded(font);
+    WebSandboxSupport* ss = webKitClient()->sandboxSupport();
+
+    // if there is no sandbox, then we can assume the font
+    // was able to be loaded successfully already
+    return ss ? ss->ensureFontLoaded(font) : true;
 }
 #endif
 
