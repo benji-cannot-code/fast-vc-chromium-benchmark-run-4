0009992774f4c39037f7a62e6543e439df264274
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-25 20:34:04 +0000

    WebCore:
    2009-06-25  John Gregg  <johnnyg@google.com>
    
            Reviewed by Sam Weinig.
    
            Bug 23721: Changing dropdown's selectedIndex within onchange handler fires another onchange
            https://bugs.webkit.org/show_bug.cgi?id=23721
    
            onchange events fire when a SELECT element has
            focus and the selectedIndex is updated by script in some way--either
            during another onchange, onkeypress, onfocus, or timer--and then
            focus is lost.
    
            Fixed by making a separate method for user-driven selectedIndex
            changes, leaving scripts to use one which doesn't cause onchange to
            be queued.
    
            Test: fast/forms/select-script-onchange.html
    
            * dom/SelectElement.cpp: check if the pending change is user driven
            before calling onchange
            (WebCore::SelectElement::menuListOnChange):
            (WebCore::SelectElement::setSelectedIndex):
            * dom/SelectElement.h: store whether the pending change is user driven
            (WebCore::SelectElementData::userDrivenChange):
            (WebCore::SelectElementData::setUserDrivenChange):
            * html/HTMLSelectElement.cpp: split into two methods -- script version
            [non-user-driven] corresponds to IDL defined property name
            (WebCore::HTMLSelectElement::setSelectedIndex):
            (WebCore::HTMLSelectElement::setSelectedIndexByUser):
            * html/HTMLSelectElement.h:
            * rendering/RenderMenuList.cpp: use ByUser method when coming through
            the renderer
            (WebCore::RenderMenuList::valueChanged):
    
    LayoutTests:
    
    2009-06-25  John Gregg  <johnnyg@google.com>
    
            Reviewed by Sam Weinig.
    
            Bug 23721: Changing dropdown's selectedIndex within onchange handler fires another onchange
            https://bugs.webkit.org/show_bug.cgi?id=23721
    
            Check for unnecessary calls to onchange in response to script
            actions.
    
            * fast/forms/select-script-onchange-expected.txt: Added.
            * fast/forms/select-script-onchange.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45185 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLSelectElement.h
+++ third_party/WebKit/WebCore/html/HTMLSelectElement.h
@@ -40,7 +40,8 @@ public:
     HTMLSelectElement(const QualifiedName&, Document*, HTMLFormElement* = 0);
 
     virtual int selectedIndex() const;
-    virtual void setSelectedIndex(int index, bool deselect = true, bool fireOnChange = false);
+    virtual void setSelectedIndex(int index, bool deselect = true);
+    virtual void setSelectedIndexByUser(int index, bool deselect = true, bool fireOnChangeNow = false);
 
     unsigned length() const;
 
