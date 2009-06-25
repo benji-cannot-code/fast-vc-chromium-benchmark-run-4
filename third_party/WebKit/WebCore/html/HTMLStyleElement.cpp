70663e3725d00ad760b9d39c65eed3eb39a5f84d
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-25 19:45:30 +0000

    WebCore:
            Reviewed by Darin Adler.
    
            - fix https://bugs.webkit.org/show_bug.cgi?id=26671
              <rdar://problem/7001880> Safari 4.0 crashes in
              WebCore::DOMTimer::fired()
    
            Test: fast/dom/style-sheet-candidate-remove-unrendered-document.html
    
            When a "style sheet candidate" element is removed from a document,
            call Document::removeStyleSheetCandidateNode() regardless of whether
            the document is rendered. Otherwise, the document's style sheet
            candidate set can end up containing stale references.
    
            * dom/ProcessingInstruction.cpp:
            (WebCore::ProcessingInstruction::removedFromDocument):
            * html/HTMLLinkElement.cpp:
            (WebCore::HTMLLinkElement::removedFromDocument):
            * html/HTMLStyleElement.cpp:
            (WebCore::HTMLStyleElement::removedFromDocument):
    
    LayoutTests:
    
            Reviewed by Darin Adler.
    
            - test for https://bugs.webkit.org/show_bug.cgi?id=26671
              <rdar://problem/7001880> Safari 4.0 crashes in
              WebCore::DOMTimer::fired()
    
            * fast/dom/style-sheet-candidate-remove-unrendered-document-expected.txt: Added.
            * fast/dom/style-sheet-candidate-remove-unrendered-document.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45180 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLStyleElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLStyleElement.cpp
@@ -72,8 +72,7 @@ void HTMLStyleElement::insertedIntoDocument()
 void HTMLStyleElement::removedFromDocument()
 {
     HTMLElement::removedFromDocument();
-    if (document()->renderer())
-        document()->removeStyleSheetCandidateNode(this);
+    document()->removeStyleSheetCandidateNode(this);
     StyleElement::removedFromDocument(document());
 }
 
