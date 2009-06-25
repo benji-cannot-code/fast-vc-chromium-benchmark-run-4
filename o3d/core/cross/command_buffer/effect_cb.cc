d04598e43b1c818028fb90123438a311a64f5337
rlp@google.com <rlp@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-25 18:42:32 +0000

    Fixing unit_test bugs. One of hte fixes is for the texture param. This quiets the unit test, but has a todo under the actual code. To make all examples run, this should be completed, but if we're going ot eliminate the texture path in favor of the sampler path, then it doesn't make sense to fill this in if we'll delete it shortly.
    Review URL: http://codereview.chromium.org/146133
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19268 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/command_buffer/effect_cb.cc
+++ o3d/core/cross/command_buffer/effect_cb.cc
@@ -167,6 +167,8 @@ static const ObjectBase::Class* CBTypeToParamType(
       return ParamMatrix4::GetApparentClass();
     case effect_param::SAMPLER:
       return ParamSampler::GetApparentClass();
+    case effect_param::TEXTURE:
+      return ParamTexture::GetApparentClass();
     default : {
       DLOG(ERROR) << "Cannot convert command buffer type "
                   << type
@@ -200,7 +202,7 @@ void EffectCB::GetParameterInfo(EffectParameterInfoArray *array) {
 }
 
 void EffectCB::GetStreamInfo(EffectStreamInfoArray *array) {
-  // To be filled in
+  // TODO(rlp)
 }
 
 }  // namespace o3d
