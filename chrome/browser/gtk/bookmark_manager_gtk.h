eb79e4c43d23664280addf6d9457a4bf8affb481
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-25 23:48:54 +0000

    Fix another bookmark manager crasher.
    Also, don't allow drags to originate from parts of the table view that don't have rows.
    
    BUG=http://crbug.com/15388
    TEST=try to drag from the white space below the lowest row of the bookmark manager right pane onto the bookmark bar (nothing should happen)
    
    Review URL: http://codereview.chromium.org/149024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19310 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/bookmark_manager_gtk.h
+++ chrome/browser/gtk/bookmark_manager_gtk.h
@@ -186,6 +186,9 @@ class BookmarkManagerGtk : public BookmarkModelObserver,
   static void OnLeftTreeViewFocusIn(GtkTreeView* tree_view,
       GdkEventFocus* event, BookmarkManagerGtk* bookmark_manager);
 
+  static gboolean OnRightTreeViewButtonPress(GtkWidget* tree_view,
+      GdkEventButton* event, BookmarkManagerGtk* bookmark_manager);
+
   static gboolean OnTreeViewButtonRelease(GtkTreeView* tree_view,
       GdkEventButton* button, BookmarkManagerGtk* bookmark_manager);
 
