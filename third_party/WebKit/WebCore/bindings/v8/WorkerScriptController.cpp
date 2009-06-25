0c88b193d42eb27a60f2b0184a6aca5324b98907
agl@chromium.org <agl@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-25 22:32:40 +0000

    2009-06-25  Adam Langley  <agl@google.com>
            TBRed: fix for Chromium tree.
    
            Add missing include for r45199.
    
            https://bugs.webkit.org/show_bug.cgi?id=26736
    
            r45199 added a reference to throwError without including V8Proxy.h
    
            * bindings/v8/WorkerScriptController.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45203 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/WorkerScriptController.cpp
+++ third_party/WebKit/WebCore/bindings/v8/WorkerScriptController.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScriptValue.h"
 #include "DOMTimer.h"
 #include "V8DOMMap.h"
+#include "V8Proxy.h"
 #include "WorkerContext.h"
 #include "WorkerContextExecutionProxy.h"
 #include "WorkerObjectProxy.h"
