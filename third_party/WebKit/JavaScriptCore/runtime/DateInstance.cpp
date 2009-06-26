0842076315780a761feb1d8757c3636cb1cf2b8a
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-26 10:27:21 +0000

    2009-06-26  Zoltan Horvath  <hzoltan@inf.u-szeged.hu>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=26750
    
            Allow custom memory allocation control for JavaScriptCore/runtime
            directory's classes which instantiated by new:
    
            class  IdentifierTable
            struct RegExpObjectData
            class  HashEntry
            struct DataInstance::Cache
            class  UString
            struct RegExpConstructorPrivate
            class  ScopeChain
    
            * runtime/DateInstance.cpp:
            * runtime/Identifier.cpp:
            * runtime/Lookup.h:
            * runtime/RegExpConstructor.cpp:
            * runtime/RegExpObject.h:
            * runtime/ScopeChain.h:
            * runtime/UString.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/DateInstance.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/DateInstance.cpp
@@ -31,7 +31,7 @@ using namespace WTF;
 
 namespace JSC {
 
-struct DateInstance::Cache {
+struct DateInstance::Cache : public FastAllocBase {
     double m_gregorianDateTimeCachedForMS;
     GregorianDateTime m_cachedGregorianDateTime;
     double m_gregorianDateTimeUTCCachedForMS;
