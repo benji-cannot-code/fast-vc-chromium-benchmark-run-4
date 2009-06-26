1c706595b811cbe3fc33dd87493da2560b6cb7aa
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 18:22:54 +0000

    linux plugins: eliminate GtkWidget* from plugin process
    [retry with mac hopefully fixed this time]
    
    GtkWidget* (and its wrapper, gfx::NativeView) only work within a
    single process.  Plugins already work with a lower-level type that
    works across processes -- an X Window id.  The parent of a plugin
    is an HWND on windows, but it's an X Window id on X.  So we
    introduce a new typedef wrapping that and push it through all the
    places in the code that needs it.
    
    Since we no longer have a GtkSocket in the WebPluginDelegateImpl,
    we need to do a bit more work in the WebViewDelegate (aka the
    browser process in the multiproc world).
    
    We also need the plugin host (the browser) to track the
    GtkSockets that are hosting the plugin, as well as destroy them
    when necessary.  To do this we require the plugin owner to
    grab the plug-removed signal rather than putting its handler in
    GtkPluginContainer; this is the way it worked before I'd refactored
    into GtkPluginContainer so it shouldn't be so bad.
    
    This change still only works in test_shell, but the refactoring
    should translate to the multiprocess case pretty easily.
    
    Review URL: http://codereview.chromium.org/146009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19387 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_impl.h
+++ webkit/glue/webplugin_impl.h
@@ -149,8 +149,8 @@ class WebPluginImpl : public WebPlugin,
                 int arg_count, char** arg_names, char** arg_values);
 
   // WebPlugin implementation:
-  void SetWindow(gfx::NativeView window);
-  void WillDestroyWindow(gfx::NativeView window) { }
+  void SetWindow(gfx::PluginWindowHandle window);
+  void WillDestroyWindow(gfx::PluginWindowHandle window) { }
 #if defined(OS_WIN)
   void SetWindowlessPumpEvent(HANDLE pump_messages_event) { }
 #endif
@@ -334,7 +334,7 @@ class WebPluginImpl : public WebPlugin,
   std::vector<ClientInfo> clients_;
 
   bool windowless_;
-  gfx::NativeView window_;
+  gfx::PluginWindowHandle window_;
   WebCore::HTMLPlugInElement* element_;
   WebFrameImpl* webframe_;
 
