65f5090f3da9a54fd19f97def378c5e2dd74bf70
erikkay@google.com <erikkay@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 19:50:52 +0000

    Enable extension protocols on all platforms.  This should enable background pages and tab-hosted pages from extensions.
    BUG=none
    TEST=load an extension that has a background page and verify that the process is created (see chrome://extensions)
    
    Review URL: http://codereview.chromium.org/149091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19400 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process_impl.h"
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/dom_ui/chrome_url_data_manager.h"
+#include "chrome/browser/extensions/extension_protocols.h"
 #include "chrome/browser/first_run.h"
 #include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/browser/net/dns_global.h"
@@ -87,7 +88,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win_util.h"
 #include "chrome/browser/browser.h"
 #include "chrome/browser/browser_trial.h"
-#include "chrome/browser/extensions/extension_protocols.h"
 #include "chrome/browser/jankometer.h"
 #include "chrome/browser/metrics/user_metrics.h"
 #include "chrome/browser/net/url_fixer_upper.h"
@@ -108,10 +108,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/widget/accelerator_handler.h"
 
 #endif  // defined(OS_WIN)
-
-#if defined(LINUX2)
-#include "chrome/browser/extensions/extension_protocols.h"
-#endif  // defined(LINUX2)
 
 #if defined(TOOLKIT_GTK)
 #include "chrome/common/gtk_util.h"
@@ -719,10 +715,7 @@ int BrowserMain(const MainFunctionParams& parameters) {
   // Register our global network handler for chrome:// and
   // chrome-extension:// URLs.
   RegisterURLRequestChromeJob();
-
-#if defined(OS_WIN) || defined(LINUX2)
   RegisterExtensionProtocols();
-#endif
 
   // In unittest mode, this will do nothing.  In normal mode, this will create
   // the global GoogleURLTracker instance, which will promptly go to sleep for
