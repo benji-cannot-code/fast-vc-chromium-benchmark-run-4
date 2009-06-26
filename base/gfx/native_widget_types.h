629273f0714754b8272d39424119daec884df578
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 00:51:11 +0000

    mac: retry build fix.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19327 0039d316-1c4b-4281-b951-d872f2087c98

--- base/gfx/native_widget_types.h
+++ base/gfx/native_widget_types.h
@@ -125,7 +125,7 @@ NativeViewId IdFromNativeView(NativeView view);
   typedef unsigned long PluginWindowHandle;
 #else
   // On OS X we don't have windowed plugins.
-  typedef void* PluginWindowHandle;
+  typedef gfx::NativeView PluginWindowHandle;
 #endif
 
 }  // namespace gfx
