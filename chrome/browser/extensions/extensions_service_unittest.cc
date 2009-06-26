a97af99e6646f12d67e9bd08d33edfcf5a18b5b3
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 02:04:51 +0000

    Fix proper MACOSX exlussion.
    TBR=rafaelw
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/149051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19332 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service_unittest.cc
+++ chrome/browser/extensions/extensions_service_unittest.cc
@@ -1223,9 +1223,9 @@ TEST_F(ExtensionsServiceTest, ExternalInstallRegistry) {
 
 #endif
 
-// TODO(rafaelw): Re-enable on mac. This broke with: 
+// TODO(rafaelw): Re-enable on mac. This broke with:
 // http://codereview.chromium.org/140018
-#if !defined(OS_MAC)
+#if !defined(OS_MACOSX)
 
 TEST_F(ExtensionsServiceTest, ExternalInstallPref) {
   InitializeEmptyExtensionsService();
@@ -1340,7 +1340,7 @@ TEST_F(ExtensionsServiceTest, ExternalInstallPref) {
               std::string::npos);
 }
 
-#endif  // !defined(OS_MAC)
+#endif  // !defined(OS_MACOSX)
 
 TEST_F(ExtensionsServiceTest, ExternalPrefProvider) {
   InitializeEmptyExtensionsService();
