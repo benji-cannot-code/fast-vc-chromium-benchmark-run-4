ea663223261e32a554c03b3ae975302620b384e1
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 00:06:05 +0000

    Disable ExtensionsService.ExternalInstallPref unittest on mac.
    Broke with: http://codereview.chromium.org/140018.
    
    R=rvargas
    
    Review URL: http://codereview.chromium.org/147183
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19316 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service_unittest.cc
+++ chrome/browser/extensions/extensions_service_unittest.cc
@@ -1223,6 +1223,10 @@ TEST_F(ExtensionsServiceTest, ExternalInstallRegistry) {
 
 #endif
 
+// TODO(rafaelw): Re-enable on mac. This broke with: 
+// http://codereview.chromium.org/140018
+#if !defined(OS_MAC)
+
 TEST_F(ExtensionsServiceTest, ExternalInstallPref) {
   InitializeEmptyExtensionsService();
   // Verify that starting with no providers loads no extensions.
@@ -1335,6 +1339,8 @@ TEST_F(ExtensionsServiceTest, ExternalInstallPref) {
   ASSERT_TRUE(GetErrors()[0].find("Extensions are not enabled") !=
               std::string::npos);
 }
+
+#endif  // !defined(OS_MAC)
 
 TEST_F(ExtensionsServiceTest, ExternalPrefProvider) {
   InitializeEmptyExtensionsService();
