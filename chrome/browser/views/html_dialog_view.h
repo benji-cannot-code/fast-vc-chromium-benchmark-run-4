f4ced9a484b256046e9a923d62e78e4e8fb5fdc1
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 14:54:44 +0000

    Add a 'RegisterMessages' function to DOMMessageHandler to make it easy to construct these objects by deferring the DOMUI dependency to the init / "Attach" step.
    Add GetMessageHandlers to HtmlDialogUIDelegate to allow
    sending/receiving messages to the page hosted in the dialog as opposed to only on open/close.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/131094
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19359 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/html_dialog_view.h
+++ chrome/browser/views/html_dialog_view.h
@@ -55,6 +55,8 @@ class HtmlDialogView
   virtual bool IsDialogModal() const;
   virtual std::wstring GetDialogTitle() const;
   virtual GURL GetDialogContentURL() const;
+  virtual void GetDOMMessageHandlers(
+      std::vector<DOMMessageHandler*>* handlers) const;
   virtual void GetDialogSize(gfx::Size* size) const;
   virtual std::string GetDialogArgs() const;
   virtual void OnDialogClosed(const std::string& json_retval);
