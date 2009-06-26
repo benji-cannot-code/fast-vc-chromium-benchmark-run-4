468b927b177ebffd12add1d15fc193bc2d96001d
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 21:17:24 +0000

    Use platform-appropriate newlines in JSON output.
    BUG=15462
    TEST=base_unittests, unit_tests, check newlines in bookmarks file
    Review URL: http://codereview.chromium.org/147220
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19418 0039d316-1c4b-4281-b951-d872f2087c98

--- base/json_writer.cc
+++ base/json_writer.cc
@@ -10,7 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "base/string_escape.h"
 
-const char kPrettyPrintLineEnding[] = "\r\n";
+#if defined(OS_WIN)
+static const char kPrettyPrintLineEnding[] = "\r\n";
+#else
+static const char kPrettyPrintLineEnding[] = "\n";
+#endif
 
 /* static */
 void JSONWriter::Write(const Value* const node,
