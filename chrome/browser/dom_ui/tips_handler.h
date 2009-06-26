5a30fb0d58bc168d833829fc7d7af2c5638f0429
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 20:55:01 +0000

    Validates links which are to be passed on to the NTP by ensuring that they start with "http://" or "https://".
    BUG= http://crbug.com/15457
    TEST= Change cached tip link in preferences file to be invalid.  Note that this tip will not be included on the NTP.
    
    Review URL: http://codereview.chromium.org/149083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19415 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/tips_handler.h
+++ chrome/browser/dom_ui/tips_handler.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // "tip_cache": {
 //    "0": {
-//        "index": should become time field (or not)
-//        "snippet": the text of the item
-//        "source": text describing source (i.e., "New York Post")
-//        "thumbnail": URL of thumbnail on popgadget server
 //        "title": text giving title of item
 //        "url": link to item's page
 //    },
@@ -44,6 +40,9 @@ class TipsHandler : public DOMMessageHandler {
   static void RegisterUserPrefs(PrefService* prefs);
 
  private:
+  // Make sure the string we are pushing to the NTP is a valid URL.
+  bool IsValidURL(const std::wstring& url_string);
+
   // So we can push data out to the page that has called this handler.
   DOMUI* dom_ui_;
 
