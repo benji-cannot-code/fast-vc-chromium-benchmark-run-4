d2d82c134c75710088f57f50f941a09d394a98e8
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 19:49:40 +0000

    Fix two issues with Drag-N-Drop in gtk tabstrip: * Handle drag-leave and drag-failed by hiding or destroying the drag arrow respectively. * Hide the drop arrow instead of destroying the drop info when we set the drop index to -1.  This compensates for the fact that gtk emits the drag-leave signal before the drag-data-received signal.
    BUG=none
    TEST=Drag a URL in and out of the tabstrip.  The drop arrow should disappear when the dragged mouse leaves the tabstrip or when the drag fails.
    Review URL: http://codereview.chromium.org/149093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19399 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/tabs/tab_strip_gtk.cc
+++ chrome/browser/gtk/tabs/tab_strip_gtk.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/gtk/tabs/tab_strip_gtk.h"
 
+#include <algorithm>
+
 #include "app/gfx/canvas_paint.h"
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
@@ -498,6 +500,10 @@ void TabStripGtk::Init() {
                    G_CALLBACK(OnDragMotion), this);
   g_signal_connect(G_OBJECT(tabstrip_.get()), "drag-drop",
                    G_CALLBACK(OnDragDrop), this);
+  g_signal_connect(G_OBJECT(tabstrip_.get()), "drag-leave",
+                   G_CALLBACK(OnDragLeave), this);
+  g_signal_connect(G_OBJECT(tabstrip_.get()), "drag-failed",
+                   G_CALLBACK(OnDragFailed), this);
   g_signal_connect(G_OBJECT(tabstrip_.get()), "drag-data-received",
                    G_CALLBACK(OnDragDataReceived), this);
 
@@ -1126,7 +1132,7 @@ void TabStripGtk::RemoveMessageLoopObserver() {
 gfx::Rect TabStripGtk::GetDropBounds(int drop_index,
                                      bool drop_before,
                                      bool* is_beneath) {
-  DCHECK(drop_index != -1);
+  DCHECK_NE(drop_index, -1);
   int center_x;
   if (drop_index < GetTabCount()) {
     TabGtk* tab = GetTabAt(drop_index);
@@ -1181,9 +1187,12 @@ void TabStripGtk::UpdateDropIndex(GdkDragContext* context, gint x, gint y) {
 void TabStripGtk::SetDropIndex(int index, bool drop_before) {
   if (index == -1) {
     if (drop_info_.get())
-      drop_info_.reset(NULL);
+      gtk_widget_hide(drop_info_->container);
     return;
   }
+
+  if (drop_info_.get() && !GTK_WIDGET_VISIBLE(drop_info_->container))
+    gtk_widget_show(drop_info_->container);
 
   if (drop_info_.get() && drop_info_->drop_index == index &&
       drop_info_->drop_before == drop_before) {
@@ -1535,6 +1544,23 @@ gboolean TabStripGtk::OnDragDrop(GtkWidget* widget, GdkDragContext* context,
 
   g_free(list);
   return TRUE;
+}
+
+// static
+gboolean TabStripGtk::OnDragLeave(GtkWidget* widget, GdkDragContext* context,
+                                  guint time, TabStripGtk* tabstrip) {
+  // Hide the drop indicator.
+  tabstrip->SetDropIndex(-1, false);
+  return FALSE;
+}
+
+// static
+gboolean TabStripGtk::OnDragFailed(GtkWidget* widget, GdkDragContext* context,
+                                   GtkDragResult result,
+                                   TabStripGtk* tabstrip) {
+  // Hide the drop indicator.
+  tabstrip->SetDropIndex(-1, false);
+  return FALSE;
 }
 
 // static
