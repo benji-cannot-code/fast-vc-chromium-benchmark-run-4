93eaf2485f33c2f87d4538939037457c2e0c33de
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-26 16:27:18 +0000

    2009-06-26  Ben Murdoch  <benm@google.com>
            Reviewed by Darin Fisher.
    
            Add #if ENABLE(DOM_STORAGE) to the V8 custom bindings for local/session storage.
            https://bugs.webkit.org/show_bug.cgi?id=26757
    
            * bindings/v8/custom/V8StorageCustom.cpp
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45261 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8StorageCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8StorageCustom.cpp
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
+#if ENABLE(DOM_STORAGE)
+
 #include "Storage.h"
 #include "V8Binding.h"
 #include "V8CustomBinding.h"
@@ -141,3 +143,5 @@ NAMED_PROPERTY_DELETER(Storage)
 }
 
 } // namespace WebCore
+
+#endif
