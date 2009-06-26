21696ac0332cd8e96f89e6f7f8dc4adb34849588
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 18:13:35 +0000

    fix windows build break.
    Review URL: http://codereview.chromium.org/147222
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19385 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/firefox_importer_utils_win.cc
+++ chrome/browser/importer/firefox_importer_utils_win.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/importer/firefox_importer_utils.h"
 
+#include <shlobj.h>
+
 #include "base/registry.h"
 
 namespace {
