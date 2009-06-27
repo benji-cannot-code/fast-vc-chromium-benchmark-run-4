67baabcf429e8e0a34039b95e19439284776f574
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-27 01:52:55 +0000

    2009-06-26  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=26767
    
            Fix a build break due to LocalStorage.h being included in WebKitDLL.cpp
            despite it being deleted from the repo by the following patch/commit:
            https://bugs.webkit.org/show_bug.cgi?id=26732
            http://trac.webkit.org/changeset/45290
    
            * WebKitDLL.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45302 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebKitDLL.cpp
+++ third_party/WebKit/WebKit/win/WebKitDLL.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKitClassFactory.h"
 #include <WebCore/COMPtr.h>
 #include <WebCore/IconDatabase.h>
-#include <WebCore/LocalStorage.h>
 #include <WebCore/Page.h>
 #include <WebCore/PageGroup.h>
 #include <WebCore/RenderThemeWin.h>
