c53f38448fee33a6c7f9b2f6bd5dedb78b879331
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-27 08:34:47 +0000

    Bug 26771: Canvas is incorrectly tainted when drawing from a video element that uses <source> elements
    Reviewed by Maciej Stachowiak.
    
    The drawImage(<video>) logic naively assumes that it just needs
    to check the src attribute of the video element when in fact it
    needs to look at the url that is being played instead.  Failure
    to do this means that video provided through source elements
    taints the canvas.
    
    Test: media/video-canvas-source.html
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45311 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/WebCore/html/CanvasRenderingContext2D.cpp
@@ -938,6 +938,13 @@ void CanvasRenderingContext2D::checkOrigin(const KURL& url)
         m_canvas->setOriginTainted();
 }
 
+void CanvasRenderingContext2D::checkOrigin(const String& url)
+{
+    RefPtr<SecurityOrigin> origin = SecurityOrigin::createFromString(url);
+    if (!m_canvas->document()->securityOrigin()->canAccess(origin.get()))
+        m_canvas->setOriginTainted();
+}
+
 void CanvasRenderingContext2D::drawImage(HTMLImageElement* image, float x, float y)
 {
     ASSERT(image);
@@ -1083,7 +1090,7 @@ void CanvasRenderingContext2D::drawImage(HTMLVideoElement* video, const FloatRec
         return;
 
     if (m_canvas->originClean())
-        checkOrigin(video->src());
+        checkOrigin(video->currentSrc());
 
     if (m_canvas->originClean() && !video->hasSingleSecurityOrigin())
         m_canvas->setOriginTainted();
