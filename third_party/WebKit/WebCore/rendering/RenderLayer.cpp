bf1ad84a4ddcc59c9e6f63c398815e932b000f0c
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-28 17:25:01 +0000

    2009-06-27  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=26780
    
            Do not make compositing layers for non-self-painting RenderLayers,
            since these exist only to push a clip onto the clipping stack. If such
            a layer gets compositied for some other reason, it should not paint.
    
            Also ensure that we update composited layer positions correctly
            inside overflow:scroll layers. We can't assume that the contents
            are child layers, so we have to go up to the compositing ancestor,
            and tell it to update all its child layer positions as we do
            after layout.
    
            Tests: compositing/layers-inside-overflow-scroll.html
                   compositing/self-painting-layers.html
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::scrollToOffset):
            * rendering/RenderLayerBacking.cpp:
            (WebCore::RenderLayerBacking::paintIntoLayer):
            * rendering/RenderLayerCompositor.cpp:
            (WebCore::RenderLayerCompositor::calculateCompositedBounds):
            (WebCore::RenderLayerCompositor::needsToBeComposited):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayer.cpp
@@ -1063,8 +1063,10 @@ void RenderLayer::scrollToOffset(int x, int y, bool updateScrollbars, bool repai
         child->updateLayerPositions(false, false);
 
 #if USE(ACCELERATED_COMPOSITING)
-    if (isComposited())
-        m_backing->updateGraphicsLayerGeometry();
+    if (compositor()->inCompositingMode()) {
+        if (RenderLayer* compositingAncestor = ancestorCompositingLayer())
+            compositingAncestor->backing()->updateAfterLayout();
+    }
 #endif
     
     RenderView* view = renderer()->view();
