9d53643a6b07965b6efc8865f676445aa95e33a4
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-29 02:54:03 +0000

    WebCore:
            Reviewed by Simon Fraser.
    
            - fix https://bugs.webkit.org/show_bug.cgi?id=26783
              <rdar://problem/7014543> REGRESSION (r45296): Subfolders not displayed
              in MobileMe iDisk Web App
    
            Test: added a case to fast/dom/Element/scrollWidth.html
    
            Ensure that scroll{Width, Height} is greater or equal to
            client{Width, Height}.
    
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::scrollWidth):
            (WebCore::RenderBox::scrollHeight):
    
    LayoutTests:
    
            Reviewed by Simon Fraser.
    
            - add a test case for https://bugs.webkit.org/show_bug.cgi?id=26783
              <rdar://problem/7014543> REGRESSION (r45296): Subfolders not displayed
              in MobileMe iDisk Web App
    
            * fast/dom/Element/scrollWidth-expected.txt:
            * fast/dom/Element/scrollWidth.html:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45332 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBox.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBox.cpp
@@ -255,7 +255,7 @@ int RenderBox::scrollWidth() const
         return layer()->scrollWidth();
     // For objects with visible overflow, this matches IE.
     if (style()->direction() == LTR)
-        return rightmostPosition(true, false) - borderLeft();
+        return max(clientWidth(), rightmostPosition(true, false) - borderLeft());
     return clientWidth() - min(0, leftmostPosition(true, false) - borderLeft());
 }
 
@@ -264,7 +264,7 @@ int RenderBox::scrollHeight() const
     if (hasOverflowClip())
         return layer()->scrollHeight();
     // For objects with visible overflow, this matches IE.
-    return lowestPosition(true, false) - borderTop();
+    return max(clientHeight(), lowestPosition(true, false) - borderTop());
 }
 
 int RenderBox::scrollLeft() const
