35a36ad1ab33c06ae8d16c52fd6efefcf6a801e8
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-29 18:13:05 +0000

    Coverity: Initialize delegate_.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/150029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19504 0039d316-1c4b-4281-b951-d872f2087c98

--- base/directory_watcher_win.cc
+++ base/directory_watcher_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,7 +15,7 @@ namespace {
 class DirectoryWatcherImpl : public DirectoryWatcher::PlatformDelegate,
                              public base::ObjectWatcher::Delegate {
  public:
-  DirectoryWatcherImpl() : handle_(INVALID_HANDLE_VALUE) {}
+  DirectoryWatcherImpl() : delegate_(NULL), handle_(INVALID_HANDLE_VALUE) {}
   virtual ~DirectoryWatcherImpl();
 
   virtual bool Watch(const FilePath& path, DirectoryWatcher::Delegate* delegate,
