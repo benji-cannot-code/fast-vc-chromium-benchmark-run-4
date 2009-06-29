914e3773c2b8723df0fe6c8d00610fca300b7159
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-06-29 19:36:23 +0000

    Reviewed by Adam Treat. Rename go-task-animation.* to ontimer-inline-event.* to reflect what it really tests.
    git-svn-id: svn://svn.chromium.org/blink/trunk@45343 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/wml/resources/ontimer-inline-event.js
+++ third_party/WebKit/LayoutTests/wml/resources/ontimer-inline-event.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/// [Name] go-task-animation.js
+/// [Name] ontimer-inline-event.js
 
-createWMLTestCase("Tests &lt;timer&gt; and &lt;go&gt; element combinations", false, "resources/animation.wml");
+createWMLTestCase("Tests &lt;timer&gt; and &lt;go&gt; element combinations", false, "resources/ontimer-inline-event.wml");
 
 var counter = 0;
 
