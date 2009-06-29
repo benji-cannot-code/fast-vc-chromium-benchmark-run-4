55d6253433f9dbdb999478f102ef772990a6e5bc
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-29 17:21:22 +0000

    Wires up the create browser window.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/151026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/new_browser_window_widget.cc
+++ chrome/browser/views/new_browser_window_widget.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser.h"
 #include "chrome/browser/browser_window.h"
 #include "chrome/browser/profile.h"
+#include "chrome/browser/views/tabs/tab_overview_types.h"
 #include "chrome/common/page_transition_types.h"
 #include "googleurl/src/gurl.h"
 #include "grit/theme_resources.h"
@@ -29,9 +30,12 @@ NewBrowserWindowWidget::NewBrowserWindowWidget(Profile* profile)
       new views::WidgetGtk(views::WidgetGtk::TYPE_WINDOW);
   widget->MakeTransparent();
   widget->Init(NULL, gfx::Rect(0, 0, pref.width(), pref.height()), false);
+  TabOverviewTypes::instance()->SetWindowType(
+      widget->GetNativeView(),
+      TabOverviewTypes::WINDOW_TYPE_CREATE_BROWSER_WINDOW,
+      NULL);
   widget->GetRootView()->SetLayoutManager(new views::FillLayout());
   widget->GetRootView()->AddChildView(button);
-  // TODO: set window type.
   widget_ = widget;
   widget->Show();
 }
