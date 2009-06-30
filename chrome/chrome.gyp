1db2e00557c57de986d30131b9ffd6aad72e2cd7
idanan@chromium.org <idanan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-30 18:52:58 +0000

    Empty placeholder for Privacy Blacklist Tool to be implemented.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/150105
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19623 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -4016,6 +4016,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ],
         },
         {
+          'target_name': 'pbl_tool',
+          'type': 'executable',
+          'msvs_guid': '265C427B-D1FF-4922-A47C-0754DDFF52E1',
+          'dependencies': [
+            '../base/base.gyp:base',
+            'browser'
+          ],
+          'sources': [
+            'tools/pbl_tool/pbl_tool.cc',
+          ],
+        },
+        {
           'target_name': 'perf_tests',
           'type': 'executable',
           'msvs_guid': '9055E088-25C6-47FD-87D5-D9DD9FD75C9F',
---
+++ chrome/tools/pbl_tool/pbl_tool.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This tool manages privacy blacklists. Primarily for loading a text
+// blacklist into the binary agregate blacklist.
+#include <iostream>
+
+#include "base/process_util.h"
+#include "chrome/browser/privacy_blacklist/blacklist.h"
+#include "chrome/browser/privacy_blacklist/blacklist_io.h"
+
+int main(int argc, char* argv[]) {
+  base::EnableTerminationOnHeapCorruption();
+  std::cout << "Aw, Snap! This is not implemented yet." << std::endl;
+  CHECK(std::string() == Blacklist::StripCookies(std::string()));
+}
