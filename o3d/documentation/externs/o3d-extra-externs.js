b2ad66ca0fe5f209918769b9fde83925697225de
gman@google.com <gman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-30 21:44:39 +0000

    This CL contains the the changes needed to build an externs file for the JSCompiler as well as the code to build a compiled verison of the o3djs sample libs.
    Review URL: http://codereview.chromium.org/147079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19659 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ o3d/documentation/externs/o3d-extra-externs.js
@@ -0,0 +1,49 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+/**
+ * Special global variable for V8 instances.
+ */
+var plugin;
+
+/**
+ * The main namespace for the o3d plugin.
+ * @namespace
+ */
+var o3d;
+
+/**
+ * @type {!Object}
+ */
+var Exception = goog.typedef;
+
+/**
+ * A namespace for the Cursor.
+ * @namespace
+ */
+o3d.Cursor = o3d.Cursor || { };
+
+/**
+ * A namespace for the VectorMath.
+ * @namespace
+ */
+var Vectormath;
+
+/**
+ * A namespace for the VectorMath.Aos
+ * @namespace
+ */
+Vectormath.Aos = Vectormath.Aos || { };
+
+/**
+ * A 4x4 Matrix of floats
+ * @type {!Array.<!Array.<number>>}
+ */
+o3d.Matrix4 = goog.typedef;
+
+/**
+ * RangeError.
+ * why is this sometimes needed and sometimes not?
+ * @exception
+ */
+var RangeError;
+
