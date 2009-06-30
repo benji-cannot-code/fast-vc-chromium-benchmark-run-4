10df56cd08ccf29c3e8f1f0f712a352d3d9642de
apatrick@google.com <apatrick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-30 21:50:16 +0000

    Review URL: http://codereview.chromium.org/151103
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19663 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/object_manager.cc
+++ o3d/core/cross/object_manager.cc
@@ -44,8 +44,7 @@ const InterfaceId ObjectManager::kInterfaceId =
 
 ObjectManager::ObjectManager(ServiceLocator* service_locator)
     : service_locator_(service_locator),
-      service_(service_locator_, this),
-      allow_pack_creation_(true) {
+      service_(service_locator_, this) {
 }
 
 ObjectManager::~ObjectManager() {
@@ -53,10 +52,6 @@ ObjectManager::~ObjectManager() {
   pack_array_.clear();
 
   DLOG_ASSERT(object_map_.empty()) << "Client node leak.";
-}
-
-void ObjectManager::DisallowPackCreation() {
-  allow_pack_creation_ = false;
 }
 
 std::vector<ObjectBase*> ObjectManager::GetObjects(
@@ -135,10 +130,6 @@ bool ObjectManager::DestroyPack(Pack* pack) {
 }
 
 Pack* ObjectManager::CreatePack() {
-  if (!allow_pack_creation_) {
-    O3D_ERROR(service_locator_) << "Pack creation not allowed";
-    return NULL;
-  }
   Pack::Ref pack(new Pack(service_locator_));
   if (pack) {
     pack_array_.push_back(pack);
