db0a1b166bc703b7814dcfc058f210a491834c88
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-30 16:19:00 +0000

    Stop leaking ConnectCallbackWrapper if Connect() does not complete before the socket is deleted. BUG=NONE TEST=NONE
    Review URL: http://codereview.chromium.org/151059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19592 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.h
+++ net/socket/ssl_client_socket_nss.h
@@ -57,6 +57,7 @@ class SSLClientSocketNSS : public SSLClientSocket {
   void InvalidateSessionIfBadCertificate();
   X509Certificate* UpdateServerCert();
   void DoCallback(int result);
+  void DoConnectCallback(int result);
   void OnIOComplete(int result);
 
   int DoLoop(int last_io_result);
@@ -90,6 +91,7 @@ class SSLClientSocketNSS : public SSLClientSocket {
   std::string hostname_;
   SSLConfig ssl_config_;
 
+  CompletionCallback* user_connect_callback_;
   CompletionCallback* user_callback_;
 
   // Used by both Read and Write functions.
