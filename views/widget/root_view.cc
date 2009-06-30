cfa8209e1ab0ed5de9b69ab50fcb5b126c3730ed
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-30 20:33:51 +0000

    Fixing a breakage in the GTK toolkit build that my last check-in caused.
    BUG=None
    TEST=None
    TBR=brettw
    Review URL: http://codereview.chromium.org/150133
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19650 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/root_view.cc
+++ views/widget/root_view.cc
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include "base/logging.h"
 #include "base/message_loop.h"
-#if defined(OS_WIN)
 #include "views/focus/view_storage.h"
+#if defined(OS_WIN)
 #include "views/widget/root_view_drop_target.h"
 #endif
 #include "views/widget/widget.h"
