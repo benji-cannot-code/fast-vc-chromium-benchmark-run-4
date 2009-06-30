3307efe4c373685b26ad5e9440b78a94e7438c9f
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-30 20:27:30 +0000

    Fix compilation error in the focus manager by returning NULL from unimplemented function.
    Review URL: http://codereview.chromium.org/152001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19649 0039d316-1c4b-4281-b951-d872f2087c98

--- views/focus/focus_manager_gtk.cc
+++ views/focus/focus_manager_gtk.cc
@@ -21,6 +21,7 @@ void FocusManager::FocusNativeView(gfx::NativeView native_view) {
 FocusManager* FocusManager::GetFocusManagerForNativeView(
     gfx::NativeView native_view) {
   NOTIMPLEMENTED();
+  return NULL;
 }
 
 }  // namespace views
