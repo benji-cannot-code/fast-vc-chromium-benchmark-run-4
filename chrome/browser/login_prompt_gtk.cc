6e999396313c28f24da5bdc3918c150d1964b7af
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-30 22:50:32 +0000

    Fix missing include.
    Review URL: http://codereview.chromium.org/151123
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19680 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/login_prompt_gtk.cc
+++ chrome/browser/login_prompt_gtk.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/resource_dispatcher_host.h"
 #include "chrome/browser/tab_contents/navigation_controller.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/common/gtk_util.h"
 #include "chrome/common/notification_service.h"
 #include "grit/generated_resources.h"
