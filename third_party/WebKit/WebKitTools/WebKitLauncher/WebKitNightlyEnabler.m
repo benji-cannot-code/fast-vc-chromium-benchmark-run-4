cff526804eec4ac2dc2f5c4c9b1f70d4ad3139ce
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-01 21:41:01 +0000

    Shuffle some code around in preparation for a change to how poseAsWebKitApp works.
    Reviewed by Simon Fraser.
    
    * WebKitLauncher/WebKitNightlyEnabler.m:
    (systemVersion):
    (webKitLauncherBundle):
    (insideSafari4OnTigerTrampoline):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/WebKitLauncher/WebKitNightlyEnabler.m
+++ third_party/WebKit/WebKitTools/WebKitLauncher/WebKitNightlyEnabler.m
@@ -46,6 +46,16 @@ static char *webKitAppPath;
 static bool extensionBundlesWereLoaded = NO;
 static NSSet *extensionPaths = nil;
 
+static int32_t systemVersion()
+{
+    static SInt32 version = 0;
+    if (!version)
+        Gestalt(gestaltSystemVersion, &version);
+
+    return version;
+}
+
+
 static void myBundleDidLoad(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
 {
     NSBundle *bundle = (NSBundle *)object;
@@ -99,6 +109,14 @@ static void myApplicationWillTerminate(CFNotificationCenterRef center, void *obs
     [userDefaults synchronize];
 }
 
+NSBundle *webKitLauncherBundle()
+{
+    NSString *executablePath = [NSString stringWithUTF8String:webKitAppPath];
+    NSRange appLocation = [executablePath rangeOfString:@".app/" options:NSBackwardsSearch];
+    NSString *appPath = [executablePath substringToIndex:appLocation.location + appLocation.length];
+    return [NSBundle bundleWithPath:appPath];
+}
+
 extern char **_CFGetProcessPath() __attribute__((weak));
 
 static void poseAsWebKitApp()
@@ -121,22 +139,10 @@ static void poseAsWebKitApp()
     unsetenv("WebKitAppPath");
 }
 
-NSBundle *webKitLauncherBundle()
-{
-    NSString *executablePath = [NSString stringWithUTF8String:webKitAppPath];
-    NSRange appLocation = [executablePath rangeOfString:@".app/" options:NSBackwardsSearch];
-    NSString *appPath = [executablePath substringToIndex:appLocation.location + appLocation.length];
-    return [NSBundle bundleWithPath:appPath];
-}
-
 static BOOL insideSafari4OnTigerTrampoline()
 {
-    SInt32 systemVersion;
-    if (Gestalt(gestaltSystemVersion, &systemVersion) != noErr)
-        return NO;
-
     // If we're not on Tiger then we can't be in the trampoline state.
-    if ((systemVersion & 0xFFF0) != 0x1040)
+    if ((systemVersion() & 0xFFF0) != 0x1040)
         return NO;
 
     // If we're running Safari < 4.0 then we can't be in the trampoline state.
