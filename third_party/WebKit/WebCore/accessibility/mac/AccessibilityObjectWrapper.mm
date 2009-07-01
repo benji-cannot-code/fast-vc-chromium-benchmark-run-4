f033cf3ad5f47326526d6aa2f6368662cd91ff19
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-01 22:01:16 +0000

    Bug 26900: AX: Manual spell check with Command-; does not bring up suggestions https://bugs.webkit.org/show_bug.cgi?id=26900
    git-svn-id: svn://svn.chromium.org/blink/trunk@45452 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/mac/AccessibilityObjectWrapper.mm
+++ third_party/WebKit/WebCore/accessibility/mac/AccessibilityObjectWrapper.mm
@@ -1460,6 +1460,10 @@ static NSString* roleValueToNSString(AccessibilityRole value)
     if ([attributeName isEqualToString:NSAccessibilityLanguageAttribute]) 
         return m_object->language();
     
+    // this is used only by DumpRenderTree for testing
+    if ([attributeName isEqualToString:@"AXClickPoint"])
+        return [NSValue valueWithPoint:m_object->clickPoint()];
+    
     return nil;
 }
 
