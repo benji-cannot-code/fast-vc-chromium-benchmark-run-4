81dcce6f394ec837a56139acac72b0a294773f68
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-01 19:53:59 +0000

    Adds time-out to browser-tests to prevent a hanging test from hanging the entire test suite.
    Bug=None
    TEST=None
    Review URL: http://codereview.chromium.org/151158
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19755 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop.h
+++ base/message_loop.h
@@ -232,6 +232,9 @@ class MessageLoop : public base::MessagePump::Delegate {
     exception_restoration_ = restore;
   }
 
+  // Returns true if we are currently running a nested message loop.
+  bool IsNested();
+
   //----------------------------------------------------------------------------
  protected:
   struct RunState {
--- chrome/test/in_process_browser_test.cc
+++ chrome/test/in_process_browser_test.cc
@@ -34,6 +34,13 @@ extern int BrowserMain(const MainFunctionParams&);
 
 const wchar_t kUnitTestShowWindows[] = L"show-windows";
 
+// Delay for the time-out at which we stop the inner-message loop the first
+// time.
+const int kInitialTimeoutInMS = 30000;
+
+// Delay for sub-sequent time-outs once the initial time-out happened.
+const int kSubsequentTimeoutInMS = 5000;
+
 namespace {
 
 bool DieFileDie(const std::wstring& file, bool recurse) {
@@ -206,6 +213,11 @@ void InProcessBrowserTest::RunTestOnMainThreadLoop() {
 
   browser_ = CreateBrowser(profile);
 
+  // Start the timeout timer to prevent hangs.
+  MessageLoopForUI::current()->PostDelayedTask(FROM_HERE,
+      NewRunnableMethod(this, &InProcessBrowserTest::TimedOut),
+      kInitialTimeoutInMS);
+
   RunTestOnMainThread();
 
   BrowserList::const_iterator browser = BrowserList::begin();
@@ -224,4 +236,17 @@ void InProcessBrowserTest::ConfigureHostMapper(
   // See http://en.wikipedia.org/wiki/Web_Proxy_Autodiscovery_Protocol
   // We don't want the test code to use it.
   host_mapper->AddSimulatedFailure("wpad");
+}
+
+void InProcessBrowserTest::TimedOut() {
+  DCHECK(MessageLoopForUI::current()->IsNested());
+
+  GTEST_NONFATAL_FAILURE_("Timed-out");
+
+  // Start the timeout timer to prevent hangs.
+  MessageLoopForUI::current()->PostDelayedTask(FROM_HERE,
+      NewRunnableMethod(this, &InProcessBrowserTest::TimedOut),
+      kSubsequentTimeoutInMS);
+
+  MessageLoopForUI::current()->Quit();
 }
