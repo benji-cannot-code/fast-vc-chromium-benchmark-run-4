c20c9c06578b37e06c1bdb9fda55666a4458383f
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-01 18:49:07 +0000

    Allow getting the theme tint as a value so that it can be applied independent of the theme provider. Since the Mac needs it to tint its vector resources it's added to the Mac.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/151153
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19751 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_theme_provider_gtk.cc
+++ chrome/browser/browser_theme_provider_gtk.cc
@@ -42,7 +42,7 @@ GdkPixbuf* BrowserThemeProvider::GetPixbufNamed(int id) {
   return empty_bitmap;
 }
 
-void BrowserThemeProvider::FreePlatformImages() {
+void BrowserThemeProvider::FreePlatformCaches() {
   DCHECK(CalledOnValidThread());
 
   // Free GdkPixbufs.
