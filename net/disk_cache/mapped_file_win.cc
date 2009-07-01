18ad14b37163ec80013a52e15d0ac20b127a2893
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-01 23:57:02 +0000

    Fix 15 coverity complaints.
    Most of the changes are cosmetic, but there is also a
    memory leak in CreateExpiredServer.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/153001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19788 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/mapped_file_win.cc
+++ net/disk_cache/mapped_file_win.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace disk_cache {
 
-void* MappedFile::Init(const std::wstring name, size_t size) {
+void* MappedFile::Init(const std::wstring& name, size_t size) {
   DCHECK(!init_);
   if (init_ || !File::Init(name))
     return NULL;
