58f31d591a5e9bb4f24e2cf8fd78f89a14238463
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-01 06:07:40 +0000

    2009-06-30  Adele Peterson  <adele@apple.com>
            Reviewed by Dan Bernstein.
    
            Fix for <rdar://problem/7014389> REGRESSION(4-TOT): Hyperlinks have no tooltips in Mail unless you first click in the message body
    
            Updated these new methods (added as part of the viewless WebKit effort) that call through to WebHTMLView to use _selectedOrMainFrame, 
            instead of just selectedFrame.  When the selection changes, I don't think there is a situation where there's no selected frame, but
            it seems better to keep these two uses the same, since if there is a case, this will be more like the old behavior, before there was
            a version of _selectionChanged in WebView. 
    
            * WebView/WebView.mm:
            (-[WebView _setToolTip:]):
            (-[WebView _selectionChanged]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45427 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -5328,7 +5328,7 @@ static WebFrameView *containingFrameView(NSView *view)
 - (void)_setToolTip:(NSString *)toolTip
 {
     if (_private->usesDocumentViews) {
-        id documentView = [[[self selectedFrame] frameView] documentView];
+        id documentView = [[[self _selectedOrMainFrame] frameView] documentView];
         if ([documentView isKindOfClass:[WebHTMLView class]])
             [documentView _setToolTip:toolTip];
         return;
@@ -5340,7 +5340,7 @@ static WebFrameView *containingFrameView(NSView *view)
 - (void)_selectionChanged
 {
     if (_private->usesDocumentViews) {
-        id documentView = [[[self selectedFrame] frameView] documentView];
+        id documentView = [[[self _selectedOrMainFrame] frameView] documentView];
         if ([documentView isKindOfClass:[WebHTMLView class]])
             [documentView _selectionChanged];
         return;
