c7710e6d37e19fc1244da12808e190cfe634284e
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-02 23:56:22 +0000

    Revert 19829.
    BUG= http://crbug.com/1455
    
    Review URL: http://codereview.chromium.org/149156
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19861 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/status_bubble_mac.mm
+++ chrome/browser/cocoa/status_bubble_mac.mm
@@ -193,10 +193,6 @@ void StatusBubbleMac::UpdateDownloadShelfVisibility(bool visible) {
   NOTIMPLEMENTED();
 }
 
-void StatusBubbleMac::SetBubbleWidth(int width) {
-  NOTIMPLEMENTED();
-}
-
 void StatusBubbleMac::Create() {
   if (window_)
     return;
