1a3e9edbf143818703a7f8ec38ad60c2f5bb3e39
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-02 18:39:15 +0000

    Fix warnings from update-webkit-localizable-strings
    WebKit:
    
            Fix warnings from update-webkit-localizable-strings
    
            Rubber-stamped by Eric Carlson.
    
            * English.lproj/Localizable.strings: Updated.
    
    WebKit/mac:
    
            Fix warnings from update-webkit-localizable-strings
    
            Rubber-stamped by Eric Carlson.
    
            * WebCoreSupport/WebViewFactory.mm:
            (-[WebViewFactory mediaElementLoadingStateText]):
            (-[WebViewFactory mediaElementLiveBroadcastStateText]):
            Changed the localization comments to match the comments in the Windows
            version of this file, to avoid warnings about different comments for
            the same localized string.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45484 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebViewFactory.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebViewFactory.mm
@@ -601,12 +601,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSString*)mediaElementLoadingStateText
 {
-    return UI_STRING("Loading...", "text that shows up in the media controller when the media is loading");
+    return UI_STRING("Loading...", "Media controller status message when the media is loading");
 }
 
 - (NSString*)mediaElementLiveBroadcastStateText
 {
-    return UI_STRING("Live Broadcast", "text that shows up in the media controller when watching a live broadcast");
+    return UI_STRING("Live Broadcast", "Media controller status message when watching a live broadcast");
 }
 
 @end
