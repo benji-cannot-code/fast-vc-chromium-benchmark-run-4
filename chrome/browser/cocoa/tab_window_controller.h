561dd9d69020a71b446a264c09cbe46e0f30fb26
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-02 20:32:54 +0000

    Pop-up chrome.
    BUG=http://crbug.com/15727
    
    TEST=Load a web page that has a popup.  Example:
      <!DOCTYPE html>
      Click button to open a new window.
      <br>
      <button onclick="w = window.open('http://www.google.com', 'New Window', 'width=512,height=512'); w.moveTo(300,300);">Open with w,h</button>
    
    Now click on the button to get a pop-up.  In the new window, make sure
    there is no "tab" area above the URL bar, and no "new tab" button.
    Hit Cmd-T to create a new tab, and make sure it gets created in the
    OTHER window.
    
    More work is needed to minimize pop-up chrome more, but this'll
    prevent the most brutal failures (e.g. team meeting "demo" today).
    
    
    Review URL: http://codereview.chromium.org/151135
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19837 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_window_controller.h
+++ chrome/browser/cocoa/tab_window_controller.h
@@ -87,6 +87,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The title of the selected tab.
 - (NSString*)selectedTabTitle;
 
+// Called to check if we are a normal window (e.g. not a pop-up) and
+// want normal behavior (e.g. a tab strip).  Return YES if so.  The
+// default implementation returns YES.
+- (BOOL)isNormalWindow;
+
+
 @end
 
 @interface TabWindowController(ProtectedMethods)
