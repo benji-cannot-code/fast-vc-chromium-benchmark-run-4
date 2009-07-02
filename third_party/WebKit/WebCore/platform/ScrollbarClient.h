a1075487aaa4db11239e7a2eef5c44cf0998b657
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-02 17:51:50 +0000

    Fix build bustage caused by wrong include case.
    git-svn-id: svn://svn.chromium.org/blink/trunk@45481 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ScrollbarClient.h
+++ third_party/WebKit/WebCore/platform/ScrollbarClient.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ScrollbarClient_h
 
 #include "IntRect.h"
-#include "ScrollBar.h"
+#include "Scrollbar.h"
 #include <wtf/Vector.h>
 
 namespace WebCore {
