6a66ecf1e1b09a3aba7465000a36a446bf6b07c3
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-02 16:15:01 +0000

    Set the WebViewDelegate earlier during WebView creation to ensure that DidCreateDataSource gets called when constructing the initial data source for the WebView's main frame.
    This ensures that the NavigationState associated with a WebDataSource
    can never be null.  I went ahead and removed some of the null-checks
    that were added to avoid other related crashes.  (To fix bug 15594, I
    really really didn't want to add another one of those null checks.)
    
    BUG=15594
    TEST=partially covered by existing ui tests
    R=dglazkov
    
    Review URL: http://codereview.chromium.org/150208
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19818 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -1077,18 +1077,15 @@ void RenderView::DidPaint() {
 
   WebDataSource* ds = main_frame->GetDataSource();
   NavigationState* navigation_state = NavigationState::FromDataSource(ds);
-  // TODO(darin): It should not be possible for navigation_state to
-  // be null here! But the UI test DownloadTest.IncognitoDownload
-  // can cause it to happen.
-  if (navigation_state) {
-    Time now = Time::Now();
-    if (navigation_state->first_paint_time().is_null()) {
-      navigation_state->set_first_paint_time(now);
-    }
-    if (navigation_state->first_paint_after_load_time().is_null() &&
-        !navigation_state->finish_load_time().is_null()) {
-      navigation_state->set_first_paint_after_load_time(now);
-    }
+  DCHECK(navigation_state);
+
+  Time now = Time::Now();
+  if (navigation_state->first_paint_time().is_null()) {
+    navigation_state->set_first_paint_time(now);
+  }
+  if (navigation_state->first_paint_after_load_time().is_null() &&
+      !navigation_state->finish_load_time().is_null()) {
+    navigation_state->set_first_paint_after_load_time(now);
   }
 }
 
@@ -1319,10 +1316,8 @@ void RenderView::DidReceiveTitle(WebView* webview,
 void RenderView::DidFinishLoadForFrame(WebView* webview, WebFrame* frame) {
   WebDataSource* ds = frame->GetDataSource();
   NavigationState* navigation_state = NavigationState::FromDataSource(ds);
-  // TODO(darin): It should not be possible for navigation_state to be null
-  // here!
-  if (navigation_state)
-    navigation_state->set_finish_load_time(Time::Now());
+  DCHECK(navigation_state);
+  navigation_state->set_finish_load_time(Time::Now());
 }
 
 void RenderView::DidFailLoadWithError(WebView* webview,
@@ -1337,10 +1332,8 @@ void RenderView::DidFinishDocumentLoadForFrame(WebView* webview,
                                                WebFrame* frame) {
   WebDataSource* ds = frame->GetDataSource();
   NavigationState* navigation_state = NavigationState::FromDataSource(ds);
-  // TODO(darin): It should not be possible for navigation_state to be null
-  // here!
-  if (navigation_state)
-    navigation_state->set_finish_document_load_time(Time::Now());
+  DCHECK(navigation_state);
+  navigation_state->set_finish_document_load_time(Time::Now());
 
   Send(new ViewHostMsg_DocumentLoadedInFrame(routing_id_));
 
@@ -2728,6 +2721,7 @@ void RenderView::DidAddHistoryItem() {
   DCHECK(ds != NULL);
 
   NavigationState* navigation_state = NavigationState::FromDataSource(ds);
+  DCHECK(navigation_state);
   if (navigation_state->transition_type() == PageTransition::START_PAGE)
     return;
 
