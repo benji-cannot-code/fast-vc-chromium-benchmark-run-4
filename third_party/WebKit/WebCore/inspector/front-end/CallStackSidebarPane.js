e26c2e6aea3ddf1f529f637007b78815ffec7b1e
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-02 01:03:18 +0000

    2009-07-01  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Timothy Hatcher.
    
            - supported keyboard shortcuts compatible with Firebug in Scripts Debugger:
            F5, Ctrl+/ (win,lin), Command+/ (mac) - continue
            F10, Ctrl+' (win, lin), Command+' (mac) - step over
            F11, Ctrl+; (win, lin), Command+; (mac) - step into
            Shift+F11, Ctrl+Shift+; (win, lin), Command+Shift+; (mac) - step out
    
            - added call stack navigation shortcuts:
            Ctrl+. - next call frame
            Ctrl+, - previouse call frame
    
            https://bugs.webkit.org/show_bug.cgi?id=23849
    
            * inspector/front-end/CallStackSidebarPane.js:
            (WebInspector.CallStackSidebarPane):
            (WebInspector.CallStackSidebarPane.prototype.handleKeyEvent):
            (WebInspector.CallStackSidebarPane.prototype._selectNextCallFrameOnStack):
            (WebInspector.CallStackSidebarPane.prototype._selectPreviousCallFrameOnStack):
            (WebInspector.CallStackSidebarPane.prototype._selectedPlacardByIndex):
            (WebInspector.CallStackSidebarPane.prototype._selectedCallFrameIndex):
            * inspector/front-end/KeyboardShortcut.js: Added.
            (WebInspector.KeyboardShortcut):
            (WebInspector.KeyboardShortcut.makeKey):
            (WebInspector.KeyboardShortcut.makeKeyFromEvent):
            (WebInspector.KeyboardShortcut.makeKeyFromCodeAndModifiers_):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel):
            (WebInspector.ScriptsPanel.prototype.handleKeyEvent):
            * inspector/front-end/WebKit.qrc:
            * inspector/front-end/inspector.html:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45460 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/CallStackSidebarPane.js
+++ third_party/WebKit/WebCore/inspector/front-end/CallStackSidebarPane.js
@@ -27,6 +27,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.CallStackSidebarPane = function()
 {
     WebInspector.SidebarPane.call(this, WebInspector.UIString("Call Stack"));
+    
+    this._shortcuts = {};
+
+    var shortcut = WebInspector.KeyboardShortcut.makeKey(WebInspector.KeyboardShortcut.KeyCodes.Period,
+                                                         WebInspector.KeyboardShortcut.Modifiers.Ctrl);
+    this._shortcuts[shortcut] = this._selectNextCallFrameOnStack.bind(this);
+
+    var shortcut = WebInspector.KeyboardShortcut.makeKey(WebInspector.KeyboardShortcut.KeyCodes.Comma,
+                                                         WebInspector.KeyboardShortcut.Modifiers.Ctrl);
+    this._shortcuts[shortcut] = this._selectPreviousCallFrameOnStack.bind(this);
 }
 
 WebInspector.CallStackSidebarPane.prototype = {
@@ -99,6 +109,53 @@ WebInspector.CallStackSidebarPane.prototype = {
         }
 
         this.dispatchEventToListeners("call frame selected");
+    },
+
+    handleKeyEvent: function(event)
+    {
+        var shortcut = WebInspector.KeyboardShortcut.makeKeyFromEvent(event);
+        var handler = this._shortcuts[shortcut];
+        if (handler) {
+            handler(event);
+            event.preventDefault();
+            event.handled = true;
+        }
+    },
+
+    _selectNextCallFrameOnStack: function()
+    {
+        var index = this._selectedCallFrameIndex();
+        if (index == -1)
+            return;
+        this._selectedPlacardByIndex(index + 1);
+    },
+
+    _selectPreviousCallFrameOnStack: function()
+    {
+        var index = this._selectedCallFrameIndex();
+        if (index == -1)
+            return;
+        this._selectedPlacardByIndex(index - 1);
+    },
+
+    _selectedPlacardByIndex: function(index)
+    {
+        if (index < 0 || index >= this.placards.length)
+            return;
+        var placard = this.placards[index];
+        this.selectedCallFrame = placard.callFrame
+    },
+
+    _selectedCallFrameIndex: function()
+    {
+        if (!this._selectedCallFrame)
+            return -1;
+        for (var i = 0; i < this.placards.length; ++i) {
+            var placard = this.placards[i];
+            if (placard.callFrame === this._selectedCallFrame)
+                return i;
+        }
+        return -1;
     },
 
     _placardSelected: function(event)
