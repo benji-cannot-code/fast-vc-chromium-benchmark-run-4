b6dd93a06ad7d4d0dccef71a99ff833990ed3cd4
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-03 02:29:18 +0000

    Fixing unitialized variable (Coverity nit).
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/155025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19888 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/json_value_serializer.h
+++ chrome/common/json_value_serializer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,6 +29,7 @@ class JSONStringValueSerializer : public ValueSerializer {
   JSONStringValueSerializer(const std::string& json_string)
       : json_string_(&const_cast<std::string&>(json_string)),
         initialized_with_const_string_(true),
+        pretty_print_(false),
         allow_trailing_comma_(false) {
   }
 
@@ -59,7 +60,7 @@ class JSONStringValueSerializer : public ValueSerializer {
   // If true, deserialization will allow trailing commas.
   bool allow_trailing_comma_;
 
-  DISALLOW_EVIL_CONSTRUCTORS(JSONStringValueSerializer);
+  DISALLOW_COPY_AND_ASSIGN(JSONStringValueSerializer);
 };
 
 class JSONFileValueSerializer : public ValueSerializer {
