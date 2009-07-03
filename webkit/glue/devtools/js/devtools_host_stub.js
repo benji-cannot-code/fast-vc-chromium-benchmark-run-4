bc08362ba4ae229fbaf468a67f04746c60c001c8
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-03 11:07:48 +0000

    DevTools: Console tests. Review URL: http://codereview.chromium.org/149178
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19904 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/devtools_host_stub.js
+++ webkit/glue/devtools/js/devtools_host_stub.js
@@ -385,6 +385,11 @@ DevToolsHostStub.prototype.addResourceSourceToFrame = function(
 };
 
 
+DevToolsHostStub.prototype.addSourceToFrame = function(mimeType, source,
+    element) {
+};
+
+
 if (!window['DevToolsHost']) {
   window['RemoteDebuggerAgent'] = new RemoteDebuggerAgentStub();
   window['RemoteDebuggerCommandExecutor'] =
