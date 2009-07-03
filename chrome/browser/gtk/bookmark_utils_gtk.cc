a63929c30c70d4c764a36839879f029fa80ca096
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-03 00:43:13 +0000

    Change some numbers on the gtk bookmark bar.
    height = 29 matches windows constant
    padding between bookmark name and favico: I couldn't find a relevant constant in the windows code, so I experimentally matched the spacing between the two, for the same bookmark on linux and windows.
    
    BUG=http://crbug.com/15882
    TEST=look at bookmark bar
    
    Review URL: http://codereview.chromium.org/155019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19871 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/bookmark_utils_gtk.cc
+++ chrome/browser/gtk/bookmark_utils_gtk.cc
@@ -48,7 +48,7 @@ namespace bookmark_utils {
 
 const char kBookmarkNode[] = "bookmark-node";
 
-const int kBarButtonPadding = 2;
+const int kBarButtonPadding = 4;
 
 GdkPixbuf* GetFolderIcon() {
   ResourceBundle& rb = ResourceBundle::GetSharedInstance();
