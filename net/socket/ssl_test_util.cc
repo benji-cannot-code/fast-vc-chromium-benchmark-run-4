9f294ddfef705f4752fbf41c2453a010b38e7508
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-06 18:32:21 +0000

    Fix up some code not to set off coverity warnings. BUG=none TEST=none
    Review URL: http://codereview.chromium.org/149149
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19962 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_test_util.cc
+++ net/socket/ssl_test_util.cc
@@ -109,7 +109,7 @@ TestServerLauncher::TestServerLauncher() : process_handle_(NULL)
 
 namespace {
 
-void AppendToPythonPath(FilePath dir) {
+void AppendToPythonPath(const FilePath& dir) {
   // Do nothing if dir already on path.
 
 #if defined(OS_WIN)
