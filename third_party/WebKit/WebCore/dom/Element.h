13dbc46b0ddc315508eec88524ffdcea962c6d9d
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-07 23:45:45 +0000

    WebCore:
            Reviewed by Dave Hyatt and Darin Adler.
    
            - fix https://bugs.webkit.org/show_bug.cgi?id=26963
              <rdar://problem/7030998> Reproducible crash at
              FontCache::getFontData() when a custom font is used in a pseudo-style
    
            Test: fast/css/pseudo-cache-stale.html
    
            * dom/Element.cpp:
            (WebCore::Element::pseudoStyleCacheIsInvalid): Added. Given the old
            style and the new style, goes over cached pseudo-styles in the old
            style and re-resolves the same style types off the new style. If any of
            the new pseudo-styles is different from the currently cached
            corresponding style, returns true. Otherwise, returns false.
            (WebCore::Element::recalcStyle): Validate the pseudo-style cache before
            deciding to keep the existing style.
            * dom/Element.h:
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::uncachedFirstLineStyle): Added this version that
            returns an uncached first-line style based off the given style.
            (WebCore::RenderObject::getUncachedPseudoStyle): Added the 'ownStyle'
            parameter.
            * rendering/RenderObject.h:
            * rendering/style/RenderStyle.cpp:
            (WebCore::RenderStyle::getPseudoStyleCache): Added. Returns the cached
            pseudo-styles in the passed-in vector.
            * rendering/style/RenderStyle.h:
    
    LayoutTests:
    
            Reviewed by Dave Hyatt and Darin Adler.
    
            - test for https://bugs.webkit.org/show_bug.cgi?id=26963
              <rdar://problem/7030998> Reproducible crash at
              FontCache::getFontData() when a custom font is used in a pseudo-style
    
            * fast/css/pseudo-cache-stale.html: Added.
            * platform/mac/fast/css/pseudo-cache-stale-expected.checksum: Added.
            * platform/mac/fast/css/pseudo-cache-stale-expected.png: Added.
            * platform/mac/fast/css/pseudo-cache-stale-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45612 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Element.h
+++ third_party/WebKit/WebCore/dom/Element.h
@@ -171,6 +171,8 @@ public:
     virtual void formatForDebugger(char* buffer, unsigned length) const;
 #endif
 
+    bool pseudoStyleCacheIsInvalid(const RenderStyle* currentStyle, RenderStyle* newStyle);
+
     String innerText() const;
     String outerText() const;
  
