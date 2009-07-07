14f54a19ff0c60cdd6743502d59f3ccafc238555
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-07 00:59:02 +0000

    Bug 27001: Fix improper use of PassRefPtr<Node> to RefPtr<Node>
            <https://bugs.webkit.org/show_bug.cgi?id=27001>
    
            Reviewed by Geoff Garen.
    
            PassRefPtr<> should only be used for arguments to functions that
            take ownership of the object, or as return values from functions
            that relinquish ownership of the object.
    
            * editing/Editor.cpp:
            (WebCore::Editor::increaseSelectionListLevelOrdered): Changed
            stack-allocated PassRefPtr<Node> to RefPtr<Node> and call
            release() on returned object.
            (WebCore::Editor::increaseSelectionListLevelUnordered): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45577 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/Editor.cpp
+++ third_party/WebKit/WebCore/editing/Editor.cpp
@@ -656,9 +656,9 @@ PassRefPtr<Node> Editor::increaseSelectionListLevelOrdered()
     if (!canEditRichly() || m_frame->selection()->isNone())
         return 0;
     
-    PassRefPtr<Node> newList = IncreaseSelectionListLevelCommand::increaseSelectionListLevelOrdered(m_frame->document());
+    RefPtr<Node> newList = IncreaseSelectionListLevelCommand::increaseSelectionListLevelOrdered(m_frame->document());
     revealSelectionAfterEditingOperation();
-    return newList;
+    return newList.release();
 }
 
 PassRefPtr<Node> Editor::increaseSelectionListLevelUnordered()
@@ -666,9 +666,9 @@ PassRefPtr<Node> Editor::increaseSelectionListLevelUnordered()
     if (!canEditRichly() || m_frame->selection()->isNone())
         return 0;
     
-    PassRefPtr<Node> newList = IncreaseSelectionListLevelCommand::increaseSelectionListLevelUnordered(m_frame->document());
+    RefPtr<Node> newList = IncreaseSelectionListLevelCommand::increaseSelectionListLevelUnordered(m_frame->document());
     revealSelectionAfterEditingOperation();
-    return newList;
+    return newList.release();
 }
 
 void Editor::decreaseSelectionListLevel()
