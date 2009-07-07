cb0af4e4420b8eacb2460a6a4a373976e59a0026
glen@chromium.org <glen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-07 17:36:23 +0000

    Fix retarded bitmath. << 8 != * 255
    BUG=13360
    
    Review URL: http://codereview.chromium.org/149164
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20040 0039d316-1c4b-4281-b951-d872f2087c98

--- base/gfx/png_encoder.cc
+++ base/gfx/png_encoder.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/scoped_ptr.h"
 #include "third_party/skia/include/core/SkBitmap.h"
+#include "third_party/skia/include/core/SkUnPreMultiply.h"
 
 extern "C" {
 #include "third_party/libpng/png.h"
@@ -215,9 +216,10 @@ bool PNGEncoder::EncodeBGRASkBitmap(const SkBitmap& input,
 
       int alpha = SkColorGetA(pixel);
       if (alpha != 0 && alpha != 255) {
-        divided[i + 0] = (SkColorGetR(pixel) << 8) / alpha;
-        divided[i + 1] = (SkColorGetG(pixel) << 8) / alpha;
-        divided[i + 2] = (SkColorGetB(pixel) << 8) / alpha;
+        SkColor unmultiplied = SkUnPreMultiply::PMColorToColor(pixel);
+        divided[i + 0] = SkColorGetR(unmultiplied);
+        divided[i + 1] = SkColorGetG(unmultiplied);
+        divided[i + 2] = SkColorGetB(unmultiplied);
         divided[i + 3] = alpha;
       } else {
         divided[i + 0] = SkColorGetR(pixel);
