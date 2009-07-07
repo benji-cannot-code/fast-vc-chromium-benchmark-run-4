8962bbc132f8f0d624fb5982f16d0d2120dcbe06
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-07 17:31:49 +0000

    Refactoring to introduce refcount to WebMediaPlayerImpl WebMediaPlayerImpl interacts with multiple threads that it becomes necessary to make it refcounted so we can post task on different threads' message loop.
    Review URL: http://codereview.chromium.org/147225
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20038 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/media/video_renderer_impl.cc
+++ webkit/glue/media/video_renderer_impl.cc
@@ -10,12 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace webkit_glue {
 
-VideoRendererImpl::VideoRendererImpl(WebMediaPlayerImpl* delegate)
-    : delegate_(delegate),
+  VideoRendererImpl::VideoRendererImpl(WebMediaPlayerImpl::Proxy* proxy)
+    : proxy_(proxy),
       last_converted_frame_(NULL) {
   // TODO(hclam): decide whether to do the following line in this thread or
   // in the render thread.
-  delegate_->SetVideoRenderer(this);
+  proxy->SetVideoRenderer(this);
 }
 
 // static
@@ -25,7 +25,6 @@ bool VideoRendererImpl::IsMediaFormatSupported(
   int height = 0;
   return ParseMediaFormat(media_format, &width, &height);
 }
-
 
 bool VideoRendererImpl::OnInitialize(media::VideoDecoder* decoder) {
   int width = 0;
@@ -45,11 +44,14 @@ bool VideoRendererImpl::OnInitialize(media::VideoDecoder* decoder) {
 }
 
 void VideoRendererImpl::OnStop() {
-  delegate_->SetVideoRenderer(NULL);
+  DCHECK(proxy_);
+  proxy_->SetVideoRenderer(NULL);
+  proxy_ = NULL;
 }
 
 void VideoRendererImpl::OnFrameAvailable() {
-  delegate_->PostRepaintTask();
+  DCHECK(proxy_);
+  proxy_->Repaint();
 }
 
 void VideoRendererImpl::SetRect(const gfx::Rect& rect) {
