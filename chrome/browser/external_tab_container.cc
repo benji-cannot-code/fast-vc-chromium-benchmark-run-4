0110bee133b9dcf05b3c40dbe8d0ca3f680d7f84
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-07 17:15:55 +0000

    Fix a typo.  TextDirection is the name of the enum, while GetTextDirection returns the current text direction.  Also fixes a few lint nits.
    CID=4539
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/149257
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20035 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/external_tab_container.cc
+++ chrome/browser/external_tab_container.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/external_tab_container.h"
 
+#include <string>
+
 #include "app/l10n_util.h"
 #include "app/win_util.h"
 #include "base/logging.h"
@@ -241,7 +243,7 @@ void ExternalTabContainer::ToolbarSizeChanged(TabContents* source,
 void ExternalTabContainer::ForwardMessageToExternalHost(
     const std::string& message, const std::string& origin,
     const std::string& target) {
-  if(automation_) {
+  if (automation_) {
     automation_->Send(
         new AutomationMsg_ForwardMessageToExternalHost(0, tab_handle_,
             message, origin, target));
@@ -272,7 +274,7 @@ bool ExternalTabContainer::HandleContextMenu(const ContextMenuParams& params) {
   POINT screen_pt = { params.x, params.y };
   MapWindowPoints(GetNativeView(), HWND_DESKTOP, &screen_pt, 1);
 
-  bool rtl = l10n_util::TextDirection() == l10n_util::RIGHT_TO_LEFT;
+  bool rtl = l10n_util::GetTextDirection() == l10n_util::RIGHT_TO_LEFT;
   automation_->Send(
       new AutomationMsg_ForwardContextMenuToExternalHost(0, tab_handle_,
           external_context_menu_->GetMenuHandle(), screen_pt.x, screen_pt.y,
