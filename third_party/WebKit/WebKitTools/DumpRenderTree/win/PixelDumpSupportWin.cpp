90263b6905e742127e5cbf819b86bbbb3b5ee86a
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-08 18:56:22 +0000

    2009-07-08  Brent Fulgham  <bfulgham@webkit.org>
            Build fix for Windows Cairo.
    
            Cairo build was incorrectly #including the PixelDumpSupportCG.h
            instead of PixelDumpSupportCairo.h
    
            * DumpRenderTree/win/PixelDumpSupportWin.cpp: Correct #include
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45631 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/win/PixelDumpSupportWin.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/win/PixelDumpSupportWin.cpp
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if PLATFORM(CG)
 #include "PixelDumpSupportCG.h"
 #elif PLATFORM(CAIRO)
-#include "PixelDumpSupportCG.h"
+#include "PixelDumpSupportCairo.h"
 #endif
 
 #include "DumpRenderTree.h"
