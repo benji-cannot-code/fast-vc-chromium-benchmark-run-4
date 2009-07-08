e32314b59245f1a4aa77757bd28c6aeef6d499e6
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-08 23:12:27 +0000

    Make bookmark bar folder buttons look active so long as the menu is open.
    Make sure chrome buttons have no child displacement (so the contents don't jiggle when you press them).
    
    BUG=none
    TEST=do a click on a folder, click away, make sure the button always looks as you'd expect
    
    Review URL: http://codereview.chromium.org/149355
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20203 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/bookmark_menu_controller_gtk.cc
+++ chrome/browser/gtk/bookmark_menu_controller_gtk.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "chrome/browser/gtk/bookmark_context_menu.h"
 #include "chrome/browser/gtk/bookmark_utils_gtk.h"
+#include "chrome/browser/gtk/gtk_chrome_button.h"
 #include "chrome/browser/gtk/gtk_dnd_util.h"
 #include "chrome/browser/gtk/gtk_theme_provider.h"
 #include "chrome/browser/gtk/menu_gtk.h"
@@ -56,9 +57,12 @@ BookmarkMenuController::BookmarkMenuController(Browser* browser,
       page_navigator_(navigator),
       parent_window_(window),
       node_(node),
-      ignore_button_release_(false) {
+      ignore_button_release_(false),
+      triggering_widget_(NULL) {
   menu_.Own(gtk_menu_new());
   BuildMenu(node, start_child_index, menu_.get());
+  g_signal_connect(menu_.get(), "hide",
+                   G_CALLBACK(OnMenuHidden), this);
   gtk_widget_show_all(menu_.get());
 }
 
@@ -71,6 +75,9 @@ void BookmarkMenuController::Popup(GtkWidget* widget, gint button_type,
                                    guint32 timestamp) {
   profile_->GetBookmarkModel()->AddObserver(this);
 
+  triggering_widget_ = widget;
+  gtk_chrome_button_set_paint_state(GTK_CHROME_BUTTON(widget),
+                                    GTK_STATE_ACTIVE);
   gtk_menu_popup(GTK_MENU(menu_.get()), NULL, NULL,
                  &MenuGtk::MenuPositionFunc,
                  widget, button_type, timestamp);
@@ -158,6 +165,7 @@ void BookmarkMenuController::BuildMenu(const BookmarkNode* parent,
   }
 }
 
+// static
 gboolean BookmarkMenuController::OnButtonPressed(
     GtkWidget* sender,
     GdkEventButton* event,
@@ -182,6 +190,7 @@ gboolean BookmarkMenuController::OnButtonPressed(
   return FALSE;
 }
 
+// static
 gboolean BookmarkMenuController::OnButtonReleased(
     GtkWidget* sender,
     GdkEventButton* event,
@@ -207,6 +216,16 @@ gboolean BookmarkMenuController::OnButtonReleased(
   return FALSE;
 }
 
+// static
+void BookmarkMenuController::OnMenuHidden(GtkWidget* menu,
+    BookmarkMenuController* controller) {
+  if (controller->triggering_widget_) {
+    gtk_chrome_button_unset_paint_state(
+        GTK_CHROME_BUTTON(controller->triggering_widget_));
+  }
+}
+
+// static
 void BookmarkMenuController::OnMenuItemActivated(
     GtkMenuItem* menu_item, BookmarkMenuController* controller) {
   controller->NavigateToMenuItem(GTK_WIDGET(menu_item), CURRENT_TAB);
