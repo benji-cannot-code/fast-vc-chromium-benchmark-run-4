72f01ec8d79a20f00cf58044199cb9899b9016aa
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-08 21:22:38 +0000

    Bug 27083: bugzilla.py: Clean up bug_server use
            <https://bugs.webkit.org/show_bug.cgi?id=27083>
    
            Reviewed by Darin Adler.
    
            * Scripts/modules/bugzilla.py:
            (Bugzilla.fetch_bug_ids_from_commit_queue): Use bug_server
            instead of hard-coding the URL.
            (Bugzilla.authenticate): Remove extra '/' before URL path.
            (Bugzilla.add_patch_to_bug): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45638 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/bugzilla.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/bugzilla.py
@@ -179,7 +179,7 @@ class Bugzilla:
     def fetch_bug_ids_from_commit_queue(self):
         # FIXME: We should have an option for restricting the search by email.  Example:
         # unassigned_only = "&emailassigned_to1=1&emailtype1=substring&email1=unassigned"
-        commit_queue_url = "https://bugs.webkit.org/buglist.cgi?query_format=advanced&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&field0-0-0=flagtypes.name&type0-0-0=equals&value0-0-0=review%2B"
+        commit_queue_url = self.bug_server + "buglist.cgi?query_format=advanced&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&field0-0-0=flagtypes.name&type0-0-0=equals&value0-0-0=review%2B"
         log("Loading commit queue")
 
         page = urllib2.urlopen(commit_queue_url)
@@ -217,7 +217,7 @@ class Bugzilla:
         if self.dryrun:
             self.authenticated = True
             return
-        self.browser.open(self.bug_server + "/index.cgi?GoAheadAndLogIn=1")
+        self.browser.open(self.bug_server + "index.cgi?GoAheadAndLogIn=1")
         self.browser.select_form(name="login")
         self.browser['Bugzilla_login'] = username
         self.browser['Bugzilla_password'] = password
@@ -239,7 +239,7 @@ class Bugzilla:
             log(comment_text)
             return
         
-        self.browser.open(self.bug_server + "/attachment.cgi?action=enter&bugid=" + bug_id)
+        self.browser.open(self.bug_server + "attachment.cgi?action=enter&bugid=" + bug_id)
         self.browser.select_form(name="entryform")
         self.browser['description'] = description
         self.browser['ispatch'] = ("1",)
