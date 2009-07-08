a5b11b71d11d8213ccb8ced61918377e523f278a
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-08 14:53:13 +0000

    Removes ifdefs in browser_render_process_host.cc that prevented adding custom words to the dictionary on platforms other than windows. This functionality has now been implemented on all platforms. Patch from pwicks86@gmail.com (Paul Wicks) BUG=None TEST=Adding words to dictionary on mac and linux.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/render_widget_host.h"
 #include "chrome/browser/renderer_host/resource_message_filter.h"
 #include "chrome/browser/renderer_host/web_cache_manager.h"
+#include "chrome/browser/spellchecker.h"
 #include "chrome/browser/visitedlink_master.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/chrome_descriptors.h"
@@ -67,7 +68,6 @@ using WebKit::WebCache;
 // TODO(port): see comment by the only usage of RenderViewHost in this file.
 #include "chrome/browser/renderer_host/render_view_host.h"
 
-#include "chrome/browser/spellchecker.h"
 
 // Once the above TODO is finished, then this block is all Windows-specific
 // files.
@@ -540,17 +540,11 @@ void BrowserRenderProcessHost::WidgetHidden() {
 }
 
 void BrowserRenderProcessHost::AddWord(const std::wstring& word) {
-#if !defined(OS_WIN)
-  // TODO(port): reimplement when we get the spell checker up and running on
-  // other platforms.
-  NOTIMPLEMENTED();
-#else
   base::Thread* io_thread = g_browser_process->io_thread();
   if (profile()->GetSpellChecker()) {
     io_thread->message_loop()->PostTask(FROM_HERE, NewRunnableMethod(
         profile()->GetSpellChecker(), &SpellChecker::AddWord, word));
   }
-#endif // !defined(OS_WIN)
 }
 
 void BrowserRenderProcessHost::AddVisitedLinks(
