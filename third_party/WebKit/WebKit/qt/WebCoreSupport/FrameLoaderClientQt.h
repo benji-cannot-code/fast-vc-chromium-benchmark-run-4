9c9330389a1c79bc768b4b745318e0d0df8c001e
ariya@webkit.org <ariya@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-08 16:38:40 +0000

    2009-07-08  Pradeepto Bhattacharya  <pradeepto@kde.org>
            Reviewed by Ariya Hidayat.
    
            Build fix.
    
            * WebCoreSupport/FrameLoaderClientQt.h: Removed the slot slotCallPolicyFunction().
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
@@ -60,8 +60,6 @@ namespace WebCore {
 
         friend class ::QWebFrame;
         void callPolicyFunction(FramePolicyFunction function, PolicyAction action);
-    private slots:
-        void slotCallPolicyFunction(int);
     signals:
         void loadStarted();
         void loadProgress(int d);
