62f7a6ef9cce186be8b8df574f0232cc06255eb3
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-08 16:47:36 +0000

    Add context menus to tabs. BUG=14920 TEST=context menus on tabs. Review URL: http://codereview.chromium.org/155173
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20155 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_controller_target.h
+++ chrome/browser/cocoa/tab_controller_target.h
@@ -6,10 +6,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_COCOA_TAB_CONTROLLER_TARGET_H_
 #define CHROME_BROWSER_COCOA_TAB_CONTROLLER_TARGET_H_
 
+#include "chrome/browser/tabs/tab_strip_model.h"
+
+@class TabController;
+
 // A protocol to be implemented by a TabController's target.
 @protocol TabControllerTarget
 - (void)selectTab:(id)sender;
 - (void)closeTab:(id)sender;
+
+// Dispatch context menu commands for the given tab controller.
+- (void)commandDispatch:(TabStripModel::ContextMenuCommand)command
+          forController:(TabController*)controller;
+// Returns YES if the specificed command should be enabled for the given
+// controller.
+- (BOOL)isCommandEnabled:(TabStripModel::ContextMenuCommand)command
+           forController:(TabController*)controller;
 @end
 
 #endif  // CHROME_BROWSER_COCOA_TAB_CONTROLLER_TARGET_H_
