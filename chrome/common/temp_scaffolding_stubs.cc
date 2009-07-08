cf3be5cb03017b5ed17b6b0d974dc38dd492c82e
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-08 03:58:29 +0000

    linux: port Jankometer
    Originally I had split it into multiple files, but I think it's
    cleaner to just use one.
    
    (I want to use this for measuring plugin jank.)
    
    BUG=8077
    
    Review URL: http://codereview.chromium.org/155194
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20130 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.cc
+++ chrome/common/temp_scaffolding_stubs.cc
@@ -205,6 +205,7 @@ Upgrade::TryResult ShowTryChromeDialog() {
 
 //--------------------------------------------------------------------------
 
+#if defined(OS_MACOSX)
 void InstallJankometer(const CommandLine&) {
   // http://code.google.com/p/chromium/issues/detail?id=8077
 }
@@ -212,6 +213,7 @@ void InstallJankometer(const CommandLine&) {
 void UninstallJankometer() {
   // http://code.google.com/p/chromium/issues/detail?id=8077
 }
+#endif
 
 //--------------------------------------------------------------------------
 
