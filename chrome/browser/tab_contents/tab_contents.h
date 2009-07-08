cc06e727ee4616eea983da666a2f14f9a20da319
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-08 16:39:02 +0000

    Remove url_constants dependency from tab_contents.h.
    Remove obsolete SupportsURL function from when we have TabContents types.
    
    Review URL: http://codereview.chromium.org/155171
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20154 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.h
+++ chrome/browser/tab_contents/tab_contents.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/navigation_controller.h"
 #include "chrome/browser/tab_contents/page_navigator.h"
 #include "chrome/browser/tab_contents/render_view_host_manager.h"
-#include "chrome/common/url_constants.h"
 #include "chrome/common/gears_api.h"
 #include "chrome/common/navigation_types.h"
 #include "chrome/common/notification_registrar.h"
@@ -141,15 +140,8 @@ class TabContents : public PageNavigator,
   // NavigationController).
   Profile* profile() const { return controller_.profile(); }
 
-  // Returns whether this tab contents supports the provided URL.This method
-  // matches the tab contents type with the result of TypeForURL(). |url| points
-  // to the actual URL that will be used. It can be modified as needed.
-  bool SupportsURL(GURL* url);
-
   // Returns true if contains content rendered by an extension.
-  const bool HostsExtension() const {
-    return GetURL().SchemeIs(chrome::kExtensionScheme);
-  }
+  bool HostsExtension() const;
 
   // Returns the AutofillManager, creating it if necessary.
   AutofillManager* GetAutofillManager();
