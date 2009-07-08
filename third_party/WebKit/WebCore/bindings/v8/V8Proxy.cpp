edfce9271cf9887268613d2a2a9dc0ba83a9150f
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-08 23:06:29 +0000

    Unreviewed fix for Chromium link errors.
    git-svn-id: svn://svn.chromium.org/blink/trunk@45649 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8Proxy.cpp
+++ third_party/WebKit/WebCore/bindings/v8/V8Proxy.cpp
@@ -509,7 +509,7 @@ private:
 };
 
 // Create object groups for DOM tree nodes.
-static void gcPrologue()
+void V8GCController::gcPrologue()
 {
     v8::HandleScope scope;
 
@@ -552,7 +552,7 @@ ACTIVE_DOM_OBJECT_TYPES(MAKE_CASE)
 }
 };
 
-static void gcEpilogue()
+void V8GCController::gcEpilogue()
 {
     v8::HandleScope scope;
 
