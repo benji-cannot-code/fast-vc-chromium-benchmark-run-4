99418dd1e7fe6253aa10c22edead2a2fb974f3a8
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-08 21:05:48 +0000

    Bug 27081: Wrap RunLoopTimerCF.cpp in PLATFORM(MAC) && HAVE(RUNLOOP_TIMER)
            <https://bugs.webkit.org/show_bug.cgi?id=27081>
    
            Reviewed by Timothy Hatcher.
    
            * platform/cf/RunLoopTimerCF.cpp: This code is only used on
            Mac OS X when HAVE(RUNLOOP_TIMER) is enabled, so wrap the code
            in that macro as well.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/cf/RunLoopTimerCF.cpp
+++ third_party/WebKit/WebCore/platform/cf/RunLoopTimerCF.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "RunLoopTimer.h"
 
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) && HAVE(RUNLOOP_TIMER)
 
 namespace WebCore {
 
@@ -82,4 +82,4 @@ bool RunLoopTimerBase::isActive() const
 
 } // namespace WebCore
 
-#endif
+#endif // PLATFORM(MAC) && HAVE(RUNLOOP_TIMER)
