6cf32c3dad3d768ab735fbaad2cf829140a78908
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-09 23:57:23 +0000

    Made MediaFilter::host_ and MediaFilter::message_loop_ private.
    More cleanup relating to pipeline cleanup.
    
    BUG=16008
    TEST=no real code chage, nothing should change
    
    Review URL: http://codereview.chromium.org/155230
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20338 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_video_decoder.cc
+++ media/filters/ffmpeg_video_decoder.cc
@@ -338,7 +338,7 @@ VideoSurface::Format FFmpegVideoDecoder::GetSurfaceFormat(
 }
 
 void FFmpegVideoDecoder::SignalPipelineError() {
-  host_->Error(PIPELINE_ERROR_DECODE);
+  host()->Error(PIPELINE_ERROR_DECODE);
   state_ = kDecodeFinished;
 }
 
