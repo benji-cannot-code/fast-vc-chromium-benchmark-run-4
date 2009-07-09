acad8f562faf445720288e366ae233592315ca3b
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-09 00:03:46 +0000

    ExtensionsServiceBackend::GarbageCollectionExtensions() makes a local copy of install_directory_. It was forcing the persistent copy to an absolute path which was causing the ExternsionsService and ExtensionsPrefs install_directory_ values to diverge.
    Re-enable ExternalInstallPref on mac.
    
    TBR=mpcomplete
    
    Review URL: http://codereview.chromium.org/155187
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20214 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service_unittest.cc
+++ chrome/browser/extensions/extensions_service_unittest.cc
@@ -1228,10 +1228,6 @@ TEST_F(ExtensionsServiceTest, ExternalInstallRegistry) {
 
 #endif
 
-// TODO(rafaelw): Re-enable on mac. This broke with:
-// http://codereview.chromium.org/140018
-#if !defined(OS_MACOSX)
-
 TEST_F(ExtensionsServiceTest, ExternalInstallPref) {
   InitializeEmptyExtensionsService();
   // Verify that starting with no providers loads no extensions.
@@ -1344,8 +1340,6 @@ TEST_F(ExtensionsServiceTest, ExternalInstallPref) {
   ASSERT_TRUE(GetErrors()[0].find("Extensions are not enabled") !=
               std::string::npos);
 }
-
-#endif  // !defined(OS_MACOSX)
 
 TEST_F(ExtensionsServiceTest, ExternalPrefProvider) {
   InitializeEmptyExtensionsService();
