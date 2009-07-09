0155f4bd8156618ef41c33988ce06644b5b90d68
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-09 18:42:05 +0000

    Support running app_unittests under Valgrind
    Review URL: http://codereview.chromium.org/155216
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20290 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/chrome_tests.py
+++ tools/valgrind/chrome_tests.py
@@ -89,6 +89,7 @@ class ChromeTests:
       "test_shell": self.TestTestShell, "test_shell_tests": self.TestTestShell,
       "ui": self.TestUI,                "ui_tests": self.TestUI,
       "unit": self.TestUnit,            "unit_tests": self.TestUnit,
+      "app": self.TestApp,               "app_unittests": self.TestApp,
     }
 
     if test not in self._test_list:
@@ -257,6 +258,9 @@ class ChromeTests:
 
   def TestUnit(self):
     return self.SimpleTest("chrome", "unit_tests")
+
+  def TestApp(self):
+    return self.SimpleTest("chrome", "app_tests")
 
   def TestUI(self):
     return self.SimpleTest("chrome", "ui_tests",
