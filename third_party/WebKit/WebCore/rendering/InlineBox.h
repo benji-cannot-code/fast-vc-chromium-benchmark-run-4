191a419e367c448f0b3875e72432b51a75e09958
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-10 05:26:42 +0000

    2009-07-09  Roland Steiner  <rolandsteiner@google.com>
            Reviewed by Maciej Stachowiak.
    
            added InlineBox::isLeaf()
            firstLeafChild()/lastLeafChild() not virtual and not callable on InlineBox anymore.
            firstLeafChild()/lastLeafChild() will no longer return a node outside of the given subtree.   
            Removed firstLeafChildAfterBox()/lastLeafChildBeforeBox()
            Removed potentially quadratic behavior if all nodes before/after a given box are empty InlineFlowBoxes
    
            Currently, these methods are called on RootInlineBox objects only, so above changes should not have
            any observable effect (only the removal of the square performance behavior could apply, 
            but the conditions for that are probably of a rather theoretical nature).
    
            * rendering/InlineBox.cpp:
            (WebCore::InlineBox::nextLeafChild):
            (WebCore::InlineBox::prevLeafChild):
            * rendering/InlineBox.h:
            (WebCore::InlineBox::isLeaf):
            * rendering/InlineFlowBox.cpp:
            (WebCore::InlineFlowBox::firstLeafChild):
            (WebCore::InlineFlowBox::lastLeafChild):
            * rendering/InlineFlowBox.h:
            (WebCore::InlineFlowBox::firstChild):
            (WebCore::InlineFlowBox::lastChild):
            (WebCore::InlineFlowBox::isLeaf):
            * rendering/RootInlineBox.cpp:
            (WebCore::RootInlineBox::closestLeafChildForXPos):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/InlineBox.h
+++ third_party/WebKit/WebCore/rendering/InlineBox.h
@@ -176,10 +176,10 @@ public:
     bool nextOnLineExists() const;
     bool prevOnLineExists() const;
 
-    virtual InlineBox* firstLeafChild();
-    virtual InlineBox* lastLeafChild();
-    InlineBox* nextLeafChild();
-    InlineBox* prevLeafChild();
+    virtual bool isLeaf() const { return true; }
+    
+    InlineBox* nextLeafChild() const;
+    InlineBox* prevLeafChild() const;
         
     RenderObject* renderer() const { return m_renderer; }
 
