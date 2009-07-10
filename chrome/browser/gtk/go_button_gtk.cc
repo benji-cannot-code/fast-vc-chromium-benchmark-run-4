810962f990f95798914106108c9078f54adfb8b7
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-10 19:41:53 +0000

    Revert "Revert "Add button tinting to the toolbar buttons.""
    This reverts commit r20399 and re-applies the button tinting
    code.  There's a fix for the go button gtk unittest by checking
    to see if the browser is non-NULL before getting the theme provider.
    
    TBR=erg
    
    Review URL: http://codereview.chromium.org/155369
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20402 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/go_button_gtk.cc
+++ chrome/browser/gtk/go_button_gtk.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/browser/browser.h"
 #include "chrome/browser/gtk/location_bar_view_gtk.h"
+#include "chrome/browser/profile.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
 
@@ -22,8 +23,10 @@ GoButtonGtk::GoButtonGtk(LocationBarViewGtk* location_bar, Browser* browser)
       intended_mode_(MODE_GO),
       visible_mode_(MODE_GO),
       state_(BS_NORMAL),
-      go_(IDR_GO, IDR_GO_P, IDR_GO_H, 0),
-      stop_(IDR_STOP, IDR_STOP_P, IDR_STOP_H, 0),
+      go_(browser ? browser->profile()->GetThemeProvider() : NULL,
+          IDR_GO, IDR_GO_P, IDR_GO_H, 0),
+      stop_(browser ? browser->profile()->GetThemeProvider() : NULL,
+            IDR_STOP, IDR_STOP_P, IDR_STOP_H, 0),
       widget_(gtk_button_new()) {
   gtk_widget_set_size_request(widget_.get(),
                               gdk_pixbuf_get_width(go_.pixbufs(0)),
