3fa23dd0dbc5805ca38ac16eb20eb8f5787b2644
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-10 17:41:44 +0000

    2009-07-09  Brian Weinstein  <bweinstein@apple.com>
            Reviewed by Tim Hatcher.
    
            https://bugs.webkit.org/show_bug.cgi?id=27141
            
            Updated WebInspectorClient to use Web Inspector Preferences to remember whether or not it should be 
            docked or a free window instead of NSUserDefaults it used to have.
    
            * WebCoreSupport/WebInspectorClient.mm:
            (-[WebInspectorWindowController init]):
            (-[WebInspectorWindowController showWindow:]):
            (-[WebInspectorWindowController attach]):
            (-[WebInspectorWindowController detach]):
    
    2009-07-09  Brian Weinstein  <bweinstein@apple.com>
    
            Reviewed by Tim Hatcher.
    
            Updated WebCore.base.exp to add some needed functions.
    
            * WebCore.base.exp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebInspectorClient.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebInspectorClient.mm
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 
+static const char* const inspectorStartsAttachedName = "inspectorStartsAttached";
+
 @interface WebInspectorWindowController : NSWindowController <NSWindowDelegate> {
 @private
     WebView *_inspectedWebView;
@@ -192,10 +194,6 @@ void WebInspectorClient::updateWindowTitle() const
 
     [preferences release];
 
-    NSNumber *attached = [[NSUserDefaults standardUserDefaults] objectForKey:WebKitInspectorAttachedKey];
-    ASSERT(!attached || [attached isKindOfClass:[NSNumber class]]);
-    _shouldAttach = attached ? [attached boolValue] : YES;
-
     NSString *path = [[NSBundle bundleWithIdentifier:@"com.apple.WebCore"] pathForResource:@"inspector" ofType:@"html" inDirectory:@"inspector"];
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL fileURLWithPath:path]];
     [[_webView mainFrame] loadRequest:request];
@@ -319,6 +317,10 @@ void WebInspectorClient::updateWindowTitle() const
     }
 
     _visible = YES;
+    
+    // If no preference is set - default to an attached window
+    InspectorController::Setting shouldAttach = [_inspectedWebView page]->inspectorController()->setting(inspectorStartsAttachedName);
+    _shouldAttach = (shouldAttach.type() == InspectorController::Setting::BooleanType) ? shouldAttach.booleanValue() : true;
 
     if (_shouldAttach) {
         WebFrameView *frameView = [[_inspectedWebView mainFrame] frameView];
@@ -354,15 +356,13 @@ void WebInspectorClient::updateWindowTitle() const
     if (_attachedToInspectedWebView)
         return;
 
-    _shouldAttach = YES;
+    [_inspectedWebView page]->inspectorController()->setSetting(inspectorStartsAttachedName, InspectorController::Setting(true));
     _movingWindows = YES;
 
     [self close];
     [self showWindow:nil];
 
     _movingWindows = NO;
-
-    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:WebKitInspectorAttachedKey];
 }
 
 - (void)detach
@@ -370,7 +370,7 @@ void WebInspectorClient::updateWindowTitle() const
     if (!_attachedToInspectedWebView)
         return;
 
-    _shouldAttach = NO;
+    [_inspectedWebView page]->inspectorController()->setSetting(inspectorStartsAttachedName, InspectorController::Setting(false));
     _movingWindows = YES;
 
     [self close];
@@ -378,7 +378,6 @@ void WebInspectorClient::updateWindowTitle() const
 
     _movingWindows = NO;
 
-    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:WebKitInspectorAttachedKey];
 }
 
 - (void)setAttachedWindowHeight:(unsigned)height
