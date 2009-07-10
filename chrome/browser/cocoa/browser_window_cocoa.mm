f748d97068f8cb114b192cecfdef3d97332cf5f5
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-10 00:44:42 +0000

    fix mac compile bustage - use a different string conversion function for GetCurrentPageTitle.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20343 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_window_cocoa.mm
+++ chrome/browser/cocoa/browser_window_cocoa.mm
@@ -92,7 +92,7 @@ void BrowserWindowCocoa::SelectedTabToolbarSizeChanged(bool is_animating) {
 
 void BrowserWindowCocoa::UpdateTitleBar() {
   NSString* newTitle =
-      base::SysWideToNSString(browser_->GetCurrentPageTitle());
+      base::SysUTF16ToNSString(browser_->GetCurrentPageTitle());
 
   // Window menu
   [NSApp changeWindowsItem:window_ title:newTitle filename:NO];
