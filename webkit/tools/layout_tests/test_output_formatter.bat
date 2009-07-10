395c3a4f2250d7dda93c49715e75061ed4cfb041
gwilson@google.com <gwilson@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-10 23:49:47 +0000

    New layout test formatting script. This script parses the output of a specific builder (or build), retrieves baselines, compares expected/actual ouput, and emits an HTML file for easy viewing. There's still much more work to do on this, including refactoring to use other utilities in this directory and integration with the rebaselining tool, but this is the first pass.
    BUG=none
    TEST=Run "run_result_formatter.bat -i"
    R=ojan,dglazkov,eroman
    
    Review URL: http://codereview.chromium.org/147099
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20442 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/tools/layout_tests/test_output_formatter.bat
@@ -0,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+@%~dp0\..\..\..\third_party\python_24\python.exe %~dp0\test_output_formatter.py -v %*
---
+++ webkit/tools/layout_tests/test_output_formatter.sh
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+exec_dir=$(dirname $0)
+
+if [ "$OSTYPE" = "cygwin" ]; then
+  system_root=`cygpath "$SYSTEMROOT"`
+  PATH="/usr/bin:$system_root/system32:$system_root:$system_root/system32/WBEM"
+  export PATH
+  PYTHON_PROG="$exec_dir/../../../third_party/python_24/python.exe"
+else
+  PYTHON_PROG=python
+  # When not using the included python, we don't get automatic site.py paths.
+  # Specifically, run_webkit_tests needs the paths in:
+  # third_party/python_24/Lib/site-packages/google.pth
+  PYTHONPATH="${exec_dir}/../../../tools/python:$PYTHONPATH"
+  export PYTHONPATH
+fi
+
+"$PYTHON_PROG" "$exec_dir/test_output_formatter.py" "-v" "$@"
