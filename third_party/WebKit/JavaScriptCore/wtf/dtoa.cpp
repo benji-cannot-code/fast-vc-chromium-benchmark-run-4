d39cc7126b7a14ea956233b8820d0388eb42ca8d
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-10 05:10:11 +0000

    2009-07-09  Maciej Stachowiak  <mjs@apple.com>
            Not reviewed, build fix.
    
            Windows build fix for the last change.
    
            * wtf/dtoa.cpp: Forgot to include Vector.h
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45700 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/dtoa.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/dtoa.cpp
@@ -149,6 +149,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/AlwaysInline.h>
 #include <wtf/Assertions.h>
 #include <wtf/FastMalloc.h>
+#include <wtf/Vector.h>
 #include <wtf/Threading.h>
 
 #include <stdio.h>
