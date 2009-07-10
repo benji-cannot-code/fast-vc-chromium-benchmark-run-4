8c7ad8a5a858a1a1005035c5eba1f9690c275d94
craig.schlenter@chromium.org <craig.schlenter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-10 04:57:12 +0000

    Quick fix for linux shared build.
    Review URL: http://codereview.chromium.org/155233
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20365 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/page_overlays.cc
+++ printing/page_overlays.cc
@@ -138,6 +138,9 @@ std::wstring PageOverlays::ReplaceVariables(const std::wstring& input,
                                             const PrintedDocument& document,
                                             const PrintedPage& page) {
   std::wstring output(input);
+// Prevent references to document.page_count() on Linux until
+// printed_document.cc is included in printing.gyp.
+#if !defined(OS_LINUX)
   for (size_t offset = output.find(L'{', 0);
        offset != std::wstring::npos;
        offset = output.find(L'{', offset)) {
@@ -201,6 +204,7 @@ std::wstring PageOverlays::ReplaceVariables(const std::wstring& input,
       ++offset;
     }
   }
+#endif // !defined(OS_LINUX)
   return output;
 }
 
