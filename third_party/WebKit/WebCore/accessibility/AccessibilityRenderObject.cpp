bf166f3252a36522e37f703edd667f695b66f0f1
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-10 00:41:28 +0000

    Bug 27130 - Need to implement ARIA role="toolbar" https://bugs.webkit.org/show_bug.cgi?id=27130
    git-svn-id: svn://svn.chromium.org/blink/trunk@45684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AccessibilityRenderObject.cpp
+++ third_party/WebKit/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -2205,7 +2205,8 @@ static const ARIARoleMap& createARIARoleMap()
         { "range", SliderRole },
         { "slider", SliderRole },
         { "spinbutton", ProgressIndicatorRole },
-        { "textbox", TextAreaRole }
+        { "textbox", TextAreaRole },
+        { "toolbar", ToolbarRole }
     };
     ARIARoleMap& roleMap = *new ARIARoleMap;
         
