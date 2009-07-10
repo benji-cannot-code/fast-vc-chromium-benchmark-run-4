e2825065c46678dbaca12b6bdcc12c3f251bf380
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-10 22:57:58 +0000

    2009-07-10  Brady Eidson  <beidson@apple.com>
            Reviewed by Antti Koivisto.
    
            https://bugs.webkit.org/show_bug.cgi?id=26496 and <rdar://problem/7046520>
            REGRESSION: XHR stream connection blocks iFrame loading and resource downloading
    
            With this test we ended up going over the maximum-connections-per-host limit that CFNetwork expected.
            When that happened, the first request that was over the limit ended up in a bizarre state where it 
            wasn't fully serviced until after the long running XHR was complete.
    
            Loader and Loader::Host work together to try to not exceed the max-connection limit but non-cache
            resources - such as XHR - could still end up causing this limit to be exceeded.
    
            This fix adds a workaround specifically for XHR while we hash out a more thorough solution that will
            handle this at the resource handle level.
    
            * loader/loader.cpp:
            (WebCore::Loader::nonCacheRequestInFlight):
            (WebCore::Loader::nonCacheRequestComplete):
            (WebCore::Loader::Host::Host):
            (WebCore::Loader::Host::nonCacheRequestInFlight):
            (WebCore::Loader::Host::nonCacheRequestComplete):
            (WebCore::Loader::Host::servePendingRequests): Take nonCacheRequestsInFlight into account.
            * loader/loader.h:
            (WebCore::Loader::Host::processingResource): Take nonCacheRequestsInFlight into account.
    
            * xml/XMLHttpRequest.cpp:
            (WebCore::XMLHttpRequest::XMLHttpRequest):
            (WebCore::XMLHttpRequest::~XMLHttpRequest): 
            (WebCore::XMLHttpRequest::loadRequestAsynchronously): Bump the nonCacheRequestInFlight count.
            (WebCore::XMLHttpRequest::didFail): Decrement that count if the Loader was notified.
            (WebCore::XMLHttpRequest::didFinishLoading): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/loader.h
+++ third_party/WebKit/WebCore/loader/loader.h
@@ -36,6 +36,7 @@ namespace WebCore {
 
     class CachedResource;
     class DocLoader;
+    class KURL;
     class Request;
 
     class Loader : Noncopyable {
@@ -53,6 +54,9 @@ namespace WebCore {
         bool isSuspendingPendingRequests() { return m_isSuspendingPendingRequests; }
         void suspendPendingRequests();
         void resumePendingRequests();
+        
+        void nonCacheRequestInFlight(const KURL&);
+        void nonCacheRequestComplete(const KURL&);
 
     private:
         Priority determinePriority(const CachedResource*) const;
@@ -70,11 +74,13 @@ namespace WebCore {
             
             const AtomicString& name() const { return m_name; }
             void addRequest(Request*, Priority);
+            void nonCacheRequestInFlight();
+            void nonCacheRequestComplete();
             void servePendingRequests(Priority minimumPriority = Low);
             void cancelRequests(DocLoader*);
             bool hasRequests() const;
 
-            bool processingResource() const { return m_numResourcesProcessing != 0; }
+            bool processingResource() const { return m_numResourcesProcessing != 0 || m_nonCachedRequestsInFlight !=0; }
 
         private:
             Host(const AtomicString&, unsigned);
@@ -95,6 +101,7 @@ namespace WebCore {
             const AtomicString m_name;
             const int m_maxRequestsInFlight;
             int m_numResourcesProcessing;
+            int m_nonCachedRequestsInFlight;
         };
         typedef HashMap<AtomicStringImpl*, RefPtr<Host> > HostMap;
         HostMap m_hosts;
--- third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
+++ third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "XMLHttpRequest.h"
 
+#include "Cache.h"
 #include "CString.h"
 #include "CrossOriginAccessControl.h"
 #include "CrossOriginPreflightResultCache.h"
@@ -146,6 +147,7 @@ XMLHttpRequest::XMLHttpRequest(ScriptExecutionContext* context)
     , m_uploadComplete(false)
     , m_sameOriginRequest(true)
     , m_inPreflight(false)
+    , m_didTellLoaderAboutRequest(false)
     , m_receivedLength(0)
     , m_lastSendLineNumber(0)
     , m_exceptionCode(0)
@@ -155,6 +157,10 @@ XMLHttpRequest::XMLHttpRequest(ScriptExecutionContext* context)
 
 XMLHttpRequest::~XMLHttpRequest()
 {
+    if (m_didTellLoaderAboutRequest) {
+            cache()->loader()->nonCacheRequestComplete(m_url);
+            m_didTellLoaderAboutRequest = false;
+    }
     if (m_upload)
         m_upload->disconnectXMLHttpRequest();
 }
@@ -682,6 +688,10 @@ void XMLHttpRequest::loadRequestAsynchronously(ResourceRequest& request)
         // a request is in progress because we need to keep the listeners alive,
         // and they are referenced by the JavaScript wrapper.
         setPendingActivity(this);
+        
+        ASSERT(!m_didTellLoaderAboutRequest);
+        cache()->loader()->nonCacheRequestInFlight(m_url);
+        m_didTellLoaderAboutRequest = true;
     }
 }
 
@@ -962,6 +972,11 @@ String XMLHttpRequest::statusText(ExceptionCode& ec) const
 
 void XMLHttpRequest::didFail(const ResourceError& error)
 {
+    if (m_didTellLoaderAboutRequest) {
+        cache()->loader()->nonCacheRequestComplete(m_url);
+        m_didTellLoaderAboutRequest = false;
+    }
+    
     // If we are already in an error state, for instance we called abort(), bail out early.
     if (m_error)
         return;
@@ -983,6 +998,11 @@ void XMLHttpRequest::didFailRedirectCheck()
 
 void XMLHttpRequest::didFinishLoading(unsigned long identifier)
 {
+    if (m_didTellLoaderAboutRequest) {
+        cache()->loader()->nonCacheRequestComplete(m_url);
+        m_didTellLoaderAboutRequest = false;
+    }
+
     if (m_error)
         return;
 
