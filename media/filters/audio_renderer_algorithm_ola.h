ecff526c79c28d77e6047a2d2a0d18642e13c7ad
kylep@chromium.org <kylep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-10 21:04:44 +0000

    Modify OLA to use window size in seconds instead of bytes. BUG=16011 TEST=none Review URL: http://codereview.chromium.org/157001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20416 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/audio_renderer_algorithm_ola.h
+++ media/filters/audio_renderer_algorithm_ola.h
@@ -49,6 +49,9 @@ class AudioRendererAlgorithmOLA : public AudioRendererAlgorithmBase {
   // Length for crossfade in bytes.
   size_t crossfade_size_;
 
+  // Window size, in bytes (calculated from audio properties).
+  size_t window_size_;
+
   DISALLOW_COPY_AND_ASSIGN(AudioRendererAlgorithmOLA);
 };
 
