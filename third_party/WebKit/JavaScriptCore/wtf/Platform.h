4b65b7fb5733bc172a4a02316cb1e3bbbfdbe20a
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-11 18:44:46 +0000

    2009-07-11  Simon Fraser  <simon.fraser@apple.com>
            Enable support for accelerated compositing and 3d transforms on Leopard.
            <https://bugs.webkit.org/show_bug.cgi?id=20166>
            <rdar://problem/6120614>
    
            Reviewed by Oliver Hunt.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45754 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -636,7 +636,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* Accelerated compositing */
 #if PLATFORM(MAC)
-#if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD)
+#if !defined(BUILDING_ON_TIGER)
 #define WTF_USE_ACCELERATED_COMPOSITING 1
 #endif
 #endif
