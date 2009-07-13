06c06b39d83f1f75542b940854b90676b3b7c4fe
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-13 04:07:52 +0000

    2009-07-12  Nate Chapin  <japhet@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Upstream UndetectableHTMLCollection.idl as HTMLAllCollection.idl.
    
            https://bugs.webkit.org/show_bug.cgi?id=27132
    
            * bindings/scripts/CodeGeneratorV8.pm:
            * bindings/v8/DOMObjectsInclude.h:
            * bindings/v8/DerivedSourcesAllInOne.cpp:
            * bindings/v8/V8DOMWrapper.cpp:
            (WebCore::V8DOMWrapper::getTemplate):
            (WebCore::V8DOMWrapper::instantiateV8Object):
            * bindings/v8/V8Index.cpp:
            * bindings/v8/V8Index.h:
            * dom/HTMLAllCollection.idl: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45785 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/DOMObjectsInclude.h
+++ third_party/WebKit/WebCore/bindings/v8/DOMObjectsInclude.h
@@ -206,7 +206,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
     // A helper class for undetectable document.all
-    class UndetectableHTMLCollection : public HTMLCollection {
+    class HTMLAllCollection : public HTMLCollection {
     };
 
 } // namespace WebCore
