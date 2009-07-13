7ab571ecd2442c21e361a76c29aec9c4744cd6d3
jmalonzo@webkit.org <jmalonzo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-13 09:57:52 +0000

    https://bugs.webkit.org/show_bug.cgi?id=26718 [Gtk] Add support for javascript windows for DRT
            Reviewed by Gustavo Noronha and Xan Lopez.
    
            Unskip tests that now pass due to support for JS windows in Gtk DRT.
    
            * platform/gtk/Skipped:
    
            Add API to support opening and closing of JavaScript windows automatically.
    
            * WebCoreSupport/ChromeClientGtk.cpp:
            (WebKit::ChromeClient::closeWindowSoon):
            * webkit/webkitprivate.h:
            * webkit/webkitwebsettings.cpp:
            (webkit_web_settings_class_init):
            (webkit_web_settings_set_property):
            (webkit_web_settings_get_property):
            (webkit_web_settings_copy):
            * webkit/webkitwebview.cpp:
            (webkit_web_view_real_close_web_view):
            (webkit_web_view_class_init):
            (webkit_web_view_update_settings):
            (webkit_web_view_settings_notify):
            (webkit_web_view_set_group_name):
            * webkit/webkitwebview.h:
    
            Support running of tests that open and close JS windows automatically.
    
            * DumpRenderTree/gtk/DumpRenderTree.cpp:
            (dumpFramesAsText):
            (dumpBackForwardListForAllWebViews):
            (resetWebViewToConsistentStateBeforeTesting):
            (dump):
            (runTest):
            (webViewClose):
            (createWebView):
            (webViewCreate):
            (main):
            * DumpRenderTree/gtk/DumpRenderTreeGtk.h:
            * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
            (LayoutTestController::windowCount):
            (LayoutTestController::setPopupBlockingEnabled):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
@@ -142,6 +142,7 @@ enum {
     MOVE_CURSOR,
     PRINT_REQUESTED,
     PLUGIN_WIDGET,
+    CLOSE_WEB_VIEW,
     LAST_SIGNAL
 };
 
@@ -693,6 +694,11 @@ static gboolean webkit_web_view_real_web_view_ready(WebKitWebView*)
     return FALSE;
 }
 
+static gboolean webkit_web_view_real_close_web_view(WebKitWebView*)
+{
+    return FALSE;
+}
+
 static WebKitNavigationResponse webkit_web_view_real_navigation_requested(WebKitWebView*, WebKitWebFrame*, WebKitNetworkRequest*)
 {
     return WEBKIT_NAVIGATION_RESPONSE_ACCEPT;
@@ -1104,6 +1110,26 @@ static void webkit_web_view_class_init(WebKitWebViewClass* webViewClass)
             G_TYPE_FROM_CLASS(webViewClass),
             (GSignalFlags)G_SIGNAL_RUN_LAST,
             G_STRUCT_OFFSET (WebKitWebViewClass, web_view_ready),
+            g_signal_accumulator_true_handled,
+            NULL,
+            webkit_marshal_BOOLEAN__VOID,
+            G_TYPE_BOOLEAN, 0);
+
+    /**
+     * WebKitWebView::close-web-view:
+     * @web_view: the object on which the signal is emitted
+     * @return: %TRUE to stop handlers from being invoked for the event or
+     * %FALSE to propagate the event furter
+     *
+     * Emitted when closing a WebView is requested. This occurs when a call
+     * is made from JavaScript's window.close function.
+     *
+     * Since 1.1.11
+     */
+    webkit_web_view_signals[CLOSE_WEB_VIEW] = g_signal_new("close-web-view",
+            G_TYPE_FROM_CLASS(webViewClass),
+            (GSignalFlags)G_SIGNAL_RUN_LAST,
+            G_STRUCT_OFFSET (WebKitWebViewClass, close_web_view),
             g_signal_accumulator_true_handled,
             NULL,
             webkit_marshal_BOOLEAN__VOID,
@@ -1720,6 +1746,7 @@ static void webkit_web_view_class_init(WebKitWebViewClass* webViewClass)
      */
     webViewClass->create_web_view = webkit_web_view_real_create_web_view;
     webViewClass->web_view_ready = webkit_web_view_real_web_view_ready;
+    webViewClass->close_web_view = webkit_web_view_real_close_web_view;
     webViewClass->navigation_requested = webkit_web_view_real_navigation_requested;
     webViewClass->window_object_cleared = webkit_web_view_real_window_object_cleared;
     webViewClass->choose_file = webkit_web_view_real_choose_file;
@@ -2050,7 +2077,7 @@ static void webkit_web_view_update_settings(WebKitWebView* webView)
     gboolean autoLoadImages, autoShrinkImages, printBackgrounds,
         enableScripts, enablePlugins, enableDeveloperExtras, resizableTextAreas,
         enablePrivateBrowsing, enableCaretBrowsing, enableHTML5Database, enableHTML5LocalStorage,
-        enableXSSAuditor;
+        enableXSSAuditor, javascriptCanOpenWindows;
 
     g_object_get(webSettings,
                  "default-encoding", &defaultEncoding,
@@ -2073,6 +2100,7 @@ static void webkit_web_view_update_settings(WebKitWebView* webView)
                  "enable-html5-database", &enableHTML5Database,
                  "enable-html5-local-storage", &enableHTML5LocalStorage,
                  "enable-xss-auditor", &enableXSSAuditor,
+                 "javascript-can-open-windows-automatically", &javascriptCanOpenWindows,
                  NULL);
 
     settings->setDefaultTextEncodingName(defaultEncoding);
@@ -2095,6 +2123,7 @@ static void webkit_web_view_update_settings(WebKitWebView* webView)
     settings->setDatabasesEnabled(enableHTML5Database);
     settings->setLocalStorageEnabled(enableHTML5LocalStorage);
     settings->setXSSAuditorEnabled(enableXSSAuditor);
+    settings->setJavaScriptCanOpenWindowsAutomatically(javascriptCanOpenWindows);
 
     g_free(defaultEncoding);
     g_free(cursiveFontFamily);
@@ -2173,6 +2202,8 @@ static void webkit_web_view_settings_notify(WebKitWebSettings* webSettings, GPar
         settings->setLocalStorageEnabled(g_value_get_boolean(&value));
     else if (name == g_intern_string("enable-xss-auditor"))
         settings->setXSSAuditorEnabled(g_value_get_boolean(&value));
+    else if (name == g_intern_string("javascript-can-open-windows-automatically"))
+        settings->setJavaScriptCanOpenWindowsAutomatically(g_value_get_boolean(&value));
     else if (!g_object_class_find_property(G_OBJECT_GET_CLASS(webSettings), name))
         g_warning("Unexpected setting '%s'", name);
     g_value_unset(&value);
@@ -3379,4 +3410,16 @@ void webkit_web_view_move_cursor(WebKitWebView* webView, GtkMovementStep step, g
 
     gboolean handled;
     g_signal_emit(webView, webkit_web_view_signals[MOVE_CURSOR], 0, step, count, &handled);
+}
+
+void webkit_web_view_set_group_name(WebKitWebView* webView, const gchar* groupName)
+{
+    g_return_if_fail(WEBKIT_IS_WEB_VIEW(webView));
+
+    WebKitWebViewPrivate* priv = webView->priv;
+
+    if (!priv->corePage)
+        return;
+
+    priv->corePage->setGroupName(String::fromUTF8(groupName));
 }
--- third_party/WebKit/WebKitTools/DumpRenderTree/gtk/DumpRenderTreeGtk.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/gtk/DumpRenderTreeGtk.h
@@ -33,10 +33,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <webkit/webkitdefines.h>
 #include <JavaScriptCore/JSBase.h>
 
+#include <glib.h>
+
 extern WebKitWebFrame* mainFrame;
 extern WebKitWebFrame* topLoadingFrame;
 extern guint waitToDumpWatchdog;
 extern bool waitForPolicy;
+extern GSList* webViewList;
 
 gchar* JSStringCopyUTF8CString(JSStringRef jsString);
 
