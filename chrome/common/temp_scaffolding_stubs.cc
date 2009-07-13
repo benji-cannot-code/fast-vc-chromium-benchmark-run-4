65f65189c1409bb35517cfcc98d5d108271e2e7a
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-13 15:02:41 +0000

    Added an async automation message AutomationMsg_PrintAsync to asynchronously print a TabContents. This is on the same lines as the existing sync message AutomationMsg_PrintNow
    Review URL: http://codereview.chromium.org/149468
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20484 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.cc
+++ chrome/common/temp_scaffolding_stubs.cc
@@ -99,6 +99,10 @@ void AutomationProvider::PrintNow(int tab_handle,
   NOTIMPLEMENTED();
 }
 
+void AutomationProvider::PrintAsync(int tab_handle) {
+  NOTIMPLEMENTED();
+}
+
 #if defined(OS_MACOSX)
 void AutomationProvider::GetAutocompleteEditText(int autocomplete_edit_handle,
                                                  bool* success,
