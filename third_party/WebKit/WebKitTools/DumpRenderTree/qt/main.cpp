6588d636125a5f265c89bc6e9d2f86881b6f095b
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-13 13:04:55 +0000

    WebKitTools:
    2009-07-13  Simon Hausmann  <simon.hausmann@nokia.com>
    
            Reviewed by Ariya Hidayat.
    
            Always run the Qt DRT with the raster graphicssystem on X11, for
            increased stability in the image based tests, in particular canvas.
    
            * DumpRenderTree/qt/main.cpp:
            (main):
    
    LayoutTests:
    
    2009-07-13  Simon Hausmann  <simon.hausmann@nokia.com>
    
            Reviewed by Ariya Hidayat.
    
            Removed Qt DRT specific result for canvas-getImageData as we pass this
            test with the raster graphicssystem.
    
            * platform/qt/fast/canvas/canvas-getImageData-expected.txt: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45817 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/qt/main.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/qt/main.cpp
@@ -99,6 +99,7 @@ int main(int argc, char* argv[])
 #ifdef Q_WS_X11
     FcInit();
     WebCore::DumpRenderTree::initializeFonts();
+    QApplication::setGraphicsSystem("raster");
 #endif
     QApplication app(argc, argv);
 #ifdef Q_WS_X11
