472e948a577c3becd30d451815fbb09ced958383
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-13 17:06:53 +0000

    Keep the cairo clipping region in sync with the Skia one.
    The PlatformCanvas now tracks this, so we don't need to have the similar code
    in gfx::Canvas. I moved most references of cairo_surface_t to cairo_t since the
    cairo_t has a transform and clip but the surface does not.
    
    Review URL: http://codereview.chromium.org/149409
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20499 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/bitmap_platform_device_linux.h
+++ skia/ext/bitmap_platform_device_linux.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -86,9 +86,11 @@ class BitmapPlatformDevice : public PlatformDevice {
 
   // If someone wants to paint on a Cairo surface version of our
   // buffer, then give them the surface we're already using.
-  virtual cairo_surface_t* beginPlatformPaint() { return surface(); }
+  virtual cairo_t* beginPlatformPaint();
 
-  cairo_surface_t* surface() const;
+  // Loads the given transform and clipping region into the HDC. This is
+  // overridden from SkDevice.
+  virtual void setMatrixClip(const SkMatrix& transform, const SkRegion& region);
 
  private:
   scoped_refptr<BitmapPlatformDeviceData> data_;
--- skia/ext/canvas_paint_linux.h
+++ skia/ext/canvas_paint_linux.h
@@ -22,7 +22,7 @@ class CanvasPaintT : public T {
  public:
   // This constructor assumes the result is opaque.
   explicit CanvasPaintT(GdkEventExpose* event)
-      : surface_(NULL),
+      : context_(NULL),
         window_(event->window),
         rectangle_(event->area),
         composite_alpha_(false) {
@@ -30,7 +30,7 @@ class CanvasPaintT : public T {
   }
 
   CanvasPaintT(GdkEventExpose* event, bool opaque)
-      : surface_(NULL),
+      : context_(NULL),
         window_(event->window),
         rectangle_(event->area),
         composite_alpha_(false) {
@@ -45,7 +45,8 @@ class CanvasPaintT : public T {
       cairo_t* cr = gdk_cairo_create(window_);
       if (composite_alpha_)
         cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
-      cairo_set_source_surface(cr, surface_, rectangle_.x, rectangle_.y);
+      cairo_surface_t* source_surface = cairo_get_target(context_);
+      cairo_set_source_surface(cr, source_surface, rectangle_.x, rectangle_.y);
       cairo_rectangle(cr, rectangle_.x, rectangle_.y,
                       rectangle_.width, rectangle_.height);
       cairo_fill(cr);
@@ -81,10 +82,10 @@ class CanvasPaintT : public T {
     // surface.
     T::translate(-SkIntToScalar(rectangle_.x), -SkIntToScalar(rectangle_.y));
 
-    surface_ = T::getTopPlatformDevice().beginPlatformPaint();
+    context_ = T::getTopPlatformDevice().beginPlatformPaint();
   }
 
-  cairo_surface_t* surface_;
+  cairo_t* context_;
   GdkWindow* window_;
   GdkRectangle rectangle_;
   // See description above setter.
