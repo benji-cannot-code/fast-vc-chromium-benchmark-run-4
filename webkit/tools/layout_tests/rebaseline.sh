a32d34dee6aca77f107a1caba1127e3e0b8b593c
victorw@chromium.org <victorw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-13 17:37:24 +0000

    When using rebaseline tool in cygwin, svn related shell commands fail due to directory format. Fixing the problem by using cygwin python instead of the included python for windows.
    TEST=rebaselining tool
    BUG=none
    
    Review URL: http://codereview.chromium.org/155239
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20502 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/layout_tests/rebaseline.sh
+++ webkit/tools/layout_tests/rebaseline.sh
@@ -3,18 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 exec_dir=$(dirname $0)
 
-if [ "$OSTYPE" = "cygwin" ]; then
-  system_root=`cygpath "$SYSTEMROOT"`
-  PATH="/usr/bin:$system_root/system32:$system_root:$system_root/system32/WBEM"
-  export PATH
-  PYTHON_PROG="$exec_dir/../../../third_party/python_24/python.exe"
-else
-  PYTHON_PROG=python
-  # When not using the included python, we don't get automatic site.py paths.
-  # Specifically, run_webkit_tests needs the paths in:
-  # third_party/python_24/Lib/site-packages/google.pth
-  PYTHONPATH="${exec_dir}/../../../tools/python:$PYTHONPATH"
-  export PYTHONPATH
-fi
+PYTHON_PROG=python
+# When not using the included python, we don't get automatic site.py paths.
+# Specifically, rebaseline.py needs the paths in:
+# third_party/python_24/Lib/site-packages/google.pth
+PYTHONPATH="${exec_dir}/../../../tools/python:$PYTHONPATH"
+export PYTHONPATH
 
 "$PYTHON_PROG" "$exec_dir/rebaseline.py" "$@"
