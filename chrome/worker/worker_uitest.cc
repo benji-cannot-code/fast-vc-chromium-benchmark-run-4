22fd7b8b523d47854b9328ef65e04eea8e6f42db
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-13 23:21:10 +0000

    Platform cleanup in worker_uitest.cc.
    It compiles and links on Linux after this change, but doesn't pass, so I don't enable it.
    
    Review URL: http://codereview.chromium.org/149561
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20559 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/worker/worker_uitest.cc
+++ chrome/worker/worker_uitest.cc
@@ -220,7 +220,7 @@ void WorkerTest::RunLayoutTest(const std::string& test_case_file_name,
   if (is_http_test)
     new_test_url.reset(new GURL(
         std::string("http://localhost:8080/") +
-        UTF16ToUTF8(test_case_dir_.ToWStringHack()) +
+        WideToUTF8(test_case_dir_.ToWStringHack()) +
         "/" +
         test_case_file_name));
   else
@@ -305,7 +305,7 @@ TEST_F(WorkerTest, WorkerFastLayoutTests) {
   worker_test_dir = worker_test_dir.AppendASCII("workers");
   InitializeForLayoutTest(fast_test_dir, worker_test_dir, false);
 
-  for (int i = 0; i < arraysize(kLayoutTestFiles); ++i)
+  for (size_t i = 0; i < arraysize(kLayoutTestFiles); ++i)
     RunLayoutTest(kLayoutTestFiles[i], false);
 }
 
@@ -326,7 +326,7 @@ TEST_F(WorkerTest, WorkerHttpLayoutTests) {
   InitializeForLayoutTest(http_test_dir, worker_test_dir, true);
 
   StartHttpServer(new_http_root_dir_);
-  for (int i = 0; i < arraysize(kLayoutTestFiles); ++i)
+  for (size_t i = 0; i < arraysize(kLayoutTestFiles); ++i)
     RunLayoutTest(kLayoutTestFiles[i], true);
   StopHttpServer();
 }
@@ -351,7 +351,7 @@ TEST_F(WorkerTest, WorkerXhrHttpLayoutTests) {
   InitializeForLayoutTest(http_test_dir, worker_test_dir, true);
 
   StartHttpServer(new_http_root_dir_);
-  for (int i = 0; i < arraysize(kLayoutTestFiles); ++i)
+  for (size_t i = 0; i < arraysize(kLayoutTestFiles); ++i)
     RunLayoutTest(kLayoutTestFiles[i], true);
   StopHttpServer();
 }
