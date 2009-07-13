ce0de88f329f0d69b80b135ff33b8b2413431f6c
piman@google.com <piman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-13 19:35:54 +0000

    Fixing linux regressions from http://codereview.chromium.org/149130
    Review URL: http://codereview.chromium.org/155434
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20512 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/plugin/linux/config.cc
+++ o3d/plugin/linux/config.cc
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iostream>
 #include <fstream>
 
+namespace o3d {
+
 // Checks OS version
 bool CheckOSVersion(NPP npp) {
   return true;
@@ -61,3 +63,5 @@ bool GetUserConfigMetrics() {
   // TODO: Finish this function!
   return true;
 }
+
+}  // namespace o3d
--- o3d/plugin/linux/main_linux.cc
+++ o3d/plugin/linux/main_linux.cc
@@ -571,17 +571,6 @@ static gboolean GtkTimeoutCallback(gpointer user_data) {
   return TRUE;
 }
 
-NPError PlatformNPPGetValue(NPP instance, NPPVariable variable, void *value) {
-  switch (variable) {
-    case NPPVpluginNeedsXEmbed:
-      *static_cast<NPBool *>(value) = g_xembed_support;
-      return NPERR_NO_ERROR;
-    default:
-      return NPERR_INVALID_PARAM;
-  }
-  return NPERR_NO_ERROR;
-}
-
 NPError InitializePlugin() {
   if (!o3d::SetupOutOfMemoryHandler())
     return NPERR_MODULE_LOAD_FAILED_ERROR;
@@ -647,6 +636,17 @@ NPError OSCALL NP_Shutdown(void) {
 }  // namespace o3d / extern "C"
 
 namespace o3d {
+
+NPError PlatformNPPGetValue(NPP instance, NPPVariable variable, void *value) {
+  switch (variable) {
+    case NPPVpluginNeedsXEmbed:
+      *static_cast<NPBool *>(value) = g_xembed_support;
+      return NPERR_NO_ERROR;
+    default:
+      return NPERR_INVALID_PARAM;
+  }
+  return NPERR_NO_ERROR;
+}
 
 void RenderOnDemandCallbackHandler::Run() {
   DrawPlugin(obj_);
