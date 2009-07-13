00defa9f591add21489029b1057880701648b742
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-13 09:31:12 +0000

    2009-07-13  Mads Ager  <ager@chromium.org>
            Reviewed by Adam Barth.
    
            Fix memory leak in the V8 binding layer.
            https://bugs.webkit.org/show_bug.cgi?id=27163
    
            Reinitializing the context is not necessary when clearing the proxy for navigation
            and it will lead us to hold on to an empty context for each frame.
    
            Test for empty context instead of empty global object handle when
            updating the document for a context.
    
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::clearForNavigation):
            (WebCore::V8Proxy::updateDocument):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45797 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8Proxy.cpp
+++ third_party/WebKit/WebCore/bindings/v8/V8Proxy.cpp
@@ -995,10 +995,6 @@ void V8Proxy::clearForNavigation()
         m_context->DetachGlobal();
 
         disposeContextHandles();
-
-        // Reinitialize the context so the global object points to
-        // the new DOM window.
-        initContextIfNeeded();
     }
 }
 
@@ -1042,10 +1038,8 @@ void V8Proxy::updateDocument()
     if (!m_frame->document())
         return;
 
-    if (m_global.IsEmpty()) {
-        ASSERT(m_context.IsEmpty());
+    if (m_context.IsEmpty())
         return;
-    }
 
     // We have a new document and we need to update the cache.
     updateDocumentWrapperCache();
