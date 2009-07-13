d328747dea09d1106667258c94ac535e35ab4b78
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-13 23:42:10 +0000

    2009-07-13  Brent Fulgham  <bfulgham@webkit.org>
            Reviewed by Adam Roben.
    
            Refactor WebFrame::spoolPages for easier sharing with Windows Cairo.
            https://bugs.webkit.org/show_bug.cgi?id=27240.
    
            * WebFrame.cpp:
            (WebFrame::printHeader): New shared method to print the header.
            (WebFrame::printFooter): New shared method to print the footer.
            (WebFrame::spoolPage): New conditionally-compiled method to print
             a page.
            (WebFrame::spoolPages): Revised to call the platform-specific spoolPage
             for each page.
            * WebFrame.h: Add new method prototypes.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45846 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebFrame.h
+++ third_party/WebKit/WebKit/win/WebFrame.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma warning(push, 0)
 #include <WebCore/FrameWin.h>
+#include <WebCore/GraphicsContext.h>
 #include <WebCore/KURL.h>
 #include <WebCore/PlatformString.h>
 #include <WebCore/ResourceHandleClient.h>
@@ -50,6 +51,7 @@ namespace WebCore {
     class DocumentLoader;
     class Element;
     class Frame;
+    class GraphicsContext;
     class HTMLFrameOwnerElement;
     class IntRect;
     class Page;
@@ -59,6 +61,12 @@ namespace WebCore {
 
 typedef const struct OpaqueJSContext* JSContextRef;
 typedef struct OpaqueJSValue* JSObjectRef;
+
+#if PLATFORM(CG)
+typedef struct CGContext PlatformGraphicsContext;
+#elif PLATFORM(CAIRO)
+typedef struct _cairo PlatformGraphicsContext;
+#endif
 
 class WebFrame;
 class WebFramePolicyListener;
@@ -335,6 +343,9 @@ protected:
     void setPrinting(bool printing, float minPageWidth, float maxPageWidth, bool adjustViewSize);
     void headerAndFooterHeights(float*, float*);
     WebCore::IntRect printerMarginRect(HDC);
+    void spoolPage (PlatformGraphicsContext* pctx, WebCore::GraphicsContext* spoolCtx, HDC printDC, IWebUIDelegate*, float headerHeight, float footerHeight, UINT page, UINT pageCount);
+    void drawHeader(PlatformGraphicsContext* pctx, IWebUIDelegate*, const WebCore::IntRect& pageRect, float headerHeight);
+    void drawFooter(PlatformGraphicsContext* pctx, IWebUIDelegate*, const WebCore::IntRect& pageRect, UINT page, UINT pageCount, float headerHeight, float footerHeight);
 
 protected:
     ULONG               m_refCount;
