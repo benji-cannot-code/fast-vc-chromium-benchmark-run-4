be1628ac8290dd31313940cbd02f652d8c45a514
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-14 22:31:32 +0000

    linux: destroy browser-side plugin windows on plugin crash
    Because the plugin windows must be destroyed by the UI thread,
    we store the map of plugin PID -> plugin windows there,
    on the RenderWidgetHostView.
    
    This differs slightly from Windows, where the map is stored
    on the PluginProcessHost, but is otherwise pretty similar.
    
    TEST=crash a windowed plugin; you should see a sad plugin
    icon in the place of the plugin
    
    Review URL: http://codereview.chromium.org/155517
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20683 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_view_host.h
+++ chrome/browser/renderer_host/render_view_host.h
@@ -526,7 +526,7 @@ class RenderViewHost : public RenderWidgetHost,
 
   void OnUserMetricsRecordAction(const std::wstring& action);
   void OnMissingPluginStatus(int status);
-  void OnCrashedPlugin(const FilePath& plugin_path);
+  void OnCrashedPlugin(base::ProcessId pid, const FilePath& plugin_path);
   void OnMessageReceived(IPC::Message* msg) { }
 
   void OnReceivedSavableResourceLinksForCurrentPage(
