da58fd912db6c739247681ac5681fe79e0710708
mhm@chromium.org <mhm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-14 01:51:32 +0000

    Hookup Print HTML page to the DOM UI for Print Preview and Settings
    Depends on initial foundation: 
    http://src.chromium.org/viewvc/chrome?view=rev&revision=19906
    
    And html mockup: 
    http://src.chromium.org/viewvc/chrome?view=rev&revision=19918
    
    BUG=173, 947
    TEST=The user will see the print html page as a html test webpage.
    Review URL: http://codereview.chromium.org/155067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20595 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/print_ui.h
+++ chrome/browser/dom_ui/print_ui.h
@@ -6,14 +6,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_DOM_UI_PRINT_UI_H_
 #define CHROME_BROWSER_DOM_UI_PRINT_UI_H_
 
+#include <string>
+
+#include "chrome/browser/dom_ui/chrome_url_data_manager.h"
 #include "chrome/browser/dom_ui/dom_ui.h"
 
+// The TabContents used for the print page.
 class PrintUI : public DOMUI {
  public:
   explicit PrintUI(TabContents* contents);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(PrintUI);
+};
+
+// To serve dynamic print data off of chrome.
+class PrintUIHTMLSource : public ChromeURLDataManager::DataSource {
+ public:
+  PrintUIHTMLSource();
+
+  // ChromeURLDataManager overrides.
+  virtual void StartDataRequest(const std::string& path, int request_id);
+  virtual std::string GetMimeType(const std::string&) const {
+    return "text/html";
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(PrintUIHTMLSource);
 };
 
 #endif  // CHROME_BROWSER_DOM_UI_PRINT_UI_H_
