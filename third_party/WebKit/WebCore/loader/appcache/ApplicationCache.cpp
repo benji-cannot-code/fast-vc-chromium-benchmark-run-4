d1c41ec6fc57c08d012a3ec945cc3b25ffc45f2c
ap@webkit.org <ap@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-14 00:07:03 +0000

    Reviewed by Darin Adler.
            https://bugs.webkit.org/show_bug.cgi?id=26925
            <rdar://problem/7027850> URL Fragment Breaks Application Cache Loads
    
            Test: http/tests/appcache/main-resource-hash.html
    
            * loader/appcache/ApplicationCache.cpp:
            (WebCore::ApplicationCache::resourceForURL):
            (WebCore::ApplicationCache::resourceForRequest):
            * loader/appcache/ApplicationCacheGroup.cpp:
            (WebCore::ApplicationCacheGroup::cacheForMainRequest):
            (WebCore::ApplicationCacheGroup::fallbackCacheForMainRequest):
            (WebCore::ApplicationCacheGroup::selectCache):
            (WebCore::ApplicationCacheGroup::finishedLoadingMainResource):
            (WebCore::ApplicationCacheGroup::didReceiveResponse):
            (WebCore::ApplicationCacheGroup::didFail):
            (WebCore::ApplicationCacheGroup::addEntry):
            Remove URL fragment at appcache code borders.
    
            * loader/appcache/ApplicationCacheResource.h:
            (WebCore::ApplicationCacheResource::create):
            * loader/appcache/ApplicationCacheStorage.cpp:
            (WebCore::ApplicationCacheStorage::findOrCreateCacheGroup):
            (WebCore::ApplicationCacheStorage::cacheGroupForURL):
            (WebCore::ApplicationCacheStorage::fallbackCacheGroupForURL):
            Assert that there is no URL fragment in URL at key points in appcache code.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/appcache/ApplicationCache.cpp
+++ third_party/WebKit/WebCore/loader/appcache/ApplicationCache.cpp
@@ -107,6 +107,7 @@ unsigned ApplicationCache::removeResource(const String& url)
     
 ApplicationCacheResource* ApplicationCache::resourceForURL(const String& url)
 {
+    ASSERT(!KURL(url).hasRef());
     return m_resources.get(url).get();
 }    
 
@@ -126,8 +127,12 @@ ApplicationCacheResource* ApplicationCache::resourceForRequest(const ResourceReq
     // We only care about HTTP/HTTPS GET requests.
     if (!requestIsHTTPOrHTTPSGet(request))
         return false;
-    
-    return resourceForURL(request.url());
+
+    KURL url(request.url());
+    if (url.hasRef())
+        url.removeRef();
+
+    return resourceForURL(url);
 }
 
 void ApplicationCache::setOnlineWhitelist(const Vector<KURL>& onlineWhitelist)
