ac8f86843a7f183123b9a46bedca31686c0cc320
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-14 00:14:13 +0000

    Add "learn more" link to privacy settings.
    BUG=11507
    
    Review URL: http://codereview.chromium.org/149581
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20584 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/options/advanced_contents_gtk.cc
+++ chrome/browser/gtk/options/advanced_contents_gtk.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "app/l10n_util.h"
 #include "base/basictypes.h"
+#include "chrome/browser/browser_list.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/gtk/gtk_chrome_link_button.h"
 #include "chrome/browser/gtk/options/options_layout_gtk.h"
 #include "chrome/browser/net/dns_global.h"
 #include "chrome/browser/options_page_base.h"
@@ -19,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/google_update_settings.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
+#include "grit/locale_settings.h"
 #include "net/base/cookie_policy.h"
 
 namespace {
@@ -114,6 +117,8 @@ class PrivacySection : public OptionsPageBase {
   virtual void NotifyPrefChanged(const std::wstring* pref_name);
 
   // The callback functions for the options widgets.
+  static void OnLearnMoreLinkClicked(GtkButton *button,
+                                     PrivacySection* privacy_section);
   static void OnEnableLinkDoctorChange(GtkWidget* widget,
                                        PrivacySection* options_window);
   static void OnEnableSuggestChange(GtkWidget* widget,
@@ -164,7 +169,16 @@ PrivacySection::PrivacySection(Profile* profile)
   gtk_box_pack_start(GTK_BOX(page_), section_description_label,
                      FALSE, FALSE, 0);
 
-  // TODO(mattm): Learn more link
+  GtkWidget* learn_more_link = gtk_chrome_link_button_new(
+      l10n_util::GetStringUTF8(IDS_LEARN_MORE).c_str());
+  // Stick it in an hbox so it doesn't expand to the whole width.
+  GtkWidget* learn_more_hbox = gtk_hbox_new(FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(learn_more_hbox), learn_more_link,
+                     FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(page_), learn_more_hbox,
+                     FALSE, FALSE, 0);
+  g_signal_connect(learn_more_link, "clicked",
+                   G_CALLBACK(OnLearnMoreLinkClicked), this);
 
   enable_link_doctor_checkbox_ = CreateCheckButtonWithWrappedLabel(
       IDS_OPTIONS_LINKDOCTOR_PREF);
@@ -239,6 +253,14 @@ PrivacySection::PrivacySection(Profile* profile)
   cookie_behavior_.Init(prefs::kCookieBehavior, profile->GetPrefs(), this);
 
   NotifyPrefChanged(NULL);
+}
+
+// static
+void PrivacySection::OnLearnMoreLinkClicked(GtkButton *button,
+                                            PrivacySection* privacy_section) {
+  BrowserList::GetLastActive()->
+      OpenURL(GURL(l10n_util::GetStringUTF8(IDS_LEARN_MORE_PRIVACY_URL)),
+              GURL(), NEW_WINDOW, PageTransition::LINK);
 }
 
 // static
