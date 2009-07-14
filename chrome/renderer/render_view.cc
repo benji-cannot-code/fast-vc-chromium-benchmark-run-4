862995d8d36005559b66219cfddf8bb8933f52cd
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-14 22:14:01 +0000

    Chrome-side of fixes for content-script messaging.
    This change adds registration of content scripts, parented to a frame's
    context.  When a frame's context goes away, we unregister it and any content
    script contexts for it.
    
    There's a corresponding webkit change that lets us know when a content script
    context is created.  Filed upstream as
    https://bugs.webkit.org/show_bug.cgi?id=27104.
    
    BUG=16228
    TEST=install an extension with a content script that communicates with a parent
    process.  Messages should be sendable both ways.
    
    Review URL: http://codereview.chromium.org/155309
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20677 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -1462,12 +1462,16 @@ void RenderView::DocumentElementAvailable(WebFrame* frame) {
         frame, UserScript::DOCUMENT_START);
 }
 
-void RenderView::DidCreateScriptContext(WebFrame* webframe) {
+void RenderView::DidCreateScriptContextForFrame(WebFrame* webframe) {
   EventBindings::HandleContextCreated(webframe);
 }
 
-void RenderView::DidDestroyScriptContext(WebFrame* webframe) {
+void RenderView::DidDestroyScriptContextForFrame(WebFrame* webframe) {
   EventBindings::HandleContextDestroyed(webframe);
+}
+
+void RenderView::DidCreateIsolatedScriptContext(WebFrame* webframe) {
+  EventBindings::HandleContextCreated(webframe);
 }
 
 WindowOpenDisposition RenderView::DispositionForNavigationAction(
