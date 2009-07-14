4aac64a529ce7eec15d13f643e37361c7d77717f
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-14 21:08:48 +0000

    ipc: pull in more message headers into ipc_logging
    This makes IPC logging show plugin messages on Mac/Linux,
    and worker messages on all platforms.
    
    Review URL: http://codereview.chromium.org/155530
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20659 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/ipc_logging.cc
+++ chrome/common/ipc_logging.cc
@@ -26,15 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This include list should contain all _messages.h header files so that they
 // can get *MsgLog function etc. This makes ipc logs much more informative.
-#include "chrome/common/render_messages.h"
-#include "chrome/test/automation/automation_messages.h"
-
-#if defined(OS_WIN)
-// Pulling this file in Mac/Linux causes a lot of binaries to need to bring in
-// WebKit and all the dependencies, which results in a very large number of
-// linker errors.
 #include "chrome/common/plugin_messages.h"
-#endif
+#include "chrome/common/render_messages.h"
+#include "chrome/common/worker_messages.h"
+#include "chrome/test/automation/automation_messages.h"
 
 #if defined(OS_POSIX)
 #include "base/string_util.h"
