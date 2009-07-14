164a61c9f4a3c0bfb677c82bd59a1b01d3c4911c
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-14 21:23:15 +0000

    Add missing include in browser_browsertest.cc, fixing the build.
    TBR=nsylvain
    
    Review URL: http://codereview.chromium.org/155531
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20664 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_browsertest.cc
+++ chrome/browser/browser_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/app_modal_dialog.h"
 #include "chrome/browser/browser.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/renderer_host/render_process_host.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/common/page_transition_types.h"
 #include "chrome/test/in_process_browser_test.h"
