50fba2e90e6389ab05ce44c4b8b6adf3cf93e1f1
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-15 15:33:37 +0000

    2009-07-15  Simon Hausmann  <simon.hausmann@nokia.com>
            Reviewed by Ariya Hidayat.
    
            Fix the build without media elements.
    
            * rendering/HitTestResult.cpp:
            (WebCore::HitTestResult::absoluteMediaURL): Add #if ENABLE(VIDEO)
            markers around the body of the method.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45912 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/HitTestResult.cpp
+++ third_party/WebKit/WebCore/rendering/HitTestResult.cpp
@@ -273,6 +273,7 @@ KURL HitTestResult::absoluteImageURL() const
 
 KURL HitTestResult::absoluteMediaURL() const
 {
+#if ENABLE(VIDEO)
     if (!(m_innerNonSharedNode && m_innerNonSharedNode->document()))
         return KURL();
 
@@ -287,6 +288,9 @@ KURL HitTestResult::absoluteMediaURL() const
         return KURL();
 
     return m_innerNonSharedNode->document()->completeURL(parseURL(urlString));
+#else
+    return KURL();
+#endif
 }
 
 KURL HitTestResult::absoluteLinkURL() const
