35f777abbb440e26e4794a13d3f5278439812744
pdherbemont@apple.com <pdherbemont@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-15 05:40:07 +0000

    2009-07-14  Pierre d'Herbemont  <pdherbemont@apple.com>
            Reviewed by Oliver Hunt.
    
            HTMLMediaElement::supportsFullscreen() should return false
            https://bugs.webkit.org/show_bug.cgi?id=27284
    
            (Reverting a part of 45875)
    
            HTMLVideoElement::supportsFullscreen() will properly do the
            job, and check if the backend supports fullscreen.
    
            HTMLVideoElement is the only subclass to support fullscreen
            (conditionnaly). HTMLAudioElement fullscreen is not supported
            and is a different kind of fullscreen, if it comes to be wanted.
    
            No test can be done currently given that none of the media
            backends support fullscreen.
    
            * html/HTMLMediaElement.cpp:
            * html/HTMLMediaElement.h:
            (WebCore::HTMLMediaElement::supportsFullscreen):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45894 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLMediaElement.h
+++ third_party/WebKit/WebCore/html/HTMLMediaElement.h
@@ -69,8 +69,9 @@ public:
 
     void rewind(float timeDelta);
     void returnToRealtime();
-    
-    virtual bool supportsFullscreen() const;
+
+    // Eventually overloaded in HTMLVideoElement
+    virtual bool supportsFullscreen() const { return false; };
     virtual bool supportsSave() const;
 
     void scheduleLoad();
