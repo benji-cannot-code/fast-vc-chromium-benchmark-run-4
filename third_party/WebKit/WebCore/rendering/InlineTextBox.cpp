bd89f0c1df0367ee3b22359df625a9d4d573b14f
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-15 23:11:18 +0000

    WebCore:
            Reviewed by Dave Hyatt.
    
            text-shadow is not drawn for text with transparent colour
            https://bugs.webkit.org/show_bug.cgi?id=21374
    
            Test: fast/text/shadow-translucent-fill.html
    
            * rendering/InlineTextBox.cpp:
            (WebCore::paintTextWithShadows): If the text fill color is not opaque,
            paint all shadows separately from the text, by casting them from
            clipped-out opaque text.
    
    LayoutTests:
    
            Reviewed by Dave Hyatt.
    
            text-shadow is not drawn for text with transparent colour
            https://bugs.webkit.org/show_bug.cgi?id=21374
    
            * fast/text/shadow-translucent-fill.html: Added.
            * platform/mac/fast/css/shadow-multiple-expected.checksum:
            * platform/mac/fast/css/shadow-multiple-expected.png:
            * platform/mac/fast/text/shadow-translucent-fill-expected.checksum: Added.
            * platform/mac/fast/text/shadow-translucent-fill-expected.png: Added.
            * platform/mac/fast/text/shadow-translucent-fill-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45956 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/InlineTextBox.cpp
+++ third_party/WebKit/WebCore/rendering/InlineTextBox.cpp
@@ -257,6 +257,11 @@ bool InlineTextBox::nodeAtPoint(const HitTestRequest&, HitTestResult& result, in
 
 static void paintTextWithShadows(GraphicsContext* context, const Font& font, const TextRun& textRun, int startOffset, int endOffset, const IntPoint& textOrigin, int x, int y, int w, int h, ShadowData* shadow, bool stroked)
 {
+    Color fillColor = context->fillColor();
+    bool opaque = fillColor.alpha() == 255;
+    if (!opaque)
+        context->setFillColor(Color::black);
+
     do {
         IntSize extraOffset;
 
@@ -265,7 +270,7 @@ static void paintTextWithShadows(GraphicsContext* context, const Font& font, con
             int shadowBlur = shadow->blur;
             const Color& shadowColor = shadow->color;
 
-            if (shadow->next || stroked) {
+            if (shadow->next || stroked || !opaque) {
                 IntRect shadowRect(x, y, w, h);
                 shadowRect.inflate(shadowBlur);
                 shadowRect.move(shadowOffset);
@@ -276,7 +281,8 @@ static void paintTextWithShadows(GraphicsContext* context, const Font& font, con
                 shadowOffset -= extraOffset;
             }
             context->setShadow(shadowOffset, shadowBlur, shadowColor);
-        }
+        } else if (!opaque)
+            context->setFillColor(fillColor);
 
         if (startOffset <= endOffset)
             context->drawText(font, textRun, textOrigin + extraOffset, startOffset, endOffset);
@@ -290,13 +296,13 @@ static void paintTextWithShadows(GraphicsContext* context, const Font& font, con
         if (!shadow)
             break;
 
-        if (shadow->next || stroked)
+        if (shadow->next || stroked || !opaque)
             context->restore();
         else
             context->clearShadow();
 
         shadow = shadow->next;
-    } while (shadow || stroked);
+    } while (shadow || stroked || !opaque);
 }
 
 void InlineTextBox::paint(RenderObject::PaintInfo& paintInfo, int tx, int ty)
