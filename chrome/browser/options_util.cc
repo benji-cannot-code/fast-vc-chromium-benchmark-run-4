c17e0a02af298cf3344d8b1b867a2f92517bdd5d
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-15 22:09:55 +0000

    Move the ResolveMetricsReportingEnabled logic to OptionsUtil, make gtk options use it.
    Only show the reporting checkbox in Google Chrome builds.
    
    Show the restart required message box when changing the reporting setting.
    
    BUG=11507
    TEST=on Chromium build, option should not show up.  On Google Chrome build in should show up, and enable both crash reporting and metrics settings.
    
    Review URL: http://codereview.chromium.org/149665
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20796 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/options_util.cc
+++ chrome/browser/options_util.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profile.h"
+#include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_service.h"
+#include "chrome/installer/util/google_update_settings.h"
 
 // static
 void OptionsUtil::ResetToDefaults(Profile* profile) {
@@ -63,4 +65,32 @@ void OptionsUtil::ResetToDefaults(Profile* profile) {
   };
   for (size_t i = 0; i < arraysize(kLocalStatePrefs); ++i)
     local_state->ClearPref(kLocalStatePrefs[i]);
+}
+
+// static
+bool OptionsUtil::ResolveMetricsReportingEnabled(bool enabled) {
+  GoogleUpdateSettings::SetCollectStatsConsent(enabled);
+  bool update_pref = GoogleUpdateSettings::GetCollectStatsConsent();
+
+  if (enabled != update_pref) {
+    DLOG(INFO) <<
+        "OptionsUtil: Unable to set crash report status to " <<
+        enabled;
+  }
+
+  // Only change the pref if GoogleUpdateSettings::GetCollectStatsConsent
+  // succeeds.
+  enabled = update_pref;
+
+  MetricsService* metrics = g_browser_process->metrics_service();
+  DCHECK(metrics);
+  if (metrics) {
+    metrics->SetUserPermitsUpload(enabled);
+    if (enabled)
+      metrics->Start();
+    else
+      metrics->Stop();
+  }
+
+  return enabled;
 }
--- chrome/browser/options_util.h
+++ chrome/browser/options_util.h
@@ -15,6 +15,10 @@ class OptionsUtil {
   // Resets all prefs to their default values.
   static void ResetToDefaults(Profile* profile);
 
+  // Try to make the the crash stats consent and the metrics upload
+  // permission match |enabled|, returns the actual enabled setting.
+  static bool ResolveMetricsReportingEnabled(bool enabled);
+
   DISALLOW_IMPLICIT_CONSTRUCTORS(OptionsUtil);
 };
 
