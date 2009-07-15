37eed4aaffed2847ad9151a318dea46665c224af
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-15 00:43:08 +0000

    linux: destroy browser-side plugin windows on plugin crash
    Because the plugin windows must be destroyed by the UI thread,
    we store the map of plugin PID -> plugin windows there,
    on the RenderWidgetHostView.
    
    This differs slightly from Windows, where the map is stored
    on the PluginProcessHost, but is otherwise pretty similar.
    
    TEST=crash a windowed plugin; you should see a sad plugin
    icon in the place of the plugin
    
    Review URL: http://codereview.chromium.org/155517
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20693 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host.h
+++ chrome/browser/renderer_host/render_widget_host.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <queue>
 #include <vector>
 
+#include "base/process.h"
 #include "base/gfx/native_widget_types.h"
 #include "base/gfx/size.h"
 #include "base/scoped_ptr.h"
@@ -395,7 +396,8 @@ class RenderWidgetHost : public IPC::Channel::Listener {
   // having to bring in render_messages.h in a header file.
   void OnMsgImeUpdateStatus(int control, const gfx::Rect& caret_rect);
 #if defined(OS_LINUX)
-  void OnMsgCreatePluginContainer(gfx::PluginWindowHandle *container);
+  void OnMsgCreatePluginContainer(base::ProcessId pid,
+                                  gfx::PluginWindowHandle* container);
   void OnMsgDestroyPluginContainer(gfx::PluginWindowHandle container);
 #elif defined(OS_MACOSX)
   void OnMsgShowPopup(const ViewHostMsg_ShowPopup_Params& params);
