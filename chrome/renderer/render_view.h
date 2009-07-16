b63b5b90e44d4e5814a9dc5c6ab59ee35500ae8f
sverrir@chromium.org <sverrir@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-16 17:23:58 +0000

    Print only the focused frame.  This makes more sense than trying to print all frames since most of the time you end up with ugly clipping and scroll bars on the printed page. This also fixes printing issue with print selection since we don't pick up the currently selected text if's not in the main frame. Also did some refactoring of the printing test in render_view_unittest.  Mainly to reuse the new Image class.
    BUG=http://crbug.com/15250
    TEST=Print pages with frames.  Print selection when using multiple frames (one example in bug).
    
    Review URL: http://codereview.chromium.org/149644
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20876 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.h
+++ chrome/renderer/render_view.h
@@ -391,6 +391,7 @@ class RenderView : public RenderWidget,
   FRIEND_TEST(RenderViewTest, ImeComposition);
   FRIEND_TEST(RenderViewTest, OnSetTextDirection);
   FRIEND_TEST(RenderViewTest, OnPrintPages);
+  FRIEND_TEST(RenderViewTest, PrintWithIframe);
   FRIEND_TEST(RenderViewTest, PrintLayoutTest);
   FRIEND_TEST(RenderViewTest, OnHandleKeyboardEvent);
   FRIEND_TEST(RenderViewTest, InsertCharacters);
