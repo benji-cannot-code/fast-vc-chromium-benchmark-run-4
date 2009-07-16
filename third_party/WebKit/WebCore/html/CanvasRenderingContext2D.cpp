5a17e6e8d743a1e49b8a07b911f8ab904866f144
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-16 15:10:09 +0000

    WebCore:
    2009-07-16  Dean McNamee  <deanm@chromium.org>
    
            Reviewed by Oliver Hunt.
    
            https://bugs.webkit.org/show_bug.cgi?id=27292
            Improve handling of <canvas> path operations on an empty path.
            Implement Skia's Path::hasCurrentPoint().
    
            * html/CanvasRenderingContext2D.cpp:
            (WebCore::CanvasRenderingContext2D::lineTo):
            (WebCore::CanvasRenderingContext2D::quadraticCurveTo):
            (WebCore::CanvasRenderingContext2D::bezierCurveTo):
            * platform/graphics/skia/PathSkia.cpp:
            (WebCore::Path::hasCurrentPoint):
    
    LayoutTests:
    
    2009-07-16  Dean McNamee  <deanm@chromium.org>
    
            Reviewed by Oliver Hunt.
    
            https://bugs.webkit.org/show_bug.cgi?id=27292
            Improve handling of <canvas> path operations on an empty path.
            Implement Skia's Path::hasCurrentPoint().
    
            * fast/canvas/canvas-modify-emptyPath-expected.txt:
            * fast/canvas/resources/canvas-modify-emptyPath.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/WebCore/html/CanvasRenderingContext2D.cpp
@@ -549,7 +549,8 @@ void CanvasRenderingContext2D::lineTo(float x, float y)
         return;
     if (!m_path.hasCurrentPoint())
         m_path.moveTo(FloatPoint(x, y));
-    m_path.addLineTo(FloatPoint(x, y));
+    else
+        m_path.addLineTo(FloatPoint(x, y));
 }
 
 void CanvasRenderingContext2D::quadraticCurveTo(float cpx, float cpy, float x, float y)
@@ -559,8 +560,9 @@ void CanvasRenderingContext2D::quadraticCurveTo(float cpx, float cpy, float x, f
     if (!state().m_invertibleCTM)
         return;
     if (!m_path.hasCurrentPoint())
-        m_path.moveTo(FloatPoint(cpx, cpy));
-    m_path.addQuadCurveTo(FloatPoint(cpx, cpy), FloatPoint(x, y));
+        m_path.moveTo(FloatPoint(x, y));
+    else
+        m_path.addQuadCurveTo(FloatPoint(cpx, cpy), FloatPoint(x, y));
 }
 
 void CanvasRenderingContext2D::bezierCurveTo(float cp1x, float cp1y, float cp2x, float cp2y, float x, float y)
@@ -570,8 +572,9 @@ void CanvasRenderingContext2D::bezierCurveTo(float cp1x, float cp1y, float cp2x,
     if (!state().m_invertibleCTM)
         return;
     if (!m_path.hasCurrentPoint())
-        m_path.moveTo(FloatPoint(cp1x, cp1y));
-    m_path.addBezierCurveTo(FloatPoint(cp1x, cp1y), FloatPoint(cp2x, cp2y), FloatPoint(x, y));
+        m_path.moveTo(FloatPoint(x, y));
+    else
+        m_path.addBezierCurveTo(FloatPoint(cp1x, cp1y), FloatPoint(cp2x, cp2y), FloatPoint(x, y));
 }
 
 void CanvasRenderingContext2D::arcTo(float x0, float y0, float x1, float y1, float r, ExceptionCode& ec)
