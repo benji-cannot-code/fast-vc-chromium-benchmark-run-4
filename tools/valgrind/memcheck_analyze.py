c52462e801a4a0d39d7a72eae49efe34844d6e92
dkegel@google.com <dkegel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-16 15:55:52 +0000

    Patch from timurrrr: Re-factor valgrind scripts to add tsan support First reviewed at http://codereview.chromium.org/125272 BUG=none TEST=none
    Review URL: http://codereview.chromium.org/155528
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20870 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/memcheck_analyze.py
+++ tools/valgrind/memcheck_analyze.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# valgrind_analyze.py
+# memcheck_analyze.py
 
 ''' Given a valgrind XML file, parses errors and uniques them.'''
 
@@ -177,7 +177,7 @@ class ValgrindError:
   def __eq__(self, rhs):
     return self.UniqueString() == rhs
 
-class ValgrindAnalyze:
+class MemcheckAnalyze:
   ''' Given a set of Valgrind XML files, parse all the errors out of them,
   unique them and output the results.'''
 
@@ -193,6 +193,7 @@ class ValgrindAnalyze:
     self._errors = set()
     badfiles = set()
     start = time.time()
+    self._parse_failed = False
     for file in files:
       # Wait up to three minutes for valgrind to finish writing all files,
       # but after that, just skip incomplete files and warn.
@@ -218,6 +219,7 @@ class ValgrindAnalyze:
                 getTextOf(raw_error, "kind") != "Leak_PossiblyLost"):
               self._errors.add(ValgrindError(source_dir, raw_error))
         except ExpatError, e:
+          self._parse_failed = True
           logging.warn("could not parse %s: %s" % (file, e))
           lineno = e.lineno - 1
           context_lines = 5
@@ -238,6 +240,10 @@ class ValgrindAnalyze:
       logging.warn("valgrind didn't finish writing %d files?!" % len(badfiles))
 
   def Report(self):
+    if self._parse_failed:
+      logging.error("FAIL! Couldn't parse Valgrind output file")
+      return -2
+
     if self._errors:
       logging.error("FAIL! There were %s errors: " % len(self._errors))
 
@@ -250,7 +256,7 @@ class ValgrindAnalyze:
     return 0
 
 def _main():
-  '''For testing only. The ValgrindAnalyze class should be imported instead.'''
+  '''For testing only. The MemcheckAnalyze class should be imported instead.'''
   retcode = 0
   parser = optparse.OptionParser("usage: %prog [options] <files to analyze>")
   parser.add_option("", "--source_dir",
@@ -262,7 +268,7 @@ def _main():
     parser.error("no filename specified")
   filenames = args
 
-  analyzer = ValgrindAnalyze(options.source_dir, filenames)
+  analyzer = MemcheckAnalyze(options.source_dir, filenames)
   retcode = analyzer.Report()
 
   sys.exit(retcode)
