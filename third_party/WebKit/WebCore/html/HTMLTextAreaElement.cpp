93677b55c8c4ec324b2b8a15154c28364e30831d
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-16 00:14:36 +0000

    WebCore:
    2009-07-15  David Levin  <levin@chromium.org>
    
            Layout test fix, reverting previous change.
    
            Reverting r45962 as it caused several layout test failures.
    
            * html/HTMLTextAreaElement.cpp:
            (WebCore::HTMLTextAreaElement::parseMappedAttribute):
            * rendering/RenderTextControlMultiLine.cpp:
            (WebCore::RenderTextControlMultiLine::createInnerTextStyle):
    
    LayoutTests:
    
    2009-07-15  David Levin  <levin@chromium.org>
    
            Layout test fix, reverting previous change.
    
            Reverting r45962 as it caused several layout test failures.
    
            * fast/forms/basic-textareas.html:
            * platform/mac/fast/forms/basic-textareas-expected.checksum:
            * platform/mac/fast/forms/basic-textareas-expected.png:
            * platform/mac/fast/forms/basic-textareas-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@45963 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTextAreaElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLTextAreaElement.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLTextAreaElement.h"
 
 #include "ChromeClient.h"
-#include "CSSValueKeywords.h"
 #include "Document.h"
 #include "Event.h"
 #include "EventNames.h"
@@ -174,15 +173,6 @@ void HTMLTextAreaElement::parseMappedAttribute(MappedAttribute* attr)
             wrap = SoftWrap;
         if (wrap != m_wrap) {
             m_wrap = wrap;
-
-            if (shouldWrapText()) {
-                addCSSProperty(attr, CSSPropertyWhiteSpace, CSSValuePreWrap);
-                addCSSProperty(attr, CSSPropertyWordWrap, CSSValueBreakWord);
-            } else {
-                addCSSProperty(attr, CSSPropertyWhiteSpace, CSSValuePre);
-                addCSSProperty(attr, CSSPropertyWordWrap, CSSValueNormal);
-            }
-
             if (renderer())
                 renderer()->setNeedsLayoutAndPrefWidthsRecalc();
         }
