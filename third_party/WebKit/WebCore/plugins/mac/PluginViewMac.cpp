88e208c33acded401dde04bd476494ff40915427
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-17 12:43:05 +0000

    2009-07-17  Tor Arne Vestbø  <tor.arne.vestbo@nokia.com>
            Reviewed by Simon Hausmann.
    
            PluginViewMac: Stop the plugin when loading fails
    
            Also, prevent event propagation when in the stopped state
    
            * plugins/mac/PluginViewMac.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/mac/PluginViewMac.cpp
+++ third_party/WebKit/WebCore/plugins/mac/PluginViewMac.cpp
@@ -147,6 +147,7 @@ void PluginView::init()
 
     if (!start()) {
         m_status = PluginStatusCanNotLoadPlugin;
+        stop(); // Make sure we unregister the plugin
         return;
     }
 
@@ -180,6 +181,7 @@ void PluginView::init()
         m_status = PluginStatusCanNotLoadPlugin;
         LOG(Plugins, "Plug-in '%s' uses unsupported event model %s",
                 m_plugin->name().utf8().data(), prettyNameForEventModel(m_eventModel));
+        stop();
         return;
     }
 
@@ -188,6 +190,7 @@ void PluginView::init()
         m_status = PluginStatusCanNotLoadPlugin;
         LOG(Plugins, "Plug-in '%s' uses unsupported drawing model %s",
                 m_plugin->name().utf8().data(), prettyNameForDrawingModel(m_drawingModel));
+        stop();
         return;
     }
 
@@ -529,6 +532,9 @@ void PluginView::forceRedraw()
 
 void PluginView::handleMouseEvent(MouseEvent* event)
 {
+    if (!m_isStarted)
+        return;
+
     EventRecord record;
 
     if (event->type() == eventNames().mousemoveEvent) {
@@ -572,6 +578,9 @@ void PluginView::handleMouseEvent(MouseEvent* event)
 
 void PluginView::handleKeyboardEvent(KeyboardEvent* event)
 {
+    if (!m_isStarted)
+        return;
+
     LOG(Plugins, "PluginView::handleKeyboardEvent() ----------------- ");
 
     LOG(Plugins, "PV::hKE(): KE.keyCode: 0x%02X, KE.charCode: %d",
