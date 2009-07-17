59d463f82d17460293348bf385564adc8bc6f665
eroman@chromium.org <eroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-17 19:27:39 +0000

    Reviewed by Darin Adler.
    Build fix for non-Darwin.
    Add a guard for inclusion of RetainPtr.h which includes CoreFoundation.h
    
    https://bugs.webkit.org/show_bug.cgi?id=27382
    
    * wtf/unicode/icu/CollatorICU.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46049 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/unicode/icu/CollatorICU.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/unicode/icu/CollatorICU.cpp
@@ -33,12 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(ICU_UNICODE) && !UCONFIG_NO_COLLATION
 
 #include "Assertions.h"
-#include "RetainPtr.h"
 #include "Threading.h"
 #include <unicode/ucol.h>
 #include <string.h>
 
 #if PLATFORM(DARWIN)
+#include "RetainPtr.h"
 #include <CoreFoundation/CoreFoundation.h>
 #endif
 
