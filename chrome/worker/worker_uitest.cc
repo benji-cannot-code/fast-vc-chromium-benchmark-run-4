37007b6e1e5d59a510cd1f5aa48764d6aefe265e
dimich@google.com <dimich@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-17 18:12:56 +0000

    Attempt to re-enable the http worker tests. I can't repro the 'flak' so I'm going to enable them and watch closely. BUG=16934 TEST=none Review URL: http://codereview.chromium.org/149763
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20974 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/worker/worker_uitest.cc
+++ chrome/worker/worker_uitest.cc
@@ -308,7 +308,7 @@ TEST_F(WorkerTest, WorkerFastLayoutTests) {
     RunLayoutTest(kLayoutTestFiles[i], false);
 }
 
-TEST_F(WorkerTest, DISABLED_WorkerHttpLayoutTests) {
+TEST_F(WorkerTest, WorkerHttpLayoutTests) {
   static const char* kLayoutTestFiles[] = {
     "text-encoding.html",
     "worker-importScripts.html",
