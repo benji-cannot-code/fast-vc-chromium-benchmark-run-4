cd3e368fc1245ff613e5ac00d54f185d94b9b8e0
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-17 13:40:01 +0000

    Fix merge error (benign) from r20910
    TBR=mpcomplete
    
    Review URL: http://codereview.chromium.org/149783
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20948 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extension_process_bindings.js
+++ chrome/renderer/resources/extension_process_bindings.js
@@ -16,7 +16,6 @@ var chrome = chrome || {};
   native function StartRequest();
   native function GetCurrentPageActions();
   native function GetViews();
-  native function GetCurrentPageActions();
   native function GetChromeHidden();
   native function GetNextRequestId();
 
