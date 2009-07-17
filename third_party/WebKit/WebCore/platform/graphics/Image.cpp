40e281e2825e267aa749acc0f309f45e49cd8582
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-17 00:47:47 +0000

    WebCore:
            Reviewed by Simon Fraser.
    
            REGRESSION (r41238) Repainted portion of a scaled image does not line up with full image
            https://bugs.webkit.org/show_bug.cgi?id=26747
            rdar://problem/7009243
    
            Test: fast/repaint/background-misaligned.html
    
            * platform/graphics/Image.cpp:
            (WebCore::Image::drawTiled): Moved a variable definition closer to where
            it is used.
            * platform/graphics/cg/ImageCG.cpp:
            (WebCore::BitmapImage::draw): In the subimage code path, compute a
            pixel-aligned source rect, because the subiamge is always pixel-aligned
            in source space, and adjust the destination rect to preserve the
            source -> destination mapping. Clip to the (original) destination rect
            to prevent bleeding out.
    
    LayoutTests:
    
            Reviewed by Simon Fraser.
    
            REGRESSION (r41238) Repainted portion of a scaled image does not line up with full image
            https://bugs.webkit.org/show_bug.cgi?id=26747
            rdar://problem/7009243
    
            * fast/repaint/background-misaligned.html: Added.
            * platform/mac/fast/backgrounds/size/backgroundSize15-expected.checksum:
            * platform/mac/fast/backgrounds/size/backgroundSize15-expected.png:
            * platform/mac/fast/borders/border-image-rotate-transform-expected.checksum:
            * platform/mac/fast/borders/border-image-rotate-transform-expected.png:
            * platform/mac/fast/repaint/background-misaligned-expected.checksum: Added.
            * platform/mac/fast/repaint/background-misaligned-expected.png: Added.
            * platform/mac/fast/repaint/background-misaligned-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46002 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/Image.cpp
+++ third_party/WebKit/WebCore/platform/graphics/Image.cpp
@@ -120,7 +120,6 @@ void Image::drawTiled(GraphicsContext* ctxt, const FloatRect& destRect, const Fl
 
     FloatSize scale(scaledTileSize.width() / intrinsicTileSize.width(),
                     scaledTileSize.height() / intrinsicTileSize.height());
-    TransformationMatrix patternTransform = TransformationMatrix().scaleNonUniform(scale.width(), scale.height());
 
     FloatRect oneTileRect;
     oneTileRect.setX(destRect.x() + fmodf(fmodf(-srcPoint.x(), scaledTileSize.width()) - scaledTileSize.width(), scaledTileSize.width()));
@@ -138,6 +137,7 @@ void Image::drawTiled(GraphicsContext* ctxt, const FloatRect& destRect, const Fl
         return;
     }
 
+    TransformationMatrix patternTransform = TransformationMatrix().scaleNonUniform(scale.width(), scale.height());
     FloatRect tileRect(FloatPoint(), intrinsicTileSize);    
     drawPattern(ctxt, tileRect, patternTransform, oneTileRect.location(), op, destRect);
     
