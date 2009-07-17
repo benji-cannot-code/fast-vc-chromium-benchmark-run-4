8f1fc18808cc4c3d4f9623137d77eacd1947a0e3
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-17 05:29:17 +0000

    2009-07-16  Holger Hans Peter Freyther  <zecke@selfish.org>
            Reviewed by Jan Alonzo.
    
            [GTK+] Update the focus handling code.
    
            Change focus-in to always call setActive(true), call
            FocusController::setFocusedFrame when no focused frame exists
            other SelectionController::setActive. This is matching Windows
            and Qt.
    
            On focus-out call FocusController::setActive(false) and
            SelectionController::setFocued(false). This is matching Windows
            and Qt as well.
    
            * webkit/webkitwebview.cpp:
            (webkit_web_view_focus_in_event):
            (webkit_web_view_focus_out_event):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
@@ -566,7 +566,11 @@ static void webkit_web_view_grab_focus(GtkWidget* widget)
     if (GTK_WIDGET_IS_SENSITIVE(widget)) {
         WebKitWebView* webView = WEBKIT_WEB_VIEW(widget);
         FocusController* focusController = core(webView)->focusController();
-        core(webView)->focusController()->setFocused(true);
+
+        if (Frame* frame = focusController->focusedFrame())
+            focusController->setFocused(true);
+        else
+            focusController->setFocusedFrame(core(webView)->mainFrame());
     }
 
     return GTK_WIDGET_CLASS(webkit_web_view_parent_class)->grab_focus(widget);
@@ -581,8 +585,12 @@ static gboolean webkit_web_view_focus_in_event(GtkWidget* widget, GdkEventFocus*
         WebKitWebView* webView = WEBKIT_WEB_VIEW(widget);
         FocusController* focusController = core(webView)->focusController();
 
-        focusController->setActive(core(webView)->mainFrame());
-        focusController->setFocused(true);
+        focusController->setActive(true);
+
+        if (Frame* frame = focusController->focusedFrame())
+            focusController->setFocused(true);
+        else
+            focusController->setFocusedFrame(core(webView)->mainFrame());
     }
     return GTK_WIDGET_CLASS(webkit_web_view_parent_class)->focus_in_event(widget, event);
 }
