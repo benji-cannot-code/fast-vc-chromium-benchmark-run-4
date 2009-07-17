8f9eb9c015f175eb831a35af38cd6e99f9d77324
gman@google.com <gman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-17 22:25:08 +0000

    Add RawData request in preparation for manual loading of Bitmaps and being able to flip them, scale them, etc...
    Basically this just makes it possible to download a RawData
    directly which you can then pass you'll be able to pass to
    pack->CreateBitmapFromRawData.
    
    Some design comments:
    
    I used SetFromFile instead of making a different constructor
    since it seemed wrong to do file IO in a constructor. Given
    that SetFromFile is private I don't think this is a problem
    since you can't call it directly.
    
    Also, I thought about loading the file first and then calling
     the original constructor but it seemed like a waste to load 
    the file into memory, then copy it to a new buffer when I could 
    just load it directly. 
    
    Finally I made it take a String instead of a FilePath because
    it meant other places had to do less work.
    
    Review URL: http://codereview.chromium.org/149784
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21015 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/import/cross/raw_data.h
+++ o3d/import/cross/raw_data.h
@@ -47,6 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/cross/param.h"
 #include "core/cross/types.h"
 
+class FilePath;
+
 namespace o3d {
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -58,6 +60,10 @@ class RawData : public ParamObject {
                              const String &uri,
                              const void *data,
                              size_t length);
+
+  static RawData::Ref CreateFromFile(ServiceLocator* service_locator,
+                                     const String &uri,
+                                     const String& filename);
 
   virtual ~RawData();
 
@@ -95,6 +101,7 @@ class RawData : public ParamObject {
   mutable scoped_array<uint8> data_;
   size_t length_;
   FilePath temp_filepath_;
+  bool allow_string_value_;
 
   // Deletes temp file if it exists
   void DeleteTempFile();
@@ -103,6 +110,8 @@ class RawData : public ParamObject {
           const String &uri,
           const void *data,
           size_t length);
+
+  bool SetFromFile(const String& filename);
 
   friend class IClassManager;
   friend class Pack;
--- o3d/plugin/cross/async_loading.h
+++ o3d/plugin/cross/async_loading.h
@@ -33,8 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This file declares the glue for FileRequest actions.
 
-#ifndef EXPERIMENTAL_O3D_O3DPLUGIN_AUTOGEN_O3D_GLUE_ASYNC_LOADING_H_
-#define EXPERIMENTAL_O3D_O3DPLUGIN_AUTOGEN_O3D_GLUE_ASYNC_LOADING_H_
+#ifndef O3D_PLUGIN_CROSS_ASYNC_LOADING_H_
+#define O3D_PLUGIN_CROSS_ASYNC_LOADING_H_
 
 #include "core/cross/callback.h"
 #include "core/cross/types.h"
@@ -65,4 +65,4 @@ void userglue_method_send(void *plugin_data,
 }  // namespace namespace_o3d
 }  // namespace glue
 
-#endif  // EXPERIMENTAL_O3D_O3DPLUGIN_AUTOGEN_O3D_GLUE_ASYNC_LOADING_H_
+#endif  // O3D_PLUGIN_CROSS_ASYNC_LOADING_H_
