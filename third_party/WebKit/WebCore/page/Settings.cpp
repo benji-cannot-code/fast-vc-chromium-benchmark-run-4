8d691491c8d67ad1a5febcc417ce56227896fd27
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-17 00:41:56 +0000

    2009-07-16  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Adam Barth.
    
            Add a sessionStorageEnabled setting to the settings class.
            https://bugs.webkit.org/show_bug.cgi?id=27318
    
            Allow LocalStorage to be enabled without enabling SessionStorage at
            runtime.  There is a settings class setting for localStorage, but not
            for sessionStorage.  We want to be able to test one of these features
            without necessarily enabling the other.
    
            SessionStorage defaults to true so as to not change behavior and
            because there really aren't any security concerns around SessionStorage
            (unlike LocalsStorage).  The flag is needed in Chromium only because
            we want to enable the compile time flag in the default build, but don't
            want it on by default until it's been thoroughly tested.
    
            * page/DOMWindow.cpp:
            (WebCore::DOMWindow::sessionStorage):  Check the new flag
            (WebCore::DOMWindow::localStorage):  A bit of cleanup
            * page/Settings.cpp:
            (WebCore::Settings::Settings):  Default the flag to true
            (WebCore::Settings::setSessionStorageEnabled):  Add the new flag
            * page/Settings.h:
            (WebCore::Settings::sessionStorageEnabled):  Get the new flag
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Settings.cpp
+++ third_party/WebKit/WebCore/page/Settings.cpp
@@ -65,6 +65,7 @@ Settings::Settings(Page* page)
     , m_arePluginsEnabled(false)
     , m_databasesEnabled(false)
     , m_localStorageEnabled(false)
+    , m_sessionStorageEnabled(true)
     , m_isJavaScriptEnabled(false)
     , m_isWebSecurityEnabled(true)
     , m_allowUniversalAccessFromFileURLs(true)
@@ -243,6 +244,11 @@ void Settings::setDatabasesEnabled(bool databasesEnabled)
 void Settings::setLocalStorageEnabled(bool localStorageEnabled)
 {
     m_localStorageEnabled = localStorageEnabled;
+}
+
+void Settings::setSessionStorageEnabled(bool sessionStorageEnabled)
+{
+    m_sessionStorageEnabled = sessionStorageEnabled;
 }
 
 void Settings::setPrivateBrowsingEnabled(bool privateBrowsingEnabled)
--- third_party/WebKit/WebCore/page/Settings.h
+++ third_party/WebKit/WebCore/page/Settings.h
@@ -126,6 +126,9 @@ namespace WebCore {
         void setLocalStorageEnabled(bool);
         bool localStorageEnabled() const { return m_localStorageEnabled; }
 
+        void setSessionStorageEnabled(bool);
+        bool sessionStorageEnabled() const { return m_sessionStorageEnabled; }
+
         void setPrivateBrowsingEnabled(bool);
         bool privateBrowsingEnabled() const { return m_privateBrowsingEnabled; }
 
@@ -278,6 +281,7 @@ namespace WebCore {
         bool m_arePluginsEnabled : 1;
         bool m_databasesEnabled : 1;
         bool m_localStorageEnabled : 1;
+        bool m_sessionStorageEnabled : 1;
         bool m_isJavaScriptEnabled : 1;
         bool m_isWebSecurityEnabled : 1;
         bool m_allowUniversalAccessFromFileURLs: 1;
