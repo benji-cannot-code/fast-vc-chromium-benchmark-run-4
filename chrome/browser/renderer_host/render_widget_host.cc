6c85166e9548e93195a2be6614ed59e1063cff0b
cpu@google.com <cpu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-17 17:17:43 +0000

    More bad ipc message handling for RenderWidgetHost - For input handling so far
    BUG=16673
    TEST=unit test included
    
    Review URL: http://codereview.chromium.org/155613
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20962 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host.cc
+++ chrome/browser/renderer_host/render_widget_host.cc
@@ -698,8 +698,8 @@ void RenderWidgetHost::OnMsgInputEventAck(const IPC::Message& message) {
 
   void* iter = NULL;
   int type = 0;
-  bool r = message.ReadInt(&iter, &type);
-  DCHECK(r);
+  if (!message.ReadInt(&iter, &type) || (type < WebInputEvent::Undefined))
+    process()->ReceivedBadMessage(message.type());
 
   if (type == WebInputEvent::MouseMove) {
     mouse_move_pending_ = false;
@@ -721,8 +721,8 @@ void RenderWidgetHost::OnMsgInputEventAck(const IPC::Message& message) {
                  << type << "). Ignoring event.";
     } else {
       bool processed = false;
-      r = message.ReadBool(&iter, &processed);
-      DCHECK(r);
+      if (!message.ReadBool(&iter, &processed))
+        process()->ReceivedBadMessage(message.type());
 
       KeyQueue::value_type front_item = key_queue_.front();
       key_queue_.pop();
@@ -740,7 +740,7 @@ void RenderWidgetHost::OnMsgInputEventAck(const IPC::Message& message) {
 
 void RenderWidgetHost::OnMsgFocus() {
   // Only the user can focus a RenderWidgetHost.
-  NOTREACHED();
+  process()->ReceivedBadMessage(ViewHostMsg_Focus__ID);
 }
 
 void RenderWidgetHost::OnMsgBlur() {
