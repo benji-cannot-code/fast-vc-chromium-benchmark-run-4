a880d0ec5fbac349df27a20ff690c64d10e935e5
ager@chromium.org <ager@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-17 21:14:52 +0000

    Land Anton's change to cast from v8::Value to v8::Object before converting a wrapper to a native Node pointer in devtools.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/159003.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21002 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webdevtoolsclient_impl.cc
+++ webkit/glue/webdevtoolsclient_impl.cc
@@ -266,7 +266,8 @@ v8::Handle<v8::Value> WebDevToolsClientImpl::JsAddSourceToFrame(
   if (source_string.isEmpty() || exception_catcher.HasCaught()) {
     return v8::Undefined();
   }
-  Node* node = V8DOMWrapper::convertDOMWrapperToNode<Node>(args[2]);
+  v8::Handle<v8::Object> wrapper = v8::Handle<v8::Object>::Cast(args[2]);
+  Node* node = V8DOMWrapper::convertDOMWrapperToNode<Node>(wrapper);
   if (!node || !node->attached()) {
     return v8::Undefined();
   }
@@ -285,7 +286,8 @@ v8::Handle<v8::Value> WebDevToolsClientImpl::JsAddResourceSourceToFrame(
   if (mime_type.isEmpty()) {
     return v8::Undefined();
   }
-  Node* node = V8DOMWrapper::convertDOMWrapperToNode<Node>(args[2]);
+  v8::Handle<v8::Object> wrapper = v8::Handle<v8::Object>::Cast(args[2]);
+  Node* node = V8DOMWrapper::convertDOMWrapperToNode<Node>(wrapper);
   WebDevToolsClientImpl* client = static_cast<WebDevToolsClientImpl*>(
       v8::External::Cast(*args.Data())->Value());
   client->AddResourceSourceToFrame(resource_id, mime_type, node);
