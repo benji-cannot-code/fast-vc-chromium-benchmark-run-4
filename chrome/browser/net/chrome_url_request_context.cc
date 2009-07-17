8348d0fd004c5b9d95fcedfac21aad43652c69bc
jshin@chromium.org <jshin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-17 17:44:46 +0000

    Added an observer for 'pref::kDefaultCharset' change I forgot to add in r15113 (http://src.chromium.org/viewvc/chrome?view=rev&revision=15113) . Besides, set referrer_charset_ (used in guessing the C-D charset in GetFileNameFromCD) to pref::kDefaultCharset without any q values appended.
    TEST=1. Set the default charset in Options | Advanced | Fonts & Languages to ISO-8859-1. 
         2. Type http://i18nl10n.com/moztest/random21.yyy in the omnibox and the filename in the download bar should start with "횉횗짹횤21"
         3. Change the default charset to Korean
         4. Try #2 again and the filename in the download bar should start with '?쒓?21'. 
    
    BUG=1148 (http://crbug.com/1148 )
    
    
    
    Review URL: http://codereview.chromium.org/149705
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20965 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_url_request_context.cc
+++ chrome/browser/net/chrome_url_request_context.cc
@@ -272,8 +272,10 @@ ChromeURLRequestContext::ChromeURLRequestContext(Profile* profile)
   // Set up Accept-Language and Accept-Charset header values
   accept_language_ = net::HttpUtil::GenerateAcceptLanguageHeader(
       WideToASCII(prefs_->GetString(prefs::kAcceptLanguages)));
-  accept_charset_ = net::HttpUtil::GenerateAcceptCharsetHeader(
-      WideToASCII(prefs_->GetString(prefs::kDefaultCharset)));
+  std::string default_charset =
+      WideToASCII(prefs_->GetString(prefs::kDefaultCharset));
+  accept_charset_ =
+      net::HttpUtil::GenerateAcceptCharsetHeader(default_charset);
 
   // At this point, we don't know the charset of the referring page
   // where a url request originates from. This is used to get a suggested
@@ -287,7 +289,7 @@ ChromeURLRequestContext::ChromeURLRequestContext(Profile* profile)
   // have an *arguably* better default charset for interpreting a raw 8bit
   // C-D header field.  It means the native OS codepage fallback in
   // net_util::GetSuggestedFilename is unlikely to be taken.
-  referrer_charset_ = accept_charset_;
+  referrer_charset_ = default_charset;
 
   cookie_policy_.SetType(net::CookiePolicy::FromInt(
       prefs_->GetInteger(prefs::kCookieBehavior)));
@@ -310,6 +312,7 @@ ChromeURLRequestContext::ChromeURLRequestContext(Profile* profile)
 
   prefs_->AddPrefObserver(prefs::kAcceptLanguages, this);
   prefs_->AddPrefObserver(prefs::kCookieBehavior, this);
+  prefs_->AddPrefObserver(prefs::kDefaultCharset, this);
 
   if (!is_off_the_record_) {
     registrar_.Add(this, NotificationType::EXTENSIONS_LOADED,
@@ -341,6 +344,13 @@ void ChromeURLRequestContext::Observe(NotificationType type,
           NewRunnableMethod(this,
                             &ChromeURLRequestContext::OnCookiePolicyChange,
                             policy_type));
+    } else if (*pref_name_in == prefs::kDefaultCharset) {
+      std::string default_charset =
+          WideToASCII(prefs->GetString(prefs::kDefaultCharset));
+      g_browser_process->io_thread()->message_loop()->PostTask(FROM_HERE,
+          NewRunnableMethod(this,
+                            &ChromeURLRequestContext::OnDefaultCharsetChange,
+                            default_charset));
     }
   } else if (NotificationType::EXTENSIONS_LOADED == type) {
     ExtensionPaths* new_paths = new ExtensionPaths;
@@ -368,6 +378,7 @@ void ChromeURLRequestContext::CleanupOnUIThread() {
   // Unregister for pref notifications.
   prefs_->RemovePrefObserver(prefs::kAcceptLanguages, this);
   prefs_->RemovePrefObserver(prefs::kCookieBehavior, this);
+  prefs_->RemovePrefObserver(prefs::kDefaultCharset, this);
   prefs_ = NULL;
 
   registrar_.RemoveAll();
@@ -429,6 +440,15 @@ void ChromeURLRequestContext::OnCookiePolicyChange(
   DCHECK(MessageLoop::current() ==
          ChromeThread::GetMessageLoop(ChromeThread::IO));
   cookie_policy_.SetType(type);
+}
+
+void ChromeURLRequestContext::OnDefaultCharsetChange(
+    const std::string& default_charset) {
+  DCHECK(MessageLoop::current() ==
+         ChromeThread::GetMessageLoop(ChromeThread::IO));
+  referrer_charset_ = default_charset;
+  accept_charset_ =
+      net::HttpUtil::GenerateAcceptCharsetHeader(default_charset);
 }
 
 void ChromeURLRequestContext::OnNewExtensions(ExtensionPaths* new_paths) {
