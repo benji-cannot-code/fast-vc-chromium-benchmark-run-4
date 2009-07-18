4e4efa13136ab3c88f8f827033420551c94be9e9
jparent@chromium.org <jparent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-18 00:37:25 +0000

    Spellcheck should not only work if the element is contentEditable, it should also work if the document is in designMode.  WebKit has an helper function to check if something is editable in any way (and properly excludes things like read only text areas), so use that here instead of our home grown check.  Note that this code defaults text inputs to not be spell checkable, which is preserving the current behavior.
    BUG=14870
    TEST=Will commit a WebKit LayoutTest for this after I check this in.
    Review URL: http://codereview.chromium.org/155611
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21034 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/editor_client_impl.cc
+++ webkit/glue/editor_client_impl.cc
@@ -119,6 +119,8 @@ bool EditorClientImpl::isSelectTrailingWhitespaceEnabled() {
 }
 
 bool EditorClientImpl::ShouldSpellcheckByDefault() {
+  // Spellcheck should be enabled for all editable areas (such as textareas,
+  // contentEditable regions, and designMode docs), except text inputs.
   const WebCore::Frame* frame = web_view_->GetFocusedWebCoreFrame();
   if (!frame)
     return false;
@@ -134,12 +136,8 @@ bool EditorClientImpl::ShouldSpellcheckByDefault() {
   const WebCore::RenderObject* renderer = node->renderer();
   if (!renderer)
     return false;
-  // We should also retrieve the contenteditable attribute of this element to
-  // determine if this element needs spell-checking.
-  const WebCore::EUserModify user_modify = renderer->style()->userModify();
-  return (renderer->isTextArea() && editor->canEdit()) ||
-         user_modify == WebCore::READ_WRITE ||
-         user_modify == WebCore::READ_WRITE_PLAINTEXT_ONLY;
+
+  return (!renderer->isTextField() && editor->canEdit());
 }
 
 bool EditorClientImpl::isContinuousSpellCheckingEnabled() {
