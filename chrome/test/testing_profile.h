208b89168bc2e08699e0659fe959ba63bd2ff2d6
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-18 02:19:39 +0000

    Remove file_path.h from testing_profile.h, it's already included in file_util.h.
    This is really a dummy change to kick the buildbot.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21037 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/testing_profile.h
+++ chrome/test/testing_profile.h
@@ -7,9 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_TEST_TESTING_PROFILE_H_
 
 #include "base/base_paths.h"
-#include "base/file_path.h"
-#include "base/path_service.h"
 #include "base/file_util.h"
+#include "base/path_service.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/browser_prefs.h"
 #include "chrome/browser/browser_theme_provider.h"
@@ -58,6 +57,9 @@ class TestingProfile : public Profile {
   void CreateThemeProvider();
 
   virtual FilePath GetPath() {
+    return path_;
+  }
+  virtual FilePath GetCachePath() {
     return path_;
   }
   // Sets whether we're off the record. Default is false.
