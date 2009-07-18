9bff6c81325f2f02c0b4923c50d92835a33bd25e
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-18 17:47:50 +0000

    2009-07-18  Simon Fraser  <simon.fraser@apple.com>
            Fix Tiger DRT build.
    
            * DumpRenderTree/mac/DumpRenderTree.mm:
            (createWebViewAndOffscreenWindow):
            * DumpRenderTree/mac/PixelDumpSupportMac.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46093 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/PixelDumpSupportMac.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/PixelDumpSupportMac.mm
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "LayoutTestController.h"
 #include <CoreGraphics/CGBitmapContext.h>
-#if !defined(BUILDING_ON_TIGER)
+#if defined(BUILDING_ON_TIGER)
 #include <OpenGL/OpenGL.h>
 #include <OpenGL/CGLMacro.h>
 #endif
