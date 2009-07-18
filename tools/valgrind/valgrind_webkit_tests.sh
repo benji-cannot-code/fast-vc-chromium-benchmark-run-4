7fdbe06aafa73086b813c3584fd69b7e2706104f
dank@chromium.org <dank@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-18 03:35:32 +0000

    Script to valgrind layout tests
    Review URL: http://codereview.chromium.org/159047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21043 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/valgrind/valgrind_webkit_tests.sh
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+# Script to run layout tests under valgrind
+# Example:
+#  sh $0 LayoutTests/fast
+# Caveats:
+#  More of an example than a universal script.
+#  Must be run from src directory.
+#  Uses our standard suppressions; edit
+#    tools/valgrind/memcheck/suppressions.txt
+#  to disable any for bugs you're trying to reproduce.
+
+cat > vlayout-wrapper.sh <<"_EOF_"
+#!/bin/sh
+valgrind --suppressions=tools/valgrind/memcheck/suppressions.txt --tool=memcheck --smc-check=all --num-callers=30 --trace-children=yes --leak-check=full --log-file=vlayout-%p.log "$@"
+_EOF_
+chmod +x vlayout-wrapper.sh
+
+rm -f vlayout-*.log
+export BROWSER_WRAPPER=`pwd`/vlayout-wrapper.sh
+export G_SLICE=always-malloc
+export NSS_DISABLE_ARENA_FREE_LIST=1
+sh webkit/tools/layout_tests/run_webkit_tests.sh --run-singly -v --noshow-results --time-out-ms=200000 --nocheck-sys-deps --debug "$@"
+cat vlayout-*.log
