6dd5f67752c805d44c5337ad4b9b76b0566186b6
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-20 20:21:53 +0000

    Add a simple "sidebar"-like panel container that will scroll.
    This doesn't actually hold any panels at the moment, just dummy content.
    
    Review URL: http://codereview.chromium.org/155593
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21092 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/views/panels/panel_scroller_container.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.  Use of this
+// source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
+
+#include "chrome/browser/views/panels/panel_scroller_container.h"
+
+#include "app/gfx/canvas.h"
+
+PanelScrollerContainer::PanelScrollerContainer(PanelScroller* scroller,
+                                               views::View* contents)
+    : views::View(),
+      scroller_(scroller),
+      contents_(contents) {
+  AddChildView(0, contents_);
+  // TODO(brettw) figure out memory management.
+}
+
+PanelScrollerContainer::~PanelScrollerContainer() {
+}
+
+gfx::Size PanelScrollerContainer::GetPreferredSize() {
+  return gfx::Size(100, 500);
+}
+
+void PanelScrollerContainer::Layout() {
+}
+
+void PanelScrollerContainer::Paint(gfx::Canvas* canvas) {
+  canvas->DrawLineInt(0xFF000080, 0, 0, size().width(), size().height());
+}
