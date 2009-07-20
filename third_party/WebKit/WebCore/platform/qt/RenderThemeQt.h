eaef76f48442431114a5852a355b041c382fb2d0
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-20 09:32:27 +0000

    2009-07-20  Simon Hausmann  <simon.hausmann@nokia.com>
            Reviewed by and done with Tor Arne Vestbø.
    
            Fix fast/css/pseudo-required-optional-*.html in the Qt build
            after r46062.
    
            These tests triggered a bug in RenderThemeQt where we did not fall back
            to the unstyled painting of text areas and input fields when they have
            a styled background.
    
            Our re-implementation of isControlStyled incorrectly only checked the
            border for determining whether to style or not. The base-implementation
            performs the same check, but also includes the background. Removing
            our implementation fixes the appearance.
    
            * platform/qt/RenderThemeQt.cpp: Removed isControlStyled reimplementation.
            * platform/qt/RenderThemeQt.h: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46111 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/RenderThemeQt.h
+++ third_party/WebKit/WebCore/platform/qt/RenderThemeQt.h
@@ -59,9 +59,6 @@ public:
 
     virtual void adjustRepaintRect(const RenderObject* o, IntRect& r);
 
-    virtual bool isControlStyled(const RenderStyle*, const BorderData&,
-                                 const FillLayer&, const Color&) const;
-
     // The platform selection color.
     virtual Color platformActiveSelectionBackgroundColor() const;
     virtual Color platformInactiveSelectionBackgroundColor() const;
