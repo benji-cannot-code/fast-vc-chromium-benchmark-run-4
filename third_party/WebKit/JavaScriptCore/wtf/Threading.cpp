84aba3954ef585fbd619250ab5c2b155c5067a0f
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-20 10:04:10 +0000

    2009-07-20  Zoltan Horvath  <hzoltan@inf.u-szeged.hu>
            Reviewed by Oliver Hunt.
    
            Allow custom memory allocation control in NewThreadContext
            https://bugs.webkit.org/show_bug.cgi?id=27338
    
            Inherits NewThreadContext struct from FastAllocBase because it
            has been instantiated by 'new' JavaScriptCore/wtf/Threading.cpp:76.
    
            * wtf/Threading.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46121 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Threading.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/Threading.cpp
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-struct NewThreadContext {
+struct NewThreadContext : FastAllocBase {
     NewThreadContext(ThreadFunction entryPoint, void* data, const char* name)
         : entryPoint(entryPoint)
         , data(data)
