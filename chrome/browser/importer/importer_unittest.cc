b009c762848e9b29d552c34f9436e1be2cd79d4c
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-20 22:35:32 +0000

    Add missing TemplateURL include.
    TBR=brettw
    
    Review URL: http://codereview.chromium.org/159102
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21119 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/importer_unittest.cc
+++ chrome/browser/importer/importer_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/importer/importer.h"
 #include "chrome/browser/password_manager/ie7_password.h"
 #include "chrome/browser/profile.h"
+#include "chrome/browser/search_engines/template_url.h"
 #include "chrome/common/chrome_paths.h"
 #include "webkit/glue/password_form.h"
 
