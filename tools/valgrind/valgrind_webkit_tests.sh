a591789511196390a01fec27a26718a1696c0cc0
dank@chromium.org <dank@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-20 04:08:45 +0000

    Wait for valgrind to finish (the python script already does this, just forgot)
    Review URL: http://codereview.chromium.org/159065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21068 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/valgrind_webkit_tests.sh
+++ tools/valgrind/valgrind_webkit_tests.sh
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 cat > vlayout-wrapper.sh <<"_EOF_"
 #!/bin/sh
-valgrind --suppressions=tools/valgrind/memcheck/suppressions.txt --tool=memcheck --smc-check=all --num-callers=30 --trace-children=yes --leak-check=full --log-file=vlayout-%p.log "$@"
+valgrind --suppressions=tools/valgrind/memcheck/suppressions.txt --tool=memcheck --smc-check=all --num-callers=30 --trace-children=yes --leak-check=full --log-file=vlayout-%p.log --gen-suppressions=all --track-origins=yes "$@"
 _EOF_
 chmod +x vlayout-wrapper.sh
 
@@ -21,4 +21,17 @@ export BROWSER_WRAPPER=`pwd`/vlayout-wrapper.sh
 export G_SLICE=always-malloc
 export NSS_DISABLE_ARENA_FREE_LIST=1
 sh webkit/tools/layout_tests/run_webkit_tests.sh --run-singly -v --noshow-results --time-out-ms=200000 --nocheck-sys-deps --debug "$@"
+
+# Have to wait a bit for the output files to finish
+nfiles=`ls vlayout-*.log | wc -l`
+while true
+do
+  ndone=`grep -l "LEAK SUMMARY" vlayout-*.log | wc -l`
+  if test $nfiles = $ndone
+  then
+    break
+  fi
+  echo "Waiting for valgrind to finish..."
+  sleep 1
+done
 cat vlayout-*.log
