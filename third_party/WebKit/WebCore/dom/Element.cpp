f63844e8e9838b78fe134e3457031a0721219065
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-20 23:59:58 +0000

    Try to fix release builds after r46136
            * dom/Element.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46141 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Element.cpp
+++ third_party/WebKit/WebCore/dom/Element.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLNames.h"
 #include "NamedNodeMap.h"
 #include "NodeList.h"
+#include "NodeRenderStyle.h"
 #include "Page.h"
 #include "RenderView.h"
 #include "TextIterator.h"
