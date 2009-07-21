ed035dded6b02c3f0c24b9e0a77315acddd098cb
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-21 09:53:28 +0000

    2009-07-21  James Hawkins  <jhawkins@google.com>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=27467
            Return an empty path in PlatformContextSkia::currentPathInLocalCoordinates
            if matrix.invert() fails.  This prevents the use of an uninitialized
            value in inverseMatrix.
    
            No new tests added.  Run
            LayoutTests/svg/dynamic-updates/SVGMarkerElement-dom-markerHeight-attr.html
            under valgrind and notice there are no errors.
    
            * platform/graphics/skia/PlatformContextSkia.cpp:
            (PlatformContextSkia::currentPathInLocalCoordinates):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46158 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
+++ third_party/WebKit/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
@@ -489,7 +489,8 @@ SkPath PlatformContextSkia::currentPathInLocalCoordinates() const
     SkPath localPath = m_path;
     const SkMatrix& matrix = m_canvas->getTotalMatrix();
     SkMatrix inverseMatrix;
-    matrix.invert(&inverseMatrix);
+    if (!matrix.invert(&inverseMatrix))
+        return SkPath();
     localPath.transform(inverseMatrix);
     return localPath;
 }
