b664d58c836e7933924ed47753a63e470b410b6e
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-21 19:00:07 +0000

    wx build fix. Don't include winsock2.h on wx, it conflicts with wx's inclusion of winsock.
    git-svn-id: svn://svn.chromium.org/blink/trunk@46183 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/curl/ResourceHandleManager.h
+++ third_party/WebKit/WebCore/platform/network/curl/ResourceHandleManager.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Timer.h"
 #include "ResourceHandleClient.h"
 
-#if defined(WIN32) || defined(_WIN32)
+#if PLATFORM(WIN)
 #include <winsock2.h>
 #include <windows.h>
 #endif
