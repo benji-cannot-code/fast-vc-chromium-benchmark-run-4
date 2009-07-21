0d0ef20740b401a5b13ebef93b51a5a2acb3c74c
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-21 21:11:09 +0000

    Fix the Windows build, and update the comment on top now that wx uses WebCorePrefix.h too.
    git-svn-id: svn://svn.chromium.org/blink/trunk@46195 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCorePrefix.h
+++ third_party/WebKit/WebCore/WebCorePrefix.h
@@ -19,8 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
-/* This prefix file is for use on Mac OS X and Windows only. It should contain only:
- *    1) files to precompile on Mac OS X and Windows for faster builds
+/* This prefix file should contain only:
+ *    1) files to precompile for faster builds
  *    2) in one case at least: OS-X-specific performance bug workarounds
  *    3) the special trick to catch us using new or delete without including "config.h"
  * The project should be able to build without this header, although we rarely test that.
@@ -106,7 +106,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <time.h>
 
-#if defined(__APPLE__)
+#ifndef BUILDING_WX__
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
 #endif
