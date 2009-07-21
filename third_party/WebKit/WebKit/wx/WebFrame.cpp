d0403ca05814bb4dc6c03caaef964e381347942d
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-21 20:52:35 +0000

    wx build fix. Missing header added.
    git-svn-id: svn://svn.chromium.org/blink/trunk@46193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/wx/WebFrame.cpp
+++ third_party/WebKit/WebKit/wx/WebFrame.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLFrameOwnerElement.h"
 #include "markup.h"
 #include "Page.h"
+#include "PlatformString.h"
 #include "RenderTreeAsText.h"
 #include "RenderObject.h"
 #include "RenderView.h"
