9a6d36defc79d4778c485c8104a5d873f9844754
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-21 01:14:18 +0000

    Fix some problems with my panel scroller.
    These problems were caused by me addressing review comment and forgetting to compile before checkin.
    
    Review URL: http://codereview.chromium.org/159114
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21139 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/panels/panel_scroller.cc
+++ chrome/browser/views/panels/panel_scroller.cc
@@ -9,13 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/stl_util-inl.h"
-#include "chrome/browser/views/panels/panel_container.h"
+#include "base/string_util.h"
+#include "chrome/browser/views/panels/panel_scroller_container.h"
 #include "chrome/browser/views/panels/panel_scroller_header.h"
 #include "views/widget/widget_gtk.h"
 
 struct PanelScroller::Panel {
   PanelScrollerHeader* header;
-  PanelContainer* container;
+  PanelScrollerContainer* container;
 };
 
 PanelScroller::PanelScroller()
@@ -32,31 +33,31 @@ PanelScroller::PanelScroller()
   Panel* panel = new Panel;
   panel->header = new PanelScrollerHeader(this);
   panel->header->set_title(ASCIIToUTF16("Email"));
-  panel->container = new PanelContainer(this, new views::View());
+  panel->container = new PanelScrollerContainer(this, new views::View());
   panels_.push_back(panel);
 
   panel = new Panel;
   panel->header = new PanelScrollerHeader(this);
   panel->header->set_title(ASCIIToUTF16("Chat"));
-  panel->container = new PanelContainer(this, new views::View());
+  panel->container = new PanelScrollerContainer(this, new views::View());
   panels_.push_back(panel);
 
   panel = new Panel;
   panel->header = new PanelScrollerHeader(this);
   panel->header->set_title(ASCIIToUTF16("Calendar"));
-  panel->container = new PanelContainer(this, new views::View());
+  panel->container = new PanelScrollerContainer(this, new views::View());
   panels_.push_back(panel);
 
   panel = new Panel;
   panel->header = new PanelScrollerHeader(this);
   panel->header->set_title(ASCIIToUTF16("Recent searches"));
-  panel->container = new PanelContainer(this, new views::View());
+  panel->container = new PanelScrollerContainer(this, new views::View());
   panels_.push_back(panel);
 
   panel = new Panel;
   panel->header = new PanelScrollerHeader(this);
   panel->header->set_title(ASCIIToUTF16("Pony news"));
-  panel->container = new PanelContainer(this, new views::View());
+  panel->container = new PanelScrollerContainer(this, new views::View());
   panels_.push_back(panel);
 
   // Add the containers first since they're on the bottom.
--- chrome/browser/views/panels/panel_scroller.h
+++ chrome/browser/views/panels/panel_scroller.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "views/view.h"
 
-class PanelContainer;
 class PanelScrollerHeader;
 
 class PanelScroller : public views::View, public AnimationDelegate {
