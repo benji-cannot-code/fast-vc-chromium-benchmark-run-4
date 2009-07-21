49a070b63483739baf7b80c9b130526fe626130a
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-21 00:15:50 +0000

    Resurrect toolkit_views build.
    Review URL: http://codereview.chromium.org/155797
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21132 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/dialog_stubs_gtk.cc
+++ chrome/browser/views/dialog_stubs_gtk.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // browser_dialogs.h that are currently unimplemented in GTK-views.
 
 #include "base/logging.h"
+#include "chrome/browser/fonts_languages_window.h"
+#include "chrome/browser/options_window.h"
 #include "chrome/browser/views/browser_dialogs.h"
 
 namespace browser {
@@ -100,3 +102,14 @@ void ShowPageInfo(gfx::NativeView parent,
 }
 
 }  // namespace browser
+
+void ShowOptionsWindow(OptionsPage page,
+                       OptionsGroup highlight_group,
+                       Profile* profile) {
+  NOTIMPLEMENTED();
+}
+void ShowFontsLanguagesWindow(gfx::NativeWindow window,
+                              FontsLanguagesPage page,
+                              Profile* profile) {
+  NOTIMPLEMENTED();
+}
--- chrome/browser/views/page_info_window_view.cc
+++ chrome/browser/views/page_info_window_view.cc
@@ -52,7 +52,7 @@ class PageInfoWindowView : public views::View,
                            public views::ButtonListener,
                            public PageInfoModel::PageInfoModelObserver {
  public:
-  PageInfoWindowView(gfx::NativeView parent,
+  PageInfoWindowView(gfx::NativeWindow parent,
                      Profile* profile,
                      const GURL& url,
                      const NavigationEntry::SSLStatus& ssl,
@@ -60,7 +60,7 @@ class PageInfoWindowView : public views::View,
   virtual ~PageInfoWindowView();
 
   // This is the main initializer that creates the window.
-  virtual void Init(gfx::NativeView parent);
+  virtual void Init(gfx::NativeWindow parent);
 
   // views::View overrides:
   virtual gfx::Size GetPreferredSize();
@@ -154,7 +154,7 @@ int PageInfoWindowView::opened_window_count_ = 0;
 ////////////////////////////////////////////////////////////////////////////////
 // PageInfoWindowViews
 
-PageInfoWindowView::PageInfoWindowView(gfx::NativeView parent,
+PageInfoWindowView::PageInfoWindowView(gfx::NativeWindow parent,
                                        Profile* profile,
                                        const GURL& url,
                                        const NavigationEntry::SSLStatus& ssl,
@@ -170,11 +170,13 @@ PageInfoWindowView::~PageInfoWindowView() {
   opened_window_count_--;
 }
 
-void PageInfoWindowView::Init(gfx::NativeView parent) {
+void PageInfoWindowView::Init(gfx::NativeWindow parent) {
+#if defined(OS_WIN)
   DWORD sys_color = ::GetSysColor(COLOR_3DFACE);
   SkColor color = SkColorSetRGB(GetRValue(sys_color), GetGValue(sys_color),
                                 GetBValue(sys_color));
   set_background(views::Background::CreateSolidBackground(color));
+#endif
 
   LayoutSections();
 
@@ -289,9 +291,13 @@ void PageInfoWindowView::ButtonPressed(views::Button* sender) {
 void PageInfoWindowView::CalculateWindowBounds(gfx::Rect* bounds) {
   const int kDefaultOffset = 15;
 
+#if defined(OS_WIN)
   gfx::Rect monitor_bounds(win_util::GetMonitorBoundsForRect(*bounds));
   if (monitor_bounds.IsEmpty())
     return;
+#else
+  gfx::Rect monitor_bounds(0, 0, 1024, 768);
+#endif
 
   // If necessary, move the window so it is visible on the screen.
   gfx::Rect adjusted_bounds = bounds->AdjustToFit(monitor_bounds);
@@ -373,8 +379,12 @@ Section::Section(const std::wstring& title,
   title_label_->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
   AddChildView(title_label_);
 
+#if defined(OS_WIN)
   separator_ = new views::Separator();
   AddChildView(separator_);
+#else
+  NOTIMPLEMENTED();
+#endif
 
   status_image_ = new views::ImageView();
   status_image_->SetImage(state ? good_state_icon_ : bad_state_icon_);
@@ -427,9 +437,13 @@ void Section::Layout() {
   gfx::Size size = title_label_->GetPreferredSize();
   title_label_->SetBounds(x, y, size.width(), size.height());
   x += size.width() + kHGapTitleToSeparator;
+#if defined(OS_WIN)
   separator_->SetBounds(x + kHExtraSeparatorPadding, y,
                         width() - x - 2 * kHExtraSeparatorPadding,
                         size.height());
+#else
+  NOTIMPLEMENTED();
+#endif
 
   // Then the image, head-line and description.
   x = kHGapToBorder;
@@ -455,7 +469,7 @@ void Section::Layout() {
 
 namespace browser {
 
-void ShowPageInfo(gfx::NativeView parent,
+void ShowPageInfo(gfx::NativeWindow parent,
                   Profile* profile,
                   const GURL& url,
                   const NavigationEntry::SSLStatus& ssl,
