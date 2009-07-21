8e2c31a86e1a30df753fd555b2c78cb4532c2a8d
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-21 00:34:36 +0000

    Fix GTK/Mac builds.
    Review URL: http://codereview.chromium.org/155826
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21136 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/page_info_window_mac.mm
+++ chrome/browser/cocoa/page_info_window_mac.mm
@@ -19,8 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/cert_status_flags.h"
 #include "net/base/x509_certificate.h"
 
-void PageInfoWindowMac::ShowPageInfo(gfx::NativeView parent,
-                                     Profile* profile,
+void PageInfoWindowMac::ShowPageInfo(Profile* profile,
                                      const GURL& url,
                                      const NavigationEntry::SSLStatus& ssl,
                                      bool show_history) {
