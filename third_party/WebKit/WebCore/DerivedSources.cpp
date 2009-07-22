f8f5c6769338aefd321e2fc02cbc2d6017e35787
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-22 05:19:21 +0000

    2009-07-21  Sam Weinig  <sam@webkit.org>
            Attempt to fix the Windows build.
    
            * DerivedSources.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46214 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/DerivedSources.cpp
+++ third_party/WebKit/WebCore/DerivedSources.cpp
@@ -173,6 +173,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSRange.cpp"
 #include "JSRangeException.cpp"
 #include "JSRect.cpp"
+#include "JSRGBColor.cpp"
 #include "JSScreen.cpp"
 #include "JSSharedWorker.cpp"
 #include "JSSQLError.cpp"
