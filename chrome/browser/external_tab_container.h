9a70707c29fe7e94b93b5f1dc18d220def7df215
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-22 18:07:51 +0000

    The ExternalTabContainer object which implements the TabContentsDelegate interface now implements the ShowHtmlDialog function which is needed to support HTML dialog popups like those displayed by Gears.'
    Fixes http://b/issue?id=1963582
    
    Bug=1963582
    
    Review URL: http://codereview.chromium.org/159162
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21289 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/external_tab_container.h
+++ chrome/browser/external_tab_container.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "chrome/browser/automation/automation_resource_message_filter.h"
+#include "chrome/browser/browser.h"
 #include "chrome/browser/tab_contents/tab_contents_delegate.h"
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_registrar.h"
@@ -104,6 +105,14 @@ class ExternalTabContainer : public TabContentsDelegate,
   // parameter.
   virtual bool ExecuteContextMenuCommand(int command);
 
+  // Show a dialog with HTML content. |delegate| contains a pointer to the
+  // delegate who knows how to display the dialog (which file URL and JSON
+  // string input to use during initialization). |parent_window| is the window
+  // that should be parent of the dialog, or NULL for the default.
+  virtual void ShowHtmlDialog(HtmlDialogUIDelegate* delegate,
+                              gfx::NativeWindow parent_window);
+
+
  protected:
   // Overridden from views::WidgetWin:
   virtual void OnDestroy();
@@ -145,6 +154,9 @@ class ExternalTabContainer : public TabContentsDelegate,
 
   // If all the url requests for this tab are to be loaded via automation.
   bool load_requests_via_automation_;
+
+  // Scoped browser object for this ExternalTabContainer instance.
+  scoped_ptr<Browser> browser_;
 
   DISALLOW_COPY_AND_ASSIGN(ExternalTabContainer);
 };
