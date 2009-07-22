9fc778735802f3a2aeb96f02878d28fcb51b4b81
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-22 00:43:21 +0000

    2009-07-21  Jian Li  <jianli@chromium.org>
            Reviewed by David Levin.
    
            Implement AbstractWorker::dispatchScriptErrorEvent by generating an ErrorEvent.
            https://bugs.webkit.org/show_bug.cgi?id=27515
    
            * workers/AbstractWorker.cpp:
            (WebCore::AbstractWorker::dispatchScriptErrorEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46203 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/workers/AbstractWorker.cpp
+++ third_party/WebKit/WebCore/workers/AbstractWorker.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AbstractWorker.h"
 
+#include "ErrorEvent.h"
 #include "Event.h"
 #include "EventException.h"
 #include "EventNames.h"
@@ -115,10 +116,18 @@ void AbstractWorker::dispatchLoadErrorEvent()
     ASSERT(!ec);
 }
 
-void AbstractWorker::dispatchScriptErrorEvent(const String&, const String&, int)
+void AbstractWorker::dispatchScriptErrorEvent(const String& message, const String& sourceURL, int lineNumber)
 {
-    //FIXME: Generate an ErrorEvent instead of a simple event
-    dispatchLoadErrorEvent();
+    RefPtr<ErrorEvent> event = ErrorEvent::create(message, sourceURL, static_cast<unsigned>(lineNumber));
+    if (m_onErrorListener) {
+        event->setTarget(this);
+        event->setCurrentTarget(this);
+        m_onErrorListener->handleEvent(event.get(), true);
+    }
+
+    ExceptionCode ec = 0;
+    dispatchEvent(event.release(), ec);
+    ASSERT(!ec);
 }
 
 } // namespace WebCore
