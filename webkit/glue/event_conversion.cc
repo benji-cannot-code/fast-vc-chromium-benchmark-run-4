50d421cc41c84e9d28282a5892363abc49f53175
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-22 17:10:38 +0000

    Remove some bogus includes.
    BUG=none
    TEST=none
    R=jorlow
    
    Review URL: http://codereview.chromium.org/155898
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21282 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/event_conversion.cc
+++ webkit/glue/event_conversion.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/api/public/WebInputEvent.h"
 #include "webkit/api/public/WebKit.h"
 #include "webkit/glue/glue_util.h"
-#include "webkit/glue/webkit_glue.h"
 
 using namespace WebCore;
 
