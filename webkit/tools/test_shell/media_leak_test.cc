8f3e412c2abae1edad400efd043e1c18e81a4ad4
hclam@google.com <hclam@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-22 21:08:47 +0000

    Fix broken build because of r21317
    TBR=scherkus, nsylvain
    
    Review URL: http://codereview.chromium.org/159226
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21319 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/media_leak_test.cc
+++ webkit/tools/test_shell/media_leak_test.cc
@@ -11,17 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/tools/test_shell/test_shell_test.h"
 
 class MediaLeakTest : public TestShellTest {
- public:
-  void RunTest(const char* file) {
-    FilePath media_file;
-    ASSERT_TRUE(PathService::Get(base::DIR_SOURCE_ROOT, &media_file));
-    media_file = media_file.Append(FILE_PATH_LITERAL("webkit"))
-                           .Append(FILE_PATH_LITERAL("data"))
-                           .Append(FILE_PATH_LITERAL("media"))
-                           .Append(FILE_PATH_LITERAL(file));
-    test_shell_->LoadURL(media_file.ToWStringHack().c_str());
-    test_shell_->WaitTestFinished();
-  }
 };
 
 #if defined(OS_WIN) || defined(OS_LINUX)
@@ -29,13 +18,27 @@ class MediaLeakTest : public TestShellTest {
 // This test plays a Theora video file for 1 second. It tries to expose
 // memory leaks during a normal playback.
 TEST_F(MediaLeakTest, VideoBear) {
-  RunTest("bear.html");
+  FilePath media_file;
+  ASSERT_TRUE(PathService::Get(base::DIR_SOURCE_ROOT, &media_file));
+  media_file = media_file.Append(FILE_PATH_LITERAL("webkit"))
+                         .Append(FILE_PATH_LITERAL("data"))
+                         .Append(FILE_PATH_LITERAL("media"))
+                         .Append(FILE_PATH_LITERAL("bear.html"));
+  test_shell_->LoadURL(media_file.ToWStringHack().c_str());
+  test_shell_->WaitTestFinished();
 }
 
 // This test loads a Theora video file and unloads it many times. It tries
 // to expose memory leaks in the glue layer with WebKit.
 TEST_F(MediaLeakTest, ManyVideoBear) {
-  RunTest("manybear.html");
+  FilePath media_file;
+  ASSERT_TRUE(PathService::Get(base::DIR_SOURCE_ROOT, &media_file));
+  media_file = media_file.Append(FILE_PATH_LITERAL("webkit"))
+                         .Append(FILE_PATH_LITERAL("data"))
+                         .Append(FILE_PATH_LITERAL("media"))
+                         .Append(FILE_PATH_LITERAL("manybear.html"));
+  test_shell_->LoadURL(media_file.ToWStringHack().c_str());
+  test_shell_->WaitTestFinished();
 }
 
 #endif
