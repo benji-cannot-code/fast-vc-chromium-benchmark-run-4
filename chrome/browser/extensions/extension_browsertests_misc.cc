e5802fdf70b7d6fd4b119e5fff148a2075f6fd33
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-22 21:32:24 +0000

    Fix an issue with unregistering content script contexts.
    Reenable content script messaging test.
    
    BUG=no
    TEST=no
    
    Review URL: http://codereview.chromium.org/155887
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21324 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_browsertests_misc.cc
+++ chrome/browser/extensions/extension_browsertests_misc.cc
@@ -322,8 +322,6 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, MessagingExtensionTab) {
 }
 
 // Tests that message passing between extensions and content scripts works.
-#if 0
-// TODO(mpcomplete): re-enable this
 IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, MessagingContentScript) {
   ASSERT_TRUE(LoadExtension(
       test_data_dir_.AppendASCII("good").AppendASCII("Extensions")
@@ -376,4 +374,3 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, MessagingContentScript) {
       host->render_view_host(), L"", L"testDisconnectOnClose()", &result);
   EXPECT_TRUE(result);
 }
-#endif
