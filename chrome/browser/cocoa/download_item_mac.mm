b9cc4b16ac30cc1fa997ae31b9580d7b1363f743
paul@chromium.org <paul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-22 19:20:45 +0000

    Load Mac download shelf icons asynchronously.
    BUG=16226 (http://crbug.com/16226)
    TEST=none.
    
    Review URL: http://codereview.chromium.org/155831
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21305 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/download_item_mac.mm
+++ chrome/browser/cocoa/download_item_mac.mm
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/cocoa/download_item_mac.h"
 
+#include "chrome/browser/browser_process.h"
 #import "chrome/browser/cocoa/download_item_controller.h"
 #include "chrome/browser/download/download_item_model.h"
+#include "skia/ext/skia_utils_mac.h"
 
 // DownloadItemMac -------------------------------------------------------------
 
@@ -18,6 +20,7 @@ DownloadItemMac::DownloadItemMac(BaseDownloadItemModel* download_model,
 
 DownloadItemMac::~DownloadItemMac() {
   download_model_->download()->RemoveObserver(this);
+  icon_consumer_.CancelAllRequests();
 }
 
 void DownloadItemMac::OnDownloadUpdated(DownloadItem* download) {
@@ -35,4 +38,35 @@ void DownloadItemMac::OnDownloadUpdated(DownloadItem* download) {
     default:
         NOTREACHED();
   }
+}
+
+void DownloadItemMac::LoadIcon() {
+  IconManager* icon_manager = g_browser_process->icon_manager();
+  if (!icon_manager) {
+    NOTREACHED();
+    return;
+  }
+
+  // We may already have this particular image cached.
+  FilePath file = download_model_->download()->full_path();
+  SkBitmap* icon_bitmap = icon_manager->LookupIcon(file, IconLoader::SMALL);
+  if (icon_bitmap) {
+    NSImage* icon = gfx::SkBitmapToNSImage(*icon_bitmap);
+    [item_controller_ setIcon:icon];
+    return;
+  }
+
+  // The icon isn't cached, load it asynchronously.
+  icon_manager->LoadIcon(file, IconLoader::NORMAL, &icon_consumer_,
+                         NewCallback(this,
+                                     &DownloadItemMac::OnExtractIconComplete));
+}
+
+void DownloadItemMac::OnExtractIconComplete(IconManager::Handle handle,
+                                            SkBitmap* icon_bitmap) {
+  if (!icon_bitmap)
+    return;
+
+  NSImage* icon = gfx::SkBitmapToNSImage(*icon_bitmap);
+  [item_controller_ setIcon:icon];
 }
