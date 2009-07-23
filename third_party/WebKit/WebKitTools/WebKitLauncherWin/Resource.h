2da729fb90b6fea0879bd96e62151a76122e3dac
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-23 05:39:55 +0000

    Checkpoint new Windows nightly launcher.
            Reviewed by Mark Rowe.
    
            * WebKitLauncherWin: Added.
            * WebKitLauncherWin/Resource.h: Added.
            * WebKitLauncherWin/WebKitLauncherWin.cpp: Added.
            (getStringValue): Retrieve a string registry value.
            (applePathFromRegistry): Get an Apple-related path out of the registry.
            (copyEnvironmentVariable): Copy an environment variable.
            (safariInstallDir): Helper function to get the install directory for Safari.
            (safariBrowserExe): Helper function to get the full path of the Safari executable.
            (_tWinMain): Locate Safari and launch it after setting up an environment variable.
            * WebKitLauncherWin/WebKitLauncherWin.h: Added.
            * WebKitLauncherWin/WebKitLauncherWin.rc: Added.
            * WebKitLauncherWin/WebKitLauncherWin.vcproj: Added.
            * WebKitLauncherWin/webkit.ico: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46254 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKitTools/WebKitLauncherWin/Resource.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WebKitLauncherWin.rc
+//
+
+#define IDI_WEBKITLAUNCHERWIN       107
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC                 130
+#define _APS_NEXT_RESOURCE_VALUE    129
+#define _APS_NEXT_COMMAND_VALUE     32771
+#define _APS_NEXT_CONTROL_VALUE     1000
+#define _APS_NEXT_SYMED_VALUE       110
+#endif
+#endif
