11aafca594cc906ea3e078af97f435f9358ac4f5
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-23 18:55:48 +0000

    2009-07-23  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Eric Seidel.
    
            copyInheritableProperties and removeComputedInheritablePropertiesFrom should be deprecated
            https://bugs.webkit.org/show_bug.cgi?id=27325
    
            This patch deprecates copyInheritableProperties because it has been used for two different purposes:
            1.  Calculating the typing style.
            2.  Moving HTML subtrees and seeking to remove redundant styles.
            These tasks should be broken out into two separate functions.  New code should not use this function.
    
            It deletes removeComputedInheritablePropertiesFrom because it hasn't been used anywhere.
    
            There is no test since the patch does not change any behavior.
    
            * css/CSSComputedStyleDeclaration.cpp: removeComputedInheritablePropertiesFrom has been removed
            (WebCore::CSSComputedStyleDeclaration::deprecatedCopyInheritableProperties): has been renamed from copyInheritableProperties
            * css/CSSComputedStyleDeclaration.h: ditto
            * editing/DeleteSelectionCommand.cpp: ditto
            (WebCore::removeEnclosingAnchorStyle): ditto
            (WebCore::DeleteSelectionCommand::saveTypingStyleState): ditto
            * editing/EditCommand.cpp: ditto
            (WebCore::EditCommand::styleAtPosition): ditto
            * editing/RemoveFormatCommand.cpp: ditto
            (WebCore::RemoveFormatCommand::doApply): ditto
            * editing/ReplaceSelectionCommand.cpp: ditto
            (WebCore::handleStyleSpansBeforeInsertion): ditto
            (WebCore::ReplaceSelectionCommand::handleStyleSpans): ditto
            * editing/markup.cpp: ditto
            (WebCore::removeEnclosingMailBlockquoteStyle): ditto
            (WebCore::removeDefaultStyles): ditto
            (WebCore::createMarkup): ditto
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46280 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/ReplaceSelectionCommand.cpp
+++ third_party/WebKit/WebCore/editing/ReplaceSelectionCommand.cpp
@@ -551,7 +551,7 @@ static bool handleStyleSpansBeforeInsertion(ReplacementFragment& fragment, const
     Node* sourceDocumentStyleSpan = topNode;
     RefPtr<Node> copiedRangeStyleSpan = sourceDocumentStyleSpan->firstChild();
     
-    RefPtr<CSSMutableStyleDeclaration> styleAtInsertionPos = rangeCompliantEquivalent(insertionPos).computedStyle()->copyInheritableProperties();
+    RefPtr<CSSMutableStyleDeclaration> styleAtInsertionPos = rangeCompliantEquivalent(insertionPos).computedStyle()->deprecatedCopyInheritableProperties();
     String styleText = styleAtInsertionPos->cssText();
     
     if (styleText == static_cast<Element*>(sourceDocumentStyleSpan)->getAttribute(styleAttr)) {
@@ -607,8 +607,8 @@ void ReplaceSelectionCommand::handleStyleSpans()
     // styles from blockquoteNode are allowed to override those from the source document, see <rdar://problem/4930986> and <rdar://problem/5089327>.
     Node* blockquoteNode = isMailPasteAsQuotationNode(context) ? context : nearestMailBlockquote(context);
     if (blockquoteNode) {
-        RefPtr<CSSMutableStyleDeclaration> blockquoteStyle = computedStyle(blockquoteNode)->copyInheritableProperties();
-        RefPtr<CSSMutableStyleDeclaration> parentStyle = computedStyle(blockquoteNode->parentNode())->copyInheritableProperties();
+        RefPtr<CSSMutableStyleDeclaration> blockquoteStyle = computedStyle(blockquoteNode)->deprecatedCopyInheritableProperties();
+        RefPtr<CSSMutableStyleDeclaration> parentStyle = computedStyle(blockquoteNode->parentNode())->deprecatedCopyInheritableProperties();
         parentStyle->diff(blockquoteStyle.get());
 
         CSSMutableStyleDeclaration::const_iterator end = blockquoteStyle->end();
@@ -620,7 +620,7 @@ void ReplaceSelectionCommand::handleStyleSpans()
         context = blockquoteNode->parentNode();
     }
     
-    RefPtr<CSSMutableStyleDeclaration> contextStyle = computedStyle(context)->copyInheritableProperties();
+    RefPtr<CSSMutableStyleDeclaration> contextStyle = computedStyle(context)->deprecatedCopyInheritableProperties();
     contextStyle->diff(sourceDocumentStyle.get());
     
     // Remove block properties in the span's style. This prevents properties that probably have no effect 
@@ -656,7 +656,7 @@ void ReplaceSelectionCommand::handleStyleSpans()
     
     // Remove redundant styles.
     context = copiedRangeStyleSpan->parentNode();
-    contextStyle = computedStyle(context)->copyInheritableProperties();
+    contextStyle = computedStyle(context)->deprecatedCopyInheritableProperties();
     contextStyle->diff(copiedRangeStyle.get());
     
     // See the comments above about removing block properties.
