6adfacfae1c2a4399b1a35f63305ad703cd01a3c
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-23 16:23:56 +0000

    Add a correct include for the IPC switches.
    TEST=none
    
    Review URL: http://codereview.chromium.org/160016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21391 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/renderer_main_unittest.cc
+++ chrome/renderer/renderer_main_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/main_function_params.h"
 #include "ipc/ipc_channel.h"
+#include "ipc/ipc_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 // TODO(port): Bring up this test this on other platforms.
