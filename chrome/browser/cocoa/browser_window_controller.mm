62e2e440b894a8c2e5c7704a9dec91961014dbca
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-23 16:35:56 +0000

    Propagate theme changes to the tabstrip.
    BUG=http://crbug.com/17519
    TEST=change themes; tabs should theme too
    
    Review URL: http://codereview.chromium.org/159271
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21393 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_window_controller.mm
+++ chrome/browser/cocoa/browser_window_controller.mm
@@ -835,6 +835,8 @@ willPositionSheet:(NSWindow*)sheet
 - (void)userChangedTheme {
   [self setTheme];
   [self applyTheme];
+
+  [tabStripController_ userChangedTheme];
 }
 
 // TODO(rohitrao, jrg): Move this logic out of BrowserWindowController?
