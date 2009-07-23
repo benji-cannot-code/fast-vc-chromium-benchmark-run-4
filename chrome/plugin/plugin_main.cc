741dab0109adb6412e50cf715c2273d1294583c5
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-23 18:17:55 +0000

    Unrevert 21355 because the revert might be causing the worker tests failures.
    Review URL: http://codereview.chromium.org/159276
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21412 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/plugin/plugin_main.cc
+++ chrome/plugin/plugin_main.cc
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // main() routine for running as the plugin process.
 int PluginMain(const MainFunctionParams& parameters) {
-  // The main thread of the plugin services IO.
-  MessageLoopForIO main_message_loop;
+  // The main thread of the plugin services UI.
+  MessageLoop main_message_loop(MessageLoop::TYPE_UI);
   std::wstring app_name = chrome::kBrowserAppName;
   PlatformThread::SetName(WideToASCII(app_name + L"_PluginMain").c_str());
 
@@ -81,7 +81,8 @@ int PluginMain(const MainFunctionParams& parameters) {
   }
 
   {
-    ChildProcess plugin_process(new PluginThread());
+    ChildProcess plugin_process;
+    plugin_process.set_main_thread(new PluginThread());
 #if defined(OS_WIN)
     if (!no_sandbox && target_services)
       target_services->LowerToken();
