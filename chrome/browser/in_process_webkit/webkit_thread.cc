83ed61b68c17ea909884ff583208c28031146d4c
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-23 00:09:02 +0000

    One part of many for enabling DOM Storage.
    Add browser-process IPC code + tweak the WebKit Thread.  Note that this code can't possibly be called/run yet.
    
    WebKitThread now has a PostIOThreadTask methods that can safely post back to the IO thread.  This should be used for all WebKit thread -> the rest of the world communication.
    
    There are many TODOs in this code that should be resolved before this is shipped without a --enable-local-storage flag.
    
    BUG=4360
    TEST=none
    Review URL: http://codereview.chromium.org/155845
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21346 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/in_process_webkit/webkit_thread.cc
+++ chrome/browser/in_process_webkit/webkit_thread.cc
@@ -11,13 +11,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/api/public/WebKit.h"
 
 // This happens on the UI thread before the IO thread has been shut down.
-WebKitThread::WebKitThread() {
+WebKitThread::WebKitThread()
+    : io_message_loop_(ChromeThread::GetMessageLoop(ChromeThread::IO)) {
   // The thread is started lazily by InitializeThread() on the IO thread.
 }
 
 // This happens on the UI thread after the IO thread has been shut down.
 WebKitThread::~WebKitThread() {
+  // There's no good way to see if we're on the UI thread.
   DCHECK(!ChromeThread::CurrentlyOn(ChromeThread::WEBKIT));
+  DCHECK(!ChromeThread::CurrentlyOn(ChromeThread::IO));
+  DCHECK(!io_message_loop_);
+}
+
+void WebKitThread::Shutdown() {
+  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
+  DCHECK(io_message_loop_);
+
+  // TODO(jorlow): Start flushing LocalStorage?
+
+  AutoLock lock(io_message_loop_lock_);
+  io_message_loop_ = NULL;
+}
+
+bool WebKitThread::PostIOThreadTask(
+    const tracked_objects::Location& from_here, Task* task) {
+  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::WEBKIT));
+  {
+    AutoLock lock(io_message_loop_lock_);
+    if (io_message_loop_) {
+      io_message_loop_->PostTask(from_here, task);
+      return true;
+    }
+  }
+  delete task;
+  return false;
 }
 
 WebKitThread::InternalWebKitThread::InternalWebKitThread()
@@ -30,13 +58,12 @@ void WebKitThread::InternalWebKitThread::Init() {
   webkit_client_ = new BrowserWebKitClientImpl;
   DCHECK(webkit_client_);
   WebKit::initialize(webkit_client_);
-  // Don't do anything heavyweight here since this can block the IO thread from
-  // executing (since InitializeThread() is called on the IO thread).
+  // If possible, post initialization tasks to this thread (rather than doing
+  // them now) so we don't block the IO thread any longer than we have to.
 }
 
 void WebKitThread::InternalWebKitThread::CleanUp() {
-  // Don't do anything heavyweight here since this can block the IO thread from
-  // executing (since the thread is shutdown from the IO thread).
+  // TODO(jorlow): Block on LocalStorage being 100% shut down.
   DCHECK(webkit_client_);
   WebKit::shutdown();
   delete webkit_client_;
@@ -46,6 +73,7 @@ MessageLoop* WebKitThread::InitializeThread() {
   if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kSingleProcess))
     return NULL;
 
+  DCHECK(io_message_loop_);
   DCHECK(!webkit_thread_.get());
   webkit_thread_.reset(new InternalWebKitThread);
   bool started = webkit_thread_->Start();
--- chrome/browser/renderer_host/resource_message_filter.cc
+++ chrome/browser/renderer_host/resource_message_filter.cc
@@ -170,6 +170,9 @@ ResourceMessageFilter::~ResourceMessageFilter() {
   // This function should be called on the IO thread.
   DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
 
+  // Tell the DOM Storage dispatcher host to stop sending messages via us.
+  dom_storage_dispatcher_host_->Shutdown();
+
   // Let interested observers know we are being deleted.
   NotificationService::current()->Notify(
       NotificationType::RESOURCE_MESSAGE_FILTER_SHUTDOWN,
@@ -245,7 +248,8 @@ bool ResourceMessageFilter::OnMessageReceived(const IPC::Message& message) {
                                                 message, this, &msg_is_ok) ||
                  app_cache_dispatcher_host_->OnMessageReceived(
                                                  message, &msg_is_ok) ||
-                 dom_storage_dispatcher_host_->OnMessageReceived(message) ||
+                 dom_storage_dispatcher_host_->OnMessageReceived(
+                                                   message, &msg_is_ok) ||
                  audio_renderer_host_->OnMessageReceived(message, &msg_is_ok);
 
   if (!handled) {
