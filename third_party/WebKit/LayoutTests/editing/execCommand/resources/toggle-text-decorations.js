4d554ffa43c0ada275fd2f1e6a935dbecd91e566
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-23 02:01:02 +0000

    WebCore:
    2009-07-22  Ryosuke Niwa  <rniwa@webkit.org>
    
            Reviewed by Eric Seidel.
    
            execCommand('underline' / 'strikeThrough') doesn't work properly with multiple styles in text-decoration
            https://bugs.webkit.org/show_bug.cgi?id=27476
    
            executeStrikethrough and executeUnderline were toggling between "line-through" / "underline" and "none".
            This patch adds executeToggleStyleInList that toggles a style in CSSValueList instead of toggling the entire value.
            It modifies CSSComputedStyleDeclaration to return CSSValueList instead of CSSPrimitiveValue for text decorations,
            and adds removeAll member function to CSSValueList.
    
            Tests: editing/execCommand/toggle-text-decorations.html
                   fast/css/getComputedStyle/getComputedStyle-text-decoration.html
    
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::renderTextDecorationFlagsToCSSValue): Creates a CSSValueList
            (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue): Returns a CSSValueList instead of CSSValue
            * css/CSSParser.cpp:
            (WebCore::CSSParser::parseValue): Text decorations are space separated instead of comma separated
            * css/CSSValueList.cpp:
            (WebCore::CSSValueList::removeAll): Removes all values that match the specified value
            * css/CSSValueList.h:
            * editing/EditorCommand.cpp:
            (WebCore::applyCommandToFrame): Apply style to a frame using specified command
            (WebCore::executeApplyStyle): Uses applyCommandToFrame
            (WebCore::executeToggleStyleInList): Uses applyCommandToFrame
            (WebCore::executeToggleStyle): Toggles a style in CSSValueList
            (WebCore::executeStrikethrough): Uses executeToggleStyleInList
            (WebCore::executeUnderline): Uses executeToggleStyleInList
    
    LayoutTests:
    
    2009-07-22  Ryosuke Niwa  <rniwa@webkit.org>
    
            Reviewed by Eric Seidel.
    
            execCommand('underline' / 'strikeThrough') doesn't work properly with multiple styles in text-decoration
            https://bugs.webkit.org/show_bug.cgi?id=27476
    
            toggle-text-decorations checks whether we can toggle text decorations with multiple styles properly.
            e.g. execCommand("underline") should modify "text-decoration: underline overline" to "text-decoration: overline"
    
            getComputedStyle-text-decorations checks whether the CSS value of text decoration and its computed style are
            both space-separated CSSValueList instead of CSSPrimitiveValue or comma-separated CSSValueList.
    
            * editing/execCommand/resources/toggle-text-decorations.js: Added.
            (testSingleToggle):
            * editing/execCommand/toggle-text-decorations-expected.txt: Added.
            * editing/execCommand/toggle-text-decorations.html: Added.
            * fast/css/getComputedStyle/getComputedStyle-text-decoration-expected.txt: Added.
            * fast/css/getComputedStyle/getComputedStyle-text-decoration.html: Added.
            * fast/css/getComputedStyle/resources: Added.
            * fast/css/getComputedStyle/resources/TEMPLATE.html: Added.
            * fast/css/getComputedStyle/resources/getComputedStyle-text-decoration.js: Added.
            (expect):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46251 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/editing/execCommand/resources/toggle-text-decorations.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test to make sure we can toggle text decorations correctly.")
+
+var testContainer = document.createElement("div");
+testContainer.contentEditable = true;
+document.body.appendChild(testContainer);
+
+function testSingleToggle(toggleCommand, initialContents, expectedContents)
+{
+    testContainer.innerHTML = initialContents;
+    window.getSelection().selectAllChildren(testContainer);
+    document.execCommand(toggleCommand, false, null);
+    if (testContainer.innerHTML === expectedContents) {
+        testPassed("one " + toggleCommand + " command converted " + initialContents + " to " + expectedContents);
+    } else {
+        testFailed("one " + toggleCommand + " command converted " + initialContents + " to " + testContainer.innerHTML + ", expected " + expectedContents);
+    }
+}
+
+document.execCommand("styleWithCSS", true, null);
+testSingleToggle("underline", "test", "<span class=\"Apple-style-span\" style=\"text-decoration: underline;\">test</span>");
+testSingleToggle("underline", "<span class=\"Apple-style-span\" style=\"text-decoration: underline;\">test</span>", "test");
+testSingleToggle("underline", "<span class=\"Apple-style-span\" style=\"text-decoration: underline line-through overline;\">test</span>", "<span class=\"Apple-style-span\" style=\"text-decoration: overline line-through;\">test</span>");
+testSingleToggle("strikethrough", "test", "<span class=\"Apple-style-span\" style=\"text-decoration: line-through;\">test</span>");
+testSingleToggle("strikethrough", "<span class=\"Apple-style-span\" style=\"text-decoration: line-through;\">test</span>", "test");
+testSingleToggle("strikethrough", "<span class=\"Apple-style-span\" style=\"text-decoration: underline line-through overline;\">test</span>", "<span class=\"Apple-style-span\" style=\"text-decoration: underline overline;\">test</span>");
+
+document.body.removeChild(testContainer);
+
+var successfullyParsed = true;
--- third_party/WebKit/WebCore/css/CSSValueList.h
+++ third_party/WebKit/WebCore/css/CSSValueList.h
@@ -53,6 +53,7 @@ public:
 
     void append(PassRefPtr<CSSValue>);
     void prepend(PassRefPtr<CSSValue>);
+    bool removeAll(CSSValue*);
 
     virtual String cssText() const;
 
