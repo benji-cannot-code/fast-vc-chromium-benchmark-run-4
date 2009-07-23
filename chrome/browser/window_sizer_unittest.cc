ca43566d5ea194454c474878c345b030e885bc79
arv@google.com <arv@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-23 01:29:53 +0000

    Tweak thumbnail sizes a bit so it the NNTP looks better side by side on a 1920x1200 screen.
    Also, tweak the default size of new windows slightly. In case we decide
    to use side by side windows size the window in such a way that 2
    windows fit on the screen and have the kWindowTilePixels spacing
    between each other and the screen edges.
    
    BUG=None
    
    TEST=Show two windows side. On a 1920x1200 screen they should both be
    showing the thumbnails in normal size.
    
    Review URL: http://codereview.chromium.org/155930
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21364 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/window_sizer_unittest.cc
+++ chrome/browser/window_sizer_unittest.cc
@@ -264,7 +264,7 @@ TEST(WindowSizerTest, DefaultSizeCase) {
                     false, DEFAULT, &window_bounds, &maximized);
     EXPECT_FALSE(maximized);
     EXPECT_EQ(gfx::Rect(kWindowTilePixels, kWindowTilePixels,
-                        840 - kWindowTilePixels * 3,
+                        840 - static_cast<int>(kWindowTilePixels * 1.5),
                         1050 - kWindowTilePixels * 2),
               window_bounds);
   }
@@ -276,7 +276,7 @@ TEST(WindowSizerTest, DefaultSizeCase) {
                     false, DEFAULT, &window_bounds, &maximized);
     EXPECT_FALSE(maximized);
     EXPECT_EQ(gfx::Rect(kWindowTilePixels, kWindowTilePixels,
-                        960 - kWindowTilePixels * 3,
+                        960 - static_cast<int>(kWindowTilePixels * 1.5),
                         1200 - kWindowTilePixels * 2),
               window_bounds);
   }
