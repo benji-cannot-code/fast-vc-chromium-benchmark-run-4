e54ee7fca0e9adc8b491b7b6ac29b07e1ecba494
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-23 17:20:13 +0000

    WebCore:
    2009-07-23  David Hyatt  <hyatt@apple.com>
    
            Reviewed by Dan Bernstein.
    
            https://bugs.webkit.org/show_bug.cgi?id=27581
            Drop the prefix from the box-shadow property.
    
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::):
            (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
            * css/CSSParser.cpp:
            (WebCore::CSSParser::parseValue):
            (WebCore::ShadowParseContext::commitLength):
            (WebCore::cssPropertyID):
            * css/CSSPropertyNames.in:
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::applyProperty):
            * page/animation/AnimationBase.cpp:
            (WebCore::ensurePropertyMap):
    
    LayoutTests:
    
    2009-07-23  David Hyatt  <hyatt@apple.com>
    
            Reviewed by Dan Bernstein.
            
            https://bugs.webkit.org/show_bug.cgi?id=27581
            Drop the prefix from the box-shadow property.
    
            * platform/mac/fast/css/getComputedStyle/computed-style-expected.txt:
            * platform/mac/fast/css/getComputedStyle/computed-style-without-renderer-expected.txt:
            * svg/css/getComputedStyle-basic-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSComputedStyleDeclaration.cpp
+++ third_party/WebKit/WebCore/css/CSSComputedStyleDeclaration.cpp
@@ -73,6 +73,7 @@ static const int computedProperties[] = {
     CSSPropertyBorderTopStyle,
     CSSPropertyBorderTopWidth,
     CSSPropertyBottom,
+    CSSPropertyBoxShadow,
     CSSPropertyCaptionSide,
     CSSPropertyClear,
     CSSPropertyClip,
@@ -168,7 +169,6 @@ static const int computedProperties[] = {
     CSSPropertyWebkitBoxOrient,
     CSSPropertyWebkitBoxPack,
     CSSPropertyWebkitBoxReflect,
-    CSSPropertyWebkitBoxShadow,
     CSSPropertyWebkitBoxSizing,
     CSSPropertyWebkitColumnBreakAfter,
     CSSPropertyWebkitColumnBreakBefore,
@@ -744,7 +744,7 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(int proper
         }
         case CSSPropertyWebkitBoxReflect:
             return valueForReflection(style->boxReflect());
-        case CSSPropertyWebkitBoxShadow:
+        case CSSPropertyBoxShadow:
             return valueForShadow(style->boxShadow(), static_cast<CSSPropertyID>(propertyID));
         case CSSPropertyCaptionSide:
             return CSSPrimitiveValue::create(style->captionSide());
