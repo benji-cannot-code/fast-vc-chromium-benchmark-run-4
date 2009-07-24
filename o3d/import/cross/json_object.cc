4f88adafbdb2d5be62dd9ef78861919cc50243b5
gman@google.com <gman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-24 09:17:27 +0000

    Fix for bug in GCC
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21521 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/import/cross/json_object.cc
+++ o3d/import/cross/json_object.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This file defines the JSON Object class.
 
-#include "core/cross/precompile.h"
 #include "import/cross/json_object.h"
 
 namespace o3d {
--- o3d/import/cross/json_object_test.cc
+++ o3d/import/cross/json_object_test.cc
@@ -212,6 +212,10 @@ class TestJSONObject : public JSONObject {
     optional_string_value_->set_value(value);
   }
 
+  static ObjectBase::Ref Create(ServiceLocator* service_locator) {
+    return ObjectBase::Ref(new TestJSONObject(service_locator));
+  }
+
  private:
   explicit TestJSONObject(ServiceLocator* service_locator)
       : JSONObject(service_locator) {
@@ -233,11 +237,6 @@ class TestJSONObject : public JSONObject {
     RegisterJSONValue(kOptionalIntegerValueName, &optional_integer_value_);
     RegisterJSONValue(kOptionalBooleanValueName, &optional_boolean_value_);
     RegisterJSONValue(kOptionalStringValueName, &optional_string_value_);
-  }
-
-  friend class IClassManager;
-  static ObjectBase::Ref Create(ServiceLocator* service_locator) {
-    return ObjectBase::Ref(new TestJSONObject(service_locator));
   }
 
   // One of each type of JSONValue
