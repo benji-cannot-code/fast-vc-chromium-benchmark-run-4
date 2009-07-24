a3345210771d1750d5aa04bcdd34b5543836594a
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-24 07:31:33 +0000

    Roll out r46321 as it broke the Windows build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@46325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebMutableURLRequest.cpp
+++ third_party/WebKit/WebKit/win/WebMutableURLRequest.cpp
@@ -31,9 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKit.h"
 #include "MarshallingHelpers.h"
 #include "WebKit.h"
-#if PLATFORM(CFNETWORK)
 #include <CFNetwork/CFURLRequestPriv.h>
-#endif
 #pragma warning(push, 0)
 #include <WebCore/BString.h>
 #include <WebCore/COMPtr.h>
