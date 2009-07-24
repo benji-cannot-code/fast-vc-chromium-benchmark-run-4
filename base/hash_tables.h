513d6acf6511691358f9759a484d1664998e0ff6
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-24 01:20:08 +0000

    I'm apparently the first one who's tried to do hash_map<string16, ...>.  Unfortunately, hash_map<> does not know how to hash a string16 when string16 isn't a wstring.  I looked in <tr1/functional> and saw that only std::string and std::wstring have hashing functions.
    This change adds a hashing function for string16's when they're not the same as a std::wstring.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/160062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21494 0039d316-1c4b-4281-b951-d872f2087c98

--- base/hash_tables.h
+++ base/hash_tables.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
+#include "base/string16.h"
+
 #if defined(COMPILER_MSVC)
 #include <hash_map>
 #include <hash_set>
@@ -100,6 +102,32 @@ struct hash<long long> {
     return std::tr1::hash<long>()((long) i);
   }
 };
+
+#if defined(WCHAR_T_IS_UTF32)
+template<>
+struct hash<string16> {
+  size_t operator()(const string16& s) const {
+    // This comes from GNU libstdc++, but the types have been changed to
+    // make it compile.  The lib only defines the hash for string and wstring.
+    std::size_t result = 0;
+    for (string16::const_iterator i = s.begin(); i != s.end(); ++i)
+      result = (result * 131) + *i;
+    return result;
+  }
+};
+
+template<>
+struct hash<const string16> {
+  size_t operator()(const string16& s) const {
+    // This comes from GNU libstdc++, but the types have been changed to
+    // make it compile.  The lib only defines the hash for string and wstring.
+    std::size_t result = 0;
+    for (string16::const_iterator i = s.begin(); i != s.end(); ++i)
+      result = (result * 131) + *i;
+    return result;
+  }
+};
+#endif
 
 }
 
