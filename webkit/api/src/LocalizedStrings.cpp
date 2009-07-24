96276895ad7eb1c4c0ee3cc2adff92b5939ad45a
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-24 22:08:24 +0000

    Verified that these strings are not used, so removing the FIXME.
    BUG=15854
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/159360
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21589 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/src/LocalizedStrings.cpp
+++ webkit/api/src/LocalizedStrings.cpp
@@ -242,8 +242,6 @@ String contextMenuItemTagLeftToRight() { return String(); }
 String contextMenuItemTagRightToLeft() { return String(); }
 String contextMenuItemTagInspectElement() { return String(); }
 String contextMenuItemTagShowSpellingPanel(bool show) { return String(); }
-
-// FIXME: These should return something.
 String mediaElementLiveBroadcastStateText() { return String(); }
 String mediaElementLoadingStateText() { return String(); }
 
