f104bcb4482c83a23b457003a33af3c41ab71d4f
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-24 07:07:43 +0000

    Move parts of npruntime into the WebKit API so it no longer has any dependencies on WebCore.
    TEST=none
    BUG=none
    Review URL: http://codereview.chromium.org/160091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21516 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/npruntime_util.h
+++ webkit/glue/npruntime_util.h
@@ -6,13 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_GLUE_NPRUNTIME_UTIL_H_
 #define WEBKIT_GLUE_NPRUNTIME_UTIL_H_
 
-#include "third_party/npapi/bindings/npruntime.h"
+#include "bindings/npruntime.h"
 
 class Pickle;
-
-namespace WebKit {
-class WebDragData;
-}
 
 namespace webkit_glue {
 
@@ -20,17 +16,6 @@ namespace webkit_glue {
 bool SerializeNPIdentifier(NPIdentifier identifier, Pickle* pickle);
 bool DeserializeNPIdentifier(const Pickle& pickle, void** pickle_iter,
                              NPIdentifier* identifier);
-
-// Return true (success) if the given npobj is the current
-// drag event in browser dispatch, and is accessible based on context execution
-// frames and their security origins and WebKit clipboard access policy. If so,
-// return the event id and the clipboard data (WebDragData).
-bool GetDragData(NPObject* npobj, int* event_id, WebKit::WebDragData* data);
-
-// Invoke the event access policy checks listed above with
-// GetDragData().  No need for clipboard data or event_id outputs, just confirm
-// the given npobj is the current & accessible drag event.
-bool IsDragEvent(NPObject* npobj);
 
 }  // namespace webkit_glue
 
