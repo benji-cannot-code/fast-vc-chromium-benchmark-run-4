904fd0e3b831bc671e02d8c5e2dfe4a4e529f6a0
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-24 17:34:42 +0000

    Reviewed by Anders Carlsson.
            Add the shadow style member to the ShadowData constructor and ==
            operator
    
            * rendering/style/ShadowData.cpp:
            (WebCore::ShadowData::ShadowData):
            (WebCore::ShadowData::operator==):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/style/ShadowData.cpp
+++ third_party/WebKit/WebCore/rendering/style/ShadowData.cpp
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 1999 Antti Koivisto (koivisto@kde.org)
- * Copyright (C) 2004, 2005, 2006, 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -30,6 +30,7 @@ ShadowData::ShadowData(const ShadowData& o)
     , y(o.y)
     , blur(o.blur)
     , spread(o.spread)
+    , style(o.style)
     , color(o.color)
 {
     next = o.next ? new ShadowData(*o.next) : 0;
@@ -41,7 +42,7 @@ bool ShadowData::operator==(const ShadowData& o) const
         (next && o.next && *next != *o.next))
         return false;
     
-    return x == o.x && y == o.y && blur == o.blur && spread == o.spread && color == o.color;
+    return x == o.x && y == o.y && blur == o.blur && spread == o.spread && style == o.style && color == o.color;
 }
 
 } // namespace WebCore
