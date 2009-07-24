353ae4cbe4f2077e6bd9ebd44f9f9d13523ffd52
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-24 13:55:43 +0000

    Hide ia32 asm when not building for ARCH_CPU_X86_FAMILY
    r21097, 'Linux sandbox: plumb timezone calls through the sandbox' adds some
    ia32 assembly by the way of a define.  This is bad for building for ARM.
    
    Patch by Joel Stanley.
    
    Review URL: http://codereview.chromium.org/160097
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21527 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/zygote_main_linux.cc
+++ chrome/browser/zygote_main_linux.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/prctl.h>
 #include <sys/wait.h>
 
+#include "base/basictypes.h"
 #include "base/command_line.h"
 #include "base/eintr_wrapper.h"
 #include "base/global_descriptors_posix.h"
@@ -310,8 +311,10 @@ static bool MaybeEnterChroot() {
     // files and cache the results or the descriptors.
     base::RandUint64();
 
+#if defined(ARCH_CPU_X86_FAMILY)
     PATCH_GLOBAL_OFFSET_TABLE(localtime, sandbox_wrapper::localtime);
     PATCH_GLOBAL_OFFSET_TABLE(localtime_r, sandbox_wrapper::localtime_r);
+#endif
 
     static const char kChrootMe = 'C';
     static const char kChrootMeSuccess = 'O';
