c67251e5cbf34d304d2704244e7af24c20ace4ce
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-25 14:54:29 +0000

    npruntime_util.h in webkit/glue should use the absolute path to npruntime.h rather than the "bindings/npruntime.h" that we must use in the WebKit API code. Change this back and revert the DEPS file change that allowed it to happen.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/160140
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21617 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/npruntime_util.h
+++ webkit/glue/npruntime_util.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_GLUE_NPRUNTIME_UTIL_H_
 #define WEBKIT_GLUE_NPRUNTIME_UTIL_H_
 
-#include "bindings/npruntime.h"
+#include "third_party/npapi/bindings/npruntime.h"
 
 class Pickle;
 
