2944e5f1567377a06f80ac084f3b17ac9a1e80f1
mhm@chromium.org <mhm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-25 00:24:31 +0000

    Reverting 21593. TBR=thiago Review URL: http://codereview.chromium.org/159377
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21594 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.h
+++ chrome/browser/automation/automation_provider.h
@@ -31,10 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/automation/automation_constants.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_channel.h"
-#if defined(TOOLKIT_VIEWS)
+#if defined(OS_WIN)
 #include "views/event.h"
-#include "views/view.h"
-#endif  // defined(TOOLKIT_VIEWS)
+#endif  // defined(OS_WIN)
 
 struct AutomationMsg_Find_Params;
 
