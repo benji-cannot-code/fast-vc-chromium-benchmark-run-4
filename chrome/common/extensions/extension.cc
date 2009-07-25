19ca5f9828cd4680c482e739f4512af07be455ba
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-25 03:15:58 +0000

    Implement mimetype sniffing for extensions.
    abarth: can you review the changes to mime_sniffer.cc?
    paul: everything else?
    
    BUG=13296
    TEST=Added unit tests
    Review URL: http://codereview.chromium.org/159345
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21612 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension.cc
+++ chrome/common/extensions/extension.cc
@@ -75,6 +75,8 @@ const char* Extension::kExtensionRegistryPath =
 // first 16 bytes of SHA256 hashed public key.
 const size_t Extension::kIdSize = 16;
 
+const char Extension::kMimeType[] = "application/x-chrome-extension";
+
 const int Extension::kKnownIconSizes[] = { 128 };
 
 Extension::~Extension() {
