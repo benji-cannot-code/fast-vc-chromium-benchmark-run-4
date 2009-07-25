c7e423a641555704b66a9943bb3fdee350e5ffc9
adachan@apple.com <adachan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-25 01:49:12 +0000

    In preparation for https://bugs.webkit.org/show_bug.cgi?id=27236:         Remove TCMALLOC_TRACK_DECOMMITED_SPANS.  We'll always track decommitted spans.         We have tested this and show it has little impact on performance.
            Reviewed by Mark Rowe.
    
            * wtf/FastMalloc.cpp:
            (WTF::TCMalloc_PageHeap::New):
            (WTF::TCMalloc_PageHeap::AllocLarge):
            (WTF::propagateDecommittedState):
            (WTF::mergeDecommittedStates):
            (WTF::TCMalloc_PageHeap::Delete):
            (WTF::TCMalloc_PageHeap::IncrementalScavenge):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46387 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/FastMalloc.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/FastMalloc.cpp
@@ -96,8 +96,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define FORCE_SYSTEM_MALLOC 1
 #endif
 
-#define TCMALLOC_TRACK_DECOMMITED_SPANS (HAVE(VIRTUALALLOC) || HAVE(MADV_FREE_REUSE))
-
 #ifndef NDEBUG
 namespace WTF {
 
@@ -1044,11 +1042,7 @@ struct Span {
 #endif
 };
 
-#if TCMALLOC_TRACK_DECOMMITED_SPANS
 #define ASSERT_SPAN_COMMITTED(span) ASSERT(!span->decommitted)
-#else
-#define ASSERT_SPAN_COMMITTED(span)
-#endif
 
 #ifdef SPAN_HISTORY
 void Event(Span* span, char op, int v = 0) {
@@ -1370,12 +1364,10 @@ inline Span* TCMalloc_PageHeap::New(Length n) {
 
     Span* result = ll->next;
     Carve(result, n, released);
-#if TCMALLOC_TRACK_DECOMMITED_SPANS
     if (result->decommitted) {
         TCMalloc_SystemCommit(reinterpret_cast<void*>(result->start << kPageShift), static_cast<size_t>(n << kPageShift));
         result->decommitted = false;
     }
-#endif
     ASSERT(Check());
     free_pages_ -= n;
     return result;
@@ -1432,12 +1424,10 @@ Span* TCMalloc_PageHeap::AllocLarge(Length n) {
 
   if (best != NULL) {
     Carve(best, n, from_released);
-#if TCMALLOC_TRACK_DECOMMITED_SPANS
     if (best->decommitted) {
         TCMalloc_SystemCommit(reinterpret_cast<void*>(best->start << kPageShift), static_cast<size_t>(n << kPageShift));
         best->decommitted = false;
     }
-#endif
     ASSERT(Check());
     free_pages_ -= n;
     return best;
@@ -1462,14 +1452,10 @@ Span* TCMalloc_PageHeap::Split(Span* span, Length n) {
   return leftover;
 }
 
-#if !TCMALLOC_TRACK_DECOMMITED_SPANS
-static ALWAYS_INLINE void propagateDecommittedState(Span*, Span*) { }
-#else
 static ALWAYS_INLINE void propagateDecommittedState(Span* destination, Span* source)
 {
     destination->decommitted = source->decommitted;
 }
-#endif
 
 inline void TCMalloc_PageHeap::Carve(Span* span, Length n, bool released) {
   ASSERT(n > 0);
@@ -1496,9 +1482,6 @@ inline void TCMalloc_PageHeap::Carve(Span* span, Length n, bool released) {
   }
 }
 
-#if !TCMALLOC_TRACK_DECOMMITED_SPANS
-static ALWAYS_INLINE void mergeDecommittedStates(Span*, Span*) { }
-#else
 static ALWAYS_INLINE void mergeDecommittedStates(Span* destination, Span* other)
 {
     if (destination->decommitted && !other->decommitted) {
@@ -1510,7 +1493,6 @@ static ALWAYS_INLINE void mergeDecommittedStates(Span* destination, Span* other)
         destination->decommitted = true;
     }
 }
-#endif
 
 inline void TCMalloc_PageHeap::Delete(Span* span) {
   ASSERT(Check());
@@ -1557,15 +1539,12 @@ inline void TCMalloc_PageHeap::Delete(Span* span) {
 
   Event(span, 'D', span->length);
   span->free = 1;
-#if TCMALLOC_TRACK_DECOMMITED_SPANS
   if (span->decommitted) {
     if (span->length < kMaxPages)
       DLL_Prepend(&free_[span->length].returned, span);
     else
       DLL_Prepend(&large_.returned, span);
-  } else
-#endif
-  {
+  } else {
     if (span->length < kMaxPages)
       DLL_Prepend(&free_[span->length].normal, span);
     else
@@ -1597,9 +1576,7 @@ void TCMalloc_PageHeap::IncrementalScavenge(Length n) {
       DLL_Remove(s);
       TCMalloc_SystemRelease(reinterpret_cast<void*>(s->start << kPageShift),
                              static_cast<size_t>(s->length << kPageShift));
-#if TCMALLOC_TRACK_DECOMMITED_SPANS
       s->decommitted = true;
-#endif
       DLL_Prepend(&slist->returned, s);
 
       scavenge_counter_ = std::max<size_t>(64UL, std::min<size_t>(kDefaultReleaseDelay, kDefaultReleaseDelay - (free_pages_ / kDefaultReleaseDelay)));
