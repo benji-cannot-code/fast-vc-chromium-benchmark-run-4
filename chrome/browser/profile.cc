4a8b303288d96da6651e746cc127d26c53db6dc9
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-26 03:41:47 +0000

    Add infobar preview for themes, remove --enable-extension requirement. Review URL: http://codereview.chromium.org/160141
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21622 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profile.cc
+++ chrome/browser/profile.cc
@@ -671,6 +671,7 @@ ProfileImpl::~ProfileImpl() {
   bookmark_bar_model_.reset();
 
   extension_message_service_->ProfileDestroyed();
+  extensions_service_->ProfileDestroyed();
 
   MarkAsCleanShutdown();
 }
