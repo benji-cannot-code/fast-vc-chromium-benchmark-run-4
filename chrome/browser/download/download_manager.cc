85ffeafc7a4dbf210223573a35d0e674730f6bda
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-26 07:25:32 +0000

    Modify detection of Chrome extensions in download manager. It turns out many servers send the no-sniff option, including the one we use for our sample extensions.
    So we can't rely on mime types to detect Chrome extensions, at
    least until we have some place to put our samples that doesn't
    have this problem.
    
    TBR=erikkay
    Review URL: http://codereview.chromium.org/159391
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21627 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_manager.cc
+++ chrome/browser/download/download_manager.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/resource_dispatcher_host.h"
 #include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/platform_util.h"
@@ -109,6 +110,19 @@ static bool DownloadPathIsDangerous(const FilePath& download_path) {
     return false;
   }
   return (download_path == desktop_dir);
+}
+
+// Helper to determine if a download is a Chrome extension. We should be able to
+// just use the mime type, but even our own servers are not setup to serve the
+// right headers yet, so we have a short-term file extension heuristic, too.
+static bool IsChromeExtension(const FilePath& path,
+                              const std::string& mime_type) {
+  // If the server says it is an extension, it is definitely an extension.
+  if (mime_type == Extension::kMimeType)
+    return true;
+
+  // Otherwise, it is an extension if it has the right, err, extension.
+  return path.Extension().substr(1) == chrome::kExtensionFileExtension;
 }
 
 // DownloadItem implementation -------------------------------------------------
@@ -575,7 +589,7 @@ void DownloadManager::StartDownload(DownloadCreateInfo* info) {
     // b) They are an extension that is not from the gallery
     if (IsDangerous(info->suggested_path.BaseName()))
       info->is_dangerous = true;
-    else if (info->mime_type == Extension::kMimeType &&
+    else if (IsChromeExtension(info->path, info->mime_type) &&
              !ExtensionsService::IsDownloadFromGallery(info->url,
                                                        info->referrer_url)) {
       info->is_dangerous = true;
@@ -855,7 +869,7 @@ void DownloadManager::ContinueDownloadFinished(DownloadItem* download) {
     extension = extension.substr(1);
 
   // Handle chrome extensions explicitly and skip the shell execute.
-  if (download->mime_type() == Extension::kMimeType) {
+  if (IsChromeExtension(download->full_path(), download->mime_type())) {
     OpenChromeExtension(download->full_path(), download->url(),
                         download->referrer_url());
     download->set_auto_opened(true);
@@ -1225,7 +1239,7 @@ void DownloadManager::OpenDownload(const DownloadItem* download,
                                    gfx::NativeView parent_window) {
   // Open Chrome extensions with ExtensionsService. For everything else do shell
   // execute.
-  if (download->mime_type() == Extension::kMimeType) {
+  if (IsChromeExtension(download->full_path(), download->mime_type())) {
     OpenChromeExtension(download->full_path(), download->url(),
                         download->referrer_url());
   } else {
