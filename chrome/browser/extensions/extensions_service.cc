ed35c1417016ad317263f929e9f6ac438d75370c
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-26 03:54:27 +0000

    Fix broken tree on mac/linux.
    Review URL: http://codereview.chromium.org/159389
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21623 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service.cc
+++ chrome/browser/extensions/extensions_service.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_updater.h"
 #include "chrome/browser/extensions/external_extension_provider.h"
 #include "chrome/browser/extensions/external_pref_extension_provider.h"
-#include "chrome/browser/views/extensions/theme_preview_infobar_delegate.h"
+#include "chrome/browser/extensions/theme_preview_infobar_delegate.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/utility_process_host.h"
