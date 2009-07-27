bd975da892e8f3b957097689e8e8cfbdcb2befbd
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-27 22:16:11 +0000

    Don't create an ExtensionDOMUI for urls with invalid extension IDs.
    This fixes a crash bug.
    
    BUG=17546
    TEST=Navigate to chrome-extension://baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/ with no
    extensions installed.  Chrome should not crash.
    
    Review URL: http://codereview.chromium.org/160199
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21721 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_dom_ui.h
+++ chrome/browser/extensions/extension_dom_ui.h
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef CHROME_BROWSER_EXTENSIONS_EXTENSION_DOM_UI_H_
 #define CHROME_BROWSER_EXTENSIONS_EXTENSION_DOM_UI_H_
 
