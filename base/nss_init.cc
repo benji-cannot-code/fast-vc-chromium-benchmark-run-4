31b0bfe3da8e2aa7aaab546db47b966988ab08f0
dkegel@google.com <dkegel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-27 16:47:12 +0000

    Avoid runtime linker warning "Symbol `SSL_ImplementedCiphers' has different size in shared object, consider re-linking" if SSL_ImplementedCiphers is different size on target system than it was on build system.
    BUG=12826
    TEST=run on Jaunty, look for "Symbol `SSL_ImplementedCiphers' has different size in shared object" in console output
    
    Review URL: http://codereview.chromium.org/118367
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21649 0039d316-1c4b-4281-b951-d872f2087c98

--- base/nss_init.cc
+++ base/nss_init.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/nss_init.h"
 
+#include <dlfcn.h>
 #include <nss.h>
 #include <plarena.h>
 #include <prerror.h>
@@ -94,12 +95,22 @@ class NSSInitSingleton {
 
     NSS_SetDomesticPolicy();
 
+    // Use late binding to avoid scary but benign warning
+    // "Symbol `SSL_ImplementedCiphers' has different size in shared object,
+    //  consider re-linking"
+    const PRUint16* pSSL_ImplementedCiphers = static_cast<const PRUint16*>(
+        dlsym(RTLD_DEFAULT, "SSL_ImplementedCiphers"));
+    if (pSSL_ImplementedCiphers == NULL) {
+      NOTREACHED() << "Can't get list of supported ciphers";
+      return;
+    }
+
     // Explicitly enable exactly those ciphers with keys of at least 80 bits
     for (int i = 0; i < SSL_NumImplementedCiphers; i++) {
       SSLCipherSuiteInfo info;
-      if (SSL_GetCipherSuiteInfo(SSL_ImplementedCiphers[i], &info,
+      if (SSL_GetCipherSuiteInfo(pSSL_ImplementedCiphers[i], &info,
                                  sizeof(info)) == SECSuccess) {
-        SSL_CipherPrefSetDefault(SSL_ImplementedCiphers[i],
+        SSL_CipherPrefSetDefault(pSSL_ImplementedCiphers[i],
                                  (info.effectiveKeyBits >= 80));
       }
     }
