d70a8f25587ff84ab74e763e6de1e5d42fd769bb
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-27 22:18:25 +0000

    WebCore:
    2009-07-27  Nikolas Zimmermann  <nikolas.zimmermann@torchmobile.com>
    
            Reviewed by George Staikos.
    
            [WML] 'onpick' intrinsic event handling missing
            https://bugs.webkit.org/show_bug.cgi?id=27723
    
            Trigger 'onpick' intrinsic events from WMLOptionElement::setSelectedState().
            All was in place, just forgot to enable the relevant code.
    
            Fixes manual-tests/wml/select-onpick-event.wml
            Test: wml/option-element-onpick.html
    
            * wml/WMLOptionElement.cpp:
            (WebCore::WMLOptionElement::setSelectedState):
    
    LayoutTests:
    
    2009-07-27  Nikolas Zimmermann  <nikolas.zimmermann@torchmobile.com>
    
            Reviewed by George Staikos.
    
            [WML] 'onpick' intrinsic event handling missing
            https://bugs.webkit.org/show_bug.cgi?id=27723
    
            Add a new WML layout test covering 'onpick' support.
    
            * wml/option-element-onpick-expected.txt: Added.
            * wml/option-element-onpick.html: Added.
            * wml/resources/option-element-onpick.js: Added.
            (setupTestDocument):
            (prepareTest):
            (executeTest):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46432 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/wml/resources/option-element-onpick.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// [Name] option-element-onpick.js
+
+createWMLTestCase("Tests onpick intrinsic event support of option elements");
+
+function setupTestDocument() {
+    var cardElement = testDocument.documentElement.firstChild;
+
+    var selectElement = createWMLElement("select");
+    selectElement.setAttribute("name", "result");
+    selectElement.setAttribute("multiple", "false");
+    cardElement.appendChild(selectElement);
+
+    var optionElement1 = createWMLElement("option");
+    optionElement1.setAttribute("onpick", "#card2");
+    optionElement1.setAttribute("value", "doggy");
+    optionElement1.textContent = "Dog";
+    selectElement.appendChild(optionElement1);
+
+    var optionElement2 = createWMLElement("option");
+    optionElement2.setAttribute("value", "kitten");
+    optionElement2.textContent = "Cat";
+    selectElement.appendChild(optionElement2);
+}
+
+function prepareTest() {
+    startTest(25, 15);
+}
+
+function executeTest() {
+    completeTest();
+}
+
+var successfullyParsed = true;
