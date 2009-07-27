5cb4459a1a8346d55990c44c12a4ca5359fcd7b2
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-27 21:12:54 +0000

    Fix gradient drawing on the Mac. (Base coordinate system != window base coordinate system.)
    BUG=http://crbug.com/17716
    TEST=as in bug
    
    Review URL: http://codereview.chromium.org/160194
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21707 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/background_gradient_view.mm
+++ chrome/browser/cocoa/background_gradient_view.mm
@@ -53,10 +53,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     CGFloat winHeight = NSHeight([[self window] frame]);
     NSGradient *gradient = [theme gradientForStyle:GTMThemeStyleToolBar
                                              state:isKey];
-    NSPoint startPoint = [self convertPointFromBase:
-                          NSMakePoint(0, winHeight - kToolbarTopOffset)];
-    NSPoint endPoint = [self convertPointFromBase:
-        NSMakePoint(0, winHeight - kToolbarTopOffset - kToolbarMaxHeight)];
+    NSPoint startPoint =
+        [self convertPoint:NSMakePoint(0, winHeight - kToolbarTopOffset)
+                  fromView:nil];
+    NSPoint endPoint =
+        [self convertPoint:NSMakePoint(0, winHeight -
+                                            kToolbarTopOffset -
+                                            kToolbarMaxHeight)
+                  fromView:nil];
 
     [gradient drawFromPoint:startPoint
                     toPoint:endPoint
