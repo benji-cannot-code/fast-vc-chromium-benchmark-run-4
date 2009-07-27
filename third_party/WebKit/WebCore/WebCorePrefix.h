ec34d9cfde5a6e2084e1e6d20e1e504024c74338
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-27 07:02:02 +0000

    2009-07-26  Brent Fulgham  <bfulgham@webkit.org>
            Build correct, no review.
    
            Change in r46407 broke Apple Windows build.
    
            * WebCorePrefix.h: Use WTF_PLATFORM_CG to decide if
              CoreServices.h should be included.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCorePrefix.h
+++ third_party/WebKit/WebCore/WebCorePrefix.h
@@ -110,7 +110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <CoreFoundation/CoreFoundation.h>
 #endif
 
-#if defined(__APPLE__)
+#ifdef WTF_PLATFORM_CG
 #include <CoreServices/CoreServices.h>
 #endif
 
