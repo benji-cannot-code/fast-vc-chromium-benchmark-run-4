3dd446d12dfde3be2598f491eeeb2ec704a590b8
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-27 07:17:48 +0000

    2009-07-27  Brent Fulgham  <bfulgham@webkit.org>
            Build correct, no review.
    
            Change in r46407 broke Apple Windows build.
            Switch to WinCairo-only test, to avoid any
            other platform break.
    
            * WebCorePrefix.h: 
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46409 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCorePrefix.h
+++ third_party/WebKit/WebCore/WebCorePrefix.h
@@ -108,10 +108,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifndef BUILDING_WX__
 #include <CoreFoundation/CoreFoundation.h>
-#endif
-
-#ifdef WTF_PLATFORM_CG
+#ifndef WIN_CAIRO
 #include <CoreServices/CoreServices.h>
+#endif
 #endif
 
 #ifdef __OBJC__
