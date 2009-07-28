288652a634a79d13409a4afb59d55516f7ce045a
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-28 15:03:09 +0000

    2009-07-28  Nikolas Zimmermann  <nikolas.zimmermann@torchmobile.com>
            Reviewed by George Staikos.
    
            [WML] Page title doesn't update after variable substitution, if it contained variables
            https://bugs.webkit.org/show_bug.cgi?id=27725
    
            Fix bug in containsVariableReference() function, we we're decreasing the nameEndPosition
            too much, leading to a bug with 1-char variable names, which were not detected properly.
    
            Extend wml/variable-reference-valid.html layout test to cover 1-char variable names.
            Also fixes manual-tests/wml/task-refresh-in-anchor.wml, which happened to use 1-char variable names.
    
            * wml/WMLVariables.cpp:
            (WebCore::containsVariableReference):
    
    2009-07-28  Nikolas Zimmermann  <nikolas.zimmermann@torchmobile.com>
    
            Reviewed by George Staikos.
    
            [WML] Page title doesn't update after variable substitution, if it contained variables
            https://bugs.webkit.org/show_bug.cgi?id=27725
    
            Extend variable-reference-valid.html test to include one-character variable tests.
    
            * wml/resources/variable-reference-valid.js:
            (setupTestDocument):
            (prepareTest):
            (executeTest):
            * wml/variable-reference-valid-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46486 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/wml/resources/variable-reference-valid.js
+++ third_party/WebKit/LayoutTests/wml/resources/variable-reference-valid.js
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 createWMLTestCase("Tests valid variable references");
 
-var pElement1;
-var pElement2;
-var pElement3;
-var pElement4;
-
 function setupTestDocument() {
     var cardElement = testDocument.documentElement.firstChild;
 
@@ -31,6 +26,11 @@ function setupTestDocument() {
     setvarElement2.setAttribute("value", "TEST%20PASSED");
     refreshElement.appendChild(setvarElement2);
 
+    var setvarElement3 = createWMLElement("setvar");
+    setvarElement3.setAttribute("name", "v");
+    setvarElement3.setAttribute("value", "TEST PASSED");
+    refreshElement.appendChild(setvarElement3);
+
     pElement1 = createWMLElement("p");
     pElement1.textContent = "Result: $var";
     cardElement.appendChild(pElement1);
@@ -46,6 +46,14 @@ function setupTestDocument() {
     pElement4 = createWMLElement("p");
     pElement4.textContent = "Result: $(var2:unesc)";
     cardElement.appendChild(pElement4);
+
+    pElement5 = createWMLElement("p");
+    pElement5.textContent = "Result: $v";
+    cardElement.appendChild(pElement5);
+
+    pElement6 = createWMLElement("p");
+    pElement6.textContent = "Result: $(v:e)";
+    cardElement.appendChild(pElement6);
 }
 
 function prepareTest() {
@@ -53,7 +61,9 @@ function prepareTest() {
     shouldBeEqualToString("pElement2.textContent", "Result: $(var:e)");
     shouldBeEqualToString("pElement3.textContent", "Result: $(var2)");
     shouldBeEqualToString("pElement4.textContent", "Result: $(var2:unesc)");
- 
+    shouldBeEqualToString("pElement5.textContent", "Result: $v");
+    shouldBeEqualToString("pElement6.textContent", "Result: $(v:e)");
+
     startTest(25, 15);
 }
 
@@ -62,6 +72,8 @@ function executeTest() {
     shouldBeEqualToString("pElement2.textContent", "Result: TEST%20PASSED");
     shouldBeEqualToString("pElement3.textContent", "Result: TEST%20PASSED");
     shouldBeEqualToString("pElement4.textContent", "Result: TEST PASSED");
+    shouldBeEqualToString("pElement5.textContent", "Result: TEST PASSED");
+    shouldBeEqualToString("pElement6.textContent", "Result: TEST%20PASSED");
 
     completeTest();
 }
