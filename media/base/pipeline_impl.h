409fd0c0b9f28f850f33dfd6a7d949594a3d7fd5
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-28 20:36:37 +0000

    Implemented a proper clock for audio/video synchronization.
    More or less a change to pull out time management from PipelineImpl into a new class ClockImpl.  Biggest difference is ClockImpl will use the system clock + linear interpolation to provide a more "precise" representation of the current playback position.
    
    BUG=16508
    TEST=a/v sync should remain the same, currentTime should report more precise values
    
    Review URL: http://codereview.chromium.org/159517
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21882 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/pipeline_impl.h
+++ media/base/pipeline_impl.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ref_counted.h"
 #include "base/thread.h"
 #include "base/time.h"
+#include "media/base/clock_impl.h"
 #include "media/base/filter_host.h"
 #include "media/base/pipeline.h"
 
@@ -283,8 +284,10 @@ class PipelineImpl : public Pipeline, public FilterHost {
   // the filters.
   float playback_rate_;
 
-  // Current playback time.  Set by filters.
-  base::TimeDelta time_;
+  // Reference clock.  Keeps track of current playback time.  Uses system
+  // clock and linear interpolation, but can have its time manually set
+  // by filters.
+  ClockImpl clock_;
 
   // Status of the pipeline.  Initialized to PIPELINE_OK which indicates that
   // the pipeline is operating correctly. Any other value indicates that the
