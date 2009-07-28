0a3ebf5d887485d08349e4331fbcd935b65a8708
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-28 14:26:49 +0000

    2009-07-28  Simon Hausmann  <simon.hausmann@nokia.com>
            Rubber-stamped by Ariya Hidayat.
    
            Fix compilation with the precompiled header.
    
            * WebKit_pch.h: Don't include JSDOMBinding.h and MathObject.h,
            as they include AtomicString.h. AtomicString.cpp needs to enable
            a #define before including AtomicString.h, which breaks if the PCH
            forces the inclusion beforehand.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46482 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebKit_pch.h
+++ third_party/WebKit/WebKit/qt/WebKit_pch.h
@@ -78,7 +78,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/UnusedParam.h>
 #include <wtf/AlwaysInline.h>
 #include <wtf/GetPtr.h>
-
-#include "../../WebCore/bindings/js/JSDOMBinding.h"
-#include "../../JavaScriptCore/runtime/MathObject.h"
 #endif
