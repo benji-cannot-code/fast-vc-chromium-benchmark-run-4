3cec0b19571ddd211d4aeeca2d459b30d9b84de1
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-28 01:28:31 +0000

    Gtk: Implement font selection options.
    BUG=11507
    
    Review URL: http://codereview.chromium.org/159458
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21786 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/options/fonts_languages_window_gtk.cc
+++ chrome/browser/gtk/options/fonts_languages_window_gtk.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util.h"
 #include "base/message_loop.h"
 #include "chrome/browser/profile.h"
+#include "chrome/browser/gtk/options/fonts_page_gtk.h"
 #include "chrome/common/gtk_util.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
@@ -40,6 +41,9 @@ class FontsLanguagesWindowGtk {
   // The Profile associated with these options.
   Profile* profile_;
 
+  // The fonts page.
+  FontsPageGtk fonts_page_;
+
   DISALLOW_COPY_AND_ASSIGN(FontsLanguagesWindowGtk);
 };
 
@@ -52,7 +56,8 @@ FontsLanguagesWindowGtk::FontsLanguagesWindowGtk(Profile* profile)
       // Always show preferences for the original profile. Most state when off
       // the record comes from the original profile, but we explicitly use
       // the original profile to avoid potential problems.
-    : profile_(profile->GetOriginalProfile()) {
+    : profile_(profile->GetOriginalProfile()),
+      fonts_page_(profile_) {
   dialog_ = gtk_dialog_new_with_buttons(
       l10n_util::GetStringFUTF8(IDS_FONT_LANGUAGE_SETTING_WINDOWS_TITLE,
           l10n_util::GetStringUTF16(IDS_PRODUCT_NAME)).c_str(),
@@ -68,11 +73,12 @@ FontsLanguagesWindowGtk::FontsLanguagesWindowGtk(Profile* profile)
                       gtk_util::kContentAreaSpacing);
 
   notebook_ = gtk_notebook_new();
+  gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog_)->vbox), notebook_);
 
   // Fonts and Encoding tab.
   gtk_notebook_append_page(
       GTK_NOTEBOOK(notebook_),
-      gtk_label_new("TODO content"),
+      fonts_page_.get_page_widget(),
       gtk_label_new(
           l10n_util::GetStringUTF8(
               IDS_FONT_LANGUAGE_SETTING_FONT_TAB_TITLE).c_str()));
@@ -84,8 +90,6 @@ FontsLanguagesWindowGtk::FontsLanguagesWindowGtk(Profile* profile)
       gtk_label_new(
           l10n_util::GetStringUTF8(
               IDS_FONT_LANGUAGE_SETTING_LANGUAGES_TAB_TITLE).c_str()));
-
-  gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog_)->vbox), notebook_);
 
   // Show the notebook.
   gtk_widget_show_all(dialog_);
--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -1122,6 +1122,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/gtk/options/cookies_view.cc',
         'browser/gtk/options/cookies_view.h',
         'browser/gtk/options/fonts_languages_window_gtk.cc',
+        'browser/gtk/options/fonts_page_gtk.cc',
+        'browser/gtk/options/fonts_page_gtk.h',
         'browser/gtk/options/general_page_gtk.cc',
         'browser/gtk/options/general_page_gtk.h',
         'browser/gtk/options/options_layout_gtk.cc',
