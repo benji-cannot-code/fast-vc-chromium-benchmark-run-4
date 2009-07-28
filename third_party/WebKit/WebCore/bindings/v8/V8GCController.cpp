7b9d8588ed2e9ba9fc4323c465dc86405328ce75
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-28 06:05:44 +0000

    2009-07-25  Adam Barth  <abarth@webkit.org>
            Reviewed by David Levin.
    
            [V8] Split up V8DOMMap.cpp by class
            https://bugs.webkit.org/show_bug.cgi?id=27685
    
            No behavior change.  Just copy-and-paste.
    
            * WebCore.gypi:
            * bindings/v8/ChildThreadDOMData.cpp: Added.
            (WebCore::ChildThreadDOMData::ChildThreadDOMData):
            (WebCore::ChildThreadDOMData::getStore):
            * bindings/v8/ChildThreadDOMData.h: Added.
            * bindings/v8/DOMData.cpp: Added.
            (WebCore::DOMData::DOMData):
            (WebCore::DOMData::getCurrent):
            (WebCore::DOMData::getCurrentMainThread):
            (WebCore::DOMData::handleWeakObject):
            (WebCore::DOMData::ensureDeref):
            (WebCore::DOMData::derefObject):
            (WebCore::DOMData::derefDelayedObjects):
            (WebCore::DOMData::derefDelayedObjectsInCurrentThread):
            (WebCore::DOMData::removeObjectsFromWrapperMap):
            * bindings/v8/DOMData.h: Added.
            (WebCore::):
            * bindings/v8/DOMDataStore.cpp: Added.
            (WebCore::DOMDataStore::DOMDataStore):
            (WebCore::DOMDataStore::~DOMDataStore):
            (WebCore::DOMDataStore::allStores):
            (WebCore::DOMDataStore::allStoresMutex):
            (WebCore::DOMDataStore::getDOMWrapperMap):
            (WebCore::forget):
            (WebCore::DOMDataStore::weakDOMObjectCallback):
            (WebCore::DOMDataStore::weakActiveDOMObjectCallback):
            (WebCore::DOMDataStore::weakNodeCallback):
            (WebCore::DOMDataStore::weakSVGElementInstanceCallback):
            (WebCore::DOMDataStore::weakSVGObjectWithContextCallback):
            * bindings/v8/DOMDataStore.h: Added.
            (WebCore::DOMDataStore::):
            (WebCore::DOMDataStore::InternalDOMWrapperMap::InternalDOMWrapperMap):
            (WebCore::DOMDataStore::InternalDOMWrapperMap::forgetOnly):
            (WebCore::DOMDataStore::domData):
            (WebCore::DOMDataStore::domNodeMap):
            (WebCore::DOMDataStore::domObjectMap):
            (WebCore::DOMDataStore::activeDomObjectMap):
            (WebCore::DOMDataStore::domSvgElementInstanceMap):
            (WebCore::DOMDataStore::domSvgObjectWithContextMap):
            * bindings/v8/MainThreadDOMData.cpp: Added.
            (WebCore::MainThreadDOMData::MainThreadDOMData):
            (WebCore::MainThreadDOMData::getStore):
            * bindings/v8/MainThreadDOMData.h: Added.
            * bindings/v8/ScopedDOMDataStore.cpp: Added.
            (WebCore::ScopedDOMDataStore::ScopedDOMDataStore):
            (WebCore::ScopedDOMDataStore::~ScopedDOMDataStore):
            * bindings/v8/ScopedDOMDataStore.h: Added.
            * bindings/v8/StaticDOMDataStore.cpp: Added.
            (WebCore::StaticDOMDataStore::StaticDOMDataStore):
            * bindings/v8/StaticDOMDataStore.h: Added.
            * bindings/v8/V8DOMMap.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8GCController.cpp
+++ third_party/WebKit/WebCore/bindings/v8/V8GCController.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "V8GCController.h"
 
+#include "DOMDataStore.h"
 #include "DOMObjectsInclude.h"
 #include "V8DOMMap.h"
 #include "V8Index.h"
@@ -389,14 +390,14 @@ public:
     {
         V8ClassIndex::V8WrapperType type = V8DOMWrapper::domWrapperType(wrapper);
         switch (type) {
-#define MAKE_CASE(TYPE, NAME)                                           \
-        case V8ClassIndex::TYPE: {                                  \
-          NAME* impl = static_cast<NAME*>(object);                  \
-          if (impl->hasPendingActivity()) {                         \
-            ASSERT(!wrapper.IsWeak());                              \
-            wrapper.MakeWeak(impl, &weakActiveDOMObjectCallback);   \
-          }                                                         \
-          break;                                                    \
+#define MAKE_CASE(TYPE, NAME)                                                   \
+        case V8ClassIndex::TYPE: {                                              \
+          NAME* impl = static_cast<NAME*>(object);                              \
+          if (impl->hasPendingActivity()) {                                     \
+            ASSERT(!wrapper.IsWeak());                                          \
+            wrapper.MakeWeak(impl, &DOMDataStore::weakActiveDOMObjectCallback); \
+          }                                                                     \
+          break;                                                                \
         }
 ACTIVE_DOM_OBJECT_TYPES(MAKE_CASE)
         default:
