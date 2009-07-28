f33359fefa53b2676f06dbbbbe31e9bac87f1ad0
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-28 20:46:46 +0000

    Revert "Pipeline will execute a callback whenever an run-time error has happened."
    TBR=kylep
    
    Review URL: http://codereview.chromium.org/160287
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21885 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webmediaplayer_impl.h
+++ webkit/glue/webmediaplayer_impl.h
@@ -107,7 +107,6 @@ class WebMediaPlayerImpl : public WebKit::WebMediaPlayer,
     // WebMediaPlayerImpl.
     void PipelineInitializationCallback();
     void PipelineSeekCallback();
-    void PipelineErrorCallback();
 
    private:
     // Invoke |webmediaplayer_| to perform a repaint.
@@ -118,9 +117,6 @@ class WebMediaPlayerImpl : public WebKit::WebMediaPlayer,
 
     // Notify |webmediaplayer_| that a seek has finished.
     void PipelineSeekTask();
-
-    // Notify |webmediaplayer_| that a pipeline error has been set.
-    void PipelineErrorTask();
 
     // The render message loop where WebKit lives.
     MessageLoop* render_loop_;
@@ -217,8 +213,6 @@ class WebMediaPlayerImpl : public WebKit::WebMediaPlayer,
   void OnPipelineInitialize();
 
   void OnPipelineSeek();
-
-  void OnPipelineError();
 
  private:
   // Helpers that set the network/ready state and notifies the client if
