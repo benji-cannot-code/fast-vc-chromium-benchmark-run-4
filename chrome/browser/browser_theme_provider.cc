8920d4a8f90749bf902047b74f84272e33b5cc81
glen@chromium.org <glen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-28 01:37:19 +0000

    Generate tab images on browser load (previously we were only doing it on start).
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/159470
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21788 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_theme_provider.cc
+++ chrome/browser/browser_theme_provider.cc
@@ -931,6 +931,7 @@ void BrowserThemeProvider::LoadThemePrefs() {
         prefs->GetMutableDictionary(prefs::kCurrentThemeDisplayProperties));
     GenerateFrameColors();
     GenerateFrameImages();
+    GenerateTabImages();
     UserMetrics::RecordAction(L"Themes_loaded", profile_);
   }
 }
