91260b0cbcd4ac9ea58b439f8e3c758b957f2753
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-29 23:00:25 +0000

    GTK: move the findbar out of the way when it covers a find result.
    BUG=15875
    
    Review URL: http://codereview.chromium.org/160350
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22012 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/find_bar_controller.h
+++ chrome/browser/find_bar_controller.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_FIND_BAR_CONTROLLER_H_
 
 #include "base/basictypes.h"
+#include "base/gfx/rect.h"
 #include "base/scoped_ptr.h"
 #include "chrome/common/notification_registrar.h"
 
@@ -39,6 +40,13 @@ class FindBarController : public NotificationObserver {
                        const NotificationDetails& details);
 
   FindBar* find_bar() const { return find_bar_.get(); }
+
+  // Reposition |view_location| such that it avoids |avoid_overlapping_rect|,
+  // and return the new location.
+  static gfx::Rect GetLocationForFindbarView(
+      gfx::Rect view_location,
+      const gfx::Rect& dialog_bounds,
+      const gfx::Rect& avoid_overlapping_rect);
 
  private:
   // Sents an update to the find bar with the tab contents' current result. The
