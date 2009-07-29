b08cb033dad85a50e617a95ff38b2abf2f07fa83
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-29 23:23:25 +0000

    GTK Themes: Native omnibox part 2.
    Put the star and go/stop buttons inside the location bar drawing. Reverts my changes to
    location_bar_view_gtk.cc in the previous patch. (Showed mocks of this to Ben and Glen.)
    
    Also fixes rendering of the location bar background under Crux and other themes that
    draw an entry_bg.
    
    Review URL: http://codereview.chromium.org/159610
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22018 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_toolbar_gtk.h
+++ chrome/browser/gtk/browser_toolbar_gtk.h
@@ -115,6 +115,9 @@ class BrowserToolbarGtk : public CommandUpdater::CommandObserver,
   // Gtk callback for the "expose-event" signal.
   static gboolean OnToolbarExpose(GtkWidget* widget, GdkEventExpose* e,
                                   BrowserToolbarGtk* toolbar);
+  static gboolean OnLocationHboxExpose(GtkWidget* omnibox_hbox,
+                                       GdkEventExpose* e,
+                                       BrowserToolbarGtk* toolbar);
 
   // Gtk callback for the "clicked" signal.
   static void OnButtonClick(GtkWidget* button, BrowserToolbarGtk* toolbar);
@@ -205,6 +208,10 @@ class BrowserToolbarGtk : public CommandUpdater::CommandObserver,
   int last_release_event_flags_;
 
   NotificationRegistrar registrar_;
+
+  // A GtkEntry that isn't part of the hierarchy. We keep this for native
+  // rendering.
+  OwnedWidgetGtk offscreen_entry_;
 
   DISALLOW_COPY_AND_ASSIGN(BrowserToolbarGtk);
 };
