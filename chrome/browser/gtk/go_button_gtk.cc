d2b7498c2cd9900b6fff631c269b923e024dfd41
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-29 17:38:10 +0000

    GTK Themes: Native location bar area.
    The combined star/location bar/go construct doesn't look native. In
    GTK mode, make the star and go buttons act like toolbar buttons, and
    draw a GTK text entry widget onto the toolbar. The omnibox popup
    is just the size of the entry when in GTK theme mode.
    
    There's still a lot of work to be done on this; I want to properly
    draw focus rings, have the rest of the location box use theme colors, et cetera, but this is less broken then what's currently
    there.
    
    Review URL: http://codereview.chromium.org/159532
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21965 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/go_button_gtk.cc
+++ chrome/browser/gtk/go_button_gtk.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/browser/browser.h"
+#include "chrome/browser/gtk/gtk_chrome_button.h"
 #include "chrome/browser/gtk/gtk_theme_provider.h"
 #include "chrome/browser/gtk/location_bar_view_gtk.h"
 #include "chrome/browser/profile.h"
@@ -29,7 +30,7 @@ GoButtonGtk::GoButtonGtk(LocationBarViewGtk* location_bar, Browser* browser)
                       GtkThemeProvider::GetFrom(browser->profile()) : NULL),
       go_(theme_provider_, IDR_GO, IDR_GO_P, IDR_GO_H, 0),
       stop_(theme_provider_, IDR_STOP, IDR_STOP_P, IDR_STOP_H, 0),
-      widget_(gtk_button_new()) {
+      widget_(gtk_chrome_button_new()) {
   gtk_widget_set_size_request(widget_.get(),
                               gdk_pixbuf_get_width(go_.pixbufs(0)),
                               gdk_pixbuf_get_height(go_.pixbufs(0)));
@@ -200,7 +201,9 @@ void GoButtonGtk::SetTooltip() {
 }
 
 void GoButtonGtk::UpdateThemeButtons() {
-  if (theme_provider_ && theme_provider_->UseGtkTheme()) {
+  bool use_gtk = theme_provider_ && theme_provider_->UseGtkTheme();
+
+  if (use_gtk) {
     // TODO(erg): Waiting for Glen to make a version of these that don't have a
     // button border on it.
     if (intended_mode_ == MODE_GO) {
@@ -225,4 +228,7 @@ void GoButtonGtk::UpdateThemeButtons() {
     // We effectively double-buffer by virtue of having only one image...
     gtk_widget_set_double_buffered(widget_.get(), FALSE);
   }
+
+  gtk_chrome_button_set_use_gtk_rendering(
+      GTK_CHROME_BUTTON(widget_.get()), use_gtk);
 }
--- chrome/browser/gtk/toolbar_star_toggle_gtk.cc
+++ chrome/browser/gtk/toolbar_star_toggle_gtk.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gfx/rect.h"
 #include "chrome/browser/gtk/bookmark_bubble_gtk.h"
 #include "chrome/browser/gtk/browser_toolbar_gtk.h"
+#include "chrome/browser/gtk/gtk_chrome_button.h"
 #include "chrome/browser/gtk/gtk_theme_provider.h"
 #include "chrome/browser/profile.h"
 #include "chrome/common/notification_service.h"
@@ -16,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 ToolbarStarToggleGtk::ToolbarStarToggleGtk(BrowserToolbarGtk* host)
     : host_(host),
-      widget_(gtk_button_new()),
+      widget_(gtk_chrome_button_new()),
       is_starred_(false),
       theme_provider_(GtkThemeProvider::GetFrom(host->profile())),
       unstarred_(theme_provider_, IDR_STAR, IDR_STAR_P, IDR_STAR_H, IDR_STAR_D),
@@ -91,7 +92,9 @@ gboolean ToolbarStarToggleGtk::OnExpose(GtkWidget* widget, GdkEventExpose* e,
 }
 
 void ToolbarStarToggleGtk::UpdateGTKButton() {
-  if (theme_provider_->UseGtkTheme()) {
+  bool use_gtk = theme_provider_ && theme_provider_->UseGtkTheme();
+
+  if (use_gtk) {
     GdkPixbuf* pixbuf = NULL;
     if (is_starred_) {
       pixbuf = theme_provider_->GetPixbufNamed(IDR_STARRED_NOBORDER);
@@ -115,4 +118,7 @@ void ToolbarStarToggleGtk::UpdateGTKButton() {
     // We effectively double-buffer by virtue of having only one image...
     gtk_widget_set_double_buffered(widget_.get(), FALSE);
   }
+
+  gtk_chrome_button_set_use_gtk_rendering(
+      GTK_CHROME_BUTTON(widget_.get()), use_gtk);
 }
