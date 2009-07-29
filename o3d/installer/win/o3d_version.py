212a300343cbedd1b313020fb6d314970785d7d6
bradnelson@google.com <bradnelson@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-29 22:41:23 +0000

    Version number bump for release
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22008 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/installer/win/o3d_version.py
+++ o3d/installer/win/o3d_version.py
@@ -3,5 +3,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2008-9 Google Inc. All Rights Reserved.
 
 # version = (major, minor, trunk, patch)
-plugin_version = (0, 1, 40, 0)
+plugin_version = (0, 1, 41, 0)
 sdk_version = plugin_version
