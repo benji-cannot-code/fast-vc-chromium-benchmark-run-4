55037ef7f40e585eaffcf0244a17b3f4ddefce4d
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-29 23:29:06 +0000

    2009-07-29  Darin Adler  <darin@apple.com>
            Reviewed by Dan Bernstein.
    
            Illegal values for <ol start> cause list numbering start at 0 (should start at 1)
            https://bugs.webkit.org/show_bug.cgi?id=27810
    
            Test: fast/lists/ol-start-parsing.html
    
            * html/HTMLOListElement.cpp:
            (WebCore::HTMLOListElement::parseMappedAttribute): Check the result of toInt
            and use the value 1 if it failed to parse. Before we were getting toInt's default
            behavior, which is to return 0.
    
    2009-07-29  Darin Adler  <darin@apple.com>
    
            Reviewed by Dan Bernstein.
    
            Illegal values for <ol start> cause list numbering start at 0 (should start at 1)
            https://bugs.webkit.org/show_bug.cgi?id=27810
    
            Couldn't make a text-only test because list numbers do not show up in
            text dumps. This matches the other fast/lists tests.
    
            * fast/lists/ol-start-parsing.html: Added.
            * platform/mac/fast/lists/ol-start-parsing-expected.checksum: Added.
            * platform/mac/fast/lists/ol-start-parsing-expected.png: Added.
            * platform/mac/fast/lists/ol-start-parsing-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLOListElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLOListElement.cpp
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/**
+/*
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *
@@ -64,12 +64,16 @@ void HTMLOListElement::parseMappedAttribute(MappedAttribute* attr)
         else if (attr->value() == "1")
             addCSSProperty(attr, CSSPropertyListStyleType, CSSValueDecimal);
     } else if (attr->name() == startAttr) {
-        int s = !attr->isNull() ? attr->value().toInt() : 1;
-        if (s != m_start) {
-            m_start = s;
-            for (RenderObject* r = renderer(); r; r = r->nextInPreOrder(renderer()))
-                if (r->isListItem())
-                    static_cast<RenderListItem*>(r)->updateValue();
+        bool canParse;
+        int start = attr->value().toInt(&canParse);
+        if (!canParse)
+            start = 1;
+        if (start == m_start)
+            return;
+        m_start = start;
+        for (RenderObject* child = renderer(); child; child = child->nextInPreOrder(renderer())) {
+            if (child->isListItem())
+                static_cast<RenderListItem*>(child)->updateValue();
         }
     } else
         HTMLElement::parseMappedAttribute(attr);
