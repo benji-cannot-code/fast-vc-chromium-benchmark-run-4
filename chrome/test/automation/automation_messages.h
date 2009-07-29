f8042f705d14ea266a09d9dc486d8c73eb90fc92
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-29 00:22:37 +0000

    Cause POST data to be marhsaled across the automation interface when using AutomationMsg_RequestStart.
    Review URL: http://codereview.chromium.org/159228
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21930 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automation/automation_messages.h
+++ chrome/test/automation/automation_messages.h
@@ -14,7 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/security_style.h"
 #include "chrome/common/common_param_traits.h"
 #include "chrome/test/automation/automation_constants.h"
-#include "ipc/ipc_message_utils.h"
+#include "net/base/upload_data.h"
+
 
 struct AutomationMsg_Find_Params {
   // Unused value, which exists only for backwards compat.
@@ -231,6 +232,7 @@ struct AutomationURLRequest {
   std::string method;
   std::string referrer;
   std::string extra_request_headers;
+  scoped_refptr<net::UploadData> upload_data;
 };
 
 // Traits for AutomationURLRequest structure to pack/unpack.
@@ -242,12 +244,14 @@ struct ParamTraits<AutomationURLRequest> {
     WriteParam(m, p.method);
     WriteParam(m, p.referrer);
     WriteParam(m, p.extra_request_headers);
+    WriteParam(m, p.upload_data);
   }
   static bool Read(const Message* m, void** iter, param_type* p) {
     return ReadParam(m, iter, &p->url) &&
            ReadParam(m, iter, &p->method) &&
            ReadParam(m, iter, &p->referrer) &&
-           ReadParam(m, iter, &p->extra_request_headers);
+           ReadParam(m, iter, &p->extra_request_headers) &&
+           ReadParam(m, iter, &p->upload_data);
   }
   static void Log(const param_type& p, std::wstring* l) {
     l->append(L"(");
@@ -258,6 +262,8 @@ struct ParamTraits<AutomationURLRequest> {
     LogParam(p.referrer, l);
     l->append(L", ");
     LogParam(p.extra_request_headers, l);
+    l->append(L", ");
+    LogParam(p.upload_data, l);
     l->append(L")");
   }
 };
