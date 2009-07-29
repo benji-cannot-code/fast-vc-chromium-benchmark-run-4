5c893c1e8c23cda60b50da16b1720d53f9d28c53
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-29 00:16:53 +0000

    WebCore:
            Reviewed by Simon Fraser.
    
            [CSS3 Backgrounds and Borders] Drop the prefix from the border-radius
            properties
            https://bugs.webkit.org/show_bug.cgi?id=27578
    
            [CSS3 Backgrounds and Borders] Handle the / and 4 values in
            border-radius
            https://bugs.webkit.org/show_bug.cgi?id=27584
    
            Test: fast/borders/border-radius-parsing.html
    
            Dropped the -webkit- prefix from the individual corner border-radius
            properties. Added border-radius with the / and 4-value syntax, and
            maintained -webkit-border-radius with legacy 2-value syntax.
    
            * css/CSSComputedStyleDeclaration.cpp:
            (computedProperties): Removed Webkit prefix.
            (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue): Ditto.
            * css/CSSParser.cpp:
            (WebCore::CSSParser::parseValue): Removed Webkit prefix from the
                single-corner properties. Call out to parseBorderRadius() to parse
                border-radius and -webkit-border-radius. 
            (WebCore::completeBorderRadii): Added this helper function that
                completes the values for all four corners when fewer than four are
                specified.
            (WebCore::CSSParser::parseBorderRadius): Added.
            (WebCore::cssPropertyID): Map -webkit-border-*-*-radius to the
                unprefixed property.
            * css/CSSParser.h:
            * css/CSSPropertyLonghand.cpp:
            (WebCore::initShorthandMap): Removed the Webkit prefix. Added an entry
                for border-radius. Kept the entry for -webkit-border-radius.
            * css/CSSPropertyNames.in: Removed the -webkit- prefix from the
                single-corenr properties. Added border-radius. Kept
                -webkit-border-radius because of its conflicting syntax.
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::applyProperty): Removed the Webkit prefix.
            * page/animation/AnimationBase.cpp:
            (WebCore::ensurePropertyMap): Ditto.
    
    LayoutTests:
    
            Reviewed by Simon Fraser.
    
            New test, updated test, and updated test results for:
    
            [CSS3 Backgrounds and Borders] Drop the prefix from the border-radius
            properties
            https://bugs.webkit.org/show_bug.cgi?id=27578
    
            [CSS3 Backgrounds and Borders] Handle the / and 4 values in
            border-radius
            https://bugs.webkit.org/show_bug.cgi?id=27584
    
            * fast/borders/border-radius-parsing-expected.txt: Added.
            * fast/borders/border-radius-parsing.html: Added.
            * fast/css/remove-shorthand-expected.txt:
            * fast/css/remove-shorthand.html: Added a test for border-radius.
            * platform/mac/fast/css/getComputedStyle/computed-style-expected.txt:
            * platform/mac/fast/css/getComputedStyle/computed-style-without-renderer-expected.txt:
            * svg/css/getComputedStyle-basic-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46508 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSComputedStyleDeclaration.cpp
+++ third_party/WebKit/WebCore/css/CSSComputedStyleDeclaration.cpp
@@ -60,6 +60,8 @@ static const int computedProperties[] = {
     CSSPropertyBackgroundPosition, // more-specific background-position-x/y are non-standard
     CSSPropertyBackgroundRepeat,
     CSSPropertyBorderBottomColor,
+    CSSPropertyBorderBottomLeftRadius,
+    CSSPropertyBorderBottomRightRadius,
     CSSPropertyBorderBottomStyle,
     CSSPropertyBorderBottomWidth,
     CSSPropertyBorderCollapse,
@@ -70,6 +72,8 @@ static const int computedProperties[] = {
     CSSPropertyBorderRightStyle,
     CSSPropertyBorderRightWidth,
     CSSPropertyBorderTopColor,
+    CSSPropertyBorderTopLeftRadius,
+    CSSPropertyBorderTopRightRadius,
     CSSPropertyBorderTopStyle,
     CSSPropertyBorderTopWidth,
     CSSPropertyBottom,
@@ -153,13 +157,9 @@ static const int computedProperties[] = {
     CSSPropertyWebkitBackgroundComposite,
     CSSPropertyWebkitBackgroundOrigin,
     CSSPropertyWebkitBackgroundSize,
-    CSSPropertyWebkitBorderBottomLeftRadius,
-    CSSPropertyWebkitBorderBottomRightRadius,
     CSSPropertyWebkitBorderFit,
     CSSPropertyWebkitBorderHorizontalSpacing,
     CSSPropertyWebkitBorderImage,
-    CSSPropertyWebkitBorderTopLeftRadius,
-    CSSPropertyWebkitBorderTopRightRadius,
     CSSPropertyWebkitBorderVerticalSpacing,
     CSSPropertyWebkitBoxAlign,
     CSSPropertyWebkitBoxDirection,
@@ -1226,13 +1226,13 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(int proper
             return CSSPrimitiveValue::create(style->userDrag());
         case CSSPropertyWebkitUserSelect:
             return CSSPrimitiveValue::create(style->userSelect());
-        case CSSPropertyWebkitBorderBottomLeftRadius:
+        case CSSPropertyBorderBottomLeftRadius:
             return getBorderRadiusCornerValue(style->borderBottomLeftRadius());
-        case CSSPropertyWebkitBorderBottomRightRadius:
+        case CSSPropertyBorderBottomRightRadius:
             return getBorderRadiusCornerValue(style->borderBottomRightRadius());
-        case CSSPropertyWebkitBorderTopLeftRadius:
+        case CSSPropertyBorderTopLeftRadius:
             return getBorderRadiusCornerValue(style->borderTopLeftRadius());
-        case CSSPropertyWebkitBorderTopRightRadius:
+        case CSSPropertyBorderTopRightRadius:
             return getBorderRadiusCornerValue(style->borderTopRightRadius());
         case CSSPropertyClip: {
             if (!style->hasClip())
@@ -1298,6 +1298,7 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(int proper
         case CSSPropertyBorderBottom:
         case CSSPropertyBorderColor:
         case CSSPropertyBorderLeft:
+        case CSSPropertyBorderRadius:
         case CSSPropertyBorderRight:
         case CSSPropertyBorderStyle:
         case CSSPropertyBorderTop:
