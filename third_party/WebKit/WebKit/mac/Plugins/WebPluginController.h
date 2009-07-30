8a4259ae2c9a51e2bf84826a6ed054dc2c864264
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-30 00:08:20 +0000

    <http://webkit.org/b/27788> Don't export WebPluginController.h as a private header
            Reviewed by Mark Rowe.
    
    WebKit:
    
            WebPluginController.h includes WebPluginContainerCheck.h, which
            is not a private header.  Since WebPluginController.h doesn't
            appear to be used anywhere, remove its private header status.
    
            * WebKit.xcodeproj/project.pbxproj: Remove private header
            attribute from WebPluginController.h.
    
    WebKit/mac:
    
            * Plugins/WebPluginController.h: Changed #import of
            WebPluginContainerCheck.h to use a framework-style include in
            case other platforms wish to export WebPluginController.h as a
            private header.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebPluginController.h
+++ third_party/WebKit/WebKit/mac/Plugins/WebPluginController.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #import <WebKit/WebBasePluginPackage.h>
-#import "WebPluginContainerCheck.h"
+#import <WebKit/WebPluginContainerCheck.h>
 
 @class WebFrame;
 @class WebHTMLView;
