743eb66d9baa3f5b2907a300e78aeb7b2680bc4d
mdm@chromium.org <mdm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-30 20:00:02 +0000

    gcc >= 4.3 fixes Patch from joel.stan@gmail.com: http://codereview.chromium.org/160377 BUG=none TEST=none
    Review URL: http://codereview.chromium.org/159648
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22085 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_util.cc
+++ net/base/net_util.cc
@@ -639,7 +639,7 @@ bool IsIDNComponentSafe(const char16* str,
   std::string languages_list(WideToASCII(languages));
   StringTokenizer t(languages_list, ",");
   while (t.GetNext()) {
-    if (safe = IsComponentCoveredByLang(component_characters, t.token()))
+    if ((safe = IsComponentCoveredByLang(component_characters, t.token())))
       break;
   }
   return safe;
