7f893321c7501124f8a024c7a0a2950b4c020601
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-30 16:31:57 +0000

    [Qt] Fix build with Qt 4.4 after r46535.
    git-svn-id: svn://svn.chromium.org/blink/trunk@46584 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/ResourceHandleQt.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/ResourceHandleQt.cpp
@@ -44,13 +44,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "NotImplemented.h"
 
+#if QT_VERSION >= 0x040500
+#include <QAbstractNetworkCache>
+#endif
 #include <QCoreApplication>
 #include <QUrl>
 #if QT_VERSION >= 0x040400
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 #include <QNetworkReply>
-#include <QAbstractNetworkCache>
 #else
 #include "qwebnetworkinterface_p.h"
 #endif
