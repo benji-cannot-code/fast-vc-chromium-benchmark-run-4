4e2fd03c8d4e189de7a6844f4350dfd21f465b72
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-31 07:51:32 +0000

    Integrate OCSP handlers in SSLClientSocketNSS. It depends on http://codereview.chromium.org/126046
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/155912
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22147 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.cc
+++ net/socket/ssl_client_socket_nss.cc
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
 #include "net/base/ssl_info.h"
+#include "net/ocsp/nss_ocsp.h"
 
 static const int kRecvBufferSize = 4096;
 
@@ -215,8 +216,12 @@ SSLClientSocketNSS::~SSLClientSocketNSS() {
 
 int SSLClientSocketNSS::Init() {
   EnterFunction("");
-  // Call NSS_NoDB_Init() in a threadsafe way.
+  // Initialize NSS in a threadsafe way.
   base::EnsureNSSInit();
+  // We must call EnsureOCSPInit() here, on the IO thread, to get the IO loop
+  // by MessageLoopForIO::current().
+  // X509Certificate::Verify() runs on a worker thread of CertVerifier.
+  EnsureOCSPInit();
 
   LeaveFunction("");
   return OK;
