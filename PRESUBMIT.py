21594641ed7a99ba8f335431dcf11c0928c5f338
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-31 18:09:56 +0000

    Dummy checkin to trigger the bots
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22160 0039d316-1c4b-4281-b951-d872f2087c98

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Top-level presubmit script for Chromium.
 
-See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts for
-details on the presubmit API built into gcl.
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for details on the presubmit API built into gcl.
 """
 
 EXCLUDED_PATHS = (
