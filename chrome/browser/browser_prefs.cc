ef6cd95293efad95476547ab2ce02f2f7130ec56
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-31 21:52:44 +0000

    Linux: Autodetect whether the custom frame should be used.
    Listing a bunch of strings from WMs' source sucks,
    but I can't think of any way to detect tiling WMs that
    don't support the EWMH (see the bug and the comment in
    this CL), so this seems safer.
    
    Tested with the following:
    - wiped out related prefs and started chrome under ion3;
      custom frame not used
    - quit and started under metacity; custom frame *was* used
    - toggled custom frame pref off and restarted under
      metacity again; custom frame not used
    - toggled it back on and restarted under ion3; custom frame
      was used
    
    BUG=15861
    TEST=see above
    
    Review URL: http://codereview.chromium.org/160374
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22193 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_prefs.cc
+++ chrome/browser/browser_prefs.cc
@@ -31,12 +31,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/frame/browser_view.h"
 #endif
 
-#if defined(TOOLKIT_VIEWS) || defined(OS_LINUX)
+#if defined(OS_WIN) || defined(OS_LINUX)
 #include "chrome/browser/task_manager.h"
 #endif
 
 #if defined(OS_WIN)
 #include "chrome/browser/views/keyword_editor_view.h"
+#endif
+
+#if defined(TOOLKIT_GTK)
+#include "chrome/browser/gtk/browser_window_gtk.h"
 #endif
 
 namespace browser {
@@ -57,7 +61,7 @@ void RegisterAllPrefs(PrefService* user_prefs, PrefService* local_state) {
 #if defined(TOOLKIT_VIEWS)  // TODO(port): whittle this down as we port
   BrowserView::RegisterBrowserViewPrefs(local_state);
 #endif
-#if defined(TOOLKIT_VIEWS) || defined(OS_LINUX)
+#if defined(OS_WIN) || defined(OS_LINUX)
   TaskManager::RegisterPrefs(local_state);
 #endif
 
@@ -75,6 +79,9 @@ void RegisterAllPrefs(PrefService* user_prefs, PrefService* local_state) {
   NewTabUI::RegisterUserPrefs(user_prefs);
   BlockedPopupContainer::RegisterUserPrefs(user_prefs);
   DevToolsManager::RegisterUserPrefs(user_prefs);
+#if defined(TOOLKIT_GTK)
+  BrowserWindowGtk::RegisterUserPrefs(user_prefs);
+#endif
 }
 
 }  // namespace browser
