ace8798ca0074fc6450cd5bcc0bd3032ff4b4e42
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-31 19:46:53 +0000

    Implement FindBarGtk::StopAnimation().
    Not convinced it's 100% necessary but it can't hurt.
    
    BUG=11979
    
    Review URL: http://codereview.chromium.org/159715
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22170 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/slide_animator_gtk.h
+++ chrome/browser/gtk/slide_animator_gtk.h
@@ -62,6 +62,9 @@ class SlideAnimatorGtk : public AnimationDelegate {
   // Slide shut.
   void Close();
 
+  // End the current animation.
+  void End();
+
   // Immediately hide the widget.
   void CloseWithoutAnimation();
 
