f0a17410a570ddcb563dacdb2042b2c40d2db8ff
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-31 20:20:35 +0000

    Fixes possible crash in bookmark menus. Specifically if you had a menu showing and brought up the context menu and clicked a separator we would do the wrong thing. We would try to show the first menu again, without cancelling out of the inner loop, which would most likely result in a crash. The right thing to do in this case is not close the context menu (this behavior can be seen in windows as well).
    BUG=17862
    TEST=see bug, but also covered by ui test now.
    
    Review URL: http://codereview.chromium.org/160458
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22180 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/menu/chrome_menu.cc
+++ views/controls/menu/chrome_menu.cc
@@ -1791,7 +1791,7 @@ void MenuController::OnMousePressed(SubmenuView* source,
 
   bool open_submenu = false;
   if (!part.menu) {
-    part.menu = source->GetMenuItem();
+    part.menu = part.parent;
     open_submenu = true;
   } else {
     if (part.menu->GetDelegate()->CanDrag(part.menu)) {
@@ -2340,6 +2340,8 @@ bool MenuController::GetMenuPartByScreenCoordinateImpl(
     View::ConvertPointToView(NULL, menu, &menu_loc);
     part->menu = GetMenuItemAt(menu, menu_loc.x(), menu_loc.y());
     part->type = MenuPart::MENU_ITEM;
+    if (!part->menu)
+      part->parent = menu->GetMenuItem();
     return true;
   }
 
