8518e563be8535a8a1ca82b557e9099e879ceff4
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-31 18:51:04 +0000

    Use real KVO notification for the default browser text.
    BUG=none
    TEST=make sure the default browser explanatory text stays working.
    
    Review URL: http://codereview.chromium.org/159708
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22164 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/preferences_window_controller.mm
+++ chrome/browser/cocoa/preferences_window_controller.mm
@@ -65,7 +65,6 @@ std::wstring GetNewTabUIURLString() {
 - (void)setRestoreOnStartupIndex:(NSInteger)type;
 - (void)setShowHomeButton:(BOOL)value;
 - (void)setShowPageOptionsButtons:(BOOL)value;
-- (void)setDefaultBrowser:(BOOL)value;
 - (void)setPasswordManagerEnabledIndex:(NSInteger)value;
 - (void)setFormAutofillEnabledIndex:(NSInteger)value;
 - (void)setShowAlternateErrorPages:(BOOL)value;
@@ -545,6 +544,8 @@ enum { kHomepageNewTabPage, kHomepageURL };
 // Called when the user clicks the button to make Chromium the default
 // browser. Registers http and https.
 - (IBAction)makeDefaultBrowser:(id)sender {
+  [self willChangeValueForKey:@"defaultBrowser"];
+
   ShellIntegration::SetAsDefaultBrowser();
   [self recordUserAction:L"Options_SetAsDefaultBrowser"];
   // If the user made Chrome the default browser, then he/she arguably wants
@@ -552,13 +553,7 @@ enum { kHomepageNewTabPage, kHomepageURL };
   prefs_->SetBoolean(prefs::kCheckDefaultBrowser, true);
 
   // Tickle KVO so that the UI updates.
-  [self setDefaultBrowser:YES];
-}
-
-// A stub setter so that we can trick KVO into thinking the UI needs
-// to be updated.
-- (void)setDefaultBrowser:(BOOL)ignore {
-  // Do nothing.
+  [self didChangeValueForKey:@"defaultBrowser"];
 }
 
 // Returns if Chromium is the default browser.
