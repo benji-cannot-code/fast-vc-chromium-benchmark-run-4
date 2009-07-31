b6b7b48f615eac8182697d6a13d797c75f1f8b02
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-07-31 06:37:40 +0000

    2009-07-30  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Jan Alonzo.
    
            Fix compiler warning.
    
            GCC does not like C++-style comments in preprocessor directives.
    
            * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46623 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -564,7 +564,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 #define WTF_USE_JSVALUE32 1
 #endif
-#endif // !defined(WTF_USE_JSVALUE64) && !defined(WTF_USE_JSVALUE32) && !defined(WTF_USE_JSVALUE32_64)
+#endif /* !defined(WTF_USE_JSVALUE64) && !defined(WTF_USE_JSVALUE32) && !defined(WTF_USE_JSVALUE32_64) */
 
 #if !defined(ENABLE_REPAINT_THROTTLING)
 #define ENABLE_REPAINT_THROTTLING 0
