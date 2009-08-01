79441dfbf2fc08f2d25841b944a0ed956f3376c0
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-01 07:51:19 +0000

    2009-08-01  Zoltan Horvath  <hzoltan@inf.u-szeged.hu>
            Reviewed by Darin Adler.
    
            Allow custom memory allocation control for JavaScriptCore's RegExpConstructorPrivate struct
            https://bugs.webkit.org/show_bug.cgi?id=27833
    
            Inherits RegExpConstructorPrivate class from FastAllocBase because it has been
            instantiated by 'new' in JavaScriptCore/runtime/RegExpConstructor.cpp:152.
    
            * runtime/RegExpConstructor.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46667 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/RegExpConstructor.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/RegExpConstructor.cpp
@@ -90,7 +90,7 @@ const ClassInfo RegExpConstructor::info = { "Function", &InternalFunction::info,
 @end
 */
 
-struct RegExpConstructorPrivate {
+struct RegExpConstructorPrivate : FastAllocBase {
     // Global search cache / settings
     RegExpConstructorPrivate()
         : lastNumSubPatterns(0)
