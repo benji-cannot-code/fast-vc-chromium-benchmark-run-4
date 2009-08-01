c86f65f2219beb56645b0c808a610330c30dae3a
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-01 02:58:26 +0000

    2009-07-31  Jon Honeycutt  <jhoneycutt@apple.com>
            Windows build fix!
    
            Unreviewed.
    
            * page/win/PageWin.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46654 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/win/PageWin.cpp
+++ third_party/WebKit/WebCore/page/win/PageWin.cpp
@@ -28,8 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Page.h"
 
 #include "Frame.h"
+#include "FrameLoaderClient.h"
 #include "FrameView.h"
 #include "FloatRect.h"
+#include "PluginView.h"
 #include <windows.h>
 
 namespace WebCore {
