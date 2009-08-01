abc4058eb488a724e0842260d0e63344ce9a3196
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-01 05:26:11 +0000

    2009-07-31  Yong Li  <yong.li@torchmobile.com>
            Reviewed by George Staikos.
    
            Resurrect the old GetTickCount implementation of currentTime, controlled by WTF_USE_QUERY_PERFORMANCE_COUNTER
            currentSystemTime taken from older WebKit; currentTime written by Yong Li <yong.li@torchmobile.com>; cleanup by Joe Mason <joe.mason@torchmobile.com>
            https://bugs.webkit.org/show_bug.cgi?id=27848
    
            * wtf/CurrentTime.cpp:
            (WTF::currentSystemTime): get current time with GetCurrentFT
            (WTF::currentTime): track msec elapsed since first currentSystemTime call using GetTickCount
            * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46660 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -339,6 +339,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_JSC_MULTIPLE_THREADS 1
 #endif
 
+/* On Windows, use QueryPerformanceCounter by default */
+#if PLATFORM(WIN_OS)
+#define WTF_USE_QUERY_PERFORMANCE_COUNTER  1
+#endif
+
 #if PLATFORM(WINCE) && !PLATFORM(QT)
 #undef ENABLE_JSC_MULTIPLE_THREADS
 #define ENABLE_JSC_MULTIPLE_THREADS        0
