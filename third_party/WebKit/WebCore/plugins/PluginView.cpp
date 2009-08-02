c92d134988fcbd91ef9b73c99e45573693167cea
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-02 14:00:29 +0000

    2009-07-27  Tor Arne Vestbø  <tor.arne.vestbo@nokia.com>
            Reviewed by Anders Carlsson.
    
            Unregister plugin from PluginMainThreadScheduler if NPP_New fails
    
            https://bugs.webkit.org/show_bug.cgi?id=27706
    
            Since the plugin has not been started yet (m_isStarted is not set),
            calling stop() will not have any affect, so we manually unregister
            in start() before returning.
    
            * plugins/PluginView.cpp:
            (WebCore::PluginView::start):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/PluginView.cpp
+++ third_party/WebKit/WebCore/plugins/PluginView.cpp
@@ -173,6 +173,7 @@ bool PluginView::start()
 
     if (npErr != NPERR_NO_ERROR) {
         m_status = PluginStatusCanNotLoadPlugin;
+        PluginMainThreadScheduler::scheduler().unregisterPlugin(m_instance);
         return false;
     }
 
