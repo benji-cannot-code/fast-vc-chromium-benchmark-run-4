37efe5bdc2db49a85baab5f415158632d0661e73
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-02 01:41:17 +0000

    wtf/Threading.h: added include of Platform.h
            Reviewed by Mark Rowe.
    
            * wtf/Threading.h: Added #include "Platform.h" since this header
            uses PLATFORM() and other macros.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Threading.h
+++ third_party/WebKit/JavaScriptCore/wtf/Threading.h
@@ -60,6 +60,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef Threading_h
 #define Threading_h
 
+#include "Platform.h"
+
 #if PLATFORM(WINCE)
 #include <windows.h>
 #endif
