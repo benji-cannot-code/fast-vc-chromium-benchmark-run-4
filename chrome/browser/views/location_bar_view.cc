83dc3a51b6734da779af717ad3c428714449e1b8
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-02 19:35:20 +0000

    LocationBarView::PageActionImageView::LoadImageTask::Run() may pass a NULL image pointer through to OnImageLoaded; make sure that the pointer is not dereferenced in these cases.
    BUG= http://crbug.com/18140
    TEST= none.
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=22213
    Review URL: http://codereview.chromium.org/159711
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22254 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/location_bar_view.cc
+++ chrome/browser/views/location_bar_view.cc
@@ -1161,8 +1161,10 @@ class LocationBarView::PageActionImageView::ImageLoadingTracker
   }
 
   void OnImageLoaded(SkBitmap* image, int index) {
-    if (image == NULL)
+    if (image == NULL) {
+      NOTREACHED();
       image = new SkBitmap();
+    }
     if (view_)
       view_->OnImageLoaded(image, index);
     delete image;
