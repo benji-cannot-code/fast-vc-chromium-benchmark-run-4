b669b72dfe9784b129377cd64d485b86f2b3db13
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-02 21:45:45 +0000

    2009-08-02  Geoffrey Garen  <ggaren@apple.com>
            Windows build fix: Nixed JSImmediate::prototype, JSImmediate::toObject,
            and JSImmediate::toThisObject, and removed their exported symbols.
    
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def:
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore_debug.def:
            * runtime/JSImmediate.cpp:
            * runtime/JSImmediate.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSImmediate.h
+++ third_party/WebKit/JavaScriptCore/runtime/JSImmediate.h
@@ -270,8 +270,6 @@ namespace JSC {
 
         static double toDouble(JSValue);
         static bool toBoolean(JSValue);
-        static JSObject* toObject(JSValue, ExecState*);
-        static JSObject* toThisObject(JSValue, ExecState*);
 
         static bool getUInt32(JSValue, uint32_t&);
         static bool getTruncatedInt32(JSValue, int32_t&);
@@ -286,8 +284,6 @@ namespace JSC {
         static JSValue nullImmediate();
         static JSValue zeroImmediate();
         static JSValue oneImmediate();
-
-        static JSObject* prototype(JSValue, ExecState*);
 
     private:
 #if USE(JSVALUE64)
