ae26c8a6af19467240c6a6e95e608e008faf9251
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-02 21:52:17 +0000

    2009-08-02  Geoffrey Garen  <ggaren@apple.com>
            Qt build fix.
            
            * runtime/Collector.cpp: #include <limits.h>, for a definition of ULONG_MAX.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46703 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/Collector.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/Collector.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Nodes.h"
 #include "Tracing.h"
 #include <algorithm>
+#include <limits.h>
 #include <setjmp.h>
 #include <stdlib.h>
 #include <wtf/FastMalloc.h>
