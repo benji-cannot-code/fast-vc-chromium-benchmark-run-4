03d7a2f8ca26e503983b2707aeef22edd4b0b5ae
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-02 21:56:23 +0000

    2009-08-02  Geoffrey Garen  <ggaren@apple.com>
            GTK build fix.
    
            * jit/JITStubs.cpp: #include <stdarg.h>, for a definition of va_start.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46704 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/jit/JITStubs.cpp
+++ third_party/WebKit/JavaScriptCore/jit/JITStubs.cpp
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RegExpPrototype.h"
 #include "Register.h"
 #include "SamplingTool.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 using namespace std;
