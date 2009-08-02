3f1347531961ffd76258444cdb6faea39b6882b6
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-02 01:04:20 +0000

    2009-08-01  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Adele Peterson.
    
            isContentEditable function in htmlediting.* must be removed
            https://bugs.webkit.org/show_bug.cgi?id=27870
    
            This patch removes isContentEditable function in htmlediting.cpp and
            replaces all usage by calling Node::isContentEditable or its derivatives.
    
            * editing/AppendNodeCommand.cpp: ditto
            (WebCore::AppendNodeCommand::AppendNodeCommand): ditto
            * editing/IndentOutdentCommand.cpp: ditto
            (WebCore::IndentOutdentCommand::outdentParagraph): ditto
            * editing/InsertNodeBeforeCommand.cpp: ditto
            (WebCore::InsertNodeBeforeCommand::InsertNodeBeforeCommand): ditto
            * editing/htmlediting.cpp: ditto
            (WebCore::enclosingNodeWithTag): ditto
            (WebCore::enclosingNodeOfType): ditto
            (WebCore::canMergeLists): ditto
            * editing/htmlediting.h: Updated prototype
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46681 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/htmlediting.h
+++ third_party/WebKit/WebCore/editing/htmlediting.h
@@ -55,7 +55,6 @@ VisiblePosition lastEditablePositionBeforePositionInRoot(const Position&, Node*)
 int comparePositions(const Position&, const Position&);
 int comparePositions(const VisiblePosition&, const VisiblePosition&);
 Node* lowestEditableAncestor(Node*);
-bool isContentEditable(const Node*);
 Position nextCandidate(const Position&);
 Position nextVisuallyDistinctCandidate(const Position&);
 Position previousCandidate(const Position&);
