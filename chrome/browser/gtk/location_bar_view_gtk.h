8ba0a502ab37b32e8fbae0c8203ae5908ae3e74e
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-03 18:51:35 +0000

    Fix breakage.
    Review URL: http://codereview.chromium.org/159802
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22295 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/location_bar_view_gtk.h
+++ chrome/browser/gtk/location_bar_view_gtk.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class AutocompleteEditViewGtk;
 class AutocompletePopupPositioner;
 class CommandUpdater;
+class GtkThemeProvider;
 class Profile;
 class SkBitmap;
 class TabContents;
