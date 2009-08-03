7f524c3446409d59eef95fd96584b70e34ae040a
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-03 23:32:27 +0000

    This unneeded include broke if someone subsequently included net/url_request/url_request_status.h
    Review URL: http://codereview.chromium.org/159818
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22346 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/panel_controller.cc
+++ chrome/browser/views/panel_controller.cc
@@ -5,11 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/views/panel_controller.h"
 
-#include <gdk/gdkx.h>
-extern "C" {
-#include <X11/Xlib.h>
-}
-
 #include "app/resource_bundle.h"
 #include "base/logging.h"
 #include "base/singleton.h"
