7d26cc911659f8c02f7d0cff40c875ab144e609b
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-03 18:47:26 +0000

    2009-08-03  Tony Chang  <tony@chromium.org>
            Reviewed by Darin Adler.
    
            Fix Chromium build by removing PluginView.h from Page.cpp.
            Chromium doesn't use PluginView and the pointer is never
            dereferenced so the header doesn't need to be included.
            https://bugs.webkit.org/show_bug.cgi?id=27946
    
            No new tests because this is a build fix.
    
            * page/Page.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46726 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Page.cpp
+++ third_party/WebKit/WebCore/page/Page.cpp
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NetworkStateNotifier.h"
 #include "PageGroup.h"
 #include "PluginData.h"
-#include "PluginView.h"
 #include "ProgressTracker.h"
 #include "RenderWidget.h"
 #include "RenderTheme.h"
