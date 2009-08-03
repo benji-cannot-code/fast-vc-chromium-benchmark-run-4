a540fd69a5eb0dd49d2c77af8fc1471c83ea5c23
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-03 23:14:34 +0000

    Add timeouts for ConnectJobs.  Limit ConnectJobs per group to number of Requests per group + 1. In the histograms for Net.SocketRequestTime, late binding has a much longer tail.  This is presumably because I didn't implement timeouts and CancelRequest() never cancelled jobs.  I'm limiting TCPConnectJobs to 60 seconds and cancelling jobs if there are too many more than there are requests.
    Review URL: http://codereview.chromium.org/160499
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22338 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/tcp_client_socket_pool.h
+++ net/socket/tcp_client_socket_pool.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
+#include "base/time.h"
+#include "base/timer.h"
 #include "net/socket/client_socket_pool_base.h"
 #include "net/socket/client_socket_pool.h"
 
@@ -25,17 +27,13 @@ class TCPConnectJob : public ConnectJob {
   TCPConnectJob(const std::string& group_name,
                 const HostResolver::RequestInfo& resolve_info,
                 const ClientSocketHandle* handle,
+                base::TimeDelta timeout_duration,
                 ClientSocketFactory* client_socket_factory,
                 HostResolver* host_resolver,
                 Delegate* delegate);
   virtual ~TCPConnectJob();
 
   // ConnectJob methods.
-
-  // Begins the host resolution and the TCP connect.  Returns OK on success
-  // and ERR_IO_PENDING if it cannot immediately service the request.
-  // Otherwise, it returns a net error code.
-  virtual int Connect();
 
  private:
   enum State {
@@ -45,6 +43,11 @@ class TCPConnectJob : public ConnectJob {
     kStateTCPConnectComplete,
     kStateNone,
   };
+
+  // Begins the host resolution and the TCP connect.  Returns OK on success
+  // and ERR_IO_PENDING if it cannot immediately service the request.
+  // Otherwise, it returns a net error code.
+  virtual int ConnectInternal();
 
   void OnIOComplete(int result);
 
