8308c1be3bbefc809ad71c00a1c848ae1ea3b175
mhm@chromium.org <mhm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-03 17:04:03 +0000

    Reverting 22273. Review URL: http://codereview.chromium.org/160524
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22274 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_shelf.cc
+++ chrome/browser/download/download_shelf.cc
@@ -74,15 +74,13 @@ bool DownloadShelfContextMenu::IsItemCommandEnabled(int id) const {
     case CANCEL:
       return download_->state() == DownloadItem::IN_PROGRESS;
     case REMOVE_ITEM:
-      return download_->state() == DownloadItem::COMPLETE ||
-          download_->state() == DownloadItem::CANCELLED;
+      return download_->state() == DownloadItem::COMPLETE;
     default:
       return id > 0 && id < MENU_LAST;
   }
 }
 
 void DownloadShelfContextMenu::ExecuteItemCommand(int id) {
-  item_removed_ = true;
   switch (id) {
     case SHOW_IN_FOLDER:
       download_->manager()->ShowDownloadInShell(download_);
@@ -101,7 +99,6 @@ void DownloadShelfContextMenu::ExecuteItemCommand(int id) {
       model_->CancelTask();
       break;
     case REMOVE_ITEM:
-      item_removed_ = true;
       download_->Remove(false);
       break;
     default:
