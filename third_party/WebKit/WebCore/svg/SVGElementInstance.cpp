7175295283709df4d620f5725ed1e895c123fa07
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-03 04:12:37 +0000

    2009-08-02  Adam Barth  <abarth@webkit.org>
            Reviewed by Darin Adler.
    
            SVGElementInstance::scriptExecutionContext should call document() instead of scriptExecutionContext()
    
            https://bugs.webkit.org/show_bug.cgi?id=27932
    
            There's no reason to pay for a virtual function call in this case.  We
    
            can just call document() directly because we know that element is a
    
            Node*.
    
            * svg/SVGElementInstance.cpp:
    
            (WebCore::SVGElementInstance::scriptExecutionContext):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46711 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGElementInstance.cpp
+++ third_party/WebKit/WebCore/svg/SVGElementInstance.cpp
@@ -137,7 +137,7 @@ void SVGElementInstance::setNeedsUpdate(bool value)
 ScriptExecutionContext* SVGElementInstance::scriptExecutionContext() const
 {
     if (SVGElement* element = correspondingElement())
-        return element->scriptExecutionContext();
+        return element->document();
     return 0;
 }
 
