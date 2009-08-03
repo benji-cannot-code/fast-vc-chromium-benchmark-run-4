9b2cfff7c63fa37536d9939521db98b3e391db96
adachan@apple.com <adachan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-03 18:34:19 +0000

    Reviewed by Darin Adler.
            Don't use background thread to scavenge memory on Tiger until we figure out why it causes a crash.
            https://bugs.webkit.org/show_bug.cgi?id=27900
    
            * wtf/FastMalloc.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/FastMalloc.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/FastMalloc.cpp
@@ -98,7 +98,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 // Use a background thread to periodically scavenge memory to release back to the system
+// https://bugs.webkit.org/show_bug.cgi?id=27900: don't turn this on for Tiger until we have figured out why it caused a crash.
+#if !defined(BUILDING_ON_TIGER)
 #define USE_BACKGROUND_THREAD_TO_SCAVENGE_MEMORY 1
+#endif
 
 #ifndef NDEBUG
 namespace WTF {
