be781a9ff1f0f8ddc7bd517ce4aaab3983a00985
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-04 15:20:34 +0000

    Fixing potential memory leak in BookmarkCodec::DecodeNode.
    Coverity finds a condition where this leaks. We are at the moment not using it in such a way that it leaks, but that might of course change. It looks to me like the API is not intended to be used in such a way anyway, so we can just check for it and return false.
    
    BUG=15350
    TEST=Decoding bookmarks should work as before.
    
    Review URL: http://codereview.chromium.org/159075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22379 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_codec.cc
+++ chrome/browser/bookmarks/bookmark_codec.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -181,6 +181,13 @@ bool BookmarkCodec::DecodeChildren(const ListValue& child_value_list,
 bool BookmarkCodec::DecodeNode(const DictionaryValue& value,
                                BookmarkNode* parent,
                                BookmarkNode* node) {
+  // If no |node| is specified, we'll create one and add it to the |parent|.
+  // Therefore, in that case, |parent| must be non-NULL.
+  if (!node && !parent) {
+    NOTREACHED();
+    return false;
+  }
+
   std::string id_string;
   int64 id = 0;
   if (!value.GetString(kIdKey, &id_string) || !StringToInt64(id_string, &id))
@@ -210,7 +217,7 @@ bool BookmarkCodec::DecodeNode(const DictionaryValue& value,
     if (!node)
       node = new BookmarkNode(id, GURL(WideToUTF8(url_string)));
     else
-      return false; // Node invalid.
+      return false;  // Node invalid.
 
     if (parent)
       parent->Add(parent->GetChildCount(), node);
