ab3dfc4cbe5e42e1300000fab4e671efbc4e04fd
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-04 09:26:54 +0000

    2009-08-04  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            bugzilla-tool --commit-queue should validate commit-queue setter is a committer
            https://bugs.webkit.org/show_bug.cgi?id=27974
    
            With a test!
    
            * Scripts/modules/bugzilla.py:
            * Scripts/modules/bugzilla_unittest.py:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/bugzilla_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/bugzilla_unittest.py
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
-from modules.committers import CommitterList, Reviewer
+from modules.committers import CommitterList, Reviewer, Committer
 from modules.bugzilla import Bugzilla
 
 from modules.BeautifulSoup import BeautifulSoup
@@ -54,6 +54,11 @@ class BugzillaTest(unittest.TestCase):
                 status="+"
                 setter="one@test.com"
            />
+          <flag name="commit-queue"
+                id="17932"
+                status="+"
+                setter="two@test.com"
+           />
         </attachment>
 '''
     _expected_example_attachment_parsing = {
@@ -64,12 +69,14 @@ class BugzillaTest(unittest.TestCase):
         'url' : "https://bugs.webkit.org/attachment.cgi?id=33721",
         'name' : "Fixed whitespace issue",
         'type' : "text/plain",
-        'reviewer' : 'Test One'
+        'reviewer' : 'Test One',
+        'commit-queue' : 'Test Two'
     }
 
     def test_attachment_parsing(self):
         reviewer = Reviewer('Test One', 'one@test.com')
-        committer_list = CommitterList(committers=[], reviewers=[reviewer])
+        committer = Committer('Test Two', 'two@test.com')
+        committer_list = CommitterList(committers=[committer], reviewers=[reviewer])
         bugzilla = Bugzilla(committers=committer_list)
 
         soup = BeautifulSoup(self._example_attachment)
@@ -78,7 +85,7 @@ class BugzillaTest(unittest.TestCase):
         self.assertTrue(attachment)
 
         for key, expected_value in self._expected_example_attachment_parsing.items():
-            self.assertEquals(attachment[key], expected_value, ("Failure for key: %s" % key))
+            self.assertEquals(attachment[key], expected_value, ("Failure for key: %s: Actual='%s' Expected='%s'" % (key, attachment[key], expected_value)))
 
 if __name__ == '__main__':
     unittest.main()
