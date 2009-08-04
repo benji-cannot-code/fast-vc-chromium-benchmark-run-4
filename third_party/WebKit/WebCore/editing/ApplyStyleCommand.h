57e90606ec2a221631c36dcf312e493cf8f11fbc
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-04 07:13:01 +0000

    2009-08-04  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Eric Seidel.
    
            selectionHasStyle needs clean up to fix Bug 27858
            https://bugs.webkit.org/show_bug.cgi?id=27865
    
            This patch cleans up selectionStartHasStyle and selectionHasStyle.
            No test is added because the patch does not change any behavior.
    
            * editing/ApplyStyleCommand.cpp:
            (WebCore::getPropertiesNotInComputedStyle): Used in triStateOfStyleInComputedStyle but will be used here as well
            * editing/ApplyStyleCommand.h: Added the prototype of getPropertiesNotInComputedStyle
            * editing/Editor.cpp:
            (WebCore::triStateOfStyleInComputedStyle): Determines if the specified style is present in the specified computed style.
            (WebCore::Editor::selectionStartHasStyle): Uses triStateOfStyleInComputedStyle
            (WebCore::Editor::selectionHasStyle): Uses triStateOfStyleInComputedStyle instead of updateState
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/ApplyStyleCommand.h
+++ third_party/WebKit/WebCore/editing/ApplyStyleCommand.h
@@ -115,6 +115,7 @@ private:
 
 bool isStyleSpan(const Node*);
 PassRefPtr<HTMLElement> createStyleSpanElement(Document*);
+RefPtr<CSSMutableStyleDeclaration> getPropertiesNotInComputedStyle(CSSStyleDeclaration* style, CSSComputedStyleDeclaration* computedStyle);
 
 } // namespace WebCore
 
