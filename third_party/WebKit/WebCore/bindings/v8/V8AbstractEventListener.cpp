4670190b92dfcf3d9b9888c0208c4455d4be3786
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-04 23:20:18 +0000

    2009-08-04  Nate Chapin  <japhet@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Revert r46421 and r46769, which appear to have totally hosed the v8 bindings.
    
            https://bugs.webkit.org/show_bug.cgi?id=27719
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46784 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8AbstractEventListener.cpp
+++ third_party/WebKit/WebCore/bindings/v8/V8AbstractEventListener.cpp
@@ -74,11 +74,11 @@ void V8AbstractEventListener::invokeEventHandler(v8::Handle<v8::Context> v8Conte
         tryCatch.SetVerbose(true);
 
         // Save the old 'event' property so we can restore it later.
-        v8::Local<v8::Value> savedEvent = v8Context->Global()->Get(eventSymbol);
+        v8::Local<v8::Value> savedEvent = v8Context->Global()->GetHiddenValue(eventSymbol);
         tryCatch.Reset();
 
         // Make the event available in the global object, so DOMWindow can expose it.
-        v8Context->Global()->Set(eventSymbol, jsEvent);
+        v8Context->Global()->SetHiddenValue(eventSymbol, jsEvent);
         tryCatch.Reset();
 
         // Call the event handler.
@@ -87,9 +87,9 @@ void V8AbstractEventListener::invokeEventHandler(v8::Handle<v8::Context> v8Conte
 
         // Restore the old event. This must be done for all exit paths through this method.
         if (savedEvent.IsEmpty())
-            v8Context->Global()->Set(eventSymbol, v8::Undefined());
+            v8Context->Global()->SetHiddenValue(eventSymbol, v8::Undefined());
         else
-            v8Context->Global()->Set(eventSymbol, savedEvent);
+            v8Context->Global()->SetHiddenValue(eventSymbol, savedEvent);
         tryCatch.Reset();
     }
 
