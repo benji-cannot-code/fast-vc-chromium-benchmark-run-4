4acff49d9cb7b4ae81c0806ae6a1d2538a7f7f44
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-04 07:06:02 +0000

    2009-08-03  John Abd-El-Malek  <jam@chromium.org>
            Reviewed by David Levin.
    
            Fix MessagePort construction in worker process.  Also make the other object constructions match this.
    
            https://bugs.webkit.org/show_bug.cgi?id=27906
    
            * bindings/v8/V8DOMWrapper.cpp:
            (WebCore::V8DOMWrapper::convertToV8Object):
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::setDOMException):
            * bindings/v8/WorkerContextExecutionProxy.cpp:
            (WebCore::WorkerContextExecutionProxy::ToV8Object):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46748 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/WorkerContextExecutionProxy.cpp
+++ third_party/WebKit/WebCore/bindings/v8/WorkerContextExecutionProxy.cpp
@@ -40,9 +40,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DOMCoreException.h"
 #include "Event.h"
 #include "EventException.h"
+#include "MessagePort.h"
 #include "RangeException.h"
 #include "V8Binding.h"
 #include "V8DOMMap.h"
+#include "V8Index.h"
 #include "V8Proxy.h"
 #include "V8WorkerContextEventListener.h"
 #include "V8WorkerContextObjectEventListener.h"
@@ -51,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WorkerLocation.h"
 #include "WorkerNavigator.h"
 #include "WorkerScriptController.h"
+#include "XMLHttpRequest.h"
 #include "XMLHttpRequestException.h"
 
 namespace WebCore {
@@ -212,16 +215,34 @@ v8::Handle<v8::Value> WorkerContextExecutionProxy::ToV8Object(V8ClassIndex::V8Wr
     if (type == V8ClassIndex::DEDICATEDWORKERCONTEXT)
         return WorkerContextToV8Object(static_cast<WorkerContext*>(impl));
 
-    if (type == V8ClassIndex::WORKER || type == V8ClassIndex::XMLHTTPREQUEST) {
+    bool isActiveDomObject = false;
+    switch (type) {
+#define MAKE_CASE(TYPE, NAME) case V8ClassIndex::TYPE:
+        ACTIVE_DOM_OBJECT_TYPES(MAKE_CASE)
+        isActiveDomObject = true;
+        break;
+#undef MAKE_CASE
+    default:
+        break;
+    }
+
+    if (isActiveDomObject) {
         v8::Persistent<v8::Object> result = getActiveDOMObjectMap().get(impl);
         if (!result.IsEmpty())
             return result;
 
         v8::Local<v8::Object> object = toV8(type, type, impl);
-        if (!object.IsEmpty())
-            static_cast<Worker*>(impl)->ref();
+        switch (type) {
+#define MAKE_CASE(TYPE, NAME) \
+        case V8ClassIndex::TYPE: static_cast<NAME*>(impl)->ref(); break;
+            ACTIVE_DOM_OBJECT_TYPES(MAKE_CASE)
+#undef MAKE_CASE
+        default:
+            ASSERT_NOT_REACHED();
+        }
+
         result = v8::Persistent<v8::Object>::New(object);
-        V8DOMWrapper::setJSWrapperForDOMObject(impl, result);
+        V8DOMWrapper::setJSWrapperForActiveDOMObject(impl, result);
         return result;
     }
 
