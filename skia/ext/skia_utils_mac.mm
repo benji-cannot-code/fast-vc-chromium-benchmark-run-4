8b5ff7b5381174895cb947256103b1538b3223fc
paul@chromium.org <paul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-04 21:14:49 +0000

    Fix a bug where the old graphics context was released when we were actually trying to save it for later use.
    TEST=Set download items to auto open a specific file type (eg .gz) and
         repeatedly download items of that type, noticing that we no longer
         crash.
    BUG=17571 (http://crbug.com/17571)
    
    Review URL: http://codereview.chromium.org/160602
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22408 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/skia_utils_mac.mm
+++ skia/ext/skia_utils_mac.mm
@@ -149,7 +149,7 @@ SkBitmap NSImageToSkBitmap(NSImage* image, NSSize size, bool is_opaque) {
   DCHECK(context != NULL);
 
   // Save the current graphics context so that we can restore it later.
-  NSGraphicsContext* current_context = [NSGraphicsContext currentContext];
+  [NSGraphicsContext saveGraphicsState];
 
   // Dummy context that we will draw into.
   NSGraphicsContext* context_cocoa =
@@ -163,7 +163,7 @@ SkBitmap NSImageToSkBitmap(NSImage* image, NSSize size, bool is_opaque) {
            fraction:1.0];
 
   // Done drawing, restore context.
-  [NSGraphicsContext setCurrentContext:current_context];
+  [NSGraphicsContext restoreGraphicsState];
 
   return bitmap;
 }
