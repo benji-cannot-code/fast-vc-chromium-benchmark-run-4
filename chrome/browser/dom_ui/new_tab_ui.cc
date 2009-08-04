8e26274bed26b884b9ef1074f32ffc1f0c96d314
arv@google.com <arv@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-04 18:16:28 +0000

    Change the numbe of recently closed tabs to a maximum of 10 instead of 6.
    Bug=17879
    
    TEST=Close a lot of tabs. You should see the 10 latest closed tabs in the
    recent activities section.
    
    Review URL: http://codereview.chromium.org/160588
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22396 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/new_tab_ui.cc
+++ chrome/browser/dom_ui/new_tab_ui.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -1314,7 +1314,7 @@ void RecentlyClosedTabsHandler::TabRestoreServiceChanged(
   ListValue list_value;
   std::set<std::wstring> unique_items;
   int added_count = 0;
-  const int max_count = NewTabUI::UseOldNewTabPage() ? 3 : 6;
+  const int max_count = NewTabUI::UseOldNewTabPage() ? 3 : 10;
 
   // We filter the list of recently closed to only show 'interesting' entries,
   // where an interesting entry is either a closed window or a closed tab
