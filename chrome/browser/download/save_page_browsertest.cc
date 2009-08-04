e650202f9f9d840387f11b5a48f2c6ecedf4a06e
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-04 21:48:59 +0000

    Port more tests from save_page_uitest.cc to save_page_browsertest.cc.
    Also add more detailed checks for saved page contents.
    
    TEST=Covered by browser_tests.
    http://crbug.com/3791
    
    Review URL: http://codereview.chromium.org/159858
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22413 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/save_page_browsertest.cc
+++ chrome/browser/download/save_page_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_path.h"
 #include "base/scoped_temp_dir.h"
+#include "chrome/app/chrome_dll_resource.h"
 #include "chrome/browser/browser.h"
 #include "chrome/browser/browser_window.h"
 #include "chrome/browser/net/url_request_mock_http_job.h"
@@ -16,6 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 static const FilePath::CharType* kTestDir = FILE_PATH_LITERAL("save_page");
+
+static const char* kAppendedExtension =
+#if defined(OS_WIN)
+    ".htm";
+#else
+    ".html";
+#endif
 
 namespace {
 
@@ -74,8 +82,9 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveHTMLOnly) {
 
   FilePath full_file_name = save_dir_.path().Append(file_name);
   FilePath dir = save_dir_.path().AppendASCII("a_files");
-  current_tab->SavePage(full_file_name.ToWStringHack(), dir.ToWStringHack(),
-                        SavePackage::SAVE_AS_ONLY_HTML);
+  ASSERT_TRUE(current_tab->SavePage(full_file_name.ToWStringHack(),
+                                    dir.ToWStringHack(),
+                                    SavePackage::SAVE_AS_ONLY_HTML));
 
   SavePageFinishedObserver observer;
 
@@ -99,8 +108,9 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveCompleteHTML) {
 
   FilePath full_file_name = save_dir_.path().Append(file_name);
   FilePath dir = save_dir_.path().AppendASCII("b_files");
-  current_tab->SavePage(full_file_name.ToWStringHack(), dir.ToWStringHack(),
-                        SavePackage::SAVE_AS_COMPLETE_HTML);
+  ASSERT_TRUE(current_tab->SavePage(full_file_name.ToWStringHack(),
+                                    dir.ToWStringHack(),
+                                    SavePackage::SAVE_AS_COMPLETE_HTML));
 
   SavePageFinishedObserver observer;
 
@@ -108,7 +118,50 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveCompleteHTML) {
   EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   EXPECT_TRUE(file_util::PathExists(dir));
-  // TODO(phajdan.jr): Check saved html file's contents (http://crbug.com/3791).
+  EXPECT_TRUE(file_util::TextContentsEqual(
+      test_dir_.Append(FilePath(kTestDir)).AppendASCII("b.saved1.htm"),
+      full_file_name));
+  EXPECT_TRUE(file_util::ContentsEqual(
+      test_dir_.Append(FilePath(kTestDir)).AppendASCII("1.png"),
+      dir.AppendASCII("1.png")));
+  EXPECT_TRUE(file_util::ContentsEqual(
+      test_dir_.Append(FilePath(kTestDir)).AppendASCII("1.css"),
+      dir.AppendASCII("1.css")));
+}
+
+IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, NoSave) {
+  ui_test_utils::NavigateToURL(browser(), GURL("about:blank"));
+  ASSERT_TRUE(browser()->command_updater()->SupportsCommand(IDC_SAVE_PAGE));
+  EXPECT_FALSE(browser()->command_updater()->IsCommandEnabled(IDC_SAVE_PAGE));
+}
+
+IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, FileNameFromPageTitle) {
+  FilePath file_name(FILE_PATH_LITERAL("b.htm"));
+
+  GURL url = URLRequestMockHTTPJob::GetMockUrl(
+      FilePath(kTestDir).Append(file_name).ToWStringHack());
+  ui_test_utils::NavigateToURL(browser(), url);
+
+  FilePath full_file_name = save_dir_.path().AppendASCII(
+      std::string("Test page for saving page feature") + kAppendedExtension);
+  FilePath dir = save_dir_.path().AppendASCII(
+      "Test page for saving page feature_files");
+
+  TabContents* current_tab = browser()->GetSelectedTabContents();
+  ASSERT_TRUE(current_tab);
+
+  ASSERT_TRUE(current_tab->SavePage(full_file_name.ToWStringHack(),
+                                    dir.ToWStringHack(),
+                                    SavePackage::SAVE_AS_COMPLETE_HTML));
+  SavePageFinishedObserver observer;
+
+  EXPECT_EQ(url, observer.page_url());
+  EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
+  EXPECT_TRUE(file_util::PathExists(full_file_name));
+  EXPECT_TRUE(file_util::PathExists(dir));
+  EXPECT_TRUE(file_util::TextContentsEqual(
+      test_dir_.Append(FilePath(kTestDir)).AppendASCII("b.saved2.htm"),
+      full_file_name));
   EXPECT_TRUE(file_util::ContentsEqual(
       test_dir_.Append(FilePath(kTestDir)).AppendASCII("1.png"),
       dir.AppendASCII("1.png")));
