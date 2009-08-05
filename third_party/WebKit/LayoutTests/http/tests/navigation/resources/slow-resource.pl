92801db422772da6be6e9efd36b5852e0eb4dd2f
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-05 19:56:06 +0000

    Fix two tests to not load a resource that takes five seconds to load.
    Reviewed by Brady Eidson.
    
    We achieve this by making the delay in slow-resource.pl configurable in a query parameter.
    This drops the run time of each of the tests from over five seconds to less than a third
    of a second.
    
    * http/tests/misc/DOMContentLoaded-event.html:
    * http/tests/navigation/multiple-back-forward-entries.html:
    * http/tests/navigation/resources/slow-resource.pl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46808 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/navigation/resources/slow-resource.pl
+++ third_party/WebKit/LayoutTests/http/tests/navigation/resources/slow-resource.pl
@@ -1,6 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/perl -w
 
+use strict;
+use CGI;
+use Time::HiRes qw(sleep);
+
+my $cgi = new CGI;
+my $delay = $cgi->param('delay');
+$delay = 5000 unless $delay;
+
 # flush the buffers after each print
 select (STDOUT);
 $| = 1;
@@ -12,6 +20,6 @@ print "Pragma: no-cache\n";
 print "\n";
 
 print "Test page for back/forward list that takes awhile to\n";
-sleep 5;
+sleep $delay / 1000;
 print "Finish loading\n";
 
