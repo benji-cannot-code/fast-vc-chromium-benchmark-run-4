73c11310788d44985fffc675af7a871dfc91a0d0
tejasshah@google.com <tejasshah@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-05 22:55:39 +0000

    This is a quick fix for include file path issue in livesynctest related file. Review URL: http://codereview.chromium.org/165015
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22549 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/bookmark_model_verifier.cc
+++ chrome/test/live_sync/bookmark_model_verifier.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/tree_node_iterator.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/bookmarks/bookmark_utils.h"
-#include "chrome/browser/test/live_sync/live_bookmarks_sync_test.h"
+#include "chrome/test/live_sync/live_bookmarks_sync_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 // static
