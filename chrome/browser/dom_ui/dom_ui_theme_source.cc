b1d2d64dba286cb25d218c63a41b7d68359f2c20
glen@chromium.org <glen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-05 03:32:20 +0000

    Use the theme ID as as the cache-buster, making subsequent NNTP loads much faster.
    BUG=18427
    TEST=Install a few themes, verify that the NNTP never gets out of sync with the installed theme.
    
    Review URL: http://codereview.chromium.org/159900
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22462 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/dom_ui_theme_source.cc
+++ chrome/browser/dom_ui/dom_ui_theme_source.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_theme_provider.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/theme_resources_util.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "googleurl/src/gurl.h"
 #include "grit/browser_resources.h"
@@ -115,8 +116,8 @@ void DOMUIThemeSource::SendNewTabCSS(int request_id) {
   std::vector<string16> subst2;
 
   // Cache-buster for background.
-  subst.push_back(UTF8ToUTF16(IntToString(static_cast<int>(
-      base::Time::Now().ToDoubleT()))));  // $1
+  subst.push_back(WideToUTF16(
+      profile_->GetPrefs()->GetString(prefs::kCurrentThemeID)));  // $1
 
   // Colors.
   subst.push_back(SkColorToRGBAString(color_background));  // $2
