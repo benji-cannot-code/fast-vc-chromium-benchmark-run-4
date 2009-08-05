f4c0147af0a0d8c849047638b4216dbe9795ce73
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-05 17:28:39 +0000

    On bookmark edit, the OK button is now disabled if the entered URL is invalid (e.g. is the empty string).  The Cancel button is never disabled.
    BUG=http://crbug.com/17006
    
    TEST=Right click on a bookmark button to edit it.
    Make sure OK is enabled.
    Set URL to "" (the empty string).
    Make sure OK is DISabled.
    Set URL to "x".
    Make sure OK is enabled.
    
    Review URL: http://codereview.chromium.org/160628
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22490 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_editor_controller.h
+++ chrome/browser/cocoa/bookmark_editor_controller.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   IBOutlet NSTextField* nameField_;
   IBOutlet NSTextField* urlField_;
   IBOutlet NSBrowser* browser_;
+  IBOutlet NSButton* okButton_;
   IBOutlet NSButton* newFolderButton_;
 
   NSWindow* parentWindow_;
@@ -48,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface BookmarkEditorController(TestingAPI)
 @property (assign) NSString* displayName;
 @property (assign) NSString* displayURL;
+@property (readonly) BOOL okButtonEnabled;
 @end
 
 
