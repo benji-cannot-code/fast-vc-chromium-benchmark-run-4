c75971214b84b5f56fecaf463ea132dda326ed22
apatrick@google.com <apatrick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-05 22:19:59 +0000

    Updated O3D to work with most recent Chrome base. Added build.scons for Chrome IPC. Review URL: http://codereview.chromium.org/160616
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22545 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/types.h
+++ o3d/core/cross/types.h
@@ -106,12 +106,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // We can add more sophisticated per-platform #defines as necessary here
 #define IS_LITTLE_ENDIAN 1
 
-#if defined(GYP_BUILD)
-// We only need this in the gyp build because we're using the current
-// chrome/base, which doesn't have down_cast in it anymore.
-// TODO(gspencer): Remove this #ifdef when we are moved fully over to
-// gyp.
-
 // When you upcast (that is, cast a pointer from type Foo to type
 // SuperclassOfFoo), it's fine to use implicit_cast<>, since upcasts
 // always succeed.  When you downcast (that is, cast a pointer from
@@ -138,7 +132,6 @@ inline To down_cast(From* f) {  // Defined as From* so we only accept pointers.
 
   return static_cast<To>(f);
 }
-#endif  // GYP_BUILD
 
 namespace o3d {
 
