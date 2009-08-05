5765e6a5047361befae2c9bad6e0ad35a01e48fe
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-05 21:26:57 +0000

    2009-08-05  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Darin Adler.
    
            Implement the matchMedium method on the Media interface described in the CSSOM View Module.
            https://bugs.webkit.org/show_bug.cgi?id=22495
    
            Add a new Media interface, obtainable via the AbstractView, that can
            evaluate queries via its matchesMedium() method.
    
            Test: fast/media/matchmedium-query-api.html
    
            * DerivedSources.cpp:
            * DerivedSources.make:
            * GNUmakefile.am:
            * WebCore.gypi:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * WebCoreSources.bkl:
            Add Media.* files to the build.
    
            * css/Media.cpp: Added.
            (WebCore::Media::Media):
            (WebCore::Media::type):
            (WebCore::Media::matchMedium):
            * css/Media.h: Added.
            (WebCore::Media::create):
            (WebCore::Media::document):
            * css/Media.idl: Added.
            Media::matchMedium() uses a MediaQueryEvaluator() to test the query.
    
            * page/AbstractView.idl:
            * page/DOMWindow.cpp:
            (WebCore::DOMWindow::media):
            * page/DOMWindow.h:
            * page/DOMWindow.idl:
            Add a readonly attribute for Media.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46816 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/Media.cpp
+++ third_party/WebKit/WebCore/css/Media.cpp
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
- * Copyright (C) 2006 Samuel Weinig <sam.weinig@gmail.com>
+ * Copyright (C) 2009 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -25,14 +24,52 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-module views {
+#include "config.h"
 
-    // Introduced in DOM Level 2:
-    interface [
-        ObjCCustomImplementation
-    ] AbstractView {
-        readonly attribute Document document;
-        readonly attribute Media media;
-    };
+#include "Media.h"
+#include "CSSStyleSelector.h"
+#include "Frame.h"
+#include "FrameView.h"
+#include "MediaList.h"
+#include "MediaQueryEvaluator.h"
 
+namespace WebCore {
+
+Media::Media(DOMWindow* window)
+    : m_window(window)
+{
 }
+
+String Media::type() const
+{
+    Frame* frame = m_window->frame();
+    FrameView* view = frame ? frame->view() : 0;
+    if (view)
+        return view->mediaType();
+
+    return String();
+}
+
+bool Media::matchMedium(const String& query) const
+{
+    Document* document = m_window->document();
+    Frame* frame = m_window->frame();
+
+    CSSStyleSelector* styleSelector = document->styleSelector();
+    Element* docElement = document->documentElement();
+    if (!styleSelector || !docElement || !frame)
+        return false;
+
+    RefPtr<RenderStyle> rootStyle = styleSelector->styleForElement(docElement, 0 /*defaultParent*/, false /*allowSharing*/, true /*resolveForRootDefault*/);
+    RefPtr<MediaList> media = MediaList::create();
+
+    ExceptionCode ec = 0;
+    media->setMediaText(query, ec);
+    if (ec)
+        return false;
+
+    MediaQueryEvaluator screenEval(type(), frame, rootStyle.get());
+    return screenEval.eval(media.get());
+}
+
+} // namespace WebCore
--- third_party/WebKit/WebCore/page/DOMWindow.h
+++ third_party/WebKit/WebCore/page/DOMWindow.h
@@ -53,6 +53,7 @@ namespace WebCore {
     class Frame;
     class History;
     class Location;
+    class Media;
     class MessagePort;
     class Navigator;
     class Node;
@@ -172,6 +173,8 @@ namespace WebCore {
 
         // DOM Level 2 AbstractView Interface
         Document* document() const;
+        // CSSOM View Module
+        PassRefPtr<Media> media() const;
 
         // DOM Level 2 Style Interface
         PassRefPtr<CSSStyleDeclaration> getComputedStyle(Element*, const String& pseudoElt) const;
