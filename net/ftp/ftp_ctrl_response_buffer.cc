40975cc0c21a748f42e6685683af75068528ff7f
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-05 18:34:47 +0000

    Correctly handle multiple control responses for RETR command.
    Re-enable tests which were intermittently failing before this fix and remove
    debugging code used to track down the issue.
    
    TEST=Covered by net_unittests.
    http://crbug.com/18036
    
    Review URL: http://codereview.chromium.org/160537
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22500 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_ctrl_response_buffer.cc
+++ net/ftp/ftp_ctrl_response_buffer.cc
@@ -9,19 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "net/base/net_errors.h"
 
-namespace {
-
-// TODO(phajdan.jr): Remove when http://crbug.com/18036 is diagnosed.
-void LogResponse(const net::FtpCtrlResponse& response) {
-  DLOG(INFO) << "received response with code " << response.status_code;
-  for (std::vector<std::string>::const_iterator i = response.lines.begin();
-       i != response.lines.end(); ++i) {
-    DLOG(INFO) << "line [" << *i << "]";
-  }
-}
-
-}  // namespace
-
 namespace net {
 
 // static
@@ -44,7 +31,6 @@ int FtpCtrlResponseBuffer::ConsumeData(const char* data, int data_length) {
         line_buf_ = line.status_text;
       } else {
         response_buf_.lines.push_back(line.status_text);
-        LogResponse(response_buf_);
         responses_.push(response_buf_);
 
         // Prepare to handle following lines.
@@ -64,7 +50,6 @@ int FtpCtrlResponseBuffer::ConsumeData(const char* data, int data_length) {
 
       if (!line.is_multiline) {
         response_buf_.lines.push_back(line_buf_);
-        LogResponse(response_buf_);
         responses_.push(response_buf_);
 
         // Prepare to handle following lines.
