1e0b647b7010411eb0db43d843ca3231e3ac4437
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-05 00:32:04 +0000

    Undo my bonehead mistake of checking in a modification to the hardcoded theme gallery URL.
    Review URL: http://codereview.chromium.org/159888
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22448 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service.cc
+++ chrome/browser/extensions/extensions_service.cc
@@ -33,16 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const char* ExtensionsService::kInstallDirectoryName = "Extensions";
 const char* ExtensionsService::kCurrentVersionFileName = "Current Version";
 
-/*
 const char* ExtensionsService::kGalleryDownloadURLPrefix =
     "https://dl-ssl.google.com/chrome/";
 const char* ExtensionsService::kGalleryURLPrefix =
     "https://tools.google.com/chrome/";
-*/
-const char* ExtensionsService::kGalleryDownloadURLPrefix =
-    "http://www.corp.google.com/~glen/chrome/";
-const char* ExtensionsService::kGalleryURLPrefix =
-    "http://www.corp.google.com/~glen/chrome/";
 
 // static
 bool ExtensionsService::IsDownloadFromGallery(const GURL& download_url,
