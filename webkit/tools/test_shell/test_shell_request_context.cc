74949ef27f63c13925fa5c46da1f52b564bf32d3
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-06 23:00:35 +0000

    Enable new FTP for Windows TestShell so that the results of block-test.html are the same across platforms.  WTC tells me that it should be ready to go, and it anyways is only exercised by these two security/block-test*.html tests.
    Also, add back the fix from r22651.
    
    Extend that fix a bit so that when we do not have a special mapping, we just spit out the existing error code value.  This way, if we get an unexpected result, the output will tell us what the original error code was.  This change doesn't impact any layout tests.
    
    R=dglazkov,wtc
    BUG=18665
    TEST=none
    
    Review URL: http://codereview.chromium.org/165091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22686 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_request_context.cc
+++ webkit/tools/test_shell/test_shell_request_context.cc
@@ -5,13 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/tools/test_shell/test_shell_request_context.h"
 
-#include "base/command_line.h"
 #include "net/base/cookie_monster.h"
 #include "net/base/host_resolver.h"
 #include "net/ftp/ftp_network_layer.h"
 #include "net/proxy/proxy_service.h"
 #include "webkit/glue/webkit_glue.h"
-#include "webkit/tools/test_shell/test_shell_switches.h"
 
 TestShellRequestContext::TestShellRequestContext() {
   Init(std::wstring(), net::HttpCache::NORMAL, false);
@@ -59,14 +57,7 @@ void TestShellRequestContext::Init(
   cache->set_mode(cache_mode);
   http_transaction_factory_ = cache;
 
-  // The kNewFtp switch is Windows specific only because we have multiple FTP
-  // implementations on Windows.
-#if defined(OS_WIN)
-  if (CommandLine::ForCurrentProcess()->HasSwitch(test_shell::kNewFtp))
-    ftp_transaction_factory_ = new net::FtpNetworkLayer(host_resolver_);
-#else
   ftp_transaction_factory_ = new net::FtpNetworkLayer(host_resolver_);
-#endif
 }
 
 TestShellRequestContext::~TestShellRequestContext() {
