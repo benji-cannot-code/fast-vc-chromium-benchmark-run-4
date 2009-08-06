aad51380439c009768ca95826077aec22d2da21e
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-06 01:56:20 +0000

    Remove an obsolete TODO comment.  HostResolver has changed significantly.
    R=eroman
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/147165
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22574 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/cert_verifier.h
+++ net/base/cert_verifier.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2008-2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,9 +21,6 @@ class X509Certificate;
 // verify a single certificate at a time, so if you need to verify multiple
 // certificates at the same time, you will need to allocate a CertVerifier
 // object for each certificate.
-//
-// TODO(wtc): This class is based on HostResolver.  We should create a base
-// class for the common code between the two classes.
 //
 class CertVerifier {
  public:
