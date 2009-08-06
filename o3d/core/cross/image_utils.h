a6d171758251f921a69985fae71ab53fa9cff5f6
gman@google.com <gman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-06 08:56:27 +0000

    Fixes for gcc
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22597 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/image_utils.h
+++ o3d/core/cross/image_utils.h
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef O3D_CORE_CROSS_IMAGE_UTILS_H_
 #define O3D_CORE_CROSS_IMAGE_UTILS_H_
 
-#include "core/cross/precompile.h"
-
 #include "base/cross/bits.h"
 #include "core/cross/types.h"
 #include "core/cross/texture_base.h"
