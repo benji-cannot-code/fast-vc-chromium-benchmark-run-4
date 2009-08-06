8e97559564dd85b563ad2f623e1bd57ec9e59877
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-06 03:50:50 +0000

    2009-08-05  Dimitri Glazkov  <dglazkov@chromium.org>
            Unreviewed, build fix.
    
            [V8] Revert http://trac.webkit.org/changeset/46821, which was
            landed to supplement http://trac.webkit.org/changeset/46816, but
            now that the latter is reverted, the former needs the same.
    
            * bindings/v8/DOMObjectsInclude.h:
            * bindings/v8/DerivedSourcesAllInOne.cpp:
            * bindings/v8/V8Index.cpp:
            * bindings/v8/V8Index.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46829 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/DOMObjectsInclude.h
+++ third_party/WebKit/WebCore/bindings/v8/DOMObjectsInclude.h
@@ -89,7 +89,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorBackend.h"
 #include "KeyboardEvent.h"
 #include "Location.h"
-#include "Media.h"
 #include "MediaError.h"
 #include "MediaList.h"
 #include "MediaPlayer.h"
