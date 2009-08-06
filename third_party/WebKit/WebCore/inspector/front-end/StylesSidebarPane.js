49568e121b4ea66009e433867b7969eec7c71526
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-06 16:37:54 +0000

    2009-08-06  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            WebInspector: move style-related utilities into InjectedScript.
    
            http://bugs.webkit.org/show_bug.cgi?id=27939
    
            * inspector/front-end/DOMAgent.js:
            (WebInspector.CSSStyleDeclaration):
            (WebInspector.CSSStyleDeclaration.prototype.isPropertyImplicit):
            (WebInspector.CSSStyleDeclaration.prototype.styleTextWithShorthands):
            (WebInspector.CSSStyleDeclaration.prototype.getLonghandProperties):
            (WebInspector.CSSStyleDeclaration.prototype.getShorthandPriority):
            * inspector/front-end/InjectedScript.js:
            (InjectedScript.applyStyleText):
            (InjectedScript.toggleStyleEnabled):
            (InjectedScript._serializeStyle):
            (InjectedScript._getUniqueStyleProperties):
            (InjectedScript._getLonghandProperties):
            (InjectedScript._getShorthandPriority):
            * inspector/front-end/StylesSidebarPane.js:
            (WebInspector.StylesSidebarPane.prototype._update):
            (WebInspector.StylePropertiesSection.prototype.isPropertyOverloaded):
            (WebInspector.StylePropertiesSection.prototype.onpopulate):
            (WebInspector.StylePropertyTreeElement.prototype.get priority):
            (WebInspector.StylePropertyTreeElement.prototype.get value):
            (WebInspector.StylePropertyTreeElement.prototype):
            * inspector/front-end/utilities.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/StylesSidebarPane.js
+++ third_party/WebKit/WebCore/inspector/front-end/StylesSidebarPane.js
@@ -205,7 +205,7 @@ WebInspector.StylesSidebarPane.prototype = {
                     continue;
 
                 var style = styleRules[i].style;
-                var uniqueProperties = getUniqueStyleProperties(style);
+                var uniqueProperties = style.uniqueStyleProperties;
                 for (var j = 0; j < uniqueProperties.length; ++j) {
                     var name = uniqueProperties[j];
                     if (style.getPropertyPriority(name).length) {
@@ -438,7 +438,7 @@ WebInspector.StylePropertiesSection.prototype = {
 
         // Find out if any of the individual longhand properties of the shorthand
         // are used, if none are then the shorthand is overloaded too.
-        var longhandProperties = getLonghandProperties(this.styleRule.style, property);
+        var longhandProperties = this.styleRule.style.getLonghandProperties(property);
         for (var j = 0; j < longhandProperties.length; ++j) {
             var individualProperty = longhandProperties[j];
             if (individualProperty in this.usedProperties)
@@ -477,7 +477,7 @@ WebInspector.StylePropertiesSection.prototype = {
         var style = this.styleRule.style;
 
         var foundShorthands = {};
-        var uniqueProperties = getUniqueStyleProperties(style);
+        var uniqueProperties = style.uniqueStyleProperties;
         var disabledProperties = style.__disabledPropertyValues || {};
 
         for (var name in disabledProperties)
@@ -778,14 +778,14 @@ WebInspector.StylePropertyTreeElement.prototype = {
     {
         if (this.disabled && this.style.__disabledPropertyPriorities && this.name in this.style.__disabledPropertyPriorities)
             return this.style.__disabledPropertyPriorities[this.name];
-        return (this.shorthand ? getShorthandPriority(this.style, this.name) : this.style.getPropertyPriority(this.name));
+        return (this.shorthand ? this.style.getShorthandPriority(this.name) : this.style.getPropertyPriority(this.name));
     },
 
     get value()
     {
         if (this.disabled && this.style.__disabledPropertyValues && this.name in this.style.__disabledPropertyValues)
             return this.style.__disabledPropertyValues[this.name];
-        return (this.shorthand ? getShorthandValue(this.style, this.name) : this.style.getPropertyValue(this.name));
+        return (this.shorthand ? this.style.getShorthandValue(this.name) : this.style.getPropertyValue(this.name));
     },
 
     onattach: function()
@@ -1028,7 +1028,7 @@ WebInspector.StylePropertyTreeElement.prototype = {
         if (this.children.length || !this.shorthand)
             return;
 
-        var longhandProperties = getLonghandProperties(this.style, this.name);
+        var longhandProperties = this.style.getLonghandProperties(this.name);
         for (var i = 0; i < longhandProperties.length; ++i) {
             var name = longhandProperties[i];
 
@@ -1152,7 +1152,7 @@ WebInspector.StylePropertyTreeElement.prototype = {
         if (!this.originalCSSText) {
             // Remember the rule's original CSS text, so it can be restored
             // if the editing is canceled and before each apply.
-            this.originalCSSText = getStyleTextWithShorthands(this.style);
+            this.originalCSSText = this.style.styleTextWithShorthands();
         } else {
             // Restore the original CSS text before applying user changes. This is needed to prevent
             // new properties from sticking around if the user adds one, then removes it.
@@ -1275,6 +1275,7 @@ WebInspector.StylePropertyTreeElement.prototype = {
                 }
                 if (updateInterface)
                     self.updateTitle();
+                return;
             }
 
             var newPayload = result[0];
