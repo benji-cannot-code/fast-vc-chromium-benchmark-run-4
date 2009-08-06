cfde1ac26bd93b7bc6a41f089b695301bfe13230
gman@google.com <gman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-06 04:54:53 +0000

    oops, forgot these files
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22584 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/vertex_source_test.cc
+++ o3d/core/cross/vertex_source_test.cc
@@ -33,12 +33,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This file implements unit tests for class VertexSource.
 
-#include "core/cross/client.h"
 #include "tests/common/win/testing_common.h"
 #include "core/cross/vertex_source.h"
+#include "core/cross/error_status.h"
+#include "core/cross/evaluation_counter.h"
 #include "core/cross/fake_vertex_source.h"
 #include "core/cross/pointer_utils.h"
-#include "core/cross/evaluation_counter.h"
+#include "core/cross/buffer.h"
+#include "core/cross/object_manager.h"
+#include "core/cross/pack.h"
+#include "core/cross/service_dependency.h"
 
 namespace o3d {
 
