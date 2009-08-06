60a9d0d69d48db040233d8b67f00699430ee58a0
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-06 22:17:26 +0000

    Linux: Pass font settings through to interstitial renderers.
    BUG=18483
    TESTED=restarted and visited a page with a broken SSL cert a few times while changing font settings via gnome-appearance-properties
    
    Review URL: http://codereview.chromium.org/164094
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22679 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/interstitial_page.cc
+++ chrome/browser/tab_contents/interstitial_page.cc
@@ -18,6 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/tab_contents/tab_contents_view.h"
 #include "chrome/common/bindings_policy.h"
+#if defined(TOOLKIT_GTK)
+#include "chrome/common/gtk_util.h"
+#endif
 #include "chrome/common/notification_service.h"
 #include "grit/browser_resources.h"
 #include "net/base/escape.h"
@@ -131,6 +134,10 @@ InterstitialPage::InterstitialPage(TabContents* tab,
   // a page) when we have a pending entry (in the process of loading a new top
   // frame).
   DCHECK(new_navigation || !tab->controller().pending_entry());
+
+#if defined(TOOLKIT_GTK)
+  gtk_util::InitRendererPrefsFromGtkSettings(&renderer_preferences_);
+#endif
 }
 
 InterstitialPage::~InterstitialPage() {
--- chrome/common/gtk_util.h
+++ chrome/common/gtk_util.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef struct _GtkWidget GtkWidget;
 
+struct RendererPreferences;  // from common/renderer_preferences.h
+
 namespace event_utils {
 
 // Translates event flags into what kind of disposition they represent.
@@ -144,6 +146,10 @@ void SetLabelColor(GtkWidget* label, const GdkColor* color);
 
 // Adds the given widget to an alignment identing it by |kGroupIndent|.
 GtkWidget* IndentWidget(GtkWidget* content);
+
+// Initialize the font settings in |prefs| (used when creating new renderers)
+// based on GtkSettings (which itself comes from XSETTINGS).
+void InitRendererPrefsFromGtkSettings(RendererPreferences* prefs);
 
 }  // namespace gtk_util
 
