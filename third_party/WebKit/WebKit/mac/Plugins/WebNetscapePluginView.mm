0cb853c456f13e226193502dddfb7f7077a8b091
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-06 22:51:27 +0000

    2009-08-06  Anders Carlsson  <andersca@apple.com>
            Remove WebGraphicsExtras.h include.
            
            * Plugins/WebNetscapePluginView.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46866 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginView.mm
+++ third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginView.mm
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebDefaultUIDelegate.h"
 #import "WebFrameInternal.h" 
 #import "WebFrameView.h"
-#import "WebGraphicsExtras.h"
 #import "WebKitErrorsPrivate.h"
 #import "WebKitLogging.h"
 #import "WebNetscapeContainerCheckPrivate.h"
