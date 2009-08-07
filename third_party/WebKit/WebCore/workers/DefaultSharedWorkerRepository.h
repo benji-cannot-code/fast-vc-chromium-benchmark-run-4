37066c625cab84b64660867a93f5cf1cf9c5276c
atwilson@chromium.org <atwilson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-07 23:24:30 +0000

    WebCore:
    2009-08-07  Drew Wilson  <atwilson@google.com>
    
            Reviewed by David Levin.
    
            SharedWorkers should be shared
            https://bugs.webkit.org/show_bug.cgi?id=28058
    
            Added tracking of shared workers to the SharedWorkerRepository.
    
            * workers/DefaultSharedWorkerRepository.cpp:
            (WebCore::SharedWorkerProxy::create):
            Changed SharedWorkerProxy to derive from ThreadSafeShared, as it needs to outlive the repository (if closed while some documents are still loading).
            (WebCore::SharedWorkerProxy::thread):
            (WebCore::SharedWorkerProxy::closing):
            Added flag used to determine whether the worker has closed itself while loading.
            (WebCore::SharedWorkerProxy::url):
            (WebCore::SharedWorkerProxy::name):
            Proxy now tracks worker URL and name to allow validity checks per section 4.8.3 of the WebWorkers spec.
            (WebCore::SharedWorkerProxy::SharedWorkerProxy):
            (WebCore::SharedWorkerProxy::addToDocumentSet):
            Added API to track the document set for a worker to allow worker shutdown. Currently does nothing.
            (WebCore::SharedWorkerScriptLoader::SharedWorkerScriptLoader):
            Renamed SharedWorkerLoader to be SharedWorkerScriptLoader for clarity.
            (WebCore::SharedWorkerScriptLoader::load):
            (WebCore::SharedWorkerScriptLoader::notifyFinished):
            (WebCore::DefaultSharedWorkerRepository::instance):
            (WebCore::DefaultSharedWorkerRepository::workerScriptLoaded):
            Now handles "worker already loaded" case.
            (WebCore::SharedWorkerRepository::connect):
            Delegates to DefaultSharedWorkerRepository::connectToWorker
            (WebCore::DefaultSharedWorkerRepository::connectToWorker):
            Added code to track the SharedWorkerProxy in a map, and to handle the "worker already loaded" case.
            (WebCore::DefaultSharedWorkerRepository::getProxy):
            Atomic get-or-create operation on the internal tracking HashMap.
            * workers/DefaultSharedWorkerRepository.h:
            * workers/SharedWorker.cpp:
            (WebCore::SharedWorker::SharedWorker):
            * workers/SharedWorkerRepository.h:
            Removed instance() API and made constructor private to prevent instantiation.
            (WebCore::SharedWorkerRepository::SharedWorkerRepository):
    
    LayoutTests:
    
    2009-08-07  Drew Wilson  <atwilson@google.com>
    
            Reviewed by David Levin.
    
            SharedWorkers should be shared
            https://bugs.webkit.org/show_bug.cgi?id=28058
    
            Added tracking to the SharedWorkerRepository so instances of SharedWorkers are shared across pages.
    
            * fast/workers/shared-worker-shared-expected.txt: Added.
            * fast/workers/shared-worker-shared.html-disabled: Added sharing test.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46925 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/workers/DefaultSharedWorkerRepository.h
+++ third_party/WebKit/WebCore/workers/DefaultSharedWorkerRepository.h
@@ -35,25 +35,48 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(SHARED_WORKERS)
 
 #include "SharedWorkerRepository.h"
-
+#include "StringHash.h"
+#include <wtf/HashMap.h>
 #include <wtf/Noncopyable.h>
+#include <wtf/RefPtr.h>
+#include <wtf/Threading.h>
 
 namespace WebCore {
 
     class KURL;
+    class ScriptExecutionContext;
+    class SecurityOrigin;
+    class SharedWorkerProxy;
 
-    // Platform-specific implementation of the SharedWorkerRepository singleton.
-    class DefaultSharedWorkerRepository : public SharedWorkerRepository, public Noncopyable {
+    struct SecurityOriginHash;
+    struct SecurityOriginTraits;
+
+    // Platform-specific implementation of the SharedWorkerRepository static interface.
+    class DefaultSharedWorkerRepository : public Noncopyable {
     public:
+        // Invoked once the worker script has been loaded to fire up the worker thread.
+        void workerScriptLoaded(SharedWorkerProxy&, const String& userAgent, const String& workerScript, PassOwnPtr<MessagePortChannel>);
+
+        // Internal implementation of SharedWorkerRepository::connect()
+        void connectToWorker(PassRefPtr<SharedWorker>, PassOwnPtr<MessagePortChannel>, const KURL&, const String& name, ExceptionCode&);
+
+        static DefaultSharedWorkerRepository& instance();
+    private:
         DefaultSharedWorkerRepository();
         ~DefaultSharedWorkerRepository();
 
-        // Invoked once the worker script has been loaded to fire up the worker thread.
-        void workerScriptLoaded(const String& name, const KURL&, const String& userAgent, const String& workerScript, PassOwnPtr<MessagePortChannel>);
+        PassRefPtr<SharedWorkerProxy> getProxy(const String& name, const KURL&);
+        // Mutex used to protect internal data structures.
+        Mutex m_lock;
 
-        static DefaultSharedWorkerRepository* instance();
+        typedef HashMap<String, RefPtr<SharedWorkerProxy> > SharedWorkerNameMap;
+        typedef HashMap<RefPtr<SecurityOrigin>, SharedWorkerNameMap*, SecurityOriginHash> SharedWorkerProxyCache;
+
+        // Items in this cache may be freed on another thread, so all keys and values must be either copied before insertion or thread safe.
+        SharedWorkerProxyCache m_cache;
     };
-}
+
+} // namespace WebCore
 
 #endif // ENABLE(SHARED_WORKERS)
 
