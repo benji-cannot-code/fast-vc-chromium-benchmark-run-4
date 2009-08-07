5481c96b9f280d448ce5fa4d1918f18eb97b0032
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-07 07:32:03 +0000

    2009-08-07  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Adele Peterson.
    
            Cannot scroll for box-reflect:right
            https://bugs.webkit.org/show_bug.cgi?id=27979
    
            Update m_overflowLeft an m_overflowWidth for reflection just like
            m_overflowTop and m_overflowHeight.
    
            * fast/reflections/reflection-overflow-scroll-expected.txt: Added.
            * fast/reflections/reflection-overflow-scroll.html: Added.
            * fast/reflections/resources/reflection-overflow-scroll.js: Added.
            * platform/mac/fast/reflections/reflection-nesting-expected.txt:
            * platform/mac/fast/repaint/reflection-redraw-expected.txt:
    
    2009-08-07  Shinichiro Hamaji  <hamaji@chromium.org>
    
            Reviewed by Adele Peterson.
    
            Cannot scroll for box-reflect:right
            https://bugs.webkit.org/show_bug.cgi?id=27979
    
            Update m_overflowLeft an m_overflowWidth for reflection just like
            m_overflowTop and m_overflowHeight.
    
            Test: fast/reflections/reflection-overflow-scroll.html
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::layoutBlock):
    
    2009-08-07  Shinichiro Hamaji  <hamaji@chromium.org>
    
            Reviewed by Adele Peterson.
    
            Cannot scroll for box-reflect:right
            https://bugs.webkit.org/show_bug.cgi?id=27979
    
            Update m_overflowLeft an m_overflowWidth for reflection just like
            m_overflowTop and m_overflowHeight.
    
            * Scripts/make-js-test-wrappers:  Added regexp to skip box-shadow-overflo
    w-scroll.js
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46889 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/reflections/resources/reflection-overflow-scroll.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"Check if the size of reflected images are considered as overflow size. See this bug for detail: https://bugs.webkit.org/show_bug.cgi?id=27979."
+);
+
+var element = null;
+
+element = document.getElementById("horizontal");
+element.scrollLeft = 500;
+shouldBe("element.scrollLeft", "150");
+
+element = document.getElementById("vertical");
+element.scrollTop = 500;
+shouldBe("element.scrollTop", "80");
+
+// If we are in DTR, we don't need meaningless trailing messages.
+if (window.layoutTestController) {
+    document.getElementById("main").innerHTML = "";
+}
+
+var successfullyParsed = true;
