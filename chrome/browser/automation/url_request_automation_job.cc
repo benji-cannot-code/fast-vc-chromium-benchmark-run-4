87f3af70741f9d7458f52a6f2b267c0128fb83eb
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-07 20:52:58 +0000

    The UrlRequestAutomationJob::GetResponseInfo function needs to return a valid X509Certificate in the HttpResponseInfo structure passed in if the current URL scheme is https
    Not returning this certificate causes a number of DCHECKS to fire in the network stack implementation and in the renderer process, which eventually causes the navigation to fail.
    
    Fix is to return a dummy X509Certificate on the same lines as Gears.
    
    This fixes bug http://b/issue?id=2039699
    
    Bug=2039699
    
    Review URL: http://codereview.chromium.org/164110
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22784 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/url_request_automation_job.cc
+++ chrome/browser/automation/url_request_automation_job.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/automation/url_request_automation_job.h"
 
 #include "base/message_loop.h"
+#include "base/time.h"
 #include "chrome/browser/automation/automation_resource_message_filter.h"
 #include "chrome/browser/renderer_host/render_view_host.h"
 #include "chrome/browser/renderer_host/resource_dispatcher_host.h"
@@ -16,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_context.h"
 
+using base::Time;
+using base::TimeDelta;
 
 // The list of filtered headers that are removed from requests sent via
 // StartAsync(). These must be lower case.
@@ -157,7 +160,19 @@ void URLRequestAutomationJob::GetResponseInfo(net::HttpResponseInfo* info) {
   if (headers_)
     info->headers = headers_;
   if (request_->url().SchemeIsSecure()) {
-    // TODO(joshia): fill up SSL related fields.
+    // Make up a fake certificate for this response since we don't have
+    // access to the real SSL info.
+    const char* kCertIssuer = "Chrome Internal";
+    const int kLifetimeDays = 100;
+
+    info->ssl_info.cert =
+        new net::X509Certificate(request_->url().GetWithEmptyPath().spec(),
+                                 kCertIssuer,
+                                 Time::Now(),
+                                 Time::Now() +
+                                     TimeDelta::FromDays(kLifetimeDays));
+    info->ssl_info.cert_status = 0;
+    info->ssl_info.security_bits = 0;
   }
 }
 
