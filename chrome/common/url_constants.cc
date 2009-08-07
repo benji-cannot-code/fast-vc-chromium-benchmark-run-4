df17e130e6a58750bfe37fed3a29155a6682b8b8
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-07 17:37:20 +0000

    Removing unused #include from url_constants.cc.
    Patch by Thiago Farina, original review: http://codereview.chromium.org/165113
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/165135
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22733 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/url_constants.h"
 
-// TODO(port): Remove this header when last ifdef is removed from this file.
-#include "build/build_config.h"
-
 namespace chrome {
 
 const char kAboutScheme[] = "about";
