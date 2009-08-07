0df37e6f14cdfc54fb52c11afb7a3e282b36dd3d
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-07 04:11:00 +0000

    2009-08-06  Norbert Leser  <norbert.leser@nokia.com>
            Reviewed by Darin Adler.
    
            Updated patch for bug #27059:
            Symbian platform always uses little endian encoding,
            regardless of compiler.
            We need to make sure that we correctly detect EABI architecture
            for armv5 targets on Symbian,
            where __EABI__ is set but not __ARM_EABI__
    
            * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -221,10 +221,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_PLATFORM_ARM 1
 #if defined(__ARMEB__)
 #define WTF_PLATFORM_BIG_ENDIAN 1
-#elif !defined(__ARM_EABI__) && !defined(__ARMEB__) && !defined(__VFP_FP__)
+#elif !defined(__ARM_EABI__) && !defined(__EABI__) && !defined(__VFP_FP__)
 #define WTF_PLATFORM_MIDDLE_ENDIAN 1
 #endif
-#if !defined(__ARM_EABI__)
+#if !defined(__ARM_EABI__) && !defined(__EABI__)
 #define WTF_PLATFORM_FORCE_PACK 1
 #endif
 #define ARM_ARCH_VERSION 3
