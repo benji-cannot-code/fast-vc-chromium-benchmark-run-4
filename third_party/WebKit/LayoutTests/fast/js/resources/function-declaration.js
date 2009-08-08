da3377f5eedcd92cff35fe7c6ffe312b1bc19139
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-08 05:17:29 +0000

    Test function declaration parsing
    Reviewed by Cameron Zwarich
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46950 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/resources/function-declaration.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This test ensures we correctly parse (or not) various function declarations"
+);
+
+shouldBeUndefined("eval('function f(){return true;}')");
+shouldBeTrue("eval('function f(){return true;};f')()");
+shouldThrow("eval('function(){return false;}')()");
+
+successfullyParsed = true;
