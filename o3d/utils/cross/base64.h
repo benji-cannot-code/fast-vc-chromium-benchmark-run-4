c54a8b385e998dd3a06b8c5a2349f2e51ab2970c
gman@google.com <gman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-08 21:22:57 +0000

    fix for gcc
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22871 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/utils/cross/base64.h
+++ o3d/utils/cross/base64.h
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef O3D_UTILS_CROSS_BASE64_H_
 #define O3D_UTILS_CROSS_BASE64_H_
 
+#include <stddef.h>
+
 namespace o3d {
 namespace base64 {
 
