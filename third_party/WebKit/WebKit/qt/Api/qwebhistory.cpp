d0c50a440969366fceea904830295d240bfcc5b3
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-08 09:29:20 +0000

    Doc: there is no group of explicitly shared classes, only one class uses this.
    Patch by Volker Hilsheimer <volker.hilsheimer@nokia.com> on 2009-08-08
    Reviewed by Simon Hausmann.
    
    Explain the implications in the QWebHistoryItem documentation, and get rid
    of the "group".
    
    * Api/qwebhistory.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46953 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebhistory.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebhistory.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /*!
   \class QWebHistoryItem
-  \ingroup explicitly-shared
   \since 4.4
   \brief The QWebHistoryItem class represents one item in the history of a QWebPage
 
@@ -53,13 +52,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   \row    \o userData()    \o The user specific data that was stored with the history item.
   \endtable
 
-  \note QWebHistoryItem objects are value based and \l{explicitly shared}.
+  \note QWebHistoryItem objects are value based, but \e{explicitly shared}. Changing
+  a QWebHistoryItem instance by calling setUserData() will change all copies of that
+  instance.
 
   \sa QWebHistory, QWebPage::history(), QWebHistoryInterface
 */
 
 /*!
-  Constructs a history item from \a other.
+  Constructs a history item from \a other. The new item and \a other
+  will share their data, and modifying either this item or \a other will
+  modify both instances.
 */
 QWebHistoryItem::QWebHistoryItem(const QWebHistoryItem &other)
     : d(other.d)
@@ -67,7 +70,9 @@ QWebHistoryItem::QWebHistoryItem(const QWebHistoryItem &other)
 }
 
 /*!
-  Assigns the \a other history item to this.
+  Assigns the \a other history item to this. This item and \a other
+  will share their data, and modifying either this item or \a other will
+  modify both instances.
 */
 QWebHistoryItem &QWebHistoryItem::operator=(const QWebHistoryItem &other)
 {
@@ -164,6 +169,8 @@ QVariant QWebHistoryItem::userData() const
   \since 4.5
 
  Stores user specific data \a userData with the history item.
+ 
+ \note All copies of this item will be modified.
 
  \sa userData()
 */
