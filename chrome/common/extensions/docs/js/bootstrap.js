79cbcedd7759ad6f6136c4cf738ed3cca9158e49
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-09 22:38:16 +0000

    Fix the extension doc page titles and flicker a bit.
    TBR=rafaelw@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22887 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/js/bootstrap.js
+++ chrome/common/extensions/docs/js/bootstrap.js
@@ -1,12 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Hide body content initially to minimize flashing.
+document.write('<style type="text/css">');
+document.write('body { display:none; }');
+document.write('</style>');
+
 window.onload = function() {
   // Regenerate page if we are passed the "?regenerate" search param
   // or if the user-agent is chrome AND the document is being served
   // from the file:/// scheme.
   if (window.location.search == "?regenerate" ||
       navigator.userAgent.indexOf("Chrome") > -1) {
-    // Hide body content initially to minimize flashing.
-    document.getElementsByTagName("body")[0].className = "hidden";
     window.renderPage();
   }
 }
