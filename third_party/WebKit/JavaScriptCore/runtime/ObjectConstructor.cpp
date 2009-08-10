470321dca1a9863edf122e95951f06a6ed9c1dba
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-10 08:01:37 +0000

    Fix compilation with the interpreter instead of the JIT by including PrototypeFunction.h as forward-declared through NativeFunctionWrapper.h.
    Patch by Simon Hausmann <simon.hausmann@nokia.com> on 2009-08-10
    * runtime/ObjectConstructor.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46970 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/ObjectConstructor.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/ObjectConstructor.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSFunction.h"
 #include "JSGlobalObject.h"
 #include "ObjectPrototype.h"
+#include "PrototypeFunction.h"
 
 namespace JSC {
 
