78a1c7ba283386f93463cda562ae2e55089fb590
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-10 17:56:57 +0000

    Remove the redundant, Windows CE-only GlyphBuffer::advnaceData()
    Reviewed by Anders Carlsson.
    
    * platform/graphics/GlyphBuffer.h: Removed advanceData().
    * platform/graphics/wince/GraphicsContextWince.cpp:
    (WebCore::GraphicsContext::drawText): Changed to use advances(from)
        instead of advanceData() + from.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@46985 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/GlyphBuffer.h
+++ third_party/WebKit/WebCore/platform/graphics/GlyphBuffer.h
@@ -132,13 +132,6 @@ public:
 #endif
     }
 
-#if PLATFORM(WINCE)
-    const GlyphBufferAdvance* advanceData() const
-    {
-        return m_advances.data();
-    }
-#endif
-
     FloatSize offsetAt(int index) const
     {
 #if PLATFORM(WIN)
