2e16800f1f9512a597af108e238cf0b74da47c85
dimich@chromium.org <dimich@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-11 02:18:17 +0000

    WebKit/mac: Added support for overriding default preferences per-test. Originally implemented by Glenn Wilson <gwilson@chromium.org>. Reviewed by Adam Roben. Added new methods for overriding default WebPreference values and for resetting preferences to their defaults. https://bugs.webkit.org/show_bug.cgi?id=20534
    * WebView/WebPreferences.mm:
    (-[WebPreferences _setPreferenceForTestWithValue:withKey:]): added.
    * WebView/WebPreferencesPrivate.h: same.
    
    WebKit/win: Added support for overriding default preferences per-test.
    Originally implemented by Glenn Wilson  <gwilson@chromium.org>.
    Reviewed by Adam Roben.
    Added support for overriding default preferences per-test.
    https://bugs.webkit.org/show_bug.cgi?id=20534
    
    * Interfaces/IWebPreferencesPrivate.idl: added new method setPreferenceForTest
    * WebPreferences.cpp: same.
    * WebPreferences.h: same.
    
    WebKitTools: Added support for overriding default preferences per-test.
    Originally implemented by Glenn Wilson  <gwilson@chromium.org>.
    Reviewed by Adam Roben.
    Added support for overriding default preferences per-test.
    https://bugs.webkit.org/show_bug.cgi?id=20534
    
    * DumpRenderTree/LayoutTestController.cpp:
    (overridePreferenceCallback): add wiring for layoutTestController.overridePreference.
    (LayoutTestController::staticFunctions): same.
    * DumpRenderTree/LayoutTestController.h: same.
    * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
    (LayoutTestController::overridePreference): added empty overridePreference method.
    * DumpRenderTree/mac/DumpRenderTree.mm: implemented preference override.
    (resetDefaultsToConsistentValues): new method, resets preferences to same set of value before every test.
    (setDefaultsToConsistentValuesForTesting): new method, sets other details of testing environment, every time  DRT starts.
    (resetWebViewToConsistentStateBeforeTesting): move some preference setting from here to new resetDefaultsToConsistentValues().
    * DumpRenderTree/mac/LayoutTestControllerMac.mm:
    (LayoutTestController::overridePreference):
    * DumpRenderTree/win/DumpRenderTree.cpp:
    (resetDefaultsToConsistentValues): new method, resets preferences to same set of value before every test.
    (resetWebViewToConsistentStateBeforeTesting): move some preference setting from here to new resetDefaultsToConsistentValues().
    (createWebViewAndOffscreenWindow): same.
    (main): same.
    * DumpRenderTree/win/LayoutTestControllerWin.cpp:
    (LayoutTestController::overridePreference):
    * DumpRenderTree/wx/LayoutTestControllerWx.cpp:
    (LayoutTestController::overridePreference):
    
    LayoutTests: Added support for overriding default preferences per-test.
    Originally implemented by Glenn Wilson  <gwilson@chromium.org>.
    Reviewed by Adam Roben.
    Added tests to verify that default preferences can be overridden per-test
    and that overridden values are reset between each test.
    https://bugs.webkit.org/show_bug.cgi?id=20534
    
    * fast/harness/override-preferences-2-expected.txt: Added.
    * fast/harness/override-preferences-2.html: Added. Verifies overridePreverence("WebKitDefaultFontSize", "24").
    * fast/harness/override-preferences-expected.txt: Added.
    * fast/harness/override-preferences.html: Added. Verifies overridePreference("WebKitJavaScriptEnabled", false).
    * fast/harness/override-zzz-reset-expected.txt: Added.
    * fast/harness/override-zzz-reset.html: Added. Because of 'zzz' this test will run after the tests above and
    verify that override of preferences does not 'spill' to the subsequent tests in a batch.
    * platform/gtk/Skipped: Tests above disabled for GTK until GTK DRT implements preference override.
    * platform/qt/Skipped: same for QT.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47015 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.h
@@ -54,6 +54,7 @@ public:
     bool isCommandEnabled(JSStringRef name);
     void keepWebHistory();
     void notifyDone();
+    void overridePreference(JSStringRef key, JSStringRef value);
     JSStringRef pathToLocalResource(JSContextRef, JSStringRef url);
     void queueBackNavigation(int howFarBackward);
     void queueForwardNavigation(int howFarForward);
