e8356c9484dc196253404d655b45d70d1c9c8c56
suzhe@chromium.org <suzhe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-11 01:32:49 +0000

    Cleanup GtkIMContext related code by splitting it into separated source file.
    This CL splits GtkIMContext related code into separated source files from render_widget_host_view_gtk.{h,cc}, making the code clearer to read. It also fixes issue 18061.
    Focus moving issues related to AutocompleteEditViewGtk will be addressed in another CL, see issue 18393 for reference.
    
    BUG=18061: pressing tab on form textfields goes directly to submit or login buttons
    TEST=Starts scim's xim server with command "scim -d", then launches chrome with GTK_IM_MODULE=xim. Opens a web page with multiple input box, such as gmail.com, presses Tab and Shift-Tab multiple times to see if focus moves among focusable elements in the web page.
    
    Review URL: http://codereview.chromium.org/160577
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22996 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_gtk.h
+++ chrome/browser/renderer_host/render_widget_host_view_gtk.h
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_RENDERER_HOST_RENDER_WIDGET_HOST_VIEW_GTK_H_
 
 #include <gdk/gdk.h>
+
+#include <map>
 #include <vector>
+#include <string>
 
 #include "base/gfx/native_widget_types.h"
 #include "base/scoped_ptr.h"
@@ -19,18 +22,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class RenderWidgetHost;
 // A conveience wrapper class for GtkIMContext;
-class RenderWidgetHostViewGtkIMContext;
+class GtkIMContextWrapper;
 
 typedef struct _GtkClipboard GtkClipboard;
 typedef struct _GtkSelectionData GtkSelectionData;
-typedef struct _GtkIMContext GtkIMContext;
 
 // -----------------------------------------------------------------------------
 // See comments in render_widget_host_view.h about this class and its members.
 // -----------------------------------------------------------------------------
 class RenderWidgetHostViewGtk : public RenderWidgetHostView {
  public:
-  RenderWidgetHostViewGtk(RenderWidgetHost* widget);
+  explicit RenderWidgetHostViewGtk(RenderWidgetHost* widget);
   ~RenderWidgetHostViewGtk();
 
   // Initialize this object for use as a drawing area.
@@ -125,7 +127,7 @@ class RenderWidgetHostViewGtk : public RenderWidgetHostView {
   bool was_focused_before_grab_;
 
   // A conveience wrapper object for GtkIMContext;
-  scoped_ptr<RenderWidgetHostViewGtkIMContext> im_context_;
+  scoped_ptr<GtkIMContextWrapper> im_context_;
 
   // Helper class that lets us allocate plugin containers and move them.
   GtkPluginContainerManager plugin_container_manager_;
--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -1482,6 +1482,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/renderer_host/download_throttling_resource_handler.h',
         'browser/renderer_host/file_system_accessor.cc',
         'browser/renderer_host/file_system_accessor.h',
+        'browser/renderer_host/gtk_im_context_wrapper.cc',
+        'browser/renderer_host/gtk_im_context_wrapper.h',
         'browser/renderer_host/render_process_host.cc',
         'browser/renderer_host/render_process_host.h',
         'browser/renderer_host/render_sandbox_host_linux.h',
