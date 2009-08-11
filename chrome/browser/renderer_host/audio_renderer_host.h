fc568844c85b44957e9f8ca1efac1a77ce81ae41
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-11 19:09:28 +0000

    AudioRendererHost send ViewMsg_AudioStreamState
    AudioRendererHost should use ViewMsg_AudioStreamState to
    notify renderer of its state instead of AudioOutputStream::State.
    The enum of AudioOutputStream::State is not used anywhere, thus
    removed.
    
    TEST=unit_tests --gtest_filter=Audio*
    
    Review URL: http://codereview.chromium.org/165255
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23061 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/audio_renderer_host.h
+++ chrome/browser/renderer_host/audio_renderer_host.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //          .--------->  [ Stopped ]  <--------.
 //          |                ^                 |
 //          |                |                 |
-//    *[ Created ]  -->  [ Started ]  -->  [ Paused ]
+//    *[ Created ]  -->  [ Playing ]  -->  [ Paused ]
 //                           ^                 |
 //                           |                 |
 //                           `-----------------`
@@ -49,8 +49,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //      |    <<<<<< RequestAudioPacket <<<<<<<<        |
 //      |    >>>>>>> AudioPacketReady >>>>>>>>>        |
 //      |                                              |
-//      |    >>>>>>>>>>>>> Start >>>>>>>>>>>>>>        |
-//      |    <<<<<<<<<<<< Started <<<<<<<<<<<<<        |  time
+//      |    >>>>>>>>>>>>> Play >>>>>>>>>>>>>>>        |
+//      |    <<<<<<<<<<<< Playing <<<<<<<<<<<<<        |  time
 //      |                   ...                        |
 //      |    <<<<<< RequestAudioPacket <<<<<<<<        |
 //      |    >>>>>>> AudioPacketReady >>>>>>>>>        |
@@ -127,9 +127,7 @@ class AudioRendererHost : public base::RefCountedThreadSafe<AudioRendererHost> {
   virtual void Send(IPC::Message* message);
 
   // A helper method for sending error IPC messages.
-  virtual void SendErrorMessage(int32 render_view_id,
-                                int32 stream_id,
-                                int info);
+  virtual void SendErrorMessage(int32 render_view_id, int32 stream_id);
 
   // A helper method for calling OnDestroySource on IO thread.
   virtual void DestroySource(IPCAudioSource* source);
@@ -144,6 +142,15 @@ class AudioRendererHost : public base::RefCountedThreadSafe<AudioRendererHost> {
   // via IPC.
   class IPCAudioSource : public AudioOutputStream::AudioSourceCallback {
    public:
+    // Internal state of the source.
+    enum State {
+      kCreated,
+      kPlaying,
+      kPaused,
+      kClosed,
+      kError,
+    };
+
     // Factory method for creating an IPCAudioSource, returns NULL if failed.
     // The IPCAudioSource object will have an internal state of
     // AudioOutputStream::STATE_CREATED after creation.
@@ -171,7 +178,7 @@ class AudioRendererHost : public base::RefCountedThreadSafe<AudioRendererHost> {
     // Starts the playback of this audio output stream. The internal state will
     // be updated to AudioOutputStream::STATE_STARTED and the state update is
     // sent to the renderer.
-    void Start();
+    void Play();
 
     // Pause this audio output stream. The audio output stream will stop
     // reading from the |push_source_|. The internal state will be updated
@@ -237,7 +244,7 @@ class AudioRendererHost : public base::RefCountedThreadSafe<AudioRendererHost> {
     size_t decoded_packet_size_;
     size_t buffer_capacity_;
 
-    AudioOutputStream::State state_;
+    State state_;
     base::SharedMemory shared_memory_;
     PushSource push_source_;
 
@@ -270,11 +277,11 @@ class AudioRendererHost : public base::RefCountedThreadSafe<AudioRendererHost> {
                       const ViewHostMsg_Audio_CreateStream& params);
 
   // Starts buffering for the audio output stream. Delegates the start method
-  // call to the corresponding IPCAudioSource::Start().
+  // call to the corresponding IPCAudioSource::Play().
   // ViewMsg_NotifyAudioStreamStateChanged with
   // AudioOutputStream::AUDIO_STREAM_ERROR is sent back to renderer if the
   // required IPCAudioSource is not found.
-  void OnStartStream(const IPC::Message& msg, int stream_id);
+  void OnPlayStream(const IPC::Message& msg, int stream_id);
 
   // Pauses the audio output stream. Delegates the pause method call to the
   // corresponding IPCAudioSource::Pause(),
--- media/audio/audio_output.h
+++ media/audio/audio_output.h
@@ -42,15 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // a given AudioOutputStream might or might not talk directly to hardware.
 class AudioOutputStream {
  public:
-  enum State {
-    STATE_CREATED = 0,  // The output stream is created.
-    STATE_STARTED,      // The output stream is started.
-    STATE_PAUSED,       // The output stream is paused.
-    STATE_STOPPED,      // The output stream is stopped.
-    STATE_CLOSED,       // The output stream is closed.
-    STATE_ERROR,        // The output stream is in error state.
-  };
-
   // Audio sources must implement AudioSourceCallback. This interface will be
   // called in a random thread which very likely is a high priority thread. Do
   // not rely on using this thread TLS or make calls that alter the thread
