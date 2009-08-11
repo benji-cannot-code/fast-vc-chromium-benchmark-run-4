541717e24bb9c739d2bdee8290b2c0907d9725e1
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-11 21:31:17 +0000

    First Run Import Progress UI.
    First run code needed some reworking in order to get things in order for progress notification.
    
    BUG=18773
    TEST=When importing another browser's settings, a progress dialog should be displayed.
    
    Review URL: http://codereview.chromium.org/164290
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23080 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/first_run_dialog.h
+++ chrome/browser/cocoa/first_run_dialog.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Called when the "Learn More" button is pressed.
 - (IBAction)learnMore:(id)sender;
 
-// Properties for bindings
+// Properties for bindings.
 @property(assign) BOOL userDidCancel;
 @property(assign) BOOL statsEnabled;
 @property(assign) BOOL makeDefaultBrowser;
