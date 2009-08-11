5ab12c385c3ad46da9aff507ad6748e9977c7c9b
pkasting@chromium.org <pkasting@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-11 23:20:07 +0000

    https://bugs.webkit.org/show_bug.cgi?id=27965 Move cairo/ImageSourceCairo.cpp to ImageSource.cpp in preparation for merging most of the other ImageSource*.cpp files with it.
    Reviewed by Adam Treat.
    
    * GNUmakefile.am:
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * platform/graphics/ImageSource.cpp: Copied from WebCore/platform/graphics/cairo/ImageSourceCairo.cpp.
    * platform/graphics/cairo/ImageSourceCairo.cpp: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47073 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/ImageSource.cpp
+++ third_party/WebKit/WebCore/platform/graphics/ImageSource.cpp
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PNGImageDecoder.h"
 #include "XBMImageDecoder.h"
 #include "SharedBuffer.h"
-#include <cairo.h>
 
 namespace WebCore {
 
