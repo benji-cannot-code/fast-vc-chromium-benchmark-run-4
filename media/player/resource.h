db7a274ff8f579e8a5b38c7f9abb3b7e7053441e
fbarchard@chromium.org <fbarchard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-11 19:33:06 +0000

    media player cleanup of old unused menu items BUG=17415 TEST=options menu has no greyed out items in it.  about box date updated.
    Review URL: http://codereview.chromium.org/164334
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23067 0039d316-1c4b-4281-b951-d872f2087c98

--- media/player/resource.h
+++ media/player/resource.h
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ID_PLAY_PLAY                    32781
 #define ID_PLAY_PLAY_PAUSE              32782
 #define ID_OPTIONS_EXIT                 32840
-#define ID_OPTIONS_OPENMP               32790
-#define ID_OPTIONS_SWSCALER             32791
 #define ID_OPTIONS_DRAW                 32792
 #define ID_OPTIONS_AUDIO                32793
 #define ID_OPTIONS_DUMPYUVFILE          32794
