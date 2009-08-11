7baafcafc665e5f9e252cc24c58d332522cf8e92
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-11 00:03:56 +0000

    Linux system libs: build fix.
    net_unittests was broken when using the system zlib.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22979 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/gzip_filter_unittest.cc
+++ net/base/gzip_filter_unittest.cc
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iostream>
 
 #if defined(USE_SYSTEM_ZLIB)
+// The code below uses the MOZ_Z_ forms of these functions in order that things
+// should work on Windows. In order to make this code cross platform, we map
+// back to the normal functions here in the case that we are using the system
+// zlib.
+#define MOZ_Z_deflate deflate
+#define MOZ_Z_deflateEnd deflateEnd
 #include <zlib.h>
 #else
 #include "third_party/zlib/zlib.h"
