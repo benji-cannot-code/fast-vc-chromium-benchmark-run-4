0a5f8ccaf957681858e501222864e260ffd6f091
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-12 19:19:28 +0000

    Make FtpNetworkTransaction report its load state.
    TEST=Covered by net_unittests.
    http://crbug.com/19095
    
    Review URL: http://codereview.chromium.org/164354
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23201 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_new_ftp_job.cc
+++ net/url_request/url_request_new_ftp_job.cc
@@ -102,6 +102,11 @@ void URLRequestNewFtpJob::Kill() {
   URLRequestJob::Kill();
 }
 
+net::LoadState URLRequestNewFtpJob::GetLoadState() const {
+  return transaction_.get() ?
+      transaction_->GetLoadState() : net::LOAD_STATE_IDLE;
+}
+
 bool URLRequestNewFtpJob::ReadRawData(net::IOBuffer* buf,
                                       int buf_size,
                                       int *bytes_read) {
