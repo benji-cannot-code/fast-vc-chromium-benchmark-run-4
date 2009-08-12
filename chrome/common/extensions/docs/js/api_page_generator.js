587923b03e120bce73fc934292d7d6f62756def1
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-12 19:18:24 +0000

    Add chrome.extension to extension_api.json and docs.
    This required making it so that extension_api.json definitions defer to existing bindings so that the chrome.extension object can remain setup in renderer_extension_bindings.js.
    
    Also fixed a minor bug with error handling in the api_generator.js
    
    Moved "Port" type to the new extension module.
    
    Removed a bunch of trailing whitespace from the extension_api.json.
    
    Review URL: http://codereview.chromium.org/165323
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23200 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/js/api_page_generator.js
+++ chrome/common/extensions/docs/js/api_page_generator.js
@@ -120,8 +120,6 @@ function fetchContent(url, onSuccess, onError) {
         if (xhr.responseText) {
           window.clearTimeout(abortTimerId);
           onSuccess(xhr.responseText);
-        } else {
-          handleError("responseText empty.");
         }
       }
     }
