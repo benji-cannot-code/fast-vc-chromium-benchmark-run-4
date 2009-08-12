bf1061f56409b8ada21094eb7db6a4e390035241
ariya@webkit.org <ariya@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-12 13:55:31 +0000

    WebCore/ChangeLog:
    2009-08-12  Ariya Hidayat  <ariya.hidayat@nokia.com>
    
            Reviewed by Holger Freyther.
    
            [Qt] Fix box-shadow painting in r47103. The edge should not be blurry.
            Also optimize by not having to save and restore painter state.
    
            * platform/graphics/qt/GraphicsContextQt.cpp:
            (WebCore::GraphicsContext::drawRect):
            (WebCore::GraphicsContext::fillRect):
    
    LayoutTests/ChangeLog:
    
    2009-08-12  Ariya Hidayat  <ariya.hidayat@nokia.com>
    
            Reviewed by Holger Hans Peter Freyther.
    
            [Qt] Update the pixel tests for shadow edge fix.
    
            * platform/qt/fast/box-shadow/basic-shadows-expected.checksum:
            * platform/qt/fast/box-shadow/basic-shadows-expected.png:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
@@ -420,12 +420,10 @@ void GraphicsContext::drawRect(const IntRect& rect)
     int shadowBlur;
     Color shadowColor;
     if (textDrawingMode() == cTextFill && getShadow(shadowSize, shadowBlur, shadowColor)) {
-        p->save();
-        p->translate(shadowSize.width(), shadowSize.height());
-        p->setPen(QColor(shadowColor));
-        p->setBrush(QBrush(QColor(shadowColor)));
-        p->drawRect(rect);
-        p->restore();
+        IntRect shadowRect = rect;
+        shadowRect.move(shadowSize.width(), shadowSize.height());
+        shadowRect.inflate(p->pen().widthF());
+        p->fillRect(shadowRect, QColor(shadowColor));
     }
 
     p->drawRect(rect);
@@ -721,12 +719,10 @@ void GraphicsContext::fillRect(const FloatRect& rect, const Color& c)
     Color shadowColor;
     QPainter* p = m_data->p();
     if (textDrawingMode() == cTextFill && getShadow(shadowSize, shadowBlur, shadowColor)) {
-        p->save();
-        p->translate(shadowSize.width(), shadowSize.height());
-        p->setPen(QColor(shadowColor));
-        p->setBrush(QBrush(QColor(shadowColor)));
-        p->drawRect(rect);
-        p->restore();
+        FloatRect shadowRect = rect;
+        shadowRect.move(shadowSize.width(), shadowSize.height());
+        shadowRect.inflate(p->pen().widthF());
+        p->fillRect(shadowRect, QColor(shadowColor));
     }
     p->fillRect(rect, m_data->solidColor);
 }
