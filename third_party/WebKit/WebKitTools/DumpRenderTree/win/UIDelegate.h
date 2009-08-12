a9a1c1bd64c59a0ab256199ea569e1f763b5ce12
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-12 03:30:17 +0000

    2009-08-11  John Gregg  <johnnyg@google.com>
    Reviewed by Maciej Stachowiak.
    
    WebCore:
    Move the accessor for notifications presenter from WebUIDelegate
    to WebUIDelegate2 which extends it.  When making this call from
    WebCoreSupport, check using QueryInterface before calling.
    
    https://bugs.webkit.org/show_bug.cgi?id=28198
    
    * Interfaces/IWebUIDelegate.idl:
    * Interfaces/IWebUIDelegate2.idl: Added.
    * Interfaces/WebKit.idl:
    * WebCoreSupport/WebDesktopNotificationsDelegate.cpp:
    (WebDesktopNotificationsDelegate::show):
    (WebDesktopNotificationsDelegate::cancel):
    (WebDesktopNotificationsDelegate::notificationObjectDestroyed):
    (WebDesktopNotificationsDelegate::requestPermission):
    (WebDesktopNotificationsDelegate::checkPermission):
    (WebDesktopNotificationsDelegate::hasNotificationDelegate):
    (WebDesktopNotificationsDelegate::notificationDelegate):
    * WebCoreSupport/WebDesktopNotificationsDelegate.h:
    
    DumpRenderTree:
    Switch DumpRenderTree to contain a WebUIDelegate2, which extends
    WebUIDelegate, so that the notifications tests will still work.
    https://bugs.webkit.org/show_bug.cgi?id=28198
    
    * DumpRenderTree/win/UIDelegate.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47084 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/win/UIDelegate.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/win/UIDelegate.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class DRTUndoManager;
 class DRTDesktopNotificationPresenter;
 
-class UIDelegate : public IWebUIDelegate, IWebUIDelegatePrivate {
+class UIDelegate : public IWebUIDelegate2, IWebUIDelegatePrivate {
 public:
     UIDelegate();
 
