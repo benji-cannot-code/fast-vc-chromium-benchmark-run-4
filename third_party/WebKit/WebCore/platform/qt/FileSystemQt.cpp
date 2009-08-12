f7c52d7bcb44741768e8da96211aa70adba1277a
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-12 16:41:59 +0000

    2009-08-12  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Eric Seidel.
    
            HTML 5 database and LocalStorage is not persistent on the Symbian port of QtWebKit
            https://bugs.webkit.org/show_bug.cgi?id=28175
    
            Call QDir::toNativeSeparators() before the path is passed to the OS.
    
            Based on work by David Leong.
    
            * platform/qt/FileSystemQt.cpp:
            (WebCore::pathByAppendingComponent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47114 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/FileSystemQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/FileSystemQt.cpp
@@ -82,7 +82,7 @@ bool makeAllDirectories(const String& path)
 
 String pathByAppendingComponent(const String& path, const String& component)
 {
-    return QDir(path).filePath(component);
+    return QDir::toNativeSeparators(QDir(path).filePath(component));
 }
 
 String homeDirectoryPath()
