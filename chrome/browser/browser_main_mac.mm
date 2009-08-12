61560c7f71c9a30bce1a5711a3174a984846477a
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-12 19:00:42 +0000

    Remove TransformProcessType from the browser process now that LSUIElement is gone.
    BUG=8044
    TEST=Dock icon still works properly
    Review URL: http://codereview.chromium.org/165384
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23196 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main_mac.mm
+++ chrome/browser/browser_main_mac.mm
@@ -3,17 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <crt_externs.h>
-#include <Cocoa/Cocoa.h>
+#import <Cocoa/Cocoa.h>
 #include "base/command_line.h"
-#include "chrome/app/keystone_glue.h"
-#include "chrome/browser/app_controller_mac.h"
+#import "chrome/app/keystone_glue.h"
 #include "chrome/browser/browser_main_win.h"
 #include "chrome/common/result_codes.h"
 
 namespace Platform {
 
-// Perform and platform-specific work that needs to be done before the main
+// Perform any platform-specific work that needs to be done before the main
 // message loop is created and initialized.
 //
 // For Mac, this involves telling Cooca to finish its initalization, which we
@@ -23,32 +21,13 @@ namespace Platform {
 // load the main nib directly. The main event loop is run from common code using
 // the MessageLoop API, which works out ok for us because it's a wrapper around
 // CFRunLoop.
-void WillInitializeMainMessageLoop(const CommandLine & command_line) {
+void WillInitializeMainMessageLoop(const CommandLine& command_line) {
   [NSApplication sharedApplication];
   [NSBundle loadNibNamed:@"MainMenu" owner:NSApp];
 
-  // Doesn't need to be in a GOOGLE_CHROME_BUILD since this references
-  // a framework only distributed with Google Chrome.
+  // This is a no-op if the KeystoneRegistration framework is not present.
+  // The framework is only distributed with branded Google Chrome builds.
   [[KeystoneGlue defaultKeystoneGlue] registerWithKeystone];
-
-  // TODO(port): Use of LSUIElement=1 is a temporary fix.  The right
-  // answer is to fix the renderer to not use Cocoa.
-  //
-  // Chromium.app currently as LSUIElement=1 in it's Info.plist.  This
-  // allows subprocesses (created with a relaunch of our binary) to
-  // create an NSApplication without showing up in the dock.
-  // Subprocesses (such as the renderer) need an NSApplication for
-  // Cocoa happiness However, for the browser itself, we DO want it in
-  // the dock.  These 3 lines make it happen.
-  //
-  // Real fix tracked by http://code.google.com/p/chromium/issues/detail?id=8044
-  ProcessSerialNumber psn;
-  GetCurrentProcess(&psn);
-  TransformProcessType(&psn, kProcessTransformToForegroundApplication);
-  // Fix the menubar.  Ugly.  To be removed along with the rest of the
-  // LSUIElement stuff.
-  [NSApp deactivate];
-  [NSApp activateIgnoringOtherApps:YES];
 }
 
 // Perform platform-specific work that needs to be done after the main event
@@ -60,7 +39,7 @@ void WillTerminate() {
                     object:NSApp];
 }
 
-}
+}  // namespace Platform
 
 // From browser_main_win.h, stubs until we figure out the right thing...
 
@@ -69,14 +48,14 @@ int DoUninstallTasks(bool chrome_still_running) {
 }
 
 bool DoUpgradeTasks(const CommandLine& command_line) {
-  return ResultCodes::NORMAL_EXIT;
+  return false;
 }
 
 bool CheckForWin2000() {
   return false;
 }
 
-int HandleIconsCommands(const CommandLine &parsed_command_line) {
+int HandleIconsCommands(const CommandLine& parsed_command_line) {
   return 0;
 }
 
@@ -84,7 +63,7 @@ bool CheckMachineLevelInstall() {
   return false;
 }
 
-void PrepareRestartOnCrashEnviroment(const CommandLine &parsed_command_line) {
+void PrepareRestartOnCrashEnviroment(const CommandLine& parsed_command_line) {
 }
 
 void RecordBreakpadStatusUMA(MetricsService* metrics) {
