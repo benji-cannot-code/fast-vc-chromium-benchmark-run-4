e488c9a78f6c5e83c3102a49d5b949d8a9de2b5f
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-12 09:13:01 +0000

    Prospective build fix for Mac and 32-bit Windows.
    Patch by Simon Hausmann <simon.hausmann@nokia.com> on 2009-08-12
    * runtime/UString.cpp: Include wtf/StringExtras.h for snprintf.
    (JSC::UString::from): Use %lld instead of %I64d for snprintf
    on non-windows platforms.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47097 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/UString.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/UString.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/ASCIICType.h>
 #include <wtf/Assertions.h>
 #include <wtf/MathExtras.h>
+#include <wtf/StringExtras.h>
 #include <wtf/Vector.h>
 #include <wtf/unicode/UTF8.h>
 
@@ -951,7 +952,11 @@ UString UString::from(long long i)
         *--p = '0';
     else if (i == LLONG_MIN) {
         char minBuf[1 + sizeof(i) * 3];
+#if PLATFORM(WIN_OS)
         snprintf(minBuf, sizeof(minBuf) - 1, "%I64d", LLONG_MIN);
+#else
+        snprintf(minBuf, sizeof(minBuf) - 1, "%lld", LLONG_MIN);
+#endif
         return UString(minBuf);
     } else {
         bool negative = false;
