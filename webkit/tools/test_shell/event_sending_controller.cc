9fe3232569877235d0be17eff6f3c3a11093edc1
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-12 21:25:34 +0000

    Add a few more |#undef LOG| directives to code that might see both WebKit's and Chromium's definitions of LOG.  This is necessary after WebKit r47092 exposed its LOG in a few more places.
    BUG=0
    TEST=WebKit Canaries come back to life
    Review URL: http://codereview.chromium.org/164421
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23221 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/event_sending_controller.cc
+++ webkit/tools/test_shell/event_sending_controller.cc
@@ -15,14 +15,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The behavior of queuing events and replaying them can be disabled by a
 // layout test by setting eventSender.dragMode to false.
 
-#include "webkit/tools/test_shell/event_sending_controller.h"
-
-#include <queue>
-
 // TODO(darin): This is very wrong.  We should not be including WebCore headers
 // directly like this!!
 #include "config.h"
 #include "KeyboardCodes.h"
+
+#undef LOG
+
+#include "webkit/tools/test_shell/event_sending_controller.h"
+
+#include <queue>
 
 #include "base/compiler_specific.h"
 #include "base/logging.h"
--- webkit/tools/test_shell/test_webview_delegate.cc
+++ webkit/tools/test_shell/test_webview_delegate.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
+#undef LOG
+
 #include "webkit/tools/test_shell/test_webview_delegate.h"
 
 #include "base/file_util.h"
