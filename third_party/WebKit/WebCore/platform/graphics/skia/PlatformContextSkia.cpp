a7968c3c8601d7734248d210cfb61464d1069ab0
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-12 22:55:59 +0000

    2009-08-12  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Dan Bernstein.
    
            fix a typo in platform/graphics/skia/PlatformContextSkia.cpp
            https://bugs.webkit.org/show_bug.cgi?id=28172
    
            No new tests because it's a just typo fix.
    
            * platform/graphics/skia/PlatformContextSkia.cpp:
            (PlatformContextSkia::setupPaintCommon):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47157 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
+++ third_party/WebKit/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
@@ -296,7 +296,7 @@ void PlatformContextSkia::drawRect(SkRect rect)
 
 void PlatformContextSkia::setupPaintCommon(SkPaint* paint) const
 {
-#ifdef SK_DEBUGx
+#ifdef SK_DEBUG
     {
         SkPaint defaultPaint;
         SkASSERT(*paint == defaultPaint);
