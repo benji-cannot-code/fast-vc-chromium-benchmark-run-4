bf3cc849fd2984d8c3a6d0db7ef2c277652def5c
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-13 23:59:55 +0000

    Show favicon in panel title, highlight focused panel, fix breakage recent search box changes Review URL: http://codereview.chromium.org/164425
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23395 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_window_gtk.h
+++ chrome/browser/gtk/browser_window_gtk.h
@@ -170,6 +170,8 @@ class BrowserWindowGtk : public BrowserWindow,
 
   GtkWindow* window() const { return window_; }
 
+  gfx::Rect bounds() const { return bounds_; }
+
   static void RegisterUserPrefs(PrefService* prefs);
 
  protected:
