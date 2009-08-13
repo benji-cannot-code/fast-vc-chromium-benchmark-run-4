69f1df63b2933c3805fe40465ec51b5881660883
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-13 21:35:30 +0000

    WebCore: <rdar://problem/7138591> and https://bugs.webkit.org/show_bug.cgi?id=28260 onhashchange property cannot be set from javascript.
    Reviewed by Eric Seidel.
    
    Test: fast/loader/onhashchange-attribute-listeners.html
    
    * html/HTMLBodyElement.cpp:
    (WebCore::HTMLBodyElement::onhashchange):
    (WebCore::HTMLBodyElement::setOnhashchange):
    * html/HTMLBodyElement.h:
    * html/HTMLBodyElement.idl:
    
    * html/HTMLFrameSetElement.cpp:
    (WebCore::HTMLFrameSetElement::onhashchange):
    (WebCore::HTMLFrameSetElement::setOnhashchange):
    * html/HTMLFrameSetElement.h:
    * html/HTMLFrameSetElement.idl:
    
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::onhashchange):
    (WebCore::DOMWindow::setOnhashchange):
    * page/DOMWindow.h:
    * page/DOMWindow.idl:
    
    LayoutTests: <rdar://problem/7138591> and https://bugs.webkit.org/show_bug.cgi?id=28260
    onhashchange property cannot be set from javascript.
    
    Reviewed by Eric Seidel.
    
    * fast/dom/Window/window-properties-expected.txt:
    * fast/loader/onhashchange-attribute-listeners-expected.txt: Added.
    * fast/loader/onhashchange-attribute-listeners.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47233 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLBodyElement.h
+++ third_party/WebKit/WebCore/html/HTMLBodyElement.h
@@ -58,6 +58,8 @@ public:
     void setOnbeforeunload(PassRefPtr<EventListener>);
     EventListener* onmessage() const;
     void setOnmessage(PassRefPtr<EventListener>);
+    EventListener* onhashchange() const;
+    void setOnhashchange(PassRefPtr<EventListener>);
     EventListener* onoffline() const;
     void setOnoffline(PassRefPtr<EventListener>);
     EventListener* ononline() const;
--- third_party/WebKit/WebCore/html/HTMLFrameSetElement.h
+++ third_party/WebKit/WebCore/html/HTMLFrameSetElement.h
@@ -80,6 +80,8 @@ public:
 
     EventListener* onbeforeunload() const;
     void setOnbeforeunload(PassRefPtr<EventListener>);
+    EventListener* onhashchange() const;
+    void setOnhashchange(PassRefPtr<EventListener>);
     EventListener* onmessage() const;
     void setOnmessage(PassRefPtr<EventListener>);
     EventListener* onoffline() const;
