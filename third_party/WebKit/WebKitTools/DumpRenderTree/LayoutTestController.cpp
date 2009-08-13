cbcc4f0ed81e150aefc7943ff22f661950eb5c79
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-13 22:45:31 +0000

    2009-08-13  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
            Unreviewed build fix. Include stdio.h for using stdout, stderr,
            and fprintf.
    
            * DumpRenderTree/LayoutTestController.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47245 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WorkQueueItem.h"
 #include <JavaScriptCore/JSObjectRef.h>
 #include <JavaScriptCore/JSRetainPtr.h>
+#include <stdio.h>
 #include <wtf/Assertions.h>
 #include <wtf/MathExtras.h>
 
