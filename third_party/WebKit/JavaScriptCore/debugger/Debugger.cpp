e0d1d2330ca874c588a89399da26a278522999df
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-13 22:22:50 +0000

    Build fix so JavaScriptCore builds, might fail linking WebCore or WebKit. Added includes and removed a missing function from JavaScriptCore_debug.def and JavascriptCore.def.
    git-svn-id: svn://svn.chromium.org/blink/trunk@47239 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/debugger/Debugger.cpp
+++ third_party/WebKit/JavaScriptCore/debugger/Debugger.cpp
@@ -24,9 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Debugger.h"
 
 #include "CollectorHeapIterator.h"
+#include "Error.h"
 #include "Interpreter.h"
+#include "JSFunction.h"
 #include "JSGlobalObject.h"
 #include "Parser.h"
+#include "Protect.h"
 
 namespace JSC {
 
--- third_party/WebKit/JavaScriptCore/runtime/RegExpConstructor.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/RegExpConstructor.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RegExpConstructor.h"
 
 #include "ArrayPrototype.h"
+#include "Error.h"
 #include "JSArray.h"
 #include "JSFunction.h"
 #include "JSString.h"
