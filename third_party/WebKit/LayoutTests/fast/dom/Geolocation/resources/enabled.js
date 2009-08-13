ea5caa81601798e5775e9d11b1f5d458f01e7fc6
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-13 00:23:44 +0000

    2009-08-12  Steve Block  <steveblock@google.com>
            Reviewed by Eric Seidel.
    
            Bug 27716: Add Geolocation layout tests.
            https://bugs.webkit.org/show_bug.cgi?id=27716
    
            Adds the first, basic Geolocation layout test. This simply tests that
            navigator.geolocation is present when it should be.
    
            * fast/dom/Geolocation: Added. Directory for Geolocation layout tests.
            * fast/dom/Geolocation/enabled.html: Added. HTML wrapper for enabled.js test.
            * fast/dom/Geolocation/resources: Added. Directory for Geolocation layout tests JS source.
            * fast/dom/Geolocation/resources/enabled.js: Added. Tests that navigator.geolocation is present.
            * fast/dom/Geolocation/resources/TEMPLATE.html: Added. Template for creating HTML wrapper files for JS tests with make-js-test-wrappers.
            * fast/dom/Geolocation/enabled-expected.txt: Added. Expected result for above test.
            * platform/mac/Skipped: Modified. Skip Geolocation tests.
            * platform/qt/Skipped: Modified. Skip Geolocation tests.
            * platform/win/Skipped: Modified. Skip Geolocation tests.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/Geolocation/resources/enabled.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Tests that the navigator.geolocation object is present.");
+
+function hasGeolocationProperty()
+{
+    for (var property in navigator) {
+        if (property == "geolocation") {
+            return true;
+        }
+    }
+    return false;
+}
+
+shouldBeTrue("typeof navigator.geolocation == 'object'");
+shouldBeTrue("hasGeolocationProperty()");
+shouldBeTrue("'geolocation' in navigator");
+shouldBeTrue("navigator.hasOwnProperty('geolocation')");
+
+var successfullyParsed = true;
