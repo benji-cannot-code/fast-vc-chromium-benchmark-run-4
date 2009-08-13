17df370a4b7a2166bc11853daadcd695fa6e6782
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-13 23:36:38 +0000

    Add a missing Stop() call to DoDataResolveHostComplete(). Fix some minor nits.
    The patch is contributed by Ibrar Ahmed <ibrar.ahmad@gmail.com>.
    Original review URL: http://codereview.chromium.org/164330
    
    R=wtc
    BUG=http://crbug.com/4965
    TEST=none
    Review URL: http://codereview.chromium.org/165496
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23389 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_network_transaction.cc
+++ net/ftp/ftp_network_transaction.cc
@@ -129,7 +129,7 @@ uint64 FtpNetworkTransaction::GetUploadProgress() const {
   return 0;
 }
 
-// Used to prepare and send FTP commad.
+// Used to prepare and send FTP command.
 int FtpNetworkTransaction::SendFtpCommand(const std::string& command,
                                           Command cmd) {
   // If we send a new command when we still have unprocessed responses
@@ -912,7 +912,7 @@ int FtpNetworkTransaction::ProcessResponseLIST(
   return OK;
 }
 
-// Quit command
+// QUIT command
 int FtpNetworkTransaction::DoCtrlWriteQUIT() {
   std::string command = "QUIT";
   next_state_ = STATE_CTRL_READ;
@@ -940,9 +940,11 @@ int FtpNetworkTransaction::DoDataResolveHost() {
 }
 
 int FtpNetworkTransaction::DoDataResolveHostComplete(int result) {
-  if (result == OK)
+  if (result == OK) {
     next_state_ = STATE_DATA_CONNECT;
-  return result;
+    return result;
+  }
+  return Stop(result);
 }
 
 int FtpNetworkTransaction::DoDataConnect() {
