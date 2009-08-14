c6f3c5ba1b9f30cf450817967e3edc5a67cd174c
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-14 05:41:20 +0000

    Attempt to fix windows build
    git-svn-id: svn://svn.chromium.org/blink/trunk@47268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/Collector.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/Collector.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CallFrame.h"
 #include "CollectorHeapIterator.h"
 #include "Interpreter.h"
+#include "JSArray.h"
 #include "JSGlobalObject.h"
 #include "JSLock.h"
 #include "JSONObject.h"
