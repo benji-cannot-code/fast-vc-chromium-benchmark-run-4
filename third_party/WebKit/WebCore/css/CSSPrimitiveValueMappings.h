d5b400da32012f6c821ac786ed3a91301be28971
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-14 00:52:56 +0000

    WebCore: 'box-orient: block-axis' behaves like 'box-orient: horizontal', causes an assertion failure in debug builds https://bugs.webkit.org/show_bug.cgi?id=28279
    Reviewed by Eric Seidel.
    
    Test: fast/flexbox/block-axis.html
    
    * css/CSSPrimitiveValueMappings.h:
    (WebCore::CSSPrimitiveValue::operator EBoxOrient): Map block-axis to
    vertical.
    
    LayoutTests:     2009-08-13  Dan Bernstein  <mitz@apple.com>
    
    Reviewed by Eric Seidel.
    
    'box-orient: block-axis' behaves like 'box-orient: horizontal', causes
    an assertion failure in debug builds
    https://bugs.webkit.org/show_bug.cgi?id=28279
    
    * fast/flexbox/block-axis-expected.checksum: Added.
    * fast/flexbox/block-axis-expected.png: Added.
    * fast/flexbox/block-axis-expected.txt: Added.
    * fast/flexbox/block-axis.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47261 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSPrimitiveValueMappings.h
+++ third_party/WebKit/WebCore/css/CSSPrimitiveValueMappings.h
@@ -524,6 +524,7 @@ template<> inline CSSPrimitiveValue::operator EBoxOrient() const
         case CSSValueInlineAxis:
             return HORIZONTAL;
         case CSSValueVertical:
+        case CSSValueBlockAxis:
             return VERTICAL;
         default:
             ASSERT_NOT_REACHED();
