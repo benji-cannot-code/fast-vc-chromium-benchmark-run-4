ccdf648b56f16fb1685dd9513ee9d005c5affb3a
sidchat@google.com <sidchat@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-14 23:05:13 +0000

    Fix for spell check toggle checkbox not getting selected after reset to default button is clicked. Patch by tfarina (original patch: http://codereview.chromium.org/165524)
    BUG=www.crbug.com/4602
    TEST=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23490 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/options_util.cc
+++ chrome/browser/options_util.cc
@@ -26,6 +26,7 @@ void OptionsUtil::ResetToDefaults(Profile* profile) {
     prefs::kDnsPrefetchingEnabled,
     prefs::kDownloadDefaultDirectory,
     prefs::kDownloadExtensionsToOpen,
+    prefs::kEnableSpellCheck,
     prefs::kFormAutofillEnabled,
     prefs::kHomePage,
     prefs::kHomePageIsNewTabPage,
