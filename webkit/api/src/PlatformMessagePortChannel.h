ec1146417d1f964d36e42a923987451b4036a838
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-14 06:39:41 +0000

    namespace Foo { class Bar; } is used many places, so I just left it.
    There were 2 places that did
    namespace Foo {
    class Bar;
    }
    
    So I just spaced these in.  I feel like from now on we should just go ahead and
    use the latter format.
    
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/165521
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23424 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/src/PlatformMessagePortChannel.h
+++ webkit/api/src/PlatformMessagePortChannel.h
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Threading.h>
 
 namespace WebKit {
-class WebMessagePortChannel;
+    class WebMessagePortChannel;
 }
 
 namespace WebCore {
