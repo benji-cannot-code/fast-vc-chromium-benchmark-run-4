56fd24d0ad818e368eb2e2e3035a49e58e1f2a6d
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-14 03:00:30 +0000

    Reverting 23406.
    It seems that Tab2OutOfTabStrip is failing consistently after this change.
    
    BUG=None
    TEST=None
    TBR=jhawkins@chromium.org
    
    Review URL: http://codereview.chromium.org/164543
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23411 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/default_plugin/plugin_impl_win.h
+++ webkit/default_plugin/plugin_impl_win.h
@@ -6,10 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_DEFAULT_PLUGIN_PLUGIN_IMPL_WIN_H_
 #define WEBKIT_DEFAULT_PLUGIN_PLUGIN_IMPL_WIN_H_
 
+#include <atlbase.h>
+#include <atlwin.h>
 #include <string>
 #include <vector>
 
-#include "base/window_impl.h"
 #include "third_party/npapi/bindings/npapi.h"
 #include "webkit/default_plugin/install_dialog.h"
 #include "webkit/default_plugin/plugin_database_handler.h"
@@ -36,7 +37,7 @@ class PluginDatabaseHandler;
 // Provides the plugin installation functionality. This class is
 // instantiated with the information like the mime type of the
 // target plugin, the display mode, etc.
-class PluginInstallerImpl : public base::WindowImpl {
+class PluginInstallerImpl : public CWindowImpl<PluginInstallerImpl> {
  public:
   static const int kRefreshPluginsMessage  = WM_APP + 1;
   static const int kInstallMissingPluginMessage = WM_APP + 2;
@@ -47,7 +48,7 @@ class PluginInstallerImpl : public base::WindowImpl {
   explicit PluginInstallerImpl(int16 mode);
   virtual ~PluginInstallerImpl();
 
-  BEGIN_MSG_MAP_EX(PluginInstallerImpl)
+  BEGIN_MSG_MAP(PluginInstallerImpl)
     MESSAGE_HANDLER(WM_ERASEBKGND, OnEraseBackGround)
     MESSAGE_HANDLER(WM_PAINT, OnPaint)
     MESSAGE_HANDLER(WM_LBUTTONDOWN, OnLButtonDown)
@@ -151,6 +152,7 @@ class PluginInstallerImpl : public base::WindowImpl {
   // activex.
   int16 NPP_HandleEvent(void* event);
 
+  HWND window() const { return m_hWnd; }
   const std::string& mime_type() const { return mime_type_; }
 
   // Replaces a resource string with the placeholder passed in as an argument
@@ -374,7 +376,7 @@ class PluginInstallerImpl : public base::WindowImpl {
   std::string activex_clsid_;
   CComObject<ActiveXInstaller>* activex_installer_;
 
-  DISALLOW_COPY_AND_ASSIGN(PluginInstallerImpl);
+  DISALLOW_EVIL_CONSTRUCTORS(PluginInstallerImpl);
 };
 
 
--- webkit/webkit.gyp
+++ webkit/webkit.gyp
@@ -1506,9 +1506,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ],
         }, {  # else: OS=="win"
           'sources/': [['exclude', '_posix\\.cc$']],
-          'include_dirs': [
-            '../chrome/third_party/wtl/include',
-          ],
           'dependencies': [
             '../build/win/system.gyp:cygwin',
             'activex_shim/activex_shim.gyp:activex_shim',
