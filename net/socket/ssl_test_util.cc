cd80549834571f0648e2bbb76ececf2f7fd78d80
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-14 22:37:35 +0000

    Speed up net_unittests a bit by re-using launched test server.
    On POSIX it makes the server fork a separate process for each request for better
    test isolation.
    
    Starting with just few tests to limit impact of an eventual breakage. The results are promising.
    
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/164522
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23481 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_test_util.cc
+++ net/socket/ssl_test_util.cc
@@ -96,6 +96,7 @@ const int TestServerLauncher::kBadHTTPSPort = 9666;
 const wchar_t TestServerLauncher::kCertIssuerName[] = L"Test CA";
 
 TestServerLauncher::TestServerLauncher() : process_handle_(NULL),
+                                           forking_(false),
                                            connection_attempts_(10),
                                            connection_timeout_(1000)
 #if defined(OS_LINUX)
@@ -108,6 +109,7 @@ TestServerLauncher::TestServerLauncher() : process_handle_(NULL),
 TestServerLauncher::TestServerLauncher(int connection_attempts,
                                        int connection_timeout)
                         : process_handle_(NULL),
+                          forking_(false),
                           connection_attempts_(connection_attempts),
                           connection_timeout_(connection_timeout)
 #if defined(OS_LINUX)
@@ -223,6 +225,8 @@ bool TestServerLauncher::Start(Protocol protocol,
     command_line.append(file_root_url);
     command_line.append(L"\"");
   }
+  // Deliberately do not pass the --forking flag. It breaks the tests
+  // on Windows.
 
   if (!base::LaunchApp(command_line, false, true, &process_handle_)) {
     LOG(ERROR) << "Failed to launch " << command_line;
@@ -239,6 +243,8 @@ bool TestServerLauncher::Start(Protocol protocol,
     command_line.push_back("-f");
   if (!cert_path.value().empty())
     command_line.push_back("--https=" + WideToUTF8(cert_path.ToWStringHack()));
+  if (forking_)
+    command_line.push_back("--forking");
 
   base::file_handle_mapping_vector no_mappings;
   LOG(INFO) << "Trying to launch " << command_line[0] << " ...";
