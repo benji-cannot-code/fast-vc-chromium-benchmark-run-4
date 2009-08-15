4055858b3c7472321dc871e570df0115162cc0e0
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-15 00:04:01 +0000

    Ensure we don't load plugins on the IO thread.
    I had to move the locks from PluginService to PluginList, so that a lock (which can block other threads) isn't held while loading the plugins.
    
    BUG=17938
    TEST=added asserts which crash if plugins loaded on IO thread, current UI tests exercise them
    Review URL: http://codereview.chromium.org/164305
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23501 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_service.cc
+++ chrome/browser/metrics/metrics_service.cc
@@ -182,7 +182,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/load_notification_details.h"
 #include "chrome/browser/memory_details.h"
-#include "chrome/browser/plugin_service.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
 #include "chrome/browser/search_engines/template_url.h"
@@ -198,6 +197,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/render_messages.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/load_flags.h"
+#include "webkit/glue/plugins/plugin_list.h"
 
 #if defined(OS_POSIX)
 // TODO(port): Move these headers above as they are ported.
@@ -289,9 +289,15 @@ class MetricsMemoryDetails : public MemoryDetails {
 };
 
 class MetricsService::GetPluginListTaskComplete : public Task {
+ public:
+  explicit GetPluginListTaskComplete(
+      const std::vector<WebPluginInfo>& plugins) : plugins_(plugins) { }
   virtual void Run() {
-    g_browser_process->metrics_service()->OnGetPluginListTaskComplete();
+    g_browser_process->metrics_service()->OnGetPluginListTaskComplete(plugins_);
   }
+
+ private:
+  std::vector<WebPluginInfo> plugins_;
 };
 
 class MetricsService::GetPluginListTask : public Task {
@@ -301,9 +307,10 @@ class MetricsService::GetPluginListTask : public Task {
 
   virtual void Run() {
     std::vector<WebPluginInfo> plugins;
-    PluginService::GetInstance()->GetPlugins(false, &plugins);
+    NPAPI::PluginList::Singleton()->GetPlugins(false, &plugins);
 
-    callback_loop_->PostTask(FROM_HERE, new GetPluginListTaskComplete());
+    callback_loop_->PostTask(
+        FROM_HERE, new GetPluginListTaskComplete(plugins));
   }
 
  private:
@@ -752,8 +759,10 @@ void MetricsService::InitializeMetricsState() {
   ScheduleNextStateSave();
 }
 
-void MetricsService::OnGetPluginListTaskComplete() {
+void MetricsService::OnGetPluginListTaskComplete(
+    const std::vector<WebPluginInfo>& plugins) {
   DCHECK(state_ == PLUGIN_LIST_REQUESTED);
+  plugins_ = plugins;
   if (state_ == PLUGIN_LIST_REQUESTED)
     state_ = PLUGIN_LIST_ARRIVED;
 }
@@ -1123,11 +1132,9 @@ bool MetricsService::TransmissionPermitted() const {
 
 void MetricsService::PrepareInitialLog() {
   DCHECK(state_ == PLUGIN_LIST_ARRIVED);
-  std::vector<WebPluginInfo> plugins;
-  PluginService::GetInstance()->GetPlugins(false, &plugins);
 
   MetricsLog* log = new MetricsLog(client_id_, session_id_);
-  log->RecordEnvironment(plugins, profile_dictionary_.get());
+  log->RecordEnvironment(plugins_, profile_dictionary_.get());
 
   // Histograms only get written to current_log_, so setup for the write.
   MetricsLog* save_log = current_log_;
--- chrome/browser/plugin_service.h
+++ chrome/browser/plugin_service.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/hash_tables.h"
-#include "base/lock.h"
 #include "base/ref_counted.h"
 #include "base/singleton.h"
 #include "base/waitable_event_watcher.h"
@@ -37,17 +36,14 @@ class URLRequestContext;
 class ResourceDispatcherHost;
 class ResourceMessageFilter;
 
-// This can be called on the main thread and IO thread.  However it must
-// be created on the main thread.
+// This must be created on the main thread but it's only called on the IO/file
+// thread.
 class PluginService
     : public base::WaitableEventWatcher::Delegate,
       public NotificationObserver {
  public:
   // Returns the PluginService singleton.
   static PluginService* GetInstance();
-
-  // Gets the list of available plugins.
-  void GetPlugins(bool refresh, std::vector<WebPluginInfo>* plugins);
 
   // Load all the plugins that should be loaded for the lifetime of the browser
   // (ie, with the LoadOnStartup flag set).
@@ -83,8 +79,6 @@ class PluginService
                            const std::wstring& locale,
                            IPC::Message* reply_msg);
 
-  bool HavePluginFor(const std::string& mime_type, bool allow_wildcard);
-
   // Get the path to the plugin specified.  policy_url is the URL of the page
   // requesting the plugin, so we can verify whether the plugin is allowed
   // on that page.
@@ -116,9 +110,6 @@ class PluginService
   virtual void Observe(NotificationType type, const NotificationSource& source,
                        const NotificationDetails& details);
 
-  // Get plugin info by matching full path.
-  bool GetPluginInfoByPath(const FilePath& plugin_path, WebPluginInfo* info);
-
   // Returns true if the given plugin is allowed to be used by a page with
   // the given URL.
   bool PluginAllowedForURL(const FilePath& plugin_path, const GURL& url);
@@ -143,10 +134,6 @@ class PluginService
   // extension-only plugins.
   typedef base::hash_map<FilePath, GURL> PrivatePluginMap;
   PrivatePluginMap private_plugins_;
-
-  // Need synchronization whenever we access the plugin_list singelton through
-  // webkit_glue since this class is called on the main and IO thread.
-  Lock lock_;
 
   NotificationRegistrar registrar_;
 
