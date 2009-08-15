e7777d85aafa7f8f7d5c6a3d208b600be878d945
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-15 00:46:37 +0000

    Touch PSS as an experiment, to see what the builder recompiles.
    No-op code change.
    
    Review URL: http://codereview.chromium.org/172016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23504 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_unittest.cc
+++ chrome/browser/sync/profile_sync_service_unittest.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #ifdef CHROME_PERSONALIZATION
-#ifndef COMPILING_SYNCAPI_STUB  // This test requires the real deal syncapi.
+#ifndef COMPILING_SYNCAPI_STUB  // This test requires the real-deal syncapi.
 
 #include <stack>
 #include <vector>
