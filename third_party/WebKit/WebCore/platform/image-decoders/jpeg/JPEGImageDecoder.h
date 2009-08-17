8d4cef8d019daa56d5b564a5bf978c7b2a32ef49
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-17 19:27:39 +0000

    2009-08-17  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
            Unreviewed. Reverts 47371, since it breaks GTK+.
    
            * platform/graphics/ImageSource.cpp:
            (WebCore::ImageSource::setData):
            * platform/image-decoders/ImageDecoder.cpp: Removed.
            * platform/image-decoders/ImageDecoder.h:
            (WebCore::ImageDecoder::ImageDecoder):
            * platform/image-decoders/jpeg/JPEGImageDecoder.cpp:
            (WebCore::convertCMYKToRGBA):
            (WebCore::convertRGBToRGBA):
            (WebCore::JPEGImageDecoder::outputScanlines):
            * platform/image-decoders/jpeg/JPEGImageDecoder.h:
            * platform/image-decoders/png/PNGImageDecoder.cpp:
            (WebCore::PNGImageDecoder::headerAvailable):
            (WebCore::PNGImageDecoder::rowAvailable):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47378 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.h
+++ third_party/WebKit/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.h
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
- * Copyright (C) 2008-2009 Torch Mobile, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -58,16 +57,6 @@ namespace WebCore {
 
         bool outputScanlines();
         void jpegComplete();
-
-#if ENABLE(IMAGE_DECODER_DOWN_SAMPLING)
-        bool setSize(int width, int height)
-        {
-            if (!ImageDecoder::setSize(width, height))
-                return false;
-            prepareScaleDataIfNecessary();
-            return true;
-        }
-#endif
 
     private:
         JPEGImageReader* m_reader;
