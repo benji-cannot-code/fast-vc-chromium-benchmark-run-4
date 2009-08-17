4a4d88f03a1976ef2025cc3aa28b73a40914fbed
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-17 05:24:35 +0000

    <http://webkit.org/b/28366> WebHTMLViewInternal.h: add @class CALayer declaration
            Reviewed by Simon Fraser.
    
            Without WebKitPrefix.h, the missing @class CALayer declaration
            causes a compile-time error.
    
            * WebView/WebHTMLViewInternal.h: Added @class CALayer
            declaration with USE(ACCELERATED_COMPOSITING).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47349 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLViewInternal.h
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLViewInternal.h
@@ -31,6 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "WebHTMLViewPrivate.h"
 
+#if USE(ACCELERATED_COMPOSITING)
+@class CALayer;
+#endif
 @class WebFrame;
 
 namespace WebCore {
