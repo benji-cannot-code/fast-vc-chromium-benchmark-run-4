9a802740eb620f6f39b946084bb6b5813cde8ccf
yong.li@torchmobile.com <yong.li@torchmobile.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-17 20:11:28 +0000

    2009-08-17  Yong Li  <yong.li@torchmobile.com>
            Reviewed by Adam Treat.
    
            Allow image decoders to down-sample the image directly
            to scaled output buffer. This can be enabled/disabled by
            macro ENABLE(IMAGE_DECODER_DOWN_SAMPLING).
            Only JPEG and PNG decoders are modified to support it now.
            https://bugs.webkit.org/show_bug.cgi?id=28308
    
            * platform/graphics/ImageSource.cpp:
            (WebCore::ImageSource::setData):
            * platform/image-decoders/ImageDecoder.cpp: Added.
            * platform/image-decoders/ImageDecoder.h:
            (WebCore::ImageDecoder::ImageDecoder):
            (WebCore::ImageDecoder::setMaxNumPixels):
            * platform/image-decoders/jpeg/JPEGImageDecoder.cpp:
            (WebCore::convertCMYKToRGBA):
            (WebCore::convertRGBToRGBA):
            (WebCore::JPEGImageDecoder::outputScanlines):
            * platform/image-decoders/jpeg/JPEGImageDecoder.h:
            (WebCore::JPEGImageDecoder::setSize):
            * platform/image-decoders/png/PNGImageDecoder.cpp:
            (WebCore::PNGImageDecoder::headerAvailable):
            (WebCore::PNGImageDecoder::rowAvailable):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47381 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/image-decoders/ImageDecoder.h
+++ third_party/WebKit/WebCore/platform/image-decoders/ImageDecoder.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2008-2009 Torch Mobile, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -183,9 +184,17 @@ namespace WebCore {
     // and the base class manages the RGBA32 frame cache.
     class ImageDecoder {
     public:
+        // ENABLE(IMAGE_DECODER_DOWN_SAMPLING) allows image decoders to write directly to
+        // scaled output buffers by down sampling. Call setMaxNumPixels() to specify the
+        // biggest size that decoded images can have. Image decoders will deflate those
+        // images that are bigger than m_maxNumPixels. (Not supported by all image decoders yet)
         ImageDecoder()
             : m_failed(false)
             , m_sizeAvailable(false)
+#if ENABLE(IMAGE_DECODER_DOWN_SAMPLING)
+            , m_maxNumPixels(-1)
+            , m_scaled(false)
+#endif
         {
         }
 
@@ -267,8 +276,25 @@ namespace WebCore {
         // since in practice only GIFs will ever use this.
         virtual void clearFrameBufferCache(size_t clearBeforeFrame) { }
 
+#if ENABLE(IMAGE_DECODER_DOWN_SAMPLING)
+        void setMaxNumPixels(int m) { m_maxNumPixels = m; }
+#endif
+
     protected:
+#if ENABLE(IMAGE_DECODER_DOWN_SAMPLING)
+        void prepareScaleDataIfNecessary();
+        int upperBoundScaledX(int origX, int searchStart = 0);
+        int lowerBoundScaledX(int origX, int searchStart = 0);
+        int scaledY(int origY, int searchStart = 0);
+#endif
+
         RefPtr<SharedBuffer> m_data; // The encoded data.
+#if ENABLE(IMAGE_DECODER_DOWN_SAMPLING)
+        int m_maxNumPixels;
+        Vector<int> m_scaledColumns;
+        Vector<int> m_scaledRows;
+        bool m_scaled;
+#endif
         Vector<RGBA32Buffer> m_frameBufferCache;
         bool m_failed;
 
--- third_party/WebKit/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.h
+++ third_party/WebKit/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2008-2009 Torch Mobile, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -57,6 +58,16 @@ namespace WebCore {
 
         bool outputScanlines();
         void jpegComplete();
+
+#if ENABLE(IMAGE_DECODER_DOWN_SAMPLING)
+        bool setSize(int width, int height)
+        {
+            if (!ImageDecoder::setSize(width, height))
+                return false;
+            prepareScaleDataIfNecessary();
+            return true;
+        }
+#endif
 
     private:
         JPEGImageReader* m_reader;
