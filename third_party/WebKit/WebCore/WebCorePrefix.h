e98f59f727e45cdf7a13ec932536d5c7bef3b63a
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-17 23:11:45 +0000

    Build fix.
    CFNetwork isn't present in the framework search path on Mac OS X so we can't directly include its headers.
    We include CoreServices.h, the umbrella framework that contains CFNetwork, from the prefix header which
    results in the CFNetwork headers being included on Mac OS X.  CoreServices.h doesn't include CFNetwork.h on
    Windows though so we explicitly include this header from the prefix header when on Windows.
    
    * WebCorePrefix.h:
    * platform/network/cf/DNSCFNet.cpp: Remove #include that is no longer needed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47396 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCorePrefix.h
+++ third_party/WebKit/WebCore/WebCorePrefix.h
@@ -114,6 +114,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdio.h>
 #else
 #include <CoreServices/CoreServices.h>
+
+#if defined(WIN32) || defined(_WIN32)
+/* Including CoreServices.h on Windows doesn't include CFNetwork.h, so we do
+   it explicitly here to make Windows more consistent with Mac. */
+#include <CFNetwork/CFNetwork.h>
+#endif
+
 #endif
 #endif
 
--- third_party/WebKit/WebCore/platform/network/cf/DNSCFNet.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/DNSCFNet.cpp
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DNS.h"
 
 #include "PlatformString.h"
-#include <CFNetwork/CFNetwork.h>
 #include <wtf/RetainPtr.h>
 
 #ifdef BUILDING_ON_TIGER
