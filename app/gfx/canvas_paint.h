6c911ac842164e73f610572be1317c45c926cc8a
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-17 18:05:42 +0000

    A canvas_paint for OS X.
    Makes it possible to use skia to paint something into a bitmap and then blit that bitmap to screen. I need this so that I can call download_util::PaintDownloadProgress(), which needs an SkCanvas.
    
    To be used like this:
    
      {
        gfx::CanvasPaint canvas(myRect);
        canvas.set_composite_alpha(true);
        download_util::PaintDownloadprogres(&canvas, ...);
      }
    
    BUG=18902
    TEST=none
    
    Review URL: http://codereview.chromium.org/172046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23551 0039d316-1c4b-4281-b951-d872f2087c98

--- app/gfx/canvas_paint.h
+++ app/gfx/canvas_paint.h
@@ -14,9 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
-#if defined(OS_WIN) || defined(OS_LINUX)
 typedef skia::CanvasPaintT<Canvas> CanvasPaint;
-#endif
 
 }  // namespace gfx
 
--- skia/ext/canvas_paint.h
+++ skia/ext/canvas_paint.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(WIN32)
 #include "skia/ext/canvas_paint_win.h"
+#elif defined(__APPLE__)
+#include "skia/ext/canvas_paint_mac.h"
 #elif defined(__linux__)
 #include "skia/ext/canvas_paint_linux.h"
 #endif
