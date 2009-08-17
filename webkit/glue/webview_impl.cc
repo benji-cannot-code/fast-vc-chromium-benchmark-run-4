3a0bd126ea6f4bffe2eda739cefa12de2531d33a
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-17 03:20:10 +0000

    A quick fix for Issue 18844 (Take 2).
    This is the same change as <http://codereview.chromium.org/164309> that I reverted to investigate a test_shell_test failure (Issue 19263).
    
    This change checks if the modifier flag is a WebInputEvent::MetaKey flag to
    switch the scroll granularity when a user types a up/down key.
    Editor::Commands ("MoveToBeginningOfDocument" and "MoveToEndOfDocument") works
    only in an editable control, such as <textarea>. On the other hand, this
    WebViewImpl::ScrollViewWithKeyboard() is for scrolling a page.
    
    BUG=18844 "Apple (command) key + down arrow is not working"
    TEST=Type command+down keys in a web page which contains a scroll bar, and
    verify Chrome scrolls the page to its end.
    Review URL: http://codereview.chromium.org/165523
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23540 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webview_impl.cc
+++ webkit/glue/webview_impl.cc
@@ -812,7 +812,7 @@ bool WebViewImpl::KeyEventDefault(const WebKeyboardEvent& event) {
       if (event.windowsKeyCode == VKEY_SPACE) {
         int key_code = ((event.modifiers & WebInputEvent::ShiftKey) ?
                          VKEY_PRIOR : VKEY_NEXT);
-        return ScrollViewWithKeyboard(key_code);
+        return ScrollViewWithKeyboard(key_code, event.modifiers);
       }
       break;
     }
@@ -839,7 +839,7 @@ bool WebViewImpl::KeyEventDefault(const WebKeyboardEvent& event) {
         }
       }
       if (!event.isSystemKey) {
-        return ScrollViewWithKeyboard(event.windowsKeyCode);
+        return ScrollViewWithKeyboard(event.windowsKeyCode, event.modifiers);
       }
       break;
     }
@@ -850,7 +850,7 @@ bool WebViewImpl::KeyEventDefault(const WebKeyboardEvent& event) {
   return false;
 }
 
-bool WebViewImpl::ScrollViewWithKeyboard(int key_code) {
+bool WebViewImpl::ScrollViewWithKeyboard(int key_code, int modifiers) {
   Frame* frame = GetFocusedWebCoreFrame();
   if (!frame)
     return false;
@@ -869,11 +869,29 @@ bool WebViewImpl::ScrollViewWithKeyboard(int key_code) {
       break;
     case VKEY_UP:
       scroll_direction = ScrollUp;
+#if defined(OS_MACOSX)
+      // Many Mac applications (such as TextEdit, Safari, Firefox, etc.) scroll
+      // to the beginning of a page when typing command+up keys. It is better
+      // for Mac Chrome to emulate this behavior to improve compatibility with
+      // these applications.
+      scroll_granularity = (modifiers == WebInputEvent::MetaKey) ?
+          ScrollByDocument : ScrollByLine;
+#else
       scroll_granularity = ScrollByLine;
+#endif
       break;
     case VKEY_DOWN:
       scroll_direction = ScrollDown;
+#if defined(OS_MACOSX)
+      // Many Mac applications (such as TextEdit, Safari, Firefox, etc.) scroll
+      // to the end of a page when typing command+down keys. It is better
+      // for Mac Chrome to emulate this behavior to improve compatibility with
+      // these applications.
+      scroll_granularity = (modifiers == WebInputEvent::MetaKey) ?
+          ScrollByDocument : ScrollByLine;
+#else
       scroll_granularity = ScrollByLine;
+#endif
       break;
     case VKEY_HOME:
       scroll_direction = ScrollUp;
