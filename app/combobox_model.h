f6774b3cbfb5c83598abd730cdd3895fb6d8cf3b
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-18 01:16:10 +0000

    Add missing include.
    TBR=ben
    Review URL: http://codereview.chromium.org/171092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23604 0039d316-1c4b-4281-b951-d872f2087c98

--- app/combobox_model.h
+++ app/combobox_model.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef APP_COMBOBOX_MODEL_H_
 #define APP_COMBOBOX_MODEL_H_
 
+#include <string>
+
 // The interface for models backing a combobox.
 class ComboboxModel {
  public:
