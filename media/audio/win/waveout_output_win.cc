450bc1075887c693847aee907445fb986c42789b
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-18 22:34:24 +0000

    Possible deadlock in PCM audio Start() method - Implement Alpha's idea of pausing the device while we queue the first two packets. - This way we don't risk the chance to enter the callback from two threads at the same time.
    TEST=unit test added
    BUG=19276
    
    
    Review URL: http://codereview.chromium.org/173022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23668 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/waveout_output_win.cc
+++ media/audio/win/waveout_output_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -154,15 +154,25 @@ void PCMWaveOutAudioOutputStream::Start(AudioSourceCallback* callback) {
     buffer = GetNextBuffer(buffer);
   }
   buffer = buffer_;
-
-  // Send the buffers to the audio driver.
+  MMRESULT result = ::waveOutPause(waveout_);
+  if (result != MMSYSERR_NOERROR) {
+    HandleError(result);
+    return;
+  }
+  // Send the buffers to the audio driver. Note that the device is paused
+  // so we avoid entering the callback method while still here.
   for (int ix = 0; ix != kNumBuffers; ++ix) {
-    MMRESULT result = ::waveOutWrite(waveout_, buffer, sizeof(WAVEHDR));
+    result = ::waveOutWrite(waveout_, buffer, sizeof(WAVEHDR));
     if (result != MMSYSERR_NOERROR) {
       HandleError(result);
       break;
     }
     buffer = GetNextBuffer(buffer);
+  }
+  result = ::waveOutRestart(waveout_);
+  if (result != MMSYSERR_NOERROR) {
+    HandleError(result);
+    return;
   }
 }
 
