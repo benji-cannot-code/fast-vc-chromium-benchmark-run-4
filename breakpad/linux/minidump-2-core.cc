c3968d31ead50511843bd51b12b323863e848013
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-18 22:04:32 +0000

    Include linux_syscall_support.h to get definition of NT_PRXFPREG.
    BUG=none
    TEST=app_unittests && base_unittests --gtest_filter=-ConditionVariableTest.LargeFastTaskTest
    
    http://codereview.chromium.org/171033
    
    (Patch from Jacob Mandelson)
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23659 0039d316-1c4b-4281-b951-d872f2087c98

--- breakpad/linux/minidump-2-core.cc
+++ breakpad/linux/minidump-2-core.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "google_breakpad/common/minidump_format.h"
 #include "google_breakpad/common/minidump_cpu_x86.h"
+#include "breakpad/linux/linux_syscall_support.h"
 #include "breakpad/linux/minidump_format_linux.h"
 
 #if __WORDSIZE == 64
