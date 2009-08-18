6958b5642b9b3457e43d1e722407ac252033d976
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-18 13:46:18 +0000

    DevTools Heap profiler: add a bar with Code / Objects and Data / Others size distribution.
    This helps to reveal memory size occupied by compiled code and engine's data. And this is also an eye candy.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/172090
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23619 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/profiler_processor.js
+++ webkit/glue/devtools/js/profiler_processor.js
@@ -178,13 +178,14 @@ devtools.profiler.Processor = function() {
           processor: this.processHeapSampleBegin_ },
       'heap-sample-stats': { parsers: [null, null, parseInt, parseInt],
           processor: this.processHeapSampleStats_ },
+      'heap-sample-item': { parsers: [null, parseInt, parseInt],
+          processor: this.processHeapSampleItem_ },
       'heap-js-cons-item': { parsers: [null, parseInt, parseInt],
           processor: this.processHeapJsConsItem_ },
       'heap-sample-end': { parsers: [null, null],
           processor: this.processHeapSampleEnd_ },
       // Not used in DevTools Profiler.
       'shared-library': null,
-      'heap-sample-item': null,
       // Obsolete row types.
       'code-allocate': null,
       'begin-code-region': null,
@@ -389,6 +390,7 @@ devtools.profiler.Processor.prototype.processHeapSampleBegin_ = function(
   this.currentHeapSnapshot_ = {
       number: this.heapSnapshotId_++,
       entries: {},
+      lowlevels: {},
       ticks: ticks
   };
 };
@@ -399,6 +401,15 @@ devtools.profiler.Processor.prototype.processHeapSampleStats_ = function(
   if (space != 'Heap') return;
   this.currentHeapSnapshot_.capacity = capacity;
   this.currentHeapSnapshot_.used = used;
+};
+
+
+devtools.profiler.Processor.prototype.processHeapSampleItem_ = function(
+    item, number, size) {
+  if (!this.currentHeapSnapshot_) return;
+  this.currentHeapSnapshot_.lowlevels[item] = {
+    type: item, count: number, size: size
+  };
 };
 
 
@@ -414,8 +425,15 @@ devtools.profiler.Processor.prototype.processHeapJsConsItem_ = function(
 devtools.profiler.Processor.prototype.processHeapSampleEnd_ = function(
     space, state) {
   if (space != 'Heap') return;
-  WebInspector.panels.heap.addSnapshot(this.currentHeapSnapshot_);
+  var snapshot = this.currentHeapSnapshot_;
   this.currentHeapSnapshot_ = null;
+  // For some reason, 'used' from 'heap-sample-stats' sometimes differ from
+  // the sum of objects sizes. To avoid discrepancy, we re-calculate 'used'.
+  snapshot.used = 0;
+  for (var item in snapshot.lowlevels) {
+      snapshot.used += snapshot.lowlevels[item].size;
+  }
+  WebInspector.panels.heap.addSnapshot(snapshot);
 };
 
 
