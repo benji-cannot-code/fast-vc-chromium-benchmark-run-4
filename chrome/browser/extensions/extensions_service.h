9dd53ff0a91c4bf91a7a60ad77306d2f8ee1a7ad
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-18 20:10:37 +0000

    Revert "Fix "crashed extension" infobar browser crashes."
    TBR=cevans
    
    Review URL: http://codereview.chromium.org/173014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23644 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service.h
+++ chrome/browser/extensions/extensions_service.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/external_extension_provider.h"
 #include "chrome/browser/extensions/sandboxed_extension_unpacker.h"
 #include "chrome/common/extensions/extension.h"
-#include "chrome/common/notification_registrar.h"
 
 class Browser;
 class DictionaryValue;
@@ -54,7 +53,6 @@ class ExtensionUpdateService {
 // Manages installed and running Chromium extensions.
 class ExtensionsService
     : public ExtensionUpdateService,
-      public NotificationObserver,
       public base::RefCountedThreadSafe<ExtensionsService> {
  public:
 
@@ -212,11 +210,6 @@ class ExtensionsService
   // Whether the extension service is ready.
   bool is_ready() { return ready_; }
 
-  // NotificationObserver
-  virtual void Observe(NotificationType type,
-                       const NotificationSource& source,
-                       const NotificationDetails& details);
-
  private:
   // Look up an extension by ID, optionally including either or both of enabled
   // and disabled extensions.
@@ -253,8 +246,6 @@ class ExtensionsService
 
   // Is the service ready to go?
   bool ready_;
-
-  NotificationRegistrar registrar_;
 
   // Our extension updater, if updates are turned on.
   scoped_refptr<ExtensionUpdater> updater_;
