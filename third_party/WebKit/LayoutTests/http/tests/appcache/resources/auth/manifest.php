0cd4fe4fbf1f670880d7071b396f7e646b607f1c
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-18 19:03:16 +0000

    Reviewed by Darin Adler.
            https://bugs.webkit.org/show_bug.cgi?id=28431
            <rdar://problem/7137610> Application Cache doesn't work with HTTP authentication
    
            Test: http/tests/appcache/auth.html
    
            * loader/appcache/ApplicationCacheGroup.h:
            (WebCore::ApplicationCacheGroup::shouldUseCredentialStorage): If appcache is enabled,
            always allow it to use stored credentials (we can't ask WebKit client, and maybe we
            shouldn't ask it, because it's not a document that's loading these resources).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47439 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/appcache/resources/auth/manifest.php
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+  if (!isset($_SERVER['PHP_AUTH_USER'])) {
+   header('WWW-Authenticate: Basic realm="WebKit AppCache Test Realm"');
+   header('HTTP/1.0 401 Unauthorized');
+   echo 'Authentication canceled';
+   exit;
+  } else {
+   header("Content-Type: text/cache-manifest");
+   echo "CACHE MANIFEST\n";
+   echo "subresource.php\n";
+  }
+?>
