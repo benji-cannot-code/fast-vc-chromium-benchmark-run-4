5dda402b7f411a75aabe03f9fc39319577534440
atwilson@chromium.org <atwilson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-18 18:40:19 +0000

    Unreviewed (build break).
    Added missing include for WebWorkersPrivate.h.
    
    * WebKitClassFactory.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47434 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebKitClassFactory.cpp
+++ third_party/WebKit/WebKit/win/WebKitClassFactory.cpp
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebURLProtectionSpace.h"
 #include "WebURLResponse.h"
 #include "WebView.h"
+#include "WebWorkersPrivate.h"
 #pragma warning(push, 0)
 #include <JavaScriptCore/InitializeThreading.h>
 #include <WebCore/FontDatabase.h>
