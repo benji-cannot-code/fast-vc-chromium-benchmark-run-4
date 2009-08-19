4bb4193b9c805e56cbd41b61831c63a4051b1b04
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-19 03:33:44 +0000

    Relands drop support:
    Adds drop support for views on gtk. As X lazily provides drop data I
    needed to tweak the views API a bit.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/173025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23690 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automated_ui_tests/automated_ui_test_base.cc
+++ chrome/test/automated_ui_tests/automated_ui_test_base.cc
@@ -10,7 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/automation/tab_proxy.h"
 #include "chrome/test/automation/window_proxy.h"
 #include "chrome/test/ui/ui_test.h"
+
+#if defined(TOOLKIT_VIEWS)
 #include "views/view.h"
+#endif
 
 AutomatedUITestBase::AutomatedUITestBase() {}
 
