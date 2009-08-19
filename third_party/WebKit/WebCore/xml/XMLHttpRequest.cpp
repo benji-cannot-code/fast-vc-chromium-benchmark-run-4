eaf096ac18d72987d4a8f571fb5e5a7fe37bbd3d
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-19 03:13:16 +0000

    Release build fix.
    * xml/XMLHttpRequest.cpp: Sprinkle some #ifndef NDEBUG around.
    (WebCore::XMLHttpRequest::XMLHttpRequest):
    (WebCore::XMLHttpRequest::~XMLHttpRequest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
+++ third_party/WebKit/WebCore/xml/XMLHttpRequest.cpp
@@ -47,13 +47,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/StdLibExtras.h>
 #include <wtf/RefCountedLeakCounter.h>
 
-static WTF::RefCountedLeakCounter xmlHttpRequestCounter("XMLHttpRequest");
-
 #if USE(JSC)
 #include "JSDOMWindow.h"
 #endif
 
 namespace WebCore {
+
+#ifndef NDEBUG
+static WTF::RefCountedLeakCounter xmlHttpRequestCounter("XMLHttpRequest");
+#endif
 
 struct XMLHttpRequestStaticData {
     XMLHttpRequestStaticData();
@@ -154,8 +156,10 @@ XMLHttpRequest::XMLHttpRequest(ScriptExecutionContext* context)
     , m_lastSendLineNumber(0)
     , m_exceptionCode(0)
 {
-    xmlHttpRequestCounter.increment();
     initializeXMLHttpRequestStaticData();
+#ifndef NDEBUG
+    xmlHttpRequestCounter.increment();
+#endif
 }
 
 XMLHttpRequest::~XMLHttpRequest()
@@ -167,7 +171,9 @@ XMLHttpRequest::~XMLHttpRequest()
     if (m_upload)
         m_upload->disconnectXMLHttpRequest();
 
+#ifndef NDEBUG
     xmlHttpRequestCounter.decrement();
+#endif
 }
 
 Document* XMLHttpRequest::document() const
