3a5db4df22f00191ac33ee71923d7d928e6547e1
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-19 02:09:01 +0000

    Move LanguageComboboxModel to chrome/browser
    BUG=13524
    TEST=none
    
    Review URL: http://codereview.chromium.org/172083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23683 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/language_combobox_model.h
+++ chrome/browser/language_combobox_model.h
@@ -1,10 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_VIEWS_OPTIONS_LANGUAGE_COMBOBOX_MODEL_H_
-#define CHROME_BROWSER_VIEWS_OPTIONS_LANGUAGE_COMBOBOX_MODEL_H_
+#ifndef CHROME_BROWSER_LANGUAGE_COMBOBOX_MODEL_H_
+#define CHROME_BROWSER_LANGUAGE_COMBOBOX_MODEL_H_
+
+#include <map>
+#include <string>
+#include <vector>
 
 #include "app/combobox_model.h"
 #include "chrome/browser/profile.h"
@@ -68,4 +72,4 @@ class LanguageComboboxModel : public ComboboxModel {
   DISALLOW_COPY_AND_ASSIGN(LanguageComboboxModel);
 };
 
-#endif  // #ifndef CHROME_BROWSER_VIEWS_OPTIONS_LANGUAGE_COMBOBOX_MODEL_H_
+#endif  // #ifndef CHROME_BROWSER_LANGUAGE_COMBOBOX_MODEL_H_
--- chrome/browser/views/options/advanced_contents_view.cc
+++ chrome/browser/views/options/advanced_contents_view.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/shell_dialogs.h"
 #include "chrome/browser/views/options/cookies_view.h"
 #include "chrome/browser/views/options/fonts_languages_window_view.h"
-#include "chrome/browser/views/options/language_combobox_model.h"
 #include "chrome/browser/views/restart_message_box.h"
 #include "chrome/common/pref_member.h"
 #include "chrome/common/pref_names.h"
