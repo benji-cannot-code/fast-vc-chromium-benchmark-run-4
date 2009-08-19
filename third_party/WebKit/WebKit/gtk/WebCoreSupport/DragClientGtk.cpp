b1eefe22ec9b9fb2205f2e4f08eadce02a51bfd4
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-19 07:01:41 +0000

    2009-08-18  Xan Lopez  <xlopez@igalia.com>
            Rubber-stamped by Holger Freyther.
    
            Remove an unneeded include.
    
            * WebCoreSupport/DragClientGtk.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47497 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/WebCoreSupport/DragClientGtk.cpp
+++ third_party/WebKit/WebKit/gtk/WebCoreSupport/DragClientGtk.cpp
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkitwebview.h"
 
 #include <gtk/gtk.h>
-#include <gtk/gtkversion.h>
 #if !GTK_CHECK_VERSION(2, 14, 0)
 #define gtk_widget_get_window(widget) (widget)->window
 #endif
