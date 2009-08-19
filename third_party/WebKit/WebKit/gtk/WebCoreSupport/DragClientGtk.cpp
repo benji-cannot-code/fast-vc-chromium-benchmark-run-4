cc36e25cf880fda911721a140aaca30a08df0330
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-19 06:49:46 +0000

    2009-08-18  Fumitoshi Ukai  <ukai@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=28443
            Fix build break with gtk older than 2.14.
    
            WebKit gtk port requires gtk 2.10 or later, but DragClientGtk.cpp
            uses gtk_widget_get_window that is introduced in 2.14.
    
            * WebCoreSupport/DragClientGtk.cpp:
             define gtk_widget_get_window for gtk older than 2.14.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47496 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/WebCoreSupport/DragClientGtk.cpp
+++ third_party/WebKit/WebKit/gtk/WebCoreSupport/DragClientGtk.cpp
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkitwebview.h"
 
 #include <gtk/gtk.h>
+#include <gtk/gtkversion.h>
+#if !GTK_CHECK_VERSION(2, 14, 0)
+#define gtk_widget_get_window(widget) (widget)->window
+#endif
 
 using namespace WebCore;
 
@@ -116,4 +120,3 @@ void DragClient::dragControllerDestroyed()
     delete this;
 }
 }
-
