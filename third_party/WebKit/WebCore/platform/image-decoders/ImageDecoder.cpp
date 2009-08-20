15cbd34329410540b6218ce516047849b8fa7ae8
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-20 04:58:27 +0000

    Non-precomp headers build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@47547 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/image-decoders/ImageDecoder.cpp
+++ third_party/WebKit/WebCore/platform/image-decoders/ImageDecoder.cpp
@@ -19,9 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
+#include "config.h"
+
 #if ENABLE(IMAGE_DECODER_DOWN_SAMPLING)
 
-#include "config.h"
 #include "ImageDecoder.h"
 
 #include <algorithm>
