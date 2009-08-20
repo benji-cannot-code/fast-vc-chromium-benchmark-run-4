15b30acbd07c8711942f89291e4e70d7185442cd
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-20 19:03:05 +0000

    Change the enum style to use MACRO_STYLE rather than kConstant style to be consistent with chrome code base.
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/174145
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23851 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/appcache/manifest_parser.cc
+++ webkit/appcache/manifest_parser.cc
@@ -39,10 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace appcache {
 
 enum Mode {
-  kExplicit,
-  kFallback,
-  kOnlineWhitelist,
-  kUnknown,
+  EXPLICIT,
+  FALLBACK,
+  ONLINE_WHITELIST,
+  UNKNOWN,
 };
 
 bool ParseManifest(const GURL& manifest_url, const char* data, int length,
@@ -54,7 +54,7 @@ bool ParseManifest(const GURL& manifest_url, const char* data, int length,
   DCHECK(manifest.online_whitelist_namespaces.empty());
   DCHECK(!manifest.online_whitelist_all);
 
-  Mode mode = kExplicit;
+  Mode mode = EXPLICIT;
 
   std::wstring data_string;
   // TODO(jennb): cannot do UTF8ToWide(data, length, &data_string);
@@ -118,19 +118,19 @@ bool ParseManifest(const GURL& manifest_url, const char* data, int length,
     std::wstring line(line_start, tmp - line_start + 1);
 
     if (line == L"CACHE:") {
-      mode = kExplicit;
+      mode = EXPLICIT;
     } else if (line == L"FALLBACK:") {
-      mode = kFallback;
+      mode = FALLBACK;
     } else if (line == L"NETWORK:") {
-      mode = kOnlineWhitelist;
+      mode = ONLINE_WHITELIST;
     } else if (*(line.end() - 1) == ':') {
-      mode = kUnknown;
-    } else if (mode == kUnknown) {
+      mode = UNKNOWN;
+    } else if (mode == UNKNOWN) {
       continue;
-    } else if (line == L"*" && mode == kOnlineWhitelist) {
+    } else if (line == L"*" && mode == ONLINE_WHITELIST) {
       manifest.online_whitelist_all = true;
       continue;
-    } else if (mode == kExplicit || mode == kOnlineWhitelist) {
+    } else if (mode == EXPLICIT || mode == ONLINE_WHITELIST) {
       const wchar_t *line_p = line.c_str();
       const wchar_t *line_end = line_p + line.length();
 
@@ -154,12 +154,12 @@ bool ParseManifest(const GURL& manifest_url, const char* data, int length,
         continue;
       }
 
-      if (mode == kExplicit) {
+      if (mode == EXPLICIT) {
         manifest.explicit_urls.insert(url.spec());
       } else {
         manifest.online_whitelist_namespaces.push_back(url);
       }
-    } else if (mode == kFallback) {
+    } else if (mode == FALLBACK) {
       const wchar_t* line_p = line.c_str();
       const wchar_t* line_end = line_p + line.length();
 
