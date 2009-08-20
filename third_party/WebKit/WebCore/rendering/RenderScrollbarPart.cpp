21783a153fbfa1a7ba3bd82b8640cc3eb82239b4
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-20 00:04:48 +0000

    - Fix <rdar://problem/7152589> Stylable scrollbar corners aren't working.
    Reviewed by David Hyatt and Dan Bernstein.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::FrameView):
    Initialize m_scrollCorner.
    
    (WebCore::FrameView::~FrameView):
    Assert that m_scrollCorner is null.
    
    (WebCore::FrameView::detachCustomScrollbars):
    Destroy m_scrollCorner.
    
    (WebCore::FrameView::invalidateScrollCorner):
    Invalidate the scroll corner.
            
    (WebCore::FrameView::updateScrollCorner):
    Create/destroy the scroll corner.
            
    (WebCore::FrameView::paintScrollCorner):
    Paint the scroll corner.
            
    * page/FrameView.h:
    (WebCore::FrameView::isFrameViewScrollCorner):
    Return whether a RenderScrollbarPart is the frame view scroll corner.
            
    * platform/ScrollView.cpp:
    (WebCore::ScrollView::updateScrollbars):
    Go ahead and update the scroll corner.
            
    (WebCore::ScrollView::setScrollbarsSuppressed):
    Pass the scroll corner rect to invalidateRect.
            
    (WebCore::ScrollView::scrollCornerRect):
    Return the scroll corner rect.
            
    * platform/ScrollView.h:
    * rendering/RenderScrollbarPart.cpp:
    (WebCore::RenderScrollbarPart::imageChanged):
    If this scrollbar part is the frame view scroll corner, invalidate it.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderScrollbarPart.cpp
+++ third_party/WebKit/WebCore/rendering/RenderScrollbarPart.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderScrollbarPart.h"
 #include "RenderScrollbar.h"
 #include "RenderScrollbarTheme.h"
+#include "RenderView.h"
 
 using namespace std;
 
@@ -141,8 +142,16 @@ void RenderScrollbarPart::imageChanged(WrappedImagePtr image, const IntRect* rec
 {
     if (m_scrollbar && m_part != NoPart)
         m_scrollbar->theme()->invalidatePart(m_scrollbar, m_part);
-    else
+    else {
+        if (FrameView* frameView = view()->frameView()) {
+            if (frameView->isFrameViewScrollCorner(this)) {
+                frameView->invalidateScrollCorner();
+                return;
+            }
+        }
+        
         RenderBlock::imageChanged(image, rect);
+    }
 }
 
 void RenderScrollbarPart::paintIntoRect(GraphicsContext* graphicsContext, int tx, int ty, const IntRect& rect)
