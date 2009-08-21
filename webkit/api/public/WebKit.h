a55da2ec58026be785362ab4fcd4fd0559a724cf
dimich@google.com <dimich@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-21 02:26:02 +0000

    Reverting 23931.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23939 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/public/WebKit.h
+++ webkit/api/public/WebKit.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebKit_h
 
 #include "WebCommon.h"
-#include "WebURL.h"
 
 namespace v8 {
     class Extension;
@@ -102,13 +101,6 @@ namespace WebKit {
     // Enables HTML5 database support.
     WEBKIT_API void enableDatabases();
     WEBKIT_API bool databasesEnabled();
-
-    // Support for whitelisting access to origins beyond the same-origin policy.
-    WEBKIT_API void whiteListAccessFromOrigin(
-        const WebURL& sourceOrigin, const WebString& destinationProtocol,
-        const WebString& destinationHost, bool allowDestinationSubdomains);
-    WEBKIT_API void resetOriginAccessWhiteLists();
-
 } // namespace WebKit
 
 #endif
