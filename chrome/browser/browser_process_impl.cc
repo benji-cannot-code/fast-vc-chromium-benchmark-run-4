5709cfd65a2b96e14420a19f5555a4942bfb3e5f
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-21 22:35:29 +0000

    Mask the BrokerServices class behind an ifdef for Windows only; Linux and Mac have their own sandboxes.  Remove the corresponding stubs from temp_scaffolding_stubs.h.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/173228
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24032 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -124,8 +124,10 @@ BrowserProcessImpl::BrowserProcessImpl(const CommandLine& command_line)
       created_db_thread_(false),
       created_profile_manager_(false),
       created_local_state_(false),
+#if defined(OS_WIN)
       initialized_broker_services_(false),
       broker_services_(NULL),
+#endif  // defined(OS_WIN)
       created_icon_manager_(false),
       created_debugger_wrapper_(false),
       created_devtools_manager_(false),
@@ -393,6 +395,7 @@ void BrowserProcessImpl::CreateLocalState() {
   local_state_.reset(new PrefService(local_state_path, file_thread()));
 }
 
+#if defined(OS_WIN)
 void BrowserProcessImpl::InitBrokerServices(
     sandbox::BrokerServices* broker_services) {
   DCHECK(!initialized_broker_services_ && broker_services_ == NULL);
@@ -400,6 +403,7 @@ void BrowserProcessImpl::InitBrokerServices(
   initialized_broker_services_ = true;
   broker_services_ = broker_services;
 }
+#endif  // defined(OS_WIN)
 
 void BrowserProcessImpl::CreateIconManager() {
   DCHECK(!created_icon_manager_ && icon_manager_.get() == NULL);
