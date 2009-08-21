5a0ebd114b3ac4faee1c311f2dc30cdba2ec5e96
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-21 22:57:39 +0000

    Tweak bookmark star bubble with UI dudes: - Bubble arrow now gives STAR a wedgie (Glen) - Bubble slightly higher up (Ben) - 'Remove' button moved to bottom-left; made full size (Cole, Glen, Ben) - Title alignment tweaks (Cole) - Window height += 8 for better alignment (Cole) - Window has shadow and no explicit border (Cole)
    Review URL: http://codereview.chromium.org/173215
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24038 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bubble_window.mm
+++ chrome/browser/cocoa/bookmark_bubble_window.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self setExcludedFromWindowsMenu:YES];
     [self setAlphaValue:1.0];
     [self setOpaque:NO];
+    [self setHasShadow:YES];
   }
   return self;
 }
