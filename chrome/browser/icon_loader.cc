bcdd969cd9bdbdce5c62db7f2321edec07a70751
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-21 22:37:35 +0000

    Move gtk_settings_get_default() call to the UI thread.
    BUG=19971
    
    Review URL: http://codereview.chromium.org/173216
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24034 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/icon_loader.cc
+++ chrome/browser/icon_loader.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/icon_loader.h"
 
 #include "base/message_loop.h"
+#include "base/mime_util.h"
 #include "base/thread.h"
 #include "chrome/browser/browser_process.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -24,6 +25,11 @@ IconLoader::~IconLoader() {
 
 void IconLoader::Start() {
   target_message_loop_ = MessageLoop::current();
+
+#if defined(OS_LINUX)
+  // This call must happen on the UI thread before we can start loading icons.
+  mime_util::DetectGtkTheme();
+#endif
 
   g_browser_process->file_thread()->message_loop()->PostTask(FROM_HERE,
       NewRunnableMethod(this, &IconLoader::ReadIcon));
