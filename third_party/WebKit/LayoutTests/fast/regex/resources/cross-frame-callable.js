1aa6d0b70c5c5557f3c7d43c82aad71125d5f5c6
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-21 22:57:50 +0000

    2009-08-21  Christian Plesner Hansen  <christian.plesner.hansen@gmail.com>
            Reviewed by Eric Seidel.
    
            Regression test for bug fixed in
            http://codereview.chromium.org/171039.  Regexps created in one
            frame were not callable outside that frame.
            https://bugs.webkit.org/show_bug.cgi?id=28387
    
            * fast/regex/cross-frame-callable-expected.txt: Added.
            * fast/regex/cross-frame-callable.html: Added.
            * fast/regex/resources/cross-frame-callable.js: Added.
            (doTest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47647 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/regex/resources/cross-frame-callable.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function doTest(childRegExp)
+{
+    re = childRegExp;
+    shouldBe("re('a')", "['a']");
+}
+
+var iframe = document.createElement('iframe');
+document.body.appendChild(iframe);
+iframe.contentDocument.write('<script>top.doTest(/a/)</script>');
+document.write('DONE');
+
+var successfullyParsed = true;
