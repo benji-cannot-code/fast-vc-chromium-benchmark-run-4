8a663f10af19ca8679df7d8379f0792eb4c6c2eb
dpranke@google.com <dpranke@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-21 21:07:53 +0000

    Try change 23924 again - refactor layout_package.path_utils and layout_package.platform_utils.
    Deltas from that change:
    
      * Don't enable 'win-xp' as the default platform
      * Don't assume 'webkit/Release' exists (it doesn't by default on mac and 
          linux), and fix the output results directory path in run_webkit_tests
      * Update tools/valgrind/chrome_tests.py (which I didn't realize
          depended on the layout_package code).
    
      BUG=none
      R=tony@chromium.org
      TEST=none
    
    Review URL: http://codereview.chromium.org/174209
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24007 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/layout_tests/layout_package/test_failures.py
+++ webkit/tools/layout_tests/layout_package/test_failures.py
@@ -1,11 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+# Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 """Classes for failures that occur during tests."""
-
-import path_utils
 
 class FailureSort(object):
   """A repository for failure sort orders and tool to facilitate sorting."""
