3b8a576d7cb376fdfbe5d4fc8df32736bb8bcb13
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-21 20:24:58 +0000

    Reverting r24002 due to test_shell_tests redness
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24004 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/pipeline_impl.cc
+++ media/base/pipeline_impl.cc
@@ -263,11 +263,6 @@ bool PipelineImpl::IsStreaming() const {
   return streaming_;
 }
 
-bool PipelineImpl::IsLoaded() const {
-  AutoLock auto_lock(lock_);
-  return loaded_;
-}
-
 PipelineError PipelineImpl::GetError() const {
   AutoLock auto_lock(lock_);
   return error_;
@@ -293,7 +288,6 @@ void PipelineImpl::ResetState() {
   buffered_time_    = kZero;
   buffered_bytes_   = 0;
   streaming_        = false;
-  loaded_           = false;
   total_bytes_      = 0;
   video_width_      = 0;
   video_height_     = 0;
@@ -410,12 +404,6 @@ void PipelineImpl::NotifyEnded() {
   DCHECK(IsRunning());
   message_loop_->PostTask(FROM_HERE,
       NewRunnableMethod(this, &PipelineImpl::NotifyEndedTask));
-}
-
-void PipelineImpl::SetLoaded(bool loaded) {
-  DCHECK(IsRunning());
-  AutoLock auto_lock(lock_);
-  loaded_ = loaded;
 }
 
 void PipelineImpl::BroadcastMessage(FilterMessage message) {
