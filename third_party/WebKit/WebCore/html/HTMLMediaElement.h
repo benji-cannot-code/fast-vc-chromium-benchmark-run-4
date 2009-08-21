67e39dce900a6d654e292e47031463292b5cb4e6
ajwong@chromium.org <ajwong@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-21 04:10:10 +0000

    WebCore: HTML5 media elements do not fire waiting events correctly https://bugs.webkit.org/show_bug.cgi?id=28335
    Patch by Albert J. Wong <ajwong@chromium.org> on 2009-08-20
    Reviewed by David Levin.
    
    Fire the waiting event before seeking, and fix a case where firing of
    the seeked event is missed when the ready state is changed during
    a seek.
    
    Added video-waiting-seeking.html into manual tests because not
    all platforms allow seeking into non-buffered ranges.
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::setReadyState): add support for
    waiting event when seeking.
    (WebCore::HTMLMediaElement::finishSeek): send seeked event
    whenever seeking finishes.
    (WebCore::HTMLMediaElement::mediaPlayerTimeChanged): dispatch to
    new function.
    * html/HTMLMediaElement.h:
    * manual-tests/video-waiting-seeking.html: Added.
    
    LayoutTests: HTML5 media elements do not fire waiting events correctly
    https://bugs.webkit.org/show_bug.cgi?id=28335
    
    Patch by Albert J. Wong <ajwong@chromium.org> on 2009-08-20
    Reviewed by David Levin.
    
    New manual test to check if waiting/seeking events are fired
    when seeking into a non-buffered region.
    
    * http/tests/media/video-throttled-load.cgi: Added.
    * media/video-test.js:
    (isInTimeRanges):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47619 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLMediaElement.h
+++ third_party/WebKit/WebCore/html/HTMLMediaElement.h
@@ -192,6 +192,7 @@ private:
     void stopPeriodicTimers();
 
     void seek(float time, ExceptionCode&);
+    void finishSeek();
     void checkIfSeekNeeded();
     
     void scheduleTimeupdateEvent(bool periodicEvent);
