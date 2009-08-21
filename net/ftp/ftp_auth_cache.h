d8583e96e3041576f7ed64155307dffd254977bb
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-21 15:55:25 +0000

    Followup after http://codereview.chromium.org/165167, fixing the nits in FtpAuthCache.
    TEST=Covered by net_unittests.
    BUG=none
    
    Review URL: http://codereview.chromium.org/174184
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23961 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_auth_cache.h
+++ net/ftp/ftp_auth_cache.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <list>
 
-#include "net/base/auth.h"
 #include "googleurl/src/gurl.h"
+#include "net/base/auth.h"
 
 namespace net {
 
@@ -23,11 +23,11 @@ namespace net {
 //   GURL("ftp://myserver/PATH") -- WRONG, paths not allowed
 class FtpAuthCache {
  public:
-  FtpAuthCache() {}
-  ~FtpAuthCache() {}
-
   // Maximum number of entries we allow in the cache.
   static const size_t kMaxEntries;
+
+  FtpAuthCache() {}
+  ~FtpAuthCache() {}
 
   // Check if we have authentication data for ftp server at |origin|.
   // Returns the address of corresponding AuthData object (if found) or NULL
@@ -54,7 +54,7 @@ class FtpAuthCache {
   typedef std::list<Entry> EntryList;
 
   // Return Entry corresponding to given |origin| or NULL if not found.
-  Entry* LookupByOrigin(const GURL& origin);
+  Entry* LookupEntry(const GURL& origin);
 
   // Internal representation of cache, an STL list. This makes lookups O(n),
   // but we expect n to be very low.
