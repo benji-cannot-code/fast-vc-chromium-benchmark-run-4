5568bb85630b7d008d6d21a6dc3b8aa04fa8e4fa
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-21 22:46:01 +0000

    Speculative QT build fix.
    Reviewed by NOBODY (build fix).
    
    * runtime/StringPrototype.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47646 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/StringPrototype.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/StringPrototype.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CachedCall.h"
 #include "Error.h"
+#include "Executable.h"
 #include "JSArray.h"
 #include "JSFunction.h"
 #include "ObjectPrototype.h"
