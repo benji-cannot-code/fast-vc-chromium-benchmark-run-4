c107dd3c4ac7fd51c667613d8d30fd39a401151c
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-22 00:59:45 +0000

    Speculative QuickTime build fix.
    Reviewed by NOBODY (build fix).
    
    * runtime/JSArray.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47653 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSArray.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSArray.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ArrayPrototype.h"
 #include "CachedCall.h"
 #include "Error.h"
+#include "Executable.h"
 #include "PropertyNameArray.h"
 #include <wtf/AVLTree.h>
 #include <wtf/Assertions.h>
