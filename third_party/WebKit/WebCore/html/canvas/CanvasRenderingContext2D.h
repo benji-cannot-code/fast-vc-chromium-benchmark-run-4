cf2adb535d09704feebdfbb44886f6b29ea849df
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-22 16:16:00 +0000

    Backing out http://trac.webkit.org/changeset/47669
    git-svn-id: svn://svn.chromium.org/blink/trunk@47673 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.h
+++ third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CanvasRenderingContext2D_h
 #define CanvasRenderingContext2D_h
 
-#include "CanvasRenderingContext.h"
 #include "TransformationMatrix.h"
 #include "FloatSize.h"
 #include "Font.h"
@@ -56,9 +55,14 @@ namespace WebCore {
 
     typedef int ExceptionCode;
 
-    class CanvasRenderingContext2D : public CanvasRenderingContext {
+    class CanvasRenderingContext2D : public Noncopyable {
     public:
         CanvasRenderingContext2D(HTMLCanvasElement*);
+
+        void ref(); 
+        void deref(); 
+
+        HTMLCanvasElement* canvas() const { return m_canvas; } 
         virtual ~CanvasRenderingContext2D() { }
         
         virtual bool is2d() { return true; }
@@ -262,6 +266,7 @@ namespace WebCore {
         void checkOrigin(const KURL&);
         void checkOrigin(const String&);
 
+        HTMLCanvasElement* m_canvas;
         Vector<State, 1> m_stateStack;
     };
 
