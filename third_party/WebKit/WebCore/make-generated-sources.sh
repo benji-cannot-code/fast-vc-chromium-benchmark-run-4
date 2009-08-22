255c6db21c1f3885d11c88312a484e4a7f012607
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-22 06:53:16 +0000

    * WebCore.vcproj/build-generated-files.sh: * make-generated-sources.sh: Removed obsolete code to set up CREATE_HASH_TABLE.
    git-svn-id: svn://svn.chromium.org/blink/trunk@47666 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/make-generated-sources.sh
+++ third_party/WebKit/WebCore/make-generated-sources.sh
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 export SOURCE_ROOT=$PWD
 export SRCROOT=$PWD
 export WebCore=$PWD
-export CREATE_HASH_TABLE="$SRCROOT/../JavaScriptCore/create_hash_table"
 
 mkdir -p DerivedSources/WebCore &&
 make -C DerivedSources/WebCore -f ../../DerivedSources.make $@
