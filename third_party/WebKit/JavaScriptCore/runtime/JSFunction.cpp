cf1784c478b8e96f38435b5de399e1e66865c4d1
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-23 05:04:45 +0000

    REGRESSION(r47639-r47660): Webkit crashes on launch on PowerPC https://bugs.webkit.org/show_bug.cgi?id=28655
    Reviewed by Mark Rowe.
    
    * runtime/JSFunction.cpp:
    (JSC::JSFunction::JSFunction): Initialize properly with a VPtrHackExecutable.
    * wtf/Platform.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47686 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSFunction.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSFunction.cpp
@@ -53,9 +53,7 @@ bool JSFunction::isHostFunctionNonInline() const
 
 JSFunction::JSFunction(PassRefPtr<Structure> structure)
     : Base(structure)
-#if ENABLE(JIT)
     , m_executable(adoptRef(new VPtrHackExecutable()))
-#endif
 {
 }
 
