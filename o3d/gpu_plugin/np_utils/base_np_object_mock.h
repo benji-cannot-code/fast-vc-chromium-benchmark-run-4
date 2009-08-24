038edcce55343231712c17cfe403a4527d794368
apatrick@google.com <apatrick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-24 23:41:07 +0000

    Added DispatchedNPObject. Automatically invokes member functions on C++ NPObjects for NPAPI methods. See comment in DispatchedNPObject. Utilities for invoking the methods of an NPAPI object using C++ typed arguments and return values. BUG=none TEST=none
    Review URL: http://codereview.chromium.org/173237
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24177 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/gpu_plugin/np_utils/base_np_object_mock.h
+++ o3d/gpu_plugin/np_utils/base_np_object_mock.h
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef O3D_GPU_PLUGIN_NP_OBJECT_BASE_MOCK_H_
-#define O3D_GPU_PLUGIN_NP_OBJECT_BASE_MOCK_H_
+#ifndef O3D_GPU_PLUGIN_NP_UTILS_BASE_NP_OBJECT_MOCK_H_
+#define O3D_GPU_PLUGIN_NP_UTILS_BASE_NP_OBJECT_MOCK_H_
 
-#include "o3d/gpu_plugin/base_np_object.h"
+#include "o3d/gpu_plugin/np_utils/base_np_object.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
 namespace o3d {
@@ -14,14 +14,6 @@ namespace gpu_plugin {
 
 class MockBaseNPObject : public BaseNPObject {
  public:
-  explicit MockBaseNPObject(NPP npp) : BaseNPObject(npp) {
-    ++count_;
-  }
-
-  ~MockBaseNPObject() {
-    --count_;
-  }
-
   static int count() {
     return count_;
   }
@@ -38,6 +30,15 @@ class MockBaseNPObject : public BaseNPObject {
   MOCK_METHOD2(Enumerate, bool(NPIdentifier**, uint32_t*));
   MOCK_METHOD3(Construct, bool(const NPVariant*, uint32_t, NPVariant*));
 
+ protected:
+  explicit MockBaseNPObject(NPP npp) : BaseNPObject(npp) {
+    ++count_;
+  }
+
+  ~MockBaseNPObject() {
+    --count_;
+  }
+
  private:
   static int count_;
 };
@@ -45,4 +46,4 @@ class MockBaseNPObject : public BaseNPObject {
 }  // namespace gpu_plugin
 }  // namespace o3d
 
-#endif  // O3D_GPU_PLUGIN_NP_OBJECT_BASE_MOCK_H_
+#endif  // O3D_GPU_PLUGIN_NP_UTILS_BASE_NP_OBJECT_MOCK_H_
