6b146bddf8283123a89cc41160b635ef52b3508c
tschmelcher@google.com <tschmelcher@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-25 18:04:52 +0000

    Linux: fix a couple problems that Lintian reports for the .deb file:   - Eliminate useless call to ldconfig (see Debian bug #364436)   - Eliminate spurious rpath. We were setting the rpath equal to the build output directory for libs, which is wrong. The rpath is the _run-time_ library search path for the resulting binary, i.e. the one used by ld.so at load time. Setting it equal to the path to build output on the build machine is nonsense. Also:   - Use new-found knowledge of rpath to eliminate the need to symlink into /usr/lib(32) by instead setting rpath to /opt/google/o3d/lib and putting our three dependent libs there.
    Review URL: http://codereview.chromium.org/174369
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24250 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/site_scons/site_tools/component_builders.py
+++ o3d/site_scons/site_tools/component_builders.py
@@ -554,7 +554,6 @@ def generate(env):
   )
   env.Append(
       LIBPATH=['$LIB_DIR'],
-      RPATH=['$LIB_DIR'],
 
       # Default alias groups for component builders
       COMPONENT_PACKAGE_GROUPS=['all_packages'],
