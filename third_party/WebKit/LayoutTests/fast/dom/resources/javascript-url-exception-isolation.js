84535db6305aa6773719a1dd4df9b824cc2852af
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-25 19:31:51 +0000

    2009-08-25  Mads Ager  <ager@chromium.org>
            Reviewed by Adam Barth.
    
            [V8] Exception from JavaScript propagates to main script
            https://bugs.webkit.org/show_bug.cgi?id=26433
    
            Test that both compile time and runtime exceptions thrown in
            javascript URLs are isolated from the main script.
    
            * fast/dom/javascript-url-exception-isolation-expected.txt: Added.
            * fast/dom/javascript-url-exception-isolation.html: Added.
            * fast/dom/resources/javascript-url-exception-isolation.js: Added.
    2009-08-25  Mads Ager  <ager@chromium.org>
    
            Reviewed by Adam Barth.
    
            [V8] Exception from JavaScript propagates to main script
            https://bugs.webkit.org/show_bug.cgi?id=26433
    
            Make sure that exceptions thrown both at compile time and at
            runtime in javascript URLs are isolated from the main script.
    
            Test: fast/dom/javascript-url-exception-isolation.html
    
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::evaluate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/resources/javascript-url-exception-isolation.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Exceptions thrown in javascript URLs should not propagate to the main script.")
+
+var subframe = document.createElement("iframe");
+document.body.appendChild(subframe);
+
+var caughtException = false;
+
+// Runtime exception.
+try {
+    subframe.src = 'javascript:throw 42';
+} catch(e) {
+    caughtException = true;
+}
+shouldBeFalse('caughtException');
+
+// Compile-time exception.
+try {
+    subframe.src = 'javascript:<html></html>';
+} catch(e) {
+    caughtException = true;
+}
+shouldBeFalse('caughtException');
+
+var successfullyParsed = true;
