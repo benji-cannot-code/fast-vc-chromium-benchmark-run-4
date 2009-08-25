07b4e0b34539be43a21b55bb5e27f2698fdb1177
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-25 08:31:17 +0000

    Try again to commit r24174: Change the way json_schema.js is loaded.
    TBR=mpcomplete@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24224 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/json_schema_test.js
+++ chrome/test/data/extensions/json_schema_test.js
@@ -9,7 +9,7 @@ function assert(truth) {
 }
 
 function assertValid(type, instance, schema, types) {
-  var validator = new chrome.JSONSchemaValidator();
+  var validator = new chromeHidden.JSONSchemaValidator();
   if (types)
     validator.addTypes(types);
   validator["validate" + type](instance, schema, "");
@@ -21,7 +21,7 @@ function assertValid(type, instance, schema, types) {
 }
 
 function assertNotValid(type, instance, schema, errors, types) {
-  var validator = new chrome.JSONSchemaValidator();
+  var validator = new chromeHidden.JSONSchemaValidator();
   if (types)
     validator.addTypes(types);
   validator["validate" + type](instance, schema, "");
@@ -39,7 +39,7 @@ function assertNotValid(type, instance, schema, errors, types) {
 }
 
 function formatError(key, replacements) {
-  return chrome.JSONSchemaValidator.formatError(key, replacements);
+  return chromeHidden.JSONSchemaValidator.formatError(key, replacements);
 }
 
 function testFormatError() {
