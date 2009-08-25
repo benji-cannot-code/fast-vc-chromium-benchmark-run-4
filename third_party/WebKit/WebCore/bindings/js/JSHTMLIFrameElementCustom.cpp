032152de6dbda8794e33c90434940902152c017e
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-25 00:03:19 +0000

    Reviewed by Darin Adler and Sam Weinig.
            https://bugs.webkit.org/show_bug.cgi?id=28688
            REGRESSION(r24994): Cannot create a frame with a javascript URL
    
            Test: http/tests/security/javascriptURL/javascriptURL-in-new-iframe.html
    
            * bindings/js/JSAttrCustom.cpp: (WebCore::JSAttr::setValue):
            * bindings/js/JSElementCustom.cpp: (WebCore::allowSettingSrcToJavascriptURL):
            * bindings/js/JSHTMLFrameElementCustom.cpp: (WebCore::allowSettingJavascriptURL):
            * bindings/js/JSHTMLIFrameElementCustom.cpp: (WebCore::JSHTMLIFrameElement::setSrc):
            Omit the checks if there is no document in frame yet.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSHTMLIFrameElementCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSHTMLIFrameElementCustom.cpp
@@ -49,7 +49,8 @@ void JSHTMLIFrameElement::setSrc(ExecState* exec, JSValue value)
     String srcValue = valueToStringWithNullCheck(exec, value);
 
     if (protocolIsJavaScript(deprecatedParseURL(srcValue))) {
-        if (!checkNodeSecurity(exec, imp->contentDocument()))
+        Document* contentDocument = imp->contentDocument();
+        if (contentDocument && !checkNodeSecurity(exec, contentDocument))
             return;
     }
 
