a03c3d542404b61a0bc67e5bf5f3d06e33b3f774
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-25 21:21:29 +0000

    OSX: Fix the select-all-on-mouse-down in Omnibox.
    Getting clicks in the decoration areas right caused a change to how
    clicks on the unfocused field worked.  NSTextField also forwards the
    event to get click-drag selection right, this does the same thing for
    clicks in the decorations (other clicks are passed to super).
    
    http://crbug.com/20142
    TEST=Browse to www.google.com.  Click in page.  Click in omnibox
    without releasing.  You should get a caret insertion point.
    
    Review URL: http://codereview.chromium.org/173362
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24325 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/autocomplete_text_field_cell.h
+++ chrome/browser/cocoa/autocomplete_text_field_cell.h
@@ -57,6 +57,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setSearchHintString:(NSString*)aString;
 - (void)clearKeywordAndHint;
 
+// Return the portion of the cell to use for text display.
+- (NSRect)textFrameForFrame:(NSRect)cellFrame;
+
 @end
 
 // Internal methods here exposed for unit testing.
@@ -64,8 +67,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @property(readonly) NSAttributedString* keywordString;
 @property(readonly) NSAttributedString* hintString;
-
-// Return the portion of the cell to use for text display.
-- (NSRect)textFrameForFrame:(NSRect)cellFrame;
 
 @end
