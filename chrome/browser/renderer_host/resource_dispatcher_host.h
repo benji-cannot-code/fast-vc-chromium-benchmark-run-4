3033d91177e73a2af5eef9e458c3a3264a331858
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-25 23:45:58 +0000

    Delay request loading until all user scripts that request depends on are ready.
    BUG=11547
    TEST=See bug.
    
    Review URL: http://codereview.chromium.org/173239
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24372 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/resource_dispatcher_host.h
+++ chrome/browser/renderer_host/resource_dispatcher_host.h
@@ -38,6 +38,7 @@ class PluginService;
 class SafeBrowsingService;
 class SaveFileManager;
 class SSLClientAuthHandler;
+class UserScriptListener;
 class URLRequestContext;
 class WebKitThread;
 struct ViewHostMsg_Resource_Request;
@@ -537,6 +538,8 @@ class ResourceDispatcherHost : public URLRequest::Delegate {
 
   // We own the save file manager.
   scoped_refptr<SaveFileManager> save_file_manager_;
+
+  scoped_refptr<UserScriptListener> user_script_listener_;
 
   scoped_refptr<SafeBrowsingService> safe_browsing_;
 
