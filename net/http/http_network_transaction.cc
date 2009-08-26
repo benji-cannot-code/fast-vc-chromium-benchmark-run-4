3c0079c02f6a74ca1df0a82627c6f309fbf03495
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-26 02:33:46 +0000

    Add the ability to not send cookies or send user name/password.
    Added unit tests for the above functionality plus the do not save cookies functionality.
    
    BUG=http://crbug.com/10961
    TEST=Added unit tests for this. Also, this isn't yet called from the rest of the code. 
    
    Review URL: http://codereview.chromium.org/173206
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24412 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction.cc
+++ net/http/http_network_transaction.cc
@@ -1684,7 +1684,8 @@ bool HttpNetworkTransaction::ShouldApplyProxyAuth() const {
 }
 
 bool HttpNetworkTransaction::ShouldApplyServerAuth() const {
-  return !establishing_tunnel_;
+  return !establishing_tunnel_ &&
+      !(request_->load_flags & LOAD_DO_NOT_SEND_AUTH_DATA);
 }
 
 std::string HttpNetworkTransaction::BuildAuthorizationHeader(
@@ -1887,10 +1888,13 @@ int HttpNetworkTransaction::HandleAuthChallenge() {
 
   auth_identity_[target].invalid = true;
 
-  // Find the best authentication challenge that we support.
-  HttpAuth::ChooseBestChallenge(response_.headers.get(),
-                                target,
-                                &auth_handler_[target]);
+  if (target != HttpAuth::AUTH_SERVER ||
+      !(request_->load_flags & LOAD_DO_NOT_SEND_AUTH_DATA)) {
+    // Find the best authentication challenge that we support.
+    HttpAuth::ChooseBestChallenge(response_.headers.get(),
+                                  target,
+                                  &auth_handler_[target]);
+  }
 
   if (!auth_handler_[target]) {
     if (establishing_tunnel_) {
