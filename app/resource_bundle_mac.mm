df85753f1657be8071063c3a8645f313d6bad46d
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-26 16:35:58 +0000

    Add a helper to resource bundle to get an NSImage back. skia/ext/skia_utils_mac.h includes CoreGraphics/CGColor.h so we have to relay the framework include path so someone can include the header without errors.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/173363
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24453 0039d316-1c4b-4281-b951-d872f2087c98

--- app/resource_bundle_mac.mm
+++ app/resource_bundle_mac.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/string_piece.h"
 #include "base/string_util.h"
+#include "skia/ext/skia_utils_mac.h"
 
 ResourceBundle::~ResourceBundle() {
   FreeImages();
@@ -116,4 +117,15 @@ string16 ResourceBundle::GetLocalizedString(int message_id) {
   string16 msg(reinterpret_cast<const char16*>(data.data()),
                data.length() / 2);
   return msg;
+}
+
+NSImage* ResourceBundle::GetNSImageNamed(int resource_id) {
+  // Currently this doesn't make a cache holding these as NSImages because
+  // GetBitmapNamed has a cache, and we don't want to double cache.
+  SkBitmap* bitmap = GetBitmapNamed(resource_id);
+  if (!bitmap)
+    return nil;
+
+  NSImage* nsimage = gfx::SkBitmapToNSImage(*bitmap);
+  return nsimage;
 }
--- skia/skia.gyp
+++ skia/skia.gyp
@@ -645,6 +645,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           '../third_party/skia/include/effects',
           'ext',
         ],
+        'mac_framework_dirs': [
+          '$(SDKROOT)/System/Library/Frameworks/ApplicationServices.framework/Frameworks',
+        ],
       },
     },
   ],
