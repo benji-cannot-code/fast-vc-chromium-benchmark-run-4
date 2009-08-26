31248ab7b02ac81dc156da4c5ab2f830fd45cfbd
dimich@chromium.org <dimich@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-26 05:17:15 +0000

    WebCore: Timers from cached pages fire instantly rather than the after the specified timeout delay https://bugs.webkit.org/show_bug.cgi?id=28683
    Reviewed by Brady Eidson.
    
    Test: fast/dom/Window/timer-resume-on-navigation-back.html
    
    * history/CachedPage.cpp:
    (WebCore::CachedPage::restore): Remove duplicated Frame::restore() call, since
        it should be done only once in FrameLoader::open(cachedFrame)
    * page/DOMTimer.cpp: Added a debug-only flag and ASSERT to catch out-of-order suspense/restore.
    (WebCore::DOMTimer::DOMTimer): Ditto.
    (WebCore::DOMTimer::suspend): Ditto.
    (WebCore::DOMTimer::resume): Ditto.
    * page/DOMTimer.h: Ditto.
    
    LayoutTests: Timers from cached pages fire instantly rather than the specified timeout delay
    https://bugs.webkit.org/show_bug.cgi?id=28683
    
    Reviewed by Brady Eidson.
    
    Added test to verify the fix - it uses page cache and measures the timer callback fire time.
    
    * fast/dom/Window/timer-resume-on-navigation-back-expected.txt: Added.
    * fast/dom/Window/timer-resume-on-navigation-back.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47772 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/DOMTimer.cpp
+++ third_party/WebKit/WebCore/page/DOMTimer.cpp
@@ -48,6 +48,9 @@ DOMTimer::DOMTimer(ScriptExecutionContext* context, ScheduledAction* action, int
     , m_action(action)
     , m_nextFireInterval(0)
     , m_repeatInterval(0)
+#if !ASSERT_DISABLED
+    , m_suspended(false)
+#endif
 {
     static int lastUsedTimeoutId = 0;
     ++lastUsedTimeoutId;
@@ -149,7 +152,10 @@ void DOMTimer::stop()
 
 void DOMTimer::suspend()
 {
-    ASSERT(!m_nextFireInterval && !m_repeatInterval);
+#if !ASSERT_DISABLED
+    ASSERT(!m_suspended);
+    m_suspended = true;
+#endif
     m_nextFireInterval = nextFireInterval();
     m_repeatInterval = repeatInterval();
     TimerBase::stop();
@@ -157,9 +163,11 @@ void DOMTimer::suspend()
 
 void DOMTimer::resume()
 {
+#if !ASSERT_DISABLED
+    ASSERT(m_suspended);
+    m_suspended = false;
+#endif
     start(m_nextFireInterval, m_repeatInterval);
-    m_nextFireInterval = 0;
-    m_repeatInterval = 0;
 }
 
 
