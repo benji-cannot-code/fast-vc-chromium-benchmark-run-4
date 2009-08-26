4dc17cd768a5b9644664b840fc492e093e09c532
rafaelw@google.com <rafaelw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-26 05:01:44 +0000

    Revert "Linux Extensions Install Prompt"
    This reverts 24426
    
    TBR=yutak
    
    Review URL: http://codereview.chromium.org/173460
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24429 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_ui.cc
+++ chrome/browser/extensions/extension_install_ui.cc
@@ -47,7 +47,7 @@ void ExtensionInstallUI::ConfirmInstall(Delegate* delegate,
     return;
   }
 
-#if defined(OS_WIN) || defined(OS_LINUX)
+#if defined(OS_WIN)
   ShowExtensionInstallPrompt(profile_, delegate, extension, install_icon);
 
 #elif defined(OS_MACOSX)
