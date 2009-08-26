63f1003f2c5ea79b2b31f57d5cbb1a3d8a12f33a
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-26 22:32:04 +0000

    AutomationProvider: Implement GetBrowserForWindow() on mac and linux views.
    Review URL: http://codereview.chromium.org/174505
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24547 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/temp_scaffolding_stubs.cc
+++ chrome/common/temp_scaffolding_stubs.cc
@@ -109,13 +109,6 @@ void AutomationProvider::GetFocusedViewID(int handle, int* view_id) {
   NOTIMPLEMENTED();
 }
 
-void AutomationProvider::GetBrowserForWindow(int window_handle,
-                                             bool* success,
-                                             int* browser_handle) {
-  *success = false;
-  NOTIMPLEMENTED();
-}
-
 void AutomationProvider::PrintAsync(int tab_handle) {
   NOTIMPLEMENTED();
 }
