2e3540a0f5e6fdaa5ec95f782f552a41f1008f10
gman@google.com <gman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-27 05:07:06 +0000

    bug fix
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24592 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/samples/o3djs/simple.js
+++ o3d/samples/o3djs/simple.js
@@ -584,7 +584,7 @@ o3djs.simple.SimpleObject.prototype.init = function(simpleInfo, transform) {
    * The Id for this SimpleInfo.
    * @type {number}
    */
-  this.id = simpleInfo.getId();
+  this.id = simpleInfo.getNextId();
 
   /**
    * The Transform that orients this object.
