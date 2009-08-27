f4ae0933b4af8eca0d6329d17fe7bf8077a92f27
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-27 15:06:03 +0000

    Draw background of Linux extension toolstrips.
    This makes Linux extension shelf quite pretty now IMHO.
    
    TEST=Just see it.
    http://crbug.com/16759
    
    Review URL: http://codereview.chromium.org/174585
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24605 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/extension_shelf_gtk.h
+++ chrome/browser/gtk/extension_shelf_gtk.h
@@ -58,6 +58,9 @@ class ExtensionShelfGtk : public ExtensionShelfModelObserver,
   // Create the contents of the extension shelf.
   void Init(Profile* profile);
 
+  // Lazily initialize background bitmap. Can be called many times.
+  void InitBackground();
+
   // Determines what is our target height and sets it.
   void AdjustHeight();
 
@@ -80,6 +83,9 @@ class ExtensionShelfGtk : public ExtensionShelfModelObserver,
 
   // Used to position all children.
   GtkWidget* shelf_hbox_;
+
+  // Lazily-initialized background for toolstrips.
+  scoped_ptr<SkBitmap> background_;
 
   GtkThemeProvider* theme_provider_;
 
