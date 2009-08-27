db522b3b08b8afce588fc7baaf1a4654e318c93a
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-27 19:15:31 +0000

    JSON parser accepts trailing comma in array literals https://bugs.webkit.org/show_bug.cgi?id=28779
    Reviewed by Alexey Proskuryakov.
    
    Update parser to correctly fail if there's a trailing comma.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/resources/JSON-parse.js
+++ third_party/WebKit/LayoutTests/fast/js/resources/JSON-parse.js
@@ -90,6 +90,7 @@ function createTests() {
     result.push(function(jsonObject){
         return jsonObject.parse('[1,]');
     });
+    result[result.length - 1].throws = true;
     result.push(function(jsonObject){
         return jsonObject.parse('[1,2]');
     });
