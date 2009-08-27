d0e6ae4fbeb4ff2fbb7d94baa6a4f3fc01eb62ff
pkasting@chromium.org <pkasting@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-27 23:16:32 +0000

    https://bugs.webkit.org/show_bug.cgi?id=28751 Clean up ImageDecoder*.cpp a bit.
    Reviewed by Eric Seidel.
    
    * platform/image-decoders/ImageDecoder.cpp: Put functions in the same
      order as in the header file.  Also, since there's already an anonymous
      namespace in this file, use it to enclose the local helper functions.
    (WebCore::):
    (WebCore::ImageDecoder::upperBoundScaledX):
    (WebCore::ImageDecoder::lowerBoundScaledX):
    (WebCore::ImageDecoder::scaledY):
    * platform/image-decoders/cairo/ImageDecoderCairo.cpp:
    (WebCore::RGBA32Buffer::width): Style fix.
    (WebCore::RGBA32Buffer::height): Style fix.
    * platform/image-decoders/haiku/ImageDecoderHaiku.cpp:
    (WebCore::RGBA32Buffer::copyBitmapData): Ensure m_size is set correctly.
    (WebCore::RGBA32Buffer::asNewNativeImage): Remove unneeded temp.
    (WebCore::RGBA32Buffer::operator=): Simplify.
    * platform/image-decoders/wx/ImageDecoderWx.cpp:
    (WebCore::RGBA32Buffer::asNewNativeImage): Try and collect spaced-out temps under loop comment to show they're all related.  Use size_t for iterating over elements of a Vector.
    (WebCore::RGBA32Buffer::width): Style fix.
    (WebCore::RGBA32Buffer::height): Style fix.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47841 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/image-decoders/haiku/ImageDecoderHaiku.cpp
+++ third_party/WebKit/WebCore/platform/image-decoders/haiku/ImageDecoderHaiku.cpp
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <Bitmap.h>
 
-
 namespace WebCore {
 
 RGBA32Buffer::RGBA32Buffer()
@@ -62,6 +61,7 @@ void RGBA32Buffer::copyBitmapData(const RGBA32Buffer& other)
         return;
 
     m_bytes = other.m_bytes;
+    m_size = other.m_size;
     setHasAlpha(other.m_hasAlpha);
 }
 
@@ -80,11 +80,8 @@ bool RGBA32Buffer::setSize(int newWidth, int newHeight)
 
 NativeImagePtr RGBA32Buffer::asNewNativeImage() const
 {
-    const void* bytes = m_bytes.data();
-
     BBitmap* bmp = new BBitmap(BRect(0, 0, width(), height()), B_RGB32);
-    bmp->SetBits(bytes, m_size.width() * m_size.height(), 0, B_RGB32);
-
+    bmp->SetBits(m_bytes.data(), m_size.width() * m_size.height(), 0, B_RGB32);
     return bmp;
 }
 
@@ -108,9 +105,7 @@ RGBA32Buffer& RGBA32Buffer::operator=(const RGBA32Buffer& other)
     if (this == &other)
         return *this;
 
-    m_bytes = other.m_bytes;
-    m_size = other.m_size;
-    setHasAlpha(other.hasAlpha());
+    copyBitmapData(other);
     setRect(other.rect());
     setStatus(other.status());
     setDuration(other.duration());
