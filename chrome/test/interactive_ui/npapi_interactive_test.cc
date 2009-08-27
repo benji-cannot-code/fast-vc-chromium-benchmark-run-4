d7f44ccc7abb68bea3d26323fd1b74fac707d09f
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-27 05:16:03 +0000

    Fixes a crash caused due to a call to NPP_DestroyStream occuring in the context of NPP_NewStream. The plugin would invoke NPN_Evaluate to display an alert in the context of NewStream. This would cause the didFail IPC to be dispatched which would cause the plugin to invoke another call to NPP_NewStream which would repeat these steps and crash.
    The didFail call from the renderer did not honor the deferred load flag which we set in WebPluginImpl
    prior to dispatching stream IPCs to the plugin. 
    
    Fix is to dispatch the didFail call when we receive an ack from the plugin indicating that it is ready to accept stream data.
    
    This fixes bug http://code.google.com/p/chromium/issues/detail?id=20063
    
    The other change is in WebPluginImpl::TearDownPluginInstance, where we run through the list of resource
    clients and cancel them. We call didFail on these clients here, which occurs anyway through the PluginDestroyed
    code path.
    
    
    Bug=20063
    Test=Convered by interactive UI test.
    
    
    Review URL: http://codereview.chromium.org/174383
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24593 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/interactive_ui/npapi_interactive_test.cc
+++ chrome/test/interactive_ui/npapi_interactive_test.cc
@@ -33,8 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 
 #include "base/file_util.h"
+#include "chrome/browser/net/url_request_mock_http_job.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/automation/tab_proxy.h"
+#include "chrome/test/automation/window_proxy.h"
 #include "chrome/test/ui/npapi_test_helper.h"
 #include "net/base/net_util.h"
 
@@ -76,4 +78,23 @@ TEST_F(NPAPIVisiblePluginTester, SelfDeletePluginInvokeInSynchronousMouseMove) {
                   kTestCompleteCookie, kTestCompleteSuccess,
                   kShortWaitTimeout);
   }
+}
+
+TEST_F(NPAPIVisiblePluginTester, GetURLRequest404Response) {
+  if (UITest::in_process_renderer())
+    return;
+
+  GURL url(URLRequestMockHTTPJob::GetMockUrl(
+      L"npapi/plugin_url_request_404.html"));
+
+  NavigateToURL(url);
+
+  // Wait for the alert dialog and then close it.
+  automation()->WaitForAppModalDialog(action_max_timeout_ms());
+  scoped_refptr<WindowProxy> window(automation()->GetActiveWindow());
+  ASSERT_TRUE(window.get());
+  ASSERT_TRUE(window->SimulateOSKeyPress(VK_ESCAPE, 0));
+
+  WaitForFinish("geturl_404_response", "1", url, kTestCompleteCookie,
+                kTestCompleteSuccess, kShortWaitTimeout);
 }
--- webkit/glue/plugins/test/plugin_test.cc
+++ webkit/glue/plugins/test/plugin_test.cc
@@ -14,6 +14,7 @@ PluginTest::PluginTest(NPP id, NPNetscapeFuncs *host_functions) {
   id_ = id;
   id_->pdata = this;
   host_functions_ = host_functions;
+  test_completed_ = false;
 }
 
 NPError PluginTest::New(uint16 mode, int16 argc, const char* argn[],
@@ -60,6 +61,7 @@ std::string URLEncode(const std::string &sIn) {
 }
 
 void PluginTest::SignalTestCompleted() {
+  test_completed_ = true;
   // To signal test completion, we expect a couple of
   // javascript functions to be defined in the webpage
   // which hosts this plugin:
