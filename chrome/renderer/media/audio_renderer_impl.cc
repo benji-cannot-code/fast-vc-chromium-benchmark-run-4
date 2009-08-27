6cc63e49d7c52b115b28c7f1d6844fb07fd599cc
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-27 00:25:18 +0000

    Fix build bustage r24568
    TBR=scherkus
    
    Review URL: http://codereview.chromium.org/173549
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24570 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/media/audio_renderer_impl.cc
+++ chrome/renderer/media/audio_renderer_impl.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
+#include <math.h>
+
 #include "chrome/common/render_messages.h"
 #include "chrome/renderer/audio_message_filter.h"
 #include "chrome/renderer/media/audio_renderer_impl.h"
