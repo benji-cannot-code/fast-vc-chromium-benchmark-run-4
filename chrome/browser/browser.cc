f06fb93a1c44a631f24c5bb378e8a42df9d6ea61
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-28 22:47:11 +0000

    Gtk confirm form resubmission dialog.
    Refactor so that the dialog is shown by calling BrowserWindow::ShowRepostFormWarningDialog
    
    BUG=19761
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/174294
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24826 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -2060,6 +2060,10 @@ void Browser::ShowPageInfo(Profile* profile,
   window()->ShowPageInfo(profile, url, ssl, show_history);
 }
 
+void Browser::ShowRepostFormWarningDialog(TabContents *tab_contents) {
+  window()->ShowRepostFormWarningDialog(tab_contents);
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 // Browser, SelectFileDialog::Listener implementation:
 
