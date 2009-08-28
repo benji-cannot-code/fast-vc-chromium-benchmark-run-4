e2dfadba4597abe44ac3b93ace5d9f2dc86e566b
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-28 18:33:34 +0000

    Adds a default icon to the install dialog if the extension doesn't specify one.
    This icon came from Crystal Clear [1] and is licensed under
    LGPL.
    
    [1] http://commons.wikimedia.org/wiki/Crystal_Clear
    
    BUG=20510
    
    Review URL: http://codereview.chromium.org/173604
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24754 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_ui.cc
+++ chrome/browser/extensions/extension_install_ui.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "app/l10n_util.h"
+#include "app/resource_bundle.h"
 #include "base/file_util.h"
 #include "base/rand_util.h"
 #include "chrome/browser/browser_list.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/common/extensions/extension.h"
+#include "grit/browser_resources.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 
@@ -103,6 +105,11 @@ void ExtensionInstallUI::ConfirmInstall(Delegate* delegate,
   }
 
 #if defined(OS_WIN) || defined(TOOLKIT_GTK)
+  if (!install_icon) {
+    install_icon = ResourceBundle::GetSharedInstance().GetBitmapNamed(
+        IDR_DEFAULT_EXTENSION_ICON_128);
+  }
+
   ShowExtensionInstallPrompt(profile_, delegate, extension, install_icon,
                              GetInstallWarning(extension));
 
