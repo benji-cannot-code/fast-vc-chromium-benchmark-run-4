435fa3565ced2c8b640a10d297ddeab845ededc1
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-29 20:39:10 +0000

    Silence the libcmt project to only print the processed files.
    TEST=none
    BUG=none
    Review URL: http://codereview.chromium.org/175021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24858 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/tcmalloc/prep_libc.sh
+++ third_party/tcmalloc/prep_libc.sh
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/bin/sh -x
+#!/bin/sh
 # This script takes libcmt.lib for VS2005 and removes the allocation related 
 # functions from it.
 #
@@ -30,6 +30,7 @@ OBJFILES="malloc.obj free.obj realloc.obj new.obj delete.obj new2.obj delete2.ob
 
 for FILE in $OBJFILES
 do
-  LIB /IGNORE:4006,4014,4221 /REMOVE:${LIBCMTSRCPATHVS2005}${FILE} $OUTCMT
-  LIB /IGNORE:4006,4014,4221 /REMOVE:${LIBCMTSRCPATHVS2008}${FILE} $OUTCMT
+  echo ${FILE}
+  LIB /NOLOGO /IGNORE:4006,4014,4221 /REMOVE:${LIBCMTSRCPATHVS2005}${FILE} $OUTCMT
+  LIB /NOLOGO /IGNORE:4006,4014,4221 /REMOVE:${LIBCMTSRCPATHVS2008}${FILE} $OUTCMT
 done
