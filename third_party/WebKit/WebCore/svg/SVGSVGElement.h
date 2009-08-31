8b4efc44c8648a07b6c09e04afbe41016868070c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-31 11:06:26 +0000

    2009-08-31  Cameron McCormack  <cam@mcc.id.au>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=28827
            SVGSVGElement.unsuspendRedraw() shouldn't throw
    
            * svg/SVGSVGElement.cpp:
            (WebCore::SVGSVGElement::unsuspendRedraw):
            * svg/SVGSVGElement.h:
            * svg/SVGSVGElement.idl:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGSVGElement.h
+++ third_party/WebKit/WebCore/svg/SVGSVGElement.h
@@ -96,7 +96,7 @@ namespace WebCore {
         void setCurrentTime(float seconds);
 
         unsigned long suspendRedraw(unsigned long max_wait_milliseconds);
-        void unsuspendRedraw(unsigned long suspend_handle_id, ExceptionCode&);
+        void unsuspendRedraw(unsigned long suspend_handle_id);
         void unsuspendRedrawAll();
         void forceRedraw();
 
