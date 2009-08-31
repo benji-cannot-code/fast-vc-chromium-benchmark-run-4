f7f4eb8874bf0377fc3f05ce026584e538271315
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-08-31 20:15:17 +0000

    2009-08-31  Patrick Mueller  <Patrick_Mueller@us.ibm.com>
            Reviewed by Timothy Hatcher.
    
            Inspector Request Headers Should Show Data Sent With Request
            https://bugs.webkit.org/show_bug.cgi?id=22920
    
            Manual test added - see below.
    
            * English.lproj/localizedStrings.js:
            * inspector/InspectorResource.cpp:
            (WebCore::InspectorResource::updateRequest):
            (WebCore::InspectorResource::createScriptObject):
            (WebCore::InspectorResource::updateScriptObject):
            * inspector/InspectorResource.h:
            * inspector/front-end/Resource.js:
            (WebInspector.Resource):
            * inspector/front-end/ResourceView.js:
            (WebInspector.ResourceView):
            (WebInspector.ResourceView.prototype._refreshURL):
            (WebInspector.ResourceView.prototype._refreshQueryString):
            (WebInspector.ResourceView.prototype._refreshFormData):
            (WebInspector.ResourceView.prototype._refreshRequestPayload):
            (WebInspector.ResourceView.prototype._refreshParms):
            (WebInspector.ResourceView.prototype._toggleURLdecoding):
            (WebInspector.ResourceView.prototype._getHeaderValue):
            (WebInspector.ResourceView.prototype._refreshRequestHeaders):
            * inspector/front-end/inspector.css:
            * inspector/front-end/inspector.js:
            (WebInspector.addResource):
            (WebInspector.updateResource):
            * manual-tests/inspector/display-form-data.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47903 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/Resource.js
+++ third_party/WebKit/WebCore/inspector/front-end/Resource.js
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-WebInspector.Resource = function(requestHeaders, url, domain, path, lastPathComponent, identifier, mainResource, cached)
+WebInspector.Resource = function(requestHeaders, url, domain, path, lastPathComponent, identifier, mainResource, cached, requestMethod, requestFormData)
 {
     this.identifier = identifier;
 
@@ -40,6 +40,8 @@ WebInspector.Resource = function(requestHeaders, url, domain, path, lastPathComp
     this.path = path;
     this.lastPathComponent = lastPathComponent;
     this.cached = cached;
+    this.requestMethod = requestMethod || "";
+    this.requestFormData = requestFormData || "";
 
     this.category = WebInspector.resourceCategories.other;
 }
