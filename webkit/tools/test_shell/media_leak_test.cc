6f1f8fac74751571bd480fb0e35126d03f5d0ceb
mmoss@chromium.org <mmoss@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-31 18:58:19 +0000

    Linux 64-bit: Fix failing unit_tests and ignore non-working plugin/media tests.
    Related bugs:
    http://crbug.com/18337
    http://crbug.com/20467
    
    Review URL: http://codereview.chromium.org/175019
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24913 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/media_leak_test.cc
+++ webkit/tools/test_shell/media_leak_test.cc
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class MediaLeakTest : public TestShellTest {
 };
 
-#if defined(OS_WIN) || defined(OS_LINUX)
+// Ignore these until 64-bit ffmpeg libs are available. http://crbug.com/20467
+#if (defined(OS_WIN) || defined(OS_LINUX)) && !defined(ARCH_CPU_64_BITS)
 
 // This test plays a Theora video file for 1 second. It tries to expose
 // memory leaks during a normal playback.
