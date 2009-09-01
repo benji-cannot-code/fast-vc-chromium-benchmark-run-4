8712e010c538bd74df797f068afd3e03bc1baa07
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-01 13:40:24 +0000

    (Mac) Make mashing the close tab button work.
    To do this, I had to commit several crimes against humanity. In
    particular, Cocoa doesn't generate the required extra hit tests during
    animations, so we have to. Sometimes, it gets really messed up and ends
    up hitting the "drag blocking view". Moreover, we have to account for
    the possibility of the mouse down hitting a moving tab, and going up on
    the close button, etc.
    
    BUG=17720
    TEST=Mash the close tabs button under a wide variety of situations. \
         Also make sure that the handling of the tabs (dragging, etc.)  \
         hasn't accidentally been messed up.
    
    Review URL: http://codereview.chromium.org/174461
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25029 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_controller.mm
+++ chrome/browser/cocoa/tab_controller.mm
@@ -183,4 +183,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [titleView_ setTextColor:color ? color : [NSColor textColor]];
   [[self view] setNeedsDisplay:YES];
 }
+
+// Called by the tabs to determine whether we are in rapid (tab) closure mode.
+- (BOOL)inRapidClosureMode {
+  if ([[self target] respondsToSelector:@selector(inRapidClosureMode)]) {
+    return [[self target] performSelector:@selector(inRapidClosureMode)] ?
+        YES : NO;
+  }
+  return NO;
+}
+
 @end
