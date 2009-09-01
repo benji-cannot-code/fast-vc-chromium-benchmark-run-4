02275729b67d3d70b376d4a4642315fe7d1a7445
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-01 19:41:12 +0000

    Squash the bookmark bar into the toolbar by 4 pixels.
    On windows they do this by having the two views overlap. We can't so instead move the bottom 4 pixels of padding from the toolbar widget into the bookmark bar widget. When the bookmark bar is closed/hidden, it still shows those 4 pixels. When it opens, it is able to make use of those 4 pixels. Since we get 4 extra pixels, we can make the overall height of the bookmark bar 4 pixels less.
    
    BUG=18570
    
    Review URL: http://codereview.chromium.org/178029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25075 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_toolbar_gtk.h
+++ chrome/browser/gtk/browser_toolbar_gtk.h
@@ -47,9 +47,6 @@ class BrowserToolbarGtk : public CommandUpdater::CommandObserver,
   // Set the various widgets' ViewIDs.
   void SetViewIDs();
 
-  // Adds this GTK toolbar into a sizing box.
-  void AddToolbarToBox(GtkWidget* box);
-
   void Show();
   void Hide();
 
@@ -120,8 +117,9 @@ class BrowserToolbarGtk : public CommandUpdater::CommandObserver,
   void ChangeActiveMenu(GtkWidget* active_menu, guint timestamp);
 
   // Gtk callback for the "expose-event" signal.
-  static gboolean OnToolbarExpose(GtkWidget* widget, GdkEventExpose* e,
-                                  BrowserToolbarGtk* toolbar);
+  // The alignment contains the toolbar.
+  static gboolean OnAlignmentExpose(GtkWidget* widget, GdkEventExpose* e,
+                                    BrowserToolbarGtk* toolbar);
   static gboolean OnLocationHboxExpose(GtkWidget* omnibox_hbox,
                                        GdkEventExpose* e,
                                        BrowserToolbarGtk* toolbar);
