863483ed60dfd7d3fb00ad549293f0b46d3576a5
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-01 23:48:28 +0000

    Revert accidental commits: r25115 and r25116
    Review URL: http://codereview.chromium.org/183045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25120 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_host_delegate_helper.h
+++ chrome/browser/tab_contents/render_view_host_delegate_helper.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/gfx/rect.h"
 #include "base/waitable_event.h"
-#include "chrome/browser/dom_ui/dom_ui_factory.h"
 #include "webkit/glue/webpreferences.h"
 #include "webkit/glue/window_open_disposition.h"
 
@@ -33,8 +32,7 @@ class RenderViewHostDelegateViewHelper {
 
   virtual void CreateNewWindow(int route_id,
                                base::WaitableEvent* modal_dialog_event,
-                               Profile* profile, SiteInstance* site,
-                               DOMUITypeID domui_type);
+                               Profile* profile, SiteInstance* site);
   virtual RenderWidgetHostView* CreateNewWidget(int route_id, bool activatable,
                                                 RenderProcessHost* process);
   virtual TabContents* GetCreatedWindow(int route_id);
