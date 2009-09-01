615c6545a882307ca7dd4ab7d3f1bc3a5b831d7a
dank@chromium.org <dank@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-01 19:05:18 +0000

    Bundle a gdb that doesn't crash during line number lookup when presented with Chrome built with -O1. (The gdb lives in /usr/local/valgrind-10771/bin, so should not interfere with the system gdb.)
    Review URL: http://codereview.chromium.org/178042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25069 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/build-valgrind-for-chromium.sh
+++ tools/valgrind/build-valgrind-for-chromium.sh
@@ -46,6 +46,7 @@ fi
 
 # Clean checkout our untar
 rm -rf "valgrind-${VALGRIND_SVN_REV}"
+mkdir -p "valgrind-${VALGRIND_SVN_REV}"
 
 if test "x${USE_TARBALL}" != "xyes"
 then
@@ -81,7 +82,15 @@ then
   fi
 
   sh autogen.sh
+
+  # MacOSX before Snow Leopoard needs newer gdb to be able to handle -O1 chrome
+  # Kludgily download and unpack the sources in a subdirectory.
+  if test `uname` = Darwin || test "x${MAKE_TARBALL}" = "xyes"
+  then
+    curl http://www.opensource.apple.com/tarballs/gdb/gdb-1344.tar.gz | tar -xzf -
+  fi
   cd ..
+
 fi
 
 if test "x${MAKE_TARBALL}" = "xyes"
@@ -144,4 +153,19 @@ then
   else
      sudo make install
   fi
+
+  case `uname` in
+  Darwin)
+    cd gdb-1344/src
+    ./configure --prefix="${PREFIX}"
+    make -j4
+    if test -w "${parent_of_prefix}"
+    then
+       make install
+    else
+       sudo make install
+    fi
+    ;;
+  esac
+  cd ..
 fi
