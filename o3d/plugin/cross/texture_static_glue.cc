a0487665260e579876b7fa879cbaa11615fbdf8f
gman@google.com <gman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-01 02:59:36 +0000

    Update Texture::Lock to take an AccessMode.
    Also, split Lock/Unlock into common and platform
    specific parts.
    
    This is needed to work around an apparent bug in
    mac drivers where glGetTexImage apparently doesn't
    always work.
    
    This doesn't solve the issue unless we disallow
    READ completely. Not sure what to about that
    except make a small GL sample and report the 
    bug to Apple.
    
    Review URL: http://codereview.chromium.org/173640
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25012 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/plugin/cross/texture_static_glue.cc
+++ o3d/plugin/cross/texture_static_glue.cc
@@ -235,7 +235,7 @@ void SetRectCheck2D(o3d::Texture2D* self,
       return;
     }
   }
-  o3d::Texture2D::LockHelper helper(self, level);
+  o3d::Texture2D::LockHelper helper(self, level, o3d::Texture::kWriteOnly);
   void* data = helper.GetData();
   if (!data) {
     O3D_ERROR(self->service_locator()) << "could not lock texture";
@@ -311,7 +311,8 @@ void SetRectCheckCUBE(o3d::TextureCUBE* self,
       return;
     }
   }
-  o3d::TextureCUBE::LockHelper helper(self, face, level);
+  o3d::TextureCUBE::LockHelper helper(
+      self, face, level, o3d::Texture::kWriteOnly);
   void* data = helper.GetData();
   if (!data) {
     O3D_ERROR(self->service_locator()) << "could not lock texture";
@@ -495,7 +496,7 @@ std::vector<float> userglue_method_GetRect(o3d::Texture2D* self,
         << "Texture::Set not supported for this type of texture";
       return empty;
   }
-  o3d::Texture2D::LockHelper helper(self, level);
+  o3d::Texture2D::LockHelper helper(self, level, o3d::Texture::kReadOnly);
   void* data = helper.GetData();
   if (!data) {
     O3D_ERROR(self->service_locator()) << "could not lock texture";
@@ -581,7 +582,8 @@ std::vector<float> userglue_method_GetRect(o3d::TextureCUBE* self,
         << "Texture::Set not supported for this type of texture";
       return empty;
   }
-  o3d::TextureCUBE::LockHelper helper(self, face, level);
+  o3d::TextureCUBE::LockHelper helper(
+      self, face, level, o3d::Texture::kReadOnly);
   void* data = helper.GetData();
   if (!data) {
     O3D_ERROR(self->service_locator()) << "could not lock texture";
