9f7d18a4a4a13a6da8f402526374162045aa72fa
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-01 23:13:15 +0000

    Add skeleton for an about:net-internal page.
    This page contains low-level debug information that is of interest to power users, and bug investigators.
    
    The output can be filtered by using special sub-URLs.
    The sub-URLs themselves are listed when visiting about:net-internal, but to give an idea:
    
      about:net-internal -- everything
      about:net-internal/proxyservice -- all things proxy
      about:net-internal/proxyservice.config -- current settings
      about:net-internal/hostresolver.hostcache -- dns cache
      
    BUG=http://crbug.com/14478
    R=darin
    Review URL: http://codereview.chromium.org/173024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25109 0039d316-1c4b-4281-b951-d872f2087c98

--- net/net.gyp
+++ net/net.gyp
@@ -358,6 +358,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'url_request/url_request_test_job.h',
         'url_request/url_request_view_cache_job.cc',
         'url_request/url_request_view_cache_job.h',
+        'url_request/url_request_view_net_internal_job.cc',
+        'url_request/url_request_view_net_internal_job.h',
       ],
       'sources!': [
         'build/precompiled_net.h',
--- net/url_request/url_request_job_manager.cc
+++ net/url_request/url_request_job_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include "net/url_request/url_request_http_job.h"
 #include "net/url_request/url_request_view_cache_job.h"
+#include "net/url_request/url_request_view_net_internal_job.h"
 
 // The built-in set of protocol factories
 namespace {
@@ -43,6 +44,7 @@ static const SchemeToFactory kBuiltinFactories[] = {
 #endif
   { "about", URLRequestAboutJob::Factory },
   { "view-cache", URLRequestViewCacheJob::Factory },
+  { "view-net-internal", URLRequestViewNetInternalJob::Factory },
 };
 
 URLRequestJobManager::URLRequestJobManager() {
