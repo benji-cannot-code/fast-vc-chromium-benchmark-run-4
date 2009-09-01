24b909b6dc3e81c634793ff7c678999d1b6723d8
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-01 10:05:33 +0000

    2009-09-01  Vitaly Repeshko  <vitalyr@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            [V8] Inlined fast paths of V8DOMWrapper::convertNodeToV8Object and
            V8Proxy::createWrapperFromCache.
            https://bugs.webkit.org/show_bug.cgi?id=28848
    
            * bindings/v8/V8DOMWrapper.cpp:
            (WebCore::V8DOMWrapper::convertDocumentToV8Object): Special
            function for document wrapping.
            (WebCore::V8DOMWrapper::convertNewNodeToV8Object): Slow case of
            wrapping a new node.
            * bindings/v8/V8DOMWrapper.h:
            (WebCore::V8DOMWrapper::convertNodeToV8Object): Extracted fast
            path from V8DOMWrapper.cpp.
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::createWrapperFromCacheSlowCase):
            * bindings/v8/V8Proxy.h:
            (WebCore::V8Proxy::createWrapperFromCache): Extracted fast path
            from V8Proxy.cpp.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47921 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8DOMWrapper.h
+++ third_party/WebKit/WebCore/bindings/v8/V8DOMWrapper.h
@@ -32,11 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef V8DOMWrapper_h
 #define V8DOMWrapper_h
 
+#include "Document.h"
 #include "Event.h"
 #include "Node.h"
 #include "NodeFilter.h"
 #include "PlatformString.h" // for WebCore::String
 #include "V8CustomBinding.h"
+#include "V8DOMMap.h"
 #include "V8Index.h"
 #include "V8Utilities.h"
 #include <v8.h>
@@ -135,7 +137,26 @@ namespace WebCore {
             return convertNodeToV8Object(node.get());
         }
 
-        static v8::Handle<v8::Value> convertNodeToV8Object(Node*);
+        static v8::Handle<v8::Value> convertNodeToV8Object(Node* node)
+        {
+            if (!node)
+                return v8::Null();
+
+            Document* document = node->document();
+            if (node == document)
+                return convertDocumentToV8Object(document);
+
+            DOMWrapperMap<Node>& domNodeMap = getDOMNodeMap();
+            v8::Handle<v8::Object> wrapper = domNodeMap.get(node);
+            if (wrapper.IsEmpty())
+                return convertNewNodeToV8Object(node, 0, domNodeMap);
+
+            return wrapper;
+        }
+
+        static v8::Handle<v8::Value> convertDocumentToV8Object(Document*);
+
+        static v8::Handle<v8::Value> convertNewNodeToV8Object(Node*, V8Proxy*, DOMWrapperMap<Node>&);
 
         template <class C>
         static C* convertToNativeObject(V8ClassIndex::V8WrapperType type, v8::Handle<v8::Object> object)
