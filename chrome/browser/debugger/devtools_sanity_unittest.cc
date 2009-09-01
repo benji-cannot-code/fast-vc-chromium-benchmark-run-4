2e3fafeb68b3a26efb3e4198bde29caa13863895
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-01 23:32:23 +0000

    Disable two DevToolsSanityTests. They started failing after http://trac.webkit.org/changeset/47944 came downstream.
    TBR=pfeldman
    BUG=20835
    TEST=none
    
    Review URL: http://codereview.chromium.org/180073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25114 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_sanity_unittest.cc
+++ chrome/browser/debugger/devtools_sanity_unittest.cc
@@ -168,7 +168,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, DISABLED_TestPauseInEval) {
 }
 
 // Tests console eval.
-IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestConsoleEval) {
+IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, DISABLED_TestConsoleEval) {
   RunTest("testConsoleEval", kConsoleTestPage);
 }
 
@@ -178,7 +178,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestConsoleLog) {
 }
 
 // Tests eval global values.
-IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestEvalGlobal) {
+IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, DISABLED_TestEvalGlobal) {
   RunTest("testEvalGlobal", kEvalTestPage);
 }
 
