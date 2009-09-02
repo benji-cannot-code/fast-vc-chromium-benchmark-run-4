461febb54c67b6ed46da5a60988a28fff3b87a4e
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-02 21:31:34 +0000

    2009-09-02  Dimitri Glazkov  <dglazkov@chromium.org>
            Unreviewed, build fix.
    
            [Chromium] Add a missing include to fix bustage, caused by r47991.
            https://bugs.webkit.org/show_bug.cgi?id=27933
    
            * platform/graphics/skia/ImageBufferSkia.cpp: Added SkColorPriv include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47995 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/skia/ImageBufferSkia.cpp
+++ third_party/WebKit/WebCore/platform/graphics/skia/ImageBufferSkia.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ImageData.h"
 #include "PlatformContextSkia.h"
 #include "PNGImageEncoder.h"
+#include "SkColorPriv.h"
 #include "SkiaUtils.h"
 
 using namespace std;
