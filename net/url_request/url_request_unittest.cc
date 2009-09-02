8e6b3ca269542cb4a6353b4d629eacf4cd0fad13
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-02 07:19:03 +0000

    Keep track of the live URLRequest instances, and the last 25 that were destroyed.
    This functionality will be used by the "about:net-internal" page to display profiling information for the in-progress requests, as well as the recently completed requests.
    
    This does not have any performance impact. Note that all of the tracking operations are constant time.
    In particular:
    
     * Global tracking of all URLRequest instances is done by chaining them as
       linked list nodes. So we get constant-time insertion/deletion without
       needing to do any extra heap allocs.
    
     * The recent requests list is a circular queue, backed by an array. So
       insertions are constant time (and we never erase entries, just overwrite).
       Moreover, the entry types themselves are comprised of {GURL, LoadLog*},
       so very little copying actually happens -- LoadLog is refcounted so
       copy is cheap, and GURL is backed by a std::string which is also refcounted
       so the copy is cheap.
    
    R=darin
    BUG=http://crbug.com/14478
    TEST=unittests.
    
    Review URL: http://codereview.chromium.org/173175
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25158 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -118,6 +118,10 @@ scoped_refptr<net::UploadData> CreateSimpleUploadData(const char* data) {
 
 // Inherit PlatformTest since we require the autorelease pool on Mac OS X.f
 class URLRequestTest : public PlatformTest {
+ public:
+  ~URLRequestTest() {
+    EXPECT_EQ(0u, URLRequest::InstanceTracker::Get()->GetLiveRequests().size());
+  }
 };
 
 class URLRequestTestHTTP : public URLRequestTest {
@@ -200,9 +204,6 @@ TEST_F(URLRequestTestHTTP, GetTest_NoCache) {
     EXPECT_FALSE(d.received_data_before_response());
     EXPECT_NE(0, d.bytes_received());
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTestHTTP, GetTest) {
@@ -220,9 +221,86 @@ TEST_F(URLRequestTestHTTP, GetTest) {
     EXPECT_FALSE(d.received_data_before_response());
     EXPECT_NE(0, d.bytes_received());
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
+}
+
+// Test the instance tracking functionality of URLRequest.
+TEST_F(URLRequestTest, Tracking) {
+  URLRequest::InstanceTracker::Get()->ClearRecentlyDeceased();
+  EXPECT_EQ(0u, URLRequest::InstanceTracker::Get()->GetLiveRequests().size());
+  EXPECT_EQ(0u,
+            URLRequest::InstanceTracker::Get()->GetRecentlyDeceased().size());
+
+  {
+    URLRequest req1(GURL("http://req1"), NULL);
+    URLRequest req2(GURL("http://req2"), NULL);
+    URLRequest req3(GURL("http://req3"), NULL);
+
+    std::vector<URLRequest*> live_reqs =
+        URLRequest::InstanceTracker::Get()->GetLiveRequests();
+    ASSERT_EQ(3u, live_reqs.size());
+    EXPECT_EQ(GURL("http://req1"), live_reqs[0]->original_url());
+    EXPECT_EQ(GURL("http://req2"), live_reqs[1]->original_url());
+    EXPECT_EQ(GURL("http://req3"), live_reqs[2]->original_url());
+  }
+
+  EXPECT_EQ(0u, URLRequest::InstanceTracker::Get()->GetLiveRequests().size());
+
+  URLRequest::InstanceTracker::RecentRequestInfoList recent_reqs =
+      URLRequest::InstanceTracker::Get()->GetRecentlyDeceased();
+
+  // Note that the order is reversed from definition order, because
+  // this matches the destructor order.
+  ASSERT_EQ(3u, recent_reqs.size());
+  EXPECT_EQ(GURL("http://req3"), recent_reqs[0].original_url);
+  EXPECT_EQ(GURL("http://req2"), recent_reqs[1].original_url);
+  EXPECT_EQ(GURL("http://req1"), recent_reqs[2].original_url);
+}
+
+// Test the instance tracking functionality of URLRequest.
+TEST_F(URLRequestTest, TrackingGraveyardBounded) {
+  URLRequest::InstanceTracker::Get()->ClearRecentlyDeceased();
+  EXPECT_EQ(0u, URLRequest::InstanceTracker::Get()->GetLiveRequests().size());
+  EXPECT_EQ(0u, URLRequest::InstanceTracker::Get()->GetLiveRequests().size());
+
+  const size_t kMaxGraveyardSize =
+      URLRequest::InstanceTracker::kMaxGraveyardSize;
+  const size_t kMaxURLLen = URLRequest::InstanceTracker::kMaxGraveyardURLSize;
+
+  // Add twice as many requests as will fit in the graveyard.
+  for (size_t i = 0; i < kMaxGraveyardSize * 2; ++i)
+    URLRequest req(GURL(StringPrintf("http://req%d", i).c_str()), NULL);
+
+  // Check that only the last |kMaxGraveyardSize| requests are in-memory.
+
+  URLRequest::InstanceTracker::RecentRequestInfoList recent_reqs =
+      URLRequest::InstanceTracker::Get()->GetRecentlyDeceased();
+
+  ASSERT_EQ(kMaxGraveyardSize, recent_reqs.size());
+
+  for (size_t i = 0; i < kMaxGraveyardSize; ++i) {
+    size_t req_number = i + kMaxGraveyardSize;
+    GURL url(StringPrintf("http://req%d", req_number).c_str());
+    EXPECT_EQ(url, recent_reqs[i].original_url);
+  }
+
+  URLRequest::InstanceTracker::Get()->ClearRecentlyDeceased();
+  EXPECT_EQ(0u,
+            URLRequest::InstanceTracker::Get()->GetRecentlyDeceased().size());
+
+  // Check that very long URLs are truncated.
+  std::string big_url_spec("http://");
+  big_url_spec.resize(2 * kMaxURLLen, 'x');
+  GURL big_url(big_url_spec);
+  {
+    URLRequest req(big_url, NULL);
+  }
+  ASSERT_EQ(1u,
+            URLRequest::InstanceTracker::Get()->GetRecentlyDeceased().size());
+  // The +1 is because GURL canonicalizes with a trailing '/' ... maybe
+  // we should just save the std::string rather than the GURL.
+  EXPECT_EQ(kMaxURLLen + 1,
+            URLRequest::InstanceTracker::Get()->GetRecentlyDeceased()[0]
+                .original_url.spec().size());
 }
 
 TEST_F(URLRequestTestHTTP, SetExplicitlyAllowedPortsTest) {
@@ -249,10 +327,6 @@ TEST_F(URLRequestTest, QuitTest) {
   ASSERT_TRUE(NULL != server.get());
   server->SendQuit();
   EXPECT_TRUE(server->WaitToFinish(20000));
-
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 class HTTPSRequestTest : public testing::Test {
@@ -315,9 +389,6 @@ TEST_F(HTTPSRequestTest, MAYBE_HTTPSGetTest) {
     EXPECT_FALSE(d.received_data_before_response());
     EXPECT_NE(0, d.bytes_received());
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(HTTPSRequestTest, MAYBE_HTTPSMismatchedTest) {
@@ -396,9 +467,6 @@ TEST_F(URLRequestTestHTTP, CancelTest) {
     EXPECT_EQ(0, d.bytes_received());
     EXPECT_FALSE(d.received_data_before_response());
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTestHTTP, CancelTest2) {
@@ -423,9 +491,6 @@ TEST_F(URLRequestTestHTTP, CancelTest2) {
     EXPECT_FALSE(d.received_data_before_response());
     EXPECT_EQ(URLRequestStatus::CANCELED, r.status().status());
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTestHTTP, CancelTest3) {
@@ -449,9 +514,6 @@ TEST_F(URLRequestTestHTTP, CancelTest3) {
     EXPECT_FALSE(d.received_data_before_response());
     EXPECT_EQ(URLRequestStatus::CANCELED, r.status().status());
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTestHTTP, CancelTest4) {
@@ -506,10 +568,6 @@ TEST_F(URLRequestTestHTTP, CancelTest5) {
     EXPECT_EQ(0, d.bytes_received());
     EXPECT_FALSE(d.received_data_before_response());
   }
-
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTestHTTP, PostTest) {
@@ -555,9 +613,6 @@ TEST_F(URLRequestTestHTTP, PostTest) {
     EXPECT_EQ(d.data_received().compare(uploadBytes), 0);
   }
   delete[] uploadBytes;
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTestHTTP, PostEmptyTest) {
@@ -578,9 +633,6 @@ TEST_F(URLRequestTestHTTP, PostEmptyTest) {
     EXPECT_FALSE(d.received_data_before_response());
     EXPECT_TRUE(d.data_received().empty());
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTestHTTP, PostFileTest) {
@@ -628,9 +680,6 @@ TEST_F(URLRequestTestHTTP, PostFileTest) {
     ASSERT_EQ(size, d.bytes_received());
     EXPECT_EQ(0, memcmp(d.data_received().c_str(), buf.get(), size));
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTest, AboutBlankTest) {
@@ -647,9 +696,6 @@ TEST_F(URLRequestTest, AboutBlankTest) {
     EXPECT_FALSE(d.received_data_before_response());
     EXPECT_EQ(d.bytes_received(), 0);
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTest, FileTest) {
@@ -674,9 +720,6 @@ TEST_F(URLRequestTest, FileTest) {
     EXPECT_FALSE(d.received_data_before_response());
     EXPECT_EQ(d.bytes_received(), static_cast<int>(file_size));
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTest, FileTestFullSpecifiedRange) {
@@ -718,9 +761,6 @@ TEST_F(URLRequestTest, FileTestFullSpecifiedRange) {
   }
 
   EXPECT_TRUE(file_util::Delete(temp_path, false));
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTest, FileTestHalfSpecifiedRange) {
@@ -761,9 +801,6 @@ TEST_F(URLRequestTest, FileTestHalfSpecifiedRange) {
   }
 
   EXPECT_TRUE(file_util::Delete(temp_path, false));
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTest, FileTestMultipleRanges) {
@@ -792,9 +829,6 @@ TEST_F(URLRequestTest, FileTestMultipleRanges) {
   }
 
   EXPECT_TRUE(file_util::Delete(temp_path, false));
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTest, InvalidUrlTest) {
@@ -808,9 +842,6 @@ TEST_F(URLRequestTest, InvalidUrlTest) {
     MessageLoop::current()->Run();
     EXPECT_TRUE(d.request_failed());
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 // This test is disabled because it fails on some computers due to proxies
@@ -826,9 +857,6 @@ TEST_F(URLRequestTest, DISABLED_DnsFailureTest) {
     MessageLoop::current()->Run();
     EXPECT_TRUE(d.request_failed());
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 
 TEST_F(URLRequestTestHTTP, ResponseHeadersTest) {
@@ -928,11 +956,11 @@ TEST_F(URLRequestTest, ResolveShortcutTest) {
   CoInitialize(NULL);
   // Temporarily create a shortcut for test
   result = CoCreateInstance(CLSID_ShellLink, NULL,
-                          CLSCTX_INPROC_SERVER, IID_IShellLink,
-                          reinterpret_cast<LPVOID*>(&shell));
+                            CLSCTX_INPROC_SERVER, IID_IShellLink,
+                            reinterpret_cast<LPVOID*>(&shell));
   ASSERT_TRUE(SUCCEEDED(result));
   result = shell->QueryInterface(IID_IPersistFile,
-                             reinterpret_cast<LPVOID*>(&persist));
+                                reinterpret_cast<LPVOID*>(&persist));
   ASSERT_TRUE(SUCCEEDED(result));
   result = shell->SetPath(app_path.value().c_str());
   EXPECT_TRUE(SUCCEEDED(result));
@@ -977,10 +1005,6 @@ TEST_F(URLRequestTest, ResolveShortcutTest) {
   // Clean the shortcut
   DeleteFile(lnk_path.c_str());
   CoUninitialize();
-
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 }
 #endif  // defined(OS_WIN)
 
@@ -1022,9 +1046,6 @@ TEST_F(URLRequestTest, FileDirCancelTest) {
 
     MessageLoop::current()->Run();
   }
-#ifndef NDEBUG
-  DCHECK_EQ(url_request_metrics.object_count, 0);
-#endif
 
   // Take out mock resource provider.
   net::NetModule::SetResourceProvider(NULL);
