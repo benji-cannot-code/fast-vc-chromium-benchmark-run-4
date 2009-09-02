2c52ad632490c4290aa51c42243fa7ea137022a2
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-02 09:49:31 +0000

    2009-09-02  Norbert Leser  <norbert.leser@nokia.com>
            Reviewed by Eric Seidel.
    
            Use fastMalloc when neither MMAP nor VIRTUALALLOC are enabled
    
            RegisterFile constructor currently throws #error when both
            MMAP and VIRTUALALLOC conditions fail.
            On any platform that does not provide these features
            (for instance, Symbian),
            the fallback should be regular malloc (or fastMalloc).
            It is functionally equivalent in this case, even though it may
            have certain drawbacks such as lack of dynamic pre-allocation.
    
            * interpreter/RegisterFile.cpp:
            (JSC::RegisterFile::~RegisterFile):
            * interpreter/RegisterFile.h:
            (JSC::RegisterFile::RegisterFile):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@47959 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/interpreter/RegisterFile.cpp
+++ third_party/WebKit/JavaScriptCore/interpreter/RegisterFile.cpp
@@ -39,7 +39,7 @@ RegisterFile::~RegisterFile()
 #elif HAVE(VIRTUALALLOC)
     VirtualFree(m_buffer, 0, MEM_RELEASE);
 #else
-    #error "Don't know how to release virtual memory on this platform."
+    fastFree(m_buffer);
 #endif
 }
 
