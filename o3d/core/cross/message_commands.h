44b8897c776d2dbcd0d2fa8f2fb7e38137702a1d
vangelis@google.com <vangelis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-02 22:02:54 +0000

    Fixing mac and linux build Review URL: http://codereview.chromium.org/186020
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25247 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/message_commands.h
+++ o3d/core/cross/message_commands.h
@@ -40,6 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace o3d {
 
+// Max length of the string storing the version of O3D as returned by the
+// GET_VERSION message
+#define MAX_VERSION_STRING_LENGTH 128
+
 // Make sure the compiler does not add extra padding to any of the message
 // structures.
 O3D_PUSH_STRUCTURE_PACKING_1;
@@ -366,11 +370,10 @@ struct MessageGetVersion {
 
   // The response data.
   struct ResponseData {
-    static const size_t kMaxVersionLength = 128;
     // a null terminated version string in the format "x.x.x.x" where x is an
     // integer number. Note: There may be other data after the last digit which
     // is currently undefined.
-    char version[kMaxVersionLength];
+    char version[MAX_VERSION_STRING_LENGTH];
   };
 
   // A wrapper to manage the response data.
