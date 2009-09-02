3b2b76361635ca32ffb419e3201fe89e7a8c95bc
paul@chromium.org <paul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-02 23:19:54 +0000

    Exclude specific browser_tests that don't yet work on Mac.
    BUG=16322 (http://crbug.com/16322)
    TEST=Covered by browser_tests.
    
    Review URL: http://codereview.chromium.org/189007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25265 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/save_page_browsertest.cc
+++ chrome/browser/download/save_page_browsertest.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/ui_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if !defined(OS_MACOSX)
+// TODO(port): BUG16322
 static const FilePath::CharType* kTestDir = FILE_PATH_LITERAL("save_page");
 
 static const char* kAppendedExtension =
@@ -24,6 +26,8 @@ static const char* kAppendedExtension =
 #else
     ".html";
 #endif
+
+#endif  // !defined(OS_MACOSX)
 
 namespace {
 
@@ -71,6 +75,8 @@ class SavePageBrowserTest : public InProcessBrowserTest {
   ScopedTempDir save_dir_;
 };
 
+#if !defined(OS_MACOSX)
+// TODO(port): BUG16322
 IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveHTMLOnly) {
   FilePath file_name(FILE_PATH_LITERAL("a.htm"));
   GURL url = URLRequestMockHTTPJob::GetMockUrl(
@@ -128,6 +134,7 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveCompleteHTML) {
       test_dir_.Append(FilePath(kTestDir)).AppendASCII("1.css"),
       dir.AppendASCII("1.css")));
 }
+#endif  // !defined(OS_MACOSX)
 
 IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, NoSave) {
   ui_test_utils::NavigateToURL(browser(), GURL("about:blank"));
@@ -135,6 +142,8 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, NoSave) {
   EXPECT_FALSE(browser()->command_updater()->IsCommandEnabled(IDC_SAVE_PAGE));
 }
 
+#if !defined(OS_MACOSX)
+// TODO(port): BUG16322
 IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, FileNameFromPageTitle) {
   FilePath file_name(FILE_PATH_LITERAL("b.htm"));
 
@@ -169,5 +178,6 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, FileNameFromPageTitle) {
       test_dir_.Append(FilePath(kTestDir)).AppendASCII("1.css"),
       dir.AppendASCII("1.css")));
 }
+#endif  // !defined(OS_MACOSX)
 
 }  // namespace
--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/extensions/extension_browsertest.h',
       'browser/extensions/extension_browsertests_misc.cc',
       'browser/extensions/extension_override_apitest.cc',
+      'browser/ssl/ssl_browser_tests.cc',
     ],
     # TODO(jcampan): move these vars to views.gyp.
     'views_unit_tests_sources': [
