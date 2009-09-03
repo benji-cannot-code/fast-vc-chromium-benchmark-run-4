6f2fbd64f7e357e525a934404dc4e794806aedb4
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-03 09:02:05 +0000

    DevTools: re-enable sanity tests disabled during the deps roll. BUG=20835 TBR=yurys Review URL: http://codereview.chromium.org/194010
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25305 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_sanity_unittest.cc
+++ chrome/browser/debugger/devtools_sanity_unittest.cc
@@ -168,7 +168,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, DISABLED_TestPauseInEval) {
 }
 
 // Tests console eval.
-IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, DISABLED_TestConsoleEval) {
+IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestConsoleEval) {
   RunTest("testConsoleEval", kConsoleTestPage);
 }
 
@@ -178,7 +178,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestConsoleLog) {
 }
 
 // Tests eval global values.
-IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, DISABLED_TestEvalGlobal) {
+IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestEvalGlobal) {
   RunTest("testEvalGlobal", kEvalTestPage);
 }
 
