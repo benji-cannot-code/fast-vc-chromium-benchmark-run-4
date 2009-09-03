70e91ec1c259b40b9a3c7ceb8489c118b9c621d2
amanda@chromium.org <amanda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-03 21:51:10 +0000

    Adjust color spaces so that Mac Chrome renders colors properly. BUG=19951,20552 TEST=compare pages rendered in Chromium and Safari.  They should appear the same. mark: review jrg/brettw: FYI
    Review URL: http://codereview.chromium.org/194013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25380 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/backing_store_mac.mm
+++ chrome/browser/renderer_host/backing_store_mac.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/render_widget_host_view.h"
 
 #include "base/logging.h"
+#include "base/mac_util.h"
 #include "chrome/common/transport_dib.h"
 #include "skia/ext/platform_canvas.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -33,10 +34,8 @@ BackingStore::BackingStore(RenderWidgetHost* widget, const gfx::Size& size)
   if (!containing_window) {
     // If we are not in a containing window yet, create a CGBitmapContext
     // to use as a stand-in for the layer.
-    scoped_cftyperef<CGColorSpaceRef>
-        color_space(CGColorSpaceCreateDeviceRGB());
     cg_bitmap_.reset(CGBitmapContextCreate(NULL, size.width(), size.height(),
-        8, size.width() * 8, color_space,
+        8, size.width() * 8, mac_util::GetSystemColorSpace(),
         kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host));
   } else {
     CGContextRef context = static_cast<CGContextRef>(
@@ -58,13 +57,12 @@ size_t BackingStore::MemorySize() {
 void BackingStore::PaintRect(base::ProcessHandle process,
                              TransportDIB* bitmap,
                              const gfx::Rect& bitmap_rect) {
-  scoped_cftyperef<CGColorSpaceRef> color_space(CGColorSpaceCreateDeviceRGB());
   scoped_cftyperef<CGDataProviderRef> data_provider(
       CGDataProviderCreateWithData(NULL, bitmap->memory(),
       bitmap_rect.width() * bitmap_rect.height() * 4, NULL));
   scoped_cftyperef<CGImageRef> image(
       CGImageCreate(bitmap_rect.width(), bitmap_rect.height(), 8, 32,
-          4 * bitmap_rect.width(), color_space,
+          4 * bitmap_rect.width(), mac_util::GetSystemColorSpace(),
           kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host,
           data_provider, NULL, false, kCGRenderingIntentDefault));
 
@@ -112,8 +110,6 @@ void BackingStore::ScrollRect(base::ProcessHandle process,
                               int dx, int dy,
                               const gfx::Rect& clip_rect,
                               const gfx::Size& view_size) {
-  if (!cg_layer()) return;
-
   // "Scroll" the contents of the layer by creating a new CGLayer,
   // copying the contents of the old one into the new one offset by the scroll
   // amount, swapping in the new CGLayer, and then painting in the new data.
@@ -146,11 +142,9 @@ void BackingStore::ScrollRect(base::ProcessHandle process,
       cg_layer_.swap(new_layer);
     } else {
       // We don't have a layer, so scroll the contents of the CGBitmapContext.
-      scoped_cftyperef<CGColorSpaceRef>
-          color_space(CGColorSpaceCreateDeviceRGB());
       scoped_cftyperef<CGContextRef> new_bitmap(
           CGBitmapContextCreate(NULL, size_.width(), size_.height(), 8,
-              size_.width() * 8, color_space,
+              size_.width() * 8, mac_util::GetSystemColorSpace(),
               kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host));
       scoped_cftyperef<CGImageRef> bitmap_image(
           CGBitmapContextCreateImage(cg_bitmap_));
