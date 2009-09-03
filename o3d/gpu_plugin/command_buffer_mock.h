cda2cfa287ae83b6d20df0c0b84c915a1495770d
apatrick@google.com <apatrick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-03 19:41:16 +0000

    Exracted CommandBuffer class from GPUPluginObject. TEST=none BUG=none
    Review URL: http://codereview.chromium.org/200005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25348 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ o3d/gpu_plugin/command_buffer_mock.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef O3D_GPU_PLUGIN_COMMAND_BUFFER_MOCK_H_
+#define O3D_GPU_PLUGIN_COMMAND_BUFFER_MOCK_H_
+
+#include "o3d/gpu_plugin/command_buffer.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace o3d {
+namespace gpu_plugin {
+
+// An NPObject that implements a shared memory command buffer and a synchronous
+// API to manage the put and get pointers.
+class MockCommandBuffer : public CommandBuffer {
+ public:
+  explicit MockCommandBuffer(NPP npp) : CommandBuffer(npp) {
+  }
+
+  MOCK_METHOD1(Initialize, bool(int32));
+  MOCK_METHOD0(GetBuffer, NPObjectPointer<NPObject>());
+  MOCK_METHOD1(SetPutOffset, void(int32));
+  MOCK_METHOD0(GetGetOffset, int32());
+};
+
+}  // namespace gpu_plugin
+}  // namespace o3d
+
+#endif  // O3D_GPU_PLUGIN_COMMAND_BUFFER_MOCK_H_
--- o3d/gpu_plugin/gpu_plugin_object.h
+++ o3d/gpu_plugin/gpu_plugin_object.h
@@ -8,14 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "o3d/gpu_plugin/command_buffer.h"
 #include "o3d/gpu_plugin/np_utils/dispatched_np_object.h"
 #include "o3d/gpu_plugin/np_utils/np_dispatcher.h"
 #include "o3d/gpu_plugin/np_utils/np_plugin_object.h"
 #include "o3d/gpu_plugin/np_utils/np_utils.h"
 #include "third_party/npapi/bindings/npapi.h"
 #include "third_party/npapi/bindings/npruntime.h"
-
-struct NaClDesc;
 
 namespace o3d {
 namespace gpu_plugin {
@@ -44,7 +43,7 @@ class GPUPluginObject : public DispatchedNPObject, public PluginObject {
 
   virtual NPObject* GetScriptableNPObject();
 
-  // Opens and returns the command buffer shared memory object.
+  // Initializes and returns the command buffer object.
   NPObjectPointer<NPObject> OpenCommandBuffer();
 
  protected:
@@ -63,7 +62,7 @@ class GPUPluginObject : public DispatchedNPObject, public PluginObject {
 
   Status status_;
   NPWindow window_;
-  NPObjectPointer<NPObject> command_buffer_object_;
+  NPObjectPointer<CommandBuffer> command_buffer_object_;
   NPSharedMemory* shared_memory_;
 };
 
