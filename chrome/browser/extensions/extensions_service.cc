67ae583c44de165493988fd7b51951dad091b563
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-03 00:03:20 +0000

    Fix to ensure IO thread is created when ExtensionUpdater needs it.
    In some cases on MacOS it turned out that the IO Thread hadn't been
    created at the time we were asking for it.
    
    BUG=http://crbug.com/20794
    TEST=none
    
    Review URL: http://codereview.chromium.org/184024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25277 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service.cc
+++ chrome/browser/extensions/extensions_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/string_util.h"
 #include "base/values.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/extensions/crx_installer.h"
 #include "chrome/browser/extensions/extension_browser_event_router.h"
@@ -114,7 +115,7 @@ ExtensionsService::ExtensionsService(Profile* profile,
           switches::kExtensionsUpdateFrequency)));
     }
     updater_ = new ExtensionUpdater(this, prefs, update_frequency,
-        backend_loop_, ChromeThread::GetMessageLoop(ChromeThread::IO));
+        backend_loop_, g_browser_process->io_thread()->message_loop());
   }
 
   backend_ = new ExtensionsServiceBackend(install_directory_, frontend_loop);
