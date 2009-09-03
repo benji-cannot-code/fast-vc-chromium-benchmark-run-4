ce778ca3464a25d704f933a0115edf0002530b4e
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-03 05:22:24 +0000

    Extension Doc Changes (no building or testable changes).
    Fix jstemplate recursion issue which was generating way to much extraneous hidden template data.
    
    Remote jstemplate attributes from rendered files.
    
    Remote #EOF that wandered into output.
    
    Template now renders from <body>.
    
    TBR=aa
    
    Review URL: http://codereview.chromium.org/201001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25299 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/js/api_page_generator.js
+++ chrome/common/extensions/docs/js/api_page_generator.js
@@ -185,11 +185,12 @@ function renderTemplate() {
 
   // Render to template
   var input = new JsEvalContext(pageData);
-  var output = document.getElementsByTagName("html")[0];
+  var output = document.getElementsByTagName("body")[0];
   jstProcess(input, output);
 
   selectCurrentPageOnLeftNav();
   
+  document.title = getPageTitle();
   // Show
   if (window.postRender)
     window.postRender();
@@ -198,7 +199,21 @@ function renderTemplate() {
     parent.done();
 }
 
+function removeJsTemplateAttributes(root) {
+  var jsattributes = ["jscontent", "jsselect", "jsdisplay", "transclude",
+                      "jsvalues", "jsvars", "jseval", "jsskip", "jstcache"];
+
+  var nodes = root.getElementsByTagName("*");
+  for (var i = 0; i < nodes.length; i++) {
+    var n = nodes[i]
+    jsattributes.each(function(attributeName) {
+      n.removeAttribute(attributeName);
+    });
+  }
+}
+
 function serializePage() {
+ removeJsTemplateAttributes(document);
  var s = new XMLSerializer();
  return s.serializeToString(document);
 }
