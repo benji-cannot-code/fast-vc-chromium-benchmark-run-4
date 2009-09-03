c25beb5653a305a4ecd580eeb08e0a1249c4689a
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-03 23:13:07 +0000

    Fix loading bubble not disappearing with corrupt theme image.
    BUG= http://crbug.com/21044
    TEST= try installing a theme with a corrupt image.  note that loading bubble disappears.
    
    Review URL: http://codereview.chromium.org/196016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25401 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/crx_installer.cc
+++ chrome/browser/extensions/crx_installer.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/extension_file_util.h"
 #include "chrome/common/extensions/extension_error_reporter.h"
+#include "chrome/common/notification_service.h"
+#include "chrome/common/notification_type.h"
 #include "grit/chromium_strings.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "webkit/glue/image_decoder.h"
@@ -252,6 +254,11 @@ void CrxInstaller::ReportFailureFromFileThread(const std::string& error) {
 
 void CrxInstaller::ReportFailureFromUIThread(const std::string& error) {
   DCHECK(MessageLoop::current() == ui_loop_);
+
+  NotificationService* service = NotificationService::current();
+  service->Notify(NotificationType::NO_THEME_DETECTED,
+                  Source<CrxInstaller>(this),
+                  NotificationService::NoDetails());
 
   // This isn't really necessary, it is only used because unit tests expect to
   // see errors get reported via this interface.
