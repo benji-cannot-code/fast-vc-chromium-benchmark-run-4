d18d56cb302cb53e897192b028a8f42eec1005c9
yong.li@torchmobile.com <yong.li@torchmobile.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-04 14:00:22 +0000

    2009-09-03  Yong Li  <yong.li@torchmobile.com>
            Reviewed by David Levin.
    
            Remove unnecessary dependency on unistd.h
            https://bugs.webkit.org/show_bug.cgi?id=28962
    
            * runtime/Completion.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/Completion.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/Completion.cpp
@@ -32,10 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Debugger.h"
 #include <stdio.h>
 
-#if !PLATFORM(WIN_OS)
-#include <unistd.h>
-#endif
-
 namespace JSC {
 
 Completion checkSyntax(ExecState* exec, const SourceCode& source)
