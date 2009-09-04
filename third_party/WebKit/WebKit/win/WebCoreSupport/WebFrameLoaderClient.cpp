718eee71cc59cf6b8d0c9780c746665324835274
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-04 16:34:02 +0000

    2009-09-04  Adam Barth  <abarth@webkit.org>
            Unreviewed attempt at a build fix.
    
            Added missing include.
    
            * WebCoreSupport/WebFrameLoaderClient.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48054 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebCoreSupport/WebFrameLoaderClient.cpp
+++ third_party/WebKit/WebKit/win/WebCoreSupport/WebFrameLoaderClient.cpp
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebHistoryItem.h"
 #include "WebMutableURLRequest.h"
 #include "WebNotificationCenter.h"
+#include "WebSecurityOrigin.h"
 #include "WebURLAuthenticationChallenge.h"
 #include "WebURLResponse.h"
 #include "WebView.h"
