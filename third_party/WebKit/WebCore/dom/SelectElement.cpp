07da3374380337794b2bfefb316fcadb4be5aea8
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-04 08:40:37 +0000

    2009-09-04  Albert Bachand  <albertb@google.com>
            Reviewed by Eric Seidel.
    
            Modify the condition regarding the key modifier for opening combo
            boxes under GTK from just PLATFORM(GTK) to also look for
            (PLATFORM(CHROMIUM) && PLATFORM(LINUX)).
    
            * WebCore/dom/SelectElement.cpp:
            * WebCore/manual-tests/select-popup-on-spacebar.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48044 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/SelectElement.cpp
+++ third_party/WebKit/WebCore/dom/SelectElement.cpp
@@ -49,11 +49,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 // Configure platform-specific behavior when focused pop-up receives arrow/space/return keystroke.
-// (PLATFORM(MAC) is always false in Chromium, hence the extra test.)
+// (PLATFORM(MAC) and PLATFORM(GTK) are always false in Chromium, hence the extra tests.)
 #if PLATFORM(MAC) || (PLATFORM(CHROMIUM) && PLATFORM(DARWIN))
 #define ARROW_KEYS_POP_MENU 1
 #define SPACE_OR_RETURN_POP_MENU 0
-#elif PLATFORM(GTK)
+#elif PLATFORM(GTK) || (PLATFORM(CHROMIUM) && PLATFORM(LINUX))
 #define ARROW_KEYS_POP_MENU 0
 #define SPACE_OR_RETURN_POP_MENU 1
 #else
