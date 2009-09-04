9dc6caf910664ed79dd88b5bf92b69d090f718c4
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-04 16:23:19 +0000

    2009-09-04  Adam Barth  <abarth@webkit.org>
            Unreviewed attempt at a build fix.
    
            Fix typo of IWebFrameLoadDelegatePrivate2 and make
            IWebFrameLoadDelegatePrivate inherit from IWebFrameLoadDelegatePrivate
            as requestd by Adam Roben.
    
            * Interfaces/IWebFrameLoadDelegatePrivate2.idl:
            * WebCoreSupport/WebFrameLoaderClient.cpp:
            (WebFrameLoaderClient::didDisplayInsecureContent):
            (WebFrameLoaderClient::didRunInsecureContent):
    2009-09-04  Adam Barth  <abarth@webkit.org>
    
            Unreviewed build fix.
    
            Update declaration of FrameLoadDelegate to reflect that
            IWebFrameLoadDelegatePrivate2 inherits from
            IWebFrameLoadDelegatePrivate.
    
            * DumpRenderTree/win/FrameLoadDelegate.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/win/FrameLoadDelegate.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/win/FrameLoadDelegate.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class AccessibilityController;
 class GCController;
 
-class FrameLoadDelegate : public IWebFrameLoadDelegate, public IWebFrameLoadDelegatePrivate, public IWebFrameLoadDelegatePrivate2 {
+class FrameLoadDelegate : public IWebFrameLoadDelegate, public IWebFrameLoadDelegatePrivate2 {
 public:
     FrameLoadDelegate();
     virtual ~FrameLoadDelegate();
