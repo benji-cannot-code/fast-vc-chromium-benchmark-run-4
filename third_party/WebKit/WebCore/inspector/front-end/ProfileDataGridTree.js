32f5cfefb9831bf075325638b17bf8b9e70f4dff
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-04 21:11:39 +0000

    2009-09-04  Mikhail Naganov  <mnaganov@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Add "sampling profiler" preference, simplify data display when it's set.
    
            https://bugs.webkit.org/show_bug.cgi?id=28969
    
            * inspector/front-end/ProfileDataGridTree.js:
            (WebInspector.ProfileDataGridNode.prototype.get data.formatMilliseconds):
            (WebInspector.ProfileDataGridNode.prototype.get data):
            * inspector/front-end/ProfileView.js:
            (WebInspector.ProfileView):
            * inspector/front-end/inspector.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ProfileDataGridTree.js
+++ third_party/WebKit/WebCore/inspector/front-end/ProfileDataGridTree.js
@@ -51,7 +51,7 @@ WebInspector.ProfileDataGridNode.prototype = {
     {
         function formatMilliseconds(time)
         {
-            return Number.secondsToString(time / 1000, WebInspector.UIString.bind(WebInspector), true);
+            return Number.secondsToString(time / 1000, WebInspector.UIString.bind(WebInspector), !Preferences.samplingCPUProfiler);
         }
 
         var data = {};
