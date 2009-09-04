90f6e016d980ba4b7ffdcef1463dbbf1342a49cb
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-04 23:59:40 +0000

    POSIX: Have pref service save paths as UTF8.
    We were passing the raw path string to StringValue, but
    StringValue expects strings to be UTF8.  There's only
    a DCHECK to enforce this, so I imagine it was working
    silently before.
    
    This will break for users that don't currently have UTF8
    file names, but it will now work going forward.
    
    Review URL: http://codereview.chromium.org/201027
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25555 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_service.cc
+++ chrome/common/pref_service.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/stl_util-inl.h"
 #include "base/string_util.h"
+#include "base/sys_string_conversions.h"
 #include "base/thread.h"
+#include "build/build_config.h"
 #include "chrome/common/json_value_serializer.h"
 #include "chrome/common/notification_service.h"
 #include "grit/generated_resources.h"
@@ -290,6 +292,10 @@ FilePath PrefService::GetFilePath(const wchar_t* path) const {
   }
   bool rv = pref->GetValue()->GetAsString(&result);
   DCHECK(rv);
+#if defined(OS_POSIX)
+  // We store filepaths as UTF8, so convert it back to the system type.
+  result = base::SysWideToNativeMB(UTF8ToWide(result));
+#endif
   return FilePath(result);
 }
 
@@ -512,7 +518,14 @@ void PrefService::SetFilePath(const wchar_t* path, const FilePath& value) {
   }
 
   scoped_ptr<Value> old_value(GetPrefCopy(path));
+#if defined(OS_POSIX)
+  // Value::SetString only knows about UTF8 strings, so convert the path from
+  // the system native value to UTF8.
+  std::string path_utf8 = WideToUTF8(base::SysNativeMBToWide(value.value()));
+  bool rv = persistent_->SetString(path, path_utf8);
+#else
   bool rv = persistent_->SetString(path, value.value());
+#endif
   DCHECK(rv);
 
   FireObserversIfChanged(path, old_value.get());
