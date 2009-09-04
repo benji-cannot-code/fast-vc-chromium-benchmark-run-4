a0131e7f53b78636352bca4746e59f611d472e93
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-04 16:42:29 +0000

    Added missing ;
    git-svn-id: svn://svn.chromium.org/blink/trunk@48056 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebCoreSupport/WebFrameLoaderClient.cpp
+++ third_party/WebKit/WebKit/win/WebCoreSupport/WebFrameLoaderClient.cpp
@@ -542,7 +542,7 @@ void WebFrameLoaderClient::didDisplayInsecureContent()
     if (!frameLoadDelegatePriv2)
         return;
 
-    frameLoadDelegatePriv2->didDisplayInsecureContent(webView)
+    frameLoadDelegatePriv2->didDisplayInsecureContent(webView);
 }
 
 void WebFrameLoaderClient::didRunInsecureContent(SecurityOrigin* origin)
@@ -558,7 +558,7 @@ void WebFrameLoaderClient::didRunInsecureContent(SecurityOrigin* origin)
     if (!frameLoadDelegatePriv2)
         return;
 
-    frameLoadDelegatePriv2->didRunInsecureContent(webView, webSecurityOrigin.get())
+    frameLoadDelegatePriv2->didRunInsecureContent(webView, webSecurityOrigin.get());
 }
 
 PassRefPtr<DocumentLoader> WebFrameLoaderClient::createDocumentLoader(const ResourceRequest& request, const SubstituteData& substituteData)
