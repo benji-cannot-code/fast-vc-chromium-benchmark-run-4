6aff93e62066eca612ae44ee006bda86191ff16b
mhm@chromium.org <mhm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-04 13:08:58 +0000

    Fix build for mac bots
    TBR=maruel
    Review URL: http://codereview.chromium.org/201016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25446 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/print_web_view_helper.cc
+++ chrome/renderer/print_web_view_helper.cc
@@ -140,7 +140,7 @@ void PrintWebViewHelper::PrintPages(const ViewMsg_PrintPages_Params& params,
     }
   }
 }
-
+#ifndef OS_MACOSX
 void PrintWebViewHelper::PrintPageAsJPEG(
     const ViewMsg_PrintPage_Params& params,
     WebFrame* frame,
@@ -175,7 +175,7 @@ void PrintWebViewHelper::PrintPageAsJPEG(
       image_data);
   DCHECK(encoded);
 }
-
+#endif
 bool PrintWebViewHelper::Send(IPC::Message* msg) {
   return render_view_->Send(msg);
 }
