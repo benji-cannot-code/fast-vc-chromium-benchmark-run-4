b60cac002189de7eb286e55cb2bfae9bf5725c14
mhm@chromium.org <mhm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-04 12:22:45 +0000

    Implement PrintPageAsJPEG feature for Print Preview.In order to show the tab contents to the print tab, the renderer should print the page as a JPEG image so it could be rendered in the DOMUI. A unit test case has been created to make sure the image contains something from deciding whether its not 100% white.MockPrinter has been altered to remove the const parameter, since its useful to retrieve the DefaultSettings not via IPC.BUG=173 (http://crbug.com/173)TEST=created a unit test and ran it. Review URL: http://codereview.chromium.org/196021
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25445 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/print_web_view_helper.cc
+++ chrome/renderer/print_web_view_helper.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/print_web_view_helper.h"
 
 #include "app/l10n_util.h"
+#include "base/gfx/jpeg_codec.h"
 #include "base/logging.h"
 #include "chrome/common/render_messages.h"
 #include "chrome/renderer/render_view.h"
@@ -138,6 +139,41 @@ void PrintWebViewHelper::PrintPages(const ViewMsg_PrintPages_Params& params,
       }
     }
   }
+}
+
+void PrintWebViewHelper::PrintPageAsJPEG(
+    const ViewMsg_PrintPage_Params& params,
+    WebFrame* frame,
+    float zoom_factor,
+    std::vector<unsigned char>* image_data) {
+  PrepareFrameAndViewForPrint prep_frame_view(params.params,
+                                              frame,
+                                              frame->view());
+  const gfx::Size& canvas_size(prep_frame_view.GetPrintCanvasSize());
+
+  // Since WebKit extends the page width depending on the magical shrink
+  // factor we make sure the canvas covers the worst case scenario
+  // (x2.0 currently).  PrintContext will then set the correct clipping region.
+  int size_x = static_cast<int>(canvas_size.width() * params.params.max_shrink);
+  int size_y = static_cast<int>(canvas_size.height() *
+      params.params.max_shrink);
+
+  // Access the bitmap from the canvas device.
+  skia::PlatformCanvas canvas(size_x, size_y, true);
+  frame->printPage(params.page_number, &canvas);
+  const SkBitmap& bitmap = canvas.getDevice()->accessBitmap(false);
+
+  // Encode the SkBitmap to jpeg.
+  SkAutoLockPixels image_lock(bitmap);
+  bool encoded = JPEGCodec::Encode(
+      reinterpret_cast<unsigned char*>(bitmap.getAddr32(0, 0)),
+      JPEGCodec::FORMAT_BGRA,
+      static_cast<int>(bitmap.width() * zoom_factor),
+      static_cast<int>(bitmap.height() * zoom_factor),
+      static_cast<int>(bitmap.rowBytes()),
+      90,
+      image_data);
+  DCHECK(encoded);
 }
 
 bool PrintWebViewHelper::Send(IPC::Message* msg) {
