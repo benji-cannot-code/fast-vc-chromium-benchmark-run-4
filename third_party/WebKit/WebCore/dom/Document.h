982a3295a866a7ad428471949d4518d76bf00075
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-04 19:53:48 +0000

    Remove unused m_sheet variable and setCSSStyleSheet methods.
    Reviewed by Anders Carlsson.
    
    * dom/Document.cpp:
    * dom/Document.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48071 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.h
+++ third_party/WebKit/WebCore/dom/Document.h
@@ -872,7 +872,6 @@ private:
 
     virtual String nodeName() const;
     virtual NodeType nodeType() const;
-    virtual void setCSSStyleSheet(const String& url, const String& charset, const CachedCSSStyleSheet*);
     virtual bool childTypeAllowed(NodeType);
     virtual PassRefPtr<Node> cloneNode(bool deep);
     virtual bool canReplaceChild(Node* newChild, Node* oldChild);
@@ -923,7 +922,6 @@ private:
     RefPtr<DocumentType> m_docType;
     mutable RefPtr<DOMImplementation> m_implementation;
 
-    RefPtr<StyleSheet> m_sheet;
 #if FRAME_LOADS_USER_STYLESHEET
     String m_usersheet;
 #endif
