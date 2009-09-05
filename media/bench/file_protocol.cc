17857227bc5ab99c195b30698ce83b3fcd38373e
fbarchard@chromium.org <fbarchard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-05 00:43:14 +0000

    Media bench add -hash, 64 bit IO, and some cleanup of dump BUG=21126 TEST=use media_bench -hash and it compare txt files instead of raw dump output.  Should be substantially faster. Review URL: http://codereview.chromium.org/195012
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25563 0039d316-1c4b-4281-b951-d872f2087c98

--- media/bench/file_protocol.cc
+++ media/bench/file_protocol.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/bench/file_protocol.h"
 
+#include <stdio.h>
 #include "base/file_util.h"
 #include "base/logging.h"
 #include "media/filters/ffmpeg_common.h"
@@ -38,7 +39,9 @@ int WriteContext(URLContext* h, unsigned char* buf, int size) {
 
 offset_t SeekContext(URLContext* h, offset_t offset, int whence) {
 #if defined(OS_WIN)
-  return _fseeki64(ToFile(h->priv_data), offset, whence);
+  return static_cast<offset_t> (_fseeki64(ToFile(h->priv_data),
+                                          static_cast<int64>(offset),
+                                          whence));
 #else
   return fseek(ToFile(h->priv_data), offset, whence);
 #endif
