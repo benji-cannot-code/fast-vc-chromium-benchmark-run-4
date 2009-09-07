a1b7d45323022937778dfa3e136b5c378451b3e7
atwilson@chromium.org <atwilson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-07 20:50:47 +0000

    Unreviewed build fix.
    Temporarily removed depenency between JSEventTarget.cpp and WebSocket.h in an attempt to fix a windows build break.
    
    * bindings/js/JSEventTarget.cpp:
    (WebCore::toEventTarget):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48134 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSEventTarget.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSEventTarget.cpp
@@ -81,7 +81,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(WEB_SOCKETS)
 #include "JSWebSocket.h"
-#include "WebSocket.h"
+// FIXME(ukai@chromium.org): Disabled this include to fix Windows build. Re-enable this include once the Windows build files are fixed.
+//#include "WebSocket.h"
+class WebSocket;
 #endif
 
 using namespace JSC;
@@ -195,7 +197,8 @@ EventTarget* toEventTarget(JSC::JSValue value)
 #endif
 
 #if ENABLE(WEB_SOCKETS)
-    CONVERT_TO_EVENT_TARGET(WebSocket)
+// FIXME(ukai@chromium.org): Uncomment this when it compiles under Windows.
+//    CONVERT_TO_EVENT_TARGET(WebSocket)
 #endif
 
     return 0;
