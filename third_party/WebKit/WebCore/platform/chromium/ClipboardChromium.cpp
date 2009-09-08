d7190af014fcc855232cd09f0e56fe9f257b5fc5
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-08 17:58:05 +0000

    2009-09-08  Steve VanDeBogart  <vandebo@chromium.org>
            Reviewed by Eric Seidel.
    
            Handle middle click in Chromium like QT
            https://bugs.webkit.org/show_bug.cgi?id=28696
    
            Tested by middle-click-onpaste.html.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/ClipboardChromium.cpp
+++ third_party/WebKit/WebCore/platform/chromium/ClipboardChromium.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MIMETypeRegistry.h"
 #include "markup.h"
 #include "NamedNodeMap.h"
+#include "Pasteboard.h"
 #include "PlatformString.h"
 #include "Range.h"
 #include "RenderImage.h"
@@ -118,7 +119,11 @@ String ClipboardChromium::getData(const String& type, bool& success) const
         if (!isForDragging()) {
             // If this isn't for a drag, it's for a cut/paste event handler.
             // In this case, we need to check the clipboard.
-            text = ChromiumBridge::clipboardReadPlainText();
+            PasteboardPrivate::ClipboardBuffer buffer = 
+                Pasteboard::generalPasteboard()->isSelectionMode() ?
+                PasteboardPrivate::SelectionBuffer : 
+                PasteboardPrivate::StandardBuffer;
+            text = ChromiumBridge::clipboardReadPlainText(buffer);
             success = !text.isEmpty();
         } else if (!m_dataObject->plainText.isEmpty()) {
             success = true;
