49ef4cca07571595d5d64b00b9841c23fb608811
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-08 22:02:41 +0000

    WebCore: Fix for https://bugs.webkit.org/show_bug.cgi?id=27046 Implement CSSOM DocumentView.caretRangeFromPoint
    Reviewed by Timothy Hatcher.
    
    Tests: fast/dom/Document/CaretRangeFromPoint/basic.html
           fast/dom/Document/CaretRangeFromPoint/replace-element.html
    
    * dom/Document.cpp:
    (WebCore::Document::caretRangeFromPoint):
    * dom/Document.h:
    * dom/Document.idl:
    
    LayoutTests: Test for https://bugs.webkit.org/show_bug.cgi?id=27046
    Implement CSSOM DocumentView.caretRangeFromPoint
    
    Reviewed by Timothy Hatcher.
    
    * fast/dom/Document/CaretRangeFromPoint: Added.
    * fast/dom/Document/CaretRangeFromPoint/basic-expected.txt: Added.
    * fast/dom/Document/CaretRangeFromPoint/basic.html: Added.
    * fast/dom/Document/CaretRangeFromPoint/replace-element-expected.txt: Added.
    * fast/dom/Document/CaretRangeFromPoint/replace-element.html: Added.
    * fast/dom/Window/window-properties-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -111,11 +111,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SegmentedString.h"
 #include "SelectionController.h"
 #include "Settings.h"
-
-#if ENABLE(SHARED_WORKERS)
-#include "SharedWorkerRepository.h"
-#endif
-
 #include "StyleSheetList.h"
 #include "TextEvent.h"
 #include "TextIterator.h"
@@ -129,6 +124,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "XMLHttpRequest.h"
 #include "XMLNames.h"
 #include "XMLTokenizer.h"
+#include "htmlediting.h"
 #include <wtf/CurrentTime.h>
 #include <wtf/HashFunctions.h>
 #include <wtf/MainThread.h>
@@ -138,6 +134,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(DATABASE)
 #include "Database.h"
 #include "DatabaseThread.h"
+#endif
+
+#if ENABLE(SHARED_WORKERS)
+#include "SharedWorkerRepository.h"
 #endif
 
 #if ENABLE(DOM_STORAGE)
@@ -942,6 +942,49 @@ Element* Document::elementFromPoint(int x, int y) const
     if (n)
         n = n->shadowAncestorNode();
     return static_cast<Element*>(n);
+}
+
+PassRefPtr<Range> Document::caretRangeFromPoint(int x, int y)
+{
+    if (!renderer())
+        return 0;
+    Frame* frame = this->frame();
+    if (!frame)
+        return 0;
+
+    float zoomFactor = frame->pageZoomFactor();
+    IntPoint point = roundedIntPoint(FloatPoint(x * zoomFactor, y * zoomFactor));
+
+    FrameView* frameView = frame->view();
+    if (!frameView)
+        return 0;
+    if (!frameView->boundsRect().contains(point))
+        return 0;
+
+    HitTestRequest request(HitTestRequest::ReadOnly | HitTestRequest::Active);
+    HitTestResult result(point);
+    renderView()->layer()->hitTest(request, result);
+
+    Node* node = result.innerNode();
+    if (!node)
+        return 0;
+
+    Node* shadowAncestorNode = node->shadowAncestorNode();
+    if (shadowAncestorNode != node) {
+        unsigned offset = shadowAncestorNode->nodeIndex();
+        Node* container = shadowAncestorNode->parentNode();
+        return Range::create(this, container, offset, container, offset);
+    }
+
+    RenderObject* renderer = node->renderer();
+    if (!renderer)
+        return 0;
+    VisiblePosition visiblePosition = renderer->positionForPoint(result.localPoint());
+    if (visiblePosition.isNull())
+        return 0;
+
+    Position rangeCompliantPosition = rangeCompliantEquivalent(visiblePosition);
+    return Range::create(this, rangeCompliantPosition, rangeCompliantPosition);
 }
 
 void Document::addElementById(const AtomicString& elementId, Element* element)
