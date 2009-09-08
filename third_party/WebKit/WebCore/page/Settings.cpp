041516a5a7a3c929ed21954cbd42ca0e0ae5d537
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-08 20:26:14 +0000

    https://bugs.webkit.org/show_bug.cgi?id=28987, make the Document cache the page's user stylesheet in parsed form.  Allows the sheet to survive across destruction/re-creation of the CSSStyleSelector.
    Reviewed by Eric Seidel.
    
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::CSSStyleSelector):
    * css/CSSStyleSelector.h:
    * dom/Document.cpp:
    (WebCore::Document::attach):
    (WebCore::Document::pageUserSheet):
    (WebCore::Document::clearPageUserSheet):
    (WebCore::Document::recalcStyleSelector):
    * dom/Document.h:
    * loader/PlaceholderDocument.cpp:
    (WebCore::PlaceholderDocument::attach):
    * page/Page.cpp:
    (WebCore::Page::userStyleSheetLocationChanged):
    * page/Settings.cpp:
    (WebCore::Settings::setUserStyleSheetLocation):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Settings.cpp
+++ third_party/WebKit/WebCore/page/Settings.cpp
@@ -274,7 +274,6 @@ void Settings::setUserStyleSheetLocation(const KURL& userStyleSheetLocation)
     m_userStyleSheetLocation = userStyleSheetLocation;
 
     m_page->userStyleSheetLocationChanged();
-    setNeedsReapplyStylesInAllFrames(m_page);
 }
 
 void Settings::setShouldPrintBackgrounds(bool shouldPrintBackgrounds)
