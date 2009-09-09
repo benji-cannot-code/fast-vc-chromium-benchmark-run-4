aea1e5d535d8e7f4c027694465b86d0e431c6597
arv@google.com <arv@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-09 21:20:24 +0000

    NNTP: Disable tabbing to hidden elements.
    This is a work around since the WebKit fix is not on the M3 branch.
    
    BUG=15711
    
    TEST=Tab around. At all times something on the page should have focus
    (unless the address bar got the focus).
    
    
    Review URL: http://codereview.chromium.org/201068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25789 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/new_new_tab.js
+++ chrome/browser/resources/new_new_tab.js
@@ -189,7 +189,7 @@ function onShownSections(mask) {
     }
 
     mostVisited.updateDisplayMode();
-    layoutRecentlyClosed();
+    renderRecentlyClosed();
     updateOptionMenu();
   }
 
@@ -299,7 +299,7 @@ function handleWindowResize() {
   if (layoutMode != oldLayoutMode){
     mostVisited.invalidate();
     mostVisited.layout();
-    layoutRecentlyClosed();
+    renderRecentlyClosed();
   }
 }
 
@@ -330,7 +330,7 @@ function showSection(section) {
       shownSections &= ~Section.THUMB;
       mostVisited.invalidate();
     } else {
-      layoutRecentlyClosed();
+      renderRecentlyClosed();
     }
 
     updateOptionMenu();
@@ -348,7 +348,7 @@ function hideSection(section) {
     }
 
     if (section & Section.RECENT) {
-      layoutRecentlyClosed();
+      renderRecentlyClosed();
     }
 
     updateOptionMenu();
@@ -678,11 +678,6 @@ function layoutRecentlyClosed() {
   } else {
     style.opacity = style.height = '';
 
-    // Show all items.
-    for (var i = 0, child; child = recentElement.children[i]; i++) {
-      child.style.display = '';
-    }
-
     // We cannot use clientWidth here since the width has a transition.
     var spacing = 20;
     var headerEl = recentElement.firstElementChild;
@@ -701,7 +696,7 @@ function layoutRecentlyClosed() {
     }
 
     elementsToHide.forEach(function(el) {
-      el.style.display = 'none';
+      el.parentNode.removeChild(el);
     });
   }
 }
@@ -920,6 +915,8 @@ function showNotification(text, actionText, opt_f, opt_delay) {
   notificationElement.onmouseout = delayedHide;
   actionLink.onfocus = show;
   actionLink.onblur = delayedHide;
+  // Enable tabbing to the link now that it is shown.
+  actionLink.tabIndex = 0;
 
   show();
   delayedHide();
@@ -928,6 +925,9 @@ function showNotification(text, actionText, opt_f, opt_delay) {
 function hideNotification() {
   var notificationElement = $('notification');
   removeClass(notificationElement, 'show');
+  var actionLink = notificationElement.querySelector('.link');
+  // Prevent tabbing to the hidden link.
+  actionLink.tabIndex = -1;
 }
 
 function showFirstRunNotification() {
