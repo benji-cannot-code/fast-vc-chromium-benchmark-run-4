7de83a73b61644783c4b6529507cc8e9ca9268ec
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-09 23:55:07 +0000

    2009-09-09  Alpha Lam  <hclam@chromium.org>
            Reviewed by Eric Carlson.
    
            Media layout tests should have a way to provide test files in different formats
            https://bugs.webkit.org/show_bug.cgi?id=28327
    
            Added media-file.js which has a findMediaFile(type, name) method
            to find a proper media file to load according to supported codecs.
    
            Also changed 10 layout tests to use above method to locate the test file.
    
            * media/controls-strict.html: Use findMediaFile(type, name).
            * media/controls-styling.html: ditto.
            * media/media-file.js: Added. Provide findMediaFile(type, name).
            * media/video-append-source.html: Use findMediaFile(type, name).
            * media/video-aspect-ratio.html: ditto.
            * media/video-autoplay.html: ditto.
            * media/video-buffered.html: ditto.
            * media/video-controls-rendering.html: ditto.
            * media/video-layer-crash.html: ditto.
            * media/video-transformed.html: Use findMediaFile(type, name).
            * media/video-zoom-controls.html: ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48237 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/media/media-file.js
@@ -0,0 +1,50 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var audioCodecs = [
+    ["audio/aac", "m4a"],
+    ["audio/mpeg", "mp3"],
+    ["audio/wav", "wav"],
+    ["audio/ogg", "ogg"]
+];
+
+var videoCodecs = [
+    ["video/mp4", "mp4"],
+    ["video/mpeg", "mpg"],
+    ["video/quicktime", "mov"],
+    ["video/ogg", "ogg"]
+];
+
+function findMediaFile(tagName, name) {
+    var codecs;
+    if (tagName == "audio")
+        codecs = audioCodecs;
+    else
+        codecs = videoCodecs;
+
+    var element;
+    try {
+        element = document.getElementsByTagName(tagName)[0];
+    } catch (ex) {
+        element = document.createElement(tagName);
+    }
+
+    for (var i = 0; i < codecs.length; ++i) {
+        if (element.canPlayType(codecs[i][0]))
+            return name + "." + codecs[i][1];
+    }
+
+    return "";
+}
+
+function setSrcByTagName(tagName, src) {
+    var elements = document.getElementsByTagName(tagName);
+    if (elements) {
+        for (var i = 0; i < elements.length; ++i)
+            elements[i].src = src;
+    }
+}
+
+function setSrcById(id, src) {
+    var element = document.getElementById(id);
+    if (element)
+        id.src = src;
+}
