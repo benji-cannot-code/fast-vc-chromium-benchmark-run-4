9c85549009e8701561b8f7a41b5658461089770b
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-09 05:55:54 +0000

    Reviewed by NOBODY (Build fix).
    Remove a few incorrect changes I left in.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48201 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -288,7 +288,7 @@ sub GenerateGetOwnPropertySlotBody
 
     my $manualLookupGetterGeneration = sub {
         my $requiresManualLookup = $dataNode->extendedAttributes->{"HasIndexGetter"} || $dataNode->extendedAttributes->{"HasNameGetter"};
-        if ($requiresManualLookup and $numAttributes > 0) {
+        if ($requiresManualLookup) {
             push(@getOwnPropertySlotImpl, "    const ${namespaceMaybe}HashEntry* entry = ${className}Table.entry(exec, propertyName);\n");
             push(@getOwnPropertySlotImpl, "    if (entry) {\n");
             push(@getOwnPropertySlotImpl, "        slot.setCustom(this, entry->propertyGetter());\n");
@@ -365,7 +365,7 @@ sub GenerateGetOwnPropertyDescriptorBody
     
     my $manualLookupGetterGeneration = sub {
         my $requiresManualLookup = $dataNode->extendedAttributes->{"HasIndexGetter"} || $dataNode->extendedAttributes->{"HasNameGetter"};
-        if ($requiresManualLookup and $numAttributes > 0) {
+        if ($requiresManualLookup) {
             push(@getOwnPropertyDescriptorImpl, "    const ${namespaceMaybe}HashEntry* entry = ${className}Table.entry(exec, propertyName);\n");
             push(@getOwnPropertyDescriptorImpl, "    if (entry) {\n");
             push(@getOwnPropertyDescriptorImpl, "        PropertySlot slot;\n");
