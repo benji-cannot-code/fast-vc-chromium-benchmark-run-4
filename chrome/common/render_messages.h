743c2f3fe3e005a519679ddb6e7163caabbc99b5
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-09 22:06:49 +0000

    Use gtk-cursor-blink from GtkSettings to set the caret blink interval on Linux
    This can be set with the "Cursor Blinking" slider and checkbox in gnome-keyboard-properties.  The changed setting will appear in a new renderer, so opening a new tab will see the change.
    
    WebKit side is at https://bugs.webkit.org/show_bug.cgi?id=28931
    
    Patch by Joel Stanley <joel@jms.id.au>
    
    BUG=20772
    
    Review URL: http://codereview.chromium.org/186009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25797 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/render_messages.h
+++ chrome/common/render_messages.h
@@ -1580,6 +1580,7 @@ struct ParamTraits<RendererPreferences> {
     WriteParam(m, p.focus_ring_color_g);
     WriteParam(m, p.focus_ring_color_b);
     WriteParam(m, p.browser_handles_top_level_requests);
+    WriteParam(m, p.caret_blink_interval);
   }
   static bool Read(const Message* m, void** iter, param_type* p) {
     if (!ReadParam(m, iter, &p->can_accept_load_drops))
@@ -1615,6 +1616,9 @@ struct ParamTraits<RendererPreferences> {
     p->focus_ring_color_b = focus_ring_color_b;
 
     if (!ReadParam(m, iter, &p->browser_handles_top_level_requests))
+      return false;
+
+    if (!ReadParam(m, iter, &p->caret_blink_interval))
       return false;
 
     return true;
