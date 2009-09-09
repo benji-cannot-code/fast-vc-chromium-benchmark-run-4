315eec08903d8ef7cae8cc169ed5b9d65ea533cc
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-09 22:10:53 +0000

    Disable AutomationProxyVisibleTest.AutocompleteMatchesTest.
    It is flaky, and the underlying automation seems to be horribly broken.
    
    I'm going to be fixing that in small steps. Disabling the test is the first one.
    
    TEST=none
    http://crbug.com/19876
    
    Review URL: http://codereview.chromium.org/193064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25801 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automation/automation_proxy_uitest.cc
+++ chrome/test/automation/automation_proxy_uitest.cc
@@ -982,9 +982,8 @@ TEST_F(AutomationProxyTest, AutocompleteParallelProxy) {
 
 #endif  // defined(OS_WIN) || defined(OS_LINUX)
 
-// Not run on linux because of flakiness. http://crbug.com/19876
-#if defined(OS_WIN)
-TEST_F(AutomationProxyVisibleTest, AutocompleteMatchesTest) {
+// So flaky, http://crbug.com/19876. Consult phajdan.jr before re-enabling.
+TEST_F(AutomationProxyVisibleTest, DISABLED_AutocompleteMatchesTest) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
   scoped_refptr<AutocompleteEditProxy> edit(
@@ -1001,7 +1000,6 @@ TEST_F(AutomationProxyVisibleTest, AutocompleteMatchesTest) {
   EXPECT_TRUE(edit->GetAutocompleteMatches(&matches));
   EXPECT_FALSE(matches.empty());
 }
-#endif  // defined(OS_WIN)
 
 // Disabled because flaky see bug #5314.
 TEST_F(AutomationProxyTest, DISABLED_AppModalDialogTest) {
