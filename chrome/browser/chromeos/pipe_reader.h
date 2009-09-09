834980658c4e8cfeae9eb83a5fad55c3e92b9d8d
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-09 00:46:58 +0000

    Fix build break for chrome os.
    Add #include <stdio.h> for fclose et al.
    Without stdio.h, we got errors like:
    In file included from /usr/local/google/home/ukai/chrome/src/chrome/browser/chromeos/pipe_reader.cc:5:
    /usr/local/google/home/ukai/chrome/src/chrome/browser/chromeos/pipe_reader.h: In destructor 'virtual PipeReader::~PipeReader()':
    /usr/local/google/home/ukai/chrome/src/chrome/browser/chromeos/pipe_reader.h:33: error: 'fclose' was not declared in this scope
    /usr/local/google/home/ukai/chrome/src/chrome/browser/chromeos/pipe_reader.cc: In member function 'virtual std::string PipeReader::Read(uint32)':
    /usr/local/google/home/ukai/chrome/src/chrome/browser/chromeos/pipe_reader.cc:11: error: 'fopen' was not declared in this scope
    /usr/local/google/home/ukai/chrome/src/chrome/browser/chromeos/pipe_reader.cc:12: error: 'fgets' was not declared in this scope
    scons: *** [/usr/local/google/home/ukai/chrome/src/sconsbuild/Debug/obj/chrome/browser/chromeos/pipe_reader.o] Error 1
    
    Review URL: http://codereview.chromium.org/199046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25693 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/pipe_reader.h
+++ chrome/browser/chromeos/pipe_reader.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_PIPE_READER_H_
 
 #include <fcntl.h>
+#include <stdio.h>
 #include <string>
 #include <sys/stat.h>
 #include <sys/types.h>
