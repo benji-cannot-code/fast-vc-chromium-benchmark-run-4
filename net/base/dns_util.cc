9069adf10bd6d603fd1c3f508eb03cb87a427db0
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-09 02:01:10 +0000

    Include fixes for gcc >= 4.3
    Patch by Joel Stanley <joel@jms.id.au>
    
    Review URL: http://codereview.chromium.org/197055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25699 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/dns_util.cc
+++ net/base/dns_util.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/base/dns_util.h"
 
+#include <cstring>
+
 namespace net {
 
 // Based on DJB's public domain code.
