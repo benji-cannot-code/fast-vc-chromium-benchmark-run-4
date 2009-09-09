987c5740c923a609f7b59a91d84557f68ae7a8ef
gman@google.com <gman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-09 22:45:15 +0000

    Make more stuff private in renderer
    Review URL: http://codereview.chromium.org/199066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25805 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/renderer.cc
+++ o3d/core/cross/renderer.cc
@@ -100,12 +100,12 @@ bool Renderer::IsForceSoftwareRenderer() {
 }
 
 Renderer::Renderer(ServiceLocator* service_locator)
-    : current_render_surface_(NULL),
-      current_depth_surface_(NULL),
-      current_render_surface_is_back_buffer_(true),
-      service_locator_(service_locator),
+    : service_locator_(service_locator),
       service_(service_locator, this),
       features_(service_locator),
+      current_render_surface_(NULL),
+      current_depth_surface_(NULL),
+      current_render_surface_is_back_buffer_(true),
       viewport_(0.0f, 0.0f, 1.0f, 1.0f),
       depth_range_(0.0f, 1.0f),
       write_mask_(0xf),
@@ -252,9 +252,8 @@ void Renderer::SetClientSize(int width, int height) {
 
 bool Renderer::StartRendering() {
   DCHECK_GE(start_depth_, 0);
-  ++start_depth_;
   bool result = true;
-  if (start_depth_ == 1) {
+  if (start_depth_ == 0) {
     ++render_frame_count_;
     rendering_ = true;
     transforms_culled_ = 0;
@@ -277,6 +276,9 @@ bool Renderer::StartRendering() {
         Clear(Float4(0.5f, 0.5f, 0.5f, 1.0f), true, 1.0f, true, 0, true);
       }
     }
+  }
+  if (result) {
+    ++start_depth_;
   }
   return result;
 }
@@ -309,6 +311,9 @@ void Renderer::FinishRendering() {
   if (start_depth_ == 0) {
     ApplyDirtyStates();
     PlatformSpecificFinishRendering();
+    // Don't hold pointers to these when we are finished rendering.
+    current_render_surface_ = NULL;
+    current_depth_surface_ = NULL;
     rendering_ = false;
   }
 }
