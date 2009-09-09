5dcc7956a1b496b976e3908d3c69929afff5f6f4
sidchat@google.com <sidchat@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-09 18:51:44 +0000

    Make BrowserView layout both extension shelf and bookmarks bar if toolstrips change in the extension shelf.
    BUG=21269
    BUG=21270
    BUG=21274
    TEST=none
    Review URL: http://codereview.chromium.org/192041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25760 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/extensions/extension_shelf.cc
+++ chrome/browser/views/extensions/extension_shelf.cc
@@ -706,7 +706,8 @@ void ExtensionShelf::ChildPreferredSizeChanged(View* child) {
   Toolstrip *toolstrip = ToolstripForView(static_cast<ExtensionView*>(child));
   if (!toolstrip)
     return;
-  Layout();
+
+  PreferredSizeChanged();
 }
 
 void ExtensionShelf::Layout() {
