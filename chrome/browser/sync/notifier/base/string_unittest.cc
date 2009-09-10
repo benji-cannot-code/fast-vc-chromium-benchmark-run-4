15b1ddafe18667b6e89aac5bba2dff2f30b10dfa
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-10 07:19:12 +0000

    Fix checkdeps by removing some include paths and adding a DEPS rule to allow "talk/".
    This code is not yet compiled,
    but it is still subject to checkdeps.
    
    TEST=ran checkdeps.py
    BUG=none
    
    Review URL: http://codereview.chromium.org/201077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25851 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/notifier/base/string_unittest.cc
+++ chrome/browser/sync/notifier/base/string_unittest.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/sync/notifier/base/string.h"
-#include "notifier/testing/notifier/unittest.h"
 
 namespace notifier {
 
