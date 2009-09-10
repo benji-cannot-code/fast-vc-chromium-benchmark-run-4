df41fc6e01fa8f85226429e4657e466eded9643c
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-10 05:25:20 +0000

    Remove a NOTIMPLEMENTED that was causing layout tests to fail on the Mac.
    For some reason these just started acting up now.
    
    TBR=eroman
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/196075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25844 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_mac.mm
+++ webkit/tools/test_shell/test_shell_mac.mm
@@ -612,7 +612,6 @@ void TestShell::ShowStartupDebuggingDialog() {
 
 StringPiece TestShell::NetResourceProvider(int key) {
   // TODO(port): Return the requested resource.
-  NOTIMPLEMENTED();
   return StringPiece();
 }
 
