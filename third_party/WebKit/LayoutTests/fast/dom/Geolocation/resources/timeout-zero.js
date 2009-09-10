8d40b2df9837c5c143d7f3403a987487e0f10a4a
benm@google.com <benm@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-10 11:32:47 +0000

    WebCore: Correctly applies the Geolocation timeout property. https://bugs.webkit.org/show_bug.cgi?id=27256
    Patch by Steve Block <steveblock@google.com> on 2009-09-09
    Reviewed by Adam Barth.
    
    If a timeout is specified, the timer is started as soon as the location acquistion process starts.
    If a position fix is not obtained within the timeout interval, the position request is cancelled
    and the error callback (if specified) is invoked. If a position fix is obtained within the timeout
    interval, the timer is stopped. Permissions are then requested from the user if required. See
    http://www.w3.org/TR/geolocation-API/#position-options.
    
    Note that this logic means that the time required to obtain permissions from the user is not
    included in the timeout interval.
    
    Tests: fast/dom/Geolocation/timeout.html
           fast/dom/Geolocation/timeout-zero.html
    
    * page/Geolocation.cpp: Modified.
    (WebCore::Geolocation::GeoNotifier::GeoNotifier): Modified. Takes Geolocation object as parameter.
    (WebCore::Geolocation::GeoNotifier::hasZeroTimeout): Added. Determines whether this request has a zero timeout.
    (WebCore::Geolocation::GeoNotifier::startTimerIfNeeded): Renamed from startTimer. Starts the timer if a timeout was specified.
    (WebCore::Geolocation::GeoNotifier::timerFired): Modified. Calls the error callback if specified, and reports the timeout to the Geolocation object.
    (WebCore::Geolocation::getCurrentPosition): Modified. Calls maybeStartTimer when starting the location acquisition process.
    (WebCore::Geolocation::watchPosition): Modified. Calls maybeStartTimer when starting the location acquisition process.
    (WebCore::Geolocation::requestTimedOut): Added. Cancels a request when it times out.
    (WebCore::Geolocation::setIsAllowed): Modified. No longer starts timers.
    (WebCore::Geolocation::sendPosition): Modified. No longer stops timers.
    (WebCore::Geolocation::stopTimer): Added. Stops the timers for a vector of notifiers.
    (WebCore::Geolocation::stopTimersForOneShots): Added. Stops the timers for all one-shots.
    (WebCore::Geolocation::stopTimersForWatchers): Added. Stops the timers for all watchers.
    (WebCore::Geolocation::stopTimers): Added. Stops the timers for all requests.
    (WebCore::Geolocation::geolocationServicePositionChanged): Modfified. Stops the timers for all requests.
    * page/Geolocation.h: Modified.
    (WebCore::Geolocation::GeoNotifier::create): Modified. Takes Geolocation object as parameter.
    
    LayoutTests: Correctly applies the Geolocation timeout property.
    https://bugs.webkit.org/show_bug.cgi?id=27256
    
    Patch by Steve Block <steveblock@google.com> on 2009-09-09
    Reviewed by Adam Barth.
    
    * fast/dom/Geolocation/resources/timeout.js: Added. Tests that when a non-zero timeout is used, the success callback is invoked as expected.
    * fast/dom/Geolocation/timeout.html: Added. Wrapper for above test.
    * fast/dom/Geolocation/timeout-expected.txt: Added. Expected result for above test.
    * fast/dom/Geolocation/resources/timeout-zero.js: Added. Tests that when a zero timeout is used, the error callback is invoked with code TIMEOUT.
    * fast/dom/Geolocation/timeout-zero.html: Added. Wrapper for above test.
    * fast/dom/Geolocation/timeout-zero-expected.txt: Added. Expected result for above test.
    * platform/gtk/Skipped: Modified. Skips the above tests.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48249 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/Geolocation/resources/timeout-zero.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Tests that when timeout is zero (and maximumAge is too), the error callback is called immediately with code TIMEOUT.");
+
+layoutTestController.setMockGeolocationPosition(51.478, -0.166, 100.0);
+
+var error;
+navigator.geolocation.getCurrentPosition(function(p) {
+    testFailed('Success callback invoked unexpectedly');
+    window.layoutTestController.notifyDone();
+}, function(e) {
+    error = e
+    shouldBe('error.code', 'error.TIMEOUT');
+    shouldBe('error.message', '"Timeout expired"');
+    shouldBe('error.UNKNOWN_ERROR', '0');
+    shouldBe('error.PERMISSION_DENIED', '1');
+    shouldBe('error.POSITION_UNAVAILABLE', '2');
+    shouldBe('error.TIMEOUT', '3');
+    debug('<br /><span class="pass">TEST COMPLETE</span>');
+    window.layoutTestController.notifyDone();
+}, {
+    timeout: 0
+});
+window.layoutTestController.waitUntilDone();
