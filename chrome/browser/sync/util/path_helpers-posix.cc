c884ab1afaea49148c66af77ca6b7d7bcc55c09b
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-10 07:42:43 +0000

    Fix one more checkdeps path.  Left this out of the last change.
    TEST=checkdeps.py
    BUG=none
    
    Review URL: http://codereview.chromium.org/200076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25853 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/util/path_helpers-posix.cc
+++ chrome/browser/sync/util/path_helpers-posix.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string.h>
 
-#include "base/port.h"
 #include "chrome/browser/sync/util/path_helpers.h"
-#include "strings/strutil.h"
 
 #if ((!defined(OS_LINUX)) && (!defined(OS_MACOSX)))
 #error Compile this file on Mac OS X or Linux only.
