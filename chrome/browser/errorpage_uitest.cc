efd135040810dc8413860ee8a13071b8664e3d2f
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-10 17:50:01 +0000

    Get more info in the logs when ErrorPageTest fails.
    TEST=none
    BUG=19361
    
    Review URL: http://codereview.chromium.org/199076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25875 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/errorpage_uitest.cc
+++ chrome/browser/errorpage_uitest.cc
@@ -18,6 +18,7 @@ class ErrorPageTest : public UITest {
         return true;
       PlatformThread::Sleep(sleep_timeout_ms() / 10);
     }
+    EXPECT_EQ(title, GetActiveTabTitle());
     return false;
   }
   bool WaitForTitleContaining(const std::string& title_substring) {
@@ -27,6 +28,8 @@ class ErrorPageTest : public UITest {
         return true;
       PlatformThread::Sleep(sleep_timeout_ms() / 10);
     }
+    LOG(ERROR) << "Could not find " << title_substring << " in "
+               << GetActiveTabTitle();
     return false;
   }
 };
