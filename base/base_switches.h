01cf6400c2afb8a8a42d4f972595ed26a9fec97e
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-10 18:35:16 +0000

    Strict transport security: come out from behind the flag.
    With this change, Strict Transport Security is active by default and
    doesn't need the --force-https flag any more.
    
    http://codereview.chromium.org/193067
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25888 0039d316-1c4b-4281-b951-d872f2087c98

--- base/base_switches.h
+++ base/base_switches.h
@@ -18,7 +18,6 @@ extern const wchar_t kNoErrorDialogs[];
 extern const wchar_t kProcessType[];
 extern const wchar_t kEnableDCHECK[];
 extern const wchar_t kNoMessageBox[];
-extern const wchar_t kForceHTTPS[];
 
 }  // namespace switches
 
