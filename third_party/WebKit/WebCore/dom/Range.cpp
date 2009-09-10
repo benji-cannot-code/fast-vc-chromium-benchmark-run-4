c2d2909997865a2ad6783da90c8cb26b0d414aec
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-10 23:00:58 +0000

    2009-09-10  Xiaomei Ji  <xji@chromium.org>
            Reviewed by Eric Seidel.
    
            Implement Range.expand().
            https://bugs.webkit.org/show_bug.cgi?id=27632
    
            * fast/dom/Range/range-expand-expected.txt: Added.
            * fast/dom/Range/range-expand.html: Added.
            * fast/dom/Window/window-properties-expected.txt:
    2009-09-10  Xiaomei Ji  <xji@chromium.org>
    
            Reviewed by Eric Seidel.
    
            Implment Range.expand().
            https://bugs.webkit.org/show_bug.cgi?id=27632
    
            Test: fast/dom/Range/range-expand.html
    
            * dom/Range.cpp:
            (WebCore::Range::expand):
            * dom/Range.h:
            * dom/Range.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Range.cpp
+++ third_party/WebKit/WebCore/dom/Range.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Text.h"
 #include "TextIterator.h"
 #include "VisiblePosition.h"
+#include "htmlediting.h"
 #include "markup.h"
 #include "visible_units.h"
 #include <stdio.h>
@@ -1797,6 +1798,28 @@ void Range::textNodeSplit(Text* oldNode)
     ASSERT(oldNode->nextSibling()->isTextNode());
     boundaryTextNodesSplit(m_start, oldNode);
     boundaryTextNodesSplit(m_end, oldNode);
+}
+
+void Range::expand(const String& unit, ExceptionCode& ec)
+{
+    VisiblePosition start(startPosition());
+    VisiblePosition end(endPosition());
+    if (unit == "word") {
+        start = startOfWord(start);
+        end = endOfWord(end);
+    } else if (unit == "sentence") {
+        start = startOfSentence(start);
+        end = endOfSentence(end);
+    } else if (unit == "block") {
+        start = startOfParagraph(start);
+        end = endOfParagraph(end);
+    } else if (unit == "document") {
+        start = startOfDocument(start);
+        end = endOfDocument(end);
+    } else
+        return;
+    setStart(start.deepEquivalent().containerNode(), start.deepEquivalent().computeOffsetInContainerNode(), ec);
+    setEnd(end.deepEquivalent().containerNode(), end.deepEquivalent().computeOffsetInContainerNode(), ec);
 }
 
 }
