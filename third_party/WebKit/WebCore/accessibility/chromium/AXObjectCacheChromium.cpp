ddef4104a205423577ea94cd03dd4d19837a37df
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-10 06:38:31 +0000

    AX notifications should be an ENUM type instead of strings https://bugs.webkit.org/show_bug.cgi?id=28963
    Reviewed by Darin Adler.
    
    Moves AX notifications over to an ENUM type instead of
    using the actual string values that are used on OS X.
    
    * accessibility/AXObjectCache.cpp:
    (WebCore::AXObjectCache::postNotification):
    (WebCore::AXObjectCache::selectedChildrenChanged):
    * accessibility/AXObjectCache.h:
    (WebCore::AXObjectCache::):
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::handleActiveDescendantChanged):
    * accessibility/chromium/AXObjectCacheChromium.cpp:
    (WebCore::AXObjectCache::postPlatformNotification):
    * accessibility/gtk/AXObjectCacheAtk.cpp:
    (WebCore::AXObjectCache::postPlatformNotification):
    * accessibility/mac/AXObjectCacheMac.mm:
    (WebCore::AXObjectCache::postPlatformNotification):
    * accessibility/win/AXObjectCacheWin.cpp:
    (WebCore::AXObjectCache::postPlatformNotification):
    * dom/Document.cpp:
    (WebCore::Document::implicitClose):
    * dom/Element.cpp:
    (WebCore::Element::updateAfterAttributeChanged):
    * editing/Editor.cpp:
    (WebCore::Editor::respondToChangedContents):
    * editing/mac/SelectionControllerMac.mm:
    (WebCore::SelectionController::notifyAccessibilityForSelectionChange):
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::setChecked):
    * page/FrameView.cpp:
    (WebCore::FrameView::layout):
    * rendering/RenderTextControl.cpp:
    (WebCore::RenderTextControl::setInnerTextValue):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48244 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/chromium/AXObjectCacheChromium.cpp
+++ third_party/WebKit/WebCore/accessibility/chromium/AXObjectCacheChromium.cpp
@@ -44,7 +44,7 @@ void AXObjectCache::attachWrapper(AccessibilityObject*)
     // In Chromium, AccessibilityObjects are wrapped lazily.
 }
 
-void AXObjectCache::postPlatformNotification(AccessibilityObject*, const String&)
+void AXObjectCache::postPlatformNotification(AccessibilityObject*, AXNotification)
 {
 }
 
--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -650,7 +650,7 @@ void FrameView::layout(bool allowSubtree)
 
 #if PLATFORM(MAC)
     if (AXObjectCache::accessibilityEnabled())
-        root->document()->axObjectCache()->postNotification(root, "AXLayoutComplete", true);
+        root->document()->axObjectCache()->postNotification(root, AXObjectCache::AXLayoutComplete, true);
 #endif
 #if ENABLE(DASHBOARD_SUPPORT)
     updateDashboardRegions();
