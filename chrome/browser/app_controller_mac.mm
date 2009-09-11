4eb183937b21352492b8aa52a006879bee83d579
sgk@google.com <sgk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-11 22:40:02 +0000

    Revert 26016 - valgrind leak - Add "New Window" and "New Incognito Window" items to the Dock Menu. Add a unit test module for AppController with a simple DockMenu test. BUG=none TEST=none Review URL: http://codereview.chromium.org/199031
    TBR=sgk@google.com
    Review URL: http://codereview.chromium.org/196101
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26039 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/app_controller_mac.mm
+++ chrome/browser/app_controller_mac.mm
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)windowLayeringDidChange:(NSNotification*)inNotification;
 - (BOOL)userWillWaitForInProgressDownloads:(int)downloadCount;
 - (BOOL)shouldQuitWithInProgressDownloads;
-- (NSMenu*)applicationDockMenu:(NSApplication*)sender;
 @end
 
 @implementation AppController
@@ -666,32 +665,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (![[aboutController_ window] isVisible])
     [[aboutController_ window] center];
   [aboutController_ showWindow:self];
-}
-
-- (NSMenu*)applicationDockMenu:(id)sender {
-  NSMenu* result = [[[NSMenu alloc] initWithTitle: @""] autorelease];
-  NSString* titleStr;
-  id item;
-
-  titleStr = base::SysWideToNSString(
-      l10n_util::GetString(IDS_NEW_WINDOW_MAC));
-  item = [[[NSMenuItem alloc] initWithTitle:titleStr
-                                     action:@selector(commandDispatch:)
-                              keyEquivalent:@""] autorelease];
-  [item setTarget:self];
-  [item setTag:IDC_NEW_WINDOW];
-  [result addItem:item];
-
-  titleStr = base::SysWideToNSString(
-      l10n_util::GetString(IDS_NEW_INCOGNITO_WINDOW_MAC));
-  item = [[[NSMenuItem alloc] initWithTitle:titleStr
-                                     action:@selector(commandDispatch:)
-                              keyEquivalent:@""] autorelease];
-  [item setTarget:self];
-  [item setTag:IDC_NEW_INCOGNITO_WINDOW];
-  [result addItem:item];
-
-  return result;
 }
 
 @end
