975f751468a61e0ca4c00f8c047090b872c434fb
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-11 00:33:38 +0000

    Add command line switch --activate-on-launch to help Selenium. Implemented for Mac; behavior implicit on Windows.
    If you do NOT get an "activate on launch" behavior on Windows, it is
    because Selenium is not the current foreground app.  See
    http://msdn.microsoft.com/en-us/library/ms633539(VS.85).aspx 
    for details on foreground behavior.  This isn't something we can fix
    in Chrome on Windows.
    
    BUG=http://crbug.com/20935 
    TEST=Launch Chrome from the command line.
    E.g. "..../Chromium.app/Contents/MacOS/Chromium".  See it NOT gain
    foreground.  Add --activate-on-launch.  DO see it gain foreground.
    
    Review URL: http://codereview.chromium.org/202053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25958 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/app_controller_mac.mm
+++ chrome/browser/app_controller_mac.mm
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/download_manager.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/sessions/tab_restore_service.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/pref_service.h"
 #include "chrome/browser/profile_manager.h"
@@ -109,6 +110,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Set up the command updater for when there are no windows open
   [self initMenuState];
+
+  // Activate (bring to foreground) if asked to do so.  On
+  // Windows this logic isn't necessary since
+  // BrowserWindow::Activate() calls ::SetForegroundWindow() which is
+  // adequate.  On Mac, BrowserWindow::Activate() calls -[NSWindow
+  // makeKeyAndOrderFront:] which does not activate the application
+  // itself.
+  const CommandLine& parsed_command_line = *CommandLine::ForCurrentProcess();
+  if (parsed_command_line.HasSwitch(switches::kActivateOnLaunch)) {
+    [NSApp activateIgnoringOtherApps:YES];
+  }
 }
 
 // Checks if there are any tabs with sheets open, and if so, raises one of
