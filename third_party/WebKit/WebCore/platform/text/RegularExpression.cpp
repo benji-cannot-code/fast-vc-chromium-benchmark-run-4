68c0c8938b4a14dad13b14f42e5ab3a2bbe052d9
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-11 08:43:32 +0000

    2009-09-11  Yongjun Zhang  <yongjun.zhang@nokia.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=29132
            Symbian winscw compiler fails compiling WebCore/text/RegularExpression.cpp.
    
            Add RegularExpression class qualifier before Private.
    
            * platform/text/RegularExpression.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48290 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/RegularExpression.cpp
+++ third_party/WebKit/WebCore/platform/text/RegularExpression.cpp
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class RegularExpression::Private : public RefCounted<Private> {
+class RegularExpression::Private : public RefCounted<RegularExpression::Private> {
 public:
     static PassRefPtr<Private> create(const String& pattern, TextCaseSensitivity);
     ~Private();
