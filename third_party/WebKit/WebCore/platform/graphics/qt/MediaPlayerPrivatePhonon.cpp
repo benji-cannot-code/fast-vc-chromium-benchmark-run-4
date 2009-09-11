e28b5985a09820f269dab07421edfb166bf235b4
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-11 08:12:34 +0000

    Fix the Qt build, remove stray semicolon from function definition.
    Patch by Simon Hausmann <simon.hausmann@nokia.com> on 2009-09-11
    * platform/graphics/qt/MediaPlayerPrivatePhonon.cpp:
    (WebCore::MediaPlayerPrivate::buffered):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48286 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/MediaPlayerPrivatePhonon.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/MediaPlayerPrivatePhonon.cpp
@@ -262,7 +262,7 @@ void MediaPlayerPrivate::setEndTime(float endTime)
     notImplemented();
 }
 
-PassRefPtr<TimeRanges> MediaPlayerPrivate::buffered() const;
+PassRefPtr<TimeRanges> MediaPlayerPrivate::buffered() const
 {
     notImplemented();
     return TimeRanges::create();
