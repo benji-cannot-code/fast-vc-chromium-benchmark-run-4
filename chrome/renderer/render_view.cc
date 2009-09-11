e7ba27d6848201689f8bdb9e798c7a2401f9b267
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-11 18:04:59 +0000

    Eliminate some methods.
    Removes WebFrame::reportFindInPageSelection since it was only
    ever called by WebFrameImpl.  Now it is a private method.
    
    Removes WebViewDelegate::DownloadUrl since it can be expressed
    using WebFrameClient::loadURLExternally.
    
    R=dglazkov
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/201092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25987 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -1626,9 +1626,12 @@ void RenderView::willClose(WebFrame* frame) {
 void RenderView::loadURLExternally(
     WebFrame* frame, const WebURLRequest& request,
     WebNavigationPolicy policy) {
-  OpenURL(request.url(),
-          GURL(request.httpHeaderField(WebString::fromUTF8("Referer"))),
-          policy);
+  GURL referrer(request.httpHeaderField(WebString::fromUTF8("Referer")));
+  if (policy == WebKit::WebNavigationPolicyDownload) {
+    Send(new ViewHostMsg_DownloadUrl(routing_id_, request.url(), referrer));
+  } else {
+    OpenURL(request.url(), referrer, policy);
+  }
 }
 
 WebNavigationPolicy RenderView::decidePolicyForNavigation(
@@ -2659,10 +2662,6 @@ void RenderView::SetTooltipText(WebView* webview,
                                 WebTextDirection text_direction_hint) {
   Send(new ViewHostMsg_SetTooltipText(routing_id_, tooltip_text,
                                       text_direction_hint));
-}
-
-void RenderView::DownloadUrl(const GURL& url, const GURL& referrer) {
-  Send(new ViewHostMsg_DownloadUrl(routing_id_, url, referrer));
 }
 
 void RenderView::UpdateInspectorSettings(const std::wstring& raw_settings) {
