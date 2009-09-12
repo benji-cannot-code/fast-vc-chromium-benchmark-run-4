62b1f4541a9fe5a7a496c12bc38e054917383d32
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-12 01:28:51 +0000

    Clean up a few things about the first run bubble code: * Constants at top didn't need "Static" since they were already in an anonymous namespace * Move function definitions out from declarations.  This avoids auto-inlining these (sometimes large) functions and makes it easier to see what a class contains. * Now that I could see the class functions, make as many as possible private. * Make sure definition order and declaration order match.
    This shouldn't make any functional changes.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/202065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26069 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/first_run_bubble.h
+++ chrome/browser/views/first_run_bubble.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,19 +20,14 @@ class FirstRunBubble : public InfoBubble,
                               const gfx::Rect& position_relative_to,
                               bool use_OEM_bubble);
 
-  FirstRunBubble()
-      : has_been_activated_(false),
-        ALLOW_THIS_IN_INITIALIZER_LIST(enable_window_method_factory_(this)),
-        view_(NULL) {
-  }
-
-  virtual ~FirstRunBubble() {
-    // We should have called RevokeAll on the method factory already.
-    DCHECK(enable_window_method_factory_.empty());
-    enable_window_method_factory_.RevokeAll();
-  }
+ private:
+  FirstRunBubble();
+  virtual ~FirstRunBubble();
 
   void set_view(FirstRunBubbleViewBase* view) { view_ = view; }
+
+  // Re-enable the parent window.
+  void EnableParent();
 
 #if defined(OS_WIN)
   // Overridden from InfoBubble:
@@ -43,10 +38,6 @@ class FirstRunBubble : public InfoBubble,
   virtual void InfoBubbleClosing(InfoBubble* info_bubble,
                                  bool closed_by_escape);
   virtual bool CloseOnEscape() { return true; }
-
- private:
-  // Re-enable the parent window.
-  void EnableParent();
 
   // Whether we have already been activated.
   bool has_been_activated_;
