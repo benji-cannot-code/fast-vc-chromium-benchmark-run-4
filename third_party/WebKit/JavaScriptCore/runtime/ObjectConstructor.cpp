055416fc8c9b00b0a431ebc902cea6b06e3f49c7
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-13 03:00:49 +0000

    Fix non-allinonefile builds
    git-svn-id: svn://svn.chromium.org/blink/trunk@48337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/ObjectConstructor.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/ObjectConstructor.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ObjectPrototype.h"
 #include "PropertyDescriptor.h"
 #include "PrototypeFunction.h"
+#include "PrototypeNameArray.h"
 
 namespace JSC {
 
