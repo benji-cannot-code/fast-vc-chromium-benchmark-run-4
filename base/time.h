d190e9b5351aa1ebdc36dccad46627b3537584ca
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-14 18:49:03 +0000

    Make int64 -> Time conversion explicit.
    Landing Patch for Jacob Mandelson, original review: http://codereview.chromium.org/200093
    
    BUG=none
    TEST=base_unittests & app_unittests
    
    Review URL: http://codereview.chromium.org/205003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26134 0039d316-1c4b-4281-b951-d872f2087c98

--- base/time.h
+++ base/time.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -314,10 +314,10 @@ class Time {
 
   // Return a new time modified by some delta.
   Time operator+(TimeDelta delta) const {
-    return us_ + delta.delta_;
+    return Time(us_ + delta.delta_);
   }
   Time operator-(TimeDelta delta) const {
-    return us_ - delta.delta_;
+    return Time(us_ - delta.delta_);
   }
 
   // Comparison operators
@@ -351,7 +351,7 @@ class Time {
   // |is_local = true| or UTC |is_local = false|.
   static Time FromExploded(bool is_local, const Exploded& exploded);
 
-  Time(int64 us) : us_(us) {
+  explicit Time(int64 us) : us_(us) {
   }
 
   // The representation of Jan 1, 1970 UTC in microseconds since the
