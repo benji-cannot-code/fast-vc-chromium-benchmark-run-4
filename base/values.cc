3693b1ed60e3251b36adde6b9ee157877f35af79
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-14 16:56:12 +0000

    Landing tfarina patch. See http://codereview.chromium.org/195040/show
    Fix some styles issues reported by lint in base\values.h. 
    
    BUG=None 
    TEST=None
    Review URL: http://codereview.chromium.org/200112
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26113 0039d316-1c4b-4281-b951-d872f2087c98

--- base/values.cc
+++ base/values.cc
@@ -185,7 +185,7 @@ bool StringValue::Equals(const Value* other) const {
 
 ///////////////////// BinaryValue ////////////////////
 
-//static
+// static
 BinaryValue* BinaryValue::Create(char* buffer, size_t size) {
   if (!buffer)
     return NULL;
@@ -193,7 +193,7 @@ BinaryValue* BinaryValue::Create(char* buffer, size_t size) {
   return new BinaryValue(buffer, size);
 }
 
-//static
+// static
 BinaryValue* BinaryValue::CreateWithCopiedBuffer(char* buffer, size_t size) {
   if (!buffer)
     return NULL;
