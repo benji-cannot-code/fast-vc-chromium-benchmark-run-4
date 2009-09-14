44c0a3e1cd047c5556bf0ac01df2cb8d2fc032eb
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-14 19:36:31 +0000

    Fix to use FilePath in more unittests.
    Landing patch for Thiago Farina, original review: http://codereview.chromium.org/187005
    
    BUG=None
    TEST=run unit_tests.exe, base_unittests.exe.
    
    Review URL: http://codereview.chromium.org/193101
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26142 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/privacy_blacklist/blacklist_io_unittest.cc
+++ chrome/browser/privacy_blacklist/blacklist_io_unittest.cc
@@ -17,11 +17,9 @@ TEST(BlacklistIOTest, Generic) {
   FilePath data_dir;
   PathService::Get(chrome::DIR_TEST_DATA, &data_dir);
 
-  FilePath input =
-      data_dir.Append(FilePath::FromWStringHack(L"blacklist_small.pbl"));
+  FilePath input = data_dir.AppendASCII("blacklist_small.pbl");
 
-  FilePath expected =
-      data_dir.Append(FilePath::FromWStringHack(L"blacklist_small.pbr"));
+  FilePath expected = data_dir.AppendASCII("blacklist_small.pbr");
 
   BlacklistIO io;
   EXPECT_TRUE(io.Read(input));
@@ -41,7 +39,7 @@ TEST(BlacklistIOTest, Generic) {
 
   FilePath output;
   PathService::Get(base::DIR_TEMP, &output);
-  output = output.Append(FilePath::FromWStringHack(L"blacklist_small.pbr"));
+  output = output.AppendASCII("blacklist_small.pbr");
   CHECK(io.Write(output));
   EXPECT_TRUE(file_util::ContentsEqual(output, expected));
   EXPECT_TRUE(file_util::Delete(output, false));
@@ -51,16 +49,13 @@ TEST(BlacklistIOTest, Combine) {
   // Testing data path.
   FilePath data_dir;
   PathService::Get(chrome::DIR_TEST_DATA, &data_dir);
-  data_dir = data_dir.Append(FilePath::FromWStringHack(L"blacklist_samples"));
+  data_dir = data_dir.AppendASCII("blacklist_samples");
 
-  FilePath input1 =
-      data_dir.Append(FilePath::FromWStringHack(L"annoying_ads.pbl"));
+  FilePath input1 = data_dir.AppendASCII("annoying_ads.pbl");
 
-  FilePath input2 =
-    data_dir.Append(FilePath::FromWStringHack(L"block_flash.pbl"));
+  FilePath input2 = data_dir.AppendASCII("block_flash.pbl");
 
-  FilePath input3 =
-    data_dir.Append(FilePath::FromWStringHack(L"session_cookies.pbl"));
+  FilePath input3 = data_dir.AppendASCII("session_cookies.pbl");
 
   BlacklistIO io;
   EXPECT_TRUE(io.Read(input1));
@@ -95,10 +90,9 @@ TEST(BlacklistIOTest, Combine) {
 
   FilePath output;
   PathService::Get(base::DIR_TEMP, &output);
-  output = output.Append(FilePath::FromWStringHack(L"combine3.pbr"));
+  output = output.AppendASCII("combine3.pbr");
 
-  FilePath expected =
-      data_dir.Append(FilePath::FromWStringHack(L"combine3.pbr"));
+  FilePath expected = data_dir.AppendASCII("combine3.pbr");
 
   CHECK(io.Write(output));
   EXPECT_TRUE(file_util::ContentsEqual(output, expected));
