b52dde0332bbcb2942ae014dace99949fb7ae8d2
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-14 18:41:54 +0000

    Try to fix the Linux Views build.
    TBR=sky
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/203053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26130 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/info_bubble.h
+++ chrome/browser/views/info_bubble.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_VIEWS_INFO_BUBBLE_H_
 #define CHROME_BROWSER_VIEWS_INFO_BUBBLE_H_
 
-#include "build/build_config.h"
+#include "views/view.h"
 
 #if defined(OS_WIN)
 #include "views/widget/widget_win.h"
