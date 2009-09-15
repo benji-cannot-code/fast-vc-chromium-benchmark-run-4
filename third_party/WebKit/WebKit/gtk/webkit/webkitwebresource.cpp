f769c132155837fbdc88fe4488c1f693be8cfbb4
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-15 02:11:24 +0000

    2009-09-14  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
            Reviewed by Xan Lopez and Jan Alonzo.
    
            https://bugs.webkit.org/show_bug.cgi?id=29134
            [GTK] Add API to access sub resources
    
            Implement getting subresources, and improve testing of
            main, and sub resources loading.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48382 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebresource.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebresource.cpp
@@ -231,6 +231,18 @@ WebKitWebResource* webkit_web_resource_new_with_core_resource(PassRefPtr<Archive
     return webResource;
 }
 
+void webkit_web_resource_init_with_core_resource(WebKitWebResource* webResource, PassRefPtr<ArchiveResource> resource)
+{
+    ASSERT(resource);
+
+    WebKitWebResourcePrivate* priv = webResource->priv;
+
+    if (priv->resource)
+        priv->resource->deref();
+
+    priv->resource = resource.releaseRef();
+}
+
 /**
  * webkit_web_resource_new:
  * @data: the data to initialize the #WebKitWebResource
@@ -284,6 +296,9 @@ GString* webkit_web_resource_get_data(WebKitWebResource* webResource)
     g_return_val_if_fail(WEBKIT_IS_WEB_RESOURCE(webResource), NULL);
 
     WebKitWebResourcePrivate* priv = webResource->priv;
+
+    if (!priv->resource)
+        return NULL;
 
     if (!priv->data)
         priv->data = g_string_new_len(priv->resource->data()->data(), priv->resource->data()->size());
