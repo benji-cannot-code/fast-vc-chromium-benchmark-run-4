c8fae677fbbaf36015aba191f9940963ed15006b
erikkay@chromium.org <erikkay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-15 21:22:56 +0000

    Handle mole/toolstrip URLs properly. * expand/collapse to chrome-extension://crashme no longer crashes * expand/collapse to a relative URL now works
    BUG=20412,21905
    TEST=browser_tests ExtensionApi.Toolstrip
    (note that the test doesn't actually exercise these changes due to 21905)
    
    Review URL: http://codereview.chromium.org/195093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26266 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_toolstrip_api.cc
+++ chrome/browser/extensions/extension_toolstrip_api.cc
@@ -83,14 +83,13 @@ bool ToolstripExpandFunction::RunImpl() {
     return false;
   }
 
-
   GURL url;
   if (args->HasKey(keys::kUrlKey)) {
     std::string url_string;
     EXTENSION_FUNCTION_VALIDATE(args->GetString(keys::kUrlKey,
                                                 &url_string));
-    url = GURL(url_string);
-    if (!url.is_valid() && !url.is_empty()) {
+    url = dispatcher()->url().Resolve(url_string);
+    if (!url.is_valid()) {
       error_ = kInvalidURLError;
       return false;
     }
@@ -118,8 +117,8 @@ bool ToolstripCollapseFunction::RunImpl() {
       std::string url_string;
       EXTENSION_FUNCTION_VALIDATE(args->GetString(keys::kUrlKey,
                                                   &url_string));
-      url = GURL(url_string);
-      if (!url.is_valid() && !url.is_empty()) {
+      url = dispatcher()->url().Resolve(url_string);
+      if (!url.is_valid()) {
         error_ = kInvalidURLError;
         return false;
       }
