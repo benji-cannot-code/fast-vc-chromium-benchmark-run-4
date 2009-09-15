ff237ad4d8ad67dfb076999d6faba1d6948bc6ef
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-15 02:08:46 +0000

    Java
    git-svn-id: svn://svn.chromium.org/blink/trunk@48381 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/GOwnPtr.h
+++ third_party/WebKit/JavaScriptCore/wtf/GOwnPtr.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <glib.h>
 #include <wtf/Assertions.h>
 #include <wtf/Noncopyable.h>
+#include <gio/gio.h>
 
 namespace WTF {
     template <typename T> inline void freeOwnedGPtr(T* ptr) { g_free(reinterpret_cast<void*>(ptr)); }
@@ -37,6 +38,7 @@ namespace WTF {
     template<> void freeOwnedGPtr<GPatternSpec>(GPatternSpec*);
     template<> void freeOwnedGPtr<GDir>(GDir*);
     template<> void freeOwnedGPtr<GHashTable>(GHashTable*);
+    template<> void freeOwnedGPtr<GFile>(GFile*);
 
     template <typename T> class GOwnPtr : public Noncopyable {
     public:
