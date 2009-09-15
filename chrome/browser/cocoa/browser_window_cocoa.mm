65506dedb5f875d9716f341916a0ee77998a7d78
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-15 22:15:08 +0000

    Some scaffolding for the task manager.
    BUG=13156
    TEST=Connect view->task manager to first responder's commandDispatch: and give the menu item the tag 40005. Now clicking that menu item should bring up a completely unfunctional task manager window (but it has a localized title and button).
    
    Review URL: http://codereview.chromium.org/200094
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26278 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_window_cocoa.mm
+++ chrome/browser/cocoa/browser_window_cocoa.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/cocoa/clear_browsing_data_controller.h"
 #import "chrome/browser/cocoa/download_shelf_controller.h"
 #include "chrome/browser/cocoa/page_info_window_mac.h"
+#include "chrome/browser/cocoa/task_manager_mac.h"
 #include "chrome/browser/browser.h"
 #include "chrome/browser/download/download_shelf.h"
 #include "chrome/common/notification_service.h"
@@ -202,7 +203,7 @@ void BrowserWindowCocoa::ShowAboutChromeDialog() {
 }
 
 void BrowserWindowCocoa::ShowTaskManager() {
-  NOTIMPLEMENTED();
+  TaskManagerMac::Show();
 }
 
 void BrowserWindowCocoa::ShowBookmarkManager() {
