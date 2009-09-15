0798256c3ec4060b23fcedc1aada6f3390dd522f
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-15 05:39:01 +0000

    Potential fix for http://crbug.com/13111
    The browser_window_controller is destroyed before the window is destroyed.
    In its dealloc, it sends the download shelf an |exiting| message that deletes
    all item controllers, but the items's views might still be visible and hold
    weak references to their controllers. When they are clicked, they would se
    nd a message to free'd memory.
    
    This is all speculation.
    
    BUG=13111
    TEST=Release to dev channel, monitor crash reports. Should go down.
    
    Review URL: http://codereview.chromium.org/193109
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26198 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/download_item_controller.mm
+++ chrome/browser/cocoa/download_item_controller.mm
@@ -62,6 +62,7 @@ class DownloadShelfContextMenuMac : public DownloadShelfContextMenu {
 
 - (void)dealloc {
   [[NSNotificationCenter defaultCenter] removeObserver:self];
+  [[self view] removeFromSuperview];
   [super dealloc];
 }
 
