535a9d630e9f52f66951fb73fdb02e76e239abf7
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-15 23:18:07 +0000

    applicationDockMenu: is used in the unit test, it should not be private Review URL: http://codereview.chromium.org/201127
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26294 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/app_controller_mac.mm
+++ chrome/browser/app_controller_mac.mm
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)windowLayeringDidChange:(NSNotification*)inNotification;
 - (BOOL)userWillWaitForInProgressDownloads:(int)downloadCount;
 - (BOOL)shouldQuitWithInProgressDownloads;
-- (NSMenu*)applicationDockMenu:(NSApplication*)sender;
 @end
 
 @implementation AppController
