302f528aee9ed5be2f5a3e9f92ee8dc8fbad4f21
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-16 21:31:41 +0000

    2009-09-16  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Added a test that checks that margin property shorthand has correct value
            when there is background-repeat property in the style declaration.
    
            https://bugs.webkit.org/show_bug.cgi?id=28973
    
            * fast/backgrounds/repeat/margin-shorthand-expected.txt: Added.
            * fast/backgrounds/repeat/margin-shorthand.html: Added.
            * fast/backgrounds/repeat/resources/TEMPLATE.html: Added.
            * fast/backgrounds/repeat/resources/margin-shorthand.css: Added.
            * fast/backgrounds/repeat/resources/margin-shorthand.js: Added.
            (iconMarginValue):
    2009-09-16  Yury Semikhatsky  <yurys@chromium.org>
    
            Reviewed by Timothy Hatcher.
    
            Always reset m_implicitShorthand to false after parsing
            background-repeat style property otherwise properties
            following after it may be erroneously marked as implicit.
    
            https://bugs.webkit.org/show_bug.cgi?id=28972
            https://bugs.webkit.org/show_bug.cgi?id=28973
    
            Test: fast/backgrounds/repeat/margin-shorthand.html
    
            * css/CSSParser.cpp:
            (WebCore::CSSParser::parseValue):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48436 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/backgrounds/repeat/resources/margin-shorthand.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('Tests that shorthand property value is correct even if' +
+            ' background-repeat property is declared before it in the ' +
+            ' style declaration. It tests regression described in ' + 
+            '<a href="https://bugs.webkit.org/show_bug.cgi?id=28973">this bug</a>.');
+
+function iconMarginValue()
+{
+    var iconDiv = document.getElementById('icon');
+    var rules = window.getMatchedCSSRules(iconDiv,'',false);
+    return rules[1].style.getPropertyValue('margin');
+}
+
+shouldBe('iconMarginValue()', '"0px"');
+
+var successfullyParsed = true;
