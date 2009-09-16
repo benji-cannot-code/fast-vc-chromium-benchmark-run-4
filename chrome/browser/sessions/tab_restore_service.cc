39ef735e01be093acf17f77c90a74130b9079f54
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-16 19:52:04 +0000

    valgrind: memset the window command data structure.  |timestamp| is aligned on a 16 byte boundary leaving 4 bytes of uninitialized data in the middle of the struct.  We write this data to disk, which is a possible security risk.
    BUG=22031
    TEST=TabRestoreUITest.RestoreToDifferentWindow
    Review URL: http://codereview.chromium.org/196144
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26379 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/tab_restore_service.cc
+++ chrome/browser/sessions/tab_restore_service.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <iterator>
+#include <map>
 
 #include "base/scoped_vector.h"
 #include "base/stl_util-inl.h"
@@ -64,7 +65,8 @@ namespace {
 typedef int32 RestoredEntryPayload;
 
 // Payload used for the start of a window close. This is the old struct that is
-// used for backwards compat when it comes to reading the session files.
+// used for backwards compat when it comes to reading the session files. This
+// struct must be POD, because we memset the contents.
 struct WindowPayload {
   SessionID::id_type window_id;
   int32 selected_tab_index;
@@ -78,7 +80,8 @@ struct SelectedNavigationInTabPayload {
   int32 index;
 };
 
-// Payload used for the start of a window close.
+// Payload used for the start of a window close.  This struct must be POD,
+// because we memset the contents.
 struct WindowPayload2 : WindowPayload {
   int64 timestamp;
 };
@@ -503,6 +506,9 @@ SessionCommand* TabRestoreService::CreateWindowCommand(SessionID::id_type id,
                                                        int num_tabs,
                                                        Time timestamp) {
   WindowPayload2 payload;
+  // |timestamp| is aligned on a 16 byte boundary, leaving 4 bytes of
+  // uninitialized memory in the struct.
+  memset(&payload, 0, sizeof(payload));
   payload.window_id = id;
   payload.selected_tab_index = selected_tab_index;
   payload.num_tabs = num_tabs;
