be221e868955cd36d40b6142e1bf251eca32ac97
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-16 00:40:54 +0000

    WAI-ARIA: add support for ranges, including the progressbar, slider, and spinbutton roles https://bugs.webkit.org/show_bug.cgi?id=28841
    Reviewed by Beth Dakin.
    
    WebCore: 
    
    Allow certain ARIA roles to change value with AXIncrement and AXDecrement. 
    Consolidate increment/decrement/changeValue methods. 
    
    Test: accessibility/aria-slider-value-change.html
    
    * accessibility/AccessibilityObject.cpp:
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::increment):
    (WebCore::AccessibilityRenderObject::decrement):
    (WebCore::AccessibilityRenderObject::changeValueByPercent):
    (WebCore::AccessibilityRenderObject::setValue):
    (WebCore::AccessibilityRenderObject::canSetFocusAttribute):
    * accessibility/AccessibilityRenderObject.h:
    * accessibility/AccessibilitySlider.cpp:
    * accessibility/AccessibilitySlider.h:
    * dom/Element.cpp:
    (WebCore::Element::updateAfterAttributeChanged):
    
    LayoutTests: 
    
    * accessibility/aria-slider-value-change-expected.txt: Added.
    * accessibility/aria-slider-value-change.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48405 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AccessibilitySlider.h
+++ third_party/WebKit/WebCore/accessibility/AccessibilitySlider.h
@@ -59,14 +59,10 @@ namespace WebCore {
         virtual float minValueForRange() const;
         virtual AccessibilityOrientation orientation() const;
 
-        virtual void increment();
-        virtual void decrement();
-
     protected:
         AccessibilitySlider(RenderObject*);
 
     private:
-        void changeValue(float /*percentChange*/);
         HTMLInputElement* element() const;
     };
 
