573a604372990d779e7737de85690aaab51dc563
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-16 17:32:11 +0000

    posix: clean up shared memory code
    At first I rewrote this to use the shm_*() family of
    functions, but that doesn't work on OS X.  Now I've just
    cleaned up some bits and added an extra print to help with
    the below bug.
    
    BUG=16371
    
    Review URL: http://codereview.chromium.org/204024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26361 0039d316-1c4b-4281-b951-d872f2087c98

--- base/shared_memory.h
+++ base/shared_memory.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/process.h"
 
+class FilePath;
+
 namespace base {
 
 // SharedMemoryHandle is a platform specific type which represents
@@ -161,8 +163,7 @@ class SharedMemory {
  private:
 #if defined(OS_POSIX)
   bool CreateOrOpen(const std::wstring &name, int posix_flags, size_t size);
-  bool FilenameForMemoryName(const std::wstring &memname,
-                             std::wstring *filename);
+  bool FilePathForMemoryName(const std::wstring& memname, FilePath* path);
   void LockOrUnlockCommon(int function);
 
 #endif
