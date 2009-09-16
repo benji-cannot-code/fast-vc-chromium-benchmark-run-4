ee49a243abffdc0f3c5fa70029e4cb03e2f5ed38
maf@google.com <maf@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-16 00:40:56 +0000

    Fix build breakage concerning CreateTemporaryFile vs CreateTemporaryFileName. Review URL: http://codereview.chromium.org/193122
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26310 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/utils/cross/temporary_file_test.cc
+++ o3d/utils/cross/temporary_file_test.cc
@@ -51,7 +51,7 @@ TEST_F(TemporaryFileTest, BasicConstruction) {
 
 TEST_F(TemporaryFileTest, BasicFunction) {
   FilePath path;
-  file_util::CreateTemporaryFile(&path);
+  file_util::CreateTemporaryFileName(&path);
   EXPECT_TRUE(file_util::PathExists(path));
   {
     TemporaryFile temporary_file(path);
@@ -65,8 +65,8 @@ TEST_F(TemporaryFileTest, BasicFunction) {
 TEST_F(TemporaryFileTest, Reset) {
   FilePath path;
   FilePath path1;
-  file_util::CreateTemporaryFile(&path);
-  file_util::CreateTemporaryFile(&path1);
+  file_util::CreateTemporaryFileName(&path);
+  file_util::CreateTemporaryFileName(&path1);
   EXPECT_TRUE(file_util::PathExists(path));
   EXPECT_TRUE(file_util::PathExists(path1));
   {
@@ -87,7 +87,7 @@ TEST_F(TemporaryFileTest, Reset) {
 
 TEST_F(TemporaryFileTest, Release) {
   FilePath path;
-  file_util::CreateTemporaryFile(&path);
+  file_util::CreateTemporaryFileName(&path);
   EXPECT_TRUE(file_util::PathExists(path));
   {
     TemporaryFile temporary_file(path);
