00d6bb452135565c93176bced85f746e624b6e15
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-17 22:24:48 +0000

    Implement WebKitTabToLinksPreferenceKey for the test_shell.
    BUG=21267
    
    TEST=Run layout tests
    
    Review URL: http://codereview.chromium.org/212004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26508 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/layout_test_controller.cc
+++ webkit/tools/test_shell/layout_test_controller.cc
@@ -923,6 +923,8 @@ void LayoutTestController::overridePreference(
       preferences->local_storage_enabled = CppVariantToBool(value);
     else if (key == "WebKitOfflineWebApplicationCacheEnabled")
       preferences->application_cache_enabled = CppVariantToBool(value);
+    else if (key == "WebKitTabToLinksPreferenceKey")
+      preferences->tabs_to_links = CppVariantToBool(value);
     else {
       std::string message("Invalid name for preference: ");
       message.append(key);
