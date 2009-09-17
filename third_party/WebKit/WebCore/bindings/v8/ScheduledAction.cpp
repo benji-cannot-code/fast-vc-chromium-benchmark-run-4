60f3296683841319a7079010a243cf0d1aedbd32
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-17 19:13:58 +0000

    2009-09-17  Dimitri Glazkov  <dglazkov@chromium.org>
            Unreviewed, build fix.
    
            [V8] Partial roll out of http://trac.webkit.org/changeset/48455 to
            fix crashes that started happening in V8Proxy::getEnteredContext().
    
            * bindings/v8/ScheduledAction.cpp:
            (WebCore::ScheduledAction::execute):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48483 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/ScheduledAction.cpp
+++ third_party/WebKit/WebCore/bindings/v8/ScheduledAction.cpp
@@ -108,7 +108,8 @@ void ScheduledAction::execute(V8Proxy* proxy)
     ASSERT(proxy);
 
     v8::HandleScope handleScope;
-    v8::Handle<v8::Context> v8Context = m_context.get();
+    // FIXME: Figure out why using m_context instead of proxy->context() here causes crashes in V8Proxy::getEnteredContext();
+    v8::Handle<v8::Context> v8Context = proxy->context();
     if (v8Context.IsEmpty())
         return; // JS may not be enabled.
 
@@ -136,7 +137,8 @@ void ScheduledAction::execute(WorkerContext* workerContext)
 
     if (!m_function.IsEmpty() && m_function->IsFunction()) {
         v8::HandleScope handleScope;
-        v8::Handle<v8::Context> v8Context = m_context.get();
+        // FIXME: Figure out why using m_context instead of proxy->context() here causes crashes in V8Proxy::getEnteredContext();
+        v8::Handle<v8::Context> v8Context = scriptController->proxy()->context();
         ASSERT(!v8Context.IsEmpty());
         v8::Context::Scope scope(v8Context);
         m_function->Call(v8Context->Global(), m_argc, m_argv);
