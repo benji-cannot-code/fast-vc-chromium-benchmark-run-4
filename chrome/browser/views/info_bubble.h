8f2fd9f895fb5a9ad50d9e51acb8b18807206a12
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-17 22:04:33 +0000

    Try to make ImageBubbles draw correctly on Linux Views as well.  This also fixes an error in the window region calculation (I used right()/bottom() where I should have used width()/height()) and updates some comments and variable names in the hope of more clarity.
    BUG=21028
    TEST=Look at image bubbles on Linux Views
    Review URL: http://codereview.chromium.org/207021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26497 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/info_bubble.h
+++ chrome/browser/views/info_bubble.h
@@ -82,7 +82,7 @@ class InfoBubbleDelegate {
 // That way InfoBubble has no (or very few) ifdefs.
 #if defined(OS_WIN)
 class InfoBubble : public views::WidgetWin {
-#else
+#elif defined(OS_LINUX)
 class InfoBubble : public views::WidgetGtk {
 #endif
  public:
