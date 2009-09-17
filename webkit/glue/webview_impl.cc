66696505023f0ab0571bfcef2af1a079d4f223f6
paul@chromium.org <paul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-17 22:13:39 +0000

    Fix a bug introduced by a WebKit merge where we did not tell our autocomplete popup to hide when it was closed.
    BUG=22032
    TEST=Navigate to a page with an autocompletable input box
         (e.g. google.com login page) and notice that the
         autocomplete box hides when you select an item from it
         and hit enter.
    Review URL: http://codereview.chromium.org/211005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26503 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webview_impl.cc
+++ webkit/glue/webview_impl.cc
@@ -236,6 +236,7 @@ class AutocompletePopupMenuClient : public WebCore::PopupMenuClient {
     return selected_index_;
   }
   virtual void popupDidHide() {
+    hidePopup();
   }
   virtual void hidePopup() {
     webview_->HideAutoCompletePopup();
