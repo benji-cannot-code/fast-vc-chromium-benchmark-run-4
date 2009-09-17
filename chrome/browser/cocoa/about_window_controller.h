554ffb93763a595a2f1d1135c087ce72b0ef27ec
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-17 21:41:04 +0000

    Reverting 26482.
    relied on making the field editable which really doesn't work for legal text you show a user
    
    BUG=20493
    BUG=20855
    BUG=22179
    BUG=22180
    Review URL: http://codereview.chromium.org/212010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26488 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/about_window_controller.h
+++ chrome/browser/cocoa/about_window_controller.h
@@ -22,8 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   IBOutlet NSTextField* version_;
   IBOutlet BackgroundTileView* backgroundView_;
   IBOutlet NSImageView* logoView_;
-  IBOutlet NSView* legalBlock_;
-  IBOutlet NSTextView* legalText_;
+  IBOutlet NSTextField* legalBlock_;
   IBOutlet NSView* updateBlock_;  // Holds everything related to updates
   IBOutlet NSProgressIndicator* spinner_;
   IBOutlet NSImageView* updateStatusIndicator_;
--- chrome/browser/cocoa/about_window_controller.mm
+++ chrome/browser/cocoa/about_window_controller.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/sys_string_conversions.h"
 #import "chrome/app/keystone_glue.h"
-#include "chrome/browser/browser_list.h"
 #import "chrome/browser/cocoa/about_window_controller.h"
 #import "chrome/browser/cocoa/background_tile_view.h"
 #include "chrome/browser/cocoa/restart_browser.h"
@@ -164,7 +163,7 @@ NSAttributedString* BuildLegalTextBlock() {
   NSString* about_terms = base::SysWideToNSString(w_about_terms);
   NSString* terms_link_text = l10n_util::GetNSString(IDS_TERMS_OF_SERVICE);
 
-  AttributedStringAppendString(legal_block, @"\n\n");
+  AttributedStringAppendString(legal_block, @"\n");
   sub_str = [about_terms substringToIndex:url_offsets[0]];
   AttributedStringAppendString(legal_block, sub_str);
   AttributedStringAppendHyperlink(legal_block, terms_link_text, kTOS);
@@ -211,16 +210,9 @@ NSAttributedString* BuildLegalTextBlock() {
   DCHECK(logoImage);
   [logoView_ setImage:logoImage];
 
-  [legalText_ insertText:BuildLegalTextBlock()];
+  // Put the legal text into
+  [legalBlock_ setAttributedStringValue:BuildLegalTextBlock()];
 
-  // Resize our text view now so that the |updateShift| below is set correctly.
-  NSRect oldLegalRect = [legalBlock_ frame];
-  [legalText_ sizeToFit];
-  NSRect newRect = oldLegalRect;
-  newRect.size.height = [legalText_ frame].size.height;
-  [legalBlock_ setFrame:newRect];
-  CGFloat legalShift = newRect.size.height - oldLegalRect.size.height;
-  
   KeystoneGlue* keystone = [self defaultKeystoneGlue];
   CGFloat updateShift = 0.0;
   if (keystone) {
@@ -235,8 +227,12 @@ NSAttributedString* BuildLegalTextBlock() {
     // (and it's spacing).
     updateShift = NSMinY([legalBlock_ frame]) - NSMinY([updateBlock_ frame]);
   }
-  
+
   // Adjust the sizes/locations.
+
+  CGFloat legalShift =
+      [GTMUILocalizerAndLayoutTweaker sizeToFitFixedWidthTextField:legalBlock_];
+
   NSRect rect = [legalBlock_ frame];
   rect.origin.y -= updateShift;
   [legalBlock_ setFrame:rect];
@@ -383,15 +379,6 @@ NSAttributedString* BuildLegalTextBlock() {
                                 IntToString16(kUpdateInstallFailedToStart));
     [self stopProgressMessage:message imageID:IDR_UPDATE_FAIL];
   }
-}
-
-- (BOOL)textView:(NSTextView *)aTextView
-   clickedOnLink:(id)link
-         atIndex:(NSUInteger)charIndex {
-  BrowserList::GetLastActive()->
-    OpenURL(GURL([link UTF8String]), GURL(), NEW_WINDOW,
-            PageTransition::LINK);
-  return YES;
 }
 
 - (NSButton*)updateButton {
