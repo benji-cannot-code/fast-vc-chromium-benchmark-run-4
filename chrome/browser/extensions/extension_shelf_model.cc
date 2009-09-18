6067e3d9700a2070385fa905ba7652c2c4181d6a
erikkay@chromium.org <erikkay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-18 22:29:32 +0000

    Change the view mode when switching between moles and toolstrips, and propogate this into the class of the document element so that it's possible to use CSS rules to control the display of your toolstrip/mole.
    BUG=21939,15494
    TEST=run the Mappy extension and verify it can open and close
    
    Review URL: http://codereview.chromium.org/208020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26635 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_shelf_model.cc
+++ chrome/browser/extensions/extension_shelf_model.cc
@@ -139,6 +139,7 @@ void ExtensionShelfModel::ExpandToolstrip(iterator toolstrip,
   ToolstripEventRouter::OnToolstripExpanded(browser_->profile(),
                                             routing_id,
                                             url, height);
+  toolstrip->host->SetRenderViewType(ViewType::EXTENSION_MOLE);
 }
 
 void ExtensionShelfModel::CollapseToolstrip(iterator toolstrip,
@@ -153,6 +154,7 @@ void ExtensionShelfModel::CollapseToolstrip(iterator toolstrip,
   ToolstripEventRouter::OnToolstripCollapsed(browser_->profile(),
                                              routing_id,
                                              url);
+  toolstrip->host->SetRenderViewType(ViewType::EXTENSION_TOOLSTRIP);
 }
 
 void ExtensionShelfModel::Observe(NotificationType type,
