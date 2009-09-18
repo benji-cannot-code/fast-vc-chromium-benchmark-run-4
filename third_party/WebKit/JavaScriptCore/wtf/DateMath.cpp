e989845af2f705a78a3c48d43ce8239914d348d2
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-18 15:34:46 +0000

    Fix the Qt/Windows CE build.
    Patch by Joerg Bornemann <joerg.bornemann@nokia.com> on 2009-09-18
    Reviewed by Simon Hausmann.
    
    * JavaScriptCore.pri: Build the ce_time.cpp functions from
    within Qt externally.
    * wtf/DateMath.cpp: Removed unnecessary Qt #ifdef, for the
    Qt build these functions are no external, too.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/DateMath.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/DateMath.cpp
@@ -66,7 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <notify.h>
 #endif
 
-#if PLATFORM(WINCE) && !PLATFORM(QT)
+#if PLATFORM(WINCE)
 extern "C" size_t strftime(char * const s, const size_t maxsize, const char * const format, const struct tm * const t);
 extern "C" struct tm * localtime(const time_t *timer);
 #endif
