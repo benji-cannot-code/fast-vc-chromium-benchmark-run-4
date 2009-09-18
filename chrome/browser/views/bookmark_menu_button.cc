60d837d8312eddda885889707855d99eee30a9e5
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-18 13:53:15 +0000

    Lands http://codereview.chromium.org/203029 for Thiago:
    [Windows] Bookmark-menu button should be themed like any other menu button. 
    Command: 
    $ chrome --bookmark-menu 
    
    BUG=18954
    TEST=open chrome/chromium with --bookmark-menu switch in the command line, apply a theme, see if the bookmark-menu 
    is themed? Reset to default theme, see if the bookmark-menu come backs
    to default state.
    
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/208017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26563 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/bookmark_menu_button.cc
+++ chrome/browser/views/bookmark_menu_button.cc
@@ -11,8 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gtk/gtk.h>
 #endif
 
-#include "app/resource_bundle.h"
 #include "app/os_exchange_data.h"
+#include "app/resource_bundle.h"
+#include "app/theme_provider.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/browser.h"
@@ -30,10 +31,10 @@ BookmarkMenuButton::BookmarkMenuButton(Browser* browser)
   set_menu_delegate(this);
   SetID(VIEW_ID_BOOKMARK_MENU);
 
-  ResourceBundle &rb = ResourceBundle::GetSharedInstance();
-  // TODO (sky): if we keep this code, we need real icons, a11y support, and a
+  ThemeProvider* tp = browser_->profile()->GetThemeProvider();
+  // TODO(sky): if we keep this code, we need real icons, a11y support, and a
   // tooltip.
-  SetIcon(*rb.GetBitmapNamed(IDR_MENU_BOOKMARK));
+  SetIcon(*tp->GetBitmapNamed(IDR_MENU_BOOKMARK));
 }
 
 BookmarkMenuButton::~BookmarkMenuButton() {
