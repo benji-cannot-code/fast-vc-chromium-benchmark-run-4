a6e91902fbcfd45edb5e6307d9150fc613864df5
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-18 22:16:46 +0000

    Fix Chexkbox::SetLabel on windows, which uses view's label instead of native one.
    Review URL: http://codereview.chromium.org/208018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26632 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/button/checkbox.cc
+++ views/controls/button/checkbox.cc
@@ -166,6 +166,12 @@ void Checkbox::InitBorder() {
   // No border, so we do nothing.
 }
 
+void Checkbox::SetLabel(const std::wstring& label) {
+  NativeButton::SetLabel(label);
+  if (!native_wrapper_->UsesNativeLabel())
+    label_->SetText(label);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Checkbox, protected:
 
