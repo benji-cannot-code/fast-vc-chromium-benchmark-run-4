ed0e39c1aa15357f0a7bad0703b76c8538fcdcc3
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-19 22:14:56 +0000

    Implement ES5 Object.defineProperties function https://bugs.webkit.org/show_bug.cgi?id=29522
    Reviewed by Sam Weinig
    
    Implement Object.defineProperties.  Fairly simple patch, simply makes use of
    existing functionality used for defineProperty.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/CommonIdentifiers.h
+++ third_party/WebKit/JavaScriptCore/runtime/CommonIdentifiers.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     macro(configurable) \
     macro(constructor) \
     macro(defineProperty) \
+    macro(defineProperties) \
     macro(enumerable) \
     macro(eval) \
     macro(exec) \
