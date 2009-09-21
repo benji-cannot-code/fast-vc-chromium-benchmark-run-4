ba0388d1e56860c7783442ac8b9d8d0a854af169
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-21 15:10:58 +0000

    [Mac] Save/restore focus in TabContentsViewMac.
    BUG=http://crbug.com/12556
    TEST=Open two tabs.  Focus the omnibox in one, switch tabs and then back again.  Omnibox should still have focus.  Repeat with the findbar instead of the omnibox.
    Review URL: http://codereview.chromium.org/206015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26682 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents_view_mac.h
+++ chrome/browser/tab_contents/tab_contents_view_mac.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class FilePath;
 class FindBarMac;
+@class FocusTracker;
 @class SadTabView;
 class TabContentsViewMac;
 @class WebDragSource;
@@ -87,8 +88,9 @@ class TabContentsViewMac : public TabContentsView,
   // The Cocoa NSView that lives in the view hierarchy.
   scoped_nsobject<TabContentsViewCocoa> cocoa_view_;
 
-  // The Cocoa NSView to restore the focus to when focus returns.
-  scoped_nsobject<NSView> latent_focus_view_;
+  // Keeps track of which NSView has focus so we can restore the focus when
+  // focus returns.
+  scoped_nsobject<FocusTracker> focus_tracker_;
 
   // Used to get notifications about renderers coming and going.
   NotificationRegistrar registrar_;
