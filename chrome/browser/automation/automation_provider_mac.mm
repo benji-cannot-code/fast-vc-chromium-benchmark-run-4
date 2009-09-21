2e851802d25bbe8de1c88eccb34b2b258827bfce
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-21 17:57:47 +0000

    Revert a file that slipped into r26699 by mistake.
    TBR=stuartmorgan
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26700 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider_mac.mm
+++ chrome/browser/automation/automation_provider_mac.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 #include "base/gfx/point.h"
 #include "base/gfx/rect.h"
-#include "chrome/test/automation/automation_messages.h"
 
 void AutomationProvider::SetWindowBounds(int handle, const gfx::Rect& bounds,
                                          bool* success) {
@@ -72,14 +71,4 @@ void AutomationProvider::GetBookmarkBarVisibility(int handle, bool* visible,
   *visible = false;
   *animating = false;
   NOTIMPLEMENTED();
-}
-
-void AutomationProvider::WindowSimulateDrag(int handle,
-                                            std::vector<gfx::Point> drag_path,
-                                            int flags,
-                                            bool press_escape_en_route,
-                                            IPC::Message* reply_message) {
-  NOTIMPLEMENTED();
-  AutomationMsg_WindowDrag::WriteReplyParams(reply_message, false);
-  Send(reply_message);
 }
