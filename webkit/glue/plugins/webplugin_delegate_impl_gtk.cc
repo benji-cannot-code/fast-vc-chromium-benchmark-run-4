4a06747d0f2b7b182f790a1b1c5931e7aef28f21
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-21 20:12:56 +0000

    Enable compiler -Werror and -Wall on our webkit code for mac and linux.
    This fixes some initialization order, removes unused varibles,
    and fixes a couple signed/unsigned comparison errors.  Please
    check the signed/unsigned cases.
    
    We can't enable it for windows yet because MSVC complains
    about headers pulled in from WebCore.  We'll have to wait
    until the API is upstreamed.
    
    BUG=21140
    
    Review URL: http://codereview.chromium.org/214024
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26728 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/webplugin_delegate_impl_gtk.cc
+++ webkit/glue/plugins/webplugin_delegate_impl_gtk.cc
@@ -42,9 +42,9 @@ WebPluginDelegateImpl::WebPluginDelegateImpl(
     NPAPI::PluginInstance *instance)
     : windowed_handle_(0),
       windowed_did_set_window_(false),
+      windowless_needs_set_window_(true),
       windowless_(false),
       plugin_(NULL),
-      windowless_needs_set_window_(true),
       instance_(instance),
       pixmap_(NULL),
       first_event_time_(-1.0),
@@ -607,6 +607,8 @@ static bool NPEventFromWebMouseEvent(const WebMouseEvent& event,
         case WebMouseEvent::ButtonRight:
           button_event.button = Button3;
           break;
+        default:
+          NOTREACHED();
       }
       button_event.same_screen = True;
       break;
