bd4b5b150a3db99e4ceb391aa3e426e13ac2dfa0
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-21 20:23:16 +0000

    Make sure to include compiler_specific.h before using ALLOW_UNUSED.
    TBR=mark
    
    Review URL: http://codereview.chromium.org/214041
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26731 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/devtools_rpc.h
+++ webkit/glue/devtools/devtools_rpc.h
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/OwnPtr.h>
 
 #include "base/basictypes.h"
+#include "base/compiler_specific.h"
 #include "base/string_util.h"
 #include "webkit/glue/glue_util.h"
 
