054f86061e21f62df3297891128d7377f177897a
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-21 23:07:15 +0000

    Mock the LinkDoctor for tests. Should greatly decrease ErrorPage test flakiness.
    Not getting rid of WaitForTitleMatching because I have to add few more calls to the automation framework (http://crbug.com/19395) to wait properly after navigating back and forward.
    
    TEST=Covered by ui_tests and browser_tests.
    http://crbug.com/18365, http://crbug.com/19361
    
    Review URL: http://codereview.chromium.org/174179
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26761 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/url_request_mock_util.cc
+++ chrome/browser/net/url_request_mock_util.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/net/url_request_failed_dns_job.h"
 #include "chrome/browser/net/url_request_mock_http_job.h"
+#include "chrome/browser/net/url_request_mock_link_doctor_job.h"
 #include "chrome/browser/net/url_request_slow_download_job.h"
 #include "chrome/browser/net/url_request_slow_http_job.h"
 #include "chrome/common/chrome_paths.h"
@@ -27,6 +28,7 @@ void SetUrlRequestMocksEnabled(bool enabled) {
     URLRequestFilter::GetInstance()->ClearHandlers();
 
     URLRequestFailedDnsJob::AddUrlHandler();
+    URLRequestMockLinkDoctorJob::AddUrlHandler();
     URLRequestSlowDownloadJob::AddUrlHandler();
 
     std::wstring root_http;
