11fa6b62fdf9c67beab09ea24dcea53387dde95e
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-21 18:54:41 +0000

    Attempt to fix the Tiger build
    * WebView/WebPDFDocumentExtras.mm:
    * WebView/WebPDFRepresentation.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48589 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebPDFDocumentExtras.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebPDFDocumentExtras.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "WebPDFDocumentExtras.h"
 
+#import "WebTypesInternal.h"
 #import <JavaScriptCore/RetainPtr.h>
 #import <PDFKit/PDFDocument.h>
 #import <objc/objc-runtime.h>
--- third_party/WebKit/WebKit/mac/WebView/WebPDFRepresentation.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebPDFRepresentation.mm
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebNSObjectExtras.h"
 #import "WebPDFDocumentExtras.h"
 #import "WebPDFView.h"
+#import "WebTypesInternal.h"
 #import <JavaScriptCore/Assertions.h>
 #import <JavaScriptCore/JSContextRef.h>
 #import <JavaScriptCore/JSStringRef.h>
