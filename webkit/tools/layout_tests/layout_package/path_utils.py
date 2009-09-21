484dcacff215efe191510a18524e57a8eb042bf6
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-21 21:47:48 +0000

    Implement KillAllTestShells.
    The linux valgrind bot is trying to call this function.  Seems like it was
    removed in an earlier refactoring by mistake.
    
    Review URL: http://codereview.chromium.org/207043
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26739 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/layout_tests/layout_package/path_utils.py
+++ webkit/tools/layout_tests/layout_package/path_utils.py
@@ -291,3 +291,6 @@ def FuzzyMatchPath():
 
 def ShutDownHTTPServer(server_process):
   return platform_utils.ShutDownHTTPServer(server_process)
+
+def KillAllTestShells():
+  platform_utils.KillAllTestShells()
