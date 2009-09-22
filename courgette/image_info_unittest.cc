cff86c14cfa5e01bd4e7a4f24374eaf1af4415f0
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-22 00:19:24 +0000

    Replaces the uses of the deprecated AppendPath function in courgette
    this is same http://codereview.chromium.org/196022
    
    commiting for him.
    
    BUG=none
    TEST=the modified test should run fine.
    
    TBR=sra
    
    
    
    Review URL: http://codereview.chromium.org/214044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26768 0039d316-1c4b-4281-b951-d872f2087c98

--- courgette/image_info_unittest.cc
+++ courgette/image_info_unittest.cc
@@ -22,8 +22,8 @@ class ImageInfoTest : public testing::Test {
  private:
   void SetUp() {
     PathService::Get(base::DIR_SOURCE_ROOT, &test_dir_);
-    file_util::AppendToPath(&test_dir_, L"courgette");
-    file_util::AppendToPath(&test_dir_, L"testdata");
+    test_dir_ = test_dir_.AppendASCII("courgette");
+    test_dir_ = test_dir_.AppendASCII("testdata");
   }
 
   void TearDown() {
@@ -33,13 +33,13 @@ class ImageInfoTest : public testing::Test {
 
   std::string FileContents(const char* file_name) const;
 
-  std::wstring test_dir_;
+  FilePath test_dir_;
 };
 
 //  Reads a test file into a string.
 std::string ImageInfoTest::FileContents(const char* file_name) const {
-  std::wstring file_path = test_dir_;
-  file_util::AppendToPath(&file_path, UTF8ToWide(file_name));
+  FilePath file_path = test_dir_;
+  file_path = file_path.AppendASCII(file_name);
   std::string file_bytes;
   if (!file_util::ReadFileToString(file_path, &file_bytes)) {
     EXPECT_TRUE(!"Could not read test data");
