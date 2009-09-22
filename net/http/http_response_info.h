6ecf5cb16740148def2e1f0a7eed2bdb66c63792
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-22 20:41:06 +0000

    Because we use scoped_refptr<> to the HttpResponseInfo and SSLCertRequestInfo, simply declaring the forward class definition is not enough to include this header.  This header is really dependent on those two class definitions.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/216032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26854 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_info.h
+++ net/http/http_response_info.h
@@ -8,13 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time.h"
 #include "net/base/auth.h"
+#include "net/base/ssl_cert_request_info.h"
 #include "net/base/ssl_info.h"
+#include "net/http/http_response_headers.h"
 #include "net/http/http_vary_data.h"
 
 namespace net {
-
-class HttpResponseHeaders;
-class SSLCertRequestInfo;
 
 class HttpResponseInfo {
  public:
