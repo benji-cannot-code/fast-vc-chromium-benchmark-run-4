8009ce0dc1e2920436ef6e916cab141620904c2b
sgk@chromium.org <sgk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-22 16:16:39 +0000

    Add a simple about:tcmalloc page that just displays its canned stats. Add USE_TCMALLOC to control its inclusion.  Make browser depend on tcmalloc to get the PERFTOOLS_DLL_DECL definition. BUG=none TEST=none Review URL: http://codereview.chromium.org/216033
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26816 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -53,6 +53,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/chromeos_version_loader.h"
 #endif
 
+#if defined(USE_TCMALLOC)
+#include "third_party/tcmalloc/google/malloc_extension.h"
+#endif
+
 #ifdef CHROME_PERSONALIZATION
 #include "chrome/browser/sync/auth_error_state.h"
 #include "chrome/browser/sync/profile_sync_service.h"
@@ -71,6 +75,7 @@ const char kHistogramsPath[] = "histograms";
 const char kObjectsPath[] = "objects";
 const char kMemoryRedirectPath[] = "memory-redirect";
 const char kMemoryPath[] = "memory";
+const char kTcmallocPath[] = "tcmalloc";
 const char kPluginsPath[] = "plugins";
 const char kStatsPath[] = "stats";
 const char kVersionPath[] = "version";
@@ -181,6 +186,18 @@ std::string AboutDns() {
   chrome_browser_net::DnsPrefetchGetHtmlInfo(&data);
   return data;
 }
+
+#if defined(USE_TCMALLOC)
+std::string AboutTcmalloc(const std::string& query) {
+  std::string data;
+  char buffer[1024*32];
+  MallocExtension::instance()->GetStats(buffer, sizeof(buffer));
+  data.append("<html><head><title>About tcmalloc</title></head><body><pre>\n");
+  data.append(buffer);
+  data.append("</pre></body></html>\n");
+  return data;
+}
+#endif
 
 std::string AboutHistograms(const std::string& query) {
   TimeDelta wait_time = TimeDelta::FromMilliseconds(10000);
@@ -551,6 +568,10 @@ void AboutSource::StartDataRequest(const std::string& path_raw,
     response = AboutPlugins();
   } else if (path == kStatsPath) {
     response = AboutStats();
+#if defined(USE_TCMALLOC)
+  } else if (path == kTcmallocPath) {
+    response = AboutTcmalloc(info);
+#endif
   } else if (path == kVersionPath || path.empty()) {
 #if defined(OS_CHROMEOS)
     new ChromeOSAboutVersionHandler(this, request_id);
--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -2405,11 +2405,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'third_party/wtl/include',
           ],
           'dependencies': [
-            '../google_update/google_update.gyp:google_update',
-            'installer/installer.gyp:installer_util',
-            '../third_party/cld/cld.gyp:cld',
-            '../views/views.gyp:views',
             '../gears/gears.gyp:gears',
+            '../google_update/google_update.gyp:google_update',
+            '../third_party/cld/cld.gyp:cld',
+            '../third_party/tcmalloc/tcmalloc.gyp:tcmalloc',
+            '../views/views.gyp:views',
+            'installer/installer.gyp:installer_util',
           ],
           'export_dependent_settings': [
             '../views/views.gyp:views',
