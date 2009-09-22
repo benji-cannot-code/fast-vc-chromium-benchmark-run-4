dd0455b5a648cd51bbb6958a6eb68dafb2e30549
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-22 19:50:31 +0000

    Fix appcache_service and request_context referencing.
    There is one appcache service per profile and several request context per profile. The profile holds a reference to the appcache service. Those request contexts which are subject to retrieval from appcaches hold a reference to the appcache service too. The appcache service is provided with a pointer back to the 'main' request context, this context is used when updating appcaches.
    
    Initialization is a little tricky because profiles can't be used on the IO thread and request contexts can't be used on the UI thread.
    
    BUG=22597,22125
    TEST=many existing tests exercise profile/context creation
    Review URL: http://codereview.chromium.org/215024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26844 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_url_request_context.cc
+++ chrome/browser/net/chrome_url_request_context.cc
@@ -111,9 +111,15 @@ static net::ProxyService* CreateProxyService(URLRequestContext* context,
 // static
 ChromeURLRequestContext* ChromeURLRequestContext::CreateOriginal(
     Profile* profile, const FilePath& cookie_store_path,
-    const FilePath& disk_cache_path, int cache_size) {
+    const FilePath& disk_cache_path, int cache_size,
+    ChromeAppCacheService* appcache_service) {
   DCHECK(!profile->IsOffTheRecord());
-  ChromeURLRequestContext* context = new ChromeURLRequestContext(profile);
+  ChromeURLRequestContext* context = new ChromeURLRequestContext(
+      profile, appcache_service);
+
+  // The appcache service uses the profile's original context for UpdateJobs.
+  DCHECK(!appcache_service->request_context());
+  appcache_service->set_request_context(context);
 
   // Global host resolver for the context.
   context->host_resolver_ = chrome_browser_net::GetGlobalHostResolver();
@@ -170,17 +176,19 @@ ChromeURLRequestContext* ChromeURLRequestContext::CreateOriginal(
 
 // static
 ChromeURLRequestContext* ChromeURLRequestContext::CreateOriginalForMedia(
-    Profile* profile, const FilePath& disk_cache_path, int cache_size) {
+    Profile* profile, const FilePath& disk_cache_path, int cache_size,
+    ChromeAppCacheService* appcache_service) {
   DCHECK(!profile->IsOffTheRecord());
   return CreateRequestContextForMedia(profile, disk_cache_path, cache_size,
-                                      false);
+                                      false, appcache_service);
 }
 
 // static
 ChromeURLRequestContext* ChromeURLRequestContext::CreateOriginalForExtensions(
     Profile* profile, const FilePath& cookie_store_path) {
   DCHECK(!profile->IsOffTheRecord());
-  ChromeURLRequestContext* context = new ChromeURLRequestContext(profile);
+  ChromeURLRequestContext* context = new ChromeURLRequestContext(
+      profile, NULL);
 
   // All we care about for extensions is the cookie store.
   DCHECK(!cookie_store_path.empty());
@@ -201,9 +209,14 @@ ChromeURLRequestContext* ChromeURLRequestContext::CreateOriginalForExtensions(
 
 // static
 ChromeURLRequestContext* ChromeURLRequestContext::CreateOffTheRecord(
-    Profile* profile) {
+    Profile* profile, ChromeAppCacheService* appcache_service) {
   DCHECK(profile->IsOffTheRecord());
-  ChromeURLRequestContext* context = new ChromeURLRequestContext(profile);
+  ChromeURLRequestContext* context = new ChromeURLRequestContext(
+      profile, appcache_service);
+
+  // The appcache service uses the profile's original context for UpdateJobs.
+  DCHECK(!appcache_service->request_context());
+  appcache_service->set_request_context(context);
 
   // Share the same proxy service and host resolver as the original profile.
   // TODO(eroman): although ProxyService is reference counted, this sharing
@@ -244,7 +257,8 @@ ChromeURLRequestContext* ChromeURLRequestContext::CreateOffTheRecord(
 ChromeURLRequestContext*
 ChromeURLRequestContext::CreateOffTheRecordForExtensions(Profile* profile) {
   DCHECK(profile->IsOffTheRecord());
-  ChromeURLRequestContext* context = new ChromeURLRequestContext(profile);
+  ChromeURLRequestContext* context =
+      new ChromeURLRequestContext(profile, NULL);
   net::CookieMonster* cookie_monster = new net::CookieMonster;
 
   // Enable cookies for extension URLs only.
@@ -258,10 +272,11 @@ ChromeURLRequestContext::CreateOffTheRecordForExtensions(Profile* profile) {
 // static
 ChromeURLRequestContext* ChromeURLRequestContext::CreateRequestContextForMedia(
     Profile* profile, const FilePath& disk_cache_path, int cache_size,
-    bool off_the_record) {
+    bool off_the_record, ChromeAppCacheService* appcache_service) {
   URLRequestContext* original_context =
       profile->GetOriginalProfile()->GetRequestContext();
-  ChromeURLRequestContext* context = new ChromeURLRequestContext(profile);
+  ChromeURLRequestContext* context =
+      new ChromeURLRequestContext(profile, appcache_service);
   context->is_media_ = true;
 
   // Share the same proxy service of the common profile.
@@ -302,8 +317,10 @@ ChromeURLRequestContext* ChromeURLRequestContext::CreateRequestContextForMedia(
   return context;
 }
 
-ChromeURLRequestContext::ChromeURLRequestContext(Profile* profile)
-    : prefs_(profile->GetPrefs()),
+ChromeURLRequestContext::ChromeURLRequestContext(
+    Profile* profile, ChromeAppCacheService* appcache_service)
+    : appcache_service_(appcache_service),
+      prefs_(profile->GetPrefs()),
       is_media_(false),
       is_off_the_record_(profile->IsOffTheRecord()) {
   // Set up Accept-Language and Accept-Charset header values
@@ -359,8 +376,6 @@ ChromeURLRequestContext::ChromeURLRequestContext(Profile* profile)
   }
 
   ssl_config_service_ = profile->GetSSLConfigService();
-
-  appcache_service_ = profile->GetAppCacheService();
 }
 
 ChromeURLRequestContext::ChromeURLRequestContext(
@@ -379,13 +394,13 @@ ChromeURLRequestContext::ChromeURLRequestContext(
   referrer_charset_ = other->referrer_charset_;
 
   // Set ChromeURLRequestContext members
+  appcache_service_ = other->appcache_service_;
   extension_paths_ = other->extension_paths_;
   user_script_dir_path_ = other->user_script_dir_path_;
   prefs_ = other->prefs_;
   blacklist_ = other->blacklist_;
   is_media_ = other->is_media_;
   is_off_the_record_ = other->is_off_the_record_;
-  appcache_service_ = other->appcache_service_;
 }
 
 // NotificationObserver implementation.
@@ -539,6 +554,9 @@ void ChromeURLRequestContext::OnUnloadedExtension(
 
 ChromeURLRequestContext::~ChromeURLRequestContext() {
   DCHECK(NULL == prefs_);
+
+  if (appcache_service_.get() && appcache_service_->request_context() == this)
+    appcache_service_->set_request_context(NULL);
 
   NotificationService::current()->Notify(
       NotificationType::URL_REQUEST_CONTEXT_RELEASED,
--- webkit/appcache/appcache_service.h
+++ webkit/appcache/appcache_service.h
@@ -14,8 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/ref_counted.h"
 #include "base/task.h"
-#include "net/url_request/url_request_context.h"
 #include "googleurl/src/gurl.h"
+
+class URLRequestContext;
 
 namespace appcache {
 
@@ -35,7 +36,7 @@ class AppCacheService {
 
     // If a load fails the object pointer will be NULL.
     virtual void CacheLoadedCallback(AppCache* cache, int64 cache_id) = 0;
-    virtual void GroupLoadedCallback(AppCacheGroup* cache,
+    virtual void GroupLoadedCallback(AppCacheGroup* group,
                                      const GURL& manifest_url) = 0;
   };
 
@@ -45,12 +46,12 @@ class AppCacheService {
   void Initialize(const FilePath& cache_directory);
 
   // Context for use during cache updates, should only be accessed
-  // on the IO thread.
-  URLRequestContext* request_context() { return request_context_.get(); }
+  // on the IO thread. We do NOT add a reference to the request context,
+  // it is the callers responsibility to ensure that the pointer
+  // remains valid while set.
+  URLRequestContext* request_context() const { return request_context_; }
   void set_request_context(URLRequestContext* context) {
-    // TODO(michaeln): need to look into test failures that occur
-    // when we take this reference? Stubbing out for now.
-    // request_context_ = context;
+    request_context_ = context;
   }
 
   // TODO(jennb): API to set service settings, like file paths for storage
@@ -63,7 +64,7 @@ class AppCacheService {
     return (it != backends_.end()) ? it->second : NULL;
   }
 
-  // Track what we have in or in-memory cache.
+  // Track what we have in our in-memory cache.
   void AddCache(AppCache* cache);
   void RemoveCache(AppCache* cache);
   void AddGroup(AppCacheGroup* group);
@@ -118,7 +119,7 @@ class AppCacheService {
   FilePath cache_directory_;
 
   // Context for use during cache updates.
-  scoped_refptr<URLRequestContext> request_context_;
+  URLRequestContext* request_context_;
 
   // TODO(michaeln): cache and group loading book keeping.
   // TODO(michaeln): database and response storage
