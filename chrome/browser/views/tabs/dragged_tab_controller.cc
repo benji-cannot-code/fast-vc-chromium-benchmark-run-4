1f6b19f44ca009b584890faad70aac97a9191503
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-22 16:43:46 +0000

    Makes tab dragging on views restrict the drag to the main window.
    As X is async, this results in annoying flicker right now. If we stick
    with this we're going to need to avoid creating a new window. But this
    is good enough for now.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/210035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26820 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/dragged_tab_controller.cc
+++ chrome/browser/views/tabs/dragged_tab_controller.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -612,10 +612,16 @@ void DraggedTabController::ContinueDragging() {
   // guaranteed to be correct regardless of monitor config.
   gfx::Point screen_point = GetCursorScreenPoint();
 
+  // TODO(sky): make this define OS_CHROMEOS when we merge views and chromeos.
+#if defined(OS_LINUX)
+  // We don't allow detaching in chrome os.
+  TabStrip* target_tabstrip = source_tabstrip_;
+#else
   // Determine whether or not we have dragged over a compatible TabStrip in
   // another browser window. If we have, we should attach to it and start
   // dragging within it.
   TabStrip* target_tabstrip = GetTabStripForPoint(screen_point);
+#endif
   if (target_tabstrip != attached_tabstrip_) {
     // Make sure we're fully detached from whatever TabStrip we're attached to
     // (if any).
@@ -1060,6 +1066,14 @@ gfx::Point DraggedTabController::GetDraggedViewPoint(
         (tabstrip_bounds.bottom() + vertical_drag_magnetism)) {
       y = max_y;
     }
+    // TODO(sky): make this define OS_CHROMEOS when we merge views and
+    // chromeos.
+#if defined(OS_LINUX)
+    // We currently don't allow detaching on chromeos. This restricts dragging
+    // to the main window.
+    x = std::min(std::max(x, tabstrip_bounds.x()), max_x);
+    y = tabstrip_bounds.y();
+#endif
   }
   return gfx::Point(x, y);
 }
