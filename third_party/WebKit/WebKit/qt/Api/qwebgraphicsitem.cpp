93b7bfd487274013ae0f7eed7c9ac2650a3d78e6
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-23 16:23:47 +0000

    2009-09-23  Norbert Leser  <norbert.leser@nokia.com>
            Reviewed by Tor Arne Vestbø.
    
            Need to guard QX11Info include with Q_WS_X11.
            That class may not be available (in QT 4.5 for Symbian, for instance).
            Completes fixes in r48627 and r48604.
    
            * Api/qwebgraphicsitem.cpp:
            * Api/qwebview.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48676 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebgraphicsitem.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebgraphicsitem.cpp
@@ -30,7 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QtGui/qapplication.h>
 #include <QtGui/qgraphicssceneevent.h>
 #include <QtGui/qstyleoption.h>
+#if defined(Q_WS_X11)
 #include <QX11Info>
+#endif
 
 class QWebGraphicsItemPrivate : public QWebPageClient {
 public:
--- third_party/WebKit/WebKit/qt/Api/qwebview.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebview.cpp
@@ -32,7 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qprinter.h"
 #include "qdir.h"
 #include "qfile.h"
+#if defined(Q_WS_X11)
 #include <QX11Info>
+#endif
 
 class QWebViewPrivate : public QWebPageClient {
 public:
