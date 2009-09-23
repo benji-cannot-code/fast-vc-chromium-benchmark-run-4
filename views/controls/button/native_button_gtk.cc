24fae86a1f97e58a382408866d71a48e88737c7a
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-23 19:33:22 +0000

    removing unnecessary invocations of super class constructor TEST=none BUG=none
    Review URL: http://codereview.chromium.org/220012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26963 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/button/native_button_gtk.cc
+++ views/controls/button/native_button_gtk.cc
@@ -18,8 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 
 NativeButtonGtk::NativeButtonGtk(NativeButton* native_button)
-    : NativeControlGtk(),
-      native_button_(native_button) {
+    : native_button_(native_button) {
   // Associates the actual GtkWidget with the native_button so the native_button
   // is the one considered as having the focus (not the wrapper) when the
   // GtkWidget is focused directly (with a click for example).
