8e18b9c1deca1d544ab5418ce3450d4793158d1a
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-23 22:09:31 +0000

    Makes Chrome on Chrome OS not restore the last session if the last session crashed.
    BUG=22835
    TEST=On Chrome OS bring up Chrome, wait a few seconds, go to a
      terminal and kill Chrome, when Chrome comes up again you should see
      the crashed info bar.
    
    Review URL: http://codereview.chromium.org/220018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26999 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/defaults.cc
+++ chrome/browser/defaults.cc
@@ -16,7 +16,6 @@ const int kAutocompletePopupFontSize = 7;
 const SessionStartupPref::Type kDefaultSessionStartupType =
     SessionStartupPref::LAST;
 const bool kSuppressCrashInfoBar = true;
-const bool kRestoreAfterCrash = true;
 const int kPinnedTabWidth = 64;
 const bool kCanToggleSystemTitleBar = false;
 
@@ -41,7 +40,6 @@ const bool kCanToggleSystemTitleBar = true;
 const SessionStartupPref::Type kDefaultSessionStartupType =
     SessionStartupPref::DEFAULT;
 const bool kSuppressCrashInfoBar = false;
-const bool kRestoreAfterCrash = false;
 const int kPinnedTabWidth = 56;
 
 #endif
