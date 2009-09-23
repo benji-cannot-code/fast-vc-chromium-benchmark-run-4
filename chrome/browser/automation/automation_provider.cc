15b668996ba1bcf36526f1ffaf020ab3b202c9c3
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-23 22:15:04 +0000

    Reinstating the AutomationMsg_TabReposition message and related stuff which is used by ChromeFrame. These were incorrectly removed under the assumption that they are unused.
    This should fix the ChromeFrame build issues.
    
    TBR=amit,estade
    
    Review URL: http://codereview.chromium.org/220022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27003 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.cc
+++ chrome/browser/automation/automation_provider.cc
@@ -348,6 +348,7 @@ void AutomationProvider::OnMessageReceived(const IPC::Message& message) {
                                     WaitForTabToBeRestored)
     IPC_MESSAGE_HANDLER(AutomationMsg_SetInitialFocus, SetInitialFocus)
 #if defined(OS_WIN)
+    IPC_MESSAGE_HANDLER(AutomationMsg_TabReposition, OnTabReposition)
     IPC_MESSAGE_HANDLER(AutomationMsg_ForwardContextMenuCommandToChrome,
                         OnForwardContextMenuCommandToChrome)
 #endif
--- chrome/browser/automation/automation_provider.h
+++ chrome/browser/automation/automation_provider.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct AutomationMsg_Find_Params;
 
 namespace IPC {
+struct Reposition_Params;
 struct ExternalTabSettings;
 }
 
@@ -322,6 +323,8 @@ class AutomationProvider : public base::RefCounted<AutomationProvider>,
 
 // TODO(port): remove windowisms.
 #if defined(OS_WIN)
+  void OnTabReposition(int tab_handle,
+                       const IPC::Reposition_Params& params);
   void OnForwardContextMenuCommandToChrome(int tab_handle, int command);
 #endif  // defined(OS_WIN)
 
