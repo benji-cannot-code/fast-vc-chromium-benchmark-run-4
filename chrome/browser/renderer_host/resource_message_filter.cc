be53e0183ab95b1b6177373345cf4a9ffcdc4b31
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-23 18:38:56 +0000

    Be sure to initialize AppCacheDispatcherHost on the IO thread.
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/221002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26955 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/resource_message_filter.cc
+++ chrome/browser/renderer_host/resource_message_filter.cc
@@ -195,8 +195,8 @@ ResourceMessageFilter::~ResourceMessageFilter() {
 }
 
 void ResourceMessageFilter::Init() {
+  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
   render_widget_helper_->Init(id(), resource_dispatcher_host_);
-  appcache_dispatcher_host_->Initialize(this, id());
 }
 
 // Called on the IPC thread:
@@ -213,6 +213,8 @@ void ResourceMessageFilter::OnFilterAdded(IPC::Channel* channel) {
 // Called on the IPC thread:
 void ResourceMessageFilter::OnChannelConnected(int32 peer_pid) {
   DCHECK(!handle()) << " " << handle();
+  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
+
   base::ProcessHandle peer_handle;
   if (!base::OpenProcessHandle(peer_pid, &peer_handle)) {
     NOTREACHED();
@@ -225,6 +227,8 @@ void ResourceMessageFilter::OnChannelConnected(int32 peer_pid) {
 
   WorkerService::GetInstance()->Initialize(
       resource_dispatcher_host_, ui_loop());
+
+  appcache_dispatcher_host_->Initialize(this, id());
 }
 
 void ResourceMessageFilter::OnChannelError() {
