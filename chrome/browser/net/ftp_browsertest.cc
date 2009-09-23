8bd76fccee4009c2b4435001863187a16f5cad7e
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-23 21:07:07 +0000

    Add a very basic test for FTP directory listing, browser side.
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/220015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26982 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/net/ftp_browsertest.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/in_process_browser_test.h"
+#include "chrome/test/ui_test_utils.h"
+#include "net/url_request/url_request_unittest.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class FtpBrowserTest : public InProcessBrowserTest {
+ public:
+  FtpBrowserTest() : server_(FTPTestServer::CreateServer(L"")) {
+  }
+
+ protected:
+  scoped_refptr<FTPTestServer> server_;
+};
+
+IN_PROC_BROWSER_TEST_F(FtpBrowserTest, DirectoryListing) {
+  ASSERT_TRUE(NULL != server_.get());
+  ui_test_utils::NavigateToURL(browser(), server_->TestServerPage("/"));
+  // TODO(phajdan.jr): test more things.
+}
--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/download/save_page_browsertest.cc',
       'browser/gtk/bookmark_manager_browsertest.cc',
       'browser/gtk/view_id_util_browsertest.cc',
+      'browser/net/ftp_browsertest.cc',
       'browser/ssl/ssl_browser_tests.cc',
       'browser/extensions/cross_origin_xhr_apitest.cc',
       'browser/extensions/execute_script_apitest.cc',
