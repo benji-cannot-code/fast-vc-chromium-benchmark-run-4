606c0bb886e18ff7e7a8ff99e2141271b7161b88
ojan@chromium.org <ojan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-23 05:11:23 +0000

    Include skipped tests statistics when running sharded tests. Review URL: http://codereview.chromium.org/215054
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26907 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/layout_tests/run_webkit_tests.py
+++ webkit/tools/layout_tests/run_webkit_tests.py
@@ -260,6 +260,11 @@ class TestRunner:
     else:
       self._test_files_list.sort(self.TestFilesSort)
 
+    # Chunking replaces self._expectations, which loses all the skipped test
+    # information. Keep the prechunk expectations for tracking number of
+    # skipped tests.
+    self.prechunk_expectations = self._expectations;
+
     # If the user specifies they just want to run a subset of the tests,
     # just grab a subset of the non-skipped tests.
     if self._options.run_chunk or self._options.run_part:
@@ -824,8 +829,15 @@ class TestRunner:
         AddFailure(count_group, failure.__class__)
         failure_group.add(test)
 
+    # Here and below, use the prechuncked expectations object for numbers of
+    # skipped tests. Chunking removes the skipped tests before creating the
+    # expectations file.
+    #
+    # This is a bit inaccurate, since the number of skipped tests will be
+    # duplicated across all shard, but it's the best we can reasonably do.
+
     deduped_fixable_count = len(fixable_failures |
-        self._expectations.GetFixableSkipped())
+        self.prechunk_expectations.GetFixableSkipped())
     all_fixable_count = len(self._test_files -
         self._expectations.GetWontFix() - self._expectations.GetDeferred())
 
@@ -835,19 +847,19 @@ class TestRunner:
         self._expectations.GetFixable() | fixable_failures,
         fixable_failures,
         fixable_counts,
-        self._expectations.GetFixableSkipped())
+        self.prechunk_expectations.GetFixableSkipped())
 
     deferred_result_summary = ResultSummaryEntry(
         self._expectations.GetDeferred(),
         deferred_failures,
         deferred_counts,
-        self._expectations.GetDeferredSkipped())
+        self.prechunk_expectations.GetDeferredSkipped())
 
     wontfix_result_summary = ResultSummaryEntry(
         self._expectations.GetWontFix(),
         wontfix_failures,
         wontfix_counts,
-        self._expectations.GetWontFixSkipped())
+        self.prechunk_expectations.GetWontFixSkipped())
 
     return ResultSummary(deferred_result_summary, wontfix_result_summary,
         fixable_result_summary, deduped_fixable_count, all_fixable_count)
