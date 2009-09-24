8207f3f4695187b0c406e5896410a27f593ce7da
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-24 21:58:59 +0000

    Implement correct horizontal scrollbar behavior for GTK+
    WebCore
    
    2009-09-24  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
    
            Reviewed by Oliver Hunt.
    
            Implement correct horizontal scrollbar behavior for GTK+ also on
            styled scrollbars.
    
            https://bugs.webkit.org/show_bug.cgi?id=29348
            [Gtk] Scrollwheel on horizontal scrollbars should slide horizontally
    
            Test: platform/gtk/scrollbars/overflow-scrollbar-horizontal-wheel-scroll.html
    
            * page/EventHandler.cpp:
            (WebCore::EventHandler::handleWheelEvent): on GTK+, when using the
            wheel with the pointer on the horizontal scrollbar, scroll
            horizontally;
            * platform/PlatformWheelEvent.h:
            * platform/gtk/WheelEventGtk.cpp:
            (WebCore::PlatformWheelEvent::swapOrientation): allow adding a
            vertical scroll to the horizontal one;
    
    WebKitTools
    
    2009-09-24  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
    
            Reviewed by Oliver Hunt.
    
            Add support for DRT to send mouse wheel events.
    
            https://bugs.webkit.org/show_bug.cgi?id=29348
            [Gtk] Scrollwheel on horizontal scrollbars should slide horizontally
    
            * DumpRenderTree/gtk/EventSender.cpp:
            (mouseWheelToCallback):
    
    LayoutTests
    
    2009-09-24  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
    
            Reviewed by Oliver Hunt.
    
            Test for correct wheel scrolling behavior.
    
            https://bugs.webkit.org/show_bug.cgi?id=29348
            [Gtk] Scrollwheel on horizontal scrollbars should slide horizontally
    
            * platform/gtk/scrollbars/overflow-scrollbar-horizontal-wheel-scroll.html: Added.
            * platform/gtk/scrollbars/overflow-scrollbar-horizontal-wheel-scroll-expected.txt: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48735 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/WebCore/page/EventHandler.cpp
@@ -1759,6 +1759,13 @@ bool EventHandler::dispatchMouseEvent(const AtomicString& eventType, Node* targe
     return swallowEvent;
 }
 
+#if !PLATFORM(GTK)
+bool EventHandler::shouldTurnVerticalTicksIntoHorizontal(const HitTestResult& result) const
+{
+    return false;
+}
+#endif
+
 bool EventHandler::handleWheelEvent(PlatformWheelEvent& e)
 {
     Document* doc = m_frame->document();
@@ -1778,11 +1785,12 @@ bool EventHandler::handleWheelEvent(PlatformWheelEvent& e)
     bool isOverWidget;
     bool didSetLatchedNode = false;
 
+    HitTestRequest request(HitTestRequest::ReadOnly);
+    HitTestResult result(vPoint);
+    doc->renderView()->layer()->hitTest(request, result);
+
     if (m_useLatchedWheelEventNode) {
         if (!m_latchedWheelEventNode) {
-            HitTestRequest request(HitTestRequest::ReadOnly);
-            HitTestResult result(vPoint);
-            doc->renderView()->layer()->hitTest(request, result);
             m_latchedWheelEventNode = result.innerNode();
             m_widgetIsLatched = result.isOverWidget();
             didSetLatchedNode = true;
@@ -1796,12 +1804,12 @@ bool EventHandler::handleWheelEvent(PlatformWheelEvent& e)
         if (m_previousWheelScrolledNode)
             m_previousWheelScrolledNode = 0;
 
-        HitTestRequest request(HitTestRequest::ReadOnly);
-        HitTestResult result(vPoint);
-        doc->renderView()->layer()->hitTest(request, result);
         node = result.innerNode();
         isOverWidget = result.isOverWidget();
     }
+
+    if (shouldTurnVerticalTicksIntoHorizontal(result))
+        e.turnVerticalTicksIntoHorizontal();
 
     if (node) {
         // Figure out which view to send the event to.
