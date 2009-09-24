f4866a80ef65e7b9f201c6c283ef67efdb9048cb
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-24 10:12:36 +0000

    On Windows JSChar is typedef'ed to wchar_t.
    Patch by Janne Koskinen <janne.p.koskinen@digia.com> on 2009-09-24
    Reviewed by Simon Hausmann.
    
    When building with WINSCW for Symbian we need to do the
    same typedef.
    
    * API/JSStringRef.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48711 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/JSStringRef.h
+++ third_party/WebKit/JavaScriptCore/API/JSStringRef.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 extern "C" {
 #endif
 
-#if !defined(WIN32) && !defined(_WIN32)
+#if !defined(WIN32) && !defined(_WIN32) && !defined(__WINSCW__)
 /*!
 @typedef JSChar
 @abstract A Unicode character.
