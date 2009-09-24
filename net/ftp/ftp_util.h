4afad1e6a4c419e77daf9c895e5a14ac64416f8e
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-24 21:46:50 +0000

    Correctly talk to VMS servers (translate UNIX paths to VMS and vice versa).
    This way we get a native VMS listing and not a UNIX emulation mode, which is
    slightly different than native UNIX and confuses the parser.
    
    TEST=Covered by net_unittests.
    BUG=22193
    
    Review URL: http://codereview.chromium.org/215058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27129 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/ftp/ftp_util.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_FTP_FTP_UTIL_H_
+#define NET_FTP_FTP_UTIL_H_
+
+#include <string>
+
+namespace net {
+
+class FtpUtil {
+ public:
+  // Convert Unix file path to VMS path (must be a file, and not a directory).
+  static std::string UnixFilePathToVMS(const std::string& unix_path);
+
+  // Convert Unix directory path to VMS path (must be a directory).
+  static std::string UnixDirectoryPathToVMS(const std::string& unix_path);
+
+  // Convert VMS path to Unix-style path.
+  static std::string VMSPathToUnix(const std::string& vms_path);
+};
+
+}  // namespace net
+
+#endif  // NET_FTP_FTP_UTIL_H_
