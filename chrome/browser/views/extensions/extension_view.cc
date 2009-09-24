dcf4ad49a0ef7f8df185f38a0afb0599f1dc872d
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-24 22:59:09 +0000

    Revert r27137: "Create renderers for ExtensionHosts one at a time to avoid blocking the UI"
    BUG=14040
    TBR=erikkay
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27143 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/extensions/extension_view.cc
+++ chrome/browser/views/extensions/extension_view.cc
@@ -87,7 +87,7 @@ void ExtensionView::CreateWidgetHostView() {
   NOTIMPLEMENTED();
 #endif
 
-  host_->CreateRenderViewSoon(view);
+  host_->CreateRenderView(view);
   SetVisible(false);
 
   if (!pending_background_.empty()) {
