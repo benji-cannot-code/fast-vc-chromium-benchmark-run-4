f178471eb882bb04574c15a546be3736c8e56770
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-24 00:47:51 +0000

    2009-09-23  Jedrzej Nowacki  <jedrzej.nowacki@nokia.com>
            Reviewed by Simon Hausmann.
    
            [Qt] Crash fix in QWebHistory back and forward methods.
    
            QWebHistory::back() and QWebHistory::forward() were crashing on
            ASSERT in WebCore::BackForwardList. The methods should check
            canGoBack() and canGoForward() at the beginning.
    
            https://bugs.webkit.org/show_bug.cgi?id=29675
    
            * Api/qwebhistory.cpp:
            (QWebHistory::back):
            (QWebHistory::forward):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48691 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebhistory.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebhistory.cpp
@@ -357,9 +357,11 @@ bool QWebHistory::canGoForward() const
 */
 void QWebHistory::back()
 {
-    d->lst->goBack();
-    WebCore::Page* page = d->lst->page();
-    page->goToItem(currentItem().d->item, WebCore::FrameLoadTypeIndexedBackForward);
+    if (canGoBack()) {
+        d->lst->goBack();
+        WebCore::Page* page = d->lst->page();
+        page->goToItem(currentItem().d->item, WebCore::FrameLoadTypeIndexedBackForward);
+    }
 }
 
 /*!
@@ -370,9 +372,11 @@ void QWebHistory::back()
 */
 void QWebHistory::forward()
 {
-    d->lst->goForward();
-    WebCore::Page* page = d->lst->page();
-    page->goToItem(currentItem().d->item, WebCore::FrameLoadTypeIndexedBackForward);
+    if (canGoForward()) {
+        d->lst->goForward();
+        WebCore::Page* page = d->lst->page();
+        page->goToItem(currentItem().d->item, WebCore::FrameLoadTypeIndexedBackForward);
+    }
 }
 
 /*!
