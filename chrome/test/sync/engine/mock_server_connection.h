01733b913334f5214e44a0a0764d8a9652ccf3a6
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-24 20:24:48 +0000

    Use chrome/base synchronization primitives and threads instead of pthreads in SyncerThread.  The old pthread impl can be used by specifying --syncer-thread-pthreads for comparison until we settle fully on the final impl (I have a MessageLoop-based impl in progress).
    The default SyncerThread is as close to the pthreads-impl semantics as
    I could get, with one exception: it does not offer a time-out
    when calling Stop(), because it greatly simplifies the implementation.
    I first implemented it *with* the timeout, and for sake of
    experimentation while this is in shuffle I am checking it in
    as SyncerThreadTimedStop, available by using --syncer-thread-timed-stop. I'm not sure which we want ultimately, but it's useful to have around when building the MessageLoop based impl.
    
    I had to refactor the interface slightly to allow multiple implementations,
    I think it will be quite useful while working on the MessageLoop impl.
    
    Added several tests to SyncerThreadUnittest, which all impls now pass (
    just pass the command line flag to try each out).
    
    TEST=SyncerThreadTest, SyncerThreadWithSyncerTest, integration tests
    
    Review URL: http://codereview.chromium.org/214033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27117 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/sync/engine/mock_server_connection.h
+++ chrome/test/sync/engine/mock_server_connection.h
@@ -32,6 +32,10 @@ class MockConnectionManager : public browser_sync::ServerConnectionManager {
   // activity would normally take place. This aids simulation of race
   // conditions.
   typedef bool (*TestCallbackFunction)(syncable::Directory* dir);
+  class MidCommitObserver {
+   public:
+    virtual void Observe() = 0;
+  };
 
   MockConnectionManager(syncable::DirectoryManager* dirmgr, PathString name);
   virtual ~MockConnectionManager();
@@ -46,6 +50,7 @@ class MockConnectionManager : public browser_sync::ServerConnectionManager {
 
   // Control of commit response.
   void SetMidCommitCallbackFunction(TestCallbackFunction callback);
+  void SetMidCommitObserver(MidCommitObserver* observer);
 
   // Set this if you want commit to perform commit time rename. Will request
   // that the client renames all commited entries, prepending this string.
@@ -178,11 +183,13 @@ class MockConnectionManager : public browser_sync::ServerConnectionManager {
   bool fail_next_postbuffer_;
 
   // Our directory.
-  syncable::ScopedDirLookup directory_;
+  syncable::DirectoryManager* directory_manager_;
+  PathString directory_name_;
 
   // The updates we'll return to the next request.
   sync_pb::GetUpdatesResponse updates_;
   TestCallbackFunction mid_commit_callback_function_;
+  MidCommitObserver* mid_commit_observer_;
 
   scoped_ptr<sync_pb::ClientCommand> client_command_;
 
