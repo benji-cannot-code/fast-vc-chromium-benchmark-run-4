5a1c27511c596cdbc88dec2ffb4353d76f7d9cc8
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-24 21:05:36 +0000

    Reverting 27117. Review URL: http://codereview.chromium.org/235010
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27124 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -141,8 +141,6 @@ extern const wchar_t kGearsPluginPathOverride[];
 extern const wchar_t kEnableFastback[];
 
 extern const wchar_t kEnableSync[];
-extern const wchar_t kSyncerThreadTimedStop[];
-extern const wchar_t kSyncerThreadPthreads[];
 
 extern const wchar_t kSdchFilter[];
 
