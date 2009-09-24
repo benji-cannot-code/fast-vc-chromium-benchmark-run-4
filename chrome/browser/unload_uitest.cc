6e2a24d759912607b8939c7e8adb77ea1413f2c2
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-24 19:15:50 +0000

    Temporarily disable BrowserCloseBeforeUnload* tests while I am working on fixing them.
    TBR=thomasvl
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/222023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27097 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/unload_uitest.cc
+++ chrome/browser/unload_uitest.cc
@@ -241,7 +241,7 @@ TEST_F(UnloadTest, BrowserCloseUnload) {
 // Tests closing the browser with a beforeunload handler and clicking
 // OK in the beforeunload confirm dialog.
 #if !defined(OS_LINUX)
-TEST_F(UnloadTest, BrowserCloseBeforeUnloadOK) {
+TEST_F(UnloadTest, DISABLED_BrowserCloseBeforeUnloadOK) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   NavigateToDataURL(BEFORE_UNLOAD_HTML, L"beforeunload");
 
@@ -253,7 +253,7 @@ TEST_F(UnloadTest, BrowserCloseBeforeUnloadOK) {
 
 // Tests closing the browser with a beforeunload handler and clicking
 // CANCEL in the beforeunload confirm dialog.
-TEST_F(UnloadTest, BrowserCloseBeforeUnloadCancel) {
+TEST_F(UnloadTest, DISABLED_BrowserCloseBeforeUnloadCancel) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   NavigateToDataURL(BEFORE_UNLOAD_HTML, L"beforeunload");
 
