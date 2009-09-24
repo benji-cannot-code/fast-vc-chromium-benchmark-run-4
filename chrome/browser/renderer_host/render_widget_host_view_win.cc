79d0b9a8079c95c3e515f363e9e7f33a4fab354a
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-24 16:02:17 +0000

    Add a DCHECK to get more info about flaky NPAPIVisiblePluginTester failures.
    Example failure:
    
    [FATAL:render_widget_host_view_win.cc(399)] Check failed: parent.
    
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/220021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27073 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_win.cc
+++ chrome/browser/renderer_host/render_widget_host_view_win.cc
@@ -391,6 +391,7 @@ HWND RenderWidgetHostViewWin::ReparentWindow(HWND window) {
     wcex.hIconSm        = 0;
     window_class = RegisterClassEx(&wcex);
   }
+  DCHECK(window_class);
 
   HWND parent = CreateWindowEx(
       WS_EX_LEFT | WS_EX_LTRREADING | WS_EX_RIGHTSCROLLBAR,
