23d1bd8523ecacbf3a1c67aae25999da61bc9d9d
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-25 19:56:12 +0000

    Use delegates in AutocompleteEditViewMac and do other stuff to get it to build with the 10.6 SDK. Review URL: http://codereview.chromium.org/244009
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27227 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/autocomplete_text_field_unittest_helper.h
+++ chrome/browser/cocoa/autocomplete_text_field_unittest_helper.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#import "base/cocoa_protocols_mac.h"
 #include "base/scoped_nsobject.h"
 #import "chrome/browser/cocoa/autocomplete_text_field.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -16,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Return the right field editor for AutocompleteTextField instance.
 
-@interface AutocompleteTextFieldWindowTestDelegate : NSObject {
+@interface AutocompleteTextFieldWindowTestDelegate :
+    NSObject<NSWindowDelegate> {
   scoped_nsobject<AutocompleteTextFieldEditor> editor_;
 }
 - (id)windowWillReturnFieldEditor:(NSWindow *)sender toObject:(id)anObject;
