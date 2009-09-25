71df3a8b16b18b000097324f19c729863340e04d
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-25 19:15:01 +0000

    Committing change for Roger.  Silencing turning GetTypeInfo DCHECK into a DLOG.
    TEST=none
    BUG=none
    TBR=robertshield
    
    Review URL: http://codereview.chromium.org/242008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/com_message_event.cc
+++ chrome_frame/com_message_event.cc
@@ -41,7 +41,7 @@ bool ComMessageEvent::Initialize(IOleContainer* container,
     }
   }
 
-  basic_event_ = basic_event;  
+  basic_event_ = basic_event;
   return true;
 }
 
@@ -49,13 +49,13 @@ STDMETHODIMP ComMessageEvent::GetTypeInfoCount(UINT* info) {
   // Don't DCHECK as python scripts might still call this function
   // inadvertently.
   DLOG(WARNING) << "Not implemented: " << __FUNCTION__;
-  return E_NOTIMPL;  
+  return E_NOTIMPL;
 }
 
 STDMETHODIMP ComMessageEvent::GetTypeInfo(UINT which_info, LCID lcid,
                                           ITypeInfo** type_info) {
-  NOTREACHED();
-  return E_NOTIMPL;  
+  DLOG(WARNING) << "Not implemented: " << __FUNCTION__;
+  return E_NOTIMPL;
 }
 
 STDMETHODIMP ComMessageEvent::GetIDsOfNames(REFIID iid, LPOLESTR* names,
@@ -95,7 +95,7 @@ STDMETHODIMP ComMessageEvent::GetIDsOfNames(REFIID iid, LPOLESTR* names,
       }
     }
   }
-  return hr;                           
+  return hr;
 }
 
 STDMETHODIMP ComMessageEvent::Invoke(DISPID dispid, REFIID iid, LCID lcid,
