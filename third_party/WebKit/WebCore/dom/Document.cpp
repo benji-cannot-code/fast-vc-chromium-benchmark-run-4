c9167385ba1186d24f7c12296277ca01e2a36e1f
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-25 22:24:45 +0000

    https://bugs.webkit.org/show_bug.cgi?id=24399 Make @import work in user stylesheets.  The first bug was that the URL wasn't being set on the user sheets themselves, so relative @import URLs couldn't resolve properly.  The second bug was that the loads would be denied.  This is fixed by using the requestUserCSSStyleSheet method instead of the normal request method.  In order to know when to do this, CSSStyleSheets now have a propagated boolean, m_isUserStyleSheet, that lets them know if they are user stylesheets or not.
    Reviewed by Anders Carlsson.
    
    * css/CSSImportRule.cpp:
    (WebCore::CSSImportRule::insertedIntoParent):
    * css/CSSStyleSheet.cpp:
    (WebCore::CSSStyleSheet::CSSStyleSheet):
    * css/CSSStyleSheet.h:
    (WebCore::CSSStyleSheet::setIsUserStyleSheet):
    (WebCore::CSSStyleSheet::isUserStyleSheet):
    * dom/Document.cpp:
    (WebCore::Document::pageUserSheet):
    (WebCore::Document::pageGroupUserSheets):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48773 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -1942,7 +1942,8 @@ CSSStyleSheet* Document::pageUserSheet()
         return 0;
     
     // Parse the sheet and cache it.
-    m_pageUserSheet = CSSStyleSheet::create(this);
+    m_pageUserSheet = CSSStyleSheet::create(this, settings()->userStyleSheetLocation());
+    m_pageUserSheet->setIsUserStyleSheet(true);
     m_pageUserSheet->parseString(userSheetText, !inCompatMode());
     return m_pageUserSheet.get();
 }
@@ -1974,7 +1975,8 @@ const Vector<RefPtr<CSSStyleSheet> >* Document::pageGroupUserSheets() const
         const UserStyleSheetVector* sheets = it->second;
         for (unsigned i = 0; i < sheets->size(); ++i) {
             const UserStyleSheet* sheet = sheets->at(i).get();
-            RefPtr<CSSStyleSheet> parsedSheet = CSSStyleSheet::create(const_cast<Document*>(this));
+            RefPtr<CSSStyleSheet> parsedSheet = CSSStyleSheet::create(const_cast<Document*>(this), sheet->url());
+            parsedSheet->setIsUserStyleSheet(true);
             parsedSheet->parseString(sheet->source(), !inCompatMode());
             if (!m_pageGroupUserSheets)
                 m_pageGroupUserSheets.set(new Vector<RefPtr<CSSStyleSheet> >);
