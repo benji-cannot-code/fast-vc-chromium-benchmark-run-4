3ef569479d12af8682f46f3fb498ba2cde83be39
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-25 23:11:46 +0000

    2009-09-25  Darin Fisher  <darin@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Declare RegisteredEventListener as a class instead of a struct.
            This fixes a warning in the Chromium build.
    
            * dom/RegisteredEventListener.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/RegisteredEventListener.h
+++ third_party/WebKit/WebCore/dom/RegisteredEventListener.h
@@ -30,7 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    struct RegisteredEventListener {
+    class RegisteredEventListener {
+    public:
         RegisteredEventListener(PassRefPtr<EventListener> listener, bool useCapture)
             : listener(listener)
             , useCapture(useCapture)
