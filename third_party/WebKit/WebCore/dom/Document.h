5310eb286a2efafe9f351be756870e90ac071654
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-25 17:53:43 +0000

    2009-09-25  Yuan Song  <song.yuan@ericsson.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=14566
    
            Add test "invalid-domain-change-throws-exception.html" to ensure a SECURITY_ERR exception is raised if an attempt is made to change document.domain to an invalid value. In the existing test case "basic-textareas.html", fix the faulty behavior of setting document.domain to invalid value, and replace the data URL to a file URL that points to "basic-textareas-standards.html" in order to make "basic-textareas.html" runnable in a browser.
    
            * fast/forms/basic-textareas.html:
            * fast/forms/resources/basic-textareas-standards.html: Added.
            * fast/js/invalid-domain-change-throws-exception-expected.txt: Added.
            * fast/js/invalid-domain-change-throws-exception.html: Added.
            * fast/js/resources/invalid-domain-change-throws-exception.js: Added.
    2009-09-25  Yuan Song  <song.yuan@ericsson.com>
    
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=14566
    
            Raise SECURITY_ERR exception if an attempt is made to change document.domain to an invalid value.
    
            Test: fast/js/invalid-domain-change-throws-exception.html
    
            * dom/Document.cpp:
            (WebCore::Document::setDomain):
            * dom/Document.h:
            * dom/Document.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48761 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.h
+++ third_party/WebKit/WebCore/dom/Document.h
@@ -673,7 +673,7 @@ public:
     String referrer() const;
 
     String domain() const;
-    void setDomain(const String& newDomain);
+    void setDomain(const String& newDomain, ExceptionCode&);
 
     String lastModified() const;
 
