e8965cb4992b3dda98aa94c1b65caaee23c2bbfe
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-25 00:07:31 +0000

    Clean up a few bits of files that I'm about to modify.  The only visible change is the removal of some extra spacing Ben erroneously added to the link infobar a while back (clients already do their own spacing as needed, so this just added extra whitespace in mid-sentence).
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/223029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27151 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/link.h
+++ views/controls/link.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -33,8 +33,6 @@ class LinkController {
 ////////////////////////////////////////////////////////////////////////////////
 class Link : public Label {
  public:
-  static const char kViewClassName[];
-
   Link();
   Link(const std::wstring& title);
   virtual ~Link();
@@ -63,6 +61,8 @@ class Link : public Label {
   void SetHighlightedColor(const SkColor& color);
   void SetDisabledColor(const SkColor& color);
   void SetNormalColor(const SkColor& color);
+
+  static const char kViewClassName[];
 
  private:
 
