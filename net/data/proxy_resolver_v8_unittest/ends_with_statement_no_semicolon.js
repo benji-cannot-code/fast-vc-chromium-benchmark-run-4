6b8c8748f7647d3b100695249fb2e0c146266062
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-25 04:52:39 +0000

    Add an additional unit-test for when PAC script is missing newline. This variation uses a statement without semi-colon, rather than a comment, as the last line.
    BUG=http://crbug.com/22864
    
    Review URL: http://codereview.chromium.org/238004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27171 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/data/proxy_resolver_v8_unittest/ends_with_statement_no_semicolon.js
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Ends with a statement, and no terminal newline.
+function FindProxyForURL(url, host) { return "PROXY success:" + x; }
+x = 3
