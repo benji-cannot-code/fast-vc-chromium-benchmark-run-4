20fed281df3604b83b973e069c444656d36a9afa
erikkay@chromium.org <erikkay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-25 22:31:24 +0000

    TBR: rafaelw,leiz disable a ExtensionApiTest.Toolstrip which is flaky on linux
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27265 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_toolstrip_apitest.cc
+++ chrome/browser/extensions/extension_toolstrip_apitest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Toolstrip) {
+// TODO(rafaelw,erikkay) disabled due to flakiness
+// BUG=22668 (probably the same bug)
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Toolstrip) {
   ASSERT_TRUE(RunExtensionTest("toolstrip")) << message_;
 }
