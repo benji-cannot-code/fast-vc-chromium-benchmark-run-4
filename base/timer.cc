f9d908274636c76e95f9582885546acb8aab5ff5
jshin@chromium.org <jshin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-25 01:13:20 +0000

    Roll back Markus's CL ( http://codereview.chromium.org/196053)
    BUG=none
    TEST=UI test and valgrind test pass
    TBR=markus
    Review URL: http://codereview.chromium.org/222031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27160 0039d316-1c4b-4281-b951-d872f2087c98

--- base/timer.cc
+++ base/timer.cc
@@ -23,7 +23,7 @@ void BaseTimer_Helper::InitiateDelayedTask(TimerTask* timer_task) {
   delayed_task_->timer_ = this;
   MessageLoop::current()->PostDelayedTask(
       FROM_HERE, timer_task,
-      static_cast<int>(timer_task->delay_.InMillisecondsRoundedUp()));
+      static_cast<int>(timer_task->delay_.InMilliseconds()));
 }
 
 }  // namespace base
