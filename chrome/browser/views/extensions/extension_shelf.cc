fa86edebc2c7ffb34e53cf7b0190068d83c59ee6
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-25 21:50:56 +0000

    Make --show-extensions-on-top look exactly, pixel-for-pixel identical to the bookmark bar.
    Used the same semi-transparent image for the button background
    that the bookmarkbar uses, so that it will work for themes.
    
    The upside of this is that we look the same as the bookmark bar. The downside is that we look the same as the bookmark bar (which currently looks pretty bad with some themes).
    
    However, with the default theme, I think that the bookmark bar currently looks better than our toolstrips. The UI team should probably create some images that look good with different themes for the bookmark bar, then we can update this again.
    
    Review URL: http://codereview.chromium.org/225025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27258 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/extensions/extension_shelf.cc
+++ chrome/browser/views/extensions/extension_shelf.cc
@@ -32,7 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Margins around the content.
-static const int kTopMargin = 2;
+static const int kTopMarginWhenExtensionsOnTop = 1;
+static const int kTopMarginWhenExtensionsOnBottom = 2;
 static const int kBottomMargin = 2;
 static const int kLeftMargin = 0;
 static const int kRightMargin = 0;
@@ -59,9 +60,6 @@ static const int kNewtabVerticalPadding = 12;
 // corners of the extension shelf.
 static const int kNewtabExtraHorMargin = 2;
 static const int kNewtabExtraVerMargin = 2;
-
-// Height of the toolstrip within the shelf.
-static const int kToolstripHeight = kShelfHeight - (kTopMargin + kBottomMargin);
 
 // Padding for the title inside the handle.
 static const int kHandlePadding = 4;
@@ -637,7 +635,8 @@ void ExtensionShelf::Toolstrip::Collapse(const GURL& url) {
     mole_animation_->Hide();
 
   gfx::Size extension_size = view()->GetPreferredSize();
-  extension_size.set_height(kToolstripHeight);
+  extension_size.set_height(
+      kShelfHeight - (shelf_->top_margin() + kBottomMargin));
   view()->SetPreferredSize(extension_size);
 
   if (!url.is_empty() && url != host_->GetURL()) {
@@ -685,6 +684,12 @@ ExtensionShelf::ExtensionShelf(Browser* browser)
       model_(browser->extension_shelf_model()),
       fullscreen_(false) {
   SetID(VIEW_ID_DEV_EXTENSION_SHELF);
+
+  if (IsOnTop())
+    top_margin_ = kTopMarginWhenExtensionsOnTop;
+  else
+    top_margin_ = kTopMarginWhenExtensionsOnBottom;
+
   model_->AddObserver(this);
   LoadFromModel();
   EnableCanvasFlippingForRTLUI(true);
@@ -1036,8 +1041,8 @@ gfx::Size ExtensionShelf::LayoutItems(bool compute_bounds_only) {
 
   gfx::Size prefsize;
   int x = kLeftMargin;
-  int y = kTopMargin;
-  int content_height = kShelfHeight - kTopMargin - kBottomMargin;
+  int y = top_margin_;
+  int content_height = kShelfHeight - top_margin_ - kBottomMargin;
   int max_x = width() - kRightMargin;
 
   if (OnNewTabPage()) {
