720958750fa85cb8db05dd10359fda9278b9e567
gregoryd@google.com <gregoryd@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-26 09:47:54 +0000

    First step towards NaCl-Chrome integration:1. NaCl plugin becomes a built-in plugin in Chrome and runs in the renderer process.2. Most of the changes are related to launching the NaCl process (that loads and runs the NaCl module) and establishing the initial communication between that process and the NaCl plugin.3. Command line flag "--internal-nacl" is required to enable the built-in NaCl plugin. NaCl still cannot run in Chrome sandbox, the flag automatically disables the sandboxCommitted: http://src.chromium.org/viewvc/chrome?view=rev&revision=27315 Review URL: http://codereview.chromium.org/153002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27324 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/nacl/nacl_thread.h
+++ chrome/nacl/nacl_thread.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -24,13 +24,10 @@ class NaClThread : public ChildThread {
 
  private:
   virtual void OnControlMessageReceived(const IPC::Message& msg);
-  void OnStartSelLdr(const int channel_descriptor,
-                     const nacl::FileDescriptor handle);
+  void OnStartSelLdr(int channel_descriptor, nacl::FileDescriptor handle);
+
   // TODO(gregoryd): do we need to override Cleanup as in PluginThread?
-
-  scoped_ptr<NotificationService> notification_service_;
-
-  DISALLOW_EVIL_CONSTRUCTORS(NaClThread);
+  DISALLOW_COPY_AND_ASSIGN(NaClThread);
 };
 
 #endif  // CHROME_NACL_NACL_THREAD_H_
