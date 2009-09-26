adea6551138a1a8221f89f31399944e5cbe46a7f
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-26 06:31:43 +0000

    Build fix. Adding missing header files.
    git-svn-id: svn://svn.chromium.org/blink/trunk@48785 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSNamedNodeMapCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSNamedNodeMapCustom.cpp
@@ -29,6 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "JSNode.h"
 
+#include "Element.h"
+#include "NamedNodeMap.h"
+
 using namespace JSC;
 
 namespace WebCore {
