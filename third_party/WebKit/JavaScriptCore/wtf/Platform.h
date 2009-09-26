65308c03150212da605d4b6f7f566fab798a8f3c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-26 02:27:02 +0000

    2009-09-25  Gabor Loki  <loki@inf.u-szeged.hu>
            Reviewed by Gavin Barraclough.
    
            Fix unaligned data access in YARR_JIT on ARMv5 and below.
            https://bugs.webkit.org/show_bug.cgi?id=29695
    
            On ARMv5 and below all data access should be naturally aligned.
            In the YARR_JIT there is a case when character pairs are
            loaded from the input string, but this data access is not
            naturally aligned. This fix introduces load32WithUnalignedHalfWords
            and branch32WithUnalignedHalfWords functions which contain
            naturally aligned memory loads - half word loads - on ARMv5 and below.
    
            * assembler/MacroAssemblerARM.cpp:
            (JSC::MacroAssemblerARM::load32WithUnalignedHalfWords):
            * assembler/MacroAssemblerARM.h:
            (JSC::MacroAssemblerARM::load32WithUnalignedHalfWords):
            (JSC::MacroAssemblerARM::branch32WithUnalignedHalfWords):
            * assembler/MacroAssemblerARMv7.h:
            (JSC::MacroAssemblerARMv7::load32WithUnalignedHalfWords):
            (JSC::MacroAssemblerARMv7::branch32):
            (JSC::MacroAssemblerARMv7::branch32WithUnalignedHalfWords):
            * assembler/MacroAssemblerX86Common.h:
            (JSC::MacroAssemblerX86Common::load32WithUnalignedHalfWords):
            (JSC::MacroAssemblerX86Common::branch32WithUnalignedHalfWords):
            * wtf/Platform.h:
            * yarr/RegexJIT.cpp:
            (JSC::Yarr::RegexGenerator::generatePatternCharacterPair):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -258,6 +258,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #undef ARM_ARCH_VERSION
 #define ARM_ARCH_VERSION 7
 #endif
+/* On ARMv5 and below the natural alignment is required. */
+#if !defined(ARM_REQUIRE_NATURAL_ALIGNMENT) && ARM_ARCH_VERSION <= 5
+#define ARM_REQUIRE_NATURAL_ALIGNMENT 1
+#endif
 /* Defines two pseudo-platforms for ARM and Thumb-2 instruction set. */
 #if !defined(WTF_PLATFORM_ARM_TRADITIONAL) && !defined(WTF_PLATFORM_ARM_THUMB2)
 #  if defined(thumb2) || defined(__thumb2__)
