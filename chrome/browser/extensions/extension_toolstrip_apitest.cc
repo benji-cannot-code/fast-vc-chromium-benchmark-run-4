08d1212bf65452c7d5494afae96ca8fe51dc9a97
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-27 19:50:54 +0000

    The comment above the ExtensionApiTest.Toolstrip says that it's disabled because of flakyness, but it's not disabled, and it's really flaky, so i'm disabling it for real.
    BUG:22668
    
    Review URL: http://codereview.chromium.org/242027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27342 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_toolstrip_apitest.cc
+++ chrome/browser/extensions/extension_toolstrip_apitest.cc
@@ -7,6 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(rafaelw,erikkay) disabled due to flakiness
 // BUG=22668 (probably the same bug)
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Toolstrip) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Toolstrip) {
   ASSERT_TRUE(RunExtensionTest("toolstrip")) << message_;
 }
