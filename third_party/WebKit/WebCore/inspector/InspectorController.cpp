d8b074e1bb7951a27193d9e4833c78c28481baeb
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-27 09:19:13 +0000

    2009-09-27  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: DOM store is being unbound twice, leading to assertion failure.
    
            https://bugs.webkit.org/show_bug.cgi?id=29770
    
            * inspector/InspectorController.cpp:
            (WebCore::InspectorController::didOpenDatabase):
            (WebCore::InspectorController::didUseDOMStorage):
            * inspector/InspectorDOMStorageResource.cpp:
            (WebCore::InspectorDOMStorageResource::unbind):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48799 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorController.cpp
+++ third_party/WebKit/WebCore/inspector/InspectorController.cpp
@@ -1095,7 +1095,8 @@ void InspectorController::didOpenDatabase(Database* database, const String& doma
 
     m_databaseResources.add(resource);
 
-    if (m_frontend)
+    // Resources are only bound while visible.
+    if (m_frontend && windowVisible())
         resource->bind(m_frontend.get());
 }
 #endif
@@ -1116,7 +1117,8 @@ void InspectorController::didUseDOMStorage(StorageArea* storageArea, bool isLoca
 
     m_domStorageResources.add(resource);
 
-    if (m_frontend)
+    // Resources are only bound while visible.
+    if (m_frontend && windowVisible())
         resource->bind(m_frontend.get());
 }
 
