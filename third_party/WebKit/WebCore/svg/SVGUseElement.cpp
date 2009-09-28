7a44429177c4a30c8287ba3862cc1092713bec3d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-28 13:35:39 +0000

    2009-09-28  Charles Wei  <charles.wei@torchmobile.com.cn>
            Reviewed by Nikolas Zimmermann.
    
            Fix the crash of SVG that crashes when use a non-exist symbol
            https://bugs.webkit.org/show_bug.cgi?id=27693
    
            * svg/custom/use-non-existing-symbol-crash-expected.txt: Added.
            * svg/custom/use-non-existing-symbol-crash.svg: Added.
    2009-09-28  Charles Wei  <charles.wei@torchmobile.com.cn>
    
            Reviewed by Nikolas Zimmermann.
    
            Fix the crash of SVG that crashes when use a non-exist symbol
            https://bugs.webkit.org/show_bug.cgi?id=27693
    
            Test: svg/custom/use-non-existing-symbol-crash.svg
    
            * svg/SVGUseElement.cpp:
            (WebCore::shadowTreeContainsChangedNodes):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGUseElement.cpp
+++ third_party/WebKit/WebCore/svg/SVGUseElement.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
     Copyright (C) 2004, 2005, 2006, 2007, 2008 Nikolas Zimmermann <zimmermann@kde.org>
                   2004, 2005, 2006, 2007 Rob Buis <buis@kde.org>
+    Copyright (C) Research In Motion Limited 2009, All rights reserved. Charles Wei <charles.wei@torchmobile.com.cn>
 
     This file is part of the KDE project
 
@@ -159,6 +160,9 @@ void SVGUseElement::childrenChanged(bool changedByParser, Node* beforeChange, No
  
 static bool shadowTreeContainsChangedNodes(SVGElementInstance* target)
 {
+    if (!target)      // when use is referencing an non-existing element, there will be no Instance tree built
+        return false;
+
     if (target->needsUpdate())
         return true;
 
