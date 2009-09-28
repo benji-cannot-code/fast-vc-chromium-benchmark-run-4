6c481ad2c26bca6f121d798f2891a69b3cbe6d5a
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-28 18:39:54 +0000

    Build fix take 2.
    (We don't have try servers for ChromeOS yet.)
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27385 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/breakpad_linux.h
+++ chrome/app/breakpad_linux.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_APP_BREAKPAD_LINUX_H_
 #define CHROME_APP_BREAKPAD_LINUX_H_
 
+#include <stdlib.h>
+
 extern void InitCrashReporter();
 
 static const size_t kMaxActiveURLSize = 1024;
