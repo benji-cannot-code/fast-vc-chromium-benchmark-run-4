3dbbea4fe1657133efd9dd8f221d9462cd07ca39
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-28 18:23:09 +0000

    Build fix: ChromeOS is using a more recent compiler.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27380 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/breakpad_linux.cc
+++ chrome/app/breakpad_linux.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
 
