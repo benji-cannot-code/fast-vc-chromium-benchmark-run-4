806da9e3a5c50cd9df104ac826d53f39393d6066
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-28 22:30:53 +0000

    Chromium side implementations of some FileSystem methods. This is needed to complete the WebKit bug fix https://bugs.webkit.org/show_bug.cgi?id=29109.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/209072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27420 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkitclient_impl.cc
+++ webkit/glue/webkitclient_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event.h"
 #include "grit/webkit_resources.h"
 #include "grit/webkit_strings.h"
+#include "net/base/net_util.h"
 #include "webkit/api/public/WebCursorInfo.h"
 #include "webkit/api/public/WebData.h"
 #include "webkit/api/public/WebFrameClient.h"
@@ -462,5 +463,23 @@ void WebKitClientImpl::widgetSetFocus(WebCore::Widget* widget) {
 WebCore::WorkerContextProxy* WebKitClientImpl::createWorkerContextProxy(
     WebCore::Worker* worker) {
   return WebWorkerClientImpl::createWorkerContextProxy(worker);
+}
+
+WebKit::WebString WebKitClientImpl::getAbsolutePath(
+    const WebKit::WebString& path) {
+  FilePath file_path(webkit_glue::WebStringToFilePathString(path));
+  file_util::AbsolutePath(&file_path);
+  return webkit_glue::FilePathStringToWebString(file_path.value());
+}
+
+bool WebKitClientImpl::isDirectory(const WebKit::WebString& path) {
+  FilePath file_path(webkit_glue::WebStringToFilePathString(path));
+  return file_util::DirectoryExists(file_path);
+}
+
+WebKit::WebURL WebKitClientImpl::filePathToURL(const WebKit::WebString& path) {
+  FilePath file_path(webkit_glue::WebStringToFilePathString(path));
+  GURL file_url = net::FilePathToFileURL(file_path);
+  return webkit_glue::KURLToWebURL(webkit_glue::GURLToKURL(file_url));
 }
 }  // namespace webkit_glue
