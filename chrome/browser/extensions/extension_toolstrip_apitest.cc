c8c132e8e458cf1c32ea3c28befe4dcc07f7102c
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-29 18:28:47 +0000

    Enable ExtensionApiTest.Toolstrip and add additional debug output.
    This output is testing the prospect that the cause of the flakiness is a race to the IO thread between the renderer that is requesting a chrome-extension page and the message to insert the correct mapping to service the request.
    
    BUG=22668
    
    Review URL: http://codereview.chromium.org/251025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27516 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_toolstrip_apitest.cc
+++ chrome/browser/extensions/extension_toolstrip_apitest.cc
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
+// TEMPORARILY ENABLED TO GET DEBUG OUTPUT:
 // TODO(rafaelw,erikkay) disabled due to flakiness
 // BUG=22668 (probably the same bug)
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Toolstrip) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Toolstrip) {
   ASSERT_TRUE(RunExtensionTest("toolstrip")) << message_;
 }
