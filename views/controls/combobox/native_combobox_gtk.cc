48012fdba154b4f739e4ac225333bf010ca13929
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-29 22:03:33 +0000

    style fix. the header for .cc has to be the 1st include.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/242045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27537 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/combobox/native_combobox_gtk.cc
+++ views/controls/combobox/native_combobox_gtk.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
-#include <gtk/gtk.h>
-
 #include "views/controls/combobox/native_combobox_gtk.h"
+
+#include <gtk/gtk.h>
 
 #include "app/combobox_model.h"
 #include "base/logging.h"
