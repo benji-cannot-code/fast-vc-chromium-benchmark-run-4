560cd73a1f81252dc5d6da06a24dfa1cb404688f
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-29 04:01:09 +0000

    2009-09-28  Yaar Schnitman  <yaar@chromium.org>
            Reviewed by David Kilzer.
    
            Integrated chromium port building into webkit tools update-webkit and
            build-webkit.
    
            https://bugs.webkit.org/show_bug.cgi?id=29749
    
            * Scripts/build-webkit: When --chromium is specified, will build
              the chromium port (currently only Mac is supported).
            * Scripts/update-webkit: When --chromium is specified, delegates to
              update-webkit-chromium.
            * Scripts/webkitdirs.pm: Added chromium specific defs.
            * Scripts/update-webkit-chromium: Uses gclient and gyp to fetch
              chromium port's dependencies and update its project files.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48853 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (C) 2005, 2006, 2007 Apple Inc. All rights reserved.
+# Copyright (C) 2009 Google Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -996,7 +997,7 @@ sub checkRequiredSystemConfig
             print "http://developer.apple.com/tools/xcode\n";
             print "*************************************************************\n";
         }
-    } elsif (isGtk() or isQt() or isWx()) {
+    } elsif (isGtk() or isQt() or isWx() or isChromium()) {
         my @cmds = qw(flex bison gperf);
         my @missing = ();
         foreach my $cmd (@cmds) {
@@ -1355,6 +1356,28 @@ sub buildGtkProject($$@)
     }
 
     return buildAutotoolsProject($clean, @buildArgs);
+}
+
+sub buildChromium($@)
+{
+    my ($clean, @options) = @_;
+
+    my $result = 1;
+    if (isDarwin()) {
+        # Mac build - builds the root xcode project.
+        $result = buildXCodeProject("WebKit/chromium/webkit", $clean, (@options));
+    } elsif (isCygwin()) {
+        # Windows build
+        # FIXME support windows.
+        print STDERR "Windows build is not supported. Yet.";
+    } elsif (isLinux()) {
+        # Linux build
+        # FIXME support linux.
+        print STDERR "Linux build is not supported. Yet.";
+    } else {
+        print STDERR "This platform is not supported by chromium.";
+    }
+    return $result;
 }
 
 sub setPathForRunningWebKitApp
