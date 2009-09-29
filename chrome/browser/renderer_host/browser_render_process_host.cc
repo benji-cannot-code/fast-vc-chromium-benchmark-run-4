f4413bfbe9258ba499e493cf2b1fbd5eb601624c
gregoryd@google.com <gregoryd@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-29 00:33:46 +0000

    First step towards NaCl-Chrome integration:1. NaCl plugin becomes a built-in plugin in Chrome and runs in the renderer process.2. Most of the changes are related to launching the NaCl process (that loads and runs the NaCl module) and establishing the initial communication between that process and the NaCl plugin.3. Command line flag "--internal-nacl" is required to enable the built-in NaCl plugin. NaCl still cannot run in Chrome sandbox, the flag automatically disables the sandboxCommitted: http://src.chromium.org/viewvc/chrome?view=rev&revision=27315
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=27324
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=27397
    Review URL: http://codereview.chromium.org/153002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27445 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -330,6 +330,7 @@ bool BrowserRenderProcessHost::Init() {
     switches::kDisableAudio,
     switches::kSimpleDataSource,
     switches::kEnableBenchmarking,
+    switches::kInternalNaCl,
     switches::kEnableDatabases,
   };
 
--- chrome/browser/renderer_host/resource_message_filter.cc
+++ chrome/browser/renderer_host/resource_message_filter.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/extensions/extension_message_service.h"
 #include "chrome/browser/in_process_webkit/dom_storage_dispatcher_host.h"
+#include "chrome/browser/nacl_process_host.h"
 #include "chrome/browser/net/chrome_url_request_context.h"
 #include "chrome/browser/net/dns_global.h"
 #include "chrome/browser/plugin_service.h"
@@ -296,6 +297,7 @@ bool ResourceMessageFilter::OnMessageReceived(const IPC::Message& msg) {
                                   OnReceiveContextMenuMsg(msg))
       IPC_MESSAGE_HANDLER_DELAY_REPLY(ViewHostMsg_OpenChannelToPlugin,
                                       OnOpenChannelToPlugin)
+      IPC_MESSAGE_HANDLER(ViewHostMsg_LaunchNaCl, OnLaunchNaCl)
       IPC_MESSAGE_HANDLER(ViewHostMsg_CreateDedicatedWorker,
                           OnCreateDedicatedWorker)
       IPC_MESSAGE_HANDLER(ViewHostMsg_CancelCreateDedicatedWorker,
@@ -582,6 +584,12 @@ void ResourceMessageFilter::OnOpenChannelToPlugin(const GURL& url,
                                                   IPC::Message* reply_msg) {
   plugin_service_->OpenChannelToPlugin(
       this, url, mime_type, locale, reply_msg);
+}
+
+void ResourceMessageFilter::OnLaunchNaCl(int channel_descriptor,
+                                         nacl::FileDescriptor* handle) {
+  NaClProcessHost* nacl_host = new NaClProcessHost(resource_dispatcher_host_);
+  nacl_host->Launch(this, channel_descriptor, handle);
 }
 
 void ResourceMessageFilter::OnCreateDedicatedWorker(const GURL& url,
