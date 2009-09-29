0568f3fd7998e3aab75e75d01163a0873dbc4f4f
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-09-29 18:39:43 +0000

    Reviewed by Timothy Hatcher.
            Build fix - pass a proper ScriptExecutipnContext to getEventListenerHandlerBody()
    
            * bindings/js/ScriptEventListener.cpp:
            (WebCore::getEventListenerHandlerBody):
            * bindings/js/ScriptEventListener.h:
            * bindings/v8/ScriptEventListener.cpp:
            (WebCore::getEventListenerHandlerBody):
            * bindings/v8/ScriptEventListener.h:
            * inspector/InspectorDOMAgent.cpp:
            (WebCore::InspectorDOMAgent::buildObjectForEventListener):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48887 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/ScriptEventListener.h
+++ third_party/WebKit/WebCore/bindings/js/ScriptEventListener.h
@@ -46,7 +46,7 @@ namespace WebCore {
 
     PassRefPtr<JSLazyEventListener> createAttributeEventListener(Node*, Attribute*);
     PassRefPtr<JSLazyEventListener> createAttributeEventListener(Frame*, Attribute*);
-    String getEventListenerHandlerBody(ScriptState*, EventListener*);
+    String getEventListenerHandlerBody(ScriptExecutionContext*, ScriptState*, EventListener*);
 
 } // namespace WebCore
 
