b4670b8061a5ade4c125c0cbccb3f7708507f8b3
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-30 13:58:22 +0000

    Fix a bug where dragging a window that's been closed by JS back into another window would crash. Be more specific about when the window will be closed/deleted during dragging in hopes of making future crashes a little more clear. BUG=none TEST=dragging tabs between windows should work. tabs closed via window.close() on a timer should work. Review URL: http://codereview.chromium.org/246035
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27606 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_window_controller.mm
+++ chrome/browser/cocoa/tab_window_controller.mm
@@ -43,8 +43,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)removeOverlay {
   [self setUseOverlay:NO];
+  if (closeDeferred_)
+    [[self window] performClose:self];  // Autoreleases the controller.
 }
 
+// TODO(pinkerton): Nobody calls this, can we remove it?
 - (void)removeOverlayAfterDelay:(NSTimeInterval)delay {
   [NSObject cancelPreviousPerformRequestsWithTarget:self
                                            selector:@selector(removeOverlay)
@@ -198,6 +201,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [lockedTabs_ removeObject:tabView];
   else
     [lockedTabs_ addObject:tabView];
+}
+
+// Tell the window that it needs to call performClose: as soon as the current
+// drag is complete. This prevents a window (and its overlay) from going away
+// during a drag.
+- (void)deferPerformClose {
+  closeDeferred_ = YES;
 }
 
 @end
