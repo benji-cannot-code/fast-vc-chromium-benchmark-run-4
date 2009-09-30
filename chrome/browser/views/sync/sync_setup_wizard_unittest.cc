70bf3d1204056e0a678207b7aa6e32bd65f16b5c
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-30 22:48:58 +0000

    Tweak constants used by sync setup wizard unittest. Reviewed by nick.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27677 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/sync/sync_setup_wizard_unittest.cc
+++ chrome/browser/views/sync/sync_setup_wizard_unittest.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/test_browser_window.h"
 
 static const char* kTestUser = "chrome.p13n.test@gmail.com";
-static const char* kTestPassword = "g00gl3g00gl3";
+static const char* kTestPassword = "passwd";
 
 // A PSS subtype to inject.
 class ProfileSyncServiceForWizardTest : public ProfileSyncService {
