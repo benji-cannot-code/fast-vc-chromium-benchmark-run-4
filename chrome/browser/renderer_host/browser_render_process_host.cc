0e5f3e38534828e82929c06bf686cdc2048e3200
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-30 03:23:04 +0000

    Revert r27589 that broke the tree
    TBR=scherkus
    
    Review URL: http://codereview.chromium.org/246043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27590 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -332,7 +332,6 @@ bool BrowserRenderProcessHost::Init() {
     switches::kEnableBenchmarking,
     switches::kInternalNaCl,
     switches::kEnableDatabases,
-    switches::kEnableByteRangeSupport,
   };
 
   for (size_t i = 0; i < arraysize(switch_names); ++i) {
