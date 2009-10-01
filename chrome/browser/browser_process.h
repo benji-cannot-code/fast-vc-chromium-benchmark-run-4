c3bab8d816c520cd206ef6ff885e3eacb6ac3eb8
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-01 17:17:36 +0000

    Fix a typo in a comment.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/244053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27728 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process.h
+++ chrome/browser/browser_process.h
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This interfaces is for managing the global services of the application. Each
+// This interface is for managing the global services of the application. Each
 // service is lazily created when requested the first time. The service getters
 // will return NULL if the service is not available, so callers must check for
 // this condition.
