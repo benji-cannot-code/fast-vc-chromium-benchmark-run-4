536105117bb0ac8b3764b2d3aa0eb27078b17599
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-01 19:12:30 +0000

    Coverity: Remove a check for < 0 on an unsigned int.
    CID=4546
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/249047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27753 0039d316-1c4b-4281-b951-d872f2087c98

--- app/l10n_util.h
+++ app/l10n_util.h
@@ -9,12 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef APP_L10N_UTIL_H_
 #define APP_L10N_UTIL_H_
 
-#include "build/build_config.h"
-
 #include <algorithm>
 #include <functional>
 #include <string>
 #include <vector>
+
+#include "build/build_config.h"
 
 #include "base/basictypes.h"
 #include "base/logging.h"
@@ -328,12 +328,12 @@ void SortStringsUsingMethod(const std::wstring& locale,
   scoped_ptr<icu::Collator> collator(icu::Collator::createInstance(loc, error));
   if (U_FAILURE(error)) {
     sort(elements->begin(), elements->end(),
-         StringMethodComparator<T,Method>(method));
+         StringMethodComparator<T, Method>(method));
     return;
   }
 
   std::sort(elements->begin(), elements->end(),
-      StringMethodComparatorWithCollator<T,Method>(collator.get(), method));
+      StringMethodComparatorWithCollator<T, Method>(collator.get(), method));
 }
 
 // Compares two elements' string keys and returns true if the first element's
@@ -378,7 +378,7 @@ void SortVectorWithStringKey(const std::string& locale,
                              unsigned int begin_index,
                              unsigned int end_index,
                              bool needs_stable_sort) {
-  DCHECK(begin_index >= 0 && begin_index < end_index &&
+  DCHECK(begin_index < end_index &&
          end_index <= static_cast<unsigned int>(elements->size()));
   UErrorCode error = U_ZERO_ERROR;
   icu::Locale loc(locale.c_str());
