beba0be1f782aa6c34c966f355ee718641237ceb
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-01 16:59:28 +0000

    Make Software Input Panel requests work with QGraphicsWebView _and_ QWebView by sharing the event code in handleSoftwareInputPanel().
    Patch by Simon Hausmann <simon.hausmann@nokia.com> on 2009-10-01
    Reviewed by Tor Arne Vestbø.
    
    * Api/qwebpage.cpp:
    (QWebPagePrivate::mouseReleaseEvent):
    (QWebPagePrivate::handleSoftwareInputPanel):
    * Api/qwebpage_p.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@48977 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage_p.h
+++ third_party/WebKit/WebKit/qt/Api/qwebpage_p.h
@@ -112,6 +112,7 @@ public:
     void shortcutOverrideEvent(QKeyEvent*);
     void leaveEvent(QEvent*);
     void handleClipboard(QEvent*, Qt::MouseButton);
+    void handleSoftwareInputPanel(Qt::MouseButton);
     bool handleScrolling(QKeyEvent*, WebCore::Frame*);
 
     void setInspector(QWebInspector*);
