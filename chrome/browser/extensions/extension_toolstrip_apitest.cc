1cc90344ea6050ed0899f52c80c81ceac06e3b6c
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-01 22:36:06 +0000

    Ensure ChromeURLRequestContext finds out first about extension loading.
    This prevents races from arising where extension renderers may try to navigate to extension urls before the request context knows how to map the urls to extension resources.
    
    BUG=22668
    
    Review URL: http://codereview.chromium.org/255047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27781 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_toolstrip_apitest.cc
+++ chrome/browser/extensions/extension_toolstrip_apitest.cc
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-// TEMPORARILY ENABLED TO GET DEBUG OUTPUT:
-// TODO(rafaelw,erikkay) disabled due to flakiness
-// BUG=22668 (probably the same bug)
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Toolstrip) {
   ASSERT_TRUE(RunExtensionTest("toolstrip")) << message_;
 }
--- chrome/browser/extensions/extensions_service.cc
+++ chrome/browser/extensions/extensions_service.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/external_extension_provider.h"
 #include "chrome/browser/extensions/external_pref_extension_provider.h"
 #include "chrome/browser/profile.h"
+#include "chrome/browser/net/chrome_url_request_context.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_error_reporter.h"
@@ -243,10 +244,7 @@ void ExtensionsService::EnableExtension(const std::string& extension_id) {
   ExtensionDOMUI::RegisterChromeURLOverrides(profile_,
       extension->GetChromeURLOverrides());
 
-  NotificationService::current()->Notify(
-      NotificationType::EXTENSION_LOADED,
-      Source<ExtensionsService>(this),
-      Details<Extension>(extension));
+  NotifyExtensionLoaded(extension);
 }
 
 void ExtensionsService::DisableExtension(const std::string& extension_id) {
@@ -270,10 +268,7 @@ void ExtensionsService::DisableExtension(const std::string& extension_id) {
   ExtensionDOMUI::UnregisterChromeURLOverrides(profile_,
       extension->GetChromeURLOverrides());
 
-  NotificationService::current()->Notify(
-      NotificationType::EXTENSION_UNLOADED,
-      Source<ExtensionsService>(this),
-      Details<Extension>(extension));
+  NotifyExtensionUnloaded(extension);
 }
 
 void ExtensionsService::LoadExtension(const FilePath& extension_path) {
@@ -322,6 +317,51 @@ void ExtensionsService::LoadInstalledExtension(
     backend_loop_->PostTask(FROM_HERE, NewRunnableMethod(backend_.get(),
         &ExtensionsServiceBackend::CheckExternalUninstall,
         scoped_refptr<ExtensionsService>(this), id, location));
+  }
+}
+
+void ExtensionsService::NotifyExtensionLoaded(Extension* extension) {
+  LOG(INFO) << "Sending EXTENSION_LOADED";
+
+  // The ChromeURLRequestContext needs to be first to know that the extension
+  // was loaded, otherwise a race can arise where a renderer that is created
+  // for the extension may try to load an extension URL with an extension id
+  // that the request context doesn't yet know about.
+  if (profile_ && !profile_->IsOffTheRecord()) {
+    ChromeURLRequestContext* context = static_cast<ChromeURLRequestContext*>(
+        profile_->GetRequestContext());
+    if (context) {
+      g_browser_process->io_thread()->message_loop()->PostTask(FROM_HERE,
+          NewRunnableMethod(context,
+                            &ChromeURLRequestContext::OnNewExtensions,
+                            extension->id(),
+                            extension->path()));
+    }
+  }
+
+  NotificationService::current()->Notify(
+      NotificationType::EXTENSION_LOADED,
+      Source<ExtensionsService>(this),
+      Details<Extension>(extension));
+}
+
+void ExtensionsService::NotifyExtensionUnloaded(Extension* extension) {
+  LOG(INFO) << "Sending EXTENSION_UNLOADED";
+
+  NotificationService::current()->Notify(
+      NotificationType::EXTENSION_UNLOADED,
+      Source<ExtensionsService>(this),
+      Details<Extension>(extension));
+
+  if (profile_ && !profile_->IsOffTheRecord()) {
+    ChromeURLRequestContext* context = static_cast<ChromeURLRequestContext*>(
+        profile_->GetRequestContext());
+    if (context) {
+      g_browser_process->io_thread()->message_loop()->PostTask(FROM_HERE,
+          NewRunnableMethod(context,
+                            &ChromeURLRequestContext::OnUnloadedExtension,
+                            extension->id()));
+    }
   }
 }
 
@@ -414,10 +454,7 @@ void ExtensionsService::UnloadExtension(const std::string& extension_id) {
   // Remove the extension from our list.
   extensions_.erase(iter);
 
-  // Tell other services the extension is gone.
-  NotificationService::current()->Notify(NotificationType::EXTENSION_UNLOADED,
-                                         Source<ExtensionsService>(this),
-                                         Details<Extension>(extension.get()));
+  NotifyExtensionUnloaded(extension.get());
 }
 
 void ExtensionsService::UnloadAllExtensions() {
@@ -499,11 +536,7 @@ void ExtensionsService::OnExtensionLoaded(Extension* extension,
         if (extension->location() != Extension::LOAD)
           extension_prefs_->MigrateToPrefs(extension);
 
-        LOG(INFO) << "Sending EXTENSION_LOADED";
-        NotificationService::current()->Notify(
-            NotificationType::EXTENSION_LOADED,
-            Source<ExtensionsService>(this),
-            Details<Extension>(extension));
+        NotifyExtensionLoaded(extension);
 
         if (extension->IsTheme() && extension->location() == Extension::LOAD) {
           NotificationService::current()->Notify(
