efc6785d32aedc7ee7267d50f3d2753b3177dff3
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-02 21:49:56 +0000

    2009-10-02  Brian Weinstein  <bweinstein@apple.com>
            Reviewed by Jon Honeycutt.
    
            Fix test breakages by adding null checks, and putting inspector code in
            ENABLE(INSPECTOR).
    
            * dom/Document.cpp:
            (WebCore::Document::finishedParsing):
            * page/DOMWindow.cpp:
            (WebCore::DOMWindow::dispatchLoadEvent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49043 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -4020,9 +4020,14 @@ void Document::finishedParsing()
     if (Frame* f = frame()) {
         f->loader()->finishedParsing();
 
+#if ENABLE(INSPECTOR)
+        if (!page())
+            return;
+
         if (InspectorController* controller = page()->inspectorController())
             controller->mainResourceFiredDOMContentEvent(f->loader()->documentLoader(), url());
     }
+#endif
 }
 
 Vector<String> Document::formElementsState() const
