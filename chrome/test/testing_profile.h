40b0f08c2368651413180b478f89f2c722b51c2b
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-02 01:29:29 +0000

    Add Profile::DeleteSpellChecker(), which my MemoryPurger will use to purge the spellchecker objects from memory.
    This reworks the SpellChecker management functions inside the ProfileImpl for simplicity.  The only notable side effect is that GetSpellChecker() will now cause a notification if it inits |spellchecker_|, just like ReinitializeSpellChecker() already did.  This was just to simplify the code; at the point this fires, no one will be listening yet, so it won't actually do anything.
    
    Also use a temporary at one spot in browser_render_process_host.cc instead of calling a heavyweight function twice in a row.
    
    BUG=23400
    TEST=none
    Review URL: http://codereview.chromium.org/259009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27816 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/testing_profile.h
+++ chrome/test/testing_profile.h
@@ -161,6 +161,7 @@ class TestingProfile : public Profile {
   virtual void ResetTabRestoreService() {}
   virtual void ReinitializeSpellChecker() {}
   virtual SpellChecker* GetSpellChecker() { return NULL; }
+  virtual void DeleteSpellChecker() {}
   virtual WebKitContext* GetWebKitContext() { return NULL; }
   virtual WebKitContext* GetOffTheRecordWebKitContext() { return NULL; }
   virtual void MarkAsCleanShutdown() {}
