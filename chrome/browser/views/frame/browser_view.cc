60bd7a794b7792639bcb81299d74379a3e3471b0
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-02 02:20:29 +0000

    Attempt to fix the Linux views build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27822 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_view.cc
+++ chrome/browser/views/frame/browser_view.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/keyboard_codes.h"
 #include "base/time.h"
-#include "base/win_util.h"
 #include "build/build_config.h"
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/browser/app_modal_dialog_queue.h"
@@ -1132,7 +1131,7 @@ void BrowserView::ShowAppMenu() {
 
 int BrowserView::GetCommandId(const NativeWebKeyboardEvent& event) {
   views::Accelerator accelerator(
-      win_util::WinToKeyboardCode(event.windowsKeyCode),
+      static_cast<base::KeyboardCode>(event.windowsKeyCode),
       (event.modifiers & NativeWebKeyboardEvent::ShiftKey) ==
           NativeWebKeyboardEvent::ShiftKey,
       (event.modifiers & NativeWebKeyboardEvent::ControlKey) ==
