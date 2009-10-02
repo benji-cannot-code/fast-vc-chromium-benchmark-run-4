6827050d5ff6972a7356a7543baebe393e653a5a
yurys@google.com <yurys@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-02 12:01:44 +0000

    DevTools: reenable step-out test as issues causing its failure on linux was fixed in WebKit r48970
    TBR=pfeldman
    Review URL: http://codereview.chromium.org/256039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27837 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_sanity_unittest.cc
+++ chrome/browser/debugger/devtools_sanity_unittest.cc
@@ -182,7 +182,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestStepOver) {
 }
 
 // Tests step out functionality in the debugger.
-IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, DISABLED_TestStepOut) {
+IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestStepOut) {
   RunTest("testStepOut", kDebuggerStepTestPage);
 }
 
