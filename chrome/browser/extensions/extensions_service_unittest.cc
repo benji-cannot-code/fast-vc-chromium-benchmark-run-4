391ccf3aee583085f4ca9bc17b097ac8f13ba255
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-02 04:11:04 +0000

    Coverity: Check GetInteger & GetBoolean return.
    CID=5937,5838
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/255051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27825 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service_unittest.cc
+++ chrome/browser/extensions/extensions_service_unittest.cc
@@ -398,7 +398,7 @@ class ExtensionsServiceTest
     ASSERT_TRUE(dict->GetDictionary(ASCIIToWide(extension_id), &pref)) << msg;
     EXPECT_TRUE(pref != NULL) << msg;
     bool val;
-    pref->GetBoolean(pref_path, &val);
+    ASSERT_TRUE(pref->GetBoolean(pref_path, &val)) << msg;
     EXPECT_EQ(must_equal, val) << msg;
   }
 
@@ -438,7 +438,7 @@ class ExtensionsServiceTest
     ASSERT_TRUE(dict->GetDictionary(ASCIIToWide(extension_id), &pref)) << msg;
     EXPECT_TRUE(pref != NULL) << msg;
     int val;
-    pref->GetInteger(pref_path, &val);
+    ASSERT_TRUE(pref->GetInteger(pref_path, &val)) << msg;
     EXPECT_EQ(must_equal, val) << msg;
   }
 
