7f049b6105ac6313cf4c0a90bb91ebeecd0d1107
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-02 17:18:46 +0000

    Fix the NewTabUI load UMA metric.
    We were attaching the paint observer to the a RenderWidgetHost that
    was getting immediately swapped out when the new tab page started
    to load.  Instead, wait and use the DOM UI callback methods which
    happen right after NewTabUI is created.
    
    Review URL: http://codereview.chromium.org/242115
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27850 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/new_tab_ui.cc
+++ chrome/browser/dom_ui/new_tab_ui.cc
@@ -133,8 +133,7 @@ void SetURLTitleAndDirection(DictionaryValue* dictionary,
 // messages and wait for the page to stop repainting.
 class PaintTimer : public RenderWidgetHost::PaintObserver {
  public:
-  PaintTimer()
-      : ALLOW_THIS_IN_INITIALIZER_LIST(method_factory_(this)) {
+  PaintTimer() {
     Start();
   }
 
@@ -142,8 +141,9 @@ class PaintTimer : public RenderWidgetHost::PaintObserver {
   void Start() {
     start_ = base::TimeTicks::Now();
     last_paint_ = start_;
-    MessageLoop::current()->PostDelayedTask(FROM_HERE,
-        method_factory_.NewRunnableMethod(&PaintTimer::Timeout), kTimeoutMs);
+
+    timer_.Start(base::TimeDelta::FromMilliseconds(kTimeoutMs), this,
+                 &PaintTimer::Timeout);
   }
 
   // A callback that is invoked whenever our RenderWidgetHost paints.
@@ -168,8 +168,8 @@ class PaintTimer : public RenderWidgetHost::PaintObserver {
       // Not enough quiet time has elapsed.
       // Some more paints must've occurred since we set the timeout.
       // Wait some more.
-      MessageLoop::current()->PostDelayedTask(FROM_HERE,
-          method_factory_.NewRunnableMethod(&PaintTimer::Timeout), kTimeoutMs);
+      timer_.Start(base::TimeDelta::FromMilliseconds(kTimeoutMs), this,
+                   &PaintTimer::Timeout);
     }
   }
 
@@ -182,7 +182,7 @@ class PaintTimer : public RenderWidgetHost::PaintObserver {
   // The last time we got a paint notification.
   base::TimeTicks last_paint_;
   // Scoping so we can be sure our timeouts don't outlive us.
-  ScopedRunnableMethodFactory<PaintTimer> method_factory_;
+  base::OneShotTimer<PaintTimer> timer_;
 
   DISALLOW_COPY_AND_ASSIGN(PaintTimer);
 };
@@ -1644,8 +1644,6 @@ NewTabUI::NewTabUI(TabContents* contents)
   if (NewTabUI::FirstRunDisabled())
     NewTabHTMLSource::set_first_run(false);
 
-  tab_contents()->render_view_host()->set_paint_observer(new PaintTimer);
-
   if (GetProfile()->IsOffTheRecord()) {
     incognito_ = true;
 
@@ -1698,6 +1696,14 @@ NewTabUI::NewTabUI(TabContents* contents)
 }
 
 NewTabUI::~NewTabUI() {
+}
+
+void NewTabUI::RenderViewCreated(RenderViewHost* render_view_host) {
+  render_view_host->set_paint_observer(new PaintTimer);
+}
+
+void NewTabUI::RenderViewReused(RenderViewHost* render_view_host) {
+  render_view_host->set_paint_observer(new PaintTimer);
 }
 
 void NewTabUI::Observe(NotificationType type,
