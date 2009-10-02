57c77e7ebc1a175d38ebc5ea83844764d58819e5
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-02 18:08:14 +0000

    2009-10-02  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Adam Roben.
    
            <rdar://problem/7271334> 
            Rename MediaControllerThemeQT to MediaControllerThemeQuickTime
    
            * WebKitSystemInterface.h: 
            MediaControllerThemeQT -> MediaControllerThemeQuickTime
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
+++ third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
@@ -217,7 +217,7 @@ typedef enum {
 
 typedef enum {
     WKMediaControllerThemeClassic   = 1,
-    WKMediaControllerThemeQT        = 2
+    WKMediaControllerThemeQuickTime = 2
 } WKMediaControllerThemeStyle;
 
 typedef enum {
