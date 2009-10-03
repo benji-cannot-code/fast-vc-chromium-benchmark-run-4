7a5db646914b44a99b87ecd1c194f0569d55deb5
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-03 05:24:48 +0000

    2009-10-02  Brian Weinstein  <bweinstein@apple.com>
            Reviewed by Timothy Hatcher.
    
            Fixes <https://bugs.webkit.org/show_bug.cgi?id=30036>
            Should be able to resize Cookie Columns.
    
            * inspector/front-end/CookieItemsView.js:
            (WebInspector.CookieItemsView.prototype.update.callback):
            (WebInspector.CookieItemsView.prototype.update):
            (WebInspector.CookieItemsView.prototype.resize):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/CookieItemsView.js
+++ third_party/WebKit/WebCore/inspector/front-end/CookieItemsView.js
@@ -71,6 +71,7 @@ WebInspector.CookieItemsView.prototype = {
             if (dataGrid) {
                 self._dataGrid = dataGrid;
                 self.element.appendChild(dataGrid.element);
+                self._dataGrid.updateWidths();
                 if (isAdvanced)
                     self.deleteButton.visible = true;
             } else {
@@ -246,6 +247,12 @@ WebInspector.CookieItemsView.prototype = {
             nodes[0].selected = true;
 
         return dataGrid;
+    },
+    
+    resize: function()
+    {
+        if (this._datagrid)
+            this._dataGrid.updateWidths();
     },
 
     _deleteButtonClicked: function(event)
