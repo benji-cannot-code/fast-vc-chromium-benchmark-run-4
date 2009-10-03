9b76f7cbfd5d2e35e06746e3044c98bf7faa3d30
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-03 19:39:24 +0000

    Add an API to manipulate the browser action badge.
    BUG=23268
    
    Review URL: http://codereview.chromium.org/256032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27951 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_action.h
+++ chrome/common/extensions/extension_action.h
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/scoped_ptr.h"
 #include "googleurl/src/gurl.h"
+#include "third_party/skia/include/core/SkColor.h"
 
 class ExtensionAction {
  public:
@@ -86,11 +88,24 @@ typedef std::map<std::string, ExtensionAction*> ExtensionActionMap;
 class ExtensionActionState {
  public:
   ExtensionActionState(std::string title, int icon_index)
-    : title_(title), icon_index_(icon_index) {
+    : title_(title), icon_index_(icon_index),
+      badge_background_color_(new SkColor(SkColorSetARGB(255, 218, 0, 24))) {
   }
 
-  std::string title() const { return title_; }
+  const std::string& title() const { return title_; }
   void set_title(const std::string& title) { title_ = title; }
+
+  const std::string& badge_text() const { return badge_text_; }
+  void set_badge_text(const std::string& badge_text) {
+    badge_text_ = badge_text;
+  }
+
+  SkColor* badge_background_color() const {
+    return badge_background_color_.get();
+  }
+  void set_badge_background_color(const SkColor& badge_background_color) {
+    badge_background_color_.reset(new SkColor(badge_background_color));
+  }
 
   int icon_index() const { return icon_index_; }
   void set_icon_index(int icon_index) { icon_index_ = icon_index; }
@@ -101,6 +116,12 @@ class ExtensionActionState {
 
   // The icon to use.
   int icon_index_;
+
+  // The badge text.
+  std::string badge_text_;
+
+  // The background color for the badge.
+  scoped_ptr<SkColor> badge_background_color_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionActionState);
 };
