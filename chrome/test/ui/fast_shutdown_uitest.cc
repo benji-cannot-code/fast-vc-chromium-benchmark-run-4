25c6aa65f2677b54f56df2fc6d6d58bb4d5e7718
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-03 21:41:20 +0000

    disable the SlowTermination tests because it keeps failing on Linux.
    TBR:estade
    
    Review URL: http://codereview.chromium.org/259037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27958 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/fast_shutdown_uitest.cc
+++ chrome/test/ui/fast_shutdown_uitest.cc
@@ -14,19 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/automation/window_proxy.h"
 #include "views/event.h"
 
-#if defined(OS_WIN)
-#define MAYBE_SlowTermination DISABLED_SlowTermination
-#else
-#define MAYBE_SlowTermination SlowTermination
-#endif
-
 class FastShutdown : public UITest {
 };
 
 // This tests for a previous error where uninstalling an onbeforeunload
 // handler would enable fast shutdown even if an onUnload handler still
 // existed.
-TEST_F(FastShutdown, MAYBE_SlowTermination) {
+TEST_F(FastShutdown, DISABLED_SlowTermination) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
   scoped_refptr<WindowProxy> window(browser->GetWindow());
