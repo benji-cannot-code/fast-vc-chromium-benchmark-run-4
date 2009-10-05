52624a66c487127a54b1fb5d9e30b4d9b03bef0d
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-05 20:39:17 +0000

    Adds command line flag to enable desktop notifications.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/249057/show
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28031 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/src/WebSettingsImpl.cpp
+++ webkit/api/src/WebSettingsImpl.cpp
@@ -250,4 +250,9 @@ void WebSettingsImpl::setExperimentalWebGLEnabled(bool enabled)
     m_settings->setWebGLEnabled(enabled);
 }
 
+void WebSettingsImpl::setExperimentalNotificationsEnabled(bool enabled)
+{
+    m_settings->setExperimentalNotificationsEnabled(enabled);
+}
+
 } // namespace WebKit
