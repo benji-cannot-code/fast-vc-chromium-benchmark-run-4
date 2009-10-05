f685d0eb7ab0543f170d35eae66b10fc44ff6884
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-05 10:57:23 +0000

    2009-10-05  Jakub Wieczorek  <faw217@gmail.com>
            Reviewed by Tor Arne Vestbø.
    
            [Qt] windowsKeyCodeForKeyEvent() returns a wrong value for the F10 key.
            https://bugs.webkit.org/show_bug.cgi?id=30042
    
            * platform/qt/PlatformKeyboardEventQt.cpp:
            (WebCore::windowsKeyCodeForKeyEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49089 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/PlatformKeyboardEventQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/PlatformKeyboardEventQt.cpp
@@ -218,7 +218,7 @@ static int windowsKeyCodeForKeyEvent(unsigned int keycode, bool isKeypad = false
         case Qt::Key_F9:
             return VK_F9;
         case Qt::Key_F10:
-            return VK_F11;
+            return VK_F10;
         case Qt::Key_F11:
             return VK_F11;
         case Qt::Key_F12:
