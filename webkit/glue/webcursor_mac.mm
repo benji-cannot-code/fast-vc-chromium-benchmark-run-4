219944a1b352624624aa7d35a861fa730e35acb9
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-05 20:27:47 +0000

    Make Mac WebCursor glue use the nsimage_cache
    BUG=16438, 14610 (in in support of bundling changes to fix autoupdate)
    TEST=Hover over a link, get the finger
    Review URL: http://codereview.chromium.org/259049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28027 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webcursor_mac.mm
+++ webkit/glue/webcursor_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <AppKit/AppKit.h>
 
 #include "base/logging.h"
+#include "base/nsimage_cache_mac.h"
 #include "base/scoped_cftyperef.h"
 #include "webkit/api/public/WebCursorInfo.h"
 #include "webkit/api/public/WebImage.h"
@@ -19,11 +20,12 @@ using WebKit::WebSize;
 
 namespace {
 
-// TODO(avi): Is loading resources what we want to do here?
+// TODO: This image fech can (and probably should) be serviced by the resource
+// resource bundle instead of going through nsimage_cache.
 NSCursor* LoadCursor(const char* name, int x, int y) {
   NSString* file_name = [NSString stringWithUTF8String:name];
   DCHECK(file_name);
-  NSImage* cursor_image = [NSImage imageNamed:file_name];
+  NSImage* cursor_image = nsimage_cache::ImageNamed(file_name);
   DCHECK(cursor_image);
   return [[[NSCursor alloc] initWithImage:cursor_image
                                   hotSpot:NSMakePoint(x, y)] autorelease];
