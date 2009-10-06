7728fbfd461f2cf282d0c3a558c358452405416c
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-06 07:31:22 +0000

    [Qt] Add support for windowless NPAPI plugins
    Patch by Girish Ramakrishnan <girish@forwardbias.in> on 2009-10-06
    Reviewed by Simon Hausmann.
    
    https://bugs.webkit.org/show_bug.cgi?id=20081
    
    * plugins/PluginView.cpp:
    (WebCore::PluginView::setFrameRect):
    (WebCore::PluginView::handleEvent):
    (WebCore::PluginView::PluginView):
    * plugins/PluginView.h:
    * plugins/qt/PluginViewQt.cpp:
    (WebCore::PluginView::updatePluginWidget):
    (WebCore::PluginView::paint):
    (WebCore::PluginView::dispatchNPEvent):
    (WebCore::setSharedXEventFields):
    (WebCore::PluginView::initXEvent):
    (WebCore::setXKeyEventSpecificFields):
    (WebCore::PluginView::handleKeyboardEvent):
    (WebCore::inputEventState):
    (WebCore::setXButtonEventSpecificFields):
    (WebCore::setXMotionEventSpecificFields):
    (WebCore::setXCrossingEventSpecificFields):
    (WebCore::PluginView::handleMouseEvent):
    (WebCore::PluginView::handleFocusInEvent):
    (WebCore::PluginView::handleFocusOutEvent):
    (WebCore::PluginView::setNPWindowRect):
    (WebCore::PluginView::setNPWindowIfNeeded):
    (WebCore::PluginView::getValueStatic):
    (WebCore::PluginView::invalidateRect):
    (WebCore::PluginView::invalidateRegion):
    (WebCore::PluginView::forceRedraw):
    (WebCore::getPluginDisplay):
    (WebCore::PluginView::platformStart):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49158 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/PluginView.h
+++ third_party/WebKit/WebCore/plugins/PluginView.h
@@ -53,6 +53,10 @@ typedef HWND PlatformPluginWidget;
 typedef PlatformWidget PlatformPluginWidget;
 #endif
 
+#if defined(Q_WS_X11)
+#include <QPixmap>
+#endif
+
 namespace JSC {
     namespace Bindings {
         class Instance;
@@ -256,6 +260,10 @@ namespace WebCore {
 
         void handleKeyboardEvent(KeyboardEvent*);
         void handleMouseEvent(MouseEvent*);
+#if defined(Q_WS_X11)
+        void handleFocusInEvent();
+        void handleFocusOutEvent();
+#endif
 
         int m_mode;
         int m_paramCount;
@@ -323,6 +331,10 @@ private:
 
 #if defined(Q_WS_X11)
         bool m_hasPendingGeometryChange;
+        QPixmap m_drawable;
+        Display* m_pluginDisplay;
+
+        void initXEvent(XEvent* event);
 #endif
 
         IntRect m_clipRect; // The clip rect to apply to a windowed plug-in
