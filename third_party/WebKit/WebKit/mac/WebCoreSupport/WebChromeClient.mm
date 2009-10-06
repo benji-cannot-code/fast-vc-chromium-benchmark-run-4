f3acfd1316ae58a71e0952ac20f8eeeeb09e2a92
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-06 01:05:34 +0000

    2009-10-05  Simon Fraser  <simon.fraser@apple.com>
            Build fix. If you call Node::hasTagName(), you also have to include Element.h.
    
            * WebCoreSupport/WebChromeClient.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebChromeClient.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebChromeClient.mm
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 #import <WebCore/BlockExceptions.h>
 #import <WebCore/Console.h>
+#import <WebCore/Element.h>
 #import <WebCore/FileChooser.h>
 #import <WebCore/FloatRect.h>
 #import <WebCore/Frame.h>
