dd9842c02ddbd4fc93b604fabf9b9560f0f2520f
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-06 18:20:34 +0000

    Cleanup some rotten references to old command line switches.
    Review URL: http://codereview.chromium.org/243105
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28131 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/sync_backend_host.cc
+++ chrome/browser/sync/glue/sync_backend_host.cc
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/glue/bookmark_model_worker.h"
 #include "webkit/glue/webkit_glue.h"
 
-static const char kSwitchSyncServiceURL[] = "sync-url";
-static const char kSwitchSyncServicePort[] = "sync-port";
 static const int kSaveChangesIntervalSeconds = 10;
 static const char kGaiaServiceId[] = "chromiumsync";
 static const char kGaiaSourceForChrome[] = "ChromiumBrowser";
