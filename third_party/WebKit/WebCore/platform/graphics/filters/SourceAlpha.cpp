8b705eb081c40b0f6b9af1c13b5c997a8717f015
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-06 05:58:44 +0000

    2009-10-05  Dirk Schulze  <krit@webkit.org>
            Reviewed by Nikolas Zimmermann.
    
            SVG Filters do not support source images besides "sourceGraphic"
            [https://bugs.webkit.org/show_bug.cgi?id=6022]
    
            Add support for SourceAlpha to SVG filters.
    
            Test: svg/filters/sourceAlpha.svg
    
            * platform/graphics/filters/SourceAlpha.cpp:
            (WebCore::SourceAlpha::calculateEffectRect):
            (WebCore::SourceAlpha::apply):
            * platform/graphics/filters/SourceAlpha.h:
    
            Test for SVG's SourceAlpha support for Filters.
    
            * platform/mac/svg/filters/sourceAlpha-expected.checksum: Added.
            * platform/mac/svg/filters/sourceAlpha-expected.png: Added.
            * platform/mac/svg/filters/sourceAlpha-expected.txt: Added.
            * svg/filters/sourceAlpha.svg: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49153 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/filters/SourceAlpha.cpp
+++ third_party/WebKit/WebCore/platform/graphics/filters/SourceAlpha.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(FILTERS)
 #include "SourceAlpha.h"
 
+#include "Color.h"
 #include "GraphicsContext.h"
 #include "PlatformString.h"
 #include "Filter.h"
@@ -42,8 +43,28 @@ const AtomicString& SourceAlpha::effectName()
     return s_effectName;
 }
 
-void SourceAlpha::apply(Filter*)
+FloatRect SourceAlpha::calculateEffectRect(Filter* filter)
 {
+    FloatRect clippedSourceRect = filter->sourceImageRect();
+    if (filter->sourceImageRect().x() < filter->filterRegion().x())
+        clippedSourceRect.setX(filter->filterRegion().x());
+    if (filter->sourceImageRect().y() < filter->filterRegion().y())
+        clippedSourceRect.setY(filter->filterRegion().y());
+    setSubRegion(clippedSourceRect);
+    return filter->filterRegion();
+}
+
+void SourceAlpha::apply(Filter* filter)
+{
+    GraphicsContext* filterContext = getEffectContext();
+    if (!filterContext)
+        return;
+
+    FloatRect imageRect(FloatPoint(), filter->sourceImage()->image()->size());
+    filterContext->save();
+    filterContext->clipToImageBuffer(imageRect, filter->sourceImage());
+    filterContext->fillRect(imageRect, Color::black);
+    filterContext->restore();
 }
 
 void SourceAlpha::dump()
