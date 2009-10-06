6a39ca0a831fa69ff05b848cf51d6be1d4dc3a19
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-06 07:02:35 +0000

    2009-10-05  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Darin Fisher.
    
            Convert DOM Storage's "remove-item" test into the new format.
            https://bugs.webkit.org/show_bug.cgi?id=30099
    
            Convert DOM Storage's "remove-item" test into the new format and add it for
            session storage.  Make it a (tiny bit) more robust as well.
    
            * storage/domstorage/localstorage/remove-item-expected.txt:
            * storage/domstorage/localstorage/remove-item.html:
            * storage/domstorage/script-tests/remove-item.js: Added.
            (runTest):
            * storage/domstorage/sessionstorage/remove-item-expected.txt: Copied from LayoutTests/storage/domstorage/localstorage/remove-item-expected.txt.
            * storage/domstorage/sessionstorage/remove-item.html: Copied from LayoutTests/storage/domstorage/localstorage/remove-item.html.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49157 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/storage/domstorage/script-tests/remove-item.js
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test .removeItem within DOM Storage.");
+
+function runTest(storageString)
+{
+    storage = eval(storageString);
+    if (!storage) {
+        testFailed(storageString + " DOES NOT exist");
+        return;
+    }
+
+    debug("Testing " + storageString);
+
+    evalAndLog("storage.clear()");
+    shouldBe("storage.length", "0");
+
+    debug("");
+    shouldBeUndefined("storage.foo1");
+    evalAndLog("storage.foo1 = 'bar'");
+    shouldBeEqualToString("storage.foo1", "bar");
+    evalAndLog("storage.removeItem('foo1')");
+    shouldBeUndefined("storage.foo1");
+    evalAndLog("storage.removeItem('foo1')");
+    shouldBeUndefined("storage.foo1");
+
+    debug("");
+    shouldBeUndefined("storage['foo2']");
+    evalAndLog("storage['foo2'] = 'bar'");
+    shouldBeEqualToString("storage['foo2']", "bar");
+    evalAndLog("storage.removeItem('foo2')");
+    shouldBeUndefined("storage['foo2']");
+    evalAndLog("storage.removeItem('foo2')");
+    shouldBeUndefined("storage['foo2']");
+
+    debug("");
+    shouldBeNull("storage.getItem('foo3')");
+    evalAndLog("storage.setItem('foo3', 'bar')");
+    shouldBeEqualToString("storage.getItem('foo3')", "bar");
+    evalAndLog("storage.removeItem('foo3')");
+    shouldBeNull("storage.getItem('foo3')");
+    evalAndLog("storage.removeItem('foo3')");
+    shouldBeNull("storage.getItem('foo3')");
+
+    window.successfullyParsed = true;
+}
+
