bdfa516ce13d7372f025bd9b370136b3f0b7947b
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-06 01:13:26 +0000

    Order of preferences needs to match serialization.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/242151
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28083 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webpreferences.h
+++ webkit/glue/webpreferences.h
@@ -50,7 +50,6 @@ struct WebPreferences {
   bool session_storage_enabled;
   bool application_cache_enabled;
   bool tabs_to_links;
-  bool experimental_notifications_enabled;
 
   // TODO(tc): User style sheets will not work in chrome because it tries to
   // load the style sheet using a request without a frame.
@@ -60,6 +59,7 @@ struct WebPreferences {
   bool allow_universal_access_from_file_urls;
 
   bool experimental_webgl_enabled;
+  bool experimental_notifications_enabled;
 
   // We try to keep the default values the same as the default values in
   // chrome, except for the cases where it would require lots of extra work for
@@ -96,10 +96,10 @@ struct WebPreferences {
         session_storage_enabled(false),
         application_cache_enabled(false),
         tabs_to_links(true),
-        experimental_notifications_enabled(false),
         user_style_sheet_enabled(false),
         allow_universal_access_from_file_urls(false),
-        experimental_webgl_enabled(false) {
+        experimental_webgl_enabled(false),
+        experimental_notifications_enabled(false) {
   }
 
   void Apply(WebView* web_view) const;
