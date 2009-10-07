febc4c523ef32394d50ad4c38a6201b2802bedcd
chron@chromium.org <chron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-07 00:13:47 +0000

    Remove pthreads from syncable. Remove unused kernel scoped unlock.
    BUG=19895
    TEST=Existing unit tests
    
    Review URL: http://codereview.chromium.org/246097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28198 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/syncable/syncable.cc
+++ chrome/browser/sync/syncable/syncable.cc
@@ -203,7 +203,6 @@ Directory::Kernel::Kernel(const PathString& db_path,
   next_metahandle(info.max_metahandle + 1),
   next_id(info.kernel_info.next_id) {
   info_status_ = Directory::KERNEL_SHARE_INFO_VALID;
-  CHECK(0 == pthread_mutex_init(&mutex, NULL));
 }
 
 inline void DeleteEntry(EntryKernel* kernel) {
@@ -223,7 +222,6 @@ Directory::Kernel::~Kernel() {
   CHECK(0 == refcount);
   delete channel;
   delete changes_channel;
-  CHECK(0 == pthread_mutex_destroy(&mutex));
   delete unsynced_metahandles;
   delete unapplied_update_metahandles;
   delete extended_attributes;
@@ -765,7 +763,9 @@ void Directory::TakeSnapshotForSaveChanges(SaveChangesSnapshot* snapshot) {
 bool Directory::SaveChanges() {
   bool success = false;
   DCHECK(store_);
-  PThreadScopedLock<PThreadMutex> lock(&kernel_->save_changes_mutex);
+
+  AutoLock(kernel_->save_changes_mutex);
+
   // Snapshot and save.
   SaveChangesSnapshot snapshot;
   TakeSnapshotForSaveChanges(&snapshot);
@@ -1105,22 +1105,9 @@ void Directory::CheckTreeInvariants(syncable::BaseTransaction* trans,
 // ScopedKernelLocks
 
 ScopedKernelLock::ScopedKernelLock(const Directory* dir)
-  : dir_(const_cast<Directory*>(dir)) {
+  : dir_(const_cast<Directory*>(dir)), scoped_lock_(dir_->kernel_->mutex) {
   // Swap out the dbhandle to enforce the "No IO while holding kernel
   // lock" rule.
-  // HA!! Yeah right. What about your pre-cached queries :P
-  pthread_mutex_lock(&dir->kernel_->mutex);
-}
-ScopedKernelLock::~ScopedKernelLock() {
-  pthread_mutex_unlock(&dir_->kernel_->mutex);
-}
-
-ScopedKernelUnlock::ScopedKernelUnlock(ScopedKernelLock* lock)
-  : lock_(lock) {
-  pthread_mutex_unlock(&lock->dir_->kernel_->mutex);
-}
-ScopedKernelUnlock::~ScopedKernelUnlock() {
-  pthread_mutex_lock(&lock_->dir_->kernel_->mutex);
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -1168,7 +1155,7 @@ void BaseTransaction::UnlockAndLog(OriginalEntries* originals_arg) {
     return;
   }
 
-  dirkernel_->changes_channel_mutex.Lock();
+  AutoLock(dirkernel_->changes_channel_mutex);
   // Tell listeners to calculate changes while we still have the mutex.
   DirectoryChangeEvent event = { DirectoryChangeEvent::CALCULATE_CHANGES,
                                  originals.get(), this, writer_ };
@@ -1180,7 +1167,6 @@ void BaseTransaction::UnlockAndLog(OriginalEntries* originals_arg) {
       { DirectoryChangeEvent::TRANSACTION_COMPLETE,
         NULL, NULL, INVALID };
   dirkernel_->changes_channel->NotifyListeners(complete_event);
-  dirkernel_->changes_channel_mutex.Unlock();
 }
 
 ReadTransaction::ReadTransaction(Directory* directory, const char* file,
