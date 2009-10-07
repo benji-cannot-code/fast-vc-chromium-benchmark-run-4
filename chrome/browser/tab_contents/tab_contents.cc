c61e4bf5ceb659d563f6f34e388e2804db4ef332
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-07 17:01:44 +0000

    Revert "Revert "Add startup tests that log start script, domcontentloaded, and onload.""
    This reverts commit r28209 and relands r28199.
    
    The failure seems to have been flaky (it passed on the other bots and on the following run).  I want to
    try to land again.  If it fails again, I'll disable the test.
    
    TBR=mattm
    
    Review URL: http://codereview.chromium.org/268003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28268 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/jsmessage_box_handler.h"
 #include "chrome/browser/load_from_memory_cache_details.h"
 #include "chrome/browser/load_notification_details.h"
+#include "chrome/browser/metrics/metric_event_duration_details.h"
 #include "chrome/browser/modal_html_dialog_delegate.h"
 #include "chrome/browser/omnibox_search_hint.h"
 #include "chrome/browser/password_manager/password_manager.h"
@@ -1178,6 +1179,9 @@ void TabContents::LogNewTabTime(const std::string& event_name) {
     return;
 
   base::TimeDelta duration = base::TimeTicks::Now() - new_tab_start_time_;
+  MetricEventDurationDetails details(event_name,
+      static_cast<int>(duration.InMilliseconds()));
+
   if (event_name == "NewTab.ScriptStart") {
     UMA_HISTOGRAM_TIMES("NewTab.ScriptStart", duration);
   } else if (event_name == "NewTab.DOMContentLoaded") {
@@ -1186,7 +1190,13 @@ void TabContents::LogNewTabTime(const std::string& event_name) {
     UMA_HISTOGRAM_TIMES("NewTab.Onload", duration);
     // The new tab page has finished loading; reset it.
     new_tab_start_time_ = base::TimeTicks();
+  } else {
+    NOTREACHED();
   }
+  NotificationService::current()->Notify(
+      NotificationType::METRIC_EVENT_DURATION,
+      Source<TabContents>(this),
+      Details<MetricEventDurationDetails>(&details));
 }
 
 // Notifies the RenderWidgetHost instance about the fact that the page is
