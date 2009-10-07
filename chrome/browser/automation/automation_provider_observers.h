6e30932164e766d78f65956b09a0e50327eb2600
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-07 00:16:15 +0000

    Add startup tests that log start script, domcontentloaded, and onload.
    TabContents sends a notification which is picked up by the browser
    automation controller which keeps a map of events and times.  Add
    an automation IPC so that tests can get these values from the
    browser automation controller.  Finally, add a feature startup test
    to collect these values.
    
    This is just to get some consistent timings from the buildbot.
    
    BUG=23120
    
    Review URL: http://codereview.chromium.org/246105
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider_observers.h
+++ chrome/browser/automation/automation_provider_observers.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_AUTOMATION_AUTOMATION_PROVIDER_OBSERVERS_H_
 #define CHROME_BROWSER_AUTOMATION_AUTOMATION_PROVIDER_OBSERVERS_H_
 
+#include <map>
 #include <set>
 
 #include "chrome/common/notification_observer.h"
@@ -338,5 +339,26 @@ class DocumentPrintedNotificationObserver : public NotificationObserver {
   DISALLOW_COPY_AND_ASSIGN(DocumentPrintedNotificationObserver);
 };
 #endif  // defined(OS_WIN)
+
+// Collects METRIC_EVENT_DURATION notifications and keep track of the times.
+class MetricEventDurationObserver : public NotificationObserver {
+ public:
+  MetricEventDurationObserver();
+
+  // Get the duration of an event.  Returns -1 if we haven't seen the event.
+  int GetEventDurationMs(const std::string& event_name);
+
+  // NotificationObserver interface.
+  virtual void Observe(NotificationType type, const NotificationSource& source,
+                       const NotificationDetails& details);
+
+ private:
+  NotificationRegistrar registrar_;
+
+  typedef std::map<std::string, int> EventDurationMap;
+  EventDurationMap durations_;
+
+  DISALLOW_COPY_AND_ASSIGN(MetricEventDurationObserver);
+};
 
 #endif  // CHROME_BROWSER_AUTOMATION_AUTOMATION_PROVIDER_OBSERVERS_H_
--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -1642,6 +1642,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/memory_purger.h',
         'browser/meta_table_helper.cc',
         'browser/meta_table_helper.h',
+        'browser/metrics/metric_event_duration_details.h',
         'browser/metrics/metrics_log.cc',
         'browser/metrics/metrics_log.h',
         'browser/metrics/metrics_response.cc',
