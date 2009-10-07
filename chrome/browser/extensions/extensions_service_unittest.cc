10f4e12b06e71d6a620e9e7f83e92fd3fc2cd7c2
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-07 03:46:46 +0000

    HTML Pack Extension Dialog. This removes the views implementation of the ExtensionPackDialog, and implements the dialog in html in the extensions_ui DOMUI.
    Additionally, support is added for packaging extensions via ---pack-extension on linux and mac
    
    Review URL: http://codereview.chromium.org/207062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28229 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service_unittest.cc
+++ chrome/browser/extensions/extensions_service_unittest.cc
@@ -728,7 +728,6 @@ TEST_F(ExtensionsServiceTest, InstallExtension) {
   // TODO(erikkay): add tests for upgrade cases.
 }
 
-#if defined(OS_WIN)  // TODO(port)
 // Test Packaging and installing an extension.
 // TODO(rafaelw): add more tests for failure cases.
 TEST_F(ExtensionsServiceTest, PackExtension) {
@@ -792,7 +791,6 @@ TEST_F(ExtensionsServiceTest, PackExtensionOpenSSLKey) {
 
   file_util::Delete(crx_path, false);
 }
-#endif  // defined(OS_WIN)
 
 TEST_F(ExtensionsServiceTest, InstallTheme) {
   InitializeEmptyExtensionsService();
--- chrome/browser/extensions/extensions_ui.h
+++ chrome/browser/extensions/extensions_ui.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/dom_ui/chrome_url_data_manager.h"
 #include "chrome/browser/dom_ui/dom_ui.h"
+#include "chrome/browser/extensions/pack_extension_job.h"
 #include "chrome/browser/shell_dialogs.h"
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_registrar.h"
@@ -53,6 +54,7 @@ class ExtensionsUIHTMLSource : public ChromeURLDataManager::DataSource {
 class ExtensionsDOMHandler
     : public DOMMessageHandler,
       public NotificationObserver,
+      public PackExtensionJob::Client,
       public SelectFileDialog::Listener {
  public:
   explicit ExtensionsDOMHandler(ExtensionsService* extension_service);
@@ -72,13 +74,13 @@ class ExtensionsDOMHandler
       const UserScript& script,
       const FilePath& extension_path);
 
- private:
-#if defined(OS_WIN)
-  // The implementation of this method is platform-specific and defined
-  // elsewhere.
-  static void ShowPackDialog();
-#endif
+  // ExtensionPackJob::Client
+  virtual void OnPackSuccess(const FilePath& crx_file,
+                             const FilePath& key_file);
 
+  virtual void OnPackFailure(const std::wstring& message);
+
+ private:
   // Callback for "requestExtensionsData" message.
   void HandleRequestExtensionsData(const Value* value);
 
@@ -103,6 +105,12 @@ class ExtensionsDOMHandler
   // Callback for "autoupdate" message.
   void HandleAutoUpdateMessage(const Value* value);
 
+  // Utility for calling javascript window.alert in the page.
+  void ShowAlert(const std::string& message);
+
+  // Callback for "selectFilePath" message.
+  void HandleSelectFilePathMessage(const Value* value);
+
   // SelectFileDialog::Listener
   virtual void FileSelected(const FilePath& path,
                             int index, void* params);
@@ -126,6 +134,9 @@ class ExtensionsDOMHandler
 
   // Used to pick the directory when loading an extension.
   scoped_refptr<SelectFileDialog> load_extension_dialog_;
+
+  // Used to package the extension.
+  scoped_refptr<PackExtensionJob> pack_job_;
 
   // We monitor changes to the extension system so that we can reload when
   // necessary.
