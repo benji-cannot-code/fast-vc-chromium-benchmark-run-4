ac6054fd9e3de59ec4f825eff44c41ebb51b6cec
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-07 10:36:44 +0000

    2009-10-07  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Gustavo Noronha.
    
            getChildrenWithRange expects as last parameter the end index, not
            the length of the range. Correct this and clarify the variable
            names to reflect how the code works.
    
            * DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp:
            (AccessibilityUIElement::getChildrenWithRange):
            (AccessibilityUIElement::getChildAtIndex):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49235 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp
@@ -68,9 +68,9 @@ void AccessibilityUIElement::getChildren(Vector<AccessibilityUIElement>& childre
     }
 }
 
-void AccessibilityUIElement::getChildrenWithRange(Vector<AccessibilityUIElement>& elementVector, unsigned location, unsigned length)
+void AccessibilityUIElement::getChildrenWithRange(Vector<AccessibilityUIElement>& elementVector, unsigned start, unsigned end)
 {
-    for (unsigned i = location; i < length; i++) {
+    for (unsigned i = start; i < end; i++) {
         AtkObject* child = atk_object_ref_accessible_child(ATK_OBJECT(m_element), i);
         elementVector.append(AccessibilityUIElement(child));
     }
@@ -95,7 +95,7 @@ AccessibilityUIElement AccessibilityUIElement::elementAtPoint(int x, int y)
 AccessibilityUIElement AccessibilityUIElement::getChildAtIndex(unsigned index)
 {
     Vector<AccessibilityUIElement> children;
-    getChildrenWithRange(children, index, 1);
+    getChildrenWithRange(children, index, index + 1);
 
     if (children.size() == 1)
         return children.at(0);
