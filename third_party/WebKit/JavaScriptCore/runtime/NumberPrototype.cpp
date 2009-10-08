2752ce314826c8e5c2fd298ccc619b927d094524
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-08 23:29:50 +0000

    Windows build fix: added missing #include.
    Patch by Geoffrey Garen <ggaren@apple.com> on 2009-10-08
    * runtime/NumberPrototype.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49332 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/NumberPrototype.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/NumberPrototype.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PrototypeFunction.h"
 #include "dtoa.h"
 #include "Operations.h"
+#include "StructureChain.h"
 #include <wtf/Assertions.h>
 #include <wtf/MathExtras.h>
 #include <wtf/Vector.h>
