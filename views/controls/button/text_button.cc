d2c0058272f1708d7ff4aee242fa36f2abf16cb5
chocobo@google.com <chocobo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-08 01:33:21 +0000

    Refactor cros library code into central location and have the UI elements observe changes. - implemented power menu button showing remaining battery info. - implemented clock menu button showing detailed clock info. - also implemented monitoring of network status change. - don't show highlighted state for these menu buttons BUG=23923 TEST=none Review URL: http://codereview.chromium.org/251099
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28366 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/button/text_button.cc
+++ views/controls/button/text_button.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "views/controls/button/text_button.h"
 
+#include <algorithm>
+
 #include "app/gfx/canvas.h"
 #include "app/l10n_util.h"
 #include "app/throb_animation.h"
@@ -183,6 +185,10 @@ void TextButton::SetText(const std::wstring& text) {
 
 void TextButton::SetIcon(const SkBitmap& icon) {
   icon_ = icon;
+}
+
+void TextButton::SetFont(const gfx::Font& font) {
+  font_ = font;
 }
 
 void TextButton::SetEnabledColor(SkColor color) {
