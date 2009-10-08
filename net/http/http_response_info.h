fdf033bf8d56416e83c686441bb5e9d9c50e291d
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-08 01:14:44 +0000

    Make HttpResponseInfo pickle'able, just moves some code from HttpCache to HttpResponseInfo.
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/269012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28363 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_info.h
+++ net/http/http_response_info.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_response_headers.h"
 #include "net/http/http_vary_data.h"
 
+class Pickle;
+
 namespace net {
 
 class HttpResponseInfo {
@@ -56,6 +58,14 @@ class HttpResponseInfo {
 
   // The "Vary" header data for this response.
   HttpVaryData vary_data;
+
+  // Initializes from the representation stored in the given pickle.
+  bool InitFromPickle(const Pickle& pickle, bool* response_truncated);
+
+  // Call this method to persist the response info.
+  void Persist(Pickle* pickle,
+               bool skip_transient_headers,
+               bool response_truncated) const;
 };
 
 }  // namespace net
