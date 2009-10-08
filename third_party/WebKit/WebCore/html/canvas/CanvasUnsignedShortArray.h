567c369cf55ffd2e40eb9e97ef731e4c2300a81c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-08 06:13:09 +0000

    2009-10-07  Joel Stanley  <joel@jms.id.au>
            Reviewed by Mark Rowe.
    
            Include <limits> for std::numeric_limits.  Required by gcc-4.4
            https://bugs.webkit.org/show_bug.cgi?id=30197
    
            * html/canvas/CanvasByteArray.h:
            * html/canvas/CanvasIntArray.h:
            * html/canvas/CanvasShortArray.h:
            * html/canvas/CanvasUnsignedByteArray.h:
            * html/canvas/CanvasUnsignedIntArray.h:
            * html/canvas/CanvasUnsignedShortArray.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49289 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/CanvasUnsignedShortArray.h
+++ third_party/WebKit/WebCore/html/canvas/CanvasUnsignedShortArray.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CanvasUnsignedShortArray_h
 
 #include "CanvasArray.h"
+#include <limits>
 #include <wtf/MathExtras.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
