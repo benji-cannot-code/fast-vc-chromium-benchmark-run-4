545280e6abe66a981358f878479b25c62e1b3448
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-08 22:14:14 +0000

    Fixes <https://bugs.webkit.org/show_bug.cgi?id=30237>. DOM Content and Load lines aren't shown if a page is loaded when the inspector is up.
    Reviewed by Timothy Hatcher.
    
    Make sure to call update script object if we have an InspectorFrontend to
    call it on when we get one of the new events.
    
    * inspector/InspectorController.cpp:
    (WebCore::InspectorController::mainResourceFiredDOMContentEvent):
    (WebCore::InspectorController::mainResourceFiredLoadEvent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49327 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorController.cpp
+++ third_party/WebKit/WebCore/inspector/InspectorController.cpp
@@ -905,8 +905,11 @@ void InspectorController::mainResourceFiredDOMContentEvent(DocumentLoader* loade
     if (!enabled() || !isMainResourceLoader(loader, url))
         return;
 
-    if (m_mainResource)
+    if (m_mainResource) {
         m_mainResource->markDOMContentEventTime();
+        if (m_frontend)
+            m_mainResource->updateScriptObject(m_frontend.get());
+    }
 }
 
 void InspectorController::mainResourceFiredLoadEvent(DocumentLoader* loader, const KURL& url)
@@ -914,8 +917,11 @@ void InspectorController::mainResourceFiredLoadEvent(DocumentLoader* loader, con
     if (!enabled() || !isMainResourceLoader(loader, url))
         return;
 
-    if (m_mainResource)
+    if (m_mainResource) {
         m_mainResource->markLoadEventTime();
+        if (m_frontend)
+            m_mainResource->updateScriptObject(m_frontend.get());
+    }
 }
 
 bool InspectorController::isMainResourceLoader(DocumentLoader* loader, const KURL& requestUrl)
