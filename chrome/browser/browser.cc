7ac30d7807c190de9df9400c60d97c11ec67ea4f
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 18:13:23 +0000

    Allow the NTP to be bookmarked.
    BUG=none
    TEST=Try to bookmark the NTP.
    Review URL: http://codereview.chromium.org/269028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28564 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -898,16 +898,14 @@ void Browser::Exit() {
 void Browser::BookmarkCurrentPage() {
   UserMetrics::RecordAction(L"Star", profile_);
 
-  TabContents* contents = GetSelectedTabContents();
-  GURL url;
-  std::wstring title;
-  if (!bookmark_utils::GetURLAndTitleToBookmark(contents, &url, &title))
-    return;
-
-  BookmarkModel* model = contents->profile()->GetBookmarkModel();
+  BookmarkModel* model = profile()->GetBookmarkModel();
   if (!model || !model->IsLoaded())
     return;  // Ignore requests until bookmarks are loaded.
 
+  GURL url;
+  std::wstring title;
+  bookmark_utils::GetURLAndTitleToBookmark(GetSelectedTabContents(), &url,
+                                           &title);
   bool was_bookmarked = model->IsBookmarked(url);
   model->SetURLStarred(url, title, true);
   if (window_->IsActive()) {
@@ -1683,15 +1681,7 @@ bool Browser::CanCloseContentsAt(int index) {
 
 bool Browser::CanBookmarkAllTabs() const {
   BookmarkModel* model = profile()->GetBookmarkModel();
-  if (!model || !model->IsLoaded())
-    return false;
-
-  int bookmarkable_tab_contents = 0;
-  for (int i = 0; i < tab_count(); ++i) {
-    if (CanBookmarkTabContents(GetTabContentsAt(i)))
-      ++bookmarkable_tab_contents;
-  }
-  return (bookmarkable_tab_contents > 1);
+  return (model && model->IsLoaded() && (tab_count() > 1));
 }
 
 void Browser::BookmarkAllTabs() {
@@ -2434,6 +2424,9 @@ void Browser::InitCommandState() {
     command_updater_.UpdateCommandEnabled(IDC_RESTORE_TAB,
         normal_window && !profile_->IsOffTheRecord());
 
+  // Page-related commands
+    command_updater_.UpdateCommandEnabled(IDC_BOOKMARK_PAGE, normal_window);
+
     // Show various bits of UI
     command_updater_.UpdateCommandEnabled(IDC_CLEAR_BROWSING_DATA,
                                           normal_window);
@@ -2465,8 +2458,6 @@ void Browser::UpdateCommandsForTabState() {
 
   // Page-related commands
   window_->SetStarredState(current_tab->is_starred());
-  command_updater_.UpdateCommandEnabled(IDC_BOOKMARK_PAGE,
-                                        CanBookmarkTabContents(current_tab));
   command_updater_.UpdateCommandEnabled(IDC_BOOKMARK_ALL_TABS,
                                         CanBookmarkAllTabs());
 
@@ -2497,11 +2488,6 @@ void Browser::UpdateStopGoState(bool is_loading, bool force) {
   window_->UpdateStopGoState(is_loading, force);
   command_updater_.UpdateCommandEnabled(IDC_GO, !is_loading);
   command_updater_.UpdateCommandEnabled(IDC_STOP, is_loading);
-}
-
-bool Browser::CanBookmarkTabContents(TabContents* tab_contents) const {
-  return (type() == TYPE_NORMAL) &&
-      bookmark_utils::GetURLAndTitleToBookmark(tab_contents, NULL, NULL);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
