8db3653b3d4e08971f8ee1f6aff488ae326749c6
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 04:44:37 +0000

    Revert r28519-28523. Accidentally used wrong command, committing a bunch of local changes individually with bad commit messages.
    TBR=rafaelw@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28525 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/theme_preview_infobar_delegate.h
+++ chrome/browser/extensions/theme_preview_infobar_delegate.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_VIEWS_EXTENSIONS_THEME_INSTALLED_INFOBAR_DELEGATE_H_
-#define CHROME_BROWSER_VIEWS_EXTENSIONS_THEME_INSTALLED_INFOBAR_DELEGATE_H_
+#ifndef CHROME_BROWSER_VIEWS_EXTENSIONS_THEME_PREVIEW_INFOBAR_DELEGATE_H_
+#define CHROME_BROWSER_VIEWS_EXTENSIONS_THEME_PREVIEW_INFOBAR_DELEGATE_H_
 
 #include "chrome/browser/tab_contents/infobar_delegate.h"
 
@@ -13,15 +13,18 @@ class TabContents;
 
 // When a user installs a theme, we display it immediately, but provide an
 // infobar allowing them to cancel.
-class ThemeInstalledInfoBarDelegate : public ConfirmInfoBarDelegate {
+//
+// TODO(aa): Rename this to ThemeInstalledInfoBarDelegate, since it isn't
+// used for previewing anymore.
+class ThemePreviewInfobarDelegate : public ConfirmInfoBarDelegate {
  public:
-  ThemeInstalledInfoBarDelegate(TabContents* tab_contents,
-                                const std::string& name,
-                                const std::string& previous_theme);
+  ThemePreviewInfobarDelegate(TabContents* tab_contents,
+                              const std::string& name,
+                              const std::string& previous_theme);
   virtual void InfoBarClosed();
   virtual std::wstring GetMessageText() const;
   virtual SkBitmap* GetIcon() const;
-  virtual ThemeInstalledInfoBarDelegate* AsThemePreviewInfobarDelegate();
+  virtual ThemePreviewInfobarDelegate* AsThemePreviewInfobarDelegate();
   virtual int GetButtons() const;
   virtual std::wstring GetButtonLabel(
       ConfirmInfoBarDelegate::InfoBarButton button) const;
@@ -36,4 +39,4 @@ class ThemeInstalledInfoBarDelegate : public ConfirmInfoBarDelegate {
   std::string previous_theme_id_;  // used to undo theme install
 };
 
-#endif  // CHROME_BROWSER_VIEWS_EXTENSIONS_THEME_INSTALLED_INFOBAR_DELEGATE_H_
+#endif  // CHROME_BROWSER_VIEWS_EXTENSIONS_THEME_PREVIEW_INFOBAR_DELEGATE_H_
