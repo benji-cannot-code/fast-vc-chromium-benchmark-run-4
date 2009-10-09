f0e094cf554d8a9fe0d269ae2819b7e03ad91a42
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-09 23:28:04 +0000

    [v8] Need change to V8WorkerContextEventListener::reportError signature to match the base class. https://bugs.webkit.org/show_bug.cgi?id=30264
    Patch by David Levin <levin@chromium.org> on 2009-10-09
    Reviewed by Eric Seidel.
    
    The base class method changed in r48884.
    
    Test: fast/worker/worker-script-error.html
    
    * bindings/v8/V8WorkerContextEventListener.cpp:
    (WebCore::V8WorkerContextEventListener::reportError):
    * bindings/v8/V8WorkerContextEventListener.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8WorkerContextEventListener.h
+++ third_party/WebKit/WebCore/bindings/v8/V8WorkerContextEventListener.h
@@ -51,7 +51,7 @@ namespace WebCore {
         }
 
         virtual void handleEvent(ScriptExecutionContext*, Event*);
-        virtual bool reportError(const String& message, const String& url, int lineNumber);
+        virtual bool reportError(ScriptExecutionContext*, const String& message, const String& url, int lineNumber);
 
         WorkerContextExecutionProxy* proxy() const { return m_proxy; }
 
