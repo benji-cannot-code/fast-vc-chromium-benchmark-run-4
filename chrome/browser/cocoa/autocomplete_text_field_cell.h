9ccbc5993b3e6ddb2292b63499141d8ef3307156
hawk@chromium.org <hawk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 23:41:56 +0000

    Add EV certificate text to the Mac location bar BUG=10910 TEST=EV sites (e.g., http://www.paypal.com and most bank sites) get a green text description next to the lock icon in the location bar, non-EV (and non-SSL) sites do not Review URL: http://codereview.chromium.org/216031
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28627 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/autocomplete_text_field_cell.h
+++ chrome/browser/cocoa/autocomplete_text_field_cell.h
@@ -41,6 +41,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Icon that represents the state of the SSL connection
   scoped_nsobject<NSImage> hintIcon_;
+
+  // Optional text that appears to the right of the hint icon which
+  // appears only alongside the icon (i.e., it's possible to display a
+  // hintIcon without an hintIconLabel, but not vice-versa).
+  scoped_nsobject<NSAttributedString> hintIconLabel_;
 }
 
 @property BOOL fieldEditorNeedsReset;
@@ -60,7 +65,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setSearchHintString:(NSString*)aString;
 - (void)clearKeywordAndHint;
 
-- (void)setHintIcon:(NSImage*)icon;
+// Sets the hint icon and optional icon label. If |icon| is nil, the current
+// icon is cleared. If |label| is provided, |color| must be provided as well.
+- (void)setHintIcon:(NSImage*)icon label:(NSString*)label color:(NSColor*)color;
 
 // Return the portion of the cell to show the text cursor over.
 - (NSRect)textCursorFrameForFrame:(NSRect)cellFrame;
@@ -80,5 +87,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(readonly) NSAttributedString* keywordString;
 @property(readonly) NSAttributedString* hintString;
 @property(readonly) NSImage* hintIcon;
+@property(readonly) NSAttributedString* hintIconLabel;
 
 @end
