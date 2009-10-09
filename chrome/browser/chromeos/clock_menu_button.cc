4953c06048a6a12b227643b2b40a629b7549f840
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 18:44:00 +0000

    Really fix the changed include to fix the ChromeOS build this time.
    BUG=none
    TEST=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28577 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/clock_menu_button.cc
+++ chrome/browser/chromeos/clock_menu_button.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/gfx/font.h"
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
-#include "base/i18n/time_format.h"
+#include "base/i18n/time_formatting.h"
 #include "base/string_util.h"
 #include "base/time.h"
 #include "grit/generated_resources.h"
