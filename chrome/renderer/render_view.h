70cf7f5e5572a44c67b45a447552810b49208b27
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 04:08:59 +0000

    Eliminate WebView::GetDelegate and replace RenderViewSet with a map from WebView to RenderView.
    R=mpcomplete
    BUG=10033
    TEST=none
    
    Review URL: http://codereview.chromium.org/265044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28515 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.h
+++ chrome/renderer/render_view.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_RENDERER_RENDER_VIEW_H_
 #define CHROME_RENDERER_RENDER_VIEW_H_
 
+#include <map>
 #include <set>
 #include <string>
 #include <queue>
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/external_host_bindings.h"
 #include "chrome/renderer/notification_provider.h"
 #include "chrome/renderer/render_widget.h"
+#include "chrome/renderer/render_view_visitor.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "testing/gtest/include/gtest/gtest_prod.h"
 #include "webkit/api/public/WebConsoleMessage.h"
@@ -112,9 +114,12 @@ class RenderView : public RenderWidget,
                    public webkit_glue::DomSerializerDelegate,
                    public base::SupportsWeakPtr<RenderView> {
  public:
-  struct RenderViewSet {
-    std::set<RenderView*> render_view_set_;
-  };
+  // Visit all RenderViews with a live WebView (i.e., RenderViews that have
+  // been closed but not yet destroyed are excluded).
+  static void ForEach(RenderViewVisitor* visitor);
+
+  // Returns the RenderView containing the given WebView.
+  static RenderView* FromWebView(WebView* webview);
 
   // Creates a new RenderView.  The parent_hwnd specifies a HWND to use as the
   // parent of the WebView HWND that will be created.  If this is a constrained
@@ -432,12 +437,11 @@ class RenderView : public RenderWidget,
   bool SendAndRunNestedMessageLoop(IPC::SyncMessage* message);
 
  protected:
-  // RenderWidget override.
+  // RenderWidget overrides:
+  virtual void Close();
   virtual void OnResize(const gfx::Size& new_size,
                         const gfx::Rect& resizer_rect);
-  // RenderWidget override
   virtual void DidPaint();
-  // RenderWidget override.
   virtual void DidHandleKeyEvent();
 
  private:
@@ -811,6 +815,10 @@ class RenderView : public RenderWidget,
   // same page twice in a row.
   int32 last_indexed_page_id_;
 
+  // The next available page ID to use. This ensures that the page IDs are
+  // globally unique in the renderer.
+  static int32 next_page_id_;
+
   // Used for popups.
   bool opened_by_user_gesture_;
   GURL creator_url_;
@@ -983,6 +991,7 @@ class RenderView : public RenderWidget,
   typedef std::set<webkit_glue::ImageResourceFetcher*> ImageResourceFetcherSet;
   ImageResourceFetcherSet image_fetchers_;
 
+  typedef std::map<WebView*, RenderView*> ViewMap;
 
   DISALLOW_COPY_AND_ASSIGN(RenderView);
 };
