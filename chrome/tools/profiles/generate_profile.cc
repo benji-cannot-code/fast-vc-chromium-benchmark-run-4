d5895b8f6bd323d5b8c5e525990243801b3cf22d
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 20:12:17 +0000

    Fix an include.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28585 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/tools/profiles/generate_profile.cc
+++ chrome/tools/profiles/generate_profile.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/gfx/codec/jpeg_codec.h"
 #include "base/at_exit.h"
 #include "base/file_path.h"
-#include "base/icu_util.h"
+#include "base/i18n/icu_util.h"
 #include "base/message_loop.h"
 #include "base/path_service.h"
 #include "base/process_util.h"
--- chrome_frame/test/net/fake_external_tab.cc
+++ chrome_frame/test/net/fake_external_tab.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/file_util.h"
-#include "base/icu_util.h"
+#include "base/i18n/icu_util.h"
 #include "base/path_service.h"
 #include "base/scoped_bstr_win.h"
 #include "base/scoped_comptr_win.h"
