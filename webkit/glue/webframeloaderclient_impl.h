0f01ac99935879f76a18480f2e0103991ecf1f33
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 21:23:12 +0000

    Convert types in WebFrameLoaderClientImpl in preparation for moving this class into the WebKit API implementation.
    This is a redo of r28545, which was reverted in r28552.
    
    TBR=dglazkov
    BUG=10034
    TEST=none
    
    Review URL: http://codereview.chromium.org/261047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28599 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webframeloaderclient_impl.h
+++ webkit/glue/webframeloaderclient_impl.h
@@ -7,10 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WEBKIT_GLUE_WEBFRAMELOADERCLIENT_IMPL_H_
 
 #include "FrameLoaderClient.h"
+#include "KURL.h"
 #include <wtf/PassOwnPtr.h>
 #include <wtf/RefPtr.h>
 
-#include "googleurl/src/gurl.h"
 #include "webkit/api/public/WebNavigationPolicy.h"
 #include "webkit/glue/webview_delegate.h"
 
@@ -210,7 +210,7 @@ class WebFrameLoaderClient : public WebCore::FrameLoaderClient {
       WebKit::WebNavigationPolicy* policy);
 
   // Called when a dummy back-forward navigation is intercepted.
-  void HandleBackForwardNavigation(const GURL&);
+  void HandleBackForwardNavigation(const WebCore::KURL&);
 
   PassOwnPtr<WebKit::WebPluginLoadObserver> GetPluginLoadObserver();
 
@@ -229,8 +229,8 @@ class WebFrameLoaderClient : public WebCore::FrameLoaderClient {
   // and the dest URL matches that load, we know that it was the result of a
   // previous client redirect and the source should be added as a redirect.
   // Both should be empty if unused.
-  GURL expected_client_redirect_src_;
-  GURL expected_client_redirect_dest_;
+  WebCore::KURL expected_client_redirect_src_;
+  WebCore::KURL expected_client_redirect_dest_;
 
   // Contains a pointer to the plugin widget.
   WTF::RefPtr<WebKit::WebPluginContainerImpl> plugin_widget_;
