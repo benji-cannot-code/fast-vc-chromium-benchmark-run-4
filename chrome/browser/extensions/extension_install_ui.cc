070b20b1d2aaaac49a2c1b4b969d11fc392e7a90
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 20:52:48 +0000

    Reland: Rename ThemePreviewInfobarDelegate to ThemeInstalledInfoBarDelegate
    Contributed by Thiago Farina <tfarina@gmail.com>
    
    TBR=rafaelw@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28595 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_ui.cc
+++ chrome/browser/extensions/extension_install_ui.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/rand_util.h"
 #include "chrome/browser/browser_list.h"
 #include "chrome/browser/browser_window.h"
-#include "chrome/browser/extensions/theme_preview_infobar_delegate.h"
+#include "chrome/browser/extensions/theme_installed_infobar_delegate.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/common/extensions/extension.h"
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sys_string_conversions.h"
 #include <CoreFoundation/CFUserNotification.h>
 #elif defined(TOOLKIT_GTK)
-#include "chrome/browser/extensions/gtk_theme_preview_infobar_delegate.h"
+#include "chrome/browser/extensions/gtk_theme_installed_infobar_delegate.h"
 #include "chrome/browser/gtk/gtk_theme_provider.h"
 #endif
 
@@ -232,12 +232,12 @@ void ExtensionInstallUI::ShowThemeInfoBar(Extension* new_theme) {
   // Then either replace that old one or add a new one.
   InfoBarDelegate* new_delegate =
 #if defined(TOOLKIT_GTK)
-      new GtkThemePreviewInfobarDelegate(
+      new GtkThemeInstalledInfoBarDelegate(
           tab_contents,
           new_theme->name(), previous_theme_id_, previous_use_gtk_theme_);
 #else
-      new ThemePreviewInfobarDelegate(tab_contents,
-                                      new_theme->name(), previous_theme_id_);
+      new ThemeInstalledInfoBarDelegate(tab_contents,
+                                        new_theme->name(), previous_theme_id_);
 #endif
 
   if (old_delegate)
