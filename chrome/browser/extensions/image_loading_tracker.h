d17c1c6f21549d6a8c256adb0bf44e545a47c4f6
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 00:58:57 +0000

    Revert r28489 as it broke the linux build.
    TBR=rafaelw@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28495 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/image_loading_tracker.h
+++ chrome/browser/extensions/image_loading_tracker.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTENSIONS_IMAGE_LOADING_TRACKER_H_
 #define CHROME_BROWSER_EXTENSIONS_IMAGE_LOADING_TRACKER_H_
 
-#include "base/gfx/size.h"
 #include "base/ref_counted.h"
 
 class ExtensionResource;
@@ -47,8 +46,7 @@ class ImageLoadingTracker
   // Specify image resource to load.  This method must be called a number of
   // times equal to the |image_count| arugment to the constructor.  Calling it
   // any more or less than that is an error.
-  void PostLoadImageTask(const ExtensionResource& resource,
-                         gfx::Size* max_size);
+  void PostLoadImageTask(const ExtensionResource& resource);
 
  private:
   class LoadImageTask;
