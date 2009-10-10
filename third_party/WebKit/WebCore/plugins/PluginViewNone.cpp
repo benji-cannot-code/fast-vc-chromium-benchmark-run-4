aa40f12ba02b7cd89ede7dbce43b71c527f85e13
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-10 16:19:26 +0000

    2009-10-10  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Holger Freyther.
    
            Build fix if Netscape plugin support is turned off
            https://bugs.webkit.org/show_bug.cgi?id=30275
    
            In addition to fixing the build break with guards
            remove notImplemented() from PluginViewNone.cpp and
            make them just just be empty functions.
    
            * plugins/PluginView.cpp:
            (WebCore::PluginView::handleEvent):
            (WebCore::PluginView::PluginView):
            * plugins/PluginView.h:
            * plugins/PluginViewNone.cpp:
            (WebCore::PluginView::setFocus):
            (WebCore::PluginView::show):
            (WebCore::PluginView::hide):
            (WebCore::PluginView::paint):
            (WebCore::PluginView::handleKeyboardEvent):
            (WebCore::PluginView::handleMouseEvent):
            (WebCore::PluginView::setParent):
            (WebCore::PluginView::setNPWindowRect):
            (WebCore::PluginView::handlePostReadFile):
            (WebCore::PluginView::getValue):
            (WebCore::PluginView::getValueStatic):
            (WebCore::PluginView::invalidateRect):
            (WebCore::PluginView::invalidateRegion):
            (WebCore::PluginView::forceRedraw):
            (WebCore::PluginView::platformStart):
            (WebCore::PluginView::platformDestroy):
            (WebCore::PluginView::setParentVisible):
            (WebCore::PluginView::updatePluginWidget):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49418 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/PluginViewNone.cpp
+++ third_party/WebKit/WebCore/plugins/PluginViewNone.cpp
@@ -27,114 +27,90 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "PluginView.h"
 
-#include "NotImplemented.h"
-#include "PluginPackage.h"
-
 using namespace WTF;
 
 namespace WebCore {
 
 void PluginView::setFocus()
 {
-    notImplemented();
 }
 
 void PluginView::show()
 {
-    notImplemented();
 }
 
 void PluginView::hide()
 {
-    notImplemented();
 }
 
 void PluginView::paint(GraphicsContext*, const IntRect&)
 {
-    notImplemented();
 }
 
 void PluginView::handleKeyboardEvent(KeyboardEvent*)
 {
-    notImplemented();
 }
 
 void PluginView::handleMouseEvent(MouseEvent*)
 {
-    notImplemented();
 }
 
 void PluginView::setParent(ScrollView*)
 {
-    notImplemented();
 }
 
 void PluginView::setNPWindowRect(const IntRect&)
 {
-    notImplemented();
 }
 
 NPError PluginView::handlePostReadFile(Vector<char>&, uint32, const char*)
 {
-    notImplemented();
-
     return 0;
 }
 
 NPError PluginView::getValue(NPNVariable, void*)
 {
-    notImplemented();
     return 0;
 }
 
 #if ENABLE(NETSCAPE_PLUGIN_API)
 NPError PluginView::getValueStatic(NPNVariable variable, void* value)
 {
-    notImplemented();
     return 0;
 }
 #endif
 
 void PluginView::invalidateRect(NPRect*)
 {
-    notImplemented();
 }
 
 void PluginView::invalidateRect(const IntRect&)
 {
-    notImplemented();
 }
 
 void PluginView::invalidateRegion(NPRegion)
 {
-    notImplemented();
 }
 
 void PluginView::forceRedraw()
 {
-    notImplemented();
 }
 
 bool PluginView::platformStart()
 {
-    notImplemented();
-
     return true;
 }
 
 void PluginView::platformDestroy()
 {
-    notImplemented();
 }
 
 void PluginView::setParentVisible(bool)
 {
-    notImplemented();
 }
 
 void PluginView::updatePluginWidget()
 {
-    notImplemented();
 }
 
 void PluginView::halt()
