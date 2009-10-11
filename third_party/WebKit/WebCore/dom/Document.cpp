860af428f1638b6cdd10b884058cc596c055cd27
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-11 22:34:05 +0000

    2009-10-11  Lyon Chen  <lyon.chen@torchmobile.com>
            Reviewed by Alexey Proskuryakov.
    
            Correct the position of #endif sentence inside WebCore::Document::finishedParsing().
    
            https://bugs.webkit.org/show_bug.cgi?id=30244
    
            This change requires no test case as it fix a compiling error.
    
            * dom/Document.cpp:
            (WebCore::Document::finishedParsing):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49428 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -4028,8 +4028,8 @@ void Document::finishedParsing()
 
         if (InspectorController* controller = page()->inspectorController())
             controller->mainResourceFiredDOMContentEvent(f->loader()->documentLoader(), url());
-    }
 #endif
+    }
 }
 
 Vector<String> Document::formElementsState() const
