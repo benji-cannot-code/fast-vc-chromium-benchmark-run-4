4676500a1457be0ca5ad01efc14c542529e390a2
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-12 15:26:53 +0000

    2009-10-12  Cameron McCormack  <cam@mcc.id.au>
            Reviewed by Eric Seidel.
    
            Drop in="" from <feFlood>
            https://bugs.webkit.org/show_bug.cgi?id=29001
    
            This makes <feFlood in="bogus"> have an effect, too.
    
            http://www.w3.org/2003/01/REC-SVG11-20030114-errata#feflood-attribute
    
            Test: svg/dom/feFlood-no-in1.html
    
            * svg/SVGFEFloodElement.cpp:
            (WebCore::SVGFEFloodElement::SVGFEFloodElement):
            (WebCore::SVGFEFloodElement::build):
            * svg/SVGFEFloodElement.h:
            * svg/SVGFEFloodElement.idl:
            * svg/graphics/filters/SVGFEFlood.cpp:
            (WebCore::FEFlood::FEFlood):
            (WebCore::FEFlood::create):
            * svg/graphics/filters/SVGFEFlood.h:
    
            LayoutTest:
    
            Drop in="" from <feFlood>
            https://bugs.webkit.org/show_bug.cgi?id=29001
    
            This makes <feFlood in="bogus"> have an effect, too.
    
            http://www.w3.org/2003/01/REC-SVG11-20030114-errata#feflood-attribute
    
            * svg/dom/feFlood-no-in1-expected.txt: Added.
            * svg/dom/feFlood-no-in1.html: Added.
            * svg/dom/script-tests/feFlood-no-in1.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/graphics/filters/SVGFEFlood.h
+++ third_party/WebKit/WebCore/svg/graphics/filters/SVGFEFlood.h
@@ -32,7 +32,7 @@ namespace WebCore {
 
     class FEFlood : public FilterEffect {
     public:
-        static PassRefPtr<FEFlood> create(FilterEffect*, const Color&, const float&);
+        static PassRefPtr<FEFlood> create(const Color&, const float&);
 
         Color floodColor() const;
         void setFloodColor(const Color &);
@@ -45,9 +45,8 @@ namespace WebCore {
         TextStream& externalRepresentation(TextStream& ts) const;
 
     private:
-        FEFlood(FilterEffect*, const Color&, const float&);
+        FEFlood(const Color&, const float&);
 
-        RefPtr<FilterEffect> m_in;
         Color m_floodColor;
         float m_floodOpacity;
     };
