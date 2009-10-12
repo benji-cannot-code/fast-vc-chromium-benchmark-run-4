4197176ad2f4ce17d98a47984748c5fd46fa4405
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-12 15:48:32 +0000

    Fix ChromeOS build by adding missing header.
    TBR=brettw
    
    Review URL: http://codereview.chromium.org/274002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28706 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/main_menu.cc
+++ chrome/browser/chromeos/main_menu.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/render_view_host_delegate_helper.h"
 #include "grit/app_resources.h"
 #include "grit/generated_resources.h"
+#include "grit/theme_resources.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "views/background.h"
 #include "views/painter.h"
