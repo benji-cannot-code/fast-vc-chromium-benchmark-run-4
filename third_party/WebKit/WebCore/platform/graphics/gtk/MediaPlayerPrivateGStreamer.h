68876b207bd9b60ca2ab48a328ab561d81e6472b
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-12 14:18:48 +0000

    2009-10-12  Sebastian Dröge  <sebastian.droege@collabora.co.uk>
            Reviewed by Gustavo Noronha.
    
            https://bugs.webkit.org/show_bug.cgi?id=29997
    
            Fixes double memcpy of all rendered video frames.
    
            * platform/graphics/gtk/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::mediaPlayerPrivateRepaintCallback):
            (WebCore::MediaPlayerPrivate::MediaPlayerPrivate):
            (WebCore::MediaPlayerPrivate::~MediaPlayerPrivate):
            (WebCore::MediaPlayerPrivate::duration):
            * platform/graphics/gtk/MediaPlayerPrivateGStreamer.h:
            * platform/graphics/gtk/VideoSinkGStreamer.cpp:
            (webkit_video_sink_timeout_func):
            (webkit_video_sink_dispose):
            (unlock_buffer_mutex):
            (webkit_video_sink_unlock):
            (marshal_VOID__MINIOBJECT):
            (webkit_video_sink_class_init):
            (webkit_video_sink_new):
            * platform/graphics/gtk/VideoSinkGStreamer.h:
            Directly pass the buffers up to the MediaPlayerPrivate::paint()
            method instead of rendering them first to an intermediate cairo
            surface. This should increase performance and peak memory usage.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gtk/MediaPlayerPrivateGStreamer.h
+++ third_party/WebKit/WebCore/platform/graphics/gtk/MediaPlayerPrivateGStreamer.h
@@ -31,8 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cairo.h>
 #include <glib.h>
 
-typedef struct _GstElement GstElement;
+typedef struct _WebKitVideoSink WebKitVideoSink;
+typedef struct _GstBuffer GstBuffer;
 typedef struct _GstMessage GstMessage;
+typedef struct _GstElement GstElement;
 typedef struct _GstBus GstBus;
 
 namespace WebCore {
@@ -50,6 +52,7 @@ namespace WebCore {
         friend gboolean mediaPlayerPrivateErrorCallback(GstBus* bus, GstMessage* message, gpointer data);
         friend gboolean mediaPlayerPrivateEOSCallback(GstBus* bus, GstMessage* message, gpointer data);
         friend gboolean mediaPlayerPrivateStateCallback(GstBus* bus, GstMessage* message, gpointer data);
+        friend void mediaPlayerPrivateRepaintCallback(WebKitVideoSink*, GstBuffer *buffer, MediaPlayerPrivate* playerPrivate);
 
         public:
             static void registerMediaEngine(MediaEngineRegistrar);
@@ -137,7 +140,7 @@ namespace WebCore {
             mutable bool m_isStreaming;
             IntSize m_size;
             bool m_visible;
-            cairo_surface_t* m_surface;
+            GstBuffer* m_buffer;
 
             bool m_paused;
             bool m_seeking;
