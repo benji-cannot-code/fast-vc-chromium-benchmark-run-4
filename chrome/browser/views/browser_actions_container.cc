1f3129435aad45c07c94c5b695c48d20dfad9d1c
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-13 20:22:49 +0000

    Use predefined color names where possible for clarity.  Also use "SkColorSetARGB(0, 0, 0, 0)" in place of "NULL" since it's not obvious what the latter means as a color.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/273042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28874 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/browser_actions_container.cc
+++ chrome/browser/views/browser_actions_container.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -304,7 +304,7 @@ void BrowserActionView::PaintChildren(gfx::Canvas* canvas) {
   SkTypeface* typeface = SkTypeface::CreateFromName("Arial", SkTypeface::kBold);
   SkPaint text_paint;
   text_paint.setAntiAlias(true);
-  text_paint.setColor(SkColorSetARGB(255, 255, 255, 255));
+  text_paint.setColor(SK_ColorWHITE);
   text_paint.setFakeBoldText(true);
   text_paint.setTextAlign(SkPaint::kLeft_Align);
   text_paint.setTextSize(SkIntToScalar(kTextSize));
--- chrome/browser/views/find_bar_view.cc
+++ chrome/browser/views/find_bar_view.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -45,7 +45,7 @@ static const SkColor kTextColorMatchCount = SkColorSetRGB(178, 178, 178);
 static const SkColor kTextColorNoMatch = SK_ColorBLACK;
 
 // The background color of the match count label when results are found.
-static const SkColor kBackgroundColorMatch = SkColorSetRGB(255, 255, 255);
+static const SkColor kBackgroundColorMatch = SK_ColorWHITE;
 
 // The background color of the match count label when no results are found.
 static const SkColor kBackgroundColorNoMatch = SkColorSetRGB(255, 102, 102);
