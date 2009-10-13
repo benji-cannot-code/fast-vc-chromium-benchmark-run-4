7ec1add4c2455525623eef569e865ff64886a518
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-13 20:16:34 +0000

    Implement getter/setter-based bound properties for CppBoundClass.
    This is necessary for AccessibilityController, which has non-trivial
    property accessors.
    
    R=darin
    BUG=10322
    TEST=CppBoundClassTest.SetAndGetPropertiesWithCallbacks
    
    Review URL: http://codereview.chromium.org/243064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28872 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/cpp_binding_example.cc
+++ webkit/glue/cpp_binding_example.cc
@@ -3,10 +3,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This file contains the definition for CppBindingExample, a usage example
-// that is not actually used anywhere.  See cpp_binding_example.h.
+// This file contains the definition for CppBindingExample, which is used in
+// cpp_bound_class_unittest.
 
 #include "cpp_binding_example.h"
+
+namespace {
+
+class PropertyCallbackExample : public CppBoundClass::PropertyCallback {
+ public:
+  virtual bool GetValue(CppVariant* value) {
+    value->Set(value_);
+    return true;
+  }
+
+  virtual bool SetValue(const CppVariant& value) {
+    value_.Set(value);
+    return true;
+  }
+
+ private:
+  CppVariant value_;
+};
+
+}
 
 CppBindingExample::CppBindingExample() {
   // Map properties.  It's recommended, but not required, that the JavaScript
@@ -14,6 +34,11 @@ CppBindingExample::CppBindingExample() {
   // variables exposed through those names.
   BindProperty("my_value", &my_value);
   BindProperty("my_other_value", &my_other_value);
+
+  // Bind property with a callback.
+  BindProperty("my_value_with_callback", new PropertyCallbackExample());
+  // Bind property with a getter callback.
+  BindProperty("same", &CppBindingExample::same);
 
   // Map methods.  See comment above about names.
   BindMethod("echoValue", &CppBindingExample::echoValue);
@@ -87,6 +112,10 @@ void CppBindingExample::plus(const CppArgumentList& args,
     sum += arg2.value.intValue;
 
   result->Set(sum);
+}
+
+void CppBindingExample::same(CppVariant* result) {
+  result->Set(42);
 }
 
 void CppBindingExample::fallbackMethod(const CppArgumentList& args,
