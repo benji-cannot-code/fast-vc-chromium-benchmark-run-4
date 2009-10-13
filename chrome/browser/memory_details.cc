e409278fcd22ea932ad2aa03b61a699c93060468
gregoryd@google.com <gregoryd@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-13 21:15:29 +0000

    Revert 28875 - Adding support for Native Client in Chrome's task manager and "stats for nerds" screen. TEST=none BUG=http://code.google.com/p/nativeclient/issues/detail?id=108 Review URL: http://codereview.chromium.org/267043
    TBR=gregoryd@google.com
    
    Review URL: http://codereview.chromium.org/266067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28888 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/memory_details.cc
+++ chrome/browser/memory_details.cc
@@ -207,9 +207,6 @@ void MemoryDetails::UpdateHistograms() {
      case ChildProcessInfo::SANDBOX_HELPER_PROCESS:
        UMA_HISTOGRAM_MEMORY_KB("Memory.SandboxHelper", sample);
        break;
-     case ChildProcessInfo::NACL_PROCESS:
-       UMA_HISTOGRAM_MEMORY_KB("Memory.NativeClient", sample);
-       break;
      default:
        NOTREACHED();
     }
