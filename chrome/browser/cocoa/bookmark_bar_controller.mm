1262469fc54cbeb6008f24a25a1dbd4bd4493e1d
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-13 17:18:47 +0000

    Mac: Fix bookmark bar buttons not being layed out correctly on startup.
    BUG=17625
    
    Review URL: http://codereview.chromium.org/271058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28826 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bar_controller.mm
+++ chrome/browser/cocoa/bookmark_bar_controller.mm
@@ -56,15 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (int)preferredHeight;
 @end
 
-namespace {
-
-// Magic numbers from Cole
-const CGFloat kDefaultBookmarkWidth = 150.0;
-const CGFloat kBookmarkVerticalPadding = 2.0;
-const CGFloat kBookmarkHorizontalPadding = 1.0;
-
-}  // namespace
-
 @implementation BookmarkBarController
 
 - (id)initWithBrowser:(Browser*)browser
@@ -553,7 +544,7 @@ const CGFloat kBookmarkHorizontalPadding = 1.0;
 // For a bookmark named "SFGateFooWoo", it is just fine.
 - (CGFloat)widthForBookmarkButtonCell:(NSCell*)cell {
   CGFloat desired = [cell cellSize].width + 2;
-  return std::min(desired, kDefaultBookmarkWidth);
+  return std::min(desired, bookmarks::kDefaultBookmarkWidth);
 }
 
 // Returns a frame appropriate for the given bookmark cell, suitable
@@ -562,13 +553,17 @@ const CGFloat kBookmarkHorizontalPadding = 1.0;
 // for the next button.
 - (NSRect)frameForBookmarkButtonFromCell:(NSCell*)cell
                                  xOffset:(int*)xOffset {
-  NSRect bounds = [[self view] bounds];
-  // TODO: be smarter about this; the animator delays the right height
-  if (bounds.size.height == 0)
-    bounds.size.height = bookmarks::kBookmarkBarHeight;
+  NSRect bounds = [buttonView_ bounds];
+  // TODO(erg,jrg): There used to be an if statement here, comparing the height
+  // to 0. This essentially broke sizing because we're dealing with floats and
+  // the height wasn't precisely zero. The previous author wrote that they were
+  // doing this because of an animator, but we are not doing animations for beta
+  // so we do not care.
+  bounds.size.height = bookmarks::kBookmarkBarHeight;
+
   NSRect frame = NSInsetRect(bounds,
-                             kBookmarkHorizontalPadding,
-                             kBookmarkVerticalPadding);
+                             bookmarks::kBookmarkHorizontalPadding,
+                             bookmarks::kBookmarkVerticalPadding);
   frame.size.width = [self widthForBookmarkButtonCell:cell];
 
   // Add an X offset based on what we've already done
