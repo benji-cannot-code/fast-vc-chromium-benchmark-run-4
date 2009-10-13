44f7d6bb6a0212cebbbc2a4c92809f18402a6f47
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-13 06:11:19 +0000

    2009-10-12  Jakub Wieczorek  <faw217@gmail.com>
            Reviewed by Simon Hausmann.
    
            [Qt] Missing default value for the default text encoding.
            https://bugs.webkit.org/show_bug.cgi?id=30311
    
            QtWebKit has provided a default, hardcoded value for default charset but since
            the addition of the defaultTextEncoding setting in QWebSettings, that hardcoded
            value has had no effect.
    
            Added a regression test and unskipped fast/dom/Document/document-charset.html,
            which is passing now.
    
            * platform/qt/Skipped:
    2009-10-12  Jakub Wieczorek  <faw217@gmail.com>
    
            Reviewed by Simon Hausmann.
    
            [Qt] Missing default value for the default text encoding.
            https://bugs.webkit.org/show_bug.cgi?id=30311
    
            QtWebKit has provided a default, hardcoded value for default charset but since
            the addition of the defaultTextEncoding setting in QWebSettings, that hardcoded
            value has had no effect.
    
            Added a regression test and unskipped fast/dom/Document/document-charset.html,
            which is passing now.
    
            * Api/qwebpage.cpp:
            (QWebPagePrivate::QWebPagePrivate):
            * Api/qwebsettings.cpp:
            (QWebSettings::QWebSettings):
            * tests/qwebpage/tst_qwebpage.cpp:
            (tst_QWebPage::defaultTextEncoding):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49492 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebsettings.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebsettings.cpp
@@ -378,6 +378,7 @@ QWebSettings::QWebSettings()
     d->attributes.insert(QWebSettings::LocalContentCanAccessRemoteUrls, false);
     d->attributes.insert(QWebSettings::SessionStorageEnabled, true);
     d->offlineStorageDefaultQuota = 5 * 1024 * 1024;
+    d->defaultTextEncoding = QLatin1String("iso-8859-1");
 }
 
 /*!
