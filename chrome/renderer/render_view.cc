a55e206bebe571e6ba93735ba406760aabfaafb0
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-13 02:47:16 +0000

    Spawn the pepper WebPluginDelegateImpl when a MIME type begins with "pepper-".
    This is a temporary hack for testing. I wrapped it in USE_PEPPER_PLUGINS ifdef,
    what ifdef are you using in real life?
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/271047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28782 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -89,6 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/password_form.h"
 #include "webkit/glue/plugins/plugin_list.h"
 #include "webkit/glue/plugins/webplugin_delegate_impl.h"
+#include "webkit/glue/plugins/webplugin_delegate_pepper_impl.h"
 #include "webkit/glue/searchable_form_data.h"
 #include "webkit/glue/webaccessibilitymanager_impl.h"
 #include "webkit/glue/webdevtoolsagent_delegate.h"
@@ -2434,6 +2435,14 @@ webkit_glue::WebPluginDelegate* RenderView::CreatePluginDelegate(
     mime_type_to_use = actual_mime_type;
   else
     mime_type_to_use = &mime_type;
+
+#if defined(PEPPER_APIS_ENABLED)
+  const char kPepperPrefix[] = "pepper-";
+  if (StartsWithASCII(*mime_type_to_use, kPepperPrefix, true)) {
+    return WebPluginDelegatePepperImpl::Create(
+        path, *mime_type_to_use, gfx::NativeViewFromId(host_window_));
+  }
+#endif
 
   bool in_process_plugin = RenderProcess::current()->in_process_plugins();
   if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kInternalNaCl)) {
