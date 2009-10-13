f5bb1bb12d20c3fff7a2e352cf2af821455602b0
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-13 23:58:45 +0000

    2009-10-13  Simon Fraser  <simon.fraser@apple.com>
            Temporary debugging changes to figure out why the webgl tests are crashing on the build bot.
    
            * platform/graphics/mac/GraphicsContext3DMac.cpp:
            (WebCore::GraphicsContext3D::GraphicsContext3D):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/GraphicsContext3DMac.cpp
+++ third_party/WebKit/WebCore/platform/graphics/mac/GraphicsContext3DMac.cpp
@@ -55,6 +55,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 GraphicsContext3D::GraphicsContext3D()
+    : m_contextObj(0)
+    , m_texture(0)
+    , m_fbo(0)
+    , m_depthBuffer(0)
 {
     CGLPixelFormatAttribute attribs[] =
     {
@@ -65,12 +69,23 @@ GraphicsContext3D::GraphicsContext3D()
         (CGLPixelFormatAttribute) 0
     };
     
-    CGLPixelFormatObj pixelFormatObj;
-    GLint numPixelFormats;
+    CGLPixelFormatObj pixelFormatObj = 0;
+    GLint numPixelFormats = 0;
     
-    CGLChoosePixelFormat(attribs, &pixelFormatObj, &numPixelFormats);
+    CGLError err = CGLChoosePixelFormat(attribs, &pixelFormatObj, &numPixelFormats);
+    if (err != kCGLNoError) {
+        // FIXME: temporary change to get error.
+        fprintf(stderr, "CGLChoosePixelFormat failed, err %d\n", err);
+        return;
+    }
     
-    CGLCreateContext(pixelFormatObj, 0, &m_contextObj);
+    err = CGLCreateContext(pixelFormatObj, 0, &m_contextObj);
+    if (err != kCGLNoError || !m_contextObj) {
+        // FIXME: temporary change to get error.
+        fprintf(stderr, "CGLCreateContext failed, err %d (context %p)\n", err, m_contextObj);
+        m_contextObj = 0;
+        return;
+    }
     
     CGLDestroyPixelFormat(pixelFormatObj);
     
