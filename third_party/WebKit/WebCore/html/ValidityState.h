29f17738262154932de53f20e7f97e20c14e6a3a
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-13 20:53:12 +0000

    2009-10-13  Michelangelo De Simone  <micdesim@gmail.com>
            Reviewed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=27457
            Test case for static email validation on type=email input elements as
            per HTML5 specs:
            http://www.whatwg.org/specs/web-apps/current-work/#e-mail-state
    
            * fast/forms/ValidityState-typeMismatch-email-expected.txt: Added.
            * fast/forms/ValidityState-typeMismatch-email.html: Added.
            * fast/forms/resources/ValidityState-typeMismatch-email.js: Added.
            ():
    2009-10-13  Michelangelo De Simone  <micdesim@gmail.com>
    
            Reviewed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=27457
            Added support for static validation on type=email input elements as per
            HTML5 specs:
            http://www.whatwg.org/specs/web-apps/current-work/#e-mail-state
    
            Test: fast/forms/ValidityState-typeMismatch-email.html
    
            * html/ValidityState.cpp:
            (WebCore::ValidityState::typeMismatch): ValidityState.typeMismatch
            performs validation on type=email input elements now.
            (WebCore::ValidityState::isValidEmailAddress): simple validation method
            * html/ValidityState.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49508 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/ValidityState.h
+++ third_party/WebKit/WebCore/html/ValidityState.h
@@ -57,6 +57,7 @@ namespace WebCore {
         String m_customErrorMessage;
 
         static bool isValidColorString(const String&);
+        bool isValidEmailAddress(const String&);
     };
 
 } // namespace WebCore
