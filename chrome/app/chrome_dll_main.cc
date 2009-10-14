954679094507b9283873ac9eb93538c7a6429318
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-14 20:30:40 +0000

    linux: tickle the render sandbox early so it forks at the right time
    Otherwise, the first time the singleton is touched we've already started
    loading a page!  We surely have a bunch of threads going at that point.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=27528
    
    Review URL: http://codereview.chromium.org/245039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29010 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_dll_main.cc
+++ chrome/app/chrome_dll_main.cc
@@ -36,9 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/debug_util.h"
-#if defined(OS_POSIX)
-#include "base/global_descriptors_posix.h"
-#endif
 #include "base/i18n/icu_util.h"
 #include "base/message_loop.h"
 #include "base/path_service.h"
@@ -47,17 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stats_counters.h"
 #include "base/stats_table.h"
 #include "base/string_util.h"
-#if defined(OS_WIN)
-#include "base/win_util.h"
-#endif
-#if defined(OS_MACOSX)
-#include "base/mac_util.h"
-#include "chrome/common/chrome_paths_internal.h"
-#include "chrome/app/breakpad_mac.h"
-#endif
-#if defined(OS_LINUX)
-#include "base/nss_init.h"
-#endif
 #include "chrome/app/scoped_ole_initializer.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
 #include "chrome/common/chrome_constants.h"
@@ -69,12 +55,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/main_function_params.h"
 #include "chrome/common/sandbox_init_wrapper.h"
 #include "ipc/ipc_switches.h"
+
+#if defined(OS_LINUX)
+#include "base/nss_init.h"
+#include "chrome/browser/renderer_host/render_sandbox_host_linux.h"
+#endif
+
+#if defined(OS_MACOSX)
+#include "base/mac_util.h"
+#include "chrome/common/chrome_paths_internal.h"
+#include "chrome/app/breakpad_mac.h"
+#include "third_party/WebKit/WebKit/mac/WebCoreSupport/WebSystemInterface.h"
+#endif
+
+#if defined(OS_POSIX)
+#include "base/global_descriptors_posix.h"
+#endif
+
 #if defined(OS_WIN)
+#include "base/win_util.h"
 #include "sandbox/src/sandbox.h"
 #include "tools/memory_watcher/memory_watcher.h"
-#endif
-#if defined(OS_MACOSX)
-#include "third_party/WebKit/WebKit/mac/WebCoreSupport/WebSystemInterface.h"
 #endif
 
 extern int BrowserMain(const MainFunctionParams&);
@@ -578,6 +579,9 @@ int ChromeMain(int argc, char** argv) {
 #endif
   } else if (process_type.empty()) {
 #if defined(OS_LINUX)
+    // Tickle the sandbox host so it forks now.
+    Singleton<RenderSandboxHostLinux>().get();
+
     // We want to be sure to init NSPR on the main thread.
     base::EnsureNSPRInit();
 
