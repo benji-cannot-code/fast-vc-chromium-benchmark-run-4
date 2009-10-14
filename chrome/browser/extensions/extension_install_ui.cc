9a0b4067944db2fc4bae919bf0e9793ec27314b4
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-14 22:37:43 +0000

    Fix extension canceling; the CFUserNotification functionality gets a bit weird.
    BUG=http://crbug.com/23405
    TEST=try to cancel extension load; it should work.
    
    Review URL: http://codereview.chromium.org/279006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29046 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_ui.cc
+++ chrome/browser/extensions/extension_install_ui.cc
@@ -155,7 +155,7 @@ void ExtensionInstallUI::ConfirmInstall(Delegate* delegate,
       l10n_util::GetString(IDS_EXTENSION_PROMPT_CANCEL_BUTTON)));
 
   CFOptionFlags response;
-  if (kCFUserNotificationAlternateResponse == CFUserNotificationDisplayAlert(
+  CFUserNotificationDisplayAlert(
       0, kCFUserNotificationCautionAlertLevel,
       NULL, // TODO(port): show the install_icon instead of a default.
       NULL, NULL, // Sound URL, localization URL.
@@ -164,7 +164,8 @@ void ExtensionInstallUI::ConfirmInstall(Delegate* delegate,
       NULL, // Default button.
       confirm_cancel,
       NULL, // Other button.
-      &response)) {
+      &response);
+  if (response == kCFUserNotificationAlternateResponse) {
     delegate->AbortInstall();
   } else {
     delegate->ContinueInstall();
