f7ac25dd6925a34c218dd43949837248022a6314
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-14 21:09:09 +0000

    Mac: colorize the "bookmarks go here" text to match the theme.
    BUG=http://crbug.com/23536
    TEST=as in bug
    
    Review URL: http://codereview.chromium.org/278008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29025 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bar_view.mm
+++ chrome/browser/cocoa/bookmark_bar_view.mm
@@ -4,8 +4,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "chrome/browser/cocoa/bookmark_bar_view.h"
+#import "third_party/GTM/AppKit/GTMTheme.h"
+
+@interface BookmarkBarView (Private)
+- (void)themeDidChangeNotification:(NSNotification*)aNotification;
+- (void)updateTheme:(GTMTheme*)theme;
+@end
 
 @implementation BookmarkBarView
+
+- (void)dealloc {
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+  [super dealloc];
+}
+
+- (void)awakeFromNib {
+  NSNotificationCenter* defaultCenter = [NSNotificationCenter defaultCenter];
+  [defaultCenter addObserver:self
+                    selector:@selector(themeDidChangeNotification:)
+                        name:kGTMThemeDidChangeNotification
+                      object:nil];
+}
+
+- (void)viewDidMoveToWindow {
+  if ([self window])
+    [self updateTheme:[self gtm_theme]];
+}
+
+- (void)themeDidChangeNotification:(NSNotification*)aNotification {
+  GTMTheme* theme = [aNotification object];
+  [self updateTheme:theme];
+}
+
+- (void)updateTheme:(GTMTheme*)theme {
+  NSColor* color = [theme textColorForStyle:GTMThemeStyleBookmarksBarButton
+                                      state:GTMThemeStateActiveWindow];
+  [noItemTextfield_ setTextColor:color];
+}
 
 // Mouse down events on the bookmark bar should not allow dragging the parent
 // window around.
