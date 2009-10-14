2257fa1bf04eafe83ebd495957f21af3d844b3be
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-14 14:10:52 +0000

    ImageDecoderQt: Minor tweaks to the decoder
    - Only cache the data when we start to use it.
    - Start with a repetition count of none for normal images.
    - Do not use canRead as this will trigger parsing of the full image
    - Cope with a GIF failing to decode the first frame, do not
      set m_failed to true if decoding the first frame failed
    - Inform the QImageReader about the format that was detected
    - Always create a ImageDecoderQt when when we have more
      than four byte.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49559 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/ImageDecoderQt.h
+++ third_party/WebKit/WebCore/platform/graphics/qt/ImageDecoderQt.h
@@ -41,7 +41,7 @@ namespace WebCore {
 class ImageDecoderQt : public ImageDecoder
 {
 public:
-    ImageDecoderQt(const QByteArray& imageFormat);
+    ImageDecoderQt();
     ~ImageDecoderQt();
 
     virtual void setData(SharedBuffer* data, bool allDataReceived);
@@ -66,7 +66,7 @@ private:
     void failRead();
 
 private:
-    String m_format;
+    QByteArray m_format;
     QBuffer* m_buffer;
     QImageReader* m_reader;
     mutable int m_repetitionCount;
