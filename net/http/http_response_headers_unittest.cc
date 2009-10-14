08a0e98aaba51d409e642eee45af24bbc1858b97
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-14 22:05:54 +0000

    Http Cache: More unit tests for byte range support.
    BUG=b/2071330
    TEST=unittests
    
    Review URL: http://codereview.chromium.org/267101
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29035 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_headers_unittest.cc
+++ net/http/http_response_headers_unittest.cc
@@ -254,7 +254,7 @@ TEST(HttpResponseHeadersTest, NormalizeHeadersOfWhitepace) {
     "HTTP/1.0 200 OK\n",
 
     200,
-    HttpVersion(0,0), // Parse error
+    HttpVersion(0,0),  // Parse error
     HttpVersion(1,0)
   };
   TestCommon(test);
@@ -1158,6 +1158,13 @@ TEST(HttpResponseHeaders, GetContentRange) {
     { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: \t   bytes \t  0    -   50   /   5   1",
       false,
+      0,
+      50,
+      -1
+    },
+    { "HTTP/1.1 206 Partial Content\n"
+      "Content-Range: \t   bytes \t  0    -   5 0   /   51",
+      false,
       -1,
       -1,
       -1
@@ -1165,34 +1172,34 @@ TEST(HttpResponseHeaders, GetContentRange) {
     { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: bytes 50-0/51",
       false,
-      -1,
-      -1,
+      50,
+      0,
       -1
     },
     { "HTTP/1.1 416 Requested range not satisfiable\n"
-      "Content-Range: bytes */*",
-      true,
+      "Content-Range: bytes * /*",
+      false,
       -1,
       -1,
       -1
     },
     { "HTTP/1.1 416 Requested range not satisfiable\n"
       "Content-Range: bytes *   /    *   ",
-      true,
+      false,
       -1,
       -1,
       -1
     },
     { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: bytes 0-50/*",
-      true,
+      false,
       0,
       50,
       -1
     },
     { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: bytes 0-50  /    * ",
-      true,
+      false,
       0,
       50,
       -1
@@ -1207,15 +1214,29 @@ TEST(HttpResponseHeaders, GetContentRange) {
     { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: bytes 0-10000000000/10000000000",
       false,
-      -1,
-      -1,
-      -1
+      0,
+      10000000000ll,
+      10000000000ll
     },
-    // The following header is invalid for response code of 206, this should be
-    // verified by the user.
+    // 64 bits wraparound.
+    { "HTTP/1.1 206 Partial Content\n"
+      "Content-Range: bytes 0 - 9223372036854775807 / 100",
+      false,
+      0,
+      kint64max,
+      100
+    },
+    // 64 bits wraparound.
+    { "HTTP/1.1 206 Partial Content\n"
+      "Content-Range: bytes 0 - 100 / -9223372036854775808",
+      false,
+      0,
+      100,
+      kint64min
+    },
     { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: bytes */50",
-      true,
+      false,
       -1,
       -1,
       50
@@ -1223,16 +1244,16 @@ TEST(HttpResponseHeaders, GetContentRange) {
     { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: bytes 0-50/10",
       false,
-      -1,
-      -1,
-      -1
+      0,
+      50,
+      10
     },
     { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: bytes 0-50/-10",
       false,
-      -1,
-      -1,
-      -1
+      0,
+      50,
+      -10
     },
     { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: bytes 0-0/1",
@@ -1270,15 +1291,8 @@ TEST(HttpResponseHeaders, GetContentRange) {
       -1
     },
     { "HTTP/1.1 206 Partial Content\n"
-      "Content-Range: bytes 0-40000000000000000000/40000000000000000001",
-      false,
-      -1,
-      -1,
-      -1
-    },
-    { "HTTP/1.1 206 Partial Content\n"
       "Content-Range: bytes 0-1233/*",
-      true,
+      false,
       0,
       1233,
       -1
@@ -1304,11 +1318,9 @@ TEST(HttpResponseHeaders, GetContentRange) {
                                                 &last_byte_position,
                                                 &instance_size);
     EXPECT_EQ(tests[i].expected_return_value, return_value);
-    if (return_value) {
-      EXPECT_EQ(tests[i].expected_first_byte_position, first_byte_position);
-      EXPECT_EQ(tests[i].expected_last_byte_position, last_byte_position);
-      EXPECT_EQ(tests[i].expected_instance_size, instance_size);
-    }
+    EXPECT_EQ(tests[i].expected_first_byte_position, first_byte_position);
+    EXPECT_EQ(tests[i].expected_last_byte_position, last_byte_position);
+    EXPECT_EQ(tests[i].expected_instance_size, instance_size);
   }
 }
 
