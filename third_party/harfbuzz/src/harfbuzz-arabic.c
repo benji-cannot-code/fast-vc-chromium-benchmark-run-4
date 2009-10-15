52a31794d62e71eec1cdb94e89ed5b12974c41de
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-15 02:22:46 +0000

    Fix harfbuzz read past end of string on arabic text.
    BUG=23450
    TEST=valgrind_webkit_tests.sh LayoutTests/fast/text/drawBidiText.html
    
    Review URL: http://codereview.chromium.org/271098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29080 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/harfbuzz/src/harfbuzz-arabic.c
+++ third_party/harfbuzz/src/harfbuzz-arabic.c
@@ -1010,7 +1010,7 @@ static HB_Bool arabicSyriacOpenTypeShape(HB_ShaperItem *item, HB_Bool *ot_ok)
         ++l;
         ++properties;
     }
-    if (f + l < item->stringLength) {
+    if (f + l + item->item.pos < item->stringLength) {
         ++l;
     }
     getArabicProperties(uc+f, l, props);
@@ -1087,5 +1087,3 @@ HB_Bool HB_ArabicShape(HB_ShaperItem *item)
     HB_HeuristicPosition(item);
     return TRUE;
 }
-
-
