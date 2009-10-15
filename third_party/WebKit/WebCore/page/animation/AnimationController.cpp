99c30ce972db730ee9de7b3abad8587f37ffea6a
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-15 16:52:51 +0000

    2009-10-15  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dan Bernstein.
    
            Transitions fail to run sometimes
            https://bugs.webkit.org/show_bug.cgi?id=26770
    
            Fix an issue where we could attempt to start accelerated animations or transitions on
            GraphicsLayer that were not rooted (because of visibility:hidden), which would leave
            the AnimationController's m_waitingForResponse flag in a state that killed subsequent
            software transitions.
    
            * page/animation/AnimationController.cpp:
            (WebCore::AnimationControllerPrivate::AnimationControllerPrivate):
            (WebCore::AnimationControllerPrivate::endAnimationUpdate):
            (WebCore::AnimationControllerPrivate::receivedStartTimeResponse):
            (WebCore::AnimationControllerPrivate::addToStartTimeResponseWaitList):
            (WebCore::AnimationControllerPrivate::startTimeResponse):
            * page/animation/AnimationControllerPrivate.h:
            Make some methods non-inline for ease of debugging (these are not hot methods).
            Rename m_waitingForAResponse to m_waitingForResponse.
    
            * platform/graphics/GraphicsLayer.h:
            * platform/graphics/GraphicsLayer.cpp:
            (WebCore::GraphicsLayer::hasAncestor):
            New method to report whether the receiver has the given layer as an ancestor. Used for checking
            whether a layer is rooted.
    
            * rendering/RenderLayerBacking.cpp:
            (WebCore::RenderLayerBacking::startAnimation):
            (WebCore::RenderLayerBacking::startTransition):
            Don't try to start accelerated animations or transitions on non-rooted GraphicsLayers.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/AnimationController.cpp
+++ third_party/WebKit/WebCore/page/animation/AnimationController.cpp
@@ -56,7 +56,7 @@ AnimationControllerPrivate::AnimationControllerPrivate(Frame* frame)
     , m_lastStyleAvailableWaiter(0)
     , m_responseWaiters(0)
     , m_lastResponseWaiter(0)
-    , m_waitingForAResponse(false)
+    , m_waitingForResponse(false)
 {
 }
 
@@ -280,6 +280,19 @@ double AnimationControllerPrivate::beginAnimationUpdateTime()
     return m_beginAnimationUpdateTime;
 }
 
+void AnimationControllerPrivate::endAnimationUpdate()
+{
+    styleAvailable();
+    if (!m_waitingForResponse)
+        startTimeResponse(beginAnimationUpdateTime());
+}
+
+void AnimationControllerPrivate::receivedStartTimeResponse(double time)
+{
+    m_waitingForResponse = false;
+    startTimeResponse(time);
+}
+
 PassRefPtr<RenderStyle> AnimationControllerPrivate::getAnimatedStyleForRenderer(RenderObject* renderer)
 {
     if (!renderer)
@@ -379,7 +392,7 @@ void AnimationControllerPrivate::addToStartTimeResponseWaitList(AnimationBase* a
     ASSERT(!animation->next());
     
     if (willGetResponse)
-        m_waitingForAResponse = true;
+        m_waitingForResponse = true;
     
     if (m_responseWaiters)
         m_lastResponseWaiter->setNext(animation);
@@ -409,13 +422,13 @@ void AnimationControllerPrivate::removeFromStartTimeResponseWaitList(AnimationBa
     }
 }
 
-void AnimationControllerPrivate::startTimeResponse(double t)
+void AnimationControllerPrivate::startTimeResponse(double time)
 {
     // Go through list of waiters and send them on their way
     for (AnimationBase* animation = m_responseWaiters; animation; ) {
         AnimationBase* nextAnimation = animation->next();
         animation->setNext(0);
-        animation->onAnimationStartResponse(t);
+        animation->onAnimationStartResponse(time);
         animation = nextAnimation;
     }
     
