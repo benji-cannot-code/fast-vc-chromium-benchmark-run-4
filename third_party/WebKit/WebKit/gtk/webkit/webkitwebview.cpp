ae996ff4371a986884820be5f2fa1f3390f7001c
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-15 18:42:11 +0000

    WebKit/gtk
    2009-10-15  Zan Dobersek  <zandobersek@gmail.com>
    
            Reviewed by Gustavo Noronha.
    
            Add a settings to enable support for experimental Web Sockets.
            Currently this comes in handy in layout tests.
    
            * webkit/webkitwebsettings.cpp:
            (webkit_web_settings_class_init):
            (webkit_web_settings_set_property):
            (webkit_web_settings_get_property):
            (webkit_web_settings_copy):
            * webkit/webkitwebview.cpp:
            (webkit_web_view_update_settings):
            (webkit_web_view_settings_notify):
    
    WebKitTools
    
    2009-10-15  Zan Dobersek  <zandobersek@gmail.com>
    
            Reviewed by Gustavo Noronha.
    
            Enable Web Sockets support when running layout tests.
    
            * DumpRenderTree/gtk/DumpRenderTree.cpp:
            (resetDefaultsToConsistentValues):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
@@ -2393,7 +2393,7 @@ static void webkit_web_view_update_settings(WebKitWebView* webView)
         enableScripts, enablePlugins, enableDeveloperExtras, resizableTextAreas,
         enablePrivateBrowsing, enableCaretBrowsing, enableHTML5Database, enableHTML5LocalStorage,
         enableXSSAuditor, javascriptCanOpenWindows, enableOfflineWebAppCache,
-        enableUniversalAccessFromFileURI;
+        enableUniversalAccessFromFileURI, enableWebSockets;
 
     WebKitEditingBehavior editingBehavior;
 
@@ -2422,6 +2422,7 @@ static void webkit_web_view_update_settings(WebKitWebView* webView)
                  "enable-offline-web-application-cache", &enableOfflineWebAppCache,
                  "editing-behavior", &editingBehavior,
                  "enable-universal-access-from-file-uris", &enableUniversalAccessFromFileURI,
+                 "enable-web-sockets", &enableWebSockets,
                  NULL);
 
     settings->setDefaultTextEncodingName(defaultEncoding);
@@ -2448,6 +2449,7 @@ static void webkit_web_view_update_settings(WebKitWebView* webView)
     settings->setOfflineWebApplicationCacheEnabled(enableOfflineWebAppCache);
     settings->setEditingBehavior(core(editingBehavior));
     settings->setAllowUniversalAccessFromFileURLs(enableUniversalAccessFromFileURI);
+    settings->setExperimentalWebSocketsEnabled(enableWebSockets);
 
     g_free(defaultEncoding);
     g_free(cursiveFontFamily);
@@ -2534,6 +2536,8 @@ static void webkit_web_view_settings_notify(WebKitWebSettings* webSettings, GPar
         settings->setEditingBehavior(core(static_cast<WebKitEditingBehavior>(g_value_get_enum(&value))));
     else if (name == g_intern_string("enable-universal-access-from-file-uris"))
         settings->setAllowUniversalAccessFromFileURLs(g_value_get_boolean(&value));
+    else if (name == g_intern_string("enable-web-sockets"))
+        settings->setExperimentalWebSocketsEnabled(g_value_get_boolean(&value));
     else if (!g_object_class_find_property(G_OBJECT_GET_CLASS(webSettings), name))
         g_warning("Unexpected setting '%s'", name);
     g_value_unset(&value);
