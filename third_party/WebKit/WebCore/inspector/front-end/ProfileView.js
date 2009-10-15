99915b20315bf95418a86c61ef985b4ca396d596
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-15 16:50:23 +0000

    2009-10-15  Alexander Pavlov  <apavlov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Refactor ProfilesPanel to support multiple profile types
    
            Data describing different profile types are now stored in distinct objects.
            https://bugs.webkit.org/show_bug.cgi?id=30332
    
            * WebCore.vcproj/WebCore.vcproj:
            * inspector/front-end/ProfileView.js:
            (WebInspector.CPUProfileView.profileCallback):
            (WebInspector.CPUProfileView):
            (WebInspector.CPUProfileView.prototype._sortData):
            (WebInspector.CPUProfileType):
            (WebInspector.CPUProfileType.prototype.get buttonTooltip):
            (WebInspector.CPUProfileType.prototype.get buttonStyle):
            (WebInspector.CPUProfileType.prototype.buttonClicked):
            (WebInspector.CPUProfileType.prototype.setRecordingProfile):
            (WebInspector.CPUProfile):
            (WebInspector.CPUProfile.prototype.get title):
            (WebInspector.CPUProfile.prototype.get uid):
            (WebInspector.CPUProfile.prototype.get head):
            (WebInspector.CPUProfile.prototype.createView):
            (WebInspector.CPUProfile.prototype.viewForProfile):
            * inspector/front-end/ProfilesPanel.js:
            (WebInspector.ProfileType):
            (WebInspector.ProfileType.prototype.get buttonTooltip):
            (WebInspector.ProfileType.prototype.get buttonStyle):
            (WebInspector.ProfileType.prototype.get buttonCaption):
            (WebInspector.ProfileType.prototype.get id):
            (WebInspector.ProfileType.prototype.get name):
            (WebInspector.ProfileType.prototype.buttonClicked):
            (WebInspector.ProfilesPanel):
            (WebInspector.ProfilesPanel.prototype.get statusBarItems.clickHandler):
            (WebInspector.ProfilesPanel.prototype.get statusBarItems):
            (WebInspector.ProfilesPanel.prototype.reset):
            (WebInspector.ProfilesPanel.prototype.registerProfileType):
            (WebInspector.ProfilesPanel.prototype._makeKey):
            (WebInspector.ProfilesPanel.prototype.addProfileHeader):
            (WebInspector.ProfilesPanel.prototype.showProfile):
            (WebInspector.ProfilesPanel.prototype.getProfileType):
            (WebInspector.ProfilesPanel.prototype.showProfileForURL):
            (WebInspector.ProfilesPanel.prototype.updateProfileTypeButtons):
            (WebInspector.ProfilesPanel.prototype.displayTitleForProfileLink):
            (WebInspector.ProfilesPanel.prototype.get searchableViews):
            (WebInspector.ProfilesPanel.prototype._updateInterface):
            * inspector/front-end/inspector.js:
            (WebInspector._createPanels):
            (WebInspector.documentClick.followLink):
            (WebInspector.documentClick):
            (WebInspector.addProfileHeader):
            (WebInspector.setRecordingProfile):
            (WebInspector.linkifyStringAsFragment):
            (WebInspector.showProfileForURL):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49632 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ProfileView.js
+++ third_party/WebKit/WebCore/inspector/front-end/ProfileView.js
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-WebInspector.ProfileView = function(profile)
+// FIXME: Rename the file.
+
+WebInspector.CPUProfileView = function(profile)
 {
     WebInspector.View.call(this);
 
@@ -82,7 +84,7 @@ WebInspector.ProfileView = function(profile)
     var self = this;
     function profileCallback(profile)
     {
-        self.profile = profile;
+        self.profile.representedObject = profile;
         self._assignParentsInProfile();
       
         self.profileDataGridTree = self.bottomUpProfileDataGridTree;
@@ -97,7 +99,7 @@ WebInspector.ProfileView = function(profile)
     InspectorController.getProfile(callId, this.profile.uid);
 }
 
-WebInspector.ProfileView.prototype = {
+WebInspector.CPUProfileView.prototype = {
     get statusBarItems()
     {
         return [this.viewSelectElement, this.percentButton.element, this.focusButton.element, this.excludeButton.element, this.resetButton.element];
@@ -169,7 +171,7 @@ WebInspector.ProfileView.prototype = {
         WebInspector.View.prototype.hide.call(this);
         this._currentSearchResultIndex = -1;
     },
-    
+
     resize: function()
     {
         if (this.dataGrid)
@@ -504,7 +506,7 @@ WebInspector.ProfileView.prototype = {
 
     _sortData: function(event)
     {
-        this._sortProfile(this.profile);
+        this._sortProfile(this.profile.representedObject);
     },
 
     _sortProfile: function()
@@ -567,4 +569,77 @@ WebInspector.ProfileView.prototype = {
     }
 }
 
-WebInspector.ProfileView.prototype.__proto__ = WebInspector.View.prototype;
+WebInspector.CPUProfileView.prototype.__proto__ = WebInspector.View.prototype;
+
+WebInspector.CPUProfileType = function()
+{
+    WebInspector.ProfileType.call(this, WebInspector.CPUProfileType.TypeId, WebInspector.UIString("CPU PROFILES"));
+    this._recording = false;
+}
+
+WebInspector.CPUProfileType.TypeId = "CPU";
+
+WebInspector.CPUProfileType.prototype = {
+    get buttonTooltip()
+    {
+        return this._recording ? WebInspector.UIString("Stop profiling.") : WebInspector.UIString("Start profiling.");
+    },
+
+    get buttonStyle()
+    {
+        return this._recording ? "record-profile-status-bar-item status-bar-item toggled-on" : "record-profile-status-bar-item status-bar-item";
+    },
+
+    buttonClicked: function()
+    {
+        this._recording = !this._recording;
+
+        if (this._recording)
+            InspectorController.startProfiling();
+        else
+            InspectorController.stopProfiling();
+    },
+
+    setRecordingProfile: function(isProfiling)
+    {
+        this._recording = isProfiling;
+    }
+}
+
+WebInspector.CPUProfileType.prototype.__proto__ = WebInspector.ProfileType.prototype;
+
+WebInspector.CPUProfile = function(profile)
+{
+    this.representedObject = profile;
+    this.typeId = WebInspector.CPUProfileType.TypeId;
+}
+
+WebInspector.CPUProfile.prototype = {
+    get title()
+    {
+        return this.representedObject.title;
+    },
+
+    get uid()
+    {
+        return this.representedObject.uid;
+    },
+
+    get head()
+    {
+        return this.representedObject.head;
+    },
+
+    createView: function()
+    {
+        return new WebInspector.CPUProfileView(this);
+    },
+
+    // FIXME: Extract this into a superclass so that createView can be simply overridden by subclasses.
+    viewForProfile: function()
+    {
+        if (!this._profileView)
+            this._profileView = this.createView();
+        return this._profileView;
+    }
+}
