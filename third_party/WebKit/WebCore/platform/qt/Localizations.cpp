d107e49b482c223ac139aae679c9d3df353edda0
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-16 12:47:17 +0000

    2009-10-16  Shu Chang  <Chang.Shu@nokia.com>
            Reviewed by Eric Seidel.
    
            Added test case for testing navigator.language.
            https://bugs.webkit.org/show_bug.cgi?id=29653
    
            Different browsers may not return the exact same result.
            Some examples here:
            Safari 4 WinXP: "en-US"
            Safari 4 Mac: "en-us"
            Firefox 3 Mac/WinXP: "en-US"
            Opera 9.64: "en"
            Epiphany 2 GTK: "en"
            Chrome 3 WinXP: "en-US"
    
            The test case checks if the first two letters are "en"
            when system locale is set to "en_US.iso88591".
    
            * fast/js/navigator-language-expected.txt: Added.
            * fast/js/navigator-language.html: Added.
    2009-10-16  Shu Chang  <Chang.Shu@nokia.com>
    
            Reviewed by Eric Seidel.
    
            Default language translation should be determined by locale settings,
            e.g., "en-US".
            https://bugs.webkit.org/show_bug.cgi?id=29653
    
            Test: fast/js/navigator-language.html
    
            * platform/qt/Localizations.cpp:
            (WebCore::defaultLanguage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49675 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/Localizations.cpp
+++ third_party/WebKit/WebCore/platform/qt/Localizations.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PlatformString.h"
 
 #include <QCoreApplication>
+#include <QLocale>
 
 namespace WebCore {
 
@@ -54,7 +55,8 @@ String resetButtonDefaultLabel()
 
 String defaultLanguage()
 {
-    return "en";
+    QLocale locale;
+    return locale.name().replace("_", "-");
 }
 
 String searchableIndexIntroduction()
