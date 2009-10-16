74e59fe4d3f0eaca09901100d7344706b7bbb2a9
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-16 00:53:06 +0000

    Let cmd-` switch windows again.
    BUG=24817
    TEST=Open three windows. Focus web contents. Cmd-` should cycle windows Cmd-shift-` should cycle in the other direction.
    
    Review URL: http://codereview.chromium.org/280005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29229 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents_view_mac.mm
+++ chrome/browser/tab_contents/tab_contents_view_mac.mm
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import <Carbon/Carbon.h>
+
 #include "chrome/browser/tab_contents/tab_contents_view_mac.h"
 
 #include <string>
@@ -260,6 +262,10 @@ void TabContentsViewMac::Observe(NotificationType type,
   }
 }
 
+@interface NSApplication(SPI)
+- (void)_cycleWindowsReversed:(BOOL)reversed;
+@end
+
 @implementation TabContentsViewCocoa
 
 - (id)initWithTabContentsViewMac:(TabContentsViewMac*)w {
@@ -302,6 +308,25 @@ void TabContentsViewMac::Observe(NotificationType type,
     // We need to dispatch this to the menu.
     if ([[NSApp mainMenu] performKeyEquivalent:event])
       return;
+  }
+
+  // Cmd-` is not in the menu and it's apparently handled by |NSApp sendEvent|
+  // if the application doesn't swallow it. We do, so we need to handle this
+  // key ourself. On foreign keyboards, the "switch windows" key is not the
+  // ` key, so do this by keycode instead of |event characters|.
+  if ([event type] == NSKeyDown &&
+      [event keyCode] == kVK_ANSI_Grave &&
+      [NSApp respondsToSelector:@selector(_cycleWindowsReversed:)]) {
+    const NSUInteger kModifierMask = NSShiftKeyMask |
+                                     NSControlKeyMask |
+                                     NSAlternateKeyMask |
+                                     NSCommandKeyMask;
+    if (([event modifierFlags] & kModifierMask) == NSCommandKeyMask)
+      [NSApp _cycleWindowsReversed:NO];
+    else if (([event modifierFlags] & kModifierMask) ==
+        (NSCommandKeyMask | NSShiftKeyMask) &&
+        [NSApp respondsToSelector:@selector(_cycleWindowsReversed:)])
+      [NSApp _cycleWindowsReversed:YES];
   }
 
   // If this tab is no longer active, it's window will be |nil|. In that case,
