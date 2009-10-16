8eb0b1ab2996720777d6b554ce1fcb8aa79f9636
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-16 19:37:29 +0000

    Make Mac SelectFileDialogImpl load SaveAccessoryView.nib from the proper bundle.
    BUG=24923
    TEST=File:Save Page As… should work and offer a Format pop-up menu
    Review URL: http://codereview.chromium.org/285003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29311 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/shell_dialogs_mac.mm
+++ chrome/browser/cocoa/shell_dialogs_mac.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "base/logging.h"
+#include "base/mac_util.h"
 #include "base/scoped_cftyperef.h"
 #import "base/scoped_nsobject.h"
 #include "base/sys_string_conversions.h"
@@ -240,7 +241,8 @@ NSView* SelectFileDialogImpl::GetAccessoryView(const FileTypeInfo* file_types,
                                                int file_type_index) {
   DCHECK(file_types);
   scoped_nsobject<NSNib> nib (
-      [[NSNib alloc] initWithNibNamed:@"SaveAccessoryView" bundle:nil]);
+      [[NSNib alloc] initWithNibNamed:@"SaveAccessoryView"
+                               bundle:mac_util::MainAppBundle()]);
   if (!nib)
     return nil;
 
