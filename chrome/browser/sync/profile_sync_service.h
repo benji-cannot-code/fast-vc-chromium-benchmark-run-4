0356c7a75c5646a7de3dc3de503c4262a0d889ac
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-16 06:17:54 +0000

    Reverted 29253.
    TBR: nick@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29254 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.h
+++ chrome/browser/sync/profile_sync_service.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/glue/change_processor.h"
 #include "chrome/browser/sync/glue/model_associator.h"
 #include "chrome/browser/sync/glue/sync_backend_host.h"
-#include "chrome/browser/sync/sync_setup_wizard.h"
+#include "chrome/browser/views/sync/sync_setup_wizard.h"
 #include "chrome/common/notification_registrar.h"
 #include "googleurl/src/gurl.h"
 
