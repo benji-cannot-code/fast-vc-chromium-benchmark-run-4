7ad14ff0685c23dcbf4cc95d42ce5c76e0eca115
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-16 04:56:29 +0000

    Attempt to fix the build by adding an include.
    TBR=darin
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/284005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29247 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/src/WebKit.cpp
+++ webkit/api/src/WebKit.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DOMTimer.h"
 #include "FrameLoader.h"
 #include "Page.h"
+#include "SecurityOrigin.h"
 #include "TextEncoding.h"
 #include "WorkerContextExecutionProxy.h"
 #include <wtf/Assertions.h>
