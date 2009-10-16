15a300392590edba0f8002446f825f86c3b4577c
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-16 19:35:59 +0000

    Bookmark bubble tweaks TitleCase the other folder menu options. Use the helper that handles ellipsis.
    TEST=the other folder option is title case
    BUG=24912
    Review URL: http://codereview.chromium.org/276065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29309 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bubble_controller.mm
+++ chrome/browser/cocoa/bookmark_bubble_controller.mm
@@ -157,7 +157,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Add "Choose another folder...".  Remember it for later to compare against.
   chooseAnotherFolder_.reset(
-    [l10n_util::GetNSString(IDS_BOOMARK_BUBBLE_CHOOSER_ANOTHER_FOLDER)
+    [l10n_util::GetNSStringWithFixup(IDS_BOOMARK_BUBBLE_CHOOSER_ANOTHER_FOLDER)
               retain]);
   [folderComboBox_ addItemWithObjectValue:chooseAnotherFolder_.get()];
 
