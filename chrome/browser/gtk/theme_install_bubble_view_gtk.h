bae2130650f9eb014d0c906a0c6a0cc68e372358
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-17 00:28:55 +0000

    Linux: Make theme install bubble prettier (for compositing WMs)
    BUG=none
    TEST=screenshot
    
    Review URL: http://codereview.chromium.org/289004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29362 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/theme_install_bubble_view_gtk.h
+++ chrome/browser/gtk/theme_install_bubble_view_gtk.h
@@ -34,15 +34,6 @@ class ThemeInstallBubbleViewGtk : public NotificationObserver {
   // Reposition |widget_| to be centered over |parent_|.
   void MoveWindow();
 
-  // Move with the parent window.
-  static gboolean HandleParentConfigureThunk(GtkWidget* widget,
-                                             GdkEventConfigure* event,
-                                             gpointer user_data) {
-    return reinterpret_cast<ThemeInstallBubbleViewGtk*>(user_data)->
-        HandleParentConfigure(event);
-  }
-  gboolean HandleParentConfigure(GdkEventConfigure* event);
-
   // Our parent is going down; self destruct.
   static gboolean HandleParentUnmapThunk(GtkWidget* widget,
                                          GdkEvent* event,
@@ -51,6 +42,16 @@ class ThemeInstallBubbleViewGtk : public NotificationObserver {
         HandleParentUnmap();
   }
   gboolean HandleParentUnmap();
+
+  // Draw the background. This is only signalled if we are using a compositing
+  // window manager, otherwise we just use ActAsRoundedWindow().
+  static gboolean HandleExposeEventThunk(GtkWidget* widget,
+                                         GdkEventExpose* event,
+                                         gpointer user_data) {
+    return reinterpret_cast<ThemeInstallBubbleViewGtk*>(user_data)->
+        HandleExposeEvent(event);
+  }
+  gboolean HandleExposeEvent(GdkEventExpose* event);
 
   GtkWidget* widget_;
 
