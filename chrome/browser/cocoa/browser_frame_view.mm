e0b9b4bfe0fd31906d0a31d1618c1177bc28ddfc
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-19 00:22:15 +0000

    Clean up use of scoped_nsobject<NSAutoreleasePool>. I have a CL coming later that will protect us against these in the future.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/283026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29382 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_frame_view.mm
+++ chrome/browser/cocoa/browser_frame_view.mm
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Carbon/Carbon.h>
 
 #include "base/logging.h"
-#include "base/scoped_nsobject.h"
+#include "base/scoped_nsautorelease_pool.h"
 #import "chrome/browser/cocoa/chrome_browser_window.h"
 #import "third_party/GTM/AppKit/GTMTheme.h"
 
@@ -34,7 +34,7 @@ static const NSInteger kBrowserFrameViewPatternPhaseOffset = 2;
   // others. If they all fail, we will lose window frame theming and
   // roll overs for our close widgets, but things should still function
   // correctly.
-  scoped_nsobject<NSAutoreleasePool> pool([[NSAutoreleasePool alloc] init]);
+  base::ScopedNSAutoreleasePool pool;
   Class grayFrameClass = NSClassFromString(@"NSGrayFrame");
   DCHECK(grayFrameClass);
   if (!grayFrameClass) return;
