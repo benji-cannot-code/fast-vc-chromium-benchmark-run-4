393f698e6950fc964daca0a9d5256806669fc248
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-19 06:51:24 +0000

    2009-10-18  Kenneth Rohde Christiansen  <kenneth@webkit.org>
            Reviewed by Simon Hausmann.
    
            Rename fixedContentsSize property to preferredContentsSize as
            agreed upon with Siman Hausmann and Matthias Ettrich.
    
            * Api/qwebpage.cpp:
            (QWebPage::preferredContentsSize):
            (QWebPage::setPreferredContentsSize):
            * Api/qwebpage.h:
            * WebCoreSupport/FrameLoaderClientQt.cpp:
            (WebCore::FrameLoaderClientQt::transitionToCommittedForNewPage):
    2009-10-18  Kenneth Rohde Christiansen  <kenneth@webkit.org>
    
            Reviewed by Simon Hausmann.
    
            Use the setPreferredContentsSize method instead
            of setFixedContentsSize, as the method has been renamed.
    
            * DumpRenderTree/qt/DumpRenderTree.cpp:
            (WebCore::DumpRenderTree::open):
            * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
            (LayoutTestController::setFixedContentsSize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49766 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/qt/DumpRenderTree.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/qt/DumpRenderTree.cpp
@@ -317,7 +317,7 @@ void DumpRenderTree::open(const QUrl& aurl)
     int width = isW3CTest ? 480 : maxViewWidth;
     int height = isW3CTest ? 360 : maxViewHeight;
     m_page->view()->resize(QSize(width, height));
-    m_page->setFixedContentsSize(QSize());
+    m_page->setPreferredContentsSize(QSize());
     m_page->setViewportSize(QSize(width, height));
 
     QFocusEvent ev(QEvent::FocusIn);
