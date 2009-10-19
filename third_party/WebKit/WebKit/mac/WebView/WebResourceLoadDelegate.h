222c5562932c863bacdf336945851fec87871673
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-19 21:08:24 +0000

    Documentation fix for -[NSObject(WebResourceLoadDelegate) webView:resource:willSendRequest:redirectResponse:fromDataSource:]
    Reviewed by Dan Bernstein.
    
    * WebView/WebResourceLoadDelegate.h: Prepended "webView:" to the
    @method declaration of
    -webView:resource:willSendRequest:redirectResponse:fromDataSource:
    which was absent before r3953 and was missed during the great
    documentation clean up of r4269.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49808 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebResourceLoadDelegate.h
+++ third_party/WebKit/WebKit/mac/WebView/WebResourceLoadDelegate.h
@@ -68,7 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (id)webView:(WebView *)sender identifierForInitialRequest:(NSURLRequest *)request fromDataSource:(WebDataSource *)dataSource;
 
 /*!
-    @method resource:willSendRequest:redirectResponse:fromDataSource:
+    @method webView:resource:willSendRequest:redirectResponse:fromDataSource:
     @discussion This message is sent before a load is initiated.  The request may be modified
     as necessary by the receiver.
     @param webView The WebView sending the message.
