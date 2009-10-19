7dc6b743adadc4860f11de2dc1e652f87be85ba9
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-19 10:44:43 +0000

    2009-10-19  Girish Ramakrishnan  <girish@forwardbias.in>
            Reviewed by Holger Freyther.
    
            [Qt] Windowed Plugins: Fix crash when QWebPage is deleted after QWebView.
    
            Fixes various sources of crashes:
            1. The PluginContainer is a child of QWebView. When the view gets deleted,
            the PluginView is not notified about the deletion of PluginContainer.
            2. QWebView destructor does not set client to 0.
            3. Sometimes pending paint events are sent after the plugin has died, so add
            a check in PluginView::setNPWindowIfNeeded.
    
            https://bugs.webkit.org/show_bug.cgi?id=30354
    
            * plugins/qt/PluginContainerQt.cpp:
            (PluginContainerQt::~PluginContainerQt):
            * plugins/qt/PluginViewQt.cpp:
            (WebCore::PluginView::setNPWindowIfNeeded):
    2009-10-19  Girish Ramakrishnan  <girish@forwardbias.in>
    
            Reviewed by Holger Freyther.
    
            [Qt] Windowed Plugins: Fix crash when QWebPage is deleted after QWebView.
    
            Fixes various sources of crashes:
            1. The PluginContainer is a child of QWebView. When the view gets deleted,
            the PluginView is not notified about the deletion of PluginContainer.
            2. QWebView destructor does not set client to 0.
            3. Sometimes pending paint events are sent after the plugin has died, so add
            a check in PluginView::setNPWindowIfNeeded.
    
            https://bugs.webkit.org/show_bug.cgi?id=30354
    
            * Api/qwebview.cpp:
            (QWebView::~QWebView):
            * tests/qwebview/qwebview.pro:
            * tests/qwebview/tst_qwebview.cpp:
            (tst_QWebView::reusePage_data):
            (tst_QWebView::reusePage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49770 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/tests/qwebview/tst_qwebview.cpp
+++ third_party/WebKit/WebKit/qt/tests/qwebview/tst_qwebview.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
     Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies)
     Copyright (C) 2009 Torch Mobile Inc.
+    Copyright (C) 2009 Girish Ramakrishnan <girish@forwardbias.in>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qnetworkrequest.h>
 #include <qdiriterator.h>
 #include <qwebkitversion.h>
+#include <qwebframe.h>
 
 class tst_QWebView : public QObject
 {
@@ -43,6 +45,9 @@ private slots:
     void guessUrlFromString_data();
     void guessUrlFromString();
     void getWebKitVersion();
+
+    void reusePage_data();
+    void reusePage();
 };
 
 // This will be called before the first test function is executed.
@@ -166,6 +171,46 @@ void tst_QWebView::guessUrlFromString()
 void tst_QWebView::getWebKitVersion()
 {
     QVERIFY(qWebKitVersion().toDouble() > 0);
+}
+
+void tst_QWebView::reusePage_data()
+{
+    QTest::addColumn<QString>("html");
+    QTest::newRow("WithoutPlugin") << "<html><body id='b'>text</body></html>";
+    QTest::newRow("WindowedPlugin") << QString("<html><body id='b'>text<embed src='resources/test.swf'></embed></body></html>");
+    QTest::newRow("WindowlessPlugin") << QString("<html><body id='b'>text<embed src='resources/test.swf' wmode=\"transparent\"></embed></body></html>");
+}
+
+void tst_QWebView::reusePage()
+{
+    QDir::setCurrent(SRCDIR);
+
+    QFETCH(QString, html);
+    QWebView* view1 = new QWebView;
+    QPointer<QWebPage> page = new QWebPage;
+    view1->setPage(page);
+    page->settings()->setAttribute(QWebSettings::PluginsEnabled, true);
+    QWebFrame* mainFrame = page->mainFrame();
+    mainFrame->setHtml(html, QUrl::fromLocalFile(QDir::currentPath()));
+    if (html.contains("</embed>")) {
+        // some reasonable time for the PluginStream to feed test.swf to flash and start painting
+        QTest::qWait(2000);
+    }
+
+    view1->show();
+    QTest::qWait(2000);
+    delete view1;
+    QVERIFY(page != 0); // deleting view must not have deleted the page, since it's not a child of view
+
+    QWebView *view2 = new QWebView;
+    view2->setPage(page);
+    view2->show(); // in Windowless mode, you should still be able to see the plugin here
+    QTest::qWait(2000);
+    delete view2;
+
+    delete page; // must not crash
+
+    QDir::setCurrent(QApplication::applicationDirPath());
 }
 
 QTEST_MAIN(tst_QWebView)
