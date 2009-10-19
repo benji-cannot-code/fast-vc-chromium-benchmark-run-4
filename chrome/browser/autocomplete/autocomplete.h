d4eae835c16ed99f7c41404e8074c2a3c2633ce7
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-19 18:42:36 +0000

    Allow the history URL provider to handle input of type QUERY.  This helps in the case where the user types something that on its own isn't navigable, but might be the prefix of something else navigable.
    While there are no tests for this directly, another change of mine to treat more inputs with explicit schemes as queries (e.g. "http:/") relies on this, and does unittest for it.
    
    In order to fit the new relevance scores into the table, I went through and simplified the relevance scoring so that generally providers used the same scores for more input types.  The effects of this should be barely noticeable (it affects the ranking of past search queries that are old against results from the secondary search provider), and it simplifies the code noticeably.
    
    This also fixes a "bug" that the NavSuggest results were incremented backwards, but since we only score one of these right now there's no visible effect.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/291005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29428 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete.h
+++ chrome/browser/autocomplete/autocomplete.h
@@ -64,8 +64,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // --------------------------------------------------------------------|-----
 // Keyword (non-substituting or in keyword UI mode, exact match)       | 1500
 // HistoryURL (exact or inline autocomplete match)                     | 1400
-// HistoryURL (what you typed)                                         | 1300
-// Search Primary Provider (what you typed)                            | 1200
+// HistoryURL (what you typed)                                         | 1200
+// Search Primary Provider (what you typed)                            | 1150
 // Keyword (substituting, exact match)                                 | 1100
 // Search Primary Provider (past query in history)                     | 1050--
 // HistoryContents (any match in title of starred page)                | 1000++
@@ -101,16 +101,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // QUERY input type:
 // --------------------------------------------------------------------|-----
 // Keyword (non-substituting or in keyword UI mode, exact match)       | 1500
-// Keyword (substituting, exact match)                                 | 1400
+// Keyword (substituting, exact match)                                 | 1450
+// HistoryURL (exact or inline autocomplete match)                     | 1400
 // Search Primary Provider (what you typed)                            | 1300
-// Search Primary Provider (past query in history)                     | 1250--
-// HistoryContents (any match in title of starred page)                | 1200++
-// Search Primary Provider (navigational suggestion)                   | 1000++
-// HistoryContents (any match in title of nonstarred page)             |  900++
-// Search Primary Provider (suggestion)                                |  800++
-// HistoryContents (any match in body of starred page)                 |  750++
-// HistoryContents (any match in body of nonstarred page)              |  700++
-// Keyword (inexact match)                                             |  650
+// Search Primary Provider (past query in history)                     | 1050--
+// HistoryContents (any match in title of starred page)                | 1000++
+// HistoryURL (inexact match)                                          |  900++
+// Search Primary Provider (navigational suggestion)                   |  800++
+// HistoryContents (any match in title of nonstarred page)             |  700++
+// Search Primary Provider (suggestion)                                |  600++
+// HistoryContents (any match in body of starred page)                 |  550++
+// HistoryContents (any match in body of nonstarred page)              |  500++
+// Keyword (inexact match)                                             |  450
 // Search Secondary Provider (what you typed)                          |  250
 // Search Secondary Provider (past query in history)                   |  200--
 // Search Secondary Provider (navigational suggestion)                 |  150++
@@ -118,14 +120,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // FORCED_QUERY input type:
 // --------------------------------------------------------------------|-----
-// Search Primary Provider (what you typed)                            | 1500
-// Search Primary Provider (past query in history)                     | 1250--
-// HistoryContents (any match in title of starred page)                | 1200++
-// Search Primary Provider (navigational suggestion)                   | 1000++
-// HistoryContents (any match in title of nonstarred page)             |  900++
-// Search Primary Provider (suggestion)                                |  800++
-// HistoryContents (any match in body of starred page)                 |  750++
-// HistoryContents (any match in body of nonstarred page)              |  700++
+// Search Primary Provider (what you typed)                            | 1300
+// Search Primary Provider (past query in history)                     | 1050--
+// HistoryContents (any match in title of starred page)                | 1000++
+// Search Primary Provider (navigational suggestion)                   |  800++
+// HistoryContents (any match in title of nonstarred page)             |  700++
+// Search Primary Provider (suggestion)                                |  600++
+// HistoryContents (any match in body of starred page)                 |  550++
+// HistoryContents (any match in body of nonstarred page)              |  500++
 //
 // (A search keyword is a keyword with a replacement string; a bookmark keyword
 // is a keyword with no replacement string, that is, a shortcut for a URL.)
@@ -158,6 +160,10 @@ typedef std::vector<AutocompleteProvider*> ACProviders;
 // The user input for an autocomplete query.  Allows copying.
 class AutocompleteInput {
  public:
+  // Note that the type below may be misleading.  For example, "http:/" alone
+  // cannot be opened as a URL, so it is marked as a QUERY; yet the user
+  // probably intends to type more and have it eventually become a URL, so we
+  // need to make sure we still run it through inline autocomplete.
   enum Type {
     INVALID,        // Empty input
     UNKNOWN,        // Valid input whose type cannot be determined
