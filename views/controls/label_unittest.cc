cd5346110a5ac0db9d50986ab96e3fae429820cc
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-19 20:55:24 +0000

    Change expectations for linux views multiline label tests to expect NO_ELLIPSIS.
    Review URL: http://codereview.chromium.org/294008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29454 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/label_unittest.cc
+++ views/controls/label_unittest.cc
@@ -407,6 +407,9 @@ TEST(LabelTest, DrawSingleLineString) {
   EXPECT_EQ(0, flags);
 }
 
+// On Linux the underlying pango routines require a max height in order to
+// ellide multiline text. So until that can be resolved, we set all
+// multiline lables to not ellide in Linux only.
 TEST(LabelTest, DrawMultiLineString) {
   Label label;
   label.SetFocusable(false);
@@ -430,7 +433,15 @@ TEST(LabelTest, DrawMultiLineString) {
   EXPECT_EQ(0, text_bounds.y());
   EXPECT_GT(text_bounds.width(), kMinTextDimension);
   EXPECT_GT(text_bounds.height(), kMinTextDimension);
+#if defined(OS_WIN)
   EXPECT_EQ(gfx::Canvas::MULTI_LINE | gfx::Canvas::TEXT_ALIGN_CENTER, flags);
+#else
+  EXPECT_EQ(
+      gfx::Canvas::MULTI_LINE |
+      gfx::Canvas::TEXT_ALIGN_CENTER |
+      gfx::Canvas::NO_ELLIPSIS,
+      flags);
+#endif
   gfx::Rect center_bounds(text_bounds);
 
   label.SetHorizontalAlignment(Label::ALIGN_LEFT);
@@ -442,7 +453,15 @@ TEST(LabelTest, DrawMultiLineString) {
   EXPECT_EQ(0, text_bounds.y());
   EXPECT_GT(text_bounds.width(), kMinTextDimension);
   EXPECT_GT(text_bounds.height(), kMinTextDimension);
+#if defined(OS_WIN)
   EXPECT_EQ(gfx::Canvas::MULTI_LINE | gfx::Canvas::TEXT_ALIGN_LEFT, flags);
+#else
+  EXPECT_EQ(
+      gfx::Canvas::MULTI_LINE |
+      gfx::Canvas::TEXT_ALIGN_LEFT |
+      gfx::Canvas::NO_ELLIPSIS,
+      flags);
+#endif
 
   label.SetHorizontalAlignment(Label::ALIGN_RIGHT);
   paint_text.clear();
@@ -453,7 +472,15 @@ TEST(LabelTest, DrawMultiLineString) {
   EXPECT_EQ(0, text_bounds.y());
   EXPECT_GT(text_bounds.width(), kMinTextDimension);
   EXPECT_GT(text_bounds.height(), kMinTextDimension);
+#if defined(OS_WIN)
   EXPECT_EQ(gfx::Canvas::MULTI_LINE | gfx::Canvas::TEXT_ALIGN_RIGHT, flags);
+#else
+  EXPECT_EQ(
+      gfx::Canvas::MULTI_LINE |
+      gfx::Canvas::TEXT_ALIGN_RIGHT |
+      gfx::Canvas::NO_ELLIPSIS,
+      flags);
+#endif
 
   // Test multiline drawing with a border.
   gfx::Insets border(19, 92, 23, 2);
@@ -471,7 +498,15 @@ TEST(LabelTest, DrawMultiLineString) {
   EXPECT_EQ(center_bounds.y() + border.top(), text_bounds.y());
   EXPECT_EQ(center_bounds.width(), text_bounds.width());
   EXPECT_EQ(center_bounds.height(), text_bounds.height());
+#if defined(OS_WIN)
   EXPECT_EQ(gfx::Canvas::MULTI_LINE | gfx::Canvas::TEXT_ALIGN_CENTER, flags);
+#else
+  EXPECT_EQ(
+      gfx::Canvas::MULTI_LINE |
+      gfx::Canvas::TEXT_ALIGN_CENTER |
+      gfx::Canvas::NO_ELLIPSIS,
+      flags);
+#endif
 }
 
 }  // namespace views
