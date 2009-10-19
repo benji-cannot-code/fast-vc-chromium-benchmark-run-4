5e74ae0054ba87366c0254c420bde73702600334
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-19 03:13:31 +0000

    Make backspace not go back while IME is active.
    Linux does this in GtkIMContextWrapper::ProcessFilteredKeyPressEvent() by setting hardware_keycode to 0. It's a pain to change an NSEvent, so I just added a bool "skip" to NativeWebKeybordEvent and set it to "true" for rawkeydowns while ime is active.
    
    BUG=25000
    TEST=Open webpage. Focus text field. Type something, hit backspace. Text should be deleted. Focus background, hit backspace. Browser should go back one page. Focus text field, start IME (either by going to syspref->international->input menu and enabling e.g. "Kotoeri" and then selecting this in the top right corner and typing a single "a", or by just activating dead key ime on an us layout by hitting opt-u, opt-i, or opt-e), hit backspace. Browser should not go back but end IME mode.
    
    Review URL: http://codereview.chromium.org/295003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29388 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/native_web_keyboard_event_mac.mm
+++ chrome/common/native_web_keyboard_event_mac.mm
@@ -12,12 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using WebKit::WebInputEventFactory;
 
 NativeWebKeyboardEvent::NativeWebKeyboardEvent()
-    : os_event(NULL) {
+    : os_event(NULL),
+      skip_in_browser(false) {
 }
 
 NativeWebKeyboardEvent::NativeWebKeyboardEvent(NSEvent* event)
     : WebKeyboardEvent(WebInputEventFactory::keyboardEvent(event)),
-      os_event([event retain]) {
+      os_event([event retain]),
+      skip_in_browser(false) {
 }
 
 NativeWebKeyboardEvent::NativeWebKeyboardEvent(wchar_t character,
@@ -26,13 +28,15 @@ NativeWebKeyboardEvent::NativeWebKeyboardEvent(wchar_t character,
     : WebKeyboardEvent(WebInputEventFactory::keyboardEvent(character,
                                                            modifiers,
                                                            time_stamp_seconds)),
-      os_event(NULL) {
+      os_event(NULL),
+      skip_in_browser(false) {
 }
 
 NativeWebKeyboardEvent::NativeWebKeyboardEvent(
     const NativeWebKeyboardEvent& other)
     : WebKeyboardEvent(other),
-      os_event([other.os_event retain]) {
+      os_event([other.os_event retain]),
+      skip_in_browser(other.skip_in_browser) {
 }
 
 NativeWebKeyboardEvent& NativeWebKeyboardEvent::operator=(
@@ -42,6 +46,8 @@ NativeWebKeyboardEvent& NativeWebKeyboardEvent::operator=(
   NSObject* previous = os_event;
   os_event = [other.os_event retain];
   [previous release];
+
+  skip_in_browser = other.skip_in_browser;
 
   return *this;
 }
