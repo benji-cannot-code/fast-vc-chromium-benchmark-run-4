72b5f715a3bb1957f9159ca9e6c7ac8918b10d3a
dimich@google.com <dimich@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-19 21:39:54 +0000

    Degrade the https color and lock icon for the page that has a worker that loads subresource with bad cert. The idea is to make UI reflect loading of resources with bad certificates that were previously approved by the user. bug=20176 TEST=SSLUITest.TestUnsafeContentsInWorker unit_test. Review URL: http://codereview.chromium.org/268006
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29461 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/resource_request_details.h
+++ chrome/browser/renderer_host/resource_request_details.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/cert_store.h"
 #include "chrome/browser/renderer_host/resource_dispatcher_host.h"
 #include "chrome/browser/renderer_host/resource_dispatcher_host_request_info.h"
+#include "chrome/browser/worker_host/worker_service.h"
 #include "googleurl/src/gurl.h"
 #include "net/url_request/url_request_status.h"
 
@@ -40,7 +41,14 @@ class ResourceRequestDetails {
     frame_origin_ = info->frame_origin();
     main_frame_origin_ = info->main_frame_origin();
     filter_policy_ = info->filter_policy();
-    origin_child_id_ = info->child_id();
+
+    // If request is from the worker process on behalf of a renderer, use
+    // the renderer process id, since it consumes the notification response
+    // such as ssl state etc.
+    const WorkerProcessHost::WorkerInstance* worker_instance =
+        WorkerService::GetInstance()->FindWorkerInstance(info->child_id());
+    origin_child_id_ =
+        worker_instance ? worker_instance->renderer_id : info->child_id();
   }
 
   virtual ~ResourceRequestDetails() {}
