64a6380645d3ee31da01881397a977a9e1cbe2b4
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-19 18:43:18 +0000

    Use SSL to fetch the "known plug-ins" list.
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/280002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29430 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_process_host.cc
+++ chrome/browser/plugin_process_host.cc
@@ -76,7 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 static const char kDefaultPluginFinderURL[] =
-    "http://cache.pack.google.com/edgedl/chrome/plugins/plugins2.xml";
+    "https://dl-ssl.google.com/edgedl/chrome/plugins/plugins2.xml";
 
 #if defined(OS_WIN)
 
