2212a11c6b023c4c999348ff66549afb3a26f900
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-19 17:23:09 +0000

    Add a compile time assert to ensure that Google Chrome Frame builds of the installer have successfully picked up the presence of the Chrome Frame Dll at build time.
    This is to avoid project dependency regressions along the lines of http://crbug.com/25074 which cause Chrome Frame to build fine, but not be registered at install time.
    
    BUG=http://crbug.com/25074
    TEST=Compile of setup.exe must fail if CHROME_FRAME_BUILD is defined and the chrome frame dll is not present in the build directory.
    
    Review URL: http://codereview.chromium.org/292013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29416 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/setup/install.cc
+++ chrome/installer/setup/install.cc
@@ -30,6 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "installer_util_strings.h"
 #include "registered_dlls.h"
 
+#if defined(CHROME_FRAME_BUILD)
+COMPILE_ASSERT(kNumDllsToRegister > 0,
+               Chrome_Frame_DLL_must_be_built_before_setup);
+#endif
+
 namespace {
 
 std::wstring AppendPath(const std::wstring& parent_path,
