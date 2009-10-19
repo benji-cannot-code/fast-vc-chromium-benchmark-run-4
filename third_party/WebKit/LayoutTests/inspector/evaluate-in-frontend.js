5b67a83268d08d7667a60641d3236d66dd1f6710
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-19 00:20:43 +0000

    2009-10-18  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: make console tests more granular.
    
            https://bugs.webkit.org/show_bug.cgi?id=30496
    
            * inspector/console-dir-expected.txt: Added.
            * inspector/console-dir.html: Added.
            * inspector/console-dirxml-expected.txt: Added.
            * inspector/console-dirxml.html: Added.
            * inspector/console-format-expected.txt: Added.
            * inspector/console-format.html: Added.
            * inspector/console-tests-expected.txt:
            * inspector/console-tests.html:
            * platform/gtk/Skipped:
            * platform/qt/Skipped:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49760 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/inspector/evaluate-in-frontend.js
+++ third_party/WebKit/LayoutTests/inspector/evaluate-in-frontend.js
@@ -12,7 +12,8 @@ if (window.layoutTestController) {
 var ignoreLoad = window.location.href.indexOf("?reload") === -1;
 if (ignoreLoad) {
     setTimeout(function() {
-        layoutTestController.showWebInspector();
+        if (window.layoutTestController)
+            layoutTestController.showWebInspector();
         window.location.href += "?reload";
     }, 0);
 }
@@ -43,9 +44,8 @@ function evaluateInWebInspector(script, callback)
     var callId = lastCallId++;
     callbacks[callId] = callback;
     setTimeout(function() {
-        if (window.layoutTestController) {
+        if (window.layoutTestController)
             layoutTestController.evaluateInWebInspector(callId, script);
-        }
     }, 0);
 
 }
