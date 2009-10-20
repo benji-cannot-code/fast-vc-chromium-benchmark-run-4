72523b9c0a50b97064071b756b7757c278e8cfe3
zoltan@webkit.org <zoltan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-20 20:12:32 +0000

    2009-10-20  Zoltan Horvath  <zoltan@webkit.org>
            Reviewed by Eric Seidel.
    
            Allow custom memory allocation control for WebCore's CSSProperty
            https://bugs.webkit.org/show_bug.cgi?id=30564
    
            Inherits CSSProperty class from FastAllocBase because it has been
            instantiated by 'new' in WebCore/css/CSSParser.cpp:367.
    
            * css/CSSProperty.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSProperty.h
+++ third_party/WebKit/WebCore/css/CSSProperty.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class CSSProperty {
+class CSSProperty : public FastAllocBase {
 public:
     CSSProperty(int propID, PassRefPtr<CSSValue> value, bool important = false, int shorthandID = 0, bool implicit = false)
         : m_id(propID)
