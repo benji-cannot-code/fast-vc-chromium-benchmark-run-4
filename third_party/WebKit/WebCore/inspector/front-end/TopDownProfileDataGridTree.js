1c4e6a0e7fd73c04355d6ff67d0215a1b06b3d57
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-20 23:10:46 +0000

    2009-10-20  Mikhail Naganov  <mnaganov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: populate child nodes before sorting them.
    
            https://bugs.webkit.org/show_bug.cgi?id=29673
    
            * inspector/front-end/BottomUpProfileDataGridTree.js:
            (WebInspector.BottomUpProfileDataGridNode): Swapped with BottomUpProfileDataGridTree to be consistent with TopDownProfileDataGridNode.
            (WebInspector.BottomUpProfileDataGridNode.prototype._takePropertiesFromProfileDataGridNode):
            (WebInspector.BottomUpProfileDataGridNode.prototype._keepOnlyChild):
            (WebInspector.BottomUpProfileDataGridNode.prototype._exclude):
            (WebInspector.BottomUpProfileDataGridNode.prototype._merge):
            (WebInspector.BottomUpProfileDataGridNode.prototype._sharedPopulate):
            (WebInspector.BottomUpProfileDataGridTree.prototype.exclude):
            * inspector/front-end/ProfileDataGridTree.js:
            (WebInspector.ProfileDataGridNode.prototype.sort): Added missing parentheses.
            (WebInspector.ProfileDataGridNode.prototype.get _parent):
            (WebInspector.ProfileDataGridNode.prototype._populate):
            * inspector/front-end/TopDownProfileDataGridTree.js:
            (WebInspector.TopDownProfileDataGridNode.prototype._sharedPopulate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49891 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/TopDownProfileDataGridTree.js
+++ third_party/WebKit/WebCore/inspector/front-end/TopDownProfileDataGridTree.js
@@ -34,16 +34,13 @@ WebInspector.TopDownProfileDataGridNode = function(/*ProfileView*/ profileView, 
 }
 
 WebInspector.TopDownProfileDataGridNode.prototype = {
-    _populate: function(event)
+    _sharedPopulate: function()
     {
         var children = this._remainingChildren;
         var childrenLength = children.length;
 
         for (var i = 0; i < childrenLength; ++i)
             this.appendChild(new WebInspector.TopDownProfileDataGridNode(this.profileView, children[i], this.tree));
-
-        if (this.removeEventListener)
-            this.removeEventListener("populate", this._populate, this);
 
         this._remainingChildren = null;
     },
@@ -106,7 +103,9 @@ WebInspector.TopDownProfileDataGridTree.prototype = {
             this.sort(this.lastComparator, true);
     },
 
-    _merge: WebInspector.TopDownProfileDataGridNode.prototype._merge
+    _merge: WebInspector.TopDownProfileDataGridNode.prototype._merge,
+
+    _sharedPopulate: WebInspector.TopDownProfileDataGridNode.prototype._sharedPopulate
 }
 
 WebInspector.TopDownProfileDataGridTree.prototype.__proto__ = WebInspector.ProfileDataGridTree.prototype;
