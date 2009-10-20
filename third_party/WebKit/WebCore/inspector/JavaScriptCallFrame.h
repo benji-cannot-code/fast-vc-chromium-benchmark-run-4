7a1db8f835405f597d89c75136c761301981e9bd
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-20 02:59:23 +0000

    Small changes to fully invalidate and update the JavaScriptCallFrame.
    Patch by Oliver Hunt <oliver@apple.com> on 2009-10-19
    <rdar://problem/7020755> JSDebugger crashes after reloading from a breakpoint
    https://bugs.webkit.org/show_bug.cgi?id=27146
    
    Reviewed by Timothy Hatcher.
    
    * inspector/JavaScriptCallFrame.h:
    (WebCore::JavaScriptCallFrame::invalidate):
    (WebCore::JavaScriptCallFrame::update):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/JavaScriptCallFrame.h
+++ third_party/WebKit/WebCore/inspector/JavaScriptCallFrame.h
@@ -45,7 +45,12 @@ namespace WebCore {
             return adoptRef(new JavaScriptCallFrame(debuggerCallFrame, caller, sourceID, line));
         }
 
-        void invalidate() { m_isValid = false; }
+        void invalidate()
+        {
+            m_isValid = false;
+            m_debuggerCallFrame = 0;
+        }
+
         bool isValid() const { return m_isValid; }
 
         JavaScriptCallFrame* caller();
@@ -57,6 +62,7 @@ namespace WebCore {
             m_debuggerCallFrame = debuggerCallFrame;
             m_line = line;
             m_sourceID = sourceID;
+            m_isValid = true;
         }
 
         String functionName() const;
