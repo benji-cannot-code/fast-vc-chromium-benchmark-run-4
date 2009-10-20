488af0f5ce6c5c214d0833c6b7d6a3c5d054dcc6
dumi@chromium.org <dumi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-20 01:09:20 +0000

    Fixing a typo.
    Patch by Dumitru Daniliuc <dumi@chromium.org> on 2009-10-19
    Reviewed by Darin Adler.
    
    https://bugs.webkit.org/show_bug.cgi?id=30543
    
    * platform/sql/SQLiteDatabase.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49826 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/sql/SQLiteDatabase.h
+++ third_party/WebKit/WebCore/platform/sql/SQLiteDatabase.h
@@ -25,8 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#ifndef SQLDatabase_h
-#define SQLDatabase_h
+#ifndef SQLiteDatabase_h
+#define SQLiteDatabase_h
 
 #include "PlatformString.h"
 #include <wtf/Threading.h>
