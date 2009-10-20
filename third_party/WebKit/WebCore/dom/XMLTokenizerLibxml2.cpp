23e6be8a6f24126e8436b0664e928faac6994c8f
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-20 04:39:20 +0000

    2009-10-19  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Eric Seidel.
    
            Check COMPILER(MSVC) instead of PLATFORM(WIN_OS) to avoid the use of vasprintf.
            https://bugs.webkit.org/show_bug.cgi?id=30473
    
            vasprintf is missing in MSVC. Use COMPILER(MSVC) guards instead of
            PLALTFORM(WIN_OS) guards.
    
            * dom/XMLTokenizerLibxml2.cpp:
            (WebCore::XMLTokenizer::error):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/XMLTokenizerLibxml2.cpp
+++ third_party/WebKit/WebCore/dom/XMLTokenizerLibxml2.cpp
@@ -888,7 +888,7 @@ void XMLTokenizer::error(ErrorType type, const char* message, va_list args)
     if (m_parserStopped)
         return;
 
-#if PLATFORM(WIN_OS)
+#if COMPILER(MSVC)
     char m[1024];
     vsnprintf(m, sizeof(m) - 1, message, args);
 #else
@@ -902,7 +902,7 @@ void XMLTokenizer::error(ErrorType type, const char* message, va_list args)
     else
         handleError(type, m, lineNumber(), columnNumber());
 
-#if !PLATFORM(WIN_OS)
+#if !COMPILER(MSVC)
     free(m);
 #endif
 }
