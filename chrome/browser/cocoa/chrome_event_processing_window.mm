ffabe1a4eb25b21f176602a69188d889ee309856
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-20 02:33:54 +0000

    Fix DCHECK.
    BUG=none
    TEST=Focus background of google.com, hit "a". Shouldn't DCHECK.
    
    Review URL: http://codereview.chromium.org/297017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29488 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/chrome_event_processing_window.mm
+++ chrome/browser/cocoa/chrome_event_processing_window.mm
@@ -88,6 +88,7 @@ typedef int (*KeyToCommandMapper)(bool, bool, bool, int);
 }
 
 - (void)redispatchEvent:(NSEvent*)event {
+  DCHECK(event);
   DCHECK([event window] == self);
   redispatchingEvent_ = YES;
   [NSApp sendEvent:event];
