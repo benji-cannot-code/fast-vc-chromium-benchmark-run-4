c54697e95f45bacffe4f31acea64daf190ef56e1
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-20 16:44:35 +0000

    Cleanup unneccessary include.
    Review URL: http://codereview.chromium.org/308002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29519 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents_view_mac.mm
+++ chrome/browser/tab_contents/tab_contents_view_mac.mm
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "chrome/browser/browser.h" // TODO(beng): this dependency is awful.
-#import "chrome/browser/chrome_application_mac.h"
 #import "chrome/browser/cocoa/focus_tracker.h"
 #import "chrome/browser/cocoa/chrome_browser_window.h"
 #import "chrome/browser/cocoa/browser_window_controller.h"
