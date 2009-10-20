ed05d1017069ad397ed29a5eb5acb85234e020a0
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-20 21:56:00 +0000

    Windows build fix: updated variable name.
    Patch by Geoffrey Garen <ggaren@apple.com> on 2009-10-20
    * runtime/DatePrototype.cpp:
    (JSC::formatLocaleDate):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49889 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/DatePrototype.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/DatePrototype.cpp
@@ -255,7 +255,7 @@ static JSCell* formatLocaleDate(ExecState* exec, const GregorianDateTime& gdt, L
 static JSCell* formatLocaleDate(ExecState* exec, DateInstance* dateObject, double, LocaleDateTimeFormat format, const ArgList&)
 {
     GregorianDateTime gregorianDateTime;
-    const bool notUTC = false;
+    const bool outputIsUTC = false;
     if (!dateObject->getGregorianDateTime(outputIsUTC, gregorianDateTime))
         return jsNontrivialString(exec, "Invalid Date");
     return formatLocaleDate(exec, gregorianDateTime, format);
