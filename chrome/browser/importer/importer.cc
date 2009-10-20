363e7e3d875bcf42fda63d31ca7037023b0391ff
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-20 00:11:09 +0000

    Add dialog to tell user to close Firefox during import, if it's open and blocking.  OS-Mac only.
    BUG= http://crbug.com/20899
    TEST= Open Firefox; start up Chrome and try to import settings from Firefox. Dialog box warns you to close Firefox.
    
    Review URL: http://codereview.chromium.org/283023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29481 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/importer.cc
+++ chrome/browser/importer/importer.cc
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/window/window.h"
 #elif defined(OS_LINUX)
 #include "chrome/browser/gtk/import_lock_dialog_gtk.h"
+#elif defined(OS_MACOSX)
+#include "chrome/browser/cocoa/importer_lock_dialog.h"
 #endif
 
 using webkit_glue::PasswordForm;
@@ -470,8 +472,7 @@ void ImporterHost::ShowWarningDialog() {
 #elif defined(OS_LINUX)
     ImportLockDialogGtk::Show(parent_window_, this);
 #else
-    // TODO(port): Need CreateChromeWindow.
-    NOTIMPLEMENTED();
+    ImportLockDialogCocoa(this);
 #endif
   }
 }
--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -1125,6 +1125,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/cocoa/hung_renderer_controller.mm',
         'browser/cocoa/import_progress_dialog.h',
         'browser/cocoa/import_progress_dialog.mm',
+        'browser/cocoa/importer_lock_dialog.h',
+        'browser/cocoa/importer_lock_dialog.mm',
         'browser/cocoa/infobar.h',
         'browser/cocoa/infobar_container_controller.h',
         'browser/cocoa/infobar_container_controller.mm',
