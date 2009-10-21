b115f56a579350fc422e9a47846b519ce86c8635
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-21 02:31:48 +0000

    Fix leak of WebSocketChannel by adopting the newly-created reference.
    Reviewed by Oliver Hunt.
    
    * websockets/WebSocketChannel.h:
    (WebCore::WebSocketChannel::create):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49896 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/websockets/WebSocketChannel.h
+++ third_party/WebKit/WebCore/websockets/WebSocketChannel.h
@@ -48,7 +48,7 @@ namespace WebCore {
 
     class WebSocketChannel : public RefCounted<WebSocketChannel>, public SocketStreamHandleClient {
     public:
-        static PassRefPtr<WebSocketChannel> create(ScriptExecutionContext* context, WebSocketChannelClient* client, const KURL& url, const String& protocol) { return new WebSocketChannel(context, client, url, protocol); }
+        static PassRefPtr<WebSocketChannel> create(ScriptExecutionContext* context, WebSocketChannelClient* client, const KURL& url, const String& protocol) { return adoptRef(new WebSocketChannel(context, client, url, protocol)); }
         virtual ~WebSocketChannel();
 
         virtual void connect();
