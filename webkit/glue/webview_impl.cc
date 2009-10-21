8545d2d99ec5a8280fde56fc0217ed6d0201eac0
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-21 14:52:14 +0000

    [Mac] Call setIsActive() when changing focus in layout test mode.
    This change effectively reverts http://src.chromium.org/viewvc/chrome?view=rev&revision=25491 and fixes http://crbug.com/16018 in a different way.
    
    BUG=http://crbug.com/16018
    BUG=http://crbug.com/21738
    TEST=layout tests should still pass
    Review URL: http://codereview.chromium.org/258035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29644 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webview_impl.cc
+++ webkit/glue/webview_impl.cc
@@ -1099,12 +1099,11 @@ void WebViewImpl::mouseCaptureLost() {
 }
 
 void WebViewImpl::setFocus(bool enable) {
+  page_->focusController()->setFocused(enable);
   if (enable) {
-    // Note that we call setActive and setFocued in opposite order in focus vs
-    // blur. This is done so that setActive does not dispatch any events since
-    // the event will be dispatched in setFocused.
+    // Note that we don't call setActive() when disabled as this cause extra
+    // focus/blur events to be dispatched.
     page_->focusController()->setActive(true);
-    page_->focusController()->setFocused(true);
     ime_accept_events_ = true;
   } else {
     HideAutoCompletePopup();
@@ -1125,8 +1124,6 @@ void WebViewImpl::setFocus(bool enable) {
         editor->confirmComposition();
       ime_accept_events_ = false;
     }
-    page_->focusController()->setFocused(false);
-    page_->focusController()->setActive(false);
   }
 }
 
