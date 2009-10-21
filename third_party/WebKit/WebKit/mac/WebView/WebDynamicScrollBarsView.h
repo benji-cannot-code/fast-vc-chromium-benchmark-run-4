1f2bf2da01be467266951f7efd4dcfb8adc2133b
sullivan@apple.com <sullivan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-21 20:02:32 +0000

    Rubber-stamped by Dan Bernstein.
    * WebView/WebDynamicScrollBarsView.h:
    Corrected a couple of long-obsolete comments regarding Safari's use of this class.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49920 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebDynamicScrollBarsView.h
+++ third_party/WebKit/WebKit/mac/WebView/WebDynamicScrollBarsView.h
@@ -30,8 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This is a Private header (containing SPI), despite the fact that its name
 // does not contain the word Private.
 
-// FIXME: Does Safari really need to use this any more? AppKit added autohidesScrollers
-// in Panther, and that was the original reason we needed this view in Safari.
+// This was once used by Safari, but has not been for a long time.
 
 // FIXME: <rdar://problem/5898985> Mail currently expects this header to define WebCoreScrollbarAlwaysOn.
 extern const int WebCoreScrollbarAlwaysOn;
@@ -48,5 +47,8 @@ extern const int WebCoreScrollbarAlwaysOn;
     BOOL horizontallyPinnedByPreviousWheelEvent;
     unsigned inUpdateScrollersLayoutPass;
 }
-- (void)setAllowsHorizontalScrolling:(BOOL)flag; // This method is used by Safari, so it cannot be removed.
+
+// This was originally added for Safari's benefit, but Safari has not used it for a long time.
+// Perhaps it can be removed.
+- (void)setAllowsHorizontalScrolling:(BOOL)flag;
 @end
