1e8b2134884abd2e50ec5f44d15c49d675c2c90a
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-21 18:41:44 +0000

    Added a static function pointer to cache the SetActiveURL pointer. - Windows only change.
    This is basically landing jschuh change http://codereview.chromium.org/293040
    
    TEST=none
    BUG=25366
    
    
    
    Review URL: http://codereview.chromium.org/313009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29677 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/child_process_logging_win.cc
+++ chrome/common/child_process_logging_win.cc
@@ -12,19 +12,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/gurl.h"
 
 namespace child_process_logging {
-
+// exported in breakpad_win.cc: void __declspec(dllexport) __cdecl SetActiveURL.
 typedef void (__cdecl *MainSetActiveURL)(const wchar_t*);
 
 void SetActiveURL(const GURL& url) {
-  HMODULE exe_module = GetModuleHandle(chrome::kBrowserProcessExecutableName);
-  if (!exe_module)
-    return;
-
-  MainSetActiveURL set_active_url =
-      reinterpret_cast<MainSetActiveURL>(
-          GetProcAddress(exe_module, "SetActiveURL"));
-  if (!set_active_url)
-    return;
+  static MainSetActiveURL set_active_url = NULL;
+  // note: benign race condition on set_active_url.
+  if (!set_active_url) {
+    HMODULE exe_module = GetModuleHandle(chrome::kBrowserProcessExecutableName);
+    if (!exe_module)
+      return;
+    set_active_url = reinterpret_cast<MainSetActiveURL>(
+        GetProcAddress(exe_module, "SetActiveURL"));
+    if (!set_active_url)
+      return;
+  }
 
   (set_active_url)(UTF8ToWide(url.possibly_invalid_spec()).c_str());
 }
