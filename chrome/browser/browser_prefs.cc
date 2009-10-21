db58df6dbcd6c041db4677ce35aa67faab306572
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-21 21:47:00 +0000

    Rename AutofillManager to FormFieldHistoryManager to better reflect the purpose of the class.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/292050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29717 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_prefs.cc
+++ chrome/browser/browser_prefs.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_prefs.h"
 
-#include "chrome/browser/autofill_manager.h"
 #include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/blocked_popup_container.h"
 #include "chrome/browser/browser.h"
@@ -15,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/download_manager.h"
 #include "chrome/browser/extensions/extension_dom_ui.h"
 #include "chrome/browser/external_protocol_handler.h"
+#include "chrome/browser/form_field_history_manager.h"
 #include "chrome/browser/google_url_tracker.h"
 #include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/browser/net/dns_global.h"
@@ -76,7 +76,7 @@ void RegisterAllPrefs(PrefService* user_prefs, PrefService* local_state) {
   DownloadManager::RegisterUserPrefs(user_prefs);
   SSLManager::RegisterUserPrefs(user_prefs);
   bookmark_utils::RegisterUserPrefs(user_prefs);
-  AutofillManager::RegisterUserPrefs(user_prefs);
+  FormFieldHistoryManager::RegisterUserPrefs(user_prefs);
   TabContents::RegisterUserPrefs(user_prefs);
   TemplateURLPrepopulateData::RegisterUserPrefs(user_prefs);
   ExtensionDOMUI::RegisterUserPrefs(user_prefs);
