2ab3678299f74cb346c7a9a945c6be6ebd6e4ec4
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-21 18:11:28 +0000

    Mac: Switch from "Cancel" to "Do Nothing" for the external protocol dialog, and allow remembering the "Do Nothing" choice.
    BUG=none
    TEST=Verify that "Do Nothing" is the left button, and that clicking the "Remember" checkbox remembers the "Do Nothing" state.
    
    Review URL: http://codereview.chromium.org/314003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29671 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/external_protocol_dialog.mm
+++ chrome/browser/cocoa/external_protocol_dialog.mm
@@ -54,7 +54,8 @@ void ExternalProtocolHandler::RunExternalProtocolDialog(
       l10n_util::GetNSStringWithFixup(IDS_EXTERNAL_PROTOCOL_OK_BUTTON_TEXT)];
   [allowButton setKeyEquivalent:@""];  // disallow as default
   [alert_ addButtonWithTitle:
-      l10n_util::GetNSStringWithFixup(IDS_CANCEL)];
+      l10n_util::GetNSStringWithFixup(
+        IDS_EXTERNAL_PROTOCOL_CANCEL_BUTTON_TEXT)];
 
   NSString* urlString = l10n_util::GetNSStringFWithFixup(
       IDS_EXTERNAL_PROTOCOL_INFORMATION,
@@ -107,11 +108,8 @@ void ExternalProtocolHandler::RunExternalProtocolDialog(
       NOTREACHED();
   }
 
-  // Set the "don't warn me again" info if the protocol was allowed ("cancel"
-  // always means "make this dialog go away with no permanent effect" no matter
-  // what).
-  if ([[alert_ suppressionButton] state] == NSOnState &&
-      blockState == ExternalProtocolHandler::DONT_BLOCK) {
+  // Set the "don't warn me again" info.
+  if ([[alert_ suppressionButton] state] == NSOnState) {
     ExternalProtocolHandler::SetBlockState(UTF8ToWide(url_.scheme()),
                                            blockState);
   }
