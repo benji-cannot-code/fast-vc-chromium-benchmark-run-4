61ed49934d96655d2e4a2d766205cf74aaf9e009
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-21 19:36:08 +0000

    2009-10-21  Keishi Hattori  <casey.hattori@gmail.com>
            Reviewed by Timothy Hatcher.
    
            REGRESSION: Clicking on nodes in the console should take you to the element in the DOM
            https://bugs.webkit.org/show_bug.cgi?id=27231
    
            * inspector/front-end/ConsoleView.js:
            (WebInspector.ConsoleView.prototype._formatnode): Turns on ElementsTreeOutline.showInElementsPanelEnabled
            * inspector/front-end/ElementsTreeOutline.js:
            (WebInspector.ElementsTreeOutline): Added ElementsTreeOutline.showInElementsPanelEnabled
            (WebInspector.ElementsTreeElement.prototype.onmousedown): Reveals the node in the Elements panel if
            treeOutline.showInElementsPanelEnabled is true.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49917 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ElementsTreeOutline.js
+++ third_party/WebKit/WebCore/inspector/front-end/ElementsTreeOutline.js
@@ -39,6 +39,7 @@ WebInspector.ElementsTreeOutline = function() {
 
     this.includeRootDOMNode = true;
     this.selectEnabled = false;
+    this.showInElementsPanelEnabled = false;
     this.rootDOMNode = null;
     this.focusedDOMNode = null;
 }
@@ -509,6 +510,11 @@ WebInspector.ElementsTreeElement.prototype = {
     {
         if (this._editing)
             return;
+
+        if (this.treeOutline.showInElementsPanelEnabled) {    
+            WebInspector.showElementsPanel();
+            WebInspector.panels.elements.focusedDOMNode = this.representedObject;
+        }
 
         // Prevent selecting the nearest word on double click.
         if (event.detail >= 2)
