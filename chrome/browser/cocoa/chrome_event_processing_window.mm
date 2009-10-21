1d2c6ed7efa9504a88c373bea2e44bce4287094c
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-21 15:39:12 +0000

    Revert 29646 - Fix cmdoptleft/right.
    This surfaced because I moved global shortcut handling before menu dispatch recently, and global keyboard handling didn't look at the opt button, so it thought cmdoptleft meant cmdleft and swallowed the event.
    
    BUG=25396
    TEST=Focus text box in web. cmdoptleft/right should switch tabs, cmdleft/right should move cursor in text box. Focus web background. cmdoptleft/right should switch tabs, cmdleft/right should do history navigation. Focus omnibox. cmdoptleft/right should still switch tabs, cmdleft/right should move cursor. cmd1/2 should switch tabs in all three places, cmdopt1/2 should not do anything.
    
    Review URL: http://codereview.chromium.org/313004
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/306025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29648 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/chrome_event_processing_window.mm
+++ chrome/browser/cocoa/chrome_event_processing_window.mm
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/renderer_host/render_widget_host_view_mac.h"
 #include "chrome/browser/global_keyboard_shortcuts_mac.h"
 
-typedef int (*KeyToCommandMapper)(bool, bool, bool, bool, int);
+typedef int (*KeyToCommandMapper)(bool, bool, bool, int);
 
 @implementation ChromeEventProcessingWindow
 
@@ -23,10 +23,9 @@ typedef int (*KeyToCommandMapper)(bool, bool, bool, bool, int);
   const bool cmdKey = modifers & NSCommandKeyMask;
   const bool shiftKey = modifers & NSShiftKeyMask;
   const bool cntrlKey = modifers & NSControlKeyMask;
-  const bool optKey = modifers & NSAlternateKeyMask;
   const int keyCode = [event keyCode];
 
-  int cmdNum = commandForKeyboardShortcut(cmdKey, shiftKey, cntrlKey, optKey,
+  int cmdNum = commandForKeyboardShortcut(cmdKey, shiftKey, cntrlKey,
       keyCode);
 
   BrowserWindowController* controller =
