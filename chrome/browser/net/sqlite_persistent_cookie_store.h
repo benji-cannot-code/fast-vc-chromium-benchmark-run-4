7fdb6ecf699547f8f93272e52076f6e2be3ce7bd
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-21 01:28:58 +0000

    revert r29588.
    John says the DB thread always gets shut down by the IO thread, so the change is unnecessary, also, it caused valgrind leaks.
    
    BUG=25245
    
    Review URL: http://codereview.chromium.org/293039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29596 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/sqlite_persistent_cookie_store.h
+++ chrome/browser/net/sqlite_persistent_cookie_store.h
@@ -23,7 +23,8 @@ class MessageLoop;
 class SQLitePersistentCookieStore
     : public net::CookieMonster::PersistentCookieStore {
  public:
-  SQLitePersistentCookieStore(const FilePath& path);
+  SQLitePersistentCookieStore(const FilePath& path,
+                              MessageLoop* background_loop);
   ~SQLitePersistentCookieStore();
 
   virtual bool Load(std::vector<net::CookieMonster::KeyedCanonicalCookie>*);
@@ -42,6 +43,9 @@ class SQLitePersistentCookieStore
 
   FilePath path_;
   scoped_refptr<Backend> backend_;
+
+  // Background MessageLoop on which to access the backend_;
+  MessageLoop* background_loop_;
 
   sql::MetaTable meta_table_;
 
