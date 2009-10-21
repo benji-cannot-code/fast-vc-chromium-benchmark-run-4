f24073e24e838224e6b5f6f943a2c62ee318af31
chocobo@google.com <chocobo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-21 20:01:35 +0000

    Switch to use libcros to make changes to touchpad settings. BUG=none TEST=none Review URL: http://codereview.chromium.org/300011
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29693 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/preferences.cc
+++ chrome/browser/chromeos/preferences.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/preferences.h"
 
 #include "base/string_util.h"
-#include "chrome/browser/chromeos/touchpad.h"
+#include "chrome/browser/chromeos/synaptics_library.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/pref_member.h"
 #include "chrome/common/pref_names.h"
@@ -46,13 +46,18 @@ void Preferences::NotifyPrefChanged(const std::wstring* pref_name) {
   if (!pref_name || *pref_name == prefs::kTimeZone)
     SetTimeZone(timezone_.GetValue());
   if (!pref_name || *pref_name == prefs::kTapToClickEnabled)
-    Touchpad::SetTapToClick(tap_to_click_enabled_.GetValue());
+    SynapticsLibrary::Get()->SetBoolParameter(PARAM_BOOL_TAP_TO_CLICK,
+        tap_to_click_enabled_.GetValue());
   if (!pref_name || *pref_name == prefs::kVertEdgeScrollEnabled)
-    Touchpad::SetVertEdgeScroll(vert_edge_scroll_enabled_.GetValue());
+    SynapticsLibrary::Get()->SetBoolParameter(
+        PARAM_BOOL_VERTICAL_EDGE_SCROLLING,
+        vert_edge_scroll_enabled_.GetValue());
   if (!pref_name || *pref_name == prefs::kTouchpadSpeedFactor)
-    Touchpad::SetSpeedFactor(speed_factor_.GetValue());
+    SynapticsLibrary::Get()->SetRangeParameter(PARAM_RANGE_SPEED_SENSITIVITY,
+                                               speed_factor_.GetValue());
   if (!pref_name || *pref_name == prefs::kTouchpadSensitivity)
-    Touchpad::SetSensitivity(sensitivity_.GetValue());
+    SynapticsLibrary::Get()->SetRangeParameter(PARAM_RANGE_TOUCH_SENSITIVITY,
+                                               sensitivity_.GetValue());
 }
 
 void Preferences::SetTimeZone(const std::wstring& id) {
