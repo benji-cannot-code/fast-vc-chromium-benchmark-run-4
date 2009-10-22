3d637d14e3639296a482ef6c04cb97ef8c722fe0
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-22 17:59:08 +0000

    Hook up the browser in the bookmark editor. Adjust the logic controlling the OK button enabling.
    In the BookmarkEditor.xib: Connect the NSBrowser's delegate (showing the bookmark tree) to the File's Owner (BookmarkEditorController). 
    Patch from Mike Rossetti (mrosseti@chromium.org)
    Original review at: http://codereview.chromium.org/287014
    BUG=17613
    TEST=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29784 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_editor_controller.h
+++ chrome/browser/cocoa/bookmark_editor_controller.h
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property (assign) NSString* displayName;
 @property (assign) NSString* displayURL;
 @property (readonly) BOOL okButtonEnabled;
+- (void)selectTestNodeInBrowser:(const BookmarkNode*)node;
 @end
 
 
