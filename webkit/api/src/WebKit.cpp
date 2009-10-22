d5dbbc95d3cfc786a3075cf99b9c72f4f55887bd
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-22 01:24:43 +0000

    Switch over to the new way of enabling/disabling session/local storage.
    BUG=none
    TEST=|'localStorage' in window| should only be true iff --enable-local-storage
         is specified
    
    Review URL: http://codereview.chromium.org/293032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29747 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/src/WebKit.cpp
+++ webkit/api/src/WebKit.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoader.h"
 #include "Page.h"
 #include "SecurityOrigin.h"
+#include "Storage.h"
 #include "TextEncoding.h"
 #include "WebSocket.h"
 #include "WorkerContextExecutionProxy.h"
@@ -160,6 +161,20 @@ bool webSocketsEnabled()
     return WebCore::WebSocket::isAvailable();
 #else
     return false;
+#endif
+}
+
+void setLocalStorageEnabled(bool enabled)
+{
+#if ENABLE(DOM_STORAGE)
+    WebCore::Storage::setLocalStorageAvailable(enabled);
+#endif
+}
+
+void setSessionStorageEnabled(bool enabled)
+{
+#if ENABLE(DOM_STORAGE)
+    WebCore::Storage::setSessionStorageAvailable(enabled);
 #endif
 }
 
