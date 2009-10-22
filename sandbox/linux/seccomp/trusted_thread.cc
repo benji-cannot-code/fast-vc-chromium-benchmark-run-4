e51da60cbb6a977d567a02cf31f29e30cdc1aea3
markus@chromium.org <markus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-22 22:12:29 +0000

    GCC's optimizer is getting more aggressive. It is no longer good enough to just pass the address of a structure as an input parameter to assembly code. The assembly code must also mark "memory" as getting clobbered, even if it only wants to read from the structure. This seems to be a result of strict aliasing and the lack of an ability for the assembly code to clearly say which pointers it dereferences.
    Furthermore, if the assembly code touches the stack (e.g. uses
    "push"), it must now mark the stack pointer as getting
    clobbered. Otherwise, GCC assumes that the red zone won't be
    clobbered, and that it is possible to use the stack pointer as an
    input register.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/320008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29829 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/seccomp/trusted_thread.cc
+++ sandbox/linux/seccomp/trusted_thread.cc
@@ -587,7 +587,7 @@ void Sandbox::createTrustedThread(int processFdPub, int cloneFdPub,
       :
       : "g"(&args)
       : "rax", "rcx", "rdx", "rdi", "rsi", "r8", "r9", "r10", "r11", "r12",
-        "r13", "r14", "r15"
+        "r13", "r14", "r15", "rsp", "memory"
 #elif defined(__i386__)
   struct user_desc u;
   u.entry_number    = (typeof u.entry_number)-1;
@@ -1198,7 +1198,7 @@ void Sandbox::createTrustedThread(int processFdPub, int cloneFdPub,
       "pop  %%ebx\n"
       :
       : "g"(&args)
-      : "eax", "ecx", "edx", "edi", "esi"
+      : "eax", "ecx", "edx", "edi", "esi", "esp", "memory"
 #else
 #error Unsupported target platform
 #endif
