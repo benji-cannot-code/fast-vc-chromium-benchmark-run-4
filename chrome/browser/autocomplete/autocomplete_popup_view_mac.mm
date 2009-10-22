d9a31403e1359761043e0ba22f0fa635e9f30e6f
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-22 00:35:53 +0000

    [Mac] Cmd-Return in the omnibox should revert the omnibox text back to its original state.
    BUG=23514
    TEST=Type a URL into the omnibox, then Cmd-Return.  The omnibox text should revert to its original state.
    Review URL: http://codereview.chromium.org/260008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29743 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_popup_view_mac.mm
+++ chrome/browser/autocomplete/autocomplete_popup_view_mac.mm
@@ -455,7 +455,7 @@ AutocompletePopupModel* AutocompletePopupViewMac::GetModel() {
   return model_.get();
 }
 
-void AutocompletePopupViewMac::AcceptInput() {
+void AutocompletePopupViewMac::OnClick() {
   const NSInteger selectedRow = [[popup_ contentView] selectedRow];
 
   // -1 means no cells were selected.  This can happen if the user
@@ -464,17 +464,23 @@ void AutocompletePopupViewMac::AcceptInput() {
   if (selectedRow == -1) {
     PaintUpdatesNow();
   } else {
-    model_->SetSelectedLine(selectedRow, false);
-    WindowOpenDisposition disposition =
-        event_utils::WindowOpenDispositionFromNSEvent([NSApp currentEvent]);
-    edit_view_->AcceptInput(disposition, false);
+    OpenURLForRow(selectedRow, false);
   }
 }
 
 void AutocompletePopupViewMac::OnMiddleClick() {
-  const NSInteger row = [[popup_ contentView] highlightedRow];
+  OpenURLForRow([[popup_ contentView] highlightedRow], true);
+}
+
+void AutocompletePopupViewMac::OpenURLForRow(int row, bool force_background) {
   if (row == -1) {
     return;
+  }
+
+  WindowOpenDisposition disposition = NEW_BACKGROUND_TAB;
+  if (!force_background) {
+    disposition =
+        event_utils::WindowOpenDispositionFromNSEvent([NSApp currentEvent]);
   }
 
   // OpenURL() may close the popup, which will clear the result set
@@ -485,7 +491,7 @@ void AutocompletePopupViewMac::OnMiddleClick() {
   const GURL url(match.destination_url);
   std::wstring keyword;
   const bool is_keyword_hint = model_->GetKeywordForMatch(match, &keyword);
-  edit_view_->OpenURL(url, NEW_BACKGROUND_TAB, match.transition, GURL(), row,
+  edit_view_->OpenURL(url, disposition, match.transition, GURL(), row,
                       is_keyword_hint ? std::wstring() : keyword);
 }
 
@@ -714,7 +720,7 @@ void AutocompletePopupViewMac::OnMiddleClick() {
 
 - (void)select:(id)sender {
   DCHECK(popup_view_);
-  popup_view_->AcceptInput();
+  popup_view_->OnClick();
 }
 
 - (void)middleSelect:(id)sender {
