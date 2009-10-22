bd30d7cadf66654e7e979568e5521915705efde8
yaar@chromium.org <yaar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-22 20:51:55 +0000

    Move ContextMenuClientImpl to api/src ISSUE: http://code.google.com/p/chromium/issues/detail?id=24611
    Review URL: http://codereview.chromium.org/295054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29814 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webview_impl.h
+++ webkit/glue/webview_impl.h
@@ -14,9 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/api/public/WebString.h"
 #include "webkit/api/public/WebView.h"
 #include "webkit/api/src/NotificationPresenterImpl.h"
+#include "webkit/api/src/WebContextMenuClientImpl.h"
 #include "webkit/glue/back_forward_list_client_impl.h"
 #include "webkit/glue/chrome_client_impl.h"
-#include "webkit/glue/context_menu_client_impl.h"
 #include "webkit/glue/dragclient_impl.h"
 #include "webkit/glue/editor_client_impl.h"
 #include "webkit/glue/inspector_client_impl.h"
@@ -38,6 +38,7 @@ class Widget;
 
 namespace WebKit {
 class WebAccessibilityObject;
+class WebContextMenuClientImpl;
 class WebKeyboardEvent;
 class WebMouseEvent;
 class WebMouseWheelEvent;
@@ -258,7 +259,7 @@ class WebViewImpl : public WebKit::WebView, public RefCounted<WebViewImpl> {
 
   webkit_glue::BackForwardListClientImpl back_forward_list_client_impl_;
   ChromeClientImpl chrome_client_impl_;
-  ContextMenuClientImpl context_menu_client_impl_;
+  WebKit::WebContextMenuClientImpl context_menu_client_impl_;
   DragClientImpl drag_client_impl_;
   EditorClientImpl editor_client_impl_;
   InspectorClientImpl inspector_client_impl_;
