9a01e7d41b3abe052f687ef5ce03508b72cbfa3c
ojan@chromium.org <ojan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-23 22:37:17 +0000

    Disable test failing since r29947.
    TBR=tony@chromium.org
    Review URL: http://codereview.chromium.org/338006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29966 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_storage_apitest.cc
+++ chrome/browser/extensions/extension_storage_apitest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Storage) {
+// Started failing with r29947. WebKit merge 49961:49992.
+IN_PROC_BROWSER_TEST_F(DISABLED_ExtensionApiTest, Storage) {
   ASSERT_TRUE(RunExtensionTest("storage")) << message_;
 }
