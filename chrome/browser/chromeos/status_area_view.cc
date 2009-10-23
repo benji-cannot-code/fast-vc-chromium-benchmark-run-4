49ad7356917a3febf79c0d4f2447fe491d554ef9
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-23 21:11:14 +0000

    Get the About panel to align its links correctly, but undo a previous change to the way we measure strings in pango.
    Review URL: http://codereview.chromium.org/338001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29943 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status_area_view.cc
+++ chrome/browser/chromeos/status_area_view.cc
@@ -34,8 +34,6 @@ namespace {
 const int kLeftBorder = 1;
 // Number of pixels to separate the clock from the next item on the right.
 const int kClockSeparation = 4;
-// Number of pixels to adjust the y value of the clock.
-const int kClockYAdjustment = 2;
 
 // BrowserWindowGtk tiles its image with this offset
 const int kCustomFrameBackgroundVerticalOffset = 15;
@@ -173,10 +171,6 @@ void StatusAreaView::Layout() {
 
     // Handle odd number of pixels.
     cur_y += (height() - cur_size.height()) % 2;
-
-    // Adjustment to make clock line up right.
-    if (cur == clock_view_)
-      cur_y += kClockYAdjustment;
 
     // Put next in row horizontally, and center vertically.
     cur->SetBounds(cur_x, cur_y, cur_size.width(), cur_size.height());
