85a17561a68c163c02b35fe78af8943861ba025a
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-23 04:48:04 +0000

    Build fix following bug #30696.
    Patch by Gavin Barraclough <barraclough@apple.com> on 2009-10-22
    Reviewed by NOBODY (build fix).
    
    * WebCoreSupport/FrameLoaderClientGtk.cpp:
    (WebKit::FrameLoaderClient::windowObjectCleared):
    * webkit/webkitwebframe.cpp:
    (webkit_web_frame_get_global_context):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@49964 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebframe.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebframe.cpp
@@ -684,7 +684,7 @@ JSGlobalContextRef webkit_web_frame_get_global_context(WebKitWebFrame* frame)
     if (!coreFrame)
         return NULL;
 
-    return toGlobalRef(coreFrame->script()->globalObject()->globalExec());
+    return toGlobalRef(coreFrame->script()->globalObject(mainThreadNormalWorld())->globalExec());
 }
 
 /**
