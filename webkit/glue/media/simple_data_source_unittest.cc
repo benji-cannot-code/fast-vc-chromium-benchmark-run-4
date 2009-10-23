ba112d6d956a8b659504f81d93f83924c916220c
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-23 01:09:45 +0000

    Fixed valgrind breakage by r29848
    TBR=scherkus
    
    References shouldn't have been released.
    
    Review URL: http://codereview.chromium.org/333004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29856 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/media/simple_data_source_unittest.cc
+++ webkit/glue/media/simple_data_source_unittest.cc
@@ -51,9 +51,10 @@ class SimpleDataSourceTest : public testing::Test {
   }
 
   virtual ~SimpleDataSourceTest() {
-    if (bridge_.get()) {
+    if (bridge_.get())
       EXPECT_CALL(*bridge_, OnDestroy());
-    }
+    if (bridge_factory_.get())
+      EXPECT_CALL(*bridge_factory_, OnDestroy());
   }
 
   void InitializeDataSource(const char* url) {
@@ -191,9 +192,6 @@ TEST_F(SimpleDataSourceTest, InitializeFile) {
 }
 
 TEST_F(SimpleDataSourceTest, InitializeData) {
-  // Bridge is not used at all.
-  ReleaseBridge();
-
   media::MediaFormat url_format;
   url_format.SetAsString(media::MediaFormat::kMimeType,
                          media::mime_type::kURL);
@@ -217,10 +215,6 @@ TEST_F(SimpleDataSourceTest, InitializeData) {
 }
 
 TEST_F(SimpleDataSourceTest, InitializeInvalid) {
-  // They are not used at all.
-  ReleaseBridge();
-  ReleaseBridgeFactory();
-
   media::MediaFormat url_format;
   url_format.SetAsString(media::MediaFormat::kMimeType,
                          media::mime_type::kURL);
