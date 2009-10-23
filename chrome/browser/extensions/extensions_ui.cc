14cb0295ca760f9c95992ec05c9b7fe94fd4b1b4
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-23 20:09:34 +0000

    Switching to a plugin icon as the favicon for the extensions page.
    TBR=arv
    BUG=25509
    TEST=Open the extensions page and it should have a plugin as it's favicon.
    
    Review URL: http://codereview.chromium.org/329012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29935 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_ui.cc
+++ chrome/browser/extensions/extensions_ui.cc
@@ -515,5 +515,5 @@ ExtensionsUI::ExtensionsUI(TabContents* contents) : DOMUI(contents) {
 // static
 RefCountedMemory* ExtensionsUI::GetFaviconResourceBytes() {
   return ResourceBundle::GetSharedInstance().
-      LoadImageResourceBytes(IDR_EXTENSIONS_FAVICON);
+      LoadImageResourceBytes(IDR_PLUGIN);
 }
