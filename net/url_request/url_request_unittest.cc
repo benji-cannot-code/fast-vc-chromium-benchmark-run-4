8c46c783a7d53c2925fcf5d11c6d3f61912a5642
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-23 16:09:41 +0000

    Fixes ChromeFrame net tests which run the URLRequest unit tests by launching IE and having it issue the corresponding HTTP requests via automation. Fixes as below:-
    1. The DefaultAcceptCharset and DefaultAcceptLanguage tests were failing because the URL request automation
       job would only read the extra headers from the request. These tests set these headers in the URLRequestContext.
       We needed to mimic the functionality in the URLRequestHttpJob to add in these headers if they were not
       already present. As part of this I moved the AppendHeaderIfMissing function from url_request_http_job.cc
       to HttpUtil as it is needed by the automation job as well.
    
    2. The OverrideAcceptLanguage and OverrideAcceptCharset tests started failing in chrome frame net tests
       after the fixes to get the default versions of these tests to pass. These tests basically pass in the
       Accept-Language and Accept-Charset headers and expect the same values to be echoed back. IE ends up
       caching the responses from the default versions of these tests and thus echoes back the old response
       which causes these tests to fail. I tried passing in the no-cache header from our HTTP server for
       the EchoHeader tests but this did not work. To fix this we now pass in the echoheaderoverride
       parameter for the OverrideAcceptLanguage and OverrideAcceptCharset tests. The HTTP server has been
       updated to support this.
    
    3. NotifyDone can be called on the job if the original request was redirected. Added a check for whether
       NotifyDone was already called on the job in URLRequestAutomationJob
    
    Review URL: http://codereview.chromium.org/322004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29895 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -2178,7 +2178,8 @@ TEST_F(URLRequestTestHTTP, DefaultAcceptLanguage) {
 TEST_F(URLRequestTestHTTP, OverrideAcceptLanguage) {
   ASSERT_TRUE(NULL != server_.get());
   TestDelegate d;
-  TestURLRequest req(server_->TestServerPage("echoheader?Accept-Language"), &d);
+  TestURLRequest
+      req(server_->TestServerPage("echoheaderoverride?Accept-Language"), &d);
   req.set_context(new URLRequestTestContext());
   req.SetExtraRequestHeaders("Accept-Language: ru");
   req.Start();
@@ -2202,7 +2203,8 @@ TEST_F(URLRequestTestHTTP, DefaultAcceptCharset) {
 TEST_F(URLRequestTestHTTP, OverrideAcceptCharset) {
   ASSERT_TRUE(NULL != server_.get());
   TestDelegate d;
-  TestURLRequest req(server_->TestServerPage("echoheader?Accept-Charset"), &d);
+  TestURLRequest
+      req(server_->TestServerPage("echoheaderoverride?Accept-Charset"), &d);
   req.set_context(new URLRequestTestContext());
   req.SetExtraRequestHeaders("Accept-Charset: koi-8r");
   req.Start();
