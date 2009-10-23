f4a15f6e3c37e8363a339a52a40ae30630d0a746
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-23 02:25:38 +0000

    Add unittest for WebSocket::ProcessFrameData.
    It catches a bug fixed in r29760.
    Also find another bug and fixed in this change.
    (same bug found in WebKit code. reported as
     http://bugs.webkit.org/show_bug.cgi?id=30668)
    
    BUG=none
    TEST=net_unittests passes
    
    Review URL: http://codereview.chromium.org/307036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29864 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket.cc
+++ net/websockets/websocket.cc
@@ -49,7 +49,7 @@ WebSocket::WebSocket(Request* request, WebSocketDelegate* delegate)
 }
 
 WebSocket::~WebSocket() {
-  DCHECK(!delegate_);
+  DCHECK(ready_state_ == INITIALIZED || !delegate_);
   DCHECK(!socket_stream_);
 }
 
@@ -132,17 +132,7 @@ void WebSocket::OnSentData(SocketStream* socket_stream, int amount_sent) {
 void WebSocket::OnReceivedData(SocketStream* socket_stream,
                                const char* data, int len) {
   DCHECK(socket_stream == socket_stream_);
-  DCHECK(current_read_buf_);
-  // Check if |current_read_buf_| has enough space to store |len| of |data|.
-  if (len >= current_read_buf_->RemainingCapacity()) {
-    current_read_buf_->set_capacity(
-        current_read_buf_->offset() + len);
-  }
-
-  DCHECK(current_read_buf_->RemainingCapacity() >= len);
-  memcpy(current_read_buf_->data(), data, len);
-  current_read_buf_->set_offset(current_read_buf_->offset() + len);
-
+  AddToReadBuffer(data, len);
   origin_loop_->PostTask(FROM_HERE,
                          NewRunnableMethod(this, &WebSocket::DoReceivedData));
 }
@@ -390,6 +380,8 @@ void WebSocket::ProcessFrameData() {
       if (p + length < end) {
         p += length;
         next_frame = p;
+      } else {
+        break;
       }
     } else {
       const char* msg_start = p;
@@ -406,7 +398,23 @@ void WebSocket::ProcessFrameData() {
   SkipReadBuffer(next_frame - start_frame);
 }
 
+void WebSocket::AddToReadBuffer(const char* data, int len) {
+  DCHECK(current_read_buf_);
+  // Check if |current_read_buf_| has enough space to store |len| of |data|.
+  if (len >= current_read_buf_->RemainingCapacity()) {
+    current_read_buf_->set_capacity(
+        current_read_buf_->offset() + len);
+  }
+
+  DCHECK(current_read_buf_->RemainingCapacity() >= len);
+  memcpy(current_read_buf_->data(), data, len);
+  current_read_buf_->set_offset(current_read_buf_->offset() + len);
+}
+
 void WebSocket::SkipReadBuffer(int len) {
+  if (len == 0)
+    return;
+  DCHECK_GT(len, 0);
   read_consumed_len_ += len;
   int remaining = current_read_buf_->offset() - read_consumed_len_;
   DCHECK_GE(remaining, 0);
