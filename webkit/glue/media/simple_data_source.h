ccda911735fccfc9beef0a05120ca06c400bf1dc
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-23 00:23:05 +0000

    Allow <video> to read from a data URI. Data URI is now handled by SimpleDataSource. Selection of data source is done by the existing factory method by calling to IsMediaFormatSupported. And each data source would determine if they support the mentioned scheme.
    BUG=24357
    TEST=open a video with data:/ scheme.
    
    Review URL: http://codereview.chromium.org/284007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29848 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/media/simple_data_source.h
+++ webkit/glue/media/simple_data_source.h
@@ -35,6 +35,10 @@ class SimpleDataSource : public media::DataSource,
                                                         bridge_factory);
   }
 
+  // media::FilterFactoryImpl2 implementation.
+  static bool IsMediaFormatSupported(
+      const media::MediaFormat& media_format);
+
   // MediaFilter implementation.
   virtual void Stop();
 
@@ -79,6 +83,9 @@ class SimpleDataSource : public media::DataSource,
 
   // Cancels and deletes the resource loading on the render thread.
   void CancelTask();
+
+  // Perform initialization completion tasks under a lock.
+  void DoneInitialization_Locked(bool success);
 
   // Primarily used for asserting the bridge is loading on the render thread.
   MessageLoop* render_loop_;
--- webkit/tools/test_shell/test_shell.cc
+++ webkit/tools/test_shell/test_shell.cc
@@ -691,7 +691,10 @@ bool IsDefaultPluginEnabled() {
 }
 
 bool IsProtocolSupportedForMedia(const GURL& url) {
-  if (url.SchemeIsFile() || url.SchemeIs("http") || url.SchemeIs("https"))
+  if (url.SchemeIsFile() ||
+      url.SchemeIs("http") ||
+      url.SchemeIs("https") ||
+      url.SchemeIs("data"))
     return true;
   return false;
 }
