60691678b51f03c814ba8be52a45acee4e81ed50
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-24 00:23:07 +0000

    Initialize WebContextMenuData::mediaFlags.
    Before we were having completely random results right-clicking media elements.  Fun, but not user friendly.
    
    BUG=25358
    TEST=right click on media elements -- observe consistent/expected behaviour
    
    Review URL: http://codereview.chromium.org/332018
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29976 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/src/WebContextMenuClientImpl.cpp
+++ webkit/api/src/WebContextMenuClientImpl.cpp
@@ -158,6 +158,7 @@ PlatformMenuDescription WebContextMenuClientImpl::getCustomMenuFromDefaultItems(
     data.linkURL = r.absoluteLinkURL();
 
     data.mediaType = WebContextMenuData::MediaTypeNone;
+    data.mediaFlags = WebContextMenuData::MediaNone;
 
     if (!r.absoluteImageURL().isEmpty()) {
         data.srcURL = r.absoluteImageURL();
