5fdb998833c42f0d9cf32e72e9e13a5406a12a0a
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-24 16:26:46 +0000

    Attempts to get the chrome_frame_tests going again on the chrome frame builder. Fixes as below:- 1. Removed a DCHECK from    ChromeFrameAutomationClient::CleanupRequests which checks    if the request was actually deleted. This DCHECK is not    correct anymore as the request is stopped asynchronously.
    2. We now stop the worker thread in WM_DESTROY as the 
       activex window needs to be valid for Urlmon requests
       to be released correctly. In some cases IE reuses the 
       ActiveX instance. So we need to create the worker
       thread on demand.
    
    TBR=amit
    
    
    Review URL: http://codereview.chromium.org/334020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30007 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_frame_automation.cc
+++ chrome_frame/chrome_frame_automation.cc
@@ -1043,7 +1043,6 @@ void ChromeFrameAutomationClient::CleanupRequests() {
     if (request) {
       int request_id = request->id();
       request->Stop();
-      DCHECK(request_map_.end() == request_map_.find(request_id));
     }
   }
 
