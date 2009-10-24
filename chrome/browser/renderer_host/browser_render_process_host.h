33f02a211310f73ec590fab147341ef6f4354c5b
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-24 03:46:14 +0000

    Enable HTML5 databases for all extension renderer processes.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/330010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29995 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.h
+++ chrome/browser/renderer_host/browser_render_process_host.h
@@ -55,7 +55,7 @@ class BrowserRenderProcessHost : public RenderProcessHost,
   ~BrowserRenderProcessHost();
 
   // RenderProcessHost implementation (public portion).
-  virtual bool Init();
+  virtual bool Init(bool is_extensions_process);
   virtual int GetNextRoutingID();
   virtual void CancelResourceRequests(int render_widget_id);
   virtual void CrossSiteClosePageACK(const ViewMsg_ClosePage_Params& params);
