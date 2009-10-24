80c2727914a80369bcf659f348f9481158845ced
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-24 00:37:58 +0000

    Instructing V8 to disable the binding for the window.openDatabase() function.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/295053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29980 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/src/WebKit.cpp
+++ webkit/api/src/WebKit.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AtomicString.h"
 #include "DOMTimer.h"
 #include "Page.h"
+#include "RuntimeEnabledFeatures.h"
 #include "TextEncoding.h"
 #include "WebSocket.h"
 #include "WorkerContextExecutionProxy.h"
@@ -111,6 +112,7 @@ void enableDatabases()
 {
 #if ENABLE(DATABASE)
     s_databasesEnabled = true;
+    WebCore::RuntimeEnabledFeatures::setDatabaseEnabled(true);
 #endif
 }
 
