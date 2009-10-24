f9de79d6f5eada295959bf1e4e5644274cbc0125
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-24 00:33:09 +0000

    Mac: remove some now-dead code and add a comment.
    I slightly jumped the gun on committing CL 328010, and this patch does some things requested in a review.
    
    BUG=23825
    TEST=none
    
    Review URL: http://codereview.chromium.org/326024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29979 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/safari_importer.h
+++ chrome/browser/importer/safari_importer.h
@@ -55,7 +55,6 @@ class SafariImporter : public Importer {
   void ImportBookmarks();
   void ImportPasswords();
   void ImportHistory();
-  void ImportHomepage();
 
   // Parse Safari's stored bookmarks.
   void ParseBookmarks(std::vector<ProfileWriter::BookmarkEntry>* bookmarks);
