288d921734013f22623c6a3586baba6ecda5090b
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-24 01:28:39 +0000

    Clean up database files.  Some changes are due to Google style guide violations, some to be consistent with prevailing team style, some merely in hopes of increasing readability.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/330007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29985 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/database_dispatcher_host.h
+++ chrome/browser/renderer_host/database_dispatcher_host.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_RENDERER_HOST_DATABASE_DISPATCHER_HOST_H_
 
 #include "base/file_path.h"
-#include "base/ref_counted.h"
 
 class MessageLoop;
 class ResourceMessageFilter;
@@ -19,24 +18,29 @@ class Message;
 class DatabaseDispatcherHost {
  public:
   DatabaseDispatcherHost(const FilePath& profile_path,
-    ResourceMessageFilter* resource_message_filter);
-  ~DatabaseDispatcherHost();
+                         ResourceMessageFilter* resource_message_filter);
+  ~DatabaseDispatcherHost() {}
 
   // Returns true iff the message is HTML5 DB related and was processed.
   bool OnMessageReceived(const IPC::Message& message, bool* message_was_ok);
 
   // Processes the request to return a handle to the given DB file.
   void OnDatabaseOpenFile(const FilePath& file_name,
-    int desired_flags, int32 message_id);
+                          int desired_flags,
+                          int32 message_id);
+
   // Processes the request to delete the given DB file.
-  void OnDatabaseDeleteFile(const FilePath& file_name, const bool& sync_dir,
-    int32 message_id);
+  void OnDatabaseDeleteFile(const FilePath& file_name,
+                            const bool& sync_dir,
+                            int32 message_id);
+
   // Processes the request to return the attributes of the given DB file.
   void OnDatabaseGetFileAttributes(const FilePath& file_name,
-    int32 message_id);
+                                   int32 message_id);
+
   // Processes the request to return the size of the given file.
   void OnDatabaseGetFileSize(const FilePath& file_name,
-    int32 message_id);
+                             int32 message_id);
 
  private:
   // Determines if the message is HTML5 DB related.
@@ -44,6 +48,7 @@ class DatabaseDispatcherHost {
 
   // Returns the directory where all DB files are stored.
   FilePath GetDBDir();
+
   // Returns the absolute name of the given DB file.
   FilePath GetDBFileFullPath(const FilePath& file_name);
 
