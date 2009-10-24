715874c814402ad9547c736f8a8b573993fd2a67
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-24 22:16:30 +0000

    Missing images at 350.org due to wrong URLs https://bugs.webkit.org/show_bug.cgi?id=30748
    Reviewed by Sam Weinig.
    
    WebCore: 
    
    Test: fast/dom/HTMLImageElement/parse-src.html
    
    * dom/Element.cpp:
    (WebCore::Element::getURLAttribute): Pass the attribute value through deprecateParseURL().
    This matches what the Objective-C bindings do, as well as what HTMLImageLoader::sourceURI()
    and SVGImageLoader::sourceURI() do.
    
    LayoutTests: 
    
    * fast/dom/HTMLImageElement/parse-src-expected.txt: Added.
    * fast/dom/HTMLImageElement/parse-src.html: Added.
    * fast/dom/HTMLImageElement/script-tests: Added.
    * fast/dom/HTMLImageElement/script-tests/TEMPLATE.html: Copied from LayoutTests/fast/dom/Document/script-tests/TEMPLATE.html.
    * fast/dom/HTMLImageElement/script-tests/parse-src.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Element.cpp
+++ third_party/WebKit/WebCore/dom/Element.cpp
@@ -1415,7 +1415,7 @@ KURL Element::getURLAttribute(const QualifiedName& name) const
             ASSERT(isURLAttribute(attribute));
     }
 #endif
-    return document()->completeURL(getAttribute(name));
+    return document()->completeURL(deprecatedParseURL(getAttribute(name)));
 }
 
 } // namespace WebCore
