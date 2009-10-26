f35e0a6d7ea11e7f184aae2316755047cf916850
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-26 22:21:42 +0000

    Makes inactive pinned tabs throb on views when the title changes.
    BUG=25308
    TEST=go to gmail, pin the tab, select another tab. Have someone IM you
         and make sure your gmail tab throbs.
    
    Review URL: http://codereview.chromium.org/329021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30113 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/dragged_tab_controller.cc
+++ chrome/browser/views/tabs/dragged_tab_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "app/animation.h"
+#include "app/slide_animation.h"
 #include "app/gfx/canvas.h"
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
