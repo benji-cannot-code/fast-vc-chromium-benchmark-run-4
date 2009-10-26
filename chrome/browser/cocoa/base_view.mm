ac936e5a8448e240adf865161b3aa6f8a3ba1497
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-26 04:53:43 +0000

    Mac: Make status bubbles pop properly when mouse leaves content area.
    We hadn't set up our tracking area (in BaseView) to send mouse enter/exits.
    
    BUG=22199
    TEST=Get a page with lots of links which upon resizing can be made to abut the left edge (e.g., <http://www.nytimes.com/>), and resize/scroll to make it so; hover over a link to show status bubble; move mouse off the window, staying on the same link until off the window. The status bubble should then fade away.
    
    Review URL: http://codereview.chromium.org/337016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30033 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/base_view.mm
+++ chrome/browser/cocoa/base_view.mm
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (self) {
     trackingArea_ =
         [[NSTrackingArea alloc] initWithRect:frame
-                                     options:NSTrackingMouseMoved |
+                                     options:NSTrackingMouseEnteredAndExited |
+                                             NSTrackingMouseMoved |
                                              NSTrackingActiveInActiveApp |
                                              NSTrackingInVisibleRect
                                        owner:self
