35642e8609f0edff0688550cb43e7ad7ddccc193
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-27 18:47:51 +0000

    Add "remember my choice" checkbox for windows external protocol dialog.
    BUG=24085
    
    Review URL: http://codereview.chromium.org/332039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/external_protocol_dialog.h
+++ chrome/browser/views/external_protocol_dialog.h
@@ -32,6 +32,7 @@ class ExternalProtocolDialog : public views::DialogDelegate {
       MessageBoxFlags::DialogButton button) const;
   virtual std::wstring GetWindowTitle() const;
   virtual void DeleteDelegate();
+  virtual bool Cancel();
   virtual bool Accept();
   virtual views::View* GetContentsView();
 
