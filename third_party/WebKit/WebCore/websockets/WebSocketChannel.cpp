e9037a67a12efa32a0a14717c07a1050a107802e
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-27 17:00:52 +0000

    2009-10-27  Fumitoshi Ukai  <ukai@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Fix crash found in chromium test_shell.
            https://bugs.webkit.org/show_bug.cgi?id=30808
    
            When WebSocket is deleted without close, webkit would crash
            when it handles didClose.
    
            Check scriptExecutionContext before post task for event.
            Use WebSocketChannel::disconnect() instead of close() in WebSocket
            destructor, so that WebSocketChannel should not call deleted WebSocket
            back in didClose().
            To make sure WebSocketChannel alive while it is processing WebSocket
            protocol over SocketStreamHandle, ref() in connect() and deref() in
            didClose().
    
            * websockets/WebSocket.cpp:
            (WebCore::WebSocket::~WebSocket):
            (WebCore::WebSocket::didConnect):
            (WebCore::WebSocket::didReceiveMessage):
            (WebCore::WebSocket::didClose):
            * websockets/WebSocketChannel.cpp:
            (WebCore::WebSocketChannel::connect):
            (WebCore::WebSocketChannel::disconnect):
            (WebCore::WebSocketChannel::didClose):
            (WebCore::WebSocketChannel::didReceiveData):
            * websockets/WebSocketChannel.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50155 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/websockets/WebSocketChannel.cpp
+++ third_party/WebKit/WebCore/websockets/WebSocketChannel.cpp
@@ -72,6 +72,7 @@ void WebSocketChannel::connect()
     LOG(Network, "WebSocketChannel %p connect", this);
     ASSERT(!m_handle.get());
     m_handshake.reset();
+    ref();
     m_handle = SocketStreamHandle::create(m_handshake.url(), this);
 }
 
@@ -104,6 +105,14 @@ void WebSocketChannel::close()
         m_handle->close();  // will call didClose()
 }
 
+void WebSocketChannel::disconnect()
+{
+    LOG(Network, "WebSocketChannel %p disconnect", this);
+    m_client = 0;
+    if (m_handle.get())
+        m_handle->close();
+}
+
 void WebSocketChannel::willOpenStream(SocketStreamHandle*, const KURL&)
 {
 }
@@ -127,13 +136,15 @@ void WebSocketChannel::didClose(SocketStreamHandle* handle)
 {
     LOG(Network, "WebSocketChannel %p didClose", this);
     ASSERT(handle == m_handle.get() || !m_handle.get());
-    if (!m_handle.get())
-        return;
-    m_unhandledBufferSize = handle->bufferedAmount();
-    WebSocketChannelClient* client = m_client;
-    m_client = 0;
-    m_handle = 0;
-    client->didClose();
+    if (m_handle.get()) {
+        m_unhandledBufferSize = handle->bufferedAmount();
+        WebSocketChannelClient* client = m_client;
+        m_client = 0;
+        m_handle = 0;
+        if (client)
+            client->didClose();
+    }
+    deref();
 }
 
 void WebSocketChannel::didReceiveData(SocketStreamHandle* handle, const char* data, int len)
@@ -141,6 +152,10 @@ void WebSocketChannel::didReceiveData(SocketStreamHandle* handle, const char* da
     LOG(Network, "WebSocketChannel %p didReceiveData %d", this, len);
     ASSERT(handle == m_handle.get());
     if (!appendToBuffer(data, len)) {
+        handle->close();
+        return;
+    }
+    if (!m_client) {
         handle->close();
         return;
     }
