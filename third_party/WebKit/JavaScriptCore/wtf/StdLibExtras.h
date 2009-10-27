6cb599fde7d6db3ba6214f0eeb4a159804ecaca6
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-27 21:04:20 +0000

    2009-10-27  Carol Szabo  <carol.szabo@nokia.com>
            Reviewed by Darin Adler.
    
            Give an ability to WebKit to free statically allocated pointers
            before quit.
            https://bugs.webkit.org/show_bug.cgi?id=27980
    
            No new tests for this as the change does not affect WebKit
            functionality, but only memory usage.
    
            * JavaScriptCore/wtf/StdLibExtras.h:
            Allowed for the override of DEFINE_STATIC_LOCAL via compiler
            commandline options or elsewhere in the code. Useful for
            implementing custom builds that free resources allocated via
            DEFINE_STATIC_LOCAL.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50171 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/StdLibExtras.h
+++ third_party/WebKit/JavaScriptCore/wtf/StdLibExtras.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use these to declare and define a static local variable (static T;) so that
 //  it is leaked so that its destructors are not called at exit. Using this
 //  macro also allows workarounds a compiler bug present in Apple's version of GCC 4.0.1.
+#ifndef DEFINE_STATIC_LOCAL
 #if COMPILER(GCC) && defined(__APPLE_CC__) && __GNUC__ == 4 && __GNUC_MINOR__ == 0 && __GNUC_PATCHLEVEL__ == 1
 #define DEFINE_STATIC_LOCAL(type, name, arguments) \
     static type* name##Ptr = new type arguments; \
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 #define DEFINE_STATIC_LOCAL(type, name, arguments) \
     static type& name = *new type arguments
+#endif
 #endif
 
 // OBJECT_OFFSETOF: Like the C++ offsetof macro, but you can use it with classes.
