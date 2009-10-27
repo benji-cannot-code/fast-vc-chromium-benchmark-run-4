bce6b5021d48413e8a8cdca9b5ce5ec40cfb6e32
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-27 20:44:57 +0000

    Map update check failure to "update server not available."
    BUG=25948
    TEST=With a suitably recent KSR, mess up the update server URL in the ticket,
         and verify that the update status line in the About box shows the gray
         icon with "update server not available" after attempting to check for
         an update.
    Review URL: http://codereview.chromium.org/344002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30240 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/keystone_glue.mm
+++ chrome/app/keystone_glue.mm
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/worker_pool_mac.h"
 #include "chrome/common/chrome_constants.h"
 
+namespace {
+
 // Provide declarations of the Keystone registration bits needed here.  From
 // KSRegistration.h.
 typedef enum { kKSPathExistenceChecker } KSExistenceCheckerType;
@@ -18,6 +20,7 @@ NSString *KSRegistrationCheckForUpdateNotification =
     @"KSRegistrationCheckForUpdateNotification";
 NSString *KSRegistrationStatusKey = @"Status";
 NSString *KSRegistrationVersionKey = @"Version";
+NSString *KSRegistrationUpdateCheckErrorKey = @"Error";
 
 NSString *KSRegistrationStartUpdateNotification =
     @"KSRegistrationStartUpdateNotification";
@@ -26,6 +29,8 @@ NSString *KSUpdateCheckSuccessfullyInstalledKey = @"SuccessfullyInstalled";
 
 NSString *KSRegistrationRemoveExistingTag = @"";
 #define KSRegistrationPreserveExistingTag nil
+
+}  // namespace
 
 @interface KSRegistration : NSObject
 
@@ -254,10 +259,10 @@ static KeystoneGlue* sDefaultKeystoneGlue = nil;  // leaked
 
 - (void)checkForUpdateComplete:(NSNotification*)notification {
   NSDictionary* userInfo = [notification userInfo];
-  BOOL updatesAvailable =
-      [[userInfo objectForKey:KSRegistrationStatusKey] boolValue];
 
-  if (updatesAvailable) {
+  if ([[userInfo objectForKey:KSRegistrationUpdateCheckErrorKey] boolValue]) {
+    [self updateStatus:kAutoupdateCheckFailed version:nil];
+  } else if ([[userInfo objectForKey:KSRegistrationStatusKey] boolValue]) {
     // If an update is known to be available, go straight to
     // -updateStatus:version:.  It doesn't matter what's currently on disk.
     NSString* version = [userInfo objectForKey:KSRegistrationVersionKey];
@@ -288,12 +293,10 @@ static KeystoneGlue* sDefaultKeystoneGlue = nil;  // leaked
 
 - (void)installUpdateComplete:(NSNotification*)notification {
   NSDictionary* userInfo = [notification userInfo];
-  BOOL checkSuccessful =
-      [[userInfo objectForKey:KSUpdateCheckSuccessfulKey] boolValue];
-  int installs =
-      [[userInfo objectForKey:KSUpdateCheckSuccessfullyInstalledKey] intValue];
 
-  if (!checkSuccessful || !installs) {
+  if (![[userInfo objectForKey:KSUpdateCheckSuccessfulKey] boolValue] ||
+      ![[userInfo objectForKey:KSUpdateCheckSuccessfullyInstalledKey]
+          intValue]) {
     [self updateStatus:kAutoupdateInstallFailed version:nil];
   } else {
     updateSuccessfullyInstalled_ = YES;
