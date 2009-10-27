cde52f688c69d6fdc8e3823247886b6bf02b37ec
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-27 09:07:43 +0000

    2009-10-27  Eric Seidel  <eric@webkit.org>
            No review, rolling out r50131.
            http://trac.webkit.org/changeset/50131
    
            * bridge/objc/objc_instance.h:
            * bridge/objc/objc_instance.mm:
            (ObjcInstance::ObjcInstance):
            (ObjcInstance::virtualEnd):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50133 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/objc/objc_instance.h
+++ third_party/WebKit/WebCore/bridge/objc/objc_instance.h
@@ -72,7 +72,7 @@ private:
     
     RetainPtr<ObjectStructPtr> _instance;
     mutable ObjcClass *_class;
-    RetainPtr<ObjectStructPtr> _pool;
+    ObjectStructPtr _pool;
     int _beginCount;
 };
 
