5a3ace5171f1c45c00f1e5562298a807476eb3ba
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-27 17:45:41 +0000

    2009-10-27  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            [V8] DOM Storage runtime flag should default to on
            https://bugs.webkit.org/show_bug.cgi?id=30828
    
            The DOM Storage runtime flag was supposed to default to on.  I accidentally
            changed the logic to default to false when I moved the flag.
    
            * bindings/v8/RuntimeEnabledFeatures.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50158 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/RuntimeEnabledFeatures.cpp
+++ third_party/WebKit/WebCore/bindings/v8/RuntimeEnabledFeatures.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 bool RuntimeEnabledFeatures::isDatabaseEnabled = false;
-bool RuntimeEnabledFeatures::isLocalStorageEnabled = false;
-bool RuntimeEnabledFeatures::isSessionStorageEnabled = false;
+bool RuntimeEnabledFeatures::isLocalStorageEnabled = true;
+bool RuntimeEnabledFeatures::isSessionStorageEnabled = true;
 
 } // namespace WebCore
