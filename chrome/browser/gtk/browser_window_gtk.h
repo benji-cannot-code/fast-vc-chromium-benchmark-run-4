64e406c8db0960012f58be63e765d28986742b2f
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-28 19:17:09 +0000

    Remove the non-const browser() getter from BrowserWindowGtk.
    Review URL: http://codereview.chromium.org/347006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30375 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_window_gtk.h
+++ chrome/browser/gtk/browser_window_gtk.h
@@ -55,8 +55,6 @@ class BrowserWindowGtk : public BrowserWindow,
   explicit BrowserWindowGtk(Browser* browser);
   virtual ~BrowserWindowGtk();
 
-  Browser* browser() const { return browser_.get(); }
-
   // Process a keyboard event which was not handled by webkit.
   bool HandleKeyboardEvent(GdkEventKey* event);
 
@@ -193,9 +191,7 @@ class BrowserWindowGtk : public BrowserWindow,
   // ID of the top-level X window of this object.
   static GtkWindow* GetBrowserWindowForXID(XID xid);
 
-  Browser* browser() {
-    return browser_.get();
-  }
+  Browser* browser() const { return browser_.get(); }
 
   GtkWindow* window() const { return window_; }
 
