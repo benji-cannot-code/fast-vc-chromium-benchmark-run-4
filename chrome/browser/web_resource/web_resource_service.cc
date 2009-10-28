3bae6890e3ffc84ee976f548249fe469e6c20089
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-28 00:17:51 +0000

    Simple fix so the utility process respects single process mode.
    BUG=24746
    
    Review URL: http://codereview.chromium.org/339033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30289 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/web_resource_service.cc
+++ chrome/browser/web_resource/web_resource_service.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 #include "chrome/browser/web_resource/web_resource_service.h"
 
+#include "base/command_line.h"
 #include "base/string_util.h"
 #include "base/time.h"
 #include "base/values.h"
@@ -11,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/net/url_fetcher.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/load_flags.h"
@@ -107,7 +109,8 @@ class WebResourceService::UnpackerClient
     // If we don't have a resource_dispatcher_host_, assume we're in
     // a test and run the unpacker directly in-process.
     bool use_utility_process =
-        web_resource_service_->resource_dispatcher_host_ != NULL;
+        web_resource_service_->resource_dispatcher_host_ != NULL &&
+        !CommandLine::ForCurrentProcess()->HasSwitch(switches::kSingleProcess);
 
 #if defined(OS_POSIX)
     // TODO(port): Don't use a utility process on linux (crbug.com/22703) or
