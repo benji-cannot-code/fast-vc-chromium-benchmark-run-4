70d2ae2e89db8c13ace9415eecba768eb3ebd4e0
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-28 22:54:47 +0000

    Move a Linux only #include into the right place.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/341026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30402 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/histogram.h"
 #include "base/lazy_instance.h"
-#include "base/linux_util.h"
 #include "base/scoped_nsautorelease_pool.h"
 #include "base/path_service.h"
 #include "base/process_util.h"
@@ -76,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(USE_LINUX_BREAKPAD)
+#include "base/linux_util.h"
 #include "chrome/app/breakpad_linux.h"
 #endif
 
