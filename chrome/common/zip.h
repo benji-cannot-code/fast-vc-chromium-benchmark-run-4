886b27a0e946f58680507cbe0eefb9dbe3e585ea
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-28 02:30:43 +0000

    Filter out hidden files, both when loading extensions and when packaging them.
    We also special case the common OS X zip dropping "__MACOSX" when
    loading extensions.
    
    BUG=23004
    
    Review URL: http://codereview.chromium.org/340018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30312 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/zip.h
+++ chrome/common/zip.h
@@ -13,7 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Zip the contents of src_dir into dest_file. src_path must be a directory.
 // An entry will *not* be created in the zip for the root folder -- children
 // of src_dir will be at the root level of the created zip.
-bool Zip(const FilePath& src_dir, const FilePath& dest_file);
+// If |include_hidden_files| is true, files starting with "." are included.
+// Otherwise they are omitted.
+bool Zip(const FilePath& src_dir, const FilePath& dest_file,
+         bool include_hidden_files);
 
 // Unzip the contents of zip_file into dest_dir.
 bool Unzip(const FilePath& zip_file, const FilePath& dest_dir);
