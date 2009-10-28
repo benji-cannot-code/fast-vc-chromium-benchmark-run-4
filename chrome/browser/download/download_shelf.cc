c5a16acec8874a5fa7b240c0062bbf0bb3536907
rolandsteiner@chromium.org <rolandsteiner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-28 05:40:09 +0000

    Fix for bug 10876 that resulted in some refactoring:
    The bug originates from extensions being treated case sensitive on Windows and Mac OSX, where they shouldn't be. 
    
    Therefore I added generic static methods to FilePath to compare strings in the same way the file system does, and changed the relevant parts of the code to make use of them.
    I tested the methods under Windows and Mac OS X. I also wrote a basic version for Linux/Posix that behaves the same way as the original code, so there should at least be no regression.
    
    Also, while fixing this I found some confusion in the code about whether extensions are used with or without leading dot. For this reason I changed some functions that were taking an extension as parameter to instead take the whole file path. This makes calling these functions easier and the caller doesn't need to know whether the extension is supposed to be with or without dot.
    
    In the same vein, I split DownloadManager::IsExecutable into IsExecutableFile, where one again passes in the whole file and doesn't have to worry about getting the extension right, and IsExecutableExtension, which corresponds to the original functionality. Ideally only the former method should be public, but that again would have required further code scrubbing that was (even more) outside of the original bug fix.
    
    Finally, fixed a wrong comment in the file path tests.
    
    
    BUG=10876
    TEST=FilePathTest.MatchesExtension, .CompareIgnoreCase
    
    Review URL: http://codereview.chromium.org/149796
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30323 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_shelf.cc
+++ chrome/browser/download/download_shelf.cc
@@ -33,9 +33,8 @@ bool DownloadShelfContextMenu::ItemIsChecked(int id) const {
       return download_->open_when_complete();
     }
     case ALWAYS_OPEN_TYPE: {
-      const FilePath::StringType extension =
-          file_util::GetFileExtensionFromPath(download_->full_path());
-      return download_->manager()->ShouldOpenFileExtension(extension);
+      return download_->manager()->ShouldOpenFileBasedOnExtension(
+          download_->full_path());
     }
     case TOGGLE_PAUSE: {
       return download_->is_paused();
@@ -97,10 +96,8 @@ void DownloadShelfContextMenu::ExecuteItemCommand(int id) {
       download_util::OpenDownload(download_);
       break;
     case ALWAYS_OPEN_TYPE: {
-      const FilePath::StringType extension =
-          file_util::GetFileExtensionFromPath(download_->full_path());
-      download_->manager()->OpenFilesOfExtension(
-          extension, !ItemIsChecked(ALWAYS_OPEN_TYPE));
+      download_->manager()->OpenFilesBasedOnExtension(
+          download_->full_path(), !ItemIsChecked(ALWAYS_OPEN_TYPE));
       break;
     }
     case CANCEL:
