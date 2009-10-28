cab2bdd5bed69c7546daaa7d6d78aa9ea8ac97a5
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-28 10:16:27 +0000

    [Qt] Custom Cursor doesn't use hotspot.
    Cursors loaded from a QPixmap didn't specifiy the
    hotspot. Take the hotspot values from the mac
    implementation.
    
    * platform/qt/CursorQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/CursorQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/CursorQt.cpp
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Copyright (C) 2006 Dirk Mueller <mueller@kde.org>
  * Copyright (C) 2006 George Staikos <staikos@kde.org>
  * Copyright (C) 2006 Charles Samuels <charles@kde.org>
- * Copyright (C) 2008 Holger Hans Peter Freyther
+ * Copyright (C) 2008, 2009 Holger Hans Peter Freyther
  *
  * All rights reserved.
  *
@@ -93,14 +93,14 @@ protected:
         , SplitVCursor(Qt::SplitVCursor)
         , NoDropCursor(Qt::ForbiddenCursor)
         , BlankCursor(Qt::BlankCursor)
-        , ZoomInCursor(QPixmap(QLatin1String(":/webkit/resources/zoomInCursor.png")))
-        , ZoomOutCursor(QPixmap(QLatin1String(":/webkit/resources/zoomOutCursor.png")))
-        , VerticalTextCursor(QPixmap(QLatin1String(":/webkit/resources/verticalTextCursor.png")))
-        , CellCursor(QPixmap(QLatin1String(":/webkit/resources/cellCursor.png")))
-        , ContextMenuCursor(QPixmap(QLatin1String(":/webkit/resources/contextMenuCursor.png")))
-        , CopyCursor(QPixmap(QLatin1String(":/webkit/resources/copyCursor.png")))
-        , ProgressCursor(QPixmap(QLatin1String(":/webkit/resources/progressCursor.png")))
-        , AliasCursor(QPixmap(QLatin1String(":/webkit/resources/aliasCursor.png")))
+        , ZoomInCursor(QCursor(QPixmap(QLatin1String(":/webkit/resources/zoomInCursor.png")), 7, 7))
+        , ZoomOutCursor(QCursor(QPixmap(QLatin1String(":/webkit/resources/zoomOutCursor.png")), 7, 7))
+        , VerticalTextCursor(QCursor(QPixmap(QLatin1String(":/webkit/resources/verticalTextCursor.png")), 7, 7))
+        , CellCursor(QCursor(QPixmap(QLatin1String(":/webkit/resources/cellCursor.png")), 7, 7))
+        , ContextMenuCursor(QCursor(QPixmap(QLatin1String(":/webkit/resources/contextMenuCursor.png")), 3, 2))
+        , CopyCursor(QCursor(QPixmap(QLatin1String(":/webkit/resources/copyCursor.png")), 3, 2))
+        , ProgressCursor(QCursor(QPixmap(QLatin1String(":/webkit/resources/progressCursor.png")), 3, 2))
+        , AliasCursor(QCursor(QPixmap(QLatin1String(":/webkit/resources/aliasCursor.png")), 11, 3))
 
 #endif
     {
