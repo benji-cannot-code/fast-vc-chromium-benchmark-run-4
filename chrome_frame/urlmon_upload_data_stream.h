642a355b8112e85277618a01f1e4be903819c3a7
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-29 03:57:23 +0000

    Using thread safe reference counting to wrap the UploadData instance.Moving the post_data_len_ member variable to the PluginUrlRequest class.TEST=This is a tentative fix for bug 25531.BUG=25531 Review URL: http://codereview.chromium.org/340006
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30432 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/urlmon_upload_data_stream.h
+++ chrome_frame/urlmon_upload_data_stream.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Provides an IStream interface to the very different UploadDataStream
 // implementation.
-class UrlmonUploadDataStream : public CComObjectRoot,
+class UrlmonUploadDataStream : public CComObjectRootEx<CComMultiThreadModel>,
                                public IStream {
  public:
   UrlmonUploadDataStream() {}
