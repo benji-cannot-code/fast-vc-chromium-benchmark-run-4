672e84a714516fdf5e08a8e9847bb327d5538188
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-29 16:13:29 +0000

    DevTools:  Not all resources are displayed in Resources tab BUG=26056
    Review URL: http://codereview.chromium.org/342036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30458 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webdevtoolsagent_impl.cc
+++ webkit/glue/webdevtoolsagent_impl.cc
@@ -166,8 +166,8 @@ void WebDevToolsAgentImpl::DidCommitLoadForFrame(
     tools_agent_delegate_stub_->FrameNavigate(
         webkit_glue::WebURLToKURL(url).string());
     SetApuAgentEnabledInUtilityContext(utility_context_, apu_agent_enabled_);
+    UnhideResourcesPanelIfNecessary();
   }
-  UnhideResourcesPanelIfNecessary();
 }
 
 void WebDevToolsAgentImpl::WindowObjectCleared(WebFrameImpl* webframe) {
