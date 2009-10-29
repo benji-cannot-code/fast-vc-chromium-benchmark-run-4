7df2895769ee1a4ad98c8fedaa7d7a39c4b4e87e
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-29 22:25:36 +0000

    Some new tab ui fixes.
    1) Always intialize the css cache.  Previously we were only
    initializing if not in incognito mode.  If the first NTP was an
    incognito page, it wouldn't be styled.
    
    2) Small optimization in generating the HTML to avoid calling
    ReplaceFirstSubstringAfterOffset which would need to move the
    bytes after the placeholder.
    
    3) Only generate the css for the incognito or normal mode.  Since
    the NTP only needs one, we only need to generate one.
    
    BUG=26228
    
    Review URL: http://codereview.chromium.org/344027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30527 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/dom_ui_theme_source.cc
+++ chrome/browser/dom_ui/dom_ui_theme_source.cc
@@ -54,8 +54,10 @@ static std::string StripQueryParams(const std::string& path) {
 DOMUIThemeSource::DOMUIThemeSource(Profile* profile)
     : DataSource(chrome::kChromeUIThemePath, MessageLoop::current()),
       profile_(profile) {
-  InitNewTabCSS();
-  InitNewIncognitoTabCSS();
+  if (profile->IsOffTheRecord())
+    InitNewIncognitoTabCSS();
+  else
+    InitNewTabCSS();
 }
 
 void DOMUIThemeSource::StartDataRequest(const std::string& path,
