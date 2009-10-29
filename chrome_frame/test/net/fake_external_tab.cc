582eb486498cba4f286fa7238e9b73db5ca9faea
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-29 20:12:40 +0000

    Instantiate a dummy notification service instance in chrome frame on the thread where the network tests run. Some of the url request tests rely on having a valid notification service pointer in the TLS on the current thread.
    TBR=tommi
    
    Review URL: http://codereview.chromium.org/344025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30502 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/net/fake_external_tab.cc
+++ chrome_frame/test/net/fake_external_tab.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_paths_internal.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/notification_service.h"
 #include "chrome/common/pref_names.h"
 
 #include "chrome_frame/utils.h"
@@ -298,6 +299,8 @@ void CFUrlRequestUnittestRunner::StartTests() {
 // static
 DWORD CFUrlRequestUnittestRunner::RunAllUnittests(void* param) {
   PlatformThread::SetName("CFUrlRequestUnittestRunner");
+  // Needed for some url request tests like the intercept job tests, etc.
+  NotificationService service;
   CFUrlRequestUnittestRunner* me =
       reinterpret_cast<CFUrlRequestUnittestRunner*>(param);
   me->Run();
