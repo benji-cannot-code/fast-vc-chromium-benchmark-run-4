c4900d6843affb1f1fd8d3c27570ce72c9f20330
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-29 23:47:40 +0000

    Change the extension.onRequest even to include the sender info.
    BUG=23865
    
    Review URL: http://codereview.chromium.org/341025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30534 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/renderer_extension_bindings.js
+++ chrome/renderer/resources/renderer_extension_bindings.js
@@ -74,6 +74,10 @@ var chrome = chrome || {};
     // the right event.
     var isExternal = sourceExtensionId != chromeHidden.extensionId;
 
+    if (tab)
+      tab = JSON.parse(tab);
+    var sender = {tab: tab, id: sourceExtensionId};
+
     // Special case for sendRequest/onRequest.
     if (channelName == chromeHidden.kRequestChannel) {
       var requestEvent = (isExternal ?
@@ -81,7 +85,7 @@ var chrome = chrome || {};
       if (requestEvent.hasListeners()) {
         var port = chromeHidden.Port.createPort(portId, channelName);
         port.onMessage.addListener(function(request) {
-          requestEvent.dispatch(request, function(response) {
+          requestEvent.dispatch(request, sender, function(response) {
             port.postMessage(response);
           });
         });
@@ -93,10 +97,7 @@ var chrome = chrome || {};
         chrome.extension.onConnectExternal : chrome.extension.onConnect);
     if (connectEvent.hasListeners()) {
       var port = chromeHidden.Port.createPort(portId, channelName);
-      if (tab) {
-        tab = JSON.parse(tab);
-      }
-      port.sender = {tab: tab, id: sourceExtensionId};
+      port.sender = sender;
       // TODO(EXTENSIONS_DEPRECATED): port.tab is obsolete.
       port.tab = port.sender.tab;
 
