946b7cb4b86b488964c5eb516d3107d726b34ec5
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-29 17:51:36 +0000

    Adds UI components for desktop notifications, including balloon view classes to display toasts on the screen, and manager for controlling the layout of the balloons.
    BUG=none
    TEST=none yet (part of larger patch)
    
    Review URL: http://codereview.chromium.org/338051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30471 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/notifications/balloon_collection_mac.mm
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/notifications/balloon_collection.h"
+
+#include "base/gfx/size.h"
+#include "base/logging.h"
+
+Balloon* BalloonCollectionImpl::MakeBalloon(const Notification& notification,
+                                            Profile* profile) {
+  // TODO(johnnyg): http://crbug.com/23066.  Part of future Mac support.
+  NOTIMPLEMENTED();
+  return NULL;
+}
+
+bool BalloonCollectionImpl::Layout::RefreshSystemMetrics() {
+  // TODO(johnnyg): http://crbug.com/23066.  Part of future Mac support.
+  NOTIMPLEMENTED();
+  return false;
+}
+
