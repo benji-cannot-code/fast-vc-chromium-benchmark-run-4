5b21b5d584067d53ef72666d7ce18aeb32d13de4
zoltan@webkit.org <zoltan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-29 15:38:27 +0000

    Allow custom memory allocation control for WebCore's AXObjectCache https://bugs.webkit.org/show_bug.cgi?id=30848
    Reviewed by Darin Adler.
    
    Inherits AXObjectCache class from FastAllocBase because it is
    instantiated by 'new' in WebCore/dom/Document.cpp:1537 and                                
    it is no need to be copyable.
    
    * accessibility/AXObjectCache.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50272 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AXObjectCache.h
+++ third_party/WebKit/WebCore/accessibility/AXObjectCache.h
@@ -56,7 +56,7 @@ namespace WebCore {
         EAffinity affinity;
     };
 
-    class AXObjectCache {
+    class AXObjectCache : public Noncopyable {
     public:
         AXObjectCache();
         ~AXObjectCache();
