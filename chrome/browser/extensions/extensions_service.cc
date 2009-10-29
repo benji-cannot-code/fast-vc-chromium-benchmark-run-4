b0ac3cc1dbb6c0b74a2a5732b733033f58fa12e5
cira@chromium.org <cira@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-29 21:47:40 +0000

    - Removes fallback from Prefs to LoadExtension on UI thread. - Fix extension_service_unittest to actually load extensions from prefs.
    BUG=19733
    TEST=Remove extension manifest from Preferences file. Extension shouldn't load even if the necessary files are present.
    Review URL: http://codereview.chromium.org/339058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30521 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service.cc
+++ chrome/browser/extensions/extensions_service.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/chrome_url_request_context.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension.h"
+#include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/extensions/extension_error_reporter.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/pref_names.h"
@@ -281,11 +282,7 @@ void ExtensionsService::LoadInstalledExtension(
       extension = tmp.release();
     }
   } else {
-    // TODO(mpcomplete): obsolete. remove after migration period.
-    // http://code.google.com/p/chromium/issues/detail?id=19733
-    extension = extension_file_util::LoadExtension(path,
-                                                   true,  // Require id
-                                                   &error);
+    error = extension_manifest_errors::kManifestUnreadable;
   }
 
   if (!extension) {
