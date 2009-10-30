133ee8104f27fd334167fabe40b8afd4333ddfc1
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-30 04:33:27 +0000

    Fix build bustage.
    TBR=yaar
    
    Review URL: http://codereview.chromium.org/340043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30559 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/src/InspectorClientImpl.cpp
+++ webkit/api/src/InspectorClientImpl.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DOMWindow.h"
 #include "FloatRect.h"
 #include "InspectorController.h"
+#include "NotImplemented.h"
 #include "Page.h"
 #include "Settings.h"
 #include "WebRect.h"
