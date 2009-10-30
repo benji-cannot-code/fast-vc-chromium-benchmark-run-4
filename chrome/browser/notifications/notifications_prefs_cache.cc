f3ecf81ca11cae8dd4501286e43e50c493b8d846
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-30 20:00:48 +0000

    Need to load the permissions lists every time at startup for notifications.
    BUG=26274
    TEST=grant notifications permissions and restart browser
    
    Review URL: http://codereview.chromium.org/339077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30623 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/notifications_prefs_cache.cc
+++ chrome/browser/notifications/notifications_prefs_cache.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/api/public/WebNotificationPresenter.h"
 
 NotificationsPrefsCache::NotificationsPrefsCache(
-    ListValue* allowed, ListValue* denied) {
+    const ListValue* allowed, const ListValue* denied) {
   ListValue::const_iterator i;
   std::wstring origin;
   if (allowed) {
