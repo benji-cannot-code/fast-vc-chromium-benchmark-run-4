adc53a83349832e4dc9d8e6c82f2bccb50970236
zoltan@webkit.org <zoltan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-30 22:59:07 +0000

    Allow custom memory allocation control for WebCore's CSSNamespace https://bugs.webkit.org/show_bug.cgi?id=30849
    Reviewed by Darin Adler.
    
    Inherits CSSNamespace struct from Noncopyable because it is
    instantiated by 'new' in WebCore/css/CSSStyleSheet.cpp:141 and
    it is no need to be copyable.
    
    * css/CSSNamespace.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50363 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSNamespace.h
+++ third_party/WebKit/WebCore/css/CSSNamespace.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    struct CSSNamespace {
+    struct CSSNamespace : Noncopyable {
         AtomicString m_prefix;
         AtomicString m_uri;
         CSSNamespace* m_parent;
