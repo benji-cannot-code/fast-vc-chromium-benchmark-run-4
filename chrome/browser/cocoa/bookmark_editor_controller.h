67f5c8d04cd7bc6fed7be463d92ad86878a4adff
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-30 17:32:22 +0000

    BookmarkEditorController needs to implement NSMatrixDelegate for the 10.6 SDK build.
    BUG=arthropod
    TEST=build with mac_sdk=10.6
    Review URL: http://codereview.chromium.org/345026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30591 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_editor_controller.h
+++ chrome/browser/cocoa/bookmark_editor_controller.h
@@ -17,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // A controller for the bookmark editor, opened with Edit... from the
 // context menu of a bookmark button.
-@interface BookmarkEditorController : NSWindowController<NSTextFieldDelegate> {
+@interface BookmarkEditorController : NSWindowController<NSMatrixDelegate,
+                                                         NSTextFieldDelegate> {
  @private
   IBOutlet NSTextField* nameField_;
   IBOutlet NSTextField* urlField_;
