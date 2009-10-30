86b1c2525f46891a67f5868bab2417f1372d72a5
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-30 07:13:38 +0000

    REGRESSION (r50218-r50262): E*TRADE accounts page is missing content https://bugs.webkit.org/show_bug.cgi?id=30947 <rdar://problem/7348833>
    Reviewed by Maciej Stachowiak
    
    The logic for flagging that a structure has non-enumerable properties
    was in addPropertyWithoutTransition, rather than in the core Structure::put
    method.  Despite this I was unable to produce a testcase that caused
    the failure that etrade was experiencing, but the new assertion in
    getEnumerablePropertyNames triggers on numerous layout tests without
    the fix, so in effecti all for..in enumeration in any test ends up
    doing the required consistency check.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/Structure.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/Structure.cpp
@@ -557,8 +557,6 @@ size_t Structure::addPropertyWithoutTransition(const Identifier& propertyName, u
     materializePropertyMapIfNecessary();
 
     m_isPinnedPropertyTable = true;
-    if (attributes & DontEnum)
-        m_hasNonEnumerableProperties = true;
 
     size_t offset = put(propertyName, attributes, specificValue);
     if (propertyStorageSize() > propertyStorageCapacity())
@@ -739,6 +737,9 @@ size_t Structure::put(const Identifier& propertyName, unsigned attributes, JSCel
     ASSERT(get(propertyName) == notFound);
 
     checkConsistency();
+
+    if (attributes & DontEnum)
+        m_hasNonEnumerableProperties = true;
 
     UString::Rep* rep = propertyName._ustring.rep();
 
@@ -1026,6 +1027,7 @@ void Structure::getEnumerablePropertyNames(PropertyNameArray& propertyNames)
         int i = 0;
         unsigned entryCount = m_propertyTable->keyCount + m_propertyTable->deletedSentinelCount;
         for (unsigned k = 1; k <= entryCount; k++) {
+            ASSERT(m_hasNonEnumerableProperties || !(m_propertyTable->entries()[k].attributes & DontEnum));
             if (m_propertyTable->entries()[k].key && !(m_propertyTable->entries()[k].attributes & DontEnum)) {
                 PropertyMapEntry* value = &m_propertyTable->entries()[k];
                 int j;
@@ -1113,6 +1115,7 @@ void Structure::checkConsistency()
 
     unsigned nonEmptyEntryCount = 0;
     for (unsigned c = 1; c <= m_propertyTable->keyCount + m_propertyTable->deletedSentinelCount; ++c) {
+        ASSERT(m_hasNonEnumerableProperties || !(m_propertyTable->entries()[c].attributes & DontEnum));
         UString::Rep* rep = m_propertyTable->entries()[c].key;
         if (!rep)
             continue;
