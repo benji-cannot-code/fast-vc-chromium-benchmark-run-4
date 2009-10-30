1527176388870b604a935594d38cfcea142dae79
chase@chromium.org <chase@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-30 01:22:06 +0000

    Re-enable the warm perf load test.
    This is an experiment to see the current status of
    the test.
    
    BUG=13921
    TEST=warm perf load test is greenish
    
    Review URL: http://codereview.chromium.org/342045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30545 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/startup/feature_startup_test.cc
+++ chrome/test/startup/feature_startup_test.cc
@@ -164,7 +164,7 @@ TEST_F(NewTabUIStartupTest, PerfCold) {
                  UITest::DEFAULT_THEME);
 }
 
-TEST_F(NewTabUIStartupTest, DISABLED_PerfWarm) {
+TEST_F(NewTabUIStartupTest, PerfWarm) {
   RunStartupTest("tab_warm", true /* warm */, false /* not important */,
                  UITest::DEFAULT_THEME);
 }
