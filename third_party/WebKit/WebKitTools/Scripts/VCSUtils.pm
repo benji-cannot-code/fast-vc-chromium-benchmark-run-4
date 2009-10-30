eaefe33640a173e95ddf67a82f1fe2f2e84a677a
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-10-30 06:12:02 +0000

    Fix “Undefined subroutine” errors in svn-*apply by moving the removeEOL subroutine from the two scripts that define it but don’t use it to the script that uses it but doesn’t define it.
    Reviewed by Mark Rowe.
    
    * Scripts/VCSUtils.pm:
    * Scripts/svn-apply:
    * Scripts/svn-unapply:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/VCSUtils.pm
+++ third_party/WebKit/WebKitTools/Scripts/VCSUtils.pm
@@ -299,6 +299,14 @@ sub canonicalizePath($)
     return ($#dirs >= 0) ? File::Spec->catdir(@dirs) : ".";
 }
 
+sub removeEOL($)
+{
+    my ($line) = @_;
+
+    $line =~ s/[\r\n]+$//g;
+    return $line;
+}
+
 sub svnStatus($)
 {
     my ($fullPath) = @_;
