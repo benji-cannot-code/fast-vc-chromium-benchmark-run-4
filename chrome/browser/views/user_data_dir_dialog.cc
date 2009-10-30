6e6ebb2a224ce9cf306f69269468cbfb3af879ac
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-30 16:16:19 +0000

    Use FilePath in UserDataDirDialog to remove the use of FilePath::FromWStringHack.
    Patch by tfarina.
    
    BUG=None
    TEST=None 
    
    Review URL: http://codereview.chromium.org/334017
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30584 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/user_data_dir_dialog.cc
+++ chrome/browser/views/user_data_dir_dialog.cc
@@ -14,20 +14,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/window/window.h"
 
 // static
-std::wstring UserDataDirDialog::RunUserDataDirDialog(
-    const std::wstring& user_data_dir) {
+FilePath UserDataDirDialog::RunUserDataDirDialog(
+    const FilePath& user_data_dir) {
   // When the window closes, it will delete itself.
   UserDataDirDialog* dlg = new UserDataDirDialog(user_data_dir);
   MessageLoopForUI::current()->Run(dlg);
   return dlg->user_data_dir();
 }
 
-UserDataDirDialog::UserDataDirDialog(const std::wstring& user_data_dir)
+UserDataDirDialog::UserDataDirDialog(const FilePath& user_data_dir)
     : ALLOW_THIS_IN_INITIALIZER_LIST(
           select_file_dialog_(SelectFileDialog::Create(this))),
       is_blocking_(true) {
   std::wstring message_text = l10n_util::GetStringF(
-      IDS_CANT_WRITE_USER_DIRECTORY_SUMMARY, user_data_dir);
+      IDS_CANT_WRITE_USER_DIRECTORY_SUMMARY, user_data_dir.ToWStringHack());
   const int kDialogWidth = 400;
   message_box_view_ = new MessageBoxView(MessageBoxFlags::kIsConfirmMessageBox,
       message_text.c_str(), std::wstring(), kDialogWidth);
@@ -92,7 +92,7 @@ bool UserDataDirDialog::Dispatch(const MSG& msg) {
 
 void UserDataDirDialog::FileSelected(const FilePath& path,
                                      int index, void* params) {
-  user_data_dir_ = path.ToWStringHack();
+  user_data_dir_ = path;
   is_blocking_ = false;
 }
 
