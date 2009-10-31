388b14598f91b3b18ad591f0392ea02e5455cc09
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-31 03:19:15 +0000

    Update: I removed TYPE_ANY to see if I could fool the builders into letting this pass without crashing in browser_tests. It makes the code uglier, but I want to debug and figure out what's going on.
    Rework the way the FindBrowserWithProfile/Type methods work.
    
    We now always walk the last active list backwards rather than consulting the last active then walking the registered browser list forwards.
    
    This ensures that when the last active browser is a popup or app frame the last active TYPE_NORMAL browser is located when opening a new tab.
    
    http://crbug.com/17498
    TEST=Open an app frame. Open a browser window (Ctrl+N) and load a page. Minimize it. Open another browser window and minimize it. Activate the app frame. Press Ctrl+T. The second browser window should be restored and have a new tab added to it rather than the first.
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=30531
    Review URL: http://codereview.chromium.org/330013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30659 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -1617,7 +1617,8 @@ void Browser::DuplicateContentsAt(int index) {
   } else {
     Browser* browser = NULL;
     if (type_ & TYPE_APP) {
-      browser = Browser::CreateForApp(app_name_, profile_, type_ & TYPE_POPUP);
+      browser = Browser::CreateForApp(app_name_, profile_,
+                                      !!(type_ & TYPE_POPUP));
     } else if (type_ == TYPE_POPUP) {
       browser = Browser::CreateForPopup(profile_);
     }
@@ -1974,7 +1975,7 @@ void Browser::DetachContents(TabContents* source) {
 
 bool Browser::IsPopup(TabContents* source) {
   // A non-tabbed BROWSER is an unconstrained popup.
-  return (type() & TYPE_POPUP);
+  return !!(type() & TYPE_POPUP);
 }
 
 void Browser::ToolbarSizeChanged(TabContents* source, bool is_animating) {
