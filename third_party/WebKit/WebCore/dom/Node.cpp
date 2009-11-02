6a7f733fd24587c89828dd02614f46456c51bc6c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-02 22:02:33 +0000

    2009-11-02  Kelly Norton  <knorton@google.com>
            Reviewed by Timothy Hatcher.
    
            Adds a missed case for InspectorTimeline, DOMWindow dispatch of DOM events.
            https://bugs.webkit.org/show_bug.cgi?id=31030
    
            * dom/Node.cpp:
            (WebCore::eventHasListeners): Checks DOMWindow for listeners.
            (WebCore::Node::dispatchGenericEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50428 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Node.cpp
+++ third_party/WebKit/WebCore/dom/Node.cpp
@@ -2447,8 +2447,11 @@ bool Node::dispatchEvent(PassRefPtr<Event> prpEvent)
     return dispatchGenericEvent(event.release());
 }
 
-static bool eventHasListeners(const AtomicString& eventType, Node* node, Vector<RefPtr<ContainerNode> >& ancestors)
+static bool eventHasListeners(const AtomicString& eventType, DOMWindow* window, Node* node, Vector<RefPtr<ContainerNode> >& ancestors)
 {
+    if (window && window->hasEventListeners(eventType))
+        return true;
+
     if (node->hasEventListeners(eventType))
         return true;
 
@@ -2476,13 +2479,6 @@ bool Node::dispatchGenericEvent(PassRefPtr<Event> prpEvent)
     Vector<RefPtr<ContainerNode> > ancestors;
     eventAncestors(ancestors);
 
-#if ENABLE(INSPECTOR)
-    InspectorTimelineAgent* timelineAgent = document()->inspectorTimelineAgent();
-    bool timelineAgentIsActive = timelineAgent && eventHasListeners(event->type(), this, ancestors);    
-    if (timelineAgentIsActive)
-        timelineAgent->willDispatchEvent(*event);
-#endif
-
     // Set up a pointer to indicate whether / where to dispatch window events.
     // We don't dispatch load events to the window. That quirk was originally
     // added because Mozilla doesn't propagate load events to the window object.
@@ -2492,6 +2488,13 @@ bool Node::dispatchGenericEvent(PassRefPtr<Event> prpEvent)
         if (topLevelContainer->isDocumentNode())
             targetForWindowEvents = static_cast<Document*>(topLevelContainer)->domWindow();
     }
+
+#if ENABLE(INSPECTOR)
+    InspectorTimelineAgent* timelineAgent = document()->inspectorTimelineAgent();
+    bool timelineAgentIsActive = timelineAgent && eventHasListeners(event->type(), targetForWindowEvents, this, ancestors);
+    if (timelineAgentIsActive)
+        timelineAgent->willDispatchEvent(*event);
+#endif
 
     // Give the target node a chance to do some work before DOM event handlers get a crack.
     void* data = preDispatchEventHandler(event.get());
