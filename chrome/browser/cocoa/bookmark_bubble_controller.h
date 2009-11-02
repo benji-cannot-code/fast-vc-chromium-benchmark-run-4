14a3bc062f3510a8b20da37a4fd27691a2804a2a
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-02 23:02:29 +0000

    bookmark STAR bubble: Disambiguate folders with the same name
    BUG=http://crbug.com/19408
    
    TEST=Create a bookmark.
    Create 2 bookmark folders both with the same name, "foo".
    Go to your bookmarked page.
    Click STAR to get bookmark bubble.
    Change parent folder to the 1st "foo".
    Confirm it's there on the bar.
    Click STAR to get bookmark bubble.
    Change parent folder to the 2nd "foo".
    Confirm it's there on the bar.
    Click STAR to get bookmark bubble.
    Chose "choose another folder" to be sure that logic still works.
    
    Review URL: http://codereview.chromium.org/340042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30762 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bubble_controller.h
+++ chrome/browser/cocoa/bookmark_bubble_controller.h
@@ -33,8 +33,8 @@ class BookmarkNode;
   BookmarkModel* model_;  // weak
   const BookmarkNode* node_;  // weak
 
-  // A mapping from titles to nodes so we only have to walk this once.
-  scoped_nsobject<NSMutableArray> titleMapping_;
+  // A mapping from NSComboBox index to parent nodes.
+  scoped_nsobject<NSMutableArray> parentMapping_;
 
   BOOL alreadyBookmarked_;
   scoped_nsobject<NSString> chooseAnotherFolder_;
@@ -72,6 +72,7 @@ class BookmarkNode;
 - (void)addFolderNodes:(const BookmarkNode*)parent toComboBox:(NSComboBox*)box;
 - (void)updateBookmarkNode;
 - (void)setTitle:(NSString*)title parentFolder:(NSString*)folder;
+- (void)setParentFolderSelection:(const BookmarkNode*)parent;
 - (NSString*)chooseAnotherFolderString;
 - (NSComboBox*)folderComboBox;
 @end
