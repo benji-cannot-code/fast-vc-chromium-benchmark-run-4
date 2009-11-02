65b43820440356d3d5d7f7b7df5238658b093e53
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-02 18:17:18 +0000

    2009-11-02  Jocelyn Turcotte  <jocelyn.turcotte@nokia.com>
            Reviewed by Tor Arne Vestbø.
    
            [Qt] Fix Qt build on Windows.
            https://bugs.webkit.org/show_bug.cgi?id=30905
    
            * WebCore.pro:
            * platform/graphics/BitmapImage.h:
            * platform/graphics/qt/ImageQt.cpp:
            (WebCore::BitmapImage::BitmapImage):
            (WebCore::BitmapImage::create):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50416 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/BitmapImage.h
+++ third_party/WebKit/WebCore/platform/graphics/BitmapImage.h
@@ -141,8 +141,10 @@ public:
     virtual CGImageRef getCGImageRef();
 #endif
 
-#if PLATFORM(WIN)
+#if PLATFORM(WIN) || (PLATFORM(QT) && PLATFORM(WIN_OS))
     static PassRefPtr<BitmapImage> create(HBITMAP);
+#endif
+#if PLATFORM(WIN)
     virtual bool getHBITMAP(HBITMAP);
     virtual bool getHBITMAPOfSize(HBITMAP, LPSIZE);
 #endif
