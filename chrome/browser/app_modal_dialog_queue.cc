6c443dd3b25b577ffac3bf997442fed903b413e9
pam@chromium.org <pam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-02 23:27:15 +0000

    Set active_dialog_ before showing it, since showing a dialog can sometimes actually move to the next one in the queue instead.
    BUG=26398
    TEST=Make sure alert/confirm boxes work properly. Make sure a background
        tab that shows a (delayed) alert box works. Same with a background
        browser. Especially make sure http://crbug.com/10699 doesn't recur.
    Review URL: http://codereview.chromium.org/339094
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30766 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/app_modal_dialog_queue.cc
+++ chrome/browser/app_modal_dialog_queue.cc
@@ -31,6 +31,8 @@ void AppModalDialogQueue::ActivateModalDialog() {
 }
 
 void AppModalDialogQueue::ShowModalDialog(AppModalDialog* dialog) {
-  dialog->ShowModalDialog();
+  // Set active_dialog_ before showing it, because ShowModalDialog can wind up
+  // calling ShowNextDialog in some cases, which will change active_dialog_.
   active_dialog_ = dialog;
+  dialog->ShowModalDialog();
 }
