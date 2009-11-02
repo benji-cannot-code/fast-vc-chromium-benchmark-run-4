4edbe96b0f42e48a16cd392b7a8c7661ae262afe
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-02 17:03:14 +0000

    WebKit/mac: WebKit part of making the appearance of the full-screen video HUD match QuickTime Player X’s HUD.
    Reviewed by John Sullivan.
    
    * WebView/WebVideoFullscreenHUDWindowController.h: Removed unnecessary
    #import statements, cleaned up style, and changed _timeline,
    _volumeSlider and _playButton to have more specific types.
    * WebView/WebVideoFullscreenHUDWindowController.mm: Updated #import
    statements.
    (webkit_CGFloor): Added this helper function.
    (-[WebVideoFullscreenHUDWindowController init]): Cleaned up style.
    (createTimeTextField): Changed to use the bold system font.
    (-[WebVideoFullscreenHUDWindowController windowDidLoad]): Changed the
    subviews’ metrics and the text fields’ text alignment.
    (-[WebVideoFullscreenHUDWindowController updateTime]): Avoid conversion
    from double to float.
    (stringToTimeTextAttributed): Removed this useless function that
    returned an NSAttributedString masquerading as an NSString.
    (-[WebVideoFullscreenHUDWindowController remainingTimeText]): Removed
    call to stringToTimeTextAttributed().
    (-[WebVideoFullscreenHUDWindowController elapsedTimeText]): Ditto.
    
    WebKitLibraries: WebKitSystemInterface part of making the appearance of the full-screen video HUD match
    QuickTime Player X’s HUD.
    
    Reviewed by John Sullivan.
    
    * libWebKitSystemInterfaceLeopard.a:
    * libWebKitSystemInterfaceSnowLeopard.a:
    * libWebKitSystemInterfaceTiger.a:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebVideoFullscreenHUDWindowController.h
+++ third_party/WebKit/WebKit/mac/WebView/WebVideoFullscreenHUDWindowController.h
@@ -26,13 +26,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(VIDEO)
 
-#import <Cocoa/Cocoa.h>
-#import <WebCore/HTMLMediaElement.h>
+namespace WebCore {
+    class HTMLMediaElement;
+}
 
 @protocol WebVideoFullscreenHUDWindowControllerDelegate;
 
-@interface WebVideoFullscreenHUDWindowController : NSWindowController {
-@private
+@interface WebVideoFullscreenHUDWindowController : NSWindowController
+{
     id<WebVideoFullscreenHUDWindowControllerDelegate> _delegate;
     NSTimer *_timelineUpdateTimer;
 #if !defined(BUILDING_ON_TIGER)
@@ -41,12 +42,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     BOOL _mouseIsInHUD;
     BOOL _isEndingFullscreen;
 
-    NSControl *_timeline;
+    NSSlider *_timeline;
     NSTextField *_remainingTimeText;
     NSTextField *_elapsedTimeText;
-    NSControl *_volumeSlider;
-    NSControl *_playButton;
+    NSSlider *_volumeSlider;
+    NSButton *_playButton;
 }
+
 - (id<WebVideoFullscreenHUDWindowControllerDelegate>)delegate;
 - (void)setDelegate:(id<WebVideoFullscreenHUDWindowControllerDelegate>)delegate;
 - (void)fadeWindowIn;
