73b90e52a164fb5e6c693dcc295dfad991852a69
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-03 02:07:51 +0000

    Fix for <rdar://problem/7038305> REGRESSION (Safari 4.0.2 - ToT): After navigating back to a known phishy page, the "Ignore warning" button appears highlighted (along with the "Go Back" button)
    Reviewed by Darin Adler.
    
    This bug is timing dependent, and not always reproducible.  I could not think of a way to add a 
    layout test that would demonstrate the problem and fix.
    
    * platform/mac/ThemeMac.mm:
    (WebCore::checkbox): Update style.
    (WebCore::paintCheckbox): ditto.
    (WebCore::radio): ditto.
    (WebCore::paintRadio): ditto.
    (WebCore::setupButtonCell): Added convenience method.
    (WebCore::button): Use a separate NSButtonCell for defaultButtons and regular buttons.
    (WebCore::paintButton): Don't check for the key window here.  Consider that when deciding if the button should have the default style in RenderTheme.
    * rendering/RenderTheme.cpp: (WebCore::RenderTheme::isDefault): Only consider a button to be default if the page is active.  This fixes 
    a problem I noticed where the button would flicker crazily if the page with the default button was in the background.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50436 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTheme.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTheme.cpp
@@ -702,6 +702,10 @@ bool RenderTheme::isHovered(const RenderObject* o) const
 
 bool RenderTheme::isDefault(const RenderObject* o) const
 {
+    // A button should only have the default appearance if the page is active
+    if (!isActive(o))
+        return false;
+
     if (!o->document())
         return false;
 
