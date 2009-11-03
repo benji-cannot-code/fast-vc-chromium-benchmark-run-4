1b1537c8923b054b993b23ab2e37911ae647ff15
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-03 19:24:56 +0000

    2009-11-03  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: update timeline content boundaries on timer.
    
            https://bugs.webkit.org/show_bug.cgi?id=31072
    
            * inspector/front-end/TimelinePanel.js:
            (WebInspector.TimelinePanel.prototype.refresh):
            (WebInspector.TimelinePanel.prototype._setWindowPosition):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50471 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/TimelinePanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/TimelinePanel.js
@@ -279,6 +279,7 @@ WebInspector.TimelinePanel.prototype = {
     refresh: function()
     {
         WebInspector.AbstractTimelinePanel.prototype.refresh.call(this);
+        this.adjustScrollPosition();
 
         // Clear summary bars.
         var timelines = {};
@@ -399,7 +400,7 @@ WebInspector.TimelinePanel.prototype = {
             this._rightResizeElement.style.left = this.calculator.windowRight*100 + "%";
         }
         this._overviewWindowElement.style.width = (this.calculator.windowRight - this.calculator.windowLeft)*100 + "%";
-        this.refresh();
+        this.needsRefresh = true;
     },
 
     _endWindowDragging: function(event)
