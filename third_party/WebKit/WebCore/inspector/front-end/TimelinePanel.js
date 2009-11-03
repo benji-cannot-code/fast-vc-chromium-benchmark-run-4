6b2a204b978bebd4b2e53e8802f3b146df63e20d
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-03 19:06:02 +0000

    2009-11-03  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: only show timeline records that contribute
            to the current window.
    
            https://bugs.webkit.org/show_bug.cgi?id=31069
    
            * inspector/front-end/TimelinePanel.js:
            (WebInspector.TimelinePanel.prototype.refresh):
            (WebInspector.TimelineGraph):
            (WebInspector.TimelineGraph.prototype.refresh):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/TimelinePanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/TimelinePanel.js
@@ -279,7 +279,6 @@ WebInspector.TimelinePanel.prototype = {
     refresh: function()
     {
         WebInspector.AbstractTimelinePanel.prototype.refresh.call(this);
-        this._overviewGrid.updateDividers(true, this._overviewCalculator);
 
         // Clear summary bars.
         var timelines = {};
@@ -320,6 +319,7 @@ WebInspector.TimelinePanel.prototype = {
                 chunkStart = -1;
             }
         }
+        this._overviewGrid.updateDividers(true, this._overviewCalculator);
     },
 
     _resizeWindow: function(resizeElement, event)
@@ -611,7 +611,7 @@ WebInspector.TimelineGraph = function(record)
     this._graphElement.className = "timeline-graph-side";
 
     this._barAreaElement = document.createElement("div");
-    this._barAreaElement.className = "timeline-graph-bar-area hidden";
+    this._barAreaElement.className = "timeline-graph-bar-area";
     this._graphElement.appendChild(this._barAreaElement);
 
     this._barElement = document.createElement("div");
@@ -638,11 +638,15 @@ WebInspector.TimelineGraph.prototype = {
 
         this._percentages = percentages;
 
-        this._barAreaElement.removeStyleClass("hidden");
-
-        this._barElement.style.setProperty("left", percentages.start + "%");
-        this._barElement.style.setProperty("right", (100 - percentages.end) + "%");
-
+        if (percentages.start > 100 || percentages.end < 0) {
+            this._graphElement.addStyleClass("hidden");
+            this.record._itemTreeElement.listItemElement.addStyleClass("hidden");
+        } else {
+            this._barElement.style.setProperty("left", percentages.start + "%");
+            this._barElement.style.setProperty("right", (100 - percentages.end) + "%");
+            this._graphElement.removeStyleClass("hidden");
+            this.record._itemTreeElement.listItemElement.removeStyleClass("hidden");
+        }
         var tooltip = (labels.tooltip || "");
         this._barElement.title = tooltip;
     }
