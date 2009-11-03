f186debb7582b5684be68639fd0e27f4cb9b42a9
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-03 14:48:25 +0000

    2009-11-03  Keishi Hattori  <casey.hattori@gmail.com>
            Reviewed by Pavel Feldman.
    
            Web Inspector: monitorEvent should be monitorEvents
            https://bugs.webkit.org/show_bug.cgi?id=31042
    
            * inspector/front-end/InjectedScript.js:
            (InjectedScript._ensureCommandLineAPIInstalled):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/InjectedScript.js
+++ third_party/WebKit/WebCore/inspector/front-end/InjectedScript.js
@@ -944,7 +944,7 @@ InjectedScript._ensureCommandLineAPIInstalled = function(evalFunction, evalObjec
             } \
             return te; \
         }, \
-        monitorEvent: function(o, t) { \
+        monitorEvents: function(o, t) { \
             if (!o || !o.addEventListener || !o.removeEventListener) \
                 return; \
             t = console._inspectorCommandLineAPI._normalizeEventTypes(t); \
@@ -953,7 +953,7 @@ InjectedScript._ensureCommandLineAPIInstalled = function(evalFunction, evalObjec
                 o.addEventListener(t[i], console._inspectorCommandLineAPI._logEvent, false); \
             } \
         }, \
-        unmonitorEvent: function(o, t) { \
+        unmonitorEvents: function(o, t) { \
             if (!o || !o.removeEventListener) \
                 return; \
             t = console._inspectorCommandLineAPI._normalizeEventTypes(t); \
