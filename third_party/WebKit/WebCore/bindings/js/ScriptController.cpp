2a3bd57406217c502971c62bb873bef311d137b0
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-03 05:51:59 +0000

    2009-11-02  Darin Fisher  <darin@chromium.org>
            Fixing JSC build bustage.
    
            * bindings/js/ScriptController.cpp: Added missing #include
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/ScriptController.cpp
+++ third_party/WebKit/WebCore/bindings/js/ScriptController.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Event.h"
 #include "EventNames.h"
 #include "Frame.h"
+#include "FrameLoaderClient.h"
 #include "GCController.h"
 #include "HTMLPlugInElement.h"
 #include "InspectorTimelineAgent.h"
