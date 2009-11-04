f2db4ee359fe06b4f4f376e714b365274b3f550d
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-04 19:49:51 +0000

    Fix for flaky browser tests as done by twiz and reviewed here: http://codereview.chromium.org/346044
    BUG=26599
    TEST=extension_popup_apitest.cc 
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30982 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_popup_apitest.cc
+++ chrome/browser/extensions/extension_popup_apitest.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_switches.h"
 
-// This test has been disabled because it was crashing 
-// browsertests. crbug.com/26599
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Popup) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Popup) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
   ASSERT_TRUE(RunExtensionTest("popup_api")) << message_;
