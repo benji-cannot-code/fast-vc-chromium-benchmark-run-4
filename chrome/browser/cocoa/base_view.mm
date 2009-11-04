30388daa410b7cb9bd98ce1bed9d13a1d6dbac60
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-04 19:54:17 +0000

    Back out r30033 which broke drag scrolling in web pages until we have a better solution. BUG=22199 TEST=dragging to scroll in web pages should again work. Review URL: http://codereview.chromium.org/367001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30983 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/base_view.mm
+++ chrome/browser/cocoa/base_view.mm
@@ -12,8 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (self) {
     trackingArea_ =
         [[NSTrackingArea alloc] initWithRect:frame
-                                     options:NSTrackingMouseEnteredAndExited |
-                                             NSTrackingMouseMoved |
+                                     options:NSTrackingMouseMoved |
                                              NSTrackingActiveInActiveApp |
                                              NSTrackingInVisibleRect
                                        owner:self
