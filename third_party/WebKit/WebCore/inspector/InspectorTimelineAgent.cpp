ecbdc7843f6b1d2a9d9249a4f9ccea0087654e71
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-04 17:17:14 +0000

    2009-11-04  Kelly Norton  <knorton@google.com>
            Reviewed by Pavel Feldman.
    
            Adds lightweight network resources to InspectorTimelineAgent.
            https://bugs.webkit.org/show_bug.cgi?id=31065
    
            * inspector/InspectorController.cpp:
            (WebCore::InspectorController::willSendRequest):
            (WebCore::InspectorController::didReceiveResponse):
            (WebCore::InspectorController::didFinishLoading):
            (WebCore::InspectorController::didFailLoading):
            * inspector/InspectorTimelineAgent.cpp:
            (WebCore::InspectorTimelineAgent::willSendResourceRequest):
            (WebCore::InspectorTimelineAgent::didReceiveResourceResponse):
            (WebCore::InspectorTimelineAgent::didFinishLoadingResource):
            * inspector/InspectorTimelineAgent.h:
            (WebCore::):
            * inspector/TimelineRecordFactory.cpp:
            (WebCore::TimelineRecordFactory::createResourceSendRequestTimelineRecord):
            (WebCore::TimelineRecordFactory::createResourceReceiveResponseTimelineRecord):
            (WebCore::TimelineRecordFactory::createResourceFinishTimelineRecord):
            * inspector/TimelineRecordFactory.h:
            * inspector/front-end/TimelineAgent.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50515 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorTimelineAgent.cpp
+++ third_party/WebKit/WebCore/inspector/InspectorTimelineAgent.cpp
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Event.h"
 #include "InspectorFrontend.h"
 #include "IntRect.h"
+#include "ResourceRequest.h"
+#include "ResourceResponse.h"
 #include "TimelineRecordFactory.h"
 
 #include <wtf/CurrentTime.h>
@@ -157,6 +159,31 @@ void InspectorTimelineAgent::willEvaluateScript(const String& url, int lineNumbe
 void InspectorTimelineAgent::didEvaluateScript()
 {
     didCompleteCurrentRecord(EvaluateScriptTimelineRecordType);
+}
+
+void InspectorTimelineAgent::willSendResourceRequest(unsigned long identifier, bool isMainResource,
+    const ResourceRequest& request)
+{
+    ScriptObject record = TimelineRecordFactory::createResourceSendRequestTimelineRecord(m_frontend, currentTimeInMilliseconds(),
+        identifier, isMainResource, request);
+    record.set("type", ResourceSendRequestTimelineRecordType);
+    m_frontend->addRecordToTimeline(record);
+}
+
+void InspectorTimelineAgent::didReceiveResourceResponse(unsigned long identifier, const ResourceResponse& response)
+{
+    ScriptObject record = TimelineRecordFactory::createResourceReceiveResponseTimelineRecord(m_frontend, currentTimeInMilliseconds(),
+        identifier, response);
+    record.set("type", ResourceReceiveResponseTimelineRecordType);
+    m_frontend->addRecordToTimeline(record);
+}
+
+void InspectorTimelineAgent::didFinishLoadingResource(unsigned long identifier, bool didFail)
+{
+    ScriptObject record = TimelineRecordFactory::createResourceFinishTimelineRecord(m_frontend, currentTimeInMilliseconds(),
+        identifier, didFail);
+    record.set("type", ResourceFinishTimelineRecordType);
+    m_frontend->addRecordToTimeline(record);
 }
 
 void InspectorTimelineAgent::didMarkTimeline(const String& message)
--- third_party/WebKit/WebCore/inspector/InspectorTimelineAgent.h
+++ third_party/WebKit/WebCore/inspector/InspectorTimelineAgent.h
@@ -42,6 +42,8 @@ namespace WebCore {
     class Event;
     class InspectorFrontend;
     class IntRect;
+    class ResourceRequest;
+    class ResourceResponse;
 
     // Must be kept in sync with TimelineAgent.js
     enum TimelineRecordType {
@@ -57,6 +59,9 @@ namespace WebCore {
         XHRLoadRecordType = 9,
         EvaluateScriptTimelineRecordType = 10,
         MarkTimelineRecordType = 11,
+        ResourceSendRequestTimelineRecordType = 12,
+        ResourceReceiveResponseTimelineRecordType = 13,
+        ResourceFinishTimelineRecordType = 14,
     };
 
     class InspectorTimelineAgent {
@@ -97,6 +102,10 @@ namespace WebCore {
         void didEvaluateScript();
 
         void didMarkTimeline(const String&);
+
+        void willSendResourceRequest(unsigned long, bool isMainResource, const ResourceRequest&);
+        void didReceiveResourceResponse(unsigned long, const ResourceResponse&);
+        void didFinishLoadingResource(unsigned long, bool didFail);
 
         static InspectorTimelineAgent* retrieve(ScriptExecutionContext*);
     private:
