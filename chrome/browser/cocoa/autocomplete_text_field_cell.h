572cef64359912d7ff93e533dce6f3c23f719527
pamg@google.com <pamg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-04 22:37:11 +0000

    Restoring r29827. The skia file is new to this attempt, and may be what was causing a unit test to crash last time.
    Refactor security-icon code to a more general form, also more consistent with
    the Windows implementation, in preparation for implementing page actions.
    
    Review URL: http://codereview.chromium.org/264037
    
    BUG=14899, 22922, 12281
    TEST=unit tests included
    Review URL: http://codereview.chromium.org/348069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31023 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/autocomplete_text_field_cell.h
+++ chrome/browser/cocoa/autocomplete_text_field_cell.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "base/scoped_nsobject.h"
+#include "chrome/browser/cocoa/location_bar_view_mac.h"
 
 // AutocompleteTextFieldCell customizes the look of the Omnibox text
 // field.  The border and focus ring are modified, as is the font
@@ -27,13 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // side of the field.  Exclusive WRT |keywordString_|;
   scoped_nsobject<NSAttributedString> hintString_;
 
-  // Icon that represents the state of the SSL connection
-  scoped_nsobject<NSImage> hintIcon_;
-
-  // Optional text that appears to the right of the hint icon which
-  // appears only alongside the icon (i.e., it's possible to display a
-  // hintIcon without an hintIconLabel, but not vice-versa).
-  scoped_nsobject<NSAttributedString> hintIconLabel_;
+  // View showing the state of the SSL connection. Owned by the location bar.
+  // Display is exclusive WRT the |hintString_| and |keywordString_|.
+  // This may be NULL during testing.
+  LocationBarViewMac::SecurityImageView* security_image_view_;
 }
 
 // Chooses |partialString| if |width| won't fit |fullString|.  Strings
@@ -55,9 +53,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
              availableWidth:(CGFloat)width;
 - (void)clearKeywordAndHint;
 
-// Sets the hint icon and optional icon label. If |icon| is nil, the current
-// icon is cleared. If |label| is provided, |color| must be provided as well.
-- (void)setHintIcon:(NSImage*)icon label:(NSString*)label color:(NSColor*)color;
+- (void)setSecurityImageView:(LocationBarViewMac::SecurityImageView*)view;
+
+// Called when the security icon is visible and clicked. Passed through to the
+// security_image_view_ to handle the click (i.e., show the page info dialog).
+- (void)onSecurityIconMousePressed;
 
 // Return the portion of the cell to show the text cursor over.
 - (NSRect)textCursorFrameForFrame:(NSRect)cellFrame;
@@ -66,8 +66,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // corresponds to the frame with our added decorations sliced off.
 - (NSRect)textFrameForFrame:(NSRect)cellFrame;
 
-// Return the portion of the cell to use for displaing the |hintIcon_|.
-- (NSRect)hintImageFrameForFrame:(NSRect)cellFrame;
+// Return the portion of the cell to use for displaying the security (SSL lock)
+// icon, leaving space for its label if any.
+- (NSRect)securityImageFrameForFrame:(NSRect)cellFrame;
 
 @end
 
@@ -76,7 +77,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @property(readonly) NSAttributedString* keywordString;
 @property(readonly) NSAttributedString* hintString;
-@property(readonly) NSImage* hintIcon;
 @property(readonly) NSAttributedString* hintIconLabel;
 
 @end
