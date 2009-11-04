5ff5ac99389b2bfff3ff855a7c64f7600c72ca30
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-04 21:00:11 +0000

    2009-11-04  Benjamin Otte  <otte@gnome.org>
            Reviewed by Gustavo Noronha.
    
            Update Cairo requirement to 1.6.
    
            https://bugs.webkit.org/show_bug.cgi?id=19266
    
            * configure.ac:
    2009-11-04  Benjamin Otte  <otte@gnome.org>
    
            Reviewed by Gustavo Noronha.
    
            Update Cairo requirement to 1.6.
    
            Also remove all conditional code and workarounds for older versions of
            Cairo.
            In particular, gain image quality by removing the use of
            CAIRO_FILTER_NEAREST when rendering images and use the default
            bilinear filter instead.
            https://bugs.webkit.org/show_bug.cgi?id=19266
    
            * platform/graphics/cairo/GraphicsContextCairo.cpp:
            (WebCore::GraphicsContext::clipOut):
            * platform/graphics/cairo/ImageCairo.cpp:
            (WebCore::BitmapImage::draw):
            (WebCore::BitmapImage::drawPattern):
            * platform/graphics/cairo/PathCairo.cpp:
            (WebCore::Path::isEmpty):
            (WebCore::Path::boundingRect):
            * platform/gtk/RenderThemeGtk.cpp:
            (WebCore::paintMozWidget):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cairo/GraphicsContextCairo.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cairo/GraphicsContextCairo.cpp
@@ -942,7 +942,6 @@ void GraphicsContext::clipOut(const Path& path)
     if (paintingDisabled())
         return;
 
-#if CAIRO_VERSION >= CAIRO_VERSION_ENCODE(1,4,0)
     cairo_t* cr = m_data->cr;
     double x1, y1, x2, y2;
     cairo_clip_extents(cr, &x1, &y1, &x2, &y2);
@@ -953,9 +952,6 @@ void GraphicsContext::clipOut(const Path& path)
     cairo_set_fill_rule(cr, CAIRO_FILL_RULE_EVEN_ODD);
     cairo_clip(cr);
     cairo_set_fill_rule(cr, savedFillRule);
-#else
-    notImplemented();
-#endif
 }
 
 void GraphicsContext::rotate(float radians)
@@ -981,7 +977,6 @@ void GraphicsContext::clipOut(const IntRect& r)
     if (paintingDisabled())
         return;
 
-#if CAIRO_VERSION >= CAIRO_VERSION_ENCODE(1,4,0)
     cairo_t* cr = m_data->cr;
     double x1, y1, x2, y2;
     cairo_clip_extents(cr, &x1, &y1, &x2, &y2);
@@ -991,9 +986,6 @@ void GraphicsContext::clipOut(const IntRect& r)
     cairo_set_fill_rule(cr, CAIRO_FILL_RULE_EVEN_ODD);
     cairo_clip(cr);
     cairo_set_fill_rule(cr, savedFillRule);
-#else
-    notImplemented();
-#endif
 }
 
 void GraphicsContext::clipOutEllipseInRect(const IntRect& r)
