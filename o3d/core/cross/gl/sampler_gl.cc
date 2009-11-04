4842e2513194563b115124c9b42045e1c933261e
gman@google.com <gman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-04 23:36:14 +0000

    GL fix. Code was DCHECKing valid conditions and therefore asserting in debug mode.
    Review URL: http://codereview.chromium.org/360033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31038 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/gl/sampler_gl.cc
+++ o3d/core/cross/gl/sampler_gl.cc
@@ -77,6 +77,8 @@ unsigned int GLAddressMode(Sampler::AddressMode o3d_mode,
 unsigned int GLMinFilter(Sampler::FilterType o3d_filter,
                          Sampler::FilterType mip_filter) {
   switch (o3d_filter) {
+    case Sampler::NONE:
+      return GL_NEAREST;
     case Sampler::POINT:
       if (mip_filter == Sampler::NONE)
         return GL_NEAREST;
@@ -84,9 +86,8 @@ unsigned int GLMinFilter(Sampler::FilterType o3d_filter,
         return GL_NEAREST_MIPMAP_NEAREST;
       else if (mip_filter == Sampler::LINEAR)
         return GL_NEAREST_MIPMAP_LINEAR;
-    default:
-      DLOG(ERROR) << "Unknown filter " << static_cast<int>(o3d_filter);
-      // fall through
+      else if (mip_filter == Sampler::ANISOTROPIC)
+        return GL_NEAREST_MIPMAP_LINEAR;
     case Sampler::ANISOTROPIC:  // Anisotropy is handled in SetTextureAndStates
     case Sampler::LINEAR:
       if (mip_filter == Sampler::NONE)
@@ -95,16 +96,22 @@ unsigned int GLMinFilter(Sampler::FilterType o3d_filter,
         return GL_LINEAR_MIPMAP_NEAREST;
       else if (mip_filter == Sampler::LINEAR)
         return GL_LINEAR_MIPMAP_LINEAR;
+      else if (mip_filter == Sampler::ANISOTROPIC)
+        return GL_LINEAR_MIPMAP_LINEAR;
   }
+  // fall through
+  DLOG(ERROR) << "Unknown filter " << static_cast<int>(o3d_filter);
   DCHECK(false);
   return GL_NONE;
 }
 
 unsigned int GLMagFilter(Sampler::FilterType o3d_filter) {
   switch (o3d_filter) {
+    case Sampler::NONE:
     case Sampler::POINT:
       return GL_NEAREST;
     case Sampler::LINEAR:
+    case Sampler::ANISOTROPIC:
       return GL_LINEAR;
     default:
       DLOG(ERROR) << "Unknown filter " << static_cast<int>(o3d_filter);
