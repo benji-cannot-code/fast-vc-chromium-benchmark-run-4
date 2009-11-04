df6a6510f9f1fd4f79271e606295249a43c5d428
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-04 01:25:52 +0000

    2009-11-03  Kenneth Russell  <kbr@google.com>
            Reviewed by Oliver Hunt.
    
            Add overridePreference call to all WebGL layout tests
            https://bugs.webkit.org/show_bug.cgi?id=31088
    
            * fast/canvas/webgl/array-unit-tests.html:
            * fast/canvas/webgl/getActiveTest.html:
            * fast/canvas/webgl/gl-get-calls.html:
            * fast/canvas/webgl/incorrect-context-object-behaviour.html:
            * fast/canvas/webgl/null-object-behaviour.html:
            * fast/canvas/webgl/resources/shader-test.js: Removed.
            * fast/canvas/webgl/resources/webgl-test.js: Copied from LayoutTests/fast/canvas/webgl/resources/shader-test.js.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50492 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/canvas/webgl/resources/webgl-test.js
+++ third_party/WebKit/LayoutTests/fast/canvas/webgl/resources/webgl-test.js
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (window.layoutTestController)
+    layoutTestController.overridePreference("WebKitWebGLEnabled", "1");
+
 function getShaderSource(file) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", file, false);
