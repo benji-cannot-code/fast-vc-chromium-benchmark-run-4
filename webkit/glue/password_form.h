d39ca3e9f4e888dc454f31935a327031811f6f37
yaar@chromium.org <yaar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-05 17:51:25 +0000

    Move PasswordFormDomManager::CreatePassword into the WebKit API http://code.google.com/p/chromium/issues/detail?id=24619
    Review URL: http://codereview.chromium.org/345056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31104 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/password_form.h
+++ webkit/glue/password_form.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time.h"
 #include "googleurl/src/gurl.h"
+#include "webkit/api/public/WebPasswordFormData.h"
 
 namespace webkit_glue {
 
@@ -137,6 +138,23 @@ struct PasswordForm {
 
   PasswordForm()
       : scheme(SCHEME_HTML),
+        ssl_valid(false),
+        preferred(false),
+        blacklisted_by_user(false) {
+  }
+
+  PasswordForm(const WebKit::WebPasswordFormData& web_password_form)
+      : scheme(SCHEME_HTML),
+        signon_realm(web_password_form.signonRealm.utf8()),
+        origin(web_password_form.origin),
+        action(web_password_form.action),
+        submit_element(web_password_form.submitElement),
+        username_element(web_password_form.userNameElement),
+        username_value(web_password_form.userNameValue),
+        password_element(web_password_form.passwordElement),
+        password_value(web_password_form.passwordValue),
+        old_password_element(web_password_form.oldPasswordElement),
+        old_password_value(web_password_form.oldPasswordValue),
         ssl_valid(false),
         preferred(false),
         blacklisted_by_user(false) {
