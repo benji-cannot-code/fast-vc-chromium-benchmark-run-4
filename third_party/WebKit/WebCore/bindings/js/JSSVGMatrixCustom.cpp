90a6432d3f2ff07a21ffc1b0feb9902db4ab2485
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-05 15:28:37 +0000

    2009-11-05  Jeff Schiller  <codedread@gmail.com>
            Reviewed by Simon Fraser.
    
            Added test to ensure proper order of matrix multiplication with SVGMatrix.
            https://bugs.webkit.org/show_bug.cgi?id=16062
    
            * svg/dom/SVGMatrix-interface-expected.txt: Added.
            * svg/dom/SVGMatrix-interface.xhtml: Added.
    2009-11-05  Jeff Schiller  <codedread@gmail.com>
    
            Reviewed by Simon Fraser.
    
            Correct order of matrix multiplication for SVGMatrix.
            https://bugs.webkit.org/show_bug.cgi?id=16062
    
            Test: svg/dom/SVGMatrix-interface.xhtml
    
            * bindings/js/JSSVGMatrixCustom.cpp:
            (WebCore::JSSVGMatrix::multiply):
            * svg/SVGMatrix.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSSVGMatrixCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSSVGMatrixCustom.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006, 2007, 2008 Nikolas Zimmermann <zimmermann@kde.org>
+ * Copyright (C) 2009 Jeff Schiller <codedread@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -25,10 +26,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "TransformationMatrix.h"
 #include "SVGException.h"
+#include <runtime/Error.h>
 
 using namespace JSC;
 
 namespace WebCore {
+
+JSValue JSSVGMatrix::multiply(ExecState* exec, const ArgList& args)
+{
+    if (args.size() < 1)
+        return throwError(exec, SyntaxError, "Not enough arguments");
+
+    if (!args.at(0).inherits(&JSSVGMatrix::s_info))
+        return throwError(exec, TypeError, "secondMatrix argument was not a SVGMatrix");
+
+    JSSVGMatrix* matrixObj = static_cast<JSSVGMatrix*>(asObject(args.at(0)));
+
+    TransformationMatrix m1(*impl());
+    TransformationMatrix m2(*(matrixObj->impl()));
+
+    JSC::JSValue result = toJS(exec, deprecatedGlobalObjectForPrototype(exec), JSSVGStaticPODTypeWrapper<TransformationMatrix>::create(m1.multLeft(m2)).get(), m_context.get());
+
+    return result;
+}
 
 JSValue JSSVGMatrix::inverse(ExecState* exec, const ArgList&)
 {
