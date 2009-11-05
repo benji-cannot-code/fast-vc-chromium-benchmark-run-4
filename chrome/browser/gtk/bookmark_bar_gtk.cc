6a8c9428629bf5ee807c2f03f567b2d1c1830488
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-05 01:03:53 +0000

    Fix a bug where we were over compensating for the height of the bookmarks bar during an animation.  What we want is the "extra" height provided by the bookmarks bar.  Since we always show at least 4px, we don't count that as extra height.
    Review URL: http://codereview.chromium.org/366019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31051 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/bookmark_bar_gtk.cc
+++ chrome/browser/gtk/bookmark_bar_gtk.cc
@@ -336,7 +336,7 @@ void BookmarkBarGtk::EnterFullscreen() {
 }
 
 int BookmarkBarGtk::GetHeight() {
-  return event_box_->allocation.height;
+  return event_box_->allocation.height - kBookmarkBarMinimumHeight;
 }
 
 bool BookmarkBarGtk::IsAnimating() {
