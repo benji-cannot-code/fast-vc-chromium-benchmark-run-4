46d47be98ab96ff52aa70b46c584bb7a0868102c
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-05 11:24:15 +0000

    DevTools: Add port method stub: avoid devtools breakage on deps roll. TBR=mnaganov
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31086 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/inspector_controller.js
+++ webkit/glue/devtools/js/inspector_controller.js
@@ -517,4 +517,8 @@ devtools.InspectorController.prototype.setting =
 };
 
 
+devtools.InspectorController.prototype.port = function() {
+  return "undefined";
+};
+
 var InspectorController = new devtools.InspectorController();
