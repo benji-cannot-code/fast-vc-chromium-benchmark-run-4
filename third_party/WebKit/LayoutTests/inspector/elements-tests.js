31e08b702a75a57c885d5469a4d7bc8a5436206e
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-05 23:28:42 +0000

    2009-11-05  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: beautify layout tests.
    
            https://bugs.webkit.org/show_bug.cgi?id=31189
    
            * inspector/console-dir-expected.txt:
            * inspector/console-dir.html:
            * inspector/console-dirxml-expected.txt:
            * inspector/console-dirxml.html:
            * inspector/console-format-collections-expected.txt:
            * inspector/console-format-collections.html:
            * inspector/console-format-expected.txt:
            * inspector/console-format.html:
            * inspector/console-tests-expected.txt:
            * inspector/console-tests.html:
            * inspector/console-tests.js: Added.
            (dumpConsoleMessages.callback):
            (dumpConsoleMessages):
            (dumpConsoleMessagesWithClasses.callback):
            (dumpConsoleMessagesWithClasses):
            (frontendScript_dumpConsoleMessages):
            * inspector/css-syntax-highlight.html: Removed.
            * inspector/elements-panel-structure-expected.txt:
            * inspector/elements-panel-structure.html:
            * inspector/elements-tests.js: Added.
            (frontendScript_expandDOMSubtree.processChildren):
            (frontendScript_expandDOMSubtree):
            * inspector/evaluate-in-frontend.html:
            * inspector/evaluate-in-frontend.js: Removed.
            * inspector/inspector-test.js: Added.
            ():
            (evaluateInWebInspector):
            (notifyDone):
            (output):
            (window.didEvaluateForTestInFrontend):
            * inspector/javascript-syntax-highlight.html: Removed.
            * inspector/styles-iframe.html:
            * inspector/syntax-highlight-css-expected.txt: Renamed from LayoutTests/inspector/css-syntax-highlight-expected.txt.
            * inspector/syntax-highlight-css.html: Added.
            * inspector/syntax-highlight-javascript-expected.txt: Renamed from LayoutTests/inspector/javascript-syntax-highlight-expected.txt.
            * inspector/syntax-highlight-javascript.html: Added.
            * inspector/syntax-highlight.js: Added.
            (doit.callback):
            (doit):
            (frontendScript_dumpSyntaxHighlight):
            * inspector/timeline-trivial.html:
            * platform/qt/Skipped:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50582 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/inspector/elements-tests.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Frontend functions.
+
+function frontend_expandDOMSubtree(node)
+{
+    function processChildren(children)
+    {
+       for (var i = 0; children && i < children.length; ++i)
+           frontend_expandDOMSubtree(children[i]);
+    }
+    WebInspector.domAgent.getChildNodesAsync(node, processChildren);
+}
---
+++ third_party/WebKit/LayoutTests/inspector/syntax-highlight.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Inspected Page functions.
+
+function doit()
+{
+    function callback(result)
+    {
+        for (var i = 0; i < result.length; ++i)
+            output(result[i]);
+        notifyDone();
+    }
+    evaluateInWebInspector("frontend_doitAndDump", callback);
+}
+
+// Frontend functions.
+
+function frontend_dumpSyntaxHighlight(str, highlighterConstructor)
+{
+    var node = document.createElement("span");
+    node.textContent = str;
+    var javascriptSyntaxHighlighter = new highlighterConstructor(null, null);
+    javascriptSyntaxHighlighter.syntaxHighlightNode(node);
+    var node_parts = [];
+    for (var i = 0; i < node.childNodes.length; i++) {
+        if (node.childNodes[i].getAttribute) {
+            node_parts.push(node.childNodes[i].getAttribute("class"));
+        } else {
+            node_parts.push("*");
+        }
+    }
+    return node_parts.join(",");
+}
