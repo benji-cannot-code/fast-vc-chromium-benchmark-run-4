f57d03a055b1da5f7f10ecb024beb41432832362
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-05 08:11:48 +0000

    Added QGraphicsWebView::modified property, for consistency with QWebView.
    Patch by Simon Hausmann <hausmann@webkit.org> on 2009-11-04
    Reviewed by Kenneth Christiansen.
    
    * Api/qgraphicswebview.cpp:
    (QGraphicsWebView::isModified):
    * Api/qgraphicswebview.h:
    * tests/qgraphicswebview/tst_qgraphicswebview.cpp:
    (tst_QGraphicsWebView::qgraphicswebview):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qgraphicswebview.h
+++ third_party/WebKit/WebKit/qt/Api/qgraphicswebview.h
@@ -47,6 +47,8 @@ class QWEBKIT_EXPORT QGraphicsWebView : public QGraphicsWidget {
 
     Q_PROPERTY(bool interactive READ isInteractive WRITE setInteractive NOTIFY interactivityChanged)
 
+    Q_PROPERTY(bool modified READ isModified)
+
 public:
     QGraphicsWebView(QGraphicsItem* parent = 0);
     ~QGraphicsWebView();
@@ -65,6 +67,8 @@ public:
 
     bool isInteractive() const;
     void setInteractive(bool);
+
+    bool isModified() const;
 
     void load(const QUrl &url);
     void load(const QNetworkRequest& request, QNetworkAccessManager::Operation operation = QNetworkAccessManager::GetOperation, const QByteArray& body = QByteArray());
--- third_party/WebKit/WebKit/qt/tests/qgraphicswebview/tst_qgraphicswebview.cpp
+++ third_party/WebKit/WebKit/qt/tests/qgraphicswebview/tst_qgraphicswebview.cpp
@@ -77,6 +77,7 @@ void tst_QGraphicsWebView::qgraphicswebview()
     item.load(QUrl());
     item.setHtml(QString());
     item.setContent(QByteArray());
+    item.isModified();
 }
 
 class WebPage : public QWebPage
