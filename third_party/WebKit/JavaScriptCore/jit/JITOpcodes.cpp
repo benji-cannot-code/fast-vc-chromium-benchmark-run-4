f2a66d5470d79911462719fe0120ec104b69b914
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-05 03:29:28 +0000

    Rubber Stamped by Oliver Hunt.
    Patch by Gavin Barraclough <barraclough@apple.com> on 2009-11-04
    Enable native call optimizations on ARMv7.  (Existing ARM_TRADITIONAL
    implementation was generic, worked perfectly, just needed turning on).
    
    * jit/JITOpcodes.cpp:
    * wtf/Platform.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50540 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/jit/JITOpcodes.cpp
+++ third_party/WebKit/JavaScriptCore/jit/JITOpcodes.cpp
@@ -1757,7 +1757,7 @@ void JIT::privateCompileCTIMachineTrampolines(RefPtr<ExecutablePool>* executable
     // so pull them off now
     addPtr(Imm32(NativeCallFrameSize - sizeof(NativeFunctionCalleeSignature)), stackPointerRegister);
 
-#elif PLATFORM(ARM_TRADITIONAL)
+#elif PLATFORM(ARM)
     emitGetFromCallFrameHeader32(RegisterFile::ArgumentCount, regT0);
 
     // Allocate stack space for our arglist
@@ -1791,7 +1791,7 @@ void JIT::privateCompileCTIMachineTrampolines(RefPtr<ExecutablePool>* executable
     move(callFrameRegister, regT0);
 
     // Setup arg4: This is a plain hack
-    move(stackPointerRegister, ARMRegisters::S0);
+    move(stackPointerRegister, ARMRegisters::r3);
 
     call(Address(regT1, OBJECT_OFFSETOF(JSFunction, m_data)));
 
