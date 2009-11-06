971f57f3dac12f103e8940d3fdd9e072d7481983
zork@google.com <zork@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-06 17:29:33 +0000

    Fix the mediator thread to check for a valid xmpp client before trying to process commands.  This fixes the problem where the auto-reconnect posts a login command just after the client connects, just before a listen for updates command is posted.
    BUG=24245
    TEST=Send a CMD_LISTEN_FOR_UPDATES immediately after the CMD_LOGIN in
    MediatorThreadImpl::Login()  Chromium will no longer crash.
    
    Review URL: http://codereview.chromium.org/372011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31242 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/notifier/listener/mediator_thread_impl.cc
+++ chrome/browser/sync/notifier/listener/mediator_thread_impl.cc
@@ -206,7 +206,12 @@ void MediatorThreadImpl::DoDisconnect() {
 }
 
 void MediatorThreadImpl::DoSubscribeForUpdates() {
-  SubscribeTask* subscription = new SubscribeTask(xmpp_client());
+  buzz::XmppClient* client = xmpp_client();
+  // If there isn't an active xmpp client, return.
+  if (!client) {
+    return;
+  }
+  SubscribeTask* subscription = new SubscribeTask(client);
   subscription->SignalStatusUpdate.connect(
       this,
       &MediatorThreadImpl::OnSubscriptionStateChange);
@@ -214,7 +219,12 @@ void MediatorThreadImpl::DoSubscribeForUpdates() {
 }
 
 void MediatorThreadImpl::DoListenForUpdates() {
-  ListenTask* listener = new ListenTask(xmpp_client());
+  buzz::XmppClient* client = xmpp_client();
+  // If there isn't an active xmpp client, return.
+  if (!client) {
+    return;
+  }
+  ListenTask* listener = new ListenTask(client);
   listener->SignalUpdateAvailable.connect(
       this,
       &MediatorThreadImpl::OnUpdateListenerMessage);
@@ -222,7 +232,12 @@ void MediatorThreadImpl::DoListenForUpdates() {
 }
 
 void MediatorThreadImpl::DoSendNotification() {
-  SendUpdateTask* task = new SendUpdateTask(xmpp_client());
+  buzz::XmppClient* client = xmpp_client();
+  // If there isn't an active xmpp client, return.
+  if (!client) {
+    return;
+  }
+  SendUpdateTask* task = new SendUpdateTask(client);
   task->SignalStatusUpdate.connect(
       this,
       &MediatorThreadImpl::OnUpdateNotificationSent);
