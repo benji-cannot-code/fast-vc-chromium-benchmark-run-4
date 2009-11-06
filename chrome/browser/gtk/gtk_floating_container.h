a537449dec9f9324e053dd8e62e1b1d4251993c7
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-06 21:09:57 +0000

    GTK: Change find bar to use a floating container rather than some hacked up gtkfixed business.
    The straw that broke this camel's back was when I realized that showing the findbar in fullscreen mode would inescapably mean an extra 1 pixel border at the top.
    
    Also fix z-ordering problems for findbar that have been around since I introduced the floating bookmark bar (the fact that no one filed a bug on this suggests that it's not that noticeable).
    
    BUG=none
    TEST=find bar still works. find bar works in fullscreen. the toolbar bottom border still exists at all times. there are no z-ordering problems. the find bar still gets out of way of find results.
    
    Review URL: http://codereview.chromium.org/373007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31296 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/gtk_floating_container.h
+++ chrome/browser/gtk/gtk_floating_container.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // that interface. The GtkBin portion contains normal content and is given the
 // same allocation that this container has.
 //
-// In addition, any number of widgets can be through the
+// In addition, any number of widgets can be added through the
 // gtk_floating_container_add_floating() method, which provides functionality
 // similar to a GtkFixed. Unlike a GtkFixed, coordinates are not set when you
 // gtk_fixed_put(). The location of the floating widgets is determined while
