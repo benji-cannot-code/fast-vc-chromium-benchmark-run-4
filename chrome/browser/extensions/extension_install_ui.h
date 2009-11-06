eb73a62c939fe6dbb928a7de92de23d16b32a2dc
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-06 23:48:54 +0000

    Revert 31322 - Extension Installed InfoBubble
    This creates UI feedback upon successful installation of an extension. An InfoBubble is shown containing the install icon and some information about how to manage extensions.
    
    TEST=Install a packaged extension. Verify the InfoBubble is shown, with the install icon and some description. The InfoBubble should disappear when the bubble looses focus (click elsewhere). For a browserAction, the bubble should point to the browserAction icon. For a pageAction **that has a "default_icon" set in it's manifest (see the samples/subscribe_page_action in this CL)**, it should point to a temporarily shown pageAction icon that will be hidden when the bubble closes. Otherwise it should point to the wrench menu.
    
    BUG=21412
    
    Review URL: http://codereview.chromium.org/362022
    
    TBR=rafaelw@chromium.org
    Review URL: http://codereview.chromium.org/375019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31326 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_ui.h
+++ chrome/browser/extensions/extension_install_ui.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/gfx/native_widget_types.h"
 #include "base/file_path.h"
 #include "base/ref_counted.h"
-#include "base/scoped_ptr.h"
-#include "third_party/skia/include/core/SkBitmap.h"
 
 #include <string>
 
@@ -20,6 +18,7 @@ class MessageLoop;
 class Profile;
 class InfoBarDelegate;
 class SandboxedExtensionUnpacker;
+class SkBitmap;
 class TabContents;
 
 // Displays all the UI around extension installation.
@@ -44,7 +43,7 @@ class ExtensionInstallUI {
                                          const std::wstring& warning_text);
   static void ShowExtensionInstallError(const std::string& error);
 
-  explicit ExtensionInstallUI(Profile* profile);
+  ExtensionInstallUI(Profile* profile);
 
   // This is called by the installer to verify whether the installation should
   // proceed.
@@ -77,7 +76,6 @@ class ExtensionInstallUI {
   Profile* profile_;
   MessageLoop* ui_loop_;
   std::string previous_theme_id_;  // Used to undo theme installation.
-  SkBitmap icon_;  // The extensions installation icon.
 
 #if defined(TOOLKIT_GTK)
   // Also needed to undo theme installation in the linux UI.
