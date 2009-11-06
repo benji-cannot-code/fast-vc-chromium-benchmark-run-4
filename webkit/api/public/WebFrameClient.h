5c8a30792b09269c6806432a4c4f076ff7295f43
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-06 21:33:49 +0000

    Wires through didChangeScrollOffset so that it triggers saving content state.
    BUG=17220
    TEST=none
    
    Review URL: http://codereview.chromium.org/373014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31303 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/public/WebFrameClient.h
+++ webkit/api/public/WebFrameClient.h
@@ -260,6 +260,9 @@ namespace WebKit {
         // The size of the content area changed.
         virtual void didChangeContentsSize(WebFrame*, const WebSize&) { }
 
+        // The main frame scrolled.
+        virtual void didChangeScrollOffset(WebFrame*) { }
+
 
         // Find-in-page notifications ------------------------------------------
 
