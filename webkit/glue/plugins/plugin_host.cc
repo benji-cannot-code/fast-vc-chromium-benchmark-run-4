a1312f15282d019181306f55b2b281fc6431e5cd
amanda@chromium.org <amanda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-06 17:08:36 +0000

    Implement the Cocoa event model for Mac NPAPI.  This is a first cut for the Google Earth plugin engineers to experiment with, though it's not specific to that plugin.  Note that the Google Earth plugin still doesn't draw correctly with this patch--we need some help from the Earth team for that, but at least it will negotiate the event model).  Other plugins that use the Cocoa event model may work as a result, but I haven't found any in the wild so far that don't also depend on the CoreAnimation drawing model (which is not yet implemented). BUG=16443 TEST=The latest Google Earth plugin should load if you add #geplugin_browserok to the URL. Review URL: http://codereview.chromium.org/355044
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31240 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/plugin_host.cc
+++ webkit/glue/plugins/plugin_host.cc
@@ -847,6 +847,7 @@ NPError NPN_GetValue(NPP id, NPNVariable variable, void *value) {
   }
   case NPNVsupportsCoreGraphicsBool:
   case NPNVsupportsCarbonBool:
+  case NPNVsupportsCocoaBool:
   {
     // we do support these drawing and event models.
     NPBool* supports_model = reinterpret_cast<NPBool*>(value);
@@ -856,7 +857,6 @@ NPError NPN_GetValue(NPP id, NPNVariable variable, void *value) {
   }
   case NPNVsupportsOpenGLBool:
   case NPNVsupportsCoreAnimationBool:
-  case NPNVsupportsCocoaBool:
   {
     // we do not support these drawing and event models.
     NPBool* supports_model = reinterpret_cast<NPBool*>(value);
@@ -948,10 +948,11 @@ NPError  NPN_SetValue(NPP id, NPPVariable variable, void *value) {
   }
   case NPPVpluginEventModel:
   {
-    // we only support the Carbon event model
+    // we support Carbon and Cocoa event models
     int model = reinterpret_cast<int>(value);
     switch (model) {
       case NPNVsupportsCarbonBool:
+      case NPNVsupportsCocoaBool:
         plugin->set_event_model(model);
         return NPERR_NO_ERROR;
         break;
