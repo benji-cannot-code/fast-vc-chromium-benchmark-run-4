17b231ebc43035fc78505a4add50bd9063a41940
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-06 20:01:14 +0000

    2009-11-06  Steve Block  <steveblock@google.com>
            Reviewed by Eric Seidel.
    
            V8DOMWindowCustom.cpp is missing WEB_SOCKETS guard on include.
            https://bugs.webkit.org/show_bug.cgi?id=31209
    
            Build fix only. No new tests possible.
    
            * bindings/v8/custom/V8DOMWindowCustom.cpp: Modified. Added WEB_SOCKETS guard on inclue of WebSockets.h.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50603 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
@@ -56,7 +56,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Settings.h"
 #include "SharedWorkerRepository.h"
 #include "Storage.h"
+#if ENABLE(WEB_SOCKETS)
 #include "WebSocket.h"
+#endif
 #include "WindowFeatures.h"
 
 // Horizontal and vertical offset, from the parent content area, around newly
