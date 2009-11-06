91313bd2d40093c573f91fd777fd805f88df78dd
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-06 22:02:16 +0000

    Reenable tests that were disabled due to the epic flakyness of LocalStorage which has since been fixed.
    TEST=none
    BUG=25746
    
    Review URL: http://codereview.chromium.org/377011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31310 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_storage_apitest.cc
+++ chrome/browser/extensions/extension_storage_apitest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-// This test is disabled. See bug 25746
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Storage) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Storage) {
   ASSERT_TRUE(RunExtensionTest("storage")) << message_;
 }
