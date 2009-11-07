31270b37cf47aad82930f46bcd442f98de2c3833
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-07 15:39:55 +0000

    Amit, please review everything. jam, please review changes to the plugin create channel IPCs. mpcomplete, please review changes to chrome_plugin_host.cc
    ChromeFrame needs to intercept URL requests issued directly by plugins like gears to ensure that they
    get routed through the host browser network stack. We decide whether or not a request is to be handled
    based on the renderer process id and the render view id (routing id), which get passed in the
    ViewHostMsg_RequestResource IPC. If this request is issued by Gears then the routing id comes in as
    MSG_ROUTING_NONE, which causes the request to go through the chrome network stack.
    
    Fix is to pass the host render view id to the plugin in the PluginMsg_Init IPC. The plugin already
    receives the renderer process id. Both these ids now come back in the ViewHostMsg_RequestResource
    IPC.
    
    This fixes an issue with wave when rendered in full tab mode in ChromeFrame, where dropping a file into
    a wave would cause the renderer to hang.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=23992
    
    Bug=23992
    
    Review URL: http://codereview.chromium.org/370007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31387 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/resource_dispatcher.cc
+++ chrome/common/resource_dispatcher.cc
@@ -57,7 +57,9 @@ class IPCResourceLoaderBridge : public ResourceLoaderBridge {
                           ResourceType::Type resource_type,
                           uint32 request_context,
                           int appcache_host_id,
-                          int routing_id);
+                          int routing_id,
+                          int host_renderer_id,
+                          int host_render_view_id);
   virtual ~IPCResourceLoaderBridge();
 
   // ResourceLoaderBridge
@@ -95,6 +97,15 @@ class IPCResourceLoaderBridge : public ResourceLoaderBridge {
   // indicates the URL of this resource request for help debugging
   std::string url_;
 #endif
+
+  // The following two members are specified if the request is initiated by
+  // a plugin like Gears.
+
+  // Contains the id of the host renderer.
+  int host_renderer_id_;
+
+  // Contains the id of the host render view.
+  int host_render_view_id_;
 };
 
 IPCResourceLoaderBridge::IPCResourceLoaderBridge(
@@ -111,11 +122,15 @@ IPCResourceLoaderBridge::IPCResourceLoaderBridge(
     ResourceType::Type resource_type,
     uint32 request_context,
     int appcache_host_id,
-    int routing_id)
+    int routing_id,
+    int host_renderer_id,
+    int host_render_view_id)
     : peer_(NULL),
       dispatcher_(dispatcher),
       request_id_(-1),
-      routing_id_(routing_id) {
+      routing_id_(routing_id),
+      host_renderer_id_(host_renderer_id),
+      host_render_view_id_(host_render_view_id) {
   DCHECK(dispatcher_) << "no resource dispatcher";
   request_.method = method;
   request_.url = url;
@@ -129,6 +144,8 @@ IPCResourceLoaderBridge::IPCResourceLoaderBridge(
   request_.resource_type = resource_type;
   request_.request_context = request_context;
   request_.appcache_host_id = appcache_host_id;
+  request_.host_renderer_id = host_renderer_id_;
+  request_.host_render_view_id = host_render_view_id_;
 
 #ifdef LOG_RESOURCE_REQUESTS
   url_ = url.possibly_invalid_spec();
@@ -567,7 +584,9 @@ webkit_glue::ResourceLoaderBridge* ResourceDispatcher::CreateBridge(
     ResourceType::Type resource_type,
     uint32 request_context,
     int appcache_host_id,
-    int route_id) {
+    int route_id,
+    int host_renderer_id,
+    int host_render_view_id) {
   return new webkit_glue::IPCResourceLoaderBridge(this, method, url,
                                                   first_party_for_cookies,
                                                   referrer, frame_origin,
@@ -576,7 +595,9 @@ webkit_glue::ResourceLoaderBridge* ResourceDispatcher::CreateBridge(
                                                   resource_type,
                                                   request_context,
                                                   appcache_host_id,
-                                                  route_id);
+                                                  route_id,
+                                                  host_renderer_id,
+                                                  host_render_view_id);
 }
 
 bool ResourceDispatcher::IsResourceDispatcherMessage(
