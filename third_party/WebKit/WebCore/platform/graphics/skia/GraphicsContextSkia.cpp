536e2814daf2a0f51cdc060aef22a4ce9d10da48
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-08 23:42:20 +0000

    Unreviewed build fix for chromium.
    Build fix for https://bugs.webkit.org/show_bug.cgi?id=31219
    Clean up GraphicsContext's current concept of ColorSpace
    
    * platform/graphics/skia/GraphicsContextSkia.cpp:
    (WebCore::GraphicsContext::fillPath):
    (WebCore::GraphicsContext::fillRect):
    (WebCore::GraphicsContext::strokePath):
    (WebCore::GraphicsContext::strokeRect):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50629 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
+++ third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
@@ -705,7 +705,7 @@ void GraphicsContext::fillPath()
       return;
 
     const GraphicsContextState& state = m_common->state;
-    ColorSpace colorSpace = state.fillColorSpace;
+    ColorType colorSpace = state.fillType;
 
     path.setFillType(state.fillRule == RULE_EVENODD ?
         SkPath::kEvenOdd_FillType : SkPath::kWinding_FillType);
@@ -728,7 +728,7 @@ void GraphicsContext::fillRect(const FloatRect& rect)
     }
 
     const GraphicsContextState& state = m_common->state;
-    ColorSpace colorSpace = state.fillColorSpace;
+    ColorType colorSpace = state.fillType;
 
     SkPaint paint;
     platformContext()->setupPaintForFilling(&paint);
@@ -1119,7 +1119,7 @@ void GraphicsContext::strokePath()
         return;
 
     const GraphicsContextState& state = m_common->state;
-    ColorSpace colorSpace = state.strokeColorSpace;
+    ColorType colorSpace = state.strokeType;
 
     SkPaint paint;
     platformContext()->setupPaintForStroking(&paint, 0, 0);
@@ -1136,7 +1136,7 @@ void GraphicsContext::strokeRect(const FloatRect& rect, float lineWidth)
         return;
 
     const GraphicsContextState& state = m_common->state;
-    ColorSpace colorSpace = state.strokeColorSpace;
+    ColorType colorSpace = state.strokeType;
 
     SkPaint paint;
     platformContext()->setupPaintForStroking(&paint, 0, 0);
