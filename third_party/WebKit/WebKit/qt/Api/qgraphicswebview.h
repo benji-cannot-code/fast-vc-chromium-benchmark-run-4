edc38746cafc7d516d0bc8b5e6cb3c402b1200e7
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-09 13:10:37 +0000

    Added a missing re-implementation of QGraphicsItem::inputMethodQuery().
    Patch by Simon Hausmann <hausmann@webkit.org> on 2009-11-05
    Reviewed by Tor Arne Vestbø.
    
    * Api/qgraphicswebview.cpp:
    (QGraphicsWebView::inputMethodQuery):
    * Api/qgraphicswebview.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50652 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qgraphicswebview.h
+++ third_party/WebKit/WebKit/qt/Api/qgraphicswebview.h
@@ -88,6 +88,8 @@ public:
 
     virtual QSizeF sizeHint(Qt::SizeHint which, const QSizeF& constraint) const;
 
+    virtual QVariant inputMethodQuery(Qt::InputMethodQuery query) const;
+
 public Q_SLOTS:
     void stop();
     void back();
