b03db0cd7a143269a062c38ae3cdeef7fa2a4f20
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-09 21:58:22 +0000

    Roll WebKit DEPS  50603:50641.
    Changed ResourceRequest to a class due to http://trac.webkit.org/changeset/50625
    
    TBR=dumi@chromium.org
    TEST=None
    BUG=None
    
    Review URL: http://codereview.chromium.org/379004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31484 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/glue_util.h
+++ webkit/glue/glue_util.h
@@ -25,11 +25,11 @@ class KURL;
 class Node;
 class Range;
 class ResourceError;
+class ResourceRequest;
 class ResourceResponse;
 class SecurityOrigin;
 class SharedBuffer;
 class String;
-struct ResourceRequest;
 }
 
 namespace WebKit {
