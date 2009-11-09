f2137f147a70798e7bea276268f40d7c73c1129c
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-09 12:29:24 +0000

    Make the QGraphicsWebView constructor explicit.
    Patch by Simon Hausmann <hausmann@webkit.org> on 2009-11-04
    Reviewed by Tor Arne Vestbø.
    
    * Api/qgraphicswebview.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qgraphicswebview.h
+++ third_party/WebKit/WebKit/qt/Api/qgraphicswebview.h
@@ -50,7 +50,7 @@ class QWEBKIT_EXPORT QGraphicsWebView : public QGraphicsWidget {
     Q_PROPERTY(bool modified READ isModified)
 
 public:
-    QGraphicsWebView(QGraphicsItem* parent = 0);
+    explicit QGraphicsWebView(QGraphicsItem* parent = 0);
     ~QGraphicsWebView();
 
     QWebPage* page() const;
