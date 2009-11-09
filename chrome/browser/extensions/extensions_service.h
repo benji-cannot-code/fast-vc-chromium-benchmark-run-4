9a746965ad962b359839adf79e2bb5df56c03edf
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-09 19:21:47 +0000

    Revert "Always destroy the ExtensionsService on UI thread."
    It introduced memory leaks in tests. I have to ensure that
    all message loops run pending tasks to guarantee that
    ExtensionsService will be destroyed.
    
    TBR=erikkay
    
    Review URL: http://codereview.chromium.org/377034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31458 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service.h
+++ chrome/browser/extensions/extensions_service.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task.h"
 #include "base/tuple.h"
 #include "base/values.h"
-#include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/extensions/extension_prefs.h"
 #include "chrome/browser/extensions/extension_process_manager.h"
 #include "chrome/browser/extensions/external_extension_provider.h"
@@ -55,8 +54,7 @@ class ExtensionUpdateService {
 
 // Manages installed and running Chromium extensions.
 class ExtensionsService
-    : public base::RefCountedThreadSafe<ExtensionsService,
-                                        ChromeThread::DeleteOnUIThread>,
+    : public base::RefCountedThreadSafe<ExtensionsService>,
       public BlacklistPathProvider,
       public ExtensionUpdateService,
       public NotificationObserver {
@@ -237,8 +235,7 @@ class ExtensionsService
                        const NotificationDetails& details);
 
  private:
-  friend class ChromeThread;
-  friend class DeleteTask<ExtensionsService>;
+  friend class base::RefCountedThreadSafe<ExtensionsService>;
 
   virtual ~ExtensionsService();
 
