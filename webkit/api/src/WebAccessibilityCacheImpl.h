d325c37d93814b7cb1d32aed69a6ffa0967ff8f6
eseidel@chromium.org <eseidel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-09 23:21:32 +0000

    Attempt to fix build per dglazkov's instruction. BUG=None TEST=None
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31503 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/api/src/WebAccessibilityCacheImpl.h
+++ webkit/api/src/WebAccessibilityCacheImpl.h
@@ -34,11 +34,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // FIXME: Move wtf/RefCounted.h include to AccessibilityObjectWrapper.h
 // once this file is upstream.
+#include <wtf/RefCounted.h>
+
 #include "AccessibilityObjectWrapper.h"
 #include "WebAccessibilityCache.h"
 #include <wtf/HashMap.h>
 #include <wtf/PassRefPtr.h>
-#include <wtf/RefCounted.h>
 
 namespace WebKit {
 
