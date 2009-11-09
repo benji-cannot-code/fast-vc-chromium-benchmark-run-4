81a3aced7a9e4f1f2e8254064b2952e22e2127b9
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-09 03:17:25 +0000

    Fix regression in which NPRES_NETWORK_ERR instead of NPRES_USER_BREAK was passed to a plugin when a tab that it created was closed by the user.  This ended up stopping the launching video.
    BUG=23102
    Review URL: http://codereview.chromium.org/373024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31415 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_impl.cc
+++ webkit/glue/webplugin_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "net/base/escape.h"
+#include "net/base/net_errors.h"
 #include "skia/ext/platform_canvas.h"
 #include "webkit/api/public/WebConsoleMessage.h"
 #include "webkit/api/public/WebCString.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/api/public/WebPluginParams.h"
 #include "webkit/api/public/WebRect.h"
 #include "webkit/api/public/WebURL.h"
+#include "webkit/api/public/WebURLError.h"
 #include "webkit/api/public/WebURLLoader.h"
 #include "webkit/api/public/WebURLLoaderClient.h"
 #include "webkit/api/public/WebURLResponse.h"
@@ -355,11 +357,13 @@ void WebPluginImpl::didFinishLoadingFrameRequest(
 
 void WebPluginImpl::didFailLoadingFrameRequest(
     const WebURL& url, void* notify_data, const WebURLError& error) {
-  // TODO(darin): Map net::ERR_ABORTED to NPRES_USER_BREAK?
-  if (delegate_) {
-    delegate_->DidFinishLoadWithReason(
-        url, NPRES_NETWORK_ERR, reinterpret_cast<intptr_t>(notify_data));
-  }
+  if (!delegate_)
+    return;
+
+  NPReason reason =
+      error.reason == net::ERR_ABORTED ? NPRES_USER_BREAK : NPRES_NETWORK_ERR;
+  delegate_->DidFinishLoadWithReason(
+      url, reason, reinterpret_cast<intptr_t>(notify_data));
 }
 
 // -----------------------------------------------------------------------------
