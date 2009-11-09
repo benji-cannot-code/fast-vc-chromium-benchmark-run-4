0109bd07e231a3c2a935e9b298879e4a8dee3b6b
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-09 01:18:08 +0000

    2009-11-08  Daniel Bates  <dbates@webkit.org>
            Reviewed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=31098
    
            Allows same-origin plugin-based content to load.
    
            Test: http/tests/security/xssAuditor/object-src-inject.html
    
            * page/XSSAuditor.cpp:
            (WebCore::XSSAuditor::canLoadExternalScriptFromSrc): Modified to call XSSAuditor::isSameOriginResource.
            (WebCore::XSSAuditor::canLoadObject): Ditto.
            (WebCore::XSSAuditor::canSetBaseElementURL): Ditto. 
            (WebCore::XSSAuditor::isSameOriginResource): Added.
            * page/XSSAuditor.h:
    2009-11-08  Daniel Bates  <dbates@webkit.org>
    
            Reviewed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=31098
    
            Tests that the XSSAuditor prevents loading plugin-based content that is not 
            from the same-origin as the enclosing page.
    
            * http/tests/security/xssAuditor/object-src-inject-expected.txt: Added.
            * http/tests/security/xssAuditor/object-src-inject.html: Added.
            * http/tests/security/xssAuditor/resources/echo-object-src.pl: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50631 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/security/xssAuditor/resources/echo-object-src.pl
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/perl -wT
+use strict;
+use CGI;
+
+my $cgi = new CGI;
+
+print "Content-Type: text/html; charset=UTF-8\n\n";
+
+print "<!DOCTYPE html>\n";
+print "<html>\n";
+print "<object name=\"plugin\" type=\"application/x-webkit-test-netscape\">\n";
+print "<param name=\"movie\" value=\"".$cgi->param('q')."\" />\n";
+print "</object>\n";
+print "</body>\n";
+print "</html>\n";
