5fd9d66256a9caa4ba93c19b503d5005c718793b
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-09 23:18:58 +0000

    Change struct ResourceRequest to class ResourceRequest per http://trac.webkit.org/changeset/50625.
    Reviewed by Dmitry Titov.
    
    * public/WebURLRequest.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50697 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebURLRequest.h
+++ third_party/WebKit/WebKit/chromium/public/WebURLRequest.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebHTTPBody.h"
 
 #if defined(WEBKIT_IMPLEMENTATION)
-namespace WebCore { struct ResourceRequest; }
+namespace WebCore { class ResourceRequest; }
 #endif
 
 namespace WebKit {
