474a1073e92cd3b7f55fa4f2032a656d96bbb762
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-09 21:40:42 +0000

    Tentatively reenabling tests now that I fixed a test running before this one that was doing a Ctrl-Esc instead of Esc, bringing up the Windows start menu. Not sure why it would cause the tests to fail on the bot, probably an activation issue (that I cannot repro locally).
    BUG=26996
    TEST=Make sure the interactive UI tests stay green on the Window build bot.
    
    Review URL: http://codereview.chromium.org/379009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31479 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/bookmark_bar_view_test.cc
+++ chrome/browser/views/bookmark_bar_view_test.cc
@@ -232,9 +232,7 @@ class BookmarkBarViewTest1 : public BookmarkBarViewEventTestBase {
   }
 };
 
-// TODO(jcampan): http://crbug.com/26996 temporarily disabled because failing
-//                since we move to running the process
-VIEW_TEST(BookmarkBarViewTest1, DISABLED_Basic)
+VIEW_TEST(BookmarkBarViewTest1, Basic)
 
 // Brings up menu, clicks on empty space and make sure menu hides.
 class BookmarkBarViewTest2 : public BookmarkBarViewEventTestBase {
@@ -281,9 +279,7 @@ class BookmarkBarViewTest2 : public BookmarkBarViewEventTestBase {
   }
 };
 
-// TODO(jcampan): http://crbug.com/26996 temporarily disabled because failing
-//                since we move to running the process
-VIEW_TEST(BookmarkBarViewTest2, DISABLED_HideOnDesktopClick)
+VIEW_TEST(BookmarkBarViewTest2, HideOnDesktopClick)
 
 // Brings up menu. Moves over child to make sure submenu appears, moves over
 // another child and make sure next menu appears.
