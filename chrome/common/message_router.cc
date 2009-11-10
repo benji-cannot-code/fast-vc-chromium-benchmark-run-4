b4c5ffb93d3367f865518a02b4efe862ef71093e
gregoryd@google.com <gregoryd@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-10 21:58:07 +0000

    Add 64 bit support + code cleanup TEST=none BUG=none
    Review URL: http://codereview.chromium.org/374022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31604 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/message_router.cc
+++ chrome/common/message_router.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/common/message_router.h"
-#include "chrome/common/render_messages.h"
 
 void MessageRouter::OnControlMessageReceived(const IPC::Message& msg) {
   NOTREACHED() <<
