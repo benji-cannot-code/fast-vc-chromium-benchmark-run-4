7b2ee0dffb4f186821500d0fe9a41b6bf87767a0
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-10 18:50:08 +0000

    [Mac] L10N cleanup of report a bug - align the baselines of the controls - quick animation when changing the buttons so it is a little less sudden. While here: - don't update the button title while dragging through the menu, do it when done. - Fixed up some kvo binding to stop using the ivars directly since the controller has accessors
    TEST=baselines match, button updates after selection not during menu drags
    BUG=27275
    Review URL: http://codereview.chromium.org/388006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31571 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bug_report_window_controller.mm
+++ chrome/browser/cocoa/bug_report_window_controller.mm
@@ -21,10 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize bugType = bugType_;
 @synthesize pageURL = pageURL_;
 @synthesize pageTitle = pageTitle_;
-@synthesize sendReportButton = sendReportButton_;
-@synthesize cancelButton = cancelButton_;
 @synthesize sendScreenshot = sendScreenshot_;
-@synthesize disableScreenshot = disableScreenshot_;
+@synthesize disableScreenshotCheckbox = disableScreenshotCheckbox_;
 @synthesize bugTypeList = bugTypeList_;
 
 - (id)initWithTabContents:(TabContents*)currentTab
@@ -42,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // TODO(mirandac): This dialog should be a tab-modal sheet if a browser
       // window exists.
       [self setSendScreenshot:YES];
-      [self setDisableScreenshot:NO];
+      [self setDisableScreenshotCheckbox:NO];
       bugTypeList_ = [[NSArray alloc] initWithObjects:
           l10n_util::GetNSStringWithFixup(IDS_BUGREPORT_PAGE_WONT_LOAD),
           l10n_util::GetNSStringWithFixup(IDS_BUGREPORT_PAGE_LOOKS_ODD),
@@ -62,7 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // If no current tab exists, create a menu without the "broken page"
       // options, with page URL and title empty, and screenshot disabled.
       [self setSendScreenshot:NO];
-      [self setDisableScreenshot:YES];
+      [self setDisableScreenshotCheckbox:YES];
       bugTypeList_ = [[NSArray alloc] initWithObjects:
           l10n_util::GetNSStringWithFixup(IDS_BUGREPORT_CHROME_MISBEHAVES),
           l10n_util::GetNSStringWithFixup(IDS_BUGREPORT_SOMETHING_MISSING),
@@ -125,21 +123,48 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       l10n_util::GetNSStringWithFixup(IDS_BUGREPORT_PHISHING_PAGE)];
 }
 
-- (void)menu:(NSMenu*)menu willHighlightItem:(NSMenuItem *)item {
-  NSString* buttonTitle = [[item title] isEqualToString:
-      l10n_util::GetNSStringWithFixup(IDS_BUGREPORT_PHISHING_PAGE)] ?
+// Custom setter to update the UI for different bug types.
+- (void)setBugType:(NSUInteger)bugType {
+  bugType_ = bugType;
+
+  // The "send" button's title is based on the type of report.
+  NSString* buttonTitle = [self isPhishingReport] ?
       l10n_util::GetNSStringWithFixup(IDS_BUGREPORT_SEND_PHISHING_REPORT) :
       l10n_util::GetNSStringWithFixup(IDS_BUGREPORT_SEND_REPORT);
-  if (![buttonTitle isEqualToString:[sendReportButton_ title]]) {
+  if (![buttonTitle isEqualTo:[sendReportButton_ title]]) {
+    NSRect sendFrame1 = [sendReportButton_ frame];
+    NSRect cancelFrame1 = [cancelButton_ frame];
+
     [sendReportButton_ setTitle:buttonTitle];
     CGFloat deltaWidth =
         [GTMUILocalizerAndLayoutTweaker sizeToFitView:sendReportButton_].width;
-    NSRect newSendButtonFrame = [sendReportButton_ frame];
-    newSendButtonFrame.origin.x -= deltaWidth;
-    NSRect newCancelButtonFrame = [cancelButton_ frame];
-    newCancelButtonFrame.origin.x -= deltaWidth;
-    [sendReportButton_ setFrame:newSendButtonFrame];
-    [cancelButton_ setFrame:newCancelButtonFrame];
+
+    NSRect sendFrame2 = [sendReportButton_ frame];
+    sendFrame2.origin.x -= deltaWidth;
+    NSRect cancelFrame2 = cancelFrame1;
+    cancelFrame2.origin.x -= deltaWidth;
+
+    // Since the buttons get updated/resize, use a quick animation so it is
+    // a little less jarring in the UI.
+    NSDictionary* sendReportButtonResize =
+        [NSDictionary dictionaryWithObjectsAndKeys:
+         sendReportButton_, NSViewAnimationTargetKey,
+         [NSValue valueWithRect:sendFrame1], NSViewAnimationStartFrameKey,
+         [NSValue valueWithRect:sendFrame2], NSViewAnimationEndFrameKey,
+         nil];
+    NSDictionary* cancelButtonResize =
+        [NSDictionary dictionaryWithObjectsAndKeys:
+         cancelButton_, NSViewAnimationTargetKey,
+         [NSValue valueWithRect:cancelFrame1], NSViewAnimationStartFrameKey,
+         [NSValue valueWithRect:cancelFrame2], NSViewAnimationEndFrameKey,
+         nil];
+    NSAnimation* animation =
+        [[[NSViewAnimation alloc] initWithViewAnimations:
+           [NSArray arrayWithObjects:sendReportButtonResize, cancelButtonResize,
+             nil]] autorelease];
+    const NSTimeInterval kQuickTransitionInterval = 0.1;
+    [animation setDuration:kQuickTransitionInterval];
+    [animation startAnimation];
   }
 }
 
