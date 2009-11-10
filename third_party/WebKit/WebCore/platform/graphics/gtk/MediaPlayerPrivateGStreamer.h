e658dec08a78653d8461d09b9deec22f9f2682c7
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-10 09:45:52 +0000

    2009-11-10  Philippe Normand  <pnormand@igalia.com>
            Reviewed by Jan Alonzo.
    
            https://bugs.webkit.org/show_bug.cgi?id=31047
            [GTK] Failing test media/video-played-ranges-1.html
    
            Fix playback rate setter by remembering the rate was changed. Also
            correctly handle reverse playback by doing a seek from end to
            beginning of the media.
    
            * platform/graphics/gtk/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::MediaPlayerPrivate::MediaPlayerPrivate):
            (WebCore::MediaPlayerPrivate::currentTime):
            (WebCore::MediaPlayerPrivate::seek):
            (WebCore::MediaPlayerPrivate::hasAudio):
            (WebCore::MediaPlayerPrivate::setVolume):
            (WebCore::MediaPlayerPrivate::setRate):
            (WebCore::MediaPlayerPrivate::updateStates):
            * platform/graphics/gtk/MediaPlayerPrivateGStreamer.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50726 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gtk/MediaPlayerPrivateGStreamer.h
+++ third_party/WebKit/WebCore/platform/graphics/gtk/MediaPlayerPrivateGStreamer.h
@@ -91,7 +91,6 @@ namespace WebCore {
             void setSize(const IntSize&);
 
             void loadStateChanged();
-            void rateChanged();
             void sizeChanged();
             void timeChanged();
             void volumeChanged();
@@ -127,6 +126,7 @@ namespace WebCore {
             GstElement* m_videoSink;
             GstElement* m_source;
             GstClockTime m_seekTime;
+            bool m_changingRate;
             float m_endTime;
             bool m_isEndReached;
             MediaPlayer::NetworkState m_networkState;
