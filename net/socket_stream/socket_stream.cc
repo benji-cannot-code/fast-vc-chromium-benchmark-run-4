e698c35bcb62c889a332a96bb8d6d7bedafc8a2f
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-10 06:04:29 +0000

    Use URLRequestContext's host_resolver instead of SystemHostResolver.
    For unittests, we can inject mock host_resolver via SetHostResolver().
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/384001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31545 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket_stream/socket_stream.cc
+++ net/socket_stream/socket_stream.cc
@@ -43,7 +43,6 @@ SocketStream::SocketStream(const GURL& url, Delegate* delegate)
       delegate_(delegate),
       max_pending_send_allowed_(kMaxPendingSendAllowed),
       next_state_(STATE_NONE),
-      host_resolver_(CreateSystemHostResolver()),
       factory_(ClientSocketFactory::GetDefaultFactory()),
       proxy_mode_(kDirectConnection),
       pac_request_(NULL),
@@ -87,6 +86,7 @@ void SocketStream::SetUserData(const void* key, UserData* data) {
 
 void SocketStream::set_context(URLRequestContext* context) {
   context_ = context;
+  host_resolver_ = context_->host_resolver();
 }
 
 void SocketStream::Connect() {
@@ -417,6 +417,7 @@ int SocketStream::DoResolveHost() {
 
   HostResolver::RequestInfo resolve_info(host, port);
 
+  DCHECK(host_resolver_.get());
   resolver_.reset(new SingleRequestHostResolver(host_resolver_.get()));
   return resolver_->Resolve(resolve_info, &addresses_, &io_callback_, NULL);
 }
