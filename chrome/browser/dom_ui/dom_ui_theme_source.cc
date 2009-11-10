1ddb61c22a49c6debf425b5c75581ed3dfe5c9cd
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-10 00:17:44 +0000

    Fix a bug where we would cache the incognito profile pointer in DOMUIThemeSource and try to use it after it was deleted. Instead, always cache the original profile pointer (which will live longer).
    While I'm here, get rid of the profile_ cached in
    NewTabHTMLSource because we don't need it after the ctor is done
    running.
    
    BUG=26840
    
    Review URL: http://codereview.chromium.org/372072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31513 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/dom_ui_theme_source.cc
+++ chrome/browser/dom_ui/dom_ui_theme_source.cc
@@ -53,11 +53,11 @@ static std::string StripQueryParams(const std::string& path) {
 
 DOMUIThemeSource::DOMUIThemeSource(Profile* profile)
     : DataSource(chrome::kChromeUIThemePath, MessageLoop::current()),
-      profile_(profile) {
+      profile_(profile->GetOriginalProfile()) {
   if (profile->IsOffTheRecord())
-    InitNewIncognitoTabCSS();
+    InitNewIncognitoTabCSS(profile);
   else
-    InitNewTabCSS();
+    InitNewTabCSS(profile);
 }
 
 void DOMUIThemeSource::StartDataRequest(const std::string& path,
@@ -117,8 +117,8 @@ MessageLoop* DOMUIThemeSource::MessageLoopForRequestPath(
 ////////////////////////////////////////////////////////////////////////////////
 // DOMUIThemeSource, private:
 
-void DOMUIThemeSource::InitNewTabCSS() {
-  ThemeProvider* tp = profile_->GetThemeProvider();
+void DOMUIThemeSource::InitNewTabCSS(Profile* profile) {
+  ThemeProvider* tp = profile->GetThemeProvider();
   DCHECK(tp);
 
   // Get our theme colors
@@ -164,7 +164,7 @@ void DOMUIThemeSource::InitNewTabCSS() {
 
   // Cache-buster for background.
   subst.push_back(WideToASCII(
-      profile_->GetPrefs()->GetString(prefs::kCurrentThemeID)));  // $1
+      profile->GetPrefs()->GetString(prefs::kCurrentThemeID)));  // $1
 
   // Colors.
   subst.push_back(SkColorToRGBAString(color_background));  // $2
@@ -185,7 +185,7 @@ void DOMUIThemeSource::InitNewTabCSS() {
   subst2.push_back(SkColorToRGBAString(color_link_underline));  // $$6
   subst2.push_back(SkColorToRGBAString(color_section_link_underline));  // $$7
 
-  if (profile_->GetPrefs()->GetInteger(prefs::kNTPThemePromoRemaining) > 0)
+  if (profile->GetPrefs()->GetInteger(prefs::kNTPThemePromoRemaining) > 0)
     subst2.push_back("block");  // $$8
   else
     subst2.push_back("none");  // $$8
@@ -202,8 +202,8 @@ void DOMUIThemeSource::InitNewTabCSS() {
       css_string, subst2, NULL);
 }
 
-void DOMUIThemeSource::InitNewIncognitoTabCSS() {
-  ThemeProvider* tp = profile_->GetThemeProvider();
+void DOMUIThemeSource::InitNewIncognitoTabCSS(Profile* profile) {
+  ThemeProvider* tp = profile->GetThemeProvider();
   DCHECK(tp);
 
   // Get our theme colors
@@ -215,7 +215,7 @@ void DOMUIThemeSource::InitNewIncognitoTabCSS() {
 
   // Cache-buster for background.
   subst.push_back(WideToUTF8(
-      profile_->GetPrefs()->GetString(prefs::kCurrentThemeID)));  // $1
+      profile->GetPrefs()->GetString(prefs::kCurrentThemeID)));  // $1
 
   // Colors.
   subst.push_back(SkColorToRGBAString(color_background));  // $2
