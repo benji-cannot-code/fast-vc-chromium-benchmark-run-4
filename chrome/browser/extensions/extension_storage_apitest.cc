a6f1482c21c277c51abb64c5b9b3ed405953c73e
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-10 01:11:19 +0000

    Disabling a failing browser test.
    BUG=http://crbug.com/27216
    TBR=willchan
    
    Review URL: http://codereview.chromium.org/377043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31520 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_storage_apitest.cc
+++ chrome/browser/extensions/extension_storage_apitest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Storage) {
+// TODO(jcampan): http://crbug.com/27216 disabled because failing.
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Storage) {
   ASSERT_TRUE(RunExtensionTest("storage")) << message_;
 }
