f1fe13003171f9beb0beff8e37dfe43d202c8615
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-11 20:59:51 +0000

    Added vertical shift for sync stuff.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31707 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/preferences_window_controller.mm
+++ chrome/browser/cocoa/preferences_window_controller.mm
@@ -565,6 +565,9 @@ class PrefObserverBridge : public NotificationObserver,
                                       delta:NSMakeSize(0.0, verticalShift)];
 
   verticalShift = 0.0;
+  verticalShift += AutoSizeGroup(personalStuffGroupSync_,
+                                 kAutoSizeGroupBehaviorVerticalToFit,
+                                 verticalShift);
   verticalShift += AutoSizeGroup(personalStuffGroupThemes_,
                                  kAutoSizeGroupBehaviorHorizontalToFit,
                                  verticalShift);
@@ -583,6 +586,7 @@ class PrefObserverBridge : public NotificationObserver,
 
   if (sync_service_) {
     sync_service_->AddObserver(observer_.get());
+    [self syncStateChanged];
   } else {
     // Disable controls if sync is disabled.
     RemoveViewFromView(personalStuffView_, syncLabel_);
@@ -637,11 +641,6 @@ class PrefObserverBridge : public NotificationObserver,
 
   // TODO(pinkerton): save/restore position based on prefs.
   [[self window] center];
-
-  // Get initial sync state.
-  if (sync_service_) {
-    [self syncStateChanged];
-  }
 }
 
 - (void)dealloc {
