1a75fe8c69d46f7ce5e2d6313575283efc4a0f88
zork@google.com <zork@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-11 00:43:53 +0000

    Check for an active network connection when network adapter status changes.
    BUG=19784
    TEST=On a system with two network adapters, ensure one has an active internet
    connection, and the other does not.  Disconnect the active one, and check that
    in notifier::NetworkStatusDetectorTaskMT::OnNetworkAliveDone() the call to
    network_alive->alive() returns false.
    
    Review URL: http://codereview.chromium.org/377019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31623 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/notifier/base/win/async_network_alive_win32.cc
+++ chrome/browser/sync/notifier/base/win/async_network_alive_win32.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "talk/base/common.h"
 #include "talk/base/criticalsection.h"
 #include "talk/base/logging.h"
+#include "talk/base/physicalsocketserver.h"
 #include "talk/base/scoped_ptr.h"
 
 namespace notifier {
@@ -103,6 +104,7 @@ class PlatformNetworkInfo {
         }
       }
     } while (true);
+
     LOG(INFO) << "alive: " << alive;
     return alive;
   }
@@ -216,7 +218,21 @@ class AsyncNetworkAliveWin32 : public AsyncNetworkAlive {
         return;
       }
     }
-    alive_ = network_info_->IsAlive(&error_);
+
+    if (network_info_->IsAlive(&error_)) {
+      // If there is an active connection, check that www.google.com:80
+      // is reachable.
+      talk_base::PhysicalSocketServer physical;
+      scoped_ptr<talk_base::Socket> socket(physical.CreateSocket(SOCK_STREAM));
+      if (socket->Connect(talk_base::SocketAddress("talk.google.com", 5222))) {
+        alive_ = false;
+      } else {
+        alive_ = true;
+      }
+    } else {
+      // If there are no available connections, then we aren't alive.
+      alive_ = false;
+    }
   }
 
   virtual void OnWorkStop() {
