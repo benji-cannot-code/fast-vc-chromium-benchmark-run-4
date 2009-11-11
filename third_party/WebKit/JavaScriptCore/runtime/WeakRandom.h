ec10aef0f5152565b5b0a48e6c985c0801bd4cca
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-11 07:30:06 +0000

    Linux build fix: Added an #include for UINT_MAX.
    * runtime/WeakRandom.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50791 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/WeakRandom.h
+++ third_party/WebKit/JavaScriptCore/runtime/WeakRandom.h
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WeakRandom_h
 #define WeakRandom_h
 
+#include <limits.h>
 #include <wtf/StdLibExtras.h>
 
 namespace JSC {
