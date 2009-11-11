c4d343a27a2f77489f10afc933d6f49405e0dcdb
hamaji@chromium.org <hamaji@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-11 03:08:27 +0000

    2009-11-10  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Darin Adler.
    
            rename counter to makeCounterNode in RenderCounter.cpp
            https://bugs.webkit.org/show_bug.cgi?id=31289
    
            No new test because this is just a small refactoring.
    
            * rendering/RenderCounter.cpp:
            (WebCore::findPlaceForCounter):
            (WebCore::makeCounterNode):
            (WebCore::RenderCounter::originalText):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50787 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderCounter.cpp
+++ third_party/WebKit/WebCore/rendering/RenderCounter.cpp
@@ -39,7 +39,7 @@ using namespace HTMLNames;
 typedef HashMap<RefPtr<AtomicStringImpl>, CounterNode*> CounterMap;
 typedef HashMap<const RenderObject*, CounterMap*> CounterMaps;
 
-static CounterNode* counter(RenderObject*, const AtomicString& counterName, bool alwaysCreateCounter);
+static CounterNode* makeCounterNode(RenderObject*, const AtomicString& counterName, bool alwaysCreateCounter);
 
 static CounterMaps& counterMaps()
 {
@@ -149,7 +149,7 @@ static bool findPlaceForCounter(RenderObject* object, const AtomicString& counte
     // sibling or the reset node. This flag controls it.
     bool createChildForReset = true;
     while ((prevCounterCandidate = prevCounterCandidate->previousInPreOrder())) {
-        CounterNode* c = counter(prevCounterCandidate, counterName, false);
+        CounterNode* c = makeCounterNode(prevCounterCandidate, counterName, false);
         if (prevCounterCandidate == resetCandidate) {
             if (!candidateCounter) {
                 candidateCounter = c;
@@ -186,7 +186,7 @@ static bool findPlaceForCounter(RenderObject* object, const AtomicString& counte
     return false;
 }
 
-static CounterNode* counter(RenderObject* object, const AtomicString& counterName, bool alwaysCreateCounter)
+static CounterNode* makeCounterNode(RenderObject* object, const AtomicString& counterName, bool alwaysCreateCounter)
 {
     ASSERT(object);
 
@@ -247,7 +247,7 @@ PassRefPtr<StringImpl> RenderCounter::originalText() const
         return 0;
 
     if (!m_counterNode)
-        m_counterNode = counter(parent(), m_counter.identifier(), true);
+        m_counterNode = makeCounterNode(parent(), m_counter.identifier(), true);
 
     CounterNode* child = m_counterNode;
     int value = child->isReset() ? child->value() : child->countInParent();
