4af35d1cfd4ad42edfeeabc945cd346608a91514
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-11 09:23:18 +0000

    2009-11-11  Yuzo Fujishima  <yuzo@google.com>
            Reviewed by David Levin.
    
            Add a test for web socket handshake error.
    
            https://bugs.webkit.org/show_bug.cgi?id=31336
    
            * websocket/tests/handshake-error-expected.txt: Added.
            * websocket/tests/handshake-error.html: Added.
            * websocket/tests/handshake-error_wsh.py: Added.
            * websocket/tests/script-tests/handshake-error.js: Added.
            (endTest):
            (ws.onopen):
            (ws.onmessage):
            (ws.onclose):
            (timeOutCallback):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/websocket/tests/script-tests/handshake-error.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Handshake error test");
+
+if (window.layoutTestController) {
+    layoutTestController.waitUntilDone();
+}
+
+function endTest()
+{
+    isSuccessfullyParsed();
+    if (window.layoutTestController) {
+        layoutTestController.notifyDone();
+    }
+}
+
+var ws = new WebSocket("ws://localhost:8880/websocket/tests/handshake-error");
+
+ws.onopen = function()
+{
+    testFailed("Unexpectedly Connected.");
+};
+
+ws.onmessage = function(messageEvent)
+{
+    testFailed("Unexpectedly Received: '" + messageEvent.data + "'");
+};
+
+ws.onclose = function()
+{
+    debug("Closed.");
+    shouldBe("ws.readyState", "2")
+    endTest();
+};
+
+function timeOutCallback()
+{
+    testFailed("Timed out in state: " + ws.readyState);
+    endTest();
+}
+
+window.setTimeout(timeOutCallback, 3000);
+
+var successfullyParsed = true;
