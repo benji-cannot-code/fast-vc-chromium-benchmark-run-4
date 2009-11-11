ee7a25aa183e123530ffdd9521a9d08d961e3a20
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-11 21:12:44 +0000

    TBR: pkasting
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31710 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/preferences_window_controller.h
+++ chrome/browser/cocoa/preferences_window_controller.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class PrefObserverBridge;
 class PrefService;
 class Profile;
-class ProfileSyncService;
 @class SearchEngineListModel;
 
 // A window controller that handles the preferences window. The bulk of the
@@ -32,8 +31,6 @@ class ProfileSyncService;
  @private
   Profile* profile_;  // weak ref
   PrefService* prefs_;  // weak ref - Obtained from profile_ for convenience.
-  // weak ref - Also obtained from profile_ for convenience.  May be NULL.
-  ProfileSyncService* sync_service_;
   scoped_ptr<PrefObserverBridge> observer_;  // Watches for pref changes.
 
   IBOutlet NSToolbar* toolbar_;
@@ -53,7 +50,6 @@ class ProfileSyncService;
   IBOutlet NSArray* basicsGroupDefaultBrowser_;
 
   // The groups of the Personal Stuff view for layout fixup.
-  IBOutlet NSArray* personalStuffGroupSync_;
   IBOutlet NSArray* personalStuffGroupPasswords_;
   IBOutlet NSArray* personalStuffGroupAutofill_;
   IBOutlet NSArray* personalStuffGroupBrowserData_;
@@ -78,9 +74,6 @@ class ProfileSyncService;
   // User Data panel
   BooleanPrefMember askSavePasswords_;
   BooleanPrefMember formAutofill_;
-  IBOutlet NSTextField* syncLabel_;
-  IBOutlet NSTextField* syncStatus_;
-  IBOutlet NSButton* syncButton_;
 
   // Under the hood panel
   IBOutlet NSView* underTheHoodContentView_;
@@ -120,7 +113,6 @@ class ProfileSyncService;
 - (IBAction)clearData:(id)sender;
 - (IBAction)resetThemeToDefault:(id)sender;
 - (IBAction)themesGallery:(id)sender;
-- (IBAction)doSyncAction:(id)sender;
 
 // Under the hood
 - (IBAction)browseDownloadLocation:(id)sender;
