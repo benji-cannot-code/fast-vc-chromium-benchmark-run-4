ba56efc9b654ca9a5d086938c66f85acd2f16a3a
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-11 01:14:43 +0000

    2009-11-10  Nate Chapin  <japhet@chromium.org>
            Unreviewed, Chromium build fix for http://trac.webkit.org/changeset/50760.
    
            * platform/chromium/PopupMenuChromium.cpp:
            (WebCore::PopupContainer::paintBorder):
            (WebCore::PopupListBox::paint):
            (WebCore::PopupListBox::paintRow):
            * platform/graphics/chromium/TransparencyWin.cpp:
            (WebCore::TransparencyWin::setupLayerForWhiteLayer):
            * platform/graphics/skia/GraphicsContextSkia.cpp:
            (WebCore::GraphicsContext::fillRect):
            (WebCore::GraphicsContext::fillRoundedRect):
            (WebCore::GraphicsContext::setPlatformFillColor):
            (WebCore::GraphicsContext::setPlatformStrokeColor):
            * rendering/RenderMediaControlsChromium.cpp:
            (WebCore::paintMediaSlider):
            (WebCore::paintMediaVolumeSlider):
            (WebCore::paintMediaTimelineContainer):
            * rendering/RenderThemeChromiumMac.mm:
            (WebCore::RenderThemeChromiumMac::paintMenuListButton):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50781 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
+++ third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
@@ -732,7 +732,7 @@ void GraphicsContext::fillRect(const FloatRect& rect)
     platformContext()->canvas()->drawRect(r, paint);
 }
 
-void GraphicsContext::fillRect(const FloatRect& rect, const Color& color)
+void GraphicsContext::fillRect(const FloatRect& rect, const Color& color, ColorSpace colorSpace)
 {
     if (paintingDisabled())
         return;
@@ -763,7 +763,8 @@ void GraphicsContext::fillRoundedRect(const IntRect& rect,
                                       const IntSize& topRight,
                                       const IntSize& bottomLeft,
                                       const IntSize& bottomRight,
-                                      const Color& color)
+                                      const Color& color,
+                                      ColorSpace colorSpace)
 {
     if (paintingDisabled())
         return;
@@ -780,7 +781,7 @@ void GraphicsContext::fillRoundedRect(const IntRect& rect,
         // Not all the radii fit, return a rect. This matches the behavior of
         // Path::createRoundedRectangle. Without this we attempt to draw a round
         // shadow for a square box.
-        fillRect(rect, color);
+        fillRect(rect, color, colorSpace);
         return;
     }
 
@@ -948,7 +949,7 @@ void GraphicsContext::setMiterLimit(float limit)
     platformContext()->setMiterLimit(limit);
 }
 
-void GraphicsContext::setPlatformFillColor(const Color& color)
+void GraphicsContext::setPlatformFillColor(const Color& color, ColorSpace colorSpace)
 {
     if (paintingDisabled())
         return;
@@ -1017,7 +1018,7 @@ void GraphicsContext::setPlatformShadow(const IntSize& size,
     dl->unref();
 }
 
-void GraphicsContext::setPlatformStrokeColor(const Color& strokecolor)
+void GraphicsContext::setPlatformStrokeColor(const Color& strokecolor, ColorSpace colorSpace)
 {
     if (paintingDisabled())
         return;
