80209bdee8e562afc965f7939f4bf77276677043
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-11 21:10:11 +0000

    2009-11-11  Yaar Schnitman  <yaar@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Tiny path fixes in Chromium API includes.
    
            https://bugs.webkit.org/show_bug.cgi?id=31369
    
            * public/linux/WebSandboxSupport.h: Fixed path.
            * src/WebBindings.cpp: Fixed path.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50836 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/linux/WebSandboxSupport.h
+++ third_party/WebKit/WebKit/chromium/public/linux/WebSandboxSupport.h
@@ -32,8 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebSandboxSupport_h
 #define WebSandboxSupport_h
 
-#include "webkit/api/public/WebCommon.h"
-#include "webkit/api/public/WebString.h"
+#include "../WebCommon.h"
+#include "../WebString.h"
 
 namespace WebKit {
 
--- third_party/WebKit/WebKit/chromium/src/WebBindings.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebBindings.cpp
@@ -34,8 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "npruntime_impl.h"
 #include "npruntime_priv.h"
-#include "webkit/api/public/WebDragData.h"
-#include "webkit/api/public/WebRange.h"
+
+#include "WebDragData.h"
+#include "WebRange.h"
 
 #if USE(V8)
 #include "ChromiumDataObject.h"
