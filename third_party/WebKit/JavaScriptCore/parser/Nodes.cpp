5d0a40db801f47c479a21f5a3835ed72abebf474
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-12 21:07:51 +0000

    Start unifying entry logic for function and eval code.
    Reviewed by Gavin Barraclough.
    
    Eval now uses a ret instruction to end execution, and sets up
    a callframe more in line with what we do for function entry.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50896 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/parser/Nodes.cpp
+++ third_party/WebKit/JavaScriptCore/parser/Nodes.cpp
@@ -1978,7 +1978,7 @@ RegisterID* EvalNode::emitBytecode(BytecodeGenerator& generator, RegisterID*)
     emitStatementsBytecode(generator, dstRegister.get());
 
     generator.emitDebugHook(DidExecuteProgram, firstLine(), lastLine());
-    generator.emitEnd(dstRegister.get());
+    generator.emitReturn(dstRegister.get());
     return 0;
 }
 
