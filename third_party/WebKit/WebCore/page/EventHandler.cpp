f8c571eea1ffabaca64e19e92f8d159b66fc0f13
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-12 09:03:32 +0000

    2009-11-12  Kinuko Yasuda  <kinuko@google.com>
            Reviewed by David Levin.
    
            Support Gtk scrollwheel behavior for horizontal scrollbars on Linux
            Chromium too.
            https://bugs.webkit.org/show_bug.cgi?id=31292
    
            No new tests. (Corresponding test for Gtk+ is
            platform/gtk/scrollbars/overflow-scrollbar-horizontal-wheel-scroll.html)
    
            * page/EventHandler.cpp:
            * page/chromium/EventHandlerChromium.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50871 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/WebCore/page/EventHandler.cpp
@@ -1792,7 +1792,7 @@ bool EventHandler::dispatchMouseEvent(const AtomicString& eventType, Node* targe
     return swallowEvent;
 }
 
-#if !PLATFORM(GTK)
+#if !PLATFORM(GTK) && !(PLATFORM(CHROMIUM) && PLATFORM(LINUX))
 bool EventHandler::shouldTurnVerticalTicksIntoHorizontal(const HitTestResult&) const
 {
     return false;
