5b425bc571b39261f512fc607c2cd0024498e737
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-12 20:55:35 +0000

    Print allocation callstacks to LOG(ERROR) rather than std err, since otherwise it doesn't show up inline in the log file. Review URL: http://codereview.chromium.org/391029
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31822 0039d316-1c4b-4281-b951-d872f2087c98

--- base/leak_tracker.h
+++ base/leak_tracker.h
@@ -91,7 +91,7 @@ class LeakTracker : public LinkNode<LeakTracker<T> > {
 
       ++count;
       LOG(ERROR) << "Leaked " << node << " which was allocated by:";
-      allocation_stack.PrintBacktrace();
+      allocation_stack.OutputToStream(&LOG(ERROR));
     }
 
     CHECK(0u == count);
