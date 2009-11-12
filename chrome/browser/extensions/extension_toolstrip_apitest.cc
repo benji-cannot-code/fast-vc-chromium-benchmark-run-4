673050de79c3215429df700146702ee4a53419e8
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-12 19:08:55 +0000

    disable toolstrip test for chromeos/linux_view
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/391033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31807 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_toolstrip_apitest.cc
+++ chrome/browser/extensions/extension_toolstrip_apitest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
+#if !defined(OS_CHROMEOS)
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Toolstrip) {
   ASSERT_TRUE(RunExtensionTest("toolstrip")) << message_;
 }
+#endif
