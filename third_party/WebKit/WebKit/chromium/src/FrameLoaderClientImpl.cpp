2fd999b9e08aa7408d5faa1dc822f8af0e52e6a3
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-12 20:03:56 +0000

    2009-11-12  Adam Barth  <abarth>
            Reviewed by Dimitri Glazkov.
    
            [Chromium] reload-subframe-object.html fails
            https://bugs.webkit.org/show_bug.cgi?id=31398
    
            More closely match FrameLoaderClient.mm.  Turns out we don't want to
            use MIMETypeRegistry::getMIMETypeForPath because we can't distinguish
            between application/octet-stream and lack of knowledge of the MIME
            type.
    
            Covered by reload-subframe-object.html.
    
            * src/FrameLoaderClientImpl.cpp:
            (WebKit::FrameLoaderClientImpl::objectContentType):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50891 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/FrameLoaderClientImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/FrameLoaderClientImpl.cpp
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Page.h"
 #include "PlatformString.h"
 #include "PluginData.h"
+#include "PluginDataChromium.h"
 #include "StringExtras.h"
 #include "WebDataSourceImpl.h"
 #include "WebDevToolsAgentPrivate.h"
@@ -1349,8 +1350,15 @@ ObjectContentType FrameLoaderClientImpl::objectContentType(
         // Try to guess the MIME type based off the extension.
         String filename = url.lastPathComponent();
         int extensionPos = filename.reverseFind('.');
-        if (extensionPos >= 0)
-            mimeType = MIMETypeRegistry::getMIMETypeForPath(url.path());
+        if (extensionPos >= 0) {
+            String extension = filename.substring(extensionPos + 1);
+            mimeType = MIMETypeRegistry::getMIMETypeForExtension(extension);
+            if (mimeType.isEmpty()) {
+                // If there's no mimetype registered for the extension, check to see
+                // if a plugin can handle the extension.
+                mimeType = getPluginMimeTypeFromExtension(extension);
+            }
+        }
 
         if (mimeType.isEmpty())
             return ObjectContentFrame;
