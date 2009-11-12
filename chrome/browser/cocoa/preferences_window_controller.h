0bf8503180c58a957d4f24f911bde1838625b7ce
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-12 07:58:10 +0000

    Added Sync-related controls to Preferences pane under "Personal Stuff".
    These controls are only visible when sync is enabled.  This isn't finished,
    but it is enough to be usable.
    
    Made RemoveViewFromView() handle top-most views.  Also added
    RemoveGroupFromView().
    
    BUG=23073
    TEST=manual testing, trybots
    
    Review URL: http://codereview.chromium.org/380006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31775 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/preferences_window_controller.h
+++ chrome/browser/cocoa/preferences_window_controller.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class PrefObserverBridge;
 class PrefService;
 class Profile;
+class ProfileSyncService;
 @class SearchEngineListModel;
 
 // A window controller that handles the preferences window. The bulk of the
@@ -31,6 +32,8 @@ class Profile;
  @private
   Profile* profile_;  // weak ref
   PrefService* prefs_;  // weak ref - Obtained from profile_ for convenience.
+  // weak ref - Also obtained from profile_ for convenience.  May be NULL.
+  ProfileSyncService* syncService_;
   scoped_ptr<PrefObserverBridge> observer_;  // Watches for pref changes.
 
   IBOutlet NSToolbar* toolbar_;
@@ -50,6 +53,7 @@ class Profile;
   IBOutlet NSArray* basicsGroupDefaultBrowser_;
 
   // The groups of the Personal Stuff view for layout fixup.
+  IBOutlet NSArray* personalStuffGroupSync_;
   IBOutlet NSArray* personalStuffGroupPasswords_;
   IBOutlet NSArray* personalStuffGroupAutofill_;
   IBOutlet NSArray* personalStuffGroupBrowserData_;
@@ -74,6 +78,8 @@ class Profile;
   // User Data panel
   BooleanPrefMember askSavePasswords_;
   BooleanPrefMember formAutofill_;
+  IBOutlet NSButton* syncButton_;
+  IBOutlet NSTextField* syncStatus_;
 
   // Under the hood panel
   IBOutlet NSView* underTheHoodContentView_;
@@ -113,6 +119,7 @@ class Profile;
 - (IBAction)clearData:(id)sender;
 - (IBAction)resetThemeToDefault:(id)sender;
 - (IBAction)themesGallery:(id)sender;
+- (IBAction)doSyncAction:(id)sender;
 
 // Under the hood
 - (IBAction)browseDownloadLocation:(id)sender;
