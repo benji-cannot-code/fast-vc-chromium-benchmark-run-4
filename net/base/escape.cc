11d719b0d09ad156dbef384a532e94036ba3f628
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-13 19:27:48 +0000

    Split *AndAdjustOffset() functions into their own header, to restore utf_string_conversions.h to a simple, readable state.
    BUG=4010
    TEST=none
    Review URL: http://codereview.chromium.org/387012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31928 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/escape.cc
+++ net/base/escape.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/i18n/icu_string_conversions.h"
 #include "base/logging.h"
-#include "base/string_util.h"
+#include "base/string_piece.h"
+#include "base/utf_string_conversions.h"
+#include "base/utf_offset_string_conversions.h"
 
 namespace {
 
