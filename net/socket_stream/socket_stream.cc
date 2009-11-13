98d881203b7533ad26d052f67ce3f3d617bbe0fd
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-13 04:48:01 +0000

    Close immediately if no context is set
    Profile::GetDefaultRequestContext() is initialized lazily, on the UI thread.
    So, if SocketStreamHost runs before it has gotten lazily initialized yet,
    SocketStream won't have context. In this case, SocketStream will die.
    Rather than that, just close immediately.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/384077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31886 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket_stream/socket_stream.cc
+++ net/socket_stream/socket_stream.cc
@@ -113,7 +113,8 @@ void SocketStream::Connect() {
       "The current MessageLoop must exist";
   DCHECK_EQ(MessageLoop::TYPE_IO, MessageLoop::current()->type()) <<
       "The current MessageLoop must be TYPE_IO";
-  ssl_config_service()->GetSSLConfig(&ssl_config_);
+  if (context_)
+    ssl_config_service()->GetSSLConfig(&ssl_config_);
   DCHECK_EQ(next_state_, STATE_NONE);
 
   AddRef();  // Released in Finish()
@@ -328,6 +329,10 @@ void SocketStream::OnWriteCompleted(int result) {
 }
 
 void SocketStream::DoLoop(int result) {
+  // If context was not set, close immediately.
+  if (!context_)
+    next_state_ = STATE_CLOSE;
+
   if (next_state_ == STATE_NONE)
     return;
 
