e69d0d15be207aa7c1187c53a09d7cd2fd66e67a
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-14 00:25:12 +0000

    Duplicating the SetActiveURL() change from the renderer to the plugin process. (Should also fix a plugin callback that may be clearing the renderer's URL prematurely.)
    BUG=22033
    TEST=None
    
    Review URL: http://codereview.chromium.org/384130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31971 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/plugin/webplugin_delegate_stub.cc
+++ chrome/plugin/webplugin_delegate_stub.cc
@@ -60,7 +60,7 @@ WebPluginDelegateStub::WebPluginDelegateStub(
 
 WebPluginDelegateStub::~WebPluginDelegateStub() {
   in_destructor_ = true;
-  child_process_logging::ScopedActiveURLSetter url_setter(page_url_);
+  child_process_logging::SetActiveURL(page_url_);
 
   if (channel_->in_send()) {
     // The delegate or an npobject is in the callstack, so don't delete it
@@ -77,7 +77,7 @@ WebPluginDelegateStub::~WebPluginDelegateStub() {
 }
 
 void WebPluginDelegateStub::OnMessageReceived(const IPC::Message& msg) {
-  child_process_logging::ScopedActiveURLSetter url_setter(page_url_);
+  child_process_logging::SetActiveURL(page_url_);
 
   // A plugin can execute a script to delete itself in any of its NPP methods.
   // Hold an extra reference to ourself so that if this does occur and we're
@@ -129,7 +129,7 @@ bool WebPluginDelegateStub::Send(IPC::Message* msg) {
 void WebPluginDelegateStub::OnInit(const PluginMsg_Init_Params& params,
                                    bool* result) {
   page_url_ = params.page_url;
-  child_process_logging::ScopedActiveURLSetter url_setter(page_url_);
+  child_process_logging::SetActiveURL(page_url_);
 
   *result = false;
   if (params.arg_names.size() != params.arg_values.size()) {
