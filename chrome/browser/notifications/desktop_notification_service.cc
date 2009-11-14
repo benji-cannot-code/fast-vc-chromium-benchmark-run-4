233f7309fe439c57bd2872d8510b0a5c10185262
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-14 02:32:11 +0000

    Escape javascript going into text notifications.
    BUG=27674
    TEST=included
    
    Review URL: http://codereview.chromium.org/386035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31989 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/desktop_notification_service.cc
+++ chrome/browser/notifications/desktop_notification_service.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
+#include "net/base/escape.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebNotificationPresenter.h"
 
 using WebKit::WebNotificationPresenter;
@@ -57,8 +58,8 @@ static string16 CreateDataUrl(const GURL& icon_url, const string16& title,
   else
     subst.push_back(EmptyString16());
 
-  subst.push_back(title);
-  subst.push_back(body);
+  subst.push_back(UTF8ToUTF16(EscapeForHTML(UTF16ToUTF8(title))));
+  subst.push_back(UTF8ToUTF16(EscapeForHTML(UTF16ToUTF8(body))));
 
   if (icon_url.is_valid()) {
     subst.push_back(ASCIIToUTF16("block"));
