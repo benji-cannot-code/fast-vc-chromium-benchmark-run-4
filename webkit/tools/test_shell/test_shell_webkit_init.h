204ec08ba9800a61d277eb64307d925753644191
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-14 05:09:03 +0000

    Enables most DB layout tests in test_shell.
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/384118
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31997 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_webkit_init.h
+++ webkit/tools/test_shell/test_shell_webkit_init.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "media/base/media.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebData.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebDatabase.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebRuntimeFeatures.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebKit.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebScriptController.h"
@@ -59,6 +60,7 @@ class TestShellWebKitInit : public webkit_glue::WebKitClientImpl {
         extensions_v8::IntervalExtension::Get());
     WebKit::WebRuntimeFeatures::enableSockets(true);
     WebKit::WebRuntimeFeatures::enableApplicationCache(true);
+    WebKit::WebRuntimeFeatures::enableDatabase(true);
 
     // Load libraries for media and enable the media player.
     FilePath module_path;
@@ -71,6 +73,8 @@ class TestShellWebKitInit : public webkit_glue::WebKitClientImpl {
     // content during the run. Upon exit that directory is deleted.
     if (appcache_dir_.CreateUniqueTempDir())
       SimpleAppCacheSystem::InitializeOnUIThread(appcache_dir_.path());
+
+    WebKit::WebDatabase::setObserver(&database_system_);
 
 #if defined(OS_WIN)
     // Ensure we pick up the default theme engine.
@@ -105,27 +109,27 @@ class TestShellWebKitInit : public webkit_glue::WebKitClientImpl {
   }
 
   virtual WebKit::WebKitClient::FileHandle databaseOpenFile(
-      const WebKit::WebString& file_name, int desired_flags,
+      const WebKit::WebString& vfs_file_name, int desired_flags,
       WebKit::WebKitClient::FileHandle* dir_handle) {
     return SimpleDatabaseSystem::GetInstance()->OpenFile(
-        webkit_glue::WebStringToFilePath(file_name),
-        desired_flags, dir_handle);
+        vfs_file_name, desired_flags, dir_handle);
   }
 
-  virtual int databaseDeleteFile(const WebKit::WebString& file_name,
+  virtual int databaseDeleteFile(const WebKit::WebString& vfs_file_name,
                                  bool sync_dir) {
     return SimpleDatabaseSystem::GetInstance()->DeleteFile(
-        webkit_glue::WebStringToFilePath(file_name), sync_dir);
+        vfs_file_name, sync_dir);
   }
 
-  virtual long databaseGetFileAttributes(const WebKit::WebString& file_name) {
+  virtual long databaseGetFileAttributes(
+      const WebKit::WebString& vfs_file_name) {
     return SimpleDatabaseSystem::GetInstance()->GetFileAttributes(
-        webkit_glue::WebStringToFilePath(file_name));
+        vfs_file_name);
   }
 
-  virtual long long databaseGetFileSize(const WebKit::WebString& file_name) {
-    return SimpleDatabaseSystem::GetInstance()->GetFileSize(
-        webkit_glue::WebStringToFilePath(file_name));
+  virtual long long databaseGetFileSize(
+      const WebKit::WebString& vfs_file_name) {
+    return SimpleDatabaseSystem::GetInstance()->GetFileSize(vfs_file_name);
   }
 
   virtual bool getFileSize(const WebKit::WebString& path, long long& result) {
