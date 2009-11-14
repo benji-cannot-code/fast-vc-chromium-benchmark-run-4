c94cd1122c7ce0f1366d72e6ea0ed4578d28720f
aa@chromium.org <aa@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-14 02:45:28 +0000

    2009-11-13  Aaron Boodman  <aa@chromium.org>
            Unreviewed fix for Chromium build.
    
            * platform/text/TextBoundaries.cpp: Use longer path to refer to Unicode.h.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50989 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/TextBoundaries.cpp
+++ third_party/WebKit/WebCore/platform/text/TextBoundaries.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "StringImpl.h"
 #include "TextBreakIterator.h"
-#include <Unicode.h>
+#include <wtf/unicode/Unicode.h>
 
 using namespace WTF;
 using namespace Unicode;
