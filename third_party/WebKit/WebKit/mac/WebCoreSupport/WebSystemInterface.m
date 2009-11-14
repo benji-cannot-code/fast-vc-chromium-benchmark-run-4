c2f68e3a3f406d11a6d3e1cea843e1ce7720951a
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-14 17:53:34 +0000

    2009-11-14  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Oliver Hunt.
    
            <rdar://problem/7287487> 
            Do not use QuickTime version to detect media controller theme
    
            * WebCore.base.exp: Export wkMediaControllerThemeAvailable
            * platform/mac/WebCoreSystemInterface.h: Ditto.
            * platform/mac/WebCoreSystemInterface.mm: Ditto.
    
            * rendering/RenderThemeMac.mm:
            (WebCore::mediaControllerTheme): Use wkMediaControllerThemeAvailable instead of the
            QuickTime version to see if it is possible to use MediaControllerThemeQuickTime.
    
    2009-11-14  Eric Carlson  <eric.carlson@apple.com>
    
            Reviewed by Oliver Hunt.
    
            <rdar://problem/7287487> 
            Do not use QuickTime version to detect media controller theme
    
            * WebCoreSupport/WebSystemInterface.m:
            (InitWebCoreSystemInterface): Initialize wkMediaControllerThemeAvailable.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50997 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebSystemInterface.m
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebSystemInterface.m
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright 2006, 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -63,6 +63,7 @@ void InitWebCoreSystemInterface(void)
     INIT(InitializeMaximumHTTPConnectionCountPerHost);
     INIT(IsLatchingWheelEvent);
     INIT(MeasureMediaUIPart);
+    INIT(MediaControllerThemeAvailable);
     INIT(PopupMenu);
     INIT(SetCGFontRenderingMode);
     INIT(SetDragImage);
