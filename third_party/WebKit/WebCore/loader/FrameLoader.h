08f64051ec78bf1e516facdf46bc5cd9a57122bf
aa@chromium.org <aa@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-14 03:11:06 +0000

    2009-11-13  Aaron Boodman  <aa@chromium.org>
            Unreviewed fix for Chromium build.
    
            * loader/FrameLoader.h: Make dispatchDidClearWindowObjectsInAllWorlds()
            public, as Chromium's V8Proxy calls it.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@50991 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/WebCore/loader/FrameLoader.h
@@ -249,6 +249,7 @@ public:
     PassRefPtr<Widget> createJavaAppletWidget(const IntSize&, HTMLAppletElement*, const HashMap<String, String>& args);
 
     void dispatchDidClearWindowObjectInWorld(DOMWrapperWorld*);
+    void dispatchDidClearWindowObjectsInAllWorlds();
     void dispatchDocumentElementAvailable();
 
     // Mixed content related functions.
@@ -396,7 +397,6 @@ private:
     bool shouldReloadToHandleUnreachableURL(DocumentLoader*);
 
     void dispatchDidCommitLoad();
-    void dispatchDidClearWindowObjectsInAllWorlds();
 
     void loadWithDocumentLoader(DocumentLoader*, FrameLoadType, PassRefPtr<FormState>); // Calls continueLoadAfterNavigationPolicy
     void load(DocumentLoader*);                                                         // Calls loadWithDocumentLoader   
