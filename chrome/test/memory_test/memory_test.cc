eb0cc474992a13577ac69a5c4383cb71925ff95e
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-14 00:56:05 +0000

    Mark memory_test as flaky on 64bit linux
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/386034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31975 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/memory_test/memory_test.cc
+++ chrome/test/memory_test/memory_test.cc
@@ -23,6 +23,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if defined(OS_LINUX) && __x86_64__
+#define MAYBE_SingleTabTest FLAKY_SingleTabTest
+#define MAYBE_FiveTabTest FLAKY_FiveTabTest
+#define MAYBE_TwelveTabTest FLAKY_TwelveTabTest
+#else
+#define MAYBE_SingleTabTest SingleTabTest
+#define MAYBE_FiveTabTest FiveTabTest
+#define MAYBE_TwelveTabTest TwelveTabTest
+#endif
+
 namespace {
 
 static const FilePath::CharType kTempDirName[] =
@@ -634,15 +644,15 @@ std::string MembusterMemoryTest::source_urls_[] = {
 size_t MembusterMemoryTest::urls_length_ =
     arraysize(MembusterMemoryTest::source_urls_);
 
-TEST_F(GeneralMixMemoryTest, SingleTabTest) {
+TEST_F(GeneralMixMemoryTest, MAYBE_SingleTabTest) {
   RunTest("1t", 1);
 }
 
-TEST_F(GeneralMixMemoryTest, FiveTabTest) {
+TEST_F(GeneralMixMemoryTest, MAYBE_FiveTabTest) {
   RunTest("5t", 5);
 }
 
-TEST_F(GeneralMixMemoryTest, TwelveTabTest) {
+TEST_F(GeneralMixMemoryTest, MAYBE_TwelveTabTest) {
   RunTest("12t", 12);
 }
 
