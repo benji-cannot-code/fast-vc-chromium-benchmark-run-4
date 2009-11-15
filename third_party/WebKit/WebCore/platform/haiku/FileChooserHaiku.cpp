8541dbf8e1983802ff3f1cb7fb099afd8f5a026a
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-15 23:28:02 +0000

    2009-11-15  Maxime Simon  <simon.maxime@gmail.com>
            Reviewed by Adam Barth.
    
            [Haiku] Build fix. The FileChooser constructor doesn't need to be redefined.
    
            * platform/haiku/FileChooserHaiku.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51003 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/haiku/FileChooserHaiku.cpp
+++ third_party/WebKit/WebCore/platform/haiku/FileChooserHaiku.cpp
@@ -29,14 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-FileChooser::FileChooser(FileChooserClient* client, const String& filename)
-    : m_client(client)
-    , m_filenames()
-    , m_icon(chooseIcon(filename))
-{
-    m_filenames.append(filename);
-}
-
 String FileChooser::basenameForWidth(const Font&, int width) const
 {
     notImplemented();
