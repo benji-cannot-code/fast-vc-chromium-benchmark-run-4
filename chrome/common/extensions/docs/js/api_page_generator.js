cd8e0337f083920e0620beec89078e3b4b5d81ca
kathyw@google.com <kathyw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-16 07:11:07 +0000

    Changes to follow the codesite L&F. Also a bit of copyediting and cleanup.
    Future work:
     * improve these styles
     * make these styles work well in IE
       (the old styles didn't work well in IE, either)
     * separate the codesite styles from ours (for easier update)
    
    BUG=27434
    TEST=none
    TBR=aa
    
    Review URL: http://codereview.chromium.org/398005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32043 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/js/api_page_generator.js
+++ chrome/common/extensions/docs/js/api_page_generator.js
@@ -85,7 +85,7 @@ function renderPage() {
     alert("Empty page name for: " + document.location.href);
     return;
   }
-  
+
   pageName = pageBase.replace(/([A-Z])/g, " $1");
   pageName = pageName.substring(0, 1).toUpperCase() + pageName.substring(1);
 
@@ -95,7 +95,7 @@ function renderPage() {
     fetchStatic();
   }, function(error) {
     alert("Failed to load " + API_TEMPLATE + ". " + error);
-  });	
+  });
 }
 
 function fetchStatic() {
@@ -116,7 +116,7 @@ function fetchSchema() {
   fetchContent(SCHEMA, function(schemaContent) {
     schema = JSON.parse(schemaContent);
     renderTemplate();
-    
+
   }, function(error) {
     alert("Failed to load " + SCHEMA);
   });
@@ -172,10 +172,10 @@ function renderTemplate() {
     if (mod.namespace == pageBase) {
       // This page is an api page. Setup types and apiDefinition.
       module = mod;
-      apiModuleTitle = "chrome." + module.namespace;  
+      apiModuleTitle = "chrome." + module.namespace;
       pageData.apiDefinition = module;
     }
-    
+
     if (mod.types) {
       mod.types.each(function(type) {
         typeModule[type.id] = mod;
@@ -189,7 +189,7 @@ function renderTemplate() {
   jstProcess(input, output);
 
   selectCurrentPageOnLeftNav();
-  
+
   document.title = getPageTitle();
   // Show
   if (window.postRender)
@@ -226,7 +226,7 @@ function evalXPathFromNode(expression, node) {
   while(n = results.iterateNext()) {
     retval.push(n);
   }
-  
+
   return retval;
 }
 
@@ -234,7 +234,7 @@ function evalXPathFromId(expression, id) {
   return evalXPathFromNode(expression, document.getElementById(id));
 }
 
-// Select the current page on the left nav. Note: if already rendered, this 
+// Select the current page on the left nav. Note: if already rendered, this
 // will not effect any nodes.
 function selectCurrentPageOnLeftNav() {
   function finalPathPart(str) {
@@ -242,10 +242,10 @@ function selectCurrentPageOnLeftNav() {
     var lastPart = pathParts[pathParts.length - 1];
     return lastPart.split(/\?/)[0];
   }
-  
+
   var pageBase = finalPathPart(document.location.href);
-  
-  evalXPathFromId(".//li/a", "leftNav").select(function(node) {
+
+  evalXPathFromId(".//li/a", "gc-toc").select(function(node) {
     if (pageBase == finalPathPart(node.href)) {
       var parent = node.parentNode;
       if (node.firstChild.nodeName == 'DIV') {
@@ -256,7 +256,7 @@ function selectCurrentPageOnLeftNav() {
       parent.removeChild(node);
       parent.insertBefore(node.firstChild, parent.firstChild);
       return true;
-    }  
+    }
   });
 }
 
@@ -288,7 +288,7 @@ function getStaticTOC() {
   var staticHNodes = evalXPathFromId(".//h2|h3", "static");
   var retval = [];
   var lastH2;
-  
+
   staticHNodes.each(function(n, i) {
     var anchorName = n.id || n.nodeName + "-" + i;
     if (!n.id) {
@@ -297,7 +297,7 @@ function getStaticTOC() {
       n.parentNode.insertBefore(a, n);
     }
     var dataNode = { name: n.innerHTML, href: anchorName };
-    
+
     if (n.nodeName == "H2") {
       retval.push(dataNode);
       lastH2 = dataNode;
@@ -306,7 +306,7 @@ function getStaticTOC() {
       lastH2.children.push(dataNode);
     }
   });
-   
+
   return retval;
 }
 
@@ -315,8 +315,8 @@ function getTypeRefPage(type) {
 }
 
 function getPageTitle() {
-  return getDataFromPageHTML("pageData-title") || 
-         apiModuleTitle || 
+  return getDataFromPageHTML("pageData-title") ||
+         apiModuleTitle ||
          pageName;
 }
 
@@ -379,7 +379,7 @@ function getSignatureString(parameters) {
     retval.push(getTypeName(param) + " " + param.name);
   });
 
-  return retval.join(", ");	
+  return retval.join(", ");
 }
 
 function sortByName(a, b) {
