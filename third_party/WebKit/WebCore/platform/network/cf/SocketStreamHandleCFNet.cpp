04af11963774cd0b6b298dac656addd1f897074b
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-16 21:28:26 +0000

    Windows build fix.
            * platform/network/cf/SocketStreamHandleCFNet.cpp: Declare constants as extern "C".
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
@@ -41,9 +41,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <SystemConfiguration/SystemConfiguration.h>
 #endif
 
+extern "C" {
 extern const CFStringRef kCFStreamPropertyCONNECTProxy;
 extern const CFStringRef kCFStreamPropertyCONNECTProxyHost;
 extern const CFStringRef kCFStreamPropertyCONNECTProxyPort;
+}
 
 namespace WebCore {
 
