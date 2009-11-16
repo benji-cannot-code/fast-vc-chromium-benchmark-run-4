ff97a0036296af2dd59baa76f6d719aa78813bc5
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-16 21:45:17 +0000

    Use GetSwitchValuePath.
    BUG=24672
    TEST=unit_tests
    Patch by Thiago Farina <thiago.farina@gmail.com>.
    
    Review URL: http://codereview.chromium.org/375016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32093 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automated_ui_tests/automated_ui_tests.cc
+++ chrome/test/automated_ui_tests/automated_ui_tests.cc
@@ -74,8 +74,7 @@ void SilentRuntimeReportHandler(const std::string& str) {
 FilePath GetInputFilePath() {
   const CommandLine& parsed_command_line = *CommandLine::ForCurrentProcess();
   if (parsed_command_line.HasSwitch(kInputFilePathSwitch)) {
-    return FilePath::FromWStringHack(
-        parsed_command_line.GetSwitchValue(kInputFilePathSwitch));
+    return parsed_command_line.GetSwitchValuePath(kInputFilePathSwitch);
   } else {
     return FilePath(kDefaultInputFilePath);
   }
@@ -84,8 +83,7 @@ FilePath GetInputFilePath() {
 FilePath GetOutputFilePath() {
   const CommandLine& parsed_command_line = *CommandLine::ForCurrentProcess();
   if (parsed_command_line.HasSwitch(kOutputFilePathSwitch)) {
-    return FilePath::FromWStringHack(
-        parsed_command_line.GetSwitchValue(kOutputFilePathSwitch));
+    return parsed_command_line.GetSwitchValuePath(kOutputFilePathSwitch);
   } else {
     return FilePath(kDefaultOutputFilePath);
   }
