979e9f5834b93b564bba558bda31b46264b22a33
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-16 22:30:55 +0000

    Windows build fix.
            * platform/network/cf/SocketStreamHandleCFNet.cpp: (WebCore::SocketStreamHandle::createStreams):
            Disable CONNECT proxies on Windows until WebKitSupportLibrary includes support for those.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
@@ -41,11 +41,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <SystemConfiguration/SystemConfiguration.h>
 #endif
 
-extern "C" {
+#if PLATFORM(MAC) // WebKitSystemInterface doesn't include these symbols.
 extern const CFStringRef kCFStreamPropertyCONNECTProxy;
 extern const CFStringRef kCFStreamPropertyCONNECTProxyHost;
 extern const CFStringRef kCFStreamPropertyCONNECTProxyPort;
-}
+#endif
 
 namespace WebCore {
 
@@ -214,10 +214,13 @@ void SocketStreamHandle::createStreams()
         break;
         }
     case CONNECTProxy: {
+#if PLATFORM(MAC)
+        // FIXME: Make CONNECT proxies work on Windows.
         const void* proxyKeys[] = { kCFStreamPropertyCONNECTProxyHost, kCFStreamPropertyCONNECTProxyPort };
         const void* proxyValues[] = { m_proxyHost.get(), m_proxyPort.get() };
         RetainPtr<CFDictionaryRef> connectDictionary(AdoptCF, CFDictionaryCreate(0, proxyKeys, proxyValues, sizeof(proxyKeys) / sizeof(*proxyKeys), &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks));
         CFReadStreamSetProperty(m_readStream.get(), kCFStreamPropertyCONNECTProxy, connectDictionary.get());
+#endif
         break;
         }
     }
