1654f1a7db2202ecea79690a118be280a69e3d40
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-16 21:28:03 +0000

    API documentation fixes.
    Reviewed by Kenneth Rohde Christiansen.
    
    * Api/qgraphicswebview.cpp: Removed duplicate docs.
    * Api/qwebelement.cpp: Added missing docs.
    * Api/qwebsettings.cpp: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
+++ third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
@@ -204,12 +204,6 @@ QObject* QGraphicsWebViewPrivate::pluginParent() const
 */
 
 /*!
-    \fn void QGraphicsWebView::statusChanged()
-
-    This signal is emitted when the status bar text is changed by the page.
-*/
-
-/*!
     \fn void QGraphicsWebView::iconChanged()
 
     This signal is emitted whenever the icon of the page is loaded or changes.
@@ -225,7 +219,7 @@ QObject* QGraphicsWebViewPrivate::pluginParent() const
 
     This signal is emitted when a new load of the page is started.
 
-    \sa progressChanged(), loadFinished()
+    \sa loadProgress(), loadFinished()
 */
 
 /*!
@@ -236,22 +230,6 @@ QObject* QGraphicsWebViewPrivate::pluginParent() const
 
     \sa loadStarted()
 */
-
-/*!
-    \fn void QGraphicsWebView::progressChanged(qreal progress)
-
-    This signal is emitted every time an element in the web page
-    completes loading and the overall loading progress advances.
-
-    This signal tracks the progress of all child frames.
-
-    The current value is provided by \a progress and scales from 0.0 to 1.0,
-    which is the default range of QProgressBar.
-
-    \sa loadStarted(), loadFinished()
-*/
-
-
 
 /*!
     Constructs an empty QGraphicsWebView with parent \a parent.
@@ -1000,54 +978,11 @@ void QGraphicsWebView::inputMethodEvent(QInputMethodEvent* ev)
     if (!ev->isAccepted())
         QGraphicsItem::inputMethodEvent(ev);
 }
-/*!
-    \fn void QGraphicsWebView::titleChanged(const QString &title)
-
-    This signal is emitted whenever the \a title of the main frame changes.
-
-    \sa title()
-*/
-
-/*!
-    \fn void QGraphicsWebView::urlChanged(const QUrl &url)
-
-    This signal is emitted when the \a url of the view changes.
-
-    \sa url(), load()
-*/
 
 /*!
     \fn void QGraphicsWebView::statusBarMessage(const QString& text)
 
     This signal is emitted when the statusbar \a text is changed by the page.
-*/
-
-/*!
-    \fn void QGraphicsWebView::iconChanged()
-
-    This signal is emitted whenever the icon of the page is loaded or changes.
-
-    In order for icons to be loaded, you will need to set an icon database path
-    using QWebSettings::setIconDatabasePath().
-
-    \sa icon(), QWebSettings::setIconDatabasePath()
-*/
-
-/*!
-    \fn void QGraphicsWebView::loadStarted()
-
-    This signal is emitted when a new load of the page is started.
-
-    \sa loadProgress(), loadFinished()
-*/
-
-/*!
-    \fn void QGraphicsWebView::loadFinished(bool ok)
-
-    This signal is emitted when a load of the page is finished.
-    \a ok will indicate whether the load was successful or any error occurred.
-
-    \sa loadStarted()
 */
 
 /*!
