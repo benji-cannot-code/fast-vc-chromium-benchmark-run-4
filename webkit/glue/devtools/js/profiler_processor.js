0bb94230a5099c0fd22644d5de463d5513a8615a
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-16 08:00:56 +0000

    DevTools: fix timer leak.
    BUG=27718
    TEST=none
    
    Review URL: http://codereview.chromium.org/384144
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32044 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/profiler_processor.js
+++ webkit/glue/devtools/js/profiler_processor.js
@@ -229,6 +229,12 @@ devtools.profiler.Processor = function() {
   this.ticksCount_ = 0;
 
   /**
+   * Interval id for updating processing status.
+   * @type {number}
+   */
+  this.processingInterval_ = null;
+
+  /**
    * The current heap snapshot.
    * @type {string}
    */
@@ -303,7 +309,6 @@ devtools.profiler.Processor.prototype.setNewProfileCallback = function(
 
 devtools.profiler.Processor.prototype.processProfiler_ = function(
     state, params) {
-  var processingInterval = null;
   switch (state) {
     case 'resume':
       if (this.currentProfile_ == null) {
@@ -318,7 +323,7 @@ devtools.profiler.Processor.prototype.processProfiler_ = function(
         this.ticksCount_ = 0;
         var self = this;
         if (this.profileProcessingStatus_) {
-          processingInterval = window.setInterval(
+          this.processingInterval_ = window.setInterval(
               function() { self.profileProcessingStatus_(self.ticksCount_); },
               1000);
         }
@@ -326,7 +331,8 @@ devtools.profiler.Processor.prototype.processProfiler_ = function(
       break;
     case 'pause':
       if (this.currentProfile_ != null) {
-        window.clearInterval(processingInterval);
+        window.clearInterval(this.processingInterval_);
+        this.processingInterval_ = null;
         if (this.finishedProfileProcessing_) {
           this.finishedProfileProcessing_(this.createProfileForView());
         }
