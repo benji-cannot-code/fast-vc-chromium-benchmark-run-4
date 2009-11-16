15ccc1e6e419271feaa06a8e7aa756ec93691830
apavlov@chromium.org <apavlov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-16 17:35:41 +0000

    Fix typos in DevTools tests
    Review URL: http://codereview.chromium.org/397012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32063 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/tests.js
+++ webkit/glue/devtools/js/tests.js
@@ -340,12 +340,12 @@ TestSuite.prototype.testResourceHeaders = function() {
           var responseHeaders = JSON.stringify(resource.responseHeaders);
           test.assertContains(responseHeaders, 'Content-type');
           test.assertContains(responseHeaders, 'Content-Length');
-          test.assertTrue(typeof resource.responseReceivedTime != 'undefnied');
+          test.assertTrue(typeof resource.responseReceivedTime != 'undefined');
           responseOk = true;
         }
 
         if (payload.didTimingChange) {
-          test.assertTrue(typeof resource.startTime != 'undefnied');
+          test.assertTrue(typeof resource.startTime != 'undefined');
           timingOk = true;
         }
 
@@ -353,7 +353,7 @@ TestSuite.prototype.testResourceHeaders = function() {
           test.assertTrue(requestOk);
           test.assertTrue(responseOk);
           test.assertTrue(timingOk);
-          test.assertTrue(typeof resource.endTime != 'undefnied');
+          test.assertTrue(typeof resource.endTime != 'undefined');
           test.releaseControl();
         }
       }, true);
@@ -364,7 +364,7 @@ TestSuite.prototype.testResourceHeaders = function() {
 
 
 /**
- * Test that profiler works.
+ * Tests that profiler works.
  */
 TestSuite.prototype.testProfilerTab = function() {
   this.showPanel('profiles');
