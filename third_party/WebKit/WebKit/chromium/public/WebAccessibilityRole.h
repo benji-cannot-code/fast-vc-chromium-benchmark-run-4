5b8a4cf1916412041438fc55b6435c290359c40e
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-16 04:44:33 +0000

    2009-11-15  Dimitri Glazkov  <dglazkov@chromium.org>
            Unreviewed, build fix.
    
            Correct an error in the previous commit
    
            * public/WebAccessibilityRole.h: It should be WebAccessibilityRoleDirectory,
              not WebAccessibilityDirectoryRole.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebAccessibilityRole.h
+++ third_party/WebKit/WebKit/chromium/public/WebAccessibilityRole.h
@@ -108,7 +108,7 @@ enum WebAccessibilityRole {
     WebAccessibilityRoleTabPanel,
     WebAccessibilityRoleTreeRole,
     WebAccessibilityRoleTreeItemRole,
-    WebAccessibilityDirectoryRole,
+    WebAccessibilityRoleDirectory,
 
     WebAccessibilityRoleLandmarkApplication,
     WebAccessibilityRoleLandmarkBanner,
