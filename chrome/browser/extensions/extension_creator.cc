b7c78218ae0da78133ec6d69819c8b9199147999
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-17 07:06:16 +0000

    Add validation to extension packaging.
    BUG=27685,27690
    
    Review URL: http://codereview.chromium.org/398001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32163 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_creator.cc
+++ chrome/browser/extensions/extension_creator.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_handle.h"
 #include "base/scoped_temp_dir.h"
 #include "base/string_util.h"
+#include "chrome/browser/extensions/extension_file_util.h"
 #include "chrome/browser/extensions/sandboxed_extension_unpacker.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/zip.h"
@@ -50,6 +51,15 @@ bool ExtensionCreator::InitializeInput(
                        "Reuse that key or delete it first.";
       return false;
   }
+
+  // Load the extension once. We don't really need it, but this does a lot of
+  // useful validation of the structure.
+  scoped_ptr<Extension> extension(
+      extension_file_util::LoadExtension(extension_dir,
+                                         false,  // key not required
+                                         &error_message_));
+  if (!extension.get())
+    return false;  // LoadExtension already set error_message_.
 
   return true;
 }
