e13935bcba8eceea1bf24d86c43a3011fee8ba11
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-17 20:38:41 +0000

    2009-11-17  Martin Robinson  <martin.james.robinson@gmail.com>
            Reviewed by Adam Barth.
    
            [GTK] Style cleanup for GOwnPtr
            https://bugs.webkit.org/show_bug.cgi?id=31506
    
            Remove forward declaration in GOwnPtr and do some style cleanup.
    
            * wtf/GOwnPtr.cpp:
            * wtf/GOwnPtr.h:
            (WTF::GOwnPtr::GOwnPtr):
            (WTF::GOwnPtr::~GOwnPtr):
            (WTF::GOwnPtr::get):
            (WTF::GOwnPtr::release):
            (WTF::GOwnPtr::outPtr):
            (WTF::GOwnPtr::set):
            (WTF::GOwnPtr::clear):
            (WTF::GOwnPtr::operator*):
            (WTF::GOwnPtr::operator->):
            (WTF::GOwnPtr::operator!):
            (WTF::GOwnPtr::operator UnspecifiedBoolType):
            (WTF::GOwnPtr::swap):
            (WTF::swap):
            (WTF::operator==):
            (WTF::operator!=):
            (WTF::getPtr):
            (WTF::freeOwnedGPtr):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/GOwnPtr.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/GOwnPtr.cpp
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "GOwnPtr.h"
 
+#include <glib.h>
+
 namespace WTF {
 
 template <> void freeOwnedGPtr<GError>(GError* ptr)
