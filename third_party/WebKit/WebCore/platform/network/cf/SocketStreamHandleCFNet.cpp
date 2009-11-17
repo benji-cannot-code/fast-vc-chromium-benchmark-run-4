aca91d849b5bca81822e8ca6ed59a96b7ea0c76e
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-17 00:04:13 +0000

    Fix a typo in previous commit.
            * platform/network/cf/SocketStreamHandleCFNet.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDING_ON_TIGER
-#define CFN_EXPORT export
+#define CFN_EXPORT extern
 #endif
 
 extern "C" {
