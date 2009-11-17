cf2111c33ccf816907e037a33b05fd197b1b8652
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-17 00:09:50 +0000

    Fix a typo in previous commit.
            * platform/network/cf/SocketStreamHandleCFNet.cpp: #ifdef, not #if.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <SystemConfiguration/SystemConfiguration.h>
 #endif
 
-#if BUILDING_ON_TIGER
+#ifdef BUILDING_ON_TIGER
 #define CFN_EXPORT extern
 #endif
 
