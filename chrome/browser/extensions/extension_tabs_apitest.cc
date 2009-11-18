b05477122497d707bfe861718fbce84976705d3f
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-18 17:37:12 +0000

    Add various tests for chrome.tabs/windows. BUG=none TEST=none Review URL: http://codereview.chromium.org/355029
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32336 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -6,5 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Tabs) {
+  StartHTTPServer();
   ASSERT_TRUE(RunExtensionTest("tabs")) << message_;
 }
