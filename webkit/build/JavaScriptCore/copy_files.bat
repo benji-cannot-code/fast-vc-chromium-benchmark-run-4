0bab70c45c235edf5c6d53154ddbb10773ad7df2
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-18 18:27:38 +0000

    Revert "Delete a bunch of deprecated and/or empty directores in src/webkit/."
    This reverts commit r32340.
    
    TBR=japhet
    
    Review URL: http://codereview.chromium.org/402054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32352 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/build/JavaScriptCore/copy_files.bat
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+@echo off
+
+set DIR=%1
+set JAVASCRIPTCORE_DIR="..\..\..\third_party\WebKit\JavaScriptCore"
+setlocal
+
+mkDIR 2>NUL %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\APICast.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\JavaScript.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\JSBase.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\JSContextRef.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\JSObjectRef.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\JSStringRef.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\JSStringRefCF.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\JSStringRefBSTR.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\JSValueRef.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\JavaScriptCore.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\JSRetainPtr.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\OpaqueJSString.h" %DIR%
+xcopy /y /d "%JAVASCRIPTCORE_DIR%\API\WebKitAvailability.h" %DIR%
+
+endlocal
