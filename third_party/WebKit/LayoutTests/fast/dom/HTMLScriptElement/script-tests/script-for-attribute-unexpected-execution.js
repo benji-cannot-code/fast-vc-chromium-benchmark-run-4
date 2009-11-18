3a6b500ff9e34dc05227085126c051fd31e147cf
dimich@chromium.org <dimich@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-18 00:35:56 +0000

    WebCore: In all valid script tags for JavaScript, the event handler in <script...for> should not get executed. https://bugs.webkit.org/show_bug.cgi?id=31567
    Patch by Johnny Ding <jnd@chromium.org> on 2009-11-17
    Reviewed by Darin Adler.
    
    * dom/ScriptElement.cpp:
    (WebCore::ScriptElementData::shouldExecuteAsJavaScript):
    
    LayoutTests: In all valid script tags for JavaScript, the event handler in <script...for> should not get executed.
    Change the original test to pure js test, and test the situation about script tags which have text or language attribute.
    https://bugs.webkit.org/show_bug.cgi?id=31567
    
    Patch by Johnny Ding <jnd@chromium.org> on 2009-11-17
    Reviewed by Darin Adler.
    
    * fast/dom/HTMLScriptElement/script-for-attribute-unexpected-execution-expected.txt:
    * fast/dom/HTMLScriptElement/script-for-attribute-unexpected-execution.html:
    * fast/dom/HTMLScriptElement/script-tests: Added.
    * fast/dom/HTMLScriptElement/script-tests/script-for-attribute-unexpected-execution.js: Added.
    (ScriptForAttributeExecute):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51097 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/HTMLScriptElement/script-tests/script-for-attribute-unexpected-execution.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("If a script has a for attribute, then it was intended to only be run under certain conditions, often as a result of a certain window event.<br>Since we don't yet support the full for attribute syntax we would run these scripts as we parsed them, often causing unintentional breakage of the site in question.<br>You should *not* see any failure when running this test. If you do, we're not properly running these scripts only when they were intended to be run.");
+
+// A variable indicates the script-for attribute doesn't get executed immediately.
+var scriptForExecuted = false;
+
+function ScriptForAttributeExecute() {
+    scriptForExecuted = true;
+}
+
+document.write('<script for=window event=onresize> ScriptForAttributeExecute(); </script>');
+shouldBe('scriptForExecuted', "false");
+document.write('<script for=window event=onresize type="text/javascript"> ScriptForAttributeExecute(); </script>');
+shouldBe('scriptForExecuted', "false");
+document.write('<script for=window event=onresize language="javascript"> ScriptForAttributeExecute(); </script>');
+shouldBe('scriptForExecuted', "false");
+
+var successfullyParsed = true;
