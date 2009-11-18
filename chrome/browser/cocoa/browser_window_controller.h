ec00169c2fabd0b7c0747f17c3e6f9f47eb22f73
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-18 01:49:45 +0000

    Fix for parent window closing behind the bookmark bubble.
    BUG=27752
    TEST=none
    
    Review URL: http://codereview.chromium.org/397006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32251 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_window_controller.h
+++ chrome/browser/cocoa/browser_window_controller.h
@@ -46,7 +46,6 @@ class TabStripModelObserverBridge;
 @interface BrowserWindowController :
   TabWindowController<NSUserInterfaceValidations,
                       BookmarkBarControllerDelegate,
-                      BookmarkBubbleControllerDelegate,
                       BrowserCommandExecutor,
                       ViewResizer,
                       GTMThemeDelegate> {
@@ -74,9 +73,7 @@ class TabStripModelObserverBridge;
   // be shut down before our destructors are called.
   StatusBubbleMac* statusBubble_;
 
-  // Strong. We don't wrap it in scoped_nsobject because we must close
-  // it appropriately in [windowWillClose:].
-  BookmarkBubbleController* bookmarkBubbleController_;
+  BookmarkBubbleController* bookmarkBubbleController_;  // Weak.
   scoped_nsobject<GTMTheme> theme_;
   BOOL ownsBrowser_;  // Only ever NO when testing
   CGFloat verticalOffsetForStatusBubble_;
