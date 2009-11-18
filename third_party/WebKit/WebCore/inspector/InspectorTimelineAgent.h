8c5f94b217f5f6c4f3310d276b7ef0cc5955f3a5
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-18 22:25:39 +0000

    2009-11-18  Nicolas Roard   <nicolas@roard.com>
            Reviewed by Dimitri Glazkov.
    
            InspectorTimelineAgent.h should be guarded by ENABLE(INSPECTOR)
            https://bugs.webkit.org/show_bug.cgi?id=31504
    
            * inspector/InspectorTimelineAgent.h:Added the guard.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51136 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorTimelineAgent.h
+++ third_party/WebKit/WebCore/inspector/InspectorTimelineAgent.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef InspectorTimelineAgent_h
 #define InspectorTimelineAgent_h
 
+#if ENABLE(INSPECTOR)
+
 #include "Document.h"
 #include "ScriptExecutionContext.h"
 #include "ScriptObject.h"
@@ -138,4 +140,5 @@ inline InspectorTimelineAgent* InspectorTimelineAgent::retrieve(ScriptExecutionC
 
 } // namespace WebCore
 
+#endif // !ENABLE(INSPECTOR)
 #endif // !defined(InspectorTimelineAgent_h)
