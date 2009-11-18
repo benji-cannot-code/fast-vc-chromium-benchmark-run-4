0ea7bb81c8691382d86867267c44f9f38f6b95e1
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-18 23:46:06 +0000

    2009-11-18  Daniel Bates  <dbates@webkit.org>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=31186
    
            Renames RenderTextControl::m_edited and RenderTextControl::m_userEdited to
            m_wasChangedSinceLastChangeEvent and m_lastChangeWasUserEdit, respectively.
            These are more descriptive names so as to clear an ambiguity surrounding
            their usage. Also, renames associated setters and getters so that they
            coincide with the renamed fields.
    
            No functionality was changed. So, no tests were included.
    
            * bindings/objc/DOMHTML.mm:
            (-[DOMHTMLInputElement _isEdited]):
            (-[DOMHTMLTextAreaElement _isEdited]):
            * dom/Document.cpp:
            (WebCore::Document::setFocusedNode):
            * html/HTMLInputElement.cpp:
            (WebCore::HTMLInputElement::defaultEventHandler):
            * rendering/RenderTextControl.cpp:
            (WebCore::RenderTextControl::RenderTextControl):
            (WebCore::RenderTextControl::setInnerTextValue):
            (WebCore::RenderTextControl::setLastChangeWasUserEdit): Formerly named setUserEdited.
            (WebCore::RenderTextControl::subtreeHasChanged):
            * rendering/RenderTextControl.h:
            (WebCore::RenderTextControl::wasChangedSinceLastChangeEvent): Formerly named isEdited.
            (WebCore::RenderTextControl::setChangedSinceLastChangeEvent): Formerly named setEdited.
            (WebCore::RenderTextControl::lastChangeWasUserEdit): Formerly named setUserEdited.
            * rendering/RenderTextControlSingleLine.cpp:
            (WebCore::RenderTextControlSingleLine::subtreeHasChanged):
            * wml/WMLInputElement.cpp:
            (WebCore::WMLInputElement::defaultEventHandler):
    2009-11-18  Daniel Bates  <dbates@webkit.org>
    
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=31186
    
            Changes associated with renaming RenderTextControl::isUserEdited.
    
            * DOMHTMLClasses.cpp:
            (DOMHTMLInputElement::isUserEdited): Formerly named isUserEdited.
            (DOMHTMLTextAreaElement::isUserEdited): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51148 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/DOMHTMLClasses.cpp
+++ third_party/WebKit/WebKit/win/DOMHTMLClasses.cpp
@@ -1362,7 +1362,7 @@ HRESULT STDMETHODCALLTYPE DOMHTMLInputElement::isUserEdited(
     if (FAILED(isTextField(&textField)) || !textField)
         return S_OK;
     RenderObject* renderer = m_element->renderer();
-    if (renderer && toRenderTextControl(renderer)->isUserEdited())
+    if (renderer && toRenderTextControl(renderer)->lastChangeWasUserEdit())
         *result = TRUE;
     return S_OK;
 }
@@ -1559,7 +1559,7 @@ HRESULT STDMETHODCALLTYPE DOMHTMLTextAreaElement::isUserEdited(
     *result = FALSE;
     ASSERT(m_element);
     RenderObject* renderer = m_element->renderer();
-    if (renderer && toRenderTextControl(renderer)->isUserEdited())
+    if (renderer && toRenderTextControl(renderer)->lastChangeWasUserEdit())
         *result = TRUE;
     return S_OK;
 }
