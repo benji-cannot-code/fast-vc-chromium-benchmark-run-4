22a4451bf3efcfd961f81b41043e7df01c85614b
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-18 21:27:21 +0000

    Implement testRepaint and sweepHorizontally methods on layoutTestController.
    This involves implementing row-at-a-time and line-at-a-time repainting of the backing store.
    
    R=darin
    BUG=21510
    TEST=LayoutTests/fast/repaint/shadow-multiple*
    
    Review URL: http://codereview.chromium.org/397027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32401 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/webwidget_host.h
+++ webkit/tools/test_shell/webwidget_host.h
@@ -68,6 +68,8 @@ class WebWidgetHost {
     canvas()->drawARGB(167, 0, 0, 0);
   }
 
+  void PaintRect(const gfx::Rect& rect);
+
  protected:
   WebWidgetHost();
   ~WebWidgetHost();
@@ -115,7 +117,6 @@ class WebWidgetHost {
 #endif
 
   void ResetScrollRect();
-  void PaintRect(const gfx::Rect& rect);
 
   void set_painting(bool value) {
 #ifndef NDEBUG
