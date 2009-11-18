9d8c7f94f54fce7bbb3a2d031948555e00464f4e
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-18 17:55:04 +0000

    Move bookmark bar bridge unittest over to CocoaTest.
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/407002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32339 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bar_bridge_unittest.mm
+++ chrome/browser/cocoa/bookmark_bar_bridge_unittest.mm
@@ -91,9 +91,8 @@ typedef std::pair<GURL,WindowOpenDisposition> OpenInfo;
 @end
 
 
-class BookmarkBarBridgeTest : public PlatformTest {
+class BookmarkBarBridgeTest : public CocoaTest {
  public:
-  CocoaTestHelper cocoa_helper_;
   BrowserTestHelper browser_test_helper_;
 };
 
