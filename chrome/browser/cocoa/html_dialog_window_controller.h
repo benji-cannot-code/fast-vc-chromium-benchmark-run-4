ab54d6a2e01dece85c07ad21ebb946affce173e7
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-19 21:37:49 +0000

    Removed parentWindow parameter from HtmlDialogWindowController.
    Center HTML dialogs using [window screen].
    
    BUG=
    TEST=manual
    
    Review URL: http://codereview.chromium.org/407010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32549 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/html_dialog_window_controller.h
+++ chrome/browser/cocoa/html_dialog_window_controller.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
-#include "app/gfx/native_widget_types.h"
 #include "base/basictypes.h"
 #include "base/scoped_ptr.h"
 #include "chrome/browser/dom_ui/html_dialog_ui.h"
@@ -35,14 +34,10 @@ class TabContents;
 }
 
 // Creates and shows an HtmlDialogWindowController with the given
-// delegate, parent window, and profile, none of which may be NULL.
-// The window is automatically destroyed when it is closed.
-//
-// TODO(akalin): Handle a NULL parentWindow as HTML dialogs may be launched
-// without any browser windows being present (on OS X).
+// delegate and profile. The window is automatically destroyed when it is
+// closed.
 + (void)showHtmlDialog:(HtmlDialogUIDelegate*)delegate
-               profile:(Profile*)profile
-          parentWindow:(gfx::NativeWindow)parent_window;
+               profile:(Profile*)profile;
 
 @end
 
@@ -51,8 +46,7 @@ class TabContents;
 // This is the designated initializer.  However, this is exposed only
 // for testing; use showHtmlDialog instead.
 - (id)initWithDelegate:(HtmlDialogUIDelegate*)delegate
-               profile:(Profile*)profile
-          parentWindow:(gfx::NativeWindow)parentWindow;
+               profile:(Profile*)profile;
 
 // Loads the HTML content from the delegate; this is not a lightweight
 // process which is why it is not part of the constructor.  Must be
