8b42ca18a5f67c310df4d959bc74abbf0f645e68
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-19 05:09:22 +0000

    2009-11-18  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Unreviewed.
    
            Fix a typo. Remove the trailing semicolon
            from an include directive after r51137.
    
            No new tests as there is no functional change.
    
            * platform/qt/QWebPageClient.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51171 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/QWebPageClient.h
+++ third_party/WebKit/WebCore/platform/qt/QWebPageClient.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define QWebPageClient_h
 
 #ifndef QT_NO_CURSOR
-#include <QCursor>;
+#include <QCursor>
 #endif
 #include <QRect>
 
