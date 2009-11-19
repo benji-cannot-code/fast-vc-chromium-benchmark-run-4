ff52d6212bf43df31530764804049c9fcd875a99
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-19 03:12:50 +0000

    2009-11-18  Kent Tamura  <tkent@chromium.org>
            Reviewed by Darin Adler.
    
            Move UString::from(double) implementation to new
            WTF::doubleToStringInJavaScriptFormat(), and expose it because WebCore
            code will use it.
            https://bugs.webkit.org/show_bug.cgi?id=31330
    
            - Introduce new function createRep(const char*, unsigned) and
              UString::UString(const char*, unsigned) to reduce 2 calls to strlen().
            - Fix a bug that dtoa() doesn't update *rve if the input value is NaN
              or Infinity.
    
            No new tests because this doesn't change the behavior.
    
            * JavaScriptCore.exp:
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def:
            * runtime/UString.cpp:
            (JSC::createRep):
            (JSC::UString::UString):
            (JSC::UString::from): Move the code to doubleToStringInJavaScriptFormat().
            * runtime/UString.h:
            * wtf/dtoa.cpp:
            (WTF::dtoa): Fix a bug about rve.
            (WTF::append): A helper for doubleToStringInJavaScriptFormat().
            (WTF::doubleToStringInJavaScriptFormat): Move the code from UString::from(double).
            * wtf/dtoa.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/UString.h
+++ third_party/WebKit/JavaScriptCore/runtime/UString.h
@@ -236,7 +236,10 @@ namespace JSC {
 
     public:
         UString();
+        // Constructor for null-terminated ASCII string.
         UString(const char*);
+        // Constructor for non-null-terminated ASCII string.
+        UString(const char*, int length);
         UString(const UChar*, int length);
         UString(UChar*, int length, bool copy);
 
