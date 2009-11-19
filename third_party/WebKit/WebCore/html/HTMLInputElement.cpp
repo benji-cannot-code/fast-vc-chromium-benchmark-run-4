8e0a508949f8b44f024b1ce99cc6fb5844261db1
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-19 04:52:35 +0000

    2009-11-18  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Unreviewed.
    
            [Qt] Build fix for QtWebKit after r51159.
    
            Include float.h for DBL_MANT_DIG. Not sure why this
            is not a problem for other ports.
    
            * html/HTMLInputElement.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51170 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLInputElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLInputElement.cpp
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderTheme.h"
 #include "StringHash.h"
 #include "TextEvent.h"
+#include <float.h>
 #include <wtf/HashMap.h>
 #include <wtf/MathExtras.h>
 #include <wtf/StdLibExtras.h>
