eb777cf91daa2d4b09b2aea7ed969b1550c57d26
gregoryd@google.com <gregoryd@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-20 17:54:47 +0000

    Remove the expiration check from Chrome code
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=119
    TEST=none
    
    Review URL: http://codereview.chromium.org/410004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32626 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/nacl/sel_main.cc
+++ chrome/nacl/sel_main.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <crt_externs.h>
 #endif
 
+#define NACL_NO_INLINE
+
 EXTERN_C_BEGIN
 #include "native_client/src/shared/platform/nacl_sync.h"
 #include "native_client/src/shared/platform/nacl_sync_checked.h"
@@ -51,6 +53,8 @@ static void StopForDebuggerInit(const struct NaClApp *state) {
 }
 
 int SelMain(const int desc, const NaClHandle handle) {
+  return 0;
+#if 0  // TODO(gregoryd): enable when the service runtime is ready
   char *av[1];
   int ac = 1;
 
@@ -72,14 +76,6 @@ int SelMain(const int desc, const NaClHandle handle) {
   extern char                   **environ;
   envp = environ;
 #endif
-
-
-  if (NaClHasExpired()) {
-    // TODO(gregoryd): report error to browser?
-    fprintf(stderr, "This version of Native Client has expired.\n");
-    fprintf(stderr, "Please visit: http://code.google.com/p/nativeclient/\n");
-    exit(-1);
-  }
 
   NaClAllModulesInit();
 
@@ -205,5 +201,6 @@ int SelMain(const int desc, const NaClHandle handle) {
   NaClAllModulesFini();
 
   return ret_code;
+#endif
 }
 
