2412094f36ae85a7ffe03dee074385137dc8c025
yael.aharon@nokia.com <yael.aharon@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-20 21:40:38 +0000

    NPN_ReloadPlugins does not reload the page even if reloadPages is true. https://bugs.webkit.org/show_bug.cgi?id=30460
    Reviewed by Kenneth Rohde Christiansen.
    
    WebCore: 
    
    Replace call to PluginDatabase::installedPlugins()->refresh() with Page::refreshPlugins().
    It already refreshes plugins and reloads pages when necessary.
    
    Tests: plugins/reloadplugins-and-pages.html
           plugins/reloadplugins-no-pages.html
    
    * plugins/PluginInfoStore.cpp:
    (WebCore::refreshPlugins):
    
    WebKitTools: 
    
    Added code for calling NPN_ReloadPlugins with reloadPages true and false.
    
    * DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.cpp:
    (pluginInvoke):
    
    LayoutTests: 
    
    Added tests for calling NPN_ReloadPlugins with reloadPages being true and false.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51257 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/PluginInfoStore.cpp
+++ third_party/WebKit/WebCore/plugins/PluginInfoStore.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PluginInfoStore.h"
 
 #include "KURL.h"
+#include "Page.h"
 #include "PluginData.h"
 #include "PluginDatabase.h"
 #include "PluginPackage.h"
@@ -94,11 +95,7 @@ bool PluginInfoStore::supportsMIMEType(const WebCore::String& mimeType)
 
 void refreshPlugins(bool reloadOpenPages)
 {
-    PluginDatabase::installedPlugins()->refresh();
-
-    if (reloadOpenPages) {
-        // FIXME: reload open pages
-    }
+    Page::refreshPlugins(reloadOpenPages);
 }
 
 }
