2a823a88a483986e05a919c056b7c0d300bbe8b5
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-20 00:47:08 +0000

    2009-11-19  Avi Drissman  <avi@chromium.org>
            Reviewed by Darin Adler.
    
            Quick style fix.
            https://bugs.webkit.org/show_bug.cgi?id=27777
    
            * platform/graphics/cg/PDFDocumentImage.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51221 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/PDFDocumentImage.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cg/PDFDocumentImage.cpp
@@ -32,10 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "GraphicsContext.h"
 #include "ImageObserver.h"
+#include <wtf/MathExtras.h>
+
 #if !PLATFORM(MAC)
 #include "ImageSourceCG.h"
 #endif
-#include <wtf/MathExtras.h>
 
 using namespace std;
 
