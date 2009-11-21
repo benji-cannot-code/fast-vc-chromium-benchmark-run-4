5acc87f9b5021a28d705fadee71302a03e5a5153
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-21 02:46:12 +0000

    Revert 32727 - Disable sync. (Will be committed on beta branch only, and this CL will be immediately reverted.)
    BUG=26712
    TEST=trybot
    
    Review URL: http://codereview.chromium.org/425015
    
    TBR=akalin@chromium.org
    Review URL: http://codereview.chromium.org/414069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32728 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -427,7 +427,7 @@ void ProfileSyncService::SyncEvent(SyncEventCodes code) {
 
 bool ProfileSyncService::IsSyncEnabled() {
 #if defined(OS_POSIX)
-  return false;
+  return CommandLine::ForCurrentProcess()->HasSwitch(switches::kEnableSync);
 #else
   return !CommandLine::ForCurrentProcess()->HasSwitch(switches::kDisableSync);
 #endif
