2c367c062584a6396991c5d321abb21dccc819ea
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-21 01:02:41 +0000

    2009-11-20  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Teach the StatusBot how to support more than just the commit-queue
            https://bugs.webkit.org/show_bug.cgi?id=31754
    
            * CommitQueueStatus/index.yaml:
             - Add indices required for the new queries.
            * CommitQueueStatus/queue_status.py:
             - Add a patch-status page and move update_status to update-status.
             - Only display "commit-queue" status records for the commit-queue.
             - Add support for a queue_name property on status records.
             - Fix _int_from_request to actually work.
            * CommitQueueStatus/update_status.html:
             - Add support for a queue_name on status records.
             - Remove unused list of bug ids.
             * Scripts/modules/commands/queues.py
              - Make the queues pass the patch instead of the bug_id to StatusBot.
            * Scripts/modules/statusbot.py:
             - Support passing the queue_name to the status updates.
             - Support fetching patch status with patch_status().
            * Scripts/modules/workqueue.py:
             - Pass the patch to the StatusBot instead of the bug_id.
             - Let WorkQueues have a name.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/workqueue.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/workqueue.py
@@ -39,6 +39,9 @@ from modules.scm import ScriptError
 from modules.statusbot import StatusBot
 
 class WorkQueueDelegate:
+    def queue_name(self):
+        raise NotImplementedError, "subclasses must implement"
+
     def queue_log_path(self):
         raise NotImplementedError, "subclasses must implement"
 
@@ -58,7 +61,7 @@ class WorkQueueDelegate:
         raise NotImplementedError, "subclasses must implement"
 
     def should_proceed_with_work_item(self, work_item):
-        # returns (safe_to_proceed, waiting_message, bug_id)
+        # returns (safe_to_proceed, waiting_message, patch)
         raise NotImplementedError, "subclasses must implement"
 
     def process_work_item(self, work_item):
@@ -69,7 +72,8 @@ class WorkQueueDelegate:
 
 
 class WorkQueue:
-    def __init__(self, delegate):
+    def __init__(self, name, delegate):
+        self._name = name
         self._delegate = delegate
         self._output_tee = OutputTee()
 
@@ -96,11 +100,11 @@ class WorkQueue:
                 if not work_item:
                     self._update_status_and_sleep("Empty queue.")
                     continue
-                (safe_to_proceed, waiting_message, bug_id) = self._delegate.should_proceed_with_work_item(work_item)
+                (safe_to_proceed, waiting_message, patch) = self._delegate.should_proceed_with_work_item(work_item)
                 if not safe_to_proceed:
                     self._update_status_and_sleep(waiting_message)
                     continue
-                self.status_bot.update_status(waiting_message, bug_id=bug_id)
+                self.status_bot.update_status(self._name, waiting_message, patch)
             except Exception, e:
                 # Don't try tell the status bot, in case telling it causes an exception.
                 self._sleep("Exception while preparing queue: %s." % e)
