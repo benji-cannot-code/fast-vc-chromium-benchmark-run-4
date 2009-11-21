a4582fb0b5d556e8e794a886b73ef92b655c8f68
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-21 03:53:27 +0000

    2009-11-20  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Fix a bunch of unit test regressions from our recent bugzilla-toll hacking
            https://bugs.webkit.org/show_bug.cgi?id=31758
    
            * Scripts/modules/multicommandtool.py:
             - Allow passing of explicit commands to MultiCommandTool.__init__
            * Scripts/modules/multicommandtool_unittest.py:
             - Use new Command.name naming system.
             - Test Command auto-discovery.
            * Scripts/modules/workqueue.py:
             - bug_id no longer exists, use patch['bug_id'] instead.
            * Scripts/modules/workqueue_unittest.py:
             - WorkQueues require names now.
             - should_proceed_with_work_item must return a patch object.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51277 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/workqueue_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/workqueue_unittest.py
@@ -86,7 +86,8 @@ class LoggingDelegate(WorkQueueDelegate):
     def should_proceed_with_work_item(self, work_item):
         self.record("should_proceed_with_work_item")
         self._test.assertEquals(work_item, "work_item")
-        return (True, "waiting_message", 42)
+        fake_patch = { 'bug_id' : 42 }
+        return (True, "waiting_message", fake_patch)
 
     def process_work_item(self, work_item):
         self.record("process_work_item")
@@ -111,12 +112,13 @@ class NotSafeToProceedDelegate(LoggingDelegate):
     def should_proceed_with_work_item(self, work_item):
         self.record("should_proceed_with_work_item")
         self._test.assertEquals(work_item, "work_item")
-        return (False, "waiting_message", 42)
+        fake_patch = { 'bug_id' : 42 }
+        return (False, "waiting_message", fake_patch)
 
 
 class FastWorkQueue(WorkQueue):
     def __init__(self, delegate):
-        WorkQueue.__init__(self, delegate)
+        WorkQueue.__init__(self, "fast-queue", delegate)
 
     # No sleep for the wicked.
     seconds_to_sleep = 0
@@ -128,7 +130,7 @@ class FastWorkQueue(WorkQueue):
 class WorkQueueTest(unittest.TestCase):
     def test_trivial(self):
         delegate = LoggingDelegate(self)
-        work_queue = WorkQueue(delegate)
+        work_queue = WorkQueue("trivial-queue", delegate)
         work_queue.run()
         self.assertEquals(delegate._callbacks, LoggingDelegate.expected_callbacks)
         self.assertTrue(os.path.exists(delegate.queue_log_path()))
@@ -136,7 +138,7 @@ class WorkQueueTest(unittest.TestCase):
 
     def test_unexpected_error(self):
         delegate = ThrowErrorDelegate(self, 3)
-        work_queue = WorkQueue(delegate)
+        work_queue = WorkQueue("error-queue", delegate)
         work_queue.run()
         expected_callbacks = LoggingDelegate.expected_callbacks[:]
         work_item_index = expected_callbacks.index('process_work_item')
@@ -147,7 +149,7 @@ class WorkQueueTest(unittest.TestCase):
 
     def test_handled_error(self):
         delegate = ThrowErrorDelegate(self, WorkQueue.handled_error_code)
-        work_queue = WorkQueue(delegate)
+        work_queue = WorkQueue("handled-error-queue", delegate)
         work_queue.run()
         self.assertEquals(delegate._callbacks, LoggingDelegate.expected_callbacks)
 
