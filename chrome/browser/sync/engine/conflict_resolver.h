973acbc076a90a6a9191e408dbf729ab0faa01e2
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-22 02:59:10 +0000

    build fix: missing include
    Not exactly sure why this was compiling for some people before.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32772 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/conflict_resolver.h
+++ chrome/browser/sync/engine/conflict_resolver.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SYNC_ENGINE_CONFLICT_RESOLVER_H_
 
 #include <set>
+#include <string>
 
 #include "base/basictypes.h"
 #include "chrome/browser/sync/engine/syncer_types.h"
