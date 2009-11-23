965fbd4395e3ba0500535ff2f65cd6e3e8f0f620
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-23 21:20:07 +0000

    Removed browser owned by HtmlDialogWindowController.  This fixes the crash described in issue 28329.
    Use a non-incognito profile for HTML dialogs and any links launched
    from them.  This diverges from the behavior on Windows, but according
    to beng this is okay.
    
    Made it so that HTML dialog boxes don't keep history.
    
    BUG=28329
    TEST=made sure there is no crash when closing the browser with the sync
    setup wizard open
    
    Review URL: http://codereview.chromium.org/416004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32851 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/html_dialog_window_controller.h
+++ chrome/browser/cocoa/html_dialog_window_controller.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_ptr.h"
 #include "chrome/browser/dom_ui/html_dialog_ui.h"
 
-class Browser;
 class HtmlDialogWindowDelegateBridge;
 class Profile;
 class TabContents;
@@ -24,9 +23,7 @@ class TabContents;
 // from a HTMLDialogUIDelegate object.
 @interface HtmlDialogWindowController : NSWindowController {
  @private
-  // An HTML dialog can exist separately from a window in OS X, so this
-  // controller needs its own browser.
-  scoped_ptr<Browser> browser_;
+  Profile* profile_;  // weak.  Always a non-incognito profile.
   // Order here is important, as tab_contents_ may send messages to
   // delegate_ when it gets destroyed.
   scoped_ptr<HtmlDialogWindowDelegateBridge> delegate_;
