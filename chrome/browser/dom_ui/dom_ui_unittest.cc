60bdf6dbdc23fd5e01039c08942e2b7e66070540
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-23 18:31:25 +0000

    Take 2 at moving NTP CSS resources into the NTP resource cache.
    Moving the HTML to the cache saved 8ms on New Tab Warm on Windows,
    maybe we can shave a few more ms off by moving the CSS.
    
    The last change had a regression on the mac new tab perf, so I suspect
    it's the extra round trip to the UI thread when trying to load the CSS
    (the UI thread is probably just busy at that time).  Instead, I'm moving
    the generation of the CSS to the time the object is constructed, rather
    than when the resource is requested.  This is like the code before,
    except sometimes we'll hit the cache.
    
    BUG=26228
    
    Review URL: http://codereview.chromium.org/427003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32815 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/dom_ui_unittest.cc
+++ chrome/browser/dom_ui/dom_ui_unittest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class DOMUITest : public RenderViewHostTestHarness {
  public:
-  DOMUITest() {}
+  DOMUITest() : ui_thread_(ChromeThread::UI, MessageLoop::current()) {}
 
   // Tests navigating with a DOM UI from a fresh (nothing pending or committed)
   // state, through pending, committed, then another navigation. The first page
@@ -76,6 +76,8 @@ class DOMUITest : public RenderViewHostTestHarness {
   }
 
  private:
+  ChromeThread ui_thread_;
+
   DISALLOW_COPY_AND_ASSIGN(DOMUITest);
 };
 
