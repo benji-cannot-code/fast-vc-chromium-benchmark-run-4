af1664cf297f776bc36b32e0ffe584b8ea6d4143
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-23 11:29:07 +0000

    Android port lacks implementation of atomicIncrement and atomicDecrement
    git-svn-id: svn://svn.chromium.org/blink/trunk@51305 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Threading.h
+++ third_party/WebKit/JavaScriptCore/wtf/Threading.h
@@ -74,6 +74,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #elif PLATFORM(DARWIN)
 #include <libkern/OSAtomic.h>
+#elif PLATFORM(ANDROID)
+#include <cutils/atomic.h>
 #elif COMPILER(GCC)
 #if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 2))
 #include <ext/atomicity.h>
@@ -232,6 +234,11 @@ inline int atomicDecrement(int volatile* addend) { return InterlockedDecrement(r
 
 inline int atomicIncrement(int volatile* addend) { return OSAtomicIncrement32Barrier(const_cast<int*>(addend)); }
 inline int atomicDecrement(int volatile* addend) { return OSAtomicDecrement32Barrier(const_cast<int*>(addend)); }
+
+#elif PLATFORM(ANDROID)
+
+inline int atomicIncrement(int volatile* addend) { return android_atomic_inc(addend); }
+inline int atomicDecrement(int volatile* addend) { return android_atomic_dec(addend); }
 
 #elif COMPILER(GCC) && !PLATFORM(SPARC64) // sizeof(_Atomic_word) != sizeof(int) on sparc64 gcc
 #define WTF_USE_LOCKFREE_THREADSAFESHARED 1
