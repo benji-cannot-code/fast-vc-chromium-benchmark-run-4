9291c65e76db9049ea232786c914b7aab23fc4ea
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-24 22:41:11 +0000

    Windows build fix.
            * platform/network/cf/SocketStreamHandleCFNet.cpp: Also, don't include the wrong one!
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ProtectionSpace.h"
 #include "SocketStreamError.h"
 #include "SocketStreamHandleClient.h"
-#include "WebCoreSystemInterface.h"
 #include <wtf/MainThread.h>
 
 #if defined(BUILDING_ON_TIGER) || defined(BUILDING_ON_LEOPARD)
