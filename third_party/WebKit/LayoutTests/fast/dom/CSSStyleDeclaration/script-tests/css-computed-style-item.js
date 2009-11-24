54eb8bd9133d3f894f8d898ce91220d7e5c88b4b
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-24 00:30:37 +0000

    2009-11-23  Erik Arvidsson  <arv@chromium.org>
            Reviewed by Eric Seidel.
    
            JSC bindings for HasIndexGetter generates incorrect code (affects
            MediaList and CSSStyleDeclaration).
            This cleans up the edge cases for indexing out of range for style and
            computed style objects to return an empty string according to the spec.
            MediaList now returns null when indexed out of range.
            https://bugs.webkit.org/show_bug.cgi?id=31683
    
            * fast/dom/CSSStyleDeclaration/css-computed-style-item-expected.txt: Added.
            * fast/dom/CSSStyleDeclaration/css-computed-style-item.html: Added.
            * fast/dom/CSSStyleDeclaration/css-style-item-expected.txt: Added.
            * fast/dom/CSSStyleDeclaration/css-style-item.html: Added.
            * fast/dom/CSSStyleDeclaration/script-tests/css-computed-style-item.js: Added.
            * fast/dom/CSSStyleDeclaration/script-tests/css-style-item.js: Added.
            * fast/dom/StyleSheet/css-medialist-item-expected.txt: Added.
            * fast/dom/StyleSheet/css-medialist-item.html: Added.
            * fast/dom/StyleSheet/script-tests/TEMPLATE.html: Copied from LayoutTests/fast/backgrounds/repeat/script-tests/TEMPLATE.html.
            * fast/dom/StyleSheet/script-tests/css-medialist-item.js: Added.
    2009-11-23  Erik Arvidsson  <arv@chromium.org>
    
            Reviewed by Eric Seidel.
    
            JSC bindings for HasIndexGetter generates incorrect code (affects
            MediaList and CSSStyleDeclaration).
            This cleans up the edge cases for indexing out of range for style and
            computed style objects to return an empty string according to the spec.
            MediaList now returns null when indexed out of range.
            https://bugs.webkit.org/show_bug.cgi?id=31683
    
            Tests: fast/dom/CSSStyleDeclaration/css-computed-style-item.html
                   fast/dom/CSSStyleDeclaration/css-style-item.html
                   fast/dom/StyleSheet/css-medialist-item.html
    
            * bindings/scripts/CodeGeneratorJS.pm: If IndexGetterReturnsString then
                    we do not check the length before calling the item function.
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::CSSComputedStyleDeclaration::item):
            * css/CSSMutableStyleDeclaration.cpp:
            (WebCore::CSSMutableStyleDeclaration::item):
            * css/CSSStyleDeclaration.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51326 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/CSSStyleDeclaration/script-tests/css-computed-style-item.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("This tests indexing outside the range of the computed style object.");
+
+var element = document.createElement('div');
+element.style.color = 'rgb(120, 120, 120)';
+document.documentElement.appendChild(element);
+var computedStyle = window.getComputedStyle(element, null);
+
+shouldBeEqualToString('typeof computedStyle.length', 'number');
+shouldBeEqualToString('computedStyle[computedStyle.length]', '');
+shouldBeUndefined('computedStyle[-1]')
+
+document.documentElement.removeChild(element);
+
+successfullyParsed = true;
