e10bd42d0a9c28cf8a54151ef38f52df708eb912
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-24 21:54:22 +0000

    2009-11-24  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dan Bernstein.
    
            Find highlight is drawn incorrectly on pages with compositing layers
            <rdar://problem/7413925>
    
            Part 2: Add a new bit to the PaintBehavior flags, PaintBehaviorFlattenCompositingLayers,
            and pass that down when painting into an image.
    
            When set, it forces painting of compositing layers to go down a software paint path
            when all layers are painted, irrespective of compositing status, and where
            3d transforms are flattened to 2d. When doing this, we also need to use temporary
            clip rects for layers which are normally composited.
    
            * page/FrameView.h:
            (WebCore::FrameView::paintBehavior):
            * page/mac/FrameMac.mm:
            (WebCore::Frame::imageFromRect):
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::renderableTransform):
            (WebCore::expandClipRectForDescendantsAndReflection):
            (WebCore::transparencyClipBox):
            (WebCore::RenderLayer::beginTransparencyLayers):
            (WebCore::RenderLayer::paintLayer):
            * rendering/RenderLayer.h:
            (WebCore::RenderLayer::paintsWithTransparency):
            (WebCore::RenderLayer::paintsWithTransform):
            * rendering/RenderLayerCompositor.cpp:
            (WebCore::RenderLayerCompositor::calculateCompositedBounds):
            * rendering/RenderObject.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51355 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayerCompositor.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayerCompositor.cpp
@@ -339,7 +339,7 @@ IntRect RenderLayerCompositor::calculateCompositedBounds(const RenderLayer* laye
         }
     }
 
-    if (layer->paintsWithTransform()) {
+    if (layer->paintsWithTransform(PaintBehaviorNormal)) {
         TransformationMatrix* affineTrans = layer->transform();
         boundingBoxRect = affineTrans->mapRect(boundingBoxRect);
         unionBounds = affineTrans->mapRect(unionBounds);
