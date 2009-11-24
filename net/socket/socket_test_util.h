3d85bbcdbce38f7325c669d9a604e4ddb839250d
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-24 19:33:54 +0000

    Add a default constructor to the MockRead struct so that we can do things like dynamically create an array of them.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/434039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32951 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/socket_test_util.h
+++ net/socket/socket_test_util.h
@@ -50,6 +50,9 @@ struct MockConnect {
 };
 
 struct MockRead {
+  // Default
+  MockRead() : async(false), result(0), data(NULL), data_len(0) {};
+
   // Read failure (no data).
   MockRead(bool async, int result) : async(async) , result(result), data(NULL),
       data_len(0) { }
