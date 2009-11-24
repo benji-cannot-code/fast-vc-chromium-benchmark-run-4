aa5b3dd344788917b9c39a4c5dd3c7424f9b565b
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-24 02:34:09 +0000

    Linux: Create app modal dialogs in a different way to work around a KDE 3 window manager bug. While we're at it, change dialog's default size to match that of Chrome on Windows.
    BUG=28230
    TEST=see bug
    Review URL: http://codereview.chromium.org/425016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32905 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/platform_util_linux.cc
+++ chrome/common/platform_util_linux.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/process_util.h"
 #include "base/string_util.h"
+#include "chrome/common/gtk_util.h"
 #include "chrome/common/process_watcher.h"
 #include "googleurl/src/gurl.h"
 
@@ -81,6 +82,7 @@ void SimpleErrorBox(gfx::NativeWindow parent,
                     const string16& message) {
   GtkWidget* dialog = gtk_message_dialog_new(parent, GTK_DIALOG_MODAL,
       GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, "%s", UTF16ToUTF8(message).c_str());
+  gtk_util::ApplyMessageDialogQuirks(dialog);
   gtk_window_set_title(GTK_WINDOW(dialog), UTF16ToUTF8(title).c_str());
   g_signal_connect(dialog, "response", G_CALLBACK(gtk_widget_destroy), NULL);
   gtk_widget_show_all(dialog);
