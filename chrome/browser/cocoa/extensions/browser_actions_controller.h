081ae73a77f4900f0037cf47cf5be2922de6f5b3
andybons@chromium.org <andybons@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-24 22:14:51 +0000

    o Updates the browser action buttons when a tab is changed. This is needed for tab-specific browser action buttons. o If a tooltip is not present on a browser action button, clear the tooltip of the button.
    BUG=28719
    TEST=Load chrome/test/data/extensions/api_test/browser_tab_specific_state, play around with it in different tabs and make sure that the state remains the same per tab.
    Review URL: http://codereview.chromium.org/434047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32980 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/extensions/browser_actions_controller.h
+++ chrome/browser/cocoa/extensions/browser_actions_controller.h
@@ -58,6 +58,9 @@ extern NSString* const kBrowserActionsChangedNotification;
 // extensions service to the toolbar.
 - (void)createButtons;
 
+// Update the display of all buttons.
+- (void)update;
+
 // Hides the browser action's popup menu (if one is present and visible).
 - (void)hidePopup;
 
--- chrome/browser/cocoa/extensions/browser_actions_controller.mm
+++ chrome/browser/cocoa/extensions/browser_actions_controller.mm
@@ -215,8 +215,11 @@ class ExtensionImageTrackerBridge : public NotificationObserver,
     return;
 
   std::string tooltip = extension_->browser_action()->GetTitle(tabId);
-  if (!tooltip.empty())
+  if (tooltip.empty()) {
+    [self setToolTip:nil];
+  } else {
     [self setToolTip:base::SysUTF8ToNSString(tooltip)];
+  }
 
   SkBitmap image = extension_->browser_action()->GetIcon(tabId);
   if (!image.isNull()) {
@@ -317,6 +320,12 @@ class ExtensionsServiceObserverBridge : public NotificationObserver {
   }
 
   return self;
+}
+
+- (void)update {
+  for (BrowserActionButton* button in [buttons_ allValues]) {
+    [button updateState];
+  }
 }
 
 - (void)hidePopup {
