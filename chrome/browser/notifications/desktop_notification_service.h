d74989a94d8dd70355f60a619a87db1cf863e5bf
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-24 22:10:49 +0000

    Make notifications permission available in the extension manifest and hook up to the desktop notification service.
    BUG=27249
    TEST=none
    
    Review URL: http://codereview.chromium.org/432005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32979 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/desktop_notification_service.h
+++ chrome/browser/notifications/desktop_notification_service.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "chrome/browser/notifications/notification.h"
+#include "chrome/common/notification_registrar.h"
+#include "chrome/common/notification_service.h"
 #include "googleurl/src/gurl.h"
 
 class NotificationUIManager;
@@ -20,16 +22,16 @@ class Task;
 
 // The DesktopNotificationService is an object, owned by the Profile,
 // which provides the creation of desktop "toasts" to web pages and workers.
-class DesktopNotificationService {
+class DesktopNotificationService : public NotificationObserver {
  public:
-  enum NotificationSource {
+  enum DesktopNotificationSource {
     PageNotification,
     WorkerNotification
   };
 
   DesktopNotificationService(Profile* profile,
                              NotificationUIManager* ui_manager);
-  ~DesktopNotificationService();
+  virtual ~DesktopNotificationService();
 
   // Requests permission (using an info-bar) for a given origin.
   // |callback_context| contains an opaque value to pass back to the
@@ -50,11 +52,11 @@ class DesktopNotificationService {
   // value to be passed back to the process when events occur on
   // this notification.
   bool ShowDesktopNotification(const GURL& origin, const GURL& url,
-      int process_id, int route_id, NotificationSource source,
+      int process_id, int route_id, DesktopNotificationSource source,
       int notification_id);
   bool ShowDesktopNotificationText(const GURL& origin, const GURL& icon,
       const string16& title, const string16& text, int process_id,
-      int route_id, NotificationSource source, int notification_id);
+      int route_id, DesktopNotificationSource source, int notification_id);
 
   // Cancels a notification.  If it has already been shown, it will be
   // removed from the screen.  If it hasn't been shown yet, it won't be
@@ -68,6 +70,11 @@ class DesktopNotificationService {
   void DenyPermission(const GURL& origin);
 
   NotificationsPrefsCache* prefs_cache() { return prefs_cache_; }
+
+  // NotificationObserver interface.
+  virtual void Observe(NotificationType type,
+                       const NotificationSource& source,
+                       const NotificationDetails& details);
 
  private:
   void InitPrefs();
@@ -87,6 +94,9 @@ class DesktopNotificationService {
   // Non-owned pointer to the notification manager which manages the
   // UI for desktop toasts.
   NotificationUIManager* ui_manager_;
+
+  // Connection to the service providing the other kind of notifications.
+  NotificationRegistrar notification_registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(DesktopNotificationService);
 };
