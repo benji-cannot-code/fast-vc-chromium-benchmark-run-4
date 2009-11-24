0accb9d926265132593374e1b49a0a5bddcc848f
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-24 21:50:55 +0000

    Reviewed by Brady Eidson.
            https://bugs.webkit.org/show_bug.cgi?id=31844
            SocketStreamHandleCFNet should support CONNECT proxy credentials
    
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * platform/network/CredentialStorage.h:
            * platform/network/mac/CredentialStorageMac.mm: Added.
            (WebCore::CredentialStorage::getFromPersistentStorage):
            * platform/network/cf/CredentialStorageCFNet.cpp: Added.
            (WebCore::CredentialStorage::getFromPersistentStorage):
            Add support for fetching credentials from persistent storage (CFNet version is currently
            a stub).
    
            * bindings/js/JSWebSocketCustom.cpp: Removed an unneeded include.
    
            * platform/network/cf/SocketStreamHandle.h:
            * platform/network/cf/SocketStreamHandleCFNet.cpp:
            (WebCore::SocketStreamHandle::SocketStreamHandle):
            (WebCore::SocketStreamHandle::createStreams):
            (WebCore::getStoredCONNECTProxyCredentials):
            (WebCore::authenticationSchemeFromAuthenticationMethod):
            (WebCore::SocketStreamHandle::addCONNECTCredentials):
            (WebCore::SocketStreamHandle::readStreamCallback):
            Check if connection attempt was resulted in 407, and try stored credentials if it did.
    
            * platform/mac/WebCoreSystemInterface.h:
            * platform/mac/WebCoreSystemInterface.mm:
            * WebCore.base.exp:
            Updated WKSI.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSWebSocketCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSWebSocketCustom.cpp
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KURL.h"
 #include "JSEventListener.h"
 #include "WebSocket.h"
-#include "NotImplemented.h"
 #include <runtime/Error.h>
 
 using namespace JSC;
