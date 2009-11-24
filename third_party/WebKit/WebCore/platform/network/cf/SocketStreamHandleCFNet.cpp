96e04368f58d960325c8a0afcf97afdead8aaac4
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-24 22:34:15 +0000

    Windows build fix.
            * platform/network/cf/SocketStreamHandleCFNet.cpp: Include the proper WKSI header.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/SocketStreamHandleCFNet.cpp
@@ -48,6 +48,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if PLATFORM(WIN)
 #include "LoaderRunLoopCF.h"
+#include <WebKitSystemInterface/WebKitSystemInterface.h>
+#else
+#include "WebCoreSystemInterface.h"
 #endif
 
 #ifdef BUILDING_ON_TIGER
