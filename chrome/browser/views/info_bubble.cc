8c4486e5e6ee9f9bda62705f7bef8205661d61e9
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-24 17:34:35 +0000

    Sets widget created by info bubbles to a custom type so that they don't get a drop shadow and centered on screen.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/431021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32936 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/info_bubble.cc
+++ chrome/browser/views/info_bubble.cc
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/widget/root_view.h"
 #include "views/window/window.h"
 
+#if defined(OS_LINUX)
+#include "chrome/browser/views/tabs/tab_overview_types.h"
+#endif
+
 // Background color of the bubble.
 #if defined(OS_WIN)
 const SkColor InfoBubble::kBackgroundColor =
@@ -237,6 +241,10 @@ void InfoBubble::Init(views::Window* parent,
   MakeTransparent();
   make_transient_to_parent();
   WidgetGtk::Init(GTK_WIDGET(parent->GetNativeWindow()), gfx::Rect());
+  TabOverviewTypes::instance()->SetWindowType(
+      GetNativeView(),
+      TabOverviewTypes::WINDOW_TYPE_CHROME_INFO_BUBBLE,
+      NULL);
 #endif
 
   // Create a View to hold the contents of the main window.
