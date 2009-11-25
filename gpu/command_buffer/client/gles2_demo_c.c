5fc6755191e0584a57eef301b625179bde4b4f9a
apatrick@google.com <apatrick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-25 19:13:19 +0000

    Merged in recent changes to command buffer code. TEST=none BUG=none
    Review URL: http://codereview.chromium.org/434063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33095 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/gles2_demo_c.c
+++ gpu/command_buffer/client/gles2_demo_c.c
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/client/gles2_demo_c.h"
 
 void GLFromCTestFunction() {
-  glClear(GL_COLOR_BUFFER_BIT);
+  // glClear(GL_COLOR_BUFFER_BIT);
 }
 
 
