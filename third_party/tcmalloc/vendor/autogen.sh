467316f5dbc061234b9cc6f029234c213268040a
sgk@chromium.org <sgk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-25 00:31:54 +0000

    Import vanilla upstream tcmalloc sources into a vendor branch as a basis for local changes. BUG=27911 TEST=none Review URL: http://codereview.chromium.org/436037
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33016 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ third_party/tcmalloc/vendor/autogen.sh
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+# Before using, you should figure out all the .m4 macros that your
+# configure.m4 script needs and make sure they exist in the m4/
+# directory.
+#
+# These are the files that this script might edit:
+#    aclocal.m4 configure Makefile.in src/config.h.in \
+#    depcomp config.guess config.sub install-sh missing mkinstalldirs \
+#    ltmain.sh
+#
+# Here's a command you can run to see what files aclocal will import:
+#  aclocal -I ../autoconf --output=- | sed -n 's/^m4_include..\([^]]*\).*/\1/p'
+
+set -ex
+rm -rf autom4te.cache
+
+aclocal --force -I m4
+grep -q LIBTOOL configure.ac && libtoolize -c -f
+autoconf -f -W all,no-obsolete
+autoheader -f -W all
+automake -a -c -f -W all,no-override
+
+rm -rf autom4te.cache
+exit 0
