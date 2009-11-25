e6c7a44d4f8dfa1e50a5918e7519a06e22aaa80b
apatrick@google.com <apatrick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-25 00:01:32 +0000

    Landing the GPU process and command buffer code again, this time with a DEPS file with the necessary include rules. TEST=none BUG=none
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33006 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -3418,6 +3418,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         ],
       },
       'conditions': [
+        ['enable_pepper==1', {
+          'dependencies': [
+            '../gpu/gpu.gyp:gpu_plugin',
+          ],
+        }],
         ['disable_nacl!=1', {
           'dependencies': [
             'nacl',
---
+++ gpu/command_buffer/common/gles2_cmd_format_test.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file contains unit tests for gles2 commmands
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "gpu/command_buffer/common/gles2_cmd_format.h"
+
+namespace command_buffer {
+namespace gles2 {
+
+#include "gpu/command_buffer/common/gles2_cmd_format_test_autogen.h"
+
+}  // namespace gles2
+}  // namespace command_buffer
+
