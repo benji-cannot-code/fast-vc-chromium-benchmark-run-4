d65ca972b27859b12c62288bc9dcdd2dfdb02c33
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-26 20:59:33 +0000

    2009-11-26  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            This is a WebCore part of the fix that allows to view plugin
            resources loaded by plugins.
    
            https://bugs.webkit.org/show_bug.cgi?id=31832
    
            * inspector/InspectorController.cpp:
            (WebCore::InspectorController::willSendRequest):
            (WebCore::InspectorController::didReceiveResponse):
            (WebCore::InspectorController::didReceiveContentLength):
            (WebCore::InspectorController::didFinishLoading):
            (WebCore::InspectorController::didFailLoading):
            * inspector/InspectorController.h: Remove unused DocumentLoader parameters from inspector methods.
            * inspector/InspectorResource.cpp:
            (WebCore::InspectorResource::addLength): Update loading end time when new data are received.
            * loader/ResourceLoadNotifier.cpp:
            (WebCore::ResourceLoadNotifier::didFailToLoad): Notify InspectorController about the failure.
            (WebCore::ResourceLoadNotifier::dispatchWillSendRequest):
            (WebCore::ResourceLoadNotifier::dispatchDidReceiveResponse):
            (WebCore::ResourceLoadNotifier::dispatchDidReceiveContentLength):
            (WebCore::ResourceLoadNotifier::dispatchDidFinishLoading):
    
    2009-11-26  Yury Semikhatsky  <yurys@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            WebCore part of the fix that allows to view plugin
            resources loaded by plugins. Methods that for resource
            loading notifications are exposed through WebDevToolsAgent
            interface to the glue code to allow Chromium's plugin
            implementation to notify InspectorController about resource
            loading in plugins.
    
            https://bugs.webkit.org/show_bug.cgi?id=31832
    
            * public/WebDevToolsAgent.h:
            * public/WebView.h:
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::createUniqueIdentifierForRequest):
            * src/WebViewImpl.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51421 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebDevToolsAgent.h
+++ third_party/WebKit/WebKit/chromium/public/WebDevToolsAgent.h
@@ -36,9 +36,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 class WebDevToolsAgentClient;
+class WebFrame;
 class WebString;
+class WebURLRequest;
+class WebURLResponse;
 class WebView;
 struct WebPoint;
+struct WebURLError;
 
 class WebDevToolsAgent {
 public:
@@ -71,6 +75,13 @@ public:
     // Installs dispatch handle that is going to be called periodically
     // while on a breakpoint.
     WEBKIT_API static void setMessageLoopDispatchHandler(MessageLoopDispatchHandler);
+
+    virtual void identifierForInitialRequest(unsigned long resourceId, WebFrame*, const WebURLRequest&) = 0;
+    virtual void willSendRequest(unsigned long resourceId, const WebURLRequest&) = 0;
+    virtual void didReceiveData(unsigned long resourceId, int length) = 0;
+    virtual void didReceiveResponse(unsigned long resourceId, const WebURLResponse&) = 0;
+    virtual void didFinishLoading(unsigned long resourceId) = 0;
+    virtual void didFailLoading(unsigned long resourceId, const WebURLError&) = 0;
 };
 
 } // namespace WebKit
