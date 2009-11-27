5d7e609cdb416cb5562a95a4cedb0763ee4c175f
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-27 18:09:36 +0000

    2009-11-27  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            [bzt] unit test ApplyPatches and ApplyAttachment
            https://bugs.webkit.org/show_bug.cgi?id=31935
    
            * Scripts/modules/commands/download.py:
            * Scripts/modules/commands/download_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/commands/download.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/commands/download.py
@@ -80,7 +80,6 @@ class Build(Command):
         sequence.run_and_handle_errors()
 
 
-# FIXME: Requires unit test. Blocking issue: WebKitApplyingScripts
 class ApplyAttachment(Command):
     name = "apply-attachment"
     show_in_main_help = True
@@ -96,7 +95,6 @@ class ApplyAttachment(Command):
         WebKitApplyingScripts.apply_patches_with_options(tool.scm(), [attachment], options)
 
 
-# FIXME: Requires unit test. Blocking issue: WebKitApplyingScripts
 class ApplyPatches(Command):
     name = "apply-patches"
     show_in_main_help = True
