efd93da3ed29177a92966e513ba799fa5700836d
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-27 08:04:38 +0000

    2009-11-27  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            [bzt] Kill WebKitLandingScripts
            https://bugs.webkit.org/show_bug.cgi?id=31904
    
            Step 6: Kill the rest.
    
            * Scripts/modules/buildsteps.py:
            * Scripts/modules/commands/download.py:
            * Scripts/modules/commands/queries.py:
            * Scripts/modules/commands/upload.py:
            * Scripts/modules/landingsequence.py:
            * Scripts/modules/scm.py:
            * Scripts/modules/webkitlandingscripts.py: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51437 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/landingsequence.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/landingsequence.py
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from modules.comments import bug_comment_from_commit_text
 from modules.logging import log
 from modules.scm import ScriptError, CheckoutNeedsUpdate
-from modules.webkitlandingscripts import WebKitLandingScripts, commit_message_for_this_commit
 from modules.webkitport import WebKitPort
 from modules.workqueue import WorkQueue
 
@@ -89,7 +88,7 @@ class LandingSequence:
         self._tool.run_tests(launch_safari=not self._options.non_interactive, fail_fast=self._options.non_interactive, quiet=self._options.quiet, port=self._port)
 
     def commit(self):
-        commit_message = commit_message_for_this_commit(self._tool.scm())
+        commit_message = self._tool.scm().commit_message_for_this_commit()
         return self._tool.scm().commit_with_message(commit_message.message())
 
     def close_patch(self, commit_log):
