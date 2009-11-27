688669adf058cff7502f8b2099be70573cd5659f
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-27 17:37:46 +0000

    2009-11-27  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            [bzt] Unit test download commands
            https://bugs.webkit.org/show_bug.cgi?id=31923
    
            Adds download_unittest and fixes a bug found while testing.
    
            * Scripts/modules/commands/commandtest.py:
            * Scripts/modules/commands/download.py:
                Fixed a bug where we'd throw an error because [].append returns
                None.
            * Scripts/modules/commands/download_unittest.py: Added.
            * Scripts/modules/mock_bugzillatool.py:
            * Scripts/run-webkit-unittests:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/commands/download.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/commands/download.py
@@ -80,6 +80,7 @@ class Build(Command):
         sequence.run_and_handle_errors()
 
 
+# FIXME: Requires unit test. Blocking issue: WebKitApplyingScripts
 class ApplyAttachment(Command):
     name = "apply-attachment"
     show_in_main_help = True
@@ -95,6 +96,7 @@ class ApplyAttachment(Command):
         WebKitApplyingScripts.apply_patches_with_options(tool.scm(), [attachment], options)
 
 
+# FIXME: Requires unit test. Blocking issue: WebKitApplyingScripts
 class ApplyPatches(Command):
     name = "apply-patches"
     show_in_main_help = True
@@ -230,7 +232,7 @@ class AbstractPatchProcessingCommand(Command):
         bugs_to_patches = {}
         for patch in patches:
             bug_id = patch["bug_id"]
-            bugs_to_patches[bug_id] = bugs_to_patches.get(bug_id, []).append(patch)
+            bugs_to_patches[bug_id] = bugs_to_patches.get(bug_id, []) + [patch]
         return bugs_to_patches
 
     def execute(self, options, args, tool):
@@ -351,6 +353,7 @@ class LandPatches(AbstractPatchLandingCommand):
         return all_patches
 
 
+# FIXME: Requires unit test.
 class Rollout(Command):
     name = "rollout"
     show_in_main_help = True
