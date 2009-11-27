34621716bc1d56be558996b67980e5c311a06177
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-27 05:53:00 +0000

    2009-11-26  Kinuko Yasuda  <kinuko@chromium.com>
            Reviewed by Eric Seidel.
    
            Rewrite the test fast/parser/comment-in-textarea to use dumpAsText
            so that the expected results can be independent of platform differences.
            https://bugs.webkit.org/show_bug.cgi?id=31580
    
            * fast/parser/comment-in-textarea-expected.txt: Added.
            * fast/parser/comment-in-textarea.html:
            * fast/parser/script-tests/comment-in-textarea.js: Added.
            * platform/mac/fast/parser/comment-in-textarea-expected.checksum: Removed.
            * platform/mac/fast/parser/comment-in-textarea-expected.png: Removed.
            * platform/mac/fast/parser/comment-in-textarea-expected.txt: Removed.
            * platform/win/fast/parser/comment-in-textarea-expected.txt: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51427 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/parser/script-tests/comment-in-textarea.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test that the HTML parser correctly parses the comment-like string in the textarea.");
+
+var element = document.createElement("div");
+element.innerHTML = "<textarea><!-- </textarea> --> This should be part of the textarea</textarea>";
+document.body.appendChild(element);
+
+var textAreas = document.getElementsByTagName("textarea");
+shouldBe("textAreas.length", "1");
+shouldBeEqualToString("textAreas[0].innerHTML", "<!-- </textarea> --> This should be part of the textarea");
+
+var successfullyParsed = true;
