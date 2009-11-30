8050589c164cc28d59934824d68b5a6ab49ff468
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-30 18:45:28 +0000

    Fix compile for ChromeOS by #including needed headers.
    TBR=maruel
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/457004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33305 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status_area_view.cc
+++ chrome/browser/chromeos/status_area_view.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include "chrome/browser/profile.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/pref_service.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
