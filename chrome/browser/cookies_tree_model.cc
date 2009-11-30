cb261b83d0a354544deddb48cd500c0adabf96da
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-30 22:41:36 +0000

    Linux: Adds back the ability to filter cookies by origin in the cookies options view
    BUG=27657
    TEST=unittest passes, manually testing typing "google" in the search field pulls up origins containing "google" in their name
    
    Review URL: http://codereview.chromium.org/437075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33359 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cookies_tree_model.cc
+++ chrome/browser/cookies_tree_model.cc
@@ -273,7 +273,6 @@ void CookiesTreeModel::DeleteAllCookies() {
   int num_children = root->GetChildCount();
   for (int i = num_children - 1; i >= 0; --i)
     delete Remove(root, i);
-  LoadCookies();
   NotifyObserverTreeNodeChanged(root);
 }
 
