43ef9bd16862b3f25d90fce4754dfad98e640ad2
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-30 00:50:35 +0000

    2009-11-29  Daniel Bates  <dbates@webkit.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=31966
    
            Removed #include RenderStyle.h from file KeyframeAnimation.h. Instead,
            forward declared it.
    
            No functionality was changed. So, no new tests.
    
            * page/animation/KeyframeAnimation.cpp:
            * page/animation/KeyframeAnimation.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/KeyframeAnimation.h
+++ third_party/WebKit/WebCore/page/animation/KeyframeAnimation.h
@@ -33,9 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AnimationBase.h"
 #include "Document.h"
 #include "KeyframeList.h"
-#include "RenderStyle.h"
 
 namespace WebCore {
+
+class RenderStyle;
 
 // A KeyframeAnimation tracks the state of an explicit animation
 // for a single RenderObject.
