f853537319b31279aff0f2834c1385574cc114fc
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-30 23:27:46 +0000

    Marking one more as flaky test. This failed 3 times out of 20 invocations.
    BUG=28808
    TEST=None
    
    Review URL: http://codereview.chromium.org/457012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33370 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/mouseleave_uitest.cc
+++ chrome/test/ui/mouseleave_uitest.cc
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Test succeeds locally, flaky on trybot
 // http://code.google.com/p/chromium/issues/detail?id=26349
 #define MAYBE_TestOnMouseOut DISABLED_TestOnMouseOut
+#elif defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
+// Test is flaky on linux/views build.
+// http://crbug.com/28808.
+#define MAYBE_TestOnMouseOut FLAKY_TestOnMouseOut
 #endif
 
 namespace {
