cca485be46402b84e1e666692c32a0db3391a98b
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-11-30 17:34:59 +0000

    2009-11-30  Steve Block  <steveblock@google.com>
            Reviewed by Eric Seidel.
    
            Adds PassOwnPtr include to ScriptExecutionContext.h.
            https://bugs.webkit.org/show_bug.cgi?id=31929
    
            Build fix only, no new tests.
    
            * dom/ScriptExecutionContext.h: Modified. Adds PassOwnPtr include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51497 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/ScriptExecutionContext.h
+++ third_party/WebKit/WebCore/dom/ScriptExecutionContext.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KURL.h"
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
+#include <wtf/PassOwnPtr.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/Threading.h>
 
