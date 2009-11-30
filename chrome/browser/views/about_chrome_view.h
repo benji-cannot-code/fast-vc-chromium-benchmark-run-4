3054700195e71442e154a939318cb32a9f216ecd
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-30 17:19:52 +0000

    Show chromium os version in about panel
    Review URL: http://codereview.chromium.org/440008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33293 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/about_chrome_view.h
+++ chrome/browser/views/about_chrome_view.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "chrome/browser/google_update.h"
+#elif defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/version_loader.h"
 #endif
 
 namespace views {
@@ -93,6 +95,12 @@ class AboutChromeView : public views::View,
                     GoogleUpdateErrorCode error_code);
 #endif
 
+#if defined(OS_CHROMEOS)
+  // Callback from chromeos::VersionLoader giving the version.
+  void OnOSVersion(chromeos::VersionLoader::Handle handle,
+                   std::string version);
+#endif
+
   // Draws a string onto the canvas (wrapping if needed) while also keeping
   // track of where it ends so we can position a URL after the text. The
   // parameter |bounds| represents the boundary we have to work with, |position|
@@ -138,6 +146,9 @@ class AboutChromeView : public views::View,
   views::ImageView* about_dlg_background_logo_;
   views::Label* about_title_label_;
   views::Textfield* version_label_;
+#if defined(OS_CHROMEOS)
+  views::Textfield* os_version_label_;
+#endif
   views::Label* copyright_label_;
   views::Label* main_text_label_;
   int main_text_label_height_;
@@ -185,6 +196,14 @@ class AboutChromeView : public views::View,
 
   // Whether text direction is left-to-right or right-to-left.
   bool text_direction_is_rtl_;
+
+#if defined(OS_CHROMEOS)
+  // Handles asynchronously loading the version.
+  chromeos::VersionLoader loader_;
+
+  // Used to request the version.
+  CancelableRequestConsumer consumer_;
+#endif
 
   DISALLOW_COPY_AND_ASSIGN(AboutChromeView);
 };
