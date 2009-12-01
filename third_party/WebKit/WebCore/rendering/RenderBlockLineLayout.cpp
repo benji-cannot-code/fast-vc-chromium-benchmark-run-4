0e42a34ccd2d2e8334985e97133307305fd7a22f
hamaji@chromium.org <hamaji@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-01 07:37:14 +0000

    2009-11-30  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Dan Bernstein.
    
            css2.1/t1205-c566-list-stl-00-e-ag.html is failing
            https://bugs.webkit.org/show_bug.cgi?id=23264
            css2.1/t1205-c565-list-pos-00-b.html is failing
            https://bugs.webkit.org/show_bug.cgi?id=23263
    
            Ignore whitespaces after list markers.
            Rendering of this was already done for the case where inside=false.
            This fixes the rendering of inside=true case and calcInlinePrefWidths.
    
            Test: fast/lists/calc-width-with-space.html
    
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::calcInlinePrefWidths):
            * rendering/RenderBlockLineLayout.cpp:
            (WebCore::RenderBlock::findNextLineBreak):
    2009-11-30  Shinichiro Hamaji  <hamaji@chromium.org>
    
            Reviewed by Dan Bernstein.
    
            css2.1/t1205-c566-list-stl-00-e-ag.html is failing
            https://bugs.webkit.org/show_bug.cgi?id=23264
    
            * editing/pasteboard/copy-crash-expected.txt:
            * fast/lists/calc-width-with-space-expected.txt: Added.
            * fast/lists/calc-width-with-space.html: Added.
            * platform/mac/css2.1/t1205-c561-list-displ-00-b-expected.checksum:
            * platform/mac/css2.1/t1205-c561-list-displ-00-b-expected.png:
            * platform/mac/css2.1/t1205-c561-list-displ-00-b-expected.txt:
            * platform/mac/css2.1/t1205-c565-list-pos-00-b-expected.checksum:
            * platform/mac/css2.1/t1205-c565-list-pos-00-b-expected.png:
            * platform/mac/css2.1/t1205-c565-list-pos-00-b-expected.txt:
            * platform/mac/css2.1/t1205-c566-list-stl-00-e-ag-expected.checksum:
            * platform/mac/css2.1/t1205-c566-list-stl-00-e-ag-expected.png:
            * platform/mac/css2.1/t1205-c566-list-stl-00-e-ag-expected.txt:
            * platform/mac/fast/block/positioning/complex-percentage-height-expected.checksum:
            * platform/mac/fast/block/positioning/complex-percentage-height-expected.png:
            * platform/mac/fast/block/positioning/complex-percentage-height-expected.txt:
            * platform/mac/fast/lists/item-not-in-list-line-wrapping-expected.checksum:
            * platform/mac/fast/lists/item-not-in-list-line-wrapping-expected.png:
            * platform/mac/fast/lists/item-not-in-list-line-wrapping-expected.txt:
            * platform/mac/fast/repaint/list-marker-expected.checksum:
            * platform/mac/fast/repaint/list-marker-expected.png:
            * platform/mac/fast/repaint/list-marker-expected.txt:
            * platform/qt/Skipped:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBlockLineLayout.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -1770,10 +1770,10 @@ InlineIterator RenderBlock::findNextLineBreak(InlineBidiResolver& resolver, bool
             currentCharacterIsSpace = false;
             currentCharacterIsWS = false;
             trailingSpaceObject = 0;
-            
+
             // Optimize for a common case. If we can't find whitespace after the list
             // item, then this is all moot. -dwh
-            if (o->isListMarker() && !toRenderListMarker(o)->isInside()) {
+            if (o->isListMarker()) {
                 if (style()->collapseWhiteSpace() && shouldSkipWhitespaceAfterStartObject(this, o, lineMidpointState)) {
                     // Like with inline flows, we start ignoring spaces to make sure that any 
                     // additional spaces we see will be discarded.
@@ -1781,6 +1781,8 @@ InlineIterator RenderBlock::findNextLineBreak(InlineBidiResolver& resolver, bool
                     currentCharacterIsWS = true;
                     ignoringSpaces = true;
                 }
+                if (toRenderListMarker(o)->isInside())
+                    tmpW += replacedBox->width() + replacedBox->marginLeft() + replacedBox->marginRight() + inlineWidth(o);
             } else
                 tmpW += replacedBox->width() + replacedBox->marginLeft() + replacedBox->marginRight() + inlineWidth(o);
         } else if (o->isText()) {
