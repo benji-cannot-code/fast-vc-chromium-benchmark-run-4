75c939e283ce6a1120ad3d3596a6082303c5425d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-01 00:07:41 +0000

    2009-11-30  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            [bzt] Generate pass messages for style-queue
            https://bugs.webkit.org/show_bug.cgi?id=31995
    
            With this change, the style-queue posts "pass" messages to bugs as
            well.  Also, added more information to the state store w.r.t. passing
            and failing.
    
            * Scripts/modules/commands/queues.py:
            * Scripts/modules/patchcollection.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51518 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/patchcollection.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/patchcollection.py
@@ -72,8 +72,9 @@ class PersistentPatchCollectionDelegate:
 
 
 class PersistentPatchCollection:
-    _initial_status = "Attempted"
-    _terminal_status = "Done"
+    _initial_status = "Pending"
+    _pass_status = "Pass"
+    _fail_status = "Fail"
     def __init__(self, delegate):
         self._delegate = delegate
         self._name = self._delegate.collection_name()
@@ -96,5 +97,8 @@ class PersistentPatchCollection:
             if not status:
                 return patch_id
 
-    def done(self, patch):
-        self._status.update_status(self._name, self._terminal_status, patch)
+    def did_pass(self, patch):
+        self._status.update_status(self._name, self._pass_status, patch)
+
+    def did_fail(self, patch):
+        self._status.update_status(self._name, self._fail_status, patch)
