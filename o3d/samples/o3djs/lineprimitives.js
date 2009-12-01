84a4aeef11371a6c0c9714d634e26a6d5b5ccadb
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-01 00:35:14 +0000

    TBR=kbr@google.com Fix for last CL
    I was testing in the wrong client :-(
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33388 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/samples/o3djs/lineprimitives.js
+++ o3d/samples/o3djs/lineprimitives.js
@@ -117,7 +117,7 @@ o3djs.lineprimitives.LineVertexInfo.prototype.setLine = function(
 o3djs.lineprimitives.LineVertexInfo.prototype.createShape = function(
     pack,
     material) {
-  this.createShapeByType(
+  return this.createShapeByType(
       pack, material, o3djs.base.o3d.Primitive.LINELIST);
 };
 
