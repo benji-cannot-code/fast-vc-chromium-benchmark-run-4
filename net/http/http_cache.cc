501b654e23d960911c33a1159d4ac1278a6eca65
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-01 01:11:15 +0000

    Update CloseIdleSockets to close out FLIP connections as well as HTTP connections.  This is used for benchmarking.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/454009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33402 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_cache.cc
+++ net/http/http_cache.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
 #include "net/disk_cache/disk_cache.h"
+#include "net/flip/flip_session_pool.h"
 #include "net/http/http_cache_transaction.h"
 #include "net/http/http_network_layer.h"
 #include "net/http/http_network_session.h"
@@ -530,6 +531,8 @@ void HttpCache::CloseIdleConnections() {
   HttpNetworkSession* session = network->GetSession();
   if (session) {
     session->tcp_socket_pool()->CloseIdleSockets();
+    if (session->flip_session_pool())
+      session->flip_session_pool()->CloseAllSessions();
   }
 }
 
