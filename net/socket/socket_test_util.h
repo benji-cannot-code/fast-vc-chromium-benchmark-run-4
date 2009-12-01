5b6b98152d0e63207a1aa7d9028e1cb58e1f1902
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-01 01:34:01 +0000

    Remove extraneous semicolons.
    R=mbelshe
    BUG=none
    TEST=no compilation errors
    Review URL: http://codereview.chromium.org/450026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33407 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/socket_test_util.h
+++ net/socket/socket_test_util.h
@@ -51,7 +51,7 @@ struct MockConnect {
 
 struct MockRead {
   // Default
-  MockRead() : async(false), result(0), data(NULL), data_len(0) {};
+  MockRead() : async(false), result(0), data(NULL), data_len(0) {}
 
   // Read failure (no data).
   MockRead(bool async, int result) : async(async) , result(result), data(NULL),
@@ -282,8 +282,8 @@ class MockClientSocket : public net::SSLClientSocket {
                    net::CompletionCallback* callback) = 0;
   virtual int Write(net::IOBuffer* buf, int buf_len,
                     net::CompletionCallback* callback) = 0;
-  virtual bool SetReceiveBufferSize(int32 size) { return true; };
-  virtual bool SetSendBufferSize(int32 size) { return true; };
+  virtual bool SetReceiveBufferSize(int32 size) { return true; }
+  virtual bool SetSendBufferSize(int32 size) { return true; }
 
   // If an async IO is pending because the SocketDataProvider returned
   // ERR_IO_PENDING, then the MockClientSocket waits until this OnReadComplete
