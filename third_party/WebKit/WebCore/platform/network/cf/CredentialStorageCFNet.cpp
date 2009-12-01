9453f7d2b2b8026040203b6caccd899381f7840f
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-01 23:59:54 +0000

    More Windows build fix.
            * platform/network/cf/CredentialStorageCFNet.cpp:
            (WebCore::CredentialStorage::getFromPersistentStorage):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/CredentialStorageCFNet.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/CredentialStorageCFNet.cpp
@@ -37,7 +37,8 @@ namespace WebCore {
 
 Credential CredentialStorage::getFromPersistentStorage(const ProtectionSpace& protectionSpace)
 {
-    RetainPtr<CFURLCredentialRef> credentialCF(AdoptCF, wkCopyCredentialFromCFPersistentStorage(protectionSpace.get()));
+    RetainPtr<CFURLProtectionSpaceRef> protectionSpaceCF(AdoptCF, createCF(protectionSpace));
+    RetainPtr<CFURLCredentialRef> credentialCF(AdoptCF, wkCopyCredentialFromCFPersistentStorage(protectionSpaceCF.get()));
     return core(credentialCF.get());
 }
 
