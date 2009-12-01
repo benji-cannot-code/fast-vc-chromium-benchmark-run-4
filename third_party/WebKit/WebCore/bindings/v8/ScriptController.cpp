63eb0e7daa1adf7b989ed36fa772d8bfd909d6f9
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-01 16:17:42 +0000

    2009-12-01  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Adam Barth.
    
            Provide a way to get ScriptState for the inspected page.
    
            https://bugs.webkit.org/show_bug.cgi?id=32020
    
            * bindings/v8/ScriptController.cpp:
            (WebCore::ScriptController::mainWorldScriptState):
            * bindings/v8/ScriptController.h:
            * bindings/v8/ScriptState.cpp:
            (WebCore::scriptStateFromPage):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/ScriptController.cpp
+++ third_party/WebKit/WebCore/bindings/v8/ScriptController.cpp
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "npruntime_priv.h"
 #include "NPV8Object.h"
 #include "ScriptSourceCode.h"
+#include "ScriptState.h"
 #include "Settings.h"
 #include "V8Binding.h"
 #include "V8NPObject.h"
@@ -353,6 +354,13 @@ void ScriptController::cleanupScriptObjectsForPlugin(Widget* nativeHandle)
 void ScriptController::getAllWorlds(Vector<DOMWrapperWorld*>& worlds)
 {
     worlds.append(mainThreadNormalWorld());
+}
+
+ScriptState* ScriptController::mainWorldScriptState()
+{
+    if (!m_mainWorldScriptState)
+        m_mainWorldScriptState.set(new ScriptState(m_frame, V8Proxy::mainWorldContext(m_frame)));
+    return m_mainWorldScriptState.get();
 }
 
 static NPObject* createNoScriptObject()
