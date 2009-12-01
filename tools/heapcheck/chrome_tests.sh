3181908971fd4d556a52f3dfe01345032928b1f7
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-01 17:56:01 +0000

    Created tools/heapcheck with scripts for running heapchecker-enabled tests.
    chrome_tests.sh -- a frontend for the buildbot slave to run the tests under heap leak checker.
    chrome_tests.py -- almost copied from the similar scripts in ../valgrind/ and ../purify/, invokes heapcheck_test.py for each target.
    heapcheck_test.py -- runs a single test, processes the logs and applies suppressions to the reports returned by the heap checker.
    heapcheck_std.sh -- a tiny wrapper for IO redirection.
    suppressions.py -- provides the support for Valgrind-style stack trace suppressions.
    
    This patch was originally prepared by Alexander Potapenko (cc'ed) as http://codereview.chromium.org/400010
    
    TBR=dank,erikkay
    Review URL: http://codereview.chromium.org/455021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33451 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/heapcheck/chrome_tests.sh
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+THISDIR=$(dirname "${0}")
+PYTHONPATH="${THISDIR}/../../webkit/tools/layout_tests/:${THISDIR}/../valgrind:${THISDIR}/../python" exec "${THISDIR}/chrome_tests.py" "${@}"
