8e88bcdac2e15fab77ec87424259d94b01fe1d64
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-01 23:06:38 +0000

    Do not call GetServerCert whenever SSLHandshake returns.  Call GetServerCert only when we're about to verify a certificate or SSLHandshake fails with a certificate error.  This allows us to remove the null pointer test for |certs| in GetServerCert.
    Remove the TAB characters reported by cpplint.py.
    
    R=hawk
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/351033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33499 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_mac.cc
+++ net/socket/ssl_client_socket_mac.cc
@@ -267,7 +267,9 @@ int KeySizeOfCipherSuite(SSLCipherSuite suite) {
 X509Certificate* GetServerCert(SSLContextRef ssl_context) {
   CFArrayRef certs;
   OSStatus status = SSLCopyPeerCertificates(ssl_context, &certs);
-  // SSLCopyPeerCertificates may succeed but return a null |certs|.
+  // SSLCopyPeerCertificates may succeed but return a null |certs|
+  // (if we're using an anonymous cipher suite or if we call it
+  // before the certificate message has arrived and been parsed).
   if (status != noErr || !certs)
     return NULL;
   scoped_cftyperef<CFArrayRef> scoped_certs(certs);
@@ -434,8 +436,10 @@ bool SSLClientSocketMac::SetSendBufferSize(int32 size) {
 
 void SSLClientSocketMac::GetSSLInfo(SSLInfo* ssl_info) {
   ssl_info->Reset();
-  if (!server_cert_)
+  if (!server_cert_) {
+    NOTREACHED();
     return;
+  }
 
   // set cert
   ssl_info->cert = server_cert_;
@@ -682,12 +686,7 @@ int SSLClientSocketMac::DoHandshakeStart() {
   if (status == errSSLWouldBlock)
     next_handshake_state_ = STATE_HANDSHAKE_START;
 
-  server_cert_ = GetServerCert(ssl_context_);
-
   if (status == noErr || status == errSSLServerAuthCompletedFlag) {
-    if (!server_cert_)
-      return ERR_UNEXPECTED;
-
     // TODO(hawk): we verify the certificate chain even on resumed sessions
     // so that we have the certificate status (valid, expired but overridden
     // by the user, EV, etc.) available. Eliminate this step once we have
@@ -702,7 +701,13 @@ int SSLClientSocketMac::DoHandshakeStart() {
     }
   }
 
-  return NetErrorFromOSStatus(status);
+  int net_error = NetErrorFromOSStatus(status);
+  if (status == noErr || IsCertificateError(net_error)) {
+    server_cert_ = GetServerCert(ssl_context_);
+    if (!server_cert_)
+      return ERR_UNEXPECTED;
+  }
+  return net_error;
 }
 
 int SSLClientSocketMac::DoVerifyCert() {
