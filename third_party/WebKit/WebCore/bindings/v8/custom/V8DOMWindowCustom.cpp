ab0a7fb8bbb5434075a194afd7142192e6459c7d
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-02 03:44:11 +0000

    2009-12-01  Adam Barth  <abarth@webkit.org>
            https://bugs.webkit.org/show_bug.cgi?id=21288
    
            Unreviewed port of @sandbox to V8.
    
            * bindings/v8/ScriptController.cpp:
            (WebCore::ScriptController::isEnabled):
            * bindings/v8/custom/V8DOMWindowCustom.cpp:
            (WebCore::createWindow):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
@@ -551,6 +551,10 @@ static Frame* createWindow(Frame* callingFrame,
     ASSERT(callingFrame);
     ASSERT(enteredFrame);
 
+    // Sandboxed iframes cannot open new auxiliary browsing contexts.
+    if (callingFrame && callingFrame->loader()->isSandboxed(SandboxNavigation))
+        return 0;
+
     ResourceRequest request;
 
     // For whatever reason, Firefox uses the entered frame to determine
