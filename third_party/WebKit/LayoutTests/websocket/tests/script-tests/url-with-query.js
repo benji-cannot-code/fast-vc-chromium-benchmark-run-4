39719c5b6bb1e14b8221f06a6f969eb6e47def0f
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-02 08:19:47 +0000

    2009-12-02  Fumitoshi Ukai  <ukai@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            WebSocket handshake check query component of URL
            https://bugs.webkit.org/show_bug.cgi?id=31617
    
            * websocket/tests/echo-location_wsh.py: Added.
            * websocket/tests/no-query_wsh.py: Added.
            * websocket/tests/script-tests/url-with-credential.js: Added.
            * websocket/tests/script-tests/url-with-empty-query.js: Added.
            * websocket/tests/script-tests/url-with-fragment.js: Added.
            * websocket/tests/script-tests/url-with-query-for-no-query.js: Added.
            * websocket/tests/script-tests/url-with-query.js: Added.
            * websocket/tests/url-with-credential-expected.txt: Added.
            * websocket/tests/url-with-credential.html: Added.
            * websocket/tests/url-with-empty-query-expected.txt: Added.
            * websocket/tests/url-with-empty-query.html: Added.
            * websocket/tests/url-with-fragment-expected.txt: Added.
            * websocket/tests/url-with-fragment.html: Added.
            * websocket/tests/url-with-query-expected.txt: Added.
            * websocket/tests/url-with-query-for-no-query-expected.txt: Added.
            * websocket/tests/url-with-query-for-no-query.html: Added.
            * websocket/tests/url-with-query.html: Added.
    2009-12-02  Fumitoshi Ukai  <ukai@chromium.org>
    
            Reviewed by Alexey Proskuryakov.
    
            WebSocket handshake check query component of URL
            https://bugs.webkit.org/show_bug.cgi?id=31617
    
            Tests: websocket/tests/url-with-credential.html
                   websocket/tests/url-with-empty-query.html
                   websocket/tests/url-with-fragment.html
                   websocket/tests/url-with-query-for-no-query.html
                   websocket/tests/url-with-query.html
    
            * platform/KURLGoogle.cpp:
            (WebCore::KURL::query): returns a null if query is not specified and returns an empty if query is specified but empty.
            * websockets/WebSocketHandshake.cpp:
            (WebCore::resourceName): added. add query component to path if specified.
            (WebCore::WebSocketHandshake::clientLocation):
            (WebCore::WebSocketHandshake::clientHandshakeMessage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51589 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/websocket/tests/script-tests/url-with-query.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Make sure handshake with URL with query components success.");
+
+if (window.layoutTestController)
+    layoutTestController.waitUntilDone();
+
+var url = "ws://127.0.0.1:8880/websocket/tests/echo-location?q=v";
+var handshake_success = false;
+var ws_location;
+
+function endTest()
+{
+    clearTimeout(timeoutID);
+    shouldBeTrue("handshake_success");
+    shouldBe("ws_location", "url");
+    isSuccessfullyParsed();
+    if (window.layoutTestController)
+        layoutTestController.notifyDone();
+}
+
+debug("url=" + url);
+var ws = new WebSocket(url);
+ws.onopen = function () {
+    debug("WebSocket is open");
+    handshake_success = true;
+};
+ws.onmessage = function (evt) {
+    ws_location = evt.data;
+    debug("received:" + ws_location);
+    ws.close();
+};
+ws.onclose = function () {
+    debug("WebSocket is closed");
+    endTest();
+};
+var timeoutID = setTimeout("endTest()", 2000);
+
+var successfullyParsed = true;
