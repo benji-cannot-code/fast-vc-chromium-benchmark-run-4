1c1be0c922d7932be08723e500e23a2c45514f40
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-02 09:43:14 +0000

    2009-12-02  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            build-queue is throwing exceptions and complaining about
            lack of --no-update on build-attachment.  Make it stop.
    
            * Scripts/modules/buildsteps.py:
            * Scripts/modules/commands/download.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51593 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/commands/download.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/commands/download.py
@@ -267,7 +267,7 @@ class CheckStyle(AbstractPatchProcessingCommand):
         sequence.run_and_handle_errors()
 
 
-class BuildAttachmentSequence(LandingSequence):
+class BuildAttachmentSequence(ConditionalLandingSequence):
     def __init__(self, patch, options, tool):
         LandingSequence.__init__(self, patch, options, tool)
 
@@ -284,6 +284,7 @@ class BuildAttachment(AbstractPatchProcessingCommand):
     def __init__(self):
         options = BuildSteps.cleaning_options()
         options += BuildSteps.build_options()
+        options += BuildSteps.land_options()
         AbstractPatchProcessingCommand.__init__(self, "Apply and build patches from bugzilla", "ATTACHMENT_ID [ATTACHMENT_IDS]", options)
 
     def _fetch_list_of_patches_to_process(self, options, args, tool):
