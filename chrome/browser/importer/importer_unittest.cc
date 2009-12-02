5218048041497cbceb70de5e6cb48e280884c5a8
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-02 08:16:39 +0000

    Fix Windows build by #including needed header.
    TBR=sky
    
    Review URL: http://codereview.chromium.org/463003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33563 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/importer_unittest.cc
+++ chrome/browser/importer/importer_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_comptr_win.h"
 #include "base/stl_util-inl.h"
 #include "chrome/browser/chrome_thread.h"
+#include "chrome/browser/history/history_types.h"
 #include "chrome/browser/importer/ie_importer.h"
 #include "chrome/browser/importer/importer.h"
 #include "chrome/browser/importer/importer_bridge.h"
