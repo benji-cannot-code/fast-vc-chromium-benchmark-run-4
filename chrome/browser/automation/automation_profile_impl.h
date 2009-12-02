19cf76dffd8e6284d7cf84d738606beef4b36bd5
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-02 00:48:55 +0000

    Speculative fix for ChromeFrame crash in bug http://code.google.com/p/chromium/issues/detail?id=29025
    The crash occurs while dereferencing the automation channel to send out the SetCookie IPC message on the
    automation channel to the host browser. Based on what I could see from the crash dump and the code
    it seems like there could be a scenario where the AutomationResourceContext object could be destroyed
    while the AutomationCookieStore object is still around and thus ends up with a stale pointer which crashes
    when dereferenced.
    
    Fix is to ensure that all related code paths hold on to a refcounted AutomationResourceContext instance.
    
    I will look into whether it is possible to come up with a unit test for this.
    
    Bug=29025
    
    Review URL: http://codereview.chromium.org/450020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33524 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_profile_impl.h
+++ chrome/browser/automation/automation_profile_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Profile;
 class ChromeURLRequestContextGetter;
+class AutomationResourceMessageFilter;
 
 namespace AutomationRequestContext {
 
@@ -18,7 +19,7 @@ namespace AutomationRequestContext {
 ChromeURLRequestContextGetter* CreateAutomationURLRequestContextForTab(
     int tab_handle,
     Profile* profile,
-    IPC::Message::Sender* automation_client);
+    AutomationResourceMessageFilter* automation_client);
 
 }
 
