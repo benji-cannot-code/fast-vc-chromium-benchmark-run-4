e093578df781d8cfe0126177c3ae76ce337e5f09
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-02 04:54:13 +0000

    Build fixage for Windows/GTK for DumpRenderTree.
    WAI-ARIA: implement support for ARIA drag and drop
    https://bugs.webkit.org/show_bug.cgi?id=32007
    
    * DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp:
    (AccessibilityUIElement::ariaIsGrabbed):
    (AccessibilityUIElement::ariaDropEffects):
    * DumpRenderTree/win/AccessibilityUIElementWin.cpp:
    (AccessibilityUIElement::ariaIsGrabbed):
    (AccessibilityUIElement::ariaDropEffects):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51583 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp
@@ -335,6 +335,16 @@ int AccessibilityUIElement::hierarchicalLevel() const
     return 0;
 }
 
+bool AccessibilityUIElement::ariaIsGrabbed() const
+{
+    return false;
+}
+ 
+JSStringRef AccessibilityUIElement::ariaDropEffects() const
+{   
+    return 0; 
+}
+
 bool AccessibilityUIElement::isExpanded() const
 {
     // FIXME: implement
