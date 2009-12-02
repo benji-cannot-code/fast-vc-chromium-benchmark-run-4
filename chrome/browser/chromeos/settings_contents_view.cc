9ebbb12421f701499083e121593fe86bb3486e43
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-02 08:14:47 +0000

    Fix ChromeOS build by #including needed header.
    TBR=sky
    
    Review URL: http://codereview.chromium.org/459012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33562 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/settings_contents_view.cc
+++ chrome/browser/chromeos/settings_contents_view.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "chrome/browser/chromeos/network_library.h"
 #include "chrome/browser/chromeos/password_dialog_view.h"
+#include "chrome/browser/profile.h"
 #include "chrome/common/pref_member.h"
 #include "chrome/common/pref_names.h"
 #include "grit/chromium_strings.h"
