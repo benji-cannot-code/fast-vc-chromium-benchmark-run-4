ec1cf9f94e235dcce2eb24fff0fab5436692ce6f
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-03 16:57:55 +0000

    2009-12-03  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Adam Barth.
    
            [V8] Attributes and NamedNodeMaps aren't tracked correctly and may be prematurely garbage-collected.
            https://bugs.webkit.org/show_bug.cgi?id=32094
    
            Covered by existing test: LayoutTests/fast/dom/Attr/access-after-element-destruction.html
    
            * bindings/v8/DOMObjectsInclude.h:
            * bindings/v8/V8DOMWrapper.cpp:
            (WebCore::V8DOMWrapper::getTemplate):
            (WebCore::V8DOMWrapper::convertToV8Object):
            (WebCore::V8DOMWrapper::convertNamedNodeMapToV8Object):
            * bindings/v8/V8DOMWrapper.h:
            * bindings/v8/V8GCController.cpp:
            (WebCore::ObjectGrouperVisitor::visitDOMWrapper):
            * bindings/v8/custom/V8CustomBinding.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51638 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/DOMObjectsInclude.h
+++ third_party/WebKit/WebCore/bindings/v8/DOMObjectsInclude.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DOMObjectsInclude_h
 #define DOMObjectsInclude_h
 
+#include "Attr.h"
 #include "BarInfo.h"
 #include "BeforeLoadEvent.h"
 #include "WebGLActiveInfo.h"
