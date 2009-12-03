7e3f49405fa30762a1215209a4e264a9fd994eb7
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-03 21:40:26 +0000

    Fix data races in Safe Browsing Service that were possible with aggressive compiler optimizations.
    BUG=28559
    TEST=none
    Review URL: http://codereview.chromium.org/455039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33726 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_service.h
+++ chrome/browser/safe_browsing/safe_browsing_service.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/hash_tables.h"
+#include "base/lock.h"
 #include "base/ref_counted.h"
 #include "base/time.h"
 #include "chrome/browser/safe_browsing/safe_browsing_util.h"
@@ -190,9 +191,7 @@ class SafeBrowsingService
   void OnIOShutdown();
 
   // Returns whether |database_| exists and is accessible.
-  bool database_available() const {
-    return !closing_database_ && (database_ != NULL);
-  }
+  bool DatabaseAvailable() const;
 
   // Called on the IO thread.  If the database does not exist, queues up a call
   // on the db thread to create it.  Returns whether the database is available.
@@ -279,6 +278,9 @@ class SafeBrowsingService
   // The sqlite database.  We don't use a scoped_ptr because it needs to be
   // destructed on a different thread than this object.
   SafeBrowsingDatabase* database_;
+
+  // Lock used to prevent possible data races due to compiler optimizations.
+  mutable Lock database_lock_;
 
   // Handles interaction with SafeBrowsing servers.
   SafeBrowsingProtocolManager* protocol_manager_;
