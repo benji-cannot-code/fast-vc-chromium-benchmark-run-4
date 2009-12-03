8a320edac31739da180a2cf5707ced110f868eae
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-03 00:22:38 +0000

    Fixed <rdar://problem/7254127> WebKit can load plug-ins even when plug-ins are disabled
    Reviewed by Anders Carlsson.
    
    Ensure that the shared WebPluginDatabase is not instantiated if no
    WebViews are used that have plug-ins enabled.
    
    * DefaultDelegates/WebDefaultPolicyDelegate.m:
    (-[WebDefaultPolicyDelegate webView:decidePolicyForMIMEType:request:frame:decisionListener:]):
    Call -[WebView _canShowMIMEType:] on the WebView instead of calling the
    class method.
    
    * WebCoreSupport/WebFrameLoaderClient.mm:
    (WebFrameLoaderClient::canShowMIMEType): Ditto.
    (WebFrameLoaderClient::transitionToCommittedForNewPage): Call
    -[WebFrameView _viewClassForMIMEType:] on the WebView instead of calling
    the class method.
    (WebFrameLoaderClient::objectContentType): Ditto.
    
    * WebView/WebDataSource.mm:
    (+[WebDataSource _representationClassForMIMEType:allowingPlugins:]):
    Added the allowPlugins parameter, which is passed through to
    +[WebView _viewClass:andRepresentationClass:forMIMEType:allowingPlugins:].
    (-[WebDataSource _makeRepresentation]): Pass an allowPlugins value
    based on the WebView’s preferences.
    
    * WebView/WebFrameView.mm:
    (-[WebFrameView _makeDocumentViewForDataSource:]): Call
    -[WebFrameView _viewClassForMIMEType:] instead of calling the class
    method.
    (+[WebFrameView _viewClassForMIMEType:allowingPlugins:]): Added the
    allowPlugins parameter, which is passed through to
    +[WebView _viewClass:andRepresentationClass:forMIMEType:allowingPlugins:].
    (-[WebFrameView _viewClassForMIMEType:]): Added. Passes an allowPlugins
    value based on the WebView’s preferences.
    
    * WebView/WebFrameViewInternal.h:
    * WebView/WebView.mm:
    (+[WebView _viewClass:andRepresentationClass:forMIMEType:allowingPlugins:]):
    Added the allowPlugins parameter. If false, skip the instantiation of
    the shared WebPluginDatabase.
    (-[WebView _viewClass:andRepresentationClass:forMIMEType:]): Pass an
    allowPlugins value based on the WebView’s preferences.
    (+[WebView _canShowMIMEType:allowingPlugins:]): Passes allowPlugins to
    +_viewClass:andRepresentationClass:forMIMEType:allowingPlugins:.
    (+[WebView canShowMIMEType:]): Changed to pass YES to
    +_canShowMIMEType:allowingPlugins:.
    (-[WebView _canShowMIMEType:]): Added. Passes an allowPlugins value
    based on the WebView’s preferences.
    (-[WebView _pluginForMIMEType:]): Return nil if plug-ins are disabled.
    (-[WebView _pluginForExtension:]): Ditto.
    (-[WebView _isMIMETypeRegisteredAsPlugin:]): Return NO if plug-ins are
    disabled.
    * WebView/WebViewInternal.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebFrameViewInternal.h
+++ third_party/WebKit/WebKit/mac/WebView/WebFrameViewInternal.h
@@ -40,7 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)_setWebFrame:(WebFrame *)webFrame;
 - (float)_verticalPageScrollDistance;
 + (NSMutableDictionary *)_viewTypesAllowImageTypeOmission:(BOOL)allowImageTypeOmission;
-+ (Class)_viewClassForMIMEType:(NSString *)MIMEType;
+- (Class)_viewClassForMIMEType:(NSString *)MIMEType;
++ (Class)_viewClassForMIMEType:(NSString *)MIMEType allowingPlugins:(BOOL)allowPlugins;
 + (BOOL)_canShowMIMETypeAsHTML:(NSString *)MIMEType;
 - (WebDynamicScrollBarsView *)_scrollView;
 - (void)_install;
