4ca9742b71fd5d1a6ac474a1b390e86bd32d2f6f
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-03 21:45:06 +0000

    Remove the deprecated version of FixupURL and fix the callers.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/455022
    Patch from tfarina.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33728 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/preferences_window_controller.mm
+++ chrome/browser/cocoa/preferences_window_controller.mm
@@ -49,9 +49,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-std::wstring GetNewTabUIURLString() {
-  std::wstring temp = UTF8ToWide(chrome::kChromeUINewTabURL);
-  return URLFixerUpper::FixupURL(temp, std::wstring());
+std::string GetNewTabUIURLString() {
+  return URLFixerUpper::FixupURL(chrome::kChromeUINewTabURL, std::string());
 }
 
 // Helper to remove all but the last view from the view heirarchy.
@@ -786,12 +785,12 @@ class PrefObserverBridge : public NotificationObserver,
 // observers not to fire, which is actually a good thing as we could end up in a
 // state where setting the homepage to an empty url would automatically reset
 // the prefs back to using the NTP, so we'd be never be able to change it.
-- (void)setHomepage:(const std::wstring&)homepage {
+- (void)setHomepage:(const std::string&)homepage {
   if (homepage.empty() || homepage == GetNewTabUIURLString()) {
     newTabPageIsHomePage_.SetValue(true);
   } else {
     newTabPageIsHomePage_.SetValue(false);
-    homepage_.SetValue(homepage);
+    homepage_.SetValue(UTF8ToWide(homepage));
   }
 }
 
@@ -994,9 +993,9 @@ enum { kHomepageNewTabPage, kHomepageURL };
   // to something valid ("http://google.com").
   if (!urlString)
     urlString = [NSString stringWithFormat:@"%s", chrome::kChromeUINewTabURL];
-  std::wstring temp = base::SysNSStringToWide(urlString);
-  std::wstring fixedString = URLFixerUpper::FixupURL(temp, std::wstring());
-  if (GURL(WideToUTF8(fixedString)).is_valid())
+  std::string temp = base::SysNSStringToUTF8(urlString);
+  std::string fixedString = URLFixerUpper::FixupURL(temp, std::string());
+  if (GURL(fixedString).is_valid())
     [self setHomepage:fixedString];
 }
 
