f5bedfceb5b2792291f2e1f85870940c7c7288f1
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-04 23:13:27 +0000

    Fix up some issues in my SVGListTraits change from yesterday.
    Reviewed by Deth Bakin
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51717 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGListTraits.h
+++ third_party/WebKit/WebCore/svg/SVGListTraits.h
@@ -37,14 +37,14 @@ namespace WebCore {
 
     template<typename ItemPtr>
     struct SVGListTraits<true, ItemPtr*> {
-        static ItemPtr nullItem() { return 0; }
-        static bool isNull(ItemPtr it) { return !it; }
+        static ItemPtr* nullItem() { return 0; }
+        static bool isNull(ItemPtr* it) { return !it; }
     };
     
     template<typename ItemPtr>
     struct SVGListTraits<true, RefPtr<ItemPtr> > {
         static RefPtr<ItemPtr> nullItem() { return 0; }
-        static bool isNull(RefPtr<ItemPtr> it) { return !it; }
+        static bool isNull(const RefPtr<ItemPtr>& it) { return !it; }
     };
     
     template<typename Item>
@@ -62,7 +62,7 @@ namespace WebCore {
     template<>
     struct SVGListTraits<false, float> {
         static float nullItem() { return 0; }
-        static bool isNull(double) { return false; }
+        static bool isNull(float) { return false; }
     };
 
 
