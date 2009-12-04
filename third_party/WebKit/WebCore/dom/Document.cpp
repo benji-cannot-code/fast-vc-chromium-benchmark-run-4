ade58182a51ed3bfa0ef4b0b7068191a51b07ac3
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-04 05:54:58 +0000

    2009-12-03  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            OwnPtr<RenderArena> Document::m_renderArena;
            https://bugs.webkit.org/show_bug.cgi?id=32146
    
            The document owns the renderArena.  That's what the code should say.
    
            * dom/Document.cpp:
            (WebCore::Document::Document): Also, removed a redundant initialization
            of the tokenizer that I missed in my previous patch.
            (WebCore::Document::~Document):
            (WebCore::Document::attach):
            (WebCore::Document::detach):
            * dom/Document.h:
            (WebCore::Document::renderArena):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51678 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -369,7 +369,6 @@ Document::Document(Frame* frame, bool isXHTML)
     m_ignoreAutofocus = false;
 
     m_frame = frame;
-    m_renderArena = 0;
 
     m_axObjectCache = 0;
     
@@ -377,7 +376,6 @@ Document::Document(Frame* frame, bool isXHTML)
 
     visuallyOrdered = false;
     m_bParsing = false;
-    m_tokenizer = 0;
     m_wellFormed = false;
 
     setParseMode(Strict);
@@ -488,10 +486,7 @@ Document::~Document()
     delete m_styleSelector;
     m_docLoader.clear();
 
-    if (m_renderArena) {
-        delete m_renderArena;
-        m_renderArena = 0;
-    }
+    m_renderArena.clear();
 
 #if ENABLE(XBL)
     delete m_bindingManager;
@@ -1407,7 +1402,7 @@ void Document::attach()
         m_renderArena = new RenderArena();
     
     // Create the rendering tree
-    setRenderer(new (m_renderArena) RenderView(this, view()));
+    setRenderer(new (m_renderArena.get()) RenderView(this, view()));
 #if USE(ACCELERATED_COMPOSITING)
     renderView()->didMoveOnscreen();
 #endif
@@ -1484,11 +1479,7 @@ void Document::detach()
     // or this setting of the frame to 0 could be made explicit in each of the
     // callers of Document::detach().
     m_frame = 0;
-    
-    if (m_renderArena) {
-        delete m_renderArena;
-        m_renderArena = 0;
-    }
+    m_renderArena.clear();
 }
 
 void Document::removeAllEventListeners()
