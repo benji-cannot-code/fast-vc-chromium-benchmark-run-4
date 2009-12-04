c5d06beb8db697990b102c19aa63f735fb1ca300
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-04 10:18:42 +0000

    2009-12-04  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Gustavo Noronha.
    
            [GTK]Enable DNS prefetching
            https://bugs.webkit.org/show_bug.cgi?id=23846
    
            Bump libsoup required version to 2.29.3 for DNS prefetching.
    
            * configure.ac:
    
    WebCore:
    
    2009-12-04  Xan Lopez  <xlopez@igalia.com>
    
            Reviewed by Gustavo Noronha.
    
            [GTK]Enable DNS prefetching
            https://bugs.webkit.org/show_bug.cgi?id=23846
    
            Enable DNS prefetching.
    
            Based on a patch by José Millán.
    
            * platform/network/soup/DNSSoup.cpp:
            (WebCore::prefetchDNS):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51689 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/soup/DNSSoup.cpp
+++ third_party/WebKit/WebCore/platform/network/soup/DNSSoup.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2008 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2009 Igalia S.L.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,13 +28,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "DNS.h"
 
-#include "NotImplemented.h"
+#include "CString.h"
+#include "ResourceHandle.h"
 
 namespace WebCore {
 
 void prefetchDNS(const String& hostname)
 {
-    notImplemented();
+    String uri = "http://"+hostname;
+    SoupURI* soupUri = soup_uri_new(uri.utf8().data());
+    soup_session_prepare_for_uri(ResourceHandle::defaultSession(), soupUri);
+    soup_uri_free(soupUri);
 }
 
 }
