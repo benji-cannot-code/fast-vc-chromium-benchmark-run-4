3b4096ff0abd4c9e17f4b1a0a3c7078643657bdc
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-04 00:09:04 +0000

    Fix incorrect error codes.  9999 is not a valid network error code (always a negative number).  The most likely reason for memio_CreateIOLayer or SSL_ImportFD to fail is that we ran out of memory, so just return ERR_OUT_OF_MEMORY.
    R=dank
    BUG=none
    TEST=no compilation errors.
    Review URL: http://codereview.chromium.org/464024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33754 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.cc
+++ net/socket/ssl_client_socket_nss.cc
@@ -247,7 +247,7 @@ int SSLClientSocketNSS::InitializeSSLOptions() {
   // TODO(port): specify rx and tx buffer sizes separately
   nss_fd_ = memio_CreateIOLayer(kRecvBufferSize);
   if (nss_fd_ == NULL) {
-    return 9999;  // TODO(port): real error
+    return ERR_OUT_OF_MEMORY;  // TODO(port): map NSPR error code.
   }
 
   // Tell NSS who we're connected to
@@ -256,7 +256,8 @@ int SSLClientSocketNSS::InitializeSSLOptions() {
   int err = transport_->GetPeerName((struct sockaddr *)&peername, &len);
   if (err) {
     DLOG(ERROR) << "GetPeerName failed";
-    return 9999;  // TODO(port): real error
+    // TODO(wtc): Change GetPeerName to return a network error code.
+    return ERR_UNEXPECTED;
   }
   memio_SetPeerName(nss_fd_, &peername);
 
@@ -267,7 +268,7 @@ int SSLClientSocketNSS::InitializeSSLOptions() {
   /* Push SSL onto our fake I/O socket */
   nss_fd_ = SSL_ImportFD(NULL, nss_fd_);
   if (nss_fd_ == NULL) {
-      return ERR_SSL_PROTOCOL_ERROR;  // TODO(port): real error
+      return ERR_OUT_OF_MEMORY;  // TODO(port): map NSPR/NSS error code.
   }
   // TODO(port): set more ssl options!  Check errors!
 
