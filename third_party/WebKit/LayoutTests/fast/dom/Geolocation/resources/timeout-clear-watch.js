9625615ae2c70b30249f366b5afcc6c043a1de7e
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-04 11:30:41 +0000

    2009-12-04  Steve Block  <steveblock@google.com>
            Reviewed by Darin Adler.
    
            Fixes a crashing bug in Geolocation when a watch is cleared from some callbacks.
            https://bugs.webkit.org/show_bug.cgi?id=32111
    
            * fast/dom/Geolocation/resources/permission-denied-already-clear-watch.js: Added. Tests clearing the watch from the error callback after a permission denied message when permissions have already been denied.
            * fast/dom/Geolocation/permission-denied-already-clear-watch.html: Added. HTML wrapper for above test.
            * fast/dom/Geolocation/permission-denied-already-clear-watch-expected.txt: Added. Expected result for above test.
    
            * fast/dom/Geolocation/resources/timeout-clear-watch.js: Added. Tests clearing the watch from the error callback due to a timeout.
            * fast/dom/Geolocation/timeout-clear-watch.html: Added. HTML wrapper for above test.
            * fast/dom/Geolocation/timeout-clear-watch-expected.txt: Added. Expected result for above test.
    
            * platform/gtk/Skipped: Modified. Skips new tests.
    2009-12-04  Steve Block  <steveblock@google.com>
    
            Reviewed by Darin Adler.
    
            Fixes a crashing bug in Geolocation when a watch is cleared from some callbacks.
            https://bugs.webkit.org/show_bug.cgi?id=32111
    
            In Geolocation::GeoNotifier::timerFired, a JS callback may be invoked if a
            fatal error was registered on this GeoNotifier or if this request has timed
            out. If the request is a watch, and is cleared by a call to clearWatch in the
            callback, the GeoNotifier object will be destroyed. We must therefore cache the
            m_geolocation member to allow us to call Geolocation::fatalErrorOccurred or
            Geolocation::requestTimedOut.
    
            Tests: fast/dom/Geolocation/permission-denied-already-clear-watch.html
                   fast/dom/Geolocation/timeout-clear-watch.html
    
            * page/Geolocation.cpp: Modified.
            (WebCore::Geolocation::GeoNotifier::timerFired): Modified. Cache the m_geolocation member before invoking the JS callback.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51692 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/Geolocation/resources/timeout-clear-watch.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Tests that when a watch times out and is cleared from the error callback, there is no crash. This a regression test for https://bugs.webkit.org/show_bug.cgi?id=32111.");
+
+window.layoutTestController.setMockGeolocationPosition(51.478, -0.166, 100.0);
+
+var error;
+var watchId = navigator.geolocation.watchPosition(function() {
+    testFailed('Success callback invoked unexpectedly');
+    window.layoutTestController.notifyDone();
+}, function(e) {
+    error = e
+    shouldBe('error.code', 'error.TIMEOUT');
+    shouldBe('error.message', '"Timeout expired"');
+    navigator.geolocation.clearWatch(watchId);
+    window.setTimeout(completeTest, 0);
+}, {
+    timeout: 0
+});
+
+function completeTest()
+{
+    debug('<br /><span class="pass">TEST COMPLETE</span>');
+    window.layoutTestController.notifyDone();
+}
+window.layoutTestController.waitUntilDone();
+
+var isAsynchronous = true;
+var successfullyParsed = true;
