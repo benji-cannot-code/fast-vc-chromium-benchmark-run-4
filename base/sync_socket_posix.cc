4020da99eeaf3ed3372e6ebe314d583cdd011032
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-04 22:53:22 +0000

    Add non-blocking peek for the syncsocket - Windows only
    BUG=none
    TEST=unit test included
    
    
    Review URL: http://codereview.chromium.org/464020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33876 0039d316-1c4b-4281-b951-d872f2087c98

--- base/sync_socket_posix.cc
+++ base/sync_socket_posix.cc
@@ -99,4 +99,10 @@ size_t SyncSocket::Receive(void* buffer, size_t length) {
   }
 }
 
+// TODO(port). Some kind of select?
+size_t SyncSocket::Peek() {
+  NOTIMPLEMENTED();
+  return 0;
+}
+
 }  // namespace base
