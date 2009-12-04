8fae3cb74e46ee1d9ed5136c6b6fa05efd7c58ab
atwilson@chromium.org <atwilson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-04 05:54:31 +0000

    Revert r51633
    This reverts commit 4c9cc0b9a12d38654b9c687ffc1671c7307b397a.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51677 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/SimpleFontData.h
+++ third_party/WebKit/WebCore/platform/graphics/SimpleFontData.h
@@ -116,7 +116,7 @@ public:
     virtual String description() const;
 #endif
 
-#if PLATFORM(MAC) || (PLATFORM(CHROMIUM) && PLATFORM(DARWIN))
+#if PLATFORM(MAC)
     NSFont* getNSFont() const { return m_platformData.font(); }
 #endif
 
