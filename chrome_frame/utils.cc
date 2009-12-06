44443132096a5fb5dd767bd4b29a49b9e2b72303
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-06 14:26:16 +0000

    Fix the ChromeFrame build bot redness caused by us incorrectly wrapping protocol sinks which don't have an associated IWebBrowser.
    TBR=robertshield
    
    Review URL: http://codereview.chromium.org/469003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33942 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/utils.cc
+++ chrome_frame/utils.cc
@@ -662,6 +662,8 @@ bool IsSubFrameRequest(IUnknown* service_provider) {
         is_non_top_level_request = true;
       }
     }
+  } else {
+    is_non_top_level_request = true;
   }
 
   return is_non_top_level_request;
