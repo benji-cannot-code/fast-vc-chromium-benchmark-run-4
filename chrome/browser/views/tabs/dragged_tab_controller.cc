495d0cffa875aae6154f0dbd4576776f4aaa01d5
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-07 21:57:51 +0000

    Use gfx::Point instead of GET_X/Y_LPARAM to reduce the dependency on ATL.
    Patch from Thiago Farina <thiago.farina@gmail.com>
    Original review: http://codereview.chromium.org/465004
    
    BUG=5027
    TEST=None 
    Review URL: http://codereview.chromium.org/460121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33996 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/dragged_tab_controller.cc
+++ chrome/browser/views/tabs/dragged_tab_controller.cc
@@ -1297,7 +1297,7 @@ void DraggedTabController::EnsureDraggedView() {
 gfx::Point DraggedTabController::GetCursorScreenPoint() const {
 #if defined(OS_WIN)
   DWORD pos = GetMessagePos();
-  return gfx::Point(GET_X_LPARAM(pos), GET_Y_LPARAM(pos));
+  return gfx::Point(pos);
 #else
   gint x, y;
   gdk_display_get_pointer(gdk_display_get_default(), NULL, &x, &y, NULL);
