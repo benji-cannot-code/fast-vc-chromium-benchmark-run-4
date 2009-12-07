20c9efe838e03461be07e04dd7a16f0cae9e0959
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-07 21:05:39 +0000

    Reviewed by Nikolas Zimmermann.
            https://bugs.webkit.org/show_bug.cgi?id=32226
            Random crashes in WebSocket tests on Leopard Debug builbot
    
            * platform/network/SocketStreamHandleBase.cpp: (WebCore::SocketStreamHandleBase::close):
            Make sure the object still exists when setting m_state.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/SocketStreamHandleBase.cpp
+++ third_party/WebKit/WebCore/platform/network/SocketStreamHandleBase.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "SocketStreamHandleBase.h"
 
+#include "SocketStreamHandle.h"
 #include "SocketStreamHandleClient.h"
 
 namespace WebCore {
@@ -78,6 +79,8 @@ bool SocketStreamHandleBase::send(const char* data, int length)
 
 void SocketStreamHandleBase::close()
 {
+    RefPtr<SocketStreamHandle> protect(static_cast<SocketStreamHandle*>(this)); // platformClose calls the client, which may make the handle get deallocated immediately.
+
     platformClose();
     m_state = Closed;
 }
