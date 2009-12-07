73371fce6d7b04d79ff3cc83765c45f9387c927f
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-07 21:11:52 +0000

    Refactor the pepper API implementation to make it more scalable as we add more device types. Implement a stub of the 3D device API. Fix style in some of the plugin related code.
    TEST=run the pepper_test_plugin
    BUG=none
    Review URL: http://codereview.chromium.org/468012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33989 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/plugin_lib.cc
+++ webkit/glue/plugins/plugin_lib.cc
@@ -14,8 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/plugins/plugin_host.h"
 #include "webkit/glue/plugins/plugin_list.h"
 
-namespace NPAPI
-{
+namespace NPAPI {
 
 const char kPluginLibrariesLoadedCounter[] = "PluginLibrariesLoaded";
 const char kPluginInstancesActiveCounter[] = "PluginInstancesActive";
