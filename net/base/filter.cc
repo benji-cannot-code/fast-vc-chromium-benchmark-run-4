79e7738ff491060c7e4634e96da5f74a05dc76ed
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-08 08:28:17 +0000

    Remove bzip2 decoding support completely.
    BUG=14801,26577
    TEST=see bug.
    Review URL: http://codereview.chromium.org/466038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34047 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/filter.cc
+++ net/base/filter.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/string_util.h"
 #include "net/base/gzip_filter.h"
-#include "net/base/bzip2_filter.h"
 #include "net/base/io_buffer.h"
 #include "net/base/mime_util.h"
 #include "net/base/sdch_filter.h"
@@ -19,8 +18,6 @@ namespace {
 const char kDeflate[]      = "deflate";
 const char kGZip[]         = "gzip";
 const char kXGZip[]        = "x-gzip";
-const char kBZip2[]        = "bzip2";
-const char kXBZip2[]       = "x-bzip2";
 const char kSdch[]         = "sdch";
 // compress and x-compress are currently not supported.  If we decide to support
 // them, we'll need the same mime type compatibility hack we have for gzip.  For
@@ -66,9 +63,6 @@ Filter::FilterType Filter::ConvertEncodingToType(
   } else if (LowerCaseEqualsASCII(filter_type, kGZip) ||
              LowerCaseEqualsASCII(filter_type, kXGZip)) {
     type_id = FILTER_TYPE_GZIP;
-  } else if (LowerCaseEqualsASCII(filter_type, kBZip2) ||
-             LowerCaseEqualsASCII(filter_type, kXBZip2)) {
-    type_id = FILTER_TYPE_BZIP2;
   } else if (LowerCaseEqualsASCII(filter_type, kSdch)) {
     type_id = FILTER_TYPE_SDCH;
   } else {
@@ -249,15 +243,6 @@ Filter* Filter::PrependNewFilter(FilterType type_id,
       if (gz_filter->InitBuffer()) {
         if (gz_filter->InitDecoding(type_id)) {
           first_filter = gz_filter.release();
-        }
-      }
-      break;
-    }
-    case FILTER_TYPE_BZIP2: {
-      scoped_ptr<BZip2Filter> bzip2_filter(new BZip2Filter(filter_context));
-      if (bzip2_filter->InitBuffer()) {
-        if (bzip2_filter->InitDecoding(false)) {
-          first_filter = bzip2_filter.release();
         }
       }
       break;
