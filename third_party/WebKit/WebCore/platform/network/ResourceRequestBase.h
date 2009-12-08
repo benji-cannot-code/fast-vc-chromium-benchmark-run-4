0cdef4ae7d2e13242af882cd49a46c6c92437245
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-08 17:04:59 +0000

    2009-12-08  Mike Belshe  <mike@belshe.com>
            Reviewed by Darin Fisher.
    
            https://bugs.webkit.org/show_bug.cgi?id=32152
            Update the ResourceRequest::RequestType.  This previously
            was specific to Chromium.  Moved into ResourceRequestBase, enabling
            more specificity about the type (which is otherwise only known to the
            loader), and also making this information available to all platforms.
            Any platform with a network layer which can utilize this information
            may want to use it for prioritization.
    
            Note to Chromium glue: TargetIsSubResource renamed to TargetIsSubresource.
    
            * loader/loader.cpp:
            (WebCore::Loader::Loader):
            (WebCore::CachedResourceTypeToTargetType):
            (WebCore::Loader::Host::servePendingRequests):
            * platform/network/ResourceRequestBase.h:
            (WebCore::ResourceRequestBase::):
            (WebCore::ResourceRequestBase::targetType):
            (WebCore::ResourceRequestBase::setTargetType):
            (WebCore::ResourceRequestBase::ResourceRequestBase):
            * platform/network/chromium/ResourceRequest.h:
            (WebCore::ResourceRequest::ResourceRequest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51859 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceRequestBase.h
+++ third_party/WebKit/WebCore/platform/network/ResourceRequestBase.h
@@ -53,6 +53,19 @@ namespace WebCore {
     // Do not use this type directly.  Use ResourceRequest instead.
     class ResourceRequestBase {
     public:
+        // The type of this ResourceRequest, based on how the resource will be used.
+        enum TargetType {
+            TargetIsMainFrame,
+            TargetIsSubframe,
+            TargetIsSubresource,  // Resource is a generic subresource.  (Generally a specific type should be specified)
+            TargetIsStyleSheet,
+            TargetIsScript,
+            TargetIsFontResource,
+            TargetIsImage,
+            TargetIsObject,
+            TargetIsMedia
+        };
+
         static std::auto_ptr<ResourceRequest> adopt(std::auto_ptr<CrossThreadResourceRequestData>);
 
         // Gets a copy of the data suitable for passing to another thread.
@@ -118,12 +131,17 @@ namespace WebCore {
         bool reportUploadProgress() const { return m_reportUploadProgress; }
         void setReportUploadProgress(bool reportUploadProgress) { m_reportUploadProgress = reportUploadProgress; }
 
+        // What this request is for.
+        TargetType targetType() const { return m_targetType; }
+        void setTargetType(TargetType type) { m_targetType = type; }
+
     protected:
         // Used when ResourceRequest is initialized from a platform representation of the request
         ResourceRequestBase()
             : m_resourceRequestUpdated(false)
             , m_platformRequestUpdated(true)
             , m_reportUploadProgress(false)
+            , m_targetType(TargetIsSubresource)
         {
         }
 
@@ -136,6 +154,7 @@ namespace WebCore {
             , m_resourceRequestUpdated(true)
             , m_platformRequestUpdated(false)
             , m_reportUploadProgress(false)
+            , m_targetType(TargetIsSubresource)
         {
         }
 
@@ -155,6 +174,7 @@ namespace WebCore {
         mutable bool m_resourceRequestUpdated;
         mutable bool m_platformRequestUpdated;
         bool m_reportUploadProgress;
+        TargetType m_targetType;
 
     private:
         const ResourceRequest& asResourceRequest() const;
