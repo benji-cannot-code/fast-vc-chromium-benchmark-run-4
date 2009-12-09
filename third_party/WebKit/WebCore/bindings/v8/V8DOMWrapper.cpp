4816d14963a7d996db70b1ccc87e04da83d17a3d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-09 20:36:38 +0000

    2009-12-09  Michael Nordman  <michaeln@google.com>
            Reviewed by Dimitri Glazkov.
    
            Chromium build fix.
    
            No new tests.
    
            * bindings/v8/DOMData.cpp
            * bindings/v8/V8DOMWrapper.cpp
            * bindings/v8/custom/V8WebGLRenderingContextCustom.cpp:
            (WebCore::vertexAttribAndUniformHelperf):
            (WebCore::uniformHelperi):
            (WebCore::uniformMatrixHelper):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51915 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8DOMWrapper.cpp
+++ third_party/WebKit/WebCore/bindings/v8/V8DOMWrapper.cpp
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8IsolatedWorld.h"
 #include "V8Proxy.h"
 #include "WebGLArray.h"
+#include "WebGLUniformLocation.h"
 #include "WorkerContextExecutionProxy.h"
 
 #include <algorithm>
