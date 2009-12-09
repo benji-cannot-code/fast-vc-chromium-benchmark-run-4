8546b7e72d6a83823fc412dda2334b97e93e31d1
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-09 01:32:06 +0000

    Remove DCHECK because it's invalid in single process mode.
    Review URL: http://codereview.chromium.org/460151
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34123 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_thread.cc
+++ chrome/renderer/render_thread.cc
@@ -292,7 +292,6 @@ void RenderThread::OnExtensionSetAPIPermissions(
 
   // This is called when starting a new extension page, so start the idle
   // handler ticking.
-  DCHECK(is_extension_process());
   ScheduleIdleHandler(kInitialExtensionIdleHandlerDelayS);
 }
 
