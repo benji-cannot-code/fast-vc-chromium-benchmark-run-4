1e42e69e1facc07ea811976a91705eb0e7574bae
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-09 05:25:48 +0000

    Add views/examples/slider_example.h
    The example demonstrates how to use the Slider class.
    
    BUG=none
    TEST=manually
    
    Review URL: http://codereview.chromium.org/466055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34142 0039d316-1c4b-4281-b951-d872f2087c98

--- views/examples/examples_main.cc
+++ views/examples/examples_main.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/examples/message_box_example.h"
 #include "views/examples/radio_button_example.h"
 #include "views/examples/scroll_view_example.h"
+#include "views/examples/slider_example.h"
 #include "views/examples/tabbed_pane_example.h"
 #if defined(OS_WIN)
 // TableView and TableView2 are not yet ported to Linux.
@@ -80,7 +81,7 @@ void ExamplesMain::Run() {
   layout->AddView(status_label_);
 
   views::Window* window =
-      views::Window::CreateChromeWindow(NULL, gfx::Rect(0, 0, 700, 300), this);
+      views::Window::CreateChromeWindow(NULL, gfx::Rect(0, 0, 750, 300), this);
 
   examples::TextfieldExample textfield_example(this);
   tabbed_pane->AddTab(textfield_example.GetExampleTitle(),
@@ -123,6 +124,10 @@ void ExamplesMain::Run() {
   examples::WidgetExample widget_example(this);
   tabbed_pane->AddTab(widget_example.GetExampleTitle(),
                       widget_example.GetExampleView());
+
+  examples::SliderExample slider_example(this);
+  tabbed_pane->AddTab(slider_example.GetExampleTitle(),
+                      slider_example.GetExampleView());
 
   window->Show();
   views::AcceleratorHandler accelerator_handler;
