6d22a2b7dc9110200a8861b3b6f45f597e82bc7a
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-09 08:24:59 +0000

    2009-12-09  Adam Barth  <abarth@webkit.org>
            Unreviewed "build" fix.
    
            * Scripts/modules/landingsequence.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/landingsequence.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/landingsequence.py
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from modules.comments import bug_comment_from_commit_text
+from modules.executive import ScriptError
 from modules.logging import log
-from modules.processutils import ScriptError
 from modules.scm import CheckoutNeedsUpdate
 from modules.webkitport import WebKitPort
 from modules.workqueue import WorkQueue
