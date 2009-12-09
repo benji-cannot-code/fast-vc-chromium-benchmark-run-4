12634f8a44a8007353aac177b5d5393086280924
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-09 09:24:40 +0000

    2009-12-09  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            [bzt] Convert Build to use Sequence
            https://bugs.webkit.org/show_bug.cgi?id=32310
    
            So much prettier.
    
            * Scripts/modules/buildsteps.py:
            * Scripts/modules/commands/download.py:
            * Scripts/modules/landingsequence.py:
            * Scripts/modules/stepsequence.py: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51893 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/buildsteps.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/buildsteps.py
@@ -51,9 +51,10 @@ class CommandOptions(object):
 
 
 class AbstractStep(object):
-    def __init__(self, tool, options):
+    def __init__(self, tool, options, patch=None):
         self._tool = tool
         self._options = options
+        self._patch = patch
         self._port = None
 
     def _run_script(self, script_name, quiet=False, port=WebKitPort):
@@ -75,20 +76,14 @@ class AbstractStep(object):
         raise NotImplementedError, "subclasses must implement"
 
 
-class AbstractPatchStep(AbstractStep):
-    def __init__(self, tool, options, patch):
-        AbstractStep.__init__(self, tool, options)
-        self._patch = patch
-
-
 class PrepareChangelogStep(AbstractStep):
     def run(self):
         self._run_script("prepare-ChangeLog")
 
 
 class CleanWorkingDirectoryStep(AbstractStep):
-    def __init__(self, tool, options, allow_local_commits=False):
-        AbstractStep.__init__(self, tool, options)
+    def __init__(self, tool, options, patch=None, allow_local_commits=False):
+        AbstractStep.__init__(self, tool, options, patch)
         self._allow_local_commits = allow_local_commits
 
     @classmethod
@@ -99,7 +94,7 @@ class CleanWorkingDirectoryStep(AbstractStep):
         ]
 
     def run(self):
-        os.chdir(self._tool._scm.checkout_root)
+        os.chdir(self._tool.scm().checkout_root)
         if not self._allow_local_commits:
             self._tool.scm().ensure_no_local_commits(self._options.force_clean)
         if self._options.clean:
@@ -121,7 +116,7 @@ class UpdateStep(AbstractStep):
         self._tool.executive.run_and_throw_if_fail(self.port().update_webkit_command())
 
 
-class ApplyPatchStep(AbstractPatchStep):
+class ApplyPatchStep(AbstractStep):
     @classmethod
     def options(cls):
         return [
@@ -198,13 +193,13 @@ class CommitStep(AbstractStep):
         return self._tool.scm().commit_with_message(commit_message.message())
 
 
-class ClosePatchStep(AbstractPatchStep):
+class ClosePatchStep(AbstractStep):
     def run(self, commit_log):
         comment_text = bug_comment_from_commit_text(self._tool.scm(), commit_log)
         self._tool.bugs.clear_attachment_flags(self._patch["id"], comment_text)
 
 
-class CloseBugStep(AbstractPatchStep):
+class CloseBugStep(AbstractStep):
     @classmethod
     def options(cls):
         return [
