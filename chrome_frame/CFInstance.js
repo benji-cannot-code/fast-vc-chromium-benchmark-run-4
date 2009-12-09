2d220bcaafbeb5b9a3749f79559d627330ac7e69
slightlyoff@chromium.org <slightlyoff@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-09 01:06:18 +0000

    Fixes CFInstance to use a longer Deferred standard timeout. Short timeouts caused cancellation of listeners and therefore backlogged RPC callback failure. The API requires that we use a timeout, but upping the default to 10s should accomidate nearly all CF startup delays.
    BUG=None
    TEST=run all the CFInstance tests in a Debug build.
    TBR=ananta
    
    Review URL: http://codereview.chromium.org/464077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34116 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/CFInstance.js
+++ chrome_frame/CFInstance.js
@@ -602,7 +602,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
     this._timer = setTimeout(
                     hitch(this, 'errback', new Error('timeout')), 
-                    (timeout || 1000)
+                    (timeout || 10000)
                   );
     this.silentlyCancelled = false;
   };
@@ -782,7 +782,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     var fired = this.fired;
     var res = this.results[fired];
     var cb = null;
-    while ((chain.length > 0) && (this.paused == 0)) {
+    while ((chain.length) && (!this.paused)) {
       var f = chain.shift()[fired];
       if (!f) {
         continue;
@@ -1197,8 +1197,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   RPC.prototype._postMessageBacklog = function() {
     if (this._open) {
-      forEach(this._msgBacklog, this._postMessage, this);
-      this._msgBacklog = [];
+      // forEach(this._msgBacklog, this._postMessage, this);
+      // this._msgBacklog = [];
+      while (this._msgBacklog.length) {
+        var msg = this._msgBacklog.shift();
+        this._postMessage(msg);
+      }
     }
   };
 
@@ -1211,16 +1215,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   };
 
-  // currently no-ops. We may need them in the future
-  // RPC.prototype._doWithAck_load = function() { };
-  // RPC.prototype._doWithAck_init = function() { };
-
   RPC.prototype._doWithAck = function(what) {
-    var f = this['_doWithAck_' + what];
-    if (f) {
-      f.call(this);
-    }
-
     this._postMessage('doWithAckCallback:' + what, what == 'load');
   };
 
