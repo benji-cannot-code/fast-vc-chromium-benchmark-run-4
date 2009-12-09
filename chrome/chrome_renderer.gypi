a53e5d8c57c302300ca7e2a63274f7776e307569
sehr@google.com <sehr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-09 17:19:22 +0000

    Enable Pepper support by default, including building the test plugin. This is needed because the NaCl plugin code that runs in the renderer needs to use Pepper APIs all the time, and NaCl support has been enabled by default for several months now.  To cause an untrusted Pepper plugin to run in the renderer one needs to specify the --internal-pepper flag. I have also removed the enable_pepper flag from gyp.  As the build of the GPU process was tied to this flag, I have renamed the flag to enable_gpu. TEST=none BUG=none
    Review URL: http://codereview.chromium.org/464074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34161 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_renderer.gypi
+++ chrome/chrome_renderer.gypi
@@ -149,7 +149,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         ],
       },
       'conditions': [
-        ['enable_pepper==1', {
+        ['enable_gpu==1', {
           'dependencies': [
             '../gpu/gpu.gyp:gpu_plugin',
           ],
--- webkit/glue/plugins/plugin_host.cc
+++ webkit/glue/plugins/plugin_host.cc
@@ -3,9 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Pepper API support should be turned on for this module.
-#define PEPPER_APIS_ENABLED
-
 #include "webkit/glue/plugins/plugin_host.h"
 
 #include "base/file_util.h"
@@ -803,9 +800,16 @@ NPError NPN_GetValue(NPP id, NPNVariable variable, void* value) {
       break;
     }
   #endif  // OS_MACOSX
-    case NPNVPepperExtensions:
-      rv = NPAPI::GetPepperExtensionsFunctions(value);
+    case NPNVPepperExtensions: {
+      scoped_refptr<NPAPI::PluginInstance> plugin = FindInstance(id);
+      if (plugin->webplugin()->delegate() != NULL) {
+        // Delegate is only set for in-renderer Pepper plugins.
+        rv = NPAPI::GetPepperExtensionsFunctions(value);
+      } else {
+        rv = NPERR_GENERIC_ERROR;
+      }
       break;
+    }
     default:
       DLOG(INFO) << "NPN_GetValue(" << variable << ") is not implemented yet.";
       break;
