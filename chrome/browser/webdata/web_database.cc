2a00df3a681d5ab433eed5af64e472cc03f9c248
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-09 21:43:02 +0000

    linux: remove a NOTIMPL since we have a bug on it
    Also, update some comments to use the new bug number.
    
    BUG=8205,25404
    
    Review URL: http://codereview.chromium.org/483003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34193 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/web_database.cc
+++ chrome/browser/webdata/web_database.cc
@@ -20,7 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // bottleneck to use the platform methods (e.g. Keychain on the Mac, Gnome
 // Keyring / KWallet on Linux). That's going to take a massive change in its
 // API... see:
-// http://code.google.com/p/chromium/issues/detail?id=8205 (Linux)
+//   http://code.google.com/p/chromium/issues/detail?id=25404 (Linux)
+// but the (possibly-now-unused) Mac encryptor stub code needs to die too.
 #include "chrome/browser/password_manager/encryptor.h"
 
 using webkit_glue::FormField;
