99514e19c38feac37dd2e0e57f3c84318ec20fff
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-10 02:18:29 +0000

    Add views/examples/slider_example.h
    The example demonstrates how to use the Slider class.
    
    BUG=none
    TEST=manually
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=34142
    
    Review URL: http://codereview.chromium.org/466055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34223 0039d316-1c4b-4281-b951-d872f2087c98

--- views/examples/examples_main.cc
+++ views/examples/examples_main.cc
@@ -17,9 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/examples/message_box_example.h"
 #include "views/examples/radio_button_example.h"
 #include "views/examples/scroll_view_example.h"
+// Slider is not yet ported to Windows.
+#if defined(OS_LINUX)
+#include "views/examples/slider_example.h"
+#endif
 #include "views/examples/tabbed_pane_example.h"
 #if defined(OS_WIN)
-// TableView and TableView2 are not yet ported to Linux.
+// TableView is not yet ported to Linux.
 #include "views/examples/table_example.h"
 #endif
 #include "views/examples/table2_example.h"
@@ -80,7 +84,7 @@ void ExamplesMain::Run() {
   layout->AddView(status_label_);
 
   views::Window* window =
-      views::Window::CreateChromeWindow(NULL, gfx::Rect(0, 0, 700, 300), this);
+      views::Window::CreateChromeWindow(NULL, gfx::Rect(0, 0, 750, 300), this);
 
   examples::TextfieldExample textfield_example(this);
   tabbed_pane->AddTab(textfield_example.GetExampleTitle(),
@@ -123,6 +127,12 @@ void ExamplesMain::Run() {
   examples::WidgetExample widget_example(this);
   tabbed_pane->AddTab(widget_example.GetExampleTitle(),
                       widget_example.GetExampleView());
+
+#if defined(OS_LINUX)
+  examples::SliderExample slider_example(this);
+  tabbed_pane->AddTab(slider_example.GetExampleTitle(),
+                      slider_example.GetExampleView());
+#endif
 
   window->Show();
   views::AcceleratorHandler accelerator_handler;
