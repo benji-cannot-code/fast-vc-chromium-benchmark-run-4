973e01f194d41714a7ac9a1e4d8f0844290502d2
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-10 21:54:34 +0000

    2009-12-10  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: debugger shortcuts don't work when
            Search field or Console drawer has focus.
    
            https://bugs.webkit.org/show_bug.cgi?id=32392
    
            * inspector/front-end/inspector.js:
            (WebInspector.loaded):
            (WebInspector.documentKeyDown):
            (WebInspector.documentKeyUp):
            (WebInspector.searchKeyDown):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51961 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/KeyboardShortcut.js
+++ third_party/WebKit/WebCore/inspector/front-end/KeyboardShortcut.js
@@ -45,6 +45,7 @@ WebInspector.KeyboardShortcut.Modifiers = {
 };
 
 WebInspector.KeyboardShortcut.KeyCodes = {
+    Backspace: 8,
     Esc: 27,
     Space: 32,
     PageUp: 33,      // also NUM_NORTH_EAST
@@ -55,6 +56,7 @@ WebInspector.KeyboardShortcut.KeyCodes = {
     Up: 38,          // also NUM_NORTH
     Right: 39,       // also NUM_EAST
     Down: 40,        // also NUM_SOUTH
+    Delete: 46,
     F1: 112,
     F2: 113,
     F3: 114,
