7aefa33d47cb2745462b5ba928edc2208d4181a3
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-10 03:32:55 +0000

    Reviewed by Darin Adler.
            https://bugs.webkit.org/show_bug.cgi?id=32332
            WebSocket events should be dispatched synchronously
    
            Updated websocket/tests/simple to test for the new behavior.
    
            When Web Sockets API says that events should be queued for async dispatch, it means something
            different. We should keep this in mind when dealing with other HTML5-related specs.
    
            The model for HTML5 is that code running in response to network events (e.g. WebSocket or
            XMLHttpRequest algorithms) runs in a separate thread of execution, and thus needs to post
            async events as its only way to communicate with client code. As long as network events are
            queued themselves (as they are in WebKit), there is no need to queue JS events for async
            dispatch.
    
            * websockets/WebSocket.cpp:
            (WebCore::WebSocket::didConnect):
            (WebCore::WebSocket::didReceiveMessage):
            (WebCore::WebSocket::didClose):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51935 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/websockets/WebSocket.cpp
+++ third_party/WebKit/WebCore/websockets/WebSocket.cpp
@@ -49,29 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class ProcessWebSocketEventTask : public ScriptExecutionContext::Task {
-public:
-    typedef void (WebSocket::*Method)(Event*);
-    static PassOwnPtr<ProcessWebSocketEventTask> create(PassRefPtr<WebSocket> webSocket, PassRefPtr<Event> event)
-    {
-        return new ProcessWebSocketEventTask(webSocket, event);
-    }
-    virtual void performTask(ScriptExecutionContext*)
-    {
-        ExceptionCode ec = 0;
-        m_webSocket->dispatchEvent(m_event.get(), ec);
-        ASSERT(!ec);
-    }
-
-  private:
-    ProcessWebSocketEventTask(PassRefPtr<WebSocket> webSocket, PassRefPtr<Event> event)
-        : m_webSocket(webSocket)
-        , m_event(event) { }
-
-    RefPtr<WebSocket> m_webSocket;
-    RefPtr<Event> m_event;
-};
-
 static bool isValidProtocolString(const WebCore::String& protocol)
 {
     if (protocol.isNull())
@@ -207,7 +184,7 @@ void WebSocket::didConnect()
         return;
     }
     m_state = OPEN;
-    scriptExecutionContext()->postTask(ProcessWebSocketEventTask::create(this, Event::create(eventNames().openEvent, false, false)));
+    dispatchEvent(Event::create(eventNames().openEvent, false, false));
 }
 
 void WebSocket::didReceiveMessage(const String& msg)
@@ -218,15 +195,14 @@ void WebSocket::didReceiveMessage(const String& msg)
     RefPtr<MessageEvent> evt = MessageEvent::create();
     // FIXME: origin, lastEventId, source, messagePort.
     evt->initMessageEvent(eventNames().messageEvent, false, false, SerializedScriptValue::create(msg), "", "", 0, 0);
-    scriptExecutionContext()->postTask(ProcessWebSocketEventTask::create(this, evt));
+    dispatchEvent(evt);
 }
 
 void WebSocket::didClose()
 {
     LOG(Network, "WebSocket %p didClose", this);
     m_state = CLOSED;
-    if (scriptExecutionContext())
-        scriptExecutionContext()->postTask(ProcessWebSocketEventTask::create(this, Event::create(eventNames().closeEvent, false, false)));
+    dispatchEvent(Event::create(eventNames().closeEvent, false, false));
 }
 
 EventTargetData* WebSocket::eventTargetData()
