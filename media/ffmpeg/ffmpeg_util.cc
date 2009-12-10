91d27b8216e3c0ea66ece21f89f47466d7411b2f
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-10 00:05:39 +0000

    Refactor FFmpegVideoDecoder to try and generalize code common to all video decoders.
    This changes the DecoderBase API to be fully asynchronous when invoking its subclass's actions.
    
    Review URL: http://codereview.chromium.org/465044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34208 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/ffmpeg/ffmpeg_util.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.  Use of this
+// source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
+
+#include "media/ffmpeg/ffmpeg_util.h"
+
+#include "media/filters/ffmpeg_common.h"
+
+namespace media {
+
+static const AVRational kMicrosBase = { 1, base::Time::kMicrosecondsPerSecond };
+
+base::TimeDelta ConvertTimestamp(const AVRational& time_base, int64 timestamp) {
+  int64 microseconds = av_rescale_q(timestamp, time_base, kMicrosBase);
+  return base::TimeDelta::FromMicroseconds(microseconds);
+}
+
+}  // namespace media
