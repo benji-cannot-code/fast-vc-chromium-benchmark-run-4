df7e5e60ffd7b2d1f0ddc8d6767486063f22dc2d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-10 21:17:00 +0000

    2009-12-09  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            bugzilla-tool needs a command to list patches needing cq+
            https://bugs.webkit.org/show_bug.cgi?id=32351
    
            * Scripts/modules/bugzilla.py:
             - Parse attacher_email from attachment xml.
            * Scripts/modules/bugzilla_unittest.py:
             - Test new attacher_email parsing.
            * Scripts/modules/commands/queries.py:
             - Add PatchesToCommitQueue
            * Scripts/modules/commands/queries_unittest.py:
             - Tests for PatchesToCommitQueue
            * Scripts/modules/mock_bugzillatool.py:
             - Add necessary mock methods for running PatchesToCommitQueue
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51959 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/commands/queries_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/commands/queries_unittest.py
@@ -29,8 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
+from modules.bugzilla import Bugzilla
 from modules.commands.commandtest import CommandsTest
 from modules.commands.queries import *
+from modules.mock import Mock
+from modules.mock_bugzillatool import MockBugzillaTool
 
 class QueryCommandsTest(CommandsTest):
     def test_bugs_to_commit(self):
@@ -40,6 +43,17 @@ class QueryCommandsTest(CommandsTest):
         expected_stdout = "http://example.com/197\nhttp://example.com/128\n"
         expected_stderr = "Patches in commit queue:\n"
         self.assert_execute_outputs(PatchesToCommit(), None, expected_stdout, expected_stderr)
+
+    def test_patches_to_commit_queue(self):
+        expected_stdout = "http://example.com/197&action=edit\nhttp://example.com/128&action=edit\n"
+        expected_stderr = ""
+        options = Mock()
+        options.bugs = False
+        self.assert_execute_outputs(PatchesToCommitQueue(), None, expected_stdout, expected_stderr, options=options)
+
+        expected_stdout = "http://example.com/42\n"
+        options.bugs = True
+        self.assert_execute_outputs(PatchesToCommitQueue(), None, expected_stdout, expected_stderr, options=options)
 
     def test_patches_to_review(self):
         expected_stdout = "197\n128\n"
