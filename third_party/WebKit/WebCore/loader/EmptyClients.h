91e2afcec86f8748fbdf55c9e40491e9b79ffa8b
zoltan@webkit.org <zoltan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-11 16:19:36 +0000

    2009-12-11  Zoltan Horvath  <zoltan@webkit.org>
            Reviewed by Darin Adler.
    
            Allow custom memory allocation control for WebCore's EmptyFrameLoaderClient
            https://bugs.webkit.org/show_bug.cgi?id=32110
    
            Inherits the following class from Noncopyable because it is
            instantiated by 'new' and no need to be copyable:
    
            class name              - instantiated at: WebCore/'location'
    
            class EmptyFrameLoaderClient - svg/graphics/SVGImage.cpp:229
    
            * loader/FrameLoaderClient.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51990 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/EmptyClients.h
+++ third_party/WebKit/WebCore/loader/EmptyClients.h
@@ -168,7 +168,7 @@ public:
 #endif
 };
 
-class EmptyFrameLoaderClient : public FrameLoaderClient {
+class EmptyFrameLoaderClient : public FrameLoaderClient, public Noncopyable {
 public:
     virtual ~EmptyFrameLoaderClient() {  }
     virtual void frameLoaderDestroyed() { }
