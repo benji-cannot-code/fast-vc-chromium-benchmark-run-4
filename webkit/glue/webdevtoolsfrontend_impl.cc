3482fc41757894af36a9f8532396266c22aa8cf0
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-11 16:35:32 +0000

    DevTools: comment menu-related code out while landing breaking changes upstream. Review URL: http://codereview.chromium.org/490017
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34349 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webdevtoolsfrontend_impl.cc
+++ webkit/glue/webdevtoolsfrontend_impl.cc
@@ -137,7 +137,7 @@ WebDevToolsFrontendImpl::WebDevToolsFrontendImpl(
       client_(client),
       application_locale_(application_locale),
       loaded_(false) {
-  menu_selection_handler_ = MenuSelectionHandler::create(this);
+  // menu_selection_handler_ = MenuSelectionHandler::create(this);
 
   WebFrameImpl* frame = web_view_impl_->mainFrameImpl();
   v8::HandleScope scope;
@@ -217,7 +217,7 @@ WebDevToolsFrontendImpl::WebDevToolsFrontendImpl(
 }
 
 WebDevToolsFrontendImpl::~WebDevToolsFrontendImpl() {
-  menu_selection_handler_->disconnect();
+  // menu_selection_handler_->disconnect();
 }
 
 // TODO(yurys): this method is deprecated and will go away soon, use
@@ -502,8 +502,9 @@ v8::Handle<v8::Value> WebDevToolsFrontendImpl::JsWindowUnloading(
 
 // static
 v8::Handle<v8::Value> WebDevToolsFrontendImpl::JsShowContextMenu(
-    const v8::Arguments& args) {
-
+    const v8::Arguments&) {
+/*
+  TODO: Uncomment once new ContextMenu API lands upstream.
   if (args.Length() < 2)
     return v8::Undefined();
 
@@ -544,5 +545,6 @@ v8::Handle<v8::Value> WebDevToolsFrontendImpl::JsShowContextMenu(
   menu_controller->showContextMenu(event,
                                    items,
                                    frontend->menu_selection_handler_);
+*/
   return v8::Undefined();
 }
