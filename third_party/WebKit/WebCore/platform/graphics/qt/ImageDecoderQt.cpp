93a2c4a33a296627a0124355b5727c95151f7a5d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-11 14:38:31 +0000

    2009-12-11  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Simon Hausmann.
    
            [Qt] ImageDecoderQt: Fix retrieval of image file extensions
    
            QImageReader only allows retrieving the format() before actually reading the image.
    
            https://bugs.webkit.org/show_bug.cgi?id=32428
    
            * platform/graphics/qt/ImageDecoderQt.cpp:
            (WebCore::ImageDecoderQt::setData):
            (WebCore::ImageDecoderQt::internalDecodeSize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@51983 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/ImageDecoderQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/ImageDecoderQt.cpp
@@ -82,6 +82,9 @@ void ImageDecoderQt::setData(SharedBuffer* data, bool allDataReceived)
     m_buffer->setData(imageData);
     m_buffer->open(QBuffer::ReadOnly);
     m_reader = new QImageReader(m_buffer, m_format);
+
+    // QImageReader only allows retrieving the format before reading the image
+    m_format = m_reader->format();
 }
 
 bool ImageDecoderQt::isSizeAvailable()
@@ -159,7 +162,6 @@ void ImageDecoderQt::internalDecodeSize()
     if (size.isEmpty())
         return failRead();
 
-    m_format = m_reader->format();
     setSize(size.width(), size.height());
 }
 
