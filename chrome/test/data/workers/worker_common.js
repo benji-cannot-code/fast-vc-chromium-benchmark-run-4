2522ffdb9cefc92fab703ca0bd6c231cc26e29aa
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-12 17:39:23 +0000

    Enabled a number of worker tests on various platforms. Added new SingleSharedWorker and MultipleSharedWorker tests, and enabled a bunch of other tests after marking them flaky/excluding them from valgrind.
    Review URL: http://codereview.chromium.org/490023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34429 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/workers/worker_common.js
+++ chrome/test/data/workers/worker_common.js
@@ -1,4 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (!self.postMessage) {
+  // This is a shared worker - mimic dedicated worker APIs
+  onconnect = function(event) {
+    event.ports[0].onmessage = function(e) {
+      self.onmessage(e);
+    };
+    self.postMessage = function(msg, ports) {
+      event.ports[0].postMessage(msg, ports);
+    };
+  };
+}
 onmessage = function(evt) {
   if (evt.data == "ping")
     postMessage("pong");
