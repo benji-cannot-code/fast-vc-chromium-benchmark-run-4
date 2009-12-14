db840a71aaf54dac07fb6b8bdd1630aa4d100388
hayato@chromium.org <hayato@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-14 03:40:55 +0000

    Use namespace in mac_util_unittest.
    BUG=None
    TEST=MacUtilTest in base_unittest
    
    Review URL: http://codereview.chromium.org/491043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34455 0039d316-1c4b-4281-b951-d872f2087c98

--- base/mac_util_unittest.mm
+++ base/mac_util_unittest.mm
@@ -16,18 +16,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
+namespace mac_util {
+
+namespace {
+
 typedef PlatformTest MacUtilTest;
 
 TEST_F(MacUtilTest, TestFSRef) {
   FSRef ref;
   std::string path("/System/Library");
 
-  ASSERT_TRUE(mac_util::FSRefFromPath(path, &ref));
-  EXPECT_EQ(path, mac_util::PathFromFSRef(ref));
+  ASSERT_TRUE(FSRefFromPath(path, &ref));
+  EXPECT_EQ(path, PathFromFSRef(ref));
 }
 
 TEST_F(MacUtilTest, TestLibraryPath) {
-  FilePath library_dir = mac_util::GetUserLibraryPath();
+  FilePath library_dir = GetUserLibraryPath();
   // Make sure the string isn't empty.
   EXPECT_FALSE(library_dir.value().empty());
 }
@@ -46,7 +50,7 @@ TEST_F(MacUtilTest, TestGrabWindowSnapshot) {
 
   scoped_ptr<std::vector<unsigned char> > png_representation(
       new std::vector<unsigned char>);
-  mac_util::GrabWindowSnapshot(window, png_representation.get());
+  GrabWindowSnapshot(window, png_representation.get());
 
   // Copy png back into NSData object so we can make sure we grabbed a png.
   scoped_nsobject<NSData> image_data(
@@ -65,7 +69,7 @@ TEST_F(MacUtilTest, TestGetAppBundlePath) {
   FilePath out;
 
   // Make sure it doesn't crash.
-  out = mac_util::GetAppBundlePath(FilePath());
+  out = GetAppBundlePath(FilePath());
   EXPECT_TRUE(out.empty());
 
   // Some more invalid inputs.
@@ -74,7 +78,7 @@ TEST_F(MacUtilTest, TestGetAppBundlePath) {
     "foo/bar./bazquux", "foo/.app", "//foo",
   };
   for (size_t i = 0; i < arraysize(invalid_inputs); i++) {
-    out = mac_util::GetAppBundlePath(FilePath(invalid_inputs[i]));
+    out = GetAppBundlePath(FilePath(invalid_inputs[i]));
     EXPECT_TRUE(out.empty()) << "loop: " << i;
   }
 
@@ -98,7 +102,7 @@ TEST_F(MacUtilTest, TestGetAppBundlePath) {
         "/Applications/Google Foo.app" },
   };
   for (size_t i = 0; i < ARRAYSIZE_UNSAFE(valid_inputs); i++) {
-    out = mac_util::GetAppBundlePath(FilePath(valid_inputs[i].in));
+    out = GetAppBundlePath(FilePath(valid_inputs[i].in));
     EXPECT_FALSE(out.empty()) << "loop: " << i;
     EXPECT_STREQ(valid_inputs[i].expected_out,
         out.value().c_str()) << "loop: " << i;
@@ -116,15 +120,15 @@ TEST_F(MacUtilTest, TestExcludeFileFromBackups) {
   // set its exclusion property.
   NSURL* fileURL = [NSURL URLWithString:dummyFilePath];
   // Reset the exclusion in case it was set previously.
-  mac_util::SetFileBackupExclusion(file_path, false);
+  SetFileBackupExclusion(file_path, false);
   Boolean excludeByPath;
   // Initial state should be non-excluded.
   EXPECT_FALSE(CSBackupIsItemExcluded((CFURLRef)fileURL, &excludeByPath));
   // Exclude the file.
-  EXPECT_TRUE(mac_util::SetFileBackupExclusion(file_path, true));
+  EXPECT_TRUE(SetFileBackupExclusion(file_path, true));
   EXPECT_TRUE(CSBackupIsItemExcluded((CFURLRef)fileURL, &excludeByPath));
   // Un-exclude the file.
-  EXPECT_TRUE(mac_util::SetFileBackupExclusion(file_path, false));
+  EXPECT_TRUE(SetFileBackupExclusion(file_path, false));
   EXPECT_FALSE(CSBackupIsItemExcluded((CFURLRef)fileURL, &excludeByPath));
 }
 
@@ -136,10 +140,13 @@ TEST_F(MacUtilTest, TestGetValueFromDictionary) {
   CFDictionarySetValue(dict.get(), CFSTR("key"), CFSTR("value"));
 
   EXPECT_TRUE(CFEqual(CFSTR("value"),
-                      mac_util::GetValueFromDictionary(
+                      GetValueFromDictionary(
                           dict, CFSTR("key"), CFStringGetTypeID())));
-  EXPECT_FALSE(mac_util::GetValueFromDictionary(
-                   dict, CFSTR("key"), CFNumberGetTypeID()));
-  EXPECT_FALSE(mac_util::GetValueFromDictionary(
+  EXPECT_FALSE(GetValueFromDictionary(dict, CFSTR("key"), CFNumberGetTypeID()));
+  EXPECT_FALSE(GetValueFromDictionary(
                    dict, CFSTR("no-exist"), CFStringGetTypeID()));
 }
+
+}  // namespace
+
+}  // namespace mac_util
