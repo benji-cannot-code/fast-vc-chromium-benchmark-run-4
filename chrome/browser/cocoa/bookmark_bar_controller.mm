b1260c682d5416aac8bd6ed55cff5e95a49cfbb2
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-14 21:25:20 +0000

    Disable "open in incognito window" for incognito windows.
    BUG=http://crbug.com/24637
    TEST=as in bug
    
    Review URL: http://codereview.chromium.org/488018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34496 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bar_controller.mm
+++ chrome/browser/cocoa/bookmark_bar_controller.mm
@@ -670,6 +670,14 @@ const NSTimeInterval kBookmarkBarAnimationDuration = 0.12;
     }
   }
 
+  // If this is an incognito window, don't allow "open in incognito".
+  if ((action == @selector(openBookmarkInIncognitoWindow:)) ||
+      (action == @selector(openAllBookmarksIncognitoWindow:))) {
+    if (browser_->profile()->IsOffTheRecord()) {
+      return NO;
+    }
+  }
+
   // Enabled by default.
   return YES;
 }
