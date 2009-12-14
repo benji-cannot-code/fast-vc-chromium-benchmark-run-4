d266c6494fe8620ee811c7dee605b8f617a3e0a0
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-14 19:38:37 +0000

    Fix the build.
    * page/Geolocation.cpp:
    * page/Geolocation.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52104 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Geolocation.cpp
+++ third_party/WebKit/WebCore/page/Geolocation.cpp
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GeolocationController.h"
 #include "GeolocationError.h"
 #include "GeolocationPosition.h"
-#include "Geoposition.h"
 #include "PositionError.h"
 #endif
 
--- third_party/WebKit/WebCore/page/Geolocation.h
+++ third_party/WebKit/WebCore/page/Geolocation.h
@@ -28,16 +28,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define Geolocation_h
 
 #include "GeolocationService.h"
+#include "Geoposition.h"
 #include "PositionCallback.h"
 #include "PositionError.h"
 #include "PositionErrorCallback.h"
 #include "PositionOptions.h"
 #include "Timer.h"
-#include <wtf/Platform.h>
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
 #include <wtf/OwnPtr.h>
 #include <wtf/PassRefPtr.h>
+#include <wtf/Platform.h>
 #include <wtf/RefCounted.h>
 #include <wtf/RefPtr.h>
 #include <wtf/Vector.h>
@@ -45,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class Frame;
-class Geoposition;
 
 #if ENABLE(CLIENT_BASED_GEOLOCATION)
 class GeolocationPosition;
