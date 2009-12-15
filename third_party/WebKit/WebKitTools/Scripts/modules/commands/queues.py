059d52dbe8bd9b932bd35394784603053cbbde8e
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-15 05:21:33 +0000

    2009-12-14  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            [bzt] Kill LandingSequence
            https://bugs.webkit.org/show_bug.cgi?id=32464
    
            Removes LandingSequence in favor of StepSequence.  This required
            changing the Step API slightly to carry a general notion of state
            instead of carrying patches specifically.
    
            * Scripts/modules/buildsteps.py:
            * Scripts/modules/commands/download.py:
            * Scripts/modules/commands/queues.py:
            * Scripts/modules/landingsequence.py: Removed.
            * Scripts/modules/stepsequence.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52129 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/commands/queues.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/commands/queues.py
@@ -36,11 +36,11 @@ from optparse import make_option
 
 from modules.executive import ScriptError
 from modules.grammar import pluralize
-from modules.landingsequence import LandingSequence, LandingSequenceErrorHandler
 from modules.logging import error, log
 from modules.multicommandtool import Command
 from modules.patchcollection import PersistentPatchCollection, PersistentPatchCollectionDelegate
 from modules.statusbot import StatusBot
+from modules.stepsequence import StepSequenceErrorHandler
 from modules.workqueue import WorkQueue, WorkQueueDelegate
 
 class AbstractQueue(Command, WorkQueueDelegate):
@@ -105,7 +105,7 @@ class AbstractQueue(Command, WorkQueueDelegate):
         return work_queue.run()
 
 
-class CommitQueue(AbstractQueue, LandingSequenceErrorHandler):
+class CommitQueue(AbstractQueue, StepSequenceErrorHandler):
     name = "commit-queue"
     def __init__(self):
         AbstractQueue.__init__(self)
@@ -137,14 +137,14 @@ class CommitQueue(AbstractQueue, LandingSequenceErrorHandler):
     def handle_unexpected_error(self, patch, message):
         self.tool.bugs.reject_patch_from_commit_queue(patch["id"], message)
 
-    # LandingSequenceErrorHandler methods
+    # StepSequenceErrorHandler methods
 
     @classmethod
-    def handle_script_error(cls, tool, patch, script_error):
-        tool.bugs.reject_patch_from_commit_queue(patch["id"], script_error.message_with_output())
+    def handle_script_error(cls, tool, state, script_error):
+        tool.bugs.reject_patch_from_commit_queue(state["patch"]["id"], script_error.message_with_output())
 
 
-class AbstractReviewQueue(AbstractQueue, PersistentPatchCollectionDelegate, LandingSequenceErrorHandler):
+class AbstractReviewQueue(AbstractQueue, PersistentPatchCollectionDelegate, StepSequenceErrorHandler):
     def __init__(self, options=None):
         AbstractQueue.__init__(self, options)
 
@@ -180,10 +180,10 @@ class AbstractReviewQueue(AbstractQueue, PersistentPatchCollectionDelegate, Land
     def handle_unexpected_error(self, patch, message):
         log(message)
 
-    # LandingSequenceErrorHandler methods
+    # StepSequenceErrorHandler methods
 
     @classmethod
-    def handle_script_error(cls, tool, patch, script_error):
+    def handle_script_error(cls, tool, state, script_error):
         log(script_error.message_with_output())
 
 
@@ -206,12 +206,12 @@ class StyleQueue(AbstractReviewQueue):
             raise e
 
     @classmethod
-    def handle_script_error(cls, tool, patch, script_error):
+    def handle_script_error(cls, tool, state, script_error):
         command = script_error.script_args
         if type(command) is list:
             command = command[0]
         # FIXME: We shouldn't need to use a regexp here.  ScriptError should
         #        have a better API.
         if re.search("check-webkit-style", command):
-            message = "Attachment %s did not pass %s:\n\n%s" % (patch["id"], cls.name, script_error.message_with_output(output_limit=5*1024))
-            tool.bugs.post_comment_to_bug(patch["bug_id"], message, cc=cls.watchers)
+            message = "Attachment %s did not pass %s:\n\n%s" % (state["patch"]["id"], cls.name, script_error.message_with_output(output_limit=5*1024))
+            tool.bugs.post_comment_to_bug(state["patch"]["bug_id"], message, cc=cls.watchers)
