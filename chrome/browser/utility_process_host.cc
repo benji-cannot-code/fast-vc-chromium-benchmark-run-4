1876831ce2ae9a9de58febc4926019b4bb3300a4
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-15 20:46:32 +0000

    Move the utility process messages to their own file now that we have more than 16 message types.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/460119
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34597 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/utility_process_host.cc
+++ chrome/browser/utility_process_host.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/common/render_messages.h"
+#include "chrome/common/utility_messages.h"
 #include "ipc/ipc_switches.h"
 
 UtilityProcessHost::UtilityProcessHost(ResourceDispatcherHost* rdh,
--- chrome/utility/utility_thread.cc
+++ chrome/utility/utility_thread.cc
@@ -10,8 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/child_process.h"
 #include "chrome/common/extensions/extension_unpacker.h"
 #include "chrome/common/extensions/update_manifest.h"
-#include "chrome/common/render_messages.h"
+#include "chrome/common/utility_messages.h"
 #include "chrome/common/web_resource/web_resource_unpacker.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 
 UtilityThread::UtilityThread() {
   ChildProcess::current()->AddRefProcess();
