38ea6d8b0574fefa43611b2dc1138cd279ec4b40
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-15 17:38:28 +0000

    GTK: Fix regression where we don't build tinted buttons in GTK theme mode.
    (Caused by my r34486)
    TEST=Use GTK+ mode. page/app menu button should have same tint as title bar.
    BUG=NONE
    
    Review URL: http://codereview.chromium.org/507002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34566 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_theme_provider.h
+++ chrome/browser/browser_theme_provider.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_BROWSER_THEME_PROVIDER_H_
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -153,6 +154,9 @@ class BrowserThemeProvider : public NonThreadSafe,
   // Returns true and sets |result| to the requested default property, if |id|
   // is valid.
   static bool GetDefaultDisplayProperty(int id, int* result);
+
+  // Returns the set of IDR_* resources that should be tinted.
+  static const std::set<int>& GetTintableToolbarButtons();
 
   // Save the images to be written to disk, mapping file path to id.
   typedef std::map<FilePath, int> ImagesDiskCache;
