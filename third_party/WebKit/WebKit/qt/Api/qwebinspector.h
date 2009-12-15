e0965d12d96a5aa0cf886542b34575138f1a8ef2
zecke@webkit.org <zecke@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-15 09:29:35 +0000

    [Qt] Do not disable the inspector on show and hide
    https://bugs.webkit.org/show_bug.cgi?id=31851
    
    On Qt/X11 with some window managers the window will be
    hidden when switching windows. In this case all the results
    are gone when coming back to the window.
    
    Attempt to use the CloseEvent to figure out if the window
    was closed and withdrawn as this is more friendly to the
    user of the inspector client.
    
    * Api/qwebinspector.cpp:
    (QWebInspector::hideEvent):
    (QWebInspector::closeEvent):
    * Api/qwebinspector.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52144 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebinspector.h
+++ third_party/WebKit/WebKit/qt/Api/qwebinspector.h
@@ -44,6 +44,7 @@ protected:
     void resizeEvent(QResizeEvent* event);
     void showEvent(QShowEvent* event);
     void hideEvent(QHideEvent* event);
+    void closeEvent(QCloseEvent* event);
 
 private:
     QWebInspectorPrivate* d;
