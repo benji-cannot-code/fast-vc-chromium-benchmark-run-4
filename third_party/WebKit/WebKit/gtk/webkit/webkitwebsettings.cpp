18d794404cdad04e81b4e790bd2461f91a7ff5ce
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-16 17:37:03 +0000

    Rubber-stamped by Xan Lopez.
    Add tab_key_cycles_through_elements to copies of
    WebKitWebSettings.
    
    * webkit/webkitwebsettings.cpp:
    (webkit_web_settings_copy):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52200 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebsettings.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebsettings.cpp
@@ -1097,6 +1097,7 @@ WebKitWebSettings* webkit_web_settings_copy(WebKitWebSettings* web_settings)
                  "editing-behavior", priv->editing_behavior,
                  "enable-universal-access-from-file-uris", priv->enable_universal_access_from_file_uris,
                  "enable-dom-paste", priv->enable_dom_paste,
+                 "tab-key-cycles-through-elements", priv->tab_key_cycles_through_elements,
                  "enable-default-context-menu", priv->enable_default_context_menu,
                  NULL));
 
