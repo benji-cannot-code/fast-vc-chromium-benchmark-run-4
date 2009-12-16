887010d4103b4040fd7b2ea637df590da2ba0968
ojan@chromium.org <ojan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-16 02:21:28 +0000

    Shard the editing tests. Now that the http tests have moved to the dbg-3 shard, this should shave ~40 seconds off the win/linux dbg-1 bots. Review URL: http://codereview.chromium.org/500042
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34641 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/layout_tests/run_webkit_tests.py
+++ webkit/tools/layout_tests/run_webkit_tests.py
@@ -160,7 +160,7 @@ class TestRunner:
       self._http_server = http_server.Lighttpd(options.results_directory)
 
     self._shardable_directories = ['chrome', 'LayoutTests', 'pending', 'fast',
-        'svg', 'loader']
+        'svg', 'loader', 'editing']
 
     # The http tests are very stable on mac/linux.
     # TODO(ojan): Make the http server on Windows be apache so we can turn
