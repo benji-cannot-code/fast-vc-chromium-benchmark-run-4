462f5045bc47101023ace64b7975e49ca64c329d
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-17 20:38:57 +0000

    Don't include all JSC headers everywhere
    https://bugs.webkit.org/show_bug.cgi?id=32663
    
    Patch by Benjamin Otte <otte@gnome.org> on 2009-12-17
    Reviewed by Adam Roben.
    
    Remove HashMap HashSet and OwnPtr includes from PlatformString.h.
    Reduces code size of Gtk build by another 5%.
    
    * platform/text/PlatformString.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/PlatformString.h
+++ third_party/WebKit/WebCore/platform/text/PlatformString.h
@@ -32,10 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <objc/objc.h>
 #endif
 
-#include <wtf/HashMap.h>
-#include <wtf/HashSet.h>
-#include <wtf/OwnPtr.h>
-
 #if PLATFORM(CF)
 typedef const struct __CFString * CFStringRef;
 #endif
