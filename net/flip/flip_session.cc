1fae6041c74d1df1bce703c8856dd0f157deba92
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-17 21:14:03 +0000

    Increase read buffer size to 32KB for the flip session. This was artificially low for no real reason.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/505028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34873 0039d316-1c4b-4281-b951-d872f2087c98

--- net/flip/flip_session.cc
+++ net/flip/flip_session.cc
@@ -53,7 +53,7 @@ namespace net {
 
 namespace {
 
-const int kReadBufferSize = 4 * 1024;
+const int kReadBufferSize = 32 * 1024;
 
 // Convert a FlipHeaderBlock into an HttpResponseInfo.
 // |headers| input parameter with the FlipHeaderBlock.
