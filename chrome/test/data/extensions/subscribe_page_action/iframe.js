66bf4aba7f98392281daa78e75b8b9d242e5c249
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-17 22:18:14 +0000

    Switch to using sendRequest instead of postMessage. Fix a bug causing the page action onClick handler not to work (only affects certain Linux builds that don't have PageAction popup support, all other builds use the Popup). Added copyright header.
    BUG=None 
    TEST=None 
    
    Review URL: http://codereview.chromium.org/500024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34883 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/subscribe_page_action/iframe.js
+++ chrome/test/data/extensions/subscribe_page_action/iframe.js
@@ -1,4 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* Copyright (c) 2009 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+
 /* Use only multi-line comments in this file, since during testing
    its contents will get read from disk and stuffed into the
    iframe .src tag, which is a process that doesn't preserve line
