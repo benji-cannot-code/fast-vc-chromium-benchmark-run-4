a0c190a7582968d5d7714379e38812e14558a65b
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-17 07:52:52 +0000

    2009-12-16  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            [bzt] Make UpdateStep quiet
            https://bugs.webkit.org/show_bug.cgi?id=32599
    
            * Scripts/modules/buildsteps.py:
            * Scripts/modules/buildsteps_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/buildsteps_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/buildsteps_unittest.py
@@ -29,9 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
-from modules.buildsteps import UpdateChangelogsWithReviewerStep
+from modules.buildsteps import UpdateChangelogsWithReviewerStep, UpdateStep
 from modules.mock_bugzillatool import MockBugzillaTool
 from modules.outputcapture import OutputCapture
+from modules.mock import Mock
+
 
 class UpdateChangelogsWithReviewerStepTest(unittest.TestCase):
     def test_guess_reviewer_from_bug(self):
@@ -39,3 +41,15 @@ class UpdateChangelogsWithReviewerStepTest(unittest.TestCase):
         step = UpdateChangelogsWithReviewerStep(MockBugzillaTool(), [])
         expected_stderr = "0 reviewed patches on bug 1, cannot infer reviewer.\n"
         capture.assert_outputs(self, step._guess_reviewer_from_bug, [1], expected_stderr=expected_stderr)
+
+
+class StepsTest(unittest.TestCase):
+    def _run_step(self, step, options, state=None):
+        if not state:
+            state = {}
+        step(MockBugzillaTool(), options).run(state)
+
+    def test_update_step(self):
+        options = Mock()
+        options.update = True
+        self._run_step(UpdateStep, options)
