188cfdf920ebcb8fec8bf66f1b88c90a848dce82
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-17 20:32:45 +0000

    Mac build fix
    * WebView/WebVideoFullscreenController.mm: Added now-needed #import.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52277 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebVideoFullscreenController.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebVideoFullscreenController.mm
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebCore/HTMLMediaElement.h>
 #import <WebCore/SoftLinking.h>
 #import <objc/objc-runtime.h>
+#import <wtf/UnusedParam.h>
 
 SOFT_LINK_FRAMEWORK(QTKit)
 SOFT_LINK_CLASS(QTKit, QTMovieView)
