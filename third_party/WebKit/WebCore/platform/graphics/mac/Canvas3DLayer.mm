fc9e20f8f0207f8334527c11e67899b63deda1e6
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-17 20:22:52 +0000

    Mac/Qt build fixes
    * platform/graphics/mac/Canvas3DLayer.mm:
    * platform/graphics/mac/GraphicsContext3DMac.cpp:
    * platform/qt/Localizations.cpp:
    Added missing #includes.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52276 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/Canvas3DLayer.mm
+++ third_party/WebKit/WebCore/platform/graphics/mac/Canvas3DLayer.mm
@@ -34,8 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "GraphicsLayer.h"
 #import <QuartzCore/QuartzCore.h>
 #import <OpenGL/OpenGL.h>
+#import <wtf/FastMalloc.h>
 #import <wtf/RetainPtr.h>
-#include <wtf/FastMalloc.h>
+#import <wtf/UnusedParam.h>
 
 using namespace WebCore;
 
--- third_party/WebKit/WebCore/platform/graphics/mac/GraphicsContext3DMac.cpp
+++ third_party/WebKit/WebCore/platform/graphics/mac/GraphicsContext3DMac.cpp
@@ -49,9 +49,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ImageBuffer.h"
 #include "NotImplemented.h"
 #include "WebKitCSSMatrix.h"
-
 #include <CoreGraphics/CGBitmapContext.h>
 #include <OpenGL/CGLRenderers.h>
+#include <wtf/UnusedParam.h>
 
 namespace WebCore {
 
