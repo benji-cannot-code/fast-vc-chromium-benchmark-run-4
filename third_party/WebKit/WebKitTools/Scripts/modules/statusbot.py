d19190756cea258497db6594545e5195887991ec
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-17 00:40:10 +0000

    2009-12-16  Eric Seidel  <eric@webkit.org>
            Reviewed by David Levin.
    
            bugzilla-tool should not require users to install mechanize
            https://bugs.webkit.org/show_bug.cgi?id=32635
    
            Centralize our import logic.
    
            * Scripts/modules/bugzilla.py: use webkit_mechanize
            * Scripts/modules/statusbot.py: use webkit_mechanize
            * Scripts/modules/webkit_mechanize.py: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/statusbot.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/statusbot.py
@@ -30,24 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # WebKit's Python module for interacting with the Commit Queue status page.
 
 from modules.logging import log
+from modules.webkit_mechanize import Browser
 
 # WebKit includes a built copy of BeautifulSoup in Scripts/modules
 # so this import should always succeed.
 from .BeautifulSoup import BeautifulSoup
-
-try:
-    from mechanize import Browser
-except ImportError, e:
-    print """
-mechanize is required.
-
-To install:
-sudo easy_install mechanize
-
-Or from the web:
-http://wwwsearch.sourceforge.net/mechanize/
-"""
-    exit(1)
 
 import urllib2
 
