0ca7ecdafbe5042e86edffd423f4293935b45c84
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-17 06:19:05 +0000

    2009-12-16  Fumitoshi Ukai  <ukai@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            Fix flakiness in WebSocket handshake console message.
            https://bugs.webkit.org/show_bug.cgi?id=32598
    
            In chromium, LayoutTests/websocket/tests/handshake-error.html is
            flaky, because it outputs the following console message several times:
              CONSOLE MESSAGE: line 0: Unexpected response code:101
            Update m_mode in WebSocketHandshake correctly: Once it finds
            error in handshake message, set m_mode to Failed and don't try
            reading handshake message again.  If handshake message is not yet
            fully received, set m_mode to Incomplete, so next didReceiveData()
            will try to check handshake message again.
    
            * websockets/WebSocketChannel.cpp:
            (WebCore::WebSocketChannel::didReceiveData):
            * websockets/WebSocketHandshake.cpp:
            (WebCore::WebSocketHandshake::readServerHandshake):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52234 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/websockets/WebSocketChannel.cpp
+++ third_party/WebKit/WebCore/websockets/WebSocketChannel.cpp
@@ -151,7 +151,7 @@ void WebSocketChannel::didReceiveData(SocketStreamHandle* handle, const char* da
         handle->close();
         return;
     }
-    if (m_handshake.mode() != WebSocketHandshake::Connected) {
+    if (m_handshake.mode() == WebSocketHandshake::Incomplete) {
         int headerLength = m_handshake.readServerHandshake(m_buffer, m_bufferSize);
         if (headerLength <= 0)
             return;
@@ -180,6 +180,8 @@ void WebSocketChannel::didReceiveData(SocketStreamHandle* handle, const char* da
             return;
         LOG(Network, "remaining in read buf %ul", m_bufferSize);
     }
+    if (m_handshake.mode() != WebSocketHandshake::Connected)
+        return;
 
     const char* nextFrame = m_buffer;
     const char* p = m_buffer;
