51812b23c50ed383912f2f18df1bcedebae6eeeb
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-17 19:10:50 +0000

    Fix a crash in ChromeFrame caused by incorrectly instantiating the automation url request context used by the host network stack implementation with a NULL AutomationResourceMessageFilter pointer.
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=29025
    
    Will add a test for this in a subsequent CL.
    
    Bug=29025
    
    Review URL: http://codereview.chromium.org/505033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34855 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/external_tab_container.cc
+++ chrome/browser/external_tab_container.cc
@@ -188,6 +188,8 @@ bool ExternalTabContainer::Reinitialize(
   automation_resource_message_filter_ = filter;
 
   if (load_requests_via_automation_) {
+    InitializeAutomationRequestContext(tab_handle_);
+
     RenderViewHost* rvh = tab_contents_->render_view_host();
     if (rvh) {
       AutomationResourceMessageFilter::RegisterRenderView(
@@ -207,9 +209,6 @@ bool ExternalTabContainer::Reinitialize(
 
 void ExternalTabContainer::SetTabHandle(int handle) {
   tab_handle_ = handle;
-  if (load_requests_via_automation_) {
-    InitializeAutomationRequestContext(handle);
-  }
 }
 
 void ExternalTabContainer::ProcessUnhandledAccelerator(const MSG& msg) {
