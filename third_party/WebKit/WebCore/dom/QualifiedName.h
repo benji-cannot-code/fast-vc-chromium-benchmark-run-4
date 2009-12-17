290eaabd93d5eccc9f0e135c3c6591d13c876771
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-17 20:56:58 +0000

    Chromium build fix
    * dom/QualifiedName.h: Added now-needed #include of HashTraits.h
    (which itself #includes HashFunctions.h).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52280 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/QualifiedName.h
+++ third_party/WebKit/WebCore/dom/QualifiedName.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define QualifiedName_h
 
 #include "AtomicString.h"
-#include <wtf/HashFunctions.h>
+#include <wtf/HashTraits.h>
 
 namespace WebCore {
 
