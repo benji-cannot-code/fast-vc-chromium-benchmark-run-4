f2f6bcea8317bd2aac4166fd71a955410244b5cc
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-17 22:14:38 +0000

    2009-12-17  Alexander Pavlov  <apavlov@chromium.org>
            Reviewed by Eric Seidel.
    
            Disable memory cache client calls so that a new identifier isn't created
            for the same resource on reload.
    
            With the calls enabled, duplicate entries for cached resources are created
            in the Web Inspector. Safari for Windows disables the calls near the point of the
            Frame instance creation, and we just follow this for Chromium.
            https://bugs.webkit.org/show_bug.cgi?id=32618
    
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::WebViewImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
@@ -235,6 +235,7 @@ WebViewImpl::WebViewImpl(WebViewClient* client)
 
     m_page->backForwardList()->setClient(&m_backForwardListClientImpl);
     m_page->setGroupName(pageGroupName);
+    m_page->setMemoryCacheClientCallsEnabled(false);
 }
 
 WebViewImpl::~WebViewImpl()
