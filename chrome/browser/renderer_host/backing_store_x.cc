815c3a40a4814dbbcda23ae13f0305d1cefc1585
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-18 15:52:54 +0000

    Revert 34951 - Combine ViewHostMsg_{Paint,Scroll}Rect into one IPC.
    The combined IPC means that scrolling only requires one transport DIB instead
    of two.  Previously, we'd use one in the ScrollRect IPC to pass up the pixels
    for the exposed region, and then we'd use a second one in the PaintRect IPC to
    pass up the pixels for the updated scroll bar rendering.  Now all paints are
    done using a single transport DIB.
    
    Optimize RenderWidgetHostViewWin::OnPaint to only paint the damaged regions.
    This means calling GetUpdateRgn and GetRegionData to enumerate the list of
    damage rects.  Then only those rects are copied from the backing store.
    
    The same optimization is not done for Linux or Mac yet.
    
    R=brettw
    BUG=29591
    TEST=none
    
    Review URL: http://codereview.chromium.org/506013
    
    TBR=darin@chromium.org
    Review URL: http://codereview.chromium.org/506075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34953 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/backing_store_x.cc
+++ chrome/browser/renderer_host/backing_store_x.cc
@@ -312,7 +312,10 @@ void BackingStore::PaintRect(base::ProcessHandle process,
   XFreePixmap(display_, pixmap);
 }
 
-void BackingStore::ScrollRect(int dx, int dy,
+void BackingStore::ScrollRect(base::ProcessHandle process,
+                              TransportDIB* bitmap,
+                              const gfx::Rect& bitmap_rect,
+                              int dx, int dy,
                               const gfx::Rect& clip_rect,
                               const gfx::Size& view_size) {
   if (!display_)
@@ -344,6 +347,8 @@ void BackingStore::ScrollRect(int dx, int dy,
                 clip_rect.y() /* dest x */);
     }
   }
+
+  PaintRect(process, bitmap, bitmap_rect, bitmap_rect);
 }
 
 void BackingStore::ShowRect(const gfx::Rect& rect, XID target) {
