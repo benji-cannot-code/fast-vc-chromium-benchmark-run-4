004c020060cc30671c554daf5d243cf9c4d0891c
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-18 17:47:09 +0000

    V8 build fix
    * bindings/v8/custom/V8DOMWindowCustom.cpp: Added #include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8Utilities.h"
 
 #include "Base64.h"
+#include "Chrome.h"
 #include "ExceptionCode.h"
 #include "DOMTimer.h"
 #include "Frame.h"
