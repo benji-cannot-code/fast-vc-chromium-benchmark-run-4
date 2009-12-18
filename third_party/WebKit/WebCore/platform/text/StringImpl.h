044894a4f71a394622fb2248793a0ea265715fe0
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-18 17:42:33 +0000

    GTK (and others?) build fix
    * platform/text/StringImpl.h: Added #include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52315 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/StringImpl.h
+++ third_party/WebKit/WebCore/platform/text/StringImpl.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef StringImpl_h
 #define StringImpl_h
 
+#include <limits.h>
 #include <wtf/ASCIICType.h>
 #include <wtf/CrossThreadRefCounted.h>
 #include <wtf/OwnFastMallocPtr.h>
