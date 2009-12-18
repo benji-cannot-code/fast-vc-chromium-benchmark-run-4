e1dee3e0908771ca0fa854342c890a680bf6fdd5
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-18 20:41:23 +0000

    Add export the new symbol in WebCoreSystemInterface.
    Reviewed by Anders Carlsson.
    
    * WebCore.base.exp:
    * platform/mac/WebCoreSystemInterface.h:
    * platform/mac/WebCoreSystemInterface.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52331 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/WebCoreSystemInterface.mm
+++ third_party/WebKit/WebCore/platform/mac/WebCoreSystemInterface.mm
@@ -110,4 +110,6 @@ BOOL (*wkSupportsMultipartXMixedReplace)(NSMutableURLRequest *);
 
 #if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD) && !defined(BUILDING_ON_SNOW_LEOPARD)
 NSMutableArray *(*wkNoteOpenPanelFiles)(NSArray *);
+#else
+void* wkNoteOpenPanelFiles;
 #endif
