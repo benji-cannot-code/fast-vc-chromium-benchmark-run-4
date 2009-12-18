113e30f1d2f00afacd6a5c83c15189b100395cdc
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-18 14:03:51 +0000

    [Qt] Fix the build on Symbian.
    Reviewed by Tor Arne Vestbø.
    
    Use QFont::lastRestortFamily() instead of QFont::lastRestortFont(),
    which is a missing symbol in Qt 4.6.0 on Symbian.
    
    Functionally they should be equivalent, according to the source.
    
    * platform/graphics/qt/FontCacheQt.cpp:
    (WebCore::FontCache::getLastResortFallbackFont):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52310 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/FontCacheQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/FontCacheQt.cpp
@@ -57,7 +57,7 @@ FontPlatformData* FontCache::getSimilarFontPlatformData(const Font& font)
 
 FontPlatformData* FontCache::getLastResortFallbackFont(const FontDescription& fontDescription)
 {
-    const AtomicString fallbackFamily = QFont(fontDescription.family().family()).lastResortFont();
+    const AtomicString fallbackFamily = QFont(fontDescription.family().family()).lastResortFamily();
     return new FontPlatformData(fontDescription, fallbackFamily);
 }
 
