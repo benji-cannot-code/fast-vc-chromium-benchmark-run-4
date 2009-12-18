a98cd9c72b87ddc9af02b21fee23188febbfc2bb
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-18 19:22:44 +0000

    WebCore: Update WebKitSystemInterface for <rdar://problem/7237059>.
    Reviewed by Anders Carlsson.
    
    * platform/mac/WebCoreSystemInterface.h:
    * platform/mac/WebCoreSystemInterface.mm:
    
    WebKit/mac: Update WebKitSystemInterface for <rdar://problem/7237059>.        
    
    Reviewed by Anders Carlsson.
    
    * WebCoreSupport/WebSystemInterface.m:
    (InitWebCoreSystemInterface):
    
    WebKitLibraries: Update WebKitSystemInterface for <rdar://problem/7237059>.
    
    Reviewed by Anders Carlsson.
    
    * WebKitSystemInterface.h:
    * libWebKitSystemInterfaceLeopard.a:
    * libWebKitSystemInterfaceSnowLeopard.a:
    * libWebKitSystemInterfaceTiger.a:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/WebCoreSystemInterface.h
+++ third_party/WebKit/WebCore/platform/mac/WebCoreSystemInterface.h
@@ -49,6 +49,7 @@ typedef struct _NSRect NSRect;
 @class NSFont;
 @class NSImage;
 @class NSMenu;
+@class NSMutableArray;
 @class NSMutableURLRequest;
 @class NSString;
 @class NSTextFieldCell;
@@ -166,6 +167,10 @@ extern BOOL (*wkSupportsMultipartXMixedReplace)(NSMutableURLRequest *);
 #endif
 
 extern BOOL (*wkUseSharedMediaUI)();
+
+#if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD) && !defined(BUILDING_ON_SNOW_LEOPARD)
+extern NSMutableArray *(*wkNoteOpenPanelFiles)(NSArray *);
+#endif
 
 #ifdef __cplusplus
 }
