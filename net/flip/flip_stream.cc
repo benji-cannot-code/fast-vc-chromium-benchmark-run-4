87290328e92e7e4c083aa353c48be73b3dc14ee7
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-18 02:07:48 +0000

    Pass the LoadLog through the FlipStream.
    BUG=none
    TEST=FlipNetworkTransactionTest.LoadLog
    
    Review URL: http://codereview.chromium.org/500083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34920 0039d316-1c4b-4281-b951-d872f2087c98

--- net/flip/flip_stream.cc
+++ net/flip/flip_stream.cc
@@ -12,9 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-FlipStream::FlipStream(FlipSession* session,
-                       flip::FlipStreamId stream_id,
-                       bool pushed)
+FlipStream::FlipStream(FlipSession* session, flip::FlipStreamId stream_id,
+                       bool pushed, LoadLog* log)
     : stream_id_(stream_id),
       priority_(0),
       pushed_(pushed),
@@ -29,7 +28,8 @@ FlipStream::FlipStream(FlipSession* session,
       user_callback_(NULL),
       user_buffer_(NULL),
       user_buffer_len_(0),
-      cancelled_(false) {}
+      cancelled_(false),
+      load_log_(log) {}
 
 FlipStream::~FlipStream() {
   DLOG(INFO) << "Deleting FlipStream for stream " << stream_id_;
@@ -252,23 +252,35 @@ int FlipStream::DoLoop(int result) {
       // State machine 1: Send headers and wait for response headers.
       case STATE_SEND_HEADERS:
         CHECK(result == OK);
+        LoadLog::BeginEvent(load_log_,
+                            LoadLog::TYPE_FLIP_STREAM_SEND_HEADERS);
         result = DoSendHeaders();
         break;
       case STATE_SEND_HEADERS_COMPLETE:
+        LoadLog::EndEvent(load_log_,
+                          LoadLog::TYPE_FLIP_STREAM_SEND_HEADERS);
         result = DoSendHeadersComplete(result);
         break;
       case STATE_SEND_BODY:
         CHECK(result == OK);
+        LoadLog::BeginEvent(load_log_,
+                            LoadLog::TYPE_FLIP_STREAM_SEND_BODY);
         result = DoSendBody();
         break;
       case STATE_SEND_BODY_COMPLETE:
+        LoadLog::EndEvent(load_log_,
+                          LoadLog::TYPE_FLIP_STREAM_SEND_BODY);
         result = DoSendBodyComplete(result);
         break;
       case STATE_READ_HEADERS:
         CHECK(result == OK);
+        LoadLog::BeginEvent(load_log_,
+                            LoadLog::TYPE_FLIP_STREAM_READ_HEADERS);
         result = DoReadHeaders();
         break;
       case STATE_READ_HEADERS_COMPLETE:
+        LoadLog::EndEvent(load_log_,
+                          LoadLog::TYPE_FLIP_STREAM_READ_HEADERS);
         result = DoReadHeadersComplete(result);
         break;
 
@@ -277,9 +289,13 @@ int FlipStream::DoLoop(int result) {
       // the OnDataReceived()/OnClose()/ReadResponseHeaders()/etc.  Only reason
       // to do this is for consistency with the Http code.
       case STATE_READ_BODY:
+        LoadLog::BeginEvent(load_log_,
+                            LoadLog::TYPE_FLIP_STREAM_READ_BODY);
         result = DoReadBody();
         break;
       case STATE_READ_BODY_COMPLETE:
+        LoadLog::EndEvent(load_log_,
+                          LoadLog::TYPE_FLIP_STREAM_READ_BODY);
         result = DoReadBodyComplete(result);
         break;
       case STATE_DONE:
--- net/flip/flip_stream.h
+++ net/flip/flip_stream.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/bandwidth_metrics.h"
 #include "net/base/completion_callback.h"
 #include "net/base/io_buffer.h"
+#include "net/base/load_log.h"
 #include "net/flip/flip_framer.h"
 #include "net/flip/flip_protocol.h"
 
@@ -37,7 +38,8 @@ class UploadDataStream;
 class FlipStream : public base::RefCounted<FlipStream> {
  public:
   // FlipStream constructor
-  FlipStream(FlipSession* session, flip::FlipStreamId stream_id, bool pushed);
+  FlipStream(FlipSession* session, flip::FlipStreamId stream_id, bool pushed,
+             LoadLog* log);
 
   // Ideally I'd use two abstract classes as interfaces for these two sections,
   // but since we're ref counted, I can't make both abstract classes inherit
@@ -184,6 +186,8 @@ class FlipStream : public base::RefCounted<FlipStream> {
   int user_buffer_len_;
 
   bool cancelled_;
+
+  scoped_refptr<LoadLog> load_log_;
 
   DISALLOW_COPY_AND_ASSIGN(FlipStream);
 };
