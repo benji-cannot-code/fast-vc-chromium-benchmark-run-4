f30a147c8727055dac8e8cc1ad3104feadfb07b0
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-18 10:51:36 +0000

    Shut down fontconfig in test_shell to lower the amount of memory leaked in fontconfig. The remaining leaks are due to static variables in pango holding references to fontconfig data structures. These should go away for a more up to date version of pango.
    BUG=9245
    TEST=Run test_shell using valgrind. Memory leaks related to fontconfig should drop from around 138079 bytes to around 24573 bytes
    
    Patch by Jochen Eisinger <eisinger@google.com>.
    
    Review URL: http://codereview.chromium.org/507038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34945 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_gtk.cc
+++ webkit/tools/test_shell/test_shell_gtk.cc
@@ -315,6 +315,7 @@ void TestShell::PlatformCleanUp() {
         reinterpret_cast<gpointer>(MainWindowDestroyed), this);
     gtk_widget_destroy(GTK_WIDGET(m_mainWnd));
   }
+  FcFini();
 }
 
 bool TestShell::Initialize(const GURL& starting_url) {
