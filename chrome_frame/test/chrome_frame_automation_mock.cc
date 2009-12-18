8a68815325a24b8c32b29d7cdc00e0188cc4a455
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-18 22:52:47 +0000

    Fix ChromeFrame test failures on the builder caused by my CL to change the way the automation launch parameters are propagated to the ChromeFrameAutomationClient object.
    I also fixed the ChromeFrame automation mock tests which don't use gmock and instead relied
    on an incorrect behavior in ChromeFrame where we would deny the navigation from the automation
    client and still navigate in Chrome when the external tab is created.
    
    Will work on changing these tests to gmock in a subsequent CL.
    
    TBR=stoyan and thanks to him for helping debug the gmock failures.
    
    Review URL: http://codereview.chromium.org/500143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34996 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/chrome_frame_automation_mock.cc
+++ chrome_frame/test/chrome_frame_automation_mock.cc
@@ -24,7 +24,7 @@ TEST(ChromeFrame, Navigate) {
 
   mock_navigate.NavigateRelativeFile(L"postmessage_basic_frame.html");
   loop.Run(NULL);
-  EXPECT_EQ(true, mock_navigate.navigation_result());
+  EXPECT_EQ(false, mock_navigate.navigation_result());
 }
 
 TEST(ChromeFrame, PostMessage) {
@@ -33,7 +33,7 @@ TEST(ChromeFrame, PostMessage) {
 
   mock_postmessage.NavigateRelativeFile(L"postmessage_basic_frame.html");
   loop.Run(NULL);
-  EXPECT_EQ(true, mock_postmessage.postmessage_result());
+  EXPECT_EQ(false, mock_postmessage.postmessage_result());
 }
 
 TEST(ChromeFrame, RequestStart) {
