14d60a691027a03d26a11feba8b4f3a6ad7e2cb8
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-18 12:00:29 +0000

    2009-12-18  Kalle Vahlman  <kalle.vahlman@movial.com> and Alejandro G. Castro  <alex@igalia.com>
            Reviewed by Gustavo Noronha Silva.
    
            [GTK] Add enable-page-cache property to WebSettings for disabling the Page Cache
    
            * webkit/webkitwebsettings.cpp:
            (_WebKitWebSettingsPrivate::):
            (_WebKitWebSettingsPrivate::webkit_web_settings_class_init):
            (_WebKitWebSettingsPrivate::webkit_web_settings_set_property):
            (_WebKitWebSettingsPrivate::webkit_web_settings_get_property):
            (_WebKitWebSettingsPrivate::webkit_web_settings_copy):
            * webkit/webkitwebview.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52306 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *  Copyright (C) 2008 Nuanti Ltd.
  *  Copyright (C) 2008, 2009 Collabora Ltd.
  *  Copyright (C) 2009 Igalia S.L.
+ *  Copyright (C) 2009 Movial Creative Technologies Inc.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -2459,7 +2460,7 @@ static void webkit_web_view_update_settings(WebKitWebView* webView)
         enablePrivateBrowsing, enableCaretBrowsing, enableHTML5Database, enableHTML5LocalStorage,
         enableXSSAuditor, javascriptCanOpenWindows, enableOfflineWebAppCache,
         enableUniversalAccessFromFileURI, enableDOMPaste, tabKeyCyclesThroughElements,
-        enableSiteSpecificQuirks;
+        enableSiteSpecificQuirks, usePageCache;
 
     WebKitEditingBehavior editingBehavior;
 
@@ -2491,6 +2492,7 @@ static void webkit_web_view_update_settings(WebKitWebView* webView)
                  "enable-dom-paste", &enableDOMPaste,
                  "tab-key-cycles-through-elements", &tabKeyCyclesThroughElements,
                  "enable-site-specific-quirks", &enableSiteSpecificQuirks,
+                 "enable-page-cache", &usePageCache,
                  NULL);
 
     settings->setDefaultTextEncodingName(defaultEncoding);
@@ -2519,6 +2521,7 @@ static void webkit_web_view_update_settings(WebKitWebView* webView)
     settings->setAllowUniversalAccessFromFileURLs(enableUniversalAccessFromFileURI);
     settings->setDOMPasteAllowed(enableDOMPaste);
     settings->setNeedsSiteSpecificQuirks(enableSiteSpecificQuirks);
+    settings->setUsesPageCache(usePageCache);
 
     Page* page = core(webView);
     if (page)
@@ -2617,6 +2620,8 @@ static void webkit_web_view_settings_notify(WebKitWebSettings* webSettings, GPar
             page->setTabKeyCyclesThroughElements(g_value_get_boolean(&value));
     } else if (name == g_intern_string("enable-site-specific-quirks"))
         settings->setNeedsSiteSpecificQuirks(g_value_get_boolean(&value));
+    else if (name == g_intern_string("enable-page-cache"))
+        settings->setUsesPageCache(g_value_get_boolean(&value));
     else if (!g_object_class_find_property(G_OBJECT_GET_CLASS(webSettings), name))
         g_warning("Unexpected setting '%s'", name);
     g_value_unset(&value);
