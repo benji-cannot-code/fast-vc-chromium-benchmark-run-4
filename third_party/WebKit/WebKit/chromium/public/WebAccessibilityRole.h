d78f00ab3d1026ccebf0fe3284d255b9823667ed
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-19 00:47:20 +0000

    2009-12-18  Peter Kasting  <pkasting@google.com>
            Reviewed by Adam Barth.
    
            Chromium build fix
            https://bugs.webkit.org/show_bug.cgi?id=32756
    
            * public/WebAccessibilityRole.h:
            (WebKit::):
            * src/AssertMatchingEnums.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebAccessibilityRole.h
+++ third_party/WebKit/WebKit/chromium/public/WebAccessibilityRole.h
@@ -112,6 +112,8 @@ enum WebAccessibilityRole {
     WebAccessibilityRoleDirectory,
     WebAccessibilityRoleEditableText,
 
+    WebAccessibilityRoleListItem,
+
     WebAccessibilityRoleLandmarkApplication,
     WebAccessibilityRoleLandmarkBanner,
     WebAccessibilityRoleLandmarkComplementary,
