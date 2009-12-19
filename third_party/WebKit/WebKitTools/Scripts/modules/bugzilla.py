409ec0a3254249dfe3f76d1afe0de6a4a9a5c72c
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-19 06:05:14 +0000

    2009-12-18  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Add watches for EWS
            https://bugs.webkit.org/show_bug.cgi?id=32767
    
            dglazkov wanted to be added.
    
            * Scripts/modules/bugzilla.py:
            * Scripts/modules/commands/early_warning_system.py:
            * Scripts/modules/commands/queues.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52377 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/bugzilla.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/bugzilla.py
@@ -467,7 +467,7 @@ class Bugzilla(object):
             self.browser.set_value(comment_text, name='comment', nr=0)
         self.browser.submit()
 
-    def add_cc_to_bug(self, bug_id, email_address):
+    def add_cc_to_bug(self, bug_id, email_address_list):
         self.authenticate()
 
         log("Adding %s to the CC list for bug %s" % (email_address, bug_id))
@@ -476,7 +476,7 @@ class Bugzilla(object):
 
         self.browser.open(self.bug_url_for_bug_id(bug_id))
         self.browser.select_form(name="changeform")
-        self.browser["newcc"] = email_address
+        self.browser["newcc"] = email_address_list.join(", ")
         self.browser.submit()
 
     def post_comment_to_bug(self, bug_id, comment_text, cc=None):
@@ -491,7 +491,7 @@ class Bugzilla(object):
         self.browser.select_form(name="changeform")
         self.browser["comment"] = comment_text
         if cc:
-            self.browser["newcc"] = cc
+            self.browser["newcc"] = cc.join(", ")
         self.browser.submit()
 
     def close_bug_as_fixed(self, bug_id, comment_text=None):
--- third_party/WebKit/WebKitTools/Scripts/modules/commands/early_warning_system.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/commands/early_warning_system.py
@@ -89,3 +89,6 @@ class QtEWS(AbstractEarlyWarningSystem):
 class ChromiumEWS(AbstractEarlyWarningSystem):
     name = "chromium-ews"
     port_name = "chromium"
+    watchers = AbstractEarlyWarningSystem.watchers + [
+        "dglazkov@chromium.org",
+    ]
