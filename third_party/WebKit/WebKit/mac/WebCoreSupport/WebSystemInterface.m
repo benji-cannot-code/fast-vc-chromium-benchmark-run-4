ce7db9352450ad7ece1b63916e0bfb87ef027725
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-19 00:33:34 +0000

    2009-12-18  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            REGRESSION(52325) Chromium build broken due to link failure
            https://bugs.webkit.org/show_bug.cgi?id=32753
    
            * WebCoreSupport/WebSystemInterface.m:
             - Chromium Mac builds use this file, but do not
               enable Pre-compiled Header (PCH) files so that
               Chromium build bots can take advantage of the faster
               distcc 3.  Eventually all mac builds will likely use
               distcc, but for now we'll add Platform.h here and document
               its usage so it's not removed by accident.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebSystemInterface.m
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebSystemInterface.m
@@ -32,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebCore/WebCoreSystemInterface.h>
 #import <WebKitSystemInterface.h>
 
+// Needed for builds not using PCH to expose BUILDING_ macros, see bug 32753.
+#include <wtf/Platform.h>
+
 #define INIT(function) wk##function = WK##function
 
 void InitWebCoreSystemInterface(void)
