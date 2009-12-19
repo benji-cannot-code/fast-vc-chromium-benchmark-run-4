6c3f05b392104bb61fc3fc78aaa385250577b902
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-19 17:23:15 +0000

    2009-12-19  Adam Barth  <abarth@webkit.org>
            Unreviewed fixes for me being terrible at python.
    
            * Scripts/modules/bugzilla.py:
            * Scripts/run-webkit-unittests:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52385 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/bugzilla.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/bugzilla.py
@@ -476,7 +476,7 @@ class Bugzilla(object):
 
         self.browser.open(self.bug_url_for_bug_id(bug_id))
         self.browser.select_form(name="changeform")
-        self.browser["newcc"] = email_address_list.join(", ")
+        self.browser["newcc"] = ", ".join(email_address_list)
         self.browser.submit()
 
     def post_comment_to_bug(self, bug_id, comment_text, cc=None):
@@ -491,7 +491,7 @@ class Bugzilla(object):
         self.browser.select_form(name="changeform")
         self.browser["comment"] = comment_text
         if cc:
-            self.browser["newcc"] = cc.join(", ")
+            self.browser["newcc"] = ", ".join(cc)
         self.browser.submit()
 
     def close_bug_as_fixed(self, bug_id, comment_text=None):
