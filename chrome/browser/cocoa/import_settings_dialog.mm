4948d3bf5d9cef421cf2e8ce8032a8bf9881a8f4
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-20 12:51:01 +0000

    Revert r35069
    Review URL: http://codereview.chromium.org/509002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35070 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/import_settings_dialog.mm
+++ chrome/browser/cocoa/import_settings_dialog.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @property(assign, readwrite, nonatomic) BOOL historyAvailable;
 @property(assign, readwrite, nonatomic) BOOL favoritesAvailable;
+@property(assign, readwrite, nonatomic) BOOL cookiesAvailable;
 @property(assign, readwrite, nonatomic) BOOL passwordsAvailable;
 @property(assign, readwrite, nonatomic) BOOL searchEnginesAvailable;
 
@@ -64,17 +65,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize sourceBrowserIndex = sourceBrowserIndex_;
 @synthesize importHistory = importHistory_;
 @synthesize importFavorites = importFavorites_;
+@synthesize importCookies = importCookies_;
 @synthesize importPasswords = importPasswords_;
 @synthesize importSearchEngines = importSearchEngines_;
 @synthesize historyAvailable = historyAvailable_;
 @synthesize favoritesAvailable = favoritesAvailable_;
+@synthesize cookiesAvailable = cookiesAvailable_;
 @synthesize passwordsAvailable = passwordsAvailable_;
 @synthesize searchEnginesAvailable = searchEnginesAvailable_;
 
 // Set bindings dependencies for importSomething property.
 + (NSSet*)keyPathsForValuesAffectingImportSomething {
   return [NSSet setWithObjects:@"importHistory", @"importFavorites",
-          @"importPasswords", @"importSearchEngines", nil];
+          @"importCookies", @"importPasswords", @"importSearchEngines", nil];
 }
 
 - (id)initWithProfile:(Profile*)profile
@@ -200,6 +203,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self setImportHistory:[self historyAvailable]];
   [self setFavoritesAvailable:(items & FAVORITES) ? YES : NO];
   [self setImportFavorites:[self favoritesAvailable]];
+  [self setCookiesAvailable:(items & COOKIES) ? YES : NO];
+  [self setImportCookies:[self cookiesAvailable]];
   [self setPasswordsAvailable:(items & PASSWORDS) ? YES : NO];
   [self setImportPasswords:[self passwordsAvailable]];
   [self setSearchEnginesAvailable:(items & SEARCH_ENGINES) ? YES : NO];
@@ -210,6 +215,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   uint16 servicesToImport = 0;
   if ([self importHistory]) servicesToImport |= HISTORY;
   if ([self importFavorites]) servicesToImport |= FAVORITES;
+  if ([self importCookies]) servicesToImport |= COOKIES;
   if ([self importPasswords]) servicesToImport |= PASSWORDS;
   if ([self importSearchEngines]) servicesToImport |= SEARCH_ENGINES;
   return servicesToImport;
@@ -220,7 +226,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // and bound to the OK button's enable property.
 - (BOOL)importSomething {
   return [self importHistory] || [self importFavorites] ||
-      [self importPasswords] || [self importSearchEngines];
+      [self importCookies] || [self importPasswords] ||
+      [self importSearchEngines];
 }
 
 @end
