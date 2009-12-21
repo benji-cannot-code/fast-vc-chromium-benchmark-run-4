cfae71e6115955f3aa096d72fcd6881c9b2473e6
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-21 14:44:00 +0000

    Fix non-ACCELERATED_COMPOSITING builds
    * WebPreferences.cpp:
    (WebPreferences::acceleratedCompositingEnabled): Guard use of
    WKCACFLayerRenderer with USE(ACCELERATED_COMPOSITING).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebPreferences.cpp
+++ third_party/WebKit/WebKit/win/WebPreferences.cpp
@@ -1366,7 +1366,11 @@ HRESULT WebPreferences::setAcceleratedCompositingEnabled(BOOL enabled)
 
 HRESULT WebPreferences::acceleratedCompositingEnabled(BOOL* enabled)
 {
+#if USE(ACCELERATED_COMPOSITING)
     *enabled = WKCACFLayerRenderer::acceleratedCompositingAvailable() && boolValueForKey(CFSTR(WebKitAcceleratedCompositingEnabledPreferenceKey));
+#else
+    *enabled = FALSE;
+#endif
     return S_OK;
 }
 
