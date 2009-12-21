82241ad99d0df64ee716adbb28235062557da9aa
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-21 17:44:20 +0000

    Mac: Create a pid->task_t mapping in the browser process.
    Since nothing writes to this map in the browser atm, this does not have any visible effect.
    
    BUG=13156,25454
    TEST=unittest
    
    Review URL: http://codereview.chromium.org/501138
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35092 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/mach_broker_mac_unittest.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/mach_broker_mac.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+class MachBrokerTest : public testing::Test {
+ public:
+  MachBroker broker_;
+};
+
+TEST_F(MachBrokerTest, Setter) {
+  broker_.RegisterPid(1u, MachBroker::MachInfo().SetTask(2u));
+  EXPECT_EQ(2u, broker_.TaskForPid(1));
+  EXPECT_EQ(0u, broker_.TaskForPid(2));
+}
+
+TEST_F(MachBrokerTest, Invalidate) {
+  broker_.RegisterPid(1u, MachBroker::MachInfo().SetTask(2));
+  broker_.Invalidate(1u);
+  EXPECT_EQ(0u, broker_.TaskForPid(1));
+}
--- chrome/browser/task_manager.cc
+++ chrome/browser/task_manager.cc
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_job.h"
 
+#if defined(OS_MACOSX)
+#include "chrome/browser/mach_broker_mac.h"
+#endif
+
 namespace {
 
 // The delay between updates of the information (in ms).
@@ -537,9 +541,8 @@ void TaskManagerModel::AddResource(TaskManager::Resource* resource) {
 #if !defined(OS_MACOSX)
         base::ProcessMetrics::CreateProcessMetrics(process);
 #else
-        // TODO(thakis): Write a port provider that knows the task ports of
-        // child processes.
-        base::ProcessMetrics::CreateProcessMetrics(process, NULL);
+        base::ProcessMetrics::CreateProcessMetrics(process,
+                                                   MachBroker::instance());
 #endif
 
     metrics_map_[process] = pm;
