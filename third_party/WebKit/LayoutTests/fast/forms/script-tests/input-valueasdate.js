d31000e35e493eabffe19c6e444d8dae954e5459
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-21 06:20:29 +0000

    2009-12-20  Kent Tamura  <tkent@chromium.org>
            Reviewed by Darin Adler.
    
            A test for Date binding.
            https://bugs.webkit.org/show_bug.cgi?id=32698
    
            * fast/forms/input-valueasdate-expected.txt: Added.
            * fast/forms/input-valueasdate.html: Added.
            * fast/forms/script-tests/input-valueasdate.js: Added.
    
    2009-12-20  Kent Tamura  <tkent@chromium.org>
    
            Reviewed by Darin Adler.
    
            Add support for JavaScriptCore Date binding.
            https://bugs.webkit.org/show_bug.cgi?id=32698
    
            This is needed for HTMLInputElement::valueAsDate and
            HTMLTimeElement::valueAsDate.
    
            A Date instance is mapped to a double value in C++.
            - If null or undefined is set to a JavaScript property, C++ setter
              function receives NaN.
            - If a getter C++ function returns NaN or infinity, a JavaScript
              property returns null.
    
            Test: fast/forms/input-valueasdate.html
    
            * bindings/js/JSDOMBinding.cpp:
            (WebCore::jsDateOrNull): Implement the above.
            (WebCore::valueToDate): ditto.
            * bindings/js/JSDOMBinding.h: Declare them.
            * bindings/scripts/CodeGeneratorJS.pm: Produce jsDateOrNull() or
              valueToDate() for Date type.
            * html/HTMLInputElement.cpp:
            (WebCore::HTMLInputElement::valueAsDate): Temporal implementation.
            (WebCore::HTMLInputElement::setValueAsDate): ditto.
            * html/HTMLInputElement.h:
            * html/HTMLInputElement.idl: Declare valueAsDate.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52434 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/forms/script-tests/input-valueasdate.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('Test HTMLInputElement::valueAsDate binding.');
+
+var input = document.createElement('input');
+var invalidStateError = 'Error: INVALID_STATE_ERR: DOM Exception 11';
+
+debug('Unsuppported type:');
+input.type = 'text';
+shouldBe('input.valueAsDate', 'null');
+shouldThrow('input.valueAsDate = date', 'invalidStateError');
+
+debug('');
+debug('Supported type:');
+input.type = 'month';
+input.value = '2009-12';
+var valueAsDate = input.valueAsDate;
+shouldBeTrue('valueAsDate != null');
+shouldBe('typeof valueAsDate', '"object"');
+shouldBe('valueAsDate.constructor.name', '"Date"');
+
+debug('Sets an Epoch Date:');
+var date = new Date();
+date.setTime(0);
+input.valueAsDate = date;
+shouldBe('input.value', '"1970-01"');
+shouldBe('input.valueAsDate.getTime()', '0');
+debug('Sets a number 0:');
+input.valueAsDate = 0;
+shouldBe('input.value', '"1970-01"');
+shouldBe('input.valueAsDate.getTime()', '0');
+debug('Sets other types:');
+shouldThrow('input.valueAsDate = null', 'invalidStateError');
+shouldThrow('input.valueAsDate = undefined', 'invalidStateError');
+shouldThrow('input.valueAsDate = document', 'invalidStateError');
+
+var successfullyParsed = true;
--- third_party/WebKit/WebCore/bindings/js/JSDOMBinding.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDOMBinding.cpp
@@ -50,9 +50,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScriptController.h"
 #include "Settings.h"
 #include "XMLHttpRequestException.h"
+#include <runtime/DateInstance.h>
 #include <runtime/Error.h>
 #include <runtime/JSFunction.h>
 #include <runtime/PrototypeFunction.h>
+#include <wtf/MathExtras.h>
 #include <wtf/StdLibExtras.h>
 
 #if ENABLE(SVG)
@@ -595,6 +597,22 @@ UString valueToStringWithUndefinedOrNullCheck(ExecState* exec, JSValue value)
     if (value.isUndefinedOrNull())
         return UString();
     return value.toString(exec);
+}
+
+JSValue jsDateOrNull(ExecState* exec, double value)
+{
+    if (!isfinite(value))
+        return jsNull();
+    return new (exec) DateInstance(exec, value);
+}
+
+double valueToDate(ExecState* exec, JSValue value)
+{
+    if (value.isNumber())
+        return value.uncheckedGetNumber();
+    if (!value.inherits(&DateInstance::info))
+        return std::numeric_limits<double>::quiet_NaN();
+    return static_cast<DateInstance*>(value.toObject(exec))->internalNumber();
 }
 
 void reportException(ExecState* exec, JSValue exception)
