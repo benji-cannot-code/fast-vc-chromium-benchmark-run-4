f05b92b9234418268940dc5f4a09699963bbf85a
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-21 21:01:12 +0000

    Fix crash with CancelStream.  The stream didn't inform the FlipSession, and the FlipSession continued to try to notify the stream of new data.
    Also re-enable the CancelTransaction test, which did catch
    the crash.
    
    BUG=none
    TEST=FlipNetworkTransactionTest.CancelTransaction
    
    Review URL: http://codereview.chromium.org/506062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35106 0039d316-1c4b-4281-b951-d872f2087c98

--- net/flip/flip_session.cc
+++ net/flip/flip_session.cc
@@ -396,6 +396,9 @@ bool FlipSession::CancelStream(flip::FlipStreamId stream_id) {
   if (!IsStreamActive(stream_id))
     return false;
 
+  // TODO(mbelshe): We should send a FIN_STREAM control frame here
+  //                so that the server can cancel a large send.
+
   // TODO(mbelshe): Write a method for tearing down a stream
   //                that cleans it out of the active list, the pending list,
   //                etc.
@@ -532,7 +535,11 @@ void FlipSession::OnWriteComplete(int result) {
         DCHECK_GT(result, static_cast<int>(flip::FlipFrame::size()));
         result -= static_cast<int>(flip::FlipFrame::size());
       }
-      stream->OnWriteComplete(result);
+
+      // It is possible that the stream was cancelled while we were writing
+      // to the socket.
+      if (!stream->cancelled())
+        stream->OnWriteComplete(result);
 
       // Cleanup the write which just completed.
       in_flight_write_.release();
@@ -771,6 +778,7 @@ void FlipSession::OnStreamFrameData(flip::FlipStreamId stream_id,
   LOG(INFO) << "Flip data for stream " << stream_id << ", " << len << " bytes";
   bool valid_stream = IsStreamActive(stream_id);
   if (!valid_stream) {
+    // NOTE:  it may just be that the stream was cancelled.
     LOG(WARNING) << "Received data frame for invalid stream " << stream_id;
     return;
   }
@@ -867,6 +875,7 @@ void FlipSession::OnSynReply(const flip::FlipSynReplyControlFrame* frame,
   flip::FlipStreamId stream_id = frame->stream_id();
   bool valid_stream = IsStreamActive(stream_id);
   if (!valid_stream) {
+    // NOTE:  it may just be that the stream was cancelled.
     LOG(WARNING) << "Received SYN_REPLY for invalid stream " << stream_id;
     return;
   }
@@ -904,6 +913,7 @@ void FlipSession::OnSynReply(const flip::FlipSynReplyControlFrame* frame,
 
   scoped_refptr<FlipStream> stream = active_streams_[stream_id];
   CHECK(stream->stream_id() == stream_id);
+  CHECK(!stream->cancelled());
   HttpResponseInfo response;
   if (FlipHeadersToHttpResponse(*headers, &response)) {
     GetSSLInfo(&response.ssl_info);
@@ -950,11 +960,13 @@ void FlipSession::OnFin(const flip::FlipFinStreamControlFrame* frame) {
   flip::FlipStreamId stream_id = frame->stream_id();
   bool valid_stream = IsStreamActive(stream_id);
   if (!valid_stream) {
+    // NOTE:  it may just be that the stream was cancelled.
     LOG(WARNING) << "Received FIN for invalid stream" << stream_id;
     return;
   }
   scoped_refptr<FlipStream> stream = active_streams_[stream_id];
   CHECK(stream->stream_id() == stream_id);
+  CHECK(!stream->cancelled());
   if (frame->status() == 0) {
     stream->OnDataReceived(NULL, 0);
   } else {
