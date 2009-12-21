129c902c9cd6950aa9904c9210c350a704060847
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-21 02:42:46 +0000

    2009-12-20  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Prepare QueueStatusServer for new status messages
            https://bugs.webkit.org/show_bug.cgi?id=32805
    
            * QueueStatusServer/handlers/recentstatus.py:
            * QueueStatusServer/index.html: Removed.
            * QueueStatusServer/model/attachment.py:
            * QueueStatusServer/model/queues.py: Added.
            * QueueStatusServer/templates/recentstatus.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52429 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/QueueStatusServer/model/queues.py
+++ third_party/WebKit/WebKitTools/QueueStatusServer/model/queues.py
@@ -27,32 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template
-
-from model.queues import queues
-from model.queuestatus import QueueStatus
-
-class RecentStatus(webapp.RequestHandler):
-    def _title_case(self, string):
-        words = string.split(" ")
-        words = map(lambda word: word.capitalize(), words)
-        return " ".join(words)
-
-    def _pretty_queue_name(self, queue_name):
-        return self._title_case(queue_name.replace("-", " "))
-
-    # We could change "/" to just redirect to /queue-status/commit-queue in the future
-    # at which point we would not need a default value for queue_name here.
-    def get(self, queue_name="commit-queue"):
-        queue_status = {}
-        for queue in queues:
-            statuses = QueueStatus.all().filter("queue_name =", queue).order("-date").fetch(6)
-            if not statuses:
-                continue
-            queue_status[queue] = statuses
-
-        template_values = {
-            "queue_status" : queue_status,
-        }
-        self.response.out.write(template.render("templates/recentstatus.html", template_values))
+queues = [
+    "style-queue",
+    "chromium-ews",
+    "qt-ews",
+    "gtk-ews",
+    "commit-queue",
+]
