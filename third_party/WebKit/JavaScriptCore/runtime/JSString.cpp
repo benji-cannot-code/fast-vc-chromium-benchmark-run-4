dc93406360bf1eb69d9b476ec28622f7110c81fa
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-22 00:38:24 +0000

    https://bugs.webkit.org/show_bug.cgi?id=32831 Replace UString::Rep implementation, following introduction of ropes to JSC.
    Reviewed by Darin Adler.
    
    JavaScriptCore: 
    
        * Remove redundant overcapacity mechanisms.
        * Reduce memory cost of Rep's.
        * Add an inline storage mechanism akin to that in WebCore's StringImpl.
    
    ~1% Sunspider progression.
    
    * JavaScriptCore.exp:
    * JavaScriptCore.xcodeproj/project.pbxproj:
    * runtime/JSString.cpp:
    (JSC::JSString::resolveRope):
    * runtime/SmallStrings.cpp:
    (JSC::SmallStringsStorage::SmallStringsStorage):
    * runtime/UString.cpp:
    (JSC::initializeUString):
    (JSC::createRep):
    (JSC::UString::createFromUTF8):
    (JSC::UString::createUninitialized):
    (JSC::UString::spliceSubstringsWithSeparators):
    (JSC::UString::replaceRange):
    (JSC::UString::ascii):
    (JSC::UString::operator=):
    (JSC::UString::toStrictUInt32):
    (JSC::equal):
    * runtime/UString.h:
    (JSC::UString::isEmpty):
    (JSC::UString::cost):
    (JSC::makeString):
    * runtime/UStringImpl.cpp: Added.
    (JSC::UStringImpl::baseSharedBuffer):
    (JSC::UStringImpl::sharedBuffer):
    (JSC::UStringImpl::destroy):
    (JSC::UStringImpl::computeHash):
    * runtime/UStringImpl.h: Added.
    (JSC::UntypedPtrAndBitfield::UntypedPtrAndBitfield):
    (JSC::UntypedPtrAndBitfield::asPtr):
    (JSC::UntypedPtrAndBitfield::operator&=):
    (JSC::UntypedPtrAndBitfield::operator|=):
    (JSC::UntypedPtrAndBitfield::operator&):
    (JSC::UStringImpl::create):
    (JSC::UStringImpl::createCopying):
    (JSC::UStringImpl::createUninitialized):
    (JSC::UStringImpl::data):
    (JSC::UStringImpl::size):
    (JSC::UStringImpl::cost):
    (JSC::UStringImpl::hash):
    (JSC::UStringImpl::computedHash):
    (JSC::UStringImpl::setHash):
    (JSC::UStringImpl::identifierTable):
    (JSC::UStringImpl::setIdentifierTable):
    (JSC::UStringImpl::ref):
    (JSC::UStringImpl::deref):
    (JSC::UStringImpl::allocChars):
    (JSC::UStringImpl::copyChars):
    (JSC::UStringImpl::computeHash):
    (JSC::UStringImpl::null):
    (JSC::UStringImpl::empty):
    (JSC::UStringImpl::checkConsistency):
    (JSC::UStringImpl::):
    (JSC::UStringImpl::UStringImpl):
    (JSC::UStringImpl::operator new):
    (JSC::UStringImpl::bufferOwnerString):
    (JSC::UStringImpl::bufferOwnership):
    (JSC::UStringImpl::isStatic):
    
    JavaScriptGlue: 
    
    * ForwardingHeaders/wtf/PossiblyNull.h: Added.
        - add forwarding header.
    
    WebCore: 
    
    * ForwardingHeaders/runtime/UStringImpl.h: Added.
        - add forwarding header.
    
    * platform/text/StringImpl.cpp:
    (WebCore::StringImpl::ustring):
        - order of arguments to UString::Rep constructor for shared strings changed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSString.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSString.cpp
@@ -97,13 +97,13 @@ void JSString::resolveRope(ExecState* exec) const
 
     // Allocate the buffer to hold the final string, position initially points to the end.
     UChar* buffer;
-    if (!tryFastMalloc(m_stringLength * sizeof(UChar)).getValue(buffer)) {
+    m_value = UString::createUninitialized(m_stringLength, buffer);
+    if (!buffer) {
         for (unsigned i = 0; i < m_ropeLength; ++i)
             m_fibers[i].deref();
         m_ropeLength = 0;
         ASSERT(!isRope());
         ASSERT(m_value == UString());
-
         throwOutOfMemoryError(exec);
         return;
     }
@@ -134,7 +134,6 @@ void JSString::resolveRope(ExecState* exec) const
             if (workQueue.isEmpty()) {
                 // Create a string from the UChar buffer, clear the rope RefPtr.
                 ASSERT(buffer == position);
-                m_value = UString::Rep::create(buffer, m_stringLength);
                 for (unsigned i = 0; i < m_ropeLength; ++i)
                     m_fibers[i].deref();
                 m_ropeLength = 0;
