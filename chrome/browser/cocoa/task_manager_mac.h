ac85aa69f9573e659d65912ca7579a39e92e1f52
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-22 21:42:43 +0000

    Mac: Hook up "End process" button on task manager.
    Nib changes: Connect "End process" button to action and outlet, allow multiple selection on the table, make TaskManagerWindowController the delegate of the table view.
    
    BUG=13156
    TEST=Hook up task manager in menu, open it. Button should be enabled if at least one task is selected, clicking the button should terminate the selected processes.
    
    Review URL: http://codereview.chromium.org/505073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35172 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/task_manager_mac.h
+++ chrome/browser/cocoa/task_manager_mac.h
@@ -15,16 +15,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface TaskManagerWindowController : NSWindowController {
  @private
   IBOutlet NSTableView* tableView_;
+  IBOutlet NSButton* endProcessButton_;
+  TaskManager* taskManager_;  // weak
   TaskManagerModel* model_;  // weak
 }
 
 // Creates and shows the task manager's window.
-- (id)initWithModel:(TaskManagerModel*)model;
+- (id)initWithTaskManager:(TaskManager*)taskManager;
 
 // Refreshes all data in the task manager table.
 - (void)reloadData;
 
+// Callback for "Stats for nerds" link.
 - (IBAction)statsLinkClicked:(id)sender;
+
+// Callback for "End process" button.
+- (IBAction)killSelectedProcesses:(id)sender;
 @end
 
 // This class listens to task changed events sent by chrome.
--- chrome/browser/cocoa/task_manager_mac.mm
+++ chrome/browser/cocoa/task_manager_mac.mm
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 
 // TODO(thakis): Autoremember window size/pos (and selected columns?)
-// TODO(thakis): Activate button iff something is selected, hook it up
 // TODO(thakis): Column sort comparator
+// TODO(thakis): Double-clicking a tab should activate that tab
 // TODO(thakis): Clicking column header doesn't sort
 // TODO(thakis): Double-clicking a row seems to do something on win/linux
 // TODO(thakis): On window close, stop updating
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setUpTableColumns;
 - (void)setUpTableHeaderContextMenu;
 - (void)toggleColumn:(id)sender;
+- (void)adjustEndProcessButton;
 @end
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -35,12 +36,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation TaskManagerWindowController
 
-- (id)initWithModel:(TaskManagerModel*)model {
+- (id)initWithTaskManager:(TaskManager*)taskManager {
   NSString* nibpath = [mac_util::MainAppBundle()
                         pathForResource:@"TaskManager"
                                  ofType:@"nib"];
   if ((self = [super initWithWindowNibPath:nibpath owner:self])) {
-    model_ = model;
+    taskManager_ = taskManager;
+    model_ = taskManager_->model();
     [[self window] makeKeyAndOrderFront:self];
   }
   return self;
@@ -48,15 +50,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)reloadData {
   [tableView_ reloadData];
+  [self adjustEndProcessButton];
 }
 
 - (IBAction)statsLinkClicked:(id)sender {
   TaskManager::GetInstance()->OpenAboutMemory();
 }
 
+- (IBAction)killSelectedProcesses:(id)sender {
+  NSIndexSet* selection = [tableView_ selectedRowIndexes];
+  for (NSUInteger i = [selection lastIndex];
+       i != NSNotFound;
+       i = [selection indexLessThanIndex:i]) {
+    taskManager_->KillProcess(i);
+  }
+}
+
 - (void)awakeFromNib {
   [self setUpTableColumns];
   [self setUpTableHeaderContextMenu];
+  [self adjustEndProcessButton];
 }
 
 // Adds a column which has the given string id as title. |isVisible| specifies
@@ -130,6 +143,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [item setState:newState];
   [tableView_ sizeToFit];
   [tableView_ setNeedsDisplay];
+}
+
+// This function appropriately sets the enabled states on the table's editing
+// buttons.
+- (void)adjustEndProcessButton {
+  bool selection_contains_browser_process = false;
+
+  NSIndexSet* selection = [tableView_ selectedRowIndexes];
+  for (NSUInteger i = [selection lastIndex];
+       i != NSNotFound;
+       i = [selection indexLessThanIndex:i]) {
+    if (taskManager_->IsBrowserProcess(i)) {
+      selection_contains_browser_process = true;
+      break;
+    }
+  }
+
+  bool enabled = [selection count] > 0 && !selection_contains_browser_process;
+  [endProcessButton_ setEnabled:enabled];
+}
+
+// Table view delegate method.
+- (void)tableViewSelectionDidChange:(NSNotification*)aNotification {
+  [self adjustEndProcessButton];
 }
 
 @end
@@ -217,7 +254,7 @@ TaskManagerMac::TaskManagerMac()
   : task_manager_(TaskManager::GetInstance()),
     model_(TaskManager::GetInstance()->model()) {
   window_controller_.reset(
-      [[TaskManagerWindowController alloc] initWithModel:model_]);
+      [[TaskManagerWindowController alloc] initWithTaskManager:task_manager_]);
   model_->AddObserver(this);
 }
 
