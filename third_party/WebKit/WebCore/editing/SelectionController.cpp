e0168b0b39ac8b84281343e7be5c87da51848bda
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-22 19:26:29 +0000

    Reentrancy problem with selection in some edge cases. https://bugs.webkit.org/show_bug.cgi?id=32842 rdar://problem/7449974
    Reviewed by Sam Weinig.
    
    WebCore: 
    
    Test: fast/forms/selection-layout-reentry-strange-case.html
    
    Move the selection display update process done in the
    selectionLayoutChanged function into the layout timer
    instead of doing it immediately when selection changes occur.
    
    * editing/SelectionController.cpp:
    (WebCore::SelectionController::SelectionController):
    Initialize m_needsDisplayUpdate to false.
    (WebCore::SelectionController::setSelection): Call
    the new setNeedsDisplayUpdate function instead of the old
    badly named Frame::selectionLayoutChanged function.
    (WebCore::SelectionController::setNeedsDisplayUpdate):
    Set m_needsDisplayUpdate. If it is just becoming true, then
    call FrameView::scheduleRelayout.
    
    * editing/SelectionController.h: Added setNeedsDisplayUpdate,
    needsDisplayUpdate, and m_needsDisplayUpdate.
    
    * page/Frame.cpp:
    (WebCore::Frame::setCaretVisible): Call setNeedsDisplayUpdate.
    (WebCore::Frame::selectionLayoutChanged): Call
    setNeedsDisplayUpdate to set it to false, since this is the
    function that performs "selection display update". Later I want
    to rename this function.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::needsLayout): Add a new clause, since
    we need a call to layout if needsDisplayUpdate is true.
    
    LayoutTests: 
    
    * fast/forms/selection-layout-reentry-strange-case-expected.txt: Added.
    * fast/forms/selection-layout-reentry-strange-case.html: Added.
    
    * platform/mac/accessibility/frame-with-title-expected.txt: Updated since
    the number of layouts is now different.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/SelectionController.cpp
+++ third_party/WebKit/WebCore/editing/SelectionController.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2004, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2004, 2008, 2009 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -71,6 +71,7 @@ SelectionController::SelectionController(Frame* frame, bool isDragCaretControlle
     , m_isDragCaretController(isDragCaretController)
     , m_isCaretBlinkingSuspended(false)
     , m_focused(frame && frame->page() && frame->page()->focusController()->focusedFrame() == frame)
+    , m_needsDisplayUpdate(false)
 {
 }
 
@@ -146,7 +147,8 @@ void SelectionController::setSelection(const VisibleSelection& s, bool closeTypi
     if (!s.isNone())
         m_frame->setFocusedNodeIfNeeded();
     
-    m_frame->selectionLayoutChanged();
+    setNeedsDisplayUpdate();
+
     // Always clear the x position used for vertical arrow navigation.
     // It will be restored by the vertical arrow navigation code if necessary.
     m_xPosForVerticalArrowNavigation = NoXPosForVerticalArrowNavigation;
@@ -1300,6 +1302,20 @@ void SelectionController::setFocused(bool flag)
 bool SelectionController::isFocusedAndActive() const
 {
     return m_focused && m_frame->page() && m_frame->page()->focusController()->isActive();
+}
+
+void SelectionController::setNeedsDisplayUpdate(bool needsUpdate)
+{
+    if (m_needsDisplayUpdate == needsUpdate)
+        return;
+    m_needsDisplayUpdate = needsUpdate;
+
+    if (!m_needsDisplayUpdate)
+        return;
+    FrameView* view = m_frame->view();
+    if (!view)
+        return;
+    view->scheduleRelayout();
 }
 
 #ifndef NDEBUG
--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -564,7 +564,7 @@ void Frame::setCaretVisible(bool flag)
         return;
     clearCaretRectIfNeeded();
     m_caretVisible = flag;
-    selectionLayoutChanged();
+    selection()->setNeedsDisplayUpdate();
 }
 
 void Frame::clearCaretRectIfNeeded()
@@ -631,6 +631,8 @@ void Frame::setFocusedNodeIfNeeded()
 
 void Frame::selectionLayoutChanged()
 {
+    selection()->setNeedsDisplayUpdate(false);
+
     bool caretRectChanged = selection()->recomputeCaretRect();
 
 #if ENABLE(TEXT_CARET)
