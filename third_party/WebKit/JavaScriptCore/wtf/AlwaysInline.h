937cde277e9b68765b162c6ed48be8e9fb7662a6
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-22 18:36:41 +0000

    2009-12-22  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Darin Adler.
    
            Define ALWAYS_INLINE and WTF_PRIVATE_INLINE to __forceinline for RVCT
            https://bugs.webkit.org/show_bug.cgi?id=32853
    
            Use __forceinline forces RVCT to compile a C or C++ function
            inline. The compiler attempts to inline the function, regardless of
            the characteristics of the function.
    
            * wtf/AlwaysInline.h:
            * wtf/FastMalloc.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52490 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/AlwaysInline.h
+++ third_party/WebKit/JavaScriptCore/wtf/AlwaysInline.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ALWAYS_INLINE
 #if COMPILER(GCC) && defined(NDEBUG) && !COMPILER(MINGW)
 #define ALWAYS_INLINE inline __attribute__((__always_inline__))
-#elif COMPILER(MSVC) && defined(NDEBUG)
+#elif (COMPILER(MSVC) || COMPILER(RVCT)) && defined(NDEBUG)
 #define ALWAYS_INLINE __forceinline
 #else
 #define ALWAYS_INLINE inline
