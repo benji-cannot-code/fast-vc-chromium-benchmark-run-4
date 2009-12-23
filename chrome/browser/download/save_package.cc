6e1e1b46dd4fd8d94a9a0bd61516d3df5c394ff4
yaar@chromium.org <yaar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-23 11:55:07 +0000

    Chromium to use upstream WebPageSerializer instead of glue/DomSerializer.
    See corresponding changes in webkit here: https://bugs.webkit.org/show_bug.cgi?id=31737
    
    Review URL: http://codereview.chromium.org/434087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35216 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/save_package.cc
+++ chrome/browser/download/save_package.cc
@@ -43,9 +43,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/mime_util.h"
 #include "net/base/net_util.h"
 #include "net/url_request/url_request_context.h"
-#include "webkit/glue/dom_serializer_delegate.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebPageSerializerClient.h"
 
 using base::Time;
+using WebKit::WebPageSerializerClient;
 
 // This structure is for storing parameters which we will use to create a
 // SavePackage object later.
@@ -902,9 +903,8 @@ void SavePackage::GetSerializedHtmlDataForCurrentPageWithLocalLinks() {
 void SavePackage::OnReceivedSerializedHtmlData(const GURL& frame_url,
                                                const std::string& data,
                                                int32 status) {
-  webkit_glue::DomSerializerDelegate::PageSavingSerializationStatus flag =
-      static_cast<webkit_glue::DomSerializerDelegate::
-                      PageSavingSerializationStatus>(status);
+  WebPageSerializerClient::PageSerializationStatus flag =
+      static_cast<WebPageSerializerClient::PageSerializationStatus>(status);
   // Check current state.
   if (wait_state_ != HTML_DATA)
     return;
@@ -912,7 +912,7 @@ void SavePackage::OnReceivedSerializedHtmlData(const GURL& frame_url,
   int id = tab_id();
   // If the all frames are finished saving, we need to close the
   // remaining SaveItems.
-  if (flag == webkit_glue::DomSerializerDelegate::ALL_FRAMES_ARE_FINISHED) {
+  if (flag == WebPageSerializerClient::AllFramesAreFinished) {
     for (SaveUrlItemMap::iterator it = in_progress_items_.begin();
          it != in_progress_items_.end(); ++it) {
       ChromeThread::PostTask(
@@ -950,7 +950,7 @@ void SavePackage::OnReceivedSerializedHtmlData(const GURL& frame_url,
   }
 
   // Current frame is completed saving, call finish in file thread.
-  if (flag == webkit_glue::DomSerializerDelegate::CURRENT_FRAME_IS_FINISHED) {
+  if (flag == WebPageSerializerClient::CurrentFrameIsFinished) {
     ChromeThread::PostTask(
         ChromeThread::FILE, FROM_HERE,
         NewRunnableMethod(file_manager_,
--- chrome/renderer/render_view.h
+++ chrome/renderer/render_view.h
@@ -44,10 +44,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/WebKit/chromium/public/WebFrameClient.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebMediaPlayerAction.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebNode.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebPageSerializerClient.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebTextDirection.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebView.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebViewClient.h"
-#include "webkit/glue/dom_serializer_delegate.h"
 #include "webkit/glue/form_data.h"
 #include "webkit/glue/image_resource_fetcher.h"
 #include "webkit/glue/password_form_dom_manager.h"
@@ -117,8 +117,8 @@ typedef base::RefCountedData<int> SharedRenderViewCounter;
 class RenderView : public RenderWidget,
                    public WebKit::WebViewClient,
                    public WebKit::WebFrameClient,
+                   public WebKit::WebPageSerializerClient,
                    public webkit_glue::WebPluginPageDelegate,
-                   public webkit_glue::DomSerializerDelegate,
                    public base::SupportsWeakPtr<RenderView> {
  public:
   // Visit all RenderViews with a live WebView (i.e., RenderViews that have
@@ -352,6 +352,11 @@ class RenderView : public RenderWidget,
   virtual void reportFindInPageSelection(
       int request_id, int active_match_ordinal, const WebKit::WebRect& sel);
 
+  // webPageSerializerClient
+  virtual void didSerializeDataForFrame(const WebKit::WebURL& frame_url,
+                                        const WebKit::WebCString& data,
+                                        PageSerializationStatus status);
+
   // webkit_glue::WebPluginPageDelegate
   virtual webkit_glue::WebPluginDelegate* CreatePluginDelegate(
       const GURL& url,
@@ -367,10 +372,6 @@ class RenderView : public RenderWidget,
       const gfx::Size& size,
       const std::string& json_arguments,
       std::string* json_retval);
-
-  // DomSerializerDelegate
-  virtual void DidSerializeDataForFrame(const GURL& frame_url,
-      const std::string& data, PageSavingSerializationStatus status);
 
   // Do not delete directly.  This class is reference counted.
   virtual ~RenderView();
