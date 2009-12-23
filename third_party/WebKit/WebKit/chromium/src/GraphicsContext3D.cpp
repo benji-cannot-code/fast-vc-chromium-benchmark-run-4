4d1c4bf641b80e895fed35bee92d536081782a31
agl@chromium.org <agl@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-23 01:34:28 +0000

    2009-12-22  Adam Langley  <agl@google.com>
            Build fix: not reviewed.
    
            r52504 (https://bugs.webkit.org/show_bug.cgi?id=32845) broke the
            Chromium build on Windows and Mac due to a misplaced #endif. The
            buildbot didn't find the problem because it runs on Linux.
    
            Have verified this patch with the author of the above.
    
            * src/GraphicsContext3D.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52514 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/GraphicsContext3D.cpp
+++ third_party/WebKit/WebKit/chromium/src/GraphicsContext3D.cpp
@@ -290,9 +290,9 @@ private:
 };
 
 bool GraphicsContext3DInternal::s_initializedGLEW = false;
+
 #if PLATFORM(LINUX)
 GraphicsContext3DInternal::GLConnection* GraphicsContext3DInternal::s_gl = 0;
-#endif
 
 GraphicsContext3DInternal::GLConnection* GraphicsContext3DInternal::GLConnection::create()
 {
@@ -352,6 +352,8 @@ GraphicsContext3DInternal::GLConnection::~GLConnection()
     XCloseDisplay(m_display);
     dlclose(m_libGL);
 }
+
+#endif  // PLATFORM(LINUX)
 
 GraphicsContext3DInternal::VertexAttribPointerState::VertexAttribPointerState()
     : enabled(false)
