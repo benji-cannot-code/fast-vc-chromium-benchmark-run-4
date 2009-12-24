d3b32596e0ab993e0a60fef565c075a916aeb8bc
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-24 12:31:45 +0000

    DevTools: Add support for setting delegation to inspector controller. TBR=yurys
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35260 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/inspector_controller_impl.js
+++ webkit/glue/devtools/js/inspector_controller_impl.js
@@ -31,6 +31,7 @@ devtools.InspectorBackendImpl = function() {
   this.installInspectorControllerDelegate_('removeAttribute');
   this.installInspectorControllerDelegate_('removeDOMStorageItem');
   this.installInspectorControllerDelegate_('removeNode');
+  this.installInspectorControllerDelegate_('saveFrontendSettings');
   this.installInspectorControllerDelegate_('setAttribute');
   this.installInspectorControllerDelegate_('setDOMStorageItem');
   this.installInspectorControllerDelegate_('setTextNodeValue');
