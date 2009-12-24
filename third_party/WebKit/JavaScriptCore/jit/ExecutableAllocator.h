be6696a16e4ff756528c996626bce39688851d3f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-24 02:15:44 +0000

    2009-12-23  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Laszlo Gombos.
    
            Include stddef.h for ptrdiff_t
            https://bugs.webkit.org/show_bug.cgi?id=32891
    
            ptrdiff_t is typedef-ed in stddef.h.
            Include stddef.h in jit/ExecutableAllocator.h.
    
            * jit/ExecutableAllocator.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/jit/ExecutableAllocator.h
+++ third_party/WebKit/JavaScriptCore/jit/ExecutableAllocator.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ExecutableAllocator_h
 #define ExecutableAllocator_h
 
+#include <stddef.h> // for ptrdiff_t
 #include <limits>
 #include <wtf/Assertions.h>
 #include <wtf/PassRefPtr.h>
