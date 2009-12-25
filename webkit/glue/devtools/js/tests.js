2eab99c21c4819fc433d87d5b88234b34c73bd39
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-25 15:06:46 +0000

    DevTools: restore devtools tests. BUG=515032 TBR=yurys
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35276 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/tests.js
+++ webkit/glue/devtools/js/tests.js
@@ -1116,14 +1116,11 @@ TestSuite.prototype._waitForScriptPause = function(expectations, callback) {
       WebInspector,
       'pausedScript',
       function(callFrames) {
-        test.assertEquals(expectations.functionsOnStack.length,
-                          callFrames.length,
-                          'Unexpected stack depth');
-
         var functionsOnStack = [];
         for (var i = 0; i < callFrames.length; i++) {
           functionsOnStack.push(callFrames[i].functionName);
         }
+
         test.assertEquals(
             expectations.functionsOnStack.join(','),
             functionsOnStack.join(','), 'Unexpected stack.');
@@ -1599,8 +1596,8 @@ TestSuite.prototype._findChildProperty = function(
 TestSuite.prototype._hookGetPropertiesCallback = function(hook, code) {
   var orig = InjectedScriptAccess.getProperties;
   InjectedScriptAccess.getProperties = function(objectProxy,
-      ignoreHasOwnProperty, callback) {
-    orig.call(InjectedScriptAccess, objectProxy, ignoreHasOwnProperty,
+      ignoreHasOwnProperty, abbreviate, callback) {
+    orig.call(InjectedScriptAccess, objectProxy, ignoreHasOwnProperty, abbreviate,
         function() {
           callback.apply(this, arguments);
           hook();
