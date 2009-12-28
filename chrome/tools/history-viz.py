a327a9040f8b87bb81d8ca570375c6718a3dc2d3
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-28 16:41:51 +0000

    Add some licensing comments under chrome/.
    BUG=28291
    
    Review URL: http://codereview.chromium.org/507067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35301 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/tools/history-viz.py
+++ chrome/tools/history-viz.py
@@ -1,7 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/python
-#
-# Copyright 2008 Google Inc. All Rights Reserved.
+# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 """Process a History database and dump a .dot file suitable for GraphViz.
 
