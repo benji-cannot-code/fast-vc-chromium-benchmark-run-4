992686e554ec5ab6ce1a8602c246575b43dd48e4
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-28 17:35:14 +0000

    2009-12-28  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            [bzt] Add an edit-changelog command
            https://bugs.webkit.org/show_bug.cgi?id=32986
    
            This command makes it easier to edit ChangeLogs.  It's similar to
            prepare-ChangeLog -o, except it works with already existing ChangeLogs.
    
            * Scripts/modules/commands/upload.py:
            * Scripts/modules/commands/upload_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/commands/upload.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/commands/upload.py
@@ -125,6 +125,14 @@ class CreateReview(AbstractSequencedCommmand):
         return { "bug_id" : bug_id }
 
 
+class EditChangeLog(AbstractSequencedCommmand):
+    name = "edit-changelog"
+    help_text = "Opens modified ChangeLogs in $EDITOR"
+    steps = [
+        EditChangeLogStep,
+    ]
+
+
 class PostCommits(Command):
     name = "post-commits"
     show_in_main_help = True
