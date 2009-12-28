9ebe02ef889642688331937184b6236bf0e0a427
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-28 23:29:19 +0000

    Authorization headers set using XHR with ChromeFrame were stripped in the outgoing HTTP requests sent via the host network stack.
    Fix is to remove the authorization header from the list of filtered headers.
    
    Added a unit test for this.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=23103
    
    Bug=23103
    Test=Covered by unit test.
    
    Review URL: http://codereview.chromium.org/519013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35319 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/url_request_automation_job.cc
+++ chrome/browser/automation/url_request_automation_job.cc
@@ -25,7 +25,6 @@ using base::TimeDelta;
 // StartAsync(). These must be lower case.
 static const char* kFilteredHeaderStrings[] = {
   "accept",
-  "authorization",
   "cache-control",
   "connection",
   "cookie",
