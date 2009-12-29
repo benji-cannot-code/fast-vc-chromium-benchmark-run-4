16565d0e8c42c2bd1f1e425814f98ea41c181127
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-29 06:33:19 +0000

    2009-12-28  Kinuko Yasuda  <kinuko@chromium.org>
            Reviewed by Maciej Stachowiak.
    
            Fixed isKeypadEvent behavior for chromium/Mac to correctly report
            keyLocation as DOM_KEY_LOCATION_NUMPAD for numeric pad key events.
            https://bugs.webkit.org/show_bug.cgi?id=28247
    
            Test: fast/events/keydown-numpad-keys.html
    
            * src/mac/WebInputEventFactory.mm:
            (WebKit::isKeypadEvent):
    2009-12-28  Kinuko Yasuda  <kinuko@chromium.org>
    
            Reviewed by Maciej Stachowiak.
    
            Fixed isKeypadEvent behavior for Mac to correctly report keyLocation
            as DOM_KEY_LOCATION_NUMPAD for numeric pad key events.
            https://bugs.webkit.org/show_bug.cgi?id=28247
    
            Test: fast/events/keydown-numpad-keys.html
    
            * platform/mac/KeyEventMac.mm:
            (WebCore::isKeypadEvent):
    2009-12-28  Kinuko Yasuda  <kinuko@chromium.org>
    
            Reviewed by Maciej Stachowiak.
    
            Added a layout-test that tests if numeric pad arrow key
            events are correctly reported with keyLocation as
            DOM_KEY_LOCATION_NUMPAD.  Currently this test only passes on Mac.
            https://bugs.webkit.org/show_bug.cgi?id=28247
    
            * fast/events/keydown-numpad-keys-expected.txt: Added.
            * fast/events/keydown-numpad-keys.html: Added.
            * fast/events/script-tests/keydown-numpad-keys.js: Added.
            (recordKeyEvent):
            (testKeyEventWithLocation):
    
            * platform/gtk/Skipped: fast/events/keydown-numpad-keys.html added.
            * platform/qt/Skipped: fast/events/keydown-numpad-keys.html added.
            * platform/win/Skipped: fast/events/keydown-numpad-keys.html added.
    2009-12-28  Kinuko Yasuda  <kinuko@chromium.org>
    
            Reviewed by Maciej Stachowiak.
    
            Extended EventSender.keyDown method to take another (3rd) argument
            for indicating the keyLocation to make numeric pad key events testable.
            Expected values for the argument is one of the KeyLocationCode
            specified in DOM Level 3
            (http://www.w3.org/TR/DOM-Level-3-Events/#events-keyboardevents).
            https://bugs.webkit.org/show_bug.cgi?id=28247
    
            Test: fast/events/keydown-numpad-keys.html
    
            * DumpRenderTree/mac/EventSendingController.mm:
            (+[EventSendingController isSelectorExcludedFromWebScript:]):
            (+[EventSendingController webScriptNameForSelector:]):
            (-[EventSendingController keyDown:withModifiers:withLocation:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52608 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/EventSendingController.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/EventSendingController.mm
@@ -126,7 +126,7 @@ BOOL replayingSavedEvents;
             || aSelector == @selector(contextClick)
             || aSelector == @selector(enableDOMUIEventLogging:)
             || aSelector == @selector(fireKeyboardEventsToElement:)
-            || aSelector == @selector(keyDown:withModifiers:)
+            || aSelector == @selector(keyDown:withModifiers:withLocation:)
             || aSelector == @selector(leapForward:)
             || aSelector == @selector(mouseDown:)
             || aSelector == @selector(mouseMoveToX:Y:)
@@ -155,7 +155,7 @@ BOOL replayingSavedEvents;
         return @"enableDOMUIEventLogging";
     if (aSelector == @selector(fireKeyboardEventsToElement:))
         return @"fireKeyboardEventsToElement";
-    if (aSelector == @selector(keyDown:withModifiers:))
+    if (aSelector == @selector(keyDown:withModifiers:withLocation:))
         return @"keyDown";
     if (aSelector == @selector(leapForward:))
         return @"leapForward";
@@ -475,7 +475,7 @@ static NSEventType eventTypeForMouseButtonAndAction(int button, MouseAction acti
     savedMouseEvents = nil;
 }
 
-- (void)keyDown:(NSString *)character withModifiers:(WebScriptObject *)modifiers
+- (void)keyDown:(NSString *)character withModifiers:(WebScriptObject *)modifiers withLocation:(unsigned long)keyLocation
 {
     NSString *eventCharacter = character;
     if ([character isEqualToString:@"leftArrow"]) {
@@ -538,6 +538,9 @@ static NSEventType eventTypeForMouseButtonAndAction(int button, MouseAction acti
                 modifierFlags |= NSCommandKeyMask;
         }
     }
+
+    if (keyLocation == DOM_KEY_LOCATION_NUMPAD)
+        modifierFlags |= NSNumericPadKeyMask;
 
     [[[mainFrame frameView] documentView] layout];
 
