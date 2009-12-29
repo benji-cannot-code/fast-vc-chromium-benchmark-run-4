75cce995e14b16a2f23610752d961e7c8425ce7e
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-29 00:59:31 +0000

    Clarify licenses on a bunch of files.
    In this process, I also filed bugs on various projects we depend
    on that also have unclear licenses.
    
    BUG=28291
    
    Review URL: http://codereview.chromium.org/521009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35323 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_x11.h
+++ webkit/tools/test_shell/test_shell_x11.h
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef WEBKIT_TOOLS_TEST_SHELL_TEST_SHELL_X11_H
 #define WEBKIT_TOOLS_TEST_SHELL_TEST_SHELL_X11_H
 
