2abe883cbb8468f84ad5447970e12a5c9948ff71
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-30 18:57:38 +0000

    Added pause button to DownloadItemMac via IB and IBAction to receive the event. BUG=23069 TEST=Download large file, click pause in download item's menu to pause/unpause.
    Review URL: http://codereview.chromium.org/524009
    Patch from sig11@reprehensible.net.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35384 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/download_item_controller.h
+++ chrome/browser/cocoa/download_item_controller.h
@@ -99,6 +99,7 @@ class DownloadShelfContextMenuMac;
 - (IBAction)handleAlwaysOpen:(id)sender;
 - (IBAction)handleReveal:(id)sender;
 - (IBAction)handleCancel:(id)sender;
+- (IBAction)handleTogglePause:(id)sender;
 
 @end
 
