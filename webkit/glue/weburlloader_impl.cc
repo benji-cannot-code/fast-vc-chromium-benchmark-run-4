84f4710f663beb97c62a95320570962178155cfc
japhet@chromium.org <japhet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-30 16:45:35 +0000

    If we receive a redirect response, we should copy the http referer field from the old request.
    BUG=7357
    TEST=none
    
    Review URL: http://codereview.chromium.org/470010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35379 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/weburlloader_impl.cc
+++ webkit/glue/weburlloader_impl.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_util.h"
 #include "net/http/http_response_headers.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebHTTPHeaderVisitor.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebSecurityPolicy.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebURL.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebURLError.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebURLLoaderClient.h"
@@ -33,7 +34,9 @@ using base::TimeDelta;
 using WebKit::WebData;
 using WebKit::WebHTTPBody;
 using WebKit::WebHTTPHeaderVisitor;
+using WebKit::WebSecurityPolicy;
 using WebKit::WebString;
+using WebKit::WebURL;
 using WebKit::WebURLError;
 using WebKit::WebURLLoader;
 using WebKit::WebURLLoaderClient;
@@ -414,6 +417,12 @@ bool WebURLLoaderImpl::Context::OnReceivedRedirect(
   // request that resulted from the redirect.
   WebURLRequest new_request(new_url);
   new_request.setFirstPartyForCookies(request_.firstPartyForCookies());
+
+  WebString referrer_string = WebString::fromUTF8("Referer");
+  WebString referrer = request_.httpHeaderField(referrer_string);
+  if (!WebSecurityPolicy::shouldHideReferrer(new_url, referrer))
+    new_request.setHTTPHeaderField(referrer_string, referrer);
+
   if (response.httpStatusCode() == 307)
     new_request.setHTTPMethod(request_.httpMethod());
 
