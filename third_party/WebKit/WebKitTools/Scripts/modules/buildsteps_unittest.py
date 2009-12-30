e3a5c815f6b80ad908a5587ec10668ad2e3af64c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-30 04:19:58 +0000

    2009-12-29  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            [bzt] Don't crash if user types a number during PromptForBugOrTitleStep
            https://bugs.webkit.org/show_bug.cgi?id=33038
    
            Simple fix with test.
    
            * Scripts/modules/buildsteps.py:
            * Scripts/modules/buildsteps_unittest.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52640 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/modules/buildsteps_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/modules/buildsteps_unittest.py
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
-from modules.buildsteps import UpdateChangeLogsWithReviewerStep, UpdateStep
+from modules.buildsteps import UpdateChangeLogsWithReviewerStep, UpdateStep, PromptForBugOrTitleStep
 from modules.mock_bugzillatool import MockBugzillaTool
 from modules.outputcapture import OutputCapture
 from modules.mock import Mock
@@ -44,12 +44,21 @@ class UpdateChangeLogsWithReviewerStepTest(unittest.TestCase):
 
 
 class StepsTest(unittest.TestCase):
-    def _run_step(self, step, options, state=None):
+    def _run_step(self, step, tool=None, options=None, state=None):
+        if not tool:
+            tool = MockBugzillaTool()
+        if not options:
+            options = Mock()
         if not state:
             state = {}
-        step(MockBugzillaTool(), options).run(state)
+        step(tool, options).run(state)
 
     def test_update_step(self):
         options = Mock()
         options.update = True
         self._run_step(UpdateStep, options)
+
+    def test_prompt_for_bug_or_title_step(self):
+        tool = MockBugzillaTool()
+        tool.user.prompt = lambda message: 42
+        self._run_step(PromptForBugOrTitleStep, tool=tool)
