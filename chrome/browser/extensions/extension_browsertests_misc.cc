0a3b5ec53c4f5b1e78e262f0124005942afeccd5
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-30 21:25:05 +0000

    Roll webkit 52633 -> 52667.
    Rebaseline a bunch of SVG test broken to due webkit r52647.
    
    BUG=25977,31028,29737,31350
    TEST=mostly green layout test try runs.
    
    Review URL: http://codereview.chromium.org/515067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35391 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_browsertests_misc.cc
+++ chrome/browser/extensions/extension_browsertests_misc.cc
@@ -484,7 +484,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, ParseFeedInvalidFeed3) {
 }
 
 // Tests that message passing between extensions and tabs works.
-IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, MessagingExtensionTab) {
+IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, DISABLED_MessagingExtensionTab) {
   ASSERT_TRUE(LoadExtension(
       test_data_dir_.AppendASCII("good").AppendASCII("Extensions")
                     .AppendASCII("bjafgdebaacbbbecmhlhpofkepfkgcpa")
@@ -543,7 +543,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, LastError) {
 }
 
 // Tests that message passing between extensions and content scripts works.
-IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, MessagingContentScript) {
+IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, DISABLED_MessagingContentScript) {
   HTTPTestServer* server = StartHTTPServer();
 
   ASSERT_TRUE(LoadExtension(
