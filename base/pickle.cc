3709ded08466f742dece333802f4f39929298b7f
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-30 20:09:02 +0000

    Defense-in-depth: initialize some variables.
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/517025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35387 0039d316-1c4b-4281-b951-d872f2087c98

--- base/pickle.cc
+++ base/pickle.cc
@@ -264,6 +264,7 @@ bool Pickle::ReadString16(void** iter, string16* result) const {
 bool Pickle::ReadBytes(void** iter, const char** data, int length) const {
   DCHECK(iter);
   DCHECK(data);
+  *data = 0;
 
   if (!IteratorHasRoomFor(*iter, length))
     return false;
@@ -278,6 +279,8 @@ bool Pickle::ReadData(void** iter, const char** data, int* length) const {
   DCHECK(iter);
   DCHECK(data);
   DCHECK(length);
+  *length = 0;
+  *data = 0;
 
   if (!ReadLength(iter, length))
     return false;
