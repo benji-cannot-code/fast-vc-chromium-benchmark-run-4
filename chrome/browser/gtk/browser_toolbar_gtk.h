5a9474e9015601bf298193075daee98922c04bb5
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-31 02:08:06 +0000

    GTK: Remove OwnedWidgetGtk wrapper for the GtkMenu* in MenuGtk.
    The "execute the command later" workaround, which was in place to avoid OwnedWidget-DCHECKing when selecting menu items that might cause cascading object-destruction effects, caused problems with the Browser::SetBlockCommandExecution() workaround, the reasoning for which can be found in BrowserWindowGtk and is reproduced here:
    
        // This piece of code is based on the fact that calling
        // gtk_window_activate_key() method against |window_| may only trigger a
        // browser command execution, by matching either a global accelerator
        // defined in above |kAcceleratorMap| or the accelerator key of a menu
        // item defined in chrome/browser/gtk/standard_menus.cc.
        //
        // Here we need to retrieve the command id (if any) associated to the
        // keyboard event. Instead of looking up the command id in above
        // |kAcceleratorMap| table by ourselves, we block the command execution of
        // the |browser_| object then send the keyboard event to the |window_| by
        // calling gtk_window_activate_key() method, as if we are activating an
        // accelerator key. Then we can retrieve the command id from the
        // |browser_| object.
        //
        // Pros of this approach:
        // 1. We can handle accelerators defined not only in above
        //    |kAcceleratorMap| table, but also those in standard_menus.cc.
        // 2. We don't need to care about keyboard layout problem, as
        //    gtk_window_activate_key() method handles it for us.
        //
        // Cons:
        // 1. The logic is a little complicated.
        // 2. We should be careful not to introduce any accelerators that trigger
        //    customized code instead of browser commands.
    
    The easiest and cleanest thing is just to remove the OwnedWidgetGtk.
    
    BUG=none
    TEST=interactive ui tests pass again
    
    Review URL: http://codereview.chromium.org/522028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35404 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_toolbar_gtk.h
+++ chrome/browser/gtk/browser_toolbar_gtk.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_registrar.h"
+#include "chrome/common/owned_widget_gtk.h"
 #include "chrome/common/pref_member.h"
 
 class BackForwardButtonGtk;
