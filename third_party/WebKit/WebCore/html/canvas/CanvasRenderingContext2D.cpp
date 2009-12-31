43fd932ece5b7fccb0d9aca98418bf493ea66bd5
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2009-12-31 18:07:03 +0000

    2009-12-31  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            Do not include Frame.h under WebCore/html/canvas
            https://bugs.webkit.org/show_bug.cgi?id=33082
    
            No new tests, as there is no new functionality.
    
            * html/canvas/CanvasRenderingContext2D.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52682 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "ExceptionCode.h"
 #include "FloatConversion.h"
-#include "Frame.h"
 #include "GraphicsContext.h"
 #include "HTMLCanvasElement.h"
 #include "HTMLImageElement.h"
