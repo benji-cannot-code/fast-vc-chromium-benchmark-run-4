1add7ffd64e031130e796a8a704e05fe7ba70112
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-04 12:01:40 +0000

    2010-01-04  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Adam Barth.
    
            Document CPU() macros in comments.
            https://bugs.webkit.org/show_bug.cgi?id=33147
    
            * wtf/Platform.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -58,8 +58,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* ==== COMPILER() - the compiler being used to build the project ==== */
 
-/* COMPILER(MSVC) */
-/* COMPILER(MSVC7) */
+/* COMPILER(MSVC) Microsoft Visual C++ */
+/* COMPILER(MSVC7) Microsoft Visual C++ v7 or lower*/
 #if defined(_MSC_VER)
 #define WTF_COMPILER_MSVC 1
 #if _MSC_VER < 1400
@@ -67,24 +67,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif
 
-/* COMPILER(RVCT) */
+/* COMPILER(RVCT)  - ARM RealView Compilation Tools */
 #if defined(__CC_ARM) || defined(__ARMCC__)
 #define WTF_COMPILER_RVCT 1
 #endif
 
-/* COMPILER(GCC) */
+/* COMPILER(GCC) - GNU Compiler Collection */
 /* --gnu option of the RVCT compiler also defines __GNUC__ */
 #if defined(__GNUC__) && !COMPILER(RVCT)
 #define WTF_COMPILER_GCC 1
 #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #endif
 
-/* COMPILER(MINGW) */
+/* COMPILER(MINGW) - MinGW GCC */
 #if defined(MINGW) || defined(__MINGW32__)
 #define WTF_COMPILER_MINGW 1
 #endif
 
-/* COMPILER(WINSCW) */
+/* COMPILER(WINSCW) - CodeWarrior for Symbian emulator */
 #if defined(__WINSCW__)
 #define WTF_COMPILER_WINSCW 1
 #endif
