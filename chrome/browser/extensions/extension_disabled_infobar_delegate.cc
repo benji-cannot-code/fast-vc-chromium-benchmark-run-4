8958e1bcd15bdb488e61bbe0cf2d033eac67aa82
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-04 21:28:57 +0000

    Permission escalation when extension updates should trigger the install warning when enabling the extension through the Extensions Management UI.
    TBR=mpcomplete
    BUG=http://crbug.com/30752
    TEST=This requires an extension that autoupdates _and_ increases its
    permissions. If you ignore the infobar that says it has been disabled, you
    should still get the Install Warning dialog when you try to enable the
    extension.
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35480 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_disabled_infobar_delegate.cc
+++ chrome/browser/extensions/extension_disabled_infobar_delegate.cc
@@ -39,6 +39,8 @@ class ExtensionDisabledDialogDelegate
 
   // ExtensionInstallUI::Delegate
   virtual void InstallUIProceed() {
+    ExtensionPrefs* prefs = service_->extension_prefs();
+    prefs->SetShowInstallWarningOnEnable(extension_, false);
     service_->EnableExtension(extension_->id());
     Release();
   }
@@ -157,4 +159,10 @@ void ShowExtensionDisabledUI(ExtensionsService* service, Profile* profile,
 
   tab_contents->AddInfoBar(new ExtensionDisabledInfobarDelegate(
       tab_contents, service, extension));
+}
+
+void ShowExtensionDisabledDialog(ExtensionsService* service, Profile* profile,
+                                 Extension* extension) {
+  // This object manages its own lifetime.
+  new ExtensionDisabledDialogDelegate(profile, service, extension);
 }
--- chrome/browser/extensions/extension_disabled_infobar_delegate.h
+++ chrome/browser/extensions/extension_disabled_infobar_delegate.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "chrome/browser/extensions/extension_install_ui.h"
+#include "chrome/common/extensions/extension_resource.h"
+
 class Extension;
 class ExtensionsService;
 class Profile;
@@ -16,5 +19,9 @@ class Profile;
 // to higher permissions.
 void ShowExtensionDisabledUI(ExtensionsService* service, Profile* profile,
                              Extension* extension);
+
+// Shows the extension install dialog.
+void ShowExtensionDisabledDialog(ExtensionsService* service, Profile* profile,
+                                 Extension* extension);
 
 #endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_DISABLED_INFOBAR_DELEGATE_H_
