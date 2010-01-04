fe1486c3787d690431ae6b69eda75386121678e2
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-04 22:42:25 +0000

    * wtf/HashFunctions.h:
    Reviewed by NOBODY build fix for DRT, fix incorrect style include.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52761 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/HashFunctions.h
+++ third_party/WebKit/JavaScriptCore/wtf/HashFunctions.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_HashFunctions_h
 
 #include "RefPtr.h"
-#include "wtf/unicode/Unicode.h"
+#include "Unicode.h"
 #include <stdint.h>
 
 namespace WTF {
