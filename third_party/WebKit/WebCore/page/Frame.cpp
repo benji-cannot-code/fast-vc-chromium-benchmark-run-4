3faa74faa79af24896ed36e4e013cbece6f43969
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-04 21:49:01 +0000

    WebCore: Reentrancy problem with selection in some edge cases. https://bugs.webkit.org/show_bug.cgi?id=32842 rdar://problem/7449974
    Reviewed by Dan Bernstein.
    
    Test: fast/forms/selection-layout-reentry-strange-case.html
    
    Move the selection display update process done in the
    selectionLayoutChanged function into the layout timer
    instead of doing it immediately when selection changes occur.
    
    * dom/Document.cpp:
    (WebCore::Document::updateLayout): Changed this to use the
    definition of needsLayout from FrameView rather than rolling
    its own.
    
    * editing/SelectionController.cpp:
    (WebCore::SelectionController::SelectionController):
    Initialize m_needsDisplayUpdate to false.
    (WebCore::SelectionController::setSelection): Call
    the new setNeedsDisplayUpdate function instead of the old
    badly named Frame::selectionLayoutChanged function.
    (WebCore::SelectionController::setNeedsDisplayUpdate):
    Set m_needsDisplayUpdate. If it is just becoming true, then
    call FrameView::scheduleRelayout.
    
    * editing/SelectionController.h: Added setNeedsDisplayUpdate,
    needsDisplayUpdate, and m_needsDisplayUpdate.
    
    * page/Frame.cpp:
    (WebCore::Frame::setCaretVisible): Call setNeedsDisplayUpdate.
    (WebCore::Frame::selectionLayoutChanged): Call
    setNeedsDisplayUpdate to set it to false, since this is the
    function that performs "selection display update". Later I want
    to rename this function. Also added a global reentrancy check
    since that's the easiest way I could think of to prevent infinite
    recursion in the case where recomputeCaretRect ends up triggering
    a layout. As a side effect, fixed theoretical problems in
    TEXT_CARET builds by improving the ifdef.
    (WebCore::Frame::caretBlinkTimerFired): Loosen assertions a
    bit. Later we might want to decouple caret rect from caret state
    a bit more and add these assertions back.
    (WebCore::Frame::selectionBounds): Call Document::updateLayout.
    This function is for external clients and they do not currently
    do anything to make sure layout is up to date.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::needsLayout): Add a new clause, since
    we need a call to layout if needsDisplayUpdate is true.
    
    LayoutTests: Reentrancy problem with selection in some edge cases. 
    https://bugs.webkit.org/show_bug.cgi?id=32842 
    rdar://problem/7449974 
    
    Reviewed by Dan Bernstein.
    
    * fast/forms/selection-layout-reentry-strange-case-expected.txt: Added.
    * fast/forms/selection-layout-reentry-strange-case.html: Added.
    * platform/mac/accessibility/frame-with-title-expected.txt: Updated since 
    the number of layouts is now different. 
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *                     2000 Simon Hausmann <hausmann@kde.org>
  *                     2000 Stefan Schimanski <1Stein@gmx.de>
  *                     2001 George Staikos <staikos@kde.org>
- * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
  * Copyright (C) 2005 Alexey Proskuryakov <ap@nypop.com>
  * Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies)
  * Copyright (C) 2008 Eric Seidel <eric@webkit.org>
@@ -564,7 +564,7 @@ void Frame::setCaretVisible(bool flag)
         return;
     clearCaretRectIfNeeded();
     m_caretVisible = flag;
-    selectionLayoutChanged();
+    selection()->setNeedsDisplayUpdate();
 }
 
 void Frame::clearCaretRectIfNeeded()
@@ -631,9 +631,17 @@ void Frame::setFocusedNodeIfNeeded()
 
 void Frame::selectionLayoutChanged()
 {
-    bool caretRectChanged = selection()->recomputeCaretRect();
+    selection()->setNeedsDisplayUpdate(false);
 
 #if ENABLE(TEXT_CARET)
+    static bool alreadyInSelectionLayoutChanged;
+    if (alreadyInSelectionLayoutChanged)
+        return;
+
+    alreadyInSelectionLayoutChanged = true;
+
+    bool caretRectChanged = selection()->recomputeCaretRect();
+
     bool caretBrowsing = settings() && settings()->caretBrowsingEnabled();
     bool shouldBlink = m_caretVisible
         && selection()->isCaret() && (selection()->isContentEditable() || caretBrowsing);
@@ -654,9 +662,8 @@ void Frame::selectionLayoutChanged()
             selection()->invalidateCaretRect();
         }
     }
-#else
-    if (!caretRectChanged)
-        return;
+
+    alreadyInSelectionLayoutChanged = false;
 #endif
 
     RenderView* view = contentRenderer();
@@ -692,8 +699,10 @@ void Frame::selectionLayoutChanged()
 void Frame::caretBlinkTimerFired(Timer<Frame>*)
 {
 #if ENABLE(TEXT_CARET)
-    ASSERT(m_caretVisible);
-    ASSERT(selection()->isCaret());
+    if (!m_caretVisible || !selection()->isCaret()) {
+        ASSERT(selection()->needsDisplayUpdate());
+        return;
+    }
     bool caretPaint = m_caretPaint;
     if (selection()->isCaretBlinkingSuspended() && caretPaint)
         return;
@@ -1253,6 +1262,8 @@ void Frame::setExcludeFromTextSearch(bool exclude)
 // returns FloatRect because going through IntRect would truncate any floats
 FloatRect Frame::selectionBounds(bool clipToVisibleContent) const
 {
+    m_doc->updateLayout();
+
     RenderView* root = contentRenderer();
     FrameView* view = m_view.get();
     if (!root || !view)
