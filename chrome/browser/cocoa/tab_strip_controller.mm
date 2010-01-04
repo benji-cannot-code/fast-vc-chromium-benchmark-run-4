063fe236025a7f1eef696a54819b4578edca06e4
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-04 15:43:20 +0000

    Mac: fix crashes related to detaching tabs.
    There are actually two problems, with two fixes:
    - Remove TabStripController as observer when removing a tab. (Otherwise, there's a race condition which can result in a stale observation.)
    - Make sure the TabController (and its TabView) aren't deleted while handling a torn-off tab.
    
    BUG=31138
    TEST=Attach/re-attach/move/close tabs under a variety of circumstances (see in particular the situation described in the bug). Do so till the cows come home. It should hopefully not crash. (Perhaps also check a debug build, and make sure no assertions fail.)
    
    Review URL: http://codereview.chromium.org/515066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35455 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_strip_controller.mm
+++ chrome/browser/cocoa/tab_strip_controller.mm
@@ -948,8 +948,8 @@ private:
   }
 }
 
-// Remove all knowledge about this tab and it's associated
-// controller and remove the view from the strip.
+// Remove all knowledge about this tab and its associated controller, and remove
+// the view from the strip.
 - (void)removeTab:(TabController*)controller {
   NSUInteger index = [tabArray_ indexOfObject:controller];
 
@@ -962,6 +962,12 @@ private:
   // Remove the view from the tab strip.
   NSView* tab = [controller view];
   [tab removeFromSuperview];
+
+  // Remove ourself as an observer.
+  [[NSNotificationCenter defaultCenter]
+      removeObserver:self
+                name:NSViewDidUpdateTrackingAreasNotification
+              object:tab];
 
   // Clear the tab controller's target.
   // TODO(viettrungluu): [crbug.com/23829] Find a better way to handle the tab
