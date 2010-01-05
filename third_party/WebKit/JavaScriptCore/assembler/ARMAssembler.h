c9e05906311dca55c14c61353ccfad28ee965c26
loki@webkit.org <loki@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-05 15:53:17 +0000

    Follow r52729 in ARMAssembler. https://bugs.webkit.org/show_bug.cgi?id=33208
    Reviewed by Maciej Stachowiak.
    
    Use WTF_ARM_ARCH_AT_LEAST instead of ARM_ARCH_VERSION
    
    * assembler/ARMAssembler.cpp:
    (JSC::ARMAssembler::encodeComplexImm): Move tmp declaration to ARMv7
    * assembler/ARMAssembler.h:
    (JSC::ARMAssembler::):
    (JSC::ARMAssembler::bkpt):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52797 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/assembler/ARMAssembler.h
+++ third_party/WebKit/JavaScriptCore/assembler/ARMAssembler.h
@@ -139,11 +139,11 @@ namespace JSC {
             FSITOD = 0x0eb80bc0,
             FTOSID = 0x0ebd0b40,
             FMSTAT = 0x0ef1fa10,
-#if ARM_ARCH_VERSION >= 5
+#if WTF_ARM_ARCH_AT_LEAST(5)
             CLZ = 0x016f0f10,
             BKPT = 0xe120070,
 #endif
-#if ARM_ARCH_VERSION >= 7
+#if WTF_ARM_ARCH_AT_LEAST(7)
             MOVW = 0x03000000,
             MOVT = 0x03400000,
 #endif
@@ -343,7 +343,7 @@ namespace JSC {
             emitInst(static_cast<ARMWord>(cc) | MOV, rd, ARMRegisters::r0, op2);
         }
 
-#if ARM_ARCH_VERSION >= 7
+#if WTF_ARM_ARCH_AT_LEAST(7)
         void movw_r(int rd, ARMWord op2, Condition cc = AL)
         {
             ASSERT((op2 | 0xf0fff) == 0xf0fff);
@@ -531,7 +531,7 @@ namespace JSC {
             m_buffer.putInt(static_cast<ARMWord>(cc) | FMSTAT);
         }
 
-#if ARM_ARCH_VERSION >= 5
+#if WTF_ARM_ARCH_AT_LEAST(5)
         void clz_r(int rd, int rm, Condition cc = AL)
         {
             m_buffer.putInt(static_cast<ARMWord>(cc) | CLZ | RD(rd) | RM(rm));
@@ -540,7 +540,7 @@ namespace JSC {
 
         void bkpt(ARMWord value)
         {
-#if ARM_ARCH_VERSION >= 5
+#if WTF_ARM_ARCH_AT_LEAST(5)
             m_buffer.putInt(BKPT | ((value & 0xff0) << 4) | (value & 0xf));
 #else
             // Cannot access to Zero memory address
@@ -748,7 +748,7 @@ namespace JSC {
 
         static ARMWord getOp2(ARMWord imm);
 
-#if ARM_ARCH_VERSION >= 7
+#if WTF_ARM_ARCH_AT_LEAST(7)
         static ARMWord getImm16Op2(ARMWord imm)
         {
             if (imm <= 0xffff)
