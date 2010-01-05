43a1af7b8ba2e4a3b224f2f394dee0084f736f8e
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-05 03:07:07 +0000

    linux: implement gpu plugin
    Review URL: http://codereview.chromium.org/500132
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35509 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/plugin/command_buffer_stub.cc
+++ chrome/plugin/command_buffer_stub.cc
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using gpu::Buffer;
 
 CommandBufferStub::CommandBufferStub(PluginChannel* channel,
-                                     gfx::NativeView view)
+                                     gfx::PluginWindowHandle window)
     : channel_(channel),
-      view_(view) {
+      window_(window) {
   route_id_ = channel->GenerateRouteID();
   channel->AddRoute(route_id_, this, false);
 }
@@ -55,7 +55,7 @@ void CommandBufferStub::OnInitialize(int32 size,
     Buffer buffer = command_buffer_->GetRingBuffer();
     if (buffer.shared_memory) {
       processor_ = new gpu::GPUProcessor(command_buffer_.get());
-      if (processor_->Initialize(view_)) {
+      if (processor_->Initialize(window_)) {
         command_buffer_->SetPutOffsetChangeCallback(
             NewCallback(processor_.get(),
                         &gpu::GPUProcessor::ProcessCommands));
@@ -94,7 +94,7 @@ void CommandBufferStub::OnGetTransferBuffer(
     int32 id,
     base::SharedMemoryHandle* transfer_buffer,
     size_t* size) {
-  *transfer_buffer = 0;
+  *transfer_buffer = base::SharedMemoryHandle();
   *size = 0;
 
   // Assume service is responsible for duplicating the handle to the calling
--- gpu/command_buffer/service/gpu_processor_linux.cc
+++ gpu/command_buffer/service/gpu_processor_linux.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <windows.h>
-
+#include <gdk/gdkx.h>
 #include "gpu/command_buffer/service/gpu_processor.h"
+#include "gpu/command_buffer/service/x_utils.h"
 
 using ::base::SharedMemory;
 
@@ -15,7 +15,7 @@ bool GPUProcessor::Initialize(gfx::PluginWindowHandle handle) {
   DCHECK(handle);
 
   // Cannot reinitialize.
-  if (decoder_->hwnd() != NULL)
+  if (decoder_->window() != NULL)
     return false;
 
   // Map the ring buffer and create the parser.
@@ -33,15 +33,19 @@ bool GPUProcessor::Initialize(gfx::PluginWindowHandle handle) {
   }
 
   // Initialize GAPI immediately if the window handle is valid.
-  decoder_->set_hwnd(handle);
+  XWindowWrapper *window = new XWindowWrapper(GDK_DISPLAY(), handle);
+  decoder_->set_window_wrapper(window);
   return decoder_->Initialize();
 }
 
 void GPUProcessor::Destroy() {
   // Destroy GAPI if window handle has not already become invalid.
-  if (decoder_->hwnd()) {
+  XWindowWrapper *window = decoder_->window();
+  if (window) {
     decoder_->Destroy();
-    decoder_->set_hwnd(NULL);
+    decoder_->set_window_wrapper(NULL);
+    delete window;
   }
 }
+
 }  // namespace gpu
