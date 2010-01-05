38a1201c0f4fc20b5ad6d9d908e3375227bb7c22
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-05 01:07:26 +0000

    WebCore: Added missing null check in BarInfo code.
    Reviewed by Sam Weinig.
    
    Test: fast/dom/Window/BarInfo-after-frame-removed.html
    
    * page/BarInfo.cpp:
    (WebCore::BarInfo::visible): Added null check. A few other
    cleanups.
    
    LayoutTests: Added test for use of BarInfo object after a frame is no longer
    associated with a web page.
    
    Reviewed by Sam Weinig.
    
    * fast/dom/Window/BarInfo-after-frame-removed-expected.txt: Added.
    * fast/dom/Window/BarInfo-after-frame-removed.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52772 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/BarInfo.cpp
+++ third_party/WebKit/WebCore/page/BarInfo.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007 Apple Inc.  All rights reserved.
+ * Copyright (C) 2007, 2010 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -61,23 +61,25 @@ bool BarInfo::visible() const
 {
     if (!m_frame)
         return false;
+    Page* page = m_frame->page();
+    if (!page)
+        return false;
 
     switch (m_type) {
     case Locationbar:
-        return m_frame->page()->chrome()->toolbarsVisible();
-    case Toolbar:
-        return m_frame->page()->chrome()->toolbarsVisible();
     case Personalbar:
-        return m_frame->page()->chrome()->toolbarsVisible();
+    case Toolbar:
+        return page->chrome()->toolbarsVisible();
     case Menubar:
-        return m_frame->page()->chrome()->menubarVisible();
+        return page->chrome()->menubarVisible();
     case Scrollbars:
-        return m_frame->page()->chrome()->scrollbarsVisible();
+        return page->chrome()->scrollbarsVisible();
     case Statusbar:
-        return m_frame->page()->chrome()->statusbarVisible();
-    default:
-        return false;
+        return page->chrome()->statusbarVisible();
     }
+
+    ASSERT_NOT_REACHED();
+    return false;
 }
 
 } // namespace WebCore
