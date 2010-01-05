829426b6b21e0109679d37317a74650fbf75c313
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-05 04:45:28 +0000

    <rdar://problem/6655695> REGRESSION: VoiceOver is not highlighting some web elements correctly <rdar://problem/7397558> REGRESSION: AXImages inside of AXLink cause AXLink to have wrong bounds
    Reviewed by Simon Fraser.
    
    WebCore: 
    
    Test: accessibility/dimensions-include-descendants.html
    
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::boundingBoxRect): Use absoluteFocusRingQuads()
    for non-text renderers. The focus ring encompasses descendants, which is
    what we want for the accessibility indicator.
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::absoluteFocusRingQuads): Added.
    * rendering/RenderObject.h:
    
    LayoutTests: 
    
    * accessibility/dimensions-include-descendants-expected.txt: Added.
    * accessibility/dimensions-include-descendants.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52783 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AccessibilityRenderObject.cpp
+++ third_party/WebKit/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -1140,7 +1140,10 @@ IntRect AccessibilityRenderObject::boundingBoxRect() const
         obj = obj->node()->renderer();
     
     Vector<FloatQuad> quads;
-    obj->absoluteQuads(quads);
+    if (obj->isText())
+        obj->absoluteQuads(quads);
+    else
+        obj->absoluteFocusRingQuads(quads);
     const size_t n = quads.size();
     if (!n)
         return IntRect();
