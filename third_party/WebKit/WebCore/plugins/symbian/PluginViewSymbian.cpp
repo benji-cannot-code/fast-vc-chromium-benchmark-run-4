d51ffe7360e51fe6fc1b5263ec6d3c93a54614cb
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-05 10:28:35 +0000

    Unreviewed trivial Symbian build fix
    * plugins/symbian/PluginViewSymbian.cpp: Add missing HostWindow.h inclusion
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/symbian/PluginViewSymbian.cpp
+++ third_party/WebKit/WebCore/plugins/symbian/PluginViewSymbian.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 #include "HTMLNames.h"
 #include "HTMLPlugInElement.h"
+#include "HostWindow.h"
 #include "Image.h"
 #include "JSDOMBinding.h"
 #include "KeyboardEvent.h"
