b5476d0f88e15e1b141f8fe4598abdf5aff887b4
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-06 22:27:39 +0000

    sync: Add two functions to sync_ui_util header and update the callers accordingly.
    * ShouldShowSyncErrorButton: added.
    * GetSyncMenuLabel: added.
    
    (original code review: http://codereview.chromium.org/501142 , patch by thiago.farina@gmail.com )
    
    Review URL: http://codereview.chromium.org/519069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35656 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/bookmark_bar_gtk.cc
+++ chrome/browser/gtk/bookmark_bar_gtk.cc
@@ -312,7 +312,7 @@ void BookmarkBarGtk::Show(bool animate) {
       gdk_window_lower(event_box_->window);
   }
 
-  if (ShouldShowSyncErrorButton()) {
+  if (sync_ui_util::ShouldShowSyncErrorButton(sync_service_)) {
     gtk_widget_show(sync_error_button_);
   } else {
     gtk_widget_hide(sync_error_button_);
@@ -344,7 +344,7 @@ void BookmarkBarGtk::Hide(bool animate) {
 }
 
 void BookmarkBarGtk::OnStateChanged() {
-  if (ShouldShowSyncErrorButton()) {
+  if (sync_ui_util::ShouldShowSyncErrorButton(sync_service_)) {
     gtk_widget_show(sync_error_button_);
   } else {
     gtk_widget_hide(sync_error_button_);
@@ -1297,11 +1297,4 @@ void BookmarkBarGtk::PopupForButtonNextTo(GtkWidget* button,
   int shift = dir == GTK_MENU_DIR_PARENT ? -1 : 1;
   button_idx = (button_idx + shift + folder_list.size()) % folder_list.size();
   PopupForButton(folder_list[button_idx]);
-}
-
-// The sync state reported by the profile sync service determines whether or
-// not the re-login indicator button should be visible.
-bool BookmarkBarGtk::ShouldShowSyncErrorButton() {
-  return sync_service_ && sync_service_->HasSyncSetupCompleted() &&
-      (sync_ui_util::GetStatus(sync_service_) == sync_ui_util::SYNC_ERROR);
 }
