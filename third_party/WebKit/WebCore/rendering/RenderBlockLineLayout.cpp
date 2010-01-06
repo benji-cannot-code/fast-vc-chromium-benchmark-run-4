d5fa8caaf740c434d515abb992ad8972c227d837
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-06 01:13:11 +0000

    2010-01-05  James Robinson  <jamesr@google.com>
            Reviewed by Eric Seidel.
    
            Typing in Google Wave repaints the whole screen
            https://bugs.webkit.org/show_bug.cgi?id=32295
    
            Add a repaint test to make sure that when mutations happen in a grandchild of a block element that has no in-flow children
            the entire block element is not unnecessarily repainted.
            The bug only shows up when running pixel tests.
    
            * fast/repaint/block-no-inflow-children.html: Added.
            * platform/mac/fast/repaint/block-no-inflow-children-expected.checksum: Added.
            * platform/mac/fast/repaint/block-no-inflow-children-expected.png: Added.
            * platform/mac/fast/repaint/block-no-inflow-children-expected.txt: Added.
    2010-01-05  James Robinson  <jamesr@google.com>
    
            Reviewed by Eric Seidel.
    
            Typing in Google Wave repaints the whole screen
            https://bugs.webkit.org/show_bug.cgi?id=32295
    
            Marks a RenderBlock without block children for layout and painting when it has no line boxes only if it has inline immediate children.
            The existing logic uses the existence of line boxes as a dirty flag to indicate that line boxes need to be regenerated.  This heuristic
            fails when the RenderBlock does not have any in-flow children at all, causing the RenderBlock to get unnecessarily marked for layout and
            painting.  This is particularly bad for common scrollbar manipulation tricks like putting one positioned div inside another - this
            structure caused the entire outer div's bounds to get scheduled for a paint whenever there was any mutation of any descendant.
    
            Test: LayoutTests/fast/repaint/block-no-inline-children.html (only shows up in pixel tests)
    
            * rendering/RenderBlockLineLayout.cpp:
            (WebCore::RenderBlock::layoutInlineChildren):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52839 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBlockLineLayout.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2000 Lars Knoll (knoll@kde.org)
  * Copyright (C) 2003, 2004, 2006, 2007, 2008, 2009 Apple Inc. All right reserved.
+ * Copyright (C) 2010 Google Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -844,6 +845,7 @@ void RenderBlock::layoutInlineChildren(bool relayoutChildren, int& repaintTop, i
         bool endOfInline = false;
         RenderObject* o = bidiFirst(this, 0, false);
         Vector<FloatWithRect> floats;
+        bool hasInlineChild = false;
         while (o) {
             if (o->isReplaced() || o->isFloating() || o->isPositioned()) {
                 RenderBox* box = toRenderBox(o);
@@ -866,6 +868,7 @@ void RenderBlock::layoutInlineChildren(bool relayoutChildren, int& repaintTop, i
                     o->layoutIfNeeded();
                 }
             } else if (o->isText() || (o->isRenderInline() && !endOfInline)) {
+                hasInlineChild = true;
                 if (fullLayout || o->selfNeedsLayout())
                     dirtyLineBoxesForRenderer(o, fullLayout);
                 o->setNeedsLayout(false);
@@ -882,7 +885,7 @@ void RenderBlock::layoutInlineChildren(bool relayoutChildren, int& repaintTop, i
         bool previousLineBrokeCleanly = true;
         RootInlineBox* startLine = determineStartPosition(firstLine, fullLayout, previousLineBrokeCleanly, resolver, floats, floatIndex);
 
-        if (fullLayout && !selfNeedsLayout()) {
+        if (fullLayout && hasInlineChild && !selfNeedsLayout()) {
             setNeedsLayout(true, false);  // Mark ourselves as needing a full layout. This way we'll repaint like
                                           // we're supposed to.
             RenderView* v = view();
