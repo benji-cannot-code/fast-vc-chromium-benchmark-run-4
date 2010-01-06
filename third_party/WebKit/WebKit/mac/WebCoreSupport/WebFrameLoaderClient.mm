1690ecb167a642ea7b502e9f411d827bf046f480
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-06 23:09:28 +0000

    Attempt to fix non-Snow Leopard Mac build.
    * WebCoreSupport/WebFrameLoaderClient.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52874 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebFrameLoaderClient.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebFrameLoaderClient.mm
@@ -1480,11 +1480,6 @@ static NSView *pluginView(WebFrame *frame, WebPluginPackage *pluginPackage,
 
 class NetscapePluginWidget : public PluginWidget {
 public:
-    NetscapePluginWidget(WebBaseNetscapePluginView *view)
-        : PluginWidget(view)
-    {
-    }
-    
     virtual void handleEvent(Event*)
     {
         Frame* frame = Frame::frameForWidget(this);
