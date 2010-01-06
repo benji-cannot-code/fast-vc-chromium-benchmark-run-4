92a668931991eb331df7941da0f7d0a2bf3ad33f
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-06 08:24:15 +0000

    2010-01-06  Darin Fisher  <darin@chromium.org>
            Fix release build bustage.
    
            * src/WebKit.cpp: Need to include Logging.h
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52852 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebKit.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebKit.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AtomicString.h"
 #include "DOMTimer.h"
+#include "Logging.h"
 #include "Page.h"
 #include "RuntimeEnabledFeatures.h"
 #include "TextEncoding.h"
