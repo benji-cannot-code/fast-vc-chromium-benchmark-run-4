f9210f5fa5fc178fbd72da07472e907567aef146
yong.li@torchmobile.com <yong.li@torchmobile.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-06 18:25:46 +0000

    2010-01-06  Yong Li  <yoli@rim.com>
            Reviewed by Darin Adler.
    
            Let ReourceLoader deliver segmented data
            when loading resource from a SharedBuffer object.
            https://bugs.webkit.org/show_bug.cgi?id=33222
    
            * loader/DocumentLoader.cpp:
            (WebCore::DocumentLoader::substituteResourceDeliveryTimerFired):
            * loader/MainResourceLoader.cpp:
            (WebCore::MainResourceLoader::continueAfterContentPolicy):
            * loader/ResourceLoader.cpp:
            (WebCore::ResourceLoader::didReceiveData):
            * loader/ResourceLoader.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/ResourceLoader.cpp
+++ third_party/WebKit/WebCore/loader/ResourceLoader.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006, 2007 Apple Inc. All rights reserved.
  *           (C) 2007 Graham Dennis (graham.dennis@gmail.com)
+ * Copyright (C) Research In Motion Limited 2009-2010. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -252,6 +253,16 @@ void ResourceLoader::didReceiveData(const char* data, int length, long long leng
     // Could be an issue with a giant local file.
     if (m_sendResourceLoadCallbacks && m_frame)
         frameLoader()->notifier()->didReceiveData(this, data, length, static_cast<int>(lengthReceived));
+}
+
+void ResourceLoader::didReceiveData(const SharedBuffer& data)
+{
+    const char* segment;
+    unsigned offset = 0;
+    while (unsigned length = data.getSomeData(segment, offset)) {
+        offset += length;
+        didReceiveData(segment, static_cast<int>(length), offset, false);
+    }
 }
 
 void ResourceLoader::willStopBufferingData(const char* data, int length)
