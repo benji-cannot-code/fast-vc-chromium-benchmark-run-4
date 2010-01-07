caef60be12e013e81bfc4ec68161d52b9d9eca5b
kdecker@apple.com <kdecker@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-07 20:26:04 +0000

    Reviewed by Simon Fraser.
            https://bugs.webkit.org/show_bug.cgi?id=33331   
            (WebHostedNetscapePluginView should pass acceleratedCompositingEnabled bool to NetscapePluginHostManager when instantiating plug-ins)
            
            * Plugins/Hosted/NetscapePluginHostManager.h: Modify method signature for instantiatePlugin to include acceleratedCompositingEnabled bool.
            * Plugins/Hosted/NetscapePluginHostManager.mm:
            (WebKit::NetscapePluginHostManager::instantiatePlugin):
            * Plugins/Hosted/WebHostedNetscapePluginView.mm:
            (-[WebHostedNetscapePluginView createPlugin]): Pass accelerated compositing status to NetscapePluginHostManage
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52942 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/Hosted/NetscapePluginHostManager.h
+++ third_party/WebKit/WebKit/mac/Plugins/Hosted/NetscapePluginHostManager.h
@@ -44,7 +44,7 @@ class NetscapePluginHostManager {
 public:
     static NetscapePluginHostManager& shared();
     
-    PassRefPtr<NetscapePluginInstanceProxy> instantiatePlugin(WebNetscapePluginPackage *, WebHostedNetscapePluginView *, NSString *mimeType, NSArray *attributeKeys, NSArray *attributeValues, NSString *userAgent, NSURL *sourceURL, bool fullFrame, bool isPrivateBrowsingEnabled);
+    PassRefPtr<NetscapePluginInstanceProxy> instantiatePlugin(WebNetscapePluginPackage *, WebHostedNetscapePluginView *, NSString *mimeType, NSArray *attributeKeys, NSArray *attributeValues, NSString *userAgent, NSURL *sourceURL, bool fullFrame, bool isPrivateBrowsingEnabled, bool isAcceleratedCompositingEnabled);
 
     void pluginHostDied(NetscapePluginHostProxy*);
 
