9f64d707f736991200015b230d171a2c506b74c2
kdecker@apple.com <kdecker@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-07 01:01:10 +0000

    Reviewed by Simon Fraser
            Second step of:
            <rdar://problem/6398111> Integrate hardware layers with out-of-process plug-ins layer hosting mechanism
    
            * Plugins/Hosted/WebHostedNetscapePluginView.h: Expose the CALayer of the plugin by adding -pluginLayer method.
            * Plugins/Hosted/WebHostedNetscapePluginView.mm:
            (-[WebHostedNetscapePluginView pluginLayer]): Added.
            * Plugins/WebNetscapePluginView.h: Expose the CALayer of the plugin by adding -pluginLayer method.
            * Plugins/WebNetscapePluginView.mm:
            (-[WebNetscapePluginView pluginLayer]): Added.
            Reviewed by Simon Fraser.
    
            Second step of:
            <rdar://problem/6398111> Integrate hardware layers with out-of-process plug-ins layer hosting mechanism
    
            * Plugins/Hosted/WebHostedNetscapePluginView.h: Expose the CALayer of the plugin by adding -pluginLayer method.
            * Plugins/Hosted/WebHostedNetscapePluginView.mm:
            (-[WebHostedNetscapePluginView pluginLayer]): Added.
            * Plugins/WebNetscapePluginView.h: Expose the CALayer of the plugin by adding -pluginLayer method.
            * Plugins/WebNetscapePluginView.mm:
            (-[WebNetscapePluginView pluginLayer]): Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52885 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/PluginWidget.h
+++ third_party/WebKit/WebCore/plugins/PluginWidget.h
@@ -27,15 +27,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PluginWidget_h
 
 #include "Widget.h"
+#include "GraphicsLayer.h"
 
 namespace WebCore {
 
-// FIXME: The Mac plug-in code should inheirt from PluginView. When
-// this happens PluginWidget and PluginView can become one class. 
 class PluginWidget : public Widget {
+    
 public:
     PluginWidget(PlatformWidget widget = 0) : Widget(widget) { }
-
+#if USE(ACCELERATED_COMPOSITING)
+    PlatformLayer* platformLayer() const;
+#endif
+    
+private:
     virtual void invalidateRect(const IntRect&);
     virtual bool isPluginWidget() const { return true; }
 };
--- third_party/WebKit/WebCore/plugins/mac/PluginWidgetMac.mm
+++ third_party/WebKit/WebCore/plugins/mac/PluginWidgetMac.mm
@@ -26,6 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "PluginWidget.h"
 
+#if USE(ACCELERATED_COMPOSITING)
+@interface NSView (WebKitSecretsWebCoreKnowsAbout)
+- (CALayer *)pluginLayer;
+@end
+#endif
 namespace WebCore {
 
 void PluginWidget::invalidateRect(const IntRect& rect)
@@ -33,4 +38,13 @@ void PluginWidget::invalidateRect(const IntRect& rect)
     [platformWidget() setNeedsDisplayInRect:rect];
 }
 
+#if USE(ACCELERATED_COMPOSITING)
+PlatformLayer* PluginWidget::platformLayer() const
+{
+    if (![platformWidget() respondsToSelector:@selector(pluginLayer)])
+        return 0;
+    
+    return [platformWidget() pluginLayer];   
+}
+#endif
 } // namespace WebCore
