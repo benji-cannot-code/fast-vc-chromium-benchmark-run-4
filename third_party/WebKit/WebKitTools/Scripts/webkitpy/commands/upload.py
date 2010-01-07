0d5a9f84a6c55d21243f40f53dd142b95362aedd
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-07 09:51:01 +0000

    2010-01-07  Adam Barth  <abarth@webkit.org>
            Reviewed by Maciej Stachowiak.
    
            Add prepare and post to webkit-patch main help
            https://bugs.webkit.org/show_bug.cgi?id=33313
    
            * Scripts/webkitpy/commands/upload.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52909 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/commands/upload.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/commands/upload.py
@@ -114,6 +114,7 @@ class Post(AbstractPatchUploadingCommand):
     name = "post"
     help_text = "Attach the current working directory diff to a bug as a patch file"
     argument_names = "[BUGID]"
+    show_in_main_help = True
     steps = [
         steps.ConfirmDiff,
         steps.ObsoletePatches,
@@ -132,6 +133,7 @@ class Prepare(AbstractSequencedCommmand):
     name = "prepare"
     help_text = "Creates a bug (or prompts for an existing bug) and prepares the ChangeLogs"
     argument_names = "[BUGID]"
+    show_in_main_help = True
     steps = [
         steps.PromptForBugOrTitle,
         steps.CreateBug,
