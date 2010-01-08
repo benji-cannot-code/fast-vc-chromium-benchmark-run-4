cd6047bfa3e23b43aa8f9ab4b11808af790034e6
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-08 01:56:08 +0000

    Rollout http://trac.webkit.org/changeset/52756 due to a large performance regression.
    WebCore: 
    
    * dom/Document.cpp:
    (WebCore::Document::updateLayout):
    * editing/SelectionController.cpp:
    (WebCore::SelectionController::SelectionController):
    (WebCore::SelectionController::setSelection):
    * editing/SelectionController.h:
    * page/Frame.cpp:
    (WebCore::Frame::setCaretVisible):
    (WebCore::Frame::selectionLayoutChanged):
    (WebCore::Frame::caretBlinkTimerFired):
    (WebCore::Frame::selectionBounds):
    * page/FrameView.cpp:
    (WebCore::FrameView::needsLayout):
    
    LayoutTests: 
    
    * fast/forms/selection-layout-reentry-strange-case-expected.txt: Removed.
    * fast/forms/selection-layout-reentry-strange-case.html: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52964 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *                     2000 Simon Hausmann <hausmann@kde.org>
  *                     2000 Stefan Schimanski <1Stein@gmx.de>
  *                     2001 George Staikos <staikos@kde.org>
- * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
  * Copyright (C) 2005 Alexey Proskuryakov <ap@nypop.com>
  * Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies)
  * Copyright (C) 2008 Eric Seidel <eric@webkit.org>
@@ -564,7 +564,7 @@ void Frame::setCaretVisible(bool flag)
         return;
     clearCaretRectIfNeeded();
     m_caretVisible = flag;
-    selection()->setNeedsDisplayUpdate();
+    selectionLayoutChanged();
 }
 
 void Frame::clearCaretRectIfNeeded()
@@ -631,17 +631,9 @@ void Frame::setFocusedNodeIfNeeded()
 
 void Frame::selectionLayoutChanged()
 {
-    selection()->setNeedsDisplayUpdate(false);
-
-#if ENABLE(TEXT_CARET)
-    static bool alreadyInSelectionLayoutChanged;
-    if (alreadyInSelectionLayoutChanged)
-        return;
-
-    alreadyInSelectionLayoutChanged = true;
-
     bool caretRectChanged = selection()->recomputeCaretRect();
 
+#if ENABLE(TEXT_CARET)
     bool caretBrowsing = settings() && settings()->caretBrowsingEnabled();
     bool shouldBlink = m_caretVisible
         && selection()->isCaret() && (selection()->isContentEditable() || caretBrowsing);
@@ -662,8 +654,9 @@ void Frame::selectionLayoutChanged()
             selection()->invalidateCaretRect();
         }
     }
-
-    alreadyInSelectionLayoutChanged = false;
+#else
+    if (!caretRectChanged)
+        return;
 #endif
 
     RenderView* view = contentRenderer();
@@ -699,10 +692,8 @@ void Frame::selectionLayoutChanged()
 void Frame::caretBlinkTimerFired(Timer<Frame>*)
 {
 #if ENABLE(TEXT_CARET)
-    if (!m_caretVisible || !selection()->isCaret()) {
-        ASSERT(selection()->needsDisplayUpdate());
-        return;
-    }
+    ASSERT(m_caretVisible);
+    ASSERT(selection()->isCaret());
     bool caretPaint = m_caretPaint;
     if (selection()->isCaretBlinkingSuspended() && caretPaint)
         return;
@@ -1262,8 +1253,6 @@ void Frame::setExcludeFromTextSearch(bool exclude)
 // returns FloatRect because going through IntRect would truncate any floats
 FloatRect Frame::selectionBounds(bool clipToVisibleContent) const
 {
-    m_doc->updateLayout();
-
     RenderView* root = contentRenderer();
     FrameView* view = m_view.get();
     if (!root || !view)
