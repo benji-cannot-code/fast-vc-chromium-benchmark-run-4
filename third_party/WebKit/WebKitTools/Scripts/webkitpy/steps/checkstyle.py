3dff15752106726808c6e5087bc573a3a1113ce1
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-08 04:58:10 +0000

    2010-01-07  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            webkit-patch edit-changelogs (or upload) will open blank files if run outside the root
            https://bugs.webkit.org/show_bug.cgi?id=33341
    
            Fix this by moving to the root directory before editing ChangeLogs.
            There is a related bug with CheckStyle.
    
            * Scripts/webkitpy/steps/checkstyle.py:
            * Scripts/webkitpy/steps/editchangelog.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52968 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/steps/checkstyle.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/steps/checkstyle.py
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import os
+
 from webkitpy.executive import ScriptError
 from webkitpy.steps.abstractstep import AbstractStep
 from webkitpy.steps.options import Options
@@ -43,6 +45,7 @@ class CheckStyle(AbstractStep):
     def run(self, state):
         if not self._options.check_style:
             return
+        os.chdir(self._tool.scm().checkout_root)
         try:
             self._run_script("check-webkit-style")
         except ScriptError, e:
--- third_party/WebKit/WebKitTools/Scripts/webkitpy/steps/editchangelog.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/steps/editchangelog.py
@@ -27,9 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import os
+
 from webkitpy.steps.abstractstep import AbstractStep
 
 
 class EditChangeLog(AbstractStep):
     def run(self, state):
+        os.chdir(self._tool.scm().checkout_root)
         self._tool.user.edit(self.cached_lookup(state, "changelogs"))
