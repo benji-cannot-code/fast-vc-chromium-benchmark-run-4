95de42c110b0d9e177507a91c58bb321e20eac57
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-08 00:44:19 +0000

    Fix a crash seen on the buildbots.
    Reviewed by Sam Weinig.
    
    JavaScriptCore: 
    
    * runtime/JSGlobalObject.cpp:
    (JSC::JSGlobalObject::init): Disable specific function tracking here,
    instead of in WebCore, to ensure that the disabling happens before a
    specific function can be registered.
    
    WebCore: 
    
    * bindings/js/JSDOMWindowBase.cpp:
    (WebCore::JSDOMWindowBase::JSDOMWindowBase): Don't disable specific
    function tracking here; it's too late, as some specific functions
    may already have been tracked in the base class constructor.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@52962 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSGlobalObject.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSGlobalObject.cpp
@@ -129,6 +129,8 @@ void JSGlobalObject::init(JSObject* thisValue)
 {
     ASSERT(JSLock::currentThreadIsHoldingLock());
 
+    structure()->disableSpecificFunctionTracking();
+
     d()->globalData = Heap::heap(this)->globalData();
     d()->globalScopeChain = ScopeChain(this, d()->globalData.get(), this, thisValue);
 
