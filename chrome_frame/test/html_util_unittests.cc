9894eb392dde8972c7235af8f14adf46dd65826f
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-08 02:57:03 +0000

    Deleting cookies by setting the expires attribute on them with an empty value would not work in ChromeFrame with the host network stack enabled. When we receive a response in the host browser (IE) we send over the response headers which include the Set-Cookie header and a list of cookies retreived via the InternetGetCookie API. We call this API to retrieve the persistent cookies and send them over to Chrome.
    However this API returns session cookies as well as persistent cookies. There is no documented way to return
    only persistent cookies from IE. As a result we would end up setting duplicate cookies in Chrome, which caused
    this issu.e. To workaround this issue when we receive the response in the url request automation job which
    handles ChromeFrame network requests, we strip out duplicate cookies sent via InternetGetCookie.
    
    When a script deletes a cookie we now handle it correctly in IE and set the data to an empty string. However
    this does not delete the cookie. When such cookies show up in Chrome, we strip them out as well.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=30786
    
    The changes to chrome_frame_npapi.cc/.h are to move the NPAPI functions to the chrome_frame namespace as they
    conflict with similar functions in NACL.
    
    Added the DeleteCookie function to the CookieStore interface, which I think missed out by oversight.
    
    Bug=30786
    Test=Covered by ChromeFrame unit tests. I also added a unit test to test the newly added
         URLRequestAutomationJob::IsCookiePresentInCookieHeader function
    
    Review URL: http://codereview.chromium.org/518054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35769 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/html_util_unittests.cc
+++ chrome_frame/test/html_util_unittests.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #include <atlsecurity.h>
 #include <shellapi.h>
+#include <string>
+#include <vector>
 
 #include "base/basictypes.h"
 #include "base/file_util.h"
@@ -23,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome_frame/chrome_frame_delegate.h"
 #include "chrome_frame/html_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "chrome/browser/automation/url_request_automation_job.h"
 
 const char kChromeFrameUserAgent[] = "chromeframe";
 
@@ -361,4 +364,25 @@ TEST(HttpUtils, HasFrameBustingHeader) {
     "X-Frame-Options: ALLOWall\r\n"));
 }
 
+TEST(HttpCookieTest, IdentifyDuplicateCookieTest) {
+  std::vector<std::string> header_cookies;
+  header_cookies.push_back("BLAHHH; Path=/;");
+
+  EXPECT_FALSE(URLRequestAutomationJob::IsCookiePresentInCookieHeader(
+                  "BLAHHH=1", header_cookies));
+
+  header_cookies.clear();
+
+  header_cookies.push_back("BLAHHH=1; Path=/;");
+
+  EXPECT_TRUE(URLRequestAutomationJob::IsCookiePresentInCookieHeader(
+                  "BLAHHH=1", header_cookies));
+
+  header_cookies.clear();
+
+  header_cookies.push_back("BLAH=1; Path=/blah;");
+
+  EXPECT_FALSE(URLRequestAutomationJob::IsCookiePresentInCookieHeader(
+                  "BLAH", header_cookies));
+}
 
