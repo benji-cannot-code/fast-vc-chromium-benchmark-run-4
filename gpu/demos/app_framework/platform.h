68dfb948077638f6f3746f53f2248582adeeaec7
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-08 00:22:37 +0000

    Added an application framework for demos. This framework currently only supports stand-alone apps. It will be extended to support trusted pepper and untrusted nacl apps. BUG=26099 Review URL: http://codereview.chromium.org/525083
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35758 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ gpu/demos/app_framework/platform.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Platform-specific types and definitions for native widget handles.
+
+#ifndef GPU_DEMOS_APP_FRAMEWORK_PLATFORM_H_
+#define GPU_DEMOS_APP_FRAMEWORK_PLATFORM_H_
+
+#ifdef _WINDOWS
+#include <windows.h>
+#endif  // _WINDOWS
+
+#include "build/build_config.h"
+
+namespace gpu_demos {
+
+#if defined(OS_WIN)
+typedef HWND NativeWindowHandle;
+#endif  // defined(OS_WIN)
+
+}  // namespace gpu_demos
+#endif  // GPU_DEMOS_APP_FRAMEWORK_PLATFORM_H_
