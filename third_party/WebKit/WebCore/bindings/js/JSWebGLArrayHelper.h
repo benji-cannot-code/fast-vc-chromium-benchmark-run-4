c5e2cf201d6ec602fac5567f3eba43e8070c998c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-09 01:02:53 +0000

    2010-01-08  Kenneth Russell  <kbr@google.com>
            Reviewed by Dimitri Glazkov.
    
            Passing array that is too large to set method of WebGLArrays does not throw an exception
            https://bugs.webkit.org/show_bug.cgi?id=33352
    
            Added needed range checks to JSC and V8 custom bindings. Expanded
            preexisting test suite for WebGLArrays and updated its expected
            results. Tested in WebKit and Chromium.
    
            * fast/canvas/webgl/array-set-out-of-bounds-expected.txt: Added.
            * fast/canvas/webgl/array-set-out-of-bounds.html: Added.
            * fast/canvas/webgl/array-unit-tests-expected.txt:
            * fast/canvas/webgl/array-unit-tests.html:
    2010-01-08  Kenneth Russell  <kbr@google.com>
    
            Reviewed by Dimitri Glazkov.
    
            Passing array that is too large to set method of WebGLArrays does not throw an exception
            https://bugs.webkit.org/show_bug.cgi?id=33352
    
            Added needed range checks to JSC and V8 custom bindings. Expanded
            preexisting test suite for WebGLArrays and updated its expected
            results. Tested in WebKit and Chromium.
    
            Test: fast/canvas/webgl/array-set-out-of-bounds.html
    
            * bindings/js/JSWebGLArrayHelper.h:
            (WebCore::setWebGLArrayFromArray):
            * bindings/v8/custom/V8WebGLArrayCustom.h:
            (WebCore::setWebGLArrayFromArray):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53026 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSWebGLArrayHelper.h
+++ third_party/WebKit/WebCore/bindings/js/JSWebGLArrayHelper.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef JSWebGLArrayHelper_h
 #define JSWebGLArrayHelper_h
 
+#include "ExceptionCode.h"
+#include "JSDOMBinding.h"
 #include <interpreter/CallFrame.h>
 #include <runtime/ArgList.h>
 #include <runtime/Error.h>
@@ -46,11 +48,15 @@ JSC::JSValue setWebGLArrayFromArray(JSC::ExecState* exec, T* webGLArray, JSC::Ar
         if (args.size() == 2)
             offset = args.at(1).toInt32(exec);
         int length = array->get(exec, JSC::Identifier(exec, "length")).toInt32(exec);
-        for (int i = 0; i < length; i++) {
-            JSC::JSValue v = array->get(exec, i);
-            if (exec->hadException())
-                return JSC::jsUndefined();
-            webGLArray->set(i + offset, v.toNumber(exec));
+        if (offset + length > webGLArray->length())
+            setDOMException(exec, INDEX_SIZE_ERR);
+        else {
+            for (int i = 0; i < length; i++) {
+                JSC::JSValue v = array->get(exec, i);
+                if (exec->hadException())
+                    return JSC::jsUndefined();
+                webGLArray->set(i + offset, v.toNumber(exec));
+            }
         }
 
         return JSC::jsUndefined();
