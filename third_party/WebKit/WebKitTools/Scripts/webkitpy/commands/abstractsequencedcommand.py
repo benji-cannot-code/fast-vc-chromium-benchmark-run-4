bbc1f7fc7112eae7d388a8693e10489d12359676
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-11 06:14:37 +0000

    2010-01-10  Adam Barth  <abarth@webkit.org>
            Rubber stamped by David Kilzer.
    
            bugzilla-tool submit-patch mistakenly picks up bug URLs in non-ChangeLog files
            https://bugs.webkit.org/show_bug.cgi?id=33197
    
            Fix a typo: Commmand -> Command.
    
            * Scripts/webkitpy/commands/abstractsequencedcommand.py:
            * Scripts/webkitpy/commands/download.py:
            * Scripts/webkitpy/commands/queries.py:
            * Scripts/webkitpy/commands/upload.py:
            * Scripts/webkitpy/multicommandtool.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53066 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/commands/abstractsequencedcommand.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/commands/abstractsequencedcommand.py
@@ -27,15 +27,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from webkitpy.multicommandtool import AbstractDeclarativeCommmand
+from webkitpy.multicommandtool import AbstractDeclarativeCommand
 from webkitpy.stepsequence import StepSequence
 
 
-class AbstractSequencedCommmand(AbstractDeclarativeCommmand):
+class AbstractSequencedCommand(AbstractDeclarativeCommand):
     steps = None
     def __init__(self):
         self._sequence = StepSequence(self.steps)
-        AbstractDeclarativeCommmand.__init__(self, self._sequence.options())
+        AbstractDeclarativeCommand.__init__(self, self._sequence.options())
 
     def _prepare_state(self, options, args, tool):
         return None
