579aad691a885ee4580f93d7c27a4bb8c1795970
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-11 19:55:39 +0000

    2010-01-11  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Alexey Proskuryakov.
    
            https://bugs.webkit.org/show_bug.cgi?id=33481
            Uninitialized data members in ArrayStorage
            
            SunSpider reports no change.
    
            * runtime/JSArray.cpp:
            (JSC::JSArray::JSArray): Initialize missing data members in the two cases
            where we don't use fastZeroedMalloc, so it doesn't happen automatically.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53091 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSArray.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSArray.cpp
@@ -153,6 +153,7 @@ JSArray::JSArray(NonNullPassRefPtr<Structure> structure, unsigned initialLength)
     m_storage->m_numValuesInVector = 0;
     m_storage->m_sparseValueMap = 0;
     m_storage->lazyCreationData = 0;
+    m_storage->reportedMapCapacity = 0;
 
     JSValue* vector = m_storage->m_vector;
     for (size_t i = 0; i < initialCapacity; ++i)
@@ -173,6 +174,8 @@ JSArray::JSArray(NonNullPassRefPtr<Structure> structure, const ArgList& list)
     m_vectorLength = initialCapacity;
     m_storage->m_numValuesInVector = initialCapacity;
     m_storage->m_sparseValueMap = 0;
+    m_storage->lazyCreationData = 0;
+    m_storage->reportedMapCapacity = 0;
 
     size_t i = 0;
     ArgList::const_iterator end = list.end();
