621120e0318513c5245dd583be71b1a94f9abd96
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-11 04:55:26 +0000

    2010-01-10  Kent Hansen  <kent.hansen@nokia.com>
            Reviewed by Darin Adler.
    
            RegExp.prototype.toString returns "//" for empty regular expressions
            https://bugs.webkit.org/show_bug.cgi?id=33319
    
            "//" starts a single-line comment, hence "/(?:)/" should be used, according to ECMA.
    
            * runtime/RegExpPrototype.cpp:
            (JSC::regExpProtoFuncToString):
    
            * tests/mozilla/ecma_2/RegExp/properties-001.js:
            (AddRegExpCases):
            * tests/mozilla/js1_2/regexp/toString.js:
            Update relevant Mozilla tests (Mozilla has had this behavior since November 2003).
    2010-01-10  Kent Hansen  <kent.hansen@nokia.com>
    
            Reviewed by Darin Adler.
    
            RegExp.prototype.toString returns "//" for empty regular expressions
            https://bugs.webkit.org/show_bug.cgi?id=33319
    
            Add new test cases and adapt existing ones.
    
            * fast/js/kde/RegExp-expected.txt:
            * fast/js/kde/script-tests/RegExp.js:
            * fast/js/regexp-compile-expected.txt:
            * fast/js/script-tests/regexp-compile.js:
            * fast/regex/non-pattern-characters-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/tests/mozilla/js1_2/regexp/toString.js
+++ third_party/WebKit/JavaScriptCore/tests/mozilla/js1_2/regexp/toString.js
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // var re = new RegExp(); re.toString()
 	var re = new RegExp();
 	testcases[count++] = new TestCase ( SECTION, "var re = new RegExp(); re.toString()",
-	                                    '//', re.toString());
+	                                    '/(?:)/', re.toString());
 
     // re = /.+/; re.toString();
     re = /.+/;
