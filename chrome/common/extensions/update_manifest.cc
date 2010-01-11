d2bc05e87792915ecfe8e9a714b790bd5f0af5dd
craig.schlenter@chromium.org <craig.schlenter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-11 16:26:07 +0000

    Extensions: Ensure that variable strings passed to ParseError aren't interpreted as format strings.
    BUG=31085
    
    Review URL: http://codereview.chromium.org/523131
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35904 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/update_manifest.cc
+++ chrome/common/extensions/update_manifest.cc
@@ -194,7 +194,7 @@ bool UpdateManifest::Parse(const std::string& manifest_xml) {
   ScopedXmlDocument document(xmlParseDoc(
       reinterpret_cast<const xmlChar*>(manifest_xml.c_str())));
   if (!document.get()) {
-    ParseError(xml_errors.c_str());
+    ParseError("%s", xml_errors.c_str());
     return false;
   }
 
@@ -229,7 +229,7 @@ bool UpdateManifest::Parse(const std::string& manifest_xml) {
     Result current;
     std::string error;
     if (!ParseSingleAppTag(apps[i], gupdate_ns, &current, &error)) {
-      ParseError(error.c_str());
+      ParseError("%s", error.c_str());
       return false;
     }
     results_.push_back(current);
