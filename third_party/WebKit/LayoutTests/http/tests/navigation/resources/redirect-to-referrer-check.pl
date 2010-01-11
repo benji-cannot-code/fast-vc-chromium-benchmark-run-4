a9d7efc2a4848867b01aaff2f25c3eef78ab19db
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-11 17:05:48 +0000

    2010-01-08  Nate Chapin  <japhet@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Test that document.referrer is properly preserved through a redirect.
    
            https://bugs.webkit.org/show_bug.cgi?id=33407
    
            * http/tests/navigation/redirect-preserves-referrer-expected.txt: Added.
            * http/tests/navigation/redirect-preserves-referrer.html: Added.
            * http/tests/navigation/resources/redirect-to-referrer-check.pl: Added.
            * http/tests/navigation/resources/referrer.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53081 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/navigation/resources/redirect-to-referrer-check.pl
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/perl
+# Simple script to generate a 302 HTTP redirect
+
+print "Status: 302 Moved Temporarily\r\n";
+print "Location: referrer.html\r\n";
+print "Content-type: text/html\r\n";
+print "\r\n";
+
+print <<HERE_DOC_END
+<html><body>
+This page should not be seen - it is a 302 redirect to another page.
+</body></html>
+HERE_DOC_END
