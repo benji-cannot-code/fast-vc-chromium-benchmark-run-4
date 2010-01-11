a9a1211b09ee09c88afdf649fd86a00a224a9bce
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-11 05:11:21 +0000

    2010-01-10  Adam Barth  <abarth@webkit.org>
            Reviewed by Darin Adler.
    
            Let webkit-patch work with options in $EDITOR
            https://bugs.webkit.org/show_bug.cgi?id=33414
    
            The $EDITOR evironment variable might have command line options like
            bbedit -w.  This patch lets us run those $EDITORs.
    
            * Scripts/webkitpy/user.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53063 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/user.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/user.py
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
+import shlex
 import subprocess
 import webbrowser
 
@@ -37,7 +38,8 @@ class User(object):
 
     def edit(self, files):
         editor = os.environ.get("EDITOR") or "vi"
-        subprocess.call([editor] + files)
+        args = shlex.split(editor)
+        subprocess.call(args + files)
 
     def page(self, message):
         pager = os.environ.get("PAGER") or "less"
