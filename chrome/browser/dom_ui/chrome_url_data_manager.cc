e4399488f6400c8a3d772be710f517bf029d2baf
jshin@chromium.org <jshin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-11 17:34:42 +0000

    1. Increase the width for 'date-container' in download.html to 110px to make room for dates in some languages. Increase the margin-left (LTR) and margin-rigth(RTL) accordingly.
    2. Decrease the web font size (dom UI font size) for Bengali on Vista or later to 84% (the same value as used for English) while using 110% for XP. This is done by introducing a new 'string' (IDS_WEB_FONT_SIZE_XP) on Windows (other platforms won't get this). 
    
    
    BUG=21699
    TEST=Run Chrome with '--lang=ml' (--lang=kn, --lang=bn) on Windows and go to the download page. Make sure that dates are in a single line. 
    
    
    Review URL: http://codereview.chromium.org/381009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35908 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/chrome_url_data_manager.cc
+++ chrome/browser/dom_ui/chrome_url_data_manager.cc
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/thread.h"
 #include "base/values.h"
+#if defined(OS_WIN)
+#include "base/win_util.h"
+#endif
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/net/chrome_url_request_context.h"
@@ -297,8 +300,16 @@ void ChromeURLDataManager::DataSource::SetFontAndTextDirection(
     DictionaryValue* localized_strings) {
   localized_strings->SetString(L"fontfamily",
       l10n_util::GetString(IDS_WEB_FONT_FAMILY));
+
+  int web_font_size_id = IDS_WEB_FONT_SIZE;
+#if defined(OS_WIN)
+  // Some fonts used for some languages changed a lot in terms of the font
+  // metric in Vista. So, we need to use different size before Vista.
+  if (win_util::GetWinVersion() < win_util::WINVERSION_VISTA)
+    web_font_size_id = IDS_WEB_FONT_SIZE_XP;
+#endif
   localized_strings->SetString(L"fontsize",
-      l10n_util::GetString(IDS_WEB_FONT_SIZE));
+      l10n_util::GetString(web_font_size_id));
 
   localized_strings->SetString(L"textdirection",
       (l10n_util::GetTextDirection() == l10n_util::RIGHT_TO_LEFT) ?
