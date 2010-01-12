705dcdd4572884b9964e42ba258d2514ecf7f220
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-12 18:10:14 +0000

    2010-01-12  Tony Chang  <tony@chromium.org>
            Reviewed by Maciej Stachowiak.
    
            Add a test for a Chromium crash when loading multipart/x-mixed-replace
            data.
    
            https://bugs.webkit.org/show_bug.cgi?id=31446
    
            * http/tests/multipart/multipart-wait-before-boundary-expected.txt: Added.
            * http/tests/multipart/multipart-wait-before-boundary.html: Added.
            * http/tests/multipart/resources/multipart-wait-before-boundary.php: Added.
    2010-01-12  Tony Chang  <tony@chromium.org>
    
            Reviewed by Maciej Stachowiak.
    
            Fix a crash in Chromium when receiving multipart/x-mixed-replace data.
            If we stop a multipart load after ResourceLoader::didReceiveResponse
            but before ResourceLoader::didReceiveData, we have a NULL document
            loader because it has been moved back to the provisional state.  New
            loads that happen after this will dereference the NULL document
            loader.
    
            Work around this by moving the provisional document loader back as
            the document loader when the request is stopped.
    
            https://bugs.webkit.org/show_bug.cgi?id=31446
    
            Test: http/tests/multipart/multipart-wait-before-boundary.html
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::checkLoadCompleteForThisFrame):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53143 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/multipart/resources/multipart-wait-before-boundary.php
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+    if (isset($_GET['done'])) {
+      header("Content-Type: text/html");
+      echo("<script>parent.success()</script>");
+      exit(0);
+    }
+
+    $boundary = "cutHere";
+
+    function sendHeader()
+    {
+        global $boundary;
+
+        echo("--$boundary\r\n");
+        echo("Content-Type: text/html\r\n\r\n");
+        flush();
+    }
+
+    header("Content-Type: multipart/x-mixed-replace; boundary=$boundary");
+
+    // generate some padding to work around CFNetwork handling of multipart data
+    $padding = "aa";
+    for ($i = 0; $i < 10; $i++) {
+      $padding .= $padding;
+    }
+
+    sendHeader();
+    echo("test html\n");
+    echo("<!-- $padding -->");
+    flush();
+    sendHeader();
+    echo("second html");
+    echo("<script>parent.childLoaded()</script>");
+    echo("<!-- $padding -->");
+    flush();
+    sendHeader();
+    echo("third html");
+    echo("<!-- $padding -->");
+    flush();
+    usleep(30 * 1000000);
+?>
--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -2923,6 +2923,9 @@ void FrameLoader::checkLoadCompleteForThisFrame()
                 stopLoadingSubframes();
                 pdl->stopLoading();
 
+                if (isReplacing())
+                    setDocumentLoader(m_provisionalDocumentLoader.get());
+
                 // Finish resetting the load state, but only if another load hasn't been started by the
                 // delegate callback.
                 if (pdl == m_provisionalDocumentLoader)
