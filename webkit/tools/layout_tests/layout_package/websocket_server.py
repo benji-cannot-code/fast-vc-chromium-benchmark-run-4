12b022a52e2f5a65f8f64936289bd13a74a84713
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-12 08:07:56 +0000

    Initialize self._wsout.
    If websocket_server.py is called with --server stop, it will call
    Stop without calling Start, so it claims as follows:
      AttributeError: 'PyWebSocket' object has no attribute '_wsout'
    
    Review URL: http://codereview.chromium.org/545023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35998 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/layout_tests/layout_package/websocket_server.py
+++ webkit/tools/layout_tests/layout_package/websocket_server.py
@@ -75,6 +75,7 @@ class PyWebSocket(http_server.Lighttpd):
     else:
       self._server_name = 'PyWebSocket'
     self._pidfile = pidfile
+    self._wsout = None
 
     # Webkit tests
     if self._root:
