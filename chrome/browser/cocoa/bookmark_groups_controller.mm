097bda668bac9442c1e99b2a1e5a628c2c7620b9
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-12 22:10:29 +0000

    Cocoa bookmarks manager and autocomplete text field editor: in the 10.6 SDK, delegates implement protocols, and -delegate methods no longer return type |id|.  Add casts as needed.
    BUG=none
    TEST=10.6 SDK build
    Review URL: http://codereview.chromium.org/550022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36055 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_groups_controller.mm
+++ chrome/browser/cocoa/bookmark_groups_controller.mm
@@ -291,7 +291,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (NSMenu*)menu {
-  return [[self delegate] menu];
+  return [(BookmarkGroupsController*)[self delegate] menu];
 }
 
 @end
--- chrome/browser/cocoa/bookmark_tree_controller.mm
+++ chrome/browser/cocoa/bookmark_tree_controller.mm
@@ -358,7 +358,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (NSMenu*)menu {
-  return [[self delegate] menu];
+  return [(BookmarkTreeController*)[self delegate] menu];
 }
 
 @end
