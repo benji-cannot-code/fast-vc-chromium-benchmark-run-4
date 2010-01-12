484aaa3494b88920d052742ffff640af0a40a46d
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-12 02:17:02 +0000

    Implement Linux/GTK ExtensionInstalledBubble
    BUG=26973
    TEST=Install a packaged extension. Verify the InfoBubble is shown, with the install icon and some description. The InfoBubble should disappear when the bubble looses focus (click elsewhere). For a browserAction, the bubble should point to the browserAction icon. For a pageAction **that has a "default_icon" set in it's manifest (see the samples/subscribe_page_action in this CL)**, it should point to a temporarily shown pageAction icon that will be hidden when the bubble closes. Otherwise it should point to the wrench menu.
    
    Review URL: http://codereview.chromium.org/547010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35989 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_ui.cc
+++ chrome/browser/extensions/extension_install_ui.cc
@@ -19,7 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/tab_contents.h"
 #if defined(TOOLKIT_VIEWS)  // TODO(port)
 #include "chrome/browser/views/extensions/extension_installed_bubble.h"
-#endif  // TOOLKIT_VIEWS
+#elif defined(TOOLKIT_GTK)
+#include "chrome/browser/gtk/extension_installed_bubble_gtk.h"
+#endif
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/platform_util.h"
@@ -186,18 +188,24 @@ void ExtensionInstallUI::OnInstallSuccess(Extension* extension) {
     return;
   }
 
-#if defined(TOOLKIT_VIEWS)
   // GetLastActiveWithProfile will fail on the build bots. This needs to
   // implemented differently if any test is created which depends on
   // ExtensionInstalledBubble showing.
+#if defined(TOOLKIT_VIEWS)
   Browser* browser = BrowserList::GetLastActiveWithProfile(profile_);
   if (!browser)
     return;
 
   ExtensionInstalledBubble::Show(extension, browser, icon_);
+#elif defined(TOOLKIT_GTK)
+  Browser* browser = BrowserList::GetLastActiveWithProfile(profile_);
+  if (!browser)
+    return;
+
+  ExtensionInstalledBubbleGtk::Show(extension, browser, icon_);
 #else
-// TODO(port) crbug.com/26973 (linux) crbug.com/26974 (mac)
-#endif  // TOOLKIT_VIEWS
+// TODO(port) crbug.com/26974 (mac)
+#endif
 }
 
 void ExtensionInstallUI::OnInstallFailure(const std::string& error) {
