0b2fa4bb01ee85c0d2cc99cf4bb7577f9068532f
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-12 22:48:20 +0000

    WebCore: Update all-in-one derived sources for Geolocation.
    Reviewed by Sam Weinig.
    
    * DerivedSources.cpp: Add JSCoordinates.cpp, JSGeolocation.cpp, JSGeoposition.cpp, JSPositionError.cpp
    
    WebKit/win: Add Geolocation interfaces in WebKit for Windows.
    
    Reviewed by Adam Roben, Sam Weinig.
    
    * ForEachCoClass.h: Added WebGeolocationPosition.
    * Interfaces/IWebError.idl:
    * Interfaces/IWebGeolocationPosition.idl: Added.
    * Interfaces/IWebGeolocationProvider.idl: Added.
    * Interfaces/IWebViewPrivate.idl:
    * Interfaces/WebKit.idl:
    * WebCoreSupport/WebGeolocationControllerClient.cpp: Added.
    (WebGeolocationControllerClient::WebGeolocationControllerClient):
    (WebGeolocationControllerClient::startUpdating):
    (WebGeolocationControllerClient::stopUpdating):
    (WebGeolocationControllerClient::lastPosition):
    * WebCoreSupport/WebGeolocationControllerClient.h: Added.
    * WebGeolocationPosition.cpp: Added IWebGeolocationPosition implementation.
    * WebGeolocationPosition.h: Added IWebGeolocationPosition implementation.
    * WebKit.vcproj/Interfaces.vcproj: Added IWebGeolocationPosition.idl, IWebGeolocationProvider.idl,
    relocated JavaScriptCoreAPITypes.idl, WebScrollbarTypes.idl
    * WebKit.vcproj/WebKit.vcproj: Added WebGeolocationPosition.h/.cpp, WebGeolocationControllerClient.h/cpp,        
    * WebKitClassFactory.cpp: Add WebGeolocationPosition include.
    * WebView.cpp:
    (WebView::initWithFrame): Create WebGeolocationControllerClient if Geolocation is enabled.
    (WebView::setGeolocationProvider): Added Geolocation-specific method.
    (WebView::geolocationProvider): Added Geolocation-specific method.
    (WebView::geolocationDidChangePosition): Added Geolocation-specific method.
    (WebView::geolocationDidFailWithError): Added Geolocation-specific method.
    * WebView.h: Added setGeolocationProvider, geolocationProvider, geolocationDidChangePosition, geolocationDidFailWithError.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/ForEachCoClass.h
+++ third_party/WebKit/WebKit/win/ForEachCoClass.h
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     macro(WebCookieManager) \
     macro(WebWorkersPrivate) \
     macro(WebScriptWorld) \
+    macro(WebGeolocationPosition) \
     // end of macro
 
 // Everything below this point is deprecated. Please do not use.
--- third_party/WebKit/WebKit/win/WebKitClassFactory.cpp
+++ third_party/WebKit/WebKit/win/WebKitClassFactory.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebDownload.h"
 #include "WebError.h"
 #include "WebFrame.h"
+#include "WebGeolocationPosition.h"
 #include "WebHistory.h"
 #include "WebHistoryItem.h"
 #include "WebIconDatabase.h"
