aa227d7dded6664b2deae7063ee92e76e735e5d6
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-12 11:28:18 +0000

    2010-01-12  Simon Hausmann  <simon.hausmann@nokia.com>
            Reviewed by Holger Freyther.
    
            [Qt] WebCore::Path allocates QPainterPath unnecessarily on the heap
            https://bugs.webkit.org/show_bug.cgi?id=33466
    
            WebCore::Path is a pointer to a PlatformPath. In case of Qt that's a
            QPainterPath, which itself is a pointer to the elements (QVector).
            That creates unecessary allocations in PathQt.cpp.
    
            Replaced the "PlatformPath* m_path;" with a PlatformPathPtr, which
            is a plain QPainterPath.
    
            * platform/graphics/Path.h:
            (WebCore::Path::platformPath):
            * platform/graphics/qt/GraphicsContextQt.cpp:
            (WebCore::drawFilledShadowPath):
            (WebCore::GraphicsContext::fillPath):
            (WebCore::GraphicsContext::fillRoundedRect):
            (WebCore::GraphicsContext::addPath):
            (WebCore::GraphicsContext::clip):
            (WebCore::GraphicsContext::clipOut):
            * platform/graphics/qt/PathQt.cpp:
            (WebCore::Path::~Path):
            (WebCore::Path::operator=):
            (WebCore::Path::contains):
            (WebCore::Path::strokeContains):
            (WebCore::Path::translate):
            (WebCore::Path::boundingRect):
            (WebCore::Path::strokeBoundingRect):
            (WebCore::Path::moveTo):
            (WebCore::Path::addLineTo):
            (WebCore::Path::addQuadCurveTo):
            (WebCore::Path::addBezierCurveTo):
            (WebCore::Path::addArcTo):
            (WebCore::Path::closeSubpath):
            (WebCore::Path::addArc):
            (WebCore::Path::addRect):
            (WebCore::Path::addEllipse):
            (WebCore::Path::clear):
            (WebCore::Path::isEmpty):
            (WebCore::Path::debugString):
            (WebCore::Path::apply):
            (WebCore::Path::transform):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53131 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/Path.h
+++ third_party/WebKit/WebCore/platform/graphics/Path.h
@@ -35,10 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if PLATFORM(CG)
 typedef struct CGPath PlatformPath;
 #elif PLATFORM(QT)
-#include <qglobal.h>
-QT_BEGIN_NAMESPACE
-class QPainterPath;
-QT_END_NAMESPACE
+#include <qpainterpath.h>
 typedef QPainterPath PlatformPath;
 #elif PLATFORM(WX) && USE(WXGC)
 class wxGraphicsPath;
@@ -60,6 +57,13 @@ namespace WebCore {
 }
 #else
 typedef void PlatformPath;
+#endif
+
+#if PLATFORM(QT)
+/* QPainterPath is valued based */
+typedef PlatformPath PlatformPathPtr;
+#else
+typedef PlatformPath* PlatformPathPtr;
 #endif
 
 namespace WebCore {
@@ -132,7 +136,7 @@ namespace WebCore {
 
         String debugString() const;
 
-        PlatformPath* platformPath() const { return m_path; }
+        PlatformPathPtr platformPath() const { return m_path; }
 
         static Path createRoundedRectangle(const FloatRect&, const FloatSize& roundingRadii);
         static Path createRoundedRectangle(const FloatRect&, const FloatSize& topLeftRadius, const FloatSize& topRightRadius, const FloatSize& bottomLeftRadius, const FloatSize& bottomRightRadius);
@@ -145,7 +149,7 @@ namespace WebCore {
         void transform(const TransformationMatrix&);
 
     private:
-        PlatformPath* m_path;
+        PlatformPathPtr m_path;
     };
 
 }
