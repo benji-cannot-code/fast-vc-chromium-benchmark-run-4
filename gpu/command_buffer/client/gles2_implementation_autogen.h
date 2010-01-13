caa6e024103ab6204875ca852e6526ef7680a809
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-13 20:06:09 +0000

    Adds a sync to glFinish and SwapBuffers at the GLES2Implementation level.
    You can still call these async from a gh GLES2Helper level.
    
    Review URL: http://codereview.chromium.org/545041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36160 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/gles2_implementation_autogen.h
+++ gpu/command_buffer/client/gles2_implementation_autogen.h
@@ -200,9 +200,7 @@ void EnableVertexAttribArray(GLuint index) {
   helper_->EnableVertexAttribArray(index);
 }
 
-void Finish() {
-  helper_->Finish();
-}
+void Finish();
 
 void Flush() {
   helper_->Flush();
@@ -653,9 +651,7 @@ void Viewport(GLint x, GLint y, GLsizei width, GLsizei height) {
   helper_->Viewport(x, y, width, height);
 }
 
-void SwapBuffers() {
-  helper_->SwapBuffers();
-}
+void SwapBuffers();
 
 #endif  // GPU_COMMAND_BUFFER_CLIENT_GLES2_IMPLEMENTATION_AUTOGEN_H_
 
