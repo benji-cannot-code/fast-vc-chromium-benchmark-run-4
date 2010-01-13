e14482a0048bec62cee2fdd1f7f8a513290d7d0a
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-13 04:55:06 +0000

    Mac: UI tweaks for task manager.
    Nib change: Reduce row height, make scrollbars smaller.
    
    Make text in task manager table slightly smaller.
    
    Show in decimal digit for %cpu.
    
    Show memory in KB/MB, not always in K.
    
    Change update frequency from 1s to 2s to match Activity Monitor's default.
    
    (all mac-only. ui team is fine with this.)
    
    Finally, turn taskman on.
    
    BUG=13156
    TEST=Open task manager, look at it. Should look & feel similar to Activity Monitor.
    
    Review URL: http://codereview.chromium.org/536038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36096 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/task_manager_mac.mm
+++ chrome/browser/cocoa/task_manager_mac.mm
@@ -92,6 +92,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [[column.get() headerCell] setAlignment:textAlignment];
   [[column.get() dataCell] setAlignment:textAlignment];
 
+  NSFont* font = [NSFont systemFontOfSize:[NSFont smallSystemFontSize]];
+  [[column.get() dataCell] setFont:font];
+  
   [column.get() setHidden:!isVisible];
   [column.get() setEditable:NO];
   [tableView_ addTableColumn:column.get()];
@@ -110,6 +113,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [[NSButtonCell alloc] initTextCell:@""]);
   [nameCell.get() setImagePosition:NSImageLeft];
   [nameCell.get() setButtonType:NSSwitchButton];
+  [nameCell.get() setAlignment:[[nameColumn dataCell] alignment]];
+  [nameCell.get() setFont:[[nameColumn dataCell] font]];
   [nameColumn setDataCell:nameCell.get()];
 
   [self addColumnWithId:IDS_TASK_MANAGER_PHYSICAL_MEM_COLUMN visible:YES];
