72ee18f54a3896d5ac713998887be6f6f41d334d
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-13 06:51:24 +0000

    2010-01-11  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Darin Fisher.
    
            [Chromium] Move getSessionStorageNamespace from WebKitClient to WebViewClient
            https://bugs.webkit.org/show_bug.cgi?id=33492
    
            * public/WebKitClient.h:
            * public/WebViewClient.h:
            (WebKit::WebViewClient::getSessionStorageNamespace):
            * src/StorageNamespaceProxy.cpp:
            (WebCore::StorageNamespace::sessionStorageNamespace):
    2010-01-11  Jeremy Orlow  <jorlow@chromium.org>
    
            Reviewed by Darin Fisher.
    
            [Chromium] Move getSessionStorageNamespace from WebKitClient to WebViewClient
            https://bugs.webkit.org/show_bug.cgi?id=33492
    
            * page/Page.cpp:
            (WebCore::Page::sessionStorage):
            * storage/StorageNamespace.cpp:
            (WebCore::StorageNamespace::sessionStorageNamespace):
            * storage/StorageNamespace.h:
            (WebCore::StorageNamespace::~StorageNamespace):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53180 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/StorageNamespace.h
+++ third_party/WebKit/WebCore/storage/StorageNamespace.h
@@ -36,21 +36,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    class SecurityOrigin;
-    class StorageArea;
+class Page;
+class SecurityOrigin;
+class StorageArea;
 
-    // This interface is required for Chromium since these actions need to be proxied between processes.
-    class StorageNamespace : public RefCounted<StorageNamespace> {
-    public:
-        static PassRefPtr<StorageNamespace> localStorageNamespace(const String& path, unsigned quota);
-        static PassRefPtr<StorageNamespace> sessionStorageNamespace();
+// This interface is required for Chromium since these actions need to be proxied between processes.
+class StorageNamespace : public RefCounted<StorageNamespace> {
+public:
+    static PassRefPtr<StorageNamespace> localStorageNamespace(const String& path, unsigned quota);
+    static PassRefPtr<StorageNamespace> sessionStorageNamespace(Page*);
 
-        virtual ~StorageNamespace() { }
-        virtual PassRefPtr<StorageArea> storageArea(PassRefPtr<SecurityOrigin>) = 0;
-        virtual PassRefPtr<StorageNamespace> copy() = 0;
-        virtual void close() = 0;
-        virtual void unlock() = 0;
-    };
+    virtual ~StorageNamespace() { }
+    virtual PassRefPtr<StorageArea> storageArea(PassRefPtr<SecurityOrigin>) = 0;
+    virtual PassRefPtr<StorageNamespace> copy() = 0;
+    virtual void close() = 0;
+    virtual void unlock() = 0;
+};
 
 } // namespace WebCore
 
