c138db4c7ea9a7b3727520c7f455047e8d4c01bb
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-13 02:09:53 +0000

    2010-01-12  Fumitoshi Ukai  <ukai@chromium.org>
            Reviewed by Darin Adler.
    
            Add a test of WebSockets for shared workers
            https://bugs.webkit.org/show_bug.cgi?id=33285
    
            * websocket/tests/workers/resources/worker-simple.js:
            * websocket/tests/workers/shared-worker-simple-expected.txt: Added.
            * websocket/tests/workers/shared-worker-simple.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/websocket/tests/workers/resources/worker-simple.js
+++ third_party/WebKit/LayoutTests/websocket/tests/workers/resources/worker-simple.js
@@ -2,9 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 if (self.postMessage)
     runTests();
 else
-    onconnect = handleConnect();
+    onconnect = handleConnect;
 
-function handleConnect(evevnt)
+function handleConnect(event)
 {
     // For shared workers, create a faux postMessage() API to send message back to the parent page.
     self.postMessage = function (message) { event.ports[0].postMessage(message); };
@@ -13,6 +13,7 @@ function handleConnect(evevnt)
 
 function runTests()
 {
+    var ws;
     try {
         postMessage("PASS: worker: init");
         if ('WebSocket' in self)
