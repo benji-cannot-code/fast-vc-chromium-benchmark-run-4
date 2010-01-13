7ce65238c5f89a40c450ec48d194294cb7352298
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-13 00:22:24 +0000

    Webkit update to 53151.
    TBR=atwilson
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/546020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36069 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/errorpage_uitest.cc
+++ chrome/browser/errorpage_uitest.cc
@@ -53,7 +53,7 @@ TEST_F(ErrorPageTest, FLAKY_DNSError_GoBack1) {
 }
 
 // Flaky, http://crbug.com/19361.
-TEST_F(ErrorPageTest, FLAKY_DNSError_GoBack2) {
+TEST_F(ErrorPageTest, DISABLED_DNSError_GoBack2) {
   // Test that a DNS error occuring in the main frame does not result in an
   // additional session history entry.
   GURL test_url(URLRequestFailedDnsJob::kTestUrl);
@@ -77,7 +77,7 @@ TEST_F(ErrorPageTest, FLAKY_DNSError_GoBack2) {
 }
 
 // Flaky, http://crbug.com/19361.
-TEST_F(ErrorPageTest, FLAKY_DNSError_GoBack2AndForward) {
+TEST_F(ErrorPageTest, DISABLED_DNSError_GoBack2AndForward) {
   // Test that a DNS error occuring in the main frame does not result in an
   // additional session history entry.
 
@@ -105,7 +105,7 @@ TEST_F(ErrorPageTest, FLAKY_DNSError_GoBack2AndForward) {
 }
 
 // Flaky, http://crbug.com/19361.
-TEST_F(ErrorPageTest, FLAKY_DNSError_GoBack2Forward2) {
+TEST_F(ErrorPageTest, DISABLED_DNSError_GoBack2Forward2) {
   // Test that a DNS error occuring in the main frame does not result in an
   // additional session history entry.
 
