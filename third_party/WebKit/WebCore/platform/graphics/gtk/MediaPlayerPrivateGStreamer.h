06079c1cb41827bf3f1690c46ca7d594ee550970
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-14 11:40:18 +0000

    2009-12-09  Philippe Normand  <pnormand@igalia.com>
            Reviewed by Xan Lopez.
    
            [GStreamer] Check return values of gst_element_set_state()
            https://bugs.webkit.org/show_bug.cgi?id=30000
    
            Check for state change failure when going from READY/NULL to
            PAUSED or PLAYING. Also refactored the common code of play() and
            pause() to a new private method of the player.
    
            * platform/graphics/gtk/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::MediaPlayerPrivate::changePipelineState):
            (WebCore::MediaPlayerPrivate::play):
            (WebCore::MediaPlayerPrivate::pause):
            * platform/graphics/gtk/MediaPlayerPrivateGStreamer.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gtk/MediaPlayerPrivateGStreamer.h
+++ third_party/WebKit/WebCore/platform/graphics/gtk/MediaPlayerPrivateGStreamer.h
@@ -121,6 +121,7 @@ class MediaPlayerPrivate : public MediaPlayerPrivateInterface {
             void startEndPointTimerIfNeeded();
 
             void createGSTPlayBin(String url);
+            bool changePipelineState(GstState state);
 
         private:
             MediaPlayer* m_player;
