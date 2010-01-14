fb87827677e978ada02912d32e4563a68db2eabf
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-14 00:49:00 +0000

    2010-01-13  Adam Barth  <abarth@webkit.org>
            Reviewed by Darin Adler.
    
            REGRESSION(53133): webkit-patch land dies if it can't find a bug id.
            https://bugs.webkit.org/show_bug.cgi?id=33634
    
            Deal with the case of no bug patch object in the state.
    
            * Scripts/webkitpy/steps/updatechangelogswithreview_unittests.py:
            * Scripts/webkitpy/steps/updatechangelogswithreviewer.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53215 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/steps/updatechangelogswithreview_unittests.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/steps/updatechangelogswithreview_unittests.py
@@ -30,12 +30,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 
 from webkitpy.steps.updatechangelogswithreviewer import UpdateChangeLogsWithReviewer
+from webkitpy.mock import Mock
 from webkitpy.mock_bugzillatool import MockBugzillaTool
 from webkitpy.outputcapture import OutputCapture
 
-class UpdateChangeLogsWithReviewerStepTest(unittest.TestCase):
+class UpdateChangeLogsWithReviewerTest(unittest.TestCase):
     def test_guess_reviewer_from_bug(self):
         capture = OutputCapture()
-        step = UpdateChangeLogsWithReviewer(MockBugzillaTool(), [])
+        step = UpdateChangeLogsWithReviewer(MockBugzillaTool(), Mock())
         expected_stderr = "0 reviewed patches on bug 75, cannot infer reviewer.\n"
         capture.assert_outputs(self, step._guess_reviewer_from_bug, [75], expected_stderr=expected_stderr)
+
+    def test_empty_state(self):
+        capture = OutputCapture()
+        step = UpdateChangeLogsWithReviewer(MockBugzillaTool(), Mock())
+        capture.assert_outputs(self, step.run, [{}])
