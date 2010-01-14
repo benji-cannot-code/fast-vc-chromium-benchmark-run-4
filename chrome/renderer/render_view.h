4f72750206c60b58a7630a9d38b9930123e29019
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-14 18:07:55 +0000

    Revert 36246 (mistakingly included unrelated code).
    One of my previous CL accidentaly reverted the change 35951
    for this 2 files. (not sure how it happened).
    Relanding the change.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/548035
    
    TBR=jcampan@chromium.org
    Review URL: http://codereview.chromium.org/536062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36248 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.h
+++ chrome/renderer/render_view.h
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest_prod.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebConsoleMessage.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebContextMenuData.h"
-#include "third_party/WebKit/WebKit/chromium/public/WebEventListener.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFrameClient.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebMediaPlayerAction.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebNode.h"
@@ -117,7 +116,6 @@ typedef base::RefCountedData<int> SharedRenderViewCounter;
 // communication interface with an embedding application process
 //
 class RenderView : public RenderWidget,
-                   public WebKit::WebEventListener,
                    public WebKit::WebViewClient,
                    public WebKit::WebFrameClient,
                    public WebKit::WebPageSerializerClient,
@@ -185,9 +183,6 @@ class RenderView : public RenderWidget,
       int status);
   virtual void UserMetricsRecordAction(const std::string& action);
   virtual void DnsPrefetch(const std::vector<std::string>& host_names);
-
-  // WebKit::WebEventListener
-  virtual void handleEvent(WebKit::WebEvent* event);
 
   // WebKit::WebViewClient
   virtual WebKit::WebView* createView(WebKit::WebFrame* creator);
@@ -521,9 +516,11 @@ class RenderView : public RenderWidget,
                         SkBitmap* thumbnail,
                         ThumbnailScore* score);
 
-  // Capture a snapshot of a view.  This is used to allow an extension
-  // to get a snapshot of a tab using chrome.tabs.captureVisibleTab().
-  bool CaptureSnapshot(WebKit::WebView* view, SkBitmap* snapshot);
+  // Calculates how "boring" a thumbnail is. The boring score is the
+  // 0,1 ranged percentage of pixels that are the most common
+  // luma. Higher boring scores indicate that a higher percentage of a
+  // bitmap are all the same brightness.
+  double CalculateBoringScore(SkBitmap* bitmap);
 
   bool RunJavaScriptMessage(int type,
                             const std::wstring& message,
@@ -537,7 +534,6 @@ class RenderView : public RenderWidget,
 
   // RenderView IPC message handlers
   void SendThumbnail();
-  void SendSnapshot();
   void OnPrintPages();
   void OnPrintingDone(int document_cookie, bool success);
   void OnNavigate(const ViewMsg_Navigate_Params& params);
