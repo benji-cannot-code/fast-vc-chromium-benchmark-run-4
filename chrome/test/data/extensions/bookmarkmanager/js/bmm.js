73a5f22d9a784b6e673a74bdb40f80711079b0b2
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-14 20:15:32 +0000

    Bookmark manager extension
    This is an early change list to get early feedback on the code.
    
    BUG=None
    TEST=Load unpacked extension and then go to chrome-extensions/<ID>/main.html
    
    Review URL: http://codereview.chromium.org/534001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36262 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/bookmarkmanager/js/bmm.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+cr.define('bmm', function() {
+  function isFolder(bookmarkNode) {
+    return !bookmarkNode.url;
+  }
+
+  function contains(parent, descendant) {
+    if (descendant.parentId == parent.id)
+      return true;
+    // the bmm.treeLookup contains all folders
+    var parentTreeItem = bmm.treeLookup[descendant.parentId];
+    if (!parentTreeItem || !parentTreeItem.bookmarkNode)
+      return false;
+    return this.contains(parent, parentTreeItem.bookmarkNode);
+  }
+
+  return {
+    isFolder: isFolder,
+    contains: contains
+  };
+});
