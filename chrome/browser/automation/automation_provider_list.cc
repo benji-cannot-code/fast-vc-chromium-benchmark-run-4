a4aa5256d5c404904f35280a8355c969538fcefd
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-14 15:25:13 +0000

    Fix a Chrome crash which occurs when the chrome instance launched via ChromeFrame is shutting down. I was not able to reproduce this locally. Based on the crash dump, it looks like the crash occurs when the sync channel used by the automation provider is shutting down and it is attempting a PostTask of OnChannelClosed. The crash occurs while posting the task to the message loop, because the message loop is invalid.
    This could occur if the message loop has been destroyed. The message loop is destroyed when the module
    refcount drops to zero. The automation provider releases the module ref count in OnChannelError. In theory
    there is a race condition here, i.e. the message loop could be destroyed before the ExternalTabContainer,
    etc are destroyed.
    
    Suggested fix is to call AddRefModule which grabs a reference on the UI message loop in the automation provider
    constructor and release it in the destructor. We also close the channel before invoking ReleaseModule in the
    destructor.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=31621
    
    Bug=31621
    
    Review URL: http://codereview.chromium.org/542036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36240 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider_list.cc
+++ chrome/browser/automation/automation_provider_list.cc
@@ -21,7 +21,6 @@ AutomationProviderList::~AutomationProviderList() {
   while (iter != automation_providers_.end()) {
     (*iter)->Release();
     iter = automation_providers_.erase(iter);
-    g_browser_process->ReleaseModule();
   }
   instance_ = NULL;
 }
@@ -29,7 +28,6 @@ AutomationProviderList::~AutomationProviderList() {
 bool AutomationProviderList::AddProvider(AutomationProvider* provider) {
   provider->AddRef();
   automation_providers_.push_back(provider);
-  g_browser_process->AddRefModule();
   return true;
 }
 
@@ -39,7 +37,6 @@ bool AutomationProviderList::RemoveProvider(AutomationProvider* provider) {
   if (remove_provider != automation_providers_.end()) {
     (*remove_provider)->Release();
     automation_providers_.erase(remove_provider);
-    g_browser_process->ReleaseModule();
     if (automation_providers_.empty())
       OnLastProviderRemoved();
     return true;
