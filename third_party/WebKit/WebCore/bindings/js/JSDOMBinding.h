4d8d7e3bfc61f1ae64be9971fb923e64b27e69dc
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-14 06:45:27 +0000

    <rdar://problem/7403736> REGRESSION (r49963,r49965): 8% Dromaeo Core DOM test regression
    Reviewed by Oliver Hunt.
    
    Adding isolated worlds support to the JSC bindings introduced a regression due to
    additional map lookups.  Add a mechanism to quickly detect that a lookup is for the
    'normal' world, and add fast paths to a couple of methods to check the normal world
    first.
    
    * bindings/js/JSDOMBinding.cpp:
    (WebCore::Document::getWrapperCache):
    (WebCore::DOMWrapperWorld::DOMWrapperWorld):
    (WebCore::forgetDOMObject):
    * bindings/js/JSDOMBinding.h:
    (WebCore::DOMWrapperWorld::isNormal):
    (WebCore::WebCoreJSClientData::WebCoreJSClientData):
    * bindings/js/ScriptController.cpp:
    (WebCore::IsolatedWorld::IsolatedWorld):
    * dom/Document.cpp:
    (WebCore::Document::Document):
    (WebCore::Document::createWrapperCache):
    * dom/Document.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53240 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDOMBinding.h
+++ third_party/WebKit/WebCore/bindings/js/JSDOMBinding.h
@@ -145,7 +145,7 @@ namespace WebCore {
 
     class DOMWrapperWorld : public RefCounted<DOMWrapperWorld> {
     public:
-        DOMWrapperWorld(JSC::JSGlobalData*);
+        DOMWrapperWorld(JSC::JSGlobalData*, bool isNormal);
         ~DOMWrapperWorld();
 
         void rememberDocument(Document* document) { documentsWithWrappers.add(document); }
@@ -154,9 +154,12 @@ namespace WebCore {
         // FIXME: can we make this private?
         DOMObjectWrapperMap m_wrappers;
 
+        bool isNormal() const { return m_isNormal; }
+
     private:
         JSC::JSGlobalData* m_globalData;
         HashSet<Document*> documentsWithWrappers;
+        bool m_isNormal;
     };
 
     // Map from static HashTable instances to per-GlobalData ones.
@@ -188,7 +191,7 @@ namespace WebCore {
 
     public:
         WebCoreJSClientData(JSC::JSGlobalData* globalData)
-            : m_normalWorld(globalData)
+            : m_normalWorld(globalData, true)
         {
             m_worldSet.add(&m_normalWorld);
         }
