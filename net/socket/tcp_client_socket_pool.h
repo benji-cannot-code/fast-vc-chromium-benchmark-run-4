8f34d851b61f557f88a0d767c4d7fce027cb356b
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-14 06:31:32 +0000

    Switch on socket late binding. Histograms for the 4.0.266.0 dev channel release indicate the following changes for late binding: (a) Net.TCPSocketType shows a decrease (from 41.85% to 39.29%) in used of newly connected sockets.  Part of this decrease is due to using previously used sockets more often (increase from 58.15% to 58.53%), but is primarily due to being able to use sockets that were connected, but not immediately handed over to a socket request (increased from 0 [not supported without late binding] to 2.18%). (b) Net.SocketIdleTimeBeforeNextUse_ReusedSocket indicates that reused sockets are getting used more quickly than before, with a decrease of mean idle time from 11.65 seconds to 11.34 seconds. (c) Net.Transaction_Connected_Under_10 indicates shows that the mean for time until the first byte of the transaction response decreased from 1585ms to 1481ms. The code change deletes the old non socket late binding code paths, cleaning up the code significantly.  It also deletes duplicated tests in ClientSocketPoolBase which covered both pathways.  A TCPClientSocketPool test had to be updated as well. BUG=http://crbug.com/30354.
    Review URL: http://codereview.chromium.org/543051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36230 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/tcp_client_socket_pool.h
+++ net/socket/tcp_client_socket_pool.h
@@ -27,7 +27,6 @@ class TCPConnectJob : public ConnectJob {
  public:
   TCPConnectJob(const std::string& group_name,
                 const HostResolver::RequestInfo& resolve_info,
-                const ClientSocketHandle* handle,
                 base::TimeDelta timeout_duration,
                 ClientSocketFactory* client_socket_factory,
                 HostResolver* host_resolver,
