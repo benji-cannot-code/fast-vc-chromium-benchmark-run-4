9af5b54286077120eb291d58e4e29f41ea5230df
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-14 06:37:13 +0000

    2010-01-13  Kent Hansen  <kent.hansen@nokia.com>
            Reviewed by Oliver Hunt.
    
            JSON.stringify and JSON.parse needlessly process properties in the prototype chain
            https://bugs.webkit.org/show_bug.cgi?id=33053
    
            * runtime/JSONObject.cpp:
            (JSC::Stringifier::Holder::appendNextProperty):
            (JSC::Walker::walk):
    2010-01-13  Kent Hansen  <kent.hansen@nokia.com>
    
            Reviewed by Oliver Hunt.
    
            JSON.stringify and JSON.parse needlessly process properties in the prototype chain
            https://bugs.webkit.org/show_bug.cgi?id=33053
    
            Add tests that verify that properties in the prototype chain are ignored.
    
            * fast/js/JSON-parse-expected.txt:
            * fast/js/JSON-stringify-expected.txt:
            * fast/js/resources/JSON-parse.js:
            * fast/js/resources/JSON-stringify.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53239 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSONObject.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSONObject.cpp
@@ -502,7 +502,7 @@ bool Stringifier::Holder::appendNextProperty(Stringifier& stringifier, StringBui
                 m_propertyNames = stringifier.m_arrayReplacerPropertyNames.data();
             else {
                 PropertyNameArray objectPropertyNames(exec);
-                m_object->getPropertyNames(exec, objectPropertyNames);
+                m_object->getOwnPropertyNames(exec, objectPropertyNames);
                 m_propertyNames = objectPropertyNames.releaseData();
             }
             m_size = m_propertyNames->propertyNameVector().size();
@@ -747,7 +747,7 @@ NEVER_INLINE JSValue Walker::walk(JSValue unfiltered)
                 objectStack.append(object);
                 indexStack.append(0);
                 propertyStack.append(PropertyNameArray(m_exec));
-                object->getPropertyNames(m_exec, propertyStack.last());
+                object->getOwnPropertyNames(m_exec, propertyStack.last());
                 // fallthrough
             }
             objectStartVisitMember:
