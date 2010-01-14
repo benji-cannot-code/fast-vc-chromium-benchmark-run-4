368f131a9a4838cd5e75676aeef03ba4210dcfed
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-14 08:14:36 +0000

    Revert 36230 - Switch on socket late binding. Broke mac valgrind for test_shell_tests.  Various errors.  I also missed fixing a bug that eroman pointed out in review.  Total brainfart. Histograms for the 4.0.266.0 dev channel release indicate the following changes for late binding: (a) Net.TCPSocketType shows a decrease (from 41.85% to 39.29%) in used of newly connected sockets.  Part of this decrease is due to using previously used sockets more often (increase from 58.15% to 58.53%), but is primarily due to being able to use sockets that were connected, but not immediately handed over to a socket request (increased from 0 [not supported without late binding] to 2.18%). (b) Net.SocketIdleTimeBeforeNextUse_ReusedSocket indicates that reused sockets are getting used more quickly than before, with a decrease of mean idle time from 11.65 seconds to 11.34 seconds. (c) Net.Transaction_Connected_Under_10 indicates shows that the mean for time until the first byte of the transaction response decreased from 1585ms to 1481ms. The code change deletes the old non socket late binding code paths, cleaning up the code significantly.  It also deletes duplicated tests in ClientSocketPoolBase which covered both pathways.  A TCPClientSocketPool test had to be updated as well. BUG=http://crbug.com/30354.
    Review URL: http://codereview.chromium.org/543051
    
    TBR=willchan@chromium.org
    Review URL: http://codereview.chromium.org/549047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36231 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/tcp_client_socket_pool.h
+++ net/socket/tcp_client_socket_pool.h
@@ -27,6 +27,7 @@ class TCPConnectJob : public ConnectJob {
  public:
   TCPConnectJob(const std::string& group_name,
                 const HostResolver::RequestInfo& resolve_info,
+                const ClientSocketHandle* handle,
                 base::TimeDelta timeout_duration,
                 ClientSocketFactory* client_socket_factory,
                 HostResolver* host_resolver,
