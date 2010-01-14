afa089f15bee421a9d3be127e6fef77b0602fa0e
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-14 19:03:39 +0000

    Reviewed by Brady Eidson.
            https://bugs.webkit.org/show_bug.cgi?id=33652
            REGRESSION: Frames stop appearing after browsing for a while
    
            <rdar://problem/7308312> REGRESSION: iBench gets slower after several runs
    
            I do not know how not creating renderers for frames could make iBench slower, but it did.
    
            Test: fast/frames/cached-frame-counter.html
    
            * history/CachedFrame.cpp:
            (WebCore::CachedFrame::CachedFrame): A frame that's cached is moved away from Page,
            so decrement frame count here.
            (WebCore::CachedFrame::open): Increase frame count when a frame is restored.
    
            * history/CachedFrame.h: Remove unneeded forward declarations.
    
            * history/CachedPage.h: Ditto.
    
            * history/CachedPage.cpp: (WebCore::CachedPage::restore): Assert that page frame count is
            zero prior to restoring a page from b/f cache.
            
            * page/Page.cpp: (WebCore::Page::checkFrameCountConsistency): A debug-only method to
            check that frame count stored in Page matches the number of frames in frame tree.
    
            * page/Page.h:
            (WebCore::Page::decrementFrameCount): Assert that frame count is positive.
            (WebCore::Page::frameCount): Call checkFrameCountConsistency().
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/history/CachedFrame.h
+++ third_party/WebKit/WebCore/history/CachedFrame.h
@@ -32,15 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/RefPtr.h>
 
 namespace WebCore {
-    
-    class CachedFrame;
-    class CachedFramePlatformData;
-    class DOMWindow;
-    class Document;
-    class DocumentLoader;
-    class Frame;
-    class FrameView;
-    class Node;
+
+class CachedFrame;
+class CachedFramePlatformData;
+class Document;
+class DocumentLoader;
+class FrameView;
+class Node;
 
 typedef Vector<RefPtr<CachedFrame> > CachedFrameVector;
 
--- third_party/WebKit/WebCore/history/CachedPage.h
+++ third_party/WebKit/WebCore/history/CachedPage.h
@@ -31,14 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
     
-    class CachedFramePlatformData;
-    class DOMWindow;
-    class Document;
-    class DocumentLoader;
-    class FrameView;
-    class KURL;
-    class Node;
-    class Page;
+class Document;
+class DocumentLoader;
+class Page;
 
 class CachedPage : public RefCounted<CachedPage> {
 public:
