2695c30f790f0f0a7e937c324500b4199b614203
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-14 12:15:03 +0000

    2010-01-14  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Enable scrolling optimization for pages with embedded widgets
    
            https://bugs.webkit.org/show_bug.cgi?id=33373
    
            Added a basic manual test for scrolling of embedded QWidgets.
    
            * manual-tests/qt/qtplugin-scrolling.html: Added.
            * platform/ScrollView.cpp:
            (WebCore::ScrollView::scrollContents):
            (WebCore::ScrollView::setParent):
            * platform/ScrollView.h:
            * platform/qt/ScrollViewQt.cpp:
            (WebCore::ScrollView::platformInit):
            (WebCore::ScrollView::platformAddChild):
            (WebCore::ScrollView::platformRemoveChild):
            * plugins/qt/PluginViewQt.cpp:
            (WebCore::PluginView::updatePluginWidget):
            (WebCore::PluginView::invalidateRect):
    2010-01-14  Andreas Kling  <andreas.kling@nokia.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Add an "alien_QLabel" classId for manual testing of alien widgets.
    
            https://bugs.webkit.org/show_bug.cgi?id=33373
    
            * QtLauncher/main.cpp:
            (WebPage::createPlugin):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/qt/PluginViewQt.cpp
+++ third_party/WebKit/WebCore/plugins/qt/PluginViewQt.cpp
@@ -128,6 +128,10 @@ void PluginView::updatePluginWidget()
     // scroll, we need to move/resize immediately.
     if (!m_windowRect.intersects(frameView->frameRect()))
         setNPWindowIfNeeded();
+
+    // Make sure we get repainted afterwards. This is necessary for downward
+    // scrolling to move the plugin widget properly.
+    invalidate();
 }
 
 void PluginView::setFocus()
@@ -645,7 +649,8 @@ NPError PluginView::getValue(NPNVariable variable, void* value)
 void PluginView::invalidateRect(const IntRect& rect)
 {
     if (m_isWindowed) {
-        platformWidget()->update(rect);
+        if (platformWidget())
+            platformWidget()->update(rect);
         return;
     }
 
