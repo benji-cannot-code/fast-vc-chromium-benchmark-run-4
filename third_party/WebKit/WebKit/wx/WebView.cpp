14fab791d8bede29f9caa05fa616b73430ad282f
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-14 19:10:19 +0000

    wx fixes after XBM removal and recent changes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@53275 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/wx/WebView.cpp
+++ third_party/WebKit/WebKit/wx/WebView.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Editor.h"
 #include "EmptyClients.h"
 #include "EventHandler.h"
+#include "FileChooser.h"
 #include "FocusController.h"
 #include "Frame.h"
 #include "FrameLoader.h"
