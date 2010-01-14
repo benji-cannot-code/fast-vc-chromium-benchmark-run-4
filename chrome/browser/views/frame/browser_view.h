b6445c4379507cf2657fb543d503aca3b0415d2f
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-14 22:09:19 +0000

    Factoring out the layout code from browser_view in preparation to implement side bar. (and this will also allow us to clean up browser extender)  - BrowserLayoutManager interface with extra layout methods for BrowserView  - ChromeLayoutManager implements the same layout policy as of today.  - Added new view IDs so that layout manager can identify comoponents added to the BrowserView.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/543035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36276 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_view.h
+++ chrome/browser/views/frame/browser_view.h
@@ -37,6 +37,7 @@ class BookmarkBarView;
 class Browser;
 class BrowserBubble;
 class BrowserExtender;
+class BrowserLayoutManager;
 class DownloadShelfView;
 class EncodingMenuModel;
 class ExtensionShelf;
@@ -356,6 +357,9 @@ class BrowserView : public BrowserWindow,
     return browser_extender_.get();
   }
 
+  // Layout the Status Bubble.
+  void LayoutStatusBubble(int top);
+
  protected:
   // Overridden from views::View:
   virtual void Layout();
@@ -372,28 +376,8 @@ class BrowserView : public BrowserWindow,
   // Creates the system menu.
   void InitSystemMenu();
 #endif
-
-  // Layout the TabStrip, returns the coordinate of the bottom of the TabStrip,
-  // for laying out subsequent controls.
-  int LayoutTabStrip();
-  // Layout the following controls, starting at |top|, returns the coordinate
-  // of the bottom of the control, for laying out the next control.
-  int LayoutToolbar(int top);
-  int LayoutBookmarkAndInfoBars(int top);
-  int LayoutTopBar(int top);
-  int LayoutInfoBar(int top);
-  // Layout the TabContents container, between the coordinates |top| and
-  // |bottom|.
-  void LayoutTabContents(int top, int bottom);
-  int LayoutExtensionAndDownloadShelves();
-  // Layout the Extension Shelf, returns the coordinate of the top of the
-  // control, for laying out the previous control.
-  int LayoutExtensionShelf(int bottom);
-  // Layout the Download Shelf, returns the coordinate of the top of the
-  // control, for laying out the previous control.
-  int LayoutDownloadShelf(int bottom);
-  // Layout the Status Bubble.
-  void LayoutStatusBubble(int top);
+  // Returns the BrowserLayoutManager.
+  BrowserLayoutManager* GetBrowserLayoutManager() const;
 
   // Prepare to show the Bookmark Bar for the specified TabContents. Returns
   // true if the Bookmark Bar can be shown (i.e. it's supported for this
@@ -477,9 +461,6 @@ class BrowserView : public BrowserWindow,
 
   // The InfoBarContainer that contains InfoBars for the current tab.
   InfoBarContainer* infobar_container_;
-
-  // The distance the FindBar is from the top of the window, in pixels.
-  int find_bar_y_;
 
   // The view that contains the selected TabContents.
   TabContentsContainer* contents_container_;
