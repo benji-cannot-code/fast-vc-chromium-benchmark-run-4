a1cfe39ae09a2fedeea18884051eef7f6600891a
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-14 00:55:17 +0000

    2010-01-13  Adam Barth  <abarth@webkit.org>
            Unreviewed.  Repeat the below for CloseBugForLandDiff.
    
            * Scripts/test-webkitpy:
            * Scripts/webkitpy/steps/closebugforlanddiff.py:
            * Scripts/webkitpy/steps/closebugforlanddiff_unittest.py: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/steps/closebugforlanddiff_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/steps/closebugforlanddiff_unittest.py
@@ -1,20 +1,20 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (C) 2010 Google Inc. All rights reserved.
-# 
+# Copyright (C) 2009 Google Inc. All rights reserved.
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
-#     * Redistributions of source code must retain the above copyright
+#
+#    * Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
+#    * Redistributions in binary form must reproduce the above
 # copyright notice, this list of conditions and the following disclaimer
 # in the documentation and/or other materials provided with the
 # distribution.
-#     * Neither the name of Google Inc. nor the names of its
+#    * Neither the name of Google Inc. nor the names of its
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -27,33 +27,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from webkitpy.comments import bug_comment_from_commit_text
-from webkitpy.steps.abstractstep import AbstractStep
-from webkitpy.steps.options import Options
-from webkitpy.webkit_logging import log
+import unittest
 
+from webkitpy.steps.closebugforlanddiff import CloseBugForLandDiff
+from webkitpy.mock import Mock
+from webkitpy.mock_bugzillatool import MockBugzillaTool
+from webkitpy.outputcapture import OutputCapture
 
-class CloseBugForLandDiff(AbstractStep):
-    @classmethod
-    def options(cls):
-        return [
-            Options.close_bug,
-        ]
-
-    def run(self, state):
-        comment_text = bug_comment_from_commit_text(self._tool.scm(), state["commit_text"])
-        bug_id = state.get("bug_id")
-        if not bug_id and state.get("patch"):
-            bug_id = state.get("patch").bug_id()
-
-        if bug_id:
-            log("Updating bug %s" % bug_id)
-            if self._options.close_bug:
-                self._tool.bugs.close_bug_as_fixed(bug_id, comment_text)
-            else:
-                # FIXME: We should a smart way to figure out if the patch is attached
-                # to the bug, and if so obsolete it.
-                self._tool.bugs.post_comment_to_bug(bug_id, comment_text)
-        else:
-            log(comment_text)
-            log("No bug id provided.")
+class UpdateChangeLogsWithReviewerTest(unittest.TestCase):
+    def test_empty_state(self):
+        capture = OutputCapture()
+        step = CloseBugForLandDiff(MockBugzillaTool(), Mock())
+        capture.assert_outputs(self, step.run, [{}])
