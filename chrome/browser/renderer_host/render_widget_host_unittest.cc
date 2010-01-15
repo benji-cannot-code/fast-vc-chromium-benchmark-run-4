2add8dbef73328dc82151a02fca566c8f19e989b
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-15 22:15:52 +0000

    Mac: Coalesce scroll events.
    Based on work by trungl, codereview issue 502056. He's busy, but I think this is an important patch, so I'll try to get it through review for him.
    
    BUG=28954
    TEST=On a MacBook (Pro) -- the slower the better -- go to <http://www.starcraft2.com/faq.xml> and make sure that scrolling using a two-fingered swipe on the trackpad is much faster than before. Also make sure that using the mouse wheel to scroll remains okay with various devices on all platforms.
    
    Review URL: http://codereview.chromium.org/536059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_unittest.cc
+++ chrome/browser/renderer_host/render_widget_host_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 using WebKit::WebInputEvent;
+using WebKit::WebMouseWheelEvent;
 
 // RenderWidgetHostProcess -----------------------------------------------------
 
@@ -120,7 +121,7 @@ class TestView : public TestRenderWidgetHostView {
   DISALLOW_COPY_AND_ASSIGN(TestView);
 };
 
-// MockRenderWidgetHostTest ----------------------------------------------------
+// MockRenderWidgetHost ----------------------------------------------------
 
 class MockRenderWidgetHost : public RenderWidgetHost {
  public:
@@ -219,6 +220,15 @@ class RenderWidgetHostTest : public testing::Test {
     key_event.type = type;
     key_event.windowsKeyCode = base::VKEY_L;  // non-null made up value.
     host_->ForwardKeyboardEvent(key_event);
+  }
+
+  void SimulateWheelEvent(float dX, float dY, int modifiers) {
+    WebMouseWheelEvent wheel_event;
+    wheel_event.type = WebInputEvent::MouseWheel;
+    wheel_event.deltaX = dX;
+    wheel_event.deltaY = dY;
+    wheel_event.modifiers = modifiers;
+    host_->ForwardWheelEvent(wheel_event);
   }
 
   MessageLoopForUI message_loop_;
@@ -556,4 +566,38 @@ TEST_F(RenderWidgetHostTest, PreHandleRawKeyDownEvent) {
 
   EXPECT_TRUE(host_->unhandled_keyboard_event_called());
   EXPECT_EQ(WebInputEvent::KeyUp, host_->unhandled_keyboard_event_type());
+}
+
+TEST_F(RenderWidgetHostTest, CoalescesWheelEvents) {
+  process_->sink().ClearMessages();
+
+  // Simulate wheel events.
+  SimulateWheelEvent(0, -5, 0);  // sent directly
+  SimulateWheelEvent(0, -10, 0);  // enqueued
+  SimulateWheelEvent(8, -6, 0);  // coalesced into previous event
+  SimulateWheelEvent(9, -7, 1);  // enqueued, different modifiers
+
+  // Check that only the first event was sent.
+  EXPECT_EQ(1U, process_->sink().message_count());
+  EXPECT_TRUE(process_->sink().GetUniqueMessageMatching(
+                  ViewMsg_HandleInputEvent::ID));
+  process_->sink().ClearMessages();
+
+  // Check that the ACK sends the second message.
+  SendInputEventACK(WebInputEvent::MouseWheel, true);
+  EXPECT_EQ(1U, process_->sink().message_count());
+  EXPECT_TRUE(process_->sink().GetUniqueMessageMatching(
+                  ViewMsg_HandleInputEvent::ID));
+  process_->sink().ClearMessages();
+
+  // One more time.
+  SendInputEventACK(WebInputEvent::MouseWheel, true);
+  EXPECT_EQ(1U, process_->sink().message_count());
+  EXPECT_TRUE(process_->sink().GetUniqueMessageMatching(
+                  ViewMsg_HandleInputEvent::ID));
+  process_->sink().ClearMessages();
+
+  // After the final ack, the queue should be empty.
+  SendInputEventACK(WebInputEvent::MouseWheel, true);
+  EXPECT_EQ(0U, process_->sink().message_count());
 }
