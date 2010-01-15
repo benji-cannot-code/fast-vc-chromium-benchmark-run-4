41810ba78e547c80865685ba8012f60dd7f2be77
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-15 18:44:42 +0000

    Factoring out the layout code from browser_view in preparation to implement side bar. (and this will also allow us to clean up browser extender)  - BrowserLayoutManager interface with extra layout methods for BrowserView  - ChromeLayoutManager implements the same layout policy as of today.  - Added new view IDs so that layout manager can identify comoponents added to the BrowserView.
    BUG=none
    TEST=none
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=36276
    
    Review URL: http://codereview.chromium.org/543035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36377 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/infobars/infobar_container.cc
+++ chrome/browser/views/infobars/infobar_container.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/tab_contents/infobar_delegate.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/browser/view_ids.h"
 #include "chrome/browser/views/frame/browser_view.h"
 #include "chrome/browser/views/infobars/infobars.h"
 #include "chrome/common/notification_service.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 InfoBarContainer::InfoBarContainer(BrowserView* browser_view)
     : browser_view_(browser_view),
       tab_contents_(NULL) {
+  SetID(VIEW_ID_INFO_BAR_CONTAINER);
 }
 
 InfoBarContainer::~InfoBarContainer() {
