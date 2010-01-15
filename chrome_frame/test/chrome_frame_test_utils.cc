28a7007e33b38fc070c1a30f53dff1a4734d0fbe
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-15 22:14:25 +0000

    Changing DLOG to LOG for tests so that we'll get the errors in the test run logs.
    TEST=n/a
    BUG=none
    
    Review URL: http://codereview.chromium.org/552018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36420 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/chrome_frame_test_utils.cc
+++ chrome_frame/test/chrome_frame_test_utils.cc
@@ -52,7 +52,7 @@ BOOL CALLBACK CloseWindowsThreadCallback(HWND hwnd, LPARAM param) {
       DWORD results = 0;
       if (!::SendMessageTimeout(hwnd, WM_SYSCOMMAND, SC_CLOSE, 0, SMTO_BLOCK,
                                 10000, &results)) {
-        DLOG(WARNING) << "Window hung: " << StringPrintf(L"%08X", hwnd);
+        LOG(WARNING) << "Window hung: " << StringPrintf(L"%08X", hwnd);
       }
       count++;
     } else {
@@ -197,7 +197,7 @@ bool EnsureProcessInForeground(base::ProcessId process_id) {
   }
 
   bool ret = ForceSetForegroundWindow(paw.hwnd);
-  DLOG_IF(ERROR, !ret) << "ForceSetForegroundWindow: " << ret;
+  LOG_IF(ERROR, !ret) << "ForceSetForegroundWindow: " << ret;
 
   return ret;
 }
