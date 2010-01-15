7a8ff7f6ff4ce1b84be09efdbfeac538456e1db7
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-15 20:58:44 +0000

    Simplify the gtk AutoFill dialog and bring it more in line with the mocks.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/551034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36402 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/gtk_util.cc
+++ chrome/common/gtk_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gdk/gdkx.h>
 
 #include <cstdarg>
+#include <map>
 
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
@@ -224,15 +225,18 @@ void GetWidgetSizeFromCharacters(GtkWidget* widget, double width_chars,
   g_object_unref(context);
 }
 
-void SetWindowWidthFromResources(GtkWindow* window, int resource_id,
-                                 bool resizable) {
-  int width;
-  gtk_util::GetWidgetSizeFromResources(GTK_WIDGET(window), resource_id, 0,
-                                       &width, NULL);
+void SetWindowSizeFromResources(GtkWindow* window,
+                                int width_id, int height_id, bool resizable) {
+  int width = -1;
+  int height = -1;
+  gtk_util::GetWidgetSizeFromResources(GTK_WIDGET(window), width_id, height_id,
+                                       (width_id != -1) ? &width : NULL,
+                                       (height_id != -1) ? &height : NULL);
+
   if (resizable) {
-    gtk_window_set_default_size(window, width, -1);
+    gtk_window_set_default_size(window, width, height);
   } else {
-    gtk_widget_set_size_request(GTK_WIDGET(window), width, -1);
+    gtk_widget_set_size_request(GTK_WIDGET(window), width, height);
   }
   gtk_window_set_resizable(window, resizable ? TRUE : FALSE);
 }
