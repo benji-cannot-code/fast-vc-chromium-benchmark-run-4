e651d7c923300d5796825c7aaaee9114ddb2c3a6
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-15 20:19:06 +0000

    linux: enable pepper_test_plugin
    This includes a way to build it with full support on x64 (and arm ?), by specifying linux_fpic=1 in GYP_DEFINES and re-running gyp.
    
    Review URL: http://codereview.chromium.org/551041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36399 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/pgl/pgl.cc
+++ gpu/pgl/pgl.cc
@@ -155,7 +155,7 @@ PGLBoolean pglDestroyContext(PGLContext pgl_context) {
   if (!pgl_context)
     return false;
 
-  delete pgl_context;
+  delete static_cast<PGLContextImpl*>(pgl_context);
   return true;
 }
 
