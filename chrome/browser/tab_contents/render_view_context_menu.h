c54424aa6d4d5089b701f11ca186ebbe2cfbc62a
apavlov@chromium.org <apavlov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-15 10:52:44 +0000

    Hide the View Page Source/Info and other irrelevant actions in the DevTools page context menu
    BUG=32073
    TEST=none
    
    Review URL: http://codereview.chromium.org/542057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36345 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_context_menu.h
+++ chrome/browser/tab_contents/render_view_context_menu.h
@@ -77,6 +77,7 @@ class RenderViewContextMenu {
   Profile* profile_;
 
  private:
+  static bool IsDevToolsURL(const GURL& url);
   bool AppendCustomItems();
   void AppendDeveloperItems();
   void AppendLinkItems();
