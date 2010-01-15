dafce4734402e80bcd8395ed78045bc7bf133168
antonm@chromium.org <antonm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-15 11:51:33 +0000

    Remove introduced trailing whitespace.
    TBR=podivilov@chromium.org
    
    Review URL: http://codereview.chromium.org/542088
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36348 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/dromaeo/webrunner.js
+++ chrome/test/data/dromaeo/webrunner.js
@@ -402,7 +402,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 						type: "POST",
 						url: "store.php",
 						data: "data=" + encodeURIComponent(JSON.stringify(window.automation.GetResults()))
-					});  
+					});
 				}
 				else {
 					window.automation.SetDone();
