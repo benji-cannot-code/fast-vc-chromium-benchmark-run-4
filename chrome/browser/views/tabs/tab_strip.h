fa938f8a2dde246138dfbd4c6aab3ba5f9a4e257
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-16 09:10:20 +0000

    Refactoring to reduce chromeos dependency in chrome.   * renamed BrowserLayoutManager to more explicit, BrowserViewLayoutManager.   * Added factory method to create chromeos specific tab/tabstrip and layoutmanagers.   * Introduced ChromeosBrowserView and chromeos specific tab/tabstrip and layoutmanager, and moved chromeos specifi code into them.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/543095
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36452 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/tab_strip.h
+++ chrome/browser/views/tabs/tab_strip.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/controls/button/image_button.h"
 #include "views/view.h"
 
-class BrowserExtender;
 class DraggedTabController;
 class ScopedMouseCloseWidthCalculator;
 class TabStripModel;
@@ -118,7 +117,12 @@ class TabStrip : public views::View,
   virtual void SetAccessibleName(const std::wstring& name);
   virtual views::View* GetViewForPoint(const gfx::Point& point);
   virtual void ThemeChanged();
+
  protected:
+  // Creates a new tab.
+  virtual Tab* CreateTab();
+
+  // views::View implementation:
   virtual void ViewHierarchyChanged(bool is_add,
                                     views::View* parent,
                                     views::View* child);
