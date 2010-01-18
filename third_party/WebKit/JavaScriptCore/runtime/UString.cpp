024d9c928aa98c82aad9aa95de081cb759b99f6d
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-18 12:14:49 +0000

    Rubber-stamped by Gustavo Noronha Silva.
    Rolling out r53391 and r53392 because of random crashes on buildbots.
    https://bugs.webkit.org/show_bug.cgi?id=33731
    
    * bytecode/CodeBlock.h:
    (JSC::CallLinkInfo::seenOnce):
    (JSC::CallLinkInfo::setSeen):
    (JSC::MethodCallLinkInfo::MethodCallLinkInfo):
    (JSC::MethodCallLinkInfo::seenOnce):
    (JSC::MethodCallLinkInfo::setSeen):
    * jit/JIT.cpp:
    (JSC::JIT::unlinkCall):
    * jit/JITPropertyAccess.cpp:
    (JSC::JIT::patchMethodCallProto):
    * runtime/UString.cpp:
    (JSC::UString::spliceSubstringsWithSeparators):
    (JSC::UString::replaceRange):
    * runtime/UString.h:
    * runtime/UStringImpl.cpp:
    (JSC::UStringImpl::baseSharedBuffer):
    (JSC::UStringImpl::sharedBuffer):
    (JSC::UStringImpl::~UStringImpl):
    * runtime/UStringImpl.h:
    (JSC::UntypedPtrAndBitfield::UntypedPtrAndBitfield):
    (JSC::UntypedPtrAndBitfield::asPtr):
    (JSC::UntypedPtrAndBitfield::operator&=):
    (JSC::UntypedPtrAndBitfield::operator|=):
    (JSC::UntypedPtrAndBitfield::operator&):
    (JSC::UStringImpl::create):
    (JSC::UStringImpl::cost):
    (JSC::UStringImpl::isIdentifier):
    (JSC::UStringImpl::setIsIdentifier):
    (JSC::UStringImpl::ref):
    (JSC::UStringImpl::deref):
    (JSC::UStringImpl::checkConsistency):
    (JSC::UStringImpl::UStringImpl):
    (JSC::UStringImpl::bufferOwnerString):
    (JSC::UStringImpl::bufferOwnership):
    (JSC::UStringImpl::isStatic):
    * wtf/StringHashFunctions.h:
    (WTF::stringHash):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53400 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/UString.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/UString.cpp
@@ -349,6 +349,8 @@ UString UString::from(double d)
 
 UString UString::spliceSubstringsWithSeparators(const Range* substringRanges, int rangeCount, const UString* separators, int separatorCount) const
 {
+    m_rep->checkConsistency();
+
     if (rangeCount == 1 && separatorCount == 0) {
         int thisSize = size();
         int position = substringRanges[0].position;
@@ -390,6 +392,8 @@ UString UString::spliceSubstringsWithSeparators(const Range* substringRanges, in
 
 UString UString::replaceRange(int rangeStart, int rangeLength, const UString& replacement) const
 {
+    m_rep->checkConsistency();
+
     int replacementLength = replacement.size();
     int totalLength = size() - rangeLength + replacementLength;
     if (totalLength == 0)
