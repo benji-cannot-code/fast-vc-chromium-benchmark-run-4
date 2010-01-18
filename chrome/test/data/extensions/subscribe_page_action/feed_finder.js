2cf9138eb1abf77d89567441eba8e10b2da6301c
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-18 23:05:13 +0000

    Ignore the namespace while detecting RSS feed to fix issues with no RSS being found on pages with content type application/xhtml+xml.
    I also now force the mime type to be text/xml for all feeds so that servers who return incorrect mime-type don't trip up the XML parsing in handleResponse.
    
    BUG=None
    TEST=A feed should be detected on pages such as cielonegro dot org.
    
    Review URL: http://codereview.chromium.org/536088
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36498 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/subscribe_page_action/feed_finder.js
+++ chrome/test/data/extensions/subscribe_page_action/feed_finder.js
@@ -1,15 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 findFeeds();
-window.addEventListener("focus", findFeeds);
 
 function findFeeds() {
   // Find all the RSS link elements.
   var result = document.evaluate(
-      '//link[@rel="alternate"][contains(@type, "rss") or ' +
+      '//*[local-name()="link"][@rel="alternate"][contains(@type, "rss") or ' +
       'contains(@type, "atom") or contains(@type, "rdf")]',
       document, null, 0, null);
 
--- chrome/test/data/extensions/subscribe_page_action/subscribe.js
+++ chrome/test/data/extensions/subscribe_page_action/subscribe.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -108,6 +108,10 @@ function main() {
   feedUrl = decodeURIComponent(feedUrl);
   req.onload = handleResponse;
   req.onerror = handleError;
+  // Not everyone sets the mime type correctly, which causes handleResponse
+  // to fail to XML parse the response text from the server. By forcing
+  // it to text/xml we avoid this.
+  req.overrideMimeType('text/xml');
   req.open("GET", feedUrl, !synchronousRequest);
   req.send(null);
 
