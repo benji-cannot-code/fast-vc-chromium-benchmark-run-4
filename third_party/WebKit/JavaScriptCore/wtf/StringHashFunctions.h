bb7d060547d379d1289763bd4f329f19ff3cafdc
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-18 07:36:45 +0000

    https://bugs.webkit.org/show_bug.cgi?id=33731 Remove UntypedPtrAndBitfield from UStringImpl (akin to PtrAndFlags).
    Reviewed by Oliver Hunt.
    
    This break the OS X Leaks tool.  Instead, free up some more bits from the refCount.
    
    * runtime/UStringImpl.cpp:
    (JSC::UStringImpl::sharedBuffer):
    (JSC::UStringImpl::~UStringImpl):
    * runtime/UStringImpl.h:
    (JSC::UStringImpl::cost):
    (JSC::UStringImpl::checkConsistency):
    (JSC::UStringImpl::UStringImpl):
    (JSC::UStringImpl::bufferOwnerString):
    (JSC::UStringImpl::):
    * wtf/StringHashFunctions.h:
    (WTF::stringHash):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/StringHashFunctions.h
+++ third_party/WebKit/JavaScriptCore/wtf/StringHashFunctions.h
@@ -61,13 +61,11 @@ inline unsigned stringHash(const UChar* data, unsigned length)
     hash += hash >> 15;
     hash ^= hash << 10;
 
-    hash &= 0x7fffffff;
-
     // this avoids ever returning a hash code of 0, since that is used to
     // signal "hash not computed yet", using a value that is likely to be
     // effectively the same as 0 when the low bits are masked
     if (hash == 0)
-        hash = 0x40000000;
+        hash = 0x80000000;
 
     return hash;
 }
@@ -101,13 +99,11 @@ inline unsigned stringHash(const char* data, unsigned length)
     hash += hash >> 15;
     hash ^= hash << 10;
 
-    hash &= 0x7fffffff;
-
     // this avoids ever returning a hash code of 0, since that is used to
     // signal "hash not computed yet", using a value that is likely to be
     // effectively the same as 0 when the low bits are masked
     if (hash == 0)
-        hash = 0x40000000;
+        hash = 0x80000000;
 
     return hash;
 }
@@ -142,13 +138,11 @@ inline unsigned stringHash(const char* data)
     hash += hash >> 15;
     hash ^= hash << 10;
 
-    hash &= 0x7fffffff;
-
     // This avoids ever returning a hash code of 0, since that is used to
     // signal "hash not computed yet", using a value that is likely to be
     // effectively the same as 0 when the low bits are masked.
     if (hash == 0)
-        hash = 0x40000000;
+        hash = 0x80000000;
 
     return hash;
 }
