03909eb3f3e8ca5e177fe1e3d6ebb413012b760b
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-19 21:04:42 +0000

    2010-01-19  Daniel Bates  <dbates@webkit.org>
            Reviewed by Adam Roben.
    
            https://bugs.webkit.org/show_bug.cgi?id=33822
    
            REGRESSION(r53273): Fixes redefinition of XFORM error when building
            WebKit using Qt Windows with the MinGW compiler.
     
            Note, MinGW has a slightly different declaration of the struct XFORM
            from that in the Microsoft SDK. So, we need to substitute an alternative
            typedef for XFORM when compiling with MinGW.
    
            No functionality was changed. So, no new tests.
    
            * platform/graphics/transforms/TransformationMatrix.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53485 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/transforms/TransformationMatrix.h
+++ third_party/WebKit/WebCore/platform/graphics/transforms/TransformationMatrix.h
@@ -45,7 +45,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if PLATFORM(WIN) || (PLATFORM(QT) && OS(WINDOWS)) || (PLATFORM(WX) && OS(WINDOWS))
+#if COMPILER(MINGW)
+typedef struct _XFORM XFORM;
+#else
 typedef struct tagXFORM XFORM;
+#endif
 #endif
 
 namespace WebCore {
