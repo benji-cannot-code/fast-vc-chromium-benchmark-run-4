0113b29dc8a7294d439bf427af1ad9bde13bb91c
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-19 01:12:43 +0000

    Script attributes are copied and pasted, making cross-domain attacks possible (30019) <rdar://problem/6008809> https://bugs.webkit.org/show_bug.cgi?id=30019
    Patch by Enrica Casucci <enrica@apple.com> on 2010-01-18
    Reviewed by Darin Adler.
    
    When we create the document fragment from a markup string,
    either to perform a paste operation or a drag and drop, we
    want to remove all the event handlers and any attribute that contain
    a value that leads to code execution.
    The HTMLParser class is now aware of the needs of stripping these attributes.
    I've modified the call to createMarkupString for every platform.
    
    Test: editing/pasteboard/paste-noscript.html
    
    * WebCore.base.exp:
    * dom/Element.cpp:
    (WebCore::isEventHandlerAttribute):
    (WebCore::Element::setAttributeMap):
    * dom/Element.h:
    * dom/MappedAttributeEntry.h:
    (WebCore::):
    * editing/markup.cpp:
    (WebCore::createFragmentFromMarkup):
    * editing/markup.h:
    * html/HTMLElement.cpp:
    (WebCore::HTMLElement::createContextualFragment):
    * html/HTMLElement.h:
    * html/HTMLParser.cpp:
    (WebCore::HTMLParser::HTMLParser):
    (WebCore::HTMLParser::parseToken):
    * html/HTMLParser.h:
    * html/HTMLTokenizer.cpp:
    (WebCore::HTMLTokenizer::HTMLTokenizer):
    (WebCore::parseHTMLDocumentFragment):
    * html/HTMLTokenizer.h:
    * platform/chromium/DragDataChromium.cpp:
    (WebCore::DragData::asFragment):
    * platform/chromium/PasteboardChromium.cpp:
    (WebCore::Pasteboard::documentFragment):
    * platform/gtk/PasteboardGtk.cpp:
    (WebCore::Pasteboard::documentFragment):
    * platform/mac/PasteboardMac.mm:
    (WebCore::Pasteboard::documentFragment):
    * platform/qt/DragDataQt.cpp:
    (WebCore::DragData::asFragment):
    * platform/qt/PasteboardQt.cpp:
    (WebCore::Pasteboard::documentFragment):
    * platform/win/ClipboardUtilitiesWin.cpp:
    (WebCore::fragmentFromCF_HTML):
    (WebCore::fragmentFromHTML):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/jni/jsc/JavaClassJSC.h
+++ third_party/WebKit/WebCore/bridge/jni/jsc/JavaClassJSC.h
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#ifndef JNI_CLASS_H_
-#define JNI_CLASS_H_
+#ifndef JavaClassJSC_h
+#define JavaClassJSC_h
 
 #if ENABLE(MAC_JAVA_BRIDGE)
 
@@ -60,4 +60,4 @@ private:
 
 #endif // ENABLE(MAC_JAVA_BRIDGE)
 
-#endif // JNI_CLASS_H_
+#endif // JavaClassJSC_h
