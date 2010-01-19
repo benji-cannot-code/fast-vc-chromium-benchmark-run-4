bac5c737329c06b5fc69722cab5887229d3bb362
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-19 02:22:37 +0000

    https://bugs.webkit.org/show_bug.cgi?id=33731 Remove uses of PtrAndFlags from WebCore::StringImpl.
    Reviewed by Darin Adler & Oliver Hunt.
    
    These break the OS X Leaks tool.  Move the management of null-terminated copies
    out from StringImpl to String, and use a bit stolen from the refCount to hold the
    'InTable' flag.
    
    * platform/sql/SQLiteFileSystem.cpp:
    (WebCore::SQLiteFileSystem::openDatabase):
    * platform/sql/SQLiteStatement.cpp:
    (WebCore::SQLiteStatement::prepare):
    * platform/sql/SQLiteStatement.h:
    * platform/text/PlatformString.h:
    * platform/text/String.cpp:
    (WebCore::String::copyWithNullTermination):
    * platform/text/StringImpl.cpp:
    (WebCore::StringImpl::StringImpl):
    (WebCore::StringImpl::~StringImpl):
    (WebCore::StringImpl::create):
    (WebCore::StringImpl::crossThreadString):
    (WebCore::StringImpl::sharedBuffer):
    * platform/text/StringImpl.h:
    (WebCore::StringImpl::inTable):
    (WebCore::StringImpl::setInTable):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53447 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/OriginUsageRecord.cpp
+++ third_party/WebKit/WebCore/storage/OriginUsageRecord.cpp
@@ -43,8 +43,8 @@ OriginUsageRecord::OriginUsageRecord()
 void OriginUsageRecord::addDatabase(const String& identifier, const String& fullPath)
 {
     ASSERT(!m_databaseMap.contains(identifier));
-    ASSERT_ARG(identifier, identifier.impl()->refCount() == 1);
-    ASSERT_ARG(fullPath, fullPath.impl()->refCount() == 1);
+    ASSERT_ARG(identifier, identifier.impl()->hasOneRef());
+    ASSERT_ARG(fullPath, fullPath.impl()->hasOneRef());
 
     m_databaseMap.set(identifier, DatabaseEntry(fullPath));
     m_unknownSet.add(identifier);
@@ -64,7 +64,7 @@ void OriginUsageRecord::removeDatabase(const String& identifier)
 void OriginUsageRecord::markDatabase(const String& identifier)
 {
     ASSERT(m_databaseMap.contains(identifier));
-    ASSERT_ARG(identifier, identifier.impl()->refCount() == 1);
+    ASSERT_ARG(identifier, identifier.impl()->hasOneRef());
 
     m_unknownSet.add(identifier);
     m_cachedDiskUsageIsValid = false;
