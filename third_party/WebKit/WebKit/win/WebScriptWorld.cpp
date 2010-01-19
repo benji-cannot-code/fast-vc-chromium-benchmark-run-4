1e932c4801d9fdc670d46e6c8ccb29baeddd9ff6
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-19 18:29:44 +0000

    Windows build fix
    * WebScriptWorld.cpp: Added missing #include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebScriptWorld.cpp
+++ third_party/WebKit/WebKit/win/WebScriptWorld.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKitDLL.h"
 #include "WebScriptWorld.h"
 
+#include <JavaScriptCore/APICast.h>
 #include <WebCore/JSDOMBinding.h>
 #include <WebCore/ScriptController.h>
 
