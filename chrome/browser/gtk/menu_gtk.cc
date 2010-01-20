b4951829a7445492706663cbf3cad660a2e8c6a8
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-20 00:12:25 +0000

    GTK: respect menu model's radio button groupings.
    (don't just group by proximity)
    
    BUG=32222
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/548074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36579 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/menu_gtk.cc
+++ chrome/browser/gtk/menu_gtk.cc
@@ -251,7 +251,7 @@ void MenuGtk::BuildMenuFromModel() {
 }
 
 void MenuGtk::BuildSubmenuFromModel(menus::MenuModel* model, GtkWidget* menu) {
-  GtkWidget* last_menu_item = NULL;
+  std::map<int, GtkWidget*> radio_groups;
   GtkWidget* menu_item = NULL;
   for (int i = 0; i < model->GetItemCount(); ++i) {
     SkBitmap icon;
@@ -267,16 +267,20 @@ void MenuGtk::BuildSubmenuFromModel(menus::MenuModel* model, GtkWidget* menu) {
         menu_item = gtk_check_menu_item_new_with_mnemonic(label.c_str());
         break;
 
-      case menus::MenuModel::TYPE_RADIO:
-        if (last_menu_item && GTK_IS_RADIO_MENU_ITEM(last_menu_item)) {
-          menu_item = gtk_radio_menu_item_new_with_mnemonic_from_widget(
-              GTK_RADIO_MENU_ITEM(last_menu_item), label.c_str());
-        } else {
+      case menus::MenuModel::TYPE_RADIO: {
+        std::map<int, GtkWidget*>::iterator iter =
+            radio_groups.find(model->GetGroupIdAt(i));
+
+        if (iter == radio_groups.end()) {
           menu_item = gtk_radio_menu_item_new_with_mnemonic(
               NULL, label.c_str());
+          radio_groups[model->GetGroupIdAt(i)] = menu_item;
+        } else {
+          menu_item = gtk_radio_menu_item_new_with_mnemonic_from_widget(
+              GTK_RADIO_MENU_ITEM(iter->second), label.c_str());
         }
         break;
-
+      }
       case menus::MenuModel::TYPE_SUBMENU:
       case menus::MenuModel::TYPE_COMMAND:
         if (model->GetIconAt(i, &icon))
@@ -309,7 +313,6 @@ void MenuGtk::BuildSubmenuFromModel(menus::MenuModel* model, GtkWidget* menu) {
                       reinterpret_cast<void*>(model));
     AppendMenuItemToMenu(i, menu_item, menu);
 
-    last_menu_item = menu_item;
     menu_item = NULL;
   }
 }
