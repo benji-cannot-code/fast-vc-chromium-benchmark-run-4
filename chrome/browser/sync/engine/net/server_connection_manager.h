16bec8227c6065e513e786458db29865db6efab0
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-20 00:46:13 +0000

    Implemented AddressWatchTask for OS X.
    Added some utility methods to ServerConnectionManager.
    
    BUG=32430
    TEST=Manual testing with a laptop
    
    Review URL: http://codereview.chromium.org/543098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36586 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/net/server_connection_manager.h
+++ chrome/browser/sync/engine/net/server_connection_manager.h
@@ -215,6 +215,10 @@ class ServerConnectionManager {
   // Updates status and broadcasts events on change.
   bool CheckServerReachable();
 
+  // Updates server status to "unreachable" and broadcasts events if
+  // necessary.
+  void SetServerUnreachable();
+
   // Signal the shutdown event to notify listeners.
   virtual void kill();
 
@@ -251,6 +255,8 @@ class ServerConnectionManager {
   void GetServerParameters(std::string* server_url,
                            int* port,
                            bool* use_ssl) const;
+
+  std::string GetServerHost() const;
 
   bool terminate_all_io() const {
     AutoLock lock(terminate_all_io_mutex_);
