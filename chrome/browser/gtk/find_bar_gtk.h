e513dbce48794ac15a2bf62ab525f41d8f654500
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-20 00:23:12 +0000

    Style cleanup of member variable in FindBarGtk.
    I missed this during review.
    
    Review URL: http://codereview.chromium.org/543112
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36583 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/find_bar_gtk.h
+++ chrome/browser/gtk/find_bar_gtk.h
@@ -198,7 +198,7 @@ class FindBarGtk : public FindBar,
 
   // The selection rect we are currently showing. We cache it to avoid covering
   // it up.
-  gfx::Rect selection_rect;
+  gfx::Rect selection_rect_;
 
   NotificationRegistrar registrar_;
 
