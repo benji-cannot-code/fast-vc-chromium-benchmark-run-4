19a14e26d3c2af8877f379cacefba1072d62f453
jpetsovits@rim.com <jpetsovits@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-21 20:56:25 +0000

    2010-01-21  Jakob Petsovits  <jpetsovits@rim.com>
            Reviewed by Adam Treat.
    
            Don't call vgGetError()/eglGetError() repeatedly
            https://bugs.webkit.org/show_bug.cgi?id=33959
    
            For vgGetError(), I missed the part of the spec where
            it says that calling that function clears the error
            and subsequent calls will return VG_NO_ERROR again.
    
            For eglGetError(), the specification doesn't mention
            that kind of behavior, and interpretations seem to
            differ between EGL implementations (even within
            Khronos: the OpenVG reference implementation doesn't
            reset the error code - and even mentions the difference
            to vgGetError() in a comment - whereas the online
            OpenGL ES API document explicitly specifies clearing
            the error code).
    
            It thus makes sense not to call either of the two
            error functions more than once for checking a single
            EGL/OpenVG call. This patch adapts assertions to
            accommodate for this behavior, and also needs to
            change surface creation methods as they previously
            relied on multiple calls of eglGetError().
    
            * platform/graphics/openvg/EGLDisplayOpenVG.cpp:
            (WebCore::EGLDisplayOpenVG::sharedPlatformSurface):
            (WebCore::EGLDisplayOpenVG::createPbufferSurface):
            * platform/graphics/openvg/EGLDisplayOpenVG.h:
            * platform/graphics/openvg/EGLUtils.h:
            * platform/graphics/openvg/SurfaceOpenVG.cpp:
            (WebCore::SurfaceOpenVG::SurfaceOpenVG):
            * platform/graphics/openvg/SurfaceOpenVG.h:
            * platform/graphics/openvg/VGUtils.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53639 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/openvg/SurfaceOpenVG.cpp
+++ third_party/WebKit/WebCore/platform/graphics/openvg/SurfaceOpenVG.cpp
@@ -45,7 +45,7 @@ SurfaceOpenVG* SurfaceOpenVG::currentSurface()
 }
 
 #if PLATFORM(EGL)
-SurfaceOpenVG::SurfaceOpenVG(const IntSize& size, const EGLDisplay& display, EGLConfig* confPtr)
+SurfaceOpenVG::SurfaceOpenVG(const IntSize& size, const EGLDisplay& display, EGLConfig* confPtr, EGLint* errorCode)
     : m_eglDisplay(display)
     , m_eglSurface(EGL_NO_SURFACE)
     , m_eglContext(EGL_NO_CONTEXT)
@@ -54,7 +54,7 @@ SurfaceOpenVG::SurfaceOpenVG(const IntSize& size, const EGLDisplay& display, EGL
 
     EGLDisplayOpenVG* displayManager = EGLDisplayOpenVG::forDisplay(m_eglDisplay);
     EGLConfig config = confPtr ? (*confPtr) : displayManager->defaultPbufferConfig();
-    m_eglSurface = displayManager->createPbufferSurface(size, config);
+    m_eglSurface = displayManager->createPbufferSurface(size, config, errorCode);
 
     if (m_eglSurface == EGL_NO_SURFACE)
         return;
@@ -73,9 +73,7 @@ SurfaceOpenVG::SurfaceOpenVG(EGLNativeWindowType window, const EGLDisplay& displ
     EGLDisplayOpenVG* displayManager = EGLDisplayOpenVG::forDisplay(m_eglDisplay);
     EGLConfig config = confPtr ? (*confPtr) : displayManager->defaultWindowConfig();
     m_eglSurface = displayManager->surfaceForWindow(window, config);
-
-    if (m_eglSurface == EGL_NO_SURFACE)
-        return;
+    ASSERT(m_eglSurface != EGL_NO_SURFACE);
 
     m_eglContext = displayManager->contextForSurface(m_eglSurface);
     EGLDisplayOpenVG::registerPlatformSurface(this);
