0f010eb578576750fdaf85107eadca0d59039dd3
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-21 19:46:27 +0000

    Remove gtest_prod.h include from JSONReader.
    I'd like to build this code for Chrome OS, but the gtest
    include path is different there.  I'm removing the include
    and adding the FRIEND_TEST() macro directly to json_reader.h.
    
    BUG=none
    TEST=compiled
    
    Review URL: http://codereview.chromium.org/546100
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36775 0039d316-1c4b-4281-b951-d872f2087c98

--- base/json/json_reader.h
+++ base/json/json_reader.h
@@ -35,7 +35,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
-#include "testing/gtest/include/gtest/gtest_prod.h"
+
+// Chromium and Chromium OS check out gtest to different places, so we're
+// unable to compile on both if we include gtest_prod.h here.  Instead, include
+// its only contents -- this will need to be updated if the macro ever changes.
+#define FRIEND_TEST(test_case_name, test_name)\
+friend class test_case_name##_##test_name##_Test
 
 class Value;
 
