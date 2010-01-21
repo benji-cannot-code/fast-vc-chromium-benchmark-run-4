d86d05d921989ec5a685db8a49cb0308a8804c90
avayvod@chromium.org <avayvod@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-21 09:58:47 +0000

    Removed restriction for {} so that javascript blocks can be used in the url. Added unittests for TemplateURLRef::ParseParameter and TemplateURLRef::ParseURL methods.
    BUG=2238
    TEST=Try adding urls with different combinations of {}, they all should work.
    
    Review URL: http://codereview.chromium.org/555012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36743 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url.h
+++ chrome/browser/search_engines/template_url.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SEARCH_ENGINES_TEMPLATE_URL_H_
 #define CHROME_BROWSER_SEARCH_ENGINES_TEMPLATE_URL_H_
 
+#include <string>
 #include <vector>
 
 #include "base/time.h"
 #include "googleurl/src/gurl.h"
+#include "testing/gtest/include/gtest/gtest_prod.h"
 
 class TemplateURL;
 
@@ -112,6 +114,13 @@ class TemplateURLRef {
   friend class TemplateURL;
   friend class TemplateURLModelTest;
   friend class TemplateURLTest;
+  FRIEND_TEST(TemplateURLTest, ParseParameterKnown);
+  FRIEND_TEST(TemplateURLTest, ParseParameterUnknown);
+  FRIEND_TEST(TemplateURLTest, ParseURLEmpty);
+  FRIEND_TEST(TemplateURLTest, ParseURLNoTemplateEnd);
+  FRIEND_TEST(TemplateURLTest, ParseURLNoKnownParameters);
+  FRIEND_TEST(TemplateURLTest, ParseURLTwoParameters);
+  FRIEND_TEST(TemplateURLTest, ParseURLNestedParameter);
 
   // Enumeration of the known types.
   enum ReplacementType {
@@ -147,9 +156,11 @@ class TemplateURLRef {
   // range of the parameter in the url, including the braces. If the parameter
   // is valid, url is updated to reflect the appropriate parameter. If
   // the parameter is one of the known parameters an element is added to
-  // replacements indicating the type and range of the element.
+  // replacements indicating the type and range of the element. The original
+  // parameter is erased from the url.
   //
-  // If the parameter is not a known parameter, false is returned.
+  // If the parameter is not a known parameter, it's not erased and false is
+  // returned.
   bool ParseParameter(size_t start,
                       size_t end,
                       std::wstring* url,
@@ -158,8 +169,8 @@ class TemplateURLRef {
   // Parses the specified url, replacing parameters as necessary. If
   // successful, valid is set to true, and the parsed url is returned. For all
   // known parameters that are encountered an entry is added to replacements.
-  // If there is an error parsing (unknown parameter, or bogus url), valid is
-  // set to false, and an empty string is returned.
+  // If there is an error parsing the url, valid is set to false, and an empty
+  // string is returned.
   std::wstring ParseURL(const std::wstring& url,
                         Replacements* replacements,
                         bool* valid) const;
