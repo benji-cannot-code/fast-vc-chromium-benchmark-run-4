c452909e86e76b99d7ee583895eba1dccf480297
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-22 23:03:47 +0000

    Add feed sniffing to the RSS extension.
    BUG=None
    TEST=Go to http://www.whitehouse.gov/rss and click on any of the links to the feeds. It should take you to the subscription preview page in a new tab.
    
    Review URL: http://codereview.chromium.org/543129
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36918 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/subscribe_page_action/sniff_common.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function containsFeed(doc) {
+  // Find all the RSS link elements.
+  var result = doc.evaluate(
+      '//*[local-name()="rss" or local-name()="feed" or local-name()="RDF"]',
+      doc, null, 0, null);
+
+  if (!result)
+    return false;  // This is probably overly defensive, but whatever.
+
+  var node = result.iterateNext();
+
+  if (!node)
+    return false;  // No RSS tags were found.
+
+  // The feed for arab dash jokes dot net, for example, contains
+  // a feed that is a child of the body tag so we continue only if the
+  // node contains no parent or if the parent is the body tag.
+  if (node.parentElement && node.parentElement.tagName != "BODY")
+    return false;
+
+  return true;
+}
