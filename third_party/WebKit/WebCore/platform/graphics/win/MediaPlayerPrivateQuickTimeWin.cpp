4a08f43b140b5cf460604498d0e7631ee45c14ca
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-22 23:49:39 +0000

    2010-01-22  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Simon Fraser.
    
            Update r53711 for GraphicsLayer method rename.
    
            * platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp:
            (WebCore::MediaPlayerPrivate::createLayerForMovie): Rename from change setContentsToVideo.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp
@@ -832,7 +832,7 @@ void MediaPlayerPrivate::createLayerForMovie()
 #endif
 
     // Hang the video layer from the render layer.
-    videoGraphicsLayer->setContentsToVideo(m_qtVideoLayer->platformLayer());
+    videoGraphicsLayer->setContentsToMedia(m_qtVideoLayer->platformLayer());
 #endif
 }
 
