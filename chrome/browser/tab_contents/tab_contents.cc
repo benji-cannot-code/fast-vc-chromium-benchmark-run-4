d3b6614df7ccaf16117c54844b44a19ba42da963
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-22 05:26:06 +0000

    Split platform specific settings out from locale_settings.grd so we can have per platform config and overrides.  This is easier to manage than the mess of <if>'s in the existing grd and xtb files.
    Each platform only builds its locale_settings_{win,mac,linux}.grd file.
    All the grd files generate the same platform_locale_settings.h
    header (for easy inclusion in .cc files).
    
    We use the fallback feature of grit to only specify locale specific values
    in xtb files if we want to override the default for the locale.
    
    BUG=28590
    
    Review URL: http://codereview.chromium.org/545132
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36849 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "grit/generated_resources.h"
 #include "grit/locale_settings.h"
+#include "grit/platform_locale_settings.h"
 #include "net/base/mime_util.h"
 #include "net/base/net_errors.h"
 #include "net/base/net_util.h"
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'common',
         'chrome_resources',
         'chrome_strings',
+        'platform_locale_settings',
         'theme_resources',
         '../app/app.gyp:app_resources',
         '../app/app.gyp:app_strings',
