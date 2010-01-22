a4777ed76cfc728812b5c5dabe3e762269c2920a
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-22 20:30:46 +0000

    linux: add display option to media_bench, using GL
    Review URL: http://codereview.chromium.org/523120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36886 0039d316-1c4b-4281-b951-d872f2087c98

--- media/tools/player_x11/x11_video_renderer.h
+++ media/tools/player_x11/x11_video_renderer.h
@@ -1,11 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.  Use of this
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.  Use of this
 // source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
 #ifndef MEDIA_TOOLS_PLAYER_X11_X11_VIDEO_RENDERER_H_
 #define MEDIA_TOOLS_PLAYER_X11_X11_VIDEO_RENDERER_H_
 
+#include <GL/glew.h>
+#include <GL/glxew.h>
 #include <X11/Xlib.h>
 
 #include "base/lock.h"
@@ -62,6 +64,19 @@ class X11VideoRenderer : public media::VideoRendererBase {
   unsigned long picture_;
 
   bool use_render_;
+
+  bool use_gl_;
+
+  // GL context.
+  GLXContext gl_context_;
+
+  // 3 textures, one for each plane.
+  GLuint textures_[3];
+
+  // Shaders and program for YUV->RGB conversion.
+  GLuint vertex_shader_;
+  GLuint fragment_shader_;
+  GLuint program_;
 
   static X11VideoRenderer* instance_;
 
