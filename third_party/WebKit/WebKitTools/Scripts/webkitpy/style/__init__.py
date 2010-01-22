ce5bdf6c24d29b4ffba817e34d28298552ed41f6
cjerdonek@webkit.org <cjerdonek@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-22 22:04:51 +0000

    33791: Proposed patch 1 (amended with preferred license)
    git-svn-id: svn://svn.chromium.org/blink/trunk@53715 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/style/__init__.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/style/__init__.py
@@ -1,7 +1,2 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Required for Python to search this directory for module files
-
-import os
-
-# Add containing "webkitpy" package directory to search path.
-__path__.append(os.path.join(__path__[0], ".."))
