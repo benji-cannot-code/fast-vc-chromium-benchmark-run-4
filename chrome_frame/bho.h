33bdef72ef4e01ee2df46d758abf0a920d2154cf
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-22 18:49:47 +0000

    Implement OptInUrls using the same mechanism we use in the onhttpequiv notification. A noteworthy change here is that OptInUrls doesn't rely on cf: anymore.
    TEST=The OptInUrls registry key should start working properly again.
    BUG=32660
    
    Review URL: http://codereview.chromium.org/549129
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36874 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/bho.h
+++ chrome_frame/bho.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class PatchHelper {
  public:
-  enum State { UNKNOWN, PATCH_IBROWSER, PATCH_IBROWSER_OK, PATCH_PROTOCOL };
+  enum State { UNKNOWN, PATCH_IBROWSER, PATCH_PROTOCOL };
   PatchHelper() : state_(UNKNOWN) {
   }
 
