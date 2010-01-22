c27961b1741ce635b55e3a2f435dbc054b7ebcda
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-22 02:07:34 +0000

    wx build fixes, add a missing header and add a new dir to the build system.
    git-svn-id: svn://svn.chromium.org/blink/trunk@53665 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/ScriptExecutionContext.cpp
+++ third_party/WebKit/WebCore/dom/ScriptExecutionContext.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScriptExecutionContext.h"
 
 #include "ActiveDOMObject.h"
+#include "Database.h"
 #include "DatabaseTask.h"
 #include "DatabaseThread.h"
 #include "MessagePort.h"
