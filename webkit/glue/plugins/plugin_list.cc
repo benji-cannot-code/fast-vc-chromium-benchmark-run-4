4732c063958dcb05f3a0ddea9dcf9ab93969d7d6
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-23 02:15:56 +0000

    Linux: Revert r33888. We no longer need to collect the list of loaded plugins since we do not unload them now, thus they already show up in the module list.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/551122
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36937 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/plugin_list.cc
+++ webkit/glue/plugins/plugin_list.cc
@@ -16,13 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/webkit_glue.h"
 #include "googleurl/src/gurl.h"
 
-#if defined(USE_LINUX_BREAKPAD)
-namespace {
-// Comma-separated list of loaded plugin filenames.
-std::string loaded_plugin_list;
-}  // anonymous namespace
-#endif
-
 namespace NPAPI {
 
 base::LazyInstance<PluginList> g_singleton(base::LINKER_INITIALIZED);
@@ -82,14 +75,7 @@ bool PluginList::ReadPluginInfo(const FilePath &filename,
   // Not an internal plugin.
   *entry_points = NULL;
 
-  bool ret = PluginLib::ReadWebPluginInfo(filename, info);
-
-#if defined(USE_LINUX_BREAKPAD)
-  if (ret)
-    loaded_plugin_list.append(filename.BaseName().value() + ",");
-#endif
-
-  return ret;
+  return PluginLib::ReadWebPluginInfo(filename, info);
 }
 
 bool PluginList::CreateWebPluginInfo(const PluginVersionInfo& pvi,
@@ -175,10 +161,6 @@ void PluginList::LoadPlugins(bool refresh) {
     extra_plugin_dirs = extra_plugin_dirs_;
     internal_plugins = internal_plugins_;
   }
-
-#if defined(USE_LINUX_BREAKPAD)
-  loaded_plugin_list.clear();
-#endif
 
   base::TimeTicks start_time = base::TimeTicks::Now();
 
@@ -366,14 +348,5 @@ bool PluginList::GetPluginInfoByPath(const FilePath& plugin_path,
 void PluginList::Shutdown() {
   // TODO
 }
-
-#if defined(USE_LINUX_BREAKPAD)
-// static
-// WARNING: This is called when we are in the middle of a browser crash.
-// This function may not call into libc nor allocate memory.
-std::string* PluginList::GetLoadedPlugins() {
-  return &loaded_plugin_list;
-}
-#endif
 
 }  // namespace NPAPI
