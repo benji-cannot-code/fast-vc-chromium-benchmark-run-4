9b8d1f56cc1eed58e4b2122bdd09e755079bea09
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-23 05:27:40 +0000

    2010-01-22  Elliot Glaysher  <erg@chromium.org>
            Reviewed by David Levin.
    
            Chromium: theme selection colors to match gtk theme
            Add functions to RenderThemeChromiumLinux to change the selection color
            according to the current GTK+ theme.
    
            Since the change is to the Chromium WebKit API layer, testing is done
            in Chromium's test shell (see Chromium side of this patch:
            http://codereview.chromium.org/554004)
    
            https://bugs.webkit.org/show_bug.cgi?id=33921
    
            * rendering/RenderThemeChromiumLinux.cpp:
            (WebCore::RenderThemeChromiumLinux::platformActiveSelectionBackgroundColor):
            (WebCore::RenderThemeChromiumLinux::platformInactiveSelectionBackgroundColor):
            (WebCore::RenderThemeChromiumLinux::platformActiveSelectionForegroundColor):
            (WebCore::RenderThemeChromiumLinux::platformInactiveSelectionForegroundColor):
            (WebCore::RenderThemeChromiumLinux::setSelectionColors):
            * rendering/RenderThemeChromiumLinux.h: Adds static members.
    2010-01-22  Elliot Glaysher  <erg@chromium.org>
    
            Reviewed by David Levin.
    
            Chromium: theme selection colors to match gtk theme
            Add functions to RenderThemeChromiumLinux to change the selection color
            according to the current GTK+ theme.
    
            Since the change is to the Chromium WebKit API layer, testing is done
            in Chromium's test shell (see Chromium side of this patch:
            http://codereview.chromium.org/554004)
    
            https://bugs.webkit.org/show_bug.cgi?id=33921
    
            * public/WebView.h: Adds interface to change the selection colors
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::setSelectionColors): Implements new interface.
            * src/WebViewImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53759 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebViewImpl.h
+++ third_party/WebKit/WebKit/chromium/src/WebViewImpl.h
@@ -158,6 +158,10 @@ public:
     virtual void setScrollbarColors(unsigned inactiveColor,
                                     unsigned activeColor,
                                     unsigned trackColor);
+    virtual void setSelectionColors(unsigned activeBackgroundColor,
+                                    unsigned activeForegroundColor,
+                                    unsigned inactiveBackgroundColor,
+                                    unsigned inactiveForegroundColor);
     virtual void performCustomContextMenuAction(unsigned action);
 
     // WebViewImpl
