044803091390af1b5a4a91bbe4c31c6b2016a79b
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-23 03:06:14 +0000

    Mac build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@53751 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/GeolocationService.cpp
+++ third_party/WebKit/WebCore/platform/GeolocationService.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-#if !ENABLE(GEOLOCATION)
+#if !ENABLE(GEOLOCATION) || ENABLE(CLIENT_BASED_GEOLOCATION)
 static GeolocationService* createGeolocationServiceNull(GeolocationServiceClient*)
 {
     return 0;
