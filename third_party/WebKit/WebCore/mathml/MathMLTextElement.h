5716d09e6fb7ca4375464001e808b961c2854c23
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-23 07:11:59 +0000

    2010-01-22  Alex Milowski  <alex@milowski.com>
            Reviewed by David Hyatt.
    
            Updated tests due to stylesheet changes.
    
            * platform/mac/mathml/presentation/sub-expected.checksum:
            * platform/mac/mathml/presentation/sub-expected.png:
            * platform/mac/mathml/presentation/sub-expected.txt:
            * platform/mac/mathml/presentation/sup-expected.checksum:
            * platform/mac/mathml/presentation/sup-expected.png:
            * platform/mac/mathml/presentation/sup-expected.txt:
            * platform/mac/mathml/presentation/tables-expected.checksum:
            * platform/mac/mathml/presentation/tables-expected.png:
            * platform/mac/mathml/presentation/tables-expected.txt:
    2010-01-22  Alex Milowski  <alex@milowski.com>
    
            Reviewed by David Hyatt.
    
            Added support for RenderMathMLBlock base object and isRenderMathMLBlock()
            method on RenderObject.
    
            Added support for text elements and updated tag and attribute names
    
            Also, updated the stylesheet added files to the project
    
            * DerivedSources.make:
            * WebCore.xcodeproj/project.pbxproj:
            * css/mathml.css:
            * mathml/MathMLInlineContainerElement.cpp:
            (WebCore::MathMLInlineContainerElement::createRenderer):
            * mathml/MathMLTextElement.cpp: Added.
            (WebCore::MathMLTextElement::MathMLTextElement):
            (WebCore::MathMLTextElement::create):
            (WebCore::MathMLTextElement::createRenderer):
            * mathml/MathMLTextElement.h: Added.
            * mathml/RenderMathMLBlock.cpp: Added.
            (WebCore::RenderMathMLBlock::RenderMathMLBlock):
            (WebCore::RenderMathMLBlock::styleDidChange):
            (WebCore::RenderMathMLBlock::isChildAllowed):
            (WebCore::RenderMathMLBlock::makeBlockStyle):
            (WebCore::RenderMathMLBlock::nonOperatorHeight):
            (WebCore::RenderMathMLBlock::stretchToHeight):
            * mathml/RenderMathMLBlock.h: Added.
            (WebCore::RenderMathMLBlock::isRenderMathMLBlock):
            (WebCore::RenderMathMLBlock::isRenderMathMLOperator):
            (WebCore::RenderMathMLBlock::isRenderMathMLRow):
            (WebCore::RenderMathMLBlock::isRenderMathMLMath):
            (WebCore::RenderMathMLBlock::hasBase):
            (WebCore::toRenderMathMLBlock):
            * mathml/mathattrs.in: Added.
            * rendering/RenderObject.h:
            (WebCore::RenderObject::isRenderMathMLBlock):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53764 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/mathml/MathMLTextElement.h
+++ third_party/WebKit/WebCore/mathml/MathMLTextElement.h
@@ -24,39 +24,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "config.h"
+#ifndef MathMLTextElement_h
+#define MathMLTextElement_h
 
 #if ENABLE(MATHML)
-
-#include "MathMLInlineContainerElement.h"
-
-#include "MathMLNames.h"
-#include "RenderMathMLBlock.h"
+#include "MathMLElement.h"
 
 namespace WebCore {
     
-using namespace MathMLNames;
+class MathMLTextElement : public MathMLElement {
+public:
+    static PassRefPtr<MathMLTextElement> create(const QualifiedName& tagName, Document*);
 
-MathMLInlineContainerElement::MathMLInlineContainerElement(const QualifiedName& tagName, Document* document)
-    : MathMLElement(tagName, document)
-{
-}
-
-PassRefPtr<MathMLInlineContainerElement> MathMLInlineContainerElement::create(const QualifiedName& tagName, Document* document)
-{
-    return new MathMLInlineContainerElement(tagName, document);
-}
-
-RenderObject* MathMLInlineContainerElement::createRenderer(RenderArena *arena, RenderStyle* style)
-{
-    // FIXME: This method will contain the specialized renderers based on element name
-    RenderObject* object = new (arena) RenderMathMLBlock(this);
-    object->setStyle(style);
-    return object;
-}
+    virtual RenderObject* createRenderer(RenderArena*, RenderStyle*);
     
+protected:
+    MathMLTextElement(const QualifiedName& tagName, Document*);
+    
+};
     
 }
 
 #endif // ENABLE(MATHML)
-
+#endif // MathMLTextElement_h
