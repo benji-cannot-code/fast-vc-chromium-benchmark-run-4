b33f9fcc5118276b6a6cb36f22517421e36ac043
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-23 08:21:35 +0000

    DevTools: prepare for landing webkit change for 32554.
    BUG=29068
    
    Review URL: http://codereview.chromium.org/546105
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36957 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/devtools/js/devtools.js
+++ webkit/glue/devtools/js/devtools.js
@@ -527,3 +527,8 @@ InspectorFrontendHost.addResourceSourceToFrame = function(identifier, element) {
   originalAddToFrame.call(this, identifier, resource.mimeType, element);
 };
 })();
+
+WebInspector.pausedScript = function(callFrames)
+{
+    this.panels.scripts.debuggerPaused(callFrames);
+};
