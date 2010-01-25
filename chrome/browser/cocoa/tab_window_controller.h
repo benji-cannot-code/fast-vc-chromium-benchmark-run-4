eabf7ac06d67f7d634f88843bef082b1b2f5ff82
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-25 21:30:31 +0000

    [Mac] Add methods to prevent tab detaching or window moving if needed.  Modify tab view to respect these new settings.  Prereq for fullscreen.
    (Changes really made by viettrungluu@chromium.org.) 
    
    BUG=31638
    TEST=Tab dragging and window moving should still work when not in fullscreen mode.
    Review URL: http://codereview.chromium.org/554064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37045 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_window_controller.h
+++ chrome/browser/cocoa/tab_window_controller.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -77,10 +77,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // implementation.
 - (void)removePlaceholder;
 
-// Returns YES if tab dragging is currently allowed. Any number of things
-// can choose to disable it, such as pending animations. The default is to
-// always return YES. Subclasses should override as appropriate.
+// The follow return YES if tab dragging/tab tearing (off the tab strip)/window
+// movement is currently allowed. Any number of things can choose to disable it,
+// such as pending animations. The default implementations always return YES.
+// Subclasses should override as appropriate.
 - (BOOL)tabDraggingAllowed;
+- (BOOL)tabTearingAllowed;
+- (BOOL)windowMovementAllowed;
 
 // Show or hide the new tab button. The button is hidden immediately, but
 // waits until the next call to |-layoutTabs| to show it again.
