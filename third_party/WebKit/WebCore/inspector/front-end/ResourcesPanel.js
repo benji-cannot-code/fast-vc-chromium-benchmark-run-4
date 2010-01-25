71dd70e031e82b9509a8705374eeed111eddd8aa
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-25 16:06:45 +0000

    2010-01-25  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: don't show headers/content tabs in scripts view.
    
            https://bugs.webkit.org/show_bug.cgi?id=34083
    
            * inspector/front-end/ResourceView.js:
            (WebInspector.ResourceView):
            (WebInspector.ResourceView.prototype.set headersVisible):
            (WebInspector.ResourceView.prototype._selectContentTab):
            (WebInspector.ResourceView.prototype._innerSelectContentTab):
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.ResourcesPanel.prototype.show):
            (WebInspector.ResourcesPanel.prototype.recreateViewForResourceIfNeeded):
            (WebInspector.ResourcesPanel.prototype.showResource):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel.prototype.show):
            (WebInspector.ScriptsPanel.prototype._showScriptOrResource):
            * inspector/front-end/inspector.css:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ResourcesPanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/ResourcesPanel.js
@@ -193,9 +193,10 @@ WebInspector.ResourcesPanel.prototype = {
         WebInspector.AbstractTimelinePanel.prototype.show.call(this);
 
         var visibleView = this.visibleView;
-        if (this.visibleResource)
-            visibleView.show(this.viewsContainerElement);
-        else if (visibleView)
+        if (this.visibleResource) {
+            this.visibleView.headersVisible = true;
+            this.visibleView.show(this.viewsContainerElement);
+        } else if (visibleView)
             visibleView.show();
 
         // Hide any views that are visible that are not this panel's current visible view.
@@ -445,6 +446,8 @@ WebInspector.ResourcesPanel.prototype = {
 
         resource._resourcesView = newView;
 
+        newView.headersVisible = oldView.headersVisible;
+
         if (oldView.visible && oldView.element.parentNode)
             newView.show(oldView.element.parentNode);
     },
@@ -470,6 +473,7 @@ WebInspector.ResourcesPanel.prototype = {
             this.visibleResource._resourcesView.hide();
 
         var view = this.resourceViewForResource(resource);
+        view.headersVisible = true;
         view.show(this.viewsContainerElement);
 
         if (line) {
