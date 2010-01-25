d01bac2b81fae1c435d466146ada89b4a4ddbebf
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-25 17:38:16 +0000

    Remove flaky label for a test. My fix seems to have worked (hasn't been failing for a few days now -- used to fail regularly).
    BUG=16447
    TEST=None (making a change to a test)
    TBR=jcampan
    
    Review URL: http://codereview.chromium.org/553070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37018 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/find_bar_host_browsertest.cc
+++ chrome/browser/views/find_bar_host_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -555,9 +555,7 @@ IN_PROC_BROWSER_TEST_F(FindInPageControllerTest,
 }
 
 // Make sure Find box moves out of the way if it is obscuring the active match.
-// Flaky, see http://crbug.com/16447.
-IN_PROC_BROWSER_TEST_F(FindInPageControllerTest,
-                       FLAKY_FindMovesWhenObscuring) {
+IN_PROC_BROWSER_TEST_F(FindInPageControllerTest, FindMovesWhenObscuring) {
   HTTPTestServer* server = StartHTTPServer();
 
   GURL url = server->TestServerPageW(kMoveIfOver);
