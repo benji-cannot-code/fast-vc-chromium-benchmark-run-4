2658866806c54bb4615bd4952e8284aa83f701a9
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-25 20:52:06 +0000

    [Mac] Makes the tab strip left-side indent changeable.  Prereq for fullscreen.
    (Changes really made by viettrungluu@chromium.org.)
    
    BUG=31638
    TEST=Tab strip should still draw with the proper indent.
    
    Review URL: http://codereview.chromium.org/551145
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37038 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_strip_controller.h
+++ chrome/browser/cocoa/tab_strip_controller.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -103,12 +103,18 @@ class ToolbarModel;
   // The default favicon, so we can use one copy for all buttons.
   scoped_nsobject<NSImage> defaultFavIcon_;
 
+  // The amount by which to indent the tabs on the left (to make room for the
+  // red/yellow/green buttons).
+  CGFloat indentForControls_;
+
   // Manages per-tab sheets.
   scoped_nsobject<GTMWindowSheetController> sheetController_;
 
   // Is the mouse currently inside the strip;
   BOOL mouseInside_;
 }
+
+@property(nonatomic) CGFloat indentForControls;
 
 // Initialize the controller with a view and browser that contains
 // everything else we'll need. |switchView| is the view whose contents get
@@ -176,6 +182,9 @@ class ToolbarModel;
 
 // Default height for tabs.
 + (CGFloat)defaultTabHeight;
+
+// Default indentation for tabs (see |indentForControls_|).
++ (CGFloat)defaultIndentForControls;
 
 // Returns the (lazily created) window sheet controller of this window. Used
 // for the per-tab sheets.
