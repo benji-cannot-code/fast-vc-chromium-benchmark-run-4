50ffd09f415eaccc56d2c4a89997146b3d281904
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-26 22:44:51 +0000

    [Mac] Possibly prevent crashes by properly handling missing/invalid favicons in HungRendererController.
    BUG=33160
    TEST=None.  (No repro case.  Hopefully crashes will go away.)
    Review URL: http://codereview.chromium.org/556022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37161 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/hung_renderer_controller.mm
+++ chrome/browser/cocoa/hung_renderer_controller.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/logging_chrome.h"
 #include "chrome/common/result_codes.h"
 #include "grit/chromium_strings.h"
+#include "grit/app_resources.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
 #include "skia/ext/skia_utils_mac.h"
@@ -143,8 +144,16 @@ HungRendererController* g_instance = NULL;
       } else {
         [titles addObject:base::SysUTF16ToNSString(title)];
       }
-      [favicons addObject:gfx::SkBitmapToNSImage(it->GetFavIcon())];
 
+      // TabContents can return a null SkBitmap if it has no favicon.  If this
+      // happens, use the default favicon.
+      const SkBitmap& bitmap = it->GetFavIcon();
+      if (!bitmap.isNull()) {
+        [favicons addObject:gfx::SkBitmapToNSImage(bitmap)];
+      } else {
+        ResourceBundle& rb = ResourceBundle::GetSharedInstance();
+        [favicons addObject:rb.GetNSImageNamed(IDR_DEFAULT_FAVICON)];
+      }
     }
   }
   hungTitles_.reset([titles copy]);
