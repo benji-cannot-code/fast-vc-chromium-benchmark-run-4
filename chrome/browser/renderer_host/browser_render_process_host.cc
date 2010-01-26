a4f522b986c5b5ab893305a18a8da021f67598c4
michaeln@chromium.org <michaeln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-26 20:42:37 +0000

    Enable the appcache by default and add a cmd line switch to optionally disable it.
    BUG=25977
    TEST=manual, verified disabled with --disable-application-cache
    
    Review URL: http://codereview.chromium.org/548122
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37150 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -533,7 +533,7 @@ void BrowserRenderProcessHost::PropogateBrowserCommandLineToRenderer(
     switches::kDisableLocalStorage,
     switches::kEnableSessionStorage,
     switches::kDisableSharedWorkers,
-    switches::kEnableApplicationCache,
+    switches::kDisableApplicationCache,
     switches::kEnableGeolocation,
     switches::kShowPaintRects,
     switches::kEnableOpenMax,
