3cb0e293c319e5c8eaa87dae39f30a01875f5bb8
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-26 21:00:54 +0000

    Content Settings dialog and related Options UI changes.
    BUG=32719
    TEST=none
    
    Review URL: http://codereview.chromium.org/554045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37153 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/options_util.cc
+++ chrome/browser/options_util.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -78,7 +78,6 @@ void OptionsUtil::ResetToDefaults(Profile* profile) {
   // don't reset it.
   const wchar_t* kLocalStatePrefs[] = {
     prefs::kApplicationLocale,
-    prefs::kOptionsWindowLastTabIndex,
   };
   for (size_t i = 0; i < arraysize(kLocalStatePrefs); ++i)
     local_state->ClearPref(kLocalStatePrefs[i]);
