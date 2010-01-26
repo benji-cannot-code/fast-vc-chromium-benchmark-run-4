9621c53c2d65e3b009af482bb5df30c28cf0adf2
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-26 19:50:54 +0000

    Add support for UserIdleTime to Linux.
    BUG=32421
    TEST=none
    
    Review URL: http://codereview.chromium.org/553051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37136 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/sync/engine/idle_query_linux.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SYNC_ENGINE_IDLE_QUERY_LINUX_H_
+#define CHROME_BROWSER_SYNC_ENGINE_IDLE_QUERY_LINUX_H_
+
+#include "base/scoped_ptr.h"
+
+namespace browser_sync {
+
+class IdleData;
+
+class IdleQueryLinux {
+ public:
+  IdleQueryLinux();
+  ~IdleQueryLinux();
+  int IdleTime();
+
+ private:
+  scoped_ptr<IdleData> idle_data_;
+};
+
+}  // namespace browser_sync
+#endif  // CHROME_BROWSER_SYNC_ENGINE_IDLE_QUERY_LINUX_H_
