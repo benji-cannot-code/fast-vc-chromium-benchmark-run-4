c07d908e4426581eb4a50148ccb5687090fc057c
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-26 07:20:45 +0000

    Hopefully fix the Linux build for real. I didn't realize *_X86 wasn't defined on X86_64.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/555097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37094 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/gpu/gpu_config.h
+++ chrome/gpu/gpu_config.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if defined(OS_LINUX) && defined(ARCH_CPU_X86)
+#if defined(OS_LINUX) && !defined(ARCH_CPU_ARMEL)
 
 // Only define GLX support for Intel CPUs for now until we can get the
 // proper dependencies and build setup for ARM.
