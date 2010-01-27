817309926b19bbf34f10392847a78a903a10d8c5
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-27 04:45:37 +0000

    2010-01-26  Kent Tamura  <tkent@chromium.org>
            Reviewed by Darin Adler.
    
            HTMLInputElement::valueAsNumber support except type=datetime-local.
            https://bugs.webkit.org/show_bug.cgi?id=32696
    
            input-valueasnumber-datetime-expected.txt and
            input-valueasnumber-time-expected.txt have FAIL lines because they
            test unimplemented features.
    
            * fast/forms/input-valueasnumber-date-expected.txt: Added.
            * fast/forms/input-valueasnumber-date.html: Added.
            * fast/forms/input-valueasnumber-datetime-expected.txt: Added.
            * fast/forms/input-valueasnumber-datetime.html: Added.
            * fast/forms/input-valueasnumber-month-expected.txt: Added.
            * fast/forms/input-valueasnumber-month.html: Added.
            * fast/forms/input-valueasnumber-number-expected.txt: Added.
            * fast/forms/input-valueasnumber-number.html: Added.
            * fast/forms/input-valueasnumber-range-expected.txt: Added.
            * fast/forms/input-valueasnumber-range.html: Added.
            * fast/forms/input-valueasnumber-time-expected.txt: Added.
            * fast/forms/input-valueasnumber-time.html: Added.
            * fast/forms/input-valueasnumber-unsupported-expected.txt: Added.
            * fast/forms/input-valueasnumber-unsupported.html: Added.
            * fast/forms/input-valueasnumber-week-expected.txt: Added.
            * fast/forms/input-valueasnumber-week.html: Added.
            * fast/forms/script-tests/input-valueasnumber-date.js: Added.
            * fast/forms/script-tests/input-valueasnumber-datetime.js: Added.
            * fast/forms/script-tests/input-valueasnumber-month.js: Added.
            * fast/forms/script-tests/input-valueasnumber-number.js: Added.
            * fast/forms/script-tests/input-valueasnumber-range.js: Added.
            * fast/forms/script-tests/input-valueasnumber-time.js: Added.
            * fast/forms/script-tests/input-valueasnumber-unsupported.js: Added.
            * fast/forms/script-tests/input-valueasnumber-week.js: Added.
    
    2010-01-26  Kent Tamura  <tkent@chromium.org>
    
            Reviewed by Darin Adler.
    
            HTMLInputElement::valueAsNumber support except type=datetime-local.
            https://bugs.webkit.org/show_bug.cgi?id=32696
    
            Tests: fast/forms/input-valueasnumber-date.html
                   fast/forms/input-valueasnumber-datetime.html
                   fast/forms/input-valueasnumber-month.html
                   fast/forms/input-valueasnumber-number.html
                   fast/forms/input-valueasnumber-range.html
                   fast/forms/input-valueasnumber-time.html
                   fast/forms/input-valueasnumber-unsupported.html
                   fast/forms/input-valueasnumber-week.html
    
            * html/HTMLInputElement.cpp:
            (WebCore::HTMLInputElement::valueAsNumber):
              According to the specification, we should return NaN for invalid values.
            (WebCore::HTMLInputElement::setValueAsNumber):
              According to the specification, throws NOT_SUPPORTED_ERR for Infinitiy or NaN,
              and throws INVALID_STATE_ERR for unsupported types.
            (WebCore::HTMLInputElement::formStringToDouble):
              According to the step 14 of Real Number in the specification, we
              should round -0 to 0.
            * html/HTMLInputElement.h: Declare valueAsNumber() and setValueAsNumber().
            * html/HTMLInputElement.idl: Add valueAsNumber.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53893 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLInputElement.h
+++ third_party/WebKit/WebCore/html/HTMLInputElement.h
@@ -148,6 +148,9 @@ public:
     double valueAsDate() const;
     void setValueAsDate(double, ExceptionCode&);
 
+    double valueAsNumber() const;
+    void setValueAsNumber(double, ExceptionCode&);
+
     virtual String placeholder() const;
     virtual void setPlaceholder(const String&);
 
