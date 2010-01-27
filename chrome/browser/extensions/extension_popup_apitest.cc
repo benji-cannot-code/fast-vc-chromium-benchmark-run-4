04e487397c6fb3aa32eacd26c1f1f1ccaec2ba1b
twiz@google.com <twiz@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-27 18:07:22 +0000

    Correction of permissions of extension manifest file for the extension_popup_apitest.cc unit test.
    I also removed the flaky designator from this test, as I was able to run the test (under stress conditions of 10 concurrent instances) without failure.
    
    BUG=27271
    TEST=extension_popup_apitest.cc (ExtensionApiTest.Popup)
    Review URL: http://codereview.chromium.org/548157
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37266 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_popup_apitest.cc
+++ chrome/browser/extensions/extension_popup_apitest.cc
@@ -7,8 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_switches.h"
 
-// Flaky, http://crbug.com/27271.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, FLAKY_Popup) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Popup) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
   CommandLine::ForCurrentProcess()->AppendSwitch(
