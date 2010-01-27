f8f75712c90974de2bd0a49cc510e903d12975e7
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-27 14:25:11 +0000

    2010-01-27  Kent Hansen  <kent.hansen@nokia.com>
            Reviewed by Simon Hausmann.
    
            [Qt] Meta-methods can't be introspected using ES5 API
            https://bugs.webkit.org/show_bug.cgi?id=34087
    
            Add getOwnPropertyDescriptor() and getOwnPropertyNames() reimplementations.
    
            Tests are in WebKit/qt/tests/qwebframe
    
            * bridge/qt/qt_runtime.cpp:
            (JSC::Bindings::QtRuntimeMetaMethod::getOwnPropertyDescriptor):
            (JSC::Bindings::QtRuntimeMetaMethod::getOwnPropertyNames):
            (JSC::Bindings::QtRuntimeConnectionMethod::getOwnPropertyDescriptor):
            (JSC::Bindings::QtRuntimeConnectionMethod::getOwnPropertyNames):
            * bridge/qt/qt_runtime.h:
    2010-01-27  Kent Hansen  <kent.hansen@nokia.com>
    
            Reviewed by Simon Hausmann.
    
            [Qt] Meta-methods can't be introspected using ES5 API
            https://bugs.webkit.org/show_bug.cgi?id=34087
    
            Test that Object.getOwnPropertyDescriptor and
            Object.getOwnPropertyNames work with meta-methods.
    
            * tests/qwebframe/tst_qwebframe.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53930 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_runtime.h
+++ third_party/WebKit/WebCore/bridge/qt/qt_runtime.h
@@ -156,7 +156,7 @@ public:
     }
 
 protected:
-    static const unsigned StructureFlags = OverridesGetOwnPropertySlot | InternalFunction::StructureFlags | OverridesMarkChildren;
+    static const unsigned StructureFlags = OverridesGetOwnPropertySlot | OverridesGetPropertyNames | InternalFunction::StructureFlags | OverridesMarkChildren;
 
     QtRuntimeMethodData *d_func() const {return d_ptr;}
     QtRuntimeMethod(QtRuntimeMethodData *dd, ExecState *exec, const Identifier &n, PassRefPtr<QtInstance> inst);
@@ -169,6 +169,8 @@ public:
     QtRuntimeMetaMethod(ExecState *exec, const Identifier &n, PassRefPtr<QtInstance> inst, int index, const QByteArray& signature, bool allowPrivate);
 
     virtual bool getOwnPropertySlot(ExecState *, const Identifier&, PropertySlot&);
+    virtual bool getOwnPropertyDescriptor(ExecState*, const Identifier&, PropertyDescriptor&);
+    virtual void getOwnPropertyNames(ExecState*, PropertyNameArray&, EnumerationMode mode = ExcludeDontEnumProperties);
 
     virtual void markChildren(MarkStack& markStack);
 
@@ -190,6 +192,8 @@ public:
     QtRuntimeConnectionMethod(ExecState *exec, const Identifier &n, bool isConnect, PassRefPtr<QtInstance> inst, int index, const QByteArray& signature );
 
     virtual bool getOwnPropertySlot(ExecState *, const Identifier&, PropertySlot&);
+    virtual bool getOwnPropertyDescriptor(ExecState*, const Identifier&, PropertyDescriptor&);
+    virtual void getOwnPropertyNames(ExecState*, PropertyNameArray&, EnumerationMode mode = ExcludeDontEnumProperties);
 
 protected:
     QtRuntimeConnectionMethodData* d_func() const {return reinterpret_cast<QtRuntimeConnectionMethodData*>(d_ptr);}
