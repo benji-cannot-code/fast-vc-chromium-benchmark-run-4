3b15409739c683ae843d1dc3f6504dff5b3b0179
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-27 01:21:48 +0000

    Do not try to swap chrome.exe if machine is shutting down.
    BUG=18063
    TEST=Installer an update while Chrome is running. Log out of machine without closing Chrome first and make sure sure Chrome gets updated cleanly whenever it is launched next.
    
    Review URL: http://codereview.chromium.org/553079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37202 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_shutdown.cc
+++ chrome/browser/browser_shutdown.cc
@@ -149,7 +149,8 @@ void Shutdown() {
     ResourceBundle::CleanupSharedInstance();
 
 #if defined(OS_WIN)
-  if (!Upgrade::IsBrowserAlreadyRunning()) {
+  if (!Upgrade::IsBrowserAlreadyRunning() &&
+      shutdown_type_ != browser_shutdown::END_SESSION) {
     Upgrade::SwapNewChromeExeIfPresent();
   }
 
