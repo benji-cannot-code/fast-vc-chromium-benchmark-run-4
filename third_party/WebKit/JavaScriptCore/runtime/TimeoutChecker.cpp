e8a87109f289b0c31f14fcfdcc7b40a642f8ecdb
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-27 13:34:24 +0000

    2010-01-27  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Eric Seidel.
    
            [BREWMP] Port getCPUTime
            https://bugs.webkit.org/show_bug.cgi?id=33572
    
            Use GETUPTIMEMS which returns a continuously and
            linearly increasing millisecond timer from the time the device
            was powered on. This function is enough to implement getCPUTime.
    
            * runtime/TimeoutChecker.cpp:
            (JSC::getCPUTime):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53926 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/TimeoutChecker.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/TimeoutChecker.cpp
@@ -42,6 +42,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CurrentTime.h"
 #endif
 
+#if PLATFORM(BREWMP)
+#include <AEEStdLib.h>
+#endif
+
 using namespace std;
 
 namespace JSC {
@@ -81,6 +85,11 @@ static inline unsigned getCPUTime()
     GetThreadTimes(GetCurrentThread(), &creationTime, &exitTime, &kernelTime.fileTime, &userTime.fileTime);
     
     return userTime.fileTimeAsLong / 10000 + kernelTime.fileTimeAsLong / 10000;
+#elif PLATFORM(BREWMP)
+    // This function returns a continuously and linearly increasing millisecond
+    // timer from the time the device was powered on.
+    // There is only one thread in BREW, so this is enough.
+    return GETUPTIMEMS();
 #else
     // FIXME: We should return the time the current thread has spent executing.
     return currentTime() * 1000;
