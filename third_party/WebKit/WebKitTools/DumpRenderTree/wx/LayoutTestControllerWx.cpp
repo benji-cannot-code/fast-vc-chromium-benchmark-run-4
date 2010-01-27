4954661457dfd4ea24f503470bbd486aacf326fa
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-27 00:09:13 +0000

    [wx] Build fix, make sure stub function returns a value.
    git-svn-id: svn://svn.chromium.org/blink/trunk@53870 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/wx/LayoutTestControllerWx.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/wx/LayoutTestControllerWx.cpp
@@ -358,5 +358,5 @@ void LayoutTestController::whiteListAccessFromOrigin(JSStringRef sourceOrigin, J
 
 JSRetainPtr<JSStringRef> LayoutTestController::counterValueForElementById(JSStringRef id)
 {
-
+    return 0;
 }
