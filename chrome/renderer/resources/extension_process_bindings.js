17918171e1a18af7e39d0ca5bf9c86ab9d704391
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-27 23:07:47 +0000

    revert 37335:
    Add an accessibility API for events raised outside of the web content.
    
    BUG=none
    TEST=none
    
    patch by Dominic Mazzoni <dmazzoni [at] google>
    review url: http://codereview.chromium.org/402099/show
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37337 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extension_process_bindings.js
+++ chrome/renderer/resources/extension_process_bindings.js
@@ -552,9 +552,6 @@ var chrome = chrome || {};
   if (!chrome.experimental)
     chrome.experimental = {};
 
-  if (!chrome.experimental.accessibility)
-    chrome.experimental.accessibility = {};
-
   if (!chrome.experimental.history)
     chrome.experimental.history = {};
 
