f8b4e7d070d61dca1fe283025de09fb5c57bb157
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-27 01:20:55 +0000

    linux: also load plugins from ~/.config/google-chrome/Plugins
    This matches Mozilla, which uses ~/.mozilla/plugins.
    
    BUG=22261
    TEST=watch an strace, see that it hits the appropriate dir when loading plugins
    
    Review URL: http://codereview.chromium.org/553102
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37201 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_service.cc
+++ chrome/browser/plugin_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/plugin_service.h"
 
 #include "base/command_line.h"
+#include "base/path_service.h"
 #include "base/string_util.h"
 #include "base/thread.h"
 #include "base/waitable_event.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/plugin_process_host.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
 #include "chrome/common/chrome_plugin_lib.h"
+#include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/gpu_plugin.h"
@@ -88,6 +90,14 @@ PluginService::PluginService()
   if (hklm_key_.StartWatching()) {
     hklm_event_.reset(new base::WaitableEvent(hklm_key_.watch_event()));
     hklm_watcher_.StartWatching(hklm_event_.get(), this);
+  }
+#elif defined(OS_POSIX) && !defined(OS_MACOSX)
+  // Also find plugins in a user-specific plugins dir,
+  // e.g. ~/.config/chromium/Plugins.
+  FilePath user_data_dir;
+  if (PathService::Get(chrome::DIR_USER_DATA, &user_data_dir)) {
+    NPAPI::PluginList::Singleton()->AddExtraPluginDir(
+        user_data_dir.Append("Plugins"));
   }
 #endif
 
