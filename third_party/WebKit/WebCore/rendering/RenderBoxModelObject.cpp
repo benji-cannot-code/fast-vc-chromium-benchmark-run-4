50ae8180178d48558c03a9b9c216147277b02bce
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-27 21:36:16 +0000

    2010-01-27  Darin Fisher  <darin@chromium.org>
            Fix Chromium build bustage.
    
            * rendering/RenderBoxModelObject.cpp: Add wtf/CurrentTime.h include.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBoxModelObject.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBoxModelObject.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderInline.h"
 #include "RenderLayer.h"
 #include "RenderView.h"
+#include <wtf/CurrentTime.h>
 
 using namespace std;
 
