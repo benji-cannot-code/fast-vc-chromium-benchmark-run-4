111c703fd0cf06bc04719fe770fabdc6cc974cfc
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-27 19:45:15 +0000

    [Mac] Hook up AutocompleteEditViewMac delegate calls properly.
    BUG=None
    TEST=None
    Review URL: http://codereview.chromium.org/546107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37290 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view_mac.mm
+++ chrome/browser/autocomplete/autocomplete_edit_view_mac.mm
@@ -553,6 +553,7 @@ void AutocompleteEditViewMac::OnDidBeginEditing() {
   NSEvent* theEvent = [NSApp currentEvent];
   const bool controlDown = ([theEvent modifierFlags]&NSControlKeyMask) != 0;
   model_->OnSetFocus(controlDown);
+  controller_->OnSetFocus();
 
   // Capture the current state.
   OnBeforePossibleChange();
@@ -571,6 +572,7 @@ void AutocompleteEditViewMac::OnDidEndEditing() {
 
   // Tell the model to reset itself.
   model_->OnKillFocus();
+  controller_->OnKillFocus();
 }
 
 bool AutocompleteEditViewMac::OnDoCommandBySelector(SEL cmd) {
