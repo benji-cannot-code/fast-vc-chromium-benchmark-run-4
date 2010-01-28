b9b02aee2e44a8e55d23fd13967e8ed3f2d261c4
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-28 23:03:10 +0000

    Fix an assertion beneath SerializedScriptValue::deserialize on 32-bit builds (e.g., on Windows)
    Passing a JSValue to toRef on 32-bit builds can perform an allocation,
    so we need to make sure we hold a JSLock when that occurs.
    
    Reviewed by Oliver Hunt.
    
    * bindings/js/SerializedScriptValue.cpp:
    (WebCore::SerializedScriptValueData::serialize):
    (WebCore::SerializedScriptValueData::deserialize):
    Moved JSLocks from here...
    
    (WebCore::SerializedScriptValue::create):
    (WebCore::SerializedScriptValue::deserialize):
    ...to this higher level, from which toRef can be called.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/SerializedScriptValue.cpp
+++ third_party/WebKit/WebCore/bindings/js/SerializedScriptValue.cpp
@@ -599,7 +599,6 @@ private:
 
 SerializedScriptValueData SerializedScriptValueData::serialize(ExecState* exec, JSValue inValue)
 {
-    JSLock lock(SilenceAssertionsOnly);
     SerializingTreeWalker context(exec);
     return walk<SerializingTreeWalker>(context, inValue);
 }
@@ -762,7 +761,6 @@ private:
 
 JSValue SerializedScriptValueData::deserialize(ExecState* exec, JSGlobalObject* global, bool mustCopy) const
 {
-    JSLock lock(SilenceAssertionsOnly);
     DeserializingTreeWalker context(exec, global, mustCopy);
     return walk<DeserializingTreeWalker>(context, *this);
 }
@@ -922,6 +920,7 @@ SerializedScriptValue::~SerializedScriptValue()
 
 PassRefPtr<SerializedScriptValue> SerializedScriptValue::create(JSContextRef originContext, JSValueRef apiValue, JSValueRef* exception)
 {
+    JSLock lock(SilenceAssertionsOnly);
     ExecState* exec = toJS(originContext);
     JSValue value = toJS(exec, apiValue);
     PassRefPtr<SerializedScriptValue> serializedValue = SerializedScriptValue::create(exec, value);
@@ -937,6 +936,7 @@ PassRefPtr<SerializedScriptValue> SerializedScriptValue::create(JSContextRef ori
 
 JSValueRef SerializedScriptValue::deserialize(JSContextRef destinationContext, JSValueRef* exception)
 {
+    JSLock lock(SilenceAssertionsOnly);
     ExecState* exec = toJS(destinationContext);
     JSValue value = deserialize(exec, exec->lexicalGlobalObject());
     if (exec->hadException()) {
