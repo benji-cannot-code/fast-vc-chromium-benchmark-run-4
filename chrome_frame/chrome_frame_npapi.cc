120d89c363842e26e8a3615692d19321da3f8ea3
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-28 19:36:58 +0000

    Window.open calls issued by pages within ChromeFrame which use the NEW_WINDOW/NEW_POPUP dispositions open up as regular Chrome windows, which results in them not using the host network stack, which means that HTTP sessions/cookies, etc established by the main page will not work for the popup.
    Fix is to ensure that these dispositions also get routed back to the host browser where a new tab
    would be created which would attach itself to the newly created ExternalTabContainer instance.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=33324
    
    Bug=33324
    Test=Covered by ChromeFrame unit test.
    
    Review URL: http://codereview.chromium.org/549183
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37425 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_frame_npapi.cc
+++ chrome_frame/chrome_frame_npapi.cc
@@ -501,6 +501,7 @@ void ChromeFrameNPAPI::OnOpenURL(int tab_handle,
       target = "_blank";
       break;
     case NEW_WINDOW:
+    case NEW_POPUP:
       target = "_new";
       break;
     default:
