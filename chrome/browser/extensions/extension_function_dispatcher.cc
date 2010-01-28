818dc5babf52fb13f6c890d7f805936c156776ed
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-28 00:10:29 +0000

    Allow extensions to add, remove, or change their page action popup.
    A similar change will be made for browser action popups.
    
    BUG=27526
    TEST=Added unit tests.  Manual testing on linux.
    
    Review URL: http://codereview.chromium.org/545068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37353 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_dispatcher.cc
+++ chrome/browser/extensions/extension_function_dispatcher.cc
@@ -114,6 +114,7 @@ void FactoryRegistry::ResetFunctions() {
   RegisterFunction<PageActionHideFunction>();
   RegisterFunction<PageActionSetIconFunction>();
   RegisterFunction<PageActionSetTitleFunction>();
+  RegisterFunction<PageActionSetPopupFunction>();
 
   // Browser Actions.
   RegisterFunction<BrowserActionSetIconFunction>();
