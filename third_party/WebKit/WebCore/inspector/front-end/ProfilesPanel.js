387718b824210a89ed7775ed8fee80d1a27e8ac8
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-28 12:34:02 +0000

    2010-01-28  Mikhail Naganov  <mnaganov@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Simplify views logic in Profiler panel, and fix the problem described in the issue.
            Remove image from Welcome view.
            Preserve border on status bar-like buttons in Welcome view on window deactivation.
    
            https://bugs.webkit.org/show_bug.cgi?id=34201
    
            * inspector/front-end/ProfilesPanel.js:
            (WebInspector.ProfilesPanel.prototype.show):
            (WebInspector.ProfilesPanel.prototype.reset):
            (WebInspector.ProfilesPanel.prototype.addProfileHeader):
            (WebInspector.ProfilesPanel.prototype.showView):
            (WebInspector.ProfilesPanel.prototype._updateInterface):
            * inspector/front-end/WelcomeView.js:
            (WebInspector.WelcomeView):
            * inspector/front-end/inspector.css:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@53996 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ProfilesPanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/ProfilesPanel.js
@@ -158,12 +158,6 @@ WebInspector.ProfilesPanel.prototype = {
     show: function()
     {
         WebInspector.Panel.prototype.show.call(this);
-        this._updateInterface();
-        if (!this.visibleView) {
-            this.visibleView = this.welcomeView;
-            this.welcomeView.show();
-        }
-        this.visibleView.show();
         if (this._shouldPopulateProfiles)
             this._populateProfiles();
     },
@@ -191,6 +185,7 @@ WebInspector.ProfilesPanel.prototype = {
     {
         for (var i = 0; i < this._profiles.length; ++i)
             delete this._profiles[i]._profileView;
+        delete this.visibleView;
 
         delete this.currentQuery;
         this.searchCanceled();
@@ -210,6 +205,7 @@ WebInspector.ProfilesPanel.prototype = {
         this.profileViewStatusBarItemsContainer.removeChildren();
 
         this._updateInterface();
+        this.welcomeView.show();
     },
 
     registerProfileType: function(profileType)
@@ -308,7 +304,8 @@ WebInspector.ProfilesPanel.prototype = {
         profile._profilesTreeElement = profileTreeElement;
 
         sidebarParent.appendChild(profileTreeElement);
-        if (!this.visibleView || this.visibleView === this.welcomeView)
+        this.welcomeView.hide();
+        if (!this.visibleView)
             this.showProfile(profile);
     },
 
@@ -337,19 +334,7 @@ WebInspector.ProfilesPanel.prototype = {
 
     showView: function(view)
     {
-        if (this.visibleView === view) {
-            if (this.visibleView && !this.visibleView.visible)
-                view.show();
-            return;
-        }
-
-        this._previousVisibleView = this.visibleView;
-        if (view === this.welcomeView) {
-            this.closeVisibleView();
-            this.visibleView = view;
-            view.show();
-        } else
-            this.showProfile(view.profile);
+        this.showProfile(view.profile);
     },
 
     getProfileType: function(typeId)
@@ -452,17 +437,13 @@ WebInspector.ProfilesPanel.prototype = {
                 this._profileTypeButtonsByIdMap[typeId].removeStyleClass("hidden");
             this.profileViewStatusBarItemsContainer.removeStyleClass("hidden");
             this.panelEnablerView.visible = false;
-            this.welcomeView.visible = true;
-            this.visibleView = this._previousVisibleView || this.welcomeView;
         } else {
             this.enableToggleButton.title = WebInspector.UIString("Profiling disabled. Click to enable.");
             this.enableToggleButton.toggled = false;
             for (var typeId in this._profileTypeButtonsByIdMap)
                 this._profileTypeButtonsByIdMap[typeId].addStyleClass("hidden");
             this.profileViewStatusBarItemsContainer.addStyleClass("hidden");
-            this.welcomeView.visible = false;
             this.panelEnablerView.visible = true;
-            this.visibleView = this.panelEnablerView;
         }
     },
 
