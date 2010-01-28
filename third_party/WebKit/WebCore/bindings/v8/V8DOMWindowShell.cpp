1b0c1c975e1887460727a2db07098455321d257a
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-28 21:34:44 +0000

    2010-01-28  Steve Block  <steveblock@google.com>
            Reviewed by Adam Barth.
    
            Fix missing forward-declarations and includes in V8 bindings
            https://bugs.webkit.org/show_bug.cgi?id=34272
    
            No new tests, build fix only.
    
            * bindings/v8/ScriptController.h: Modified. Forward-declare NPObject
            * bindings/v8/V8DOMWindowShell.cpp: Modified. Include CString.h and PlatformBridge.h
            * bindings/v8/V8DOMWrapper.h: Modified. Forward-declare V8Proxy
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54014 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8DOMWindowShell.cpp
+++ third_party/WebKit/WebCore/bindings/v8/V8DOMWindowShell.cpp
@@ -32,7 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "V8DOMWindowShell.h"
 
-#include "ChromiumBridge.h"
+#include "CString.h"
+#include "PlatformBridge.h"
 #include "CSSMutableStyleDeclaration.h"
 #include "DateExtension.h"
 #include "DocumentLoader.h"
