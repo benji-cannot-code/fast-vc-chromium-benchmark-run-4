115c15b371c60404c781829a85340a6da3bba7e9
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-28 00:32:08 +0000

    First revision of the AutoFill settings dialog. What works: 1. Layout (for the most part) 2. Editing and saving the items What does not 1. Scroll 2. Delete/Add buttons 3. some layout quirks 4. collapsible editable sets. BUG=33026 TEST=none for now. Review URL: http://codereview.chromium.org/554081
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37357 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_dialog_win.cc
+++ chrome/browser/autofill/autofill_dialog_win.cc
@@ -4,12 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/autofill/autofill_dialog.h"
+#include "chrome/browser/autofill/autofill_profiles_view_win.h"
 
-// TODO(dhollowa): Remove these as each platform implements this
-// function.  The last one to implement the function should remove this file.
-#if defined(OS_MACOSX)
 void ShowAutoFillDialog(AutoFillDialogObserver* observer,
                         const std::vector<AutoFillProfile*>& profiles,
                         const std::vector<CreditCard*>& credit_cards) {
+  AutoFillProfilesView::Show(observer, profiles, credit_cards);
 }
-#endif  // defined(OS_WIN) || defined(OS_MACOSX)
+
