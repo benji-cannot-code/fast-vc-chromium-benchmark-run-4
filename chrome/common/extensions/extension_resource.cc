50b03d237f7d1a65308b8c77172d81ff336549f6
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-28 03:42:48 +0000

    Removing unnecessary includes.
    BUG=0
    TEST=none
    
    Review URL: http://codereview.chromium.org/549186
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37378 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_resource.cc
+++ chrome/common/extensions/extension_resource.cc
@@ -5,11 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/extensions/extension_resource.h"
 
-#include "base/file_path.h"
 #include "base/file_util.h"
-#include "base/string_util.h"
-#include "chrome/common/extensions/extension_l10n_util.h"
-#include "googleurl/src/gurl.h"
+#include "base/logging.h"
 
 ExtensionResource::ExtensionResource() {
 }
