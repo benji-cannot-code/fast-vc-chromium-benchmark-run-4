45d31bd3dc950e41c8c2f78b22e79ac4bd279c0c
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-29 22:08:05 +0000

    2010-01-29  Steve Falkenburg  <sfalken@apple.com>
            Reviewed by Darin Adler.
    
            Client-based Geolocation starts updating before getting consent from the user
            https://bugs.webkit.org/show_bug.cgi?id=34343
    
            * page/Geolocation.cpp:
            (WebCore::Geolocation::startRequest): Pass notifier instead of options to startUpdating.
            (WebCore::Geolocation::setIsAllowed): Add the observer or notify of error for deferred startUpdating.
            (WebCore::Geolocation::startUpdating): Pass notifier instead of options, since we may need to call it if we fail to get user consent.
            Defer adding the observer if we don't yet have user consent, since this could kick off
            server-based wifi Geolocation requests.
            * page/Geolocation.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Geolocation.cpp
+++ third_party/WebKit/WebCore/page/Geolocation.cpp
@@ -246,7 +246,7 @@ PassRefPtr<Geolocation::GeoNotifier> Geolocation::startRequest(PassRefPtr<Positi
     if (isDenied())
         notifier->setFatalError(PositionError::create(PositionError::PERMISSION_DENIED, permissionDeniedErrorMessage));
     else {
-        if (notifier->hasZeroTimeout() || startUpdating(notifier->m_options.get()))
+        if (notifier->hasZeroTimeout() || startUpdating(notifier.get()))
             notifier->startTimerIfNeeded();
         else
             notifier->setFatalError(PositionError::create(PositionError::POSITION_UNAVAILABLE, "Failed to start Geolocation service"));
@@ -301,6 +301,26 @@ void Geolocation::resume()
 void Geolocation::setIsAllowed(bool allowed)
 {
     m_allowGeolocation = allowed ? Yes : No;
+    
+#if ENABLE(CLIENT_BASED_GEOLOCATION)
+    if (m_startRequestPermissionNotifier) {
+        if (isAllowed()) {
+            // Permission request was made during the startUpdating process
+            m_startRequestPermissionNotifier = 0;
+            if (!m_frame)
+                return;
+            Page* page = m_frame->page();
+            if (!page)
+                return;
+            page->geolocationController()->addObserver(this);
+        } else {
+            m_startRequestPermissionNotifier->setFatalError(PositionError::create(PositionError::PERMISSION_DENIED, permissionDeniedErrorMessage));
+            m_oneShots.add(m_startRequestPermissionNotifier);
+            m_startRequestPermissionNotifier = 0;
+        }
+        return;
+    }
+#endif
     
     if (isAllowed())
         makeSuccessCallbacks();
@@ -480,12 +500,17 @@ void Geolocation::geolocationServiceErrorOccurred(GeolocationService* service)
 
 #endif
 
-bool Geolocation::startUpdating(PositionOptions* options)
+bool Geolocation::startUpdating(GeoNotifier* notifier)
 {
 #if ENABLE(CLIENT_BASED_GEOLOCATION)
     // FIXME: Pass options to client.
-    UNUSED_PARAM(options);
 
+    if (!isAllowed()) {
+        m_startRequestPermissionNotifier = notifier;
+        requestPermission();
+        return true;
+    }
+    
     if (!m_frame)
         return false;
 
@@ -496,7 +521,7 @@ bool Geolocation::startUpdating(PositionOptions* options)
     page->geolocationController()->addObserver(this);
     return true;
 #else
-    return m_service->startUpdating(options);
+    return m_service->startUpdating(notifier->options);
 #endif
 }
 
