e23399138fbe9639490c7e74b2061a840602f3c8
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-29 22:35:33 +0000

    Fix passing pointers between processes.
    BUG=31880
    Review URL: http://codereview.chromium.org/558036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37555 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_delegate.h
+++ webkit/glue/webplugin_delegate.h
@@ -95,7 +95,7 @@ class WebPluginDelegate : public WebPlugin2DDeviceDelegate,
   // Receives notification about a resource load that the plugin initiated
   // for a frame.
   virtual void DidFinishLoadWithReason(const GURL& url, NPReason reason,
-                                       intptr_t notify_data) = 0;
+                                       int notify_id) = 0;
 
   // Returns the process id of the process that is running the plugin.
   virtual int GetProcessId() = 0;
@@ -104,8 +104,8 @@ class WebPluginDelegate : public WebPlugin2DDeviceDelegate,
   // function.
   virtual void SendJavaScriptStream(const GURL& url,
                                     const std::string& result,
-                                    bool success, bool notify_needed,
-                                    intptr_t notify_data) = 0;
+                                    bool success,
+                                    int notify_id) = 0;
 
   // Receives notification about data being available.
   virtual void DidReceiveManualResponse(const GURL& url,
@@ -130,9 +130,12 @@ class WebPluginDelegate : public WebPlugin2DDeviceDelegate,
   virtual WebPluginResourceClient* CreateResourceClient(
       unsigned long resource_id,
       const GURL& url,
-      bool notify_needed,
-      intptr_t notify_data,
-      intptr_t stream) = 0;
+      int notify_id) = 0;
+
+  // Creates a WebPluginResourceClient instance for an existing stream that is
+  // has become seekable.
+  virtual WebPluginResourceClient* CreateSeekableResourceClient(
+      unsigned long resource_id, int range_request_id) = 0;
 };
 
 }  // namespace webkit_glue
