aad847e95ef5e5e88af896f4080cc4dc67e5b412
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-29 14:10:54 +0000

    2010-01-29  Alexander Pavlov  <apavlov@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: Source view displays text using wrong fonts
            https://bugs.webkit.org/show_bug.cgi?id=34269
    
            * inspector/front-end/TextEditor.js:
            (WebInspector.TextEditor):
            (WebInspector.TextEditor.prototype._initFont):
            * inspector/front-end/inspector.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/inspector.js
+++ third_party/WebKit/WebCore/inspector/front-end/inspector.js
@@ -56,13 +56,6 @@ var WebInspector = {
     cookieDomains: {},
     missingLocalizedStrings: {},
     pendingDispatches: 0,
-    OS: {
-        Windows: "windows",
-        WindowsVistaOrLater: "windows-vista-or-later",
-        MacTiger: "mac-tiger",
-        MacLeopard: "mac-leopard",
-        MacSnowLeopard: "mac-snowleopard"
-    },
 
     // RegExp groups:
     // 1 - scheme
@@ -421,6 +414,14 @@ var WebInspector = {
             this.showingDOMNodeHighlight = false;
         }
     }
+}
+
+WebInspector.OS = {
+    Windows: "windows",
+    WindowsVistaOrLater: "windows-vista-or-later",
+    MacTiger: "mac-tiger",
+    MacLeopard: "mac-leopard",
+    MacSnowLeopard: "mac-snowleopard"
 }
 
 WebInspector.loaded = function()
