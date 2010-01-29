516bc88024da0b3b3a835fa7ec7a339a5bddd9e2
slightlyoff@chromium.org <slightlyoff@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-29 22:07:10 +0000

    Re-enables CFInstance RPC tests for Firefox that were previously timing out due to insufficient windows for allowing plugin startup. We now assume that startup/loading will succeed in 5 seconds (reasonable for a debug build or slow bots) and that we should wait no longer than 1 seconds to send the ack messages. This puts us well inside the 10s initialization window of CFInstall.js, upped from a previous 1s window in:
    	http://codereview.chromium.org/464077
    
    BUG=24100
    TEST=chrome_frame_tests.exe --gtest_filter=*WidgetModeFF_CFInstanceRPCInternal*
    
    Review URL: http://codereview.chromium.org/551199
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37550 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/chrome_frame_unittests.cc
+++ chrome_frame/test/chrome_frame_unittests.cc
@@ -524,9 +524,7 @@ TEST_F(ChromeFrameTestWithWebServer, WidgetModeIE_CFInstanceRPCInternal) {
   SimpleBrowserTest(IE, kCFIRPCInternalPage, L"CFInstanceRPCInternal");
 }
 
-// Disabled: http://b/issue?id=2050201
-TEST_F(ChromeFrameTestWithWebServer,
-       DISABLED_WidgetModeFF_CFInstanceRPCInternal) {
+TEST_F(ChromeFrameTestWithWebServer, WidgetModeFF_CFInstanceRPCInternal) {
   SimpleBrowserTest(FIREFOX, kCFIRPCInternalPage, L"CFInstanceRPCInternal");
 }
 
