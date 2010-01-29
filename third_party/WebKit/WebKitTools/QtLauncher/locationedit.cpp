cfab0eb24b7d7eba4534194c5a2c03de4f64fddf
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-29 19:46:56 +0000

    [Qt] Show progress reaching 100% for loads.
    Reviewed by Ariya Hidayat.
    
    * QtLauncher/locationedit.cpp:
    (LocationEdit::LocationEdit):
    (LocationEdit::setProgress):
    (LocationEdit::reset):
    (LocationEdit::paintEvent):
    * QtLauncher/locationedit.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54072 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/QtLauncher/locationedit.cpp
+++ third_party/WebKit/WebKitTools/QtLauncher/locationedit.cpp
@@ -29,15 +29,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "locationedit.h"
 
 LocationEdit::LocationEdit(QWidget* parent)
-    : QLineEdit(parent),
-      m_progress(0)
+    : QLineEdit(parent)
+    , m_progress(0)
 {
+    m_clearTimer.setSingleShot(true);
+    connect(&m_clearTimer, SIGNAL(timeout()), this, SLOT(reset()));
 }
 
 void LocationEdit::setProgress(int progress)
 {
+    m_clearTimer.stop();
     m_progress = progress;
     update();
+}
+
+void LocationEdit::reset()
+{
+    setProgress(0);
 }
 
 void LocationEdit::paintEvent(QPaintEvent* ev)
@@ -46,16 +54,20 @@ void LocationEdit::paintEvent(QPaintEvent* ev)
     QColor progressColor = QColor(120, 180, 240);
     QPalette p = palette();
 
-    if (!m_progress || m_progress == 100)
+    if (!m_progress)
         p.setBrush(QPalette::Base, backgroundColor);
     else {
         QLinearGradient gradient(0, 0, width(), 0);
         gradient.setColorAt(0, progressColor);
-        gradient.setColorAt(((double)m_progress) / 100, progressColor);
-        gradient.setColorAt(((double)m_progress) / 100 + 0.001, backgroundColor);
+        gradient.setColorAt(((double) m_progress) / 100, progressColor);
+        if (m_progress != 100)
+            gradient.setColorAt((double) m_progress / 100 + 0.001, backgroundColor);
         p.setBrush(QPalette::Base, gradient);
     }
     setPalette(p);
 
     QLineEdit::paintEvent(ev);
+
+    if (m_progress == 100)
+        m_clearTimer.start(100);
 }
