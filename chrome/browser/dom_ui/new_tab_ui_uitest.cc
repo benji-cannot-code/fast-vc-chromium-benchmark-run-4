f8b0dada9c1146013d116a2af298a37dcb9a3ce4
victorw@chromium.org <victorw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-29 01:49:20 +0000

    Disable NewTabUITest.HomePageLink
    TBR=aa
    BUG=33451
    TEST=mac bot ui test green
    
    Review URL: http://codereview.chromium.org/552226
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37481 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/new_tab_ui_uitest.cc
+++ chrome/browser/dom_ui/new_tab_ui_uitest.cc
@@ -112,6 +112,8 @@ TEST_F(NewTabUITest, UpdateUserPrefsVersion) {
   ASSERT_FALSE(migrated);
 }
 
+// TODO(aa): http://crbug.com/33451
+#if !defined(OS_MACOSX)
 TEST_F(NewTabUITest, HomePageLink) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
@@ -183,3 +185,4 @@ TEST_F(NewTabUITest, HomePageLink) {
               &is_home_page));
   ASSERT_TRUE(is_home_page);
 }
+#endif
