0e6264d466237373b1e511d098f02b63de7da599
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-29 00:31:22 +0000

    Add missing includes for HashMap/HashSet https://bugs.webkit.org/show_bug.cgi?id=34276
    Reviewed by David Levin.
    
    No new tests, build fix only.
    
    * platform/KURL.cpp: Modified. Include HashMap.h
    * platform/network/CredentialStorage.cpp: Modified. Include HashMap.h and HashSet.h
    * platform/network/ProtectionSpaceHash.h: Modified. Include HashTraits.h
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54028 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/KURL.cpp
+++ third_party/WebKit/WebCore/platform/KURL.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CString.h"
 #include "StringHash.h"
 #include "TextEncoding.h"
+#include <wtf/HashMap.h>
 #include <wtf/StdLibExtras.h>
 
 #if USE(ICU_UNICODE)
--- third_party/WebKit/WebCore/platform/network/ProtectionSpaceHash.h
+++ third_party/WebKit/WebCore/platform/network/ProtectionSpaceHash.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ProtectionSpaceHash_h
 
 #include "ProtectionSpace.h"
+#include <wtf/HashTraits.h>
 
 namespace WebCore {
 
