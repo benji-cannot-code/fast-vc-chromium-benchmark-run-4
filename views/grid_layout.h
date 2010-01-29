a3eeea21ff30076be9f69b294a4f70da26b7a873
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-29 22:34:12 +0000

    Try to add a useful comment about a case I hit today, at Scott's urging.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/558031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37554 0039d316-1c4b-4281-b951-d872f2087c98

--- views/grid_layout.h
+++ views/grid_layout.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -276,6 +276,11 @@ class ColumnSet {
   // with no explicit alignment. fixed_width gives a specific width for the
   // column, and is only used if size_type == FIXED. min_width gives the
   // minimum width for the column.
+  //
+  // If none of the columns in a columnset are resizable, the views are only
+  // made as wide as the widest views in each column, even if extra space is
+  // provided. In other words, GridLayout does not automatically resize views
+  // unless the column is marked as resizable.
   void AddColumn(GridLayout::Alignment h_align,
                  GridLayout::Alignment v_align,
                  float resize_percent,
