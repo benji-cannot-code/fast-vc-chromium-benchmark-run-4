19dbdece1c186dfeb4fd9d035a01b1dad6f925ed
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-29 01:15:07 +0000

    2010-01-28  Nayan Kumar K  <nayankk@gmail.com>
            Reviewed by Laszlo Gombos.
    
            Fix for compilation error in WML enabled build introduced because of the changeset 53514.
    
            https://bugs.webkit.org/show_bug.cgi?id=34254
    
            * wml/WMLDocument.cpp:
            (WebCore::WMLDocument::WMLDocument):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/wml/WMLDocument.cpp
+++ third_party/WebKit/WebCore/wml/WMLDocument.cpp
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 WMLDocument::WMLDocument(Frame* frame)
-    : Document(frame, false) 
+    : Document(frame, false, false) 
     , m_activeCard(0)
 {
     clearXMLVersion();
