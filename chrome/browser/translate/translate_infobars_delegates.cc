501402237e63d1c55bfbdf3318b5c2d6ec6d10fb
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-29 23:48:06 +0000

    use stubs for yet-to-be-implemented non-windows translate infobars
    BUG=31297
    TEST=none.
    Review URL: http://codereview.chromium.org/557064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37567 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_infobars_delegates.cc
+++ chrome/browser/translate/translate_infobars_delegates.cc
@@ -178,6 +178,13 @@ void BeforeTranslateInfoBarDelegate::ToggleSiteBlacklist() {
     prefs_.RemoveSiteFromBlacklist(site_);
 }
 
+#if !defined(TOOLKIT_VIEWS)
+InfoBar* BeforeTranslateInfoBarDelegate::CreateInfoBar() {
+  NOTIMPLEMENTED();
+  return NULL;
+}
+#endif  // !TOOLKIT_VIEWS
+
 // AfterTranslateInfoBarDelegate: public: --------------------------------------
 
 AfterTranslateInfoBarDelegate::AfterTranslateInfoBarDelegate(
@@ -213,3 +220,10 @@ void AfterTranslateInfoBarDelegate::ToggleAlwaysTranslate() {
     prefs_.RemoveLanguagePairFromWhitelist(original_language(),
         target_language());
 }
+
+#if !defined(TOOLKIT_VIEWS)
+InfoBar* AfterTranslateInfoBarDelegate::CreateInfoBar() {
+  NOTIMPLEMENTED();
+  return NULL;
+}
+#endif  // !TOOLKIT_VIEWS
