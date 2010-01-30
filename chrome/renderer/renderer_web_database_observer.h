cdd352e12a5b593261955d8b6ed317d4e9a7d569
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-30 01:04:25 +0000

    Wait for the DB thread to finish posting tasks to the main thread before setting the WebKit client to NULL.
    TEST=none
    BUG=32228,32226
    
    Review URL: http://codereview.chromium.org/545178
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37594 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/renderer_web_database_observer.h
+++ chrome/renderer/renderer_web_database_observer.h
@@ -1,13 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.  Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef CHROME_RENDERER_RENDERER_WEB_DATABASE_OBSERVER_H_
 #define CHROME_RENDERER_RENDERER_WEB_DATABASE_OBSERVER_H_
 
 #include "ipc/ipc_message.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebDatabaseObserver.h"
+#include "webkit/database/database_connections.h"
 
 class RendererWebDatabaseObserver : public WebKit::WebDatabaseObserver {
  public:
@@ -16,8 +17,12 @@ class RendererWebDatabaseObserver : public WebKit::WebDatabaseObserver {
   virtual void databaseModified(const WebKit::WebDatabase& database);
   virtual void databaseClosed(const WebKit::WebDatabase& database);
 
+  void WaitForAllDatabasesToClose();
+
  private:
   IPC::Message::Sender* sender_;
+  bool waiting_for_dbs_to_close_;
+  webkit_database::DatabaseConnections database_connections_;
 };
 
 #endif  // CHROME_RENDERER_RENDERER_WEB_DATABASE_OBSERVER_H_
