07948ee000d8c2e16b930f18c4ed32de415d096c
cira@google.com <cira@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-30 00:01:05 +0000

    Trying to re-land. It's the same as http://codereview.chromium.org/546040. Moved IsValidName to public (where it was before the change), and made test cases for it cleaner by explicitly using std::string() around var name. Template IsValidName method had to be defined within .h file to avoid linking error on mac release build.
    Add reserved messages to ExtensionMessageBundle dictionary. They are of the form @@somename, i.e. @@ui_locale.
    
    TEST=If user specified any reserved messages in the messages.json we should show error. In memory dictionary should have @@ui_locale set to current Chrome UI locale.
    
    Review URL: http://codereview.chromium.org/555159
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37571 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_constants.cc
+++ chrome/common/extensions/extension_constants.cc
@@ -211,6 +211,8 @@ const char* kLocalesMessagesFileMissing =
     "Messages file is missing for locale.";
 const char* kInvalidOptionsPage =
     "Invalid value for 'options_page'.";
+const char* kReservedMessageFound =
+    "Reserved key * found in message catalog.";
 }  // namespace extension_manifest_errors
 
 namespace extension_urls {
