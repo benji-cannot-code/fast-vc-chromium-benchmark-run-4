b67faa6ea7344e9227624f5d877159857d4b0683
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-30 02:53:39 +0000

    Rewrite the HostContentSettingsMap to address some issues: * Interface not well-designed for the actual uses * Objects in URL request context must be refcounted since Profile is destroyed before the UI thread
    BUG=33314
    TEST=none
    Review URL: http://codereview.chromium.org/555184
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37603 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_url_request_context.h
+++ chrome/browser/net/chrome_url_request_context.h
@@ -206,7 +206,7 @@ class ChromeURLRequestContext : public URLRequestContext {
   FilePath user_script_dir_path_;
 
   scoped_refptr<ChromeAppCacheService> appcache_service_;
-  HostContentSettingsMap* host_content_settings_map_;
+  scoped_refptr<HostContentSettingsMap> host_content_settings_map_;
   scoped_refptr<HostZoomMap> host_zoom_map_;
   scoped_refptr<Blacklist> privacy_blacklist_;
 
@@ -376,7 +376,7 @@ class ChromeURLRequestContextFactory {
   // TODO(aa): I think this can go away now as we no longer support standalone
   // user scripts.
   FilePath user_script_dir_path_;
-  HostContentSettingsMap* host_content_settings_map_;
+  scoped_refptr<HostContentSettingsMap> host_content_settings_map_;
   scoped_refptr<HostZoomMap> host_zoom_map_;
   scoped_refptr<Blacklist> privacy_blacklist_;
   net::TransportSecurityState* transport_security_state_;
