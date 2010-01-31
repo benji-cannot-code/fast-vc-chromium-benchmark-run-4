b2fcfb60b7be0ee74a0087f84bf13d0b9d73cac7
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-31 07:38:52 +0000

    2010-01-31  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Make ScriptState destructor protected since all non-empty
            ScriptStates are managed by GC. Remove obsolete constructor.
    
            https://bugs.webkit.org/show_bug.cgi?id=34266
    
            * bindings/v8/ScriptState.cpp:
            * bindings/v8/ScriptState.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54108 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/ScriptState.cpp
+++ third_party/WebKit/WebCore/bindings/v8/ScriptState.cpp
@@ -43,11 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-ScriptState::ScriptState(Frame*, v8::Handle<v8::Context> context)
-    : m_context(v8::Persistent<v8::Context>::New(context))
-{
-}
-
 ScriptState::ScriptState(v8::Handle<v8::Context> context)
     : m_context(v8::Persistent<v8::Context>::New(context))
 {
