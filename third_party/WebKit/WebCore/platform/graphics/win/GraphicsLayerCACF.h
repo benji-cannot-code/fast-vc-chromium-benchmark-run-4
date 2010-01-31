8a748f5cfe44eea033e46d3c255287e66d56efdb
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-01-31 23:43:20 +0000

    2010-01-31  Eric Carlson  <eric.carlson@apple.com>
            Revert r54095 because it caused many media tests to fail.
    
            * platform/graphics/win/GraphicsLayerCACF.cpp:
            (WebCore::GraphicsLayerCACF::setContentsToVideo):
            (WebCore::GraphicsLayerCACF::updateContentsVideo):
            * platform/graphics/win/GraphicsLayerCACF.h:
            (WebCore::GraphicsLayerCACF::):
            * platform/graphics/win/QTMovieWin.cpp:
            (QTMovieWinPrivate::task):
            (QTMovieWinPrivate::setSize):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54114 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/GraphicsLayerCACF.h
+++ third_party/WebKit/WebCore/platform/graphics/win/GraphicsLayerCACF.h
@@ -83,7 +83,7 @@ public:
     virtual void setContentsRect(const IntRect&);
 
     virtual void setContentsToImage(Image*);
-    virtual void setContentsToMedia(PlatformLayer*);
+    virtual void setContentsToVideo(PlatformLayer*);
     
     virtual PlatformLayer* platformLayer() const;
 
@@ -116,7 +116,7 @@ private:
     void updateLayerBackgroundColor();
 
     void updateContentsImage();
-    void updateContentsMedia();
+    void updateContentsVideo();
     void updateContentsRect();
     void updateGeometryOrientation();
     
@@ -130,7 +130,7 @@ private:
     enum ContentsLayerPurpose {
         NoContentsLayer = 0,
         ContentsLayerForImage,
-        ContentsLayerForMedia
+        ContentsLayerForVideo
     };
     
     ContentsLayerPurpose m_contentsLayerPurpose;
