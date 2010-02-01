ad7cb60ef8e868fd26a8b51dc76282c4a514a359
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-01 19:04:07 +0000

    Temporarily allow writing to the default malloc zone structure while modifying it.
    BUG=none
    TEST=no crash when launching Chromium next year
    
    Review URL: http://codereview.chromium.org/551229
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37721 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_mac.mm
+++ base/process_util_mac.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <mach/task.h>
 #include <malloc/malloc.h>
 #include <spawn.h>
+#include <sys/mman.h>
 #include <sys/sysctl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/eintr_wrapper.h"
 #include "base/logging.h"
 #include "base/string_util.h"
+#include "base/sys_info.h"
 #include "base/sys_string_conversions.h"
 #include "base/time.h"
 
@@ -424,7 +426,25 @@ void EnableTerminationOnOutOfMemory() {
   // Nevertheless this is better than nothing for now.
   // TODO(avi):Do better. http://crbug.com/12673
 
+  int32 major;
+  int32 minor;
+  int32 bugfix;
+  SysInfo::OperatingSystemVersionNumbers(&major, &minor, &bugfix);
+  bool zone_allocators_protected = ((major == 10 && minor > 6) || major > 10);
+
   malloc_zone_t* default_zone = malloc_default_zone();
+
+  vm_address_t page_start;
+  vm_size_t len;
+  if (zone_allocators_protected) {
+    // See http://trac.webkit.org/changeset/53362/trunk/WebKitTools/DumpRenderTree/mac
+    page_start = reinterpret_cast<vm_address_t>(default_zone) &
+        static_cast<vm_size_t>(~(getpagesize() - 1));
+    len = reinterpret_cast<vm_address_t>(default_zone) -
+        page_start + sizeof(malloc_zone_t);
+    mprotect(reinterpret_cast<void*>(page_start), len, PROT_READ | PROT_WRITE);
+  }
+
   g_old_malloc = default_zone->malloc;
   g_old_calloc = default_zone->calloc;
   g_old_valloc = default_zone->valloc;
@@ -436,6 +456,10 @@ void EnableTerminationOnOutOfMemory() {
   default_zone->calloc = oom_killer_calloc;
   default_zone->valloc = oom_killer_valloc;
   default_zone->realloc = oom_killer_realloc;
+
+  if (zone_allocators_protected) {
+    mprotect(reinterpret_cast<void*>(page_start), len, PROT_READ);
+  }
 }
 
 }  // namespace base
