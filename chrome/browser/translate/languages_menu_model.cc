d3581cb1a98a25e6f5baab9d816ea188d13620e4
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-02 06:42:11 +0000

    implement proper localized display names for translate languages - in the process, modify to use indexes into vector of languages to communicate between infobars and delegates
    BUG=31297
    TEST=verify that language names in translate infobars' language buttons, dropdown lists and options are properly localized.
    Review URL: http://codereview.chromium.org/566012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37809 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/languages_menu_model.cc
+++ chrome/browser/translate/languages_menu_model.cc
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved. Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #include "chrome/browser/translate/languages_menu_model.h"
 
@@ -25,7 +25,7 @@ LanguagesMenuModel::LanguagesMenuModel(
   }
   std::vector<std::string>::const_iterator iter = languages.begin();
   for (int i = base_command_id; iter != languages.end(); ++i, ++iter) {
-    AddItem(i, ASCIIToUTF16(*iter));
+    AddItem(i, TranslateInfoBarDelegate::GetDisplayNameForLocale(*iter));
   }
 }
 
