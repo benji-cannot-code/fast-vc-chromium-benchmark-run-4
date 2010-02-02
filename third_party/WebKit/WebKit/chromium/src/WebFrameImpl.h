529965c95b45c8d0418479d1bad2e42dcdef6f4a
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-02 08:25:30 +0000

    2010-02-02  Kent Tamura  <tkent@chromium.org>
            Reviewed by Darin Fisher.
    
            [Chromium] Should not select a word on right-click.
            https://bugs.webkit.org/show_bug.cgi?id=33364
    
            For non-Mac platforms, do not select a word around the caret when
            a context menu is opening. This behavior is not common in non-Mac
            platforms, and it prevents pasting with a context menu.
    
            In order that the spell checker works without the selection, we
            introduce WebFrame::selectWordAroundCaret(). We can replace a word
            around the caret with selectWordAroundCaret() + replaceSelection().
    
            * public/WebFrame.h: Add pure selectWordAroundCaret() declaration.
            * src/ContextMenuClientImpl.cpp:
            (WebKit::selectMisspelledWord): Move word-selection code to
            WebFrameImpl::selectWordAroundPosition(), and clear the selection
            on non-Mac.
            * src/WebFrameImpl.cpp:
            (WebKit::WebFrameImpl::selectWordAroundPosition):
            (WebKit::WebFrameImpl::selectWordAroundCaret):
            * src/WebFrameImpl.h: Add selectWordAroundCaret() declaration.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54212 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebFrameImpl.h
+++ third_party/WebKit/WebKit/chromium/src/WebFrameImpl.h
@@ -142,6 +142,7 @@ public:
     virtual WebRange selectionRange() const;
     virtual WebString selectionAsText() const;
     virtual WebString selectionAsMarkup() const;
+    virtual bool selectWordAroundCaret();
     virtual int printBegin(const WebSize& pageSize);
     virtual float printPage(int pageToPrint, WebCanvas*);
     virtual float getPrintPageShrink(int page);
@@ -219,6 +220,8 @@ public:
 
     WebFrameClient* client() const { return m_client; }
     void dropClient() { m_client = 0; }
+
+    static void selectWordAroundPosition(WebCore::Frame*, WebCore::VisiblePosition);
 
 private:
     class DeferredScopeStringMatches;
