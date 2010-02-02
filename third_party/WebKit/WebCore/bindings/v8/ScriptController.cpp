38a9f33199dd6505a7f6dc96c51af75f5a7b29ba
hamaji@chromium.org <hamaji@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-02 04:13:58 +0000

    2010-02-01  Shinichiro Hamaji  <hamaji@chromium.org>
            [Chromium] Unreviewed attempt to fix the chromium build.
    
            Corresponding change: http://trac.webkit.org/changeset/54182
    
            * bindings/scripts/CodeGeneratorV8.pm:
            * bindings/v8/ScriptController.cpp:
            (WebCore::ScriptController::processingUserGesture):
            * bindings/v8/ScriptController.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/ScriptController.cpp
+++ third_party/WebKit/WebCore/bindings/v8/ScriptController.cpp
@@ -149,7 +149,7 @@ void ScriptController::updatePlatformScriptObjects()
     notImplemented();
 }
 
-bool ScriptController::processingUserGesture() const
+bool ScriptController::processingUserGesture(DOMWrapperWorld*) const
 {
     Frame* activeFrame = V8Proxy::retrieveFrameForEnteredContext();
     // No script is running, so it must be run by users.
