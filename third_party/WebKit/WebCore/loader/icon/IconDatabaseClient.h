ab44fc66957043af60280a8a1baf753486451eaa
zoltan@webkit.org <zoltan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-02 07:56:07 +0000

    2010-02-01  Zoltan Horvath  <zoltan@webkit.org>
            Reviewed by Darin Adler.
    
            Allow custom memory allocation control for IconDatabaseClient class
            https://bugs.webkit.org/show_bug.cgi?id=33252
    
            Inherits the following struct from Noncopyable because it is 
            instantiated by 'new' and no need to be copyable:
    
            class name               - instantiated at: WebCore/'location'
            class IconDatabaseClient - loader/icon/IconDatabase.cpp:89
    
            * loader/icon/IconDatabaseClient.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54210 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/icon/IconDatabaseClient.h
+++ third_party/WebKit/WebCore/loader/icon/IconDatabaseClient.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IconDatabaseClient_h
 #define IconDatabaseClient_h
 
+#include <wtf/Noncopyable.h>
+
 // All of these client methods will be called from a non-main thread
 // Take appropriate measures
  
@@ -37,7 +39,7 @@ namespace WebCore {
 
 class String;
 
-class IconDatabaseClient {
+class IconDatabaseClient : public Noncopyable {
 public:
     virtual ~IconDatabaseClient() { }
     virtual bool performImport() { return true; }
