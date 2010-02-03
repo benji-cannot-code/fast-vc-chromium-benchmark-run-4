1f67d09591fad71c5f3e15878dc6eb4c26020a09
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-03 01:43:04 +0000

    Revert 37923 - More Cros bustage fixing
    TBR=oshima
    
    Review URL: http://codereview.chromium.org/564026
    
    TBR=ben@chromium.org
    Review URL: http://codereview.chromium.org/563016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37926 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/browser_view.cc
+++ chrome/browser/chromeos/browser_view.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/frame/browser_extender.h"
 #include "chrome/browser/views/frame/browser_frame_gtk.h"
 #include "chrome/browser/views/frame/browser_view.h"
-#include "chrome/browser/views/frame/browser_view_layout.h"
+#include "chrome/browser/views/frame/chrome_browser_view_layout_manager.h"
 #include "chrome/browser/views/tabs/tab.h"
 #include "chrome/browser/views/tabs/tab_overview_types.h"
 #include "chrome/browser/views/tabs/tab_strip.h"
