da07939a9cc4669ba72d4f084317b83e1fee3f8d
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-03 00:37:18 +0000

    Clear local state (cookies, databases, local storage) on exit.
    BUG=32719
    TEST=none
    
    Review URL: http://codereview.chromium.org/564012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37913 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/sqlite_persistent_cookie_store.h
+++ chrome/browser/net/sqlite_persistent_cookie_store.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -32,6 +32,8 @@ class SQLitePersistentCookieStore
   virtual void UpdateCookieAccessTime(
       const net::CookieMonster::CanonicalCookie&);
   virtual void DeleteCookie(const net::CookieMonster::CanonicalCookie&);
+
+  static void ClearLocalState(const FilePath& path);
 
  private:
   class Backend;
--- chrome/browser/options_util.cc
+++ chrome/browser/options_util.cc
@@ -24,6 +24,7 @@ void OptionsUtil::ResetToDefaults(Profile* profile) {
   const wchar_t* kUserPrefs[] = {
     prefs::kAcceptLanguages,
     prefs::kAlternateErrorPagesEnabled,
+    prefs::kClearSiteDataOnExit,
     prefs::kCookieBehavior,
     prefs::kDefaultCharset,
     prefs::kDnsPrefetchingEnabled,
