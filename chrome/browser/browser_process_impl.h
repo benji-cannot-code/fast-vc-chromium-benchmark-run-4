f23611a199d38decd2e5295dcb110d06dd437286
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-03 02:35:08 +0000

    Reland 37913.  Clear local state on exit.
    BUG=32719
    TEST=none
    
    Review URL: http://codereview.chromium.org/560024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37936 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_impl.h
+++ chrome/browser/browser_process_impl.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 class CommandLine;
+class FilePath;
 class NotificationService;
 
 // Real implementation of BrowserProcess that creates and returns the services.
@@ -213,6 +214,9 @@ class BrowserProcessImpl : public BrowserProcess, public NonThreadSafe {
 #endif
 
  private:
+  void ClearLocalState(const FilePath& profile_path);
+  bool ShouldClearLocalState(FilePath* profile_path);
+
   void CreateResourceDispatcherHost();
   void CreatePrefService();
   void CreateMetricsService();
--- chrome/browser/views/options/cookie_filter_page_view.cc
+++ chrome/browser/views/options/cookie_filter_page_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/options/cookies_view.h"
 #include "chrome/browser/views/options/exceptions_view.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/pref_service.h"
 #include "grit/generated_resources.h"
 #include "grit/locale_settings.h"
 #include "views/controls/button/checkbox.h"
@@ -33,6 +34,8 @@ CookieFilterPageView::CookieFilterPageView(Profile* profile)
       clear_on_close_check_(NULL),
       show_cookies_button_(NULL),
       OptionsPageView(profile) {
+  clear_site_data_on_exit_.Init(prefs::kClearSiteDataOnExit,
+                                profile->GetPrefs(), NULL);
 }
 
 CookieFilterPageView::~CookieFilterPageView() {
@@ -129,8 +132,6 @@ void CookieFilterPageView::InitControlLayout() {
       l10n_util::GetString(IDS_COOKIES_CLEAR_WHEN_CLOSE_CHKBOX));
   clear_on_close_check_->set_listener(this);
 
-  // TODO(pkasting): Set clear-on-close checkbox to be checked or not.
-
   layout->StartRow(0, single_column_set_id);
   layout->AddView(clear_on_close_check_);
   layout->AddPaddingRow(0, kRelatedControlVerticalSpacing);
@@ -150,6 +151,13 @@ void CookieFilterPageView::InitControlLayout() {
   layout->StartRow(0, single_column_set_id);
   layout->AddView(flash_settings_link, 1, 1, GridLayout::LEADING,
                   GridLayout::FILL);
+}
+
+void CookieFilterPageView::NotifyPrefChanged(const std::wstring* pref_name) {
+  if (!pref_name || *pref_name == prefs::kClearSiteDataOnExit) {
+    clear_on_close_check_->SetChecked(
+        clear_site_data_on_exit_.GetValue());
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -174,7 +182,7 @@ void CookieFilterPageView::ButtonPressed(
   } else if (sender == block_3rdparty_check_) {
     settings_map->SetBlockThirdPartyCookies(block_3rdparty_check_->checked());
   } else if (sender == clear_on_close_check_) {
-    // TODO(pkasting): Set clear-on-close setting.
+    clear_site_data_on_exit_.SetValue(clear_on_close_check_->checked());
   } else {
     DCHECK_EQ(sender, show_cookies_button_);
     UserMetricsRecordAction("Options_ShowCookies", NULL);
