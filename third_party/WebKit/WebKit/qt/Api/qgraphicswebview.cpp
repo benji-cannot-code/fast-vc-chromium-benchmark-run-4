c9fd8ec3fc78e3e2f883ea655b942be0a9d7ae6b
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-03 18:02:01 +0000

    Unreviewed build fix.
            [Qt] Roll-out r54281 because it broke the build on the Qt Release bot.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
+++ third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qwebpage_p.h"
 #include "QWebPageClient.h"
 #include <FrameView.h>
-#include <QtCore/qmetaobject.h>
 #include <QtCore/qsharedpointer.h>
 #include <QtCore/qtimer.h>
 #include <QtGui/qapplication.h>
@@ -80,14 +79,13 @@ public:
         , page(0)
 #if USE(ACCELERATED_COMPOSITING)
         , rootGraphicsLayer(0)
-        , shouldSync(false)
+        , shouldSync(true)
 #endif
     {
 #if USE(ACCELERATED_COMPOSITING)
         // the overlay and stays alive for the lifetime of
         // this QGraphicsWebView as the scrollbars are needed when there's no compositing
         q->setFlag(QGraphicsItem::ItemUsesExtendedStyleOption);
-        syncMetaMethod = q->metaObject()->method(q->metaObject()->indexOfMethod("syncLayers()"));
 #endif
     }
 
@@ -133,9 +131,6 @@ public:
     // we need to sync the layers if we get a special call from the WebCore
     // compositor telling us to do so. We'll get that call from ChromeClientQt
     bool shouldSync;
-
-    // we have to flush quite often, so we use a meta-method instead of QTimer::singleShot for putting the event in the queue
-    QMetaMethod syncMetaMethod;
 
     // we need to put the root graphics layer behind the overlay (which contains the scrollbar)
     enum { RootGraphicsLayerZValue, OverlayZValue };
@@ -184,7 +179,7 @@ void QGraphicsWebViewPrivate::markForSync(bool scheduleSync)
 {
     shouldSync = true;
     if (scheduleSync)
-        syncMetaMethod.invoke(q, Qt::QueuedConnection);
+        QTimer::singleShot(0, q, SLOT(syncLayers()));
 }
 
 void QGraphicsWebViewPrivate::updateCompositingScrollPosition()
@@ -230,7 +225,6 @@ void QGraphicsWebViewPrivate::update(const QRect & dirtyRect)
 #if USE(ACCELERATED_COMPOSITING)
     if (overlay)
         overlay->update(QRectF(dirtyRect));
-    syncLayers();
 #endif
 }
 
@@ -449,6 +443,7 @@ void QGraphicsWebView::paint(QPainter* painter, const QStyleOptionGraphicsItem* 
 {
 #if USE(ACCELERATED_COMPOSITING)
     page()->mainFrame()->render(painter, d->overlay ? QWebFrame::ContentsLayer : QWebFrame::AllLayers, option->exposedRect.toAlignedRect());
+    d->syncLayers();
 #else
     page()->mainFrame()->render(painter, QWebFrame::AllLayers, option->exposedRect.toRect());
 #endif
