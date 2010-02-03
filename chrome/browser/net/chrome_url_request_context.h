e2221fa15c36e4a365f41f0893337d5ad12737b6
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-03 22:14:15 +0000

    Revert 38001 and 38002.
    Modify CookiePolicy to work asynchronously
    
    This change will enable us to prompt the user before setting a cookie.  While
    we only need to prompt before setting, we actually need to make both
    CanSetCookie and CanGetCookies asynchronous.  This is necessary in order to
    preserve FIFO ordering since the value returned by GetCookies depends on the
    changes made to the cookie DB by SetCookie.
    
    This change also includes some simplification of CookieStore.  Instead of N
    virtual functions, I distilled it down to only 4.  The remaining functions are
    instead expressed in terms of those.
    
    While studying all the places where we currently use CookiePolicy, I found that
    some of them were not appropriate.  After discussing with Amit, I decided to
    remove the policy checks in URLRequestAutomationJob.  See the comments in the
    code regarding this.
    
    I changed the signature of CookieMonster::GetRawCookies to GetAllCookiesForURL
    to better match GetAllCookies.  Related to this change webkit/glue/webcookie.h
    grows a constructor that takes a CanonicalCookie to help clean up some code.
    
    On the Chrome side, ChromeURLRequestContext now has a ChromeCookiePolicy
    object.  That object is threadsafe ref counted because it is passed between the
    UI and IO threads.  It is responsible for implementing the queuing logic
    described above.  It will also in the future trigger the Chrome UI code to
    actually show the setcookie prompt.
    
    Please review the state machinery changes in URLRequestHttpJob carefully.
    
    R=eroman
    BUG=34331
    TEST=no tests yet for prompting.
    
    Review URL: http://codereview.chromium.org/564045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38028 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_url_request_context.h
+++ chrome/browser/net/chrome_url_request_context.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/host_content_settings_map.h"
 #include "chrome/browser/host_zoom_map.h"
 #include "chrome/browser/privacy_blacklist/blacklist.h"
+#include "chrome/browser/net/chrome_cookie_policy.h"
 #include "chrome/browser/net/url_request_context_getter.h"
 #include "chrome/common/appcache/chrome_appcache_service.h"
 #include "chrome/common/extensions/extension.h"
@@ -35,8 +36,7 @@ class IOThread;
 //
 // All methods of this class must be called from the IO thread,
 // including the constructor and destructor.
-class ChromeURLRequestContext : public URLRequestContext,
-                                public net::CookiePolicy {
+class ChromeURLRequestContext : public URLRequestContext {
  public:
   // Maintains some extension-related state we need on the IO thread.
   // TODO(aa): It would be cool if the Extension objects in ExtensionsService
@@ -122,10 +122,6 @@ class ChromeURLRequestContext : public URLRequestContext,
   // False only if cookies are globally blocked without exception.
   bool AreCookiesEnabled() const;
 
-  // CookiePolicy methods:
-  virtual bool CanGetCookies(const GURL& url, const GURL& first_party);
-  virtual bool CanSetCookie(const GURL& url, const GURL& first_party);
-
  protected:
   // Copies the dependencies from |other| into |this|. If you use this
   // constructor, then you should hold a reference to |other|, as we
@@ -168,6 +164,10 @@ class ChromeURLRequestContext : public URLRequestContext,
   void set_cookie_store(net::CookieStore* cookie_store) {
     cookie_store_ = cookie_store;
   }
+  void set_cookie_policy(ChromeCookiePolicy* cookie_policy) {
+    chrome_cookie_policy_ = cookie_policy;  // Take a strong reference.
+    cookie_policy_ = cookie_policy;
+  }
   void set_proxy_service(net::ProxyService* service) {
     proxy_service_ = service;
   }
@@ -207,6 +207,7 @@ class ChromeURLRequestContext : public URLRequestContext,
   FilePath user_script_dir_path_;
 
   scoped_refptr<ChromeAppCacheService> appcache_service_;
+  scoped_refptr<ChromeCookiePolicy> chrome_cookie_policy_;
   scoped_refptr<HostContentSettingsMap> host_content_settings_map_;
   scoped_refptr<HostZoomMap> host_zoom_map_;
   scoped_refptr<Blacklist> privacy_blacklist_;
