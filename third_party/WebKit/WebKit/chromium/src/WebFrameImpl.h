6e4a2736e0e6a9756b35f629b7877c6696738753
hamaji@chromium.org <hamaji@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-03 04:20:05 +0000

    2010-02-02  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            [Chromium] Implement WebFrameImpl::pageNumberForElementById
            https://bugs.webkit.org/show_bug.cgi?id=34471
    
            * public/WebFrame.h:
            * src/WebFrameImpl.cpp:
            (WebKit::WebFrameImpl::pageNumberForElementById):
            * src/WebFrameImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54270 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebFrameImpl.h
+++ third_party/WebKit/WebKit/chromium/src/WebFrameImpl.h
@@ -165,6 +165,9 @@ public:
     virtual WebString contentAsMarkup() const;
     virtual WebString renderTreeAsText() const;
     virtual WebString counterValueForElementById(const WebString& id) const;
+    virtual int pageNumberForElementById(const WebString& id,
+                                         float pageWidthInPixels,
+                                         float pageHeightInPixels) const;
 
     static PassRefPtr<WebFrameImpl> create(WebFrameClient* client);
     ~WebFrameImpl();
