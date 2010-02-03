97affa914ab3aff2bb4acda699e3bd1de924b75c
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-03 06:40:21 +0000

    Copy percent-escaped URL when copying all the text from the address bar (Mac)
    BUG=31104
    TEST=Visit http://www.google.com/search?ie=UTF-8&q=上地 and select entire text in Omnibox, then copy the text.
    Paste the copied text into other applications such as notepad and check if the pasted text contains %E4%B8%8A%E5%9C%B0, not 上地.
    Paste the copied text into Safari's URL bar and type return. Check if you are navigated to the same website.
    Paste the copied text into textareas in Chrome and check if the pasted text contains %E4%B8%8A%E5%9C%B0.
    Paste the copied text into Gmail's rich text editing mode and check if the pasted text contains 上地.
    Right click the link you pasted in Gmail and check if you see "Go to: http://www.google.com/...%E4%B8%8A%E5%9C%B0".
    Paste the copied text into TextEdit and check if the pasted text isn't garbled and correctly contains 上地.
    Check if the link in TextEdit is pointing to encoded URL (http://www.google.com/...%E4%B8%8A%E5%9C%B0).
    TEST=Visit http://www.google.com/search?ie=UTF-8&q=上地 again and select "q=上地" in Omnibox, then copy the text.
    Paste it into somewhere and check if you see "q=上地".
    TEST=Copy "上地" from somewhere and paste it into Omnibox.
    Copy from Omnibox and paste it into notepad. Then check if the pasted text is NOT encoded.
    
    Review URL: http://codereview.chromium.org/549172
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37955 0039d316-1c4b-4281-b951-d872f2087c98

--- app/clipboard/clipboard_unittest.cc
+++ app/clipboard/clipboard_unittest.cc
@@ -35,7 +35,7 @@ namespace {
 
 bool ClipboardContentsIsExpected(const string16& copied_markup,
                                  const string16& pasted_markup) {
-#if defined(OS_LINUX)
+#if defined(OS_POSIX)
   return pasted_markup.find(copied_markup) != string16::npos;
 #else
   return copied_markup == pasted_markup;
@@ -354,4 +354,3 @@ TEST_F(ClipboardTest, WriteEverything) {
 
   // Passes if we don't crash.
 }
-
