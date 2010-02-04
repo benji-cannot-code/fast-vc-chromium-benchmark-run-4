5eb26d88daf10d04b023ed19c5e93150bafa9567
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-04 17:28:56 +0000

    Fix bustage. The app panel frame change went in moments before mine :-(
    BUG=none
    TEST=none
    TBR=sky
    
    Review URL: http://codereview.chromium.org/577002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38111 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/app_panel_browser_frame_view.h
+++ chrome/browser/views/frame/app_panel_browser_frame_view.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/controls/button/button.h"
 #include "views/window/non_client_view.h"
 
+class BaseTabStrip;
 class BrowserView;
 class TabContents;
-class TabStrip;
 namespace gfx {
 class Font;
 }
@@ -34,7 +34,7 @@ class AppPanelBrowserFrameView : public BrowserNonClientFrameView,
   virtual ~AppPanelBrowserFrameView();
 
   // Overridden from BrowserNonClientFrameView:
-  virtual gfx::Rect GetBoundsForTabStrip(TabStrip* tabstrip) const;
+  virtual gfx::Rect GetBoundsForTabStrip(BaseTabStrip* tabstrip) const;
   virtual void UpdateThrobber(bool running);
   virtual gfx::Size GetMinimumSize();
 
