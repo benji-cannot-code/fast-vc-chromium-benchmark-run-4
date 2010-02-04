0b6d4ce6add7df8a67bc2b3417de5d713feffd83
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-04 00:50:08 +0000

    Fix for https://bugs.webkit.org/show_bug.cgi?id=34547 static shouldTrackVisitedLinks is not initialized
    Reviewed by Brady Eidson.
    
    For ports that don't call into WebKit to initialize this, 
    we should initialize to true so they get the visited link behavior by default.
    
    * page/PageGroup.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54313 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/PageGroup.cpp
+++ third_party/WebKit/WebCore/page/PageGroup.cpp
@@ -52,7 +52,7 @@ static unsigned getUniqueIdentifier()
 
 // --------
 
-static bool shouldTrackVisitedLinks;
+static bool shouldTrackVisitedLinks = true;
 
 PageGroup::PageGroup(const String& name)
     : m_name(name)
