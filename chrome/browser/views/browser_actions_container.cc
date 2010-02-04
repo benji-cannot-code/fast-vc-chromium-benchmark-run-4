92447d638387ef1928fa8991bb9c7dfa595c70c3
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-04 21:53:28 +0000

    Fixing paint issue with browser actions not being updated in one window when dragged around in another.
    BUG=34506
    TEST=See bug (make sure you specify a zip code for the weather extension when testing).
    
    Review URL: http://codereview.chromium.org/570028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/browser_actions_container.cc
+++ chrome/browser/views/browser_actions_container.cc
@@ -976,6 +976,7 @@ void BrowserActionsContainer::BrowserActionMoved(Extension* extension,
   DeleteBrowserActionViews();
   CreateBrowserActionViews();
   Layout();
+  SchedulePaint();
 }
 
 int BrowserActionsContainer::WidthOfNonIconArea() const {
