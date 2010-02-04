19645c01c87e6a2d71682759e455294821cfff40
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-04 01:52:15 +0000

    WebCore: Fix for <rdar://problem/7594212> https://bugs.webkit.org/show_bug.cgi?id=34549 - CSS counters crash at http://www.w3.org/TR/css3-content/ CrashTracer: [USER] 20 crashes in Safari at com.apple.WebCore: WebCore::CounterNode::insertAfter + 319
    Reviewed by Simon Fraser.
    
    Test: fast/css/counters/counter-before-selector-crash.html
    
    * rendering/RenderCounter.cpp: (WebCore::updateCounters): nil check.
    
    LayoutTests: Test for: <rdar://problem/7594212> 
    https://bugs.webkit.org/show_bug.cgi?id=34549 - CSS counters crash at http://www.w3.org/TR/css3-content/
    CrashTracer: [USER] 20 crashes in Safari at com.apple.WebCore: WebCore::CounterNode::insertAfter + 319
    
    Reviewed by Simon Fraser.
    
    * fast/css/counters/counter-before-selector-crash-expected.txt: Added.
    * fast/css/counters/counter-before-selector-crash.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderCounter.cpp
+++ third_party/WebKit/WebCore/rendering/RenderCounter.cpp
@@ -431,7 +431,8 @@ static void updateCounters(RenderObject* renderer)
             continue;
         if (parent)
             parent->removeChild(node, it->first.get());
-        newParent->insertAfter(node, newPreviousSibling, it->first.get());
+        if (newParent)
+            newParent->insertAfter(node, newPreviousSibling, it->first.get());
     }
 }
 
