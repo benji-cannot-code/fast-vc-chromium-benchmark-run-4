abbdba94684f7dab5a2c210b1f5112bd1044c808
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-04 11:15:34 +0000

    2010-02-04  Jedrzej Nowacki  <jedrzej.nowacki@nokia.com>
            Reviewed by Simon Hausmann.
    
            Increase test coverage for the QScriptValue.
    
            https://bugs.webkit.org/show_bug.cgi?id=34533
    
            * qt/tests/qscriptvalue/qscriptvalue.pro:
            * qt/tests/qscriptvalue/tst_qscriptvalue.cpp:
            (tst_QScriptValue::tst_QScriptValue):
            (tst_QScriptValue::~tst_QScriptValue):
            (tst_QScriptValue::dataHelper):
            (tst_QScriptValue::newRow):
            (tst_QScriptValue::testHelper):
            (tst_QScriptValue::ctor):
            * qt/tests/qscriptvalue/tst_qscriptvalue.h: Added.
            * qt/tests/qscriptvalue/tst_qscriptvalue_generated.cpp: Added.
            (tst_QScriptValue::initScriptValues):
            (tst_QScriptValue::isValid_initData):
            (tst_QScriptValue::isValid_makeData):
            (tst_QScriptValue::isValid_test):
            (tst_QScriptValue::isBool_initData):
            (tst_QScriptValue::isBool_makeData):
            (tst_QScriptValue::isBool_test):
            (tst_QScriptValue::isBoolean_initData):
            (tst_QScriptValue::isBoolean_makeData):
            (tst_QScriptValue::isBoolean_test):
            (tst_QScriptValue::isFunction_initData):
            (tst_QScriptValue::isFunction_makeData):
            (tst_QScriptValue::isFunction_test):
            (tst_QScriptValue::isNull_initData):
            (tst_QScriptValue::isNull_makeData):
            (tst_QScriptValue::isNull_test):
            (tst_QScriptValue::isString_initData):
            (tst_QScriptValue::isString_makeData):
            (tst_QScriptValue::isString_test):
            (tst_QScriptValue::isUndefined_initData):
            (tst_QScriptValue::isUndefined_makeData):
            (tst_QScriptValue::isUndefined_test):
            (tst_QScriptValue::isObject_initData):
            (tst_QScriptValue::isObject_makeData):
            (tst_QScriptValue::isObject_test):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54340 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/qt/tests/qscriptvalue/tst_qscriptvalue.cpp
+++ third_party/WebKit/JavaScriptCore/qt/tests/qscriptvalue/tst_qscriptvalue.cpp
@@ -18,35 +18,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     Boston, MA 02110-1301, USA.
 */
 
-#include "qscriptengine.h"
-#include "qscriptvalue.h"
-#include <QtTest/qtest.h>
+#include "tst_qscriptvalue.h"
+#include <QtCore/qnumeric.h>
 
-Q_DECLARE_METATYPE(QScriptValue*);
-Q_DECLARE_METATYPE(QScriptValue);
+tst_QScriptValue::tst_QScriptValue()
+    : engine(0)
+{
+}
 
-class tst_QScriptValue : public QObject {
-    Q_OBJECT
+tst_QScriptValue::~tst_QScriptValue()
+{
+    delete engine;
+}
 
-public:
-    tst_QScriptValue() {}
-    virtual ~tst_QScriptValue() {}
+void tst_QScriptValue::dataHelper(InitDataFunction init, DefineDataFunction define)
+{
+    QTest::addColumn<QString>("__expression__");
+    (this->*init)();
+    QHash<QString, QScriptValue>::const_iterator it;
+    for (it = m_values.constBegin(); it != m_values.constEnd(); ++it) {
+        m_currentExpression = it.key();
+        (this->*define)(it.key().toLatin1());
+    }
+    m_currentExpression = QString();
+}
 
-private slots:
-    void toString_data();
-    void toString();
-    void copyConstructor_data();
-    void copyConstructor();
-    void assignOperator_data();
-    void assignOperator();
-    void dataSharing();
-    void constructors_data();
-    void constructors();
-    void call();
+QTestData& tst_QScriptValue::newRow(const char* tag)
+{
+    return QTest::newRow(tag) << m_currentExpression;
+}
 
-    // copied from Qt's QtScript.
-    void ctor();
-};
+void tst_QScriptValue::testHelper(TestFunction fun)
+{
+    QFETCH(QString, __expression__);
+    QScriptValue value = m_values.value(__expression__);
+    (this->*fun)(__expression__.toLatin1(), value);
+}
+
 
 void tst_QScriptValue::ctor()
 {
@@ -54,7 +62,7 @@ void tst_QScriptValue::ctor()
     {
         QScriptValue v;
         QCOMPARE(v.isValid(), false);
-        QCOMPARE(v.engine(), (QScriptEngine *)0);
+        QCOMPARE(v.engine(), (QScriptEngine*)0);
     }
     {
         QScriptValue v(&eng, QScriptValue::UndefinedValue);
@@ -169,14 +177,14 @@ void tst_QScriptValue::ctor()
         QCOMPARE(v.isValid(), true);
         QCOMPARE(v.isUndefined(), true);
         QCOMPARE(v.isObject(), false);
-        QCOMPARE(v.engine(), (QScriptEngine *)0);
+        QCOMPARE(v.engine(), (QScriptEngine*)0);
     }
     {
         QScriptValue v(QScriptValue::NullValue);
         QCOMPARE(v.isValid(), true);
         QCOMPARE(v.isNull(), true);
         QCOMPARE(v.isObject(), false);
-        QCOMPARE(v.engine(), (QScriptEngine *)0);
+        QCOMPARE(v.engine(), (QScriptEngine*)0);
     }
     {
         QScriptValue v(false);
@@ -185,7 +193,7 @@ void tst_QScriptValue::ctor()
         QCOMPARE(v.isBool(), true);
         QCOMPARE(v.isObject(), false);
         QCOMPARE(v.toBoolean(), false);
-        QCOMPARE(v.engine(), (QScriptEngine *)0);
+        QCOMPARE(v.engine(), (QScriptEngine*)0);
     }
     {
         QScriptValue v(int(1));
@@ -193,7 +201,7 @@ void tst_QScriptValue::ctor()
         QCOMPARE(v.isNumber(), true);
         QCOMPARE(v.isObject(), false);
         QCOMPARE(v.toNumber(), 1.0);
-        QCOMPARE(v.engine(), (QScriptEngine *)0);
+        QCOMPARE(v.engine(), (QScriptEngine*)0);
     }
     {
         QScriptValue v(uint(1));
@@ -201,7 +209,7 @@ void tst_QScriptValue::ctor()
         QCOMPARE(v.isNumber(), true);
         QCOMPARE(v.isObject(), false);
         QCOMPARE(v.toNumber(), 1.0);
-        QCOMPARE(v.engine(), (QScriptEngine *)0);
+        QCOMPARE(v.engine(), (QScriptEngine*)0);
     }
     {
         QScriptValue v(1.0);
@@ -209,7 +217,7 @@ void tst_QScriptValue::ctor()
         QCOMPARE(v.isNumber(), true);
         QCOMPARE(v.isObject(), false);
         QCOMPARE(v.toNumber(), 1.0);
-        QCOMPARE(v.engine(), (QScriptEngine *)0);
+        QCOMPARE(v.engine(), (QScriptEngine*)0);
     }
     {
         QScriptValue v("ciao");
@@ -217,7 +225,7 @@ void tst_QScriptValue::ctor()
         QCOMPARE(v.isString(), true);
         QCOMPARE(v.isObject(), false);
         QCOMPARE(v.toString(), QLatin1String("ciao"));
-        QCOMPARE(v.engine(), (QScriptEngine *)0);
+        QCOMPARE(v.engine(), (QScriptEngine*)0);
     }
     {
         QScriptValue v(QString("ciao"));
@@ -225,19 +233,19 @@ void tst_QScriptValue::ctor()
         QCOMPARE(v.isString(), true);
         QCOMPARE(v.isObject(), false);
         QCOMPARE(v.toString(), QLatin1String("ciao"));
-        QCOMPARE(v.engine(), (QScriptEngine *)0);
+        QCOMPARE(v.engine(), (QScriptEngine*)0);
     }
     // copy constructor, operator=
     {
         QScriptValue v(1.0);
         QScriptValue v2(v);
         QCOMPARE(v2.strictlyEquals(v), true);
-        QCOMPARE(v2.engine(), (QScriptEngine *)0);
+        QCOMPARE(v2.engine(), (QScriptEngine*)0);
 
         QScriptValue v3(v);
         QCOMPARE(v3.strictlyEquals(v), true);
         QCOMPARE(v3.strictlyEquals(v2), true);
-        QCOMPARE(v3.engine(), (QScriptEngine *)0);
+        QCOMPARE(v3.engine(), (QScriptEngine*)0);
 
         QScriptValue v4(2.0);
         QCOMPARE(v4.strictlyEquals(v), false);
@@ -424,5 +432,5 @@ void tst_QScriptValue::call()
     QVERIFY(incr.call().isValid()); // Exception.
 }
 
+
 QTEST_MAIN(tst_QScriptValue)
-#include "tst_qscriptvalue.moc"
