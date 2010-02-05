76ed2a1394960ae32b40b35384eba91727386454
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-05 21:10:38 +0000

    GTK: Implement the new content blocking address bar bubbles on Linux.
    BUG=33314
    TEST=none
    
    Review URL: http://codereview.chromium.org/573023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38246 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_toolbar_gtk.cc
+++ chrome/browser/gtk/browser_toolbar_gtk.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -246,6 +246,10 @@ void BrowserToolbarGtk::Init(Profile* profile,
     if (actions_toolbar_->button_count() == 0)
       gtk_widget_hide(actions_toolbar_->widget());
   }
+
+  // Because the above does a recursive show all on all widgets we need to
+  // update the icon visibility to hide them.
+  location_bar_->UpdateContentBlockedIcons();
 
   SetViewIDs();
 }
