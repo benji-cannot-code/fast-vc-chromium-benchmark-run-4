9c1e9cd90d8890d616b972dc52cbbe8b77514a6f
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-05 01:01:45 +0000

    2010-02-04  Darin Adler  <darin@apple.com>
            Reviewed by David Levin.
    
            Make MathExtras.h compatible with <cmath>
            https://bugs.webkit.org/show_bug.cgi?id=34618
    
            * wtf/MathExtras.h: Include <cmath> instead of <math.h>.
            Use "using" as we do elsewhere in WTF for the four functions from <cmath>
            we want to use without the prefix. Later we could consider making the std
            explicit at call sites instead.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/MathExtras.h
+++ third_party/WebKit/JavaScriptCore/wtf/MathExtras.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2006, 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2006, 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WTF_MathExtras_h
 #define WTF_MathExtras_h
 
+#include <cmath>
 #include <float.h>
-#include <math.h>
 #include <stdlib.h>
 
 #if OS(SOLARIS)
@@ -186,5 +186,10 @@ inline float turn2deg(float t) { return t * 360.0f; }
 inline float deg2turn(float d) { return d / 360.0f; }
 inline float rad2grad(float r) { return r * 200.0f / piFloat; }
 inline float grad2rad(float g) { return g * piFloat / 200.0f; }
+
+using std::isfinite;
+using std::isinf;
+using std::isnan;
+using std::signbit;
 
 #endif // #ifndef WTF_MathExtras_h
