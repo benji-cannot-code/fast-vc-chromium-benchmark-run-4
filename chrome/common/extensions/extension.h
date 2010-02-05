47b4bd8b678737ce130de741088422d04a360b63
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-05 04:45:50 +0000

    Implement launch disposition for extension-apps.
    This change adds an --app-id command switch that signifies that the extension-app with the given id should be launched according to its configuration.
    
    It also adds parsing for app.window_type in the manifest and the behavior that when installed and a desktop shortcut is created, the --app-id switch is used rather than the --app switch.
    
    BUG=32361
    
    Review URL: http://codereview.chromium.org/573016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38184 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension.h
+++ chrome/common/extensions/extension.h
@@ -63,6 +63,11 @@ class Extension {
     EXTENSION_ICON_BITTY = 16,
   };
 
+  enum AppLaunchWindowType {
+    APP,
+    PANEL
+  };
+
   // Icon sizes used by the extension system.
   static const int kIconSizes[];
 
@@ -284,6 +289,9 @@ class Extension {
   const URLPatternList& app_extent() const { return app_extent_; }
   const GURL& app_launch_url() const { return app_launch_url_; }
   bool IsApp() const { return !app_launch_url_.is_empty(); }
+  AppLaunchWindowType app_launch_window_type() {
+    return app_launch_window_type_;
+  }
 
  private:
   // Helper method that loads a UserScript object from a
@@ -416,8 +424,10 @@ class Extension {
 
   // The URL an app should launch to.
   GURL app_launch_url_;
-
-
+  
+  // The type of window to start when the application is launched.
+  AppLaunchWindowType app_launch_window_type_;
+  
   // Runtime data:
 
   // True if the background page is ready.
