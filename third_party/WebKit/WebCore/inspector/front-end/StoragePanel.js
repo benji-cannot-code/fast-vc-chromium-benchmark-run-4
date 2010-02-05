daaba216070b22de51e60e75b4a5a3a180c8269f
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-05 19:22:43 +0000

    2010-02-04  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: group cookies by frame, show total
            cookies size, allow sorting cookie table.
    
            https://bugs.webkit.org/show_bug.cgi?id=34617
    
            * English.lproj/localizedStrings.js:
            * inspector/InspectorController.cpp:
            (WebCore::InspectorController::deleteCookie):
            * inspector/front-end/CookieItemsView.js:
            (WebInspector.CookieItemsView):
            (WebInspector.CookieItemsView.prototype.update):
            (WebInspector.CookieItemsView.prototype._updateWithCookies):
            (WebInspector.CookieItemsView.prototype._cookiesForDomain):
            (WebInspector.CookieItemsView.prototype.dataGridForCookies):
            (WebInspector.CookieItemsView.prototype._createNodes):
            (WebInspector.CookieItemsView.prototype._sortData.localeCompare):
            (WebInspector.CookieItemsView.prototype._sortData.numberCompare):
            (WebInspector.CookieItemsView.prototype._sortData.expiresCompare):
            (WebInspector.CookieItemsView.prototype._sortData):
            * inspector/front-end/StoragePanel.js:
            (WebInspector.StoragePanel.prototype.showCookies):
            (WebInspector.CookieSidebarTreeElement):
            (WebInspector.CookieSidebarTreeElement.prototype.onselect):
            (WebInspector.CookieSidebarTreeElement.prototype.get subtitle):
            (WebInspector.CookieSidebarTreeElement.prototype.set subtitle):
            * inspector/front-end/inspector.js:
            (WebInspector.updateResource):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54435 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/StoragePanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/StoragePanel.js
@@ -221,14 +221,14 @@ WebInspector.StoragePanel.prototype = {
             this.storageViewStatusBarItemsContainer.appendChild(statusBarItems[i]);
     },
 
-    showCookies: function(cookieDomain)
+    showCookies: function(treeElement, cookieDomain)
     {
         if (this.visibleView)
             this.visibleView.hide();
 
         var view = this._cookieViews[cookieDomain];
         if (!view) {
-            view = new WebInspector.CookieItemsView(cookieDomain);
+            view = new WebInspector.CookieItemsView(treeElement, cookieDomain);
             this._cookieViews[cookieDomain] = view;
         }
 
@@ -508,6 +508,7 @@ WebInspector.CookieSidebarTreeElement = function(cookieDomain)
 {
     WebInspector.SidebarTreeElement.call(this, "cookie-sidebar-tree-item", cookieDomain, "", null, false);
     this._cookieDomain = cookieDomain;
+    this._subtitle = "";
 
     this.refreshTitles();
 }
@@ -515,9 +516,9 @@ WebInspector.CookieSidebarTreeElement = function(cookieDomain)
 WebInspector.CookieSidebarTreeElement.prototype = {
     onselect: function()
     {
-        WebInspector.panels.storage.showCookies(this._cookieDomain);
+        WebInspector.panels.storage.showCookies(this, this._cookieDomain);
     },
-    
+
     get mainTitle()
     {
         return this._cookieDomain ? this._cookieDomain : WebInspector.UIString("Local Files");
@@ -530,12 +531,13 @@ WebInspector.CookieSidebarTreeElement.prototype = {
 
     get subtitle()
     {
-        return "";
+        return this._subtitle;
     },
 
     set subtitle(x)
     {
-        // Do nothing.
+        this._subtitle = x;
+        this.refreshTitles();
     }
 }
 
