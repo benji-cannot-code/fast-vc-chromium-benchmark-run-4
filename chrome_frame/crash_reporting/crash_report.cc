e6c2afd26f0d541f24f5a3e6a801500bde7aa6ec
siggi@chromium.org <siggi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-05 18:24:04 +0000

    Tweak the crash dump flags to make sure we get PEB/TEB and the unloaded module list where available.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38224 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/crash_reporting/crash_report.cc
+++ chrome_frame/crash_reporting/crash_report.cc
@@ -14,6 +14,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const wchar_t kGoogleUpdatePipeName[] = L"\\\\.\\pipe\\GoogleCrashServices\\";
 static google_breakpad::ExceptionHandler * g_breakpad = NULL;
 
+// These minidump flag combinations have been tested safe agains the
+// DbgHelp.dll version that ships with Windows XP SP2.
+const MINIDUMP_TYPE kSmallDumpType = static_cast<MINIDUMP_TYPE>(
+    MiniDumpWithProcessThreadData |  // Get PEB and TEB.
+    MiniDumpWithUnloadedModules);  // Get unloaded modules when available.
+
+const MINIDUMP_TYPE kLargerDumpType = static_cast<MINIDUMP_TYPE>(
+    MiniDumpWithProcessThreadData |  // Get PEB and TEB.
+    MiniDumpWithUnloadedModules |  // Get unloaded modules when available.
+    MiniDumpWithIndirectlyReferencedMemory);  // Get memory referenced by stack.
+
+// Large dump with all process memory.
+const MINIDUMP_TYPE kFullDumpType = static_cast<MINIDUMP_TYPE>(
+    MiniDumpWithFullMemory |  // Full memory from process.
+    MiniDumpWithProcessThreadData |  // Get PEB and TEB.
+    MiniDumpWithHandleData |  // Get all handle information.
+    MiniDumpWithUnloadedModules);  // Get unloaded modules when available.
+
 #pragma code_seg(push, ".text$va")
 static void veh_segment_start() {}
 #pragma code_seg(pop)
@@ -121,7 +139,8 @@ bool InitializeVectoredCrashReportingWithPipeName(
     return false;
   }
 
-  MINIDUMP_TYPE dump_type = full_dump ? MiniDumpWithFullMemory : MiniDumpNormal;
+  // TODO(siggi): Consider switching to kSmallerDumpType post-beta.
+  MINIDUMP_TYPE dump_type = full_dump ? kFullDumpType : kLargerDumpType;
   g_breakpad = new google_breakpad::ExceptionHandler(
       dump_path, NULL, NULL, NULL,
       google_breakpad::ExceptionHandler::HANDLER_INVALID_PARAMETER |
