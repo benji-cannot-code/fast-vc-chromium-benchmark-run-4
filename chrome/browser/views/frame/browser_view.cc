371666d89cc885fbe503195f8364cdbf66b6a225
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-05 01:34:34 +0000

    Make the side tabstrip participate in the layout of the BrowserView. Turn on the side tabstrip by default when --enable-vertical-tabs is specified, for ease of testing/debugging. Adjust glass rendering in this mode.
    http://crbug.com/34509
    TEST=none
    
    Review URL: http://codereview.chromium.org/570029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38160 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_view.cc
+++ chrome/browser/views/frame/browser_view.cc
@@ -545,6 +545,10 @@ bool BrowserView::IsTabStripVisible() const {
   return browser_->SupportsWindowFeature(Browser::FEATURE_TABSTRIP);
 }
 
+bool BrowserView::UsingSideTabs() const {
+  return SideTabStrip::Visible(browser_->profile());
+}
+
 bool BrowserView::IsOffTheRecord() const {
   return browser_->profile()->IsOffTheRecord();
 }
@@ -1596,12 +1600,9 @@ views::LayoutManager* BrowserView::CreateLayoutManager() const {
 }
 
 BaseTabStrip* BrowserView::CreateTabStrip(TabStripModel* model) {
-  BaseTabStrip* tabstrip = NULL;
-  if (SideTabStrip::Visible(browser_->profile()))
-    tabstrip = new SideTabStrip;
-  else
-    tabstrip = new TabStrip(model);
-  return tabstrip;
+  if (UsingSideTabs())
+    return new SideTabStrip;
+  return new TabStrip(model);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- chrome/browser/views/frame/glass_browser_frame_view.cc
+++ chrome/browser/views/frame/glass_browser_frame_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/browser/browser_theme_provider.h"
 #include "chrome/browser/views/frame/browser_view.h"
+#include "chrome/browser/views/tabs/side_tab_strip.h"
 #include "chrome/browser/views/tabs/tab_strip.h"
 #include "grit/app_resources.h"
 #include "grit/theme_resources.h"
@@ -82,6 +83,10 @@ GlassBrowserFrameView::~GlassBrowserFrameView() {
 
 gfx::Rect GlassBrowserFrameView::GetBoundsForTabStrip(
     BaseTabStrip* tabstrip) const {
+  if (browser_view_->UsingSideTabs()) {
+    gfx::Size ps = tabstrip->GetPreferredSize();
+    return gfx::Rect(0, 0, ps.width(), browser_view_->height());
+  }
   int minimize_button_offset = frame_->GetMinimizeButtonOffset();
   int tabstrip_x = browser_view_->ShouldShowOffTheRecordAvatar() ?
       (otr_avatar_bounds_.right() + kOTRSideSpacing) :
@@ -229,8 +234,11 @@ int GlassBrowserFrameView::NonClientTopBorderHeight() const {
   // We'd like to use FrameBorderThickness() here, but the maximized Aero glass
   // frame has a 0 frame border around most edges and a CXSIZEFRAME-thick border
   // at the top (see AeroGlassFrame::OnGetMinMaxInfo()).
+  const int kRestoredHeight = browser_view_->UsingSideTabs()
+      ? GetSystemMetrics(SM_CYCAPTION)
+      : kNonClientRestoredExtraThickness;
   return GetSystemMetrics(SM_CXSIZEFRAME) + (browser_view_->IsMaximized() ?
-      -kTabstripTopShadowThickness : kNonClientRestoredExtraThickness);
+      -kTabstripTopShadowThickness : kRestoredHeight);
 }
 
 void GlassBrowserFrameView::PaintDistributorLogo(gfx::Canvas* canvas) {
@@ -307,6 +315,11 @@ void GlassBrowserFrameView::PaintRestoredClientEdge(gfx::Canvas* canvas) {
       tp->GetBitmapNamed(IDR_CONTENT_TOP_LEFT_CORNER)->height();
 
   gfx::Rect client_area_bounds = CalculateClientAreaBounds(width(), height());
+  if (browser_view_->UsingSideTabs()) {
+    client_area_bounds.Inset(
+        GetBoundsForTabStrip(browser_view_->tabstrip()).width(), 0, 0, 0);
+  }
+
   int client_area_bottom =
       std::max(client_area_top, height() - NonClientBorderThickness());
   int client_area_height = client_area_bottom - client_area_top;
