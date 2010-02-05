dfe8f3f56892cd894a5292863768c086e184634c
cmasone@google.com <cmasone@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-05 19:19:59 +0000

    Don't need ipc_message.h any more Review URL: http://codereview.chromium.org/577025
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38229 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_manager_view.h
+++ chrome/browser/chromeos/login/login_manager_view.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_LOGIN_LOGIN_MANAGER_VIEW_H_
 
 #include <string>
-#include "chrome/browser/chromeos/login/ipc_message.h"
 #include "chrome/browser/chromeos/version_loader.h"
 #include "views/accelerator.h"
 #include "views/controls/label.h"
