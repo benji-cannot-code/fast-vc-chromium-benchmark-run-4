0d8c1c7877027ee66743426f3cdd3abc61a6a228
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-05 21:58:45 +0000

    2010-02-05  James Hawkins  <jhawkins@chromium.org>
            Reviewed by Darin Fisher.
    
            Implement WebInputElement::isActivatedSubmit().
    
            https://bugs.webkit.org/show_bug.cgi?id=34623
    
            * public/WebInputElement.h:
            * src/WebInputElement.cpp:
            (WebKit::WebInputElement::isActivatedSubmit):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebInputElement.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebInputElement.cpp
@@ -76,7 +76,12 @@ WebString WebInputElement::formControlType() const
 {
     return constUnwrap<HTMLInputElement>()->formControlType();
 }
-    
+
+bool WebInputElement::isActivatedSubmit() const
+{
+    return constUnwrap<HTMLInputElement>()->isActivatedSubmit();
+}
+
 void WebInputElement::setActivatedSubmit(bool activated)
 {
     unwrap<HTMLInputElement>()->setActivatedSubmit(activated);
