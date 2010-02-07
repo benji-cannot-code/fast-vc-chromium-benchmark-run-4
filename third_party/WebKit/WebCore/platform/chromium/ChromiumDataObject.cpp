043d5c8265ed481ff1dc259ba6c8a3c5b19af4b2
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-07 12:35:36 +0000

    2010-02-07  Jian Li  <jianli@chromium.org>
            Reviewed by Darin Fisher.
    
            [chromium] Change chromium interface to handle DownloadURL format.
            https://bugs.webkit.org/show_bug.cgi?id=34655
    
            * platform/chromium/ChromiumDataObject.cpp:
            (WebCore::ChromiumDataObject::clear):
            (WebCore::ChromiumDataObject::hasData):
            (WebCore::ChromiumDataObject::ChromiumDataObject):
            * platform/chromium/ChromiumDataObject.h:
            * platform/chromium/ClipboardChromium.cpp:
            (WebCore::ClipboardChromium::setData):
    2010-02-07  Jian Li  <jianli@chromium.org>
    
            Reviewed by Darin Fisher.
    
            [chromium] Change chromium interface to handle DownloadURL format.
            https://bugs.webkit.org/show_bug.cgi?id=34655
    
            * public/WebDragData.h:
            * src/WebDragData.cpp:
            (WebKit::WebDragData::downloadMetadata):
            (WebKit::WebDragData::setDownloadMetadata):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/ChromiumDataObject.cpp
+++ third_party/WebKit/WebCore/platform/chromium/ChromiumDataObject.cpp
@@ -39,6 +39,7 @@ void ChromiumDataObject::clear()
     url = KURL();
     urlTitle = "";
     downloadURL = KURL();
+    downloadMetadata = "";
     fileExtension = "";
     filenames.clear();
     plainText = "";
@@ -53,6 +54,7 @@ bool ChromiumDataObject::hasData() const
 {
     return !url.isEmpty()
         || !downloadURL.isEmpty()
+        || !downloadMetadata.isEmpty()
         || !fileExtension.isEmpty()
         || !filenames.isEmpty()
         || !plainText.isEmpty()
@@ -64,6 +66,7 @@ ChromiumDataObject::ChromiumDataObject(const ChromiumDataObject& other)
     : url(other.url)
     , urlTitle(other.urlTitle)
     , downloadURL(other.downloadURL)
+    , downloadMetadata(other.downloadMetadata)
     , fileExtension(other.fileExtension)
     , filenames(other.filenames)
     , plainText(other.plainText)
--- third_party/WebKit/WebCore/platform/chromium/ChromiumDataObject.h
+++ third_party/WebKit/WebCore/platform/chromium/ChromiumDataObject.h
@@ -61,6 +61,7 @@ namespace WebCore {
         String urlTitle;
 
         KURL downloadURL;
+        String downloadMetadata;
 
         String fileExtension;
         Vector<String> filenames;
