28bb2de4974e12b4dd160af57bc36d78110f1de7
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-08 21:28:31 +0000

    Update bug reference link in ExtensionApiTest.Toolstrip
    TBR=erikkay
    TEST=no code change
    BUG=30151, 35034
    
    Review URL: http://codereview.chromium.org/580015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38394 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_toolstrip_apitest.cc
+++ chrome/browser/extensions/extension_toolstrip_apitest.cc
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-// Disabled, http://crbug.com/30151.
+// Disabled, http://crbug.com/30151 (Linux and ChromeOS),
+// http://crbug.com/35034 (others).
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Toolstrip) {
   ASSERT_TRUE(RunExtensionTest("toolstrip")) << message_;
 }
