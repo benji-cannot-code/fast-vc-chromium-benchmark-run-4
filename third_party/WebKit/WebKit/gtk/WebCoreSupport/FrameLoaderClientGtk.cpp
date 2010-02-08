f2dd8ec5504173e16d9a9e78b3b0117e5c281fdf
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-08 21:48:12 +0000

    2010-02-08  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Gustavo Noronha.
    
            [GTK] Implement FrameLoaderClient::hasWebView
            https://bugs.webkit.org/show_bug.cgi?id=34682
    
            Used for sanity-check ASSERTS in the FrameLoader code.
    
            * WebCoreSupport/FrameLoaderClientGtk.cpp:
            (WebKit::FrameLoaderClient::hasWebView):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54508 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
+++ third_party/WebKit/WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
@@ -564,8 +564,7 @@ void FrameLoaderClient::setMainFrameDocumentReady(bool)
 
 bool FrameLoaderClient::hasWebView() const
 {
-    notImplemented();
-    return true;
+    return getViewFromFrame(m_frame);
 }
 
 void FrameLoaderClient::dispatchDidFinishLoad()
