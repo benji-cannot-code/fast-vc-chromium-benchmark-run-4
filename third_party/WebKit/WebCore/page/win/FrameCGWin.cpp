7e558ad36716998bcd013750415fafc215021afc
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-08 20:55:42 +0000

    2010-02-08  Dirk Schulze  <krit@webkit.org>
            Unreviewed windows build-fix.
    
            * page/win/FrameCGWin.cpp:
            (WebCore::drawRectIntoContext):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54505 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/win/FrameCGWin.cpp
+++ third_party/WebKit/WebCore/page/win/FrameCGWin.cpp
@@ -46,7 +46,7 @@ static void drawRectIntoContext(IntRect rect, FrameView* view, GraphicsContext* 
     rect.move(-offset.width(), -offset.height());
     rect = view->convertToContainingWindow(rect);
 
-    gc->concatCTM(TransformationMatrix().translate(-rect.x(), -rect.y()));
+    gc->concatCTM(AffineTransform().translate(-rect.x(), -rect.y()));
 
     view->paint(gc, rect);
 }
