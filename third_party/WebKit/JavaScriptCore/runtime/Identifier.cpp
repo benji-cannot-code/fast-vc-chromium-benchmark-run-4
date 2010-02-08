3b907e37009dfda86db5e814db8422309e3f410b
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-08 22:26:59 +0000

    Use an empty identifier instead of a null identifier for parse tokens without an identifier.
    Reviewed by Sam Weinig.
    
    This helps encapsulate the null UStringImpl within UString.
    
    * parser/Grammar.y:
    * parser/NodeConstructors.h:
    (JSC::ContinueNode::ContinueNode):
    (JSC::BreakNode::BreakNode):
    (JSC::ForInNode::ForInNode):
    * runtime/CommonIdentifiers.cpp:
    (JSC::CommonIdentifiers::CommonIdentifiers):
    * runtime/CommonIdentifiers.h:
    * runtime/FunctionPrototype.cpp:
    (JSC::FunctionPrototype::FunctionPrototype):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/Identifier.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/Identifier.cpp
@@ -124,11 +124,8 @@ struct CStringTranslator {
 
 PassRefPtr<UString::Rep> Identifier::add(JSGlobalData* globalData, const char* c)
 {
-    if (!c) {
-        UString::Rep* rep = UString::null().rep();
-        rep->hash();
-        return rep;
-    }
+    ASSERT(c);
+
     if (!c[0]) {
         UString::Rep::empty().hash();
         return &UString::Rep::empty();
