c6d764ed1a1e9b365ede2c2c1f4322f25df542e5
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-09 21:48:04 +0000

    Make SyncerThread stop polling if an AUTH_INVALID response is parsed.
    BUG=34396
    TEST=SyncerTHreadWithSyncerTest.AuthInvalid
    
    
    Review URL: http://codereview.chromium.org/596007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38513 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/syncer_thread.h
+++ chrome/browser/sync/engine/syncer_thread.h
@@ -55,6 +55,7 @@ class SyncerThread : public base::RefCountedThreadSafe<SyncerThread>,
   FRIEND_TEST(SyncerThreadWithSyncerTest, Polling);
   FRIEND_TEST(SyncerThreadWithSyncerTest, Nudge);
   FRIEND_TEST(SyncerThreadWithSyncerTest, Throttling);
+  FRIEND_TEST(SyncerThreadWithSyncerTest, AuthInvalid);
   friend class SyncerThreadWithSyncerTest;
   friend class SyncerThreadFactory;
  public:
