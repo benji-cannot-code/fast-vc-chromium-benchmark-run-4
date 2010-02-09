84cc6f5a9aeb1aa01b81ae891fd756adc5c42a09
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-09 20:54:48 +0000

    Fixes leak in cookie filter page view and clear browsing data view that lead to chrome not shutting down.
    BUG=34941
    TEST=bring up clear browsing data dialog, click on link, close
      windows, make sure no chrome processes still around.
    
    Review URL: http://codereview.chromium.org/594012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38500 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/clear_browsing_data.cc
+++ chrome/browser/views/clear_browsing_data.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util.h"
 #include "app/gfx/insets.h"
 #include "chrome/browser/browser.h"
+#include "chrome/browser/browser_window.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/search_engines/template_url_model.h"
 #include "chrome/common/pref_names.h"
@@ -385,7 +386,8 @@ void ClearBrowsingDataView::LinkActivated(views::Link* source,
                                           int event_flags) {
   Browser* browser = Browser::Create(profile_);
   browser->OpenURL(GURL(l10n_util::GetStringUTF8(IDS_FLASH_STORAGE_URL)),
-                   GURL(), NEW_WINDOW, PageTransition::LINK);
+                   GURL(), NEW_FOREGROUND_TAB, PageTransition::LINK);
+  browser->window()->Show();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- chrome/browser/views/options/cookie_filter_page_view.cc
+++ chrome/browser/views/options/cookie_filter_page_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
 #include "chrome/browser/browser.h"
+#include "chrome/browser/browser_window.h"
 #include "chrome/browser/host_content_settings_map.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/views/options/cookies_view.h"
@@ -198,5 +199,6 @@ void CookieFilterPageView::LinkActivated(views::Link* source, int event_flags) {
   // behind other windows.
   Browser* browser = Browser::Create(profile());
   browser->OpenURL(GURL(l10n_util::GetString(IDS_FLASH_STORAGE_URL)), GURL(),
-                   NEW_WINDOW, PageTransition::LINK);
+                   NEW_FOREGROUND_TAB, PageTransition::LINK);
+  browser->window()->Show();
 }
