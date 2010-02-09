755c3687067c44625604f5b7f4d0bdab48dc6330
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-09 18:22:55 +0000

    2010-02-09  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Gustavo Noronha.
    
            [GTK] Set GtkAdjustments on our FrameView when loading a page in the PageCache
            https://bugs.webkit.org/show_bug.cgi?id=34754
    
            Set the Gtk Adjustments of the FrameViews when they are restored
            from the PageCache too. Right we only do it for the newly created
            FrameViews in transitionToCommittedForNewPage, but we it also
            needs to be done in the equilavent transition method for cached
            pages.
    
            * WebCoreSupport/FrameLoaderClientGtk.cpp:
            (WebKit::postCommitFrameViewSetup):
            (WebKit::FrameLoaderClient::transitionToCommittedFromCachedFrame):
            (WebKit::FrameLoaderClient::transitionToCommittedForNewPage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54559 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
+++ third_party/WebKit/WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoaderClientGtk.h"
 
 #include "ArchiveResource.h"
+#include "CachedFrame.h"
 #include "Color.h"
 #include "DocumentLoader.h"
 #include "DocumentLoaderGtk.h"
@@ -1120,8 +1121,30 @@ void FrameLoaderClient::savePlatformDataToCachedFrame(CachedFrame*)
 {
 }
 
-void FrameLoaderClient::transitionToCommittedFromCachedFrame(CachedFrame*)
+static void postCommitFrameViewSetup(WebKitWebFrame *frame, FrameView *view)
 {
+    WebKitWebView* containingWindow = getViewFromFrame(frame);
+    WebKitWebViewPrivate* priv = WEBKIT_WEB_VIEW_GET_PRIVATE(containingWindow);
+    view->setGtkAdjustments(priv->horizontalAdjustment, priv->verticalAdjustment);
+
+    if (priv->currentMenu) {
+        GtkMenu* menu = priv->currentMenu;
+        priv->currentMenu = 0;
+
+        gtk_menu_popdown(menu);
+        g_object_unref(menu);
+    }
+}
+
+void FrameLoaderClient::transitionToCommittedFromCachedFrame(CachedFrame* cachedFrame)
+{
+    ASSERT(cachedFrame->view());
+
+    Frame* frame = core(m_frame);
+    if (frame != frame->page()->mainFrame())
+        return;
+
+    postCommitFrameViewSetup(m_frame, cachedFrame->view());
 }
 
 void FrameLoaderClient::transitionToCommittedForNewPage()
@@ -1140,16 +1163,7 @@ void FrameLoaderClient::transitionToCommittedForNewPage()
     if (frame != frame->page()->mainFrame())
         return;
 
-    WebKitWebViewPrivate* priv = WEBKIT_WEB_VIEW_GET_PRIVATE(containingWindow);
-    frame->view()->setGtkAdjustments(priv->horizontalAdjustment, priv->verticalAdjustment);
-
-    if (priv->currentMenu) {
-        GtkMenu* menu = priv->currentMenu;
-        priv->currentMenu = NULL;
-
-        gtk_menu_popdown(menu);
-        g_object_unref(menu);
-    }
+    postCommitFrameViewSetup(m_frame, frame->view());
 }
 
 }
