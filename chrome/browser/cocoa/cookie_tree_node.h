dda02a710fd5aefe6ec115be7075ed64658a6080
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-09 21:04:28 +0000

    [Mac] Add local storage nodes to the cookie manager
    * Add local storage getters to CookieTreeNode
    * XIB: Embed cookie info labels into an NSView (inside the NSBox) and add
      another for info NSView for local storage.
    * Roll GTM r280:293
    
    BUG=33068
    TEST=Chromium-->Preferences-->Under the Hood-->Show cookies... Find and click on a local storage node. Info should be displayed.
    
    Review URL: http://codereview.chromium.org/599003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38504 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/cookie_tree_node.h
+++ chrome/browser/cocoa/cookie_tree_node.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,27 +8,46 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_nsobject.h"
 #include "chrome/browser/cookies_tree_model.h"
 
+// This enum specifies the type of display node a CocoaCookieTreeNode is. If
+// this system is rewritten to not use bindings, this class should be
+// subclassed and specialized, rather than using an enum to determine type.
+enum CocoaCookieTreeNodeType {
+  // Represents grouping data for the actual data.
+  kCocoaCookieTreeNodeTypeFolder = 0,
+
+  // A cookie node.
+  kCocoaCookieTreeNodeTypeCookie = 1,
+
+  // A local storage node.
+  kCocoaCookieTreeNodeTypeLocalStorage = 2
+};
+
 // This class is used by CookiesWindowController and represents a node in the
 // cookie tree view.
 @interface CocoaCookieTreeNode : NSObject {
   scoped_nsobject<NSString> title_;
   scoped_nsobject<NSMutableArray> children_;
-  // We lazily create children, so we need to know if we are a leaf.
-  BOOL isLeaf_;
+
+  CocoaCookieTreeNodeType nodeType_;
 
   // The platform-independent model node.
   CookieTreeNode* treeNode_;  // weak
 
-  // These members are only set for true cookie nodes.
-  BOOL isCookie_;
+  // These members are only set for kCocoaCookieTreeNodeTypeCookie nodes.
   scoped_nsobject<NSString> name_;
   scoped_nsobject<NSString> content_;
-  scoped_nsobject<NSString> domain_;
   scoped_nsobject<NSString> path_;
   scoped_nsobject<NSString> sendFor_;
   // Stringifed dates.
   scoped_nsobject<NSString> created_;
   scoped_nsobject<NSString> expires_;
+
+  // These members are only set for kCocoaCookieTreeNodeTypeLocalStorage nodes.
+  scoped_nsobject<NSString> fileSize_;
+  scoped_nsobject<NSString> lastModified_;
+
+  // These members are set for both of the two specialized node types.
+  scoped_nsobject<NSString> domain_;
 }
 
 // Designated initializer.
@@ -37,20 +56,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Re-sets all the members of the node based on |treeNode_|.
 - (void)rebuild;
 
-- (BOOL)isLeaf;
-
-// Getters.
+// Common getters..
 - (NSString*)title;
+- (CocoaCookieTreeNodeType)nodeType;
+- (TreeModelNode*)treeNode;
 
 // |-mutableChildren| exists so that the CookiesTreeModelObserverBridge can
 // operate on the children. Note that this lazily creates children.
 - (NSMutableArray*)mutableChildren;
 - (NSArray*)children;
+- (BOOL)isLeaf;
 
-- (TreeModelNode*)treeNode;
-
-// Used only by cookies. Nil for non-cookie nodes.
-- (BOOL)isCookie;
+// Used only by kCocoaCookieTreeNodeTypeCookie. Nil for other types.
 - (NSString*)name;
 - (NSString*)content;
 - (NSString*)domain;
@@ -58,5 +75,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSString*)sendFor;
 - (NSString*)created;
 - (NSString*)expires;
+
+// Used by kCocoaCookieTreeNodeTypeLocalStorage nodes. Nil for other types.
+- (NSString*)fileSize;
+- (NSString*)lastModified;
 
 @end
--- chrome/browser/cocoa/cookie_tree_node.mm
+++ chrome/browser/cocoa/cookie_tree_node.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util_mac.h"
 #import "base/i18n/time_formatting.h"
 #include "base/sys_string_conversions.h"
+#include "chrome/browser/browsing_data_local_storage_helper.h"
 #include "grit/generated_resources.h"
 
 @implementation CocoaCookieTreeNode
@@ -16,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if ((self = [super init])) {
     DCHECK(node);
     treeNode_ = node;
-    isLeaf_ = (node->GetChildCount() == 0);
-
     [self rebuild];
   }
   return self;
@@ -25,11 +24,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)rebuild {
   title_.reset([base::SysWideToNSString(treeNode_->GetTitle()) retain]);
-  isCookie_ = NO;
+  children_.reset();
+  nodeType_ = kCocoaCookieTreeNodeTypeFolder;
 
   CookieTreeNode::DetailedInfo info = treeNode_->GetDetailedInfo();
-  if (info.node_type == CookieTreeNode::DetailedInfo::TYPE_COOKIE) {
-    isCookie_ = YES;
+  CookieTreeNode::DetailedInfo::NodeType nodeType = info.node_type;
+  if (nodeType == CookieTreeNode::DetailedInfo::TYPE_COOKIE) {
+    nodeType_ = kCocoaCookieTreeNodeTypeCookie;
     net::CookieMonster::CanonicalCookie cookie = info.cookie->second;
 
     name_.reset([base::SysUTF8ToNSString(cookie.Name()) retain]);
@@ -56,15 +57,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       sendFor_.reset([l10n_util::GetNSStringWithFixup(
           IDS_COOKIES_COOKIE_SENDFOR_ANY) retain]);
     }
+  } else if (nodeType == CookieTreeNode::DetailedInfo::TYPE_LOCAL_STORAGE) {
+    const BrowsingDataLocalStorageHelper::LocalStorageInfo* storageInfo =
+        info.local_storage_info;
+    nodeType_ = kCocoaCookieTreeNodeTypeLocalStorage;
+    domain_.reset([base::SysUTF8ToNSString(storageInfo->origin) retain]);
+    fileSize_.reset([base::SysWideToNSString(FormatBytes(storageInfo->size,
+        GetByteDisplayUnits(storageInfo->size), true)) retain]);
+    lastModified_.reset([base::SysWideToNSString(
+        base::TimeFormatFriendlyDateAndTime(
+            storageInfo->last_modified)) retain]);
   }
-}
-
-- (BOOL)isLeaf {
-  return isLeaf_;
 }
 
 - (NSString*)title {
   return title_.get();
+}
+
+- (CocoaCookieTreeNodeType)nodeType {
+  return nodeType_;
+}
+
+- (TreeModelNode*)treeNode {
+  return treeNode_;
 }
 
 - (NSMutableArray*)mutableChildren {
@@ -85,15 +100,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return [self mutableChildren];
 }
 
-- (TreeModelNode*)treeNode {
-  return treeNode_;
+- (BOOL)isLeaf {
+  return nodeType_ != kCocoaCookieTreeNodeTypeFolder;
+}
+
+- (NSString*)description {
+  NSString* format =
+      @"<CocoaCookieTreeNode @ %p (title=%@, nodeType=%d, childCount=%u)";
+  return [NSString stringWithFormat:format, self, [self title],
+      [self nodeType], [[self children] count]];
 }
 
 #pragma mark Cookie Accessors
-
-- (BOOL)isCookie {
-  return isCookie_;
-}
 
 - (NSString*)name {
   return name_.get();
@@ -121,6 +139,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSString*)expires {
   return expires_.get();
+}
+
+#pragma mark Local Storage Accessors
+
+- (NSString*)fileSize {
+  return fileSize_.get();
+}
+
+- (NSString*)lastModified {
+  return lastModified_.get();
 }
 
 @end
