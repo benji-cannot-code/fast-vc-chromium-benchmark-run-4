3c2fc651548add44af5ebaeff65955fd50425a13
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-09 03:38:20 +0000

    Close deleting browsing data window after deleting data.
    This behavior was disabled by debugging code introduced in r38379
    
    BUG=none
    TEST=delete browsing data on windows. The dialog should close eventually.
    R=sky
    
    Review URL: http://codereview.chromium.org/595003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38441 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/clear_browsing_data.cc
+++ chrome/browser/views/clear_browsing_data.cc
@@ -460,5 +460,5 @@ void ClearBrowsingDataView::OnBrowsingDataRemoverDone() {
   // No need to remove ourselves as an observer as BrowsingDataRemover deletes
   // itself after we return.
   remover_ = NULL;
-  //  window()->Close();
+  window()->Close();
 }
