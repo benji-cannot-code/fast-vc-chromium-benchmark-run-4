3e9755d08b2a7326877d63dc56efe6416f20e5fd
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-09 23:16:56 +0000

    Desktop Notifications for the mac
    BUG=23066
    TEST=notify_demo.html
    
    
    Review URL: http://codereview.chromium.org/548208
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38533 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/balloon_collection.cc
+++ chrome/browser/notifications/balloon_collection.cc
@@ -30,9 +30,15 @@ const int kInterBalloonMargin = 5;
 }  // namespace
 
 // static
+#if defined(OS_MACOSX)
+BalloonCollectionImpl::Layout::Placement
+    BalloonCollectionImpl::Layout::placement_ =
+        Layout::VERTICALLY_FROM_TOP_RIGHT;
+#else
 BalloonCollectionImpl::Layout::Placement
     BalloonCollectionImpl::Layout::placement_ =
         Layout::VERTICALLY_FROM_BOTTOM_RIGHT;
+#endif
 
 BalloonCollectionImpl::BalloonCollectionImpl()
     : space_change_listener_(NULL) {
