610467ce9ffffb8b550b9079c8da75c30b07d95d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-09 09:35:03 +0000

    2010-02-09  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Laszlo Gombos.
    
            [BREWMP] Fix macro redefinition error in BREWMP simulator build.
            https://bugs.webkit.org/show_bug.cgi?id=34738
    
            Remove warning: 'FAR' macro redefinition.
    
            * platform/image-decoders/jpeg/JPEGImageDecoder.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54537 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.cpp
+++ third_party/WebKit/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.cpp
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JPEGImageDecoder.h"
 #include <stdio.h>  // Needed by jpeglib.h for FILE.
 
-#if OS(WINCE)
+#if OS(WINCE) || PLATFORM(BREWMP_SIMULATOR)
 // Remove warning: 'FAR' macro redefinition
 #undef FAR
 
