971a6ce68f80f31a814cc6ec4b56a5681bd6c72d
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-09 07:53:51 +0000

    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def: * runtime/StringPrototype.cpp:
    Reviewed by NOBODY (SL/win build fixes).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54532 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/StringPrototype.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/StringPrototype.cpp
@@ -245,6 +245,7 @@ public:
     int length;
 };
 
+JSValue jsSpliceSubstringsWithSeparators(ExecState* exec, JSString* sourceVal, const UString& source, const StringRange* substringRanges, int rangeCount, const UString* separators, int separatorCount);
 JSValue jsSpliceSubstringsWithSeparators(ExecState* exec, JSString* sourceVal, const UString& source, const StringRange* substringRanges, int rangeCount, const UString* separators, int separatorCount)
 {
     if (rangeCount == 1 && separatorCount == 0) {
@@ -287,6 +288,7 @@ JSValue jsSpliceSubstringsWithSeparators(ExecState* exec, JSString* sourceVal, c
     return jsString(exec, impl);
 }
 
+JSValue jsReplaceRange(ExecState* exec, const UString& source, int rangeStart, int rangeLength, const UString& replacement);
 JSValue jsReplaceRange(ExecState* exec, const UString& source, int rangeStart, int rangeLength, const UString& replacement)
 {
     int replacementLength = replacement.size();
