2bc2c11c454ce4f333ed6e294a9412da1175ddaf
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-09 18:59:18 +0000

    Revert "Disable flaky test_shell_test"
    This reverts commit 06d65042f11f082ba49c42f8a5e9fef17cadb80a.
    
    TBR=michaeln
    BUG=35032
    
    Review URL: http://codereview.chromium.org/598002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38482 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/appcache/appcache_storage_impl_unittest.cc
+++ webkit/appcache/appcache_storage_impl_unittest.cc
@@ -982,8 +982,7 @@ TEST_F(AppCacheStorageImplTest, BasicFindMainResponseInWorkingSet) {
       &AppCacheStorageImplTest::BasicFindMainResponseInWorkingSet);
 }
 
-// Flaky crash. http://code.google.com/p/chromium/issues/detail?id=35032
-TEST_F(AppCacheStorageImplTest, DISABLED_BasicFindMainFallbackResponseInDatabase) {
+TEST_F(AppCacheStorageImplTest, BasicFindMainFallbackResponseInDatabase) {
   RunTestOnIOThread(
       &AppCacheStorageImplTest::BasicFindMainFallbackResponseInDatabase);
 }
