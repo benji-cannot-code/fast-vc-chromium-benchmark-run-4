0e98cb8523baa9bfce03beeeae995604c7212654
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-10 11:59:02 +0000

    Use derefIfNotNull() to work around WINSCW compiler forward declaration bug
    Patch by Yongjun Zhang <yongjun.zhang@nokia.com> on 2009-10-06
    Reviewed by Simon Hausmann.
    
    The compiler bug is reported at
    https://xdabug001.ext.nokia.com/bugzilla/show_bug.cgi?id=9812.
    
    The change should be reverted when the above bug is fixed in WINSCW compiler.
    
    JObjectWrapper::ref() and deref() made public accessible to derefIfNull().
    
    https://bugs.webkit.org/show_bug.cgi?id=28054
    
    * bridge/jni/jsc/JavaInstanceJSC.h:
    * bridge/jni/v8/JavaInstanceV8.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54596 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/RefPtr.h
+++ third_party/WebKit/JavaScriptCore/wtf/RefPtr.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include "AlwaysInline.h"
 #include "FastAllocBase.h"
+#include "PassRefPtr.h"
 
 namespace WTF {
 
@@ -51,13 +52,13 @@ namespace WTF {
         RefPtr(HashTableDeletedValueType) : m_ptr(hashTableDeletedValue()) { }
         bool isHashTableDeletedValue() const { return m_ptr == hashTableDeletedValue(); }
 
-        ~RefPtr() { if (T* ptr = m_ptr) ptr->deref(); }
+        ~RefPtr() { derefIfNotNull(m_ptr); }
         
         template <typename U> RefPtr(const RefPtr<U>& o) : m_ptr(o.get()) { if (T* ptr = m_ptr) ptr->ref(); }
         
         T* get() const { return m_ptr; }
         
-        void clear() { if (T* ptr = m_ptr) ptr->deref(); m_ptr = 0; }
+        void clear() { derefIfNotNull(m_ptr); m_ptr = 0; }
         PassRefPtr<T> release() { PassRefPtr<T> tmp = adoptRef(m_ptr); m_ptr = 0; return tmp; }
 
         T& operator*() const { return *m_ptr; }
