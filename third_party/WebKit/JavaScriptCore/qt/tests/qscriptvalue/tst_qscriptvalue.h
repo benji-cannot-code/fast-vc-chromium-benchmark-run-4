b764f280283da728e2bdbc0970bfa8ca9f832c82
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-10 17:55:12 +0000

    2010-02-10  Jedrzej Nowacki  <jedrzej.nowacki@nokia.com>
            Reviewed by Simon Hausmann.
    
            Fix QScriptValue::toBool.
    
            Fix ECMA compliance in the QScriptValue for values like 0, NaN and
            empty strings.
    
            [Qt] QScriptValue::toBool problem
            https://bugs.webkit.org/show_bug.cgi?id=34793
    
            * qt/api/qscriptvalue_p.h:
            (QScriptValuePrivate::toBool):
            * qt/tests/qscriptvalue/tst_qscriptvalue.h:
            * qt/tests/qscriptvalue/tst_qscriptvalue_generated.cpp:
            (tst_QScriptValue::toBool_initData):
            (tst_QScriptValue::toBool_makeData):
            (tst_QScriptValue::toBool_test):
            (tst_QScriptValue::toBoolean_initData):
            (tst_QScriptValue::toBoolean_makeData):
            (tst_QScriptValue::toBoolean_test):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54605 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/qt/tests/qscriptvalue/tst_qscriptvalue.h
+++ third_party/WebKit/JavaScriptCore/qt/tests/qscriptvalue/tst_qscriptvalue.h
@@ -80,6 +80,12 @@ private slots:
     void toNumber_data();
     void toNumber();
 
+    void toBool_data();
+    void toBool();
+
+    void toBoolean_data();
+    void toBoolean();
+
 private:
     typedef void (tst_QScriptValue::*InitDataFunction)();
     typedef void (tst_QScriptValue::*DefineDataFunction)(const char*);
@@ -132,6 +138,14 @@ private:
     void toNumber_initData();
     void toNumber_makeData(const char*);
     void toNumber_test(const char*, const QScriptValue&);
+
+    void toBool_initData();
+    void toBool_makeData(const char*);
+    void toBool_test(const char*, const QScriptValue&);
+
+    void toBoolean_initData();
+    void toBoolean_makeData(const char*);
+    void toBoolean_test(const char*, const QScriptValue&);
 
 private:
     QScriptEngine* engine;
