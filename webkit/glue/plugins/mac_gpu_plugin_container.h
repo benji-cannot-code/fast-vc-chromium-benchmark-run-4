7dc3c901acab7ebbaab9e05229fec1d177256a3b
kbr@google.com <kbr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-10 00:00:40 +0000

    Fixed clipping of GPU plugin instances on Mac OS X against the browser window's scrollbars, etc.
    BUG=none
    TEST=none (ran Pepper test plugin, resized and scrolled window)
    
    Review URL: http://codereview.chromium.org/595011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38546 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/mac_gpu_plugin_container.h
+++ webkit/glue/plugins/mac_gpu_plugin_container.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "app/gfx/native_widget_types.h"
 #include "base/basictypes.h"
+#include "base/gfx/rect.h"
 
 namespace webkit_glue {
 struct WebPluginGeometry;
@@ -68,8 +69,6 @@ class MacGPUPluginContainer {
   // will work on Leopard.
 
   // The x and y coordinates of the plugin window on the web page.
-  // TODO(kbr): see whether additional clipping information is
-  // necessary.
   int x_;
   int y_;
 
@@ -84,6 +83,9 @@ class MacGPUPluginContainer {
   // The width and height of the surface.
   int32 width_;
   int32 height_;
+
+  // The clip rectangle, relative to the (x_, y_) origin.
+  gfx::Rect clipRect_;
 
   // The "live" OpenGL texture referring to this IOSurfaceRef. Note
   // that per the CGLTexImageIOSurface2D API we do not need to
