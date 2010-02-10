2c2ac320ef1e9938ef0761bd84eb38b40465a9bb
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-10 00:09:01 +0000

    Changed CreateWorker to coalesce any matching queued shared workers when a shared worker is started.
    Added tests for worker close and queued shared worker cases.
    BUG=29998
    TEST=new UI tests added.
    
    Review URL: http://codereview.chromium.org/580007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38549 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/workers/worker_common.js
+++ chrome/test/data/workers/worker_common.js
@@ -1,20 +1,29 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Track the number of clients for this worker - tests can use this to ensure
+// that shared workers are actually shared, not distinct.
+var num_clients = 0;
+
 if (!self.postMessage) {
   // This is a shared worker - mimic dedicated worker APIs
   onconnect = function(event) {
+    num_clients++;
     event.ports[0].onmessage = function(e) {
+      self.postMessage = function(msg) {
+        event.ports[0].postMessage(msg);
+      };
       self.onmessage(e);
     };
-    self.postMessage = function(msg, ports) {
-      event.ports[0].postMessage(msg, ports);
-    };
   };
+} else {
+  num_clients++;
 }
 onmessage = function(evt) {
   if (evt.data == "ping")
     postMessage("pong");
   else if (evt.data == "auth")
     importScripts("/auth-basic");
+  else if (evt.data == "close")
+    close();
   else if (/eval.+/.test(evt.data)) {
     try {
       postMessage(eval(evt.data.substr(5)));
