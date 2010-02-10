a026c65828fe33ba580fae8c768fb3ffdffe13e9
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-10 22:01:38 +0000

    Attempt to fix the Linux Views and ChromiumOS builds following r38658.
    chrome/browser/views/tab_contents/render_view_context_menu_win.cc appears to
    be a views file, not a Windows file, so why is it named with _win.cc?  This is
    a speculative change.
    
    BUG=none
    TEST=Linux Builder (Views dbg) and Linux Builder (ChromiumOS) compile steps
         turn green
    Review URL: http://codereview.chromium.org/600049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38672 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tab_contents/render_view_context_menu_gtk.h
+++ chrome/browser/views/tab_contents/render_view_context_menu_gtk.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_VIEWS_TAB_CONTENTS_RENDER_VIEW_CONTEXT_MENU_H_
-#define CHROME_BROWSER_VIEWS_TAB_CONTENTS_RENDER_VIEW_CONTEXT_MENU_H_
+#ifndef CHROME_BROWSER_VIEWS_TAB_CONTENTS_RENDER_VIEW_CONTEXT_MENU_GTK_H_
+#define CHROME_BROWSER_VIEWS_TAB_CONTENTS_RENDER_VIEW_CONTEXT_MENU_GTK_H_
 
 #include "app/menus/simple_menu_model.h"
 #include "base/scoped_ptr.h"
@@ -12,13 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/render_view_context_menu.h"
 #include "views/controls/menu/menu_2.h"
 
-class RenderViewContextMenuWin : public RenderViewContextMenu,
+class RenderViewContextMenuGtk : public RenderViewContextMenu,
                                  public menus::SimpleMenuModel::Delegate {
  public:
-  RenderViewContextMenuWin(TabContents* tab_contents,
+  RenderViewContextMenuGtk(TabContents* tab_contents,
                            const ContextMenuParams& params);
 
-  virtual ~RenderViewContextMenuWin();
+  virtual ~RenderViewContextMenuGtk();
 
   void RunMenuAt(int x, int y);
 
@@ -62,4 +62,4 @@ class RenderViewContextMenuWin : public RenderViewContextMenu,
   ScopedVector<menus::SimpleMenuModel> submenu_models_;
 };
 
-#endif  // CHROME_BROWSER_VIEWS_TAB_CONTENTS_RENDER_VIEW_CONTEXT_MENU_H_
+#endif  // CHROME_BROWSER_VIEWS_TAB_CONTENTS_RENDER_VIEW_CONTEXT_MENU_GTK_H_
