713adff608a93ef219692d6ef891ced135457be6
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-10 21:05:55 +0000

    Optimize ImageBuffer::toDataURL's CG implementation
    There was some unused code that could have been removed in r31830 when
    this function was changed not to flip the CG image anymore.
    
    Fixes <http://webkit.org/b/34808> ImageBuffer::toDataURL allocates
    unnecessary memory under CoreGraphics
    
    Reviewed by Sam Weinig.
    
    * platform/graphics/cg/ImageBufferCG.cpp:
    (WebCore::ImageBuffer::toDataURL): Don't allocate an unused CGImageRef
    and buffer, and switch to the new overload of base64Encode that
    doesn't require us to copy all the image data into a Vector first.
    
    * platform/text/Base64.cpp:
    (WebCore::base64Encode):
    * platform/text/Base64.h:
    Added an overload that takes a raw data pointer and length, just like
    we have for base64Decode. The overload that takes a Vector as input
    just calls through to the new overload.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54616 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/Base64.h
+++ third_party/WebKit/WebCore/platform/text/Base64.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 void base64Encode(const Vector<char>&, Vector<char>&, bool insertLFs = false);
+void base64Encode(const char*, unsigned, Vector<char>&, bool insertLFs = false);
 
 // this decoder is not general purpose - it returns an error if it encounters a linefeed, as needed for window.atob
 bool base64Decode(const Vector<char>&, Vector<char>&);
