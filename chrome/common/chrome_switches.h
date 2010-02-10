602c167568f373b364cb980d10f77a930e403b6b
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-10 07:22:40 +0000

    Add option to suppress HTTP Referer header.
    BUG=none
    TEST=start chrome and run tcpdump -A. Should be contain any referer header.
    
    Review URL: http://codereview.chromium.org/600008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38587 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -135,6 +135,7 @@ extern const char kNoEvents[];
 extern const char kNoFirstRun[];
 extern const char kNoJsRandomness[];
 extern const char kNoProxyServer[];
+extern const char kNoReferrers[];
 extern const char kNoSandbox[];
 extern const char kOmniBoxPopupCount[];
 extern const char kOpenInNewWindow[];
