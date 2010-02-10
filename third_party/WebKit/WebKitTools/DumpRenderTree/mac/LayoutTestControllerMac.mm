00786f8f2a7bc9b4ebbf6996e349d0a8d0de4041
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-10 23:23:06 +0000

    2010-02-10  Jesus Sanchez-Palencia  <jesus.palencia@openbossa.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            Support frameset flattening
            https://bugs.webkit.org/show_bug.cgi?id=32717
    
            Add expected results of the FrameSet Flattening tests.
    
            * platform/mac/fast/frames/flattening/frameset-flattening-advanced-expected.txt: Added.
            * platform/mac/fast/frames/flattening/frameset-flattening-grid-expected.txt: Added.
            * platform/mac/fast/frames/flattening/frameset-flattening-simple-expected.txt: Added.
            * platform/mac/fast/frames/flattening/frameset-flattening-subframe-resize-expected.txt: Added.
            * platform/mac/fast/frames/flattening/frameset-flattening-subframesets-expected.txt: Added.
            * platform/mac/Skipped:
    2010-02-10  Jesus Sanchez-Palencia  <jesus.palencia@openbossa.org>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            Support frameset flattening
            https://bugs.webkit.org/show_bug.cgi?id=32717
    
            Add FrameSet Flattening support to Mac DRT.
    
            * DumpRenderTree/LayoutTestController.cpp:
            (setFrameSetFlatteningEnabledCallback):
            (LayoutTestController::staticFunctions):
            * DumpRenderTree/LayoutTestController.h:
            * DumpRenderTree/mac/DumpRenderTree.mm:
            (resetDefaultsToConsistentValues):
            * DumpRenderTree/mac/LayoutTestControllerMac.mm:
            (LayoutTestController::setFrameSetFlatteningEnabled):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/LayoutTestControllerMac.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/LayoutTestControllerMac.mm
@@ -325,6 +325,11 @@ void LayoutTestController::setXSSAuditorEnabled(bool enabled)
     [[[mainFrame webView] preferences] setXSSAuditorEnabled:enabled];
 }
 
+void LayoutTestController::setFrameSetFlatteningEnabled(bool enabled)
+{
+    [[[mainFrame webView] preferences] setFrameSetFlatteningEnabled:enabled];
+}
+
 void LayoutTestController::setAllowUniversalAccessFromFileURLs(bool enabled)
 {
     [[[mainFrame webView] preferences] setAllowUniversalAccessFromFileURLs:enabled];
