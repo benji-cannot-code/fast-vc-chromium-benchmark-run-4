5fe45ef8decc317c596f0ffe0b04d218c737fceb
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-10 16:51:02 +0000

    Remove unnecessary #include of shfolder.h
    shfolder.h has been deprecated in newer versions of the Windows SDK.
    
    Fixes <http://webkit.org/b/34803> WebPreferences.cpp fails to compile
    under VS2010 RC due to #include of shfolder.h.
    
    Reviewed by Darin Adler.
    
    * WebPreferences.cpp: Removed #include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54604 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebPreferences.cpp
+++ third_party/WebKit/WebKit/win/WebPreferences.cpp
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <CoreFoundation/CoreFoundation.h>
 #include <limits>
 #include <shlobj.h>
-#include <shfolder.h>
 #include <tchar.h>
 #include <wtf/HashMap.h>
 #include <wtf/OwnArrayPtr.h>
