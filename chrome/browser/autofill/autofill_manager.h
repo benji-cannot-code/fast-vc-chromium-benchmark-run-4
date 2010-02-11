4f98f7242433e1f2921f11fa8de0fb4d61d2df6f
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-11 19:28:56 +0000

    Revert 38479 - Send forms seen in the renderer to the AutoFillManager.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/584009
    
    TBR=jhawkins@chromium.org
    Review URL: http://codereview.chromium.org/597045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38797 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager.h
+++ chrome/browser/autofill/autofill_manager.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/scoped_ptr.h"
-#include "base/scoped_vector.h"
 #include "chrome/browser/autofill/autofill_dialog.h"
 #include "chrome/browser/autofill/personal_data_manager.h"
 #include "chrome/browser/renderer_host/render_view_host_delegate.h"
@@ -40,8 +39,6 @@ class AutoFillManager : public RenderViewHostDelegate::AutoFill,
   // RenderViewHostDelegate::AutoFill implementation:
   virtual void FormFieldValuesSubmitted(
       const webkit_glue::FormFieldValues& form);
-  virtual void FormsSeen(
-      const std::vector<webkit_glue::FormFieldValues>& forms);
 
   // AutoFillDialogObserver implementation:
   virtual void OnAutoFillDialogApply(
@@ -75,11 +72,11 @@ class AutoFillManager : public RenderViewHostDelegate::AutoFill,
   TabContents* tab_contents_;
 
   // The personal data manager, used to save and load personal data to/from the
-  // web database.  This may be NULL if we're OTR.
+  // web database.
   PersonalDataManager* personal_data_;
 
   // Our copy of the form data.
-  ScopedVector<FormStructure> form_structures_;
+  std::vector<FormStructure*> form_structures_;
   scoped_ptr<FormStructure> upload_form_structure_;
 
   // The infobar that asks for permission to store form information.
