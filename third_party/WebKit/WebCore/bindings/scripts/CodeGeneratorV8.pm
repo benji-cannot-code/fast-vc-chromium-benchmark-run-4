6162996ffbdfa00c1ca4639627da958f5be594cf
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-11 18:27:49 +0000

    Fixes CodeGeneratorV8.pm to include the header for the indexer type when using setCollectionIndexedGetter() https://bugs.webkit.org/show_bug.cgi?id=34836
    setCollectionIndexedGetter() uses getIndexedPropertyOfCollection(),
    which in turn uses toV8() with the indexer type, which is declared in
    the header for the indexer type.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -1326,6 +1326,8 @@ END
             push(@implContent, <<END);
   setCollectionIndexedGetter<${interfaceName}, ${indexerType}>(desc, V8ClassIndex::${indexerClassIndex});
 END
+            # Include the header for this indexer type, because setCollectionIndexedGetter() requires toV8() for this type.
+            $implIncludes{"V8${indexerType}.h"} = 1;
         }
 
         return;
