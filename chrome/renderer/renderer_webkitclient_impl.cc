5520088813102aa559b1a8afdbff9002facaa8ac
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-11 16:41:57 +0000

    Change ViewHostMsg_SetCookie to be routed so we can show the blocked cookie notification.
    We get the RenderView identifier by looking at the current WebFrame.
    
    R=jorlow
    BUG=34573
    TEST=none
    
    Review URL: http://codereview.chromium.org/596050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38777 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/renderer_webkitclient_impl.cc
+++ chrome/renderer/renderer_webkitclient_impl.cc
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.  Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #include "chrome/renderer/renderer_webkitclient_impl.h"
 
@@ -22,10 +22,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/plugin/npobject_util.h"
 #include "chrome/renderer/net/render_dns_master.h"
 #include "chrome/renderer/render_thread.h"
+#include "chrome/renderer/render_view.h"
 #include "chrome/renderer/renderer_webstoragenamespace_impl.h"
 #include "chrome/renderer/visitedlink_slave.h"
 #include "googleurl/src/gurl.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebCookie.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebStorageEventDispatcher.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebString.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebURL.h"
@@ -44,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using WebKit::WebApplicationCacheHost;
 using WebKit::WebApplicationCacheHostClient;
 using WebKit::WebCookie;
+using WebKit::WebFrame;
 using WebKit::WebKitClient;
 using WebKit::WebStorageArea;
 using WebKit::WebStorageEventDispatcher;
@@ -112,10 +115,18 @@ RendererWebKitClientImpl::createMessagePortChannel() {
 void RendererWebKitClientImpl::setCookies(const WebURL& url,
                                           const WebURL& first_party_for_cookies,
                                           const WebString& value) {
+  // TODO(darin): Modify WebKit to pass the WebFrame.
+  RenderView* view =
+      RenderView::FromWebView(WebFrame::frameForCurrentContext()->view());
+  DCHECK(view);
+
   std::string value_utf8;
   UTF16ToUTF8(value.data(), value.length(), &value_utf8);
   RenderThread::current()->Send(
-      new ViewHostMsg_SetCookie(url, first_party_for_cookies, value_utf8));
+      new ViewHostMsg_SetCookie(view->routing_id(),
+                                url,
+                                first_party_for_cookies,
+                                value_utf8));
 }
 
 WebString RendererWebKitClientImpl::cookies(
