f8797dc0053de1f7e4ba65d59e68f8d42995b633
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-11 21:04:19 +0000

    Build fix for Windows
    git-svn-id: svn://svn.chromium.org/blink/trunk@54670 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -4659,7 +4659,7 @@ HRESULT WebView::notifyPreferencesChanged(IWebNotification* notification)
 #endif
 
 #if ENABLE(3D_CANVAS)
-    settings->setExperimentalWebGLEnabled(true);
+    settings->setWebGLEnabled(true);
 #endif  // ENABLE(3D_CANVAS)
 
     if (!m_closeWindowTimer.isActive())
