4e06aeb473308d9d4d09e716480a9b0c0e0530d4
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-11 23:38:05 +0000

    2010-02-11  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            run-chromium-webkit-tests fails random pixel tests on Mac
            https://bugs.webkit.org/show_bug.cgi?id=34862
    
            This is due to the fact that the Mac port has an
            invalid path to the image diff tool.  Currently it points
            to image_diff even though ImageDiff would be correct.  We
            can't change it to the right path yet without causing the
            script to hang.  ImageDiff expects to be long-running and
            be passed image data over stdin.  image_diff (chromium's fork)
            expects to be passed command line arguments.
            This fix works around the random failures by disabling pixel
            tests on mac and logging if the user was trying to run with pixel
            tests enabled.
    
            * Scripts/webkitpy/layout_tests/port/mac.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/mac.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/mac.py
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """WebKit Mac implementation of the Port interface."""
 
 import fcntl
+import logging
 import os
 import pdb
 import platform
@@ -273,8 +274,12 @@ class MacDriver(base.Driver):
             cmd += self._options.wrapper.split()
         # FIXME: Using arch here masks any possible file-not-found errors from a non-existant driver executable.
         cmd += ['arch', '-i386', port._path_to_driver(), '-']
+
+        # FIXME: This is a hack around our lack of ImageDiff support for now.
         if not self._port._options.no_pixel_tests:
-            cmd.append('--pixel-tests')
+            logging.warn("This port does not yet support pixel tests.")
+            self._port._options.no_pixel_tests = True
+            #cmd.append('--pixel-tests')
 
         #if driver_options:
         #    cmd += driver_options
