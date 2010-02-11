140e830ba4c2d4b3f3b6854b82c663bc81f1e4b2
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-11 00:11:07 +0000

    Make context menu in Browser Action overflow menu do something.
    In a previous reorg change the lifetime of the context menu
    changed, causing it to get deleted before executing its 
    actions. This change makes the browser action container
    own the context menu, lazily create it and reuse it for
    every browser action from that point on.
    
    Also delaying a bit when we clear the drop indicator
    (it shouldn't go away until the dropdown menu appears).
    
    BUG=34796
    TEST=Context menu off of the Browser Action Container
    overflow menu should now work. Dragging and dropping
    between the container and the overflow menu should still
    work (and reordering within both as well).
    
    Review URL: http://codereview.chromium.org/601033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38712 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/extensions/extension_action_context_menu.cc
+++ chrome/browser/views/extensions/extension_action_context_menu.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -24,6 +24,7 @@ void ExtensionActionContextMenu::Run(Extension* extension,
   context_menu_contents_.reset(
       new ExtensionActionContextMenuModel(extension));
   context_menu_menu_.reset(new views::Menu2(context_menu_contents_.get()));
+  // This call blocks until the menu is dismissed or something is selected.
   context_menu_menu_->RunContextMenuAt(point);
 }
 
