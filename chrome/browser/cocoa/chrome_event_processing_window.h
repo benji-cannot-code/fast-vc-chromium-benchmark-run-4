cff7567e9252c779f83e15368e9037f14200db2c
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-11 21:11:15 +0000

    Mac: (for fullscreen mode) make key redispatch handle "misdirected" events.
    Moving things between windows can lead to the key redispatch getting an event
    for the wrong window. This makes the redispatcher synthesize new events in that
    case.
    
    BUG=31638
    TEST=Compiles/unit tests. Enable fullscreen mode (in code), and switch back and forth between regular and fullscreen mode (esp. while loading slow web pages) like mad.
    
    Review URL: http://codereview.chromium.org/594025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38812 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/chrome_event_processing_window.h
+++ chrome/browser/cocoa/chrome_event_processing_window.h
@@ -19,12 +19,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   BOOL eventHandled_;
 }
 
-// Sends an event to |NSApp sendEvent:|, but also makes sure that it's not
+// Sends a key event to |NSApp sendEvent:|, but also makes sure that it's not
 // short-circuited to the RWHV. This is used to send keyboard events to the menu
 // and the cmd-` handler if a keyboard event comes back unhandled from the
-// renderer.
+// renderer. The event must be of type |NSKeyDown|, |NSKeyUp|, or
+// |NSFlagsChanged|.
 // Returns |YES| if |event| has been handled.
-- (BOOL)redispatchEvent:(NSEvent*)event;
+- (BOOL)redispatchKeyEvent:(NSEvent*)event;
 
 // See global_keyboard_shortcuts_mac.h for details on the next two functions.
 
