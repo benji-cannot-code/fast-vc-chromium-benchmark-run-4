fc3756bf2ae791550a2f405d6371010257a63495
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-11 21:28:52 +0000

    JavaScriptCore: Added an SPI for asking about all the different live objects on the heap. Useful for memory debugging.
    Reviewed by Oliver Hunt.
    
    * JavaScriptCore.exp: Export the new SPI.
    
    * runtime/Collector.cpp:
    (JSC::typeName): Use a little capitalization. Don't crash in the case of
    a non-object cell, since it might just be an uninitialized cell.
    
    (JSC::Heap::objectTypeCounts): The new SPI.
    
    * runtime/Collector.h:
    * runtime/CollectorHeapIterator.h:
    (JSC::CollectorHeapIterator::advance):
    (JSC::LiveObjectIterator::operator++):
    (JSC::DeadObjectIterator::operator++):
    (JSC::ObjectIterator::operator++): Made 2 tweaks to these iterators:
    (1) Skip the last cell in the block, since it's a dummy sentinel, and
    we don't want it to confuse the object count; (2) Fixed a logic error
    in LiveObjectIterator that could cause it to iterate dead objects if
    m_block were equal to m_heap.nextBlock and m_cell were less than
    m_heap.nextCell. No test for this since I can't think of a way that this
    could make WebKit behave badly.
    
    WebKit/mac: Exported some new JavaScript heap introspection.
    
    Reviewed by Oliver Hunt.
    
    * Misc/WebCoreStatistics.h:
    * Misc/WebCoreStatistics.mm:
    (+[WebCoreStatistics javaScriptObjectTypeCounts]): Just like
    javaScriptProtectedObjectTypeCounts, except this function enumerates all
    live objects, not just protected objects.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54672 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/CollectorHeapIterator.h
+++ third_party/WebKit/JavaScriptCore/runtime/CollectorHeapIterator.h
@@ -39,7 +39,7 @@ namespace JSC {
     
     protected:
         CollectorHeapIterator(CollectorHeap&, size_t startBlock, size_t startCell);
-        void advance(size_t cellsPerBlock);
+        void advance(size_t max);
 
         CollectorHeap& m_heap;
         size_t m_block;
@@ -81,10 +81,12 @@ namespace JSC {
         return reinterpret_cast<JSCell*>(m_heap.blocks[m_block]->cells + m_cell);
     }
     
-    inline void CollectorHeapIterator::advance(size_t cellsPerBlock)
+    // Iterators advance up to the next-to-last -- and not the last -- cell in a
+    // block, since the last cell is a dummy sentinel.
+    inline void CollectorHeapIterator::advance(size_t max)
     {
         ++m_cell;
-        if (m_cell == cellsPerBlock) {
+        if (m_cell == max) {
             m_cell = 0;
             ++m_block;
         }
@@ -98,14 +100,12 @@ namespace JSC {
 
     inline LiveObjectIterator& LiveObjectIterator::operator++()
     {
-        if (m_block < m_heap.nextBlock || m_cell < m_heap.nextCell) {
-            advance(HeapConstants::cellsPerBlock);
+        advance(HeapConstants::cellsPerBlock - 1);
+        if (m_block < m_heap.nextBlock || (m_block == m_heap.nextBlock && m_cell < m_heap.nextCell))
             return *this;
-        }
 
-        do {
-            advance(HeapConstants::cellsPerBlock);
-        } while (m_block < m_heap.usedBlocks && !m_heap.blocks[m_block]->marked.get(m_cell));
+        while (m_block < m_heap.usedBlocks && !m_heap.blocks[m_block]->marked.get(m_cell))
+            advance(HeapConstants::cellsPerBlock - 1);
         return *this;
     }
 
@@ -118,7 +118,7 @@ namespace JSC {
     inline DeadObjectIterator& DeadObjectIterator::operator++()
     {
         do {
-            advance(HeapConstants::cellsPerBlock);
+            advance(HeapConstants::cellsPerBlock - 1);
             ASSERT(m_block > m_heap.nextBlock || (m_block == m_heap.nextBlock && m_cell >= m_heap.nextCell));
         } while (m_block < m_heap.usedBlocks && m_heap.blocks[m_block]->marked.get(m_cell));
         return *this;
@@ -132,7 +132,7 @@ namespace JSC {
 
     inline ObjectIterator& ObjectIterator::operator++()
     {
-        advance(HeapConstants::cellsPerBlock);
+        advance(HeapConstants::cellsPerBlock - 1);
         return *this;
     }
 
--- third_party/WebKit/WebKit/mac/Misc/WebCoreStatistics.h
+++ third_party/WebKit/WebKit/mac/Misc/WebCoreStatistics.h
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (size_t)javaScriptProtectedObjectsCount;
 + (size_t)javaScriptProtectedGlobalObjectsCount;
 + (NSCountedSet *)javaScriptProtectedObjectTypeCounts;
++ (NSCountedSet *)javaScriptObjectTypeCounts;
 
 + (void)garbageCollectJavaScriptObjects;
 + (void)garbageCollectJavaScriptObjectsOnAlternateThreadForDebugging:(BOOL)waitUntilDone;
