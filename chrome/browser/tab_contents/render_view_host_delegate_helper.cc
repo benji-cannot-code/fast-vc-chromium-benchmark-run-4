b4a5eb4b91fdf21045bb913d1254d9cf011a6dc7
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-12 19:43:32 +0000

    Remove --disable-images and --disable-javascript now that we have UI that does that.
    This doesn't remove --disable-plugins or --disable-popup-blocking as various different test executables pass those and I'm not sure there's a way for them to set default pref values instead.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/593064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38914 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_host_delegate_helper.cc
+++ chrome/browser/tab_contents/render_view_host_delegate_helper.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -169,7 +169,6 @@ WebPreferences RenderViewHostDelegateHelper::GetWebkitPrefs(
         !command_line.HasSwitch(switches::kDisableJava) &&
         prefs->GetBoolean(prefs::kWebKitJavaEnabled);
     web_prefs.loads_images_automatically =
-        !command_line.HasSwitch(switches::kDisableImages) &&
         prefs->GetBoolean(prefs::kWebKitLoadsImagesAutomatically);
     web_prefs.uses_page_cache =
         command_line.HasSwitch(switches::kEnableFastback);
