b38393d7f5df70c3a9c5bedc4dac24819859d709
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-12 13:16:09 +0000

    2010-02-12  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: restore highlighting animation, bring back
            keyboard navigation, beautify search results.
    
            https://bugs.webkit.org/show_bug.cgi?id=34853
    
            * inspector/front-end/TextViewer.js:
            (WebInspector.TextViewer):
            (WebInspector.TextViewer.prototype.highlightLine):
            * inspector/front-end/textViewer.css:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54720 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/TextViewer.js
+++ third_party/WebKit/WebCore/inspector/front-end/TextViewer.js
@@ -37,6 +37,7 @@ WebInspector.TextViewer = function(textModel, platform, url)
 
     this.element = document.createElement("div");
     this.element.className = "text-editor monospace";
+    this.element.tabIndex = 0;
 
     this.element.addEventListener("scroll", this._scroll.bind(this), false);
 
@@ -101,7 +102,6 @@ WebInspector.TextViewer.prototype = {
 
     highlightLine: function(lineNumber)
     {
-        // FIXME: restore animation.
         if (typeof this._highlightedLine === "number") {
             var chunk = this._makeLineAChunk(this._highlightedLine);
             chunk.removeDecoration("webkit-highlighted-line");
