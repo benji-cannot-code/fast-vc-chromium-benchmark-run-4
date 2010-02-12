36b47babd232af062d895a1d81ac45133cfefa11
amit@chromium.org <amit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-12 18:19:07 +0000

    First batch of context menu tests
    Refactored various methods to send keyboard and mouse
    input. Fixed the context menu focus issue on IE7.
    Improved existing tests to make them less flaky and
    added 3 new tests for context menu items.
    
    
    BUG=34673
    TEST=new tests added
    
    
    Review URL: http://codereview.chromium.org/604014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38905 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/chrome_frame_test_utils.h
+++ chrome_frame/test/chrome_frame_test_utils.h
@@ -23,35 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_variant_win.h"
 
 #include "chrome_frame/test_utils.h"
+#include "chrome_frame/test/simulate_input.h"
 // Include without path to make GYP build see it.
 #include "chrome_tab.h"  // NOLINT
 
 namespace chrome_frame_test {
 
-bool IsTopLevelWindow(HWND window);
 int CloseVisibleWindowsOnAllThreads(HANDLE process);
-bool ForceSetForegroundWindow(HWND window);
-bool EnsureProcessInForeground(base::ProcessId process_id);
-
-// Iterates through all the characters in the string and simulates
-// keyboard input.  The input goes to the currently active application.
-bool SendString(const wchar_t* s);
-
-// Sends a virtual key such as VK_TAB, VK_RETURN or a character that has been
-// translated to a virtual key.
-// The extended flag indicates if this is an extended key
-void SendVirtualKey(int16 key, bool extended);
-
-// Translates a single char to a virtual key and calls SendVirtualKey.
-void SendChar(char c);
-
-// Sends an ascii string, char by char (calls SendChar for each).
-void SendString(const char* s);
-
-// Sends a keystroke to the currently active application with optional
-// modifiers set.
-bool SendMnemonic(WORD mnemonic_char, bool shift_pressed, bool control_pressed,
-                  bool alt_pressed);
 
 base::ProcessHandle LaunchFirefox(const std::wstring& url);
 base::ProcessHandle LaunchOpera(const std::wstring& url);
@@ -72,26 +50,6 @@ extern const wchar_t kFirefoxImageName[];
 extern const wchar_t kOperaImageName[];
 extern const wchar_t kSafariImageName[];
 extern const wchar_t kChromeImageName[];
-
-// Displays the chrome frame context menu by posting mouse move messages to
-// Chrome
-void ShowChromeFrameContextMenu();
-
-// Sends keyboard messages to the chrome frame context menu to select the About
-// Chrome frame option.
-void SelectAboutChromeFrame();
-
-// Returns a handle to the chrome frame render widget child window.
-// Returns NULL on failure.
-HWND GetChromeRendererWindow();
-
-// Sends the specified input to the window passed in.
-void SendInputToWindow(HWND window, const std::string& input_string);
-
-// Helper function to set keyboard focus to a window. This is achieved by
-// sending a mouse move followed by a mouse down/mouse up combination to the
-// window.
-void SetKeyboardFocusToWindow(HWND window, int x, int y);
 
 // Temporarily impersonate the current thread to low integrity for the lifetime
 // of the object. Destructor will automatically revert integrity level.
@@ -203,7 +161,14 @@ class WebBrowserEventSink
 
   // Send keyboard input to the renderer window hosted in chrome using
   // SendInput API
-  void SendInputToChrome(const std::string& input_string);
+  void SendKeys(const wchar_t* input_string);
+
+  // Send mouse click to the renderer window hosted in chrome using
+  // SendInput API
+  void SendMouseClick(int x, int y, simulate_input::MouseButton button);
+
+  void Exec(const GUID* cmd_group_guid, DWORD command_id,
+            DWORD cmd_exec_opt, VARIANT* in_args, VARIANT* out_args);
 
 BEGIN_COM_MAP(WebBrowserEventSink)
 END_COM_MAP()
@@ -258,7 +223,8 @@ END_SINK_MAP()
   // Chrome frame callbacks
   virtual void OnLoad(const wchar_t* url) {}
   virtual void OnLoadError(const wchar_t* url) {}
-  virtual void OnMessage(const wchar_t* message) {}
+  virtual void OnMessage(const wchar_t* message, const wchar_t* origin,
+                         const wchar_t* source) {}
 
   IWebBrowser2* web_browser2() {
     return web_browser2_.get();
@@ -267,6 +233,7 @@ END_SINK_MAP()
   virtual void OnNewBrowserWindow(IDispatch* new_window, const wchar_t* url) {}
 
   HRESULT SetWebBrowser(IWebBrowser2* web_browser2);
+  void ExpectRendererWindowHasfocus();
 
  protected:
   STDMETHOD(OnBeforeNavigate2Internal)(IDispatch* dispatch, VARIANT* url,
@@ -289,7 +256,7 @@ END_SINK_MAP()
 
   void ConnectToChromeFrame();
   void DisconnectFromChromeFrame();
-  HWND GetTabWindow();
+  HWND GetRendererWindow();
 
  public:
   ScopedComPtr<IWebBrowser2> web_browser2_;
