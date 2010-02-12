3ffe2a2c649ffa9a4434d0497b1dd6a15eff9f55
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-12 18:04:27 +0000

    Pass NetworkChangeNotifier to ProxyService so it gets network change notifications. BUG=12293
    Review URL: http://codereview.chromium.org/596074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38903 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_url_request_context.cc
+++ chrome/browser/net/chrome_url_request_context.cc
@@ -81,6 +81,7 @@ net::ProxyConfigService* CreateProxyConfigService(
 
 // Create a proxy service according to the options on command line.
 net::ProxyService* CreateProxyService(
+    net::NetworkChangeNotifier* network_change_notifier,
     URLRequestContext* context,
     net::ProxyConfigService* proxy_config_service,
     const CommandLine& command_line,
@@ -99,8 +100,7 @@ net::ProxyService* CreateProxyService(
       proxy_config_service,
       use_v8,
       context,
-      NULL, // TODO(eroman): Pass a valid NetworkChangeNotifier implementation
-            //               (http://crbug.com/12293).
+      network_change_notifier,
       io_loop);
 }
 
@@ -147,7 +147,8 @@ ChromeURLRequestContext* FactoryForOriginal::Create() {
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
 
   context->set_proxy_service(
-      CreateProxyService(context,
+      CreateProxyService(io_thread()->globals()->network_change_notifier.get(),
+                         context,
                          proxy_config_service_.release(),
                          command_line,
                          MessageLoop::current() /*io_loop*/));
