5f3ecf9a0de5bb65d883bc50b5900a6a90a5fbf4
slightlyoff@chromium.org <slightlyoff@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-12 21:34:25 +0000

    make combine_libs.py not bail out when run under the system provided python under Cygwin.
    BUG=35342
    TEST=in cygwin do: %> devenv /build Debug chrome.sln
    
    Review URL: http://codereview.chromium.org/593087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38950 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/combine_libs.py
+++ chrome_frame/combine_libs.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -52,7 +52,7 @@ def CombineLibraries(output, remove_re, inputs):
   removals = []
   if remove_re:
     removals = CollectRemovals(remove_re, inputs)
-  
+
   print removals
 
   args = ['lib.exe', '/out:%s' % output]
@@ -101,7 +101,7 @@ def Main():
   else:
     remove_re = None
 
-  if sys.platform != 'win32':
+  if sys.platform != 'win32' and sys.platform != 'cygwin':
     parser.error('this script only works on Windows for now')
 
   # If this is set, we can't capture lib.exe's output.
