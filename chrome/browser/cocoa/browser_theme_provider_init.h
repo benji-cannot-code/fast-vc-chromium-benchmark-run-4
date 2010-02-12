18c7a9bf86c58922ee62a738e13e70e9ed40e17b
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-12 16:50:25 +0000

    Mac: Part 1 of browser_window_controller.* re-organization.
    BUG=35543
    TEST=everything still builds and works
    Review URL: http://codereview.chromium.org/606002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38899 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/cocoa/browser_theme_provider_init.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_COCOA_BROWSER_THEME_PROVIDER_INIT_H_
+#define CHROME_BROWSER_COCOA_BROWSER_THEME_PROVIDER_INIT_H_
+
+#import "third_party/GTM/AppKit/GTMTheme.h"
+
+class BrowserThemeProvider;
+
+// Provides a routine to initialize GTMTheme with data from a provided
+// |BrowserThemeProvider|.
+@interface GTMTheme(BrowserThemeProviderInitialization)
++ (GTMTheme*)themeWithBrowserThemeProvider:(BrowserThemeProvider*)provider
+                            isOffTheRecord:(BOOL)offTheRecord;
+@end
+
+#endif  // CHROME_BROWSER_COCOA_BROWSER_THEME_PROVIDER_INIT_H_
