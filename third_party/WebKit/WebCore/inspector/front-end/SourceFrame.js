207e74254a1065444eaf1d516e17c20c42beb1ae
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-15 10:34:36 +0000

    2010-02-14  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: highlight actual search match in elements panel.
    
            https://bugs.webkit.org/show_bug.cgi?id=34251
    
            * inspector/front-end/ElementsPanel.js:
            (WebInspector.ElementsPanel.prototype.searchCanceled):
            (WebInspector.ElementsPanel.prototype.performSearch):
            (WebInspector.ElementsPanel.prototype.addNodesToSearchResult):
            (WebInspector.ElementsPanel.prototype.jumpToNextSearchResult):
            (WebInspector.ElementsPanel.prototype.jumpToPreviousSearchResult):
            (WebInspector.ElementsPanel.prototype._highlightCurrentSearchResult):
            (WebInspector.ElementsPanel.prototype._hideSearchHighlights):
            * inspector/front-end/InjectedScript.js:
            * inspector/front-end/SourceFrame.js:
            (WebInspector.SourceFrame.prototype.findSearchMatches):
            * inspector/front-end/TextViewer.js:
            (WebInspector.TextViewer.prototype._paintLine):
            * inspector/front-end/textViewer.css:
            * inspector/front-end/utilities.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54770 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/SourceFrame.js
+++ third_party/WebKit/WebCore/inspector/front-end/SourceFrame.js
@@ -197,14 +197,7 @@ WebInspector.SourceFrame.prototype = {
         var ranges = [];
 
         // First do case-insensitive search.
-        var regex = "";
-        for (var i = 0; i < query.length; ++i) {
-            var char = query.charAt(i);
-            if (char === "]")
-                char = "\\]";
-            regex += "[" + char + "]";
-        }
-        var regexObject = new RegExp(regex, "i");
+        var regexObject = createSearchRegex(query);
         this._collectRegexMatches(regexObject, ranges);
 
         // Then try regex search if user knows the / / hint.
