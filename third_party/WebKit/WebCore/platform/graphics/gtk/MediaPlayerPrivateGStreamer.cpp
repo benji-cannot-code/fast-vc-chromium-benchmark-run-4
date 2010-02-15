74247765912dd4571a0d94b36e922300c728b475
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-15 18:35:01 +0000

    2010-02-15  Philippe Normand  <pnormand@igalia.com>
            Rubber-stamped by Gustavo Noronha Silva.
    
            Removed redefine of LOG_VERBOSE. This should not have commited in
            the first place.
    
            * platform/graphics/gtk/MediaPlayerPrivateGStreamer.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gtk/MediaPlayerPrivateGStreamer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/gtk/MediaPlayerPrivateGStreamer.cpp
@@ -56,8 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <webkit/webkitwebview.h>
 #include <wtf/gtk/GOwnPtr.h>
 
-#define LOG_VERBOSE(channel, fmt, args...) g_printerr(fmt "\n", ##args)
-
 using namespace std;
 
 namespace WebCore {
