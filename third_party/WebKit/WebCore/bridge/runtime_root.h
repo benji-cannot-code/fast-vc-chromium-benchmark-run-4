1f6bdbd2fb7d9f51a6dbdc44f4b3e669b1c94905
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-15 19:16:21 +0000

    Reviewed by Kevin Decker.
            <rdar://problem/7130641> Browser objects identity is not preserved by Safari
    
            Test: plugins/netscape-browser-object-identity.html
    
            * bridge/runtime_root.h: (JSC::Bindings::RootObject::addInvalidationCallback):
            RootObject can now call out during invalidation, making it possible for other code to know
            when this happens.
    
            * bridge/runtime_root.cpp:
            (JSC::Bindings::RootObject::InvalidationCallback::~InvalidationCallback): Empty destructor,
            in cpp file since it's virtual.
            (JSC::Bindings::RootObject::invalidate): Invoke invalidation callbacks.
    
            * bridge/NP_jsobject.cpp:
            (ObjectMap): Keep a JSObject->NPObject map for each RootObject. It somewhat cleaner to
            keep it outside RootObject, because (1) it is agnostic of what kinds of objects can wrap
            JSObject, and (2) out of process NPAPI implementation also keeps its corresponding map
            separately, due to supporting per-instance granularity (as opposed to per-RootObject here).
            (jsDeallocate): Remove the corresponding map entry.
            (_NPN_CreateScriptObject): Try to fetch existing object from the map, incrementing refcount.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54783 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/runtime_root.h
+++ third_party/WebKit/WebCore/bridge/runtime_root.h
@@ -73,6 +73,13 @@ public:
 
     void addRuntimeObject(RuntimeObjectImp*);
     void removeRuntimeObject(RuntimeObjectImp*);
+
+    struct InvalidationCallback {
+        virtual void operator()(RootObject*) = 0;
+        virtual ~InvalidationCallback();
+    };
+    void addInvalidationCallback(InvalidationCallback* callback) { m_invalidationCallbacks.add(callback); }
+
 private:
     RootObject(const void* nativeHandle, JSGlobalObject*);
     
@@ -80,9 +87,11 @@ private:
     
     const void* m_nativeHandle;
     ProtectedPtr<JSGlobalObject> m_globalObject;
-    ProtectCountSet m_protectCountSet;
 
+    ProtectCountSet m_protectCountSet;
     HashSet<RuntimeObjectImp*> m_runtimeObjects;    
+
+    HashSet<InvalidationCallback*> m_invalidationCallbacks;
 };
 
 } // namespace Bindings
