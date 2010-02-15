8cc9a81e87444843df33b03158f457c053316364
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-15 21:09:49 +0000

    Removed some mistaken code added in http://trac.webkit.org/changeset/53860.
    Reviewed by Oliver Hunt.
    
    * API/APIShims.h:
    (JSC::APICallbackShim::APICallbackShim):
    (JSC::APICallbackShim::~APICallbackShim): No need to start/stop the
    timeout checker when calling out from the API to the client; we want to
    monitor the VM for timeouts, not the client. This mistake was harmless /
    undetectable, since it's totally redundant with the APIEntryShim, which
    also starts / stops the timeout checker.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/APIShims.h
+++ third_party/WebKit/JavaScriptCore/API/APIShims.h
@@ -81,12 +81,10 @@ public:
         , m_globalData(&exec->globalData())
     {
         resetCurrentIdentifierTable();
-        m_globalData->timeoutChecker.start();
     }
 
     ~APICallbackShim()
     {
-        m_globalData->timeoutChecker.stop();
         setCurrentIdentifierTable(m_globalData->identifierTable);
     }
 
