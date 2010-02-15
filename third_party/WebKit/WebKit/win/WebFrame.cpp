f8ff97f224961bddb49b3d09d5f2b862128e3e6e
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-15 21:48:31 +0000

    Add IWebFramePrivate::visibleContentRect
    Fixes <http://webkit.org/b/34956> Add API to get a WebFrame's visible
    content rect
    
    Reviewed by Jon Honeycutt.
    
    * Interfaces/IWebFramePrivate.idl: Added visibleContentRect.
    
    * Interfaces/WebKit.idl: Touched to force a build.
    
    * WebFrame.cpp:
    (WebFrame::visibleContentRect):
    * WebFrame.h:
    Added. Calls through to FrameView::visibleContentRect.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54796 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebFrame.cpp
+++ third_party/WebKit/WebKit/win/WebFrame.cpp
@@ -1193,6 +1193,24 @@ HRESULT WebFrame::pauseSVGAnimation(BSTR elementId, IDOMNode* node, double secon
     return S_OK;
 }
 
+HRESULT WebFrame::visibleContentRect(RECT* rect)
+{
+    if (!rect)
+        return E_POINTER;
+    SetRectEmpty(rect);
+
+    Frame* frame = core(this);
+    if (!frame)
+        return E_FAIL;
+
+    FrameView* view = frame->view();
+    if (!view)
+        return E_FAIL;
+
+    *rect = view->visibleContentRect(false);
+    return S_OK;
+}
+
 HRESULT WebFrame::numberOfActiveAnimations(UINT* number)
 {
     if (!number)
