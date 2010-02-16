1f66d4249843deb12e06e0bc6889d348eba861af
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-16 01:32:06 +0000

    Some general Rope related refactoring.
    Reviewed by Oliver Hunt.
    
    Rename Rope::m_ropeLength to m_fiberCount, to be more descriptive.
    Rename Rope::m_stringLength to simply m_length (since this is the
    more conventional name for the length of a string).  Move append
    behaviour out into a new RopeBuilder class, so that Rope no longer
    needs any knowledge of the JSString or UString implementation.
    
    Make Rope no longer be nested within JSString.
    (Rope now no-longer need reside within JSString.h, but leaving
    the change of moving this out to a different header as a separate
    change from these renames).
    
    * JavaScriptCore.exp:
    * jit/JITOpcodes.cpp:
    (JSC::JIT::privateCompileCTIMachineTrampolines):
    * runtime/JSString.cpp:
    (JSC::Rope::destructNonRecursive):
    (JSC::Rope::~Rope):
    (JSC::JSString::resolveRope):
    (JSC::JSString::toBoolean):
    (JSC::JSString::getStringPropertyDescriptor):
    * runtime/JSString.h:
    (JSC::Rope::Fiber::Fiber):
    (JSC::Rope::Fiber::deref):
    (JSC::Rope::Fiber::ref):
    (JSC::Rope::Fiber::refAndGetLength):
    (JSC::Rope::Fiber::isRope):
    (JSC::Rope::Fiber::rope):
    (JSC::Rope::Fiber::isString):
    (JSC::Rope::Fiber::string):
    (JSC::Rope::Fiber::nonFiber):
    (JSC::Rope::tryCreateUninitialized):
    (JSC::Rope::append):
    (JSC::Rope::fiberCount):
    (JSC::Rope::length):
    (JSC::Rope::fibers):
    (JSC::Rope::Rope):
    (JSC::Rope::operator new):
    (JSC::):
    (JSC::RopeBuilder::JSString):
    (JSC::RopeBuilder::~JSString):
    (JSC::RopeBuilder::length):
    (JSC::RopeBuilder::canGetIndex):
    (JSC::RopeBuilder::appendStringInConstruct):
    (JSC::RopeBuilder::appendValueInConstructAndIncrementLength):
    (JSC::RopeBuilder::isRope):
    (JSC::RopeBuilder::fiberCount):
    (JSC::JSString::getStringPropertySlot):
    * runtime/Operations.h:
    (JSC::jsString):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54804 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSString.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSString.cpp
@@ -32,13 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace JSC {
 
-void JSString::Rope::destructNonRecursive()
+void Rope::destructNonRecursive()
 {
     Vector<Rope*, 32> workQueue;
     Rope* rope = this;
 
     while (true) {
-        unsigned length = rope->ropeLength();
+        unsigned length = rope->fiberCount();
         for (unsigned i = 0; i < length; ++i) {
             Fiber& fiber = rope->fibers(i);
             if (fiber.isString())
@@ -62,7 +62,7 @@ void JSString::Rope::destructNonRecursive()
     }
 }
 
-JSString::Rope::~Rope()
+Rope::~Rope()
 {
     destructNonRecursive();
 }
@@ -83,36 +83,36 @@ void JSString::resolveRope(ExecState* exec) const
 
     // Allocate the buffer to hold the final string, position initially points to the end.
     UChar* buffer;
-    if (PassRefPtr<UStringImpl> newImpl = UStringImpl::tryCreateUninitialized(m_stringLength, buffer))
+    if (PassRefPtr<UStringImpl> newImpl = UStringImpl::tryCreateUninitialized(m_length, buffer))
         m_value = newImpl;
     else {
-        for (unsigned i = 0; i < m_ropeLength; ++i) {
+        for (unsigned i = 0; i < m_fiberCount; ++i) {
             m_fibers[i].deref();
             m_fibers[i] = static_cast<void*>(0);
         }
-        m_ropeLength = 0;
+        m_fiberCount = 0;
         ASSERT(!isRope());
         ASSERT(m_value == UString());
         throwOutOfMemoryError(exec);
         return;
     }
-    UChar* position = buffer + m_stringLength;
+    UChar* position = buffer + m_length;
 
     // Start with the current Rope.
     Vector<Rope::Fiber, 32> workQueue;
     Rope::Fiber currentFiber;
-    for (unsigned i = 0; i < (m_ropeLength - 1); ++i)
+    for (unsigned i = 0; i < (m_fiberCount - 1); ++i)
         workQueue.append(m_fibers[i]);
-    currentFiber = m_fibers[m_ropeLength - 1];
+    currentFiber = m_fibers[m_fiberCount - 1];
     while (true) {
         if (currentFiber.isRope()) {
             Rope* rope = currentFiber.rope();
             // Copy the contents of the current rope into the workQueue, with the last item in 'currentFiber'
             // (we will be working backwards over the rope).
-            unsigned ropeLengthMinusOne = rope->ropeLength() - 1;
-            for (unsigned i = 0; i < ropeLengthMinusOne; ++i)
+            unsigned fiberCountMinusOne = rope->fiberCount() - 1;
+            for (unsigned i = 0; i < fiberCountMinusOne; ++i)
                 workQueue.append(rope->fibers(i));
-            currentFiber = rope->fibers(ropeLengthMinusOne);
+            currentFiber = rope->fibers(fiberCountMinusOne);
         } else {
             UString::Rep* string = currentFiber.string();
             unsigned length = string->size();
@@ -123,11 +123,11 @@ void JSString::resolveRope(ExecState* exec) const
             if (workQueue.isEmpty()) {
                 // Create a string from the UChar buffer, clear the rope RefPtr.
                 ASSERT(buffer == position);
-                for (unsigned i = 0; i < m_ropeLength; ++i) {
+                for (unsigned i = 0; i < m_fiberCount; ++i) {
                     m_fibers[i].deref();
                     m_fibers[i] = static_cast<void*>(0);
                 }
-                m_ropeLength = 0;
+                m_fiberCount = 0;
 
                 ASSERT(!isRope());
                 return;
@@ -154,7 +154,7 @@ bool JSString::getPrimitiveNumber(ExecState* exec, double& number, JSValue& resu
 
 bool JSString::toBoolean(ExecState*) const
 {
-    return m_stringLength;
+    return m_length;
 }
 
 double JSString::toNumber(ExecState* exec) const
@@ -216,13 +216,13 @@ bool JSString::getOwnPropertySlot(ExecState* exec, const Identifier& propertyNam
 bool JSString::getStringPropertyDescriptor(ExecState* exec, const Identifier& propertyName, PropertyDescriptor& descriptor)
 {
     if (propertyName == exec->propertyNames().length) {
-        descriptor.setDescriptor(jsNumber(exec, m_stringLength), DontEnum | DontDelete | ReadOnly);
+        descriptor.setDescriptor(jsNumber(exec, m_length), DontEnum | DontDelete | ReadOnly);
         return true;
     }
     
     bool isStrictUInt32;
     unsigned i = propertyName.toStrictUInt32(&isStrictUInt32);
-    if (isStrictUInt32 && i < m_stringLength) {
+    if (isStrictUInt32 && i < m_length) {
         descriptor.setDescriptor(jsSingleCharacterSubstring(exec, value(exec), i), DontDelete | ReadOnly);
         return true;
     }
