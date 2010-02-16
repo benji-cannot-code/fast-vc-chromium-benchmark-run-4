6b0e1e72027d18aa15a16c726871711337b89267
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-16 21:56:18 +0000

    [Mac] Prevent a fullscreen crash by destroying the GTMWindowSheetController in setFullscreen.
    BUG=31638
    TEST=Trigger a sheet (ex: http auth dialog) after going fullscreen.  Should not crash.
    Review URL: http://codereview.chromium.org/606029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39141 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_strip_controller.mm
+++ chrome/browser/cocoa/tab_strip_controller.mm
@@ -1633,6 +1633,12 @@ private:
   return sheetController_.get();
 }
 
+- (void)destroySheetController {
+  // Make sure there are no open sheets.
+  DCHECK_EQ(0U, [[sheetController_ viewsWithAttachedSheets] count]);
+  sheetController_.reset();
+}
+
 - (TabContentsController*)activeTabContentsController {
   int modelIndex = tabStripModel_->selected_index();
   if (modelIndex < 0)
