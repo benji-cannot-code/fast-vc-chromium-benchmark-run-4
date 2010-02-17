c9f8474f8c6384f40ec2b3afdf1256e90d4666e5
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-17 03:37:58 +0000

    2010-02-16  Bryan Yeung  <bryeung@chromium.org>
            Reviewed by Nikolas Zimmermann.
    
            Remove the bounds on stroke width and miter for the skia platform.
            https://bugs.webkit.org/show_bug.cgi?id=34954
    
            Test: svg/custom/stroke-width-large.svg
    
            * platform/graphics/skia/PlatformContextSkia.cpp:
            (PlatformContextSkia::setupPaintForStroking):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54864 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gtk/WebKitWebSourceGStreamer.h
+++ third_party/WebKit/WebCore/platform/graphics/gtk/WebKitWebSourceGStreamer.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- *  Copyright (C) 2009 Sebastian Dröge <sebastian.droege@collabora.co.uk>
+ *  Copyright (C) 2009,2010 Sebastian Dröge <sebastian.droege@collabora.co.uk>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
