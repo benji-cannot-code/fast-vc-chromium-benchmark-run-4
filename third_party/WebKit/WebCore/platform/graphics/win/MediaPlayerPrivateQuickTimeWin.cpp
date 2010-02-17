e06425ae7d657900ec6acfbac7c1c9dbb135f9ce
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-17 05:26:01 +0000

    Forgot a .get() because this is an OwnPtr
    git-svn-id: svn://svn.chromium.org/blink/trunk@54869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp
@@ -864,7 +864,7 @@ void MediaPlayerPrivate::acceleratedRenderingStateChanged()
 
 void MediaPlayerPrivate::notifySyncRequired(const GraphicsLayer*)
 {
-    GraphicsLayerCACF* videoGraphicsLayer = static_cast<GraphicsLayerCACF*>(m_qtVideoLayer);
+    GraphicsLayerCACF* videoGraphicsLayer = static_cast<GraphicsLayerCACF*>(m_qtVideoLayer.get());
     if (videoGraphicsLayer)
         videoGraphicsLayer->notifySyncRequired();
  }
