a8109da3f702104113ce9ae962d92e87114d88ff
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-17 08:38:14 +0000

    Not reviewed - demo.
    Made some examples of using <details> for table descriptions.
    
    * demos/hover-summary: Added.
    * demos/hover-summary/details.css: Added.
    * demos/hover-summary/details.js: Added.
    (summaryClickListener):
    ():
    (window.onload):
    * demos/hover-summary/example1.html: Added.
    * demos/hover-summary/example2.html: Added.
    * demos/hover-summary/horizontal-triangle.png: Added.
    * demos/hover-summary/vertical-triangle.png: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54879 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKitSite/demos/hover-summary/details.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+function summaryClickListener(evt)
+{
+    if (this.parentNode.getAttribute("open") == null) {
+        this.parentNode.setAttribute("open", "");
+        this.parentNode.className = "xxx";
+    } else {
+        this.parentNode.removeAttribute("open");
+        this.parentNode.className = "yyy";
+    }
+}
+
+
+function detailsKeyPressListener(evt)
+{
+    if (this.getAttribute("open") == null) {
+        this.setAttribute("open", "");
+        this.className = "xxx";
+    } else {
+        this.removeAttribute("open");
+        this.className = "yyy";
+    }
+}
+
+
+window.onload = function() {
+    var summaryElements = document.getElementsByTagName("summary");
+    for (var i = 0; i < summaryElements.length; ++i) {
+        summaryElements[i].parentNode.setAttribute("tabindex", 0);
+        summaryElements[i].addEventListener("click", summaryClickListener, false);
+        summaryElements[i].parentNode.addEventListener("keypress", detailsKeyPressListener, false);
+    }
+};
