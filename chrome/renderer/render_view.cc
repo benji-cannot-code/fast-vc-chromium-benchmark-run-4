30721ea45283a8957c6b0a1f9a68dbe77d6aa4df
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-17 14:12:06 +0000

    Disable Mac-like input behavior on all platforms but Mac.
    BUG=none
    TEST=e.g. double click a word to select it, hit delete. should only delete the word (not the whitespace before)
    
    Review URL: http://codereview.chromium.org/601093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39219 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -1585,6 +1585,14 @@ void RenderView::didStopLoading() {
       kDelayForCaptureMs);
 }
 
+bool RenderView::isSmartInsertDeleteEnabled() {
+#if defined(OS_MACOSX)
+  return true;
+#else
+  return false;
+#endif
+}
+
 bool RenderView::isSelectTrailingWhitespaceEnabled() {
 #if defined(OS_WIN)
   return true;
