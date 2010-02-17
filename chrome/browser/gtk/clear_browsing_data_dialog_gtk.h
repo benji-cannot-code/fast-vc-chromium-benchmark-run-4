25da0993903b1f98a166226f966daef94b4818eb
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-17 17:49:19 +0000

    GTK: Add a link to flash settings from the clear browsing history dialog.
    BUG=35183
    TEST=none
    
    Review URL: http://codereview.chromium.org/606070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39235 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/clear_browsing_data_dialog_gtk.h
+++ chrome/browser/gtk/clear_browsing_data_dialog_gtk.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -37,6 +37,12 @@ class ClearBrowsingDataDialogGtk {
   }
   void OnDialogResponse(GtkWidget* widget, int response);
   void OnDialogWidgetClicked(GtkWidget* widget);
+
+  static void HandleOnFlashLinkClicked(GtkWidget* button,
+                                       ClearBrowsingDataDialogGtk* user_data) {
+    user_data->OnFlashLinkClicked(button);
+  }
+  void OnFlashLinkClicked(GtkWidget* widget);
 
   // UI elements.
   GtkWidget* del_history_checkbox_;
