1f6742066ccf3178ed4e1d9691f5b7263085e4f5
snej@chromium.org <snej@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-18 22:45:05 +0000

    Client-side SSL cert support for Mac. This includes sending an existing identity cert, and asking the user which cert to use. Doesn't yet handle SSL renegotiation, or key-gen. BUG=16831 TEST=none
    Review URL: http://codereview.chromium.org/604067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39389 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ssl/ssl_client_auth_handler_gtk.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ssl/ssl_client_auth_handler.h"
+#include "base/logging.h"
+#include "net/base/x509_certificate.h"
+
+void SSLClientAuthHandler::DoSelectCertificate() {
+  NOTIMPLEMENTED();
+  CertificateSelected(NULL);
+}
