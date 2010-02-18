0a8448c50e5d97cf4f7e06c8b2659c89bb25f323
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-18 17:32:06 +0000

    2010-02-18  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Don't truncate long lines when evaluating expressions on a breakpoint.
    
            https://bugs.webkit.org/show_bug.cgi?id=35102
    
            * src/js/DebuggerAgent.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54968 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/js/DebuggerAgent.js
+++ third_party/WebKit/WebKit/chromium/src/js/DebuggerAgent.js
@@ -1365,7 +1365,8 @@ devtools.CallFrame.prototype.evaluate_ = function(expression, callback)
             "frame": this.id,
             "global": false,
             "disable_break": false,
-            "compactFormat": true
+            "compactFormat": true,
+            "maxStringLength": -1
         },
         function(response) {
             var result = {};
