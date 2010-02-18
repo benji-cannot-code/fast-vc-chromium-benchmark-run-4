f3c1e6e4a8e39584d3020faee0bcd756b9a82184
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-18 05:14:59 +0000

    2010-02-17  Yuzo Fujishima  <yuzo@google.com>
            Reviewed by Eric Seidel.
    
            In diffing render styles, consider all pseudo style changes.
            Without this patch, only :before and :after are considered. This is the cause of the following bug.
            https://bugs.webkit.org/show_bug.cgi?id=32187
    
            * fast/css/first-letter-first-line-hover.html: Added.
            * platform/mac/fast/css/first-letter-first-line-hover-expected.checksum: Added.
            * platform/mac/fast/css/first-letter-first-line-hover-expected.png: Added.
            * platform/mac/fast/css/first-letter-first-line-hover-expected.txt: Added.
    2010-02-17  Yuzo Fujishima  <yuzo@google.com>
    
            Reviewed by Eric Seidel.
    
            In diffing render styles, consider all pseudo style changes.
            Without this patch, only :before and :after are considered. This is the cause of the following bug.
            https://bugs.webkit.org/show_bug.cgi?id=32187
    
            Test: fast/css/first-letter-first-line-hover.html
    
            * dom/Node.cpp:
            (WebCore::Node::diff):
            * rendering/style/RenderStyle.cpp:
            (WebCore::RenderStyle::hasAnyPublicPseudoStyles):
            * rendering/style/RenderStyle.h:
            * rendering/style/RenderStyleConstants.h:
            (WebCore::):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54926 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/style/RenderStyle.h
+++ third_party/WebKit/WebCore/rendering/style/RenderStyle.h
@@ -351,6 +351,7 @@ public:
 
     bool isStyleAvailable() const;
 
+    bool hasAnyPublicPseudoStyles() const;
     bool hasPseudoStyle(PseudoId pseudo) const;
     void setHasPseudoStyle(PseudoId pseudo);
 
