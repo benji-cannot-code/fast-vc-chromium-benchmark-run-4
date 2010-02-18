46d80c60fb607850b64d91a2d3971389d04a53d1
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-18 12:11:14 +0000

    Switch browser prefs to use the higher level geolocation::RegisterPrefs method rather than call into random internal component of geolocaiton (and so avoid the associated header file dependency bloat) geolocation_prefs.h was already added in http://src.chromium.org/viewvc/chrome?view=rev&revision=39232 I just forgot to include this edit
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/618016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39352 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_prefs.cc
+++ chrome/browser/browser_prefs.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extensions_ui.h"
 #include "chrome/browser/external_protocol_handler.h"
 #include "chrome/browser/form_field_history_manager.h"
-#include "chrome/browser/geolocation/access_token_store.h"
+#include "chrome/browser/geolocation/geolocation_prefs.h"
 #include "chrome/browser/google_url_tracker.h"
 #include "chrome/browser/host_content_settings_map.h"
 #include "chrome/browser/host_zoom_map.h"
@@ -83,7 +83,7 @@ void RegisterLocalState(PrefService* local_state) {
 #if defined(OS_WIN)
   CookiePromptModalDialog::RegisterPrefs(local_state);
 #endif
-  AccessTokenStore::RegisterPrefs(local_state);
+  geolocation::RegisterPrefs(local_state);
 }
 
 void RegisterUserPrefs(PrefService* user_prefs) {
