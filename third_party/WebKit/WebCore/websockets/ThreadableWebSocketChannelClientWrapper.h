99421e4a3767d07dee894dc961fe96c69e819751
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-18 05:32:05 +0000

    2010-02-17  Fumitoshi Ukai  <ukai@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            WebSocket bufferedAmount should not be 0 when send after close.
            https://bugs.webkit.org/show_bug.cgi?id=34633
    
            * websocket/tests/bufferedAmount-after-close-expected.txt: Added.
            * websocket/tests/bufferedAmount-after-close.html: Added.
            * websocket/tests/script-tests/bufferedAmount-after-close.js: Added.
    2010-02-17  Fumitoshi Ukai  <ukai@chromium.org>
    
            Reviewed by Alexey Proskuryakov.
    
            WebSocket bufferedAmount should not be 0 when send after close.
            https://bugs.webkit.org/show_bug.cgi?id=34633
    
            Fixed bug in webkit r54694.  check m_client after it calls user
            callback, because user callback may call close(), which would
            call onclose event handler.
    
            Test: websocket/tests/bufferedAmount-after-close.html
    
            * websockets/ThreadableWebSocketChannelClientWrapper.h:
            (WebCore::ThreadableWebSocketChannelClientWrapper::didClose):
            * websockets/WebSocket.cpp:
            (WebCore::WebSocket::WebSocket):
            (WebCore::WebSocket::send):
            (WebCore::WebSocket::close):
            (WebCore::WebSocket::bufferedAmount):
            (WebCore::WebSocket::didConnect):
            (WebCore::WebSocket::didClose):
            * websockets/WebSocket.h:
            * websockets/WebSocketChannel.cpp:
            (WebCore::WebSocketChannel::WebSocketChannel):
            (WebCore::WebSocketChannel::send):
            (WebCore::WebSocketChannel::bufferedAmount):
            (WebCore::WebSocketChannel::didClose):
            (WebCore::WebSocketChannel::didReceiveData):
            * websockets/WebSocketChannel.h:
            * websockets/WebSocketChannelClient.h:
            (WebCore::WebSocketChannelClient::didClose):
            * websockets/WorkerThreadableWebSocketChannel.cpp:
            (WebCore::workerContextDidClose):
            (WebCore::WorkerThreadableWebSocketChannel::Peer::didClose):
            * websockets/WorkerThreadableWebSocketChannel.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@54927 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/websockets/ThreadableWebSocketChannelClientWrapper.h
+++ third_party/WebKit/WebCore/websockets/ThreadableWebSocketChannelClientWrapper.h
@@ -100,10 +100,10 @@ public:
             m_client->didReceiveMessage(msg);
     }
 
-    void didClose()
+    void didClose(unsigned long unhandledBufferedAmount)
     {
         if (m_client)
-            m_client->didClose();
+            m_client->didClose(unhandledBufferedAmount);
     }
 
 protected:
