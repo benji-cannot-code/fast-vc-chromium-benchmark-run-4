697ef788a38deba5ed1db4599b62fce7253379ba
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-19 12:40:14 +0000

    2010-02-19  Pavel Feldman  <pfeldman@chromium.org>
            Not reviewed. Chromium tests fix: update devtools code to match new
            web inspector object proxy format.
    
            * src/js/DebuggerAgent.js:
            (devtools.DebuggerAgent.prototype.formatCallFrame_):
            (devtools.DebuggerAgent.formatObjectProxy_):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/js/DebuggerAgent.js
+++ third_party/WebKit/WebKit/chromium/src/js/DebuggerAgent.js
@@ -1054,7 +1054,7 @@ devtools.DebuggerAgent.prototype.formatCallFrame_ = function(stackFrame)
     for (var i = 0; i < stackFrame.scopes.length; i++) {
         var scope = stackFrame.scopes[i];
         scope.frameNumber = stackFrame.index;
-        var scopeObjectProxy = new WebInspector.ObjectProxy(0, scope, [], 0, "", true);
+        var scopeObjectProxy = new WebInspector.ObjectProxy(0, scope, [], "", true);
         scopeObjectProxy.isScope = true;
         switch(scope.type) {
             case ScopeType.Global:
@@ -1150,7 +1150,7 @@ devtools.DebuggerAgent.formatObjectProxy_ = function(v)
     } else
         description = "<unresolved ref: " + v.ref + ", type: " + v.type + ">";
 
-    var proxy = new WebInspector.ObjectProxy(0, v, [], 0, description, hasChildren);
+    var proxy = new WebInspector.ObjectProxy(0, v, [], description, hasChildren);
     proxy.type = v.type;
     proxy.isV8Ref = true;
     return proxy;
