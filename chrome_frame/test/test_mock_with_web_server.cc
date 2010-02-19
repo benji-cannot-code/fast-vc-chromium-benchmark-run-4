e3b910f4dd91e67dbb4115cfb5cfd087aa8ce2a4
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-19 17:43:41 +0000

    Mark four more FullTabModeIE tests as flaky
    FullTabModeIE_ContextMenuBackForward
    FullTabModeIE_ContextMenuReload
    FullTabModeIE_ContextMenuViewSource
    FullTabModeIE_KeyboardBackForwardTes
    
    TBR=ananta
    
    BUG=26549
    TEST=none
    
    Review URL: http://codereview.chromium.org/650041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39459 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/test_mock_with_web_server.cc
+++ chrome_frame/test/test_mock_with_web_server.cc
@@ -921,7 +921,9 @@ TEST_F(ChromeFrameTestWithWebServer,
 
 // Test Back/Forward from context menu. Loads page 1 in chrome and page 2
 // in IE. Then it tests back and forward using context menu
-TEST_F(ChromeFrameTestWithWebServer, FullTabModeIE_ContextMenuBackForward) {
+// Marking this test FLAKY as it fails at times on the buildbot.
+// http://code.google.com/p/chromium/issues/detail?id=26549
+TEST_F(ChromeFrameTestWithWebServer, FLAKY_FullTabModeIE_ContextMenuBackForward) {
   CloseIeAtEndOfScope last_resort_close_ie;
   chrome_frame_test::TimedMsgLoop loop;
   ComStackObjectWithUninitialize<MockWebBrowserEventSink> mock;
@@ -1047,7 +1049,9 @@ TEST_F(ChromeFrameTestWithWebServer, FullTabModeIE_ContextMenuBackForward) {
 }
 
 // Test Reload from context menu.
-TEST_F(ChromeFrameTestWithWebServer, FullTabModeIE_ContextMenuReload) {
+// Marking this test FLAKY as it fails at times on the buildbot.
+// http://code.google.com/p/chromium/issues/detail?id=26549
+TEST_F(ChromeFrameTestWithWebServer, FLAKY_FullTabModeIE_ContextMenuReload) {
   CloseIeAtEndOfScope last_resort_close_ie;
   chrome_frame_test::TimedMsgLoop loop;
   ComStackObjectWithUninitialize<MockWebBrowserEventSink> mock;
@@ -1114,7 +1118,9 @@ TEST_F(ChromeFrameTestWithWebServer, FullTabModeIE_ContextMenuReload) {
 }
 
 // Test view source using context menu
-TEST_F(ChromeFrameTestWithWebServer, FullTabModeIE_ContextMenuViewSource) {
+// Marking this test FLAKY as it fails at times on the buildbot.
+// http://code.google.com/p/chromium/issues/detail?id=26549
+TEST_F(ChromeFrameTestWithWebServer, FLAKY_FullTabModeIE_ContextMenuViewSource) {
   CloseIeAtEndOfScope last_resort_close_ie;
   chrome_frame_test::TimedMsgLoop loop;
   ComStackObjectWithUninitialize<MockWebBrowserEventSink> mock;
@@ -1218,7 +1224,9 @@ TEST_F(ChromeFrameTestWithWebServer, FullTabModeIE_ContextMenuViewSource) {
   loop.RunFor(kChromeFrameLongNavigationTimeoutInSeconds);
 }
 
-TEST_F(ChromeFrameTestWithWebServer, FullTabModeIE_KeyboardBackForwardTest) {
+// Marking this test FLAKY as it fails at times on the buildbot.
+// http://code.google.com/p/chromium/issues/detail?id=26549
+TEST_F(ChromeFrameTestWithWebServer, FLAKY_FullTabModeIE_KeyboardBackForwardTest) {
   chrome_frame_test::TimedMsgLoop loop;
   ComStackObjectWithUninitialize<MockWebBrowserEventSink> mock;
 
