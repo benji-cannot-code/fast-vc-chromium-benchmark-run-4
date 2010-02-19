7ba956d6ebe37c6bd2e2474798e11b25b1396e9f
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-19 01:57:41 +0000

    Fix a Chrome crash caused in a ChromeFrame instance while displaying the inspector window. The inspector window uses the browser view which instantiates the whole view including the ToolBar, which in turn instantiates the Autocomplete edit bar which uses Richedit on Windows. The auto complete edit bar dynamically loads and unloads the richedit control and it uses ATL to superclass the richedit control. If the rich edit dll loads at a different base address it causes a crash in Chrome while creating the window as ATL caches the wndproc address for a class.
    Fix is to setup an implicit dependency on the riched20.dll and not load and free it all the time. To achieve this we use #pragma comment for the 
    riched20.lib and a dummy call to the CreateTextServices function exported by the dll to ensure that the linker does not discard the import.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=33308
    
    Bug=33308
    
    Review URL: http://codereview.chromium.org/646043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39415 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view_win.h
+++ chrome/browser/autocomplete/autocomplete_edit_view_win.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -488,10 +488,6 @@ class AutocompleteEditViewWin
 
   // Instance of accessibility information and handling.
   mutable ScopedComPtr<IAccessible> autocomplete_accessibility_;
-
-  // We explicitly retain a handle to this library so it never gets unloaded out
-  // from underneath us.
-  HMODULE riched20dll_handle_;
 
   DISALLOW_COPY_AND_ASSIGN(AutocompleteEditViewWin);
 };
