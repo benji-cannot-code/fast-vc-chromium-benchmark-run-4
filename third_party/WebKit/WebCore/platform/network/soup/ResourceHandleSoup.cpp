0481898456243fa26dbc5f5e98d7ae9437df1004
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-20 12:35:35 +0000

    WebCore
    2010-02-19  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
    
            Reviewed by Xan Lopez.
    
            [Soup] loses information related to message flags when converting from/to Resource{Request,Response}
            https://bugs.webkit.org/show_bug.cgi?id=35093
    
            Store message flags in WebCore objects, and set them on the
            SoupMessage, when creating one from them.
    
            No behaviour change.
    
            * platform/network/soup/ResourceHandleSoup.cpp:
            (WebCore::fillResponseFromMessage):
            * platform/network/soup/ResourceRequest.h:
            (WebCore::ResourceRequest::soupMessageFlags):
            (WebCore::ResourceRequest::setSoupMessageFlags):
            * platform/network/soup/ResourceRequestSoup.cpp:
            (WebCore::ResourceRequest::toSoupMessage):
            (WebCore::ResourceRequest::updateFromSoupMessage):
            * platform/network/soup/ResourceResponse.h:
            (WebCore::ResourceResponse::soupMessageFlags):
            (WebCore::ResourceResponse::setSoupMessageFlags):
            * platform/network/soup/ResourceResponseSoup.cpp:
            (WebCore::ResourceResponse::toSoupMessage):
            (WebCore::ResourceResponse::updateFromSoupMessage):
    
    WebKit/gtk
    
    2010-02-18  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
    
            Reviewed by Xan Lopez.
    
            [Soup] loses information related to message flags when converting from/to Resource{Request,Response}
            https://bugs.webkit.org/show_bug.cgi?id=35093
    
            Update the flags that are stored in the request, when the response
            is received.
    
            * WebCoreSupport/FrameLoaderClientGtk.cpp:
            (WebKit::FrameLoaderClient::dispatchDidReceiveResponse):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/soup/ResourceHandleSoup.cpp
+++ third_party/WebKit/WebCore/platform/network/soup/ResourceHandleSoup.cpp
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2008 Alp Toker <alp@atoker.com>
  * Copyright (C) 2008 Xan Lopez <xan@gnome.org>
- * Copyright (C) 2008 Collabora Ltd.
+ * Copyright (C) 2008, 2010 Collabora Ltd.
  * Copyright (C) 2009 Holger Hans Peter Freyther
  * Copyright (C) 2009 Gustavo Noronha Silva <gns@gnome.org>
  * Copyright (C) 2009 Christian Dywan <christian@imendio.com>
