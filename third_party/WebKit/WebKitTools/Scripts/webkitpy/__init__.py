2260573949606d0bd6131671a72270744625d88e
cjerdonek@webkit.org <cjerdonek@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-20 17:42:46 +0000

    2010-02-20  Chris Jerdonek  <cjerdonek@webkit.org>
            Reviewed by Adam Barth.
    
            Created a directory for third-party Python code, and moved
            autoinstall.py into it.
    
            https://bugs.webkit.org/show_bug.cgi?id=34540
    
            * Scripts/webkitpy/__init__.py:
              - Updated "import autoinstall" statement.
    
            * Scripts/webkitpy/autoinstall.py: Removed.
              - Moved to thirdparty/autoinstall.py.
    
            * Scripts/webkitpy/thirdparty: Added.
            * Scripts/webkitpy/thirdparty/__init__.py: Added.
            * Scripts/webkitpy/thirdparty/autoinstall.py: Copied from WebKitTools/Scripts/webkitpy/autoinstall.py.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/__init__.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/__init__.py
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Required for Python to search this directory for module files
 
-import autoinstall
+import thirdparty.autoinstall as autoinstall
 
 # List our third-party library dependencies here and where they can be
 # downloaded.
