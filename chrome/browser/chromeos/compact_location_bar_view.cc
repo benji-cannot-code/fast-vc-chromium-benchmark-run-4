a7745108d233b2a8caa45a7582ef54f78c37ac4b
avayvod@chromium.org <avayvod@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-20 04:38:50 +0000

    Bound page reloading ignoring cache to the following user actions: - pressing Ctrl+F5 or Shift+F5 or Ctrl+Shift+R - Shift- or Ctrl-clicking the Reload button. Should work on Windows and Chrome OS (including compact navigation mode).
    BUG=36244
    TEST=Verify that everything works as described above.
    
    Review URL: http://codereview.chromium.org/650048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39543 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/compact_location_bar_view.cc
+++ chrome/browser/chromeos/compact_location_bar_view.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -212,8 +212,14 @@ void CompactLocationBarView::Focus() {
 // views::ButtonListener overrides:
 
 void CompactLocationBarView::ButtonPressed(views::Button* sender,
-                                       const views::Event& event) {
+                                           const views::Event& event) {
   int id = sender->tag();
+  // Shift-clicking or Ctrl-clicking the reload button means we should
+  // ignore any cached content.
+  // TODO(avayvod): eliminate duplication of this logic in
+  // CompactLocationBarView.
+  if (id == IDC_RELOAD && (event.IsShiftDown() || event.IsControlDown()))
+    id = IDC_RELOAD_IGNORING_CACHE;
   browser()->ExecuteCommandWithDisposition(
       id, event_utils::DispositionFromEventFlags(sender->mouse_event_flags()));
 }
--- chrome/browser/views/toolbar_view.cc
+++ chrome/browser/views/toolbar_view.cc
@@ -234,6 +234,12 @@ void ToolbarView::ButtonPressed(
       // ongoing user edits, since it doesn't realize this is a user-initiated
       // action.
       location_bar_->Revert();
+      // Shift-clicking or Ctrl-clicking the reload button means we should
+      // ignore any cached content.
+      // TODO(avayvod): eliminate duplication of this logic in
+      // CompactLocationBarView.
+      if (id == IDC_RELOAD && (event.IsShiftDown() || event.IsControlDown()))
+        id = IDC_RELOAD_IGNORING_CACHE;
       break;
   }
   browser_->ExecuteCommandWithDisposition(
