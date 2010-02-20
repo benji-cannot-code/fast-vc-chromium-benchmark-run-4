e4497657700ea18b831f49f0db3cee9a4a930f22
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-20 08:21:19 +0000

    Pull gallery special case down into Extension::CanAccessHost() to avoid duplicating it in multiple places.
    Also rename that method to better reflect what it is used for.
    
    BUG=35382
    
    Review URL: http://codereview.chromium.org/650086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39550 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_constants.h
+++ chrome/common/extensions/extension_constants.h
@@ -146,6 +146,8 @@ namespace extension_manifest_errors {
   extern const char* kLocalesMessagesFileMissing;
   extern const char* kInvalidOptionsPage;
   extern const char* kReservedMessageFound;
+  extern const char* kCannotAccessPage;
+  extern const char* kCannotScriptGallery;
 }  // namespace extension_manifest_errors
 
 namespace extension_urls {
