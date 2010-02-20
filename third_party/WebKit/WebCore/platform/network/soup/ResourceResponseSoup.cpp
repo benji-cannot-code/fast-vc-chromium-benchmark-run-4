a0d8e609a7b3a68deefaf60cdb7109022f606496
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-20 12:25:17 +0000

    2010-02-19  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
            Reviewed by Xan Lopez.
    
            [Soup] loses information related to message flags when converting from/to Resource{Request,Response}
            https://bugs.webkit.org/show_bug.cgi?id=35093
    
            Refactor updating of ResourceResponse objects from soup message
            objects, to avoid code duplication.
    
            No behaviour change.
    
            * platform/network/soup/ResourceHandleSoup.cpp:
            (WebCore::fillResponseFromMessage):
            * platform/network/soup/ResourceResponseSoup.cpp:
            (WebCore::ResourceResponse::updateFromSoupMessage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/soup/ResourceResponseSoup.cpp
+++ third_party/WebKit/WebCore/platform/network/soup/ResourceResponseSoup.cpp
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CString.h"
 #include "GOwnPtr.h"
+#include "HTTPParsers.h"
+#include "MIMETypeRegistry.h"
 #include "PlatformString.h"
 
 using namespace std;
@@ -66,6 +68,14 @@ void ResourceResponse::updateFromSoupMessage(SoupMessage* soupMessage)
     soup_message_headers_iter_init(&headersIter, soupMessage->response_headers);
     while (soup_message_headers_iter_next(&headersIter, &headerName, &headerValue))
         m_httpHeaderFields.set(String::fromUTF8(headerName), String::fromUTF8(headerValue));
+
+    String contentType = soup_message_headers_get_one(soupMessage->response_headers, "Content-Type");
+    setMimeType(extractMIMETypeFromMediaType(contentType));
+
+    setTextEncodingName(extractCharsetFromMediaType(contentType));
+    setExpectedContentLength(soup_message_headers_get_content_length(soupMessage->response_headers));
+    setHTTPStatusText(soupMessage->reason_phrase);
+    setSuggestedFilename(filenameFromHTTPContentDisposition(httpHeaderField("Content-Disposition")));
 }
 
 }
