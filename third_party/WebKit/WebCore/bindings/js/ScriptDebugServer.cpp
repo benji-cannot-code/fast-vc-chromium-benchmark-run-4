1d86df69108036f6e404c29a2d1b7e251a914e8d
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-22 13:02:47 +0000

    2010-02-22  Alexander Pavlov  <apavlov@chromium.org>
            Not reviewed: mac build fix
    
            * bindings/js/ScriptDebugServer.cpp:
            (WebCore::ScriptDebugServer::ScriptDebugServer):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55078 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/ScriptDebugServer.cpp
+++ third_party/WebKit/WebCore/bindings/js/ScriptDebugServer.cpp
@@ -71,9 +71,9 @@ ScriptDebugServer::ScriptDebugServer()
     , m_pauseOnNextStatement(false)
     , m_paused(false)
     , m_doneProcessingDebuggerEvents(true)
+    , m_breakpointsActivated(true)
     , m_pauseOnCallFrame(0)
     , m_recompileTimer(this, &ScriptDebugServer::recompileAllJSFunctions)
-    , m_breakpointsActivated(true)
 {
 }
 
