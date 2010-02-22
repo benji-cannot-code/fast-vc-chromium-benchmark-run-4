af789032d15d691fb5d873d3160db59727b0f106
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-22 18:15:34 +0000

    Initial HTTP requests issued by ChromeFrame popups could bypass the host network stack. The host network stack in ChromeFrame intercepts HTTP requests based on registered render views. When a popup window is created we register it in the AddNewContents notification on the TabContentsDelegate. This is a little late as network requests could be issued before the view actually becomes visible.
    To fix this we now have a new method in the TabContentsDelegate interface called TabContentsCreated which
    notifies the delegate about the creation of a new TabContents. The ExternalTabContainer implements this
    method and registers the render view as pending. I also changed the signature of the
    RenderViewHostDelegateViewHelper::CreateNewWindow method to return the new TabContents.
    
    As part of this fix we also unregister the render view when the ExternalTabContainer is deleted. The
    container registers for the NotificationType::RENDER_VIEW_HOST_DELETED notification. However we end up
    deleting the TabContents in WM_DESTROY, which results in the container never receiving this notification.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=36323
    
    Bug=36323
    Test=Will be covered by the automation proxy test which Stoyan is adding
    
    Review URL: http://codereview.chromium.org/652009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39611 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_host_delegate_helper.cc
+++ chrome/browser/tab_contents/render_view_host_delegate_helper.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 
-void RenderViewHostDelegateViewHelper::CreateNewWindow(
+TabContents* RenderViewHostDelegateViewHelper::CreateNewWindow(
     int route_id,
     Profile* profile,
     SiteInstance* site,
@@ -43,6 +43,7 @@ void RenderViewHostDelegateViewHelper::CreateNewWindow(
 
   // Save the created window associated with the route so we can show it later.
   pending_contents_[route_id] = new_contents;
+  return new_contents;
 }
 
 RenderWidgetHostView* RenderViewHostDelegateViewHelper::CreateNewWidget(
