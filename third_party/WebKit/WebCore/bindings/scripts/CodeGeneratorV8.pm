9532ac60a0cca18e03a0aa714e1c5516c28fbe98
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-22 20:03:17 +0000

    2010-02-22  Nate Chapin  <japhet@chromium.org>
            Unreviewed, Chromium build fix.
    
            * bindings/scripts/CodeGeneratorV8.pm: Compile break due to bad patch merge.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55097 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -1011,7 +1011,7 @@ sub GenerateNewFunctionTemplate
     return "v8::FunctionTemplate::New($callback, v8::Handle<v8::Value>(), $signature)";
 }
 
-sub GenerateNewFunctionTemplatellback
+sub GenerateEventListenerCallback
 {
     my $implClassName = shift;
     my $functionName = shift;
