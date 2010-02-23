9bdac9726d83eb206cbb1bc65204e08914af9883
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-23 17:54:37 +0000

    WebKit/mac: <rdar://problem/7611158> Incomplete repaint of YouTube timeline thumb while scrolling https://bugs.webkit.org/show_bug.cgi?id=34381
    Reviewed by Simon Fraser.
    
    Test: fast/repaint/repaint-during-scroll.html
    
    NSClipView offsets any rects marked as needing display during scrolling
    by the scroll offset. Compensate for this when -setNeedsDisplay: is called
    during scrolling.
    
    * WebView/WebHTMLView.mm:
    (-[WebHTMLView _frameOrBoundsChanged]): Set inScrollPositionChanged to YES
    around to call to FrameView::scrollPositionChanged().
    (-[WebHTMLView setNeedsDisplayInRect:]): When called beneath
    scrollPositionChanged(), adjust the rect by the inverse of the scroll offset.
    
    LayoutTests: Rubber-stamped by Simon Fraser.
    
    <rdar://problem/7611158> Incomplete repaint of YouTube timeline thumb while scrolling
    https://bugs.webkit.org/show_bug.cgi?id=34381
    
    * fast/repaint/repaint-during-scroll.html: Added.
    * platform/mac/fast/repaint/repaint-during-scroll-expected.checksum: Added.
    * platform/mac/fast/repaint/repaint-during-scroll-expected.png: Added.
    * platform/mac/fast/repaint/repaint-during-scroll-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55159 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
@@ -430,6 +430,9 @@ struct WebHTMLViewInterpretKeyEventsParameters {
     BOOL exposeInputContext;
 
     NSPoint lastScrollPosition;
+#ifndef BUILDING_ON_TIGER
+    BOOL inScrollPositionChanged;
+#endif
 
     WebPluginController *pluginController;
     
@@ -1172,8 +1175,15 @@ static void _updateMouseoverTimerCallback(CFRunLoopTimerRef timer, void *info)
     NSPoint origin = [[self superview] bounds].origin;
     if (!NSEqualPoints(_private->lastScrollPosition, origin)) {
         if (Frame* coreFrame = core([self _frame])) {
-            if (FrameView* coreView = coreFrame->view())
+            if (FrameView* coreView = coreFrame->view()) {
+#ifndef BUILDING_ON_TIGER
+                _private->inScrollPositionChanged = YES;
+#endif
                 coreView->scrollPositionChanged();
+#ifndef BUILDING_ON_TIGER
+                _private->inScrollPositionChanged = NO;
+#endif
+            }
         }
     
         [_private->completionController endRevertingChange:NO moveLeft:NO];
@@ -3138,11 +3148,29 @@ WEBCORE_COMMAND(yankAndSelect)
     return [[self _webView] drawsBackground];
 }
 
+#if !LOG_DISABLED
 - (void)setNeedsDisplay:(BOOL)flag
 {
     LOG(View, "%@ setNeedsDisplay:%@", self, flag ? @"YES" : @"NO");
     [super setNeedsDisplay:flag];
 }
+#endif
+
+#ifndef BUILDING_ON_TIGER
+- (void)setNeedsDisplayInRect:(NSRect)invalidRect
+{
+    if (_private->inScrollPositionChanged) {
+        // When scrolling, the dirty regions are adjusted for the scroll only
+        // after NSViewBoundsDidChangeNotification is sent. Translate the invalid
+        // rect to pre-scrolled coordinates in order to get the right dirty region
+        // after adjustment. See <rdar://problem/7678927>.
+        NSPoint origin = [[self superview] bounds].origin;
+        invalidRect.origin.x -= _private->lastScrollPosition.x - origin.x;
+        invalidRect.origin.y -= _private->lastScrollPosition.y - origin.y;
+    }
+    [super setNeedsDisplayInRect:invalidRect];
+}
+#endif
 
 - (void)setNeedsLayout: (BOOL)flag
 {
