8a3543ea3b0d3b8b796af31d812f0c9602bf34ca
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-23 23:46:50 +0000

    linux: Show an icon to the right indicating if the host is valid or not.
    BUG=34181
    TEST=open the exceptions editor, type some text in the host textfield, see the indication.
    
    Review URL: http://codereview.chromium.org/650122
    Patch from thiago.farina@gmail.com.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39802 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/options/content_exception_editor.h
+++ chrome/browser/gtk/options/content_exception_editor.h
@@ -52,6 +52,10 @@ class ContentExceptionEditor {
   ContentSetting SettingForIndex(int index);
   int IndexForSetting(ContentSetting setting);
 
+  bool IsHostValid(const std::string& host) const;
+
+  void UpdateImage(GtkWidget* image, bool is_valid);
+
   // GTK callbacks
   static void OnEntryChanged(GtkEditable* entry,
                              ContentExceptionEditor* window);
@@ -73,6 +77,7 @@ class ContentExceptionEditor {
   // UI widgets.
   GtkWidget* dialog_;
   GtkWidget* entry_;
+  GtkWidget* host_image_;
   GtkWidget* action_combo_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentExceptionEditor);
