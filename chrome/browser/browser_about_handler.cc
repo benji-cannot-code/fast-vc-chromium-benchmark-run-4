5e9a4a6c35993884167700bc7bbf67212648aa43
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-23 18:25:17 +0000

    do not mention other browsers in about:memory for chromeos
    BUG=34839
    TEST=verify per bug report
    
    Review URL: http://codereview.chromium.org/650161
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39746 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_thread.h"
+#include "chrome/browser/defaults.h"
 #include "chrome/browser/dom_ui/chrome_url_data_manager.h"
 #include "chrome/browser/google_service_auth_error.h"
 #include "chrome/browser/memory_details.h"
@@ -843,6 +844,9 @@ void AboutMemoryHandler::OnDetailsAvailable() {
     else
       AppendProcess(child_data, &process.processes[index]);
   }
+
+  root.SetBoolean(L"show_other_browsers",
+      browser_defaults::kShowOtherBrowsersInAboutMemory);
 
   // Get about_memory.html
   static const base::StringPiece memory_html(
