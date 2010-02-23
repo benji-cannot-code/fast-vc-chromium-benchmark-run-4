0bb0a34a5a881c9786c193a061cc0598de5b3ea1
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-23 17:28:42 +0000

    Whitespace!
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/652127
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39740 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/nsmenuitem_additions.mm
+++ chrome/browser/cocoa/nsmenuitem_additions.mm
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #import "chrome/browser/cocoa/nsmenuitem_additions.h"
 
 #include <Carbon/Carbon.h>
@@ -98,4 +102,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 @end
-
