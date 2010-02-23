e9012b10cd2fd4dad644533bcc35d6b7c092958f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-23 13:12:15 +0000

    2010-02-23  Ben Murdoch  <benm@google.com>
            Reviewed by Eric Seidel.
    
            pageX/Y co-ordinates of TouchEvent should be 0,0
            https://bugs.webkit.org/show_bug.cgi?id=35239
    
            Update layout tests to reflect the WebCore changes in this bug.
    
            * fast/events/touch/basic-multi-touch-events-expected.txt: Updated.
            * fast/events/touch/basic-single-touch-events-expected.txt: Updated.
            * fast/events/touch/script-tests/basic-multi-touch-events.js:
            (verifyTouchEvent): Add checking of TouchEvent.pageX/Y
            * fast/events/touch/script-tests/basic-single-touch-events.js:
            (verifyTouchEvent): ditto.
    2010-02-23  Ben Murdoch  <benm@google.com>
    
            Reviewed by Eric Seidel.
    
            pageX/Y co-ordinates of TouchEvent should be 0,0
            https://bugs.webkit.org/show_bug.cgi?id=35239
    
            The co-ordinates attached to a touch event are not used for tracking touch motion, rather the co-ordinates attached to Touches within the touch event should be used. Set the co-ordinates on the event itself to 0 to avoid confusion and match observed iPhone behavior.
    
            * page/EventHandler.cpp:
            (WebCore::EventHandler::handleTouchEvent): Set the pageX/Y and screenX/Y co-ordinates attached to the touch event to 0,0 to match observed behavior on the iPhone.
            * page/EventHandler.h: Remove now unused members m_firstTouchScreenPos and m_firstTouchPagePos.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55146 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/WebCore/page/EventHandler.cpp
@@ -2590,8 +2590,6 @@ bool EventHandler::handleTouchEvent(const PlatformTouchEvent& event)
 
         if (event.type() == TouchStart && !i) {
             m_touchEventTarget = target;
-            m_firstTouchScreenPos = point.screenPos();
-            m_firstTouchPagePos = pagePoint;
         }
 
         if (point.state() == PlatformTouchPoint::TouchReleased)
@@ -2621,9 +2619,7 @@ bool EventHandler::handleTouchEvent(const PlatformTouchEvent& event)
         RefPtr<TouchEvent> cancelEv =
             TouchEvent::create(TouchList::create().get(), TouchList::create().get(), cancelTouches.get(),
                                                    *eventName, m_touchEventTarget->document()->defaultView(),
-                                                   m_firstTouchScreenPos.x(), m_firstTouchScreenPos.y(),
-                                                   m_firstTouchPagePos.x(), m_firstTouchPagePos.y(),
-                                                   event.ctrlKey(), event.altKey(), event.shiftKey(),
+                                                   0, 0, 0, 0, event.ctrlKey(), event.altKey(), event.shiftKey(),
                                                    event.metaKey());
 
         ExceptionCode ec = 0;
@@ -2636,9 +2632,7 @@ bool EventHandler::handleTouchEvent(const PlatformTouchEvent& event)
         RefPtr<TouchEvent> endEv = 
             TouchEvent::create(touches.get(), targetTouches.get(), releasedTouches.get(),
                                                    *eventName, m_touchEventTarget->document()->defaultView(),
-                                                   m_firstTouchScreenPos.x(), m_firstTouchScreenPos.y(),
-                                                   m_firstTouchPagePos.x(), m_firstTouchPagePos.y(),
-                                                   event.ctrlKey(), event.altKey(), event.shiftKey(),
+                                                   0, 0, 0, 0, event.ctrlKey(), event.altKey(), event.shiftKey(),
                                                    event.metaKey());
         ExceptionCode ec = 0;
         m_touchEventTarget->dispatchEvent(endEv.get(), ec);
@@ -2656,9 +2650,7 @@ bool EventHandler::handleTouchEvent(const PlatformTouchEvent& event)
         RefPtr<TouchEvent> startEv = 
             TouchEvent::create(touches.get(), targetTouches.get(), pressedTouches.get(),
                                                    *eventName, m_touchEventTarget->document()->defaultView(),
-                                                   m_firstTouchScreenPos.x(), m_firstTouchScreenPos.y(),
-                                                   m_firstTouchPagePos.x(), m_firstTouchPagePos.y(),
-                                                   event.ctrlKey(), event.altKey(), event.shiftKey(),
+                                                   0, 0, 0, 0, event.ctrlKey(), event.altKey(), event.shiftKey(),
                                                    event.metaKey());
         ExceptionCode ec = 0;
         m_touchEventTarget->dispatchEvent(startEv.get(), ec);
@@ -2669,9 +2661,7 @@ bool EventHandler::handleTouchEvent(const PlatformTouchEvent& event)
         RefPtr<TouchEvent> moveEv = 
             TouchEvent::create(touches.get(), targetTouches.get(), movedTouches.get(),
                                                    *eventName, m_touchEventTarget->document()->defaultView(),
-                                                   m_firstTouchScreenPos.x(), m_firstTouchScreenPos.y(),
-                                                   m_firstTouchPagePos.x(), m_firstTouchPagePos.y(),
-                                                   event.ctrlKey(), event.altKey(), event.shiftKey(),
+                                                   0, 0, 0, 0, event.ctrlKey(), event.altKey(), event.shiftKey(),
                                                    event.metaKey());
         ExceptionCode ec = 0;
         m_touchEventTarget->dispatchEvent(moveEv.get(), ec);
