342557374c7661726a4059d8534a87da5a6ad1c8
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-23 07:12:22 +0000

    Add MessageLoop::QuitNow and use it to terminate a sync IPC.
    This is a test to observe the performance impact of this change.
    As such, I'm reverting r39518 as part of this change.
    
    R=jam
    BUG=36310
    TEST=none
    
    Review URL: http://codereview.chromium.org/652089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39699 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_sync_channel.cc
+++ ipc/ipc_sync_channel.cc
@@ -351,7 +351,7 @@ void SyncChannel::SyncContext::OnWaitableEventSignaled(WaitableEvent* event) {
   } else {
     // We got the reply, timed out or the process shutdown.
     DCHECK(event == GetSendDoneEvent());
-    MessageLoop::current()->Quit();
+    MessageLoop::current()->QuitNow();
   }
 }
 
