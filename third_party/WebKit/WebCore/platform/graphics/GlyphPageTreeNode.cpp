3a62a104aadaeb6cd68445cf44613f85e0b64284
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-23 00:55:34 +0000

    2010-02-22  Stephan Aßmus  <superstippi@gmx.de>
            Reviewed by Eric Seidel.
    
            Build fix for debug builds of GlyphPageTreeNode.cpp
            https://bugs.webkit.org/show_bug.cgi?id=34528
    
            Covered by existing tests.
    
            * platform/graphics/GlyphPageTreeNode.cpp:
                include <stdio.h> since printf() is used if NDEBUG is not defined.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55113 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/GlyphPageTreeNode.cpp
+++ third_party/WebKit/WebCore/platform/graphics/GlyphPageTreeNode.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CharacterNames.h"
 #include "SegmentedFontData.h"
 #include "SimpleFontData.h"
+#include <stdio.h>
 #include <wtf/unicode/Unicode.h>
 
 namespace WebCore {
