881d069d7d7e68f96fba11312c8d76742be4d49c
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-23 15:21:04 +0000

    2010-02-23  Leandro Pereira  <leandro@profusion.mobi>
            Reviewed by Gustavo Noronha Silva.
    
            Fixes references to GOwnPtr and GRefPtr so the GTK+ port builds
            again.
            http://webkit.org/b/35084
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55150 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/DataObjectGtk.h
+++ third_party/WebKit/WebCore/platform/gtk/DataObjectGtk.h
@@ -22,11 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CString.h"
 #include "FileList.h"
+#include <GRefPtr.h>
 #include "KURL.h"
 #include "Range.h"
 #include "StringHash.h"
 #include <wtf/RefCounted.h>
-#include <wtf/gtk/GRefPtr.h>
 
 typedef struct _GdkPixbuf GdkPixbuf;
 typedef struct _GdkDragContext GdkDragContext;
