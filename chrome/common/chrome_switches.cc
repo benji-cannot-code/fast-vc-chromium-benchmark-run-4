6dd41347bcf899c55be81beca2be25055fa9c233
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-23 19:25:20 +0000

    Remove the --enable-new-autofill switch.  This enables the AutoFillManager to show the infobar asking the user to enable/disable AutoFill++.
    BUG=18201
    TEST=none
    Review URL: http://codereview.chromium.org/650169
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39755 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -257,9 +257,6 @@ const char kEnableNaClOnMac[]                    = "enable-nacl-on-mac";
 // Enable Native Web Worker support.
 const char kEnableNativeWebWorkers[]        = "enable-native-web-workers";
 
-// Enable AutoFill++.
-const char kEnableNewAutoFill[]             = "enable-new-autofill";
-
 // Enable Privacy Blacklists.
 const char kEnablePrivacyBlacklists[]       = "enable-privacy-blacklists";
 
@@ -731,7 +728,7 @@ const char kWorkerStartupDialog[]           = "worker-startup-dialog";
 const char kZygoteCmdPrefix[]               = "zygote-cmd-prefix";
 
 // Causes the process to run as a renderer zygote.
-const char kZygoteProcess[]                 = "zygote"; //
+const char kZygoteProcess[]                 = "zygote";
 
 #if defined(OS_CHROMEOS)
 // The name of the pipe over which the Chrome OS login manager will send
