8282602fa0d2eeef947140fa7bd39d49aedc8a4f
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-24 12:35:32 +0000

    Adds Geolocation browser test.
    Note: they're disabled on Mac pending my investigation (it seems to be a similar issue to http://code.google.com/p/chromium/issues/detail?id=29424).
    This will at least get some coverage for geolocation on win and linux for the time being.
    
    TEST=geolocation_browsertest.cc
    Review URL: http://codereview.chromium.org/650134
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39885 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/geolocation/geolocation_permission_context.cc
+++ chrome/browser/geolocation/geolocation_permission_context.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/render_view_host_notification_task.h"
 #include "chrome/browser/tab_contents/infobar_delegate.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/common/json_value_serializer.h"
 #include "chrome/common/render_messages.h"
 #include "grit/generated_resources.h"
@@ -218,20 +219,13 @@ void GeolocationPermissionContext::RequestPermissionFromUI(
   }
   DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
 
-  Browser* browser = BrowserList::GetLastActiveWithProfile(profile_);
-  for (int i = 0; i < browser->tab_count(); ++i) {
-    TabContents* tab_contents = browser->GetTabContentsAt(i);
-    RenderViewHost* render_view_host = tab_contents->render_view_host();
-    if (render_view_host->process()->id() == render_process_id &&
-        render_view_host->routing_id() == render_view_id &&
-        tab_contents->GetURL().GetOrigin() == origin) {
-      tab_contents->AddInfoBar(
-          new GeolocationConfirmInfoBarDelegate(
-              tab_contents, this, render_process_id, render_view_id,
-              bridge_id, origin));
-      break;
-    }
-  }
+  TabContents* tab_contents =
+      tab_util::GetTabContentsByID(render_process_id, render_view_id);
+  DCHECK(tab_contents);
+  tab_contents->AddInfoBar(
+      new GeolocationConfirmInfoBarDelegate(
+          tab_contents, this, render_process_id, render_view_id,
+          bridge_id, origin));
 }
 
 void GeolocationPermissionContext::NotifyPermissionSet(
