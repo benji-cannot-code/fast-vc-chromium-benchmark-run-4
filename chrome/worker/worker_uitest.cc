8cdd1265fea27983ff2c5eb7bb9133d748e47721
dimich@google.com <dimich@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-24 04:04:19 +0000

    Temporarily disable WorkerTest.WorkerCloseFast to investigate hang after r55180 upstream. TBR=levin BUG=36630 TEST=none Review URL: http://codereview.chromium.org/657063
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39864 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/worker/worker_uitest.cc
+++ chrome/worker/worker_uitest.cc
@@ -208,7 +208,8 @@ TEST_F(WorkerTest, FLAKY_WorkerClonePort) {
   RunWorkerFastLayoutTest("worker-cloneport.html");
 }
 
-TEST_F(WorkerTest, WorkerCloseFast) {
+// Hangs. http://crbug.com/36630
+TEST_F(WorkerTest, DISABLED_WorkerCloseFast) {
   RunWorkerFastLayoutTest("worker-close.html");
 }
 
