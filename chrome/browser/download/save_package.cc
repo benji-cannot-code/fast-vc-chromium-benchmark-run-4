425fa3d4eb7bbc6bcc2128aa9ec046011b8f1da7
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-24 19:29:28 +0000

    This change makes incognito downloads show up in the downloads page of an incognito window. These downloads are not persisted to history; hence they do not persist between incognito sessions.
    There is no special UI for the incognito downloads. That can be added later.
    
    BUG=28702
    
    Review URL: http://codereview.chromium.org/650065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39910 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/save_package.cc
+++ chrome/browser/download/save_package.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -305,7 +305,8 @@ bool SavePackage::Init() {
 
   // Create the fake DownloadItem and display the view.
   download_ = new DownloadItem(1, saved_main_file_path_, 0, page_url_, GURL(),
-      "", FilePath(), Time::Now(), 0, -1, -1, false, false, false, false);
+      "", FilePath(), Time::Now(), 0, -1, -1, false, false,
+      profile->IsOffTheRecord(), false, false);
   download_->set_manager(tab_contents_->profile()->GetDownloadManager());
   tab_contents_->OnStartDownload(download_);
 
