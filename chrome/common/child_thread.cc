d914f52bfa24a50878be867c448eac1111f92a0b
dimich@google.com <dimich@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-24 23:50:53 +0000

    Make sure the workers are given a chance to terminate their thread when the IPC channel fails. Usually, the ChildThread::OnChannelError() simply kills the message loop. we want to give workers an opportunity to get out of their threads to avoid crashes when main thread destroys globals. BUG=35963 TEST=WorkerTest.StressJSExecution Review URL: http://codereview.chromium.org/647064
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39951 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/child_thread.cc
+++ chrome/common/child_thread.cc
@@ -72,6 +72,7 @@ ChildThread::~ChildThread() {
 }
 
 void ChildThread::OnChannelError() {
+  set_on_channel_error_called(true);
   MessageLoop::current()->Quit();
 }
 
@@ -161,7 +162,7 @@ ChildThread* ChildThread::current() {
 }
 
 void ChildThread::OnProcessFinalRelease() {
-  if (!check_with_browser_before_shutdown_) {
+  if (on_channel_error_called_ || !check_with_browser_before_shutdown_) {
     MessageLoop::current()->Quit();
     return;
   }
--- chrome/worker/worker_thread.h
+++ chrome/worker/worker_thread.h
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_WORKER_WORKER_THREAD_H_
 #define CHROME_WORKER_WORKER_THREAD_H_
 
+#include <set>
+
 #include "chrome/common/child_thread.h"
 
 class GURL;
+class WebWorkerStubBase;
 class WorkerWebKitClientImpl;
 
 class WorkerThread : public ChildThread {
@@ -19,13 +22,21 @@ class WorkerThread : public ChildThread {
   // Returns the one worker thread.
   static WorkerThread* current();
 
+  // Invoked from stub constructors/destructors. Stubs own themselves.
+  void AddWorkerStub(WebWorkerStubBase* stub);
+  void RemoveWorkerStub(WebWorkerStubBase* stub);
+
  private:
   virtual void OnControlMessageReceived(const IPC::Message& msg);
+  virtual void OnChannelError();
 
   void OnCreateWorker(
       const GURL& url, bool is_shared, const string16& name, int route_id);
 
   scoped_ptr<WorkerWebKitClientImpl> webkit_client_;
+
+  typedef std::set<WebWorkerStubBase*> WorkerStubsList;
+  WorkerStubsList worker_stubs_;
 
   DISALLOW_COPY_AND_ASSIGN(WorkerThread);
 };
