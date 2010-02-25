d929cfe238b805404bf1803536a2a7892ddc6486
stoyan@chromium.org <stoyan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-25 23:59:57 +0000

    Give UITest ability to have MessageLoopforUI message loop instead of the default one. ExternalTabContainer tests create the host window and the automationproxy in the main thread, that will cause deadlock (especially when host window is visible). Added test for openening popup windows.
    TEST=ui_tests
    Review URL: http://codereview.chromium.org/661028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40072 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/ui_test.h
+++ chrome/test/ui/ui_test.h
@@ -54,6 +54,7 @@ class UITestBase {
 
   // Constructor
   UITestBase();
+  explicit UITestBase(MessageLoop::Type msg_loop_type);
 
   virtual ~UITestBase();
 
@@ -619,6 +620,10 @@ class UITestBase {
 
 class UITest : public UITestBase, public PlatformTest {
  protected:
+  UITest() {}
+  explicit UITest(MessageLoop::Type msg_loop_type)
+    : UITestBase(msg_loop_type), PlatformTest() {
+  }
   virtual void SetUp();
   virtual void TearDown();
 };
