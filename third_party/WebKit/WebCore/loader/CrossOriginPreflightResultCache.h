2591ac639835fec5273ed1a187c6fc4f72c5dc2c
jocelyn.turcotte@nokia.com <jocelyn.turcotte@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-25 16:05:19 +0000

    Add missing include guards
    Reviewed by Tor Arne Vestbø.
    
    * loader/CrossOriginPreflightResultCache.h:
    * loader/MainResourceLoader.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55237 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/CrossOriginPreflightResultCache.h
+++ third_party/WebKit/WebCore/loader/CrossOriginPreflightResultCache.h
@@ -25,6 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
+#ifndef CrossOriginPreflightResultCacheItem_h
+#define CrossOriginPreflightResultCacheItem_h
+
 #include "KURLHash.h"
 #include "StringHash.h"
 #include <wtf/HashMap.h>
@@ -78,3 +81,5 @@ namespace WebCore {
     };
 
 } // namespace WebCore
+
+#endif
--- third_party/WebKit/WebCore/loader/MainResourceLoader.h
+++ third_party/WebKit/WebCore/loader/MainResourceLoader.h
@@ -27,6 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#ifndef MainResourceLoader_h
+#define MainResourceLoader_h
+
 #include "FrameLoaderTypes.h"
 #include "ResourceLoader.h"
 #include "SubstituteData.h"
@@ -108,3 +111,5 @@ namespace WebCore {
     };
 
 }
+
+#endif
