fd2315c53041fc677be3c301a1be9c013eb4752b
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-25 17:13:25 +0000

    2010-02-24  Darin Fisher  <darin@chromium.org>
            Reviewed by David Levin.
    
            Add a missing WebString.h include.
            https://bugs.webkit.org/show_bug.cgi?id=35360
    
            This is required since some of the methods have implementations that
            return a WebString.
    
            * public/WebCookieJar.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55239 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebCookieJar.h
+++ third_party/WebKit/WebKit/chromium/public/WebCookieJar.h
@@ -32,8 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebCookieJar_h
 #define WebCookieJar_h
 
+#include "WebString.h"
+
 namespace WebKit {
-class WebString;
 class WebURL;
 struct WebCookie;
 template <typename T> class WebVector;
