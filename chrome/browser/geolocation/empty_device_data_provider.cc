7ad71069241c41fe3b6e38e0a8e293d2ec3b614f
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-25 10:15:23 +0000

    Port the gears OSX wifi data provider into chrome Pulls in the custom header file uesd to access these functions in gears
    BUG=11246
    TEST=unit_tests.exe --gtest_filer=Geoloc* 
    
    Review URL: http://codereview.chromium.org/600146
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40004 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/geolocation/empty_device_data_provider.cc
+++ chrome/browser/geolocation/empty_device_data_provider.cc
@@ -5,14 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/geolocation/empty_device_data_provider.h"
 
+// No platform has (cellular) radio data provider yet.
 // static
 template<>
 RadioDataProviderImplBase* RadioDataProvider::DefaultFactoryFunction() {
   return new EmptyDeviceDataProvider<RadioData>();
 }
 
-// Windows has a real wifi data provider.
-#if !defined(OS_WIN)
+// Only define for platforms that lack a real wifi data provider.
+#if !defined(OS_WIN) && !defined(OS_MACOSX)
 // static
 template<>
 WifiDataProviderImplBase* WifiDataProvider::DefaultFactoryFunction() {
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -978,6 +978,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/geolocation/network_location_provider.h',
         'browser/geolocation/network_location_request.cc',
         'browser/geolocation/network_location_request.h',
+        'browser/geolocation/osx_wifi.h',
         'browser/geolocation/wifi_data_provider_common.cc',
         'browser/geolocation/wifi_data_provider_common.h',
         'browser/geolocation/wifi_data_provider_common_win.cc',
