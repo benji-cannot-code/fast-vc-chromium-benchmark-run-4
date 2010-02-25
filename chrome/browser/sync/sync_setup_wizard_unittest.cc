e723853b91ead25c13f7a5c8cb6009401f958180
skrul@chromium.org <skrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-25 00:42:55 +0000

    Move data type start/stop management into DataTypeManager This change introduces a new interface/class called DataTypeManager whose job is to choreograph the start up and shut down of all registered data types.  It starts each data type serially, waiting for each data type to finish starting before starting the next one.  If anything goes wrong on startup, all data types are stopped.
    Note that this change also simplifies the ProfileSyncServiceStartupTest as many of the cases it tested for are now the responsibility of the DataTypeManagerImpl (and these are thoroughly tested in its unit test).
    
    I also killed off the TestProfileSyncFactory in the ProfileSyncServiceTest since it was lame and could be done with the mock.
    
    BUG=36506
    
    Review URL: http://codereview.chromium.org/650175
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39964 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/sync_setup_wizard_unittest.cc
+++ chrome/browser/sync/sync_setup_wizard_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_list.h"
 #include "chrome/browser/google_service_auth_error.h"
 #include "chrome/browser/pref_service.h"
+#include "chrome/browser/sync/profile_sync_factory_mock.h"
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/sync_setup_flow.h"
 #include "chrome/browser/sync/sync_setup_wizard.h"
@@ -30,8 +31,8 @@ typedef GoogleServiceAuthError AuthError;
 // A PSS subtype to inject.
 class ProfileSyncServiceForWizardTest : public ProfileSyncService {
  public:
-  explicit ProfileSyncServiceForWizardTest(Profile* profile)
-      : ProfileSyncService(profile, false),
+  ProfileSyncServiceForWizardTest(ProfileSyncFactory* factory, Profile* profile)
+      : ProfileSyncService(factory, profile, false),
         user_accepted_merge_and_sync_(false),
         user_cancelled_dialog_(false) {
     RegisterPreferences();
@@ -84,13 +85,14 @@ class ProfileSyncServiceForWizardTest : public ProfileSyncService {
 class TestingProfileWithSyncService : public TestingProfile {
  public:
   TestingProfileWithSyncService() {
-    sync_service_.reset(new ProfileSyncServiceForWizardTest(this));
+    sync_service_.reset(new ProfileSyncServiceForWizardTest(&factory_, this));
   }
 
   virtual ProfileSyncService* GetProfileSyncService() {
     return sync_service_.get();
   }
  private:
+  ProfileSyncFactoryMock factory_;
   scoped_ptr<ProfileSyncService> sync_service_;
 };
 
