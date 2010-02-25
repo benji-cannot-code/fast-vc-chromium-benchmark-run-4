3522817479b9cd446067342725418ab3936e2b9f
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-25 18:22:12 +0000

    Remove submit handling in FillFormImpl.  The one caller passes false to |submit|, so this is essentially dead code.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/660059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40033 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/dom_operations.cc
+++ webkit/glue/dom_operations.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -171,7 +171,7 @@ struct FormElements {
 typedef std::vector<FormElements*> FormElementsList;
 
 // Internal implementation of FillForm API.
-static bool FillFormImpl(FormElements* fe, const FormData& data, bool submit) {
+static bool FillFormImpl(FormElements* fe, const FormData& data) {
   if (!fe->form_element.autoComplete())
     return false;
 
@@ -180,14 +180,8 @@ static bool FillFormImpl(FormElements* fe, const FormData& data, bool submit) {
     data_map[data.elements[i]] = data.values[i];
   }
 
-  bool submit_found = false;
   for (FormInputElementMap::iterator it = fe->input_elements.begin();
        it != fe->input_elements.end(); ++it) {
-    if (it->first == data.submit) {
-      it->second.setActivatedSubmit(true);
-      submit_found = true;
-      continue;
-    }
     if (!it->second.value().isEmpty())  // Don't overwrite pre-filled values.
       continue;
     it->second.setValue(data_map[it->first]);
@@ -195,10 +189,6 @@ static bool FillFormImpl(FormElements* fe, const FormData& data, bool submit) {
     it->second.dispatchFormControlChangeEvent();
   }
 
-  if (submit && submit_found) {
-    fe->form_element.submit();
-    return true;
-  }
   return false;
 }
 
@@ -289,11 +279,10 @@ void FillPasswordForm(WebView* view,
     // FormElementsList use that.
     scoped_ptr<FormElements> form_elements(*iter);
 
-    // False param to FillFormByAction is so we don't auto-submit password
-    // forms. If wait_for_username is true, we don't want to initially fill
-    // the form until the user types in a valid username.
+    // If wait_for_username is true, we don't want to initially fill the form
+    // until the user types in a valid username.
     if (!data.wait_for_username)
-      FillFormImpl(form_elements.get(), data.basic_data, false);
+      FillFormImpl(form_elements.get(), data.basic_data);
 
     // Attach autocomplete listener to enable selecting alternate logins.
     // First, get pointers to username element.
