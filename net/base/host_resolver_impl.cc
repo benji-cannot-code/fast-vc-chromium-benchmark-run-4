938f502c7f9459db36e9d8585b5655b0f1f53680
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-25 07:40:28 +0000

    Revert 39996 - Refine IPv6 probe to require that the client has an IPv6 address on an interface
    This currently only works on Posix, not windows.
    
    Network changes are monitored, and the test is repeated each time interfaces
    change (which is a subset of any IP addresses changing).
    
    The test performed is still relatively low latency, and we *may* need
    to eventually move to an high latency test, such as a DNS resolution,
    or an actual test connection.  If we move in that direction, then we'll
    need to post a task to perform the work, rather than immediately returning.
    
    BUG=25680
    BUG=12754
    r=wtc,eroman
    Review URL: http://codereview.chromium.org/652072
    
    TBR=jar@chromium.org
    Review URL: http://codereview.chromium.org/660073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39998 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_resolver_impl.cc
+++ net/base/host_resolver_impl.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/host_resolver_proc.h"
 #include "net/base/load_log.h"
 #include "net/base/net_errors.h"
-#include "net/base/net_util.h"
 #include "net/base/network_change_notifier.h"
 
 #if defined(OS_WIN)
@@ -577,8 +576,7 @@ HostResolverImpl::HostResolverImpl(
       resolver_proc_(resolver_proc),
       default_address_family_(ADDRESS_FAMILY_UNSPECIFIED),
       shutdown_(false),
-      network_change_notifier_(network_change_notifier),
-      ipv6_probe_monitoring_(false) {
+      network_change_notifier_(network_change_notifier) {
   DCHECK_GT(max_jobs, 0u);
 
   // It is cumbersome to expose all of the constraints in the constructor,
@@ -737,17 +735,6 @@ void HostResolverImpl::RemoveObserver(HostResolver::Observer* observer) {
   DCHECK(it != observers_.end());
 
   observers_.erase(it);
-}
-
-void HostResolverImpl::SetDefaultAddressFamily(AddressFamily address_family) {
-  ipv6_probe_monitoring_ = false;
-  default_address_family_ = address_family;
-}
-
-void HostResolverImpl::ProbeIPv6Support() {
-  DCHECK(!ipv6_probe_monitoring_);
-  ipv6_probe_monitoring_ = true;
-  OnIPAddressChanged();  // Give initial setup call.
 }
 
 void HostResolverImpl::Shutdown() {
@@ -991,14 +978,6 @@ void HostResolverImpl::OnCancelRequest(LoadLog* load_log,
 void HostResolverImpl::OnIPAddressChanged() {
   if (cache_.get())
     cache_->clear();
-  if (ipv6_probe_monitoring_) {
-    bool support = IPv6Supported();
-    default_address_family_ = support ? ADDRESS_FAMILY_UNSPECIFIED
-                                      : ADDRESS_FAMILY_IPV4;
-    LOG(INFO) << "IPv6Probe forced AddressFamily setting to "
-              << (support ? "ADDRESS_FAMILY_UNSPECIFIED"
-                          : "ADDRESS_FAMILY_IPV4");
-  }
 }
 
 // static
