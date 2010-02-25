f7670d32f615ceb03568c119613bda67e2ffe8a9
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-25 04:15:31 +0000

    Unreviewed build fix.
    Fix gtk build.
    
    Include JSC headers as runtime/ instead of JavaScriptCore/
    
    * bindings/js/JSDOMWrapper.h:
    * bindings/js/ScriptWrappable.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55216 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/ScriptWrappable.h
+++ third_party/WebKit/WebCore/bindings/js/ScriptWrappable.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ScriptWrappable_h
 
 #include "JSDOMWrapper.h"
-#include <JavaScriptCore/WeakGCPtr.h>
+#include <runtime/WeakGCPtr.h>
 
 namespace WebCore {
 
