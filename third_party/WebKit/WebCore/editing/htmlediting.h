f55d3f5cafa46983a42633179262775f81edb145
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-26 08:39:22 +0000

    Unreviewed. Roll-out r55263 because it broke fast/forms/textarea-type-spaces-pretty-diff.html. https://bugs.webkit.org/show_bug.cgi?id=30946
    WebCore: 
    
    * editing/CompositeEditCommand.cpp:
    (WebCore::isWhitespace):
    (WebCore::CompositeEditCommand::rebalanceWhitespaceAt):
    * editing/InsertTextCommand.cpp:
    (WebCore::InsertTextCommand::input):
    * editing/InsertTextCommand.h:
    * editing/htmlediting.cpp:
    * editing/htmlediting.h:
    
    LayoutTests: 
    
    * editing/execCommand/5142012-3-expected.txt:
    * editing/inserting/rebalance-whitespace-1-expected.txt: Removed.
    * editing/inserting/rebalance-whitespace-1.html: Removed.
    * editing/pasteboard/5521237-expected.txt:
    * platform/mac/editing/execCommand/5482023-expected.checksum:
    * platform/mac/editing/execCommand/5482023-expected.png: Added.
    * platform/mac/editing/execCommand/5482023-expected.txt:
    * platform/mac/editing/execCommand/remove-formatting-2-expected.checksum:
    * platform/mac/editing/execCommand/remove-formatting-2-expected.png: Added.
    * platform/mac/editing/execCommand/remove-formatting-expected.checksum:
    * platform/mac/editing/execCommand/remove-formatting-expected.png: Added.
    * platform/mac/editing/execCommand/remove-formatting-expected.txt:
    * platform/mac/editing/inserting/editable-html-element-expected.checksum:
    * platform/mac/editing/inserting/editable-html-element-expected.png: Added.
    * platform/mac/editing/inserting/editable-html-element-expected.txt:
    * platform/mac/editing/pasteboard/4989774-expected.checksum:
    * platform/mac/editing/pasteboard/4989774-expected.png: Added.
    * platform/mac/editing/pasteboard/4989774-expected.txt:
    * platform/mac/editing/selection/4983858-expected.checksum:
    * platform/mac/editing/selection/4983858-expected.png: Added.
    * platform/mac/editing/selection/4983858-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/htmlediting.h
+++ third_party/WebKit/WebCore/editing/htmlediting.h
@@ -41,7 +41,6 @@ class Node;
 class Position;
 class Range;
 class String;
-class Text;
 class VisiblePosition;
 class VisibleSelection;
 
@@ -231,7 +230,6 @@ VisibleSelection selectionForParagraphIteration(const VisibleSelection&);
 // Miscellaneous functions on String
     
 String stringWithRebalancedWhitespace(const String&, bool, bool);
-bool extentOfWhitespaceForRebalancingAt(PassRefPtr<Text> node, unsigned offset, unsigned& start, unsigned& end);
 const String& nonBreakingSpaceString();
 bool validBlockTag(const AtomicString&);
     
