12ab6bbf3a97de7211f27927fd998339ad377087
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-26 18:43:09 +0000

    fix bug where translate infobar gets stuck at "Loading..." between switching tabs.
    - problem happened because infobars are destructed when switching between tabs, only the delegates persist.  without the infobar to receive the PAGE_TRANSLATED notification to update the translate state, the infobar gets stuck at translating state when user switches back to the associated tab.
    - fix was to update the state in the infobar delegate on receiving PAGE_TRANSLATE notification so that when a new translate infobar is created, its visual display will reflect the updated after-translate state.
    
    BUG=36895
    TEST=verify per bug report.
    
    Review URL: http://codereview.chromium.org/660187
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40135 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_infobars_delegates.cc
+++ chrome/browser/translate/translate_infobars_delegates.cc
@@ -98,6 +98,8 @@ void TranslateInfoBarDelegate::GetAvailableTargetLanguages(
 }
 
 void TranslateInfoBarDelegate::Translate() {
+  if (state_ == kBeforeTranslate)
+    UpdateState(kTranslating);
   tab_contents_->TranslatePage(original_lang_code(), target_lang_code());
 }
 
