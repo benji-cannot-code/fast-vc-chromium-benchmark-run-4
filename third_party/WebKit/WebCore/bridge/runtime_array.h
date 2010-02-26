2bdb04e3aa4b15b39a28ae03eef9073fd9a6f518
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-26 00:22:34 +0000

    Reviewed by Anders Carlsson.
            https://bugs.webkit.org/show_bug.cgi?id=35406
            <rdar://problem/6945502> Make generic array methods work with JavaArray
    
            Test: java/array-sort.html
    
            Made RuntimeArray inherit from JSArray, keeping the promise given in ClassInfo.
    
            * bridge/runtime_array.cpp:
            (JSC::RuntimeArray::RuntimeArray):
            (JSC::RuntimeArray::~RuntimeArray):
            * bridge/runtime_array.h:
            (JSC::RuntimeArray::classInfo):
            (JSC::RuntimeArray::getConcreteArray):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55262 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/runtime_array.h
+++ third_party/WebKit/WebCore/bridge/runtime_array.h
@@ -28,29 +28,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RUNTIME_ARRAY_H_
 
 #include "Bridge.h"
-#include <runtime/JSGlobalObject.h>
+#include <runtime/ArrayPrototype.h>
 
 namespace JSC {
     
-class RuntimeArray : public JSObject {
+class RuntimeArray : public JSArray {
 public:
     RuntimeArray(ExecState*, Bindings::Array*);
+    virtual ~RuntimeArray();
 
     virtual void getOwnPropertyNames(ExecState*, PropertyNameArray&, EnumerationMode mode = ExcludeDontEnumProperties);
-    virtual bool getOwnPropertySlot(ExecState *, const Identifier&, PropertySlot&);
-    virtual bool getOwnPropertySlot(ExecState *, unsigned, PropertySlot&);
-    virtual bool getOwnPropertyDescriptor(ExecState *, const Identifier&, PropertyDescriptor&);
+    virtual bool getOwnPropertySlot(ExecState*, const Identifier&, PropertySlot&);
+    virtual bool getOwnPropertySlot(ExecState*, unsigned, PropertySlot&);
+    virtual bool getOwnPropertyDescriptor(ExecState*, const Identifier&, PropertyDescriptor&);
     virtual void put(ExecState*, const Identifier& propertyName, JSValue, PutPropertySlot&);
     virtual void put(ExecState*, unsigned propertyName, JSValue);
     
-    virtual bool deleteProperty(ExecState *exec, const Identifier &propertyName);
-    virtual bool deleteProperty(ExecState *exec, unsigned propertyName);
+    virtual bool deleteProperty(ExecState* exec, const Identifier &propertyName);
+    virtual bool deleteProperty(ExecState* exec, unsigned propertyName);
     
-    virtual const ClassInfo *classInfo() const { return &s_info; }
+    virtual const ClassInfo* classInfo() const { return &s_info; }
     
     unsigned getLength() const { return getConcreteArray()->getLength(); }
     
-    Bindings::Array *getConcreteArray() const { return _array.get(); }
+    Bindings::Array* getConcreteArray() const { return static_cast<Bindings::Array*>(subclassData()); }
 
     static const ClassInfo s_info;
 
@@ -59,17 +60,10 @@ public:
         return globalObject->arrayPrototype();
     }
 
-    static PassRefPtr<Structure> createStructure(JSValue prototype)
-    {
-        return Structure::create(prototype, TypeInfo(ObjectType, StructureFlags), AnonymousSlotCount);
-    }
-
 private:
     static const unsigned StructureFlags = OverridesGetOwnPropertySlot | OverridesGetPropertyNames | JSObject::StructureFlags;
     static JSValue lengthGetter(ExecState*, const Identifier&, const PropertySlot&);
     static JSValue indexGetter(ExecState*, const Identifier&, const PropertySlot&);
-
-    OwnPtr<Bindings::Array> _array;
 };
     
 } // namespace JSC
