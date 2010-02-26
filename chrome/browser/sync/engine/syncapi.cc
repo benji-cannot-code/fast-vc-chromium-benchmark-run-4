0f30276ce67087e9d70c167d22d420f6c33a3f21
chron@google.com <chron@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-26 23:43:05 +0000

    Use syncable hashes that are base64.
    Review URL: http://codereview.chromium.org/660209
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40179 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/syncapi.cc
+++ chrome/browser/sync/engine/syncapi.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/base64.h"
 #include "base/lock.h"
 #include "base/platform_thread.h"
 #include "base/scoped_ptr.h"
@@ -400,7 +401,10 @@ std::string BaseNode::GenerateSyncableHash(
   std::string hash_input;
   serialized_type.AppendToString(&hash_input);
   hash_input.append(client_tag);
-  return HexEncode(hash_input.data(), hash_input.length());
+
+  std::string encode_output;
+  CHECK(base::Base64Encode(base::SHA1HashString(hash_input), &encode_output));
+  return encode_output;
 }
 
 int64 BaseNode::GetParentId() const {
