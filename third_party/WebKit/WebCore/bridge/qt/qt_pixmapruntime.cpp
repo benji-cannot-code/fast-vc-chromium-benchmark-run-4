9af4569f898d0eea7d37644e5398f8b2d8423279
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-26 22:53:39 +0000

    * bridge/qt/qt_pixmapruntime.cpp: (JSC::Bindings::QtPixmapInstance::invokeMethod):
    Reviewed by NOBODY (Build fix following r55312).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_pixmapruntime.cpp
+++ third_party/WebKit/WebCore/bridge/qt/qt_pixmapruntime.cpp
@@ -187,7 +187,7 @@ JSValue QtPixmapInstance::getMethod(ExecState* exec, const Identifier& propertyN
 
 JSValue QtPixmapInstance::invokeMethod(ExecState* exec, RuntimeMethod* runtimeMethod, const ArgList& args)
 {
-    const MethodList& methodList = *runtimeMethod->methods();
+    const MethodList& methods = *runtimeMethod->methods();
 
     if (methods.size() == 1) {
         QtPixmapRuntimeMethod* method = static_cast<QtPixmapRuntimeMethod*>(methods[0]);
