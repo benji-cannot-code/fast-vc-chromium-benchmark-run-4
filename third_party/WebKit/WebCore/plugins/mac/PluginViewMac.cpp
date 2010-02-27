e47fcc3ce8275c3e1c43e44ee3b91923767e63d6
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-02-27 02:43:10 +0000

    2010-02-26  Luiz Agostini  <luiz.agostini@openbossa.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            Qt/Mac: Revert null timer removal (r51105)
            https://bugs.webkit.org/show_bug.cgi?id=35396
    
            r51105 removed the null timer event of the carbon event model. This however
            breaks the flash of many sites.
    
            Applying patch suggested by Girish Ramakrishnan in bug comment #1.
    
            * plugins/PluginView.h:
            * plugins/mac/PluginViewMac.cpp:
            (WebCore::PluginView::platformStart):
            (WebCore::PluginView::handleMouseEvent):
            (WebCore::PluginView::nullEventTimerFired):
            (WebCore::PluginView::mousePosForPlugin):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55332 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/mac/PluginViewMac.cpp
+++ third_party/WebKit/WebCore/plugins/mac/PluginViewMac.cpp
@@ -220,6 +220,12 @@ bool PluginView::platformStart()
 
     show();
 
+    // TODO: Implement null timer throttling depending on plugin activation
+    m_nullEventTimer.set(new Timer<PluginView>(this, &PluginView::nullEventTimerFired));
+    m_nullEventTimer->startRepeating(0.02);
+
+    m_lastMousePos.h = m_lastMousePos.v = 0;
+
     return true;
 }
 
@@ -579,7 +585,9 @@ void PluginView::handleMouseEvent(MouseEvent* event)
     EventRecord record;
 
     if (event->type() == eventNames().mousemoveEvent) {
-        record.what = nullEvent;
+        // Mouse movement is handled by null timer events
+        m_lastMousePos = mousePosForPlugin(event);
+        return;
     } else if (event->type() == eventNames().mouseoverEvent) {
         record.what = adjustCursorEvent;
     } else if (event->type() == eventNames().mouseoutEvent) {
@@ -595,21 +603,7 @@ void PluginView::handleMouseEvent(MouseEvent* event)
     } else {
         return;
     }
-
-    if (platformPluginWidget()) {
-        record.where = globalMousePosForPlugin();
-    } else {
-        if (event->button() == 2) {
-            // always pass the global position for right-click since Flash uses it to position the context menu
-            record.where = globalMousePosForPlugin();
-        } else {
-            IntPoint postZoomPos = roundedIntPoint(m_element->renderer()->absoluteToLocal(event->absoluteLocation()));
-            record.where.h = postZoomPos.x() + m_windowRect.x();
-            // The number 22 is the height of the title bar. As to why it figures in the calculation below 
-            // is left as an exercise to the reader :-)
-            record.where.v = postZoomPos.y() + m_windowRect.y() - 22;
-        }
-    }
+    record.where = mousePosForPlugin(event);
     record.modifiers = modifiersForEvent(event);
 
     if (!event->buttonDown())
@@ -701,6 +695,22 @@ void PluginView::handleKeyboardEvent(KeyboardEvent* event)
         event->setDefaultHandled();
 }
 
+void PluginView::nullEventTimerFired(Timer<PluginView>*)
+{
+    EventRecord record;
+
+    record.what = nullEvent;
+    record.message = 0;
+    record.when = TickCount();
+    record.where = m_lastMousePos;
+    record.modifiers = GetCurrentKeyModifiers();
+    if (!Button())
+        record.modifiers |= btnState;
+
+    if (!dispatchNPEvent(record))
+        LOG(Events, "PluginView::nullEventTimerFired(): Null event not accepted");
+}
+
 static int modifiersForEvent(UIEventWithKeyState* event)
 {
     int modifiers = 0;
@@ -754,6 +764,26 @@ Point PluginView::globalMousePosForPlugin() const
     pos.v -= top;
 #endif
 
+    return pos;
+}
+
+Point PluginView::mousePosForPlugin(MouseEvent* event) const
+{
+    ASSERT(event);
+    if (platformPluginWidget())
+        return globalMousePosForPlugin();
+
+    if (event->button() == 2) {
+        // always pass the global position for right-click since Flash uses it to position the context menu
+        return globalMousePosForPlugin();
+    }
+
+    Point pos;
+    IntPoint postZoomPos = roundedIntPoint(m_element->renderer()->absoluteToLocal(event->absoluteLocation()));
+    pos.h = postZoomPos.x() + m_windowRect.x();
+    // The number 22 is the height of the title bar. As to why it figures in the calculation below
+    // is left as an exercise to the reader :-)
+    pos.v = postZoomPos.y() + m_windowRect.y() - 22;
     return pos;
 }
 
