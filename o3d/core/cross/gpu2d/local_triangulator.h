656788560121c13df55d212c50982ca188098adc
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-27 01:23:12 +0000

    Incorporating change missed in previous CL (http://codereview.chromium.org/652016).
    Review URL: http://codereview.chromium.org/661232
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40191 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/gpu2d/local_triangulator.h
+++ o3d/core/cross/gpu2d/local_triangulator.h
@@ -193,7 +193,7 @@ class LocalTriangulator {
                    bool fill_right_side);
 
   // Number of triangles computed by Triangulate().
-  int num_triangles() {
+  int num_triangles() const {
     return num_triangles_;
   }
 
@@ -206,7 +206,7 @@ class LocalTriangulator {
 
   // Number of vertices facing the inside of the shape, if
   // compute_inside_edges was true when Triangulate() was called.
-  int num_interior_vertices() {
+  int num_interior_vertices() const {
     return num_interior_vertices_;
   }
 
