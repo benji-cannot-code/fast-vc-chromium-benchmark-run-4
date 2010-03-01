09cc0f7caa537433b5c278efcda41004b6c0200f
cira@google.com <cira@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-01 22:53:43 +0000

    Revert 40305 - Fix for cases when info bar is stealing focus on Windows
    Breaking Mac 10.5 tests.
    
    TEST=in the bug
    BUG=36943
    Review URL: http://codereview.chromium.org/660218
    
    TBR=georgey@chromium.org
    Review URL: http://codereview.chromium.org/661312
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40313 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/infobars/infobars.cc
+++ chrome/browser/views/infobars/infobars.cc
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
 #include "app/slide_animation.h"
-#if defined(OS_WIN)
-#include "app/win_util.h"
-#endif  // defined(OS_WIN)
 #include "base/message_loop.h"
 #include "chrome/browser/views/event_utils.h"
 #include "chrome/browser/views/infobars/infobar_container.h"
@@ -141,16 +138,7 @@ void InfoBar::Open() {
 }
 
 void InfoBar::AnimateClose() {
-  bool restore_focus = true;
-#if defined(OS_WIN)
-  // Do not restore focus (and active state with it) on Windows if some other
-  // top-level window became active.
-  if (GetWidget() &&
-      !win_util::DoesWindowBelongToActiveWindow(GetWidget()->GetNativeView())) {
-    restore_focus = false;
-  }
-#endif  // defined(OS_WIN)
-  DestroyFocusTracker(restore_focus);
+  DestroyFocusTracker(true);
   animation_->Hide();
 }
 
