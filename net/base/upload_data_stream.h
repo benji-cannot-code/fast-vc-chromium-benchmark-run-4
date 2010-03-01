a2f63ae6ee6b64c39fd89b3db6156542f321076c
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-01 20:01:24 +0000

    Revert "Fix the case where the browser livelocks if we cannot open a file."
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40291 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/upload_data_stream.h
+++ net/base/upload_data_stream.h
@@ -15,7 +15,7 @@ class IOBuffer;
 
 class UploadDataStream {
  public:
-  explicit UploadDataStream(UploadData* data);
+  explicit UploadDataStream(const UploadData* data);
   ~UploadDataStream();
 
   // Returns the stream's buffer and buffer length.
@@ -43,7 +43,7 @@ class UploadDataStream {
   // left to fill the buffer with.
   void FillBuf();
 
-  UploadData* data_;
+  const UploadData* data_;
 
   // This buffer is filled with data to be uploaded.  The data to be sent is
   // always at the front of the buffer.  If we cannot send all of the buffer at
