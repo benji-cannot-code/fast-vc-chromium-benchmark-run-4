ce603233afeadf6fd7cb0be1e738ba224a404c67
cjerdonek@webkit.org <cjerdonek@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-01 09:19:11 +0000

    Unreviewed. Rolling out r55350: http://trac.webkit.org/changeset/55350
    https://bugs.webkit.org/show_bug.cgi?id=33639
    
    Need to roll out because this patch (pep8) depends on the newly rewritten
    autoinstall.py (r55348), which is breaking for people with Python 2.4:
    
    https://bugs.webkit.org/show_bug.cgi?id=35163#c21
    
    That also needs to be rolled out and will be rolled out next.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/style/checker.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/style/checker.py
@@ -43,7 +43,6 @@ from optparser import DefaultCommandOptionValues
 from processors.common import check_no_carriage_return
 from processors.common import categories as CommonCategories
 from processors.cpp import CppProcessor
-from processors.python import PythonProcessor
 from processors.text import TextProcessor
 
 
@@ -232,8 +231,7 @@ class FileType:
     NONE = 1
     # Alphabetize remaining types
     CPP = 2
-    PYTHON = 3
-    TEXT = 4
+    TEXT = 3
 
 
 class ProcessorDispatcher(object):
@@ -254,6 +252,7 @@ class ProcessorDispatcher(object):
         'mm',
         'php',
         'pm',
+        'py',
         'txt',
         )
 
@@ -287,8 +286,6 @@ class ProcessorDispatcher(object):
             # reading from stdin, cpp_style tests should not rely on
             # the extension.
             return FileType.CPP
-        elif file_extension == "py":
-            return FileType.PYTHON
         elif ("ChangeLog" in file_path
               or "WebKitTools/Scripts/" in file_path
               or file_extension in self.text_file_extensions):
@@ -303,8 +300,6 @@ class ProcessorDispatcher(object):
         elif file_type == FileType.CPP:
             file_extension = self._file_extension(file_path)
             processor = CppProcessor(file_path, file_extension, handle_style_error, verbosity)
-        elif file_type == FileType.PYTHON:
-            processor = PythonProcessor(file_path, handle_style_error)
         elif file_type == FileType.TEXT:
             processor = TextProcessor(file_path, handle_style_error)
         else:
