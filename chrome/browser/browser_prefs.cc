6d7d872d6fc79d5a6f48e969c8fb4b7b9c919546
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-02 19:55:45 +0000

    Rename FormFieldHistoryManager to AutocompleteHistory manager to better reflect its relation to the Autocomplete feature.
    BUG=33032
    TEST=none
    Review URL: http://codereview.chromium.org/661323
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40408 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_prefs.cc
+++ chrome/browser/browser_prefs.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_prefs.h"
 
+#include "chrome/browser/autocomplete_history_manager.h"
 #include "chrome/browser/autofill/autofill_manager.h"
 #include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/browser.h"
@@ -15,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_dom_ui.h"
 #include "chrome/browser/extensions/extensions_ui.h"
 #include "chrome/browser/external_protocol_handler.h"
-#include "chrome/browser/form_field_history_manager.h"
 #include "chrome/browser/geolocation/geolocation_prefs.h"
 #include "chrome/browser/google_url_tracker.h"
 #include "chrome/browser/host_content_settings_map.h"
@@ -97,7 +97,7 @@ void RegisterUserPrefs(PrefService* user_prefs) {
   DownloadManager::RegisterUserPrefs(user_prefs);
   SSLManager::RegisterUserPrefs(user_prefs);
   bookmark_utils::RegisterUserPrefs(user_prefs);
-  FormFieldHistoryManager::RegisterUserPrefs(user_prefs);
+  AutocompleteHistoryManager::RegisterUserPrefs(user_prefs);
   TabContents::RegisterUserPrefs(user_prefs);
   TemplateURLPrepopulateData::RegisterUserPrefs(user_prefs);
   ExtensionDOMUI::RegisterUserPrefs(user_prefs);
--- chrome/browser/tab_contents/tab_contents.h
+++ chrome/browser/tab_contents/tab_contents.h
@@ -70,7 +70,7 @@ class BlockedPopupContainer;
 class DOMUI;
 class DownloadItem;
 class Extension;
-class FormFieldHistoryManager;
+class AutocompleteHistoryManager;
 class LoadNotificationDetails;
 class OmniboxSearchHint;
 class PasswordManager;
@@ -994,8 +994,8 @@ class TabContents : public PageNavigator,
   // SavePackage, lazily created.
   scoped_refptr<SavePackage> save_package_;
 
-  // FormFieldHistoryManager, lazily created.
-  scoped_ptr<FormFieldHistoryManager> form_field_history_manager_;
+  // AutocompleteHistoryManager, lazily created.
+  scoped_ptr<AutocompleteHistoryManager> autocomplete_history_manager_;
 
   // AutoFillManager, lazily created.
   scoped_ptr<AutoFillManager> autofill_manager_;
