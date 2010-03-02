d8c9c8c73c25f191a7b9a573725015e0b3dcfda6
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-02 04:10:59 +0000

    RSS feed subscription localization, part 2.
    Re-enabling the tests that were disabled in the first pass.
    
    Also changing how we substitute localization values (using direct method that avoids flashing __MSG__const while the page is loading).
    
    Also made some minor fixes after testing various locales.
    
    BUG=37109
    TEST=This CL is about enabling automated tests.
    
    Review URL: http://codereview.chromium.org/660328
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40354 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui_test_utils.cc
+++ chrome/test/ui_test_utils.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -260,6 +260,26 @@ class SimpleNotificationObserver : public NotificationObserver {
   DISALLOW_COPY_AND_ASSIGN(SimpleNotificationObserver);
 };
 
+class TabParentedNotificationObserver : public NotificationObserver {
+ public:
+  TabParentedNotificationObserver() {
+    registrar_.Add(this, NotificationType::TAB_PARENTED,
+        NotificationService::AllSources());
+    ui_test_utils::RunMessageLoop();
+  }
+
+  virtual void Observe(NotificationType type,
+                       const NotificationSource& source,
+                       const NotificationDetails& details) {
+    MessageLoopForUI::current()->Quit();
+  }
+
+ private:
+  NotificationRegistrar registrar_;
+
+  DISALLOW_COPY_AND_ASSIGN(TabParentedNotificationObserver);
+};
+
 class LanguageDetectionNotificationObserver : public NotificationObserver {
  public:
   explicit LanguageDetectionNotificationObserver(TabContents* tab) {
@@ -394,6 +414,10 @@ void WaitForNavigations(NavigationController* controller,
 void WaitForNewTab(Browser* browser) {
   SimpleNotificationObserver<Browser>
       new_tab_observer(NotificationType::TAB_ADDED, browser);
+}
+
+void WaitForTabParented() {
+  TabParentedNotificationObserver new_tab_observer;
 }
 
 void WaitForLoadStop(NavigationController* controller) {
