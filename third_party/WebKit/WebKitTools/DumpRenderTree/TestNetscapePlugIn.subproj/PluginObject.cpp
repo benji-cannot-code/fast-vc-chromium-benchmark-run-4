7a7430d5b9b1897385f981938ccc793b2cd86ee1
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-02 01:35:58 +0000

    WebKitTools
    2010-03-01  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
    
            Reviewed by Eric Seidel.
    
            [GTK] plugins/setProperty.html fails on 64bit Release
            https://bugs.webkit.org/show_bug.cgi?id=35425
    
            Check invoke's return code before releasing the variant, since
            there's a chance it won't be properly initialized, leading to
            memory corruption, in some cases.
    
            * DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.cpp:
            (testCallback):
            (testEnumerate):
            (testDocumentOpen):
            (testWindowOpen):
            (handleCallback):
    
    LayoutTests
    
    2010-03-01  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
    
            Reviewed by Eric Seidel.
    
            [GTK] plugins/setProperty.html fails on 64bit Release
            https://bugs.webkit.org/show_bug.cgi?id=35425
    
            Unskip the tests that failed as a consequence of the memory
            corruption.
    
            * platform/gtk/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006, 2007, 2008 Apple Inc. All rights reserved.
  * Copyright (C) 2009 Holger Hans Peter Freyther
+ * Copyright (C) 2010 Collabora Ltd.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -406,8 +407,8 @@ static bool testCallback(PluginObject* obj, const NPVariant* args, uint32_t argC
     free(callbackString);
 
     NPVariant browserResult;
-    browser->invoke(obj->npp, windowScriptObject, callbackIdentifier, 0, 0, &browserResult);
-    browser->releasevariantvalue(&browserResult);
+    if (browser->invoke(obj->npp, windowScriptObject, callbackIdentifier, 0, 0, &browserResult))
+        browser->releasevariantvalue(&browserResult);
 
     browser->releaseobject(windowScriptObject);
     
@@ -520,8 +521,8 @@ static bool testEnumerate(PluginObject* obj, const NPVariant* args, uint32_t arg
             NPVariant args[1];
             STRINGZ_TO_NPVARIANT(string, args[0]);
             NPVariant browserResult;
-            browser->invoke(obj->npp, outArray, pushIdentifier, args, 1, &browserResult);
-            browser->releasevariantvalue(&browserResult);
+            if (browser->invoke(obj->npp, outArray, pushIdentifier, args, 1, &browserResult))
+                browser->releasevariantvalue(&browserResult);
             browser->memfree(string);
         }
 
@@ -721,8 +722,8 @@ bool testDocumentOpen(NPP npp)
     STRINGZ_TO_NPVARIANT("_blank", openArgs[1]);
 
     NPVariant result;
-    browser->invoke(npp, documentObject, openId, openArgs, 2, &result);
-    browser->releaseobject(documentObject);
+    if (browser->invoke(npp, documentObject, openId, openArgs, 2, &result))
+        browser->releaseobject(documentObject);
 
     if (result.type == NPVariantType_Object) {
         pluginLogWithWindowObjectVariableArgs(windowObject, npp, "DOCUMENT OPEN SUCCESS");
@@ -748,8 +749,8 @@ bool testWindowOpen(NPP npp)
     STRINGZ_TO_NPVARIANT("_blank", openArgs[1]);
 
     NPVariant result;
-    browser->invoke(npp, windowObject, openId, openArgs, 2, &result);
-    if (result.type == NPVariantType_Object) {
+    bool didSucceed = browser->invoke(npp, windowObject, openId, openArgs, 2, &result);
+    if (didSucceed && (result.type == NPVariantType_Object)) {
         pluginLogWithWindowObjectVariableArgs(windowObject, npp, "WINDOW OPEN SUCCESS");
         notifyTestCompletion(npp, result.value.objectValue);
         browser->releaseobject(result.value.objectValue);
@@ -948,8 +949,8 @@ void handleCallback(PluginObject* object, const char *url, NPReason reason, void
         NULL_TO_NPVARIANT(args[1]);
 
     NPVariant browserResult;
-    browser->invoke(object->npp, windowScriptObject, callbackIdentifier, args, 2, &browserResult);
-    browser->releasevariantvalue(&browserResult);
+    if (browser->invoke(object->npp, windowScriptObject, callbackIdentifier, args, 2, &browserResult))
+        browser->releasevariantvalue(&browserResult);
 
     free(strHdr);
 }
