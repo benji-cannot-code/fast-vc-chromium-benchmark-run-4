5928b6a7cdf06f6a5f2485480cebe852836d3b7d
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-02 22:06:27 +0000

    Moves ActionComboboxModel class to its own header so we can share common code between windows and linux.
    BUG=None
    TEST=compiles, and still works as before.
    
    Review URL: http://codereview.chromium.org/661225
    Patch from thiago.farina@gmail.com.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40446 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/options/exception_editor_view.h
+++ chrome/browser/views/options/exception_editor_view.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "app/combobox_model.h"
+#include "chrome/browser/content_setting_combo_model.h"
 #include "chrome/common/content_settings.h"
 #include "chrome/common/content_settings_types.h"
 #include "views/window/dialog_delegate.h"
@@ -74,23 +74,6 @@ class ExceptionEditorView : public views::View,
                                const views::Textfield::Keystroke& key);
 
  private:
-  // Model for the combobox.
-  class ActionComboboxModel : public ComboboxModel {
-   public:
-    explicit ActionComboboxModel(bool show_ask) : show_ask_(show_ask) {}
-
-    virtual int GetItemCount();
-    virtual std::wstring GetItemAt(int index);
-
-    ContentSetting setting_for_index(int index);
-    int index_for_setting(ContentSetting);
-
-   private:
-    const bool show_ask_;
-
-    DISALLOW_COPY_AND_ASSIGN(ActionComboboxModel);
-  };
-
   void Init();
 
   views::Label* CreateLabel(int message_id);
@@ -104,7 +87,7 @@ class ExceptionEditorView : public views::View,
 
   Delegate* delegate_;
   ContentExceptionsTableModel* model_;
-  ActionComboboxModel cb_model_;
+  ContentSettingComboModel cb_model_;
 
   // Index of the item being edited. If -1, indices this is a new entry.
   const int index_;
