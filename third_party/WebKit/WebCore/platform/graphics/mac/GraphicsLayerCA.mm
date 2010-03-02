df12a8364e5d0bcd478c59a5809de59137127cf2
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-02 17:52:58 +0000

    2010-03-02  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dan Bernstein.
    
            https://bugs.webkit.org/show_bug.cgi?id=35555
            QuickTime plugin content can spill outside the <object> tag
    
            Set -masksToBounds on the layer that is handed to us by plug-ins, to ensure that sublayers
            of that layer don't spill outside the <object> contents rect.
    
            Manual test because it relies on QuickTime, and pixel results depend on movie loading timing.
    
            * manual-tests/plugins/object-clipping.html: Added.
            * platform/graphics/mac/GraphicsLayerCA.mm:
            (WebCore::GraphicsLayerCA::setupContentsLayer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55412 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/GraphicsLayerCA.mm
+++ third_party/WebKit/WebCore/platform/graphics/mac/GraphicsLayerCA.mm
@@ -2227,6 +2227,7 @@ void GraphicsLayerCA::setupContentsLayer(CALayer* contentsLayer)
 {
     // Turn off implicit animations on the inner layer.
     [contentsLayer setStyle:[NSDictionary dictionaryWithObject:nullActionsDictionary() forKey:@"actions"]];
+    [contentsLayer setMasksToBounds:YES];
 
     if (defaultContentsOrientation() == CompositingCoordinatesBottomUp) {
         CATransform3D flipper = {
