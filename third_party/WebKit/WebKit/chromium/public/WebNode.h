25c9fc85f05f2d47c0f9b7ca6bfdcd2d3336314c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-02 18:19:41 +0000

    2010-03-02  James Hawkins  <jhawkins@chromium.org>
            Reviewed by Darin Fisher.
    
            [Chromium] Implement WebNode::equals and add inline operators for ==
            and !=.
    
            https://bugs.webkit.org/show_bug.cgi?id=35543
    
            * public/WebNode.h:
            (WebKit::operator==):
            (WebKit::operator!=):
            * src/WebNode.cpp:
            (WebKit::WebNode::equals):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55413 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebNode.h
+++ third_party/WebKit/WebKit/chromium/public/WebNode.h
@@ -63,6 +63,8 @@ public:
     WEBKIT_API void reset();
     WEBKIT_API void assign(const WebNode&);
 
+    WEBKIT_API bool equals(const WebNode&) const;
+
     bool isNull() const { return !m_private; }
 
 #if WEBKIT_IMPLEMENTATION
@@ -135,6 +137,16 @@ protected:
         return static_cast<const T*>(m_private);
     }
 };
+
+inline bool operator==(const WebNode& a, const WebNode& b)
+{
+    return a.equals(b);
+}
+
+inline bool operator!=(const WebNode& a, const WebNode& b)
+{
+    return !(a == b);
+}
 
 } // namespace WebKit
 
