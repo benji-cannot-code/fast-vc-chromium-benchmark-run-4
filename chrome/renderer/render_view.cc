c3adac4cb2637b950fb0acbbbb9022b44fb05480
victorw@chromium.org <victorw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-02 22:04:33 +0000

    Revert 39670 which breaks suggestion popup
    Do not send extra blur and focus events if popup menu is showing
    
    Add flag to RenderWidget to remember the popup menu state and
    suppress focus / blur events when popup menu is showing. This
    fixes the issue that extra focus / blur events are fired after
    select control is clicked.
    
    R=darin
    BUG=23499
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/647047
    
    TBR=victorw@chromium.org
    Review URL: http://codereview.chromium.org/660408
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40445 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -1566,9 +1566,6 @@ WebWidget* RenderView::createPopupMenu(bool activatable) {
   RenderWidget* widget = RenderWidget::Create(routing_id_,
                                               render_thread_,
                                               activatable);
-  showing_popup_menu_ = true;
-  widget->SetPopupMenuOwnerWidget(this);
-
   return widget->webwidget();
 }
 
@@ -1576,9 +1573,6 @@ WebWidget* RenderView::createPopupMenu(const WebPopupMenuInfo& info) {
   RenderWidget* widget = RenderWidget::Create(routing_id_,
                                               render_thread_,
                                               true);
-  showing_popup_menu_ = true;
-  widget->SetPopupMenuOwnerWidget(this);
-
   widget->ConfigureAsExternalPopupMenu(info);
   return widget->webwidget();
 }
