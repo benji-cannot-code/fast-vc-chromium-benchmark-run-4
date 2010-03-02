a3dabc0886d70b4686e638e1c879fbbc57cd623d
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-02 14:22:29 +0000

    [Mac] Spin event loop longer for tearing down test windows.
    BookmarkBubbleControllerTest needs _three_ passes.  I believe it's
    because the bubble is a child window.
    
    BUG=none
    TEST=allows test to run on tsan bots.
    
    Review URL: http://codereview.chromium.org/661347
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40378 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bubble_controller_unittest.mm
+++ chrome/browser/cocoa/bookmark_bubble_controller_unittest.mm
@@ -27,6 +27,8 @@ class BookmarkBubbleControllerTest : public CocoaTest {
   }
 
   virtual void TearDown() {
+    // TODO(shess): Figure out why CocoaTest::TearDown() needs 3
+    // passes through the event loop to fully close out these windows.
     [controller_ close];
     controller_ = nil;
     CocoaTest::TearDown();
