2dd05504ccf7edd103c717ac31a787a786a3d312
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-03 22:51:52 +0000

    Checking in patch for Denis Romanov (originally reviewed here: http://codereview.chromium.org/651027).
    Make mouse wheel scroll contents of views::ScrollView control in Linux (GTK). 
    
    BUG=26970
    TEST=Make and run out/Debug/view_examples and scroll in ScrollView tab. 
    TBR=denisromanov
    
    Review URL: http://codereview.chromium.org/667008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40555 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/widget_gtk.h
+++ views/widget/widget_gtk.h
@@ -187,6 +187,26 @@ class WidgetGtk
   virtual View* GetFocusTraversableParentView();
 
  protected:
+  // If widget containes another widget, translates event coordinates to the
+  // contained widget's coordinates, else returns original event coordinates.
+  template<class Event> bool GetContainedWidgetEventCoordinates(Event* event,
+                                                                int* x,
+                                                                int* y) {
+    if (event == NULL || x == NULL || y == NULL)
+      return false;
+    *x = event->x;
+    *y = event->y;
+    GdkWindow* dest = GTK_WIDGET(window_contents_)->window;
+    if (event->window != dest) {
+      int dest_x, dest_y;
+      gdk_window_get_root_origin(dest, &dest_x, &dest_y);
+      *x = event->x_root - dest_x;
+      *y = event->y_root - dest_y;
+      return true;
+    }
+    return false;
+  }
+
   // Returns the view::Event::flags for a GdkEventButton.
   static int GetFlagsForEventButton(const GdkEventButton& event);
 
@@ -229,9 +249,7 @@ class WidgetGtk
                                   gint y,
                                   gboolean keyboard_mode,
                                   GtkTooltip* tooltip);
-  virtual gboolean OnScroll(GtkWidget* widget, GdkEventScroll* event) {
-    return false;
-  }
+  virtual gboolean OnScroll(GtkWidget* widget, GdkEventScroll* event);
   virtual gboolean OnVisibilityNotify(GtkWidget* widget,
                                       GdkEventVisibility* event) {
     return false;
@@ -272,6 +290,8 @@ class WidgetGtk
   // Process a mouse click.
   bool ProcessMousePressed(GdkEventButton* event);
   void ProcessMouseReleased(GdkEventButton* event);
+  // Process scroll event.
+  bool ProcessScroll(GdkEventScroll* event);
 
   static void SetRootViewForWidget(GtkWidget* widget, RootView* root_view);
 
