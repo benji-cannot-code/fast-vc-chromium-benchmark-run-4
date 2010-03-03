16a193e7bbf9463bb2269232d12f61f67fd051e7
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-03 17:47:13 +0000

    Fix attempt for bug 34158. Reset the TalkMediator after calling Logout to ensure it is destroyed from the core thread.  This ensures the task to destroy the GaiaAuth object is run on the core thread and not the UI thread.
    Fred and I debugged this together and actually saw a second crash appear to result
    from this change, but then we did a clean build and were unable to repro that crash,
    so I think this as a net win at the moment.
    
    BUG=34158
    
    Review URL: http://codereview.chromium.org/660437
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40521 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/syncapi.cc
+++ chrome/browser/sync/engine/syncapi.cc
@@ -954,7 +954,9 @@ class SyncManager::SyncInternal {
         initialized_(false) {
   }
 
-  ~SyncInternal() { }
+  ~SyncInternal() {
+    DCHECK(!talk_mediator_.get());
+  }
 
   bool Init(const FilePath& database_location,
             const std::string& sync_server_and_path,
@@ -1484,6 +1486,7 @@ void SyncManager::SyncInternal::Shutdown() {
   LOG(INFO) << "P2P: Mediator logout started.";
   if (talk_mediator()) {
     talk_mediator()->Logout();
+    talk_mediator_.reset();
   }
   LOG(INFO) << "P2P: Mediator logout completed.";
 
