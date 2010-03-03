449a13f1186c19801eeae06f042dc9756f895198
dumi@chromium.org <dumi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-03 23:03:49 +0000

    Move #include "DatabaseCallback.h" from Database.h to Database.cpp.
    Reviewed by nobody, build fix.
    
    * storage/Database.cpp:
    * storage/Database.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55480 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/Database.h
+++ third_party/WebKit/WebCore/storage/Database.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define Database_h
 
 #if ENABLE(DATABASE)
-#include "DatabaseCallback.h"
 #include "PlatformString.h"
 #include "SecurityOrigin.h"
 #include "SQLiteDatabase.h"
