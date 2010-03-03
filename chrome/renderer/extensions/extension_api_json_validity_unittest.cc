5616b59caa43055634c6bed885a2b963468f1cf1
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-03 14:25:39 +0000

    Add unit test that uses V8's JSON.parse() to read extension_api.json .
    BUG=18717
    TEST=ExtensionApiJsonValidityTest.WithV8
    
    Review URL: http://codereview.chromium.org/661406
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40516 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/extension_api_json_validity_unittest.cc
+++ chrome/renderer/extensions/extension_api_json_validity_unittest.cc
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_ptr.h"
 #include "chrome/common/json_value_serializer.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/renderer/extensions/bindings_utils.h"
+#include "chrome/test/v8_unit_test.h"
+#include "grit/common_resources.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -65,10 +68,15 @@ bool FindDictionaryWithProperyValue(ListValue* list,
 
 }
 
-class ExtensionApiJsonValidityTest : public testing::Test {
+class ExtensionApiJsonValidityTest : public V8UnitTest {
 };
 
-TEST(ExtensionApiJsonValidityTest, Basic) {
+// Read extension_api.json with JSONFileValueSerializer.  This test checks
+// that the file is valid without using V8 or grit generated code.
+// Unlike V8's JSON.Parse(), it produces easy to read error messages.
+// If this test passes, and ExtensionApiJsonValidityTest.WithV8 fails,
+// check to see if V8 or the grit build step is broken.
+TEST_F(ExtensionApiJsonValidityTest, Basic) {
   // Build the path to extension_api.json, and check that the file exists.
   FilePath extension_api_json;
   ASSERT_TRUE(PathService::Get(base::DIR_SOURCE_ROOT, &extension_api_json));
@@ -111,4 +119,18 @@ TEST(ExtensionApiJsonValidityTest, Basic) {
       << error_message;
 }
 
-// TODO(skerner): Add a test that parses extension_api.json using v8.
+// Use V8 to load the string resource version of extension_api.json .
+// This test mimics the method extension_api.json is loaded in
+// chrome/renderer/resources/extension_process_bindings.js .
+TEST_F(ExtensionApiJsonValidityTest, WithV8) {
+  std::string ext_api_string =
+      bindings_utils::GetStringResource<IDR_EXTENSION_API_JSON>();
+
+  // Create a global variable holding the text of extension_api.json .
+  SetGlobalStringVar("ext_api_json_text", ext_api_string);
+
+  // Parse the text of extension_api.json .  If there is a parse error,
+  // an exception will be printed that includes a line number.
+  std::string test_js = "var extension_api = JSON.parse(ext_api_json_text);";
+  ExecuteScriptInContext(test_js, "ParseExtensionApiJson");
+}
