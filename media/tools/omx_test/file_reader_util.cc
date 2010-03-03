3e804b0f46d37d57fb8484a4e11c3b5ff41202ad
fbarchard@chromium.org <fbarchard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-03 01:19:05 +0000

    ffmpeg better message for error on opening file if the file opens but the codec is not supported. BUG=37256 TEST=ffmpeg_tests.exe d:\mediatests\elephant2.m4v with chromium and you should get 'File format not supported'
    Review URL: http://codereview.chromium.org/661426
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40472 0039d316-1c4b-4281-b951-d872f2087c98

--- media/tools/omx_test/file_reader_util.cc
+++ media/tools/omx_test/file_reader_util.cc
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.  Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #include "media/tools/omx_test/file_reader_util.h"
 
@@ -125,10 +125,20 @@ FFmpegFileReader::~FFmpegFileReader() {
 }
 
 bool FFmpegFileReader::Initialize() {
-  if (av_open_input_file(&format_context_,
-                         filename_.c_str(), NULL, 0, NULL) < 0) {
-    LOG(ERROR) << "can't open file " << filename_;
-    return false;;
+  int result = av_open_input_file(&format_context_, filename_.c_str(),
+                                  NULL, 0, NULL);
+  if (result < 0) {
+    switch (result) {
+      case AVERROR_NOFMT:
+        LOG(ERROR) << "Error: File format not supported "
+                  << filename_ << std::endl;
+        break;
+      default:
+        LOG(ERROR) << "Error: Could not open input for "
+                  << filename_ << std::endl;
+        break;
+    }
+    return false;
   }
   if (av_find_stream_info(format_context_) < 0) {
     LOG(ERROR) << "can't use FFmpeg to parse stream info";
