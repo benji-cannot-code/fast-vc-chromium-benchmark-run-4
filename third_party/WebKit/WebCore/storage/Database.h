26aaf7564e54b68b85fcd065d9bff1ad514ff7ae
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-04 00:38:00 +0000

    2010-03-03  David Levin  <levin@chromium.org>
            No review, rolling out r55474.
            http://trac.webkit.org/changeset/55480
    
            The patch broke fast/frames/sandboxed-iframe-storage.html
    
            * Android.jscbindings.mk:
            * Android.v8bindings.mk:
            * GNUmakefile.am:
            * WebCore.gypi:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * bindings/js/JSDOMWindowCustom.cpp:
            * bindings/v8/custom/V8DOMWindowCustom.cpp:
            * dom/Document.cpp:
            (WebCore::Document::postTask):
            * page/DOMWindow.cpp:
            (WebCore::DOMWindow::openDatabase):
            * page/DOMWindow.h:
            * page/DOMWindow.idl:
            * storage/Database.cpp:
            (WebCore::Database::openDatabase):
            (WebCore::Database::Database):
            (WebCore::Database::performOpenAndVerify):
            * storage/Database.h:
            * workers/WorkerContext.cpp:
            (WebCore::WorkerContext::openDatabase):
            * workers/WorkerContext.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55485 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/Database.h
+++ third_party/WebKit/WebCore/storage/Database.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define Database_h
 
 #if ENABLE(DATABASE)
-#include "DatabaseCallback.h"
 #include "PlatformString.h"
 #include "SecurityOrigin.h"
 #include "SQLiteDatabase.h"
@@ -53,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class DatabaseAuthorizer;
-class DatabaseCallback;
 class DatabaseThread;
 class ScriptExecutionContext;
 class SQLResultSet;
@@ -76,10 +74,7 @@ public:
     ~Database();
 
 // Direct support for the DOM API
-    static PassRefPtr<Database> openDatabase(ScriptExecutionContext* context, const String& name,
-                                             const String& expectedVersion, const String& displayName,
-                                             unsigned long estimatedSize, PassRefPtr<DatabaseCallback> creationCallback,
-                                             ExceptionCode&);
+    static PassRefPtr<Database> openDatabase(ScriptExecutionContext* context, const String& name, const String& expectedVersion, const String& displayName, unsigned long estimatedSize, ExceptionCode&);
     String version() const;
     void changeVersion(const String& oldVersion, const String& newVersion,
                        PassRefPtr<SQLTransactionCallback> callback, PassRefPtr<SQLTransactionErrorCallback> errorCallback,
@@ -117,8 +112,6 @@ public:
     void stop();
     bool stopped() const { return m_stopped; }
 
-    bool isNew() const { return m_new; }
-
     unsigned long long databaseSize() const;
     unsigned long long maximumSize() const;
 
@@ -129,7 +122,6 @@ public:
     bool performOpenAndVerify(ExceptionCode&);
 
     Vector<String> performGetTableNames();
-    void performCreationCallback();
 
     SQLTransactionClient* transactionClient() const;
     SQLTransactionCoordinator* transactionCoordinator() const;
@@ -137,7 +129,7 @@ public:
 private:
     Database(ScriptExecutionContext* context, const String& name,
              const String& expectedVersion, const String& displayName,
-             unsigned long estimatedSize, PassRefPtr<DatabaseCallback> creationCallback);
+             unsigned long estimatedSize);
 
     bool openAndVerifyVersion(ExceptionCode&);
 
@@ -168,12 +160,8 @@ private:
 
     bool m_opened;
 
-    bool m_new;
-
     SQLiteDatabase m_sqliteDatabase;
     RefPtr<DatabaseAuthorizer> m_databaseAuthorizer;
-
-    RefPtr<DatabaseCallback> m_creationCallback;
 
 #ifndef NDEBUG
     String databaseDebugName() const { return m_mainThreadSecurityOrigin->toString() + "::" + m_name; }
