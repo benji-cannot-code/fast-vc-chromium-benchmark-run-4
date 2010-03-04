62fcd32777c1091ce4a159c7dbb8fa7d33963f76
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-04 17:32:58 +0000

    Yet another try to hotfix the ARM builbots. Sigh.
    BUG=none
    TEST=ARM buildbots go green?
    TBR=zelidrag
    Review URL: http://codereview.chromium.org/668069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40635 0039d316-1c4b-4281-b951-d872f2087c98

--- build/linux/python_arch.sh
+++ build/linux/python_arch.sh
@@ -12,12 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #
 
 python=$(readlink -f "$1")
-if [ -z "$python" ]; then
+if [ ! -r "$python" ]; then
   echo unknown
   exit 0;
 fi
 file_out=$(file "$python")
-if [ -z "$file_out" ]; then
+if [ $? -ne 0 ]; then
   echo unknown
   exit 0;
 fi
