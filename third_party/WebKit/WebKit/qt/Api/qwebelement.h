07b15a70a72d2dc054fb104b851c1f0ed9b48a0f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-04 19:12:12 +0000

    2010-03-04  Simon Hausmann  <simon.hausmann@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] qwebelement.h does not include QtCore headers correctly
            https://bugs.webkit.org/show_bug.cgi?id=35748
    
            The header files of QtCore must be included as QtCore/foo.h.
    
            See also http://bugreports.qt.nokia.com/browse/QTBUG-8661
    
            * Api/qwebelement.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55539 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebelement.h
+++ third_party/WebKit/WebKit/qt/Api/qwebelement.h
@@ -21,11 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef QWEBELEMENT_H
 #define QWEBELEMENT_H
 
-#include <QString>
-#include <QStringList>
-#include <QRect>
-#include <QVariant>
-#include <QExplicitlySharedDataPointer>
+#include <QtCore/qstring.h>
+#include <QtCore/qstringlist.h>
+#include <QtCore/qrect.h>
+#include <QtCore/qvariant.h>
+#include <QtCore/qshareddata.h>
 
 #include "qwebkitglobal.h"
 namespace WebCore {
