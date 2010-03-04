3c553eb4a58f51fa27210ba0e152b1a0bcdd9509
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-04 18:48:35 +0000

    2010-03-04  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: Clicking on an error should take me to the error
    
            https://bugs.webkit.org/show_bug.cgi?id=34860
    
            * inspector/front-end/ResourceView.js:
            (WebInspector.ResourceView):
            (WebInspector.ResourceView.prototype._selectTab):
            (WebInspector.ResourceView.prototype.selectContentTab):
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.ResourcesPanel.prototype.showResource):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ResourceView.js
+++ third_party/WebKit/WebCore/inspector/front-end/ResourceView.js
@@ -48,7 +48,7 @@ WebInspector.ResourceView = function(resource)
     this.tabsElement.appendChild(this.contentTabElement);
 
     this.headersTabElement.addEventListener("click", this._selectHeadersTab.bind(this), false);
-    this.contentTabElement.addEventListener("click", this._selectContentTab.bind(this), false);
+    this.contentTabElement.addEventListener("click", this.selectContentTab.bind(this), false);
 
     this.headersElement = document.createElement("div");
     this.headersElement.className = "resource-view-headers";
@@ -156,7 +156,7 @@ WebInspector.ResourceView.prototype = {
             if (WebInspector.settings.resourceViewTab === "headers")
                 this._selectHeadersTab();
             else
-                this._selectContentTab();
+                this.selectContentTab();
         } else
             this._innerSelectContentTab();
     },
@@ -170,7 +170,7 @@ WebInspector.ResourceView.prototype = {
         this.contentElement.addStyleClass("hidden");
     },
 
-    _selectContentTab: function()
+    selectContentTab: function()
     {
         WebInspector.settings.resourceViewTab = "content";
         this._innerSelectContentTab();
