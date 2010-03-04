d41da80097a741a222ecc995593588206cf33b20
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-04 21:29:16 +0000

    Clean up TooltipManagerWin a bit.  Original patch by Jay Soffian (see http://codereview.chromium.org/668021 ), r=me.
    BUG=37368
    TEST=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40662 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/tooltip_manager_win.h
+++ views/widget/tooltip_manager_win.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,11 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "app/gfx/native_widget_types.h"
 #include "base/basictypes.h"
+#include "base/gfx/point.h"
 #include "base/task.h"
 #include "views/widget/tooltip_manager.h"
 
 namespace gfx {
-class Font;
+class Point;
 }
 
 namespace views {
@@ -85,7 +86,7 @@ class TooltipManagerWin : public TooltipManager {
   gfx::NativeView GetParent();
 
   // Updates the tooltip for the specified location.
-  void UpdateTooltip(int x, int y);
+  void UpdateTooltip(const gfx::Point& location);
 
   // Tooltip control window.
   HWND tooltip_hwnd_;
@@ -94,8 +95,7 @@ class TooltipManagerWin : public TooltipManager {
   TOOLINFO toolinfo_;
 
   // Last location of the mouse. This is in the coordinates of the rootview.
-  int last_mouse_x_;
-  int last_mouse_y_;
+  gfx::Point last_mouse_pos_;
 
   // Whether or not the tooltip is showing.
   bool tooltip_showing_;
