5b51e6011b4305fc56a97c73756c6abc05f8f10e
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-04 09:41:30 +0000

    2010-03-04  Vangelis Kokkevis  <vangelis@chromium.org>
            Reviewed by Oliver Hunt.
    
            Update existing test to check that getUniformLocation() returns
            null if the uniform requested is not found.
            https://bugs.webkit.org/show_bug.cgi?id=34669
    
            * fast/canvas/webgl/script-tests/uniform-location.js:
            * fast/canvas/webgl/uniform-location-expected.txt:
    2010-03-04  Vangelis Kokkevis  <vangelis@chromium.org>
    
            Reviewed by Oliver Hunt.
    
            getUniformLocation() now returns null if uniform requested
            is not found.
            https://bugs.webkit.org/show_bug.cgi?id=34669
    
            Test:LayoutTests/fast/canvas/webgl/uniform-location.html
            (added missing test)
    
            * html/canvas/WebGLRenderingContext.cpp:
            (WebCore::WebGLRenderingContext::getUniformLocation):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55514 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/canvas/webgl/script-tests/uniform-location.js
+++ third_party/WebKit/LayoutTests/fast/canvas/webgl/script-tests/uniform-location.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description("Tests calling WebGL APIs with objects from other contexts");
+description("Tests WebGL APIs related to shader uniforms");
 
 var contextA = create3DDebugContext();
 var contextB = create3DDebugContext();
@@ -33,5 +33,7 @@ shouldBe("contextA.getUniform(programS, locationArray0)", "4.0");
 shouldBeUndefined("contextA.useProgram(programV)");
 shouldBeUndefined("contextA.uniform4fv(locationVec4, vec)");
 shouldBe("contextA.getUniform(programV, locationVec4)", "vec");
+
+shouldBeNull("contextA.getUniformLocation(programV, \"IDontExist\")");
 
 successfullyParsed = true;
