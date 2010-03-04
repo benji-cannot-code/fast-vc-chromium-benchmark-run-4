74722fa6091e47e7c05c932eca5655ba82ffd43b
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-04 09:35:16 +0000

    Disable ExtensionApiTest.Tabs on Mac. TEST=no random redness on Mac OS X 10.6 bot BUG=37387 TBR=skerner@
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40616 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -10,8 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 #include "chrome/common/pref_names.h"
 
-// TODO(skerner): This test is flaky in chromeos.  Figure out why and fix.
-#if defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
+// TODO(skerner): This test is flaky in chromeos and on Mac OS X 10.6.  Figure
+// out why and fix.
+#if (defined(OS_LINUX) && defined(TOOLKIT_VIEWS)) || defined(OS_MACOSX)
 #define MAYBE_Tabs DISABLED_Tabs
 #else
 #define MAYBE_Tabs Tabs
