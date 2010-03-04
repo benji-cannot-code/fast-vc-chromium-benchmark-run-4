4546e7eb1b66ee5e881d3653d959e4ea200ede07
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-04 21:09:41 +0000

    Unreviewed attempt to (again) fix Mac build.
    * page/SpatialNavigation.cpp:
    (WebCore::spatialDistance):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55548 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/SpatialNavigation.cpp
+++ third_party/WebKit/WebCore/page/SpatialNavigation.cpp
@@ -40,8 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-using namespace std;
-
 static long long spatialDistance(FocusDirection, const IntRect&, const IntRect&);
 static IntRect renderRectRelativeToRootDocument(RenderObject*);
 static RectsAlignment alignmentForRects(FocusDirection, const IntRect&, const IntRect&);
@@ -385,7 +383,10 @@ static long long spatialDistance(FocusDirection direction, const IntRect& a, con
         }
     }
 
-    long long distance = pow((x1 - x2), 2) + pow((y1 - y2), 2);
+    long long dx = x1 - x2;
+    long long dy = y1 - y2;
+
+    long long distance = (dx * dx) + (dy * dy);
     return abs(distance);
 }
 
