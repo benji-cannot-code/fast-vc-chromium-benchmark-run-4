369967ad2545d3168c03d418764dcb6a02003075
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-05 23:26:41 +0000

    Unreviewed buildfix after r55593. (To fix Qt --minimal build.)
    * bindings/js/JSDOMWindowCustom.cpp: Missing #if ENABLE(DATABASE) guard added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55598 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDOMWindowCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDOMWindowCustom.cpp
@@ -1021,6 +1021,7 @@ JSValue JSDOMWindow::removeEventListener(ExecState* exec, const ArgList& args)
     return jsUndefined();
 }
 
+#if ENABLE(DATABASE)
 JSValue JSDOMWindow::openDatabase(ExecState* exec, const ArgList& args)
 {
     if (!allowsAccessFrom(exec) || (args.size() < 4))
@@ -1038,6 +1039,7 @@ JSValue JSDOMWindow::openDatabase(ExecState* exec, const ArgList& args)
     setDOMException(exec, ec);
     return result;
 }
+#endif
 
 DOMWindow* toDOMWindow(JSValue value)
 {
