345059c7204fbe3504b30b29f71c0e8ab48d9d69
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-05 00:23:29 +0000

    Use a framework-style include to pull in WebInspector.h for consistency with other parts of WebKit.
    Reviewed by Sam Weinig.
    
    * WebInspector/WebInspectorPrivate.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55559 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebInspector/WebInspectorPrivate.h
+++ third_party/WebKit/WebKit/mac/WebInspector/WebInspectorPrivate.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This header contains the WebInspector SPI.
 
-#import "WebInspector.h"
+#import <WebKit/WebInspector.h>
 
 @interface WebInspector (WebPrivate)
 - (void)evaluateInFrontend:(id)sender callId:(long)callId script:(NSString *)script;
