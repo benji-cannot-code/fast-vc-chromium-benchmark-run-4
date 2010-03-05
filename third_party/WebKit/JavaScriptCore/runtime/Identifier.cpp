eb63768300021a00a0dc99a57ebc64f6bd8e8100
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-05 23:29:13 +0000

    2010-03-05  Oliver Hunt  <oliver@apple.com>
            Reviewed by Gavin Barraclough.
    
            JSC should cache int to Identifier conversion as it does for ordinary strings
            https://bugs.webkit.org/show_bug.cgi?id=35814
    
            Make the NumericStrings cache cache unsigned ints in addition to signed.
            We keep them separate from the int cache as it both simplifies code, and
            also because the unsigned path is exclusive to property access and therefore
            seems to have different usage patterns.
    
            The primary trigger for the unsigned to Identifier propertyName conversion
            is the construction of array-like objects out of normal objects.  Given these
            tend to be relative small numbers, and the array-like behaviour lends itself
            to sequential values this patch also adds a non-colliding cache for all small
            numbers.
    
            * JavaScriptCore.exp:
            * runtime/Identifier.cpp:
            (JSC::Identifier::from):
            * runtime/Identifier.h:
            * runtime/NumericStrings.h:
            (JSC::NumericStrings::add):
            (JSC::NumericStrings::lookup):
            (JSC::NumericStrings::lookupSmallString):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/Identifier.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/Identifier.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Identifier.h"
 
 #include "CallFrame.h"
+#include "NumericStrings.h"
 #include <new> // for placement new
 #include <string.h> // for strlen
 #include <wtf/Assertions.h>
@@ -236,6 +237,21 @@ PassRefPtr<UString::Rep> Identifier::addSlowCase(ExecState* exec, UString::Rep* 
 void Identifier::remove(UString::Rep* r)
 {
     currentIdentifierTable()->remove(r);
+}
+    
+Identifier Identifier::from(ExecState* exec, unsigned value)
+{
+    return Identifier(exec, exec->globalData().numericStrings.add(value));
+}
+
+Identifier Identifier::from(ExecState* exec, int value)
+{
+    return Identifier(exec, exec->globalData().numericStrings.add(value));
+}
+
+Identifier Identifier::from(ExecState* exec, double value)
+{
+    return Identifier(exec, exec->globalData().numericStrings.add(value));
 }
 
 #ifndef NDEBUG
