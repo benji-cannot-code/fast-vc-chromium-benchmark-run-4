b3bc313a3da75d64e2ddcfcd94a424b9395b0c54
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-06 02:18:02 +0000

    Revert 40810 (broke 10.6 bots) - Mac: Fix another crash in content exceptions dialog.
    Also add a couple unit tests. Also fix another potential crash and other problems uncovered by the tests.
    
    BUG=37208
    TEST=See bug. Also, unittests.
    
    Review URL: http://codereview.chromium.org/668046
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/668211
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40811 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/content_exceptions_window_controller.h
+++ chrome/browser/cocoa/content_exceptions_window_controller.h
@@ -41,8 +41,7 @@ class UpdatingContentSettingsObserver;
   // is used to suppress updates at bad times.
   BOOL updatesEnabled_;
 
-  // This is non-NULL only while a new element is being added and its host
-  // is being edited.
+  // This is non-NULL only while a new element is being added.
   scoped_ptr<HostContentSettingsMap::HostSettingPair> newException_;
 }
 
@@ -53,9 +52,4 @@ class UpdatingContentSettingsObserver;
 - (IBAction)removeException:(id)sender;
 - (IBAction)removeAllExceptions:(id)sender;
 
-@end
-
-@interface ContentExceptionsWindowController(VisibleForTesting)
-- (void)cancel:(id)sender;
-- (BOOL)editingNewException;
 @end
