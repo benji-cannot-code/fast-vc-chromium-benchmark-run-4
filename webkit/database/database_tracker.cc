74cd4ca7c69c34fb3576018fc3d6504d15e8a73c
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-06 22:17:27 +0000

    webkit/database: string_util.h -> utf_string_conversions.h fix.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/668230
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40850 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/database/database_tracker.cc
+++ webkit/database/database_tracker.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/file_path.h"
 #include "base/file_util.h"
-#include "base/string_util.h"
+#include "base/utf_string_conversions.h"
 #include "net/base/net_errors.h"
 #include "webkit/database/databases_table.h"
 #include "webkit/database/quota_table.h"
