010034c179b5bc26af0f1af1ddebcefbeb3a898a
gregoryd@google.com <gregoryd@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-06 01:56:28 +0000

    Add support for starting NaCl win64 process if nacl64.exe is in the version directory. We now assume that nacl64.exe is in the same directory as chrome.dll (and not as chrome.exe as we assumed before).
    BUG=28176
    TEST=none
    Review URL: http://codereview.chromium.org/668130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40809 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/nacl_host/nacl_broker_host.cc
+++ chrome/browser/nacl_host/nacl_broker_host.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/nacl_host/nacl_broker_host.h"
 
 #include "base/command_line.h"
+#include "base/path_service.h"
 #include "ipc/ipc_switches.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/nacl_host/nacl_broker_service.h"
@@ -35,11 +36,11 @@ bool NaClBrokerHost::Init() {
     return false;
 
   // Create the path to the nacl broker/loader executable.
-  FilePath exe_path = GetChildPath(false);
-  if (exe_path.empty())
+  FilePath module_path;
+  if (!PathService::Get(base::FILE_MODULE, &module_path))
     return false;
 
-  FilePath nacl_path = exe_path.DirName().Append(chrome::kNaClAppName);
+  FilePath nacl_path = module_path.DirName().Append(chrome::kNaClAppName);
   CommandLine* cmd_line = new CommandLine(nacl_path);
   nacl::CopyNaClCommandLineArguments(cmd_line);
 
