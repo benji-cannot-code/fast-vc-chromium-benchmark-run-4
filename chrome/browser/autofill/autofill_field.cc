e7ef1e5b018395f32f6c7d79df49a08d707c4ac2
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-07 21:44:29 +0000

    Add tests for AutoFillField.
    BUG=none
    TEST=AutoFillFieldTest
    Review URL: http://codereview.chromium.org/669203
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40876 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_field.cc
+++ chrome/browser/autofill/autofill_field.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -39,6 +39,14 @@ AutoFillField::AutoFillField(const webkit_glue::FormField& field,
       heuristic_type_(UNKNOWN_TYPE) {
 }
 
+void AutoFillField::set_heuristic_type(const AutoFillFieldType& type) {
+  DCHECK(type >= 0 && type < MAX_VALID_FIELD_TYPE);
+  if (type >= 0 && type < MAX_VALID_FIELD_TYPE)
+    heuristic_type_ = type;
+  else
+    heuristic_type_ = UNKNOWN_TYPE;
+}
+
 AutoFillFieldType AutoFillField::type() const {
   if (server_type_ != NO_SERVER_DATA)
     return server_type_;
@@ -55,14 +63,6 @@ std::string AutoFillField::FieldSignature() const {
   std::string type = UTF16ToUTF8(form_control_type());
   std::string field_string = field_name + "&" + type;
   return Hash32Bit(field_string);
-}
-
-void AutoFillField::set_heuristic_type(const AutoFillFieldType& type) {
-  DCHECK(type >= 0 && type < MAX_VALID_FIELD_TYPE);
-  if (type >= 0 && type < MAX_VALID_FIELD_TYPE)
-    heuristic_type_ = type;
-  else
-    heuristic_type_ = UNKNOWN_TYPE;
 }
 
 bool AutoFillField::IsFieldFillable() const {
--- chrome/browser/autofill/autofill_field.h
+++ chrome/browser/autofill/autofill_field.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,7 +21,12 @@ class AutoFillField : public webkit_glue::FormField {
   const string16& unique_name() const { return unique_name_; }
 
   AutoFillFieldType heuristic_type() const { return heuristic_type_; }
+  AutoFillFieldType server_type() const { return server_type_; }
   const FieldTypeSet& possible_types() const { return possible_types_; }
+
+  // Sets the heuristic type of this field, validating the input.
+  void set_heuristic_type(const AutoFillFieldType& type);
+  void set_server_type(const AutoFillFieldType& type) { server_type_ = type; }
   void set_possible_types(const FieldTypeSet& possible_types) {
     possible_types_ = possible_types;
   }
@@ -36,9 +41,6 @@ class AutoFillField : public webkit_glue::FormField {
   // The unique signature of this field, composed of the field name and the html
   // input type in a 32-bit hash.
   std::string FieldSignature() const;
-
-  // Sets the heuristic type of this field, validating the input.
-  void set_heuristic_type(const AutoFillFieldType& type);
 
   // Returns true if the field type has been determined (without the text in the
   // field).
