87da42969520c00ba0c216f04f8baaaec14e2a6f
suzhe@chromium.org <suzhe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-08 07:41:32 +0000

    Fix issue 27927: linux hang in RenderViewTest.ImeComposition.
    BUG=27927
    TEST=none
    
    Review URL: http://codereview.chromium.org/668219
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40885 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view_unittest.cc
+++ chrome/renderer/render_view_unittest.cc
@@ -24,7 +24,8 @@ using WebKit::WebString;
 using WebKit::WebTextDirection;
 using WebKit::WebURLError;
 
-#if 0 // !defined(OS_LINUX)
+namespace {
+
 static WebCompositionCommand ToCompositionCommand(int string_type) {
   switch (string_type) {
     default:
@@ -37,7 +38,8 @@ static WebCompositionCommand ToCompositionCommand(int string_type) {
       return WebKit::WebCompositionCommandConfirm;
   }
 }
-#endif
+
+}  // namespace
 
 TEST_F(RenderViewTest, OnLoadAlternateHTMLText) {
   // Test a new navigation.
@@ -145,8 +147,6 @@ TEST_F(RenderViewTest, OnImeStateChanged) {
 // cases, this test should not only call IME-related functions in the
 // RenderWidget class, but also call some RenderWidget members, e.g.
 // ExecuteJavaScript(), RenderWidget::OnSetFocus(), etc.
-// Disabled on Linux: http://crbug.com/27927
-#if 0 // !defined(OS_LINUX)
 TEST_F(RenderViewTest, ImeComposition) {
   enum ImeCommand {
     IME_INITIALIZE,
@@ -264,7 +264,6 @@ TEST_F(RenderViewTest, ImeComposition) {
     }
   }
 }
-#endif
 
 // Test that the RenderView::OnSetTextDirection() function can change the text
 // direction of the selected input element.
