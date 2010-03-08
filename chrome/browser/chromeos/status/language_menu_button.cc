a4654d8ce9f941ad1fe87b2ba50acb16d747767a
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-08 04:47:22 +0000

    Remove obsolete TODOs. No code change.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/668238
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40879 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/language_menu_button.cc
+++ chrome/browser/chromeos/status/language_menu_button.cc
@@ -182,12 +182,11 @@ int LanguageMenuButton::GetGroupIdAt(int index) const {
 }
 
 bool LanguageMenuButton::HasIcons() const  {
-  // TODO(yusukes): Display IME icons.
+  // We don't support IME nor keyboard icons on Chrome OS.
   return false;
 }
 
 bool LanguageMenuButton::GetIconAt(int index, SkBitmap* icon) const {
-  // TODO(yusukes): Display IME icons.
   return false;
 }
 
@@ -338,7 +337,6 @@ void LanguageMenuButton::UpdateIcon(const std::wstring& name) {
   SetEnabledColor(SK_ColorWHITE);
   SetShowHighlighted(false);
   SetText(name);
-  // TODO(yusukes): Show icon on the status area?
   set_alignment(TextButton::ALIGN_RIGHT);
   SchedulePaint();
 }
