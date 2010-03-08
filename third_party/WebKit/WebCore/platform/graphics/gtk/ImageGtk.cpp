f9dd4eb622f0b6a13ceb17cf4c792c9d35515ae1
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-08 02:51:07 +0000

    Gtk build fix.
    * platform/graphics/gtk/ImageGtk.cpp: Include SharedBuffer.h since we're using WebCore::SharedBuffer.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55650 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gtk/ImageGtk.cpp
+++ third_party/WebKit/WebCore/platform/graphics/gtk/ImageGtk.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "BitmapImage.h"
 #include "CString.h"
 #include "GOwnPtr.h"
-
+#include "SharedBuffer.h"
 #include <cairo.h>
 #include <gtk/gtk.h>
 
