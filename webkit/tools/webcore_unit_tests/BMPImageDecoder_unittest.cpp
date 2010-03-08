bf2439033103afb867aea2b20b254740688c3ad8
yaar@chromium.org <yaar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-08 22:11:41 +0000

    UpStreamimg *image_decoder_unittest. Used WebKit API's ImageDecoder to decouple WebCore deps. Corresponding WebKit Bug 35415.
    BUG=28063
    TEST=Run unit tests.
    
    Review URL: http://codereview.chromium.org/661231
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40951 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/webcore_unit_tests/BMPImageDecoder_unittest.cpp
+++ webkit/tools/webcore_unit_tests/BMPImageDecoder_unittest.cpp
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2008, Google Inc.
-// All rights reserved.
+// Copyright (c) 2008, Google Inc. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
@@ -29,17 +28,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "config.h"
-#include "webkit/tools/test_shell/image_decoder_unittest.h"
 
-#include "BMPImageDecoder.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebImageDecoder.h"
+#include "webkit/tools/test_shell/image_decoder_unittest.h"
 
 class BMPImageDecoderTest : public ImageDecoderTest {
  public:
   BMPImageDecoderTest() : ImageDecoderTest("bmp") { }
 
  protected:
-  virtual WebCore::ImageDecoder* CreateDecoder() const {
-    return new WebCore::BMPImageDecoder();
+  virtual WebKit::WebImageDecoder* CreateWebKitImageDecoder() const {
+    return new WebKit::WebImageDecoder(WebKit::WebImageDecoder::TypeBMP);
   }
 
   // The BMPImageDecoderTest tests are really slow under Valgrind.
@@ -56,13 +55,3 @@ TEST_F(BMPImageDecoderTest, DecodingFast) {
 TEST_F(BMPImageDecoderTest, DecodingSlow) {
   TestDecoding(TEST_BIGGER, kThresholdSize);
 }
-
-#ifndef CALCULATE_MD5_SUMS
-TEST_F(BMPImageDecoderTest, ChunkedDecodingFast) {
-  TestChunkedDecoding(TEST_SMALLER, kThresholdSize);
-}
-
-TEST_F(BMPImageDecoderTest, ChunkedDecodingSlow) {
-  TestChunkedDecoding(TEST_BIGGER, kThresholdSize);
-}
-#endif
