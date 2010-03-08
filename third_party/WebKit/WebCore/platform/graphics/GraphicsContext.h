2a8d43f976939d7cd64d8e3efaf179ed0c0e8845
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-08 02:29:22 +0000

    Gtk build fix.
    * platform/graphics/GraphicsContext.h: Include wtf/PassOwnPtr.h since some platforms use WTF::PassOwnPtr in this header.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55647 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/GraphicsContext.h
+++ third_party/WebKit/WebCore/platform/graphics/GraphicsContext.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Path.h"
 #include "TextDirection.h"
 #include <wtf/Noncopyable.h>
+#include <wtf/PassOwnPtr.h>
 
 #if PLATFORM(CG)
 typedef struct CGContext PlatformGraphicsContext;
