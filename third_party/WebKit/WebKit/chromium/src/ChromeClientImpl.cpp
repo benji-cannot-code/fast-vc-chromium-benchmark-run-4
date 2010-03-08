a20297bedef9ca01b5c285d358d7ae982bcfb4b4
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-08 22:38:39 +0000

    2010-03-08  Michael Nordman  <michaeln@google.com>
            Reviewed by Darin Fisher.
    
            Quick fix for a chromium unit test failures around r55675 (repaint refactoring).
            This change restores the tests for empty rects that were deleted.
    
            https://bugs.webkit.org/show_bug.cgi?id=35878
    
            * src/ChromeClientImpl.cpp:
            (WebKit::ChromeClientImpl::invalidateContentsAndWindow):
            * src/WebPopupMenuImpl.cpp:
            (WebKit::WebPopupMenuImpl::invalidateContentsAndWindow):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/ChromeClientImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/ChromeClientImpl.cpp
@@ -479,6 +479,8 @@ void ChromeClientImpl::invalidateWindow(const IntRect&, bool)
 
 void ChromeClientImpl::invalidateContentsAndWindow(const IntRect& updateRect, bool /*immediate*/)
 {
+    if (updateRect.isEmpty())
+        return;
     if (m_webView->client())
         m_webView->client()->didInvalidateRect(updateRect);
 }
