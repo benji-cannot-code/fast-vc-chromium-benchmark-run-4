2c2605a7c0eea490ec388213e7d9a62d8f3ddf3d
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-08 02:05:29 +0000

    Qt build fix.
    * platform/network/qt/DnsPrefetchHelper.cpp: Include PlatformString.h since we're using WebCore::String.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55641 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/DnsPrefetchHelper.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/DnsPrefetchHelper.cpp
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "DnsPrefetchHelper.h"
 
-#include "CString.h"
+#include "PlatformString.h"
 
 namespace WebCore {
 // this is called on mouse over a href and on page loading
