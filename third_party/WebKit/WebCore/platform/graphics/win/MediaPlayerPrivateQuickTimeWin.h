463e46ac6fac23d160c8823b7c8cf5e1c2a707a4
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-08 17:36:12 +0000

    2010-03-08  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Darin Adler.
    
            YouTube HTML5 video never starts playing on Windows
            https://bugs.webkit.org/show_bug.cgi?id=33954
    
            * platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp:
            (WebCore::MediaPlayerPrivate::rfc2616DateStringFromTime): New, create an rfc 2616 formatted
            string for an absolute time value.
            (WebCore::addCookieParam): New, add a cookie param and value to a string builder.
            (WebCore::MediaPlayerPrivate::setUpCookiesForQuickTime): Copy cookies for the movie to
                be loaded from CFNetwork into WinINet so they are available when QuickTime tries to
                download the movie.
            (WebCore::MediaPlayerPrivate::load): Call setupCookiesForQuickTime.
            * platform/graphics/win/MediaPlayerPrivateQuickTimeWin.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55668 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeWin.h
+++ third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeWin.h
@@ -159,6 +159,9 @@ private:
     void createLayerForMovie();
     void destroyLayerForMovie();
 
+    void setUpCookiesForQuickTime(const String& url);
+    String rfc2616DateStringFromTime(CFAbsoluteTime);
+
     MediaPlayer* m_player;
     OwnPtr<QTMovieWin> m_qtMovie;
 #if USE(ACCELERATED_COMPOSITING)
