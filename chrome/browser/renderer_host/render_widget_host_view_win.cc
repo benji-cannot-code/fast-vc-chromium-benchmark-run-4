ffb098e4b99ab4f648daa6b0251b384684a01c34
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-08 21:05:29 +0000

    Windows: fix some graphical glitches with render view deemphasis.
    This doesn't fix bullet points 3 or 4 from the bug report, which are cross-platform. This only fixes the windows-specific painting issues.
    
    BUG=37563
    TEST=manual
    
    Review URL: http://codereview.chromium.org/669228
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40933 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_win.cc
+++ chrome/browser/renderer_host/render_widget_host_view_win.cc
@@ -233,7 +233,7 @@ BOOL CALLBACK DetachPluginWindowsCallback(HWND window, LPARAM param) {
 void DrawDeemphasized(const gfx::Rect& paint_rect,
                       HDC backing_store_dc,
                       HDC paint_dc) {
-  gfx::Canvas canvas(paint_rect.width(), paint_rect.height(), false);
+  gfx::Canvas canvas(paint_rect.width(), paint_rect.height(), true);
   HDC dc = canvas.beginPlatformPaint();
   BitBlt(dc,
          0,
@@ -782,7 +782,11 @@ bool RenderWidgetHostViewWin::ContainsNativeView(
 }
 
 void RenderWidgetHostViewWin::SetVisuallyDeemphasized(bool deemphasized) {
+  if (visually_deemphasized_ == deemphasized)
+    return;
+
   visually_deemphasized_ = deemphasized;
+  InvalidateRect(NULL, FALSE);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
