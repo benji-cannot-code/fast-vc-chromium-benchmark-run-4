4c9e5b25812db2274ab929cee3d8e21346c246e6
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-08 19:40:49 +0000

    2010-03-08  Adam Barth  <abarth@webkit.org>
            Reviewed by Nate Chapin.
    
            [V8] Block popups from inline script
            https://bugs.webkit.org/show_bug.cgi?id=35474
    
            Test that we block popups generated from <script>window.open(...)</script>.
    
            * http/tests/security/popup-blocked-from-window-open-expected.txt: Added.
            * http/tests/security/popup-blocked-from-window-open.html: Added.
    2010-03-08  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Nate Chapin.
    
            [V8] Block popups from inline script
            https://bugs.webkit.org/show_bug.cgi?id=35474
    
            Apparently, we're supposed to look at the sourceURL to figure out
            whether we're running a script tag or a hyperlink.  This logic is
            copied from the JSC version.
    
            Test: http/tests/security/popup-blocked-from-window-open.html
    
            * bindings/v8/ScriptController.cpp:
            (WebCore::ScriptController::processingUserGesture):
            (WebCore::ScriptController::evaluate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/ScriptController.cpp
+++ third_party/WebKit/WebCore/bindings/v8/ScriptController.cpp
@@ -191,7 +191,7 @@ bool ScriptController::processingUserGesture(DOMWrapperWorld*) const
 
         if (eventOk)
             return true;
-    } else if (activeProxy->inlineCode() && !activeProxy->timerCallback()) {
+    } else if (m_sourceURL && m_sourceURL->isNull() && !activeProxy->timerCallback()) {
         // This is the <a href="javascript:window.open('...')> case -> we let it through.
         return true;
     }
@@ -220,7 +220,9 @@ void ScriptController::evaluateInIsolatedWorld(unsigned worldID, const Vector<Sc
 ScriptValue ScriptController::evaluate(const ScriptSourceCode& sourceCode)
 {
     String sourceURL = sourceCode.url();
-    
+    const String* savedSourceURL = m_sourceURL;
+    m_sourceURL = &sourceURL;
+
     if (!m_XSSAuditor->canEvaluate(sourceCode.source())) {
         // This script is not safe to be evaluated.
         return ScriptValue();
@@ -238,8 +240,10 @@ ScriptValue ScriptController::evaluate(const ScriptSourceCode& sourceCode)
     v8::Local<v8::Value> object = m_proxy->evaluate(sourceCode, 0);
 
     // Evaluating the JavaScript could cause the frame to be deallocated
-    // so we starot the keep alive timer here.
+    // so we start the keep alive timer here.
     m_frame->keepAlive();
+
+    m_sourceURL = savedSourceURL;
 
     if (object.IsEmpty() || object->IsUndefined())
         return ScriptValue();
