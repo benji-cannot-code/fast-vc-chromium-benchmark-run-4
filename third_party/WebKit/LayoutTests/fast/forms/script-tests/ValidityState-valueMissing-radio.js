926b6ae070eeed5a869bd95dc43e4acb5241b3b6
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-08 01:53:33 +0000

    2010-03-07  Kent Tamura  <tkent@chromium.org>
            Reviewed by Eric Seidel.
    
            Fix a bug that validity.valueMissing for a radio button with required
            in a form element always returns true.
            https://bugs.webkit.org/show_bug.cgi?id=35472
    
            Add tests for radio buttons in a form element, and merge an existing
            radio button test to ValidityState-valueMissing-radio.html.
    
            * fast/forms/ValidityState-valueMissing-007-expected.txt: Removed.
            * fast/forms/ValidityState-valueMissing-007.html: Removed.
            * fast/forms/ValidityState-valueMissing-radio-expected.txt: Added.
            * fast/forms/ValidityState-valueMissing-radio.html: Added.
            * fast/forms/script-tests/ValidityState-valueMissing-radio.js: Added.
    
    2010-03-07  Kent Tamura  <tkent@chromium.org>
    
            Reviewed by Eric Seidel.
    
            Fix a bug that validity.valueMissing for a radio button with required
            in a form element always returns true.
            https://bugs.webkit.org/show_bug.cgi?id=35472
    
            Test: fast/forms/ValidityState-valueMissing-radio.html
    
            * html/HTMLInputElement.cpp:
            (WebCore::checkedRadioButtons): Move the location to be used by valueMissing().
            (WebCore::HTMLInputElement::valueMissing):
              Use checkedRadioButtons() instead of document()->checkedRadioButtons().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55640 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/forms/script-tests/ValidityState-valueMissing-radio.js
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('valueMissing tests for radio buttons');
+
+var parent = document.createElement('div');
+document.body.appendChild(parent);
+
+debug('Without form element');
+parent.innerHTML = '<input name=victim type=radio required/>'
+    + '<input name=victim type=radio/>';
+var inputs = document.getElementsByName('victim');
+debug('No checked button:');
+shouldBeTrue('inputs[0].validity.valueMissing');
+// The following result should be false because the element does not have
+// "required".  It conforms to HTML5, and this behavior has no practical
+// problems.
+shouldBeFalse('inputs[1].validity.valueMissing');
+debug('The first button has been checked:');
+inputs[0].checked = true;
+shouldBeFalse('inputs[0].validity.valueMissing');
+shouldBeFalse('inputs[1].validity.valueMissing');
+debug('The second button has been checked:');
+inputs[1].checked = true;
+shouldBeFalse('inputs[0].validity.valueMissing');
+shouldBeFalse('inputs[1].validity.valueMissing');
+
+debug('With form element');
+parent.innerHTML = '<form>'
+    + '<input name=victim type=radio required/>'
+    + '<input name=victim type=radio/>'
+    + '</form>';
+inputs = document.getElementsByName('victim');
+debug('No checked button:');
+shouldBeTrue('inputs[0].validity.valueMissing');
+// The following result should be false.
+shouldBeFalse('inputs[1].validity.valueMissing');
+debug('The first button has been checked:');
+inputs[0].checked = true;
+shouldBeFalse('inputs[0].validity.valueMissing');
+shouldBeFalse('inputs[1].validity.valueMissing');
+debug('The second button has been checked:');
+inputs[1].checked = true;
+shouldBeFalse('inputs[0].validity.valueMissing');
+shouldBeFalse('inputs[1].validity.valueMissing');
+
+var successfullyParsed = true;
--- third_party/WebKit/WebCore/html/HTMLInputElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLInputElement.cpp
@@ -164,6 +164,13 @@ bool HTMLInputElement::autoComplete() const
     return true;
 }
 
+static inline CheckedRadioButtons& checkedRadioButtons(const HTMLInputElement* element)
+{
+    if (HTMLFormElement* form = element->form())
+        return form->checkedRadioButtons();
+    return element->document()->checkedRadioButtons();
+}
+
 bool HTMLInputElement::valueMissing() const
 {
     if (!isRequiredFormControl() || readOnly() || disabled())
@@ -188,7 +195,7 @@ bool HTMLInputElement::valueMissing() const
         case CHECKBOX:
             return !checked();
         case RADIO:
-            return !document()->checkedRadioButtons().checkedButtonForGroup(name());
+            return !checkedRadioButtons(this).checkedButtonForGroup(name());
         case COLOR:
             return false;
         case BUTTON:
@@ -677,14 +684,6 @@ void HTMLInputElement::stepUp(int n, ExceptionCode& ec)
 void HTMLInputElement::stepDown(int n, ExceptionCode& ec)
 {
     applyStep(-n, ec);
-}
-
-static inline CheckedRadioButtons& checkedRadioButtons(const HTMLInputElement *element)
-{
-    if (HTMLFormElement* form = element->form())
-        return form->checkedRadioButtons();
-    
-    return element->document()->checkedRadioButtons();
 }
 
 bool HTMLInputElement::isKeyboardFocusable(KeyboardEvent* event) const
