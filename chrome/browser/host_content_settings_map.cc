b88f5270b7078a5db73d49a1d9fa93e8b4cab3c3
jschuh@google.com <jschuh@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-08 19:29:41 +0000

    Correctly handle trailing dots in hostname blacklist.
    Submitted for inferno@chromium.org
    
    BUG=35510
    TEST=HostContentSettingsMapTest.HostTrimEndingDotCheck
    
    Review URL: http://codereview.chromium.org/669284
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40921 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/host_content_settings_map.cc
+++ chrome/browser/host_content_settings_map.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_type.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
+#include "net/base/dns_util.h"
 #include "net/base/static_cookie_policy.h"
 
 // static
@@ -127,7 +128,8 @@ ContentSetting HostContentSettingsMap::GetContentSetting(
     const std::string& host,
     ContentSettingsType content_type) const {
   AutoLock auto_lock(lock_);
-  HostContentSettings::const_iterator i(host_content_settings_.find(host));
+  HostContentSettings::const_iterator i(host_content_settings_.find(
+    net::TrimEndingDot(host)));
   if (i != host_content_settings_.end()) {
     ContentSetting setting = i->second.settings[content_type];
     if (setting != CONTENT_SETTING_DEFAULT)
@@ -146,7 +148,8 @@ ContentSetting HostContentSettingsMap::GetContentSetting(
 ContentSettings HostContentSettingsMap::GetContentSettings(
     const std::string& host) const {
   AutoLock auto_lock(lock_);
-  HostContentSettings::const_iterator i(host_content_settings_.find(host));
+  HostContentSettings::const_iterator i(host_content_settings_.find(
+    net::TrimEndingDot(host)));
   if (i == host_content_settings_.end())
     return default_content_settings_;
 
