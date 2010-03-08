64ce1a1ccac6476c01bda4273b37303dcce89567
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-08 02:05:32 +0000

    Chromium build fix.
    * platform/chromium/ChromiumDataObject.h: Include SharedBuffer.h since the inline constructor of this
    class means that the pointed-to type of the RefPtr member must be available.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55642 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/ChromiumDataObject.h
+++ third_party/WebKit/WebCore/platform/chromium/ChromiumDataObject.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "KURL.h"
 #include "PlatformString.h"
+#include "SharedBuffer.h"
 #include <wtf/RefPtr.h>
 #include <wtf/Vector.h>
 
