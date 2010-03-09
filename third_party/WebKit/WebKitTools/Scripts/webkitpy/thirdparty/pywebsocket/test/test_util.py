988aa260af2154584d7fc9219aa669f58ea7172a
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-09 10:40:49 +0000

    2010-03-03  Fumitoshi Ukai  <ukai@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            pywebsocket should support html and cgi in the same directory.
            https://bugs.webkit.org/show_bug.cgi?id=34879
    
            * websocket/tests/cookies: Removed.
            * websocket/tests/cookies/echo-cookie_wsh.py: Removed.
            * websocket/tests/cookies/httponly-cookie-expected.txt: Removed.
            * websocket/tests/cookies/httponly-cookie.pl: Removed.
            * websocket/tests/echo-cookie_wsh.py: Copied from LayoutTests/websocket/tests/cookies/echo-cookie_wsh.py.
            * websocket/tests/httponly-cookie-expected.txt: Copied from LayoutTests/websocket/tests/cookies/httponly-cookie-expected.txt.
            * websocket/tests/httponly-cookie.pl: Copied from LayoutTests/websocket/tests/cookies/httponly-cookie.pl.
    2010-03-03  Fumitoshi Ukai  <ukai@chromium.org>
    
            Reviewed by Alexey Proskuryakov.
    
            pywebsocket should support html and cgi in the same directory.
            https://bugs.webkit.org/show_bug.cgi?id=34879
    
            Import pywebsocket 0.4.9.2
            Specify --server-host 127.0.0.1, so that it only binds listening socket
            to 127.0.0.1 to prevent access from non-localhost.
            Change --cgi-paths from /websocket/tests/cookies to /websocket/tests,
            because pywebsocket 0.4.9.2 supports html and cgi in the same directory
            and only executable (httponly-cookies.pl) will be handled as cgi
            script.
    
            * Scripts/run-webkit-tests:
            * Scripts/run-webkit-websocketserver:
            * Scripts/webkitpy/layout_tests/port/websocket_server.py:
            * Scripts/webkitpy/thirdparty/pywebsocket/README.webkit:
            * Scripts/webkitpy/thirdparty/pywebsocket/example/echo_client.py:
            * Scripts/webkitpy/thirdparty/pywebsocket/example/handler_map.txt: Added.
            * Scripts/webkitpy/thirdparty/pywebsocket/mod_pywebsocket/standalone.py:
            * Scripts/webkitpy/thirdparty/pywebsocket/mod_pywebsocket/util.py:
            * Scripts/webkitpy/thirdparty/pywebsocket/setup.py:
            * Scripts/webkitpy/thirdparty/pywebsocket/test/test_util.py:
            * Scripts/webkitpy/thirdparty/pywebsocket/test/testdata/README: Added.
            * Scripts/webkitpy/thirdparty/pywebsocket/test/testdata/hello.pl: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55719 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/thirdparty/pywebsocket/test/test_util.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/thirdparty/pywebsocket/test/test_util.py
@@ -34,11 +34,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Tests for util module."""
 
 
+import os
+import sys
 import unittest
 
 import config  # This must be imported before mod_pywebsocket.
 from mod_pywebsocket import util
 
+_TEST_DATA_DIR = os.path.join(os.path.split(__file__)[0], 'testdata')
 
 class UtilTest(unittest.TestCase):
     def test_get_stack_trace(self):
@@ -55,6 +58,18 @@ class UtilTest(unittest.TestCase):
         self.assertEqual('World', str(exc))
         util.prepend_message_to_exception('Hello ', exc)
         self.assertEqual('Hello World', str(exc))
+
+    def test_get_script_interp(self):
+        cygwin_path = 'c:\\cygwin\\bin'
+        cygwin_perl = os.path.join(cygwin_path, 'perl')
+        self.assertEqual(None, util.get_script_interp(
+            os.path.join(_TEST_DATA_DIR, 'README')))
+        self.assertEqual(None, util.get_script_interp(
+            os.path.join(_TEST_DATA_DIR, 'README'), cygwin_path))
+        self.assertEqual('/usr/bin/perl -wT', util.get_script_interp(
+            os.path.join(_TEST_DATA_DIR, 'hello.pl')))
+        self.assertEqual(cygwin_perl + ' -wT', util.get_script_interp(
+            os.path.join(_TEST_DATA_DIR, 'hello.pl'), cygwin_path))
 
 
 if __name__ == '__main__':
