40a7647e5c69bd37144d11f6a901c434d302b432
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-09 14:18:09 +0000

    Add enable/disable translate pref to "Under the Hood".
    NIB change: Added checkbox for pref into web content section of "Under the Hood" and moved other controls down to match.
    
    BUG=37527
    TEST=navigate to www.google.co.il using a branded build translate infobar should appear/not appear based on pref.
    
    Review URL: http://codereview.chromium.org/722004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41026 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/preferences_window_controller.h
+++ chrome/browser/cocoa/preferences_window_controller.h
@@ -106,6 +106,7 @@ class ProfileSyncService;
   IBOutlet NSButton* downloadLocationButton_;
   StringPrefMember defaultDownloadLocation_;
   BooleanPrefMember askForSaveLocation_;
+  BooleanPrefMember translateEnabled_;
   FontLanguageSettingsController* fontLanguageSettings_;
   StringPrefMember currentTheme_;
   IBOutlet NSButton* enableLoggingCheckbox_;
