f2c62722763c01869a2882f1786583b911c25484
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-09 07:43:50 +0000

    Synchronous XHR should only pump events if we are going to show a cookie prompt.
    Also, LocalStorage.setItem needs to reset the CookieMessageFilter's
    pump_messages_event after it is done with sending its IPC.
    
    To support both of these fixes, the contents of RendererWebCookieJarImpl's
    SendSynchronousMessage is merged into RenderThread::Send.
    
    R=jam
    BUG=37571
    TEST=none
    
    Review URL: http://codereview.chromium.org/669224
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41013 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/renderer_webstoragearea_impl.cc
+++ chrome/renderer/renderer_webstoragearea_impl.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/renderer_webstoragearea_impl.h"
 
 #include "chrome/common/render_messages.h"
-#include "chrome/renderer/cookie_message_filter.h"
 #include "chrome/renderer/render_thread.h"
 #include "chrome/renderer/render_view.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFrame.h"
@@ -59,8 +58,7 @@ void RendererWebStorageAreaImpl::setItem(
   IPC::SyncMessage* message =
       new ViewHostMsg_DOMStorageSetItem(routing_id, storage_area_id_, key,
                                         value, url, &result, &old_value);
-  message->set_pump_messages_event(
-      RenderThread::current()->cookie_message_filter()->pump_messages_event());
+  // NOTE: This may pump events (see RenderThread::Send).
   RenderThread::current()->Send(message);
   old_value_webkit = old_value;
 
