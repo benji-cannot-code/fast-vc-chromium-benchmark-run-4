21de2822450b403c4101ff43430b99f2c1e052c9
glotov@google.com <glotov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-09 17:54:05 +0000

    Update screen view is reset while language is switched.
    BUG=none
    TODO=none
    
    Review URL: http://codereview.chromium.org/736001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41047 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -180,6 +180,7 @@ void WizardController::OnSwitchLanguage(std::string lang) {
   network_screen_.reset();
   login_screen_.reset();
   account_screen_.reset();
+  update_screen_.reset();
   contents_->RemoveAllChildViews(true);
 
   // Switch the locale.
