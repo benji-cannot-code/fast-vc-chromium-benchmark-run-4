e2306e6c94d4f1fc4a54ef1e173b39d67b909641
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-09 01:08:56 +0000

    Roll out a file I checked in by accident.
    * WebView/WebView.mm: Back to previous version.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55700 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -976,6 +976,8 @@ static bool shouldEnableLoadDeferring()
     WTF::RefCountedLeakCounter::suppressMessages("At least one WebView was closed with fast teardown.");
 #endif
 
+    _private->closed = YES;
+    
     [[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 
@@ -1007,8 +1009,6 @@ static bool fastDocumentTeardownEnabled()
 {
     if (!_private || _private->closed)
         return;
-
-    _private->closed = YES;
 
     [self _closingEventHandling];
 
@@ -1042,6 +1042,9 @@ static bool fastDocumentTeardownEnabled()
     [self setUIDelegate:nil];
 
     [_private->inspector webViewClosed];
+
+    // setHostWindow:nil must be called before this value is set (see 5408186)
+    _private->closed = YES;
 
     // To avoid leaks, call removeDragCaret in case it wasn't called after moveDragCaretToPoint.
     [self removeDragCaret];
@@ -3337,7 +3340,7 @@ static bool needsWebViewInitThreadWorkaround()
 
 - (void)setHostWindow:(NSWindow *)hostWindow
 {
-    if (_private->closed && hostWindow)
+    if (_private->closed)
         return;
     if (hostWindow == _private->hostWindow)
         return;
