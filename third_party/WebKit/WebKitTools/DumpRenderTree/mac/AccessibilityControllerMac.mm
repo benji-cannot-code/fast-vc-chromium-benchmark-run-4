eefcf35331e546c1c05bf7fdfaf5bf9fb3a83f0c
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-09 18:11:18 +0000

    DRT build fix for Tiger. No review.
    AX: hit testing a list box doesn't work anymore
    https://bugs.webkit.org/show_bug.cgi?id=35893
    
    * DumpRenderTree/mac/AccessibilityControllerMac.mm:
    (AccessibilityController::elementAtPoint):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/AccessibilityControllerMac.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/AccessibilityControllerMac.mm
@@ -43,7 +43,7 @@ AccessibilityController::~AccessibilityController()
 
 AccessibilityUIElement AccessibilityController::elementAtPoint(int x, int y)
 {
-    id accessibilityObject = [[[mainFrame frameView] documentView] accessibilityHitTest:NSMakePoint((CGFloat)x, (CGFloat)y)];
+    id accessibilityObject = [[[mainFrame frameView] documentView] accessibilityHitTest:NSMakePoint(x, y)];
     return AccessibilityUIElement(accessibilityObject);    
 }
 
