8aed8f689f752f8e659437227208530e604df8ab
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-09 19:52:07 +0000

    Plug-ins don't always respect the cookie accept policy. <rdar://problem/7338359> and https://bugs.webkit.org/show_bug.cgi?id=26391
    Reviewed by Darin Adler.
    
    WebCore: 
    
    The problem is that the various plug-in implementations call into a ResourceLoader
    directly instead of filtering the request through FrameLoader. This bypassed the step
    of adding extra fields to the requests, such as the firstPartyForCookies URL.
    
    Since plug-in code is currently so strewn about and very platform specific, I
    think reworking it needs to be a task for domain experts. I don't know the implications
    of adding *all* the extra fields to plug-in requests, for example.
    
    There's no harm in this targeted fix for the hole in our cookie accept policy until
    plug-ins can more fundamentally change.
    
    Test: http/tests/plugins/third-party-cookie-accept-policy.html
    
    * loader/ResourceLoader.cpp:
    (WebCore::ResourceLoader::load): Don't load a resource without first giving the request 
      a firstPartyForCookies URL.
    
    LayoutTests: 
    
    * http/tests/cookies/resources/cookie-utility.php:
    * http/tests/plugins/resources/third-party-cookie-accept-policy-iframe.html: Added.
    * http/tests/plugins/third-party-cookie-accept-policy.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55738 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/cookies/resources/cookie-utility.php
+++ third_party/WebKit/LayoutTests/http/tests/cookies/resources/cookie-utility.php
@@ -4,7 +4,7 @@ parse_str($_SERVER["QUERY_STRING"]);
 
 function deleteCookie($value, $name)
 {
-    setcookie($name, "deleted", time() - 86400);
+    setcookie($name, "deleted", time() - 86400, '/');
 }
 
 if ($queryfunction == "deleteCookies") {
@@ -14,14 +14,14 @@ if ($queryfunction == "deleteCookies") {
 }
 
 if ($queryfunction == "setFooCookie") {
-    setcookie("foo", "awesomevalue", time() + 86400);
+    setcookie("foo", "awesomevalue", time() + 86400, '/');
     echo "Set the foo cookie";
     return;
 }
 
 if ($queryfunction == "setFooAndBarCookie") {
-    setcookie("foo", "awesomevalue", time() + 86400);
-    setcookie("bar", "anotherawesomevalue", time() + 86400);
+    setcookie("foo", "awesomevalue", time() + 86400, '/');
+    setcookie("bar", "anotherawesomevalue", time() + 86400, '/');
     echo "Set the foo and bar cookies";
     return;
 }
