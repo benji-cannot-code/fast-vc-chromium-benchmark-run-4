a498f8053c01e785a2989ce7c56543ac86c9d899
cira@google.com <cira@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-09 17:52:29 +0000

    All platforms don't support same locales at the same time. Make locale check for extensions less strict (we log a warning now, but let extension load).
    TEST=Create extension with _locales/xxx_yyy and see that it loads.
    
    Review URL: http://codereview.chromium.org/671011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41046 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_unpacker.cc
+++ chrome/common/extensions/extension_unpacker.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/extensions/extension_file_util.h"
+#include "chrome/common/extensions/extension_l10n_util.h"
 #include "chrome/common/json_value_serializer.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/url_constants.h"
@@ -116,22 +117,17 @@ bool ExtensionUnpacker::ReadAllMessageCatalogs(
   FilePath locales_path =
     temp_install_dir_.Append(Extension::kLocaleFolder);
 
-  // Treat all folders under _locales as valid locales.
+  // Not all folders under _locales have to be valid locales.
   file_util::FileEnumerator locales(locales_path,
                                     false,
                                     file_util::FileEnumerator::DIRECTORIES);
 
-  FilePath locale_path = locales.Next();
-  do {
-    // Since we use this string as a key in a DictionaryValue, be paranoid about
-    // skipping any strings with '.'. This happens sometimes, for example with
-    // '.svn' directories.
-    FilePath relative_path;
-    // message_path was created from temp_install_dir. This should never fail.
-    if (!temp_install_dir_.AppendRelativePath(locale_path, &relative_path))
-      NOTREACHED();
-    std::wstring subdir(relative_path.ToWStringHack());
-    if (std::find(subdir.begin(), subdir.end(), L'.') != subdir.end())
+  std::set<std::string> all_locales;
+  extension_l10n_util::GetAllLocales(&all_locales);
+  FilePath locale_path;
+  while (!(locale_path = locales.Next()).empty()) {
+    if (extension_l10n_util::ShouldSkipValidation(locales_path, locale_path,
+                                                  all_locales))
       continue;
 
     FilePath messages_path =
@@ -139,7 +135,7 @@ bool ExtensionUnpacker::ReadAllMessageCatalogs(
 
     if (!ReadMessageCatalog(messages_path))
       return false;
-  } while (!(locale_path = locales.Next()).empty());
+  }
 
   return true;
 }
