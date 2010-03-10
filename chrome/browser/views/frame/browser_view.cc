97c8a4f6fc551986bdeaedb0ab8d225d5335b34b
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-10 19:22:03 +0000

    Initial focus fix on x11/gtk  The initial focus on x11/gtk was not working propertly. It was jsut  working by coincident, and chromeos's compact location bar revealed  the bug. This CL fixes by setting a focus when the window itself gets focus.
    BUG=34159
    TEST=on chromeos build, omnibox should have a focus on a new window.
    
    Review URL: http://codereview.chromium.org/756002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41187 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_view.cc
+++ chrome/browser/views/frame/browser_view.cc
@@ -677,9 +677,7 @@ void BrowserView::Show() {
   //
   // If we do find there are cases where we need to restore the focus on show,
   // that should be added and this should be removed.
-  TabContents* selected_tab_contents = GetSelectedTabContents();
-  if (selected_tab_contents)
-    selected_tab_contents->view()->RestoreFocus();
+  RestoreFocus();
 
   // Restore split offset.
   int split_offset = g_browser_process->local_state()->GetInteger(
@@ -841,6 +839,12 @@ bool BrowserView::IsFullscreenBubbleVisible() const {
 
 void BrowserView::FullScreenStateChanged() {
   ProcessFullscreen(IsFullscreen());
+}
+
+void BrowserView::RestoreFocus() {
+  TabContents* selected_tab_contents = GetSelectedTabContents();
+  if (selected_tab_contents)
+    selected_tab_contents->view()->RestoreFocus();
 }
 
 LocationBar* BrowserView::GetLocationBar() const {
