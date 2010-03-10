7dd083e6cd5d9350a6ccbef9506d58f805c0df94
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-10 21:34:58 +0000

    https://bugs.webkit.org/show_bug.cgi?id=35977
    Reviewed by Sam Weinig.
    
    Turns out the previous attempt to clear/reset cookies wasn't working because of the way
    the URLs for the .js and its .cgi were being resolved.
    
    So this is a better way to make the cookies consistent both before and after these tests.
            
    If this fails to make cookie-related tests reliable, then we might have to bite the bullet
    and give DRT itself the ability to forcibly remove cookies via API methods.
            
    This would be bulletproof and could be automatic if a test declares itself "cookie sensitive"
    but we'd lose the ability to reliably run these tests in the browser.  :(
    
    * http/tests/cookies/resources/resetCookies.js: Added.
    * http/tests/cookies/resources/third-party-cookie-relaxing-iframe.html:
    * http/tests/cookies/third-party-cookie-relaxing.html:
    * http/tests/plugins/resources/third-party-cookie-accept-policy-iframe.html:
    * http/tests/plugins/third-party-cookie-accept-policy.html:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/cookies/resources/resetCookies.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function resetCookies()
+{
+    if (window.layoutTestController)
+        layoutTestController.setAlwaysAcceptCookies(true);
+
+    // Due to cross-origin restrictions, we can only (simply) reset cookies for our current origin.
+    var url = "http://" + window.location.hostname +":8000/cookies/resources/cookie-utility.php?queryfunction=deleteCookies";
+    var req = new XMLHttpRequest();
+    try {
+        req.open('GET', url, false);
+        req.send();
+    } catch (e) {
+        alert("Attempt to clear " + url + " cookies might have failed.  Test results might be off from here on out. (" + e + ")");
+    }
+    
+    if (window.layoutTestController)
+        layoutTestController.setAlwaysAcceptCookies(false);
+}
