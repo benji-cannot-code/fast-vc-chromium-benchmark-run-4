f03accc7a6689e9968ed8636ac51222a7722e27d
dspringer@google.com <dspringer@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-10 02:08:48 +0000

    Another #include of <string.h> so that the nacl-g++ compiler will build Native Client on 64-bit platforms.
    Review URL: http://codereview.chromium.org/777004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41117 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/cmd_buffer_helper.h
+++ gpu/command_buffer/client/cmd_buffer_helper.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_COMMAND_BUFFER_CLIENT_CMD_BUFFER_HELPER_H_
 #define GPU_COMMAND_BUFFER_CLIENT_CMD_BUFFER_HELPER_H_
 
+#include <string.h>
+
 #include "../common/logging.h"
 #include "../common/constants.h"
 #include "../common/cmd_buffer_common.h"
