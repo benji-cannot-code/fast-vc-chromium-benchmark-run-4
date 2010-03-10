5b3c6a3f003a0b5c48641bf80680d3dcfe11e1c8
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-10 19:54:41 +0000

    Bookmark manager: Open in incognito window.
    Make IsIncognitoEnabled take an Extension* as an argument as well and use that where possible.
    
    BUG=4890
    TEST=Select one more bookmarks and right click/tools and select "Open in incognito". The bookmarks should open in an incognito window.
    
    Review URL: http://codereview.chromium.org/750001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41193 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/browser_actions_container.cc
+++ chrome/browser/views/browser_actions_container.cc
@@ -1162,6 +1162,5 @@ void BrowserActionsContainer::NotifyMenuDeleted(
 bool BrowserActionsContainer::ShouldDisplayBrowserAction(Extension* extension) {
   // Only display incognito-enabled extensions while in incognito mode.
   return (!profile_->IsOffTheRecord() ||
-          profile_->GetExtensionsService()->
-              IsIncognitoEnabled(extension->id()));
+          profile_->GetExtensionsService()->IsIncognitoEnabled(extension));
 }
