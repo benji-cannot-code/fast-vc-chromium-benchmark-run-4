8c2ebfc975b91885421777f6fc5b7d81f3bb0f6b
craig.schlenter@chromium.org <craig.schlenter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-10 16:19:00 +0000

    Fix warning that is now an error.
    C:\b\slave\chromium-rel-xp\build\src\sandbox\src\sandbox.cc(16) : warning C4010: single-line comment contains line-continuation character
    
    Review URL: http://codereview.chromium.org/788003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41164 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/src/sandbox.cc
+++ sandbox/src/sandbox.cc
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(_WIN64) && !defined(NACL_WIN64)
 // We allow building this code for Win64 as part of NaCl to enable development
 #error Sandbox code was not tested on 64-bit Windows. See \
- http://code.google.com/p/chromium/issues/detail?id=27218 for details \
- and progress log.
+ http://crbug.com/27218 for details and progress log.
 #endif
 
 
