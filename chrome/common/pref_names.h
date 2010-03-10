4f9c3d4c6965faa7a2fdfbec76dff2ada21c0d78
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-10 22:55:01 +0000

    Switch to continuous reporting of uptime
    Uptime was being reported only at startup, and then, for the previous session.
    This meant that folks with a long uptime would be very slow to report.
    This change moves the reporting to an incremental reporting, done as
    often as we report pageloads etc. (i.e., in each UMA upload).
    
    This also fixes several bugs relating to uptime calculation, by using
    TimeTicks (which show process time) rather than Time (which shows wall
    clock time, and is subject to corruption if the user changes the will
    clock time on the computer).  It also resolves a bug that may have caused
    negative uptimes to be reported.
    
    BUG=37630
    r=hunar
    Review URL: http://codereview.chromium.org/675001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41216 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -156,7 +156,6 @@ extern const wchar_t kStabilityRendererCrashCount[];
 extern const wchar_t kStabilityExtensionRendererCrashCount[];
 extern const wchar_t kStabilityLaunchTimeSec[];
 extern const wchar_t kStabilityLastTimestampSec[];
-extern const wchar_t kStabilityUptimeSec[];
 extern const wchar_t kStabilityRendererHangCount[];
 extern const wchar_t kStabilityChildProcessCrashCount[];
 
