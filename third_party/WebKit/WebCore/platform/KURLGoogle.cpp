bd8d11926bee072f8a2201cb1691cd9d6ed82644
brettw@chromium.org <brettw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-11 18:38:03 +0000

    010-03-08  Brett Wilson  <brettw@chromium.org>
            Reviewed by Adam Barth.
    
            Remove the deprecated argument to url_util::IsStandard in preparation
            for deleting that version of the function. Pull the latest googleurl
            with the new version for the Chromium builder.
    
            * platform/KURLGoogle.cpp:
            (WebCore::KURL::isHierarchical):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55846 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/KURLGoogle.cpp
+++ third_party/WebKit/WebCore/platform/KURLGoogle.cpp
@@ -1038,7 +1038,6 @@ bool KURL::isHierarchical() const
         return false;
     return url_util::IsStandard(
         &m_url.utf8String().data()[m_url.m_parsed.scheme.begin],
-        m_url.utf8String().length(),
         m_url.m_parsed.scheme);
 }
 
