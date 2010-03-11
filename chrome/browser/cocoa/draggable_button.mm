09647dc3d6fdd61b12b380587cdcab07045dd2fb
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-11 23:21:05 +0000

    Fix "drag cancel" semantics.
    BUG=37967
    
    TEST=\
    1) make sure bookmark buttons still work (click to open)
    2) make sure basic DnD of bookmark buttons still works
    3) In Debug build, drag a bookmark button.  ESC to cancel.  Then mouse up.
       Make sure no crash.
    
    Review URL: http://codereview.chromium.org/871002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41340 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/draggable_button.mm
+++ chrome/browser/cocoa/draggable_button.mm
@@ -39,14 +39,6 @@ const CGFloat kWebDragStartHysteresisY = 5.0;
   // Make sure that we can't start a drag until we see a mouse down again.
   mayDragStart_ = NO;
 
-  // This conditional is never true (DnD loops in Cocoa eat the mouse
-  // up) but I added it in case future versions of Cocoa do unexpected
-  // things.
-  if (beingDragged_) {
-    NOTREACHED();
-    return [super mouseUp:theEvent];
-  }
-
   // There are non-drag cases where a mouseUp: may happen
   // (e.g. mouse-down, cmd-tab to another application, move mouse,
   // mouse-up).  So we check.
@@ -55,7 +47,12 @@ const CGFloat kWebDragStartHysteresisY = 5.0;
   if (NSPointInRect(viewLocal, [self bounds])) {
     [self performClick:self];
   } else {
-    [self endDrag];
+    // Make sure an ESC to end a drag doesn't trigger 2 endDrags.
+    if (beingDragged_) {
+      [self endDrag];
+    } else {
+      [super mouseUp:theEvent];
+    }
   }
 }
 
@@ -89,6 +86,7 @@ const CGFloat kWebDragStartHysteresisY = 5.0;
              [self hasCrossedDragThreshold:theEvent]) {
     // Starting drag. Never start another drag until another mouse down.
     mayDragStart_ = NO;
+    beingDragged_ = YES;
     [self beginDrag:theEvent];
   }
 }
