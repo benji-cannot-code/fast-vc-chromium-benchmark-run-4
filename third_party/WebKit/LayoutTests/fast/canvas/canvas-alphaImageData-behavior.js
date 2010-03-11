5fad1671ef5526063c1ab1e62e0a64457a44f5c9
chang.shu@nokia.com <chang.shu@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-11 03:23:18 +0000

    2010-03-10  Chang Shu  <chang.shu@nokia.com>
            Reviewed by Darin Adler.
    
            While calculating alpha channel, convert the floating point value to
            an integer in [0, 256) with equal distribution.
            https://bugs.webkit.org/show_bug.cgi?id=22150
    
            * css/CSSParser.cpp:
            (WebCore::CSSParser::parseColorParameters):
    
    2010-03-10  Chang Shu  <chang.shu@nokia.com>
    
            Reviewed by Darin Adler.
    
            While calculating alpha channel, convert the floating point value to
            an integer in [0, 256) with equal distribution.
            Updated test results based on this new behavior.
            https://bugs.webkit.org/show_bug.cgi?id=22150
    
            * fast/canvas/canvas-alphaImageData-behavior-expected.txt:
            * fast/canvas/canvas-alphaImageData-behavior.js:
            * platform/qt/Skipped:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/canvas/canvas-alphaImageData-behavior.js
+++ third_party/WebKit/LayoutTests/fast/canvas/canvas-alphaImageData-behavior.js
@@ -10,7 +10,7 @@ var imgdata = imageData.data;
 shouldBe("imgdata[4]", "0");
 shouldBe("imgdata[5]", "255");
 shouldBe("imgdata[6]", "0");
-shouldBe("imgdata[7]", "253");
+shouldBe("imgdata[7]", "254");
 
 ctx.putImageData(imageData, 0, 0);
 
@@ -19,6 +19,6 @@ imgdata = ctx.getImageData(0, 0, 200, 200).data;
 shouldBe("imgdata[4]", "0");
 shouldBe("imgdata[5]", "255");
 shouldBe("imgdata[6]", "0");
-shouldBe("imgdata[7]", "253");
+shouldBe("imgdata[7]", "254");
 
 var successfullyParsed = true;
