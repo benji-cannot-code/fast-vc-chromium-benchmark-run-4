d9e73428e2e030f9e22affcda4bd6099f2ae1805
cmasone@google.com <cmasone@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-11 00:33:58 +0000

    Code got moved out from under me, and when I merged, I missed a line.  People not using my new code will be unaffected, but this change makes it so the new stuff works again. Review URL: http://codereview.chromium.org/826003
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41227 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/utils.cc
+++ chrome/browser/chromeos/login/utils.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 #include "chrome/browser/profile_manager.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/notification_service.h"
 #include "views/widget/widget_gtk.h"
 
@@ -46,7 +47,8 @@ void CompleteLogin(const std::string& username) {
   Profile* profile = profile_manager->GetDefaultProfile(user_data_dir);
   int return_code;
 
-  ExternalCookieHandler::GetCookies(command_line, profile);
+  if (!CommandLine::ForCurrentProcess()->HasSwitch(switches::kInChromeAuth))
+    ExternalCookieHandler::GetCookies(command_line, profile);
   browser_init.LaunchBrowser(command_line, profile, std::wstring(), true,
                              &return_code);
 }
