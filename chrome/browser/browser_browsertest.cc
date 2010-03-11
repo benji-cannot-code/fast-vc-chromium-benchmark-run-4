65b440a388b56aaa33bbdb21695e916b3f9b65db
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-11 17:18:27 +0000

    Disabling browser test that is crashing on the Mac bots.
    BUG=29424
    TEST=Disabling JavascriptAlertActivatesTab.
    TBR=pamg
    
    Review URL: http://codereview.chromium.org/866004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41282 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_browsertest.cc
+++ chrome/browser/browser_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -184,7 +184,14 @@ IN_PROC_BROWSER_TEST_F(BrowserTest, Title) {
   EXPECT_EQ(WideToUTF16(test_title), tab_title);
 }
 
-IN_PROC_BROWSER_TEST_F(BrowserTest, JavascriptAlertActivatesTab) {
+#if defined(OS_MACOSX)
+// Test is crashing on Mac, see http://crbug.com/29424.
+#define MAYBE_JavascriptAlertActivatesTab DISABLED_JavascriptAlertActivatesTab
+#else
+#define MAYBE_JavascriptAlertActivatesTab JavascriptAlertActivatesTab
+#endif
+
+IN_PROC_BROWSER_TEST_F(BrowserTest, MAYBE_JavascriptAlertActivatesTab) {
   GURL url(ui_test_utils::GetTestUrl(L".", L"title1.html"));
   ui_test_utils::NavigateToURL(browser(), url);
   browser()->AddTabWithURL(url, GURL(), PageTransition::TYPED,
