8051ba3b58ce3e6b706339475c707effe47937ea
dumi@chromium.org <dumi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-11 02:18:41 +0000

    WebCore: Adding support for the optional creation callback that could be passed to openDatabase().
    Reviewed by Adam Barth.
    
    Tests: storage/open-database-creation-callback.html
           storage/open-database-creation-callback-isolated-world.html
    
    https://bugs.webkit.org/show_bug.cgi?id=34726
    
    * Android.jscbindings.mk
    * Android.v8bindings.mk
    * GNUmakefile.am:
    * WebCore.gypi:
    * WebCore.pro:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * bindings/js/JSDOMWindowCustom.cpp:
    (WebCore::JSDOMWindow::openDatabase):
    * bindings/js/JSDatabaseCallback.cpp: Added.
    (WebCore::JSDatabaseCallback::JSDatabaseCallback):
    (WebCore::JSDatabaseCallback::~JSDatabaseCallback):
    (WebCore::JSDatabaseCallback::handleEvent):
    * bindings/js/JSDatabaseCallback.h: Added.
    (WebCore::JSDatabaseCallback::create):
    * bindings/v8/custom/V8DOMWindowCustom.cpp:
    (WebCore::V8DOMWindow::openDatabaseCallback):
    * bindings/v8/custom/V8DatabaseCallback.cpp: Added.
    (WebCore::V8DatabaseCallback::V8DatabaseCallback):
    (WebCore::V8DatabaseCallback::~V8DatabaseCallback):
    (WebCore::V8DatabaseCallback::handleEvent):
    * bindings/v8/custom/V8DatabaseCallback.h: Added.
    (WebCore::V8DatabaseCallback::create):
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::openDatabase):
    * page/DOMWindow.h:
    * page/DOMWindow.idl:
    * storage/Database.cpp:
    (WebCore::DatabaseCreationCallbackTask::create):
    (WebCore::DatabaseCreationCallbackTask::performTask):
    (WebCore::DatabaseCreationCallbackTask::DatabaseCreationCallbackTask):
    (WebCore::Database::openDatabase):
    (WebCore::Database::Database):
    (WebCore::Database::performOpenAndVerify):
    (WebCore::Database::performCreationCallback):
    * storage/Database.h:
    (WebCore::Database::isNew):
    * storage/DatabaseCallback.h: Added.
    (WebCore::DatabaseCallback::~DatabaseCallback):
    * workers/WorkerContext.cpp:
    (WebCore::WorkerContext::openDatabase):
    * workers/WorkerContext.h:
    
    LayoutTests: Adding a test to test the creation callback passed to
    openDatabase() and another one to make sure it is executed in the
    correct world.
    
    Reviewed by Adam Barth.
    
    Also, fixing fast/frames/sandboxed-iframe-storage.html: calling
    openDatabase() without at least 4 parameters should always fail,
    because the database name, version, description and estimated size
    are not optional according to the spec.
    
    https://bugs.webkit.org/show_bug.cgi?id=34726
    
    * fast/frames/resources/sandboxed-iframe-storage-allowed.html:
    * fast/frames/resources/sandboxed-iframe-storage-disallowed.html:
    * fast/frames/sandboxed-iframe-storage-expected.txt:
    * platform/qt/Skipped:
    * storage/open-database-creation-callback-expected.txt: Added.
    * storage/open-database-creation-callback.html: Added.
    * storage/open-database-creation-callback-isolated-world-expected.txt: Added.
    * storage/open-database-creation-callback-isolated-world.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55823 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/DOMWindow.cpp
+++ third_party/WebKit/WebCore/page/DOMWindow.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Chrome.h"
 #include "Console.h"
 #include "Database.h"
+#include "DatabaseCallback.h"
 #include "DOMApplicationCache.h"
 #include "DOMSelection.h"
 #include "DOMTimer.h"
@@ -1178,7 +1179,7 @@ double DOMWindow::devicePixelRatio() const
 }
 
 #if ENABLE(DATABASE)
-PassRefPtr<Database> DOMWindow::openDatabase(const String& name, const String& version, const String& displayName, unsigned long estimatedSize, ExceptionCode& ec)
+PassRefPtr<Database> DOMWindow::openDatabase(const String& name, const String& version, const String& displayName, unsigned long estimatedSize, PassRefPtr<DatabaseCallback> creationCallback, ExceptionCode& ec)
 {
     if (!m_frame)
         return 0;
@@ -1190,7 +1191,7 @@ PassRefPtr<Database> DOMWindow::openDatabase(const String& name, const String& v
     if (!document->securityOrigin()->canAccessDatabase())
         return 0;
 
-    return Database::openDatabase(document, name, version, displayName, estimatedSize, ec);
+    return Database::openDatabase(document, name, version, displayName, estimatedSize, creationCallback, ec);
 }
 #endif
 
