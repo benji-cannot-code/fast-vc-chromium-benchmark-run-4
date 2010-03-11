b7b7300f1a100aff2b957a3e7fb66eb931850c88
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-11 19:05:07 +0000

    Fix linux perf compile:
    fix "may be used uninitialized" error
    
    BUG=NONE
    TEST=buildbot compiles
    TBR=finnur
    
    Review URL: http://codereview.chromium.org/880002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41299 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_view_host.cc
+++ chrome/browser/renderer_host/render_view_host.cc
@@ -408,6 +408,7 @@ void RenderViewHost::StopFinding(
       break;
     default:
       NOTREACHED();
+      params.action = ViewMsg_StopFinding_Params::kKeepSelection;
   }
   Send(new ViewMsg_StopFinding(routing_id(), params));
 }
