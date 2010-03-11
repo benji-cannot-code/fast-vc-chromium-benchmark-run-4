4c5e31a83b808abf2c9809b53dec128b6124dcb3
zoltan@webkit.org <zoltan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-11 08:41:02 +0000

    2010-03-11  Zoltan Horvath  <zoltan@webkit.org>
            Reviewed by Simon Hausmann.
    
            Allow custom memory allocation control for TimingFunction struct
            https://bugs.webkit.org/show_bug.cgi?id=35855
    
            Inherits the following struct from FastAllocBase because it is 
            instantiated by 'new':
    
            class name    - instantiated at: WebCore/'location'
            TimingFuction - platform/graphics/GraphicsLayer.h:89
    
            * platform/animation/TimingFunction.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/animation/TimingFunction.h
+++ third_party/WebKit/WebCore/platform/animation/TimingFunction.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-struct TimingFunction {
+struct TimingFunction : FastAllocBase {
     TimingFunction()
         : m_type(CubicBezierTimingFunction)
         , m_x1(0.25)
