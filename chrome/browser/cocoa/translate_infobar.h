74c94020b136490c4909654f7491dfd9a5fd8350
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-11 17:34:41 +0000

    Revert 41274 - Unit Tests for OS X Translte infobars.
    This has had a few tests crash on the bots, so its starting out flaky/bad.
    
    BUG=None
    TEST=New unit tests, tree should stay green after this checkin.
    
    Review URL: http://codereview.chromium.org/669276
    
    TBR=jeremy@chromium.org
    Review URL: http://codereview.chromium.org/885001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41284 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/translate_infobar.h
+++ chrome/browser/cocoa/translate_infobar.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_ptr.h"
 #include "chrome/browser/translate/languages_menu_model.h"
 #include "chrome/browser/translate/options_menu_model.h"
-#include "chrome/browser/translate/translate_infobars_delegates.h"
 #include "chrome/common/notification_registrar.h"
 
 class TranslateInfoBarMenuModel;
@@ -56,30 +55,5 @@ class TranslateNotificationObserverBridge;
   scoped_ptr<TranslateInfoBarMenuModel> menu_model_;
   scoped_ptr<TranslateNotificationObserverBridge> observer_bridge_;
 }
-
-@end
-
-@interface TranslateInfoBarController (TestingAPI)
-
-// Main function to update the toolbar graphic state and data model after
-// the state has changed.
-// Controls are moved around as needed and visibility changed to match the
-// current state.
-- (void)updateState;
-
-// Called when the source or target language selection changes in a menu.
-// |newLanguageIdx| is the index of the newly selected item in the appropriate
-// menu.
-- (void)sourceLanguageModified:(NSInteger)newLanguageIdx;
-- (void)targetLanguageModified:(NSInteger)newLanguageIdx;
-
-// Called when an item in one of the toolbar's menus is selected.
-- (void)menuItemSelected:(id)item;
-
-// Returns the underlying options menu.
-- (NSMenu*)optionsMenu;
-
-// Verifies that the layout of the infobar is correct for |state|.
-- (bool)verifyLayout:(TranslateInfoBarDelegate::TranslateState)state;
 
 @end
