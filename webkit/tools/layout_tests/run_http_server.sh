2bb8d5e4da5ada840468f59effdb08661ea66df8
michaeln@chromium.org <michaeln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-11 01:42:37 +0000

    Add a script to 'run_http_server' in src/webkit/tools/layout_tests. Just a wrapper over the real tool in src/third_party/WebKit/WebKitTools/Scripts/
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/781003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41235 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/tools/layout_tests/run_http_server.sh
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+exec_dir=$(dirname $0)
+
+if [ "$OSTYPE" = "cygwin" ]; then
+  system_root=`cygpath "$SYSTEMROOT"`
+  PATH="/usr/bin:$system_root/system32:$system_root:$system_root/system32/WBEM"
+  export PATH
+  unset PYTHONPATH
+  PYTHON_PROG="$exec_dir/../../../third_party/python_24/python.exe"
+  SCRIPT=$(cygpath -wa "$exec_dir/run_http_server.py")
+else
+  PYTHON_PROG=python
+  unset PYTHONPATH
+  SCRIPT="$exec_dir/run_http_server.py"
+fi
+
+"$PYTHON_PROG" "$SCRIPT" "$@"
