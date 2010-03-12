37638d60ecbbfd0ffc6c4a37016c487a4cf87f07
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 22:18:53 +0000

    Change to check for MSVC instead of WINDOWS.
    BUG=none
    TEST=none
    TBR=dimich
    
    Review URL: http://codereview.chromium.org/850007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41497 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/image_decoder_unittest.h
+++ webkit/tools/test_shell/image_decoder_unittest.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "Vector.h"
-#if OS(WINDOWS)
+#if COMPILER(MSVC)
 #pragma warning(disable: 4355) // 'this' used in initializer list
 #endif
 #include "ImageDecoder.h"
