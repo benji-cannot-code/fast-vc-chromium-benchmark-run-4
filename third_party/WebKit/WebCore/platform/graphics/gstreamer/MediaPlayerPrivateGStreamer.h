73820e3c6bdd40a5bd6150be2fa8e0b6aaff7625
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-12 20:11:06 +0000

    2010-03-12  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
            Reviewed by Eric Carlson.
    
            media/video-preload.html fails
            https://bugs.webkit.org/show_bug.cgi?id=35793
    
            Only effectively load, and start buffering when playing, or when
            the preload attribute is set.
    
            Test: media/video-preload.html
    
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::MediaPlayerPrivate::MediaPlayerPrivate):
            (WebCore::MediaPlayerPrivate::load):
            (WebCore::MediaPlayerPrivate::commitLoad):
            (WebCore::MediaPlayerPrivate::prepareToPlay):
            (WebCore::MediaPlayerPrivate::setPreload):
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55927 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
+++ third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
@@ -64,9 +64,11 @@ class MediaPlayerPrivate : public MediaPlayerPrivateInterface {
             bool hasAudio() const;
 
             void load(const String &url);
+            void commitLoad();
             void cancelLoad();
             bool loadNextLocation();
 
+            void prepareToPlay();
             void play();
             void pause();
 
@@ -168,6 +170,8 @@ class MediaPlayerPrivate : public MediaPlayerPrivateInterface {
             guint m_fillTimeoutId;
             float m_maxTimeLoaded;
             int m_bufferingPercentage;
+            MediaPlayer::Preload m_preload;
+            bool m_delayingLoad;
     };
 }
 
