9b48d3e80d446773ab08ab84afc92dec943af329
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 14:46:38 +0000

    Mac: disable minimize in fullscreen mode.
    If it's set (in System Prefs) to minimize windows on double-click (on the title
    bar), double-clicking on the blank area of the floating bar will beep.
    
    BUG=37972
    TEST=Make sure Minimize is disabled in the Window menu, Cmd-M shouldn't work, and double-clicking on the blank area of the floating bar shouldn't minimize (it should beep if System Prefs is set as described above).
    
    Review URL: http://codereview.chromium.org/862007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41431 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/floating_bar_backing_view.mm
+++ chrome/browser/cocoa/floating_bar_backing_view.mm
@@ -38,7 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Eat this too, except that ...
 - (void)mouseUp:(NSEvent*)event {
-  // a double-click in the blank area should minimize.
+  // a double-click in the blank area should try to minimize, to be consistent
+  // with double-clicks on the contiguous tab strip area. (It'll fail and beep.)
   if ([event clickCount] == 2 &&
       mac_util::ShouldWindowsMiniaturizeOnDoubleClick())
     [[self window] performMiniaturize:self];
--- chrome/browser/cocoa/fullscreen_window.mm
+++ chrome/browser/cocoa/fullscreen_window.mm
@@ -72,21 +72,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 }
 
-// Similarly, we need this too....
-- (void)performMiniaturize:(id)sender {
-  [self miniaturize:sender];
-}
-
 - (BOOL)validateUserInterfaceItem:(id<NSValidatedUserInterfaceItem>)item {
   SEL action = [item action];
 
   // Explicitly enable |-performClose:| (see above); otherwise the fact that
   // this window does not have a close button results in it being disabled.
   if (action == @selector(performClose:))
-    return YES;
-
-  // Similarly for |-performMiniaturize:|.
-  if (action == @selector(performMiniaturize:))
     return YES;
 
   return [super validateUserInterfaceItem:item];
