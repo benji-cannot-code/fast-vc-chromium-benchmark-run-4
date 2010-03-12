b73d2fb57566bdbe85d3cbad60c67d9609da8222
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-12 18:03:24 +0000

    Build fix.
    * platform/chromium/PlatformKeyboardEventChromium.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55911 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/PlatformKeyboardEventChromium.cpp
+++ third_party/WebKit/WebCore/platform/chromium/PlatformKeyboardEventChromium.cpp
@@ -38,6 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+#if OS(WINDOWS)
+static const unsigned short HIGH_BIT_MASK_SHORT = 0x8000;
+#endif
+
 void PlatformKeyboardEvent::disambiguateKeyDownEvent(Type type, bool backwardCompatibilityMode)
 {
 #if OS(WINDOWS)
