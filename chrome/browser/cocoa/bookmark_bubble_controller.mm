91b5236dbf61a253bb84bf48a731cb047583d3b0
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 00:49:20 +0000

    Take 2: make bubble more tolerant of notification order when quitting.
    BUG=37807
    
    TEST=\
    Run a debug build of Chrome like this:  
      NSZombieEnabled=YES xcodebuild/Debug/Chromium.app/Contents/MacOS/Chromium 
    Open a bubble then Cmd-Q.
    Make sure you don't see a line like this:
      2010-03-10 19:24:20.381 Chromium[73024:903] *** -[ChromeBrowserWindow removeChildWindow:]: message sent to deallocated instance 0x18f8b2a0
    Repeat 20 times.
    
    Review URL: http://codereview.chromium.org/851005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41373 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bubble_controller.mm
+++ chrome/browser/cocoa/bookmark_bubble_controller.mm
@@ -124,6 +124,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)close {
   [parentWindow_ removeChildWindow:[self window]];
+
+  // If you quit while the bubble is open, sometimes we get a
+  // DidResignKey before we get our parent's WindowWillClose and
+  // sometimes not.  We protect against a multiple close (or reference
+  // to parentWindow_ at a bad time) by clearing it out once we're
+  // done, and by removing ourself from future notifications.
+  [[NSNotificationCenter defaultCenter]
+    removeObserver:self
+              name:NSWindowWillCloseNotification
+            object:parentWindow_];
+  parentWindow_ = nil;
+
   [super close];
 }
 
