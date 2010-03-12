f70ea0aaab81b50a18a851b222cc148af980aaea
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 20:31:13 +0000

    Lands http://codereview.chromium.org/669132 for Thiago:
    windows: Add UMA metrics for each time that the bookmarks toolbar menu
    button it's clicked.
    
    Since it isn't fully supported we need this to know how many people
    are using it.
    
    BUG=36191
    TEST=none
    
    Review URL: http://codereview.chromium.org/885005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/bookmark_menu_button.cc
+++ chrome/browser/views/bookmark_menu_button.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/browser.h"
 #include "chrome/browser/browser_theme_provider.h"
+#include "chrome/browser/metrics/user_metrics.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/view_ids.h"
@@ -29,12 +30,12 @@ BookmarkMenuButton::BookmarkMenuButton(Browser* browser)
       browser_(browser),
       bookmark_drop_menu_(NULL),
       drop_operation_(0) {
+  // TODO(sky): if we keep this code, we need real icons, a11y support, and a
+  // tooltip.
   set_menu_delegate(this);
   SetID(VIEW_ID_BOOKMARK_MENU);
 
   ThemeProvider* tp = browser_->profile()->GetThemeProvider();
-  // TODO(sky): if we keep this code, we need real icons, a11y support, and a
-  // tooltip.
   SetIcon(*tp->GetBitmapNamed(IDR_MENU_BOOKMARK));
 }
 
@@ -128,6 +129,9 @@ void BookmarkMenuButton::RunMenu(views::View* source,
                                  gfx::NativeWindow hwnd,
                                  bool for_drop) {
   Profile* profile = browser_->profile();
+
+  UserMetrics::RecordAction("BookmarkMenu_clicked", profile);
+
   BookmarkMenuController* menu = new BookmarkMenuController(
       browser_, profile, browser_->GetSelectedTabContents(), hwnd,
       GetBookmarkModel()->GetBookmarkBarNode(), 0, true);
