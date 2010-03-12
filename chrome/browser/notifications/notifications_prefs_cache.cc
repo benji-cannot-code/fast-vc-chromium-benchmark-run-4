7b925acfd58d2841cd195b77b7647f2b56fb5b51
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 00:33:24 +0000

    This DCHECK occurs when an installed extension that declares the notification permission is loaded.
    Review URL: http://codereview.chromium.org/842002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41368 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/notifications_prefs_cache.cc
+++ chrome/browser/notifications/notifications_prefs_cache.cc
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/WebKit/chromium/public/WebNotificationPresenter.h"
 
 NotificationsPrefsCache::NotificationsPrefsCache(
-    const ListValue* allowed, const ListValue* denied) {
+    const ListValue* allowed, const ListValue* denied)
+        : is_initialized_(false) {
   ListValue::const_iterator i;
   std::wstring origin;
   if (allowed) {
@@ -31,7 +32,7 @@ NotificationsPrefsCache::NotificationsPrefsCache(
 
 void NotificationsPrefsCache::CacheAllowedOrigin(
     const GURL& origin) {
-  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
+  CheckThreadAccess();
   std::set<GURL>::iterator iter;
   allowed_origins_.insert(origin);
   if ((iter = denied_origins_.find(origin)) != denied_origins_.end())
@@ -40,7 +41,7 @@ void NotificationsPrefsCache::CacheAllowedOrigin(
 
 void NotificationsPrefsCache::CacheDeniedOrigin(
     const GURL& origin) {
-  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
+  CheckThreadAccess();
   std::set<GURL>::iterator iter;
   denied_origins_.insert(origin);
   if ((iter = allowed_origins_.find(origin)) != allowed_origins_.end())
@@ -57,12 +58,20 @@ int NotificationsPrefsCache::HasPermission(const GURL& origin) {
 
 bool NotificationsPrefsCache::IsOriginAllowed(
     const GURL& origin) {
-  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
+  CheckThreadAccess();
   return (allowed_origins_.find(origin) != allowed_origins_.end());
 }
 
 bool NotificationsPrefsCache::IsOriginDenied(
     const GURL& origin) {
-  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
+  CheckThreadAccess();
   return (denied_origins_.find(origin) != denied_origins_.end());
+}
+
+void NotificationsPrefsCache::CheckThreadAccess() {
+  if (is_initialized_) {
+    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
+  } else {
+    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
+  }
 }
