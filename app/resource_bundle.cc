f64b8d2242361e878266c3f03021a52c163486f4
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 05:42:26 +0000

    Remove some unnecessary dependencies on net from app/win_util and app/resource_bundle.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/858006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41401 0039d316-1c4b-4281-b951-d872f2087c98

--- app/resource_bundle.cc
+++ app/resource_bundle.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/gfx/font.h"
 #include "base/logging.h"
 #include "base/string_piece.h"
-#include "net/base/file_stream.h"
-#include "net/base/net_errors.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 
 ResourceBundle* ResourceBundle::g_shared_instance_ = NULL;
