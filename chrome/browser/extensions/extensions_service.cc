890498829091b714ba413cb01e44671e9f4f23c0
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 18:12:44 +0000

    Fix a bug with persistent unpacked extensions where we would not pick up manifest changes on browser restart.
    BUG=24850
    
    Review URL: http://codereview.chromium.org/789007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41448 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service.cc
+++ chrome/browser/extensions/extensions_service.cc
@@ -84,6 +84,16 @@ class InstalledExtensionSet {
   std::map<std::string, std::string> versions_;
 };
 
+static bool ShouldReloadExtensionManifest(const ExtensionInfo& info) {
+  // Always reload LOAD extension manifests, because they can change on disk
+  // independent of the manifest in our prefs.
+  if (info.extension_location == Extension::LOAD)
+    return true;
+
+  // Otherwise, reload the manifest it needs to be relocalized.
+  return extension_l10n_util::ShouldRelocalizeManifest(info);
+}
+
 }  // namespace
 
 // ExtensionsService.
@@ -357,11 +367,11 @@ void ExtensionsService::LoadAllExtensions() {
   // If any extensions need localization, we bounce them all to the file thread
   // for re-reading and localization.
   for (size_t i = 0; i < info->size(); ++i) {
-    if (extension_l10n_util::ShouldRelocalizeManifest(*info->at(i))) {
+    if (ShouldReloadExtensionManifest(*info->at(i))) {
       ChromeThread::PostTask(
         ChromeThread::FILE, FROM_HERE, NewRunnableMethod(
             backend_.get(),
-            &ExtensionsServiceBackend::ReloadExtensionManifestsForLocaleChanged,
+            &ExtensionsServiceBackend::ReloadExtensionManifests,
             info.release(),  // Callee takes ownership of the memory.
             start_time,
             scoped_refptr<ExtensionsService>(this)));
@@ -1122,7 +1132,7 @@ void ExtensionsServiceBackend::OnExternalExtensionFound(
           version->GetString(), path, location));
 }
 
-void ExtensionsServiceBackend::ReloadExtensionManifestsForLocaleChanged(
+void ExtensionsServiceBackend::ReloadExtensionManifests(
     ExtensionPrefs::ExtensionsInfo* extensions_to_reload,
     base::TimeTicks start_time,
     scoped_refptr<ExtensionsService> frontend) {
@@ -1130,10 +1140,7 @@ void ExtensionsServiceBackend::ReloadExtensionManifestsForLocaleChanged(
 
   for (size_t i = 0; i < extensions_to_reload->size(); ++i) {
     ExtensionInfo* info = extensions_to_reload->at(i).get();
-    if (!info->extension_manifest.get())
-      continue;
-
-    if (!extension_l10n_util::ShouldRelocalizeManifest(*info))
+    if (!ShouldReloadExtensionManifest(*info))
       continue;
 
     // We need to reload original manifest in order to localize properly.
