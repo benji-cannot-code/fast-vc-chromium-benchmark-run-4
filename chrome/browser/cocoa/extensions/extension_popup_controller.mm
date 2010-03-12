6e3c90e19279b15696a47da321b2942b7576f5d2
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 19:30:00 +0000

    Change Mac to DCHECK if NetworkChangeNotifier observer list is not empty by destruction time.
    Review URL: http://codereview.chromium.org/870003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41459 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/extensions/extension_popup_controller.mm
+++ chrome/browser/cocoa/extensions/extension_popup_controller.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/logging.h"
 #include "chrome/browser/browser.h"
 #import "chrome/browser/cocoa/browser_window_cocoa.h"
 #import "chrome/browser/cocoa/extension_view_mac.h"
@@ -92,6 +93,7 @@ CGFloat Clamp(CGFloat value, CGFloat min, CGFloat max) {
 }
 
 - (void)dealloc {
+  LOG(ERROR) << "Yep, I'm getting deleted.";
   [[NSNotificationCenter defaultCenter] removeObserver:self];
   [super dealloc];
 }
