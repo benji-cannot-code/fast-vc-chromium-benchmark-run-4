c21d2539ec259edcff8574d54f9b063aa84b99cc
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 05:57:53 +0000

    Reset the AutoFillManager's cache of forms when the main page is loaded, not when forms are sent to be stored.
    BUG=37990
    TEST=Form at ebay.com; in bug.
    Review URL: http://codereview.chromium.org/906002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41403 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager.cc
+++ chrome/browser/autofill/autofill_manager.cc
@@ -75,7 +75,6 @@ void AutoFillManager::FormsSeen(
   if (!IsAutoFillEnabled())
     return;
 
-  form_structures_.reset();
   for (std::vector<webkit_glue::FormFieldValues>::const_iterator iter =
            forms.begin();
        iter != forms.end(); ++iter) {
@@ -254,6 +253,7 @@ void AutoFillManager::OnInfoBarAccepted() {
 
 void AutoFillManager::Reset() {
   upload_form_structure_.reset();
+  form_structures_.reset();
 }
 
 void AutoFillManager::DeterminePossibleFieldTypes(
--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -1485,6 +1485,10 @@ void TabContents::DidNavigateMainFramePostCommit(
 
   // Update the starred state.
   UpdateStarredStateForCurrentURL();
+
+  // Clear the cache of forms in AutoFill.
+  if (autofill_manager_.get())
+    autofill_manager_->Reset();
 }
 
 void TabContents::DidNavigateAnyFramePostCommit(
