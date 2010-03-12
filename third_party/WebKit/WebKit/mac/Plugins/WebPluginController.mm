9a441d60d3f67cfa774347e9923ca90e7a269fc8
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-12 15:45:25 +0000

    2010-03-12  Andy Estes  <aestes@apple.com>
            Reviewed by Brady Eidson.
    
            Remove an unused method.
    
            https://bugs.webkit.org/show_bug.cgi?id=35940
    
            * Plugins/WebPluginController.mm:
            removed - (void)showURL:(NSURL *) inFrame:(NSString *)
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebPluginController.mm
+++ third_party/WebKit/WebKit/mac/Plugins/WebPluginController.mm
@@ -352,12 +352,6 @@ static void cancelOutstandingCheck(const void *item, void *context)
     }
 }
 
-// For compatibility only.
-- (void)showURL:(NSURL *)URL inFrame:(NSString *)target
-{
-    [self webPlugInContainerLoadRequest:[NSURLRequest requestWithURL:URL] inFrame:target];
-}
-
 - (void)webPlugInContainerShowStatus:(NSString *)message
 {
     if (!message) {
