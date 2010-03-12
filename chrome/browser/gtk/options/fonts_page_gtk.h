b8088f2c14d354b31009deaf11656cab6a488424
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 21:40:49 +0000

    Re-apply r41458 | estade@chromium.org | 2010-03-12 11:28:50 -0800 (Fri, 12 Mar 2010) | 8 lines
    Fix the rest of the gtk/options directory to use new callback macros.
    
    Problematically, the callback macros don't cover the case where the GObject* isn't a GtkWidget* D:
    
    BUG=None
    TEST=compile
    
    with fix for unit test
    
    Review URL: http://codereview.chromium.org/846005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/options/fonts_page_gtk.h
+++ chrome/browser/gtk/options/fonts_page_gtk.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gtk/gtk.h>
 #include <vector>
 
+#include "app/gtk_signal.h"
 #include "chrome/browser/character_encoding.h"
 #include "chrome/browser/pref_member.h"
 #include "chrome/browser/options_page_base.h"
@@ -37,17 +38,12 @@ class FontsPageGtk : public OptionsPageBase {
   // GtkFontSelector returns the value in points not pixels.
   void SetFontsFromButton(StringPrefMember* name_pref,
                           IntegerPrefMember* size_pref,
-                          GtkFontButton* font_button);
+                          GtkWidget* font_button);
 
-  // Callbacks
-  static void OnSerifFontSet(GtkFontButton* font_button,
-                             FontsPageGtk* fonts_page);
-  static void OnSansFontSet(GtkFontButton* font_button,
-                            FontsPageGtk* fonts_page);
-  static void OnFixedFontSet(GtkFontButton* font_button,
-                             FontsPageGtk* fonts_page);
-  static void OnDefaultEncodingChanged(GtkComboBox* combo_box,
-                                       FontsPageGtk* fonts_page);
+  CHROMEGTK_CALLBACK_0(FontsPageGtk, void, OnSerifFontSet);
+  CHROMEGTK_CALLBACK_0(FontsPageGtk, void, OnSansFontSet);
+  CHROMEGTK_CALLBACK_0(FontsPageGtk, void, OnFixedFontSet);
+  CHROMEGTK_CALLBACK_0(FontsPageGtk, void, OnDefaultEncodingChanged);
 
   // The font chooser widgets
   GtkWidget* serif_font_button_;
--- chrome/browser/gtk/options/passwords_exceptions_page_gtk.h
+++ chrome/browser/gtk/options/passwords_exceptions_page_gtk.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "app/gtk_signal.h"
 #include "chrome/browser/password_manager/password_store.h"
 #include "chrome/browser/profile.h"
 
@@ -35,17 +36,13 @@ class PasswordsExceptionsPageGtk {
   // Sets the exception list contents to the given data.
   void SetExceptionList(const std::vector<webkit_glue::PasswordForm*>& result);
 
-  // Callback for the remove button.
-  static void OnRemoveButtonClicked(GtkButton* widget,
-                                    PasswordsExceptionsPageGtk* page);
+  CHROMEGTK_CALLBACK_0(PasswordsExceptionsPageGtk, void, OnRemoveButtonClicked);
+  CHROMEGTK_CALLBACK_0(PasswordsExceptionsPageGtk, void,
+                       OnRemoveAllButtonClicked);
 
-  // Callback for the remove all button.
-  static void OnRemoveAllButtonClicked(GtkButton* widget,
-                                       PasswordsExceptionsPageGtk* page);
-
-  // Callback for selection changed events.
-  static void OnExceptionSelectionChanged(GtkTreeSelection* selection,
-                                          PasswordsExceptionsPageGtk* page);
+  static void OnExceptionSelectionChanged(
+      GtkTreeSelection* selection,
+      PasswordsExceptionsPageGtk* page);
 
   // Sorting function.
   static gint CompareSite(GtkTreeModel* model,
@@ -64,6 +61,7 @@ class PasswordsExceptionsPageGtk {
     // PasswordsExceptionsPageGtk.
     void populate();
 
+    // PasswordStoreConsumer implementation.
     // Send the password store's reply back to the PasswordsExceptionsPageGtk.
     virtual void OnPasswordStoreRequestDone(
         int handle, const std::vector<webkit_glue::PasswordForm*>& result);
