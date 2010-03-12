33c81627cd224866f5dfd824b7ff6f635e77aa50
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-12 22:10:22 +0000

    Make canary bot happy by disabling warning resulted from StringImpl.h.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/892003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41496 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/image_decoder_unittest.h
+++ webkit/tools/test_shell/image_decoder_unittest.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "Vector.h"
+#if OS(WINDOWS)
+#pragma warning(disable: 4355) // 'this' used in initializer list
+#endif
 #include "ImageDecoder.h"
 
 #undef LOG
