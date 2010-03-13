5a87cf11b1dbe53064c901d53a1402318d52e281
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-13 13:43:33 +0000

    2010-03-13  Dirk Schulze  <krit@webkit.org>
            No review, rolling out r55927.
            http://trac.webkit.org/changeset/55927
            https://bugs.webkit.org/show_bug.cgi?id=35793
    
            Breaks Gtk build bots.
    
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::MediaPlayerPrivate::MediaPlayerPrivate):
            (WebCore::MediaPlayerPrivate::load):
            (WebCore::MediaPlayerPrivate::setPreload):
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55962 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
+++ third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
@@ -64,11 +64,9 @@ class MediaPlayerPrivate : public MediaPlayerPrivateInterface {
             bool hasAudio() const;
 
             void load(const String &url);
-            void commitLoad();
             void cancelLoad();
             bool loadNextLocation();
 
-            void prepareToPlay();
             void play();
             void pause();
 
@@ -170,8 +168,6 @@ class MediaPlayerPrivate : public MediaPlayerPrivateInterface {
             guint m_fillTimeoutId;
             float m_maxTimeLoaded;
             int m_bufferingPercentage;
-            MediaPlayer::Preload m_preload;
-            bool m_delayingLoad;
     };
 }
 
