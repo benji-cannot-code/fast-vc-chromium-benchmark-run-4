5fd9845a5c399f88ae5b02a9bb002854142e4d9e
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-13 08:14:24 +0000

    2010-03-13  Jesus Sanchez-Palencia  <jesus.palencia@openbossa.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            Add a "Toggle FullScreen" option to QtLauncher Menu.
    
            [Qt] QtLauncher needs an option to toggle FullScreen Mode
            https://bugs.webkit.org/show_bug.cgi?id=35755
    
            * QtLauncher/main.cpp:
            (LauncherWindow::init):
            (LauncherWindow::eventFilter):
            (LauncherWindow::initializeView):
            (LauncherWindow::toggleFullScreenMode):
            (LauncherWindow::createChrome):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/QtLauncher/main.cpp
+++ third_party/WebKit/WebKitTools/QtLauncher/main.cpp
@@ -71,7 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 void QWEBKIT_EXPORT qt_drt_garbageCollector_collect();
 #endif
 
-
+static const int gExitClickArea = 80;
 static bool gUseGraphicsView = false;
 static bool gUseCompositing = false;
 static bool gCacheWebView = false;
@@ -91,8 +91,9 @@ public:
 
 #if QT_VERSION >= QT_VERSION_CHECK(4, 6, 0)
     void sendTouchEvent();
-    bool eventFilter(QObject* obj, QEvent* event);
 #endif
+
+    bool eventFilter(QObject* obj, QEvent* event);
 
 protected slots:
     void loadStarted();
@@ -120,10 +121,14 @@ protected slots:
     void toggleWebGL(bool toggle);
     void initializeView(bool useGraphicsView = false);
     void toggleSpatialNavigation(bool b);
+    void toggleFullScreenMode(bool enable);
 
 public slots:
     void newWindow();
     void cloneWindow();
+
+signals:
+    void enteredFullScreenMode(bool on);
 
 private:
     void createChrome();
@@ -184,8 +189,9 @@ void LauncherWindow::init(bool useGraphicsView)
     setCentralWidget(splitter);
 
 #if defined(Q_WS_S60)
-    showMaximized();
+    setWindowState(Qt::WindowMaximized);
 #else
+    setWindowState(Qt::WindowNoState);
     resize(800, 600);
 #endif
 
@@ -195,6 +201,7 @@ void LauncherWindow::init(bool useGraphicsView)
     connect(page(), SIGNAL(loadFinished(bool)), this, SLOT(loadFinished()));
     connect(page(), SIGNAL(linkHovered(const QString&, const QString&, const QString&)),
             this, SLOT(showLinkHover(const QString&, const QString&)));
+    connect(this, SIGNAL(enteredFullScreenMode(bool)), this, SLOT(toggleFullScreenMode(bool)));
 
     if (!gInspectorUrl.isEmpty())
       page()->settings()->setInspectorUrl(gInspectorUrl);
@@ -305,9 +312,23 @@ void LauncherWindow::sendTouchEvent()
     if (m_touchPoints.size() > 1 && m_touchPoints[1].state() == Qt::TouchPointReleased)
         m_touchPoints.removeAt(1);
 }
+#endif // QT_VERSION >= QT_VERSION_CHECK(4, 6, 0)
 
 bool LauncherWindow::eventFilter(QObject* obj, QEvent* event)
 {
+    // If click pos is the bottom right corner (square with size defined by gExitClickArea)
+    // and the window is on FullScreen, the window must return to its original state.
+    if (event->type() == QEvent::MouseButtonRelease) {
+        QMouseEvent* ev = static_cast<QMouseEvent*>(event);
+        if (windowState() == Qt::WindowFullScreen
+            && ev->pos().x() > (width() - gExitClickArea)
+            && ev->pos().y() > (height() - gExitClickArea)) {
+
+            emit enteredFullScreenMode(false);
+        }
+    }
+
+#if QT_VERSION >= QT_VERSION_CHECK(4, 6, 0)
     if (!m_touchMocking || obj != m_view)
         return QObject::eventFilter(obj, event);
 
@@ -369,9 +390,10 @@ bool LauncherWindow::eventFilter(QObject* obj, QEvent* event)
             m_touchPoints.last().setState(Qt::TouchPointStationary);
         }
     }
+#endif // QT_VERSION >= QT_VERSION_CHECK(4, 6, 0)
+
     return false;
 }
-#endif // QT_VERSION >= QT_VERSION_CHECK(4, 6, 0)
 
 void LauncherWindow::loadStarted()
 {
@@ -518,6 +540,9 @@ void LauncherWindow::initializeView(bool useGraphicsView)
     if (!useGraphicsView) {
         WebViewTraditional* view = new WebViewTraditional(splitter);
         view->setPage(page());
+
+        view->installEventFilter(this);
+
         m_view = view;
     } else {
         WebViewGraphicsBased* view = new WebViewGraphicsBased(splitter);
@@ -529,11 +554,14 @@ void LauncherWindow::initializeView(bool useGraphicsView)
         if (m_flipYAnimated)
             connect(m_flipYAnimated, SIGNAL(triggered()), view, SLOT(animatedYFlip()));
 
+        // The implementation of QAbstractScrollArea::eventFilter makes us need
+        // to install the event filter on the viewport of a QGraphicsView.
+        view->viewport()->installEventFilter(this);
+
         m_view = view;
     }
 
 #if QT_VERSION >= QT_VERSION_CHECK(4, 6, 0)
-    m_view->installEventFilter(this);
     m_touchMocking = false;
 #endif
 }
@@ -541,6 +569,19 @@ void LauncherWindow::initializeView(bool useGraphicsView)
 void LauncherWindow::toggleSpatialNavigation(bool b)
 {
     page()->settings()->setAttribute(QWebSettings::SpatialNavigationEnabled, b);
+}
+
+void LauncherWindow::toggleFullScreenMode(bool enable)
+{
+    if (enable)
+        setWindowState(Qt::WindowFullScreen);
+    else {
+#if defined(Q_WS_S60)
+        setWindowState(Qt::WindowMaximized);
+#else
+        setWindowState(Qt::WindowNoState);
+#endif
+    }
 }
 
 void LauncherWindow::newWindow()
@@ -606,6 +647,15 @@ void LauncherWindow::createChrome()
     zoomIn->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_Plus));
     zoomOut->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_Minus));
     resetZoom->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_0));
+
+    QMenu* windowMenu = menuBar()->addMenu("&Window");
+    QAction* toggleFullScreen = windowMenu->addAction("Toggle FullScreen", this, SIGNAL(enteredFullScreenMode(bool)));
+    toggleFullScreen->setCheckable(true);
+    toggleFullScreen->setChecked(false);
+
+    // when exit fullscreen mode by clicking on the exit area (bottom right corner) we must
+    // uncheck the Toggle FullScreen action
+    toggleFullScreen->connect(this, SIGNAL(enteredFullScreenMode(bool)), SLOT(setChecked(bool)));
 
     QMenu* toolsMenu = menuBar()->addMenu("&Develop");
     toolsMenu->addAction("Select Elements...", this, SLOT(selectElements()));
