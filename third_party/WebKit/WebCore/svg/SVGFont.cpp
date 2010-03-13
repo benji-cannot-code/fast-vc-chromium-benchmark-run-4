54caef3926d154874a56b6692beb4c1368b85195
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-13 07:07:43 +0000

    2010-03-12  Dirk Schulze  <krit@webkit.org>
            Reviewed by Nikolas Zimmermann.
    
            SVG Glyphs - transform path not context
            https://bugs.webkit.org/show_bug.cgi?id=36070
    
            SVGFont should transform the path of a glyph, not the context. Modifying
            the context causes wrong gradient transformations. This bug doesn't
            influence CG because we generally fill or stroke texts with a mask image.
            All other platforms provide a direct way to make the drawings.
    
            * svg/SVGFont.cpp:
            (WebCore::Font::drawTextUsingSVGFont):
    2010-03-12  Dirk Schulze  <krit@webkit.org>
    
            Reviewed by Nikolas Zimmermann.
    
            SVG Glyphs - transform path not context
            https://bugs.webkit.org/show_bug.cgi?id=36070
    
            Some tests don't look pixel perfect after the SVGFont changes (difference of 0.1%).
            They just need an update.
            Tests for the changes on other platforms than CG are
                * svg/batik/text/textEffect.svg
                * svg/batik/text/textEffect3.svg
                * svg/W3C-SVG-1.1/pservers-grad-08-b.svg
    
            * platform/mac-leopard/svg/custom/svg-fonts-in-html-expected.checksum:
            * platform/mac-leopard/svg/custom/svg-fonts-in-html-expected.png:
            * platform/mac/svg/W3C-SVG-1.1/animate-elem-03-t-expected.checksum:
            * platform/mac/svg/W3C-SVG-1.1/animate-elem-03-t-expected.png:
            * platform/mac/svg/W3C-SVG-1.1/animate-elem-36-t-expected.checksum:
            * platform/mac/svg/W3C-SVG-1.1/animate-elem-36-t-expected.png:
            * platform/mac/svg/W3C-SVG-1.1/animate-elem-40-t-expected.checksum:
            * platform/mac/svg/W3C-SVG-1.1/animate-elem-40-t-expected.png:
            * platform/mac/svg/W3C-SVG-1.1/pservers-grad-08-b-expected.checksum:
            * platform/mac/svg/W3C-SVG-1.1/pservers-grad-08-b-expected.png:
            * platform/mac/svg/batik/text/textEffect-expected.checksum:
            * platform/mac/svg/batik/text/textEffect-expected.png:
            * platform/mac/svg/batik/text/textEffect3-expected.checksum:
            * platform/mac/svg/batik/text/textEffect3-expected.png:
            * platform/mac/svg/text/selection-doubleclick-expected.png:
            * platform/mac/svg/text/selection-tripleclick-expected.png:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@55949 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGFont.cpp
+++ third_party/WebKit/WebCore/svg/SVGFont.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * Copyright (C) 2007, 2008 Nikolas Zimmermann <zimmermann@kde.org>
+ * Copyright (C) Research In Motion Limited 2010. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -529,22 +530,16 @@ void Font::drawTextUsingSVGFont(GraphicsContext* context, const TextRun& run,
                         glyphOrigin.setY(identifier.verticalOriginY * scale);
                     }
 
-                    context->translate(xStartOffset + currentPoint.x() + glyphOrigin.x(), currentPoint.y() + glyphOrigin.y());
-                    context->scale(FloatSize(scale, -scale));
+                    AffineTransform glyphPathTransform;
+                    glyphPathTransform.translate(xStartOffset + currentPoint.x() + glyphOrigin.x(), currentPoint.y() + glyphOrigin.y());
+                    glyphPathTransform.scale(scale, -scale);
+
+                    Path glyphPath = identifier.pathData;
+                    glyphPath.transform(glyphPathTransform);
 
                     context->beginPath();
-                    context->addPath(identifier.pathData);
-
-                    // FIXME: setup() tries to get objectBoundingBox() from run.referencingRenderObject()
-                    // which is wrong.  We need to change setup() to take a bounding box instead, or pass
-                    // a RenderObject which would return the bounding box for identifier.pathData
+                    context->addPath(glyphPath);
                     if (activePaintServer->setup(context, run.referencingRenderObject(), targetType)) {
-                        // Spec: Any properties specified on a text elements which represents a length, such as the
-                        // 'stroke-width' property, might produce surprising results since the length value will be
-                        // processed in the coordinate system of the glyph. (TODO: What other lengths? miter-limit? dash-offset?)
-                        if (targetType == ApplyToStrokeTargetType && scale != 0.0f)
-                            context->setStrokeThickness(context->strokeThickness() / scale);
-
                         activePaintServer->renderPath(context, run.referencingRenderObject(), targetType);
                         activePaintServer->teardown(context, run.referencingRenderObject(), targetType);
                     }
