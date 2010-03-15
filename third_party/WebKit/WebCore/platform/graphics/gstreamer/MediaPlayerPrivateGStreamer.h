d7db0a1e28f5b2cc29a3ec75a06f05488d9b8189
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-15 20:18:47 +0000

    2010-03-15  Philippe Normand  <pnormand@igalia.com>
            Reviewed by Gustavo Noronha Silva.
    
            [GStreamer] duration query optimizations
            https://bugs.webkit.org/show_bug.cgi?id=36116
    
            Don't reattempt duration queries that previously failed and cache
            media duration only if it's known.
    
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::MediaPlayerPrivate::MediaPlayerPrivate):
            (WebCore::MediaPlayerPrivate::duration):
            (WebCore::MediaPlayerPrivate::updateStates):
            (WebCore::MediaPlayerPrivate::durationChanged):
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56010 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
+++ third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
@@ -171,6 +171,7 @@ class MediaPlayerPrivate : public MediaPlayerPrivateInterface {
             int m_bufferingPercentage;
             MediaPlayer::Preload m_preload;
             bool m_delayingLoad;
+            bool m_mediaDurationKnown;
     };
 }
 
