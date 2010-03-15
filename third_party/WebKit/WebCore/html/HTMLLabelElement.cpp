873732f699de93b9419e58e5f94d2a824af68fc8
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-15 22:07:20 +0000

    VO not able to perform a VO-spacebar on facebook links https://bugs.webkit.org/show_bug.cgi?id=36132
    Reviewed by Beth Dakin.
    
    WebCore: 
    
    When a label element is used as a click event handler, and it doesn't have any
    corresponding control, it should handle the action itself.
    
    Test: accessibility/label-element-press.html
    
    * html/HTMLLabelElement.cpp:
    (WebCore::HTMLLabelElement::accessKeyAction):
    
    LayoutTests: 
    
    * accessibility/label-element-press-expected.txt: Added.
    * accessibility/label-element-press.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56019 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLLabelElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLLabelElement.cpp
@@ -140,6 +140,8 @@ void HTMLLabelElement::accessKeyAction(bool sendToAnyElement)
 {
     if (HTMLElement* element = correspondingControl())
         element->accessKeyAction(sendToAnyElement);
+    else
+        HTMLElement::accessKeyAction(sendToAnyElement);
 }
 
 String HTMLLabelElement::accessKey() const
