53a2877cb596557f4e3a565b343fada91813fbca
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-15 23:19:30 +0000

    Fix ExtensionApiTest.Storage. openDatabase() always takes at least 4 parameters.
    BUG=22006
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/982003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41653 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_storage_apitest.cc
+++ chrome/browser/extensions/extension_storage_apitest.cc
@@ -5,11 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-#if defined(OS_WIN)
-// http://crbug.com/22006
-#define Storage FLAKY_Storage
-#endif
-
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Storage) {
   ASSERT_TRUE(RunExtensionTest("storage")) << message_;
 }
