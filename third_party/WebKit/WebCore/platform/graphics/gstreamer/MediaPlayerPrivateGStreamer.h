edf8b3aa702f3c520ad4ca43b279012a42588180
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-15 20:11:55 +0000

    2010-03-15  Philippe Normand  <pnormand@igalia.com>
            Reviewed by Gustavo Noronha Silva.
    
            [GStreamer] updateStates called for all elements posting a state-change
            https://bugs.webkit.org/show_bug.cgi?id=36115
    
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::mediaPlayerPrivateMessageCallback): Update player state
            only for state-change messages coming from playbin2 directly.
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h:
            (WebCore::MediaPlayerPrivate::pipeline): public accessor for the
            playbin element.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
+++ third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
@@ -119,6 +119,7 @@ class MediaPlayerPrivate : public MediaPlayerPrivateInterface {
 
             bool supportsFullscreen() const;
 
+            GstElement* pipeline() const { return m_playBin; }
             bool pipelineReset() const { return m_resetPipeline; }
 
         private:
