af77eddea6730c21c6910ba32bce798566c085e1
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-15 18:54:04 +0000

    Fix header include guards - remove BASE_ prefix that is no longer accurate.
    TBR=darin
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/972003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41614 0039d316-1c4b-4281-b951-d872f2087c98

--- gfx/point.h
+++ gfx/point.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BASE_GFX_POINT_H__
-#define BASE_GFX_POINT_H__
+#ifndef GFX_POINT_H_
+#define GFX_POINT_H_
 
 #include "build/build_config.h"
 
@@ -78,4 +78,4 @@ class Point {
 
 std::ostream& operator<<(std::ostream& out, const gfx::Point& p);
 
-#endif  // BASE_GFX_POINT_H__
+#endif  // GFX_POINT_H_
