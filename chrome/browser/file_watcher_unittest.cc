04716b3a857956fd223e302d2f945861b44b0e0d
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-15 09:09:04 +0000

    Disable FileWatcher unittests on mac, they're flakey.
    TBR=phajdan.jr
    
    Review URL: http://codereview.chromium.org/957001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41568 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/file_watcher_unittest.cc
+++ chrome/browser/file_watcher_unittest.cc
@@ -129,6 +129,9 @@ class TestDelegate : public FileWatcher::Delegate {
   bool got_notification_;
 };
 
+
+#if !defined(OS_MACOSX)
+// TODO(tony): Test is flakey on mac.  http://crbug.com/38188
 // Basic test: Create the file and verify that we notice.
 TEST_F(FileWatcherTest, NewFile) {
   FileWatcher watcher;
@@ -240,4 +243,5 @@ TEST_F(FileWatcherTest, NonExistentDirectory) {
   ASSERT_FALSE(watcher.Watch(test_file().AppendASCII("FileToWatch"), NULL));
 }
 
+#endif
 }  // namespace
