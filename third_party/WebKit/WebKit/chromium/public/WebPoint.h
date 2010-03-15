3fe5e3803cc6e4033a009052eacb097731c891c0
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-15 23:28:07 +0000

    2010-03-15  Darin Fisher  <darin@chromium.org>
            Reviewed by David Levin.
    
            https://bugs.webkit.org/show_bug.cgi?id=36131
            Update include paths for chromium Rect, Point, and Size types.
    
            Update DEPS to pull latest chromium code that reflects the
            new location of these headers.
    
            * DEPS:
            * public/WebPoint.h:
            * public/WebRect.h:
            * public/WebSize.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56024 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebPoint.h
+++ third_party/WebKit/WebKit/chromium/public/WebPoint.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if WEBKIT_IMPLEMENTATION
 #include "IntPoint.h"
 #else
-#include <base/gfx/rect.h>
+#include <gfx/point.h>
 #endif
 
 namespace WebKit {
