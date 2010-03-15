3a0d3bb9c7398f915ad7d0a0e0d69952cecc90a1
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-15 21:03:13 +0000

    Making browser bubble notifies to its delegate whether it lost focus to a child window.  This is used to prevent closing browser/page action bubble when they lose focus to a select popup or alert box.
    BUG=37784
    TEST=Start Chrome with extensions that have a page action
         and browser action that contains selects (combobox)
         and a way to show an alert box.  Verify that showing
         the select popup or the alert dialog does not cause
         the bubble to close.
    
    Review URL: http://codereview.chromium.org/899002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41630 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/browser_bubble_gtk.cc
+++ chrome/browser/views/browser_bubble_gtk.cc
@@ -35,7 +35,7 @@ class BubbleWidget : public views::WidgetGtk {
     if (IsActive()) {
       BrowserBubble::Delegate* delegate = bubble_->delegate();
       if (delegate)
-        delegate->BubbleLostFocus(bubble_, NULL);
+        delegate->BubbleLostFocus(bubble_, false);
     }
     views::WidgetGtk::Close();
   }
@@ -44,7 +44,7 @@ class BubbleWidget : public views::WidgetGtk {
     if (IsActive()) {
       BrowserBubble::Delegate* delegate = bubble_->delegate();
       if (delegate)
-        delegate->BubbleLostFocus(bubble_, NULL);
+        delegate->BubbleLostFocus(bubble_, false);
     }
     views::WidgetGtk::Hide();
   }
@@ -59,7 +59,10 @@ class BubbleWidget : public views::WidgetGtk {
       return;
     }
 
-    delegate->BubbleLostFocus(bubble_, GetNativeView());
+    // TODO(jcampan): http://crbugs.com/29131 Check if the window we are losing
+    //                focus to is a child window and pass that to the call
+    //                below.
+    delegate->BubbleLostFocus(bubble_, false);
   }
 
   virtual gboolean OnFocusIn(GtkWidget* widget, GdkEventFocus* event) {
