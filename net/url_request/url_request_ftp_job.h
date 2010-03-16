c93715247d9df9a52fd5410319c476770063a306
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-16 12:40:13 +0000

    Rename url_request_new_ftp_job -> url_request_ftp_job
    TEST=net_unittests
    BUG=none
    Review URL: http://codereview.chromium.org/960001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41713 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_ftp_job.h
+++ net/url_request/url_request_ftp_job.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_URL_REQUEST_URL_REQUEST_NEW_FTP_JOB_H_
-#define NET_URL_REQUEST_URL_REQUEST_NEW_FTP_JOB_H_
+#ifndef NET_URL_REQUEST_URL_REQUEST_FTP_JOB_H_
+#define NET_URL_REQUEST_URL_REQUEST_FTP_JOB_H_
 
 #include <string>
 
@@ -22,10 +22,10 @@ struct list_state;
 
 // A URLRequestJob subclass that is built on top of FtpTransaction. It
 // provides an implementation for FTP.
-class URLRequestNewFtpJob : public URLRequestJob {
+class URLRequestFtpJob : public URLRequestJob {
  public:
 
-  explicit URLRequestNewFtpJob(URLRequest* request);
+  explicit URLRequestFtpJob(URLRequest* request);
 
   static URLRequestJob* Factory(URLRequest* request, const std::string& scheme);
 
@@ -33,7 +33,7 @@ class URLRequestNewFtpJob : public URLRequestJob {
   virtual bool GetMimeType(std::string* mime_type) const;
 
  private:
-  virtual ~URLRequestNewFtpJob();
+  virtual ~URLRequestFtpJob();
 
   // URLRequestJob methods:
   virtual void Start();
@@ -63,8 +63,8 @@ class URLRequestNewFtpJob : public URLRequestJob {
   net::FtpRequestInfo request_info_;
   scoped_ptr<net::FtpTransaction> transaction_;
 
-  net::CompletionCallbackImpl<URLRequestNewFtpJob> start_callback_;
-  net::CompletionCallbackImpl<URLRequestNewFtpJob> read_callback_;
+  net::CompletionCallbackImpl<URLRequestFtpJob> start_callback_;
+  net::CompletionCallbackImpl<URLRequestFtpJob> read_callback_;
 
   bool read_in_progress_;
 
@@ -74,7 +74,7 @@ class URLRequestNewFtpJob : public URLRequestJob {
   // before us.
   scoped_refptr<URLRequestContext> context_;
 
-  DISALLOW_COPY_AND_ASSIGN(URLRequestNewFtpJob);
+  DISALLOW_COPY_AND_ASSIGN(URLRequestFtpJob);
 };
 
-#endif  // NET_URL_REQUEST_URL_REQUEST_NEW_FTP_JOB_H_
+#endif  // NET_URL_REQUEST_URL_REQUEST_FTP_JOB_H_
--- net/url_request/url_request_job_manager.cc
+++ net/url_request/url_request_job_manager.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_data_job.h"
 #include "net/url_request/url_request_error_job.h"
 #include "net/url_request/url_request_file_job.h"
-#include "net/url_request/url_request_new_ftp_job.h"
+#include "net/url_request/url_request_ftp_job.h"
 #include "net/url_request/url_request_http_job.h"
 
 // The built-in set of protocol factories
@@ -32,7 +32,7 @@ static const SchemeToFactory kBuiltinFactories[] = {
   { "http", URLRequestHttpJob::Factory },
   { "https", URLRequestHttpJob::Factory },
   { "file", URLRequestFileJob::Factory },
-  { "ftp", URLRequestNewFtpJob::Factory },
+  { "ftp", URLRequestFtpJob::Factory },
   { "about", URLRequestAboutJob::Factory },
   { "data", URLRequestDataJob::Factory },
 };
