51a42f08e5fee54992cfdfa06e72fa66733fe52d
jamesr@google.com <jamesr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-16 02:38:09 +0000

    Add a defined(OS_LINUX) guard for the include of tcmalloc's profiler.h
    This file is not mapped in to the WebKit Chromium checkout on windows and causes the compilation to fail.  It's only useful on linux anyway.
    
    Review URL: http://codereview.chromium.org/1031001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41669 0039d316-1c4b-4281-b951-d872f2087c98

--- base/profiler.cc
+++ base/profiler.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/profiler.h"
 #include "base/string_util.h"
 
-#if defined(USE_TCMALLOC)
+#if defined(USE_TCMALLOC) && defined(OS_LINUX)
 #include "third_party/tcmalloc/chromium/src/google/profiler.h"
 #endif
 
