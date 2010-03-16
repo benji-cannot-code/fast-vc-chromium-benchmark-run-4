a01d7f31e00183814b2065c894447431eef3a4bc
hamaji@chromium.org <hamaji@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-16 07:39:28 +0000

    2010-03-16  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Darin Adler.
    
            RenderText::m_text should be a String, not RefPtr<StringImpl>
            https://bugs.webkit.org/show_bug.cgi?id=36010
    
            Refactoring only, so no new tests.
    
            * platform/text/PlatformString.h:
            (WebCore::String::makeLower):
            (WebCore::String::makeUpper):
            (WebCore::String::makeSecure):
            (WebCore::String::makeCapitalized):
            (WebCore::String::containsOnlyASCII):
            * rendering/RenderText.cpp:
            (WebCore::RenderText::RenderText):
            (WebCore::RenderText::widthFromCache):
            (WebCore::RenderText::trimmedPrefWidths):
            (WebCore::RenderText::containsOnlyWhitespace):
            (WebCore::RenderText::setTextInternal):
            (WebCore::RenderText::setText):
            (WebCore::RenderText::previousOffset):
            (WebCore::RenderText::previousOffsetForBackwardDeletion):
            (WebCore::RenderText::nextOffset):
            * rendering/RenderText.h:
            (WebCore::RenderText::text):
            (WebCore::RenderText::characters):
            (WebCore::RenderText::textLength):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/PlatformString.h
+++ third_party/WebKit/WebCore/platform/text/PlatformString.h
@@ -64,6 +64,27 @@ class CString;
 class SharedBuffer;
 struct StringHash;
 
+// Declarations of string operations
+
+bool charactersAreAllASCII(const UChar*, size_t);
+int charactersToIntStrict(const UChar*, size_t, bool* ok = 0, int base = 10);
+unsigned charactersToUIntStrict(const UChar*, size_t, bool* ok = 0, int base = 10);
+int64_t charactersToInt64Strict(const UChar*, size_t, bool* ok = 0, int base = 10);
+uint64_t charactersToUInt64Strict(const UChar*, size_t, bool* ok = 0, int base = 10);
+intptr_t charactersToIntPtrStrict(const UChar*, size_t, bool* ok = 0, int base = 10);
+
+int charactersToInt(const UChar*, size_t, bool* ok = 0); // ignores trailing garbage
+unsigned charactersToUInt(const UChar*, size_t, bool* ok = 0); // ignores trailing garbage
+int64_t charactersToInt64(const UChar*, size_t, bool* ok = 0); // ignores trailing garbage
+uint64_t charactersToUInt64(const UChar*, size_t, bool* ok = 0); // ignores trailing garbage
+intptr_t charactersToIntPtr(const UChar*, size_t, bool* ok = 0); // ignores trailing garbage
+
+double charactersToDouble(const UChar*, size_t, bool* ok = 0);
+float charactersToFloat(const UChar*, size_t, bool* ok = 0);
+
+int find(const UChar*, size_t, UChar, int startPosition = 0);
+int reverseFind(const UChar*, size_t, UChar, int startPosition = -1);
+
 class String {
 public:
     String() { } // gives null string, distinguishable from an empty string
@@ -133,6 +154,11 @@ public:
     String& replace(UChar a, const String& b) { if (m_impl) m_impl = m_impl->replace(a, b.impl()); return *this; }
     String& replace(const String& a, const String& b) { if (m_impl) m_impl = m_impl->replace(a.impl(), b.impl()); return *this; }
     String& replace(unsigned index, unsigned len, const String& b) { if (m_impl) m_impl = m_impl->replace(index, len, b.impl()); return *this; }
+
+    void makeLower() { if (m_impl) m_impl = m_impl->lower(); }
+    void makeUpper() { if (m_impl) m_impl = m_impl->upper(); }
+    void makeSecure(UChar aChar) { if (m_impl) m_impl = m_impl->secure(aChar); }
+    void makeCapitalized(UChar previousCharacter) { if (m_impl) m_impl = m_impl->capitalize(previousCharacter); }
 
     void truncate(unsigned len);
     void remove(unsigned pos, int len = 1);
@@ -257,6 +283,8 @@ public:
     // the specified grapheme cluster length.
     unsigned numCharactersInGraphemeClusters(unsigned) const;
 
+    bool containsOnlyASCII() const { return charactersAreAllASCII(characters(), length()); }
+
 private:
     RefPtr<StringImpl> m_impl;
 };
@@ -295,27 +323,7 @@ inline bool operator!(const String& str) { return str.isNull(); }
 
 inline void swap(String& a, String& b) { a.swap(b); }
 
-// String Operations
-
-bool charactersAreAllASCII(const UChar*, size_t);
-
-int charactersToIntStrict(const UChar*, size_t, bool* ok = 0, int base = 10);
-unsigned charactersToUIntStrict(const UChar*, size_t, bool* ok = 0, int base = 10);
-int64_t charactersToInt64Strict(const UChar*, size_t, bool* ok = 0, int base = 10);
-uint64_t charactersToUInt64Strict(const UChar*, size_t, bool* ok = 0, int base = 10);
-intptr_t charactersToIntPtrStrict(const UChar*, size_t, bool* ok = 0, int base = 10);
-
-int charactersToInt(const UChar*, size_t, bool* ok = 0); // ignores trailing garbage
-unsigned charactersToUInt(const UChar*, size_t, bool* ok = 0); // ignores trailing garbage
-int64_t charactersToInt64(const UChar*, size_t, bool* ok = 0); // ignores trailing garbage
-uint64_t charactersToUInt64(const UChar*, size_t, bool* ok = 0); // ignores trailing garbage
-intptr_t charactersToIntPtr(const UChar*, size_t, bool* ok = 0); // ignores trailing garbage
-
-double charactersToDouble(const UChar*, size_t, bool* ok = 0);
-float charactersToFloat(const UChar*, size_t, bool* ok = 0);
-
-int find(const UChar*, size_t, UChar, int startPosition = 0);
-int reverseFind(const UChar*, size_t, UChar, int startPosition = -1);
+// Definitions of string operations
 
 #ifdef __OBJC__
 // This is for situations in WebKit where the long standing behavior has been
