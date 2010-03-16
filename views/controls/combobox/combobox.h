17b406f4eb78818c64924f4b34024c6b272903c0
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-16 05:45:33 +0000

    Reapply r41640.
    Original patch by thiago.farina@gmail.com
    
    Revision r41640 was reverted in r41676 because it looked like it might have been responsible for Interactive UI test failures in linux and windows.  However, reverting didn't fix, so I'm reapplying it.
    
    Review URL: http://codereview.chromium.org/982004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41683 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/combobox/combobox.h
+++ views/controls/combobox/combobox.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef VIEWS_CONTROLS_COMBOBOX_COMBOBOX_H_
 #define VIEWS_CONTROLS_COMBOBOX_COMBOBOX_H_
 
+#include <string>
+
 #include "views/view.h"
 
 class ComboboxModel;
@@ -41,7 +43,7 @@ class Combobox : public View {
   void ModelChanged();
 
   // Gets/Sets the selected item.
-  int selected_item() const { return selected_item_; };
+  int selected_item() const { return selected_item_; }
   void SetSelectedItem(int index);
 
   // Called when the combo box's selection is changed by the user.
@@ -56,6 +58,9 @@ class Combobox : public View {
   virtual void SetEnabled(bool enabled);
   virtual bool SkipDefaultKeyEventProcessing(const KeyEvent& e);
   virtual void PaintFocusBorder(gfx::Canvas* canvas);
+  virtual bool GetAccessibleName(std::wstring* name);
+  virtual bool GetAccessibleRole(AccessibilityTypes::Role* role);
+  virtual void SetAccessibleName(const std::wstring& name);
 
  protected:
   virtual void Focus();
@@ -75,6 +80,9 @@ class Combobox : public View {
 
   // The current selection.
   int selected_item_;
+
+  // The accessible name of this control.
+  std::wstring accessible_name_;
 
   DISALLOW_COPY_AND_ASSIGN(Combobox);
 };
