6868f3450a002302ed8fb2c2e7834f4b965f3a12
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-17 07:35:56 +0000

    Add more sync calls to try to make the FileWatcher test not flaky on OSX.
    BUG=38188
    
    Review URL: http://codereview.chromium.org/1045004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41819 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/file_watcher_unittest.cc
+++ chrome/browser/file_watcher_unittest.cc
@@ -70,6 +70,7 @@ class FileWatcherTest : public testing::Test {
     LOG(INFO) << "WriteTestFile";
     int write_size = file_util::WriteFile(test_file(), content.c_str(),
                                           content.length());
+    SyncIfPOSIX();
     return write_size == static_cast<int>(content.length());
   }
 
@@ -160,7 +161,6 @@ TEST_F(FileWatcherTest, MAYBE(NewFile)) {
 // Verify that modifying the file is caught.
 TEST_F(FileWatcherTest, MAYBE(ModifiedFile)) {
   ASSERT_TRUE(WriteTestFile("content"));
-  SyncIfPOSIX();
 
   FileWatcher watcher;
   TestDelegate delegate(this);
@@ -174,7 +174,6 @@ TEST_F(FileWatcherTest, MAYBE(ModifiedFile)) {
 
 TEST_F(FileWatcherTest, MAYBE(DeletedFile)) {
   ASSERT_TRUE(WriteTestFile("content"));
-  SyncIfPOSIX();
 
   FileWatcher watcher;
   TestDelegate delegate(this);
@@ -183,6 +182,7 @@ TEST_F(FileWatcherTest, MAYBE(DeletedFile)) {
   // Now make sure we get notified if the file is deleted.
   SetExpectedNumberOfNotifiedDelegates(1);
   file_util::Delete(test_file(), false);
+  SyncIfPOSIX();
   VerifyExpectedNumberOfNotifiedDelegates();
 }
 
