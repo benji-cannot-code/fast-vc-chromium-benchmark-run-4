ebda9dbf80b2bb4449d738b510cfdfef65756c88
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-17 09:48:42 +0000

    Implement selection of rows on mouse clicks in TableView2 for GTK.
    Before this change, clicking rows in TableView2 for GTK did not
    select the clicked rows.
    
    TEST=manually with view_examples.
    BUG=none
    
    Review URL: http://codereview.chromium.org/1015005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41825 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/table/native_table_gtk.h
+++ views/controls/table/native_table_gtk.h
@@ -1,11 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved. Use of this
+// Copyright (c) 2010 The Chromium Authors. All rights reserved. Use of this
 // source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
 #ifndef VIEWS_CONTROLS_TABLE_NATIVE_TABLE_GTK_H_
 #define VIEWS_CONTROLS_TABLE_NATIVE_TABLE_GTK_H_
 
+#include "app/gtk_signal.h"
 #include "app/table_model.h"
 #include "views/controls/native_control_gtk.h"
 #include "views/controls/table/native_table_wrapper.h"
@@ -58,6 +59,9 @@ class NativeTableGtk : public NativeControlGtk, public NativeTableWrapper {
   // Sets the content of the row pointed to by |iter| in the tree_view_, using
   // the data in the model at row |index|.
   void SetRowData(int index, GtkTreeIter* iter);
+
+  // Handles the "cursor-changed" event.
+  CHROMEGTK_CALLBACK_0(NativeTableGtk, void, OnCursorChanged);
 
   // Returns the icon that should be displayed for the row at |row|.
   GdkPixbuf* GetModelIcon(int row);
