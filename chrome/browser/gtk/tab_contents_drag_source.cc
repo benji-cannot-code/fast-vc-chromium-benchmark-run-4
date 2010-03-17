462e4aa715eac2def8fb230f5a5e6ade6a75e709
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-17 07:05:22 +0000

    fixing bustage... file was moved
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41814 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/tab_contents_drag_source.cc
+++ chrome/browser/gtk/tab_contents_drag_source.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "app/gfx/gtk_util.h"
 #include "app/gtk_dnd_util.h"
 #include "base/file_util.h"
 #include "base/mime_util.h"
@@ -20,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/render_view_host_delegate.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/tab_contents/tab_contents_view.h"
+#include "gfx/gtk_util.h"
 #include "net/base/file_stream.h"
 #include "net/base/net_util.h"
 #include "webkit/glue/webdropdata.h"
