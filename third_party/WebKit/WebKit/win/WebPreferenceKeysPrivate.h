c606acd1391ded86e16a321e79016b5299878c47
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-17 18:00:15 +0000

    Missing support for showing compositing layers borders and repaint count on Windows. <rdar://problem/7760736> <https://bugs.webkit.org/show_bug.cgi?id=36197>
    Reviewed by Darin Adler.
    
    WebCore: 
    
    * platform/graphics/win/GraphicsLayerCACF.cpp:
    (WebCore::WebLayer::drawInContext): Modified to use the correct font on Windows.
    
    WebKit/win: 
    
    * Interfaces/IWebPreferencesPrivate.idl:
    * WebPreferenceKeysPrivate.h:
    * WebPreferences.cpp:
    (WebPreferences::initializeDefaultSettings):
    (WebPreferences::showDebugBorders):
    (WebPreferences::setShowDebugBorders):
    (WebPreferences::showRepaintCounter):
    (WebPreferences::setShowRepaintCounter):
    * WebPreferences.h:
    * WebView.cpp:
    (WebView::notifyPreferencesChanged):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56118 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebPreferenceKeysPrivate.h
+++ third_party/WebKit/WebKit/win/WebPreferenceKeysPrivate.h
@@ -136,4 +136,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define WebKitAcceleratedCompositingEnabledPreferenceKey "WebKitAcceleratedCompositingEnabled"
 
+#define WebKitShowDebugBordersPreferenceKey "WebKitShowDebugBorders"
+
+#define WebKitShowRepaintCounterPreferenceKey "WebKitShowRepaintCounter"
+
 #define WebKitCustomDragCursorsEnabledPreferenceKey "WebKitCustomDragCursorsEnabled"
