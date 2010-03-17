46fd4ba44f5632822cb048e0f372c1cc9bcf4567
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-17 22:32:16 +0000

    Fix for ExtensionApiTest.Popup flakiness
    This fires the OnPopupClosed event in a later event via PostTask. This gives the popup time to complete closing before the OnPopupClosed is received by the extension
    
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/1041004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41888 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_popup_api.cc
+++ chrome/browser/extensions/extension_popup_api.cc
@@ -73,7 +73,7 @@ class ExtensionPopupHost : public ExtensionPopup::Observer,
  public:
   explicit ExtensionPopupHost(ExtensionFunctionDispatcher* dispatcher)
       : dispatcher_(dispatcher), popup_(NULL) {
-    AddRef();  // Balanced in ExtensionPopupClosed().
+    AddRef();  // Balanced in DispatchPopupClosedEvent().
     views::FocusManager::GetWidgetFocusManager()->AddFocusChangeListener(this);
   }
 
@@ -88,6 +88,13 @@ class ExtensionPopupHost : public ExtensionPopup::Observer,
 
   // Overriden from ExtensionPopup::Observer
   virtual void ExtensionPopupClosed(ExtensionPopup* popup) {
+    // The OnPopupClosed event should be sent later to give the popup time to
+    // complete closing.
+    MessageLoop::current()->PostTask(FROM_HERE, NewRunnableMethod(this,
+      &ExtensionPopupHost::DispatchPopupClosedEvent));
+  }
+
+  virtual void DispatchPopupClosedEvent() {
     RenderViewHost* render_view_host = dispatcher_->GetExtensionHost() ?
         dispatcher_->GetExtensionHost()->render_view_host() :
         dispatcher_->GetExtensionDOMUI()->GetRenderViewHost();
