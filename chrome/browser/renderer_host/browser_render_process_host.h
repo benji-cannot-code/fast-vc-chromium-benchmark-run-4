5519ad6198765b33d4ad2c3261fb91e92fc984cf
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-18 10:00:14 +0000

    Delete an out of date comment and include header cleanup.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/838008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41934 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.h
+++ chrome/browser/renderer_host/browser_render_process_host.h
@@ -13,10 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/process.h"
-#include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
-#include "base/shared_memory.h"
-#include "base/string16.h"
 #include "base/timer.h"
 #include "chrome/common/transport_dib.h"
 #include "chrome/browser/child_process_launcher.h"
@@ -32,6 +29,10 @@ class RenderWidgetHelper;
 class TabContents;
 class VisitedLinkUpdater;
 class URLRequestContextGetter;
+
+namespace base {
+class SharedMemory;
+}
 
 namespace gfx {
 class Size;
@@ -137,8 +138,7 @@ class BrowserRenderProcessHost : public RenderProcessHost,
                                              CommandLine* renderer_cmd) const;
 
   // Callers can reduce the RenderProcess' priority.
-  // Returns true if the priority is backgrounded; false otherwise.
-  void SetBackgrounded(bool boost);
+  void SetBackgrounded(bool backgrounded);
 
   // The renderer has requested that we initialize its spellchecker. This should
   // generally only be called once per session, as after the first call, all
