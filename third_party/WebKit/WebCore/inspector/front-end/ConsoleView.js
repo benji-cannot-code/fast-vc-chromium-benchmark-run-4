6193112f159076285bdff864b05c1906c97b2624
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-18 16:20:57 +0000

    2010-03-18  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Preserve console history between debugging sessions.
    
            https://bugs.webkit.org/show_bug.cgi?id=36223
    
            * inspector/front-end/ConsoleView.js:
            (WebInspector.ConsoleView.prototype._settingsLoaded):
            (WebInspector.ConsoleView.prototype._enterKeyPressed.printResult):
            (WebInspector.ConsoleView.prototype._enterKeyPressed):
            * inspector/front-end/Settings.js:
            (WebInspector.Settings.prototype._load):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ConsoleView.js
+++ third_party/WebKit/WebCore/inspector/front-end/ConsoleView.js
@@ -49,6 +49,7 @@ WebInspector.ConsoleView = function(drawer)
     this.promptElement.className = "source-code";
     this.promptElement.addEventListener("keydown", this._promptKeyDown.bind(this), true);
     this.prompt = new WebInspector.TextPrompt(this.promptElement, this.completions.bind(this), ExpressionStopCharacters + ".");
+    WebInspector.settings.addEventListener("loaded", this._settingsLoaded, this);
 
     this.topGroup = new WebInspector.ConsoleGroup(null, 0);
     this.messagesElement.insertBefore(this.topGroup.element, this.promptElement);
@@ -116,6 +117,10 @@ WebInspector.ConsoleView = function(drawer)
 }
 
 WebInspector.ConsoleView.prototype = {
+    _settingsLoaded: function()
+    {
+        this.prompt.history = WebInspector.settings.consoleHistory;
+    },
     
     _updateFilter: function(e)
     {
@@ -505,6 +510,9 @@ WebInspector.ConsoleView.prototype = {
             self.prompt.history.push(str);
             self.prompt.historyOffset = 0;
             self.prompt.text = "";
+
+            WebInspector.settings.consoleHistory = self.prompt.history.slice(-30);
+
             self.addMessage(new WebInspector.ConsoleCommandResult(result, exception, commandMessage));
         }
         this.evalInInspectedWindow(str, "console", printResult);
