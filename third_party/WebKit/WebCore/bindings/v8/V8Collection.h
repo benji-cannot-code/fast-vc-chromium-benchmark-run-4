caeebc9d938304314e7a244a1ec91d1a70443188
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-18 16:59:55 +0000

    2010-03-18  Nate Chapin  <japhet@chromium.org>
            Unreviewed, Chromium build fix.
    
            Missed a #include in V8Collection.h.
    
            * bindings/v8/V8Collection.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8Collection.h
+++ third_party/WebKit/WebCore/bindings/v8/V8Collection.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLFormElement.h"
 #include "HTMLSelectElement.h"
 #include "V8Binding.h"
+#include "V8Node.h"
 #include "V8Proxy.h"
 #include <v8.h>
 
