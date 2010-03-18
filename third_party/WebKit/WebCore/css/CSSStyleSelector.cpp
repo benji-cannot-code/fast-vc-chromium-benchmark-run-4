0bbf2abb194bdd1f289810a4d572297ce35bfeca
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-18 17:57:37 +0000

    2010-03-18  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dave Hyatt.
    
            https://bugs.webkit.org/show_bug.cgi?id=36271
            CSS3 :enabled on an input type=hidden element fails
    
            The CSS3 selectors spec now makes it clear that hidden inputs should respect
            the :enabled and :disabled pseudo-classes.
    
            Test: fast/forms/hidden-input-enabled.html
    
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::SelectorChecker::checkOneSelector): Remove checks for hidden inputs.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56170 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/WebCore/css/CSSStyleSelector.cpp
@@ -2412,12 +2412,6 @@ bool CSSStyleSelector::SelectorChecker::checkOneSelector(CSSSelector* sel, Eleme
                 break;
             case CSSSelector::PseudoEnabled:
                 if (e && e->isFormControlElement()) {
-                    InputElement* inputElement = toInputElement(e);
-                    if (inputElement && inputElement->isInputTypeHidden())
-                        break;
-                    // The UI spec states that you can't match :enabled unless you are an object that can
-                    // "receive focus and be activated."  We will limit matching of this pseudo-class to elements
-                    // that are non-"hidden" controls.
                     return e->isEnabledFormControl();
                 }
                 break;
@@ -2428,13 +2422,6 @@ bool CSSStyleSelector::SelectorChecker::checkOneSelector(CSSSelector* sel, Eleme
                 return e && e->isDefaultButtonForForm();
             case CSSSelector::PseudoDisabled:
                 if (e && e->isFormControlElement()) {
-                    InputElement* inputElement = toInputElement(e);
-                    if (inputElement && inputElement->isInputTypeHidden())
-                        break;
-
-                    // The UI spec states that you can't match :enabled unless you are an object that can
-                    // "receive focus and be activated."  We will limit matching of this pseudo-class to elements
-                    // that are non-"hidden" controls.
                     return !e->isEnabledFormControl();
                 }
                 break;
