5812198665983fb2fa56d63cf11b86f0ccdc28c4
yutak@chromium.org <yutak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-18 08:38:33 +0000

    Add #include to fix compile errors on "Linux Perf (webkit.org)" bot.
    TBR=markus
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1107001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41931 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/seccomp/timestats.cc
+++ sandbox/linux/seccomp/timestats.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
 #include <time.h>
