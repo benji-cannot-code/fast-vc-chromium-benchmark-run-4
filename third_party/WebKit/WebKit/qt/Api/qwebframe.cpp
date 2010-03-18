d047a27572cbdffb9eb86c2d89e24c599fcae0f8
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-18 02:16:45 +0000

    2010-03-17  Antti Koivisto  <koivisto@iki.fi>
            Reviewed by Tor Arne Vestbø.
    
            Fix Qt build with tiled backing store disabled.
    
            * Api/qwebframe.cpp:
            * Api/qwebframe_p.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56141 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
@@ -329,6 +329,7 @@ WebCore::Scrollbar* QWebFramePrivate::verticalScrollBar() const
     return frame->view()->verticalScrollbar();
 }
 
+#if ENABLE(TILED_BACKING_STORE)
 void QWebFramePrivate::renderFromTiledBackingStore(GraphicsContext* context, const QRegion& clip)
 {
     ASSERT(frame->tiledBackingStore());
@@ -361,6 +362,7 @@ void QWebFramePrivate::renderFromTiledBackingStore(GraphicsContext* context, con
         painter->restore();
     }
 }
+#endif
 
 void QWebFramePrivate::renderRelativeCoords(GraphicsContext* context, QWebFrame::RenderLayer layer, const QRegion& clip)
 {
