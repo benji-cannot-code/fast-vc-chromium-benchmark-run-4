6a5469cd0580517d0cee2969b448c76141f9287d
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-19 10:05:04 +0000

    2010-03-19  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Misc bug fixes to make the SheriffBot actually work
            https://bugs.webkit.org/show_bug.cgi?id=36355
    
            With these changes, I can actually run the sheriff-bot from start to
            finish.
    
            * Scripts/webkitpy/irc/ircproxy.py:
            * Scripts/webkitpy/patch/patcher.py:
            * Scripts/webkitpy/statusserver.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56235 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/irc/ircproxy.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/irc/ircproxy.py
@@ -31,6 +31,8 @@ import threading
 
 from webkitpy.irc.ircbot import IRCBot
 from webkitpy.irc.threadedmessagequeue import ThreadedMessageQueue
+from webkitpy.webkit_logging import log
+
 
 class _IRCThread(threading.Thread):
     def __init__(self, message_queue, bot_class, password):
@@ -47,6 +49,7 @@ class _IRCThread(threading.Thread):
 
 class IRCProxy(object):
     def __init__(self, bot_class=IRCBot, password=None):
+        log("Connecting to IRC")
         self._message_queue = ThreadedMessageQueue()
         self._child_thread = _IRCThread(self._message_queue, bot_class, password=password)
         self._child_thread.start()
@@ -55,6 +58,7 @@ class IRCProxy(object):
         self._message_queue.post(message)
 
     def disconnect(self):
+        log("Disconnecting from IRC...")
         self._message_queue.stop()
         self._child_thread.join()
 
--- third_party/WebKit/WebKitTools/Scripts/webkitpy/patch/patcher.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/patch/patcher.py
@@ -42,6 +42,7 @@ from webkitpy.commands.queues import *
 from webkitpy.commands.sheriffbot import *
 from webkitpy.commands.upload import *
 from webkitpy.executive import Executive
+from webkitpy.irc.ircproxy import IRCProxy
 from webkitpy.webkit_logging import log
 from webkitpy.webkitcheckout import WebKitCheckout
 from webkitpy.multicommandtool import MultiCommandTool
@@ -85,7 +86,7 @@ class WebKitPatch(MultiCommandTool):
 
         return self._scm
 
-    def checkout():
+    def checkout(self):
         if not self._checkout:
             self._checkout = WebKitCheckout(self.scm())
         return self._checkout
