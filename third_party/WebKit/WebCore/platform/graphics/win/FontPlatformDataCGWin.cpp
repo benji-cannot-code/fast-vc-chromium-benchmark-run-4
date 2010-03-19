ff062bbab12165924cada7b97f0323343f795641
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-19 20:01:43 +0000

    Windows clean build fix after r56192
    * WebCore.vcproj/WebCore.vcproj: Copy headers from
    platform/graphics/cg to $WebKitOutputDir as part of the post-build
    event so that WebKit can use them. (The only one we currently require
    is FontPlatformData.h.)
    
    * platform/graphics/win/FontPlatformDataCGWin.cpp: Touched to force a
    build.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/FontPlatformDataCGWin.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/FontPlatformDataCGWin.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * This file is part of the internal font implementation.  It should not be included by anyone other than
+ * This file is part of the internal font implementation. It should not be included by anyone other than
  * FontMac.cpp, FontWin.cpp and Font.cpp.
  *
  * Copyright (C) 2006, 2007, 2008, 2009 Apple Inc.
