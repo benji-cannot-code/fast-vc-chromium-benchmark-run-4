8866ddd57c4d8eef22bb600497a17a28810086ba
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-19 01:33:43 +0000

    <http://webkit.org/b/36338> Remove unused RenderReplaced::adjustOverflowForBoxShadowAndReflect() declaration
    Rubber-stamped by Darin Adler.
    
    This declaration should have been removed in r47440.
    
    * rendering/RenderReplaced.h:
    (WebCore::RenderReplaced::adjustOverflowForBoxShadowAndReflect): Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderReplaced.h
+++ third_party/WebKit/WebCore/rendering/RenderReplaced.h
@@ -49,7 +49,6 @@ protected:
 
     virtual void paint(PaintInfo&, int tx, int ty);
     bool shouldPaint(PaintInfo&, int& tx, int& ty);
-    void adjustOverflowForBoxShadowAndReflect();
     IntRect localSelectionRect(bool checkWhetherSelected = true) const;
 
 private:
