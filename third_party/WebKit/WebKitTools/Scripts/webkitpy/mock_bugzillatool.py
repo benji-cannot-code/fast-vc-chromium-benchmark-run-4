c377b2bd13c28c2468cb34c86491d344b4252322
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-19 09:44:25 +0000

    2010-03-19  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Second cut at SheriffBot
            https://bugs.webkit.org/show_bug.cgi?id=36353
    
            This patch should contain a complete SheriffBot that's capable of
            saying reasonable things on IRC.  I had to refactor the use of
            CommitInfo to make the SheriffBot testable, but I did the minimum
            necessary.  We should grow webkitcheckout over time to contain the
            knowledge of ChangeLogs from scm.
    
            * Scripts/webkitpy/commands/sheriffbot.py:
            * Scripts/webkitpy/commands/sheriffbot_unittest.py:
            * Scripts/webkitpy/mock_bugzillatool.py:
            * Scripts/webkitpy/patch/patcher.py:
            * Scripts/webkitpy/webkitcheckout.py: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56233 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/mock_bugzillatool.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/mock_bugzillatool.py
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 
 from webkitpy.bugzilla import Bug, Attachment
+from webkitpy.commitinfo import CommitInfo
 from webkitpy.committers import CommitterList, Reviewer
 from webkitpy.scm import CommitMessage
 from webkitpy.thirdparty.mock import Mock
@@ -182,6 +183,15 @@ _bug4 = {
 }
 
 
+class MockBuilder(object):
+
+    def name(self):
+        return "Mock builder name (Tests)"
+
+
+mock_builder = MockBuilder()
+
+
 class MockBugzillaQueries(Mock):
 
     def __init__(self, bugzilla):
@@ -285,7 +295,7 @@ class MockBugzilla(Mock):
         return "%s/%s%s" % (self.bug_server_url, attachment_id, action_param)
 
 
-class MockBuildBot(Mock):
+class MockBuildBot(object):
 
     def __init__(self):
         self._tree_is_on_fire = False
@@ -307,6 +317,10 @@ class MockBuildBot(Mock):
     def light_tree_on_fire(self):
         self._tree_is_on_fire = True
 
+    def revisions_causing_failures(self):
+        return {
+            "29837": [mock_builder]
+        }
 
 class MockSCM(Mock):
 
@@ -354,6 +368,17 @@ class MockSCM(Mock):
         return []
 
 
+class MockWebKitCheckout(object):
+
+    def commit_info_for_revision(self, svn_revision):
+        return CommitInfo(svn_revision, "eric@webkit.org", {
+            "bug_id": 42,
+            "author_name": "Adam Barth",
+            "author_email": "abarth@webkit.org",
+            "reviewer_text": "Darin Adler"
+        })
+
+
 class MockUser(object):
 
     @staticmethod
@@ -391,8 +416,14 @@ class MockStatusServer(object):
     def patch_status(self, queue_name, patch_id):
         return None
 
+    def svn_revision(self, svn_revision):
+        return None
+
     def update_status(self, queue_name, status, patch=None, results_file=None):
         return 187
+
+    def update_svn_revision(self, svn_revision, broken_bot):
+        return 191
 
 
 class MockBugzillaTool():
@@ -404,10 +435,14 @@ class MockBugzillaTool():
         self._irc = None
         self.user = MockUser()
         self._scm = MockSCM()
+        self._checkout = MockWebKitCheckout()
         self.status_server = MockStatusServer()
 
     def scm(self):
         return self._scm
+
+    def checkout(self):
+        return self._checkout
 
     def ensure_irc_connected(self):
         if not self._irc:
--- third_party/WebKit/WebKitTools/Scripts/webkitpy/patch/patcher.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/patch/patcher.py
@@ -42,6 +42,7 @@ from webkitpy.commands.queues import *
 from webkitpy.commands.upload import *
 from webkitpy.executive import Executive
 from webkitpy.webkit_logging import log
+from webkitpy.webkitcheckout import WebKitCheckout
 from webkitpy.multicommandtool import MultiCommandTool
 from webkitpy.scm import detect_scm_system
 from webkitpy.user import User
@@ -64,6 +65,7 @@ class WebKitPatch(MultiCommandTool):
         self._irc = None
         self.user = User()
         self._scm = None
+        self._checkout = None
         self.status_server = StatusServer()
 
     def scm(self):
@@ -81,6 +83,11 @@ class WebKitPatch(MultiCommandTool):
                 error("FATAL: Failed to determine the SCM system for either %s or %s" % (original_cwd, script_directory))
 
         return self._scm
+
+    def checkout():
+        if not self._checkout:
+            self._checkout = WebKitCheckout(self.scm())
+        return self._checkout
 
     # FIXME: Add a parameter for nickname?
     def ensure_irc_connected(self):
