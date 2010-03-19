4e89baed706f09f0bcd005a0521a5a170757c095
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-19 22:22:56 +0000

    Allow dynamic switching in and out of sidetabs mode.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1001003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42156 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_frame_gtk.cc
+++ chrome/browser/views/frame/browser_frame_gtk.cc
@@ -103,9 +103,6 @@ views::Window* BrowserFrameGtk::GetWindow() {
   return this;
 }
 
-void BrowserFrameGtk::TabStripCreated(BaseTabStrip* tabstrip) {
-}
-
 int BrowserFrameGtk::GetMinimizeButtonOffset() const {
   NOTIMPLEMENTED();
   return 0;
@@ -138,6 +135,9 @@ views::View* BrowserFrameGtk::GetFrameView() const {
 }
 
 void BrowserFrameGtk::PaintTabStripShadow(gfx::Canvas* canvas) {
+}
+
+void BrowserFrameGtk::TabStripDisplayModeChanged() {
 }
 
 ThemeProvider* BrowserFrameGtk::GetThemeProvider() const {
