c29dcb9bc8f1cc9578ac57584699da1b30edd9c1
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-19 01:39:19 +0000

    Enable tabbed bookmark manager by default.
    BUG=4890
    TEST=None 
    		
    Review URL: http://codereview.chromium.org/1139001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42061 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -1401,10 +1401,10 @@ void Browser::ToggleExtensionShelf() {
 void Browser::OpenBookmarkManager() {
   UserMetrics::RecordAction("ShowBookmarkManager", profile_);
   if (CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableTabbedBookmarkManager)) {
-    ShowBookmarkManagerTab();
-  } else {
+          switches::kDisableTabbedBookmarkManager)) {
     window_->ShowBookmarkManager();
+  } else {
+    ShowBookmarkManagerTab();
   }
 }
 
--- chrome/browser/extensions/extension_browsertests_misc.cc
+++ chrome/browser/extensions/extension_browsertests_misc.cc
@@ -295,14 +295,14 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, RSSMultiRelLink) {
 // Tests that tooltips of a browser action icon can be specified using UTF8.
 // See http://crbug.com/25349.
 IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, TitleLocalizationBrowserAction) {
+  ExtensionsService* service = browser()->profile()->GetExtensionsService();
+  const size_t size_before = service->extensions()->size();
   FilePath extension_path(test_data_dir_.AppendASCII("browsertest")
                                         .AppendASCII("title_localized"));
   ASSERT_TRUE(LoadExtension(extension_path));
 
-  ExtensionsService* service = browser()->profile()->GetExtensionsService();
-  const ExtensionList* extensions = service->extensions();
-  ASSERT_EQ(1u, extensions->size());
-  Extension* extension = extensions->at(0);
+  ASSERT_EQ(size_before + 1, service->extensions()->size());
+  Extension* extension = service->extensions()->at(size_before);
 
   EXPECT_STREQ(WideToUTF8(L"Hreggvi\u00F0ur: l10n browser action").c_str(),
                extension->description().c_str());
@@ -318,6 +318,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, TitleLocalizationBrowserAction) {
 IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, TitleLocalizationPageAction) {
   HTTPTestServer* server = StartHTTPServer();
 
+  ExtensionsService* service = browser()->profile()->GetExtensionsService();
+  const size_t size_before = service->extensions()->size();
+
   FilePath extension_path(test_data_dir_.AppendASCII("browsertest")
                                         .AppendASCII("title_localized_pa"));
   ASSERT_TRUE(LoadExtension(extension_path));
@@ -327,10 +330,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, TitleLocalizationPageAction) {
   ui_test_utils::NavigateToURL(browser(), url);
   ASSERT_TRUE(WaitForPageActionVisibilityChangeTo(1));
 
-  ExtensionsService* service = browser()->profile()->GetExtensionsService();
-  const ExtensionList* extensions = service->extensions();
-  ASSERT_EQ(1u, extensions->size());
-  Extension* extension = extensions->at(0);
+  ASSERT_EQ(size_before + 1, service->extensions()->size());
+  Extension* extension = service->extensions()->at(size_before);
 
   EXPECT_STREQ(WideToUTF8(L"Hreggvi\u00F0ur: l10n page action").c_str(),
                extension->description().c_str());
@@ -699,8 +700,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, MAYBE_PluginLoadUnload) {
   EXPECT_FALSE(result);
 
   ExtensionsService* service = browser()->profile()->GetExtensionsService();
+  const size_t size_before = service->extensions()->size();
   ASSERT_TRUE(LoadExtension(extension_dir));
-  EXPECT_EQ(1u, service->extensions()->size());
+  EXPECT_EQ(size_before + 1, service->extensions()->size());
   // Now the plugin should be in the cache, but we have to reload the page for
   // it to work.
   ui_test_utils::ExecuteJavaScriptAndExtractBool(
@@ -712,9 +714,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, MAYBE_PluginLoadUnload) {
       tab->render_view_host(), L"", L"testPluginWorks()", &result);
   EXPECT_TRUE(result);
 
-  EXPECT_EQ(1u, service->extensions()->size());
-  UnloadExtension(service->extensions()->at(0)->id());
-  EXPECT_EQ(0u, service->extensions()->size());
+  EXPECT_EQ(size_before + 1, service->extensions()->size());
+  UnloadExtension(service->extensions()->at(size_before)->id());
+  EXPECT_EQ(size_before, service->extensions()->size());
 
   // Now the plugin should be out of the cache again, but existing pages will
   // still work until we reload them.
