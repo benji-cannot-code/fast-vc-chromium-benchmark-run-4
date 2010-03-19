9208da608ecb4c435f46691c996b89036aa6863e
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-19 07:04:57 +0000

    2010-03-19  Adam Barth  <abarth@webkit.org>
            Reviewed by Adam Barth.
    
            Actually pass the IRC password to the IRC object
            https://bugs.webkit.org/show_bug.cgi?id=36346
    
            I wanted to do this before, but both patches were in flight.  This
            patch finally closes the loop and makes the IRCProxy system complete.
    
            * Scripts/webkitpy/patch/patcher.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56219 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/patch/patcher.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/patch/patcher.py
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
-# Copyright (c) 2009, Google Inc. All rights reserved.
+# Copyright (c) 2010 Google Inc. All rights reserved.
 # Copyright (c) 2009 Apple Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -86,7 +85,7 @@ class WebKitPatch(MultiCommandTool):
     # FIXME: Add a parameter for nickname?
     def ensure_irc_connected(self):
         if not self._irc:
-            self._irc = IRCProxy()
+            self._irc = IRCProxy(password=self._irc_password)
 
     def irc(self):
         # We don't automatically construct IRCProxy here because constructing
