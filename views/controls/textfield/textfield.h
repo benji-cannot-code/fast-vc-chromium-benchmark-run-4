92110f73b9b250802f2ef4417578d696e533e26d
ctguil@chromium.org <ctguil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-19 18:50:40 +0000

    views: Add accessibility support to the Textfield control.
    BUG=9619
    TEST=open AccExplorer.exe, open bookmarks manager dialog, point it to the search Textfield (edit box)
    and see it if has the correct information for role (editable text), name (the label within it's connected) and state (read only).
    Review URL: http://codereview.chromium.org/1058002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42128 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/textfield/textfield.h
+++ views/controls/textfield/textfield.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined (OS_LINUX)
 #include <gdk/gdk.h>
 #endif
+
+#include <string>
 
 #include "app/gfx/font.h"
 #include "base/basictypes.h"
@@ -203,6 +205,12 @@ class Textfield : public View {
   virtual void SetEnabled(bool enabled);
   virtual void PaintFocusBorder(gfx::Canvas* canvas);
 
+  // Accessibility accessors, overridden from View:
+  virtual bool GetAccessibleRole(AccessibilityTypes::Role* role);
+  virtual bool GetAccessibleName(std::wstring* name);
+  virtual bool GetAccessibleState(AccessibilityTypes::State* state);
+  virtual void SetAccessibleName(const std::wstring& name);
+
  protected:
   virtual void Focus();
   virtual void ViewHierarchyChanged(bool is_add, View* parent, View* child);
@@ -262,6 +270,9 @@ class Textfield : public View {
   // TODO(beng): remove this once NativeTextfieldWin subclasses
   //             NativeControlWin.
   bool initialized_;
+
+  // The storage string for the accessibility name associated with this control.
+  std::wstring accessible_name_;
 
   DISALLOW_COPY_AND_ASSIGN(Textfield);
 };
