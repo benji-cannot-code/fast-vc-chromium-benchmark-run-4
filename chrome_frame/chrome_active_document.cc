6086321ef3092b060dd259c748aa1c26b43476b4
amit@chromium.org <amit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-19 07:09:09 +0000

    Fix top crashers in the latest build.
    Fixes crashes due to NavigationManager instance being NULL. 
    
    TBR=tommi
    BUG=38602,38603
    
    Review URL: http://codereview.chromium.org/1100006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42083 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_active_document.cc
+++ chrome_frame/chrome_active_document.cc
@@ -233,11 +233,12 @@ STDMETHODIMP ChromeActiveDocument::Load(BOOL fully_avalable,
       moniker_name, bind_context,
       mgr ? mgr->original_url_with_fragment() : std::wstring()));
 
+  scoped_refptr<RequestData> data;
   if (mgr) {
     mgr->set_original_url_with_fragment(L"");
+    data = mgr->GetActiveRequestData(url.c_str());
   }
 
-  scoped_refptr<RequestData> data(mgr->GetActiveRequestData(url.c_str()));
   DLOG_IF(INFO, data) << "Got active request data";
   DLOG_IF(WARNING, data.get() == NULL) << "NO active request data";
 
