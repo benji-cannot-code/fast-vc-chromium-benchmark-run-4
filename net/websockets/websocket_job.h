9dbd57596c56015051d3807213f10ef93d23c8da
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-19 05:19:45 +0000

    Try to fix flaky websocket tests.
    Some websoket layout tests became flaky from r41818.
    This is because it adds websocket throttling in WebSocketJob.
    
    Make sure Close() will call OnClose() even if it is waiting resolving or waiting
    in throttling queue, so that WebSocketJob is removed from throttling queue
    and wake up next WebSocketJob.
    
    BUG=38397
    TEST=layout tests websocket/tests passes
    
    Review URL: http://codereview.chromium.org/1096001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42074 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_job.h
+++ net/websockets/websocket_job.h
@@ -83,6 +83,7 @@ class WebSocketJob : public SocketStreamJob, public SocketStream::Delegate {
   void SetWaiting();
   bool IsWaiting() const;
   void Wakeup();
+  void DoCallback();
 
   SocketStream::Delegate* delegate_;
   State state_;
