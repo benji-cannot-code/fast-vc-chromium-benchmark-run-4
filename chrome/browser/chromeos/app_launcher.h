9e9f4426f217753f773720e46004e6a525d7bf3f
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-19 16:17:41 +0000

    AppLauncher cleanup.   Pass browser to ctor as AppLauncher is created per browser instance.
    BUG=chromium-os:1795
    TEST=none
    
    Review URL: http://codereview.chromium.org/1085006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42111 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/app_launcher.h
+++ chrome/browser/chromeos/app_launcher.h
@@ -63,11 +63,11 @@ class AppLauncher : public RenderViewHostDelegate,
                     public ActiveWindowWatcherX::Observer,
                     public views::AcceleratorTarget {
  public:
-  AppLauncher();
+  AppLauncher(Browser* browser);
   ~AppLauncher();
 
   // Shows the menu.
-  void Show(Browser* browser);
+  void Show();
 
  private:
   // TabContentsDelegate and RenderViewHostDelegate::View have some methods
@@ -144,8 +144,8 @@ class AppLauncher : public RenderViewHostDelegate,
   // hidden.
   void Cleanup();
 
-  // Updates the app launcher's state and layout with the |browser|.
-  void Update(Browser* browser);
+  // Updates the app launcher's state.
+  void Update();
 
   // RenderViewHostDelegate overrides.
   virtual int GetBrowserWindowID() const {
