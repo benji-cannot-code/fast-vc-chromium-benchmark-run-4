8e6f85c92ee359e059a00cb42d3109088a7c2abd
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-21 20:08:39 +0000

    Blind attempt #2 to fix the Windows build after r56314
    * API/tests/testapi.c: Include JSObjectRefPrivate.h for the new
    methods instead of declaring them locally (and non-extern).
    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def:
    Backed out previous change.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/API/tests/testapi.c
+++ third_party/WebKit/JavaScriptCore/API/tests/testapi.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JavaScriptCore.h"
 #include "JSBasePrivate.h"
 #include "JSContextRefPrivate.h"
+#include "JSObjectRefPrivate.h"
 #include <math.h>
 #define ASSERT_DISABLED 0
 #include <wtf/Assertions.h>
@@ -42,10 +43,6 @@ static double nan(const char*)
 }
 
 #endif
-
-bool JSObjectSetPrivateProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef value);
-JSValueRef JSObjectGetPrivateProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName);
-bool JSObjectDeletePrivateProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName);
 
 static JSGlobalContextRef context;
 static int failed;
