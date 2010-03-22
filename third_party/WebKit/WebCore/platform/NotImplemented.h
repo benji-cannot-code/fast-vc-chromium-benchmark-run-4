bf56545d3a1edfe55680e67187143236878f7c71
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-22 08:05:51 +0000

    2010-03-22  Leandro Pereira  <leandro@profusion.mobi>
            Reviewed by Simon Hausmann.
    
            Add EFL-specific code to platform/NotImplemented.h.
            http://webkit.org/b/36320
    
            * platform/NotImplemented.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56323 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/NotImplemented.h
+++ third_party/WebKit/WebCore/platform/NotImplemented.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Logging.h"
 #include <wtf/Assertions.h>
 
-#if PLATFORM(GTK)
+#if PLATFORM(GTK) || PLATFORM(EFL)
     #define supressNotImplementedWarning() getenv("DISABLE_NI_WARNING")
 #elif PLATFORM(QT)
     #include <QByteArray>
