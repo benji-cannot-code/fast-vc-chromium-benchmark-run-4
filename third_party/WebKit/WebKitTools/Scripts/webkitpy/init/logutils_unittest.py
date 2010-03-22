1ff3bf04a3bab881e366324a76bf4c7e87fd67bb
cjerdonek@webkit.org <cjerdonek@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-22 12:49:05 +0000

    Created a function for getting a module-specific logging.logger based on the __file__ value of the module.
    Reviewed by Adam Barth.
    
    https://bugs.webkit.org/show_bug.cgi?id=35821
    
    This function allows us to get the module-specific logger for
    a module without having to hard-code the fully-qualified name
    of the module in the module itself.  The code can be the same
    in every case: "_log = logutils.get_logger(__file__)".
    
    * Scripts/webkitpy/init/logutils.py: Added.
      - Added a module with a get_logger() function to return
        a module-specific logger based on the module's __file__
        variable.
    
    * Scripts/webkitpy/init/logutils_unittest.py: Added.
      - Added unit tests for logutils.py.
    
    * Scripts/webkitpy/init/unittests.py:
      - Added logutils_unittest to the list of imports.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56331 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/init/logutils_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/init/logutils_unittest.py
@@ -21,9 +21,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""Runs init package unit tests."""
+"""Unit tests for logutils.py."""
 
-# This module is imported by the module that imports all webkitpy unit tests.
+import os
+import unittest
 
-from logutils_unittest import *
-from versioning_unittest import *
+import logutils
+
+
+class GetLoggerTest(unittest.TestCase):
+
+    """Tests get_logger()."""
+
+    def test_get_logger_in_webkitpy(self):
+        logger = logutils.get_logger(__file__)
+        self.assertEquals(logger.name, "webkitpy.init.logutils_unittest")
+
+    def test_get_logger_not_in_webkitpy(self):
+        # Temporarily change the working directory so that we
+        # can test get_logger() for a path outside of webkitpy.
+        working_directory = os.getcwd()
+        root_dir = "/"
+        os.chdir(root_dir)
+
+        logger = logutils.get_logger("/WebKitTools/Scripts/test-webkitpy")
+        self.assertEquals(logger.name, "test-webkitpy")
+
+        logger = logutils.get_logger("/WebKitTools/Scripts/test-webkitpy.py")
+        self.assertEquals(logger.name, "test-webkitpy")
+
+        os.chdir(working_directory)
