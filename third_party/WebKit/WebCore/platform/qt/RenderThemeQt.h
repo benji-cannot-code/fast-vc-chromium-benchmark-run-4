1b3cec0861920400a4798078e0946f41373329c4
jwieczorek@webkit.org <jwieczorek@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-22 17:42:13 +0000

    2010-03-22  Jakub Wieczorek  <jwieczorek@webkit.org>
            Reviewed by Simon Hausmann.
    
            [Qt] Don't construct a QLineEdit every time when painting a text field
            https://bugs.webkit.org/show_bug.cgi?id=36373
    
            Instead, keep one instance per RenderTheme around.
    
            * platform/qt/RenderThemeQt.cpp:
            (WebCore::findFrameLineWidth):
    2010-03-22  Jakub Wieczorek  <jwieczorek@webkit.org>
    
            Reviewed by Simon Hausmann.
    
            [Qt] Don't construct a QLineEdit every time when painting a text field
            https://bugs.webkit.org/show_bug.cgi?id=36373
    
            Add a simple benchmark covering this area.
    
            * tests/benchmarks/painting/tst_painting.cpp:
            (tst_Painting::textAreas):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56343 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/RenderThemeQt.h
+++ third_party/WebKit/WebCore/platform/qt/RenderThemeQt.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QStyle>
 
 QT_BEGIN_NAMESPACE
+class QLineEdit;
 class QPainter;
 class QWidget;
 QT_END_NAMESPACE
@@ -165,6 +166,8 @@ private:
 
     void setPaletteFromPageClientIfExists(QPalette&) const;
 
+    int findFrameLineWidth(QStyle* style) const;
+
     QStyle* fallbackStyle() const;
 
     Page* m_page;
@@ -175,6 +178,7 @@ private:
     QString m_buttonFontFamily;
 
     QStyle* m_fallbackStyle;
+    mutable QLineEdit* m_lineEdit;
 };
 
 class StylePainter {
--- third_party/WebKit/WebKit/qt/tests/benchmarks/painting/tst_painting.cpp
+++ third_party/WebKit/WebKit/qt/tests/benchmarks/painting/tst_painting.cpp
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <QtTest/QtTest>
 
+#include <qwebelement.h>
 #include <qwebframe.h>
 #include <qwebview.h>
 #include <qpainter.h>
@@ -60,6 +61,7 @@ public Q_SLOTS:
 private Q_SLOTS:
     void paint_data();
     void paint();
+    void textAreas();
 
 private:
     QWebView* m_view;
@@ -102,6 +104,31 @@ void tst_Painting::paint()
     QBENCHMARK {
         QPainter painter(&pixmap);
         mainFrame->render(&painter, QRect(QPoint(0, 0), m_page->viewportSize()));
+        painter.end();
+    }
+}
+
+void tst_Painting::textAreas()
+{
+    m_view->load(QUrl("data:text/html;<html><body></body></html>"));
+    ::waitForSignal(m_view, SIGNAL(loadFinished(bool)));
+
+    QWebElement bodyElement = m_page->mainFrame()->findFirstElement("body");
+
+    int count = 100;
+    while (count--) {
+        QString markup("<textarea cols='1' rows='1'></textarea>");
+        bodyElement.appendInside(markup);
+    }
+
+    /* force a layout */
+    QWebFrame* mainFrame = m_page->mainFrame();
+    mainFrame->toPlainText();
+
+    QPixmap pixmap(mainFrame->contentsSize());
+    QBENCHMARK {
+        QPainter painter(&pixmap);
+        mainFrame->render(&painter, QRect(QPoint(0, 0), mainFrame->contentsSize()));
         painter.end();
     }
 }
