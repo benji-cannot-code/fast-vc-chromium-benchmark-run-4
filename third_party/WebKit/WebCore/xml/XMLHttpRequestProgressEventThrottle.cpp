a5c6c7ac96d757606fa49a15466deb6e7a7fc3fd
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-23 15:47:02 +0000

    Unreviewed build fix.
    Try to make the window buildbot happy.
    
    * xml/XMLHttpRequestProgressEventThrottle.cpp:
    * xml/XMLHttpRequestProgressEventThrottle.h: Move initialization of the
    static const in the cpp file.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/xml/XMLHttpRequestProgressEventThrottle.cpp
+++ third_party/WebKit/WebCore/xml/XMLHttpRequestProgressEventThrottle.cpp
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+const double XMLHttpRequestProgressEventThrottle::minimumProgressEventDispatchingIntervalInSeconds = .05; // 50 ms per specification.
+
 XMLHttpRequestProgressEventThrottle::XMLHttpRequestProgressEventThrottle(EventTarget* target)
     : m_target(target)
     , m_loaded(0)
