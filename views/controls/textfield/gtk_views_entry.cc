3e38e946a89c4abd0bfc64d2e21c077dee9ac502
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-23 17:54:13 +0000

    Fixes compile error because gfx moved to src.
    BUG=none
    TEST=none
    TBR=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/1150007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42359 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/textfield/gtk_views_entry.cc
+++ views/controls/textfield/gtk_views_entry.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "views/controls/textfield/gtk_views_entry.h"
 
-#include "app/gfx/canvas_paint.h"
+#include "gfx/canvas_paint.h"
 #include "base/utf_string_conversions.h"
 #include "gfx/insets.h"
 #include "gfx/skia_utils_gtk.h"
