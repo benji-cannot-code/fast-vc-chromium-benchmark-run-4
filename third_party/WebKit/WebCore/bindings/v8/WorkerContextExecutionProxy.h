eaecf2bd433da40d16fc4e8b80a6ca9c16e025d6
dimich@chromium.org <dimich@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-23 02:21:02 +0000

    [v8] Crash if the worker is terminated before its initial script is executed. https://bugs.webkit.org/show_bug.cgi?id=36336
    Reviewed by Nate Chapin.
    
    WebCore:
    
    Test: fast/workers/termination-early.html
    
    * bindings/v8/WorkerContextExecutionProxy.cpp:
    (WebCore::WorkerContextExecutionProxy::initContextIfNeeded): Check for 0 context. When terminated, the v8 heap initialization returns 0.
    (WebCore::WorkerContextExecutionProxy::evaluate): Don't execute script if initialization failed.
    * bindings/v8/WorkerContextExecutionProxy.h:
    
    LayoutTests:
    
    * fast/workers/termination-early-expected.txt: Added.
    * fast/workers/termination-early.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56375 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/WorkerContextExecutionProxy.h
+++ third_party/WebKit/WebCore/bindings/v8/WorkerContextExecutionProxy.h
@@ -80,7 +80,7 @@ namespace WebCore {
 
     private:
         void initV8IfNeeded();
-        void initContextIfNeeded();
+        bool initContextIfNeeded();
         void dispose();
 
         // Run an already compiled script.
