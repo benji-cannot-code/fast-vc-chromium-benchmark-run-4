9ca87b402f49640897a42e4d6519a450121565a0
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-23 23:28:13 +0000

    2010-03-23  Kenneth Russell  <kbr@google.com>
            Unreviewed build fix.
    
            Add #include <stdio.h> to fix Chromium Linux build.
    
            * src/WebGraphicsContext3DDefaultImpl.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebGraphicsContext3DDefaultImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebGraphicsContext3DDefaultImpl.cpp
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(3D_CANVAS)
 
+#include <stdio.h>
+
 #include "WebGraphicsContext3DDefaultImpl.h"
 
 #include "NotImplemented.h"
