1548d91ace9f109dd84089df00851dafa76079e5
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-23 06:40:20 +0000

    Try to fix flaky websocket tests again.
    Some websoket layout tests became flaky from r41818.
    This is because it adds websocket throttling in WebSocketJob.
    
    Make sure Close() will call OnClose() even if it is waiting resolving or waiting
    in throttling queue, so that WebSocketJob is removed from throttling queue
    and wake up next WebSocketJob.
    
    r42074 was reverted because failure in SocketStreamMetrics::OnClose
    on Linux Builder (Views dbg).
    Ignore UMA record if the connection has not been established.
    
    TBR=tyoshino
    BUG=38397
    TEST=layout tests websocket/tests passes
    
    Review URL: http://codereview.chromium.org/1134004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42320 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_job.cc
+++ net/websockets/websocket_job.cc
@@ -13,30 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context.h"
 #include "net/websockets/websocket_throttle.h"
 
-namespace {
-
-class CompletionCallbackRunner
-    : public base::RefCountedThreadSafe<CompletionCallbackRunner> {
- public:
-  explicit CompletionCallbackRunner(net::CompletionCallback* callback)
-      : callback_(callback) {
-    DCHECK(callback_);
-  }
-  void Run() {
-    callback_->Run(net::OK);
-  }
- private:
-  friend class base::RefCountedThreadSafe<CompletionCallbackRunner>;
-
-  virtual ~CompletionCallbackRunner() {}
-
-  net::CompletionCallback* callback_;
-
-  DISALLOW_COPY_AND_ASSIGN(CompletionCallbackRunner);
-};
-
-}
-
 namespace net {
 
 // lower-case header names.
@@ -159,10 +135,17 @@ void WebSocketJob::DetachDelegate() {
   Singleton<WebSocketThrottle>::get()->RemoveFromQueue(this);
   Singleton<WebSocketThrottle>::get()->WakeupSocketIfNecessary();
 
+  scoped_refptr<WebSocketJob> protect(this);
+
   delegate_ = NULL;
   if (socket_)
     socket_->DetachDelegate();
   socket_ = NULL;
+  if (callback_) {
+    waiting_ = false;
+    callback_ = NULL;
+    Release();  // Balanced with OnStartOpenConnection().
+  }
 }
 
 int WebSocketJob::OnStartOpenConnection(
@@ -174,6 +157,7 @@ int WebSocketJob::OnStartOpenConnection(
   if (!waiting_)
     return OK;
   callback_ = callback;
+  AddRef();  // Balanced when callback_ becomes NULL.
   return ERR_IO_PENDING;
 }
 
@@ -207,9 +191,16 @@ void WebSocketJob::OnClose(SocketStream* socket) {
   Singleton<WebSocketThrottle>::get()->RemoveFromQueue(this);
   Singleton<WebSocketThrottle>::get()->WakeupSocketIfNecessary();
 
+  scoped_refptr<WebSocketJob> protect(this);
+
   SocketStream::Delegate* delegate = delegate_;
   delegate_ = NULL;
   socket_ = NULL;
+  if (callback_) {
+    waiting_ = false;
+    callback_ = NULL;
+    Release();  // Balanced with OnStartOpenConnection().
+  }
   if (delegate)
     delegate->OnClose(socket);
 }
@@ -438,16 +429,24 @@ bool WebSocketJob::IsWaiting() const {
 }
 
 void WebSocketJob::Wakeup() {
+  if (!waiting_)
+    return;
   waiting_ = false;
   DCHECK(callback_);
-  // We wrap |callback_| to keep this alive while this is released.
-  scoped_refptr<CompletionCallbackRunner> runner =
-      new CompletionCallbackRunner(callback_);
-  callback_ = NULL;
   MessageLoopForIO::current()->PostTask(
       FROM_HERE,
-      NewRunnableMethod(runner.get(),
-                        &CompletionCallbackRunner::Run));
+      NewRunnableMethod(this,
+                        &WebSocketJob::DoCallback));
+}
+
+void WebSocketJob::DoCallback() {
+  // |callback_| may be NULL if OnClose() or DetachDelegate() was called.
+  if (callback_) {
+    net::CompletionCallback* callback = callback_;
+    callback_ = NULL;
+    callback->Run(net::OK);
+    Release();  // Balanced with OnStartOpenConnection().
+  }
 }
 
 }  // namespace net
--- net/websockets/websocket_job.h
+++ net/websockets/websocket_job.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/ref_counted.h"
 #include "net/base/address_list.h"
 #include "net/base/completion_callback.h"
 #include "net/socket_stream/socket_stream_job.h"
@@ -83,6 +82,7 @@ class WebSocketJob : public SocketStreamJob, public SocketStream::Delegate {
   void SetWaiting();
   bool IsWaiting() const;
   void Wakeup();
+  void DoCallback();
 
   SocketStream::Delegate* delegate_;
   State state_;
