88a3ec3ab3485e5796e20449e6d6d56e42cadd9e
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-23 20:51:13 +0000

    2010-03-23  Daniel Bates  <dbates@rim.com>
            Reviewed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=36149
    
            Import the GNU readline interface to modify the behavior
            of raw_input so as to provide line editing support. In
            particular this will prevent "delete" characters from
            appearing in the returned value for function raw_input.
    
            * Scripts/webkitpy/user.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56413 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/user.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/user.py
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
+import readline
 import shlex
 import subprocess
 import webbrowser
