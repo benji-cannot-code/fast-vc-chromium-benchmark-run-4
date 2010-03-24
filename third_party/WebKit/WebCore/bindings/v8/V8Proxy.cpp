ea1eb1b6ae8a21d9ab1c19b8b10676c9f736620a
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-24 16:25:48 +0000

    2010-03-24  Pavel Feldman  <pfeldman@chromium.org>
            Not reviewed. Chromium test fix: added a frameCount check to the
            debugger function call.
    
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::createUtilityContext):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8Proxy.cpp
+++ third_party/WebKit/WebCore/bindings/v8/V8Proxy.cpp
@@ -766,6 +766,8 @@ void V8Proxy::createUtilityContext()
     // JavaScript stack frame.
     DEFINE_STATIC_LOCAL(const char*, frameSourceLineSource,
         ("function frameSourceLine(exec_state) {"
+        "  if (!exec_state.frameCount())"
+        "      return undefined;"
         "  return exec_state.frame(0).sourceLine();"
         "}"));
     v8::Script::Compile(v8::String::New(frameSourceLineSource))->Run();
@@ -774,6 +776,8 @@ void V8Proxy::createUtilityContext()
     // JavaScript stack frame.
     DEFINE_STATIC_LOCAL(const char*, frameSourceNameSource,
         ("function frameSourceName(exec_state) {"
+        "  if (!exec_state.frameCount())"
+        "      return undefined;"
         "  var frame = exec_state.frame(0);"
         "  if (frame.func().resolved() && "
         "      frame.func().script() && "
