bce1238b3d0bddd781b86836fc5133b6e2c07d91
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-24 19:05:25 +0000

    WebCore: Add a way to check if the page client is making use of a QWidget.
    Reviewed by Simon Hausmann.
    
    * platform/qt/QWebPageClient.h:
    (QWebPageClient::isQWidgetClient):
    
    WebKit/qt: Calling setView on a QWebPage being shown by a QGraphicsWebView,
    would uninstall the page client, resulting in not expected
    behaviour.
    
    Reviewed by Simon Hausmann.
    
    * Api/qwebpage.cpp:
    (QWebPageWidgetClient::QWebPageWidgetClient):
    (QWebPageWidgetClient::isQWidgetClient):
    (QWebPageWidgetClient::screenNumber):
    (QWebPage::QWebPage):
    (QWebPage::setView):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/QWebPageClient.h
+++ third_party/WebKit/WebCore/platform/qt/QWebPageClient.h
@@ -41,7 +41,9 @@ QT_END_NAMESPACE
 class QWebPageClient {
 public:
     virtual ~QWebPageClient() { }
-        
+
+    virtual bool isQWidgetClient() const { return false; }
+
     virtual void scroll(int dx, int dy, const QRect&) = 0;
     virtual void update(const QRect&) = 0;
     virtual void setInputMethodEnabled(bool enable) = 0;
