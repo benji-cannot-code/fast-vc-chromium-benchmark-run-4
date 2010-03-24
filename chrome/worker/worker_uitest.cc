5a9fa76366c298fa3bce818902204aac975c397f
dpranke@google.com <dpranke@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-24 03:47:48 +0000

    Use upstream layout_test baselines since the downstream ones are going away.
        BUG=none
        TEST=bots stay green
        R=eroman@chromium.org
    
    Review URL: http://codereview.chromium.org/1214001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42426 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/worker/worker_uitest.cc
+++ chrome/worker/worker_uitest.cc
@@ -80,7 +80,6 @@ class WorkerTest : public UILayoutTest {
 
   void RunWorkerFastLayoutTest(const std::string& test_case_file_name) {
     FilePath fast_test_dir;
-    fast_test_dir = fast_test_dir.AppendASCII("LayoutTests");
     fast_test_dir = fast_test_dir.AppendASCII("fast");
 
     FilePath worker_test_dir;
@@ -314,7 +313,6 @@ TEST_F(WorkerTest, DISABLED_SharedWorkerFastLayoutTests) {
   };
 
   FilePath fast_test_dir;
-  fast_test_dir = fast_test_dir.AppendASCII("LayoutTests");
   fast_test_dir = fast_test_dir.AppendASCII("fast");
 
   FilePath worker_test_dir;
@@ -352,7 +350,6 @@ TEST_F(WorkerTest, FLAKY_WorkerHttpLayoutTests) {
   };
 
   FilePath http_test_dir;
-  http_test_dir = http_test_dir.AppendASCII("LayoutTests");
   http_test_dir = http_test_dir.AppendASCII("http");
   http_test_dir = http_test_dir.AppendASCII("tests");
 
