c6b3f7ca2b4e63449893fd5c01feb3d9480fa38b
dpranke@google.com <dpranke@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-24 03:55:32 +0000

    Revert r42426 - tests are failing because the WebKit LayoutTests directories aren't checked out on the bots. We need to figure out how to deal with this since we don't want to keep expectations downstream if we can avoid it.
        BUG=none
        R=none
        TEST=bots stay green
    
    Review URL: http://codereview.chromium.org/1253002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42427 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/in_process_webkit/dom_storage_uitest.cc
+++ chrome/browser/in_process_webkit/dom_storage_uitest.cc
@@ -41,7 +41,8 @@ class DOMStorageTest : public UILayoutTest {
  protected:
   DOMStorageTest()
       : UILayoutTest(),
-        test_dir_(FilePath().AppendASCII("storage").AppendASCII("domstorage")) {
+        test_dir_(FilePath().AppendASCII("LayoutTests").
+                  AppendASCII("storage").AppendASCII("domstorage")) {
   }
 
   virtual ~DOMStorageTest() { }
@@ -55,7 +56,8 @@ class DOMStorageTest : public UILayoutTest {
   // Add those to the list to be copied.
   void AddJSTestResources() {
     // Add other paths our tests require.
-    FilePath js_dir = FilePath().AppendASCII("fast").AppendASCII("js");
+    FilePath js_dir = FilePath().AppendASCII("LayoutTests").
+                      AppendASCII("fast").AppendASCII("js");
     AddResourceForLayoutTest(js_dir, FilePath().AppendASCII("resources"));
   }
 
