c7b1be28529e99e337a6f479e61ce6ec00ca2a9f
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-24 06:36:19 +0000

    Replace about:plugins page with new page.
    Do this by "remapping" about:plugins to chrome://plugins/. Also delete old
    about:plugins page and associated strings.
    
    BUG=736
    TEST=Go to about:plugins; should get new plugins page (same as chrome://plugins/).
    
    Review URL: http://codereview.chromium.org/1219005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42434 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -44,6 +44,7 @@ const char kAboutCreditsURL[] = "about:credits";
 const char kAboutHangURL[] = "about:hang";
 const char kAboutMemoryURL[] = "about:memory";
 const char kAboutNetInternalsURL[] = "about:net-internals";
+const char kAboutPluginsURL[] = "about:plugins";
 const char kAboutShorthangURL[] = "about:shorthang";
 const char kAboutTermsURL[] = "about:terms";
 
