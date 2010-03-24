a51efbb4ffd74b54909cf9065d33ee1136b33e27
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-24 17:02:15 +0000

    Revert 42474 - Make repost form warning tabmodal on Gtk.
    This is the Gtk version of http://codereview.chromium.org/969003.
    
    BUG=26271
    TEST=Go to http://www.cs.unc.edu/~jbs/resources/perl/perlcgi/programs/form1POST.html, hit Submit, then refresh. The warning sheet should be tabmodal, not windowmodal. Opening a login form (which is also tabmodal) while the warning is shown should not crash, neither should bringing up the warning while a login form is shown.
    
    Review URL: http://codereview.chromium.org/660434
    
    TBR=bauerb@chromium.org
    Review URL: http://codereview.chromium.org/1247004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42479 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/repost_form_warning_gtk.h
+++ chrome/browser/gtk/repost_form_warning_gtk.h
@@ -9,25 +9,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gtk/gtk.h>
 
 #include "chrome/common/notification_registrar.h"
-#include "chrome/browser/gtk/constrained_window_gtk.h"
-#include "app/gtk_signal.h"
 
 class NavigationController;
-class TabContents;
 
 // Displays a dialog that warns the user that they are about to resubmit a form.
 // To display the dialog, allocate this object on the heap. It will open the
 // dialog from its constructor and then delete itself when the user dismisses
 // the dialog.
-class RepostFormWarningGtk : public NotificationObserver,
-    ConstrainedWindowGtkDelegate {
+class RepostFormWarningGtk : public NotificationObserver {
  public:
-  RepostFormWarningGtk(GtkWindow* parent, TabContents* tab_contents);
+  RepostFormWarningGtk(GtkWindow* parent,
+                       NavigationController* navigation_controller);
   virtual ~RepostFormWarningGtk();
-
-  virtual GtkWidget* GetWidgetRoot();
-
-  virtual void DeleteDelegate();
 
  private:
   // NotificationObserver implementation.
@@ -37,15 +30,13 @@ class RepostFormWarningGtk : public NotificationObserver,
                        const NotificationDetails& details);
 
   // Tell Gtk to destroy the dialog window.  This will only be done once, even
-  // if Destroy is called multiple times.
+  // if Destroy is called multiple times (eg, from both OnResponse and Observe.)
   void Destroy();
 
-  CHROMEGTK_CALLBACK_0(RepostFormWarningGtk, void, OnRefresh);
-  CHROMEGTK_CALLBACK_0(RepostFormWarningGtk, void, OnCancel);
-  CHROMEGTK_CALLBACK_1(RepostFormWarningGtk,
-                       void,
-                       OnHierarchyChanged,
-                       GtkWidget*);
+  static void OnResponse(GtkWidget* widget,
+                         int response,
+                         RepostFormWarningGtk* dialog);
+  static void OnWindowDestroy(GtkWidget* widget, RepostFormWarningGtk* dialog);
 
   NotificationRegistrar registrar_;
 
@@ -53,10 +44,6 @@ class RepostFormWarningGtk : public NotificationObserver,
   NavigationController* navigation_controller_;
 
   GtkWidget* dialog_;
-  GtkWidget* ok_;
-  GtkWidget* cancel_;
-
-  ConstrainedWindow* window_;
 
   DISALLOW_COPY_AND_ASSIGN(RepostFormWarningGtk);
 };
