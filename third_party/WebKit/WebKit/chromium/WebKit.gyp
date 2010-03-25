5e5bfbd6238216c46cb5ffdedf8ead2818bb5e04
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-25 23:48:30 +0000

    2010-03-25  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Darin Fisher.
    
            [Chromium] Add an ASSERT macro to the Chromium WebKit API
            https://bugs.webkit.org/show_bug.cgi?id=36545
    
            * WebKit.gyp:  Add WebCommon.cpp
            * public/WebCommon.h:  Add the Macro.
            * public/WebPrivatePtr.h:
            (WebKit::WebPrivatePtr::~WebPrivatePtr):  Verify the pointer is now 0.
            * src/WebCommon.cpp: Added.
            (WebKit::failedAssertion): Calls the WTF assert function and then crashes.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56579 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/WebKit.gyp
+++ third_party/WebKit/WebKit/chromium/WebKit.gyp
@@ -288,6 +288,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 'src/WebBindings.cpp',
                 'src/WebCache.cpp',
                 'src/WebColor.cpp',
+                'src/WebCommon.cpp',
                 'src/WebCrossOriginPreflightResultCache.cpp',
                 'src/WebCString.cpp',
                 'src/WebCursorInfo.cpp',
--- third_party/WebKit/WebKit/chromium/public/WebCommon.h
+++ third_party/WebKit/WebKit/chromium/public/WebCommon.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2009 Google Inc. All rights reserved.
+ * Copyright (C) 2010 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -90,6 +90,21 @@ typedef wchar_t WebUChar;
 typedef unsigned short WebUChar;
 #endif
 
+// -----------------------------------------------------------------------------
+// Assertions
+
+WEBKIT_API void failedAssertion(const char* file, int line, const char* function, const char* assertion);
+
 } // namespace WebKit
+
+// Ideally, only use inside the public directory but outside of WEBKIT_IMPLEMENTATION blocks.  (Otherwise use WTF's ASSERT.)
+#if defined(NDEBUG)
+#define WEBKIT_ASSERT(assertion) ((void)0)
+#else
+#define WEBKIT_ASSERT(assertion) do { \
+    if (!(assertion)) \
+        failedAssertion(__FILE__, __LINE__, __FUNCTION__, #assertion); \
+} while (0)
+#endif
 
 #endif
