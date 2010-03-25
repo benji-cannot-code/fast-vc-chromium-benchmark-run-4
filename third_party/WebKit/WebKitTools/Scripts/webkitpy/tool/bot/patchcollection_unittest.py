387ec33593306b39aaa0d887c504914c6a3280a9
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-25 06:40:33 +0000

    2010-03-24  Adam Barth  <abarth@webkit.org>
            Unreviewed.  Move patchcollection.py to its new home.
    
            * Scripts/webkitpy/patchcollection.py: Removed.
            * Scripts/webkitpy/patchcollection_unittest.py: Removed.
            * Scripts/webkitpy/tool/bot: Added.
            * Scripts/webkitpy/tool/bot/__init__.py: Added.
            * Scripts/webkitpy/tool/bot/patchcollection.py: Copied from WebKitTools/Scripts/webkitpy/patchcollection.py.
            * Scripts/webkitpy/tool/bot/patchcollection_unittest.py: Copied from WebKitTools/Scripts/webkitpy/patchcollection_unittest.py.
            * Scripts/webkitpy/tool/commands/queues.py:
            * Scripts/webkitpy/unittests.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/bot/patchcollection_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/bot/patchcollection_unittest.py
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
-from webkitpy.patchcollection import PersistentPatchCollection, PersistentPatchCollectionDelegate
+from webkitpy.tool.bot.patchcollection import PersistentPatchCollection, PersistentPatchCollectionDelegate
 from webkitpy.thirdparty.mock import Mock
 
 
