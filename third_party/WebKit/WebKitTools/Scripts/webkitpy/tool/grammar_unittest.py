d205ae5f2b6eae8841b8e20cf102828441274636
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-25 08:50:14 +0000

    2010-03-25  Adam Barth  <abarth@webkit.org>
            Unreviewed.  Move grammary.py into webkitpy.tool.
            https://bugs.webkit.org/show_bug.cgi?id=36093
    
            * Scripts/webkitpy/grammar.py: Removed.
            * Scripts/webkitpy/grammar_unittest.py: Removed.
            * Scripts/webkitpy/tool/commands/download.py:
            * Scripts/webkitpy/tool/commands/queries.py:
            * Scripts/webkitpy/tool/commands/queues.py:
            * Scripts/webkitpy/tool/commands/upload.py:
            * Scripts/webkitpy/tool/grammar.py: Copied from Scripts/webkitpy/grammar.py.
            * Scripts/webkitpy/tool/grammar_unittest.py: Copied from Scripts/webkitpy/grammar_unittest.py.
            * Scripts/webkitpy/tool/multicommandtool.py:
            * Scripts/webkitpy/tool/steps/obsoletepatches.py:
            * Scripts/webkitpy/tool/steps/updatechangelogswithreviewer.py:
            * Scripts/webkitpy/unittests.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56516 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/grammar_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/grammar_unittest.py
@@ -28,7 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import unittest
-from webkitpy.grammar import join_with_separators
+
+from webkitpy.tool.grammar import join_with_separators
 
 class GrammarTest(unittest.TestCase):
 
--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/steps/updatechangelogswithreviewer.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/steps/updatechangelogswithreviewer.py
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 
 from webkitpy.common.checkout.changelog import ChangeLog
-from webkitpy.grammar import pluralize
+from webkitpy.tool.grammar import pluralize
 from webkitpy.tool.steps.abstractstep import AbstractStep
 from webkitpy.tool.steps.options import Options
 from webkitpy.webkit_logging import log, error
