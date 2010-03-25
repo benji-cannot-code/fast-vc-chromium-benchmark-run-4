02d620ac430d36b2b9b3dc320b90a136e15da97f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-25 06:22:55 +0000

    2010-03-24  Eric Seidel  <eric@webkit.org>
            Unreviewed, build fix only.
    
            Abstract LayoutTestResults logic for easier reuse
            https://bugs.webkit.org/show_bug.cgi?id=36579
    
            * Scripts/webkitpy/buildbot_unittest.py: Add a missing import.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56496 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/buildbot_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/buildbot_unittest.py
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
-from webkitpy.buildbot import BuildBot, Builder, Build
+from webkitpy.buildbot import BuildBot, Builder, Build, LayoutTestResults
 
 from webkitpy.thirdparty.BeautifulSoup import BeautifulSoup
 
