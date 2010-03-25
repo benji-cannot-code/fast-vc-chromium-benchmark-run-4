fe932a0d457892ff764e069a9322ad7f6c77fe94
slightlyoff@chromium.org <slightlyoff@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-25 22:30:03 +0000

    Implements IDeleteBrowsing history and moves the GCF profile into the IE TIF directory for non-priv mode users on IE < 8.
    Implementation notes:
    
    Earlier work enabled InPrivate browsing detection and mapped it to creation of an incognito profile instance.Privacy features and how they operate with this change:
    
    "Delete Browsing History":
    
    	IE 6 & 7: all history (including databases) is deleted if cache is cleared *WITHOUT* an active Chrome process holding references to the profile resources. If GCF is rendering a page when the cache is cleared, history *WILL NOT* be deleted on the GCF side, however GCF will continue to operate and IE will remove all other history artifacts as usual.
    
    	IE 8: GCF cache is cleared in alignment with the options specified by the user. Clearing Temporary Internet Files may destroy the profile entirely, and so we need to consider not moving the GCF profile on IE 8.
    
    "InPrivate Filtering":
    
    	IE 8 (only): more testing required.
    
    "InPrivate Browsing":
    
    	IE 8 (only): pages rendered in GCF *after* entering InPrivate mode are not persisted to disk (use an incognito wrapper on the specified profile). Currently displayed pages are not effected by the switch, although refreshing them will invoke the new behavior. Generally speaking, BHO's are disabled by IE 8 while in InPrivate mode, so entering this state is wonky to begin with but we handle it as well as can be expected.
    
    BUG=22846
    TEST=On IE 8, clear the cache entirely, note GCF entries in DbgView (better tests coming)
    Review URL: http://codereview.chromium.org/858003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42684 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/automation_client_mock.cc
+++ chrome_frame/test/automation_client_mock.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 #include "chrome_frame/test/automation_client_mock.h"
@@ -45,7 +45,7 @@ void MockProxyFactory::GetServerImpl(ChromeFrameAutomationProxy* pxy,
 void CFACMockTest::SetAutomationServerOk() {
   EXPECT_CALL(factory_, GetAutomationServer(testing::NotNull(),
               testing::Field(&ChromeFrameLaunchParams::profile_name,
-                             testing::StrEq(profile_)),
+                  testing::StrEq(profile_path_.BaseName().ToWStringHack())),
               testing::NotNull()))
     .Times(1)
     .WillOnce(testing::Invoke(CreateFunctor(&factory_,
@@ -92,7 +92,8 @@ TEST(CFACWithChrome, CreateTooFast) {
   MockCFDelegate cfd;
   chrome_frame_test::TimedMsgLoop loop;
   int timeout = 0;  // Chrome cannot send Hello message so fast.
-  const std::wstring profile = L"Adam.N.Epilinter";
+  const FilePath profile_path(
+      chrome_frame_test::GetProfilePath(L"Adam.N.Epilinter"));
 
   scoped_refptr<ChromeFrameAutomationClient> client;
   client = new ChromeFrameAutomationClient();
@@ -101,7 +102,8 @@ TEST(CFACWithChrome, CreateTooFast) {
       .Times(1)
       .WillOnce(QUIT_LOOP(loop));
 
-  EXPECT_TRUE(client->Initialize(&cfd, timeout, false, profile, L"", false));
+  EXPECT_TRUE(client->Initialize(&cfd, timeout, false, profile_path, L"",
+                                 false));
   loop.RunFor(10);
   client->Uninitialize();
 }
@@ -113,7 +115,8 @@ TEST(CFACWithChrome, CreateTooFast) {
 TEST(CFACWithChrome, CreateNotSoFast) {
   MockCFDelegate cfd;
   chrome_frame_test::TimedMsgLoop loop;
-  const std::wstring profile = L"Adam.N.Epilinter";
+  const FilePath profile_path(
+      chrome_frame_test::GetProfilePath(L"Adam.N.Epilinter"));
   int timeout = 10000;
 
   scoped_refptr<ChromeFrameAutomationClient> client;
@@ -126,7 +129,8 @@ TEST(CFACWithChrome, CreateNotSoFast) {
   EXPECT_CALL(cfd, OnAutomationServerLaunchFailed(_, _))
       .Times(0);
 
-  EXPECT_TRUE(client->Initialize(&cfd, timeout, false, profile, L"", false));
+  EXPECT_TRUE(client->Initialize(&cfd, timeout, false, profile_path, L"",
+                                 false));
 
   loop.RunFor(11);
   client->Uninitialize();
@@ -136,8 +140,9 @@ TEST(CFACWithChrome, CreateNotSoFast) {
 TEST(CFACWithChrome, NavigateOk) {
   MockCFDelegate cfd;
   chrome_frame_test::TimedMsgLoop loop;
-  const std::wstring profile = L"Adam.N.Epilinter";
   const std::string url = "about:version";
+  const FilePath profile_path(
+      chrome_frame_test::GetProfilePath(L"Adam.N.Epilinter"));
   int timeout = 10000;
 
   scoped_refptr<ChromeFrameAutomationClient> client;
@@ -166,7 +171,8 @@ TEST(CFACWithChrome, NavigateOk) {
         .WillOnce(QUIT_LOOP(loop));
   }
 
-  EXPECT_TRUE(client->Initialize(&cfd, timeout, false, profile, L"", false));
+  EXPECT_TRUE(client->Initialize(&cfd, timeout, false, profile_path, L"",
+                                 false));
   loop.RunFor(10);
   client->Uninitialize();
   client = NULL;
@@ -175,7 +181,8 @@ TEST(CFACWithChrome, NavigateOk) {
 TEST(CFACWithChrome, NavigateFailed) {
   MockCFDelegate cfd;
   chrome_frame_test::TimedMsgLoop loop;
-  const std::wstring profile = L"Adam.N.Epilinter";
+  const FilePath profile_path(
+      chrome_frame_test::GetProfilePath(L"Adam.N.Epilinter"));
   const std::string url = "http://127.0.0.3:65412/";
   const URLRequestStatus connection_failed(URLRequestStatus::FAILED,
                                            net::ERR_INVALID_URL);
@@ -205,7 +212,8 @@ TEST(CFACWithChrome, NavigateFailed) {
       .Times(1)
       .WillOnce(QUIT_LOOP_SOON(loop, 2));
 
-  EXPECT_TRUE(client->Initialize(&cfd, 10000, false, profile, L"", false));
+  EXPECT_TRUE(client->Initialize(&cfd, 10000, false, profile_path, L"",
+                                 false));
 
   loop.RunFor(10);
   client->Uninitialize();
@@ -238,7 +246,8 @@ TEST_F(CFACMockTest, MockedCreateTabOk) {
   EXPECT_CALL(proxy_, CancelAsync(_)).Times(testing::AnyNumber());
 
   // Here we go!
-  EXPECT_TRUE(client_->Initialize(&cfd_, timeout, false, profile_, L"", false));
+  EXPECT_TRUE(client_->Initialize(&cfd_, timeout, false, profile_path_, L"",
+                                  false));
   loop_.RunFor(10);
   client_->Uninitialize();
 }
@@ -263,7 +272,7 @@ TEST_F(CFACMockTest, MockedCreateTabFailed) {
   Set_CFD_LaunchFailed(AUTOMATION_CREATE_TAB_FAILED);
 
   // Here we go!
-  EXPECT_TRUE(client_->Initialize(&cfd_, timeout_, false, profile_, L"",
+  EXPECT_TRUE(client_->Initialize(&cfd_, timeout_, false, profile_path_, L"",
               false));
   loop_.RunFor(4);
   client_->Uninitialize();
