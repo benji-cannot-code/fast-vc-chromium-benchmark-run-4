1a91391d483b62aedd089cc9e326bd59c494b5aa
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-25 21:14:09 +0000

    Convert tab to app after user clicks "create" button per issue 27905.
    BUG=27905
    TEST=Verify fix for 27095.
    
    Review URL: http://codereview.chromium.org/1351001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42670 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/create_application_shortcuts_dialog_gtk.cc
+++ chrome/browser/gtk/create_application_shortcuts_dialog_gtk.cc
@@ -10,26 +10,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/gtk/gtk_util.h"
 #include "chrome/browser/shell_integration.h"
+#include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/browser/tab_contents/tab_contents_delegate.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/locale_settings.h"
 
 // static
 void CreateApplicationShortcutsDialogGtk::Show(GtkWindow* parent,
-                                               const GURL& url,
-                                               const string16& title,
-                                               const SkBitmap& favicon) {
-  new CreateApplicationShortcutsDialogGtk(parent, url, title, favicon);
+                                               TabContents* tab_contents) {
+  new CreateApplicationShortcutsDialogGtk(parent, tab_contents);
 }
 
 CreateApplicationShortcutsDialogGtk::CreateApplicationShortcutsDialogGtk(
     GtkWindow* parent,
-    const GURL& url,
-    const string16& title,
-    const SkBitmap& favicon)
-    : url_(url),
-      title_(title),
-      favicon_(favicon),
+    TabContents* tab_contents)
+    : tab_contents_(tab_contents),
+      url_(tab_contents->GetURL()),
+      title_(tab_contents->GetTitle()),
+      favicon_(tab_contents->FavIconIsValid() ? tab_contents->GetFavIcon() :
+                                                SkBitmap()),
       error_dialog_(NULL) {
   DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
 
@@ -108,6 +108,9 @@ void CreateApplicationShortcutsDialogGtk::OnCreateDialogResponse(
          NewRunnableMethod(this,
              &CreateApplicationShortcutsDialogGtk::CreateDesktopShortcut,
              shortcut_info));
+
+    if (tab_contents_->delegate())
+      tab_contents_->delegate()->ConvertContentsToApplication(tab_contents_);
   } else {
     Release();
   }
