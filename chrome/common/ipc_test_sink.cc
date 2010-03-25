23555bddb1b9bf570a8967070b7528c6dd58f659
neb@chromium.org <neb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-25 19:39:57 +0000

    Add a Pepper audio basic functionality unit test.
    Also changed TestSink to be derived from IPC::Channel and made MockRenderThread service AddFilter/RemoveFilter, so that it can be used by MessageFilters.
    
    Review URL: http://codereview.chromium.org/1207005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42656 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/ipc_test_sink.cc
+++ chrome/common/ipc_test_sink.cc
@@ -13,8 +13,9 @@ TestSink::TestSink() {
 TestSink::~TestSink() {
 }
 
-void TestSink::OnMessageReceived(const Message& msg) {
-  messages_.push_back(Message(msg));
+bool TestSink::Send(Message* msg) {
+  messages_.push_back(Message(*msg));
+  return true;
 }
 
 void TestSink::ClearMessages() {
