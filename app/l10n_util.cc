124d6b2334aca180862e9677c652b5d0da097440
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-25 00:48:49 +0000

    Enable LAGNUAGE environment for chromeos in debug build.  This is still useful when debugging/troubleshooting.
    BUG=none
    TEST=build debug build with GYP_DEFINES=chromeos=1 and run "$ LAGNAUGE=he ./out/Debug/chrome".
    
    Review URL: http://codereview.chromium.org/1139005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42566 0039d316-1c4b-4281-b951-d872f2087c98

--- app/l10n_util.cc
+++ app/l10n_util.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -396,6 +396,13 @@ std::string GetApplicationLocale(const std::wstring& pref_locale) {
   candidates.push_back(system_locale);
 
 #elif defined(OS_CHROMEOS)
+  // We use --lang on chroemos for debugging/troubleshooting purpose.
+  const CommandLine& parsed_command_line = *CommandLine::ForCurrentProcess();
+  const std::string& lang_arg =
+      parsed_command_line.GetSwitchValueASCII(switches::kLang);
+  if (!lang_arg.empty())
+    candidates.push_back(lang_arg);
+
   // On ChromeOS, try user prefs. This restores the locale used by the
   // previous run of the OS.
   if (!pref_locale.empty())
