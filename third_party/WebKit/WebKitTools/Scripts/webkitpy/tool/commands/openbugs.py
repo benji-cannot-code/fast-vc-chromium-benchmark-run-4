12786bfd7ec506fe2022d7b8a555abac330884a0
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-25 06:59:09 +0000

    2010-03-24  Adam Barth  <abarth@webkit.org>
            Unreviewed.  Move comments.py and multicommandtool.py to their new
            home.
    
            * Scripts/webkitpy/comments.py: Removed.
            * Scripts/webkitpy/multicommandtool.py: Removed.
            * Scripts/webkitpy/multicommandtool_unittest.py: Removed.
            * Scripts/webkitpy/tool/commands/abstractsequencedcommand.py:
            * Scripts/webkitpy/tool/commands/download.py:
            * Scripts/webkitpy/tool/commands/openbugs.py:
            * Scripts/webkitpy/tool/commands/queries.py:
            * Scripts/webkitpy/tool/commands/queues.py:
            * Scripts/webkitpy/tool/commands/upload.py:
            * Scripts/webkitpy/tool/comments.py: Copied from Scripts/webkitpy/comments.py.
            * Scripts/webkitpy/tool/main.py:
            * Scripts/webkitpy/tool/multicommandtool.py: Copied from Scripts/webkitpy/multicommandtool.py.
            * Scripts/webkitpy/tool/multicommandtool_unittest.py: Copied from Scripts/webkitpy/multicommandtool_unittest.py.
            * Scripts/webkitpy/tool/steps/closebugforlanddiff.py:
            * Scripts/webkitpy/tool/steps/closepatch.py:
            * Scripts/webkitpy/tool/steps/reopenbugafterrollout.py:
            * Scripts/webkitpy/unittests.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/commands/openbugs.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/commands/openbugs.py
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import re
 import sys
 
-from webkitpy.multicommandtool import AbstractDeclarativeCommand
+from webkitpy.tool.multicommandtool import AbstractDeclarativeCommand
 from webkitpy.webkit_logging import log
 
 
