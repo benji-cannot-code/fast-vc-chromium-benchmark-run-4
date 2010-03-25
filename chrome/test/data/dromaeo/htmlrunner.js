544f9fb5f84316b04e0fe5deedec14cd91842a9b
antonm@chromium.org <antonm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-25 12:49:49 +0000

    Update to the current Dromaeo ToT.
    BUG=37993
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42604 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/dromaeo/htmlrunner.js
+++ chrome/test/data/dromaeo/htmlrunner.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-var startTest = top.startTest || function(){};
-var test = top.test || function(name, fn){ fn(); };
-var endTest = top.endTest || function(){};
-var prep = top.prep || function(fn){ fn(); };
+var startTest = parent.startTest || function(){};
+var test = parent.test || function(name, fn){ fn(); };
+var endTest = parent.endTest || function(){};
+var prep = parent.prep || function(fn){ fn(); };
