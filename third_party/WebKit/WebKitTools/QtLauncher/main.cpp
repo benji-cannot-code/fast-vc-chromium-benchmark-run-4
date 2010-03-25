87c924eff0bf2d1feeea63a83a7cffbdaf60a120
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-25 23:09:32 +0000

    2010-03-25  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Unreviewed, build fix.
    
            [Qt] Fix QtLauncher guards.
    
            * QtLauncher/main.cpp:
            (LauncherWindow::showFPS):
            (LauncherWindow::updateFPS):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56575 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/QtLauncher/main.cpp
+++ third_party/WebKit/WebKitTools/QtLauncher/main.cpp
@@ -81,7 +81,7 @@ static QUrl gInspectorUrl;
 static bool gResizesToContents = false;
 static bool gUseTiledBackingStore = false;
 
-#ifdef Q_WS_MAEMO_5 || defined(Q_WS_S60)
+#if defined(Q_WS_MAEMO_5) || defined(Q_WS_S60)
 static bool gUseFrameFlattening = true;
 #else
 static bool gUseFrameFlattening = false;
@@ -663,7 +663,7 @@ void LauncherWindow::showFPS(bool enable)
     view->setFrameRateMeasurementEnabled(enable);
 
     if (!enable) {
-#ifdef Q_WS_MAEMO_5 && defined(Q_WS_S60)
+#if defined(Q_WS_MAEMO_5) && defined(Q_WS_S60)
         setWindowTitle("");
 #else
         statusBar()->clearMessage();
@@ -704,7 +704,7 @@ void LauncherWindow::updateFPS(int fps)
 {
     QString fpsStatusText = QString("Current FPS: %1").arg(fps);
 
-#ifdef Q_WS_MAEMO_5 && defined(Q_WS_S60)
+#if defined(Q_WS_MAEMO_5) && defined(Q_WS_S60)
     setWindowTitle(fpsStatusText);
 #else
     statusBar()->showMessage(fpsStatusText);
