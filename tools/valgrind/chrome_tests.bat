a6ff3d1b568c5c0dddb1dcb74ec1431d6b7df8dd
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-26 15:52:58 +0000

    Added tools/valgrind/chrome_tests.bat to run chrome_tests.py on Windows TBR=glider Review URL: http://codereview.chromium.org/1359004
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42762 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/valgrind/chrome_tests.bat
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+@echo off
+:: Copyright (c) 2010 The Chromium Authors. All rights reserved.
+:: Use of this source code is governed by a BSD-style license that can be
+:: found in the LICENSE file.
+
+set PYTHONPATH=%~dp0../python
+python %~dp0/chrome_tests.py %*
