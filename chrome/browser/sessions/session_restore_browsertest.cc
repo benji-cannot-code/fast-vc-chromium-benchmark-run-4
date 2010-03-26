954647fad73533d2ca02f8cf21f07b4a5c9f3521
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-26 16:52:11 +0000

    Disabling newly added test as failed on linux views. Will investigate after landing.
    BUG=none
    TEST=none
    TBR=brettw@chromium.org
    
    Review URL: http://codereview.chromium.org/1394006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42772 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_restore_browsertest.cc
+++ chrome/browser/sessions/session_restore_browsertest.cc
@@ -12,6 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef InProcessBrowserTest SessionRestoreTest;
 
+#if defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
+// http://crbug.com/39476
+#define RestoreOnNewWindowWithNoTabbedBrowsers \
+        DISABLED_RestoreOnNewWindowWithNoTabbedBrowsers
+#endif
+
 // Makes sure when session restore is triggered in the same process we don't end
 // up with an extra tab.
 IN_PROC_BROWSER_TEST_F(SessionRestoreTest,
