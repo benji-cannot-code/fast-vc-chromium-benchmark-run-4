18f4ad11656ac21dcdc9d112f7131900de433a87
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-26 00:29:09 +0000

    GTK: Provide GTK+ themed notifications.
    BUG=34514
    TEST=none
    
    Review URL: http://codereview.chromium.org/1353003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42705 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/notifications/balloon_view_gtk.h
+++ chrome/browser/gtk/notifications/balloon_view_gtk.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class BalloonCollection;
 class BalloonViewHost;
+class GtkThemeProvider;
 class MenuGtk;
 class NineBox;
 class NotificationDetails;
@@ -101,6 +102,8 @@ class BalloonViewImpl : public BalloonView,
 
   // Non-owned pointer to the balloon which owns this object.
   Balloon* balloon_;
+
+  GtkThemeProvider* theme_provider_;
 
   // The window that contains the frame of the notification.
   GtkWidget* frame_container_;
