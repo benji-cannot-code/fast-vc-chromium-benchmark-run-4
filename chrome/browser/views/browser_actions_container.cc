0ed268c5f3ad4dd527cfee015dda8ec0e7c16769
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-26 00:10:58 +0000

    Views: fix a crash where in the browser actions container.
    This crash didn't actually affect linux/views (for some reason the RunContextMenu() call seems to never return).
    
    BUG=38964
    TEST=crash an extension while the context menu for it is showing.
    
    Review URL: http://codereview.chromium.org/1237004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42703 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/browser_actions_container.cc
+++ chrome/browser/views/browser_actions_container.cc
@@ -126,7 +126,13 @@ BrowserActionButton::BrowserActionButton(Extension* extension,
                                Extension::kBrowserActionIconMaxSize));
 }
 
-BrowserActionButton::~BrowserActionButton() {
+void BrowserActionButton::Destroy() {
+  if (showing_context_menu_) {
+    context_menu_menu_->CancelMenu();
+    MessageLoop::current()->DeleteSoon(FROM_HERE, this);
+  } else {
+    delete this;
+  }
 }
 
 gfx::Insets BrowserActionButton::GetInsets() const {
@@ -236,6 +242,8 @@ bool BrowserActionButton::OnMousePressed(const views::MouseEvent& e) {
     context_menu_menu_->RunContextMenuAt(point);
 
     SetButtonNotPushed();
+    showing_context_menu_ = false;
+
     return false;
   } else if (IsPopup()) {
     return MenuButton::OnMousePressed(e);
@@ -277,6 +285,9 @@ void BrowserActionButton::SetButtonNotPushed() {
   menu_visible_ = false;
 }
 
+BrowserActionButton::~BrowserActionButton() {
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // BrowserActionView
@@ -288,6 +299,11 @@ BrowserActionView::BrowserActionView(Extension* extension,
   button_->SetDragController(panel_);
   AddChildView(button_);
   button_->UpdateState();
+}
+
+BrowserActionView::~BrowserActionView() {
+  RemoveChildView(button_);
+  button_->Destroy();
 }
 
 gfx::Canvas* BrowserActionView::GetIconWithBadge() {
