0839a261d1d9241229e4a8525f177adec75ec58c
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-26 22:08:03 +0000

    Override SimpleWebMimeRegistryImpl::supportsMediaMIMEType() for test_shell.
    Different versions of Chromium support different codecs, which results in all media layout tests failing since they are generated against ogg/vorbis/theora content.  By overriding supportsMediaMIMEType() all media layout tests will use ogg/vorbis/theora test input.
    
    BUG=25886
    TEST=test_shell on offical builder should start passing media layout tests
    
    Review URL: http://codereview.chromium.org/1356003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42837 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell.gypi
+++ webkit/tools/test_shell/test_shell.gypi
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -96,6 +96,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'test_shell_switches.h',
         'test_shell_win.cc',
         'test_shell_webkit_init.h',
+        'test_shell_webmimeregistry_impl.cc',
+        'test_shell_webmimeregistry_impl.h',
         'test_shell_webthemecontrol.h',
         'test_shell_webthemecontrol.cc',
         'test_shell_webthemeengine.h',
--- webkit/tools/test_shell/test_shell_webkit_init.h
+++ webkit/tools/test_shell/test_shell_webkit_init.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/database/vfs_backend.h"
 #include "webkit/extensions/v8/gears_extension.h"
 #include "webkit/extensions/v8/interval_extension.h"
-#include "webkit/glue/simple_webmimeregistry_impl.h"
 #include "webkit/glue/webclipboard_impl.h"
 #include "webkit/glue/webkit_glue.h"
 #include "webkit/glue/webkitclient_impl.h"
@@ -36,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/tools/test_shell/simple_database_system.h"
 #include "webkit/tools/test_shell/simple_resource_loader_bridge.h"
 #include "webkit/tools/test_shell/simple_webcookiejar_impl.h"
+#include "webkit/tools/test_shell/test_shell_webmimeregistry_impl.h"
 #include "v8/include/v8.h"
 
 #if defined(OS_WIN)
@@ -235,7 +235,7 @@ class TestShellWebKitInit : public webkit_glue::WebKitClientImpl {
   }
 
  private:
-  webkit_glue::SimpleWebMimeRegistryImpl mime_registry_;
+  TestShellWebMimeRegistryImpl mime_registry_;
   MockWebClipboardImpl mock_clipboard_;
   webkit_glue::WebClipboardImpl real_clipboard_;
   ScopedTempDir appcache_dir_;
