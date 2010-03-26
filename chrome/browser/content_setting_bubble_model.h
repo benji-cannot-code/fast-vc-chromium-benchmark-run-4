29dc6830b7c9e5fae253dd73b7819bbde876c728
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-26 09:32:20 +0000

    Re-attempt at http://codereview.chromium.org/1344002
    Adds geolocaiton support to the location bar content image model and content bubble model.
    Most of these edits were lifted out of http://codereview.chromium.org/650180
    
    TODO: add geolocation support to the views in the three UI platforms for the bubble model.
    (NOTE this change results in poorly formed bubble contents for the geolocaiton bubble, this will be fixed up in the following CLs)
    
    
    BUG=11246
    TEST=open a site that uses geolocaiton, select allow/deny & click the icon.
    
    Review URL: http://codereview.chromium.org/1369002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42739 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_setting_bubble_model.h
+++ chrome/browser/content_setting_bubble_model.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CONTENT_SETTING_BUBBLE_MODEL_H_
 #define CHROME_BROWSER_CONTENT_SETTING_BUBBLE_MODEL_H_
 
+#include <set>
 #include <string>
 #include <vector>
 
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_registrar.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 
+class GURL;
 class Profile;
 class SkBitmap;
 class TabContents;
@@ -47,11 +49,18 @@ class ContentSettingBubbleModel : public NotificationObserver {
   };
   typedef std::vector<RadioGroup> RadioGroups;
 
+  struct DomainList {
+    std::string title;
+    std::set<std::string> hosts;
+  };
+
   struct BubbleContent {
     std::string title;
     PopupItems popup_items;
     RadioGroups radio_groups;
+    std::vector<DomainList> domain_lists;
     std::string manage_link;
+    std::string clear_link;
   };
 
   const BubbleContent& bubble_content() const { return bubble_content_; }
@@ -64,6 +73,7 @@ class ContentSettingBubbleModel : public NotificationObserver {
   virtual void OnRadioClicked(int radio_group, int radio_index) {}
   virtual void OnPopupClicked(int index) {}
   virtual void OnManageLinkClicked() {}
+  virtual void OnClearLinkClicked() {}
 
  protected:
   ContentSettingBubbleModel(TabContents* tab_contents, Profile* profile,
@@ -79,8 +89,14 @@ class ContentSettingBubbleModel : public NotificationObserver {
   void add_radio_group(const RadioGroup& radio_group) {
     bubble_content_.radio_groups.push_back(radio_group);
   }
+  void add_domain_list(const DomainList& domain_list) {
+    bubble_content_.domain_lists.push_back(domain_list);
+  }
   void set_manage_link(const std::string& link) {
     bubble_content_.manage_link = link;
+  }
+  void set_clear_link(const std::string& link) {
+    bubble_content_.clear_link = link;
   }
 
  private:
