888057122e01c31fa97ae20bc5650010232f781c
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-26 13:57:16 +0000

    Don't allow drops on the omnibox for popup windows. BUG=33071 TEST=drags to omnibox in popup should be denied. drags to omnibox in normal window should be allowed and load the url. Review URL: http://codereview.chromium.org/1225007
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42753 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/autocomplete_text_field.mm
+++ chrome/browser/cocoa/autocomplete_text_field.mm
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)awakeFromNib {
   DCHECK([[self cell] isKindOfClass:[AutocompleteTextFieldCell class]]);
-  dropHandler_.reset([[URLDropTargetHandler alloc] initWithView:self]);
   currentToolTips_.reset([[NSMutableArray alloc] init]);
 }
 
@@ -305,6 +304,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
            selector:@selector(windowDidResignKey:)
                name:NSWindowDidResignKeyNotification
              object:[self window]];
+    // Only register for drops if not in a popup window. Lazily create the
+    // drop handler when the type of window is known.
+    BrowserWindowController* windowController =
+        [BrowserWindowController browserWindowControllerForView:self];
+    if ([windowController isNormalWindow])
+      dropHandler_.reset([[URLDropTargetHandler alloc] initWithView:self]);
   }
 }
 
@@ -330,8 +335,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // (URLDropTarget protocol)
 - (id<URLDropTargetController>)urlDropController {
-  BrowserWindowController* windowController = [[self window] windowController];
-  DCHECK([windowController isKindOfClass:[BrowserWindowController class]]);
+  BrowserWindowController* windowController =
+      [BrowserWindowController browserWindowControllerForView:self];
   return [windowController toolbarController];
 }
 
