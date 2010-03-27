7c7eb6df4d678bae897ae0da09a2aebee4ebaa02
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-27 05:37:12 +0000

    2010-03-26  Adam Barth  <abarth@webkit.org>
            Unreviewed.  Add a missing "_".
    
            * Scripts/webkitpy/common/net/irc/ircproxy.py:
            * Scripts/webkitpy/common/net/irc/ircproxy_unittest.py: Added.
            * Scripts/webkitpy/common/net/irc/unittests.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/common/net/irc/unittests.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/common/net/irc/unittests.py
@@ -31,4 +31,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # This module is imported by the module that imports all webkitpy unit tests.
 
-# No tests yet!  :(
+from webkitpy.common.net.irc.ircproxy_unittest import *
