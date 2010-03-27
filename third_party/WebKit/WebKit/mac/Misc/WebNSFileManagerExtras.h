c40065d28b1c31c8247cad87cd4dee3e58254dd0
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-27 19:20:44 +0000

    WebKit NSFileManager methods don't handle broken symlinks correctly. Part of <rdar://problem/7574046>.
    Reviewed by Mark Rowe.
    
    * Misc/WebNSFileManagerExtras.h: Removed unused defines and methods.
    * Misc/WebNSFileManagerExtras.m: Removed unused methods.
    (fileExists): Added. For use instead of fileExistsAtPath: for cases where we'd like
    to treat a broken symlink as a file that does indeed exist.
    (-[NSFileManager _webkit_pathWithUniqueFilenameForPath:]): Use fileExists.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56666 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Misc/WebNSFileManagerExtras.h
+++ third_party/WebKit/WebKit/mac/Misc/WebNSFileManagerExtras.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2005, 2010 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,19 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-#define WEB_UREAD     (00400)   /* Read by owner */
-#define WEB_UWRITE    (00200)   /* Write by owner */
-#define WEB_UEXEC     (00100)   /* Execute/Search by owner */
-
 @interface NSFileManager (WebNSFileManagerExtras)
-
-- (void)_webkit_backgroundRemoveFileAtPath:(NSString *)path;
-- (void)_webkit_backgroundRemoveLeftoverFiles:(NSString *)path;
-- (BOOL)_webkit_removeFileOnlyAtPath:(NSString *)path;
 - (void)_webkit_setMetadataURL:(NSString *)URLString referrer:(NSString *)referrer atPath:(NSString *)path;
 - (NSString *)_webkit_startupVolumeName;
 - (NSString *)_webkit_pathWithUniqueFilenameForPath:(NSString *)path;
-
 @end
 
 
