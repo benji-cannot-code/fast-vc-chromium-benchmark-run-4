3324790326591fd0d17efa7706eaf864b24ca81b
joepeck@webkit.org <joepeck@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-28 08:49:39 +0000

    2010-03-27  Joseph Pecoraro  <joepeck@webkit.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Edit Tag Names
            https://bugs.webkit.org/show_bug.cgi?id=36481
    
            Allow editing an Element's Tag Name by double clicking
            on the tag name in the Element's Tree Hierarchy.
    
              The usual asynchronous InspectorBackend, InspectorDOMAgent, InspectorFrontend flow.
    
            * inspector/InspectorBackend.cpp: moved DOM manipulation to InspectorDOMAgent
            (WebCore::InspectorBackend::removeNode):
            (WebCore::InspectorBackend::changeTagName):
            * inspector/InspectorBackend.h:
            * inspector/InspectorBackend.idl:
            * inspector/InspectorDOMAgent.cpp:
            (WebCore::InspectorDOMAgent::removeNode):
            (WebCore::InspectorDOMAgent::changeTagName):
            * inspector/InspectorDOMAgent.h:
            * inspector/InspectorFrontend.cpp:
            (WebCore::InspectorFrontend::didChangeTagName):
            * inspector/InspectorFrontend.h:
    
            * inspector/front-end/DOMAgent.js:
    
              Handle the UI for editing an Element's tag name.
    
            * inspector/front-end/ElementsTreeOutline.js:
            (WebInspector.ElementsTreeElement.prototype._startEditingFromEvent): allow editing from double click.
            (WebInspector.ElementsTreeElement.prototype._startEditingTagName.keyupListener): update the closing tag
            (WebInspector.ElementsTreeElement.prototype._startEditingTagName.editingComitted): remove extra listener and commit
            (WebInspector.ElementsTreeElement.prototype._startEditingTagName.editingCancelled): remove extra listener and cancel
            (WebInspector.ElementsTreeElement.prototype._startEditingTagName):
            (WebInspector.ElementsTreeElement.prototype._tagNameEditingCommitted.cancel):
            (WebInspector.ElementsTreeElement.prototype._tagNameEditingCommitted.moveToNextAttributeIfNeeded):
            (WebInspector.ElementsTreeElement.prototype._tagNameEditingCommitted.editTagNameCallback):
            (WebInspector.ElementsTreeElement.prototype._tagNameEditingCommitted):
            (WebInspector.ElementsTreeElement.prototype._distinctClosingTagElement): get the closing tag for an opening tag
            (WebInspector.ElementsTreeElement.prototype._nodeTitleInfo): wrap the tagName in span.webkit-html-tag-name
    
              Miscellaneous updates.
    
            * inspector/front-end/treeoutline.js: fixed a typo.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorBackend.h
+++ third_party/WebKit/WebCore/inspector/InspectorBackend.h
@@ -7,13 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * are met:
  *
  * 1.  Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer. 
+ *     notice, this list of conditions and the following disclaimer.
  * 2.  Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution. 
+ *     documentation and/or other materials provided with the distribution.
  * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
  *     its contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission. 
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -113,6 +113,7 @@ public:
     void getEventListenersForNode(long callId, long nodeId);
     void copyNode(long nodeId);
     void removeNode(long callId, long nodeId);
+    void changeTagName(long callId, long nodeId, const AtomicString& tagName, bool expanded);
 
     void getStyles(long callId, long nodeId, bool authOnly);
     void getAllStyles(long callId);
--- third_party/WebKit/WebCore/inspector/front-end/DOMAgent.js
+++ third_party/WebKit/WebCore/inspector/front-end/DOMAgent.js
@@ -677,6 +677,7 @@ WebInspector.didApplyDomChange = WebInspector.Callback.processCallback;
 WebInspector.didRemoveAttribute = WebInspector.Callback.processCallback;
 WebInspector.didSetTextNodeValue = WebInspector.Callback.processCallback;
 WebInspector.didRemoveNode = WebInspector.Callback.processCallback;
+WebInspector.didChangeTagName = WebInspector.Callback.processCallback;
 WebInspector.didGetEventListenersForNode = WebInspector.Callback.processCallback;
 
 WebInspector.didGetStyles = WebInspector.Callback.processCallback;
