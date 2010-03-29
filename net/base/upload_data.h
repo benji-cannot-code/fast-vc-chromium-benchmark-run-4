c85862fd4de5c91e814e6954e715f1ed82b0183c
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-29 19:58:36 +0000

    Report unreadable files as size zero when uploading. Upload zero bytes if the file size shrinks.
    BUG=30850
    TEST=uploading an unreadable file works
    
    Review URL: http://codereview.chromium.org/1250002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42981 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/upload_data.h
+++ net/base/upload_data.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/file_path.h"
+#include "base/logging.h"
 #include "base/ref_counted.h"
+#include "net/base/file_stream.h"
 #include "base/time.h"
 #include "testing/gtest/include/gtest/gtest_prod.h"
 
@@ -29,7 +31,14 @@ class UploadData : public base::RefCounted<UploadData> {
    public:
     Element() : type_(TYPE_BYTES), file_range_offset_(0),
                 file_range_length_(kuint64max),
-                override_content_length_(false) {
+                override_content_length_(false),
+                content_length_computed_(false),
+                file_stream_(NULL) {
+    }
+
+    ~Element() {
+      // In the common case |file__stream_| will be null.
+      delete file_stream_;
     }
 
     Type type() const { return type_; }
@@ -66,7 +75,14 @@ class UploadData : public base::RefCounted<UploadData> {
 
     // Returns the byte-length of the element.  For files that do not exist, 0
     // is returned.  This is done for consistency with Mozilla.
-    uint64 GetContentLength() const;
+    // Once called, this function will always return the same value.
+    uint64 GetContentLength();
+
+    // Returns a FileStream opened for reading for this element, positioned at
+    // |file_range_offset_|.  The caller gets ownership and is responsible
+    // for cleaning up the FileStream. Returns NULL if this element is not of
+    // type TYPE_FILE or if the file is not openable.
+    FileStream* NewFileStreamForReading();
 
    private:
     // Allows tests to override the result of GetContentLength.
@@ -82,7 +98,9 @@ class UploadData : public base::RefCounted<UploadData> {
     uint64 file_range_length_;
     base::Time expected_file_modification_time_;
     bool override_content_length_;
+    bool content_length_computed_;
     uint64 content_length_;
+    FileStream* file_stream_;
 
     FRIEND_TEST(UploadDataStreamTest, FileSmallerThanLength);
     FRIEND_TEST(HttpNetworkTransactionTest, UploadFileSmallerThanLength);
@@ -109,10 +127,10 @@ class UploadData : public base::RefCounted<UploadData> {
   }
 
   // Returns the total size in bytes of the data to upload.
-  uint64 GetContentLength() const;
+  uint64 GetContentLength();
 
-  const std::vector<Element>& elements() const {
-    return elements_;
+  std::vector<Element>* elements() {
+    return &elements_;
   }
 
   void set_elements(const std::vector<Element>& elements) {
