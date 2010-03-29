bd34cb5bff66cf51832307677783672566ef93e5
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-29 22:02:47 +0000

    Reland r42300: "HttpRequestHeaders refactor." Adds a fix and tests for empty header values.  The particular bug happened when the value was non-empty, but was all LWS, so it was effectively empty. BUG=22588
    Review URL: http://codereview.chromium.org/1370001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43000 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -1093,7 +1093,7 @@ TEST_F(URLRequestTestHTTP, VaryHeader) {
     TestDelegate d;
     URLRequest req(server_->TestServerPage("echoheader?foo"), &d);
     req.set_context(context);
-    req.SetExtraRequestHeaders("foo:1");
+    req.SetExtraRequestHeaders("foo: 1");
     req.Start();
     MessageLoop::current()->Run();
   }
@@ -1103,7 +1103,7 @@ TEST_F(URLRequestTestHTTP, VaryHeader) {
     TestDelegate d;
     URLRequest req(server_->TestServerPage("echoheader?foo"), &d);
     req.set_context(context);
-    req.SetExtraRequestHeaders("foo:1");
+    req.SetExtraRequestHeaders("foo: 1");
     req.Start();
     MessageLoop::current()->Run();
 
@@ -1115,7 +1115,7 @@ TEST_F(URLRequestTestHTTP, VaryHeader) {
     TestDelegate d;
     URLRequest req(server_->TestServerPage("echoheader?foo"), &d);
     req.set_context(context);
-    req.SetExtraRequestHeaders("foo:2");
+    req.SetExtraRequestHeaders("foo: 2");
     req.Start();
     MessageLoop::current()->Run();
 
