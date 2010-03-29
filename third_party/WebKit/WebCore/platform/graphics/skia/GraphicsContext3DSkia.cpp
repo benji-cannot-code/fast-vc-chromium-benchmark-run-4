fbe0c76ecd1b4c13a461bdddf79d0bb5162103e3
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-29 18:28:08 +0000

    2010-03-29  Kenneth Russell  <kbr@google.com>
            Reviewed by Darin Fisher.
    
            Eliminate use of GL_BGRA in GraphicsContext3DSkia.cpp
            https://bugs.webkit.org/show_bug.cgi?id=36737
    
            No new tests; ran WebGL demos in Chromium on Windows to verify fix.
    
            * platform/graphics/skia/GraphicsContext3DSkia.cpp:
            (WebCore::GraphicsContext3D::getImageData):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56735 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContext3DSkia.cpp
+++ third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContext3DSkia.cpp
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Image.h"
 #include "NativeImageSkia.h"
 
+#include <algorithm>
+
 namespace WebCore {
 
 bool GraphicsContext3D::getImageData(Image* image,
@@ -60,13 +62,17 @@ bool GraphicsContext3D::getImageData(Image* image,
     ASSERT(rowBytes == width * 4);
     uint8_t* pixels = reinterpret_cast<uint8_t*>(skiaImage->getPixels());
     outputVector.resize(rowBytes * height);
-    memcpy(outputVector.data(), pixels, rowBytes * height);
+    int size = rowBytes * height;
+    memcpy(outputVector.data(), pixels, size);
     *hasAlphaChannel = true;
     if (!premultiplyAlpha)
         // FIXME: must fetch the image data before the premultiplication step
         *neededAlphaOp = kAlphaDoUnmultiply;
-    // FIXME: remove this dependency on desktop OpenGL
-    *format = 0x80E1; // GL_BGRA
+    // Convert from BGRA to RGBA. FIXME: add GL_BGRA extension support
+    // to all underlying OpenGL implementations.
+    for (int i = 0; i < size; i += 4)
+        std::swap(outputVector[i], outputVector[i + 2]);
+    *format = RGBA;
     return true;
 }
 
