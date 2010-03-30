922d46c400d4a79d4143bd73ffadf97c44474960
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-30 18:15:32 +0000

    Make the location content setting tab be dependent on the --enable-geolocation switch
    BUG=39806
    TEST=run browser with and without the --enable-geolocation flag, and open the content settings page. (restart browser when Location tab was selected as current tab)
    
    Review URL: http://codereview.chromium.org/1575005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43111 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/content_settings_dialog_controller.mm
+++ chrome/browser/cocoa/content_settings_dialog_controller.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "app/l10n_util.h"
+#include "base/command_line.h"
 #include "base/mac_util.h"
 #include "chrome/browser/browser.h"
 #include "chrome/browser/browser_window.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/host_content_settings_map.h"
 #include "chrome/browser/pref_service.h"
 #include "chrome/browser/profile.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -42,6 +44,14 @@ ContentSettingsDialogController* g_instance = nil;
 const NSInteger kGeolocationEnabledIndex = 0;
 const NSInteger kGeolocationAskIndex = 1;
 const NSInteger kGeolocationDisabledIndex = 2;
+
+int ExpectedNumberOfTabs() {
+  if (!CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableGeolocation))
+    return CONTENT_SETTINGS_NUM_TYPES - 1;
+  else
+    return CONTENT_SETTINGS_NUM_TYPES;
+}
 
 }  // namespace
 
@@ -107,7 +117,7 @@ class PrefObserverBridge : public NotificationObserver {
   if (settingsType == CONTENT_SETTINGS_TYPE_DEFAULT) {
     // Remember the last visited page from local state.
     int value = g_instance->lastSelectedTab_.GetValue();
-    if (value >= 0 && value < CONTENT_SETTINGS_NUM_TYPES)
+    if (value >= 0 && value < ExpectedNumberOfTabs())
       settingsType = static_cast<ContentSettingsType>(value);
     if (settingsType == CONTENT_SETTINGS_TYPE_DEFAULT)
       settingsType = CONTENT_SETTINGS_TYPE_COOKIES;
@@ -142,6 +152,14 @@ class PrefObserverBridge : public NotificationObserver {
 - (void)awakeFromNib {
   DCHECK([self window]);
   DCHECK_EQ(self, [[self window] delegate]);
+  if (!CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableGeolocation)) {
+    NSTabViewItem* geolocation_tab =
+        [tabView_ tabViewItemAtIndex:CONTENT_SETTINGS_TYPE_GEOLOCATION];
+    DCHECK(geolocation_tab);
+    [tabView_ removeTabViewItem:geolocation_tab];
+  }
+  DCHECK_EQ(ExpectedNumberOfTabs(), [tabView_ numberOfTabViewItems]);
 }
 
 // NSWindowDelegate method.
@@ -162,9 +180,8 @@ class PrefObserverBridge : public NotificationObserver {
   DCHECK_EQ(tabView_, tabView);
   NSInteger index = [tabView indexOfTabViewItem:tabViewItem];
   DCHECK_GT(index, CONTENT_SETTINGS_TYPE_DEFAULT);
-  DCHECK_LT(index, CONTENT_SETTINGS_NUM_TYPES);
-  if (index > CONTENT_SETTINGS_TYPE_DEFAULT &&
-       index < CONTENT_SETTINGS_NUM_TYPES)
+  DCHECK_LT(index, ExpectedNumberOfTabs());
+  if (index > CONTENT_SETTINGS_TYPE_DEFAULT && index < ExpectedNumberOfTabs())
     lastSelectedTab_.SetValue(index);
 }
 
