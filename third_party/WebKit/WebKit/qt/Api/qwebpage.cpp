3af9b8a69255d1f835602ad878bff04baa68586d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-30 20:10:52 +0000

    2010-03-30  Joe Ligman  <joseph.ligman@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] QGraphicsScene mousePressEvent does not set the clickCausedFocus flag
            https://bugs.webkit.org/show_bug.cgi?id=35259
    
            The clickCausedFocus flag is not being set in method
            mousePressEvent(QGraphicsSceneMouseEvent* ev). This flag is used
            in conjunction with QStyle::RSIP_OnMouseClickAndAlreadyFocused when
            deciding to launch the software input panel.
    
            * Api/qwebpage.cpp:
            (QWebPagePrivate::mousePressEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56805 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -798,6 +798,11 @@ void QWebPagePrivate::mousePressEvent(QGraphicsSceneMouseEvent* ev)
     if (!frame->view())
         return;
 
+    RefPtr<WebCore::Node> oldNode;
+    if (page->focusController()->focusedFrame()
+        && page->focusController()->focusedFrame()->document())
+        oldNode = page->focusController()->focusedFrame()->document()->focusedNode();
+
     if (tripleClickTimer.isActive()
             && (ev->pos().toPoint() - tripleClick).manhattanLength()
                 < QApplication::startDragDistance()) {
@@ -811,6 +816,14 @@ void QWebPagePrivate::mousePressEvent(QGraphicsSceneMouseEvent* ev)
     if (mev.button() != NoButton)
         accepted = frame->eventHandler()->handleMousePressEvent(mev);
     ev->setAccepted(accepted);
+
+    RefPtr<WebCore::Node> newNode;
+    if (page->focusController()->focusedFrame()
+        && page->focusController()->focusedFrame()->document())
+        newNode = page->focusController()->focusedFrame()->document()->focusedNode();
+
+    if (newNode && oldNode != newNode)
+        clickCausedFocus = true;
 }
 
 void QWebPagePrivate::mousePressEvent(QMouseEvent *ev)
