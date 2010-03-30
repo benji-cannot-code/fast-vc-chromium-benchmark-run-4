feeff4f882b7cde9c61798523a10d88c92a32ad6
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-30 01:26:28 +0000

    Added --enable-video-logging, which will log the presentation timestamp of video frames.
    In conjunction with a log parser we can detect dropped frames and our display jitter.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1398001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43038 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -548,6 +548,7 @@ void BrowserRenderProcessHost::PropagateBrowserCommandLineToRenderer(
     switches::kShowPaintRects,
     switches::kEnableOpenMax,
     switches::kEnableVideoLayering,
+    switches::kEnableVideoLogging,
     // We propagate the Chrome Frame command line here as well in case the
     // renderer is not run in the sandbox.
     switches::kChromeFrame,
--- webkit/glue/media/video_renderer_impl.cc
+++ webkit/glue/media/video_renderer_impl.cc
@@ -3,16 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "webkit/glue/media/video_renderer_impl.h"
+
 #include "media/base/video_frame.h"
 #include "media/base/yuv_convert.h"
-#include "webkit/glue/media/video_renderer_impl.h"
 #include "webkit/glue/webmediaplayer_impl.h"
 
 namespace webkit_glue {
 
-  VideoRendererImpl::VideoRendererImpl(WebMediaPlayerImpl::Proxy* proxy)
+VideoRendererImpl::VideoRendererImpl(WebMediaPlayerImpl::Proxy* proxy,
+                                     bool pts_logging)
     : proxy_(proxy),
-      last_converted_frame_(NULL) {
+      last_converted_frame_(NULL),
+      pts_logging_(pts_logging) {
   // TODO(hclam): decide whether to do the following line in this thread or
   // in the render thread.
   proxy->SetVideoRenderer(this);
@@ -20,9 +23,11 @@ namespace webkit_glue {
 
 // static
 media::FilterFactory* VideoRendererImpl::CreateFactory(
-    WebMediaPlayerImpl::Proxy* proxy) {
-  return new media::FilterFactoryImpl1<VideoRendererImpl,
-                                       WebMediaPlayerImpl::Proxy*>(proxy);
+    WebMediaPlayerImpl::Proxy* proxy,
+    bool pts_logging) {
+  return new media::FilterFactoryImpl2<VideoRendererImpl,
+                                       WebMediaPlayerImpl::Proxy*,
+                                       bool>(proxy, pts_logging);
 }
 
 // static
@@ -74,6 +79,15 @@ void VideoRendererImpl::Paint(skia::PlatformCanvas* canvas,
       FastPaint(video_frame, canvas, dest_rect);
     } else {
       SlowPaint(video_frame, canvas, dest_rect);
+    }
+
+    // Presentation timestamp logging is primarily used to measure performance
+    // on low-end devices.  When profiled on an Intel Atom N280 @ 1.66GHz this
+    // code had a ~63 microsecond perf hit when logging to a file (not stdout),
+    // which is neglible enough for measuring playback performance.
+    if (pts_logging_) {
+      LOG(INFO) << "pts="
+                << video_frame->GetTimestamp().InMicroseconds();
     }
     video_frame = NULL;
   }
