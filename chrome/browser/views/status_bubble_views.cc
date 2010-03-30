afed1cc2ec3657787dfaf9cc4bd7368a656101be
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-30 20:51:19 +0000

    Convert Label::GetFont() to unix_hacker style.  Original patch by Thiago Farina (see http://codereview.chromium.org/1580002 ), r=me.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/1528009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43136 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/status_bubble_views.cc
+++ chrome/browser/views/status_bubble_views.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -421,7 +421,7 @@ void StatusBubbleViews::StatusView::Paint(gfx::Canvas* canvas) {
   // Draw highlight text and then the text body. In order to make sure the text
   // is aligned to the right on RTL UIs, we mirror the text bounds if the
   // locale is RTL.
-  int text_width = std::min(views::Label::GetFont().GetStringWidth(text_),
+  int text_width = std::min(views::Label::font().GetStringWidth(text_),
       width - (kShadowThickness * 2) - kTextPositionX - kTextHorizPadding);
   int text_height = height - (kShadowThickness * 2);
   gfx::Rect body_bounds(kShadowThickness + kTextPositionX,
@@ -439,7 +439,7 @@ void StatusBubbleViews::StatusView::Paint(gfx::Canvas* canvas) {
       (SkColorGetG(text_color) + SkColorGetR(toolbar_color)) / 2,
       (SkColorGetB(text_color) + SkColorGetR(toolbar_color)) / 2);
   canvas->DrawStringInt(text_,
-                        views::Label::GetFont(),
+                        views::Label::font(),
                         text_color,
                         body_bounds.x(),
                         body_bounds.y(),
@@ -554,7 +554,7 @@ void StatusBubbleViews::SetURL(const GURL& url, const std::wstring& languages) {
   popup_->GetBounds(&popup_bounds, true);
   int text_width = static_cast<int>(popup_bounds.width() -
       (kShadowThickness * 2) - kTextPositionX - kTextHorizPadding - 1);
-  url_text_ = gfx::ElideUrl(url, view_->Label::GetFont(), text_width,
+  url_text_ = gfx::ElideUrl(url, view_->Label::font(), text_width,
                             languages);
 
   // An URL is always treated as a left-to-right string. On right-to-left UIs
