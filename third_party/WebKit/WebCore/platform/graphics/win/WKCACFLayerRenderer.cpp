17cbd5182d33773feeca2b56d7687f56df7ea653
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-30 14:13:12 +0000

    Enable extra Direct3D debugging information in Debug builds
    This makes it easier to track down memory leaks and misuses of the D3D
    APIs in Debug builds.
    
    Fixes <http://webkit.org/b/36820>
    
    Reviewed by John Sullivan.
    
    * platform/graphics/win/WKCACFLayerRenderer.cpp: Define D3D_DEBUG_INFO
    in Debug builds before #including d3d9.h so that extra D3D debugging
    information will be enabled.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56785 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayerRenderer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayerRenderer.cpp
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if USE(ACCELERATED_COMPOSITING)
 
+#ifndef NDEBUG
+#define D3D_DEBUG_INFO
+#endif
+
 #include "WKCACFLayerRenderer.h"
 
 #include "WKCACFContextFlusher.h"
