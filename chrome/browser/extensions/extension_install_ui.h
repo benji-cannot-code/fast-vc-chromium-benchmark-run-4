79d4b037f6fb800711b1c7d7f1366cbb36d7563d
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-30 23:48:43 +0000

    GTK: New extension install prompt ui.
    BUG=29633
    TEST=manual
    
    Review URL: http://codereview.chromium.org/1513002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_ui.h
+++ chrome/browser/extensions/extension_install_ui.h
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_EXTENSIONS_EXTENSION_INSTALL_UI_H_
 
 #include <string>
+#include <vector>
 
 #include "base/file_path.h"
 #include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
+#include "base/string16.h"
 #include "chrome/browser/extensions/image_loading_tracker.h"
 #include "gfx/native_widget_types.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -115,13 +117,11 @@ class ExtensionInstallUI : public ImageLoadingTracker::Observer {
       Profile* profile, Delegate* delegate, Extension* extension,
       SkBitmap* icon, const string16& warning, PromptType type);
 
-#if defined(OS_WIN)
   // Implements the showing of the new install dialog. The implementations of
   // this function are platform-specific.
   static void ShowExtensionInstallUIPrompt2Impl(
       Profile* profile, Delegate* delegate, Extension* extension,
-      SkBitmap* icon, const std::vector<std::wstring>& permissions);
-#endif
+      SkBitmap* icon, const std::vector<string16>& permissions);
 
   Profile* profile_;
   MessageLoop* ui_loop_;
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -1195,6 +1195,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/gtk/edit_search_engine_dialog.cc',
         'browser/gtk/edit_search_engine_dialog.h',
         'browser/gtk/extension_install_prompt_gtk.cc',
+        'browser/gtk/extension_install_prompt2_gtk.cc',
         'browser/gtk/extension_installed_bubble_gtk.cc',
         'browser/gtk/extension_installed_bubble_gtk.h',
         'browser/gtk/extension_view_gtk.cc',
