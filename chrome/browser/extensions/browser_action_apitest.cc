85a2bdf2c931476f5c03f62fe34a62a1d9bda762
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 20:11:55 +0000

    Fix typo
    BUG=None
    TEST=None
    TBR=mpcomplete
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43250 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/browser_action_apitest.cc
+++ chrome/browser/extensions/browser_action_apitest.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_MAC)
 // http://crbug.com/40002
 #define IncognitoBasic \
-        DISABLE_IncognitoBasic
+        DISABLED_IncognitoBasic
 #endif
 
 static const int kTimeoutMs = 60 * 1000;  // 1 minute
