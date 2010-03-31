bc559ad25d2c06ed03133e5d43111eb13aedc414
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-31 14:26:20 +0000

    2010-03-31  Diego Gonzalez  <diego.gonzalez@openbossa.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Custom user agent for user agent switcher
            https://bugs.webkit.org/show_bug.cgi?id=36757
    
            * QtLauncher/main.cpp:
            (LauncherWindow::showUserAgentDialog):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/QtLauncher/main.cpp
+++ third_party/WebKit/WebKitTools/QtLauncher/main.cpp
@@ -726,6 +726,11 @@ void LauncherWindow::showUserAgentDialog()
         file.close();
     }
 
+    QSettings settings;
+    QString customUserAgent = settings.value("CustomUserAgent").toString();
+    if (!items.contains(customUserAgent) && !customUserAgent.isEmpty())
+        items << customUserAgent;
+
     QDialog* dialog = new QDialog(this);
     dialog->setWindowTitle("Change User Agent");
 
@@ -734,8 +739,12 @@ void LauncherWindow::showUserAgentDialog()
 
     QComboBox* combo = new QComboBox(dialog);
     combo->setMaximumWidth(size().width() * 0.7);
+    combo->setEditable(true);
     combo->insertItems(0, items);
     layout->addWidget(combo);
+
+    int index = combo->findText(page()->userAgentForUrl(QUrl()));
+    combo->setCurrentIndex(index);
 
     QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok
             | QDialogButtonBox::Cancel, Qt::Horizontal, dialog);
@@ -743,8 +752,11 @@ void LauncherWindow::showUserAgentDialog()
     connect(buttonBox, SIGNAL(rejected()), dialog, SLOT(reject()));
     layout->addWidget(buttonBox);
 
-    if (dialog->exec() && !combo->currentText().isEmpty())
+    if (dialog->exec() && !combo->currentText().isEmpty()) {
         page()->setUserAgent(combo->currentText());
+        if (!items.contains(combo->currentText()))
+            settings.setValue("CustomUserAgent", combo->currentText());
+    }
 
     delete dialog;
 }
