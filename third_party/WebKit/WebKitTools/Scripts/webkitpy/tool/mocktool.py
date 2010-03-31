a1a87468595188dcf9dce9a4720c124576326644
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-31 20:28:18 +0000

    2010-03-31  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            webkit-patch reads wrong bug url from unified diff context
            https://bugs.webkit.org/show_bug.cgi?id=36477
    
            Instead of trying to figure out the bug_id from the diff, we should
            just get the information from the Checkout object, which understands
            these concepts.
    
            * Scripts/webkitpy/common/checkout/api.py:
            * Scripts/webkitpy/common/checkout/commitinfo.py:
            * Scripts/webkitpy/tool/commands/download.py:
            * Scripts/webkitpy/tool/commands/upload.py:
            * Scripts/webkitpy/tool/mocktool.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/mocktool.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/mocktool.py
@@ -379,6 +379,9 @@ class MockCheckout(object):
             "reviewer": self._committer_list.committer_by_name("Darin Adler"),
         })
 
+    def bug_id_for_revision(self, svn_revision):
+        return 12345
+
     def modified_changelogs(self):
         # Ideally we'd return something more interesting here.  The problem is
         # that LandDiff will try to actually read the patch from disk!
@@ -392,6 +395,7 @@ class MockCheckout(object):
 
     def apply_reverse_diff(self, revision):
         pass
+
 
 class MockUser(object):
 
