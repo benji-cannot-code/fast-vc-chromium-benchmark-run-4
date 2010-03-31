00a3e0838ca414c76d947a47d11d915b4e75cb16
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 20:48:33 +0000

    Fixes bug in ImageLoadintTracker where it would incorrectly hit a DCHECK if the extension doesn't have an image for a particular resource.
    BUG=none
    TEST=none
    TBR=aa
    
    Review URL: http://codereview.chromium.org/1559010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43255 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/image_loading_tracker.cc
+++ chrome/browser/extensions/image_loading_tracker.cc
@@ -134,8 +134,6 @@ void ImageLoadingTracker::LoadImage(Extension* extension,
                                     const ExtensionResource& resource,
                                     const gfx::Size& max_size,
                                     CacheParam cache) {
-  DCHECK(extension->path() == resource.extension_root());
-
   // If we don't have a path we don't need to do any further work, just respond
   // back.
   int id = next_id_++;
@@ -143,6 +141,8 @@ void ImageLoadingTracker::LoadImage(Extension* extension,
     OnImageLoaded(NULL, resource, id);
     return;
   }
+
+  DCHECK(extension->path() == resource.extension_root());
 
   // See if the extension has the image already.
   if (extension->HasCachedImage(resource)) {
