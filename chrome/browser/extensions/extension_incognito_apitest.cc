05daa887629d6f4490ef23e7c173e5b09566eba7
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 21:38:45 +0000

    Fix type in test name s/DISABLE/DISABLED/
    BUG=None
    TEST=None
    TBR=dmaclach
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43262 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_incognito_apitest.cc
+++ chrome/browser/extensions/extension_incognito_apitest.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_MAC)
 // http://crbug.com/40002
 #define IncognitoPopup \
-        DISABLE_IncognitoPopup
+        DISABLED_IncognitoPopup
 #endif
 
 IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, IncognitoNoScript) {
