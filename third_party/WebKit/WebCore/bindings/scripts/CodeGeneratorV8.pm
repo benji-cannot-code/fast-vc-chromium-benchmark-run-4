39c7385f37f16c938eb13ce3a94753b29bb4e5c1
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-31 08:37:13 +0000

    2010-03-31  John Gregg  <johnnyg@google.com>
            Reviewed by Darin Fisher.
    
            [chromium] add logging of cross-frame property accesses for site isolation
            https://bugs.webkit.org/show_bug.cgi?id=35773
    
            No new tests as no new functionality.
    
            * bindings/scripts/CodeGeneratorV8.pm:
            * bindings/v8/V8Utilities.cpp:
            (WebCore::logPropertyAccess):
            * bindings/v8/V8Utilities.h:
            * loader/FrameLoaderClient.h:
            (WebCore::FrameLoaderClient::logCrossFramePropertyAccess):
    2010-03-31  John Gregg  <johnnyg@google.com>
    
            Reviewed by Darin Fisher.
    
            [chromium] add logging of cross-frame property accesses for site isolation
            https://bugs.webkit.org/show_bug.cgi?id=35773
    
            * public/WebFrameClient.h:
            (WebKit::WebFrameClient::logCrossFramePropertyAccess):
            * src/FrameLoaderClientImpl.cpp:
            (WebKit::FrameLoaderClientImpl::logCrossFramePropertyAccess):
            * src/FrameLoaderClientImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56829 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -675,6 +675,11 @@ END
         push(@implContentDecls, "    if (!V8BindingSecurity::checkNodeSecurity(V8BindingState::Only(), imp->$attrName())) return v8::Handle<v8::Value>();\n\n");
     } elsif ($attribute->signature->extendedAttributes->{"CheckFrameSecurity"}) {
         push(@implContentDecls, "    if (!V8BindingSecurity::checkNodeSecurity(V8BindingState::Only(), imp->contentDocument())) return v8::Handle<v8::Value>();\n\n");
+    } 
+
+    if ($attrExt->{"DoNotCheckDomainSecurity"} || 
+        $attrExt->{"DoNotCheckDomainSecurityOnGet"}) {
+        push(@implContentDecls, "    logPropertyAccess(name, info);\n");
     }
 
     my $useExceptions = 1 if @{$attribute->getterExceptions} and !($isPodType);
@@ -1447,6 +1452,7 @@ sub GenerateImplementation
          "#include \"V8Binding.h\"\n" .
          "#include \"V8BindingState.h\"\n" .
          "#include \"V8DOMWrapper.h\"\n" .
+         "#include \"V8Utilities.h\"\n" .
          "#include \"V8IsolatedContext.h\"\n\n" .
          "#undef LOG\n\n");
 
--- third_party/WebKit/WebCore/bindings/v8/V8Utilities.h
+++ third_party/WebKit/WebCore/bindings/v8/V8Utilities.h
@@ -57,6 +57,8 @@ namespace WebCore {
 
     ScriptExecutionContext* getScriptExecutionContext();
 
+    void logPropertyAccess(v8::Local<v8::String> name, const v8::AccessorInfo& info);
+
     class AllowAllocation {
     public:
         inline AllowAllocation()
