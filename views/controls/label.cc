e8d9597484746d124130d44431cbe16213aa8461
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 10:16:50 +0000

    Fix a bug that caused single-line labels to be line-wrapped with Views for Linux.
    Change Label::GetTextSize() to compute |w| with std::numeric_limits<int>::max() in the single line mode, just like it used to be before r42527.
    
    BUG=39799
    TEST=Confirmed labels are not line-wrapped in candidate_window with the fix.
    
    Review URL: http://codereview.chromium.org/1542005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43200 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/label.cc
+++ views/controls/label.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/controls/label.h"
 
 #include <cmath>
+#include <limits>
 
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
@@ -375,7 +376,12 @@ gfx::Rect Label::GetTextBounds() const {
 
 gfx::Size Label::GetTextSize() const {
   if (!text_size_valid_) {
-    int w = GetAvailableRect().width();
+    // For single-line strings, we supply the largest possible width, because
+    // while adding NO_ELLIPSIS to the flags works on Windows for forcing
+    // SizeStringInt() to calculate the desired width, it doesn't seem to work
+    // on Linux.
+    int w = is_multi_line_ ?
+        GetAvailableRect().width() : std::numeric_limits<int>::max();
     int h = font_.height();
     // For single-line strings, ignore the available width and calculate how
     // wide the text wants to be.
