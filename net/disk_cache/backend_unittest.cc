1fbe1acbf4a55728321a089b333bf48ad700c5ae
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 15:04:02 +0000

    Disable more DiskCacheBackendTest's as the crash on Linux/shlib TBR=rvargas,amit BUG=38562 Review URL: http://codereview.chromium.org/1546007
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43213 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -1346,7 +1346,8 @@ void DiskCacheBackendTest::BackendDisable3() {
   EXPECT_EQ(1, cache_->GetEntryCount());
 }
 
-TEST_F(DiskCacheBackendTest, DisableSuccess3) {
+// http://code.google.com/p/chromium/issues/detail?id=38562
+TEST_F(DiskCacheBackendTest, DISABLED_DisableSuccess3) {
   ASSERT_TRUE(CopyTestCache(L"bad_rankings2"));
   DisableFirstCleanup();
   SetMaxSize(20 * 1024 * 1024);
@@ -1354,7 +1355,8 @@ TEST_F(DiskCacheBackendTest, DisableSuccess3) {
   BackendDisable3();
 }
 
-TEST_F(DiskCacheBackendTest, NewEvictionDisableSuccess3) {
+// http://code.google.com/p/chromium/issues/detail?id=38562
+TEST_F(DiskCacheBackendTest, DISABLED_NewEvictionDisableSuccess3) {
   ASSERT_TRUE(CopyTestCache(L"bad_rankings2"));
   DisableFirstCleanup();
   SetMaxSize(20 * 1024 * 1024);
