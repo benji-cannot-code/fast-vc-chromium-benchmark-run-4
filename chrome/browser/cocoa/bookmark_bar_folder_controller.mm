cf90a16772ddb5b0a632045b9fa1285e709e83ad
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 22:02:49 +0000

    Make the "off the side" menu (chevron) behave like the other bookmark bar folders.  E.g. supports DragAndDrop.
    BUG=34910
    
    TEST=Make sure you can DnD items to/from the "off the side" chevron.
    Make sure context menus on those buttons work as well (e.g. delete).
    Make sure the chevron is NOT draggable.
    Make sure the chevron works properly as a "hover open" button when
      dragging something else around; e.g. it pops open if you hover over
      it.  (It's small so this may take patience).
    Drop a folder from the bar into itself.  Make sure we don't crash.
    
    xib change:
    - chevron button is now BookmarkButton, not MenuButton.
      Also has BookmarkButtonCell in it.
    - "off the side" menu removed.
    
    Review URL: http://codereview.chromium.org/1520003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43263 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bar_folder_controller.mm
+++ chrome/browser/cocoa/bookmark_bar_folder_controller.mm
@@ -221,9 +221,10 @@ const CGFloat kBookmarkBarFolderScrollAmount =
   NSPoint newWindowTopLeft = [self windowTopLeft];
   const BookmarkNode* node = [parentButton_ bookmarkNode];
   DCHECK(node);
-  int buttons = node->GetChildCount();
-  if (buttons == 0)
-    buttons = 1;  // the "empty" button
+  int startingIndex = [[parentButton_ cell] startingChildIndex];
+  DCHECK_LE(startingIndex, node->GetChildCount());
+  // Must have at least 1 button (for "empty")
+  int buttons = std::max(node->GetChildCount() - startingIndex, 1);
 
   int height = buttons * bookmarks::kBookmarkButtonHeight;
 
@@ -259,7 +260,9 @@ const CGFloat kBookmarkBarFolderScrollAmount =
     [buttons_ addObject:button];
     [mainView_ addSubview:button];
   } else {
-    for (int i = 0; i < node->GetChildCount(); i++) {
+    for (int i = startingIndex;
+         i < node->GetChildCount();
+         i++) {
       const BookmarkNode* child = node->GetChild(i);
       BookmarkButton* button = [self makeButtonForNode:child
                                                  frame:buttonsOuterFrame];
@@ -649,7 +652,9 @@ static BOOL ValueInRangeInclusive(CGFloat low, CGFloat value, CGFloat high) {
   const BookmarkNode* beforeNode = [buttonToTheTopOfDraggedButton
                                        bookmarkNode];
   DCHECK(beforeNode);
-  return beforeNode->GetParent()->IndexOfChild(beforeNode) + 1;
+  // Be careful if the number of buttons != number of nodes.
+  return ((beforeNode->GetParent()->IndexOfChild(beforeNode) + 1) -
+          [[parentButton_ cell] startingChildIndex]);
 }
 
 - (BookmarkModel*)bookmarkModel {
@@ -681,6 +686,8 @@ static BOOL ValueInRangeInclusive(CGFloat low, CGFloat value, CGFloat high) {
     // Else we're dropping somewhere in the folder, so find the right spot.
     destParent = [parentButton_ bookmarkNode];
     destIndex = [self indexForDragOfButton:sourceButton toPoint:point];
+    // Be careful if the number of buttons != number of nodes.
+    destIndex += [[parentButton_ cell] startingChildIndex];
   }
 
   if (copy)
