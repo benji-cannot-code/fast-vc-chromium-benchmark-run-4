184cd61070d514684c93dad33e8d8d0bcdc6a8e3
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-31 11:04:25 +0000

    2010-03-31  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Add stubs for moveWindowBy and setAttachedWindowHeight.
    
            https://bugs.webkit.org/show_bug.cgi?id=36842
    
            * src/WebDevToolsFrontendImpl.cpp:
            (WebKit::WebDevToolsFrontendImpl::WebDevToolsFrontendImpl):
            (WebKit::WebDevToolsFrontendImpl::jsSetAttachedWindowHeight):
            (WebKit::WebDevToolsFrontendImpl::jsMoveWindowBy):
            * src/WebDevToolsFrontendImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56836 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebDevToolsFrontendImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebDevToolsFrontendImpl.cpp
@@ -169,6 +169,12 @@ WebDevToolsFrontendImpl::WebDevToolsFrontendImpl(
     devToolsHost.addProtoFunction(
         "canAttachWindow",
         WebDevToolsFrontendImpl::jsCanAttachWindow);
+    devToolsHost.addProtoFunction(
+        "setAttachedWindowHeight",
+        WebDevToolsFrontendImpl::jsSetAttachedWindowHeight);
+    devToolsHost.addProtoFunction(
+        "moveWindowBy",
+        WebDevToolsFrontendImpl::jsMoveWindowBy);
     devToolsHost.build();
 }
 
@@ -407,6 +413,15 @@ v8::Handle<v8::Value> WebDevToolsFrontendImpl::jsShowContextMenu(const v8::Argum
 v8::Handle<v8::Value> WebDevToolsFrontendImpl::jsCanAttachWindow(const v8::Arguments& args)
 {
     return v8Boolean(true);
+}
+v8::Handle<v8::Value> WebDevToolsFrontendImpl::jsSetAttachedWindowHeight(const v8::Arguments& args)
+{
+    return v8::Undefined();
+}
+
+v8::Handle<v8::Value> WebDevToolsFrontendImpl::jsMoveWindowBy(const v8::Arguments& args)
+{
+    return v8::Undefined();
 }
 
 } // namespace WebKit
