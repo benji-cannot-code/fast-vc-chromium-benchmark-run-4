80361319daec13bdf9f7733f1bbc2d27c2319ebc
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 17:15:18 +0000

    Mark SessionRestoreTest.RestoreOnNewWindowWithNoTabbedBrowsers as flaky on non windows.
    BUG=39476
    TEST=None
    TBR=dmaclach
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43229 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_restore_browsertest.cc
+++ chrome/browser/sessions/session_restore_browsertest.cc
@@ -13,10 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef InProcessBrowserTest SessionRestoreTest;
 
-#if defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
+#if !defined(OS_WIN)
 // http://crbug.com/39476
 #define RestoreOnNewWindowWithNoTabbedBrowsers \
-        DISABLED_RestoreOnNewWindowWithNoTabbedBrowsers
+        FLAKY_RestoreOnNewWindowWithNoTabbedBrowsers
 #endif
 
 // Makes sure when session restore is triggered in the same process we don't end
