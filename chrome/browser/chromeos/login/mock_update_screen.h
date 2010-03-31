b88656a4f6c09114e45bca5f737936e67b5a94c8
glotov@google.com <glotov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 18:14:19 +0000

    Control flow test family added. BUG=none TEST=out/Debug/browser_tests --gtest_filter=WizardControllerFlowTest.ControlFlow*
    Review URL: http://codereview.chromium.org/1513005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43238 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/chromeos/login/mock_update_screen.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_LOGIN_MOCK_UPDATE_SCREEN_H_
+#define CHROME_BROWSER_CHROMEOS_LOGIN_MOCK_UPDATE_SCREEN_H_
+
+#include "chrome/browser/chromeos/login/view_screen.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+class MockUpdateScreen : public UpdateScreen {
+ public:
+  explicit MockUpdateScreen(WizardScreenDelegate* d) : UpdateScreen(d) {}
+  MOCK_METHOD0(StartUpdate, void());
+};
+
+#endif  // CHROME_BROWSER_CHROMEOS_LOGIN_MOCK_UPDATE_SCREEN_H_
