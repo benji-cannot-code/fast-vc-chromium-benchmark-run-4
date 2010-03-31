b6b963b65a668a2ca7bca06a5dd0948f6dcd4182
victorw@chromium.org <victorw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 16:36:48 +0000

    Remove run_webkit_tests dependency on third_party\python_24.
    R=dpranke,jeffbailey@google.com
    BUG=none
    TEST=build bot green
    
    Review URL: http://codereview.chromium.org/1568004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43224 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/layout_tests/run_webkit_tests.bat
+++ webkit/tools/layout_tests/run_webkit_tests.bat
@@ -1,2 +1,2 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-@%~dp0\..\..\..\third_party\python_24\python.exe %~dp0\run_webkit_tests.py %*
+@python %~dp0\run_webkit_tests.py %*
--- webkit/tools/layout_tests/run_webkit_tests.sh
+++ webkit/tools/layout_tests/run_webkit_tests.sh
@@ -8,16 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 exec_dir=$(dirname $0)
 
 if [ "$OSTYPE" = "cygwin" ]; then
-  system_root=`cygpath "$SYSTEMROOT"`
-  PATH="/usr/bin:$system_root/system32:$system_root:$system_root/system32/WBEM"
-  export PATH
-  unset PYTHONPATH
-  PYTHON_PROG="$exec_dir/../../../third_party/python_24/python.exe"
   SCRIPT=$(cygpath -wa "$exec_dir/run_webkit_tests.py")
 else
-  PYTHON_PROG=python
-  unset PYTHONPATH
   SCRIPT="$exec_dir/run_webkit_tests.py"
 fi
+
+PYTHON_PROG=python
+unset PYTHONPATH
 
 "$PYTHON_PROG" "$SCRIPT" "$@"
