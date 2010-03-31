a6bcbbf55946452dcef7f98f8c495bb21426c7ff
hayato@chromium.org <hayato@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 09:34:50 +0000

    Page Cycler Intl2 perf regression.
    Revert 43181 - AutoFill: Send the forms cached in the FormManager to the AutoFillManager on
    page load instead of duplicating this parsing in RenderView.
    
    BUG=38325
    TEST=none
    
    Review URL: http://codereview.chromium.org/1530011
    
    TBR=jhawkins@chromium.org
    Review URL: http://codereview.chromium.org/1540008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43195 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -80,11 +80,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/WebKit/chromium/public/WebDocument.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebDragData.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFileChooserParams.h"
-#include "third_party/WebKit/WebKit/chromium/public/WebFormControlElement.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFormElement.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebHistoryItem.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebImage.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebInputElement.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebNode.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebNodeList.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebPageSerializer.h"
@@ -107,7 +107,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/appcache/web_application_cache_host_impl.h"
 #include "webkit/default_plugin/default_plugin_shared.h"
 #include "webkit/glue/dom_operations.h"
-#include "webkit/glue/form_data.h"
 #include "webkit/glue/form_field.h"
 #include "webkit/glue/form_field_values.h"
 #include "webkit/glue/glue_serialize.h"
@@ -136,7 +135,6 @@ using appcache::WebApplicationCacheHostImpl;
 using base::Time;
 using base::TimeDelta;
 using webkit_glue::AltErrorPageResourceFetcher;
-using webkit_glue::FormData;
 using webkit_glue::FormField;
 using webkit_glue::FormFieldValues;
 using webkit_glue::ImageResourceFetcher;
@@ -160,11 +158,11 @@ using WebKit::WebDragOperationsMask;
 using WebKit::WebEditingAction;
 using WebKit::WebFileChooserCompletion;
 using WebKit::WebFindOptions;
-using WebKit::WebFormControlElement;
 using WebKit::WebFormElement;
 using WebKit::WebFrame;
 using WebKit::WebHistoryItem;
 using WebKit::WebImage;
+using WebKit::WebInputElement;
 using WebKit::WebMediaPlayer;
 using WebKit::WebMediaPlayerAction;
 using WebKit::WebMediaPlayerClient;
@@ -1953,12 +1951,10 @@ void RenderView::queryAutofillSuggestions(const WebNode& node,
   static int query_counter = 0;
   autofill_query_id_ = query_counter++;
   autofill_query_node_ = node;
-  const WebKit::WebFormControlElement element =
-      node.toConstElement<WebFormControlElement>();
-  FormField field;
-  FormManager::WebFormControlElementToFormField(element, &field);
+  const WebKit::WebInputElement input_element =
+      node.toConstElement<WebInputElement>();
   Send(new ViewHostMsg_QueryFormFieldAutofill(
-      routing_id_, autofill_query_id_, field));
+      routing_id_, autofill_query_id_, FormField(input_element)));
 }
 
 void RenderView::removeAutofillSuggestions(const WebString& name,
@@ -1974,8 +1970,7 @@ void RenderView::didAcceptAutoFillSuggestion(
   autofill_query_id_ = query_counter++;
 
   webkit_glue::FormData form;
-  const WebFormControlElement element =
-      node.toConstElement<WebFormControlElement>();
+  const WebInputElement element = node.toConstElement<WebInputElement>();
   if (!form_manager_.FindForm(element, &form))
     return;
 
@@ -4659,18 +4654,22 @@ void RenderView::focusAccessibilityObject(
 }
 
 void RenderView::SendForms(WebFrame* frame) {
-  std::vector<FormData> forms;
-  FormManager::RequirementsMask requirements =
-      static_cast<FormManager::RequirementsMask>(
-          FormManager::REQUIRE_AUTOCOMPLETE |
-          FormManager::REQUIRE_ELEMENTS_ENABLED);
-  form_manager_.GetFormsInFrame(frame, requirements, &forms);
-  if (!forms.empty()) {
-    // TODO(jhawkins): Remove this call once AutoFillManager uses FormData.
-    std::vector<FormFieldValues> form_field_values;
-    FormManager::FormDataToFormFieldValues(forms, &form_field_values);
-    Send(new ViewHostMsg_FormsSeen(routing_id_, form_field_values));
+  WebVector<WebFormElement> web_forms;
+  frame->forms(web_forms);
+
+  std::vector<FormFieldValues> forms;
+  for (size_t i = 0; i < web_forms.size(); ++i) {
+    const WebFormElement& web_form = web_forms[i];
+
+    if (web_form.autoComplete()) {
+      scoped_ptr<FormFieldValues> form(FormFieldValues::Create(web_form));
+      if (form.get())
+        forms.push_back(*form);
+    }
   }
+
+  if (!forms.empty())
+    Send(new ViewHostMsg_FormsSeen(routing_id_, forms));
 }
 
 void RenderView::didChangeAccessibilityObjectState(
