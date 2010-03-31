b7d7730d0fd0e21b80e36438d1a36330b978eb0e
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-03-31 19:51:36 +0000

    https://bugs.webkit.org/show_bug.cgi?id=36897         <rdar://problem/7804018> REGRESSION (r56429): Flash ads are clipped when main page is scrolled (boxofficemojo.com)
            Addressing additional review comments.
    
            * rendering/RenderWidget.cpp: Removed an unneeded include.
            * rendering/RenderWidget.h: Reworded the new comment.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56859 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderWidget.cpp
+++ third_party/WebKit/WebCore/rendering/RenderWidget.cpp
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AXObjectCache.h"
 #include "AnimationController.h"
-#include "CString.h"
 #include "GraphicsContext.h"
 #include "HitTestResult.h"
 #include "RenderView.h"
