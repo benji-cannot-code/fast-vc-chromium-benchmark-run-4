89150dca3b0de1fd0a3b277265d42b4e342aa9e0
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-31 19:16:45 +0000

    Don't apply toolstrip theming stylesheet to popups.
    BUG=33284
    TEST=Install a theme that uses white fonts and make
    sure popup text for extension HCalendar is visible.
    
    Tiesto theme:
    https://tools.google.com/chrome/intl/en/themes/theme_at_djtiesto.html
    
    Review URL: http://codereview.chromium.org/1517008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43247 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/extensions/extension_shelf.cc
+++ chrome/browser/views/extensions/extension_shelf.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -798,7 +798,7 @@ void ExtensionShelf::ThemeChanged() {
   // Refresh the CSS to update toolstrip text colors from theme.
   int count = model_->count();
   for (int i = 0; i < count; ++i)
-    ToolstripAtIndex(i)->view()->host()->InsertThemeCSS();
+    ToolstripAtIndex(i)->view()->host()->InsertThemedToolstripCSS();
 
   Layout();
 }
