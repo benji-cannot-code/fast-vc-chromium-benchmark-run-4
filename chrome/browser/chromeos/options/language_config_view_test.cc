d9b4c67220512d9692bffc2cdd1be5a6c2e3782f
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-01 07:57:10 +0000

    Fix a bug where we were showing "t" in language config dialog.
    Instead, we'll be showing "Others"
    
    BUG=crosbug.com/2167
    
    Review URL: http://codereview.chromium.org/1608002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43325 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/chromeos/options/language_config_view_test.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/options/language_config_view.h"
+#include <string>
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+
+TEST(LanguageConfigViewTest, MaybeRewriteLanguageName) {
+  EXPECT_EQ(L"English",
+            LanguageConfigView::MaybeRewriteLanguageName(L"English"));
+  EXPECT_EQ(L"Others",
+            LanguageConfigView::MaybeRewriteLanguageName(L"t"));
+}
+
+}  // namespace chromeos
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -590,6 +590,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/chromeos/external_metrics_unittest.cc',
         'browser/chromeos/gview_request_interceptor_unittest.cc',
         'browser/chromeos/login/google_authenticator_unittest.cc',
+        'browser/chromeos/options/language_config_view_test.cc',
         'browser/chromeos/pipe_reader_unittest.cc',
         'browser/chromeos/status/language_menu_l10n_util_unittest.cc',
         'browser/chromeos/version_loader_unittest.cc',
