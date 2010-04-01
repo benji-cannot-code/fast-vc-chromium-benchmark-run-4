d2a288a9c7ba9cadfd1ca40a544c48ce3c656871
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-01 23:50:16 +0000

    [Mac] Polish the keyword/search engine editor based on feedback from the UI team.
    XIB changes:
    * Make the button bar 35px tall.
    * Change the buttons to have Rounded Textured style.
    
    BUG=none
    TEST=Chromium --> Preferences --> Basic --> [Manage]. Bottom button bar should have a textured appearance.
    
    Review URL: http://codereview.chromium.org/1519014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43411 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/keyword_editor_cocoa_controller.mm
+++ chrome/browser/cocoa/keyword_editor_cocoa_controller.mm
@@ -21,6 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/skia_utils_mac.h"
 #include "third_party/GTM/AppKit/GTMUILocalizerAndLayoutTweaker.h"
 
+namespace {
+
+const CGFloat kButtonBarHeight = 35.0;
+
+}  // namespace
+
 @interface KeywordEditorCocoaController (Private)
 - (void)adjustEditingButtons;
 - (void)editKeyword:(id)sender;
@@ -173,6 +179,11 @@ typedef std::map<Profile*,KeywordEditorCocoaController*> ProfileControllerMap;
   NSSize size = [makeDefaultButton_ frame].size;
   size.height = NSHeight([addButton_ frame]);
   [makeDefaultButton_ setFrameSize:size];
+
+  [[self window] setAutorecalculatesContentBorderThickness:NO
+                                                   forEdge:NSMinYEdge];
+  [[self window] setContentBorderThickness:kButtonBarHeight
+                                   forEdge:NSMinYEdge];
 
   [self adjustEditingButtons];
   [tableView_ setDoubleAction:@selector(editKeyword:)];
