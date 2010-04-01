ff5687950e9200970d73243aa1a381b189be5644
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-01 10:48:18 +0000

    Flip the flag to enable geolocation by default.
    BUG=39808
    TEST=run browser with & withou --disable-geolocaiton.
    
    Review URL: http://codereview.chromium.org/1534008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43337 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_geolocation_apitest.cc
+++ chrome/browser/extensions/extension_geolocation_apitest.cc
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GeolocationApiTest : public ExtensionApiTest {
 public:
-  void SetUpCommandLine(CommandLine* command_line) {
-    ExtensionApiTest::SetUpCommandLine(command_line);
-    command_line->AppendSwitch(switches::kEnableGeolocation);
+  // InProcessBrowserTest
+  void SetUpInProcessBrowserTestFixture() {
+    ExtensionApiTest::SetUpInProcessBrowserTestFixture();
     GeolocationArbitrator::SetProviderFactoryForTest(
         &NewAutoSuccessMockLocationProvider);
   }
