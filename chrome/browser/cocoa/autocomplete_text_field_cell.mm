a9463e1c68e4cb3a5cf4084a122916b7a6cff826
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-01 17:22:25 +0000

    [Mac] Line up omnibox popup under field.
    Also line up the icons.  Spacing can be adjusted later.
    
    BUG=37865
    TEST=Popup edges line up under field edges.
    TEST=Popup icons and text should line up under field icon and text.
    
    Review URL: http://codereview.chromium.org/1608001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43357 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/autocomplete_text_field_cell.mm
+++ chrome/browser/cocoa/autocomplete_text_field_cell.mm
@@ -46,7 +46,9 @@ const NSInteger kIconLabelYOffset = 7;
 // decorations need to be trimmed.
 const CGFloat kEditorHorizontalInset = 3.0;
 
-const CGFloat kLocationIconXOffset = 3.0;
+// Cause the location icon to line up above the icons in the popup.
+const CGFloat kLocationIconXOffset = 9.0;
+const CGFloat kLocationIconXPad = 5.0;
 
 // Conveniences to centralize width+offset calculations.
 CGFloat WidthForHint(NSAttributedString* hintString) {
@@ -289,8 +291,10 @@ void DrawImageInRect(NSImage* image, NSView* view, const NSRect& rect) {
   // code could be made simpler.
   if (!keywordString_ && locationIconView_ && locationIconView_->IsVisible()) {
     const NSSize imageSize = locationIconView_->GetImageSize();
-    textFrame.origin.x += imageSize.width;
-    textFrame.size.width -= imageSize.width;
+    const CGFloat locationIconWidth =
+        kLocationIconXOffset + kLocationIconXPad + imageSize.width;
+    textFrame.origin.x += locationIconWidth;
+    textFrame.size.width -= locationIconWidth;
   }
 
   if (hintString_) {
