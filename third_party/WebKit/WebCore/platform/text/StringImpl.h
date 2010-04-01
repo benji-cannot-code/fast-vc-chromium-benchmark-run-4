f05e0f7077405760deebb7b275e55f22d61d0c5d
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-01 00:12:39 +0000

    Remove dependency on TextBreakIterator from StringImpl.cpp, and from member methods of String.
    Reviewed by Oliver Hunt.
    
    Make 'numGraphemeClusters' & 'numCharactersInGraphemeClusters'
    global function defined in PlatformString.h, rather than
    member methods on String (these can be implemented purely
    using the characters() and length() public interface),
    and make 'makeCapitalized' a static function in RenderText.cpp.
    
    * dom/InputElement.cpp:
    (WebCore::InputElement::sanitizeUserInputValue):
    (WebCore::InputElement::handleBeforeTextInsertedEvent):
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::tooLong):
    * html/HTMLTextAreaElement.cpp:
    (WebCore::HTMLTextAreaElement::handleBeforeTextInsertedEvent):
    (WebCore::HTMLTextAreaElement::sanitizeUserInputValue):
    (WebCore::HTMLTextAreaElement::tooLong):
    * platform/text/PlatformString.h:
    (WebCore::String::makeSecure):
    * platform/text/String.cpp:
    (WebCore::numGraphemeClusters):
    (WebCore::numCharactersInGraphemeClusters):
    * platform/text/StringImpl.cpp:
    * platform/text/StringImpl.h:
    * rendering/RenderText.cpp:
    (WebCore::makeCapitalized):
    (WebCore::RenderText::setTextInternal):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56878 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/StringImpl.h
+++ third_party/WebKit/WebCore/platform/text/StringImpl.h
@@ -212,7 +212,6 @@ public:
     PassRefPtr<StringImpl> lower();
     PassRefPtr<StringImpl> upper();
     PassRefPtr<StringImpl> secure(UChar aChar);
-    PassRefPtr<StringImpl> capitalize(UChar previousCharacter);
     PassRefPtr<StringImpl> foldCase();
 
     PassRefPtr<StringImpl> stripWhiteSpace();
