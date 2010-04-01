15f633890368c0373ed82fe1750be73bf805a4e5
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-01 09:41:51 +0000

    Added the link to heapchecker howto.
    TBR=timurrrr
    
    Review URL: http://codereview.chromium.org/1527006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43330 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/heapcheck/heapcheck_test.py
+++ tools/heapcheck/heapcheck_test.py
@@ -180,6 +180,9 @@ class HeapcheckWrapper(object):
     minutes = seconds / 60
     seconds %= 60
     logging.info('elapsed time: %02d:%02d:%02d', hours, minutes, seconds)
+    logging.info('For more information on the Heapcheck bot see '
+                 'http://dev.chromium.org/developers/how-tos/'
+                 'using-the-heap-leak-checker')
     return retcode
 
 
