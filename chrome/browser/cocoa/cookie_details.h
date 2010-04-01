889f990d98d51d3e252d9da970a6f53f5dc03581
danno@chromium.org <danno@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-01 10:57:57 +0000

    [Mac] add additional fields to cookie prompt for web databases
    xib changes: added two new lines to the web database cookie prompt subview of the cookie details NSView, one for database description and one for expected database size. Wired bindings to the appropriate methods in the cookie details object in the same style as the existing fields.
    
    Web database cookie prompt now looks like this: http://img208.imageshack.us/img208/9703/picture6j.png
    
    BUG=36435
    TEST=augmented unit tests and manual tests
    
    Review URL: http://codereview.chromium.org/1582003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43338 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/cookie_details.h
+++ chrome/browser/cocoa/cookie_details.h
@@ -84,7 +84,8 @@ enum CocoaCookieDetailsType {
   scoped_nsobject<NSString> name_;
 
   // Only set for type kCocoaCookieDetailsTypeTreeLocalStorage,
-  // kCocoaCookieDetailsTypeTreeDatabase and
+  // kCocoaCookieDetailsTypeTreeDatabase,
+  // kCocoaCookieDetailsTypePromptDatabase and
   // kCocoaCookieDetailsTypeTreeAppCache nodes.
   scoped_nsobject<NSString> fileSize_;
 
@@ -100,7 +101,8 @@ enum CocoaCookieDetailsType {
   // kCocoaCookieDetailsTypePromptLocalStorage nodes.
   scoped_nsobject<NSString> domain_;
 
-  // Used only for type kCocoaCookieTreeNodeTypeDatabaseStorage.
+  // Used only for type kCocoaCookieTreeNodeTypeDatabaseStorage and
+  // kCocoaCookieDetailsTypePromptDatabase nodes.
   scoped_nsobject<NSString> databaseDescription_;
 
   // Used only for type kCocoaCookieDetailsTypePromptLocalStorage.
@@ -166,7 +168,9 @@ enum CocoaCookieDetailsType {
 
 // Used for database details in the cookie prompt dialog.
 - (id)initWithDatabase:(const std::string&)domain
-                  name:(const string16&)name;
+          databaseName:(const string16&)databaseName
+   databaseDescription:(const string16&)databaseDescription
+              fileSize:(unsigned long)fileSize;
 
 // -initWithAppCacheInfo: creates a cookie details with the manifest URL plus
 // all of this additional information that is available after an appcache is
--- chrome/browser/cocoa/cookie_details.mm
+++ chrome/browser/cocoa/cookie_details.mm
@@ -197,12 +197,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (id)initWithDatabase:(const std::string&)domain
-                  name:(const string16&)name {
+          databaseName:(const string16&)databaseName
+   databaseDescription:(const string16&)databaseDescription
+              fileSize:(unsigned long)fileSize {
   if ((self = [super init])) {
     type_ = kCocoaCookieDetailsTypePromptDatabase;
     canEditExpiration_ = NO;
-    name_.reset([base::SysUTF16ToNSString(name) retain]);
+    name_.reset([base::SysUTF16ToNSString(databaseName) retain]);
     domain_.reset([base::SysUTF8ToNSString(domain) retain]);
+    databaseDescription_.reset(
+        [base::SysUTF16ToNSString(databaseDescription) retain]);
+    fileSize_.reset([base::SysWideToNSString(FormatBytes(fileSize,
+        GetByteDisplayUnits(fileSize), true)) retain]);
   }
   return self;
 }
@@ -272,7 +278,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   } else if (type == CookiePromptModalDialog::DIALOG_TYPE_DATABASE) {
     details = [[CocoaCookieDetails alloc]
         initWithDatabase:dialog->origin().host()
-                    name:dialog->database_name()];
+            databaseName:dialog->database_name()
+     databaseDescription:dialog->display_name()
+                fileSize:dialog->estimated_size()];
   } else if (type == CookiePromptModalDialog::DIALOG_TYPE_APPCACHE) {
     details = [[CocoaCookieDetails alloc]
         initWithAppCacheManifestURL:dialog->appcache_manifest_url().spec()];
