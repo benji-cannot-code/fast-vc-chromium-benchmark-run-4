5f90a55c4042ef3d30d144e4579f443059656fca
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-01 13:42:27 +0000

    Make "Reset to Defaults" reset geolocation permissions too
    BUG=38626
    TEST=Allow or block a site using geolocation, select Reset To Defaults, reload page. It should re-prompt.
    
    Review URL: http://codereview.chromium.org/1530014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43347 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/options_util.cc
+++ chrome/browser/options_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/download/download_manager.h"
+#include "chrome/browser/geolocation/geolocation_content_settings_map.h"
 #include "chrome/browser/host_content_settings_map.h"
 #include "chrome/browser/host_zoom_map.h"
 #include "chrome/browser/metrics/metrics_service.h"
@@ -69,6 +70,7 @@ void OptionsUtil::ResetToDefaults(Profile* profile) {
   };
   profile->GetDownloadManager()->ResetAutoOpenFiles();
   profile->GetHostContentSettingsMap()->ResetToDefaults();
+  profile->GetGeolocationContentSettingsMap()->ResetToDefault();
   profile->GetHostZoomMap()->ResetToDefaults();
   for (size_t i = 0; i < arraysize(kUserPrefs); ++i)
     prefs->ClearPref(kUserPrefs[i]);
