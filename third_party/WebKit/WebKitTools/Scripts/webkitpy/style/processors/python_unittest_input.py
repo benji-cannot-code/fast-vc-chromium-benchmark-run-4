091495904a170ce3bc7049fd6d405ee6254f1fd9
cjerdonek@webkit.org <cjerdonek@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-01 06:35:50 +0000

    Added Python style checking to check-webkit-style using the third-party pep8 module (via autoinstall).
    Reviewed by Dave Levin.
    
    https://bugs.webkit.org/show_bug.cgi?id=33639
    
    * Scripts/webkitpy/style/checker.py:
      - Added PYTHON to FileType.
      - Updated ProcessorDispatcher to return a PythonProcessor
        for *.py files.
    
    * Scripts/webkitpy/style/checker_unittest.py:
      - Updated the ProcessorDispatcher unit tests for *.py files.
    
    * Scripts/webkitpy/style/processors/python.py: Added.
      - Added PythonProcessor class.
    
    * Scripts/webkitpy/style/processors/python_unittest.py: Added.
      - Added PythonProcessor unit tests.
    
    * Scripts/webkitpy/style/processors/python_unittest_input.py: Added.
      - Added a sample Python file to test the PythonProcessor.process()
        code path (since pep8 accepts a file path).
    
    * Scripts/webkitpy/style/unittests.py:
      - Updated the style unit test file to import python_unittest.py.
    
    * Scripts/webkitpy/style_references.py:
      - Adjusted style references to import pep8.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56899 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/style/processors/python_unittest_input.py
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# This file is sample input for python_unittest.py and includes a single
+# error which is an extra space at the end of this line. 
