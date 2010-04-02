85402f83e89b1adb37cf44a665043ec9f3e7ef97
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-02 21:01:35 +0000

    Implement HMAC-SHA-256.
    R=albertb
    BUG=none
    TEST=added new HMAC unit tests
    Review URL: http://codereview.chromium.org/1513012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43522 0039d316-1c4b-4281-b951-d872f2087c98

--- base/hmac_mac.cc
+++ base/hmac_mac.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,8 +17,8 @@ struct HMACPlatformData {
 
 HMAC::HMAC(HashAlgorithm hash_alg)
     : hash_alg_(hash_alg), plat_(new HMACPlatformData()) {
-  // Only SHA-1 digest is supported now.
-  DCHECK(hash_alg_ == SHA1);
+  // Only SHA-1 and SHA-256 hash algorithms are supported now.
+  DCHECK(hash_alg_ == SHA1 || hash_alg_ == SHA256);
 }
 
 bool HMAC::Init(const unsigned char *key, int key_length) {
@@ -49,6 +49,10 @@ bool HMAC::Sign(const std::string& data,
     case SHA1:
       algorithm = kCCHmacAlgSHA1;
       algorithm_digest_length = CC_SHA1_DIGEST_LENGTH;
+      break;
+    case SHA256:
+      algorithm = kCCHmacAlgSHA256;
+      algorithm_digest_length = CC_SHA256_DIGEST_LENGTH;
       break;
     default:
       NOTREACHED();
