a0dface60c9e44066c110d68c7806edf668e4225
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-02 00:26:12 +0000

    Revert 43413 - Add a notification when the list of startup URLs change.
    BUG=38557
    TEST=Run two instances of chromes with difference profiles that are syncing.  Change the startup pages.  Check that the change propagates.
    
    Review URL: http://codereview.chromium.org/1562008
    
    TBR=zork@chromium.org
    Review URL: http://codereview.chromium.org/1539014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43416 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/session_startup_pref.cc
+++ chrome/browser/session_startup_pref.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/defaults.h"
 #include "chrome/browser/pref_service.h"
 #include "chrome/browser/profile.h"
-#include "chrome/common/notification_service.h"
 #include "chrome/common/pref_names.h"
 
 namespace {
@@ -73,20 +72,6 @@ void SessionStartupPref::SetStartupPref(PrefService* prefs,
   for (size_t i = 0; i < pref.urls.size(); ++i) {
     url_pref_list->Set(static_cast<int>(i),
                        new StringValue(UTF8ToWide(pref.urls[i].spec())));
-  }
-
-  std::wstring path_str(prefs::kURLsToRestoreOnStartup);
-  PrefService::PrefObserverMap::iterator observer_iterator =
-    prefs->pref_observers_.find(path_str);
-  if (observer_iterator != prefs->pref_observers_.end()) {
-    PrefService::NotificationObserverList::Iterator it(
-        *(observer_iterator->second));
-    NotificationObserver* observer;
-    while ((observer = it.GetNext()) != NULL) {
-      observer->Observe(NotificationType::PREF_CHANGED,
-                        Source<PrefService>(prefs),
-                        Details<std::wstring>(&path_str));
-    }
   }
 }
 
