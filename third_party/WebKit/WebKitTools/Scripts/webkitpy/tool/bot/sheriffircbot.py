645c51d87bb66009878904816079ed2c33356145
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-02 07:14:58 +0000

    2010-04-02  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Record the IRC nick of folks who request rollouts
            https://bugs.webkit.org/show_bug.cgi?id=36999
    
            * Scripts/webkitpy/common/net/irc/ircbot.py:
            * Scripts/webkitpy/tool/bot/irc_command.py:
            * Scripts/webkitpy/tool/bot/sheriffircbot.py:
            * Scripts/webkitpy/tool/bot/sheriffircbot_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@56977 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/bot/sheriffircbot.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/bot/sheriffircbot.py
@@ -41,8 +41,8 @@ class _IRCThreadTearoff(IRCBotDelegate):
 
     # IRCBotDelegate methods
 
-    def irc_message_received(self, message):
-        self._message_queue.post(message)
+    def irc_message_received(self, nick, message):
+        self._message_queue.post([nick, message])
         self._wakeup_event.set()
 
     def irc_nickname(self):
@@ -72,15 +72,17 @@ class SheriffIRCBot(object):
                                  self._tool.wakeup_event)
 
     def process_message(self, message):
-        tokenized_message = message.strip().split(" ")
-        if not tokenized_message:
+        (nick, request) = message
+        tokenized_request = request.strip().split(" ")
+        if not tokenized_request:
             return
-        command = self.commands.get(tokenized_message[0])
+        command = self.commands.get(tokenized_request[0])
         if not command:
-            self._tool.irc().post(
-                "Available commands: %s" % ", ".join(self.commands.keys()))
+            self._tool.irc().post("%s: Available commands: %s" % (
+                                  nick, ", ".join(self.commands.keys())))
             return
-        response = command().execute(tokenized_message[1:],
+        response = command().execute(nick,
+                                     tokenized_request[1:],
                                      self._tool,
                                      self._sheriff)
         if response:
