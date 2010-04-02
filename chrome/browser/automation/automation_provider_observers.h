a611d369b1e60382d1addf0e13951a88ece5fe8f
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-02 02:24:04 +0000

    Add generic "json dict" entry point for pyauto commands.  Will prevent the need to modify the automation proxy anymore.  New pyauto commands will only need to edit pyauto.py (to add a new SendJSONCommand() call) and browser_proxy.cc (to implement the other side).  Contrast with the normal editing of ~8 files.
    Also added WaitForAllDownloadsToComplete using new JSON path.
    
    BUG=http://crbug.com/39274
    
    
    Review URL: http://codereview.chromium.org/1547012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43436 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider_observers.h
+++ chrome/browser/automation/automation_provider_observers.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "chrome/browser/bookmarks/bookmark_model_observer.h"
+#include "chrome/browser/download/download_manager.h"
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_registrar.h"
 #include "chrome/common/notification_type.h"
@@ -451,6 +452,52 @@ class AutomationProviderBookmarkModelObserver : BookmarkModelObserver {
   BookmarkModel* model_;
 
   DISALLOW_COPY_AND_ASSIGN(AutomationProviderBookmarkModelObserver);
+};
+
+// When asked for pending downloads, the DownloadManager places
+// results in a DownloadManager::Observer.
+class AutomationProviderDownloadManagerObserver :
+    public DownloadManager::Observer {
+ public:
+  AutomationProviderDownloadManagerObserver() : DownloadManager::Observer()  {}
+  virtual ~AutomationProviderDownloadManagerObserver() {}
+  virtual void ModelChanged() {}
+  virtual void SetDownloads(std::vector<DownloadItem*>& downloads) {
+    downloads_ = downloads;
+  }
+  std::vector<DownloadItem*> Downloads() {
+    return downloads_;
+  }
+ private:
+  std::vector<DownloadItem*> downloads_;
+
+  DISALLOW_COPY_AND_ASSIGN(AutomationProviderDownloadManagerObserver);
+};
+
+
+// Allows the automation provider to wait for all downloads to finish.
+class AutomationProviderDownloadItemObserver : public DownloadItem::Observer {
+ public:
+  AutomationProviderDownloadItemObserver(
+      AutomationProvider* provider,
+      IPC::Message* reply_message,
+      int downloads) {
+    provider_ = provider;
+    reply_message_ = reply_message;
+    downloads_ = downloads;
+  }
+  virtual ~AutomationProviderDownloadItemObserver() {}
+
+  virtual void OnDownloadUpdated(DownloadItem* download) { }
+  virtual void OnDownloadFileCompleted(DownloadItem* download);
+  virtual void OnDownloadOpened(DownloadItem* download) { }
+
+ private:
+  AutomationProvider* provider_;
+  IPC::Message* reply_message_;
+  int downloads_;
+
+  DISALLOW_COPY_AND_ASSIGN(AutomationProviderDownloadItemObserver);
 };
 
 #endif  // CHROME_BROWSER_AUTOMATION_AUTOMATION_PROVIDER_OBSERVERS_H_
