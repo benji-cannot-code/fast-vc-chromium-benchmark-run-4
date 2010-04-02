7ecc36b75327954ce8208d36d4569365bfd0c5f5
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-02 18:38:53 +0000

    Make sure that we go to existing open bookmark manager when pressing Shift+Ctrl+B
    BUG=37354
    TEST=ui_test.exe --gtest_filter=BookmarksUITest.*
    
    Review URL: http://codereview.chromium.org/1519015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43503 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/browser_window.h"
+#include "chrome/browser/browser_url_handler.h"
 #include "chrome/browser/character_encoding.h"
 #include "chrome/browser/debugger/devtools_manager.h"
 #include "chrome/browser/debugger/devtools_window.h"
@@ -835,10 +836,19 @@ bool Browser::NavigateToIndexWithDisposition(int index,
 }
 
 void Browser::ShowSingletonTab(const GURL& url) {
+  // In case the URL was rewritten by the BrowserURLHandler we need to ensure
+  // that we do not open another URL that will get redirected to the rewritten
+  // URL.
+  GURL rewritten_url(url);
+  bool reverse_on_redirect = false;
+  BrowserURLHandler::RewriteURLIfNecessary(&rewritten_url, profile_,
+                                           &reverse_on_redirect);
+
   // See if we already have a tab with the given URL and select it if so.
   for (int i = 0; i < tabstrip_model_.count(); i++) {
     TabContents* tc = tabstrip_model_.GetTabContentsAt(i);
-    if (CompareURLsIgnoreRef(tc->GetURL(), url)) {
+    if (CompareURLsIgnoreRef(tc->GetURL(), url) ||
+        CompareURLsIgnoreRef(tc->GetURL(), rewritten_url)) {
       tabstrip_model_.SelectTabContentsAt(i, false);
       return;
     }
