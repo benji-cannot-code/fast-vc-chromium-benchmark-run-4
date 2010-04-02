0f704dd083391c8cdb65a03608473d83a11d9f0e
dhg@chromium.org <dhg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-02 22:47:45 +0000

    CHanging the mount library to mount the drives explicitly. Adding the notion of parent devices as well. Review URL: http://codereview.chromium.org/1608004
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43541 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/mount_library.cc
+++ chrome/browser/chromeos/cros/mount_library.cc
@@ -28,12 +28,18 @@ void MountLibraryImpl::RemoveObserver(Observer* observer) {
   observers_.RemoveObserver(observer);
 }
 
+bool MountLibraryImpl::MountPath(const char* device_path) {
+  return MountDevicePath(device_path);
+}
+
 void MountLibraryImpl::ParseDisks(const MountStatus& status) {
   disks_.clear();
   for (int i = 0; i < status.size; i++) {
     std::string path;
     std::string mountpath;
     std::string systempath;
+    bool parent;
+    bool hasmedia;
     if (status.disks[i].path != NULL) {
       path = status.disks[i].path;
     }
@@ -43,7 +49,13 @@ void MountLibraryImpl::ParseDisks(const MountStatus& status) {
     if (status.disks[i].systempath != NULL) {
       systempath = status.disks[i].systempath;
     }
-    disks_.push_back(Disk(path, mountpath, systempath));
+    parent = status.disks[i].isparent;
+    hasmedia = status.disks[i].hasmedia;
+    disks_.push_back(Disk(path,
+                          mountpath,
+                          systempath,
+                          parent,
+                          hasmedia));
   }
 }
 
@@ -63,9 +75,9 @@ MountLibraryImpl::~MountLibraryImpl() {
 
 // static
 void MountLibraryImpl::MountStatusChangedHandler(void* object,
-                                             const MountStatus& status,
-                                             MountEventType evt,
-                                             const  char* path) {
+                                                 const MountStatus& status,
+                                                 MountEventType evt,
+                                                 const  char* path) {
   MountLibraryImpl* mount = static_cast<MountLibraryImpl*>(object);
   std::string devicepath = path;
   mount->ParseDisks(status);
