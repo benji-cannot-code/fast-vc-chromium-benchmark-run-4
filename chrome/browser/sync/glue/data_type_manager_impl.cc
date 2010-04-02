b91eebc2d599b5bb4f94675ffc0592cdaf2431fd
skrul@chromium.org <skrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-02 16:04:38 +0000

    Update routing info and nudge syncer in ConfigureDataTypes(). Here is an incomplete change that will reconfigure the routing info when the DTM calls ConfigureDataTypes.  Some open questions:
    - How will the SBH actually nudge the syncer?  I can add a SyncManager::Nudge method if that makes sense.
    
    - Looking at the nudge code, it looks like it is possible for nudges to get dropped on the floor.  This could be really bad since sync can't start until the syncer thread runs and downloads updates for new data types.  Is there a better way to make sure the syncer thread runs at this point?
    
    Also, I changed the callback method used for the SBH to notify the DTM that downloads are ready by switching the raw CancelableTask instance with a RunnableMethod.  The previous version has the DTM touching the CancelableTask pointer after it was handed over to the SBH which was bad.  This should be better.
    
    Review URL: http://codereview.chromium.org/1484001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43487 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/data_type_manager_impl.cc
+++ chrome/browser/sync/glue/data_type_manager_impl.cc
@@ -54,7 +54,7 @@ DataTypeManagerImpl::DataTypeManagerImpl(
       controllers_(controllers),
       state_(DataTypeManager::STOPPED),
       current_dtc_(NULL),
-      download_ready_task_(NULL) {
+      ALLOW_THIS_IN_INITIALIZER_LIST(method_factory_(this)) {
   DCHECK(backend_);
   DCHECK_GT(arraysize(kStartOrder), 0U);
   // Ensure all data type controllers are stopped.
@@ -69,8 +69,6 @@ DataTypeManagerImpl::DataTypeManagerImpl(
 }
 
 DataTypeManagerImpl::~DataTypeManagerImpl() {
-  if (download_ready_task_)
-    download_ready_task_->Cancel();
 }
 
 void DataTypeManagerImpl::Configure(const TypeSet& desired_types) {
@@ -159,13 +157,13 @@ void DataTypeManagerImpl::Restart() {
   // Tell the backend about the new set of data types we wish to sync.
   // The task will be invoked when updates are downloaded.
   state_ = DOWNLOAD_PENDING;
-  download_ready_task_ = new DownloadReadyTask(this);
-  backend_->ConfigureDataTypes(last_requested_types_, download_ready_task_);
+  backend_->ConfigureDataTypes(
+      last_requested_types_,
+      method_factory_.NewRunnableMethod(&DataTypeManagerImpl::DownloadReady));
 }
 
 void DataTypeManagerImpl::DownloadReady() {
   DCHECK(state_ == DOWNLOAD_PENDING || state_ == RESTARTING);
-  download_ready_task_ = NULL;
 
   // If we had a restart while waiting for downloads, just restart.
   if (state_ == RESTARTING) {
@@ -289,10 +287,10 @@ void DataTypeManagerImpl::Stop() {
     aborted = true;
   }
 
-  // If Stop() is called while waiting for download, cancel the task.
+  // If Stop() is called while waiting for download, cancel all
+  // outstanding tasks.
   if (state_ == DOWNLOAD_PENDING) {
-    download_ready_task_->Cancel();
-    download_ready_task_ = NULL;
+    method_factory_.RevokeAll();
     aborted = true;
   }
 
