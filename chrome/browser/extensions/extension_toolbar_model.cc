6f0b01716262238b1457714184eb8458ddeea900
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-02 20:16:32 +0000

    GTK browser actions toolbar fiddling:
    - make the toolbar size restore properly when the toolbar is created before the model is loaded
    - allow the user to right click menu items in the overflow menu
    
    BUG=40068 (sorta)
    TEST=manual
    
    Review URL: http://codereview.chromium.org/1602003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43515 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_toolbar_model.cc
+++ chrome/browser/extensions/extension_toolbar_model.cc
@@ -190,6 +190,7 @@ void ExtensionToolbarModel::InitializeExtensionList() {
   UpdatePrefs();
 
   extensions_initialized_ = true;
+  FOR_EACH_OBSERVER(Observer, observers_, ModelLoaded());
 }
 
 void ExtensionToolbarModel::UpdatePrefs() {
