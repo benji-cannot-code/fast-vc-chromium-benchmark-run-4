ed11ddd90bae73ca91dbf1b59399b4af157cfbb4
jwieczorek@webkit.org <jwieczorek@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-03 14:34:09 +0000

    2010-04-03  Jakub Wieczorek  <jwieczorek@webkit.org>
            Unreviewed.
    
            Fix debug build with GCC >= 4.3.
    
            * platform/graphics/GraphicsLayer.cpp: Include stdio.h explicitly.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/GraphicsLayer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/GraphicsLayer.cpp
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TextStream.h"
 #include <wtf/text/CString.h>
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 namespace WebCore {
 
 void KeyframeValueList::insert(const AnimationValue* value)
