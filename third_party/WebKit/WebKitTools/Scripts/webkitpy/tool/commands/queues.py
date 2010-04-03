89cee9d2eeadbca6c780e1b6d88b98a13b35d380
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-03 00:14:45 +0000

    2010-04-02  Adam Barth  <abarth@webkit.org>
            Reviewed by Darin Adler.
    
            commit-queue should ignore builders when landing rollouts
            https://bugs.webkit.org/show_bug.cgi?id=37051
    
            When we moved the "builders are red" check into the master process, we
            forgot about rollouts.  I thought we had a test covering this case, but
            looking at the test, it was a bit too loose.  I added a new test and
            introduced some new logging technology into MockTool to make the test
            tighter.
    
            * Scripts/webkitpy/tool/commands/queues.py:
            * Scripts/webkitpy/tool/commands/queues_unittest.py:
            * Scripts/webkitpy/tool/mocktool.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57035 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/commands/queues.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/commands/queues.py
@@ -206,7 +206,9 @@ class CommitQueue(AbstractPatchQueue, StepSequenceErrorHandler):
 
     def _land(self, patch, first_run=False):
         try:
-            if not self._builders_are_green():
+            # We need to check the builders, unless we're trying to land a
+            # rollout (in which case the builders are probably red.)
+            if not patch.is_rollout() and not self._builders_are_green():
                 # We return true here because we want to return to the main
                 # QueueEngine loop as quickly as possible.
                 return True
@@ -232,10 +234,6 @@ class CommitQueue(AbstractPatchQueue, StepSequenceErrorHandler):
                 # know we're testing the same revision that we successfully
                 # built and tested.
                 args.append("--no-update")
-            if patch.is_rollout():
-                # We need to ignore the builders when landing a rollout
-                # because they're probably red.
-                args.append("--ignore-builders")
             self.run_webkit_patch(args)
             self._did_pass(patch)
             return True
