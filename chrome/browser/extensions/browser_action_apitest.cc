b053c1b2dc43319dfcc93ea5b058b376501770bc
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-05 18:50:11 +0000

    Fixes bug numbers for disabled tests.
    BUG=None
    TEST=None
    Review URL: http://codereview.chromium.org/1594006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43632 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/browser_action_apitest.cc
+++ chrome/browser/extensions/browser_action_apitest.cc
@@ -25,9 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gfx/rect.h"
 #include "gfx/size.h"
 
-// http://crbug.com/40002
 #if defined(OS_MACOSX)
+// http://crbug.com/40002
 #define MAYBE_IncognitoBasic DISABLED_IncognitoBasic
+// http://crbug.com/40133
 #define MAYBE_BrowserActionAddPopup DISABLED_BrowserActionAddPopup
 #else
 #define MAYBE_IncognitoBasic IncognitoBasic
