47482e432b59d849fa218ba28afab65bccb2a425
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-05 19:18:58 +0000

    Fix regression for browser action container width on Toolkit Views.
    Once we introduced the model for keeping track of the number of
    visible icons, there was a race that was introduced where the 
    toolbar could get constructed before the model knew about any
    icons, so on startup the width of the container was 0.
    
    This CL hooks up the newly added ModelLoaded notification and
    sets the width on receiving that notification, if the width
    hasn't been set already.
    
    BUG=40329
    TEST=None
    
    Review URL: http://codereview.chromium.org/1547021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43635 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/browser_actions_container.h
+++ chrome/browser/views/browser_actions_container.h
@@ -384,6 +384,10 @@ class BrowserActionsContainer
   virtual void BrowserActionAdded(Extension* extension, int index);
   virtual void BrowserActionRemoved(Extension* extension);
   virtual void BrowserActionMoved(Extension* extension, int index);
+  virtual void ModelLoaded();
+
+  // Sets the initial container width.
+  void SetContainerWidth();
 
   // Closes the overflow menu if open.
   void CloseOverflowMenu();
