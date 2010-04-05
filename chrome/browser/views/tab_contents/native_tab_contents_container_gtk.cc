97be3ac2e26d6c19ae2f1c4b8503c9d57354fba7
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-05 16:20:34 +0000

    A patch to fix a couple of issues in interactive_ui_tests on ChromeOS
    * Removed two static SkBitmap objects.
     - default_favicion does not seem to be in use. Removed along with InitClass.
     - Moved to local static and moved to heap. SkBitmap destructor was failing on
       exit (in pthread_mutex_lock)
    
    * Set VIEW_ID_TAB_CONTAINER_FOCUS_VIEW to the tab container (this ID is required in the test)
    
    * Use FocusManager and call TabContentsDelegate::TakeFocus
      in TabContentsViewGtk::TakeFocus.
    
    BUG=39736
    TEST=none (interactive_ui_tests needs more patch to run)
    
    Review URL: http://codereview.chromium.org/1594003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43617 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tab_contents/native_tab_contents_container_gtk.cc
+++ chrome/browser/views/tab_contents/native_tab_contents_container_gtk.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/render_widget_host_view.h"
 #include "chrome/browser/tab_contents/interstitial_page.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/browser/view_ids.h"
 #include "chrome/browser/views/tab_contents/tab_contents_container.h"
 #include "views/focus/focus_manager.h"
 #include "views/widget/root_view.h"
@@ -20,6 +21,7 @@ NativeTabContentsContainerGtk::NativeTabContentsContainerGtk(
     TabContentsContainer* container)
     : container_(container),
       focus_callback_id_(0) {
+  SetID(VIEW_ID_TAB_CONTAINER_FOCUS_VIEW);
 }
 
 NativeTabContentsContainerGtk::~NativeTabContentsContainerGtk() {
