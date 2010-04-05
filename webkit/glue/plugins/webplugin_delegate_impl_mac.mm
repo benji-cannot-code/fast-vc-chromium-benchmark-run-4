5c59feaf8d0bcd2dc9941a0a4d6377072ba7632e
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-05 20:57:17 +0000

    Fix DivX crash on the Mac
    Protect all instances of NPP_HandleEvent from being sent before NPP_SetWindow return; this could happen if a plugin made a synchronous call from within NPP_SetWindow, and we thus ended up in a nested call.
    
    Unblacklist DivX 2.0 since it no longer crashes.
    
    BUG=25690
    TEST=Visit DixX test sites (see bug); plugin should not crash (although video may not draw).
    
    Review URL: http://codereview.chromium.org/1622002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43650 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/webplugin_delegate_impl_mac.mm
+++ webkit/glue/plugins/webplugin_delegate_impl_mac.mm
@@ -493,8 +493,9 @@ void WebPluginDelegateImpl::DrawLayerInSurface() {
 
 void WebPluginDelegateImpl::WindowlessPaint(gfx::NativeDrawingContext context,
                                             const gfx::Rect& damage_rect) {
-  // If we somehow get a paint before we've set up the plugin buffer, bail.
-  if (!buffer_context_)
+  // If we get a paint event before we are completely set up (e.g., a nested
+  // call while the plugin is still in NPP_SetWindow), bail.
+  if (!have_called_set_window_ || !buffer_context_)
     return;
   DCHECK(buffer_context_ == context);
 
@@ -602,6 +603,9 @@ std::set<WebPluginDelegateImpl*> WebPluginDelegateImpl::GetActiveDelegates() {
 }
 
 void WebPluginDelegateImpl::FocusChanged(bool has_focus) {
+  if (!have_called_set_window_)
+    return;
+
   if (has_focus == have_focus_)
     return;
   have_focus_ = has_focus;
@@ -1031,7 +1035,7 @@ bool WebPluginDelegateImpl::PlatformHandleInputEvent(
     const WebInputEvent& event, WebCursorInfo* cursor_info) {
   DCHECK(cursor_info != NULL);
 
-  // If we somehow get an event before we've set up the plugin, bail.
+  // If we get an event before we've set up the plugin, bail.
   if (!have_called_set_window_)
     return false;
 #ifndef NP_NO_CARBON
@@ -1179,6 +1183,9 @@ bool WebPluginDelegateImpl::PlatformHandleInputEvent(
 void WebPluginDelegateImpl::FireIdleEvent() {
   // Avoid a race condition between IO and UI threads during plugin shutdown
   if (!instance())
+    return;
+  // Don't send idle events until we've called SetWindow.
+  if (!have_called_set_window_)
     return;
 
 #ifndef NP_NO_QUICKDRAW
