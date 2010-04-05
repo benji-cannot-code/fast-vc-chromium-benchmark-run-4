a14de6a56c81ad207dbaa74e8c1a16e75416e313
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-05 13:47:35 +0000

    2010-04-05  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] [Symbian] Remove obsolete build flags for Symbian
            https://bugs.webkit.org/show_bug.cgi?id=37083
    
            Symbian port of QtWebKit port does not use icu, so it does
            not need U_HAVE_* defines.
    
            Symbian now has inttypes.h as part of OpenC.
    
            stdio.h, limits.h and MathExtras.h are already included in
            more appropriate locations.
    
            No new tests as there is no new functionality.
    
            * config.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/config.h
+++ third_party/WebKit/WebCore/config.h
@@ -177,15 +177,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #undef SKIP_STATIC_CONSTRUCTORS_ON_GCC
 #endif
 #define USE_SYSTEM_MALLOC 1
-#define U_HAVE_INT8_T 0
-#define U_HAVE_INT16_T 0
-#define U_HAVE_INT32_T 0
-#define U_HAVE_INT64_T 0
-#define U_HAVE_INTTYPES_H 0
-
-#include <stdio.h>
-#include <limits.h>
-#include <wtf/MathExtras.h>
 #endif
 
 #if PLATFORM(CHROMIUM)
