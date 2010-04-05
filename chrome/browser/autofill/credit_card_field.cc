1261de86c6d9610b70969b56cb96bd26fb0d30ab
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-05 18:15:49 +0000

    Revert 43627 - AutoFill credit card field filling, with nameoncard field.
    Enables credit card filling when "Name on Card" field is present.  This adds
    to existing method of clicking on card number to fill.
    
    BUG=38320
    TEST=CreditCardFieldTest, FormStructureTest, manual testing with cc.html (attached).
    
    Review URL: http://codereview.chromium.org/1518017
    
    TBR=dhollowa@chromium.org
    Review URL: http://codereview.chromium.org/1605011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43630 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/credit_card_field.cc
+++ chrome/browser/autofill/credit_card_field.cc
@@ -62,7 +62,7 @@ CreditCardField* CreditCardField::Parse(
       } else {
         if (fields == 0 || credit_card_field.expiration_month_) {
           // at beginning or end
-          name_pattern = ASCIIToUTF16("card holder|name on card|nameoncard");
+          name_pattern = ASCIIToUTF16("card holder|name on card");
         } else {
           name_pattern = ASCIIToUTF16("name");
         }
@@ -153,10 +153,7 @@ CreditCardField* CreditCardField::Parse(
 
     // Some pages (e.g. ExpediaBilling.html) have a "card description"
     // field; we parse this field but ignore it.
-    // We also ignore any other fields within a credit card block that
-    // start with "card", under the assumption that they are related to
-    // the credit card section being processed but are uninteresting to us.
-    if (ParseText(&q, ASCIIToUTF16("^card")))
+    if (ParseText(&q, ASCIIToUTF16("card description")))
       continue;
 
     break;
