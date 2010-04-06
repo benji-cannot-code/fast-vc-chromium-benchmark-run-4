063b206cc39d9ea7444f91a06f0da992ea50ce77
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-06 10:47:24 +0000

    Unreviewed buildfix for --minimal build. Buildfix after r57134. Add ENABLE(WORKERS) guard.
    original bug: https://bugs.webkit.org/show_bug.cgi?id=36375
    
    * bindings/js/JSWorkerContextErrorHandler.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57141 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSWorkerContextErrorHandler.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSWorkerContextErrorHandler.cpp
@@ -30,6 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+
+#if ENABLE(WORKERS)
+
 #include "JSWorkerContextErrorHandler.h"
 
 #include "ErrorEvent.h"
@@ -110,3 +113,5 @@ void JSWorkerContextErrorHandler::handleEvent(ScriptExecutionContext* scriptExec
 }
 
 } // namespace WebCore
+
+#endif // ENABLE(WORKERS)
