107cbeb19667fa88ea8104875a05b34700798ac1
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-06 17:49:34 +0000

    2010-04-06  Andrei Popescu  <andreip@google.com>
            Reviewed by Dimitri Glazkov.
    
            [V8][Android] PageCache crashes when JavaScript is disabled
            https://bugs.webkit.org/show_bug.cgi?id=37150
    
            This patch ensures that saving and restoration of the script
            state only happens when JS is enabled and each frame has a
            non-null context.
    
            No new tests: existing tests show the feature is working
            when JS is enabled. When JS is disabled, manual testing
            is required.
    
            * bindings/v8/ScriptCachedFrameData.cpp:
            (WebCore::ScriptCachedFrameData::ScriptCachedFrameData):
            (WebCore::ScriptCachedFrameData::restore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57157 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/ScriptCachedFrameData.cpp
+++ third_party/WebKit/WebCore/bindings/v8/ScriptCachedFrameData.cpp
@@ -34,13 +34,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 ScriptCachedFrameData::ScriptCachedFrameData(Frame* frame)
-    : m_domWindow(frame->domWindow())
+    : m_domWindow(0)
 {
     v8::HandleScope handleScope;
     // The context can only be the context of the main world.
     ASSERT(V8Proxy::mainWorldContext(frame) == V8Proxy::context(frame));
     m_context.set(V8Proxy::mainWorldContext(frame));
+    // The context can be 0, e.g. if JS is disabled in the browser.
+    if (m_context.get().IsEmpty())
+        return;
     m_global.set(m_context.get()->Global());
+    m_domWindow = frame->domWindow();
 }
 
 DOMWindow* ScriptCachedFrameData::domWindow() const
@@ -50,6 +54,9 @@ DOMWindow* ScriptCachedFrameData::domWindow() const
 
 void ScriptCachedFrameData::restore(Frame* frame)
 {
+    if (m_context.get().IsEmpty())
+        return;
+
     v8::HandleScope handleScope;
     v8::Context::Scope contextScope(m_context.get());
 
