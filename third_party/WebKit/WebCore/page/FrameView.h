af26448bf234ce8dab82d5c6fbfce14c59950d26
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-06 20:44:42 +0000

    2010-04-06  James Robinson  <jamesr@chromium.org>
            Reviewed by Simon Fraser.
    
            Reverts the incorrect fixed position fastpath scrolling logic
            https://bugs.webkit.org/show_bug.cgi?id=33150
    
            This code does not properly handle overflow or transforms on fixed
            position elements, causing repaint bugs on scroll.
    
            No new tests.
    
            * page/FrameView.cpp:
            (WebCore::FrameView::addFixedObject):
            (WebCore::FrameView::removeFixedObject):
            * page/FrameView.h:
            * platform/ScrollView.cpp:
            (WebCore::ScrollView::scrollContents):
            * platform/ScrollView.h:
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::styleWillChange):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.h
+++ third_party/WebKit/WebCore/page/FrameView.h
@@ -210,9 +210,6 @@ public:
     bool isFrameViewScrollCorner(RenderScrollbarPart* scrollCorner) const { return m_scrollCorner == scrollCorner; }
     void invalidateScrollCorner();
 
-protected:
-    virtual bool scrollContentsFastPath(const IntSize& scrollDelta, const IntRect& rectToScroll, const IntRect& clipRect);
-
 private:
     FrameView(Frame*);
 
