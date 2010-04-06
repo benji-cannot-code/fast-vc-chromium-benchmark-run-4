80e95e2926e45fdb8adf593e92d153ed171c4c05
yuzo@google.com <yuzo@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-06 18:56:30 +0000

    2010-04-01  Yuzo Fujishima  <yuzo@google.com>
            Reviewed by Darin Adler.
    
            Fix bug: CSS3 :not selector with ID simple selector sequence test fails
            https://bugs.webkit.org/show_bug.cgi?id=36276
    
            * fast/css/invalid-not-with-simple-selector-sequence-expected.txt: Added.
            * fast/css/invalid-not-with-simple-selector-sequence.html: Added.
    2010-04-01  Yuzo Fujishima  <yuzo@google.com>
    
            Reviewed by Darin Adler.
    
            Fix bug: CSS3 :not selector with ID simple selector sequence test fails
            As per http://www.w3.org/TR/css3-selectors/#negation, :not(X) takes a simple selector as an argument.
            WebKit was accepting a simple selector *sequence*.
            This patch adds WebCore::CSSSelector::isSimple which judges if the selector is simple.
            The method is used in CSSGrammar.y to decide whether to accept the selector as the argument of :not().
            https://bugs.webkit.org/show_bug.cgi?id=36276
    
            Test: fast/css/invalid-not-with-simple-selector-sequence.html
    
            * css/CSSGrammar.y:
            * css/CSSSelector.cpp:
            (WebCore::CSSSelector::isSimple):
            * css/CSSSelector.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57161 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSSelector.h
+++ third_party/WebKit/WebCore/css/CSSSelector.h
@@ -224,6 +224,7 @@ namespace WebCore {
 
         bool isLastInSelectorList() const { return m_isLastInSelectorList; }
         void setLastInSelectorList() { m_isLastInSelectorList = true; }
+        bool isSimple() const;
 
         mutable AtomicString m_value;
         QualifiedName m_tag;
