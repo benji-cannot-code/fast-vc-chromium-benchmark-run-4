382bb985a1fb4c252049e57b640a4434114840c4
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-06 14:11:44 +0000

    Fix docking behaviour for devtools windows that don't have an associated browser window
    BUG=40090
    TEST=none
    
    Review URL: http://codereview.chromium.org/1567014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43709 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_window.cc
+++ chrome/browser/debugger/devtools_window.cc
@@ -122,6 +122,7 @@ void DevToolsWindow::Show(bool open_console) {
     if (inspected_window) {
       tab_contents_->set_delegate(this);
       inspected_window->UpdateDevTools();
+      SetAttachedWindow();
       tab_contents_->view()->SetInitialFocus();
       return;
     } else {
@@ -134,6 +135,7 @@ void DevToolsWindow::Show(bool open_console) {
     CreateDevToolsBrowser();
 
   browser_->window()->Show();
+  SetAttachedWindow();
   tab_contents_->view()->SetInitialFocus();
 
   if (open_console) {
@@ -142,6 +144,7 @@ void DevToolsWindow::Show(bool open_console) {
     else
       open_console_on_load_ = true;
   }
+
 }
 
 void DevToolsWindow::Activate() {
@@ -158,6 +161,10 @@ void DevToolsWindow::Activate() {
 
 void DevToolsWindow::SetDocked(bool docked) {
   if (docked_ == docked) {
+    return;
+  }
+  if (docked && !GetInspectedBrowserWindow()) {
+    // Cannot dock, avoid window flashing due to close-reopen cycle.
     return;
   }
   docked_ = docked;
@@ -226,14 +233,18 @@ BrowserWindow* DevToolsWindow::GetInspectedBrowserWindow() {
   return NULL;
 }
 
+void DevToolsWindow::SetAttachedWindow() {
+  tab_contents_->render_view_host()->
+      ExecuteJavascriptInWebFrame(
+          L"", docked_ ? L"WebInspector.setAttachedWindow(true);" :
+                         L"WebInspector.setAttachedWindow(false);");
+}
+
 void DevToolsWindow::Observe(NotificationType type,
                              const NotificationSource& source,
                              const NotificationDetails& details) {
   if (type == NotificationType::LOAD_STOP) {
-    tab_contents_->render_view_host()->
-        ExecuteJavascriptInWebFrame(
-            L"", docked_ ? L"WebInspector.setAttachedWindow(true);" :
-                           L"WebInspector.setAttachedWindow(false);");
+    SetAttachedWindow();
     is_loaded_ = true;
     if (open_console_on_load_) {
       OpenConsole();
