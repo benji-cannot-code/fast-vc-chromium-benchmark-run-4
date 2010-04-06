c4b21f5562ce287a4ff673d411a9e65950bfcac3
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-06 19:43:07 +0000

    Revert 43741 - [Mac] Make Mac phantom tabs match the appearance of the other platforms.
    BUG=none
    TEST=Open a new tab, navigate, and pin it. Open a new tab. Close the pinned tab. No tab background should be drawn, just the favicon.
    
    Review URL: http://codereview.chromium.org/1562014
    
    TBR=rsesek@chromium.org
    Review URL: http://codereview.chromium.org/1627004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43744 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_controller.mm
+++ chrome/browser/cocoa/tab_controller.mm
@@ -21,6 +21,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize target = target_;
 @synthesize action = action_;
 
+namespace {
+
+// If the tab is phantom, the opacity of the TabView is adjusted to this value.
+const CGFloat kPhantomTabAlpha = 105.0 / 255.0;
+
+};  // anonymous namespace
+
 namespace TabControllerInternal {
 
 // A C++ delegate that handles enabling/disabling menu items and handling when
@@ -239,6 +246,11 @@ class MenuDelegate : public menus::SimpleMenuModel::Delegate {
 
   // If the tab is a mini-tab, hide the title.
   [titleView_ setHidden:[self mini]];
+
+  // If it's a phantom mini-tab, draw it alpha-style. Windows does this.
+  CGFloat alphaValue = [self phantom] ? kPhantomTabAlpha
+                                      : 1.0;
+  [[self view] setAlphaValue:alphaValue];
 
   BOOL oldShowCloseButton = [closeButton_ isHidden] ? NO : YES;
   BOOL newShowCloseButton = [self shouldShowCloseButton] ? YES : NO;
--- chrome/browser/cocoa/tab_view.mm
+++ chrome/browser/cocoa/tab_view.mm
@@ -600,11 +600,6 @@ const CGFloat kRapidCloseDist = 2.5;
 }
 
 - (void)drawRect:(NSRect)rect {
-  // If this tab is phantom, do not draw the tab background itself. The only UI
-  // element that will represent this tab is the favicon.
-  if ([controller_ phantom])
-    return;
-
   NSGraphicsContext* context = [NSGraphicsContext currentContext];
   [context saveGraphicsState];
   rect = [self bounds];
