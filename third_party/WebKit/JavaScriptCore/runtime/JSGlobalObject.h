5942512a68c044bf496b1882d343b2399c66b15e
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-06 04:18:18 +0000

    2010-04-05  Oliver Hunt  <oliver@apple.com>
            Reviewed by Gavin Barraclough.
    
            Support weak maps in JSC
            https://bugs.webkit.org/show_bug.cgi?id=37132
    
            Expose an API to allow creation of a map for storing
            weak JS references.
    
            * API/JSWeakObjectMapRefInternal.h: Added.
            (OpaqueJSWeakObjectMap::create):
            (OpaqueJSWeakObjectMap::map):
            (OpaqueJSWeakObjectMap::~OpaqueJSWeakObjectMap):
            (OpaqueJSWeakObjectMap::OpaqueJSWeakObjectMap):
            * API/JSWeakObjectMapRefPrivate.cpp: Added.
            * API/JSWeakObjectMapRefPrivate.h: Added.
            * JavaScriptCore.exp:
            * JavaScriptCore.xcodeproj/project.pbxproj:
            * runtime/JSGlobalObject.h:
            (JSC::JSGlobalObject::registerWeakMap):
            (JSC::JSGlobalObject::deregisterWeakMap):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57120 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSGlobalObject.h
+++ third_party/WebKit/JavaScriptCore/runtime/JSGlobalObject.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSArray.h"
 #include "JSGlobalData.h"
 #include "JSVariableObject.h"
+#include "JSWeakObjectMapRefInternal.h"
 #include "NativeFunctionWrapper.h"
 #include "NumberPrototype.h"
 #include "StringPrototype.h"
@@ -57,6 +58,7 @@ namespace JSC {
     class JSGlobalObject : public JSVariableObject {
     protected:
         using JSVariableObject::JSVariableObjectData;
+        typedef HashSet<RefPtr<OpaqueJSWeakObjectMap> > WeakMapSet;
 
         struct JSGlobalObjectData : public JSVariableObjectData {
             // We use an explicit destructor function pointer instead of a
@@ -154,6 +156,7 @@ namespace JSC {
             RefPtr<JSGlobalData> globalData;
 
             HashSet<GlobalCodeBlock*> codeBlocks;
+            WeakMapSet weakMaps;
         };
 
     public:
@@ -269,6 +272,16 @@ namespace JSC {
         static PassRefPtr<Structure> createStructure(JSValue prototype)
         {
             return Structure::create(prototype, TypeInfo(ObjectType, StructureFlags), AnonymousSlotCount);
+        }
+
+        void registerWeakMap(OpaqueJSWeakObjectMap* map)
+        {
+            d()->weakMaps.add(map);
+        }
+
+        void deregisterWeakMap(OpaqueJSWeakObjectMap* map)
+        {
+            d()->weakMaps.remove(map);
         }
 
     protected:
