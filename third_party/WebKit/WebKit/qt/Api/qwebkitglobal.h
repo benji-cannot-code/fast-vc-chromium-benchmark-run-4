d9aad088a1268b8620247d3748bd60fb14887f53
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-06 17:29:36 +0000

    2010-04-06  Diego Gonzalez  <diego.gonzalez@openbossa.org>
            Reviewed by Simon Hausmann.
    
            [Qt] Add mechanism to detect QtWebKit 2.0 via the preprocessor
            https://bugs.webkit.org/show_bug.cgi?id=36538
    
            * Api/qwebkitglobal.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57155 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebkitglobal.h
+++ third_party/WebKit/WebKit/qt/Api/qwebkitglobal.h
@@ -23,6 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <QtCore/qglobal.h>
 
+#define QTWEBKIT_VERSION_STR "2.0.0"
+// QTWEBKIT_VERSION is (major << 16) + (minor << 8) + patch. Similar to Qt.
+#define QTWEBKIT_VERSION 0x020000
+// Use: #if (QTWEBKIT_VERSION >= QTWEBKIT_VERSION_CHECK(2, 0, 0)). Similar to Qt.
+#define QTWEBKIT_VERSION_CHECK(major, minor, patch) ((major<<16)|(minor<<8)|(patch))
+
 #if defined(QT_MAKEDLL)        /* create a Qt DLL library */
 #  if defined(BUILD_WEBKIT)
 #      define QWEBKIT_EXPORT Q_DECL_EXPORT
