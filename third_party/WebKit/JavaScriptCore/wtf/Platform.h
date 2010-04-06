c6cc9f488edae2c91e33093dd8c52a2ead269985
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-06 00:55:17 +0000

    2010-04-05  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Symbian] Consolidate Symbian WINSCW environment configuration
            https://bugs.webkit.org/show_bug.cgi?id=37100
    
            Move the "undefinition" of WIN32 and _WIN32 from WebCore/config.h
            to JavaScriptCore/wtf/Platform.h as it is not specific to WebCore.
    
            PLATFORM(WIN) and OS(WIN) no longer needs to be undefined as
            undefining WIN32 takes care of it.
    
            * wtf/Platform.h:
    2010-04-05  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Symbian] Consolidate Symbian WINSCW environment configuration
            https://bugs.webkit.org/show_bug.cgi?id=37100
    
            Move the "undefinition" of WIN32 and _WIN32 from WebCore/config.h
            to JavaScriptCore/wtf/Platform.h as it is not specific to WebCore.
    
            No new tests as there is no new functionality.
    
            * config.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57108 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -92,6 +92,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* COMPILER(WINSCW) - CodeWarrior for Symbian emulator */
 #if defined(__WINSCW__)
 #define WTF_COMPILER_WINSCW 1
+/* cross-compiling, it is not really windows */
+#undef WIN32
+#undef _WIN32
 #endif
 
 
@@ -398,9 +401,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* OS(SYMBIAN) - Symbian */
 #if defined (__SYMBIAN32__)
-/* we are cross-compiling, it is not really windows */
-#undef WTF_OS_WINDOWS
-#undef WTF_PLATFORM_WIN
 #define WTF_OS_SYMBIAN 1
 #endif
 
--- third_party/WebKit/WebCore/config.h
+++ third_party/WebKit/WebCore/config.h
@@ -171,8 +171,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif // PLATFORM(MAC)
 
 #if OS(SYMBIAN)
-#undef WIN32
-#undef _WIN32
 #define USE_SYSTEM_MALLOC 1
 #endif
 
