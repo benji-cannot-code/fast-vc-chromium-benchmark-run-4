911e3621695cfcf2345c6332ad912542ee3cf919
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-06 19:50:58 +0000

    2010-04-06  Simon Hausmann  <simon.hausmann@nokia.com>
            Reviewed by Darin Adler.
    
            JS code generator does not support feature conditional attributes that are writable
            https://bugs.webkit.org/show_bug.cgi?id=37149
    
            Write out the feature #ifdef not only for the getter, but also for the setter
            function.
    
            * bindings/scripts/CodeGeneratorJS.pm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -1467,6 +1467,12 @@ sub GenerateImplementation
                         my $putFunctionName = "setJS" . $interfaceName .  $codeGenerator->WK_ucfirst($name) . ($attribute->signature->type =~ /Constructor$/ ? "Constructor" : "");
                         my $implSetterFunctionName = $codeGenerator->WK_ucfirst($name);
 
+                        my $conditional = $attribute->signature->extendedAttributes->{"Conditional"};
+                        if ($conditional) {
+                            $conditionalString = "ENABLE(" . join(") && ENABLE(", split(/&/, $conditional)) . ")";
+                            push(@implContent, "#if ${conditionalString}\n");
+                        }
+
                         push(@implContent, "void ${putFunctionName}(ExecState* exec, JSObject* thisObject, JSValue value)\n");
                         push(@implContent, "{\n");
 
@@ -1535,7 +1541,13 @@ sub GenerateImplementation
                             }
                         }
                         
-                        push(@implContent, "}\n\n");
+                        push(@implContent, "}\n");
+
+                        if ($conditional) {
+                            push(@implContent, "#endif\n");
+                        }
+
+                        push(@implContent, "\n");
                     }
                 }
             }
