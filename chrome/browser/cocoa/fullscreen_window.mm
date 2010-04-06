4baf13b7efc9639da3d5d961c6468884db788458
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-06 19:56:03 +0000

    Theme delegate fixes for the 10.6 SDK folllowing r43708.
    In the 10.6 SDK, -[NSWindow delegate] returns id<NSWindowDelegate>, not just
    id, and the three selectors used in r43708 are not declared in
    NSWindowDelegate.  This results in errors such as "warning: '-themeProvider'
    not found in protocol(s)".
    
    Testing the selectors before using them is safe and not incorrect, but you
    guys might actually want to make more of an assertion about what it means to
    be a ChromeBrowserWindow's or FullscreenWindow's delegate, or perhaps even
    a ChromeEventProcessingWindow's delegate.  Alternatively, it may be
    appropriate to add a ChromeThemedWindow layer as a subclass of CEPW and
    superclass of CBW and FW.  (CEPW's other subclass is InfoBubbleWindow.)
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/1630001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43746 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/fullscreen_window.mm
+++ chrome/browser/cocoa/fullscreen_window.mm
@@ -84,15 +84,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (ThemeProvider*)themeProvider {
-  return [[self delegate] themeProvider];
+  id delegate = [self delegate];
+  if (![delegate respondsToSelector:@selector(themeProvider)])
+    return NULL;
+  return [delegate themeProvider];
 }
 
 - (ThemedWindowStyle)themedWindowStyle {
-  return [[self delegate] themedWindowStyle];
+  id delegate = [self delegate];
+  if (![delegate respondsToSelector:@selector(themedWindowStyle)])
+    return THEMED_NORMAL;
+  return [delegate themedWindowStyle];
 }
 
 - (NSPoint)themePatternPhase {
-  return [[self delegate] themePatternPhase];
+  id delegate = [self delegate];
+  if (![delegate respondsToSelector:@selector(themePatternPhase)])
+    return NSMakePoint(0, 0);
+  return [delegate themePatternPhase];
 }
 
 @end
