ea1e5cccf60122a8b62faf50c3927e9615c7afaf
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-06 22:20:30 +0000

    Reland: No longer accept 'null' as signifying unset optional object property in extension calls
    This changes the json schema validation to treat a object property set to null as significant, rather than meaning the property is not specified.
    
    original review: http://codereview.chromium.org/1558021/show
    
    BUG=39465
    TBR=oshima
    TEST=all tests should pass
    
    Review URL: http://codereview.chromium.org/1618005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43767 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/json_schema.js
+++ chrome/renderer/resources/json_schema.js
@@ -165,7 +165,7 @@ chromeHidden.JSONSchemaValidator.prototype.validate = function(
   // that schema too.
   if (schema.extends)
     this.validate(instance, schema.extends, path);
-  
+
   // If the schema has a $ref property, the instance must validate against
   // that schema too. It must be present in this.types to be referenced.
   if (schema["$ref"]) {
@@ -268,8 +268,7 @@ chromeHidden.JSONSchemaValidator.prototype.validateObject = function(
     var propPath = path ? path + "." + prop : prop;
     if (schema.properties[prop] == undefined) {
       this.addError(propPath, "invalidPropertyType");
-    } else if (prop in instance && instance[prop] !== null &&
-        instance[prop] !== undefined) {
+    } else if (prop in instance && instance[prop] !== undefined) {
       this.validate(instance[prop], schema.properties[prop], propPath);
     } else if (!schema.properties[prop].optional) {
       this.addError(propPath, "propertyRequired");
