085e281454ab12018b913de59cb1217ebaaa04ce
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-07 17:57:10 +0000

    Fixing TIGER build
    git-svn-id: svn://svn.chromium.org/blink/trunk@57217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/SimpleFontDataMac.mm
+++ third_party/WebKit/WebCore/platform/graphics/mac/SimpleFontDataMac.mm
@@ -420,6 +420,7 @@ GlyphMetrics SimpleFontData::platformMetricsForGlyph(Glyph glyph, GlyphMetricsMo
     }
     GlyphMetrics metrics;
     metrics.horizontalAdvance = advance.width + m_syntheticBoldOffset;
+#ifndef BUILDING_ON_TIGER
     if (metricsMode == GlyphBoundingBox) {
         CGRect boundingBox;
         CGFontGetGlyphBBoxes(platformData().cgFont(), &glyph, 1, &boundingBox);
@@ -428,6 +429,9 @@ GlyphMetrics SimpleFontData::platformMetricsForGlyph(Glyph glyph, GlyphMetricsMo
         if (m_syntheticBoldOffset)
             metrics.boundingBox.setWidth(metrics.boundingBox.width() + m_syntheticBoldOffset);
     }
+#else
+    UNUSED_PARAM(metricsMode);
+#endif
     return metrics;
 }
 
