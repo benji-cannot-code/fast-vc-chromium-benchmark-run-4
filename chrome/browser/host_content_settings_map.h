0ca8a69a45f7ca22032dafff65f4c4ba8ad6f7ce
albertb@chromium.org <albertb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-07 22:45:28 +0000

    Use ScopedPrefUpdate to correctly send out notifications when the host content settnigs are update.
    BUG=none
    TEST=HostContentSettingsMapTest
    
    Review URL: http://codereview.chromium.org/1606016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43890 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/host_content_settings_map.h
+++ chrome/browser/host_content_settings_map.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lock.h"
 #include "base/ref_counted.h"
 #include "chrome/common/content_settings.h"
+#include "chrome/common/notification_observer.h"
 #include "googleurl/src/gurl.h"
 
 class DictionaryValue;
@@ -25,7 +26,8 @@ class PrefService;
 class Profile;
 
 class HostContentSettingsMap
-    : public base::RefCountedThreadSafe<HostContentSettingsMap> {
+    : public NotificationObserver,
+      public base::RefCountedThreadSafe<HostContentSettingsMap> {
  public:
   // Details for the CONTENT_SETTINGS_CHANGED notification. This is sent when
   // content settings change for at least one host. If settings change for more
@@ -117,6 +119,11 @@ class HostContentSettingsMap
   // This should only be called on the UI thread.
   void ResetToDefaults();
 
+  // NotificationObserver implementation.
+  virtual void Observe(NotificationType type,
+                       const NotificationSource& source,
+                       const NotificationDetails& details);
+
  private:
   friend class base::RefCountedThreadSafe<HostContentSettingsMap>;
 
@@ -129,6 +136,14 @@ class HostContentSettingsMap
   static const ContentSetting kDefaultSettings[CONTENT_SETTINGS_NUM_TYPES];
 
   ~HostContentSettingsMap();
+
+  // Reads the default settings from the prefereces service. If |overwrite| is
+  // true and the preference is missing, the local copy will be cleared as well.
+  void ReadDefaultSettings(bool overwrite);
+
+  // Reads the host settings from the prefereces service. If |overwrite| is true
+  // and the preference is missing, the local copy will be cleared as well.
+  void ReadPerHostSettings(bool overwrite);
 
   // Returns true if we should allow all content types for this URL.  This is
   // true for various internal objects like chrome:// URLs, so UI and other
@@ -164,6 +179,10 @@ class HostContentSettingsMap
 
   // Used around accesses to the settings objects to guarantee thread safety.
   mutable Lock lock_;
+
+  // Whether we are currently updating preferences, this is used to ignore
+  // notifications from the preferences service that we triggered ourself.
+  bool updating_settings_;
 
   DISALLOW_COPY_AND_ASSIGN(HostContentSettingsMap);
 };
