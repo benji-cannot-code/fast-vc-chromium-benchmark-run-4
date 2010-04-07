c13e9a44c7a0a008a5264665af5eb9fdedf083b4
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-07 14:14:27 +0000

    https://bugs.webkit.org/show_bug.cgi?id=36827
    Patch by Dawit Alemayehu <adawit@kde.org> on 2010-04-07
    Reviewed by Simon Hausmann.
    
    WebCore:
    
    Replaced the 'shouldTreatAsAttachment' function with a more generic
    function that returns the content disposition type.
    
    See comments 39-42 in https://bugs.webkit.org/show_bug.cgi?id=36395
    
    * platform/network/HTTPParsers.cpp:
    (WebCore::contentDispositionType):
    * platform/network/HTTPParsers.h:
    (WebCore::):
    
    WebKit/chromium:
    
    Updated the WebCore::shouldTreatAsAttachement function call with the
    new more generic replacement WebCore::contentDispositionType.
    
    See comments 39-42 in https://bugs.webkit.org/show_bug.cgi?id=36395
    
    * src/FrameLoaderClientImpl.cpp:
    (WebKit::FrameLoaderClientImpl::dispatchDecidePolicyForMIMEType):
    
    WebKit/qt:
    
    Updated the WebCore::shouldTreatAsAttachement function call with the
    new more generic replacement WebCore::contentDispositionType.
    
    See comments 39-42 in https://bugs.webkit.org/show_bug.cgi?id=36395
    
    * WebCoreSupport/FrameLoaderClientQt.cpp:
    (WebCore::FrameLoaderClientQt::dispatchDecidePolicyForMIMEType):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57208 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/HTTPParsers.h
+++ third_party/WebKit/WebCore/platform/network/HTTPParsers.h
@@ -42,8 +42,14 @@ enum XSSProtectionDisposition {
     XSSProtectionBlockEnabled
 };
 
+typedef enum {
+    ContentDispositionNone,
+    ContentDispositionInline,
+    ContentDispositionAttachment,
+    ContentDispositionOther
+} ContentDispositionType;
 
-bool shouldTreatAsAttachment(const ResourceResponseBase& response);
+ContentDispositionType contentDispositionType(const String&);
 bool parseHTTPRefresh(const String& refresh, bool fromHttpEquivMeta, double& delay, String& url);
 double parseDate(const String&);
 String filenameFromHTTPContentDisposition(const String&); 
