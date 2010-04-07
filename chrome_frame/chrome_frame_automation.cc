3382965354de3cb9f32fa8d9b03e9dd4adbd0385
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-07 01:48:07 +0000

    Speculative fix for a ChromeFrame crash reported on the crash server. The crash occurs when the ChromeFrame active document instance is torn down. This takes down the automation client instance which handles communication with Chrome. When the automation client instance is torn down it tries to stop pending chrome requests which causes a crash because of an invalid url fetcher object. This could only happen if the url fetcher object belongs to a deleted active document instance, which could occur if the automation client instance was reused during a Reinitialize operation which failed.
    Fix is to tear down the pending url requests at the top of the Reinitialize function before checking for failure cases.
    
    Should fix bug http://code.google.com/p/chromium/issues/detail?id=40590
    
    Bug=40590
    
    Review URL: http://codereview.chromium.org/1524015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43797 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_frame_automation.cc
+++ chrome_frame/chrome_frame_automation.cc
@@ -538,6 +538,7 @@ void ChromeFrameAutomationClient::Uninitialize() {
   if (url_fetcher_) {
     // Clean up any outstanding requests
     url_fetcher_->StopAllRequests();
+    url_fetcher_ = NULL;
   }
 
   if (tab_.get()) {
@@ -1218,6 +1219,12 @@ void ChromeFrameAutomationClient::PrintTab() {
 bool ChromeFrameAutomationClient::Reinitialize(
     ChromeFrameDelegate* delegate,
     PluginUrlRequestManager* url_fetcher) {
+  if (url_fetcher_) {
+    // Clean up any outstanding requests
+    url_fetcher_->StopAllRequests();
+    url_fetcher_ = NULL;
+  }
+
   if (!tab_.get() || !::IsWindow(chrome_window_)) {
     NOTREACHED();
     DLOG(WARNING) << "ChromeFrameAutomationClient instance reused "
@@ -1230,7 +1237,6 @@ bool ChromeFrameAutomationClient::Reinitialize(
     return false;
   }
 
-  url_fetcher_->StopAllRequests();
   chrome_frame_delegate_ = delegate;
   DeleteAllPendingTasks();
   SetUrlFetcher(url_fetcher);
