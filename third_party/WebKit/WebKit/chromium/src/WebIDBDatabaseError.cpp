543cefa5b1d07c55ec4650e653eac5f7ae61e91f
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-07 15:52:54 +0000

    2010-04-07  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Darin Fisher.
    
            [Chromium] WebIDBDatabaseError::assign doesn't work correctly
            https://bugs.webkit.org/show_bug.cgi?id=37209
    
            * src/WebIDBDatabaseError.cpp:
            (WebKit::WebIDBDatabaseError::assign):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57209 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebIDBDatabaseError.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebIDBDatabaseError.cpp
@@ -48,7 +48,7 @@ WebIDBDatabaseError::~WebIDBDatabaseError()
 
 void WebIDBDatabaseError::assign(const WebIDBDatabaseError& value)
 {
-    m_private = value;
+    m_private = value.m_private;
 }
 
 void WebIDBDatabaseError::assign(unsigned short code, const WebString& message)
