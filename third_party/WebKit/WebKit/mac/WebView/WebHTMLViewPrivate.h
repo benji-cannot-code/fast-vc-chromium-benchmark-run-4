dc92959fba678240e9735be1efc882d1b8a2121f
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-07 02:29:32 +0000

    2010-04-06  Adam Barth  <abarth@webkit.org>
            Unreviewed.
    
            Speculative build fix for Tiger.
    
            * WebView/WebHTMLViewPrivate.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57186 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLViewPrivate.h
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLViewPrivate.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#import <Cocoa/Cocoa.h>
 #import <WebKit/WebHTMLView.h>
 
 #if !defined(ENABLE_NETSCAPE_PLUGIN_API)
