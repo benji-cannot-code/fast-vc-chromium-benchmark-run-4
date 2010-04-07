aba63850b327e243228b59f4f83227b555390902
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-07 20:00:46 +0000

    notifications: Simplify the callback handler for the close button event.
    BUG=None
    TEST=compiles and the close button still works as before
    
    Patch from Thiago Farina <thiago.farina@gmail.com>
    
    Review URL: http://codereview.chromium.org/1575018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43868 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/notifications/balloon_view.h
+++ chrome/browser/views/notifications/balloon_view.h
@@ -46,6 +46,7 @@ class BalloonViewImpl : public BalloonView,
                         public views::View,
                         public views::ViewMenuDelegate,
                         public views::WidgetDelegate,
+                        public views::ButtonListener,
                         public menus::SimpleMenuModel::Delegate,
                         public NotificationObserver,
                         public AnimationDelegate {
@@ -74,6 +75,9 @@ class BalloonViewImpl : public BalloonView,
 
   // views::WidgetDelegate interface.
   void DisplayChanged();
+
+  // views::ButtonListener interface.
+  virtual void ButtonPressed(views::Button* sender, const views::Event&);
 
   // menus::SimpleMenuModel::Delegate interface.
   virtual bool IsCommandIdChecked(int command_id) const;
@@ -162,9 +166,6 @@ class BalloonViewImpl : public BalloonView,
 
   // Pointer to sub-view is owned by View class.
   views::Label* source_label_;
-
-  // Listener for clicks on the close button.
-  scoped_ptr<views::ButtonListener> close_button_listener_;
 
   // An animation to move the balloon on the screen as its position changes.
   scoped_ptr<SlideAnimation> animation_;
