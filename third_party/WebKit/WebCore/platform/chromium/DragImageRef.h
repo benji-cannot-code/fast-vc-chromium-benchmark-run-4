18477d91047400149eedad870a7ada251f01771c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-07 01:02:11 +0000

    2010-04-06  Nicolas Weber  <thakis@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Implement DragImage functionality for chromium/mac.
            https://bugs.webkit.org/show_bug.cgi?id=37069
    
            * page/chromium/DragControllerChromium.cpp:
            (WebCore::DragController::maxDragImageSize):
            Use a max size of 400x400 on OS X.
            * platform/chromium/DragImageChromiumMac.cpp:
            (WebCore::dragImageSize): Implement.
            (WebCore::deleteDragImage): Implement.
            (WebCore::scaleDragImage): Implement.
            (WebCore::dissolveDragImageToFraction): Implement.
            (WebCore::createDragImageFromImage): Implement.
            * platform/chromium/DragImageRef.h:
            Use CGImageRefs as DragImageRef on OS X.
    2010-04-06  Nicolas Weber  <thakis@chromium.org>
    
            Reviewed by Dimitri Glazkov.
    
            Use drag images on OS X as well.
            https://bugs.webkit.org/show_bug.cgi?id=37069
    
            * src/DragClientImpl.cpp:
            (WebKit::DragClientImpl::startDrag):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57180 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/DragImageRef.h
+++ third_party/WebKit/WebCore/platform/chromium/DragImageRef.h
@@ -30,13 +30,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DragImageRef_h
 #define DragImageRef_h
 
+#if OS(DARWIN)
+typedef struct CGImage* CGImageRef;
+#else
 class SkBitmap;
+#endif
 
 namespace WebCore {
 
 #if OS(DARWIN)
-// FIXME: not implemented.
-typedef void* DragImageRef;
+typedef CGImageRef DragImageRef;
 #else
 typedef SkBitmap* DragImageRef;
 #endif
