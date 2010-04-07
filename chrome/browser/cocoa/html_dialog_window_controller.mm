9315c8fa7bcef3835c70007bc3b43c6d6b3092c3
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-07 01:42:06 +0000

    Integrated customize sync dialog with sync setup wizard on OS X.
    Changed ShowHtmlDialog() on OS X to return the created window.
    
    BUG=34209
    TEST=manual
    
    Review URL: http://codereview.chromium.org/1535018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43796 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/html_dialog_window_controller.mm
+++ chrome/browser/cocoa/html_dialog_window_controller.mm
@@ -73,8 +73,9 @@ private:
 
 namespace html_dialog_window_controller {
 
-void ShowHtmlDialog(HtmlDialogUIDelegate* delegate, Profile* profile) {
-  [HtmlDialogWindowController showHtmlDialog:delegate profile:profile];
+gfx::NativeWindow ShowHtmlDialog(
+    HtmlDialogUIDelegate* delegate, Profile* profile) {
+  return [HtmlDialogWindowController showHtmlDialog:delegate profile:profile];
 }
 
 }  // namespace html_dialog_window_controller
@@ -218,13 +219,14 @@ void HtmlDialogWindowDelegateBridge::HandleKeyboardEvent(
 // NOTE(akalin): We'll probably have to add the parentWindow parameter back
 // in once we implement modal dialogs.
 
-+ (void)showHtmlDialog:(HtmlDialogUIDelegate*)delegate
-               profile:(Profile*)profile {
++ (NSWindow*)showHtmlDialog:(HtmlDialogUIDelegate*)delegate
+                    profile:(Profile*)profile {
   HtmlDialogWindowController* htmlDialogWindowController =
     [[HtmlDialogWindowController alloc] initWithDelegate:delegate
                                                  profile:profile];
   [htmlDialogWindowController loadDialogContents];
   [htmlDialogWindowController showWindow:nil];
+  return [htmlDialogWindowController window];
 }
 
 - (id)initWithDelegate:(HtmlDialogUIDelegate*)delegate
--- chrome/browser/cocoa/html_dialog_window_controller_cppsafe.h
+++ chrome/browser/cocoa/html_dialog_window_controller_cppsafe.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_COCOA_HTML_DIALOG_WINDOW_CONTROLLER_CPPSAFE_H_
 #define CHROME_BROWSER_COCOA_HTML_DIALOG_WINDOW_CONTROLLER_CPPSAFE_H_
 
+#include "gfx/native_widget_types.h"
+
 // We declare this in a separate file that is safe for including in C++ code.
 
 // TODO(akalin): It would be nice if there were a platform-agnostic way to
@@ -17,8 +19,12 @@ namespace html_dialog_window_controller {
 
 // Creates and shows an HtmlDialogWindowController with the given
 // delegate and profile. The window is automatically destroyed when it is
-// closed.
-void ShowHtmlDialog(HtmlDialogUIDelegate* delegate, Profile* profile);
+// closed.  Returns the created window.
+//
+// Make sure to use the returned window only when you know it is safe
+// to do so, i.e. before OnDialogClosed() is called on the delegate.
+gfx::NativeWindow ShowHtmlDialog(
+    HtmlDialogUIDelegate* delegate, Profile* profile);
 
 }  // namespace html_dialog_window_controller
 
