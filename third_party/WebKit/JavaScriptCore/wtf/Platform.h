56440bba2baf300788be77c029e7ca431d9af6b3
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-07 17:59:49 +0000

    https://bugs.webkit.org/show_bug.cgi?id=37219
    Reviewed by Darin Adler.
    
    JavaScriptCore: 
    
    This change disables text caret for the iPhone platflorm.
            
    * wtf/Platform.h: Disabled text caret for iPhone.
    
    WebCore: 
    
    This change disables text caret for the iPhone platflorm.
    Added UNUSED_PARAM to build when ENABLE_TEXT_CARET is 0.
    
    * editing/SelectionController.cpp:
    (WebCore::SelectionController::recomputeCaretRect):
    (WebCore::SelectionController::paintCaret):
    * page/Frame.cpp:
    (WebCore::Frame::paintDragCaret):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57218 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -868,6 +868,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_NOTIFICATIONS 0
 #endif
 
+#if PLATFORM(IPHONE)
+#define ENABLE_TEXT_CARET 0
+#endif
+
 #if !defined(ENABLE_TEXT_CARET)
 #define ENABLE_TEXT_CARET 1
 #endif
