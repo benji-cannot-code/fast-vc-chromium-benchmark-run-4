84c62c5fac35a6f2fc9a6f13f0c65addb03a15a1
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-08 20:34:45 +0000

    Increase the default cache time for failed host resolves from 1 second to 1 minute.
    BUG=1672
    Review URL: http://codereview.chromium.org/1576021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43997 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_resolver_impl.cc
+++ net/base/host_resolver_impl.cc
@@ -39,7 +39,7 @@ HostCache* CreateDefaultCache() {
   HostCache* cache = new HostCache(
       kMaxHostCacheEntries,
       base::TimeDelta::FromMinutes(1),
-      base::TimeDelta::FromSeconds(1));
+      base::TimeDelta::FromMinutes(1));
 
   return cache;
 }
