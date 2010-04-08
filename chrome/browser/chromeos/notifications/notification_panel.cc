76ed502d80f10b4078ee2aa92cf86ac1595c1600
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-08 17:45:32 +0000

    ifdef'ed debug only code
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1600015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43967 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/notifications/notification_panel.cc
+++ chrome/browser/chromeos/notifications/notification_panel.cc
@@ -36,6 +36,7 @@ const int kStaleTimeoutInSeconds = 10;
 using chromeos::BalloonViewImpl;
 using chromeos::NotificationPanel;
 
+#if !defined(NDEBUG)
 // A utility function to convert State enum to string.
 const char* ToStr(const NotificationPanel::State state) {
   switch (state) {
@@ -53,6 +54,7 @@ const char* ToStr(const NotificationPanel::State state) {
       return "unknown";
   }
 }
+#endif
 
 class PanelWidget : public views::WidgetGtk {
  public:
@@ -611,8 +613,10 @@ void NotificationPanel::OnStale(BalloonViewImpl* view) {
 }
 
 void NotificationPanel::SetState(State new_state, const char* name) {
+#if !defined(NDEBUG)
   DLOG(INFO) << "state transition " << ToStr(state_) << " >> "
              << ToStr(new_state) << " in " << name;
+#endif
   state_ = new_state;
 }
 
