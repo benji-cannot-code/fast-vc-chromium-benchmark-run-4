18f223b8aa44057197d832535c1ab65bdf0125ed
stoyan@chromium.org <stoyan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-08 16:53:57 +0000

    Move Urlmon network requests to UI thread, thus allowing reusing the bind context (and already fetched data) via INET_E_TERMINATED_BIND error code. Make caching of data more sensible, so the UI does not freeze (the reason why url request have been moved to a background thread). Always return INET_E_TERMINATED_BIND on OnDataAvailable when BSCF_LASTDATANOTIFICATION flag is detected, so if a subsequent DownloadToHost request comes from Chrome, bind context and already fetched data can be reused.
    BUG=36694
    Review URL: http://codereview.chromium.org/1523014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43953 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/urlmon_url_request.h
+++ chrome_frame/urlmon_url_request.h
@@ -22,27 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class UrlmonUrlRequest;
 
-class RequestDataForUrl {
- public:
-  RequestDataForUrl(RequestData* data, const std::wstring& url)
-      : request_data_(data), url_(url) {
-  }
-  ~RequestDataForUrl() {
-  }
-
-  const std::wstring& url() const {
-    return url_;
-  }
-
-  RequestData* request_data() const {
-    return request_data_;
-  }
-
- protected:
-  scoped_refptr<RequestData> request_data_;
-  std::wstring url_;
-};
-
 class UrlmonUrlRequestManager
     : public PluginUrlRequestManager,
       public PluginUrlRequestDelegate {
@@ -67,13 +46,13 @@ class UrlmonUrlRequestManager
   UrlmonUrlRequestManager();
   ~UrlmonUrlRequestManager();
 
-  // Use cached data when Chrome request this url.
+  // Use specific bind context when Chrome request this url.
   // Used from ChromeActiveDocument's implementation of IPersistMoniker::Load().
-  void UseRequestDataForUrl(RequestData* data, const std::wstring& url);
+  void SetInfoForUrl(const std::wstring& url,
+                     IMoniker* moniker, LPBC bind_context);
 
   // Returns a copy of the url privacy information for this instance.
   PrivacyInfo privacy_info() {
-    AutoLock lock(privacy_info_lock_);
     return privacy_info_;
   }
 
@@ -102,7 +81,7 @@ class UrlmonUrlRequestManager
   // PluginUrlRequestManager implementation.
   virtual bool IsThreadSafe();
   virtual void StartRequest(int request_id,
-                            const ThreadSafeAutomationUrlRequest& request_info);
+                            const IPC::AutomationURLRequest& request_info);
   virtual void ReadRequest(int request_id, int bytes_to_read);
   virtual void EndRequest(int request_id);
   virtual void DownloadRequestInHost(int request_id);
@@ -119,33 +98,41 @@ class UrlmonUrlRequestManager
                                  base::Time last_modified,
                                  const std::string& redirect_url,
                                  int redirect_status);
-  virtual void OnReadComplete(int request_id, const void* buffer, int len);
+  virtual void OnReadComplete(int request_id, const std::string& data);
   virtual void OnResponseEnd(int request_id, const URLRequestStatus& status);
-
-  bool ExecuteInWorkerThread(const tracked_objects::Location& from_here,
-                             Task* task);
-  // Methods executed in worker thread.
-  void StartRequestWorker(int request_id,
-                          const ThreadSafeAutomationUrlRequest& request_info,
-                          RequestDataForUrl* use_request);
-  void ReadRequestWorker(int request_id, int bytes_to_read);
-  void EndRequestWorker(int request_id);
-  void StopAllWorker();
 
   // Map for (request_id <-> UrlmonUrlRequest)
   typedef std::map<int, scoped_refptr<UrlmonUrlRequest> > RequestMap;
   RequestMap request_map_;
   scoped_refptr<UrlmonUrlRequest> LookupRequest(int request_id);
 
-  scoped_ptr<RequestDataForUrl> request_data_for_url_;
-  STAThread worker_thread_;
-  base::WaitableEvent map_empty_;
-  bool stopping_;
-  Lock worker_thread_access_;
+  struct UrlInfo {
+    void Clear() {
+      url_ = GURL::EmptyGURL();
+      bind_ctx_.Release();
+      moniker_.Release();
+    }
 
-  // This lock is used to synchronize access to the PrivacyInfo data structure
-  // as it can be accessed from the ui thread and the worker thread.
-  Lock privacy_info_lock_;
+    void Set(const std::wstring& url, IMoniker* moniker, LPBC bc) {
+      DCHECK(bind_ctx_.get() == NULL);
+      DCHECK(moniker_.get() == NULL);
+      url_ = GURL(url);
+      moniker_ = moniker;
+      bind_ctx_ = bc;
+    }
+
+    bool IsForUrl(const std::string& url) {
+      return GURL(url) == url_;
+    }
+
+    GURL url_;
+    ScopedComPtr<IBindCtx> bind_ctx_;
+    ScopedComPtr<IMoniker> moniker_;
+  } url_info_;
+
+  bool stopping_;
+  int calling_delegate_;  // re-entrancy protection (debug only check)
+
   PrivacyInfo privacy_info_;
   // The window to be used to fire notifications on.
   HWND notification_window_;
