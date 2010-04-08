6f0a830166cef604beda6077de924994133da23f
vitalyr@chromium.org <vitalyr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-08 10:15:12 +0000

    2010-04-08  Vitaly Repeshko  <vitalyr@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            [V8] Fix crash in NodeFilterCondition in detached iframe
            https://bugs.webkit.org/show_bug.cgi?id=37234
    
            Test: fast/dom/node-filter-detached-iframe-crash.html
    
            * bindings/v8/V8NodeFilterCondition.cpp:
            (WebCore::V8NodeFilterCondition::acceptNode): Switched to using
            callFunctionWithoutFrame.
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::callFunctionWithoutFrame): Added a function to
            call V8 with no current frame.
            * bindings/v8/V8Proxy.h:
    
    2010-04-08  Vitaly Repeshko  <vitalyr@chromium.org>
    
            Reviewed by Dimitri Glazkov.
    
            [V8] Fix crash in NodeFilterCondition in detached iframe
            https://bugs.webkit.org/show_bug.cgi?id=37234
    
            * fast/dom/node-filter-detached-iframe-crash-expected.txt: Added.
            * fast/dom/node-filter-detached-iframe-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8CustomXPathNSResolver.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8CustomXPathNSResolver.cpp
@@ -39,14 +39,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-PassRefPtr<V8CustomXPathNSResolver> V8CustomXPathNSResolver::create(V8Proxy* proxy, v8::Handle<v8::Object> resolver)
+PassRefPtr<V8CustomXPathNSResolver> V8CustomXPathNSResolver::create(v8::Handle<v8::Object> resolver)
 {
-    return adoptRef(new V8CustomXPathNSResolver(proxy, resolver));
+    return adoptRef(new V8CustomXPathNSResolver(resolver));
 }
 
-V8CustomXPathNSResolver::V8CustomXPathNSResolver(V8Proxy* proxy, v8::Handle<v8::Object> resolver)
-        : m_proxy(proxy)
-        , m_resolver(resolver)
+V8CustomXPathNSResolver::V8CustomXPathNSResolver(v8::Handle<v8::Object> resolver)
+    : m_resolver(resolver)
 {
 }
 
@@ -56,14 +55,6 @@ V8CustomXPathNSResolver::~V8CustomXPathNSResolver()
 
 String V8CustomXPathNSResolver::lookupNamespaceURI(const String& prefix)
 {
-    V8Proxy* proxy = m_proxy;
-
-    if (!proxy) {
-        proxy = V8Proxy::retrieve();
-        if (!proxy)
-            return String();
-    }
-
     v8::Handle<v8::Function> lookupNamespaceURIFunc;
     v8::Handle<v8::String> lookupNamespaceURIName = v8::String::New("lookupNamespaceURI");
 
@@ -75,8 +66,10 @@ String V8CustomXPathNSResolver::lookupNamespaceURI(const String& prefix)
     }
 
     if (lookupNamespaceURIFunc.IsEmpty() && !m_resolver->IsFunction()) {
-        Frame* frame = proxy->frame();
-        logInfo(frame, "XPathNSResolver does not have a lookupNamespaceURI method.", String());
+        if (V8Proxy* proxy = V8Proxy::retrieve()) {
+            if (Frame* frame = proxy->frame())
+                logInfo(frame, "XPathNSResolver does not have a lookupNamespaceURI method.", String());
+        }
         return String();
     }
 
@@ -88,7 +81,7 @@ String V8CustomXPathNSResolver::lookupNamespaceURI(const String& prefix)
     v8::Handle<v8::Value> argv[argc] = { v8String(prefix) };
     v8::Handle<v8::Function> function = lookupNamespaceURIFunc.IsEmpty() ? v8::Handle<v8::Function>::Cast(m_resolver) : lookupNamespaceURIFunc;
 
-    v8::Handle<v8::Value> retval = proxy->callFunction(function, m_resolver, argc, argv);
+    v8::Handle<v8::Value> retval = V8Proxy::callFunctionWithoutFrame(function, m_resolver, argc, argv);
 
     // Eat exceptions from namespace resolver and return an empty string. This will most likely cause NAMESPACE_ERR.
     if (try_catch.HasCaught())
