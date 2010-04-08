f4541f8519aefdab084f22abab87088ddaf5eb31
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-08 14:43:50 +0000

    2010-04-05  Antonio Gomes  <tonikitoo@webkit.org>
            Reviewed by Simon Hausmann.
    
            [Qt] REGRESSION:(r50665) QWebFrame::setScrollBarPolicy(Qt::Vertical,Qt::ScrollBarAlwaysOff) has no effect.
            https://bugs.webkit.org/show_bug.cgi?id=29431
    
            Added stubs for Mac, win, gtk and wx DRTs to implement setScrollbarPolicy method.
    
            * DumpRenderTree/LayoutTestController.cpp:
            (setScrollbarPolicyCallback):
            (LayoutTestController::staticFunctions):
            * DumpRenderTree/LayoutTestController.h:
            * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
            (LayoutTestController::setScrollbarPolicy):
            * DumpRenderTree/mac/LayoutTestControllerMac.mm:
            (LayoutTestController::setScrollbarPolicy):
            * DumpRenderTree/win/LayoutTestControllerWin.cpp:
            (LayoutTestController::setScrollbarPolicy):
            * DumpRenderTree/wx/LayoutTestControllerWx.cpp:
            (LayoutTestController::setScrollbarPolicy):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.h
@@ -96,6 +96,7 @@ public:
     void setXSSAuditorEnabled(bool flag);
     void setFrameFlatteningEnabled(bool enable);
     void setSpatialNavigationEnabled(bool enable);
+    void setScrollbarPolicy(JSStringRef orientation, JSStringRef policy);
 
     void waitForPolicyDelegate();
     size_t webHistoryItemCount();
