007482b626a3df6e4bcdfb7280abe35ea3e622d1
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-08 19:52:21 +0000

    Fix a typo.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/1604015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43993 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/result_codes.h
+++ chrome/common/result_codes.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ResultCodes {
  public:
   enum ExitCode {
-    NORMAL_EXIT = base::PROCESS_END_NORMAL_TERMINATON,
+    NORMAL_EXIT = base::PROCESS_END_NORMAL_TERMINATION,
     TASKMAN_KILL = base::PROCESS_END_KILLED_BY_USER,
     HUNG = base::PROCESS_END_PROCESS_WAS_HUNG,
     INVALID_CMDLINE_URL,        // An invalid command line url was given.
