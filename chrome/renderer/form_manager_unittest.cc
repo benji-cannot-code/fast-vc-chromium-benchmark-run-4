e924c777ba224b5172f009cee14748b184067e40
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-08 03:16:17 +0000

    Reverting: 43802 AutoFill: Re-enable label scraping.
    Reverting due to perf regression.
    
    BUG=40306
    TEST=FormManagerTest.*
    TBR=jhawkins@chromium.org
    
    
    Review URL: http://codereview.chromium.org/1519026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43922 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/form_manager_unittest.cc
+++ chrome/renderer/form_manager_unittest.cc
@@ -423,7 +423,8 @@ TEST_F(FormManagerTest, Reset) {
   ASSERT_EQ(0U, forms.size());
 }
 
-TEST_F(FormManagerTest, Labels) {
+// http://crbug.com/40306
+TEST_F(FormManagerTest, DISABLED_Labels) {
   LoadHTML("<FORM name=\"TestForm\" action=\"http://cnn.com\" method=\"post\">"
            "  <LABEL for=\"firstname\"> First name: </LABEL>"
            "    <INPUT type=\"text\" id=\"firstname\" value=\"John\"/>"
@@ -516,7 +517,8 @@ TEST_F(FormManagerTest, InvalidLabels) {
 // This test has three form control elements, only one of which has a label
 // element associated with it.  The first element is disabled because of the
 // autocomplete=off attribute.
-TEST_F(FormManagerTest, OneLabelElementFirstControlElementDisabled) {
+// http://crbug.com/40306
+TEST_F(FormManagerTest, DISABLED_OneLabelElementFirstControlElementDisabled) {
   LoadHTML("<FORM name=\"TestForm\" action=\"http://cnn.com\" method=\"post\">"
            "  First name:"
            "    <INPUT type=\"text\" id=\"firstname\" autocomplete=\"off\"/>"
@@ -561,7 +563,8 @@ TEST_F(FormManagerTest, OneLabelElementFirstControlElementDisabled) {
                       fields[2]);
 }
 
-TEST_F(FormManagerTest, LabelsInferredFromText) {
+// http://crbug.com/40306
+TEST_F(FormManagerTest, DISABLED_LabelsInferredFromText) {
   LoadHTML("<FORM name=\"TestForm\" action=\"http://cnn.com\" method=\"post\">"
            "  First name:"
            "    <INPUT type=\"text\" id=\"firstname\" value=\"John\"/>"
@@ -604,7 +607,8 @@ TEST_F(FormManagerTest, LabelsInferredFromText) {
                       fields[2]);
 }
 
-TEST_F(FormManagerTest, LabelsInferredFromParagraph) {
+// http://crbug.com/40306
+TEST_F(FormManagerTest, DISABLED_LabelsInferredFromParagraph) {
   LoadHTML("<FORM name=\"TestForm\" action=\"http://cnn.com\" method=\"post\">"
            "  <P>First name:</P><INPUT type=\"text\" "
            "                           id=\"firstname\" value=\"John\"/>"
@@ -647,7 +651,8 @@ TEST_F(FormManagerTest, LabelsInferredFromParagraph) {
                       fields[2]);
 }
 
-TEST_F(FormManagerTest, LabelsInferredFromTableCell) {
+// http://crbug.com/40306
+TEST_F(FormManagerTest, DISABLED_LabelsInferredFromTableCell) {
   LoadHTML("<FORM name=\"TestForm\" action=\"http://cnn.com\" method=\"post\">"
            "<TABLE>"
            "  <TR>"
@@ -701,7 +706,8 @@ TEST_F(FormManagerTest, LabelsInferredFromTableCell) {
                       fields[2]);
 }
 
-TEST_F(FormManagerTest, InferredLabelsWithSameName) {
+// http://crbug.com/40306
+TEST_F(FormManagerTest, DISABLED_InferredLabelsWithSameName) {
   LoadHTML("<FORM name=\"TestForm\" action=\"http://cnn.com\" method=\"post\">"
            "  Address Line 1:"
            "    <INPUT type=\"text\" name=\"Address\"/>"
