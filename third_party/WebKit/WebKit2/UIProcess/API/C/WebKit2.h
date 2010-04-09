60542230f916880da670c2180dc29718b30fba8e
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-09 21:19:47 +0000

    Add new WKString.h and WKURL.h headers to top level include.
    Reviewed by Anders Carlsson.
    
    * UIProcess/API/C/WebKit2.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57364 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/C/WebKit2.h
+++ third_party/WebKit/WebKit2/UIProcess/API/C/WebKit2.h
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebKit2/WKPage.h>
 #include <WebKit2/WKPageNamespace.h>
 #include <WebKit2/WKPreferences.h>
+#include <WebKit2/WKString.h>
+#include <WebKit2/WKURL.h>
 #include <WebKit2/WKView.h>
 
 #endif /* WebKit2_h */
