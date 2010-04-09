3fdeda7d054d5070c42a268f12979f3c840084f3
hamaji@chromium.org <hamaji@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-09 23:40:48 +0000

    2010-04-09  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Eric Seidel.
    
            Prevent wrong use of PrintContext
            https://bugs.webkit.org/show_bug.cgi?id=37194
    
            No new tests because this doesn't change the behavior.
    
            * page/PrintContext.cpp:
            (WebCore::PrintContext::PrintContext):
            (WebCore::PrintContext::~PrintContext):
            (WebCore::PrintContext::begin):
            (WebCore::PrintContext::end):
            * page/PrintContext.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/PrintContext.cpp
+++ third_party/WebKit/WebCore/page/PrintContext.cpp
@@ -33,11 +33,13 @@ namespace WebCore {
 
 PrintContext::PrintContext(Frame* frame)
     : m_frame(frame)
+    , m_isPrinting(false)
 {
 }
 
 PrintContext::~PrintContext()
 {
+    ASSERT(!m_isPrinting);
     m_pageRects.clear();
 }
 
@@ -115,6 +117,9 @@ void PrintContext::computePageRectsWithPageSizeInternal(const FloatSize& pageSiz
 
 void PrintContext::begin(float width)
 {
+    ASSERT(!m_isPrinting);
+    m_isPrinting = true;
+
     // By imaging to a width a little wider than the available pixels,
     // thin pages will be scaled down a little, matching the way they
     // print in IE and Camino. This lets them use fewer sheets than they
@@ -151,6 +156,8 @@ void PrintContext::spoolPage(GraphicsContext& ctx, int pageNumber, float width)
 
 void PrintContext::end()
 {
+    ASSERT(m_isPrinting);
+    m_isPrinting = false;
     m_frame->setPrinting(false, 0, 0, true);
 }
 
