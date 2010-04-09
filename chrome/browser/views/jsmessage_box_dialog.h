4af430190a103cbd32d5936f93437c220dc4c235
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-09 21:06:10 +0000

    Attempt fix for crash bug when alert closes while owning host is invalid.
    I can't actually repro this bug, but I think this should fix it.
    
    BUG=40439
    
    Review URL: http://codereview.chromium.org/1540024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44125 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/jsmessage_box_dialog.h
+++ chrome/browser/views/jsmessage_box_dialog.h
@@ -43,7 +43,6 @@ class JavaScriptMessageBoxDialog : public ModalDialogDelegate {
   virtual bool IsModal() const { return true; }
   virtual views::View* GetContentsView();
   virtual views::View* GetInitiallyFocusedView();
-  virtual void OnClose();
 
  private:
   JavaScriptMessageBoxClient* client() {
