3341f95620992c81eaafaa45e047cde755e901f7
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-09 07:00:08 +0000

    Fix issue where the size of notification on linux/views gets created bigger than max size.
    BUG=none
    TEST=manual: run linux/view build or chromeos build and create a web notification. The size of balloon should be same as one in Windows.
    
    Review URL: http://codereview.chromium.org/1631007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44062 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/notifications/balloon_collection_impl.cc
+++ chrome/browser/chromeos/notifications/balloon_collection_impl.cc
@@ -47,6 +47,9 @@ class NotificationMatcher {
 
 namespace chromeos {
 
+const int BalloonCollectionImpl::kBalloonWidth = 300;
+const int BalloonCollectionImpl::kBalloonMinHeight = 24;
+
 BalloonCollectionImpl::BalloonCollectionImpl()
     : notification_ui_(new NotificationPanel()) {
   registrar_.Add(this, NotificationType::BROWSER_CLOSED,
@@ -160,6 +163,7 @@ Balloon* BalloonCollectionImpl::MakeBalloon(const Notification& notification,
                                             Profile* profile) {
   Balloon* new_balloon = new Balloon(notification, profile, this);
   new_balloon->set_view(new chromeos::BalloonViewImpl(false, true));
+  new_balloon->set_content_size(gfx::Size(kBalloonWidth, kBalloonMinHeight));
   return new_balloon;
 }
 
