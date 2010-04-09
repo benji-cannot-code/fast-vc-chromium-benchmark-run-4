43d4c591b31c472cb84e547027d07524e0ed3f18
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-09 20:35:00 +0000

    Fix Windows build.
    * Shared/NotImplemented.h:
    Include stdio.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Shared/NotImplemented.h
+++ third_party/WebKit/WebKit2/Shared/NotImplemented.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NotImplemented_h
 
 #include <wtf/Assertions.h>
+#include <stdio.h>
 
 #if defined(NDEBUG)
 #define notImplemented() ((void)0)
