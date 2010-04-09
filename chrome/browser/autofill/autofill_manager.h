c944d46f2cde3d5004d1aa8455ae5a1abe2d394c
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-09 03:33:33 +0000

    Fixes for several crashing bugs ##40674,40720,40744 BUG=40674,40720,40744 TEST=In the bugs. Review URL: http://codereview.chromium.org/1594021
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44052 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager.h
+++ chrome/browser/autofill/autofill_manager.h
@@ -34,7 +34,6 @@ extern const char* kAutoFillLearnMoreUrl;
 // Manages saving and restoring the user's personal information entered into web
 // forms.
 class AutoFillManager : public RenderViewHostDelegate::AutoFill,
-                        public AutoFillDialogObserver,
                         public PersonalDataManager::Observer,
                         public AutoFillDownloadManager::Observer {
  public:
@@ -57,11 +56,6 @@ class AutoFillManager : public RenderViewHostDelegate::AutoFill,
                                     const webkit_glue::FormData& form,
                                     const string16& value,
                                     const string16& label);
-
-  // AutoFillDialogObserver implementation:
-  virtual void OnAutoFillDialogApply(
-      std::vector<AutoFillProfile>* profiles,
-      std::vector<CreditCard>* credit_cards);
 
   // PersonalDataManager::Observer implementation:
   virtual void OnPersonalDataLoaded();
