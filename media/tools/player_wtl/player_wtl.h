e6586472916a0a098f5d05a885852b61234b29de
fbarchard@chromium.org <fbarchard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-09 21:26:27 +0000

    add filtering to player_wtl BUG=19113 TEST=none
    Review URL: http://codereview.chromium.org/1576030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44130 0039d316-1c4b-4281-b951-d872f2087c98

--- media/tools/player_wtl/player_wtl.h
+++ media/tools/player_wtl/player_wtl.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MEDIA_TOOLS_PLAYER_WTL_PLAYER_WTL_H_
 
 // Enable timing code by turning on TESTING macro.
-//#define TESTING 1
+#define TESTING 1
 
 // ATL and WTL require order dependent includes.
 #include <atlbase.h>    // NOLINT
--- media/tools/player_wtl/resource.h
+++ media/tools/player_wtl/resource.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ID_VIEW_ROTATE90                32808
 #define ID_VIEW_ROTATE180               32809
 #define ID_VIEW_ROTATE270               32810
+#define ID_VIEW_FILTER                  32842
 #define ID_VIEW_MIRROR_HORIZONTAL       32811
 #define ID_VIEW_MIRROR_VERTICAL         32812
 #define ID_VIEW_QUARTERSIZE             32815
@@ -72,11 +73,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        206
-#define _APS_NEXT_COMMAND_VALUE         32842
+#define _APS_NEXT_COMMAND_VALUE         32843
 #define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
