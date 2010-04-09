2d201bd30240d15b317eab1d5311139e66bd644f
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-09 15:41:51 +0000

    Don't allow drag or click on location icon when editing in omnibox.
    After editing the omnibox, clicking the location icon would provide
    the page info for the page being shown, though the rest of the omnibox
    might be inconsistent with that page.  Exposes the logic used by
    AutocompleteEditView::GetIcon() to determine when there's a real URL
    for page-info (or drag).
    
    BUG=none
    TEST=Navigate to a page.  Click location icon to see page info.  Edit the omnibox, click should not show page info.
    
    Review URL: http://codereview.chromium.org/1594012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44087 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/location_bar_view_mac.h
+++ chrome/browser/cocoa/location_bar_view_mac.h
@@ -62,6 +62,9 @@ class LocationBarViewMac : public AutocompleteEditController,
   virtual void InvalidatePageActions();
   virtual void SaveStateToContents(TabContents* contents);
   virtual void Revert();
+  virtual const AutocompleteEditView* location_entry() const {
+    return edit_view_.get();
+  }
   virtual AutocompleteEditView* location_entry() {
     return edit_view_.get();
   }
