7f2aaf14e3a7963f204fc50ae5904db8bbc1a8b1
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-09 21:41:05 +0000

    Revert r44125 BUG=40439
    TBR=sky@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44132 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/js_modal_dialog.h
+++ chrome/browser/js_modal_dialog.h
@@ -72,9 +72,11 @@ class JavaScriptAppModalDialog : public AppModalDialog,
   // Callbacks from NativeDialog when the user accepts or cancels the dialog.
   void OnCancel();
   void OnAccept(const std::wstring& prompt_text, bool suppress_js_messages);
+  void OnClose();
 
  protected:
   // AppModalDialog overrides.
+  virtual void Cleanup();
   virtual NativeDialog CreateNativeDialog();
 
  private:
