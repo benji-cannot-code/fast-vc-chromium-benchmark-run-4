56165b0649462a001d873e9908ac01e291505be9
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-10 22:17:15 +0000

    2010-04-10  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            run_webkit_tests.py shouldn't have platform-specific logic
            https://bugs.webkit.org/show_bug.cgi?id=37387
    
            Dirk Pranke pointed out that my last patch was wrong because I
            introduced platform-specific logic into run_webkit_tests.py, limiting
            the parallelism in Chromium to work around a bug in the main Mac port.
    
            * Scripts/webkitpy/common/system/executive.py:
                - Fix a typo pointed out by Chris Jerdonek.
            * Scripts/webkitpy/layout_tests/port/base.py:
            * Scripts/webkitpy/layout_tests/port/mac.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57422 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/mac.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/mac.py
@@ -46,7 +46,6 @@ import base
 import server_process
 
 import webkitpy
-import webkitpy.common.system.executive as executive
 import webkitpy.common.system.ospath as ospath
 
 _log = logging.getLogger("webkitpy.layout_tests.port.mac")
@@ -67,6 +66,15 @@ class MacPort(base.Port):
            options.pixel_tests is None):
             options.pixel_tests = False
 
+    def default_num_dump_render_trees(self):
+        # FIXME: new-run-webkit-tests is unstable on Mac running more than
+        # four threads in parallel.
+        # See https://bugs.webkit.org/show_bug.cgi?id=36622
+        num_dump_render_trees = base.Port.default_num_dump_render_trees(self)
+        if num_dump_render_trees > 4:
+            return 4
+        return num_dump_render_trees
+
     def baseline_path(self):
         return self._webkit_baseline_path(self._name)
 
@@ -86,7 +94,7 @@ class MacPort(base.Port):
             self.script_path("build-dumprendertree"),
             self.flag_from_configuration(self._options.configuration),
         ]
-        if executive.run_command(build_drt_command, return_exit_code=True):
+        if self._executive.run_command(build_drt_command, return_exit_code=True):
             return False
 
         if not self.check_image_diff():
@@ -99,7 +107,7 @@ class MacPort(base.Port):
 
         java_tests_path = os.path.join(self.layout_tests_dir(), "java")
         build_java = ["/usr/bin/make", "-C", java_tests_path]
-        if executive.run_command(build_java, return_exit_code=True):
+        if self._executive.run_command(build_java, return_exit_code=True):
             _log.error("Failed to build Java support files: %s" % build_java)
             return False
 
@@ -325,7 +333,7 @@ class MacPort(base.Port):
 
     def _webkit_build_directory(self, args):
         cmd = [self.script_path("webkit-build-directory")] + args
-        return executive.run_command(cmd).rstrip()
+        return self._executive.run_command(cmd).rstrip()
 
     def _build_path(self, *comps):
         if not self._cached_build_root:
