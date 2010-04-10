4ca4597e75f9237133bb3e3bc44f71440989eff8
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-10 02:16:26 +0000

    2010-04-09  Tasuku Suzuki  <tasuku.suzuki@nokia.com>
            Reviewed by Simon Hausmann.
    
            [Qt]Fix compile error with QT_NO_IM
            https://bugs.webkit.org/show_bug.cgi?id=36533
    
            * WebCoreSupport/QtFallbackWebPopup.cpp:
            (WebCore::QtFallbackWebPopupCombo::hidePopup):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/QtFallbackWebPopup.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/QtFallbackWebPopup.cpp
@@ -56,6 +56,7 @@ void QtFallbackWebPopupCombo::showPopup()
 
 void QtFallbackWebPopupCombo::hidePopup()
 {
+#ifndef QT_NO_IM
     QWidget* activeFocus = QApplication::focusWidget();
     if (activeFocus && activeFocus == QComboBox::view()
         && activeFocus->testAttribute(Qt::WA_InputMethodEnabled)) {
@@ -65,6 +66,7 @@ void QtFallbackWebPopupCombo::hidePopup()
             qic->setFocusWidget(0);
         }
     }
+#endif // QT_NO_IM
 
     QComboBox::hidePopup();
 
