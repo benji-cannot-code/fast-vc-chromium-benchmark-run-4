8c59ff81b317aafcbf2994880875f16ab88acbd9
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-10 01:09:44 +0000

    Tidied up some more DOMWrapperWorld lifetime issues.
    Reviewed by Sam "R the K!" Weinig.
    
    * bindings/js/DOMWrapperWorld.cpp:
    (WebCore::DOMWrapperWorld::~DOMWrapperWorld): Notify the document that
    we're going away instead of deleting its data, since deleting other
    objects' data is error-prone and, more importantly, rude.
    
    * bindings/js/DOMWrapperWorld.h:
    (WebCore::DOMWrapperWorld::didCreateWrapperCache):
    (WebCore::DOMWrapperWorld::didDestroyWrapperCache): A few renames for
    clarity.
    
    * bindings/js/JSDOMBinding.cpp:
    * bindings/js/JSDOMBinding.h: Converted forgetAllDOMNodesForDocument
    to a Document member function, for the same reason.
    
    * dom/Document.cpp: 
    (WebCore::Document::~Document):
    (WebCore::Document::createWrapperCache):
    (WebCore::Document::destroyWrapperCache):
    (WebCore::Document::destroyAllWrapperCaches):
    * dom/Document.h: Added a destroyWrapperCache function to balance
    createWrapperCache, so it's clear who allocates and deletes these wrapper
    caches.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDOMBinding.h
+++ third_party/WebKit/WebCore/bindings/js/JSDOMBinding.h
@@ -129,7 +129,6 @@ namespace WebCore {
 
     JSNode* getCachedDOMNodeWrapper(JSC::ExecState*, Document*, Node*);
     void cacheDOMNodeWrapper(JSC::ExecState*, Document*, Node*, JSNode* wrapper);
-    void forgetAllDOMNodesForDocument(Document*);
     void updateDOMNodeDocument(Node*, Document* oldDocument, Document* newDocument);
 
     void markDOMNodesForDocument(JSC::MarkStack&, Document*);
