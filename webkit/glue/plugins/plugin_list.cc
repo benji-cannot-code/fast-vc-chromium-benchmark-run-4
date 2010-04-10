7763633e2dd7ef1497c47b8b752b6fdf4b23f121
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-10 01:43:14 +0000

    Bypass plugin installer if the plugin has been disabled
    - Since we can't add strings just block the default_plugin to be
    used as the plugin to show when we can't find a pluging for a
    given mime type and the a matching plugin that is disabled is found.
    
    
    BUG=39718
    TEST= see bug for details
    
    Review URL: http://codereview.chromium.org/1530029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44176 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/plugin_list.cc
+++ webkit/glue/plugins/plugin_list.cc
@@ -288,6 +288,24 @@ bool PluginList::FindPlugin(const std::string& mime_type,
   return false;
 }
 
+bool PluginList::FindDisabledPlugin(const std::string& mime_type,
+                                    bool allow_wildcard,
+                                    WebPluginInfo* info) {
+  DCHECK(mime_type == StringToLowerASCII(mime_type));
+
+  LoadPlugins(false);
+  AutoLock lock(lock_);
+  for (size_t i = 0; i < plugins_.size(); ++i) {
+    if (!plugins_[i].enabled &&
+        SupportsType(plugins_[i], mime_type, allow_wildcard)) {
+      *info = plugins_[i];
+      return true;
+    }
+  }
+
+  return false;
+}
+
 bool PluginList::FindPlugin(const GURL &url,
                             std::string* actual_mime_type,
                             WebPluginInfo* info) {
@@ -380,6 +398,8 @@ bool PluginList::GetPluginInfo(const GURL& url,
     if (FindPlugin(url, actual_mime_type, &info2)) {
       found = true;
       *info = info2;
+    } else if (FindDisabledPlugin(mime_type, allow_wildcard, &info2)) {
+      found = false;
     }
   }
 
--- webkit/glue/plugins/plugin_list.h
+++ webkit/glue/plugins/plugin_list.h
@@ -183,6 +183,11 @@ class PluginList {
                   bool allow_wildcard,
                   WebPluginInfo* info);
 
+  // Just like |FindPlugin| but it only looks at the disabled plug-ins.
+  bool FindDisabledPlugin(const std::string &mime_type,
+                          bool allow_wildcard,
+                          WebPluginInfo* info);
+
   // Find a plugin by extension; only searches enabled plugins. Returns the
   // corresponding mime type.
   bool FindPlugin(const GURL &url,
