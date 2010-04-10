f16dedd4f08cfd668574aed4916ec305da250a42
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-10 02:59:27 +0000

    2010-04-09  Jer Noble  <jer.noble@apple.com>
            Reviewed by Darin Adler.
    
            Work around QTMovieView bug (<rdar://problem/7712713>) by using a QTMovieLayer instead.
            https://bugs.webkit.org/show_bug.cgi?id=37311 / <rdar://problem/7749993>
    
            * WebView/WebVideoFullscreenController.mm:
            (-[WebVideoFullscreenController windowDidLoad]):
            (-[WebVideoFullscreenController setMediaElement:WebCore::]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57403 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebVideoFullscreenController.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebVideoFullscreenController.mm
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <wtf/UnusedParam.h>
 
 SOFT_LINK_FRAMEWORK(QTKit)
-SOFT_LINK_CLASS(QTKit, QTMovieView)
+SOFT_LINK_CLASS(QTKit, QTMovieLayer)
 
 SOFT_LINK_POINTER(QTKit, QTMovieRateDidChangeNotification, NSString *)
 
@@ -86,17 +86,20 @@ SOFT_LINK_POINTER(QTKit, QTMovieRateDidChangeNotification, NSString *)
 
 - (void)windowDidLoad
 {
+#ifdef BUILDING_ON_TIGER
+    // WebVideoFullscreenController is not supported on Tiger:
+    ASSERT_NOT_REACHED();
+#else
     WebVideoFullscreenWindow *window = [self fullscreenWindow];
-    QTMovieView *view = [[getQTMovieViewClass() alloc] init];
-    [view setFillColor:[NSColor clearColor]];
-    [window setContentView:view];
-    [view setControllerVisible:NO];
-    [view setPreservesAspectRatio:YES];
+    QTMovieLayer *layer = [[getQTMovieLayerClass() alloc] init];
+    [[window contentView] setLayer:layer];
+    [[window contentView] setWantsLayer:YES];
     if (_mediaElement)
-        [view setMovie:_mediaElement->platformMedia().qtMovie];
+        [layer setMovie:_mediaElement->platformMedia().qtMovie];
     [window setHasShadow:YES]; // This is nicer with a shadow.
     [window setLevel:NSPopUpMenuWindowLevel-1];
-    [view release];
+    [layer release];
+#endif
 }
 
 - (WebCore::HTMLMediaElement*)mediaElement;
@@ -106,19 +109,24 @@ SOFT_LINK_POINTER(QTKit, QTMovieRateDidChangeNotification, NSString *)
 
 - (void)setMediaElement:(WebCore::HTMLMediaElement*)mediaElement;
 {
+#ifdef BUILDING_ON_TIGER
+    // WebVideoFullscreenController is not supported on Tiger:
+    ASSERT_NOT_REACHED();
+#else
     _mediaElement = mediaElement;
     if ([self isWindowLoaded]) {
-        QTMovieView *movieView = (QTMovieView *)[[self fullscreenWindow] contentView];
         QTMovie *movie = _mediaElement->platformMedia().qtMovie;
+        QTMovieLayer *movieLayer = (QTMovieLayer *)[[[self fullscreenWindow] contentView] layer];
 
-        ASSERT(movieView && [movieView isKindOfClass:[getQTMovieViewClass() class]]);
+        ASSERT(movieLayer && [movieLayer isKindOfClass:[getQTMovieLayerClass() class]]);
         ASSERT(movie);
-        [movieView setMovie:movie];
+        [movieLayer setMovie:movie];
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(rateChanged:) 
                                                      name:QTMovieRateDidChangeNotification 
                                                    object:movie];
     }
+#endif
 }
 
 - (id <WebVideoFullscreenControllerDelegate>)delegate
