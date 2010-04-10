389df76632fb6c925f8b6e09671484bf339cf530
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-10 01:39:43 +0000

    Fix a memory error writing to a deleted URLFetcher instance.
    BUG=40505
    TEST=existing test under valgrind.
    Review URL: http://codereview.chromium.org/1589028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44175 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/url_fetcher_unittest.cc
+++ chrome/browser/net/url_fetcher_unittest.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::Time;
 using base::TimeDelta;
 
+// TODO(eroman): Add a regression test for http://crbug.com/40505.
+
 namespace {
 
 const wchar_t kDocRoot[] = L"chrome/test/data";
