0a7b591139f39043e8d28365073b161ab2956f7a
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-11 09:05:17 +0000

    [Qt] Unreviewed buildfix for --debug build after r57433.
    Refactor Qt DRT support in QtWebKit
    https://bugs.webkit.org/show_bug.cgi?id=35844
    
    * QtLauncher/main.cpp: qt_drt_garbageCollector_collect(); renamed to DumpRenderTreeSupportQt::garbageCollectorCollect();
    (launcherMain):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57448 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/QtLauncher/main.cpp
+++ third_party/WebKit/WebKitTools/QtLauncher/main.cpp
@@ -65,15 +65,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webinspector.h"
 #include "webpage.h"
 #include "webview.h"
+#include "../../WebKit/qt/WebCoreSupport/DumpRenderTreeSupportQt.h"
 
 #ifdef Q_WS_MAEMO_5
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #undef KeyPress
-#endif
-
-#ifndef NDEBUG
-void QWEBKIT_EXPORT qt_drt_garbageCollector_collect();
 #endif
 
 static const int gExitClickArea = 80;
@@ -1011,7 +1008,7 @@ int launcherMain(const QApplication& app)
 {
 #ifndef NDEBUG
     int retVal = app.exec();
-    qt_drt_garbageCollector_collect();
+    DumpRenderTreeSupportQt::garbageCollectorCollect();
     QWebSettings::clearMemoryCaches();
     return retVal;
 #else
