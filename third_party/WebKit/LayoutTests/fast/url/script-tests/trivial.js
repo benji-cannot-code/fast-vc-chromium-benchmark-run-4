d14ece35b130785b712dab2585eab6a572553de9
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-12 23:42:42 +0000

    2010-04-12  Adam Barth  <abarth@webkit.org>
            Reviewed by Maciej Stachowiak.
    
            Add a trivial test for URL parsing
            https://bugs.webkit.org/show_bug.cgi?id=37475
    
            Hopefully this will be the starting point for many URL tests.
    
            * fast/url/script-tests/TEMPLATE.html: Added.
            * fast/url/script-tests/trivial.js: Added.
            (canonicalize):
            * fast/url/trivial-expected.txt: Added.
            * fast/url/trivial.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57488 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/url/script-tests/trivial.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test basic features of URL canonicalization");
+
+function canonicalize(url) {
+  var a = document.createElement("a");
+  a.href = url;
+  return a.href;
+}
+
+shouldBe("canonicalize('http://example.com/')", "'http://example.com/'");
+shouldBe("canonicalize('/')", "'file:///'");
+
+var successfullyParsed = true;
