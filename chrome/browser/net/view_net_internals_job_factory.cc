5a163a410f63dc55963b24835813acc7f695f09c
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-12 18:35:04 +0000

    Initialize the new net internals page using the passively collected log entries.
    BUG=37421
    
    Review URL: http://codereview.chromium.org/1560025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44266 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/view_net_internals_job_factory.cc
+++ chrome/browser/net/view_net_internals_job_factory.cc
@@ -31,6 +31,10 @@ namespace {
 
 const char kViewHttpCacheSubPath[] = "view-cache";
 
+// TODO(eroman): Delete this file. It should be replaced by
+//               chrome/browser/dom_ui/net_internals_ui.cc once the porting is
+//               complete.
+
 PassiveLogCollector* GetPassiveLogCollector(URLRequestContext* context) {
   // Really this is the same as:
   // g_browser_process->io_thread()->globals()->
@@ -66,6 +70,25 @@ std::string GetDetails(const GURL& url) {
 
 GURL MakeURL(const std::string& details) {
   return GURL(std::string(chrome::kNetworkViewInternalsURL) + details);
+}
+
+// Converts a PassiveLogCollector::EntryList to a CapturingNetLog::EntryList.
+//
+// They are basically the same thing, except PassiveLogCollector has an extra
+// "order" field which we will drop.
+net::CapturingNetLog::EntryList ConvertEntryList(
+    const PassiveLogCollector::EntryList& input) {
+  net::CapturingNetLog::EntryList result;
+  for (size_t i = 0; i < input.size(); ++i) {
+    result.push_back(
+        net::CapturingNetLog::Entry(
+            input[i].type,
+            input[i].time,
+            input[i].source,
+            input[i].phase,
+            input[i].extra_parameters));
+  }
+  return result;
 }
 
 // A job subclass that implements a protocol to inspect the internal
@@ -260,8 +283,11 @@ class ProxyServiceLastInitLogSubSection : public SubSection {
   }
 
   virtual void OutputBody(URLRequestContext* context, std::string* out) {
-    OutputTextInPre(net::NetLogUtil::PrettyPrintAsEventTree(
-        GetInitProxyResolverTracker(context)->entries(), 0), out);
+    OutputTextInPre(
+        net::NetLogUtil::PrettyPrintAsEventTree(
+            ConvertEntryList(GetInitProxyResolverTracker(context)->entries()),
+            0),
+        out);
   }
 };
 
@@ -481,7 +507,7 @@ void OutputURLAndLoadLog(const PassiveLogCollector::RequestInfo& request,
   out->append("</nobr>");
   OutputTextInPre(
       net::NetLogUtil::PrettyPrintAsEventTree(
-          request.entries,
+          ConvertEntryList(request.entries),
           request.num_entries_truncated),
       out);
   out->append("</li>");
--- chrome/browser/resources/net_internals/main.js
+++ chrome/browser/resources/net_internals/main.js
@@ -210,6 +210,11 @@ function(hostResolverCache) {
       hostResolverCache);
 };
 
+BrowserBridge.prototype.receivedPassiveLogEntries = function(entries) {
+  for (var i = 0; i < entries.length; ++i)
+    this.receivedLogEntry(entries[i]);
+};
+
 //------------------------------------------------------------------------------
 
 /**
