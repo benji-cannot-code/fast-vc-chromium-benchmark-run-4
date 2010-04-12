6af0807742a7a61f8af8eee173ad29a6facea909
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-12 00:39:18 +0000

    Turn on internal Flash Player by default.
    BUG=none
    TEST=Run a branded Google Chrome (without any command-line flags); check about:plugins to make sure it picks up the internal Flash. Run it with --disable-internal-flash; check about:plugins to make sure it doesn't pick up the internal Flash.
    
    Review URL: http://codereview.chromium.org/1564025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44223 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -115,10 +115,13 @@ const char kDisableDevTools[]               = "disable-dev-tools";
 const char kDisableExtensions[]             = "disable-extensions";
 
 // Suppresses support for the Geolocation javascript API.
-const char kDisableGeolocation[]             = "disable-geolocation";
+const char kDisableGeolocation[]            = "disable-geolocation";
 
 // Suppresses hang monitor dialogs in renderer processes.
 const char kDisableHangMonitor[]            = "disable-hang-monitor";
+
+// Disable the internal Flash Player.
+const char kDisableInternalFlash[]          = "disable-internal-flash";
 
 // Don't resolve hostnames to IPv6 addresses. This can be used when debugging
 // issues relating to IPv6, but shouldn't otherwise be needed. Be sure to
@@ -133,7 +136,7 @@ const char kDisableJavaScript[]             = "disable-javascript";
 const char kDisableJava[]                   = "disable-java";
 
 // Disable LocalStorage.
-const char kDisableLocalStorage[]            = "disable-local-storage";
+const char kDisableLocalStorage[]           = "disable-local-storage";
 
 // Force logging to be disabled.  Logging is enabled by default in debug
 // builds.
@@ -253,9 +256,6 @@ const char kEnableFileCookies[]             = "enable-file-cookies";
 
 // Enable the Indexed Database API.
 const char kEnableIndexedDatabase[]         = "enable-indexed-database";
-
-// Enable the internal Flash Player (if available).
-const char kEnableInternalFlash[]           = "enable-internal-flash";
 
 // Enable IPv6 support, even if probes suggest that it may not be fully
 // supported.  Some probes may require internet connections, and this flag will
