e77a6e4cf18a5cd1bae5ed09451cc965fdfb69b4
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-12 02:54:37 +0000

    2010-04-11  Adam Barth  <abarth@webkit.org>
            Unreviewed.
    
            Fix new-run-webkit-tests regressions cased by Eric's option parsing patch
            https://bugs.webkit.org/show_bug.cgi?id=37430
    
            We need some basic unit testing of this script, or we're going to keep
            breaking it like this.  Added missing namespace qualifiers and
            propagated renaming of an option.
    
            * Scripts/webkitpy/common/config/ports.py:
            * Scripts/webkitpy/layout_tests/driver_test.py:
            * Scripts/webkitpy/layout_tests/port/base.py:
            * Scripts/webkitpy/layout_tests/port/mac.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57466 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/common/config/ports.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/common/config/ports.py
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 import platform
 
-from optparse import make_option
-
 from webkitpy.common.system.executive import Executive
 
 
