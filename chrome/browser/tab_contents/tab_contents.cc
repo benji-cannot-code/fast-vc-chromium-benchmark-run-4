b3085e308486e2ee277909a41ac6a543b3d9e916
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-12 18:33:04 +0000

    [GTTF] Make automation calls for Downloads, Extensions, and History pages reliable.
    This way we'll be able to write good pyauto tests for them.
    
    TEST=ui_tests
    BUG=none
    
    Review URL: http://codereview.chromium.org/1562021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44264 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -1705,6 +1705,11 @@ bool TabContents::UpdateTitleForEntry(NavigationEntry* entry,
   // Lastly, set the title for the view.
   view_->SetPageTitle(final_title);
 
+  NotificationService::current()->Notify(
+      NotificationType::TAB_CONTENTS_TITLE_UPDATED,
+      Source<TabContents>(this),
+      NotificationService::NoDetails());
+
   return true;
 }
 
