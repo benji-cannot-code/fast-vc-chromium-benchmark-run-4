04e21ec79121fb0f943e43633c9a4ed309caeecf
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-13 14:43:02 +0000

    Reland 44348 Add support for restricting broadcasting events to extensions based on host permissions
    BUG=38398
    TEST=none
    TBR=jochen@chromium.org
    
    Review URL: http://codereview.chromium.org/1642005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44351 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_history_api.cc
+++ chrome/browser/extensions/extension_history_api.cc
@@ -144,7 +144,7 @@ void ExtensionHistoryEventRouter::DispatchEvent(Profile* profile,
                                                 const std::string& json_args) {
   if (profile && profile->GetExtensionMessageService()) {
     profile->GetExtensionMessageService()->DispatchEventToRenderers(
-        event_name, json_args, profile->IsOffTheRecord());
+        event_name, json_args, profile->IsOffTheRecord(), GURL());
   }
 }
 
