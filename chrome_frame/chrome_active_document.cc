b176e0bdc4620e5410fda3156b91142b9bb147f8
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-13 16:53:06 +0000

    Return an error for command groups we don't support.
    TEST=Should be no feature change but if anything this could clear up false positives where we report the status of a menu item as being enabled instead of not available.
    
    BUG=none
    
    Review URL: http://codereview.chromium.org/1589029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44364 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_active_document.cc
+++ chrome_frame/chrome_active_document.cc
@@ -294,6 +294,23 @@ STDMETHODIMP ChromeActiveDocument::QueryStatus(const GUID* cmd_group_guid,
                                                OLECMD commands[],
                                                OLECMDTEXT* command_text) {
   DLOG(INFO) << __FUNCTION__;
+  const GUID* supported_groups[] = {
+    &GUID_NULL,
+    &CGID_MSHTML,
+    &CGID_Explorer,
+  };
+
+  bool supported = (cmd_group_guid == NULL);
+  for (int i = 0; !supported && i < arraysize(supported_groups); ++i) {
+    supported = (IsEqualGUID(*cmd_group_guid, *supported_groups[i]) != FALSE);
+  }
+
+  if (!supported) {
+    DLOG(INFO) << "unsupported command group: "
+        << GuidToString(*cmd_group_guid);
+    return OLECMDERR_E_NOTSUPPORTED;
+  }
+
   for (ULONG command_index = 0; command_index < number_of_commands;
        command_index++) {
     DLOG(INFO) << "Command id = " << commands[command_index].cmdID;
@@ -955,7 +972,7 @@ bool ChromeActiveDocument::ParseUrl(const std::wstring& url,
 
 bool ChromeActiveDocument::LaunchUrl(const std::wstring& url,
                                      bool is_new_navigation) {
-  url_.Reset(::SysAllocString(url.c_str()));
+  url_.Allocate(url.c_str());
 
   if (!is_new_navigation) {
     WStringTokenizer tokenizer(url, L"&");
