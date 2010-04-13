ab1568dc890d7588208fc788ce20039ab7062569
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-13 15:31:19 +0000

    2010-04-13  Stephan Aßmus  <superstippi@gmx.de>
            Reviewed by David Levin.
    
            [Haiku] Use the system's default font family as last fall back font.
            https://bugs.webkit.org/show_bug.cgi?id=37501
    
            No new tests needed.
    
            * platform/graphics/haiku/FontCacheHaiku.cpp:
            (WebCore::FontCache::getLastResortFallbackFont):
                - Use the system's default font family (from be_plain_font global).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57517 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/haiku/FontCacheHaiku.cpp
+++ third_party/WebKit/WebCore/platform/graphics/haiku/FontCacheHaiku.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006 Dirk Mueller <mueller@kde.org>
  * Copyright (C) 2007 Ryan Leavengood <leavengood@gmail.com>
+ * Copyright (C) 2010 Stephan Aßmus <superstippi@gmx.de>
  *
  * All rights reserved.
  *
@@ -38,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FontPlatformData.h"
 #include "NotImplemented.h"
 #include <String.h>
-
+#include <interface/Font.h>
 
 namespace WebCore {
 
@@ -60,10 +61,11 @@ SimpleFontData* FontCache::getSimilarFontPlatformData(const Font& font)
 
 SimpleFontData* FontCache::getLastResortFallbackFont(const FontDescription& fontDescription)
 {
-    // FIXME: Would be even better to somehow get the user's default font here.
-    // For now we'll pick the default that the user would get without changing any prefs.
-    static AtomicString defaultString("DejaVu Serif");
-    return getCachedFontData(fontDescription, defaultString);
+    font_family family;
+    font_style style;
+    be_plain_font->GetFamilyAndStyle(&family, &style);
+    AtomicString plainFontFamily(family);
+    return getCachedFontData(fontDescription, plainFontFamily);
 }
 
 FontPlatformData* FontCache::createFontPlatformData(const FontDescription& fontDescription, const AtomicString& family)
