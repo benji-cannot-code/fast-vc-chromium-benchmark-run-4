06a6440fe61c21c3e5ef08b0354becfde3272a99
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-13 18:58:35 +0000

    https://bugs.webkit.org/show_bug.cgi?id=37513, clean up StyleBoxData and the RenderStyle variable that uses it.
    Reviewed by Oliver Hunt.
    
    * rendering/style/RenderStyle.cpp:
    (WebCore::RenderStyle::RenderStyle):
    (WebCore::RenderStyle::operator==):
    (WebCore::RenderStyle::diff):
    * rendering/style/RenderStyle.h:
    (WebCore::):
    (WebCore::InheritedFlags::width):
    (WebCore::InheritedFlags::height):
    (WebCore::InheritedFlags::minWidth):
    (WebCore::InheritedFlags::maxWidth):
    (WebCore::InheritedFlags::minHeight):
    (WebCore::InheritedFlags::maxHeight):
    (WebCore::InheritedFlags::verticalAlignLength):
    (WebCore::InheritedFlags::boxSizing):
    (WebCore::InheritedFlags::setWidth):
    (WebCore::InheritedFlags::setHeight):
    (WebCore::InheritedFlags::setMinWidth):
    (WebCore::InheritedFlags::setMaxWidth):
    (WebCore::InheritedFlags::setMinHeight):
    (WebCore::InheritedFlags::setMaxHeight):
    (WebCore::InheritedFlags::setVerticalAlignLength):
    (WebCore::InheritedFlags::hasAutoZIndex):
    (WebCore::InheritedFlags::setHasAutoZIndex):
    (WebCore::InheritedFlags::zIndex):
    (WebCore::InheritedFlags::setZIndex):
    (WebCore::InheritedFlags::setBoxSizing):
    * rendering/style/StyleBoxData.cpp:
    (WebCore::StyleBoxData::StyleBoxData):
    (WebCore::StyleBoxData::operator==):
    * rendering/style/StyleBoxData.h:
    (WebCore::StyleBoxData::width):
    (WebCore::StyleBoxData::height):
    (WebCore::StyleBoxData::minWidth):
    (WebCore::StyleBoxData::minHeight):
    (WebCore::StyleBoxData::maxWidth):
    (WebCore::StyleBoxData::maxHeight):
    (WebCore::StyleBoxData::verticalAlign):
    (WebCore::StyleBoxData::zIndex):
    (WebCore::StyleBoxData::hasAutoZIndex):
    (WebCore::StyleBoxData::boxSizing):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57528 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/style/StyleBoxData.h
+++ third_party/WebKit/WebCore/rendering/style/StyleBoxData.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define StyleBoxData_h
 
 #include "Length.h"
+#include "RenderStyleConstants.h"
 #include <wtf/RefCounted.h>
 #include <wtf/PassRefPtr.h>
 
@@ -43,24 +44,42 @@ public:
         return !(*this == o);
     }
 
-    Length width;
-    Length height;
-
-    Length min_width;
-    Length max_width;
-
-    Length min_height;
-    Length max_height;
-
-    Length vertical_align;
-
-    int z_index;
-    bool z_auto : 1;
-    unsigned boxSizing : 1; // EBoxSizing
+    Length width() const { return m_width; }
+    Length height() const { return m_height; }
     
+    Length minWidth() const { return m_minWidth; }
+    Length minHeight() const { return m_minHeight; }
+    
+    Length maxWidth() const { return m_maxWidth; }
+    Length maxHeight() const { return m_maxHeight; }
+    
+    Length verticalAlign() const { return m_verticalAlign; }
+    
+    int zIndex() const { return m_zIndex; }
+    bool hasAutoZIndex() const { return m_hasAutoZIndex; }
+    
+    EBoxSizing boxSizing() const { return static_cast<EBoxSizing>(m_boxSizing); }
+
 private:
+    friend class RenderStyle;
+
     StyleBoxData();
     StyleBoxData(const StyleBoxData&);
+
+    Length m_width;
+    Length m_height;
+
+    Length m_minWidth;
+    Length m_maxWidth;
+
+    Length m_minHeight;
+    Length m_maxHeight;
+
+    Length m_verticalAlign;
+
+    int m_zIndex;
+    bool m_hasAutoZIndex : 1;
+    unsigned m_boxSizing : 1; // EBoxSizing
 };
 
 } // namespace WebCore
