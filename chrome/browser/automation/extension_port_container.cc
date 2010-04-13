dee88a1d47b06076055305a56bf71935b846e343
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-13 13:18:10 +0000

    Revert 44348 - Add support for restricting broadcasting events to extensions based on host permissions.
    BUG=38398
    TEST=none
    
    Review URL: http://codereview.chromium.org/1095002
    
    TBR=jochen@chromium.org
    Review URL: http://codereview.chromium.org/1575036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44349 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/extension_port_container.cc
+++ chrome/browser/automation/extension_port_container.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -115,8 +115,7 @@ bool ExtensionPortContainer::Send(IPC::Message *message) {
 void ExtensionPortContainer::OnExtensionMessageInvoke(
     const std::string& function_name,
     const ListValue& args,
-    bool requires_incognito_access,
-    const GURL& event_url) {
+    bool requires_incognito_access) {
   if (function_name == ExtensionMessageService::kDispatchOnMessage) {
     DCHECK_EQ(args.GetSize(), 2u);
 
