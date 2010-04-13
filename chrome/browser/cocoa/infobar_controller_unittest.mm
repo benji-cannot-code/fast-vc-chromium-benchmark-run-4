e60115fdff38a7da5518828f9f28f51429ecd260
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-13 21:37:57 +0000

    Mac: Let infobar text survive window resizes and fullscreen toggles.
    BUG=40604
    TEST=Do something that shows an infobar (e.g. go to youtube and kill flash). Switch to and from fullscreen, resize window. Infobar text should stay visible. For infobars with links (e.g. geolocation infobar), the link should still show a hand cursor.
    
    Review URL: http://codereview.chromium.org/1654004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44400 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/infobar_controller_unittest.mm
+++ chrome/browser/cocoa/infobar_controller_unittest.mm
@@ -18,11 +18,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface InfoBarController (ExposedForTesting)
 - (NSString*)labelString;
+- (NSRect)labelFrame;
 @end
 
 @implementation InfoBarController (ExposedForTesting)
 - (NSString*)labelString {
   return [label_.get() string];
+}
+- (NSRect)labelFrame {
+  return [label_.get() frame];
 }
 @end
 
@@ -144,6 +148,20 @@ TEST_F(AlertInfoBarControllerTest, DeallocController) {
   // InfoBarClosed() message to the delegate.
   controller_.reset(nil);
   EXPECT_FALSE(delegate_.closed);
+}
+
+TEST_F(AlertInfoBarControllerTest, ResizeView) {
+  NSRect originalLabelFrame = [controller_ labelFrame];
+
+  // Expand the view by 20 pixels and make sure the label frame changes
+  // accordingly.
+  const CGFloat width = 20;
+  NSRect newViewFrame = [[controller_ view] frame];
+  newViewFrame.size.width += width;
+  [[controller_ view] setFrame:newViewFrame];
+
+  NSRect newLabelFrame = [controller_ labelFrame];
+  EXPECT_EQ(NSWidth(newLabelFrame), NSWidth(originalLabelFrame) + width);
 }
 
 TEST_VIEW(LinkInfoBarControllerTest, [controller_ view]);
