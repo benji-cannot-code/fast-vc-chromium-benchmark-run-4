bd9000fd6e97474856250e683745017a3586c484
albertb@chromium.org <albertb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-13 22:23:10 +0000

    Refactor PasswordStoreDefault on Linux to store the logins in a LoginDatabase instead of using WebDataService. Once PasswordStoreWin is migrated as well, PasswordStoreLinux will likely be folded back into PasswordStoreDefault.
    BUG=35038
    TEST=PasswordStoreLinuxTest.*
    
    Review URL: http://codereview.chromium.org/1525026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44409 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profile.cc
+++ chrome/browser/profile.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/ssl_config_service_manager.h"
 #include "chrome/browser/notifications/desktop_notification_service.h"
 #include "chrome/browser/password_manager/password_store_default.h"
+#include "chrome/browser/password_manager/password_store_linux.h"
 #include "chrome/browser/privacy_blacklist/blacklist.h"
 #include "chrome/browser/profile_manager.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
@@ -1185,7 +1186,16 @@ void ProfileImpl::CreatePasswordStore() {
   // TODO(evanm): implement "native" password management.
   // This bug describes the issues.
   // http://code.google.com/p/chromium/issues/detail?id=12351
-  ps = new PasswordStoreDefault(GetWebDataService(Profile::IMPLICIT_ACCESS));
+  FilePath login_db_file_path = GetPath();
+  login_db_file_path = login_db_file_path.Append(chrome::kLoginDataFileName);
+  LoginDatabase* login_db = new LoginDatabase();
+  if (!login_db->Init(login_db_file_path)) {
+    LOG(ERROR) << "Could not initialize login database.";
+    delete login_db;
+    return;
+  }
+  ps = new PasswordStoreLinux(login_db, this,
+                              GetWebDataService(Profile::IMPLICIT_ACCESS));
   if (!ps->Init())
     return;
 #else
@@ -1193,7 +1203,7 @@ void ProfileImpl::CreatePasswordStore() {
 #endif
   if (!ps || !ps->Init()) {
     // Try falling back to the default password manager
-    LOG(WARNING) << "Could not initialise native password manager - "
+    NOTREACHED() << "Could not initialise native password manager - "
                     "falling back to default";
     ps = new PasswordStoreDefault(GetWebDataService(Profile::IMPLICIT_ACCESS));
     if (!ps->Init())
