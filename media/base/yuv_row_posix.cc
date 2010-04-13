209c14834157e6b6f973b462fdacdf01dcaa3626
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-13 01:32:33 +0000

    Changed horizontal scaler to start with 0 instead of 0.5. BUG=19113 TEST=Left border in WebKit/LayoutTests/media/content/test.ogv is clearly visible.
    Review URL: http://codereview.chromium.org/1650002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44311 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/yuv_row_posix.cc
+++ media/base/yuv_row_posix.cc
@@ -379,7 +379,7 @@ void LinearScaleYUVToRGB32Row(const uint8* y_buf,
                               int width,
                               int scaled_dx) {
   asm(
-  "movq   $32768,%%r11\n"
+  "xor    %%r11,%%r11\n"
   "sub    $0x2,%4\n"
   "js     .lscalenext\n"
 
@@ -639,7 +639,7 @@ void LinearScaleYUVToRGB32Row(const uint8* y_buf,
   "mov    0x24(%esp),%edx\n"
   "mov    0x28(%esp),%edi\n"
   "mov    0x30(%esp),%ebp\n"
-  "movl   $32768,%ebx\n"
+  "xor    %ebx,%ebx\n"
 
   // width = width * scaled_dx + ebx
   "mov    0x34(%esp), %ecx\n"
@@ -902,7 +902,7 @@ void PICLinearScaleYUVToRGB32Row(const uint8* y_buf,
   "mov    0x30(%esp),%ebp\n"
   "mov    0x34(%esp),%ecx\n"
   "mov    0x3c(%esp),%edi\n"
-  "movl   $32768,%ebx\n"
+  "xor    %ebx,%ebx\n"
 
   // width = width * scaled_dx + ebx
   "mov    0x34(%esp), %ecx\n"
