ec7a7eb7d8e912de1120d08036fe1d945ba6189d
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-13 22:23:56 +0000

    SecurityOrigin needs a way to remove individual OriginAccessEntries https://bugs.webkit.org/show_bug.cgi?id=37449
    Reviewed by Dave Hyatt.
    
    WebCore:
    
    Test: http/tests/xmlhttprequest/origin-whitelisting-removal.html
    
    * WebCore.base.exp: Added SecurityOrigin::removeOriginAccessWhitelistEntry.
    * page/OriginAccessEntry.h:
    (WebCore::OriginAccessEntry::protocol): Added. Returns m_protocol.
    (WebCore::OriginAccessEntry::host): Added. Returns m_host.
    (WebCore::OriginAccessEntry::subdomainSettings): Added. Returns m_subdomainSettings.
    (WebCore::operator==): Added. Compares OriginAccessEntry.
    (WebCore::operator!=): Ditto.
    * page/SecurityOrigin.cpp:
    (WebCore::SecurityOrigin::addOriginAccessWhitelistEntry): Use the add method to prevent a
    second hash lookup.
    (WebCore::SecurityOrigin::removeOriginAccessWhitelistEntry): Added. Find a matching
    OriginAccessEntry and remove it.
    * page/SecurityOrigin.h: Added removeOriginAccessWhitelistEntry.
    
    WebKit/mac:
    
    * WebView/WebView.mm:
    (+[WebView _removeOriginAccessWhitelistEntryWithSourceOrigin:destinationProtocol:destinationHost:allowDestinationSubdomains:]):
    Call SecurityOrigin::removeOriginAccessWhitelistEntry.
    * WebView/WebViewPrivate.h: Added _removeOriginAccessWhitelistEntryWithSourceOrigin.
    
    WebKit/win:
    
    * Interfaces/IWebViewPrivate.idl:
    * WebView.cpp:
    (WebView::removeOriginAccessWhitelistEntry): Call SecurityOrigin::removeOriginAccessWhitelistEntry.
    * WebView.h: Added removeOriginAccessWhitelistEntry.
    
    WebKitTools:
    
    * DumpRenderTree/LayoutTestController.cpp:
    (removeOriginAccessWhitelistEntryCallback): Added. Call LayoutTestController::removeOriginAccessWhitelistEntry.
    (LayoutTestController::staticFunctions): Added removeOriginAccessWhitelistEntry.
    * DumpRenderTree/LayoutTestController.h:
    * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
    (LayoutTestController::removeOriginAccessWhitelistEntry): Added. FIXME to implement.
    * DumpRenderTree/mac/LayoutTestControllerMac.mm:
    (LayoutTestController::removeOriginAccessWhitelistEntry): Added.
    * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
    (LayoutTestController::removeOriginAccessWhitelistEntry): Added. FIXME to implement.
    * DumpRenderTree/win/LayoutTestControllerWin.cpp:
    (LayoutTestController::removeOriginAccessWhitelistEntry): Added.
    * DumpRenderTree/wx/LayoutTestControllerWx.cpp:
    (LayoutTestController::whiteListAccessFromOrigin): FIXME to implement.
    (LayoutTestController::removeOriginAccessWhitelistEntry): Added. FIXME to implement.
    
    LayoutTests:
    
    * http/tests/xmlhttprequest/origin-whitelisting-removal-expected.txt: Added.
    * http/tests/xmlhttprequest/origin-whitelisting-removal.html: Added.
    * platform/gtk/Skipped: Added origin-whitelisting-removal.html.
    * platform/qt/Skipped: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57537 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/SecurityOrigin.h
+++ third_party/WebKit/WebCore/page/SecurityOrigin.h
@@ -196,6 +196,7 @@ public:
     static bool shouldTreatURLSchemeAsNoAccess(const String&);
 
     static void addOriginAccessWhitelistEntry(const SecurityOrigin& sourceOrigin, const String& destinationProtocol, const String& destinationDomains, bool allowDestinationSubdomains);
+    static void removeOriginAccessWhitelistEntry(const SecurityOrigin& sourceOrigin, const String& destinationProtocol, const String& destinationDomains, bool allowDestinationSubdomains);
     static void resetOriginAccessWhitelists();
 
 private:
--- third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.h
@@ -227,6 +227,7 @@ public:
     unsigned numberOfActiveAnimations() const;
 
     void addOriginAccessWhitelistEntry(JSStringRef sourceOrigin, JSStringRef destinationProtocol, JSStringRef destinationHost, bool allowDestinationSubdomains);
+    void removeOriginAccessWhitelistEntry(JSStringRef sourceOrigin, JSStringRef destinationProtocol, JSStringRef destinationHost, bool allowDestinationSubdomains);
 
     void addUserScript(JSStringRef source, bool runAtStart);
     void addUserStyleSheet(JSStringRef source);
