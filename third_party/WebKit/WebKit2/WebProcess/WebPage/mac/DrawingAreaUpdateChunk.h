3b4ee58398b181ccf187d2b07d6ee7b5df56d536
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-14 19:42:31 +0000

    2010-04-14  Anders Carlsson  <andersca@apple.com>
            Reviewed by Sam Weinig.
    
            Factor code to paint into an update chunk out into a separate function.
            https://bugs.webkit.org/show_bug.cgi?id=37594
    
            * WebProcess/WebPage/mac/DrawingAreaUpdateChunk.cpp:
            (WebKit::DrawingAreaUpdateChunk::paintIntoUpdateChunk):
            (WebKit::DrawingAreaUpdateChunk::display):
            (WebKit::DrawingAreaUpdateChunk::setSize):
            * WebProcess/WebPage/mac/DrawingAreaUpdateChunk.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57596 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebPage/mac/DrawingAreaUpdateChunk.h
+++ third_party/WebKit/WebKit2/WebProcess/WebPage/mac/DrawingAreaUpdateChunk.h
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
+class UpdateChunk;
+
 class DrawingAreaUpdateChunk : public DrawingArea {
 public:
     DrawingAreaUpdateChunk(WebPage*);
@@ -48,6 +50,8 @@ public:
     virtual void didReceiveMessage(CoreIPC::Connection*, CoreIPC::MessageID, CoreIPC::ArgumentDecoder&);
 
 private:
+    void paintIntoUpdateChunk(UpdateChunk*);
+    
     void scheduleDisplay();
     void setSize(const WebCore::IntSize& viewSize);
 
