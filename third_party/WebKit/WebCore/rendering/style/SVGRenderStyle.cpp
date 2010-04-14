b2405dca0218eb02feb6adecd01accc52dee5a86
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-14 11:00:41 +0000

    2010-04-14  Nikolas Zimmermann  <nzimmermann@rim.com>
            Not reviewed. Attempt to unbreak build - NodeRenderStyle.h is an _interessting_ concept...
    
            * rendering/style/SVGRenderStyle.cpp: Include 'NodeRenderStyle.h'
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57576 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/style/SVGRenderStyle.cpp
+++ third_party/WebKit/WebCore/rendering/style/SVGRenderStyle.cpp
@@ -34,8 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSPrimitiveValue.h"
 #include "CSSValueList.h"
 #include "IntRect.h"
-#include "RenderObject.h"
-#include "RenderStyle.h"
+#include "NodeRenderStyle.h"
 #include "SVGStyledElement.h"
 
 using namespace std;
