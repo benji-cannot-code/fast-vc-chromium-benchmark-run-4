d73b4a86617da340d1de5eaf7ff41e614c84b284
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-14 15:47:00 +0000

    Try to fix CrOS build.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44477 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/metadata_url_request.cc
+++ chrome/browser/net/metadata_url_request.cc
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char kMetadataScheme[] = "metadata";
-
 class MetadataRequestHandler : public URLRequestJob {
  public:
   explicit MetadataRequestHandler(URLRequest* request);
