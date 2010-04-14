25a062a73c541a9314f23f704fc4791d1d2052a6
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-14 17:24:51 +0000

    Improve plugin performance on the Mac
    This makes a few changes:
    - Switch to memcpy instead of CG drawing to copy pixels from the transport bitmap to the backing bitmap (this is a substantial perf win).
    - Send transparancy information to the WebPluginDelegate, since it's no longer equivalent to not having a background DIB on the Mac (and likely on the other platforms in the future).
    - Don't clear the transport DIB for non-transparent plugins, since they will be drawing over whatever is there anyway.
    
    BUG=41340
    TEST=Both transparent and non-transparent plugins should continue to draw correctly. Framerates for large CG plugins (e.g., in-window Netflix streaming) should be higher.
    
    Review URL: http://codereview.chromium.org/1629017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44490 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/plugin/webplugin_proxy.h
+++ chrome/plugin/webplugin_proxy.h
@@ -118,7 +118,8 @@ class WebPluginProxy : public webkit_glue::WebPlugin {
   void UpdateGeometry(const gfx::Rect& window_rect,
                       const gfx::Rect& clip_rect,
                       const TransportDIB::Handle& windowless_buffer,
-                      const TransportDIB::Handle& background_buffer
+                      const TransportDIB::Handle& background_buffer,
+                      bool transparent
 #if defined(OS_MACOSX)
                       ,
                       int ack_key
@@ -171,6 +172,7 @@ class WebPluginProxy : public webkit_glue::WebPlugin {
 
   // Variables used for desynchronized windowless plugin painting.  See note in
   // webplugin_delegate_proxy.h for how this works.
+  bool transparent_;
 #if defined(OS_MACOSX)
   scoped_ptr<TransportDIB> windowless_dib_;
   scoped_ptr<TransportDIB> background_dib_;
