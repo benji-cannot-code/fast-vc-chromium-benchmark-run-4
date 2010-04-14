470eadb57afece694ed5ef5b96c3b11236c437d4
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-14 21:30:41 +0000

    Add chrome.tabs.getCurrent() api call to extensions.
    This is primarily so that code running in an infobar can discover what tab the info is inserted in. It also works for code running directly in a TabContents. The call will return undefined if called from a background page or popup view.
    
    BUG=40428
    TEST=All tests should pass (additional tests included)
    
    Review URL: http://codereview.chromium.org/1521032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44542 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/extension_api_client_unittest.cc
+++ chrome/renderer/extensions/extension_api_client_unittest.cc
@@ -160,7 +160,7 @@ TEST_F(ExtensionAPIClientTest, GetWindow) {
                "windows.get", "2");
 }
 
-TEST_F(ExtensionAPIClientTest, GetCurentWindow) {
+TEST_F(ExtensionAPIClientTest, GetCurrentWindow) {
   ExpectJsFail("chrome.windows.getCurrent(function(){}, 20);",
                "Uncaught Error: Too many arguments.");
 
@@ -314,6 +314,21 @@ TEST_F(ExtensionAPIClientTest, GetTab) {
 
   ExpectJsPass("chrome.tabs.get(2, function(){})",
                "tabs.get", "2");
+}
+
+TEST_F(ExtensionAPIClientTest, GetCurrentTab) {
+  ExpectJsFail("chrome.tabs.getCurrent(function(){}, 20);",
+               "Uncaught Error: Too many arguments.");
+
+  ExpectJsFail("chrome.tabs.getCurrent();",
+               "Uncaught Error: Parameter 0 is required.");
+
+  ExpectJsFail("chrome.tabs.getCurrent('abc');",
+               "Uncaught Error: Invalid value for argument 0. "
+               "Expected 'function' but got 'string'.");
+
+  ExpectJsPass("chrome.tabs.getCurrent(function(){})",
+               "tabs.getCurrent", "null");
 }
 
 TEST_F(ExtensionAPIClientTest, DetectTabLanguage) {
