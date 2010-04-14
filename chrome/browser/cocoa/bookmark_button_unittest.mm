5a5bbcaabb74adfe0d7a065cca31bca7b3b2cc58
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-14 17:55:19 +0000

    [Mac] Adds a BookmarkButtonDelegate method to retrieve the top-level browser window.  Used to lock bar visibility when dragging.
    BUG=40740
    TEST=Go Fullscreen.  Drag a bookmark button that is inside a folder.  When the folder disappears, the overlay bar should stay visible.
    Review URL: http://codereview.chromium.org/1610023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44497 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_button_unittest.mm
+++ chrome/browser/cocoa/bookmark_button_unittest.mm
@@ -37,6 +37,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BOOL)dragShouldLockBarVisibility {
   return NO;
 }
+
+- (NSWindow*)browserWindow {
+  return nil;
+}
 @end
 
 namespace {
