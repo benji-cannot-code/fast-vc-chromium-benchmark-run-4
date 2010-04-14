843db6dcb4fc4ad101d1f8ee586068519af0f2bf
antonm@chromium.org <antonm@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-14 12:12:28 +0000

    2010-04-14  Anton Muhin  <antonm@chromium.org>
            Not review, build fix.
    
            Add <limits.h> include to bring UINT_MAX.
    
            * html/canvas/WebGLArray.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/WebGLArray.h
+++ third_party/WebKit/WebCore/html/canvas/WebGLArray.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include "ExceptionCode.h"
+#include <limits.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
 #include <wtf/RefPtr.h>
