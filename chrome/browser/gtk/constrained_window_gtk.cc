942c54974a3680f04ec3104839e27ac1ae0cc691
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-14 20:15:29 +0000

    Fixed ESC handling on constrained dialogs on GTK. We used to stop the entire page navigation previously instead of continuing with other page elements if some can't be loaded. This change makes GTK implementation consistent with other platforms.
    BUG=27585
    TEST=go to http://www/~thakis/cgi-bin/test.html, press ESC when login dialog appears, observe broken image #1, press ESC on the next dialog, observe broken image #2.
    
    Review URL: http://codereview.chromium.org/1627022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44527 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/constrained_window_gtk.cc
+++ chrome/browser/gtk/constrained_window_gtk.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/gtk/constrained_window_gtk.h"
 
+#include <gdk/gdkkeysyms.h>
+
+#include "chrome/browser/browser_list.h"
 #include "chrome/browser/gtk/gtk_util.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/tab_contents/tab_contents_view_gtk.h"
@@ -13,7 +16,8 @@ ConstrainedWindowGtk::ConstrainedWindowGtk(
     TabContents* owner, ConstrainedWindowGtkDelegate* delegate)
     : owner_(owner),
       delegate_(delegate),
-      visible_(false) {
+      visible_(false),
+      accel_group_(gtk_accel_group_new()) {
   DCHECK(owner);
   DCHECK(delegate);
   GtkWidget* dialog = delegate->GetWidgetRoot();
@@ -31,10 +35,18 @@ ConstrainedWindowGtk::ConstrainedWindowGtk(
   gtk_container_add(GTK_CONTAINER(frame), alignment);
   gtk_container_add(GTK_CONTAINER(ebox), frame);
   border_.Own(ebox);
+  ConnectAccelerators();
 }
 
 ConstrainedWindowGtk::~ConstrainedWindowGtk() {
   border_.Destroy();
+
+  gtk_accel_group_disconnect_key(accel_group_, GDK_Escape,
+                                 static_cast<GdkModifierType>(0));
+  gtk_window_remove_accel_group(
+      GTK_WINDOW(ContainingView()->GetTopLevelNativeWindow()),
+      accel_group_);
+  g_object_unref(accel_group_);
 }
 
 void ConstrainedWindowGtk::ShowConstrainedWindow() {
@@ -60,9 +72,32 @@ TabContentsViewGtk* ConstrainedWindowGtk::ContainingView() {
   return static_cast<TabContentsViewGtk*>(owner_->view());
 }
 
+void ConstrainedWindowGtk::ConnectAccelerators() {
+  gtk_accel_group_connect(accel_group_,
+                          GDK_Escape, static_cast<GdkModifierType>(0),
+                          static_cast<GtkAccelFlags>(0),
+                          g_cclosure_new(G_CALLBACK(OnEscapeThunk),
+                                         this, NULL));
+  gtk_window_add_accel_group(
+      GTK_WINDOW(ContainingView()->GetTopLevelNativeWindow()),
+      accel_group_);
+}
+
+
+gboolean ConstrainedWindowGtk::OnEscape() {
+  // Handle this accelerator only if this is on the currently selected tab.
+  Browser* browser = BrowserList::GetLastActive();
+  if (!browser || browser->GetSelectedTabContents() != owner_)
+    return FALSE;
+
+  CloseConstrainedWindow();
+  return TRUE;
+}
+
 // static
 ConstrainedWindow* ConstrainedWindow::CreateConstrainedDialog(
     TabContents* parent,
     ConstrainedWindowGtkDelegate* delegate) {
   return new ConstrainedWindowGtk(parent, delegate);
 }
+
