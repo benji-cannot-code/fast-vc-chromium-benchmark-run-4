b8e16d9df56383680490d4709cf12a622c4d3569
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-15 18:06:39 +0000

    Add automation hooks to get info about downloads.
    Makes info about downloads available to pyauto.
    Also add JSONInterfaceError to represent an error in the json ipc interace.
    
    TEST=None
    BUG=None
    
    Review URL: http://codereview.chromium.org/1637008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44664 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/pyautolib/pyauto_errors.py
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""PyAuto Errors."""
+
+class JSONInterfaceError(RuntimeError):
+  """Represent an error in the JSON ipc interface."""
+  pass
