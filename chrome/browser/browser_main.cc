79c1612e36d2888f55600c3378b416ea008f083b
huanr@chromium.org <huanr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-15 18:14:37 +0000

    Start up extension event routers later in BrowserMain, so bookmarks is loaded after bookmark import in first run.
    I am not familiar with the extension code. It seems currently
    ExtensionHistoryEventRouter and ExtensionAccessibilityEventRouter
    are always initialized while ExtensionBrowserEventRouter
    and ExtensionBookmarkEventRouter are initialized on demand. Since
    we have a build in bookmark manager extension, I start all event
    routers by default in this change. Let me know if this is an
    issue.
    
    BUG=40144
    TEST=chrome.exe --first-run
    
    Review URL: http://codereview.chromium.org/1578032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44668 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/dom_ui/chrome_url_data_manager.h"
 #include "chrome/browser/extensions/extension_protocols.h"
+#include "chrome/browser/extensions/extensions_service.h"
 #include "chrome/browser/first_run.h"
 #include "chrome/browser/jankometer.h"
 #include "chrome/browser/metrics/histogram_synchronizer.h"
@@ -1139,6 +1140,10 @@ int BrowserMain(const MainFunctionParams& parameters) {
 #if defined(OS_CHROMEOS)
   metrics->StartExternalMetrics(profile);
 #endif
+
+  // This will initialize bookmarks. Call it after bookmark import is done.
+  // See issue 40144.
+  profile->GetExtensionsService()->InitEventRouters();
 
   int result_code = ResultCodes::NORMAL_EXIT;
   if (parameters.ui_task) {
