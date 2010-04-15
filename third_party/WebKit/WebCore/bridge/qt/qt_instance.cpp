5568488dad3b115f5387557487a35f00b69e5b5e
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-15 10:03:31 +0000

    2010-04-15  Bruno Schmidt  <bruno.schmidt@gmail.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Null QObjects properties cause Segmentation Fault
            https://bugs.webkit.org/show_bug.cgi?id=34730
    
            QObjects exported to the QWebkit javascript with properties that are
            a null "QObject*" cause Segmentation Fault.
    
            If an QObject is added to the javascript context and it contains
            properties of the type QObject* with NULL value, calling the property
            causes Segmentation Fault.
            So now the code below properly checks for null pointers:
    
            * bridge/qt/qt_instance.cpp:
            (JSC::Bindings::QtInstance::getClass): may return NULL
            (JSC::Bindings::QtInstance::getMethod): may return jsNull()
            (JSC::Bindings::QtInstance::stringValue): may return jsNull()
            (JSC::Bindings::QtInstance::booleanValue): may return false
            * bridge/qt/qt_runtime.cpp:
            (JSC::Bindings::convertValueToQVariant):
            (JSC::Bindings::convertQVariantToValue): May return jsNull on QObjectStar
    2010-04-15  Bruno Schmidt  <bruno.schmidt@gmail.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Null QObjects properties cause Segmentation Fault
            https://bugs.webkit.org/show_bug.cgi?id=34730
    
            QObjects exported to the QWebkit javascript with properties that are
            a null "QObject*" cause Segmentation Fault.
    
            If an QObject is added to the javascript context and it contains
            properties of the type QObject* with NULL value, calling the property
            causes Segmentation Fault.
    
            Follow the tests for the corrections done over WebCore.
    
            * tests/qwebframe/tst_qwebframe.cpp:
            (MyQObject::MyQObject): init the field m_objectStar
            (MyQObject::objectStarProperty): read the Object* prop
            (MyQObject::setObjectStarProperty): write the Object* prop
            (tst_QWebFrame::getSetStaticProperty): new tests for the new prop
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57638 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_instance.cpp
+++ third_party/WebKit/WebCore/bridge/qt/qt_instance.cpp
@@ -172,6 +172,8 @@ QtInstance* QtInstance::getInstance(JSObject* object)
 
 Class* QtInstance::getClass() const
 {
+    if (!m_object)
+        return 0;
     if (!m_class)
         m_class = QtClass::classForObject(m_object);
     return m_class;
@@ -239,7 +241,9 @@ void QtInstance::getPropertyNames(ExecState* exec, PropertyNameArray& array)
 
 JSValue QtInstance::getMethod(ExecState* exec, const Identifier& propertyName)
 {
-    MethodList methodList = getClass()->methodsNamed(propertyName, this);
+    if (!getClass())
+        return jsNull();
+    MethodList methodList = m_class->methodsNamed(propertyName, this);
     return new (exec) RuntimeMethod(exec, propertyName, methodList);
 }
 
@@ -260,12 +264,15 @@ JSValue QtInstance::defaultValue(ExecState* exec, PreferredPrimitiveType hint) c
 
 JSValue QtInstance::stringValue(ExecState* exec) const
 {
+    QObject* obj = getObject();
+    if (!obj)
+        return jsNull();
+
     // Hmm.. see if there is a toString defined
     QByteArray buf;
     bool useDefault = true;
     getClass();
-    QObject* obj = getObject();
-    if (m_class && obj) {
+    if (m_class) {
         // Cheat and don't use the full name resolution
         int index = obj->metaObject()->indexOfMethod("toString()");
         if (index >= 0) {
@@ -310,7 +317,7 @@ JSValue QtInstance::numberValue(ExecState* exec) const
 JSValue QtInstance::booleanValue() const
 {
     // ECMA 9.2
-    return jsBoolean(true);
+    return jsBoolean(getObject());
 }
 
 JSValue QtInstance::valueOf(ExecState* exec) const
