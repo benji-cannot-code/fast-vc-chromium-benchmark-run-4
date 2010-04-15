3b0977be37407a6ddf3bc832d6887d88f503fe1c
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-15 23:51:26 +0000

    Fix Windows WebKit2 build.
    WebKit2: 
    
    Patch by Adam Roben <aroben@apple.com> on 2010-04-15
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::processDidExit):
    * win/WebKit2Generated.make:
    
    WebKitTools: 
    
    Patch by Adam Roben <aroben@apple.com> on 2010-04-15
    * MiniBrowser/win/MiniBrowser.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/MiniBrowser/win/MiniBrowser.cpp
+++ third_party/WebKit/WebKitTools/MiniBrowser/win/MiniBrowser.cpp
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include "stdafx.h"
+
 #include "BrowserWindow.h"
 #include "MiniBrowser.h"
 #include <assert.h>
