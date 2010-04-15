72d32c7b4d4cd55e7b9f897e73c79095f5d9a179
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-15 21:15:45 +0000

    2010-04-15  Anders Carlsson  <andersca@apple.com>
            Reviewed by Sam Weinig.
    
            Start the mini browser in threaded mode if shift is pressed during startup.
            https://bugs.webkit.org/show_bug.cgi?id=37670
    
            * MiniBrowser/mac/AppDelegate.m:
            (-[BrowserAppDelegate init]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57675 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/MiniBrowser/mac/AppDelegate.m
+++ third_party/WebKit/WebKitTools/MiniBrowser/mac/AppDelegate.m
@@ -35,9 +35,13 @@ static const WKProcessModel defaultProcessModel = kWKProcessModelSecondaryProces
 - (id)init
 {
     self = [super init];
-    if (self)
-        currentProcessModel = defaultProcessModel;
-    
+    if (self) {
+        if ([NSEvent modifierFlags] & NSShiftKeyMask)
+            currentProcessModel = kWKProcessModelSecondaryThread;
+        else
+            currentProcessModel = kWKProcessModelSecondaryProcess;
+    }
+
     return self;
 }
 
