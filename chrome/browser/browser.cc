749f09d6d9884667e713f5c0dbf33a2f3a93e751
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-15 22:03:25 +0000

    Merge the --enable-app-launcher flag with the --enable- extension-apps flag since we will be using them together.
    Review URL: http://codereview.chromium.org/1659004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44712 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -1064,7 +1064,7 @@ void Browser::NewTab() {
   UserMetrics::RecordAction(UserMetricsAction("NewTab"), profile_);
 #if defined(OS_WIN)
   if (CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kAppLauncherForNewTab)) {
+          switches::kEnableExtensionApps)) {
     AppLauncher::ShowForNewTab(this);
     return;
   }
