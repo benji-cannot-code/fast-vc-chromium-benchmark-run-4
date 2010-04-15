fe36451eb26560c12a9e5d76515e30298768c1e5
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-15 11:51:21 +0000

    2010-04-15  Gyuyoung Kim  <gyuyoung.kim@samsung.com>
            Reviewed by Eric Seidel.
    
            Duplicated patches related to wml were pushed to trunk. So, one of
            the patches should be reverted.
            https://bugs.webkit.org/show_bug.cgi?id=37542
    
            * wml/WMLOptionElement.h:
            * wml/WMLSelectElement.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57639 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/wml/WMLSelectElement.h
+++ third_party/WebKit/WebCore/wml/WMLSelectElement.h
@@ -89,8 +89,6 @@ public:
     bool initialized() const { return m_initialized; }
     
     virtual void listBoxSelectItem(int listIndex, bool allowMultiplySelections, bool shift, bool fireOnChangeNow = true);
-
-    virtual void listBoxSelectItem(int listIndex, bool allowMultiplySelections, bool shift, bool fireOnChangeNow = true) { return; }
 private:
     virtual void insertedIntoTree(bool);
 
