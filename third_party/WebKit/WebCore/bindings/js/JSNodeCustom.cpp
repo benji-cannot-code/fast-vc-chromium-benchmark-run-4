3ee10cce48a2d3da46b55b0d5d1f87598b96d68a
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-15 03:37:43 +0000

    2010-04-14  Adam Barth  <abarth@webkit.org>
            Unreviewed attempt to fix Qt build.
    
            * bindings/js/JSNodeCustom.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSNodeCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSNodeCustom.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DocumentType.h"
 #include "Entity.h"
 #include "EntityReference.h"
+#include "ExceptionCode.h"
 #include "HTMLElement.h"
 #include "JSAttr.h"
 #include "JSCDATASection.h"
