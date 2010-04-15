594d8cdef335f25d94cb5fe8252dc35310d88f7c
jcivelli@google.com <jcivelli@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-15 20:00:00 +0000

    Making the app launcher use a DOMView. This makes the contents now work properly.
    BUG=41270
    TEST=Start Chrome with --app-launcher-new-tab --enable-extension-apps and
         some apps installed. Open a new tab, the app launcher should show.
         Click on an app, it should be opened in a new tab.
    
    Review URL: http://codereview.chromium.org/1619020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44688 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/dom_view.h
+++ chrome/browser/views/dom_view.h
@@ -20,8 +20,7 @@ class TabContents;
 
 class DOMView : public views::NativeViewHost {
  public:
-  // Construct a DOMView to display the given data: URL.
-  explicit DOMView();
+  DOMView();
   virtual ~DOMView();
 
   // Initialize the view, creating the contents. This should be
@@ -33,6 +32,9 @@ class DOMView : public views::NativeViewHost {
 
   // Loads the given URL into the page. You must have previously called Init().
   void LoadURL(const GURL& url);
+
+  // The tab contents displaying the actual contents.
+  TabContents* tab_contents() const { return tab_contents_.get(); }
 
  protected:
   // Overridden from View.
