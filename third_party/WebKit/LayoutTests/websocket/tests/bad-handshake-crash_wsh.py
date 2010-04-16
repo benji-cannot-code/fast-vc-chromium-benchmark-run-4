c47917b5467eba1b42249c5aa87d266cbe075db6
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-16 23:13:30 +0000

    2010-04-16  Fumitoshi Ukai  <ukai@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            WebSocket crash when it receives bad header.
            https://bugs.webkit.org/show_bug.cgi?id=37682
    
            * websocket/tests/bad-handshake-crash-expected.txt: Added.
            * websocket/tests/bad-handshake-crash.html: Added.
            * websocket/tests/bad-handshake-crash_wsh.py: Added.
            * websocket/tests/script-tests/bad-handshake-crash.js: Added.
    2010-04-16  Fumitoshi Ukai  <ukai@chromium.org>
    
            Reviewed by Alexey Proskuryakov.
    
            WebSocket crash when it receives bad header.
            https://bugs.webkit.org/show_bug.cgi?id=37682
    
            If name or value is not valid UTF-8, nameStr or valueStr would be
            null string, so crashed in headers->add(nameStr, valueStr).
            Check both nameStr and valueStr are not null string.
            Otherwise handshake will fail.
    
            Test: websocket/tests/bad-handshake-crash.html
    
            * websockets/WebSocketHandshake.cpp:
            (WebCore::WebSocketHandshake::readHTTPHeaders): check nameStr and valueStr are not null string.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57760 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/websocket/tests/bad-handshake-crash_wsh.py
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def web_socket_do_extra_handshake(request):
+  msg = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
+  msg += "Upgrade: WebSocket\r\n"
+  msg += "Connection: Upgrade\r\n"
+  msg += "\xa5:\r\n"
+  msg += "\r\n"
+  request.connection.write(msg)
+  print msg
+
+def web_socket_transfer_data(request):
+  pass
