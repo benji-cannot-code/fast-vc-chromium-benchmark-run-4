06ec430326c9aed383b21707ab0ce5ce2f23b562
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-16 23:39:26 +0000

    Use the new tab page instead of chrome://apps/ in the app launcher
    BUG=None
    TEST=Start chrome with --enable-extension-apps --app-launcher-new-tab. Pressing Ctrl+T should show the NTP in the overlay.
    
    Review URL: http://codereview.chromium.org/1660005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44860 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -50,7 +50,6 @@ extern const char kAboutTermsURL[];
 
 // chrome: URLs (including schemes). Should be kept in sync with the
 // components below.
-extern const char kChromeUIAppsURL[];
 extern const char kChromeUIBookmarksURL[];
 extern const char kChromeUIDevToolsURL[];
 extern const char kChromeUIDownloadsURL[];
@@ -66,7 +65,6 @@ extern const char kChromeUINewTabURL[];
 
 // chrome components of URLs. Should be kept in sync with the full URLs
 // above.
-extern const char kChromeUIAppsHost[];
 extern const char kChromeUIBookmarksHost[];
 extern const char kChromeUIDevToolsHost[];
 extern const char kChromeUIDialogHost[];
