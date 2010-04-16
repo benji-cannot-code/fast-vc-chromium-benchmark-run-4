e93d6e1fcf1c6ce649af50bb933e74d4e94f2e45
dhg@chromium.org <dhg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-16 19:41:56 +0000

    fixing issues with filebrowser namely, namely allowing it to grow on mount, and fixing download cases so the flow is better.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/1527036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44808 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/filebrowse_ui.cc
+++ chrome/browser/dom_ui/filebrowse_ui.cc
@@ -63,8 +63,6 @@ static const std::string kPicasawebBaseUrl = "http://picasaweb.google.com/";
 static const char* kFilebrowseURLHash = "chrome://filebrowse#";
 static const int kPopupLeft = 0;
 static const int kPopupTop = 0;
-static const int kPopupWidth = 250;
-static const int kPopupHeight = 300;
 
 class FileBrowseUIHTMLSource : public ChromeURLDataManager::DataSource {
  public:
@@ -960,7 +958,9 @@ FileBrowseUI::FileBrowseUI(TabContents* contents) : HtmlDialogUI(contents) {
 
 // static
 Browser* FileBrowseUI::OpenPopup(Profile* profile,
-                                 const std::string& hashArgument) {
+                                 const std::string& hashArgument,
+                                 int width,
+                                 int height) {
   // Get existing pop up for given hashArgument.
   Browser* browser = GetPopupForPath(hashArgument);
 
@@ -976,8 +976,8 @@ Browser* FileBrowseUI::OpenPopup(Profile* profile,
         true, -1, false, NULL);
     browser->window()->SetBounds(gfx::Rect(kPopupLeft,
                                            kPopupTop,
-                                           kPopupWidth,
-                                           kPopupHeight));
+                                           width,
+                                           height));
 
     browser->window()->Show();
   }
@@ -1002,3 +1002,8 @@ Browser* FileBrowseUI::GetPopupForPath(const std::string& path) {
 
   return NULL;
 }
+
+const int FileBrowseUI::kPopupWidth = 250;
+const int FileBrowseUI::kPopupHeight = 300;
+const int FileBrowseUI::kSmallPopupWidth = 250;
+const int FileBrowseUI::kSmallPopupHeight = 50;
