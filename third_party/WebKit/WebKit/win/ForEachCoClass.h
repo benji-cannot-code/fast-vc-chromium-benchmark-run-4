0f33facb2f527af1476428f274615d32330a12a3
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-16 18:40:10 +0000

    Make it possible for clients to instantiate a WebUserContentURLPattern
    Reviewed by Tim Hatcher.
    
    * ForEachCoClass.h: Added WebUserContentURLPattern to the FOR_EACH_COCLASS macro, which
    lists all our instantiatable classes.
    
    * WebKitClassFactory.cpp: Added a now-required #include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/ForEachCoClass.h
+++ third_party/WebKit/WebKit/win/ForEachCoClass.h
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     macro(WebScriptWorld) \
     macro(WebGeolocationPosition) \
     macro(WebSerializedJSValue) \
+    macro(WebUserContentURLPattern) \
     // end of macro
 
 // Everything below this point is deprecated. Please do not use.
--- third_party/WebKit/WebKit/win/WebKitClassFactory.cpp
+++ third_party/WebKit/WebKit/win/WebKitClassFactory.cpp
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebURLCredential.h"
 #include "WebURLProtectionSpace.h"
 #include "WebURLResponse.h"
+#include "WebUserContentURLPattern.h"
 #include "WebView.h"
 #include "WebWorkersPrivate.h"
 #pragma warning(push, 0)
