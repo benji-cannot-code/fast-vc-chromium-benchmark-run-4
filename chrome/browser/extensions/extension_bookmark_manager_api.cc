31d9add427cc4b1ea4c76c632fb1783f77a2f9b3
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-16 18:06:27 +0000

    Fixes bug where GetSubtreeBookmarkManagerFunction would crash if the node no longer exists.
    BUG=41630
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/1521036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44798 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_bookmark_manager_api.cc
+++ chrome/browser/extensions/extension_bookmark_manager_api.cc
@@ -418,6 +418,10 @@ bool GetSubtreeBookmarkManagerFunction::RunImpl() {
     }
     node = model->GetNodeByID(id);
   }
+  if (!node) {
+    error_ = keys::kNoNodeError;
+    return false;
+  }
   scoped_ptr<ListValue> json(new ListValue());
   if (folders_only) {
     extension_bookmark_helpers::AddNodeFoldersOnly(node,
