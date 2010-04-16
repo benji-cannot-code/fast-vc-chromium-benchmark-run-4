b9aabe7358b962e181db8a746257c92102f53d96
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-16 20:34:44 +0000

    * WebFrame.cpp: (WebFrame::stringByEvaluatingJavaScriptInScriptWorld): * WebView.cpp: (WebView::stringByEvaluatingJavaScriptFromString):
    Reviewed by NOBODY (Windows build fix).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57740 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -3065,7 +3065,7 @@ HRESULT STDMETHODCALLTYPE WebView::stringByEvaluatingJavaScriptFromString(
     else if (scriptExecutionResult.isString()) {
         JSLock lock(JSC::SilenceAssertionsOnly);
         JSC::ExecState* exec = coreFrame->script()->globalObject(mainThreadNormalWorld())->globalExec();
-        *result = BString(String(scriptExecutionResult.getString(exec)));
+        *result = BString(ustringToString(scriptExecutionResult.getString(exec)));
     }
 
     return S_OK;
