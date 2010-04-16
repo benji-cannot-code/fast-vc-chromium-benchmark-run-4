b606ecf680abb9ebb6b9896ab50aac80bfaa00ce
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-16 00:10:48 +0000

    2010-04-15  Eric Uhrhane  <ericu@chromium.org>
            Reviewed by Dmitry Titov.
    
            Add bindings for async DB API in Workers.
            https://bugs.webkit.org/show_bug.cgi?id=34992
    
            * storage/change-version-handle-reuse.html: Refactored to share code with the worker version of the test.
            * storage/change-version-handle-reuse-expected.html: Removed reference to reloading, which this test doesn't do.
            * storage/change-version-handle-reuse.js: This is the extracted shared core of the test.
            * storage/execute-sql-args.html: Refactored to share code with the worker version of the test.
            * storage/execute-sql-args.js: This is the extracted shared core of the test.
    
            These are the worker versions of the tests; they're tiny shims around shared code.
            * fast/workers/execute-sql-args-worker-expected.txt: Added.
            * fast/workers/execute-sql-args-worker.html: Added.
            * fast/workers/change-version-handle-reuse-worker-expected.txt: Added.
            * fast/workers/change-version-handle-reuse-worker.html: Added.
            This file gets included from worker database tests; it does all the boilerplate that's needed on the DOM side.
            * fast/workers/resources/database-worker-controller.js: Added.
            This file is the boilerplate for the worker side of the database tests.  It will import and run the actual test on command, and will report back completion and exceptions.
            * fast/workers/resources/database-worker.js: Added.
    2010-04-15  Eric Uhrhane  <ericu@chromium.org>
    
            Reviewed by Dmitry Titov.
    
            Add bindings for async DB API in Workers.
            https://bugs.webkit.org/show_bug.cgi?id=34992
    
            Tests: storage/change-version-handle-reuse-worker.html
                   storage/execute-sql-args-worker.html
    
            * bindings/js/JSWorkerContextCustom.cpp: Add openDatabase binding.
            (WebCore::JSWorkerContext::openDatabase):
    
            * bindings/v8/custom/V8WorkerContextCustom.cpp: Add openDatabase stub; Chromium will need work both in V8 and in the browser process before we can turn this on there.
            (WebCore::V8WorkerContext::openDatabaseCallback):
    
            Add NoStaticTables flags to all objects now shared with workers.
            * storage/Database.idl:
            * storage/SQLError.idl:
            * storage/SQLResultSet.idl:
            * storage/SQLResultSetRowList.idl:
            * storage/SQLTransaction.idl:
    
            * workers/WorkerContext.h: Add databaseExceededQuota.
            * workers/WorkerContext.cpp:
            (WebCore::WorkerContext::databaseExceededQuota): Add stub implementation for testing; you just get 5MB for now.
            (WebCore::WorkerContext::openDatabase): Remove invalid assertion.
    
            Add the IDL for the call to openDatabase.
            * workers/WorkerContext.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57688 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSWorkerContextCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSWorkerContextCustom.cpp
@@ -30,6 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "JSWorkerContext.h"
 
+#include "Database.h"
+#include "JSDatabase.h"
+#include "JSDatabaseCallback.h"
 #include "JSDOMBinding.h"
 #include "JSDOMGlobalObject.h"
 #include "JSEventListener.h"
@@ -163,6 +166,24 @@ JSValue JSWorkerContext::messageChannel(ExecState* exec) const
 }
 #endif
 
+#if ENABLE(DATABASE)
+JSValue JSWorkerContext::openDatabase(ExecState* exec, const ArgList& args) 
+{ 
+    const UString& name = args.at(0).toString(exec); 
+    const UString& version = args.at(1).toString(exec); 
+    const UString& displayName = args.at(2).toString(exec); 
+    unsigned long estimatedSize = args.at(3).toInt32(exec); 
+    RefPtr<DatabaseCallback> creationCallback; 
+    if ((args.size() >= 5) && args.at(4).isObject()) 
+        creationCallback = JSDatabaseCallback::create(asObject(args.at(4)), globalObject()); 
+ 
+    ExceptionCode ec = 0; 
+    JSValue result = toJS(exec, globalObject(), WTF::getPtr(impl()->openDatabase(name, version, displayName, estimatedSize, creationCallback.release(), ec))); 
+    setDOMException(exec, ec); 
+    return result; 
+} 
+#endif
+ 
 } // namespace WebCore
 
 #endif // ENABLE(WORKERS)
--- third_party/WebKit/WebCore/workers/WorkerContext.cpp
+++ third_party/WebKit/WebCore/workers/WorkerContext.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WorkerContext.h"
 
 #include "ActiveDOMObject.h"
+#include "DatabaseTracker.h"
 #include "DOMTimer.h"
 #include "DOMWindow.h"
 #include "Database.h"
@@ -276,11 +277,19 @@ PassRefPtr<Database> WorkerContext::openDatabase(const String& name, const Strin
         return 0;
     }
 
-    ASSERT(Database::isAvailable());
     if (!Database::isAvailable())
         return 0;
 
     return Database::openDatabase(this, name, version, displayName, estimatedSize, creationCallback, ec);
+}
+
+void WorkerContext::databaseExceededQuota(const String&)
+{
+#if !PLATFORM(CHROMIUM)
+    // FIXME: This needs a real implementation; this is a temporary solution for testing.
+    const unsigned long long defaultQuota = 5 * 1024 * 1024;
+    DatabaseTracker::tracker().setQuota(securityOrigin(), defaultQuota);
+#endif
 }
 #endif
 
