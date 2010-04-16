b83f3663402857a5d72ebece12667a84c9fd6566
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-16 20:52:03 +0000

    Make sure to update the current graphics context when calling out to AppKit.
    Reviewed by Sam Weinig.
    
    * platform/mac/ThemeMac.mm:
    (WebCore::paintCheckbox):
    (WebCore::paintRadio):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/ThemeMac.mm
+++ third_party/WebKit/WebCore/platform/mac/ThemeMac.mm
@@ -214,6 +214,7 @@ static void paintCheckbox(ControlStates states, GraphicsContext* context, const 
 
     // Determine the width and height needed for the control and prepare the cell for painting.
     NSButtonCell *checkboxCell = checkbox(states, zoomedRect, zoomFactor);
+    LocalCurrentGraphicsContext localContext(context);
 
     context->save();
 
@@ -291,6 +292,7 @@ static void paintRadio(ControlStates states, GraphicsContext* context, const Int
 {
     // Determine the width and height needed for the control and prepare the cell for painting.
     NSButtonCell *radioCell = radio(states, zoomedRect, zoomFactor);
+    LocalCurrentGraphicsContext localContext(context);
 
     context->save();
 
