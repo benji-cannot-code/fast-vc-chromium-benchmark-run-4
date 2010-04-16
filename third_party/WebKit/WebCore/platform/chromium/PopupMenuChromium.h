86f8ca576c20ff24f988e56f7a90af9e60cfd850
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-16 18:07:57 +0000

    2010-04-16  Jian Li  <jianli@chromium.org>
            Reviewed by Dmitry Titov.
    
            Update FormDataList to fix style violations in old code.
            https://bugs.webkit.org/show_bug.cgi?id=37689
    
            * html/FormDataList.h:
            (WebCore::FormDataList::appendData):
            (WebCore::FormDataList::appendBlob):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57727 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/PopupMenuChromium.h
+++ third_party/WebKit/WebCore/platform/chromium/PopupMenuChromium.h
@@ -178,9 +178,6 @@ public:
 
     PopupType popupType() const { return m_popupType; }
 
-    // Whether the popup is currently open.
-    bool m_popupOpen;
-
 private:
     friend class WTF::RefCounted<PopupContainer>;
 
@@ -198,6 +195,8 @@ private:
 
     PopupContainerSettings m_settings;
     PopupType m_popupType;
+    // Whether the popup is currently open.
+    bool m_popupOpen;
 };
 
 } // namespace WebCore
