d6a4df5d9fc8c5804d25a97a2bb1b8aef786f515
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-17 06:14:13 +0000

    Unreviewed, rolling out r57770. http://trac.webkit.org/changeset/57770 https://bugs.webkit.org/show_bug.cgi?id=37746
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2010-04-16
    8 test cases crashed (Requested by Ossy on #webkit).
    
    WebCore: 
    
    * platform/graphics/qt/GraphicsLayerQt.cpp:
    (WebCore::AnimationQtBase::AnimationQtBase):
    (WebCore::TransformAnimationQt::~TransformAnimationQt):
    (WebCore::TransformAnimationQt::applyFrame):
    (WebCore::OpacityAnimationQt::applyFrame):
    (WebCore::GraphicsLayerQt::addAnimation):
    
    LayoutTests: 
    
    * platform/qt/Skipped:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsLayerQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsLayerQt.cpp
@@ -996,7 +996,6 @@ public:
         , m_webkitPropertyID(values.property())
         , m_webkitAnimation(anim)
         , m_keyframesName(name)
-        , m_fillsForwards(false)
     {
     }
 
@@ -1020,7 +1019,6 @@ public:
     // we might need this in case the same animation is added again (i.e. resumed by WebCore)
     const Animation* m_webkitAnimation;
     QString m_keyframesName;
-    bool m_fillsForwards;
 };
 
 // we'd rather have a templatized QAbstractAnimation than QPropertyAnimation / QVariantAnimation;
@@ -1105,10 +1103,10 @@ public:
 
     ~TransformAnimationQt()
     {
-        if (m_fillsForwards)
-            setCurrentTime(1);
-        else
-            m_layer.data()->setBaseTransform(m_layer.data()->m_layer->transform());
+        // this came up during the compositing/animation LayoutTests
+        // when the animation dies, the transform has to go back to default
+        if (m_layer)
+            m_layer.data()->updateTransform();
     }
 
     // the idea is that we let WebCore manage the transform-operations
@@ -1147,8 +1145,6 @@ public:
             }
         }
         m_layer.data()->setBaseTransform(transformMatrix);
-        if (m_fillsForwards)
-            m_layer.data()->m_layer->setTransform(m_layer.data()->m_baseTransform);
     }
 
     virtual void updateState(QAbstractAnimation::State newState, QAbstractAnimation::State oldState)
@@ -1178,13 +1174,6 @@ public:
     {
     }
 
-    ~OpacityAnimationQt()
-    {
-        if (m_fillsForwards)
-            setCurrentTime(1);
-        else
-            m_layer.data()->setOpacity(m_layer.data()->m_layer->opacity());
-    }
     virtual void applyFrame(const qreal& fromValue, const qreal& toValue, qreal progress)
     {
         qreal opacity = qBound(qreal(0), fromValue + (toValue-fromValue)*progress, qreal(1));
@@ -1195,8 +1184,6 @@ public:
             m_layer.data()->scene()->update();
 
         m_layer.data()->setOpacity(opacity);
-        if (m_fillsForwards)
-            m_layer.data()->m_layer->setOpacity(opacity);
     }
 
     virtual void updateState(QAbstractAnimation::State newState, QAbstractAnimation::State oldState)
@@ -1213,7 +1200,7 @@ bool GraphicsLayerQt::addAnimation(const KeyframeValueList& values, const IntSiz
     if (!anim->duration() || !anim->iterationCount())
         return false;
 
-    AnimationQtBase* newAnim = 0;
+    QAbstractAnimation* newAnim = 0;
 
     // fixed: we might already have the Qt animation object associated with this WebCore::Animation object
     for (QList<QWeakPointer<QAbstractAnimation> >::iterator it = m_impl->m_animations.begin(); it != m_impl->m_animations.end(); ++it) {
@@ -1238,17 +1225,12 @@ bool GraphicsLayerQt::addAnimation(const KeyframeValueList& values, const IntSiz
 
         // we make sure WebCore::Animation and QAnimation are on the same terms
         newAnim->setLoopCount(anim->iterationCount());
-        newAnim->m_fillsForwards = anim->fillsForwards();
         m_impl->m_animations.append(QWeakPointer<QAbstractAnimation>(newAnim));
         QObject::connect(&m_impl->m_suspendTimer, SIGNAL(timeout()), newAnim, SLOT(resume()));
     }
 
     // flush now or flicker...
     m_impl->flushChanges(false);
-
-    // when fill-mode is backwards/both, we set the value to 0 before the delay takes place
-    if (anim->fillsBackwards())
-        newAnim->setCurrentTime(0);
 
     if (anim->delay())
         QTimer::singleShot(anim->delay() * 1000, newAnim, SLOT(start()));
