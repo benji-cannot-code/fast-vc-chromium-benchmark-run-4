b80478b1f9ac51678378aa0a0f235dc0502497fa
robert@webkit.org <robert@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-18 10:36:12 +0000

    [Qt] Add support for LayoutTestController commands 2010-04-18  Robert Hogan  <robert@webkit.org>
            Reviewed by Simon Hausmann.
    
            [Qt] Add support for LayoutTestController commands:
                       setSmartInsertDeleteEnabled
                       setSelectTrailingWhitespaceEnabled
                       execCommand
                       isCommandEnabled
    
            https://bugs.webkit.org/show_bug.cgi?id=35844
    
            * Api/qwebpage.cpp:
            (QWebPagePrivate::QWebPagePrivate):
            * Api/qwebpage_p.h:
            * WebCoreSupport/DumpRenderTreeSupportQt.cpp:
            (DumpRenderTreeSupportQt::setSmartInsertDeleteEnabled):
            (DumpRenderTreeSupportQt::setSelectTrailingWhitespaceEnabled):
            (DumpRenderTreeSupportQt::executeCoreCommandByName):
            (DumpRenderTreeSupportQt::isCommandEnabled):
            * WebCoreSupport/DumpRenderTreeSupportQt.h:
            * WebCoreSupport/EditorClientQt.cpp:
            (WebCore::EditorClientQt::smartInsertDeleteEnabled):
            (WebCore::EditorClientQt::toggleSmartInsertDelete):
            (WebCore::EditorClientQt::isSelectTrailingWhitespaceEnabled):
            * WebCoreSupport/EditorClientQt.h:
    
    2010-04-18  Robert Hogan  <robert@webkit.org>
    
            Reviewed by Simon Hausmann.
    
            [Qt] Add support for LayoutTestController commands:
                       setSmartInsertDeleteEnabled
                       setSelectTrailingWhitespaceEnabled
                       execCommand
                       isCommandEnabled
    
            Unskip tests:
            editing/deleting/smart-editing-disabled.html
            editing/execCommand/19089.html
            editing/execCommand/delete-image-in-anchor.html
            editing/execCommand/enabling-and-selection-2.html
            editing/selection/doubleclick-whitespace-crash.html
            editing/selection/doubleclick-whitespace-img-crash.html
            editing/selection/doubleclick-whitespace.html
            editing/selection/select-line.html
            editing/deleting/5300379.html
            editing/deleting/smart-delete-003.html
            editing/deleting/smart-delete-004.html
            editing/selection/5195166-1.html
            editing/selection/5195166-2.html
    
            https://bugs.webkit.org/show_bug.cgi?id=35844
    
            * platform/qt/Skipped:
            * platform/qt/editing/deleting/5300379-expected.txt: Update platform-specific results.
            * platform/qt/editing/deleting/smart-delete-003-expected.txt: Update platform-specific results.
            * platform/qt/editing/deleting/smart-delete-004-expected.txt: Update platform-specific results.
            * platform/qt/editing/selection/5195166-1-expected.txt: Copied from LayoutTests/platform/mac/editing/selection/5195166-1-expected.txt.
            * platform/qt/editing/selection/5195166-2-expected.txt: Update platform-specific results.
    
    2010-04-18  Robert Hogan  <robert@webkit.org>
    
            Reviewed by Simon Hausmann.
    
            [Qt] Add support for LayoutTestController commands:
                       setSmartInsertDeleteEnabled
                       setSelectTrailingWhitespaceEnabled
                       execCommand
                       isCommandEnabled
    
            https://bugs.webkit.org/show_bug.cgi?id=35844
    
            * DumpRenderTree/qt/DumpRenderTreeQt.cpp:
            (WebCore::WebPage::resetSettings):
            * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
            (LayoutTestController::setSmartInsertDeleteEnabled):
            (LayoutTestController::setSelectTrailingWhitespaceEnabled):
            (LayoutTestController::execCommand):
            (LayoutTestController::isCommandEnabled):
            * DumpRenderTree/qt/LayoutTestControllerQt.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage_p.h
+++ third_party/WebKit/WebKit/qt/Api/qwebpage_p.h
@@ -165,6 +165,8 @@ public:
     QNetworkAccessManager *networkManager;
 
     bool forwardUnsupportedContent;
+    bool smartInsertDeleteEnabled;
+    bool selectTrailingWhitespaceEnabled;
     QWebPage::LinkDelegationPolicy linkPolicy;
 
     QSize viewportSize;
