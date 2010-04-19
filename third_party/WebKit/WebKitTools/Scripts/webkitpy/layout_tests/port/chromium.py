benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 1e4ae256e3b8357f9873bc28c7d71f435cab2e95

2010-04-19  Kent Tamura  <tkent@chromium.org>
        Reviewed by Eric Seidel.

        [Chromium] new-run-webkit-tests should use WebKitDriver for --use-drt
        https://bugs.webkit.org/show_bug.cgi?id=37793

        We need to use WebKitDriver instead of ChromiumDriver for Chromium
        DRT because its interface is different from test_shell.

        Chromium DRT has no UI.  So we can't use it to show test results.

        * Scripts/webkitpy/layout_tests/port/chromium.py:

git-svn-id: svn://svn.chromium.org/blink/trunk@57806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/chromium.py
@@ -37,9 +37,11 @@ import signal
 import subprocess
 import sys
 import time
+import webbrowser
 
 import base
 import http_server
+import webkit
 import websocket_server
 
 _log = logging.getLogger("webkitpy.layout_tests.port.chromium")
@@ -151,11 +153,16 @@ class ChromiumPort(base.Port):
             shutil.rmtree(cachedir)
 
     def show_results_html_file(self, results_filename):
-        subprocess.Popen([self._path_to_driver(),
-                          self.filename_to_uri(results_filename)])
+        uri = self.filename_to_uri(results_filename)
+        if self._options.use_drt:
+            webbrowser.open(uri, new=1)
+        else:
+            subprocess.Popen([self._path_to_driver(), uri])
 
     def start_driver(self, image_path, options):
         """Starts a new Driver and returns a handle to it."""
+        if self._options.use_drt:
+            return webkit.WebKitDriver(self, image_path, options)
         return ChromiumDriver(self, image_path, options)
 
     def start_helper(self):

commit 3ba47de7646e625a4e47a73866c218ed0709c147

2010-04-19  Dirk Pranke  <dpranke@chromium.org>
        Unreviewed, build fix.

        Fix breakage of chromium-win canary bots caused by r57806. That patch
        introduced the option of using Chrome's new port of DumpRenderTree,
        but unfortunately that port relies on the webkit.py class
        implementation which uses non-blocking I/O that isn't available on
        Windows. This patch turns off that option and doesn't import the
        class if we're running on Windows.

        https://bugs.webkit.org/show_bug.cgi?id=37817

        * Scripts/webkitpy/layout_tests/port/chromium.py:

git-svn-id: svn://svn.chromium.org/blink/trunk@57831 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/chromium.py
@@ -41,7 +41,14 @@ import webbrowser
 
 import base
 import http_server
-import webkit
+
+# FIXME: To use the DRT-based version of this file, we need to be able to
+# run the webkit code, which uses server_process, which requires UNIX-style
+# non-blocking I/O with selects(), which requires fcntl() which doesn't exist
+# on Windows.
+if sys.platform not in ('win32', 'cygwin'):
+    import webkit
+
 import websocket_server
 
 _log = logging.getLogger("webkitpy.layout_tests.port.chromium")
@@ -74,6 +81,10 @@ class ChromiumPort(base.Port):
     def __init__(self, port_name=None, options=None):
         base.Port.__init__(self, port_name, options)
         self._chromium_base_dir = None
+
+        # FIXME: see comment above re: import webkit
+        if sys.platform in ('win32', 'cygwin') and options:
+            options.use_drt = False
 
     def baseline_path(self):
         return self._webkit_baseline_path(self._name)