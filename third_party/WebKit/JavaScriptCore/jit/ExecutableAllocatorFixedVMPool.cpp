8e660c5d464ed9706a9d6ee5fbf3d583fa8b8f31
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-19 18:34:06 +0000

    [Qt] Build fix for WinCE.
    Patch by Jocelyn Turcotte <jocelyn.turcotte@nokia.com> on 2010-04-19
    Reviewed by Simon Hausmann.
    
    Moved the include of the non-existing errno.h header file inside
    platform guard macros.
    
    * jit/ExecutableAllocatorFixedVMPool.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57821 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/jit/ExecutableAllocatorFixedVMPool.cpp
+++ third_party/WebKit/JavaScriptCore/jit/ExecutableAllocatorFixedVMPool.cpp
@@ -28,9 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ExecutableAllocator.h"
 
-#include <errno.h>
-
 #if ENABLE(ASSEMBLER) && OS(DARWIN) && CPU(X86_64)
+
+#include <errno.h>
 
 #include "TCSpinLock.h"
 #include <mach/mach_init.h>
