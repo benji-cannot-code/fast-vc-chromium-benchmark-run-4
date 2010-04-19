bbbb27efde4e68922e9fe430e1fd8a19caf9b1c1
adachan@apple.com <adachan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-19 19:13:34 +0000

    Build fix: wrap Settings::setLocalStorageQuota() and Settings::setSessionStorageQuota() in #if ENABLE(DOM_STORAGE).
    * page/Settings.cpp:
    (WebCore::Settings::Settings):
    * page/Settings.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57826 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Settings.cpp
+++ third_party/WebKit/WebCore/page/Settings.cpp
@@ -65,8 +65,10 @@ Settings::Settings(Page* page)
     , m_defaultFontSize(0)
     , m_defaultFixedFontSize(0)
     , m_maximumDecodedImageSize(numeric_limits<size_t>::max())
+#if ENABLE(DOM_STORAGE)            
     , m_localStorageQuota(5 * 1024 * 1024)  // Suggested by the HTML5 spec.
     , m_sessionStorageQuota(StorageMap::noQuota)
+#endif
     , m_pluginAllowedRunTime(numeric_limits<unsigned>::max())
     , m_zoomMode(ZoomPage)
     , m_isSpatialNavigationEnabled(false)
@@ -275,6 +277,7 @@ void Settings::setLocalStorageEnabled(bool localStorageEnabled)
     m_localStorageEnabled = localStorageEnabled;
 }
 
+#if ENABLE(DOM_STORAGE)        
 void Settings::setLocalStorageQuota(unsigned localStorageQuota)
 {
     m_localStorageQuota = localStorageQuota;
@@ -284,6 +287,7 @@ void Settings::setSessionStorageQuota(unsigned sessionStorageQuota)
 {
     m_sessionStorageQuota = sessionStorageQuota;
 }
+#endif
 
 void Settings::setPrivateBrowsingEnabled(bool privateBrowsingEnabled)
 {
