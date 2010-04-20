6eff90cfa518c1498c83f58aa5b8e828dd78ba2b
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-20 00:53:22 +0000

    2010-04-19  Dirk Pranke  <dpranke@chromium.org>
             Reviewed by Adam Barth.
    
             new-run-webkit-tests - repurpose the "Passing" port as "Dryrun" port
             that can be used to test platforms other than the one you are running
             on. This can be useful for checking baselines and testing code
             coverage.
    
             Note that running the code on the "wrong" port requires each
             port-specific implementation to actually not require any
             platform-specific python code (e.g., the chromium-win port must
             test for the existence of windows functions before calling them).
    
             https://bugs.webkit.org/show_bug.cgi?id=37782
    
             * Scripts/webkitpy/layout_tests/port/chromium_win.py:
             * Scripts/webkitpy/layout_tests/port/dryrun.py: Renamed from WebKitTools/Scripts/webkitpy/layout_tests/port/passing.py.
             * Scripts/webkitpy/layout_tests/port/factory.py:
             * Scripts/webkitpy/layout_tests/run_webkit_tests_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57860 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/base.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/base.py
@@ -326,7 +326,7 @@ class Port(object):
         This is used by the rebaselining tool. Raises NotImplementedError
         if the port does not use expectations files."""
         raise NotImplementedError('Port.path_to_test_expectations_file')
- 
+
     def remove_directory(self, *path):
         """Recursively removes a directory, even if it's marked read-only.
 
