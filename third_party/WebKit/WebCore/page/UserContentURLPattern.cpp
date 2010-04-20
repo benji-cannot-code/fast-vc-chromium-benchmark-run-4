eef1a86dca15bdd81a8da97cc9faadc7b42bd008
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-20 21:50:32 +0000

    Fix matching of "file:///*" patterns by not trying to compare the host. The host is irrelevant for file URLs.
    Also fix comparisons to be case insensitive.
    
    https://bugs.webkit.org/show_bug.cgi?id=37889
    
    Reviewed by Dave Hyatt.
    
    * page/UserContentURLPattern.cpp:
    (WebCore::UserContentURLPattern::parse): Use equalIgnoringCase when comparing for "file" schemes.
    (WebCore::UserContentURLPattern::matches): Use equalIgnoringCase when comparing schemes. Only call
    matchesHost if the scheme isn't "file".
    (WebCore::UserContentURLPattern::matchesHost): Call equalIgnoringCase when comparing hosts. The endsWith
    was already doing a case-insensitive compare, so existing tests worked though this path.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57923 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/UserContentURLPattern.cpp
+++ third_party/WebKit/WebCore/page/UserContentURLPattern.cpp
@@ -76,7 +76,7 @@ bool UserContentURLPattern::parse(const String& pattern)
 
     int pathStartPos = 0;
 
-    if (m_scheme == "file")
+    if (equalIgnoringCase(m_scheme, "file"))
         pathStartPos = hostStartPos;
     else {
         int hostEndPos = pattern.find("/", hostStartPos);
@@ -115,10 +115,10 @@ bool UserContentURLPattern::matches(const KURL& test) const
     if (m_invalid)
         return false;
 
-    if (test.protocol() != m_scheme)
+    if (!equalIgnoringCase(test.protocol(), m_scheme))
         return false;
 
-    if (!matchesHost(test))
+    if (!equalIgnoringCase(m_scheme, "file") && !matchesHost(test))
         return false;
 
     return matchesPath(test);
@@ -126,7 +126,7 @@ bool UserContentURLPattern::matches(const KURL& test) const
 
 bool UserContentURLPattern::matchesHost(const KURL& test) const
 {
-    if (test.host() == m_host)
+    if (equalIgnoringCase(test.host(), m_host))
         return true;
 
     if (!m_matchSubdomains)
