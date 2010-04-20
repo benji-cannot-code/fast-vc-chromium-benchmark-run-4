ed1b47e29050f8f55e04c2d9c109287aed6142f1
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-20 00:56:03 +0000

    Add IME prefs to sync.
    BUG=crosbug.com/488
    TEST=Run two instances of ChromeOS with sync logged in.  Check that changes to the IME settings propagate.
    
    Review URL: http://codereview.chromium.org/1572046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44993 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/synchronized_preferences.h
+++ chrome/browser/sync/glue/synchronized_preferences.h
@@ -99,6 +99,15 @@ static const wchar_t* kSynchronizedPreferences[] = {
   // Desktop notification permissions.
   prefs::kDesktopNotificationAllowedOrigins,
   prefs::kDesktopNotificationDeniedOrigins,
+
+#if defined(OS_CHROMEOS)
+  // IME prefs
+  prefs::kLanguageUseGlobalEngine,
+  prefs::kLanguageHotkeyNextEngine,
+  prefs::kLanguageHotkeyTrigger,
+  prefs::kLanguagePreloadEngines,
+  prefs::kLanguageHangulKeyboard,
+#endif
 };
 
 }  // namespace browser_sync
