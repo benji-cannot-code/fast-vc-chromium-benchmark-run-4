2ed45052b6c9dcd7526d8afcd9c2d81f47a24043
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-21 01:16:14 +0000

    Task Manager sorting:
    - remove dead code that papered over the fact certain columns don't sort, replace with NOTREACHED pothole
    - allow sorting by the javascript memory column.
    - allow sorting by goats teleported (useful for debugging)
    - stabilize \# of goats teleported (remove observer effect)
    
    BUG=42037
    TEST=sort by these columns in the task manager
    
    Review URL: http://codereview.chromium.org/1706002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45139 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/task_manager_mac.mm
+++ chrome/browser/cocoa/task_manager_mac.mm
@@ -325,8 +325,8 @@ static const struct ColumnWidth {
       return base::SysWideToNSString(model_->GetResourceGoatsTeleported(row));
 
     default:
-      return base::SysWideToNSString(
-          model_->GetResourceStatsValue(row, columnId));
+      NOTREACHED();
+      return @"";
   }
 }
 
