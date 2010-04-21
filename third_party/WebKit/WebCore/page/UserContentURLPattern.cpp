0e8b69381111b336a4cbb5c6e2eb97dbf294c520
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-21 16:40:03 +0000

    Make UserContentURLPattern correctly check for subdomains and the URL has the same suffix as the pattern. Also improve the parsing of the host.
    https://bugs.webkit.org/show_bug.cgi?id=37357
    
    Reviewed by Darin Adler.
    
    * page/UserContentURLPattern.cpp:
    (WebCore::UserContentURLPattern::parse): Simplify the subdomain pattern parsing to
    simply check for "*" only or a "*." prefix and then do a substring.
    (WebCore::UserContentURLPattern::matchesHost): Check that the host has a "." in the
    position before the suffix to ensure it a subdomain and not just a suffix match.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57990 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/UserContentURLPattern.cpp
+++ third_party/WebKit/WebCore/page/UserContentURLPattern.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2009, 2010 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -82,22 +82,20 @@ bool UserContentURLPattern::parse(const String& pattern)
         int hostEndPos = pattern.find("/", hostStartPos);
         if (hostEndPos == -1)
             return false;
-    
-        m_host = pattern.substring(hostStartPos, hostEndPos - hostStartPos);
 
-        // The first component can be '*', which means to match all subdomains.
-        Vector<String> hostComponents;
-        m_host.split(".", hostComponents); 
-        if (hostComponents[0] == "*") {
-            m_matchSubdomains = true;
+        m_host = pattern.substring(hostStartPos, hostEndPos - hostStartPos);
+        m_matchSubdomains = false;
+
+        if (m_host == "*") {
+            // The pattern can be just '*', which means match all domains.
             m_host = "";
-            for (unsigned i = 1; i < hostComponents.size(); ++i) {
-                m_host = m_host + hostComponents[i];
-                if (i < hostComponents.size() - 1)
-                    m_host = m_host + ".";
-            }
+            m_matchSubdomains = true;
+        } else if (m_host.startsWith("*.")) {
+            // The first component can be '*', which means to match all subdomains.
+            m_host = m_host.substring(2); // Length of "*."
+            m_matchSubdomains = true;
         }
-        
+
         // No other '*' can occur in the host.
         if (m_host.find("*") != -1)
             return false;
@@ -126,7 +124,8 @@ bool UserContentURLPattern::matches(const KURL& test) const
 
 bool UserContentURLPattern::matchesHost(const KURL& test) const
 {
-    if (equalIgnoringCase(test.host(), m_host))
+    const String& host = test.host();
+    if (equalIgnoringCase(host, m_host))
         return true;
 
     if (!m_matchSubdomains)
@@ -137,8 +136,14 @@ bool UserContentURLPattern::matchesHost(const KURL& test) const
     if (!m_host.length())
         return true;
 
-    // Check if the test host is a subdomain of our host.
-    return test.host().endsWith(m_host, false);
+    // Check if the domain is a subdomain of our host.
+    if (!host.endsWith(m_host, false))
+        return false;
+
+    ASSERT(host.length() > m_host.length());
+
+    // Check that the character before the suffix is a period.
+    return host[host.length() - m_host.length() - 1] == '.';
 }
 
 struct MatchTester
