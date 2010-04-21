a0d0c7453abd36957178d45aed7fcfddb3772ab4
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-21 15:13:47 +0000

    Lands http://codereview.chromium.org/1610030/show from Luigi:
    This changes makes histograms dynamically defined by external metrics
    clients,
    instead of hardcoding them in Chrome source. This is important because
    making even simple changes to Chrome is laborious and takes a fair
    amount of
    learning. Unfortunately user actions still require code in Chrome,
    because
    of a pre-processing script that extracts them from the source.
    
    BUG=NONE
    TEST=the included unit test
    
    Review URL: http://codereview.chromium.org/1718003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45189 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_service.h
+++ chrome/browser/metrics/metrics_service.h
@@ -34,7 +34,6 @@ class BookmarkModel;
 class BookmarkNode;
 class HistogramSynchronizer;
 class PrefService;
-class Profile;
 class TemplateURLModel;
 
 // This is used to quickly log stats from child process related notifications in
@@ -128,7 +127,7 @@ class MetricsService : public NotificationObserver,
 #if defined(OS_CHROMEOS)
   // Start the external metrics service, which collects metrics from Chrome OS
   // and passes them to UMA.
-  void StartExternalMetrics(Profile* profile);
+  void StartExternalMetrics();
 #endif
 
   bool recording_active() const;
