7c9ae5d42dd455a40e2ded4bf0ba8596e739bbc4
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-21 00:35:01 +0000

    Check for proper locale before recording search engine data, or offering the search engine experimental dialog.
    BUG= 37564
    TEST= none
    
    Review URL: http://codereview.chromium.org/1709003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45129 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run_win.cc
+++ chrome/browser/first_run_win.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <shellapi.h>
 #include <shlobj.h>
 
+#include <set>
 #include <sstream>
 
 // TODO(port): trim this include list once first run has been refactored fully.
@@ -321,7 +322,8 @@ bool FirstRun::ProcessMasterPreferences(const FilePath& user_data_dir,
     }
   }
 
-  if (installer_util::GetDistroBooleanPreference(prefs.get(),
+  if (InSearchExperimentLocale() &&
+      installer_util::GetDistroBooleanPreference(prefs.get(),
       installer_util::master_preferences::kSearchEngineExperimentPref,
       &value) && value) {
     // Set the first run dialog to include the search choice window.
@@ -728,6 +730,19 @@ int FirstRun::ImportFromBrowser(Profile* profile,
       true);
   observer.RunLoop();
   return observer.import_result();
+}
+
+// static
+bool FirstRun::InSearchExperimentLocale() {
+  static std::set<std::string> allowed_locales;
+  if (allowed_locales.empty()) {
+    // List of locales in which search experiment can be run.
+    allowed_locales.insert("en-GB");
+    allowed_locales.insert("en-US");
+  }
+  const std::string app_locale = g_browser_process->GetApplicationLocale();
+  std::set<std::string>::iterator locale = allowed_locales.find(app_locale);
+  return locale != allowed_locales.end();
 }
 
 //////////////////////////////////////////////////////////////////////////
