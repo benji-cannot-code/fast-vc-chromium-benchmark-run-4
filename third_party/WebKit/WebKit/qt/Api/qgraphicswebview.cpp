6f4d16bc0d86bd9b4c5a81e7c44c2b66aaf667e7
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-21 10:09:48 +0000

    2010-04-21  No'am Rosenthal  <noam.rosenthal@nokia.com>
            Reviewed by Simon Fraser.
    
            [Qt] Fix or remove the runtime flag for accelerated compositing.
    
            This adds a way for a chrome client to disallow layers from becoming composited,
            even if the settings enable accelerated compositing. This is necessary for platforms
            where different views can be applied with the same settings to the same page.
    
            We enable an API through ChromeClient to ask the chrome-client whether or not
            it can render composited layers, which is taken into account when the compositor
            decides whether or not to start compositing.
    
            https://bugs.webkit.org/show_bug.cgi?id=37313
    
            Pages under LayoutTests/compositing now work under QWebView, even when
            QWebSettings::AcceleratedCompositingEnabled is on.
    
            * page/ChromeClient.h:
            (WebCore::ChromeClient::allowsAcceleratedCompositing):
            * platform/qt/QWebPageClient.h:
            (QWebPageClient::allowsAcceleratedCompositing):
            * rendering/RenderLayerCompositor.cpp:
            (WebCore::RenderLayerCompositor::canBeComposited):
    2010-04-21  No'am Rosenthal  <noam.rosenthal@nokia.com>
    
            Reviewed by Simon Fraser.
    
            [Qt] Fix or remove the runtime flag for accelerated compositing.
            https://bugs.webkit.org/show_bug.cgi?id=37313
    
            This lets the QWebPageClient "veto" the settings value for accelerated compositing.
            In this case we allow accelerated compositing only on QGraphicsWebView.
    
            * Api/qgraphicswebview.cpp:
            (QGraphicsWebViewPrivate::allowsAcceleratedCompositing):
            * WebCoreSupport/ChromeClientQt.cpp:
            (WebCore::ChromeClientQt::allowsAcceleratedCompositing):
            * WebCoreSupport/ChromeClientQt.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57961 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
+++ third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
@@ -123,6 +123,9 @@ public:
     virtual void setRootGraphicsLayer(QGraphicsItem* layer);
     virtual void markForSync(bool scheduleSync);
     void updateCompositingScrollPosition();
+
+    // QGraphicsWebView can render composited layers
+    virtual bool allowsAcceleratedCompositing() const { return true; }
 #endif
 
     void updateResizesToContentsForPage();
