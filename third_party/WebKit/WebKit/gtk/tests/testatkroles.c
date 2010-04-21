9f51a9d7062b7be08ffc8dd8f843fc05224e4b6c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-21 13:17:56 +0000

    2010-04-21  Diego Escalante Urrelo  <descalante@igalia.com>
            Reviewed by Xan Lopez.
    
            [Gtk] Evaluate and create tests for all the AtkRole's implemented by
            WebKitGtk
            https://bugs.webkit.org/show_bug.cgi?id=34449
    
            Expand testatkroles to test ATK_ROLE_FORM.
    
            * tests/testatkroles.c:
            (test_webkit_atk_get_role_form):
            (main):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/tests/testatkroles.c
+++ third_party/WebKit/WebKit/gtk/tests/testatkroles.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HTML_SECTION "<html><body><div>This is a test.</div></body></html>"
 #define HTML_TABLE "<html><body><table border='1'><tr><td>This is</td><td>a test.</td></tr></table></body></html>"
 /* Form roles */
+#define HTML_FORM "<html><body><form>This is a test.</form></body></html>"
 #define HTML_CHECK_BOX "<html><body><input type='checkbox' />This is a test.</body></html>"
 #define HTML_LABELED_ENTRY "<html><body><label for='foo'>Name:</label><input type='text' id='foo' /></body></html>"
 #define HTML_LISTBOX "<html><body><select size='3'><option>one</option><option>two</option><option>three</option></select></body></html>"
@@ -176,6 +177,11 @@ static void test_webkit_atk_get_role_table(AtkRolesFixture* fixture, gconstpoint
 }
 
 /* Form roles */
+static void test_webkit_atk_get_role_form(AtkRolesFixture *fixture, gconstpointer data)
+{
+    get_child_and_test_role(fixture->documentFrame, 0, ATK_ROLE_FORM);
+}
+
 static void test_webkit_atk_get_role_check_box(AtkRolesFixture* fixture, gconstpointer data)
 {
     // This is an extraneous object of ATK_ROLE_PANEL which we should get rid of.
@@ -318,6 +324,11 @@ int main(int argc, char** argv)
                atk_roles_fixture_teardown);
 
     /* Form roles */
+    g_test_add("/webkit/atk/test_webkit_atk_get_role_form",
+               AtkRolesFixture, HTML_FORM,
+               atk_roles_fixture_setup,
+               test_webkit_atk_get_role_form,
+               atk_roles_fixture_teardown);
     g_test_add("/webkit/atk/test_webkit_atk_get_role_check_box",
                AtkRolesFixture, HTML_CHECK_BOX,
                atk_roles_fixture_setup,
