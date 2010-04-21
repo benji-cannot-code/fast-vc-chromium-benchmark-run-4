9ac4a4f90c3b66d6459ad2e9d26d2c8ed9bbea9f
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-21 15:24:15 +0000

    2010-04-21  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Adam Barth.
    
            [GTK] GObject DOM bindings
            https://bugs.webkit.org/show_bug.cgi?id=33590
    
            Add build bits for the GObject DOM bindings.
    
            * GNUmakefile.am:
    
    2010-04-21  Xan Lopez  <xlopez@igalia.com>
    
            Reviewed by Adam Barth.
    
            [GTK] GObject DOM bindings
            https://bugs.webkit.org/show_bug.cgi?id=33590
    
            Initial version of the GObject DOM bindings.
    
            Only bindings for Node.idl and a few of its dependencies are
            provided, without public API to access them at the
            moment. References to the Document interfaces and to
            EventListeners in Node.idl are ignored for GObject to make the
            initial patch as small as possible, but will be enabled in a
            follow-up patch.
    
            * GNUmakefile.am:
            * bindings/gobject/ConvertToUTF8String.cpp: Added.
            (convertToUTF8String):
            * bindings/gobject/ConvertToUTF8String.h: Added.
            * bindings/gobject/WebKitDOMBinding.cpp: Added.
            (WebKit::domObjects):
            (WebKit::DOMObjectCache::get):
            (WebKit::DOMObjectCache::put):
            (WebKit::DOMObjectCache::forget):
            (WebKit::createWrapper):
            (WebKit::kit):
            * bindings/gobject/WebKitDOMBinding.h: Added.
            * bindings/gobject/WebKitDOMObject.cpp: Added.
            (webkit_dom_object_init):
            (webkit_dom_object_class_init):
            * bindings/gobject/WebKitDOMObject.h: Added.
            * bindings/scripts/CodeGeneratorGObject.pm: Added.
            * bindings/scripts/gobject-generate-headers.pl: Added.
            * dom/Node.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57985 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebCore/bindings/gobject/WebKitDOMObject.cpp
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+ * Copyright (C) 2008 Luke Kenneth Casson Leighton <lkcl@lkcl.net>
+ * Copyright (C) 2008 Martin Soto <soto@freedesktop.org>
+ * Copyright (C) 2008 Alp Toker <alp@atoker.com>
+ * Copyright (C) 2008 Apple Inc.
+ * Copyright (C) 2009 Igalia S.L.
+ */
+#include "config.h"
+#include "WebKitDOMObject.h"
+
+#include "glib-object.h"
+#include "WebKitDOMBinding.h"
+
+G_DEFINE_TYPE(WebKitDOMObject, webkit_dom_object, G_TYPE_OBJECT);
+
+static void webkit_dom_object_init(WebKitDOMObject* object)
+{
+}
+
+static void webkit_dom_object_class_init(WebKitDOMObjectClass* klass)
+{
+}
+
