e5564eabf9a834af9ec0e60b9ef0f8ed4addb60d
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-21 08:13:50 +0000

    Fix arm compile after turning on gcc warnings.
    Actually fix a comment that was pointed out during review.
    
    TBR=rolandsteiner
    
    Review URL: http://codereview.chromium.org/1734003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45165 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/default_plugin_stubs_win.cc
+++ webkit/support/default_plugin_stubs_win.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// These are stubs for default_plugin which isn't current build in
+// These are stubs for default_plugin which isn't currently built in
 // a webkit only checkout.
 
 #include "base/logging.h"
