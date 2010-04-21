6d12f6fa06b5a00ea16be48d5536397a7c9470bd
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-21 12:57:13 +0000

    2010-04-21  Benjamin Poulain  <ikipou@gmail.com>
            Reviewed by Simon Fraser.
    
            Update of fixed elements is not made correctly when the page has been scrolled
            https://bugs.webkit.org/show_bug.cgi?id=36783
    
            * fast/repaint/fixed-child-move-after-scroll.html: Added.
            * fast/repaint/fixed-child-of-fixed-move-after-scroll.html: Added.
            * fast/repaint/fixed-child-of-transformed-move-after-scroll.html: Added.
            * fast/repaint/fixed-move-after-scroll.html: Added.
            * platform/mac/fast/repaint/fixed-child-move-after-scroll-expected.checksum: Added.
            * platform/mac/fast/repaint/fixed-child-move-after-scroll-expected.png: Added.
            * platform/mac/fast/repaint/fixed-child-move-after-scroll-expected.txt: Added.
            * platform/mac/fast/repaint/fixed-child-of-fixed-move-after-scroll-expected.checksum: Added.
            * platform/mac/fast/repaint/fixed-child-of-fixed-move-after-scroll-expected.png: Added.
            * platform/mac/fast/repaint/fixed-child-of-fixed-move-after-scroll-expected.txt: Added.
            * platform/mac/fast/repaint/fixed-child-of-transformed-move-after-scroll-expected.checksum: Added.
            * platform/mac/fast/repaint/fixed-child-of-transformed-move-after-scroll-expected.png: Added.
            * platform/mac/fast/repaint/fixed-child-of-transformed-move-after-scroll-expected.txt: Added.
            * platform/mac/fast/repaint/fixed-move-after-scroll-expected.checksum: Added.
            * platform/mac/fast/repaint/fixed-move-after-scroll-expected.png: Added.
            * platform/mac/fast/repaint/fixed-move-after-scroll-expected.txt: Added.
    2010-04-21  Benjamin Poulain  <ikipou@gmail.com>
    
            Reviewed by Simon Fraser.
    
            Update of fixed elements is not made correctly when the page has been scrolled
            https://bugs.webkit.org/show_bug.cgi?id=36783
    
            When a fixed element was updated, the old geometry was not repainted correctly
            because the repaint rect was cached during the layout and not updated when
            scrolling.
    
            The rect is now updated while scrolling so the region updated correspond to the
            region of the element on the screen.
    
            The method RenderLayer::updateRepaintRectsAfterScroll() updates
            the repaint rect of all fixed tree after scroll.
    
            Tests: fast/repaint/fixed-child-move-after-scroll.html
                   fast/repaint/fixed-child-of-fixed-move-after-scroll.html
                   fast/repaint/fixed-child-of-transformed-move-after-scroll.html
                   fast/repaint/fixed-move-after-scroll.html
    
            * page/FrameView.cpp:
            (WebCore::FrameView::scrollPositionChanged):
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::updateRepaintRectsAfterScroll):
            * rendering/RenderLayer.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@57971 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -1012,6 +1012,7 @@ void FrameView::scrollPositionChanged()
     if (!m_nestedLayoutCount && hasFixedObjects()) {
         if (RenderView* root = m_frame->contentRenderer()) {
             root->updateWidgetPositions();
+            root->layer()->updateRepaintRectsAfterScroll();
 #if USE(ACCELERATED_COMPOSITING)
             if (root->usesCompositing())
                 root->compositor()->updateCompositingLayers(CompositingUpdateOnScroll);
