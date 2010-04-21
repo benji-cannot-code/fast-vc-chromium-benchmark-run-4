7564b0ba454e6ed69bb81d2b8595054f16575786
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-21 14:07:24 +0000

    Rename OnBlockedContentChange to OnContentSettingsChange.
    BUG=None
    TEST=compiles
    
    (Patch from Thiago Farina <thiago.farina@gmail.com> http://codereview.chromium.org/1664003/show)
    
    Review URL: http://codereview.chromium.org/1750002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45186 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -927,7 +927,7 @@ void TabContents::PopupNotificationVisibilityChanged(bool visible) {
   if (!dont_notify_render_view_)
     render_view_host()->PopupNotificationVisibilityChanged(visible);
   if (delegate_)
-    delegate_->OnBlockedContentChange(this);
+    delegate_->OnContentSettingsChange(this);
 }
 
 gfx::NativeView TabContents::GetContentNativeView() const {
@@ -1562,7 +1562,7 @@ void TabContents::DidNavigateMainFramePostCommit(
     ClearBlockedContentSettings();
     ClearGeolocationContentSettings(details);
     if (delegate_)
-      delegate_->OnBlockedContentChange(this);
+      delegate_->OnContentSettingsChange(this);
   }
 
   // Close constrained windows if necessary.
@@ -2100,16 +2100,15 @@ void TabContents::OnContentBlocked(ContentSettingsType type) {
   DCHECK(type != CONTENT_SETTINGS_TYPE_GEOLOCATION);
   content_blocked_[type] = true;
   if (delegate_)
-    delegate_->OnBlockedContentChange(this);
+    delegate_->OnContentSettingsChange(this);
 }
 
 void TabContents::OnGeolocationPermissionSet(const GURL& requesting_origin,
                                              bool allowed) {
   geolocation_content_settings_[requesting_origin] =
       allowed ? CONTENT_SETTING_ALLOW : CONTENT_SETTING_BLOCK;
-  // TODO(bulach): rename OnBlockedContentChange to OnContentSettingsChange.
   if (delegate_)
-    delegate_->OnBlockedContentChange(this);
+    delegate_->OnContentSettingsChange(this);
 }
 
 RenderViewHostDelegate::View* TabContents::GetViewDelegate() {
