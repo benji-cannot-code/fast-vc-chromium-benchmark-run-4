e996555da7952add55db38492597f0976c7fc030
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-22 00:25:32 +0000

    aria-liveregion-notifications.html fails on leopard release bot https://bugs.webkit.org/show_bug.cgi?id=37112
    Reviewed by Alexey Proskuryakov.
    
    WebCore: 
    
    Change the method that DRT uses to monitor AX notifications so that its robust
    by just sending out NSNotification that can be listened to by anyone, instead
    of keeping a static function pointer around.
       
    This change is aimed to avoid flakiness seen in DRT when the notification handlers
    are not being called at the appropriate time.
    
    Tests: platform/mac/accessibility/aria-liveregions-addedelement.html
           platform/mac/accessibility/aria-liveregions-changedalt.html
           platform/mac/accessibility/aria-liveregions-changedtext.html
           platform/mac/accessibility/aria-liveregions-removedelement.html
    
    * accessibility/mac/AccessibilityObjectWrapper.h:
    * accessibility/mac/AccessibilityObjectWrapper.mm:
    (-[AccessibilityObjectWrapper accessibilitySetShouldRepostNotifications:]):
    (-[AccessibilityObjectWrapper accessibilityPostedNotification:]):
    
    WebKitTools: 
    
    Change the way that notifications are listened for by forcing clients
    to call a remove listener as well to match the add listener. DRT will
    assert if those are not done in the correct order. 
    
    * DumpRenderTree/AccessibilityUIElement.cpp:
    (removeNotificationListenerCallback):
    (AccessibilityUIElement::getJSClass):
    * DumpRenderTree/AccessibilityUIElement.h:
    * DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp:
    (AccessibilityUIElement::removeNotificationListener):
    * DumpRenderTree/mac/AccessibilityUIElementMac.mm:
    (-[AccessibilityNotificationHandler initWithPlatformElement:]):
    (-[AccessibilityNotificationHandler dealloc]):
    (-[AccessibilityNotificationHandler _notificationReceived:]):
    (-[AccessibilityNotificationHandler setCallback:]):
    (AccessibilityUIElement::AccessibilityUIElement):
    (AccessibilityUIElement::~AccessibilityUIElement):
    (AccessibilityUIElement::addNotificationListener):
    (AccessibilityUIElement::removeNotificationListener):
    * DumpRenderTree/win/AccessibilityUIElementWin.cpp:
    (AccessibilityUIElement::removeNotificationListener):
    
    LayoutTests: 
    
    Add four more tests that test specific live region functionality. Having these
    four tests run in order seems to stress the notification mechanism and will expose issues 
    more frequently.
    Modify tests that use notification listeners to also "removeNotificationListener".
    
    * platform/mac-leopard/Skipped:
    * platform/mac/accessibility/aria-listbox-selectedchildren-change.html:
    * platform/mac/accessibility/aria-liveregion-on-image.html:
    * platform/mac/accessibility/aria-liveregions-addedelement-expected.txt: Added.
    * platform/mac/accessibility/aria-liveregions-addedelement.html: Added.
    * platform/mac/accessibility/aria-liveregions-changedalt-expected.txt: Added.
    * platform/mac/accessibility/aria-liveregions-changedalt.html: Added.
    * platform/mac/accessibility/aria-liveregions-changedtext-expected.txt: Added.
    * platform/mac/accessibility/aria-liveregions-changedtext.html: Added.
    * platform/mac/accessibility/aria-liveregions-notifications.html:
    * platform/mac/accessibility/aria-liveregions-removedelement-expected.txt: Added.
    * platform/mac/accessibility/aria-liveregions-removedelement.html: Added.
    * platform/mac/accessibility/change-notification-on-scroll.html:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/mac/AccessibilityObjectWrapper.h
+++ third_party/WebKit/WebCore/accessibility/mac/AccessibilityObjectWrapper.h
@@ -54,7 +54,7 @@ class VisiblePosition;
 - (WebCore::AccessibilityObject*)accessibilityObject;
 
 // Used to inform an element when a notification is posted for it. Used by DRT.
-- (void)accessibilityPostedNotification:(NSString *)notification;
+- (void)accessibilityPostedNotification:(NSString *)notificationName;
 
 - (NSView*)attachmentView;
 
