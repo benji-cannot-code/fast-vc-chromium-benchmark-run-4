f1bc4ba95ef3c3f421356707abb64a592af27b75
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-22 18:06:07 +0000

    2010-04-22  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Simon Fraser.
    
            Do not pause movie when readyState drops below HAVE_FUTURE_DATA
            https://bugs.webkit.org/show_bug.cgi?id=37991
            <rdar://problem/7893937>
    
            No new tests, we don't have infrastructure in DRT to test with streamed
            movies.
    
            * html/HTMLMediaElement.cpp:
            (WebCore::HTMLMediaElement::HTMLMediaElement): Initialize m_readyStateMaximum.
            (WebCore::HTMLMediaElement::prepareForLoad): Reset m_readyStateMaximum.
            (WebCore::HTMLMediaElement::setReadyState): Maintain m_readyStateMaximum.
            (WebCore::HTMLMediaElement::potentiallyPlaying): Also return true if the readyState was
            previously >= HAVE_FUTURE_DATA. 
            * html/HTMLMediaElement.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58107 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLMediaElement.cpp
@@ -93,6 +93,7 @@ HTMLMediaElement::HTMLMediaElement(const QualifiedName& tagName, Document* doc)
     , m_webkitPreservesPitch(true)
     , m_networkState(NETWORK_EMPTY)
     , m_readyState(HAVE_NOTHING)
+    , m_readyStateMaximum(HAVE_NOTHING)
     , m_volume(1.0f)
     , m_lastSeekTime(0)
     , m_previousProgress(0)
@@ -494,6 +495,7 @@ void HTMLMediaElement::prepareForLoad()
     if (m_networkState != NETWORK_EMPTY) {
         m_networkState = NETWORK_EMPTY;
         m_readyState = HAVE_NOTHING;
+        m_readyStateMaximum = HAVE_NOTHING;
         m_paused = true;
         m_seeking = false;
         scheduleEvent(eventNames().emptiedEvent);
@@ -853,6 +855,9 @@ void HTMLMediaElement::setReadyState(MediaPlayer::ReadyState state)
     if (m_readyState == oldState)
         return;
     
+    if (oldState > m_readyStateMaximum)
+        m_readyStateMaximum = oldState;
+
     if (m_networkState == NETWORK_EMPTY)
         return;
 
@@ -1629,7 +1634,11 @@ PassRefPtr<TimeRanges> HTMLMediaElement::seekable() const
 
 bool HTMLMediaElement::potentiallyPlaying() const
 {
-    return m_readyState >= HAVE_FUTURE_DATA && couldPlayIfEnoughData();
+    // "pausedToBuffer" means the media engine's rate is 0, but only because it had to stop playing
+    // when it ran out of buffered data. A movie is this state is "potentially playing", modulo the
+    // checks in couldPlayIfEnoughData().
+    bool pausedToBuffer = m_readyStateMaximum >= HAVE_FUTURE_DATA && m_readyState < HAVE_FUTURE_DATA;
+    return (pausedToBuffer || m_readyState >= HAVE_FUTURE_DATA) && couldPlayIfEnoughData();
 }
 
 bool HTMLMediaElement::couldPlayIfEnoughData() const
