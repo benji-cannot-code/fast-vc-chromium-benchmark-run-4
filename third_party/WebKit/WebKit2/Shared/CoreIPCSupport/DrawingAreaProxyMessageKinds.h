dd6a81e676310fee755c36298ac4473241d84136
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-22 22:09:03 +0000

    Rename SetFrame and DidSetFrame to SetSize and DidSetSize.
    Reviewed by Sam Weinig.
    
    * Shared/CoreIPCSupport/DrawingAreaMessageKinds.h:
    (DrawingAreaMessage::):
    * Shared/CoreIPCSupport/DrawingAreaProxyMessageKinds.h:
    (DrawingAreaProxyMessage::):
    * UIProcess/mac/DrawingAreaProxyUpdateChunk.mm:
    (WebKit::DrawingAreaProxyUpdateChunk::drawRectIntoContext):
    (WebKit::DrawingAreaProxyUpdateChunk::setSize):
    (WebKit::DrawingAreaProxyUpdateChunk::didReceiveMessage):
    * WebProcess/WebPage/mac/DrawingAreaUpdateChunk.cpp:
    (WebKit::DrawingAreaUpdateChunk::setSize):
    (WebKit::DrawingAreaUpdateChunk::didReceiveMessage):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58119 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Shared/CoreIPCSupport/DrawingAreaProxyMessageKinds.h
+++ third_party/WebKit/WebKit2/Shared/CoreIPCSupport/DrawingAreaProxyMessageKinds.h
@@ -35,7 +35,7 @@ namespace DrawingAreaProxyMessage {
 
 enum Kind {
     Update,
-    DidSetFrame
+    DidSetSize
 };
 
 }
--- third_party/WebKit/WebKit2/WebProcess/WebPage/mac/DrawingAreaUpdateChunk.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebPage/mac/DrawingAreaUpdateChunk.cpp
@@ -145,7 +145,7 @@ void DrawingAreaUpdateChunk::setSize(const IntSize& viewSize)
 
     m_displayTimer.stop();
 
-    WebProcess::shared().connection()->send(DrawingAreaProxyMessage::DidSetFrame, m_webPage->pageID(), CoreIPC::In(viewSize, updateChunk));
+    WebProcess::shared().connection()->send(DrawingAreaProxyMessage::DidSetSize, m_webPage->pageID(), CoreIPC::In(viewSize, updateChunk));
 }
 
 void DrawingAreaUpdateChunk::didUpdate()
@@ -161,7 +161,7 @@ void DrawingAreaUpdateChunk::didUpdate()
 void DrawingAreaUpdateChunk::didReceiveMessage(CoreIPC::Connection*, CoreIPC::MessageID messageID, CoreIPC::ArgumentDecoder& arguments)
 {
     switch (messageID.get<DrawingAreaMessage::Kind>()) {
-        case DrawingAreaMessage::SetFrame: {
+        case DrawingAreaMessage::SetSize: {
             IntSize size;
             if (!arguments.decode(CoreIPC::Out(size)))
                 return;
