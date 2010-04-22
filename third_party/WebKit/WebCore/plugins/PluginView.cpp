a9acef9f975c010ce8795186c381a5c012ac06aa
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-22 02:51:04 +0000

    2010-04-21  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Darin Adler.
    
            Fix build if NPAPI support is disabled
            https://bugs.webkit.org/show_bug.cgi?id=36621
    
            No new tests, this is a build fix.
            Re-submit r56585 with fix for Chromium.
    
            * plugins/PluginView.cpp: Guard getValueStatic() with
            NETSCAPE_PLUGIN_API
            (WebCore::PluginView::getValue):
            * plugins/PluginView.h: Guard getValue() with NETSCAPE_PLUGIN_API
            * plugins/PluginViewNone.cpp: Guard platformGetValue() and
            platformGetValueStatic with NETSCAPE_PLUGIN_API;
            Guard privateBrowsingStateChanged() and setJavaScriptPaused() with
            PLATFORM(MAC) or PLATFORM(CHROMIUM)
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58043 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/PluginView.cpp
+++ third_party/WebKit/WebCore/plugins/PluginView.cpp
@@ -1297,7 +1297,6 @@ void PluginView::keepAlive(NPP instance)
 
     view->keepAlive();
 }
-#endif
 
 NPError PluginView::getValueStatic(NPNVariable variable, void* value)
 {
@@ -1322,7 +1321,6 @@ NPError PluginView::getValue(NPNVariable variable, void* value)
         return result;
 
     switch (variable) {
-#if ENABLE(NETSCAPE_PLUGIN_API)
     case NPNVWindowNPObject: {
         if (m_isJavaScriptPaused)
             return NPERR_GENERIC_ERROR;
@@ -1357,7 +1355,6 @@ NPError PluginView::getValue(NPNVariable variable, void* value)
 
         return NPERR_NO_ERROR;
     }
-#endif
 
     case NPNVprivateModeBool: {
         Page* page = m_parentFrame->page();
@@ -1371,6 +1368,7 @@ NPError PluginView::getValue(NPNVariable variable, void* value)
         return NPERR_GENERIC_ERROR;
     }
 }
+#endif
 
 void PluginView::privateBrowsingStateChanged(bool privateBrowsingEnabled)
 {
--- third_party/WebKit/WebCore/plugins/PluginViewNone.cpp
+++ third_party/WebKit/WebCore/plugins/PluginViewNone.cpp
@@ -68,12 +68,12 @@ NPError PluginView::handlePostReadFile(Vector<char>&, uint32, const char*)
     return 0;
 }
 
+#if ENABLE(NETSCAPE_PLUGIN_API)
 bool PluginView::platformGetValue(NPNVariable, void*, NPError*)
 {
     return false;
 }
 
-#if ENABLE(NETSCAPE_PLUGIN_API)
 bool PluginView::platformGetValueStatic(NPNVariable, void*, NPError*)
 {
     return false;
@@ -127,6 +127,7 @@ void PluginView::keepAlive(NPP)
 }
 #endif
 
+#if PLATFORM(MAC) || PLATFORM(CHROMIUM)
 void PluginView::privateBrowsingStateChanged(bool)
 {
 }
@@ -134,6 +135,7 @@ void PluginView::privateBrowsingStateChanged(bool)
 void PluginView::setJavaScriptPaused(bool)
 {
 }
+#endif
 
 #if defined(XP_UNIX) && ENABLE(NETSCAPE_PLUGIN_API)
 void PluginView::handleFocusInEvent()
