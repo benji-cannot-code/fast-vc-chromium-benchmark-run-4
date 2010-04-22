a8f632a96d1eb098f80de412e8e093149b8705ea
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-22 07:29:58 +0000

    2010-04-22  Stephan Aßmus  <superstippi@gmx.de>
            Reviewed by David Levin.
    
            [Haiku] Improve SimpleFontDataHaiku
            https://bugs.webkit.org/show_bug.cgi?id=37411
    
            Covered by existing tests.
    
            * platform/graphics/haiku/SimpleFontDataHaiku.cpp:
                - Cleaned up includes
                - Removed prototype "charUnicodeToUTF8HACK"
            (WebCore::SimpleFontData::platformInit):
                - Use const BFont pointer
            (WebCore::SimpleFontData::smallCapsFontData):
                - "fontPlatformData" was leaked. It is only
                  used as the key for the font cache lookup.
            (WebCore::SimpleFontData::platformMetricsForGlyph):
                - Use existing WebCore encoding infrastructure
                  instead of adding a hack for Haiku.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/haiku/SimpleFontDataHaiku.cpp
+++ third_party/WebKit/WebCore/platform/graphics/haiku/SimpleFontDataHaiku.cpp
@@ -35,18 +35,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FontCache.h"
 #include "FontDescription.h"
 #include "NotImplemented.h"
-#include <Rect.h>
-#include <unicode/uchar.h>
-#include <unicode/unorm.h>
+#include "TextEncoding.h"
+#include <wtf/text/CString.h>
 
-
-extern int charUnicodeToUTF8HACK(unsigned short, char*);
 
 namespace WebCore {
 
 void SimpleFontData::platformInit()
 {
-    BFont* font = m_platformData.font();
+    const BFont* font = m_platformData.font();
     if (!font)
         return;
 
@@ -77,8 +74,8 @@ SimpleFontData* SimpleFontData::smallCapsFontData(const FontDescription& fontDes
     if (!m_smallCapsFontData) {
         FontDescription desc = FontDescription(fontDescription);
         desc.setSpecifiedSize(0.70f * fontDescription.computedSize());
-        const FontPlatformData* fontPlatformData = new FontPlatformData(desc, desc.family().family());
-        m_smallCapsFontData = new SimpleFontData(*fontPlatformData);
+        FontPlatformData fontPlatformData(desc, desc.family().family());
+        m_smallCapsFontData = new SimpleFontData(fontPlatformData);
     }
     return m_smallCapsFontData;
 }
@@ -96,15 +93,14 @@ void SimpleFontData::determinePitch()
 
 GlyphMetrics SimpleFontData::platformMetricsForGlyph(Glyph glyph, GlyphMetricsMode) const
 {
-    if (!m_platformData.font())
-        return 0;
-
-    char charArray[4];
-    float escapements[1];
-
-    charUnicodeToUTF8HACK(glyph, charArray);
-    m_platformData.font()->GetEscapements(charArray, 1, escapements);
     GlyphMetrics metrics;
+    if (!m_platformData.font())
+        return metrics;
+
+    CString encoded = UTF8Encoding().encode(static_cast<UChar*>(&glyph), 1,
+                                            URLEncodedEntitiesForUnencodables);
+    float escapements[1];
+    m_platformData.font()->GetEscapements(encoded.data(), 1, escapements);
     metrics.horizontalAdvance = escapements[0] * m_platformData.font()->Size();
     return metrics;
 }
