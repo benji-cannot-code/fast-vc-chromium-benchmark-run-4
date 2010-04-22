8856ab1ee9bcc7fe23822d31fda8a3c8d3496439
aestes@apple.com <aestes@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-22 07:11:39 +0000

    Reference count WebView's backing store bitmap to prevent deleteBackingStore() from freeing the bitmap while it is still being referenced by Core Animation.
    Reviewed by Maciej Stachowiak.
    
    https://bugs.webkit.org/show_bug.cgi?id=37954
    
    * WebView.cpp:
    (WebView::ensureBackingStore):
    (WebView::addToDirtyRegion):
    (WebView::scrollBackingStore):
    (WebView::updateBackingStore):
    (WebView::paint):
    (WebView::backingStore):
    (releaseBackingStoreCallback): deref m_backingStoreBitmap once Core
    Animation has dropeed its reference to the memory.
    (WebView::updateRootLayerContents): ref m_backingStoreBitmap before
    passing the memory to Core Animation to prevent deleteBackingStore()
    from freeing it while it is still referenced by CA.
    * WebView.h: Make m_backingStoreBitmap a RefCountedGDIHandle<HBITMAP>,
    and make m_backingStoreDirtyRegion a RefCountedGDIHandle<HRGN>.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58067 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -759,7 +759,7 @@ bool WebView::ensureBackingStore()
         BitmapInfo bitmapInfo = BitmapInfo::createBottomUp(IntSize(m_backingStoreSize));
 
         void* pixels = NULL;
-        m_backingStoreBitmap.set(::CreateDIBSection(NULL, &bitmapInfo, DIB_RGB_COLORS, &pixels, NULL, 0));
+        m_backingStoreBitmap = RefCountedHBITMAP::create(::CreateDIBSection(0, &bitmapInfo, DIB_RGB_COLORS, &pixels, 0, 0));
         return true;
     }
 
@@ -783,11 +783,11 @@ void WebView::addToDirtyRegion(HRGN newRegion)
 
     if (m_backingStoreDirtyRegion) {
         HRGN combinedRegion = ::CreateRectRgn(0,0,0,0);
-        ::CombineRgn(combinedRegion, m_backingStoreDirtyRegion.get(), newRegion, RGN_OR);
+        ::CombineRgn(combinedRegion, m_backingStoreDirtyRegion->handle(), newRegion, RGN_OR);
         ::DeleteObject(newRegion);
-        m_backingStoreDirtyRegion.set(combinedRegion);
+        m_backingStoreDirtyRegion = RefCountedHRGN::create(combinedRegion);
     } else
-        m_backingStoreDirtyRegion.set(newRegion);
+        m_backingStoreDirtyRegion = RefCountedHRGN::create(newRegion);
 
 #if USE(ACCELERATED_COMPOSITING)
     if (m_layerRenderer)
@@ -816,7 +816,7 @@ void WebView::scrollBackingStore(FrameView* frameView, int dx, int dy, const Int
     // Collect our device context info and select the bitmap to scroll.
     HDC windowDC = ::GetDC(m_viewWindow);
     HDC bitmapDC = ::CreateCompatibleDC(windowDC);
-    ::SelectObject(bitmapDC, m_backingStoreBitmap.get());
+    ::SelectObject(bitmapDC, m_backingStoreBitmap->handle());
     
     // Scroll the bitmap.
     RECT scrollRectWin(scrollViewRect);
@@ -893,7 +893,7 @@ void WebView::updateBackingStore(FrameView* frameView, HDC dc, bool backingStore
     if (!dc) {
         windowDC = ::GetDC(m_viewWindow);
         bitmapDC = ::CreateCompatibleDC(windowDC);
-        ::SelectObject(bitmapDC, m_backingStoreBitmap.get());
+        ::SelectObject(bitmapDC, m_backingStoreBitmap->handle());
     }
 
     if (m_backingStoreBitmap && (m_backingStoreDirtyRegion || backingStoreCompletelyDirty)) {
@@ -905,8 +905,8 @@ void WebView::updateBackingStore(FrameView* frameView, HDC dc, bool backingStore
         Vector<IntRect> paintRects;
         if (!backingStoreCompletelyDirty) {
             RECT regionBox;
-            ::GetRgnBox(m_backingStoreDirtyRegion.get(), &regionBox);
-            getUpdateRects(m_backingStoreDirtyRegion.get(), regionBox, paintRects);
+            ::GetRgnBox(m_backingStoreDirtyRegion->handle(), &regionBox);
+            getUpdateRects(m_backingStoreDirtyRegion->handle(), regionBox, paintRects);
         } else {
             RECT clientRect;
             ::GetClientRect(m_viewWindow, &clientRect);
@@ -971,7 +971,7 @@ void WebView::paint(HDC dc, LPARAM options)
 
     HDC bitmapDC = ::CreateCompatibleDC(hdc);
     bool backingStoreCompletelyDirty = ensureBackingStore();
-    ::SelectObject(bitmapDC, m_backingStoreBitmap.get());
+    ::SelectObject(bitmapDC, m_backingStoreBitmap->handle());
 
     // Update our backing store if needed.
     updateBackingStore(frameView, bitmapDC, backingStoreCompletelyDirty, windowsToPaint);
@@ -5617,7 +5617,7 @@ HRESULT STDMETHODCALLTYPE WebView::backingStore(
 {
     if (!hBitmap)
         return E_POINTER;
-    *hBitmap = reinterpret_cast<OLE_HANDLE>(m_backingStoreBitmap.get());
+    *hBitmap = reinterpret_cast<OLE_HANDLE>(m_backingStoreBitmap->handle());
     return S_OK;
 }
 
@@ -6117,6 +6117,14 @@ void WebView::setAcceleratedCompositing(bool accelerated)
     }
 }
 
+void releaseBackingStoreCallback(void* info, const void* data, size_t size)
+{
+    // Release the backing store bitmap previously retained by updateRootLayerContents().
+    ASSERT(info);
+    if (info)
+        static_cast<RefCountedHBITMAP*>(info)->deref();
+}
+
 void WebView::updateRootLayerContents()
 {
     if (!m_backingStoreBitmap || !m_layerRenderer)
@@ -6124,13 +6132,12 @@ void WebView::updateRootLayerContents()
 
     // Get the backing store into a CGImage
     BITMAP bitmap;
-    GetObject(m_backingStoreBitmap.get(), sizeof(bitmap), &bitmap);
-    int bmSize = bitmap.bmWidthBytes * bitmap.bmHeight;
-    RetainPtr<CFDataRef> data(AdoptCF, 
-                                CFDataCreateWithBytesNoCopy(
-                                        0, static_cast<UInt8*>(bitmap.bmBits),
-                                        bmSize, kCFAllocatorNull));
-    RetainPtr<CGDataProviderRef> cgData(AdoptCF, CGDataProviderCreateWithCFData(data.get()));
+    GetObject(m_backingStoreBitmap->handle(), sizeof(bitmap), &bitmap);
+    size_t bmSize = bitmap.bmWidthBytes * bitmap.bmHeight;
+    RetainPtr<CGDataProviderRef> cgData(AdoptCF,
+        CGDataProviderCreateWithData(static_cast<void*>(m_backingStoreBitmap.get()),
+                                                        bitmap.bmBits, bmSize,
+                                                        releaseBackingStoreCallback));
     RetainPtr<CGColorSpaceRef> space(AdoptCF, CGColorSpaceCreateDeviceRGB());
     RetainPtr<CGImageRef> backingStoreImage(AdoptCF, CGImageCreate(bitmap.bmWidth, bitmap.bmHeight,
                                      8, bitmap.bmBitsPixel, 
@@ -6138,6 +6145,11 @@ void WebView::updateRootLayerContents()
                                      kCGBitmapByteOrder32Little | kCGImageAlphaNoneSkipFirst,
                                      cgData.get(), 0, false, 
                                      kCGRenderingIntentDefault));
+
+    // Retain the backing store bitmap so that it is not deleted by deleteBackingStore()
+    // while still in use within CA. When CA is done with the bitmap, it will
+    // call releaseBackingStoreCallback(), which will release the backing store bitmap.
+    m_backingStoreBitmap->ref();
 
     // Hand the CGImage to CACF for compositing
     m_layerRenderer->setRootContents(backingStoreImage.get());
--- third_party/WebKit/WebKit/win/WebView.h
+++ third_party/WebKit/WebKit/win/WebView.h
@@ -33,18 +33,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebPreferences.h"
 #include <WebCore/DragActions.h>
 #include <WebCore/IntRect.h>
+#include <WebCore/RefCountedGDIHandle.h>
 #include <WebCore/Timer.h>
 #include <WebCore/WindowMessageListener.h>
 #include <WebCore/WKCACFLayer.h>
 #include <WebCore/WKCACFLayerRenderer.h>
 #include <wtf/HashSet.h>
 #include <wtf/OwnPtr.h>
+#include <wtf/RefPtr.h>
 
 class FullscreenVideoController;
 class WebBackForwardList;
 class WebFrame;
 class WebInspector;
 class WebInspectorClient;
+
+typedef WebCore::RefCountedGDIHandle<HBITMAP> RefCountedHBITMAP;
+typedef WebCore::RefCountedGDIHandle<HRGN> RefCountedHRGN;
 
 WebView* kit(WebCore::Page*);
 WebCore::Page* core(IWebView*);
@@ -940,9 +945,9 @@ protected:
     WebFrame* m_mainFrame;
     WebCore::Page* m_page;
     
-    OwnPtr<HBITMAP> m_backingStoreBitmap;
+    RefPtr<RefCountedHBITMAP> m_backingStoreBitmap;
     SIZE m_backingStoreSize;
-    OwnPtr<HRGN> m_backingStoreDirtyRegion;
+    RefPtr<RefCountedHRGN> m_backingStoreDirtyRegion;
 
     COMPtr<IWebEditingDelegate> m_editingDelegate;
     COMPtr<IWebFrameLoadDelegate> m_frameLoadDelegate;
