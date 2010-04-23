c338a80c3fed823b3dfc94d1d2a808bf407094e0
mirandac@google.com <mirandac@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-23 23:09:34 +0000

    Fix XP crasher in Search Engine Experiment.
    This was causing a crash because the View "owns" its images, and was trying to delete the ImageView which had already been deleted in a scoped_ptr.  Solution: put the ImageView in a regular pointer instead.
    
    BUG= 42346
    TEST=run with search engine experiment enabled in XP, don't crash.
    
    Review URL: http://codereview.chromium.org/1696010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45510 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/first_run_search_engine_view.cc
+++ chrome/browser/views/first_run_search_engine_view.cc
@@ -121,7 +121,7 @@ SearchEngineChoice::SearchEngineChoice(views::ButtonListener* listener,
       logo_image->SetImageSize(gfx::Size(kSmallLogoWidth, kSmallLogoHeight));
     // Tooltip text provides accessibility.
     logo_image->SetTooltipText(search_engine_->short_name());
-    choice_view_.reset(logo_image);
+    choice_view_ = logo_image;
   } else {
     // No logo -- we must show a text label.
     views::Label* logo_label = new views::Label(search_engine_->short_name());
@@ -129,7 +129,7 @@ SearchEngineChoice::SearchEngineChoice(views::ButtonListener* listener,
     logo_label->SetFont(logo_label->font().DeriveFont(2, gfx::Font::NORMAL));
     logo_label->SetHorizontalAlignment(views::Label::ALIGN_CENTER);
     logo_label->SetTooltipText(search_engine_->short_name());
-    choice_view_.reset(logo_label);
+    choice_view_ = logo_label;
   }
 }
 
--- chrome/browser/views/first_run_search_engine_view.h
+++ chrome/browser/views/first_run_search_engine_view.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "base/scoped_ptr.h"
 #include "chrome/browser/views/keyword_editor_view.h"
 #include "gfx/size.h"
 #include "views/controls/button/native_button.h"
@@ -43,7 +42,7 @@ class SearchEngineChoice : public views::NativeButton {
 
   // These methods return data about the logo or text view associated
   // with this search engine choice.
-  views::View* GetView() { return choice_view_.get(); }
+  views::View* GetView() { return choice_view_; }
   int GetChoiceViewWidth();
   int GetChoiceViewHeight();
 
@@ -55,8 +54,9 @@ class SearchEngineChoice : public views::NativeButton {
   const TemplateURL* GetSearchEngine() { return search_engine_; }
 
  private:
-  // Either an ImageView of a logo, or a Label with text.
-  scoped_ptr<views::View> choice_view_;
+  // Either an ImageView of a logo, or a Label with text.  Owned by
+  // FirstRunSearchEngineView.
+  views::View* choice_view_;
 
   // True if choice_view_ is holding an ImageView.
   bool is_image_label_;
