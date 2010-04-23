71709d3d9c6707f2c1e691fb8c50278ab6209279
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-23 00:16:22 +0000

    Errk, accidentally committed commenting out two ASSERTs! reverting.
    Reviewed by NOBODY (build fix).
    
    * runtime/Collector.cpp:
    (JSC::Heap::protect):
    (JSC::Heap::unprotect):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58134 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/Collector.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/Collector.cpp
@@ -973,7 +973,7 @@ void Heap::markStackObjectsConservatively(MarkStack& markStack)
 void Heap::protect(JSValue k)
 {
     ASSERT(k);
-//    ASSERT(JSLock::currentThreadIsHoldingLock() || !m_globalData->isSharedInstance());
+    ASSERT(JSLock::currentThreadIsHoldingLock() || !m_globalData->isSharedInstance());
 
     if (!k.isCell())
         return;
@@ -984,7 +984,7 @@ void Heap::protect(JSValue k)
 bool Heap::unprotect(JSValue k)
 {
     ASSERT(k);
-//    ASSERT(JSLock::currentThreadIsHoldingLock() || !m_globalData->isSharedInstance());
+    ASSERT(JSLock::currentThreadIsHoldingLock() || !m_globalData->isSharedInstance());
 
     if (!k.isCell())
         return false;
