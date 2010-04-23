84c9136adc39bf34789e83b8cc59329abdbf5470
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-23 20:22:17 +0000

    2010-04-23  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dan Bernstein.
    
            CompositeAnimation::updateKeyframeAnimations() can make a KeyframeAnimation for animation "none"
            https://bugs.webkit.org/show_bug.cgi?id=38017
    
            Check to see if the animation name is "none", and don't fire off a keyframe animation in
            that case.
    
            * page/animation/CompositeAnimation.cpp:
            (WebCore::CompositeAnimation::updateKeyframeAnimations):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58185 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/CompositeAnimation.cpp
+++ third_party/WebKit/WebCore/page/animation/CompositeAnimation.cpp
@@ -206,6 +206,8 @@ void CompositeAnimation::updateKeyframeAnimations(RenderObject* renderer, Render
             
         // Toss the animation order map.
         m_keyframeAnimationOrderMap.clear();
+
+        DEFINE_STATIC_LOCAL(const AtomicString, none, ("none"));
         
         // Now mark any still active animations as active and add any new animations.
         if (targetStyle->animations()) {
@@ -233,7 +235,7 @@ void CompositeAnimation::updateKeyframeAnimations(RenderObject* renderer, Render
                     // Set the saved animation to this new one, just in case the play state has changed.
                     keyframeAnim->setAnimation(anim);
                     keyframeAnim->setIndex(i);
-                } else if ((anim->duration() || anim->delay()) && anim->iterationCount()) {
+                } else if ((anim->duration() || anim->delay()) && anim->iterationCount() && animationName != none) {
                     keyframeAnim = KeyframeAnimation::create(const_cast<Animation*>(anim), renderer, i, this, targetStyle);
                     m_keyframeAnimations.set(keyframeAnim->name().impl(), keyframeAnim);
                 }
