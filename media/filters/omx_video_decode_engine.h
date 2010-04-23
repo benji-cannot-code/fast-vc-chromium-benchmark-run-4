4aa45aec7fdd877af17b2a2ee4bc3ef5d2cb81d0
jiesun@google.com <jiesun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-23 16:24:46 +0000

    Remove AVFrame dependency.
    Currently AVFrame is used in both OmxVideoDecodeEngine and FFmpegVideoDecodeEngine.  This causes an unnecessary memory copy from AVFrame to VideoFrame.  We can remove the additional copy by using VideoFrame.
    
    Also in the hardware (OpenMAX) path, VideoFrames are allocated inside decode engines, therefore the interface was changed such that VideoFrames are created inside the decode engines but buffered in VideoDecoderImpl.
    
    BUGS=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1669002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45448 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/omx_video_decode_engine.h
+++ media/filters/omx_video_decode_engine.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class MessageLoop;
 
 // FFmpeg types.
-struct AVFrame;
 struct AVRational;
 struct AVStream;
 
@@ -35,7 +34,8 @@ class OmxVideoDecodeEngine : public VideoDecodeEngine,
 
   // Implementation of the VideoDecodeEngine Interface.
   virtual void Initialize(AVStream* stream, Task* done_cb);
-  virtual void DecodeFrame(Buffer* buffer, AVFrame* yuv_frame,
+  virtual void DecodeFrame(Buffer* buffer,
+                           scoped_refptr<VideoFrame>* video_frame,
                            bool* got_result, Task* done_cb);
   virtual void Flush(Task* done_cb);
   virtual VideoFrame::Format GetSurfaceFormat() const;
@@ -84,13 +84,13 @@ class OmxVideoDecodeEngine : public VideoDecodeEngine,
   // A struct to hold parameters of a decode request. Objects pointed by
   // these parameters are owned by the caller.
   struct DecodeRequest {
-    DecodeRequest(AVFrame* f, bool* b, Task* cb)
+    DecodeRequest(scoped_refptr<VideoFrame>* f, bool* b, Task* cb)
         : frame(f),
           got_result(b),
           done_cb(cb) {
     }
 
-    AVFrame* frame;
+    scoped_refptr<VideoFrame>* frame;
     bool* got_result;
     Task* done_cb;
   };
