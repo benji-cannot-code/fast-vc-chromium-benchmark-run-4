d984845fe482a231fce10a5415544abba53c5927
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-23 03:03:26 +0000

    Added Google Cache Invalidation library to DEPS and got it to compile with the Chromium libraries.
    Added some override files to make it work, the biggest of which defines
    NewPermanentCallback() overrides that base/callback.h doesn't handle.
    
    BUG=34646
    TEST=added unittests
    
    Review URL: http://codereview.chromium.org/1720001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45412 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ third_party/cacheinvalidation/overrides/google/cacheinvalidation/logging.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GOOGLE_CACHEINVALIDATION_LOGGING_H_
+#define GOOGLE_CACHEINVALIDATION_LOGGING_H_
+
+#include "base/logging.h"
+
+namespace invalidation {
+
+using logging::LogMessage;
+
+}  // namespace invalidation
+
+#endif  // GOOGLE_CACHEINVALIDATION_LOGGING_H_
