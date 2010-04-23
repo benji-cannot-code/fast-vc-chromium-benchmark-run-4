3a959c93025d31a27faa47725a1e5dab2923d9c1
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-23 16:04:14 +0000

    Fix cases where SpdySession can double-remove from the SpdySessionPool.
    BUG=42229
    TEST=SpdyNetworkTransactionTest.GoAwayWithActiveStream
    
    Review URL: http://codereview.chromium.org/1747006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45447 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session.cc
+++ net/spdy/spdy_session.cc
@@ -211,7 +211,8 @@ SpdySession::SpdySession(const HostPortPair& host_port_pair,
       streams_initiated_count_(0),
       streams_pushed_count_(0),
       streams_pushed_and_claimed_count_(0),
-      streams_abandoned_count_(0) {
+      streams_abandoned_count_(0),
+      in_session_pool_(true) {
   // TODO(mbelshe): consider randomization of the stream_hi_water_mark.
 
   spdy_framer_.set_visitor(this);
@@ -804,7 +805,7 @@ void SpdySession::CloseSessionOnError(net::Error err) {
     state_ = CLOSED;
     error_ = err;
     CloseAllStreams(err);
-    session_->spdy_session_pool()->Remove(this);
+    RemoveFromPool();
   }
 }
 
@@ -829,6 +830,13 @@ void SpdySession::DeactivateStream(spdy::SpdyStreamId id) {
   }
 
   active_streams_.erase(id);
+}
+
+void SpdySession::RemoveFromPool() {
+  if (in_session_pool_) {
+    session_->spdy_session_pool()->Remove(this);
+    in_session_pool_ = false;
+  }
 }
 
 scoped_refptr<SpdyStream> SpdySession::GetPushStream(const std::string& path) {
@@ -1103,7 +1111,7 @@ void SpdySession::OnFin(const spdy::SpdyRstStreamControlFrame& frame) {
 void SpdySession::OnGoAway(const spdy::SpdyGoAwayControlFrame& frame) {
   LOG(INFO) << "Spdy GOAWAY for session[" << this << "] for " <<
       host_port_pair().ToString();
-  session_->spdy_session_pool()->Remove(this);
+  RemoveFromPool();
 
   // TODO(willchan): Cancel any streams that are past the GoAway frame's
   // |last_accepted_stream_id|.
