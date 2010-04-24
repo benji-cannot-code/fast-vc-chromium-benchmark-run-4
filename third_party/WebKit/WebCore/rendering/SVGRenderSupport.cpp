13a7feae4c200723e053b44924bc5c61540dfec8
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-24 12:47:00 +0000

    2010-04-24  Nikolas Zimmermann  <nzimmermann@rim.com>
            Not reviewed. Fix linkage on several bots (no idea, why it worked in a from-scratch rebuild on Leopard on my machine!) -> include NodeRenderStyle.h
            Having to include NodeRenderStyle.h just to pull in the renderStyle() is a crazy concept, but I see what it tries to avoid (including RenderObject.h in Node.h)
    
            * rendering/SVGRenderSupport.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58214 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/SVGRenderSupport.cpp
+++ third_party/WebKit/WebCore/rendering/SVGRenderSupport.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AffineTransform.h"
 #include "Document.h"
 #include "ImageBuffer.h"
+#include "NodeRenderStyle.h"
 #include "RenderObject.h"
 #include "RenderSVGContainer.h"
 #include "RenderSVGResource.h"
