c963cb4b75a11d918edbe3e4f706c55f1f0e3c42
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-24 00:28:01 +0000

    This adds a new kind of browser window: EXTENSION_APP. EXTENSION_APP windows have a bigger titlebar with a giant icon and title, and supports tabs.
    Review URL: http://codereview.chromium.org/1730010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45520 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/view_ids.h
+++ chrome/browser/view_ids.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,6 +28,8 @@ enum ViewID {
   VIEW_ID_TAB_9,
   VIEW_ID_TAB_LAST,
 
+  VIEW_ID_EXTENSION_APP_ICON,
+  VIEW_ID_EXTENSION_APP_TITLE,
   VIEW_ID_TAB_STRIP,
 
   // Toolbar & toolbar elements.
