3e04dd1035df3c2ff87cfc764baabf92cedf6498
albertb@chromium.org <albertb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-26 23:00:56 +0000

    Send out notifications when logins are changed through PasswordStoreMac so that the sync backend can register as an observer.
    BUG=35038
    TEST=none
    
    Review URL: http://codereview.chromium.org/1752007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45640 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/password_store_mac.h
+++ chrome/browser/password_manager/password_store_mac.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/thread.h"
 #include "chrome/browser/password_manager/login_database.h"
 #include "chrome/browser/password_manager/password_store.h"
+#include "chrome/common/notification_service.h"
 
 class MacKeychain;
 
@@ -69,11 +70,19 @@ class PasswordStoreMac : public PasswordStore {
   void RemoveKeychainForms(
       const std::vector<webkit_glue::PasswordForm*>& forms);
 
+  // Allows the creation of |notification_service_| to be scheduled on the right
+  // thread.
+  void CreateNotificationService();
+
   scoped_ptr<MacKeychain> keychain_;
   scoped_ptr<LoginDatabase> login_metadata_db_;
 
   // Thread that the synchronous methods are run on.
   scoped_ptr<base::Thread> thread_;
+
+  // Since we aren't running on a well-known thread but still want to send out
+  // notifications, we need to run our own service.
+  scoped_ptr<NotificationService> notification_service_;
 
   DISALLOW_COPY_AND_ASSIGN(PasswordStoreMac);
 };
