322db635a7ebd151d804100c3df43502201286d8
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-26 18:57:00 +0000

    Increase the global TCP socket limit.  It's too low. wtc likes powers of 2, so I chose 256 as we discussed earlier.  We need to experiment more in the future. BUG=41289
    Review URL: http://codereview.chromium.org/1692008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45600 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_session.cc
+++ net/http/http_network_session.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 // static
-int HttpNetworkSession::max_sockets_ = 100;
+int HttpNetworkSession::max_sockets_ = 256;
 
 // static
 int HttpNetworkSession::max_sockets_per_group_ = 6;
