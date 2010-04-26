947cf5938d07834e802663b7ddd2723d48f3f91b
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-26 22:43:07 +0000

    POSIX: Make sure files in plugin directories are regular files.
    BUG=none
    TEST=Visiting about:plugins with MOZ_PLUGIN_PATH=/tmp does not hang when there is a pipe in /tmp.
    Review URL: http://codereview.chromium.org/1700014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45638 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/plugin_list_posix.cc
+++ webkit/glue/plugins/plugin_list_posix.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -95,9 +95,9 @@ void PluginList::GetPluginDirectories(std::vector<FilePath>* plugin_dirs) {
   // 2) NS_USER_PLUGINS_DIR: ~/.mozilla/plugins.
   // This is a de-facto standard, so even though we're not Mozilla, let's
   // look in there too.
-  const char* home = getenv("HOME");
-  if (home)
-    plugin_dirs->push_back(FilePath(home).Append(".mozilla/plugins"));
+  FilePath home = file_util::GetHomeDir();
+  if (!home.empty())
+    plugin_dirs->push_back(home.Append(".mozilla/plugins"));
 
   // 3) NS_SYSTEM_PLUGINS_DIR:
   // This varies across different browsers and versions, so check 'em all.
@@ -214,4 +214,4 @@ bool PluginList::ShouldLoadPlugin(const WebPluginInfo& info,
   return true;
 }
 
-} // namespace NPAPI
+}  // namespace NPAPI
