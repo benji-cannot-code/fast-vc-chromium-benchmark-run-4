253338f7d866aa8b00dd6afe8d0ed56cdd51119f
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-26 14:29:09 +0000

    Add message to rebaseline.sh
    The rebaseline tool now works on the upstream test_expectations.txt.
    If someone adds a REBASELINE tag to the local version we'll now print
    a (hopefully) helpful message telling them what's up.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45577 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/layout_tests/rebaseline.sh
+++ webkit/tools/layout_tests/rebaseline.sh
@@ -7,6 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 exec_dir=$(dirname $0)
 
+if grep -q REBASELINE $exec_dir/test_expectations.txt ; then
+  echo "test_expectations.txt has been moved upstream! The test_expectations.txt"
+  echo "here is just a set of local overrides. You have to put the REBASELINE "
+  echo "tags in the upstream version and commit the new baselines into the WebKit"
+  echo "tree."
+  echo
+  echo "Try:"
+  echo "cd ../../../third_party/WebKit"
+  echo "\$EDITOR LayoutTests/platform/chromium/test_expectations.txt"
+  echo "./WebKitTools/Scripts/rebaseline-chromium-webkit-tests"
+  echo
+  echo "See also https://trac.webkit.org/wiki/Rebaseline"
+  exit 1
+fi
+
 PYTHON_PROG=python
 
 "$PYTHON_PROG" "$exec_dir/../../../third_party/WebKit/WebKitTools/Scripts/rebaseline-chromium-webkit-tests" "$@"
