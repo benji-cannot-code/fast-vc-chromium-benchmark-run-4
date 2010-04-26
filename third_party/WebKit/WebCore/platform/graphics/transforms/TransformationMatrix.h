c1489efef0aedc4faf780b19910031ff70f21d4f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-26 14:58:02 +0000

    2010-04-26  Ruben Van Boxem  <vanboxem.ruben@gmail.com>
            Reviewed by Adam Roben.
    
            Fixes for Win64 compilation under gcc (mingw-w64)
    
            *  WebCore/bridge/npapi.h: for win64 compatibility, mirroring mozilla-central, see Mozilla bug 560298
            *  WebCore/platform/Arena.h: uword is used to cast from pointers here. unsigned long is 32-bit on Windows (but 64-bit on mac), and too small to hold a pointer. uintptr_t is 32-bit on 32-bit systems (mac, linux and windows) and 64-bit on all 64-bit systems
            *  WebCore/platform/graphics/transforms/TransformationMatrix.h: let mingw-w64/w32 use MSVC codepath
            *  WebCore/platform/text/TextStream.cpp: let mingw-w64 use MSVC codepath
            *  WebCore/platform/text/TextStream.h: let mingw-w64 use MSVC codepath
            *  WebCore/plugins/PluginView.cpp: fix pointer casts on WIN64 and let mingw-w64 use MSVC codepath
            *  WebCore/plugins/win/PluginViewWin.cpp: fix pointer casts on WIN64
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/transforms/TransformationMatrix.h
+++ third_party/WebKit/WebCore/platform/graphics/transforms/TransformationMatrix.h
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if PLATFORM(WIN) || (PLATFORM(QT) && OS(WINDOWS)) || (PLATFORM(WX) && OS(WINDOWS))
-#if COMPILER(MINGW)
+#if COMPILER(MINGW) && !COMPILER(MINGW64)
 typedef struct _XFORM XFORM;
 #else
 typedef struct tagXFORM XFORM;
--- third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
+++ third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
@@ -545,7 +545,7 @@ void PluginView::paintIntoTransformedContext(HDC hdc)
 
     NPEvent npEvent;
     npEvent.event = WM_WINDOWPOSCHANGED;
-    npEvent.lParam = reinterpret_cast<uint32>(&windowpos);
+    npEvent.lParam = reinterpret_cast<uintptr_t>(&windowpos);
     npEvent.wParam = 0;
 
     dispatchNPEvent(npEvent);
@@ -553,7 +553,7 @@ void PluginView::paintIntoTransformedContext(HDC hdc)
     setNPWindowRect(frameRect());
 
     npEvent.event = WM_PAINT;
-    npEvent.wParam = reinterpret_cast<uint32>(hdc);
+    npEvent.wParam = reinterpret_cast<uintptr_t>(hdc);
 
     // This is supposed to be a pointer to the dirty rect, but it seems that the Flash plugin
     // ignores it so we just pass null.
@@ -830,7 +830,7 @@ void PluginView::setNPWindowRect(const IntRect& rect)
 #else
         WNDPROC currentWndProc = (WNDPROC)GetWindowLongPtr(platformPluginWidget(), GWLP_WNDPROC);
         if (currentWndProc != PluginViewWndProc)
-            m_pluginWndProc = (WNDPROC)SetWindowLongPtr(platformPluginWidget(), GWLP_WNDPROC, (LONG)PluginViewWndProc);
+            m_pluginWndProc = (WNDPROC)SetWindowLongPtr(platformPluginWidget(), GWLP_WNDPROC, (LONG_PTR)PluginViewWndProc);
 #endif
     }
 }
@@ -979,7 +979,7 @@ bool PluginView::platformStart()
 
         // Calling SetWindowLongPtrA here makes the window proc ASCII, which is required by at least
         // the Shockwave Director plug-in.
-#if OS(WINDOWS) && CPU(X86_64) && COMPILER(MSVC)
+#if OS(WINDOWS) && CPU(X86_64)
         ::SetWindowLongPtrA(platformPluginWidget(), GWLP_WNDPROC, (LONG_PTR)DefWindowProcA);
 #elif OS(WINCE)
         ::SetWindowLong(platformPluginWidget(), GWL_WNDPROC, (LONG)DefWindowProc);
