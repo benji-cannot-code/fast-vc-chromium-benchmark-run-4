71e4e214e88fc3370d8955cae851c3a1f7c03922
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-26 06:04:02 +0000

    Some more chrome frame test pure virtual function call crash fixes. There is another crash which occurs when the remote COM apartment running in Ie shuts down and tries to free references to the web browser event sink. We unsubscribe from the event sink in the destructor of the ComStackObjectWithUninitialize object. The call to DispEventUnadvise fails every time. Unsubscribing and call CoDisconnectObject to release the server side stub references in the OnQuit notification fixes the crash.
    To aid debugging this issue in the future we now override the purecall handler in chrome frame tests.
    
    Bug=41980
    TBR=amit
    
    
    Review URL: http://codereview.chromium.org/1695015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45569 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/chrome_frame_test_utils.h
+++ chrome_frame/test/chrome_frame_test_utils.h
@@ -136,7 +136,8 @@ class WebBrowserEventSink
     : public CComObjectRootEx<CComMultiThreadModel>,
       public IDispEventSimpleImpl<0, WebBrowserEventSink,
                                   &DIID_DWebBrowserEvents2>,
-      public WindowObserver {
+      public WindowObserver,
+      public IUnknown {
  public:
   typedef IDispEventSimpleImpl<0, WebBrowserEventSink,
                                &DIID_DWebBrowserEvents2> DispEventsImpl;
@@ -156,6 +157,7 @@ class WebBrowserEventSink
   }
 
 BEGIN_COM_MAP(WebBrowserEventSink)
+  COM_INTERFACE_ENTRY(IUnknown)
 END_COM_MAP()
 
 BEGIN_SINK_MAP(WebBrowserEventSink)
@@ -241,6 +243,8 @@ END_SINK_MAP()
       ::GetWindowThreadProcessId(hwnd, &process_id_to_wait_for_);
 
     OnQuit();
+    CoDisconnectObject(this, 0);
+    DispEventUnadvise(web_browser2_);
   }
 #ifdef _DEBUG
   STDMETHOD(Invoke)(DISPID dispid, REFIID riid,
