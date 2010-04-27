2cf07289bbd2cc96b6354cf75dd433ac6ff0f4b5
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-27 04:31:04 +0000

    Views: Disable BrowserKeyEventsTests.AccessKeys. It's crashing on Linux views.
    BUG=40037
    TEST=Linux Views bot goes green?
    TBR=oshima
    Review URL: http://codereview.chromium.org/1792004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45672 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_keyevents_browsertest.cc
+++ chrome/browser/browser_keyevents_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -448,7 +448,7 @@ IN_PROC_BROWSER_TEST_F(BrowserKeyEventsTest, CtrlKeyEvents) {
   EXPECT_NO_FATAL_FAILURE(TestKeyEvent(tab_index, kTestCtrlEnter));
 }
 
-#if defined(OS_CHROMEOS)
+#if defined(TOOLKIT_VIEWS) && defined(OS_LINUX)
 // See http://crbug.com/40037 for details.
 #define MAYBE_AccessKeys DISABLED_AccessKeys
 #else
