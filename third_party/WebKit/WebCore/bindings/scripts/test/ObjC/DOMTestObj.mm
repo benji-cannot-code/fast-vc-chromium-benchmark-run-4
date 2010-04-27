50e56ba412522b49c860de07cf214fa676c6b4bc
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-27 20:55:11 +0000

    2010-04-27  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Remove custom NodeIterator bindings
            https://bugs.webkit.org/show_bug.cgi?id=38188
    
            Another example of CallWith=ScriptState.  This case shows us the right
            way to handle the interaction with exception codes.
    
            In my previous patch, I put the exception code handling after the
            ScriptState exception handling.  It turns out this was a mistake
            (although a harmless one at the time because no IDL caused us to
            generate that code).  Now that we're generating both kinds of exception
            handling, we need to generate them in the right order.
    
            * WebCore.gypi:
            * bindings/js/JSNodeIteratorCustom.cpp:
            * bindings/scripts/CodeGeneratorJS.pm:
            * bindings/scripts/CodeGeneratorV8.pm:
            * bindings/scripts/test/GObject/WebKitDOMTestObj.cpp:
            (webkit_dom_test_obj_with_script_state_void_exception):
            (webkit_dom_test_obj_with_script_state_obj_exception):
            * bindings/scripts/test/GObject/WebKitDOMTestObj.h:
            * bindings/scripts/test/JS/JSTestObj.cpp:
            (WebCore::):
            (WebCore::jsTestObjPrototypeFunctionWithScriptStateVoidException):
            (WebCore::jsTestObjPrototypeFunctionWithScriptStateObjException):
            * bindings/scripts/test/JS/JSTestObj.h:
            * bindings/scripts/test/ObjC/DOMTestObj.h:
            * bindings/scripts/test/ObjC/DOMTestObj.mm:
            (-[DOMTestObj withScriptStateVoidException]):
            (-[DOMTestObj withScriptStateObjException]):
            * bindings/scripts/test/TestObj.idl:
            * bindings/scripts/test/V8/V8TestObj.cpp:
            (WebCore::TestObjInternal::withScriptStateVoidCallback):
            (WebCore::TestObjInternal::withScriptStateObjCallback):
            (WebCore::TestObjInternal::withScriptStateVoidExceptionCallback):
            (WebCore::TestObjInternal::withScriptStateObjExceptionCallback):
            (WebCore::):
            * bindings/v8/custom/V8NodeIteratorCustom.cpp: Removed.
            * dom/NodeIterator.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58333 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/test/ObjC/DOMTestObj.mm
+++ third_party/WebKit/WebCore/bindings/scripts/test/ObjC/DOMTestObj.mm
@@ -258,6 +258,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return kit(WTF::getPtr(IMPL->withScriptStateObj()));
 }
 
+- (void)withScriptStateVoidException
+{
+    WebCore::ExceptionCode ec = 0;
+    IMPL->withScriptStateVoidException(ec);
+    WebCore::raiseOnDOMError(ec);
+}
+
+- (DOMTestObj *)withScriptStateObjException
+{
+    WebCore::ExceptionCode ec = 0;
+    DOMTestObj *result = kit(WTF::getPtr(IMPL->withScriptStateObjException(ec)));
+    WebCore::raiseOnDOMError(ec);
+    return result;
+}
+
 - (void)methodWithOptionalArg:(int)opt
 {
     IMPL->methodWithOptionalArg(opt);
