35dc5f80e515d7772ebf0d8d8d4f2cfa7615d5ee
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-27 23:24:21 +0000

    2010-04-27  Kinuko Yasuda  <kinuko@chromium.org>
            Reviewed by Jian Li.
    
            [V8] Handle OR ('|') conditional in action_derivedsourcesallinone.py
            https://bugs.webkit.org/show_bug.cgi?id=38207
    
            No new tests.
    
            * WebCore.gyp/scripts/action_derivedsourcesallinone.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58352 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCore.gyp/scripts/action_derivedsourcesallinone.py
+++ third_party/WebKit/WebCore/WebCore.gyp/scripts/action_derivedsourcesallinone.py
@@ -50,7 +50,7 @@ import subprocess
 import sys
 
 # A regexp for finding Conditional attributes in interface definitions.
-conditionalPattern = re.compile('interface[\s]*\[[^\]]*Conditional=([\_0-9a-zA-Z&]*)')
+conditionalPattern = re.compile('interface[\s]*\[[^\]]*Conditional=([\_0-9a-zA-Z&|]*)')
 
 copyrightTemplate = """/*
  * THIS FILE WAS AUTOMATICALLY GENERATED, DO NOT EDIT.
@@ -83,11 +83,13 @@ copyrightTemplate = """/*
 """
 
 
-# Wraps conditional with ENABLE() and && if more than one conditional is specified.
+# Wraps conditional with ENABLE() and replace '&','|' with '&&','||' if more than one conditional is specified.
 def formatConditional(conditional):
     def wrapWithEnable(s):
+        if re.match('[|&]$', s):
+            return s * 2
         return 'ENABLE(' + s + ')'
-    return ' && '.join(map(wrapWithEnable, conditional))
+    return ' '.join(map(wrapWithEnable, conditional))
 
 
 # Find the conditional interface attribute.
@@ -102,7 +104,7 @@ def extractConditional(idlFilePath):
     match = conditionalPattern.search(idlContents)
     if match:
         conditional = match.group(1)
-        conditional = conditional.split('&')
+        conditional = re.split('([|&])', conditional)
 
     return conditional
 
