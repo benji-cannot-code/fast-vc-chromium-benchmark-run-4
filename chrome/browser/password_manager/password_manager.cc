benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit b6b111777d08386944380de20aa43b51c786a558

Fix password mgr heuristics for sites that keep the login form around after signin The test for whether the login form reappeared on the next page load now ignores invisible forms. BUG=28911 TEST=none (manual testing with reduced web pages)
Review URL: http://codereview.chromium.org/1656005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45841 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/password_manager/password_manager.cc
@@ -117,7 +117,7 @@ void PasswordManager::DidStopLoading() {
   }
 }
 
-void PasswordManager::PasswordFormsSeen(
+void PasswordManager::PasswordFormsFound(
     const std::vector<PasswordForm>& forms) {
   if (!delegate_->GetProfileForPasswordManager())
     return;
@@ -129,22 +129,28 @@ void PasswordManager::PasswordFormsSeen(
 
   std::vector<PasswordForm>::const_iterator iter;
   for (iter = forms.begin(); iter != forms.end(); iter++) {
-    if (provisional_save_manager_.get() &&
-        provisional_save_manager_->DoesManage(*iter)) {
-      // The form trying to be saved has immediately re-appeared. Assume
-      // login failure and abort this save.  Fallback to pending login state
-      // since the user may try again.
-      pending_login_managers_.push_back(provisional_save_manager_.release());
+    bool ssl_valid = iter->origin.SchemeIsSecure() && !had_ssl_error;
+    PasswordFormManager* manager =
+        new PasswordFormManager(delegate_->GetProfileForPasswordManager(),
+                                this, *iter, ssl_valid);
+    pending_login_managers_.push_back(manager);
+    manager->FetchMatchingLoginsFromWebDatabase();
+  }
+}
+
+void PasswordManager::PasswordFormsVisible(
+    const std::vector<PasswordForm>& visible_forms) {
+  if (!provisional_save_manager_.get())
+    return;
+  std::vector<PasswordForm>::const_iterator iter;
+  for (iter = visible_forms.begin(); iter != visible_forms.end(); iter++) {
+    if (provisional_save_manager_->DoesManage(*iter)) {
+      // The form trying to be saved has immediately re-appeared. Assume login
+      // failure and abort this save, by clearing provisional_save_manager_.
       // Don't delete the login managers since the user may try again
       // and we want to be able to save in that case.
+      provisional_save_manager_.release();
       break;
-    } else {
-      bool ssl_valid = iter->origin.SchemeIsSecure() && !had_ssl_error;
-      PasswordFormManager* manager =
-          new PasswordFormManager(delegate_->GetProfileForPasswordManager(),
-                                  this, *iter, ssl_valid);
-      pending_login_managers_.push_back(manager);
-      manager->FetchMatchingLoginsFromWebDatabase();
     }
   }
 }

chrome/browser/chromeos/login/account_creation_view.cc
@@ -79,8 +79,8 @@ class AccountCreationTabContents : public TabContents,
       }
       if (!user_name.empty()) {
         // We don't have password here because all password fields were
-        // stripped. Overriding TabContents::PasswordFormsSeen also has no sense
-        // becuase password value is always empty for account create page.
+        // stripped. Overriding TabContents::PasswordFormsFound also makes no
+        // sense because password value is always empty for account create page.
         delegate_->OnUserCreated(user_name + "@" + domain, "");
       }
     }

chrome/browser/login_prompt.cc
@@ -374,7 +374,7 @@ class LoginDialogTask : public Task {
         parent_contents->GetPasswordManager();
     std::vector<PasswordForm> v;
     MakeInputForPasswordManager(&v);
-    password_manager->PasswordFormsSeen(v);
+    password_manager->PasswordFormsFound(v);
     handler_->SetPasswordManager(password_manager);
 
     std::wstring explanation = auth_info_->realm.empty() ?
@@ -389,7 +389,7 @@ class LoginDialogTask : public Task {
 
  private:
   // Helper to create a PasswordForm and stuff it into a vector as input
-  // for PasswordManager::PasswordFormsSeen, the hook into PasswordManager.
+  // for PasswordManager::PasswordFormsFound, the hook into PasswordManager.
   void MakeInputForPasswordManager(
       std::vector<PasswordForm>* password_manager_input) {
     PasswordForm dialog_form;

chrome/browser/password_manager/password_manager.h
@@ -72,7 +72,11 @@ class PasswordManager : public LoginModel {
   void DidStopLoading();
 
   // Notifies the password manager that password forms were parsed on the page.
-  void PasswordFormsSeen(const std::vector<webkit_glue::PasswordForm>& forms);
+  void PasswordFormsFound(const std::vector<webkit_glue::PasswordForm>& forms);
+
+  // Notifies the password manager which password forms are initially visible.
+  void PasswordFormsVisible(
+       const std::vector<webkit_glue::PasswordForm>& visible_forms);
 
   // When a form is submitted, we prepare to save the password but wait
   // until we decide the user has successfully logged in. This is step 1

chrome/browser/password_manager/password_manager_unittest.cc
@@ -123,7 +123,8 @@ TEST_F(PasswordManagerTest, FormSubmitEmptyStore) {
   std::vector<PasswordForm> observed;
   PasswordForm form(MakeSimpleForm());
   observed.push_back(form);
-  manager()->PasswordFormsSeen(observed);  // The initial load.
+  manager()->PasswordFormsFound(observed);  // The initial load.
+  manager()->PasswordFormsVisible(observed);  // The initial layout.
 
   // And the form submit contract is to call ProvisionallySavePassword.
   manager()->ProvisionallySavePassword(form);
@@ -135,7 +136,7 @@ TEST_F(PasswordManagerTest, FormSubmitEmptyStore) {
   // Now the password manager waits for the navigation to complete.
   manager()->DidStopLoading();
 
-  EXPECT_FALSE(NULL == form_to_save.get());
+  ASSERT_FALSE(NULL == form_to_save.get());
   EXPECT_CALL(*store_, AddLogin(FormMatches(form)));
 
   // Simulate saving the form, as if the info bar was accepted.
@@ -157,7 +158,8 @@ TEST_F(PasswordManagerTest, FormSubmitNoGoodMatch) {
   std::vector<PasswordForm> observed;
   PasswordForm form(MakeSimpleForm());
   observed.push_back(form);
-  manager()->PasswordFormsSeen(observed);  // The initial load.
+  manager()->PasswordFormsFound(observed);  // The initial load.
+  manager()->PasswordFormsVisible(observed);  // The initial layout.
   manager()->ProvisionallySavePassword(form);
 
   // We still expect an add, since we didn't have a good match.
@@ -180,7 +182,8 @@ TEST_F(PasswordManagerTest, FormSeenThenLeftPage) {
   std::vector<PasswordForm> observed;
   PasswordForm form(MakeSimpleForm());
   observed.push_back(form);
-  manager()->PasswordFormsSeen(observed);  // The initial load.
+  manager()->PasswordFormsFound(observed);  // The initial load.
+  manager()->PasswordFormsVisible(observed);  // The initial layout.
 
   manager()->DidNavigate();
 
@@ -192,16 +195,68 @@ TEST_F(PasswordManagerTest, FormSubmitFailedLogin) {
   std::vector<PasswordForm*> result;  // Empty password store.
   EXPECT_CALL(delegate_, FillPasswordForm(_)).Times(Exactly(0));
   EXPECT_CALL(*store_, GetLogins(_,_))
-    .WillOnce(DoAll(WithArg<1>(InvokeConsumer(0, result)), Return(0)));
+    .WillRepeatedly(DoAll(WithArg<1>(InvokeConsumer(0, result)), Return(0)));
   std::vector<PasswordForm> observed;
   PasswordForm form(MakeSimpleForm());
   observed.push_back(form);
-  manager()->PasswordFormsSeen(observed);  // The initial load.
+  manager()->PasswordFormsFound(observed);  // The initial load.
+  manager()->PasswordFormsVisible(observed);  // The initial layout.
 
   manager()->ProvisionallySavePassword(form);
 
-  manager()->PasswordFormsSeen(observed);  // Simulated re-appearance.
+  // The form reappears, and is visible in the layout:
+  manager()->PasswordFormsFound(observed);
+  manager()->PasswordFormsVisible(observed);
 
   // No expected calls to the PasswordStore...
+  manager()->DidStopLoading();
+}
+
+TEST_F(PasswordManagerTest, FormSubmitInvisibleLogin) {
+  // Tests fix of issue 28911: if the login form reappears on the subsequent
+  // page, but is invisible, it shouldn't count as a failed login.
+  std::vector<PasswordForm*> result;  // Empty password store.
+  EXPECT_CALL(delegate_, FillPasswordForm(_)).Times(Exactly(0));
+  EXPECT_CALL(*store_, GetLogins(_,_))
+      .WillRepeatedly(DoAll(WithArg<1>(InvokeConsumer(0, result)), Return(0)));
+  std::vector<PasswordForm> observed;
+  PasswordForm form(MakeSimpleForm());
+  observed.push_back(form);
+  manager()->PasswordFormsFound(observed);  // The initial load.
+  manager()->PasswordFormsVisible(observed);  // The initial layout.
+
+  manager()->ProvisionallySavePassword(form);
+
+  // The form reappears, but is not visible in the layout:
+  manager()->PasswordFormsFound(observed);
+  // No call to PasswordFormsVisible.
+
+  // Expect info bar to appear:
+  scoped_ptr<PasswordFormManager> form_to_save;
+  EXPECT_CALL(delegate_, AddSavePasswordInfoBar(_))
+      .WillOnce(WithArg<0>(SaveToScopedPtr(&form_to_save)));
+
+  manager()->DidStopLoading();
+
+  ASSERT_FALSE(NULL == form_to_save.get());
+  EXPECT_CALL(*store_, AddLogin(FormMatches(form)));
+  // Simulate saving the form.
+  form_to_save->Save();
+}
+
+TEST_F(PasswordManagerTest, InitiallyInvisibleForm) {
+  // Make sure an invisible login form still gets autofilled.
+  std::vector<PasswordForm*> result;
+  PasswordForm* existing = new PasswordForm(MakeSimpleForm());
+  result.push_back(existing);
+  EXPECT_CALL(delegate_, FillPasswordForm(_));
+  EXPECT_CALL(*store_, GetLogins(_,_))
+      .WillRepeatedly(DoAll(WithArg<1>(InvokeConsumer(0, result)), Return(0)));
+  std::vector<PasswordForm> observed;
+  PasswordForm form(MakeSimpleForm());
+  observed.push_back(form);
+  manager()->PasswordFormsFound(observed);  // The initial load.
+  // PasswordFormsVisible is not called.
+
   manager()->DidStopLoading();
 }

chrome/browser/renderer_host/render_view_host.cc
@@ -764,7 +764,9 @@ void RenderViewHost::OnMessageReceived(const IPC::Message& msg) {
     IPC_MESSAGE_HANDLER_DELAY_REPLY(ViewHostMsg_ShowModalHTMLDialog,
                                     OnMsgShowModalHTMLDialog)
     IPC_MESSAGE_HANDLER(ViewHostMsg_FormsSeen, OnMsgFormsSeen)
-    IPC_MESSAGE_HANDLER(ViewHostMsg_PasswordFormsSeen, OnMsgPasswordFormsSeen)
+    IPC_MESSAGE_HANDLER(ViewHostMsg_PasswordFormsFound, OnMsgPasswordFormsFound)
+    IPC_MESSAGE_HANDLER(ViewHostMsg_PasswordFormsVisible,
+                        OnMsgPasswordFormsVisible)
     IPC_MESSAGE_HANDLER(ViewHostMsg_FormSubmitted, OnMsgFormSubmitted)
     IPC_MESSAGE_HANDLER(ViewHostMsg_StartDragging, OnMsgStartDragging)
     IPC_MESSAGE_HANDLER(ViewHostMsg_UpdateDragCursor, OnUpdateDragCursor)
@@ -1358,9 +1360,14 @@ void RenderViewHost::OnMsgFormsSeen(const std::vector<FormData>& forms) {
     autofill_delegate->FormsSeen(forms);
 }
 
-void RenderViewHost::OnMsgPasswordFormsSeen(
+void RenderViewHost::OnMsgPasswordFormsFound(
     const std::vector<PasswordForm>& forms) {
-  delegate_->PasswordFormsSeen(forms);
+  delegate_->PasswordFormsFound(forms);
+}
+
+void RenderViewHost::OnMsgPasswordFormsVisible(
+    const std::vector<PasswordForm>& visible_forms) {
+  delegate_->PasswordFormsVisible(visible_forms);
 }
 
 void RenderViewHost::OnMsgFormSubmitted(const FormData& form) {

chrome/browser/renderer_host/render_view_host.h
@@ -551,8 +551,10 @@ class RenderViewHost : public RenderWidgetHost {
                                 IPC::Message* reply_msg);
   void OnMsgFormsSeen(
       const std::vector<webkit_glue::FormData>& forms);
-  void OnMsgPasswordFormsSeen(
+  void OnMsgPasswordFormsFound(
       const std::vector<webkit_glue::PasswordForm>& forms);
+  void OnMsgPasswordFormsVisible(
+      const std::vector<webkit_glue::PasswordForm>& visible_forms);
   void OnMsgFormSubmitted(const webkit_glue::FormData& forms);
   void OnMsgStartDragging(const WebDropData& drop_data,
                           WebKit::WebDragOperationsMask operations_allowed,

chrome/browser/renderer_host/render_view_host_delegate.h
@@ -577,8 +577,12 @@ class RenderViewHostDelegate {
                                    IPC::Message* reply_msg) {}
 
   // Password forms have been detected in the page.
-  virtual void PasswordFormsSeen(
+  virtual void PasswordFormsFound(
       const std::vector<webkit_glue::PasswordForm>& forms) {}
+
+  // On initial layout, password forms are known to be visible on the page.
+  virtual void PasswordFormsVisible(
+      const std::vector<webkit_glue::PasswordForm>& visible_forms) {}
 
   // Notification that the page has an OpenSearch description document.
   virtual void PageHasOSDD(RenderViewHost* render_view_host,

commit 2c45635e09ffe3936917fe10a65f885382b8b815

Fix memory leak introduced in r45841. BUG=none TEST=PasswordManagerTest::FormSubmitFailedLogin + valgrind
Review URL: http://codereview.chromium.org/1795008

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45884 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/password_manager/password_manager.cc
@@ -149,7 +149,7 @@ void PasswordManager::PasswordFormsVisible(
       // failure and abort this save, by clearing provisional_save_manager_.
       // Don't delete the login managers since the user may try again
       // and we want to be able to save in that case.
-      provisional_save_manager_.release();
+      ClearProvisionalSave();
       break;
     }
   }