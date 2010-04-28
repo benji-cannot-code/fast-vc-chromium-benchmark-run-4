92c20a402ff59ecfe39546abdf562693cde13c31
stoyan@chromium.org <stoyan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-28 21:00:54 +0000

    Nicely steal moniker and bind context from the urlmon url request. BUG=42104 Review URL: http://codereview.chromium.org/1811001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45860 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/urlmon_url_request_private.h
+++ chrome_frame/urlmon_url_request_private.h
@@ -37,7 +37,10 @@ class UrlmonUrlRequest
                       HWND notification_window, IStream* cache);
 
   // Used from "DownloadRequestInHost".
-  void StealMoniker(IMoniker** moniker, IBindCtx** bctx);
+  // Callback will be invoked either right away (if operation is finished) or
+  // from inside ::OnStopBinding() when it is safe to reuse the bind_context.
+  typedef Callback2<IMoniker*, IBindCtx*>::Type TerminateBindCallback;
+  void TerminateBind(TerminateBindCallback* callback);
 
   // Parent Window for UrlMon error dialogs
   void set_parent_window(HWND parent_window) {
@@ -109,6 +112,10 @@ class UrlmonUrlRequest
 
   bool pending() const {
     return pending_;
+  }
+
+  bool terminate_requested() const {
+    return terminate_bind_callback_.get() != NULL;
   }
 
   std::string response_headers() {
@@ -272,6 +279,7 @@ class UrlmonUrlRequest
   // Set to true if the ChromeFrame instance is running in privileged mode.
   bool privileged_mode_;
   bool pending_;
+  scoped_ptr<TerminateBindCallback> terminate_bind_callback_;
   std::string response_headers_;
   DISALLOW_COPY_AND_ASSIGN(UrlmonUrlRequest);
 };
