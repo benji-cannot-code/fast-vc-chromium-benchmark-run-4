32f79222e4708ba9516fe5fc0086033c0399ff8f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-28 12:58:19 +0000

    2010-04-28  Luiz Agostini  <luiz.agostini@openbossa.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] QWebPage viewMode property
            https://bugs.webkit.org/show_bug.cgi?id=38119
    
            Replacing method qt_wrt_setViewMode by wrt_viewMode property.
    
            * Api/qwebpage.cpp:
            (QWebPagePrivate::dynamicPropertyChangeEvent):
            (QWebPage::event):
            * Api/qwebpage_p.h:
            * tests/qwebpage/tst_qwebpage.cpp:
            (tst_QWebPage::wrt_viewModes):
    2010-04-28  Luiz Agostini  <luiz.agostini@openbossa.org>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] QWebPage viewMode property
            https://bugs.webkit.org/show_bug.cgi?id=38119
    
            Replacing method qt_wrt_setViewMode by wrt_viewMode property.
    
            * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
            (LayoutTestController::setViewModeMediaFeature):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58405 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -122,15 +122,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 
-void QWEBKIT_EXPORT qt_wrt_setViewMode(QWebPage* page, const QString& mode)
-{
-    QWebPagePrivate::priv(page)->viewMode = mode;
-    WebCore::Frame* frame = QWebFramePrivate::core(page->mainFrame());
-    WebCore::FrameView* view = frame->view();
-    frame->document()->updateStyleSelector();
-    view->forceLayout();
-}
-
 bool QWebPagePrivate::drtRun = false;
 
 // Lookup table mapping QWebPage::WebActions to the associated Editor commands
@@ -1215,6 +1206,20 @@ void QWebPagePrivate::inputMethodEvent(QInputMethodEvent *ev)
     }
 
     ev->accept();
+}
+
+void QWebPagePrivate::dynamicPropertyChangeEvent(QDynamicPropertyChangeEvent* event)
+{
+    if (event->propertyName() == "wrt_viewMode") {
+        QString mode = q->property("wrt_viewMode").toString();
+        if (mode != viewMode) {
+            viewMode = mode;
+            WebCore::Frame* frame = QWebFramePrivate::core(q->mainFrame());
+            WebCore::FrameView* view = frame->view();
+            frame->document()->updateStyleSelector();
+            view->forceLayout();
+        }
+    }
 }
 
 void QWebPagePrivate::shortcutOverrideEvent(QKeyEvent* event)
@@ -2565,6 +2570,9 @@ bool QWebPage::event(QEvent *ev)
         d->touchEvent(static_cast<QTouchEvent*>(ev));
         break;
 #endif
+    case QEvent::DynamicPropertyChange:
+        d->dynamicPropertyChangeEvent(static_cast<QDynamicPropertyChangeEvent*>(ev));
+        break;
     default:
         return QObject::event(ev);
     }
