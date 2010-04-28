1ab6e3382374b66cab54a61196a8720fea3dd75e
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-28 02:12:07 +0000

    Windows/Views: delete native bookmark manager code.
    Context menu code still needs cleanup/refactoring.
    String resources still need to be deleted.
    
    BUG=38908
    TEST=everything still works
    
    Review URL: http://codereview.chromium.org/1730015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45776 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/frame/browser_view.h
+++ chrome/browser/views/frame/browser_view.h
@@ -298,7 +298,6 @@ class BrowserView : public BrowserBubbleHost,
   virtual void ToggleExtensionShelf();
   virtual void ShowAboutChromeDialog();
   virtual void ShowTaskManager();
-  virtual void ShowBookmarkManager();
   virtual void ShowBookmarkBubble(const GURL& url, bool already_bookmarked);
   virtual void SetDownloadShelfVisible(bool visible);
   virtual bool IsDownloadShelfVisible() const;
