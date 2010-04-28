b6b111777d08386944380de20aa43b51c786a558
snej@chromium.org <snej@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-28 19:20:41 +0000

    Fix password mgr heuristics for sites that keep the login form around after signin The test for whether the login form reappeared on the next page load now ignores invisible forms. BUG=28911 TEST=none (manual testing with reduced web pages)
    Review URL: http://codereview.chromium.org/1656005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45841 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -2686,7 +2686,7 @@ void RenderView::didFinishDocumentLoad(WebFrame* frame) {
   // TODO(jhawkins): Make these use the FormManager.
   form_manager_.ExtractForms(frame);
   SendForms(frame);
-  SendPasswordForms(frame);
+  SendPasswordForms(frame, false);
 
   // Check whether we have new encoding name.
   UpdateEncoding(frame, frame->view()->pageEncoding().utf8());
@@ -2730,6 +2730,9 @@ void RenderView::didFinishLoad(WebFrame* frame) {
   DCHECK(navigation_state);
   navigation_state->set_finish_load_time(Time::Now());
   navigation_state->user_script_idle_scheduler()->DidFinishLoad();
+
+  // Let the password manager know which password forms are actually visible.
+  SendPasswordForms(frame, true);
 }
 
 void RenderView::didNavigateWithinPage(
@@ -4719,7 +4722,7 @@ void RenderView::didChangeAccessibilityObjectState(
 #endif
 }
 
-void RenderView::SendPasswordForms(WebFrame* frame) {
+void RenderView::SendPasswordForms(WebFrame* frame, bool only_visible) {
   WebVector<WebFormElement> forms;
   frame->forms(forms);
 
@@ -4728,16 +4731,22 @@ void RenderView::SendPasswordForms(WebFrame* frame) {
     const WebFormElement& form = forms[i];
 
     // Respect autocomplete=off.
-    if (form.autoComplete()) {
-      scoped_ptr<PasswordForm> password_form(
-          PasswordFormDomManager::CreatePasswordForm(form));
-      if (password_form.get())
-        password_forms.push_back(*password_form);
-    }
+    if (!form.autoComplete())
+      continue;
+    if (only_visible && !form.hasNonEmptyBoundingBox())
+      continue;
+    scoped_ptr<PasswordForm> password_form(
+        PasswordFormDomManager::CreatePasswordForm(form));
+    if (password_form.get())
+      password_forms.push_back(*password_form);
   }
 
-  if (!password_forms.empty())
-    Send(new ViewHostMsg_PasswordFormsSeen(routing_id_, password_forms));
+  if (password_forms.empty())
+    return;
+  if (only_visible)
+    Send(new ViewHostMsg_PasswordFormsVisible(routing_id_, password_forms));
+  else
+    Send(new ViewHostMsg_PasswordFormsFound(routing_id_, password_forms));
 }
 
 void RenderView::Print(WebFrame* frame, bool script_initiated) {
