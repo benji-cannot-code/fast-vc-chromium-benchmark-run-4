499541b273542441d57306117a0d5cf2538dae58
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-28 17:21:38 +0000

    Added shader type to shader info. Review URL: http://codereview.chromium.org/1696012
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45823 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/shader_manager.cc
+++ gpu/command_buffer/service/shader_manager.cc
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 namespace gles2 {
 
-void ShaderManager::CreateShaderInfo(GLuint client_id, GLuint service_id) {
+void ShaderManager::CreateShaderInfo(GLuint client_id,
+                                     GLuint service_id,
+                                     GLenum shader_type) {
   std::pair<ShaderInfoMap::iterator, bool> result =
       shader_infos_.insert(std::make_pair(
-          client_id, ShaderInfo::Ref(new ShaderInfo(service_id))));
+          client_id, ShaderInfo::Ref(new ShaderInfo(service_id, shader_type))));
   DCHECK(result.second);
 }
 
