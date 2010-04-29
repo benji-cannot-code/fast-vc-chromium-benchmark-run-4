9e209df6f737af5ccbffd43b915bddaf649d75c9
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-29 03:20:27 +0000

    Relands your patch to have an option for enabling vertical tabs. I've added the following additions: . made the layout work for opaque_browser_frame. . Only show the menu if the user supplied --enable-vertical-tabs.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1708014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45906 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/side_tab_strip.cc
+++ chrome/browser/views/tabs/side_tab_strip.cc
@@ -5,13 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/views/tabs/side_tab_strip.h"
 
-#include "base/command_line.h"
-#include "chrome/browser/pref_service.h"
-#include "chrome/browser/profile.h"
 #include "chrome/browser/view_ids.h"
-#include "chrome/common/chrome_switches.h"
-#include "chrome/common/pref_names.h"
-#include "gfx/canvas.h"
 
 namespace {
 const int kVerticalTabSpacing = 2;
@@ -31,18 +25,6 @@ SideTabStrip::~SideTabStrip() {
 
 void SideTabStrip::SetModel(SideTabStripModel* model) {
   model_.reset(model);
-}
-
-// static
-bool SideTabStrip::Available() {
-  return CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableVerticalTabs);
-}
-
-// static
-bool SideTabStrip::Visible(Profile* profile) {
-  return Available() &&
-      profile->GetPrefs()->GetBoolean(prefs::kUseVerticalTabs);
 }
 
 void SideTabStrip::AddTabAt(int index) {
@@ -87,6 +69,10 @@ void SideTabStrip::SelectTab(SideTab* tab) {
 
 void SideTabStrip::CloseTab(SideTab* tab) {
   model_->CloseTab(GetIndexOfSideTab(tab));
+}
+
+void SideTabStrip::ShowContextMenu(SideTab* tab, const gfx::Point& p) {
+  model_->ShowContextMenu(GetIndexOfSideTab(tab), p);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
