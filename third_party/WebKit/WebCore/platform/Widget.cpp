f7bd186cc336a149cce218ac8ead7bb08ce780fa
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-29 08:46:29 +0000

    2010-04-29  Gustavo Sverzut Barbieri  <barbieri@profusion.mobi>
            Reviewed by Eric Seidel.
    
            Add EFL-specific code to Widget.h and move the empty
            frameRectsChanged() definition to Widget.cpp, since the EFL port
            needs to override that.
            http://webkit.org/b/36317
    
            No new tests required.
    
            * WebCore.base.exp:
            * platform/Widget.cpp:
            (WebCore::Widget::frameRectsChanged):
            * platform/Widget.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58487 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/Widget.cpp
+++ third_party/WebKit/WebCore/platform/Widget.cpp
@@ -175,4 +175,10 @@ IntPoint Widget::convertFromContainingView(const IntPoint& parentPoint) const
     return parentPoint;
 }
 
+#if !PLATFORM(EFL)
+void Widget::frameRectsChanged()
+{
+}
+#endif
+
 } // namespace WebCore
