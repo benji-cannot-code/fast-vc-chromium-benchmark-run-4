e28b27568483c73f80e9624663fa7a8febd230f5
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-29 07:15:15 +0000

    2010-04-29  Noam Rosenthal  <noam.rosenthal@nokia.com>
            Reviewed by Simon Hausmann.
    
            [Qt] GraphicsLayer: flicker when starting an animation before the previous one ended.
            https://bugs.webkit.org/show_bug.cgi?id=38076
    
            This was due to the cude in the removeAnimations functions, which called deleteLater() without stopping the
            animation synchronously. The delay between the call to that function and the actual call to the animation's destructor
            is when the flicker occured. We fix this by calling stop() synchronously, and making sure that the value is reverted
            upon stop (updateState) and not upon the object's destruction.
    
            http://staff.washington.edu/fmf/2009/03/25/iphone-3d-css-transformations/ now doesn't flicker when
            the animation is toggled frequently.
    
            * platform/graphics/qt/GraphicsLayerQt.cpp:
            (WebCore::TransformAnimationQt::~TransformAnimationQt):
            (WebCore::TransformAnimationQt::applyFrame):
            (WebCore::TransformAnimationQt::updateState):
            (WebCore::OpacityAnimationQt::~OpacityAnimationQt):
            (WebCore::OpacityAnimationQt::updateState):
            (WebCore::GraphicsLayerQt::removeAnimationsForProperty):
            (WebCore::GraphicsLayerQt::removeAnimationsForKeyframes):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58480 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsLayerQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsLayerQt.cpp
@@ -1169,8 +1169,6 @@ public:
     {
         if (m_fillsForwards)
             setCurrentTime(1);
-        else if (m_layer && m_layer.data()->m_layer)
-            m_layer.data()->setBaseTransform(m_layer.data()->m_layer->transform());
     }
 
     // the idea is that we let WebCore manage the transform-operations
@@ -1208,6 +1206,7 @@ public:
                 transformMatrix.blend(m_sourceMatrix, progress);
             }
         }
+
         m_layer.data()->setBaseTransform(transformMatrix);
         if (m_fillsForwards)
             m_layer.data()->m_layer->setTransform(m_layer.data()->m_baseTransform);
@@ -1226,7 +1225,10 @@ public:
             m_sourceMatrix = m_layer.data()->m_layer->transform();
             m_layer.data()->m_transformAnimationRunning = true;
         } else if (newState == QAbstractAnimation::Stopped) {
+            // We update the transform back to the default. This already takes fill-modes into account.
             m_layer.data()->m_transformAnimationRunning = false;
+            if (m_layer && m_layer.data()->m_layer)
+                m_layer.data()->setBaseTransform(m_layer.data()->m_layer->transform());
         }
     }
 
@@ -1244,8 +1246,6 @@ public:
     {
         if (m_fillsForwards)
             setCurrentTime(1);
-        else if (m_layer && m_layer.data()->m_layer)
-            m_layer.data()->setOpacity(m_layer.data()->m_layer->opacity());
     }
     virtual void applyFrame(const qreal& fromValue, const qreal& toValue, qreal progress)
     {
@@ -1267,6 +1267,12 @@ public:
 
         if (m_layer)
             m_layer.data()->m_opacityAnimationRunning = (newState == QAbstractAnimation::Running);
+
+        // If stopped, we update the opacity back to the default. This already takes fill-modes into account.
+        if (newState == Stopped)
+            if (m_layer && m_layer.data()->m_layer)
+                m_layer.data()->setOpacity(m_layer.data()->m_layer->opacity());
+
     }
 };
 
@@ -1332,6 +1338,8 @@ void GraphicsLayerQt::removeAnimationsForProperty(AnimatedPropertyID id)
         if (*it) {
             AnimationQtBase* anim = static_cast<AnimationQtBase*>(it->data());
             if (anim && anim->m_webkitPropertyID == id) {
+                // We need to stop the animation right away, or it might flicker before it's deleted.
+                anim->stop();
                 anim->deleteLater();
                 it = m_impl->m_animations.erase(it);
                 --it;
@@ -1346,7 +1354,9 @@ void GraphicsLayerQt::removeAnimationsForKeyframes(const String& name)
         if (*it) {
             AnimationQtBase* anim = static_cast<AnimationQtBase*>((*it).data());
             if (anim && anim->m_keyframesName == QString(name)) {
-                (*it).data()->deleteLater();
+                // We need to stop the animation right away, or it might flicker before it's deleted.
+                anim->stop();
+                anim->deleteLater();
                 it = m_impl->m_animations.erase(it);
                 --it;
             }
