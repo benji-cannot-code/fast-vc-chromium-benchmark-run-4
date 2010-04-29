1735b35077e4de341a189395d9a009f29b84274c
jcivelli@google.com <jcivelli@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-29 16:07:48 +0000

    The app launcher now uses the location bar (instead of the autocomplete edit), so it has "tab to search" and the icons on the left side, Also split location_bar_view.cc so that every inner-class gets its own .h and .cc file.
    BUG=None
    TEST=Make sure the location bar works as expected (tab to search, bookmark star,
         page actions...).
         Open the app launcher, make sure "tab to search" works as expected.
    
    Review URL: http://codereview.chromium.org/1746009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45936 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -103,7 +103,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/user_data_manager.h"
 #include "chrome/browser/view_ids.h"
 #include "chrome/browser/views/app_launcher.h"
-#include "chrome/browser/views/location_bar_view.h"
+#include "chrome/browser/views/location_bar/location_bar_view.h"
 #endif  // OS_WIN
 
 #if defined(OS_MACOSX)
--- chrome/browser/browser_focus_uitest.cc
+++ chrome/browser/browser_focus_uitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(TOOLKIT_VIEWS)
 #include "chrome/browser/views/frame/browser_view.h"
-#include "chrome/browser/views/location_bar_view.h"
+#include "chrome/browser/views/location_bar/location_bar_view.h"
 #include "chrome/browser/views/tab_contents/tab_contents_container.h"
 #endif
 
