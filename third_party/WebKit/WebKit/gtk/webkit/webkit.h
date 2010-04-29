abe7aa7e29ebd1a04b00f60877512692610a0e3d
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-04-29 13:48:53 +0000

    2010-04-29  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Gustavo Noronha.
    
            [GTK] GObject DOM bindings
            https://bugs.webkit.org/show_bug.cgi?id=33590
    
            Include <webkit/webkitdom.h> in the main webkit.h header.
    
            * webkit/webkit.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkit.h
+++ third_party/WebKit/WebKit/gtk/webkit/webkit.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <webkit/webkitversion.h>
 #include <webkit/webkitdefines.h>
+#include <webkit/webkitdom.h>
 #include <webkit/webkitdownload.h>
 #include <webkit/webkitgeolocationpolicydecision.h>
 #include <webkit/webkithittestresult.h>
