44fb613ec5be71b6078e8922e66f39e134e4605e
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-30 17:31:42 +0000

    [Linux] Mark ExtensionApiTest.Storage as flaky. BUG=42943 TEST=none TBR=rafaelw
    Review URL: http://codereview.chromium.org/1810012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46083 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_storage_apitest.cc
+++ chrome/browser/extensions/extension_storage_apitest.cc
@@ -5,7 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Storage) {
+#if defined(OS_LINUX)
+// See http://crbug.com/42943.
+#define MAYBE_Storage FLAKY_Storage
+#else
+#define MAYBE_Storage Storage
+#endif
+
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_Storage) {
   ASSERT_TRUE(RunExtensionTest("storage")) << message_;
 }
 
