ee5151fd1d6eb62c2fa3ae244d4aa483d264f90d
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-30 17:33:18 +0000

    Update typed_url sync to support visits.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1775004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46085 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/typed_url_change_processor.h
+++ chrome/browser/sync/glue/typed_url_change_processor.h
@@ -25,6 +25,7 @@ namespace history {
 class HistoryBackend;
 struct URLsDeletedDetails;
 struct URLsModifiedDetails;
+struct URLVisitedDetails;
 class URLRow;
 };
 
@@ -61,16 +62,12 @@ class TypedUrlChangeProcessor : public ChangeProcessor,
   virtual void StopImpl();
 
  private:
-  bool WriteTypedUrl(sync_api::WriteNode* node,
-                     const string16& name,
-                     const string16& value,
-                     std::vector<base::Time>& timestamps);
-
   void StartObserving();
   void StopObserving();
 
   void HandleURLsModified(history::URLsModifiedDetails* details);
   void HandleURLsDeleted(history::URLsDeletedDetails* details);
+  void HandleURLsVisited(history::URLVisitedDetails* details);
 
   // The two models should be associated according to this ModelAssociator.
   TypedUrlModelAssociator* model_associator_;
