32354bf254b48b65fea496fb5bc376fc6a959257
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-30 17:32:55 +0000

    Disk cache: Re-enable some unit tests that were disabled or marked as flaky.
    BUG=21110,38562
    TEST=unittests
    
    Review URL: http://codereview.chromium.org/1800010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46084 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -1302,8 +1302,7 @@ void DiskCacheBackendTest::BackendDisable2() {
   EXPECT_EQ(0, cache_->GetEntryCount());
 }
 
-// http://code.google.com/p/chromium/issues/detail?id=38562
-TEST_F(DiskCacheBackendTest, DISABLED_DisableSuccess2) {
+TEST_F(DiskCacheBackendTest, DisableSuccess2) {
   ASSERT_TRUE(CopyTestCache(L"list_loop"));
   DisableFirstCleanup();
   SetDirectMode();
@@ -1320,8 +1319,7 @@ TEST_F(DiskCacheBackendTest, NewEvictionDisableSuccess2) {
   BackendDisable2();
 }
 
-// http://code.google.com/p/chromium/issues/detail?id=38562
-TEST_F(DiskCacheBackendTest, DISABLED_DisableFailure2) {
+TEST_F(DiskCacheBackendTest, DisableFailure2) {
   ASSERT_TRUE(CopyTestCache(L"list_loop"));
   DisableFirstCleanup();
   SetDirectMode();
@@ -1330,8 +1328,7 @@ TEST_F(DiskCacheBackendTest, DISABLED_DisableFailure2) {
   BackendDisable2();
 }
 
-// http://code.google.com/p/chromium/issues/detail?id=38562
-TEST_F(DiskCacheBackendTest, DISABLED_NewEvictionDisableFailure2) {
+TEST_F(DiskCacheBackendTest, NewEvictionDisableFailure2) {
   ASSERT_TRUE(CopyTestCache(L"list_loop"));
   DisableFirstCleanup();
   SetDirectMode();
@@ -1358,8 +1355,7 @@ void DiskCacheBackendTest::BackendDisable3() {
   EXPECT_EQ(1, cache_->GetEntryCount());
 }
 
-// http://code.google.com/p/chromium/issues/detail?id=38562
-TEST_F(DiskCacheBackendTest, DISABLED_DisableSuccess3) {
+TEST_F(DiskCacheBackendTest, DisableSuccess3) {
   ASSERT_TRUE(CopyTestCache(L"bad_rankings2"));
   DisableFirstCleanup();
   SetMaxSize(20 * 1024 * 1024);
@@ -1367,8 +1363,7 @@ TEST_F(DiskCacheBackendTest, DISABLED_DisableSuccess3) {
   BackendDisable3();
 }
 
-// http://code.google.com/p/chromium/issues/detail?id=38562
-TEST_F(DiskCacheBackendTest, DISABLED_NewEvictionDisableSuccess3) {
+TEST_F(DiskCacheBackendTest, NewEvictionDisableSuccess3) {
   ASSERT_TRUE(CopyTestCache(L"bad_rankings2"));
   DisableFirstCleanup();
   SetMaxSize(20 * 1024 * 1024);
@@ -1433,9 +1428,7 @@ TEST_F(DiskCacheBackendTest, DisableSuccess4) {
   BackendDisable4();
 }
 
-// Flaky, http://crbug.com/21110.
-// TODO(rvargas): Add more debugging code to help identify the root cause.
-TEST_F(DiskCacheBackendTest, FLAKY_NewEvictionDisableSuccess4) {
+TEST_F(DiskCacheBackendTest, NewEvictionDisableSuccess4) {
   ASSERT_TRUE(CopyTestCache(L"bad_rankings"));
   DisableFirstCleanup();
   SetDirectMode();
