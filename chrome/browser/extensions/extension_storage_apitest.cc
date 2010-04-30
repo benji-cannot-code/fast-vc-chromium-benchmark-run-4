35347cc1624fc6990be2d4914cff83a71dc530cf
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-30 23:25:57 +0000

    Attempt to fix AppApiTest.AppProcess and ExtensionApiTest.Storage flakiness. BUG=42497,42943 TEST=none
    Review URL: http://codereview.chromium.org/1705025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46151 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_storage_apitest.cc
+++ chrome/browser/extensions/extension_storage_apitest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_MACOSX)
 // See http://crbug.com/42943.
 #define MAYBE_Storage FLAKY_Storage
 #else
