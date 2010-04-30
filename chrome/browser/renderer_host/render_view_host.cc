b2b9ae9c8603da0f69ccb1c2e9562713c1684f31
twiz@chromium.org <twiz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-30 22:39:22 +0000

    Clean-up of the asynchronous behaviour of the experimental extension popup API.
     - It was previously possible to programmatically launch two popups from the same extension.  (Simply call popup.show twice in a row, or in a callback chain.)  
    
     I removed this incorrect funcationality by registering ExtensionPopupHost to listen for EXTENSION_HOST_CREATED notifications.  If a popup is shown, and a new ExtensionHost is constructed of type EXTENSION_POPUP, then the presently displayed popup is dismissed. 
    
     - The callback function for popup.show(...) was  previously called in response to EXTENSION_POPUP_VIEW_READY, as processed in  response to a ViewHostMsg_DocumentAvailableInMainFrame message.  This message wassent after PARSING of the conent of the popup view.  Because of this behaviour, the API was difficult to use because one could not meaningfully interact with the popup page 
    during the callback:  The callback would race with completion of the onload handler within the popup, so some sort of polling for onload-complete was required.
      I fixed the problem by adding new notifications and messages so that EXTENSION_POPUP_VIEW_READY is now sent only after all onload handlers have been invoked.Corresponding unit-tests have also been added.
    
    BUG=None
    TEST=ExtensionApiTest.Popup
    
    Review URL: http://codereview.chromium.org/1512007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46136 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_view_host.cc
+++ chrome/browser/renderer_host/render_view_host.cc
@@ -724,6 +724,8 @@ void RenderViewHost::OnMessageReceived(const IPC::Message& msg) {
     IPC_MESSAGE_HANDLER(ViewHostMsg_DidStopLoading, OnMsgDidStopLoading)
     IPC_MESSAGE_HANDLER(ViewHostMsg_DocumentAvailableInMainFrame,
                         OnMsgDocumentAvailableInMainFrame)
+    IPC_MESSAGE_HANDLER(ViewHostMsg_DocumentOnLoadCompletedInMainFrame,
+                        OnMsgDocumentOnLoadCompletedInMainFrame)
     IPC_MESSAGE_HANDLER(ViewHostMsg_DidLoadResourceFromMemoryCache,
                         OnMsgDidLoadResourceFromMemoryCache)
     IPC_MESSAGE_HANDLER(ViewHostMsg_DidDisplayInsecureContent,
@@ -1050,6 +1052,10 @@ void RenderViewHost::OnMsgDidStopLoading() {
 
 void RenderViewHost::OnMsgDocumentAvailableInMainFrame() {
   delegate_->DocumentAvailableInMainFrame(this);
+}
+
+void RenderViewHost::OnMsgDocumentOnLoadCompletedInMainFrame() {
+  delegate_->DocumentOnLoadCompletedInMainFrame(this);
 }
 
 void RenderViewHost::OnMsgDidLoadResourceFromMemoryCache(
