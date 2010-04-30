caf5dddad3226ce8e832f80a5fe0a9f61ea13eb2
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-30 01:49:57 +0000

    Rename a bunch of DOMUI shared resources.
    BUG=None
    TEST=manual
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46027 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/local_strings.js
+++ chrome/browser/resources/shared/js/local_strings.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(arv): Namespace
+
 /**
  * The local strings get injected into the page usig a variable named
  * {@code templateData}. This class provides a simpler interface to access those
