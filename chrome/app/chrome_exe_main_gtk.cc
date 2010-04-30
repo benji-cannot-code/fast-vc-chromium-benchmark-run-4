c394f46fe3d66cc63de95b2cd146bd3fae05f7bc
prasadt@chromium.org <prasadt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-30 18:55:53 +0000

    r46025 reverted r46023 which caused a build break on chromeos. This change reverts r46025 and fixes the build break which is just a one line change in chrome/browser/first_run_gtk.cc to move the definition of Upgrade::new_command_line_ to be inside a #if.  Details on the change and code review feedback for the original CL can be found at http://codereview.chromium.org/1633021.
    BUG=40975
    TEST=none
    
    Review URL: http://codereview.chromium.org/1691022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46103 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_exe_main_gtk.cc
+++ chrome/app/chrome_exe_main_gtk.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/process_util.h"
+#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+#include "chrome/browser/first_run.h"
+#endif
 
 // The entry point for all invocations of Chromium, browser and renderer. On
 // windows, this does nothing but load chrome.dll and invoke its entry point in
@@ -45,5 +48,13 @@ int main(int argc, const char** argv) {
   // keep it.
   // base::AtExitManager exit_manager;
 
-  return ChromeMain(argc, argv);
+  int return_code = ChromeMain(argc, argv);
+
+#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+  // Launch a new instance if we're shutting down because we detected an
+  // upgrade in the persistent mode.
+  Upgrade::RelaunchChromeBrowserWithNewCommandLineIfNeeded();
+#endif
+
+  return return_code;
 }
