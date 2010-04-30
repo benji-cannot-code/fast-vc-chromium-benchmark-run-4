c549282f4e86bb57ef03bfec4a258b252dcf5b4b
gdk@chromium.org <gdk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-30 14:23:04 +0000

    Create a command-line switch for enabling touch events.
    BUG=36415
    TEST=none
    
    Review URL: http://codereview.chromium.org/1575033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46068 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_thread.cc
+++ chrome/renderer/render_thread.cc
@@ -890,6 +890,9 @@ void RenderThread::EnsureWebKitInitialized() {
       command_line.HasSwitch(switches::kEnableExperimentalWebGL));
 
   WebRuntimeFeatures::enablePushState(true);
+
+  WebRuntimeFeatures::enableTouch(
+      command_line.HasSwitch(switches::kEnableTouch));
 }
 
 void RenderThread::IdleHandler() {
