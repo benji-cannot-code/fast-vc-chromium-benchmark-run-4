7cba84ec2cc0b92f06c130400be3ccc95ef89475
hamaji@chromium.org <hamaji@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-01 05:34:28 +0000

    2010-04-30  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Darin Adler.
    
            Add layoutTestController.setPrinting()
            https://bugs.webkit.org/show_bug.cgi?id=37203
    
            * platform/mac/Skipped:
            * platform/mac/printing/media-queries-print-expected.txt:
    2010-04-30  Shinichiro Hamaji  <hamaji@chromium.org>
    
            Reviewed by Darin Adler.
    
            Add layoutTestController.setPrinting()
            https://bugs.webkit.org/show_bug.cgi?id=37203
    
            Use the renderer's width insteead of screen's width as the width of
            a screen depends on machines.
    
            * rendering/RenderTreeAsText.cpp:
            (WebCore::externalRepresentation):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58629 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTreeAsText.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTreeAsText.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSMutableStyleDeclaration.h"
 #include "CharacterNames.h"
 #include "Document.h"
-#include "DOMWindow.h"
 #include "Frame.h"
 #include "FrameView.h"
 #include "HTMLElement.h"
@@ -46,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderTableCell.h"
 #include "RenderView.h"
 #include "RenderWidget.h"
-#include "Screen.h"
 #include "SelectionController.h"
 #include "TextStream.h"
 #include <wtf/UnusedParam.h>
@@ -622,8 +620,11 @@ static void writeSelection(TextStream& ts, const RenderObject* o)
 String externalRepresentation(Frame* frame, RenderAsTextBehavior behavior)
 {
     PrintContext printContext(frame);
-    if (behavior & RenderAsTextPrintingMode)
-        printContext.begin(frame->domWindow()->screen()->width());
+    if (behavior & RenderAsTextPrintingMode) {
+        if (!frame->contentRenderer())
+            return String();
+        printContext.begin(frame->contentRenderer()->width());
+    }
 
     frame->document()->updateLayout();
 
