780bd58cc8bbea26936d58eded62db762a42313b
sanjeevr@chromium.org <sanjeevr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-01 02:01:42 +0000

    Redo of http://codereview.chromium.org/1700003/show which was reverted. We check for a sync specific service URL (or an empty) one before acting on a notification from TalkMediator. BUG=None TEST=Test Bookmarks Sync Notifications.
    Review URL: http://codereview.chromium.org/1817006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46174 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/syncer_thread.cc
+++ chrome/browser/sync/engine/syncer_thread.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <queue>
 
 #include "base/dynamic_annotations.h"
+#include "chrome/browser/sync/sync_constants.h"
 #include "chrome/browser/sync/engine/auth_watcher.h"
 #include "chrome/browser/sync/engine/model_safe_worker.h"
 #include "chrome/browser/sync/engine/net/server_connection_manager.h"
@@ -634,11 +635,15 @@ void SyncerThread::HandleTalkMediatorEvent(const TalkMediatorEvent& event) {
       p2p_subscribed_ = false;
       break;
     case TalkMediatorEvent::NOTIFICATION_RECEIVED:
-      // TODO(sanjeevr): Check if the service url is a sync URL.
-      // An empty service URL is treated as a legacy sync notification.
-      LOG(INFO) << "P2P: Updates on server, pushing syncer";
-      if (NULL != vault_.syncer_) {
-        NudgeSyncImpl(0, kNotification);
+      // Check if the service url is a sync URL. An empty service URL
+      // is treated as a legacy sync notification
+      if (event.notification_data.service_url.empty() ||
+          (event.notification_data.service_url == kSyncLegacyServiceUrl) ||
+          (event.notification_data.service_url == kSyncServiceUrl)) {
+        LOG(INFO) << "P2P: Updates on server, pushing syncer";
+        if (NULL != vault_.syncer_) {
+          NudgeSyncImpl(0, kNotification);
+        }
       }
       break;
     default:
