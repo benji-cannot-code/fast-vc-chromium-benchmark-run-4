9a9e1ca65a7e14a396b143378e53901e2aef0520
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-01 01:01:06 +0000

    Fix the build.
    * Platform/CoreIPC/mac/ConnectionMac.cpp: Add missing #include.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Platform/CoreIPC/mac/ConnectionMac.cpp
+++ third_party/WebKit/WebKit2/Platform/CoreIPC/mac/ConnectionMac.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CoreIPCMessageKinds.h"
 #include "MachPort.h"
 #include "RunLoop.h"
+#include <mach/vm_map.h>
 
 using namespace std;
 
