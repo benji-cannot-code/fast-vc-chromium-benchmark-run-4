bfe78107e628cb03a1865890c2547d7b680db600
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-01 00:06:00 +0000

    The chrome frame install script instantiates the chrome frame activex via the new ActiveXObject call, which causes it to not receive the IOleObject_SetClientSite call.
    We now bootstrap our BHO in an explicit method call RegisterBHOIfNeeded
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=42790
    
    Bug=42790
    
    Review URL: http://codereview.chromium.org/1823001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46165 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/CFInstall.js
+++ chrome_frame/CFInstall.js
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       try {
         var obj = new ActiveXObject('ChromeTab.ChromeFrame');
         if (obj) {
+          obj.RegisterBHOIfNeeded();
           return true;
         }
       } catch(e) {
