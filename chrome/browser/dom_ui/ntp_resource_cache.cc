da1ec7f59f815658246fab2360872155d2902333
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-01 00:52:14 +0000

    When apps are enabled we hide the most visited and recent changes at first run.
    This is done by a new pref kNTPAppLauncherFirstRun to ensure we only hide them once.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/1763022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46170 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/ntp_resource_cache.cc
+++ chrome/browser/dom_ui/ntp_resource_cache.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_theme_provider.h"
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/dom_ui/chrome_url_data_manager.h"
+#include "chrome/browser/dom_ui/shown_sections_handler.h"
 #include "chrome/browser/google_util.h"
 #include "chrome/browser/pref_service.h"
 #include "chrome/browser/profile.h"
@@ -337,6 +338,7 @@ void NTPResourceCache::CreateNewTabHTML() {
   localized_strings.SetString(L"anim", anim);
 
   // Pass the shown_sections pref early so that we can prevent flicker.
+  ShownSectionsHandler::SetFirstAppLauncherRunPref(profile_->GetPrefs());
   const int shown_sections = profile_->GetPrefs()->GetInteger(
       prefs::kNTPShownSections);
   localized_strings.SetInteger(L"shown_sections", shown_sections);
