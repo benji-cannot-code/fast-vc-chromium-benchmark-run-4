4cc96a5c6b43fd75bdac7120a583ada4fa389f2e
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-01 00:49:01 +0000

    Caret may fail to blink if a focus handler brings up a modal dialog https://bugs.webkit.org/show_bug.cgi?id=38372
    Reviewed by Darin Adler.
    
    WebCore:
    
    * manual-tests/onfocus-alert-blinking-caret.html: Added.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::handleMousePressEvent):
    Moved the call to setCaretBlinkingSuspended() from here...
    (WebCore::EventHandler::handleMousePressEvent):
    ... to here. This makes us suspend caret blinking before dispatching the
    mouse down event. If dispatching the mouse down event allows the message
    loop to run, we want mouse up events received in that message loop to be
    able to resume caret blinking.
    (WebCore::EventHandler::lostMouseCapture):
    We've lost mouse capture and won't be notified of mouse up events;
    resume caret blinking.
    
    * page/EventHandler.h:
    Declare lostMouseCapture().
    
    WebKit/win:
    
    * WebView.cpp:
    (WebView::handleMouseEvent):
    If the message is WM_CANCELMODE, which indicates that we our capturing
    of mouse events has been cancelled, tell the EventHandler.
    It's possible to re-enter this function if handling a mouse event allows
    the message loop to run; moved up the call to setMouseActivated(), so
    that if we do re-enter this function, the later mouse event will not be
    considered as activating the window.
    (WebView::WebViewWndProc):
    Handle WM_CANCELMODE by calling handleMouseEvent().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58625 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/WebCore/page/EventHandler.cpp
@@ -440,7 +440,6 @@ bool EventHandler::handleMousePressEvent(const MouseEventWithHitTestResults& eve
 #endif
 
     bool swallowEvent = false;
-    m_frame->selection()->setCaretBlinkingSuspended(true);
     m_mousePressed = true;
     m_beganSelectingText = false;
 
@@ -621,7 +620,12 @@ void EventHandler::updateSelectionForMouseDrag(Node* targetNode, const IntPoint&
     }
 }
 #endif // ENABLE(DRAG_SUPPORT)
-    
+
+void EventHandler::lostMouseCapture()
+{
+    m_frame->selection()->setCaretBlinkingSuspended(false);
+}
+
 bool EventHandler::handleMouseUp(const MouseEventWithHitTestResults& event)
 {
     if (eventLoopHandleMouseUp(event))
@@ -1269,6 +1273,8 @@ bool EventHandler::handleMousePressEvent(const PlatformMouseEvent& mouseEvent)
             return true;
         }
     }
+
+    m_frame->selection()->setCaretBlinkingSuspended(true);
 
     bool swallowEvent = dispatchMouseEvent(eventNames().mousedownEvent, mev.targetNode(), true, m_clickCount, mouseEvent, true);
     m_capturesDragging = !swallowEvent;
