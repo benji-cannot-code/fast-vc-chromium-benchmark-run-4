5f1e734f7f9e1f49fd0e57e17c5717785509d5c9
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-02 07:23:16 +0000

    2010-05-02  Rafael Weinstein  <rafaelw@chromium.org>
            Reviewed by Darin Fisher.
    
            Include WebWindowFeatures in call to WebViewClient::createView.
            https://bugs.webkit.org/show_bug.cgi?id=38301
    
            * public/WebViewClient.h:
            (WebKit::WebViewClient::createView):
            * public/WebWindowFeatures.h:
            * src/ChromeClientImpl.cpp:
            (WebKit::ChromeClientImpl::createWindow):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebWindowFeatures.h
+++ third_party/WebKit/WebKit/chromium/public/WebWindowFeatures.h
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebWindowFeatures_h
 
 #include "WebCommon.h"
+#include "WebString.h"
+#include "WebVector.h"
 
 #if WEBKIT_IMPLEMENTATION
 #include "WindowFeatures.h"
--- third_party/WebKit/WebKit/chromium/src/ChromeClientImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/ChromeClientImpl.cpp
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebURLRequest.h"
 #include "WebViewClient.h"
 #include "WebViewImpl.h"
+#include "WebWindowFeatures.h"
 #include "WindowFeatures.h"
 #include "WrappedResourceRequest.h"
 
@@ -234,7 +235,7 @@ Page* ChromeClientImpl::createWindow(
         return 0;
 
     WebViewImpl* newView = static_cast<WebViewImpl*>(
-        m_webView->client()->createView(WebFrameImpl::fromFrame(frame)));
+        m_webView->client()->createView(WebFrameImpl::fromFrame(frame), features));
     if (!newView)
         return 0;
 
