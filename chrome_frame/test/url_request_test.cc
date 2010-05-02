38c5d9b3bf5cf09853f95bc517ba9539a31881fa
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-02 02:45:55 +0000

    Re-enable the Head test.
    TEST=Fixes the UrlRequest Head test
    BUG=none
    TBR=amit
    
    Review URL: http://codereview.chromium.org/1829001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46190 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/url_request_test.cc
+++ chrome_frame/test/url_request_test.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 #include <atlbase.h>
 #include <atlcom.h>
+
 #include "app/win_util.h"
+#include "chrome_frame/test/test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gmock_mutant.h"
@@ -105,18 +107,21 @@ TEST(UrlmonUrlRequestTest, Simple1) {
 
 // Same as Simple1 except we use the HEAD verb to fetch only the headers
 // from the server.
-// TODO(tommi): Fix and reenable.
-TEST(UrlmonUrlRequestTest, DISABLED_Head) {
+TEST(UrlmonUrlRequestTest, Head) {
   MockUrlDelegate mock;
-  ChromeFrameHTTPServer server;
   chrome_frame_test::TimedMsgLoop loop;
+  // Use SimpleWebServer instead of the python server to support HEAD
+  // requests.
+  test_server::SimpleWebServer server(13337);
+  test_server::SimpleResponse head_response("/head", "");
+  server.AddResponse(&head_response);
+
   win_util::ScopedCOMInitializer init_com;
   CComObjectStackEx<UrlmonUrlRequest> request;
 
-  server.SetUp();
   request.AddRef();
   request.Initialize(&mock, 1,  // request_id
-      server.Resolve(L"files/chrome_frame_window_open.html").spec(),
+      "http://localhost:13337/head",
       "head",
       "",      // referrer
       "",      // extra request
@@ -130,7 +135,6 @@ TEST(UrlmonUrlRequestTest, DISABLED_Head) {
     .WillOnce(testing::IgnoreResult(testing::InvokeWithoutArgs(CreateFunctor(
         &request, &UrlmonUrlRequest::Read, 512))));
 
-
   // For HEAD requests we don't expect content reads.
   EXPECT_CALL(mock, OnReadComplete(1, testing::_)).Times(0);
 
@@ -141,7 +145,6 @@ TEST(UrlmonUrlRequestTest, DISABLED_Head) {
   request.Start();
   loop.RunFor(kChromeFrameLongNavigationTimeoutInSeconds);
   request.Release();
-  server.TearDown();
 }
 
 TEST(UrlmonUrlRequestTest, UnreachableUrl) {
