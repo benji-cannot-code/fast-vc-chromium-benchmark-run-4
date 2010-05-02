0835ae54bb4f215e2af063e7e13f89c9743a7b30
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-02 08:55:04 +0000

    2010-05-02  Benjamin Poulain  <benjamin.poulain@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] QWebPage::userAgentForUrl is terrible API
            https://bugs.webkit.org/show_bug.cgi?id=33875
    
            Simplify the creation of the user agent string to avoid some
            overhead for each loaded url.
    
            The static part of the user agent is cached so it only have
            to be made once.
            This creation has been made in order to simplify the code.
    
            The two variable: application name and current language are
            set dynamically when needed.
            The default locale is non longer created if the widget locale
            is used.
    
            * Api/qwebpage.cpp:
            (QWebPage::userAgentForUrl):
            * tests/qwebpage/tst_qwebpage.cpp:
            (tst_QWebPage::userAgentApplicationName):
            (tst_QWebPage::userAgentLocaleChange):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58648 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/tests/qwebpage/tst_qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/tests/qwebpage/tst_qwebpage.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QDir>
 #include <QGraphicsWidget>
 #include <QLineEdit>
+#include <QLocale>
 #include <QMenu>
 #include <QPushButton>
 #include <QtTest/QtTest>
@@ -108,6 +109,8 @@ private slots:
     void errorPageExtension();
     void errorPageExtensionInIFrames();
     void errorPageExtensionInFrameset();
+    void userAgentApplicationName();
+    void userAgentLocaleChange();
 
     void wrt_viewModes();
 
@@ -1764,6 +1767,38 @@ void tst_QWebPage::errorPageExtensionInFrameset()
     QCOMPARE(page->mainFrame()->childFrames()[1]->toPlainText(), QString("data:text/html,error"));
 
     m_view->setPage(0);
+}
+
+class FriendlyWebPage : public QWebPage
+{
+public:
+    friend class tst_QWebPage;
+};
+
+void tst_QWebPage::userAgentApplicationName()
+{
+    const QString oldApplicationName = QCoreApplication::applicationName();
+    FriendlyWebPage page;
+
+    const QString applicationNameMarker = QString::fromUtf8("StrangeName\342\210\236");
+    QCoreApplication::setApplicationName(applicationNameMarker);
+    QVERIFY(page.userAgentForUrl(QUrl()).contains(applicationNameMarker));
+
+    QCoreApplication::setApplicationName(oldApplicationName);
+}
+
+void tst_QWebPage::userAgentLocaleChange()
+{
+    FriendlyWebPage page;
+    m_view->setPage(&page);
+
+    const QString markerString = QString::fromLatin1(" nn-NO)");
+
+    if (page.userAgentForUrl(QUrl()).contains(markerString))
+        QSKIP("marker string already present", SkipSingle);
+
+    m_view->setLocale(QLocale(QString::fromLatin1("nn_NO")));
+    QVERIFY(page.userAgentForUrl(QUrl()).contains(markerString));
 }
 
 void tst_QWebPage::crashTests_LazyInitializationOfMainFrame()
