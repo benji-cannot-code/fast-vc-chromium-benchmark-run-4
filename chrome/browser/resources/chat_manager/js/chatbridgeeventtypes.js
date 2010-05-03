12889ce0a598373caa57eb7167911ec2f83c1798
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-03 17:11:04 +0000

    Checking in chat_manager built-in extension.
    Patch by seh@chromium.org
    http://codereview.chromium.org/1719023/show
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1875001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46232 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/resources/chat_manager/js/chatbridgeeventtypes.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Chat bridge event types.
+ * @enum {string}
+ */
+var ChatBridgeEventTypes = {
+  SHOW_CHAT: 'showChat',
+  START_VIDEO: 'startVideo',
+  START_VOICE: 'startVoice',
+  CENTRAL_USER_UPDATE: 'centralJidUpdate',
+  REQUEST_CENTRAL_USER: 'getCentralJid'
+};
