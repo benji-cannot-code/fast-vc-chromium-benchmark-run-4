400ca39869ef5a7f23fab7967799060e07bd4a0a
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-03 17:28:17 +0000

    Fix for https://bugs.webkit.org/show_bug.cgi?id=38471 Add generic callback mechanism
    Reviewed by Anders Carlsson.
    
    Added GenericCallback class replacing RenderTreeExternalRepresentationCallback
    and ScriptReturnValueCallback.
            
    Also,
    - Standardize C API callbacks to take the context last.
    - Standardize C API callbacks to not have the _f suffix (now the block
      variants have a _b suffix).
    - Re-write toWK and toRef methods as a set of template functions using
      the generic API->implementation mapping information.
    
    * UIProcess/API/C/WKAPICast.h:
    * UIProcess/API/C/WKPage.cpp:
    (WKPageRunJavaScriptInMainFrame):
    (callRunJavaScriptBlockAndRelease):
    (disposeRunJavaScriptBlock):
    (WKPageRunJavaScriptInMainFrame_b):
    (WKPageRenderTreeExternalRepresentation):
    (WKPageRenderTreeExternalRepresentation_b):
    * UIProcess/API/C/WKPage.h:
    * UIProcess/API/C/WKPagePrivate.h:
    * UIProcess/GenericCallback.h: Added.
    (WebKit::GenericCallback::create):
    (WebKit::GenericCallback::~GenericCallback):
    (WebKit::GenericCallback::performCallbackWithReturnValue):
    (WebKit::GenericCallback::invalidate):
    (WebKit::GenericCallback::callbackID):
    (WebKit::GenericCallback::generateCallbackID):
    (WebKit::GenericCallback::GenericCallback):
    * UIProcess/RenderTreeExternalRepresentationCallback.cpp: Removed.
    * UIProcess/RenderTreeExternalRepresentationCallback.h: Removed.
    * UIProcess/ScriptReturnValueCallback.cpp: Removed.
    * UIProcess/ScriptReturnValueCallback.h: Removed.
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::didRunJavaScriptInMainFrame):
    (WebKit::WebPageProxy::didGetRenderTreeExternalRepresentation):
    * UIProcess/WebPageProxy.h:
    * WebKit2.xcodeproj/project.pbxproj:
    * win/WebKit2.vcproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/WebPageProxy.cpp
+++ third_party/WebKit/WebKit2/UIProcess/WebPageProxy.cpp
@@ -783,7 +783,7 @@ void WebPageProxy::didRunJavaScriptInMainFrame(const String& resultString, uint6
         return;
     }
 
-    callback->performCallbackWithReturnValue(resultString);
+    callback->performCallbackWithReturnValue(resultString.impl());
 }
 
 void WebPageProxy::didGetRenderTreeExternalRepresentation(const String& resultString, uint64_t callbackID)
@@ -794,7 +794,7 @@ void WebPageProxy::didGetRenderTreeExternalRepresentation(const String& resultSt
         return;
     }
 
-    callback->performCallbackWithReturnValue(resultString);
+    callback->performCallbackWithReturnValue(resultString.impl());
 }
 
 void WebPageProxy::processDidBecomeUnresponsive()
--- third_party/WebKit/WebKit2/UIProcess/WebPageProxy.h
+++ third_party/WebKit/WebKit2/UIProcess/WebPageProxy.h
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebPageProxy_h
 
 #include "DrawingAreaProxy.h"
-#include "RenderTreeExternalRepresentationCallback.h"
-#include "ScriptReturnValueCallback.h"
+#include "GenericCallback.h"
+#include "WKBase.h"
 #include "WebEvent.h"
 #include "WebFrameProxy.h"
 #include "WebHistoryClient.h"
@@ -67,6 +67,9 @@ class WebPageNamespace;
 class WebProcessProxy;
 class WebWheelEvent;
 class WebNavigationDataStore;
+
+typedef GenericCallback<WKStringRef> RenderTreeExternalRepresentationCallback;
+typedef GenericCallback<WKStringRef> ScriptReturnValueCallback;
 
 class WebPageProxy : public RefCounted<WebPageProxy> {
 public:
