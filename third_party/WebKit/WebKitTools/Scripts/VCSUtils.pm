35c44211b107c9c65a67839c4d94543e0ad5a31a
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-03 01:49:27 +0000

    2010-05-02  Chris Jerdonek  <cjerdonek@webkit.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=38319
    
            * Scripts/VCSUtils.pm:
              - In parseDiffHeader()--
                - Added an "scmFormat" hash key to the return value to represent
                  whether the diff is Git or SVN formatted.
                - Adjusted the code so the value of "copiedFromPath" will
                  be undef rather than "does not exist" if the file was not
                  copied.
    
            * Scripts/webkitperl/VCSUtils_unittest/parseDiffHeader.pl:
              - Added a FIXME to refactor these unit tests to use is_deeply().
    
            * Scripts/webkitperl/VCSUtils_unittest/parseDiffHeader.pl:
              - Updated the unit tests to test the "scmFormat" value.
              - Simplified the unit tests by refactoring them to use is_deeply().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58663 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/VCSUtils.pm
+++ third_party/WebKit/WebKitTools/Scripts/VCSUtils.pm
@@ -440,6 +440,7 @@ sub gitdiff2svndiff($)
 #     copiedFromPath: if a file copy, the path from which the file was
 #                     copied. Otherwise, undefined.
 #     indexPath: the path in the "Index:" line.
+#     scmFormat: The string "git" or "svn" depending on the format.
 #     sourceRevision: the revision number of the source. This is the same
 #                     as the revision number the file was copied from, in
 #                     the case of a file copy.
@@ -466,6 +467,7 @@ sub parseDiffHeader($$)
 
     my %header;
 
+    my $copiedFromPath;
     my $foundHeaderEnding;
     my $lastReadLine; 
     my $sourceRevision;
@@ -487,7 +489,7 @@ sub parseDiffHeader($$)
                 if (/\(from (\S+):(\d+)\)$/) {
                     # The "from" clause is created by svn-create-patch, in
                     # which case there is always also a "revision" clause.
-                    $header{copiedFromPath} = $1;
+                    $copiedFromPath = $1;
                     die("Revision number \"$2\" in \"from\" clause does not match " .
                         "source revision number \"$sourceRevision\".") if ($2 != $sourceRevision);
                 }
@@ -510,7 +512,9 @@ sub parseDiffHeader($$)
         die("Did not find end of header block corresponding to index path \"$indexPath\".");
     }
 
+    $header{copiedFromPath} = $copiedFromPath;
     $header{indexPath} = $indexPath;
+    $header{scmFormat} = $filter ? "git" : "svn";
     $header{sourceRevision} = $sourceRevision;
     $header{svnConvertedText} = $svnConvertedText;
 
@@ -587,6 +591,7 @@ sub parseDiff($$)
     my %diffHashRef;
     $diffHashRef{copiedFromPath} = $headerHashRef->{copiedFromPath};
     $diffHashRef{indexPath} = $headerHashRef->{indexPath};
+    # FIXME: Also add scmFormat from the $headerHashRef.
     $diffHashRef{sourceRevision} = $headerHashRef->{sourceRevision};
     $diffHashRef{svnConvertedText} = $svnText;
 
