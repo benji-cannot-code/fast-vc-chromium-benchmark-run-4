e37e7422529d39461aa576a56e415a40fcebd105
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-03 06:13:14 +0000

    DevTools: focus devtools on breakpoint while docked (Linux & Mac fix).
    Review URL: http://codereview.chromium.org/1846001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46212 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_window.cc
+++ chrome/browser/debugger/devtools_window.cc
@@ -154,7 +154,7 @@ void DevToolsWindow::Activate() {
   } else {
     BrowserWindow* inspected_window = GetInspectedBrowserWindow();
     if (inspected_window)
-      inspected_window->FocusDevTools();
+      tab_contents_->view()->Focus();
   }
 }
 
