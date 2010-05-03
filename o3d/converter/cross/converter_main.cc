01c28c7df8dff231fe616d9dee832f963ef77d36
kbr@google.com <kbr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-03 22:52:49 +0000

    Compute the absolute path containing the o3dConverter tool for the case where it is being run from the current working directory and "." is not on the PATH.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1896001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46294 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/converter/cross/converter_main.cc
+++ o3d/converter/cross/converter_main.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/file_path.h"
+#include "base/file_util.h"
 #include "base/string_util.h"
 #include "converter/cross/converter.h"
 #include "utils/cross/file_path_utils.h"
@@ -70,7 +71,11 @@ int CrossMain(int argc, char**argv) {
   const CommandLine* command_line = CommandLine::ForCurrentProcess();
 
   FilePath in_filename, out_filename;
-  const FilePath converter_tool = FilePath(argv[0]).DirName().Append(
+  // Use the absolute path to the converter tool for the case that we
+  // are in the current working directory and "." is not on the PATH.
+  FilePath converter_dir = FilePath(argv[0]).DirName();
+  file_util::AbsolutePath(&converter_dir);
+  const FilePath converter_tool = converter_dir.Append(
       o3d::UTF8ToFilePath("convert.py"));
 
 
