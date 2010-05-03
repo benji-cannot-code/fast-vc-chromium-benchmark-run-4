1fdb6efe60e561d7d553cbdeff2aea6fc94b6596
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-03 22:34:35 +0000

    Fix dragging icon in Extension App Windows (it was enlarged while dragging).
    The tab contents changes are purely cosmetic (readability).
    
    BUG=43114
    TEST=Drag a tab in an Extension App Window. It should have a regular sized icon.
    
    Review URL: http://codereview.chromium.org/1889001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46291 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -5,6 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/tab_contents/tab_contents.h"
 
+#if defined(OS_CHROMEOS)
+// For GdkScreen
+#include <gdk/gdk.h>
+#endif  // defined(OS_CHROMEOS)
+
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
 #include "app/text_elider.h"
@@ -88,11 +93,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/registry_controlled_domain.h"
 #include "webkit/glue/password_form.h"
 #include "webkit/glue/webpreferences.h"
-
-#if defined(OS_CHROMEOS)
-// For GdkScreen
-#include <gdk/gdk.h>
-#endif  // defined(OS_CHROMEOS)
 
 // Cross-Site Navigations
 //
@@ -523,11 +523,9 @@ SkBitmap* TabContents::GetAppExtensionIcon() {
   // We don't show the big icons in tabs for TYPE_EXTENSION_APP windows because
   // for those windows, we already have a big icon in the top-left outside any
   // tab. Having big tab icons too looks kinda redonk.
-  if (delegate_ &&
-      delegate_->GetBrowser() &&
-      delegate_->GetBrowser()->type() == Browser::TYPE_EXTENSION_APP) {
+  Browser* browser = delegate_ ? delegate_->GetBrowser() : NULL;
+  if (browser && browser->type() == Browser::TYPE_EXTENSION_APP)
     return NULL;
-  }
 
   if (app_extension_icon_.empty())
     return NULL;
