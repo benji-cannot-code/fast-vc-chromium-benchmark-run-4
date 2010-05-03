f7a3b1226a41a83c955b4a801ef8f9931e02d312
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-03 17:55:11 +0000

    Win: Fix wrong default icon being shown on chrome.exe.
    BUG=42977
    TEST=build official exe, look at it in explorer
    
    Review URL: http://codereview.chromium.org/1736029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46241 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_dll_resource.h
+++ chrome/app/chrome_dll_resource.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDI_THROBBER_22                 123
 #define IDI_THROBBER_23                 124
 #define IDI_THROBBER_24                 125
-#define IDI_SXS                         126
+#define IDR_SXS                         126
 
 // Values around 1600 are used by installer/setup/build/
 
