736271a4e80904eab1a7bd34ef1f4356f1b3e00c
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-04 04:15:26 +0000

    AutoFill: Fix IsAutoFillable to only count fields that are heuristically matched.  Move heuristic matching into the constructor of FormStructure and update the tests accordingly.
    BUG=41255
    TEST=FormStructureTest.*
    
    Review URL: http://codereview.chromium.org/1906001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46329 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_download_unittest.cc
+++ chrome/browser/autofill/autofill_download_unittest.cc
@@ -107,6 +107,14 @@ TEST(AutoFillDownloadTest, QueryAndUploadTest) {
                                                ASCIIToUTF16("username"),
                                                string16(),
                                                ASCIIToUTF16("text")));
+  form.fields.push_back(webkit_glue::FormField(ASCIIToUTF16("First Name"),
+                                               ASCIIToUTF16("firstname"),
+                                               string16(),
+                                               ASCIIToUTF16("text")));
+  form.fields.push_back(webkit_glue::FormField(ASCIIToUTF16("Last Name"),
+                                               ASCIIToUTF16("lastname"),
+                                               string16(),
+                                               ASCIIToUTF16("text")));
   form.fields.push_back(webkit_glue::FormField(ASCIIToUTF16("email"),
                                                ASCIIToUTF16("email"),
                                                string16(),
@@ -163,6 +171,8 @@ TEST(AutoFillDownloadTest, QueryAndUploadTest) {
   const char *responses[] = {
     "<autofillqueryresponse>"
       "<field autofilltype=\"0\" />"
+      "<field autofilltype=\"3\" />"
+      "<field autofilltype=\"5\" />"
       "<field autofilltype=\"9\" />"
       "<field autofilltype=\"0\" />"
       "<field autofilltype=\"30\" />"
