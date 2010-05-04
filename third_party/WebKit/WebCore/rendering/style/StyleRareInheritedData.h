6951afc00661f0ca53592e21ba2ea4efaf95d338
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-04 00:03:57 +0000

    2010-05-03  James Robinson  <jamesr@chromium.org>
            Reviewed by Eric Seidel.
    
            Clean up a few compiler warnings
            https://bugs.webkit.org/show_bug.cgi?id=38073
    
            * wtf/text/StringImpl.cpp:
            (WebCore::StringImpl::ascii):
    2010-05-03  James Robinson  <jamesr@chromium.org>
    
            Reviewed by Eric Seidel.
    
            Clean up a few compiler warnings
            https://bugs.webkit.org/show_bug.cgi?id=38073
    
            * html/TextMetrics.h:
            (WebCore::TextMetrics::width):
            * rendering/style/StyleRareInheritedData.h:
            * rendering/style/StyleRareNonInheritedData.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58718 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/style/StyleRareInheritedData.h
+++ third_party/WebKit/WebCore/rendering/style/StyleRareInheritedData.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-struct ShadowData;
+class ShadowData;
 
 // This struct is for rarely used inherited CSS3, CSS2, and WebKit-specific properties.
 // By grouping them together, we save space, and only allocate this object when someone
