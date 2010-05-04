c7d3e3e1f576f864c51c8c2bdc2116c60c95231f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-04 21:02:40 +0000

    2010-05-04  Steve Block  <steveblock@google.com>
            Reviewed by Darin Adler.
    
            New FileSystemPOSIX functions cause linker errors on Android
            https://bugs.webkit.org/show_bug.cgi?id=38521
    
            No new tests, build fix only.
    
            * platform/android/FileSystemAndroid.cpp: Remove closeFile, which is provided in FileSystemPOSIX.cpp
            * platform/posix/FileSystemPOSIX.cpp: Guard writeToFile with !PLATFORM(ANDROID)
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58774 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/posix/FileSystemPOSIX.cpp
+++ third_party/WebKit/WebCore/platform/posix/FileSystemPOSIX.cpp
@@ -111,6 +111,7 @@ bool truncateFile(PlatformFileHandle handle, long long offset)
     return !ftruncate(handle, offset);
 }
 
+#if !PLATFORM(ANDROID)
 int writeToFile(PlatformFileHandle handle, const char* data, int length)
 {
     do {
@@ -120,6 +121,7 @@ int writeToFile(PlatformFileHandle handle, const char* data, int length)
     } while (errno == EINTR);
     return -1;
 }
+#endif
 
 int readFromFile(PlatformFileHandle handle, char* data, int length)
 {
