0061053c4672eb1ed40ccd131f8b5ac29e07e705
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-04 20:16:12 +0000

    Fix for <rdar://problem/7818509> Crash occurs when exiting Safari
    Reviewed by Mike Thole.
    
    We can avoid this crash if we call [self _removeFromAllWebViewsSet] 
    even in the case when we are doing a fastDocumentTeardown. This is 
    a much safer approach.
    * WebView/WebView.mm:
    (-[WebView _close]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58771 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -1014,6 +1014,7 @@ static bool fastDocumentTeardownEnabled()
         return;
 
     _private->closed = YES;
+    [self _removeFromAllWebViewsSet];
 
     [self _closingEventHandling];
 
@@ -1035,7 +1036,6 @@ static bool fastDocumentTeardownEnabled()
     if (Frame* mainFrame = [self _mainCoreFrame])
         mainFrame->loader()->detachFromParent();
 
-    [self _removeFromAllWebViewsSet];
     [self setHostWindow:nil];
 
     [self setDownloadDelegate:nil];
