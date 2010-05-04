290b86fade5f7b8e9ceee1bf38a7789fc5fd18fc
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-04 22:01:38 +0000

    2010-05-04  Yongjun Zhang  <yongjun_zhang@apple.com>
            Reviewed by Darin Adler.
    
            WebKit crashes at DebuggerCallFrame::functionName() if m_callFrame is the top global callframe.
            https://bugs.webkit.org/show_bug.cgi?id=38535
    
            Don't call asFunction if callee is not a FunctionType to prevent assertion failure
            in JSCell::isObject().
    
            * debugger/DebuggerCallFrame.cpp:
            (JSC::DebuggerCallFrame::functionName):
            (JSC::DebuggerCallFrame::calculatedFunctionName):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58779 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/debugger/DebuggerCallFrame.cpp
+++ third_party/WebKit/JavaScriptCore/debugger/DebuggerCallFrame.cpp
@@ -42,6 +42,9 @@ const UString* DebuggerCallFrame::functionName() const
     if (!m_callFrame->codeBlock())
         return 0;
 
+    if (!m_callFrame->callee())
+        return 0;
+
     JSFunction* function = asFunction(m_callFrame->callee());
     if (!function)
         return 0;
@@ -52,7 +55,10 @@ UString DebuggerCallFrame::calculatedFunctionName() const
 {
     if (!m_callFrame->codeBlock())
         return UString();
-    
+
+    if (!m_callFrame->callee())
+        return UString();
+
     JSFunction* function = asFunction(m_callFrame->callee());
     if (!function)
         return UString();
