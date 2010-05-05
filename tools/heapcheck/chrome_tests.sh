f585610a13c7cd5faa43b0414891e6fb8272c3fe
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-05 08:17:14 +0000

    Fix PYTHONPATH handling for heapchecker according to http://codereview.chromium.org/1736026/show
    Review URL: http://codereview.chromium.org/1910003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46446 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/heapcheck/chrome_tests.sh
+++ tools/heapcheck/chrome_tests.sh
@@ -6,4 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 THISDIR=$(dirname "${0}")
-PYTHONPATH="${THISDIR}/../valgrind:${THISDIR}/../python" exec "${THISDIR}/chrome_tests.py" "${@}"
+PYTHONPATH="${THISDIR}/../valgrind:${THISDIR}/../python/google" exec "${THISDIR}/chrome_tests.py" "${@}"
