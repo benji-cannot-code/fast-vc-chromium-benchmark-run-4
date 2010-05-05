4db338609ce77bb2c8d64d61c344601bb8d8af90
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-05 01:08:31 +0000

    2010-05-04  Vangelis Kokkevis  <vangelis@chromium.org>
            Reviewed by Darin Fisher.
    
            Provide an API for querying whether a WebWidget is using gpu accelerated compositing.
            Also changed: isAcceleratedCompositing to: isAcceleratedCompositingActive.
            https://bugs.webkit.org/show_bug.cgi?id=38220
    
            * public/WebWidget.h:
            * src/WebPopupMenuImpl.h:
            (WebKit::WebPopupMenuImpl::isAcceleratedCompositingActive):
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::WebViewImpl):
            (WebKit::WebViewImpl::paint):
            (WebKit::WebViewImpl::isAcceleratedCompositingActive):
            (WebKit::WebViewImpl::setRootGraphicsLayer):
            (WebKit::WebViewImpl::setIsAcceleratedCompositingActive):
            (WebKit::WebViewImpl::updateRootLayerContents):
            * src/WebViewImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
@@ -243,7 +243,7 @@ WebViewImpl::WebViewImpl(WebViewClient* client)
     , m_haveMouseCapture(false)
 #if USE(ACCELERATED_COMPOSITING)
     , m_layerRenderer(0)
-    , m_isAcceleratedCompositing(false)
+    , m_isAcceleratedCompositingActive(false)
 #endif
 {
     // WebKit/win/WebView.cpp does the same thing, except they call the
@@ -945,7 +945,7 @@ void WebViewImpl::paint(WebCanvas* canvas, const WebRect& rect)
 {
 
 #if USE(ACCELERATED_COMPOSITING)
-    if (!isAcceleratedCompositing()) {
+    if (!isAcceleratedCompositingActive()) {
 #endif
         WebFrameImpl* webframe = mainFrameImpl();
         if (webframe)
@@ -1279,6 +1279,15 @@ void WebViewImpl::setTextDirection(WebTextDirection direction)
         notImplemented();
         break;
     }
+}
+
+bool WebViewImpl::isAcceleratedCompositingActive() const
+{
+#if USE(ACCELERATED_COMPOSITING)
+    return m_isAcceleratedCompositingActive;
+#else
+    return false;
+#endif
 }
 
 // WebView --------------------------------------------------------------------
@@ -2058,29 +2067,29 @@ bool WebViewImpl::tabsToLinks() const
 #if USE(ACCELERATED_COMPOSITING)
 void WebViewImpl::setRootGraphicsLayer(WebCore::PlatformLayer* layer)
 {
-    setAcceleratedCompositing(layer ? true : false);
+    setIsAcceleratedCompositingActive(layer ? true : false);
     if (m_layerRenderer)
         m_layerRenderer->setRootLayer(layer);
 }
 
-void WebViewImpl::setAcceleratedCompositing(bool accelerated)
+void WebViewImpl::setIsAcceleratedCompositingActive(bool active)
 {
-    if (m_isAcceleratedCompositing == accelerated)
+    if (m_isAcceleratedCompositingActive == active)
         return;
 
-    if (accelerated) {
+    if (active) {
         m_layerRenderer = LayerRendererChromium::create();
         if (m_layerRenderer)
-            m_isAcceleratedCompositing = true;
+            m_isAcceleratedCompositingActive = true;
     } else {
         m_layerRenderer = 0;
-        m_isAcceleratedCompositing = false;
+        m_isAcceleratedCompositingActive = false;
     }
 }
 
 void WebViewImpl::updateRootLayerContents(const WebRect& rect)
 {
-    if (!isAcceleratedCompositing())
+    if (!isAcceleratedCompositingActive())
         return;
 
     WebFrameImpl* webframe = mainFrameImpl();
--- third_party/WebKit/WebKit/chromium/src/WebViewImpl.h
+++ third_party/WebKit/WebKit/chromium/src/WebViewImpl.h
@@ -101,6 +101,7 @@ public:
     virtual bool queryCompositionStatus(bool* enabled,
                                         WebRect* caretRect);
     virtual void setTextDirection(WebTextDirection direction);
+    virtual bool isAcceleratedCompositingActive() const;
 
     // WebView methods:
     virtual void initializeMainFrame(WebFrameClient*);
@@ -360,8 +361,7 @@ private:
                                                DragAction);
 
 #if USE(ACCELERATED_COMPOSITING)
-    void setAcceleratedCompositing(bool);
-    bool isAcceleratedCompositing() const { return m_isAcceleratedCompositing; }
+    void setIsAcceleratedCompositingActive(bool);
     void updateRootLayerContents(const WebRect&);
 #endif
 
@@ -497,7 +497,7 @@ private:
 
 #if USE(ACCELERATED_COMPOSITING)
     OwnPtr<WebCore::LayerRendererChromium> m_layerRenderer;
-    bool m_isAcceleratedCompositing;
+    bool m_isAcceleratedCompositingActive;
 #endif
     static const WebInputEvent* m_currentInputEvent;
 };
