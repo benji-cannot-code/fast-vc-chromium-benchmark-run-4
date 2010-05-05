67f6fec8c6c50acacf4a09aa5adfaa8906f49928
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-05 22:59:00 +0000

    [Qt]Disable Qt Multimedia backend for HTML 5 Audio and Video elements Fall back to the Phonon backend until the release situation has been cleared up.
    Reviewed by Simon Hausmann.
    
    https://bugs.webkit.org/show_bug.cgi?id=38612
    
    Buildfix for QT_VERSION >= 0x040700 after r58810.
    
    * platform/graphics/MediaPlayer.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58842 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/MediaPlayer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/MediaPlayer.cpp
@@ -51,7 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif PLATFORM(GTK)
 #include "MediaPlayerPrivateGStreamer.h"
 #elif PLATFORM(QT)
-#if QT_VERSION < 0x040700
+// QtMultimedia support is disabled currently.
+#if true || (QT_VERSION < 0x040700)
 #include "MediaPlayerPrivatePhonon.h"
 #else
 #include "MediaPlayerPrivateQt.h"
