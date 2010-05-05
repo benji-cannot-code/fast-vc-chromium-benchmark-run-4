c39472471f645b52ef724e4d8704a738f4ab63cb
amit@chromium.org <amit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-05 23:46:30 +0000

    Do not wrap the original callback if we can;t initialize sniffing cache
    CreateStreamOnHGlobal may fail if the underlying GlobalAlloc fails. It has
    a strange behavaior where if we pass in an HGlobal, it can create more
    streams per process. So the fix is to:
    1. not register us as a bind status callback if we cannot create a stream
       for sniffing data and 
    2. When initializing the data sniffing stream pre-create a HGlobal
    
    BUG=To be filed
    TEST=none
    
    Review URL: http://codereview.chromium.org/1983002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46525 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/urlmon_bind_status_callback.h
+++ chrome_frame/urlmon_bind_status_callback.h
@@ -53,7 +53,7 @@ class SniffData {
     OTHER
   };
 
-  bool InitializeCache(const std::wstring& url);
+  HRESULT InitializeCache(const std::wstring& url);
   HRESULT ReadIntoCache(IStream* stream, bool force_determination);
   HRESULT DrainCache(IBindStatusCallback* bscb, DWORD bscf,
                      CLIPFORMAT clip_format);
