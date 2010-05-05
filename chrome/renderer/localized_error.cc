4f92c4e0530a7f036380fd3620a281d31a4073de
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-05 00:24:20 +0000

    Small cleanups/style fixes.  Simplifies RTL functions slightly.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/1932006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46419 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/localized_error.cc
+++ chrome/renderer/localized_error.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -108,9 +108,9 @@ WebErrorNetErrorMap net_error_options[] = {
 
 bool LocaleIsRTL() {
 #if defined(TOOLKIT_GTK)
-  // base::i18n::GetTextDirection uses the GTK text direction, which doesn't work
-  // within the renderer sandbox.
-  return base::i18n::GetICUTextDirection() == base::i18n::RIGHT_TO_LEFT;
+  // base::i18n::IsRTL() uses the GTK text direction, which doesn't work within
+  // the renderer sandbox.
+  return base::i18n::ICUIsRTL();
 #else
   return base::i18n::IsRTL();
 #endif
