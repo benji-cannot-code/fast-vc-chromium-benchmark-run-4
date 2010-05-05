a0bbdc9679110577ee08723c51720047f672194e
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-05 02:41:38 +0000

    Revert 46384 - Fix race in zygote_host_linux where socket was being read from and written to on different threads.
    Made ZygoteHost methods all run on PROCESS_LAUNCHER thread. PostTask in linux from UI thread to PROCESS_LAUNCHER thread for DidProcessCrash. Changed DidProcessCrash to answer via a callback, which occurs asynchronously on linux. Rework cases in nacl_host, browser_render_process_host, and child_process_host where this method was being called to fit the callback model.
    
    BUG=31737
    TEST=none
    
    Review URL: http://codereview.chromium.org/1695026
    
    TBR=kkania@chromium.org
    Review URL: http://codereview.chromium.org/1933007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46429 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.h
+++ chrome/browser/renderer_host/browser_render_process_host.h
@@ -101,7 +101,6 @@ class BrowserRenderProcessHost : public RenderProcessHost,
 
   // ChildProcessLauncher::Client implementation.
   virtual void OnProcessLaunched();
-  virtual void OnDidProcessCrashDetermined(bool did_crash);
 
  private:
   friend class VisitRelayingRenderProcessHost;
--- chrome/browser/zygote_host_linux.cc
+++ chrome/browser/zygote_host_linux.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/unix_domain_socket_posix.h"
 
-#include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/renderer_host/render_sandbox_host_linux.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
@@ -195,7 +194,6 @@ void ZygoteHost::Init(const std::string& sandbox_cmd) {
 pid_t ZygoteHost::ForkRenderer(
     const std::vector<std::string>& argv,
     const base::GlobalDescriptors::Mapping& mapping) {
-  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::PROCESS_LAUNCHER));
   DCHECK(init_);
   Pickle pickle;
 
@@ -244,7 +242,6 @@ pid_t ZygoteHost::ForkRenderer(
 }
 
 void ZygoteHost::EnsureProcessTerminated(pid_t process) {
-  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::PROCESS_LAUNCHER));
   DCHECK(init_);
   Pickle pickle;
 
@@ -257,7 +254,6 @@ void ZygoteHost::EnsureProcessTerminated(pid_t process) {
 
 bool ZygoteHost::DidProcessCrash(base::ProcessHandle handle,
                                  bool* child_exited) {
-  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::PROCESS_LAUNCHER));
   DCHECK(init_);
   Pickle pickle;
   pickle.WriteInt(kCmdDidProcessCrash);
