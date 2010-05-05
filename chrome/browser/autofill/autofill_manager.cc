7ba79306bcbf32ae8e0d668296b1861d2e90ef67
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-05 23:19:42 +0000

    AutoFill: Use PrefService::FindPreference instead of PrefService::HasPrefPath. HasPrefPath will crash if the preference is not registered.
    BUG=42358
    TEST=none
    
    Review URL: http://codereview.chromium.org/2007002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46519 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager.cc
+++ chrome/browser/autofill/autofill_manager.cc
@@ -359,7 +359,7 @@ bool AutoFillManager::IsAutoFillEnabled() const {
   PrefService* prefs = tab_contents_->profile()->GetPrefs();
 
   // Migrate obsolete AutoFill pref.
-  if (prefs->HasPrefPath(prefs::kFormAutofillEnabled)) {
+  if (prefs->FindPreference(prefs::kFormAutofillEnabled)) {
     bool enabled = prefs->GetBoolean(prefs::kFormAutofillEnabled);
     prefs->ClearPref(prefs::kFormAutofillEnabled);
     prefs->SetBoolean(prefs::kAutoFillEnabled, enabled);
