84b73213e9c1edb0e45c21099277be275714176d
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-05 00:43:22 +0000

    Replace CHECK(false) by LOG(FATAL) where appropriate.
    The difference is that the error message doesn't contain
    "Assertion failed: false".
    
    Review URL: http://codereview.chromium.org/1970001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46422 0039d316-1c4b-4281-b951-d872f2087c98

--- base/bzip2_error_handler.cc
+++ base/bzip2_error_handler.cc
@@ -9,5 +9,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // error reporting.  This requires us to export our own error handler.
 extern "C"
 void bz_internal_error(int errcode) {
-  CHECK(false) << "bzip2 internal error: " << errcode;
+  LOG(FATAL) << "bzip2 internal error: " << errcode;
 }
--- net/proxy/init_proxy_resolver_unittest.cc
+++ net/proxy/init_proxy_resolver_unittest.cc
@@ -69,7 +69,7 @@ class Rules {
       if (it->url == url)
         return *it;
     }
-    CHECK(false) << "Rule not found for " << url;
+    LOG(FATAL) << "Rule not found for " << url;
     return rules_[0];
   }
 
@@ -79,7 +79,7 @@ class Rules {
       if (it->bytes() == bytes)
         return *it;
     }
-    CHECK(false) << "Rule not found for " << bytes;
+    LOG(FATAL) << "Rule not found for " << bytes;
     return rules_[0];
   }
 
