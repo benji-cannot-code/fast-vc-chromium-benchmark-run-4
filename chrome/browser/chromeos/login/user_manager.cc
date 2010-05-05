588a20bf5a327012f8838291bde835323353e166
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-05 07:18:27 +0000

    Screen Locker: 1st step  * ScreenLocker screen currently reuses Background from Login screen.  * GrabWidget is created to grab keyboard/mouse events.  * Modified StubAuthentiator to fail when an empty password is entered.  * Changed UserManager to explicitly remember the currently logged in user. UI is not hooked yet.
    BUG=43131, http://crosbug.com/2914
    TEST=none
    
    Review URL: http://codereview.chromium.org/1897001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46442 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_manager.cc
+++ chrome/browser/chromeos/login/user_manager.cc
@@ -105,6 +105,8 @@ void UserManager::UserLoggedIn(const std::string& email) {
   ListValue* prefs_users = prefs->GetMutableList(kLoggedInUsers);
   prefs_users->Clear();
 
+  logged_in_user_.set_email(email);
+
   // Make sure this user is first.
   prefs_users->Append(Value::CreateStringValue(email));
   for (std::vector<User>::iterator it = users.begin();
@@ -114,6 +116,8 @@ void UserManager::UserLoggedIn(const std::string& email) {
     // Skip the most recent user.
     if (email != user_email) {
       prefs_users->Append(Value::CreateStringValue(user_email));
+    } else {
+      logged_in_user_ = *it;
     }
   }
   prefs->ScheduleSavePersistentPrefs();
@@ -122,7 +126,7 @@ void UserManager::UserLoggedIn(const std::string& email) {
   NotificationService::current()->Notify(
       NotificationType::LOGIN_USER_CHANGED,
       Source<UserManager>(this),
-      Details<const User>(&user));
+      Details<const User>(&logged_in_user_));
 }
 
 void UserManager::DownloadUserImage(const std::string& username) {
