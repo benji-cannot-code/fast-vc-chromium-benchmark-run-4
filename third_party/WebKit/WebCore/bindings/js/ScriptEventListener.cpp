78e1ad85b9d87dc34c6d5f0445ad6de44206f8f6
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-06 08:01:23 +0000

    2010-05-06  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Joseph Pecoraro.
    
            Web Inspector: follow up to linkify event listeners.
    
            https://bugs.webkit.org/show_bug.cgi?id=38257
    
            * bindings/js/ScriptEventListener.cpp:
            (WebCore::eventListenerHandlerLocation):
            * bindings/v8/ScriptEventListener.cpp:
            (WebCore::eventListenerHandlerLocation):
            * inspector/front-end/ElementsPanel.js:
            (WebInspector.ElementsPanel.prototype.linkifyNodeReference):
            * inspector/front-end/EventListenersSidebarPane.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/ScriptEventListener.cpp
+++ third_party/WebKit/WebCore/bindings/js/ScriptEventListener.cpp
@@ -119,6 +119,7 @@ String eventListenerHandlerBody(ScriptExecutionContext* context, ScriptState* sc
 
 bool eventListenerHandlerLocation(ScriptExecutionContext*, ScriptState*, EventListener*, String&, int&)
 {
+    // FIXME: Add support for getting function location.
     return false;
 }
 
