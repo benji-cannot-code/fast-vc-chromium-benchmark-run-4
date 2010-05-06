17e1d00cd2845e03a18867e45258920bd3055bbc
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-06 18:50:42 +0000

    2010-05-06  Adam Roben  <aroben@apple.com>
            Bail out of WebView::paint when there's nothing to paint
    
            Fixes <http://webkit.org/b/38670> <rdar://problem/7947105> REGRESSION
            (r58067): Crash in WebView::paint when Web Inspector is docked and
            window is resized so small that WebView disappears
    
            When the WebView is 0-sized, ensureBackingStore() bails out without
            creating a bitmap, leaving m_backingStoreBitmap null. Before r58067,
            m_backingStoreBitmap was an HBITMAP, so we were happily passing along
            a null HBITMAP to various Windows APIs. These calls would fail but not
            crash. r58067 changed m_backingStoreBitmap to a RefCountedHBITMAP, and
            dereferencing a null RefCountedHBITMAP* of course crashes.
    
            Reviewed by Steve Falkenburg.
    
            * WebView.cpp:
            (WebView::paint): Bail if the rect to paint is empty.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -946,8 +946,6 @@ void WebView::paint(HDC dc, LPARAM options)
         return;
     FrameView* frameView = coreFrame->view();
 
-    m_paintCount++;
-
     RECT rcPaint;
     HDC hdc;
     OwnPtr<HRGN> region;
@@ -971,6 +969,14 @@ void WebView::paint(HDC dc, LPARAM options)
         // children into the HDC.
         windowsToPaint = PaintWebViewAndChildren;
     }
+
+    if (::IsRectEmpty(&rcPaint)) {
+        if (!dc)
+            EndPaint(m_viewWindow, &ps);
+        return;
+    }
+
+    m_paintCount++;
 
     HDC bitmapDC = ::CreateCompatibleDC(hdc);
     bool backingStoreCompletelyDirty = ensureBackingStore();
