ff6d269376bd5a04d766820f89608807b347ae7d
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-06 23:33:37 +0000

    Improve code generator scripts to pass additional ScriptExecutionContext argument to the constructor. https://bugs.webkit.org/show_bug.cgi?id=38687
    Reviewed by Adam Barth.
    
    Change both JSC and V8 generators to introduce "CallWith=ScriptExecutionContext"
    attribute to allow passing the additional ScriptExecutionContext argument to
    the constructor. Also add another test IDL file to test interface-level
    features.
    
    These changes are necessary in order to avoid adding custom binding codes
    when we add the FileReader interface.
    
    * bindings/scripts/CodeGeneratorJS.pm:
    * bindings/scripts/CodeGeneratorV8.pm:
    * bindings/scripts/test/GObject/WebKitDOMTestInterface.cpp: Added.
    * bindings/scripts/test/GObject/WebKitDOMTestInterface.h: Added.
    * bindings/scripts/test/GObject/WebKitDOMTestInterfacePrivate.h: Added.
    * bindings/scripts/test/JS/JSTestInterface.cpp: Added.
    * bindings/scripts/test/JS/JSTestInterface.h: Added.
    * bindings/scripts/test/ObjC/DOMTestInterface.h: Added.
    * bindings/scripts/test/ObjC/DOMTestInterface.mm: Added.
    * bindings/scripts/test/ObjC/DOMTestInterfaceInternal.h: Added.
    * bindings/scripts/test/TestInterface.idl: Added.
    * bindings/scripts/test/V8/V8TestInterface.cpp: Added.
    * bindings/scripts/test/V8/V8TestInterface.h: Added.
    * bindings/v8/V8Proxy.h:
    (WebCore::V8Proxy::constructDOMObjectWithScriptExecutionContext):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58919 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8Proxy.h
+++ third_party/WebKit/WebCore/bindings/v8/V8Proxy.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8DOMWindowShell.h"
 #include "V8DOMWrapper.h"
 #include "V8GCController.h"
+#include "V8Utilities.h"
 #include "WrapperTypeInfo.h"
 #include <v8.h>
 #include <wtf/PassRefPtr.h> // so generated bindings don't have to
@@ -297,6 +298,9 @@ namespace WebCore {
         template <typename T>
         static v8::Handle<v8::Value> constructDOMObject(const v8::Arguments&, WrapperTypeInfo*);
 
+        template <typename T>
+        static v8::Handle<v8::Value> constructDOMObjectWithScriptExecutionContext(const v8::Arguments&, WrapperTypeInfo*);
+
         // Process any pending JavaScript console messages.
         static void processConsoleMessages();
 
@@ -396,6 +400,25 @@ namespace WebCore {
         // Note: it's OK to let this RefPtr go out of scope because we also call
         // SetDOMWrapper(), which effectively holds a reference to obj.
         RefPtr<T> obj = T::create();
+        V8DOMWrapper::setDOMWrapper(args.Holder(), type, obj.get());
+        obj->ref();
+        V8DOMWrapper::setJSWrapperForDOMObject(obj.get(), v8::Persistent<v8::Object>::New(args.Holder()));
+        return args.Holder();
+    }
+
+    template <typename T>
+    v8::Handle<v8::Value> V8Proxy::constructDOMObjectWithScriptExecutionContext(const v8::Arguments& args, WrapperTypeInfo* type)
+    {
+        if (!args.IsConstructCall())
+            return throwError(V8Proxy::TypeError, "");
+
+        ScriptExecutionContext* context = getScriptExecutionContext();
+        if (!context)
+            return throwError(V8Proxy::ReferenceError, "");
+
+        // Note: it's OK to let this RefPtr go out of scope because we also call
+        // SetDOMWrapper(), which effectively holds a reference to obj.
+        RefPtr<T> obj = T::create(context);
         V8DOMWrapper::setDOMWrapper(args.Holder(), type, obj.get());
         obj->ref();
         V8DOMWrapper::setJSWrapperForDOMObject(obj.get(), v8::Persistent<v8::Object>::New(args.Holder()));
