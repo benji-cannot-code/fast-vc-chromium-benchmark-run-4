4c7d92c40bf72976ba22ad8c6954ae03c1a72532
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-06 20:01:34 +0000

    2010-05-06  Simon Hausmann  <simon.hausmann@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] REGRESSION: Loading of external CSS and JS files over network fails in some cases
            https://bugs.webkit.org/show_bug.cgi?id=36755
    
            Enable the direct connection between QtNetwork and QtWebKit only for Qt versions
            greater than 4.6.2, due to a bug in Qt that's been fixed after 4.6.2.
    
            * platform/network/qt/QNetworkReplyHandler.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58903 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
+++ third_party/WebKit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
@@ -42,8 +42,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // What type of connection should be used for the signals of the
 // QNetworkReply? This depends on if Qt has a bugfix for this or not.
-// It is fixed in Qt 4.6.1. See https://bugs.webkit.org/show_bug.cgi?id=32113
-#if QT_VERSION > QT_VERSION_CHECK(4, 6, 0)
+// It is fixed in Qt 4.6.3. See https://bugs.webkit.org/show_bug.cgi?id=32113
+// and https://bugs.webkit.org/show_bug.cgi?id=36755
+#if QT_VERSION > QT_VERSION_CHECK(4, 6, 2)
 #define SIGNAL_CONN Qt::DirectConnection
 #else
 #define SIGNAL_CONN Qt::QueuedConnection
