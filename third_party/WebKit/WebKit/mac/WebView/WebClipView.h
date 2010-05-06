benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit a3ef69ec5b4ad05ad633b076baed413b21f78f16

<rdar://problem/7932072> Iframes in composited layers don’t repaint correctly (affects Yahoo! Mail with Flash Player 10.1) https://bugs.webkit.org/show_bug.cgi?id=38427
Reviewed by Simon Fraser.

WebCore: 

Test: compositing/iframes/iframe-in-composited-layer.html

* WebCore.base.exp: Export FrameView::isEnclosedInCompositingLayer().
* page/FrameView.cpp:
(WebCore::FrameView::isEnclosedInCompositingLayer): Added this predicate.
* page/FrameView.h:

WebKit: 

* WebKit.xcodeproj/project.pbxproj: Renamed WebClipView.m to WebClipView.mm and changed it to
Objective-C++.

WebKit/mac: 

* WebView/WebClipView.m: Renamed to WebClipView.mm.
* WebView/WebClipView.mm:
(-[WebClipView visibleRect]): Added this override, which for instances used for WebFrameViews in
composited layers, returns the clip view’s entire bounds. This prevents drawing from being clipped to
AppKit’s idea of what part of the view would be visible if it was drawn as part of the view hierarchy.
Since it is drawn into a compositing layer, that’s irrelevant, and we should not be clipping.
* WebView/WebHTMLView.mm:
(setCursor): Style tweak.
(setNeedsDisplayInRect): Added. Replaces the default implementation of -[NSView setNeedsDisplayInRect:],
so that if the receiver is a descendant of a WebFrameView that draws into a composited layer, the invalidation
is routed back through the WebCore FrameView, which propagates it to the layer.
(+[WebHTMLViewPrivate initialize]): Swizzle the setNeedsDisplayInRect: override in.
(-[WebHTMLView visibleRect]): Removed whitespace.
* WebView/WebView.mm:
(layerSyncRunLoopObserverCallBack): If we bailed out on syncing, due to pending layout, do an eager layout
in preparation for the displaying of compositing layers.

LayoutTests: 

* compositing/iframes/iframe-in-composited-layer.html: Added.
* platform/mac/compositing/iframes/iframe-in-composited-layer-expected.checksum: Added.
* platform/mac/compositing/iframes/iframe-in-composited-layer-expected.png: Added.

third_party/WebKit/WebKit/mac/WebView/WebClipView.mm
@@ -29,10 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "WebClipView.h"
 
-#import <WebKit/WebHTMLView.h>
-#import <WebKit/WebNSViewExtras.h>
-#import <WebKit/WebViewPrivate.h>
-#import <wtf/Assertions.h>
+#import "WebFrameInternal.h"
+#import "WebFrameView.h"
+#import "WebViewPrivate.h"
+#import <WebCore/FrameView.h>
 
 // WebClipView's entire reason for existing is to set the clip used by focus ring redrawing.
 // There's no easy way to prevent the focus ring from drawing outside the passed-in clip rectangle
@@ -41,6 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The "additional clip" is a clip for focus ring redrawing.
 
 // FIXME: Change terminology from "additional clip" to "focus ring clip".
+
+using namespace WebCore;
 
 @interface NSView (WebViewMethod)
 - (WebView *)_webView;
@@ -67,6 +69,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     
     return self;
 }
+
+#if USE(ACCELERATED_COMPOSITING)
+- (NSRect)visibleRect
+{
+    WebFrameView *webFrameView = (WebFrameView *)[[self superview] superview];
+    if (![webFrameView isKindOfClass:[WebFrameView class]])
+        return [super visibleRect];
+
+    if (Frame* coreFrame = core([webFrameView webFrame])) {
+        if (FrameView* frameView = coreFrame->view()) {
+            if (frameView->isEnclosedInCompositingLayer())
+                return [self bounds];
+        }
+    }
+
+    return [super visibleRect];
+}
+#endif
 
 - (void)resetAdditionalClip
 {

third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -5623,6 +5623,11 @@ static void layerSyncRunLoopObserverCallBack(CFRunLoopObserverRef, CFRunLoopActi
 
     if ([webView _syncCompositingChanges])
         [webView _clearLayerSyncLoopObserver];
+    else {
+        // Since the WebView does not need display, -viewWillDraw will not be called. Perform pending layout now,
+        // so that the layers draw with up-to-date layout. 
+        [webView _viewWillDrawInternal];
+    }
 }
 
 - (void)_scheduleCompositingLayerSync

third_party/WebKit/LayoutTests/compositing/iframes/iframe-in-composited-layer.html
@@ -1,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!--
+    This is a test for
+    <i><a href="https://bugs.webkit.org/show_bug.cgi?id=38427">https://bugs.webkit.org/show_bug.cgi?id=38427</a>
+    <rdar://problem/7932072> Iframes in composited layers don&rsquo;t repaint correctly (affects Yahoo! Mail with Flash Player 10.1)</i>.
+<-->
+<div style="-webkit-transform: translateZ(0); width: 400px; height: 400px; background-color: lightyellow;">
+    <iframe id="frame" style="width: 380px; height: 380px; border: solid; margin: 7px" src="data:text/html,
+        <html style='height: 900px; width: 900px; position: relative;'>
+            <body>
+                <div style='position: absolute; width: 100px; height: 100px; background-color: red; left: 0; top: 0;'></div>
+                <div style='position: absolute; width: 100px; height: 100px; background-color: green; right: 0; bottom: 0;'></div>
+            </body>
+        </html>
+    "></iframe>
+</div>
+<script>
+    if (window.layoutTestController)
+        layoutTestController.waitUntilDone();
+
+    setTimeout(function() {
+        document.getElementById("frame").contentWindow.scrollTo(600, 600);
+        if (window.layoutTestController)
+            layoutTestController.notifyDone();
+    }, 200);
+</script>

third_party/WebKit/LayoutTests/platform/mac/compositing/iframes/iframe-in-composited-layer-expected.checksum
@@ -1,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+a8ae6ddad4af0f30f3788e085c3b59d3

third_party/WebKit/LayoutTests/platform/mac/compositing/iframes/iframe-in-composited-layer-expected.txt
@@ -1,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+layer at (8,8) size 400x400
+  RenderBlock {DIV} at (0,0) size 400x400 [bgcolor=#FFFFE0]
+    RenderPartObject {IFRAME} at (7,7) size 386x386 [border: (3px solid #000000)]
+      layer at (0,0) size 900x900
+        RenderView at (0,0) size 365x365
+      layer at (0,0) size 900x900
+        RenderBlock (relative positioned) {HTML} at (0,0) size 900x900
+          RenderBody {BODY} at (8,8) size 884x349
+      layer at (0,0) size 100x100
+        RenderBlock (positioned) {DIV} at (0,0) size 100x100 [bgcolor=#FF0000]
+      layer at (800,800) size 100x100
+        RenderBlock (positioned) {DIV} at (800,800) size 100x100 [bgcolor=#008000]
+    RenderText {#text} at (0,0) size 0x0

third_party/WebKit/WebCore/WebCore.base.exp
@@ -600,6 +600,7 @@ __ZN7WebCore6StringC1EP8NSString
 __ZN7WebCore6StringC1EPK10__CFString
 __ZN7WebCore6Widget12setFrameRectERKNS_7IntRectE
 __ZN7WebCore6Widget16removeFromParentEv
+__ZN7WebCore6Widget17frameRectsChangedEv
 __ZN7WebCore6Widget20retainPlatformWidgetEv
 __ZN7WebCore6Widget21releasePlatformWidgetEv
 __ZN7WebCore6Widget4hideEv
@@ -610,7 +611,6 @@ __ZN7WebCore6Widget9setParentEPNS_10ScrollViewE
 __ZN7WebCore6WidgetC1EP6NSView
 __ZN7WebCore6WidgetC2EP6NSView
 __ZN7WebCore6WidgetD2Ev
-__ZN7WebCore6Widget17frameRectsChangedEv
 __ZN7WebCore7Console21shouldPrintExceptionsEv
 __ZN7WebCore7Console24setShouldPrintExceptionsEb
 __ZN7WebCore7IntRect5uniteERKS0_
@@ -999,6 +999,7 @@ __ZNK7WebCore9FrameTree6parentEb
 __ZNK7WebCore9FrameView11needsLayoutEv
 __ZNK7WebCore9FrameView13paintBehaviorEv
 __ZNK7WebCore9FrameView20isSoftwareRenderableEv
+__ZNK7WebCore9FrameView28isEnclosedInCompositingLayerEv
 __ZNK7WebCore9PageCache10frameCountEv
 __ZNK7WebCore9PageCache21autoreleasedPageCountEv
 __ZTVN7WebCore12ChromeClientE

third_party/WebKit/WebCore/page/FrameView.cpp
@@ -476,6 +476,13 @@ void FrameView::setNeedsOneShotDrawingSynchronization()
     if (page)
         page->chrome()->client()->setNeedsOneShotDrawingSynchronization();
 }
+
+bool FrameView::isEnclosedInCompositingLayer() const
+{
+    RenderObject* frameOwnerRenderer = m_frame->ownerRenderer();
+    return frameOwnerRenderer && frameOwnerRenderer->containerForRepaint();
+}
+
 #endif // USE(ACCELERATED_COMPOSITING)
 
 bool FrameView::syncCompositingStateRecursive()

third_party/WebKit/WebCore/page/FrameView.h
@@ -104,6 +104,8 @@ public:
     // Called when changes to the GraphicsLayer hierarchy have to be synchronized with
     // content rendered via the normal painting path.
     void setNeedsOneShotDrawingSynchronization();
+
+    bool isEnclosedInCompositingLayer() const;
 #endif
     // Only used with accelerated compositing, but outside the #ifdef to make linkage easier.
     // Returns true if the sync was completed.

commit 16734639b998e27e0e86bd55c8be660b60594d02

<rdar://problem/7951285> REGRESSION (r58847): Composited iframe content obscures Safari's application chrome
Reviewed by Simon Fraser.

Fixed this other regression from r58847. The regression was caused by overriding -visibleRect to
return the WebClipView’s full bounds. AppKit uses -visibleRect to determine the geometry
of the surface for the child WebFrameView. The fix is to restrict the special behavior of
-[WebClipView visibleRect] to when AppKit is consulting it for the purpose of invalidating
areas while scrolling.

* WebView/WebClipView.h:
* WebView/WebClipView.mm:
(-[WebClipView visibleRect]): If the WebClipView is not scrolling, always return
[super visibleRect]. 
(-[WebClipView _immediateScrollToPoint:]): Override this internal NSClipView method
to set a flag telling -visibleRect that the view is scrolling.
* WebView/WebView.mm:
(layerSyncRunLoopObserverCallBack): Ensure that screen updates, disabled by AppKit
when it thinks an upcoming window flush will re-enable them, are enabled here in
case the -setNeedsDisplayInRect: override has prevented the window from needing to be
flushed.



git-svn-id: svn://svn.chromium.org/blink/trunk@58913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebKit/mac/WebView/WebClipView.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface WebClipView : NSClipView
 {
     BOOL _haveAdditionalClip;
+    BOOL _isScrolling;
     NSRect _additionalClip;
 }
 

third_party/WebKit/WebKit/mac/WebView/WebClipView.mm
@@ -48,6 +48,10 @@ using namespace WebCore;
 - (WebView *)_webView;
 @end
 
+@interface NSClipView (WebNSClipViewDetails)
+- (void)_immediateScrollToPoint:(NSPoint)newOrigin;
+@end
+
 @implementation WebClipView
 
 - (id)initWithFrame:(NSRect)frame
@@ -73,6 +77,9 @@ using namespace WebCore;
 #if USE(ACCELERATED_COMPOSITING)
 - (NSRect)visibleRect
 {
+    if (!_isScrolling)
+        return [super visibleRect];
+
     WebFrameView *webFrameView = (WebFrameView *)[[self superview] superview];
     if (![webFrameView isKindOfClass:[WebFrameView class]])
         return [super visibleRect];
@@ -85,6 +92,13 @@ using namespace WebCore;
     }
 
     return [super visibleRect];
+}
+
+- (void)_immediateScrollToPoint:(NSPoint)newOrigin
+{
+    _isScrolling = YES;
+    [super _immediateScrollToPoint:newOrigin];
+    _isScrolling = NO;
 }
 #endif
 

third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -190,6 +190,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface NSWindow (WebNSWindowDetails) 
 - (id)_oldFirstResponderBeforeBecoming;
+- (void)_enableScreenUpdatesIfNeeded;
 @end
 
 using namespace WebCore;
@@ -5613,17 +5614,23 @@ static WebFrameView *containingFrameView(NSView *view)
 
 static void layerSyncRunLoopObserverCallBack(CFRunLoopObserverRef, CFRunLoopActivity, void* info)
 {
-    WebView* webView = reinterpret_cast<WebView*>(info);
+    WebView *webView = reinterpret_cast<WebView*>(info);
+    NSWindow *window = [webView window];
 
     // An NSWindow may not display in the next runloop cycle after dirtying due to delayed window display logic,
     // in which case this observer can fire first. So if the window is due for a display, don't commit
     // layer changes, otherwise they'll show on screen before the view drawing.
-    if ([[webView window] viewsNeedDisplay])
+    if ([window viewsNeedDisplay])
         return;
 
-    if ([webView _syncCompositingChanges])
+    if ([webView _syncCompositingChanges]) {
         [webView _clearLayerSyncLoopObserver];
-    else {
+        // AppKit may have disabled screen updates, thinking an upcoming window flush will re-enable them.
+        // In case setNeedsDisplayInRect() has prevented the window from needing to be flushed, re-enable screen
+        // updates here.
+        if (![window isFlushWindowDisabled])
+            [window _enableScreenUpdatesIfNeeded];
+    } else {
         // Since the WebView does not need display, -viewWillDraw will not be called. Perform pending layout now,
         // so that the layers draw with up-to-date layout. 
         [webView _viewWillDrawInternal];
16734639b998e27e0e86bd55c8be660b60594d02
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-06 22:28:26 +0000

    <rdar://problem/7951285> REGRESSION (r58847): Composited iframe content obscures Safari's application chrome
    Reviewed by Simon Fraser.
    
    Fixed this other regression from r58847. The regression was caused by overriding -visibleRect to
    return the WebClipView’s full bounds. AppKit uses -visibleRect to determine the geometry
    of the surface for the child WebFrameView. The fix is to restrict the special behavior of
    -[WebClipView visibleRect] to when AppKit is consulting it for the purpose of invalidating
    areas while scrolling.
    
    * WebView/WebClipView.h:
    * WebView/WebClipView.mm:
    (-[WebClipView visibleRect]): If the WebClipView is not scrolling, always return
    [super visibleRect]. 
    (-[WebClipView _immediateScrollToPoint:]): Override this internal NSClipView method
    to set a flag telling -visibleRect that the view is scrolling.
    * WebView/WebView.mm:
    (layerSyncRunLoopObserverCallBack): Ensure that screen updates, disabled by AppKit
    when it thinks an upcoming window flush will re-enable them, are enabled here in
    case the -setNeedsDisplayInRect: override has prevented the window from needing to be
    flushed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebClipView.h
+++ third_party/WebKit/WebKit/mac/WebView/WebClipView.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface WebClipView : NSClipView
 {
     BOOL _haveAdditionalClip;
+    BOOL _isScrolling;
     NSRect _additionalClip;
 }
 
--- third_party/WebKit/WebKit/mac/WebView/WebClipView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebClipView.mm
@@ -48,6 +48,10 @@ using namespace WebCore;
 - (WebView *)_webView;
 @end
 
+@interface NSClipView (WebNSClipViewDetails)
+- (void)_immediateScrollToPoint:(NSPoint)newOrigin;
+@end
+
 @implementation WebClipView
 
 - (id)initWithFrame:(NSRect)frame
@@ -73,6 +77,9 @@ using namespace WebCore;
 #if USE(ACCELERATED_COMPOSITING)
 - (NSRect)visibleRect
 {
+    if (!_isScrolling)
+        return [super visibleRect];
+
     WebFrameView *webFrameView = (WebFrameView *)[[self superview] superview];
     if (![webFrameView isKindOfClass:[WebFrameView class]])
         return [super visibleRect];
@@ -85,6 +92,13 @@ using namespace WebCore;
     }
 
     return [super visibleRect];
+}
+
+- (void)_immediateScrollToPoint:(NSPoint)newOrigin
+{
+    _isScrolling = YES;
+    [super _immediateScrollToPoint:newOrigin];
+    _isScrolling = NO;
 }
 #endif
 
