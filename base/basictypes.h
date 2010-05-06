a127a9dea5328e2bb377940406c6e42192e938c1
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-06 00:49:47 +0000

    Revert "Remove npapi hack from basictypes.h now that npapi.h is fixed."
    This reverts commit r46528 because it broke the Chrome Frame build.
    
    Chrome Frame: why we can't have nice things.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46533 0039d316-1c4b-4281-b951-d872f2087c98

--- base/basictypes.h
+++ base/basictypes.h
@@ -20,7 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef signed char         schar;
 typedef signed char         int8;
 typedef short               int16;
+// TODO: Remove these type guards.  These are to avoid conflicts with
+// obsolete/protypes.h in the Gecko SDK.
+#ifndef _INT32
+#define _INT32
 typedef int                 int32;
+#endif
 
 // The NSPR system headers define 64-bit as |long| when possible.  In order to
 // not have typedef mismatches, we do the same on LP64.
@@ -38,7 +43,12 @@ typedef long long           int64;
 
 typedef unsigned char      uint8;
 typedef unsigned short     uint16;
+// TODO: Remove these type guards.  These are to avoid conflicts with
+// obsolete/protypes.h in the Gecko SDK.
+#ifndef _UINT32
+#define _UINT32
 typedef unsigned int       uint32;
+#endif
 
 // See the comment above about NSPR and 64-bit.
 #if __LP64__
