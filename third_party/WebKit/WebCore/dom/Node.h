ce36ffa0e7c4b67a2e2f7bcf6fe573829ee26f28
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-06 23:23:48 +0000

    Build fix, not reviewed.
    Speculative Windows and Debug build fixes for the last change.
    
    * dom/Node.h:
    (WebCore::Node::inDocument):
    (WebCore::Node::setFlag):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58918 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Node.h
+++ third_party/WebKit/WebCore/dom/Node.h
@@ -361,7 +361,7 @@ public:
     // node tree, false otherwise.
     bool inDocument() const 
     { 
-        ASSERT(m_document || !m_inDocument);
+        ASSERT(m_document || !getFlag(InDocumentFlag));
         return getFlag(InDocumentFlag);
     }
 
@@ -645,7 +645,7 @@ private:
     // 5 bits remaining
 
     bool getFlag(NodeFlags mask) const { return m_nodeFlags & mask; }
-    void setFlag(bool f, NodeFlags mask) const { m_nodeFlags = (m_nodeFlags & ~mask) | (-f & mask); } 
+    void setFlag(bool f, NodeFlags mask) const { m_nodeFlags = (m_nodeFlags & ~mask) | (-(int32_t)f & mask); } 
     void setFlag(NodeFlags mask) const { m_nodeFlags |= mask; } 
     void clearFlag(NodeFlags mask) const { m_nodeFlags &= ~mask; } 
 
