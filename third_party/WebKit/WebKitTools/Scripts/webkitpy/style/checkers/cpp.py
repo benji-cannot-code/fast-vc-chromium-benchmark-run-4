e94ca2735a5d07e7fc15e65a8bab613aa1934d5c
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-06 01:12:08 +0000

    2010-05-05  James Robinson  <jamesr@chromium.org>
            Reviewed by Adam Barth.
    
            Ban the single letter 'l' as an identifier name
            http://trac.webkit.org/changeset/58844
    
            Add a lint rule to ban the single letter 'l' as an identifier name
            since it is very easy to confuse with the numeral '1', especially
            in code like WebCore/css/CSSHelper.cpp.
    
            See http://trac.webkit.org/changeset/58844 as an example of a bug
            caused by confusing short variable names.
    
            * Scripts/webkitpy/style/checkers/cpp.py:
            * Scripts/webkitpy/style/checkers/cpp_unittest.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58852 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/style/checkers/cpp.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/style/checkers/cpp.py
@@ -2500,6 +2500,10 @@ def check_identifier_name_in_declaration(filename, line_number, line, error):
                 and not modified_identifier == "const_iterator"):
                 error(line_number, 'readability/naming', 4, identifier + " is incorrectly named. Don't use underscores in your identifier names.")
 
+        # Check for variables named 'l', these are too easy to confuse with '1' in some fonts
+        if modified_identifier == 'l':
+            error(line_number, 'readability/naming', 4, identifier + " is incorrectly named. Don't use the single letter 'l' as an identifier name.")
+
         # There can be only one declaration in non-for-control statements.
         if control_statement:
             return
@@ -2511,7 +2515,6 @@ def check_identifier_name_in_declaration(filename, line_number, line, error):
 
         number_of_identifiers += 1
         line = line[matched.end():]
-
 
 def check_c_style_cast(line_number, line, raw_line, cast_type, pattern,
                        error):
