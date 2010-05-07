4331419bbfc3a85e2f47efb9f2c18a35e3303da2
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-07 05:04:36 +0000

    2010-05-06  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dan Bernstein.
    
            Compositing layers that are clipped out by an overflow:scroll parent fail to get created on reveal
            https://bugs.webkit.org/show_bug.cgi?id=38712
    
            When compositing layer creation is testing layer overlap, we need to re-run the
            algorithm when an overflow:scroll element scrolls, to create new layers for revealed
            elements.
    
            Test: compositing/layer-creation/overflow-scroll-overlap.html
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::scrollToOffset): First, move the compositing updates
            until after the call to updateWidgetPositions(), so that this order is similar
            to what we done when a FrameView scrolls. This change has no known side effects.
            Second, if compositingConsultsOverlap() is true, we need to actually do a
            compositing layer update to compute whether revealed/hidden layers should
            be created/destroyed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58936 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayer.cpp
@@ -1269,17 +1269,6 @@ void RenderLayer::scrollToOffset(int x, int y, bool updateScrollbars, bool repai
     for (RenderLayer* child = firstChild(); child; child = child->nextSibling())
         child->updateLayerPositions(0);
 
-#if USE(ACCELERATED_COMPOSITING)
-    if (compositor()->inCompositingMode()) {
-        // Our stacking context is guaranteed to contain all of our descendants that may need
-        // repositioning, so update compositing layers from there.
-        if (RenderLayer* compositingAncestor = stackingContext()->enclosingCompositingLayer()) {
-            bool isUpdateRoot = true;
-            compositingAncestor->backing()->updateAfterLayout(RenderLayerBacking::AllDescendants, isUpdateRoot);
-        }
-    }
-#endif
-    
     RenderView* view = renderer()->view();
     
     // We should have a RenderView if we're trying to scroll.
@@ -1293,6 +1282,21 @@ void RenderLayer::scrollToOffset(int x, int y, bool updateScrollbars, bool repai
 
         view->updateWidgetPositions();
     }
+
+#if USE(ACCELERATED_COMPOSITING)
+    if (compositor()->inCompositingMode()) {
+        // Our stacking context is guaranteed to contain all of our descendants that may need
+        // repositioning, so update compositing layers from there.
+        if (RenderLayer* compositingAncestor = stackingContext()->enclosingCompositingLayer()) {
+            if (compositor()->compositingConsultsOverlap())
+                compositor()->updateCompositingLayers(CompositingUpdateOnScroll, compositingAncestor);
+            else {
+                bool isUpdateRoot = true;
+                compositingAncestor->backing()->updateAfterLayout(RenderLayerBacking::AllDescendants, isUpdateRoot);
+            }
+        }
+    }
+#endif
 
     RenderBoxModelObject* repaintContainer = renderer()->containerForRepaint();
     IntRect rectForRepaint = renderer()->clippedOverflowRectForRepaint(repaintContainer);
