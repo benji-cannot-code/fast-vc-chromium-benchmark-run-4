02c7323c56d621b94b2256bac0f9da5ac5e7bc79
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-07 00:39:58 +0000

    Not reviewed, build fix.
    Speculative fix for Qt and Gtk.
    
    * dom/Notation.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Notation.cpp
+++ third_party/WebKit/WebCore/dom/Notation.cpp
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "Notation.h"
 
+#include Document.h
+
 namespace WebCore {
 
 Notation::Notation(Document* document, const String& name, const String& publicId, const String& systemId)
