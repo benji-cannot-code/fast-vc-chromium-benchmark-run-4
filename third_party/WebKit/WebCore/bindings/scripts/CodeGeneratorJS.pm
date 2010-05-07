b87a84fb5efc83b77f8fccdd21d93efbfeaa713f
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-07 18:24:50 +0000

    2010-05-07  Nikolas Zimmermann  <nzimmermann@rim.com>
            Reviewed by Dirk Schulze.
    
            svg/custom/use-instanceRoot-as-event-target.xhtml crashes randomly
            https://bugs.webkit.org/show_bug.cgi?id=37798
    
            When creating JSEventListeners through attributes (onclick, etc..) or add/removeEventListener
            calls on a SVGElementInstance, do NOT pass the element instance itself as JS wrapper object
            but the correspondingElement(). SVGElementInstance redirects all event listener registrations
            to the correspondingElement(), as they share an event listener list, per SVG spec. The old
            code was very dangerous, leading to random assertions, when garbage collection teared down
            the JSSVGElementInstance and event listeners fired, whose listeners were registered with
            it, but residing in the correspondingElement() event listener list.
    
            Removes the need for CustomToJS code for JSSVGElementInstance, which was wrong anyways. We hoped
            to keep the event listeers alive by just creating a js wrapper of the correspondingElement(), that
            could only work as long as garbage collection didn't tear it down, just luck. Also remove the
            CustomPushEventHandlerScope marker, as it is only used for JSLazyEventListeners and only works
            for JSNode derived objects, it was a no-op for SVGElementInstance, thus removed it.
    
            Should fix all random crashes/assertions seen with svg/custom/use-instanceRoot-as-event-target.xhtml
            See bug report for a detailed crash analysis.
    
            * bindings/js/JSSVGElementInstanceCustom.cpp: Remove custom toJS()/pushEventHandlerScope() handling, not necessary anymore.
            (WebCore::JSSVGElementInstance::markChildren):
            * bindings/scripts/CodeGeneratorJS.pm: For JSSVGElementInstance pass the correspondingElement() as JS wrapper object, not itself.
            * svg/SVGElementInstance.cpp:
            (WebCore::SVGElementInstance::invalidateAllInstancesOfElement): Be sure to trigger a style update here, so dirty shadow trees for <use> get rebuild. Otherwhise DOM may be out-of-sync.
            (WebCore::SVGElementInstance::eventTargetData): Add ASSERT_NOT_REACHED(), all event listener calls are forwarded to the correspondingElement().
            (WebCore::SVGElementInstance::ensureEventTargetData): Ditto.
            * svg/SVGElementInstance.idl: Remove CustomToJS, CustomPushEventHandlerScope markers.
    2010-05-07  Nikolas Zimmermann  <nzimmermann@rim.com>
    
            Reviewed by Dirk Schulze.
    
            svg/custom/use-instanceRoot-as-event-target.xhtml crashes randomly
            https://bugs.webkit.org/show_bug.cgi?id=37798
    
            Make use-instanceRoot-as-event-target.xhtml behave more correctly. Due a copy & paste problem test #7 was not executed. Fixed.
            Make destruction more reliable, by using the standard js-test-post.js injection through <script> before calling notifyDone(), and "TEST COMPLETE" magically appears.
    
            * platform/mac/svg/hixie/error/017-expected.txt: Update result, now that invalidateAllInstancesOfElement(), correctly triggers <use> shadow tree rebuilds.
            * svg/custom/use-instanceRoot-as-event-target-expected.txt: Update result, was missing a "TEST COMPLETE" marker, and Test #7 was not executed before.
            * svg/custom/use-instanceRoot-as-event-target.xhtml: Fix typo, test #7 is now executed, it also had a bug which has been corrected. 
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@58960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (C) 2006 Alexey Proskuryakov <ap@webkit.org>
 # Copyright (C) 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
 # Copyright (C) 2009 Cameron McCormack <cam@mcc.id.au>
+# Copyright (C) Research In Motion Limited 2010. All rights reserved.
 # 
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Library General Public
@@ -127,6 +128,32 @@ sub GenerateInterface
     }
 }
 
+sub GenerateAttributeEventListenerCall
+{
+    my $className = shift;
+    my $implSetterFunctionName = shift;
+    my $windowEventListener = shift;
+
+    my $wrapperObject = $windowEventListener ? "globalObject" : "thisObject";
+    my @GenerateEventListenerImpl = ();
+
+    if ($className eq "JSSVGElementInstance") {
+        # SVGElementInstances have to create JSEventListeners with the wrapper equal to the correspondingElement
+        $wrapperObject = "asObject(correspondingElementWrapper)";
+
+        push(@GenerateEventListenerImpl, <<END);
+    JSValue correspondingElementWrapper = toJS(exec, imp->correspondingElement());
+    if (correspondingElementWrapper.isObject())
+END
+
+        # Add leading whitespace to format the imp->set... line correctly
+        push(@GenerateEventListenerImpl, "    ");
+    }
+
+    push(@GenerateEventListenerImpl, "    imp->set$implSetterFunctionName(createJSAttributeEventListener(exec, value, $wrapperObject));\n");
+    return @GenerateEventListenerImpl;
+}
+
 sub GenerateEventListenerCall
 {
     my $className = shift;
@@ -136,11 +163,23 @@ sub GenerateEventListenerCall
     $implIncludes{"JSEventListener.h"} = 1;
 
     my @GenerateEventListenerImpl = ();
+    my $wrapperObject = "castedThis";
+    if ($className eq "JSSVGElementInstance") {
+        # SVGElementInstances have to create JSEventListeners with the wrapper equal to the correspondingElement
+        $wrapperObject = "asObject(correspondingElementWrapper)";
+
+        push(@GenerateEventListenerImpl, <<END);
+    JSValue correspondingElementWrapper = toJS(exec, imp->correspondingElement());
+    if (!correspondingElementWrapper.isObject())
+        return jsUndefined();
+END
+    }
+
     push(@GenerateEventListenerImpl, <<END);
     JSValue listener = args.at(1);
     if (!listener.isObject())
         return jsUndefined();
-    imp->${functionName}EventListener(ustringToAtomicString(args.at(0).toString(exec)), JSEventListener::create(asObject(listener), castedThis, false, currentWorld(exec))$passRefPtrHandling, args.at(2).toBoolean(exec));
+    imp->${functionName}EventListener(ustringToAtomicString(args.at(0).toString(exec)), JSEventListener::create(asObject(listener), $wrapperObject, false, currentWorld(exec))$passRefPtrHandling, args.at(2).toBoolean(exec));
     return jsUndefined();
 END
     return @GenerateEventListenerImpl;
@@ -1585,11 +1624,7 @@ sub GenerateImplementation
                                 $implIncludes{"JSWorkerContextErrorHandler.h"} = 1;
                                 push(@implContent, "    imp->set$implSetterFunctionName(createJSWorkerContextErrorHandler(exec, value, thisObject));\n");
                             } else {
-                                if ($windowEventListener) {
-                                    push(@implContent, "    imp->set$implSetterFunctionName(createJSAttributeEventListener(exec, value, globalObject));\n");
-                                } else {
-                                    push(@implContent, "    imp->set$implSetterFunctionName(createJSAttributeEventListener(exec, value, thisObject));\n");
-                                }
+                                push(@implContent, GenerateAttributeEventListenerCall($className, $implSetterFunctionName, $windowEventListener));
                             }
                         } elsif ($attribute->signature->type =~ /Constructor$/) {
                             my $constructorType = $attribute->signature->type;
--- third_party/WebKit/WebCore/svg/SVGElementInstance.cpp
+++ third_party/WebKit/WebCore/svg/SVGElementInstance.cpp
@@ -90,7 +90,7 @@ void SVGElementInstance::appendChild(PassRefPtr<SVGElementInstance> child)
 
 void SVGElementInstance::invalidateAllInstancesOfElement(SVGElement* element)
 {
-    if (!element)
+    if (!element || !element->inDocument())
         return;
 
     if (element->isStyled() && static_cast<SVGStyledElement*>(element)->instanceUpdatesBlocked())
@@ -106,6 +106,9 @@ void SVGElementInstance::invalidateAllInstancesOfElement(SVGElement* element)
         ASSERT((*it)->correspondingElement() == element);
         (*it)->correspondingUseElement()->invalidateShadowTree();
     }
+
+    // Be sure to rebuild use trees, if needed
+    element->document()->updateLayoutIgnorePendingStylesheets();
 }
 
 ScriptExecutionContext* SVGElementInstance::scriptExecutionContext() const
@@ -145,15 +148,18 @@ bool SVGElementInstance::dispatchEvent(PassRefPtr<Event> prpEvent)
 
 EventTargetData* SVGElementInstance::eventTargetData()
 {
-    return m_element->eventTargetData();
+    // EventTarget would use these methods if we were actually using its add/removeEventListener logic.
+    // As we're forwarding those calls to the correspondingElement(), no one should ever call this function.
+    ASSERT_NOT_REACHED();
+    return 0;
 }
 
 EventTargetData* SVGElementInstance::ensureEventTargetData()
 {
-    // Avoid crashing - return a default dummy value
-    DEFINE_STATIC_LOCAL(EventTargetData, dummyEventTargetData, ());
-    dummyEventTargetData.eventListenerMap.clear();
-    return &dummyEventTargetData;
+    // EventTarget would use these methods if we were actually using its add/removeEventListener logic.
+    // As we're forwarding those calls to the correspondingElement(), no one should ever call this function.
+    ASSERT_NOT_REACHED();
+    return 0;
 }
 
 }
