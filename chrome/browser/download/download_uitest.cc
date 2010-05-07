b13333f8d02bbec125d5e862025001e79f55dd8f
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-07 11:49:08 +0000

    Add AutomationMsg_WaitForDownloadShelfVisibilityChange and use it in UITestBase for the download tests.
    This should decrease flakiness of download tests.
    
    TEST=DownloadTest.*, especially DownloadTest.[Dont]CloseNewTab*
    BUG=43066
    
    Review URL: http://codereview.chromium.org/1933005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46681 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_uitest.cc
+++ chrome/browser/download/download_uitest.cc
@@ -113,7 +113,7 @@ class DownloadTest : public UITest {
       // TODO(tc): check download status text
 
       // Make sure the download shelf is showing.
-      EXPECT_TRUE(WaitForDownloadShelfVisible(window.get()));
+      EXPECT_TRUE(window->WaitForDownloadShelfVisibilityChange(true));
     }
 
     FilePath filename;
@@ -188,7 +188,7 @@ TEST_F(DownloadTest, FLAKY_DownloadMimeType) {
 
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
-  EXPECT_TRUE(WaitForDownloadShelfVisible(browser.get()));
+  EXPECT_TRUE(browser->WaitForDownloadShelfVisibilityChange(true));
 }
 
 // Access a file with a viewable mime-type, verify that a download
@@ -214,7 +214,7 @@ TEST_F(DownloadTest, FLAKY_NoDownload) {
 
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
-  EXPECT_FALSE(WaitForDownloadShelfVisible(browser.get()));
+  EXPECT_FALSE(browser->WaitForDownloadShelfVisibilityChange(true));
 }
 
 // Download a 0-size file with a content-disposition header, verify that the
@@ -234,7 +234,7 @@ TEST_F(DownloadTest, FLAKY_ContentDisposition) {
   // Ensure the download shelf is visible on the window.
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
-  EXPECT_TRUE(WaitForDownloadShelfVisible(browser.get()));
+  EXPECT_TRUE(browser->WaitForDownloadShelfVisibilityChange(true));
 }
 
 // Test that the download shelf is per-window by starting a download in one
@@ -255,7 +255,7 @@ TEST_F(DownloadTest, FLAKY_PerWindowShelf) {
   // Ensure the download shelf is visible on the window.
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
-  EXPECT_TRUE(WaitForDownloadShelfVisible(browser.get()));
+  EXPECT_TRUE(browser->WaitForDownloadShelfVisibilityChange(true));
 
   // Open a second tab
   ASSERT_TRUE(browser->AppendTab(GURL()));
@@ -263,7 +263,7 @@ TEST_F(DownloadTest, FLAKY_PerWindowShelf) {
 
   // Hide shelf
   EXPECT_TRUE(browser->SetShelfVisible(false));
-  EXPECT_TRUE(WaitForDownloadShelfInvisible(browser.get()));
+  EXPECT_TRUE(browser->WaitForDownloadShelfVisibilityChange(false));
 
   // Go to first tab
   EXPECT_TRUE(browser->ActivateTab(0));
@@ -337,7 +337,7 @@ TEST_F(DownloadTest, FLAKY_IncognitoDownload) {
   ASSERT_TRUE(tab->NavigateToURL(URLRequestMockHTTPJob::GetMockUrl(file)));
 
   // Verify that the download shelf is showing for the Incognito window.
-  EXPECT_TRUE(WaitForDownloadShelfVisible(incognito.get()));
+  EXPECT_TRUE(incognito->WaitForDownloadShelfVisibilityChange(true));
 
   // Close the Incognito window and don't crash.
   ASSERT_TRUE(incognito->RunCommand(IDC_CLOSE_WINDOW));
@@ -386,8 +386,9 @@ TEST_F(DownloadTest, FLAKY_CloseNewTab1) {
   ASSERT_TRUE(tab_proxy->NavigateToURLAsyncWithDisposition(
       URLRequestMockHTTPJob::GetMockUrl(file),
       NEW_BACKGROUND_TAB));
+
   // When the download starts, we should still have one tab.
-  ASSERT_TRUE(WaitForDownloadShelfVisible(browser));
+  ASSERT_TRUE(browser->WaitForDownloadShelfVisibilityChange(true));
   EXPECT_EQ(1, GetTabCount());
 
   CheckDownload(file);
@@ -411,7 +412,7 @@ TEST_F(DownloadTest, FLAKY_DontCloseNewTab2) {
   FilePath file(FILE_PATH_LITERAL("download-test1.lib"));
   ASSERT_TRUE(tab_proxy->NavigateToURLAsync(GURL("javascript:openNew()")));
 
-  ASSERT_TRUE(WaitForDownloadShelfVisible(browser));
+  ASSERT_TRUE(browser->WaitForDownloadShelfVisibilityChange(true));
   EXPECT_EQ(2, GetTabCount());
 
   CheckDownload(file);
@@ -438,7 +439,7 @@ TEST_F(DownloadTest, FLAKY_DontCloseNewTab3) {
   ASSERT_TRUE(tab_proxy->NavigateToURLAsync(
       URLRequestMockHTTPJob::GetMockUrl(file)));
 
-  ASSERT_TRUE(WaitForDownloadShelfVisible(browser));
+  ASSERT_TRUE(browser->WaitForDownloadShelfVisibilityChange(true));
   EXPECT_EQ(2, GetTabCount());
 
   CheckDownload(file);
@@ -462,7 +463,7 @@ TEST_F(DownloadTest, FLAKY_CloseNewTab2) {
   FilePath file(FILE_PATH_LITERAL("download-test1.lib"));
   ASSERT_TRUE(tab_proxy->NavigateToURLAsync(GURL("javascript:openNew()")));
 
-  ASSERT_TRUE(WaitForDownloadShelfVisible(browser));
+  ASSERT_TRUE(browser->WaitForDownloadShelfVisibilityChange(true));
   EXPECT_EQ(1, GetTabCount());
 
   CheckDownload(file);
@@ -487,7 +488,7 @@ TEST_F(DownloadTest, FLAKY_CloseNewTab3) {
   ASSERT_TRUE(tab_proxy->NavigateToURLAsync(
       GURL("javascript:document.getElementById('form').submit()")));
 
-  ASSERT_TRUE(WaitForDownloadShelfVisible(browser));
+  ASSERT_TRUE(browser->WaitForDownloadShelfVisibilityChange(true));
   EXPECT_EQ(1, GetTabCount());
 
   CheckDownload(file);
