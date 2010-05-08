918ab497ffecd5be15f10d479589faff59c477c0
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-08 03:02:51 +0000

    Disable extension app tab throbber if command line flag set.
    This is a temporary measure until we have a better throbber for
    extension app tabs (or we remove them).
    
    BUG=None
    TEST=Pass in --disable-throbber-for-extension-apps and make
    sure the icon for nano tabs don't get replaced by a throbber.
    
    Review URL: http://codereview.chromium.org/2032003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46778 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -71,6 +71,7 @@ extern const char kDisableSyncBookmarks[];
 extern const char kDisableSyncPreferences[];
 extern const char kDisableSyncThemes[];
 extern const char kDisableSyncTypedUrls[];
+extern const char kDisableThrobberForExtensionApps[];
 extern const char kDisableWebResources[];
 extern const char kDisableWebSecurity[];
 extern const char kDisableWebSockets[];
