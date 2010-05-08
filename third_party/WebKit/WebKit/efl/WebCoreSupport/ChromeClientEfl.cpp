968ff2c6bf0ea69b61667455ddf872262d1d311e
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-08 18:40:06 +0000

    2010-05-08  Leandro Pereira  <leandro@profusion.mobi>
            Reviewed by Laszlo Gombos.
    
            [EFL] Only compile database stuff if ENABLE_DATABASE is set.
            http://webkit.org/b/38777
    
            * efl/WebCoreSupport/ChromeClientEfl.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59032 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/efl/WebCoreSupport/ChromeClientEfl.cpp
+++ third_party/WebKit/WebKit/efl/WebCoreSupport/ChromeClientEfl.cpp
@@ -34,7 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "ChromeClientEfl.h"
 
+#if ENABLE(DATABASE)
 #include "DatabaseTracker.h"
+#endif
 #include "EWebKit.h"
 #include "FloatRect.h"
 #include "FrameLoader.h"
@@ -341,6 +343,7 @@ void ChromeClientEfl::reachedMaxAppCacheSize(int64_t spaceNeeded)
 }
 #endif
 
+#if ENABLE(DATABASE)
 void ChromeClientEfl::exceededDatabaseQuota(Frame* frame, const String& databaseName)
 {
     uint64_t quota = ewk_settings_web_database_default_quota_get();
@@ -350,6 +353,7 @@ void ChromeClientEfl::exceededDatabaseQuota(Frame* frame, const String& database
 
     ewk_view_exceeded_database_quota(m_view, kit(frame), databaseName.utf8().data());
 }
+#endif
 
 void ChromeClientEfl::runOpenPanel(Frame* frame, PassRefPtr<FileChooser> prpFileChooser)
 {
