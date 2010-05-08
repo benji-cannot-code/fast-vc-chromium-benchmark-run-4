e6cb847b219ad9ab2ba66ae47eef45e42e66f190
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-08 08:35:09 +0000

    gtk: Delay load the AutoFill profiles and credit cards.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2041005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46783 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/options/content_page_gtk.h
+++ chrome/browser/gtk/options/content_page_gtk.h
@@ -16,8 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service.h"
 
 class ContentPageGtk : public OptionsPageBase,
-                       public ProfileSyncServiceObserver,
-                       public PersonalDataManager::Observer {
+                       public ProfileSyncServiceObserver {
  public:
   explicit ContentPageGtk(Profile* profile);
   ~ContentPageGtk();
@@ -40,9 +39,6 @@ class ContentPageGtk : public OptionsPageBase,
   virtual void Observe(NotificationType type,
                        const NotificationSource& source,
                        const NotificationDetails& details);
-
-  // Overriden from PersonalDataManager::Observer.
-  virtual void OnPersonalDataLoaded();
 
   // Update content area after a theme changed.
   void ObserveThemeChanged();
@@ -112,10 +108,6 @@ class ContentPageGtk : public OptionsPageBase,
   // Cached pointer to ProfileSyncService, if it exists. Kept up to date
   // and NULL-ed out on destruction.
   ProfileSyncService* sync_service_;
-
-  // The personal data manager, used to save and load personal data to/from the
-  // web database. This can be NULL.
-  PersonalDataManager* personal_data_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentPageGtk);
 };
