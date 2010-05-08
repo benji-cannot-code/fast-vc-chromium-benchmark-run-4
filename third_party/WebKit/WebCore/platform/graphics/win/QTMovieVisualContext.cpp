3c8430743be962173e08be42bb7c5cef2b57973b
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-08 06:56:38 +0000

    Fix the windows build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@59003 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/QTMovieVisualContext.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/QTMovieVisualContext.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <CVHostTime.h>
 #include <ImageCompression.h>
 #include <Movies.h>
-#include <d3d9types.h>
 #include <windows.h>
 
 struct QTCVTimeStamp {
