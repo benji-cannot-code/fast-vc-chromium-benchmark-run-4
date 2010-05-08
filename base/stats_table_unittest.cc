94d54c1c3df9b77aa123d924f848ac237884d8ef
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-08 09:30:28 +0000

    Disable StatsTableTest.MultipleThreads instead of using #if 0 (so that the code doesn't rot).
    This is http://codereview.chromium.org/1990007 done right,
    this time should not break tsan.
    
    TBR=maruel
    
    TEST=none
    BUG=10611
    
    Review URL: http://codereview.chromium.org/2022003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46784 0039d316-1c4b-4281-b951-d872f2087c98

--- base/stats_table_unittest.cc
+++ base/stats_table_unittest.cc
@@ -109,10 +109,8 @@ void StatsTableThread::Run() {
 }
 
 // Create a few threads and have them poke on their counters.
-// Currently disabled. See bug report below:
-// TODO(maruel): http://crbug.com/10611
-TEST_F(StatsTableTest, MultipleThreads) {
-#if 0
+// Disabled, http://crbug.com/10611.
+TEST_F(StatsTableTest, DISABLED_MultipleThreads) {
   // Create a stats table.
   const std::string kTableName = "MultipleThreadStatTable";
   const int kMaxThreads = 20;
@@ -165,7 +163,6 @@ TEST_F(StatsTableTest, MultipleThreads) {
   EXPECT_EQ(0, table.CountThreadsRegistered());
 
   DeleteShmem(kTableName);
-#endif
 }
 
 const std::string kMPTableName = "MultipleProcessStatTable";
