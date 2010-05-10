699d64ab393e92a12a7f4e7246de73a3d4ffbc23
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-10 15:59:19 +0000

    2010-05-10  Dirk Schulze  <krit@webkit.org>
            Reviewed by Nikolas Zimmermann.
    
            Further optimization for SVG's repaintRect calculation
            https://bugs.webkit.org/show_bug.cgi?id=38820
            
            This is a further optimization to get smaller repaintRects on SVG objects
            in combination with masker or clipper resources.
            Masker takes the unite of all stroke boundaries of it's childs.
            Both, clipper and masker, store this union to avoid multiple calls of the childs
            and the unite calculations now.
            The unite rect can be transformed to any targets objectBoundingBox, if the content
            unit of the resource is set to objectBoundingBoxMode.
            This speeds up the use of resources with multiple target objects.
            
            No behavior changes, the smaller repaintRects cause updates for DRT results.
    
            * rendering/RenderSVGResource.h:
            * rendering/RenderSVGResourceClipper.cpp:
            (WebCore::RenderSVGResourceClipper::invalidateClients):
            (WebCore::RenderSVGResourceClipper::calculateClipContentRepaintRect):
            (WebCore::RenderSVGResourceClipper::resourceBoundingBox):
            * rendering/RenderSVGResourceClipper.h:
            * rendering/RenderSVGResourceFilter.cpp:
            (WebCore::RenderSVGResourceFilter::resourceBoundingBox):
            * rendering/RenderSVGResourceFilter.h:
            * rendering/RenderSVGResourceGradient.h:
            (WebCore::RenderSVGResourceGradient::resourceBoundingBox):
            * rendering/RenderSVGResourceMarker.h:
            (WebCore::RenderSVGResourceMarker::resourceBoundingBox):
            * rendering/RenderSVGResourceMasker.cpp:
            (WebCore::RenderSVGResourceMasker::invalidateClients):
            (WebCore::RenderSVGResourceMasker::createMaskImage):
            (WebCore::RenderSVGResourceMasker::calculateMaskContentRepaintRect):
            (WebCore::RenderSVGResourceMasker::resourceBoundingBox):
            * rendering/RenderSVGResourceMasker.h:
            * rendering/RenderSVGResourcePattern.h:
            (WebCore::RenderSVGResourcePattern::resourceBoundingBox):
            * rendering/RenderSVGResourceSolidColor.h:
            (WebCore::RenderSVGResourceSolidColor::resourceBoundingBox):
    2010-05-10  Dirk Schulze  <krit@webkit.org>
    
            Reviewed by Nikolas Zimmermann.
    
            Further optimization for SVG's repaintRect calculation
            https://bugs.webkit.org/show_bug.cgi?id=38820
            
            The repaintRect of SVG objects with masks got smaller. This made
            updates of some results necessary.
    
            * platform/mac/svg/W3C-SVG-1.1/masking-intro-01-f-expected.txt:
            * platform/mac/svg/batik/masking/maskRegions-expected.txt:
            * platform/mac/svg/clip-path/clip-path-with-invisibile-child-expected.txt:
            * platform/mac/svg/css/circle-in-mask-with-shadow-expected.txt:
            * platform/mac/svg/css/mask-with-shadow-expected.txt:
            * platform/mac/svg/custom/empty-mask-expected.txt:
            * platform/mac/svg/custom/mask-excessive-malloc-expected.txt:
            * platform/mac/svg/custom/mask-inside-defs-expected.txt:
            * platform/mac/svg/custom/mask-invalidation-expected.txt:
            * platform/mac/svg/custom/mask-with-all-units-expected.txt:
            * platform/mac/svg/custom/resource-invalidate-on-target-update-expected.txt:
            * platform/mac/svg/custom/visibility-override-mask-expected.txt:
            * svg/custom/transformedMaskFails-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59081 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderSVGResourceClipper.h
+++ third_party/WebKit/WebCore/rendering/RenderSVGResourceClipper.h
@@ -52,7 +52,7 @@ public:
     virtual void invalidateClient(RenderObject*);
 
     virtual bool applyResource(RenderObject*, RenderStyle*, GraphicsContext*&, unsigned short resourceMode);
-    virtual FloatRect resourceBoundingBox(const FloatRect&) const;
+    virtual FloatRect resourceBoundingBox(const FloatRect&);
 
     virtual RenderSVGResourceType resourceType() const { return ClipperResourceType; }
 
@@ -65,7 +65,9 @@ private:
     bool applyClippingToContext(RenderObject*, const FloatRect&, const FloatRect&, GraphicsContext*);
     bool pathOnlyClipping(GraphicsContext*, const FloatRect&);
     bool createClipData(ClipperData*, const FloatRect&, const FloatRect&);
+    void calculateClipContentRepaintRect();
 
+    FloatRect m_clipBoundaries;
     HashMap<RenderObject*, ClipperData*> m_clipper;
 };
 
