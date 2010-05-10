768772c49a6731b3a2cdc01456abcc8a267c69b4
johnnyg@google.com <johnnyg@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-10 19:36:11 +0000

    2010-05-10  John Gregg  <johnnyg@google.com>
            [chromium] Try to fix the chromium build after r59085 by
            including MathExtras.h before using isnan().
            https://bugs.webkit.org/show_bug.cgi?id=38860
    
            * html/canvas/WebGLIntegralTypedArrayBase.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59089 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/WebGLIntegralTypedArrayBase.h
+++ third_party/WebKit/WebCore/html/canvas/WebGLIntegralTypedArrayBase.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WebGLTypedArrayBase.h"
 #include <limits>
+#include <wtf/MathExtras.h>
 
 // Base class for all WebGL<T>Array types holding integral
 // (non-floating-point) values.
