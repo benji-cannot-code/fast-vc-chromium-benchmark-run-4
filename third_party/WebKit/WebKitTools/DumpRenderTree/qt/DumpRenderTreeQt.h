4df30b1f15f1628164a0194d01847bd5d0f98ef6
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-10 14:03:26 +0000

    [Qt] Roll-out r59020 and r59021, because the Qt part of these changes haven't been landed in Qt trunk yet. Should be rolled-in again after the merge.
    Rubber-stamped by Simon Hausmann.
    
    https://bugs.webkit.org/show_bug.cgi?id=32967
    
    WebCore: 
    
    * platform/network/qt/ResourceRequestQt.cpp:
    (WebCore::ResourceRequest::toNetworkRequest):
    
    WebKitTools: 
    
    * DumpRenderTree/qt/DumpRenderTreeQt.cpp:
    (WebCore::WebPage::WebPage):
    (WebCore::DumpRenderTree::DumpRenderTree):
    * DumpRenderTree/qt/DumpRenderTreeQt.h:
    
    LayoutTests: 
    
    * platform/qt/Skipped:
    * platform/qt/http/tests/xmlhttprequest/cross-origin-no-authorization-expected.txt: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59076 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/qt/DumpRenderTreeQt.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/qt/DumpRenderTreeQt.h
@@ -62,7 +62,6 @@ class GCController;
 namespace WebCore {
 
 class WebPage;
-class NetworkAccessManager;
 
 class DumpRenderTree : public QObject {
 Q_OBJECT
@@ -88,7 +87,6 @@ public:
     LayoutTestController *layoutTestController() const { return m_controller; }
     EventSender *eventSender() const { return m_eventSender; }
     TextInputController *textInputController() const { return m_textInputController; }
-    NetworkAccessManager *networkAccessManager() const { return m_networkAccessManager; }
 
     QWebPage *createWindow();
     int windowCount() const;
@@ -96,6 +94,7 @@ public:
     void switchFocus(bool focused);
 
     WebPage *webPage() const { return m_page; }
+
 
 #if defined(Q_WS_X11)
     static void initializeFonts();
@@ -137,7 +136,6 @@ private:
     EventSender *m_eventSender;
     TextInputController *m_textInputController;
     GCController* m_gcController;
-    NetworkAccessManager* m_networkAccessManager;
 
     QFile *m_stdin;
 
