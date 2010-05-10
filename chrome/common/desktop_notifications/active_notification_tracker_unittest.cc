50cde0a0dd63bf26c003c76a1c3e8a60130e9eb9
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-10 18:14:01 +0000

    TTF: Re-enable ActiveNotificationTrackerTest.TestLookupAndClear.  The test was disabled because it failed under valgrind.  Now the test is specifically disabled for Valgrind only.
    TBR=johnnyg
    BUG=43764
    TEST=none
    
    Review URL: http://codereview.chromium.org/2036008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46831 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/desktop_notifications/active_notification_tracker_unittest.cc
+++ chrome/common/desktop_notifications/active_notification_tracker_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/desktop_notifications/active_notification_tracker.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-TEST(ActiveNotificationTrackerTest, DISABLED_TestLookupAndClear) {
+TEST(ActiveNotificationTrackerTest, TestLookupAndClear) {
   ActiveNotificationTracker tracker;
 
   WebKit::WebNotification notification1;
