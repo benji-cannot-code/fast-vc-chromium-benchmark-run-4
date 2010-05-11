84a7c1fa17b484c973657c18051c824cb8cad2ea
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-11 22:39:20 +0000

    2010-05-11  David Holloway  <dhollowa@chromium.org>
            Reviewed by Darin Fisher.
    
            [chromium] Add size method to WebInputElement class, providing same data
            as HTMLInputElement::size().  During form AutoFill Chromium needs to
            access the input element's size attribute in aid of form matching and
            filling heuristics.
    
            https://bugs.webkit.org/show_bug.cgi?id=38825
    
            * public/WebInputElement.h:
            * src/WebInputElement.cpp:
            (WebKit::WebInputElement::size):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59176 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebInputElement.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebInputElement.cpp
@@ -71,6 +71,11 @@ void WebInputElement::setActivatedSubmit(bool activated)
     unwrap<HTMLInputElement>()->setActivatedSubmit(activated);
 }
 
+int WebInputElement::size() const
+{
+    return constUnwrap<HTMLInputElement>()->size();
+}
+
 void WebInputElement::setValue(const WebString& value)
 {
     unwrap<HTMLInputElement>()->setValue(value);
