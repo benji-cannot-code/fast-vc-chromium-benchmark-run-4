b9669589460dd145bb30c84de115542f99efe3c9
dumi@chromium.org <dumi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-11 05:37:42 +0000

    Adding a test that makes sure openDatabaseSync() deals correctly with invalid inputs. https://bugs.webkit.org/show_bug.cgi?id=34994
    Reviewed by Brady Eidson.
    
    * fast/workers/storage: Added.
    * fast/workers/storage/open-database-sync-inputs-expected.txt: Added.
    * fast/workers/storage/open-database-sync-inputs.html: Added.
    * fast/workers/storage/resources: Added.
    * fast/workers/storage/resources/open-database-sync-inputs.js: Added.
    (notAString.toString):
    * fast/workers/storage/resources/test-inputs-common.js: Added.
    (log):
    (finishTest):
    (runTest.worker.onmessage):
    (runTest):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59135 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/workers/storage/resources/test-inputs-common.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function log(message)
+{
+    document.getElementById("console").innerHTML += message + "<br>";
+}
+
+function finishTest()
+{
+    if (window.layoutTestController)
+        layoutTestController.notifyDone();
+}
+
+function runTest(workerJSFile)
+{
+    if (!workerJSFile) {
+        log("FAIL: no JS file specified for the worker.");
+    }
+
+    if (window.layoutTestController) {
+        layoutTestController.dumpAsText();
+        layoutTestController.waitUntilDone();
+    }
+
+    var worker = new Worker(workerJSFile);
+    worker.onmessage = function(event) {
+        if (event.data == "done")
+            finishTest();
+        else
+            log(event.data);
+    }
+}
