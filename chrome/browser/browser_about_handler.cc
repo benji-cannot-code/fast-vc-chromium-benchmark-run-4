72ea4d60e05afa8dbae99e8b98f02b8f4d33c02e
chocobo@chromium.org <chocobo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-11 22:52:30 +0000

    Added an about:network page for ChromeOS. Also removed some unnecessary repeated comments. BUG=none TEST=Try loading about:network Review URL: http://codereview.chromium.org/1985015
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46972 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -58,6 +58,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/about_ipc_dialog.h"
 #include "chrome/browser/views/about_network_dialog.h"
 #elif defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/cros/cros_library.h"
+#include "chrome/browser/chromeos/cros/network_library.h"
 #include "chrome/browser/chromeos/version_loader.h"
 #elif defined(OS_MACOSX)
 #include "chrome/browser/cocoa/about_ipc_dialog.h"
@@ -99,6 +101,7 @@ const char kLinuxProxyConfigPath[] = "linux-proxy-config";
 #endif
 
 #if defined(OS_CHROMEOS)
+const char kNetworkPath[] = "network";
 const char kOSCreditsPath[] = "os-credits";
 #endif
 
@@ -215,6 +218,13 @@ std::string AboutOSCredits() {
           IDR_OS_CREDITS_HTML);
 
   return os_credits_html;
+}
+
+std::string AboutNetwork(const std::string& query) {
+  int refresh;
+  StringToInt(query, &refresh);
+  return chromeos::CrosLibrary::Get()->GetNetworkLibrary()->
+      GetHtmlInfo(refresh);
 }
 #endif
 
@@ -703,6 +713,8 @@ void AboutSource::StartDataRequest(const std::string& path_raw,
 #if defined(OS_CHROMEOS)
   } else if (path == kOSCreditsPath) {
     response = AboutOSCredits();
+  } else if (path == kNetworkPath) {
+    response = AboutNetwork(info);
 #endif
   } else if (path == kTermsPath) {
     response = AboutTerms();
