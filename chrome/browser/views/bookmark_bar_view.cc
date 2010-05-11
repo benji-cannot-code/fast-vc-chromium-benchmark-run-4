068a483590faae69b2ddda68194f94533ecc6d8f
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-11 23:24:10 +0000

    For extension apps, disable the bookmark bar from the launcher page.
    BUG=None
    TEST=With --enable-extension-apps, make sure you don't get the bookmark bar.
    
    Review URL: http://codereview.chromium.org/1990015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46980 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/bookmark_bar_view.cc
+++ chrome/browser/views/bookmark_bar_view.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/event_utils.h"
 #include "chrome/browser/views/frame/browser_view.h"
 #include "chrome/browser/views/location_bar/location_bar_view.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/page_transition_types.h"
 #include "chrome/common/pref_names.h"
@@ -461,7 +462,15 @@ void BookmarkBarView::SetPageNavigator(PageNavigator* navigator) {
 }
 
 gfx::Size BookmarkBarView::GetPreferredSize() {
-  return LayoutItems(true);
+  // We don't want the bookmark bar view in the app launcher new tab page.
+  static bool extension_apps = CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableExtensionApps);
+  bool show_bookmark_bar = !(extension_apps && OnNewTabPage());
+
+  if (show_bookmark_bar)
+    return LayoutItems(true);
+  else
+    return gfx::Size();
 }
 
 gfx::Size BookmarkBarView::GetMinimumSize() {
