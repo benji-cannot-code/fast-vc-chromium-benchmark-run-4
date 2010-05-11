cda1c544d2f9418222bfa78ce08ecf786eabf4a1
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-11 20:04:01 +0000

    Remove an extra include.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1989010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46955 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/typed_url_data_type_controller.cc
+++ chrome/browser/sync/glue/typed_url_data_type_controller.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/glue/typed_url_model_associator.h"
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/profile_sync_factory.h"
-#include "chrome/browser/webdata/web_data_service.h"
 #include "chrome/common/notification_service.h"
 
 namespace browser_sync {
