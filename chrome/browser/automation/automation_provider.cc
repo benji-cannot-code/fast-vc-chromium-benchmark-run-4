0e351bb3e099b9eaeadcce6ecc92cc5935a1d975
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-11 12:42:51 +0000

    Reland r46681: Use IPC to wait for download in DownloadTest.
    Add AutomationMsg_WaitForDownloadShelfVisibilityChange and use it in UITestBase for the download tests.
    
    This should decrease flakiness of download tests.
    
    TEST=DownloadTest.*, especially DownloadTest.[Dont]CloseNewTab*
    BUG=43066
    
    Review URL: http://codereview.chromium.org/2051002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46909 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.cc
+++ chrome/browser/automation/automation_provider.cc
@@ -214,11 +214,6 @@ NotificationObserver* AutomationProvider::AddNavigationStatusListener(
   return observer;
 }
 
-void AutomationProvider::RemoveNavigationStatusListener(
-    NotificationObserver* obs) {
-  notification_observer_list_.RemoveObserver(obs);
-}
-
 NotificationObserver* AutomationProvider::AddTabStripObserver(
     Browser* parent,
     IPC::Message* reply_message) {
@@ -229,7 +224,7 @@ NotificationObserver* AutomationProvider::AddTabStripObserver(
   return observer;
 }
 
-void AutomationProvider::RemoveTabStripObserver(NotificationObserver* obs) {
+void AutomationProvider::RemoveObserver(NotificationObserver* obs) {
   notification_observer_list_.RemoveObserver(obs);
 }
 
@@ -497,6 +492,9 @@ void AutomationProvider::OnMessageReceived(const IPC::Message& message) {
     IPC_MESSAGE_HANDLER(AutomationMsg_SetEnableExtensionAutomation,
                         SetEnableExtensionAutomation)
 #endif
+    IPC_MESSAGE_HANDLER_DELAY_REPLY(
+        AutomationMsg_WaitForDownloadShelfVisibilityChange,
+        WaitForDownloadShelfVisibilityChange)
     IPC_MESSAGE_HANDLER(AutomationMsg_SetShelfVisibility, SetShelfVisibility)
     IPC_MESSAGE_HANDLER(AutomationMsg_BlockedPopupCount, GetBlockedPopupCount)
     IPC_MESSAGE_HANDLER(AutomationMsg_SelectAll, SelectAll)
@@ -600,7 +598,7 @@ void AutomationProvider::AppendTab(int handle, const GURL& url,
   if (append_tab_response < 0) {
     // The append tab failed. Remove the TabStripObserver
     if (observer) {
-      RemoveTabStripObserver(observer);
+      RemoveObserver(observer);
       delete observer;
     }
 
@@ -1590,6 +1588,29 @@ void AutomationProvider::RemoveBookmark(int handle,
     }
   }
   *success = false;
+}
+
+void AutomationProvider::WaitForDownloadShelfVisibilityChange(
+    int browser_handle,
+    bool visibility,
+    IPC::Message* reply_message) {
+  bool success = false;
+  if (browser_tracker_->ContainsHandle(browser_handle)) {
+    Browser* browser = browser_tracker_->GetResource(browser_handle);
+    if (browser->window()->IsDownloadShelfVisible() == visibility) {
+      success = true;
+    } else {
+      notification_observer_list_.AddObserver(
+          new DownloadShelfVisibilityObserver(this,
+                                              browser,
+                                              visibility,
+                                              reply_message));
+      return;
+    }
+  }
+  AutomationMsg_WaitForDownloadShelfVisibilityChange::WriteReplyParams(
+      reply_message, success);
+  Send(reply_message);
 }
 
 // Sample json input: { "command": "GetHistoryInfo",
