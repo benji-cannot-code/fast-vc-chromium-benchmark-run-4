0024c648a15218fd7a600939de88ebd141a05158
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-11 05:08:18 +0000

    2010-05-10  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Anders Carlsson
    
            Allow compositing layers to be connected across iframe boundaries on Mac
            https://bugs.webkit.org/show_bug.cgi?id=38856
    
            Clean up some methods related to composited iframes. No behavioral changes.
    
            * rendering/RenderLayerBacking.h: Move innerRenderLayerCompositor() to be a static method:
                RenderLayerCompositor::iframeContentsCompositor().
            * rendering/RenderLayerBacking.cpp: Remove innerRenderLayerCompositor().
            (WebCore::RenderLayerBacking::updateGraphicsLayerGeometry): Call iframeContentsCompositor().
    
            * rendering/RenderLayerCompositor.cpp:
            (WebCore::RenderLayerCompositor::enableCompositingMode): Call enclosingIFrameElement()
            (WebCore::RenderLayerCompositor::rebuildCompositingLayerTree): iframeContentsCompositor() is now a static method in this class.
            Also check that the iframe contents are in compositing mode (slightly clearer than relying on rootPlatformLayer() returning
            null).
            (WebCore::RenderLayerCompositor::iframeContentsCompositor): Moved from RenderLayerBacking.
            (WebCore::RenderLayerCompositor::didMoveOnscreen): Use enclosingIFrameElement() method.
            (WebCore::RenderLayerCompositor::willMoveOffscreen): Ditto.
            (WebCore::RenderLayerCompositor::enclosingIFrameElement): New method.
            (WebCore::RenderLayerCompositor::ensureRootPlatformLayer): Call enclosingIFrameElement().
            * rendering/RenderLayerCompositor.h: New method, iframeContentsCompositor().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59129 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayerBacking.h
+++ third_party/WebKit/WebCore/rendering/RenderLayerBacking.h
@@ -125,8 +125,6 @@ public:
 
     IntRect contentsBox() const;
     
-    RenderLayerCompositor* innerRenderLayerCompositor() const;
-
 private:
     void createGraphicsLayer();
     void destroyGraphicsLayer();
--- third_party/WebKit/WebCore/rendering/RenderLayerCompositor.h
+++ third_party/WebKit/WebCore/rendering/RenderLayerCompositor.h
@@ -35,6 +35,7 @@ namespace WebCore {
 #define PROFILE_LAYER_REBUILD 0
 
 class GraphicsLayer;
+class RenderIFrame;
 #if ENABLE(VIDEO)
 class RenderVideo;
 #endif
@@ -141,6 +142,9 @@ public:
     // Some platforms may wish to connect compositing layer trees between iframes and
     // their parent document.
     static bool shouldPropagateCompositingToIFrameParent();
+
+    Element* enclosingIFrameElement() const;
+    static RenderLayerCompositor* iframeContentsCompositor(RenderIFrame*);
 
     void setRootPlatformLayerClippingBox(const IntRect& contentsBox);
 
