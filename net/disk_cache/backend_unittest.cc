b7c97d4c09d8847c2d6105f01dcfadfdd72f67a6
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-11 17:18:22 +0000

    Disk cache: Allow the disk cache histograms to generate more than one name so that we can move a user from one group to another within the same session.
    BUG=none
    TEST=unittest
    
    
    Review URL: http://codereview.chromium.org/2044007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46931 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/disk_cache/backend_impl.h"
 #include "net/disk_cache/disk_cache_test_base.h"
 #include "net/disk_cache/disk_cache_test_util.h"
+#include "net/disk_cache/histogram_macros.h"
 #include "net/disk_cache/mapped_file.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -1612,4 +1613,16 @@ TEST_F(DiskCacheTest, AutomaticMaxSize) {
             disk_cache::PreferedCacheSize(largest_size * 100));
   EXPECT_EQ(kint32max,
             disk_cache::PreferedCacheSize(largest_size * 10000));
+}
+
+// Tests that we can "migrate" a running instance from one experiment group to
+// another.
+TEST_F(DiskCacheBackendTest, Histograms) {
+  SetDirectMode();
+  InitCache();
+  disk_cache::BackendImpl* backend_ = cache_impl_;  // Needed be the macro.
+
+  for (int i = 1; i < 3; i++) {
+    CACHE_UMA(HOURS, "FillupTime", i, 28);
+  }
 }
