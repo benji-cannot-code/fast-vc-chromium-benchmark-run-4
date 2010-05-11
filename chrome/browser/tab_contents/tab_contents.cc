3a7135a40a65cf787919cfcbb0213613eee2026d
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-11 14:31:09 +0000

    Reimplement accessibility of web content by caching the entire accessibility tree in the browser process.
    Adds new RPCs for a browser tab to request accessibility info from
    a renderer; the renderer responds with a complete tree of
    accessibility metadata for the entire DOM, which is then cached
    in the RenderWidgetHostView. This part is cross-platform and will
    help with accessibility on both Windows and Mac OS X.
    
    For Windows, MSAA support for web content has been rewritten to
    use this new cache. Tested in JAWS and NVDA screen readers.
    Using Chrome with a screen reader is now fast and stable,
    unlike the previous implementation. However, note that most
    advanced functionality is still not supported, and much work remains
    to make Chrome work well with a screen reader. This is a necessary
    step to improve stability first.
    
    BUG=25564
    BUG=13291
    TEST=See http://codereview.chromium.org/1806001
    
    Review URL: http://codereview.chromium.org/1637018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46916 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -292,7 +292,8 @@ TabContents::TabContents(Profile* profile,
       renderer_preferences_(),
       opener_dom_ui_type_(DOMUIFactory::kNoDOMUI),
       language_state_(&controller_),
-      geolocation_settings_state_(profile) {
+      geolocation_settings_state_(profile),
+      requested_accessibility_tree_(false) {
   ClearBlockedContentSettings();
   renderer_preferences_util::UpdateFromSystemSettings(
       &renderer_preferences_, profile);
@@ -350,6 +351,10 @@ TabContents::TabContents(Profile* profile,
   // Set-up the showing of the omnibox search infobar if applicable.
   if (OmniboxSearchHint::IsEnabled(profile))
     omnibox_search_hint_.reset(new OmniboxSearchHint(this));
+
+  renderer_accessible_ =
+    CommandLine::ForCurrentProcess()->HasSwitch(
+        switches::kEnableRendererAccessibility);
 }
 
 TabContents::~TabContents() {
@@ -2108,6 +2113,10 @@ void TabContents::DidFailProvisionalLoadWithError(
 
 void TabContents::DocumentLoadedInFrame() {
   controller_.DocumentLoadedInFrame();
+  if (renderer_accessible_ && !requested_accessibility_tree_) {
+    render_view_host()->RequestAccessibilityTree();
+    requested_accessibility_tree_ = true;
+  }
 }
 
 void TabContents::OnContentBlocked(ContentSettingsType type) {
@@ -2276,6 +2285,8 @@ void TabContents::RenderViewDeleted(RenderViewHost* rvh) {
 void TabContents::DidNavigate(RenderViewHost* rvh,
                               const ViewHostMsg_FrameNavigate_Params& params) {
   int extra_invalidate_flags = 0;
+
+  requested_accessibility_tree_ = false;
 
   if (PageTransition::IsMainFrame(params.transition)) {
     bool was_bookmark_bar_visible = ShouldShowBookmarkBar();
