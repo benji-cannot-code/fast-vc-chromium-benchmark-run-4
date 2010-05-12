0d0ab7e0de11da2fac36a74a6820792ad7da1e71
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-12 08:19:18 +0000

    2010-05-12  Robin Qiu  <robin.qiu@torchmobile.com.cn>
            Reviewed by Dirk Schulze.
    
            Fix a bug in SVGPathSegList::getPathSegAtLength().
            This bug is just a misktake: almost all of the code is OK, but the
            parameter is not used at all, therefore, this function always returns
            "1".
            And make a modification to return the last path segment if the distance
            exceeds the actual path length.
            https://bugs.webkit.org/show_bug.cgi?id=37515
    
            * svg/dom/svgpath-getPathSegAtLength-expected.txt: Added.
            * svg/dom/svgpath-getPathSegAtLength.html: Added.
    2010-05-12  Robin Qiu  <robin.qiu@torchmobile.com.cn>
    
            Reviewed by Dirk Schulze.
    
            Fix a bug in SVGPathSegList::getPathSegAtLength().
            This bug is just a misktake: almost all of the code is OK, but the
            parameter is not used at all, therefore, this function always returns
            "1".
            And make a modification to return the last path segment if the distance
            exceeds the actual path length.
            https://bugs.webkit.org/show_bug.cgi?id=37515
    
            Test: svg/dom/svgpath-getPathSegAtLength.html
    
            * svg/SVGPathSegList.cpp:
            (WebCore::SVGPathSegList::getPathSegAtLength):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59213 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGPathSegList.cpp
+++ third_party/WebKit/WebCore/svg/SVGPathSegList.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(SVG)
 #include "SVGPathSegList.h"
 
+#include "FloatConversion.h"
 #include "FloatPoint.h"
 #include "Path.h"
 #include "PathTraversalState.h"
@@ -52,12 +53,13 @@ SVGPathSegList::~SVGPathSegList()
 {
 }
 
-unsigned SVGPathSegList::getPathSegAtLength(double, ExceptionCode& ec)
+unsigned SVGPathSegList::getPathSegAtLength(double length, ExceptionCode& ec)
 {
     // FIXME : to be useful this will need to support non-normalized SVGPathSegLists
     int len = numberOfItems();
     // FIXME: Eventually this will likely move to a "path applier"-like model, until then PathTraversalState is less useful as we could just use locals
     PathTraversalState traversalState(PathTraversalState::TraversalSegmentAtLength);
+    traversalState.m_desiredLength = narrowPrecisionToFloat(length);
     for (int i = 0; i < len; ++i) {
         SVGPathSeg* segment = getItem(i, ec).get();
         if (ec)
@@ -93,13 +95,15 @@ unsigned SVGPathSegList::getPathSegAtLength(double, ExceptionCode& ec)
         }
         traversalState.m_totalLength += segmentLength;
         if ((traversalState.m_action == PathTraversalState::TraversalSegmentAtLength)
-            && (traversalState.m_totalLength > traversalState.m_desiredLength)) {
+            && (traversalState.m_totalLength >= traversalState.m_desiredLength)) {
             return traversalState.m_segmentIndex;
         }
         traversalState.m_segmentIndex++;
     }
-    
-    return 0; // The SVG spec is unclear as to what to return when the distance is not on the path    
+
+    // The SVG spec is unclear as to what to return when the distance is not on the path.
+    // WebKit/Opera/FF all return the last path segment if the distance exceeds the actual path length:
+    return traversalState.m_segmentIndex ? traversalState.m_segmentIndex - 1 : 0;
 }
 
 Path SVGPathSegList::toPathData()
