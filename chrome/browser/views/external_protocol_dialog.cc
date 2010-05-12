b4e06ef8714e6bf0ece10ae3823c3e500a800256
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-12 16:43:16 +0000

    Second round of transition of Time to TimeTicks for histogramming
    The first CL was getting too large, and was being held back by
    some platform specific changes.  I pulled out the problems into
    this CL to make it easier ot focus on getting them all right.
    
    BUG=14193
    r=eroman
    Review URL: http://codereview.chromium.org/2033005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47037 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/external_protocol_dialog.cc
+++ chrome/browser/views/external_protocol_dialog.cc
@@ -94,7 +94,7 @@ bool ExternalProtocolDialog::Accept() {
   // users start accepting these dialogs too quickly, we should worry about
   // clickjacking.
   UMA_HISTOGRAM_LONG_TIMES("clickjacking.launch_url",
-                           base::Time::Now() - creation_time_);
+                           base::TimeTicks::Now() - creation_time_);
 
   if (message_box_view_->IsCheckBoxSelected()) {
     ExternalProtocolHandler::SetBlockState(
@@ -118,7 +118,7 @@ ExternalProtocolDialog::ExternalProtocolDialog(TabContents* tab_contents,
                                                const std::wstring& command)
     : tab_contents_(tab_contents),
       url_(url),
-      creation_time_(base::Time::Now()) {
+      creation_time_(base::TimeTicks::Now()) {
   const int kMaxUrlWithoutSchemeSize = 256;
   const int kMaxCommandSize = 256;
   std::wstring elided_url_without_scheme;
