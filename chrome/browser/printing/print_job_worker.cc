28ea09df4d3bb54829894627123722484be632b0
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-12 19:26:40 +0000

    Set the job name for the print job on the Mac.
    BUG=http://crbug.com/29188
    TEST=as in bug
    
    Review URL: http://codereview.chromium.org/1997016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47056 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_job_worker.cc
+++ chrome/browser/printing/print_job_worker.cc
@@ -66,7 +66,7 @@ void PrintJobWorker::SetNewOwner(PrintJobWorkerOwner* new_owner) {
 }
 
 void PrintJobWorker::GetSettings(bool ask_user_for_settings,
-                                 gfx::NativeWindow parent_window,
+                                 gfx::NativeView parent_view,
                                  int document_page_count,
                                  bool has_selection,
                                  bool use_overlays) {
@@ -83,11 +83,11 @@ void PrintJobWorker::GetSettings(bool ask_user_for_settings,
     ChromeThread::PostTask(
         ChromeThread::UI, FROM_HERE,
         NewRunnableMethod(this, &PrintJobWorker::GetSettingsWithUI,
-                          parent_window, document_page_count,
+                          parent_view, document_page_count,
                           has_selection));
 #else
     PrintingContext::Result result = printing_context_.AskUserForSettings(
-        parent_window, document_page_count, has_selection);
+        parent_view, document_page_count, has_selection);
     GetSettingsDone(result);
 #endif
   } else {
@@ -112,13 +112,13 @@ void PrintJobWorker::GetSettingsDone(PrintingContext::Result result) {
 }
 
 #if defined(OS_MACOSX)
-void PrintJobWorker::GetSettingsWithUI(gfx::NativeWindow parent_window,
+void PrintJobWorker::GetSettingsWithUI(gfx::NativeView parent_view,
                                        int document_page_count,
                                        bool has_selection) {
   DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
 
   PrintingContext::Result result = printing_context_.AskUserForSettings(
-      parent_window, document_page_count, has_selection);
+      parent_view, document_page_count, has_selection);
   message_loop()->PostTask(FROM_HERE, NewRunnableMethod(
       this, &PrintJobWorker::GetSettingsDone, result));
 }
