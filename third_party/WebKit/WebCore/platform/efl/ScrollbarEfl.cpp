48b8c83672d0bed6bacc47555264a622fde9ee6f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-12 09:12:40 +0000

    2010-05-12  Lucas De Marchi  <lucas.demarchi@profusion.mobi>
            Reviewed by Jeremy Orlow.
    
            [EFL] It does not make sense to use ScrollbarEfl::ScrollbarEfl, so
            use ScrollbarEfl (GCC 4.5 seems to be pickier about this). Revision 59030
            fixed cases for RenderThemeEfl, but missed this for ScrollbarEfl.
            http://webkit.org/b/38904
    
            No new tests required.
    
            * platform/efl/ScrollbarEfl.cpp:
            (scrollbarEflEdjeMessage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59218 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/efl/ScrollbarEfl.cpp
+++ third_party/WebKit/WebCore/platform/efl/ScrollbarEfl.cpp
@@ -66,7 +66,7 @@ ScrollbarEfl::~ScrollbarEfl()
 
 static void scrollbarEflEdjeMessage(void* data, Evas_Object* o, Edje_Message_Type type, int id, void* msg)
 {
-    ScrollbarEfl::ScrollbarEfl* that = static_cast<ScrollbarEfl::ScrollbarEfl*>(data);
+    ScrollbarEfl* that = static_cast<ScrollbarEfl*>(data);
     Edje_Message_Float* m;
     int v;
 
