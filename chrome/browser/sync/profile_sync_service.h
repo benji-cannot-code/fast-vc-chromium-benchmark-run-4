7e9aef3af561579cf37e919f48f3b3459dfbb10d
chron@chromium.org <chron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-12 20:33:11 +0000

    Switch sync servers depending on whether you're running stable or dev. Initially just use the same url twice.
    Review URL: http://codereview.chromium.org/2013017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47064 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.h
+++ chrome/browser/sync/profile_sync_service.h
@@ -118,6 +118,11 @@ class ProfileSyncService : public browser_sync::SyncFrontend,
     MAX_SYNC_EVENT_CODE
   };
 
+  // Default sync server URL.
+  static const char* kSyncServerUrl;
+  // Sync server URL for dev channel users
+  static const char* kDevServerUrl;
+
   ProfileSyncService(ProfileSyncFactory* factory_,
                      Profile* profile,
                      bool bootstrap_sync_authentication);
@@ -311,6 +316,7 @@ class ProfileSyncService : public browser_sync::SyncFrontend,
   friend class ProfileSyncServiceTest;
   friend class ProfileSyncServicePreferenceTest;
   friend class ProfileSyncServiceTestHarness;
+  FRIEND_TEST(ProfileSyncServiceTest, InitialState);
   FRIEND_TEST(ProfileSyncServiceTest, UnrecoverableErrorSuspendsService);
 
   // Initializes the various settings from the command line.
