c0ff1a4646febadf5f749a632d8df7673b74c1f0
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-12 02:33:32 +0000

    Add a compile time assert that sizeof(String) == sizeof(AtomicString).
    Reviewed by Sam Weinig.
    
    * wtf/text/AtomicString.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/text/AtomicString.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/text/AtomicString.cpp
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+COMPILE_ASSERT(sizeof(AtomicString) == sizeof(String), atomic_string_and_string_must_be_same_size);
+
 class AtomicStringTable {
 public:
     static AtomicStringTable* create()
