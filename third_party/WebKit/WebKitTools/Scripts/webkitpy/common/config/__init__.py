3c18d7e422b555890dd4306a2e31aaf61783a29f
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-12 23:55:28 +0000

    2010-05-10  Ojan Vafai  <ojan@chromium.org>
            Reviewed by Adam Barth.
    
            webkit-patch upload --fancy-review now uses the bugzilla bug ID as the rietveld ID
            https://bugs.webkit.org/show_bug.cgi?id=38866
    
            wkrietveld.appspot.com has already been updated to allow you to
            pass --issue for the issue creation as well as subsequent uploads.
    
            Also, remove the extra code for trying to read out the issue ID
            from the changelog description since we just use the bugzilla ID now.
    
            * Scripts/webkitpy/common/config/__init__.py:
            * Scripts/webkitpy/common/net/rietveld.py:
            * Scripts/webkitpy/tool/steps/postcodereview.py:
            * Scripts/webkitpy/tool/steps/postdiff.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/common/config/__init__.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/common/config/__init__.py
@@ -4,5 +4,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import re
 
 codereview_server_host = "wkrietveld.appspot.com"
-codereview_server_regex = "https?://%s/" % re.sub('\.', '\\.', codereview_server_host)
 codereview_server_url = "https://%s/" % codereview_server_host
