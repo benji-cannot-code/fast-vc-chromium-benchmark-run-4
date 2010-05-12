c1beaf3ed183dab22e2f55f2c5c53d349a3fcf05
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-12 18:24:19 +0000

    Hide NTP app launcher debug link behind a flag.
    Review URL: http://codereview.chromium.org/2061004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47048 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/new_new_tab.js
+++ chrome/browser/resources/new_new_tab.js
@@ -31,7 +31,9 @@ function getAppsCallback(data) {
   // for everyone.
   if (appsSection.hasChildNodes()) {
     appsSection.classList.remove('disabled');
-    debugSection.classList.remove('disabled');
+    if (data.showDebugLink) {
+      debugSection.classList.remove('disabled');
+    }
   } else {
     appsSection.classList.add('disabled');
     debugSection.classList.add('disabled');
