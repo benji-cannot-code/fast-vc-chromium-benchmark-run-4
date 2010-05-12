b8c117749ff69494841bf786971dd8cd5b86d6b7
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-12 04:36:18 +0000

    2010-05-11  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            sheriffbot is spammy.
            https://bugs.webkit.org/show_bug.cgi?id=38936
    
            Reduce sheriffbot spam by not warning about new blameworthy revisions
            that can be explained by previously blamed revisions.  This might cause
            us to not warn about some real failures, but we're getting too much
            spam from slow builders that have large blamelists (and we've already
            poked the responsible folks using data from a fast builder).
    
            * Scripts/webkitpy/tool/bot/sheriff_unittest.py:
                - Removed unneeded import
            * Scripts/webkitpy/tool/commands/sheriffbot.py:
                - The logic change
            * Scripts/webkitpy/tool/commands/sheriffbot_unittest.py:
                - Tests of the change
            * Scripts/webkitpy/tool/mocktool.py:
                - Merged two declarations of MockBuilder
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/bot/queueengine_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/bot/queueengine_unittest.py
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import datetime
 import os
 import shutil
 import tempfile
@@ -160,6 +161,12 @@ class QueueEngineTest(unittest.TestCase):
         expected_callbacks = expected_callbacks[:next_work_item_index + 2]
         expected_callbacks.append('should_continue_work_queue')
         self.assertEquals(delegate._callbacks, expected_callbacks)
+
+    def test_sleep_message(self):
+        engine = QueueEngine("test", None, None)
+        engine._now = lambda: datetime.datetime(2010, 1, 1)
+        expected_sleep_message = "MESSAGE Sleeping until 2010-01-01 00:02:00 (2 mins)."
+        self.assertEqual(engine._sleep_message("MESSAGE"), expected_sleep_message)
 
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp(suffix="work_queue_test_logs")
