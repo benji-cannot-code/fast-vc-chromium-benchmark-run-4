2771e5dd1d8ffd170ca82d4ea960d1ecde6fd77a
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-12 21:36:39 +0000

    Add include as recommended by the linux man page for realpath().
    Review URL: http://codereview.chromium.org/1985018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47074 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_posix.cc
+++ base/file_util_posix.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fcntl.h>
 #include <fnmatch.h>
 #include <libgen.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
