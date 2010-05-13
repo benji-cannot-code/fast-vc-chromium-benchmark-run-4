5b43945cb139dd29a34a0bc318c61b4f8998066b
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-13 02:12:07 +0000

    Removed non-thread-safe QName ad-hoc interning.
    BUG=43467
    TEST=made sure sync_integration_tests don't have any more ~QName crashes
    
    Review URL: http://codereview.chromium.org/2012014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47109 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libjingle/files/talk/xmllite/xmlelement.cc
+++ third_party/libjingle/files/talk/xmllite/xmlelement.cc
@@ -41,8 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace buzz {
 
-const QName QN_EMPTY(true, STR_EMPTY, STR_EMPTY);
-const QName QN_XMLNS(true, STR_EMPTY, STR_XMLNS);
+const QName QN_EMPTY(STR_EMPTY, STR_EMPTY);
+const QName QN_XMLNS(STR_EMPTY, STR_XMLNS);
 
 
 XmlChild::~XmlChild() {
