9375a882d9d90b23054a15389ec11d2407be7ad4
joepeck@webkit.org <joepeck@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-13 17:27:15 +0000

    2010-05-13  Joseph Pecoraro  <joepeck@webkit.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Event Markers should be hidden in Resource's "Size" Graph
            https://bugs.webkit.org/show_bug.cgi?id=39074
    
            Added Show / Hide event dividers functionality to the timeline grid,
            and included delegation functions in AbstractTimelinePanel.
    
            * inspector/front-end/AbstractTimelinePanel.js: delegate to the inner grid
            (WebInspector.AbstractTimelinePanel.prototype.hideEventDividers):
            (WebInspector.AbstractTimelinePanel.prototype.showEventDividers):
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.ResourcesPanel.prototype.populateSidebar): save a reference to the size graph item to know when its selected
            (WebInspector.ResourcesPanel.prototype._graphSelected): if the size graph is selected hide the dividers, otherwise show them
            * inspector/front-end/TimelineGrid.js:
            (WebInspector.TimelineGrid.prototype.hideEventDividers): set the container to display: none
            (WebInspector.TimelineGrid.prototype.showEventDividers): set the container to display: block
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59372 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/AbstractTimelinePanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/AbstractTimelinePanel.js
@@ -425,6 +425,16 @@ WebInspector.AbstractTimelinePanel.prototype = {
     addEventDivider: function(divider)
     {
         this._timelineGrid.addEventDivider(divider);
+    },
+
+    hideEventDividers: function()
+    {
+        this._timelineGrid.hideEventDividers();
+    },
+
+    showEventDividers: function()
+    {
+        this._timelineGrid.showEventDividers();
     }
 }
 
