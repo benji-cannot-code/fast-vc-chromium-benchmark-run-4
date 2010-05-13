f35a91cbe5475e8f196c5b4e1f472e7c3ea9b100
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-13 08:02:06 +0000

    Mac: Testing infrastructure for sandboxed tests.
    Create an initial framework for running tests across all Chrome sandbox types.
    Also added 2 simple initial tests to illustrate usage.
    
    The main motivation for adding this code is for an upcoming x-process font loading cl which needs this infrastructure.
    
    Test case registration is currently done manually. It would be better to have a macro that does this at compile time but given the small number of current tests this seems like overkill at this point.
    
    BUG=37285
    TEST=Unit tests should pass
    
    Review URL: http://codereview.chromium.org/1993005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47127 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/sandbox_mac.h
+++ chrome/common/sandbox_mac.h
@@ -11,7 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace sandbox {
 
 enum SandboxProcessType {
-  SANDBOX_TYPE_RENDERER,
+
+  SANDBOX_TYPE_FIRST_TYPE,  // Placeholder to ease iteration.
+
+  SANDBOX_TYPE_RENDERER = SANDBOX_TYPE_FIRST_TYPE,
 
   // The worker processes uses the most restrictive sandbox which has almost
   // *everything* locked down. Only a couple of /System/Library/ paths and
@@ -27,6 +30,8 @@ enum SandboxProcessType {
   // loader contains the user's untrusted code.
   SANDBOX_TYPE_NACL_PLUGIN,
   SANDBOX_TYPE_NACL_LOADER,
+
+  SANDBOX_AFTER_TYPE_LAST_TYPE,  // Placeholder to ease iteration.
 };
 
 // Warm up System APIs that empirically need to be accessed before the Sandbox
