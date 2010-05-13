6de9d9654c3840d20c902cddd58916853883dcfb
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-13 22:53:29 +0000

    Hook up ScreenLocker to screen lock cros library.  * Added ScreenLockerLibrary and its mock class.  * Call NotifyScreenLockCompleted when screen locker is shown  * Call NotifyScreenUnlocked when authentication is sucessful.  * Replace includes in cros_in_process_browser_test.cc with forward declarations.   * Added ScreenLockerTester for testing   * Added new browser test ScreenLockerTest.TestBasic
    BUG=2914
    TEST=added ScreenLockerTest.TestBasic browser test.
    
    Review URL: http://codereview.chromium.org/2025009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47216 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/cros_library.h
+++ chrome/browser/chromeos/cros/cros_library.h
@@ -19,6 +19,7 @@ class LoginLibrary;
 class MountLibrary;
 class NetworkLibrary;
 class PowerLibrary;
+class ScreenLockLibrary;
 class SpeechSynthesisLibrary;
 class SynapticsLibrary;
 
@@ -49,6 +50,8 @@ class CrosLibrary {
     void SetNetworkLibrary(NetworkLibrary* library, bool own);
     // Setter for PowerLibrary.
     void SetPowerLibrary(PowerLibrary* library, bool own);
+    // Setter for ScreenLockLibrary.
+    void SetScreenLockLibrary(ScreenLockLibrary* library, bool own);
     // Setter for SpeechSynthesisLibrary.
     void SetSpeechSynthesisLibrary(SpeechSynthesisLibrary* library, bool own);
     // Setter for SynapticsLibrary.
@@ -80,6 +83,9 @@ class CrosLibrary {
 
   // Getter for PowerLibrary
   PowerLibrary* GetPowerLibrary();
+
+  // Getter for ScreenLockLibrary
+  ScreenLockLibrary* GetScreenLockLibrary();
 
   // This gets the singleton SpeechSynthesisLibrary.
   SpeechSynthesisLibrary* GetSpeechSynthesisLibrary();
@@ -113,6 +119,7 @@ class CrosLibrary {
   MountLibrary* mount_lib_;
   NetworkLibrary* network_lib_;
   PowerLibrary* power_lib_;
+  ScreenLockLibrary* screen_lock_lib_;
   SpeechSynthesisLibrary* speech_synthesis_lib_;
   SynapticsLibrary* synaptics_lib_;
 
@@ -123,6 +130,7 @@ class CrosLibrary {
   bool own_mount_loader_;
   bool own_network_loader_;
   bool own_power_loader_;
+  bool own_screen_lock_lib_;
   bool own_speech_synthesis_library_;
   bool own_synaptics_library_;
 
--- chrome/browser/chromeos/login/screen_lock_view.cc
+++ chrome/browser/chromeos/login/screen_lock_view.cc
@@ -184,4 +184,3 @@ void ScreenLockView::SetImage(const SkBitmap& image,
 }
 
 }  // namespace chromeos
-
