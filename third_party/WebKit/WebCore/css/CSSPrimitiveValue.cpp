1433fda5dc4e2f82c052fc814d9e9909144209a2
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-13 22:56:14 +0000

    Follow up to: Fix for https://bugs.webkit.org/show_bug.cgi?id=38221 Memory issues due to the changes in 36556 (increased CSSPrimitiveValue size). Part of <rdar://problem/7921275>
    Reviewed by Anders Carlsson.
    
    Fix failing tests on Leopard.
    
    Initialize m_hasCachedCSSText in CSSPrimitiveValue::init methods.
    
    * css/CSSPrimitiveValue.cpp:
    (WebCore::CSSPrimitiveValue::init):
    (WebCore::CSSPrimitiveValue::cssText): Add extra assertion.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59394 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSPrimitiveValue.cpp
+++ third_party/WebKit/WebCore/css/CSSPrimitiveValue.cpp
@@ -220,12 +220,14 @@ CSSPrimitiveValue::CSSPrimitiveValue(const Length& length)
 void CSSPrimitiveValue::init(PassRefPtr<Counter> c)
 {
     m_type = CSS_COUNTER;
+    m_hasCachedCSSText = false;
     m_value.counter = c.releaseRef();
 }
 
 void CSSPrimitiveValue::init(PassRefPtr<Rect> r)
 {
     m_type = CSS_RECT;
+    m_hasCachedCSSText = false;
     m_value.rect = r.releaseRef();
 }
 
@@ -233,6 +235,7 @@ void CSSPrimitiveValue::init(PassRefPtr<Rect> r)
 void CSSPrimitiveValue::init(PassRefPtr<DashboardRegion> r)
 {
     m_type = CSS_DASHBOARD_REGION;
+    m_hasCachedCSSText = false;
     m_value.region = r.releaseRef();
 }
 #endif
@@ -240,6 +243,7 @@ void CSSPrimitiveValue::init(PassRefPtr<DashboardRegion> r)
 void CSSPrimitiveValue::init(PassRefPtr<Pair> p)
 {
     m_type = CSS_PAIR;
+    m_hasCachedCSSText = false;
     m_value.pair = p.releaseRef();
 }
 
@@ -653,7 +657,7 @@ String CSSPrimitiveValue::cssText() const
 
     if (m_hasCachedCSSText) {
         ASSERT(cssTextCache().contains(this));
-        return cssTextCache().get(this); 
+        return cssTextCache().get(this);
     }
 
     String text;
@@ -857,6 +861,8 @@ String CSSPrimitiveValue::cssText() const
             text = quoteCSSStringIfNeeded(m_value.string);
             break;
     }
+
+    ASSERT(!cssTextCache().contains(this));
     cssTextCache().set(this, text);
     m_hasCachedCSSText = true;
     return text;
