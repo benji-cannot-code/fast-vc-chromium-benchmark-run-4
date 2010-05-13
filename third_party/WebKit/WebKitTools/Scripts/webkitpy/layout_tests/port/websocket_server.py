cb0be8487cab33e1baddc642bebfe72f76f0eef9
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-13 08:52:06 +0000

    WebSocket: pywebsocket 0.5 https://bugs.webkit.org/show_bug.cgi?id=38034
    Reviewed by David Levin.
    
    Remove pywebsocket from webkitpy/thirdparty.
    Make pywebsocket autoinstalled.
    
    * Scripts/new-run-webkit-websocketserver:
      Add --output-dir option.
    * Scripts/old-run-webkit-tests:
      Use new-run-webkit-websocketserver, rather than directly run pywebsocket's standalone.py
    * Scripts/run-webkit-websocketserver:
      Ditto.
    * Scripts/webkitpy/layout_tests/port/websocket_server.py:
      Use autoinstalled pywebsocket.
    * Scripts/webkitpy/thirdparty/__init__.py:
      Autoinstall pywebsocket
    * Scripts/webkitpy/thirdparty/pywebsocket: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59353 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/websocket_server.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/websocket_server.py
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """A class to help start/stop the PyWebSocket server used by layout tests."""
 
 
+from __future__ import with_statement
+
 import codecs
 import logging
 import optparse
@@ -237,11 +239,8 @@ class PyWebSocket(http_server.Lighttpd):
             raise PyWebSocketNotStarted(
                 'Failed to start %s server.' % self._server_name)
         if self._pidfile:
-            file = codecs.open(self._pidfile, "w", "ascii")
-            try:
+            with codecs.open(self._pidfile, "w", "ascii") as file:
                 file.write("%d" % self._process.pid)
-            finally:
-                file.close()
 
     def stop(self, force=False):
         if not force and not self.is_running():
@@ -251,11 +250,8 @@ class PyWebSocket(http_server.Lighttpd):
         if self._process:
             pid = self._process.pid
         elif self._pidfile:
-            file = codecs.open(self._pidfile, "r", "ascii")
-            try:
+            with codecs.open(self._pidfile, "r", "ascii") as file:
                 pid = int(file.read().strip())
-            finally:
-                file.close()
 
         if not pid:
             raise PyWebSocketNotFound(
