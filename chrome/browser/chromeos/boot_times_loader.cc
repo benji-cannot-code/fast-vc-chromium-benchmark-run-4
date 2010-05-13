94acf21de3f902d7e6a8fe1a8e9cf8ab391d316b
nkostylev@google.com <nkostylev@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-13 14:10:49 +0000

    Fix crash on login for debug version of Chrome.
    BUG= http://crosbug.com/3247
    TEST=Use debug version of Chrome on the device. It should not crash on login.
    
    Review URL: http://codereview.chromium.org/2087001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47141 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/boot_times_loader.cc
+++ chrome/browser/chromeos/boot_times_loader.cc
@@ -15,12 +15,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/thread.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chrome_thread.h"
 #include "chrome/common/chrome_switches.h"
 
 namespace chromeos {
 
 // File uptime logs are located in.
 static const char kLogPath[] = "/tmp";
+
+// Delay in milliseconds between file read attempts.
+static const int64 kReadAttemptDelayMs = 500;
 
 BootTimesLoader::BootTimesLoader() : backend_(new Backend()) {
 }
@@ -45,7 +49,8 @@ BootTimesLoader::Handle BootTimesLoader::GetBootTimes(
       new CancelableRequest<GetBootTimesCallback>(callback));
   AddRequest(request, consumer);
 
-  g_browser_process->file_thread()->message_loop()->PostTask(
+  ChromeThread::PostTask(
+      ChromeThread::FILE,
       FROM_HERE,
       NewRunnableMethod(backend_.get(), &Backend::GetBootTimes, request));
   return request->handle();
@@ -115,10 +120,11 @@ void BootTimesLoader::Backend::GetBootTimes(
   FilePath log_dir(kLogPath);
   FilePath log_file = log_dir.Append(kLoginPromptReady);
   if (!file_util::PathExists(log_file)) {
-    g_browser_process->file_thread()->message_loop()->PostDelayedTask(
+    ChromeThread::PostDelayedTask(
+        ChromeThread::FILE,
         FROM_HERE,
         NewRunnableMethod(this, &Backend::GetBootTimes, request),
-        500);
+        kReadAttemptDelayMs);
     return;
   }
 
