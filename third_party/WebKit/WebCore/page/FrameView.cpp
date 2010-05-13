61576fd156ae07d51f5bdee4f8eb1acce59aa43f
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-13 23:14:46 +0000

    2010-05-13  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dave Hyatt.
    
            Fix scrolling in composited iframes
            https://bugs.webkit.org/show_bug.cgi?id=39088
            <rdar://problem/7980099>
    
            When propagating compositing out of iframes, we have to update the position of the iframe content
            layers on scrolling.
    
            Test: compositing/iframes/scrolling-iframe.html
    
            * page/FrameView.cpp:
            (WebCore::FrameView::scrollPositionChanged): Call updateContentLayerScrollPosition() when scrolling.
    
            * rendering/RenderLayerBacking.cpp:
            (WebCore::RenderLayerBacking::updateGraphicsLayerGeometry): Renamed setRootPlatformLayerClippingBox() to updateContentLayerOffset().
    
            * rendering/RenderLayerCompositor.h:
            * rendering/RenderLayerCompositor.cpp: Rename m_clippingLayer to m_clipLayer to go better with m_scrollLayer.
            (WebCore::RenderLayerCompositor::updateContentLayerOffset): Renamed from setRootPlatformLayerClippingBox().
                Set the size of the clipping layer from the FrameView now, so that scrollbars are not clipped out.
                Update the scroll layer position too.
            (WebCore::RenderLayerCompositor::updateContentLayerScrollPosition): Set the position of the scroll layer
                when the ScrollView is scrolled.
            (WebCore::RenderLayerCompositor::rootPlatformLayer): m_clippingLayer rename.
            (WebCore::RenderLayerCompositor::ensureRootPlatformLayer): m_clippingLayer rename. Also create m_scrollLayer at
                the same time as the clip layer, and clean it up as necessary.
            (WebCore::RenderLayerCompositor::destroyRootPlatformLayer): m_clippingLayer rename, and clean up the m_scrollLayer too.
            (WebCore::RenderLayerCompositor::detachRootPlatformLayer): m_clippingLayer rename
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59396 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -1048,6 +1048,13 @@ void FrameView::scrollPositionChanged()
 #endif
         }
     }
+
+#if USE(ACCELERATED_COMPOSITING)
+    if (RenderView* root = m_frame->contentRenderer()) {
+        if (root->usesCompositing())
+            root->compositor()->updateContentLayerScrollPosition(scrollPosition());
+    }
+#endif
 }
 
 HostWindow* FrameView::hostWindow() const
