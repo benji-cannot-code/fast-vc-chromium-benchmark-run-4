7a7f9ef3fde20ab03e6fb1889c739cc86c16b44d
vangelis@chromium.org <vangelis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-13 22:03:07 +0000

    Adding a method to RendererWebKitClientImpl to create a GLES2Context. BUG=43911 Review URL: http://codereview.chromium.org/2023008
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/renderer_webkitclient_impl.cc
+++ chrome/renderer/renderer_webkitclient_impl.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/render_view.h"
 #include "chrome/renderer/renderer_webstoragenamespace_impl.h"
 #include "chrome/renderer/visitedlink_slave.h"
+#include "chrome/renderer/webgles2context_impl.h"
 #include "chrome/renderer/webgraphicscontext3d_command_buffer_impl.h"
 #include "googleurl/src/gurl.h"
 #include "ipc/ipc_sync_message_filter.h"
@@ -333,6 +334,15 @@ RendererWebKitClientImpl::createGraphicsContext3D() {
     return NULL;
 #endif
   }
+}
+
+WebKit::WebGLES2Context*
+RendererWebKitClientImpl::createGLES2Context() {
+#if defined(ENABLE_GPU)
+    return new WebGLES2ContextImpl();
+#else
+    return NULL;
+#endif
 }
 
 //------------------------------------------------------------------------------
