0a5f1f61d7a6ae6722a7f34a83069527b39e1bca
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-14 19:49:36 +0000

    2010-05-14  Young Han Lee  <joybro@company100.net>
            Reviewed by Darin Adler.
    
            Don't include RetainPtr.h unless PLATFORM(CF) is set.
            The RetainPtr is only used for CF platform in this class.
            https://bugs.webkit.org/show_bug.cgi?id=39043
    
            No new tests, there is no new functionality.
    
            * platform/network/curl/ResourceHandleManager.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59482 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/curl/ResourceHandleManager.cpp
+++ third_party/WebKit/WebCore/platform/network/curl/ResourceHandleManager.cpp
@@ -46,7 +46,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <errno.h>
 #include <stdio.h>
+#if PLATFORM(CF)
 #include <wtf/RetainPtr.h>
+#endif
 #include <wtf/Threading.h>
 #include <wtf/Vector.h>
 #include <wtf/text/CString.h>
