6959b3280b22dd4bfd35c83a1a515ee99893b582
jocelyn.turcotte@nokia.com <jocelyn.turcotte@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-14 11:24:05 +0000

    Fix a VS2010 assert in std::copy https://bugs.webkit.org/show_bug.cgi?id=38630
    Reviewed by Alexey Proskuryakov.
    
    The assert complains that the output iterator is null.
    
    * wtf/Vector.h:
    (WTF::::operator):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Vector.h
+++ third_party/WebKit/JavaScriptCore/wtf/Vector.h
@@ -687,6 +687,12 @@ namespace WTF {
                 return *this;
         }
         
+// Works around an assert in VS2010. See https://connect.microsoft.com/VisualStudio/feedback/details/558044/std-copy-should-not-check-dest-when-first-last
+#if COMPILER(MSVC) && defined(_ITERATOR_DEBUG_LEVEL) && _ITERATOR_DEBUG_LEVEL
+        if (!begin())
+            return *this;
+#endif
+
         std::copy(other.begin(), other.begin() + size(), begin());
         TypeOperations::uninitializedCopy(other.begin() + size(), other.end(), end());
         m_size = other.size();
@@ -710,6 +716,12 @@ namespace WTF {
                 return *this;
         }
         
+// Works around an assert in VS2010. See https://connect.microsoft.com/VisualStudio/feedback/details/558044/std-copy-should-not-check-dest-when-first-last
+#if COMPILER(MSVC) && defined(_ITERATOR_DEBUG_LEVEL) && _ITERATOR_DEBUG_LEVEL
+        if (!begin())
+            return *this;
+#endif
+
         std::copy(other.begin(), other.begin() + size(), begin());
         TypeOperations::uninitializedCopy(other.begin() + size(), other.end(), end());
         m_size = other.size();
