41f8cfb42acce681c373860857739d3b22046e82
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-14 07:43:41 +0000

    2010-05-14  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Darin Adler.
    
            Add test to check that setting various CanvasRenderingContext2D properties
            to invalid values is ignored.
    
            https://bugs.webkit.org/show_bug.cgi?id=38841
    
            * fast/canvas/canvas-invalid-values-expected.txt: Added.
            * fast/canvas/canvas-invalid-values.html: Added.
            * fast/canvas/script-tests/canvas-invalid-values.js: Added.
            (trySettingMiterLimit):
            (trySettingLineWidth):
            (trySettingShadowBlur):
            (trySettingShadowOffsetX):
            (trySettingShadowOffsetY):
    2010-05-14  Andreas Kling  <andreas.kling@nokia.com>
    
            Reviewed by Darin Adler.
    
            Ignore invalid values for various CanvasRenderingContext2D properties
            (lineWidth, miterLimit, shadowOffsetX, shadowOffsetY and shadowBlur)
    
            https://bugs.webkit.org/show_bug.cgi?id=38841
    
            Test: fast/canvas/canvas-invalid-values.html
    
            * html/canvas/CanvasRenderingContext2D.cpp:
            (WebCore::CanvasRenderingContext2D::setLineWidth):
            (WebCore::CanvasRenderingContext2D::setMiterLimit):
            (WebCore::CanvasRenderingContext2D::setShadowOffsetX):
            (WebCore::CanvasRenderingContext2D::setShadowOffsetY):
            (WebCore::CanvasRenderingContext2D::setShadowBlur):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59447 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -216,7 +216,7 @@ float CanvasRenderingContext2D::lineWidth() const
 
 void CanvasRenderingContext2D::setLineWidth(float width)
 {
-    if (!(width > 0))
+    if (!(isfinite(width) && width > 0))
         return;
     state().m_lineWidth = width;
     GraphicsContext* c = drawingContext();
@@ -266,7 +266,7 @@ float CanvasRenderingContext2D::miterLimit() const
 
 void CanvasRenderingContext2D::setMiterLimit(float limit)
 {
-    if (!(limit > 0))
+    if (!(isfinite(limit) && limit > 0))
         return;
     state().m_miterLimit = limit;
     GraphicsContext* c = drawingContext();
@@ -282,6 +282,8 @@ float CanvasRenderingContext2D::shadowOffsetX() const
 
 void CanvasRenderingContext2D::setShadowOffsetX(float x)
 {
+    if (!isfinite(x))
+        return;
     state().m_shadowOffset.setWidth(x);
     applyShadow();
 }
@@ -293,6 +295,8 @@ float CanvasRenderingContext2D::shadowOffsetY() const
 
 void CanvasRenderingContext2D::setShadowOffsetY(float y)
 {
+    if (!isfinite(y))
+        return;
     state().m_shadowOffset.setHeight(y);
     applyShadow();
 }
@@ -304,6 +308,8 @@ float CanvasRenderingContext2D::shadowBlur() const
 
 void CanvasRenderingContext2D::setShadowBlur(float blur)
 {
+    if (!(isfinite(blur) && blur >= 0))
+        return;
     state().m_shadowBlur = blur;
     applyShadow();
 }
