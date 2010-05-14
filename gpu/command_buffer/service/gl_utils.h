795e49adf6c2cf43487e6613456f6960a8cd315d
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-14 22:52:48 +0000

    Map RGBA4/RGB5_A1 to RGBA and RGB565 to RGB in RenderbufferStorage. Review URL: http://codereview.chromium.org/2119001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47329 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gl_utils.h
+++ gpu/command_buffer/service/gl_utils.h
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     #define GL_MAX_FRAGMENT_UNIFORM_VECTORS     0x8DFD
     #define GL_MAX_VERTEX_UNIFORM_VECTORS       0x8DFB
     #define GL_MAX_VARYING_VECTORS              0x8DFC
+    #define GL_RGB565                         0x8D62
     #define GL_SHADER_BINARY_FORMATS          0x8DF8
     #define GL_NUM_SHADER_BINARY_FORMATS      0x8DF9
     #define GL_SHADER_COMPILER                0x8DFA
--- gpu/command_buffer/service/renderbuffer_manager.h
+++ gpu/command_buffer/service/renderbuffer_manager.h
@@ -27,7 +27,8 @@ class RenderbufferManager {
 
     explicit RenderbufferInfo(GLuint service_id)
         : service_id_(service_id),
-          cleared_(false) {
+          cleared_(false),
+          internalformat_(GL_RGBA4) {
     }
 
     GLuint service_id() const {
@@ -40,6 +41,14 @@ class RenderbufferManager {
 
     void set_cleared() {
       cleared_ = true;
+    }
+
+    GLenum internalformat() const {
+      return internalformat_;
+    }
+
+    void set_internalformat(GLenum internalformat) {
+      internalformat_ = internalformat;
     }
 
     bool IsDeleted() {
@@ -61,6 +70,9 @@ class RenderbufferManager {
 
     // Whether this renderbuffer has been cleared
     bool cleared_;
+
+    // Renderbuffer internalformat set through RenderbufferStorage().
+    GLenum internalformat_;
   };
 
   RenderbufferManager() { }
