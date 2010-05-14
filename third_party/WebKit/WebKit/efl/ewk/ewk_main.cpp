20e036d93635413cfb9db7241b667166b47e53fe
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-14 08:44:43 +0000

    2010-05-14  Lucas De Marchi  <lucas.demarchi@profusion.mobi>
            Reviewed by David Levin.
    
            [EF] Include missing header.
            http://webkit.org/b/38905
    
            * efl/ewk/ewk_main.cpp: Include PageGroup.h
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/efl/ewk/ewk_main.cpp
+++ third_party/WebKit/WebKit/efl/ewk/ewk_main.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EWebKit.h"
 #include "Logging.h"
 #include "PageCache.h"
+#include "PageGroup.h"
 #include "ewk_private.h"
 #include "runtime/InitializeThreading.h"
 #include "wtf/Threading.h"
