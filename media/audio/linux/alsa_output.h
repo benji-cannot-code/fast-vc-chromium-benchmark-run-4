3aa7f7c0bd3452895a48a0f3c84ece435fb5c4a4
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-14 23:16:00 +0000

    Removed AlsaPcmOutputStrem::Packet. Fixed bug in AlsaPcmOutputStream::ScheduleNextWrite which would cause high CPU consumption. BUG=28654 TEST=Audio still works on Linux
    Review URL: http://codereview.chromium.org/2008010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47333 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/linux/alsa_output.h
+++ media/audio/linux/alsa_output.h
@@ -40,6 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_output.h"
 #include "testing/gtest/include/gtest/gtest_prod.h"
 
+namespace media {
+class SeekableBuffer;
+};  // namespace media
+
 class AlsaWrapper;
 class AudioManagerLinux;
 
@@ -95,7 +99,7 @@ class AlsaPcmOutputStream :
   FRIEND_TEST(AlsaPcmOutputStreamTest, BufferPacket_Negative);
   FRIEND_TEST(AlsaPcmOutputStreamTest, BufferPacket_StopStream);
   FRIEND_TEST(AlsaPcmOutputStreamTest, BufferPacket_Underrun);
-  FRIEND_TEST(AlsaPcmOutputStreamTest, BufferPacket_UnfinishedPacket);
+  FRIEND_TEST(AlsaPcmOutputStreamTest, BufferPacket_FullBuffer);
   FRIEND_TEST(AlsaPcmOutputStreamTest, ConstructedState);
   FRIEND_TEST(AlsaPcmOutputStreamTest, LatencyFloor);
   FRIEND_TEST(AlsaPcmOutputStreamTest, OpenClose);
@@ -110,22 +114,6 @@ class AlsaPcmOutputStream :
   FRIEND_TEST(AlsaPcmOutputStreamTest, WritePacket_WriteFails);
 
   virtual ~AlsaPcmOutputStream();
-
-  // In-memory buffer to hold sound samples before pushing to the sound device.
-  // TODO(ajwong): There are now about 3 buffer/packet implementations. Factor
-  // them out.
-  struct Packet {
-    explicit Packet(uint32 new_capacity)
-        : capacity(new_capacity),
-          size(0),
-          used(0),
-          buffer(new char[capacity]) {
-    }
-    uint32 capacity;
-    uint32 size;
-    uint32 used;
-    scoped_array<char> buffer;
-  };
 
   // Flags indicating the state of the stream.
   enum InternalState {
@@ -145,13 +133,12 @@ class AlsaPcmOutputStream :
 
   // Functions to get another packet from the data source and write it into the
   // ALSA device.
-  void BufferPacket(Packet* packet);
-  void WritePacket(Packet* packet);
+  void BufferPacket();
+  void WritePacket();
   void WriteTask();
-  void ScheduleNextWrite(Packet* current_packet);
+  void ScheduleNextWrite();
 
   // Utility functions for talking with the ALSA API.
-  static uint32 FramesInPacket(const Packet& packet, uint32 bytes_per_frame);
   static uint32 FramesToMicros(uint32 frames, uint32 sample_rate);
   static uint32 FramesToMillis(uint32 frames, uint32 sample_rate);
   std::string FindDeviceForChannels(uint32 channels);
@@ -160,6 +147,7 @@ class AlsaPcmOutputStream :
                         uint32 latency);
   bool CloseDevice(snd_pcm_t* handle);
   snd_pcm_sframes_t GetAvailableFrames();
+  snd_pcm_sframes_t GetCurrentDelay();
 
   // Attempts to find the best matching linux audio device for the given number
   // of channels.  This function will set |device_name_| and |should_downmix_|.
@@ -228,8 +216,10 @@ class AlsaPcmOutputStream :
   std::string device_name_;
   bool should_downmix_;
   uint32 latency_micros_;
+  uint32 packet_size_;
   uint32 micros_per_packet_;
   uint32 bytes_per_output_frame_;
+  uint32 alsa_buffer_frames_;
 
   // Flag indicating the code should stop reading from the data source or
   // writing to the ALSA device.  This is set because the device has entered
@@ -247,7 +237,7 @@ class AlsaPcmOutputStream :
   // Handle to the actual PCM playback device.
   snd_pcm_t* playback_handle_;
 
-  scoped_ptr<Packet> packet_;
+  scoped_ptr<media::SeekableBuffer> buffer_;
   uint32 frames_per_packet_;
 
   // Used to check which message loop is allowed to call the public APIs.
