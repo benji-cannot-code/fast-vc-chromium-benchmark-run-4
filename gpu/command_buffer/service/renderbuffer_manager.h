b136553c489855393ab8f562aa80118b7cbc1a72
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-14 23:26:11 +0000

    Revert 47329 - Map RGBA4/RGB5_A1 to RGBA and RGB565 to RGB in RenderbufferStorage. Review URL: http://codereview.chromium.org/2119001
    TBR=zmo@google.com
    Review URL: http://codereview.chromium.org/2074008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47334 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/renderbuffer_manager.h
+++ gpu/command_buffer/service/renderbuffer_manager.h
@@ -27,8 +27,7 @@ class RenderbufferManager {
 
     explicit RenderbufferInfo(GLuint service_id)
         : service_id_(service_id),
-          cleared_(false),
-          internalformat_(GL_RGBA4) {
+          cleared_(false) {
     }
 
     GLuint service_id() const {
@@ -41,14 +40,6 @@ class RenderbufferManager {
 
     void set_cleared() {
       cleared_ = true;
-    }
-
-    GLenum internalformat() const {
-      return internalformat_;
-    }
-
-    void set_internalformat(GLenum internalformat) {
-      internalformat_ = internalformat;
     }
 
     bool IsDeleted() {
@@ -70,9 +61,6 @@ class RenderbufferManager {
 
     // Whether this renderbuffer has been cleared
     bool cleared_;
-
-    // Renderbuffer internalformat set through RenderbufferStorage().
-    GLenum internalformat_;
   };
 
   RenderbufferManager() { }
