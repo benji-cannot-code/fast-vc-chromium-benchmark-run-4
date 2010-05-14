1477e98d7bc063743d8e4659f174d17bb234ed0d
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-14 15:24:14 +0000

    .: [Qt] Rename QtLauncher to QtTestBrowser https://bugs.webkit.org/show_bug.cgi?id=37665
    Rubber-stamped by Antti Koivisto.
    
    * WebKit.pro:
    
    WebKitTools: [Qt] Rename QtLauncher to QtTestBrowser
    
    Rubber-stamped by Antti Koivisto.
    
    * QtTestBrowser: Copied from WebKitTools/QtLauncher.
    * QtTestBrowser/QtLauncher.pro: Removed.
    * QtTestBrowser/QtLauncher.qrc: Removed.
    * QtTestBrowser/QtTestBrowser.pro: Copied from WebKitTools/QtLauncher/QtLauncher.pro.
    * QtTestBrowser/QtTestBrowser.qrc: Copied from WebKitTools/QtLauncher/QtLauncher.qrc.
    * Scripts/run-launcher:
    * Scripts/webkitdirs.pm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59474 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -892,7 +892,7 @@ sub launcherName()
     if (isGtk()) {
         return "GtkLauncher";
     } elsif (isQt()) {
-        return "QtLauncher";
+        return "QtTestBrowser";
     } elsif (isWx()) {
         return "wxBrowser";
     } elsif (isAppleWebKit()) {
