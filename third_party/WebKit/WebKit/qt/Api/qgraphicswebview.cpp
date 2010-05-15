8d772829831b6a86816d9d50698c0cc93ed2fc4c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-15 14:00:37 +0000

    2010-05-15  Charles Wei  <charles.wei@torchmobile.com.cn>
            Reviewed by Simon Hausmann.
    
            This patch fixes the build failure of WebKit on Linux for Qt4.5
            https://bugs.webkig.org/show_bug.cgi?id=39108
    
            * Api/qgraphicswebview.cpp:
            (QGraphicsWebViewPrivate::syncLayers):
            * WebCoreSupport/QtPlatformPlugin.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59539 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
+++ third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
@@ -58,9 +58,7 @@ public:
 
     virtual ~QGraphicsWebViewPrivate();
 
-#if USE(ACCELERATED_COMPOSITING)
     void syncLayers();
-#endif
 
     void updateResizesToContentsForPage();
     QRectF graphicsItemVisibleRect() const;
@@ -87,12 +85,12 @@ QGraphicsWebViewPrivate::~QGraphicsWebViewPrivate()
     detachCurrentPage();
 }
 
-#if USE(ACCELERATED_COMPOSITING)
 void QGraphicsWebViewPrivate::syncLayers()
 {
+#if USE(ACCELERATED_COMPOSITING)
     static_cast<PageClientQGraphicsWidget*>(page->d->client)->syncLayers();
-}
 #endif
+}
 
 void QGraphicsWebViewPrivate::_q_doLoadFinished(bool success)
 {
