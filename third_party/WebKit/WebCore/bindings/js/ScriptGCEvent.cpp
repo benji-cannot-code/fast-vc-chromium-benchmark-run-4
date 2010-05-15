fb606aef22b91e8a7b0347c50ad1dbcf54a71d72
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-15 08:46:23 +0000

    2010-05-15  Ilya Tikhonovsky  <loislo@chromium.org>
            Reviewed by Geoffrey Garen.
    
            WebInspector: JSC Should provide heap size info for Timeline panel.
            https://bugs.webkit.org/show_bug.cgi?id=38420
    
            * JavaScriptCore.exp:
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def:
            * runtime/Collector.cpp:
            (JSC::Heap::size):
            * runtime/Collector.h:
    2010-05-15  Ilya Tikhonovsky  <loislo@chromium.org>
    
            Reviewed by Geoffrey Garen.
    
            WebInspector: JSC Should provide heap size info for Timeline panel.
            https://bugs.webkit.org/show_bug.cgi?id=38420
    
            * GNUmakefile.am:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * bindings/js/ScriptGCEvent.cpp: Added.
            (WebCore::ScriptGCEvent::getHeapSize):
            * bindings/js/ScriptGCEvent.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59526 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/ScriptGCEvent.cpp
+++ third_party/WebKit/WebCore/bindings/js/ScriptGCEvent.cpp
@@ -29,24 +29,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef ScriptGCEvent_h
-#define ScriptGCEvent_h
+#include "config.h"
+#include "ScriptGCEvent.h"
 
 #if ENABLE(INSPECTOR)
 
+#include "JSDOMWindow.h"
+#include <runtime/Collector.h>
+#include <runtime/JSGlobalData.h>
+#include <wtf/CurrentTime.h>
+
 namespace WebCore {
 
-class ScriptGCEventListener;
+using namespace JSC;
 
-class ScriptGCEvent
+void ScriptGCEvent::getHeapSize(size_t& usedHeapSize, size_t& totalHeapSize)
 {
-public:
-    static void addEventListener(ScriptGCEventListener*) { }
-    static void removeEventListener(ScriptGCEventListener*) { }
-    static void getHeapSize(size_t& usedHeapSize, size_t& totalHeapSize);
-};
-
+    JSGlobalData* globalData = JSDOMWindow::commonJSGlobalData();
+    totalHeapSize = globalData->heap.size();
+    usedHeapSize = totalHeapSize;
+}
 } // namespace WebCore
 
 #endif // !ENABLE(INSPECTOR)
-#endif // !defined(ScriptGCEvent_h)
