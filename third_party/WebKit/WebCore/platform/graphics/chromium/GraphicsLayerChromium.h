08f27169390abec7535c56d5fb518a4da2b6c899
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-15 20:03:48 +0000

    2010-05-15  Vangelis Kokkevis  <vangelis@chromium.org>
            Reviewed by Darin Fisher.
    
            Adding code path for composited layers (LayerChromium) containing only
            an Image element.
            https://bugs.webkit.org/show_bug.cgi?id=39142
    
            No new tests, covered under the existing tests for composited layers.
    
            * platform/graphics/chromium/GraphicsLayerChromium.cpp:
            (WebCore::GraphicsLayerChromium::setContentsToImage):
            (WebCore::GraphicsLayerChromium::updateContentsImage):
            * platform/graphics/chromium/GraphicsLayerChromium.h:
            * platform/graphics/chromium/LayerChromium.cpp:
            (WebCore::LayerChromium::LayerChromium):
            (WebCore::LayerChromium::setContents):
            * platform/graphics/chromium/LayerChromium.h:
            (WebCore::LayerChromium::contents):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/chromium/GraphicsLayerChromium.h
+++ third_party/WebKit/WebCore/platform/graphics/chromium/GraphicsLayerChromium.h
@@ -141,6 +141,8 @@ private:
 
     ContentsLayerPurpose m_contentsLayerPurpose;
     bool m_contentsLayerHasBackgroundColor : 1;
+
+    NativeImagePtr m_pendingContentsImage;
 };
 
 } // namespace WebCore
