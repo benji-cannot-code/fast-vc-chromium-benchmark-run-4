842e2b9493574697cd86205288a07f194c185e0f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-16 07:58:48 +0000

    2010-05-16  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            Add a test to check proper return value (null) from invalid canvas getContext() calls.
    
            https://bugs.webkit.org/show_bug.cgi?id=39150
    
            * fast/canvas/canvas-getContext-invalid-expected.txt: Added.
            * fast/canvas/canvas-getContext-invalid.html: Added.
            * fast/canvas/script-tests/canvas-getContext-invalid.js: Added.
    2010-05-16  Andreas Kling  <andreas.kling@nokia.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            Canvas's getContext() must return null when called with an invalid/unsupported parameter.
            (HTML5 spec 4.8.11): http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#dom-canvas-getcontext
    
            https://bugs.webkit.org/show_bug.cgi?id=39150
    
            Test: fast/canvas/canvas-getContext-invalid.html
    
            * bindings/js/JSHTMLCanvasElementCustom.cpp:
            (WebCore::JSHTMLCanvasElement::getContext):
            * bindings/v8/custom/V8HTMLCanvasElementCustom.cpp:
            (WebCore::V8HTMLCanvasElement::getContextCallback):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59569 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-getContext-invalid.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test that invalid canvas getContext() requests return null.");
+
+canvas = document.createElement('canvas');
+
+shouldBe("canvas.getContext('')", "null");
+shouldBe("canvas.getContext('2d#')", "null");
+shouldBe("canvas.getContext('This is clearly not a valid context name.')", "null");
+shouldBe("canvas.getContext('2d\0')", "null");
+shouldBe("canvas.getContext('2\uFF44')", "null");
+shouldBe("canvas.getContext('2D')", "null");
+
+var successfullyParsed = true;
--- third_party/WebKit/WebCore/bindings/v8/custom/V8HTMLCanvasElementCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8HTMLCanvasElementCustom.cpp
@@ -79,7 +79,7 @@ v8::Handle<v8::Value> V8HTMLCanvasElement::getContextCallback(const v8::Argument
 #endif
     CanvasRenderingContext* result = imp->getContext(contextId, attrs.get());
     if (!result)
-        return v8::Undefined();
+        return v8::Null();
     if (result->is2d())
         return toV8(static_cast<CanvasRenderingContext2D*>(result));
 #if ENABLE(3D_CANVAS)
@@ -87,7 +87,7 @@ v8::Handle<v8::Value> V8HTMLCanvasElement::getContextCallback(const v8::Argument
         return toV8(static_cast<WebGLRenderingContext*>(result));
 #endif
     ASSERT_NOT_REACHED();
-    return v8::Undefined();
+    return v8::Null();
 }
 
 } // namespace WebCore
