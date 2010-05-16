23052033d668988906ea4c34f15f0b67c52fad7a
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-16 21:46:10 +0000

    2010-05-16  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            Add a test to verify proper handling of invalid arguments to getImageData().
            Updated old putImageData() test with correct expectations.
    
            https://bugs.webkit.org/show_bug.cgi?id=39175
    
            Spec link:
            http://www.whatwg.org/specs/web-apps/current-work/#pixel-manipulation
    
            * fast/canvas/canvas-getImageData-invalid-expected.txt: Added.
            * fast/canvas/canvas-getImageData-invalid.html: Added.
            * fast/canvas/canvas-putImageData-expected.txt:
            * fast/canvas/canvas-putImageData.js:
            * fast/canvas/script-tests/canvas-getImageData-invalid.js: Added.
    2010-05-16  Andreas Kling  <andreas.kling@nokia.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            Properly handle invalid arguments to CanvasRenderingContext2D's getImageData() and putImageData().
            Both should throw NOT_SUPPORTED_ERR when called with nonfinite arguments.
            getImageData() should throw INDEX_SIZE_ERR if either width or height is 0.
    
            https://bugs.webkit.org/show_bug.cgi?id=39175
    
            Spec link:
            http://www.whatwg.org/specs/web-apps/current-work/#pixel-manipulation
    
            Test: fast/canvas/canvas-getImageData-invalid.html
    
            * html/canvas/CanvasRenderingContext2D.cpp:
            (WebCore::CanvasRenderingContext2D::createImageData):
            (WebCore::CanvasRenderingContext2D::getImageData):
            (WebCore::CanvasRenderingContext2D::putImageData):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59582 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -1346,12 +1346,12 @@ static PassRefPtr<ImageData> createEmptyImageData(const IntSize& size)
 PassRefPtr<ImageData> CanvasRenderingContext2D::createImageData(float sw, float sh, ExceptionCode& ec) const
 {
     ec = 0;
-    if (!isfinite(sw) || !isfinite(sh)) {
-        ec = NOT_SUPPORTED_ERR;
-        return 0;
-    }
     if (!sw || !sh) {
         ec = INDEX_SIZE_ERR;
+        return 0;
+    }
+    if (!isfinite(sw) || !isfinite(sh)) {
+        ec = NOT_SUPPORTED_ERR;
         return 0;
     }
 
@@ -1371,7 +1371,15 @@ PassRefPtr<ImageData> CanvasRenderingContext2D::getImageData(float sx, float sy,
         ec = SECURITY_ERR;
         return 0;
     }
-    
+    if (!sw || !sh) {
+        ec = INDEX_SIZE_ERR;
+        return 0;
+    }
+    if (!isfinite(sx) || !isfinite(sy) || !isfinite(sw) || !isfinite(sh)) {
+        ec = NOT_SUPPORTED_ERR;
+        return 0;
+    }
+
     FloatRect unscaledRect(sx, sy, sw, sh);
     IntRect scaledRect = canvas()->convertLogicalToDevice(unscaledRect);
     if (scaledRect.width() < 1)
@@ -1402,7 +1410,7 @@ void CanvasRenderingContext2D::putImageData(ImageData* data, float dx, float dy,
     }
     if (!isfinite(dx) || !isfinite(dy) || !isfinite(dirtyX) || 
         !isfinite(dirtyY) || !isfinite(dirtyWidth) || !isfinite(dirtyHeight)) {
-        ec = INDEX_SIZE_ERR;
+        ec = NOT_SUPPORTED_ERR;
         return;
     }
 
