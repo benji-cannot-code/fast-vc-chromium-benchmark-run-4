5e10d8e7d74c46d36de7a2fc2acaed0eadb95188
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-17 03:34:48 +0000

    https://bugs.webkit.org/show_bug.cgi?id=39194
    Reviewed by Dan Bernstein.
    
    Fix all of the multi-column painting and hit testing functions to properly account for RTL directionality.
    
    Added fast/multicol/column-count-with-rules.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::paintColumnRules):
    (WebCore::RenderBlock::paintColumnContents):
    (WebCore::RenderBlock::hitTestColumns):
    (WebCore::RenderBlock::adjustRectForColumns):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBlock.cpp
@@ -1561,13 +1561,12 @@ void RenderBlock::paintColumnRules(PaintInfo& paintInfo, int tx, int ty)
         return;
 
     // We need to do multiple passes, breaking up our child painting into strips.
-    int currXOffset = 0;
-    int ruleAdd = borderLeft() + paddingLeft();
-    int ruleX = 0;
     Vector<IntRect>* colRects = columnRects();
     unsigned colCount = colRects->size();
+    int currXOffset = style()->direction() == LTR ? 0 : contentWidth();
+    int ruleAdd = borderLeft() + paddingLeft();
+    int ruleX = style()->direction() == LTR ? 0 : contentWidth();
     for (unsigned i = 0; i < colCount; i++) {
-        // For each rect, we clip to the rect, and then we adjust our coords.
         IntRect colRect = colRects->at(i);
 
         // Move to the next position.
@@ -1597,11 +1596,13 @@ void RenderBlock::paintColumnContents(PaintInfo& paintInfo, int tx, int ty, bool
 {
     // We need to do multiple passes, breaking up our child painting into strips.
     GraphicsContext* context = paintInfo.context;
-    int currXOffset = 0;
-    int currYOffset = 0;
     int colGap = columnGap();
     Vector<IntRect>* colRects = columnRects();
     unsigned colCount = colRects->size();
+    if (!colCount)
+        return;
+    int currXOffset = style()->direction() == LTR ? 0 : contentWidth() - colRects->at(0).width();
+    int currYOffset = 0;
     for (unsigned i = 0; i < colCount; i++) {
         // For each rect, we clip to the rect, and then we adjust our coords.
         IntRect colRect = colRects->at(i);
@@ -3369,11 +3370,14 @@ bool RenderBlock::hitTestColumns(const HitTestRequest& request, HitTestResult& r
     // We need to do multiple passes, breaking up our hit testing into strips.
     // We can always go left to right, since column contents are clipped (meaning that there
     // can't be any overlap).
-    int currXOffset = 0;
+    Vector<IntRect>* colRects = columnRects();
+    unsigned colCount = colRects->size();
+    if (!colCount)
+        return false;
+    int currXOffset = style()->direction() == LTR ? 0 : contentWidth() - colRects->at(0).width();
     int currYOffset = 0;
     int colGap = columnGap();
-    Vector<IntRect>* colRects = columnRects();
-    for (unsigned i = 0; i < colRects->size(); i++) {
+    for (unsigned i = 0; i < colCount; i++) {
         IntRect colRect = colRects->at(i);
         colRect.move(tx, ty);
         
@@ -3898,10 +3902,13 @@ void RenderBlock::adjustRectForColumns(IntRect& r) const
     IntRect result;
     
     // Determine which columns we intersect.
-    int currXOffset = 0;
+    unsigned colCount = colRects->size();
+    if (!colCount)
+        return;
+    int currXOffset = style()->direction() == LTR ? 0 : contentWidth() - colRects->at(0).width();
     int currYOffset = 0;
     int colGap = columnGap();
-    for (unsigned i = 0; i < colRects->size(); i++) {
+    for (unsigned i = 0; i < colCount; i++) {
         IntRect colRect = colRects->at(i);
         
         IntRect repaintRect = r;
