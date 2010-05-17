cef6008854a3aff77880ee13e8924206bd26a552
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-17 17:32:46 +0000

    2010-05-17  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            Add a test to verify proper behavior of createImageData(ImageData)
            and createImageData(width, height) with negative arguments.
            https://bugs.webkit.org/show_bug.cgi?id=39189
    
            Spec link:
            http://www.whatwg.org/specs/web-apps/current-work/#dom-context-2d-createimagedata
    
            * fast/canvas/canvas-createImageData-expected.txt: Added.
            * fast/canvas/canvas-createImageData.html: Added.
            * fast/canvas/script-tests/canvas-createImageData.js: Added.
    2010-05-17  Andreas Kling  <andreas.kling@nokia.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            Bring CanvasRenderingContext2D's createImageData() in line with HTML5 spec
            Added createImageData(ImageData) which returns a new ImageData with the same size as the one passed.
            Changed createImageData(width, height) to use the absolute values of width and height.
    
            https://bugs.webkit.org/show_bug.cgi?id=39189
    
            Spec link:
            http://www.whatwg.org/specs/web-apps/current-work/#dom-context-2d-createimagedata
    
            Test: fast/canvas/canvas-createImageData.html
    
            * bindings/js/JSCanvasRenderingContext2DCustom.cpp:
            (WebCore::JSCanvasRenderingContext2D::createImageData):
            * html/canvas/CanvasRenderingContext2D.cpp:
            (WebCore::CanvasRenderingContext2D::createImageData):
            * html/canvas/CanvasRenderingContext2D.h:
            * html/canvas/CanvasRenderingContext2D.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59613 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -1343,6 +1343,17 @@ static PassRefPtr<ImageData> createEmptyImageData(const IntSize& size)
     return data.get();
 }
 
+PassRefPtr<ImageData> CanvasRenderingContext2D::createImageData(PassRefPtr<ImageData> imageData, ExceptionCode& ec) const
+{
+    if (!imageData) {
+        ec = NOT_SUPPORTED_ERR;
+        return 0;
+    }
+
+    IntSize size(imageData->width(), imageData->height());
+    return createEmptyImageData(size);
+}
+
 PassRefPtr<ImageData> CanvasRenderingContext2D::createImageData(float sw, float sh, ExceptionCode& ec) const
 {
     ec = 0;
@@ -1355,7 +1366,7 @@ PassRefPtr<ImageData> CanvasRenderingContext2D::createImageData(float sw, float 
         return 0;
     }
 
-    FloatSize unscaledSize(sw, sh);
+    FloatSize unscaledSize(fabs(sw), fabs(sh));
     IntSize scaledSize = canvas()->convertLogicalToDevice(unscaledSize);
     if (scaledSize.width() < 1)
         scaledSize.setWidth(1);
