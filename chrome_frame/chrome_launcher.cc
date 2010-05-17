a3df3b9b5f38dee390b4d3074330941ece7bb7aa
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-17 16:24:42 +0000

    Perform breakpad shutdown when returning from the chrome launcher entry point. This is a to avoid the deadlock that occurs when shutting it down in DllMain in chrome_launcher.exe.
    BUG=44212
    TEST=chrome_launcher.exe stops hanging
    
    Review URL: http://codereview.chromium.org/2094006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_launcher.cc
+++ chrome_frame/chrome_launcher.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome_frame/chrome_frame_automation.h"
+#include "chrome_frame/chrome_frame_reporting.h"
 
 namespace chrome_launcher {
 
@@ -119,11 +120,17 @@ FilePath GetChromeExecutablePath() {
 
 // Entrypoint that implements the logic of chrome_launcher.exe.
 int CALLBACK CfLaunchChrome() {
+  int result = ERROR_OPEN_FAILED;
+
   if (chrome_launcher::SanitizeAndLaunchChrome(::GetCommandLine())) {
-    return ERROR_SUCCESS;
-  } else {
-    return ERROR_OPEN_FAILED;
+    result = ERROR_SUCCESS;
   }
+
+  // Regardless of what just happened, shut down crash reporting now to avoid a
+  // hang when we are unloaded.
+  ShutdownCrashReporting();
+
+  return result;
 }
 
 // Compile-time check to see that the type CfLaunchChromeProc is correct.
