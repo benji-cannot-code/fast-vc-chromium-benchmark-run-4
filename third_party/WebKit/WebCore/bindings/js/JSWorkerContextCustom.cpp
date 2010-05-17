7b2cb163c2e06122140afa86d092872f0ced63e9
robert@webkit.org <robert@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-17 20:45:11 +0000

    2010-05-17  Robert Hogan  <robert@webkit.org>
            Rubber-stamped by Eric Seidel.
    
            Fix --no-svg build.
    
            I have no idea where the SVG-dependent include of this file is hidden, but
            it is probably better to include it explicitly anyway like V8WorkerContextCustom.cpp.
    
            * bindings/js/JSWorkerContextCustom.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSWorkerContextCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSWorkerContextCustom.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSDatabaseCallback.h"
 #include "JSDatabaseSync.h"
 #endif
+#include "ExceptionCode.h"
 #include "JSDOMBinding.h"
 #include "JSDOMGlobalObject.h"
 #include "JSEventListener.h"
