544caaf3af2b052a5e6b0f4dcf95858affa38ff2
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-17 15:04:14 +0000

    Call CARenderOGLPurge whenever we call IDirect3DDevice9::Reset
    MSDN says that all resoures allocated with D3DPOOL_DEFAULT must be
    destroyed before calling IDirect3DDevice9::Reset. The only way to
    guarantee this with Core Animation is to call CARenderOGLPurge.
    
    Unfortunately this isn't testable at the moment, as we don't use any
    features of Core Animation (e.g., mask layers) that actually make this
    required. But it seems like a good idea to fix the code now so that
    if/when we do start using those features we won't run into problems.
    
    Fixes <http://webkit.org/b/39159> WKCACFLayerRenderer::resetDevice
    might fail due to failing to destroy all D3DPOOL_DEFAULT resources
    
    Reviewed by John Sullivan.
    
    * platform/graphics/win/WKCACFLayerRenderer.cpp:
    (WebCore::WKCACFLayerRenderer::render): Moved the call to
    CARenderOGLPurge from here...
    (WebCore::WKCACFLayerRenderer::resetDevice): ...to here, so that it
    will be called whenever we reset the device (e.g., when resizing the
    window).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayerRenderer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayerRenderer.cpp
@@ -549,7 +549,6 @@ void WKCACFLayerRenderer::render(const Vector<CGRect>& dirtyRects)
 
         if (err == D3DERR_DEVICELOST) {
             // Lost device situation.
-            CARenderOGLPurge(m_renderer);
             resetDevice();
             CARenderUpdateAddRect(u, &bounds);
         }
@@ -597,6 +596,11 @@ void WKCACFLayerRenderer::initD3DGeometry()
 void WKCACFLayerRenderer::resetDevice()
 {
     ASSERT(m_d3dDevice);
+
+    // We have to purge the CARenderOGLContext whenever we reset the IDirect3DDevice9 in order to
+    // destroy any D3DPOOL_DEFAULT resources that Core Animation has allocated (e.g., textures used
+    // for mask layers). See <http://msdn.microsoft.com/en-us/library/bb174425(v=VS.85).aspx>.
+    CARenderOGLPurge(m_renderer);
 
     D3DPRESENT_PARAMETERS parameters = initialPresentationParameters();
     m_d3dDevice->Reset(&parameters);
