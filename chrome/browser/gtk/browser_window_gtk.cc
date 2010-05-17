benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit a2e1bd7ca14fe456d5f5f9a5b7f1084a5c53d86d

Only place popup windows if the position is not 0,0.
BUG=36650

Review URL: http://codereview.chromium.org/1520019

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43790 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/gtk/browser_window_gtk.cc
@@ -1399,10 +1399,15 @@ void BrowserWindowGtk::SetGeometryHints() {
   // force the position as part of session restore, as applications
   // that restore other, similar state (for instance GIMP, audacity,
   // pidgin, dia, and gkrellm) do tend to restore their positions.
-  if (browser_->bounds_overridden()) {
+  //
+  // For popup windows, we assume that if x == y == 0, the opening page
+  // did not specify a position.  Let the WM position the popup instead.
+  bool is_popup = browser_->type() & Browser::TYPE_POPUP;
+  bool popup_without_position = is_popup && bounds.x() == 0 && bounds.y() == 0;
+  if (browser_->bounds_overridden() && !popup_without_position) {
     // For popups, bounds are set in terms of the client area rather than the
     // entire window.
-    SetBoundsImpl(bounds, !(browser_->type() & Browser::TYPE_POPUP));
+    SetBoundsImpl(bounds, !is_popup);
   } else {
     // Ignore the position but obey the size.
     SetWindowSize(window_, bounds.width(), bounds.height());

commit 1704e2267ced1576ab8419e9ec8cf36610d29dc0

GTK: size popup windows by their content, not outer bounds.
This regressed in r43790 (my fault, as I reviewed it). There used to be a test that covered this functionality, but the test was removed in r37819.

BUG=44107
TEST=none.

Review URL: http://codereview.chromium.org/2109004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47429 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/gtk/browser_window_gtk.cc
@@ -644,7 +644,12 @@ void BrowserWindowGtk::SetBoundsImpl(const gfx::Rect& bounds, bool exterior) {
   gint width = static_cast<gint>(bounds.width());
   gint height = static_cast<gint>(bounds.height());
 
-  gtk_window_move(window_, x, y);
+  // For popup windows, we assume that if x == y == 0, the opening page
+  // did not specify a position.  Let the WM position the popup instead.
+  bool popup_without_position = browser_->type() & Browser::TYPE_POPUP &&
+      bounds.x() == 0 && bounds.y() == 0;
+  if (!popup_without_position)
+    gtk_window_move(window_, x, y);
 
   if (exterior) {
     SetWindowSize(window_, width, height);
@@ -1443,12 +1448,9 @@ void BrowserWindowGtk::SetGeometryHints() {
   // force the position as part of session restore, as applications
   // that restore other, similar state (for instance GIMP, audacity,
   // pidgin, dia, and gkrellm) do tend to restore their positions.
-  //
-  // For popup windows, we assume that if x == y == 0, the opening page
-  // did not specify a position.  Let the WM position the popup instead.
+
   bool is_popup = browser_->type() & Browser::TYPE_POPUP;
-  bool popup_without_position = is_popup && bounds.x() == 0 && bounds.y() == 0;
-  if (browser_->bounds_overridden() && !popup_without_position) {
+  if (browser_->bounds_overridden()) {
     // For popups, bounds are set in terms of the client area rather than the
     // entire window.
     SetBoundsImpl(bounds, !is_popup);