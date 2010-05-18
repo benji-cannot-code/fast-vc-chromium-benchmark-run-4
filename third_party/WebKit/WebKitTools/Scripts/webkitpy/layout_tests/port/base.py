684eb8c287fbc30ae21c99937790f64f4f973e7f
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-18 10:40:50 +0000

    2010-05-18  Fumitoshi Ukai  <ukai@chromium.org>
            Reviewed by Eric Seidel.
    
            run_webkit_tests_unittest fails on SnowLeopard
            https://bugs.webkit.org/show_bug.cgi?id=39279
    
            * Scripts/webkitpy/layout_tests/port/base.py:
             Return copy of os.environ.
            * Scripts/webkitpy/layout_tests/port/chromium_win_unittest.py:
             Check os.environ was not modified.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59666 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/base.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/base.py
@@ -424,7 +424,7 @@ class Port(object):
         Returns:
            Operating-system's environment.
         """
-        return os.environ
+        return os.environ.copy()
 
     def show_html_results_file(self, results_filename):
         """This routine should display the HTML file pointed at by
