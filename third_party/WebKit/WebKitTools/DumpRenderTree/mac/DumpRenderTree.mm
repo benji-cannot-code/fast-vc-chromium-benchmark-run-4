d0c58addd9b26ef75e49e12feb4e01af802783d9
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-18 20:16:25 +0000

    2010-05-18  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Roben.
    
            Add an --html5-parser option to DumpRenderTree to allow testing the new HTML5Lexer
            https://bugs.webkit.org/show_bug.cgi?id=39311
    
            This flag allows us to run the new HTML5Lexer code.
            Right now all documents parse as empty documents, but
            now that we're able to run the code we can fix that.
    
            Once we're able to lex a few basic documents I'll add
            an --html5-parser flag to run-webkit-tests so that we test
            running all of the layout tests with the HTML5 parser.
    
            * DumpRenderTree/mac/DumpRenderTree.mm:
            (resetDefaultsToConsistentValues):
            (initializeGlobalsFromCommandLineOptions):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/DumpRenderTree.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/DumpRenderTree.mm
@@ -136,6 +136,7 @@ static int dumpPixels;
 static int threaded;
 static int dumpTree = YES;
 static int forceComplexText;
+static int useHTML5Parser;
 static BOOL printSeparators;
 static RetainPtr<CFStringRef> persistentUserStyleSheetLocation;
 
@@ -452,6 +453,7 @@ static void resetDefaultsToConsistentValues()
     [preferences setUsesPageCache:NO];
     [preferences setAcceleratedCompositingEnabled:YES];
     [preferences setWebGLEnabled:NO];
+    [preferences setHTML5ParserEnabled:useHTML5Parser];
 
     [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookieAcceptPolicy:NSHTTPCookieAcceptPolicyOnlyFromMainDocumentDomain];
 
@@ -558,6 +560,7 @@ static void initializeGlobalsFromCommandLineOptions(int argc, const char *argv[]
         {"tree", no_argument, &dumpTree, YES},
         {"threaded", no_argument, &threaded, YES},
         {"complex-text", no_argument, &forceComplexText, YES},
+        {"html5-parser", no_argument, &useHTML5Parser, YES},
         {NULL, 0, NULL, 0}
     };
     
