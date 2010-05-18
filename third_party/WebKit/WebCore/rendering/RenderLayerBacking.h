8671fcc130d9b6c1cf61ba5900e35225ac609eb4
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-18 21:37:26 +0000

    2010-05-18  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Darin Adler.
    
            Clean up GraphicsLayer naming code
            https://bugs.webkit.org/show_bug.cgi?id=39316
    
            Move code that generates a name for the GraphicsLayer (used in debug builds only) into
            a new nameForLayer() method. Use it when naming the foreground layer.
    
            * rendering/RenderLayerBacking.cpp:
            (WebCore::RenderLayerBacking::createGraphicsLayer):
            (WebCore::RenderLayerBacking::updateForegroundLayer):
            (WebCore::RenderLayerBacking::nameForLayer):
            * rendering/RenderLayerBacking.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59711 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayerBacking.h
+++ third_party/WebKit/WebCore/rendering/RenderLayerBacking.h
@@ -170,6 +170,10 @@ private:
     static int graphicsLayerToCSSProperty(AnimatedPropertyID);
     static AnimatedPropertyID cssToGraphicsLayerProperty(int);
 
+#ifndef NDEBUG
+    String nameForLayer() const;
+#endif
+
 private:
     RenderLayer* m_owningLayer;
 
