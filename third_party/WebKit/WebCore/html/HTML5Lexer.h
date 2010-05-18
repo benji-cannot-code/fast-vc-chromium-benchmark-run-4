60a1f6430e3f68eb42eeb6a0867a76b76aa510f6
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-18 07:24:09 +0000

    2010-05-18  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Implement ScriptData states for HTML5Lexer
            https://bugs.webkit.org/show_bug.cgi?id=39273
    
            This patch implements more of the lexer states according to the HTML5
            specification.  Currently, this code is compiled by not tested.  We're
            working on bringing up a test harness in parallel with transliterating
            the spec into code.
    
            * html/HTML5Lexer.cpp:
            (WebCore::HTML5Lexer::tokenize):
            (WebCore::HTML5Lexer::temporaryBufferIs):
            * html/HTML5Lexer.h:
            (WebCore::HTML5Lexer::):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTML5Lexer.h
+++ third_party/WebKit/WebCore/html/HTML5Lexer.h
@@ -55,6 +55,8 @@ namespace WebCore {
         inline void emitParseError();
         void processAttribute();
 
+        inline bool temporaryBufferIs(const char*);
+
         void clearLastCharacters();
         void rememberCharacter(UChar);
         bool lastCharactersMatch(const char*, unsigned count) const;
@@ -88,7 +90,7 @@ namespace WebCore {
             ScriptDataEscapedDashDashState,
             ScriptDataEscapedLessThanSignState,
             ScriptDataEscapedEndTagOpenState,
-            ScriptDataEscapedEndtagNameState,
+            ScriptDataEscapedEndTagNameState,
             ScriptDataDoubleEscapeStartState,
             ScriptDataDoubleEscapedState,
             ScriptDataDoubleEscapedDashState,
