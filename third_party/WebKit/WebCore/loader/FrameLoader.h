7dab1b488f03282167505a4fe2deba14d69ea832
cjerdonek@webkit.org <cjerdonek@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-18 16:18:31 +0000

    2010-05-18  Chris Jerdonek  <cjerdonek@webkit.org>
            Reviewed by Darin Adler.
    
            Refactored FrameLoader::isDocumentSandboxed() from a private member function
            to a static, non-member, non-friend function.
    
            https://bugs.webkit.org/show_bug.cgi?id=39157
    
            Making this function non-private lets us make FrameLoader::createWindow()
            a non-member function in a subsequent patch.  We also made it non-member
            rather than member to increase encapsulation and limit the number of
            public functions with access to private FrameLoader data.
    
            No new functionality, so no new tests.
    
            * loader/FrameLoader.cpp:
            (WebCore::isDocumentSandboxed):
              - Moved this function from a private member function of FrameLoader
                to a static, non-member, non-friend function.
            (WebCore::FrameLoader::createWindow):
              - Updated the call to isDocumentSandboxed().
            (WebCore::FrameLoader::submitForm):
              - Updated the call to isDocumentSandboxed().
            (WebCore::FrameLoader::requestObject):
              - Updated the call to isDocumentSandboxed().
            (WebCore::FrameLoader::shouldAllowNavigation):
              - Updated the call to isDocumentSandboxed().
            * loader/FrameLoader.h:
              - Removed isDocumentSandboxed() and added a FIXME to move
                createWindow() out of the FrameLoader class.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59670 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/WebCore/loader/FrameLoader.h
@@ -126,6 +126,7 @@ public:
     static void reportLocalLoadFailed(Frame*, const String& url);
 
     // Called by createWindow in JSDOMWindowBase.cpp, e.g. to fulfill a modal dialog creation
+    // FIXME: Move this method outside of the FrameLoader class.
     Frame* createWindow(FrameLoader* frameLoaderForFrameLookup, const FrameLoadRequest&, const WindowFeatures&, bool& created);
 
     unsigned long loadResourceSynchronously(const ResourceRequest&, StoredCredentials, ResourceError&, ResourceResponse&, Vector<char>& data);
@@ -463,8 +464,6 @@ private:
     bool shouldTreatURLAsSameAsCurrent(const KURL&) const;
 
     void updateSandboxFlags();
-    // FIXME: isDocumentSandboxed should eventually replace isSandboxed.
-    bool isDocumentSandboxed(SandboxFlags) const;
 
     Frame* m_frame;
     FrameLoaderClient* m_client;
