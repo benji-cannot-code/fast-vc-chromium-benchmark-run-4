8532354bfc765a95abb4723a863b2373d0b39be7
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-18 13:06:38 +0000

    gtk: Make use of gtk signal macros in FirstRunDialog.
    BUG=None
    TEST=manually. Run out/Debug/chrome --first-run. Everything should works as before.
    
    Review URL: http://codereview.chromium.org/1998013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47507 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/first_run_dialog.h
+++ chrome/browser/gtk/first_run_dialog.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef struct _GtkButton GtkButton;
 typedef struct _GtkWidget GtkWidget;
 
+#include "app/gtk_signal.h"
 #include "chrome/browser/first_run.h"
 #include "chrome/browser/importer/importer.h"
 
@@ -17,7 +18,7 @@ class FirstRunDialog : public ImportObserver {
   // Displays the first run UI for reporting opt-in, import data etc.
   static bool Show(Profile* profile, ProcessSingleton* process_singleton);
 
-  // Overridden methods from ImportObserver.
+  // Overridden from ImportObserver:
   virtual void ImportCanceled() {
     FirstRunDone();
   }
@@ -27,16 +28,10 @@ class FirstRunDialog : public ImportObserver {
 
  private:
   FirstRunDialog(Profile* profile, int& response);
-  ~FirstRunDialog() { }
+  ~FirstRunDialog() {}
 
-  static void HandleOnResponseDialog(GtkWidget* widget,
-                                     int response,
-                                     FirstRunDialog* user_data) {
-    user_data->OnDialogResponse(widget, response);
-  }
-  void OnDialogResponse(GtkWidget* widget, int response);
-  static void OnLearnMoreLinkClicked(GtkButton *button,
-                                     FirstRunDialog* first_run);
+  CHROMEGTK_CALLBACK_1(FirstRunDialog, void, OnResponseDialog, int);
+  CHROMEG_CALLBACK_0(FirstRunDialog, void, OnLearnMoreLinkClicked, GtkButton*);
 
   // This method closes the first run window and quits the message loop so that
   // the Chrome startup can continue. This should be called when all the
