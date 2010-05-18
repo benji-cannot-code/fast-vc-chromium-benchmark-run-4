4d741f270ab016a510fe66f98800893f2baee372
glotov@chromium.org <glotov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-18 21:20:13 +0000

    Small fix: volume up and down are mixed up.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/2107011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47569 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system_key_event_listener.cc
+++ chrome/browser/chromeos/system_key_event_listener.cc
@@ -54,10 +54,10 @@ void SystemKeyEventListener::ProcessWmMessage(const WmIpc::Message& message,
       RunCommand(kMuteAudioCommand);
       break;
     case 1:
-      RunCommand(kIncreaseVolumeLevelCommand);
+      RunCommand(kDecreaseVolumeLevelCommand);
       break;
     case 2:
-      RunCommand(kDecreaseVolumeLevelCommand);
+      RunCommand(kIncreaseVolumeLevelCommand);
       break;
     default:
       DLOG(ERROR) << "SystemKeyEventListener: Unexpected message "
@@ -76,4 +76,3 @@ void SystemKeyEventListener::RunCommand(std::string command) {
 }
 
 }  // namespace chromeos
-
