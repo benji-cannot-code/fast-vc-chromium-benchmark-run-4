047e8a34a74d76709ea2f6a3f470073ca29c2320
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-18 10:38:47 +0000

    2010-05-18  Eric Seidel  <eric@webkit.org>
            Unreviewed build fix.
    
            Make it possible to enable the new HTML5Tokenizer for testing
            https://bugs.webkit.org/show_bug.cgi?id=39275
    
            * dom/NamedAttrMap.cpp:
            (WebCore::NamedNodeMap::setNamedItemNS):
             - Move this to the .cpp file so as to avoid needing to include "Node.h" in the header.
            * dom/NamedAttrMap.h:
            * html/HTML5Token.h:
             - Remove #include "String.h" which was wrong.  Adam meant "PlatformString.h"
             - Remove unneeded #include "Node.h", it was only needed because
               NamedAttrMap.h was incorrectly depending on Node.h but not
               including it.  It didn't actually need Node.h except for one
               inlined method setNamedItemNS, so I moved setNamedItemNS into
               the .cpp to get rid of the Node.h requirement.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59665 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTML5Token.h
+++ third_party/WebKit/WebCore/html/HTML5Token.h
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HTML5Token_h
 
 #include "NamedMappedAttrMap.h"
-#include "Node.h"
-#include "String.h"
 #include <wtf/Noncopyable.h>
 
 namespace WebCore {
