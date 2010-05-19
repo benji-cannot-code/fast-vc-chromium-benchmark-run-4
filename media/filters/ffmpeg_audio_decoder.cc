7b62ff08c086079fbb65c59577b834d320530d84
jiesun@google.com <jiesun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-19 00:26:55 +0000

    refactoring engine interface for openmax.
    currently only fillbufferdone callback and emptythisbuffer is used.
    
    Review URL: http://codereview.chromium.org/2095002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47603 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_audio_decoder.cc
+++ media/filters/ffmpeg_audio_decoder.cc
@@ -155,9 +155,7 @@ static void ConvertAudioF32ToS32(void* buffer, int buffer_size) {
   }
 }
 
-void FFmpegAudioDecoder::DoDecode(Buffer* input, Task* done_cb) {
-  AutoTaskRunner done_runner(done_cb);
-
+void FFmpegAudioDecoder::DoDecode(Buffer* input) {
   // Due to FFmpeg API changes we no longer have const read-only pointers.
   AVPacket packet;
   av_init_packet(&packet);
@@ -186,6 +184,7 @@ void FFmpegAudioDecoder::DoDecode(Buffer* input, Task* done_cb) {
               << input->GetDuration().InMicroseconds() << " us"
               << " , packet size: "
               << input->GetDataSize() << " bytes";
+    DecoderBase<AudioDecoder, Buffer>::OnDecodeComplete();
     return;
   }
 
@@ -216,6 +215,7 @@ void FFmpegAudioDecoder::DoDecode(Buffer* input, Task* done_cb) {
     }
 
     EnqueueResult(result_buffer);
+    DecoderBase<AudioDecoder, Buffer>::OnDecodeComplete();
     return;
   }
 
@@ -226,6 +226,7 @@ void FFmpegAudioDecoder::DoDecode(Buffer* input, Task* done_cb) {
       input->GetTimestamp() != StreamSample::kInvalidTimestamp &&
       input->GetDuration() != StreamSample::kInvalidTimestamp) {
     estimated_next_timestamp_ = input->GetTimestamp() + input->GetDuration();
+    DecoderBase<AudioDecoder, Buffer>::OnDecodeComplete();
     return;
   }
 
@@ -239,6 +240,7 @@ void FFmpegAudioDecoder::DoDecode(Buffer* input, Task* done_cb) {
     result_buffer->SetDuration(input->GetDuration());
     EnqueueResult(result_buffer);
   }
+  DecoderBase<AudioDecoder, Buffer>::OnDecodeComplete();
 }
 
 base::TimeDelta FFmpegAudioDecoder::CalculateDuration(size_t size) {
