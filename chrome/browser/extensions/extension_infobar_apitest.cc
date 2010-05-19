55bebf3afd3951c5bceb62167d418ff602d4272f
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-19 06:26:52 +0000

    [GTTF] [TTF] Test marks maintenance CL:
    * The following tests are no longer flaky:
    
        BrowserTest.PageLanguageDetection (bug 42095)
        AppApiTest.AppProcess (bug 42497)
        ExtensionApiTest.Infobars (bug 40141)
        FindInPageControllerTest.FindMovesWhenObscuring (bug 41124)
        SessionRestoreUITest.TwoWindowsCloseOneRestoreOnlyOne (bug 39905)
        RenderViewTest.OnHandleKeyboardEvent (no bug)
        RenderViewTest.DidFailProvisionalLoadWithErrorForError (no bug)
        AutomatedUITestBase.CloseTab (bug 14774)
        AutomatedUITestBase.CloseBrowserWindow (bug 14774)
        PluginTest.FlashLayoutWhilePainting (bug 21538)
        HistoryTester.VerifyHistoryLength1 (bug 39785)
        HistoryTester.ConsiderRedirectAfterGestureAsUserInitiated (bug 39785)
        HistoryTester.ConsiderSlowRedirectAsUserInitiated (bug 39785)
    
    * The following tests are not flaky, but disabled due to hangs:
    
        BrowserAppRefocusTest.OpenTab (bug 44026)
    
    * The following tests are not flaky, but seem to fail consistently:
    
        ProcessSingletonLinuxTest.NotifyOtherProcessSuccess (bug 30953)
        ProcessSingletonLinuxTest.NotifyOtherProcessHostChanged (bug 30953)
        BrowserViewsAccessibilityTest.TestChromeWindowAccObj (bug 44486)
    
    BUG=42095, 44026, 42497, 40141, 41124, 30953, 39905, 14774, 21538, 39785, 44486
    TEST=this touches a lot of tests
    
    Review URL: http://codereview.chromium.org/2128009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47633 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_infobar_apitest.cc
+++ chrome/browser/extensions/extension_infobar_apitest.cc
@@ -8,13 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 
 #if defined(TOOLKIT_VIEWS)
-#if defined (OS_WIN)
 #define MAYBE_Infobars Infobars
-#else
-// Flaky on ChromeOS, see http://crbug.com/40141.
-#define MAYBE_Infobars FLAKY_Infobars
-#endif
-
 #else
 // Need to port ExtensionInfoBarDelegate::CreateInfoBar() to other platforms.
 // See http://crbug.com/39916 for details.
