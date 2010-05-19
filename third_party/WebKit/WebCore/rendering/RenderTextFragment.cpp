6e2037fcb2e014621710c7ec24bf9a2e7c357eab
yaar@chromium.org <yaar@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-19 23:59:08 +0000

    2010-05-19  Abhishek Arya  <inferno@chromium.org>
            Reviewed by David Hyatt.
    
            Check that the node is a text node before doing a static cast
            to a Text class pointer.
            https://bugs.webkit.org/show_bug.cgi?id=38626    
    
            Test: fast/text/text-transform-nontext-node-crash.xhtml
    
            * rendering/RenderText.cpp:
            (WebCore::RenderText::originalText):
            * rendering/RenderTextFragment.cpp:
            (WebCore::RenderTextFragment::originalText):
            (WebCore::RenderTextFragment::previousCharacter):
    2010-05-19  Abhishek Arya  <inferno@chromium.org>
    
            Reviewed by David Hyatt.
    
            Tests that text transformation applied to a non-text node
            does not result in crash.
            https://bugs.webkit.org/show_bug.cgi?id=38626        
    
            * fast/text/text-transform-nontext-node-crash-expected.txt: Added.
            * fast/text/text-transform-nontext-node-crash.xhtml: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTextFragment.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTextFragment.cpp
@@ -48,7 +48,7 @@ RenderTextFragment::RenderTextFragment(Node* node, StringImpl* str)
 PassRefPtr<StringImpl> RenderTextFragment::originalText() const
 {
     Node* e = node();
-    RefPtr<StringImpl> result = (e ? static_cast<Text*>(e)->dataImpl() : contentString());
+    RefPtr<StringImpl> result = ((e && e->isTextNode()) ? static_cast<Text*>(e)->dataImpl() : contentString());
     if (result && (start() > 0 || start() < result->length()))
         result = result->substring(start(), end());
     return result.release();
@@ -81,7 +81,7 @@ UChar RenderTextFragment::previousCharacter() const
 {
     if (start()) {
         Node* e = node();
-        StringImpl*  original = (e ? static_cast<Text*>(e)->dataImpl() : contentString());
+        StringImpl*  original = ((e && e->isTextNode()) ? static_cast<Text*>(e)->dataImpl() : contentString());
         if (original)
             return (*original)[start() - 1];
     }
