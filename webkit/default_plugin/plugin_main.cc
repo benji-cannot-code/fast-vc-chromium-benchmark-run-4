e068ec60a3e456fa0f41a7f07377627868bcd2cd
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-19 21:29:08 +0000

    Mac: First steps for default plugin
    BUG=17392
    TEST=Go to http://www.adobe.com/shockwave/welcome/. The task manager should show that the default plugin is running, and the "Missing Plug-in" text should look slightly different than it did before. (this test depends on a small webkit patch)
    
    Requires commenting out
    
        if (objectContentType(url, mimeType) != ObjectContentNetscapePlugin)
            return 0;
    
    in WebKit/WebKit/chromium/src/FrameLoaderClientImpl.cpp's createPlugin() as well, else the default plugin won't load.
    
    Review URL: http://codereview.chromium.org/2075006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47726 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/default_plugin/plugin_main.cc
+++ webkit/default_plugin/plugin_main.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/webkit_glue.h"
 
 namespace default_plugin {
+
+#if defined(OS_WIN)
 //
 // Forward declare the linker-provided pseudo variable for the
 // current module handle.
@@ -21,6 +23,7 @@ extern "C" IMAGE_DOS_HEADER __ImageBase;
 inline HMODULE GetCurrentModuleHandle() {
   return reinterpret_cast<HINSTANCE>(&__ImageBase);
 }
+#endif
 
 // Initialized in NP_Initialize.
 NPNetscapeFuncs* g_browser = NULL;
@@ -72,12 +75,52 @@ void SignalTestResult(NPP instance) {
       static_cast<unsigned int>(script.length());
 
   NPVariant result_var;
-  NPError result = g_browser->evaluate(instance, window_obj,
-                                       &script_string, &result_var);
+  g_browser->evaluate(instance, window_obj,
+                      &script_string, &result_var);
   g_browser->releaseobject(window_obj);
 }
 
 }  // namespace CHROMIUM_DefaultPluginTest
+
+bool NegotiateModels(NPP instance) {
+#if defined(OS_MACOSX)
+  NPError err;
+  // Set drawing model to core graphics
+  NPBool supportsCoreGraphics = FALSE;
+  err = g_browser->getvalue(instance,
+                            NPNVsupportsCoreGraphicsBool,
+                            &supportsCoreGraphics);
+  if (err != NPERR_NO_ERROR || !supportsCoreGraphics) {
+    NOTREACHED();
+    return false;
+  }
+  err = g_browser->setvalue(instance,
+                            NPPVpluginDrawingModel,
+                            (void*)NPDrawingModelCoreGraphics);
+  if (err != NPERR_NO_ERROR) {
+    NOTREACHED();
+    return false;
+  }
+
+  // Set event model to cocoa
+  NPBool supportsCocoaEvents = FALSE;
+  err = g_browser->getvalue(instance,
+                            NPNVsupportsCocoaBool,
+                            &supportsCocoaEvents);
+  if (err != NPERR_NO_ERROR || !supportsCocoaEvents) {
+    NOTREACHED();
+    return false;
+  }
+  err = g_browser->setvalue(instance,
+                            NPPVpluginEventModel,
+                            (void*)NPEventModelCocoa);
+  if (err != NPERR_NO_ERROR) {
+    NOTREACHED();
+    return false;
+  }
+#endif
+  return true;
+}
 
 NPError NPP_New(NPMIMEType plugin_type, NPP instance, uint16 mode, int16 argc,
                 char* argn[], char* argv[], NPSavedData* saved) {
@@ -96,10 +139,19 @@ NPError NPP_New(NPMIMEType plugin_type, NPP instance, uint16 mode, int16 argc,
     return NPERR_GENERIC_ERROR;
   }
 
-
+  if (!NegotiateModels(instance))
+    return NPERR_INCOMPATIBLE_VERSION_ERROR;
+  
   PluginInstallerImpl* plugin_impl = new PluginInstallerImpl(mode);
-  plugin_impl->Initialize(GetCurrentModuleHandle(), instance, plugin_type, argc,
+  plugin_impl->Initialize(
+#if defined(OS_WIN)
+                          GetCurrentModuleHandle(),
+#else
+                          NULL,
+#endif
+                          instance, plugin_type, argc,
                           argn, argv);
+
   instance->pdata = reinterpret_cast<void*>(plugin_impl);
   return NPERR_NO_ERROR;
 }
@@ -140,8 +192,7 @@ NPError NPP_SetWindow(NPP instance, NPWindow* window_info) {
     return NPERR_GENERIC_ERROR;
   }
 
-  HWND window_handle = reinterpret_cast<HWND>(window_info->window);
-  if (!plugin_impl->SetWindow(window_handle)) {
+  if (!plugin_impl->NPP_SetWindow(window_info)) {
     delete plugin_impl;
     return NPERR_GENERIC_ERROR;
   }
