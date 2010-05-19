3a6b6d96133a2cf21fb76e6c4f70677c3fbf60f0
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-19 02:26:46 +0000

    Fix the build for platforms that don't always include UnusedParam.h
    * dom/Attribute.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Attribute.cpp
+++ third_party/WebKit/WebCore/dom/Attribute.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Attr.h"
 #include "Element.h"
 #include <wtf/HashMap.h>
+#include <wtf/UnusedParam.h>
 
 namespace WebCore {
 
