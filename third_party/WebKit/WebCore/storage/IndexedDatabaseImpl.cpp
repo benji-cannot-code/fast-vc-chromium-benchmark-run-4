a4c551ab87d9dd5744e8384990b53bc204a6bbfe
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-19 10:17:53 +0000

    2010-05-19  Jeremy Orlow  <jorlow@chromium.org>
            Unreviewed build fix for Chromium.
    
            * storage/IndexedDatabaseImpl.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/IndexedDatabaseImpl.cpp
+++ third_party/WebKit/WebCore/storage/IndexedDatabaseImpl.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "IDBDatabase.h"
 #include "IDBDatabaseError.h"
+#include "SecurityOrigin.h"
 #include <wtf/Threading.h>
 #include <wtf/UnusedParam.h>
 
