a6fec3a3b64b5e755fca4aaaa18405197460c575
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-20 23:02:40 +0000

    No review; build fix only.
    When WTF_USE_ACCELERATED_COMPOSITING is turned off, MediaPlayerPrivateQuickTimeVisualContext.cpp
    must include CoreGraphics/CGContext.h.
    
    * platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59873 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "StringHash.h"
 #include "TimeRanges.h"
 #include "Timer.h"
+#include <CoreGraphics/CGContext.h>
 #include <Wininet.h>
 #include <wtf/CurrentTime.h>
 #include <wtf/HashSet.h>
