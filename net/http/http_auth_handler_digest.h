8ceef08bf16636811a632bdf589bbbe5a3359272
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-20 11:09:24 +0000

    Remove handler from HttpAuthCache.
    This is part of a refactoring meant to simplify the connection phase of HttpNetworkTransaction.
    
    BUG=None
    TEST=net_unittests (which already includes unit tests for preemptive auth, as well as using values from cache).
    
    Review URL: http://codereview.chromium.org/2056003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47786 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_handler_digest.h
+++ net/http/http_auth_handler_digest.h
@@ -25,6 +25,8 @@ class HttpAuthHandlerDigest : public HttpAuthHandler {
     virtual int CreateAuthHandler(HttpAuth::ChallengeTokenizer* challenge,
                                   HttpAuth::Target target,
                                   const GURL& origin,
+                                  CreateReason reason,
+                                  int digest_nonce_count,
                                   scoped_refptr<HttpAuthHandler>* handler);
   };
 
@@ -40,7 +42,6 @@ class HttpAuthHandlerDigest : public HttpAuthHandler {
 
  protected:
   virtual bool Init(HttpAuth::ChallengeTokenizer* challenge) {
-    nonce_count_ = 0;
     return ParseChallenge(challenge);
   }
 
@@ -70,6 +71,8 @@ class HttpAuthHandlerDigest : public HttpAuthHandler {
     QOP_AUTH = 1 << 0,
     QOP_AUTH_INT = 1 << 1,
   };
+
+  explicit HttpAuthHandlerDigest(int nonce_count) : nonce_count_(nonce_count) {}
 
   ~HttpAuthHandlerDigest() {}
 
--- net/http/http_cache.cc
+++ net/http/http_cache.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_request_info.h"
 #include "net/http/http_response_headers.h"
 #include "net/http/http_response_info.h"
+#include "net/http/http_util.h"
 #include "net/spdy/spdy_session_pool.h"
 
 namespace net {
