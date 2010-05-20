daf577cd6ef39685ca2e6ca314515fad2bce09e8
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-20 00:30:35 +0000

    2010-05-19  Oliver Hunt  <oliver@apple.com>
            Reviewed by Geoffrey Garen.
    
            emitJumpIfNotJSCell should special case constant immediate values
            https://bugs.webkit.org/show_bug.cgi?id=39392
            <rdar://problem/8001324>
    
            Make emitJumpSlowCaseIfNotJSCell special case constant immediate
            values, in addition to the immediate JSCell optimisation.
    
            Also add assertions to make sure no one else produces code that
            attempts to load constants from the register file.
    
            * jit/JITInlineMethods.h:
            (JSC::JIT::emitJumpSlowCaseIfNotJSCell):
            * jit/JSInterfaceJIT.h:
            (JSC::JSInterfaceJIT::emitJumpIfNotJSCell):
            (JSC::JSInterfaceJIT::emitLoadInt32):
            (JSC::JSInterfaceJIT::tagFor):
            (JSC::JSInterfaceJIT::payloadFor):
            (JSC::JSInterfaceJIT::emitLoadDouble):
            (JSC::JSInterfaceJIT::addressFor):
            * jit/ThunkGenerators.cpp:
    2010-05-19  Oliver Hunt  <oliver@apple.com>
    
            Reviewed by Geoffrey Garen.
    
            emitJumpIfNotJSCell should special case constant immediate values
            https://bugs.webkit.org/show_bug.cgi?id=39392
    
            Add tests for immediate constants being used where cells are expected.
    
            * fast/js/immediate-constant-instead-of-cell-expected.txt: Added.
            * fast/js/immediate-constant-instead-of-cell.html: Added.
            * fast/js/script-tests/immediate-constant-instead-of-cell.js: Added.
            ():
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59798 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/jit/ThunkGenerators.cpp
+++ third_party/WebKit/JavaScriptCore/jit/ThunkGenerators.cpp
@@ -26,8 +26,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "ThunkGenerators.h"
-#include <wtf/text/StringImpl.h>
 
+#include "CodeBlock.h"
+#include <wtf/text/StringImpl.h>
 #include "SpecializedThunkJIT.h"
 
 #if ENABLE(JIT)
