e800222ba62a9bc09e3809e6fd8c6f4689409ef0
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-20 14:40:29 +0000

    Handle EINTR and remove offset_t in ffmpeg code.
    (Should also fix OpenBSD build I believe)
    
    http://codereview.chromium.org/2136017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47794 0039d316-1c4b-4281-b951-d872f2087c98

--- media/ffmpeg/ffmpeg_common.h
+++ media/ffmpeg/ffmpeg_common.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/singleton.h"
 
-// Used with URLProtocol.
-typedef int64 offset_t;
-
 // Include FFmpeg header files.
 extern "C" {
 // Temporarily disable possible loss of data warning.
