cd51a20e61b76d436d183501f5460a64e359ea5e
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-20 00:00:20 +0000

    Revert 47749 - Integrated new bug icon in status bar. It opens bug reporting dialog.
    BUG=chromiumos:3348
    TEST=click on the new bug icon in the status bar, it should open bug reporting dialog
    
    Review URL: http://codereview.chromium.org/2135005
    
    TBR=zelidrag@chromium.org
    Review URL: http://codereview.chromium.org/2125013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47751 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/background_view.cc
+++ chrome/browser/chromeos/login/background_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "chrome/browser/chromeos/login/rounded_rect_painter.h"
 #include "chrome/browser/chromeos/status/clock_menu_button.h"
-#include "chrome/browser/chromeos/status/feedback_menu_button.h"
 #include "chrome/browser/chromeos/status/language_menu_button.h"
 #include "chrome/browser/chromeos/status/network_menu_button.h"
 #include "chrome/browser/chromeos/status/status_area_view.h"
@@ -128,7 +127,6 @@ gfx::NativeWindow BackgroundView::GetNativeWindow() const {
 bool BackgroundView::ShouldOpenButtonOptions(
     const views::View* button_view) const {
   if (button_view == status_area_->clock_view() ||
-      button_view == status_area_->feedback_view() ||
       button_view == status_area_->language_view() ||
       button_view == status_area_->network_view()) {
     return false;
