7bda419437b4bd05ddeab0985823213c862b191a
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-20 00:23:44 +0000

    Refactor text encoding detection logic in FileReader. https://bugs.webkit.org/show_bug.cgi?id=39131
    Reviewed by Alexey Proskuryakov.
    
    WebCore:
    
    Changed FileReader::convertToText to call TextResourceDecoder::decode to
    detect the encoding from BOM and decode the text. Though the File API
    spec says that the supplied encoding should be used if it is valid, we
    choose to ignore this requirement in order to be consistent with how
    WebKit decodes the web content: always has the BOM override the provided
    encoding.
    
    * html/FileReader.cpp:
    (WebCore::FileReader::convertToText):
    * html/FileReader.h:
    
    LayoutTests:
    
    Add more test coverage for FileReader.
    
    * fast/files/file-reader-expected.txt:
    * fast/files/file-reader.html:
    * fast/files/resources/binary-file: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59797 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/FileReader.h
+++ third_party/WebKit/WebCore/html/FileReader.h
@@ -52,6 +52,7 @@ class Blob;
 class File;
 class FileStreamProxy;
 class ScriptExecutionContext;
+class TextResourceDecoder;
 
 class FileReader : public RefCounted<FileReader>, public ActiveDOMObject, public EventTarget, public FileStreamClient {
 public:
@@ -136,6 +137,7 @@ private:
 
     RefPtr<Blob> m_fileBlob;
     ReadType m_readType;
+    TextEncoding m_encoding;
 
     // Like XMLHttpRequest.m_responseText, we keep this as a ScriptString, not a WebCore::String.
     // That's because these strings can easily get huge (they are filled from the file) and
@@ -149,8 +151,8 @@ private:
     Vector<char> m_rawData;
     bool m_isRawDataConverted;
 
-    // Encoding scheme used to decode the data.
-    TextEncoding m_encoding;
+    // The decoder used to decode the text data.
+    RefPtr<TextResourceDecoder> m_decoder;
 
     // Needed to create data URL.
     String m_fileType;
