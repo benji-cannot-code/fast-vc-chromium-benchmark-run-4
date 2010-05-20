c992696ea710445767c8e7c9d90deb99d9cbb1d6
jiesun@google.com <jiesun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-20 23:08:45 +0000

    VideoDecoderImpl Mock Engine leak callback.
    Review URL: http://codereview.chromium.org/2085013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47857 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_decoder_impl_unittest.cc
+++ media/filters/video_decoder_impl_unittest.cc
@@ -61,6 +61,8 @@ class MockVideoDecodeEngine : public VideoDecodeEngine {
   MOCK_METHOD1(Flush, void(Task* done_cb));
   MOCK_CONST_METHOD0(state, State());
   MOCK_CONST_METHOD0(GetSurfaceFormat, VideoFrame::Format());
+
+  scoped_ptr<FillThisBufferCallback> fill_buffer_callback_;
 };
 
 // Class that just mocks the private functions.
@@ -207,6 +209,10 @@ TEST_F(VideoDecoderImplTest, Initialize_QueryInterfaceFails) {
   message_loop_.RunAllPending();
 }
 
+ACTION_P(SaveCallback, engine) {
+  engine->fill_buffer_callback_.reset(arg3);
+}
+
 TEST_F(VideoDecoderImplTest, Initialize_EngineFails) {
   // Test successful initialization.
   AVStreamProvider* av_stream_provider = demuxer_;
@@ -216,7 +222,7 @@ TEST_F(VideoDecoderImplTest, Initialize_EngineFails) {
       .WillOnce(Return(&stream_));
 
   EXPECT_CALL(*engine_, Initialize(_, _, _, _, _))
-      .WillOnce(WithArg<4>(InvokeRunnable()));
+      .WillOnce(DoAll(SaveCallback(engine_), WithArg<4>(InvokeRunnable())));
   EXPECT_CALL(*engine_, state())
       .WillOnce(Return(VideoDecodeEngine::kError));
 
@@ -238,7 +244,7 @@ TEST_F(VideoDecoderImplTest, Initialize_Successful) {
       .WillOnce(Return(&stream_));
 
   EXPECT_CALL(*engine_, Initialize(_, _, _, _, _))
-      .WillOnce(WithArg<4>(InvokeRunnable()));
+      .WillOnce(DoAll(SaveCallback(engine_), WithArg<4>(InvokeRunnable())));
   EXPECT_CALL(*engine_, state())
       .WillOnce(Return(VideoDecodeEngine::kNormal));
 
