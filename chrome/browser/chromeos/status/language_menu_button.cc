140fd557e5ce4b617a1ddf0da705d1673fb3b9eb
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-20 04:05:34 +0000

    Share the same label between the language selector and the options dialog.
    The "Language and Input" dialog is opened from two places.
    We should share the same label in the two places.
    
    BUG=3431
    TEST=manually
    
    Review URL: http://codereview.chromium.org/2094014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47769 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/language_menu_button.cc
+++ chrome/browser/chromeos/status/language_menu_button.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 //   (1) input method names. The size of the list is always >= 1.
 //   (2) input method properties. This list might be empty.
-//   (3) "Configure IME..." button.
+//   (3) "Customize language and input..." button.
 //
 // Example of the menu (Japanese):
 //
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // [*] Roman
 // [ ] Kana
 // ------------------------------ (separator)
-// Configure IME...               (index = 11)
+// Customize language and input...(index = 11)
 // ============================== (border of the popup window)
 //
 // Example of the menu (Simplified Chinese):
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Switch to full letter mode     (The property has 2 command buttons)
 // Switch to half punctuation mode
 // ------------------------------ (separator)
-// Configure IME...
+// Customize language and input...
 // ============================== (border of the popup window)
 //
 
@@ -60,7 +60,7 @@ namespace {
 enum {
   COMMAND_ID_INPUT_METHODS = 0,  // English, Chinese, Japanese, Arabic, ...
   COMMAND_ID_IME_PROPERTIES,  // Hiragana, Katakana, ...
-  COMMAND_ID_CONFIGURE_IME,  // The "Configure IME..." button.
+  COMMAND_ID_CUSTOMIZE_LANGUAGE,  // "Customize language and input..." button.
 };
 
 // A group ID for IME properties starts from 0. We use the huge value for the
@@ -192,7 +192,7 @@ bool LanguageMenuButton::IsItemCheckedAt(int index) const {
     return property_list.at(index).is_selection_item_checked;
   }
 
-  // Separator(s) or the "Configure IME" button.
+  // Separator(s) or the "Customize language and input..." button.
   return false;
 }
 
@@ -253,7 +253,7 @@ menus::MenuModel::ItemType LanguageMenuButton::GetTypeAt(int index) const {
   DCHECK_GE(index, 0);
 
   if (IndexPointsToConfigureImeMenuItem(index)) {
-    return menus::MenuModel::TYPE_COMMAND;  // "Configure IME"
+    return menus::MenuModel::TYPE_COMMAND;  // "Customize language and input"
   }
 
   if (IndexIsInInputMethodList(index)) {
@@ -276,8 +276,10 @@ string16 LanguageMenuButton::GetLabelAt(int index) const {
   DCHECK_GE(index, 0);
   DCHECK(input_method_descriptors_.get());
 
+  // We use IDS_OPTIONS_SETTINGS_LANGUAGES_CUSTOMIZE here as the button
+  // opens the same dialog that is opened from the main options dialog.
   if (IndexPointsToConfigureImeMenuItem(index)) {
-    return l10n_util::GetStringUTF16(IDS_STATUSBAR_IME_CONFIGURE);
+    return l10n_util::GetStringUTF16(IDS_OPTIONS_SETTINGS_LANGUAGES_CUSTOMIZE);
   }
 
   std::wstring name;
@@ -420,7 +422,8 @@ void LanguageMenuButton::RebuildModel() {
     // other items even if an item is not actually a radio item.
     if (need_separator)
       model_->AddSeparator();
-    model_->AddRadioItem(COMMAND_ID_CONFIGURE_IME, dummy_label, 0 /* dummy */);
+    model_->AddRadioItem(COMMAND_ID_CUSTOMIZE_LANGUAGE, dummy_label,
+                         0 /* dummy */);
   }
 }
 
@@ -467,7 +470,7 @@ bool LanguageMenuButton::IndexPointsToConfigureImeMenuItem(int index) const {
   }
 
   return ((model_->GetTypeAt(index) == menus::MenuModel::TYPE_RADIO) &&
-          (model_->GetCommandIdAt(index) == COMMAND_ID_CONFIGURE_IME));
+          (model_->GetCommandIdAt(index) == COMMAND_ID_CUSTOMIZE_LANGUAGE));
 }
 
 }  // namespace chromeos
