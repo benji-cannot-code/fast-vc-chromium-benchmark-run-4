603a61ade34de6491595e93ee3db2a782263a841
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-20 20:49:26 +0000

    2010-05-20  Darin Fisher  <darin@chromium.org>
            Reviewed by Nate Chapin.
    
            [chromium] Provide a way to catch exceptions thrown while interacting
            with a NPObject via WebBindings methods.
            https://bugs.webkit.org/show_bug.cgi?id=39378
    
            This change also includes a small bit of cleanup in V8NPObject and
            friends.  I moved code into the WebCore namespace where appropriate.
            
            In _NPN_Invoke, I also moved the call to _NPN_Evaluate outside of the
            context scope.  I did this to avoid having nested ExceptionCatcher
            objects, which is not strictly necessary, but it seemed cleaner.
            _NPN_Evaluate already takes care of creating a context scope.
    
            * bindings/v8/NPV8Object.cpp:
            (WebCore::v8ObjectToNPObject):
            (WebCore::npCreateV8ScriptObject):
            (_NPN_Invoke):
            (_NPN_InvokeDefault):
            (_NPN_Evaluate):
            (_NPN_EvaluateHelper):
            (_NPN_GetProperty):
            (_NPN_SetProperty):
            (_NPN_RemoveProperty):
            (_NPN_HasProperty):
            (_NPN_HasMethod):
            (_NPN_SetException):
            (_NPN_Enumerate):
            (_NPN_Construct):
            * bindings/v8/NPV8Object.h:
            * bindings/v8/V8NPObject.cpp:
            * bindings/v8/V8NPObject.h:
            * bindings/v8/V8NPUtils.cpp:
            (WebCore::convertV8ObjectToNPVariant):
            (WebCore::pushExceptionHandler):
            (WebCore::popExceptionHandler):
            (WebCore::ExceptionCatcher::ExceptionCatcher):
            (WebCore::ExceptionCatcher::~ExceptionCatcher):
            * bindings/v8/V8NPUtils.h:
            * bindings/v8/npruntime.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59862 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/NPV8Object.h
+++ third_party/WebKit/WebCore/bindings/v8/NPV8Object.h
@@ -46,12 +46,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <v8.h>
 
 namespace WebCore {
-    class DOMWindow;
 
-    static const int npObjectInternalFieldCount = v8DefaultWrapperInternalFieldCount + 0;
+class DOMWindow;
 
-    WrapperTypeInfo* npObjectTypeInfo();
-}
+static const int npObjectInternalFieldCount = v8DefaultWrapperInternalFieldCount + 0;
+
+WrapperTypeInfo* npObjectTypeInfo();
 
 extern NPClass* npScriptObjectClass;
 
@@ -60,7 +60,7 @@ extern NPClass* npScriptObjectClass;
 struct V8NPObject {
     NPObject object;
     v8::Persistent<v8::Object> v8Object;
-    WebCore::DOMWindow* rootObject;
+    DOMWindow* rootObject;
 };
 
 struct PrivateIdentifier {
@@ -71,8 +71,10 @@ struct PrivateIdentifier {
     bool isString;
 };
 
-NPObject* npCreateV8ScriptObject(NPP, v8::Handle<v8::Object>, WebCore::DOMWindow*);
+NPObject* npCreateV8ScriptObject(NPP, v8::Handle<v8::Object>, DOMWindow*);
 
 NPObject* v8ObjectToNPObject(v8::Handle<v8::Object>);
+
+} // namespace WebCore
 
 #endif // NPV8Object_h
