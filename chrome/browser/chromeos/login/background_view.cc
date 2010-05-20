9a10f3b6a1676ed11b0aa974f6e65e10d50ebece
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-20 23:31:03 +0000

    Made feedback icon hidden on login screen.
    BUG=chromium-os:3348
    TEST=make sure feedback icon does not show on login mgr screens
    
    Review URL: http://codereview.chromium.org/2081015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47861 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/background_view.cc
+++ chrome/browser/chromeos/login/background_view.cc
@@ -141,6 +141,8 @@ void BackgroundView::OpenButtonOptions(const views::View* button_view) const {
 }
 
 bool BackgroundView::IsButtonVisible(const views::View* button_view) const {
+  if (button_view == status_area_->feedback_view())
+    return false;
   return true;
 }
 
@@ -157,6 +159,7 @@ void BackgroundView::InitStatusArea() {
   DCHECK(status_area_ == NULL);
   status_area_ = new StatusAreaView(this);
   status_area_->Init();
+  status_area_->Update();
   AddChildView(status_area_);
 }
 
