cf82f8cbb5f859d4589a9426bfc7efe3fecf253e
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-21 04:19:35 +0000

    2010-05-20  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Eric Seidel.
    
            [BREWMP] Use skia as BREW MP's graphics backend.
            https://bugs.webkit.org/show_bug.cgi?id=39407
    
            Define WTF_PLATFORM_SKIA=1 for BREW MP.
    
            * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59908 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -523,6 +523,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif
 
+#if PLATFORM(BREWMP)
+#define WTF_PLATFORM_SKIA 1
+#endif
+
 #if PLATFORM(GTK)
 #define WTF_PLATFORM_CAIRO 1
 #endif
