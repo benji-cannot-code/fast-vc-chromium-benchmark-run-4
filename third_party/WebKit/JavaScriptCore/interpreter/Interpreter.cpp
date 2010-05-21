9ae8c44f77552d346d8e671c5d5735b13f3d86b3
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-21 23:52:56 +0000

    Interpreter fix following r59939.
    Rubber stamped by Oliver Hunt.
    
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::privateExecute):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59975 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/interpreter/Interpreter.cpp
+++ third_party/WebKit/JavaScriptCore/interpreter/Interpreter.cpp
@@ -3845,6 +3845,8 @@ skip_id_custom_self:
             return returnValue;
 
         functionReturnValue = returnValue;
+        codeBlock = callFrame->codeBlock();
+        ASSERT(codeBlock == callFrame->codeBlock());
 
         NEXT_INSTRUCTION();
     }
