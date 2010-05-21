167ee070e11170e0a911545b7e3ac47870251ee3
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-21 15:01:23 +0000

    2010-05-21  Jedrzej Nowacki  <jedrzej.nowacki@nokia.com>
            Reviewed by Darin Fisher.
    
            Fix the HistoryController::pushState.
    
            It should be possible to call the pushState method from
            the first page in a window.
    
            history.pushState doesn't work for the first page in a window.
            https://bugs.webkit.org/show_bug.cgi?id=39418
    
            * loader/HistoryController.cpp:
            (WebCore::HistoryController::pushState):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59933 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/HistoryController.cpp
+++ third_party/WebKit/WebCore/loader/HistoryController.cpp
@@ -645,7 +645,7 @@ void HistoryController::updateBackForwardListClippedAtTarget(bool doClip)
 
 void HistoryController::pushState(PassRefPtr<SerializedScriptValue> stateObject, const String& title, const String& urlString)
 {
-    if (!m_previousItem)
+    if (!m_currentItem)
         return;
 
     Page* page = m_frame->page();
