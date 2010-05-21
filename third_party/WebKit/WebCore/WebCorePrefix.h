56d1bb808e3ffb4cb7912e0e5e11838d655c6dd1
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-21 04:41:54 +0000

    2010-05-20  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Eric Seidel.
    
            [BREWMP] Don't include CoreFoundation.h when building for BREW MP
            https://bugs.webkit.org/show_bug.cgi?id=39405
    
            Add !defined(BUILDING_BREWMP__) guard for CoreFoundation.h.
    
            * WebCorePrefix.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59909 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCorePrefix.h
+++ third_party/WebKit/WebCore/WebCorePrefix.h
@@ -119,7 +119,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <time.h>
 
-#if !defined(BUILDING_WX__) && !defined(ANDROID)
+#if !defined(BUILDING_WX__) && !defined(ANDROID) && !defined(BUILDING_BREWMP__)
 #include <CoreFoundation/CoreFoundation.h>
 #ifdef WIN_CAIRO
 #include <ConditionalMacros.h>
