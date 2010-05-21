e523854b7da8bc28c1d2f498b5576592022739b7
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-21 16:55:52 +0000

    2010-05-21  Robin Cao  <robin.cao@torchmobile.com.cn>
            Reviewed by Adam Roben.
    
            fast/dom/HTMLObjectElement/children-changed.html times out on Windows run-webkit-tests
            https://bugs.webkit.org/show_bug.cgi?id=31315
    
            * platform/win/Skipped: The test should pass now.
    2010-05-21  Robin Cao  <robin.cao@torchmobile.com.cn>
    
            Reviewed by Adam Roben.
    
            fast/dom/HTMLObjectElement/children-changed.html times out on Windows run-webkit-tests
            https://bugs.webkit.org/show_bug.cgi?id=31315
    
            * DumpRenderTree/win/FrameLoadDelegate.cpp:
            (FrameLoadDelegate::didFailProvisionalLoadWithError): Need to invoke locationChangeDone here as mac port does.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/win/FrameLoadDelegate.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/win/FrameLoadDelegate.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2005, 2006, 2007, 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2010 Torch Mobile (Beijing) Co. Ltd. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -147,6 +148,7 @@ HRESULT STDMETHODCALLTYPE FrameLoadDelegate::didFailProvisionalLoadWithError(
     if (!done && gLayoutTestController->dumpFrameLoadCallbacks())
         printf("%s - didFailProvisionalLoadWithError\n", descriptionSuitableForTestResult(frame).c_str());
 
+    locationChangeDone(error, frame);
     return S_OK;
 }
 
