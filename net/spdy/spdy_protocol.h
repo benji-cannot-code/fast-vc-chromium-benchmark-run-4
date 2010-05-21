2b6e465eee6b8810d62deeeebda192665585fbe2
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-21 04:05:40 +0000

    Separate SPDY data-frame compression from the header compressors.
    BUG=none
    TEST=SpdyFramerTest.UnclosedStreamDataCompressors, SpdyFramerTest.DataCompression
    
    Review URL: http://codereview.chromium.org/2071009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47886 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_protocol.h
+++ net/spdy/spdy_protocol.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -131,7 +131,7 @@ enum SpdyControlType {
 enum SpdyDataFlags {
   DATA_FLAG_NONE = 0,
   DATA_FLAG_FIN = 1,
-  DATA_FLAG_COMPRESSED = 2  // TODO(mbelshe): remove me.
+  DATA_FLAG_COMPRESSED = 2
 };
 
 // Flags on control packets
@@ -158,7 +158,9 @@ enum SpdySettingsIds {
   SETTINGS_DOWNLOAD_BANDWIDTH = 0x2,
   SETTINGS_ROUND_TRIP_TIME = 0x3,
   SETTINGS_MAX_CONCURRENT_STREAMS = 0x4,
-  SETTINGS_CURRENT_CWND = 0x5
+  SETTINGS_CURRENT_CWND = 0x5,
+  // Downstream byte retransmission rate in percentage.
+  SETTINGS_DOWNLOAD_RETRANS_RATE = 0x6
 };
 
 // Status codes, as used in control frames (primarily RST_STREAM).
@@ -352,6 +354,10 @@ class SpdyDataFrame : public SpdyFrame {
   // Returns the size of the SpdyFrameBlock structure.
   // Note: this is not the size of the SpdyDataFrame class.
   static size_t size() { return SpdyFrame::size(); }
+
+  const char* payload() const {
+    return reinterpret_cast<const char*>(frame_) + size();
+  }
 
  private:
   DISALLOW_COPY_AND_ASSIGN(SpdyDataFrame);
