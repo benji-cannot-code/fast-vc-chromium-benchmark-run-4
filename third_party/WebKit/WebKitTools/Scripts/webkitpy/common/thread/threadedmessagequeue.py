5b00a56643ac70f1af6dc0944caf8ff77b6570cc
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-21 03:04:28 +0000

    2010-05-20  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            ThreadedMessageQueue should use with_statement for exception safety
            https://bugs.webkit.org/show_bug.cgi?id=39233
    
            * Scripts/webkitpy/common/thread/threadedmessagequeue.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/common/thread/threadedmessagequeue.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/common/thread/threadedmessagequeue.py
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import with_statement
+
 import threading
 
 
@@ -37,20 +39,17 @@ class ThreadedMessageQueue(object):
         self._lock = threading.Lock()
 
     def post(self, message):
-        self._lock.acquire()
-        self._messages.append(message)
-        self._lock.release()
+        with self._lock:
+            self._messages.append(message)
 
     def stop(self):
-        self._lock.acquire()
-        self._is_running = False
-        self._lock.release()
+        with self._lock:
+            self._is_running = False
 
     def take_all(self):
-        self._lock.acquire()
-        messages = self._messages
-        is_running = self._is_running
-        self._messages = []
-        self._lock.release()
+        with self._lock:
+            messages = self._messages
+            is_running = self._is_running
+            self._messages = []
         return (messages, is_running)
 
