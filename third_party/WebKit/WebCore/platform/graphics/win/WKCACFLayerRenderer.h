db8d6151466cc1a205f7a1ea955bcbe045015029
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-21 04:49:20 +0000

    2010-05-20  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Adam Roben.
    
            Avoid flushing CA layers when a layout is pending
            https://bugs.webkit.org/show_bug.cgi?id=39463
            <rdar://problem/7999463>
    
            Avoid rendering the compositing layers to the screen if there's a layout pending,
            since the layer tree not in a state that should be presented to the user.
    
            This fixes flashes in some types of content that dynamically add and remove layers.
    
            Do this by adding a WKCACFLayerRendererClient for WKCACFLayerRenderer, which it can
            call through to ask whether now is a good time to render. If not, it schedules
            another render soon.
    
            * platform/graphics/win/WKCACFLayerRenderer.h:
            (WebCore::WKCACFLayerRendererClient::~WKCACFLayerRendererClient):
            * platform/graphics/win/WKCACFLayerRenderer.cpp:
            (WebCore::WKCACFLayerRenderer::acceleratedCompositingAvailable):
            (WebCore::WKCACFLayerRenderer::create):
            (WebCore::WKCACFLayerRenderer::WKCACFLayerRenderer):
            (WebCore::WKCACFLayerRenderer::render):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayerRenderer.h
+++ third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayerRenderer.h
@@ -50,12 +50,18 @@ namespace WebCore {
 
 class WKCACFRootLayer;
 
+class WKCACFLayerRendererClient {
+public:
+    virtual ~WKCACFLayerRendererClient() { }
+    virtual bool shouldRender() const = 0;
+};
+
 // FIXME: Currently there is a WKCACFLayerRenderer for each WebView and each
 // has its own CARenderOGLContext and Direct3DDevice9, which is inefficient.
 // (https://bugs.webkit.org/show_bug.cgi?id=31855)
 class WKCACFLayerRenderer : public Noncopyable {
 public:
-    static PassOwnPtr<WKCACFLayerRenderer> create();
+    static PassOwnPtr<WKCACFLayerRenderer> create(WKCACFLayerRendererClient*);
     ~WKCACFLayerRenderer();
 
     static bool acceleratedCompositingAvailable();
@@ -78,7 +84,7 @@ protected:
     WKCACFLayer* rootLayer() const;
 
 private:
-    WKCACFLayerRenderer();
+    WKCACFLayerRenderer(WKCACFLayerRendererClient*);
 
     void renderTimerFired(Timer<WKCACFLayerRenderer>*);
 
@@ -95,6 +101,7 @@ private:
     void render(const Vector<CGRect>& dirtyRects = Vector<CGRect>());
     void paint();
 
+    WKCACFLayerRendererClient* m_client;
     bool m_mightBeAbleToCreateDeviceLater;
     COMPtr<IDirect3DDevice9> m_d3dDevice;
     RefPtr<WKCACFRootLayer> m_rootLayer;
