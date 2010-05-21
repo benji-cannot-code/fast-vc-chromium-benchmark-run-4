c6ce74858c4b62f86c060dcb6491fdf098457791
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-21 03:20:27 +0000

    REGRESSION(r53637): DivX plug-in fails to start until window is resized https://bugs.webkit.org/show_bug.cgi?id=39457 <rdar://problem/8006102>
    Before r53637, we called Widget::move() (which caused us to call
    NPP_SetWindow) before painting a plug-in. r53637 removed the call to
    Widget::move(), and we end up never calling NPP_SetWindow for plug-ins
    with the "DeferFirstSetWindowCall" quirk, including the DivX plug-in.
    
    Reviewed by Simon Fraser.
    
    * manual-tests/divx-plugin-fails-to-draw.html: Added.
    
    * plugins/PluginView.cpp:
    (WebCore::PluginView::PluginView):
    Initialize new member to false.
    
    * plugins/PluginView.h:
    Added new member.
    
    * plugins/win/PluginViewWin.cpp:
    (WebCore::PluginView::paint):
    Call setNPWindowRect() before painting to ensure that NPP_SetWindow gets
    called.
    (WebCore::PluginView::setNPWindowRect):
    Set m_haveCalledSetWindow.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@59904 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
+++ third_party/WebKit/WebCore/plugins/win/PluginViewWin.cpp
@@ -602,6 +602,10 @@ void PluginView::paint(GraphicsContext* context, const IntRect& rect)
     if (context->paintingDisabled())
         return;
 
+    // Ensure that we have called SetWindow before we try to paint.
+    if (!m_haveCalledSetWindow)
+        setNPWindowRect(frameRect());
+
     if (m_isWindowed) {
 #if !OS(WINCE)
         if (context->shouldIncludeChildWindows())
@@ -806,6 +810,8 @@ void PluginView::setNPWindowRect(const IntRect& rect)
         setCallingPlugin(true);
         m_plugin->pluginFuncs()->setwindow(m_instance, &m_npWindow);
         setCallingPlugin(false);
+
+        m_haveCalledSetWindow = true;
 
         if (!m_isWindowed)
             return;
