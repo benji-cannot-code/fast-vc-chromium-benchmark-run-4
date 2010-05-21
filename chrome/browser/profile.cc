a3e62527fa4588d30f4dd5818c3638b3aa9ff638
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-21 22:21:11 +0000

    Ensure that the default profile for cros has no extension service BUG=chromeium-os:3232 TEST=LoginProfileTest.UserNotPassed
    Review URL: http://codereview.chromium.org/2094018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47959 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profile.cc
+++ chrome/browser/profile.cc
@@ -664,14 +664,6 @@ ProfileImpl::ProfileImpl(const FilePath& path)
       TimeDelta::FromMilliseconds(kCreateSessionServiceDelayMS), this,
       &ProfileImpl::EnsureSessionServiceCreated);
 
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableExtensionTimelineApi)) {
-    extension_devtools_manager_ = new ExtensionDevToolsManager(this);
-  }
-
-  extension_process_manager_.reset(new ExtensionProcessManager(this));
-  extension_message_service_ = new ExtensionMessageService(this);
-
   PrefService* prefs = GetPrefs();
   prefs->AddPrefObserver(prefs::kSpellCheckDictionary, this);
   prefs->AddPrefObserver(prefs::kEnableSpellCheck, this);
@@ -737,6 +729,14 @@ void ProfileImpl::InitExtensions() {
     return;  // Already initialized.
 
   const CommandLine* command_line = CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(
+      switches::kEnableExtensionTimelineApi)) {
+    extension_devtools_manager_ = new ExtensionDevToolsManager(this);
+  }
+
+  extension_process_manager_.reset(new ExtensionProcessManager(this));
+  extension_message_service_ = new ExtensionMessageService(this);
+
   ExtensionErrorReporter::Init(true);  // allow noisy errors.
 
   FilePath script_dir;  // Don't look for user scripts in any directory.
@@ -890,7 +890,8 @@ ProfileImpl::~ProfileImpl() {
   // HistoryService first.
   favicon_service_ = NULL;
 
-  extension_message_service_->ProfileDestroyed();
+  if (extension_message_service_)
+    extension_message_service_->ProfileDestroyed();
 
   if (extensions_service_)
     extensions_service_->ProfileDestroyed();
