2936e927a3067918d3c44fd4ed196f8c64608bc3
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-23 12:10:12 +0000

    2010-05-23  Noam Rosenthal  <noam.rosenthal@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] GraphicsLayer: The rocket animation doesn't scroll together with the page
            https://bugs.webkit.org/show_bug.cgi?id=39517
    
            A previous fix to this issue was overridden by the PageClientQt refactor. Reapplying.
    
            * WebCoreSupport/PageClientQt.cpp:
            (WebCore::PageClientQGraphicsWidget::update):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60041 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/PageClientQt.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/PageClientQt.cpp
@@ -133,6 +133,8 @@ void PageClientQGraphicsWidget::update(const QRect& dirtyRect)
         overlay->update(QRectF(dirtyRect));
 #if USE(ACCELERATED_COMPOSITING)
     syncLayers();
+    // This might be a slow-scroll. We ensure that the compositing layers are in the right position.
+    updateCompositingScrollPosition();
 #endif
 }
 
