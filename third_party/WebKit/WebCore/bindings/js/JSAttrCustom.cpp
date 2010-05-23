benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 3137424d914ff9fcbf92b10c5ef7b5283fa3e41f

2010-05-23  Justin Schuh  <jschuh@chromium.org>
        Reviewed by Darin Adler.

        Remove custom binding for Attr.value
        https://bugs.webkit.org/show_bug.cgi?id=39499

        Test now throws a JavaScript security exception instead of not
        implemented.

        * http/tests/security/xss-DENIED-iframe-src-alias-expected.txt:
2010-05-23  Justin Schuh  <jschuh@chromium.org>

        Reviewed by Darin Adler.

        Remove custom binding for Attr.value
        https://bugs.webkit.org/show_bug.cgi?id=39499

        Custom bindings are no longer needed because origin checks were moved
        out of the bindings by: http://trac.webkit.org/changeset/59866

        * Android.v8bindings.mk:
        * WebCore.gypi:
        * bindings/js/JSAttrCustom.cpp:
        * bindings/v8/custom/V8AttrCustom.cpp: Removed.
        * dom/Attr.idl:

git-svn-id: svn://svn.chromium.org/blink/trunk@60037 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebCore/bindings/js/JSAttrCustom.cpp
@@ -32,29 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CSSHelper.h"
 #include "Document.h"
-#include "HTMLFrameElementBase.h"
 #include "HTMLNames.h"
-#include "JSDOMBinding.h"
 
 using namespace JSC;
 
 namespace WebCore {
 
 using namespace HTMLNames;
-
-void JSAttr::setValue(ExecState* exec, JSValue value)
-{
-    Attr* imp = static_cast<Attr*>(impl());
-    String attrValue = valueToStringWithNullCheck(exec, value);
-
-    Element* ownerElement = imp->ownerElement();
-    if (ownerElement && !allowSettingSrcToJavascriptURL(exec, ownerElement, imp->name(), attrValue))
-        return;
-
-    ExceptionCode ec = 0;
-    imp->setValue(attrValue, ec);
-    setDOMException(exec, ec);
-}
 
 void JSAttr::markChildren(MarkStack& markStack)
 {

third_party/WebKit/LayoutTests/http/tests/security/xss-DENIED-iframe-src-alias-expected.txt
@@ -31,6 +31,8 @@ CONSOLE MESSAGE: line 1: Unsafe JavaScript attempt to access frame with URL http
 
 CONSOLE MESSAGE: line 1: Unsafe JavaScript attempt to access frame with URL http://localhost:8080/security/resources/innocent-victim.html from frame with URL http://127.0.0.1:8000/security/xss-DENIED-iframe-src-alias.html. Domains, protocols and ports must match.
 
+CONSOLE MESSAGE: line 1: Unsafe JavaScript attempt to access frame with URL http://localhost:8080/security/resources/innocent-victim.html from frame with URL http://127.0.0.1:8000/security/xss-DENIED-iframe-src-alias.html. Domains, protocols and ports must match.
+
 This script tests if iframe.src can be set to a JavaScript URL via alternate DOM interfaces (such as Node.textContent or NamedNode.setNamedItem). The test is successful if no alerts appear and the page finishes loading.
 
 

third_party/WebKit/WebCore/Android.v8bindings.mk
@@ -85,7 +85,6 @@ LOCAL_SRC_FILES += \
 	\
 	bindings/v8/npruntime.cpp \
 	\
-	bindings/v8/custom/V8AttrCustom.cpp \
 	bindings/v8/custom/V8CSSRuleCustom.cpp \
 	bindings/v8/custom/V8CSSStyleDeclarationCustom.cpp \
 	bindings/v8/custom/V8CSSStyleSheetCustom.cpp \

third_party/WebKit/WebCore/WebCore.gypi
@@ -695,7 +695,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'bindings/ScriptControllerBase.cpp',
             'bindings/v8/ChildThreadDOMData.cpp',
             'bindings/v8/ChildThreadDOMData.h',
-            'bindings/v8/custom/V8AttrCustom.cpp',
             'bindings/v8/custom/V8CanvasPixelArrayCustom.cpp',
             'bindings/v8/custom/V8ArrayBufferViewCustom.h',
             'bindings/v8/custom/V8ArrayBufferViewCustom.cpp',

third_party/WebKit/WebCore/bindings/v8/custom/V8AttrCustom.cpp
@@ -1,59 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/*
- * Copyright (C) 2007-2009 Google Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include "config.h"
-#include "V8Attr.h"
-
-#include "Attr.h"
-#include "Element.h"
-#include "ExceptionCode.h"
-#include "V8Binding.h"
-#include "V8BindingState.h"
-#include "V8Proxy.h"
-
-namespace WebCore {
-
-void V8Attr::valueAccessorSetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::AccessorInfo& info)
-{
-    Attr* imp = V8Attr::toNative(info.Holder());
-    String attrValue = toWebCoreStringWithNullCheck(value);
-    Element* ownerElement = imp->ownerElement();
-
-    if (ownerElement && !V8BindingSecurity::allowSettingSrcToJavascriptURL(V8BindingState::Only(), ownerElement, imp->name(), attrValue))
-        return;
-
-    ExceptionCode ec = 0;
-    imp->setValue(attrValue, ec);
-    if (ec)
-        throwError(ec);
-}
-
-} // namespace WebCore

third_party/WebKit/WebCore/dom/Attr.idl
@@ -32,7 +32,7 @@ module core {
 
         readonly attribute boolean specified;
 
-                 attribute [ConvertNullStringTo=Null, ConvertNullToNullString, CustomSetter] DOMString value 
+                 attribute [ConvertNullStringTo=Null, ConvertNullToNullString] DOMString value 
                      setter raises(DOMException);
 
         // DOM Level 2

commit 08073f3da6451b3ebccb1846e9c4d2f174645ca8

Unreviewed. Fix a Qt build error caused by r60037.
git-svn-id: svn://svn.chromium.org/blink/trunk@60039 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebCore/bindings/js/JSAttrCustom.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CSSHelper.h"
 #include "Document.h"
+#include "Element.h"
 #include "HTMLNames.h"
 
 using namespace JSC;