f9a0fd7de03e0bfe6017cd002f6d6dc27e5e9faa
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-24 23:20:02 +0000

    No review; build fix only.
    Include WKCAImageQueue.h outside the ACCELERATED_COMPOSITING check.
    
    * platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60096 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(ACCELERATED_COMPOSITING)
 #include "GraphicsLayerCACF.h"
 #include "WKCACFLayer.h"
-#include "WKCAImageQueue.h"
 #endif
 
 using namespace std;
