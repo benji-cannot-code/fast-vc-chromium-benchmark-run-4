093fa4934f12370ed6291432b72e65394c901cc1
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-24 23:15:39 +0000

    2010-05-24  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Prepare HTML5TreeBuilder for addition of new HTML5 parser code
            https://bugs.webkit.org/show_bug.cgi?id=39623
    
            Before we start transcribing the parser, we need a place to put it.
    
            This also cleans up HTML5 token to not convert comment and character
            data into AtomicStrings (which makes no sense).
    
            No functionality change, so no new tests.
    
            * html/HTML5Token.h:
            (WebCore::HTML5Token::beginStartTag):
            (WebCore::HTML5Token::beginEndTag):
            (WebCore::HTML5Token::beginCharacter):
            (WebCore::HTML5Token::beginComment):
            (WebCore::HTML5Token::name):
            (WebCore::HTML5Token::adoptDataAsStringImpl):
            (WebCore::HTML5Token::characters):
            (WebCore::HTML5Token::commentData):
            (WebCore::HTML5Token::clearData):
            * html/HTML5TreeBuilder.cpp:
            (WebCore::convertToOldStyle):
            (WebCore::HTML5TreeBuilder::constructTreeFromToken):
            (WebCore::HTML5TreeBuilder::processToken):
            * html/HTML5TreeBuilder.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60095 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTML5TreeBuilder.h
+++ third_party/WebKit/WebCore/html/HTML5TreeBuilder.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Noncopyable.h>
 #include <wtf/OwnPtr.h>
 #include <wtf/PassRefPtr.h>
+#include <wtf/unicode/Unicode.h>
 
 namespace WebCore {
 class Document;
@@ -50,6 +51,7 @@ public:
 
 private:
     PassRefPtr<Node> passTokenToLegacyParser(HTML5Token&);
+    PassRefPtr<Node> processToken(HTML5Token&, UChar currentCharacter = 0);
 
     // We could grab m_document off the lexer if we wanted to save space.
     Document* m_document;
