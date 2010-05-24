dcd6ef79d85b2135bd46efd1f66e15462fb963e5
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-24 23:34:58 +0000

    2010-05-24  Eric Seidel  <eric@webkit.org>
            Unreviewed.  Add wtf/UnusedParam.h include to make Chromium happy.
    
            Chromium does not use a prefix header in order to support
            distcc3.  Other ports all do.  The real fix is to remove
            wtf/UnusedParam.h from the prefix header.
    
            * html/HTML5TreeBuilder.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60098 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTML5TreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/HTML5TreeBuilder.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLParser.h"
 #include "HTMLTokenizer.h"
 #include "NotImplemented.h"
+#include <wtf/UnusedParam.h>
 
 namespace WebCore {
 
