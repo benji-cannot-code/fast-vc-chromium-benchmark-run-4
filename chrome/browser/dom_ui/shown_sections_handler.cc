5053c62c938ecd90c5871d8da7c39b158adb862d
jcivelli@chromium.org <jcivelli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-24 16:06:24 +0000

    Disabling app launcher and enabling apps by default on ChromeOS .
    BUG=3218
    TEST=Open the new tab, the new new tab should be shown. Any installed
         app should be showing in there.
    
    Review URL: http://codereview.chromium.org/2135011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48042 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/shown_sections_handler.cc
+++ chrome/browser/dom_ui/shown_sections_handler.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/pref_service.h"
 #include "chrome/browser/profile.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/extensions/extension.h"
 #include "chrome/common/pref_names.h"
 
 namespace {
@@ -88,7 +89,7 @@ void ShownSectionsHandler::SetFirstAppLauncherRunPref(
   // If we have turned on Apps we want to hide most visited and recent to give
   // more focus to the Apps section. We do not do this in MigrateUserPrefs
   // because the pref version should not depend on command line switches.
-  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kEnableApps) &&
+  if (Extension::AppsAreEnabled() &&
       !pref_service->GetBoolean(prefs::kNTPAppLauncherFirstRun)) {
     int sections = pref_service->GetInteger(prefs::kNTPShownSections);
     sections &= ~THUMB;
@@ -102,7 +103,7 @@ void ShownSectionsHandler::SetFirstAppLauncherRunPref(
 void ShownSectionsHandler::RegisterUserPrefs(PrefService* pref_service) {
   pref_service->RegisterIntegerPref(prefs::kNTPShownSections,
                                     THUMB | RECENT | TIPS | SYNC);
-  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kEnableApps)) {
+  if (Extension::AppsAreEnabled()) {
     pref_service->RegisterBooleanPref(prefs::kNTPAppLauncherFirstRun, false);
   }
 }
