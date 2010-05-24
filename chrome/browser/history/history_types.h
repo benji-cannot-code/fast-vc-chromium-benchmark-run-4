df205b9ea990f5c237c3beca422fa2c3b6e869f8
nshkrob@chromium.org <nshkrob@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-24 23:24:17 +0000

    Adding a mock database layer. Testing support of storage in TopSites.
    BUG=None
    TEST=TopSitesTest
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=48061
    Review URL: http://codereview.chromium.org/2133011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48099 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/history_types.h
+++ chrome/browser/history/history_types.h
@@ -519,6 +519,10 @@ struct MostVisitedURL {
   string16 title;
 
   RedirectList redirects;
+
+  bool operator==(const MostVisitedURL& other) {
+    return url == other.url;
+  }
 };
 
 typedef std::vector<MostVisitedURL> MostVisitedURLList;
