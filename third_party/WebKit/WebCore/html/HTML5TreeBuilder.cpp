c099768df725327ddd77b3d2fdac9293db4e325e
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-25 02:39:52 +0000

    2010-05-24  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Add RCDATA and RAWTEXT suport to the HTML5 parser
            https://bugs.webkit.org/show_bug.cgi?id=39642
    
            Adam Barth wrote half of this patch.
    
            * html/HTML5TreeBuilder.cpp:
            (WebCore::HTML5TreeBuilder::passTokenToLegacyParser):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60116 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTML5TreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/HTML5TreeBuilder.cpp
@@ -31,12 +31,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTML5Lexer.h"
 #include "HTML5Token.h"
 #include "HTMLDocument.h"
+#include "HTMLNames.h"
 #include "HTMLParser.h"
 #include "HTMLTokenizer.h"
 #include "NotImplemented.h"
 #include <wtf/UnusedParam.h>
 
 namespace WebCore {
+
+using namespace HTMLNames;
 
 HTML5TreeBuilder::HTML5TreeBuilder(HTML5Lexer* lexer, HTMLDocument* document, bool reportErrors)
     : m_document(document)
@@ -93,10 +96,17 @@ static void convertToOldStyle(HTML5Token& token, Token& oldStyleToken)
 
 PassRefPtr<Node> HTML5TreeBuilder::passTokenToLegacyParser(HTML5Token& token)
 {
-    if (token.type() == HTML5Token::StartTag && token.name() == "script") {
+    if (token.type() == HTML5Token::StartTag) {
         // This work is supposed to be done by the parser, but
         // when using the old parser for we have to do this manually.
-        m_lexer->setState(HTML5Lexer::ScriptDataState);
+        if (token.name() == scriptTag)
+            m_lexer->setState(HTML5Lexer::ScriptDataState);
+        else if (token.name() == textareaTag || token.name() == titleTag)
+            m_lexer->setState(HTML5Lexer::RCDATAState);
+        else if (token.name() == styleTag || token.name() == iframeTag || token.name() == xmpTag) {
+            // FIXME: noscript, noframes, and noembed may conditionally enter this state as well.
+            m_lexer->setState(HTML5Lexer::RAWTEXTState);
+        }
     }
 
     if (token.type() == HTML5Token::DOCTYPE) {
