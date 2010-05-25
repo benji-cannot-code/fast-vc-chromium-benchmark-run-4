110a71505044a9f9c6fde12f70473eb7432ca746
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-25 01:56:41 +0000

    Try to fix the Qt build by including Settings.h
    git-svn-id: svn://svn.chromium.org/blink/trunk@60113 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebframe.cpp
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScriptSourceCode.h"
 #include "ScriptValue.h"
 #include "Scrollbar.h"
+#include "Settings.h"
 #include "SelectionController.h"
 #include "SubstituteData.h"
 #include "SVGSMILElement.h"
