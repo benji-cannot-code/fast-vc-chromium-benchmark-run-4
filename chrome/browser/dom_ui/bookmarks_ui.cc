25f11ff990384fa082ecec25a654cd594e0eb49f
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-25 00:02:58 +0000

    Remove some unneeded #includes.
    BUG=none
    TEST=everything builds
    
    Review URL: http://codereview.chromium.org/2089025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48106 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/bookmarks_ui.cc
+++ chrome/browser/dom_ui/bookmarks_ui.cc
@@ -9,12 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/ref_counted_memory.h"
 #include "base/singleton.h"
-#include "base/string_piece.h"
-#include "base/string_util.h"
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/dom_ui/chrome_url_data_manager.h"
 #include "chrome/common/url_constants.h"
-#include "grit/browser_resources.h"
 #include "grit/theme_resources.h"
 
 ////////////////////////////////////////////////////////////////////////////////
--- chrome/browser/dom_ui/downloads_ui.h
+++ chrome/browser/dom_ui/downloads_ui.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_DOM_UI_DOWNLOADS_UI_H_
 #define CHROME_BROWSER_DOM_UI_DOWNLOADS_UI_H_
 
-#include <vector>
-
 #include "chrome/browser/dom_ui/dom_ui.h"
 
 class RefCountedMemory;
