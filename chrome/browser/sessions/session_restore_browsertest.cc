f18976c606529d1aab3e1b1df2e8fbdc313984ec
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-25 19:35:58 +0000

    Marking RestoreOnNewWindowWithNoTabbedBrowsers as FAILS_ on non-Windows.
    It was enabled in r48104, but fails on linux views dbg.
    
    BUG=39476
    TEST=SessionRestoreTest.RestoreOnNewWindowWithNoTabbedBrowsers 
    
    Review URL: http://codereview.chromium.org/2165006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48176 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_restore_browsertest.cc
+++ chrome/browser/sessions/session_restore_browsertest.cc
@@ -13,10 +13,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef InProcessBrowserTest SessionRestoreTest;
 
+#if defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
+// http://crbug.com/39476
+#define MAYBE_RestoreOnNewWindowWithNoTabbedBrowsers \
+        FAILS_RestoreOnNewWindowWithNoTabbedBrowsers
+#else
+#define MAYBE_RestoreOnNewWindowWithNoTabbedBrowsers \
+        RestoreOnNewWindowWithNoTabbedBrowsers
+#endif
+
 // Makes sure when session restore is triggered in the same process we don't end
 // up with an extra tab.
 IN_PROC_BROWSER_TEST_F(SessionRestoreTest,
-                       RestoreOnNewWindowWithNoTabbedBrowsers) {
+                       MAYBE_RestoreOnNewWindowWithNoTabbedBrowsers) {
   if (browser_defaults::kRestorePopups)
     return;
 
