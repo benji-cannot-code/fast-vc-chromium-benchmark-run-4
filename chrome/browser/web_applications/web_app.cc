7add49349ea501aec251159a6fe45c2893eb92fa
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-26 18:15:11 +0000

    Allow "chrome-extension://" to be used as web app url.
    BUG=29201
    TEST=Verify a web app shortcut for "chrome-extension://" url could be created.
    
    Review URL: http://codereview.chromium.org/2193002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48294 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_applications/web_app.cc
+++ chrome/browser/web_applications/web_app.cc
@@ -710,6 +710,7 @@ bool IsValidUrl(const GURL& url) {
       chrome::kFtpScheme,
       chrome::kHttpScheme,
       chrome::kHttpsScheme,
+      chrome::kExtensionScheme,
   };
 
   for (size_t i = 0; i < arraysize(kValidUrlSchemes); ++i) {
