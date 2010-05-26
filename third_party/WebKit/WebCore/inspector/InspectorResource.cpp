69b6ad7145d223fd0257195a5c1c511da541a8d8
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-26 02:35:12 +0000

    2010-05-25  Yuta Kitamura  <yutak@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Show HTTP status message sent from server in Resources tab.
    
            This patch obtains an HTTP status message of each resource and pass it to
            the front end of Web Inspector. The status message is shown in "Headers" tab in
            the detail view of that resource, along with the HTTP status code.
    
            Web Inspector does not respect HTTP status message
            https://bugs.webkit.org/show_bug.cgi?id=39595
    
            * inspector/InspectorResource.cpp:
            (WebCore::InspectorResource::updateResponse):
            (WebCore::InspectorResource::updateScriptObject):
            * inspector/InspectorResource.h:
            * inspector/front-end/Resource.js:
            (WebInspector.Resource.CompareByTransferSize):
            * inspector/front-end/ResourceView.js:
            (WebInspector.ResourceView.prototype._refreshHTTPInformation):
            * inspector/front-end/inspector.js:
            (WebInspector.updateResource):
            * inspector/front-end/utilities.js:
            (String.prototype.escapeHTML): Escape '"' so that we can escape messages
            that may occur inside HTML attributes.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorResource.cpp
+++ third_party/WebKit/WebCore/inspector/InspectorResource.cpp
@@ -126,6 +126,7 @@ void InspectorResource::updateResponse(const ResourceResponse& response)
     }
     m_responseHeaderFields = response.httpHeaderFields();
     m_responseStatusCode = response.httpStatusCode();
+    m_responseStatusText = response.httpStatusText();
     m_suggestedFilename = response.suggestedFilename();
 
     m_changes.set(ResponseChange);
@@ -168,6 +169,7 @@ void InspectorResource::updateScriptObject(InspectorFrontend* frontend)
         jsonObject.set("suggestedFilename", m_suggestedFilename);
         jsonObject.set("expectedContentLength", m_expectedContentLength);
         jsonObject.set("statusCode", m_responseStatusCode);
+        jsonObject.set("statusText", m_responseStatusText);
         jsonObject.set("suggestedFilename", m_suggestedFilename);
         ScriptObject responseHeaders = frontend->newScriptObject();
         populateHeadersObject(&responseHeaders, m_responseHeaderFields);
--- third_party/WebKit/WebCore/inspector/front-end/inspector.js
+++ third_party/WebKit/WebCore/inspector/front-end/inspector.js
@@ -1161,6 +1161,7 @@ WebInspector.updateResource = function(identifier, payload)
         resource.suggestedFilename = payload.suggestedFilename;
         resource.expectedContentLength = payload.expectedContentLength;
         resource.statusCode = payload.statusCode;
+        resource.statusText = payload.statusText;
         resource.suggestedFilename = payload.suggestedFilename;
         resource.responseHeaders = payload.responseHeaders;
     }
