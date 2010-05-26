de04722680f278994ac48f50b2b61dea6b874ed8
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-26 11:54:04 +0000

    2010-05-26  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Jeremy Orlow.
    
            Style fix in JSEventCustom.cpp
            https://bugs.webkit.org/show_bug.cgi?id=39727
    
            Conditional includes should be all together after the
            unconditional includes.
    
            * bindings/js/JSEventCustom.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60231 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSEventCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSEventCustom.cpp
@@ -41,10 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSCompositionEvent.h"
 #include "JSDeviceOrientationEvent.h"
 #include "JSErrorEvent.h"
-#if ENABLE(INDEXED_DATABASE)
-#include "JSIDBErrorEvent.h"
-#include "JSIDBSuccessEvent.h"
-#endif
 #include "JSKeyboardEvent.h"
 #include "JSMessageEvent.h"
 #include "JSMouseEvent.h"
@@ -61,10 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSXMLHttpRequestProgressEvent.h"
 #include "BeforeLoadEvent.h"
 #include "ErrorEvent.h"
-#if ENABLE(INDEXED_DATABASE)
-#include "IDBErrorEvent.h"
-#include "IDBSuccessEvent.h"
-#endif
 #include "KeyboardEvent.h"
 #include "MessageEvent.h"
 #include "MouseEvent.h"
@@ -94,6 +86,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(TOUCH_EVENTS)
 #include "JSTouchEvent.h"
 #include "TouchEvent.h"
+#endif
+
+#if ENABLE(INDEXED_DATABASE)
+#include "IDBErrorEvent.h"
+#include "IDBSuccessEvent.h"
+#include "JSIDBErrorEvent.h"
+#include "JSIDBSuccessEvent.h"
 #endif
 
 using namespace JSC;
