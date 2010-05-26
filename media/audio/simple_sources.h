c8765397021a08ca0196f383444fb25fed0da46f
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-26 21:39:32 +0000

    Removed PushSource::Packet. SeekableBuffer.current_time() fixed to return kInvalidTimestamp when the time is unknown. TEST=media_unittests BUG=28654
    Review URL: http://codereview.chromium.org/2140001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48329 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/simple_sources.h
+++ media/audio/simple_sources.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/lock.h"
 #include "media/audio/audio_output.h"
+#include "media/base/seekable_buffer.h"
 
 // An audio source that produces a pure sinusoidal tone.
 class SineWaveAudioSource : public AudioOutputStream::AudioSourceCallback {
@@ -78,21 +79,12 @@ class PushSource : public AudioOutputStream::AudioSourceCallback,
   void ClearAll();
 
  private:
-  // Defines the unit of playback. We own the memory pointed by |buffer|.
-  struct Packet {
-    char* buffer;
-    uint32 size;
-  };
-
   // Free acquired resources.
   void CleanUp();
 
-  typedef std::list<Packet> PacketList;
-  PacketList packets_;
-  uint32 buffered_bytes_;
-  uint32 front_buffer_consumed_;
-  // Serialize access to packets_ and buffered_bytes_ using this lock.
-  Lock lock_;
+  media::SeekableBuffer buffer_;
+  // Serialize access to |buffer_| using this lock.
+  Lock buffer_lock_;
 };
 
 #endif  // MEDIA_AUDIO_SIMPLE_SOURCES_H_
