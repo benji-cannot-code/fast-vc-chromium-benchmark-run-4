bf582cc9fc00f513bcfee374e9892e737d577b48
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-26 23:00:31 +0000

    2010-05-26  Nate Chapin  <japhet@chromium.org>
            Reviewed by Adam Barth.
    
            Factor PageCache functionality out of FrameLoader and into
            PageCache.
    
            https://bugs.webkit.org/show_bug.cgi?id=39382
    
            Refactor only, so no new tests.
    
            * history/PageCache.cpp:
            (WebCore::pageCacheLogPrefix):
            (WebCore::pageCacheLog):
            (WebCore::logCanCacheFrameDecision):
            (WebCore::logCanCachePageDecision):
            (WebCore::PageCache::canCachePageContainingThisFrame):
            (WebCore::PageCache::canCache):
            (WebCore::PageCache::add):
            (WebCore::PageCache::get):
            * history/PageCache.h:
            * loader/DocumentLoader.cpp:
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::commitProvisionalLoad):
            (WebCore::FrameLoader::prepareForCachedPageRestore):
            (WebCore::FrameLoader::continueLoadAfterNavigationPolicy):
            (WebCore::FrameLoader::loadProvisionalItemFromCachedPage):
            (WebCore::FrameLoader::navigateToDifferentDocument):
            * loader/FrameLoader.h:
            (WebCore::FrameLoader::quickRedirectComing):
            * svg/graphics/SVGImage.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/WebCore/loader/FrameLoader.h
@@ -308,7 +308,7 @@ public:
     void setTitle(const String&);
     void setIconURL(const String&);
 
-    void commitProvisionalLoad(PassRefPtr<CachedPage>);
+    void commitProvisionalLoad();
     bool isLoadingFromCachedPage() const { return m_loadingFromCachedPage; }
 
     bool committingFirstRealLoad() const { return !m_creatingInitialEmptyDocument && !m_committedFirstRealDocumentLoad; }
@@ -352,7 +352,9 @@ public:
     bool isDisplayingInitialEmptyDocument() const { return m_isDisplayingInitialEmptyDocument; }
 
     void clear(bool clearWindowProperties = true, bool clearScriptObjects = true, bool clearFrameView = true);
-    
+
+    bool quickRedirectComing() const { return m_quickRedirectComing; }
+
     bool shouldClose();
 
 private:
@@ -373,8 +375,7 @@ private:
     void navigateWithinDocument(HistoryItem*);
     void navigateToDifferentDocument(HistoryItem*, FrameLoadType);
     
-    bool loadProvisionalItemFromCachedPage();
-    void cachePageForHistoryItem(HistoryItem*);
+    void loadProvisionalItemFromCachedPage();
     void pageHidden();
 
     void receivedFirstData();
@@ -415,7 +416,7 @@ private:
     void setState(FrameState);
 
     void closeOldDataSources();
-    void open(CachedPage&);
+    void prepareForCachedPageRestore();
 
     void updateHistoryAfterClientRedirect();
 
