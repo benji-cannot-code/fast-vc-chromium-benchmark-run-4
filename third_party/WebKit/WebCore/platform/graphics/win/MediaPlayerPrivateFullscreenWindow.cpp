fe111c5caa0078914d5f317c865efd5eb8db56b7
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-26 03:32:55 +0000

    Build fix.
    * platform/graphics/win/MediaPlayerPrivateFullscreenWindow.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60207 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateFullscreenWindow.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateFullscreenWindow.cpp
@@ -25,13 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-
 #include "MediaPlayerPrivateFullscreenWindow.h"
 
+#include "IntRect.h"
 #include "WebCoreInstanceHandle.h"
 #include <CoreGraphics/CGColor.h>
 #include <windows.h>
-#include <WebCore/IntRect.h>
 
 namespace WebCore {
 
