650666ed86df9602ed627dd21372ee74eeff25a4
jiesun@google.com <jiesun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-26 21:37:54 +0000

    refactoring decoder interface
    1. install permanent buffer exchange  callback.
    2. render provide buffer in read=>fillthisbuffer.
    3. for ffmpeg path, the provided buffer is just dummy. it had no relation to decoded buffer. so as to keep the code almost same.
    
    Review URL: http://codereview.chromium.org/2101022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48328 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/decoder_base_unittest.cc
+++ media/filters/decoder_base_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+using ::testing::_;
 using ::testing::NotNull;
 using ::testing::StrictMock;
 
@@ -26,8 +27,6 @@ class MockDecoderOutput : public media::StreamSample {
   MOCK_CONST_METHOD0(IsEndOfStream, bool());
 };
 
-class MockDecoder : public media::MediaFilter {};
-
 class MockBuffer : public media::Buffer {
  public:
   MockBuffer() {}
@@ -37,6 +36,24 @@ class MockBuffer : public media::Buffer {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(MockBuffer);
+};
+
+class MockDecoder : public media::MediaFilter {
+ public:
+  typedef Callback1<scoped_refptr<MockDecoderOutput> >::Type
+      FillBufferDoneCallback;
+  void set_fill_buffer_done_callback_(FillBufferDoneCallback* callback) {
+    fill_buffer_done_callback_.reset(callback);
+  }
+  FillBufferDoneCallback* fill_buffer_done_callback() {
+    return fill_buffer_done_callback_.get();
+  }
+  scoped_ptr<FillBufferDoneCallback> fill_buffer_done_callback_;
+};
+
+class MockDecoderCallback {
+ public:
+  MOCK_METHOD1(OnReadComplete, void(scoped_refptr<MockDecoderOutput> output));
 };
 
 class MockDecoderImpl : public media::DecoderBase<
@@ -61,16 +78,6 @@ class MockDecoderImpl : public media::DecoderBase<
   FRIEND_TEST(media::DecoderBaseTest, FlowControl);
 
   DISALLOW_COPY_AND_ASSIGN(MockDecoderImpl);
-};
-
-class MockDecoderReadCallback {
- public:
-  MockDecoderReadCallback() {}
-  virtual ~MockDecoderReadCallback() {}
-  MOCK_METHOD1(ReadCallback, void(MockDecoderOutput* output));
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(MockDecoderReadCallback);
 };
 
 }  // namespace
@@ -108,6 +115,9 @@ ACTION(CompleteDemuxRequest) {
 TEST(DecoderBaseTest, FlowControl) {
   MessageLoop message_loop;
   scoped_refptr<MockDecoderImpl> decoder = new MockDecoderImpl();
+  MockDecoderCallback read_callback;
+  decoder->set_fill_buffer_done_callback_(
+      NewCallback(&read_callback, &MockDecoderCallback::OnReadComplete));
   scoped_refptr<MockDemuxerStream> demuxer_stream = new MockDemuxerStream();
   StrictMock<MockFilterCallback> callback;
   decoder->set_message_loop(&message_loop);
@@ -121,7 +131,6 @@ TEST(DecoderBaseTest, FlowControl) {
   message_loop.RunAllPending();
 
   // Read.
-  StrictMock<MockDecoderReadCallback> read_callback;
   std::vector<scoped_refptr<Buffer> > decode_requests;
   EXPECT_CALL(*demuxer_stream, Read(NotNull()))
       .Times(2)
@@ -129,16 +138,13 @@ TEST(DecoderBaseTest, FlowControl) {
   EXPECT_CALL(*decoder, DoDecode(NotNull()))
       .Times(2)
       .WillRepeatedly(SaveDecodeRequest(&decode_requests));
-  decoder->Read(
-      NewCallback(reinterpret_cast<MockDecoderReadCallback*>(&read_callback),
-                  &MockDecoderReadCallback::ReadCallback));
-  decoder->Read(
-      NewCallback(reinterpret_cast<MockDecoderReadCallback*>(&read_callback),
-                  &MockDecoderReadCallback::ReadCallback));
+  scoped_refptr<MockDecoderOutput> output;
+  decoder->FillThisBuffer(output);
+  decoder->FillThisBuffer(output);
   message_loop.RunAllPending();
 
   // Fulfill the decode request.
-  EXPECT_CALL(read_callback, ReadCallback(NotNull())).Times(2);
+  EXPECT_CALL(read_callback, OnReadComplete(_)).Times(2);
   for (size_t i = 0; i < decode_requests.size(); ++i) {
     decoder->EnqueueResult(new MockDecoderOutput());
     decoder->OnDecodeComplete();
