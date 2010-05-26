16650a59ac7a7dd7d0033dbd89954b405cb2f6f1
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-26 21:21:57 +0000

    Revert 48315 - Linux: Initial scaffolding for default plugin.
    Basically the linux version of http://codereview.chromium.org/2075006
    
    BUG=10952
    TEST=Go to http://www.adobe.com/shockwave/welcome/. The task manager should show that the default plugin is running, and the "Missing Plugin" text should look slightly different than it did before. Also, the plugin area is now grey instead of transparent. (this test depends on a small webkit shown below)
    
    Requires commenting out
    
        if (objectContentType(url, mimeType) != ObjectContentNetscapePlugin)
            return 0;
    
    in WebKit/WebKit/chromium/src/FrameLoaderClientImpl.cpp's createPlugin() as well, else the default plugin won't load.
    
    Review URL: http://codereview.chromium.org/2079016
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/2291004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48325 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/default_plugin/plugin_impl_mac.h
+++ webkit/default_plugin/plugin_impl_mac.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "gfx/native_widget_types.h"
 #include "third_party/npapi/bindings/npapi.h"
+#include "gfx/native_widget_types.h"
 
 // Possible plugin installer states.
 enum PluginInstallerState {
@@ -54,7 +54,7 @@ class PluginInstallerImpl {
   // argv
   //   Pointer to the arguments.
   // Returns true on success.
-  bool Initialize(void* module_handle, NPP instance, NPMIMEType mime_type,
+  bool Initialize(void *module_handle, NPP instance, NPMIMEType mime_type,
                   int16 argc, char* argn[], char* argv[]);
 
   // Informs the plugin of its window information.
@@ -278,8 +278,7 @@ class PluginInstallerImpl {
   // Dimensions of the plugin
   uint32_t width_;
   uint32_t height_;
-
-  DISALLOW_COPY_AND_ASSIGN(PluginInstallerImpl);
+  DISALLOW_EVIL_CONSTRUCTORS(PluginInstallerImpl);
 };
 
 
