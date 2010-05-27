95f2effcc116220c05f1ea64a4fb68db3e661afd
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-27 10:39:32 +0000

    2010-05-27  Joone Hur  <joone@kldp.org>
            Reviewed by Xan Lopez.
    
            Add GtkVersioning.h in ScrollbackGtk.cpp for maintaining compatibility with the previous GTK+
    
            https://bugs.webkit.org/show_bug.cgi?id=39567
    
            * platform/gtk/ScrollbarGtk.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60288 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/ScrollbarGtk.cpp
+++ third_party/WebKit/WebCore/platform/gtk/ScrollbarGtk.cpp
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "ScrollbarGtk.h"
 
+#include "GtkVersioning.h"
 #include "IntRect.h"
 #include "GraphicsContext.h"
 #include "FrameView.h"
