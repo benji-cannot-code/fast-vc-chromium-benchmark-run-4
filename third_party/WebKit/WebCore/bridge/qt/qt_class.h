43575f1577b057c27972f277b456b0579d3993b2
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-27 20:01:37 +0000

    2010-05-27  Anders Bakken  <agbakken@gmail.com>
            Reviewed by David Levin.
    
            qt_class.h has coding-style errors
            https://bugs.webkit.org/show_bug.cgi?id=39742
    
            Fix webkit coding style issues in qt_class.h
    
            * bridge/qt/qt_class.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60319 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_class.h
+++ third_party/WebKit/WebCore/bridge/qt/qt_class.h
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
-#ifndef BINDINGS_QT_CLASS_H_
-#define BINDINGS_QT_CLASS_H_
+#ifndef qt_class_h
+#define qt_class_h
 
 #include "Bridge.h"
 #include "qglobal.h"
