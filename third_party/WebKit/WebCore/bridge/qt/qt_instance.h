c4a2780a00fb949cbe844d7c665fec1105ebfb3f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-27 20:14:21 +0000

    2010-05-27  Anders Bakken  <agbakken@gmail.com>
            Reviewed by David Levin.
    
            qt_instance.h has coding-style errors
            https://bugs.webkit.org/show_bug.cgi?id=39743
    
            Fix webkit coding style issues in qt_instance.h
    
            * bridge/qt/qt_instance.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_instance.h
+++ third_party/WebKit/WebCore/bridge/qt/qt_instance.h
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
-#ifndef BINDINGS_QT_INSTANCE_H_
-#define BINDINGS_QT_INSTANCE_H_
+#ifndef qt_instance_h
+#define qt_instance_h
 
 #include "Bridge.h"
 #include "runtime_root.h"
