c36c9b73d96ae6c0d240e4df1a429110a377dcd9
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-27 02:07:27 +0000

    Modified chat manager extension to become an app extension.
    Patch by seh@chromium.org:
    http://codereview.chromium.org/2249001/show
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2252004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48352 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chat_manager/js/chatbridgeeventtypes.js
+++ chrome/browser/resources/chat_manager/js/chatbridgeeventtypes.js
@@ -13,6 +13,5 @@ var ChatBridgeEventTypes = {
   START_VOICE: 'startVoice',
   CENTRAL_USER_SET: 'centralJidSet',
   CENTRAL_USER_UPDATE: 'centralJidUpdate',
-  CENTRAL_USER_WATCHER: 'getCentralJid',
-  OPEN_CENTRAL_ROSTER: 'openCentralRoster'
+  CENTRAL_USER_WATCHER: 'getCentralJid'
 };
