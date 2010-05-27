ea6df20890d926fb3a50a998884ae84880375b86
denisromanov@chromium.org <denisromanov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-27 19:44:53 +0000

    Adding initial implementation of the PartnerCustomization classes.
    BUG=chromiumos:3176
    TEST=Run out/Debug/unit_tests. Run out/Debug/chrome --login-manager --startup-manifest=./chrome/browser/chromeos/testdata/startup_manifest.json. There should be no asserts.
    
    Review URL: http://codereview.chromium.org/2101021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48424 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"  // For NOTREACHED.
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/cros/login_library.h"
+#include "chrome/browser/chromeos/customization_document.h"
 #include "chrome/browser/chromeos/login/account_screen.h"
 #include "chrome/browser/chromeos/login/background_view.h"
 #include "chrome/browser/chromeos/login/existing_user_controller.h"
@@ -164,6 +165,7 @@ WizardController* WizardController::default_controller_ = NULL;
 
 ///////////////////////////////////////////////////////////////////////////////
 // WizardController, public:
+
 WizardController::WizardController()
     : widget_(NULL),
       background_widget_(NULL),
@@ -297,6 +299,11 @@ void WizardController::SetStatusAreaVisible(bool visible) {
   if (background_view_) {
     background_view_->SetStatusAreaVisible(visible);
   }
+}
+
+void WizardController::SetCustomization(
+    const chromeos::StartupCustomizationDocument* customization) {
+  customization_.reset(customization);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -475,7 +482,21 @@ void ShowLoginWizard(const std::string& first_screen_name,
     }
   }
 
+  // Load partner customization startup manifest if needed.
+  scoped_ptr<chromeos::StartupCustomizationDocument> customization;
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kStartupManifest)) {
+    customization.reset(new chromeos::StartupCustomizationDocument());
+    FilePath manifest_path =
+        CommandLine::ForCurrentProcess()->GetSwitchValuePath(
+            switches::kStartupManifest);
+    bool manifest_loaded = customization->LoadManifestFromFile(manifest_path);
+    DCHECK(manifest_loaded) << manifest_path.value();
+  }
+
+  // Create and show the wizard.
   WizardController* controller = new WizardController();
+  controller->SetCustomization(customization.release());
   controller->ShowBackground(screen_bounds);
   controller->Init(first_screen_name, screen_bounds, true);
   controller->Show();
