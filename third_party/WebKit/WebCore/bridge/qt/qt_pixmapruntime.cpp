10626f34b9329577ea75d36c32609f98501794dd
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-27 14:36:23 +0000

    2010-05-27  Anders Bakken  <agbakken@gmail.com>
            Reviewed by David Levin.
    
            qt_pixmapruntime.cpp has coding-style errors
            https://bugs.webkit.org/show_bug.cgi?id=39745
    
            Fix webkit coding style issues in qt_pixmapruntime.cpp
    
            * bridge/qt/qt_pixmapruntime.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60300 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_pixmapruntime.cpp
+++ third_party/WebKit/WebCore/bridge/qt/qt_pixmapruntime.cpp
@@ -32,9 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QImage>
 #include <QPixmap>
 #include <QVariant>
+#include <runtime_method.h>
 #include <runtime_object.h>
 #include <runtime_root.h>
-#include <runtime_method.h>
 
 using namespace WebCore;
 namespace JSC {
