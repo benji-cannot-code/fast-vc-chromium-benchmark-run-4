d83f8ee424076677399d8f874968416e37442a67
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-27 08:17:46 +0000

    2010-05-27  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Add <pre>/<listing> hack to HTML5Lexer to fix the last remaining HTML5 test suite regressions
            https://bugs.webkit.org/show_bug.cgi?id=39818
    
            * html5lib/runner-expected-html5.txt:
    2010-05-27  Eric Seidel  <eric@webkit.org>
    
            Reviewed by Adam Barth.
    
            Add <pre>/<listing> hack to HTML5Lexer to fix the last remaining HTML5 test suite regressions
            https://bugs.webkit.org/show_bug.cgi?id=39818
    
            HTML parsers are supposed to ignore the first \n after a <pre> or <listing> tag
            for authoring convenience.  Our new HTML5Lexer didn't have this hack yet
            so there were 4 HTML5 tests failing.  Fixing this fixed the last of the HTML5
            test suite regressions using the HTML5Lexer vs the old lexer.
    
            * html/HTML5Lexer.cpp:
            (WebCore::HTML5Lexer::reset):
            (WebCore::HTML5Lexer::nextToken):
            * html/HTML5Lexer.h:
            (WebCore::HTML5Lexer::skipLeadingNewLineForListing):
            * html/HTML5TreeBuilder.cpp:
            (WebCore::HTML5TreeBuilder::passTokenToLegacyParser):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60280 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTML5Lexer.h
+++ third_party/WebKit/WebCore/html/HTML5Lexer.h
@@ -123,6 +123,10 @@ namespace WebCore {
 
         void setState(State state) { m_state = state; }
 
+        // Hack to skip leading newline in <pre>/<listing> for authoring ease.
+        // http://www.whatwg.org/specs/web-apps/current-work/multipage/tokenization.html#parsing-main-inbody
+        void skipLeadingNewLineForListing() { m_skipLeadingNewLineForListing = true; }
+
     private:
         inline void emitCharacter(UChar);
         inline void emitParseError();
@@ -150,6 +154,7 @@ namespace WebCore {
         // this member might be pointing to unallocated memory.
         HTML5Token* m_token;
 
+        bool m_skipLeadingNewLineForListing;
         bool m_emitPending;
 
         // http://www.whatwg.org/specs/web-apps/current-work/#temporary-buffer
