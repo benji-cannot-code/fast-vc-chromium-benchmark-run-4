5c1b5cbf32ce3fcc3899904915a0fd3a97c7e985
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-27 15:26:18 +0000

    First cut at GPS support in chrome linux
    Uses libgps / gpsd to get gps fixes. Modified the location arbitrator to use GPS instead of network location provider.
    Current version requires manual code edit to enable (ENABLE_LIBGPS_LOCATION_PROVIDER)  See http://docs.google.com/View?id=dfbnm49n_1g8ktswhr for demo instructions (work in progress)
    
    BUG=43868
    TEST=Built with ENABLE_LIBGPS_LOCATION_PROVIDER and used gpsd's gpsfake script (see linked doc above)
    
    Review URL: http://codereview.chromium.org/2032010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48393 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/geolocation/location_provider.cc
+++ chrome/browser/geolocation/location_provider.cc
@@ -66,7 +66,9 @@ void LocationProviderBase::InformListenersOfMovement() {
   }
 }
 
-// Currently no platforms have a GPS location provider.
+// Currently only Linux has a GPS provider.
+#if !defined(OS_LINUX)
 LocationProviderBase* NewGpsLocationProvider() {
   return NULL;
 }
+#endif
