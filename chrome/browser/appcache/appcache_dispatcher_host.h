9688ee967e904dc3e50df38b6e508509e8eef1b3
michaeln@chromium.org <michaeln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-28 23:37:20 +0000

    Defend against messages being received prior to having been initialized.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2321002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48542 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/appcache/appcache_dispatcher_host.h
+++ chrome/browser/appcache/appcache_dispatcher_host.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
 #include "chrome/browser/appcache/appcache_frontend_proxy.h"
+#include "chrome/browser/renderer_host/resource_dispatcher_host.h"
 #include "ipc/ipc_message.h"
 #include "webkit/appcache/appcache_backend_impl.h"
 
@@ -21,8 +22,8 @@ class URLRequestContextGetter;
 
 // Handles appcache related messages sent to the main browser process from
 // its child processes. There is a distinct host for each child process.
-// Messages are handled on the IO thread. The ResourceMessageFilter creates
-// an instance and delegates calls to it.
+// Messages are handled on the IO thread. The ResourceMessageFilter and
+// WorkerProcessHost create an instance and delegates calls to it.
 class AppCacheDispatcherHost {
  public:
   // Constructor for use on the IO thread.
@@ -33,8 +34,7 @@ class AppCacheDispatcherHost {
   explicit AppCacheDispatcherHost(
       URLRequestContextGetter* request_context_getter);
 
-  void Initialize(IPC::Message::Sender* sender, int process_id,
-                  base::ProcessHandle process_handle);
+  void Initialize(ResourceDispatcherHost::Receiver* receiver);
   bool OnMessageReceived(const IPC::Message& msg, bool* msg_is_ok);
 
   int process_id() const { return backend_impl_.process_id(); }
@@ -80,8 +80,9 @@ class AppCacheDispatcherHost {
   scoped_ptr<appcache::GetStatusCallback> get_status_callback_;
   scoped_ptr<appcache::StartUpdateCallback> start_update_callback_;
   scoped_ptr<appcache::SwapCacheCallback> swap_cache_callback_;
-  base::ProcessHandle process_handle_;
   scoped_ptr<IPC::Message> pending_reply_msg_;
+
+  ResourceDispatcherHost::Receiver* receiver_;
 
   DISALLOW_COPY_AND_ASSIGN(AppCacheDispatcherHost);
 };
