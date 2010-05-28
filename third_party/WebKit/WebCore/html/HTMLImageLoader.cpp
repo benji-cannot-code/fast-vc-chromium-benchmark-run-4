fcd4d3decdccbb46dbdfe61a234d2a56aa2d9271
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-28 00:44:59 +0000

    2010-05-27  Nathan Lawrence  <nlawrence@apple.com>
            Reviewed by Geoffrey Garen.
    
            https://bugs.webkit.org/show_bug.cgi?id=39460
    
            Fixes the issue where images prefetched by JavaScript do not report
            their memory usage to the GC.
    
            There is a new test manual-tests/image-prefetch-stress.html that loads
            a new 4MB image every half a second.
    
            * html/HTMLImageLoader.cpp:
            (WebCore::HTMLImageLoader::notifyFinished):
            * manual-tests/image-prefetch-stress.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLImageLoader.cpp
+++ third_party/WebKit/WebCore/html/HTMLImageLoader.cpp
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
- * Copyright (C) 2004, 2005, 2006, 2007 Apple Inc. All rights reserved.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2010 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -28,8 +28,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Element.h"
 #include "Event.h"
 #include "EventNames.h"
+#include "HTMLImageElement.h"
 #include "HTMLNames.h"
 #include "HTMLObjectElement.h"
+
+#if USE(JSC)
+#include "JSDOMWindowBase.h"
+#endif
 
 namespace WebCore {
 
@@ -61,6 +66,17 @@ void HTMLImageLoader::notifyFinished(CachedResource*)
 
     Element* elem = element();
     ImageLoader::notifyFinished(cachedImage);
+
+#if USE(JSC)
+    if (!cachedImage->errorOccurred() && !cachedImage->httpStatusCodeErrorOccurred()
+        && (elem->hasTagName(HTMLNames::imgTag) || elem->hasTagName(HTMLNames::imageTag)) ) {
+        HTMLImageElement* img = static_cast<HTMLImageElement*>(elem);
+        if (!img->inDocument()) {
+            JSC::JSGlobalData* globalData = JSDOMWindowBase::commonJSGlobalData();
+            globalData->heap.reportExtraMemoryCost(cachedImage->encodedSize());
+        }
+    }
+#endif
 
     if ((cachedImage->errorOccurred() || cachedImage->httpStatusCodeErrorOccurred()) && elem->hasTagName(HTMLNames::objectTag))
         static_cast<HTMLObjectElement*>(elem)->renderFallbackContent();
