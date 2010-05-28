771107c12bf5e5536e777b9107f0f41d514ea4c8
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-28 01:51:56 +0000

    Adding a TearDown() method to class LiveSyncTest.
    The LiveSyncTest class derives from InProcessBrowserTest and overrides
    the SetUp method to account for the case when a local sync server needs
    to be started. However, there isn't a corresponding override for the
    TearDown method that stops the local sync server. This changelist adds
    an overload for TearDown that stops the local sync server if one was
    started in SetUp.
    
    BUG=45181
    TEST=sync_integration_tests
    
    Review URL: http://codereview.chromium.org/2260001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48457 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/live_sync_test.cc
+++ chrome/test/live_sync/live_sync_test.cc
@@ -54,7 +54,7 @@ class BookmarkLoadObserver : public BookmarkModelObserver {
   DISALLOW_COPY_AND_ASSIGN(BookmarkLoadObserver);
 };
 
-LiveSyncTest::LiveSyncTest() {
+LiveSyncTest::LiveSyncTest() : started_local_test_server_(false) {
 }
 
 LiveSyncTest::~LiveSyncTest() {
