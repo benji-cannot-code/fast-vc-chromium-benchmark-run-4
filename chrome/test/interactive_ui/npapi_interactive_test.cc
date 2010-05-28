dfddc538266622e0ddf1aefa4f662553b0e393e0
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-28 20:47:16 +0000

    Make SelfDeletePluginInvokeAlert an interactive_ui test and reenable. It requires the screen to be unlocked to run.
    BUG=44958
    
    Review URL: http://codereview.chromium.org/2321005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48523 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/interactive_ui/npapi_interactive_test.cc
+++ chrome/test/interactive_ui/npapi_interactive_test.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const char kTestCompleteCookie[] = "status";
 const char kTestCompleteSuccess[] = "OK";
+static const FilePath::CharType* kTestDir = FILE_PATH_LITERAL("npapi");
 
 // Tests if a plugin executing a self deleting script in the context of
 // a synchronous mousemove works correctly
@@ -99,4 +100,23 @@ TEST_F(NPAPIVisiblePluginTester, GetURLRequest404Response) {
 
   WaitForFinish("geturl_404_response", "1", url, kTestCompleteCookie,
                 kTestCompleteSuccess, action_max_timeout_ms());
+}
+
+// Tests if a plugin executing a self deleting script using Invoke with
+// a modal dialog showing works without crashing or hanging
+TEST_F(NPAPIVisiblePluginTester, SelfDeletePluginInvokeAlert) {
+  const FilePath test_case(
+      FILE_PATH_LITERAL("self_delete_plugin_invoke_alert.html"));
+  GURL url = ui_test_utils::GetTestUrl(FilePath(kTestDir), test_case);
+  ASSERT_NO_FATAL_FAILURE(NavigateToURL(url));
+
+  // Wait for the alert dialog and then close it.
+  ASSERT_TRUE(automation()->WaitForAppModalDialog());
+  scoped_refptr<WindowProxy> window(automation()->GetActiveWindow());
+  ASSERT_TRUE(window.get());
+  ASSERT_TRUE(window->SimulateOSKeyPress(base::VKEY_ESCAPE, 0));
+
+  WaitForFinish("self_delete_plugin_invoke_alert", "1", url,
+                kTestCompleteCookie, kTestCompleteSuccess,
+                action_max_timeout_ms());
 }
