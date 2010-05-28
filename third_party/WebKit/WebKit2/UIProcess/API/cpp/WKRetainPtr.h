baadded088f241edc5f5f0805fd3d25572b0e25c
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-28 23:02:00 +0000

    Add a using declaration for WKRetainPtr matching what we do for our other smart pointers and fix the destructor.
    Reviewed by Anders Carlsson.
    
    * UIProcess/API/cpp/WKRetainPtr.h:
    (WebKit::WKRetainPtr::~WKRetainPtr):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60379 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/cpp/WKRetainPtr.h
+++ third_party/WebKit/WebKit2/UIProcess/API/cpp/WKRetainPtr.h
@@ -88,7 +88,7 @@ public:
             WKRetain(ptr);
     }
 
-    ~RetainPtr()
+    ~WKRetainPtr()
     {
         if (PtrType ptr = m_ptr)
             WKRelease(ptr);
@@ -211,5 +211,7 @@ template <typename T, typename U> inline bool operator!=(T* a, const WKRetainPtr
 }
 
 } // namespace WebKit
+
+using WebKit::WKRetainPtr;
 
 #endif // WKRetainPtr_h
