ac035bcdc03fe96ec53089a7c298828ba51cd44f
huanr@chromium.org <huanr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-28 16:50:49 +0000

    Separate version env variable for different distributions.
    BUG=26819
    
    Review URL: http://codereview.chromium.org/2339001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48495 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/chrome_frame_distribution.cc
+++ chrome/installer/util/chrome_frame_distribution.cc
@@ -98,6 +98,10 @@ std::wstring ChromeFrameDistribution::GetVersionKey() {
   return key;
 }
 
+std::wstring ChromeFrameDistribution::GetEnvVersionKey() {
+  return L"CHROME_FRAME_VERSION";
+}
+
 bool ChromeFrameDistribution::CanSetAsDefault() {
   return false;
 }
