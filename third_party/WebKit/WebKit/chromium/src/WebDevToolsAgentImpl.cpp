ddd1b8a4044ffe14c363a2f29312362e77f5f966
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-28 14:10:03 +0000

    2010-05-28  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: check that ClientMessageLoopAdapter is not 0 before
            accessing its fileds from inspectedViewClosed method. It may be 0
            if inspector frontend has not been open.
            https://bugs.webkit.org/show_bug.cgi?id=39876
    
            * src/WebDevToolsAgentImpl.cpp:
            (WebKit::):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebDevToolsAgentImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebDevToolsAgentImpl.cpp
@@ -144,7 +144,8 @@ public:
 
     static void inspectedViewClosed(WebViewImpl* view)
     {
-        s_instance->m_frozenViews.remove(view);
+        if (s_instance)
+            s_instance->m_frozenViews.remove(view);
     }
 
     static void didNavigate()
