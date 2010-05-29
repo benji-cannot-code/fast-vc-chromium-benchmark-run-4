522083fc99c1ea1763a4e35b02d09b4317e51386
sullivan@apple.com <sullivan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-29 00:40:24 +0000

    Add a using declaration for AdoptWK to match the one just added for WKRetainPtr.
    Rubber-stamped by Dan Bernstein.
    
    * UIProcess/API/cpp/WKRetainPtr.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60382 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/cpp/WKRetainPtr.h
+++ third_party/WebKit/WebKit2/UIProcess/API/cpp/WKRetainPtr.h
@@ -213,5 +213,6 @@ template <typename T, typename U> inline bool operator!=(T* a, const WKRetainPtr
 } // namespace WebKit
 
 using WebKit::WKRetainPtr;
+using WebKit::AdoptWK;
 
 #endif // WKRetainPtr_h
