74d2bb46cc0970da7146423866a04c8b92117ed2
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-05-31 15:25:32 +0000

    2010-05-31  Dirk Schulze  <krit@webkit.org>
            Reviewed by Nikolas Zimmermann.
    
            SVG Filter: Crash if parent and child elements use the same filter
            https://bugs.webkit.org/show_bug.cgi?id=39536
    
            If a parent and a child used the same filter, the temporary saved
            context reference got overwritten by the child. The reference is
            stored in FilterData now. FilterData depends to the target object
            and avoids overwriting any reference.
    
            Test: svg/filters/parent-children-with-same-filter.svg
    
            * rendering/RenderSVGResourceFilter.cpp:
            (WebCore::RenderSVGResourceFilter::RenderSVGResourceFilter):
            (WebCore::RenderSVGResourceFilter::applyResource):
            (WebCore::RenderSVGResourceFilter::postApplyResource):
            * rendering/RenderSVGResourceFilter.h:
    2010-05-31  Dirk Schulze  <krit@webkit.org>
    
            Reviewed by Nikolas Zimmermann.
    
            SVG Filter: Crash if parent and child elements use the same filter
            https://bugs.webkit.org/show_bug.cgi?id=39536
    
            Check, that parent and child nodes can take the same filter without
            crashing webkit.
    
            * platform/mac/svg/filters/parent-children-with-same-filter-expected.checksum: Added.
            * platform/mac/svg/filters/parent-children-with-same-filter-expected.png: Added.
            * platform/mac/svg/filters/parent-children-with-same-filter-expected.txt: Added.
            * svg/filters/parent-children-with-same-filter.svg: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60439 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderSVGResourceFilter.h
+++ third_party/WebKit/WebCore/rendering/RenderSVGResourceFilter.h
@@ -49,6 +49,8 @@ struct FilterData {
 
     RefPtr<SVGFilter> filter;
     OwnPtr<SVGFilterBuilder> builder;
+    OwnPtr<ImageBuffer> sourceGraphicBuffer;
+    GraphicsContext* savedContext;
     FloatRect boundaries;
     FloatSize scale;
     bool builded;
@@ -81,10 +83,6 @@ public:
 
 private:
     bool fitsInMaximumImageSize(const FloatSize&, FloatSize&);
-
-    // Intermediate storage during 
-    GraphicsContext* m_savedContext;
-    OwnPtr<ImageBuffer> m_sourceGraphicBuffer;
 
     HashMap<RenderObject*, FilterData*> m_filter;
 };
