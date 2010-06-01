47793ddf320f4a816b8be93eb9135829e76dcb64
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-01 22:05:45 +0000

    Implement most of the ridealong fixes/cleanups I suggested during review for enabling warn-on-signed-versus-unsigned-equality-comparisions on Windows.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/2395001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48666 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/html_utils.h
+++ chrome_frame/html_utils.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -141,7 +141,9 @@ std::string GetDefaultUserAgent();
 const char* GetChromeFrameUserAgent();
 
 // Returns true if there is a frame busting header (other than the do-nothing
-// "X-Frame-Options: ALLOWALL") in the provided header block.
+// "X-Frame-Options: ALLOWALL") in the provided header block.  Note that there
+// may be multiple X-Frame-Options values specified; if there is one anywhere in
+// the list with a value other than ALLOWALL, this returns true.
 bool HasFrameBustingHeader(const std::string& http_headers);
 
 }  // namespace http_utils
