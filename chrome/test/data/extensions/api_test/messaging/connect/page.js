22d263bc5c1793bb14b767db34716b81f88b95e2
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-01 22:33:23 +0000

    Prevent extensions from clobbering JSON implementation that extension calls use
    BUG=38857
    TEST=none
    
    Review URL: http://codereview.chromium.org/2387002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48667 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/messaging/connect/page.js
+++ chrome/test/data/extensions/api_test/messaging/connect/page.js
@@ -1,4 +1,20 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+JSON.parse = function() {
+  return "JSON.parse clobbered by content script.";
+}
+
+JSON.stringify = function() {
+  return "JSON.stringify clobbered by content script.";
+}
+
+Array.prototype.toJSON = function() {
+  return "Array.prototype.toJSON clobbered by content script.";
+}
+
+Object.prototype.toJSON = function() {
+  return "Object.prototype.toJSON clobbered by content script.";
+}
+
 // For complex connect tests.
 chrome.extension.onConnect.addListener(function(port) {
   console.log('connected');
