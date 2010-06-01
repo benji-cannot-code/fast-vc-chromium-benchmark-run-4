7cd2530e3898e7980d6de24a89322cf10663bd33
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-01 21:10:01 +0000

    Converted MediatorThread to use a delegate instead of sigslots.
    Simplified handling of MediatorThread events and fixed locking.
    
    BUG=none
    TEST=unittests + manual testing of notifications
    
    Review URL: http://codereview.chromium.org/2260009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48659 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/notifier/listener/mediator_thread_impl.h
+++ chrome/common/net/notifier/listener/mediator_thread_impl.h
@@ -111,6 +111,8 @@ class MediatorThreadImpl
           network_change_notifier_thread);
   virtual ~MediatorThreadImpl();
 
+  virtual void SetDelegate(Delegate* delegate);
+
   // Start the thread.
   virtual void Start();
   virtual void Stop();
@@ -151,6 +153,7 @@ class MediatorThreadImpl
 
   buzz::XmppClient* xmpp_client();
 
+  Delegate* delegate_;
   chrome_common_net::NetworkChangeNotifierThread*
       network_change_notifier_thread_;
   scoped_ptr<net::NetworkChangeNotifier> network_change_notifier_;
