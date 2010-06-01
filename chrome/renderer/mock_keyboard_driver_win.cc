5279db7d3a4e23d9693293c0fb87da615ed0cd6a
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-01 21:56:17 +0000

    Use arraysize() instead of ARRAYSIZE_UNSAFE() where possible.  Remove pointless comment about use of ARRAYSIZE_UNSAFE().
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/2364003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48663 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/mock_keyboard_driver_win.cc
+++ chrome/renderer/mock_keyboard_driver_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -156,8 +156,7 @@ int MockKeyboardDriverWin::GetCharacters(int key_code,
   CHECK(output);
   wchar_t code[16];
   int length = ToUnicodeEx(key_code, MapVirtualKey(key_code, 0),
-                           &keyboard_states_[0], &code[0],
-                           ARRAYSIZE_UNSAFE(code), 0,
+                           &keyboard_states_[0], &code[0], arraysize(code), 0,
                            active_keyboard_layout_);
   if (length > 0)
     output->assign(code);
