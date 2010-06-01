d1681dadad6f591808f0638af912e1281fd52c38
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-01 14:58:06 +0000

    2010-06-01  Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] Double clicks cause three button press events
            https://bugs.webkit.org/show_bug.cgi?id=38853
    
            Add rudimentary leapForward support to the GTK+ DRT. leapForward allows a test
            to pause for a specified amount of time. It is used in the processing of drag-
            -and-drop data as well as to separate distinct mouse double-clicks in some tests.
            This patch enables tests that rely on the latter behavior to pass.
    
            * DumpRenderTree/gtk/DumpRenderTree.cpp:
            (webViewWindowObjectCleared): Only initialize the EventSender when loading the top frame.
            * DumpRenderTree/gtk/EventSender.cpp:
            (leapForwardCallback): Add support for adjusting the time offset on leapForward().
            (contextClickCallback): Use sendOrQueueEvent.
            (updateClickCount): Take the time offset into account when counting clicks.
            (mouseDownCallback): Use sendOrQueueEvent.
            (getStateFlags): Change down/currentEventButton into buttonCurrentlyDown/lastClickButton.
            (mouseUpCallback): Use sendOrQueueEvent.
            (mouseMoveToCallback): Ditto.
            (mouseWheelToCallback): Ditto.
            (sendOrQueueEvent): Added.
            (dispatchEvent): Added.
            (replaySavedEvents): Pause when an event has a delay and defer to dispatchEvent.
            (makeEventSender): Only initialize the EventSender when loading the top frame.
            * DumpRenderTree/gtk/EventSender.h: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60478 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/gtk/EventSender.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/gtk/EventSender.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef const struct OpaqueJSContext* JSContextRef;
 typedef struct OpaqueJSValue* JSObjectRef;
 
-JSObjectRef makeEventSender(JSContextRef context);
+JSObjectRef makeEventSender(JSContextRef context, bool isTopFrame);
 void replaySavedEvents();
 
 #endif
