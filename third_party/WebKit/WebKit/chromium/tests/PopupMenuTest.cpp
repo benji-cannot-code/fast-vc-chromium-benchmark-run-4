de0c07669c60459e0f6c11624be892bf7bbd9985
jhawkins@chromium.org <jhawkins@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-01 23:58:47 +0000

    2010-06-01  James Hawkins  <jhawkins@chromium.org>
            UNREVIEWED: build fix.
    
            Implement the added virtual methods from PopupMenuClient to
            TestPopupMenuClient.
    
            * tests/PopupMenuTest.cpp:
            (WebKit::TestPopupMenuClient::selectionChanged):
            (WebKit::TestPopupMenuClient::selectionCleared):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60515 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/tests/PopupMenuTest.cpp
+++ third_party/WebKit/WebKit/chromium/tests/PopupMenuTest.cpp
@@ -60,6 +60,8 @@ public:
     {
         m_selectIndex = listIndex;
     }
+    virtual void selectionChanged(unsigned, bool) {}
+    virtual void selectionCleared() {}
 
     virtual String itemText(unsigned listIndex) const
     {
