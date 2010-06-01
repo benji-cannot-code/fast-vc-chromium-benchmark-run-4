3ff102e40c878b98e43cd27193beaba58e2dcb61
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-01 13:40:02 +0000

    Move net::HTTPSProber from net_base to net
    net::HTTPSProber belongs into the net library, where also it's only user
    net::URLRequest is living. This fixes undefined references in net_base caused
    by net::HTTPSProber when not linking against net.
    
    BUG=45493
    TEST=Building hresolv with the plain ld linker in shared library configuration should not generate undefined references.
    
    Review URL: http://codereview.chromium.org/2441001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48632 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/https_prober.cc
+++ net/url_request/https_prober.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "net/base/https_prober.h"
+#include "net/url_request/https_prober.h"
 
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_context.h"
