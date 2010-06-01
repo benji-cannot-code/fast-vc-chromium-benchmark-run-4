7554f019fa0e6ace81f1df004d017329a10fef17
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-01 17:24:13 +0000

    2010-06-01  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Darin Fisher.
    
            IndexedDB cleanup
            https://bugs.webkit.org/show_bug.cgi?id=40007
    
            Remove the exception code parameter since this function can no longer
            cause an exception.
    
            * storage/IDBObjectStoreRequest.idl:
            * storage/IndexedDatabase.h:
            * storage/IndexedDatabaseImpl.cpp:
            (WebCore::IndexedDatabaseImpl::open):
            * storage/IndexedDatabaseImpl.h:
            * storage/IndexedDatabaseRequest.cpp:
            (WebCore::IndexedDatabaseRequest::open):
            * storage/IndexedDatabaseRequest.h:
            * storage/IndexedDatabaseRequest.idl:
    2010-06-01  Jeremy Orlow  <jorlow@chromium.org>
    
            Reviewed by Darin Fisher.
    
            IndexedDB cleanup
            https://bugs.webkit.org/show_bug.cgi?id=40007
    
            Remove the exception code paramter (that hasn't been used for a while).
            Add asserts to default implementations.
    
            * public/WebIDBDatabase.h:
            (WebKit::WebIDBDatabase::name):
            (WebKit::WebIDBDatabase::description):
            (WebKit::WebIDBDatabase::version):
            (WebKit::WebIDBDatabase::objectStores):
            * public/WebIndexedDatabase.h:
            (WebKit::WebIndexedDatabase::open):
            * src/WebIndexedDatabaseImpl.cpp:
            (WebKit::WebIndexedDatabaseImpl::open):
            * src/WebIndexedDatabaseImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60484 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/IndexedDatabaseRequest.h
+++ third_party/WebKit/WebCore/storage/IndexedDatabaseRequest.h
@@ -52,7 +52,7 @@ public:
     }
     ~IndexedDatabaseRequest();
 
-    PassRefPtr<IDBRequest> open(const String& name, const String& description, ExceptionCode&);
+    PassRefPtr<IDBRequest> open(const String& name, const String& description);
 
     void disconnectFrame() { m_frame = 0; }
 
