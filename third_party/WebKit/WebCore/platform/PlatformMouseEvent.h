44bcc0870a5bb65ec9d420a76e23b07c877cd966
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-01 17:18:57 +0000

    2010-06-01  Sergio Villar Senin  <svillar@igalia.com>
            Unskipping tests. No review necessary.
    
            [GTK] unskip some editing tests that are actually working
            https://bugs.webkit.org/show_bug.cgi?id=39999
    
            * platform/gtk/Skipped:
    2010-06-01  Martin Robinson  <mrobinson@igalia.com>
    
            Reviewed by Xan Lopez.
    
            [GTK] Double clicks cause three button press events
            https://bugs.webkit.org/show_bug.cgi?id=38853
    
            Add GOwnPtr wrapper for GdkEvent and expose a setter for
            PlatformMouseEvent::m_clickCount.
    
            * GNUmakefile.am: Add GOwnPtrGtk.{cpp,h} to the sources list.
            * platform/PlatformMouseEvent.h:
            (WebCore::PlatformMouseEvent::setClickCount): Added.
            * platform/gtk/GOwnPtrGtk.cpp: Added.
            (WTF::GdkEvent):
            * platform/gtk/GOwnPtrGtk.h: Added.
    2010-06-01  Martin Robinson  <mrobinson@igalia.com>
    
            Reviewed by Xan Lopez.
    
            [GTK] Double clicks cause three button press events
            https://bugs.webkit.org/show_bug.cgi?id=38853
    
            Handle click counting much like the Windows port. GTK+ only
            counts triple-clicks natively, so use GTK+ double-click settings
            to count clicks with even larger click counts. Also filter the
            extra GDK_BUTTON_PRESS before GDK_{2,3}BUTTON_PRESS events using
            gdk_event_peek().
    
            * webkit/webkitwebview.cpp:
            (webkit_web_view_button_press_event): Count clicks manually. Filter extra events.
    2010-06-01  Martin Robinson  <mrobinson@igalia.com>
    
            Reviewed by Xan Lopez.
    
            [GTK] Double clicks cause three button press events
            https://bugs.webkit.org/show_bug.cgi?id=38853
    
            WebKit now filters the extra GDK_BUTTON_PRESS before GDK_2BUTTON_PRESS
            and GDK_3BUTTON_PRESS via gdk_event_peek(). Since EventSender bypasses
            the GDK event queue entirely, we simply do not send the extra event.
    
            * DumpRenderTree/gtk/EventSender.cpp:
            (mouseDownCallback): Do not send extra event. Send GDK_3BUTTON_PRESS appropriately.
            (dispatchEvent): Properly handle GDK_3BUTTON_PRESS.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60482 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/PlatformMouseEvent.h
+++ third_party/WebKit/WebCore/platform/PlatformMouseEvent.h
@@ -123,6 +123,7 @@ namespace WebCore {
 #if PLATFORM(GTK) 
         PlatformMouseEvent(GdkEventButton*);
         PlatformMouseEvent(GdkEventMotion*);
+        void setClickCount(int count) { m_clickCount = count; }
 #endif
 
 #if PLATFORM(EFL)
