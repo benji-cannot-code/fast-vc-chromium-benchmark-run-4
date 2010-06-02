6f357f1c21acc424a98f4064779649594a19aa8f
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-02 21:27:50 +0000

    Initial cut at scaffolding for mac sidetabs. No visible changes to browser without --enable-vertical-tabs, and even then, don't expect it to work. BUG=44773 TEST=infobars, bookmark bar, tab strip, etc should all behave correctly when showing, hiding, and widnow resizing. This cl should have no visible impact without the above flag on the command line. Review URL: http://codereview.chromium.org/2475002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48765 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/cocoa/side_tabs_toolbar_controller.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_COCOA_SIDE_TABS_TOOLBAR_CONTROLLER_H_
+#define CHROME_BROWSER_COCOA_SIDE_TABS_TOOLBAR_CONTROLLER_H_
+
+#import <Cocoa/Cocoa.h>
+
+#import "chrome/browser/cocoa/toolbar_controller.h"
+
+// A toolbar controller for when there are side tabs.
+// TODO(pinkerton): needs to be fleshed out much more and refactored. It
+// probably should not be a subclass, but the ToolbarController factored into
+// a base class. This, however, works for now.
+
+@interface SideTabsToolbarController : ToolbarController {
+ @private
+  IBOutlet NSButton* starButton_;
+  IBOutlet NSTextField* title_;
+  IBOutlet NSProgressIndicator* loadingSpinner_;
+}
+
+@end
+
+#endif  // CHROME_BROWSER_COCOA_SIDE_TABS_TOOLBAR_CONTROLLER_H_
