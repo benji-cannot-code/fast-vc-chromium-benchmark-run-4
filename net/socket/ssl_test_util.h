0c454912c3321b6b3004774b63bb795ab99e3541
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-02 21:11:15 +0000

    Restricting lifetime of python sync server on Windows via a JobObject.
    If a sync integration test runs for 30 seconds or more, OutOfProcTestRunner
    forcibly kills the test case executable. This leaves an orphaned python
    server instance in memory. When a subsequent test case is run, it
    attempts to kick off a new python server and immediately checks for the
    existence of a running server, following which it pumps messages to the
    server instance. If the server detected by the test case happens to be
    the old orphaned instance (that goes on to die when a new server
    instance is started), test cases can fail.
    
    This change list restricts the lifetime of the python.exe process started by
    a test case such that if the test case is killed by OutOfProcTestRunner,
    the python server instance it created dies with it.
    
    In order to restrict the lifetime of a test server, it needs to be
    started as a job. When a test server is spawned as a child process by a
    test case that is running under a debugger, the child process needs to
    be created using the CREATE_BREAKAWAY_FROM_JOB flag to first
    disassociate it from the JobObject created by the debugger.
    
    TestServerLauncher::Start() used to invoke base::LaunchApp() in order to
    create the child process. This changelist implements a new method in
    class TestServerLauncher called LaunchTestServerAsJob(). The call to
    base::LaunchApp() in TestServerLauncher::Start() is replaced with a call
    to TestServerLauncher::LaunchTestServerAsJob().
    
    BUG=43777
    TEST=sync_integration_tests
    
    Review URL: http://codereview.chromium.org/2344001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48762 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_test_util.h
+++ net/socket/ssl_test_util.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,6 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_path.h"
 #include "base/process_util.h"
+
+#if defined(OS_WIN)
+#include "base/scoped_handle_win.h"
+#endif
+
 #if defined(USE_NSS)
 #include "base/ref_counted.h"
 #include "net/base/x509_certificate.h"
@@ -112,6 +117,11 @@ class TestServerLauncher {
 
   base::ProcessHandle process_handle_;
 
+#if defined(OS_WIN)
+  // JobObject used to clean up orphaned child processes.
+  ScopedHandle job_handle_;
+#endif
+
   // True if the server should handle each request in a separate process.
   bool forking_;
 
@@ -126,6 +136,15 @@ class TestServerLauncher {
   DISALLOW_COPY_AND_ASSIGN(TestServerLauncher);
 };
 
-}
+#if defined(OS_WIN)
+// Launch test server as a job so that it is not orphaned if the test case is
+// abnormally terminated.
+bool LaunchTestServerAsJob(const std::wstring& cmdline,
+                           bool start_hidden,
+                           base::ProcessHandle* process_handle,
+                           ScopedHandle* job_handle);
+#endif
+
+}  // namespace net
 
 #endif  // NET_SOCKET_SSL_TEST_UTIL_H_
