0f6d008a7774bcf20835d034edc107216501fc99
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-02 19:42:42 +0000

    Truncate very long javascript alert messages. They can overflow the UI and on Linux cause crashes.
    BUG=45002
    TEST=none
    
    Review URL: http://codereview.chromium.org/2486003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48751 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/js_modal_dialog.cc
+++ chrome/browser/js_modal_dialog.cc
@@ -5,11 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/js_modal_dialog.h"
 
+#include "base/string_util.h"
 #include "chrome/browser/extensions/extension_host.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/notification_type.h"
 #include "ipc/ipc_message.h"
+
+namespace {
+
+// The maximum sizes of various texts passed to us from javascript.
+const int kMessageTextMaxSize = 3000;
+const int kDefaultPromptTextSize = 2000;
+
+}  // namespace
 
 JavaScriptAppModalDialog::JavaScriptAppModalDialog(
     JavaScriptMessageBoxClient* client,
@@ -24,11 +33,15 @@ JavaScriptAppModalDialog::JavaScriptAppModalDialog(
       client_(client),
       extension_host_(client->AsExtensionHost()),
       dialog_flags_(dialog_flags),
-      message_text_(message_text),
-      default_prompt_text_(default_prompt_text),
       display_suppress_checkbox_(display_suppress_checkbox),
       is_before_unload_dialog_(is_before_unload_dialog),
       reply_msg_(reply_msg) {
+  // We trim the various parts of the message dialog because otherwise we can
+  // overflow the message dialog (and crash/hang the GTK+ version).
+  ElideString(message_text, kMessageTextMaxSize, &message_text_);
+  ElideString(default_prompt_text, kDefaultPromptTextSize,
+              &default_prompt_text_);
+
   DCHECK((tab_contents_ != NULL) != (extension_host_ != NULL));
   InitNotifications();
 }
