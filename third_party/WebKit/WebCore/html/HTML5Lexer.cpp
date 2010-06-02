91ff3c0d70c6baee5f53bffe6513811c72216212
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-02 10:07:51 +0000

    2010-06-02  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            HTML5 parser does not track line numbers
            https://bugs.webkit.org/show_bug.cgi?id=39984
    
            Update our expectations to show the proper line numbers.
            These new line numbers match what the old parser produces.
    
            * html5lib/runner-expected-html5.txt:
            * html5lib/webkit-resumer-expected-html5.txt:
    2010-06-02  Eric Seidel  <eric@webkit.org>
    
            Reviewed by Adam Barth.
    
            HTML5 parser does not track line numbers
            https://bugs.webkit.org/show_bug.cgi?id=39984
    
            This adds rudimentary line-number tracking to the HTML5Lexer.
            We'll still need to handle \r\n cases eventually, but this
            fixes the vast majority of our line-number related failures.
    
            Fixes 268 layout tests. :)
    
            * html/HTML5Lexer.cpp:
            (WebCore::HTML5Lexer::reset):
             - Reset m_lineNumber to 0.
            (WebCore::HTML5Lexer::consumeEntity):
             - Pass m_lineNumber to advance().
            (WebCore::HTML5Lexer::nextToken):
             - Pass m_lineNumber to advance().
            * html/HTML5Lexer.h:
            (WebCore::HTML5Lexer::lineNumber):
             - Expose m_lineNumber for HTML5Tokenizer.
            (WebCore::HTML5Lexer::columnNumber):
             - Mirrors the implementation in the old HTMLTokenizer.
               we might some day support column number tracking in
               the Lexer.  It certainly would be possible.
            * html/HTML5ScriptRunner.cpp:
            (WebCore::HTML5ScriptRunner::HTML5ScriptRunner):
             - ASSERT we're passed a host.
            (WebCore::HTML5ScriptRunner::sourceFromPendingScript):
             - Use PendingScript.startingLineNumber when PendingScript is an inline script.
            (WebCore::HTML5ScriptRunner::execute):
             - Now expects a start line number passed from the TreeBuilder.
            (WebCore::HTML5ScriptRunner::runScript):
             - Now expects a start line number.
            * html/HTML5ScriptRunner.h:
            (WebCore::HTML5ScriptRunner::PendingScript::PendingScript):
             - Track startingLineNumber for any inline script.
            * html/HTML5Tokenizer.cpp:
            (WebCore::HTML5Tokenizer::begin):
             - Add a FIXME.  The old HTMLTokenizer resets its "lexer" state here.
            (WebCore::HTML5Tokenizer::pumpLexer):
             - Pass scriptStartLine to the HTML5ScriptRunner
            (WebCore::HTML5Tokenizer::lineNumber):
             - Implementation for Tokenizer.h
            (WebCore::HTML5Tokenizer::columnNumber):
             - Implementation for Tokenizer.h
            * html/HTML5Tokenizer.h:
            * html/HTML5TreeBuilder.cpp:
            (WebCore::HTML5TreeBuilder::HTML5TreeBuilder):
             - Initialize the new line number tracking variables.
            (WebCore::HTML5TreeBuilder::handleScriptEndTag):
             - Save off the line number from the start tag to pass to the ScriptRunner.
            (WebCore::HTML5TreeBuilder::takeScriptToProcess):
             - Return the line number from the start tag.
            (WebCore::HTML5TreeBuilder::passTokenToLegacyParser):
             - Save off the line number of ever script start tag we see.
            * html/HTML5TreeBuilder.h:
             - Keep both the line number of the last start tag, and the
               line number of the script we know we're expecting our caller to
               run before resuming parsing.  These are separate to keep the code
               clean, especially since the last script start tag line number is
               a hack specific to using the old HTMLParser.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60553 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTML5Lexer.cpp
+++ third_party/WebKit/WebCore/html/HTML5Lexer.cpp
@@ -155,6 +155,7 @@ void HTML5Lexer::reset()
 {
     m_state = DataState;
     m_token = 0;
+    m_lineNumber = 0;
     m_skipLeadingNewLineForListing = false;
     m_emitPending = false;
     m_additionalAllowedCharacter = '\0';
@@ -239,7 +240,7 @@ unsigned HTML5Lexer::consumeEntity(SegmentedString& source, bool& notEnoughChara
             else if (cc >= 'A' && cc <= 'F')
                 result = result * 16 + 10 + cc - 'A';
             else if (cc == ';') {
-                source.advance();
+                source.advancePastNonNewline();
                 return legalEntityFor(result);
             } else 
                 return legalEntityFor(result);
@@ -249,7 +250,7 @@ unsigned HTML5Lexer::consumeEntity(SegmentedString& source, bool& notEnoughChara
             if (cc >= '0' && cc <= '9')
                 result = result * 10 + cc - '0';
             else if (cc == ';') {
-                source.advance();
+                source.advancePastNonNewline();
                 return legalEntityFor(result);
             } else
                 return legalEntityFor(result);
@@ -1075,7 +1076,7 @@ bool HTML5Lexer::nextToken(SegmentedString& source, HTML5Token& token)
                 if (cc == '>')
                     break;
                 m_token->appendToComment(cc);
-                source.advance();
+                source.advance(m_lineNumber);
             }
             emitCurrentToken();
             m_state = DataState;
@@ -1510,7 +1511,7 @@ bool HTML5Lexer::nextToken(SegmentedString& source, HTML5Token& token)
             break;
         }
         }
-        source.advance();
+        source.advance(m_lineNumber);
         if (m_emitPending) {
             m_emitPending = false;
             return true;
--- third_party/WebKit/WebCore/html/HTML5Tokenizer.cpp
+++ third_party/WebKit/WebCore/html/HTML5Tokenizer.cpp
@@ -55,6 +55,7 @@ HTML5Tokenizer::~HTML5Tokenizer()
 
 void HTML5Tokenizer::begin()
 {
+    // FIXME: Should we reset the lexer?
 }
 
 void HTML5Tokenizer::pumpLexer()
@@ -69,7 +70,9 @@ void HTML5Tokenizer::pumpLexer()
 
         // The parser will pause itself when waiting on a script to load or run.
         // ScriptRunner executes scripts at the right times and handles reentrancy.
-        bool shouldContinueParsing = m_scriptRunner->execute(m_treeBuilder->takeScriptToProcess());
+        int scriptStartLine = 0;
+        RefPtr<Element> scriptElement = m_treeBuilder->takeScriptToProcess(scriptStartLine);
+        bool shouldContinueParsing = m_scriptRunner->execute(scriptElement.release(), scriptStartLine);
         m_treeBuilder->setPaused(!shouldContinueParsing);
         if (!shouldContinueParsing)
             return;
@@ -109,6 +112,16 @@ void HTML5Tokenizer::finish()
 int HTML5Tokenizer::executingScript() const
 {
     return m_scriptRunner->inScriptExecution();
+}
+
+int HTML5Tokenizer::lineNumber() const
+{
+    return m_lexer->lineNumber();
+}
+
+int HTML5Tokenizer::columnNumber() const
+{
+    return m_lexer->columnNumber();
 }
 
 bool HTML5Tokenizer::isWaitingForScripts() const
