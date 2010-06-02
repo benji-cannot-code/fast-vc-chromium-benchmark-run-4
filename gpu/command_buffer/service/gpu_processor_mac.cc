b90ce9cd777858b1c5bbe026abd4d91229c71dc0
kbr@google.com <kbr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-02 23:25:45 +0000

    Expose UpdateOffscreenFrameBufferSize from GLES2Decoder to allow Mac port to call it during resizing. Because the Mac port does all of its rendering offscreen, the back buffer must be resized immediately rather than waiting until the next SwapBuffers call.
    Tested with npapi_pepper_test_plugin and GPU unit tests.
    
    BUG=45246
    TEST=none
    
    Review URL: http://codereview.chromium.org/2466003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48789 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gpu_processor_mac.cc
+++ gpu/command_buffer/service/gpu_processor_mac.cc
@@ -72,6 +72,7 @@ void GPUProcessor::Destroy() {
 uint64 GPUProcessor::SetWindowSizeForIOSurface(const gfx::Size& size) {
 #if !defined(UNIT_TEST)
   ResizeOffscreenFrameBuffer(size);
+  decoder_->UpdateOffscreenFrameBufferSize();
   return surface_->SetSurfaceSize(size);
 #else
   return 0;
@@ -82,6 +83,7 @@ TransportDIB::Handle GPUProcessor::SetWindowSizeForTransportDIB(
     const gfx::Size& size) {
 #if !defined(UNIT_TEST)
   ResizeOffscreenFrameBuffer(size);
+  decoder_->UpdateOffscreenFrameBufferSize();
   return surface_->SetTransportDIBSize(size);
 #else
   return TransportDIB::DefaultHandleValue();
