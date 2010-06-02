90fce84f0877e7427d435112923cf9c9a6d49e64
nkostylev@google.com <nkostylev@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-02 15:00:00 +0000

    Add network timeout when loading account creation page.
    BUG= http://crosbug.com/3289
    TEST=Connect to the network that requires web base login and open "Create account". It should timeout after 10 secs or earlier.
    
    Review URL: http://codereview.chromium.org/2465001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48721 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/account_screen.h
+++ chrome/browser/chromeos/login/account_screen.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/scoped_ptr.h"
 #include "chrome/browser/chromeos/login/account_creation_view.h"
+#include "chrome/browser/chromeos/login/screen_observer.h"
 #include "chrome/browser/chromeos/login/view_screen.h"
 #include "chrome/browser/chromeos/login/web_page_screen.h"
 #include "chrome/browser/chromeos/login/web_page_view.h"
@@ -52,6 +53,9 @@ class AccountScreen : public ViewScreen<AccountCreationView>,
   virtual void NavigationStateChanged(const TabContents* source,
                                       unsigned changed_flags);
   virtual void LoadingStateChanged(TabContents* source);
+
+  // WebPageScreen implementation:
+  virtual void CloseScreen(ScreenObserver::ExitCodes code);
 
   // Url of account creation page. Overriden by tests.
   static scoped_ptr<GURL> new_account_page_url_;
--- chrome/browser/chromeos/login/web_page_screen.h
+++ chrome/browser/chromeos/login/web_page_screen.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_LOGIN_WEB_PAGE_SCREEN_H_
 
 #include "base/scoped_ptr.h"
+#include "base/timer.h"
+#include "chrome/browser/chromeos/login/screen_observer.h"
 #include "chrome/browser/tab_contents/tab_contents_delegate.h"
 
 class GURL;
@@ -16,8 +18,11 @@ namespace chromeos {
 // Base class for wizard screen that holds web page.
 class WebPageScreen : public TabContentsDelegate {
  public:
-  explicit WebPageScreen();
-  virtual ~WebPageScreen();
+  explicit WebPageScreen() {}
+  virtual ~WebPageScreen() {}
+
+  // Exits from the screen with the specified exit code.
+  virtual void CloseScreen(ScreenObserver::ExitCodes code) = 0;
 
  protected:
   // TabContentsDelegate implementation:
@@ -43,7 +48,17 @@ class WebPageScreen : public TabContentsDelegate {
   virtual void ToolbarSizeChanged(TabContents* source, bool is_animating) {}
   virtual bool HandleContextMenu(const ContextMenuParams& params);
 
+  // Called by |timeout_timer_|. Stops page fetching and closes screen.
+  void OnNetworkTimeout();
+
+  // Start/stop timeout timer.
+  void StartTimeoutTimer();
+  void StopTimeoutTimer();
+
  private:
+  // Timer used for network response timeout.
+  base::OneShotTimer<WebPageScreen> timeout_timer_;
+
   DISALLOW_COPY_AND_ASSIGN(WebPageScreen);
 };
 
