6bb25c23c52504be133141c8c42432c9c432850f
jshin@chromium.org <jshin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-02 00:24:35 +0000

    Disable FindInPageControllerTest.ActivateLinkNavigatesPage and ExtensionApiTest.Bookmarks on Windows
    BUG=45594,45595
    TEST=Windows bot don't turn red with browser_test failure
    TBR=jrg
    Review URL: http://codereview.chromium.org/2433003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48683 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_bookmarks_apitest.cc
+++ chrome/browser/extensions/extension_bookmarks_apitest.cc
@@ -5,6 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Bookmarks) {
+#if defined(OS_WIN)
+// On Windows, this test occasionally crash. See http://crbug.com/45595
+#define MAYBE_Bookmarks DISABLED_Bookmarks
+#else
+#define MAYBE_Bookmarks Bookmarks
+#endif
+
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_Bookmarks) {
   ASSERT_TRUE(RunExtensionTest("bookmarks")) << message_;
 }
