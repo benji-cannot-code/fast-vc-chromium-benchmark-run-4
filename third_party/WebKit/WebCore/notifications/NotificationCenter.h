0f9327ac3780b5c1fe344270d51a2cf13e7c0b90
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-02 10:10:13 +0000

    2010-06-02  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r60547.
            http://trac.webkit.org/changeset/60547
            https://bugs.webkit.org/show_bug.cgi?id=40051
    
            It made fast/overflow/overflow-with-local-background-
            attachment.html crash (Requested by Ossy on #webkit).
    
            * notifications/Notification.h:
            (WebCore::Notification::create):
            * notifications/NotificationCenter.h:
            (WebCore::NotificationCenter::createHTMLNotification):
            (WebCore::NotificationCenter::createNotification):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60554 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/notifications/NotificationCenter.h
+++ third_party/WebKit/WebCore/notifications/NotificationCenter.h
@@ -50,7 +50,7 @@ namespace WebCore {
     public:
         static PassRefPtr<NotificationCenter> create(ScriptExecutionContext* context, NotificationPresenter* presenter) { return adoptRef(new NotificationCenter(context, presenter)); }
 
-        PassRefPtr<Notification> createHTMLNotification(const String& URI, ExceptionCode& ec)
+        Notification* createHTMLNotification(const String& URI, ExceptionCode& ec)
         {
             if (!presenter()) {
                 ec = INVALID_STATE_ERR;
@@ -63,7 +63,7 @@ namespace WebCore {
             return Notification::create(m_scriptExecutionContext->completeURL(URI), context(), ec, presenter());
         }
 
-        PassRefPtr<Notification> createNotification(const String& iconURI, const String& title, const String& body, ExceptionCode& ec)
+        Notification* createNotification(const String& iconURI, const String& title, const String& body, ExceptionCode& ec)
         {
             if (!presenter()) {
                 ec = INVALID_STATE_ERR;
