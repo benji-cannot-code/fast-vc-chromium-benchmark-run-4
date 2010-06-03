8ef130572a3be1d40e0065cf15c963cf12607d70
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-03 07:03:05 +0000

    2010-06-02  Tasuku Suzuki  <tasuku.suzuki@nokia.com>
            Reviewed by Shinichiro Hamaji.
    
            [Qt] Fix compilation with QT_NO_PROPERTIES
            https://bugs.webkit.org/show_bug.cgi?id=38324
    
            * Api/qwebpage.cpp:
            (QWebPage::event):
            * Api/qwebpage_p.h:
            * WebCoreSupport/InspectorClientQt.cpp:
            (WebCore::InspectorClientQt::openInspectorFrontend):
    2010-06-02  Tasuku Suzuki  <tasuku.suzuki@nokia.com>
    
            Reviewed by Shinichiro Hamaji.
    
            [Qt] Fix compilation with QT_NO_PROPERTIES
            https://bugs.webkit.org/show_bug.cgi?id=38324
    
            * QtTestBrowser/main.cpp:
            (LauncherWindow::init):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -1204,6 +1204,7 @@ void QWebPagePrivate::inputMethodEvent(QInputMethodEvent *ev)
     ev->accept();
 }
 
+#ifndef QT_NO_PROPERTIES
 void QWebPagePrivate::dynamicPropertyChangeEvent(QDynamicPropertyChangeEvent* event)
 {
     if (event->propertyName() == "_q_viewMode") {
@@ -1258,6 +1259,7 @@ void QWebPagePrivate::dynamicPropertyChangeEvent(QDynamicPropertyChangeEvent* ev
     }
 #endif
 }
+#endif
 
 void QWebPagePrivate::shortcutOverrideEvent(QKeyEvent* event)
 {
@@ -2642,9 +2644,11 @@ bool QWebPage::event(QEvent *ev)
         d->touchEvent(static_cast<QTouchEvent*>(ev));
         break;
 #endif
+#ifndef QT_NO_PROPERTIES
     case QEvent::DynamicPropertyChange:
         d->dynamicPropertyChangeEvent(static_cast<QDynamicPropertyChangeEvent*>(ev));
         break;
+#endif
     default:
         return QObject::event(ev);
     }
