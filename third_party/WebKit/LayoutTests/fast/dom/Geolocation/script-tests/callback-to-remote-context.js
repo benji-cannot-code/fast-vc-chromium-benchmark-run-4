f275bf11d5b2216dd890894d41e252da7b50bebc
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-03 23:46:53 +0000

    2010-06-03  Steve Block  <steveblock@google.com>
            Reviewed by Alexey Proskuryakov.
    
            Geolocation needs LayoutTest to test making callbacks to remote frames
            https://bugs.webkit.org/show_bug.cgi?id=40129
    
            * fast/dom/Geolocation/callback-to-remote-context-expected.txt: Added.
            * fast/dom/Geolocation/callback-to-remote-context.html: Added.
            * fast/dom/Geolocation/resources: Added.
            * fast/dom/Geolocation/resources/callback-to-remote-context-inner.html: Added.
            * fast/dom/Geolocation/script-tests/callback-to-remote-context.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/Geolocation/script-tests/callback-to-remote-context.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Tests that when a Geolocation request is made from a remote frame, callbacks are made as usual.");
+
+function onIframeReady() {
+    // Make request from remote frame
+    iframe.contentWindow.navigator.geolocation.getCurrentPosition(function() {
+        testPassed('Success callback invoked');
+        finishJSTest();
+    }, function() {
+        testFailed('Error callback invoked unexpectedly');
+        finishJSTest();
+    });
+}
+
+var iframe = document.createElement('iframe');
+iframe.src = 'resources/callback-to-remote-context-inner.html';
+document.body.appendChild(iframe);
+
+window.jsTestIsAsync = true;
+window.successfullyParsed = true;
