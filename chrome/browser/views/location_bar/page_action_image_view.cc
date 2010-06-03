035b704ae03c7ca20630c01edc24280f2a3f0710
twiz@google.com <twiz@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-03 17:20:57 +0000

    A very basic fix that prevents experimental popups from living longer than the ExtensionFunctionDispatcher/RenderViewHost that launched the popup.  This is a problem, for example, when Chrome is closed while a popup is still displayed.
    I also removed the code that delayed sending the onClosed popup notification.  I modified the ExtensionPopup::Obeserver interface to add methods for about-to-close, and fully-closed notifications.
    
    BUG=None
    TEST=ExtensionApiTest.Popup
    
    Review URL: http://codereview.chromium.org/2092020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48841 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/location_bar/page_action_image_view.cc
+++ chrome/browser/views/location_bar/page_action_image_view.cc
@@ -210,7 +210,7 @@ void PageActionImageView::InspectPopup(ExtensionAction* action) {
                 true);  // inspect_with_devtools
 }
 
-void PageActionImageView::ExtensionPopupClosed(ExtensionPopup* popup) {
+void PageActionImageView::ExtensionPopupIsClosing(ExtensionPopup* popup) {
   DCHECK_EQ(popup_, popup);
   // ExtensionPopup is ref-counted, so we don't need to delete it.
   popup_ = NULL;
