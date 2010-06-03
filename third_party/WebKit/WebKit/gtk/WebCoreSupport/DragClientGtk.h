59a4eff54d5f4f8c0deb400bf4610e5b6cf50752
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-03 20:08:53 +0000

    2010-06-03  Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] Drag gesture can take mouse grab indefinitely
            https://bugs.webkit.org/show_bug.cgi?id=32840
    
            Start GTK+ drags with the previous mouse button down event instead of
            synthesizing the event. The synthesized event was not completely valid
            and froze some drags indefinitely.
    
            * WebCoreSupport/DragClientGtk.cpp:
            (WebKit::buttonPressEvent): Added.
            (WebKit::DragClient::DragClient):
            Connect to the 'button-press-event' signal of the WebView.
            (WebKit::DragClient::startDrag):
            Instead of synthesizing a button press event use the last real one.
            * WebCoreSupport/DragClientGtk.h:
            (WebKit::DragClient::setLastButtonPressEvent): Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60632 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/WebCoreSupport/DragClientGtk.h
+++ third_party/WebKit/WebKit/gtk/WebCoreSupport/DragClientGtk.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2007 Apple Inc.  All rights reserved.
  * Copyright (C) 2007 Holger Hans Peter Freyther
+ * Copyright (C) 2010 Igalia S.L.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,8 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DragClientGtk_h
 
 #include "DragClient.h"
+#include "GOwnPtrGtk.h"
 
 typedef struct _WebKitWebView WebKitWebView;
+typedef union _GdkEvent GdkEvent;
 
 namespace WebKit {
 
@@ -51,10 +54,12 @@ namespace WebKit {
         virtual WebCore::DragImageRef createDragImageForLink(WebCore::KURL&, const WebCore::String& label, WebCore::Frame*);
 
         virtual void dragControllerDestroyed();
+        void setLastButtonPressEvent(GdkEvent* press) { m_lastButtonPressEvent.set(press); }
 
     private:
         WebKitWebView* m_webView;
         WebCore::IntPoint m_startPos;
+        GOwnPtr<GdkEvent> m_lastButtonPressEvent;
     };
 }
 
