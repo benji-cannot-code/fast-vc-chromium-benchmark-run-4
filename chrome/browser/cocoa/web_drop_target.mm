d7c99dbe9379c36d1debd08cef4ba0f6ed37a31f
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-04 19:07:12 +0000

    Convert RTF on the pasteboard to HTML and all it to substitute if present and no HTML is on a paste or drop. Safari does not put HTML on the pasteboard, only RTF. BUG=44661 TEST=dropping/pasting html should retain basic structure, but fonts/colors don't seem to propagate. Review URL: http://codereview.chromium.org/2674002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48962 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/web_drop_target.mm
+++ chrome/browser/cocoa/web_drop_target.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -254,10 +254,13 @@ using WebKit::WebDragOperationsMask;
         base::SysNSStringToUTF16([pboard stringForType:NSStringPboardType]);
   }
 
-  // Get HTML.
+  // Get HTML. If there's no HTML, try RTF.
   if ([types containsObject:NSHTMLPboardType]) {
     data->text_html =
         base::SysNSStringToUTF16([pboard stringForType:NSHTMLPboardType]);
+  } else if ([types containsObject:NSRTFPboardType]) {
+    NSString* html = [pboard htmlFromRtf];
+    data->text_html = base::SysNSStringToUTF16(html);
   }
 
   // Get files.
