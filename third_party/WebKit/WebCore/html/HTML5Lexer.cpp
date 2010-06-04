40d0de7a48e2ad1ca06e1f76b13d955e662175cc
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-04 22:12:39 +0000

    2010-06-04  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Make HTML5Lexer go fast
            https://bugs.webkit.org/show_bug.cgi?id=40048
    
            A couple more cases like the previous patch that I missed.
    
            * html/HTML5Lexer.cpp:
            (WebCore::HTML5Lexer::nextToken):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTML5Lexer.cpp
+++ third_party/WebKit/WebCore/html/HTML5Lexer.cpp
@@ -499,9 +499,9 @@ bool HTML5Lexer::nextToken(SegmentedString& source, HTML5Token& token)
             ADVANCE_TO(BeforeAttributeNameState);
         else if (cc == '/')
             ADVANCE_TO(SelfClosingStartTagState);
-        else if (cc == '>') {
+        else if (cc == '>')
             EMIT_AND_RESUME_IN(DataState);
-        } else if (cc >= 'A' && cc <= 'Z') {
+        else if (cc >= 'A' && cc <= 'Z') {
             m_token->appendToName(toLowerCase(cc));
             ADVANCE_TO(TagNameState);
         } else {
@@ -714,10 +714,11 @@ bool HTML5Lexer::nextToken(SegmentedString& source, HTML5Token& token)
             ADVANCE_TO(ScriptDataEscapedDashState);
         } else if (cc == '<')
             ADVANCE_TO(ScriptDataEscapedLessThanSignState);
-        else
+        else {
             emitCharacter(cc);
+            ADVANCE_TO(ScriptDataEscapedState);
+        }
         // FIXME: Handle EOF properly.
-        break;
     }
     END_STATE()
 
@@ -837,9 +838,8 @@ bool HTML5Lexer::nextToken(SegmentedString& source, HTML5Token& token)
             emitCharacter(cc);
             m_temporaryBuffer.append(cc);
             ADVANCE_TO(ScriptDataDoubleEscapeStartState);
-        } else {
+        } else
             RECONSUME_IN(ScriptDataEscapedState);
-        }
     }
     END_STATE()
 
@@ -972,9 +972,9 @@ bool HTML5Lexer::nextToken(SegmentedString& source, HTML5Token& token)
             ADVANCE_TO(SelfClosingStartTagState);
         else if (cc == '=')
             ADVANCE_TO(BeforeAttributeValueState);
-        else if (cc == '=') {
+        else if (cc == '=')
             EMIT_AND_RESUME_IN(DataState);
-        } else if (cc >= 'A' && cc <= 'Z') {
+        else if (cc >= 'A' && cc <= 'Z') {
             m_token->addNewAttribute();
             m_token->appendToAttributeName(toLowerCase(cc));
             ADVANCE_TO(AttributeNameState);
@@ -1242,9 +1242,9 @@ bool HTML5Lexer::nextToken(SegmentedString& source, HTML5Token& token)
             m_token->appendToComment('-');
             m_token->appendToComment('!');
             ADVANCE_TO(CommentEndDashState);
-        } else if (cc == '>') {
+        } else if (cc == '>')
             EMIT_AND_RESUME_IN(DataState);
-        } else {
+        else {
             m_token->appendToComment('-');
             m_token->appendToComment('-');
             m_token->appendToComment('!');
@@ -1545,9 +1545,9 @@ bool HTML5Lexer::nextToken(SegmentedString& source, HTML5Token& token)
     BEGIN_STATE(AfterDOCTYPESystemIdentifierState) {
         if (cc == '\x09' || cc == '\x0A' || cc == '\x0C' || cc == ' ')
             ADVANCE_TO(AfterDOCTYPESystemIdentifierState);
-        else if (cc == '>') {
+        else if (cc == '>')
             EMIT_AND_RESUME_IN(DataState);
-        } else {
+        else {
             emitParseError();
             ADVANCE_TO(BogusDOCTYPEState);
         }
