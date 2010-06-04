ae55687709a0ab139990a432ae38494210ca980e
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-04 09:56:50 +0000

    2010-06-04  Steve Block  <steveblock@google.com>
            Reviewed by Jeremy Orlow.
    
            Geolocation needs more LayoutTests to test making callbacks to remote frames
            https://bugs.webkit.org/show_bug.cgi?id=40146
    
            This test is similar to callback-to-remote-context.html in that it test making a callback
            from a Geolocation object to a remote frame. In callback-to-remote-context.html, the call
            chain to the Geolocation method is started from the owning frame. In this test, however,
            the call chain is started from the remote frame.
    
            * fast/dom/Geolocation/callback-to-remote-context2-expected.txt: Added.
            * fast/dom/Geolocation/callback-to-remote-context2.html: Added.
            * fast/dom/Geolocation/script-tests/callback-to-remote-context2.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/Geolocation/script-tests/callback-to-remote-context2.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Tests that when a Geolocation request is made from a remote frame, and the JavaScript call chain starts from that remote frame, callbacks are made as usual.");
+
+function onIframeReady() {
+    // Make request from remote frame, with call chain starting here
+    window.setTimeout(function() {
+        iframe.contentWindow.navigator.geolocation.getCurrentPosition(function() {
+            testPassed('Success callback invoked');
+            finishJSTest();
+        }, function() {
+            testFailed('Error callback invoked unexpectedly');
+            finishJSTest();
+        });
+    }, 0);
+}
+
+var iframe = document.createElement('iframe');
+iframe.src = 'resources/callback-to-remote-context-inner.html';
+document.body.appendChild(iframe);
+
+window.jsTestIsAsync = true;
+window.successfullyParsed = true;
