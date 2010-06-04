5799fca7487f5add3c0269594dba6d025ccc6b60
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-04 20:12:06 +0000

    Fix comment typo.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2631004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48965 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/throbber_view.h
+++ chrome/browser/cocoa/throbber_view.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // - Filmstrip: Draws via a sequence of frames in an image. There is no state
 //   where the class is frozen on an image and not animating. The image needs to
-//   be made of squares such that the height divides evently into the width.
+//   be made of squares such that the height divides evenly into the width.
 //
 // - Toast: Draws an image animating down to the bottom and then another image
 //   animating up from the bottom. Stops once the animation is complete.
