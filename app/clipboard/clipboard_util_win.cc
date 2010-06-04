39bc075971b513300ac2c6ee2f577ed178ee120e
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-04 16:51:38 +0000

    <wininet.h> related cleanup.
    Document why clipboard_util_win.cc needs to include <wininet.h>.
    
    test_shell_win.cc does not need to include <wininet.h>.
    
    R=eroman
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/2606004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48937 0039d316-1c4b-4281-b951-d872f2087c98

--- app/clipboard/clipboard_util_win.cc
+++ app/clipboard/clipboard_util_win.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <shellapi.h>
 #include <shlwapi.h>
-#include <wininet.h>
+#include <wininet.h>  // For INTERNET_MAX_URL_LENGTH.
 
 #include "base/basictypes.h"
 #include "base/logging.h"
