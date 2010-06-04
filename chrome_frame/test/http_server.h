e2c12220d3bbefcd99fce06e8391b087f46dda36
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-04 21:54:53 +0000

    chrome_frame: Get rid of all occurrences of file_util::AppendToPath.
    BUG=24874
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/2637001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48976 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/http_server.h
+++ chrome_frame/test/http_server.h
@@ -1,7 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #ifndef CHROME_FRAME_TEST_HTTP_SERVER_H_
 #define CHROME_FRAME_TEST_HTTP_SERVER_H_
 
@@ -12,22 +13,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ref_counted.h"
 #include "net/url_request/url_request_unittest.h"
 
-// chrome frame specilization of http server from net.
+class FilePath;
+
+// Chrome Frame specilization of http server from net.
 class ChromeFrameHTTPServer {
  public:
   void SetUp();
   void TearDown();
   bool WaitToFinish(int milliseconds);
   GURL Resolve(const wchar_t* relative_url);
-  std::wstring GetDataDir();
+  FilePath GetDataDir();
 
-  HTTPTestServer* server() {
-    return server_;
-  }
+  HTTPTestServer* server() { return server_; }
 
  protected:
   scoped_refptr<HTTPTestServer> server_;
 };
 
 #endif  // CHROME_FRAME_TEST_HTTP_SERVER_H_
-
