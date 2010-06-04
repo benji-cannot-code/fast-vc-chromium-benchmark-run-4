2f735810ac7e8bc3d0c8ec613ee7a3a0f521bf65
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-04 14:06:46 +0000

    2010-06-04  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: Create resource for DebuggerScript.js.
            https://bugs.webkit.org/show_bug.cgi?id=40122
    
            * WebKit.grd:
            * public/WebDevToolsAgentClient.h:
            (WebKit::WebDevToolsAgentClient::debuggerScriptSource):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebDevToolsAgentClient.h
+++ third_party/WebKit/WebKit/chromium/public/WebDevToolsAgentClient.h
@@ -56,6 +56,7 @@ public:
 
     virtual WebCString injectedScriptSource() { return WebCString(); }
     virtual WebCString injectedScriptDispatcherSource() { return WebCString(); }
+    virtual WebCString debuggerScriptSource() { return WebCString(); }
 
     class WebKitClientMessageLoop {
     public:
