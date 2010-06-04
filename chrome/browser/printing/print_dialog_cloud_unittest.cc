07f6cabbdb03e6c37e82f81c20697cda36316c3e
scottbyer@chromium.org <scottbyer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-04 16:35:58 +0000

    Re-enable test, add in test detail. Add in asked-for test of getting the title through, found text-only PDF file for that test, re-enabled it.
    BUG=none
    TEST=CloudPrintDataSenderTest.CanSend
    
    Review URL: http://codereview.chromium.org/2590002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48935 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_dialog_cloud_unittest.cc
+++ chrome/browser/printing/print_dialog_cloud_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/path_service.h"
+#include "base/string_util.h"
 #include "base/values.h"
 #include "base/weak_ptr.h"
 #include "chrome/browser/chrome_thread.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using testing::A;
 using testing::AtLeast;
+using testing::Eq;
 using testing::HasSubstr;
 using testing::IsNull;
 using testing::NotNull;
@@ -35,6 +37,7 @@ using testing::_;
 static const char* const kPDFTestFile = "printing/cloud_print_unittest.pdf";
 static const char* const kEmptyPDFTestFile =
     "printing/cloud_print_emptytest.pdf";
+static const char* const kMockJobTitle = "Mock Job Title";
 
 FilePath GetTestDataFileName() {
   FilePath test_data_directory;
@@ -57,6 +60,17 @@ char* GetTestData() {
     file_util::ReadFileToString(test_file, &sTestFileData);
   }
   return &sTestFileData[0];
+}
+
+MATCHER_P(StringValueEq, expected, "StringValue") {
+  if (expected->Equals(&arg))
+    return true;
+  std::string expected_string, arg_string;
+  expected->GetAsString(&expected_string);
+  arg.GetAsString(&arg_string);
+  *result_listener << "'" << arg_string
+                   << "' (expected '" << expected_string << "')";
+  return false;
 }
 
 namespace internal_cloud_print_helpers {
@@ -173,7 +187,7 @@ class CloudPrintDataSenderTest : public testing::Test {
 
  protected:
   virtual void SetUp() {
-    string16 mock_job_title;
+    string16 mock_job_title(ASCIIToUTF16(kMockJobTitle));
     mock_helper_.reset(new MockCloudPrintDataSenderHelper);
     print_data_sender_ =
         new CloudPrintDataSender(mock_helper_.get(), mock_job_title);
@@ -189,8 +203,10 @@ class CloudPrintDataSenderTest : public testing::Test {
 
 // TODO(scottbyer): DISABLED until the binary test file can get
 // checked in separate from the patch.
-TEST_F(CloudPrintDataSenderTest, DISABLED_CanSend) {
-  EXPECT_CALL(*mock_helper_, CallJavascriptFunction(_, _, _)).
+TEST_F(CloudPrintDataSenderTest, CanSend) {
+  StringValue mock_job_title(kMockJobTitle);
+  EXPECT_CALL(*mock_helper_,
+              CallJavascriptFunction(_, _, StringValueEq(&mock_job_title))).
       WillOnce(Return());
 
   FilePath test_data_file_name = GetTestDataFileName();
@@ -248,9 +264,9 @@ class CloudPrintHtmlDialogDelegateTest : public testing::Test {
  protected:
   virtual void SetUp() {
     FilePath mock_path;
-    string16 mock_string;
+    string16 mock_title;
     MockCloudPrintFlowHandler* handler =
-        new MockCloudPrintFlowHandler(mock_path, mock_string);
+        new MockCloudPrintFlowHandler(mock_path, mock_title);
     mock_flow_handler_ = handler->AsWeakPtr();
     EXPECT_CALL(*mock_flow_handler_.get(), SetDialogDelegate(_));
     EXPECT_CALL(*mock_flow_handler_.get(), SetDialogDelegate(NULL));
