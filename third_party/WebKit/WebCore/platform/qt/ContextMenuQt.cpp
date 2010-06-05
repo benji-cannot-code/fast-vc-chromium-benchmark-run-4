5372e3aabdbea283008ea3bb8fbbdb8c0cf8ea31
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-05 12:03:47 +0000

    2010-06-05  Anders Bakken  <agbakken@gmail.com>
            Reviewed by Eric Seidel.
    
            [Qt] ContextMenuQt.cpp has coding-style errors
            https://bugs.webkit.org/show_bug.cgi?id=39779
    
            * platform/qt/ContextMenuQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/ContextMenuQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/ContextMenuQt.cpp
@@ -22,19 +22,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "config.h"
 #include "ContextMenu.h"
 
-#include <wtf/Assertions.h>
-
-#include <QAction>
-
 #include <Document.h>
 #include <Frame.h>
 #include <FrameView.h>
+#include <QAction>
+#include <wtf/Assertions.h>
 
 namespace WebCore {
 
