e423d6a1dc63e862b6c0bcec79f8135053e3f3bf
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-05 10:52:33 +0000

    2010-06-05  Anders Bakken  <agbakken@gmail.com>
            Reviewed by Eric Seidel.
    
            [Qt] FileSystemQt.cpp has coding-style errors
            https://bugs.webkit.org/show_bug.cgi?id=39775
    
            * platform/qt/FileSystemQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/FileSystemQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/FileSystemQt.cpp
@@ -10,13 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * are met:
  *
  * 1.  Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer. 
+ *     notice, this list of conditions and the following disclaimer.
  * 2.  Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution. 
+ *     documentation and/or other materials provided with the distribution.
  * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
  *     its contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission. 
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -34,14 +34,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FileSystem.h"
 
 #include "PlatformString.h"
-#include <wtf/text/CString.h>
-
-#include <QDateTime>
-#include <QFile>
-#include <QTemporaryFile>
-#include <QFileInfo>
 #include <QDateTime>
 #include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QTemporaryFile>
+#include <wtf/text/CString.h>
 
 namespace WebCore {
 
