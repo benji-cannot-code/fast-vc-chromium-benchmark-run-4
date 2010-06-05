c7eb749d1266414f9527eb9eb8881b5e29e6dfeb
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-05 01:28:34 +0000

    2010-06-04  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r60684.
            http://trac.webkit.org/changeset/60684
            https://bugs.webkit.org/show_bug.cgi?id=40196
    
            This patch broke chromium reliability tests (Requested by
            tonyg-cr on #webkit).
    
            * bindings/v8/ScriptSourceCode.h:
            (WebCore::ScriptSourceCode::ScriptSourceCode):
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::compileScript):
            (WebCore::V8Proxy::evaluate):
            * bindings/v8/V8Proxy.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/ScriptSourceCode.h
+++ third_party/WebKit/WebCore/bindings/v8/ScriptSourceCode.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ScriptSourceCode_h
 #define ScriptSourceCode_h
 
-#include "CachedResourceHandle.h"
 #include "CachedScript.h"
 #include "KURL.h"
 #include "PlatformString.h"
@@ -43,7 +42,6 @@ class ScriptSourceCode {
 public:
     ScriptSourceCode(const String& source, const KURL& url = KURL(), int startLine = 1)
         : m_source(source)
-        , m_cachedScript(0)
         , m_url(url)
         , m_startLine(startLine)
     {
@@ -53,7 +51,6 @@ public:
     // Not sure if that matters.
     ScriptSourceCode(CachedScript* cs)
         : m_source(cs->script())
-        , m_cachedScript(cs)
         , m_url(ParsedURLString, cs->url())
         , m_startLine(1)
     {
@@ -62,13 +59,11 @@ public:
     bool isEmpty() const { return m_source.isEmpty(); }
 
     const String& source() const { return m_source; }
-    CachedScript* cachedScript() const { return m_cachedScript.get(); }
     const KURL& url() const { return m_url; }
     int startLine() const { return m_startLine; }
 
 private:
     String m_source;
-    CachedResourceHandle<CachedScript> m_cachedScript;
     KURL m_url;
     int m_startLine;
 };
