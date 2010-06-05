079c8af9648f7ff1d5ec51d92b54f9fea3ed8b8f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-05 05:14:36 +0000

    2010-06-04  Anders Bakken  <agbakken@gmail.com>
            Reviewed by Eric Seidel.
    
            [Qt] SoundQt.cpp has coding-style errors
            https://bugs.webkit.org/show_bug.cgi?id=39762
    
            * platform/qt/SoundQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60733 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/SoundQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/SoundQt.cpp
@@ -27,10 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+#include "Sound.h"
 
 #include <QApplication>
-
-#include "Sound.h"
 
 namespace WebCore {
 
