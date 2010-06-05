4d018efbf6ed24d303d64c68725d5eb884b9f0c7
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-05 10:12:34 +0000

    2010-06-05  Anders Bakken  <agbakken@gmail.com>
            Reviewed by Eric Seidel.
    
            [Qt] LoggingQt.cpp has coding-style errors
            https://bugs.webkit.org/show_bug.cgi?id=39772
    
            * platform/qt/LoggingQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60745 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/LoggingQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/LoggingQt.cpp
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "Logging.h"
-#include "PlatformString.h"
 
+#include "PlatformString.h"
 #include <QDebug>
 #include <QStringList>
 
