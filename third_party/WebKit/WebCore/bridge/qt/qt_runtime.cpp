bb29bbe27189a4883d43923316236ccdb98a36bc
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-06 23:54:37 +0000

    * bridge/qt/qt_runtime.cpp: (JSC::Bindings::findMethodIndex): (JSC::Bindings::QtRuntimeMetaMethod::call): (JSC::Bindings::QtRuntimeConnectionMethod::call):
    Reviewed by NOBODY (Qt build fix pt 2).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60765 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
+++ third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
@@ -1568,9 +1568,9 @@ EncodedJSValue QtRuntimeConnectionMethod::call(ExecState* exec)
                             funcObject = asFuncObj;
                         } else {
                             if (d->m_isConnect)
-                                return throwError(exec, createTypeError(exec, "QtMetaMethod.connect: target is not a function"));
+                                return throwVMError(exec, createTypeError(exec, "QtMetaMethod.connect: target is not a function"));
                             else
-                                return throwError(exec, createTypeError(exec, "QtMetaMethod.disconnect: target is not a function"));
+                                return throwVMError(exec, createTypeError(exec, "QtMetaMethod.disconnect: target is not a function"));
                         }
                     }
                 } else {
