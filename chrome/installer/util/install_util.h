b1a3e80ce7f53aded618d213b2a8d16f161c6d3e
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-07 17:27:46 +0000

    Fix a couple of issues with MSI support through upgrades:  1) When installing from an msi, remove any uninstall shortcuts currently present for that type (system/per-user) of install.  2) When uninstalling, proactively delete the MSI marker from the Omaha ClientState key to avoid an uninstall-reinstall scenario which can cause a reinstall of a non-msi build to be tagged as being msi-managed.
    BUG=45252,45273
    TEST=Install non-MSI GCF, then install MSI GCF. Observe only a single ARP dialog entry. Then uninstall MSI GCF and quickly reinstall non-MSI GCF, again observe a single ARP dialog entry.
    
    
    
    Review URL: http://codereview.chromium.org/2459003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49069 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/install_util.h
+++ chrome/installer/util/install_util.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/version.h"
 
 class WorkItemList;
+class RegKey;
 
 // This is a utility class that provides common installation related
 // utility methods that can be used by installer and also unit tested
@@ -99,6 +100,14 @@ class InstallUtil {
                                        int dll_names_count,
                                        bool do_register,
                                        WorkItemList* registration_list);
+
+  // Deletes the registry key at path key_path under the key given by root_key.
+  static bool DeleteRegistryKey(RegKey& root_key, const std::wstring& key_path);
+
+  // Deletes the registry value named value_name at path key_path under the key
+  // given by reg_root.
+  static bool DeleteRegistryValue(HKEY reg_root, const std::wstring& key_path,
+                                  const std::wstring& value_name);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(InstallUtil);
