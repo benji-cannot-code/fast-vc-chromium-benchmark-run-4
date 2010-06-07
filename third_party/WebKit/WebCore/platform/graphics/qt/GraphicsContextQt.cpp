3ec6b78e88d500b13389b74f4d962e677558216d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-07 19:27:33 +0000

    2010-06-07  Ariya Hidayat  <ariya.hidayat@codeaurora.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Fix GraphicsContext::clipOut to take the transformation matrix into account.
    
            * platform/graphics/qt/GraphicsContextQt.cpp:
            (WebCore::GraphicsContext::clipOut):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60796 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
@@ -1012,7 +1012,8 @@ void GraphicsContext::clipOut(const Path& path)
         newClip.addPath(clippedOut);
         p->setClipPath(newClip, Qt::IntersectClip);
     } else {
-        newClip.addRect(p->window());
+        QRect windowRect = p->transform().inverted().mapRect(p->window());
+        newClip.addRect(windowRect);
         newClip.addPath(clippedOut.intersected(newClip));
         p->setClipPath(newClip);
     }
