ccb63884ce8b8be7d2661756f0c53ef6292fc033
staikos@webkit.org <staikos@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-07 04:53:45 +0000

    Unreviewed: Since 2006 I learned out to spell my name.
    git-svn-id: svn://svn.chromium.org/blink/trunk@60769 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/WidgetQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/WidgetQt.cpp
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006 Dirk Mueller <mueller@kde.org>
- * Copyright (C) 2006 George Stiakos <staikos@kde.org>
+ * Copyright (C) 2006 George Staikos <staikos@kde.org>
  * Copyright (C) 2006 Zack Rusin <zack@kde.org>
  * Copyright (C) 2006 Nikolas Zimmermann <zimmermann@kde.org>
  * Copyright (C) 2008 Holger Hans Peter Freyther
