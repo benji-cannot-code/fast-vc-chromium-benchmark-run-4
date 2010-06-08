68fcad90339a77af776e8b391aa8e56399f9f27c
luchen@google.com <luchen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-08 18:21:18 +0000

    Adding support for pointlist, linelist, linestrip, trianglestrip and triangelfan primitive drawing types. Demo file included. Review URL: http://codereview.chromium.org/2645003
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49173 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/samples/o3d-webgl/buffer.js
+++ o3d/samples/o3d-webgl/buffer.js
@@ -195,7 +195,7 @@ o3d.Buffer.prototype.set =
   if (!values.length) {
     o3d.notImplemented();
   }
-  if (this.array_ == null) {
+  if (this.array_ == null || this.array_.length != values.length) {
     this.resize(values.length);
   }
   this.lock();
