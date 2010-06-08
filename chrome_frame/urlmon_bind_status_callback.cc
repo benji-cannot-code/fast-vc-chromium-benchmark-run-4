a998d7fb29404eeac0ee16df7df69d9b209f012f
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-08 22:40:35 +0000

    Catch possible out of memory situations when looking for the meta tag.
    TEST=See bug.
    BUG=46045
    
    Review URL: http://codereview.chromium.org/2728001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49198 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/urlmon_bind_status_callback.cc
+++ chrome_frame/urlmon_bind_status_callback.cc
@@ -178,12 +178,12 @@ void SniffData::DetermineRendererType(bool last_chance) {
       if (is_cache_valid() && cache_) {
         HGLOBAL memory = NULL;
         GetHGlobalFromStream(cache_, &memory);
-        char* buffer = reinterpret_cast<char*>(GlobalLock(memory));
+        const char* buffer = reinterpret_cast<const char*>(GlobalLock(memory));
 
         std::wstring html_contents;
         // TODO(joshia): detect and handle different content encodings
         if (buffer && size_) {
-          UTF8ToWide(buffer, size_, &html_contents);
+          UTF8ToWide(buffer, std::min(size_, kMaxSniffSize), &html_contents);
           GlobalUnlock(memory);
         }
 
@@ -198,8 +198,8 @@ void SniffData::DetermineRendererType(bool last_chance) {
       }
     }
     DLOG(INFO) << __FUNCTION__ << "Url: " << url_ <<
-      StringPrintf("Renderer type: %s",
-                    renderer_type_ == CHROME ? "CHROME" : "OTHER");
+        StringPrintf("Renderer type: %s",
+                      renderer_type_ == CHROME ? "CHROME" : "OTHER");
   }
 }
 
