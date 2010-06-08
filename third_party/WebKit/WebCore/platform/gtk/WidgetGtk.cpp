bc8fd07c0d6cfae1ae4f31cdd02fa63cbd6bd702
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-08 15:48:28 +0000

    Unreviewed build fix after r60785.
    [GTK] Add missing GtkVersioning.h include to fix the build
    on older GTK+ versions.
    
    No new tests needed.
    
    * platform/gtk/WidgetGtk.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60846 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/WidgetGtk.cpp
+++ third_party/WebKit/WebCore/platform/gtk/WidgetGtk.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Cursor.h"
 #include "FrameView.h"
 #include "GraphicsContext.h"
+#include "GtkVersioning.h"
 #include "HostWindow.h"
 #include "IntRect.h"
 #include "RenderObject.h"
