817f7553b476f44e849a2d6b94ae4fb0f15d9a14
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-08 00:19:59 +0000

    First unified app menu behind the "--new-wrench-menu" flag.
    Does not contain Cole's unified edit menu / zoom controls. This is just
    scaffolding that should allow all the rest of the unified menu to exist, while
    letting each platform implement the native-only portions in their platform
    specific ways.
    
    Only modifies the linux implementation to actually use the new model, though the command line flag and the new model should be shared between platforms.
    
    BUG=45734,45757
    TEST=Run with --new-wrench-menu on linux. Notice how long the unified menu is.
    
    Review URL: http://codereview.chromium.org/2677006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49130 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_toolbar_gtk.h
+++ chrome/browser/gtk/browser_toolbar_gtk.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_menu_model.h"
 #include "chrome/browser/pref_member.h"
 #include "chrome/browser/sync/profile_sync_service.h"
+#include "chrome/browser/wrench_menu_model.h"
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_registrar.h"
 #include "chrome/common/owned_widget_gtk.h"
@@ -244,6 +245,7 @@ class BrowserToolbarGtk : public CommandUpdater::CommandObserver,
 
   PageMenuModel page_menu_model_;
   AppMenuModel app_menu_model_;
+  WrenchMenuModel wrench_menu_model_;
 
   Browser* browser_;
   BrowserWindowGtk* window_;
