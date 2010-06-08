6b982165c8eca4b441910643f0d0bd57b4b9dcb1
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-08 01:50:41 +0000

    Heartbeat error logging for chromoting. TEST=simple_host logs error received from the bot. BUG=None
    Review URL: http://codereview.chromium.org/2711003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49136 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/heartbeat_sender.h
+++ remoting/host/heartbeat_sender.h
@@ -27,9 +27,10 @@ class HeartbeatSender : public base::RefCountedThreadSafe<HeartbeatSender> {
   void Start(JingleClient* jingle_client, const std::string& host_id);
 
  private:
-
   void DoStart();
   void DoSendStanza();
+
+  void ProcessResponse(const buzz::XmlElement* response);
 
   bool started_;
   JingleClient* jingle_client_;
