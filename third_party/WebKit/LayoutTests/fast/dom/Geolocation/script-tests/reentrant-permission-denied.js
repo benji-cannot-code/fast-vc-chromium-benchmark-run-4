31d3a3c2bd0b02d9d52be8039d530db9dba7a145
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-08 12:17:31 +0000

    Geolocation should have a LayoutTest for reentrant calls that cause a PERMISSION_DENIED error https://bugs.webkit.org/show_bug.cgi?id=40174
    Reviewed by Alexey Proskuryakov.
    
    Tests for reentrant calls with success and POSITION_UNAVAILABLE responses already exist,
    but the code path for PERMISSION_DENIED is different.
    
    * fast/dom/Geolocation/reentrant-permission-denied-expected.txt: Added.
    * fast/dom/Geolocation/reentrant-permission-denied.html: Added.
    * fast/dom/Geolocation/script-tests/reentrant-permission-denied.js: Added.
    * platform/gtk/Skipped: Modified.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60839 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/Geolocation/script-tests/reentrant-permission-denied.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Tests that reentrant calls to Geolocation methods from the error callback due to a PERMISSION_DENIED error are OK.");
+
+if (window.layoutTestController) {
+    layoutTestController.setGeolocationPermission(false);
+    layoutTestController.setMockGeolocationPosition(51.478, -0.166, 100.0);
+} else
+    debug('This test can not be run without the LayoutTestController');
+
+var error;
+function checkPermissionError(e) {
+    error = e;
+    shouldBe('error.code', 'error.PERMISSION_DENIED');
+    shouldBe('error.message', '"User denied Geolocation"');
+}
+
+var errorCallbackInvoked = false;
+navigator.geolocation.getCurrentPosition(function(p) {
+    testFailed('Success callback invoked unexpectedly');
+    finishJSTest();
+}, function(e) {
+    if (errorCallbackInvoked) {
+        testFailed('Error callback invoked unexpectedly');
+        finishJSTest();
+    }
+    errorCallbackInvoked = true;
+    checkPermissionError(e);
+    continueTest();
+});
+
+function continueTest() {
+    navigator.geolocation.getCurrentPosition(function(p) {
+        testFailed('Success callback invoked unexpectedly');
+        finishJSTest();
+    }, function(e) {
+        checkPermissionError(e);
+        finishJSTest();
+    });
+}
+
+window.jsTestIsAsync = true;
+window.successfullyParsed = true;
