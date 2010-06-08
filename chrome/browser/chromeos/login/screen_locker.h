b42772a8a1418dfe18e059564f55fd900f25adc3
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-08 02:51:42 +0000

    * Show error message when unlocking failed. * Add "sign out" button * Let chrome crash when grab is borken. * send ScreenIsLocked signal after all inputs are grabbed.
    BUG=chromium-os:2914,chromium-os:2614
    TEST=manual:
    set the content of /var/lib/power_manager/use_xscreensaver to 1,
    reboot the machine and login
    hit ctr-alt-l to enter screenlock mode
    1) typing in wrong password should show message bubble with error message
    2) pressing "Sign out" will terminate the session and move to login screen
    
    Review URL: http://codereview.chromium.org/2685006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49141 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screen_locker.h
+++ chrome/browser/chromeos/login/screen_locker.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task.h"
 #include "chrome/browser/chromeos/login/login_status_consumer.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
+#include "chrome/browser/views/info_bubble.h"
 
 namespace gfx {
 class Rect;
@@ -23,6 +24,8 @@ class WidgetGtk;
 namespace chromeos {
 
 class Authenticator;
+class MessageBubble;
+class MouseEventRelay;
 class ScreenLockView;
 
 namespace test {
@@ -32,7 +35,8 @@ class ScreenLockerTester;
 // ScreenLocker creates a background view as well as ScreenLockView to
 // authenticate the user. ScreenLocker manages its life cycle and will
 // delete itself when it's unlocked.
-class ScreenLocker : public LoginStatusConsumer {
+class ScreenLocker : public LoginStatusConsumer,
+                     public InfoBubbleDelegate {
  public:
   explicit ScreenLocker(const UserManager::User& user);
 
@@ -44,11 +48,26 @@ class ScreenLocker : public LoginStatusConsumer {
   virtual void OnLoginSuccess(const std::string& username,
                               const std::string& credentials);
 
+  // Overridden from views::InfoBubbleDelegate.
+  virtual void InfoBubbleClosing(InfoBubble* info_bubble,
+                                 bool closed_by_escape);
+  virtual bool CloseOnEscape() { return true; }
+  virtual bool FadeInOnShow() { return false; }
+
   // Authenticates the user with given |password| and authenticator.
   void Authenticate(const string16& password);
 
+  // Close message bubble to clear error messages.
+  void ClearErrors();
+
   // (Re)enable input field.
   void EnableInput();
+
+  // Exit the chrome, which will sign out the current session.
+  void Signout();
+
+  // Called when the screen locker is ready.
+  void ScreenLockReady();
 
   // Returns the user to authenticate.
   const UserManager::User& user() const {
@@ -93,8 +112,17 @@ class ScreenLocker : public LoginStatusConsumer {
   // Logged in user.
   UserManager::User user_;
 
-  // Used for logging in.
+  // Used to authenticate the user to unlock.
   scoped_refptr<Authenticator> authenticator_;
+
+  // ScreenLocker grabs all keyboard and mouse events on its
+  // gdk window and never let other gdk_window to handle inputs.
+  // This MouseEventRelay object is used to forward events to
+  // the message bubble's gdk_window so that close button works.
+  scoped_ptr<MouseEventRelay> mouse_event_relay_;
+
+  // An info bubble to display login failure message.
+  MessageBubble* error_info_;
 
   // Reference to the single instance of the screen locker object.
   // This is used to make sure there is only one screen locker instance.
