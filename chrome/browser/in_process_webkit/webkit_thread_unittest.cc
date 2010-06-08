a2c681c2b21d3336c9c88e16464c291b7aff418b
senorblanco@google.com <senorblanco@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-08 18:24:30 +0000

    Disable WebKitThreadTest.ExposedInChromeThread, due to subsequent assert.
    BUG=46087
    TEST=see bug
    TBR=evan
    
    Review URL: http://codereview.chromium.org/2777001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49174 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/in_process_webkit/webkit_thread_unittest.cc
+++ chrome/browser/in_process_webkit/webkit_thread_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/in_process_webkit/webkit_thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-TEST(WebKitThreadTest, ExposedInChromeThread) {
+TEST(WebKitThreadTest, DISABLED_ExposedInChromeThread) {
   int* null = NULL;  // Help the template system out.
   EXPECT_FALSE(ChromeThread::DeleteSoon(ChromeThread::WEBKIT, FROM_HERE, null));
   {
