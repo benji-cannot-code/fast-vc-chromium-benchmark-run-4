acbf7db5db706bb34e7ce4e948fa2bdc27615c7b
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-08 14:52:59 +0000

    2010-06-08  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: extend ScriptDebugServer to support script source editing
            https://bugs.webkit.org/show_bug.cgi?id=40297
    
            * bindings/js/ScriptDebugServer.cpp:
            (WebCore::ScriptDebugServer::editScriptSource):
            * bindings/js/ScriptDebugServer.h:
            * bindings/v8/ScriptDebugServer.cpp:
            (WebCore::ScriptDebugServer::editScriptSource):
            * bindings/v8/ScriptDebugServer.h:
            * inspector/InspectorBackend.cpp:
            (WebCore::InspectorBackend::editScriptSource):
            * inspector/InspectorBackend.h:
            * inspector/InspectorBackend.idl:
            * inspector/InspectorController.cpp:
            (WebCore::InspectorController::InspectorController):
            (WebCore::InspectorController::disableDebugger):
            (WebCore::InspectorController::editScriptSource):
            (WebCore::InspectorController::currentCallFrames):
            (WebCore::InspectorController::didPause):
            (WebCore::InspectorController::didContinue):
            * inspector/InspectorController.h:
            * inspector/InspectorFrontend.cpp:
            (WebCore::InspectorFrontend::didEditScriptSource):
            * inspector/InspectorFrontend.h:
            * inspector/front-end/Script.js:
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel.prototype.canEditScripts):
            (WebInspector.ScriptsPanel.prototype.editScriptSource.mycallback):
            (WebInspector.ScriptsPanel.prototype.editScriptSource):
            * inspector/front-end/Settings.js:
            * inspector/front-end/SourceView.js:
            (WebInspector.SourceView.prototype._editLine):
            * inspector/front-end/TextViewer.js:
            (WebInspector.TextViewer.prototype._handleDoubleClick):
            (WebInspector.TextViewer.prototype._commitEditingLine):
            * inspector/front-end/inspector.css:
            (#scripts-files option.extension-script):
    
    2010-06-08  Yury Semikhatsky  <yurys@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: extend ScriptDebugServer to support script source editing
            https://bugs.webkit.org/show_bug.cgi?id=40297
    
            * src/js/DebuggerAgent.js:
            (devtools.ScriptInfo.prototype.worldType):
            * src/js/DebuggerScript.js:
            (debuggerScriptConstructor.DebuggerScript.editScriptSource):
            * src/js/DevTools.js:
            (WebInspector.loaded):
            * src/js/InspectorControllerImpl.js:
            (devtools.InspectorBackendImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60843 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorFrontend.h
+++ third_party/WebKit/WebCore/inspector/InspectorFrontend.h
@@ -99,6 +99,8 @@ namespace WebCore {
         void pausedScript(SerializedScriptValue* callFrames);
         void resumedScript();
 
+        void didEditScriptSource(long callId, bool success, const String& result, SerializedScriptValue* newCallFrames);
+
         void profilerWasEnabled();
         void profilerWasDisabled();
         void addProfileHeader(const ScriptValue& profile);
--- third_party/WebKit/WebKit/chromium/src/js/DevTools.js
+++ third_party/WebKit/WebKit/chromium/src/js/DevTools.js
@@ -184,6 +184,8 @@ WebInspector.loaded = function()
     Preferences.heapProfilerPresent = true;
     Preferences.debuggerAlwaysEnabled = true;
     Preferences.profilerAlwaysEnabled = true;
+    Preferences.canEditScriptSource = true;
+
     RemoteDebuggerAgent.setDebuggerScriptSource("(" + debuggerScriptConstructor + ")();");
  
     oldLoaded.call(this);
