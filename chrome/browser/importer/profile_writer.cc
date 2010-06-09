9c036fafd90f306758dd1398622cee17062f6278
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-09 22:52:05 +0000

    Fix password import for OS X Firefox.
    BUG=18775
    TEST=import passwords from Firefox on OS X.
    Review URL: http://codereview.chromium.org/2725005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49336 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/profile_writer.cc
+++ chrome/browser/importer/profile_writer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/thread.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/importer/importer.h"
+#include "chrome/browser/password_manager/password_store.h"
 #include "chrome/browser/pref_service.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/search_engines/template_url_model.h"
@@ -26,7 +27,7 @@ bool ProfileWriter::TemplateURLModelIsLoaded() const {
 }
 
 void ProfileWriter::AddPasswordForm(const PasswordForm& form) {
-  profile_->GetWebDataService(Profile::EXPLICIT_ACCESS)->AddLogin(form);
+  profile_->GetPasswordStore(Profile::EXPLICIT_ACCESS)->AddLogin(form);
 }
 
 #if defined(OS_WIN)
