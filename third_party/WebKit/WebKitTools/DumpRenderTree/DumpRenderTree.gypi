e58c5d74a73428d9f08e1f05f7e9aab9783a895c
rolandsteiner@chromium.org <rolandsteiner@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-09 06:29:10 +0000

    2010-06-02  Roland Steiner  <rolandsteiner@chromium.org>
            Reviewed by Tamura Kent
    
            Bug 40052 - [DRT/Chromium] Upstream test_shell_webthemeengine as WebThemeEngineDRT
            https://bugs.webkit.org/show_bug.cgi?id=40052
    
            Add WebThemeEngineDRT and WebThemeControlDRT from Chromium rev. 48907
    
            * DumpRenderTree/DumpRenderTree.gypi:
            * DumpRenderTree/chromium/TestShellWin.cpp:
            (platformInit):
            * DumpRenderTree/chromium/WebThemeControlDRT.cpp: Added.
            (WebKit::):
            (WebKit::WebThemeControlDRT::WebThemeControlDRT):
            (WebKit::WebThemeControlDRT::~WebThemeControlDRT):
            (WebKit::WebThemeControlDRT::box):
            (WebKit::WebThemeControlDRT::line):
            (WebKit::WebThemeControlDRT::triangle):
            (WebKit::WebThemeControlDRT::roundRect):
            (WebKit::WebThemeControlDRT::oval):
            (WebKit::WebThemeControlDRT::circle):
            (WebKit::WebThemeControlDRT::nestedBoxes):
            (WebKit::WebThemeControlDRT::markState):
            (WebKit::WebThemeControlDRT::draw):
            (WebKit::WebThemeControlDRT::drawTextField):
            (WebKit::WebThemeControlDRT::drawProgressBar):
            * DumpRenderTree/chromium/WebThemeControlDRT.h: Added.
            (WebKit::WebThemeControlDRT::):
            * DumpRenderTree/chromium/WebThemeEngineDRT.cpp: Added.
            (WebKit::):
            (WebKit::WebThemeEngineDRT::paintButton):
            (WebKit::WebThemeEngineDRT::paintMenuList):
            (WebKit::WebThemeEngineDRT::paintScrollbarArrow):
            (WebKit::WebThemeEngineDRT::paintScrollbarThumb):
            (WebKit::WebThemeEngineDRT::paintScrollbarTrack):
            (WebKit::WebThemeEngineDRT::paintTextField):
            (WebKit::WebThemeEngineDRT::paintTrackbar):
            (WebKit::WebThemeEngineDRT::paintProgressBar):
            * DumpRenderTree/chromium/WebThemeEngineDRT.h: Added.
            (WebKit::WebThemeEngineDRT::WebThemeEngineDRT):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60883 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.gypi
+++ third_party/WebKit/WebKitTools/DumpRenderTree/DumpRenderTree.gypi
@@ -35,5 +35,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'chromium/WebViewHost.cpp',
             'chromium/WebViewHost.h',
         ],
+        'conditions': [
+            ['OS=="win"', {
+                'drt_files': [
+                    'chromium/WebThemeControlDRT.cpp',
+                    'chromium/WebThemeControlDRT.h',
+                    'chromium/WebThemeEngineDRT.cpp',
+                    'chromium/WebThemeEngineDRT.h',
+                ],
+            }],
+        ],
     }
 }
--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/TestShellWin.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/TestShellWin.cpp
@@ -32,12 +32,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "TestShell.h"
 
+#include "WebThemeEngineDRT.h"
 #include "webkit/support/webkit_support.h"
 #include <fcntl.h>
 #include <io.h>
 #include <process.h>
 #include <shlwapi.h>
 #include <sys/stat.h>
+
+// Theme engine
+static WebThemeEngineDRT themeEngine;
 
 // Thread main to run for the thread which just tests for timeout.
 unsigned int __stdcall watchDogThread(void* arg)
@@ -105,6 +109,9 @@ void platformInit()
     // Set stdout/stderr binary mode.
     _setmode(_fileno(stdout), _O_BINARY);
     _setmode(_fileno(stderr), _O_BINARY);
+
+    // Set theme engine.
+    webkit_support::SetThemeEngine(&themeEngine);
 
     // Load Ahem font.
     // AHEM____.TTF is copied to the directory of DumpRenderTree.exe by WebKit.gyp.
