6721a9b65122f5ffea215835ac99ffd736cf0db6
ctguil@chromium.org <ctguil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-09 23:02:30 +0000

    Enable renderer accessibility by default.
    BUG=25564
    TEST=none
    
    Review URL: http://codereview.chromium.org/2358003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49339 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host.cc
+++ chrome/browser/renderer_host/render_widget_host.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/render_widget_host.h"
 
 #include "base/auto_reset.h"
+#include "base/command_line.h"
 #include "base/histogram.h"
 #include "base/keyboard_codes.h"
 #include "base/message_loop.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/render_widget_host_painting_observer.h"
 #include "chrome/browser/renderer_host/render_widget_host_view.h"
 #include "chrome/browser/renderer_host/video_layer.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/render_messages.h"
 #include "webkit/glue/webcursor.h"
@@ -58,6 +60,9 @@ static const int kHungRendererDelayMs = 20000;
 // smoothness of scrolling with a risk of falling behind the events, resulting
 // in trailing scrolls after the user ends their input.
 static const int kMaxTimeBetweenWheelMessagesMs = 250;
+
+// static
+bool RenderWidgetHost::renderer_accessible_ = false;
 
 ///////////////////////////////////////////////////////////////////////////////
 // RenderWidgetHost
@@ -1110,6 +1115,35 @@ void RenderWidgetHost::AdvanceToNextMisspelling() {
 
 void RenderWidgetHost::RequestAccessibilityTree() {
   Send(new ViewMsg_GetAccessibilityTree(routing_id()));
+}
+
+void RenderWidgetHost::SetDocumentLoaded(bool document_loaded) {
+  document_loaded_ = document_loaded;
+
+  if (!document_loaded_)
+    requested_accessibility_tree_ = false;
+
+  if (renderer_accessible_ && document_loaded_) {
+    RequestAccessibilityTree();
+    requested_accessibility_tree_ = true;
+  }
+}
+
+void RenderWidgetHost::EnableRendererAccessibility() {
+  if (renderer_accessible_)
+    return;
+
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kDisableRendererAccessibility)) {
+    return;
+  }
+
+  renderer_accessible_ = true;
+
+  if (document_loaded_ && !requested_accessibility_tree_) {
+    RequestAccessibilityTree();
+    requested_accessibility_tree_ = true;
+  }
 }
 
 void RenderWidgetHost::SetAccessibilityFocus(int acc_obj_id) {
--- chrome/browser/renderer_host/render_widget_host_view_win.h
+++ chrome/browser/renderer_host/render_widget_host_view_win.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <atlcrack.h>
 #include <atlmisc.h>
 
+#include <vector>
+
 #include "base/scoped_comptr_win.h"
 #include "base/scoped_ptr.h"
 #include "base/task.h"
@@ -250,6 +252,9 @@ class RenderWidgetHostViewWin
   // Whether the window should be activated.
   bool IsActivatable() const;
 
+  // MSAA IAccessible returned while page contents is loading.
+  ScopedComPtr<IAccessible> loading_accessible_;
+
   // The associated Model.
   RenderWidgetHost* render_widget_host_;
 
@@ -320,11 +325,6 @@ class RenderWidgetHostViewWin
   // not having anything to paint (empty backing store from renderer). This
   // value returns true for is_null() if we are not recording whiteout times.
   base::TimeTicks whiteout_start_time_;
-
-  // Whether the renderer is made accessible.
-  // TODO(jcampan): http://b/issue?id=1432077 This is a temporary work-around
-  // until that bug is fixed.
-  bool renderer_accessible_;
 
   // The time it took after this view was selected for it to be fully painted.
   base::TimeTicks tab_switch_paint_time_;
