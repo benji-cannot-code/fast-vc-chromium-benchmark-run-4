d0f0bd2f78ef7196cea7fec33132a12b9253573f
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-09 17:01:13 +0000

    Clean up geolocation_dispatcher_host.h
    It was pulling internal geolocation headers into resource_message_filter.cc
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2742002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49271 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/geolocation/geolocation_permission_context.cc
+++ chrome/browser/geolocation/geolocation_permission_context.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extensions_service.h"
 #include "chrome/browser/geolocation/geolocation_content_settings_map.h"
 #include "chrome/browser/geolocation/geolocation_dispatcher_host.h"
+#include "chrome/browser/geolocation/location_arbitrator.h"
 #include "chrome/browser/pref_service.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
