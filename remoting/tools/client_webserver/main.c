417c0491ffcf5eebeb960d92373f661f4c74fc43
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-10 15:30:03 +0000

    Fix up warning due to missing declarations.
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/2735006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49400 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/tools/client_webserver/main.c
+++ remoting/tools/client_webserver/main.c
@@ -13,11 +13,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This is useful for testing the Chromoting plugin while we wait for
 // updated mimetype support to be added to Chrome.
 
+#include "build/build_config.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <netinet/in.h>
+
+#if defined (OS_POSIX)
+#include <unistd.h>
+#endif //  OS_POSIX
 
 #define PORT 8080
 
