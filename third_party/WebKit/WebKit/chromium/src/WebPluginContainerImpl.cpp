e30ebb049c2d02a9e08d44715ba869fc90352be0
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-10 09:31:47 +0000

    2010-06-10  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Reduce FrameView.h includes to speed up build times
            https://bugs.webkit.org/show_bug.cgi?id=40408
    
            Another fix for Windows.
    
            * WebView.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60950 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebPluginContainerImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebPluginContainerImpl.cpp
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KeyboardEvent.h"
 #include "MouseEvent.h"
 #include "Page.h"
+#include "RenderBox.h"
 #include "ScrollView.h"
 #include "WheelEvent.h"
 
--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/PluginView.h>
 #include <WebCore/PopupMenu.h>
 #include <WebCore/ProgressTracker.h>
+#include <WebCore/RenderLayer.h>
 #include <WebCore/RenderTheme.h>
 #include <WebCore/RenderView.h>
 #include <WebCore/RenderWidget.h>
