4049b2c9a10f5e496423c57bd359866e48387db4
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-10 06:34:46 +0000

    2010-06-09  Tony Gentilcore  <tonyg@chromium.org>
            Reviewed by Adam Barth.
    
            HTML5 Parser: Fix fast/profiler tests that depend on event handler line numbers
            https://bugs.webkit.org/show_bug.cgi?id=40393
    
            This emulated the old behavior in HTMLTokenizer:processToken()
    
            No new tests because covered by:
             - fast/profiler/dead-time.html
             - fast/profiler/inline-event-handler.html
             - fast/profiler/stop-profiling-after-setTimeout.html
             - fast/profiler/throw-exception-from-eval.html
    
            * html/HTML5Tokenizer.cpp:
            (WebCore::HTML5Tokenizer::pumpLexer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60940 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTML5Tokenizer.h
+++ third_party/WebKit/WebCore/html/HTML5Tokenizer.h
@@ -28,8 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HTML5Tokenizer_h
 
 #include "CachedResourceClient.h"
+#include "Frame.h"
 #include "HTML5ScriptRunnerHost.h"
 #include "HTML5Token.h"
+#include "HTMLDocument.h"
 #include "SegmentedString.h"
 #include "Tokenizer.h"
 #include <wtf/OwnPtr.h>
@@ -39,7 +41,7 @@ namespace WebCore {
 class HTML5Lexer;
 class HTML5ScriptRunner;
 class HTML5TreeBuilder;
-class HTMLDocument;
+class ScriptController;
 class ScriptSourceCode;
 
 // FIXME: The whole Tokenizer class system should be renamed "Parser"
@@ -167,6 +169,8 @@ private:
     void attemptToEnd();
     void endIfDelayed();
     bool inWrite() const { return m_writeNestingLevel > 0; }
+
+    ScriptController* script() const { return m_document->frame() ? m_document->frame()->script() : 0; }
 
     InputStream m_input;
 
