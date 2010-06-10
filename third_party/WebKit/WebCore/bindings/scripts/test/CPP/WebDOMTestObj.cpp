11eee0481ed38a05dae0d5db00f47f74d8ba88d2
andreip@google.com <andreip@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-10 11:23:13 +0000

    2010-06-09  Andrei Popescu  <andreip@google.com>
            Reviewed by Adam Barth.
    
            CodeGeneratorJS.pm incorrectly increments $paramIndex when a method is declared with [CallWith]
            https://bugs.webkit.org/show_bug.cgi?id=40372
    
            Use two variables to keep track of the number of arguments passed from JavaScript vs the number
            of arguments passed to the corresponding C++ method. These numbers can be different since
            the parameter declared via [CallWith] is generated in the bindings instead of being passed
            from JavaScript.
    
            The layout tests added here test both that the parameters are read correctly as well as
            tests some basic indexed database functionality. There are also bindings specific
            tests added in TestObj.idl.
    
            * storage/indexeddb/idb-objectstore-request-expected.txt: Added.
            * storage/indexeddb/idb-objectstore-request.html: Added.
            * storage/indexeddb/script-tests/idb-objectstore-request.js: Added.
            (openSuccess):
            (createSuccess):
            (createObjectStore):
            (doCreateOrOpen):
            (test):
    2010-06-09  Andrei Popescu  <andreip@google.com>
    
            Reviewed by Adam Barth.
    
            CodeGeneratorJS.pm incorrectly increments $paramIndex when a method is declared with [CallWith]
            https://bugs.webkit.org/show_bug.cgi?id=40372
    
            Use two variables to keep track of the number of arguments passed from JavaScript vs the number
            of arguments passed to the corresponding C++ method. These numbers can be different since
            the parameter declared via [CallWith] is generated in the bindings instead of being passed
            from JavaScript.
    
            Test: storage/indexeddb/idb-objectstore-request.html
    
            * bindings/scripts/CodeGeneratorJS.pm:
            * bindings/scripts/test/CPP/WebDOMTestObj.cpp:
            (WebDOMTestObj::withScriptExecutionContext):
            * bindings/scripts/test/CPP/WebDOMTestObj.h:
            * bindings/scripts/test/JS/JSTestObj.cpp:
            (WebCore::jsTestObjPrototypeFunctionWithDynamicFrameAndArg):
            (WebCore::jsTestObjPrototypeFunctionWithDynamicFrameAndOptionalArg):
            (WebCore::jsTestObjPrototypeFunctionWithDynamicFrameAndUserGesture):
            (WebCore::jsTestObjPrototypeFunctionWithDynamicFrameAndUserGestureASAD):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/test/CPP/WebDOMTestObj.cpp
+++ third_party/WebKit/WebCore/bindings/scripts/test/CPP/WebDOMTestObj.cpp
@@ -399,6 +399,14 @@ WebDOMTestObj WebDOMTestObj::withScriptStateObjException()
     return result;
 }
 
+void WebDOMTestObj::withScriptExecutionContext()
+{
+    if (!impl())
+        return;
+
+    impl()->withScriptExecutionContext();
+}
+
 void WebDOMTestObj::methodWithOptionalArg(int opt)
 {
     if (!impl())
