43a4972a6f74b02df979084fcbe3e9d21fda1066
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-10 01:48:02 +0000

    2010-06-09  Yong Li  <yoli@rim.com>
            Reviewed by George Staikos.
    
            https://bugs.webkit.org/show_bug.cgi?id=40252
            Make image down-sampling threshold runtime adjustable.
    
            No new test needed.
    
            * platform/graphics/ImageSource.cpp:
            (WebCore::ImageSource::setData):
            * platform/graphics/ImageSource.h:
            (WebCore::ImageSource::maxPixelsPerDecodedImage):
            (WebCore::ImageSource::setMaxPixelsPerDecodedImage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60927 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/ImageSource.cpp
+++ third_party/WebKit/WebCore/platform/graphics/ImageSource.cpp
@@ -38,6 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+#if ENABLE(IMAGE_DECODER_DOWN_SAMPLING)
+unsigned ImageSource::s_maxPixelsPerDecodedImage = 1024 * 1024;
+#endif
+
 ImageSource::ImageSource()
     : m_decoder(0)
 {
@@ -76,11 +80,8 @@ void ImageSource::setData(SharedBuffer* data, bool allDataReceived)
     if (!m_decoder) {
         m_decoder = static_cast<NativeImageSourcePtr>(ImageDecoder::create(*data));
 #if ENABLE(IMAGE_DECODER_DOWN_SAMPLING)
-#ifndef IMAGE_DECODER_DOWN_SAMPLING_MAX_NUMBER_OF_PIXELS
-#define IMAGE_DECODER_DOWN_SAMPLING_MAX_NUMBER_OF_PIXELS (1024 * 1024)
-#endif
-        if (m_decoder)
-            m_decoder->setMaxNumPixels(IMAGE_DECODER_DOWN_SAMPLING_MAX_NUMBER_OF_PIXELS);
+        if (m_decoder && s_maxPixelsPerDecodedImage)
+            m_decoder->setMaxNumPixels(s_maxPixelsPerDecodedImage);
 #endif
     }
 
