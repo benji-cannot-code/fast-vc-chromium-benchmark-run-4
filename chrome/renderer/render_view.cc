e16e1026bb27223145954b8f06041becdf007558
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-10 15:54:04 +0000

    Clean up the RenderView header.
    There should be no behavior change. I fixed a bunch of naming, alphabetized the message handlers, made some public stuff private, and deleted a smattering of functions that are never used.
    
    TEST=none
    BUG=none
    Review URL: http://codereview.chromium.org/2730006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49402 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -578,8 +578,8 @@ void RenderView::OnMessageReceived(const IPC::Message& message) {
   }
 
   IPC_BEGIN_MESSAGE_MAP(RenderView, message)
-    IPC_MESSAGE_HANDLER(ViewMsg_CaptureThumbnail, SendThumbnail)
-    IPC_MESSAGE_HANDLER(ViewMsg_CaptureSnapshot, SendSnapshot)
+    IPC_MESSAGE_HANDLER(ViewMsg_CaptureThumbnail, OnCaptureThumbnail)
+    IPC_MESSAGE_HANDLER(ViewMsg_CaptureSnapshot, OnCaptureSnapshot)
     IPC_MESSAGE_HANDLER(ViewMsg_PrintPages, OnPrintPages)
     IPC_MESSAGE_HANDLER(ViewMsg_PrintingDone, OnPrintingDone)
     IPC_MESSAGE_HANDLER(ViewMsg_Navigate, OnNavigate)
@@ -643,11 +643,11 @@ void RenderView::OnMessageReceived(const IPC::Message& message) {
         ViewMsg_GetSerializedHtmlDataForCurrentPageWithLocalLinks,
         OnGetSerializedHtmlDataForCurrentPageWithLocalLinks)
     IPC_MESSAGE_HANDLER(ViewMsg_GetApplicationInfo, OnGetApplicationInfo)
-    IPC_MESSAGE_HANDLER(ViewMsg_ShouldClose, OnMsgShouldClose)
+    IPC_MESSAGE_HANDLER(ViewMsg_ShouldClose, OnShouldClose)
     IPC_MESSAGE_HANDLER(ViewMsg_ClosePage, OnClosePage)
     IPC_MESSAGE_HANDLER(ViewMsg_ThemeChanged, OnThemeChanged)
     IPC_MESSAGE_HANDLER(ViewMsg_HandleMessageFromExternalHost,
-                        OnMessageFromExternalHost)
+                        OnHandleMessageFromExternalHost)
     IPC_MESSAGE_HANDLER(ViewMsg_DisassociateFromPopupCount,
                         OnDisassociateFromPopupCount)
     IPC_MESSAGE_HANDLER(ViewMsg_AutoFillSuggestionsReturned,
@@ -698,7 +698,7 @@ void RenderView::OnMessageReceived(const IPC::Message& message) {
   IPC_END_MESSAGE_MAP()
 }
 
-void RenderView::SendThumbnail() {
+void RenderView::OnCaptureThumbnail() {
   WebFrame* main_frame = webview()->mainFrame();
   if (!main_frame)
     return;
@@ -721,7 +721,7 @@ void RenderView::SendThumbnail() {
   Send(new ViewHostMsg_Thumbnail(routing_id_, url, score, thumbnail));
 }
 
-void RenderView::SendSnapshot() {
+void RenderView::OnCaptureSnapshot() {
   SkBitmap snapshot;
   bool error = false;
 
@@ -806,8 +806,7 @@ void RenderView::CapturePageInfo(int load_id, bool preliminary_capture) {
                                       language));
   }
 
-  // thumbnail
-  SendThumbnail();
+  OnCaptureThumbnail();
 }
 
 void RenderView::CaptureText(WebFrame* frame, std::wstring* contents) {
@@ -3602,10 +3601,6 @@ void RenderView::ClearBlockedContentSettings() {
     content_blocked_[i] = false;
 }
 
-void RenderView::DnsPrefetch(const std::vector<std::string>& host_names) {
-  Send(new ViewHostMsg_DnsPrefetch(host_names));
-}
-
 void RenderView::OnZoom(PageZoom::Function function) {
   if (!webview())  // Not sure if this can happen, but no harm in being safe.
     return;
@@ -4061,7 +4056,7 @@ void RenderView::didSerializeDataForFrame(const WebURL& frame_url,
     static_cast<int32>(status)));
 }
 
-void RenderView::OnMsgShouldClose() {
+void RenderView::OnShouldClose() {
   bool should_close = webview()->dispatchBeforeUnloadEvent();
   Send(new ViewHostMsg_ShouldClose_ACK(routing_id_, should_close));
 }
@@ -4104,12 +4099,11 @@ void RenderView::OnThemeChanged() {
 #endif
 }
 
-void RenderView::OnMessageFromExternalHost(const std::string& message,
-                                           const std::string& origin,
-                                           const std::string& target) {
+void RenderView::OnHandleMessageFromExternalHost(const std::string& message,
+                                                 const std::string& origin,
+                                                 const std::string& target) {
   if (message.empty())
     return;
-
   external_host_bindings_.ForwardMessageFromExternalHost(message, origin,
                                                          target);
 }
