7997e7dfa8af0ea9db579b85df9362a94417c8ce
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-10 09:06:08 +0000

    2010-06-10  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Reduce FrameView.h includes to speed up build times
            https://bugs.webkit.org/show_bug.cgi?id=40408
    
            More attempted build fixes for Tiger and Qt.
    
            * page/Frame.cpp:
            * page/Frame.h:
    2010-06-10  Eric Seidel  <eric@webkit.org>
    
            Reviewed by Adam Barth.
    
            Reduce FrameView.h includes to speed up build times
            https://bugs.webkit.org/show_bug.cgi?id=40408
    
            * webkit/webkitwebinspector.cpp:
             - Include RenderLayer.h
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60947 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebinspector.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebinspector.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorClientGtk.h"
 #include "IntPoint.h"
 #include "Page.h"
+#include "RenderLayer.h"
 #include "RenderView.h"
 #include "webkitmarshal.h"
 #include "webkitprivate.h"
