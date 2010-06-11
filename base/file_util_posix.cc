ffab05c5d1300ce42a5748fd726007063c77c590
dhg@chromium.org <dhg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-11 17:44:38 +0000

    Adding sync to be called on ChromeOS after a delete operation has. This ensures that if the user deleted something on a sd card, its synced right after.
    BUG=chromium-os:2661
    TEST=none
    Review URL: http://codereview.chromium.org/2735013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49550 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_posix.cc
+++ base/file_util_posix.cc
@@ -175,6 +175,9 @@ bool Delete(const FilePath& path, bool recursive) {
     directories.pop();
     success = (rmdir(dir.value().c_str()) == 0);
   }
+#if defined(OS_CHROMEOS)
+  sync();
+#endif
   return success;
 }
 
