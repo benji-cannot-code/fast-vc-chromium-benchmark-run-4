33abff655f8c0037557fe350aff271be1716d7a2
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-11 23:25:16 +0000

    Build fix: Remove a duplicate MAYBE_ define.
    TBR=thestig
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2784006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49612 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_sanity_unittest.cc
+++ chrome/browser/debugger/devtools_sanity_unittest.cc
@@ -20,13 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/in_process_browser_test.h"
 #include "chrome/test/ui_test_utils.h"
 
-#if defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
-// See http://crbug.com/40764 for details.
-#define MAYBE_TestResourceContentLength FLAKY_TestResourceContentLength
-#else
-#define MAYBE_TestResourceContentLength TestResourceContentLength
-#endif
-
 namespace {
 
 // Used to block until a dev tools client window's browser is closed.
