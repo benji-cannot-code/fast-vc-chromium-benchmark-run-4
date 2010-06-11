2ecc46d405e3f9253bc9616cc3208c5a7d5bdb17
jhawkins@chromium.org <jhawkins@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-11 00:51:36 +0000

    2010-06-09  James Hawkins  <jhawkins@chromium.org>
            Reviewed by Darin Fisher.
    
            [Chromium] Add the ability to specify a separator in
            AutoFillPopupMenuClient.
    
            https://bugs.webkit.org/show_bug.cgi?id=40397
    
            * public/WebViewClient.h:
            (WebKit::WebViewClient::didAcceptAutoFillSuggestion):
            * src/AutoFillPopupMenuClient.cpp:
            (WebKit::AutoFillPopupMenuClient::getSuggestionsCount):
            (WebKit::AutoFillPopupMenuClient::getSuggestion):
            (WebKit::AutoFillPopupMenuClient::valueChanged):
            (WebKit::AutoFillPopupMenuClient::selectionChanged):
            (WebKit::AutoFillPopupMenuClient::itemIsSeparator):
            (WebKit::AutoFillPopupMenuClient::initialize):
            (WebKit::AutoFillPopupMenuClient::setSuggestions):
            * src/AutoFillPopupMenuClient.h:
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::applyAutoFillSuggestions):
            * src/WebViewImpl.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60985 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
@@ -1787,10 +1787,10 @@ void WebViewImpl::applyAutoFillSuggestions(
     const WebNode& node,
     const WebVector<WebString>& names,
     const WebVector<WebString>& labels,
-    int defaultSuggestionIndex)
+    int separatorIndex)
 {
     ASSERT(names.size() == labels.size());
-    ASSERT(defaultSuggestionIndex < static_cast<int>(names.size()));
+    ASSERT(separatorIndex < static_cast<int>(names.size()));
 
     if (names.isEmpty()) {
         hideSuggestionsPopup();
@@ -1815,7 +1815,7 @@ void WebViewImpl::applyAutoFillSuggestions(
         m_autoFillPopupClient.set(new AutoFillPopupMenuClient);
 
     m_autoFillPopupClient->initialize(inputElem, names, labels,
-                                      defaultSuggestionIndex);
+                                      separatorIndex);
 
     if (m_suggestionsPopupClient != m_autoFillPopupClient.get()) {
         hideSuggestionsPopup();
@@ -1832,7 +1832,7 @@ void WebViewImpl::applyAutoFillSuggestions(
         m_suggestionsPopup = m_autoFillPopup.get();
 
     if (m_suggestionsPopupShowing) {
-        m_autoFillPopupClient->setSuggestions(names, labels);
+        m_autoFillPopupClient->setSuggestions(names, labels, separatorIndex);
         refreshSuggestionsPopup();
     } else {
         m_suggestionsPopup->show(focusedNode->getRect(),
--- third_party/WebKit/WebKit/chromium/src/WebViewImpl.h
+++ third_party/WebKit/WebKit/chromium/src/WebViewImpl.h
@@ -167,7 +167,7 @@ public:
         const WebNode&,
         const WebVector<WebString>& names,
         const WebVector<WebString>& labels,
-        int defaultSuggestionIndex);
+        int separatorIndex);
     virtual void applyAutocompleteSuggestions(
         const WebNode&,
         const WebVector<WebString>& suggestions,
