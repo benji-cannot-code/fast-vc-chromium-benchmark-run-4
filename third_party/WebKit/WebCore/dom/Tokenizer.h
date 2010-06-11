a358069c4c25f4b536ccaee57d8786c2c97332e2
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-11 05:47:52 +0000

    2010-06-10  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r60989.
            http://trac.webkit.org/changeset/60989
            https://bugs.webkit.org/show_bug.cgi?id=40462
    
            It makes things crash (Requested by abarth on #webkit).
    
            * dom/Tokenizer.h:
            (WebCore::Tokenizer::isHTMLTokenizer):
            * html/HTML5Tokenizer.cpp:
            * html/HTML5Tokenizer.h:
            * html/HTML5TreeBuilder.h:
            * html/HTMLFormControlElement.cpp:
            (WebCore::HTMLFormControlElement::removedFromTree):
            * html/HTMLTokenizer.h:
            (WebCore::HTMLTokenizer::isHTMLTokenizer):
            (WebCore::HTMLTokenizer::htmlParser):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@60991 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Tokenizer.h
+++ third_party/WebKit/WebCore/dom/Tokenizer.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    class HTMLParser;
     class HTMLTokenizer;
     class SegmentedString;
     class XSSAuditor;
@@ -62,7 +61,7 @@ namespace WebCore {
         
         virtual void executeScriptsWaitingForStylesheets() {}
 
-        virtual HTMLParser* htmlParser() const { return 0; }
+        virtual bool isHTMLTokenizer() const { return false; }
         virtual HTMLTokenizer* asHTMLTokenizer() { return 0; }
         
         XSSAuditor* xssAuditor() const { return m_XSSAuditor; }
--- third_party/WebKit/WebCore/html/HTML5Tokenizer.h
+++ third_party/WebKit/WebCore/html/HTML5Tokenizer.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class HTMLDocument;
-class HTMLParser;
 class HTML5Lexer;
 class HTML5ScriptRunner;
 class HTML5TreeBuilder;
@@ -62,9 +61,6 @@ public:
     virtual void executeScriptsWaitingForStylesheets();
     virtual int lineNumber() const;
     virtual int columnNumber() const;
-    // FIXME: HTMLFormControlElement accesses the HTMLParser via this method.
-    // Remove this when the HTMLParser is no longer used.
-    virtual HTMLParser* htmlParser() const;
 
     // HTML5ScriptRunnerHost
     virtual void watchForLoad(CachedResource*);
