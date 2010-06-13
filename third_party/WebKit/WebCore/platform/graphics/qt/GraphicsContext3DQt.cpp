cd76b1232cab2e055af129e4b9160cf07e9f441b
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-13 08:33:18 +0000

    2010-06-13  Noam Rosenthal  <noam.rosenthal@nokia.com>
            Reviewed by Darin Adler.
    
            GraphicsContext3D.h in the Qt port includes too many unnecessary files
            https://bugs.webkit.org/show_bug.cgi?id=40536
    
            Removed unncessary includes.
    
            No new tests; Compilation improvement.
    
            * platform/graphics/GraphicsContext3D.h:
            * platform/graphics/qt/GraphicsContext3DQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61087 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebGLTexture.h"
 #include "Uint8Array.h"
 #include <QAbstractScrollArea>
+#include <QGLContext>
 #include <wtf/UnusedParam.h>
 #include <wtf/text/CString.h>
 
