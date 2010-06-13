5972ab3c202378f22e94dc7400ae7b336c148706
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-13 04:46:58 +0000

    2010-06-12  Anders Bakken  <agbakken@gmail.com>
            Reviewed by Eric Seidel.
    
            [Qt] EventHandlerQt.cpp has coding-style errors
            https://bugs.webkit.org/show_bug.cgi?id=40259
    
            Coding style change only.
    
            * page/qt/EventHandlerQt.cpp:
            (WebCore::EventHandler::eventActivatedView):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61078 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/qt/EventHandlerQt.cpp
+++ third_party/WebKit/WebCore/page/qt/EventHandlerQt.cpp
@@ -44,12 +44,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HitTestResult.h"
 #include "KeyboardEvent.h"
 #include "MouseEventWithHitTestResults.h"
+#include "NotImplemented.h"
 #include "Page.h"
 #include "PlatformKeyboardEvent.h"
 #include "PlatformWheelEvent.h"
 #include "RenderWidget.h"
 #include "Scrollbar.h"
-#include "NotImplemented.h"
 
 QT_BEGIN_NAMESPACE
 Q_GUI_EXPORT extern bool qt_tab_all_widgets; // from qapplication.cpp
@@ -97,7 +97,7 @@ bool EventHandler::passWidgetMouseDownEventToWidget(const MouseEventWithHitTestR
 
 bool EventHandler::eventActivatedView(const PlatformMouseEvent&) const
 {
-    //Qt has an activation event which is sent independently
+    // Qt has an activation event which is sent independently
     //   of mouse event so this thing will be a snafu to implement
     //   correctly
     return false;
