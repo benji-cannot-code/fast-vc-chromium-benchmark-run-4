3412e077347bb06de5cf879541d7cdab1d6c31c6
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-14 12:57:25 +0000

    2010-06-14  Jeremy Orlow  <jorlow@chromium.org>
            2 more build fixes.
    
            * storage/IDBDatabaseImpl.h:
            * storage/IndexedDatabaseRequest.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61116 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/IDBDatabaseImpl.h
+++ third_party/WebKit/WebCore/storage/IDBDatabaseImpl.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "IDBCallbacks.h"
 #include "IDBDatabase.h"
+#include "StringHash.h"
+#include <wtf/HashMap.h>
 
 #if ENABLE(INDEXED_DATABASE)
 
