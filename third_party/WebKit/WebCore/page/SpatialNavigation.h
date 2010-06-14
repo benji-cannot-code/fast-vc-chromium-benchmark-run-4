05811d8c1f9f1c01f1f1c26871523bd6b4e0856b
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-14 19:10:59 +0000

    Spatial Navigation: make it work with focusable elements in overflow content https://bugs.webkit.org/show_bug.cgi?id=36463
    Reviewed by Simon Fraser and Kenneth Christiansen.
    Patch by Antonio Gomes <tonikitoo@webkit.org>
    
    WebCore:
    
    This patch addresses the problem with Spatial Navigation. It currently does not
    properly traverse scrollable contents, including scrollable div's. For this to work,
    a new class member called scrollableEnclosingBox was introduced to FocusCandidate class which
    keeps track of the current scrollable box Node wrapping a FocusCandidate.
    
    To make use of enclosingScrollableBox of FocusCandidate, the DOM traversal routine
    (FocusController::findNextFocusableInDirection) was changed as follows: when it
    encounters a scrollable Node, each focusable node which is 'inner' keeps track of
    the container reference. By the time a sibling of the scrollable Node is encountered,
    there is no need to track this reference any more and the traversal algorithm continues
    normally.
    
    The common case is obviously that there is no scrollable container wrapping it.
    
    updateFocusCandiditeIfCloser logic was also adapted to fit the need of the
    newly introduced enclosingScrollableBox class member, getting simpler and more
    easily maintainable.
    
    Tests: fast/events/spatial-navigation/snav-div-scrollable-but-without-focusable-content.html
           fast/events/spatial-navigation/snav-clipped-overflow-content.html
    
    * page/FocusController.cpp:
    (WebCore::updateFocusCandidateInSameContainer):
    (WebCore::updateFocusCandidateIfCloser):
    (WebCore::FocusController::findFocusableNodeInDirection):
    (WebCore::FocusController::deepFindFocusableNodeInDirection):
    * page/SpatialNavigation.cpp:
    (WebCore::isScrollableContainerNode):
    * page/SpatialNavigation.h:
    (WebCore::FocusCandidate::FocusCandidate):
    (WebCore::FocusCandidate::inScrollableContainer):
    
    LayoutTests:
    
    * fast/events/spatial-navigation/snav-div-scrollable-but-without-focusable-content-expected.txt: Added.
    * fast/events/spatial-navigation/snav-div-scrollable-but-without-focusable-content.html: Added.
    * fast/events/spatial-navigation/snav-clipped-overflow-content-expected.txt: Added.
    * fast/events/spatial-navigation/snav-clipped-overflow-content.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61134 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/SpatialNavigation.h
+++ third_party/WebKit/WebCore/page/SpatialNavigation.h
@@ -98,6 +98,7 @@ enum RectsAlignment {
 struct FocusCandidate {
     FocusCandidate()
         : node(0)
+        , enclosingScrollableBox(0)
         , distance(maxDistance())
         , parentDistance(maxDistance())
         , alignment(None)
@@ -107,6 +108,7 @@ struct FocusCandidate {
 
     FocusCandidate(Node* n)
         : node(n)
+        , enclosingScrollableBox(0)
         , distance(maxDistance())
         , parentDistance(maxDistance())
         , alignment(None)
@@ -115,9 +117,11 @@ struct FocusCandidate {
     }
 
     bool isNull() const { return !node; }
+    bool inScrollableContainer() const { return node && enclosingScrollableBox; }
     Document* document() const { return node ? node->document() : 0; }
 
     Node* node;
+    Node* enclosingScrollableBox;
     long long distance;
     long long parentDistance;
     RectsAlignment alignment;
@@ -129,6 +133,7 @@ bool scrollInDirection(Frame*, FocusDirection);
 void scrollIntoView(Element*);
 bool hasOffscreenRect(Node*);
 bool isInRootDocument(Node*);
+bool isScrollableContainerNode(Node*);
 
 } // namspace WebCore
 
