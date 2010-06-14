5ec931a7d4f8e840853affaf074f18d4734aed1d
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-14 13:21:42 +0000

    2010-06-14  Jeremy Orlow  <jorlow@chromium.org>
            More build fixes.
    
            * storage/IndexedDatabaseRequest.h:
    2010-06-14  Jeremy Orlow  <jorlow@chromium.org>
    
            Build fix.
    
            * src/IDBCallbacksProxy.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61118 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/IDBCallbacksProxy.h
+++ third_party/WebKit/WebKit/chromium/src/IDBCallbacksProxy.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDBCallbacksProxy_h
 
 #include "IDBCallbacks.h"
+#include <wtf/PassOwnPtr.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefPtr.h>
 
