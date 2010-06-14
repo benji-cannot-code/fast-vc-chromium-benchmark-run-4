54374b7d4c023e7237c6cbdb9640b7e58b41ff2f
davidben@chromium.org <davidben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-14 18:24:53 +0000

    Implement KDE ReversedException setting in Chrome
    R=wtc,eroman
    BUG=45199
    TEST=Added unit tests, can also verify with steps in bug report
    
    Review URL: http://codereview.chromium.org/2725009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49701 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_config_service_linux_unittest.cc
+++ net/proxy/proxy_config_service_linux_unittest.cc
@@ -214,6 +214,10 @@ class MockGConfSettingGetter
     return !result->empty();
   }
 
+  virtual bool BypassListIsReversed() {
+    return false;
+  }
+
   // Intentionally public, for convenience when setting up a test.
   GConfValues values;
 
@@ -1078,11 +1082,11 @@ TEST_F(ProxyConfigServiceLinuxTest, KDEConfigParser) {
 
       false,                                   // auto_detect
       GURL(),                                  // pac_url
-      ProxyRulesExpectation::PerScheme(
+      ProxyRulesExpectation::PerSchemeWithBypassReversed(
           "www.google.com:80",  // http
           "",                   // https
           "",                   // ftp
-          ""),                  // bypass rules
+          "*.google.com"),      // bypass rules
     },
 
     {
@@ -1095,11 +1099,11 @@ TEST_F(ProxyConfigServiceLinuxTest, KDEConfigParser) {
 
       false,                                   // auto_detect
       GURL(),                                  // pac_url
-      ProxyRulesExpectation::PerScheme(
+      ProxyRulesExpectation::PerSchemeWithBypassReversed(
           "www.google.com:80",  // http
           "",                   // https
           "",                   // ftp
-          ""),                  // bypass rules
+          "*.google.com"),      // bypass rules
     },
 
     {
