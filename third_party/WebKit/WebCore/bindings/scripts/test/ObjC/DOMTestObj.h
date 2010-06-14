dd7a62b21153f604aced443712a564e2059c5c7f
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-14 12:09:06 +0000

    2010-06-14  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Darin Adler.
    
            Reenable IDBDatabaseRequest's description attribute
            https://bugs.webkit.org/show_bug.cgi?id=39826
    
            Existing test now fixed.
    
            * storage/indexeddb/idb-database-request-expected.txt:
    2010-06-14  Jeremy Orlow  <jorlow@chromium.org>
    
            Reviewed by Darin Adler.
    
            Reenable IDBDatabaseRequest's description attribute
            https://bugs.webkit.org/show_bug.cgi?id=39826
    
            On the 26th, Beth commented out the description attribute in
            IDBDatabaseRequest because it was causing problems with the
            objective c bindings.  Turns out that this is the same bug as
            in http://trac.webkit.org/changeset/19486.  (Thanks for the
            pointer, Mark!)
    
            The solution is to change the objective C bindings generator
            to change the name in the generated code.  id and hash both
            just appended "Name" to them in the past.  I couldn't think
            of anything else particularly better to append, so I just
            went with name as well.
    
            Test: building the world works on the first try.  Layout
                  test verifies description works again.
    
            * bindings/scripts/CodeGeneratorObjC.pm:
            * bindings/scripts/test/GObject/WebKitDOMTestObj.cpp:
            (webkit_dom_test_obj_get_description):
            (webkit_dom_test_obj_get_id):
            (webkit_dom_test_obj_set_id):
            (webkit_dom_test_obj_get_hash):
            (webkit_dom_test_obj_set_property):
            (webkit_dom_test_obj_get_property):
            (webkit_dom_test_obj_class_init):
            * bindings/scripts/test/GObject/WebKitDOMTestObj.h:
            * bindings/scripts/test/JS/JSTestObj.cpp:
            (WebCore::):
            (WebCore::jsTestObjDescription):
            (WebCore::jsTestObjId):
            (WebCore::jsTestObjHash):
            (WebCore::setJSTestObjId):
            * bindings/scripts/test/JS/JSTestObj.h:
            * bindings/scripts/test/ObjC/DOMTestObj.h:
            * bindings/scripts/test/ObjC/DOMTestObj.mm:
            (-[DOMTestObj descriptionName]):
            (-[DOMTestObj idName]):
            (-[DOMTestObj setIdName:]):
            (-[DOMTestObj hashName]):
            * bindings/scripts/test/TestObj.idl:
            * bindings/scripts/test/V8/V8TestObj.cpp:
            (WebCore::TestObjInternal::descriptionAttrGetter):
            (WebCore::TestObjInternal::idAttrGetter):
            (WebCore::TestObjInternal::idAttrSetter):
            (WebCore::TestObjInternal::hashAttrGetter):
            (WebCore::):
            * storage/IDBDatabaseRequest.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61112 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/test/ObjC/DOMTestObj.h
+++ third_party/WebKit/WebCore/bindings/scripts/test/ObjC/DOMTestObj.h
@@ -57,6 +57,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (int)customAttr;
 - (void)setCustomAttr:(int)newCustomAttr;
 - (NSString *)scriptStringAttr;
+- (int)descriptionName;
+- (int)idName;
+- (void)setIdName:(int)newIdName;
+- (NSString *)hashName;
 - (void)voidMethod;
 - (void)voidMethodWithArgs:(int)intArg strArg:(NSString *)strArg objArg:(DOMTestObj *)objArg;
 - (int)intMethod;
--- third_party/WebKit/WebCore/bindings/scripts/test/ObjC/DOMTestObj.mm
+++ third_party/WebKit/WebCore/bindings/scripts/test/ObjC/DOMTestObj.mm
@@ -213,6 +213,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return IMPL->scriptStringAttr();
 }
 
+- (int)descriptionName
+{
+    WebCore::JSMainThreadNullState state;
+    return IMPL->description();
+}
+
+- (int)idName
+{
+    WebCore::JSMainThreadNullState state;
+    return IMPL->id();
+}
+
+- (void)setIdName:(int)newIdName
+{
+    WebCore::JSMainThreadNullState state;
+    IMPL->setId(newIdName);
+}
+
+- (NSString *)hashName
+{
+    WebCore::JSMainThreadNullState state;
+    return IMPL->hash();
+}
+
 - (void)voidMethod
 {
     WebCore::JSMainThreadNullState state;
