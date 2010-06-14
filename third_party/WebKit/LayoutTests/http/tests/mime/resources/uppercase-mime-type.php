1e0585402b4440fc64dc934dba4960e3bb068475
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-14 06:32:02 +0000

    2010-06-13  Charles Wei  <charles.wei@torchmobile.com.cn>
            Reviewed by George Staikos.
    
            Test case to verify that MIME type in uppercase (TEXT/HTML) is supported.
            https://bugs.webkit.org/show_bug.cgi?id=39492
    
            * http/tests/mime/resources/uppercase-mime-type.php: Added.
            * http/tests/mime/uppercase-mime-type-expected.txt: Added.
            * http/tests/mime/uppercase-mime-type.html: Added.
    2010-06-13  Charles Wei  <charles.wei@torchmobile.com.cn>
    
            Reviewed by George Staikos.
    
            Fix the QtWebKit which doesn't recognize the MIME type of HTML/TEXT in uppercase
            https://bugs.webkit.org/show_bug.cgi?id=39492
    
            * WebCoreSupport/FrameLoaderClientQt.cpp:
            (WebCore::FrameLoaderClientQt::canShowMIMEType):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/mime/resources/uppercase-mime-type.php
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("Content-Type: TEXT/HTML");
+?>
+<html>
+<script>
+if (window.layoutTestController)
+   layoutTestController.notifyDone();
+
+</script>
+<body>
+If this text is shown, that means the new document was successfully loaded.
+</body>
+</html>
