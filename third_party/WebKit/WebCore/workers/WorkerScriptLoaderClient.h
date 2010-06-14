8d9f27ebecb3432c9e620b1c5276c72e4211c979
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-14 12:48:52 +0000

    2010-06-14  Jeremy Orlow  <jorlow@chromium.org>
            Unreviewed build fix for my last.
    
            * workers/WorkerScriptLoaderClient.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61115 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/workers/WorkerScriptLoaderClient.h
+++ third_party/WebKit/WebCore/workers/WorkerScriptLoaderClient.h
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+    class ResourceResponse;
+
     class WorkerScriptLoaderClient {
     public:
         virtual void didReceiveResponse(const ResourceResponse&) { }
