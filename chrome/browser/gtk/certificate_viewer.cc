32f54e0b3c234ca3821d239554698a3595c3cd32
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-15 20:42:20 +0000

    gtk: Certificate viewer should show both A-label and U-label for CN and SubjectAltName IDNs
    Remove extraneous '\n' from ProcessGeneralNames.
    
    BUG=43966
    TEST=manual
    
    Review URL: http://codereview.chromium.org/2733014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49831 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/certificate_viewer.cc
+++ chrome/browser/gtk/certificate_viewer.cc
@@ -285,7 +285,7 @@ void CertificateViewer::InitGeneralPage() {
            l10n_util::GetStringUTF8(IDS_CERT_INFO_SUBJECT_GROUP));
   AddKeyValue(table, row++,
               l10n_util::GetStringUTF8(IDS_CERT_INFO_COMMON_NAME_LABEL),
-              Stringize(CERT_GetCommonName(&cert->subject)));
+              psm::ProcessIDN(Stringize(CERT_GetCommonName(&cert->subject))));
   AddKeyValue(table, row++,
               l10n_util::GetStringUTF8(IDS_CERT_INFO_ORGANIZATION_LABEL),
               Stringize(CERT_GetOrgName(&cert->subject)));
@@ -302,7 +302,7 @@ void CertificateViewer::InitGeneralPage() {
            l10n_util::GetStringUTF8(IDS_CERT_INFO_ISSUER_GROUP));
   AddKeyValue(table, row++,
               l10n_util::GetStringUTF8(IDS_CERT_INFO_COMMON_NAME_LABEL),
-              Stringize(CERT_GetCommonName(&cert->issuer)));
+              psm::ProcessIDN(Stringize(CERT_GetCommonName(&cert->issuer))));
   AddKeyValue(table, row++,
               l10n_util::GetStringUTF8(IDS_CERT_INFO_ORGANIZATION_LABEL),
               Stringize(CERT_GetOrgName(&cert->issuer)));
--- chrome/third_party/mozilla_security_manager/nsNSSCertHelper.cpp
+++ chrome/third_party/mozilla_security_manager/nsNSSCertHelper.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <keyhi.h>
 #include <prprf.h>
+#include <unicode/uidna.h>
 
 #include "app/l10n_util.h"
 #include "base/i18n/number_formatting.h"
@@ -165,6 +166,43 @@ std::string ProcessRawBits(SECItem* data) {
   bytedata.data = data->data;
   bytedata.len = data->len / 8;
   return ProcessRawBytes(&bytedata);
+}
+
+std::string ProcessIDN(const std::string& input) {
+  // Convert the ASCII input to a string16 for ICU.
+  string16 input16;
+  input16.reserve(input.length());
+  std::copy(input.begin(), input.end(), std::back_inserter(input16));
+
+  string16 output16;
+  output16.resize(input.length());
+
+  UErrorCode status = U_ZERO_ERROR;
+  int output_chars = uidna_IDNToUnicode(input16.data(), input.length(),
+                                        &output16[0], output16.length(),
+                                        UIDNA_DEFAULT, NULL, &status);
+  if (status == U_ZERO_ERROR) {
+    output16.resize(output_chars);
+  } else if (status != U_BUFFER_OVERFLOW_ERROR) {
+    return input;
+  } else {
+    output16.resize(output_chars);
+    output_chars = uidna_IDNToUnicode(input16.data(), input.length(),
+                                      &output16[0], output16.length(),
+                                      UIDNA_DEFAULT, NULL, &status);
+    if (status != U_ZERO_ERROR)
+      return input;
+    DCHECK_EQ(static_cast<size_t>(output_chars), output16.length());
+    output16.resize(output_chars);  // Just to be safe.
+  }
+
+  if (input16 == output16)
+    return input;  // Input did not contain any encoded data.
+
+  // Input contained encoded data, return formatted string showing original and
+  // decoded forms.
+  return l10n_util::GetStringFUTF8(IDS_CERT_INFO_IDN_VALUE_FORMAT,
+                                   input16, output16);
 }
 
 std::string DumpOidString(SECItem* oid) {
@@ -384,7 +422,6 @@ std::string GetOIDText(SECItem* oid) {
   return DumpOidString(oid);
 }
 
-
 // Get a display string from a Relative Distinguished Name.
 std::string ProcessRDN(CERTRDN* rdn) {
   std::string rv;
@@ -398,6 +435,8 @@ std::string ProcessRDN(CERTRDN* rdn) {
       rv += " = ";
       std::string value(reinterpret_cast<char*>(decode_item->data),
                         decode_item->len);
+      if (SECOID_FindOIDTag(&avas[i]->type) == SEC_OID_AVA_COMMON_NAME)
+        value = ProcessIDN(value);
       rv += value;
       SECITEM_FreeItem(decode_item, PR_TRUE);
     }
@@ -502,6 +541,7 @@ std::string ProcessGeneralName(PRArenaPool* arena,
       key = l10n_util::GetStringUTF8(IDS_CERT_GENERAL_NAME_DNS_NAME);
       value = std::string(reinterpret_cast<char*>(current->name.other.data),
                           current->name.other.len);
+      value = ProcessIDN(value);
       break;
     case certX400Address:
       key = l10n_util::GetStringUTF8(IDS_CERT_GENERAL_NAME_X400_ADDRESS);
@@ -569,7 +609,7 @@ std::string ProcessGeneralNames(PRArenaPool* arena,
     std::string text = ProcessGeneralName(arena, current);
     if (text.empty())
       break;
-    rv += text + '\n';
+    rv += text;
     current = CERT_GetNextGeneralName(current);
   } while (current != name_list);
   return rv;
