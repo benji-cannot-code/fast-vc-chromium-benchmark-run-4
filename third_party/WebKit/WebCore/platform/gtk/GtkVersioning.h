0952e8598f26ff02d620939c14e8b308ba10f983
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-15 19:32:38 +0000

    2010-06-15  Xan Lopez  <xlopez@igalia.com>
            Try to fix GTK+ build.
    
            * platform/gtk/GtkVersioning.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61208 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/GtkVersioning.h
+++ third_party/WebKit/WebCore/platform/gtk/GtkVersioning.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif // GTK_CHECK_VERSION(2, 21, 2)
 
 #if !GTK_CHECK_VERSION(2, 20, 0)
-#define gtk_widget_set_realized(widget, FALSE) GTK_WIDGET_UNSET_FLAGS((widget), GTK_REALIZED)
 #define gtk_widget_set_realized(widget, TRUE) GTK_WIDGET_SET_FLAGS((widget), GTK_REALIZED)
 #endif // GTK_CHECK_VERSION(2, 20, 0)
 
@@ -45,9 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !GTK_CHECK_VERSION(2, 18, 0)
 #define gtk_widget_set_visible(widget, FALSE) GTK_WIDGET_UNSET_FLAGS((widget), GTK_VISIBLE)
-#define gtk_widget_set_visible(widget, TRUE) GTK_WIDGET_SET_FLAGS((widget), GTK_VISIBLE)
 #define gtk_widget_set_window(widget, new_window) (widget)->window = (new_window)
-#define gtk_widget_set_can_focus(widget, FALSE) GTK_WIDGET_UNSET_FLAGS((widget), GTK_CAN_FOCUS)
 #define gtk_widget_set_can_focus(widget, TRUE) GTK_WIDGET_SET_FLAGS((widget), GTK_CAN_FOCUS)
 #endif // GTK_CHECK_VERSION(2, 18, 0)
 
