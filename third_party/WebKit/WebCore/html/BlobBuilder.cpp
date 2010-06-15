d974ecec419f4309fe92655e5ba2110c6f744ec0
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-15 23:36:07 +0000

    2010-06-15  Kinuko Yasuda  <kinuko@chromium.org>
            Reviewed by David Levin.
    
            Fix compilation errors in BlobBuilder with FILE_WRITER enabled
            https://bugs.webkit.org/show_bug.cgi?id=40606
    
            No functionality change so no new tests.
    
            * html/BlobBuilder.cpp:
            * html/BlobBuilder.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61225 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/BlobBuilder.cpp
+++ third_party/WebKit/WebCore/html/BlobBuilder.cpp
@@ -37,8 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AtomicString.h"
 #include "Blob.h"
+#include "ExceptionCode.h"
 #include "TextEncoding.h"
-#include <wtf/text/CString.h>
 
 namespace WebCore {
 
