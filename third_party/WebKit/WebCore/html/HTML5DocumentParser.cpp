30b7a08652f646e8d740962a679a3d028f5f14b6
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-15 00:07:14 +0000

    2010-06-14  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Add a preload scanner for the HTML5 parser
            https://bugs.webkit.org/show_bug.cgi?id=40557
    
            This patch adds a simple preload scanner for the HTML5 parser.  This
            preload scanner is not as awesome as the old one because it doesn't
            scan CSS, but it's much simpler.
    
            * Android.mk:
            * CMakeLists.txt:
            * GNUmakefile.am:
            * WebCore.gypi:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
                - Build file torture.
            * html/HTML5DocumentParser.cpp:
            (WebCore::HTML5DocumentParser::write):
                - Call into the preload scanner when waiting for a script.
            * html/HTML5DocumentParser.h:
            * html/HTML5PreloadScanner.cpp: Copied from WebCore/html/PreloadScanner.cpp.
            (WebCore::HTML5PreloadScanner::HTML5PreloadScanner):
            (WebCore::HTML5PreloadScanner::scan):
                - A simple loop to pump the preload scanner's lexer.
            (WebCore::HTML5PreloadScanner::processToken):
                - Preload interesting resources.  Stolen from the old preload
                  scanner.
            (WebCore::HTML5PreloadScanner::scanningBody):
            * html/HTML5PreloadScanner.h: Copied from WebCore/html/PreloadScanner.h.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTML5DocumentParser.cpp
+++ third_party/WebKit/WebCore/html/HTML5DocumentParser.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Element.h"
 #include "Frame.h"
 #include "HTML5Lexer.h"
+#include "HTML5PreloadScanner.h"
 #include "HTML5ScriptRunner.h"
 #include "HTML5TreeBuilder.h"
 #include "HTMLDocument.h"
@@ -124,7 +125,16 @@ void HTML5DocumentParser::pumpLexer()
         bool shouldContinueParsing = m_scriptRunner->execute(scriptElement.release(), scriptStartLine);
         m_treeConstructor->setPaused(!shouldContinueParsing);
         if (!shouldContinueParsing)
-            return;
+            break;
+    }
+
+    if (isWaitingForScripts()) {
+        ASSERT(m_lexer->state() == HTML5Lexer::DataState);
+        if (!m_preloadScanner) {
+            m_preloadScanner.set(new HTML5PreloadScanner(m_document));
+            m_preloadScanner->appendToEnd(m_input.current());
+        }
+        m_preloadScanner->scan();
     }
 
     didPumpLexer();
@@ -158,6 +168,9 @@ void HTML5DocumentParser::write(const SegmentedString& source, bool appendData)
 
     if (appendData) {
         m_input.appendToEnd(source);
+        if (m_preloadScanner)
+            m_preloadScanner->appendToEnd(source);
+
         if (m_writeNestingLevel > 1) {
             // We've gotten data off the network in a nested call to write().
             // We don't want to consume any more of the input stream now.  Do
@@ -236,6 +249,7 @@ void HTML5DocumentParser::resumeParsingAfterScriptExecution()
 {
     ASSERT(!m_scriptRunner->inScriptExecution());
     ASSERT(!m_treeConstructor->isPaused());
+
     pumpLexerIfPossible();
 
     // The document already finished parsing we were just waiting on scripts when finished() was called.
--- third_party/WebKit/WebCore/html/HTML5DocumentParser.h
+++ third_party/WebKit/WebCore/html/HTML5DocumentParser.h
@@ -41,6 +41,7 @@ class LegacyHTMLTreeConstructor;
 class HTML5Lexer;
 class HTML5ScriptRunner;
 class HTML5TreeBuilder;
+class HTML5PreloadScanner;
 class ScriptController;
 class ScriptSourceCode;
 
@@ -184,6 +185,7 @@ private:
     OwnPtr<HTML5Lexer> m_lexer;
     OwnPtr<HTML5ScriptRunner> m_scriptRunner;
     OwnPtr<HTML5TreeBuilder> m_treeConstructor;
+    OwnPtr<HTML5PreloadScanner> m_preloadScanner;
     bool m_endWasDelayed;
     int m_writeNestingLevel;
 };
