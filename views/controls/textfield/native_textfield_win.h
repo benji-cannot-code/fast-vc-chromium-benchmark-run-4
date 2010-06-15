fbbf194e51bb5375f3052fc70ed2ead92f84f9d7
xji@chromium.org <xji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-15 18:08:13 +0000

    Unicode bidi mirroring characters are not correctly mirrored in textfield and omnibox (which use CRichEditCtrl) while *inputting*.
    Since bidi mirroring characters are correctly mirrored when rendering the whole text, fix it by re-rendering the whole text every time the text changes.
    
    NOTE: this change will mess up the undo queue. The continuous keystroke wont be treated as one undo event.
    Every single keystroke is treated as a undo state.
    
    Using the following as example:
    1. paste string "abcd " into find-in-page;
    2. type in 'e', 'f', and ' '.
    3. paste string "xyz".
    
    When undo, the string sequence will be:
    "abcd ef xyz" => "abcd ef " ==> "abcd ef" ==> "abcd e" ==> "abcd " =>"".
    
    Without the change, the string sequence is:
    "abcd ef xyz" => "abcd ef " => "abcd " =>"".
    
    
    
    BUG=46298
    TEST=follow the steps described in the bug.
    Review URL: http://codereview.chromium.org/2741007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49811 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/textfield/native_textfield_win.h
+++ views/controls/textfield/native_textfield_win.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -119,6 +119,21 @@ class NativeTextfieldWin
     DISALLOW_COPY_AND_ASSIGN(ScopedFreeze);
   };
 
+  // This object suspends placing any operations on the edit's undo stack until
+  // the object is destroyed.  If we don't do this, some of the operations we
+  // perform behind the user's back will be undoable by the user, which feels
+  // bizarre and confusing.
+  class ScopedSuspendUndo {
+   public:
+    explicit ScopedSuspendUndo(ITextDocument* text_object_model);
+    ~ScopedSuspendUndo();
+
+   private:
+    ITextDocument* const text_object_model_;
+
+    DISALLOW_COPY_AND_ASSIGN(ScopedSuspendUndo);
+  };
+
   // message handlers
   void OnChar(TCHAR key, UINT repeat_count, UINT flags);
   void OnContextMenu(HWND window, const POINT& point);
@@ -150,7 +165,17 @@ class NativeTextfieldWin
   // before and after the change.  These functions determine if anything
   // meaningful changed, and do any necessary updating and notification.
   void OnBeforePossibleChange();
-  void OnAfterPossibleChange();
+
+  // When a user types a BIDI mirroring character (e.g. left parenthesis
+  // U+0028, which should be rendered as '(' in LTR context unless  surrounded
+  // by RTL characters in both sides, and it should be rendered as ')' in RTL
+  // context unless surrounded by LTR characters in both sides.), the textfield
+  // does not properly mirror the character when necessary. However, if we
+  // explicitly set the text in the edit to the entire current string, then
+  // the BIDI mirroring characters will be mirrored properly. When
+  // |should_redraw_text| is true, we explicitly set the text in the edit to
+  // the entire current string any time the text changes.
+  void OnAfterPossibleChange(bool should_redraw_text);
 
   // Given an X coordinate in client coordinates, returns that coordinate
   // clipped to be within the horizontal bounds of the visible text.
