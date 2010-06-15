83dce10e5ee1493255f2299893979d20fc002cdb
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-15 01:37:23 +0000

    Fix Tiger build.
    * plugins/mac/PluginDataMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/mac/PluginDataMac.mm
+++ third_party/WebKit/WebCore/plugins/mac/PluginDataMac.mm
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "Logging.h"
 #import "WebCoreViewFactory.h"
 
+#ifdef BUILDING_ON_TIGER
+typedef unsigned NSUInteger;
+#endif
+
 namespace WebCore {
 
 void PluginData::initPlugins()
