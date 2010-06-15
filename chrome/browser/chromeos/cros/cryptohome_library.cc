3bb74594d8734127896d955abdbabf67f7414a1f
cmasone@google.com <cmasone@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-15 16:09:27 +0000

    Use GetSystemSalt from the cryptohome lib instead of reading salt off disk.
    We used to read the system salt from a file on disk, which meant that
    we had to do a bunch of dancing to do some work during login on the
    FILE thread instead of the UI thread.  Now, we no longer have to,
    because there is an API to request the system's salt value from
    cryptohomed over dbus.  This CL is a precursor to some of the other
    login/cryptohome changes that are coming.
    
    BUG=chromium-os:323
    TEST=unit tests, and loading up a chromium os device and logging in
    
    Review URL: http://codereview.chromium.org/2820006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49799 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/cryptohome_library.cc
+++ chrome/browser/chromeos/cros/cryptohome_library.cc
@@ -24,4 +24,8 @@ bool CryptohomeLibraryImpl::IsMounted() {
   return chromeos::CryptohomeIsMounted();
 }
 
+CryptohomeBlob CryptohomeLibraryImpl::GetSystemSalt() {
+  return chromeos::CryptohomeGetSystemSalt();
+}
+
 }  // namespace chromeos
