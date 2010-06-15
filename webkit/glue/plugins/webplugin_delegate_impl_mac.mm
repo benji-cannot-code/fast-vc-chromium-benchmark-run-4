e0713fa04d693f9fbc6c9d1fdadcf74c81a8e3b8
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-15 16:16:20 +0000

    Make timer-driven CA plugins less wasteful, and up the timer frequency
    Don't do cross-process IOSurface work if nothing has changed in the layer. Now that it's less expensive, up the frequency of the timer a bit to minimize the chance of dropped frames due to drift if the plugin is updating at a different speed.
    
    BUG=None
    TEST=Core Animation plugins on the Mac still work.
    
    Review URL: http://codereview.chromium.org/2847003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49800 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/webplugin_delegate_impl_mac.mm
+++ webkit/glue/plugins/webplugin_delegate_impl_mac.mm
@@ -45,7 +45,7 @@ using WebKit::WebInputEvent;
 using WebKit::WebMouseEvent;
 using WebKit::WebMouseWheelEvent;
 
-const int kCoreAnimationRedrawPeriodMs = 20;  // 50fps
+const int kCoreAnimationRedrawPeriodMs = 10;  // 100 Hz
 
 // Important implementation notes: The Mac definition of NPAPI, particularly
 // the distinction between windowed and windowless modes, differs from the
@@ -996,6 +996,11 @@ void WebPluginDelegateImpl::DrawLayerInSurface() {
   surface_->Clear(window_rect_);
 
   [renderer_ beginFrameAtTime:CACurrentMediaTime() timeStamp:NULL];
+  if (CGRectIsEmpty([renderer_ updateBounds])) {
+    // If nothing has changed, we are done.
+    [renderer_ endFrame];
+    return;
+  }
   CGRect layerRect = [layer_ bounds];
   [renderer_ addUpdateRect:layerRect];
   [renderer_ render];
