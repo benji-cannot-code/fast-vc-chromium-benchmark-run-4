9f79baa255c3cb24424ffdfcb0ae9265446ff082
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-16 17:54:41 +0000

    Revert 49967 - Client side changes to set and get nigori specifics.
    BUG=none
    TEST=after patching in 2844004, I was able to successfully modify the Nigori permanent node.
    
    Review URL: http://codereview.chromium.org/2815009
    
    TBR=albertb@chromium.org
    Review URL: http://codereview.chromium.org/2860007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49977 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/syncapi.cc
+++ chrome/browser/sync/engine/syncapi.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/protocol/autofill_specifics.pb.h"
 #include "chrome/browser/sync/protocol/bookmark_specifics.pb.h"
 #include "chrome/browser/sync/protocol/extension_specifics.pb.h"
-#include "chrome/browser/sync/protocol/nigori_specifics.pb.h"
 #include "chrome/browser/sync/protocol/password_specifics.pb.h"
 #include "chrome/browser/sync/protocol/preference_specifics.pb.h"
 #include "chrome/browser/sync/protocol/service_constants.h"
@@ -244,11 +243,6 @@ const sync_pb::BookmarkSpecifics& BaseNode::GetBookmarkSpecifics() const {
   return GetEntry()->Get(SPECIFICS).GetExtension(sync_pb::bookmark);
 }
 
-const sync_pb::NigoriSpecifics& BaseNode::GetNigoriSpecifics() const {
-  DCHECK(GetModelType() == syncable::NIGORI);
-  return GetEntry()->Get(SPECIFICS).GetExtension(sync_pb::nigori);
-}
-
 bool BaseNode::GetPasswordSpecifics(sync_pb::PasswordSpecificsData* data)
     const {
   DCHECK(GetModelType() == syncable::PASSWORDS);
@@ -334,19 +328,6 @@ void WriteNode::PutBookmarkSpecificsAndMarkForSyncing(
     const sync_pb::BookmarkSpecifics& new_value) {
   sync_pb::EntitySpecifics entity_specifics;
   entity_specifics.MutableExtension(sync_pb::bookmark)->CopyFrom(new_value);
-  PutSpecificsAndMarkForSyncing(entity_specifics);
-}
-
-void WriteNode::SetNigoriSpecifics(
-    const sync_pb::NigoriSpecifics& new_value) {
-  DCHECK(GetModelType() == syncable::NIGORI);
-  PutNigoriSpecificsAndMarkForSyncing(new_value);
-}
-
-void WriteNode::PutNigoriSpecificsAndMarkForSyncing(
-    const sync_pb::NigoriSpecifics& new_value) {
-  sync_pb::EntitySpecifics entity_specifics;
-  entity_specifics.MutableExtension(sync_pb::nigori)->CopyFrom(new_value);
   PutSpecificsAndMarkForSyncing(entity_specifics);
 }
 
