5af5ce3ecfae24a5722dfcc9fdd67c76c776da1c
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-16 14:19:46 +0000

    Peek at the event queue for wheel events for cros BUG=NONE Test=RenderWidgetHostTest.CoalescesWheelEvents (existing)
    Review URL: http://codereview.chromium.org/2679003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49938 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -266,7 +266,10 @@ extern const char kChromeosFrame[];
 extern const char kStartupManifest[];
 // TODO(denisromanov): Remove this flag when it is not needed for testing, too.
 extern const char kServicesManifest[];
-extern const char kVertScrollDelta[];
+#endif
+
+#if defined(OS_LINUX)
+extern const char kScrollPixels[];
 #endif
 
 #if defined(OS_MACOSX) || defined(OS_WIN)
