a0995d20b4a2156442e46f0d608bacbb69feab8d
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-16 15:09:22 +0000

    [Qt] Fix tiled backing store painting when calling QGraphicsWebView::setPage() after setResizesToContents(true)
    Patch by Alexis Menard <alexis.menard@nokia.com> on 2010-06-16
    Reviewed by Simon Hausmann.
    
    When calling setPage() after setResizesToContents, the value of the property wasn't propagated
    to the page client. Therefore the visible rect for the tiled backing store was incorrect.
    
    Simply move the variable assignment into updateResizesToContentsForPage(), which is always called.
    
    * Api/qgraphicswebview.cpp:
    (QGraphicsWebViewPrivate::updateResizesToContentsForPage):
    (QGraphicsWebView::setResizesToContents):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61254 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
+++ third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
@@ -123,7 +123,7 @@ void QGraphicsWebViewPrivate::_q_pageDestroyed()
 void QGraphicsWebViewPrivate::updateResizesToContentsForPage()
 {
     ASSERT(page);
-
+    static_cast<PageClientQGraphicsWidget*>(page->d->client)->viewResizesToContents = resizesToContents;
     if (resizesToContents) {
         // resizes to contents mode requires preferred contents size to be set
         if (!page->preferredContentsSize().isValid())
@@ -822,10 +822,8 @@ void QGraphicsWebView::setResizesToContents(bool enabled)
     if (d->resizesToContents == enabled)
         return;
     d->resizesToContents = enabled;
-    if (d->page) {
-        static_cast<PageClientQGraphicsWidget*>(d->page->d->client)->viewResizesToContents = enabled;
+    if (d->page)
         d->updateResizesToContentsForPage();
-    }
 }
 
 bool QGraphicsWebView::resizesToContents() const
