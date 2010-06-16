2e352178ec51bc742a10d2cc5613b04763bc3623
glotov@chromium.org <glotov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-16 17:26:54 +0000

    Volume bubble added for ChromeOS.
    BUG=crosbug.com/525
    TEST=none
    
    Review URL: http://codereview.chromium.org/2790012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49970 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system_key_event_listener.cc
+++ chrome/browser/chromeos/system_key_event_listener.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/system_key_event_listener.h"
 
 #include "chrome/browser/chromeos/audio_handler.h"
+#include "chrome/browser/chromeos/volume_bubble.h"
 #include "third_party/cros/chromeos_wm_ipc_enums.h"
 
 namespace chromeos {
@@ -18,8 +19,7 @@ const double kStepPercentage = 4.0;
 
 // static
 SystemKeyEventListener* SystemKeyEventListener::instance() {
-  SystemKeyEventListener* instance = Singleton<SystemKeyEventListener>::get();
-  return instance;
+  return Singleton<SystemKeyEventListener>::get();
 }
 
 SystemKeyEventListener::SystemKeyEventListener()
@@ -45,14 +45,19 @@ void SystemKeyEventListener::ProcessWmMessage(const WmIpc::Message& message,
       // long as mute key was held.
       // Refer to http://crosbug.com/3754 and http://crosbug.com/3751
       audio_handler_->SetMute(true);
+      VolumeBubble::instance()->ShowVolumeBubble(0);
       break;
     case WM_IPC_SYSTEM_KEY_VOLUME_DOWN:
       audio_handler_->AdjustVolumeByPercent(-kStepPercentage);
       audio_handler_->SetMute(false);
+      VolumeBubble::instance()->ShowVolumeBubble(
+          audio_handler_->GetVolumePercent());
       break;
     case WM_IPC_SYSTEM_KEY_VOLUME_UP:
       audio_handler_->AdjustVolumeByPercent(kStepPercentage);
       audio_handler_->SetMute(false);
+      VolumeBubble::instance()->ShowVolumeBubble(
+          audio_handler_->GetVolumePercent());
       break;
     default:
       DLOG(ERROR) << "SystemKeyEventListener: Unexpected message "
@@ -62,4 +67,3 @@ void SystemKeyEventListener::ProcessWmMessage(const WmIpc::Message& message,
 }
 
 }  // namespace chromeos
-
