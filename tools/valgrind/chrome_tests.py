e5654dd5aa620165495f502ba4a22332541886f3
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-16 10:23:28 +0000

    Add courgette_unittests to the list of tests runnable under Valgrind and Heapchecker.
    TBR=timurrrr
    
    Review URL: http://codereview.chromium.org/2846008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49919 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/chrome_tests.py
+++ tools/valgrind/chrome_tests.py
@@ -230,6 +230,9 @@ class ChromeTests:
   def TestGURL(self):
     return self.SimpleTest("chrome", "googleurl_unittests")
 
+  def TestCourgette(self):
+    return self.SimpleTest("courgette", "courgette_unittests")
+
   def TestMedia(self):
     return self.SimpleTest("chrome", "media_unittests")
 
@@ -387,6 +390,7 @@ class ChromeTests:
     "base": TestBase,            "base_unittests": TestBase,
     "browser": TestBrowser,      "browser_tests": TestBrowser,
     "googleurl": TestGURL,       "googleurl_unittests": TestGURL,
+    "courgette": TestCourgette,  "courgette_unittests": TestCourgette,
     "ipc": TestIpc,              "ipc_tests": TestIpc,
     "layout": TestLayout,        "layout_tests": TestLayout,
     "media": TestMedia,          "media_unittests": TestMedia,
