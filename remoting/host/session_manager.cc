47e7daf2e51a4297514e60efb238fa3da4fbe84d
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-16 21:31:08 +0000

    Fix double deletion in SessionManager
    SessionManaerTest::OneRecordCycle is broken due to double deletion.
    
    TEST=remoting_unittests passes
    TBR=dmaclach since this is <= 5 lines.
    
    Review URL: http://codereview.chromium.org/2850008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50033 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/session_manager.cc
+++ remoting/host/session_manager.cc
@@ -223,7 +223,6 @@ void SessionManager::DoSendUpdate(const UpdateStreamPacketHeader* header,
       clients_[i]->SendEndUpdateStreamMessage();
     }
   }
-  delete header;
 }
 
 void SessionManager::DoSendInit(scoped_refptr<ClientConnection> client,
