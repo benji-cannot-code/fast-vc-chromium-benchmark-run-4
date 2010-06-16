7948dc34eb07e5ce05c0c8939788895db3a31262
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-16 17:02:11 +0000

    [Mac] Fix a leak in CookiesWindowControllerTest by giving BrowserTestHelper a fake IO thread.
    Thanks to Randy Smith for tracking this down.
    
    BUG=31586
    TEST=Valgrind Mac unit_tests stay green.
    
    Review URL: http://codereview.chromium.org/2798001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49968 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_test_helper.h
+++ chrome/browser/cocoa/browser_test_helper.h
@@ -14,6 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Base class which contains a valid Browser*.  Lots of boilerplate to
 // recycle between unit test classes.
 //
+// This class creates fake UI, file, and IO threads because many objects that
+// are attached to the TestingProfile (and other objects) have traits that limit
+// their destruction to certain threads. For example, the URLRequestContext can
+// only be deleted on the IO thread; without this fake IO thread, the object
+// would never be deleted and would report as a leak under Valgrind. Note that
+// these are fake threads and they all share the same MessageLoop.
+//
 // TODO(jrg): move up a level (chrome/browser/cocoa -->
 // chrome/browser), and use in non-Mac unit tests such as
 // back_forward_menu_model_unittest.cc,
@@ -22,7 +29,8 @@ class BrowserTestHelper {
  public:
   BrowserTestHelper()
       : ui_thread_(ChromeThread::UI, &message_loop_),
-        file_thread_(ChromeThread::FILE, &message_loop_) {
+        file_thread_(ChromeThread::FILE, &message_loop_),
+        io_thread_(ChromeThread::IO, &message_loop_) {
     profile_.reset(new TestingProfile());
     profile_->CreateBookmarkModel(true);
     profile_->BlockUntilBookmarkModelLoaded();
@@ -73,6 +81,7 @@ class BrowserTestHelper {
   MessageLoopForUI message_loop_;
   ChromeThread ui_thread_;
   ChromeThread file_thread_;
+  ChromeThread io_thread_;
 };
 
 #endif  // CHROME_BROWSER_COCOA_BROWSER_TEST_HELPER_H_
