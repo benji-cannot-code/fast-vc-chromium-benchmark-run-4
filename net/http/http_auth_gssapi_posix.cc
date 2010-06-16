f8f66a4df1e6b4da69224b94f50d2667ac10e9b6
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-16 19:36:42 +0000

    Coverity: fix possible library leak in GSSAPISharedLibrary::LoadSharedLibrary.
    CID=10899
    BUG=none
    TEST=builds
    
    Review URL: http://codereview.chromium.org/2854004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50014 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_gssapi_posix.cc
+++ net/http/http_auth_gssapi_posix.cc
@@ -81,9 +81,12 @@ base::NativeLibrary GSSAPISharedLibrary::LoadSharedLibrary() {
     const char* library_name = kLibraryNames[i];
     FilePath file_path(library_name);
     base::NativeLibrary lib = base::LoadNativeLibrary(file_path);
-    // Only return this library if we can bind the functions we need.
-    if (lib && BindMethods(lib))
-      return lib;
+    if (lib) {
+      // Only return this library if we can bind the functions we need.
+      if (BindMethods(lib))
+        return lib;
+      base::UnloadNativeLibrary(lib);
+    }
   }
   LOG(WARNING) << "Unable to find a compatible GSSAPI library";
   return NULL;
@@ -512,4 +515,3 @@ int HttpAuthGSSAPI::GetNextSecurityToken(const std::wstring& spn,
 }
 
 }  // namespace net
-
