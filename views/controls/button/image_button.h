990b78fbaf8d52bd59aeaaa9e84293cd005e0abb
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-16 22:00:04 +0000

    Fix for UI issues on AutoFill dialog on Windows. BUG=46525 TEST=In the bug. Review URL: http://codereview.chromium.org/2802005
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50040 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/button/image_button.h
+++ views/controls/button/image_button.h
@@ -51,6 +51,12 @@ class ImageButton : public CustomButton {
   virtual gfx::Size GetPreferredSize();
   virtual void Paint(gfx::Canvas* canvas);
 
+  // Sets preferred size, so it could be correctly positioned in layout even if
+  // it is NULL.
+  void SetPreferredSize(const gfx::Size& preferred_size) {
+    preferred_size_ = preferred_size;
+  }
+
  protected:
   // Returns the image to paint. This is invoked from paint and returns a value
   // from images.
@@ -66,6 +72,7 @@ class ImageButton : public CustomButton {
   // Image alignment.
   HorizontalAlignment h_alignment_;
   VerticalAlignment v_alignment_;
+  gfx::Size preferred_size_;
 
   DISALLOW_COPY_AND_ASSIGN(ImageButton);
 };
