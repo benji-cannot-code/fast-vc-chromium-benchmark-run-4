7c1e6caedbe277d527efa77c70c00d22d208a39e
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-17 21:15:19 +0000

    2010-06-17  Kinuko Yasuda  <kinuko@chromium.org>
            Reviewed by Jian Li.
    
            Fix CRLF ending conversion in StringBlobItem.
            https://bugs.webkit.org/show_bug.cgi?id=40736
            Also: fix referencing uninitialized member bug and slice length bug
            in hybrid blob case.
    
            Tests will be added when we add BlobBuilder jsc bindings.
    
            * platform/BlobItem.cpp:
            (WebCore::StringBlobItem::convertToCString):
            (WebCore::DataRangeBlobItem::DataRangeBlobItem):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61349 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/BlobItem.cpp
+++ third_party/WebKit/WebCore/platform/BlobItem.cpp
@@ -91,9 +91,12 @@ unsigned long long FileBlobItem::size() const
 PassRefPtr<BlobItem> FileBlobItem::slice(long long start, long long length)
 {
     ASSERT(start >= 0 && length >= 0);
-    ASSERT(static_cast<unsigned long long>(start) < size());
-    if (!start && size() <= static_cast<unsigned long long>(length))
+    long long fileSize = size();
+    ASSERT(start < fileSize);
+    if (!start && fileSize <= length)
         return this;
+    if (start + length > fileSize)
+        length = fileSize - start;
     const FileRangeBlobItem* fileRangeItem = toFileRangeBlobItem();
     double modificationTime = fileRangeItem ? fileRangeItem->snapshotModificationTime() : getFileSnapshotModificationTime(path());
     return FileRangeBlobItem::create(path(), start, length, modificationTime);
@@ -150,6 +153,7 @@ CString StringBlobItem::convertToCString(const String& text, LineEnding ending, 
         if (c == '\r') {
             // Safe to look ahead because of trailing '\0'.
             if (*p == '\n' && ending != EndingCRLF) {
+                p++;
                 calculatedLength += (endingLength - 2);
                 ++needFix;
             } else if (ending != EndingCR) {
@@ -172,6 +176,7 @@ CString StringBlobItem::convertToCString(const String& text, LineEnding ending, 
     while (char c = *p++) {
         if (c == '\r') {
             if (*p == '\n' && ending != EndingCRLF) {
+                p++;
                 memcpy(q, endingChars, endingLength);
                 q += endingLength;
             } else if (*p != '\n' && ending != EndingCR) {
@@ -213,7 +218,7 @@ PassRefPtr<BlobItem> DataRangeBlobItem::create(PassRefPtr<DataBlobItem> item, lo
 DataRangeBlobItem::DataRangeBlobItem(PassRefPtr<DataBlobItem> item, long long start, long long length)
     : m_length(length)
 {
-    const DataRangeBlobItem* rangeItem = m_item->toDataRangeBlobItem();
+    const DataRangeBlobItem* rangeItem = item->toDataRangeBlobItem();
     if (rangeItem) {
         m_item = rangeItem->m_item;
         m_start = start + rangeItem->m_start;
