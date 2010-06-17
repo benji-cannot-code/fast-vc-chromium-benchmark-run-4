5af7485a6133e414938f810b9aec2c79e4361ed2
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-17 02:55:10 +0000

    2010-06-16  Jarkko Sakkinen  <jarkko.j.sakkinen@gmail.com>
            Reviewed by Simon Hausmann.
    
            [Qt] WebGL viewport does not show up on N900
            https://bugs.webkit.org/show_bug.cgi?id=38528
    
            With OGLES2 add default precision to the shader code.
            Use GLsizeiptr and GLintptr with OGLES2.
            Call paint() in beginPaint() so that drawTexture() is used
            whenever possible.
    
            * platform/graphics/qt/GraphicsContext3DQt.cpp:
            (WebCore::GraphicsContext3D::beginPaint):
            (WebCore::GraphicsContext3D::paint):
            (WebCore::GraphicsContext3D::shaderSource):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61301 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
@@ -57,8 +57,13 @@ typedef char GLchar;
 #define APIENTRY
 #endif
 
+#ifdef QT_OPENGL_ES_2
+typedef GLsizeiptr GLsizeiptrType;
+typedef GLintptr GLintptrType;
+#else
 typedef ptrdiff_t GLsizeiptrType;
 typedef ptrdiff_t GLintptrType;
+#endif
 
 typedef void (APIENTRY* glActiveTextureType) (GLenum);
 typedef void (APIENTRY* glAttachShaderType) (GLuint, GLuint);
@@ -518,18 +523,10 @@ void GraphicsContext3D::makeContextCurrent()
 void GraphicsContext3D::beginPaint(WebGLRenderingContext* context)
 {
     m_internal->m_glWidget->makeCurrent();
-
     HTMLCanvasElement* canvas = context->canvas();
     ImageBuffer* imageBuffer = canvas->buffer();
-
-    m_internal->bindFramebuffer(GraphicsContext3D::FRAMEBUFFER, m_internal->m_mainFbo);
-
-    glReadPixels(/* x */ 0, /* y */ 0, m_currentWidth, m_currentHeight, GraphicsContext3D::RGBA, GraphicsContext3D::UNSIGNED_BYTE, m_internal->m_pixels.bits());
-
-    QPainter* p = imageBuffer->context()->platformContext();
-    p->drawImage(/* x */ 0, /* y */ 0, m_internal->m_pixels.rgbSwapped().transformed(QMatrix().rotate(180)));
-
-    m_internal->bindFramebuffer(GraphicsContext3D::FRAMEBUFFER, m_internal->m_currentFbo);
+    QPainter* painter = imageBuffer->context()->platformContext();
+    paint(painter, QRect(QPoint(0, 0), QSize(m_currentWidth, m_currentHeight)));
 }
 
 void GraphicsContext3D::endPaint()
@@ -542,7 +539,7 @@ void GraphicsContext3D::paint(QPainter* painter, const QRect& rect) const
     QWebPageClient* webPageClient = m_internal->m_hostWindow->platformPageClient();
     QGLWidget* ownerGLWidget  = m_internal->getOwnerGLWidget(webPageClient);
     if (ownerGLWidget) {
-        ownerGLWidget->drawTexture(QPointF(0, 0), m_internal->m_texture);
+        ownerGLWidget->drawTexture(rect, m_internal->m_texture);
         return;
     } 
 #endif
@@ -1083,12 +1080,20 @@ void GraphicsContext3D::scissor(long x, long y, unsigned long width, unsigned lo
 void GraphicsContext3D::shaderSource(WebGLShader* shader, const String& source)
 {
     ASSERT(shader);
-    
+
     m_internal->m_glWidget->makeCurrent();
 
-    CString sourceCS = source.utf8();
+    String prefixedSource;
+
+#if defined (QT_OPENGL_ES_2)
+    prefixedSource.append("precision mediump float;\n");
+#endif
+
+    prefixedSource.append(source);
+
+    CString sourceCS = prefixedSource.utf8();
     const char* data = sourceCS.data();
-    int length = source.length();
+    int length = prefixedSource.length();
     m_internal->shaderSource((GLuint) shader->object(), /* count */ 1, &data, &length);
 }
 
