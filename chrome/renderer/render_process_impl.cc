00441b70789d6636ff78b352fcbcd684fef7c5ad
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-17 08:39:29 +0000

    Remove the dependency from renderer on browser.
    BUG=46666
    TEST=none
    Review URL: http://codereview.chromium.org/2852012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50081 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_process_impl.cc
+++ chrome/renderer/render_process_impl.cc
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/histogram.h"
 #include "base/path_service.h"
 #include "base/sys_info.h"
-// TODO(jar): DNS calls should be renderer specific, not including browser.
-#include "chrome/browser/net/dns_global.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/render_messages.h"
