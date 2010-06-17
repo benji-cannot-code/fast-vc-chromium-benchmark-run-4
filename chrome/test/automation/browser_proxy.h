1fa1825d469d4fa07672d01b4a336c09de90b6b7
pathorn@chromium.org <pathorn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-17 03:03:03 +0000

    Make new startup test use GetInitialLoadTimes for more accurate times.
    This change modifies RunStartupTest to call GetInitialLoadTimes, if applicable, to print out additional statistics for each tab computed on the browser.
    
    Additionally, changes |test_cold| and |important| parameters to be enums instead of booleans to make tests more readable.
    
    BUG=44129
    TEST=none
    
    Review URL: http://codereview.chromium.org/2714015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50075 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automation/browser_proxy.h
+++ chrome/test/automation/browser_proxy.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #include <string>
+#include <vector>
 
 #include "base/compiler_specific.h"
 #include "chrome/browser/browser.h"
@@ -230,6 +231,15 @@ class BrowserProxy : public AutomationResourceProxy {
   // Experimental generic pattern.
   bool SendJSONRequest(const std::string& request,
                        std::string* response) WARN_UNUSED_RESULT;
+
+  // Gets the load times for all tabs started from the command line.
+  // Puts the time of the first tab to start loading into |min_start_time|,
+  // the time when loading stopped into |max_stop_time| (should be similar to
+  // the delay that WaitForInitialLoads waits for), and a list of all
+  // finished timestamps into |stop_times|. Returns true on success.
+  bool GetInitialLoadTimes(float* min_start_time, float* max_stop_time,
+                           std::vector<float>* stop_times);
+
 
  protected:
   virtual ~BrowserProxy() {}
