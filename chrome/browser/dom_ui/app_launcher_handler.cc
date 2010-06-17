c6360247d27c755407faad14a2a108e76fc7b138
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-17 02:09:40 +0000

    Make apps launch using their default container as specified in their manifest.
    The debug options were forcefully causing apps to launch as tabs.
    
    BUG=46502
    TEST=try launching a panel or window based app, it shouldn't launch as a tab
    
    Review URL: http://codereview.chromium.org/2844008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50070 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/app_launcher_handler.cc
+++ chrome/browser/dom_ui/app_launcher_handler.cc
@@ -179,7 +179,7 @@ void AppLauncherHandler::HandleLaunchApp(const Value* value) {
     return;
   }
 
-  Extension::LaunchContainer container = Extension::LAUNCH_TAB;
+  Extension::LaunchContainer container = extension->launch_container();
   if (launch_container == "tab")
     container = Extension::LAUNCH_TAB;
   else if (launch_container == "panel")
