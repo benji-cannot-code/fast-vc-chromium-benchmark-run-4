1157e804405cf369292a98c2ffc72ef7a7832f64
jcivelli@chromium.org <jcivelli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-17 22:53:46 +0000

    Revert 50148: It causes the unit-test, ui-tests and more to fail with a not found symbol.
    - Porting the infobars on Linux to the new TranslateInfobarDelegate2.
    Each translate infobar is now its own class and most of the logic is in the delegate.
    
    BUG=40828
    TEST=Thoroughly test the translate feature on Linux.
    
    Review URL: http://codereview.chromium.org/2836006
    
    TBR=jcivelli@chromium.org
    Review URL: http://codereview.chromium.org/2805016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50157 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -57,10 +57,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_model.h"
 #include "chrome/browser/search_engines/template_url_prepopulate_data.h"
 #include "chrome/browser/shell_integration.h"
-#if defined(OS_WIN) || defined(OS_LINUX)
-#include "chrome/browser/translate/translate_manager2.h"
-#else
+#if !defined(OS_WIN)
 #include "chrome/browser/translate/translate_manager.h"
+#else
+#include "chrome/browser/translate/translate_manager2.h"
 #endif
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
@@ -1176,7 +1176,7 @@ int BrowserMain(const MainFunctionParams& parameters) {
     return ResultCodes::MACHINE_LEVEL_INSTALL_EXISTS;
 
   // Create the TranslateManager singleton.
-#if defined(OS_WIN) || defined(OS_LINUX)
+#if defined(OS_WIN)
   Singleton<TranslateManager2>::get();
 #else
   Singleton<TranslateManager>::get();
