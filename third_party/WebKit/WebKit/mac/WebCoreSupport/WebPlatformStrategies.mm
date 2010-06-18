91d5695f6783e4950864ac5e3d70c81df488a654
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-18 23:01:41 +0000

    Fix Tiger build.
    * WebCoreSupport/WebPlatformStrategies.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61449 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebPlatformStrategies.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebPlatformStrategies.mm
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebCore/BlockExceptions.h>
 #import <wtf/StdLibExtras.h>
 
+#ifdef BUILDING_ON_TIGER
+-typedef unsigned NSUInteger;
+#endif
+
 using namespace WebCore;
 
 void WebPlatformStrategies::initialize()
