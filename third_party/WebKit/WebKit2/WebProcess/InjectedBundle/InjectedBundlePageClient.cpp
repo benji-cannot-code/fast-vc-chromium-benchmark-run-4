f2b97a19d6971d494e589c94319348790c77e3b4
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-18 21:52:54 +0000

    git-svn-id: svn://svn.chromium.org/blink/trunk@61443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKit2/WebProcess/InjectedBundle/InjectedBundlePageClient.cpp
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+ *  InjectedBundlePageClient.cpp
+ *  WebKit2
+ *
+ *  Created by Sam Weinig on 6/16/10.
+ *  Copyright 2010 Apple Inc. All rights reserved.
+ *
+ */
+
+#include "InjectedBundlePageClient.h"
+
+#include "WKBundleAPICast.h"
+
+namespace WebKit {
+
+InjectedBundlePageClient::InjectedBundlePageClient()
+{
+    initialize(0);
+}
+
+void InjectedBundlePageClient::initialize(WKBundlePageClient* client)
+{
+    if (client && !client->version)
+        m_client = *client;
+    else 
+        memset(&m_client, 0, sizeof(m_client));
+}
+
+void InjectedBundlePageClient::didClearWindowObjectForFrame(WebPage* page, WebFrame* frame, JSContextRef ctx, JSObjectRef window)
+{
+    if (m_client.didClearWindowObjectForFrame)
+        m_client.didClearWindowObjectForFrame(toRef(page), toRef(frame), ctx, window, m_client.clientInfo);
+}
+
+} // namespace WebKit
