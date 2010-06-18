88226a0800c24e65537bc7d8919d0d37a0d2657e
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-18 23:51:31 +0000

    2010-06-18  Anders Carlsson  <andersca@apple.com>
            Reviewed by Sam Weinig.
    
            Get rid of the NSEnumerators from WebBasePluginPackage
            https://bugs.webkit.org/show_bug.cgi?id=40868
    
            * Plugins/WebBasePluginPackage.h:
            * Plugins/WebBasePluginPackage.mm:
            (-[WebBasePluginPackage MIMETypes]):
            (-[WebBasePluginPackage supportsExtension:]):
            (-[WebBasePluginPackage supportsMIMEType:]):
            * Plugins/WebPluginDatabase.mm:
            (-[WebPluginDatabase pluginForMIMEType:]):
            (-[WebPluginDatabase pluginForExtension:]):
            (-[WebPluginDatabase refresh]):
            (-[WebPluginDatabase _removePlugin:]):
            * WebCoreSupport/WebPlatformStrategies.mm:
            (WebPlatformStrategies::getPluginInfo):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebPlatformStrategies.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebPlatformStrategies.mm
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <wtf/StdLibExtras.h>
 
 #ifdef BUILDING_ON_TIGER
--typedef unsigned NSUInteger;
+typedef unsigned NSUInteger;
 #endif
 
 using namespace WebCore;
@@ -73,21 +73,23 @@ void WebPlatformStrategies::getPluginInfo(Vector<WebCore::PluginInfo>& plugins)
         pluginInfo.file = [plugin filename];
         pluginInfo.desc = [plugin pluginDescription];
 
-        NSEnumerator* MIMETypeEnumerator = [plugin MIMETypeEnumerator];
-        while (NSString* MIME = [MIMETypeEnumerator nextObject]) {
-            MimeClassInfo mime;
+        NSArray *MIMETypes = [plugin MIMETypes];
+        for (NSUInteger i = 0; i < [MIMETypes count]; ++i) {
+            NSString *MIMEType = [MIMETypes objectAtIndex:i];
 
-            mime.type = String(MIME).lower();
+            MimeClassInfo mimeClassInfo;
+
+            mimeClassInfo.type = String(MIMEType).lower();
             
-            NSArray *extensions = [plugin extensionsForMIMEType:MIME];
+            NSArray *extensions = [plugin extensionsForMIMEType:MIMEType];
             for (NSUInteger i = 0; i < [extensions count]; ++i) {
                 NSString *extension = [extensions objectAtIndex:i];
-                mime.extensions.append(extension);
+                mimeClassInfo.extensions.append(extension);
             }
 
-            mime.desc = [plugin descriptionForMIMEType:MIME];
+            mimeClassInfo.desc = [plugin descriptionForMIMEType:MIMEType];
             
-            pluginInfo.mimes.append(mime);
+            pluginInfo.mimes.append(mimeClassInfo);
         }
 
         plugins.append(pluginInfo);
