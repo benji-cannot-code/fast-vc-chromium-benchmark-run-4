026a982118be7be416bca68b327c1f4e77f9a44b
jcivelli@chromium.org <jcivelli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-19 00:52:05 +0000

    Porting the infobars on Linux to the new TranslateInfobarDelegate2 (relanding) Each translate infobar is now its own class and most of the logic is in the delegate.
    Original review:
    http://codereview.chromium.org/2836006/show
    
    BUG=40828
    TEST=Thoroughly test the translate feature on Linux.
    
    Review URL: http://codereview.chromium.org/2817015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50308 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/gtk/translate/translate_message_infobar_gtk.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_GTK_TRANSLATE_TRANSLATE_MESSAGE_INFOBAR_GTK_H_
+#define CHROME_BROWSER_GTK_TRANSLATE_TRANSLATE_MESSAGE_INFOBAR_GTK_H_
+
+#include "chrome/browser/gtk/translate/translate_infobar_base_gtk.h"
+
+class TranslateInfoBarDelegate2;
+
+class TranslateMessageInfoBar : public TranslateInfoBarBase {
+ public:
+  explicit TranslateMessageInfoBar(TranslateInfoBarDelegate2* delegate);
+  virtual ~TranslateMessageInfoBar();
+
+  // Overridden from TranslateInfoBarBase:
+  virtual void Init();
+
+ private:
+  CHROMEGTK_CALLBACK_0(TranslateMessageInfoBar, void, OnButtonPressed);
+
+  DISALLOW_COPY_AND_ASSIGN(TranslateMessageInfoBar);
+};
+
+#endif  // CHROME_BROWSER_GTK_TRANSLATE_TRANSLATE_MESSAGE_INFOBAR_GTK_H_
