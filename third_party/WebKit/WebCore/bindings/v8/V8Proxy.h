6fcb5fea7b2666e7e6b8be096eb7c0a1e82aba9d
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-20 00:38:00 +0000

    2010-06-19  Tony Gentilcore  <tonyg@chromium.org>
            Reviewed by Adam Barth.
    
            Persist V8's ScriptData to the memory cache.
            https://bugs.webkit.org/show_bug.cgi?id=38661
    
            When V8 ScriptData caching was originally submitted it causes crashes
            in external scripts with high-byte characters. This new test crashes on
            the original code, but now passes.
    
            * fast/js/parser-high-byte-character-expected.txt: Added.
            * fast/js/parser-high-byte-character.html: Added.
            * fast/js/script-tests/parser-high-byte-character.js: Added.
            (runTest):
    2010-06-19  Tony Gentilcore  <tonyg@chromium.org>
    
            Reviewed by Adam Barth.
    
            Persist V8's ScriptData to the memory cache.
            https://bugs.webkit.org/show_bug.cgi?id=38661
    
            This stores V8's ScriptData in the memory cache and also causes the
            network platform layer to be notified of the available cacheable
            metadata.
    
            Chromium's morejs benchmark showed a ~7% improvement when this was
            originally submitted (before it had to be rolled back).
    
            Test: fast/js/parser-high-byte-character.html
    
            * bindings/v8/ScriptSourceCode.h:
            (WebCore::ScriptSourceCode::ScriptSourceCode):
            (WebCore::ScriptSourceCode::cachedScript):
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::compileScript):
            (WebCore::V8Proxy::precompileScript):
            (WebCore::V8Proxy::evaluate):
            * bindings/v8/V8Proxy.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61499 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8Proxy.h
+++ third_party/WebKit/WebCore/bindings/v8/V8Proxy.h
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+    class CachedScript;
     class DOMWindow;
     class Frame;
     class Node;
@@ -286,7 +287,7 @@ namespace WebCore {
 
         static v8::Handle<v8::Value> checkNewLegal(const v8::Arguments&);
 
-        static v8::Handle<v8::Script> compileScript(v8::Handle<v8::String> code, const String& fileName, int baseLine);
+        static v8::Handle<v8::Script> compileScript(v8::Handle<v8::String> code, const String& fileName, int baseLine, v8::ScriptData* = 0);
 
         // If the exception code is different from zero, a DOM exception is
         // schedule to be thrown.
@@ -341,6 +342,8 @@ namespace WebCore {
         void releaseStorageMutex();
 
         void resetIsolatedWorlds();
+
+        PassOwnPtr<v8::ScriptData> precompileScript(v8::Handle<v8::String>, CachedScript*);
 
         // Returns false when we're out of memory in V8.
         bool setInjectedScriptContextDebugId(v8::Handle<v8::Context> targetContext);
