fa19adc9d7355d570fd701a9dada89c5aa08eb42
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-21 18:29:27 +0000

    Fix clang++ build.
    * loader/FormSubmission.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61560 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FormSubmission.h
+++ third_party/WebKit/WebCore/loader/FormSubmission.h
@@ -39,7 +39,7 @@ namespace WebCore {
 class Event;
 class FormData;
 class FormState;
-class FrameLoadRequest;
+struct FrameLoadRequest;
 
 class FormSubmission : public RefCounted<FormSubmission> {
 public:
