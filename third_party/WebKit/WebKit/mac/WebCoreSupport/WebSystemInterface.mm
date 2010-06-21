6d78aa317a0bf2a06b39abe33c23452694ace4d1
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-21 19:22:12 +0000

    2010-06-21  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Dan Bernstein.
    
            Chromium/Mac build fix.
    
            * WebCoreSupport/WebSystemInterface.mm: Change the order of wtf/Platform.h include to let other
                headers smell it.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebCoreSupport/WebSystemInterface.mm
+++ third_party/WebKit/WebKit/mac/WebCoreSupport/WebSystemInterface.mm
@@ -29,11 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "WebSystemInterface.h"
 
-#import <WebCore/WebCoreSystemInterface.h>
-#import <WebKitSystemInterface.h>
-
 // Needed for builds not using PCH to expose BUILDING_ macros, see bug 32753.
 #include <wtf/Platform.h>
+
+#import <WebCore/WebCoreSystemInterface.h>
+#import <WebKitSystemInterface.h>
 
 #define INIT(function) wk##function = WK##function
 
