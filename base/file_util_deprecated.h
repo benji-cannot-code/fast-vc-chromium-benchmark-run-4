2c13d0dc2724a383a2c2256efbb7cbd0e6264588
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-21 03:05:19 +0000

    Remove deprecated version of file_util::EndsWithSeparator.
    BUG=24672
    TEST=out/Debug/base_unittests
    
    Review URL: http://codereview.chromium.org/2842015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50330 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_deprecated.h
+++ base/file_util_deprecated.h
@@ -18,9 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace file_util {
 
-bool EndsWithSeparator(std::wstring* path);
-bool EndsWithSeparator(const std::wstring& path);
-
 // Use FilePath::DirName instead.
 void UpOneDirectory(std::wstring* dir);
 // Use FilePath::DirName instead.
