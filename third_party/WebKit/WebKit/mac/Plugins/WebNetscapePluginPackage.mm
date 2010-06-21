4c1449552af216edf4d19fecac1ae706ece65a25
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-21 03:35:13 +0000

    Fix Tiger build.
    * Plugins/WebNetscapePluginPackage.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61526 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginPackage.mm
+++ third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginPackage.mm
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(NETSCAPE_PLUGIN_API)
 #import "WebNetscapePluginPackage.h"
 
+#import "WebTypesInternal.h"
 #import "WebKitLogging.h"
 #import "WebKitNSStringExtras.h"
 #import "WebNSFileManagerExtras.h"
