6d2e3e66377441963135acba1b1e779ba1d1a125
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-21 19:57:30 +0000

    [wx] Build fix. Fix header includes for ENABLE(DATABASE)
    git-svn-id: svn://svn.chromium.org/blink/trunk@61566 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSExceptionBase.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSExceptionBase.cpp
@@ -31,14 +31,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSEventException.h"
 #include "JSRangeException.h"
 #include "JSXMLHttpRequestException.h"
+#if ENABLE(DATABASE)
+#include "SQLException.h"
+#include "JSSQLException.h"
+#endif
 #if ENABLE(SVG)
 #include "JSSVGException.h"
 #endif
 #if ENABLE(XPATH)
 #include "JSXPathException.h"
-#endif
-#if ENABLE(XPATH)
-#include "JSSQLException.h"
 #endif
 
 namespace WebCore {
