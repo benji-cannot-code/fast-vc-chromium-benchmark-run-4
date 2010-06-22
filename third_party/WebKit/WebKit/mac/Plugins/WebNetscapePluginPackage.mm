fd33469994fb00022ad29b028e6e47fb3667565f
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-22 19:54:41 +0000

    2010-06-22  Anders Carlsson  <andersca@apple.com>
            Reviewed by Sam Weinig.
    
            Wean NetscapePluginHostManager of any knowledge about WebNetscapePluginPackage
            https://bugs.webkit.org/show_bug.cgi?id=41006
    
            * Plugins/Hosted/NetscapePluginHostManager.h:
            * Plugins/Hosted/NetscapePluginHostManager.mm:
            (WebKit::NetscapePluginHostManager::hostForPlugin):
            (WebKit::NetscapePluginHostManager::spawnPluginHost):
            (WebKit::NetscapePluginHostManager::instantiatePlugin):
            (WebKit::NetscapePluginHostManager::createPropertyListFile):
            * Plugins/Hosted/WebHostedNetscapePluginView.mm:
            (-[WebHostedNetscapePluginView createPlugin]):
            * Plugins/WebNetscapePluginPackage.mm:
            (-[WebNetscapePluginPackage createPropertyListFile]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61625 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginPackage.mm
+++ third_party/WebKit/WebKit/mac/Plugins/WebNetscapePluginPackage.mm
@@ -303,7 +303,7 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
 
 - (void)createPropertyListFile
 {
-    NetscapePluginHostManager::createPropertyListFile(self);
+    NetscapePluginHostManager::createPropertyListFile(path, pluginHostArchitecture);
 }
 
 #endif
