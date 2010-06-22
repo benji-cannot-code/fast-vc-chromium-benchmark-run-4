83ded142dedf6b2c637e42a5c4dda38f828dfee0
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-22 01:01:32 +0000

    In pages rendered in ChromeFrame window open requests or link clicks with target blank which target a different origin should initiate the navigation in the host browser. We achieve this by performing an origin check on the opener frame and the URL being opened. If the origins don't match we allow the host browser to handle this navigation.
    There is still one issue here as a popup window creation request is still initiated and sent out to the host browser
    which initiates a dummy attach external tab navigation. Subsequently while applying policy the OpenURL IPC is sent out
    to the host browser which initiates the navigation to the expected URL. This causes a dummy attach external tab entry
    to be created in the host browser's history which would have to be deleted.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=46667
    
    Bug=46667
    Test=Covered by new chrome frame unit test.
    
    Review URL: http://codereview.chromium.org/2855017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50416 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/external_tab_container_win.cc
+++ chrome/browser/external_tab_container_win.cc
@@ -38,7 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 static const wchar_t kWindowObjectKey[] = L"ChromeWindowObject";
 
-ExternalTabContainer::PendingTabs ExternalTabContainer::pending_tabs_;
+base::LazyInstance<ExternalTabContainer::PendingTabs>
+    ExternalTabContainer::pending_tabs_(base::LINKER_INITIALIZED);
+
+// ExternalTabContainer::PendingTabs ExternalTabContainer::pending_tabs_;
 ExternalTabContainer* ExternalTabContainer::innermost_tab_for_unload_event_
     = NULL;
 
@@ -348,7 +351,7 @@ void ExternalTabContainer::AddNewContents(TabContents* source,
 
   if (result) {
     uintptr_t cookie = reinterpret_cast<uintptr_t>(new_container.get());
-    pending_tabs_[cookie] = new_container;
+    pending_tabs_.Get()[cookie] = new_container;
     new_container->set_pending(true);
     IPC::AttachExternalTabParams attach_params_;
     attach_params_.cookie = static_cast<uint64>(cookie);
@@ -765,10 +768,11 @@ bool ExternalTabContainer::InitNavigationInfo(IPC::NavigationInfo* nav_info,
 
 scoped_refptr<ExternalTabContainer> ExternalTabContainer::RemovePendingTab(
     uintptr_t cookie) {
-  PendingTabs::iterator index = pending_tabs_.find(cookie);
-  if (index != pending_tabs_.end()) {
+  ExternalTabContainer::PendingTabs& pending_tabs = pending_tabs_.Get();
+  PendingTabs::iterator index = pending_tabs.find(cookie);
+  if (index != pending_tabs.end()) {
     scoped_refptr<ExternalTabContainer> container = (*index).second;
-    pending_tabs_.erase(index);
+    pending_tabs.erase(index);
     return container;
   }
 
--- chrome/browser/external_tab_container_win.h
+++ chrome/browser/external_tab_container_win.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 #include <map>
-
+#include "base/lazy_instance.h"
 #include "chrome/browser/automation/automation_resource_message_filter.h"
 #include "chrome/browser/automation/automation_profile_impl.h"
 #include "chrome/browser/browser.h"
@@ -293,7 +293,7 @@ class ExternalTabContainer : public TabContentsDelegate,
   scoped_ptr<Browser> browser_;
 
   // Contains ExternalTabContainers that have not been connected to as yet.
-  static PendingTabs pending_tabs_;
+  static base::LazyInstance<PendingTabs> pending_tabs_;
 
   // True if this tab is currently the conduit for extension API automation.
   bool enabled_extension_automation_;
