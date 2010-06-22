30e9de241d45f765a96e6c03070f6d1cb712e60e
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-22 15:22:34 +0000

    Remove sendMessageToFrontendOnIOThread implementation from test_shell_devtools_agent.cc
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2822022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50459 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_devtools_agent.cc
+++ webkit/tools/test_shell/test_shell_devtools_agent.cc
@@ -163,9 +163,3 @@ bool TestShellDevToolsAgent::evaluateInWebInspector(
   agent->evaluateInWebInspector(call_id, WebString::fromUTF8(script));
   return true;
 }
-
-// static
-void WebKit::WebDevToolsAgentClient::sendMessageToFrontendOnIOThread(
-    WebKit::WebDevToolsMessageData const &) {
-  NOTIMPLEMENTED();
-}
