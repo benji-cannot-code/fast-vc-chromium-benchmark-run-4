0595a22307c4987ff21ff3463ea61a05aed55d82
andybons@chromium.org <andybons@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-22 20:32:30 +0000

    [Mac] Adds back in the right divider at alcor's request. Tightens up the right wrench menu in Toolbar.xib.
    TEST=none
    BUG=45764
    Review URL: http://codereview.chromium.org/2813025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50512 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/extensions/browser_actions_controller.mm
+++ chrome/browser/cocoa/extensions/browser_actions_controller.mm
@@ -41,8 +41,9 @@ const CGFloat kAnimationDuration = 0.2;
 const CGFloat kButtonOpacityLeadPadding = 5.0;
 const CGFloat kChevronHeight = 28.0;
 const CGFloat kChevronLowerPadding = 5.0;
+const CGFloat kChevronRightPadding = 5.0;
 const CGFloat kChevronWidth = 14.0;
-const CGFloat kGrippyXOffset = 7.0;
+const CGFloat kGrippyXOffset = 5.0;
 }  // namespace
 
 @interface BrowserActionsController(Private)
@@ -551,12 +552,16 @@ class ExtensionsServiceObserverBridge : public NotificationObserver,
 - (CGFloat)containerWidthWithButtonCount:(NSUInteger)buttonCount {
   CGFloat width = 0.0;
   if (buttonCount > 0) {
-    width = kGrippyXOffset +
+    width = kGrippyXOffset + (2 * kBrowserActionButtonPadding) +
         (buttonCount * (kBrowserActionWidth + kBrowserActionButtonPadding));
   }
   // Make room for the chevron if any buttons are hidden.
-  if ([self buttonCount] != [self visibleButtonCount])
+  if ([self buttonCount] != [self visibleButtonCount]) {
     width += kChevronWidth + kBrowserActionButtonPadding;
+    // Add more space if all buttons are hidden.
+    if ([self visibleButtonCount] == 0)
+      width += 3 * kBrowserActionButtonPadding;
+  }
 
   return width;
 }
@@ -729,7 +734,7 @@ class ExtensionsServiceObserverBridge : public NotificationObserver,
 }
 
 - (void)updateChevronPositionInFrame:(NSRect)frame {
-  CGFloat xPos = NSWidth(frame) - kChevronWidth;
+  CGFloat xPos = NSWidth(frame) - kChevronWidth - kChevronRightPadding;
   NSRect buttonFrame = NSMakeRect(xPos,
                                   kChevronLowerPadding,
                                   kChevronWidth,
