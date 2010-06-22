2b8282462a1069dc47a2077a6a5d55e51c483e96
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-22 07:27:25 +0000

    Whitespace change to kick builders.
    TBR=rafaelw@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50435 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension.h
+++ chrome/common/extensions/extension.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ExtensionAction;
 class ExtensionResource;
 
-// Represents a Chrome sextension.
+// Represents a Chrome extension.
 class Extension {
  public:
   typedef std::vector<URLPattern> URLPatternList;
