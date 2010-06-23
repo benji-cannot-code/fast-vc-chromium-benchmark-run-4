48d45ae64729823c5282253df5bcfcc826e0c249
ericu@google.com <ericu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-23 23:10:17 +0000

    Mark SrpcPluginTest as flaky.
    BUG=643
    TEST=none
    
    Review URL: http://codereview.chromium.org/2799029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50665 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/nacl/nacl_test.cc
+++ chrome/test/nacl/nacl_test.cc
@@ -144,7 +144,7 @@ TEST_F(NaClTest, SrpcShmTest) {
   RunTest(test_file, action_max_timeout_ms());
 }
 
-TEST_F(NaClTest, SrpcPluginTest) {
+TEST_F(NaClTest, FLAKY_SrpcPluginTest) {
   FilePath test_file(kSrpcPluginHtmlFileName);
   RunTest(test_file, action_max_timeout_ms());
 }
