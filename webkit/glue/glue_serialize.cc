1c1e83003ada4da4707365e94497112c6e689e01
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-23 21:14:02 +0000

    Remove preprocess hack now that WebKit has been updated.
    R=sky
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2832021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50642 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/glue_serialize.cc
+++ webkit/glue/glue_serialize.cc
@@ -62,11 +62,7 @@ struct SerializeObject {
 // NOTE: If the version is -1, then the pickle contains only a URL string.
 // See CreateHistoryStateForURL.
 //
-#if defined(WEBKIT_BUG_40451_IS_FIXED)
 int kVersion = 9;
-#else
-int kVersion = 8;
-#endif
 
 // A bunch of convenience functions to read/write to SerializeObjects.
 // The serializers assume the input data is in the correct format and so does
@@ -312,10 +308,8 @@ static void WriteHistoryItem(
 
   WriteStringVector(item.documentState(), obj);
 
-#if defined(WEBKIT_BUG_40451_IS_FIXED)
   if (kVersion >= 9)
     WriteInteger64(item.itemSequenceNumber(), obj);
-#endif
   if (kVersion >= 6)
     WriteInteger64(item.documentSequenceNumber(), obj);
   if (kVersion >= 7) {
@@ -375,10 +369,8 @@ static WebHistoryItem ReadHistoryItem(
 
   item.setDocumentState(ReadStringVector(obj));
 
-#if defined(WEBKIT_BUG_40451_IS_FIXED)
   if (obj->version >= 9)
     item.setItemSequenceNumber(ReadInteger64(obj));
-#endif
   if (obj->version >= 6)
     item.setDocumentSequenceNumber(ReadInteger64(obj));
   if (obj->version >= 7) {
