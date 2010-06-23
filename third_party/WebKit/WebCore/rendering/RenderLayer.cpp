d7dc549084b90820e8df6d62b95b2968c1bd247d
benjamin.poulain@nokia.com <benjamin.poulain@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-23 14:11:26 +0000

    Do not render the full frame when there is some elements with fixed positioning https://bugs.webkit.org/show_bug.cgi?id=33150
    Reviewed by Kenneth Rohde Christiansen.
    
    Do not render the full frame when there is some elements with fixed positioning
    https://bugs.webkit.org/show_bug.cgi?id=33150
    
    The frame view take into acount the list of fixed object when scrolling
    the view. If the number of object is lower than a certain threshold, the pixel
    are blitted, and the invalidated area updated.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::addFixedObject):
    (WebCore::FrameView::removeFixedObject):
    (WebCore::FrameView::scrollContentsFastPath):
    * page/FrameView.h:
    * platform/ScrollView.cpp:
    (WebCore::ScrollView::scrollContents):
    (WebCore::ScrollView::scrollContentsFastPath):
    * platform/ScrollView.h:
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::repaintRectIncludingDescendants):
    * rendering/RenderLayer.h:
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::styleWillChange):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61686 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayer.cpp
@@ -377,6 +377,14 @@ void RenderLayer::updateLayerPositions(UpdateLayerPositionsFlags flags, IntPoint
         *cachedOffset = oldCachedOffset;
 }
 
+IntRect RenderLayer::repaintRectIncludingDescendants() const
+{
+    IntRect repaintRect = m_repaintRect;
+    for (RenderLayer* child = firstChild(); child; child = child->nextSibling())
+        repaintRect.unite(child->repaintRectIncludingDescendants());
+    return repaintRect;
+}
+
 void RenderLayer::computeRepaintRects()
 {
     RenderBoxModelObject* repaintContainer = renderer()->containerForRepaint();
