800466a94bd366e53520ae5ac55d5e5baa70d1ef
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-23 00:15:58 +0000

    Enable the disabled test ProxyResolverV8Test.ReturnUnicode, by addressing some encoding issues.
    Switched some uses of UTF-8 encoded std::strings to UTF-16 encoded string16 -- this avoids the confusion on which strings are unicode, since these std::string were getting passed around and later assumed to be ASCII.
    
    BUG=46608, 47234
    
    Review URL: http://codereview.chromium.org/2827018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50549 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_resolver_v8_unittest.cc
+++ net/proxy/proxy_resolver_v8_unittest.cc
@@ -4,8 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/file_util.h"
-#include "base/string_util.h"
 #include "base/path_service.h"
+#include "base/string_util.h"
+#include "base/utf_string_conversions.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_log_unittest.h"
 #include "net/base/net_errors.h"
@@ -24,36 +25,41 @@ class MockJSBindings : public ProxyResolverJSBindings {
  public:
   MockJSBindings() : my_ip_address_count(0), my_ip_address_ex_count(0) {}
 
-  virtual void Alert(const std::string& message) {
+  virtual void Alert(const string16& message) {
     LOG(INFO) << "PAC-alert: " << message;  // Helpful when debugging.
-    alerts.push_back(message);
+    alerts.push_back(UTF16ToUTF8(message));
   }
 
-  virtual std::string MyIpAddress() {
+  virtual bool MyIpAddress(std::string* ip_address) {
     my_ip_address_count++;
-    return my_ip_address_result;
+    *ip_address = my_ip_address_result;
+    return !my_ip_address_result.empty();
   }
 
-  virtual std::string MyIpAddressEx() {
+  virtual bool MyIpAddressEx(std::string* ip_address_list) {
     my_ip_address_ex_count++;
-    return my_ip_address_ex_result;
+    *ip_address_list = my_ip_address_ex_result;
+    return !my_ip_address_ex_result.empty();
   }
 
-  virtual std::string DnsResolve(const std::string& host) {
+  virtual bool DnsResolve(const std::string& host, std::string* ip_address) {
     dns_resolves.push_back(host);
-    return dns_resolve_result;
+    *ip_address = dns_resolve_result;
+    return !dns_resolve_result.empty();
   }
 
-  virtual std::string DnsResolveEx(const std::string& host) {
+  virtual bool DnsResolveEx(const std::string& host,
+                            std::string* ip_address_list) {
     dns_resolves_ex.push_back(host);
-    return dns_resolve_ex_result;
+    *ip_address_list = dns_resolve_ex_result;
+    return !dns_resolve_ex_result.empty();
   }
 
-  virtual void OnError(int line_number, const std::string& message) {
+  virtual void OnError(int line_number, const string16& message) {
     // Helpful when debugging.
     LOG(INFO) << "PAC-error: [" << line_number << "] " << message;
 
-    errors.push_back(message);
+    errors.push_back(UTF16ToUTF8(message));
     errors_line_number.push_back(line_number);
   }
 
@@ -307,9 +313,7 @@ TEST(ProxyResolverV8Test, UnhandledException) {
   EXPECT_EQ(3, bindings->errors_line_number[0]);
 }
 
-// TODO(eroman): This test fails right now, since the parsing of
-// host/port doesn't check for non-ascii characters. http://crbug.com/46608
-TEST(ProxyResolverV8Test, FAILS_ReturnUnicode) {
+TEST(ProxyResolverV8Test, ReturnUnicode) {
   ProxyResolverV8WithMockBindings resolver;
   int result = resolver.SetPacScriptFromDisk("return_unicode.js");
   EXPECT_EQ(OK, result);
@@ -319,7 +323,7 @@ TEST(ProxyResolverV8Test, FAILS_ReturnUnicode) {
                                    BoundNetLog());
 
   // The result from this resolve was unparseable, because it
-  // wasn't ascii.
+  // wasn't ASCII.
   EXPECT_EQ(ERR_PAC_SCRIPT_FAILED, result);
 }
 
