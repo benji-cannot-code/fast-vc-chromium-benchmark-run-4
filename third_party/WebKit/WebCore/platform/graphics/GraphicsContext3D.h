2f86bf654c032c4c75430fc1ea63857e75be37a1
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-23 01:55:50 +0000

    2010-06-22  Vangelis Kokkevis  <vangelis@chromium.org>
            Reviewed by Kent Tamura.
    
            Define GraphicsContext3D::platformLayer() for all remaining (non PLATFORM(MAC))
            ports to get RenderLayerBacking.cpp compiling again.
            https://bugs.webkit.org/show_bug.cgi?id=41026
    
            * platform/graphics/GraphicsContext3D.h:
            (WebCore::GraphicsContext3D::platformLayer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61648 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/GraphicsContext3D.h
+++ third_party/WebKit/WebCore/platform/graphics/GraphicsContext3D.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GraphicsContext3D_h
 #define GraphicsContext3D_h
 
+#include "GraphicsLayer.h"
 #include "PlatformString.h"
 
 #include <wtf/ListHashSet.h>
@@ -435,12 +436,17 @@ namespace WebCore {
 #elif PLATFORM(CHROMIUM)
         PlatformGraphicsContext3D platformGraphicsContext3D() const;
         Platform3DObject platformTexture() const;
+        // FIXME: Stubbed out for now. Must be implemented to get WebGL working with
+        // accelerated compositing.
+        PlatformLayer* platformLayer() const { return 0; }
 #elif PLATFORM(QT)
         PlatformGraphicsContext3D platformGraphicsContext3D();
         Platform3DObject platformTexture() const;
+        PlatformLayer* platformLayer() const { return 0; }
 #else
         PlatformGraphicsContext3D platformGraphicsContext3D() const { return NullPlatformGraphicsContext3D; }
         Platform3DObject platformTexture() const { return NullPlatform3DObject; }
+        PlatformLayer* platformLayer() const { return 0; }
 #endif
         void makeContextCurrent();
 
