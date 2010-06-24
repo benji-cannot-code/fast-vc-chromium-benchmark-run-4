a95297eea5dd9efd13860ef0d54dc516f876fd7b
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-24 06:41:56 +0000

    2010-06-23  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Add all the HTML5 tree builder insertion modes
            https://bugs.webkit.org/show_bug.cgi?id=41124
    
            These are just copied verbatim from the spec.  No tests because these
            don't do anything yet.
    
            * html/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::HTMLTreeBuilder):
            (WebCore::HTMLTreeBuilder::passTokenToLegacyParser):
            * html/HTMLTreeBuilder.h:
            (WebCore::HTMLTreeBuilder::):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61740 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
@@ -50,7 +50,7 @@ HTMLTreeBuilder::HTMLTreeBuilder(HTMLTokenizer* tokenizer, HTMLDocument* documen
     : m_document(document)
     , m_reportErrors(reportErrors)
     , m_isPaused(false)
-    , m_insertionMode(Initial)
+    , m_insertionMode(InitialMode)
     , m_tokenizer(tokenizer)
     , m_legacyTreeBuilder(new LegacyHTMLTreeBuilder(document, reportErrors))
     , m_lastScriptElementStartLine(uninitializedLineNumberValue)
@@ -65,7 +65,7 @@ HTMLTreeBuilder::HTMLTreeBuilder(HTMLTokenizer* tokenizer, DocumentFragment* fra
     : m_document(fragment->document())
     , m_reportErrors(false) // FIXME: Why not report errors in fragments?
     , m_isPaused(false)
-    , m_insertionMode(Initial)
+    , m_insertionMode(InitialMode)
     , m_tokenizer(tokenizer)
     , m_legacyTreeBuilder(new LegacyHTMLTreeBuilder(fragment, scriptingPermission))
     , m_lastScriptElementStartLine(uninitializedLineNumberValue)
@@ -206,13 +206,13 @@ PassRefPtr<Node> HTMLTreeBuilder::passTokenToLegacyParser(HTMLToken& token)
                     // a DocumentFragment for pasting so that javascript content
                     // does not show up in pasted HTML.
                     m_lastScriptElement->removeChildren();
-                } else if (insertionMode() != AfterFrameset)
+                } else if (insertionMode() != AfterFramesetMode)
                     handleScriptEndTag(m_lastScriptElement.get(), m_lastScriptElementStartLine);
                 m_lastScriptElement = 0;
                 m_lastScriptElementStartLine = uninitializedLineNumberValue;
             }
         } else if (oldStyleToken.tagName == framesetTag)
-            setInsertionMode(AfterFrameset);
+            setInsertionMode(AfterFramesetMode);
     }
     return result.release();
 }
