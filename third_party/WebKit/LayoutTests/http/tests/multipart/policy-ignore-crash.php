80f827264b35bc782ccd9f88d3e778fb50dfff40
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-24 02:54:20 +0000

    2010-06-23  Tony Chang  <tony@chromium.org>
            Reviewed by Darin Fisher.
    
            layout test for a crash in chromium multipart request handling
            https://bugs.webkit.org/show_bug.cgi?id=41050
    
            * http/tests/multipart/policy-ignore-crash-expected.txt: Copied from LayoutTests/editing/pasteboard/copy-crash-with-extraneous-attribute-expected.txt.
            * http/tests/multipart/policy-ignore-crash.php: Added.
            * platform/mac/Skipped:
    2010-06-23  Tony Chang  <tony@chromium.org>
    
            Reviewed by Darin Fisher.
    
            layout test for a crash in chromium multipart request handling
            https://bugs.webkit.org/show_bug.cgi?id=41050
    
            Also roll DEPS to pick up the crash fix.
    
            * DEPS:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/multipart/policy-ignore-crash.php
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header('Content-type: multipart/x-mixed-replace;boundary=asdf');
+?>--asdf
+Content-type: text/html
+
+<p>This test passes if it does not crash.</p>
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+</script>
+
+<?php
+# Add some padding because CFNetwork merges small multipart segments together.
+echo str_pad('', 5000);
+?>
+--asdf
+Content-type: text/random
+
+This chunk doesn't have a content type, which can cause the policy
+for this load to be ignored. This causes the request to be canceled.
+
+<?php
+# Add some padding because CFNetwork merges small multipart segments together.
+echo str_pad('', 5000);
+?>
+
+--asdf--
