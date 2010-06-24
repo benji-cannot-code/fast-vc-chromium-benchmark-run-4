11bd5158ca106ac23f657b149b6b62b461903959
dtseng@chromium.org <dtseng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-24 23:20:08 +0000

    Fix radiobuttons and connect up child widgets again. BUG=none TEST=none Review URL: http://codereview.chromium.org/2806030
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50778 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/button/radio_button.h
+++ views/controls/button/radio_button.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved. Use of this
+// Copyright (c) 2010 The Chromium Authors. All rights reserved. Use of this
 // source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
@@ -25,6 +25,7 @@ class RadioButton : public Checkbox {
   virtual void SetChecked(bool checked);
 
   // Overridden from View:
+  virtual bool GetAccessibleRole(AccessibilityTypes::Role* role);
   virtual View* GetSelectedViewForGroup(int group_id);
   virtual bool IsGroupFocusTraversable() const;
   virtual void OnMouseReleased(const MouseEvent& event, bool canceled);
