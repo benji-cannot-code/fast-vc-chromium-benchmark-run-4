98659a757e68c125e6cbf0eaebb95add7db6d06b
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-24 09:11:04 +0000

    Single character string replacement may replace too many characters https://bugs.webkit.org/show_bug.cgi?id=41138 <rdar://problem/8097496>
    Reviewed by Maciej Stachowiak.
    
    Simple fix to stop the rope path of single character replacement
    once the first replacement occurs.
    
    * runtime/JSString.cpp:
    (JSC::JSString::replaceCharacter):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSString.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSString.cpp
@@ -151,6 +151,7 @@ JSValue JSString::replaceCharacter(ExecState* exec, UChar character, const UStri
         if (replacement.size())
             builder.append(replacement);
         builder.append(UString(string).substr(matchPosition + 1));
+        matchString = 0;
     }
 
     JSGlobalData* globalData = &exec->globalData();
