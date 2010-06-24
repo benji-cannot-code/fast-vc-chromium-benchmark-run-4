f5e045010b2485d2b378f0b7b9188bce594ba015
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-24 22:29:49 +0000

    Revert 50752 - Moves Browser::AddTypes to TabStripModel. This patch is primarily cleanup before I fix 29933, but has a couple of interesting bits beyond the enum change:
    . AddTabContents now supports adding pinned.
    . Nuked duplicate code in Browser::addTabWithURL that invoked
      wasHidden on the TabContents. This code is already in TabStripModel.
    . Moved code for setting visibility of tabcontents from
      TabStripModel::AddTabContents to InsertTabContentsAt. Since everything
      ends up in InsertTabContentsAt it should be there.
    . Converted InsertTabContents call in extensionstabmodule to pass in
      nothing (Rafael said old code was wrong).
    
    BUG=29933
    TEST=none
    
    Review URL: http://codereview.chromium.org/2863021
    
    TBR=sky@chromium.org
    Review URL: http://codereview.chromium.org/2849025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50776 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_strip_controller.mm
+++ chrome/browser/cocoa/tab_strip_controller.mm
@@ -1401,9 +1401,8 @@ private:
 
   // Insert it into this tab strip. We want it in the foreground and to not
   // inherit the current tab's group.
-  tabStripModel_->InsertTabContentsAt(
-      modelIndex, contents,
-      TabStripModel::ADD_SELECTED | (pinned ? TabStripModel::ADD_PINNED : 0));
+  tabStripModel_->InsertTabContentsAt(modelIndex, contents, true, false,
+                                      pinned);
 }
 
 // Called when the tab strip view changes size. As we only registered for
@@ -1642,8 +1641,7 @@ private:
       UserMetrics::RecordAction(UserMetricsAction("Tab_DropURLBetweenTabs"),
                                 browser_->profile());
       browser_->AddTabWithURL(url, GURL(), PageTransition::TYPED, index,
-                              TabStripModel::ADD_SELECTED |
-                                  TabStripModel::ADD_FORCE_INDEX,
+                              Browser::ADD_SELECTED | Browser::ADD_FORCE_INDEX,
                               NULL, std::string());
       break;
     case CURRENT_TAB:
--- chrome/browser/dom_ui/html_dialog_tab_contents_delegate.cc
+++ chrome/browser/dom_ui/html_dialog_tab_contents_delegate.cc
@@ -43,8 +43,7 @@ void HtmlDialogTabContentsDelegate::OpenURLFromTab(
     Browser* browser = CreateBrowser();
     TabContents* new_contents =
         browser->AddTabWithURL(url, referrer, transition, -1,
-                               TabStripModel::ADD_SELECTED, NULL,
-                               std::string());
+                               Browser::ADD_SELECTED, NULL, std::string());
     DCHECK(new_contents);
     browser->window()->Show();
     new_contents->Focus();
