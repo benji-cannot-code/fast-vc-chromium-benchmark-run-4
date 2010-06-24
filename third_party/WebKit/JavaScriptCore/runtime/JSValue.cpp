3d098f5743564717c76a5e0fac336cfc0957e67e
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-24 18:28:06 +0000

    2010-06-21  Nathan Lawrence  <nlawrence@apple.com>
     Reviewed by Geoff Garen.
    
     https://bugs.webkit.org/show_bug.cgi?id=40128
     Fixed broken debug functionality.
    
     * interpreter/Interpreter.cpp:
     (JSC::Interpreter::dumpRegisters):
         Fixed to work with updated call frame.
     * runtime/JSImmediate.h:
     (JSC::JSValue::isCell):
         Added assert for aligned cell.
     * runtime/JSValue.cpp:
     (JSC::JSValue::description):
         Fixed to work with current JSValue implementation.
     * runtime/JSZombie.cpp:
     (JSC::JSZombie::leakedZombieStructure):
         JSombies compile again.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSValue.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSValue.cpp
@@ -127,10 +127,10 @@ char* JSValue::description()
         snprintf(description, size, "False");
     else if (isNull())
         snprintf(description, size, "Null");
-    else {
-        ASSERT(isUndefined());
+    else if (isUndefined())
         snprintf(description, size, "Undefined");
-    }
+    else
+        snprintf(description, size, "INVALID");
 
     return description;
 }
