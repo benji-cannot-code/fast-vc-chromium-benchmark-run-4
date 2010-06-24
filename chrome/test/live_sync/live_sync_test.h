0579640e24d598f07d1cb6e0c82ef31d74a8179a
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-24 19:10:46 +0000

    Bookmark sync tests need to wait for bookmarks to load in SetupClients().
    The bookmark sync tests were not waiting for the BookmarkModel object in each Profile to load before running tests. Adding a call to ui_test_utils::WaitForBookmarkModelToLoad() to LiveBookmarksSyncTest::SetupClients().
    
    Also including fixes for a couple of lint errors.
    
    BUG=47361
    TEST=sync_integration_tests
    
    Review URL: http://codereview.chromium.org/2802019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50748 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/live_sync_test.h
+++ chrome/test/live_sync/live_sync_test.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/mock_host_resolver.h"
 #include "net/socket/ssl_test_util.h"
 
+#include <string>
+
 class Profile;
 class CommandLine;
 
@@ -90,6 +92,9 @@ class LiveSyncTest : public InProcessBrowserTest {
   // Helper to ProfileManager::CreateProfile that handles path creation.
   static Profile* MakeProfile(const FilePath::StringType name);
 
+  // Used to get the number of sync clients used by a test.
+  int num_clients() { return num_clients_; }
+
   // Used to access a particular sync profile.
   Profile* GetProfile(int index);
 
@@ -124,7 +129,7 @@ class LiveSyncTest : public InProcessBrowserTest {
   // GAIA password used by the test case.
   std::string password_;
 
-private:
+ private:
   // Helper method used to create a local python test server.
   virtual void SetUpLocalTestServer();
 
