91690e4f0e99c443ca46248a5d6211494f4929db
mirandac@google.com <mirandac@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-24 15:51:09 +0000

    Tweak the first run search engine dialog so that it reflects Glen's mocks.  This change adds an image to the top of the dialog, and removes the text and link at the bottom.  It also removes the horizontal separators.
    BUG=46716
    TEST=Run Chrome in first-run with master_preferences set to load the new search engine dialog.  It should look reasonable.
    Review URL: http://codereview.chromium.org/2818014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50724 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/first_run_search_engine_view.h
+++ chrome/browser/views/first_run_search_engine_view.h
@@ -11,12 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/views/keyword_editor_view.h"
 #include "gfx/size.h"
 #include "views/controls/button/native_button.h"
-#include "views/controls/link.h"
 #include "views/view.h"
 #include "views/window/window_delegate.h"
 
 namespace views {
 class ButtonListener;
+class ImageView;
 class Label;
 class Separator;
 class Window;
@@ -80,7 +80,6 @@ class SearchEngineSelectionObserver {
 class FirstRunSearchEngineView
     : public views::View,
       public views::ButtonListener,
-      public views::LinkController,
       public views::WindowDelegate,
       public TemplateURLModelObserver {
  public:
@@ -96,10 +95,6 @@ class FirstRunSearchEngineView
   // Overridden from views::View:
   virtual gfx::Size GetPreferredSize();
   virtual void Layout();
-  virtual void Paint(gfx::Canvas* canvas);
-
-  // Overridden from views::LinkActivated:
-  virtual void LinkActivated(views::Link* source, int event_flags);
 
   // Overridden from views::WindowDelegate:
   virtual std::wstring GetWindowTitle() const;
@@ -138,24 +133,12 @@ class FirstRunSearchEngineView
   // If logos are to be displayed in random order.  Used for UX testing.
   bool randomize_;
 
+  // Image of browser search box with grey background and bubble arrow.
+  views::ImageView* background_image_;
+
   // UI elements:
-  // Text above the first horizontal separator
   views::Label* title_label_;
   views::Label* text_label_;
-
-  // Horizontal separators
-  views::Separator* separator_1_;
-  views::Separator* separator_2_;
-
-  // Text below the second horizontal divider. The order of appearance of
-  // these three elements is language-dependent.
-  views::Label* subtext_label_1_;
-  views::Label* subtext_label_2_;
-  views::Link* options_link_;
-
-  // Used to figure out positioning of embedded links in RTL languages
-  // (see view_text_utils::DrawTextAndPositionUrl).
-  views::Label* dummy_subtext_label_;
 
   DISALLOW_COPY_AND_ASSIGN(FirstRunSearchEngineView);
 };
