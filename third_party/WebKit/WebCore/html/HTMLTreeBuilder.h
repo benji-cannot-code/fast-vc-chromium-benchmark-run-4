66b6f3aab1bffa1b57de91efc34bff6e18f9875e
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-24 06:52:42 +0000

    2010-06-23  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Sketch out InitialMode of HTML5 tree builder
            https://bugs.webkit.org/show_bug.cgi?id=41126
    
            Transliterated from the spec.  Currently doesn't do anything.  Mostly
            an experiment to see where this goes.
    
            * html/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::processToken):
            (WebCore::HTMLTreeBuilder::insertDoctype):
            (WebCore::HTMLTreeBuilder::insertComment):
            * html/HTMLTreeBuilder.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61742 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTreeBuilder.h
+++ third_party/WebKit/WebCore/html/HTMLTreeBuilder.h
@@ -100,8 +100,11 @@ private:
     };
 
     PassRefPtr<Node> passTokenToLegacyParser(HTMLToken&);
-    PassRefPtr<Node> processToken(HTMLToken&, UChar currentCharacter = 0);
-    
+    PassRefPtr<Node> processToken(HTMLToken&, UChar cc = 0);
+
+    PassRefPtr<Node> insertDoctype(HTMLToken&);
+    PassRefPtr<Node> insertComment(HTMLToken&);
+
     void handleScriptStartTag();
     void handleScriptEndTag(Element*, int scriptStartLine);
 
