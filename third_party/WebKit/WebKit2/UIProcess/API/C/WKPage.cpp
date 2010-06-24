15df62fcab11811e8aad81e858fc4979cb39551a
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-24 18:11:25 +0000

    Fix for https://bugs.webkit.org/show_bug.cgi?id=41168 <rdar://problem/8124605> Webkit2: Add WKPageReloadFromOrigin() to match old-WebKit functionality
    Reviewed by Anders Carlsson.
    
    * UIProcess/API/C/WKPage.cpp:
    (WKPageReload):
    (WKPageReloadFromOrigin):
    * UIProcess/API/C/WKPage.h:
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::reload):
    * UIProcess/WebPageProxy.h:
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::reload):
    (WebKit::WebPage::didReceiveMessage):
    * WebProcess/WebPage/WebPage.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/C/WKPage.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/C/WKPage.cpp
@@ -53,7 +53,12 @@ void WKPageStopLoading(WKPageRef pageRef)
 
 void WKPageReload(WKPageRef pageRef)
 {
-    toWK(pageRef)->reload();
+    toWK(pageRef)->reload(false);
+}
+
+void WKPageReloadFromOrigin(WKPageRef pageRef)
+{
+    toWK(pageRef)->reload(true);
 }
 
 bool WKPageTryClose(WKPageRef pageRef)
