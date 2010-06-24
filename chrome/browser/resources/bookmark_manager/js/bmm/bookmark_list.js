0b951cf437282e95b98ac9445404ba9903fbe4f6
feldstein@chromium.org <feldstein@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-24 19:48:34 +0000

    Fix BMM paste on mac and list hover artifact.
    The Bookamark manager on mac would paste a url into a bookmarks url field, and
    then the same paste event would propagate up to the list and paste the page as a
    new bookmark.  This stops propagation of paste events from the text fields in
    the list.
    
    Also, now that the list has spacers for the dynamic rendering, the spacers would
    get a 2 pixel hover effect.  This removes that.  You can see it by slowly moving
    from the last bookmark down to below the bottom of the list.
    BUG=43442
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/2805031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50758 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/bookmark_manager/js/bmm/bookmark_list.js
+++ chrome/browser/resources/bookmark_manager/js/bmm/bookmark_list.js
@@ -477,7 +477,8 @@ cr.define('bmm', function() {
           e.stopPropagation();
         }
 
-        var eventsToStop = ['mousedown', 'mouseup', 'contextmenu', 'dblclick'];
+        var eventsToStop =
+            ['mousedown', 'mouseup', 'contextmenu', 'dblclick', 'paste'];
         eventsToStop.forEach(function(type) {
           labelInput.addEventListener(type, stopPropagation);
         });
