916ac85675194632a0ae19c8e5e6c013732fe2ac
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-25 12:59:43 +0000

    2010-06-25  Michael Nordman  <michaeln@google.com>
            Reviewed by Dumitru Daniliuc.
    
            Additional API to pass additional info needed for AppCache progress events.
            https://bugs.webkit.org/show_bug.cgi?id=37602
    
            Also, delete an obsolete method from the worker API.
    
            * public/WebApplicationCacheHostClient.h: define a new method
            * public/WebSharedWorker.h: remove an obsolte method
            * src/ApplicationCacheHostInternal.h: implement the new method
            (WebCore::ApplicationCacheHostInternal::notifyProgressEventListener):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61856 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/ApplicationCacheHostInternal.h
+++ third_party/WebKit/WebKit/chromium/src/ApplicationCacheHostInternal.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFrameImpl.h"
 #include "WebKit.h"
 #include "WebKitClient.h"
+#include "WebURL.h"
 
 namespace WebCore {
 
@@ -56,6 +57,17 @@ public:
     virtual void notifyEventListener(WebKit::WebApplicationCacheHost::EventID eventID)
     {
         m_innerHost->notifyDOMApplicationCache(static_cast<ApplicationCacheHost::EventID>(eventID));
+    }
+
+    virtual void notifyProgressEventListener(const WebKit::WebURL&, int num_total, int num_complete) 
+    {
+        // FIXME: Modify webcore's progress event handling to carry the extra info and alter the
+        // layout tests to not fail when the more recently specified 'final' event is raised.
+        // For now, we're eating the extra info and that last event.
+        // See https://bugs.webkit.org/show_bug.cgi?id=37602
+        if (num_complete == num_total)
+            return;
+        notifyEventListener(WebKit::WebApplicationCacheHost::ProgressEvent);
     }
 
     static WebKit::WebApplicationCacheHost* toWebApplicationCacheHost(ApplicationCacheHost* innerHost)
