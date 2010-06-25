ec696e7a2a61535020af2f18f368a56efc7eae11
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-25 18:25:31 +0000

    WKBundlePageRenderTreeExternalRepresentation really needs to be WKBundlePageCopyRenderTreeExternalRepresentation so we don't have dangling pointers and adhere to naming conventions.
    Reviewed by Anders Carlsson.
    
    * WebProcess/InjectedBundle/API/c/WKBundlePage.cpp:
    (WKBundlePageCopyRenderTreeExternalRepresentation):
    * WebProcess/InjectedBundle/API/c/WKBundlePagePrivate.h:
    * mac/WebKit2.exp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61876 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundlePagePrivate.h
+++ third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundlePagePrivate.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 extern "C" {
 #endif
 
-WK_EXPORT WKStringRef WKBundlePageRenderTreeExternalRepresentation(WKBundlePageRef page);
+WK_EXPORT WKStringRef WKBundlePageCopyRenderTreeExternalRepresentation(WKBundlePageRef page);
 
 #ifdef __cplusplus
 }
