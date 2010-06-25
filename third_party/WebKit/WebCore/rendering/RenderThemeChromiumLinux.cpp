9f827251cfa854906b3ee6412885a7567258346a
estade@chromium.org <estade@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-25 22:09:41 +0000

    [chromium linux] Improve look of scrollbars https://bugs.webkit.org/show_bug.cgi?id=35775
    Reviewed by Darin Fisher.
    
    This will affect layout pixel tests, which will need to be rebaselined.
    
    * platform/Scrollbar.cpp: need the buttons to be invalidated when the
    thumb moves as they paint differently if the thumb is at the top or
    bottom
    (WebCore::Scrollbar::updateThumbPosition):
    (WebCore::Scrollbar::updateThumbProportion):
    * platform/chromium/ScrollbarThemeChromium.cpp:
    (WebCore::ScrollbarThemeChromium::trackRect): share code between
    linux/windows
    * platform/chromium/ScrollbarThemeChromiumLinux.cpp:
    (WebCore::ScrollbarThemeChromiumLinux::scrollbarThickness):
    (WebCore::outlineColor): up the contrast
    (WebCore::ScrollbarThemeChromiumLinux::paintButton): add custom button
    drawing implementation
    (WebCore::ScrollbarThemeChromiumLinux::buttonSize): add buttons
    * platform/chromium/ScrollbarThemeChromiumWin.cpp: share code between
    linux/windows
    * rendering/RenderThemeChromiumLinux.cpp: improve stock colors to get
    better contrast and integration with stock chrome theme
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61908 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderThemeChromiumLinux.cpp
+++ third_party/WebKit/WebCore/rendering/RenderThemeChromiumLinux.cpp
@@ -33,9 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-unsigned RenderThemeChromiumLinux::m_thumbInactiveColor = 0xf0ebe5;
-unsigned RenderThemeChromiumLinux::m_thumbActiveColor = 0xfaf8f5;
-unsigned RenderThemeChromiumLinux::m_trackColor = 0xe3ddd8;
+unsigned RenderThemeChromiumLinux::m_thumbInactiveColor = 0xeaeaea;
+unsigned RenderThemeChromiumLinux::m_thumbActiveColor = 0xf4f4f4;
+unsigned RenderThemeChromiumLinux::m_trackColor = 0xd3d3d3;
 unsigned RenderThemeChromiumLinux::m_activeSelectionBackgroundColor =
     0xff1e90ff;
 unsigned RenderThemeChromiumLinux::m_activeSelectionForegroundColor =
