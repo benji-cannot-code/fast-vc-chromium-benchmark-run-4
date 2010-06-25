6edd88c385d270b3e3f7575803e92f966e60bb11
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-25 21:29:10 +0000

    Make a new yes/no messagebox wrapper function, use it in the bookmark alert.
    Recommitting of r46101.
    
    BUG=http://crbug.com/34481; http://crbug.com/40011
    TEST=on Mac/ChromeOS, have at least 15 bookmarks on bookmarks bar or other bookmarks,
    right click on one of those and click on Open all bookmarks. A confirmation
    dialog box should be shown. No other visible change.
    
    Review URL: http://codereview.chromium.org/1745024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50894 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/process_singleton_win.cc
+++ chrome/browser/process_singleton_win.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win_util.h"
 #include "chrome/browser/browser_init.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/platform_util.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/profile_manager.h"
 #include "chrome/common/chrome_constants.h"
@@ -144,8 +145,7 @@ ProcessSingleton::NotifyResult ProcessSingleton::NotifyOtherProcess() {
   if (visible_window) {
     std::wstring text = l10n_util::GetString(IDS_BROWSER_HUNGBROWSER_MESSAGE);
     std::wstring caption = l10n_util::GetString(IDS_PRODUCT_NAME);
-    if (IDYES != win_util::MessageBox(NULL, text, caption,
-                                      MB_YESNO | MB_ICONSTOP | MB_TOPMOST)) {
+    if (!platform_util::SimpleYesNoBox(NULL, caption, text)) {
       // The user denied. Quit silently.
       return PROCESS_NOTIFIED;
     }
--- chrome/browser/views/bookmark_bar_view_test.cc
+++ chrome/browser/views/bookmark_bar_view_test.cc
@@ -35,8 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // See http://crbug.com/40039 for details.
 #define MAYBE_KeyEvents DISABLED_KeyEvents
 
-// See http://crbug.com/47089 for details.
+// Two bugs here. http://crbug.com/47089 for general Linux Views, and
+// http://crbug.com/47452 for ChromiumOS.
 #define MAYBE_CloseWithModalDialog DISABLED_CloseWithModalDialog
+// See http://crbug.com/47089 for details.
 #define MAYBE_CloseMenuAfterClosingContextMenu \
         DISABLED_CloseMenuAfterClosingContextMenu
 #else
