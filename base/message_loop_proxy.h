ae1b2656c6816c105c34e53b8b8ea29d2c0332e6
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-25 23:20:29 +0000

    Add virtual to some base classes that have virtual methods
    This is defensive coding to avoid memory leaks.
    
    bug=47469
    r=wtc
    Review URL: http://codereview.chromium.org/2869024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50907 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop_proxy.h
+++ base/message_loop_proxy.h
@@ -56,6 +56,9 @@ class MessageLoopProxy
 
  protected:
   friend struct MessageLoopProxyTraits;
+
+  virtual ~MessageLoopProxy() { }
+
   // Called when the proxy is about to be deleted. Subclasses can override this
   // to provide deletion on specific threads.
   virtual void OnDestruct() {
