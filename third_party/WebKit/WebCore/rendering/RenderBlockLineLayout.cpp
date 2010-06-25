0688e46ae1986940839c122f714a0e8ceb2ca871
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-25 10:03:33 +0000

    2010-06-25  Nikolas Zimmermann  <nzimmermann@rim.com>
            Reviewed by Rob Buis.
    
            Split up isSVGText() in isSVGText() / isSVGInlineText()
            https://bugs.webkit.org/show_bug.cgi?id=41204
    
            Introduce isSVGInlineText(), to provide a safe mechanism to differentiate between RenderSVGText / RenderSVGInlineText.
            No change in functionality, thus no new tests.
    
            * rendering/RenderBlockLineLayout.cpp:
            (WebCore::shouldPreserveNewline): s/isSVGText/isSVGInlineText/
            (WebCore::RenderBlock::findNextLineBreak): Ditto.
            * rendering/RenderObject.h:
            (WebCore::RenderObject::isSVGInlineText): Return false, by default.
            * rendering/RenderSVGInlineText.h:
            (WebCore::RenderSVGInlineText::isSVGInlineText): Return true here, don't return true anymore for isSVGText().
            * rendering/RenderText.cpp:
            (WebCore::RenderText::setTextInternal): s/isSVGText/isSVGInlineText/
            * rendering/RenderTreeAsText.cpp: Remove hack, now that we can properly differentiate between RenderSVGText / RenderSVGInlineText.
            (WebCore::write):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61844 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBlockLineLayout.cpp
+++ third_party/WebKit/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -1162,7 +1162,7 @@ static inline bool shouldCollapseWhiteSpace(const RenderStyle* style, bool isLin
 static inline bool shouldPreserveNewline(RenderObject* object)
 {
 #if ENABLE(SVG)
-    if (object->isSVGText())
+    if (object->isSVGInlineText())
         return false;
 #endif
 
@@ -1414,7 +1414,7 @@ InlineIterator RenderBlock::findNextLineBreak(InlineBidiResolver& resolver, bool
         autoWrapWasEverTrueOnLine = autoWrapWasEverTrueOnLine || autoWrap;
 
 #if ENABLE(SVG)
-        bool preserveNewline = o->isSVGText() ? false : RenderStyle::preserveNewline(currWS);
+        bool preserveNewline = o->isSVGInlineText() ? false : RenderStyle::preserveNewline(currWS);
 #else
         bool preserveNewline = RenderStyle::preserveNewline(currWS);
 #endif
