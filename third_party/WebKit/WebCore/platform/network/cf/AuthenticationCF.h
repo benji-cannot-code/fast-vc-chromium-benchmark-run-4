9d53c84b774a155759af000c8bb6a3bef5df86b1
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-25 00:24:07 +0000

    Windows build fix.
    WebCore: 
    
    * platform/network/cf/AuthenticationCF.h:
    
    WebKit2: 
    
    * win/WebKit2WebProcess.vcproj:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/AuthenticationCF.h
+++ third_party/WebKit/WebCore/platform/network/cf/AuthenticationCF.h
@@ -27,8 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef AuthenticationCF_h
 #define AuthenticationCF_h
 
+#include <CFNetwork/CFURLCredentialPriv.h>
+
 typedef struct _CFURLAuthChallenge* CFURLAuthChallengeRef;
-typedef struct _CFURLCredential* CFURLCredentialRef;
 typedef struct _CFURLProtectionSpace* CFURLProtectionSpaceRef;
 
 namespace WebCore {
