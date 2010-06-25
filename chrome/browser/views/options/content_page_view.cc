f888e2670745e47a34f172704430f5c1f4f39c4f
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-25 04:49:51 +0000

    Revert r50774 as it introduces valgrind failures
    TBR=dantasse@chromium.org
    TEST=Valgrind bot goes green
    BUG=none
    
    Review URL: http://codereview.chromium.org/2827025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50820 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/options/content_page_view.cc
+++ chrome/browser/views/options/content_page_view.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/importer/importer_data_types.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/sync/sync_ui_util.h"
-#include "chrome/browser/sync/sync_setup_wizard.h"
 #include "chrome/browser/views/importer_view.h"
 #include "chrome/browser/views/options/customize_sync_window_view.h"
 #include "chrome/browser/views/options/options_group_view.h"
@@ -159,7 +158,7 @@ void ContentPageView::ButtonPressed(
   } else if (sender == sync_customize_button_) {
     // sync_customize_button_ should be invisible if sync is not yet set up.
     DCHECK(sync_service_->HasSyncSetupCompleted());
-    sync_service_->ShowChooseDataTypes();
+    CustomizeSyncWindowView::Show(GetWindow()->GetNativeWindow(), profile());
   }
 }
 
