45b8191d65d27a3d141827b0f0f9cd91f6dd9da5
robert@webkit.org <robert@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-26 10:10:59 +0000

    2010-06-26  Robert Hogan  <robert@webkit.org>
            Unreviewed, Qt build fix following http://trac.webkit.org/changeset/61879
    
            Remove qwebscriptworld.h from LayoutTestControllerQt.cpp - it is no longer
            exported.
    
            * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/qt/LayoutTestControllerQt.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/qt/LayoutTestControllerQt.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WorkQueueItemQt.h"
 #include <QDir>
 #include <QLocale>
-#include <qwebscriptworld.h>
 #include <qwebsettings.h>
 
 LayoutTestController::LayoutTestController(WebCore::DumpRenderTree* drt)
