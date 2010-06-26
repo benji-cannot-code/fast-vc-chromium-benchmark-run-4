af64d9c8b2617d9fe024d0dbd1d38f3ca8036672
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-26 20:48:06 +0000

    * html/HTMLFormElement.h: Removed stray character that got in here before landing.
    git-svn-id: svn://svn.chromium.org/blink/trunk@61960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLFormElement.h
+++ third_party/WebKit/WebCore/html/HTMLFormElement.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-r/*
+/*
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2000 Dirk Mueller (mueller@kde.org)
