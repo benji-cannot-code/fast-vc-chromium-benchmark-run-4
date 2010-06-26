8e8223b73aeebc4d21c47f027e5bf00f0fe072df
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-26 05:51:03 +0000

    2010-06-25  Bernhard Bauer  <bauerb@chromium.org>
            Reviewed by Darin Fisher.
    
            Add plugin accessors to WebPluginContainer.
            https://bugs.webkit.org/show_bug.cgi?id=41145
    
            * public/WebPluginContainer.h:
            * src/WebPluginContainerImpl.h:
            (WebKit::WebPluginContainerImpl::setPlugin):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61942 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebPluginContainer.h
+++ third_party/WebKit/WebKit/chromium/public/WebPluginContainer.h
@@ -37,6 +37,7 @@ struct NPObject;
 namespace WebKit {
 
 class WebElement;
+class WebPlugin;
 class WebString;
 class WebURL;
 class WebURLRequest;
@@ -77,6 +78,9 @@ public:
     // the callback.
     virtual void loadFrameRequest(
         const WebURLRequest&, const WebString& target, bool notifyNeeded, void* notifyData) = 0;
+
+    virtual WebPlugin* plugin() = 0;
+    virtual void setPlugin(WebPlugin*) = 0;
 
 protected:
     ~WebPluginContainer() { }
