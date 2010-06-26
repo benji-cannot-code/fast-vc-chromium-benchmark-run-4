96d737b4efe24cbe352a9484209491c2b84e16c9
jcivelli@chromium.org <jcivelli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-26 00:06:11 +0000

    TranslateInfoBarDelegate2 were leaked. (Yikes!) This revealed that the NormalTranslate unit-test was incorrect (it was not testing that changing the source or target language from the after translate infobar causes a translation). This also revealed that returning reference string in the delegate is not a brilliant idea as when we switch infobars, the new infobar might be using a string from the old infobar that might have been deleted.
    BUG=None
    TEST=Linux heap check bot should be green.
    
    Review URL: http://codereview.chromium.org/2822032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50913 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_infobar_delegate2.h
+++ chrome/browser/translate/translate_infobar_delegate2.h
@@ -50,10 +50,10 @@ class TranslateInfoBarDelegate2 : public InfoBarDelegate {
   int GetLanguageCount() const;
 
   // Returns the ISO code for the language at |index|.
-  const std::string& GetLanguageCodeAt(int index) const;
+  std::string GetLanguageCodeAt(int index) const;
 
   // Returns the displayable name for the language at |index|.
-  const string16& GetLanguageDisplayableNameAt(int index) const;
+  string16 GetLanguageDisplayableNameAt(int index) const;
 
   TabContents* tab_contents() const { return tab_contents_; }
 
@@ -63,8 +63,8 @@ class TranslateInfoBarDelegate2 : public InfoBarDelegate {
   int target_language_index() const { return target_language_index_; }
 
   // Convenience methods.
-  const std::string& GetOriginalLanguageCode() const;
-  const std::string& GetTargetLanguageCode() const;
+  std::string GetOriginalLanguageCode() const;
+  std::string GetTargetLanguageCode() const;
 
   // Called by the InfoBar to notify that the original/target language has
   // changed and is now the language at |language_index|.
@@ -90,7 +90,8 @@ class TranslateInfoBarDelegate2 : public InfoBarDelegate {
 
   // InfoBarDelegate implementation:
   virtual InfoBar* CreateInfoBar();
-  void InfoBarDismissed();
+  virtual void InfoBarDismissed();
+  virtual void InfoBarClosed();
   virtual SkBitmap* GetIcon() const;
   virtual InfoBarDelegate::Type GetInfoBarType();
   virtual TranslateInfoBarDelegate2* AsTranslateInfoBarDelegate2() {
