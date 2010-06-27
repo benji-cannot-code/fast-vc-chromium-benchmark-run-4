06557bcd615bda57c0c7cc5c51f04177a5fba501
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-27 08:38:22 +0000

    2010-06-27  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            HTML5 tree builder should be able to execute inline scripts
            https://bugs.webkit.org/show_bug.cgi?id=41257
    
            This patch implements enough machinery so that we can execute inline
            scripts in extremely simple documents such as the following:
    
            <html>
            <script>
            alert(1);
            </script>
    
            To get this to work, I had to flesh out a surprising amount of the data
            structures for processing the <head>.  No tests because this is
            already covered by most LayoutTests.
    
            * html/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::HTMLTreeBuilder):
            (WebCore::HTMLTreeBuilder::constructTreeFromToken):
            (WebCore::HTMLTreeBuilder::processStartTag):
            (WebCore::HTMLTreeBuilder::processEndTag):
            (WebCore::HTMLTreeBuilder::processCharacter):
            (WebCore::HTMLTreeBuilder::insertComment):
            (WebCore::HTMLTreeBuilder::insertElement):
            (WebCore::HTMLTreeBuilder::insertScriptElement):
            * html/HTMLTreeBuilder.h:
            (WebCore::HTMLTreeBuilder::ElementRecord::ElementRecord):
            (WebCore::HTMLTreeBuilder::ElementRecord::element):
            (WebCore::HTMLTreeBuilder::ElementRecord::next):
            (WebCore::HTMLTreeBuilder::ElementRecord::releaseNext):
            (WebCore::HTMLTreeBuilder::ElementRecord::setNext):
            (WebCore::HTMLTreeBuilder::ElementStack::pop):
            (WebCore::HTMLTreeBuilder::ElementStack::push):
            (WebCore::HTMLTreeBuilder::ElementStack::top):
            (WebCore::HTMLTreeBuilder::ElementStack::remove):
            (WebCore::HTMLTreeBuilder::currentElement):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
@@ -27,9 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "HTMLTreeBuilder.h"
 
+#include "Comment.h"
 #include "DocumentFragment.h"
 #include "Element.h"
 #include "Frame.h"
+#include "HTMLElementFactory.h"
+#include "HTMLScriptElement.h"
 #include "HTMLTokenizer.h"
 #include "HTMLToken.h"
 #include "HTMLDocument.h"
@@ -39,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LegacyHTMLTreeBuilder.h"
 #include "NotImplemented.h"
 #include "ScriptController.h"
+#include "Text.h"
 #include <wtf/UnusedParam.h>
 
 namespace WebCore {
@@ -62,6 +66,7 @@ HTMLTreeBuilder::HTMLTreeBuilder(HTMLTokenizer* tokenizer, HTMLDocument* documen
     , m_reportErrors(reportErrors)
     , m_isPaused(false)
     , m_insertionMode(InitialMode)
+    , m_originalInsertionMode(InitialMode)
     , m_tokenizer(tokenizer)
     , m_legacyTreeBuilder(new LegacyHTMLTreeBuilder(document, reportErrors))
     , m_lastScriptElementStartLine(uninitializedLineNumberValue)
@@ -78,6 +83,7 @@ HTMLTreeBuilder::HTMLTreeBuilder(HTMLTokenizer* tokenizer, DocumentFragment* fra
     , m_reportErrors(false) // FIXME: Why not report errors in fragments?
     , m_isPaused(false)
     , m_insertionMode(InitialMode)
+    , m_originalInsertionMode(InitialMode)
     , m_tokenizer(tokenizer)
     , m_legacyTreeBuilder(new LegacyHTMLTreeBuilder(fragment, scriptingPermission))
     , m_lastScriptElementStartLine(uninitializedLineNumberValue)
@@ -302,7 +308,8 @@ void HTMLTreeBuilder::processStartTag(AtomicHTMLToken& token)
             return;
         }
         if (token.name() == headTag) {
-            m_headElement = insertElement(token);
+            insertElement(token);
+            m_headElement = currentElement();
             setInsertionMode(InHeadMode);
             return;
         }
@@ -461,9 +468,9 @@ void HTMLTreeBuilder::processEndTag(AtomicHTMLToken& token)
     case InHeadNoscriptMode:
         ASSERT(insertionMode() == InHeadNoscriptMode);
         if (token.name() == noscriptTag) {
-            ASSERT(m_openElements.top()->tagQName() == noscriptTag);
+            ASSERT(currentElement()->tagQName() == noscriptTag);
             m_openElements.pop();
-            ASSERT(m_openElements.top()->tagQName() == headTag);
+            ASSERT(currentElement()->tagQName() == headTag);
             setInsertionMode(InHeadMode);
             return;
         }
@@ -473,6 +480,18 @@ void HTMLTreeBuilder::processEndTag(AtomicHTMLToken& token)
         }
         processDefaultForInHeadNoscriptMode(token);
         processToken(token);
+    case TextMode:
+        if (token.name() == scriptTag) {
+            // Pause ourselves so that parsing stops until the script can be processed by the caller.
+            m_isPaused = true;
+            ASSERT(currentElement()->tagQName() == scriptTag);
+            m_scriptToProcess = currentElement();
+            m_openElements.pop();
+            m_insertionMode = m_originalInsertionMode;
+            return;
+        }
+        notImplemented();
+        break;
     default:
         notImplemented();
     }
@@ -487,8 +506,12 @@ void HTMLTreeBuilder::processComment(AtomicHTMLToken& token)
     insertComment(token);
 }
 
-void HTMLTreeBuilder::processCharacter(AtomicHTMLToken&)
+void HTMLTreeBuilder::processCharacter(AtomicHTMLToken& token)
 {
+    if (insertionMode() == TextMode) {
+        currentElement()->addChild(Text::create(m_document, token.characters()));
+        return;
+    }
     // FIXME: We need to figure out how to handle each character individually.
     notImplemented();
 }
@@ -575,13 +598,17 @@ void HTMLTreeBuilder::insertDoctype(AtomicHTMLToken& token)
 
 void HTMLTreeBuilder::insertComment(AtomicHTMLToken& token)
 {
-    ASSERT_UNUSED(token, token.type() == HTMLToken::Comment);
+    ASSERT(token.type() == HTMLToken::Comment);
+    RefPtr<Node> element = Comment::create(m_document, token.comment());
+    currentElement()->addChild(element);
 }
 
-PassRefPtr<Element> HTMLTreeBuilder::insertElement(AtomicHTMLToken& token)
+void HTMLTreeBuilder::insertElement(AtomicHTMLToken& token)
 {
-    ASSERT_UNUSED(token, token.type() == HTMLToken::StartTag);
-    return 0;
+    ASSERT(token.type() == HTMLToken::StartTag);
+    RefPtr<Element> element = HTMLElementFactory::createHTMLElement(QualifiedName(nullAtom, token.name(), xhtmlNamespaceURI), m_document, 0);
+    currentElement()->addChild(element);
+    m_openElements.push(element.release());
 }
 
 void HTMLTreeBuilder::insertCharacter(UChar cc)
@@ -602,6 +629,12 @@ void HTMLTreeBuilder::insertGenericRawTextElement(AtomicHTMLToken& token)
 void HTMLTreeBuilder::insertScriptElement(AtomicHTMLToken& token)
 {
     ASSERT_UNUSED(token, token.type() == HTMLToken::StartTag);
+    RefPtr<HTMLScriptElement> element = HTMLScriptElement::create(scriptTag, m_document, true);
+    currentElement()->addChild(element);
+    m_openElements.push(element.release());
+    m_tokenizer->setState(HTMLTokenizer::ScriptDataState);
+    m_originalInsertionMode = m_insertionMode;
+    m_insertionMode = TextMode;
 }
 
 void HTMLTreeBuilder::finished()
