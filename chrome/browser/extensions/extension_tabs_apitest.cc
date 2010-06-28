586eb1b9272e4fe5a8a02e18f4c02c16b77e3672
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-28 22:10:40 +0000

    Remove #define cruft around ExtensionApiTest.TabOnRemoved.
    ExtensionApiTest.TabOnRemoved was disabled on Chrome OS.  I fixed the issue a while ago, but did not remove some #defines.
    
    BUG=43440
    TEST=ExtensionApiTest.TabOnRemoved
    
    Review URL: http://codereview.chromium.org/2872018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51057 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -33,13 +33,6 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, TabConnect) {
   ASSERT_TRUE(RunExtensionTest("tabs/connect")) << message_;
 }
 
-#if defined(TOOLKIT_VIEWS) && defined(OS_LINUX)
-// The way ChromeOs deals with browser windows breaks this test.
-// http://crbug.com/43440.
-#define MAYBE_TabOnRemoved DISABLED_TabOnRemoved
-#else
-#define MAYBE_TabOnRemoved TabOnRemoved
-#endif
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, TabOnRemoved) {
   ASSERT_TRUE(StartHTTPServer());
   ASSERT_TRUE(RunExtensionTest("tabs/on_removed")) << message_;
