a6f3c09850d196191e3df0feb47d144014f4e207
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-28 08:15:58 +0000

    2010-06-28  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Add support for parsing attributes
            https://bugs.webkit.org/show_bug.cgi?id=41272
    
            This patch lets use parse the following document:
    
            <script src="data:text/javascript,alert('PASS')"></script>
    
            * html/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::constructTreeFromToken):
            (WebCore::HTMLTreeBuilder::insertHTMLStartTagBeforeHTML):
            (WebCore::HTMLTreeBuilder::insertScriptElement):
            (WebCore::HTMLTreeBuilder::createElement):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@61986 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
@@ -273,10 +273,10 @@ void HTMLTreeBuilder::processDoctypeToken(AtomicHTMLToken& token)
     parseError(token);
 }
 
-void HTMLTreeBuilder::insertHTMLStartTagBeforeHTML(AtomicHTMLToken&)
+void HTMLTreeBuilder::insertHTMLStartTagBeforeHTML(AtomicHTMLToken& token)
 {
     RefPtr<Element> element = HTMLHtmlElement::create(m_document);
-    // FIXME: Add attributes to |element|.
+    element->setAttributeMap(token.attributes(), m_fragmentScriptingPermission);
     m_document->addChild(element);
     m_openElements.push(element.release());
 }
@@ -840,6 +840,7 @@ void HTMLTreeBuilder::insertScriptElement(AtomicHTMLToken& token)
 {
     ASSERT_UNUSED(token, token.type() == HTMLToken::StartTag);
     RefPtr<HTMLScriptElement> element = HTMLScriptElement::create(scriptTag, m_document, true);
+    element->setAttributeMap(token.attributes(), m_fragmentScriptingPermission);
     currentElement()->addChild(element);
     m_openElements.push(element.release());
     m_tokenizer->setState(HTMLTokenizer::ScriptDataState);
@@ -849,7 +850,9 @@ void HTMLTreeBuilder::insertScriptElement(AtomicHTMLToken& token)
 
 PassRefPtr<Element> HTMLTreeBuilder::createElement(AtomicHTMLToken& token)
 {
-    return HTMLElementFactory::createHTMLElement(QualifiedName(nullAtom, token.name(), xhtmlNamespaceURI), m_document, 0);
+    RefPtr<Element> element = HTMLElementFactory::createHTMLElement(QualifiedName(nullAtom, token.name(), xhtmlNamespaceURI), m_document, 0);
+    element->setAttributeMap(token.attributes(), m_fragmentScriptingPermission);
+    return element.release();
 }
 
 void HTMLTreeBuilder::reconstructTheActiveFormattingElements()
