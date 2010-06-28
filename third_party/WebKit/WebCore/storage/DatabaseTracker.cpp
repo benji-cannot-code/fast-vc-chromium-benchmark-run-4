cfa0fa27eed7b69895efedcbb7d933620dd5bb1f
dumi@chromium.org <dumi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-28 22:36:02 +0000

    Fix a race condition that can happen when using DBs in workers. https://bugs.webkit.org/show_bug.cgi?id=41105
    Reviewed by Eric Seidel.
    
    * storage/DatabaseTracker.cpp:
    (WebCore::DatabaseTracker::addOpenDatabase):
    * storage/DatabaseTracker.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62043 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/DatabaseTracker.cpp
+++ third_party/WebKit/WebCore/storage/DatabaseTracker.cpp
@@ -507,6 +507,10 @@ void DatabaseTracker::addOpenDatabase(AbstractDatabase* database)
         databaseSet->add(database);
 
         LOG(StorageAPI, "Added open Database %s (%p)\n", database->stringIdentifier().ascii().data(), database);
+
+        Locker<OriginQuotaManager> quotaManagerLocker(originQuotaManager());
+        if (!originQuotaManager().tracksOrigin(database->securityOrigin()))
+            originQuotaManager().trackOrigin(database->securityOrigin());
     }
 
     MutexLocker lockDatabase(m_databaseGuard);
@@ -554,10 +558,10 @@ void DatabaseTracker::removeOpenDatabase(AbstractDatabase* database)
 
         m_openDatabaseMap->remove(database->securityOrigin());
         delete nameMap;
-    }
 
-    Locker<OriginQuotaManager> quotaManagerLocker(originQuotaManager());
-    originQuotaManager().removeOrigin(database->securityOrigin());
+        Locker<OriginQuotaManager> quotaManagerLocker(originQuotaManager());
+        originQuotaManager().removeOrigin(database->securityOrigin());
+    }
 }
 
 void DatabaseTracker::getOpenDatabases(SecurityOrigin* origin, const String& name, HashSet<RefPtr<AbstractDatabase> >* databases)
