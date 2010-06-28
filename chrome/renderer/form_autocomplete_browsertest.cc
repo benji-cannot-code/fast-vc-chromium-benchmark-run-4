2a214c3b2474bce39fad3afd32b4e3dd54629afe
suzhe@chromium.org <suzhe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-28 22:59:57 +0000

    Fix a build failure caused by the patch of https://bugs.webkit.org/show_bug.cgi?id=41244
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2860033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51063 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/form_autocomplete_browsertest.cc
+++ chrome/renderer/form_autocomplete_browsertest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/form_data.h"
 
 using webkit_glue::FormData;
-using WebKit::WebCompositionCommand;
 using WebKit::WebFrame;
 using WebKit::WebString;
 using WebKit::WebTextDirection;
