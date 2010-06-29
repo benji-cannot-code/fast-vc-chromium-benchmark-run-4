acefa58db2053c51fbe36ab0f2c7a5169f1d8770
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-29 20:45:25 +0000

    Protect NetworkScreen::OnDialogCancelled() from view() == NULL BUG=chromium-os:4294 (http://code.google.com/p/chromium-os/issues/detail?id=4294) TEST=unable to repro, likely an edge case
    Review URL: http://codereview.chromium.org/2809043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/network_screen.cc
+++ chrome/browser/chromeos/login/network_screen.cc
@@ -179,8 +179,10 @@ void NetworkScreen::OnDialogAccepted() {
 }
 
 void NetworkScreen::OnDialogCancelled() {
-  view()->EnableContinue(false);
-  view()->SetSelectedNetworkItem(0);
+  if (view()) {
+    view()->EnableContinue(false);
+    view()->SetSelectedNetworkItem(0);
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
