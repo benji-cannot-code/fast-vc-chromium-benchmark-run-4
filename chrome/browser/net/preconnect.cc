38b009d0c335fd3ea2089a724fd684b22ed16ad7
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-29 20:50:48 +0000

    Fix the construction of the pool names for preconnect.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2842033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51163 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/preconnect.cc
+++ chrome/browser/net/preconnect.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/common/net/url_request_context_getter.h"
+#include "net/base/host_port_pair.h"
 #include "net/http/http_network_session.h"
 #include "net/http/http_transaction_factory.h"
 #include "net/url_request/url_request_context.h"
@@ -55,8 +56,13 @@ void Preconnect::PreconnectOnIOThread(const GURL& url) {
   if (!callback_instance_)
     callback_instance_ = new Preconnect;
 
+  net::HostPortPair endpoint(url.host(), url.EffectiveIntPort());
+  std::string group_name = endpoint.ToString();
+  if (url.SchemeIs("https"))
+    group_name = StringPrintf("ssl/%s", group_name.c_str());
+
   // TODO(jar): This does not handle proxies currently.
-  handle.Init(url.spec(), params, net::LOWEST,
+  handle.Init(group_name, params, net::LOWEST,
               callback_instance_, pool, net::BoundNetLog());
   handle.Reset();
 }
