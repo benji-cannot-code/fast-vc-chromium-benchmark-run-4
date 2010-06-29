e98b0fdd9457a35b103716c508b7dda99838a936
zmo@google.com <zmo@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-29 22:22:16 +0000

    2010-06-29  Zhenyao Mo  <zmo@google.com>
            Reviewed by Dimitri Glazkov.
    
            uniformmatrix* should generate INVALID_VALUE with transpose = true
            https://bugs.webkit.org/show_bug.cgi?id=41235
    
            * fast/canvas/webgl/gl-uniformmatrix4fv-expected.txt: Added.
            * fast/canvas/webgl/gl-uniformmatrix4fv.html: Added.
    2010-06-29  Zhenyao Mo  <zmo@google.com>
    
            Reviewed by Dimitri Glazkov.
    
            uniformmatrix* should generate INVALID_VALUE with transpose = true
            https://bugs.webkit.org/show_bug.cgi?id=41235
    
            Test: fast/canvas/webgl/gl-uniformmatrix4fv.html
    
            * html/canvas/WebGLRenderingContext.cpp:
            (WebCore::WebGLRenderingContext::uniformMatrix2fv): Call validateUniformMatrixParameters instead.
            (WebCore::WebGLRenderingContext::uniformMatrix3fv): Ditto.
            (WebCore::WebGLRenderingContext::uniformMatrix4fv): Ditto.
            (WebCore::WebGLRenderingContext::validateUniformMatrixParameters): Validate input parameters for uniformMatrix*().
            * html/canvas/WebGLRenderingContext.h: Declare validateUniformMatrixParameters().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62150 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/WebGLRenderingContext.h
+++ third_party/WebKit/WebCore/html/canvas/WebGLRenderingContext.h
@@ -464,6 +464,9 @@ class WebKitCSSMatrix;
         // Helper function to validate a GL capability.
         bool validateCapability(unsigned long);
 
+        // Helper function to validate input parameters for uniformMatrix functions.
+        bool validateUniformMatrixParameters(const WebGLUniformLocation* location, bool transpose, void* v);
+
         friend class WebGLStateRestorer;
     };
 
