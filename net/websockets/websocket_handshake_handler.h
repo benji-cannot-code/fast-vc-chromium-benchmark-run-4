0d8ee99ecad3838d461204b9fc27868a4f5739c7
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-29 09:10:06 +0000

    Add Spdy support in WebSocketHandshake*Handler
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2743003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51109 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_handshake_handler.h
+++ net/websockets/websocket_handshake_handler.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ref_counted.h"
 #include "net/http/http_request_info.h"
 #include "net/http/http_response_info.h"
+#include "net/spdy/spdy_framer.h"
 
 namespace net {
 
@@ -44,8 +45,13 @@ class WebSocketHandshakeRequestHandler {
                      size_t headers_to_remove_len);
 
   // Gets request info to open WebSocket connection.
-  // Also, full challange data in |challenge|.
+  // Also, fill challange data in |challenge|.
   HttpRequestInfo GetRequestInfo(const GURL& url, std::string* challenge);
+  // Gets request as SpdyHeaderBlock.
+  // Also, fill challenge data in |challenge|.
+  bool GetRequestHeaderBlock(const GURL& url,
+                             spdy::SpdyHeaderBlock* headers,
+                             std::string* challenge);
   // Gets WebSocket handshake raw request message to open WebSocket
   // connection.
   std::string GetRawRequest();
@@ -80,6 +86,9 @@ class WebSocketHandshakeResponseHandler {
   // Parses WebSocket handshake response info given as HttpResponseInfo.
   bool ParseResponseInfo(const HttpResponseInfo& response_info,
                          const std::string& challenge);
+  // Parses WebSocket handshake response as SpdyHeaderBlock.
+  bool ParseResponseHeaderBlock(const spdy::SpdyHeaderBlock& headers,
+                                const std::string& challenge);
 
   // Gets the headers value.
   void GetHeaders(const char* const headers_to_get[],
