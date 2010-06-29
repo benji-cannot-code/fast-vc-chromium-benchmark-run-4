7fb72988c41ee751b310636fa81505e646b28355
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-29 02:52:32 +0000

    2010-06-28  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Implement HTMLTreeBuilder::reconstructTheActiveFormattingElements
            https://bugs.webkit.org/show_bug.cgi?id=41319
    
            This is basically a direct transcription of HTML5 TreeBuilder spec:
             http://www.whatwg.org/specs/web-apps/current-work/multipage/parsing.html#list-of-active-formatting-elements
    
            This code is covered by various tests in html5lib/runner which we
            can't run yet due to other asserts.  Adam and I are working on
            getting rid of those ASSERTS so that this (and other code) will
            be better tested shortly.
    
            * html/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::insertFormatingElement):
            (WebCore::HTMLTreeBuilder::reconstructTheActiveFormattingElements):
            * html/HTMLTreeBuilder.h:
            (WebCore::HTMLTreeBuilder::ElementStack::contains):
            (WebCore::HTMLTreeBuilder::FormatingElementEntry::FormatingElementEntry):
            (WebCore::HTMLTreeBuilder::FormatingElementEntry::):
            (WebCore::HTMLTreeBuilder::FormatingElementEntry::isMarker):
            (WebCore::HTMLTreeBuilder::FormatingElementEntry::element):
            (WebCore::HTMLTreeBuilder::FormatingElementEntry::replaceElement):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62077 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTreeBuilder.h
+++ third_party/WebKit/WebCore/html/HTMLTreeBuilder.h
@@ -153,6 +153,15 @@ private:
             }
         }
 
+        bool contains(Element* element) const
+        {
+            for (ElementRecord* pos = m_top.get(); pos; pos = pos->next()) {
+                if (pos->element() == element)
+                    return true;
+            }
+            return false;
+        }
+
     private:
         OwnPtr<ElementRecord> m_top;
     };
@@ -197,7 +206,7 @@ private:
     void insertCommentOnDocument(AtomicHTMLToken&);
     void insertElement(AtomicHTMLToken&);
     void insertSelfClosingElement(AtomicHTMLToken&);
-    void insertFormatingElement(AtomicHTMLToken&);
+    void insertFormattingElement(AtomicHTMLToken&);
     void insertGenericRCDATAElement(AtomicHTMLToken&);
     void insertGenericRawTextElement(AtomicHTMLToken&);
     void insertScriptElement(AtomicHTMLToken&);
@@ -207,6 +216,8 @@ private:
     void insertHTMLStartTagInBody(AtomicHTMLToken&);
 
     PassRefPtr<Element> createElement(AtomicHTMLToken&);
+    unsigned indexOfLastOpenFormattingElementOrMarker() const;
+    void reopenFormattingElementsAfterIndex(unsigned lastOpenElementIndex);
     void reconstructTheActiveFormattingElements();
 
     Element* currentElement() { return m_openElements.top(); }
@@ -214,7 +225,41 @@ private:
     RefPtr<Element> m_headElement;
     RefPtr<Element> m_formElement;
     ElementStack m_openElements;
-    Vector<Element*> m_activeFormattingElements;
+
+    class FormattingElementEntry {
+    public:
+        FormattingElementEntry(Element* element)
+            : m_element(element)
+        {
+            ASSERT(element);
+        }
+
+        enum MarkerEntryType { MarkerEntry };
+        FormattingElementEntry(MarkerEntryType)
+        {
+        }
+
+        bool isMarker() const { return !m_element; }
+
+        Element* element() const
+        {
+            // The fact that !m_element == isMarker is an implementation detail
+            // callers should check isMarker() before calling element().
+            ASSERT(m_element);
+            return m_element.get();
+        }
+
+        void replaceElement(PassRefPtr<Element> element)
+        {
+            ASSERT(m_element); // Once a marker, always a marker.
+            m_element = element;
+        }
+
+    private:
+        RefPtr<Element> m_element;
+    };
+
+    Vector<FormattingElementEntry> m_activeFormattingElements;
     bool m_framesetOk;
 
     // FIXME: Implement error reporting.
