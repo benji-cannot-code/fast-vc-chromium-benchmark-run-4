e12daf6462ee5fe38d83f522eed7d1998ade79de
lzheng@chromium.org <lzheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-29 21:53:18 +0000

    Add switches and apis in safebrowsing protocol_manager that will allow end-to-end test later.
    BUG=6787,47318
    TEST=protocol_manager_unittest.cc
    
    Review URL: http://codereview.chromium.org/2868030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51177 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 
 #include "base/callback.h"
+#include "base/command_line.h"
 #include "base/path_service.h"
 #include "base/string_util.h"
 #include "chrome/browser/browser_process.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/net/url_request_context_getter.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -32,6 +34,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::Time;
 using base::TimeDelta;
+
+// The default URL prefix where browser fetches chunk updates, hashes,
+// and reports malware.
+static const char* const kSbDefaultInfoURLPrefix =
+    "http://safebrowsing.clients.google.com/safebrowsing";
+
+// The default URL prefix where browser fetches MAC client key.
+static const char* const kSbDefaultMacKeyURLPrefix =
+    "https://sb-ssl.google.com/safebrowsing";
 
 static Profile* GetDefaultProfile() {
   FilePath user_data_dir;
@@ -234,6 +245,11 @@ void SafeBrowsingService::UpdateFinished(bool update_succeeded) {
   }
 }
 
+bool SafeBrowsingService::IsUpdateInProgress() const {
+  DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
+  return update_in_progress_;
+}
+
 void SafeBrowsingService::OnBlockingPageDone(
     const std::vector<UnsafeResource>& resources,
     bool proceed) {
@@ -346,12 +362,25 @@ void SafeBrowsingService::OnIOInitialize(
   std::string client_name("chromium");
 #endif
 #endif
+  CommandLine* cmdline = CommandLine::ForCurrentProcess();
+  bool disable_auto_update = cmdline->HasSwitch(switches::kSbDisableAutoUpdate);
+  std::string info_url_prefix =
+      cmdline->HasSwitch(switches::kSbInfoURLPrefix) ?
+      cmdline->GetSwitchValueASCII(switches::kSbInfoURLPrefix) :
+      kSbDefaultInfoURLPrefix;
+  std::string mackey_url_prefix =
+      cmdline->HasSwitch(switches::kSbMacKeyURLPrefix) ?
+      cmdline->GetSwitchValueASCII(switches::kSbMacKeyURLPrefix) :
+      kSbDefaultMacKeyURLPrefix;
 
   protocol_manager_ = new SafeBrowsingProtocolManager(this,
                                                       client_name,
                                                       client_key,
                                                       wrapped_key,
-                                                      request_context_getter);
+                                                      request_context_getter,
+                                                      info_url_prefix,
+                                                      mackey_url_prefix,
+                                                      disable_auto_update);
 
   // Balance the reference added by Start().
   request_context_getter->Release();
