2612cfe29997cf290d0e5095419806968b2b44b5
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-29 16:30:40 +0000

    Build fix. Not reviewd.
    CGColor is only used for PLATFORM(CG).  Conditionalize include
    to correct build failure for WinCairo.
    
    * platform/graphics/win/MediaPlayerPrivateFullscreenWindow.cpp:
    Conditionalize use of CGColor.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62128 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateFullscreenWindow.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/MediaPlayerPrivateFullscreenWindow.cpp
@@ -29,8 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "IntRect.h"
 #include "WebCoreInstanceHandle.h"
-#include <CoreGraphics/CGColor.h>
 #include <windows.h>
+
+#if PLATFORM(CG)
+#include <CoreGraphics/CGColor.h>
+#endif
 
 namespace WebCore {
 
