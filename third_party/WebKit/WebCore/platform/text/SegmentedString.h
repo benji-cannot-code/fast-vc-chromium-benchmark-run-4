8c740112311a84e5446cb70dbb40272a83633331
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-06-30 09:14:18 +0000

    2010-06-30  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            HTMLTokenizer needs EndOfFile support
            https://bugs.webkit.org/show_bug.cgi?id=41344
    
            EndOfFile support uncovered a bug in our implementation of finish().
            finish() may be called more than once if the first call does not
            result in end() being called (and parsing thus actually stopping).
    
            SegmentedString::close() should have ASSERTed that it was not already
            closed when close() is called.  I've added such an assert now.
    
            * html/HTMLDocumentParser.cpp:
            (WebCore::HTMLDocumentParser::finish):
            * platform/text/SegmentedString.cpp:
            (WebCore::SegmentedString::close):
            * platform/text/SegmentedString.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62172 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/SegmentedString.h
+++ third_party/WebKit/WebCore/platform/text/SegmentedString.h
@@ -83,7 +83,7 @@ public:
     const SegmentedString& operator=(const SegmentedString&);
 
     void clear();
-    void close() { m_closed = true; }
+    void close();
 
     void append(const SegmentedString&);
     void prepend(const SegmentedString&);
