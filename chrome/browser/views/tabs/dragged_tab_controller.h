ebd2ce8294695dd1528619ee514e64720370ca91
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-30 22:00:42 +0000

    Fixes bug in dragging tabs around that could occassionally result in the tab jumping around. The bug occurred if you dragged a tab from one tabstrip to another, and was the result of the target tab strip not knowing a drag is underway.
    BUG=47930
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/2858038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51296 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/dragged_tab_controller.h
+++ chrome/browser/views/tabs/dragged_tab_controller.h
@@ -43,6 +43,12 @@ class DraggedTabController : public TabContentsDelegate,
                        BaseTabStrip* source_tabstrip);
   virtual ~DraggedTabController();
 
+  // Returns true if there is a drag underway and the drag is attached to
+  // |tab_strip|.
+  // NOTE: this returns false if the dragged tab controller is in the process
+  // of finishing the drag.
+  static bool IsAttachedTo(BaseTabStrip* tab_strip);
+
   // Capture information needed to be used during a drag session for this
   // controller's associated source tab and BaseTabStrip. |mouse_offset| is the
   // distance of the mouse pointer from the tab's origin.
@@ -323,6 +329,9 @@ class DraggedTabController : public TabContentsDelegate,
 
   // Did the mouse move enough that we started a drag?
   bool started_drag_;
+
+  // Is the drag active?
+  bool active_;
 
   DISALLOW_COPY_AND_ASSIGN(DraggedTabController);
 };
