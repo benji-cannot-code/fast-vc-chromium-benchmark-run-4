dec3023c6cff0d93a87e6ab70799aac9cdeec7ce
danno@chromium.org <danno@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-30 12:24:46 +0000

    Fix multiple issues with ADM/ADMX templates
    Correct descriptions in ADM/ADMX template and change registry key to "Chrome" from "Google Chrome".
    
    Fix numerous punctuation, verbiage and wording issues.
    
    Fix HomepageIsNewTabPage so that not checking the checkbox actually forces the hompage to the homepage URL.
    
    BUG=none
    TEST=manual
    
    Review URL: http://codereview.chromium.org/2806040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51251 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/configuration_policy_provider_win.cc
+++ chrome/browser/configuration_policy_provider_win.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(GOOGLE_CHROME_BUILD)
 const wchar_t ConfigurationPolicyProviderWin::kPolicyRegistrySubKey[] =
-    L"SOFTWARE\\Policies\\Google\\Google Chrome";
+    L"SOFTWARE\\Policies\\Google\\Chrome";
 #else
 const wchar_t ConfigurationPolicyProviderWin::kPolicyRegistrySubKey[] =
     L"SOFTWARE\\Policies\\Chromium";
