e44460b78a32046d832e3a44fef253e93f7752d5
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-01 00:05:07 +0000

    Really fix the windows build.
    * bindings/js/JSBindingsAllInOne.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62221 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSBindingsAllInOne.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSBindingsAllInOne.cpp
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSDOMApplicationCacheCustom.cpp"
 #include "JSDOMBinding.cpp"
 #include "JSDOMFormDataCustom.cpp"
-#include "JSDOMFormDataCustom.cpp"
 #include "JSDOMGlobalObject.cpp"
 #include "JSDOMStringMapCustom.cpp"
 #include "JSDOMWindowBase.cpp"
