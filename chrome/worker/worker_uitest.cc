b5eb40a592340a2408c972540770d1b6deb67ab0
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-01 17:58:54 +0000

    Revert 51378 - Adding FAILS_ for failing worker tests
    TBR=yurys
    
    Review URL: http://codereview.chromium.org/2812037
    
    TBR=kinuko@chromium.org
    Review URL: http://codereview.chromium.org/2878019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51382 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/worker/worker_uitest.cc
+++ chrome/worker/worker_uitest.cc
@@ -286,7 +286,7 @@ TEST_F(WorkerTest, WorkerMapGc) {
   RunWorkerFastLayoutTest("wrapper-map-gc.html");
 }
 
-TEST_F(WorkerTest, FAILS_WorkerMessagePort) {
+TEST_F(WorkerTest, WorkerMessagePort) {
   RunWorkerFastLayoutTest("worker-messageport.html");
 }
 
@@ -315,7 +315,7 @@ TEST_F(WorkerTest, WorkerReplaceSelf) {
 #define WorkerScriptError FLAKY_WorkerScriptError
 #endif
 
-TEST_F(WorkerTest, FAILS_WorkerScriptError) {
+TEST_F(WorkerTest, WorkerScriptError) {
   RunWorkerFastLayoutTest("worker-script-error.html");
 }
 
