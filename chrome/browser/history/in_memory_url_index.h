7d887152266201ba23614af6ce275f0ede1d28b2
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-01 18:13:22 +0000

    Add basic outline for the URL history autocompletion search provider.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/2850038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51388 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/history/in_memory_url_index.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_HISTORY_IN_MEMORY_URL_INDEX_H_
+#define CHROME_BROWSER_HISTORY_IN_MEMORY_URL_INDEX_H_
+
+namespace history {
+
+// The URL history source.
+// Holds portions of the URL database in memory in an indexed form.  Used to
+// quickly look up matching URLs for a given query string.  Used by
+// the HistoryURLProvider for inline autocomplete and to provide URL
+// matches to the omnibox.
+class InMemoryURLIndex {
+ public:
+  InMemoryURLIndex();
+  ~InMemoryURLIndex();
+};
+
+}  // namespace history
+
+#endif  // CHROME_BROWSER_HISTORY_IN_MEMORY_URL_INDEX_H_
