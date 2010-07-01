57bd6db5bd534c1712beac588a1fbf6f95605f90
antti.j.koivisto@nokia.com <antti.j.koivisto@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-01 14:36:56 +0000

    [Qt] Initial WebKit2 implementation https://bugs.webkit.org/show_bug.cgi?id=40233
    Reviewed by Kenneth Rohde Christiansen.
    
    Include some stdlib headers to fix linux build.
    
    * Platform/CoreIPC/ArgumentDecoder.cpp:
    * Platform/CoreIPC/ArgumentEncoder.cpp:
    * UIProcess/WebLoaderClient.cpp:
    * UIProcess/WebPageProxy.cpp:
    * UIProcess/WebUIClient.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62260 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Platform/CoreIPC/ArgumentDecoder.cpp
+++ third_party/WebKit/WebKit2/Platform/CoreIPC/ArgumentDecoder.cpp
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ArgumentDecoder.h"
 
+#include <stdio.h>
+
 namespace CoreIPC {
 
 ArgumentDecoder::ArgumentDecoder(const uint8_t* buffer, size_t bufferSize)
--- third_party/WebKit/WebKit2/UIProcess/WebLoaderClient.cpp
+++ third_party/WebKit/WebKit2/UIProcess/WebLoaderClient.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebLoaderClient.h"
 
 #include "WKAPICast.h"
+#include <string.h>
 
 namespace WebKit {
 
