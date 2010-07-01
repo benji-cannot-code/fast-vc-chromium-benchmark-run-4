622303d19dec2ea98fb4a81ae680b2e7630f239f
jocelyn.turcotte@nokia.com <jocelyn.turcotte@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-01 12:17:13 +0000

    2010-07-01  Jocelyn Turcotte  <jocelyn.turcotte@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Create QComboBoxes when clicked and destroy on hide.
            https://bugs.webkit.org/show_bug.cgi?id=41451
    
            Currently a QComboBox is created for each RenderMenuList and
            it gets destroyed either when the RenderMenuList or the
            QWebView (its Qt parent) is destroyed. This cause a crash
            when the QWebView is destroyed before the render tree (which
            is kept in cache).
    
            This patch aim to destroy the QComboBox as soon as its popup
            gets hidden, and likewise, create it only when the popup is
            requested to be shown.
            It also removes the unneeded reference to the QGraphicsProxyWidget,
            destroying the QComboBox automatically destroys its bound
            proxywidget.
    
            * WebCoreSupport/QtFallbackWebPopup.cpp:
            (WebCore::QtFallbackWebPopupCombo::hidePopup):
            (WebCore::QtFallbackWebPopup::QtFallbackWebPopup):
            (WebCore::QtFallbackWebPopup::~QtFallbackWebPopup):
            (WebCore::QtFallbackWebPopup::show):
            (WebCore::QtFallbackWebPopup::hide):
            (WebCore::QtFallbackWebPopup::destroyPopup):
            (WebCore::QtFallbackWebPopup::populate):
            * WebCoreSupport/QtFallbackWebPopup.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62251 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/QtFallbackWebPopup.h
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/QtFallbackWebPopup.h
@@ -43,6 +43,8 @@ public:
     virtual void show();
     virtual void hide();
 
+    void destroyPopup();
+
 private slots:
     void activeChanged(int);
 
@@ -50,7 +52,6 @@ private:
     friend class QtFallbackWebPopupCombo;
     bool m_popupVisible;
     QtFallbackWebPopupCombo* m_combo;
-    QGraphicsProxyWidget* m_proxy;
 
     void populate();
 #if ENABLE(SYMBIAN_DIALOG_PROVIDERS)
