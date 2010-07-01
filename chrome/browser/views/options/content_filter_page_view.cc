5324c7d275905b99a4f8af3482a0db873510bb2c
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-01 22:16:38 +0000

    Add notifications content settings type.
    Not used anywhere yet, no functionality change.
    
     BUG=45547
    TEST=none
    
    Review URL: http://codereview.chromium.org/2815042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51432 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/options/content_filter_page_view.cc
+++ chrome/browser/views/options/content_filter_page_view.cc
@@ -53,6 +53,7 @@ void ContentFilterPageView::InitControlLayout() {
     IDS_PLUGIN_SETTING_LABEL,
     IDS_POPUP_SETTING_LABEL,
     IDS_GEOLOCATION_SETTING_LABEL,
+    IDS_NOTIFICATIONS_SETTING_LABEL,
   };
   COMPILE_ASSERT(arraysize(kTitleIDs) == CONTENT_SETTINGS_NUM_TYPES,
                  Need_a_setting_for_every_content_settings_type);
@@ -72,6 +73,7 @@ void ContentFilterPageView::InitControlLayout() {
     IDS_PLUGIN_LOAD_RADIO,
     IDS_POPUP_ALLOW_RADIO,
     IDS_GEOLOCATION_ALLOW_RADIO,
+    IDS_NOTIFICATIONS_ALLOW_RADIO,
   };
   COMPILE_ASSERT(arraysize(kAllowIDs) == CONTENT_SETTINGS_NUM_TYPES,
                  Need_a_setting_for_every_content_settings_type);
@@ -91,6 +93,7 @@ void ContentFilterPageView::InitControlLayout() {
     0,
     0,
     IDS_GEOLOCATION_ASK_RADIO,
+    IDS_NOTIFICATIONS_ASK_RADIO,
   };
   COMPILE_ASSERT(arraysize(kAskIDs) == CONTENT_SETTINGS_NUM_TYPES,
                  Need_a_setting_for_every_content_settings_type);
@@ -113,6 +116,7 @@ void ContentFilterPageView::InitControlLayout() {
     IDS_PLUGIN_NOLOAD_RADIO,
     IDS_POPUP_BLOCK_RADIO,
     IDS_GEOLOCATION_BLOCK_RADIO,
+    IDS_NOTIFICATIONS_BLOCK_RADIO,
   };
   COMPILE_ASSERT(arraysize(kBlockIDs) == CONTENT_SETTINGS_NUM_TYPES,
                  Need_a_setting_for_every_content_settings_type);
