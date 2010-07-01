2aa2bb49d53d14b7d9a810f046d7ee6106f88dc2
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-01 20:44:53 +0000

    Initial WebKit2 implementation https://bugs.webkit.org/show_bug.cgi?id=40233
    Reviewed by Antti Koivisto.
    Patch by Kenneth Christiansen <kenneth@webkit.org>
    
    Add implementation of the UI and loader client for Qt.
    
    * UIProcess/API/qt/qwkpage.cpp:
    * UIProcess/API/qt/ClientImpl.cpp: Added.
    (toQWKPage):
    (qt_wk_didStartProvisionalLoadForFrame):
    (qt_wk_didReceiveServerRedirectForProvisionalLoadForFrame):
    (qt_wk_didFailProvisionalLoadWithErrorForFrame):
    (qt_wk_didCommitLoadForFrame):
    (qt_wk_didFinishLoadForFrame):
    (qt_wk_didFailLoadWithErrorForFrame):
    (qt_wk_didReceiveTitleForFrame):
    (qt_wk_didFirstLayoutForFrame):
    (qt_wk_didFirstVisuallyNonEmptyLayoutForFrame):
    (qt_wk_didStartProgress):
    (qt_wk_didChangeProgress):
    (qt_wk_didFinishProgress):
    (qt_wk_didBecomeUnresponsive):
    (qt_wk_didBecomeResponsive):
    (qt_wk_createNewPage):
    (qt_wk_showPage):
    (qt_wk_close):
    (qt_wk_runJavaScriptAlert):
    * UIProcess/API/qt/ClientImpl.h: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qwkpage.h"
 #include "qwkpage_p.h"
 
-#include "DefaultPageLoaderClientCallbacksQt.h"
+#include "DefaultClientCallbacksQt.h"
 #include "LocalizedStrings.h"
 #include "WebEventFactory.h"
 #include "WKStringQt.h"
