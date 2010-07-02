988acb8e7cb6503aebeef8323af2cbeec37877a8
dumi@chromium.org <dumi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-02 22:50:39 +0000

    WebCore: DB clean up. https://bugs.webkit.org/show_bug.cgi?id=41404
    Reviewed by Darin Fisher.
    
    1. Made all DatabaseTasks internal classes of Database and made
       the Database methods called by those tasks private.
    2. Removed the Database::stop{ed}() methods.
    3. Removed the code that kept track of open databases from
       ScriptExecutionContext (no longer needed).
    4. Made minor changes to Database::closeImmediately() to make it
       possible for DatabaseThread to call that method instead of
       close().
    5. Minor fix to DatabaseTracker: addOpenDatabase() should call
       OriginQuotaManager::addDatabase() if the origin quota manager
       is not tracking this origin.
    6. Removed Database::performPolicyChecks(). We already update the
       DB size after each transaction, and we enforce a max size for
       the DB at the beginning of each transaction.
    
    * dom/ScriptExecutionContext.cpp:
    (WebCore::ScriptExecutionContext::stopDatabases):
    * dom/ScriptExecutionContext.h:
    (WebCore::ScriptExecutionContext::hasOpenDatabases):
    * storage/Database.cpp:
    (WebCore::Database::openDatabase):
    (WebCore::Database::Database):
    (WebCore::Database::markAsDeletedAndClose):
    (WebCore::Database::close):
    (WebCore::Database::closeImmediately):
    * storage/Database.h:
    * storage/DatabaseTask.cpp:
    (WebCore::DatabaseTask::performTask):
    (WebCore::Database::DatabaseOpenTask::DatabaseOpenTask):
    (WebCore::Database::DatabaseOpenTask::doPerformTask):
    (WebCore::Database::DatabaseOpenTask::debugTaskName):
    (WebCore::Database::DatabaseCloseTask::DatabaseCloseTask):
    (WebCore::Database::DatabaseCloseTask::doPerformTask):
    (WebCore::Database::DatabaseCloseTask::debugTaskName):
    (WebCore::Database::DatabaseTransactionTask::DatabaseTransactionTask):
    (WebCore::Database::DatabaseTransactionTask::doPerformTask):
    (WebCore::Database::DatabaseTransactionTask::debugTaskName):
    (WebCore::Database::DatabaseTableNamesTask::DatabaseTableNamesTask):
    (WebCore::Database::DatabaseTableNamesTask::doPerformTask):
    (WebCore::Database::DatabaseTableNamesTask::debugTaskName):
    * storage/DatabaseTask.h:
    (WebCore::Database::DatabaseOpenTask::create):
    (WebCore::Database::DatabaseCloseTask::create):
    (WebCore::Database::DatabaseTransactionTask::create):
    (WebCore::Database::DatabaseTableNamesTask::create):
    * storage/DatabaseThread.cpp:
    (WebCore::DatabaseThread::databaseThread):
    * storage/DatabaseTracker.cpp:
    (WebCore::DatabaseTracker::addOpenDatabase):
    * storage/SQLTransaction.cpp:
    (WebCore::SQLTransaction::executeSQL):
    (WebCore::SQLTransaction::checkAndHandleClosedDatabase):
    
    LayoutTests: Minor change to a DB test to make it run correctly with run-webkit-tests --iterations N.
    https://bugs.webkit.org/show_bug.cgi?id=41404
    
    Reviewed by Darin Fisher.
    
    * storage/open-database-creation-callback.html:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62411 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/Database.h
+++ third_party/WebKit/WebCore/storage/Database.h
@@ -72,26 +72,14 @@ public:
     virtual void markAsDeletedAndClose();
     bool deleted() const { return m_deleted; }
 
-    enum ClosePolicy { DoNotRemoveDatabaseFromContext, RemoveDatabaseFromContext };
-    void close(ClosePolicy);
+    void close();
     virtual void closeImmediately();
-
-    void stop();
-    bool stopped() const { return m_stopped; }
 
     unsigned long long databaseSize() const;
     unsigned long long maximumSize() const;
 
-    // Called from DatabaseThread, must be prepared to work on the background thread
-    void performPolicyChecks();
-
-    virtual bool performOpenAndVerify(bool setVersionInNewDatabase, ExceptionCode&);
-
-    void inProgressTransactionCompleted();
     void scheduleTransactionCallback(SQLTransaction*);
     void scheduleTransactionStep(SQLTransaction*, bool immediately = false);
-
-    Vector<String> performGetTableNames();
 
     SQLTransactionClient* transactionClient() const;
     SQLTransactionCoordinator* transactionCoordinator() const;
@@ -99,12 +87,21 @@ public:
     void incrementalVacuumIfNeeded();
 
 private:
+    class DatabaseOpenTask;
+    class DatabaseCloseTask;
+    class DatabaseTransactionTask;
+    class DatabaseTableNamesTask;
+
     Database(ScriptExecutionContext*, const String& name, const String& expectedVersion,
              const String& displayName, unsigned long estimatedSize);
 
     bool openAndVerifyVersion(bool setVersionInNewDatabase, ExceptionCode&);
+    virtual bool performOpenAndVerify(bool setVersionInNewDatabase, ExceptionCode&);
 
+    void inProgressTransactionCompleted();
     void scheduleTransaction();
+
+    Vector<String> performGetTableNames();
 
     static void deliverPendingCallback(void*);
 
@@ -116,7 +113,6 @@ private:
     RefPtr<SecurityOrigin> m_databaseThreadSecurityOrigin;
 
     bool m_deleted;
-    bool m_stopped;
 };
 
 } // namespace WebCore
