57218a4b7f886a33b1d8a9655270b68b153f0754
jcivelli@chromium.org <jcivelli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-02 20:29:13 +0000

    Fixing translate infobar background. In some cases the infobar background was not the right color.
    BUG=48191
    TEST=See bug.
    
    Review URL: http://codereview.chromium.org/2808042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51562 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/translate/translate_infobar_base_gtk.cc
+++ chrome/browser/gtk/translate/translate_infobar_base_gtk.cc
@@ -49,6 +49,8 @@ TranslateInfoBarBase::TranslateInfoBarBase(TranslateInfoBarDelegate* delegate)
       background_color_animation_->Reset(1.0);
       background_color_animation_->Hide();
     }
+  } else {
+    background_error_percent_ = delegate->IsError() ? 1 : 0;
   }
 }
 
