0c8f1e0d717d9bdfad69ea5658860645828cddbb
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-02 10:38:34 +0000

    2010-07-02  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Fix tokenization of <!----->
            https://bugs.webkit.org/show_bug.cgi?id=41505
    
            Test progression plus reduced test case.
    
            * html5lib/resources/comments01.dat:
            * html5lib/runner-expected-html5.txt:
            * html5lib/runner-expected.txt:
    2010-07-02  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Eric Seidel.
    
            Fix tokenization of <!----->
            https://bugs.webkit.org/show_bug.cgi?id=41505
    
            I noticed this error when browsing through the HTML5lib failures.
            We're adding an extra character to the comment token, contrary to what
            the spec says to do.
    
            * html/HTMLTokenizer.cpp:
            (WebCore::HTMLTokenizer::nextToken):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62378 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
+++ third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
@@ -1179,7 +1179,6 @@ bool HTMLTokenizer::nextToken(SegmentedString& source, HTMLToken& token)
         } else if (cc == '-') {
             parseError();
             m_token->appendToComment('-');
-            m_token->appendToComment(cc);
             ADVANCE_TO(CommentEndState);
         } else if (cc == InputStreamPreprocessor::endOfFileMarker) {
             parseError();
