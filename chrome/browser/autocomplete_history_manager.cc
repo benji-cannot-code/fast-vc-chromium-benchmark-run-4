4ee0d9b14002d7d0c84d969183f2f80cb2d52f0b
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-02 15:51:05 +0000

    Don't save SSNs--quick style fix.
    BUG=http://crbug.com/46590
    TEST=unit test
    
    Review URL: http://codereview.chromium.org/2862042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51536 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete_history_manager.cc
+++ chrome/browser/autocomplete_history_manager.cc
@@ -50,7 +50,7 @@ bool IsSSN(const string16& text) {
   int area = StringToInt(area_string);
   if (area < 1 ||
       area == 666 ||
-      area > 733 && area < 750 ||
+      (area > 733 && area < 750) ||
       area > 772)
     return false;
 
