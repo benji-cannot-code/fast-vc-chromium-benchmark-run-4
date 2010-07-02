a43ca725fcf75b873425d231570ce0d953e8ec08
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-02 04:31:58 +0000

    Windows build fix.
    * WebProcess/WebProcess.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62359 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebProcess.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebProcess.cpp
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebProcess.h"
 
 #include "InjectedBundle.h"
+#if PLATFORM(MAC)
 #include "MachPort.h"
+#endif
 #include "RunLoop.h"
 #include "WebCoreArgumentCoders.h"
 #include "WebFrame.h"
