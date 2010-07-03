bd9e3ecc0bab1631af021d2ed814802a9cbc374b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-03 08:17:39 +0000

    2010-07-03  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Kent Tamura.
    
            [BREWMP] Change the CRASH() macro to print "WebKit CRASH" log.
            https://bugs.webkit.org/show_bug.cgi?id=41524
    
            Print "WebKit CRASH" before crashing.
    
            * wtf/Assertions.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Assertions.h
+++ third_party/WebKit/JavaScriptCore/wtf/Assertions.h
@@ -56,6 +56,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <e32debug.h>
 #endif
 
+#if PLATFORM(BREWMP)
+#include <AEEStdLib.h>
+#endif
+
 #ifdef NDEBUG
 /* Disable ASSERT* macros in release mode. */
 #define ASSERTIONS_DISABLED_DEFAULT 1
@@ -164,6 +168,12 @@ void WTFLogVerbose(const char* file, int line, const char* function, WTFLogChann
     __DEBUGGER(); \
     User::Panic(_L("Webkit CRASH"),0); \
     } while(false)
+#elif PLATFORM(BREWMP)
+#define CRASH() do { \
+    DBGPRINTF_FATAL("WebKit CRASH"); \
+    *(int *)(uintptr_t)0xbbadbeef = 0; \
+    ((void(*)())0)(); /* More reliable, but doesn't say BBADBEEF */ \
+} while(false)
 #else
 #define CRASH() do { \
     *(int *)(uintptr_t)0xbbadbeef = 0; \
