f7d31dc9a1d34b0ae455ae4278ab4762b7ac97b2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-03 02:46:08 +0000

    2010-07-02  Luiz Agostini  <luiz.agostini@openbossa.org>
            Reviewed by Sam Weinig.
    
            Removing CoreFoundation.h include from WKFrame.h
            https://bugs.webkit.org/show_bug.cgi?id=41528
    
            * UIProcess/API/C/WKFrame.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62425 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/C/WKFrame.h
+++ third_party/WebKit/WebKit2/UIProcess/API/C/WKFrame.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WKFrame_h
 #define WKFrame_h
 
-#include <CoreFoundation/CoreFoundation.h>
 #include <WebKit2/WKBase.h>
 
 #ifndef __cplusplus
