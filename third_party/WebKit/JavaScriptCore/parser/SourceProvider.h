9a883ce2f28c2c355775ae97284ce33eebf2e846
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-03 20:30:24 +0000

    Move BOM handling out of the lexer and parser https://bugs.webkit.org/show_bug.cgi?id=41539
    Reviewed by Geoffrey Garen.
    
    JavaScriptCore:
    
    Doing the BOM stripping in the lexer meant that we could
    end up having to strip the BOMs from a source multiple times.
    To deal with this we now require all strings provided by
    a SourceProvider to already have had the BOMs stripped.
    This also simplifies some of the lexer logic.
    
    * parser/Lexer.cpp:
    (JSC::Lexer::setCode):
    (JSC::Lexer::sourceCode):
    * parser/SourceProvider.h:
    (JSC::SourceProvider::SourceProvider):
    (JSC::UStringSourceProvider::create):
    (JSC::UStringSourceProvider::getRange):
    (JSC::UStringSourceProvider::UStringSourceProvider):
    * wtf/text/StringImpl.h:
    (WebCore::StringImpl::copyStringWithoutBOMs):
    
    WebCore:
    
    Update WebCore to ensure that SourceProviders don't
    produce strings with BOMs in them.
    
    * bindings/js/ScriptSourceProvider.h:
    (WebCore::ScriptSourceProvider::ScriptSourceProvider):
    * bindings/js/StringSourceProvider.h:
    (WebCore::StringSourceProvider::StringSourceProvider):
    * loader/CachedScript.cpp:
    (WebCore::CachedScript::CachedScript):
    (WebCore::CachedScript::script):
    * loader/CachedScript.h:
    (WebCore::CachedScript::):
      CachedScript now stores decoded data with the BOMs stripped,
      and caches the presence of BOMs across memory purges.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62449 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/parser/SourceProvider.h
+++ third_party/WebKit/JavaScriptCore/parser/SourceProvider.h
@@ -35,13 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace JSC {
 
-    enum SourceBOMPresence { SourceHasNoBOMs, SourceCouldHaveBOMs };
-
     class SourceProvider : public RefCounted<SourceProvider> {
     public:
-        SourceProvider(const UString& url, SourceBOMPresence hasBOMs = SourceCouldHaveBOMs)
+        SourceProvider(const UString& url)
             : m_url(url)
-            , m_hasBOMs(hasBOMs)
         {
         }
         virtual ~SourceProvider() { }
@@ -53,29 +50,33 @@ namespace JSC {
         const UString& url() { return m_url; }
         intptr_t asID() { return reinterpret_cast<intptr_t>(this); }
 
-        SourceBOMPresence hasBOMs() const { return m_hasBOMs; }
-
     private:
         UString m_url;
-        SourceBOMPresence m_hasBOMs;
     };
 
     class UStringSourceProvider : public SourceProvider {
     public:
-        static PassRefPtr<UStringSourceProvider> create(const UString& source, const UString& url)
+        static PassRefPtr<UStringSourceProvider> create(const UString& source, const UString& url, bool hasBOMs = true)
         {
-            return adoptRef(new UStringSourceProvider(source, url));
+            return adoptRef(new UStringSourceProvider(source, url, hasBOMs));
         }
 
-        UString getRange(int start, int end) const { return m_source.substr(start, end - start); }
+        UString getRange(int start, int end) const
+        {
+            return m_source.substr(start, end - start);
+        }
         const UChar* data() const { return m_source.data(); }
         int length() const { return m_source.size(); }
 
     private:
-        UStringSourceProvider(const UString& source, const UString& url)
+        UStringSourceProvider(const UString& source, const UString& url, bool hasBOMs)
             : SourceProvider(url)
             , m_source(source)
         {
+            if (hasBOMs && m_source.size()) {
+                bool scratch = false;
+                m_source = UString(m_source.rep()->copyStringWithoutBOMs(false, scratch));
+            }
         }
 
         UString m_source;
