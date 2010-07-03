101fb08dc7a7a1e0230ee704de61487e7fddee92
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-03 04:52:45 +0000

    Clamp the number of arguments supported by function.apply https://bugs.webkit.org/show_bug.cgi?id=41351 <rdar://problem/8142141>
    Reviewed by Gavin Barraclough.
    
    JavaScriptCore:
    
    Add clamping logic to function.apply similar to that
    enforced by firefox.  We have a smaller clamp than
    firefox as our calling convention means that stack
    usage is proportional to argument count -- the firefox
    limit is larger than you could actually call.
    
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::privateExecute):
    * jit/JITStubs.cpp:
    (JSC::DEFINE_STUB_FUNCTION):
    * runtime/Arguments.h:
    (JSC::Arguments::):
    
    LayoutTests:
    
    Testcases.
    
    * fast/js/function-apply-many-args-expected.txt: Added.
    * fast/js/function-apply-many-args.html: Added.
    * fast/js/script-tests/function-apply-many-args.js: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62432 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/interpreter/Interpreter.cpp
+++ third_party/WebKit/JavaScriptCore/interpreter/Interpreter.cpp
@@ -3697,6 +3697,7 @@ skip_id_custom_self:
         int32_t argCount = 0;
         if (!arguments) {
             argCount = (uint32_t)(callFrame->argumentCount());
+            argCount = min(argCount, static_cast<int32_t>(Arguments::MaxArguments));
             int32_t sizeDelta = argsOffset + argCount + RegisterFile::CallFrameHeaderSize;
             Register* newEnd = callFrame->registers() + sizeDelta;
             if (!registerFile->grow(newEnd) || ((newEnd - callFrame->registers()) != sizeDelta)) {
@@ -3723,6 +3724,7 @@ skip_id_custom_self:
             if (asObject(arguments)->classInfo() == &Arguments::info) {
                 Arguments* args = asArguments(arguments);
                 argCount = args->numProvidedArguments(callFrame);
+                argCount = min(argCount, static_cast<int32_t>(Arguments::MaxArguments));
                 int32_t sizeDelta = argsOffset + argCount + RegisterFile::CallFrameHeaderSize;
                 Register* newEnd = callFrame->registers() + sizeDelta;
                 if (!registerFile->grow(newEnd) || ((newEnd - callFrame->registers()) != sizeDelta)) {
@@ -3733,6 +3735,7 @@ skip_id_custom_self:
             } else if (isJSArray(&callFrame->globalData(), arguments)) {
                 JSArray* array = asArray(arguments);
                 argCount = array->length();
+                argCount = min(argCount, static_cast<int32_t>(Arguments::MaxArguments));
                 int32_t sizeDelta = argsOffset + argCount + RegisterFile::CallFrameHeaderSize;
                 Register* newEnd = callFrame->registers() + sizeDelta;
                 if (!registerFile->grow(newEnd) || ((newEnd - callFrame->registers()) != sizeDelta)) {
@@ -3743,6 +3746,7 @@ skip_id_custom_self:
             } else if (asObject(arguments)->inherits(&JSArray::info)) {
                 JSObject* argObject = asObject(arguments);
                 argCount = argObject->get(callFrame, callFrame->propertyNames().length).toUInt32(callFrame);
+                argCount = min(argCount, static_cast<int32_t>(Arguments::MaxArguments));
                 int32_t sizeDelta = argsOffset + argCount + RegisterFile::CallFrameHeaderSize;
                 Register* newEnd = callFrame->registers() + sizeDelta;
                 if (!registerFile->grow(newEnd) || ((newEnd - callFrame->registers()) != sizeDelta)) {
--- third_party/WebKit/JavaScriptCore/runtime/JSArray.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/JSArray.cpp
@@ -949,10 +949,10 @@ void JSArray::fillArgList(ExecState* exec, MarkedArgumentBuffer& args)
 
 void JSArray::copyToRegisters(ExecState* exec, Register* buffer, uint32_t maxSize)
 {
-    ASSERT(m_storage->m_length == maxSize);
+    ASSERT(m_storage->m_length >= maxSize);
     UNUSED_PARAM(maxSize);
     JSValue* vector = m_storage->m_vector;
-    unsigned vectorEnd = min(m_storage->m_length, m_vectorLength);
+    unsigned vectorEnd = min(maxSize, m_vectorLength);
     unsigned i = 0;
     for (; i < vectorEnd; ++i) {
         JSValue& v = vector[i];
@@ -961,7 +961,7 @@ void JSArray::copyToRegisters(ExecState* exec, Register* buffer, uint32_t maxSiz
         buffer[i] = v;
     }
 
-    for (; i < m_storage->m_length; ++i)
+    for (; i < maxSize; ++i)
         buffer[i] = get(exec, i);
 }
 
