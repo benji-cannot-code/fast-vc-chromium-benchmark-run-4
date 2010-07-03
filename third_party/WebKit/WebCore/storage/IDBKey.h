c2f165b09e2d09d0ac2427e0c09314c4098acfa3
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-03 23:02:24 +0000

    Ugh.  Have to put the destructor in the .h file since the .ccp isn't compiled yet.
    * storage/IDBKey.h:
    (WebCore::IDBKey::~IDBKey):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62455 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/IDBKey.h
+++ third_party/WebKit/WebCore/storage/IDBKey.h
@@ -51,7 +51,7 @@ public:
     {
         return adoptRef(new IDBKey(string));
     }
-    ~IDBKey();
+    ~IDBKey() { }
 
     // In order of the least to the most precident in terms of sort order.
     enum Type {
