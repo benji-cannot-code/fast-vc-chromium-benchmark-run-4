7d50de9c6aaee9369d9118dc27b60f25fa88d278
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-03 03:14:44 +0000

    Attempted build fix: Run IO blocking task actually on IO thread.
    TBR=dhollowa
    
    Review URL: http://codereview.chromium.org/2800041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51588 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/desktop_notification_service_unittest.cc
+++ chrome/browser/notifications/desktop_notification_service_unittest.cc
@@ -13,19 +13,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+namespace {
+
+class WaitTask : public Task {
+ public:
+  WaitTask(base::WaitableEvent* event)
+      : event_(event) {
+  }
+  virtual void Run() {
+    event_->Wait();
+  }
+
+ private:
+  base::WaitableEvent* event_;
+};
+
+
 class DesktopNotificationServiceTest : public RenderViewHostTestHarness {
  public:
   DesktopNotificationServiceTest()
-      : event_(false, false),
-        ALLOW_THIS_IN_INITIALIZER_LIST(method_factory_(this)) {
+      : event_(false, false) {
   }
-
-  void LetIOThreadWait() {
-    event_.Wait();
-  }
-
   base::WaitableEvent event_;
-  ScopedRunnableMethodFactory<DesktopNotificationServiceTest> method_factory_;
 };
 
 TEST_F(DesktopNotificationServiceTest, DefaultContentSettingSentToCache) {
@@ -35,9 +44,7 @@ TEST_F(DesktopNotificationServiceTest, DefaultContentSettingSentToCache) {
   // Create IO thread, start its message loop.
   ChromeThread io_thread(ChromeThread::IO);
   io_thread.Start();
-  ChromeThread::PostTask(ChromeThread::UI, FROM_HERE,
-      method_factory_.NewRunnableMethod(
-          &DesktopNotificationServiceTest::LetIOThreadWait));
+  ChromeThread::PostTask(ChromeThread::IO, FROM_HERE, new WaitTask(&event_));
 
   // Creates the service, calls InitPrefs() on it which loads data from the
   // profile into the cache and then puts the cache in io thread mode.
@@ -64,4 +71,4 @@ TEST_F(DesktopNotificationServiceTest, DefaultContentSettingSentToCache) {
   EXPECT_EQ(CONTENT_SETTING_BLOCK, cache->CachedDefaultContentSetting());
 }
 
-
+}  // namespace
