bcec4d5819920347ba33d3f9a15928a76aef8d1f
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-04 23:15:51 +0000

    Add simple command line parser and pass mode argument to the web process https://bugs.webkit.org/show_bug.cgi?id=41586
    Reviewed by Dan Bernstein.
    
    * Shared/CommandLine.h:
    (WebKit::CommandLine::operator[]):
    Add CommandLine class.
    
    * Shared/mac/CommandLineMac.cpp:
    (WebKit::CommandLine::parse):
    Implement Mac version of CommandLine::parse.
    
    * UIProcess/Launcher/mac/ProcessLauncherMac.mm:
    (WebKit::ProcessLauncher::launchProcess):
    Add "legacywebprocess" mode to the posix_spawn call.
    
    * WebKit2.xcodeproj/project.pbxproj:
    Add CommandLine to the xcode project.
    
    * WebProcess/Launching/mac/WebProcessMain.mm:
    (webProcessMain):
    (main):
    Parse the command line and check the mode.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62471 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/Launching/mac/WebProcessMain.mm
+++ third_party/WebKit/WebKit2/WebProcess/Launching/mac/WebProcessMain.mm
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#import "CommandLine.h"
 #import "RunLoop.h"
 #import "WebProcess.h"
 #import "WebSystemInterface.h"
@@ -39,12 +40,11 @@ extern "C" kern_return_t bootstrap_look_up2(mach_port_t, const name_t, mach_port
 
 #define SHOW_CRASH_REPORTER 1
 
+using namespace WebCore;
 using namespace WebKit;
 
-int main(int argc, char** argv)
+static int webProcessMain(CommandLine*)
 {
-    ASSERT(!objc_collectingEnabled());
-
     mach_port_t serverPort;
     kern_return_t kr = bootstrap_look_up2(bootstrap_port, "com.apple.WebKit.WebProcess", &serverPort, getppid(), /* BOOTSTRAP_PER_PID_SERVICE */ 1);
     if (kr) {
@@ -80,4 +80,19 @@ int main(int argc, char** argv)
     // FIXME: Do more cleanup here.
 
     return 0;
+}
+
+int main(int argc, char** argv)
+{
+    ASSERT(!objc_collectingEnabled());
+
+    CommandLine commandLine;
+    if (!commandLine.parse(argc, argv))
+        return EXIT_FAILURE;
+
+    String mode = commandLine["mode"];
+    if (mode == "legacywebprocess")
+        return webProcessMain(&commandLine);
+
+    return EXIT_FAILURE;
 }
