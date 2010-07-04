03f8800b9924641894cc55d8648a64b51ae4b9f2
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-04 22:06:18 +0000

    2010-07-04  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            HTMLTreeBuilder needs an adoption agency
            https://bugs.webkit.org/show_bug.cgi?id=41453
    
            Fix Qt Minimal build.  SVGNames.h should always
            be generated, even when SVG is off, however that's
            not how things currently work.
    
            * html/HTMLTreeBuilder.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
@@ -42,7 +42,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LegacyHTMLDocumentParser.h"
 #include "LegacyHTMLTreeBuilder.h"
 #include "NotImplemented.h"
+#if ENABLE(SVG)
 #include "SVGNames.h"
+#endif
 #include "ScriptController.h"
 #include "Settings.h"
 #include "Text.h"
