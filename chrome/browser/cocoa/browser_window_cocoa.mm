98a362d49ebbdbe9795dc4dedb3a3740c5be796f
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-05 13:15:53 +0000

    [Mac] Implement BrowserWindowCocoa::ConfirmAddSearchProvider.
    BUG=48327
    TEST=Install the Google SSL search extension, click "Add as Search Engine".
    
    Review URL: http://codereview.chromium.org/2803036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51638 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_window_cocoa.mm
+++ chrome/browser/cocoa/browser_window_cocoa.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/cocoa/clear_browsing_data_controller.h"
 #import "chrome/browser/cocoa/content_settings_dialog_controller.h"
 #import "chrome/browser/cocoa/download_shelf_controller.h"
+#import "chrome/browser/cocoa/edit_search_engine_cocoa_controller.h"
 #import "chrome/browser/cocoa/html_dialog_window_controller.h"
 #import "chrome/browser/cocoa/import_settings_dialog.h"
 #import "chrome/browser/cocoa/keyword_editor_cocoa_controller.h"
@@ -196,7 +197,16 @@ gfx::Rect BrowserWindowCocoa::GetRootWindowResizerRect() const {
 void BrowserWindowCocoa::ConfirmAddSearchProvider(
     const TemplateURL* template_url,
     Profile* profile) {
-  NOTIMPLEMENTED();
+  // The controller will release itself when the window closes.
+  EditSearchEngineCocoaController* editor =
+      [[EditSearchEngineCocoaController alloc] initWithProfile:profile
+                                                      delegate:NULL
+                                                   templateURL:template_url];
+  [NSApp beginSheet:[editor window]
+     modalForWindow:window()
+      modalDelegate:controller_
+     didEndSelector:@selector(sheetDidEnd:returnCode:context:)
+        contextInfo:NULL];
 }
 
 LocationBar* BrowserWindowCocoa::GetLocationBar() const {
--- chrome/browser/cocoa/browser_window_controller.mm
+++ chrome/browser/cocoa/browser_window_controller.mm
@@ -1634,6 +1634,12 @@ willAnimateFromState:(bookmarks::VisualState)oldState
   return browser_->tabstrip_model()->delegate()->UseVerticalTabs();
 }
 
+- (void)sheetDidEnd:(NSWindow*)sheet
+         returnCode:(NSInteger)code
+            context:(void*)context {
+  [sheet orderOut:self];
+}
+
 @end  // @implementation BrowserWindowController
 
 
