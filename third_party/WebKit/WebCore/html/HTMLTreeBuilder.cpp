80943b02a172317e8b66eea84a726fd1d698de00
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-05 20:03:45 +0000

    2010-07-05  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Add basic <col> support to the treebuilder
            https://bugs.webkit.org/show_bug.cgi?id=41590
    
            * html/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::processStartTag):
    2010-07-05  Eric Seidel  <eric@webkit.org>
    
            Reviewed by Adam Barth.
    
            Add basic <col> support to the treebuilder
            https://bugs.webkit.org/show_bug.cgi?id=41590
    
            Test the new <col> support. Turns out the old treebuilder doesn't
            handle <table><col> correctly.
    
            * html5lib/resources/tables01.dat:
            * html5lib/runner-expected-html5.txt:
            * html5lib/runner-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62500 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
@@ -747,7 +747,10 @@ void HTMLTreeBuilder::processStartTag(AtomicHTMLToken& token)
             return;
         }
         if (token.name() == colTag) {
-            notImplemented();
+            AtomicHTMLToken fakeToken(HTMLToken::StartTag, colgroupTag.localName());
+            processStartTag(fakeToken);
+            ASSERT(InColumnGroupMode);
+            processStartTag(token);
             return;
         }
         if (isTableBodyContextTag(token.name())) {
@@ -783,6 +786,17 @@ void HTMLTreeBuilder::processStartTag(AtomicHTMLToken& token)
             return;
         }
         parseError(token);
+        notImplemented();
+        break;
+    case InColumnGroupMode:
+        if (token.name() == htmlTag) {
+            insertHTMLStartTagInBody(token);
+            return;
+        }
+        if (token.name() == colTag) {
+            insertSelfClosingElement(token);
+            return;
+        }
         notImplemented();
         break;
     case InTableBodyMode:
