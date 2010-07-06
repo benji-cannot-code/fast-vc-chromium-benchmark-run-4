8de70b4146a5a248dddc59962216ff4a7e0a59b8
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-06 20:18:59 +0000

    Try to fix build.
    * src/WebWorkerBase.cpp:
    (WebKit::WebWorkerBase::dispatchTaskToMainThread): Use leakPtr instead
    of release.
    * src/WebWorkerClientImpl.cpp:
    (WebKit::WebWorkerClientImpl::postMessageToWorkerObjectTask): Removed
    unneeded release called on a PassOwnPtr to pass it as an argument of
    type PassOwnPtr.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62585 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebWorkerClientImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebWorkerClientImpl.cpp
@@ -361,7 +361,7 @@ void WebWorkerClientImpl::postMessageToWorkerObjectTask(
 
     if (thisPtr->m_worker) {
         OwnPtr<MessagePortArray> ports =
-            MessagePort::entanglePorts(*context, channels.release());
+            MessagePort::entanglePorts(*context, channels);
         RefPtr<SerializedScriptValue> serializedMessage =
             SerializedScriptValue::createFromWire(message);
         thisPtr->m_worker->dispatchEvent(MessageEvent::create(ports.release(),
