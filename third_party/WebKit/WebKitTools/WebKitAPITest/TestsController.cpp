f56176fc65b0a257078a9af0a772226f26a78ba3
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-06 16:35:15 +0000

    Fix build.
    * WebKitAPITest/TestsController.cpp:
    (WebKitAPITest::TestsController::addTest): Use leakPtr instead of release.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62555 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/WebKitAPITest/TestsController.cpp
+++ third_party/WebKit/WebKitTools/WebKitAPITest/TestsController.cpp
@@ -72,7 +72,7 @@ bool TestsController::runAllTests()
 
 void TestsController::addTest(PassOwnPtr<Test> test)
 {
-    m_tests.append(test.release());
+    m_tests.append(test.leakPtr());
     runNextTestSoon();
 }
 
