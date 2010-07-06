4f42e056777d258c91d9c8220fe8fbca7c10e4e5
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-06 20:51:40 +0000

    Add support for dumping the contents of messages to the console to WebKitTestRunner
    Reviewed by Anders Carlsson.
    
    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
    (WTR::InjectedBundlePage::InjectedBundlePage):
    (WTR::InjectedBundlePage::_addMessageToConsole):
    (WTR::InjectedBundlePage::addMessageToConsole):
    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.h
+++ third_party/WebKit/WebKitTools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.h
@@ -39,6 +39,7 @@ public:
     WKBundlePageRef page() const { return m_page; }
 
 private:
+    // Loader Client
     static void _didStartProvisionalLoadForFrame(WKBundlePageRef page, WKBundleFrameRef frame, const void *clientInfo);
     static void _didReceiveServerRedirectForProvisionalLoadForFrame(WKBundlePageRef page, WKBundleFrameRef frame, const void *clientInfo);
     static void _didFailProvisionalLoadWithErrorForFrame(WKBundlePageRef page, WKBundleFrameRef frame, const void *clientInfo);
@@ -47,7 +48,6 @@ private:
     static void _didFailLoadWithErrorForFrame(WKBundlePageRef page, WKBundleFrameRef frame, const void *clientInfo);
     static void _didReceiveTitleForFrame(WKBundlePageRef page, WKStringRef title, WKBundleFrameRef frame, const void *clientInfo);
     static void _didClearWindowForFrame(WKBundlePageRef page, WKBundleFrameRef frame, JSContextRef ctx, JSObjectRef window, const void *clientInfo);
-
     void didStartProvisionalLoadForFrame(WKBundleFrameRef frame);
     void didReceiveServerRedirectForProvisionalLoadForFrame(WKBundleFrameRef frame);
     void didFailProvisionalLoadWithErrorForFrame(WKBundleFrameRef frame);
@@ -56,6 +56,10 @@ private:
     void didFailLoadWithErrorForFrame(WKBundleFrameRef frame);
     void didReceiveTitleForFrame(WKStringRef title, WKBundleFrameRef frame);
     void didClearWindowForFrame(WKBundleFrameRef frame, JSContextRef ctx, JSObjectRef window);
+
+    // UI Client
+    static void _addMessageToConsole(WKBundlePageRef page, WKStringRef message, uint32_t lineNumber, const void *clientInfo);
+    void addMessageToConsole(WKStringRef message, uint32_t lineNumber);
 
     WKBundlePageRef m_page;
 };
