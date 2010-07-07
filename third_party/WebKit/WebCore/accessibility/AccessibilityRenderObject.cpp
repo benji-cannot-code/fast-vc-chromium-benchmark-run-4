f4434676c0069378515dda98ce67c6f04a4e2f01
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-07 20:26:13 +0000

    AX: when a node's role changes, the AX tree might need to be updated https://bugs.webkit.org/show_bug.cgi?id=41784
    Reviewed by Beth Dakin.
    
    WebCore: 
    
    Test: platform/mac/accessibility/update-children-when-aria-role-changes.html
    
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::AccessibilityRenderObject):
    (WebCore::AccessibilityRenderObject::updateAccessibilityRole):
    
    LayoutTests: 
    
    * platform/mac/accessibility/update-children-when-aria-role-changes-expected.txt: Added.
    * platform/mac/accessibility/update-children-when-aria-role-changes.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62694 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AccessibilityRenderObject.cpp
+++ third_party/WebKit/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -92,7 +92,8 @@ AccessibilityRenderObject::AccessibilityRenderObject(RenderObject* renderer)
     , m_childrenDirty(false)
     , m_roleForMSAA(UnknownRole)
 {
-    updateAccessibilityRole();
+    m_role = determineAccessibilityRole();
+    
 #ifndef NDEBUG
     m_renderer->setHasAXObject(true);
 #endif
@@ -2969,7 +2970,12 @@ AccessibilityRole AccessibilityRenderObject::ariaRoleAttribute() const
     
 void AccessibilityRenderObject::updateAccessibilityRole()
 {
+    bool ignoredStatus = accessibilityIsIgnored();
     m_role = determineAccessibilityRole();
+    
+    // The AX hierarchy only needs to be updated if the ignored status of an element has changed.
+    if (ignoredStatus != accessibilityIsIgnored())
+        childrenChanged();
 }
     
 AccessibilityRole AccessibilityRenderObject::determineAccessibilityRole()
