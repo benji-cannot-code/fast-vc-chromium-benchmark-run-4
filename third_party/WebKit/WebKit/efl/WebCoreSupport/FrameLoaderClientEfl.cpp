c87428e2cb73be965d7815dab969f83b7a27a407
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-07 14:49:12 +0000

    2010-07-07  Gyuyoung Kim  <gyuyoung.kim@samsung.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [EFL] EFLWebKit doesn't support viewport meta tag.
            Support viewport meta tag on EFL Port.
            https://bugs.webkit.org/show_bug.cgi?id=40278
    
            * efl/EWebLauncher/main.c: Process 'viewport,changed' signal.
            (viewport_set):
            (on_viewport_changed): Add a callback function for the 'viewport,changed' signal.
            (browserCreate): Register the callback function for the 'viewport,changed' signal.
            * efl/WebCoreSupport/ChromeClientEfl.cpp: Implement method to process viewport tag.
            (WebCore::ChromeClientEfl::didReceiveViewportArguments):
            * efl/WebCoreSupport/ChromeClientEfl.h: Define method to process viewport tag.
            * efl/WebCoreSupport/FrameLoaderClientEfl.cpp:
            (WebCore::FrameLoaderClientEfl::FrameLoaderClientEfl): Implement a function to set layout
            when website doesn't contain viewport tag.
            (WebCore::FrameLoaderClientEfl::dispatchDidCommitLoad):
            (WebCore::FrameLoaderClientEfl::dispatchDidFirstLayout):
            * efl/WebCoreSupport/FrameLoaderClientEfl.h:
            (WebCore::FrameLoaderClientEfl::setInitLayoutCompleted):
            (WebCore::FrameLoaderClientEfl::getInitLayoutCompleted):
            * efl/ewk/ewk_private.h: Added internal APIs to process viewport tag.
            * efl/ewk/ewk_view.cpp: Added APIs to process and to handle viewport tag.
            (_ewk_view_priv_new):
            (ewk_view_fixed_layout_size_set):
            (ewk_view_zoom_set):
            (ewk_view_zoom_weak_set):
            (ewk_view_zoom_animated_set):
            (ewk_view_viewport_set): Reports that viewport has changed.
            (ewk_view_viewport_get): Reports that viewport has changed.
            (ewk_view_zoom_range_set): Sets the zoom range.
            (ewk_view_zoom_range_min_get): Gets minimum value of zoom range.
            (ewk_view_zoom_range_max_get): Gets maximum value of zoom range.
            (ewk_view_user_scalable_set): Sets if zoom is enabled.
            (ewk_view_user_scalable_get): Gets if zoom is enabled.
            * efl/ewk/ewk_view.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62666 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/efl/WebCoreSupport/FrameLoaderClientEfl.cpp
+++ third_party/WebKit/WebKit/efl/WebCoreSupport/FrameLoaderClientEfl.cpp
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ProgressTracker.h"
 #include "RenderPart.h"
 #include "ResourceRequest.h"
+#include "ViewportArguments.h"
 #include "ewk_private.h"
 #include <wtf/text/CString.h>
 
@@ -71,6 +72,7 @@ FrameLoaderClientEfl::FrameLoaderClientEfl(Evas_Object *view)
     , m_customUserAgent("")
     , m_pluginView(0)
     , m_hasSentResponseToPlugin(false)
+    , m_initLayoutCompleted(false)
 {
 }
 
@@ -576,11 +578,16 @@ void FrameLoaderClientEfl::dispatchDidChangeIcons()
 
 void FrameLoaderClientEfl::dispatchDidCommitLoad()
 {
+    m_initLayoutCompleted = false;
+
     ewk_frame_uri_changed(m_frame);
     if (ewk_view_frame_main_get(m_view) != m_frame)
         return;
     ewk_view_title_set(m_view, 0);
     ewk_view_uri_changed(m_view);
+
+    ViewportArguments arguments;
+    ewk_view_viewport_set(m_view, arguments.width, arguments.height, arguments.initialScale, arguments.minimumScale, arguments.maximumScale, arguments.userScalable);
 }
 
 void FrameLoaderClientEfl::dispatchDidFinishDocumentLoad()
@@ -591,7 +598,7 @@ void FrameLoaderClientEfl::dispatchDidFinishDocumentLoad()
 void FrameLoaderClientEfl::dispatchDidFirstLayout()
 {
     // emit m_frame->initialLayoutCompleted();
-    notImplemented();
+    m_initLayoutCompleted = true;
 }
 
 void FrameLoaderClientEfl::dispatchDidFirstVisuallyNonEmptyLayout()
