173873fdb6a9912926da4d97d8d82aa1d986aaa7
dtseng@chromium.org <dtseng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-07 18:55:50 +0000

    Fire focus events when CustomButtons are hottracked. BUG=47585 TEST=none Review URL: http://codereview.chromium.org/2867040
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51753 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/button/custom_button.h
+++ views/controls/button/custom_button.h
@@ -43,6 +43,7 @@ class CustomButton : public Button,
   void SetAnimationDuration(int duration);
 
   // Overridden from View:
+  virtual bool GetAccessibleState(AccessibilityTypes::State* state);
   virtual void SetEnabled(bool enabled);
   virtual bool IsEnabled() const;
   virtual bool IsFocusable() const;
