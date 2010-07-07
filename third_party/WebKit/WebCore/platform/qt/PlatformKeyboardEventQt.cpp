a2f1a19ff302651ac70563ab39fca5813403a50d
antti.j.koivisto@nokia.com <antti.j.koivisto@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-07 12:37:55 +0000

    [Qt] Initial WebKit2 implementation https://bugs.webkit.org/show_bug.cgi?id=40233
    Reviewed by Kenneth Rohde Christiansen.
    
    Export some event conversion functions needed for WebKit2.
    
    * platform/PlatformKeyboardEvent.h:
    * platform/qt/PlatformKeyboardEventQt.cpp:
    (WebCore::keyIdentifierForQtKeyCode):
    (WebCore::windowsKeyCodeForKeyEvent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62654 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/PlatformKeyboardEventQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/PlatformKeyboardEventQt.cpp
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-static String keyIdentifierForQtKeyCode(int keyCode)
+String keyIdentifierForQtKeyCode(int keyCode)
 {
     switch (keyCode) {
     case Qt::Key_Menu:
@@ -138,7 +138,7 @@ static String keyIdentifierForQtKeyCode(int keyCode)
     }
 }
 
-static int windowsKeyCodeForKeyEvent(unsigned int keycode, bool isKeypad = false)
+int windowsKeyCodeForKeyEvent(unsigned int keycode, bool isKeypad)
 {
     // Determine wheter the event comes from the keypad
     if (isKeypad) {
