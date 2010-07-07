99fc3738def2b33ff34b98cd9318db2eb1d99d77
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-07 02:43:59 +0000

    Revert 51682 - Build fix attempt.
    Parts by hbono.
    
    TBR=tony^work
    
    Review URL: http://codereview.chromium.org/2838044
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/2822050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51683 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/webplugin_delegate_impl_win.cc
+++ webkit/glue/plugins/webplugin_delegate_impl_win.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win_util.h"
 #include "skia/ext/platform_canvas.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebInputEvent.h"
+#include "webkit/default_plugin/plugin_impl.h"
 #include "webkit/glue/plugins/plugin_constants_win.h"
 #include "webkit/glue/plugins/plugin_instance.h"
 #include "webkit/glue/plugins/plugin_lib.h"
