c7b6ac49aaa5107d8dbc81a530cdd73b3fc9a995
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-07 15:38:19 +0000

    Bye bye tab close dot, we hardly knew you.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2813045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51718 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/side_tab.cc
+++ chrome/browser/views/tabs/side_tab.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gfx/path.h"
 #include "gfx/skia_util.h"
 #include "grit/app_resources.h"
+#include "grit/theme_resources.h"
 #include "views/controls/button/image_button.h"
 
 namespace {
@@ -38,8 +39,11 @@ const int kPhantomTabIconAlpha = 100;
 // SideTab, public:
 
 SideTab::SideTab(TabController* controller)
-    : BaseTab(controller, false) {
-  SetCloseButtonColor(kTextColor);
+    : BaseTab(controller) {
+  ResourceBundle& rb = ResourceBundle::GetSharedInstance();
+  close_button()->SetBackground(kTextColor,
+                                rb.GetBitmapNamed(IDR_TAB_CLOSE),
+                                rb.GetBitmapNamed(IDR_TAB_CLOSE_MASK));
 }
 
 SideTab::~SideTab() {
