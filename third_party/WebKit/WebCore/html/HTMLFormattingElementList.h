7e5da196d13b660af707f951695dec50e1f3a688
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-07 17:35:03 +0000

    2010-07-07  Adam Barth  <abarth@webkit.org>
            Unreviewed.
    
            Fix ASSERT in Eric's previous patch.  I think he tested his patch in
            Release and not Debug.
    
            * html/HTMLFormattingElementList.h:
            (WebCore::HTMLFormattingElementList::Entry::Entry):
            (WebCore::HTMLFormattingElementList::findIndex):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62681 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLFormattingElementList.h
+++ third_party/WebKit/WebCore/html/HTMLFormattingElementList.h
@@ -48,7 +48,7 @@ public:
     class Entry {
     public:
         // Inline because they're hot and Vector<T> uses them.
-        Entry(Element* element)
+        explicit Entry(Element* element)
             : m_element(element)
         {
             ASSERT(element);
@@ -76,6 +76,8 @@ public:
         bool operator!=(const Entry& other) const { return m_element != other.m_element; }
 
     private:
+        friend class HTMLFormattingElementList;
+
         RefPtr<Element> m_element;
     };
 
@@ -127,10 +129,11 @@ public:
 private:
     size_t findIndex(Element* element) const
     {
+        ASSERT(element);
         // A reverse find is more efficient than Vector<T>::find
         for (size_t i = 1; i <= m_entries.size(); ++i) {
             size_t index = m_entries.size() - i;
-            if (m_entries[index].element() == element)
+            if (m_entries[index].m_element == element)
                 return index;
         }
         return notFound;
