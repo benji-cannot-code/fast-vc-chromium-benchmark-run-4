7f81bfaad04adfe4c84f735205799f47b1ea9999
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-07 17:50:58 +0000

    Autocomplete and AutoFill entries are shown combined in the suggestions popup.
    These changes merge popup menu suggestions for Autocomplete and AutoFill.  Suggestions are collected from both sources and returned to the renderer as a single list.  This combined list is given to WebKit to display in the suggestions popup.  When the user selects an item in the popup the appropriate result gets filled in the fields.
    
    BUG=41328
    TEST=AutoFillManagerTest.*
    
    Review URL: http://codereview.chromium.org/2878020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51736 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager_unittest.cc
+++ chrome/browser/autofill/autofill_manager_unittest.cc
@@ -286,6 +286,10 @@ TEST_F(AutoFillManagerTest, GetProfileSuggestionsEmptyValue) {
       "First Name", "firstname", "", "text", &field);
   EXPECT_TRUE(autofill_manager_->GetAutoFillSuggestions(kPageID, false, field));
 
+  // No suggestions provided, so send an empty vector as the results.
+  // This triggers the combined message send.
+  rvh()->AutocompleteSuggestionsReturned(kPageID, std::vector<string16>());
+
   // Test that we sent the right message to the renderer.
   int page_id = 0;
   std::vector<string16> values;
@@ -318,6 +322,10 @@ TEST_F(AutoFillManagerTest, GetProfileSuggestionsMatchCharacter) {
       "First Name", "firstname", "E", "text", &field);
   EXPECT_TRUE(autofill_manager_->GetAutoFillSuggestions(kPageID, false, field));
 
+  // No suggestions provided, so send an empty vector as the results.
+  // This triggers the combined message send.
+  rvh()->AutocompleteSuggestionsReturned(kPageID, std::vector<string16>());
+
   // Test that we sent the right message to the renderer.
   int page_id = 0;
   std::vector<string16> values;
@@ -347,6 +355,10 @@ TEST_F(AutoFillManagerTest, GetCreditCardSuggestionsEmptyValue) {
   autofill_unittest::CreateTestFormField(
       "Card Number", "cardnumber", "", "text", &field);
   EXPECT_TRUE(autofill_manager_->GetAutoFillSuggestions(kPageID, false, field));
+
+  // No suggestions provided, so send an empty vector as the results.
+  // This triggers the combined message send.
+  rvh()->AutocompleteSuggestionsReturned(kPageID, std::vector<string16>());
 
   // Test that we sent the right message to the renderer.
   int page_id = 0;
@@ -388,6 +400,10 @@ TEST_F(AutoFillManagerTest, GetCreditCardSuggestionsMatchCharacter) {
       "Card Number", "cardnumber", "1", "text", &field);
   EXPECT_TRUE(autofill_manager_->GetAutoFillSuggestions(kPageID, false, field));
 
+  // No suggestions provided, so send an empty vector as the results.
+  // This triggers the combined message send.
+  rvh()->AutocompleteSuggestionsReturned(kPageID, std::vector<string16>());
+
   // Test that we sent the right message to the renderer.
   int page_id = 0;
   std::vector<string16> values;
@@ -421,6 +437,10 @@ TEST_F(AutoFillManagerTest, GetCreditCardSuggestionsNonCCNumber) {
   autofill_unittest::CreateTestFormField(
       "Name on Card", "nameoncard", "", "text", &field);
   EXPECT_TRUE(autofill_manager_->GetAutoFillSuggestions(kPageID, false, field));
+
+  // No suggestions provided, so send an empty vector as the results.
+  // This triggers the combined message send.
+  rvh()->AutocompleteSuggestionsReturned(kPageID, std::vector<string16>());
 
   // Test that we sent the right message to the renderer.
   int page_id = 0;
@@ -471,6 +491,10 @@ TEST_F(AutoFillManagerTest, GetCreditCardSuggestionsSemicolon) {
       "Name on Card", "nameoncard", "", "text", &field);
   EXPECT_TRUE(autofill_manager_->GetAutoFillSuggestions(kPageID, false, field));
 
+  // No suggestions provided, so send an empty vector as the results.
+  // This triggers the combined message send.
+  rvh()->AutocompleteSuggestionsReturned(kPageID, std::vector<string16>());
+
   // Test that we sent the right message to the renderer.
   int page_id = 0;
   std::vector<string16> values;
@@ -497,6 +521,49 @@ TEST_F(AutoFillManagerTest, GetCreditCardSuggestionsSemicolon) {
   EXPECT_EQ(ASCIIToUTF16("Home; 8765; 8765"), labels[7]);
 }
 
+TEST_F(AutoFillManagerTest, GetCombinedAutoFillAndAutocompleteSuggestions) {
+  FormData form;
+  CreateTestFormData(&form);
+
+  // Set up our FormStructures.
+  std::vector<FormData> forms;
+  forms.push_back(form);
+  autofill_manager_->FormsSeen(forms);
+
+  // The page ID sent to the AutoFillManager from the RenderView, used to send
+  // an IPC message back to the renderer.
+  const int kPageID = 1;
+
+  webkit_glue::FormField field;
+  autofill_unittest::CreateTestFormField(
+      "First Name", "firstname", "", "text", &field);
+  EXPECT_TRUE(autofill_manager_->GetAutoFillSuggestions(kPageID, false, field));
+
+  // Add some Autocomplete suggestions.
+  // This triggers the combined message send.
+  std::vector<string16> suggestions;
+  suggestions.push_back(ASCIIToUTF16("Jay"));
+  suggestions.push_back(ASCIIToUTF16("Jason"));
+  rvh()->AutocompleteSuggestionsReturned(kPageID, suggestions);
+
+  // Test that we sent the right message to the renderer.
+  int page_id = 0;
+  std::vector<string16> values;
+  std::vector<string16> labels;
+  EXPECT_TRUE(GetAutoFillSuggestionsMessage(&page_id, &values, &labels));
+  EXPECT_EQ(kPageID, page_id);
+  ASSERT_EQ(4U, values.size());
+  EXPECT_EQ(ASCIIToUTF16("Elvis"), values[0]);
+  EXPECT_EQ(ASCIIToUTF16("Charles"), values[1]);
+  EXPECT_EQ(ASCIIToUTF16("Jay"), values[2]);
+  EXPECT_EQ(ASCIIToUTF16("Jason"), values[3]);
+  ASSERT_EQ(4U, labels.size());
+  EXPECT_EQ(ASCIIToUTF16("Home"), labels[0]);
+  EXPECT_EQ(ASCIIToUTF16("Work"), labels[1]);
+  EXPECT_EQ(string16(), labels[2]);
+  EXPECT_EQ(string16(), labels[3]);
+}
+
 TEST_F(AutoFillManagerTest, GetFieldSuggestionsFormIsAutoFilled) {
   FormData form;
   CreateTestFormData(&form);
@@ -514,6 +581,10 @@ TEST_F(AutoFillManagerTest, GetFieldSuggestionsFormIsAutoFilled) {
   autofill_unittest::CreateTestFormField(
       "First Name", "firstname", "", "text", &field);
   EXPECT_TRUE(autofill_manager_->GetAutoFillSuggestions(kPageID, true, field));
+
+  // No suggestions provided, so send an empty vector as the results.
+  // This triggers the combined message send.
+  rvh()->AutocompleteSuggestionsReturned(kPageID, std::vector<string16>());
 
   // Test that we sent the right message to the renderer.
   int page_id = 0;
@@ -552,6 +623,10 @@ TEST_F(AutoFillManagerTest, GetFieldSuggestionsWithDuplicateValues) {
   autofill_unittest::CreateTestFormField(
       "First Name", "firstname", "", "text", &field);
   EXPECT_TRUE(autofill_manager_->GetAutoFillSuggestions(kPageID, true, field));
+
+  // No suggestions provided, so send an empty vector as the results.
+  // This triggers the combined message send.
+  rvh()->AutocompleteSuggestionsReturned(kPageID, std::vector<string16>());
 
   // Test that we sent the right message to the renderer.
   int page_id = 0;
--- chrome/browser/renderer_host/render_view_host.h
+++ chrome/browser/renderer_host/render_view_host.h
@@ -394,8 +394,7 @@ class RenderViewHost : public RenderWidgetHost {
   // ready.
   void AutocompleteSuggestionsReturned(
       int query_id,
-      const std::vector<string16>& suggestions,
-      int default_suggestion_index);
+      const std::vector<string16>& suggestions);
 
   // Called by the AutoFillManager when the FormData has been filled out.
   void AutoFillFormDataFilled(int query_id, const webkit_glue::FormData& form);
@@ -718,6 +717,12 @@ class RenderViewHost : public RenderWidgetHost {
   // Whether this render view will be used for extensions. This controls
   // what process type we use.
   bool is_extension_process_;
+
+  // AutoFill and Autocomplete suggestions.  We accumulate these separately and
+  // send them back to the renderer together.
+  int autofill_query_id_;
+  std::vector<string16> autofill_values_;
+  std::vector<string16> autofill_labels_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderViewHost);
 };
