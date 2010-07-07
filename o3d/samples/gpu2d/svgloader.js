ab59c0291fbf7a5156e06834215c5608c86af972
kbr@google.com <kbr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-07 22:50:04 +0000

    Made code which determines which side of contours to fill more robust using new outgoing "ambiguous" parameter added to Skia XRay queries. Rolled forward Skia dependency to latest revision. With these changes the SVG butterfly renders correctly in O3D. Boosted polygon offsets again to avoid pixel dropouts.
    Also tested O3D canvas demos to verify no breakage with Skia
    roll-forward.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/2813049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51791 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/samples/gpu2d/svgloader.js
+++ o3d/samples/gpu2d/svgloader.js
@@ -336,7 +336,7 @@ SVGLoader.prototype.parsePath_ = function(pathData,
   if (this.fill_) {
     path.setFill(this.fill_);
   }
-  path.setPolygonOffset(-2 * this.polygonOffset_, -3 * this.polygonOffset_);
+  path.setPolygonOffset(-3 * this.polygonOffset_, -4 * this.polygonOffset_);
   ++this.polygonOffset_;
   this.currentTransform_().addShape(path.shape);
 };
