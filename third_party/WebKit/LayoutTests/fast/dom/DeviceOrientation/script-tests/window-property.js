8f3f89055f8fdbc4ebbaca61a56b0a7010d8ee93
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-07 11:51:16 +0000

    2010-07-06  Steve Block  <steveblock@google.com>
            Reviewed by Adam Barth.
    
            DeviceOrientationEventConstructor should be exposed at window.DeviceOrientationEvent
            https://bugs.webkit.org/show_bug.cgi?id=41685
    
            Test: fast/dom/Window/window-properties-device-orientation.html
                  fast/dom/DeviceOrientation/window-property.txt
    
            * page/DOMWindow.idl:
    2010-07-06  Steve Block  <steveblock@google.com>
    
            Reviewed by Adam Barth.
    
            DeviceOrientationEventConstructor should be exposed at window.DeviceOrientationEvent
            https://bugs.webkit.org/show_bug.cgi?id=41685
    
            This required fast/dom/Window/window-properties-on-device-orientation.html to be renamed
            to fast/dom/Window/window-properties-device-orientation.html and updated.
    
            * fast/dom/DeviceOrientation/script-tests/window-property.js:
            * fast/dom/DeviceOrientation/window-property-expected.txt:
            * fast/dom/Window/window-properties-device-orientation-expected.txt: Copied from LayoutTests/fast/dom/Window/window-properties-on-device-orientation-expected.txt.
            * fast/dom/Window/window-properties-device-orientation.html: Copied from LayoutTests/fast/dom/Window/window-properties-on-device-orientation.html.
            * fast/dom/Window/window-properties-on-device-orientation-expected.txt: Removed.
            * fast/dom/Window/window-properties-on-device-orientation.html: Removed.
            * fast/dom/Window/window-properties.html:
            * platform/gtk/Skipped:
            * platform/mac/Skipped:
            * platform/qt/Skipped:
            * platform/win/Skipped:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62653 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/DeviceOrientation/script-tests/window-property.js
+++ third_party/WebKit/LayoutTests/fast/dom/DeviceOrientation/script-tests/window-property.js
@@ -1,5 +1,19 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description("Tests that the window.ondeviceorientation property is present.");
+description("Tests that the window.DeviceOrientationEvent and window.ondeviceorientation properties are present.");
+
+function hasDeviceOrientationEventProperty()
+{
+    for (var property in window) {
+        if (property == "DeviceOrientationEvent")
+            return true;
+    }
+    return false;
+}
+
+shouldBeTrue("typeof window.DeviceOrientationEvent == 'function'");
+shouldBeTrue("hasDeviceOrientationEventProperty()");
+shouldBeTrue("'DeviceOrientationEvent' in window");
+shouldBeTrue("window.hasOwnProperty('DeviceOrientationEvent')");
 
 function hasOnDeviceOrientationProperty()
 {
@@ -13,6 +27,6 @@ function hasOnDeviceOrientationProperty()
 shouldBeTrue("typeof window.ondeviceorientation == 'object'");
 shouldBeTrue("hasOnDeviceOrientationProperty()");
 shouldBeTrue("'ondeviceorientation' in window");
-shouldBeTrue("window.hasOwnProperty('ondeviceorientation')");
+shouldBeFalse("window.hasOwnProperty('ondeviceorientation')");
 
 window.successfullyParsed = true;
