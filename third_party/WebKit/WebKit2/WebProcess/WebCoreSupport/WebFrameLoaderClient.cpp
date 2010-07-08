519bd383f016a4a477a4baaa4b1a22b74601b046
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-08 23:31:46 +0000

    Fix Windows build.
    * WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFrameLoaderClient.h"
 
 #include "DummyPlugin.h"
+#if PLATFORM(MAC)
 #include "NetscapePluginModule.h"
+#endif
 #include "NotImplemented.h"
 #include "PluginView.h"
 #include "WebCoreArgumentCoders.h"
