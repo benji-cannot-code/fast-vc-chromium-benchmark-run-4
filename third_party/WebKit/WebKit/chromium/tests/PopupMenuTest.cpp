fd6fccc30f033b69877ecb6631e8f27ef5a0c686
vitalyr@chromium.org <vitalyr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-08 20:58:12 +0000

    2010-07-08  Vitaly Repeshko  <vitalyr@chromium.org>
            Reviewed by Pavel Feldman.
    
            [chromium] Fix deletion has begun assert in TestWebPopupMenuImpl.
            https://bugs.webkit.org/show_bug.cgi?id=41899
    
            * tests/PopupMenuTest.cpp:
            (WebKit::TestWebPopupMenuImpl::create):
            (WebKit::TestWebViewClient::TestWebViewClient):
            (WebKit::TestWebViewClient::createPopupMenu):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62831 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/tests/PopupMenuTest.cpp
+++ third_party/WebKit/WebKit/chromium/tests/PopupMenuTest.cpp
@@ -108,8 +108,15 @@ public:
 
 class TestWebPopupMenuImpl : public WebPopupMenuImpl {
 public:
-    TestWebPopupMenuImpl(WebWidgetClient* client) : WebPopupMenuImpl(client) { }
+    static PassRefPtr<TestWebPopupMenuImpl> create(WebWidgetClient* client)
+    {
+        return adoptRef(new TestWebPopupMenuImpl(client));
+    }
+
     ~TestWebPopupMenuImpl() { }
+
+private:
+    TestWebPopupMenuImpl(WebWidgetClient* client) : WebPopupMenuImpl(client) { }
 };
 
 class TestWebWidget : public WebWidget {
@@ -136,10 +143,10 @@ public:
 
 class TestWebViewClient : public WebViewClient {
 public:
-    TestWebViewClient() : m_webPopupMenu(&m_webWidgetClient) { }
+    TestWebViewClient() : m_webPopupMenu(TestWebPopupMenuImpl::create(&m_webWidgetClient)) { }
     ~TestWebViewClient() { }
 
-    virtual WebWidget* createPopupMenu(WebPopupType) { return &m_webPopupMenu; }
+    virtual WebWidget* createPopupMenu(WebPopupType) { return m_webPopupMenu.get(); }
 
     // We need to override this so that the popup menu size is not 0
     // (the layout code checks to see if the popup fits on the screen).
@@ -153,7 +160,7 @@ public:
 
 private:
     TestWebWidgetClient m_webWidgetClient;
-    TestWebPopupMenuImpl m_webPopupMenu;
+    RefPtr<TestWebPopupMenuImpl> m_webPopupMenu;
 };
 
 class TestWebFrameClient : public WebFrameClient {
