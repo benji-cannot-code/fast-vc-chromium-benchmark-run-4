94180e1c31ddaf3a3c0adaf0537d548e4a7cf02c
robert@webkit.org <robert@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-08 20:57:28 +0000

    2010-07-07  Robert Hogan  <robert@webkit.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Add DRT support for pageProperty etc.
    
            https://bugs.webkit.org/show_bug.cgi?id=41584
    
            Unskip:
             printing/page-rule-selection.html
             printing/page-format-data.html
    
            * platform/qt/Skipped:
    2010-07-07  Robert Hogan  <robert@webkit.org>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Add DRT support for pageProperty etc.
    
            https://bugs.webkit.org/show_bug.cgi?id=41584
    
            * WebCoreSupport/DumpRenderTreeSupportQt.cpp:
            (DumpRenderTreeSupportQt::isPageBoxVisible):
            (DumpRenderTreeSupportQt::pageSizeAndMarginsInPixels):
            (DumpRenderTreeSupportQt::pageProperty):
            (DumpRenderTreeSupportQt::addUserStyleSheet):
            * WebCoreSupport/DumpRenderTreeSupportQt.h:
    2010-07-07  Robert Hogan  <robert@webkit.org>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Add DRT support for pageProperty etc.
    
            https://bugs.webkit.org/show_bug.cgi?id=41584
    
            * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
            (LayoutTestController::isPageBoxVisible):
            (LayoutTestController::pageSizeAndMarginsInPixels):
            (LayoutTestController::pageProperty):
            (LayoutTestController::addUserStyleSheet):
            * DumpRenderTree/qt/LayoutTestControllerQt.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62830 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/qt/LayoutTestControllerQt.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/qt/LayoutTestControllerQt.h
@@ -218,6 +218,10 @@ public slots:
     void setEditingBehavior(const QString& editingBehavior);
 
     void evaluateScriptInIsolatedWorld(int worldID, const QString& script);
+    bool isPageBoxVisible(int pageIndex);
+    QString pageSizeAndMarginsInPixels(int pageIndex, int width, int height, int marginTop, int marginRight, int marginBottom, int marginLeft);
+    QString pageProperty(const QString& propertyName, int pageNumber);
+    void addUserStyleSheet(const QString& sourceCode);
 
 private slots:
     void processWork();
