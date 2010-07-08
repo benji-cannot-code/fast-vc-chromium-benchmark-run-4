0b6f71538e6fbd538760345710e3519afb0a3ce7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-08 08:37:06 +0000

    2010-07-08  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r62765.
            http://trac.webkit.org/changeset/62765
            https://bugs.webkit.org/show_bug.cgi?id=41840
    
            All jscore and layout tests crash on Qt bot (Requested by Ossy
            on #webkit).
    
            * wtf/FastMalloc.cpp:
            (WTF::TCMalloc_PageHeap::initializeScavenger):
            (WTF::TCMalloc_PageHeap::signalScavenger):
            (WTF::TCMalloc_PageHeap::scavengerThread):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/FastMalloc.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/FastMalloc.cpp
@@ -415,8 +415,6 @@ extern "C" const int jscore_fastmalloc_introspection = 0;
 #include "TCPageMap.h"
 #include "TCSpinLock.h"
 #include "TCSystemAlloc.h"
-#include "Threading.h"
-#include "ThreadSpecific.h"
 #include <algorithm>
 #include <errno.h>
 #include <limits>
@@ -1445,8 +1443,8 @@ class TCMalloc_PageHeap {
   // it's blocked waiting for more pages to be deleted.
   bool m_scavengeThreadActive;
 
-  Mutex m_scavengeMutex;
-  ThreadCondition m_scavengeCondition;
+  pthread_mutex_t m_scavengeMutex;
+  pthread_cond_t m_scavengeCondition;
 #else // !HAVE(DISPATCH_H)
   void periodicScavenge();
 
@@ -1492,6 +1490,8 @@ void TCMalloc_PageHeap::init()
 
 void TCMalloc_PageHeap::initializeScavenger()
 {
+  pthread_mutex_init(&m_scavengeMutex, 0);
+  pthread_cond_init(&m_scavengeCondition, 0);
   m_scavengeThreadActive = true;
   pthread_t thread;
   pthread_create(&thread, 0, runScavengerThread, this);
@@ -1509,7 +1509,7 @@ void* TCMalloc_PageHeap::runScavengerThread(void* context)
 ALWAYS_INLINE void TCMalloc_PageHeap::signalScavenger()
 {
   if (!m_scavengeThreadActive && shouldScavenge())
-    m_scavengeCondition.signal();
+    pthread_cond_signal(&m_scavengeCondition);
 }
 
 #else // !HAVE(DISPATCH_H)
@@ -2375,11 +2375,12 @@ void TCMalloc_PageHeap::scavengerThread()
 
   while (1) {
       if (!shouldScavenge()) {
-          MutexLocker locker(m_scavengeMutex);
+          pthread_mutex_lock(&m_scavengeMutex);
           m_scavengeThreadActive = false;
           // Block until there are enough free committed pages to release back to the system.
-          m_scavengeCondition.wait(m_scavengeMutex);
+          pthread_cond_wait(&m_scavengeCondition, &m_scavengeMutex);
           m_scavengeThreadActive = true;
+          pthread_mutex_unlock(&m_scavengeMutex);
       }
       sleep(kScavengeDelayInSeconds);
       {
