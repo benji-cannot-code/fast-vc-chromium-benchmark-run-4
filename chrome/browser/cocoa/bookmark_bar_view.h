b56b693095b9cb4adb45d8d65ce73fd8dd955355
andybons@chromium.org <andybons@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-08 16:35:53 +0000

    [Mac] Tighten up the top chrome (move the buttons closer, reduce the toolbar height). Replace the stop/reload images with ones that fit the weight better. Also replace the wrench menu icon.
    BUG=none
    TEST=make sure dat toolbar is TIGHT.
    Review URL: http://codereview.chromium.org/2811047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51851 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/bookmark_bar_view.h
+++ chrome/browser/cocoa/bookmark_bar_view.h
@@ -11,9 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#import "chrome/browser/cocoa/background_gradient_view.h"
+
 @class BookmarkBarController;
 
-@interface BookmarkBarView : NSView {
+@interface BookmarkBarView : BackgroundGradientView {
  @private
   BOOL dropIndicatorShown_;
   CGFloat dropIndicatorPosition_;  // x position
