fc31d4a2cb07664f8ecea5e2bf56cd4e5ef03e85
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-08 21:15:16 +0000

    Fix compile (add include of WebSize to webgles2context_impl.h)
    TBR=kbr
    TEST=compile
    BUG=none
    
    Review URL: http://codereview.chromium.org/2895003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51894 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/webgles2context_impl.cc
+++ chrome/renderer/webgles2context_impl.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/gpu_channel_host.h"
 #include "chrome/renderer/render_thread.h"
 #include "chrome/renderer/render_view.h"
-#include "third_party/WebKit/WebKit/chromium/public/WebSize.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebView.h"
 
 
--- chrome/renderer/webgles2context_impl.h
+++ chrome/renderer/webgles2context_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/ggl/ggl.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebGLES2Context.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebSize.h"
 
 class WebGLES2ContextImpl : public WebKit::WebGLES2Context {
  public:
