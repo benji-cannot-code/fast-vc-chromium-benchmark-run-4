16dd8c66e195f8da8af7c9dc4755b60397eefa95
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-08 21:32:30 +0000

    Add stubbed out NetscapePluginModule class https://bugs.webkit.org/show_bug.cgi?id=41901
    Reviewed by Sam Weinig.
    
    * WebKit2.xcodeproj/project.pbxproj:
    * WebProcess/Plugins/Netscape/NetscapePluginModule.cpp: Added.
    (WebKit::NetscapePluginModule::NetscapePluginModule):
    * WebProcess/Plugins/Netscape/NetscapePluginModule.h: Added.
    (WebKit::NetscapePluginModule::create):
    * WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp:
    (WebKit::WebFrameLoaderClient::createPlugin):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62843 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFrameLoaderClient.h"
 
 #include "DummyPlugin.h"
+#include "NetscapePluginModule.h"
 #include "NotImplemented.h"
 #include "PluginView.h"
 #include "WebCoreArgumentCoders.h"
@@ -781,7 +782,10 @@ PassRefPtr<Widget> WebFrameLoaderClient::createPlugin(const IntSize&, HTMLPlugIn
     if (pluginPath.isNull())
         return 0;
 
-    // FIXME: Use the plug-in path.
+    // FIXME: This is Mac specific now because Windows doesn't have the necessary parts of NetscapePluginModule implemented.
+#if PLATFORM(MAC)
+    RefPtr<NetscapePluginModule> pluginModule = NetscapePluginModule::create(pluginPath);
+#endif
 
     RefPtr<Plugin> plugin = DummyPlugin::create();
     if (!plugin->initialize(url, paramNames, paramValues, mimeType, loadManually))
