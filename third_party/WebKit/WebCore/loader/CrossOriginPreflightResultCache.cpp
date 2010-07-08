920304b9ce5533b9d8f982b2f8680bae50a65957
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-08 20:38:07 +0000

    Reviewed by David Levin.
            https://bugs.webkit.org/show_bug.cgi?id=41886
            Shorten access control failure explanation messages
    
            * loader/CrossOriginAccessControl.cpp:
            (WebCore::passesAccessControlCheck):
            * loader/CrossOriginPreflightResultCache.cpp:
            (WebCore::CrossOriginPreflightResultCacheItem::allowsCrossOriginMethod):
            (WebCore::CrossOriginPreflightResultCacheItem::allowsCrossOriginHeaders):
            Removed "response header field" parts from error text - it's long, but doesn't add much
            to header field name.
    
            * platform/network/ResourceErrorBase.cpp:
            * platform/network/ResourceErrorBase.h:
            * xml/XMLHttpRequest.cpp:
            (WebCore::XMLHttpRequest::didFail):
            * loader/DocumentThreadableLoader.cpp:
            (WebCore::DocumentThreadableLoader::DocumentThreadableLoader):
            (WebCore::DocumentThreadableLoader::makeSimpleCrossOriginAccessRequest):
            (WebCore::DocumentThreadableLoader::didReceiveResponse):
            (WebCore::DocumentThreadableLoader::preflightFailure):
            Renamed the constant for domain. WebCore shouldn't be creating errors observable by WebKit
            clients, because it can't create a platform error, and cannot load a localized string.
            We don't treat cross origin access check failures as true loading failures, so we don't tell
            clients about these.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/CrossOriginPreflightResultCache.cpp
+++ third_party/WebKit/WebCore/loader/CrossOriginPreflightResultCache.cpp
@@ -120,7 +120,7 @@ bool CrossOriginPreflightResultCacheItem::allowsCrossOriginMethod(const String& 
     if (m_methods.contains(method) || isOnAccessControlSimpleRequestMethodWhitelist(method))
         return true;
 
-    errorDescription = "Method " + method + " is not allowed by Access-Control-Allow-Methods response header field.";
+    errorDescription = "Method " + method + " is not allowed by Access-Control-Allow-Methods.";
     return false;
 }
 
@@ -129,7 +129,7 @@ bool CrossOriginPreflightResultCacheItem::allowsCrossOriginHeaders(const HTTPHea
     HTTPHeaderMap::const_iterator end = requestHeaders.end();
     for (HTTPHeaderMap::const_iterator it = requestHeaders.begin(); it != end; ++it) {
         if (!m_headers.contains(it->first) && !isOnAccessControlSimpleRequestHeaderWhitelist(it->first, it->second)) {
-            errorDescription = "Request header field " + it->first + " is not allowed by Access-Control-Allow-Headers response header field.";
+            errorDescription = "Request header field " + it->first + " is not allowed by Access-Control-Allow-Headers.";
             return false;
         }
     }
