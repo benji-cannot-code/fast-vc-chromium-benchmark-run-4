250472df6fe74932258fc5e067f96440433b668e
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-08 21:10:27 +0000

    Revert 51877, since SpdyNetworkTransactionTest.CorruptFrameSessionError started failing after this check-in (but only on vista modules builder). BUG=48588
    Original CL description:
    
    Add the capability to run multiple proxy PAC scripts in parallel.
    
    Refactors SingleThreadedProxyResolver into MultiThreadedProxyResolver.
    
    New threads are created lazily on demand, up to a fixed maximum.
    
    Note that this CL does NOT change the policy in Chrome -- it will continue to use a single thread for proxy resolving, but using the new code to do so.
    
    BUG=11079
    Review URL: http://codereview.chromium.org/2822043
    
    TBR=eroman@chromium.org
    Review URL: http://codereview.chromium.org/2945004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51893 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/sync_host_resolver_bridge.h
+++ net/proxy/sync_host_resolver_bridge.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/scoped_ptr.h"
 #include "net/base/host_resolver.h"
+#include "net/proxy/single_threaded_proxy_resolver.h"
 
 class MessageLoop;
 
@@ -35,7 +36,7 @@ class SyncHostResolverBridge : public HostResolver {
   // The Shutdown() method should be called prior to destruction, from
   // |host_resolver_loop_|. It aborts any in progress synchronous resolves, to
   // prevent deadlocks from happening.
-  virtual void Shutdown();
+  void Shutdown();
 
  private:
   class Core;
@@ -43,6 +44,22 @@ class SyncHostResolverBridge : public HostResolver {
   MessageLoop* const host_resolver_loop_;
   scoped_refptr<Core> core_;
 };
+
+// Subclass of SingleThreadedProxyResolver that additionally calls
+// |bridged_host_resolver_->Shutdown()| during its destructor.
+class SingleThreadedProxyResolverUsingBridgedHostResolver
+    : public SingleThreadedProxyResolver {
+ public:
+  SingleThreadedProxyResolverUsingBridgedHostResolver(
+      ProxyResolver* proxy_resolver,
+      SyncHostResolverBridge* bridged_host_resolver);
+
+  virtual ~SingleThreadedProxyResolverUsingBridgedHostResolver();
+
+ private:
+  scoped_refptr<SyncHostResolverBridge> bridged_host_resolver_;
+};
+
 
 }  // namespace net
 
