01f4bc8e24ebfb1f4f9419a4b189588eb9b6f672
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-08 17:26:50 +0000

    https://bugs.webkit.org/show_bug.cgi?id=41641
    Reviewed by Sam Weinig.
    
    Update compile flags to allow use of ExecutableAllocatorFixedVMPool on platforms
    other than x86-64 (this may be useful on 32-bit platforms, too).
    
    Simplify ifdefs by dividing into thwo broad allocation strategies
    (ENABLE_EXECUTABLE_ALLOCATOR_FIXED & ENABLE_EXECUTABLE_ALLOCATOR_DEMAND).
    
    Rename constant used in the code to have names descriptive of their purpose,
    rather than their specific value on a given platform.
    
    * jit/ExecutableAllocator.cpp:
    (JSC::ExecutableAllocator::reprotectRegion):
    (JSC::ExecutableAllocator::cacheFlush):
    * jit/ExecutableAllocatorFixedVMPool.cpp:
    (JSC::FixedVMPoolAllocator::FixedVMPoolAllocator):
    (JSC::FixedVMPoolAllocator::free):
    (JSC::ExecutablePool::systemAlloc):
    * jit/ExecutableAllocatorPosix.cpp:
    * jit/ExecutableAllocatorSymbian.cpp:
    * jit/ExecutableAllocatorWin.cpp:
    * wtf/Platform.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62799 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/jit/ExecutableAllocatorWin.cpp
+++ third_party/WebKit/JavaScriptCore/jit/ExecutableAllocatorWin.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ExecutableAllocator.h"
 
-#if ENABLE(ASSEMBLER) && OS(WINDOWS)
+#if ENABLE(EXECUTABLE_ALLOCATOR_DEMAND) && OS(WINDOWS)
 
 #include "windows.h"
 
