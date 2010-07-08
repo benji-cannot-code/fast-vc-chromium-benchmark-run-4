8ee43a0fc54fbe880098247cdeaebecd2d3ce6ca
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-08 04:37:04 +0000

    2010-07-07  Daniel Bates  <dbates@rim.com>
            Reviewed by Dumitru Daniliuc.
    
            VCSUtils.pm complains about uninitialized value $newLine
            https://bugs.webkit.org/show_bug.cgi?id=41333
    
            Fixes an issue where VCSUtils::fixChangeLogPatch() may read off the end of
            an array when fixing a change log entry that overlaps with an earlier
            entry. In particular, when a patch contains a change log entry inserted
            earlier in the change log file, but after an entry with the same author
            and date.
    
            * Scripts/VCSUtils.pm:
                - Added for-loop constraint to fixChangeLogPatch() so that it does not
                  read off the end of the @overlappingLines array.
            * Scripts/webkitperl/VCSUtils_unittest/fixChangeLogPatch.pl: Added unit test.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitperl/VCSUtils_unittest/fixChangeLogPatch.pl
+++ third_party/WebKit/WebKitTools/Scripts/webkitperl/VCSUtils_unittest/fixChangeLogPatch.pl
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # Unit tests of VCSUtils::fixChangeLogPatch().
 
-use Test::Simple tests => 7;
+use Test::Simple tests => 8;
 use VCSUtils;
 
 # The source ChangeLog for these tests is the following:
@@ -119,6 +119,31 @@ $in = <<'END';
          Changed some code on 2009-12-21.
  
          * File:
+END
+
+ok(fixChangeLogPatch($in) eq $in, $title);
+
+# New test
+$title = "fixChangeLogPatch: [no change] New entry inserted earlier in the file, but after an entry with the same author and date.";
+
+$in = <<'END';
+--- ChangeLog
++++ ChangeLog
+@@ -70,6 +70,14 @@
+ 
+ 2009-12-22  Alice  <alice@email.address>
+ 
++        Reviewed by Sue.
++
++        Changed some more code on 2009-12-22.
++
++        * File:
++
++2009-12-22  Alice  <alice@email.address>
++
+         Reviewed by Ray.
+ 
+         Changed some code on 2009-12-22.
 END
 
 ok(fixChangeLogPatch($in) eq $in, $title);
