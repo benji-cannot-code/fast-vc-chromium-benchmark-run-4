ecf165f6d178603c425a972d0eddfcfdf2c6c218
luiz@webkit.org <luiz@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-08 21:17:12 +0000

    2010-07-08  Luiz Agostini  <luiz.agostini@openbossa.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Move socket objects to connection's thread
            https://bugs.webkit.org/show_bug.cgi?id=41897
    
            To receive network related notifications in the WorkQueue thread, the
            QLocalSocket instances must be moved to that thread.
    
            * Platform/CoreIPC/qt/ConnectionQt.cpp:
            (CoreIPC::Connection::open):
            * Platform/WorkQueue.h:
            * Platform/qt/WorkQueueQt.cpp:
            (WorkQueue::moveSocketToWorkThread):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62837 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Platform/WorkQueue.h
+++ third_party/WebKit/WebKit2/Platform/WorkQueue.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Vector.h>
 
 #if PLATFORM(QT)
+class QLocalSocket;
 class QObject;
 class QThread;
 #endif
@@ -69,7 +70,9 @@ public:
     void unregisterHandle(HANDLE);
 #elif PLATFORM(QT)
     void connectSignal(QObject*, const char* signal, std::auto_ptr<WorkItem>);
-    void disconnectSignal(QObject*, const char* signa);
+    void disconnectSignal(QObject*, const char* signal);
+
+    void moveSocketToWorkThread(QLocalSocket*);
 #endif
 
 private:
--- third_party/WebKit/WebKit2/Platform/qt/WorkQueueQt.cpp
+++ third_party/WebKit/WebKit2/Platform/qt/WorkQueueQt.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WorkQueue.h"
 
+#include <QLocalSocket>
 #include <QObject>
 #include <QThread>
 #include <wtf/Threading.h>
@@ -91,6 +92,15 @@ void WorkQueue::disconnectSignal(QObject* o, const char* name)
         m_signalListeners.remove(it);
         return;
     }
+}
+
+void WorkQueue::moveSocketToWorkThread(QLocalSocket* socket)
+{
+    ASSERT(m_workThread);
+    ASSERT(socket);
+
+    socket->setParent(0);
+    socket->moveToThread(m_workThread);
 }
 
 void WorkQueue::platformInitialize(const char*)
