1a8a218fc0c430be75391683837d753c1ede8f2c
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-08 22:35:15 +0000

    2010-07-08  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Anders Carlsson.
    
            Allow the TestPlugin to use Core Animation rendering for testing
            https://bugs.webkit.org/show_bug.cgi?id=41872
    
            Add the ability for the TestPlugin to use the CoreAnimation rendering model
            when available, based on the "drawingmodel" attribute of the embed tag:
                drawingmodel="coreanimation"
    
            * DumpRenderTree/DumpRenderTree.xcodeproj/project.pbxproj: Add PluginObjectMac.mm
            * DumpRenderTree/TestNetscapePlugIn/PluginObject.h: Add some macros so we can detect
            when building on Tiger. Add a void* coreAnimationLayer member.
    
            * DumpRenderTree/TestNetscapePlugIn/PluginObjectMac.mm: Added.
            (createCoreAnimationLayer): Obj-C method to create the CALayer.
    
            * DumpRenderTree/TestNetscapePlugIn/main.cpp:
            (NPP_New): Look for the drawingmodel attribute to decide which drawing
            model to use.
            (NPP_Destroy): Release the CALayer if we have one.
            (NPP_GetValue): Return the retained CALayer.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62857 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/TestNetscapePlugIn/PluginObject.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/TestNetscapePlugIn/PluginObject.h
@@ -26,6 +26,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <WebKit/npfunctions.h>
 
+#if XP_MACOSX
+#if !defined(MAC_OS_X_VERSION_10_5) || MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_5
+#define BUILDING_ON_TIGER 1
+#elif !defined(MAC_OS_X_VERSION_10_6) || MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_6
+#define BUILDING_ON_LEOPARD 1
+#elif !defined(MAC_OS_X_VERSION_10_7) || MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_7
+#define BUILDING_ON_SNOW_LEOPARD 1
+#endif
+#endif // XP_MACOSX
+
 extern NPNetscapeFuncs *browser;
 
 typedef struct {
@@ -57,6 +67,9 @@ typedef struct {
 #ifdef XP_MACOSX
     NPEventModel eventModel;
 #endif
+#if XP_MACOSX && !defined(BUILDING_ON_TIGER)
+    void* coreAnimationLayer;
+#endif
     NPWindow lastWindow;
 } PluginObject;
 
@@ -67,3 +80,8 @@ extern void testNPRuntime(NPP npp);
 extern void pluginLog(NPP instance, const char* format, ...);
 extern bool testDocumentOpen(NPP npp);
 extern bool testWindowOpen(NPP npp);
+
+#if XP_MACOSX && !defined(BUILDING_ON_TIGER)
+extern void* createCoreAnimationLayer();
+#endif
+
