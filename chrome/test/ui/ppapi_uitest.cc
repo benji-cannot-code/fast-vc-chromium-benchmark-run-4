cde6687f5b08eb99238f4d9c630f3e252f5b5292
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-08 22:10:22 +0000

    Pull PPAPI to get the paint aggregator, hook up UI test. Review URL: http://codereview.chromium.org/2931002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51902 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/ppapi_uitest.cc
+++ chrome/test/ui/ppapi_uitest.cc
@@ -119,6 +119,10 @@ TEST_F(PPAPITest, URLLoader) {
   RunTestViaHTTP("URLLoader");
 }
 
+TEST_F(PPAPITest, PaintAgggregator) {
+  RunTestViaHTTP("PaintAggregator");
+}
+
 // http://crbug.com/48544
 TEST_F(PPAPITest, FAILS_Scrollbar) {
   RunTest("Scrollbar");
