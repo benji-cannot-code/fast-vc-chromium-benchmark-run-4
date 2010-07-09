0d5292f38d862ae33927831a7607ceef814ade15
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-09 13:58:31 +0000

    2010-07-09  Tony Gentilcore  <tonyg@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Implement performance.navigation.type
            https://bugs.webkit.org/show_bug.cgi?id=41564
    
            Expectations set to FAIL because disabled by default. Tests pass with --web-timing.
    
            * fast/dom/navigation-type-back-forward-expected.txt: Added.
            * fast/dom/navigation-type-back-forward.html: Added.
            * fast/dom/navigation-type-navigate-expected.txt: Added.
            * fast/dom/navigation-type-navigate.html: Added.
            * fast/dom/navigation-type-reload-expected.txt: Added.
            * fast/dom/navigation-type-reload.html: Added.
            * fast/dom/resources/navigation-type-matches-querystring.html: Added.
    2010-07-09  Tony Gentilcore  <tonyg@chromium.org>
    
            Reviewed by Dimitri Glazkov.
    
            Implement performance.navigation.type
            https://bugs.webkit.org/show_bug.cgi?id=41564
    
            Tests: fast/dom/navigation-type-back-forward.html
                   fast/dom/navigation-type-navigate.html
                   fast/dom/navigation-type-reload.html
    
            * page/Navigation.cpp:
            (WebCore::Navigation::type):
            * page/Navigation.h:
            (WebCore::Navigation::):
            * page/Navigation.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62947 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Navigation.cpp
+++ third_party/WebKit/WebCore/page/Navigation.cpp
@@ -34,7 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(WEB_TIMING)
 
+#include "DocumentLoader.h"
 #include "Frame.h"
+#include "FrameLoaderTypes.h"
 
 namespace WebCore {
 
@@ -56,9 +58,21 @@ void Navigation::disconnectFrame()
 unsigned short Navigation::type() const
 {
     if (!m_frame)
-        return 0;
+        return Navigate;
 
-    return 0; // FIXME
+    DocumentLoader* documentLoader = m_frame->loader()->documentLoader();
+    if (!documentLoader)
+        return Navigate;
+
+    WebCore::NavigationType navigationType = documentLoader->triggeringAction().type();
+    switch (navigationType) {
+    case NavigationTypeReload:
+        return Reload;
+    case NavigationTypeBackForward:
+        return BackForward;
+    default:
+        return Navigate;
+    }
 }
 
 unsigned short Navigation::redirectCount() const
