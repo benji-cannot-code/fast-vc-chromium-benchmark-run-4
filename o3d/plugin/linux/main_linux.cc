a5af485b1a368cdfce5821ce73585ee73f676cf4
zhurunz@google.com <zhurunz@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-09 17:59:25 +0000

    Some changes in RENDERMODE_CONTINUOUS mode when max_fps has been set.
    	If Renderer::max_fps has been set, rendering is driven by incoming new
    	textures. We draw on each new texture as long as not exceeding max_fps.
    
    	If we are in RENDERMODE_ON_DEMAND mode, Client::Render() can still set dirty
    	specifically.
    
    	If we are in ENDERMODE_CONTINUOUS mode, we do NOT set dirty on each tick any
    	more (since it is already driven by new textures.).
    	There is one problem here: what if new texture don't come in for some reason?
    	If that happened, no rendering callback will be invoked and this can cause
    	problem sometimes. For example, some UI may depend on the rendering callback
    	to work correctly.
    	So, in RENDERMODE_CONTINUOUS mode, if we have set max_fps but haven't
    	received any new texture for a while, we draw anyway to trigger the rendering
    	callback.
    	
    	
    Review URL: http://codereview.chromium.org/2899008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51982 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/plugin/linux/main_linux.cc
+++ o3d/plugin/linux/main_linux.cc
@@ -84,7 +84,7 @@ void LinuxTimer(XtPointer data, XtIntervalId* id) {
   obj->client()->Tick();
   obj->draw_ = true;
   if (obj->renderer()) {
-    if (obj->client()->render_mode() == o3d::Client::RENDERMODE_CONTINUOUS ||
+    if (obj->client()->NeedsContinuousRender() ||
         obj->renderer()->need_to_render()) {
 
       // NOTE: this draws no matter what instead of just invalidating the
@@ -94,7 +94,7 @@ void LinuxTimer(XtPointer data, XtIntervalId* id) {
     }
   }
   obj->xt_interval_ =
-      XtAppAddTimeOut(obj->xt_app_context_, 10, LinuxTimer, obj);
+      XtAppAddTimeOut(obj->xt_app_context_, 8, LinuxTimer, obj);
 }
 
 void LinuxExposeHandler(Widget w,
@@ -607,7 +607,7 @@ static gboolean GtkTimeoutCallback(gpointer user_data) {
   obj->draw_ = true;
   obj->client()->Tick();
   if (obj->renderer()) {
-    if (obj->client()->render_mode() == o3d::Client::RENDERMODE_CONTINUOUS ||
+    if (obj->client()->NeedsContinuousRender() ||
         obj->renderer()->need_to_render()) {
 
       GtkWidget *widget;
@@ -803,7 +803,7 @@ NPError NPP_SetWindow(NPP instance, NPWindow *window) {
       }
       gtk_widget_show(obj->gtk_container_);
       drawable = GDK_WINDOW_XID(obj->gtk_container_->window);
-      obj->timeout_id_ = g_timeout_add(10, GtkTimeoutCallback, obj);
+      obj->timeout_id_ = g_timeout_add(8, GtkTimeoutCallback, obj);
     } else {
       // No XEmbed support, the xwindow is a Xt Widget.
       Widget widget = XtWindowToWidget(display, xwindow);
@@ -823,7 +823,7 @@ NPError NPP_SetWindow(NPP instance, NPWindow *window) {
                         LinuxEnterLeaveHandler, obj);
       obj->xt_app_context_ = XtWidgetToApplicationContext(widget);
       obj->xt_interval_ =
-          XtAppAddTimeOut(obj->xt_app_context_, 10, LinuxTimer, obj);
+          XtAppAddTimeOut(obj->xt_app_context_, 8, LinuxTimer, obj);
     }
 
     // Create and assign the graphics context.
