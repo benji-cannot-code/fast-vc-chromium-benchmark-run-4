9d17bbcb6e869ddb86347e8e4cd23c1dda6a081e
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-09 18:31:15 +0000

    PreRead chrome.dll only for release builds.
    Bug=45510
    
    Review URL: http://codereview.chromium.org/2918003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51988 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/client_util.cc
+++ chrome/app/client_util.cc
@@ -109,6 +109,7 @@ HMODULE LoadChromeWithDirectory(std::wstring* dir) {
   dir->append(installer_util::kChromeDll);
 #endif
 
+#ifdef NDEBUG
   // Experimental pre-reading optimization
   // The idea is to pre read significant portion of chrome.dll in advance
   // so that subsequent hard page faults are avoided.
@@ -143,6 +144,7 @@ HMODULE LoadChromeWithDirectory(std::wstring* dir) {
       file_util::PreReadImage(dir->c_str(), pre_read_size, pre_read_step_size);
     }
   }
+#endif  // NDEBUG
 
   return ::LoadLibraryExW(dir->c_str(), NULL,
                           LOAD_WITH_ALTERED_SEARCH_PATH);
