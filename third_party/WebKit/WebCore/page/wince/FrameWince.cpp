aca6cf4bc67e216ed6c1564c9a23e3101f909cda
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-09 17:12:26 +0000

    2010-07-09  Patrick Gansterer  <paroga@paroga.com>
            Reviewed by Kent Tamura.
    
            [WINCE] Buildfix for FrameWince after r47440
            https://bugs.webkit.org/show_bug.cgi?id=41904
    
            * page/wince/FrameWince.cpp: Added property svn:eol-style.
            (WebCore::computePageRectsForFrame):
            (WebCore::imageFromSelection):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62963 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/wince/FrameWince.cpp
+++ third_party/WebKit/WebCore/page/wince/FrameWince.cpp
@@ -29,28 +29,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Frame.h"
 
 #include "Document.h"
-#include "EditorClient.h"
 #include "FloatRect.h"
-#include "FrameLoader.h"
-#include "FrameLoadRequest.h"
 #include "FrameView.h"
 #include "GraphicsContext.h"
 #include "HTMLIFrameElement.h"
 #include "HTMLNames.h"
 #include "HTMLTableCellElement.h"
 #include "KeyboardEvent.h"
-#include "NP_jsobject.h"
-#include "npruntime_impl.h"
 #include "Page.h"
-#include "Plugin.h"
-#include "RegularExpression.h"
 #include "RenderFrame.h"
-#include "RenderTableCell.h"
+#include "RenderLayer.h"
 #include "RenderView.h"
 #include "ResourceHandle.h"
-#include "runtime_root.h"
-#include "Settings.h"
-#include "TextResourceDecoder.h"
 
 #include <windows.h>
 
@@ -86,7 +76,7 @@ void computePageRectsForFrame(Frame* frame, const IntRect& printRect, float head
 
     float ratio = (float)printRect.height() / (float)printRect.width();
 
-    float pageWidth  = (float) root->overflowWidth();
+    float pageWidth  = (float) root->rightLayoutOverflow();
     float pageHeight = pageWidth * ratio;
     outPageHeight = (int) pageHeight;   // this is the height of the page adjusted by margins
     pageHeight -= (headerHeight + footerHeight);
@@ -130,8 +120,8 @@ HBITMAP imageFromSelection(Frame* frame, bool forceBlackText)
     FrameView* view = frame->view();
     if (view->parent()) {
         ir.setLocation(view->parent()->convertChildToSelf(view, ir.location()));
-        w = ir.width() * view->scale() + 0.5;
-        h = ir.height() * view->scale() + 0.5;
+        w = ir.width() * view->zoomFactor() + 0.5;
+        h = ir.height() * view->zoomFactor() + 0.5;
     } else {
         ir = view->contentsToWindow(ir);
         w = ir.width();
