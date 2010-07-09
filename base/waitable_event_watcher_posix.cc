f37250db318acfdf3ece8ee12319ea31ab51009d
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-09 02:26:33 +0000

    Coverity: uninitialized member in posix WaitableEventWatcher constructor.
    CID=9290
    TEST=builds
    BUG=none
    
    Review URL: http://codereview.chromium.org/2954001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51917 0039d316-1c4b-4281-b951-d872f2087c98

--- base/waitable_event_watcher_posix.cc
+++ base/waitable_event_watcher_posix.cc
@@ -123,6 +123,7 @@ WaitableEventWatcher::WaitableEventWatcher()
     : event_(NULL),
       message_loop_(NULL),
       cancel_flag_(NULL),
+      waiter_(NULL),
       callback_task_(NULL),
       delegate_(NULL) {
 }
