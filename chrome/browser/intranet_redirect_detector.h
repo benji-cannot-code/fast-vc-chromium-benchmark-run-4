4f51f96e39ee55cce2b6a8ee1f73bbf2ceb88f0c
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-09 05:16:20 +0000

    Rerun intranet check on network changes.
    BUG=35524
    TEST=Open chromium while on a "normal" network. Switch to a network that does dns hijacking. Type a single-word query into omnibox and hit return. No "Did you mean to go to http://query" infobar should open.
    
    Review URL: http://codereview.chromium.org/2910001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51934 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/intranet_redirect_detector.h
+++ chrome/browser/intranet_redirect_detector.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_registrar.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/host_resolver_proc.h"
+#include "net/base/network_change_notifier.h"
 
 class PrefService;
 
@@ -34,7 +35,8 @@ class PrefService;
 // return a value at all times (even during startup or in unittest mode).  If no
 // redirection is in place, the returned GURL will be empty.
 class IntranetRedirectDetector : public URLFetcher::Delegate,
-                                 public NotificationObserver {
+                                 public NotificationObserver,
+                                 public net::NetworkChangeNotifier::Observer {
  public:
   // Only the main browser process loop should call this, when setting up
   // g_browser_process->intranet_redirect_detector_.  No code other than the
@@ -58,8 +60,8 @@ class IntranetRedirectDetector : public URLFetcher::Delegate,
  private:
   typedef std::set<URLFetcher*> Fetchers;
 
-  // Called when the five second startup sleep has finished.  Runs any pending
-  // fetch.
+  // Called when the seven second startup sleep or the one second network
+  // switch sleep has finished.  Runs any pending fetch.
   void FinishSleep();
 
   // Starts the fetches to determine the redirect URL if we can currently do so.
@@ -78,13 +80,17 @@ class IntranetRedirectDetector : public URLFetcher::Delegate,
                        const NotificationSource& source,
                        const NotificationDetails& details);
 
+  // NetworkChangeNotifier::Observer
+  virtual void OnIPAddressChanged();
+
   NotificationRegistrar registrar_;
   GURL redirect_origin_;
   ScopedRunnableMethodFactory<IntranetRedirectDetector> fetcher_factory_;
   Fetchers fetchers_;
   std::vector<GURL> resulting_origins_;
-  bool in_startup_sleep_;  // True if we're in the seven-second "no fetching"
-                           // period that begins at browser start.
+  bool in_sleep_;  // True if we're in the seven-second "no fetching" period
+                   // that begins at browser start, or the one-second "no
+                   // fetching" period that begins after network switches.
   bool request_context_available_;
                            // True when the profile has been loaded and the
                            // default request context created, so we can
