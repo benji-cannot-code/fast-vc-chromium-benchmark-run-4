3ec493947ebac1447319d8a760341947df3b44c6
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-09 09:40:26 +0000

    2010-07-09  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            commit-queue should merge to TOT when checkout needs update
            https://bugs.webkit.org/show_bug.cgi?id=41944
    
            There are a bunch of different designs that are possible here.  This
            one merges to top of tree by cleaning out the working copy and
            re-applying the patch.  Once you decide to merge that way, you need to
            decide who's going to retry.  In this patch, we retry in the child
            process instead of plumbing the failure reason to the master process.
    
            This patch is difficult to test end-to-end, but hopefully it will work.
            :)
    
            * Scripts/webkitpy/tool/commands/queues.py:
            * Scripts/webkitpy/tool/commands/stepsequence.py:
            * Scripts/webkitpy/tool/multicommandtool.py:
            * Scripts/webkitpy/tool/multicommandtool_unittest.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@62923 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/commands/stepsequence.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/commands/stepsequence.py
@@ -40,6 +40,10 @@ class StepSequenceErrorHandler():
     def handle_script_error(cls, tool, patch, script_error):
         raise NotImplementedError, "subclasses must implement"
 
+    @classmethod
+    def handle_checkout_needs_update(cls, tool, state, options, error):
+        raise NotImplementedError, "subclasses must implement"
+
 
 class StepSequence(object):
     def __init__(self, steps):
@@ -67,6 +71,9 @@ class StepSequence(object):
             self._run(tool, options, state)
         except CheckoutNeedsUpdate, e:
             log("Commit failed because the checkout is out of date.  Please update and try again.")
+            if options.parent_command:
+                command = tool.command_by_name(options.parent_command)
+                command.handle_checkout_needs_update(tool, state, options, e)
             QueueEngine.exit_after_handled_error(e)
         except ScriptError, e:
             if not options.quiet:
