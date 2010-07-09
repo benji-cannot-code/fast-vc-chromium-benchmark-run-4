ca34f893a283f827b6495363fb4556c52d562c5f
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-09 20:04:05 +0000

    Made chrome: schema savable. Reenabled print context menu that was previously disabled in ChromeOS.
    BUG=chrome-os:4073
    TEST=open about:system, right mouse click and pick Save As menu item
    
    Review URL: http://codereview.chromium.org/2880009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51998 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -38,6 +38,7 @@ const char* kSavableSchemes[] = {
   kFileScheme,
   kFtpScheme,
   kExtensionScheme,
+  kChromeUIScheme,
   NULL
 };
 
@@ -51,6 +52,7 @@ const char kAboutMemoryURL[] = "about:memory";
 const char kAboutNetInternalsURL[] = "about:net-internals";
 const char kAboutPluginsURL[] = "about:plugins";
 const char kAboutShorthangURL[] = "about:shorthang";
+const char kAboutSystemURL[] = "about:system";
 const char kAboutTermsURL[] = "about:terms";
 const char kAboutAboutURL[] = "about:about";
 const char kAboutDNSURL[] = "about:dns";
