80887c827fa875252c9717967bd7703708067e20
davidben@chromium.org <davidben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-09 16:58:38 +0000

    KDE treats all host patterns as wildcard patterns
    Also correct a test description that became inaccurate when we supported a
    reversed bypass list, and remove spaces from host rules earlier. (KDE actually
    treats both comma and space as valid delimiters anyway.)
    
    R=wtc,eroman
    BUG=48486
    TEST=ProxyConfigServiceLinuxTest.KDEConfigParser
    
    Review URL: http://codereview.chromium.org/2848041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51971 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_bypass_rules.cc
+++ net/proxy/proxy_bypass_rules.cc
@@ -175,6 +175,11 @@ bool ProxyBypassRules::AddRuleFromString(const std::string& raw) {
   return AddRuleFromStringInternalWithLogging(raw, false);
 }
 
+bool ProxyBypassRules::AddRuleFromStringUsingSuffixMatching(
+    const std::string& raw) {
+  return AddRuleFromStringInternalWithLogging(raw, true);
+}
+
 void ProxyBypassRules::Clear() {
   rules_.clear();
 }
