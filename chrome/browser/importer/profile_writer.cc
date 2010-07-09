bd751501eaa3f63a2061744ac988568f6b67d965
huanr@chromium.org <huanr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-09 18:30:45 +0000

    Make bookmark import not to enable bookmark bar by default.
    BUG=46528
    TEST=mini_installer.exe --installerdata=<file_path>
    
    Review URL: http://codereview.chromium.org/2962004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51987 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/profile_writer.cc
+++ chrome/browser/importer/profile_writer.cc
@@ -132,7 +132,7 @@ void ProfileWriter::AddBookmarkEntry(
     model->EndImportMode();
   }
 
-  if (show_bookmark_toolbar)
+  if (show_bookmark_toolbar && !(options & BOOKMARK_BAR_DISABLED))
     ShowBookmarkBar();
 }
 
