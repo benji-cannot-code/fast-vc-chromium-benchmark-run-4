e64c8e00b4b69dee902767f2f0d1eb37cef8a31d
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-09 17:17:59 +0000

    Restore the bookmark manager favicon.
    The favicon used to be loaded from a png file in
    the resources directory.  Since the file is gone,
    this no longer works.  Instead, check for the bookmark
    manager extension earlier and load the image from the
    pak file.
    
    BUG=48218
    
    Review URL: http://codereview.chromium.org/2960002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51975 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/dom_ui_factory.cc
+++ chrome/browser/dom_ui/dom_ui_factory.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/url_constants.h"
 #include "googleurl/src/gurl.h"
 
@@ -174,14 +175,16 @@ DOMUI* DOMUIFactory::CreateDOMUIForURL(TabContents* tab_contents,
 // static
 RefCountedMemory* DOMUIFactory::GetFaviconResourceBytes(Profile* profile,
                                                         const GURL& page_url) {
+  // The bookmark manager is a chrome extension, so we have to check for it
+  // before we check for extension scheme.
+  if (page_url.host() == extension_misc::kBookmarkManagerId)
+    return BookmarksUI::GetFaviconResourceBytes();
+
   if (page_url.SchemeIs(chrome::kExtensionScheme))
     return ExtensionDOMUI::GetFaviconResourceBytes(profile, page_url);
 
   if (!HasDOMUIScheme(page_url))
     return NULL;
-
-  if (page_url.host() == chrome::kChromeUIBookmarksHost)
-    return BookmarksUI::GetFaviconResourceBytes();
 
   if (page_url.host() == chrome::kChromeUIDownloadsHost)
     return DownloadsUI::GetFaviconResourceBytes();
--- chrome/common/extensions/extension_constants.cc
+++ chrome/common/extensions/extension_constants.cc
@@ -280,3 +280,7 @@ const char* kDecodedImagesFilename = "DECODED_IMAGES";
 // extension_path.
 const char* kDecodedMessageCatalogsFilename = "DECODED_MESSAGE_CATALOGS";
 }
+
+namespace extension_misc {
+const char* kBookmarkManagerId = "eemcgdkfndhakfknompkggombfjjjeno";
+}
