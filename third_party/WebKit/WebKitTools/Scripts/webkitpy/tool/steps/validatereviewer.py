fbc3901269466256d0589815d3ebb4d33d1fd159
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-09 22:43:00 +0000

    2010-07-09  Ojan Vafai  <ojan@chromium.org>
            Reviewed by Adam Barth.
    
            --squash should go away and become the default
            https://bugs.webkit.org/show_bug.cgi?id=39624
    
            If there are local commits and working copy changes, then prompt the user
            whether to continue. Setting git config webkit-patch.commit_should_always_squash
            true bypasses the prompt.
    
            --git-commit=HEAD.. operates on working copy changes only.
            --git-commit=committish operates on a range of commits as a single commit.
                e.g. --git-commit=HEAD only operates on the HEAD commit.
                --git-commit=HEAD~4..HEAD~2 will operate on HEAD~3 and HEAD~2 as a single commit.
    
            --no-squash and --squash are left in with descriptive error messages if used.
    
            * Scripts/check-webkit-style:
            * Scripts/webkitpy/common/checkout/api.py:
            * Scripts/webkitpy/common/checkout/api_unittest.py:
            * Scripts/webkitpy/common/checkout/scm.py:
            * Scripts/webkitpy/common/checkout/scm_unittest.py:
            * Scripts/webkitpy/common/net/rietveld.py:
            * Scripts/webkitpy/common/system/user.py:
            * Scripts/webkitpy/style/optparser.py:
            * Scripts/webkitpy/style_references.py:
            * Scripts/webkitpy/tool/commands/download.py:
            * Scripts/webkitpy/tool/commands/upload.py:
            * Scripts/webkitpy/tool/mocktool.py:
            * Scripts/webkitpy/tool/steps/abstractstep.py:
            * Scripts/webkitpy/tool/steps/applypatchwithlocalcommit.py:
            * Scripts/webkitpy/tool/steps/checkstyle.py:
            * Scripts/webkitpy/tool/steps/checkstyle_unittest.py: Removed.
            * Scripts/webkitpy/tool/steps/commit.py:
            * Scripts/webkitpy/tool/steps/options.py:
            * Scripts/webkitpy/tool/steps/preparechangelog.py:
            * Scripts/webkitpy/tool/steps/preparechangelogforrevert.py:
            * Scripts/webkitpy/tool/steps/updatechangelogswithreviewer.py:
            * Scripts/webkitpy/tool/steps/validatereviewer.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/steps/validatereviewer.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/steps/validatereviewer.py
@@ -42,8 +42,6 @@ class ValidateReviewer(AbstractStep):
     def options(cls):
         return AbstractStep.options() + [
             Options.git_commit,
-            Options.no_squash,
-            Options.squash,
         ]
 
     # FIXME: This should probably move onto ChangeLogEntry
