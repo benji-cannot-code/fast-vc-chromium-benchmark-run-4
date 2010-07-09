c8915f98819846378d36997a0763d6b9f7250c71
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-09 21:01:35 +0000

    Update Chrome to pull latest PPAPI with new uses of point and size.
    This also pulls in the renamed enums from Darin's patch
    http://codereview.chromium.org/2900003
    
    TEST=covered by unit tests
    BUG=none
    Review URL: http://codereview.chromium.org/2925007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52010 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/pepper_file_io.cc
+++ webkit/glue/plugins/pepper_file_io.cc
@@ -38,11 +38,11 @@ int32_t Open(PP_Resource file_io_id,
              PP_CompletionCallback callback) {
   scoped_refptr<FileIO> file_io(Resource::GetAs<FileIO>(file_io_id));
   if (!file_io.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   scoped_refptr<FileRef> file_ref(Resource::GetAs<FileRef>(file_ref_id));
   if (!file_ref.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   return file_io->Open(file_ref, open_flags, callback);
 }
@@ -52,7 +52,7 @@ int32_t Query(PP_Resource file_io_id,
               PP_CompletionCallback callback) {
   scoped_refptr<FileIO> file_io(Resource::GetAs<FileIO>(file_io_id));
   if (!file_io.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   return file_io->Query(info, callback);
 }
@@ -63,7 +63,7 @@ int32_t Touch(PP_Resource file_io_id,
               PP_CompletionCallback callback) {
   scoped_refptr<FileIO> file_io(Resource::GetAs<FileIO>(file_io_id));
   if (!file_io.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   return file_io->Touch(last_access_time, last_modified_time, callback);
 }
@@ -75,7 +75,7 @@ int32_t Read(PP_Resource file_io_id,
              PP_CompletionCallback callback) {
   scoped_refptr<FileIO> file_io(Resource::GetAs<FileIO>(file_io_id));
   if (!file_io.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   return file_io->Read(offset, buffer, bytes_to_read, callback);
 }
@@ -87,7 +87,7 @@ int32_t Write(PP_Resource file_io_id,
               PP_CompletionCallback callback) {
   scoped_refptr<FileIO> file_io(Resource::GetAs<FileIO>(file_io_id));
   if (!file_io.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   return file_io->Write(offset, buffer, bytes_to_write, callback);
 }
@@ -97,7 +97,7 @@ int32_t SetLength(PP_Resource file_io_id,
                   PP_CompletionCallback callback) {
   scoped_refptr<FileIO> file_io(Resource::GetAs<FileIO>(file_io_id));
   if (!file_io.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   return file_io->SetLength(length, callback);
 }
@@ -106,7 +106,7 @@ int32_t Flush(PP_Resource file_io_id,
               PP_CompletionCallback callback) {
   scoped_refptr<FileIO> file_io(Resource::GetAs<FileIO>(file_io_id));
   if (!file_io.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   return file_io->Flush(callback);
 }
@@ -135,7 +135,7 @@ const PPB_FileIO ppb_fileio = {
 int32_t GetOSFileDescriptor(PP_Resource file_io_id) {
   scoped_refptr<FileIO> file_io(Resource::GetAs<FileIO>(file_io_id));
   if (!file_io.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   return file_io->GetOSFileDescriptor();
 }
@@ -146,7 +146,7 @@ int32_t WillWrite(PP_Resource file_io_id,
                   PP_CompletionCallback callback) {
   scoped_refptr<FileIO> file_io(Resource::GetAs<FileIO>(file_io_id));
   if (!file_io.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   return file_io->WillWrite(offset, bytes_to_write, callback);
 }
@@ -156,7 +156,7 @@ int32_t WillSetLength(PP_Resource file_io_id,
                       PP_CompletionCallback callback) {
   scoped_refptr<FileIO> file_io(Resource::GetAs<FileIO>(file_io_id));
   if (!file_io.get())
-    return PP_Error_BadResource;
+    return PP_ERROR_BADRESOURCE;
 
   return file_io->WillSetLength(length, callback);
 }
@@ -189,20 +189,20 @@ int32_t FileIO::Open(FileRef* file_ref,
                      int32_t open_flags,
                      PP_CompletionCallback callback) {
   NOTIMPLEMENTED();  // TODO(darin): Implement me!
-  return PP_Error_Failed;
+  return PP_ERROR_FAILED;
 }
 
 int32_t FileIO::Query(PP_FileInfo* info,
                       PP_CompletionCallback callback) {
   NOTIMPLEMENTED();  // TODO(darin): Implement me!
-  return PP_Error_Failed;
+  return PP_ERROR_FAILED;
 }
 
 int32_t FileIO::Touch(PP_Time last_access_time,
                       PP_Time last_modified_time,
                       PP_CompletionCallback callback) {
   NOTIMPLEMENTED();  // TODO(darin): Implement me!
-  return PP_Error_Failed;
+  return PP_ERROR_FAILED;
 }
 
 int32_t FileIO::Read(int64_t offset,
@@ -210,7 +210,7 @@ int32_t FileIO::Read(int64_t offset,
                      int32_t bytes_to_read,
                      PP_CompletionCallback callback) {
   NOTIMPLEMENTED();  // TODO(darin): Implement me!
-  return PP_Error_Failed;
+  return PP_ERROR_FAILED;
 }
 
 int32_t FileIO::Write(int64_t offset,
@@ -218,18 +218,18 @@ int32_t FileIO::Write(int64_t offset,
                       int32_t bytes_to_write,
                       PP_CompletionCallback callback) {
   NOTIMPLEMENTED();  // TODO(darin): Implement me!
-  return PP_Error_Failed;
+  return PP_ERROR_FAILED;
 }
 
 int32_t FileIO::SetLength(int64_t length,
                           PP_CompletionCallback callback) {
   NOTIMPLEMENTED();  // TODO(darin): Implement me!
-  return PP_Error_Failed;
+  return PP_ERROR_FAILED;
 }
 
 int32_t FileIO::Flush(PP_CompletionCallback callback) {
   NOTIMPLEMENTED();  // TODO(darin): Implement me!
-  return PP_Error_Failed;
+  return PP_ERROR_FAILED;
 }
 
 void FileIO::Close() {
@@ -238,20 +238,20 @@ void FileIO::Close() {
 
 int32_t FileIO::GetOSFileDescriptor() {
   NOTIMPLEMENTED();  // TODO(darin): Implement me!
-  return PP_Error_Failed;
+  return PP_ERROR_FAILED;
 }
 
 int32_t FileIO::WillWrite(int64_t offset,
                           int32_t bytes_to_write,
                           PP_CompletionCallback callback) {
   NOTIMPLEMENTED();  // TODO(darin): Implement me!
-  return PP_Error_Failed;
+  return PP_ERROR_FAILED;
 }
 
 int32_t FileIO::WillSetLength(int64_t length,
                               PP_CompletionCallback callback) {
   NOTIMPLEMENTED();  // TODO(darin): Implement me!
-  return PP_Error_Failed;
+  return PP_ERROR_FAILED;
 }
 
 }  // namespace pepper
