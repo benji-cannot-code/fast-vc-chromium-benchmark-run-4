abf8eac04895b1c5d4df2cbc97b65b8ef49da7c5
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-10 02:30:43 +0000

    2010-07-09  James Robinson  <jamesr@chromium.org>
            Unreviewed build fix.  Add #include to pick up ExceptionCode values for config (like Qt) where it's not picked up indirectly.
    
            * html/HTMLCanvasElement.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLCanvasElement.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CanvasStyle.h"
 #include "Chrome.h"
 #include "Document.h"
+#include "ExceptionCode.h"
 #include "Frame.h"
 #include "GraphicsContext.h"
 #include "HTMLNames.h"
