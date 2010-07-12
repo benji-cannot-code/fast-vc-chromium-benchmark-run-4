da960c7f7044514ba8817df87569c7a7879686e1
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-12 17:30:58 +0000

    ChromeFrame would leave chrome processes running if a page rendered in Chrome would open up a popup window which opened up in IE. This is because of an orphan external tab container created to handle the popup. This tab is destroyed in the normal case when the active document window is destroyed, which is the parent of the external tab container.
    However in this case the active document is destroyed without its window being created thus resulting in the external tab
    container lying around which holds a reference to the automation provider.
    
    Fix is to pass the ChromeFrameAutomationClient window as the parent window in the AutomationMsg_ConnectExternalTab
    message.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=48732
    
    Bug=48732
    
    Covered by ChromeFrame test FullTabNavigationTest.JavascriptWindowOpenDifferentDomain
    
    
    Review URL: http://codereview.chromium.org/2911006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52094 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/external_tab_container_win.cc
+++ chrome/browser/external_tab_container_win.cc
@@ -198,7 +198,8 @@ void ExternalTabContainer::Uninitialize() {
 
 bool ExternalTabContainer::Reinitialize(
     AutomationProvider* automation_provider,
-    AutomationResourceMessageFilter* filter) {
+    AutomationResourceMessageFilter* filter,
+    gfx::NativeWindow parent_window) {
   if (!automation_provider || !filter) {
     NOTREACHED();
     return false;
@@ -212,6 +213,9 @@ bool ExternalTabContainer::Reinitialize(
       FROM_HERE,
       external_method_factory_.NewRunnableMethod(
           &ExternalTabContainer::OnReinitialize));
+
+  if (parent_window)
+    SetParent(GetNativeView(), parent_window);
   return true;
 }
 
