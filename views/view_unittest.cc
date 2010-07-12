99dcbdab9d3fe1955223872218fe30925040194c
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-12 21:23:11 +0000

    TTF: Re-enable ViewTest.RerouteMouseWheelTest by marking it as FAILS_.
    BUG=10572
    TEST=ViewTest.RerouteMouseWheelTest
    
    Review URL: http://codereview.chromium.org/2946010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52131 0039d316-1c4b-4281-b951-d872f2087c98

--- views/view_unittest.cc
+++ views/view_unittest.cc
@@ -959,7 +959,7 @@ class SimpleWindowDelegate : public WindowDelegate {
 // under the mouse.
 // TODO(jcampan): http://crbug.com/10572 Disabled as it fails on the Vista build
 //                bot.
-TEST_F(ViewTest, DISABLED_RerouteMouseWheelTest) {
+TEST_F(ViewTest, FAILS_RerouteMouseWheelTest) {
   TestViewWithControls* view_with_controls = new TestViewWithControls();
   views::Window* window1 =
       views::Window::CreateChromeWindow(
