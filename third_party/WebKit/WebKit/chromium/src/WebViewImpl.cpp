c54f7cb066bb9ee489c9119582d51124f02838b1
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-12 14:07:07 +0000

    2010-07-12  Alexander Pavlov  <apavlov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            [Chromium] Crash when stepping on a breakpoint while debugging Web Inspector
            https://bugs.webkit.org/show_bug.cgi?id=41958
    
            WebCore:
            * page/PageGroupLoadDeferrer.cpp:
            (WebCore::PageGroupLoadDeferrer::PageGroupLoadDeferrer):
            * page/PageGroupLoadDeferrer.h:
    
            WebKit/chromium:
            * src/WebViewImpl.cpp:
            (WebKit::WebView::willEnterModalLoop):
            (WebKit::WebView::didExitModalLoop):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63080 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Settings.h"
 #include "Timer.h"
 #include "TypingCommand.h"
+#include "Vector.h"
 #include "WebAccessibilityObject.h"
 #include "WebDevToolsAgentPrivate.h"
 #include "WebDevToolsAgentImpl.h"
@@ -140,8 +141,8 @@ static const double maxTextSizeMultiplier = 3.0;
 const char* pageGroupName = "default";
 
 // Used to defer all page activity in cases where the embedder wishes to run
-// a nested event loop.
-static PageGroupLoadDeferrer* pageGroupLoadDeferrer;
+// a nested event loop. Using a stack enables nesting of message loop invocations.
+static Vector<PageGroupLoadDeferrer*> pageGroupLoadDeferrerStack;
 
 // Ensure that the WebDragOperation enum values stay in sync with the original
 // DragOperation constants.
@@ -191,19 +192,19 @@ void WebView::willEnterModalLoop()
     ASSERT(pageGroup);
 
     if (pageGroup->pages().isEmpty())
-        return;
-
-    // Pick any page in the page group since we are deferring all pages.
-    pageGroupLoadDeferrer = new PageGroupLoadDeferrer(*pageGroup->pages().begin(), true, pageGroupLoadDeferrer);
+        pageGroupLoadDeferrerStack.append(static_cast<PageGroupLoadDeferrer*>(0));
+    else {
+        // Pick any page in the page group since we are deferring all pages.
+        pageGroupLoadDeferrerStack.append(new PageGroupLoadDeferrer(*pageGroup->pages().begin(), true));
+    }
 }
 
 void WebView::didExitModalLoop()
 {
-    ASSERT(pageGroupLoadDeferrer);
-    PageGroupLoadDeferrer* nextDeferrer = pageGroupLoadDeferrer->nextDeferrer();
+    ASSERT(pageGroupLoadDeferrerStack.size());
 
-    delete pageGroupLoadDeferrer;
-    pageGroupLoadDeferrer = nextDeferrer;
+    delete pageGroupLoadDeferrerStack.last();
+    pageGroupLoadDeferrerStack.removeLast();
 }
 
 void WebViewImpl::initializeMainFrame(WebFrameClient* frameClient)
