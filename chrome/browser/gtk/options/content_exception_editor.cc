b400b98754d544e808029ad40ccde270019ca966
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-12 23:04:27 +0000

    Clean up app_resources.grd: * Alphabetize a couple entries * Move things to theme_resources.grd where possible * Scope things only used by views under an appropriate conditional
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/2953005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/options/content_exception_editor.cc
+++ chrome/browser/gtk/options/content_exception_editor.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/host_content_settings_map.h"
 #include "grit/app_resources.h"
 #include "grit/generated_resources.h"
+#include "grit/theme_resources.h"
 
 ContentExceptionEditor::ContentExceptionEditor(
     GtkWindow* parent,
--- chrome/browser/views/options/exception_editor_view.cc
+++ chrome/browser/views/options/exception_editor_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/url_parse.h"
 #include "grit/app_resources.h"
 #include "grit/generated_resources.h"
+#include "grit/theme_resources.h"
 #include "views/grid_layout.h"
 #include "views/controls/image_view.h"
 #include "views/controls/label.h"
