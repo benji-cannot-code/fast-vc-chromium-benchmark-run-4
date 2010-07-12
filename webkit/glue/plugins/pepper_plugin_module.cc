c9ee0029a36dca18de1ab60bc82cbab22a5b27a4
wjia@google.com <wjia@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-12 23:45:03 +0000

    add find hookup BUG=none TEST=compiles
    Review URL: http://codereview.chromium.org/2896009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52151 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/pepper_plugin_module.cc
+++ webkit/glue/plugins/pepper_plugin_module.cc
@@ -20,8 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/ppapi/c/ppb_file_io_trusted.h"
 #include "third_party/ppapi/c/ppb_file_system.h"
 #include "third_party/ppapi/c/ppb_image_data.h"
-#include "third_party/ppapi/c/ppb_font.h"
 #include "third_party/ppapi/c/ppb_instance.h"
+#include "third_party/ppapi/c/ppb_find.h"
+#include "third_party/ppapi/c/ppb_font.h"
 #include "third_party/ppapi/c/ppb_scrollbar.h"
 #include "third_party/ppapi/c/ppb_testing.h"
 #include "third_party/ppapi/c/ppb_url_loader.h"
@@ -186,6 +187,8 @@ const void* GetInterface(const char* name) {
     return Scrollbar::GetInterface();
   if (strcmp(name, PPB_FONT_INTERFACE) == 0)
     return Font::GetInterface();
+  if (strcmp(name, PPB_FIND_INTERFACE) == 0)
+    return PluginInstance::GetFindInterface();
 
   // Only support the testing interface when the command line switch is
   // specified. This allows us to prevent people from (ab)using this interface
