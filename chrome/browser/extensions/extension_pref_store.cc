8a3fa31bf6a52cf008a01e15f4bfd786f34f651b
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-12 23:43:17 +0000

    Fix memory leak in extension pref store BUG=none
    Memory bot which will turn green with this change:
    http://build.chromium.org/buildbot/memory/builders/Linux%20Heapcheck/builds/4870/steps/heapcheck%20test:%20unit/logs/stdio
    
    
    Review URL: http://codereview.chromium.org/2923008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52150 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_pref_store.cc
+++ chrome/browser/extensions/extension_pref_store.cc
@@ -6,12 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_pref_store.h"
 
 #include "base/logging.h"
+#include "base/stl_util-inl.h"
 #include "base/values.h"
 #include "chrome/browser/pref_service.h"
 
 ExtensionPrefStore::ExtensionPrefStore(PrefService* pref_service)
     : pref_service_(pref_service),
       prefs_(new DictionaryValue()) {
+}
+
+ExtensionPrefStore::~ExtensionPrefStore() {
+  STLDeleteElements(&extension_stack_);
 }
 
 // This could be sped up by keeping track of which extension currently controls
@@ -88,6 +93,7 @@ void ExtensionPrefStore::UninstallExtension(std::string extension_id) {
       i != extension_stack_.end(); ++i) {
     if ((*i)->extension_id == extension_id) {
       pref_values.reset((*i)->pref_values);
+      delete *i;
       extension_stack_.erase(i);
       break;
     }
