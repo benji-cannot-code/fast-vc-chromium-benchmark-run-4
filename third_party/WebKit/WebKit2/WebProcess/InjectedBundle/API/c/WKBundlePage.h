5e8564f0bf4c15bff2d6f15b6dadcc088ef329ab
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-13 15:00:09 +0000

    WebCore: [Qt] Land the initial build system for WebKit2 https://bugs.webkit.org/show_bug.cgi?id=41604
    Patch by Antti Koivisto <koivisto@iki.fi> on 2010-07-13
    Reviewed by Kenneth Rohde Christiansen.
    
    The build is enabled by passing --qmakearg="CONFIG+=webkit2" to build-webkit
    
    * WebCore.pri:
    * WebCore.pro:
    
    WebKit2: [Qt] Tweaks needed to compile WebKit2 with Qt
    https://bugs.webkit.org/show_bug.cgi?id=41604
    
    Reviewed by Kenneth Rohde Christiansen.
    
    * UIProcess/Launcher/qt/ProcessLauncherQt.cpp:
    (WebKit::ProcessLauncherHelper::launch): Adjust to PassOwnPtr API changes.
    * WebProcess/InjectedBundle/API/c/WKBundlePage.h: Don't include JavaScriptCore.h,
    as it unconditionally includes JSStringRefCF. Instead include JavaScript.h.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundlePage.h
+++ third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundlePage.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WKBundlePage_h
 #define WKBundlePage_h
 
-#include <JavaScriptCore/JavaScriptCore.h>
+#include <JavaScriptCore/JavaScript.h>
 #include <WebKit2/WKBase.h>
 #include <WebKit2/WKBundleBase.h>
 
