3d9b63e070ae24c4d628adcce7bf4ecc9c5e62e4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-13 20:24:18 +0000

    2010-07-13  Rafael Antognolli  <antognolli@profusion.mobi>
            Reviewed by Antonio Gomes.
    
            This commit implements an API for configuring a path to the cookie
            file in the Mozilla "cookies.txt" format, and functions to deal with it
            in a more EFL like API.
    
            [EFL] Add API for dealing with cookies in EFL port.
            https://bugs.webkit.org/show_bug.cgi?id=41770
    
            * CMakeListsEfl.txt:
            * efl/ewk/EWebKit.h:
            * efl/ewk/ewk_cookies.cpp: Added.
            (ewk_cookies_file_set): Set a path to the file storing cookies.
            (ewk_cookies_clear): Clear all the cookies from the current cookie jar.
            (ewk_cookies_get_all): Get all the cookies from the current cookie jar.
            (ewk_cookies_cookie_del): Delete a specific cookie from the cookie jar.
            (ewk_cookies_cookie_free): Free memory used by a cookie.
            (ewk_cookies_policy_set): Set which acceptance policy will be used.
            (ewk_cookies_policy_get): Get current acceptance policy being used.
            * efl/ewk/ewk_cookies.h: Added.
            (_Ewk_Cookie::):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/efl/ewk/EWebKit.h
+++ third_party/WebKit/WebKit/efl/ewk/EWebKit.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EWebKit_h
 
 #include "ewk_contextmenu.h"
+#include "ewk_cookies.h"
 #include "ewk_eapi.h"
 #include "ewk_frame.h"
 #include "ewk_history.h"
