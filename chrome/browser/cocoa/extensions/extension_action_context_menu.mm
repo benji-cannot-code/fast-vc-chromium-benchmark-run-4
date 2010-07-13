4d2d9dc630a9ba1e454ae3ff4125f529667057a6
andybons@chromium.org <andybons@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-13 17:24:48 +0000

    [Mac] Finish up extension infobar UI implementation. o One issue under consideration is whether the HTML content of the infobar can take up the entire width. Right now on Windows it does not but with this implementation (and on Linux) it does.
    BUG=43168
    TEST=ExtensionApiTest.Infobars
    Review URL: http://codereview.chromium.org/2973003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/extensions/extension_action_context_menu.mm
+++ chrome/browser/cocoa/extensions/extension_action_context_menu.mm
@@ -244,6 +244,7 @@ int CurrentTabId() {
           (AutocompleteTextField*)locationBar->location_entry()->
           GetNativeView();
       AutocompleteTextFieldCell* fieldCell = [field autocompleteTextFieldCell];
+      DCHECK(action_);
       NSRect popupRect =
           [fieldCell pageActionFrameForExtensionAction:action_
                                                inFrame:[field bounds]];
@@ -277,8 +278,8 @@ int CurrentTabId() {
 }
 
 - (BOOL)validateMenuItem:(NSMenuItem*)menuItem {
-  if([menuItem isEqualTo: inspectorItem_.get()]) {
-    return (action_->HasPopup(CurrentTabId()));
+  if([menuItem isEqualTo:inspectorItem_.get()]) {
+    return action_ && action_->HasPopup(CurrentTabId());
   }
   return YES;
 }
--- chrome/browser/extensions/extension_infobar_apitest.cc
+++ chrome/browser/extensions/extension_infobar_apitest.cc
@@ -7,11 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_switches.h"
 
-#if defined(TOOLKIT_VIEWS)
+#if defined(TOOLKIT_VIEWS) || defined(OS_MACOSX)
 #define MAYBE_Infobars Infobars
 #else
-// Need to port ExtensionInfoBarDelegate::CreateInfoBar() to other platforms.
-// See http://crbug.com/39916 for details.
+// Need to finish port to Linux. See http://crbug.com/39916 for details.
 #define MAYBE_Infobars DISABLED_Infobars
 #endif
 
