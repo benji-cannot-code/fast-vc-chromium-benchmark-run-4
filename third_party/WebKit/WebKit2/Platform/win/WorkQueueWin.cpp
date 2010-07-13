5552b806041c7b35a45b773655daa6541128718e
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-13 06:01:31 +0000

    Windows build fix.
    git-svn-id: svn://svn.chromium.org/blink/trunk@63174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Platform/win/WorkQueueWin.cpp
+++ third_party/WebKit/WebKit2/Platform/win/WorkQueueWin.cpp
@@ -103,7 +103,7 @@ void WorkQueue::platformInvalidate()
     // FIXME: Stop the thread and do other cleanup.
 }
 
-void WorkQueue::scheduleWork(PassOwn<WorkItem> item)
+void WorkQueue::scheduleWork(PassOwnPtr<WorkItem> item)
 {
     MutexLocker locker(m_workItemQueueLock);
     m_workItemQueue.append(item.leakPtr());
