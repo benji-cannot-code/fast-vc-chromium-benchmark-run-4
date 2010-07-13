e6d910f57268d162bcc8adb7b6ed2556467a6e69
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-13 10:05:42 +0000

    2010-07-13  Philippe Normand  <pnormand@igalia.com>
            Rubber-stamped by Xan Lopez.
    
            Updated the copyright headers of the files modified in r62476.
    
            * WebView/WebView.mm:
            * WebView/WebViewData.h:
            * WebView/WebViewInternal.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
  * Copyright (C) 2006 David Smith (catfish.man@gmail.com)
+ * Copyright (C) 2010 Igalia S.L
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
