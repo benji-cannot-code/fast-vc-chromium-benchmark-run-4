6547e7ec8a1e826de05d56fa58eeaee308ea84f4
zmo@google.com <zmo@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-13 22:53:42 +0000

    2010-07-04  Zhenyao Mo  <zmo@google.com>
            Reviewed by Darin Fisher.
    
            Need to track texture completeness
            https://bugs.webkit.org/show_bug.cgi?id=41381
    
            Test: fast/canvas/webgl/texture-complete.html
    
            * html/canvas/WebGLRenderingContext.cpp:
            (WebCore::WebGLRenderingContext::WebGLRenderingContext): Init max texture level.
            (WebCore::WebGLRenderingContext::bindTexture): Pass max texture level to setTarget().
            (WebCore::WebGLRenderingContext::copyTexImage2D): Cache full texture info rather than partial.
            (WebCore::WebGLRenderingContext::generateMipmap): Ditto.
            (WebCore::WebGLRenderingContext::texImage2DBase): Ditto.
            (WebCore::WebGLRenderingContext::validateTexFuncParameters): Also validate level.
            * html/canvas/WebGLRenderingContext.h: Add max texture level.
            * html/canvas/WebGLTexture.cpp: Update the class to fully cache texture information and track NPOT and COMPLETE states.
            (WebCore::WebGLTexture::WebGLTexture):
            (WebCore::WebGLTexture::setTarget): Check whether the texture is initialized or has been deleted.
            (WebCore::WebGLTexture::setParameteri): Ditto.
            (WebCore::WebGLTexture::setParameterf): Ditto.
            (WebCore::WebGLTexture::setLevelInfo): Set texture info.
            (WebCore::WebGLTexture::generateMipmapLevelInfo): Generate texture info for all levels after generateMipmaps() is called.
            (WebCore::WebGLTexture::getInternalFormat): Return internal format on texture face 0 level 0.
            (WebCore::WebGLTexture::isNPOT): Check whether the texture is initialized or has been deleted.
            (WebCore::WebGLTexture::needToUseBlackTexture): Ditto.
            (WebCore::WebGLTexture::_deleteObject):
            (WebCore::WebGLTexture::mapTargetToIndex): Map target to index.
            (WebCore::WebGLTexture::canGenerateMipmaps): Check whether Mipmaps can be generated.
            (WebCore::WebGLTexture::computeLevelCount): Compute texture level count from width/height.
            (WebCore::WebGLTexture::update): Update NPOT/COMPLETE states.
            * html/canvas/WebGLTexture.h: Ditto.
            (WebCore::WebGLTexture::LevelInfo::LevelInfo): Add data structure to fully cache texture info.
            (WebCore::WebGLTexture::LevelInfo::setInfo): Set information.
    2010-07-04  Zhenyao Mo  <zmo@google.com>
    
            Reviewed by Darin Fisher.
    
            Need to track texture completeness
            https://bugs.webkit.org/show_bug.cgi?id=41381
    
            * fast/canvas/webgl/texture-complete-expected.txt: Added.
            * fast/canvas/webgl/texture-complete.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63253 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/WebGLTexture.h
+++ third_party/WebKit/WebCore/html/canvas/WebGLTexture.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
+#include <wtf/Vector.h>
 
 namespace WebCore {
     
@@ -48,19 +49,26 @@ namespace WebCore {
             cubeMapRWrapModeInitialized = initialized;
         }
 
-        void setTarget(unsigned long);
+        void setTarget(unsigned long target, int maxLevel);
         void setParameteri(unsigned long pname, int param);
         void setParameterf(unsigned long pname, float param);
-        void setSize(unsigned long target, unsigned width, unsigned height);
 
-        void setInternalFormat(unsigned long internalformat) { m_internalFormat = internalformat; }
-        unsigned long getInternalFormat() const { return m_internalFormat; }
+        void setLevelInfo(unsigned long target, int level, unsigned long internalFormat, int width, int height, unsigned long type);
 
+        bool canGenerateMipmaps();
+        // Generate all level information.
+        void generateMipmapLevelInfo();
+
+        unsigned long getInternalFormat() const;
+
+        // Whether width/height is NotPowerOfTwo.
         static bool isNPOT(unsigned, unsigned);
 
-        bool isNPOT() const { return m_isNPOT; }
+        bool isNPOT() const;
         // Determine if texture sampling should always return [0, 0, 0, 1] (OpenGL ES 2.0 Sec 3.8.2).
-        bool needToUseBlackTexture() const { return m_needToUseBlackTexture; }
+        bool needToUseBlackTexture() const;
+
+        static int computeLevelCount(int width, int height);
 
     protected:
         WebGLTexture(WebGLRenderingContext*);
@@ -70,7 +78,9 @@ namespace WebCore {
     private:
         virtual bool isTexture() const { return true; }
 
-        void updateNPOTStates();
+        void update();
+
+        int mapTargetToIndex(unsigned long);
 
         bool cubeMapRWrapModeInitialized;
 
@@ -81,12 +91,37 @@ namespace WebCore {
         int m_wrapS;
         int m_wrapT;
 
-        unsigned long m_internalFormat;
+        class LevelInfo {
+        public:
+            LevelInfo()
+                : valid(false)
+                , internalFormat(0)
+                , width(0)
+                , height(0)
+                , type(0)
+            {
+            }
 
-        unsigned m_width[6];
-        unsigned m_height[6];
+            void setInfo(unsigned long internalFmt, int w, int h, unsigned long tp)
+            {
+                valid = true;
+                internalFormat = internalFmt;
+                width = w;
+                height = h;
+                type = tp;
+            }
+
+            bool valid;
+            unsigned long internalFormat;
+            int width;
+            int height;
+            unsigned long type;
+        };
+
+        Vector<Vector<LevelInfo> > m_info;
 
         bool m_isNPOT;
+        bool m_isComplete;
         bool m_needToUseBlackTexture;
     };
     
