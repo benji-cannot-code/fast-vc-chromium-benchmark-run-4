63af0239a6d127c2b828ea0646f7abfb61942770
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-14 15:04:09 +0000

    2010-07-14  Eric Carlson  <eric.carlson@apple.com>
            Unreviewed, build fix for r63319.
    
            * html/TimeRanges.cpp:
            (TimeRanges::nearest): Include math.h and use narrowPrecisionToFloat.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/TimeRanges.cpp
+++ third_party/WebKit/WebCore/html/TimeRanges.cpp
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "TimeRanges.h"
 
+#include "FloatConversion.h"
+#include <math.h>
+
 using namespace WebCore;
 
 TimeRanges::TimeRanges(float start, float end)
@@ -128,9 +131,9 @@ float TimeRanges::nearest(float time) const
         if (time >= startTime && time <= endTime)
             return time;
         if (fabs(startTime - time) < closest)
-            closest = fabs(startTime - time);
+            closest = narrowPrecisionToFloat(fabs(startTime - time));
         else if (fabs(endTime - time) < closest)
-            closest = fabs(endTime - time);
+            closest = narrowPrecisionToFloat(fabs(endTime - time));
     }
     return closest;
 }
