b4db15389ed8509346c6a664783a92293f5531ff
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-14 20:48:30 +0000

    GLES2DecoderImpl takes ownership of GLContext passed to initialize.
    This is to simplify the management of the GLContext lifetime (one per decoder) when we support shared contexts.
    
    TEST=try, WebGL demos
    BUG=none
    
    Review URL: http://codereview.chromium.org/2998001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52390 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gpu_processor_mac.cc
+++ gpu/command_buffer/service/gpu_processor_mac.cc
@@ -14,10 +14,6 @@ bool GPUProcessor::Initialize(gfx::PluginWindowHandle window,
                               const gfx::Size& size,
                               GPUProcessor* parent,
                               uint32 parent_texture_id) {
-  // Cannot reinitialize.
-  if (context_.get())
-    return false;
-
   // Get the parent decoder and the GLContext to share IDs with, if any.
   gles2::GLES2Decoder* parent_decoder = NULL;
   gfx::GLContext* parent_context = NULL;
@@ -29,8 +25,9 @@ bool GPUProcessor::Initialize(gfx::PluginWindowHandle window,
     DCHECK(parent_context);
   }
 
-  context_.reset(gfx::GLContext::CreateOffscreenGLContext(parent_context));
-  if (!context_.get())
+  scoped_ptr<gfx::GLContext> context(
+      gfx::GLContext::CreateOffscreenGLContext(parent_context));
+  if (!context.get())
     return false;
 
   // On Mac OS X since we can not render on-screen we don't even
@@ -40,14 +37,21 @@ bool GPUProcessor::Initialize(gfx::PluginWindowHandle window,
   // rendering results back to the browser.
   if (window) {
     surface_.reset(new AcceleratedSurface());
-    // TODO(apatrick): AcceleratedSurface will not work with an OSMesa context.
-    if (!surface_->Initialize(context_.get(), false)) {
+
+    // Note that although the GLContext is passed to Initialize and the
+    // GLContext will later be owned by the decoder, AcceleratedSurface does
+    // not hold on to the reference. It simply extracts the underlying GL
+    // context in order to share the namespace with another context.
+    if (!surface_->Initialize(context.get(), false)) {
       Destroy();
       return false;
     }
   }
 
-  return InitializeCommon(size, parent_decoder, parent_texture_id);
+  return InitializeCommon(context.release(),
+                          size,
+                          parent_decoder,
+                          parent_texture_id);
 
   return true;
 }
@@ -55,8 +59,9 @@ bool GPUProcessor::Initialize(gfx::PluginWindowHandle window,
 void GPUProcessor::Destroy() {
   if (surface_.get()) {
     surface_->Destroy();
+    surface_.reset();
   }
-  surface_.reset();
+
   DestroyCommon();
 }
 
@@ -80,7 +85,10 @@ void GPUProcessor::SetTransportDIBAllocAndFree(
 }
 
 void GPUProcessor::WillSwapBuffers() {
-  DCHECK(context_->IsCurrent());
+  DCHECK(decoder_.get());
+  DCHECK(decoder_->GetGLContext());
+  DCHECK(decoder_->GetGLContext()->IsCurrent());
+
   if (surface_.get()) {
     surface_->SwapBuffers();
   }
