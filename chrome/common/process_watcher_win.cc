1e4155f9f1e092ba3dbe9a07c67d83a5084eb8ae
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-14 18:45:31 +0000

    Windows build fix
    Review URL: http://codereview.chromium.org/2948012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52364 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/process_watcher_win.cc
+++ chrome/common/process_watcher_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/process_watcher.h"
 
+#include "base/scoped_ptr.h"
 #include "base/env_var.h"
 #include "base/message_loop.h"
 #include "base/object_watcher.h"
