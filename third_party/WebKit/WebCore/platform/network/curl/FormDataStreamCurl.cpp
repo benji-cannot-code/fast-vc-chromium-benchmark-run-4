f4fc3bd906b875a823413ea882ebfccb2fec66fc
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-14 23:23:20 +0000

    [CURL] Build fix after request -> firstRequest rename.
    git-svn-id: svn://svn.chromium.org/blink/trunk@63369 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/curl/FormDataStreamCurl.cpp
+++ third_party/WebKit/WebCore/platform/network/curl/FormDataStreamCurl.cpp
@@ -51,8 +51,8 @@ size_t FormDataStream::read(void* ptr, size_t blockSize, size_t numberOfBlocks)
         return 0;
 
     Vector<FormDataElement> elements;
-    if (m_resourceHandle->request().httpBody())
-        elements = m_resourceHandle->request().httpBody()->elements();
+    if (m_resourceHandle->firstRequest().httpBody())
+        elements = m_resourceHandle->firstRequest().httpBody()->elements();
 
     if (m_formDataElementIndex >= elements.size())
         return 0;
@@ -105,8 +105,8 @@ size_t FormDataStream::read(void* ptr, size_t blockSize, size_t numberOfBlocks)
 bool FormDataStream::hasMoreElements() const
 {
     Vector<FormDataElement> elements;
-    if (m_resourceHandle->request().httpBody())
-        elements = m_resourceHandle->request().httpBody()->elements();
+    if (m_resourceHandle->firstRequest().httpBody())
+        elements = m_resourceHandle->firstRequest().httpBody()->elements();
 
     return m_formDataElementIndex < elements.size();
 }
