fa63fed29edffe669078a42d88ddcd67bed22e0e
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-14 00:29:32 +0000

    2010-07-13  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Darin Adler.
    
            Avoid slow-path for put() in Array.splice()
            https://bugs.webkit.org/show_bug.cgi?id=41920
    
            Defer creation of the returned array until its final size is known
            to avoid growing it while adding elements.
    
            * runtime/JSArray.cpp:
            (JSC::JSArray::JSArray): Add two modes of creation, CreateInitialized (old)
            and CreateCompact (which should only be used when constructing arrays whose
            size and contents are known at the time of creation.)
            (JSC::JSArray::setLength): Skip first consistency check if in CreateCompact
            initialization mode. (Only applies to non-empty arrays.)
            (JSC::JSArray::checkConsistency): Build fix (JSValue::type() is gone)
            * runtime/JSArray.h:
            (JSC::JSArray::uncheckedSetIndex): Added for fast initialization of compact
            arrays. Does no bounds or other sanity checking.
            * runtime/ArrayPrototype.cpp:
            (JSC::arrayProtoFuncSplice): Optimized creation of the returned JSArray.
            * runtime/ArrayConstructor.cpp:
            (JSC::constructArrayWithSizeQuirk): Pass CreateInitialized to ctor.
            * runtime/JSGlobalObject.h:
            (JSC::constructEmptyArray): Pass CreateInitialized to ctor.
            * runtime/RegExpConstructor.cpp:
            (JSC::RegExpMatchesArray::RegExpMatchesArray): Pass CreateInitialized to ctor.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSArray.h
+++ third_party/WebKit/JavaScriptCore/runtime/JSArray.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "JSObject.h"
 
+#define CHECK_ARRAY_CONSISTENCY 0
+
 namespace JSC {
 
     typedef HashMap<unsigned, JSValue> SparseArrayValueMap;
@@ -34,8 +36,21 @@ namespace JSC {
         SparseArrayValueMap* m_sparseValueMap;
         void* subclassData; // A JSArray subclass can use this to fill the vector lazily.
         size_t reportedMapCapacity;
+#if CHECK_ARRAY_CONSISTENCY
+        bool m_inCompactInitialization;
+#endif
         JSValue m_vector[1];
     };
+
+    // The CreateCompact creation mode is used for fast construction of arrays
+    // whose size and contents are known at time of creation.
+    //
+    // There are two obligations when using this mode:
+    //
+    //   - uncheckedSetIndex() must be used when initializing the array.
+    //   - setLength() must be called after initialization.
+
+    enum ArrayCreationMode { CreateCompact, CreateInitialized };
 
     class JSArray : public JSObject {
         friend class JIT;
@@ -43,7 +58,7 @@ namespace JSC {
 
     public:
         explicit JSArray(NonNullPassRefPtr<Structure>);
-        JSArray(NonNullPassRefPtr<Structure>, unsigned initialLength);
+        JSArray(NonNullPassRefPtr<Structure>, unsigned initialLength, ArrayCreationMode);
         JSArray(NonNullPassRefPtr<Structure>, const ArgList& initialValues);
         virtual ~JSArray();
 
@@ -82,6 +97,15 @@ namespace JSC {
                     m_storage->m_length = i + 1;
             }
             x = v;
+        }
+
+        void uncheckedSetIndex(unsigned i, JSValue v)
+        {
+            ASSERT(canSetIndex(i));
+#if CHECK_ARRAY_CONSISTENCY
+            ASSERT(m_storage->m_inCompactInitialization);
+#endif
+            m_storage->m_vector[i] = v;
         }
 
         void fillArgList(ExecState*, MarkedArgumentBuffer&);
