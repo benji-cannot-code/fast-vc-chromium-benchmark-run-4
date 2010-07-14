82bc33e492fa51336ddf11b60697263782b4ba7c
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-14 08:21:25 +0000

    Prevent non-visible views from getting focused.
    BUG=48719
    TEST=FocusManagerTest.TraversalWithNonVisibleViews
    
    Review URL: http://codereview.chromium.org/2935005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52293 0039d316-1c4b-4281-b951-d872f2087c98

--- views/view.cc
+++ views/view.cc
@@ -265,7 +265,7 @@ void View::SetEnabled(bool state) {
 }
 
 bool View::IsFocusable() const {
-  return focusable_ && IsEnabled() && IsVisible();
+  return focusable_ && IsEnabled() && IsVisibleInRootView();
 }
 
 void View::SetFocusable(bool focusable) {
@@ -273,7 +273,8 @@ void View::SetFocusable(bool focusable) {
 }
 
 bool View::IsAccessibilityFocusable() const {
-  return (focusable_ || accessibility_focusable_) && IsEnabled() && IsVisible();
+  return (focusable_ || accessibility_focusable_) && IsEnabled() &&
+      IsVisibleInRootView();
 }
 
 FocusManager* View::GetFocusManager() {
