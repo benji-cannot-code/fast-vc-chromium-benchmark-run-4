f6a057b6e5ba53b13762a928a3610be30683df00
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-14 15:34:12 +0000

    2010-07-14  Eric Carlson  <eric.carlson@apple.com>
            One more unreviewed build fix for r63319.
    
            * html/TimeRanges.cpp:
            (TimeRanges::nearest):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63323 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/TimeRanges.cpp
+++ third_party/WebKit/WebCore/html/TimeRanges.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007, 2009 Apple Inc.  All rights reserved.
+ * Copyright (C) 2007, 2009, 2010 Apple Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "TimeRanges.h"
 
-#include "FloatConversion.h"
 #include <math.h>
 
 using namespace WebCore;
@@ -131,9 +130,9 @@ float TimeRanges::nearest(float time) const
         if (time >= startTime && time <= endTime)
             return time;
         if (fabs(startTime - time) < closest)
-            closest = narrowPrecisionToFloat(fabs(startTime - time));
+            closest = fabsf(startTime - time);
         else if (fabs(endTime - time) < closest)
-            closest = narrowPrecisionToFloat(fabs(endTime - time));
+            closest = fabsf(endTime - time);
     }
     return closest;
 }
