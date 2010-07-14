e532dfc485642667933bbfd498564931cb9bf1af
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-14 14:22:49 +0000

    2010-07-14  Hans Wennborg  <hans@chromium.org>
            Reviewed by Steve Block.
    
            Rename DeviceOrientationController::onOrientationChange to didChangeDeviceOrientation
            https://bugs.webkit.org/show_bug.cgi?id=42257
    
            According to convention, we should avoid "on" in favor of "did" in function names.
    
            * dom/DeviceOrientationController.cpp:
            (WebCore::DeviceOrientationController::didChangeDeviceOrientation):
            * dom/DeviceOrientationController.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/DeviceOrientationController.h
+++ third_party/WebKit/WebCore/dom/DeviceOrientationController.h
@@ -44,7 +44,7 @@ public:
     void removeListener(DOMWindow*);
     void removeAllListeners(DOMWindow*);
 
-    void onDeviceOrientationChange(DeviceOrientation*);
+    void didChangeDeviceOrientation(DeviceOrientation*);
 
 private:
     Page* m_page;
