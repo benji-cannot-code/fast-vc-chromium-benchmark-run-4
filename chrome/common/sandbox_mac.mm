f93d501c6fc545eedd6e8d92036a0f9c9bf2b5ac
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-14 16:48:05 +0000

    Revert r52326 since it's causing unit tests to fail.
    Review URL: http://codereview.chromium.org/2958011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52330 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/sandbox_mac.mm
+++ chrome/common/sandbox_mac.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 extern "C" {
 #include <sandbox.h>
 }
-#include <sys/param.h>
 
 #include "base/basictypes.h"
 #include "base/command_line.h"
@@ -120,26 +119,27 @@ bool QuotePlainString(const std::string& str_utf8, std::string* dst) {
 //
 // Returns: true on success, false otherwise.
 bool QuoteStringForRegex(const std::string& str_utf8, std::string* dst) {
-  // Characters with special meanings in sandbox profile syntax.
-  // Note: ]} are notably absent from this list although in practice escaping
-  // them has no ill effect.
+  // List of chars with special meaning to regex.
+  // This list is derived from http://perldoc.perl.org/perlre.html .
   const char regex_special_chars[] = {
     '\\',
 
     // Metacharacters
     '^',
     '.',
-    '[',
     '$',
+    '|',
     '(',
     ')',
-    '|',
+    '[',
+    ']',
 
     // Quantifiers
     '*',
     '+',
     '?',
     '{',
+    '}',
   };
 
   // Anchor regex at start of path.
@@ -346,11 +346,14 @@ bool EnableSandbox(SandboxProcessType sandbox_type,
     // needed so the caller doesn't need to worry about things like /var
     // being a link to /private/var (like in the paths CreateNewTempDirectory()
     // returns).
-    FilePath allowed_dir_canonical(allowed_dir);
-    GetCanonicalSandboxPath(&allowed_dir_canonical);
+    FilePath allowed_dir_absolute(allowed_dir);
+    if (!file_util::AbsolutePath(&allowed_dir_absolute)) {
+      PLOG(FATAL) << "Failed to resolve absolute path";
+      return false;
+    }
 
     std::string allowed_dir_escaped;
-    if (!QuoteStringForRegex(allowed_dir_canonical.value(),
+    if (!QuoteStringForRegex(allowed_dir_absolute.value(),
                              &allowed_dir_escaped)) {
       LOG(FATAL) << "Regex string quoting failed " << allowed_dir.value();
       return false;
@@ -382,12 +385,8 @@ bool EnableSandbox(SandboxProcessType sandbox_type,
     // If we ever need this on pre-10.6 OSs then we'll have to rethink the
     // surrounding sandbox syntax.
     std::string home_dir = base::SysNSStringToUTF8(NSHomeDirectory());
-
-    FilePath home_dir_canonical(home_dir);
-    GetCanonicalSandboxPath(&home_dir_canonical);
-
     std::string home_dir_escaped;
-    if (!QuotePlainString(home_dir_canonical.value(), &home_dir_escaped)) {
+    if (!QuotePlainString(home_dir, &home_dir_escaped)) {
       LOG(FATAL) << "Sandbox string quoting failed";
       return false;
     }
@@ -411,25 +410,6 @@ bool EnableSandbox(SandboxProcessType sandbox_type,
                           << error_buff;
   sandbox_free_error(error_buff);
   return success;
-}
-
-void GetCanonicalSandboxPath(FilePath* path) {
-  int fd = HANDLE_EINTR(open(path->value().c_str(), O_RDONLY));
-  if (fd < 0) {
-    PLOG(FATAL) << "GetCanonicalSandboxPath() failed for: "
-                << path->value();
-    return;
-  }
-  file_util::ScopedFD file_closer(&fd);
-
-  FilePath::CharType canonical_path[MAXPATHLEN];
-  if (HANDLE_EINTR(fcntl(fd, F_GETPATH, canonical_path)) != 0) {
-    PLOG(FATAL) << "GetCanonicalSandboxPath() failed for: "
-                << path->value();
-    return;
-  }
-
-  *path = FilePath(canonical_path);
 }
 
 }  // namespace sandbox
