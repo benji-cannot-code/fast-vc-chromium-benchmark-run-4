432e6ed065ce838d9e1236242e1a61de17353a0f
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-14 01:31:08 +0000

    Privacy dashboard link for Mac.  Only shown when --show-privacy-dashboard-link switch present.
    XIB changes: added a new link to the sync group of the preferences dialog (required adding a continuation array).  Slightly altered the spacing to fit.
    
    BUG=48844
    TEST=mac preferences pane with the above switch
    
    Review URL: http://codereview.chromium.org/2891021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52258 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/preferences_window_controller.mm
+++ chrome/browser/cocoa/preferences_window_controller.mm
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/sync_ui_util.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/notification_details.h"
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_type.h"
@@ -1275,6 +1276,15 @@ const int kDisabledIndex = 1;
   }
 }
 
+// Called when the user clicks on the link to the privacy dashboard.
+- (IBAction)showPrivacyDashboard:(id)sender {
+  Browser* browser = BrowserList::GetLastActive();
+
+  if (!browser || !browser->GetSelectedTabContents())
+    browser = Browser::Create(profile_);
+  browser->OpenPrivacyDashboardTabAndActivate();
+}
+
 // Called when the user clicks the "Customize Sync" button in the
 // "Personal Stuff" pane.  Spawns a dialog-modal sheet that cleans
 // itself up on close.
@@ -1751,6 +1761,10 @@ const int kDisabledIndex = 1;
     [syncStatus_ setBackgroundColor:syncStatusNoErrorBackgroundColor_];
     [syncLinkCell setBackgroundColor:syncLinkNoErrorBackgroundColor_];
   }
+
+  if (!CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kShowPrivacyDashboardLink))
+    [privacyDashboardLink_ setHidden:YES];
 }
 
 // Show the preferences window.
