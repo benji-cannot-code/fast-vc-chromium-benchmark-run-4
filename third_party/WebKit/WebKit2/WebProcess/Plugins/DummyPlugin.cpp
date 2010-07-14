3afb049c9fc401674ae1efb94be88450cf240260
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-14 19:38:16 +0000

    Send JavaScript url request results back to the plug-in https://bugs.webkit.org/show_bug.cgi?id=42277
    Reviewed by Sam Weinig.
    
    * WebProcess/Plugins/DummyPlugin.cpp:
    (WebKit::DummyPlugin::didEvaluateJavaScript):
    * WebProcess/Plugins/DummyPlugin.h:
    Add empty stub.
    
    * WebProcess/Plugins/Netscape/NetscapePlugin.cpp:
    (WebKit::NetscapePlugin::didEvaluateJavaScript):
    * WebProcess/Plugins/Netscape/NetscapePlugin.h:
    Add Empty stub.
    
    * WebProcess/Plugins/Plugin.h:
    Add didEvaluateJavaScript pure virtual member function.
    
    * WebProcess/Plugins/PluginView.cpp:
    (WebKit::PluginView::performJavaScriptURLRequest):
    If target is not null, Call Plugin::frameDidFail or Plugin::frameDidFinishLoading.
    If target is null, call didEvaluateJavaScript with the result string.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/Plugins/DummyPlugin.cpp
+++ third_party/WebKit/WebKit2/WebProcess/Plugins/DummyPlugin.cpp
@@ -75,4 +75,8 @@ PluginController* DummyPlugin::controller()
     return 0;
 }
 
+void DummyPlugin::didEvaluateJavaScript(uint64_t requestID, const WebCore::String& requestURLString, const WebCore::String& result)
+{
+}
+
 } // namespace WebKit
