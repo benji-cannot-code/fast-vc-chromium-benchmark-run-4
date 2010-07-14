d8d6310af3e10457a4d35f8e4db00fd7482d6e1a
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-14 21:03:03 +0000

    2010-07-14  Johnny Ding  <jnd@chromium.org>
            Reviewed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=41292
            Set right UserGestureIndicator to indicate whether the NPN_Evaluate allows popup window or not.
    
            Test: plugins/plugin-initiate-popup-window.html
    
            * bindings/v8/NPV8Object.cpp:
            (_NPN_EvaluateHelper):
    2010-07-14  Johnny Ding  <jnd@chromium.org>
    
            Reviewed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=41292
            Add a test to initiate popup windows from plugin during plugin is handling mouse/keyboard event.
            Disable this test on gtk/qt/win since the test plugin in those platforms didn't implement NPP_HandleEvent.
    
            * platform/chromium/test_expectations.txt:
            * platform/gtk/Skipped:
            * platform/qt/Skipped:
            * platform/win/Skipped:
            * plugins/plugin-initiate-popup-window-expected.txt: Added.
            * plugins/plugin-initiate-popup-window.html: Added.
    2010-07-14  Johnny Ding  <jnd@chromium.org>
    
            Reviewed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=41292
            Add a new parameter to the test plugin to allow to specify a script and a
            mouse/keyboard event. The specified script will be evaluated in the browser
            when the specified event is received by the plugin.
    
            * DumpRenderTree/TestNetscapePlugIn/PluginObject.cpp:
            (pluginAllocate):
            * DumpRenderTree/TestNetscapePlugIn/PluginObject.h:
            * DumpRenderTree/TestNetscapePlugIn/main.cpp:
            (NPP_New):
            (handleEventCarbon):
            (handleEventCocoa):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63352 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/TestNetscapePlugIn/PluginObject.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/TestNetscapePlugIn/PluginObject.h
@@ -55,6 +55,7 @@ typedef struct {
     NPBool testGetURLOnDestroy;
     NPBool testWindowOpen;
     NPBool testKeyboardFocusForPlugins;
+    NPBool mouseDownForEvaluateScript;
     char* onStreamLoad;
     char* onStreamDestroy;
     char* onDestroy;
@@ -64,6 +65,7 @@ typedef struct {
     char* firstHeaders;
     char* lastUrl;
     char* lastHeaders;
+    char* evaluateScriptOnMouseDownOrKeyDown;
 #ifdef XP_MACOSX
     NPEventModel eventModel;
 #endif
