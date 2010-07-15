47718e8e7df3219e1d27315602bbacea0179d16f
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 06:53:07 +0000

    Add command line switch to guard the search provider api additions in progress.
    BUG=38475
    TEST=None (Nothing to test yet.)
    
    Review URL: http://codereview.chromium.org/2873044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52455 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -533,6 +533,7 @@ void BrowserRenderProcessHost::PropagateBrowserCommandLineToRenderer(
     switches::kEnableDCHECK,
     switches::kSilentDumpOnDCHECK,
     switches::kUseLowFragHeapCrt,
+    switches::kEnableSearchProviderApiV2,
     switches::kEnableStatsTable,
     switches::kExperimentalSpellcheckerFeatures,
     switches::kDisableAudio,
