4a5ff11bc7cdaf13768d1f9421f93b0792d45145
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 21:03:43 +0000

    Disk cache: Code cleanup after the last cl.
    BUG=26730
    TEST=none
    
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52527 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/disk_cache_test_util.cc
+++ net/disk_cache/disk_cache_test_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/message_loop_proxy.h"
 #include "base/path_service.h"
+#include "net/base/net_errors.h"
 #include "net/disk_cache/backend_impl.h"
 #include "net/disk_cache/cache_util.h"
 #include "net/disk_cache/file.h"
@@ -100,7 +101,7 @@ bool CheckCacheIntegrity(const FilePath& path, bool new_eviction) {
   if (new_eviction)
     cache->SetNewEviction();
   cache->SetFlags(disk_cache::kNoRandom);
-  if (!cache->Init())
+  if (cache->SyncInit() != net::OK)
     return false;
   return cache->SelfCheck() >= 0;
 }
