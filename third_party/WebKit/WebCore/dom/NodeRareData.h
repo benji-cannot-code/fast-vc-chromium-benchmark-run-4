1e41da1463ef8e12319348a8553cf4331134a744
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-15 21:49:39 +0000

    Patch for https://bugs.webkit.org/show_bug.cgi?id=42410 Many leaking DatasetDOMStringMaps seen on buildbot
    Reviewed by Oliver Hunt.
    
    * dom/NodeRareData.h:
    (WebCore::NodeRareData::~NodeRareData): Add a virtual destructor so
    that the ElementRareData's destructor will be called when this is deleted
    from the Node's destructor.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63467 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/NodeRareData.h
+++ third_party/WebKit/WebCore/dom/NodeRareData.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2008, 2010 Apple Inc. All rights reserved.
  * Copyright (C) 2008 David Smith <catfish.man@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "StringHash.h"
 #include "TagNodeList.h"
 #include <wtf/HashSet.h>
-#include <wtf/PassOwnPtr.h>
 #include <wtf/OwnPtr.h>
+#include <wtf/PassOwnPtr.h>
 
 namespace WebCore {
 
@@ -75,6 +75,10 @@ public:
         , m_tabIndexWasSetExplicitly(false)
         , m_isFocused(false)
         , m_needsFocusAppearanceUpdateSoonAfterAttach(false)
+    {
+    }
+
+    virtual ~NodeRareData()
     {
     }
 
@@ -124,6 +128,6 @@ private:
     bool m_needsFocusAppearanceUpdateSoonAfterAttach : 1;
 };
 
-} //namespace
+} // namespace WebCore
 
-#endif
+#endif // NodeRareData_h
