f8ec5820a0d786a5eb164c20851511988401d7d2
vrk@google.com <vrk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 23:07:36 +0000

    Video Buffering: Caches data to disk when paused (resubmit)
    Resubmitting this buffering patch because original patch was reverted.
    This patch clears up the memory leak issue in the unit tests and adds
    valgrind suppressions to silence the uninitialized variable false positives.
    
    BUG=42285
    TEST=test_shell_tests
    
    Review URL: http://codereview.chromium.org/2908003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52569 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/media/buffered_data_source.h
+++ webkit/glue/media/buffered_data_source.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/media/media_resource_loader_bridge_factory.h"
 
 namespace webkit_glue {
-
 /////////////////////////////////////////////////////////////////////////////
 // BufferedResourceLoader
 // This class works inside demuxer thread and render thread. It contains a
@@ -91,6 +90,9 @@ class BufferedResourceLoader :
   // is not available.
   virtual int64 GetBufferedLastBytePosition();
 
+  // Sets whether deferring data is allowed or disallowed.
+  virtual void SetAllowDefer(bool is_allowed);
+
   // Gets the content length in bytes of the instance after this loader has been
   // started. If this value is -1, then content length is unknown.
   virtual int64 content_length() { return content_length_; }
@@ -132,6 +134,7 @@ class BufferedResourceLoader :
   virtual ~BufferedResourceLoader();
 
  private:
+  friend class BufferedResourceLoaderTest;
 
   // Defer the resource loading if the buffer is full.
   void EnableDeferIfNeeded();
@@ -170,6 +173,9 @@ class BufferedResourceLoader :
 
   // True if resource loading was deferred.
   bool deferred_;
+
+  // True if resource loader is allowed to defer, false otherwise.
+  bool defer_allowed_;
 
   // True if resource loading has completed.
   bool completed_;
@@ -232,6 +238,7 @@ class BufferedDataSource : public media::DataSource {
   virtual void Initialize(const std::string& url,
                           media::FilterCallback* callback);
   virtual void Stop(media::FilterCallback* callback);
+  virtual void SetPlaybackRate(float playback_rate);
 
   // media::DataSource implementation.
   // Called from demuxer thread.
@@ -288,6 +295,11 @@ class BufferedDataSource : public media::DataSource {
   // request has timed out, this task will destroy the current loader and
   // creates a new one to accommodate the read request.
   void WatchDogTask();
+
+  // This task uses the current playback rate with the previous playback rate
+  // to determine whether we are going from pause to play and play to pause,
+  // and signals the buffered resource loader accordingly.
+  void SetPlaybackRateTask(float playback_rate);
 
   // The method that performs actual read. This method can only be executed on
   // the render thread.
@@ -387,6 +399,10 @@ class BufferedDataSource : public media::DataSource {
   // This variable is set by CleanupTask() that indicates this object is stopped
   // on the render thread.
   bool stopped_on_render_loop_;
+
+  // This variable is true when we are in a paused state and false when we
+  // are in a playing state.
+  bool media_is_paused_;
 
   // This timer is to run the WatchDogTask repeatedly. We use a timer instead
   // of doing PostDelayedTask() reduce the extra reference held by the message
