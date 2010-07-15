b860c4049df774cae6ff4dcb2bf9cec9eed8c18b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-15 03:00:45 +0000

    2010-07-14  Scott Violet  <sky@chromium.org>
            Reviewed by David Levin.
    
            [chromium] Autofill menu shows seperator at the wrong place when an entry is deleted
            https://bugs.webkit.org/show_bug.cgi?id=42210
    
            * src/AutoFillPopupMenuClient.cpp:
            (WebKit::AutoFillPopupMenuClient::removeSuggestionAtIndex): Updates internal data appropriately.
            (WebKit::AutoFillPopupMenuClient::canRemoveSuggestionAtIndex): Now called before removal to see if can remove.
            * src/AutoFillPopupMenuClient.h: Fixed whitespace and added method.
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::autocompleteHandleKeyEvent): Added call to canRemoveSuggestionAtIndex.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63395 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
@@ -593,6 +593,10 @@ bool WebViewImpl::autocompleteHandleKeyEvent(const WebKeyboardEvent& event)
         }
 
         int selectedIndex = m_autoFillPopup->selectedIndex();
+
+        if (!m_autoFillPopupClient->canRemoveSuggestionAtIndex(selectedIndex))
+            return false;
+
         HTMLInputElement* inputElement = static_cast<HTMLInputElement*>(element);
         WebString name = inputElement->name();
         WebString value = m_autoFillPopupClient->itemText(selectedIndex);
