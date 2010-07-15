6fb5247e48ef7edc90f2cc5befb2875cd83bc2c1
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 05:03:59 +0000

    Fix ChromeOS build by putting back define from previous patch (tried incorrectly to clean this up).
    TEST=none
    BUG=none
    Review URL: http://codereview.chromium.org/2858054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52445 0039d316-1c4b-4281-b951-d872f2087c98

--- gfx/blit.cc
+++ gfx/blit.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/platform_canvas.h"
 #include "skia/ext/platform_device.h"
 
-#if defined(TOOLKIT_GTK)
+#if defined(OS_POSIX) && !defined(OS_MACOSX)
 #include <cairo/cairo.h>
 #endif
 
