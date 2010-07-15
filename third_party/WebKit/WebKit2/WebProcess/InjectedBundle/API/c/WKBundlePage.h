676172d1b6920fae9774af624787bf2aea170679
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-15 03:35:00 +0000

    - Fix WebKitTestRunner build
    Reviewed by Mark Rowe.
    
    * WebProcess/InjectedBundle/API/c/WKBundlePage.h: Add stdint.h include.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63398 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundlePage.h
+++ third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundlePage.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WKBundlePage_h
 
 #include <JavaScriptCore/JavaScript.h>
+#include <stdint.h>
 #include <WebKit2/WKBase.h>
 #include <WebKit2/WKBundleBase.h>
 
