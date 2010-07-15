e573c54955a1f53ab34f997bec70695e7ae0ffff
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-15 02:56:01 +0000

    2010-07-13  Mirko Damiani  <mirko@develer.com>
            Reviewed by Simon Hausmann.
    
            [Qt] CSS border style not cleared for SVG object
            https://bugs.webkit.org/show_bug.cgi?id=42150
    
            For tests and description see:
            https://bugs.webkit.org/show_bug.cgi?id=25738
    
            * platform/graphics/qt/GraphicsContextQt.cpp:
            (WebCore::GraphicsContext::setLineDash):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63394 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
@@ -1016,7 +1016,8 @@ void GraphicsContext::setLineDash(const DashArray& dashes, float dashOffset)
 
         pen.setDashPattern(pattern);
         pen.setDashOffset(dashOffset);
-    }
+    } else
+        pen.setStyle(Qt::SolidLine);
     p->setPen(pen);
 }
 
