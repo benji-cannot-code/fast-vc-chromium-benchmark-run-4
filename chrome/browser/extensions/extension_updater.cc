86d5807002d9033b9994136f14e9ff9844d7ba82
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 07:59:56 +0000

    Fixed invalid read in ExtensionsServiceTest.ReloadExtensions.
    BUG=47843,47863
    TEST=unittests, valgrind
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=51215
    
    Review URL: http://codereview.chromium.org/2872024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52461 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_updater.cc
+++ chrome/browser/extensions/extension_updater.cc
@@ -162,7 +162,6 @@ void ManifestFetchesBuilder::AddExtension(const Extension& extension) {
   AddExtensionData(extension.location(),
                    extension.id(),
                    *extension.version(),
-                   extension.converted_from_user_script(),
                    extension.is_theme(),
                    extension.update_url());
 }
@@ -176,7 +175,7 @@ void ManifestFetchesBuilder::AddPendingExtension(
   scoped_ptr<Version> version(
       Version::GetVersionFromString("0.0.0.0"));
   AddExtensionData(Extension::INTERNAL, id, *version,
-                   false, info.is_theme, info.update_url);
+                   info.is_theme, info.update_url);
 }
 
 void ManifestFetchesBuilder::ReportStats() const {
@@ -210,7 +209,6 @@ void ManifestFetchesBuilder::AddExtensionData(
     Extension::Location location,
     const std::string& id,
     const Version& version,
-    bool converted_from_user_script,
     bool is_theme,
     GURL update_url) {
   // Only internal and external extensions can be autoupdated.
@@ -229,12 +227,6 @@ void ManifestFetchesBuilder::AddExtensionData(
   // Skip extensions with empty IDs.
   if (id.empty()) {
     LOG(WARNING) << "Found extension with empty ID";
-    return;
-  }
-
-  // Skip extensions with empty update URLs converted from user
-  // scripts.
-  if (converted_from_user_script && update_url.is_empty()) {
     return;
   }
 
