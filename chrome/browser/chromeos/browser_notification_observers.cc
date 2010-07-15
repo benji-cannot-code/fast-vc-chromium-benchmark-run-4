2bc807863b40c435c87462ea130520080c2bf61d
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 18:01:13 +0000

    Chrome ARM build fix.
    Review URL: http://codereview.chromium.org/2870052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52503 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/browser_notification_observers.cc
+++ chrome/browser/chromeos/browser_notification_observers.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/file_util.h"
+#include "base/histogram.h"
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/chromeos/boot_times_loader.h"
 #include "chrome/browser/chromeos/login/authentication_notification_details.h"
