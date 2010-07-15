7acbe7f21e2d9324846392b7f6347ab541798693
jiesun@google.com <jiesun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 23:02:13 +0000

    This callback is created by void PipelineImpl::FilterStateTransitionTask() and the ownership is transferred to Filter, filter should delete this callback.
    Review URL: http://codereview.chromium.org/2845044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52565 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/decoder_base.h
+++ media/filters/decoder_base.h
@@ -28,7 +28,6 @@ namespace media {
 template <class Decoder, class Output>
 class DecoderBase : public Decoder {
  public:
-  typedef CallbackRunner< Tuple1<Output*> > ReadCallback;
 
   // MediaFilter implementation.
   virtual void Stop(FilterCallback* callback) {
@@ -189,7 +188,10 @@ class DecoderBase : public Decoder {
     expecting_discontinuous_ = true;
 
     // Signal that we're done seeking.
-    callback->Run();
+    if (callback) {
+      callback->Run();
+      delete callback;
+    }
   }
 
   void InitializeTask(DemuxerStream* demuxer_stream, FilterCallback* callback) {
@@ -302,9 +304,6 @@ class DecoderBase : public Decoder {
   // we need this extra queue.
   typedef std::deque<scoped_refptr<Output> > ResultQueue;
   ResultQueue result_queue_;
-
-  // Pause callback.
-  scoped_ptr<FilterCallback> pause_callback_;
 
   // Simple state tracking variable.
   enum State {
