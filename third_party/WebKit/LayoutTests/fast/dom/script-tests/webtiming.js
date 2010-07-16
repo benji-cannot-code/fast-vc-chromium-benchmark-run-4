ab30bff2a6136cb9292b2a7d28a0ee2f63c738f2
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-16 16:38:52 +0000

    2010-07-15  Tony Gentilcore  <tonyg@chromium.org>
            Reviewed by Darin Fisher.
    
            Update expectations for Web Timing enabled in chromium port.
            https://bugs.webkit.org/show_bug.cgi?id=42313
    
            Default expectations are to PASS (Web Timing enabled). Since the gtk, qt, win, and mac platforms do not have WEB_TIMING enabled, they must skip these tests.
    
            * fast/dom/Window/window-properties-performance-expected.txt: Added.
            * fast/dom/Window/window-properties-performance.html: Added.
            * fast/dom/Window/window-properties.html: Skip webkitPerformance so that its platform-specific expectations can be managed in window-properties-performance.html.
            * fast/dom/navigation-type-back-forward-expected.txt: Update to PASS
            * fast/dom/navigation-type-navigate-expected.txt: Update to PASS
            * fast/dom/navigation-type-reload-expected.txt: Update to PASS
            * fast/dom/webtiming-expected.txt: Updated to mostly PASS. Currently has failures because chromium is not populating the ResourceLoadTiming API. This will be resolved when http://codereview.chromium.org/2909016 lands.
            * fast/dom/webtiming.html: Update test based on new backfill functionality from https://bugs.webkit.org/show_bug.cgi?id=42303.
            * http/tests/misc/webtiming-one-redirect-expected.txt: Update to PASS
            * http/tests/misc/webtiming-two-redirects-expected.txt: Update to PASS
            * platform/gtk/Skipped:
            * platform/mac/Skipped:
            * platform/qt/Skipped:
            * platform/win/Skipped:
    2010-07-15  Tony Gentilcore  <tonyg@chromium.org>
    
            Reviewed by Darin Fisher.
    
            Enable window.webkitPerformance (Web Timing) for chromium
            https://bugs.webkit.org/show_bug.cgi?id=42313
    
            * features.gypi:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63559 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/script-tests/webtiming.js
+++ third_party/WebKit/LayoutTests/fast/dom/script-tests/webtiming.js
@@ -28,13 +28,13 @@ function checkTimingBeforeLoad()
 
     shouldBeGreaterThanOrEqual("timing.fetchStart", "timing.navigationStart");
 
-    shouldBe("timing.domainLookupStart", "0");
-    shouldBe("timing.domainLookupEnd", "0");
+    shouldBeGreaterThanOrEqual("timing.domainLookupStart", "timing.fetchStart");
+    shouldBeGreaterThanOrEqual("timing.domainLookupEnd", "timing.domainLookupStart");
 
-    shouldBe("timing.connectStart", "0");
-    shouldBe("timing.connectEnd", "0");
+    shouldBeGreaterThanOrEqual("timing.connectStart", "timing.domainLookupEnd");
+    shouldBeGreaterThanOrEqual("timing.connectEnd", "timing.connectStart");
 
-    shouldBeGreaterThanOrEqual("timing.requestStart", "timing.navigationStart");
+    shouldBeGreaterThanOrEqual("timing.requestStart", "timing.connectEnd");
     shouldBeGreaterThanOrEqual("timing.requestEnd", "timing.requestStart");
 
     shouldBeGreaterThanOrEqual("timing.responseStart", "timing.requestEnd");
@@ -57,13 +57,13 @@ function checkWebTimingOnLoad()
 
     shouldBeGreaterThanOrEqual("timing.fetchStart", "timing.navigationStart");
 
-    shouldBe("timing.domainLookupStart", "0");
-    shouldBe("timing.domainLookupEnd", "0");
+    shouldBeGreaterThanOrEqual("timing.domainLookupStart", "timing.fetchStart");
+    shouldBeGreaterThanOrEqual("timing.domainLookupEnd", "timing.domainLookupStart");
 
-    shouldBe("timing.connectStart", "0");
-    shouldBe("timing.connectEnd", "0");
+    shouldBeGreaterThanOrEqual("timing.connectStart", "timing.domainLookupEnd");
+    shouldBeGreaterThanOrEqual("timing.connectEnd", "timing.connectStart");
 
-    shouldBeGreaterThanOrEqual("timing.requestStart", "timing.navigationStart");
+    shouldBeGreaterThanOrEqual("timing.requestStart", "timing.connectEnd");
     shouldBeGreaterThanOrEqual("timing.requestEnd", "timing.requestStart");
 
     shouldBeGreaterThanOrEqual("timing.responseStart", "timing.requestEnd");
@@ -87,13 +87,13 @@ function checkWebTimingAfterLoad()
 
     shouldBeGreaterThanOrEqual("timing.fetchStart", "timing.navigationStart");
 
-    shouldBe("timing.domainLookupStart", "0");
-    shouldBe("timing.domainLookupEnd", "0");
+    shouldBeGreaterThanOrEqual("timing.domainLookupStart", "timing.fetchStart");
+    shouldBeGreaterThanOrEqual("timing.domainLookupEnd", "timing.domainLookupStart");
 
-    shouldBe("timing.connectStart", "0");
-    shouldBe("timing.connectEnd", "0");
+    shouldBeGreaterThanOrEqual("timing.connectStart", "timing.domainLookupEnd");
+    shouldBeGreaterThanOrEqual("timing.connectEnd", "timing.connectStart");
 
-    shouldBeGreaterThanOrEqual("timing.requestStart", "timing.navigationStart");
+    shouldBeGreaterThanOrEqual("timing.requestStart", "timing.connectEnd");
     shouldBeGreaterThanOrEqual("timing.requestEnd", "timing.requestStart");
 
     shouldBeGreaterThanOrEqual("timing.responseStart", "timing.requestEnd");
