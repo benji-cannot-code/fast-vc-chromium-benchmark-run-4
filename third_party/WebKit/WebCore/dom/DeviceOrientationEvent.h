72375263f0b304855b254386864d7c5207fd0f4b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-16 13:50:33 +0000

    2010-07-16  Hans Wennborg  <hans@chromium.org>
            Reviewed by Steve Block.
    
            DeviceOrientationEvent.h should not forward-declare DeviceOrientation
            https://bugs.webkit.org/show_bug.cgi?id=42447
    
            When destructing m_orientation, DeviceOrientation cannot be an incomplete type.
    
            * dom/DeviceOrientationEvent.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63544 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/DeviceOrientationEvent.h
+++ third_party/WebKit/WebCore/dom/DeviceOrientationEvent.h
@@ -27,11 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DeviceOrientationEvent_h
 #define DeviceOrientationEvent_h
 
+#include "DeviceOrientation.h"
 #include "Event.h"
 
 namespace WebCore {
-
-class DeviceOrientation;
 
 class DeviceOrientationEvent : public Event {
 public:
