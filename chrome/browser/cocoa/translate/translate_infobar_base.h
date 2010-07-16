06dc6245c78ed1081cd987de876e421c6309bc55
feldstein@chromium.org <feldstein@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-16 20:33:53 +0000

    Add the always/never translate buttons
    Port over the windows work onto the mac to add the button when a user presses
    translate or cancel more than 3 times in a row.
    
    BUG=48136
    TEST=Cancel or accept translation more than 3 times in a row to see the button
    pop up and work.
    
    Review URL: http://codereview.chromium.org/2891015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52734 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/translate/translate_infobar_base.h
+++ chrome/browser/cocoa/translate/translate_infobar_base.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/translate/languages_menu_model.h"
 #include "chrome/browser/translate/options_menu_model.h"
 #include "chrome/browser/translate/translate_infobar_delegate.h"
-#include "chrome/common/notification_registrar.h"
 #include "chrome/common/translate_errors.h"
 
 class TranslateInfoBarMenuModel;
--- chrome/browser/cocoa/translate/translate_infobar_base.mm
+++ chrome/browser/cocoa/translate/translate_infobar_base.mm
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/cocoa/translate/translate_message_infobar_controller.h"
 #include "chrome/browser/translate/translate_infobar_delegate.h"
 #include "grit/generated_resources.h"
-#include "grit/locale_settings.h"
 #include "third_party/GTM/AppKit/GTMUILocalizerAndLayoutTweaker.h"
 
 using TranslateInfoBarUtilities::MoveControl;
