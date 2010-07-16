26480c4eef4cb4b4fadbbf29e3955ed7c774bc24
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-16 23:03:40 +0000

    Part of <rdar://problem/7233974> Deprecate +[WebView _setShouldUseFontSmoothing:] https://bugs.webkit.org/show_bug.cgi?id=29355
    Reviewed by Sam Weinig.
    
    WebCore: 
    
    * WebCore.exp.in: Updated.
    * platform/graphics/Font.cpp:
    (WebCore::Font::Font): Added a font smoothing mode parameter to the constructor.
    Set the font smoothing mode in the font description.
    * platform/graphics/Font.h:
    
    WebKit/mac: 
    
    * Misc/WebKitNSStringExtras.h:
    * Misc/WebKitNSStringExtras.mm:
    (-[NSString _web_drawAtPoint:font:textColor:]): Now calls through to
    -_web_drawAtPoint:font:textColor:allowingFontSmoothing: passing YES for the last
    parameter.
    (-[NSString _web_drawAtPoint:font:textColor:allowingFontSmoothing:]): Added
    allowingFontSmoothing:. If font smoothing is not allowed, sets the font smoothing
    mode to antialiased. Otherwise, sets it to auto.
    (-[NSString _web_drawDoubledAtPoint:withTopColor:bottomColor:font:]): Instead of
    changing the font smoothing setting in the graphics context, call
    -_web_drawAtPoint:font:textColor:allowingFontSmoothing: passing NO for the last
    parameter.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/Font.cpp
+++ third_party/WebKit/WebCore/platform/graphics/Font.cpp
@@ -73,7 +73,7 @@ Font::Font(const FontDescription& fd, short letterSpacing, short wordSpacing)
 {
 }
 
-Font::Font(const FontPlatformData& fontData, bool isPrinterFont)
+Font::Font(const FontPlatformData& fontData, bool isPrinterFont, FontSmoothingMode fontSmoothingMode)
     : m_fontList(FontFallbackList::create())
     , m_letterSpacing(0)
     , m_wordSpacing(0)
@@ -81,6 +81,7 @@ Font::Font(const FontPlatformData& fontData, bool isPrinterFont)
     , m_needsTranscoding(fontTranscoder().needsTranscoding(family().family().string()))
 {
     m_fontDescription.setUsePrinterFont(isPrinterFont);
+    m_fontDescription.setFontSmoothing(fontSmoothingMode);
     m_fontList->setPlatformFont(fontData);
 }
 
