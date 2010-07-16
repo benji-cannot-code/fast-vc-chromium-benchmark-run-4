b0013f5675707ab5cde59ec78512f5e356100b3b
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-16 23:43:22 +0000

    2010-07-16  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r63593.
            http://trac.webkit.org/changeset/63593
            https://bugs.webkit.org/show_bug.cgi?id=42487
    
            Broke a few chromium pixel tests (Requested by tony^work on
            #webkit).
    
            * platform/chromium-linux/svg/stroke/path-zero-strokewidth-test-expected.checksum: Removed.
            * platform/chromium-linux/svg/stroke/path-zero-strokewidth-test-expected.png: Removed.
            * platform/chromium-linux/svg/stroke/path-zero-strokewidth-test-expected.txt: Removed.
            * platform/chromium-mac/svg/stroke/path-zero-strokewidth-test-expected.checksum: Removed.
            * platform/chromium-mac/svg/stroke/path-zero-strokewidth-test-expected.png: Removed.
            * platform/chromium-mac/svg/stroke/path-zero-strokewidth-test-expected.txt: Removed.
            * platform/mac/svg/stroke/path-zero-strokewidth-test-expected.checksum: Removed.
            * platform/mac/svg/stroke/path-zero-strokewidth-test-expected.png: Removed.
            * platform/mac/svg/stroke/path-zero-strokewidth-test-expected.txt: Removed.
            * svg/stroke/path-zero-strokewidth-test.svg: Removed.
    2010-07-16  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r63593.
            http://trac.webkit.org/changeset/63593
            https://bugs.webkit.org/show_bug.cgi?id=42487
    
            Broke a few chromium pixel tests (Requested by tony^work on
            #webkit).
    
            * platform/graphics/skia/GraphicsContextSkia.cpp:
            (WebCore::GraphicsContext::drawConvexPolygon):
            (WebCore::GraphicsContext::drawEllipse):
            (WebCore::GraphicsContext::drawLine):
            (WebCore::GraphicsContext::strokeArc):
            (WebCore::GraphicsContext::strokePath):
            (WebCore::GraphicsContext::strokeRect):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63598 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
+++ third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
@@ -474,8 +474,7 @@ void GraphicsContext::drawConvexPolygon(size_t numPoints,
 
     if (strokeStyle() != NoStroke) {
         paint.reset();
-        if (!platformContext()->setupPaintForStroking(&paint, 0, 0))
-            return;
+        platformContext()->setupPaintForStroking(&paint, 0, 0);
         platformContext()->canvas()->drawPath(path, paint);
     }
 }
@@ -507,8 +506,7 @@ void GraphicsContext::drawEllipse(const IntRect& elipseRect)
 
     if (strokeStyle() != NoStroke) {
         paint.reset();
-        if (!platformContext()->setupPaintForStroking(&paint, &rect, 0))
-            return;
+        platformContext()->setupPaintForStroking(&paint, &rect, 0);
         platformContext()->canvas()->drawOval(rect, paint);
     }
 }
@@ -571,8 +569,7 @@ void GraphicsContext::drawLine(const IntPoint& point1, const IntPoint& point2)
     // probably worth the speed up of no square root, which also won't be exact.
     FloatSize disp = p2 - p1;
     int length = SkScalarRound(disp.width() + disp.height());
-    if (!platformContext()->setupPaintForStroking(&paint, 0, length))
-        return;
+    platformContext()->setupPaintForStroking(&paint, 0, length);
 
     if (strokeStyle() == DottedStroke || strokeStyle() == DashedStroke) {
         // Do a rect fill of our endpoints.  This ensures we always have the
@@ -1114,10 +1111,8 @@ void GraphicsContext::strokeArc(const IntRect& r, int startAngle, int angleSpan)
         platformContext()->setupPaintForFilling(&paint);
         paint.setStyle(SkPaint::kStroke_Style);
         paint.setStrokeWidth(WebCoreFloatToSkScalar(strokeThickness()));
-    } else {
-        if (!platformContext()->setupPaintForStroking(&paint, 0, 0))
-            return;
-    }
+    } else
+        platformContext()->setupPaintForStroking(&paint, 0, 0);
 
     // We do this before converting to scalar, so we don't overflow SkFixed.
     startAngle = fastMod(startAngle, 360);
@@ -1140,8 +1135,7 @@ void GraphicsContext::strokePath()
         return;
 
     SkPaint paint;
-    if (!platformContext()->setupPaintForStroking(&paint, 0, 0))
-        return;
+    platformContext()->setupPaintForStroking(&paint, 0, 0);
     platformContext()->canvas()->drawPath(path, paint);
 }
 
@@ -1154,8 +1148,7 @@ void GraphicsContext::strokeRect(const FloatRect& rect, float lineWidth)
         return;
 
     SkPaint paint;
-    if (!platformContext()->setupPaintForStroking(&paint, 0, 0))
-        return;
+    platformContext()->setupPaintForStroking(&paint, 0, 0);
     paint.setStrokeWidth(WebCoreFloatToSkScalar(lineWidth));
     platformContext()->canvas()->drawRect(rect, paint);
 }
