6cebb6427bc5f3b723f31cfbaa7ab8b7279bf64c
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-16 04:58:30 +0000

    2010-07-15  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Maciej Stachowiak.
    
            Crash entering mail.yahoo.com
            https://bugs.webkit.org/show_bug.cgi?id=42394
        
            * bytecompiler/BytecodeGenerator.cpp:
            (JSC::BytecodeGenerator::argumentNumberFor): Added a NULL check. If the
            identifier we're resolving is not a local variable, registerFor returns
            NULL.
    
            * bytecompiler/NodesCodegen.cpp:
            (JSC::FunctionBodyNode::emitBytecode): Unrelated to the crash, but I
            noticed this while working on it: No need to NULL-check returnNode,
            since an early return has already done so.
    2010-07-15  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Maciej Stachowiak.
    
            Test for https://bugs.webkit.org/show_bug.cgi?id=42394
            Crash entering mail.yahoo.com
    
            * fast/js/numeric-compare.html: Added.
            * fast/js/script-tests/numeric-compare.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63515 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/numeric-compare.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+'Tests that compiling a numeric comparison function does not crash'
+);
+
+var a, b;
+(function () { return a - b; })();
+
+var successfullyParsed = true;
