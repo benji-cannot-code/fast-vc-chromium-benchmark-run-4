136e15f51227a6c12639354145f0fb9cd03eda3b
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-16 02:56:47 +0000

    renderer_host: Remove CanBlur method.
    This method was not implemented and always returns true.
    
    (Note: this was a TODO for brettw).
    
    BUG=None
    TEST=trybots
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>
    
    Review URL: http://codereview.chromium.org/2870050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52600 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_view_host_delegate.cc
+++ chrome/browser/renderer_host/render_view_host_delegate.cc
@@ -89,10 +89,6 @@ WebPreferences RenderViewHostDelegate::GetWebkitPrefs() {
   return WebPreferences();
 }
 
-bool RenderViewHostDelegate::CanBlur() const {
-  return true;
-}
-
 gfx::Rect RenderViewHostDelegate::GetRootWindowResizerRect() const {
   return gfx::Rect();
 }
