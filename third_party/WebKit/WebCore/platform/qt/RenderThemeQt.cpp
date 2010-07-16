8ae9e0812c9386ac08040bf2d2bfbb4301593ef9
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-16 19:37:49 +0000

    2010-07-15  Antonio Gomes  <tonikitoo@webkit.org>
            Reviewed by Gustavo Noronha.
    
            [Qt] Remove unnecessary WebKit headers inclusion from WebCore files
            https://bugs.webkit.org/show_bug.cgi?id=42416
    
            There are some places in WebCore unnecessarily including WebKit headers.
    
            Cleaning up only, no testing needed.
    
            * platform/network/qt/ResourceHandleQt.cpp:
            * platform/qt/RenderThemeQt.c
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/RenderThemeQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/RenderThemeQt.cpp
@@ -59,8 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScrollbarThemeQt.h"
 #include "TimeRanges.h"
 #include "UserAgentStyleSheets.h"
-#include "qwebpage.h"
-
 
 #include <QApplication>
 #include <QColor>
--- third_party/WebKit/WebCore/platform/qt/WidgetQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/WidgetQt.cpp
@@ -42,14 +42,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "QWebPageClient.h"
 #include "ScrollView.h"
 
-#include "qwebframe.h"
-#include "qwebframe_p.h"
-#include "qwebpage.h"
-
 #include <QCoreApplication>
 #include <QDebug>
 #include <QPaintEngine>
 #include <QPainter>
+#include <QWidget>
 
 namespace WebCore {
 
