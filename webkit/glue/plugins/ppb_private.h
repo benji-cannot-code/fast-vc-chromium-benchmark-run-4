ab06f464f6dca1b228120f562bb41fb9248321fb
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-16 22:48:03 +0000

    Add third_party/ prefix to ppapi include for checkdeps.
    The only users of this ppb should be inside chrome so this should work fine.
    
    TBR=jam@chromium.org
    
    Review URL: http://codereview.chromium.org/3019006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52766 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/ppb_private.h
+++ webkit/glue/plugins/ppb_private.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_GLUE_PLUGINS_PPB_PRIVATE_H_
 #define WEBKIT_GLUE_PLUGINS_PPB_PRIVATE_H_
 
-#include "ppapi/c/pp_var.h"
+#include "third_party/ppapi/c/pp_var.h"
 
 #define PPB_PRIVATE_INTERFACE "PPB_Private;1"
 
