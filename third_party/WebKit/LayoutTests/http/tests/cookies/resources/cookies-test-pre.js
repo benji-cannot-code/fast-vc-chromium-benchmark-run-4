9f398b569e5cf8de43ad0caf42cdd75626741827
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-16 18:22:41 +0000

    LayoutTests/http/tests/cookies/multiple-cookies.html sometimes fails because the cookies are returned in a different order https://bugs.webkit.org/show_bug.cgi?id=42095
    Reviewed by Alexey Proskuryakov.
    
    Normalize the cookie strings by splitting them into an array, sorting the array and then joining back.
    
    * http/tests/cookies/multiple-cookies-expected.txt:
    * http/tests/cookies/resources/cookies-test-pre.js:
    (normalizeCookie):
    (testCookies):
    * websocket/tests/httponly-cookie-expected.txt:
    * websocket/tests/httponly-cookie.pl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/cookies/resources/cookies-test-pre.js
+++ third_party/WebKit/LayoutTests/http/tests/cookies/resources/cookies-test-pre.js
@@ -173,6 +173,13 @@ function setCookies(cookie)
     }
 }
 
+// Normalize a cookie string
+function normalizeCookie(cookie)
+{
+    // Split the cookie string, sort it and then put it back together.
+    return cookie.split('; ').sort().join('; ');
+}
+
 // We get the cookies throught an XMLHttpRequest.
 function testCookies(result)
 {
@@ -180,6 +187,11 @@ function testCookies(result)
     xhr.open("GET", "resources/getCookies.cgi", false);
     xhr.send(null);
     var cookie = xhr.getResponseHeader("HTTP_COOKIE") == null ? '"null"' : xhr.getResponseHeader("HTTP_COOKIE");
+
+    // Normalize the cookie strings.
+    result = normalizeCookie(result);
+    cookie = normalizeCookie(cookie);
+    
     if (cookie === result)
         testPassed("cookie is '" + cookie + "'.");
     else
