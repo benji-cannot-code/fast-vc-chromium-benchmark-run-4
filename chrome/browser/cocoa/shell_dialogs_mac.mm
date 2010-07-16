905f4676e4f4e375e3a42adfec1b380d33f7a308
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-16 21:14:29 +0000

    When using SelectFileDialog::SelectFile with type SELECT_FOLDER, set button title to 'Select' and show the 'New Folder' button.
    BUG=none
    TEST=From preferences select a new download folder, verify button title and new folder button.
    Review URL: http://codereview.chromium.org/3028002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52747 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/shell_dialogs_mac.mm
+++ chrome/browser/cocoa/shell_dialogs_mac.mm
@@ -11,12 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 
+#include "app/l10n_util_mac.h"
 #import "base/cocoa_protocols_mac.h"
 #include "base/logging.h"
 #include "base/mac_util.h"
 #include "base/scoped_cftyperef.h"
 #import "base/scoped_nsobject.h"
 #include "base/sys_string_conversions.h"
+#include "grit/generated_resources.h"
 
 static const int kFileTypePopupTag = 1234;
 
@@ -210,6 +212,9 @@ void SelectFileDialogImpl::SelectFile(
     if (type == SELECT_FOLDER) {
       [open_dialog setCanChooseFiles:NO];
       [open_dialog setCanChooseDirectories:YES];
+      [open_dialog setCanCreateDirectories:YES];
+      NSString *prompt = l10n_util::GetNSString(IDS_SELECT_FOLDER_BUTTON_TITLE);
+      [open_dialog setPrompt:prompt];
     } else {
       [open_dialog setCanChooseFiles:YES];
       [open_dialog setCanChooseDirectories:NO];
