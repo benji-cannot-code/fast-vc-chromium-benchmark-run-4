d2ec0c035260d100354fe92c1ebdce96ddc4f747
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-17 16:55:43 +0000

    Nukes the nano tab style on gtk and brings back the toolbar for apps on gtk. This is a revert of most of r52070and 51478. I've kept parts of these changes we want in though: comments, nuking nano tab gap on views, having app tabs use a bigger icon.
    BUG=49013
    TEST=none
    
    Review URL: http://codereview.chromium.org/3011007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52830 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/tabs/tab_strip_gtk.cc
+++ chrome/browser/gtk/tabs/tab_strip_gtk.cc
@@ -1085,10 +1085,6 @@ bool TabStripGtk::IsTabDetached(const TabGtk* tab) const {
   return false;
 }
 
-bool TabStripGtk::IsToolbarVisible(const TabGtk* tab) const {
-  return model_->IsToolbarVisible(GetIndexOfTab(tab));
-}
-
 void TabStripGtk::GetCurrentTabWidths(double* unselected_width,
                                       double* selected_width) const {
   *unselected_width = current_unselected_width_;
@@ -1100,13 +1096,6 @@ bool TabStripGtk::IsTabPinned(const TabGtk* tab) const {
     return false;
 
   return model_->IsTabPinned(GetIndexOfTab(tab));
-}
-
-bool TabStripGtk::IsAppTab(const TabGtk* tab) const {
-  if (tab->closing())
-    return false;
-
-  return model_->IsAppTab(GetIndexOfTab(tab));
 }
 
 void TabStripGtk::SelectTab(TabGtk* tab) {
