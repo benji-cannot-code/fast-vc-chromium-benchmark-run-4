e11412754752fd8a45b93b50934627a05a3678e7
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-17 06:10:39 +0000

    Revert 52787 - Make the personal stuff page viewable.
    BUG=48883
    TEST=Exercise Personal Stuff page via --enabled-tabbed-options
    
    Review URL: http://codereview.chromium.org/2819046
    
    TBR=sargrass@google.com
    Review URL: http://codereview.chromium.org/3012007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52810 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/options_ui.cc
+++ chrome/browser/dom_ui/options_ui.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/dom_ui/content_settings_handler.h"
 #include "chrome/browser/dom_ui/core_options_handler.h"
 #include "chrome/browser/dom_ui/personal_options_handler.h"
-#include "chrome/browser/dom_ui/sync_options_handler.h"
 #include "chrome/browser/metrics/user_metrics.h"
 #include "chrome/browser/pref_service.h"
 #include "chrome/browser/profile.h"
@@ -118,11 +117,10 @@ OptionsUI::OptionsUI(TabContents* contents) : DOMUI(contents) {
   // TODO(zelidrag): Add all other page handlers here as we implement them.
   AddOptionsPageUIHandler(localized_strings, new BrowserOptionsHandler());
   AddOptionsPageUIHandler(localized_strings, new PersonalOptionsHandler());
-  AddOptionsPageUIHandler(localized_strings, new SyncOptionsHandler());
   AddOptionsPageUIHandler(localized_strings, new AdvancedOptionsHandler());
-  AddOptionsPageUIHandler(localized_strings, new ContentSettingsHandler());
 #if defined(OS_CHROMEOS)
   AddOptionsPageUIHandler(localized_strings, new SystemOptionsHandler());
+  AddOptionsPageUIHandler(localized_strings, new SyncOptionsHandler());
   AddOptionsPageUIHandler(localized_strings, new LabsHandler());
   AddOptionsPageUIHandler(localized_strings,
                           new LanguageHangulOptionsHandler());
@@ -130,6 +128,7 @@ OptionsUI::OptionsUI(TabContents* contents) : DOMUI(contents) {
   AddOptionsPageUIHandler(localized_strings,
                           new chromeos::AccountsOptionsHandler());
 #endif
+  AddOptionsPageUIHandler(localized_strings, new ContentSettingsHandler());
 
   // |localized_strings| ownership is taken over by this constructor.
   OptionsUIHTMLSource* html_source =
