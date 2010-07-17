a31d9a91dd426f798f52fbb5641a6ec89dfbce58
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-17 00:30:18 +0000

    Implement IsSearchProviderInstalled and a test for it.
    It is currently off by default and one must pass in a flag
    (--enable-search-provider-api-v2) to use it. Api details are
    in the bug.
    
    BUG=38475
    TEST=ui_tests --gtest_filter=SearchProviderTest.TestIsSearchProviderInstalled
    
    Review URL: http://codereview.chromium.org/2823042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52778 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_view_host_delegate.cc
+++ chrome/browser/renderer_host/render_view_host_delegate.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/render_view_host_delegate.h"
 
 #include "base/singleton.h"
+#include "chrome/common/render_messages.h"
 #include "chrome/common/renderer_preferences.h"
 #include "gfx/rect.h"
 #include "googleurl/src/gurl.h"
@@ -83,6 +84,11 @@ TabContents* RenderViewHostDelegate::GetAsTabContents() {
 
 GURL RenderViewHostDelegate::GetAlternateErrorPageURL() const {
   return GURL();
+}
+
+ViewHostMsg_GetSearchProviderInstallState_Params
+RenderViewHostDelegate::GetSearchProviderInstallState(const GURL& url) {
+  return ViewHostMsg_GetSearchProviderInstallState_Params::Denied();
 }
 
 WebPreferences RenderViewHostDelegate::GetWebkitPrefs() {
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -319,6 +319,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/unload_uitest.cc',
         'browser/views/find_bar_host_uitest.cc',
         'common/logging_chrome_uitest.cc',
+        'renderer/external_extension_uitest.cc',
         'test/automation/automation_proxy_uitest.cc',
         'test/automation/extension_proxy_uitest.cc',
         'test/automated_ui_tests/automated_ui_test_test.cc',
