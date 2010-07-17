c4b3002f9275a38af6fbfd8bff95dbe1ef5711a6
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-17 06:32:07 +0000

    Revert 52795 - Fix chromeos compile.
    TBR=sargrass
    BUG=none
    TEST=chromeos builder goes green
    
    TBR=csilv@chromium.org
    Review URL: http://codereview.chromium.org/3032006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52812 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/options_ui.cc
+++ chrome/browser/dom_ui/options_ui.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/dom_ui/labs_handler.h"
 #include "chrome/browser/chromeos/dom_ui/language_hangul_options_handler.h"
 #include "chrome/browser/chromeos/dom_ui/language_options_handler.h"
+#include "chrome/browser/chromeos/dom_ui/sync_options_handler.h"
 #include "chrome/browser/chromeos/dom_ui/system_options_handler.h"
 #endif
 
