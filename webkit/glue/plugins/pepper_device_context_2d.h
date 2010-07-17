cec3d1c2e2ccd9f17fe64e8f1241bb94b10724e8
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-17 06:14:57 +0000

    Avoid flashing when resizing plugins that use PaintManager.  Copy the bitmap from the old backing store to the new one so that we have something to display until the plugin repaints.  Credit to Darin for tracking this down and the suggested fix. Review URL: http://codereview.chromium.org/2834052
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52811 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/pepper_device_context_2d.h
+++ webkit/glue/plugins/pepper_device_context_2d.h
@@ -68,6 +68,8 @@ class DeviceContext2D : public Resource {
   void ViewInitiatedPaint();
   void ViewFlushedPaint();
 
+  ImageData* image_data() { return image_data_.get(); }
+
  private:
   // Tracks a call to flush that requires a callback.
   class FlushCallbackData {
