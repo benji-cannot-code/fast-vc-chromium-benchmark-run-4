59b8aeb56d6eb701fbabffc828d323453ac3bee9
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-17 16:50:13 +0000

    Implement WebFileSystemImpl::directoryName.  This is needed for directory upload (including webkit layout tests).
    BUG=41762
    TEST=webkit layout test fast/forms/input-file-directory-upload.html
    
    Review URL: http://codereview.chromium.org/2847056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52829 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webfilesystem_impl.cc
+++ webkit/glue/webfilesystem_impl.cc
@@ -60,8 +60,8 @@ bool WebFileSystemImpl::getFileModificationTime(const WebString& path,
 }
 
 WebString WebFileSystemImpl::directoryName(const WebString& path) {
-  NOTREACHED();
-  return WebString();
+  FilePath file_path(WebStringToFilePathString(path));
+  return FilePathToWebString(file_path.DirName());
 }
 
 WebString WebFileSystemImpl::pathByAppendingComponent(
