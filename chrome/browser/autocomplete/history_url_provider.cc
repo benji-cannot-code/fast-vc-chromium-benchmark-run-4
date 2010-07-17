2c631e66096ff9462e8315150988d947078c8823
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-17 15:19:36 +0000

    Header Cleanup: Remove unncessary include of sqlite_utils.h in some files.
    BUG=None
    TEST=trybots
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>
    
    Review URL: http://codereview.chromium.org/2843061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52827 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_url_provider.cc
+++ chrome/browser/autocomplete/history_url_provider.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
-#include "chrome/common/sqlite_utils.h"
 #include "chrome/common/url_constants.h"
 #include "googleurl/src/gurl.h"
 #include "googleurl/src/url_parse.h"
--- chrome/browser/history/history_backend.cc
+++ chrome/browser/history/history_backend.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/notification_type.h"
-#include "chrome/common/sqlite_utils.h"
 #include "chrome/common/url_constants.h"
 #include "googleurl/src/gurl.h"
 #include "grit/chromium_strings.h"
