19ffe006b7a08cc77d77116e0eb060ddb2b9d3c1
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-17 00:41:14 +0000

    Added inferred labels implementation. Label inferred from the person name + differences in other fields to distinguish entries. Also added it to generated previews. TODO: adjust indices in the Credit Cards. TEST=unit tested. BUG=none Review URL: http://codereview.chromium.org/2835026
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52786 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager_unittest.cc
+++ chrome/browser/autofill/autofill_manager_unittest.cc
@@ -312,8 +312,10 @@ TEST_F(AutoFillManagerTest, GetProfileSuggestionsEmptyValue) {
   EXPECT_EQ(ASCIIToUTF16("Elvis"), values[0]);
   EXPECT_EQ(ASCIIToUTF16("Charles"), values[1]);
   ASSERT_EQ(2U, labels.size());
-  EXPECT_EQ(ASCIIToUTF16("Home"), labels[0]);
-  EXPECT_EQ(ASCIIToUTF16("Work"), labels[1]);
+  // Inferred labels now include full first relevant field, which in this case
+  // the address #1.
+  EXPECT_EQ(ASCIIToUTF16("3734 Elvis Presley Blvd."), labels[0]);
+  EXPECT_EQ(ASCIIToUTF16("123 Apple St."), labels[1]);
 }
 
 TEST_F(AutoFillManagerTest, GetProfileSuggestionsMatchCharacter) {
@@ -347,7 +349,7 @@ TEST_F(AutoFillManagerTest, GetProfileSuggestionsMatchCharacter) {
   ASSERT_EQ(1U, values.size());
   EXPECT_EQ(ASCIIToUTF16("Elvis"), values[0]);
   ASSERT_EQ(1U, labels.size());
-  EXPECT_EQ(ASCIIToUTF16("Home"), labels[0]);
+  EXPECT_EQ(ASCIIToUTF16("3734 Elvis Presley Blvd."), labels[0]);
 }
 
 TEST_F(AutoFillManagerTest, GetCreditCardSuggestionsEmptyValue) {
@@ -591,8 +593,8 @@ TEST_F(AutoFillManagerTest, GetCombinedAutoFillAndAutocompleteSuggestions) {
   EXPECT_EQ(ASCIIToUTF16("Jay"), values[2]);
   EXPECT_EQ(ASCIIToUTF16("Jason"), values[3]);
   ASSERT_EQ(4U, labels.size());
-  EXPECT_EQ(ASCIIToUTF16("Home"), labels[0]);
-  EXPECT_EQ(ASCIIToUTF16("Work"), labels[1]);
+  EXPECT_EQ(ASCIIToUTF16("3734 Elvis Presley Blvd."), labels[0]);
+  EXPECT_EQ(ASCIIToUTF16("123 Apple St."), labels[1]);
   EXPECT_EQ(string16(), labels[2]);
   EXPECT_EQ(string16(), labels[3]);
 }
