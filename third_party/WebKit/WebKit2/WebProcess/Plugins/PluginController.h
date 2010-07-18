391b94076b6ebf69e70bb542ffea9cd53f8ab914
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-18 18:57:38 +0000

    Begin work on NPRuntime support https://bugs.webkit.org/show_bug.cgi?id=42519
    Reviewed by Dan Bernstein.
    
    * WebProcess/Plugins/Netscape/NetscapeBrowserFuncs.cpp:
    (WebKit::NPN_GetValue):
    Handle NPNVWindowNPObject and NPNVPluginElementNPObject.
    
    * WebProcess/Plugins/Netscape/NetscapePlugin.cpp:
    (WebKit::NetscapePlugin::windowScriptNPObject):
    (WebKit::NetscapePlugin::pluginElementNPObject):
    Call the plug-in controller.
    
    * WebProcess/Plugins/PluginController.h:
    Add new windowScriptNPObject and pluginElementNPObject functions.
    
    * WebProcess/Plugins/PluginView.cpp:
    (WebKit::PluginView::windowScriptNPObject):
    (WebKit::PluginView::pluginElementNPObject):
    Add stubbed out functions.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63629 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/Plugins/PluginController.h
+++ third_party/WebKit/WebKit2/WebProcess/Plugins/PluginController.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PluginController_h
 #define PluginController_h
 
+struct NPObject;
+
 namespace WebCore {
     class IntRect;
     class KURL;
@@ -54,6 +56,12 @@ public:
 
     /// Cancels the load of a stream that was requested by loadURL.
     virtual void cancelStreamLoad(uint64_t streamID) = 0;
+
+    // Get the NPObject that corresponds to the window JavaScript object. Returns a retained object.
+    virtual NPObject* windowScriptNPObject() = 0;
+
+    // Get the NPObject that corresponds to the plug-in's element. Returns a retained object.
+    virtual NPObject* pluginElementNPObject() = 0;
 
 protected:
     virtual ~PluginController() { }
