b29c378a16584eec9635fa2b06b7ff62e0804769
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-19 20:04:56 +0000

    Don't do Negotiate with GSSAPI if default credentials are not allowed.
    GSSAPI does not provide a mechanism for the user to specify username/password to obtain a TGT. If default credentials are not allowed for an end site, skip negotiate and use a different scheme.
    
    Arguably in this case it may make sense to simply prompt the user whether they want to use their existing Kerberos credentials to authenticate to the server and use the existing TGT, but we'll need UI changes.
    
    BUG=33033
    TEST=net_unittests, try to authenticate to a Kerberized server which is not in the whitelist.
    
    Review URL: http://codereview.chromium.org/3013003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52943 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_unittest.cc
+++ net/http/http_auth_unittest.cc
@@ -71,8 +71,11 @@ TEST(HttpAuthTest, ChooseBestChallenge) {
     }
   };
   GURL origin("http://www.example.com");
-  scoped_ptr<HttpAuthHandlerFactory> http_auth_handler_factory(
+  URLSecurityManagerAllow url_security_manager;
+  scoped_ptr<HttpAuthHandlerRegistryFactory> http_auth_handler_factory(
       HttpAuthHandlerFactory::CreateDefault());
+  http_auth_handler_factory->SetURLSecurityManager(
+      "negotiate", &url_security_manager);
 
   for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
     // Make a HttpResponseHeaders object.
@@ -187,8 +190,11 @@ TEST(HttpAuthTest, ChooseBestChallengeConnectionBasedNegotiate) {
     }
   };
   GURL origin("http://www.example.com");
-  scoped_ptr<HttpAuthHandlerFactory> http_auth_handler_factory(
+  URLSecurityManagerAllow url_security_manager;
+  scoped_ptr<HttpAuthHandlerRegistryFactory> http_auth_handler_factory(
       HttpAuthHandlerFactory::CreateDefault());
+  http_auth_handler_factory->SetURLSecurityManager(
+      "negotiate", &url_security_manager);
 
   scoped_ptr<HttpAuthHandler> handler;
   for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
