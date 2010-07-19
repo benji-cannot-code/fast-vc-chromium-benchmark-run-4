78e84e652ec6366a99476d45e4de4adcabc3748d
ziadh@chromium.org <ziadh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-19 08:00:42 +0000

    Compress and checksum pending logs that are going to be persisted. Persisted logs now have the following format: [list_size, log1, log2, ..., log_n, checksum].
    where each log is bzipped before being written. Upon reading the logs from disk, we verify the data and register whether we faced corruptions or not.
    
    r=jar
    
    
    
    Review URL: http://codereview.chromium.org/2936005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52885 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/metrics_helpers.cc
+++ chrome/common/metrics_helpers.cc
@@ -411,7 +411,7 @@ void MetricsLogBase::RecordHistogramDelta(
 // MetricsServiceBase
 MetricsServiceBase::MetricsServiceBase()
     : pending_log_(NULL),
-      pending_log_text_(),
+      compressed_log_(),
       current_log_(NULL),
       logged_samples_() {
 }
@@ -513,6 +513,5 @@ void MetricsServiceBase::DiscardPendingLog() {
     delete pending_log_;
     pending_log_ = NULL;
   }
-  pending_log_text_.clear();
+  compressed_log_.clear();
 }
-
