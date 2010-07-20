f4590ccf136486d504c9bc994cb6bfa2f9feca31
mirandac@google.com <mirandac@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-20 03:45:40 +0000

    Add bookmark promo to NTP on first run.
    BUG=49328
    TEST=First tip seen on import to empty profile is import bookmarks promo. This promo appears on the first 5 browser restarts, unless bookmarks are imported.
    
    Review URL: http://codereview.chromium.org/2805089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52993 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -1921,6 +1921,7 @@ void Browser::RegisterUserPrefs(PrefService* prefs) {
   prefs->RegisterBooleanPref(prefs::kWebAppCreateInQuickLaunchBar, true);
   prefs->RegisterBooleanPref(prefs::kUseVerticalTabs, false);
   prefs->RegisterBooleanPref(prefs::kEnableTranslate, true);
+  prefs->RegisterIntegerPref(prefs::kNTPPromoViewsRemaining, 5);
 }
 
 // static
