2941018930c4dfb8fb52affc50cc7f273011c83c
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-20 20:23:52 +0000

    2010-07-20  Darin Fisher  <darin@chromium.org>
            Fix Chromium build bustage.
    
            * src/ResourceHandle.cpp:
            (WebCore::ResourceHandle::hasAuthenticationChallenge): Add
            implementation of this method that always returns false.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63770 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/ResourceHandle.cpp
+++ third_party/WebKit/WebKit/chromium/src/ResourceHandle.cpp
@@ -253,6 +253,11 @@ bool ResourceHandle::start(Frame* deprecated)
     return true;
 }
 
+bool ResourceHandle::hasAuthenticationChallenge() const
+{
+    return false;
+}
+
 void ResourceHandle::clearAuthentication()
 {
 }
