a5f34a7918a636202943f1e191dbe009eed22fbf
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-20 10:15:41 +0000

    Add "settings.language.preferred_languages" for Chromium OS's Languages and Input dialog.
    Before this change, we used to get the list of preferred languages based on
    the active input methods. This is tricky. Having a preference for the value
    is simpler, and makes it easier for us to implement the DOM UI version.
    
    BUG=none
    TEST=manually
    
    Review URL: http://codereview.chromium.org/3012012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53035 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/synchronized_preferences.h
+++ chrome/browser/sync/glue/synchronized_preferences.h
@@ -126,6 +126,7 @@ static const wchar_t* kSynchronizedPreferences[] = {
   prefs::kLanguagePinyinMinusEqualPage,
   prefs::kLanguagePinyinShiftSelectCandidate,
   prefs::kLanguagePinyinTradCandidate,
+  prefs::kLanguagePreferredLanguages,
   prefs::kLanguagePreloadEngines,
   // TODO(yusukes): Add prefs for ibus-mozc (Japanese input method).
   //   prefs::kLanguageMozcPreeditMethod,
