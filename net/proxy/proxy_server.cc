671885d31a968f3770f45474b244faf901bc7c1d
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-20 17:55:53 +0000

    Add some plumbing for in-progress work on enabling SSL proxy support.
    BUG=29625
    TEST=none
    
    Review URL: http://codereview.chromium.org/2832057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53061 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_server.cc
+++ net/proxy/proxy_server.cc
@@ -35,6 +35,8 @@ ProxyServer::Scheme GetSchemeFromPacType(std::string::const_iterator begin,
     return ProxyServer::SCHEME_SOCKS5;
   if (LowerCaseEqualsASCII(begin, end, "direct"))
     return ProxyServer::SCHEME_DIRECT;
+  if (LowerCaseEqualsASCII(begin, end, "https"))
+    return ProxyServer::SCHEME_HTTPS;
 
   return ProxyServer::SCHEME_INVALID;
 }
@@ -54,6 +56,8 @@ ProxyServer::Scheme GetSchemeFromURI(std::string::const_iterator begin,
     return ProxyServer::SCHEME_SOCKS5;
   if (LowerCaseEqualsASCII(begin, end, "direct"))
     return ProxyServer::SCHEME_DIRECT;
+  if (LowerCaseEqualsASCII(begin, end, "https"))
+    return ProxyServer::SCHEME_HTTPS;
   return ProxyServer::SCHEME_INVALID;
 }
 
@@ -133,6 +137,8 @@ std::string ProxyServer::ToURI() const {
       return std::string("socks4://") + host_and_port();
     case SCHEME_SOCKS5:
       return std::string("socks5://") + host_and_port();
+    case SCHEME_HTTPS:
+      return std::string("https://") + host_and_port();
     default:
       // Got called with an invalid scheme.
       NOTREACHED();
@@ -181,6 +187,8 @@ std::string ProxyServer::ToPacString() const {
       return std::string("SOCKS ") + host_and_port();
     case SCHEME_SOCKS5:
       return std::string("SOCKS5 ") + host_and_port();
+    case SCHEME_HTTPS:
+      return std::string("HTTPS ") + host_and_port();
     default:
       // Got called with an invalid scheme.
       NOTREACHED();
@@ -196,6 +204,8 @@ int ProxyServer::GetDefaultPortForScheme(Scheme scheme) {
     case SCHEME_SOCKS4:
     case SCHEME_SOCKS5:
       return 1080;
+    case SCHEME_HTTPS:
+      return 443;
     default:
       return -1;
   }
