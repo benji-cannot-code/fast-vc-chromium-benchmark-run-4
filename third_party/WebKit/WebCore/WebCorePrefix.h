cc18f8b65ff27793bdd250290b41e235bae3d3e4
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-20 04:32:03 +0000

    (Hopefully the last) Windows build fix.
    Version of CoreServices.h in WebKit Support Libraries uses
    pragma once, unlike the version I have locally. Switch based
    on __COLORSYNCDEPRECATED__ instead.
    
    * WebCorePrefix.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63717 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCorePrefix.h
+++ third_party/WebKit/WebCore/WebCorePrefix.h
@@ -139,7 +139,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // FIXME <rdar://problem/8208868> Remove support for obsolete ColorSync API, CoreServices header in CoreGraphics
 // We can remove this once the new ColorSync APIs are available in an internal Safari SDK.
 #include <ColorSync/ColorSync.h>
-#ifdef COLORSYNC_CORE_SERVICES_H_
+#ifdef __COLORSYNCDEPRECATED__
 #define COREGRAPHICS_INCLUDES_CORESERVICES_HEADER
 #define OBSOLETE_COLORSYNC_API
 #endif
