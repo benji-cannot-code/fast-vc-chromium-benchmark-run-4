39bf478c184c8165ea8dd49b429d87605153a28b
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-20 14:16:27 +0000

    Initial SPDY flow control support
    BUG=48100
    TEST=net_unittests --gtest_filter="SpdyProtocolTest.ControlFrameStructs:SpdyNetworkTransactionTest.WindowSizeChange:SpdyNetworkTransactionTest.WindowSizeOverflow"
    
    Contributed by: agayev@google.com
    
    Review URL: http://codereview.chromium.org/2805083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53039 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session_pool.h
+++ net/spdy/spdy_session_pool.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/host_port_pair.h"
 #include "net/base/net_errors.h"
 #include "net/base/network_change_notifier.h"
+#include "testing/gtest/include/gtest/gtest_prod.h"  // For FRIEND_TEST
 
 namespace net {
 
@@ -81,6 +82,7 @@ class SpdySessionPool
   friend class base::RefCounted<SpdySessionPool>;
   friend class SpdySessionPoolPeer;  // For testing.
   friend class SpdyNetworkTransactionTest;  // For testing.
+  FRIEND_TEST(SpdyNetworkTransactionTest, WindowUpdateOverflow);
 
   typedef std::list<scoped_refptr<SpdySession> > SpdySessionList;
   typedef std::map<HostPortPair, SpdySessionList*> SpdySessionsMap;
