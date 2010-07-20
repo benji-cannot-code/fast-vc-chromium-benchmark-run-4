bf62caf821d1f176750273a478ec2065493921dd
evan@chromium.org <evan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-20 22:23:45 +0000

    2010-07-20  Daniel Erat  <derat@chromium.org>
            Reviewed by Ojan Vafai.
    
            Subpixel rendering always disabled for Chromium Linux
            https://bugs.webkit.org/show_bug.cgi?id=42220
    
            Layout test addition to check that Chromium Linux honors fontconfig
            settings enabling or disabling subpixel rendering per-strike.
    
            * platform/chromium-linux/platform/chromium/fast/text/chromium-linux-fontconfig-renderstyle-expected.checksum:
            * platform/chromium-linux/platform/chromium/fast/text/chromium-linux-fontconfig-renderstyle-expected.png:
            * platform/chromium-linux/platform/chromium/fast/text/chromium-linux-fontconfig-renderstyle-expected.txt:
            * platform/chromium/fast/text/chromium-linux-fontconfig-renderstyle.html:
    2010-07-20  Daniel Erat  <derat@chromium.org>
    
            Reviewed by Ojan Vafai.
    
            Subpixel rendering always disabled for Chromium Linux
            https://bugs.webkit.org/show_bug.cgi?id=42220
    
            Explicitly initialize global Chromium Linux isSkiaSubpixelGlyphs
            flag to false for clarity.
    
            * platform/graphics/chromium/FontPlatformDataLinux.cpp:
    2010-07-20  Daniel Erat  <derat@chromium.org>
    
            Reviewed by Ojan Vafai.
    
            Subpixel rendering always disabled for Chromium Linux
            https://bugs.webkit.org/show_bug.cgi?id=42220
    
            Honor Fontconfig subpixel rendering setting on Chromium Linux.
    
            * src/gtk/WebFontInfo.cpp:
            (WebKit::WebFontInfo::renderStyleForStrike):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63780 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/chromium/FontPlatformDataLinux.cpp
+++ third_party/WebKit/WebCore/platform/graphics/chromium/FontPlatformDataLinux.cpp
@@ -44,7 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 static SkPaint::Hinting skiaHinting = SkPaint::kNormal_Hinting;
-static bool isSkiaAntiAlias = true, isSkiaSubpixelGlyphs;
+static bool isSkiaAntiAlias = true;
+static bool isSkiaSubpixelGlyphs = false;
 
 void FontPlatformData::setHinting(SkPaint::Hinting hinting)
 {
