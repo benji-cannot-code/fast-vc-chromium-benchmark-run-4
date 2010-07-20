5533a6ce18fc400aa158f7d62666434edca21ca7
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-20 04:17:53 +0000

    Windows build fix.
    * WebCorePrefix.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63716 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCorePrefix.h
+++ third_party/WebKit/WebCore/WebCorePrefix.h
@@ -136,9 +136,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 
 #if defined(WIN32) || defined(_WIN32)
-#include <CoreServices/CoreServices.h>
+// FIXME <rdar://problem/8208868> Remove support for obsolete ColorSync API, CoreServices header in CoreGraphics
+// We can remove this once the new ColorSync APIs are available in an internal Safari SDK.
+#include <ColorSync/ColorSync.h>
+#ifdef COLORSYNC_CORE_SERVICES_H_
 #define COREGRAPHICS_INCLUDES_CORESERVICES_HEADER
 #define OBSOLETE_COLORSYNC_API
+#endif
 /* Windows doesn't include CFNetwork.h via CoreServices.h, so we do
    it explicitly here to make Windows more consistent with Mac. */
 #include <CFNetwork/CFNetwork.h>
