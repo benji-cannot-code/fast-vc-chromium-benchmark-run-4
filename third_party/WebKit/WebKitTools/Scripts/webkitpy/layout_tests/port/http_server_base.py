55467ebbaa7c651cbea0b75584ddabfec697d1b7
antonm@chromium.org <antonm@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-20 16:44:49 +0000

    2010-07-20  Anton Muhin  <antonm@chromium.org>
            Reviewed by Darin Adler.
    
            Print additional information about exception if failed to connect to apache (in verbose mode).
            https://bugs.webkit.org/show_bug.cgi?id=42627
    
            * Scripts/webkitpy/layout_tests/port/http_server_base.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/http_server_base.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/http_server_base.py
@@ -68,8 +68,8 @@ class HttpServerBase(object):
             try:
                 response = urllib.urlopen(url)
                 _log.debug("Server running at %s" % url)
-            except IOError:
-                _log.debug("Server NOT running at %s" % url)
+            except IOError, e:
+                _log.debug("Server NOT running at %s: %s" % (url, e))
                 return False
 
         return True
