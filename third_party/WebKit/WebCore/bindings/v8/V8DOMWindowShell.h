d9c67801554691b55d91a295871917c4146d0c35
antonm@chromium.org <antonm@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-21 18:14:12 +0000

    2010-07-21  Anton Muhin  <antonm@chromium.org>
            Reviewed by Adam Barth.
    
            [v8] Revert r60670 as it introduced a regression: in some cases named children couldn't be retrieved.
            https://bugs.webkit.org/show_bug.cgi?id=42766
    
            See http://code.google.com/p/chromium/issues/detail?id=48804 for more details.
    
            * bindings/scripts/CodeGeneratorV8.pm:
            * bindings/v8/ScriptController.cpp:
            (WebCore::ScriptController::namedItemAdded):
            (WebCore::ScriptController::namedItemRemoved):
            * bindings/v8/V8DOMWindowShell.cpp:
            (WebCore::V8DOMWindowShell::updateDocumentWrapperCache):
            * bindings/v8/V8DOMWindowShell.h:
            * bindings/v8/V8DOMWrapper.cpp:
            (WebCore::V8DOMWrapper::instantiateV8Object):
            * bindings/v8/custom/V8HTMLDocumentCustom.cpp:
            (WebCore::V8HTMLDocument::namedPropertyDeleter):
            (WebCore::V8HTMLDocument::namedPropertyGetter):
            (WebCore::V8HTMLDocument::indexedPropertyGetter):
            (WebCore::V8HTMLDocument::allAccessorSetter):
            (WebCore::toV8):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63845 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8DOMWindowShell.h
+++ third_party/WebKit/WebCore/bindings/v8/V8DOMWindowShell.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef V8DOMWindowShell_h
 #define V8DOMWindowShell_h
 
-#include "AtomicString.h"
 #include "WrapperTypeInfo.h"
 #include <wtf/HashMap.h>
 #include <wtf/PassRefPtr.h>
@@ -43,7 +42,6 @@ namespace WebCore {
 
 class DOMWindow;
 class Frame;
-class HTMLDocument;
 class String;
 
 // V8WindowShell represents all the per-global object state for a Frame that
@@ -56,9 +54,6 @@ public:
 
     // Update document object of the frame.
     void updateDocument();
-
-    void namedItemAdded(HTMLDocument*, const AtomicString&);
-    void namedItemRemoved(HTMLDocument*, const AtomicString&);
 
     // Update the security origin of a document
     // (e.g., after setting docoument.domain).
