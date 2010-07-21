5de718b97b2c2986641a78971c99e01a07a04518
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-21 20:54:30 +0000

    Handle broken pipes in more places in CoreIPC
    Fixes <http://webkit.org/b/42784> Assertion failure in
    Connection::readEventHandler when WebKitTestRunner exits
    
    Reviewed by Anders Carlsson.
    
    * Platform/CoreIPC/win/ConnectionWin.cpp:
    (CoreIPC::Connection::readEventHandler): Check for a broken pipe
    whenever we call ::PeekNamedPipe.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Platform/CoreIPC/win/ConnectionWin.cpp
+++ third_party/WebKit/WebKit2/Platform/CoreIPC/win/ConnectionWin.cpp
@@ -125,6 +125,10 @@ void Connection::readEventHandler()
                 DWORD bytesToRead = 0;
                 if (!::PeekNamedPipe(m_connectionPipe, 0, 0, 0, 0, &bytesToRead)) {
                     DWORD error = ::GetLastError();
+                    if (error == ERROR_BROKEN_PIPE) {
+                        connectionDidClose();
+                        return;
+                    }
                     ASSERT_NOT_REACHED();
                     return;
                 }
@@ -175,6 +179,10 @@ void Connection::readEventHandler()
         DWORD bytesToRead = 0;
         if (!::PeekNamedPipe(m_connectionPipe, 0, 0, 0, 0, &bytesToRead)) {
             DWORD error = ::GetLastError();
+            if (error == ERROR_BROKEN_PIPE) {
+                connectionDidClose();
+                return;
+            }
             ASSERT_NOT_REACHED();
         }
         if (!bytesToRead) {
