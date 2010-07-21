a8ddcb2da1d59cc3715aa5f393ff7693265a87a3
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-21 03:23:56 +0000

    Rename the download to its final name only after the download is finished
    BUG=27687, 28928
    TEST=DownloadManagerTest.DownloadRenameTest
    
    Review URL: http://codereview.chromium.org/2877008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53149 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_item.cc
+++ chrome/browser/download/download_item.cc
@@ -44,7 +44,8 @@ DownloadItem::DownloadItem(const DownloadCreateInfo& info)
       is_otr_(false),
       is_extension_install_(info.is_extension_install),
       name_finalized_(false),
-      is_temporary_(false) {
+      is_temporary_(false),
+      need_final_rename_(false) {
   if (state_ == IN_PROGRESS)
     state_ = CANCELLED;
   Init(false /* don't start progress timer */);
@@ -93,7 +94,8 @@ DownloadItem::DownloadItem(int32 download_id,
       is_otr_(is_otr),
       is_extension_install_(is_extension_install),
       name_finalized_(false),
-      is_temporary_(is_temporary) {
+      is_temporary_(is_temporary),
+      need_final_rename_(false) {
   Init(true /* start progress timer */);
 }
 
