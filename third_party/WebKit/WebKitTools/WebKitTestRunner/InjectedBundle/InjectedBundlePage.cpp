306f7083d73b28f3382dc223381ae2af168efef8
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-21 01:43:30 +0000

    Fix obvious typo.
    Reviewed by Jon Honeycutt.
    
    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
    (WTR::InjectedBundlePage::setStatusbarText): Need to call ->data() to actually get the string value.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63797 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
+++ third_party/WebKit/WebKitTools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
@@ -218,7 +218,7 @@ void InjectedBundlePage::setStatusbarText(WKStringRef statusbarText)
         return;
 
     OwnPtr<Vector<char> > utf8StatusbarText = WKStringToUTF8(statusbarText);
-    InjectedBundle::shared().os() << "UI DELEGATE STATUS CALLBACK: setStatusText:" << utf8StatusbarText << "\n";
+    InjectedBundle::shared().os() << "UI DELEGATE STATUS CALLBACK: setStatusText:" << utf8StatusbarText->data() << "\n";
 }
 
 } // namespace WTR
