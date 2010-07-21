9e68091949134fd7dbd51257ab47674e17568765
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-21 18:26:45 +0000

    Download code cleanup:
    Changed the code to be more object-oriented. Instead of exposing 150 accessors
    we should do as much as possible inside the object, exposing a nice API.
    DownloadFile just got a small step closer to that.
    
    Also, reduce amount of state duplication. For example, information about
    the download progress was both in DownloadFile and DownloadFileManager
    (maybe it's still somewhere else). The download path information
    is still duplicated, removing it is going to be harder.
    
    Finally, removed completely unused state variables from DownloadFile.
    
    TEST=unit_tests, browser_tests, ui_tests
    BUG=48913
    
    Review URL: http://codereview.chromium.org/3026012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53217 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_file.h
+++ chrome/browser/download/download_file.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/gurl.h"
 
 struct DownloadCreateInfo;
+class ResourceDispatcherHost;
 
 // These objects live exclusively on the download thread and handle the writing
 // operations for one download. These objects live only for the duration that
@@ -31,8 +32,9 @@ class DownloadFile {
 
   bool Initialize();
 
-  // Write a new chunk of data to the file. Returns true on success.
-  bool AppendDataToFile(const char* data, int data_len);
+  // Write a new chunk of data to the file. Returns true on success (all bytes
+  // written to the file).
+  bool AppendDataToFile(const char* data, size_t data_len);
 
   // Abort the download and automatically close the file.
   void Cancel();
@@ -42,6 +44,9 @@ class DownloadFile {
   // Marked virtual for testing.
   virtual bool Rename(const FilePath& full_path, bool is_final_rename);
 
+  // Indicate that the download has finished. No new data will be received.
+  void Finish();
+
   // Informs the OS that this file came from the internet.
   void AnnotateWithSourceInformation();
 
@@ -49,16 +54,13 @@ class DownloadFile {
   // Marked virtual for testing.
   virtual void DeleteCrDownload();
 
+  // Cancels the download request associated with this file.
+  void CancelDownloadRequest(ResourceDispatcherHost* rdh);
+
   // Accessors.
-  int64 bytes_so_far() const { return bytes_so_far_; }
   int id() const { return id_; }
-  FilePath full_path() const { return full_path_; }
-  int child_id() const { return child_id_; }
-  int render_view_id() const { return render_view_id_; }
-  int request_id() const { return request_id_; }
   bool path_renamed() const { return path_renamed_; }
   bool in_progress() const { return file_stream_ != NULL; }
-  void set_in_progress(bool in_progress) { in_progress_ = in_progress; }
 
  private:
   // Open or Close the OS file stream. The stream is opened in the constructor
@@ -82,14 +84,9 @@ class DownloadFile {
 
   // IDs for looking up the tab we are associated with.
   int child_id_;
-  int render_view_id_;
 
   // Handle for informing the ResourceDispatcherHost of a UI based cancel.
   int request_id_;
-
-  // Amount of data received up to this point. We may not know in advance how
-  // much data to expect since some servers don't provide that information.
-  int64 bytes_so_far_;
 
   // Full path to the downloaded file.
   FilePath full_path_;
@@ -97,14 +94,8 @@ class DownloadFile {
   // Whether the download is still using its initial temporary path.
   bool path_renamed_;
 
-  // Whether the download is still receiving data.
-  bool in_progress_;
-
   // RAII handle to keep the system from sleeping while we're downloading.
   PowerSaveBlocker dont_sleep_;
-
-  // The provider used to save the download data.
-  DownloadSaveInfo save_info_;
 
   DISALLOW_COPY_AND_ASSIGN(DownloadFile);
 };
