683b9c676fc2eb9abc7ffd40a8de08346aa42a05
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-21 23:55:37 +0000

    AutoFill Empty profiles should not be saved from AutoFillDialog Add and Edit sheets on Mac.
    Changes the AutoFillDialog on Mac to not store intermediate results of "Add" and "Edit" actions
    when those actions result in empty address or credit card information.  Empty addresses and credit
    card information is culled at this level now, not just after "Save" from the main dialog.
    
    BUG=47742
    TEST=AutoFillDialogControllerTest.AddNewProfile, AutoFillDialogControllerTest.AddNewEmptyProfile, AutoFillDialogControllerTest.AddNewCreditCard, AutoFillDialogControllerTest.AddNewEmptyCreditCard
    
    Review URL: http://codereview.chromium.org/3061001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53276 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_profile.cc
+++ chrome/browser/autofill/autofill_profile.cc
@@ -206,6 +206,7 @@ string16 AutoFillProfile::PreviewSummary() const {
   return summary_format;
 }
 
+// static
 bool AutoFillProfile::AdjustInferredLabels(
     std::vector<AutoFillProfile*>* profiles) {
   std::vector<string16> created_labels;
@@ -223,6 +224,7 @@ bool AutoFillProfile::AdjustInferredLabels(
   return updated_labels;
 }
 
+// static
 void AutoFillProfile::CreateInferredLabels(
     const std::vector<AutoFillProfile*>* profiles,
     std::vector<string16>* created_labels,
@@ -374,6 +376,12 @@ void AutoFillProfile::CreateInferredLabels(
               &non_empty_fields);
     }
   }
+}
+
+bool AutoFillProfile::IsEmpty() const {
+  FieldTypeSet types;
+  GetAvailableFieldTypes(&types);
+  return types.empty();
 }
 
 void AutoFillProfile::operator=(const AutoFillProfile& source) {
