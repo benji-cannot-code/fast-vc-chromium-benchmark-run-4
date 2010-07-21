25e63a2439eb394f825d15ef1c267b60aadb5a79
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-21 15:47:19 +0000

    Reduce static initializers by removing inline constructors and destructors from classes with instance members of type scoped_ptr_malloc<>
    Also remove #include of iostream from remaining header file.
    
    Review URL: http://codereview.chromium.org/2861065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53190 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream_parser.cc
+++ net/http/http_stream_parser.cc
@@ -37,6 +37,8 @@ HttpStreamParser::HttpStreamParser(ClientSocketHandle* connection,
   DCHECK_EQ(0, read_buffer->offset());
 }
 
+HttpStreamParser::~HttpStreamParser() {}
+
 int HttpStreamParser::SendRequest(const HttpRequestInfo* request,
                                   const std::string& headers,
                                   UploadDataStream* request_body,
