c2aff446e523c40c3069503dcc101e40ca24614e
mirandac@google.com <mirandac@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-21 17:46:03 +0000

    Revert 53198 - Some first run fixes: Add missing first run bubble back to opening page. Wait for search engines to load before displaying first run bubble, so correct search engine is displayed in first run. Because some distributions use "skip-first-run-ui", add back the "import_items" code that passes values to this code path.  Because this code path is obsolete (most import is silent), this needs to be refactored in the long term. Fix some spelling issues.
    BUG= 42612
    TEST= import with --skip-first-run-ui works 
    
    Review URL: http://codereview.chromium.org/3042010
    
    TBR=mirandac@google.com
    Review URL: http://codereview.chromium.org/3025009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53211 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/location_bar/location_bar_view.cc
+++ chrome/browser/views/location_bar/location_bar_view.cc
@@ -100,7 +100,7 @@ LocationBarView::LocationBarView(Profile* profile,
       star_view_(NULL),
       mode_(mode),
       show_focus_rect_(false),
-      bubble_type_(FirstRun::MINIMALBUBBLE) {
+      ALLOW_THIS_IN_INITIALIZER_LIST(first_run_bubble_(this)) {
   DCHECK(profile_);
   SetID(VIEW_ID_LOCATION_BAR);
   SetFocusable(true);
@@ -110,10 +110,6 @@ LocationBarView::LocationBarView(Profile* profile,
 }
 
 LocationBarView::~LocationBarView() {
-  // This is in case we're destroyed before the model loads.  This is safe
-  // if we're not a registered observer.
-  if (profile_->GetTemplateURLModel())
-    profile_->GetTemplateURLModel()->RemoveObserver(this);
 }
 
 void LocationBarView::Init() {
@@ -951,14 +947,10 @@ bool LocationBarView::CanStartDrag(View* sender,
 // LocationBarView, LocationBar implementation:
 
 void LocationBarView::ShowFirstRunBubble(FirstRun::BubbleType bubble_type) {
-  // Wait until search engines have loaded to show the first run bubble.
-  if (!profile_->GetTemplateURLModel()->loaded()) {
-    bubble_type_ = bubble_type;
-    profile_->GetTemplateURLModel()->AddObserver(this);
-    profile_->GetTemplateURLModel()->Load();
-    return;
-  }
-  ShowFirstRunBubbleInternal(bubble_type);
+  // We wait 30 milliseconds to open. It allows less flicker.
+  Task* task = first_run_bubble_.NewRunnableMethod(
+      &LocationBarView::ShowFirstRunBubbleInternal, bubble_type);
+  MessageLoop::current()->PostDelayedTask(FROM_HERE, task, 30);
 }
 
 std::wstring LocationBarView::GetInputString() const {
@@ -1044,10 +1036,3 @@ void LocationBarView::TestPageActionPressed(size_t index) {
 
   NOTREACHED();
 }
-
-void LocationBarView::OnTemplateURLModelChanged() {
-  if (profile_->GetTemplateURLModel())
-    profile_->GetTemplateURLModel()->RemoveObserver(this);
-  ShowFirstRunBubble(bubble_type_);
-}
-
