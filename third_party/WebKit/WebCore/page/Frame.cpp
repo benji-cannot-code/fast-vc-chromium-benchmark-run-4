45ed9ab1c4bd247505beddd47702d0c3c79ec2ba
yael.aharon@nokia.com <yael.aharon@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-21 18:38:12 +0000

    2010-07-21  Yael Aharon  <yael.aharon@nokia.com>
            Reviewed by Darin Adler.
    
            Crash in Notification::disconnectFrame() triggered by Frame::lifeSupportTimerFired()
            https://bugs.webkit.org/show_bug.cgi?id=42534
    
            Call NotificationsCenter::disconnectFrame() when the frame is disconnected from the page.
            Calling it from the destructor of Frame is too late and sometimes causes access violation.
            I was not able to reproduce this crash, so did not add new tests.
            This patch is based on the error reported in
            http://code.google.com/p/chromium/issues/detail?id=49323.
    
            * page/DOMWindow.cpp:
            (WebCore::DOMWindow::pageDestroyed):
            * page/DOMWindow.h:
            * page/Frame.cpp:
            (WebCore::Frame::pageDestroyed):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -1348,6 +1348,9 @@ void Frame::pageDestroyed()
     if (Frame* parent = tree()->parent())
         parent->loader()->checkLoadComplete();
 
+    if (m_domWindow)
+        m_domWindow->pageDestroyed();
+
     // FIXME: It's unclear as to why this is called more than once, but it is,
     // so page() could be NULL.
     if (page() && page()->focusController()->focusedFrame() == this)
