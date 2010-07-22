050fbe44a6e4f1241e0409b879b565fa14b0f505
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-22 17:00:12 +0000

    WebCore: Ureviewed build fix.
    Patch by Balazs Kelemen <kb@inf.u-szeged.hu> on 2010-07-22
    [Qt] Fix WebKit2 build
    
    https://bugs.webkit.org/show_bug.cgi?id=42638
    
    * WebCore.pri:
    * WebCore.pro:
    
    WebKit2: Unreviewed build fix.
    
    Patch by Balazs Kelemen <kb@inf.u-szeged.hu> on 2010-07-22
    [Qt] Fix WebKit2 build
    
    https://bugs.webkit.org/show_bug.cgi?id=42638
    
    * UIProcess/API/qt/qwkpage_p.h:
    (QWKPagePrivate::setCursor): Added empty implementation.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63898 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage_p.h
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage_p.h
@@ -43,6 +43,7 @@ public:
 
     virtual void processDidExit() {}
     virtual void processDidRevive() {}
+    virtual void setCursor(const WebCore::Cursor&) {}
     virtual void takeFocus(bool direction) {}
     virtual void toolTipChanged(const WebCore::String&, const WebCore::String&);
 
