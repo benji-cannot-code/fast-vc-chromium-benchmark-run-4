9ddc29340e0ed28fe2c4e52e7a3250abbce67f5f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-22 00:46:48 +0000

    o3d: Use char kFoo[] instead of std::wstring for static initializers.
    BUG=48097
    TEST=compiles
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>
    
    Review URL: http://codereview.chromium.org/3043004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53286 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/statsreport/const-posix.h
+++ o3d/statsreport/const-posix.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright 2009, Google Inc.
+ * Copyright 2010, Google Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,19 +32,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 // Constants for Posix stats aggregation and uploading
-#ifndef O3D_STATSREPORT_CONST_POSIX_H__
-#define O3D_STATSREPORT_CONST_POSIX_H__
+#ifndef O3D_STATSREPORT_CONST_POSIX_H_
+#define O3D_STATSREPORT_CONST_POSIX_H_
 
 #include <string>
 
 namespace stats_report {
 
-extern const std::string kCountsKeyName;
-extern const std::string kTimingsKeyName;
-extern const std::string kIntegersKeyName;
-extern const std::string kBooleansKeyName;
-extern const std::string kLastTransmissionTimeValueName;
+extern const char kCountsKeyName[];
+extern const char kTimingsKeyName[];
+extern const char kIntegersKeyName[];
+extern const char kBooleansKeyName[];
+extern const char kLastTransmissionTimeValueName[];
 
 }  // namespace stats_report
 
-#endif  // O3D_STATSREPORT_CONST_POSIX_H__
+#endif  // O3D_STATSREPORT_CONST_POSIX_H_
