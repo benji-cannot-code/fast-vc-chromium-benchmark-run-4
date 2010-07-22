1be96154d0c2c2297fa109706f12bc6ef8578544
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-22 23:17:17 +0000

    Revert "Add the actual data being read to the OnBytesRead callback."
    It broke download ui tests, but was not noticed because of the FLAKY marks.
    
    TBR=cbentzel, bmcquade@google.com
    
    Review URL: http://codereview.chromium.org/3008022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53416 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_job_tracker.cc
+++ net/url_request/url_request_job_tracker.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -52,8 +52,7 @@ void URLRequestJobTracker::OnJobRedirect(URLRequestJob* job,
 }
 
 void URLRequestJobTracker::OnBytesRead(URLRequestJob* job,
-                                       const char* buf,
                                        int byte_count) {
   FOR_EACH_OBSERVER(JobObserver, observers_,
-                    OnBytesRead(job, buf, byte_count));
+                    OnBytesRead(job, byte_count));
 }
