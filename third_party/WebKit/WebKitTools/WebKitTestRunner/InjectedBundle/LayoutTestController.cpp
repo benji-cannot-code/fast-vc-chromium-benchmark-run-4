22f20518550ae130b3b010223d3adec55824a87a
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-22 20:00:39 +0000

    2010-07-22  Darin Adler  <darin@apple.com>
            Reviewed by Sam Weinig.
    
            WebKitTestRunner needs to support layoutTestController.dumpChildFramesAsText
            https://bugs.webkit.org/show_bug.cgi?id=42325
    
            * platform/mac-wk2/Skipped: Removed a duplicate and sorted lists of tests.
            Added some tests to the pasteboard list. Removed the tests from the
            dumpChildFramesAsText list that now pass, and moved others to other lists.
    2010-07-22  Darin Adler  <darin@apple.com>
    
            Reviewed by Sam Weinig.
    
            WebKitTestRunner needs to support layoutTestController.dumpChildFramesAsText
            https://bugs.webkit.org/show_bug.cgi?id=42325
    
            * WebKitTestRunner/InjectedBundle/Bindings/LayoutTestController.idl:
            Added dumpChildFramesAsText.
    
            * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
            (WTR::dumpFrameText): Added.
            (WTR::dumpDescendantFramesText): Added.
            (WTR::InjectedBundlePage::dumpAllFramesText): Added.
            (WTR::InjectedBundlePage::dump): Changed to use the new whatToDump function
            instead of the shouldDumpAsText function, and added a case for AllFramesText.
    
            * WebKitTestRunner/InjectedBundle/InjectedBundlePage.h: Added dumpAllFramesText.
    
            * WebKitTestRunner/InjectedBundle/LayoutTestController.cpp:
            (WTR::LayoutTestController::LayoutTestController): Replaced m_dumpAsText with
            m_whatToDump.
    
            * WebKitTestRunner/InjectedBundle/LayoutTestController.h: Added
            dumpChildFramesAsText. Removed shouldDumpAsText, shouldDumpDOMAsWebArchive,
            and shouldDumpSourceAsWebArchive. Added whatToDump.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/WebKitTestRunner/InjectedBundle/LayoutTestController.cpp
+++ third_party/WebKit/WebKitTools/WebKitTestRunner/InjectedBundle/LayoutTestController.cpp
@@ -43,7 +43,7 @@ PassRefPtr<LayoutTestController> LayoutTestController::create(const std::string&
 }
 
 LayoutTestController::LayoutTestController(const std::string& testPathOrURL)
-    : m_dumpAsText(false)
+    : m_whatToDump(RenderTree)
     , m_shouldDumpAllFrameScrollPositions(false)
     , m_dumpStatusCallbacks(false)
     , m_waitToDump(false)
@@ -124,21 +124,6 @@ bool LayoutTestController::pauseAnimationAtTimeOnElementWithId(JSStringRef anima
 
     WKBundleFrameRef mainFrame = WKBundlePageGetMainFrame(InjectedBundle::shared().page()->page());
     return WKBundleFramePauseAnimationOnElementWithId(mainFrame, nameWK.get(), idWK.get(), time);
-}
-
-bool LayoutTestController::shouldDumpDOMAsWebArchive() const
-{
-    return false;
-}
-
-bool LayoutTestController::shouldDumpSourceAsWebArchive() const
-{
-    return false;
-}
-
-bool LayoutTestController::shouldDumpMainFrameScrollPosition() const
-{
-    return !shouldDumpAsText() && !shouldDumpDOMAsWebArchive() && !shouldDumpSourceAsWebArchive();
 }
 
 // Object Creation
--- third_party/WebKit/WebKitTools/WebKitTestRunner/InjectedBundle/LayoutTestController.h
+++ third_party/WebKit/WebKitTools/WebKitTestRunner/InjectedBundle/LayoutTestController.h
@@ -46,11 +46,12 @@ public:
     void makeWindowObject(JSContextRef, JSObjectRef windowObject, JSValueRef* exception);
 
     // The basics.
-    void dumpAsText() { m_dumpAsText = true; }
+    void dumpAsText() { m_whatToDump = MainFrameText; }
+    void dumpChildFramesAsText() { m_whatToDump = AllFramesText; }
     void waitUntilDone();
     void notifyDone();
 
-    // Other kinds of dumping.
+    // Other dumping.
     void dumpChildFrameScrollPositions() { m_shouldDumpAllFrameScrollPositions = true; }
     void dumpStatusCallbacks() { m_dumpStatusCallbacks = true; }
 
@@ -63,11 +64,12 @@ public:
     unsigned numberOfActiveAnimations() const;
     bool pauseAnimationAtTimeOnElementWithId(JSStringRef animationName, double time, JSStringRef elementId);
 
+    enum WhatToDump { RenderTree, MainFrameText, AllFramesText };
+    WhatToDump whatToDump() const { return m_whatToDump; }
+
     bool shouldDumpAllFrameScrollPositions() const { return m_shouldDumpAllFrameScrollPositions; }
-    bool shouldDumpAsText() const { return m_dumpAsText; }
-    bool shouldDumpDOMAsWebArchive() const;
-    bool shouldDumpMainFrameScrollPosition() const;
-    bool shouldDumpSourceAsWebArchive() const;
+    bool shouldDumpMainFrameScrollPosition() const { return m_whatToDump == RenderTree; }
+
     bool shouldDumpStatusCallbacks() const { return m_dumpStatusCallbacks; }
 
     bool waitToDump() const { return m_waitToDump; }
@@ -77,7 +79,7 @@ public:
 private:
     LayoutTestController(const std::string& testPathOrURL);
 
-    bool m_dumpAsText;
+    WhatToDump m_whatToDump;
     bool m_shouldDumpAllFrameScrollPositions;
     bool m_dumpStatusCallbacks;
     bool m_waitToDump; // True if waitUntilDone() has been called, but notifyDone() has not yet been called.
