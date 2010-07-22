4217b19b9486c7b89e209dfb12b0410140db0282
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-22 01:18:26 +0000

    o3d: Fix the mac build broken on r53286.
    BUG=None
    TEST=None
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>
    
    Review URL: http://codereview.chromium.org/3026017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53291 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/statsreport/const-mac.mm
+++ o3d/statsreport/const-mac.mm
@@ -36,10 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace stats_report {
 
-const char kTimingsKeyName = "Timings";
-const char kCountsKeyName = "Counts";
-const char kIntegersKeyName = "Integers";
-const char kBooleansKeyName = "Booleans";
+const char kTimingsKeyName[] = "Timings";
+const char kCountsKeyName[] = "Counts";
+const char kIntegersKeyName[] = "Integers";
+const char kBooleansKeyName[] = "Booleans";
 
 static NSString* FindGoogleAppDirectory(void) {
   NSString *applicationSupportFolder =
