dab75843ee5a39c174ece71f8140e9f8cb27064a
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-22 18:21:03 +0000

    Coverity: Check the return of GetTabId before using it.
    Coverity issue 7161.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/2873060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53361 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_page_actions_module.cc
+++ chrome/browser/extensions/extension_page_actions_module.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -69,9 +69,9 @@ bool PageActionFunction::SetPageActionEnabled(bool enable) {
 
   // Find the TabContents that contains this tab id.
   TabContents* contents = NULL;
-  ExtensionTabUtil::GetTabById(tab_id, profile(), include_incognito(),
-                               NULL, NULL, &contents, NULL);
-  if (!contents) {
+  bool result = ExtensionTabUtil::GetTabById(
+      tab_id, profile(), include_incognito(), NULL, NULL, &contents, NULL);
+  if (!result || !contents) {
     error_ = ExtensionErrorUtils::FormatErrorMessage(kNoTabError,
                                                      IntToString(tab_id));
     return false;
@@ -102,9 +102,9 @@ bool PageActionFunction::InitCommon(int tab_id) {
 
   // Find the TabContents that contains this tab id.
   contents_ = NULL;
-  ExtensionTabUtil::GetTabById(tab_id, profile(), include_incognito(),
-                               NULL, NULL, &contents_, NULL);
-  if (!contents_) {
+  bool result = ExtensionTabUtil::GetTabById(
+      tab_id, profile(), include_incognito(), NULL, NULL, &contents_, NULL);
+  if (!result || !contents_) {
     error_ = ExtensionErrorUtils::FormatErrorMessage(kNoTabError,
                                                      IntToString(tab_id));
     return false;
