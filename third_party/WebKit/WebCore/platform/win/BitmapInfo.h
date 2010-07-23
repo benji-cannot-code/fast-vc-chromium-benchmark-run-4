33a713b6bc92f4effe931785135fd9bc12039ef8
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-23 03:20:31 +0000

    Windows build fix.
    Commit some changes missed by the commit-queue in r63944.
    
    * platform/win/BitmapInfo.h:
    (WebCore::BitmapInfo::is16bit):
    (WebCore::BitmapInfo::is32bit):
    (WebCore::BitmapInfo::width):
    (WebCore::BitmapInfo::height):
    (WebCore::BitmapInfo::size):
    (WebCore::BitmapInfo::paddedWidth):
    (WebCore::BitmapInfo::numPixels):
    (WebCore::BitmapInfo::paddedBytesPerLine):
    (WebCore::BitmapInfo::bytesPerLine):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/BitmapInfo.h
+++ third_party/WebKit/WebCore/platform/win/BitmapInfo.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2009 Apple Inc. All Rights Reserved.
  * Copyright (C) 2009 Brent Fulgham
+ * Copyright (C) 2007-2009 Torch Mobile, Inc. All Rights Reserved.
+ * Copyright (C) 2010 Patrick Gansterer <paroga@paroga.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,17 +30,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BitmapInfo_h
 #define BitmapInfo_h
 
-#include <windows.h>
 #include "IntSize.h"
+#include <windows.h>
 
 namespace WebCore {
 
 struct BitmapInfo : public BITMAPINFO {
-    BitmapInfo ();
-    static BitmapInfo create(const IntSize&);
-    static BitmapInfo createBottomUp(const IntSize&);
-};
+    BitmapInfo();
+    static BitmapInfo create(const IntSize&, WORD bitCount = 32);
+    static BitmapInfo createBottomUp(const IntSize&, WORD bitCount = 32);
 
+    bool is16bit() const { return bmiHeader.biBitCount == 16; }
+    bool is32bit() const { return bmiHeader.biBitCount == 32; }
+    unsigned width() const { return abs(bmiHeader.biWidth); }
+    unsigned height() const { return abs(bmiHeader.biHeight); }
+    IntSize size() const { return IntSize(width(), height()); }
+    unsigned paddedWidth() const { return is16bit() ? (width() + 1) & ~0x1 : width(); }
+    unsigned numPixels() const { return paddedWidth() * height(); }
+    unsigned paddedBytesPerLine() const { return is16bit() ? paddedWidth() * 2 : width() * 4; }
+    unsigned bytesPerLine() const { return width() * bmiHeader.biBitCount / 8; }};
 } // namespace WebCore
 
 #endif // BitmapInfo_h
