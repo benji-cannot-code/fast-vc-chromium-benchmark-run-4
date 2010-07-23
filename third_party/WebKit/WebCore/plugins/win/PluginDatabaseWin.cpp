d5345176e09605f22c7f49f8839c3ba817e15dee
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-23 12:54:29 +0000

    2010-07-23  Patrick Gansterer  <paroga@paroga.com>
            Reviewed by Adam Roben.
    
            [WINCE] Buildfix for PluginDatabaseWin.cpp
            https://bugs.webkit.org/show_bug.cgi?id=41887
    
            Define KEY_ENUMERATE_SUB_KEYS only if not defined.
    
            * plugins/win/PluginDatabaseWin.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/win/PluginDatabaseWin.cpp
+++ third_party/WebKit/WebCore/plugins/win/PluginDatabaseWin.cpp
@@ -41,7 +41,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if OS(WINCE)
 // WINCE doesn't support Registry Key Access Rights. The parameter should always be 0
+#ifndef KEY_ENUMERATE_SUB_KEYS
 #define KEY_ENUMERATE_SUB_KEYS 0
+#endif
 
 DWORD SHGetValue(HKEY hkey, LPCWSTR pszSubKey, LPCWSTR pszValue, LPDWORD pdwType, LPVOID pvData, LPDWORD pcbData)
 {
