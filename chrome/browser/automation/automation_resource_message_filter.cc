7459938c3939b7c12437aaa7bcb1b0f8a30b96eb
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-23 22:42:03 +0000

    Coverity issue 8296: Uninitialized member in automation_resource_message_filter.cc
    BUG=None (Coverity issue id listed above)
    TEST=None
    
    Review URL: http://codereview.chromium.org/2825066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53540 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_resource_message_filter.cc
+++ chrome/browser/automation/automation_resource_message_filter.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,7 +28,8 @@ AutomationResourceMessageFilter::RenderViewMap
 int AutomationResourceMessageFilter::unique_request_id_ = 1;
 
 AutomationResourceMessageFilter::AutomationResourceMessageFilter()
-    : channel_(NULL) {
+    : channel_(NULL),
+      next_completion_callback_id_(0) {
   ChromeThread::PostTask(
       ChromeThread::IO, FROM_HERE,
       NewRunnableFunction(
