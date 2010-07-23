1a4ef299ce1c09217d265c43ac99899e354650fc
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-23 21:23:40 +0000

    Green Tree Tactical Force: Revert skrul's bad commits (probably a git-cl bug).
    TBR=skrul
    
    Review URL: http://codereview.chromium.org/2847072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53529 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -145,8 +145,7 @@ void ProfileSyncService::Initialize() {
     DisableForUser();  // Clean up in case of previous crash / setup abort.
 
     // Automatically start sync in Chromium OS.
-    if (bootstrap_sync_authentication_ &&
-        !profile_->GetPrefs()->GetBoolean(prefs::kSyncSuppressStart)) {
+    if (bootstrap_sync_authentication_) {
       // If the LSID is empty, we're in a CrOS UI test that is not testing sync
       // behavior, so we don't want the sync service to start.
       if (profile()->GetTokenService() &&
@@ -218,7 +217,6 @@ void ProfileSyncService::RegisterPreferences() {
     return;
   pref_service->RegisterInt64Pref(prefs::kSyncLastSyncedTime, 0);
   pref_service->RegisterBooleanPref(prefs::kSyncHasSetupCompleted, false);
-  pref_service->RegisterBooleanPref(prefs::kSyncSuppressStart, false);
 
   // If you've never synced before, or if you're using Chrome OS, all datatypes
   // are on by default.
@@ -369,7 +367,6 @@ bool ProfileSyncService::HasSyncSetupCompleted() const {
 void ProfileSyncService::SetSyncSetupCompleted() {
   PrefService* prefs = profile()->GetPrefs();
   prefs->SetBoolean(prefs::kSyncHasSetupCompleted, true);
-  prefs->SetBoolean(prefs::kSyncSuppressStart, false);
   prefs->ScheduleSavePersistentPrefs();
 }
 
@@ -452,11 +449,7 @@ void ProfileSyncService::OnBackendInitialized() {
   FOR_EACH_OBSERVER(Observer, observers_, OnStateChanged());
 
   if (bootstrap_sync_authentication_) {
-    if (profile_->GetPrefs()->GetBoolean(prefs::kSyncSuppressStart)) {
-      ShowChooseDataTypes(NULL);
-    } else {
-      SetSyncSetupCompleted();
-    }
+    SetSyncSetupCompleted();
   }
 
   if (HasSyncSetupCompleted())
@@ -503,7 +496,7 @@ void ProfileSyncService::OnStopSyncingPermanently() {
     wizard_.Step(SyncSetupWizard::SETUP_ABORTED_BY_PENDING_CLEAR);
     expect_sync_configuration_aborted_ = true;
   }
-  profile_->GetPrefs()->SetBoolean(prefs::kSyncSuppressStart, true);
+
   DisableForUser();
 }
 
@@ -549,10 +542,6 @@ SyncBackendHost::Status ProfileSyncService::QueryDetailedSyncStatus() {
         { SyncBackendHost::Status::OFFLINE_UNUSABLE };
     return status;
   }
-}
-
-bool ProfileSyncService::SetupInProgress() const {
-  return !HasSyncSetupCompleted() && WizardIsVisible();
 }
 
 std::wstring ProfileSyncService::BuildSyncStatusSummaryText(
