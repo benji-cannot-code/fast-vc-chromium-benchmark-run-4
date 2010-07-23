0570e97feba395a4c1a471f8cf9a1c12a4a33bd9
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-23 22:14:02 +0000

    2010-07-23  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            HTML5 Parser: document.write in a asynchronous script which is specified to load before page finish blows away document
            https://bugs.webkit.org/show_bug.cgi?id=42365
    
            * http/tests/misc/write-from-dom-script-expected.txt: Added.
            * http/tests/misc/write-from-dom-script.html: Added.
              - Test the scenario where a script added to the DOM asynchronously
                while the nesting level is >0.  It's supposed to have its calls to
                document.write blocked.
            * http/tests/misc/write-while-waiting.html:
              - Writes are only prevented from scripts that are added to the DOM.
                Scripts that run via setTimeout do not have their writes blocked.
    2010-07-23  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Eric Seidel.
    
            HTML5 Parser: document.write in a asynchronous script which is specified to load before page finish blows away document
            https://bugs.webkit.org/show_bug.cgi?id=42365
    
            This patch implements the changes Ian made to HTML5 to address these
            issues.  Let's try it and see how well it works.
    
            Test: http/tests/misc/write-from-dom-script.html
    
            * dom/Document.cpp:
            (WebCore::Document::Document):
            (WebCore::Document::write):
            * dom/Document.h:
            (WebCore::Document::writeDisabled):
            (WebCore::Document::setWriteDisabled):
            * dom/DocumentParser.h:
            (WebCore::DocumentParser::hasInsertionPoint):
            * dom/ScriptElement.cpp:
            (WebCore::ScriptElement::insertedIntoDocument):
            (WebCore::ScriptElementData::ScriptElementData):
            (WebCore::ScriptElementData::evaluateScript):
            * dom/ScriptElement.h:
            (WebCore::ScriptElementData::writeDisabled):
            (WebCore::ScriptElementData::setWriteDisabled):
            * html/HTMLDocumentParser.cpp:
            (WebCore::HTMLDocumentParser::hasInsertionPoint):
            (WebCore::HTMLDocumentParser::insert):
            * html/HTMLDocumentParser.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@63998 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.h
+++ third_party/WebKit/WebCore/dom/Document.h
@@ -1003,6 +1003,9 @@ public:
 
     const QualifiedName& idAttributeName() const { return m_idAttributeName; }
 
+    bool writeDisabled() const { return m_writeDisabled; }
+    void setWriteDisabled(bool flag) { m_writeDisabled = flag; }
+
 protected:
     Document(Frame*, const KURL&, bool isXHTML, bool isHTML);
 
@@ -1155,6 +1158,9 @@ private:
     bool m_frameElementsShouldIgnoreScrolling;
     bool m_containsValidityStyleRules;
     bool m_updateFocusAppearanceRestoresSelection;
+
+    // http://www.whatwg.org/specs/web-apps/current-work/#write-neutralised
+    bool m_writeDisabled;
 
     String m_title;
     String m_rawTitle;
