c9c34ebc33494e492f0a46a9d3625e8dad474f36
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-23 16:42:24 +0000

    DOMUI prefs: Add and wire up a stub overlay for adding a startup page.
    BUG=49090
    TEST=The "Add..." button on the Basics page of DOMUI options should bring up an empty overlay.
    
    Review URL: http://codereview.chromium.org/3020021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53479 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/options_ui.cc
+++ chrome/browser/dom_ui/options_ui.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/values.h"
 #include "chrome/browser/chrome_thread.h"
+#include "chrome/browser/dom_ui/add_startup_page_handler.h"
 #include "chrome/browser/dom_ui/advanced_options_handler.h"
 #include "chrome/browser/dom_ui/browser_options_handler.h"
 #include "chrome/browser/dom_ui/clear_browser_data_handler.h"
@@ -119,14 +120,17 @@ OptionsUI::OptionsUI(TabContents* contents) : DOMUI(contents) {
   AddOptionsPageUIHandler(localized_strings, new CoreOptionsHandler());
 #endif
 
-  // TODO(zelidrag): Add all other page handlers here as we implement them.
+  AddOptionsPageUIHandler(localized_strings, new AddStartupPageHandler());
+  AddOptionsPageUIHandler(localized_strings, new AdvancedOptionsHandler());
   AddOptionsPageUIHandler(localized_strings, new BrowserOptionsHandler());
+  AddOptionsPageUIHandler(localized_strings, new ClearBrowserDataHandler());
+  AddOptionsPageUIHandler(localized_strings, new ContentSettingsHandler());
+  AddOptionsPageUIHandler(localized_strings, new FontSettingsHandler());
   AddOptionsPageUIHandler(localized_strings, new PersonalOptionsHandler());
   AddOptionsPageUIHandler(localized_strings, new SyncOptionsHandler());
-  AddOptionsPageUIHandler(localized_strings, new AdvancedOptionsHandler());
-  AddOptionsPageUIHandler(localized_strings, new ContentSettingsHandler());
 #if defined(OS_CHROMEOS)
-  AddOptionsPageUIHandler(localized_strings, new SystemOptionsHandler());
+  AddOptionsPageUIHandler(localized_strings,
+                          new chromeos::AccountsOptionsHandler());
   AddOptionsPageUIHandler(localized_strings, new LabsHandler());
   AddOptionsPageUIHandler(localized_strings,
                           new LanguageChewingOptionsHandler());
@@ -135,12 +139,8 @@ OptionsUI::OptionsUI(TabContents* contents) : DOMUI(contents) {
   AddOptionsPageUIHandler(localized_strings, new LanguageOptionsHandler());
   AddOptionsPageUIHandler(localized_strings,
                           new LanguagePinyinOptionsHandler());
-  AddOptionsPageUIHandler(localized_strings,
-                          new chromeos::AccountsOptionsHandler());
+  AddOptionsPageUIHandler(localized_strings, new SystemOptionsHandler());
 #endif
-  AddOptionsPageUIHandler(localized_strings, new ContentSettingsHandler());
-  AddOptionsPageUIHandler(localized_strings, new ClearBrowserDataHandler());
-  AddOptionsPageUIHandler(localized_strings, new FontSettingsHandler());
 
   // |localized_strings| ownership is taken over by this constructor.
   OptionsUIHTMLSource* html_source =
---
+++ chrome/browser/resources/options/add_startup_page_overlay.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * AddStartupPageOverlay class
+ * Encapsulated handling of the 'Add Page' overlay page.
+ * @class
+ */
+function AddStartupPageOverlay() {
+  OptionsPage.call(this, 'addStartupPageOverlay',
+                   templateData.addStartupPageTitle,
+                   'addStartupPageOverlay');
+}
+
+cr.addSingletonGetter(AddStartupPageOverlay);
+
+AddStartupPageOverlay.prototype = {
+  __proto__: OptionsPage.prototype,
+
+  /**
+   * Initializes the page.
+   */
+  initializePage: function() {
+    OptionsPage.prototype.initializePage.call(this);
+
+    $('addStartupPageCancelButton').onclick = function(e) {
+      OptionsPage.clearOverlays();
+    };
+  }
+};
