d3c7b52feb24f80da5b6d1fcb935b31e6479d052
skrul@chromium.org <skrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-23 21:06:42 +0000

    fix header guards
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53526 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/fake/fake_mount_library.h
+++ chrome/browser/chromeos/cros/fake/fake_mount_library.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_CROS_FAKE_MOUNT_LIBRARY_H_
-#define CHROME_BROWSER_CHROMEOS_CROS_FAKE_MOUNT_LIBRARY_H_
+#ifndef CHROME_BROWSER_CHROMEOS_CROS_FAKE_FAKE_MOUNT_LIBRARY_H_
+#define CHROME_BROWSER_CHROMEOS_CROS_FAKE_FAKE_MOUNT_LIBRARY_H_
 
 #include "chrome/browser/chromeos/cros/mount_library.h"
 #include "third_party/cros/chromeos_mount.h"
@@ -30,4 +30,4 @@ class FakeMountLibrary : public MountLibrary {
 
 }  // namespace chromeos
 
-#endif  // CHROME_BROWSER_CHROMEOS_CROS_FAKE_MOUNT_LIBRARY_H_
+#endif  // CHROME_BROWSER_CHROMEOS_CROS_FAKE_FAKE_MOUNT_LIBRARY_H_
