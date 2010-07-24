c07d98b3e5aca6c421bb00486a95fece60818a6c
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-24 09:19:09 +0000

    Coverity issue 2291: Uninitialized member in automation_provider_observers.cc
    BUG=None (Coverity issue id listed above)
    TEST=None
    
    Review URL: http://codereview.chromium.org/3033023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53580 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider_observers.cc
+++ chrome/browser/automation/automation_provider_observers.cc
@@ -759,6 +759,7 @@ void ExecuteBrowserCommandObserver::Observe(
 ExecuteBrowserCommandObserver::ExecuteBrowserCommandObserver(
     AutomationProvider* automation, IPC::Message* reply_message)
     : automation_(automation),
+      notification_type_(NotificationType::ALL),
       reply_message_(reply_message) {
 }
 
