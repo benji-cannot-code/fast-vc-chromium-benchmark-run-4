9c581a32a819aa6e0abed6706138221ba157bbce
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-24 13:12:43 +0000

    Coverity 8629, 3808: Uninitialized member in StringTokenizer.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/3036010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53590 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_tokenizer.h
+++ base/string_tokenizer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -161,6 +161,7 @@ class StringTokenizerT {
     end_ = string_end;
     delims_ = delims;
     options_ = 0;
+    token_is_delim_ = false;
   }
 
   // Implementation of GetNext() for when we have no quote characters. We have
@@ -175,7 +176,7 @@ class StringTokenizerT {
       ++token_end_;
       if (delims_.find(*token_begin_) == str::npos)
         break;
-      // else skip over delim
+      // else skip over delimiter.
     }
     while (token_end_ != end_ && delims_.find(*token_end_) == str::npos)
       ++token_end_;
@@ -197,7 +198,7 @@ class StringTokenizerT {
         token_is_delim_ = true;
         return true;
       }
-      // else skip over delim
+      // else skip over delimiter.
     }
     while (token_end_ != end_ && AdvanceOne(&state, *token_end_))
       ++token_end_;
