80d1148eba8bd4660d83bfadf7d63437222f3b67
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-24 17:33:12 +0000

    2010-07-24  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Save a per-character branch in the HTML parser
            https://bugs.webkit.org/show_bug.cgi?id=42921
    
            The parsing benchmark claims this is a wash, but it makes me feel
            better.
    
            * html/HTMLToken.h:
            (WebCore::HTMLToken::clear):
            (WebCore::HTMLToken::beginStartTag):
            (WebCore::HTMLToken::beginEndTag):
            (WebCore::HTMLToken::ensureIsCharacterToken):
            (WebCore::HTMLToken::beginComment):
            (WebCore::HTMLToken::beginDOCTYPE):
            * html/HTMLTokenizer.cpp:
            (WebCore::HTMLTokenizer::bufferCharacter):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64010 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
+++ third_party/WebKit/WebCore/html/HTMLTokenizer.cpp
@@ -1609,10 +1609,7 @@ inline bool HTMLTokenizer::isAppropriateEndTag()
 inline void HTMLTokenizer::bufferCharacter(UChar character)
 {
     ASSERT(character != InputStreamPreprocessor::endOfFileMarker);
-    if (m_token->type() != HTMLToken::Character) {
-        m_token->beginCharacter(character);
-        return;
-    }
+    m_token->ensureIsCharacterToken();
     m_token->appendToCharacter(character);
 }
 
