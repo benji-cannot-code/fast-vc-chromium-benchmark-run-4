15c30478e2cec6baa0f3f3192d488bc77947c431
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-24 03:35:26 +0000

    2010-07-23  Kavita Kanetkar  <kkanetkar@chromium.org>
            Reviewed by Joseph Pecoraro.
    
            Implement remaining Inspector support for chrome's appcache
            https://bugs.webkit.org/show_bug.cgi?id=42426
    
            * inspector/InspectorApplicationCacheAgent.cpp:
            (WebCore::InspectorApplicationCacheAgent::getApplicationCaches):
            (WebCore::InspectorApplicationCacheAgent::buildObjectForApplicationCache):
            (WebCore::InspectorApplicationCacheAgent::buildArrayForApplicationCacheResources):
            (WebCore::InspectorApplicationCacheAgent::buildObjectForApplicationCacheResource):
            * inspector/InspectorApplicationCacheAgent.h:
            (WebCore::InspectorController::connectFrontend):
            * loader/appcache/ApplicationCacheHost.cpp:
            (WebCore::ApplicationCacheHost::fillResourceList):
            (WebCore::ApplicationCacheHost::applicationCacheInfo):
            * loader/appcache/ApplicationCacheHost.h: Changed creation + update times to double and moved appcache info here from InspectorApplicationCacheAgent.h
            (WebCore::ApplicationCacheHost::CacheInfo::CacheInfo):
            (WebCore::ApplicationCacheHost::ResourceInfo::ResourceInfo):
    2010-07-23  Kavita Kanetkar  <kkanetkar@chromium.org>
    
            Reviewed by Joseph Pecoraro.
    
            Implement remaining Inspector support for chrome's appcache
            https://bugs.webkit.org/show_bug.cgi?id=42426
    
            * src/ApplicationCacheHost.cpp: Plugging in chrome's appcache info.
            (WebCore::ApplicationCacheHost::applicationCacheInfo):
            (WebCore::ApplicationCacheHost::fillResourceList):
            * src/js/InspectorControllerImpl.js:
            (devtools.InspectorBackendImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64006 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/ApplicationCacheHost.cpp
+++ third_party/WebKit/WebKit/chromium/src/ApplicationCacheHost.cpp
@@ -38,11 +38,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DocumentLoader.h"
 #include "DOMApplicationCache.h"
 #include "Frame.h"
+#include "InspectorApplicationCacheAgent.h"
+#include "InspectorController.h"
+#include "Page.h"
 #include "ProgressEvent.h"
 #include "Settings.h"
 #include "WebURL.h"
 #include "WebURLError.h"
 #include "WebURLResponse.h"
+#include "WebVector.h"
 #include "WrappedResourceRequest.h"
 #include "WrappedResourceResponse.h"
 
@@ -206,6 +210,32 @@ void ApplicationCacheHost::notifyDOMApplicationCache(EventID id, int total, int 
     }
     dispatchDOMEvent(id, total, done);
 }
+
+#if ENABLE(INSPECTOR)
+ApplicationCacheHost::CacheInfo ApplicationCacheHost::applicationCacheInfo()
+{
+    if (!m_internal)
+        return CacheInfo(KURL(), 0, 0, 0);
+
+    WebKit::WebApplicationCacheHost::CacheInfo webInfo;
+    m_internal->m_outerHost->getAssociatedCacheInfo(&webInfo);
+    return CacheInfo(webInfo.manifestURL, webInfo.creationTime, webInfo.updateTime, webInfo.totalSize);
+}
+
+void ApplicationCacheHost::fillResourceList(ResourceInfoList* resources)
+{
+    if (!m_internal)
+        return;
+
+    WebKit::WebVector<WebKit::WebApplicationCacheHost::ResourceInfo> webResources;
+    m_internal->m_outerHost->getResourceList(&webResources);
+    for (int i = 0; i < webResources.size(); ++i) {
+        resources->append(ResourceInfo(
+            webResources[i].url, webResources[i].isMaster, webResources[i].isManifest, webResources[i].isFallback,
+            webResources[i].isForeign, webResources[i].isExplicit, webResources[i].size));
+    }
+}
+#endif
 
 void ApplicationCacheHost::stopDeferringEvents()
 {
--- third_party/WebKit/WebKit/chromium/src/js/InspectorControllerImpl.js
+++ third_party/WebKit/WebKit/chromium/src/js/InspectorControllerImpl.js
@@ -57,6 +57,7 @@ devtools.InspectorBackendImpl = function()
     this.installInspectorControllerDelegate_("enableTimeline");
     this.installInspectorControllerDelegate_("getChildNodes");
     this.installInspectorControllerDelegate_("getCookies");
+    this.installInspectorControllerDelegate_("getApplicationCaches");
     this.installInspectorControllerDelegate_("getDatabaseTableNames");
     this.installInspectorControllerDelegate_("getDOMStorageEntries");
     this.installInspectorControllerDelegate_("getEventListenersForNode");
