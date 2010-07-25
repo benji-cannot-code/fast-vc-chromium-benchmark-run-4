4607cb3ab962f03cb36bf5501ef8f839111497e6
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-25 01:03:15 +0000

    2010-07-24  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Atomize strings used by the HTML5 tree builder
            https://bugs.webkit.org/show_bug.cgi?id=42875
    
            This is about a 1% improvement to the parsing benchmark.
    
            * html/HTMLTagNames.in:
            * html/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::processStartTagForInBody):
            (WebCore::HTMLTreeBuilder::processStartTag):
            (WebCore::HTMLTreeBuilder::processEndTagForInBody):
            (WebCore::HTMLTreeBuilder::processStartTagForInHead):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64018 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
@@ -120,7 +120,7 @@ bool isSpecialTag(const AtomicString& tagName)
         || tagName == asideTag
         || tagName == baseTag
         || tagName == basefontTag
-        || tagName == "bgsound"
+        || tagName == bgsoundTag
         || tagName == blockquoteTag
         || tagName == bodyTag
         || tagName == brTag
@@ -128,16 +128,16 @@ bool isSpecialTag(const AtomicString& tagName)
         || tagName == centerTag
         || tagName == colTag
         || tagName == colgroupTag
-        || tagName == "command"
+        || tagName == commandTag
         || tagName == ddTag
-        || tagName == "details"
+        || tagName == detailsTag
         || tagName == dirTag
         || tagName == divTag
         || tagName == dlTag
         || tagName == dtTag
         || tagName == embedTag
         || tagName == fieldsetTag
-        || tagName == "figure"
+        || tagName == figureTag
         || tagName == footerTag
         || tagName == formTag
         || tagName == frameTag
@@ -771,8 +771,8 @@ void HTMLTreeBuilder::processStartTagForInBody(AtomicHTMLToken& token)
     }
     if (token.name() == baseTag
         || token.name() == basefontTag
-        || token.name() == "bgsound"
-        || token.name() == "command"
+        || token.name() == bgsoundTag
+        || token.name() == commandTag
         || token.name() == linkTag
         || token.name() == metaTag
         || token.name() == noframesTag
@@ -807,13 +807,13 @@ void HTMLTreeBuilder::processStartTagForInBody(AtomicHTMLToken& token)
         || token.name() == asideTag
         || token.name() == blockquoteTag
         || token.name() == centerTag
-        || token.name() == "details"
+        || token.name() == detailsTag
         || token.name() == dirTag
         || token.name() == divTag
         || token.name() == dlTag
         || token.name() == fieldsetTag
-        || token.name() == "figcaption"
-        || token.name() == "figure"
+        || token.name() == figcaptionTag
+        || token.name() == figureTag
         || token.name() == footerTag
         || token.name() == headerTag
         || token.name() == hgroupTag
@@ -822,7 +822,7 @@ void HTMLTreeBuilder::processStartTagForInBody(AtomicHTMLToken& token)
         || token.name() == olTag
         || token.name() == pTag
         || token.name() == sectionTag
-        || token.name() == "summary"
+        || token.name() == summaryTag
         || token.name() == ulTag) {
         processFakePEndTagIfPInButtonScope();
         m_tree.insertHTMLElement(token);
@@ -944,7 +944,7 @@ void HTMLTreeBuilder::processStartTagForInBody(AtomicHTMLToken& token)
     }
     if (token.name() == paramTag
         || token.name() == sourceTag
-        || token.name() == "track") {
+        || token.name() == trackTag) {
         m_tree.insertSelfClosingHTMLElement(token);
         return;
     }
@@ -1234,7 +1234,7 @@ void HTMLTreeBuilder::processStartTag(AtomicHTMLToken& token)
         }
         if (token.name() == baseTag
             || token.name() == basefontTag
-            || token.name() == "bgsound"
+            || token.name() == bgsoundTag
             || token.name() == linkTag
             || token.name() == metaTag
             || token.name() == noframesTag
@@ -1381,7 +1381,7 @@ void HTMLTreeBuilder::processStartTag(AtomicHTMLToken& token)
             return;
         }
         if (token.name() == basefontTag
-            || token.name() == "bgsound"
+            || token.name() == bgsoundTag
             || token.name() == linkTag
             || token.name() == metaTag
             || token.name() == noframesTag
@@ -1941,12 +1941,12 @@ void HTMLTreeBuilder::processEndTagForInBody(AtomicHTMLToken& token)
         || token.name() == blockquoteTag
         || token.name() == buttonTag
         || token.name() == centerTag
-        || token.name() == "details"
+        || token.name() == detailsTag
         || token.name() == dirTag
         || token.name() == divTag
         || token.name() == dlTag
         || token.name() == fieldsetTag
-        || token.name() == "figure"
+        || token.name() == figureTag
         || token.name() == footerTag
         || token.name() == headerTag
         || token.name() == hgroupTag
@@ -2737,11 +2737,10 @@ bool HTMLTreeBuilder::processStartTagForInHead(AtomicHTMLToken& token)
         m_tree.insertHTMLHtmlStartTagInBody(token);
         return true;
     }
-    // FIXME: Atomize "command".
     if (token.name() == baseTag
         || token.name() == basefontTag
-        || token.name() == "bgsound"
-        || token.name() == "command"
+        || token.name() == bgsoundTag
+        || token.name() == commandTag
         || token.name() == linkTag
         || token.name() == metaTag) {
         m_tree.insertSelfClosingHTMLElement(token);
