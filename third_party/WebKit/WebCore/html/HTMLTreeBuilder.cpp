025a745f319e60a05af1a2d988c03dc2e98535f2
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-25 21:51:04 +0000

    2010-07-25  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Implement adjustMathMLAttributes
            https://bugs.webkit.org/show_bug.cgi?id=42952
    
            Yay test progression.
    
            * html5lib/runner-expected-html5.txt:
    2010-07-25  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Eric Seidel.
    
            Implement adjustMathMLAttributes
            https://bugs.webkit.org/show_bug.cgi?id=42952
    
            This is kind of mechanical overkill for adjusting one attribute name,
            but it's nice to use the same machinery as SVG.
    
            * html/HTMLTreeBuilder.cpp:
            * mathml/mathattrs.in:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64024 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
@@ -681,14 +681,15 @@ void adjustSVGTagNameCase(AtomicHTMLToken& token)
     token.setName(casedName.localName());
 }
 
-void adjustSVGAttributes(AtomicHTMLToken& token)
+template<QualifiedName** getAttrs(size_t* length)>
+void adjustAttributes(AtomicHTMLToken& token)
 {
     static PrefixedNameToQualifiedNameMap* caseMap = 0;
     if (!caseMap) {
         caseMap = new PrefixedNameToQualifiedNameMap;
         size_t length = 0;
-        QualifiedName** svgAttrs = SVGNames::getSVGAttrs(&length);
-        mapLoweredLocalNameToName(caseMap, svgAttrs, length);
+        QualifiedName** attrs = getAttrs(&length);
+        mapLoweredLocalNameToName(caseMap, attrs, length);
     }
 
     NamedNodeMap* attributes = token.attributes();
@@ -703,9 +704,14 @@ void adjustSVGAttributes(AtomicHTMLToken& token)
     }
 }
 
-void adjustMathMLAttributes(AtomicHTMLToken&)
+void adjustSVGAttributes(AtomicHTMLToken& token)
 {
-    notImplemented();
+    adjustAttributes<SVGNames::getSVGAttrs>(token);
+}
+
+void adjustMathMLAttributes(AtomicHTMLToken& token)
+{
+    adjustAttributes<MathMLNames::getMathMLAttrs>(token);
 }
 
 void addNamesWithPrefix(PrefixedNameToQualifiedNameMap* map, const AtomicString& prefix, QualifiedName** names, size_t length)
