88d0867747128480220b1b9a328a4324a8920b97
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-26 19:30:38 +0000

    2010-07-26  Patrick Gansterer  <paroga@paroga.com>
            Reviewed by Kent Tamura.
    
            [WINCE] Port ClipboardUtilities to WinCE
            https://bugs.webkit.org/show_bug.cgi?id=42929
    
            Make ClipboardUtilitiesWin more portable and remove the global namespace prefix
            from GlobalLock/GlobalUnlock, since they are only macros on WinCE.
    
            * platform/win/ClipboardUtilitiesWin.cpp:
            (WebCore::urlFromPath):
            (WebCore::getWebLocData):
            (WebCore::createGlobalData):
            (WebCore::getURL):
            (WebCore::getPlainText):
            (WebCore::fragmentFromHTML):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/ClipboardUtilitiesWin.cpp
+++ third_party/WebKit/WebCore/platform/win/ClipboardUtilitiesWin.cpp
@@ -32,23 +32,47 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PlatformString.h"
 #include "TextEncoding.h"
 #include "markup.h"
-#include <CoreFoundation/CoreFoundation.h>
 #include <shlwapi.h>
 #include <wininet.h> // for INTERNET_MAX_URL_LENGTH
-#include <wtf/RetainPtr.h>
 #include <wtf/text/CString.h>
+
+#if PLATFORM(CF)
+#include <CoreFoundation/CoreFoundation.h>
+#include <wtf/RetainPtr.h>
+#endif
 
 namespace WebCore {
 
+#if PLATFORM(CF)
 FORMATETC* cfHDropFormat()
 {
     static FORMATETC urlFormat = {CF_HDROP, 0, DVASPECT_CONTENT, -1, TYMED_HGLOBAL};
     return &urlFormat;
 }
 
+static bool urlFromPath(CFStringRef path, String& url)
+{
+    if (!path)
+        return false;
+
+    RetainPtr<CFURLRef> cfURL(AdoptCF, CFURLCreateWithFileSystemPath(0, path, kCFURLWindowsPathStyle, false));
+    if (!cfURL)
+        return false;
+
+    url = CFURLGetString(cfURL.get());
+
+    // Work around <rdar://problem/6708300>, where CFURLCreateWithFileSystemPath makes URLs with "localhost".
+    if (url.startsWith("file://localhost/"))
+        url.remove(7, 9);
+
+    return true;
+}
+#endif
+
 static bool getWebLocData(IDataObject* dataObject, String& url, String* title) 
 {
     bool succeeded = false;
+#if PLATFORM(CF)
     WCHAR filename[MAX_PATH];
     WCHAR urlBuffer[INTERNET_MAX_URL_LENGTH];
 
@@ -56,8 +80,8 @@ static bool getWebLocData(IDataObject* dataObject, String& url, String* title)
     if (FAILED(dataObject->GetData(cfHDropFormat(), &medium)))
         return false;
 
-    HDROP hdrop = (HDROP)GlobalLock(medium.hGlobal);
-   
+    HDROP hdrop = static_cast<HDROP>(GlobalLock(medium.hGlobal));
+
     if (!hdrop)
         return false;
 
@@ -82,6 +106,7 @@ exit:
     // Free up memory.
     DragFinish(hdrop);
     GlobalUnlock(medium.hGlobal);
+#endif
     return succeeded;
 }
 
@@ -114,9 +139,9 @@ HGLOBAL createGlobalData(const KURL& url, const String& title)
     HGLOBAL cbData = ::GlobalAlloc(GPTR, size * sizeof(UChar));
 
     if (cbData) {
-        PWSTR buffer = (PWSTR)::GlobalLock(cbData);
-        swprintf_s(buffer, size, L"%s\n%s", mutableURL.charactersWithNullTermination(), mutableTitle.charactersWithNullTermination());
-        ::GlobalUnlock(cbData);
+        PWSTR buffer = static_cast<PWSTR>(GlobalLock(cbData));
+        _snwprintf(buffer, size, L"%s\n%s", mutableURL.charactersWithNullTermination(), mutableTitle.charactersWithNullTermination());
+        GlobalUnlock(cbData);
     }
     return cbData;
 }
@@ -126,10 +151,10 @@ HGLOBAL createGlobalData(const String& str)
     HGLOBAL globalData = ::GlobalAlloc(GPTR, (str.length() + 1) * sizeof(UChar));
     if (!globalData)
         return 0;
-    UChar* buffer = static_cast<UChar*>(::GlobalLock(globalData));
+    UChar* buffer = static_cast<UChar*>(GlobalLock(globalData));
     memcpy(buffer, str.characters(), str.length() * sizeof(UChar));
     buffer[str.length()] = 0;
-    ::GlobalUnlock(globalData);
+    GlobalUnlock(globalData);
     return globalData;
 }
 
@@ -138,10 +163,10 @@ HGLOBAL createGlobalData(const Vector<char>& vector)
     HGLOBAL globalData = ::GlobalAlloc(GPTR, vector.size() + 1);
     if (!globalData)
         return 0;
-    char* buffer = static_cast<char*>(::GlobalLock(globalData));
+    char* buffer = static_cast<char*>(GlobalLock(globalData));
     memcpy(buffer, vector.data(), vector.size());
     buffer[vector.size()] = 0;
-    ::GlobalUnlock(globalData);
+    GlobalUnlock(globalData);
     return globalData;
 }
 
@@ -272,24 +297,6 @@ FORMATETC* smartPasteFormat()
     return &htmlFormat;
 }
 
-static bool urlFromPath(CFStringRef path, String& url)
-{
-    if (!path)
-        return false;
-
-    RetainPtr<CFURLRef> cfURL(AdoptCF, CFURLCreateWithFileSystemPath(0, path, kCFURLWindowsPathStyle, false));
-    if (!cfURL)
-        return false;
-
-    url = CFURLGetString(cfURL.get());
-
-    // Work around <rdar://problem/6708300>, where CFURLCreateWithFileSystemPath makes URLs with "localhost".
-    if (url.startsWith("file://localhost/"))
-        url.remove(7, 9);
-
-    return true;
-}
-
 String getURL(IDataObject* dataObject, DragData::FilenameConversionPolicy filenamePolicy, bool& success, String* title)
 {
     STGMEDIUM store;
@@ -299,22 +306,24 @@ String getURL(IDataObject* dataObject, DragData::FilenameConversionPolicy filena
         success = true;
     else if (SUCCEEDED(dataObject->GetData(urlWFormat(), &store))) {
         // URL using Unicode
-        UChar* data = (UChar*)GlobalLock(store.hGlobal);
+        UChar* data = static_cast<UChar*>(GlobalLock(store.hGlobal));
         url = extractURL(String(data), title);
-        GlobalUnlock(store.hGlobal);      
+        GlobalUnlock(store.hGlobal);
         ReleaseStgMedium(&store);
         success = true;
     } else if (SUCCEEDED(dataObject->GetData(urlFormat(), &store))) {
         // URL using ASCII
-        char* data = (char*)GlobalLock(store.hGlobal);
+        char* data = static_cast<char*>(GlobalLock(store.hGlobal));
         url = extractURL(String(data), title);
-        GlobalUnlock(store.hGlobal);      
+        GlobalUnlock(store.hGlobal);
         ReleaseStgMedium(&store);
         success = true;
-    } else if (filenamePolicy == DragData::ConvertFilenames) {
+    }
+#if PLATFORM(CF)
+    else if (filenamePolicy == DragData::ConvertFilenames) {
         if (SUCCEEDED(dataObject->GetData(filenameWFormat(), &store))) {
             // file using unicode
-            wchar_t* data = (wchar_t*)GlobalLock(store.hGlobal);
+            wchar_t* data = static_cast<wchar_t*>(GlobalLock(store.hGlobal));
             if (data && data[0] && (PathFileExists(data) || PathIsUNC(data))) {
                 RetainPtr<CFStringRef> pathAsCFString(AdoptCF, CFStringCreateWithCharacters(kCFAllocatorDefault, (const UniChar*)data, wcslen(data)));
                 if (urlFromPath(pathAsCFString.get(), url)) {
@@ -327,7 +336,7 @@ String getURL(IDataObject* dataObject, DragData::FilenameConversionPolicy filena
             ReleaseStgMedium(&store);
         } else if (SUCCEEDED(dataObject->GetData(filenameFormat(), &store))) {
             // filename using ascii
-            char* data = (char*)GlobalLock(store.hGlobal);
+            char* data = static_cast<char*>(GlobalLock(store.hGlobal));
             if (data && data[0] && (PathFileExistsA(data) || PathIsUNCA(data))) {
                 RetainPtr<CFStringRef> pathAsCFString(AdoptCF, CFStringCreateWithCString(kCFAllocatorDefault, data, kCFStringEncodingASCII));
                 if (urlFromPath(pathAsCFString.get(), url)) {
@@ -340,6 +349,7 @@ String getURL(IDataObject* dataObject, DragData::FilenameConversionPolicy filena
             ReleaseStgMedium(&store);
         }
     }
+#endif
     return url;
 }
 
@@ -350,14 +360,14 @@ String getPlainText(IDataObject* dataObject, bool& success)
     success = false;
     if (SUCCEEDED(dataObject->GetData(plainTextWFormat(), &store))) {
         // Unicode text
-        UChar* data = (UChar*)GlobalLock(store.hGlobal);
+        UChar* data = static_cast<UChar*>(GlobalLock(store.hGlobal));
         text = String(data);
         GlobalUnlock(store.hGlobal);
         ReleaseStgMedium(&store);
         success = true;
     } else if (SUCCEEDED(dataObject->GetData(plainTextFormat(), &store))) {
         // ASCII text
-        char* data = (char*)GlobalLock(store.hGlobal);
+        char* data = static_cast<char*>(GlobalLock(store.hGlobal));
         text = String(data);
         GlobalUnlock(store.hGlobal);
         ReleaseStgMedium(&store);
@@ -436,9 +446,9 @@ PassRefPtr<DocumentFragment> fragmentFromHTML(Document* doc, IDataObject* data)
     String srcURL;
     if (SUCCEEDED(data->GetData(htmlFormat(), &store))) {
         // MS HTML Format parsing
-        char* data = (char*)GlobalLock(store.hGlobal);
+        char* data = static_cast<char*>(GlobalLock(store.hGlobal));
         SIZE_T dataSize = ::GlobalSize(store.hGlobal);
-        String cfhtml(UTF8Encoding().decode(data, dataSize));         
+        String cfhtml(UTF8Encoding().decode(data, dataSize));
         GlobalUnlock(store.hGlobal);
         ReleaseStgMedium(&store); 
         if (PassRefPtr<DocumentFragment> fragment = fragmentFromCFHTML(doc, cfhtml))
