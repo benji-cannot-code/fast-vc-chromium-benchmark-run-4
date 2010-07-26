f12735a729fbe09972d7f6c1b9c2cc2bf21f9e99
erikchen@google.com <erikchen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-26 22:49:52 +0000

    TBR:
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53714 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_http_stream_unittest.cc
+++ net/spdy/spdy_http_stream_unittest.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 class SpdyHttpStreamTest : public testing::Test {
- public:
-  OrderedSocketData* data() { return data_; }
  protected:
   SpdyHttpStreamTest() {}
 
@@ -75,9 +73,7 @@ TEST_F(SpdyHttpStreamTest, SendRequest) {
             http_stream->SendRequest(&response, &callback));
   MessageLoop::current()->RunAllPending();
   EXPECT_TRUE(http_session_->spdy_session_pool()->HasSession(host_port_pair));
-  http_session_->spdy_session_pool()->CloseAllSessions();
-  EXPECT_TRUE(!data()->at_read_eof());
-  EXPECT_TRUE(data()->at_write_eof());
+  http_session_->spdy_session_pool()->Remove(session_);
 }
 
 // Test case for bug: http://code.google.com/p/chromium/issues/detail?id=50058
@@ -85,12 +81,16 @@ TEST_F(SpdyHttpStreamTest, SpdyURLTest) {
   EnableCompression(false);
   SpdySession::SetSSLMode(false);
 
+  scoped_ptr<spdy::SpdyFrame> req(ConstructSpdyGet(NULL, 0, false, 1, LOWEST));
+  MockWrite writes[] = {
+    CreateMockWrite(*req.get(), 1),
+  };
   MockRead reads[] = {
     MockRead(false, 0, 2),  // EOF
   };
 
   HostPortPair host_port_pair("www.google.com", 80);
-  EXPECT_EQ(OK, InitSession(reads, arraysize(reads), NULL, 0,
+  EXPECT_EQ(OK, InitSession(reads, arraysize(reads), writes, arraysize(writes),
       host_port_pair));
 
   HttpRequestInfo request;
@@ -114,8 +114,7 @@ TEST_F(SpdyHttpStreamTest, SpdyURLTest) {
 
   MessageLoop::current()->RunAllPending();
   EXPECT_TRUE(http_session_->spdy_session_pool()->HasSession(host_port_pair));
-  http_session_->spdy_session_pool()->CloseAllSessions();
-  EXPECT_TRUE(!data()->at_read_eof());
+  http_session_->spdy_session_pool()->Remove(session_);
 }
 
 // TODO(willchan): Write a longer test for SpdyStream that exercises all
