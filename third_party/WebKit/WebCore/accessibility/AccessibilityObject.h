2e84f796dbca8ca2623c3fffc6914cbc32312311
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-26 22:49:18 +0000

    AX: remove intValue() hasIntValue() from AccessibilityObject https://bugs.webkit.org/show_bug.cgi?id=43006
    Reviewed by Darin Adler.
    
    These methods are very Mac centric. The mac centric code should be moved into the Mac wrapper.
    
    No new tests, existing tests cover changes to logic.
    
    * accessibility/AccessibilityObject.cpp:
    (WebCore::AccessibilityObject::checkboxOrRadioValue):
    * accessibility/AccessibilityObject.h:
    (WebCore::):
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::selectedRadioButton):
    (WebCore::AccessibilityRenderObject::selectedTabItem):
    (WebCore::AccessibilityRenderObject::checkboxOrRadioValue):
    * accessibility/AccessibilityRenderObject.h:
    * accessibility/mac/AccessibilityObjectWrapper.mm:
    (-[AccessibilityObjectWrapper accessibilityAttributeValue:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64084 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AccessibilityObject.h
+++ third_party/WebKit/WebCore/accessibility/AccessibilityObject.h
@@ -210,6 +210,11 @@ enum AccessibilityObjectInclusion {
     IgnoreObject,
     DefaultBehavior,
 };
+    
+enum AccessibilityButtonState {
+    ButtonStateOff = 0,
+    ButtonStateOn, 
+};
 
 struct VisiblePositionRange {
 
@@ -328,9 +333,6 @@ public:
     virtual bool canSetSelectedChildrenAttribute() const { return false; }
     virtual bool canSetExpandedAttribute() const { return false; }
     
-    bool hasIntValue() const;
-    virtual int intValue() const;
-    
     // A programmatic way to set a name on an AccessibleObject.
     virtual void setAccessibleName(String&) { }
     
@@ -339,6 +341,7 @@ public:
     virtual bool accessibilityIsIgnored() const  { return true; }
 
     virtual int headingLevel() const { return 0; }
+    virtual AccessibilityButtonState checkboxOrRadioValue() const;
     virtual String valueDescription() const { return String(); }
     virtual float valueForRange() const { return 0.0f; }
     virtual float maxValueForRange() const { return 0.0f; }
