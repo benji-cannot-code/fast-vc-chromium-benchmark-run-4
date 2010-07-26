362adf6ee0bb2e8e2c24f6e2f4f64d1d1d9128d5
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-26 22:44:07 +0000

    Significantly speed up sync integration tests.
    The sync integration tests currently use the method AwaitSyncCycleCompletion(), which ends up waiting for a sync event before it signals the end of a sync cycle. However, if it is called after the sync cycle is already complete, it ends up waiting for several seconds for a random unrelated sync event before it returns.
    
    This checkin fixes this unnecessary delay by checking to see if the client has any unsynced items before it waits, thereby significantly speeding up the sync integration tests.
    
    In addition to this, a new method called AwaitQuiescence() is now available to test cases so they can wait for racy updates initiated by multiple clients to propagate across the system.
    
    BUG=49998
    TEST=sync_integration_tests
    
    Review URL: http://codereview.chromium.org/3041018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53712 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/profile_sync_service_test_harness.cc
+++ chrome/test/live_sync/profile_sync_service_test_harness.cc
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/ui_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using browser_sync::sessions::SyncSessionSnapshot;
-
 // The default value for min_timestamp_needed_ when we're not in the
 // WAITING_FOR_UPDATES state.
 static const int kMinTimestampNeededNone = -1;
@@ -134,8 +132,7 @@ bool ProfileSyncServiceTestHarness::RunStateChangeMachine() {
       break;
     }
     case WAITING_FOR_SYNC_TO_FINISH: {
-      const SyncSessionSnapshot* snap =
-          service_->backend()->GetLastSessionSnapshot();
+      const SyncSessionSnapshot* snap = GetLastSessionSnapshot();
       DCHECK(snap) << "Should have been at least one sync session by now";
       // TODO(rsimha): In an ideal world, snap->has_more_to_sync == false should
       // be a sufficient condition for sync to have completed. However, the
@@ -151,8 +148,7 @@ bool ProfileSyncServiceTestHarness::RunStateChangeMachine() {
       break;
     }
     case WAITING_FOR_UPDATES: {
-      const SyncSessionSnapshot* snap =
-          service_->backend()->GetLastSessionSnapshot();
+      const SyncSessionSnapshot* snap = GetLastSessionSnapshot();
       DCHECK(snap) << "Should have been at least one sync session by now";
       if (snap->max_local_timestamp < min_timestamp_needed_)
         break;
@@ -176,8 +172,12 @@ void ProfileSyncServiceTestHarness::OnStateChanged() {
 
 bool ProfileSyncServiceTestHarness::AwaitSyncCycleCompletion(
     const std::string& reason) {
-  wait_state_ = WAITING_FOR_SYNC_TO_FINISH;
-  return AwaitStatusChangeWithTimeout(60, reason);
+  if (service()->backend()->HasUnsyncedItems()) {
+    wait_state_ = WAITING_FOR_SYNC_TO_FINISH;
+    return AwaitStatusChangeWithTimeout(60, reason);
+  } else {
+    return true;
+  }
 }
 
 bool ProfileSyncServiceTestHarness::AwaitMutualSyncCycleCompletion(
@@ -208,11 +208,22 @@ bool ProfileSyncServiceTestHarness::AwaitGroupSyncCycleCompletion(
   return return_value;
 }
 
+// static
+bool ProfileSyncServiceTestHarness::AwaitQuiescence(
+    std::vector<ProfileSyncServiceTestHarness*>& clients) {
+  bool return_value = true;
+  for (std::vector<ProfileSyncServiceTestHarness*>::iterator it =
+      clients.begin(); it != clients.end(); ++it) {
+    return_value = return_value &&
+        (*it)->AwaitGroupSyncCycleCompletion(clients);
+  }
+  return return_value;
+}
+
 bool ProfileSyncServiceTestHarness::WaitUntilTimestampIsAtLeast(
     int64 timestamp, const std::string& reason) {
   min_timestamp_needed_ = timestamp;
-  const SyncSessionSnapshot* snap =
-      service_->backend()->GetLastSessionSnapshot();
+  const SyncSessionSnapshot* snap = GetLastSessionSnapshot();
   DCHECK(snap) << "Should have been at least one sync session by now";
   if (snap->max_local_timestamp < min_timestamp_needed_) {
     wait_state_ = WAITING_FOR_UPDATES;
@@ -268,4 +279,10 @@ bool ProfileSyncServiceTestHarness::WaitForServiceInit() {
   }
 
   return true;
+}
+
+const SyncSessionSnapshot*
+    ProfileSyncServiceTestHarness::GetLastSessionSnapshot() const {
+  EXPECT_FALSE(service_ == NULL) << "Sync service has not yet been set up.";
+  return service_->backend()->GetLastSessionSnapshot();
 }
