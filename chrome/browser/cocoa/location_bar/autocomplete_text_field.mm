24b68114e81dddd76c53fecd354ef18cd3e8d8fe
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-27 19:57:24 +0000

    [Mac] Elide long Omnibox contents.
    Doing the most simple eliding, which is to truncate and replace with
    ellipses on a character basis.  I put this in the code rather than the
    .xib because this seems like it should be a hard-wired Omnibox
    behavior.
    
    BUG=50253
    TEST=Put in a very long URL and check that it elides as the window gets too small for it to fit.  You can easily craft long URLs by adding # and filling with whatever you desire after that.
    
    Review URL: http://codereview.chromium.org/3010031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53826 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/location_bar/autocomplete_text_field.mm
+++ chrome/browser/cocoa/location_bar/autocomplete_text_field.mm
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)awakeFromNib {
   DCHECK([[self cell] isKindOfClass:[AutocompleteTextFieldCell class]]);
+  [[self cell] setTruncatesLastVisibleLine:YES];
+  [[self cell] setLineBreakMode:NSLineBreakByTruncatingTail];
   currentToolTips_.reset([[NSMutableArray alloc] init]);
 }
 
