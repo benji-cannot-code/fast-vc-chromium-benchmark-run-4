906546428ff9db5142983f80ef0474f1a6845736
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-27 04:51:38 +0000

    2010-07-26  Yuta Kitamura  <yutak@chromium.org>
            Reviewed by Pavel Feldman.
    
            Add WebSocket resource type to Web Inspector.
    
            When a new WebSocket connection is established, a line for that connection
            will appear in Web Inspector's Resources tab. If the resource name is
            clicked, the details of handshake request and response will be shown.
    
            Web Inspector: WebSocket in Resources tab
            https://bugs.webkit.org/show_bug.cgi?id=40768
    
            * inspector/InspectorController.cpp:
            (WebCore::InspectorController::addResource): WebSocket resource does not
            have an associated loader, thus frame might be null. Need to check it.
            (WebCore::InspectorController::removeResource): Ditto.
            (WebCore::InspectorController::didCreateWebSocket):
            (WebCore::InspectorController::willSendWebSocketHandshakeRequest):
            (WebCore::InspectorController::didReceiveWebSocketHandshakeResponse):
            (WebCore::InspectorController::didCloseWebSocket):
            * inspector/InspectorController.h:
            * inspector/InspectorResource.cpp: Add null checks of m_loader and m_frame,
            because WebSocket does not have a loader and we need to allow null for
            these variables.
            (WebCore::createReadableStringFromBinary):
            (WebCore::InspectorResource::InspectorResource):
            (WebCore::InspectorResource::create): Factory function of
            regular (non-WebSocket) resources.
            (WebCore::InspectorResource::createWebSocket): Factory function of
            WebSocket resources.
            (WebCore::InspectorResource::updateWebSocketRequest):
            (WebCore::InspectorResource::updateWebSocketResponse):
            (WebCore::InspectorResource::updateScriptObject): m_frame->document() becomes
            available when Frame::setDocument() is called. We cannot obtain documentURL
            during the constructor or updateRequest() function, because m_frame->document()
            is not available yet at that point and documentURL will contain a wrong URL.
            As far as I know, updateScriptObject() is the only place where we can safely
            obtain documentURL.
            (WebCore::InspectorResource::cachedResource):
            (WebCore::InspectorResource::type):
            (WebCore::InspectorResource::resourceData):
            * inspector/InspectorResource.h:
            (WebCore::InspectorResource::):
            (WebCore::InspectorResource::markWebSocket):
            * inspector/front-end/Resource.js:
            (WebInspector.Resource.Type.toString):
            (WebInspector.Resource.prototype.set type):
            (WebInspector.Resource.prototype._mimeTypeIsConsistentWithType):
            * inspector/front-end/ResourceView.js:
            (WebInspector.ResourceView.prototype._refreshRequestHeaders):
            (WebInspector.ResourceView.prototype._refreshResponseHeaders):
            (WebInspector.ResourceView.prototype._refreshHeaders):
            * inspector/front-end/inspector.css:
            (.resources-category-websockets, .resources-category-other):
            (.resources-category-websockets .resources-graph-bar):
            (.resources-category-websockets.resource-cached .resources-graph-bar):
            * inspector/front-end/inspector.js:
            (WebInspector.loaded):
            (WebInspector.updateResource):
            * websockets/WebSocketChannel.cpp:
            (WebCore::WebSocketChannel::WebSocketChannel):
            (WebCore::WebSocketChannel::disconnect):
            (WebCore::WebSocketChannel::didOpen):
            (WebCore::WebSocketChannel::didClose):
            (WebCore::WebSocketChannel::processBuffer):
            (WebCore::WebSocketChannel::identifier):
            * websockets/WebSocketChannel.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64104 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorResource.h
+++ third_party/WebKit/WebCore/inspector/InspectorResource.h
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScriptObject.h"
 #include "ScriptState.h"
 #include "ScriptString.h"
+#include "WebSocketHandshakeRequest.h"
+#include "WebSocketHandshakeResponse.h"
 
 #include <wtf/CurrentTime.h>
 #include <wtf/OwnPtr.h>
@@ -54,6 +56,11 @@ namespace WebCore {
     class ResourceRequest;
     class ResourceResponse;
 
+#if ENABLE(WEB_SOCKETS)
+    class WebSocketHandshakeRequest;
+    class WebSocketHandshakeResponse;
+#endif
+
     class InspectorResource : public RefCounted<InspectorResource> {
     public:
 
@@ -66,15 +73,18 @@ namespace WebCore {
             Script,
             XHR,
             Media,
+            WebSocket,
             Other
         };
 
-        static PassRefPtr<InspectorResource> create(unsigned long identifier, DocumentLoader* loader, const KURL& requestURL)
-        {
-            return adoptRef(new InspectorResource(identifier, loader, requestURL));
-        }
+        static PassRefPtr<InspectorResource> create(unsigned long identifier, DocumentLoader* loader, const KURL& requestURL);
 
         static PassRefPtr<InspectorResource> createCached(unsigned long identifier, DocumentLoader*, const CachedResource*);
+
+#if ENABLE(WEB_SOCKETS)
+        // WebSocket resource doesn't have its loader. For WebSocket resources, m_loader and m_frame will become null.
+        static PassRefPtr<InspectorResource> createWebSocket(unsigned long identifier, const KURL& requestURL, const KURL& documentURL);
+#endif
 
         ~InspectorResource();
 
@@ -84,6 +94,11 @@ namespace WebCore {
 
         void updateRequest(const ResourceRequest&);
         void updateResponse(const ResourceResponse&);
+
+#if ENABLE(WEB_SOCKETS)
+        void updateWebSocketRequest(const WebSocketHandshakeRequest&);
+        void updateWebSocketResponse(const WebSocketHandshakeResponse&);
+#endif
 
         void setOverrideContent(const ScriptString& data, Type);
 
@@ -154,10 +169,15 @@ namespace WebCore {
 
         ScriptObject buildObjectForTiming(InspectorFrontend*, ResourceLoadTiming*);
 
+#if ENABLE(WEB_SOCKETS)
+        void markWebSocket() { m_isWebSocket = true; }
+#endif
+
         unsigned long m_identifier;
         RefPtr<DocumentLoader> m_loader;
         RefPtr<Frame> m_frame;
         KURL m_requestURL;
+        KURL m_documentURL;
         HTTPHeaderMap m_requestHeaderFields;
         HTTPHeaderMap m_responseHeaderFields;
         String m_mimeType;
@@ -184,6 +204,15 @@ namespace WebCore {
         String m_requestMethod;
         String m_requestFormData;
         Vector<RefPtr<InspectorResource> > m_redirects;
+
+#if ENABLE(WEB_SOCKETS)
+        bool m_isWebSocket;
+
+        // The following fields are not used for resources other than WebSocket.
+        // We allocate them dynamically to reduce memory consumption for regular resources.
+        OwnPtr<WebSocketHandshakeRequest::Key3> m_webSocketRequestKey3;
+        OwnPtr<WebSocketHandshakeResponse::ChallengeResponse> m_webSocketChallengeResponse;
+#endif
     };
 
 } // namespace WebCore
