521f4276c63747b6a1f10554c7c709fa61fe6bcc
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-27 21:29:06 +0000

    Add "pushed" as a state a TextButton can show (alongside "normal" and "hover").
    BUG=50107
    TEST=none
    Review URL: http://codereview.chromium.org/3058011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53847 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/clock_menu_button.cc
+++ chrome/browser/chromeos/status/clock_menu_button.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -35,7 +35,7 @@ ClockMenuButton::ClockMenuButton(StatusAreaHost* host)
   SetFont(ResourceBundle::GetSharedInstance().GetFont(
       ResourceBundle::BaseFont).DeriveFont(1, gfx::Font::BOLD));
   SetEnabledColor(0xB3FFFFFF); // White with 70% Alpha
-  SetShowHighlighted(false);
+  SetShowMultipleIconStates(false);
   set_alignment(TextButton::ALIGN_RIGHT);
   UpdateTextAndSetNextTimer();
 }
--- chrome/browser/chromeos/status/status_area_button.cc
+++ chrome/browser/chromeos/status/status_area_button.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,7 +20,6 @@ namespace chromeos {
 StatusAreaButton::StatusAreaButton(views::ViewMenuDelegate* menu_delegate)
     : MenuButton(NULL, std::wstring(), menu_delegate, false) {
   set_border(NULL);
-  SetShowHighlighted(true);
 }
 
 void StatusAreaButton::Paint(gfx::Canvas* canvas, bool for_drag) {
