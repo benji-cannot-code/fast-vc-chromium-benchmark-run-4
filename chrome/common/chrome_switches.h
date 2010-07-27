dd395ad7918b3ad7df26e47c82a4532692b3dbe8
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-27 17:18:23 +0000

    Mac: Add flag for expose-for-tabs
    BUG=50307
    TEST=none
    
    Review URL: http://codereview.chromium.org/3020034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53798 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -321,6 +321,7 @@ extern const char kPasswordStore[];
 
 #if defined(OS_MACOSX)
 extern const char kDisableFlashCoreAnimation[];
+extern const char kEnableExposeForTabs[];
 extern const char kEnableSandboxLogging[];
 #else
 extern const char kKioskMode[];
