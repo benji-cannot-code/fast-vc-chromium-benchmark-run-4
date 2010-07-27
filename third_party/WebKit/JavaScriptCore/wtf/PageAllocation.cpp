735bc881af6c051c26895a93b97ccbc0452694f2
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-27 05:51:10 +0000

    Chromium/GTK build fix
    * wtf/PageAllocation.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64107 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/PageAllocation.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/PageAllocation.cpp
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "PageAllocation.h"
 
+#include <unistd.h>
+
 #if HAVE(ERRNO_H)
 #include <errno.h>
 #endif
