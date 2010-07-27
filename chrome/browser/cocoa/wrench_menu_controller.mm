8183f3f308c83b8c34485c777def66f169f7c2ff
sanjeevr@chromium.org <sanjeevr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-27 19:46:04 +0000

    Exposed an IsEnabledAt for ButtonMenuItemModel (this is not used yet, will be used in a subsequent checkin). The Mac wrench menu zoom controls honor the global enabled state of the zoom commands.
    BUG=None.
    TEST=Open a PDF using the internal PDF viewer on the Mac. The Zoom controls on the Wrench menu should be disabled.
    
    Review URL: http://codereview.chromium.org/3026025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53824 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/wrench_menu_controller.mm
+++ chrome/browser/cocoa/wrench_menu_controller.mm
@@ -75,6 +75,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   NSString* title = base::SysUTF16ToNSString(
       [self wrenchMenuModel]->GetLabelForCommandId(IDC_ZOOM_PERCENT_DISPLAY));
   [[zoomItem_ viewWithTag:IDC_ZOOM_PERCENT_DISPLAY] setTitle:title];
+  bool plusEnabled = [self wrenchMenuModel]->IsCommandIdEnabled(IDC_ZOOM_PLUS);
+  bool minusEnabled = [self wrenchMenuModel]->IsCommandIdEnabled(
+      IDC_ZOOM_MINUS);
+
+  [zoomPlus_ setEnabled:plusEnabled];
+  [zoomMinus_ setEnabled:minusEnabled];
 
   NSImage* icon = [self wrenchMenuModel]->browser()->window()->IsFullscreen() ?
       [NSImage imageNamed:NSImageNameExitFullScreenTemplate] :
