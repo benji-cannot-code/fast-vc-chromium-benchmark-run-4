798eea8e49aa1d57e42f967b66b1c191e199fa0a
mirandac@google.com <mirandac@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-27 17:56:07 +0000

    Revert bookmark bar promo CL, r52993
    TBR: arv
    
    BUG=49328
    TEST=no bookmark bar promo on first run to empty profile.  or ever.
    Review URL: http://codereview.chromium.org/3053015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53804 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/web_contents_unittest.cc
+++ chrome/browser/tab_contents/web_contents_unittest.cc
@@ -185,7 +185,6 @@ class TabContentsTest : public RenderViewHostTestHarness {
   // is not supposed to overwrite a profile if it's already created.
   virtual void SetUp() {
     TestingProfile* profile = new TestingProfile();
-    profile->CreateBookmarkModel(false);
     profile_.reset(profile);
 
     // Set some (WebKit) user preferences.
