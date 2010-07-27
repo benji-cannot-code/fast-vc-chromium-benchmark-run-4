4b2769028eeb7be412cb5a4e91f991d5088db197
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-27 00:47:12 +0000

    Checking in http://codereview.chromium.org/2806070 and http://codereview.chromium.org/3028018 on behalf of zbehan@.
    third_party: add a stub cros directory and cros_api.gyp for libcros to satisfy gyp depdendencies
    cros: fix more of the third_party includes, forbid similar includes for future
    
    BUG=none
    TEST=make sure both chrome and chromeos builds are happy.
    TBR=zelidrag
     
    Review URL: http://codereview.chromium.org/3035021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53729 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/frame/panel_controller.h
+++ chrome/browser/chromeos/frame/panel_controller.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "app/x11_util.h"
 #include "views/controls/button/button.h"
-#include "third_party/cros/chromeos_wm_ipc_enums.h"
+#include "cros/chromeos_wm_ipc_enums.h"
 
 class BrowserView;
 class SkBitmap;
