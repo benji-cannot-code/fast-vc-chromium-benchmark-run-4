f1a694ed0ba31d3d661e5400f23cc2147ad84ec4
victorw@chromium.org <victorw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-27 18:14:31 +0000

    2010-07-27  Victor Wang  <victorw@chromium.org>
            Unreviewed, rolling out r64104.
            http://trac.webkit.org/changeset/64104
            https://bugs.webkit.org/show_bug.cgi?id=40768
    
            The patch causes chromium webkit socket laytest crashes on
            windows randomly
    
            * inspector/InspectorController.cpp:
            (WebCore::InspectorController::addResource):
            (WebCore::InspectorController::removeResource):
            * inspector/InspectorController.h:
            * inspector/InspectorResource.cpp:
            (WebCore::InspectorResource::InspectorResource):
            (WebCore::InspectorResource::updateScriptObject):
            (WebCore::InspectorResource::cachedResource):
            (WebCore::InspectorResource::type):
            (WebCore::InspectorResource::resourceData):
            * inspector/InspectorResource.h:
            (WebCore::InspectorResource::):
            (WebCore::InspectorResource::create):
            * inspector/front-end/Resource.js:
            (WebInspector.Resource.Type.toString):
            (WebInspector.Resource.prototype.set type):
            (WebInspector.Resource.prototype._mimeTypeIsConsistentWithType):
            * inspector/front-end/ResourceView.js:
            (WebInspector.ResourceView.prototype._refreshRequestHeaders):
            (WebInspector.ResourceView.prototype._refreshResponseHeaders):
            (WebInspector.ResourceView.prototype._refreshHeaders):
            * inspector/front-end/inspector.css:
            (.resources-category-scripts, .resources-category-xhr, .resources-category-fonts, .resources-category-other):
            * inspector/front-end/inspector.js:
            (WebInspector.loaded):
            (WebInspector.updateResource):
            * websockets/WebSocketChannel.cpp:
            (WebCore::WebSocketChannel::WebSocketChannel):
            (WebCore::WebSocketChannel::disconnect):
            (WebCore::WebSocketChannel::didOpen):
            (WebCore::WebSocketChannel::didClose):
            (WebCore::WebSocketChannel::processBuffer):
            * websockets/WebSocketChannel.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64142 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/websockets/WebSocketChannel.cpp
+++ third_party/WebKit/WebCore/websockets/WebSocketChannel.cpp
@@ -37,11 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CookieJar.h"
 #include "Document.h"
-#include "InspectorController.h"
 #include "Logging.h"
-#include "Page.h"
 #include "PlatformString.h"
-#include "ProgressTracker.h"
 #include "ScriptExecutionContext.h"
 #include "SocketStreamError.h"
 #include "SocketStreamHandle.h"
@@ -66,14 +63,7 @@ WebSocketChannel::WebSocketChannel(ScriptExecutionContext* context, WebSocketCha
     , m_suspended(false)
     , m_closed(false)
     , m_unhandledBufferedAmount(0)
-#if ENABLE(INSPECTOR)
-    , m_identifier(0)
-#endif
 {
-#if ENABLE(INSPECTOR)
-    if (InspectorController* controller = m_context->inspectorController())
-        controller->didCreateWebSocket(identifier(), url, m_context->url());
-#endif
 }
 
 WebSocketChannel::~WebSocketChannel()
@@ -122,11 +112,6 @@ void WebSocketChannel::close()
 void WebSocketChannel::disconnect()
 {
     LOG(Network, "WebSocketChannel %p disconnect", this);
-#if ENABLE(INSPECTOR)
-    if (m_context)
-        if (InspectorController* controller = m_context->inspectorController())
-            controller->didCloseWebSocket(identifier());
-#endif
     m_handshake.clearScriptExecutionContext();
     m_client = 0;
     m_context = 0;
@@ -152,10 +137,6 @@ void WebSocketChannel::didOpen(SocketStreamHandle* handle)
     ASSERT(handle == m_handle);
     if (!m_context)
         return;
-#if ENABLE(INSPECTOR)
-    if (InspectorController* controller = m_context->inspectorController())
-        controller->willSendWebSocketHandshakeRequest(identifier(), m_handshake.clientHandshakeRequest());
-#endif
     const CString& handshakeMessage = m_handshake.clientHandshakeMessage();
     if (!handle->send(handshakeMessage.data(), handshakeMessage.length())) {
         m_context->addMessage(JSMessageSource, LogMessageType, ErrorMessageLevel, "Error sending handshake message.", 0, m_handshake.clientOrigin());
@@ -166,11 +147,6 @@ void WebSocketChannel::didOpen(SocketStreamHandle* handle)
 void WebSocketChannel::didClose(SocketStreamHandle* handle)
 {
     LOG(Network, "WebSocketChannel %p didClose", this);
-#if ENABLE(INSPECTOR)
-    if (m_context)
-        if (InspectorController* controller = m_context->inspectorController())
-            controller->didCloseWebSocket(identifier());
-#endif
     ASSERT_UNUSED(handle, handle == m_handle || !m_handle);
     m_closed = true;
     if (m_handle) {
@@ -262,10 +238,6 @@ bool WebSocketChannel::processBuffer()
         if (headerLength <= 0)
             return false;
         if (m_handshake.mode() == WebSocketHandshake::Connected) {
-#if ENABLE(INSPECTOR)
-            if (InspectorController* controller = m_context->inspectorController())
-                controller->didReceiveWebSocketHandshakeResponse(identifier(), m_handshake.serverHandshakeResponse());
-#endif
             if (!m_handshake.serverSetCookie().isEmpty()) {
                 if (m_context->isDocument()) {
                     Document* document = static_cast<Document*>(m_context);
@@ -356,21 +328,6 @@ void WebSocketChannel::resumeTimerFired(Timer<WebSocketChannel>* timer)
     if (!m_suspended && m_client && m_closed && m_handle)
         didClose(m_handle.get());
 }
-
-#if ENABLE(INSPECTOR)
-unsigned long WebSocketChannel::identifier()
-{
-    if (m_identifier)
-        return m_identifier;
-
-    if (InspectorController* controller = m_context->inspectorController())
-        if (Page* page = controller->inspectedPage())
-            m_identifier = page->progress()->createUniqueIdentifier();
-
-    ASSERT(m_identifier);
-    return m_identifier;
-}
-#endif // ENABLE(INSPECTOR)
 
 }  // namespace WebCore
 
