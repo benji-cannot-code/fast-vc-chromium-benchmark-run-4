21e5c2a4de372220839b91bd45dc2271b6f1b3c7
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-27 17:21:02 +0000

    Made CacheInvalidationPacketHandler aware of XmppClient state changes.
    This is a speculative/defensive fix for bug 49274.
    
    BUG=49274
    TEST=manual (checked that this didn't break normal behavior)
    
    Review URL: http://codereview.chromium.org/3054018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53799 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/notifier/cache_invalidation_packet_handler.h
+++ chrome/browser/sync/notifier/cache_invalidation_packet_handler.h
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
+#include "talk/base/sigslot.h"
 #include "talk/xmpp/jid.h"
+#include "talk/xmpp/xmppengine.h"
 
 namespace buzz {
 class XmppClient;
@@ -28,7 +30,7 @@ namespace sync_notifier {
 
 // TODO(akalin): Add a NonThreadSafe member to this class and use it.
 
-class CacheInvalidationPacketHandler {
+class CacheInvalidationPacketHandler : public sigslot::has_slots<> {
  public:
   // Starts routing packets from |invalidation_client| through
   // |xmpp_client|.  |invalidation_client| must not already be routing
@@ -48,6 +50,8 @@ class CacheInvalidationPacketHandler {
       invalidation::NetworkEndpoint* const& network_endpoint);
 
   void HandleInboundPacket(const std::string& packet);
+
+  void OnClientStateChange(buzz::XmppEngine::State state);
 
   buzz::XmppClient* xmpp_client_;
   invalidation::InvalidationClient* invalidation_client_;
