ae13284e99cb752ca158d5f00aedb849247065a4
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-27 01:00:27 +0000

    Make the resize gripper invisible.
    BUG=50107
    TEST=Resize gripper nest to browser actions container should no longer appear
    Review URL: http://codereview.chromium.org/3052011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53732 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/extensions/extension_installed_bubble.cc
+++ chrome/browser/views/extensions/extension_installed_bubble.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/theme_resources.h"
 #include "views/controls/button/checkbox.h"
 #include "views/controls/button/image_button.h"
+#include "views/controls/image_view.h"
 #include "views/controls/label.h"
 #include "views/standard_layout.h"
 #include "views/view.h"
