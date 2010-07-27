f5c93b4812642211e03f8d0451430b1490dc1344
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-27 22:19:31 +0000

    Revert 53849 - Implement most of the startup page controls in DOMUI prefs
    Hooks up display of startup pages, removal of pages, and setting the startup pages to the currently open tabs.
    Uses a select for now, but will eventually be replaced with a List so the favicons can be displayed.
    
    BUG=48713
    TEST=Startup pages can be viewed, removed, and set to current in DOMUI prefs.
    
    Review URL: http://codereview.chromium.org/3046025
    
    TBR=stuartmorgan@chromium.org
    Review URL: http://codereview.chromium.org/3058018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53857 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/custom_home_pages_table_model.cc
+++ chrome/browser/custom_home_pages_table_model.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/common/pref_names.h"
-#include "chrome/common/url_constants.h"
 #include "gfx/codec/png_codec.h"
 #include "grit/app_resources.h"
 #include "grit/generated_resources.h"
@@ -86,9 +85,7 @@ void CustomHomePagesTableModel::SetToCurrentlyOpenPages() {
 
     for (int tab_index = 0; tab_index < browser->tab_count(); ++tab_index) {
       const GURL url = browser->GetTabContentsAt(tab_index)->GetURL();
-      if (!url.is_empty() &&
-          !(url.SchemeIs(chrome::kChromeUIScheme) &&
-            url.host() == chrome::kChromeUIOptionsHost))
+      if (!url.is_empty())
         Add(add_index++, url);
     }
   }
