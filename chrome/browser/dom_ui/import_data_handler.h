e57d76cc316cb724c4ba7fd26e93387effc1395a
sargrass@google.com <sargrass@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 21:55:15 +0000

    Stop Syncing Overlay and Import Data Overlay
    BUG=48883
    TEST=Exercise Personal Stuff page via --enabled-tabbed-options
    
    Review URL: http://codereview.chromium.org/3038016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54032 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/dom_ui/import_data_handler.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_DOM_UI_IMPORT_DATA_HANDLER_H_
+#define CHROME_BROWSER_DOM_UI_IMPORT_DATA_HANDLER_H_
+
+#include "chrome/browser/dom_ui/options_ui.h"
+#include "chrome/browser/importer/importer.h"
+
+// Chrome personal stuff import data overlay UI handler.
+class ImportDataHandler : public OptionsPageUIHandler {
+ public:
+  ImportDataHandler();
+  virtual ~ImportDataHandler();
+
+  virtual void Initialize();
+
+  // OptionsUIHandler implementation.
+  virtual void GetLocalizedValues(DictionaryValue* localized_strings);
+
+  // DOMMessageHandler implementation.
+  virtual void RegisterMessages();
+
+ private:
+  void DetectSupportedBrowsers();
+
+  // Utility class that does the actual import.
+  scoped_refptr<ImporterHost> importer_host_;
+
+  DISALLOW_COPY_AND_ASSIGN(ImportDataHandler);
+};
+
+#endif  // CHROME_BROWSER_DOM_UI_IMPORT_DATA_HANDLER_H_
