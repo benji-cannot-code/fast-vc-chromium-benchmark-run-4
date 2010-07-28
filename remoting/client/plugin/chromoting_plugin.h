4f609398ae18c9429db534a35214296f1381b29f
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 01:23:17 +0000

    Initial scriptable object implementation.
    BUG=50248
    TEST=write javascript to manually setup connection.
    
    Review URL: http://codereview.chromium.org/3064009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53892 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/chromoting_plugin.h
+++ remoting/client/plugin/chromoting_plugin.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/scoped_ptr.h"
+#include "remoting/client/client_context.h"
 #include "remoting/client/host_connection.h"
 #include "testing/gtest/include/gtest/gtest_prod.h"
 #include "third_party/ppapi/c/pp_event.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/ppapi/c/pp_resource.h"
 #include "third_party/ppapi/cpp/instance.h"
 #include "third_party/ppapi/cpp/device_context_2d.h"
+#include "third_party/ppapi/cpp/var.h"
 
 class MessageLoop;
 
@@ -50,13 +52,14 @@ class ChromotingPlugin : public pp::Instance {
   virtual ~ChromotingPlugin();
 
   virtual bool Init(uint32_t argc, const char* argn[], const char* argv[]);
+  virtual void Connect(const ClientConfig& config);
   virtual bool HandleEvent(const PP_Event& event);
-  virtual void ViewChanged(const PP_Rect& position, const PP_Rect& clip);
+  virtual pp::Var GetInstanceObject();
+  virtual void ViewChanged(const pp::Rect& position, const pp::Rect& clip);
 
   virtual bool CurrentlyOnPluginThread() const;
 
  private:
-  FRIEND_TEST(ChromotingPluginTest, ParseUrl);
   FRIEND_TEST(ChromotingPluginTest, TestCaseSetup);
 
   // Since we're an internal plugin, we can just grab the message loop during
@@ -67,15 +70,12 @@ class ChromotingPlugin : public pp::Instance {
   // TODO(ajwong): Think if there is a better way to safeguard this.
   MessageLoop* pepper_main_loop_dont_post_to_me_;
 
-  scoped_ptr<ClientContext> context_;
-
+  ClientContext context_;
   scoped_ptr<HostConnection> host_connection_;
-
   scoped_ptr<PepperView> view_;
-
   scoped_ptr<InputHandler> input_handler_;
-
   scoped_ptr<ChromotingClient> client_;
+  pp::Var instance_object_;  // JavaScript interface to control this instance.
 
   DISALLOW_COPY_AND_ASSIGN(ChromotingPlugin);
 };
