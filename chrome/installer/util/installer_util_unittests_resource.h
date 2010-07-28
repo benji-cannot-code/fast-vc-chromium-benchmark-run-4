cb323f869366323281134135f19b2caca02409fc
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 16:28:13 +0000

    Fix line ending with white spaces in some files.
    BUG=50266
    TEST=None
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>
    
    Review URL: http://codereview.chromium.org/3046028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53956 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/installer_util_unittests_resource.h
+++ chrome/installer/util/installer_util_unittests_resource.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Used by installer_util_unittests.rc
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
