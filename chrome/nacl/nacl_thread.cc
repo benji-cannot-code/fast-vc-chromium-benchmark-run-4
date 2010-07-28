548f12a15db7aeca600f0782fec884a30ad53f4f
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 16:25:33 +0000

    Make NaCl/Chromium startup interface easier to maintain
    Use the new NaClMainForChromium() from the NaCl tree, instead of
    sel_main.cc in the Chromium tree.
    
    Use the new variant of RegisterInternalNaClPlugin() that can receive
    multiple functions instead of just one.
    
    This depends on r2744 from NaCl.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=642
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=469
    TEST=Chromium's nacl_ui_tests
    
    Review URL: http://codereview.chromium.org/3039016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53952 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/nacl/nacl_thread.cc
+++ chrome/nacl/nacl_thread.cc
@@ -17,7 +17,9 @@ typedef HANDLE NaClHandle;
 typedef int NaClHandle;
 #endif  // NaClHandle
 
-int SelMain(const int desc, const NaClHandle handle);
+// This is currently necessary because we have a conflict between
+// NaCl's "struct NaClThread" and Chromium's "class NaClThread".
+extern "C" int NaClMainForChromium(int handle_count, const NaClHandle* handles);
 
 NaClThread::NaClThread() {
 }
@@ -37,5 +39,6 @@ void NaClThread::OnControlMessageReceived(const IPC::Message& msg) {
 
 void NaClThread::OnStartSelLdr(int channel_descriptor,
                                nacl::FileDescriptor handle) {
-  SelMain(channel_descriptor, nacl::ToNativeHandle(handle));
+  NaClHandle nacl_handle = nacl::ToNativeHandle(handle);
+  NaClMainForChromium(/* handle_count= */ 1, &nacl_handle);
 }
