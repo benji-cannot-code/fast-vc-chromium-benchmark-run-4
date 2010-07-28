1a40f665adcc9160c0d796be2f37824d83c08d15
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 00:03:06 +0000

    Remove support for background masks.  These are no longer necessary in the new theme.
    BUG=50107
    TEST=none
    Review URL: http://codereview.chromium.org/2847079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53868 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/back_forward_button_gtk.cc
+++ chrome/browser/gtk/back_forward_button_gtk.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -27,14 +27,13 @@ BackForwardButtonGtk::BackForwardButtonGtk(Browser* browser, bool is_forward)
     : browser_(browser),
       is_forward_(is_forward),
       show_menu_factory_(this) {
-  int normal, pushed, hover, disabled, background, tooltip;
+  int normal, pushed, hover, disabled, tooltip;
   const char* stock;
   if (is_forward) {
     normal = IDR_FORWARD;
     pushed = IDR_FORWARD_P;
     hover = IDR_FORWARD_H;
     disabled = IDR_FORWARD_D;
-    background = IDR_FORWARD_MASK;
     tooltip = IDS_TOOLTIP_FORWARD;
     stock = GTK_STOCK_GO_FORWARD;
   } else {
@@ -42,14 +41,12 @@ BackForwardButtonGtk::BackForwardButtonGtk(Browser* browser, bool is_forward)
     pushed = IDR_BACK_P;
     hover = IDR_BACK_H;
     disabled = IDR_BACK_D;
-    background = IDR_BACK_MASK;
     tooltip = IDS_TOOLTIP_BACK;
     stock = GTK_STOCK_GO_BACK;
   }
   button_.reset(new CustomDrawButton(
       GtkThemeProvider::GetFrom(browser_->profile()),
-      normal, pushed, hover, disabled, background, stock,
-      GTK_ICON_SIZE_SMALL_TOOLBAR));
+      normal, pushed, hover, disabled, stock, GTK_ICON_SIZE_SMALL_TOOLBAR));
   gtk_widget_set_tooltip_text(widget(),
                               l10n_util::GetStringUTF8(tooltip).c_str());
   menu_model_.reset(new BackForwardMenuModel(browser, is_forward ?
