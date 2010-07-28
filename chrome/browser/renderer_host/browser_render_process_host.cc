69e9bfe2d7e96aca2c759dce1947050f1784016a
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 08:31:30 +0000

    Move click-to-play behind the --enable-click-to-play switch.
    BUG=50086
    TEST=none
    
    Review URL: http://codereview.chromium.org/3074005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53924 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -578,6 +578,7 @@ void BrowserRenderProcessHost::PropagateBrowserCommandLineToRenderer(
     switches::kRemoteShellPort,
     switches::kEnablePepperTesting,
     switches::kEnableChromoting,
+    switches::kEnableClickToPlay,
     switches::kPrelaunchGpuProcess,
   };
 
