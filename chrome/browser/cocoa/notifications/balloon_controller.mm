09f255a85bb5799ee9feafb02f4449e2f106cf99
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 21:10:43 +0000

    Fix memory leaks in the balloon controller unit test by loading the NIB in the correct test-friendly way, which allows the windows to be closed correctly and the cleanup code to run as expected.
    BUG=49590
    TEST=unittests
    
    Review URL: http://codereview.chromium.org/2825073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54018 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/notifications/balloon_controller.mm
+++ chrome/browser/cocoa/notifications/balloon_controller.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
 #import "base/cocoa_protocols_mac.h"
+#include "base/mac_util.h"
 #import "base/scoped_nsobject.h"
 #include "base/utf_string_conversions.h"
 #import "chrome/browser/cocoa/menu_controller.h"
@@ -34,7 +35,10 @@ const int kRightMargin = 1;
 @implementation BalloonController
 
 - (id)initWithBalloon:(Balloon*)balloon {
-  if ((self = [super initWithWindowNibName:@"Notification"])) {
+  NSString* nibpath =
+      [mac_util::MainAppBundle() pathForResource:@"Notification"
+                                          ofType:@"nib"];
+  if ((self = [super initWithWindowNibPath:nibpath owner:self])) {
     balloon_ = balloon;
     [self initializeHost];
     menuModel_.reset(new NotificationOptionsMenuModel(balloon));
@@ -58,11 +62,14 @@ const int kRightMargin = 1;
       WideToUTF16(balloon_->notification().display_source()));
   [originLabel_ setStringValue:sourceLabelText];
 
-  gfx::NativeView contents = htmlContents_->native_view();
-  [contents setFrame:NSMakeRect(kLeftMargin, kTopMargin, 0, 0)];
-  [[htmlContainer_ superview] addSubview:contents
-                              positioned:NSWindowBelow
-                              relativeTo:nil];
+  // This condition is false in unit tests which have no RVH.
+  if (htmlContents_.get()) {
+    gfx::NativeView contents = htmlContents_->native_view();
+    [contents setFrame:NSMakeRect(kLeftMargin, kTopMargin, 0, 0)];
+    [[htmlContainer_ superview] addSubview:contents
+                                positioned:NSWindowBelow
+                                relativeTo:nil];
+  }
 }
 
 - (IBAction)optionsButtonPressed:(id)sender {
@@ -105,7 +112,9 @@ const int kRightMargin = 1;
   int w = [self desiredTotalWidth];
   int h = [self desiredTotalHeight];
 
-  htmlContents_->UpdateActualSize(balloon_->content_size());
+  if (htmlContents_.get())
+    htmlContents_->UpdateActualSize(balloon_->content_size());
+
   [[self window] setFrame:NSMakeRect(x, y, w, h)
                   display:YES
                   animate:YES];
