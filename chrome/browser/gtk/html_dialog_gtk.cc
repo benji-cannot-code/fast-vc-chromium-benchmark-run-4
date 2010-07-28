bb66a9c521a3db8071c6e0d8b476e527a564710e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 14:18:03 +0000

    gtk: Make use of the gtk signal macro in HtmlDialogGtk class.
    BUG=None
    TEST=manually and trybots.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>
    
    Review URL: http://codereview.chromium.org/3069001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53937 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/html_dialog_gtk.cc
+++ chrome/browser/gtk/html_dialog_gtk.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -143,7 +143,7 @@ void HtmlDialogGtk::InitDialog() {
       flags,
       NULL);
 
-  g_signal_connect(dialog_, "response", G_CALLBACK(OnResponse), this);
+  g_signal_connect(dialog_, "response", G_CALLBACK(OnResponseThunk), this);
 
   tab_contents_container_.reset(new TabContentsContainerGtk(NULL));
   gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog_)->vbox),
@@ -161,8 +161,6 @@ void HtmlDialogGtk::InitDialog() {
   gtk_widget_show_all(dialog_);
 }
 
-// static
-void HtmlDialogGtk::OnResponse(GtkWidget* widget, int response,
-                               HtmlDialogGtk* dialog) {
-  dialog->OnDialogClosed(std::string());
+void HtmlDialogGtk::OnResponse(GtkWidget* dialog, int response_id) {
+  OnDialogClosed(std::string());
 }
