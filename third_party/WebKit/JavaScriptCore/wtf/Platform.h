e86ed6c162162a32c3824601b0593f8ed368444a
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-28 00:30:34 +0000

    Speculative build fix for Chromium/Win
    * wtf/Platform.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64183 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -82,6 +82,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_COMPILER_GCC 1
 #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #define GCC_VERSION_AT_LEAST(major, minor, patch) (GCC_VERSION >= (major * 10000 + minor * 100 + patch))
+#else
+/* define this for !GCC compilers, just so we can write things like COMPILER(GCC) && GCC_VERSION_AT_LEAST(4,1,0) */
+#define GCC_VERSION_AT_LEAST(major, minor, patch) 0
 #endif
 
 /* COMPILER(MINGW) - MinGW GCC */
