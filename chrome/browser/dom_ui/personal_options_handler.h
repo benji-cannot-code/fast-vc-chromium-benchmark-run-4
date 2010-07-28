2c28a2b026ae8a0e1ba5d12f43f037d6d0fffc0f
sargrass@google.com <sargrass@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 23:00:31 +0000

    remove the chromeos include and reland
    Review URL: http://codereview.chromium.org/3069008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54056 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/personal_options_handler.h
+++ chrome/browser/dom_ui/personal_options_handler.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "chrome/browser/dom_ui/options_ui.h"
+#include "chrome/browser/sync/profile_sync_service.h"
 
 // Chrome personal options page UI handler.
 class PersonalOptionsHandler : public OptionsPageUIHandler {
@@ -18,7 +19,12 @@ class PersonalOptionsHandler : public OptionsPageUIHandler {
   // OptionsUIHandler implementation.
   virtual void GetLocalizedValues(DictionaryValue* localized_strings);
 
+  // DOMMessageHandler implementation.
+  virtual void RegisterMessages();
+
  private:
+  virtual void SetSyncStatusUIString(const Value* value);
+
   DISALLOW_COPY_AND_ASSIGN(PersonalOptionsHandler);
 };
 
