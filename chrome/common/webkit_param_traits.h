ff3d3efb449713c3f975e6d722f5fc3c79870ba7
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 16:48:10 +0000

    Fix lines longer than 80 characters.
    BUG=50266
    TEST=None
    
    Review URL: http://codereview.chromium.org/3013038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53958 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/webkit_param_traits.h
+++ chrome/common/webkit_param_traits.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.  Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 //
 // This file contains ParamTraits templates to support serialization of WebKit
 // data types over IPC.
@@ -14,12 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 //   o  Many WebKit structures are not thread-safe.  WebString, for example,
 //      contains a reference counted buffer, which does not use thread-safe
-//      reference counting.  If we allowed serializing WebString, then we may run
-//      the risk of introducing subtle thread-safety bugs if people passed a
+//      reference counting.  If we allowed serializing WebString, then we may
+//      run the risk of introducing subtle thread-safety bugs if people passed a
 //      WebString across threads via PostTask(NewRunnableMethod(...)).
 //
-//   o  The WebKit API has redundant types for strings, and we should avoid using
-//      those beyond code that interfaces with the WebKit API.
+//   o  The WebKit API has redundant types for strings, and we should avoid
+//      using those beyond code that interfaces with the WebKit API.
 
 #ifndef CHROME_COMMON_WEBKIT_PARAM_TRAITS_H_
 #define CHROME_COMMON_WEBKIT_PARAM_TRAITS_H_
