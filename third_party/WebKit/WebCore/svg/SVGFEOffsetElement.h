f689bee0a378e708b07bb2e7ac8de5a3d182f131
zherczeg@webkit.org <zherczeg@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-28 08:08:49 +0000

    SVGFilterElement & SVGFE*Element don't support dynamic invalidation, when attributes change https://bugs.webkit.org/show_bug.cgi?id=42244
    Reviewed by Nikolas Zimmermann.
    
    WebCore: 
    
    Implementing svgAttributeChanged for SVGFEOffsetElement object.
    
    Tests: svg/dynamic-updates/SVGFEOffsetElement-dom-dx-attr.html
           svg/dynamic-updates/SVGFEOffsetElement-dom-dy-attr.html
           svg/dynamic-updates/SVGFEOffsetElement-dom-in-attr.html
           svg/dynamic-updates/SVGFEOffsetElement-svgdom-dx-prop.html
           svg/dynamic-updates/SVGFEOffsetElement-svgdom-dy-prop.html
           svg/dynamic-updates/SVGFEOffsetElement-svgdom-in-prop.html
    
    * svg/SVGFEOffsetElement.cpp:
    (WebCore::SVGFEOffsetElement::svgAttributeChanged):
    * svg/SVGFEOffsetElement.h:
    
    LayoutTests: 
    
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-dom-dx-attr-expected.checksum: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-dom-dx-attr-expected.png: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-dom-dy-attr-expected.checksum: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-dom-dy-attr-expected.png: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-dom-in-attr-expected.checksum: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-dom-in-attr-expected.png: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-svgdom-dx-prop-expected.checksum: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-svgdom-dx-prop-expected.png: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-svgdom-dy-prop-expected.checksum: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-svgdom-dy-prop-expected.png: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-svgdom-in-prop-expected.checksum: Added.
    * platform/mac/svg/dynamic-updates/SVGFEOffsetElement-svgdom-in-prop-expected.png: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-dom-dx-attr-expected.txt: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-dom-dx-attr.html: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-dom-dy-attr-expected.txt: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-dom-dy-attr.html: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-dom-in-attr-expected.txt: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-dom-in-attr.html: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-svgdom-dx-prop-expected.txt: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-svgdom-dx-prop.html: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-svgdom-dy-prop-expected.txt: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-svgdom-dy-prop.html: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-svgdom-in-prop-expected.txt: Added.
    * svg/dynamic-updates/SVGFEOffsetElement-svgdom-in-prop.html: Added.
    * svg/dynamic-updates/script-tests/SVGFEOffsetElement-dom-dx-attr.js: Added.
    (executeTest):
    * svg/dynamic-updates/script-tests/SVGFEOffsetElement-dom-dy-attr.js: Added.
    (executeTest):
    * svg/dynamic-updates/script-tests/SVGFEOffsetElement-dom-in-attr.js: Added.
    (executeTest):
    * svg/dynamic-updates/script-tests/SVGFEOffsetElement-svgdom-dx-prop.js: Added.
    (executeTest):
    * svg/dynamic-updates/script-tests/SVGFEOffsetElement-svgdom-dy-prop.js: Added.
    (executeTest):
    * svg/dynamic-updates/script-tests/SVGFEOffsetElement-svgdom-in-prop.js: Added.
    (executeTest):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64192 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGFEOffsetElement.h
+++ third_party/WebKit/WebCore/svg/SVGFEOffsetElement.h
@@ -34,6 +34,7 @@ public:
     virtual ~SVGFEOffsetElement();
 
     virtual void parseMappedAttribute(Attribute*);
+    virtual void svgAttributeChanged(const QualifiedName&);
     virtual void synchronizeProperty(const QualifiedName&);
     virtual PassRefPtr<FilterEffect> build(SVGFilterBuilder*);
 
