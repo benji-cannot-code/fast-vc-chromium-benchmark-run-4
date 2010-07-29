6d796745e607a35bdcf8cfae30a17b73f9e6447f
cmasone@google.com <cmasone@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-29 21:28:11 +0000

    OwnerManager, allows use of OwnerKeyUtils to take ownership of a device
        BUG=chromium-os:4485
        TEST=unit tests
    
    Review URL: http://codereview.chromium.org/3058021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/notification_type.h
+++ chrome/common/notification_type.h
@@ -1069,6 +1069,10 @@ class NotificationType {
     // in chrome/browser/chromeos/network_state_notifier.h.
     // TODO(oshima): Port this to all platforms.
     NETWORK_STATE_CHANGED,
+
+    // Sent when an attempt to acquire the public key of the owner of a chromium
+    // os device has completed.  Details are a boolean value indicating success.
+    OWNER_KEY_FETCH_ATTEMPT_COMPLETE,
 #endif
 
     // Sent before the repost form warning is brought up.
