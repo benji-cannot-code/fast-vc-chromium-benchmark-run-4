3f8e07dd593cdc20b70bd4ab39a80709374f7158
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-29 07:01:58 +0000

    Fix the build - forgot to check in a file.
    BUG=none
    TEST=Bots go green.
    TBR=erg
    Review URL: http://codereview.chromium.org/3038035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54103 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/fileicon_source.cc
+++ chrome/browser/dom_ui/fileicon_source.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/dom_ui/fileicon_source.h"
 
 #include "base/callback.h"
+#include "base/ref_counted_memory.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/common/time_format.h"
