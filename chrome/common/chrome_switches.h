b74d29dcbb50f6af8dc72f549b4de12e4cbdd3da
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-29 18:47:09 +0000

    Add a command line switch to control accelerated 2d canvas
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/2873074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54159 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -92,6 +92,7 @@ extern const char kDnsPrefetchDisable[];
 extern const char kDomAutomationController[];
 extern const char kDumpHistogramsOnExit[];
 extern const char kEnableAcceleratedCompositing[];
+extern const char kEnableAccelerated2dCanvas[];
 extern const char kEnableAeroPeekTabs[];
 extern const char kEnableApps[];
 extern const char kEnableAuthNegotiatePort[];
