0b8a89c3ed5d876e37e72c20e3b7f2414fbdcf9f
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-29 03:01:53 +0000

    Another Windows build fix.
    * UIProcess/API/C/WKBase.h:
    * win/WebKit2Generated.make:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64258 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/C/WKBase.h
+++ third_party/WebKit/WebKit2/UIProcess/API/C/WKBase.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WKBase_h
 #define WKBase_h
 
+#include <stdint.h>
+
 #if defined(WIN32) || defined(_WIN32)
 #include <WebKit2/WKBaseWin.h>
 #endif
