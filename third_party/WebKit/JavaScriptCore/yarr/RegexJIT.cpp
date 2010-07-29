c55e1fef2e11f62ac1eb7cbdca8f82f40112601a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-29 21:33:19 +0000

    2010-07-29  Michael Saboff  <msaboff@apple.com>
            Reviewed by Oliver Hunt.
    
            Fixed issue where RegExp greedy jit code loops when no input is
            consumed.  Changed the code to only loop if some input was consumed,
            but fall through if we successfully match an alternative that
            doesn't consume any input.
            https://bugs.webkit.org/show_bug.cgi?id=42664
    
            * yarr/RegexJIT.cpp:
            (JSC::Yarr::RegexGenerator::generateParenthesesGreedyNoBacktrack):
    2010-07-29  Michael Saboff  <msaboff@apple.com>
    
            Reviewed by Oliver Hunt.
    
            Added tests to correspond to the changes made in
            JavaScriptCore/yarr/RegexJIT.cpp to fix
            https://bugs.webkit.org/show_bug.cgi?id=42664.
            Note that some of the new test cases fail due to one or more
            unrelated bugs in the pcre interpreter.  The expected results
            for these tests will need to be updated when the implementation
            conforms to the JS standard.
    
            * fast/js/regexp-look-ahead-empty-expected.txt: Added.
            * fast/js/regexp-look-ahead-empty.html: Added.
            * fast/js/script-tests/regexp-look-ahead-empty.js: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64307 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/yarr/RegexJIT.cpp
+++ third_party/WebKit/JavaScriptCore/yarr/RegexJIT.cpp
@@ -1033,6 +1033,9 @@ class RegexGenerator : private MacroAssembler {
         TermGenerationState parenthesesState(disjunction, state.checkedTotal);
 
         Label matchAgain(this);
+
+        storeToFrame(index, parenthesesTerm.frameLocation); // Save the current index to check for zero len matches later.
+
         for (parenthesesState.resetAlternative(); parenthesesState.alternativeValid(); parenthesesState.nextAlternative()) {
 
             PatternAlternative* alternative = parenthesesState.alternative();
@@ -1047,9 +1050,9 @@ class RegexGenerator : private MacroAssembler {
             for (parenthesesState.resetTerm(); parenthesesState.termValid(); parenthesesState.nextTerm())
                 generateTerm(parenthesesState);
 
-            // If we get here, we matched! Limit not yet supported, so just try to match more!
-            jump(matchAgain);
-            
+            // If we get here, we matched! If the index advanced then try to match more since limit isn't supported yet.
+            branch32(GreaterThan, index, Address(stackPointerRegister, (parenthesesTerm.frameLocation * sizeof(void*))), matchAgain);
+
             parenthesesState.linkAlternativeBacktracks(this);
             // We get here if the alternative fails to match - fall through to the next iteration, or out of the loop.
 
