91d2c6fd90e7e64848fe6e353d83831e9786f243
sanjeevr@chromium.org <sanjeevr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-29 19:06:56 +0000

    The button menu items on GTK menus honor the enabled state of the command. BUG=None TEST=Verify that wrench menu zoom controls are disabled when showing PDF using the Chrome PDF plugin anf enabled otherwise.
    Review URL: http://codereview.chromium.org/3032038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/menu_gtk.cc
+++ chrome/browser/gtk/menu_gtk.cc
@@ -81,24 +81,25 @@ void OnSubmenuShowButtonMenuItem(GtkWidget* widget, GtkButton* button) {
   int index = GPOINTER_TO_INT(g_object_get_data(
       G_OBJECT(button), "button-model-id"));
 
-  std::string label =
-      ConvertAcceleratorsFromWindowsStyle(
-          UTF16ToUTF8(model->GetLabelAt(index)));
-  gtk_button_set_label(GTK_BUTTON(button), label.c_str());
+  if (model->IsLabelDynamicAt(index)) {
+    std::string label =
+        ConvertAcceleratorsFromWindowsStyle(
+            UTF16ToUTF8(model->GetLabelAt(index)));
+    gtk_button_set_label(GTK_BUTTON(button), label.c_str());
+  }
+  gtk_widget_set_sensitive(GTK_WIDGET(button), model->IsEnabledAt(index));
 }
 
-void SetupDynamicLabelMenuButton(GtkWidget* button,
-                                 GtkWidget* menu,
-                                 menus::ButtonMenuItemModel* model,
-                                 int index) {
-  if (model->IsLabelDynamicAt(index)) {
-    g_object_set_data(G_OBJECT(button), "button-model",
-                      model);
-    g_object_set_data(G_OBJECT(button), "button-model-id",
-                      GINT_TO_POINTER(index));
-    g_signal_connect(menu, "show", G_CALLBACK(OnSubmenuShowButtonMenuItem),
-                     button);
-  }
+void SetupButtonShowHandler(GtkWidget* button,
+                            GtkWidget* menu,
+                            menus::ButtonMenuItemModel* model,
+                            int index) {
+  g_object_set_data(G_OBJECT(button), "button-model",
+                    model);
+  g_object_set_data(G_OBJECT(button), "button-model-id",
+                    GINT_TO_POINTER(index));
+  g_signal_connect(menu, "show", G_CALLBACK(OnSubmenuShowButtonMenuItem),
+                   button);
 }
 
 void OnSubmenuShowButtonImage(GtkWidget* widget, GtkButton* button) {
@@ -402,7 +403,7 @@ GtkWidget* MenuGtk::BuildButtomMenuItem(menus::ButtonMenuItemModel* model,
                   UTF16ToUTF8(model->GetLabelAt(i))).c_str());
         }
 
-        SetupDynamicLabelMenuButton(button, menu, model, i);
+        SetupButtonShowHandler(button, menu, model, i);
         break;
       }
       case menus::ButtonMenuItemModel::TYPE_BUTTON_LABEL: {
@@ -413,7 +414,7 @@ GtkWidget* MenuGtk::BuildButtomMenuItem(menus::ButtonMenuItemModel* model,
             GTK_BUTTON(button),
             RemoveWindowsStyleAccelerators(
                 UTF16ToUTF8(model->GetLabelAt(i))).c_str());
-        SetupDynamicLabelMenuButton(button, menu, model, i);
+        SetupButtonShowHandler(button, menu, model, i);
         break;
       }
     }
@@ -466,7 +467,7 @@ void MenuGtk::OnMenuButtonPressed(GtkMenuItem* menu_item, int command_id,
   menus::ButtonMenuItemModel* model =
       reinterpret_cast<menus::ButtonMenuItemModel*>(
           g_object_get_data(G_OBJECT(menu_item), "button-model"));
-  if (model) {
+  if (model && model->IsCommandIdEnabled(command_id)) {
     if (menu->delegate_)
       menu->delegate_->CommandWillBeExecuted();
 
