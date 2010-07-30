7d4bf19dcd19a2738d280ff7c645f866db3fdf9c
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-30 14:16:57 +0000

    Work around multiple bugs in the WrenchMenuControllerTest using a bit of hackery.
    BUG=49206
    TEST=Unit tests pass regularly.
    
    Review URL: http://codereview.chromium.org/3017044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54312 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/wrench_menu_controller.mm
+++ chrome/browser/cocoa/wrench_menu_controller.mm
@@ -14,9 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/wrench_menu_model.h"
 
 @interface WrenchMenuController (Private)
-- (WrenchMenuModel*)wrenchMenuModel;
 - (void)adjustPositioning;
-- (void)dispatchCommandInternal:(NSNumber*)tag;
+- (void)performCommandDispatch:(NSNumber*)tag;
 @end
 
 @implementation WrenchMenuController
@@ -105,16 +104,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // TODO(rsesek): It'd be great if the zoom buttons didn't have to close the
   // menu. See http://crbug.com/48679 for more info.
   [menu_ cancelTracking];
+  [self dispatchCommandInternal:tag];
+}
+
+- (void)dispatchCommandInternal:(NSInteger)tag {
   // Executing certain commands from the nested run loop of the menu can lead
   // to wonky behavior (e.g. http://crbug.com/49716). To avoid this, schedule
   // the dispatch on the outermost run loop.
-  [self performSelector:@selector(dispatchCommandInternal:)
+  [self performSelector:@selector(performCommandDispatch:)
              withObject:[NSNumber numberWithInt:tag]
              afterDelay:0.0];
 }
 
 // Used to perform the actual dispatch on the outermost runloop.
-- (void)dispatchCommandInternal:(NSNumber*)tag {
+- (void)performCommandDispatch:(NSNumber*)tag {
   [self wrenchMenuModel]->ExecuteCommand([tag intValue]);
 }
 
