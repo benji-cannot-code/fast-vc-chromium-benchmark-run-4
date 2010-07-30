6665f9189a25e4d50e5c8b3151b69189af21b7bc
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-30 14:16:33 +0000

    2010-07-30  Steve Block  <steveblock@google.com>
            Reviewed by Steve Block.
    
            Add LayoutTestController methods to test DeviceOrientation
            https://bugs.webkit.org/show_bug.cgi?id=39589
    
            * fast/dom/DeviceOrientation/basic-operation-expected.txt: Added.
            * fast/dom/DeviceOrientation/basic-operation.html: Added.
            * fast/dom/DeviceOrientation/script-tests/basic-operation.js: Added.
            * platform/gtk/Skipped:
    2010-07-30  Steve Block  <steveblock@google.com>
    
            Reviewed by Steve Block.
    
            Add LayoutTestController methods to test DeviceOrientation
            https://bugs.webkit.org/show_bug.cgi?id=39589
    
            This patch does not hook up the new LayoutTestController method to WebKit
            for any platform. This will be done in later patches.
            https://bugs.webkit.org/show_bug.cgi?id=43181 tracks this for Mac.
    
            Test: fast/dom/DeviceOrientation/basic-operation.html
    
            * Android.mk:
            * CMakeLists.txt:
            * GNUmakefile.am:
            * WebCore.exp.in:
            * WebCore.gypi:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * dom/DeviceOrientationClient.h:
            (WebCore::DeviceOrientationClient::~DeviceOrientationClient):
            * dom/DeviceOrientationController.cpp:
            (WebCore::DeviceOrientationController::DeviceOrientationController):
            * dom/DeviceOrientationEvent.cpp:
            * platform/mock/DeviceOrientationClientMock.cpp: Added.
            (WebCore::DeviceOrientationClientMock::DeviceOrientationClientMock):
            (WebCore::DeviceOrientationClientMock::setController):
            (WebCore::DeviceOrientationClientMock::startUpdating):
            (WebCore::DeviceOrientationClientMock::stopUpdating):
            (WebCore::DeviceOrientationClientMock::setOrientation):
            (WebCore::DeviceOrientationClientMock::timerFired):
            * platform/mock/DeviceOrientationClientMock.h: Added.
            (WebCore::DeviceOrientationClientMock::lastOrientation):
    2010-07-30  Steve Block  <steveblock@google.com>
    
            Reviewed by Steve Block.
    
            Add LayoutTestController methods to test DeviceOrientation
            https://bugs.webkit.org/show_bug.cgi?id=39589
    
            * DumpRenderTree/LayoutTestController.cpp:
            (setMockDeviceOrientationCallback):
            (LayoutTestController::staticFunctions):
            * DumpRenderTree/LayoutTestController.h:
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::LayoutTestController):
            (LayoutTestController::setMockDeviceOrientation):
            * DumpRenderTree/chromium/LayoutTestController.h:
            * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
            (LayoutTestController::setMockDeviceOrientation):
            * DumpRenderTree/mac/LayoutTestControllerMac.mm:
            (LayoutTestController::setMockDeviceOrientation):
            * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
            (LayoutTestController::setMockDeviceOrientation):
            * DumpRenderTree/qt/LayoutTestControllerQt.h:
            * DumpRenderTree/win/LayoutTestControllerWin.cpp:
            (LayoutTestController::setMockDeviceOrientation):
            * DumpRenderTree/wx/LayoutTestControllerWx.cpp:
            (LayoutTestController::setMockDeviceOrientation):
            * Scripts/build-webkit:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/WebCore.gypi
+++ third_party/WebKit/WebCore/WebCore.gypi
@@ -3013,6 +3013,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'platform/Logging.h',
             'platform/MIMETypeRegistry.cpp',
             'platform/MIMETypeRegistry.h',
+            'platform/mock/DeviceOrientationClientMock.cpp',
+            'platform/mock/DeviceOrientationClientMock.h',
             'platform/mock/GeolocationServiceMock.cpp',
             'platform/mock/GeolocationServiceMock.h',
             'platform/mock/SpeechInputClientMock.cpp',
--- third_party/WebKit/WebCore/dom/DeviceOrientationClient.h
+++ third_party/WebKit/WebCore/dom/DeviceOrientationClient.h
@@ -30,15 +30,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class DeviceOrientation;
+class DeviceOrientationController;
 
 class DeviceOrientationClient {
 public:
+    virtual ~DeviceOrientationClient() {}
+
+    virtual void setController(DeviceOrientationController*) = 0;
     virtual void startUpdating() = 0;
     virtual void stopUpdating() = 0;
     virtual DeviceOrientation* lastOrientation() const = 0;
-
-protected:
-    virtual ~DeviceOrientationClient() {}
 };
 
 } // namespace WebCore
