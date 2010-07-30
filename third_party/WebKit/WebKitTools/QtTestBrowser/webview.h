88f28f889c9339543a3e6304711a9897bef7db3e
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-30 13:31:53 +0000

    2010-07-29  Antonio Gomes  <tonikitoo@webkit.org>
            Reviewed by Kenneth Christiansen.
    
            [Qt] QtTestBrowser: -resizes-to-contents command line parameter is broken
            https://bugs.webkit.org/show_bug.cgi?id=43209
    
            When -resizes-to-content was being passed as a command line parameter it was simply
            not being set (although the corresponding menu item was marked as ON).
            User had to toggle the menu OFF and then ON again for it to take place.
    
            Reason: LauncherWindow::applyPrefs method sets many user options passed in from
            the command line, but not resizesToContents. Patch addresses that.
    
            * QtTestBrowser/launcherwindow.cpp:
            (LauncherWindow::createChrome):
            (LauncherWindow::applyPrefs):
            (LauncherWindow::toggleResizesToContents):
            * QtTestBrowser/webview.h:
            (WebViewGraphicsBased::resizesToContents):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/QtTestBrowser/webview.h
+++ third_party/WebKit/WebKitTools/QtTestBrowser/webview.h
@@ -83,6 +83,7 @@ public:
     virtual void paintEvent(QPaintEvent* event);
 
     void setResizesToContents(bool b);
+    bool resizesToContents() const { return m_resizesToContents; }
 
     void setYRotation(qreal angle)
     {
