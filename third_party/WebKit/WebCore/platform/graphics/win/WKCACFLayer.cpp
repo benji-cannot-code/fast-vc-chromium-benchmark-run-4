ae1181f737a972c1f33006b40ee3c10ac80d5733
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-30 17:37:26 +0000

    Remove knowledge of WKCACFContextFlusher from WKCACFLayer
    Fixes <http://webkit.org/b/43248> WKCACFLayer shouldn't know about
    WKCACFContextFlusher
    
    Reviewed by Sam Weinig.
    
    * platform/graphics/win/WKCACFLayer.cpp:
    (WebCore::WKCACFLayer::setNeedsCommit): Don't bother calling to
    WKCACFContextFlusher. Our root layer will do this for us.
    
    * platform/graphics/win/WKCACFLayerRenderer.cpp:
    (WebCore::WKCACFRootLayer::setNeedsRender): Changed to call the new
    layerTreeDidChange function.
    (WebCore::WKCACFLayerRenderer::layerTreeDidChange): Added. Tells
    WKCACFContextFlusher that the context has changed, and schedules a
    render.
    
    * platform/graphics/win/WKCACFLayerRenderer.h: Added
    layerTreeDidChange.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayer.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WKCACFLayer.h"
 
-#include "WKCACFContextFlusher.h"
 #include "WKCACFLayerRenderer.h"
 #include <wtf/text/CString.h>
 
@@ -176,15 +175,6 @@ void WKCACFLayer::becomeRootLayerForContext(CACFContextRef context)
 void WKCACFLayer::setNeedsCommit()
 {
     WKCACFLayer* root = rootLayer();
-
-    CACFContextRef context = CACFLayerGetContext(root->layer());
-
-    // The context might now be set yet. This happens if a property gets set
-    // before placing the layer in the tree. In this case we don't need to 
-    // worry about remembering the context because we will when the layer is
-    // added to the tree.
-    if (context)
-        WKCACFContextFlusher::shared().addContext(context);
 
     // Call setNeedsRender on the root layer, which will cause a render to 
     // happen in WKCACFLayerRenderer
--- third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayerRenderer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayerRenderer.cpp
@@ -99,7 +99,7 @@ public:
         return adoptRef(new WKCACFRootLayer(renderer));
     }
 
-    virtual void setNeedsRender() { m_renderer->renderSoon(); }
+    virtual void setNeedsRender() { m_renderer->layerTreeDidChange(); }
 
     // Overload this to avoid calling setNeedsDisplay on the layer, which would override the contents
     // we have placed on the root layer.
@@ -334,6 +334,12 @@ void WKCACFLayerRenderer::setRootChildLayer(WKCACFLayer* layer)
     }
 }
    
+void WKCACFLayerRenderer::layerTreeDidChange()
+{
+    WKCACFContextFlusher::shared().addContext(m_context.get());
+    renderSoon();
+}
+
 void WKCACFLayerRenderer::setNeedsDisplay()
 {
     ASSERT(m_rootLayer);
