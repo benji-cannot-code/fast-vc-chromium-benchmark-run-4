eb5b9451aedbd217a9d9050e5f0e7e6e688292ad
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-30 13:19:26 +0000

    2010-07-30  Satish Sampath  <satish@chromium.org>
            Reviewed by Jeremy Orlow.
    
            Add a check for WebViewClient being null.
            https://bugs.webkit.org/show_bug.cgi?id=43240
    
            * src/SpeechInputClientImpl.cpp:
            (WebKit::SpeechInputClientImpl::SpeechInputClientImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64352 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/SpeechInputClientImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/SpeechInputClientImpl.cpp
@@ -43,14 +43,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 SpeechInputClientImpl::SpeechInputClientImpl(WebViewClient* web_view_client)
-    : m_controller(web_view_client->speechInputController(this))
+    : m_controller(web_view_client ? web_view_client->speechInputController(this) : 0)
     , m_listener(0)
 {
-    // FIXME: Right now WebViewClient gives a null pointer, and with the
-    // runtime flag for speech input feature set to true by default this will
-    // always assert. Enable this assert once the WebViewClient starts to
-    // give a valid pointer.
-    // ASSERT(m_controller);
 }
 
 SpeechInputClientImpl::~SpeechInputClientImpl()
