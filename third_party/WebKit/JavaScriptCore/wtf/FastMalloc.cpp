915f7dfb9298b4ac45be39cd14cde058cb0dc736
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-30 01:27:24 +0000

    2010-07-29  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r64302.
            http://trac.webkit.org/changeset/64302
            https://bugs.webkit.org/show_bug.cgi?id=43223
    
            Assertion is bogus (Requested by olliej on #webkit).
    
            * assembler/ARMAssembler.cpp:
            (JSC::ARMAssembler::executableCopy):
            * assembler/AssemblerBuffer.h:
            (JSC::AssemblerBuffer::putShortUnchecked):
            (JSC::AssemblerBuffer::putIntUnchecked):
            (JSC::AssemblerBuffer::putInt64Unchecked):
            * jit/JITStubs.cpp:
            * pcre/pcre_compile.cpp:
            (jsRegExpCompile):
            * wtf/FastMalloc.cpp:
            (WTF::PageHeapAllocator::New):
            (WTF::TCMalloc_Central_FreeList::Populate):
            * wtf/MD5.cpp:
            (WTF::reverseBytes):
            (WTF::MD5::addBytes):
            (WTF::MD5::checksum):
            * wtf/StdLibExtras.h:
            * wtf/Vector.h:
            (WTF::VectorBuffer::inlineBuffer):
            * wtf/qt/StringQt.cpp:
            (WebCore::String::String):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64327 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/FastMalloc.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/FastMalloc.cpp
@@ -83,7 +83,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(JSC_MULTIPLE_THREADS)
 #include <pthread.h>
 #endif
-#include <wtf/StdLibExtras.h>
 
 #ifndef NO_TCMALLOC_SAMPLES
 #ifdef WTF_CHANGES
@@ -1017,7 +1016,7 @@ class PageHeapAllocator {
         if (!new_allocation)
           CRASH();
 
-        *reinterpret_cast_ptr<void**>(new_allocation) = allocated_regions_;
+        *(void**)new_allocation = allocated_regions_;
         allocated_regions_ = new_allocation;
         free_area_ = new_allocation + kAlignedSize;
         free_avail_ = kAllocIncrement - kAlignedSize;
@@ -2712,7 +2711,7 @@ ALWAYS_INLINE void TCMalloc_Central_FreeList::Populate() {
   char* nptr;
   while ((nptr = ptr + size) <= limit) {
     *tail = ptr;
-    tail = reinterpret_cast_ptr<void**>(ptr);
+    tail = reinterpret_cast<void**>(ptr);
     ptr = nptr;
     num++;
   }
