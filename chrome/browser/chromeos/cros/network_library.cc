e69058123bab8b148b24bec2342317aec32924c0
chocobo@chromium.org <chocobo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-30 19:07:04 +0000

    Add new flimflam error codes. BUG=chromium-os:1078 TEST=none Review URL: http://codereview.chromium.org/3018029
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54351 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/network_library.cc
+++ chrome/browser/chromeos/cros/network_library.cc
@@ -133,8 +133,12 @@ std::string Network::GetErrorString() {
       return "DHCP Failed";
     case ERROR_CONNECT_FAILED:
       return "Connect Failed";
+    case ERROR_BAD_PASSPHRASE:
+      return "Bad Passphrase";
+    case ERROR_BAD_WEPKEY:
+      return "Bad WEP Key";
   }
-  return "";
+  return "Unknown Error";
 }
 
 ////////////////////////////////////////////////////////////////////////////////
