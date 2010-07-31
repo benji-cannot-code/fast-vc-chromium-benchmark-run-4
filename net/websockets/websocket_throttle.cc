8f897e9e3cf28095852eaa98ef2c45c897212af4
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-31 18:21:05 +0000

    Add string_tuil include back, neede for stringprintf.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54459 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_throttle.cc
+++ net/websockets/websocket_throttle.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ref_counted.h"
 #include "base/singleton.h"
 #include "base/string_number_conversions.h"
+#include "base/string_util.h"
 #include "net/base/io_buffer.h"
 #include "net/base/sys_addrinfo.h"
 #include "net/socket_stream/socket_stream.h"
