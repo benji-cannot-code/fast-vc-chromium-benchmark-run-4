c0a33b7bcc71d69c02d409cb4a16ba03002d1933
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-31 23:10:07 +0000

    Fix bad license header in app/ and net/ directories.
    BUG=50266
    TEST=None
    
    Review URL: http://codereview.chromium.org/3013049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54482 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_ctrl_response_buffer.h
+++ net/ftp/ftp_ctrl_response_buffer.h
@@ -1,7 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.  Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 
 #ifndef NET_FTP_FTP_CTRL_RESPONSE_BUFFER_H_
 #define NET_FTP_FTP_CTRL_RESPONSE_BUFFER_H_
@@ -26,8 +27,7 @@ struct FtpCtrlResponse {
 
 class FtpCtrlResponseBuffer {
  public:
-  FtpCtrlResponseBuffer() : multiline_(false) {
-  }
+  FtpCtrlResponseBuffer() : multiline_(false) {}
 
   // Called when data is received from the control socket. Returns error code.
   int ConsumeData(const char* data, int data_length);
