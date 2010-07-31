c6cefe61e31f32a6b05c03bb84bf53d736e4a1b0
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-31 17:47:05 +0000

    Fix compile errors from moving number conversions to their own file.
    TEST=none
    BUG=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54455 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_focus_uitest.cc
+++ chrome/browser/browser_focus_uitest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/path_service.h"
 #include "base/ref_counted.h"
+#include "base/string_util.h"
 #include "chrome/browser/automation/ui_controls.h"
 #include "chrome/browser/browser.h"
 #include "chrome/browser/browser_window.h"
