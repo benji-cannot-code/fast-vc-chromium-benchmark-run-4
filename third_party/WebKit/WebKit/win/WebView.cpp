aa217563a47daa07ca2b00a6eae65b580587ac8b
luiz@webkit.org <luiz@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-07-31 23:43:23 +0000

    2010-07-31  Luiz Agostini  <luiz.agostini@openbossa.org>
            Build fix: Windows.
    
            * platform/win/PopupMenuWin.h:
    2010-07-31  Luiz Agostini  <luiz.agostini@openbossa.org>
    
            Build fix: Windows.
    
            * WebView.cpp:
            (WebView::mouseWheel):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64429 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebView.cpp
+++ third_party/WebKit/WebKit/win/WebView.cpp
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/PluginDatabase.h>
 #include <WebCore/PluginView.h>
 #include <WebCore/PopupMenu.h>
+#include <WebCore/PopupMenuWin.h>
 #include <WebCore/ProgressTracker.h>
 #include <WebCore/RenderLayer.h>
 #include <WebCore/RenderTheme.h>
@@ -1614,9 +1615,9 @@ bool WebView::mouseWheel(WPARAM wParam, LPARAM lParam, bool isMouseHWheel)
         TCHAR className[256];
 
         // Make sure truncation won't affect the comparison.
-        ASSERT(ARRAYSIZE(className) > _tcslen(PopupMenu::popupClassName()));
+        ASSERT(ARRAYSIZE(className) > _tcslen(PopupMenuWin::popupClassName()));
 
-        if (GetClassName(focusedWindow, className, ARRAYSIZE(className)) && !_tcscmp(className, PopupMenu::popupClassName())) {
+        if (GetClassName(focusedWindow, className, ARRAYSIZE(className)) && !_tcscmp(className, PopupMenuWin::popupClassName())) {
             // We don't let the WebView scroll here for two reasons - 1) To match Firefox behavior, 2) If we do scroll, we lose the
             // focus ring around the select menu.
             SetFocus(m_viewWindow);
