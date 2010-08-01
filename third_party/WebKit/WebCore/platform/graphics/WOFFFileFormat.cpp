bd6ff38f34893d56190cde9530158cb462810d24
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-01 06:12:12 +0000

    Try to fix the Windows build when zlib is not available.
    * platform/graphics/WOFFFileFormat.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64439 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/WOFFFileFormat.cpp
+++ third_party/WebKit/WebCore/platform/graphics/WOFFFileFormat.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef unsigned char Bytef;
 typedef unsigned long uLong;
 typedef unsigned long uLongf;
+#define Z_OK 0
 
 SOFT_LINK_LIBRARY(zlib1);
 SOFT_LINK(zlib1, uncompress, int, __cdecl, (Bytef *dest, uLongf *destLen, const Bytef *source, uLong sourceLen), (dest, destLen, source, sourceLen));
