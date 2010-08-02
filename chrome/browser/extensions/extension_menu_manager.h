52c80e4a5a21d9cfe4fcaeac29b1a8cb6851576b
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-02 23:13:04 +0000

    Make sure we fully remove child context menu items when deleting a parent.
    BUG=49742
    TEST=Follow steps in the bug report
    
    Review URL: http://codereview.chromium.org/3081009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54628 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_menu_manager.h
+++ chrome/browser/extensions/extension_menu_manager.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/gtest_prod_util.h"
 #include "base/scoped_ptr.h"
 #include "base/string16.h"
 #include "chrome/browser/extensions/extension_icon_manager.h"
@@ -135,10 +136,6 @@ class ExtensionMenuItem {
   // Takes ownership of |item| and sets its parent_id_.
   void AddChild(ExtensionMenuItem* item);
 
-  // Removes child menu item with the given id, returning true if the item was
-  // found and removed, or false otherwise.
-  bool RemoveChild(const Id& child_id);
-
   // Takes the child item from this parent. The item is returned and the caller
   // then owns the pointer.
   ExtensionMenuItem* ReleaseChild(const Id& child_id, bool recursive);
@@ -248,6 +245,8 @@ class ExtensionMenuManager : public NotificationObserver {
   static bool HasAllowedScheme(const GURL& url);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(ExtensionMenuManagerTest, DeleteParent);
+
   // This is a helper function which takes care of de-selecting any other radio
   // items in the same group (i.e. that are adjacent in the list).
   void RadioItemSelected(ExtensionMenuItem* item);
