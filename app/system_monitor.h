b0dd6a7f75ca57f558438d7499b4151511cd6b3c
ziadh@chromium.org <ziadh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-02 16:34:16 +0000

    Add undeclared virtual destructors part 5
    Preventative maintenance for abstract classes that do not declare virtual
    destructors. Base classes that do not declare their destructors as virtual
    could potentially lead to memory leaks.
    
    These files were discovered using the -Wnon-virtual-dtor flag in g++.
    
    r=jar
    BUG=47469
    
    Review URL: http://codereview.chromium.org/3080013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54540 0039d316-1c4b-4281-b951-d872f2087c98

--- app/system_monitor.h
+++ app/system_monitor.h
@@ -69,6 +69,9 @@ class SystemMonitor {
 
     // Notification that the system is resuming.
     virtual void OnResume() {}
+
+   protected:
+    virtual ~PowerObserver() {}
   };
 
   // Add a new observer.
