5de7cbcc9ef08179fa6a25f6241cf81b580f6d1c
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-02 21:01:25 +0000

    2010-08-02  Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] Style cleanup for CairoPath.h
            https://bugs.webkit.org/show_bug.cgi?id=43133
    
            Turn CairoPath into a class, as it has a constructor and destructor.
            Change raw member access to use the new accessor.
    
            No new tests as functionality has not changed.
    
            * platform/graphics/cairo/CairoPath.h: Cleanup and indentation fix.
            (WebCore::CairoPath::CairoPath):
            (WebCore::CairoPath::~CairoPath):
            (WebCore::CairoPath::context):
            * platform/graphics/cairo/GraphicsContextCairo.cpp:
            (WebCore::GraphicsContext::addPath): Convert raw member access to accessor access.
            (WebCore::GraphicsContext::clip): Ditto.
            * platform/graphics/cairo/PathCairo.cpp:
            (WebCore::Path::Path): Ditto.
            (WebCore::Path::operator=): Ditto.
            (WebCore::Path::clear): Ditto.
            (WebCore::Path::isEmpty): Ditto.
            (WebCore::Path::currentPoint): Ditto.
            (WebCore::Path::translate): Ditto.
            (WebCore::Path::moveTo): Ditto.
            (WebCore::Path::addLineTo): Ditto.
            (WebCore::Path::addRect): Ditto.
            (WebCore::Path::addQuadCurveTo): Ditto.
            (WebCore::Path::addBezierCurveTo): Ditto.
            (WebCore::Path::addArc): Ditto.
            (WebCore::Path::addArcTo): Ditto.
            (WebCore::Path::addEllipse): Ditto.
            (WebCore::Path::closeSubpath): Ditto.
            (WebCore::Path::boundingRect): Ditto.
            (WebCore::Path::strokeBoundingRect): Ditto.
            (WebCore::Path::contains): Ditto.
            (WebCore::Path::strokeContains): Ditto.
            (WebCore::Path::apply): Ditto.
            (WebCore::Path::transform): Ditto.
            (WebCore::Path::debugString): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64490 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cairo/CairoPath.h
+++ third_party/WebKit/WebCore/platform/graphics/cairo/CairoPath.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
     Copyright (C) 2007 Alp Toker <alp.toker@collabora.co.uk>
+    Copyright (C) 2010 Igalia S.L.
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -25,21 +26,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    // This is necessary since cairo_path_fixed_t isn't exposed in Cairo's public API.
-    struct CairoPath {
-        cairo_t* m_cr;
+// This is necessary since cairo_path_fixed_t isn't exposed in Cairo's public API.
+class CairoPath {
+public:
+    CairoPath()
+    {
+        static cairo_surface_t* pathSurface = cairo_image_surface_create(CAIRO_FORMAT_A8, 1, 1);
+        m_cr = cairo_create(pathSurface);
+    }
 
-        CairoPath()
-        {
-            static cairo_surface_t* pathSurface = cairo_image_surface_create(CAIRO_FORMAT_A8, 1, 1);
-            m_cr = cairo_create(pathSurface);
-        }
+    ~CairoPath()
+    {
+        cairo_destroy(m_cr);
+    }
 
-        ~CairoPath()
-        {
-            cairo_destroy(m_cr);
-        }
-    };
+    cairo_t* context() { return m_cr; }
+
+private:
+    cairo_t* m_cr;
+};
 
 } // namespace WebCore
 
