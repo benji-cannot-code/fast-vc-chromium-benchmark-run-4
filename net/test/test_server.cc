62ad67f4faff96700b3ef4c35c91a952c80b7d27
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-02 19:13:11 +0000

    Run tests faster.
    I noticed that net_unittests was spending a lot of time doing nothing.
    This CL changes the timeouts so that we can startup and shutdown
    test servers faster.
    
    BUG=none
    TEST=net_unittests
    
    http://codereview.chromium.org/3057021/show
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54576 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/test_server.cc
+++ net/test/test_server.cc
@@ -37,11 +37,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Number of connection attempts for tests.
-const int kServerConnectionAttempts = 10;
+// Number of connection attempts for tests. With a timeout of 100ms, this is 60
+// seconds.
+const int kServerConnectionAttempts = 600;
 
 // Connection timeout in milliseconds for tests.
-const int kServerConnectionTimeoutMs = 1000;
+const int kServerConnectionTimeoutMs = 100;
 
 }  // namespace
 
