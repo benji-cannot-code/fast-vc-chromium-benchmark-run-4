6080df5752a564c2eecb3e53ec11edcd5b76c39a
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-02 02:03:18 +0000

    2010-08-01  Kent Tamura  <tkent@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            [DRT/Chromium] Remove string_util.h dependency
            https://bugs.webkit.org/show_bug.cgi?id=43312
    
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::pathToLocalResource):
             Use string::find() instead of StartsWithASCII().
            (LayoutTestController::cppVariantToInt32):
             Use strtol() instead of StringToNumber().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64451 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/LayoutTestController.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/LayoutTestController.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DRTDevToolsAgent.h"
 #include "TestShell.h"
 #include "WebViewHost.h"
-#include "base/string_util.h"
 #include "public/WebAnimationController.h"
 #include "public/WebConsoleMessage.h"
 #include "public/WebDocument.h"
@@ -52,6 +51,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/WebURL.h"
 #include "public/WebView.h"
 #include "webkit/support/webkit_support.h"
+#include <algorithm>
+#include <cstdlib>
+#include <limits>
 #include <wtf/text/WTFString.h>
 
 #if OS(WINDOWS)
@@ -705,7 +707,7 @@ void LayoutTestController::pathToLocalResource(const CppArgumentList& arguments,
 
     string url = arguments[0].toString();
 #if OS(WINDOWS)
-    if (StartsWithASCII(url, "/tmp/", true)) {
+    if (!url.find("/tmp/")) {
         // We want a temp file.
         const unsigned tempPrefixLength = 5;
         size_t bufferSize = MAX_PATH;
@@ -717,7 +719,7 @@ void LayoutTestController::pathToLocalResource(const CppArgumentList& arguments,
             tempLength = GetTempPathW(bufferSize, tempPath.get());
             ASSERT(tempLength < bufferSize);
         }
-        std::string resultPath(WebString(tempPath.get(), tempLength).utf8());
+        string resultPath(WebString(tempPath.get(), tempLength).utf8());
         resultPath.append(url.substr(tempPrefixLength));
         result->set(resultPath);
         return;
@@ -726,8 +728,12 @@ void LayoutTestController::pathToLocalResource(const CppArgumentList& arguments,
 
     // Some layout tests use file://// which we resolve as a UNC path.  Normalize
     // them to just file:///.
-    while (StartsWithASCII(url, "file:////", false))
+    string lowerUrl = url;
+    transform(lowerUrl.begin(), lowerUrl.end(), lowerUrl.begin(), ::tolower);
+    while (!lowerUrl.find("file:////")) {
         url = url.substr(0, 8) + url.substr(9);
+        lowerUrl = lowerUrl.substr(0, 8) + lowerUrl.substr(9);
+    }
     result->set(webkit_support::RewriteLayoutTestsURL(url).spec());
 }
 
@@ -1025,9 +1031,12 @@ int32_t LayoutTestController::cppVariantToInt32(const CppVariant& value)
     if (value.isInt32())
         return value.toInt32();
     if (value.isString()) {
-        int number;
-        if (StringToInt(value.toString(), &number))
-            return number;
+        string stringSource = value.toString();
+        const char* source = stringSource.data();
+        char* end;
+        long number = strtol(source, &end, 10);
+        if (end == source + stringSource.length() && number >= numeric_limits<int32_t>::min() && number <= numeric_limits<int32_t>::max())
+            return static_cast<int32_t>(number);
     }
     logErrorToConsole("Invalid value for preference. Expected integer value.");
     return 0;
