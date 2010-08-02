3c061e8b0ea9ee0704ae71658abaf7e3c45b5865
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-02 20:41:29 +0000

    Fix ChromeFrame context menu tests which rely on the IE active document window having focus. This regressed recently with the change to fix focus recycling issues in ChromeFrame while switching tabs.
    Review URL: http://codereview.chromium.org/3067023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54596 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_active_document.cc
+++ chrome_frame/chrome_active_document.cc
@@ -1220,6 +1220,7 @@ LRESULT ChromeActiveDocument::OnShowWindow(UINT message, WPARAM wparam,
 LRESULT ChromeActiveDocument::OnSetFocus(UINT message, WPARAM wparam,
                                          LPARAM lparam,
                                          BOOL& handled) {  // NO_LINT
-  GiveFocusToChrome(false);
+  if (!ignore_setfocus_)
+    GiveFocusToChrome(false);
   return 0;
 }
