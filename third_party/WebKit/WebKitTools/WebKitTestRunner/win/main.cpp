bff5808169f5d5379fdebe9d06c07d01b9ff563f
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-02 21:27:00 +0000

    Fix the windows build.
    * WebKitTestRunner/win/main.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64493 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/WebKitTestRunner/win/main.cpp
+++ third_party/WebKit/WebKitTools/WebKitTestRunner/win/main.cpp
@@ -28,8 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 int main(int argc, const char* argv[])
 {
-    WTR::TestController::shared().initialize(argc, argv);
-    WTR::TestController::shared().run();
+    {
+        WTR::TestController controller(argc, argv);
+    }
 
     return 0;
 }
