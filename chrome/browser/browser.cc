fac84fd679549d716adb9977a1c3c070fe719957
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 11:46:51 +0000

    Only close a newly opened download tab if there's more than one tab.
    BUG=45464,10764
    TEST=See first bug for manual test.
    
    Review URL: http://codereview.chromium.org/3029040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54737 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -2820,7 +2820,7 @@ int Browser::GetExtraRenderViewHeight() const {
   return window_->GetExtraRenderViewHeight();
 }
 
-void Browser::OnStartDownload(DownloadItem* download) {
+void Browser::OnStartDownload(DownloadItem* download, TabContents* tab) {
   if (!window())
     return;
 
@@ -2858,6 +2858,12 @@ void Browser::OnStartDownload(DownloadItem* download) {
     DownloadStartedAnimation::Show(current_tab);
   }
 #endif
+
+  // If the download occurs in a new tab, close it
+  if (tab->controller().IsInitialNavigation() &&
+      GetConstrainingContents(tab) == tab && tab_count() > 1) {
+    CloseContents(tab);
+  }
 }
 
 void Browser::ConfirmAddSearchProvider(const TemplateURL* template_url,
