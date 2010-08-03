bd177bdfe48aaf5074b90f856f2b00908cb64c21
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-03 18:10:51 +0000

    Reviewed by Kevin Ollivier.
    Fix crash during HitTest call.
    https://bugs.webkit.org/show_bug.cgi?id=43372
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/wx/WebView.cpp
+++ third_party/WebKit/WebKit/wx/WebView.cpp
@@ -236,7 +236,9 @@ wxWebViewDOMElementInfo::wxWebViewDOMElementInfo() :
     m_isSelected(false),
     m_text(wxEmptyString),
     m_imageSrc(wxEmptyString),
-    m_link(wxEmptyString)
+    m_link(wxEmptyString),
+    m_urlElement(NULL),
+    m_innerNode(NULL)
 {
 }
 
