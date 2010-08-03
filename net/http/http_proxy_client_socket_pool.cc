a8e956a96ed5058d6d0ed3c7f5aed4fde5882642
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 07:38:59 +0000

    Recommit 54405 - Fix late binding induced mismatch of Socket and AuthController
    ClientSocketPool treats all pending SocketParams as interchangeable. Therefore they can not contain any connection specific data. This only affects the Http Proxy tunnel case. The lowest risk change to fix this problem is to create the HttpAuthController in the HttpProxyClientSocket. If we get a 407 and need to restart the Tunnel, the pending HttpProxyClientSocket is returned to the HttpNetworkTransaction in the additional error state of the connection and then complete the auth in a pair of states in the HttpNetworkTransaction. This reintroduces a dependency between tunnel setup and the HttpNetworkTransaction, but that will need to be fixed at a later date.
    
    Original Review URL: http://codereview.chromium.org/3058013
    
    BUG=49493
    TEST=existing unit tests + manually visiting many SSL sites through a kerberized http proxy.
    
    Review URL: http://codereview.chromium.org/3029052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54714 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_proxy_client_socket_pool.cc
+++ net/http/http_proxy_client_socket_pool.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_errors.h"
+#include "net/http/http_network_session.h"
 #include "net/http/http_proxy_client_socket.h"
 #include "net/socket/client_socket_factory.h"
 #include "net/socket/client_socket_handle.h"
@@ -19,12 +20,12 @@ HttpProxySocketParams::HttpProxySocketParams(
     const scoped_refptr<TCPSocketParams>& proxy_server,
     const GURL& request_url,
     HostPortPair endpoint,
-    scoped_refptr<HttpAuthController> auth_controller,
+    scoped_refptr<HttpNetworkSession> session,
     bool tunnel)
     : tcp_params_(proxy_server),
       request_url_(request_url),
       endpoint_(endpoint),
-      auth_controller_(auth_controller),
+      session_(session),
       tunnel_(tunnel) {
 }
 
@@ -134,19 +135,22 @@ int HttpProxyConnectJob::DoTCPConnectComplete(int result) {
 
 int HttpProxyConnectJob::DoHttpProxyConnect() {
   next_state_ = kStateHttpProxyConnectComplete;
+  const HostResolver::RequestInfo& tcp_destination =
+      params_->tcp_params()->destination();
+  HostPortPair proxy_server(tcp_destination.hostname(),
+                            tcp_destination.port());
 
   // Add a HttpProxy connection on top of the tcp socket.
   socket_.reset(new HttpProxyClientSocket(tcp_socket_handle_.release(),
                                           params_->request_url(),
                                           params_->endpoint(),
-                                          params_->auth_controller(),
+                                          proxy_server,
+                                          params_->session(),
                                           params_->tunnel()));
   return socket_->Connect(&callback_);
 }
 
 int HttpProxyConnectJob::DoHttpProxyConnectComplete(int result) {
-  DCHECK_NE(result, ERR_RETRY_CONNECTION);
-
   if (result == OK || result == ERR_PROXY_AUTH_REQUESTED)
       set_socket(socket_.release());
 
