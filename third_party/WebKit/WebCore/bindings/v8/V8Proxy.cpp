ecf80e7256f72b9bfceecc1e76c4af59d215bd62
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-03 02:25:16 +0000

    2010-08-02  Rajiv Makhijani  <rajivmakhijani@chromium.org>
            Reviewed by Adam Barth.
    
            Committed by Dirk Pranke.
    
            Added missing "if (window.layoutTestController)" check.
            Removed expectation for test case that should now pass with this bugfix.
    
            https://bugs.webkit.org/show_bug.cgi?id=38705
    
            * http/tests/security/sandbox-inherit-to-initial-document-2.html:
            * platform/chromium/test_expectations.txt:
    2010-08-02  Rajiv Makhijani  <rajivmakhijani@chromium.org>
    
            Reviewed by Adam Barth.
    
            Committed by Dirk Pranke.
    
            Bug 38705 - [v8] chromium fails http/tests/sandbox-inherit-to-initial-document-2
            https://bugs.webkit.org/show_bug.cgi?id=38705
    
            WebCore::V8Proxy::retrieve(Frame* frame) calls WebCore::ScriptController::canExecuteScripts
            and returns 0 if canExecuteScripts is false. It should return the proxy regardless
            of whether the frame can execute scripts.
    
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::retrieve): Removed canExecuteScripts check.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8Proxy.cpp
+++ third_party/WebKit/WebCore/bindings/v8/V8Proxy.cpp
@@ -628,7 +628,7 @@ V8Proxy* V8Proxy::retrieve(Frame* frame)
 {
     if (!frame)
         return 0;
-    return frame->script()->canExecuteScripts(NotAboutToExecuteScript) ? frame->script()->proxy() : 0;
+    return frame->script()->proxy();
 }
 
 V8Proxy* V8Proxy::retrieve(ScriptExecutionContext* context)
