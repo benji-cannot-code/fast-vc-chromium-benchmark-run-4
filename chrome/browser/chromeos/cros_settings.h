077454fbe9616193ff23f30c3d3b5ef7524a4c74
dhg@chromium.org <dhg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 23:50:47 +0000

    adding ability to register providers to handle particular properties
    BUG=none
    TEST=making sure the UI still works
    
    Review URL: http://codereview.chromium.org/3020047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54848 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros_settings.h
+++ chrome/browser/chromeos/cros_settings.h
@@ -8,11 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <string>
+#include <vector>
+#include "base/singleton.h"
 #include "base/values.h"
 
 #include "chrome/browser/chromeos/cros_settings_names.h"
 
 namespace chromeos {
+
+class CrosSettingsProvider;
 
 // A class manages per-device/global settings.
 class CrosSettings {
@@ -25,11 +29,11 @@ class CrosSettings {
 
   // Sets |in_value| to given |path| in cros settings.
   // Note that this takes ownership of |in_value|.
-  virtual void Set(const std::wstring& path, Value* in_value) = 0;
+  void Set(const std::wstring& path, Value* in_value);
 
   // Gets settings value of given |path| to |out_value|.
   // Note that |out_value| is still owned by this class.
-  virtual bool Get(const std::wstring& path, Value** out_value) const = 0;
+  bool Get(const std::wstring& path, Value** out_value) const;
 
   // Convenience forms of Set().  These methods will replace any existing
   // value at that path, even if it has a different type.
@@ -46,8 +50,18 @@ class CrosSettings {
   bool GetReal(const std::wstring& path, double* out_value) const;
   bool GetString(const std::wstring& path, std::string* out_value) const;
 
- protected:
-  virtual ~CrosSettings() {}
+ private:
+  // adding/removing of providers
+  bool AddProvider(CrosSettingsProvider* provider);
+  bool RemoveProvider(CrosSettingsProvider* provider);
+
+  std::vector<CrosSettingsProvider*> providers_;
+
+  CrosSettings();
+  ~CrosSettings();
+  CrosSettingsProvider* GetProvider(const std::wstring& path) const;
+  friend struct DefaultSingletonTraits<CrosSettings>;
+  DISALLOW_COPY_AND_ASSIGN(CrosSettings);
 };
 
 }  // namespace chromeos
