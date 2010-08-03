79e7d2b1375b4e947ab14e75a8d2bebf94cc8d80
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 23:15:38 +0000

    Fix build.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54834 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -234,7 +234,7 @@ void LoginUtils::DoBrowserLaunch(Profile* profile) {
   int return_code;
   browser_init.LaunchBrowser(*CommandLine::ForCurrentProcess(),
                              profile,
-                             std::wstring(),
+                             FilePath(),
                              true,
                              &return_code);
 }
