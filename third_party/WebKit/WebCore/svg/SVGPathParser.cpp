cf9374e9009855b2715c92c740323788248d0bf5
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-03 09:21:09 +0000

    2010-08-03  Nikolas Zimmermann  <nzimmermann@rim.com>
            Reviewed by Dirk Schulze.
    
            Cleanup all of svg/ code
            https://bugs.webkit.org/show_bug.cgi?id=43311
    
            Unify the license header in all files in svg/, to use the same template as most WebCore files use.
            Unified email adress style, etc. all generated with a perl script attached to bug 43311.i
    
            * svg/...: Updated header template in all files, in the svg/ subdirectory.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64539 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGPathParser.cpp
+++ third_party/WebKit/WebCore/svg/SVGPathParser.cpp
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2002, 2003 The Karbon Developers
- *               2006       Alexander Kellett <lypanov@kde.org>
- *               2006, 2007 Rob Buis <buis@kde.org>
- * Copyrigth (C) 2007, 2009 Apple, Inc.  All rights reserved.
+ * Copyright (C) 2006 Alexander Kellett <lypanov@kde.org>
+ * Copyright (C) 2006, 2007 Rob Buis <buis@kde.org>
+ * Copyright (C) 2007, 2009 Apple Inc. All rights reserved.
  * Copyright (C) Research In Motion Limited 2010. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * along with this library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
- *
  */
 
 #include "config.h"
