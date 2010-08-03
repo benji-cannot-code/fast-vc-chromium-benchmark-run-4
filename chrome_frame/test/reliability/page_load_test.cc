57fa2ed47b11a786b81356de51be98fc8301bbcb
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 15:29:29 +0000

    Fix the Windows build.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54750 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/reliability/page_load_test.cc
+++ chrome_frame/test/reliability/page_load_test.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/time_formatting.h"
 #include "base/path_service.h"
 #include "base/registry.h"
+#include "base/string_number_conversions.h"
 #include "base/string_util.h"
 #include "base/test/test_file_util.h"
 #include "base/time.h"
@@ -397,7 +398,7 @@ class PageLoadTest : public testing::Test {
   FilePath ConstructSavedDebugLogPath(const FilePath& debug_log_path,
                                       int index) {
     std::string suffix("_");
-    suffix.append(IntToString(index));
+    suffix.append(base::IntToString(index));
     return debug_log_path.InsertBeforeExtensionASCII(suffix);
   }
 
@@ -562,14 +563,14 @@ void SetPageRange(const CommandLine& parsed_command_line) {
 
   if (parsed_command_line.HasSwitch(kStartIndexSwitch)) {
     ASSERT_TRUE(
-        StringToInt(WideToUTF16(parsed_command_line.GetSwitchValue(
+        base::StringToInt(WideToUTF16(parsed_command_line.GetSwitchValue(
             kStartIndexSwitch)), &g_start_index));
     ASSERT_GT(g_start_index, 0);
   }
 
   if (parsed_command_line.HasSwitch(kEndIndexSwitch)) {
     ASSERT_TRUE(
-        StringToInt(WideToUTF16(parsed_command_line.GetSwitchValue(
+        base::StringToInt(WideToUTF16(parsed_command_line.GetSwitchValue(
             kEndIndexSwitch)), &g_end_index));
     ASSERT_GT(g_end_index, 0);
   }
@@ -581,7 +582,7 @@ void SetPageRange(const CommandLine& parsed_command_line) {
 
   if (parsed_command_line.HasSwitch(kIterationSwitch)) {
     ASSERT_TRUE(
-        StringToInt(WideToUTF16(parsed_command_line.GetSwitchValue(
+        base::StringToInt(WideToUTF16(parsed_command_line.GetSwitchValue(
             kIterationSwitch)), &g_iterations));
     ASSERT_GT(g_iterations, 0);
   }
@@ -597,7 +598,7 @@ void SetPageRange(const CommandLine& parsed_command_line) {
 
   if (parsed_command_line.HasSwitch(kTimeoutSwitch)) {
     ASSERT_TRUE(
-        StringToInt(WideToUTF16(parsed_command_line.GetSwitchValue(
+        base::StringToInt(WideToUTF16(parsed_command_line.GetSwitchValue(
             kTimeoutSwitch)), &g_timeout_ms));
     ASSERT_GT(g_timeout_ms, 0);
   }
