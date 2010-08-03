6d956ac006d96504a431a52be4b95d18b9cdb988
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 17:40:28 +0000

    Forward-declare AutocompleteMatch as a struct in autocomplete_popup_view_gtk.h
    AutocompleteMatch is a struct, and should thus be forward-declared as such.
    Otherwise, Clang warns about the mismatch, and we treat the warning as an error.
    
    BUG=49304
    TEST=None
    
    Review URL: http://codereview.chromium.org/2805086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54777 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_popup_view_gtk.h
+++ chrome/browser/autocomplete/autocomplete_popup_view_gtk.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class AutocompleteEditModel;
 class AutocompleteEditView;
-class AutocompleteMatch;
+struct AutocompleteMatch;
 class AutocompletePopupModel;
 class GtkThemeProvider;
 class Profile;
