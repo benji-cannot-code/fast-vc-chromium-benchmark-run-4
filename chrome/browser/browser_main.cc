5c44b34060ab918f91e0ea54153da143f2bba262
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 07:58:19 +0000

    [linux] Hook up crash reporter initialization with configuration management.
    BUG=49662
    TEST=Enable/Disable metrics reporting through policy and check whether the crash reporter respects that.
    
    Review URL: http://codereview.chromium.org/3084008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54716 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/env_vars.h"
 #include "chrome/common/json_pref_store.h"
 #include "chrome/common/jstemplate_builder.h"
 #include "chrome/common/main_function_params.h"
@@ -802,14 +803,6 @@ int BrowserMain(const MainFunctionParams& parameters) {
   // tabs.
   g_browser_process->tab_closeable_state_watcher();
 
-#if defined(USE_LINUX_BREAKPAD)
-  // Needs to be called after we have chrome::DIR_USER_DATA and
-  // g_browser_process.
-  g_browser_process->file_thread()->message_loop()->PostTask(FROM_HERE,
-      new GetLinuxDistroTask());
-  InitCrashReporter();
-#endif
-
   // The broker service initialization needs to run early because it will
   // initialize the sandbox broker, which requires the process to swap its
   // window station. During this time all the UI will be broken. This has to
@@ -818,6 +811,29 @@ int BrowserMain(const MainFunctionParams& parameters) {
 
   PrefService* local_state = InitializeLocalState(parsed_command_line,
                                                   is_first_run);
+
+#if defined(USE_LINUX_BREAKPAD)
+  // Needs to be called after we have chrome::DIR_USER_DATA and
+  // g_browser_process.
+  g_browser_process->file_thread()->message_loop()->PostTask(FROM_HERE,
+      new GetLinuxDistroTask());
+
+  // Check whether we should initialize the crash reporter. It may be disabled
+  // through configuration policy or user preference. The kHeadless environment
+  // variable overrides the decision, but only if the crash service is under
+  // control of the user.
+  const PrefService::Preference* metrics_reporting_enabled =
+      local_state->FindPreference(prefs::kMetricsReportingEnabled);
+  CHECK(metrics_reporting_enabled);
+  bool breakpad_enabled =
+      local_state->GetBoolean(prefs::kMetricsReportingEnabled);
+  if (!breakpad_enabled && metrics_reporting_enabled->IsUserModifiable()) {
+    breakpad_enabled = (getenv(env_vars::kHeadless) != NULL) ||
+        parsed_command_line.HasSwitch(switches::kEnableCrashReporter);
+  }
+  if (breakpad_enabled)
+    InitCrashReporter();
+#endif
 
   InitializeToolkit();  // Must happen before we try to display any UI.
 
