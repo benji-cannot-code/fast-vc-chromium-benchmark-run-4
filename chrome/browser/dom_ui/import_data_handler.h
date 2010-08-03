101df003d1458deb99c6ffd112396a039f943a00
sargrass@google.com <sargrass@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 01:34:59 +0000

    Implement import data overlay
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/3029044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54673 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/import_data_handler.h
+++ chrome/browser/dom_ui/import_data_handler.h
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/importer/importer.h"
 
 // Chrome personal stuff import data overlay UI handler.
-class ImportDataHandler : public OptionsPageUIHandler {
+class ImportDataHandler : public OptionsPageUIHandler,
+                          public ImporterHost::Observer {
  public:
   ImportDataHandler();
   virtual ~ImportDataHandler();
@@ -25,9 +26,17 @@ class ImportDataHandler : public OptionsPageUIHandler {
 
  private:
   void DetectSupportedBrowsers();
+  void ImportData(const Value* value);
 
-  // Utility class that does the actual import.
-  scoped_refptr<ImporterHost> importer_host_;
+  //Callback from ImporterHost. Close the Dialog.
+  virtual void ImportStarted();
+  virtual void ImportItemStarted(importer::ImportItem item);
+  virtual void ImportItemEnded(importer::ImportItem item);
+  virtual void ImportEnded();
+
+  // If non-null it means importing is in progress. ImporterHost takes care
+  // of deleting itself when done.import.
+  ImporterHost* importer_host_;
 
   DISALLOW_COPY_AND_ASSIGN(ImportDataHandler);
 };
