f9a37e86bc442544bd0f241254e30647f589f385
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 21:03:46 +0000

    [Options DOMUI] More content settings exceptions work
    BUG=48862
    TEST=editing image exceptions works
    
    Review URL: http://codereview.chromium.org/2883045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54818 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/content_settings_handler.h
+++ chrome/browser/dom_ui/content_settings_handler.h
@@ -33,6 +33,7 @@ class ContentSettingsHandler : public OptionsPageUIHandler {
   void SetContentFilter(const Value* value);
   void SetAllowThirdPartyCookies(const Value* value);
   void RemoveExceptions(const Value* value);
+  void SetException(const Value* value);
 
   NotificationRegistrar notification_registrar_;
 
