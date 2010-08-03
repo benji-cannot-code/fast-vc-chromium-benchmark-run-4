cff8fc7042b88a9d93af75bb6f6e3c7512f5ced2
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 20:20:50 +0000

    Reset chrome://newtab URLs when the user selects to configure it.
    BUG=50927
    TEST=Manually configure a profile with prefs::kHomepage = chrome://newtab and prefs::kHomePageIsNewTabPage = true, fire up chrome and try to configure a different homepage URL through the options dialog.
    
    Review URL: http://codereview.chromium.org/3092003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54807 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/preferences_window_controller.mm
+++ chrome/browser/cocoa/preferences_window_controller.mm
@@ -1061,10 +1061,13 @@ enum { kHomepageNewTabPage, kHomepageURL };
 // appropriate user metric.
 - (void)setNewTabPageIsHomePageIndex:(NSInteger)index {
   bool useNewTabPage = index == kHomepageNewTabPage ? true : false;
-  if (useNewTabPage)
+  if (useNewTabPage) {
     [self recordUserAction:UserMetricsAction("Options_Homepage_UseNewTab")];
-  else
+  } else {
     [self recordUserAction:UserMetricsAction("Options_Homepage_UseURL")];
+    if ([self isHomepageNewTabUIURL])
+      homepage_.SetValueIfNotManaged(std::string());
+  }
   newTabPageIsHomePage_.SetValueIfNotManaged(useNewTabPage);
 }
 
