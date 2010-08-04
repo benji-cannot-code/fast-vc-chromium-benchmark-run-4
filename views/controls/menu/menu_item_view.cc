14cb4c77e3f450b7f6a97c5cbdc24b5fa273e956
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-04 19:47:20 +0000

    Remove a deprecated wstring ToLower function.
    BUG=23581
    
    Review URL: http://codereview.chromium.org/3044043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54946 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/menu/menu_item_view.cc
+++ views/controls/menu/menu_item_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/controls/menu/menu_item_view.h"
 
 #include "app/l10n_util.h"
+#include "base/utf_string_conversions.h"
 #include "gfx/canvas.h"
 #include "grit/app_strings.h"
 #include "views/controls/button/text_button.h"
@@ -317,7 +318,7 @@ wchar_t MenuItemView::GetMnemonic() {
     if (index != std::wstring::npos) {
       if (index + 1 != title.size() && title[index + 1] != '&') {
         wchar_t char_array[1] = { title[index + 1] };
-        return l10n_util::ToLower(char_array)[0];
+        return UTF16ToWide(l10n_util::ToLower(WideToUTF16(char_array)))[0];
       }
       index++;
     }
