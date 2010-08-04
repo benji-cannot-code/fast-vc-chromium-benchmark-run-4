72824b55b7e0540de5b67b8f017d28b9e18d5f69
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-04 19:29:13 +0000

    Cleanup of unused code.
    BUG=none
    TEST=no change
    
    Review URL: http://codereview.chromium.org/3064040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54940 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/throbber_view.h
+++ chrome/browser/cocoa/throbber_view.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface ThrobberView : NSView {
  @private
   id<ThrobberDataDelegate> dataDelegate_;
-  NSTimer* timer_;  // Animation timer. Weak, owned by runloop.
 }
 
 // Creates a filmstrip view with |frame| and image |image|.
