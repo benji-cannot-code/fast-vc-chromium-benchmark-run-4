7059e3871b838d9cc0a32d6bfe43cc5ffb87db3c
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-04 18:00:51 +0000

    Revert 54795 - Initialize was_ever_connected_ in ClientSocket.
    I'm reverting 54771... so I had to revert this first.
    
    
    BUG=51079
    TEST=Built net_unittests
    Review URL: http://codereview.chromium.org/3010050
    
    TBR=cbentzel@chromium.org
    Review URL: http://codereview.chromium.org/3029057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54929 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/client_socket.cc
+++ net/socket/client_socket.cc
@@ -10,8 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 ClientSocket::ClientSocket()
-    : was_ever_connected_(false),
-      omnibox_speculation_(false),
+    : omnibox_speculation_(false),
       subresource_speculation_(false),
       was_used_to_transmit_data_(false) {}
 
