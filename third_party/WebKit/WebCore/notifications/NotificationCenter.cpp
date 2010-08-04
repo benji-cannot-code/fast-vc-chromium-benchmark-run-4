82c202f44bd2a8f3f0d6af470606572718d33e28
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-04 13:25:38 +0000

    2010-08-03  Abhishek Arya  <inferno@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            Null the script execution context when disconnecting frame in notifications.
            Make sure that script execution context is valid in notification requestPermission.
            https://bugs.webkit.org/show_bug.cgi?id=43295
    
            Tests: fast/notifications/notifications-document-close-crash.html
    
            * bindings/v8/custom/V8NotificationCenterCustom.cpp:
            (WebCore::V8NotificationCenter::requestPermissionCallback):
            * notifications/NotificationCenter.cpp:
            (WebCore::NotificationCenter::disconnectFrame):
    2010-08-03  Abhishek Arya  <inferno@chromium.org>
    
            Reviewed by Alexey Proskuryakov.
    
            Tests that requesting notification permission on a closed document does
            not result in crash.
            https://bugs.webkit.org/show_bug.cgi?id=43295
    
            * fast/notifications/notifications-document-close-crash-expected.txt: Added.
            * fast/notifications/notifications-document-close-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64647 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/notifications/NotificationCenter.cpp
+++ third_party/WebKit/WebCore/notifications/NotificationCenter.cpp
@@ -69,6 +69,7 @@ void NotificationCenter::disconnectFrame()
         return;
     m_notificationPresenter->cancelRequestsForPermission(m_scriptExecutionContext);
     m_notificationPresenter = 0;
+    m_scriptExecutionContext = 0;
 }
 
 } // namespace WebCore
