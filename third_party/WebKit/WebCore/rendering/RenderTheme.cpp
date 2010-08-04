9d7378528d9343242a7898a572a602a178f7dc29
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-04 07:32:53 +0000

    2010-08-03  Darin Adler  <darin@apple.com>
            Reviewed by Dan Bernstein.
    
            System color changes are not applied properly
            https://bugs.webkit.org/show_bug.cgi?id=43468
    
            * manual-tests/system-color-change.html: Added.
    
            * rendering/RenderTheme.cpp:
            (WebCore::RenderTheme::platformColorsDidChange): Added a call to
            Page::setNeedsReapplyStyles, so all frames of all pages will recompute
            in case they use system colors. It's rare for these colors to change,
            so OK to do a little extra style computation.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64634 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTheme.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTheme.cpp
@@ -1039,6 +1039,8 @@ void RenderTheme::platformColorsDidChange()
     m_inactiveListBoxSelectionForegroundColor = Color();
     m_activeListBoxSelectionBackgroundColor = Color();
     m_inactiveListBoxSelectionForegroundColor = Color();
+
+    Page::setNeedsReapplyStyles();
 }
 
 Color RenderTheme::systemColor(int cssValueId) const
