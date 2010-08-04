3dff9cd5419f5ee267439b259b7aced547d9e253
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-04 20:17:44 +0000

    [wx] Build fix. Enclose PlatformStrategy methods in USE(PLATFORM_STRATEGIES).
    git-svn-id: svn://svn.chromium.org/blink/trunk@64670 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/LocalizedStrings.cpp
+++ third_party/WebKit/WebCore/platform/LocalizedStrings.cpp
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+#if USE(PLATFORM_STRATEGIES)
+
 String inputElementAltText()
 {
     return platformStrategies()->localizationStrategy()->inputElementAltText();
@@ -562,5 +564,7 @@ String validationMessageStepMismatchText()
 {
     return platformStrategies()->localizationStrategy()->validationMessageStepMismatchText();
 }
+
+#endif // USE(PLATFORM_STRATEGIES)
 
 } // namespace WebCore
