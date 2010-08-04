045930fc9c6c6a8761ad399da822c4cad0afba63
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-04 04:13:26 +0000

    Implement "Add" and "Remove" buttons in Language and Input page.
    Along the way, remove translation for language codes in
    |localStrings| (ex "fr" -> "French").
    Instead introduce templateData.languageList, with which
    we can retrieve language information.
    
    TEST=manually
    BUG=4573
    
    Review URL: http://codereview.chromium.org/3041025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54869 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dom_ui/language_options_handler.h
+++ chrome/browser/chromeos/dom_ui/language_options_handler.h
@@ -25,6 +25,9 @@ class LanguageOptionsHandler : public OptionsPageUIHandler {
   // Get the list of input methods.
   ListValue* GetInputMethodList();
 
+  // Get the list of languages.
+  ListValue* GetLanguageList();
+
   DISALLOW_COPY_AND_ASSIGN(LanguageOptionsHandler);
 };
 
