50a35486c0dcac5eb6ca61a0e66696652adf6bc5
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-04 01:47:55 +0000

    De-wstringify some browser startup code and net utility.
    Review URL: http://codereview.chromium.org/3054042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54860 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_util.cc
+++ net/base/net_util.cc
@@ -1639,7 +1639,7 @@ GURL SimplifyUrlForRequest(const GURL& url) {
 
 // Specifies a comma separated list of port numbers that should be accepted
 // despite bans. If the string is invalid no allowed ports are stored.
-void SetExplicitlyAllowedPorts(const std::wstring& allowed_ports) {
+void SetExplicitlyAllowedPorts(const std::string& allowed_ports) {
   if (allowed_ports.empty())
     return;
 
@@ -1647,7 +1647,7 @@ void SetExplicitlyAllowedPorts(const std::wstring& allowed_ports) {
   size_t last = 0;
   size_t size = allowed_ports.size();
   // The comma delimiter.
-  const std::wstring::value_type kComma = L',';
+  const std::string::value_type kComma = ',';
 
   // Overflow is still possible for evil user inputs.
   for (size_t i = 0; i <= size; ++i) {
@@ -1659,8 +1659,7 @@ void SetExplicitlyAllowedPorts(const std::wstring& allowed_ports) {
       size_t length = i - last;
       if (length > 0) {
         int port;
-        base::StringToInt(WideToUTF8(allowed_ports.substr(last, length)),
-                          &port);
+        base::StringToInt(allowed_ports.substr(last, length), &port);
         ports.insert(port);
       }
       last = i + 1;
