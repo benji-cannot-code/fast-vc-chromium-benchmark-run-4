2f1793a6a973f05be934d07a84a6f8c6cede78d3
victorw@chromium.org <victorw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-04 23:50:58 +0000

    2010-08-04  Victor Wang  <victorw@chromium.org>
            Reviewed by Ojan Vafai.
    
            -. Add result.json incremental merging functionality to test results app engine.
            -. blobstore is not suitable for AE data merging and there is no API to
               programatically edit existing blob or write a new one yet, so replace blobstore
               with datastore. If file is oversize (>1000*1000 bytes), store file data in
               multiple datastore entries.
            -. Fix styles.
    
            Test: jsonresults_unittest to test merging logics.
    
            https://bugs.webkit.org/show_bug.cgi?id=38599
    
            * TestResultServer/handlers/dashboardhandler.py:
            * TestResultServer/handlers/menu.py:
            * TestResultServer/handlers/testfilehandler.py:
            * TestResultServer/main.py:
            * TestResultServer/model/dashboardfile.py:
            * TestResultServer/model/datastorefile.py: Added.
            * TestResultServer/model/jsonresults.py: Added.
            * TestResultServer/model/jsonresults_unittest.py: Added.
            * TestResultServer/model/testfile.py:
            * TestResultServer/templates/uploadform.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/TestResultServer/model/dashboardfile.py
+++ third_party/WebKit/WebKitTools/TestResultServer/model/dashboardfile.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #     * Neither the name of Google Inc. nor the names of its
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -36,6 +36,7 @@ from google.appengine.ext import db
 
 SVN_PATH_DASHBOARD = ("http://src.chromium.org/viewvc/chrome/trunk/tools/"
     "dashboards/")
+
 
 class DashboardFile(db.Model):
     name = db.StringProperty()
@@ -93,7 +94,7 @@ class DashboardFile(db.Model):
         if not files:
             logging.info("No existing file, added as new file.")
             return cls.add_file(name, data)
-        
+
         logging.debug("Updating existing file.")
         file = files[0]
         file.data = data
