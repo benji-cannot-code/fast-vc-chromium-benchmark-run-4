2acfc54106853f90f366bdc017a154b0732bbd6e
loki@webkit.org <loki@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-04 07:12:33 +0000

    Enable JSValue32_64 for GCC on ARM by default https://bugs.webkit.org/show_bug.cgi?id=43410
    Reviewed by Geoffrey Garen.
    
    * wtf/Platform.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64632 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -913,7 +913,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     || CPU(ALPHA) \
     || CPU(SPARC64)
 #define WTF_USE_JSVALUE64 1
-#elif CPU(ARM_TRADITIONAL) || CPU(PPC64) || CPU(MIPS)
+#elif CPU(PPC64) || CPU(MIPS) || (CPU(ARM_TRADITIONAL) && !COMPILER(GCC))
 #define WTF_USE_JSVALUE32 1
 #elif OS(WINDOWS) && COMPILER(MINGW)
 /* Using JSVALUE32_64 causes padding/alignement issues for JITStubArg
