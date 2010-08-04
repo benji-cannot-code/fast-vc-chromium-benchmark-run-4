efa161b4ee9a913ddafc31198040ee7c59040a88
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-04 19:49:29 +0000

    Windows build fix
    * WebProcess/Plugins/Netscape/NetscapeBrowserFuncs.cpp:
    (WebKit::NPN_SetValue): Added some case statements that are compiled
    on Windows to avoid a warning about having default but not cases.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64667 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/Plugins/Netscape/NetscapeBrowserFuncs.cpp
+++ third_party/WebKit/WebKit2/WebProcess/Plugins/Netscape/NetscapeBrowserFuncs.cpp
@@ -463,6 +463,8 @@ static NPError NPN_SetValue(NPP npp, NPPVariable variable, void *value)
         }
 #endif
 
+        case NPPVpluginWindowBool:
+        case NPPVpluginTransparentBool:
         default:
             notImplemented();
             return NPERR_GENERIC_ERROR;
