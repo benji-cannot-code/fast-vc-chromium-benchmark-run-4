83688ad0d8bd8bd5e305f05ac2032ded1634f472
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-04 22:41:11 +0000

    Play WebM live stream
    BUG=44891
    TEST=Can play live stream http://195.10.10.75:8800/live.webm
    
    This patch allows a live WebM stream to be played.
    
    After demuxing by FFmpeg, the duration was set to an invalid value. This
    makes the video pipeline to set duration to 0, and so WebKit never allows
    us to play because currentTime == duration all the time.
    
    This patch sets duration to an infinite value when duration is reported as
    invalid in FFmpeg.
    
    Review URL: http://codereview.chromium.org/3031036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54983 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webmediaplayer_impl.cc
+++ webkit/glue/webmediaplayer_impl.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/glue/webmediaplayer_impl.h"
 
+#include <limits>
+
 #include "base/callback.h"
 #include "base/command_line.h"
 #include "media/base/limits.h"
@@ -418,7 +420,10 @@ bool WebMediaPlayerImpl::seeking() const {
 float WebMediaPlayerImpl::duration() const {
   DCHECK(MessageLoop::current() == main_loop_);
 
-  return static_cast<float>(pipeline_->GetMediaDuration().InSecondsF());
+  base::TimeDelta duration = pipeline_->GetMediaDuration();
+  if (duration.InMicroseconds() == media::Limits::kMaxTimeInMicroseconds)
+    return std::numeric_limits<float>::infinity();
+  return static_cast<float>(duration.InSecondsF());
 }
 
 float WebMediaPlayerImpl::currentTime() const {
