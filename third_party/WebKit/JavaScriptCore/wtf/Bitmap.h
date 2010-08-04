9d85fdcb8d5332315d6c0eb0240bc8863d0c8cce
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-04 04:37:26 +0000

    Speculative windows build fix.
    * wtf/Bitmap.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64627 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Bitmap.h
+++ third_party/WebKit/JavaScriptCore/wtf/Bitmap.h
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FixedArray.h"
 #include "StdLibExtras.h"
 
+#include <stdint.h>
+
 namespace WTF {
 
 template<size_t size>
