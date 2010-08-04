a745186a7dbfaf3d8b125ae76573c3d4e07828d5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-04 17:18:07 +0000

    2010-08-04  Nathan Lawrence  <nlawrence@apple.com>
            Reviewed by Darin Adler.
    
            Refactoring MarkStack::append to take a reference.  This is in
            preparation for movable objects when we will need to update pointers.
            http://bugs.webkit.org/show_bug.cgi?id=41177
    
            Unless otherwise noted, all changes are to either return by reference
            or pass a reference to MarkStack::append.
    
            * bytecode/CodeBlock.cpp:
            (JSC::CodeBlock::markAggregate):
            * runtime/Collector.cpp:
            (JSC::Heap::markConservatively):
                Added a temporary variable to prevent marking from changing an
                unknown value on the stack
            * runtime/JSCell.h:
            (JSC::JSValue::asCell):
            (JSC::MarkStack::append):
            (JSC::MarkStack::appendInternal):
            * runtime/JSGlobalObject.cpp:
            (JSC::markIfNeeded):
            * runtime/JSONObject.cpp:
            (JSC::Stringifier::Holder::object):
            * runtime/JSObject.h:
            (JSC::JSObject::prototype):
            * runtime/JSStaticScopeObject.cpp:
            (JSC::JSStaticScopeObject::markChildren):
            * runtime/JSValue.h:
            (JSC::JSValue::JSValue):
            (JSC::JSValue::asCell):
            * runtime/MarkStack.h:
            * runtime/NativeErrorConstructor.cpp:
            (JSC::NativeErrorConstructor::createStructure):
                Changed the structure flags to include a custom markChildren.
            (JSC::NativeErrorConstructor::markChildren):
                Update the prototype of the stored structure.
            * runtime/NativeErrorConstructor.h:
                Added structure flags.
            * runtime/Structure.h:
            (JSC::Structure::storedPrototype):
    2010-08-04  Nathan Lawrence  <nlawrence@apple.com>
    
            Reviewed by Darin Adler.
    
            Removed unneeded marking.  We need to remove this marking in order to have
            MarkStack::append take references for updating movable objects.
    
            https://bugs.webkit.org/show_bug.cgi?id=41177
    
            * JSValueWrapper.cpp:
            (JSValueWrapper::JSObjectMark):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64655 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/JSValue.h
+++ third_party/WebKit/JavaScriptCore/runtime/JSValue.h
@@ -200,7 +200,8 @@ namespace JSC {
         JSValue getJSNumber(); // JSValue() if this is not a JSNumber or number object
 
         bool isCell() const;
-        JSCell* asCell() const;
+        JSCell*& asCell();
+        JSCell* const& asCell() const;
         bool isValidCallee();
 
 #ifndef NDEBUG
@@ -241,11 +242,19 @@ namespace JSC {
                 int32_t tag;
                 int32_t payload;
             } asBits;
+            struct {
+                int32_t tag;
+                JSCell* ptr;
+            } asPtr;
 #else
             struct {
                 int32_t payload;
                 int32_t tag;
             } asBits;
+            struct {
+                JSCell* ptr;
+                int32_t tag;
+            } asPtr;
 #endif
         } u;
 #else // USE(JSVALUE32_64)
@@ -492,7 +501,7 @@ namespace JSC {
             u.asBits.tag = CellTag;
         else
             u.asBits.tag = EmptyValueTag;
-        u.asBits.payload = reinterpret_cast<int32_t>(ptr);
+        u.asPtr.ptr = ptr;
 #if ENABLE(JSC_ZOMBIES)
         ASSERT(!isZombie());
 #endif
@@ -504,7 +513,7 @@ namespace JSC {
             u.asBits.tag = CellTag;
         else
             u.asBits.tag = EmptyValueTag;
-        u.asBits.payload = reinterpret_cast<int32_t>(const_cast<JSCell*>(ptr));
+        u.asPtr.ptr = const_cast<JSCell*>(ptr);
 #if ENABLE(JSC_ZOMBIES)
         ASSERT(!isZombie());
 #endif
@@ -599,10 +608,16 @@ namespace JSC {
         return u.asDouble;
     }
     
-    ALWAYS_INLINE JSCell* JSValue::asCell() const
+    ALWAYS_INLINE JSCell* const& JSValue::asCell() const
     {
         ASSERT(isCell());
-        return reinterpret_cast<JSCell*>(u.asBits.payload);
+        return u.asPtr.ptr;
+    }
+
+    ALWAYS_INLINE JSCell*& JSValue::asCell()
+    {
+        ASSERT(isCell());
+        return u.asPtr.ptr;
     }
 
     ALWAYS_INLINE JSValue::JSValue(EncodeAsDoubleTag, ExecState*, double d)
