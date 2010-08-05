b2949e4014f7afe292c1c1273b0ca10819a7c7b3
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-05 21:04:19 +0000

    SPDY: flow-control fix: resume I/O once a WINDOW_UPDATE frame is received for a stalled stream.
    BUG=none
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/3018019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55125 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_test_util.h
+++ net/spdy/spdy_test_util.h
@@ -24,6 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
+// Default upload data used by both, mock objects and framer when creating
+// data frames.
+const char kDefaultURL[] = "http://www.google.com";
+const char kUploadData[] = "hello!";
+const int kUploadDataSize = arraysize(kUploadData)-1;
+
 // NOTE: In GCC, on a Mac, this can't be in an anonymous namespace!
 // This struct holds information used to construct spdy control and data frames.
 struct SpdyHeaderInfo {
@@ -250,6 +256,10 @@ spdy::SpdyFrame* ConstructSpdyPostSynReply(const char* const extra_headers[],
 // Constructs a single SPDY data frame with the contents "hello!"
 spdy::SpdyFrame* ConstructSpdyBodyFrame(int stream_id,
                                         bool fin);
+
+// Constructs a single SPDY data frame with the given content.
+spdy::SpdyFrame* ConstructSpdyBodyFrame(int stream_id, const char* data,
+                                        uint32 len, bool fin);
 
 // Create an async MockWrite from the given SpdyFrame.
 MockWrite CreateMockWrite(const spdy::SpdyFrame& req);
