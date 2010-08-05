dad26934c576323e401f8227c46c1f1d5f3edd7c
zoltan@webkit.org <zoltan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-05 09:06:01 +0000

    2010-08-05  Zoltan Horvath  <zoltan@webkit.org>
            Reviewed by Simon Hausmann.
    
            Allow custom memory allocation control for DoctypeData class
            https://bugs.webkit.org/show_bug.cgi?id=43343
    
            Inherits the following class from Noncopyable because it is
            instantiated by 'new' and no need to be copyable:
    
            class name  - instantiated at: WebCore/'location'
            DoctypeData - html/HTMLToken.h:113
    
            * html/HTMLToken.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLToken.h
+++ third_party/WebKit/WebCore/html/HTMLToken.h
@@ -263,7 +263,7 @@ private:
     // want to end up with a cleaner interface between the two classes.
     friend class AtomicHTMLToken;
 
-    class DoctypeData {
+    class DoctypeData : public Noncopyable {
     public:
         DoctypeData()
             : m_hasPublicIdentifier(false)
