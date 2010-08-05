8eb9333becaa4b2d71379abcfca188155d5145d8
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-05 17:14:21 +0000

    2010-08-05  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
            Rubber-stamped by Xan Lopez.
    
            Remove GHashTable left-overs. GHashTable is ref-counted, and is
            correctly supported by GRefPtr.
    
            * wtf/gobject/GOwnPtr.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64761 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/gobject/GOwnPtr.h
+++ third_party/WebKit/JavaScriptCore/wtf/gobject/GOwnPtr.h
@@ -35,7 +35,6 @@ typedef struct _GCond GCond;
 typedef struct _GMutex GMutex;
 typedef struct _GPatternSpec GPatternSpec;
 typedef struct _GDir GDir;
-typedef struct _GHashTable GHashTable;
 typedef struct _GFile GFile;
 extern "C" void g_free(void*);
 
@@ -48,7 +47,6 @@ template<> void freeOwnedGPtr<GCond>(GCond*);
 template<> void freeOwnedGPtr<GMutex>(GMutex*);
 template<> void freeOwnedGPtr<GPatternSpec>(GPatternSpec*);
 template<> void freeOwnedGPtr<GDir>(GDir*);
-template<> void freeOwnedGPtr<GHashTable>(GHashTable*);
 template<> void freeOwnedGPtr<GFile>(GFile*);
 
 template <typename T> class GOwnPtr : public Noncopyable {
