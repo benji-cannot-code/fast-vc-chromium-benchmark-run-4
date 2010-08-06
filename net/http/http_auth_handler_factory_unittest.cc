5566202919eca6a1664c351fe4e0d80e09899335
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-06 14:06:28 +0000

    Change some EXPECT's to ASSERT's in http_auth_handler_factory_unittest.cc
    I tripped upon this when I temporarily disabled Negotiate authentication to track down an NTLM bug.
    
    BUG=None
    TEST=net_unittests doesn't crash when HttpAuthHandlerNegotiate::Init returns false.
    
    Review URL: http://codereview.chromium.org/3085017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55221 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_handler_factory_unittest.cc
+++ net/http/http_auth_handler_factory_unittest.cc
@@ -112,7 +112,7 @@ TEST(HttpAuthHandlerFactoryTest, DefaultFactory) {
         BoundNetLog(),
         &handler);
     EXPECT_EQ(OK, rv);
-    EXPECT_FALSE(handler.get() == NULL);
+    ASSERT_FALSE(handler.get() == NULL);
     EXPECT_STREQ("basic", handler->scheme().c_str());
     EXPECT_STREQ("FooBar", handler->realm().c_str());
     EXPECT_EQ(HttpAuth::AUTH_SERVER, handler->target());
@@ -139,7 +139,7 @@ TEST(HttpAuthHandlerFactoryTest, DefaultFactory) {
         BoundNetLog(),
         &handler);
     EXPECT_EQ(OK, rv);
-    EXPECT_FALSE(handler.get() == NULL);
+    ASSERT_FALSE(handler.get() == NULL);
     EXPECT_STREQ("digest", handler->scheme().c_str());
     EXPECT_STREQ("FooBar", handler->realm().c_str());
     EXPECT_EQ(HttpAuth::AUTH_PROXY, handler->target());
@@ -171,7 +171,7 @@ TEST(HttpAuthHandlerFactoryTest, DefaultFactory) {
         BoundNetLog(),
         &handler);
     EXPECT_EQ(OK, rv);
-    EXPECT_FALSE(handler.get() == NULL);
+    ASSERT_FALSE(handler.get() == NULL);
     EXPECT_STREQ("negotiate", handler->scheme().c_str());
     EXPECT_STREQ("", handler->realm().c_str());
     EXPECT_EQ(HttpAuth::AUTH_SERVER, handler->target());
