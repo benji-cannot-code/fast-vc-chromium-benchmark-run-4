8807e3c5b914aedbd7f93cc554ff3d3d332e6006
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-06 20:40:51 +0000

    Add CoreIPC::MessageReceiver class https://bugs.webkit.org/show_bug.cgi?id=43637
    Reviewed by Adam Roben.
    
    * Platform/CoreIPC/Connection.h:
    (CoreIPC::Connection::MessageReceiver::~MessageReceiver):
    Add MessageReceiver class and make Client inherit from it.
    
    (CoreIPC::Connection::MessageReceiver::didReceiveSyncMessage):
    Change didReceiveSyncMessage from being a pure virtual member function and make it
    assert by default instead.
    
    * UIProcess/ChunkedUpdateDrawingAreaProxy.cpp:
    * UIProcess/ChunkedUpdateDrawingAreaProxy.h:
    Remove ChunkedUpdateDrawingAreaProxy::didReceiveSyncMessage.
    
    * Shared/DrawingAreaBase.h:
    Include "Connection.h" instead.
    
    * UIProcess/DrawingAreaProxy.h:
    (WebKit::DrawingAreaProxy::didReceiveSyncMessage):
    Change didReceiveSyncMessage from being a pure virtual member function and make it
    assert by default instead.
    
    * WebProcess/WebProcess.cpp:
    * WebProcess/WebProcess.h:
    Remove didReceiveSyncMessage.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Shared/DrawingAreaBase.h
+++ third_party/WebKit/WebKit2/Shared/DrawingAreaBase.h
@@ -28,16 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DrawingAreaBase_h
 
 #include "ArgumentCoders.h"
+#include "Connection.h"
 
 namespace WebCore {
     class IntRect;
     class IntSize;
-}
-
-namespace CoreIPC {
-    class ArgumentDecoder;
-    class Connection;
-    class MessageID;
 }
 
 namespace WebKit {
