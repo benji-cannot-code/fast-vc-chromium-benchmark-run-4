312299910ce75a45fb0c4951752eff28b3a99005
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-06 02:51:19 +0000

    2010-08-05  Patrick Gansterer  <paroga@paroga.com>
            Reviewed by Adam Roben.
    
            Make SearchPopupMenuWin more portable
            https://bugs.webkit.org/show_bug.cgi?id=43527
    
            Add #if PLATFORM(CF) around CoreFoundation code.
    
            * platform/win/SearchPopupMenuWin.cpp:
            (WebCore::SearchPopupMenuWin::enabled):
            (WebCore::SearchPopupMenuWin::saveRecentSearches):
            (WebCore::SearchPopupMenuWin::loadRecentSearches):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64812 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/SearchPopupMenuWin.cpp
+++ third_party/WebKit/WebCore/platform/win/SearchPopupMenuWin.cpp
@@ -23,7 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SearchPopupMenuWin.h"
 
 #include "AtomicString.h"
+
+#if PLATFORM(CF)
 #include <wtf/RetainPtr.h>
+#endif
 
 namespace WebCore {
 
@@ -39,20 +42,27 @@ PopupMenu* SearchPopupMenuWin::popupMenu()
 
 bool SearchPopupMenuWin::enabled()
 {
+#if PLATFORM(CF)
     return true;
+#else
+    return false;
+#endif
 }
 
+#if PLATFORM(CF)
 static RetainPtr<CFStringRef> autosaveKey(const String& name)
 {
     String key = "com.apple.WebKit.searchField:" + name;
     return RetainPtr<CFStringRef>(AdoptCF, key.createCFString());
 }
+#endif
 
 void SearchPopupMenuWin::saveRecentSearches(const AtomicString& name, const Vector<String>& searchItems)
 {
     if (name.isEmpty())
         return;
 
+#if PLATFORM(CF)
     RetainPtr<CFMutableArrayRef> items;
 
     size_t size = searchItems.size();
@@ -66,6 +76,7 @@ void SearchPopupMenuWin::saveRecentSearches(const AtomicString& name, const Vect
 
     CFPreferencesSetAppValue(autosaveKey(name).get(), items.get(), kCFPreferencesCurrentApplication);
     CFPreferencesAppSynchronize(kCFPreferencesCurrentApplication);
+#endif
 }
 
 void SearchPopupMenuWin::loadRecentSearches(const AtomicString& name, Vector<String>& searchItems)
@@ -73,6 +84,7 @@ void SearchPopupMenuWin::loadRecentSearches(const AtomicString& name, Vector<Str
     if (name.isEmpty())
         return;
 
+#if PLATFORM(CF)
     searchItems.clear();
     RetainPtr<CFArrayRef> items(AdoptCF, reinterpret_cast<CFArrayRef>(CFPreferencesCopyAppValue(autosaveKey(name).get(), kCFPreferencesCurrentApplication)));
 
@@ -85,6 +97,7 @@ void SearchPopupMenuWin::loadRecentSearches(const AtomicString& name, Vector<Str
         if (CFGetTypeID(item) == CFStringGetTypeID())
             searchItems.append(item);
     }
+#endif
 }
 
 }
