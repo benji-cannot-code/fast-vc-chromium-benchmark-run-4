54774ca1a28e19cb04c3dd934abd1fa61811d2bc
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-06 06:55:51 +0000

    Build the language list just like we do in the C++ version.
    Build the language list from supported input methods, as well
    as kExtraLanguages which don't have associated input methods.
    
    TEST=manually
    BUG=chromium-os:4573
    
    Review URL: http://codereview.chromium.org/3072028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55195 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dom_ui/language_options_handler.h
+++ chrome/browser/chromeos/dom_ui/language_options_handler.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_DOM_UI_LANGUAGE_OPTIONS_HANDLER_H_
 #pragma once
 
+#include "chrome/browser/chromeos/input_method/input_method_util.h"
 #include "chrome/browser/dom_ui/options_ui.h"
 
 class DictionaryValue;
@@ -27,14 +28,16 @@ class LanguageOptionsHandler : public OptionsPageUIHandler {
   virtual void RegisterMessages();
 
  private:
-  // Gets the list of input methods. The return value will look like:
+  // Gets the list of input methods from the given input descriptors.
+  // The return value will look like:
   // [{'id': 'pinyin', 'displayName': 'Pinyin', 'languageCode': 'zh-CW'}, ...]
-  ListValue* GetInputMethodList();
+  ListValue* GetInputMethodList(const InputMethodDescriptors& descriptors);
 
-  // Gets the list of languages. The return value will look like:
+  // Gets the list of languages from the given input descriptors.
+  // The return value will look like:
   // [{'code': 'fi', 'displayName': 'Finnish', 'nativeDisplayName': 'suomi'},
   //  ...]
-  ListValue* GetLanguageList();
+  ListValue* GetLanguageList(const InputMethodDescriptors& descriptors);
 
   // Called when the UI language is changed.
   // |value| will be the language code as string (ex. "fr").
