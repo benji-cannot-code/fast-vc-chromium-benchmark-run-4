e396aa684c96db5ee8fee45c39145736fa530ee1
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-06 06:03:25 +0000

    Add UTF-8 DCHECK()s in values.cc (since keys/paths are expected to be UTF-8).
    BUG=23581
    TEST=unit tests pass
    
    Review URL: http://codereview.chromium.org/3081018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55190 0039d316-1c4b-4281-b951-d872f2087c98

--- base/values.cc
+++ base/values.cc
@@ -379,6 +379,7 @@ bool DictionaryValue::Equals(const Value* other) const {
 }
 
 bool DictionaryValue::HasKey(const std::string& key) const {
+  DCHECK(IsStringUTF8(key));
   ValueMap::const_iterator current_entry = dictionary_.find(key);
   DCHECK((current_entry == dictionary_.end()) || current_entry->second);
   return current_entry != dictionary_.end();
@@ -405,6 +406,7 @@ void DictionaryValue::Clear() {
 }
 
 void DictionaryValue::Set(const std::string& path, Value* in_value) {
+  DCHECK(IsStringUTF8(path));
   DCHECK(in_value);
 
   std::string current_path(path);
@@ -508,6 +510,7 @@ void DictionaryValue::SetWithoutPathExpansion(const std::wstring& key,
 }
 
 bool DictionaryValue::Get(const std::string& path, Value** out_value) const {
+  DCHECK(IsStringUTF8(path));
   std::string current_path(path);
   const DictionaryValue* current_dictionary = this;
   for (size_t delimiter_position = current_path.find('.');
@@ -683,6 +686,7 @@ bool DictionaryValue::GetList(const std::wstring& path,
 
 bool DictionaryValue::GetWithoutPathExpansion(const std::string& key,
                                               Value** out_value) const {
+  DCHECK(IsStringUTF8(key));
   ValueMap::const_iterator entry_iterator = dictionary_.find(key);
   if (entry_iterator == dictionary_.end())
     return false;
@@ -793,6 +797,7 @@ bool DictionaryValue::GetListWithoutPathExpansion(const std::wstring& key,
 }
 
 bool DictionaryValue::Remove(const std::string& path, Value** out_value) {
+  DCHECK(IsStringUTF8(path));
   std::string current_path(path);
   DictionaryValue* current_dictionary = this;
   size_t delimiter_position = current_path.rfind('.');
@@ -814,6 +819,7 @@ bool DictionaryValue::Remove(const std::wstring& path, Value** out_value) {
 
 bool DictionaryValue::RemoveWithoutPathExpansion(const std::string& key,
                                                  Value** out_value) {
+  DCHECK(IsStringUTF8(key));
   ValueMap::iterator entry_iterator = dictionary_.find(key);
   if (entry_iterator == dictionary_.end())
     return false;
