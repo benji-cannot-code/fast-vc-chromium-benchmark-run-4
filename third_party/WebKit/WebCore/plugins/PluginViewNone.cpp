476712c647b287edee27d18f14db8a68cf166b6d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-06 21:45:55 +0000

    2010-08-06  Patrick Gansterer  <paroga@paroga.com>
            Reviewed by Eric Seidel.
    
            Add PluginView::bindingInstance() to PluginViewNone
            https://bugs.webkit.org/show_bug.cgi?id=42936
    
            * bindings/js/ScriptControllerEfl.cpp:
            (WebCore::ScriptController::createScriptInstanceForWidget): Removed workaround.
            * plugins/PluginViewNone.cpp: Added property svn:eol-style.
            (WebCore::PluginView::bindingInstance): Added missing method and use it on WinCE.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/PluginViewNone.cpp
+++ third_party/WebKit/WebCore/plugins/PluginViewNone.cpp
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "PluginView.h"
 
+#if USE(JSC)
+#include "Bridge.h"
+#endif
+
 using namespace WTF;
 
 namespace WebCore {
@@ -136,10 +140,17 @@ void PluginView::handleFocusOutEvent()
 // ports using PluginView, but until then, if new functions like this are 
 // added, please make sure they have the proper platform #ifs so that changes
 // do not break ports who compile both this file and PluginView.cpp.   
-#if PLATFORM(MAC) || PLATFORM(CHROMIUM) || PLATFORM(EFL)
+#if PLATFORM(MAC) || PLATFORM(CHROMIUM) || PLATFORM(EFL) || OS(WINCE) && !PLATFORM(QT)
 #if ENABLE(NETSCAPE_PLUGIN_API)
 void PluginView::keepAlive(NPP)
 {
+}
+#endif
+
+#if USE(JSC)
+PassRefPtr<JSC::Bindings::Instance> PluginView::bindingInstance()
+{
+    return 0;
 }
 #endif
 
