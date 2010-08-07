d8f657c350e3ecc4b0dc605d48b039d71d08d348
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-07 17:16:11 +0000

    Convert browser/dom_ui/[o-r]*.cc to not use wstrings/wchar_t*s.
    BUG=23581
    TEST=builds and tests pass
    
    Review URL: http://codereview.chromium.org/3014060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55355 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/options_ui_uitest.cc
+++ chrome/browser/dom_ui/options_ui_uitest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "app/l10n_util.h"
 #include "base/command_line.h"
+#include "base/string16.h"
+#include "base/utf_string_conversions.h"
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
@@ -29,10 +31,10 @@ class OptionsUITest : public UITest {
   void AssertIsOptionsPage(TabProxy* tab) {
     std::wstring title;
     ASSERT_TRUE(tab->GetTabTitle(&title));
-    std::wstring expected_title =
-        l10n_util::GetStringF(IDS_OPTIONS_DIALOG_TITLE,
-            l10n_util::GetString(IDS_PRODUCT_NAME));
-    ASSERT_EQ(expected_title, title);
+    string16 expected_title =
+        l10n_util::GetStringFUTF16(IDS_OPTIONS_DIALOG_TITLE,
+            l10n_util::GetStringUTF16(IDS_PRODUCT_NAME));
+    ASSERT_EQ(expected_title, WideToUTF16Hack(title));
   }
 };
 
