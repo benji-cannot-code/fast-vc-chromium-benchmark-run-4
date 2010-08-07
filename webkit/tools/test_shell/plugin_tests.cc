dae6af053b9d1a972b603c104f7601a74a4fa8a8
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-07 18:33:43 +0000

    Add a mimetype to npapi_test_plugin so we pass a layout test that expects each plugin to have at least one mime type.
    Also remove some test code that deletes a plugin during teardown.
    This  is no longer needed and can break some tests that run later.
    
    BUG=51402
    
    Review URL: http://codereview.chromium.org/3076041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55358 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/plugin_tests.cc
+++ webkit/tools/test_shell/plugin_tests.cc
@@ -70,11 +70,6 @@ class PluginTest : public TestShellTest {
     TestShellTest::SetUp();
   }
 
-  virtual void TearDown() {
-    DeleteTestPlugin();
-    TestShellTest::TearDown();
-  }
-
   FilePath plugin_src_;
   FilePath plugin_file_path_;
 };
