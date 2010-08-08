a22cc60d0237c95d3cb4e44196112aaa84416090
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-08 19:29:54 +0000

    2010-08-08  James Robinson  <jamesr@chromium.org>
            Reviewed by Adam Barth.
    
            Set the shouldLazyAttach flag to true in the tree builder's adoption agency algorithm
            https://bugs.webkit.org/show_bug.cgi?id=43610
    
            Sets the shouldLazyAttach flag to true for appendChild() calls inside the HTML tree
            builder's adoption agency flag.  This is a speedup assuming that nodes subjected
            to the adoption agency are likely to be frequently reparented, and thus it is better
            to delay creating the render tree until later (when hopefully the DOM is finalized).
    
            On my mac pro this speeds up fast/parser/residual-style-hang.html by 2.6X.
    
            * html/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::callTheAdoptionAgency):
    2010-08-08  James Robinson  <jamesr@chromium.org>
    
            Reviewed by Adam Barth.
    
            Set the shouldLazyAttach flag to true in the tree builder's adoption agency algorithm
            https://bugs.webkit.org/show_bug.cgi?id=43610
    
            New baselines for tests where the render tree depends on 0x0 render objects being
            created/destroyed during adoption agency fixups.
    
            * platform/mac/fast/forms/formmove3-expected.txt:
            * platform/mac/fast/forms/preserveFormDuringResidualStyle-expected.txt:
            * platform/mac/fast/invalid/003-expected.txt:
            * platform/mac/fast/invalid/019-expected.txt:
            * platform/mac/tables/mozilla/bugs/bug647-expected.txt:
            * platform/mac/tables/mozilla/other/wa_table_tr_align-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64954 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/HTMLTreeBuilder.cpp
@@ -1717,7 +1717,7 @@ void HTMLTreeBuilder::callTheAdoptionAgency(AtomicHTMLToken& token)
             // 6.6
             // Use appendChild instead of parserAddChild to handle possible reparenting.
             ExceptionCode ec;
-            node->element()->appendChild(lastNode->element(), ec);
+            node->element()->appendChild(lastNode->element(), ec, true);
             ASSERT(!ec);
             // 6.7
             lastNode = node;
@@ -1732,7 +1732,7 @@ void HTMLTreeBuilder::callTheAdoptionAgency(AtomicHTMLToken& token)
             m_tree.fosterParent(lastNode->element());
         else {
             ExceptionCode ec;
-            commonAncestor->appendChild(lastNode->element(), ec);
+            commonAncestor->appendChild(lastNode->element(), ec, true);
             ASSERT(!ec);
         }
         // 8
