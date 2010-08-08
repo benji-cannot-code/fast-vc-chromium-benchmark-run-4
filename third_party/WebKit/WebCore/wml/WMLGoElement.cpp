cd89ba9d9dd3a59d252d99baffafcbcb33beabeb
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-08 03:13:32 +0000

    2010-08-07  Gyuyoung Kim  <gyuyoung.kim@samsung.com>
            Reviewed by Eric Seidel.
    
            [WML] Include FormDataBuilder.h in WMLGoElement.h
            https://bugs.webkit.org/show_bug.cgi?id=43177
    
            To fix build breaks in WMLGoElement.cpp, FormDataBuilder.h needs to be included.
    
            * wml/WMLGoElement.cpp: Include FormDataBuilder.h
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64932 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/wml/WMLGoElement.cpp
+++ third_party/WebKit/WebCore/wml/WMLGoElement.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Attribute.h"
 #include "FormData.h"
+#include "FormDataBuilder.h"
 #include "FormSubmission.h"
 #include "Frame.h"
 #include "FrameLoader.h"
