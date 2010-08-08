ebee8733a85ec8c991efd4fd7ab341f2259a31aa
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-08 17:45:22 +0000

    2010-08-08  Nikita Vasilyev  <me@elv1s.ru>
            Reviewed by Pavel Feldman.
    
            Web Inspector: speed-up Element.prototype.removeChildren
            https://bugs.webkit.org/show_bug.cgi?id=43687
    
            * inspector/front-end/utilities.js:
            (Element.prototype.removeChildren):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/utilities.js
+++ third_party/WebKit/WebCore/inspector/front-end/utilities.js
@@ -275,7 +275,8 @@ Element.prototype.query = function(query)
 
 Element.prototype.removeChildren = function()
 {
-    this.innerHTML = "";
+    if (this.firstChild)
+        this.textContent = "";
 }
 
 Element.prototype.isInsertionCaretInside = function()
