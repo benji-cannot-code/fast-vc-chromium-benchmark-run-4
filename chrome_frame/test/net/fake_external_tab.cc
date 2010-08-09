5faa939112d897c723f4ea29143215bb6ef5e510
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-09 15:47:17 +0000

    Add missing include to fix the build.
    TEST=none
    BUG=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55403 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/net/fake_external_tab.cc
+++ chrome_frame/test/net/fake_external_tab.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/resource_bundle.h"
 #include "app/win_util.h"
 #include "base/command_line.h"
+#include "base/debug_util.h"
 #include "base/file_util.h"
 #include "base/file_version_info.h"
 #include "base/i18n/icu_util.h"
