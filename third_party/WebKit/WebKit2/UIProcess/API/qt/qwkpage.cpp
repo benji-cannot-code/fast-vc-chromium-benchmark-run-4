fc9791c1407e66415b97feb5c5d8dd9e26b2658c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-09 18:01:28 +0000

    2010-08-09  Balazs Kelemen  <kb@inf.u-szeged.hu>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] No need for PageClientImpl
    
            https://bugs.webkit.org/show_bug.cgi?id=43728
    
            Remove PageClientImpl, take the implementation of WebKit::PageClient into QWKPagePrivate.
    
            * UIProcess/API/qt/qwkpage.cpp:
            (QWKPagePrivate::QWKPagePrivate):
            (QWKPagePrivate::toolTipChanged):
            * UIProcess/API/qt/qwkpage_p.h:
            (QWKPagePrivate::pageDidEnterAcceleratedCompositing):
            (QWKPagePrivate::pageDidLeaveAcceleratedCompositing):
            (QWKPagePrivate::processDidExit):
            (QWKPagePrivate::processDidRevive):
            (QWKPagePrivate::setCursor):
            (QWKPagePrivate::takeFocus):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64992 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
@@ -38,40 +38,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using namespace WebKit;
 using namespace WebCore;
 
-class PageClientImpl : public WebKit::PageClient {
-public:
-    PageClientImpl(QWKPage* page)
-        : m_page(page)
-    {
-    }
-
-#if USE(ACCELERATED_COMPOSITING)
-    void pageDidEnterAcceleratedCompositing() { }
-    void pageDidLeaveAcceleratedCompositing() { }
-#endif // USE(ACCELERATED_COMPOSITING)
-    virtual void processDidExit() { }
-    virtual void processDidRevive() { }
-    virtual void setCursor(const WebCore::Cursor&) { }
-    virtual void takeFocus(bool direction) { }
-
-    virtual void toolTipChanged(const WebCore::String&, const WebCore::String&);
-
-private:
-    QWKPage* m_page;
-};
-
-void PageClientImpl::toolTipChanged(const String&, const String& newTooltip)
-{
-    emit m_page->statusBarMessage(QString(newTooltip));
-}
-
 QWKPagePrivate::QWKPagePrivate(QWKPage* qq, WKPageNamespaceRef namespaceRef)
     : q(qq)
     , createNewPageFn(0)
 {
     memset(actions, 0, sizeof(actions));
     page = toWK(namespaceRef)->createWebPage();
-    page->setPageClient(new PageClientImpl(qq)); // Ownership is passed to the page.
+    page->setPageClient(this);
 }
 
 QWKPagePrivate::~QWKPagePrivate()
@@ -82,6 +55,11 @@ QWKPagePrivate::~QWKPagePrivate()
 void QWKPagePrivate::init(const QSize& viewportSize, DrawingAreaProxy* proxy)
 {
     page->initializeWebPage(IntSize(viewportSize), proxy);
+}
+
+void QWKPagePrivate::toolTipChanged(const String&, const String& newTooltip)
+{
+    emit q->statusBarMessage(QString(newTooltip));
 }
 
 void QWKPagePrivate::paint(QPainter* painter, QRect area)
