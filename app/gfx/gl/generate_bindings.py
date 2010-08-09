ffb5456ef381a3b313aa8353f84438d52319e074
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-09 20:45:07 +0000

    Added a couple of new GL extension bindings.
    These are used by the default Chrome WebKit WebGL implementation.
    
    TEST=try
    BUG=none
    
    Review URL: http://codereview.chromium.org/3030053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55465 0039d316-1c4b-4281-b951-d872f2087c98

--- app/gfx/gl/generate_bindings.py
+++ app/gfx/gl/generate_bindings.py
@@ -29,6 +29,10 @@ GL_FUNCTIONS = [
 ['void', ['glBlendFunc'], 'GLenum sfactor, GLenum dfactor'],
 ['void', ['glBlendFuncSeparate'],
     'GLenum srcRGB, GLenum dstRGB, GLenum srcAlpha, GLenum dstAlpha'],
+['void', ['glBlitFramebufferEXT', 'BlitFramebuffer'],
+    'GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, '
+    'GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1, '
+    'GLbitfield mask, GLenum filter'],
 ['void', ['glBufferData'],
     'GLenum target, GLsizei size, const void* data, GLenum usage'],
 ['void', ['glBufferSubData'],
@@ -105,7 +109,8 @@ GL_FUNCTIONS = [
     'GLuint program, GLsizei maxcount, GLsizei* count, GLuint* shaders'],
 ['GLint', ['glGetAttribLocation'], 'GLuint program, const char* name'],
 ['void', ['glGetBooleanv'], 'GLenum pname, GLboolean* params'],
-['void', ['glGetBufferParameteriv'], 'GLenum target, GLenum pname, GLint* params'],
+['void', ['glGetBufferParameteriv'],
+    'GLenum target, GLenum pname, GLint* params'],
 ['GLenum', ['glGetError'], 'void'],
 ['void', ['glGetFloatv'], 'GLenum pname, GLfloat* params'],
 ['void', ['glGetFramebufferAttachmentParameterivEXT',
@@ -152,6 +157,10 @@ GL_FUNCTIONS = [
     'GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, '
     'GLenum type, void* pixels'],
 ['void', ['glReleaseShaderCompiler'], 'void'],
+['void', ['glRenderbufferStorageMultisampleEXT',
+    'glRenderbufferStorageMultisample'],
+    'GLenum target, GLsizei samples, GLenum internalformat, '
+    'GLsizei width, GLsizei height'],
 ['void', ['glRenderbufferStorageEXT', 'glRenderbufferStorage'],
     'GLenum target, GLenum internalformat, GLsizei width, GLsizei height'],
 ['void', ['glSampleCoverage'], 'GLclampf value, GLboolean invert'],
@@ -261,7 +270,8 @@ EGL_FUNCTIONS = [
 ['EGLBoolean', ['eglGetConfigAttrib'],
     'EGLDisplay dpy, EGLConfig config, EGLint attribute, EGLint* value'],
 ['EGLSurface', ['eglCreateWindowSurface'],
-    'EGLDisplay dpy, EGLConfig config, EGLNativeWindowType win, const EGLint* attrib_list'],
+    'EGLDisplay dpy, EGLConfig config, EGLNativeWindowType win, '
+    'const EGLint* attrib_list'],
 ['EGLSurface', ['eglCreatePbufferSurface'],
     'EGLDisplay dpy, EGLConfig config, const EGLint* attrib_list'],
 ['EGLSurface', ['eglCreatePixmapSurface'],
