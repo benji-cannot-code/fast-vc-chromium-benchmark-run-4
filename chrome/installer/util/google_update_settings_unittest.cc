aa3780e30e4d0ef8ded7868dc12c4ef3ca5bd6d9
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-09 02:26:37 +0000

    Cleanup our Registry API.
    - Use wchar_t instead of TCHAR.
    - Use DCHECK instead of assert.
    - Remove this keyword (we don't use it on chromium).
    - Add DISALLOW_COPY_AND_ASSIGN to the classes.
    - Make it more compliant with chromium code style.
    - Remove ununsed methods.
    - Use arraysize macro for array size calculation instead of doing it manually.
    
    BUG=44644
    TEST=trybots
    
    TODO: Write unittests for this API.
    TODO: Remove all the default arguments from the methods in this API. They aren't allowed by our style guide.
    
    Review URL: http://codereview.chromium.org/3007037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55375 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/google_update_settings_unittest.cc
+++ chrome/installer/util/google_update_settings_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <windows.h>
+#include <shlwapi.h> // For SHDeleteKey.
 
 #include "base/registry.h"
 #include "base/scoped_ptr.h"
@@ -14,9 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
-const wchar_t* kHKCUReplacement =
+
+const wchar_t kHKCUReplacement[] =
     L"Software\\Google\\InstallUtilUnittest\\HKCU";
-const wchar_t* kHKLMReplacement =
+const wchar_t kHKLMReplacement[] =
     L"Software\\Google\\InstallUtilUnittest\\HKLM";
 
 const wchar_t kTestProductGuid[] = L"{89F1B351-B15D-48D4-8F10-1298721CF13D}";
