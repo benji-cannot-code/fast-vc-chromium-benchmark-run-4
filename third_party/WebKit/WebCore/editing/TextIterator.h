5bcecc5913348cd1eff438ca6dea7462fe9ad6b4
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-09 11:40:10 +0000

    2010-05-20  MORITA Hajime  <morrita@google.com>
            Reviewed by Ojan Vafai.
    
            <http://webkit.org/b/36360>
            Double clicking page's first editable inline element cannot select a word.
            <http://webkit.org/b/36359>
            Double clicking page's last editable inline element causes assertion failure.
    
            * editing/selection/doubleclick-inline-first-last-contenteditable-expected.txt: Added.
            * editing/selection/doubleclick-inline-first-last-contenteditable.html: Added.
            * editing/selection/script-tests/doubleclick-inline-first-last-contenteditable.js: Added.
    2010-05-27  MORITA Hajime  <morrita@google.com>
    
            Reviewed by Ojan Vafai.
    
            <http://webkit.org/b/36359>
            Double clicking page's last editable inline element causes assertion failure.
            <http://webkit.org/b/36360>
            Double clicking page's first editable inline element cannot select a word.
    
            nextBoundary() and previousBoundary() assumed that editable
            boundaries are on block-level elements, But it can appear on
            inline-level elements. So we start boundary look-up from the given
            node, instead of the containingBlock of the given node.
            Also, added TextIteratorEndsAtEditingBoundary to
            BackwardsCharacterIterator, otherwise, the VisiblePosition
            returned by BackwardsCharacterIterator might cross an editing
            boundary.
    
            Test: editing/selection/doubleclick-inline-first-last-contenteditable.html
    
            * dom/Position.cpp:
            (WebCore::Position::parentEditingBoundary): Added.
            * dom/Position.h:
            * editing/TextIterator.cpp:
            (WebCore::TextIterator::TextIterator):
            (WebCore::SimplifiedBackwardsTextIterator::SimplifiedBackwardsTextIterator):
            (WebCore::SimplifiedBackwardsTextIterator::advance):
            (WebCore::SimplifiedBackwardsTextIterator::crossesEditingBoundary): Added.
            (WebCore::SimplifiedBackwardsTextIterator::setCurrentNode): Added.
            (WebCore::SimplifiedBackwardsTextIterator::clearCurrentNode): Added.
            (WebCore::BackwardsCharacterIterator::BackwardsCharacterIterator):
            * editing/TextIterator.h:
            (WebCore::):
            * editing/visible_units.cpp:
            (WebCore::previousBoundary):
            (WebCore::nextBoundary):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64974 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/TextIterator.h
+++ third_party/WebKit/WebCore/editing/TextIterator.h
@@ -74,6 +74,7 @@ enum TextIteratorBehavior {
     TextIteratorEmitsCharactersBetweenAllVisiblePositions = 1 << 0,
     TextIteratorEntersTextControls = 1 << 1,
     TextIteratorEmitsTextsWithoutTranscoding = 1 << 2,
+    TextIteratorEndsAtEditingBoundary = 1 << 3
 };
 
 class TextIterator {
@@ -168,7 +169,7 @@ private:
 class SimplifiedBackwardsTextIterator {
 public:
     SimplifiedBackwardsTextIterator();
-    explicit SimplifiedBackwardsTextIterator(const Range*);
+    explicit SimplifiedBackwardsTextIterator(const Range*, TextIteratorBehavior = TextIteratorDefaultBehavior);
     
     bool atEnd() const { return !m_positionNode; }
     void advance();
@@ -184,7 +185,11 @@ private:
     bool handleReplacedElement();
     bool handleNonTextNode();
     void emitCharacter(UChar, Node*, int startOffset, int endOffset);
-    
+    bool crossesEditingBoundary(Node*) const;
+    bool setCurrentNode(Node*);
+    void clearCurrentNode();
+
+    TextIteratorBehavior m_behavior;
     // Current position, not necessarily of the text being returned, but position
     // as we walk through the DOM tree.
     Node* m_node;
@@ -248,7 +253,7 @@ private:
 class BackwardsCharacterIterator {
 public:
     BackwardsCharacterIterator();
-    explicit BackwardsCharacterIterator(const Range*);
+    explicit BackwardsCharacterIterator(const Range*, TextIteratorBehavior = TextIteratorDefaultBehavior);
 
     void advance(int);
 
@@ -257,6 +262,7 @@ public:
     PassRefPtr<Range> range() const;
 
 private:
+    TextIteratorBehavior m_behavior;
     int m_offset;
     int m_runOffset;
     bool m_atBreak;
