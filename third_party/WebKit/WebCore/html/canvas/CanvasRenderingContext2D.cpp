0eff58fb4acb00943f1f0adf1ae829dd018fc4dc
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-09 13:56:48 +0000

    2010-08-09  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Antonio Gomes.
    
            Canvas: Fast-path for setting the already set color with setStrokeColor() and setFillColor()
            https://bugs.webkit.org/show_bug.cgi?id=43718
    
            Avoid creating CanvasStyle objects when setting the same fill or stroke color using
            the WebKit-specific setters of CanvasRenderingContext2D.
    
            * html/canvas/CanvasRenderingContext2D.cpp:
            (WebCore::CanvasRenderingContext2D::setStrokeColor):
            (WebCore::CanvasRenderingContext2D::setFillColor):
            * html/canvas/CanvasStyle.cpp:
            (WebCore::CanvasStyle::isEquivalentColor):
            * html/canvas/CanvasStyle.h: Added CanvasStyle::isEquivalentColor() overloads
            for RGBA and CMYKA channel values.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@64980 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -557,6 +557,8 @@ void CanvasRenderingContext2D::setStrokeColor(const String& color)
 
 void CanvasRenderingContext2D::setStrokeColor(float grayLevel)
 {
+    if (state().m_strokeStyle && state().m_strokeStyle->isEquivalentColor(grayLevel, grayLevel, grayLevel, 1.0f))
+        return;
     setStrokeStyle(CanvasStyle::create(grayLevel, 1));
 }
 
@@ -567,16 +569,22 @@ void CanvasRenderingContext2D::setStrokeColor(const String& color, float alpha)
 
 void CanvasRenderingContext2D::setStrokeColor(float grayLevel, float alpha)
 {
+    if (state().m_strokeStyle && state().m_strokeStyle->isEquivalentColor(grayLevel, grayLevel, grayLevel, alpha))
+        return;
     setStrokeStyle(CanvasStyle::create(grayLevel, alpha));
 }
 
 void CanvasRenderingContext2D::setStrokeColor(float r, float g, float b, float a)
 {
+    if (state().m_strokeStyle && state().m_strokeStyle->isEquivalentColor(r, g, b, a))
+        return;
     setStrokeStyle(CanvasStyle::create(r, g, b, a));
 }
 
 void CanvasRenderingContext2D::setStrokeColor(float c, float m, float y, float k, float a)
 {
+    if (state().m_strokeStyle && state().m_strokeStyle->isEquivalentColor(c, m, y, k, a))
+        return;
     setStrokeStyle(CanvasStyle::create(c, m, y, k, a));
 }
 
@@ -590,6 +598,8 @@ void CanvasRenderingContext2D::setFillColor(const String& color)
 
 void CanvasRenderingContext2D::setFillColor(float grayLevel)
 {
+    if (state().m_fillStyle && state().m_fillStyle->isEquivalentColor(grayLevel, grayLevel, grayLevel, 1.0f))
+        return;
     setFillStyle(CanvasStyle::create(grayLevel, 1));
 }
 
@@ -600,16 +610,22 @@ void CanvasRenderingContext2D::setFillColor(const String& color, float alpha)
 
 void CanvasRenderingContext2D::setFillColor(float grayLevel, float alpha)
 {
+    if (state().m_fillStyle && state().m_fillStyle->isEquivalentColor(grayLevel, grayLevel, grayLevel, alpha))
+        return;
     setFillStyle(CanvasStyle::create(grayLevel, alpha));
 }
 
 void CanvasRenderingContext2D::setFillColor(float r, float g, float b, float a)
 {
+    if (state().m_fillStyle && state().m_fillStyle->isEquivalentColor(r, g, b, a))
+        return;
     setFillStyle(CanvasStyle::create(r, g, b, a));
 }
 
 void CanvasRenderingContext2D::setFillColor(float c, float m, float y, float k, float a)
 {
+    if (state().m_fillStyle && state().m_fillStyle->isEquivalentColor(c, m, y, k, a))
+        return;
     setFillStyle(CanvasStyle::create(c, m, y, k, a));
 }
 
