b2c96b50bcc2cbe4f9d902e56ef605eaccf4bfda
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-09 23:23:14 +0000

    Prevent password leakage to the suggest server by being more strict about not doing suggest when the user might have typed a username.  Since we might parse this as something else (e.g. a scheme, or a hostname that's going to have a port number), the added checks are mainly for things that don't appear to be "usernames" (which were already not sent).
    BUG=51146
    TEST=Unittest
    Review URL: http://codereview.chromium.org/3044054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55496 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/search_provider_unittest.cc
+++ chrome/browser/autocomplete/search_provider_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -137,7 +137,6 @@ void SearchProviderTest::SetUp() {
 }
 
 void SearchProviderTest::OnProviderUpdate(bool updated_matches) {
-  SearchProvider::set_query_suggest_immediately(false);
   if (quit_when_done_ && provider_->done()) {
     quit_when_done_ = false;
     message_loop_.Quit();
@@ -269,4 +268,31 @@ TEST_F(SearchProviderTest, QueryKeywordProvider) {
   // The fill into edit should contain the keyword.
   EXPECT_EQ(keyword_t_url_->keyword() + L" " + UTF16ToWide(keyword_term_),
             match.fill_into_edit);
+}
+
+TEST_F(SearchProviderTest, DontSendPrivateDataToSuggest) {
+  // None of the following input strings should be sent to the suggest server,
+  // because they may contain private data.
+  char* inputs[] = {
+    "username:password",
+    "http://username:password",
+    "https://username:password",
+    "username:password@hostname",
+    "http://username:password@hostname/",
+    "file://filename",
+    "data://data",
+    "unknownscheme:anything",
+    "http://hostname/?query=q",
+    "http://hostname/path#ref",
+    "https://hostname/path",
+  };
+
+  for (size_t i = 0; i < arraysize(inputs); ++i) {
+    QueryForInput(ASCIIToUTF16(inputs[i]));
+    // Make sure the default providers suggest service was not queried.
+    ASSERT_TRUE(test_factory_.GetFetcherByID(
+        SearchProvider::kDefaultProviderURLFetcherID) == NULL);
+    // Run till the history results complete.
+    RunTillProviderDone();
+  }
 }
