1f5a50792118b5b8313f8ef6d5ba20806be9f91a
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-10 10:08:22 +0000

    2010-08-10  Yoshiki Hayashi  <yhayashi@google.com>
            Reviewed by Kent Tamura.
    
            Iterate over :first-letter so that it will be included in innerText.
            https://bugs.webkit.org/show_bug.cgi?id=39863
    
            Test: fast/dom/inner-text-first-letter.html
    
            * editing/TextIterator.cpp:
            (WebCore::TextIterator::TextIterator):
            (WebCore::TextIterator::advance):
            (WebCore::TextIterator::handleTextNode):
            (WebCore::TextIterator::handleTextBox):
            (WebCore::TextIterator::emitText):
            * editing/TextIterator.h:
    
    2010-08-10  Yoshiki Hayashi  <yhayashi@google.com>
    
            Reviewed by Kent Tamura.
    
            Iterate over :first-letter so that it will be included in innerText.
            https://bugs.webkit.org/show_bug.cgi?id=39863
    
            * fast/dom/inner-text-first-letter-expected.txt: Added.
            * fast/dom/inner-text-first-letter.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/TextIterator.cpp
+++ third_party/WebKit/WebCore/editing/TextIterator.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderTableCell.h"
 #include "RenderTableRow.h"
 #include "RenderTextControl.h"
+#include "RenderTextFragment.h"
 #include "VisiblePosition.h"
 #include "visible_units.h"
 
@@ -254,10 +255,13 @@ TextIterator::TextIterator()
     , m_positionNode(0)
     , m_textCharacters(0)
     , m_textLength(0)
+    , m_remainingTextBox(0)
+    , m_firstLetterText(0)
     , m_lastCharacter(0)
     , m_emitsCharactersBetweenAllVisiblePositions(false)
     , m_entersTextControls(false)
     , m_emitsTextWithoutTranscoding(false)
+    , m_handledFirstLetter(false)
 {
 }
 
@@ -269,9 +273,12 @@ TextIterator::TextIterator(const Range* r, TextIteratorBehavior behavior)
     , m_positionNode(0)
     , m_textCharacters(0)
     , m_textLength(0)
+    , m_remainingTextBox(0)
+    , m_firstLetterText(0)
     , m_emitsCharactersBetweenAllVisiblePositions(behavior & TextIteratorEmitsCharactersBetweenAllVisiblePositions)
     , m_entersTextControls(behavior & TextIteratorEntersTextControls)
     , m_emitsTextWithoutTranscoding(behavior & TextIteratorEmitsTextsWithoutTranscoding)
+    , m_handledFirstLetter(false)
 {
     // FIXME: should support TextIteratorEndsAtEditingBoundary http://webkit.org/b/43609
     ASSERT(behavior != TextIteratorEndsAtEditingBoundary);
@@ -348,6 +355,12 @@ void TextIterator::advance()
         return;
     }
 
+    if (!m_textBox && m_remainingTextBox) {
+        m_textBox = m_remainingTextBox;
+        m_remainingTextBox = 0;
+        m_firstLetterText = 0;
+        m_offset = 0;
+    }
     // handle remembered text box
     if (m_textBox) {
         handleTextBox();
@@ -421,6 +434,8 @@ void TextIterator::advance()
             pushFullyClippedState(m_fullyClippedStack, m_node);
         m_handledNode = false;
         m_handledChildren = false;
+        m_handledFirstLetter = false;
+        m_firstLetterText = 0;
 
         // how would this ever be?
         if (m_positionNode)
@@ -439,8 +454,6 @@ bool TextIterator::handleTextNode()
         return false;
 
     RenderText* renderer = toRenderText(m_node->renderer());
-    if (renderer->style()->visibility() != VISIBLE)
-        return false;
         
     m_lastTextNode = m_node;
     String str = renderer->text();
@@ -448,10 +461,22 @@ bool TextIterator::handleTextNode()
     // handle pre-formatted text
     if (!renderer->style()->collapseWhiteSpace()) {
         int runStart = m_offset;
-        if (m_lastTextNodeEndedWithCollapsedSpace) {
+        if (m_lastTextNodeEndedWithCollapsedSpace && hasVisibleTextNode(renderer)) {
             emitCharacter(' ', m_node, 0, runStart, runStart);
             return false;
         }
+        if (!m_handledFirstLetter && renderer->isTextFragment()) {
+            handleTextNodeFirstLetter(static_cast<RenderTextFragment*>(renderer));
+            if (m_firstLetterText) {
+                String firstLetter = m_firstLetterText->text();
+                emitText(m_node, m_firstLetterText, m_offset, firstLetter.length());
+                m_firstLetterText = 0;
+                m_textBox = 0;
+                return false;
+            }
+        }
+        if (renderer->style()->visibility() != VISIBLE)
+            return false;
         int strLength = str.length();
         int end = (m_node == m_endContainer) ? m_endOffset : INT_MAX;
         int runEnd = min(strLength, end);
@@ -464,6 +489,15 @@ bool TextIterator::handleTextNode()
     }
 
     if (!renderer->firstTextBox() && str.length() > 0) {
+        if (!m_handledFirstLetter && renderer->isTextFragment()) {
+            handleTextNodeFirstLetter(static_cast<RenderTextFragment*>(renderer));
+            if (m_firstLetterText) {
+                handleTextBox();
+                return false;
+            }
+        }
+        if (renderer->style()->visibility() != VISIBLE)
+            return false;
         m_lastTextNodeEndedWithCollapsedSpace = true; // entire block is collapsed space
         return true;
     }
@@ -479,13 +513,19 @@ bool TextIterator::handleTextNode()
     }
     
     m_textBox = renderer->containsReversedText() ? (m_sortedTextBoxes.isEmpty() ? 0 : m_sortedTextBoxes[0]) : renderer->firstTextBox();
+    if (!m_handledFirstLetter && renderer->isTextFragment() && !m_offset)
+        handleTextNodeFirstLetter(static_cast<RenderTextFragment*>(renderer));
     handleTextBox();
     return true;
 }
 
 void TextIterator::handleTextBox()
 {    
-    RenderText* renderer = toRenderText(m_node->renderer());
+    RenderText* renderer = m_firstLetterText ? m_firstLetterText : toRenderText(m_node->renderer());
+    if (renderer->style()->visibility() != VISIBLE) {
+        m_textBox = 0;
+        return;
+    }
     String str = renderer->text();
     int start = m_offset;
     int end = (m_node == m_endContainer) ? m_endOffset : INT_MAX;
@@ -531,7 +571,7 @@ void TextIterator::handleTextBox()
                     subrunEnd = runEnd;
     
                 m_offset = subrunEnd;
-                emitText(m_node, runStart, subrunEnd);
+                emitText(m_node, renderer, runStart, subrunEnd);
             }
 
             // If we are doing a subrun that doesn't go to the end of the text box,
@@ -553,6 +593,33 @@ void TextIterator::handleTextBox()
         if (renderer->containsReversedText())
             ++m_sortedTextBoxesPosition;
     }
+    if (!m_textBox && m_remainingTextBox) {
+        m_textBox = m_remainingTextBox;
+        m_remainingTextBox = 0;
+        m_firstLetterText = 0;
+        m_offset = 0;
+        handleTextBox();
+    }
+}
+
+void TextIterator::handleTextNodeFirstLetter(RenderTextFragment* renderer)
+{
+    if (renderer->firstLetter()) {
+        RenderObject* r = renderer->firstLetter();
+        if (r->style()->visibility() != VISIBLE)
+            return;
+        for (RenderObject *currChild = r->firstChild(); currChild; currChild->nextSibling()) {
+            if (currChild->isText()) {
+                RenderText* firstLetter = toRenderText(currChild);
+                m_handledFirstLetter = true;
+                m_remainingTextBox = m_textBox;
+                m_textBox = firstLetter->firstTextBox();
+                m_firstLetterText = firstLetter;
+                return;
+            }
+        }
+    }
+    m_handledFirstLetter = true;
 }
 
 bool TextIterator::handleReplacedElement()
@@ -599,6 +666,18 @@ bool TextIterator::handleReplacedElement()
     m_lastCharacter = 0;
 
     return true;
+}
+
+bool TextIterator::hasVisibleTextNode(RenderText* renderer)
+{
+    if (renderer->style()->visibility() == VISIBLE)
+        return true;
+    if (renderer->isTextFragment()) {
+        RenderTextFragment* fragment = static_cast<RenderTextFragment*>(renderer);
+        if (fragment->firstLetter() && fragment->firstLetter()->style()->visibility() == VISIBLE)
+            return true;
+    }
+    return false;
 }
 
 static bool shouldEmitTabBeforeNode(Node* node)
@@ -892,9 +971,9 @@ void TextIterator::emitCharacter(UChar c, Node* textNode, Node* offsetBaseNode, 
     m_lastCharacter = c;
 }
 
-void TextIterator::emitText(Node* textNode, int textStartOffset, int textEndOffset)
+void TextIterator::emitText(Node* textNode, RenderObject* renderObject, int textStartOffset, int textEndOffset)
 {
-    RenderText* renderer = toRenderText(m_node->renderer());
+    RenderText* renderer = toRenderText(renderObject);
     m_text = m_emitsTextWithoutTranscoding ? renderer->textWithoutTranscoding() : renderer->text();
     ASSERT(m_text.characters());
 
@@ -908,6 +987,11 @@ void TextIterator::emitText(Node* textNode, int textStartOffset, int textEndOffs
 
     m_lastTextNodeEndedWithCollapsedSpace = false;
     m_hasEmitted = true;
+}
+
+void TextIterator::emitText(Node* textNode, int textStartOffset, int textEndOffset)
+{
+    emitText(textNode, m_node->renderer(), textStartOffset, textEndOffset);
 }
 
 PassRefPtr<Range> TextIterator::range() const
--- third_party/WebKit/WebCore/editing/TextIterator.h
+++ third_party/WebKit/WebCore/editing/TextIterator.h
@@ -33,6 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+class RenderText;
+class RenderTextFragment;
+
 // FIXME: Can't really answer this question correctly without knowing the white-space mode.
 // FIXME: Move this somewhere else in the editing directory. It doesn't belong here.
 inline bool isCollapsibleWhitespace(UChar c)
@@ -104,7 +107,10 @@ private:
     bool handleReplacedElement();
     bool handleNonTextNode();
     void handleTextBox();
+    void handleTextNodeFirstLetter(RenderTextFragment*);
+    bool hasVisibleTextNode(RenderText*);
     void emitCharacter(UChar, Node* textNode, Node* offsetBaseNode, int textStartOffset, int textEndOffset);
+    void emitText(Node* textNode, RenderObject* renderObject, int textStartOffset, int textEndOffset);
     void emitText(Node* textNode, int textStartOffset, int textEndOffset);
     
     // Current position, not necessarily of the text being returned, but position
@@ -136,6 +142,11 @@ private:
     // are false and 0, we go back to normal iterating.
     bool m_needsAnotherNewline;
     InlineTextBox* m_textBox;
+    // Used when iteration over :first-letter text to save pointer to
+    // remaining text box.
+    InlineTextBox* m_remainingTextBox;
+    // Used to point to RenderText object for :first-letter.
+    RenderText *m_firstLetterText;
     
     // Used to do the whitespace collapsing logic.
     Node* m_lastTextNode;    
@@ -161,6 +172,8 @@ private:
 
     // Used when we want texts for copying, pasting, and transposing.
     bool m_emitsTextWithoutTranscoding;
+    // Used when deciding text fragment created by :first-letter should be looked into.
+    bool m_handledFirstLetter;
 };
 
 // Iterates through the DOM range, returning all the text, and 0-length boundaries
