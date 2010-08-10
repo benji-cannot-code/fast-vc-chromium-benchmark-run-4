ba7aad753c5dbaf29e9acc472f26020f563105cd
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-10 00:18:24 +0000

    Prelaunch GPU process after 10 seconds.
    This will happen if the --prelaunch-gpu-process command line switch is present or for 5% of users in a field trial.
    
    This is so we can collect some stats of GPU capabilities.
    
    TEST=try, check --prelaunch-gpu-process works
    BUG=none
    
    Review URL: http://codereview.chromium.org/3119002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55510 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_thread.cc
+++ chrome/renderer/render_thread.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/field_trial.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/nullable_string16.h"
@@ -116,6 +117,9 @@ static const unsigned int kCacheStatsDelayMS = 2000 /* milliseconds */;
 static const double kInitialIdleHandlerDelayS = 1.0 /* seconds */;
 static const double kInitialExtensionIdleHandlerDelayS = 5.0 /* seconds */;
 static const int64 kMaxExtensionIdleHandlerDelayS = 5*60 /* seconds */;
+
+static const int kPrelauchGpuPercentage = 5;
+static const int kPrelauchGpuProcessDelayMS = 10000;
 
 // Keep the global RenderThread in a TLS slot so it is impossible to access
 // incorrectly from the wrong thread.
@@ -269,10 +273,19 @@ void RenderThread::Init() {
 
   // Establish a channel to the GPU process asynchronously if requested. If the
   // channel is established in time, EstablishGpuChannelSync will not block when
-  // it is later called.
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kPrelaunchGpuProcess)) {
-    EstablishGpuChannel();
+  // it is later called. Delays by a fixed period of time to avoid loading the
+  // GPU immediately in an attempt to not slow startup time.
+  scoped_refptr<FieldTrial> prelaunch_trial(
+      new FieldTrial("PrelaunchGpuProcessExperiment", 100));
+  int prelaunch_group = prelaunch_trial->AppendGroup("prelaunch_gpu_process",
+                                                     kPrelauchGpuPercentage);
+  if (prelaunch_group == prelaunch_trial->group() ||
+      CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kPrelaunchGpuProcess)) {
+    message_loop()->PostDelayedTask(FROM_HERE,
+                                    task_factory_->NewRunnableMethod(
+                                        &RenderThread::EstablishGpuChannel),
+                                    kPrelauchGpuProcessDelayMS);
   }
 }
 
