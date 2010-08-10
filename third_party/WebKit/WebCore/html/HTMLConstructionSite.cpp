31333c3711723267130adb9051530c571e71f4a3
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-10 10:49:34 +0000

    2010-08-10  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r65056.
            http://trac.webkit.org/changeset/65056
            https://bugs.webkit.org/show_bug.cgi?id=43773
    
            "Clearly I missed something.  I'm too tired to try and fix
            now.  Will re-land in the morning." (Requested by eseidel on
            #webkit).
    
            * html/HTMLConstructionSite.cpp:
            (WebCore::HTMLConstructionSite::attach):
            * html/HTMLElement.cpp:
            (WebCore::HTMLElement::insertAdjacentHTML):
            * html/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::processEndTagForInCell):
    2010-08-10  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r65056.
            http://trac.webkit.org/changeset/65056
            https://bugs.webkit.org/show_bug.cgi?id=43773
    
            "Clearly I missed something.  I'm too tired to try and fix
            now.  Will re-land in the morning." (Requested by eseidel on
            #webkit).
    
            * fast/dom/HTMLElement/insertAdjacentHTML-errors-expected.txt: Removed.
            * fast/dom/HTMLElement/insertAdjacentHTML-errors.html: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65064 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLConstructionSite.cpp
+++ third_party/WebKit/WebCore/html/HTMLConstructionSite.cpp
@@ -94,7 +94,7 @@ PassRefPtr<ChildType> HTMLConstructionSite::attach(Node* parent, PassRefPtr<Chil
     // doesn't.  It feels like we're missing a concept somehow.
     if (shouldFosterParent()) {
         fosterParent(child.get());
-        ASSERT(child->attached() || !child->parent()->attached());
+        ASSERT(child->attached());
         return child.release();
     }
 
