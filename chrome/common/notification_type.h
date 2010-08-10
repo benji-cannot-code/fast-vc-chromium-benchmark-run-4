740f69594cef7ee8b10c28f79a44ed415f7a0d78
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-10 05:23:33 +0000

    Revert 55449 - Fix some problems with TaskManagerBrowserTest.PopulateWebCacheFields:
    - wait for an actual update to occur; otherwise we're not testing the real thing
    - change DCHECKs to EXPECT_EQs; that's what we should use in tests
    
    This change should also fix the crashiness of this test.
    
    TEST=browser_tests
    BUG=42301
    
    Review URL: http://codereview.chromium.org/2856091
    
    TBR=phajdan.jr@chromium.org,glen@chromium.org
    Review URL: http://codereview.chromium.org/3118003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55530 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/notification_type.h
+++ chrome/common/notification_type.h
@@ -1052,12 +1052,6 @@ class NotificationType {
     // TokenRequestFailedDetails object.
     TOKEN_REQUEST_FAILED,
 
-    // Task Manager ------------------------------------------------------------
-
-    // Sent after the Task Manager has received updated ResourceTypeStats
-    // from a renderer. Source is a TaskManagerModel.
-    TASK_MANAGER_RESOURCE_TYPE_STATS_UPDATED,
-
     // Misc --------------------------------------------------------------------
 
 #if defined(OS_CHROMEOS)
