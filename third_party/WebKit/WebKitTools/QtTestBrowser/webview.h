e73dbbf43778d42cce92ef5864d44ff0dbc993a9
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-10 04:00:26 +0000

    2010-08-09  Antonio Gomes  <tonikitoo@webkit.org>
            eviewed by Ariya Hidayat.
    
            [Qt] QtTestBrowser: proper set scene rect
    
            When resizesToContents is ON scene's rect is set to the boundary of
            the mainFrame. However, navigating to other web page should keep resizing
            scene's rect according to the new document loaded. Patch addresses this issue.
    
            Now resizesToContents and scrolling properly work on QtTestBrowser.
    
            * QtTestBrowser/webview.cpp:
            (WebViewGraphicsBased::setPage):
            (WebViewGraphicsBased::contentsSizeChanged):
            (WebViewGraphicsBased::setResizesToContents):
            (WebViewGraphicsBased::resizeEvent):
            * QtTestBrowser/webview.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/QtTestBrowser/webview.h
+++ third_party/WebKit/WebKitTools/QtTestBrowser/webview.h
@@ -72,21 +72,19 @@ class WebViewGraphicsBased : public QGraphicsView {
 
 public:
     WebViewGraphicsBased(QWidget* parent);
-    virtual void resizeEvent(QResizeEvent*);
-    void setPage(QWebPage* page) { m_item->setPage(page); }
+    void setPage(QWebPage* page);
+
     void setItemCacheMode(QGraphicsItem::CacheMode mode) { m_item->setCacheMode(mode); }
     QGraphicsItem::CacheMode itemCacheMode() { return m_item->cacheMode(); }
 
     void setFrameRateMeasurementEnabled(bool enabled);
     bool frameRateMeasurementEnabled() const { return m_measureFps; }
 
+    virtual void resizeEvent(QResizeEvent*);
     virtual void paintEvent(QPaintEvent* event);
 
     void setResizesToContents(bool b);
     bool resizesToContents() const { return m_resizesToContents; }
-
-    void setCustomLayoutSize(const QSize& size) { return m_item->page()->setPreferredContentsSize(size); }
-    QSize customLayoutSize() const { return m_item->page()->preferredContentsSize(); }
 
     void setYRotation(qreal angle)
     {
@@ -110,6 +108,7 @@ public slots:
     void updateFrameRate();
     void animatedFlip();
     void animatedYFlip();
+    void contentsSizeChanged(const QSize&);
 
 signals:
     void yFlipRequest();
