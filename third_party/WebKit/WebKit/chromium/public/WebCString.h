b77f81d95f92076415ac40b3fa36a6f8b2cd0158
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-10 01:22:12 +0000

    Speculative chromium build fix.
    * public/WebCString.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebCString.h
+++ third_party/WebKit/WebKit/chromium/public/WebCString.h
@@ -40,6 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #endif
 
+namespace WTF {
+class CString;
+}
+
 namespace WebKit {
 
 class WebCStringPrivate;
