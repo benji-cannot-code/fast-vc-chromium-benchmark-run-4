4917bbfa6de6c2b57c98070dbd9249d0731db443
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-10 19:06:21 +0000

    Chromium build fix.
    * platform/graphics/chromium/FontPlatformDataChromiumWin.h:
    * platform/graphics/chromium/FontPlatformDataLinux.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65083 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/chromium/FontPlatformDataLinux.cpp
+++ third_party/WebKit/WebCore/platform/graphics/chromium/FontPlatformDataLinux.cpp
@@ -36,10 +36,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HarfbuzzSkia.h"
 #include "NotImplemented.h"
 #include "PlatformString.h"
-#include "StringImpl.h"
 
 #include "SkPaint.h"
 #include "SkTypeface.h"
+
+#include <wtf/text/StringImpl.h> 
 
 namespace WebCore {
 
