144773ab1689b4f66e51302e5ac971996a542c02
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-10 14:14:13 +0000

    2010-08-10  Steve Block  <steveblock@google.com>
            Reviewed by Jeremy Orlow.
    
            [V8] DeviceMotionEvent can not be used from JavaScript
            https://bugs.webkit.org/show_bug.cgi?id=43785
    
            This is tested by the existing tests fast/dom/DeviceMotion/create-event.html
            and fast/dom/DeviceMotion/optional-event-properties.html.
    
            * bindings/v8/custom/V8EventCustom.cpp:
            (WebCore::toV8):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65071 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8EventCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8EventCustom.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8Clipboard.h"
 #include "V8CompositionEvent.h"
 #include "V8CustomEvent.h"
+#include "V8DeviceMotionEvent.h"
 #include "V8DeviceOrientationEvent.h"
 #include "V8ErrorEvent.h"
 #include "V8IDBErrorEvent.h"
@@ -156,6 +157,8 @@ v8::Handle<v8::Value> toV8(Event* impl)
     if (impl->isBeforeLoadEvent())
         return toV8(static_cast<BeforeLoadEvent*>(impl));
 #if ENABLE(DEVICE_ORIENTATION)
+    if (impl->isDeviceMotionEvent())
+        return toV8(static_cast<DeviceMotionEvent*>(impl));
     if (impl->isDeviceOrientationEvent())
         return toV8(static_cast<DeviceOrientationEvent*>(impl));
 #endif
