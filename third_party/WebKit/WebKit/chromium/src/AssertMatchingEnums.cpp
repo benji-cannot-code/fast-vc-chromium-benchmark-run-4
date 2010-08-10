0be145e04e3f90d3384af323b0c2e60fc1d20bc2
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-10 01:37:23 +0000

    Speculative chromium build fix II.
    * src/AssertMatchingEnums.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65027 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/AssertMatchingEnums.cpp
+++ third_party/WebKit/WebKit/chromium/src/AssertMatchingEnums.cpp
@@ -70,6 +70,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPILE_ASSERT_MATCHING_ENUM(webkit_name, webcore_name) \
     COMPILE_ASSERT(int(WebKit::webkit_name) == int(WebCore::webcore_name), mismatching_enums)
 
+// These constants are in WTF, bring them into WebCore so the ASSERT still works for them!
+namespace WebCore {
+    using WTF::TextCaseSensitive;
+    using WTF::TextCaseInsensitive;
+};
+
 COMPILE_ASSERT_MATCHING_ENUM(WebAccessibilityRoleUnknown, UnknownRole);
 COMPILE_ASSERT_MATCHING_ENUM(WebAccessibilityRoleButton, ButtonRole);
 COMPILE_ASSERT_MATCHING_ENUM(WebAccessibilityRoleRadioButton, RadioButtonRole);
