9f24ba22ab0698a53f85a1056d5b2c0a730a552d
davidben@chromium.org <davidben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-10 00:34:02 +0000

    Add missing windows.h
    TBR=wtc
    BUG=none
    TEST=windows compiles
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55514 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/ssl_client_certificate_selector_win.cc
+++ chrome/browser/views/ssl_client_certificate_selector_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ssl_client_certificate_selector.h"
 
+#include <windows.h>
 #include <cryptuiapi.h>
 #pragma comment(lib, "cryptui.lib")
 
