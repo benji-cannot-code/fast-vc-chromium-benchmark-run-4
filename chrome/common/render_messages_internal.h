20787865a79b97fcee567a1c17da22cfbe80347a
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-10 18:02:03 +0000

    FBTF: Reapplies r55259, the first new IPC definition patch.
    This moves MessageWithTuple::Read() back into the main ipc_message_utils.h
    header from the private ipc_messsage_utils_impl.h header. In release mode, this
    was causing link failures.
    
    BUG=51411
    TEST=none
    
    Review URL: http://codereview.chromium.org/3069034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55587 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/render_messages_internal.h
+++ chrome/common/render_messages_internal.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/nacl_types.h"
 #include "chrome/common/notification_type.h"
 #include "chrome/common/page_zoom.h"
-#include "chrome/common/thumbnail_score.h"
 #include "chrome/common/translate_errors.h"
 #include "chrome/common/window_container_type.h"
 #include "gfx/rect.h"
@@ -35,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "webkit/glue/dom_operations.h"
 #include "webkit/glue/form_field.h"
-#include "webkit/glue/webcursor.h"
 
 #if defined(OS_POSIX)
 #include "base/file_descriptor_posix.h"
@@ -52,6 +50,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // to typedef it to avoid that.
 // Substitution map for l10n messages.
 typedef std::map<std::string, std::string> SubstitutionMap;
+
+struct ThumbnailScore;
+class WebCursor;
 
 //-----------------------------------------------------------------------------
 // RenderView messages
