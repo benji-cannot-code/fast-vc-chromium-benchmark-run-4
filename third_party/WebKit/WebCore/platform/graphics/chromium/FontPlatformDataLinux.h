50d5af7b2e4181ca6d4fbbc62941b9988af82316
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-10 18:22:19 +0000

    Chromium build fix
    * platform/graphics/chromium/FontPlatformDataLinux.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/chromium/FontPlatformDataLinux.h
+++ third_party/WebKit/WebCore/platform/graphics/chromium/FontPlatformDataLinux.h
@@ -33,10 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define FontPlatformDataLinux_h
 
 #include "FontRenderStyle.h"
-#include "StringImpl.h"
 #include <wtf/Forward.h>
 #include <wtf/RefPtr.h>
 #include <wtf/text/CString.h>
+#include <wtf/text/StringImpl.h>
 #include <SkPaint.h>
 
 class SkTypeface;
