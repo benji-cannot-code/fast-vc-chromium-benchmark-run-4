56205658ce465f07b1b1a1095bbe531c45677176
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-11 18:47:01 +0000

    Revert r55750
    TBR=mirandac@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55761 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension.h
+++ chrome/common/extensions/extension.h
@@ -425,7 +425,6 @@ class Extension {
   bool LoadLaunchFullscreen(const DictionaryValue* manifest,
                             std::string* error);
   bool LoadLaunchURL(const DictionaryValue* manifest, std::string* error);
-  bool EnsureNotHybridApp(const DictionaryValue* manifest, std::string* error);
 
   // Helper method to load an ExtensionAction from the page_action or
   // browser_action entries in the manifest.
