4034c891bb597dad2b7ebce43b10bf7772d12400
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-11 07:51:08 +0000

    2010-08-10  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Fix three ASSERTs hit with the HTML5 TreeBuilder in fragment mode
            https://bugs.webkit.org/show_bug.cgi?id=43762
    
            In fixing the insertAdjacentHTML I had to make small changes to
            the error logic, which ended up bringing us closer to HTML5.
    
            Test: fast/dom/HTMLElement/insertAdjacentHTML-errors.html
    
            * html/HTMLConstructionSite.cpp:
            (WebCore::HTMLConstructionSite::attach):
             - This ASSERT was just wrong, the child should not be attached
               in the case the parent is not attached.
            * html/HTMLElement.cpp:
            (WebCore::contextElementForInsertion):
             - This is part of HTML5, needed to know what to call the
               HTML or XML parsers with.  The previous code always passed
               "this" which is wrong in the case of beforeBegin or afterEnd insertion.
            (WebCore::HTMLElement::insertAdjacentHTML):
             - Use the right contextElement now that we know how to compute it.
            * html/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::processEndTagForInCell):
             - This ASSERT is direct from the spec, but it's wrong, so I've filed a spec bug.
    2010-08-10  Eric Seidel  <eric@webkit.org>
    
            Reviewed by Adam Barth.
    
            Fix three ASSERTs hit with the HTML5 TreeBuilder in fragment mode
            https://bugs.webkit.org/show_bug.cgi?id=43762
    
            In order to fix the insertAdjacentHTML I had to some behavior.
            The behavior I changed brings us closer to the HTML5 spec,
            but since it was not previously tested, I added tests.
    
            * fast/dom/HTMLElement/insertAdjacentHTML-errors-expected.txt: Added.
            * fast/dom/HTMLElement/insertAdjacentHTML-errors.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65134 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/HTMLElement/script-tests/insertAdjacentHTML-errors.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('Test insertAdjacentHTML exceptions to make sure they match HTML5');
+
+var div = document.createElement("div");
+
+shouldBeUndefined("div.insertAdjacentHTML('beforeBegin', 'text')");
+shouldBeUndefined("div.insertAdjacentHTML('afterEnd', 'text')");
+
+shouldThrow("div.insertAdjacentHTML('FOO', 'text')", '"Error: SYNTAX_ERR: DOM Exception 12"');
+shouldThrow("document.documentElement.insertAdjacentHTML('afterEnd', 'text')", '"Error: NO_MODIFICATION_ALLOWED_ERR: DOM Exception 7"');
+
+var successfullyParsed = true;
