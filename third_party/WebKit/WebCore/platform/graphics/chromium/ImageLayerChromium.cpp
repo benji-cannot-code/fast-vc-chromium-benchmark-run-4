5e82b0d5865775a7e8ce8f2eba7e4df772fff030
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-11 23:14:43 +0000

    2010-08-11  Nico Weber  <thakis@chromium.org>
            Reviewed by David Levin.
    
            [chromium/mac]: Fix colors in compositing mode
            https://bugs.webkit.org/show_bug.cgi?id=43823
    
            The textures are drawn by opengl, which doesn't do any color space
            correction -- it assumes everything is in the device color space
            already. Hence, when drawing into the memory that will be uploaded
            as opengl texture, use the device color space.
    
            * platform/graphics/chromium/ImageLayerChromium.cpp:
            (WebCore::ImageLayerChromium::updateTextureContents):
            * platform/graphics/chromium/LayerChromium.cpp:
            (WebCore::LayerChromium::updateTextureContents):
            * platform/graphics/chromium/LayerRendererChromium.cpp:
            (WebCore::LayerRendererChromium::setRootLayerCanvasSize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65194 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/chromium/ImageLayerChromium.cpp
+++ third_party/WebKit/WebCore/platform/graphics/chromium/ImageLayerChromium.cpp
@@ -115,7 +115,7 @@ void ImageLayerChromium::updateTextureContents(unsigned textureId)
     case kCGColorSpaceModelDeviceN:
         break;
     default:
-        colorSpace.adoptCF(CGColorSpaceCreateWithName(kCGColorSpaceGenericRGBLinear));
+        colorSpace.adoptCF(CGColorSpaceCreateDeviceRGB());
         break;
     }
     RetainPtr<CGContextRef> tempContext(AdoptCF, CGBitmapContextCreate(tempVector.data(),
