e012db583a35a96be70a315effd531cf3e1c0770
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-11 08:35:51 +0000

    2010-08-11  Kristian Monsen  <kristianm@google.com>
            Reviewed by Steve Block.
    
            Compile fix for Android, added include for <wtf/text/Cstring.h, this
            did not get included in Android.
            https://bugs.webkit.org/show_bug.cgi?id=41517
    
            Build fix only, no new tests.
    
            * page/PrintContext.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65137 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/PrintContext.cpp
+++ third_party/WebKit/WebCore/page/PrintContext.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameView.h"
 #include "RenderLayer.h"
 #include "RenderView.h"
+#include <wtf/text/CString.h>
 
 using namespace WebCore;
 
