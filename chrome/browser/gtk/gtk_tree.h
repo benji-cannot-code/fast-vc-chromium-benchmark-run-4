2f818d8f433f61ec7f429a1bfac1805172d4fe95
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-11 01:38:50 +0000

    Cleanup: Get rid of TreeNodeChildrenReordered.
    Reason: This function was never implemented by TreeNodeModel and is unlike it
    will be. Also it proved to not be necessary at all.
    
    BUG=None
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/3091016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55657 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/gtk_tree.h
+++ chrome/browser/gtk/gtk_tree.h
@@ -96,7 +96,6 @@ class TableAdapter : public TableModelObserver {
 
 // A helper class for populating a GtkTreeStore from a TreeModel.
 // TODO(mattm): support SetRootShown(true)
-// TODO(mattm): implement TreeNodeChildrenReordered
 class TreeAdapter : public TreeModelObserver {
  public:
   // Column ids for |tree_store_|.
@@ -132,7 +131,7 @@ class TreeAdapter : public TreeModelObserver {
   // Get the TreeModelNode corresponding to iter in the tree store.
   TreeModelNode* GetNode(GtkTreeIter* iter);
 
-  // TreeModelObserver implementation.
+  // Begin TreeModelObserver implementation.
   virtual void TreeNodesAdded(TreeModel* model,
                               TreeModelNode* parent,
                               int start,
@@ -141,9 +140,8 @@ class TreeAdapter : public TreeModelObserver {
                                 TreeModelNode* parent,
                                 int start,
                                 int count);
-  virtual void TreeNodeChildrenReordered(TreeModel* model,
-                                         TreeModelNode* parent);
   virtual void TreeNodeChanged(TreeModel* model, TreeModelNode* node);
+  // End TreeModelObserver implementation.
 
  private:
   // Fill the tree store values for a given node.
