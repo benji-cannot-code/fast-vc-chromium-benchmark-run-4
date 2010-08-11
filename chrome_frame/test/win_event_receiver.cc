9456fb609a9f4f2bb445ea29fabb9b915178dc0c
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-11 14:22:12 +0000

    Uninitialized member in WinEventReceiver.
    BUG=None
    TEST=None
    CID=12346
    
    Review URL: http://codereview.chromium.org/3010059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55721 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/win_event_receiver.cc
+++ chrome_frame/test/win_event_receiver.cc
@@ -11,7 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome_frame/function_stub.h"
 
 // WinEventReceiver methods
-WinEventReceiver::WinEventReceiver() : hook_(NULL), hook_stub_(NULL) {
+WinEventReceiver::WinEventReceiver()
+    : listener_(NULL),
+      hook_(NULL),
+      hook_stub_(NULL) {
 }
 
 WinEventReceiver::~WinEventReceiver() {
