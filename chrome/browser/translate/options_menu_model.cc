f7463827f316f9a69647dc6ac103e93a7ebcdf8d
jcivelli@chromium.org <jcivelli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-11 20:49:44 +0000

    Don't show preference related menus in the translate infobar in incognito mode.
    BUG=51214
    TEST=Navigate in a non incognito window to www.google.de. The translate infobar should show. The options menu should have options to always/never translate german.
         Navigate again to that page but from an incognito window, the options menu in the translate infobar should not have any preference (always/never translate) related items.
    
    Review URL: http://codereview.chromium.org/3073025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55777 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/options_menu_model.cc
+++ chrome/browser/translate/options_menu_model.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/histogram.h"
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/profile.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/translate/translate_infobar_delegate.h"
 #include "grit/generated_resources.h"
@@ -24,17 +25,20 @@ OptionsMenuModel::OptionsMenuModel(
       translate_delegate->target_language_index());
 
   // Populate the menu.
-  AddCheckItem(IDC_TRANSLATE_OPTIONS_ALWAYS,
-      l10n_util::GetStringFUTF16(IDS_TRANSLATE_INFOBAR_OPTIONS_ALWAYS,
-          original_language, target_language));
-  AddCheckItem(IDC_TRANSLATE_OPTIONS_NEVER_TRANSLATE_LANG,
-      l10n_util::GetStringFUTF16(
-          IDS_TRANSLATE_INFOBAR_OPTIONS_NEVER_TRANSLATE_LANG,
-          original_language));
-  AddCheckItem(IDC_TRANSLATE_OPTIONS_NEVER_TRANSLATE_SITE,
-      l10n_util::GetStringUTF16(
-          IDS_TRANSLATE_INFOBAR_OPTIONS_NEVER_TRANSLATE_SITE));
-  AddSeparator();
+  // Incognito mode does not get any preferences related items.
+  if (!translate_delegate->tab_contents()->profile()->IsOffTheRecord()) {
+    AddCheckItem(IDC_TRANSLATE_OPTIONS_ALWAYS,
+        l10n_util::GetStringFUTF16(IDS_TRANSLATE_INFOBAR_OPTIONS_ALWAYS,
+            original_language, target_language));
+    AddCheckItem(IDC_TRANSLATE_OPTIONS_NEVER_TRANSLATE_LANG,
+        l10n_util::GetStringFUTF16(
+            IDS_TRANSLATE_INFOBAR_OPTIONS_NEVER_TRANSLATE_LANG,
+            original_language));
+    AddCheckItem(IDC_TRANSLATE_OPTIONS_NEVER_TRANSLATE_SITE,
+        l10n_util::GetStringUTF16(
+            IDS_TRANSLATE_INFOBAR_OPTIONS_NEVER_TRANSLATE_SITE));
+    AddSeparator();
+  }
   AddItem(IDC_TRANSLATE_REPORT_BAD_LANGUAGE_DETECTION,
           l10n_util::GetStringFUTF16(IDS_TRANSLATE_INFOBAR_OPTIONS_REPORT_ERROR,
                                      original_language));
