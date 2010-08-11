c6a08c4a8ce6bdf0878a3e6f80c05430a9e6e203
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-11 14:56:36 +0000

    Implement admin control (by policy) over which extensions the user can install and run. Currently we intercept in two locations:
    1) When the user tries to install an extension (includes error message). 
    2) Every time the browser starts up and the currently installed extensions are loaded (silently skips the extension). 
    
    I implemented a whitelist and a blacklist approach, calling it a allow/deny list to not clash with the global blacklist we have for extensions. A blacklist of '*' means all extensions are blacklisted. If an extension is on the blacklist it cannot be installed/loaded, unless it also appears on the whitelist. 
    
    I also fleshed out the LIST_TYPE support for policy values, so that we don't have to use comma separated REG_SZ values for lists and can instead use ADM support for listboxes. 
    
    BUG=47085
    TEST=ExtensionsServiceTest.BlacklistedByPolicyWillNotInstall, ConfigurationPolicyProviderWinTest.TestExtensionInstall*
    
    Review URL: http://codereview.chromium.org/3014053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55727 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_constants.cc
+++ chrome/common/extensions/extension_constants.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -93,6 +93,8 @@ const char* kCannotScriptGallery =
     "The extensions gallery cannot be scripted.";
 const char* kChromeVersionTooLow =
     "This extension requires * version * or greater.";
+const char* kDisabledByPolicy =
+    "This extension has been disabled by your administrator.";
 const char* kDevToolsExperimental =
     "You must request the 'experimental' permission in order to use the"
     " DevTools API.";
