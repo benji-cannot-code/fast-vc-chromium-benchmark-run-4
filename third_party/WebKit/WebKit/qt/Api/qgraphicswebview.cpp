888705a8849902542107840fea107b55ab764bc5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-11 05:39:59 +0000

    2010-08-10  Anders Bakken  <agbakken@gmail.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Don't connect to microFocusChanged() if _q_updateMicroFocus is a noop.
            https://bugs.webkit.org/show_bug.cgi?id=40580
    
            _q_updateMicroFocus() is a noop unless this ifdef evaluates to true:
            #if !defined(QT_NO_IM) && (defined(Q_WS_X11) || defined(Q_WS_QWS) || defined(Q_OS_SYMBIAN))
    
            In such cases there's no need to carry the overhead of the extra
            signal/slot connection.
    
            * Api/qgraphicswebview.cpp:
            (QGraphicsWebViewPrivate::_q_updateMicroFocus):
            (QGraphicsWebView::setPage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65124 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
+++ third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
@@ -68,7 +68,9 @@ public:
     void _q_contentsSizeChanged(const QSize&);
     void _q_scaleChanged();
 
+#if !defined(QT_NO_IM) && (defined(Q_WS_X11) || defined(Q_WS_QWS) || defined(Q_OS_SYMBIAN))
     void _q_updateMicroFocus();
+#endif
     void _q_pageDestroyed();
 
     QGraphicsWebView* q;
@@ -101,9 +103,9 @@ void QGraphicsWebViewPrivate::_q_doLoadFinished(bool success)
     emit q->loadFinished(success);
 }
 
+#if !defined(QT_NO_IM) && (defined(Q_WS_X11) || defined(Q_WS_QWS) || defined(Q_OS_SYMBIAN))
 void QGraphicsWebViewPrivate::_q_updateMicroFocus()
 {
-#if !defined(QT_NO_IM) && (defined(Q_WS_X11) || defined(Q_WS_QWS) || defined(Q_OS_SYMBIAN))
     // Ideally, this should be handled by a common call to an updateMicroFocus function
     // in QGraphicsItem. See http://bugreports.qt.nokia.com/browse/QTBUG-7578.
     QList<QGraphicsView*> views = q->scene()->views();
@@ -112,8 +114,8 @@ void QGraphicsWebViewPrivate::_q_updateMicroFocus()
         if (ic)
             ic->update();
     }
-#endif
 }
+#endif
 
 void QGraphicsWebViewPrivate::_q_pageDestroyed()
 {
@@ -489,10 +491,12 @@ void QGraphicsWebView::setPage(QWebPage* page)
             this, SIGNAL(statusBarMessage(QString)));
     connect(d->page, SIGNAL(linkClicked(QUrl)),
             this, SIGNAL(linkClicked(QUrl)));
-    connect(d->page, SIGNAL(microFocusChanged()),
-            this, SLOT(_q_updateMicroFocus()));
     connect(d->page, SIGNAL(destroyed()),
             this, SLOT(_q_pageDestroyed()));
+#if !defined(QT_NO_IM) && (defined(Q_WS_X11) || defined(Q_WS_QWS) || defined(Q_OS_SYMBIAN))
+    connect(d->page, SIGNAL(microFocusChanged()),
+            this, SLOT(_q_updateMicroFocus()));
+#endif
 }
 
 /*!
