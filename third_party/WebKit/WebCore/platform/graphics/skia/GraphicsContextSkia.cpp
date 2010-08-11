4985d59af4eeb4531b04b0fc96063853bdd11d7c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-11 23:30:39 +0000

    2010-08-11  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by David Levin.
    
            Include <wtf/UnusedParam.h> in GraphicsContextSkia.cpp
            https://bugs.webkit.org/show_bug.cgi?id=43843
    
            wtf/UnusedParam.h must be included because UNUSED_PARAM macro is used.
    
            * platform/graphics/skia/GraphicsContextSkia.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65197 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
+++ third_party/WebKit/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>
 #include <wtf/Assertions.h>
 #include <wtf/MathExtras.h>
+#include <wtf/UnusedParam.h>
 
 #if USE(GLES2_RENDERING)
 #include "GLES2Canvas.h"
