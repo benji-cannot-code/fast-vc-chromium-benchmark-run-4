2df647dcd78bb4167c37baa1c665c9fb696cacc7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-11 17:14:54 +0000

    2010-08-11  Ryuan Choi  <ryuan.choi@samsung.com>
            Reviewed by Antonio Gomes.
    
            Implementation dispatchDecidePolicyForNavigationAction
            https://bugs.webkit.org/show_bug.cgi?id=43544
    
            Add ewk_view_navigation_policy_decision to decide whether url which
            user clicked will be loaded or not.
    
            * WebCoreSupport/FrameLoaderClientEfl.cpp:
            (WebCore::FrameLoaderClientEfl::dispatchDecidePolicyForNavigationAction):
            * ewk/ewk_private.h:
            * ewk/ewk_view.cpp:
            (ewk_view_navigation_policy_decision):
            * ewk/ewk_view.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/efl/ewk/ewk_view.h
+++ third_party/WebKit/WebKit/efl/ewk/ewk_view.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ewk_view_h
 #define ewk_view_h
 
+#include "ewk_frame.h"
 #include "ewk_history.h"
 #include "ewk_window_features.h"
 
@@ -134,6 +135,8 @@ struct _Ewk_View_Smart_Class {
     uint64_t (*exceeded_database_quota)(Ewk_View_Smart_Data *sd, Evas_Object *frame, const char *databaseName, uint64_t current_size, uint64_t expected_size);
 
     Eina_Bool (*run_open_panel)(Ewk_View_Smart_Data *sd, Evas_Object *frame, Eina_Bool allows_multiple_files, const Eina_List *suggested_filenames, Eina_List **selected_filenames);
+
+    Eina_Bool (*navigation_policy_decision)(Ewk_View_Smart_Data *sd, Ewk_Frame_Resource_Request *request);
 };
 
 #define EWK_VIEW_SMART_CLASS_VERSION 1UL /** the version you have to put into the version field in the Ewk_View_Smart_Class structure */
