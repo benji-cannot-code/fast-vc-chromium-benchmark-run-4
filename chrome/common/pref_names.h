808347d23698453d63595c930923dbb0e546c266
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-11 15:08:12 +0000

    Fix build break due to bad merge.
    BUG=None
    TEST=None
    TBR=pam
    
    Review URL: http://codereview.chromium.org/3181001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55729 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -300,6 +300,9 @@ extern const char kDisableExtensions[];
 extern const char kShowExtensionShelf[];
 extern const char kBrowserActionContainerWidth[];
 
+extern const char kLastExtensionsUpdateCheck[];
+extern const char kNextExtensionsUpdateCheck[];
+
 extern const char kExtensionInstallAllowList[];
 extern const char kExtensionInstallDenyList[];
 
