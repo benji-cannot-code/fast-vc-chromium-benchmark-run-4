6b3ee44ebf6151a66b0585593d5f07bd4accde7e
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 09:24:11 +0000

    Uninitialized member vars in CacheDumper.
    BUG=None
    TEST=None
    CID=4913
    
    Review URL: http://codereview.chromium.org/3155006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55861 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/dump_cache/dump_files.cc
+++ net/tools/dump_cache/dump_files.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -109,7 +109,10 @@ class CacheDumper {
   explicit CacheDumper(const std::wstring& path)
       : path_(path),
         block_files_(FilePath::FromWStringHack(path)),
-        index_(NULL) {}
+        index_(NULL),
+        current_hash_(0),
+        next_addr_(0) {
+  }
 
   bool Init();
 
@@ -148,8 +151,6 @@ bool CacheDumper::Init() {
     return false;
   }
 
-  current_hash_ = 0;
-  next_addr_ = 0;
   return true;
 }
 
