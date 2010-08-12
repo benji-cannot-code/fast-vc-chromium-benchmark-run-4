6f333e3d535ca21fd7b6987ed22bba5d6321d5c2
glotov@chromium.org <glotov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 14:59:42 +0000

    Prefrence policy is honored for changing crash reporting state.
    BUG=chromium-os:2884 
    TEST=none 
    
    Review URL: http://codereview.chromium.org/3072029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55885 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/eula_view.h
+++ chrome/browser/chromeos/login/eula_view.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "chrome/browser/chromeos/login/view_screen.h"
+#include "chrome/browser/pref_member.h"
 #include "chrome/browser/tab_contents/tab_contents_delegate.h"
 #include "views/controls/button/button.h"
 #include "views/controls/link.h"
@@ -116,6 +117,8 @@ class EulaView
   chromeos::ScreenObserver* observer_;
 
   GURL oem_eula_page_;
+
+  BooleanPrefMember metrics_reporting_enabled_;
 
   DISALLOW_COPY_AND_ASSIGN(EulaView);
 };
