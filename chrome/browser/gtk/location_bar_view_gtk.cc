71d5418118c72978acaf0504f23210927342df49
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 23:17:27 +0000

    Get rid of wide string constants in extension_accessibility_api_constants.
    BUG=none
    TEST=builds and passes tests
    
    Review URL: http://codereview.chromium.org/3106007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55956 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/location_bar_view_gtk.cc
+++ chrome/browser/gtk/location_bar_view_gtk.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_setting_bubble_model.h"
 #include "chrome/browser/content_setting_image_model.h"
 #include "chrome/browser/defaults.h"
-#include "chrome/browser/extensions/extension_accessibility_api_constants.h"
 #include "chrome/browser/extensions/extension_browser_event_router.h"
 #include "chrome/browser/extensions/extension_tabs_module.h"
 #include "chrome/browser/extensions/extensions_service.h"
