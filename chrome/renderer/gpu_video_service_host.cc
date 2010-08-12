8ee8132338ff105f319ed1432dca77311d909acb
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 09:24:36 +0000

    Initialize the members and avoid inlining the constructors.
    BUG=None
    TEST=None
    CID=12377, 12379, 12380, 12381
    
    Review URL: http://codereview.chromium.org/3176004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55863 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/gpu_video_service_host.cc
+++ chrome/renderer/gpu_video_service_host.cc
@@ -9,6 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/gpu_video_decoder_host.h"
 #include "chrome/renderer/render_thread.h"
 
+GpuVideoServiceHost::GpuVideoServiceHost()
+    : router_(NULL),
+      message_loop_(NULL) {
+  memset(&service_info_, 0, sizeof(service_info_));
+}
+
 void GpuVideoServiceHost::OnChannelError() {
   LOG(ERROR) << "GpuVideoServiceHost::OnChannelError";
   channel_host_.release();
