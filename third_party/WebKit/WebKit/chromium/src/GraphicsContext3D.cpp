9d30aef39e6dcd73b3e2889bf07c7f64faf5d06d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-12 07:18:31 +0000

    2010-08-12  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r65221.
            http://trac.webkit.org/changeset/65221
            https://bugs.webkit.org/show_bug.cgi?id=43896
    
            Broke snow leopard build. (Requested by dave_levin on
            #webkit).
    
            * platform/graphics/GraphicsContext3D.cpp:
            * platform/graphics/GraphicsContext3D.h:
            (WebCore::GraphicsContext3D::):
    2010-08-12  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r65221.
            http://trac.webkit.org/changeset/65221
            https://bugs.webkit.org/show_bug.cgi?id=43896
    
            Broke snow leopard build. (Requested by dave_levin on
            #webkit).
    
            * public/WebGraphicsContext3D.h:
            * src/GraphicsContext3D.cpp:
            * src/WebGraphicsContext3DDefaultImpl.cpp:
            * src/WebGraphicsContext3DDefaultImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/GraphicsContext3D.cpp
+++ third_party/WebKit/WebKit/chromium/src/GraphicsContext3D.cpp
@@ -298,7 +298,6 @@ public:
     void deleteTexture(unsigned);
 
     void synthesizeGLError(unsigned long error);
-    bool supportsBGRA();
 
 private:
     OwnPtr<WebKit::WebGraphicsContext3D> m_impl;
@@ -909,7 +908,6 @@ DELEGATE_TO_IMPL_1(deleteShader, unsigned)
 DELEGATE_TO_IMPL_1(deleteTexture, unsigned)
 
 DELEGATE_TO_IMPL_1(synthesizeGLError, unsigned long)
-DELEGATE_TO_IMPL_R(supportsBGRA, bool)
 
 //----------------------------------------------------------------------
 // GraphicsContext3D
@@ -1244,7 +1242,6 @@ DELEGATE_TO_INTERNAL_1(deleteShader, unsigned)
 DELEGATE_TO_INTERNAL_1(deleteTexture, unsigned)
 
 DELEGATE_TO_INTERNAL_1(synthesizeGLError, unsigned long)
-DELEGATE_TO_INTERNAL_R(supportsBGRA, bool)
 
 bool GraphicsContext3D::isGLES2Compliant() const
 {
