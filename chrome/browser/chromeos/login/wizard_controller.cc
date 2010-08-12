1e71550d79c047b0dca5b8243b4bd1314c288d3f
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 06:50:54 +0000

    Try to fix CrOS build (probelm with r55848), part 2.
    BUG=23581
    TEST=builds
    TBR=viettrungluu@chromium.org,brettw@chromium.org
    
    Review URL: http://codereview.chromium.org/3130011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55853 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // A boolean pref of the OOBE complete flag.
-const wchar_t kOobeComplete[] = L"OobeComplete";
+const char kOobeComplete[] = "OobeComplete";
 
 // Path to OEM partner startup customization manifest.
 const char kStartupCustomizationManifestPath[] =
