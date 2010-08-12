7ade825ffa25ba633f410aaa2d9c792793a81327
antonm@chromium.org <antonm@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-12 23:57:29 +0000

    2010-08-12  Anton Muhin  <antonm@chromium.org>
            Reviewed by Nate Chapin.
    
            [v8] Create callbacks for named access of elements from document object on fly.
            https://bugs.webkit.org/show_bug.cgi?id=43725
    
            That should speed up access to methods on document as there is no need to
            consult with interceptor before invoking the method.
    
            This reapplies http://trac.webkit.org/changeset/60670 reverted with
            http://trac.webkit.org/changeset/63845.  The main difference is in treatment
            of removed named elements: we don't remove an accessor any more, but leave it.
            Therefore any accessor should be able to treat the case when there is no
            expando property.
    
            * bindings/scripts/CodeGeneratorV8.pm:
            * bindings/v8/ScriptController.cpp:
            (WebCore::ScriptController::namedItemAdded):
            (WebCore::ScriptController::namedItemRemoved):
            * bindings/v8/V8DOMWindowShell.cpp:
            (WebCore::checkDocumentWrapper):
            (WebCore::V8DOMWindowShell::updateDocumentWrapperCache):
            (WebCore::getter):
            (WebCore::V8DOMWindowShell::namedItemAdded):
            (WebCore::V8DOMWindowShell::namedItemRemoved):
            * bindings/v8/V8DOMWindowShell.h:
            * bindings/v8/V8DOMWrapper.cpp:
            (WebCore::V8DOMWrapper::instantiateV8Object):
            * bindings/v8/custom/V8HTMLDocumentCustom.cpp:
            (WebCore::V8HTMLDocument::WrapInShadowObject):
            (WebCore::V8HTMLDocument::GetNamedProperty):
            (WebCore::V8HTMLDocument::allAccessorSetter):
            (WebCore::toV8):
            * html/HTMLDocument.cpp:
            (WebCore::HTMLDocument::removeItemFromMap):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/V8DOMWindowShell.h
+++ third_party/WebKit/WebCore/bindings/v8/V8DOMWindowShell.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef V8DOMWindowShell_h
 #define V8DOMWindowShell_h
 
+#include "AtomicString.h"
 #include "WrapperTypeInfo.h"
 #include <wtf/Forward.h>
 #include <wtf/HashMap.h>
@@ -43,6 +44,8 @@ namespace WebCore {
 
 class DOMWindow;
 class Frame;
+class HTMLDocument;
+class String;
 
 // V8WindowShell represents all the per-global object state for a Frame that
 // persist between navigations.
@@ -54,6 +57,9 @@ public:
 
     // Update document object of the frame.
     void updateDocument();
+
+    void namedItemAdded(HTMLDocument*, const AtomicString&);
+    void namedItemRemoved(HTMLDocument*, const AtomicString&);
 
     // Update the security origin of a document
     // (e.g., after setting docoument.domain).
