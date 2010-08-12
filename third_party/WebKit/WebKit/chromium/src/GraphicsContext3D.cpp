59c0920902bb263b202231ab3fd0a5e4af3cb22d
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-12 15:26:51 +0000

    2010-08-11  Stephen White  <senorblanco@chromium.org>
            Reviewed by David Levin.
    
            Add support for BGRA pixel format to GraphicsContext3D.
            https://bugs.webkit.org/show_bug.cgi?id=43858
    
            Although this has been standard in desktop GL since 1.2, it's an 
            extension in GL ES 2.0, so a query function is provided to check
            for support.  The DefaultImpl returns false, since it does not yet
            implement the TexImage2D internalFormat quirks.
    
            * public/WebGraphicsContext3D.h:
            (WebKit::WebGraphicsContext3D::supportsBGRA):
            * src/GraphicsContext3D.cpp:
            * src/WebGraphicsContext3DDefaultImpl.cpp:
            (WebKit::WebGraphicsContext3DDefaultImpl::supportsBGRA):
            * src/WebGraphicsContext3DDefaultImpl.h:
    2010-08-11  Stephen White  <senorblanco@chromium.org>
    
            Reviewed by David Levin.
    
            Add support for BGRA pixel format to GraphicsContext3D.
            https://bugs.webkit.org/show_bug.cgi?id=43858
    
            Although this has been standard in desktop GL since 1.2, it's an 
            extension in GL ES 2.0, so a query function is provided to check
            for support.  Since they differ on semantics (GL ES 2.0 requires 
            TexImage2D()'s format and internalFormat to match, while desktop GL
            will not accept BGRA as an internalFormat), the stub implementation
            returns false until these quirks have been implemented and tested on
            each port.
    
            * platform/graphics/GraphicsContext3D.cpp:
            (WebCore::GraphicsContext3D::supportsBGRA):
            Add implementation for non-chromium platforms which returns false.
            * platform/graphics/GraphicsContext3D.h:
            (WebCore::GraphicsContext3D::):
            Add query function (supportsBGRA()) and the BGRA_EXT token.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65247 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/GraphicsContext3D.cpp
+++ third_party/WebKit/WebKit/chromium/src/GraphicsContext3D.cpp
@@ -298,6 +298,7 @@ public:
     void deleteTexture(unsigned);
 
     void synthesizeGLError(unsigned long error);
+    bool supportsBGRA();
 
 private:
     OwnPtr<WebKit::WebGraphicsContext3D> m_impl;
@@ -908,6 +909,7 @@ DELEGATE_TO_IMPL_1(deleteShader, unsigned)
 DELEGATE_TO_IMPL_1(deleteTexture, unsigned)
 
 DELEGATE_TO_IMPL_1(synthesizeGLError, unsigned long)
+DELEGATE_TO_IMPL_R(supportsBGRA, bool)
 
 //----------------------------------------------------------------------
 // GraphicsContext3D
@@ -1242,6 +1244,7 @@ DELEGATE_TO_INTERNAL_1(deleteShader, unsigned)
 DELEGATE_TO_INTERNAL_1(deleteTexture, unsigned)
 
 DELEGATE_TO_INTERNAL_1(synthesizeGLError, unsigned long)
+DELEGATE_TO_INTERNAL_R(supportsBGRA, bool)
 
 bool GraphicsContext3D::isGLES2Compliant() const
 {
