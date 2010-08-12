fa782458c571751e819ccf80a04676ea3f318106
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 09:24:25 +0000

    Uninitialized member variable in UrlRequestJobMetrics.
    BUG=None
    TEST=None
    CID=1470
    
    Review URL: http://codereview.chromium.org/3146009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55862 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_job_metrics.h
+++ net/url_request/url_request_job_metrics.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,7 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class URLRequestJobMetrics {
  public:
-  URLRequestJobMetrics() : total_bytes_read_(0), number_of_read_IO_(0) { }
+  URLRequestJobMetrics()
+      : total_bytes_read_(0),
+        number_of_read_IO_(0),
+        success_(false) { }
   ~URLRequestJobMetrics() { }
 
   // The original url the job has been created for.
