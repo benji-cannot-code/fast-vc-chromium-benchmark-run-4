a28a2b54760f058453096aff82401dd0cfdc4d6b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-12 02:09:17 +0000

    2010-08-11  Joone Hur  <joone@kldp.org>
            Reviewed by Martin Robinson.
    
            [GTK] Last Hangul letter is typed again when a composition is finished with mouse press
            https://bugs.webkit.org/show_bug.cgi?id=40518
    
            When a mouse press fires during a IME composition, the current composition character
            can be entered twice at the previous editing position and a new editing position.
            Because the IME commit signal is emitted after the mouse press event.
            This patch allows to prevent the commit signal during a composition when a mouse press fires.
    
            * WebCoreSupport/EditorClientGtk.cpp:
            (WebKit::imContextCommitted):
            (WebKit::EditorClient::handleInputMethodKeydown): Allow to accept the next composition commit.
            (WebKit::EditorClient::handleInputMethodMousePress): Added for handling IME when a mouse press fires.
            (WebKit::EditorClient::EditorClient):  Initialize m_preventNextCompositionCommit
            * WebCoreSupport/EditorClientGtk.h:
            (WebKit::EditorClient::preventNextCompositionCommit): Added for checking whether skipping a commit.
            * webkit/webkitwebview.cpp:
            (webkit_web_view_button_press_event): Call handleInputMethodMousePress()
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65209 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/WebCoreSupport/EditorClientGtk.h
+++ third_party/WebKit/WebKit/gtk/WebCoreSupport/EditorClientGtk.h
@@ -62,6 +62,7 @@ namespace WebKit {
         ~EditorClient();
         WebKitWebView* webView() { return m_webView; }
         bool treatContextCommitAsKeyEvent() { return m_treatContextCommitAsKeyEvent; }
+        bool preventNextCompositionCommit() { return m_preventNextCompositionCommit; }
         void clearPendingComposition() { m_pendingComposition.set(0); }
         bool hasPendingComposition() { return m_pendingComposition; }
         void addPendingEditorCommand(const char* command) { m_pendingEditorCommands.append(command); }
@@ -113,6 +114,7 @@ namespace WebKit {
 
         virtual void handleKeyboardEvent(WebCore::KeyboardEvent*);
         virtual void handleInputMethodKeydown(WebCore::KeyboardEvent*);
+        virtual void handleInputMethodMousePress();
 
         virtual void textFieldDidBeginEditing(WebCore::Element*);
         virtual void textFieldDidEndEditing(WebCore::Element*);
@@ -136,6 +138,7 @@ namespace WebKit {
 
     private:
         WebKitWebView* m_webView;
+        bool m_preventNextCompositionCommit;
         bool m_treatContextCommitAsKeyEvent;
         GOwnPtr<gchar> m_pendingComposition;
         Vector<const char*> m_pendingEditorCommands;
