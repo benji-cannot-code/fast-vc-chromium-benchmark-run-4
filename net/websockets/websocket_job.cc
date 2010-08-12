0e69826be8ef78e2f77cfa104592343d5f765cd7
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 04:15:21 +0000

    Add WebSocket handshake message in net_log
    BUG=none
    TEST=websocket handshake message shown in chrome://net-internals/#requests
    
    Review URL: http://codereview.chromium.org/3076043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55845 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_job.cc
+++ net/websockets/websocket_job.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_tokenizer.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_errors.h"
+#include "net/base/net_log.h"
 #include "net/base/cookie_policy.h"
 #include "net/base/cookie_store.h"
 #include "net/base/io_buffer.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context.h"
 #include "net/websockets/websocket_frame_handler.h"
 #include "net/websockets/websocket_handshake_handler.h"
+#include "net/websockets/websocket_net_log_params.h"
 #include "net/websockets/websocket_throttle.h"
 
 namespace {
@@ -310,6 +312,9 @@ void WebSocketJob::OnCanGetCookiesCompleted(int policy) {
 
     const std::string& handshake_request = handshake_request_->GetRawRequest();
     handshake_request_sent_ = 0;
+    socket_->net_log()->AddEvent(
+        NetLog::TYPE_WEB_SOCKET_SEND_REQUEST_HEADERS,
+        new NetLogWebSocketHandshakeParameter(handshake_request));
     socket_->SendData(handshake_request.data(),
                       handshake_request.size());
   }
@@ -348,6 +353,10 @@ void WebSocketJob::OnReceivedHandshakeResponse(
     return;
   }
   // handshake message is completed.
+  socket_->net_log()->AddEvent(
+      NetLog::TYPE_WEB_SOCKET_READ_RESPONSE_HEADERS,
+      new NetLogWebSocketHandshakeParameter(
+          handshake_response_->GetRawResponse()));
   if (len - response_length > 0) {
     // If we received extra data, it should be frame data.
     receive_frame_handler_->AppendData(data + response_length,
