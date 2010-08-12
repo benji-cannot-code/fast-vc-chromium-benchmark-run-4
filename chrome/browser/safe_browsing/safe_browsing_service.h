ef4340210f1ed182617536476910fb3739f6d3ec
panayiotis@google.com <panayiotis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 23:59:51 +0000

    Report malware redirectors as well. BUG=none TEST=unit_tests Review URL: http://codereview.chromium.org/3028040
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55966 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_service.h
+++ chrome/browser/safe_browsing/safe_browsing_service.h
@@ -267,7 +267,8 @@ class SafeBrowsingService
   // service.
   void ReportMalware(const GURL& malware_url,
                      const GURL& page_url,
-                     const GURL& referrer_url);
+                     const GURL& referrer_url,
+                     bool is_subresource);
 
   CurrentChecks checks_;
 
