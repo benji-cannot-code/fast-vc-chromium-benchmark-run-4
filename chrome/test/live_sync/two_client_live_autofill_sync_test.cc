9ee30090b40c91a28b48b072bfd0aa52922a7d50
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 21:20:34 +0000

    Marking Client1HasData test as failed due to bug 51956.
    The TwoClientLiveAutofillSyncTest.Client1HasData test fails due to bug
    51956. Marking it as FAILS until the bug is fixed.
    
    TBR=tim@chromium.org
    
    BUG=51956
    TEST=TwoClientLiveAutofillSyncTest.Client1HasData
    
    Review URL: http://codereview.chromium.org/3146013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55948 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/two_client_live_autofill_sync_test.cc
+++ chrome/test/live_sync/two_client_live_autofill_sync_test.cc
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome/test/live_sync/live_autofill_sync_test.h"
 
-IN_PROC_BROWSER_TEST_F(TwoClientLiveAutofillSyncTest, Client1HasData) {
+// TODO(rsimha): Remove FAILS prefix after crbug.com/51956 is fixed.
+IN_PROC_BROWSER_TEST_F(TwoClientLiveAutofillSyncTest,
+                       FAILS_Client1HasData) {
   ASSERT_TRUE(SetupClients()) << "SetupClients() failed.";
 
   AutofillKeys keys;
