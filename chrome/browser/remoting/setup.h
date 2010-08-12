ed85adf4f6edea401aec2f9e0673ef5c37b4f7ee
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 00:18:32 +0000

    Adding remoting setup to wrench menu
    pranvak@ is OOO so I'm submitting for him.
    
    This patch will add a menu in wrench menu to start the chromoting
    setup flow.
    
    Submit for: pranvak@chromium.org
    Reviewed: http://codereview.chromium.org/3075024/show
    
    Review URL: http://codereview.chromium.org/3134003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55815 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/remoting/setup.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_REMOTING_SETUP_H_
+#define CHROME_BROWSER_REMOTING_SETUP_H_
+
+#include "base/string16.h"
+
+class Profile;
+
+namespace remoting_setup {
+
+// Open the appropriate setup dialog for the given profile (which can be
+// incognito).
+void OpenRemotingSetupDialog(Profile* profile);
+}  // namespace remoting_setup
+
+#endif  // CHROME_BROWSER_REMOTING_SETUP_H_
