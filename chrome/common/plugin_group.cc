a1dd7b1b3dee15b72cfc695fca81cf2daa2baaef
panayiotis@google.com <panayiotis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 20:49:29 +0000

    Update latest secure version of Quicktime for Windows. See http://support.apple.com/kb/HT4290 BUG=None TEST=unit_tests
    Review URL: http://codereview.chromium.org/3159016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56083 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/plugin_group.cc
+++ chrome/common/plugin_group.cc
@@ -37,7 +37,7 @@ static const PluginGroupDefinition kGroupDefinitions[] = {
 // TODO(panayiotis): We should group "RealJukebox NS Plugin" with the rest of
 // the RealPlayer files.
 static const PluginGroupDefinition kGroupDefinitions[] = {
-  { "Quicktime", "QuickTime Plug-in", "", "", "7.6.6",
+  { "Quicktime", "QuickTime Plug-in", "", "", "7.6.7",
     "http://www.apple.com/quicktime/download/" },
   { "Java 6", "Java", "", "6", "6.0.200",
     "http://www.java.com/" },
@@ -352,4 +352,3 @@ void PluginGroup::Enable(bool enable) {
     }
   }
 }
-
