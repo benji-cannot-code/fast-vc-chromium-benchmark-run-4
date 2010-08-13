be40609be23879d333730379cb888022656651b1
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 04:45:36 +0000

    Convert wide strings in extension_web_navigation_api_constants.
    BUG=23581
    TEST=builds
    
    Review URL: http://codereview.chromium.org/3137008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55991 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_web_navigation_api_constants.h
+++ chrome/browser/extensions/extension_web_navigation_api_constants.h
@@ -12,16 +12,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extension_web_navigation_api_constants {
 
 // Keys.
-extern const wchar_t kErrorKey[];
-extern const wchar_t kFrameIdKey[];
-extern const wchar_t kRequestIdKey[];
-extern const wchar_t kSourceTabIdKey[];
-extern const wchar_t kSourceUrlKey[];
-extern const wchar_t kTabIdKey[];
-extern const wchar_t kTargetUrlKey[];
-extern const wchar_t kTimeStampKey[];
-extern const wchar_t kTransitionTypeKey[];
-extern const wchar_t kUrlKey[];
+extern const char kErrorKey[];
+extern const char kFrameIdKey[];
+extern const char kRequestIdKey[];
+extern const char kSourceTabIdKey[];
+extern const char kSourceUrlKey[];
+extern const char kTabIdKey[];
+extern const char kTargetUrlKey[];
+extern const char kTimeStampKey[];
+extern const char kTransitionTypeKey[];
+extern const char kUrlKey[];
 
 // Events.
 extern const char kOnBeforeNavigate[];
