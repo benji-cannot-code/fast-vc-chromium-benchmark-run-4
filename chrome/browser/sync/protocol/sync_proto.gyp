8756aed09a279bbdaf9ecfb5fad668f9eec1e580
jerrica@google.com <jerrica@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 01:01:53 +0000

    sync: Add sessions to protocol and chromiumsync python test server.
    BUG=30519
    Review URL: http://codereview.chromium.org/3107008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55973 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/protocol/sync_proto.gyp
+++ chrome/browser/sync/protocol/sync_proto.gyp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'nigori_specifics.proto',
         'password_specifics.proto',
         'preference_specifics.proto',
+        'session_specifics.proto',
         'theme_specifics.proto',
         'typed_url_specifics.proto',
       ],
--- net/tools/testserver/chromiumsync.py
+++ net/tools/testserver/chromiumsync.py
@@ -22,6 +22,7 @@ import password_specifics_pb2
 import preference_specifics_pb2
 import theme_specifics_pb2
 import typed_url_specifics_pb2
+import session_specifics_pb2
 import sync_pb2
 
 # An enumeration of the various kinds of data that can be synced.
@@ -36,9 +37,9 @@ ALL_TYPES = (
     NIGORI,
     PASSWORD,
     PREFERENCE,
-    # SESSION,
+    SESSION,
     THEME,
-    TYPED_URL) = range(9)
+    TYPED_URL) = range(10)
 
 # Given a sync type from ALL_TYPES, find the extension token corresponding
 # to that datatype.  Note that TOP_LEVEL has no such token.
@@ -49,7 +50,7 @@ SYNC_TYPE_TO_EXTENSION = {
     NIGORI: nigori_specifics_pb2.nigori,
     PASSWORD: password_specifics_pb2.password,
     PREFERENCE: preference_specifics_pb2.preference,
-    # SESSION: session_specifics_pb2.session,     # Disabled
+    SESSION: session_specifics_pb2.session,
     THEME: theme_specifics_pb2.theme,
     TYPED_URL: typed_url_specifics_pb2.typed_url,
     }
@@ -167,9 +168,8 @@ class SyncDataModel(object):
                     parent_tag='google_chrome', sync_type=EXTENSIONS),
       PermanentItem('google_chrome_passwords', name='Passwords',
                     parent_tag='google_chrome', sync_type=PASSWORD),
-      # TODO(rsimha): Disabled since the protocol does not support it yet.
-      # PermanentItem('google_chrome_sessions', name='Sessions',
-      #               parent_tag='google_chrome', SESSION),
+      PermanentItem('google_chrome_sessions', name='Sessions',
+                    parent_tag='google_chrome', sync_type=SESSION),
       PermanentItem('google_chrome_themes', name='Themes',
                     parent_tag='google_chrome', sync_type=THEME),
       PermanentItem('google_chrome_typed_urls', name='Typed URLs',
