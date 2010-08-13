a783cc9d345c8a36bccf0d3c3accdcd1477f27fb
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 20:43:58 +0000

    FBTF: Mark the Read methods in the IPC subsystem as noinline.
    This forces all the ReadParam template junk to expand once in the *_messages.cc
    file, instead of at every Read() call site. Without the compiler-specific
    annotation, this builds and links in debug mode, but doesn't link in release
    mode because the individual Read() methods generated were inlined into the
    subclass Log() methods, causing disaster on the release builders, but not on
    the trybots or locally.
    
    BUG=51411
    TEST=none
    
    Review URL: http://codereview.chromium.org/3160008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56081 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_message_impl_macros.h
+++ ipc/ipc_message_impl_macros.h
@@ -201,7 +201,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                                                         \
   void msg_class::Log(const Message* msg, std::wstring* l) {            \
     if (msg->is_sync()) {                                               \
-      SendParam p;                                                      \
+      TupleTypes<SendParam>::ValueTuple p;                              \
       if (ReadSendParam(msg, &p))                                       \
         IPC::LogParam(p, l);                                            \
                                                                         \
