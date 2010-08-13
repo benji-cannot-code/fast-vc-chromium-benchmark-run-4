79ce82357e7a9c105a607df4cef771983d9ada2e
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 15:43:49 +0000

    Convert wide strings in Chrome OS DOM UI code.
    BUG=23581
    TEST=builds and passes tests
    
    Review URL: http://codereview.chromium.org/3159015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56030 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dom_ui/system_options_handler.h
+++ chrome/browser/chromeos/dom_ui/system_options_handler.h
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_DOM_UI_SYSTEM_OPTIONS_HANDLER_H_
 #pragma once
 
-#include <string>
 #include <vector>
 
+#include "base/string16.h"
 #include "chrome/browser/dom_ui/options_ui.h"
 #include "third_party/icu/public/i18n/unicode/timezone.h"
 
@@ -27,10 +27,10 @@ class SystemOptionsHandler : public OptionsPageUIHandler {
   ListValue* GetTimezoneList();
 
   // Gets timezone name.
-  std::wstring GetTimezoneName(const icu::TimeZone* timezone);
+  string16 GetTimezoneName(const icu::TimeZone* timezone);
 
   // Gets timezone ID which is also used as timezone pref value.
-  std::wstring GetTimezoneID(const icu::TimeZone* timezone);
+  string16 GetTimezoneID(const icu::TimeZone* timezone);
 
   // Timezones.
   std::vector<icu::TimeZone*> timezones_;
