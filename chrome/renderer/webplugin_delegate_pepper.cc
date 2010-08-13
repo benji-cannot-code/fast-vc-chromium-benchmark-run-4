57568780f65da527ee65f5943a05178b994b33f9
neb@chromium.org <neb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 16:44:55 +0000

    Fix building with enable_gpu=0
    BUG=crbug.com/51459
    TEST=passed compilation
    
    Review URL: http://codereview.chromium.org/3168008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56041 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/webplugin_delegate_pepper.cc
+++ chrome/renderer/webplugin_delegate_pepper.cc
@@ -67,7 +67,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/plugins/plugin_constants_win.h"
 #endif
 
+#if defined(ENABLE_GPU)
 using gpu::Buffer;
+#endif
+
 using webkit_glue::WebPlugin;
 using webkit_glue::WebPluginDelegate;
 using webkit_glue::WebPluginResourceClient;
@@ -85,7 +88,9 @@ struct Device2DImpl {
 };
 
 struct Device3DImpl {
+#if defined(ENABLE_GPU)
   gpu::CommandBuffer* command_buffer;
+#endif
   bool dynamically_created;
 };
 
@@ -1387,9 +1392,9 @@ WebPluginDelegatePepper::WebPluginDelegatePepper(
 #endif  // (OS_LINUX)
 #if defined(ENABLE_GPU)
       command_buffer_(NULL),
+      method_factory3d_(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
 #endif
       find_identifier_(-1),
-      method_factory3d_(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
       current_choose_file_callback_(NULL),
       current_choose_file_user_data_(NULL) {
   // For now we keep a window struct, although it isn't used.
