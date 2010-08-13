e8693511dd26fa28ba2368dbf0d11f96b8d68f10
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-13 17:02:07 +0000

    2010-08-13  Steve Block  <steveblock@google.com>
            Reviewed by Alexey Proskuryakov.
    
            Geolocation activity started after frame has been disconnected can cause crash
            https://bugs.webkit.org/show_bug.cgi?id=39879
    
            New requests started after the Frame has been disconnected are ignored. We do
            not invoke the error callback as this would allow buggy or malicious pages to
            hose the CPU. Such a page could hold a reference to a Geolocation object from
            a since closed Page and register new requests from the error callback to
            create an infinite loop.
    
            Tests: fast/dom/Geolocation/disconnected-frame-already.html
    
            * page/Geolocation.cpp:
    2010-08-13  Steve Block  <steveblock@google.com>
    
            Reviewed by Alexey Proskuryakov.
    
            Geolocation activity started after frame has been disconnected can cause crash
            https://bugs.webkit.org/show_bug.cgi?id=39879
    
            Added new tests to GTK skipped list.
    
            * fast/dom/Geolocation/disconnected-frame-already.html: Added.
            * fast/dom/Geolocation/disconnected-frame-already-expected.txt: Added.
            * fast/dom/Geolocation/script-tests/disconnected-frame-already.js: Added.
            * fast/dom/Geolocation/resources/disconnected-frame-already-inner1.html: Added.
            * fast/dom/Geolocation/resources/disconnected-frame-already-inner2.html: Added.
            * platform/gtk/Skipped:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65329 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Geolocation.cpp
+++ third_party/WebKit/WebCore/page/Geolocation.cpp
@@ -250,6 +250,9 @@ Geoposition* Geolocation::lastPosition()
 
 void Geolocation::getCurrentPosition(PassRefPtr<PositionCallback> successCallback, PassRefPtr<PositionErrorCallback> errorCallback, PassRefPtr<PositionOptions> options)
 {
+    if (!m_frame)
+        return;
+
     RefPtr<GeoNotifier> notifier = startRequest(successCallback, errorCallback, options);
     ASSERT(notifier);
 
@@ -258,6 +261,9 @@ void Geolocation::getCurrentPosition(PassRefPtr<PositionCallback> successCallbac
 
 int Geolocation::watchPosition(PassRefPtr<PositionCallback> successCallback, PassRefPtr<PositionErrorCallback> errorCallback, PassRefPtr<PositionOptions> options)
 {
+    if (!m_frame)
+        return 0;
+
     RefPtr<GeoNotifier> notifier = startRequest(successCallback, errorCallback, options);
     ASSERT(notifier);
 
