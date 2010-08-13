f78c936c3b36487659d16f5a5fa42393846d7378
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 20:00:28 +0000

    Include needed header for r56066; build fix.
    TBR
    
    Review URL: http://codereview.chromium.org/3119013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56070 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/infobar_test_helper.h
+++ chrome/browser/cocoa/infobar_test_helper.h
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/tab_contents/infobar_delegate.h"
 
+#include "base/utf_string_conversions.h"
+
 namespace {
 const char kMockAlertInfoBarMessage[] = "MockAlertInfoBarMessage";
 const char kMockLinkInfoBarMessage[] = "MockLinkInfoBarMessage";
