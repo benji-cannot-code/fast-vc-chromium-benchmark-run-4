02dc059796e8a03cd425eae242def3fc4891613c
nshkrob@chromium.org <nshkrob@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 20:27:15 +0000

    Add TopSites::ProcessPendingCallbacks and call it after ReadDatabase.
    Fix the test and unmark as FLAKY.
    
    BUG=51721
    TEST=unit_tests, ui_tests
    
    Review URL: http://codereview.chromium.org/3139009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56075 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/top_sites_unittest.cc
+++ chrome/browser/history/top_sites_unittest.cc
@@ -83,7 +83,7 @@ class TopSitesTest : public testing::Test {
   }
 
   // Callback for TopSites::GetMostVisitedURLs.
-  void OnTopSitesAvailable(const history::MostVisitedURLList& data) {
+  void OnTopSitesAvailable(history::MostVisitedURLList data) {
     urls_ = data;
     number_of_callbacks_++;
   }
@@ -977,6 +977,7 @@ TEST_F(TopSitesTest, QueueingRequestsForTopSites) {
 }
 
 TEST_F(TopSitesTest, CancelingRequestsForTopSites) {
+  ChromeThread db_loop(ChromeThread::DB, MessageLoop::current());
   CancelableRequestConsumer c1;
   CancelableRequestConsumer c2;
   top_sites().GetMostVisitedURLs(
@@ -1011,6 +1012,7 @@ TEST_F(TopSitesTest, CancelingRequestsForTopSites) {
   pages.push_back(url);
 
   top_sites().OnTopSitesAvailable(0, pages);
+  MessageLoop::current()->RunAllPending();
 
   // 1 request was canceled.
   EXPECT_EQ(2u, number_of_callbacks());
