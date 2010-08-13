c987188d07de83243fec27318f71e8ff9651f189
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-13 01:05:10 +0000

    Change UString constructors to match those in WTF::String. This changes behaviour of UString((char*)0) to create null strings, akin to UString() rather than UString::empty(). (This matches String).  Remove unused constructors from UString, and add null-terminated UTF-16 constructor, to match String.  Move String's constructor into the .cpp to match UString.
    Reviewed by Sam Weinig
    
    * JavaScriptCore.exp:
    * debugger/DebuggerCallFrame.cpp:
    (JSC::DebuggerCallFrame::calculatedFunctionName):
    * runtime/RegExpKey.h:
    (JSC::RegExpKey::RegExpKey):
    * runtime/SmallStrings.cpp:
    (JSC::SmallStrings::createSingleCharacterString):
    * runtime/UString.cpp:
    (JSC::UString::UString):
    * runtime/UString.h:
    (JSC::UString::UString):
    (JSC::UString::swap):
    (JSC::UString::adopt):
    (JSC::UString::operator[]):
    * wtf/text/WTFString.h:
    (WTF::String::String):
    (WTF::String::adopt):
    (WTF::String::operator[]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65286 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/RegExpKey.h
+++ third_party/WebKit/JavaScriptCore/runtime/RegExpKey.h
@@ -59,6 +59,12 @@ struct RegExpKey {
     {
     }
 
+    RegExpKey(int flags, const RefPtr<StringImpl>& pattern)
+        : flagsValue(flags)
+        , pattern(pattern)
+    {
+    }
+
     RegExpKey(const UString& flags, const UString& pattern)
         : pattern(pattern.impl())
     {
