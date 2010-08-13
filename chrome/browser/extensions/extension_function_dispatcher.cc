a244d48f7d1cd8e55a63c7522f7229c7ae299aa0
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 05:16:19 +0000

    Convert wide strings in extension_tabs_module_constants.
    BUG=23581
    TEST=builds and passes tests
    
    Review URL: http://codereview.chromium.org/3181010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55995 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_dispatcher.cc
+++ chrome/browser/extensions/extension_function_dispatcher.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_proxy_api.h"
 #include "chrome/browser/extensions/extension_rlz_module.h"
 #include "chrome/browser/extensions/extension_tabs_module.h"
-#include "chrome/browser/extensions/extension_tabs_module_constants.h"
 #include "chrome/browser/extensions/extension_test_api.h"
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/extensions/extension_tts_api.h"
--- chrome/browser/extensions/extension_host.cc
+++ chrome/browser/extensions/extension_host.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/dom_ui/dom_ui_factory.h"
 #include "chrome/browser/extensions/extension_message_service.h"
 #include "chrome/browser/extensions/extension_tabs_module.h"
-#include "chrome/browser/extensions/extension_tabs_module_constants.h"
 #include "chrome/browser/extensions/extensions_service.h"
 #include "chrome/browser/in_process_webkit/dom_storage_context.h"
 #include "chrome/browser/in_process_webkit/webkit_context.h"
