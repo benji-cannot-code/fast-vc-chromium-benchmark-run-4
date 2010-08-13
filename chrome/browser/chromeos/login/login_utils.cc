2ed5e94f105c99f12eee40a7e4b9a97dadcd2721
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 18:49:02 +0000

    TBR: Fix chromeos build.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56058 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/singleton.h"
 #include "base/string_util.h"
 #include "base/time.h"
+#include "base/utf_string_conversions.h"
 #include "chrome/browser/browser_init.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_thread.h"
