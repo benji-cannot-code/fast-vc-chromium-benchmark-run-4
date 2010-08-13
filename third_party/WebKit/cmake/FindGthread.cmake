e2bcc9d04c7eeedb278d60720a06d5e24d08bd35
leandro@webkit.org <leandro@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-13 17:35:49 +0000

    2010-08-13  Leandro Pereira  <leandro@profusion.mobi>
            [EFL] Unreviewed build fix.
    
            * cmake/FindGthread.cmake: Fix warnings about obsolete PKGCONFIG macro.
            * cmake/WebKitGenerators.cmake: FLEX_EXECUTABLE was defined already
            by the root CMakeLists.txt file, so remove relevant lines that
            depends on FIND_PACKAGE_HANDLE_STANDARD_ARGS macro, which isn't
            available anymore.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65332 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/cmake/FindGthread.cmake
+++ third_party/WebKit/cmake/FindGthread.cmake
@@ -5,18 +5,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # GTHREAD_LIBRARIES   Libraries to link against to use GTHREAD
 # GTHREAD_FOUND       GTHREAD was found
 
-IF (UNIX)
-    INCLUDE (UsePkgConfig)
-    PKGCONFIG (gthread-2.0 GTHREAD_include_dir GTHREAD_link_dir GTHREAD_libraries GTHREAD_include)
-    IF (GTHREAD_include AND GTHREAD_libraries)
-        SET (GTHREAD_FOUND TRUE)
-        EXEC_PROGRAM ("echo"
-            ARGS "${GTHREAD_include} | sed 's/[[:blank:]]*-I/;/g'"
-            OUTPUT_VARIABLE GTHREAD_INCLUDE
-        )
-        SET (GTHREAD_INCLUDE-I ${GTHREAD_include})
-        SET (GTHREAD_LIBRARIES ${GTHREAD_libraries})
-    ELSE (GTHREAD_include AND GTHREAD_libraries)
-        SET (GTHREAD_FOUND FALSE)
-    ENDIF (GTHREAD_include AND GTHREAD_libraries)
-ENDIF (UNIX)
+INCLUDE(FindPkgConfig)
+PKG_CHECK_MODULES (Gthread REQUIRED gthread-2.0>=2.20.0)
