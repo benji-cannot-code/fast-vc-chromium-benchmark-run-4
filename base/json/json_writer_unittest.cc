6ef4f98dcc26782700fd9e895ea96eb4f50da471
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-14 03:32:17 +0000

    Remove deprecated wstring DictionaryValue::SetWithoutPathExpansion().
    BUG=23581
    TEST=builds and passes tests
    
    Review URL: http://codereview.chromium.org/3121017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56128 0039d316-1c4b-4281-b951-d872f2087c98

--- base/json/json_writer_unittest.cc
+++ base/json/json_writer_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -80,18 +80,18 @@ TEST(JSONWriterTest, Writing) {
 
   // Test keys with periods
   DictionaryValue period_dict;
-  period_dict.SetWithoutPathExpansion(L"a.b", Value::CreateIntegerValue(3));
-  period_dict.SetWithoutPathExpansion(L"c", Value::CreateIntegerValue(2));
+  period_dict.SetWithoutPathExpansion("a.b", Value::CreateIntegerValue(3));
+  period_dict.SetWithoutPathExpansion("c", Value::CreateIntegerValue(2));
   DictionaryValue* period_dict2 = new DictionaryValue;
-  period_dict2->SetWithoutPathExpansion(L"g.h.i.j",
+  period_dict2->SetWithoutPathExpansion("g.h.i.j",
                                         Value::CreateIntegerValue(1));
-  period_dict.SetWithoutPathExpansion(L"d.e.f", period_dict2);
+  period_dict.SetWithoutPathExpansion("d.e.f", period_dict2);
   JSONWriter::Write(&period_dict, false, &output_js);
   ASSERT_EQ("{\"a.b\":3,\"c\":2,\"d.e.f\":{\"g.h.i.j\":1}}", output_js);
 
   DictionaryValue period_dict3;
   period_dict3.Set(L"a.b", Value::CreateIntegerValue(2));
-  period_dict3.SetWithoutPathExpansion(L"a.b", Value::CreateIntegerValue(1));
+  period_dict3.SetWithoutPathExpansion("a.b", Value::CreateIntegerValue(1));
   JSONWriter::Write(&period_dict3, false, &output_js);
   ASSERT_EQ("{\"a\":{\"b\":2},\"a.b\":1}", output_js);
 }
--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -2473,7 +2473,7 @@ void TabContents::UpdateInspectorSetting(const std::string& key,
   DictionaryValue* inspector_settings =
       profile()->GetPrefs()->GetMutableDictionary(
           prefs::kWebKitInspectorSettings);
-  inspector_settings->SetWithoutPathExpansion(UTF8ToWide(key),
+  inspector_settings->SetWithoutPathExpansion(key,
                                               Value::CreateStringValue(value));
 }
 
