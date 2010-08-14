b0c82ea3473262225c7bef75e371b04d379828ab
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-14 02:23:43 +0000

    Windows build fix.
    * WebCore.xcodeproj/project.pbxproj:
    * platform/win/SharedBufferWin.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65349 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/SharedBufferWin.cpp
+++ third_party/WebKit/WebCore/platform/win/SharedBufferWin.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "SharedBuffer.h"
+#include <wtf/text/CString.h>
 
 // INVALID_FILE_SIZE is not defined on WinCE.
 #ifndef INVALID_FILE_SIZE
