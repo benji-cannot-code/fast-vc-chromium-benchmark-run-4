b8fc24cd958534919aeab859a3b30a05c77aaf00
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-14 16:11:10 +0000

    Convert wide strings in WebResourceService.
    BUG=23581
    TEST=builds and passes tests
    
    Review URL: http://codereview.chromium.org/3130020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56139 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/web_resource_service.h
+++ chrome/browser/web_resource/web_resource_service.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,9 +28,8 @@ class WebResourceService
   // the process that will parse the JSON, and then update the cache.
   void UpdateResourceCache(const std::string& json_data);
 
-  static const wchar_t* kTipDictionaryPrefName;
-  static const wchar_t* kCurrentTipPrefName;
-  static const wchar_t* kTipCachePrefName;
+  static const char* kCurrentTipPrefName;
+  static const char* kTipCachePrefName;
 
   // Default server from which to gather resources.
   static const char* kDefaultResourceServer;
