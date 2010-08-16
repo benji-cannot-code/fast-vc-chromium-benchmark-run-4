33537fcded37612c493240302c4f6d69cf5fc27d
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-16 00:12:49 +0000

    [wx] Build fix, add missing header.
    git-svn-id: svn://svn.chromium.org/blink/trunk@65387 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/WOFFFileFormat.cpp
+++ third_party/WebKit/WebCore/platform/graphics/WOFFFileFormat.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SharedBuffer.h"
 
 #if !PLATFORM(WIN)
+#include <netinet/in.h>
 #include <zlib.h>
 #if PLATFORM(BREWMP)
 #include <AEEStdLib.h>
