227cc8d7bb39b804c4a6dfd7b3d5c5efc4394aa6
jiesun@google.com <jiesun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-16 20:38:46 +0000

    meida:fix a typo
    Review URL: http://codereview.chromium.org/3184006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56230 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_video_decoder.cc
+++ media/filters/ffmpeg_video_decoder.cc
@@ -95,7 +95,7 @@ void FFmpegVideoDecoder::Initialize(DemuxerStream* demuxer_stream,
   }
   config.opaque_context_ = av_stream;
   config.width_ = width_;
-  config.height_ = config.height_;
+  config.height_ = height_;
   decode_engine_->Initialize(message_loop(), this, config);
 }
 
