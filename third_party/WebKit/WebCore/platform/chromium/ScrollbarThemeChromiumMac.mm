ef0886ae109a7481c8d19e7e8f4cf9c747732013
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-16 23:57:18 +0000

    2010-08-16  Nate Chapin  <japhet@chromium.org>
            Unreviewed, Chromium expectations update.
    
            Mark 3 tests failing on Chromium Win/Linux after r65449.
            They appear to have same root cause as
            bugs.webkit.org/show_bug.cgi?id=43912.
    
            * platform/chromium/test_expectations.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/ScrollbarThemeChromiumMac.mm
+++ third_party/WebKit/WebCore/platform/chromium/ScrollbarThemeChromiumMac.mm
@@ -454,7 +454,7 @@ bool ScrollbarThemeChromiumMac::paint(Scrollbar* scrollbar, GraphicsContext* con
     }
 
     if (!canDrawDirectly)
-        context->drawImageBuffer(imageBuffer, DeviceColorSpace, scrollbar->frameRect().location());
+        context->drawImageBuffer(imageBuffer.get(), DeviceColorSpace, scrollbar->frameRect().location());
 
     return true;
 }
