fbdbb7fb34fe474c4be5b73d0d53161ef97692b9
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-16 20:38:42 +0000

    Remove test isolation enforcement code.
    I think it was a failed experiment, and it only
    ever worked for base_unittests.
    
    This removes some potentially risky code from LazyInstance
    that was only needed for the test isolation.
    
    The lesson from this experiment is that removing singletons
    works better than trying to make tests work fine even when
    singletons are overused.
    
    TEST=none
    BUG=12710
    
    Review URL: http://codereview.chromium.org/3127012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56229 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/run_all_unittests.cc
+++ base/test/run_all_unittests.cc
@@ -6,7 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_suite.h"
 
 int main(int argc, char** argv) {
-  TestSuite test_suite(argc, argv);
-  test_suite.EnforceTestIsolation();
-  return test_suite.Run();
+  return TestSuite(argc, argv).Run();
 }
