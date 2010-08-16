05f76bddd7d220b18824b28886c850b1103d60d1
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-16 06:42:48 +0000

    cros/input_method_library clean up.
    - Remove unused variable kDefaultKeyboardLayout.
    - Fix  bad indent.
    - Add comment to SetImeConfig().
    
    BUG=none
    TEST=manually
    
    Review URL: http://codereview.chromium.org/3148014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56186 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/input_method_library.cc
+++ chrome/browser/chromeos/cros/input_method_library.cc
@@ -37,9 +37,6 @@ bool FindAndUpdateProperty(const chromeos::ImeProperty& new_prop,
   return false;
 }
 
-// The default keyboard layout.
-const char kDefaultKeyboardLayout[] = "us";
-
 }  // namespace
 
 namespace chromeos {
@@ -685,8 +682,8 @@ class InputMethodLibraryStubImpl : public InputMethodLibrary {
     descriptions->push_back(InputMethodDescriptor(
         "xkb:gb:extd:eng", "United Kingdom - Extended - Winkeys", "gb(extd)",
         "eng"));
-  return descriptions;
-}
+    return descriptions;
+  }
 
   InputMethodDescriptor previous_input_method_;
   InputMethodDescriptor current_input_method_;
