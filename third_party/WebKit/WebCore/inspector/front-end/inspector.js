55882f862f6040374c30e2a81f05ae3218def052
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-16 12:23:47 +0000

    2010-08-16  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Joseph Pecoraro.
    
            Web Inspector: upstream frontend-side WebSocket transport.
            https://bugs.webkit.org/show_bug.cgi?id=43970
    
            Chromium already has an alternate WebSocket-based communication channel with
            the backend. Upstreaming it in this change. We will agree on the URI
            of the remote service as the protocol matures.
    
            * inspector/front-end/inspector.js:
            (.WebInspector.socket.onmessage):
            (.WebInspector.socket.onerror):
            (.WebInspector.socket.onopen):
            (WebInspector.loaded):
            (WebInspector.doLoadedDone):
            (WebInspector_syncDispatch):
    2010-08-16  Pavel Feldman  <pfeldman@chromium.org>
    
            Reviewed by Joseph Pecoraro.
    
            Web Inspector: upstream frontend-side WebSocket transport.
            https://bugs.webkit.org/show_bug.cgi?id=43970
    
            Chromium already has an alternate WebSocket-based communication channel with
            the backend. Upstreaming it in this change. We will agree on the URI
            of the remote service as the protocol matures.
    
            * src/WebDevToolsFrontendImpl.cpp:
            (WebKit::WebDevToolsFrontendImpl::dispatchOnInspectorFrontend):
            * src/js/DevTools.js:
            (WebInspector.loaded):
            (devtools.domContentLoaded):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/inspector.js
+++ third_party/WebKit/WebCore/inspector/front-end/inspector.js
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-function preloadImages()
+(function preloadImages()
 {
     (new Image()).src = "Images/clearConsoleButtonGlyph.png";
     (new Image()).src = "Images/consoleButtonGlyph.png";
@@ -46,9 +46,7 @@ function preloadImages()
     (new Image()).src = "Images/recordToggledButtonGlyph.png";
     (new Image()).src = "Images/reloadButtonGlyph.png";
     (new Image()).src = "Images/undockButtonGlyph.png";
-}
-
-preloadImages();
+})();
 
 var WebInspector = {
     resources: {},
@@ -428,9 +426,38 @@ WebInspector.PlatformFlavor = {
     MacTiger: "mac-tiger",
     MacLeopard: "mac-leopard",
     MacSnowLeopard: "mac-snowleopard"
-}
+};
+
+(function parseQueryParameters()
+{
+    WebInspector.queryParamsObject = {};
+    var queryParams = window.location.search;
+    if (!queryParams)
+        return;
+    var params = queryParams.substring(1).split("&");
+    for (var i = 0; i < params.length; ++i) {
+        var pair = params[i].split("=");
+        WebInspector.queryParamsObject[pair[0]] = pair[1];
+    }
+})();
 
 WebInspector.loaded = function()
+{
+    if ("page" in WebInspector.queryParamsObject) {
+        WebInspector.socket = new WebSocket("ws://" + window.location.host + "/devtools/page/" + WebInspector.queryParamsObject.page);
+        WebInspector.socket.onmessage = function(message) { WebInspector_syncDispatch(message.data); }
+        WebInspector.socket.onerror = function(error) { console.error(error); }
+        WebInspector.socket.onopen = function() {
+            InspectorFrontendHost.sendMessageToBackend = WebInspector.socket.send.bind(WebInspector.socket);
+            InspectorFrontendHost.loaded = WebInspector.socket.send.bind(WebInspector.socket, "loaded");
+            WebInspector.doLoadedDone();
+        }
+        return;
+    }
+    WebInspector.doLoadedDone();
+}
+
+WebInspector.doLoadedDone = function()
 {
     InspectorBackend.setInjectedScriptSource("(" + injectedScriptConstructor + ");");
 
@@ -580,6 +607,15 @@ WebInspector.dispatch = function() {
     }
     WebInspector.pendingDispatches++;
     setTimeout(delayDispatch, 0);
+}
+
+// This function is purposely put into the global scope for easy access.
+WebInspector_syncDispatch = function(message)
+{
+    var args = JSON.parse(message);
+    var methodName = args[0];
+    var parameters = args.slice(1);
+    WebInspector[methodName].apply(WebInspector, parameters);
 }
 
 WebInspector.dispatchMessageFromBackend = function(arguments)
