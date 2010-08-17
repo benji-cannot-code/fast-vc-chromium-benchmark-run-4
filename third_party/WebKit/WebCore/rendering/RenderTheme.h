89c9da7aca87bcf06de5bc776ec83587e1c8b21e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-17 15:35:29 +0000

    2010-08-17  Ademar de Souza Reis Jr  <ademar.reis@openbossa.org>
            Reviewed by David Levin.
    
            Trivial fix removing the self-inclusion of a couple of header files.
    
            Self inclusion in Document.h and RenderTheme.h
            https://bugs.webkit.org/show_bug.cgi?id=44065
    
            * dom/Document.h:
            * rendering/RenderTheme.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65504 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTheme.h
+++ third_party/WebKit/WebCore/rendering/RenderTheme.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ThemeTypes.h"
 #endif
 #include "RenderObject.h"
-#include "RenderTheme.h"
 #include "ScrollTypes.h"
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
