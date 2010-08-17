66087d4d90abdf5d51f2d319c4aeabba37ed0eb8
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-17 06:30:10 +0000

    [Gtk] Display the current content setting applying to a domain along with the domain.
    This patch only affectes the gtk ui. Here is what it looks like: http://imgur.com/g6Gox
    
    If third party cookies are blocked, a warning is shown. Next to each domain, the content setting is displayed that applies (or nothing if the default applies). When the user selects some domains and presses an button, the labels are updated accordingly.
    
    BUG=49826
    TEST=unit_tests
    
    Review URL: http://codereview.chromium.org/3096001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56315 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/gtk_tree.h
+++ chrome/browser/gtk/gtk_tree.h
@@ -67,8 +67,8 @@ class TableAdapter : public TableModelObserver {
   };
 
   // |table_model| may be NULL.
-  explicit TableAdapter(Delegate* delegate, GtkListStore* list_store,
-                        TableModel* table_model);
+  TableAdapter(Delegate* delegate, GtkListStore* list_store,
+               TableModel* table_model);
   virtual ~TableAdapter() {}
 
   // Replace the TableModel with a different one.  If the list store currenty
@@ -128,6 +128,9 @@ class TreeAdapter : public TreeModelObserver {
   // Return the tree store.
   GtkTreeStore* tree_store() { return tree_store_; }
 
+  // Return the tree model.
+  TreeModel* tree_model() { return tree_model_; }
+
   // Get the TreeModelNode corresponding to iter in the tree store.
   TreeModelNode* GetNode(GtkTreeIter* iter);
 
@@ -143,10 +146,15 @@ class TreeAdapter : public TreeModelObserver {
   virtual void TreeNodeChanged(TreeModel* model, TreeModelNode* node);
   // End TreeModelObserver implementation.
 
- private:
+ protected:
   // Fill the tree store values for a given node.
-  void FillRow(GtkTreeIter* iter, TreeModelNode* node);
+  virtual void FillRow(GtkTreeIter* iter, TreeModelNode* node);
 
+  GtkTreeStore* tree_store_;
+  TreeModel* tree_model_;
+  std::vector<GdkPixbuf*> pixbufs_;
+
+ private:
   // Fill the tree store for a row and all its descendants.
   void Fill(GtkTreeIter* parent_iter, TreeModelNode* parent_node);
 
@@ -158,9 +166,8 @@ class TreeAdapter : public TreeModelObserver {
   bool GetTreeIter(TreeModelNode* node, GtkTreeIter* iter);
 
   Delegate* delegate_;
-  GtkTreeStore* tree_store_;
-  TreeModel* tree_model_;
-  std::vector<GdkPixbuf*> pixbufs_;
+
+  DISALLOW_COPY_AND_ASSIGN(TreeAdapter);
 };
 
 }  // namespace gtk_tree
