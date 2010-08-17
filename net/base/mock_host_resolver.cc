f392a29a2d9b6dae68a74b97df8743d6fbba8025
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-17 17:50:41 +0000

    Don't resolve IP literals.
    For each resolution request this checks to see if this 'host' is a literal ip address.  If so, it synthesises a struct addrinfo and returns it without adding it to the cache.
    
    BUG=39830
    TEST=unit tests, new and old
    
    Review URL: http://codereview.chromium.org/3023048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56384 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mock_host_resolver.cc
+++ net/base/mock_host_resolver.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ref_counted.h"
 #include "net/base/net_errors.h"
 #include "net/base/net_util.h"
+#include "net/base/sys_addrinfo.h"
 
 namespace net {
 
@@ -28,15 +29,10 @@ int CreateIPAddress(const std::string& host,
     return ERR_UNEXPECTED;
   }
 
-  if (ip_number.size() == 4) {
-    *addrlist = AddressList::CreateIPv4Address(&ip_number[0], canonical_name);
-  } else if (ip_number.size() == 16) {
-    *addrlist = AddressList::CreateIPv6Address(&ip_number[0], canonical_name);
-  } else {
-    NOTREACHED();
-    return ERR_UNEXPECTED;
-  }
-
+  AddressList result(ip_number, -1, false);
+  struct addrinfo* ai = const_cast<struct addrinfo*>(result.head());
+  ai->ai_canonname = strdup(canonical_name.c_str());
+  *addrlist = result;
   return OK;
 }
 
@@ -162,6 +158,10 @@ void RuleBasedHostResolverProc::AddIPLiteralRule(
     const std::string& host_pattern,
     const std::string& ip_literal,
     const std::string& canonical_name) {
+  // Literals are always resolved to themselves by HostResolverImpl,
+  // consequently we do not support remapping them.
+  IPAddressNumber ip_number;
+  DCHECK(!ParseIPLiteralToNumber(host_pattern, &ip_number));
   Rule rule(Rule::kResolverTypeIPLiteral,
             host_pattern,
             ADDRESS_FAMILY_UNSPECIFIED,
--- net/socket/ssl_client_socket_unittest.cc
+++ net/socket/ssl_client_socket_unittest.cc
@@ -258,9 +258,6 @@ TEST_F(SSLClientSocketTest, Read) {
 
   net::HostResolver::RequestInfo info(server_.kHostName, server_.kOKHTTPSPort);
   int rv = resolver_->Resolve(info, &addr, &callback, NULL, net::BoundNetLog());
-  EXPECT_EQ(net::ERR_IO_PENDING, rv);
-
-  rv = callback.WaitForResult();
   EXPECT_EQ(net::OK, rv);
 
   net::ClientSocket* transport = new net::TCPClientSocket(addr, NULL);
@@ -320,9 +317,6 @@ TEST_F(SSLClientSocketTest, Read_FullDuplex) {
 
   net::HostResolver::RequestInfo info(server_.kHostName, server_.kOKHTTPSPort);
   int rv = resolver_->Resolve(info, &addr, &callback, NULL, net::BoundNetLog());
-  EXPECT_EQ(net::ERR_IO_PENDING, rv);
-
-  rv = callback.WaitForResult();
   EXPECT_EQ(net::OK, rv);
 
   net::ClientSocket* transport = new net::TCPClientSocket(addr, NULL);
