08eae6d015b77a8bfb75b51e1d784f4e6c66113e
bryner@chromium.org <bryner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-17 22:44:31 +0000

    Make dom_feature_extractor_unittest into a browser test.
    WebKit doesn't work correctly if it is shut down and re-initialized.  browser_tests
    runs each test in its own process, which avoids this problem.
    
    TEST=passes
    BUG=none
    
    Review URL: http://codereview.chromium.org/3184002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56436 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/safe_browsing/phishing_dom_feature_extractor_browsertest.cc
+++ chrome/renderer/safe_browsing/phishing_dom_feature_extractor_browsertest.cc
@@ -2,6 +2,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//
+// Note that although this is not a "browser" test, it runs as part of
+// browser_tests.  This is because WebKit does not work properly if it is
+// shutdown and re-initialized.  Since browser_tests runs each test in a
+// new process, this avoids the problem.
 
 #include "chrome/renderer/safe_browsing/phishing_dom_feature_extractor.h"
 
