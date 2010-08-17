3c1275728f41e20ecf9cbc8c18638009888e294b
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-17 20:42:48 +0000

    Don't show accidental search infobar for bookmark keywords.
    BUG=6854
    TEST=none
    
    Review URL: http://codereview.chromium.org/3119007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56413 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete.cc
+++ chrome/browser/autocomplete/autocomplete.cc
@@ -720,7 +720,7 @@ void AutocompleteResult::SortAndCull(const AutocompleteInput& input) {
                              &AutocompleteMatch::DestinationsEqual),
                  matches_.end());
 
-  // Find the top max_matches.
+  // Find the top |kMaxMatches| matches.
   if (matches_.size() > kMaxMatches) {
     std::partial_sort(matches_.begin(), matches_.begin() + kMaxMatches,
                       matches_.end(), &AutocompleteMatch::MoreRelevant);
@@ -747,6 +747,7 @@ void AutocompleteResult::SortAndCull(const AutocompleteInput& input) {
        (input.type() == AutocompleteInput::REQUESTED_URL)) &&
       (default_match_ != end()) &&
       (default_match_->transition != PageTransition::TYPED) &&
+      (default_match_->transition != PageTransition::KEYWORD) &&
       (input.canonicalized_url() != default_match_->destination_url))
     alternate_nav_url_ = input.canonicalized_url();
 }
