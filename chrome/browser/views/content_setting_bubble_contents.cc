f630e6538c8d17d7cbe67daf52df9052d32b6973
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-17 14:35:02 +0000

    views: Rename content_setting_bubble_views.[cc/h] to content_setting_bubble_contents.[cc/h]
    (Note: that was a TODO for bulach).
    
    BUG=None
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/3177016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56343 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/content_setting_bubble_contents.cc
+++ chrome/browser/views/content_setting_bubble_contents.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/views/content_setting_bubble_views.h"
+#include "chrome/browser/views/content_setting_bubble_contents.h"
 
 #if defined(OS_LINUX)
 #include <gdk/gdk.h>
