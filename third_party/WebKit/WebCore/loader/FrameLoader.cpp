a0754e9923b2a1457f3ccae9a034aea112b21298
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-17 23:40:00 +0000

    2010-08-17  Mihai Parparita  <mihaip@chromium.org>
            Reviewed by Darin Fisher.
    
            Back button after Google search result click broken
            https://bugs.webkit.org/show_bug.cgi?id=44086
    
            Added a reduced test case of Google search result click behavior,
            renamed gesture-before-onload-target.html to redirect-target.html
            since it's useful for this test too.
    
            * fast/history/gesture-before-onload-form-submit-expected.txt:
            * fast/history/gesture-before-onload-form-submit.html:
            * fast/history/gesture-before-onload-location-href-expected.txt:
            * fast/history/gesture-before-onload-location-href.html:
            * fast/history/redirect-via-iframe-expected.txt: Added.
            * fast/history/redirect-via-iframe.html: Added.
            * fast/history/resources/iframe-redirect.html: Added.
            * fast/history/resources/redirect-target.html: Renamed from LayoutTests/fast/history/resources/gesture-before-onload-target.html.
    2010-08-17  Mihai Parparita  <mihaip@chromium.org>
    
            Reviewed by Darin Fisher.
    
            Back button after Google search result click broken
            https://bugs.webkit.org/show_bug.cgi?id=44086
    
            Test: fast/history/redirect-via-iframe.html
    
            isLoadingInAPISense doesn't quite map to onload (it also returns true
            if frames are loading, even after the parent frame has fired its onload
            handler). Instead, add a flag for the onload event being fired, and
            use that.
    
            * loader/DocumentLoader.cpp:
            (WebCore::DocumentLoader::DocumentLoader):
            * loader/DocumentLoader.h:
            (WebCore::DocumentLoader::handledOnloadEvents):
            (WebCore::DocumentLoader::wasOnloadHandled):
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::handledOnloadEvents):
            * loader/RedirectScheduler.cpp:
            (WebCore::RedirectScheduler::mustLockBackForwardList):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -2553,10 +2553,12 @@ void FrameLoader::handledOnloadEvents()
 {
     m_client->dispatchDidHandleOnloadEvents();
 
+    if (documentLoader()) {
+        documentLoader()->handledOnloadEvents();
 #if ENABLE(OFFLINE_WEB_APPLICATIONS)
-    if (documentLoader())
         documentLoader()->applicationCacheHost()->stopDeferringEvents();
 #endif
+    }
 }
 
 void FrameLoader::frameDetached()
