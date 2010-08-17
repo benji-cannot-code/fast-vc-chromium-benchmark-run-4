ba70d18376f93a6bbbbcefcba7ecd62126edd245
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-17 04:27:30 +0000

    2010-08-16  Kinuko Yasuda  <kinuko@chromium.org>
            Unreviewed; attempt to fix Chromium Windows build.
    
            * public/WebFileUtilities.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65485 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebFileUtilities.h
+++ third_party/WebKit/WebKit/chromium/public/WebFileUtilities.h
@@ -36,6 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebString.h"
 #include "WebURL.h"
 
+#ifdef WIN32
+typedef void *HANDLE;
+#endif
+
 namespace WebKit {
 
 class WebFileUtilities {
