4ac3fe472ce0cc68cb13f46f3a53f15b3df6a046
sargrass@google.com <sargrass@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-18 18:45:05 +0000

    Make the saved passwords tab work.
    BUG=49093
    TEST=None
    
    Review URL: http://codereview.chromium.org/3194002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56571 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/dom_ui/passwords_remove_all_handler.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_DOM_UI_PASSWORDS_REMOVE_ALL_HANDLER_H_
+#define CHROME_BROWSER_DOM_UI_PASSWORDS_REMOVE_ALL_HANDLER_H_
+
+#include "chrome/browser/dom_ui/options_ui.h"
+
+// Chrome passwords and exceptions remove all overlay UI handler.
+class PasswordsRemoveAllHandler : public OptionsPageUIHandler {
+ public:
+  PasswordsRemoveAllHandler();
+  virtual ~PasswordsRemoveAllHandler();
+
+  // OptionsUIHandler implementation.
+  virtual void GetLocalizedValues(DictionaryValue* localized_strings);
+
+  // DOMMessageHandler implementation.
+  virtual void RegisterMessages();
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(PasswordsRemoveAllHandler);
+};
+
+#endif  // CHROME_BROWSER_DOM_UI_PASSWORDSREMOVE_ALL_HANDLER_H_
