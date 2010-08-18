641cdb00c4304aae9ff109ca7eb2cf4796f7f5f4
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-18 18:40:52 +0000

    Make WKBundleNodeHandleRef usable from WKRetainPtr.
    Rubber-stamped by John Sullivan.
    
    * WebKit2.xcodeproj/project.pbxproj: Make WKBundleNodeHandle.h public.
    * WebProcess/InjectedBundle/API/c/WKBundleNodeHandle.h: Add WKRetain/WKRelease overloads.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65620 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundleNodeHandle.h
+++ third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundleNodeHandle.h
@@ -44,4 +44,6 @@ WK_EXPORT void WKBundleNodeHandleRelease(WKBundleNodeHandleRef nodeHandle);
 }
 #endif
 
+WK_DECLARE_RETAIN_RELEASE_OVERLOADS(WKBundleNodeHandle)
+
 #endif /* WKBundleNodeHandle_h */
