bfcccae38b3ba6eacb03db7d1ff5d3ca982c02ca
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-18 18:33:00 +0000

    GTTF: Move TestingAutomationProvider to its own file.
    Use "svn cp" to preserve svn metadata.
    
    TEST=compile, no code change
    BUG=52097
    Review URL: http://codereview.chromium.org/3112014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56559 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_init.cc
+++ chrome/browser/browser_init.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/automation/automation_provider.h"
 #include "chrome/browser/automation/automation_provider_list.h"
 #include "chrome/browser/automation/chrome_frame_automation_provider.h"
+#include "chrome/browser/automation/testing_automation_provider.h"
 #include "chrome/browser/browser_list.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_window.h"
