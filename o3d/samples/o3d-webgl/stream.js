ebbc0b2a2409be81a76ddf4269c6da7c30d3e22b
pathorn@chromium.org <pathorn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-18 18:17:34 +0000

    o3d-webgl: Shader support for skinning.
    Requires setting skinning in properly in o3djs/effect.js
    
    BUG=none
    TEST=o3d-webgl-samples/skinning.html runs much faster.
    Review URL: http://codereview.chromium.org/2873071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56558 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/samples/o3d-webgl/stream.js
+++ o3d/samples/o3d-webgl/stream.js
@@ -42,7 +42,7 @@ o3d.Stream = function(semantic, semantic_index, field, start_index) {
   this.field = field;
   this.startIndex = start_index;
 };
-o3d.inherit('Stream', 'NamedObject')
+o3d.inherit('Stream', 'NamedObject');
 
 /**
  * @type {number}
@@ -57,7 +57,9 @@ o3d.Stream.Semantic = goog.typedef;
  *  TANGENT,
  *  BINORMAL,
  *  COLOR,
- *  TEXCOORD
+ *  TEXCOORD,
+ *  INFLUENCE_WEIGHTS,
+ *  INFLUENCE_INDICES
  *
  * Semantics used when binding buffers to the streambank.  They determine how
  * the Stream links up to the shader inputs.
@@ -69,6 +71,8 @@ o3d.Stream.TANGENT = 3;
 o3d.Stream.BINORMAL = 4;
 o3d.Stream.COLOR = 5;
 o3d.Stream.TEXCOORD = 6;
+o3d.Stream.INFLUENCE_WEIGHTS = 7;
+o3d.Stream.INFLUENCE_INDICES = 8;
 
 
 
@@ -107,7 +111,7 @@ o3d.Stream.prototype.startIndex = 0;
  *     and its buffer.
  * @private
  */
-o3d.Stream.prototype.getMaxVertices = function() {
+o3d.Stream.prototype.getMaxVertices_ = function() {
   var buffer = this.field.buffer;
   if (!buffer)
     return 0;
