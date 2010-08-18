62bf394a66ac16260a7082aacf2c635b45041823
cmasone@google.com <cmasone@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-18 21:05:24 +0000

    Re-land r56529, and add fixes for the browser tests on chromium os.
    Add fixes for browser tests broken by r56529
    
    Revert "Revert 56529 - [Chrome OS] Block the installation of extensions with NPAPI plugins"
    
    BUG=chromium-os:5454
    TEST=browser tests, unit tests
    
    This reverts commit 8e3ffd2dab317dd7fb2d501353153d3e4dcc1c90.
    
    Review URL: http://codereview.chromium.org/3164026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56601 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_startup_browsertest.cc
+++ chrome/browser/extensions/extension_startup_browsertest.cc
@@ -32,6 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ExtensionStartupTestBase : public InProcessBrowserTest {
  public:
   ExtensionStartupTestBase() : enable_extensions_(false) {
+#if defined(OS_CHROMEOS)
+    num_expected_extensions_ = 3;
+#else
+    num_expected_extensions_ = 4;
+#endif
   }
 
  protected:
@@ -125,6 +130,8 @@ class ExtensionStartupTestBase : public InProcessBrowserTest {
   FilePath user_scripts_dir_;
   bool enable_extensions_;
   FilePath load_extension_;
+
+  int num_expected_extensions_;
 };
 
 
@@ -140,7 +147,8 @@ class ExtensionsStartupTest : public ExtensionStartupTestBase {
 };
 
 IN_PROC_BROWSER_TEST_F(ExtensionsStartupTest, Test) {
-  WaitForServicesToStart(4, true);  // 1 component extension and 3 others.
+  // 1 component extension and 2 or 3 others, depending on the platform.
+  WaitForServicesToStart(num_expected_extensions_, true);
   TestInjection(true, true);
 }
 
@@ -153,7 +161,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionsStartupTest, Test) {
 // Tests that disallowing file access on an extension prevents it from injecting
 // script into a page with a file URL.
 IN_PROC_BROWSER_TEST_F(ExtensionsStartupTest, MAYBE_NoFileAccess) {
-  WaitForServicesToStart(4, true);  // 1 component extension and 3 others.
+  // 1 component extension and 2 or 3 others, depending on the platform.
+  WaitForServicesToStart(num_expected_extensions_, true);
 
   ExtensionsService* service = browser()->profile()->GetExtensionsService();
   for (size_t i = 0; i < service->extensions()->size(); ++i) {
