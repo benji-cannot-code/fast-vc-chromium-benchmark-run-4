68e54c7eae7dc46dd511a3ac71ad48e8d6e797cf
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-18 01:22:05 +0000

    2010-08-17  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Ariya Hidayat.
    
            [Qt] [Symbian] Consistently use Q_OS_SYMBIAN to guard all Symbian platform dependencies
            https://bugs.webkit.org/show_bug.cgi?id=44124
    
            Q_WS_S60 is not defined for Symbian^4 devices as Q_WS_S60 used to guard
            Avkon UI framework dependencies. Use Q_OS_SYMBIAN everywhere to mark
            Symbian dependencies.
    
            * WebCoreSupport/QtFallbackWebPopup.cpp:
            (WebCore::QtFallbackWebPopup::populate):
    2010-08-17  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
    
            Reviewed by Ariya Hidayat.
    
            [Qt] [Symbian] Consistently use Q_OS_SYMBIAN to guard all Symbian platform dependencies
            https://bugs.webkit.org/show_bug.cgi?id=44124
    
            Q_WS_S60 is not defined for Symbian^4 devices as Q_WS_S60 used to guard
            Avkon UI framework dependencies. Use Q_OS_SYMBIAN everywhere to mark
            Symbian dependencies.
    
            * DumpRenderTree/qt/DumpRenderTreeQt.cpp:
            (WebCore::DumpRenderTree::open):
            * QtTestBrowser/launcherwindow.cpp:
            (LauncherWindow::init):
            (LauncherWindow::toggleFullScreenMode):
            (LauncherWindow::showFPS):
            (LauncherWindow::updateFPS):
            * QtTestBrowser/launcherwindow.h:
            (WindowOptions::WindowOptions):
            * QtTestBrowser/mainwindow.cpp:
            (MainWindow::buildUI):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/QtTestBrowser/mainwindow.cpp
+++ third_party/WebKit/WebKitTools/QtTestBrowser/mainwindow.cpp
@@ -49,7 +49,7 @@ MainWindow::MainWindow(const QString& url)
 void MainWindow::buildUI()
 {
     QToolBar* bar = addToolBar("Navigation");
-#if defined(Q_WS_S60)
+#if defined(Q_OS_SYMBIAN)
     bar->setIconSize(QSize(16, 16));
 #endif
     QAction* reloadAction = page()->action(QWebPage::Reload);
@@ -66,7 +66,7 @@ void MainWindow::buildUI()
     QCompleter* completer = new QCompleter(this);
     urlEdit->setCompleter(completer);
     completer->setModel(&urlModel);
-#if defined(Q_WS_S60)
+#if defined(Q_OS_SYMBIAN)
     addToolBarBreak();
     addToolBar("Location")->addWidget(urlEdit);
 #else
