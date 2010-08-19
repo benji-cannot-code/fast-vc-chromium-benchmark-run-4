424a6f569f6da1845e790645246fa44eb1d8f992
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-19 14:31:52 +0000

    views: Use IDS_BROWSER_HANGMONITOR_RENDERER_TITLE as the hung renderer dialog title.
    BUG=43103
    TEST=None
    
    Review URL: http://codereview.chromium.org/3166018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56682 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/hung_renderer_view.cc
+++ chrome/browser/views/hung_renderer_view.cc
@@ -268,7 +268,7 @@ void HungRendererDialogView::EndForTabContents(TabContents* contents) {
 // HungRendererDialogView, views::DialogDelegate implementation:
 
 std::wstring HungRendererDialogView::GetWindowTitle() const {
-  return l10n_util::GetString(IDS_PRODUCT_NAME);
+  return l10n_util::GetString(IDS_BROWSER_HANGMONITOR_RENDERER_TITLE);
 }
 
 void HungRendererDialogView::WindowClosing() {
