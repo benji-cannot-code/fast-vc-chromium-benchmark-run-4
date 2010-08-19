7fc26526d8c762f1a34e68ed361c3f664e09f545
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-19 17:51:34 +0000

    Break isGLES2Compliant() flag into multiple ones.
    Review URL: http://codereview.chromium.org/3149020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56709 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/webgraphicscontext3d_command_buffer_impl.h
+++ chrome/renderer/webgraphicscontext3d_command_buffer_impl.h
@@ -52,6 +52,9 @@ class WebGraphicsContext3DCommandBufferImpl
   virtual int sizeInBytes(int type);
 
   virtual bool isGLES2Compliant();
+  virtual bool isGLES2ParameterStrict();
+  virtual bool isGLES2NPOTStrict();
+  virtual bool isErrorGeneratedOnOutOfBoundsAccesses();
 
   virtual void reshape(int width, int height);
 
