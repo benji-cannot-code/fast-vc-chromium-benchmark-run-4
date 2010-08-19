2d47d2b98605e79bcb6843a92b0372fa09f6f8ea
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-19 18:37:21 +0000

    Remove web store icon from most visited per Glen.
    BUG=52669
    
    Review URL: http://codereview.chromium.org/3140018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56721 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/most_visited_handler.h
+++ chrome/browser/dom_ui/most_visited_handler.h
@@ -64,9 +64,6 @@ class MostVisitedHandler : public DOMMessageHandler,
 
   static void RegisterUserPrefs(PrefService* prefs);
 
-  // Returns Chrome Store URL with locale applied.
-  static GURL GetChromeStoreURLWithLocale();
-
  private:
   struct MostVisitedPage;
 
@@ -112,11 +109,6 @@ class MostVisitedHandler : public DOMMessageHandler,
       const MostVisitedHandler::MostVisitedPage& page);
 
   static const std::vector<MostVisitedPage>& GetPrePopulatedPages();
-
-  static MostVisitedPage GetChromeStorePage();
-
-  // Whether we have any apps installed.
-  bool HasApps() const;
 
   NotificationRegistrar registrar_;
 
