e07aac90aa84dedf149260d8c03f284a99d95cfd
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-19 21:32:45 +0000

    BUILD FIX: FrameLoaderClientQt.h should include ResourceError.h
    * WebCoreSupport/FrameLoaderClientQt.h: Include ResourceError.h.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65708 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KURL.h"
 #include "PluginView.h"
 #include "RefCounted.h"
+#include "ResourceError.h"
 #include "ResourceResponse.h"
 #include <QUrl>
 #include <qobject.h>
