d914dade73a6bddbe82a0b2cac8d66e0be113a5a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-19 15:44:35 +0000

    2010-08-19  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: activate/deactivate breakpoints using v8 debugger flag.
            https://bugs.webkit.org/show_bug.cgi?id=44112
    
            * src/js/DebuggerScript.js:
            ():
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/js/DebuggerScript.js
+++ third_party/WebKit/WebKit/chromium/src/js/DebuggerScript.js
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var DebuggerScript = {};
 DebuggerScript._breakpoints = {};
-DebuggerScript._breakpointsActivated = true;
 
 DebuggerScript.PauseOnExceptionsState = {
     DontPauseOnExceptions : 0,
@@ -96,7 +95,7 @@ DebuggerScript.setBreakpoint = function(execState, args)
 {
     args.lineNumber = DebuggerScript._webkitToV8LineNumber(args.lineNumber);
     var breakId = Debug.setScriptBreakPointById(args.scriptId, args.lineNumber, 0 /* column */, args.condition);
-    if (!args.enabled || !DebuggerScript._breakpointsActivated)
+    if (!args.enabled)
         Debug.disableScriptBreakPoint(breakId);
 
     var locations = Debug.findBreakPointActualLocations(breakId);
@@ -199,14 +198,7 @@ DebuggerScript.clearBreakpoints = function(execState, args)
 
 DebuggerScript.setBreakpointsActivated = function(execState, args)
 {
-    for (var key in DebuggerScript._breakpoints) {
-        var breakId = DebuggerScript._breakpoints[key];
-        if (args.enabled)
-            Debug.enableScriptBreakPoint(breakId);
-        else
-            Debug.disableScriptBreakPoint(breakId);
-    }
-    DebuggerScript._breakpointsActivated = args.enabled;
+    Debug.debuggerFlags().breakPointsActive.setValue(args.enabled);
 }
 
 DebuggerScript._frameMirrorToJSCallFrame = function(frameMirror, callerFrame)
