f109e918e1a3c540d880734095bce48f929d33ea
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-19 13:30:53 +0000

    LayoutTests
    2010-08-19  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Gtk] Enable view mode media feature layout test
            https://bugs.webkit.org/show_bug.cgi?id=43278
    
            Unskip tests that are now passing.
    
            * platform/gtk/Skipped:
    
    WebKit/gtk
    
    2010-08-19  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Gtk] Enable view mode media feature layout test
            https://bugs.webkit.org/show_bug.cgi?id=43278
    
            Add the view-mode property, to control the WebCore page's viewMode.
    
            * docs/webkitgtk-sections.txt:
            * webkit/webkitwebview.cpp:
            (webkit_web_view_get_property):
            (webkit_web_view_set_property):
            (webkit_web_view_class_init):
            (webkit_web_view_set_view_mode):
            (webkit_web_view_get_view_mode):
            * webkit/webkitwebview.h:
    
    WebKitTools
    
    2010-08-19  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Gtk] Enable view mode media feature layout test
            https://bugs.webkit.org/show_bug.cgi?id=43278
    
            Add view mode media feature testing infrastructure, and add
            placeholder methods for the other platforms'
            LayoutTestControllers.
    
            * DumpRenderTree/LayoutTestController.cpp:
            (setViewModeMediaFeatureCallback):
            (LayoutTestController::staticFunctions):
            * DumpRenderTree/LayoutTestController.h:
            * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
            (LayoutTestController::setViewModeMediaFeature):
            * DumpRenderTree/mac/LayoutTestControllerMac.mm:
            (LayoutTestController::setViewModeMediaFeature):
            * DumpRenderTree/win/LayoutTestControllerWin.cpp:
            (LayoutTestController::setViewModeMediaFeature):
            * DumpRenderTree/wx/LayoutTestControllerWx.cpp:
            (LayoutTestController::setViewModeMediaFeature):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65666 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/LayoutTestController.h
@@ -105,6 +105,7 @@ public:
     void setUseDashboardCompatibilityMode(bool flag);
     void setUserStyleSheetEnabled(bool flag);
     void setUserStyleSheetLocation(JSStringRef path);
+    void setViewModeMediaFeature(JSStringRef mode);
     void setXSSAuditorEnabled(bool flag);
     void setFrameFlatteningEnabled(bool enable);
     void setSpatialNavigationEnabled(bool enable);
