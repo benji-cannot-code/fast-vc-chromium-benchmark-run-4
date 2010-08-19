4d8cb818e826458ef67745ea36f3b12100ce8124
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-19 18:41:18 +0000

    GTTF: After timeout, all further automation calls should fail immediately.
    If IPC send fails, further automation calls are extremely likely to fail.
    Avoid wasting a lot of time on further timeouts by closing the channel
    immediately on the first error.
    
    TEST=ui_tests
    BUG=51346
    
    Review URL: http://codereview.chromium.org/3131020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56722 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/ui_test.h
+++ chrome/test/ui/ui_test.h
@@ -635,6 +635,8 @@ class UITest : public UITestBase, public PlatformTest {
   virtual void SetUp();
   virtual void TearDown();
 
+  virtual AutomationProxy* CreateAutomationProxy(int execution_timeout);
+
  private:
   MessageLoop message_loop_;  // Enables PostTask to main thread.
 };
