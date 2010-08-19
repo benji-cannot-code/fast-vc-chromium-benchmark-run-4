3d4000650f7100947204a536f7d7638e8e1a342d
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-19 20:04:22 +0000

    2010-08-19  Ojan Vafai  <ojan@chromium.org>
            Reviewed by Adam Barth.
    
            add --webkit_patch_id to reitveld uploads
            https://bugs.webkit.org/show_bug.cgi?id=44289
    
            This is the first step in having rietveld store which
            patches have been uploaded and getting rid of the
            in-rietveld flag.
    
            * Scripts/webkitpy/common/net/rietveld.py:
            * Scripts/webkitpy/tool/mocktool.py:
            * Scripts/webkitpy/tool/steps/postcodereview.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65694 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/mocktool.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/mocktool.py
@@ -552,7 +552,7 @@ class MockRietveld():
     def __init__(self, executive, dryrun=False):
         pass
 
-    def post(self, diff, message=None, codereview_issue=None, cc=None):
+    def post(self, diff, patch_id, codereview_issue, message=None, cc=None):
         log("MOCK: Uploading patch to rietveld")
 
 
