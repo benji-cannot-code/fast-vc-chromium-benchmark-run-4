bb97574d633c64dc12d7d1e36ca9e602ec2e79aa
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-20 21:03:17 +0000

    2010-08-20  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dan Bernstein.
    
            <rdar://problem/8245719> backface-visibility and reflections don't play nicely together.
    
            On elements with a reflection and backface-visibility: hidden, set the doubleSided property on the
            reflection flattening layer so that backface-visibility works in all configurations.
    
            Test: compositing/reflections/backface-hidden-reflection.html
    
            * platform/graphics/mac/GraphicsLayerCA.mm:
            (WebCore::GraphicsLayerCA::updateBackfaceVisibility): Call setDoubleSided: on the structural
            layer for reflections.
            (WebCore::GraphicsLayerCA::ensureStructuralLayer): Fix a comment typo, and call updateBackfaceVisibility()
            when we gain a structural layer.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/GraphicsLayerCA.mm
+++ third_party/WebKit/WebCore/platform/graphics/mac/GraphicsLayerCA.mm
@@ -1214,6 +1214,18 @@ void GraphicsLayerCA::updateContentsOpaque()
 
 void GraphicsLayerCA::updateBackfaceVisibility()
 {
+    if (m_structuralLayer && structuralLayerPurpose() == StructuralLayerForReplicaFlattening) {
+        [m_structuralLayer.get() setDoubleSided:m_backfaceVisibility];
+
+        if (LayerMap* layerCloneMap = m_structuralLayerClones.get()) {
+            LayerMap::const_iterator end = layerCloneMap->end();
+            for (LayerMap::const_iterator it = layerCloneMap->begin(); it != end; ++it) {
+                CALayer *currLayer = it->second.get();
+                [currLayer setDoubleSided:m_backfaceVisibility];
+            }
+        }
+    }
+
     [m_layer.get() setDoubleSided:m_backfaceVisibility];
 
     if (LayerMap* layerCloneMap = m_layerClones.get()) {
@@ -1244,7 +1256,7 @@ void GraphicsLayerCA::ensureStructuralLayer(StructuralLayerPurpose purpose)
             // Release the structural layer.
             m_structuralLayer = 0;
 
-            // Update the properties of m_layer now that we no loner have a structural layer.
+            // Update the properties of m_layer now that we no longer have a structural layer.
             updateLayerPosition();
             updateLayerSize();
             updateAnchorPoint();
@@ -1295,6 +1307,7 @@ void GraphicsLayerCA::ensureStructuralLayer(StructuralLayerPurpose purpose)
     updateAnchorPoint();
     updateTransform();
     updateChildrenTransform();
+    updateBackfaceVisibility();
     
     // Set properties of m_layer to their default values, since these are expressed on on the structural layer.
     CGPoint point = CGPointMake(m_size.width() / 2.0f, m_size.height() / 2.0f);
