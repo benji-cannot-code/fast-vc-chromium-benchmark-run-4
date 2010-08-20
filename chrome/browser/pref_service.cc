bc0a4f0c50a0d83e171efbb667bf3e782deb10e1
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-20 00:36:01 +0000

    linux: add UMA stat for the file system where the prefs are stored
    I'd like to see how common it is to use a network file system for
    your home directory.  This would help evaluate how important it is
    to support these users.
    
    See also:
      http://groups.google.com/a/chromium.org/group/chromium-dev/browse_thread/thread/b43d2905f079fa1c
    
    Review URL: http://codereview.chromium.org/3187011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56786 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/pref_service.cc
+++ chrome/browser/pref_service.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util.h"
 #include "base/command_line.h"
 #include "base/file_path.h"
+#include "base/file_util.h"
 #include "base/histogram.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
@@ -81,6 +82,19 @@ void NotifyReadError(PrefService* pref, int message_id) {
 // static
 PrefService* PrefService::CreatePrefService(const FilePath& pref_filename,
                                             Profile* profile) {
+#if defined(OS_LINUX)
+  // We'd like to see what fraction of our users have the preferences
+  // stored on a network file system, as we've had no end of troubles
+  // with NFS/AFS.
+  // TODO(evanm): remove this once we've collected state.
+  file_util::FileSystemType fstype;
+  if (file_util::GetFileSystemType(pref_filename.DirName(), &fstype)) {
+    UMA_HISTOGRAM_ENUMERATION("PrefService.FileSystemType",
+                              static_cast<int>(fstype),
+                              file_util::FILE_SYSTEM_TYPE_COUNT);
+  }
+#endif
+
   return new PrefService(
       PrefValueStore::CreatePrefValueStore(pref_filename, profile, false));
 }
