ff99154dcae6a624534437350b01684735d49dae
rkc@google.com <rkc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-22 10:38:54 +0000

    Build fixes to the CL
    Review URL: http://codereview.chromium.org/3061044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57019 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/dom_ui_factory.cc
+++ chrome/browser/dom_ui/dom_ui_factory.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/dom_ui/bookmarks_ui.h"
+#include "chrome/browser/dom_ui/bug_report_ui.h"
 #include "chrome/browser/dom_ui/downloads_ui.h"
 #include "chrome/browser/dom_ui/devtools_ui.h"
 #include "chrome/browser/dom_ui/history_ui.h"
@@ -103,6 +104,8 @@ static DOMUIFactoryFunction GetDOMUIFactoryFunction(const GURL& url) {
   // after the host name.
   if (url.host() == chrome::kChromeUIBookmarksHost)
     return &NewDOMUI<BookmarksUI>;
+  if (url.host() == chrome::kChromeUIBugReportHost)
+    return &NewDOMUI<BugReportUI>;
   if (url.host() == chrome::kChromeUIDevToolsHost)
     return &NewDOMUI<DevToolsUI>;
   if (url.host() == chrome::kChromeUIDownloadsHost)
