ea3f1a78807c63cba2f121c0c8e8c2d95406de47
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-22 02:10:44 +0000

    Remove wstrings in bookmarks, part 10.
    This converts the text parameter in GetBookmarksContainingText() and
    DoesBookmarkContainText() to string16. (The languages parameter will wait until
    I decide what I want for languages parameters in general.)
    
    BUG=23581
    TEST=builds and passes tests
    
    Review URL: http://codereview.chromium.org/3197009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57008 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_utils.cc
+++ chrome/browser/bookmarks/bookmark_utils.cc
@@ -502,13 +502,13 @@ bool MoreRecentlyAdded(const BookmarkNode* n1, const BookmarkNode* n2) {
 }
 
 void GetBookmarksContainingText(BookmarkModel* model,
-                                const std::wstring& text,
+                                const string16& text,
                                 size_t max_count,
                                 const std::wstring& languages,
                                 std::vector<const BookmarkNode*>* nodes) {
   std::vector<string16> words;
   QueryParser parser;
-  parser.ExtractQueryWords(l10n_util::ToLower(WideToUTF16(text)), &words);
+  parser.ExtractQueryWords(l10n_util::ToLower(text), &words);
   if (words.empty())
     return;
 
@@ -524,11 +524,11 @@ void GetBookmarksContainingText(BookmarkModel* model,
 }
 
 bool DoesBookmarkContainText(const BookmarkNode* node,
-                             const std::wstring& text,
+                             const string16& text,
                              const std::wstring& languages) {
   std::vector<string16> words;
   QueryParser parser;
-  parser.ExtractQueryWords(l10n_util::ToLower(WideToUTF16(text)), &words);
+  parser.ExtractQueryWords(l10n_util::ToLower(text), &words);
   if (words.empty())
     return false;
 
