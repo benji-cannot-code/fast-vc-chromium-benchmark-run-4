ec85d2e0a66701b316bdda81957c7d2b37ef008a
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-22 04:40:20 +0000

    Remove wstrings from bookmarks, part 11.
    This removes wstrings from BookmarkIndex.
    
    BUG=23581
    TEST=builds and passes tests
    
    Review URL: http://codereview.chromium.org/3112027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57012 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_index.h
+++ chrome/browser/bookmarks/bookmark_index.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 #include <map>
 #include <set>
-#include <string>
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/string16.h"
 
 class BookmarkNode;
 class Profile;
@@ -48,13 +48,13 @@ class BookmarkIndex {
 
   // Returns up to |max_count| of bookmarks containing the text |query|.
   void GetBookmarksWithTitlesMatching(
-      const std::wstring& query,
+      const string16& query,
       size_t max_count,
       std::vector<bookmark_utils::TitleMatch>* results);
 
  private:
   typedef std::set<const BookmarkNode*> NodeSet;
-  typedef std::map<std::wstring, NodeSet> Index;
+  typedef std::map<string16, NodeSet> Index;
 
   // Used when finding the set of bookmarks that match a query. Each match
   // represents a set of terms (as an interator into the Index) matching the
@@ -117,7 +117,7 @@ class BookmarkIndex {
   // Populates |matches| for the specified term. If |first_term| is true, this
   // is the first term in the query. Returns true if there is at least one node
   // matching the term.
-  bool GetBookmarksWithTitleMatchingTerm(const std::wstring& term,
+  bool GetBookmarksWithTitleMatchingTerm(const string16& term,
                                          bool first_term,
                                          Matches* matches);
 
@@ -143,13 +143,13 @@ class BookmarkIndex {
                       Matches* result);
 
   // Returns the set of query words from |query|.
-  std::vector<std::wstring> ExtractQueryWords(const std::wstring& query);
+  std::vector<string16> ExtractQueryWords(const string16& query);
 
   // Adds |node| to |index_|.
-  void RegisterNode(const std::wstring& term, const BookmarkNode* node);
+  void RegisterNode(const string16& term, const BookmarkNode* node);
 
   // Removes |node| from |index_|.
-  void UnregisterNode(const std::wstring& term, const BookmarkNode* node);
+  void UnregisterNode(const string16& term, const BookmarkNode* node);
 
   Index index_;
 
