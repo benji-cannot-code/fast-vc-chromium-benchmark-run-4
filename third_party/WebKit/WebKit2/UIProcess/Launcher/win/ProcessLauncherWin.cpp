c558cd36a072cb929152d04c45bbe8193f3765c0
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-23 21:29:42 +0000

    Layout tests do not run when using WebKitTestRunner in a Debug build. https://bugs.webkit.org/show_bug.cgi?id=44409
    Reviewed by Dan Bernstein.
    
    WebKit2:
    
    * UIProcess/Launcher/win/ProcessLauncherWin.cpp:
    (WebKit::ProcessLauncher::launchProcess):
    Use WebKit2WebProcess_debug.exe when building Debug.
    
    WebKitTools:
    
    * WebKitTestRunner/win/TestControllerWin.cpp:
    (WTR::TestController::initializeInjectedBundlePath):
    Use InjectedBundle_debug.dll when building Debug.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65831 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/Launcher/win/ProcessLauncherWin.cpp
+++ third_party/WebKit/WebKit2/UIProcess/Launcher/win/ProcessLauncherWin.cpp
@@ -35,6 +35,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 
+#if !defined(NDEBUG) && (!defined(DEBUG_INTERNAL) || defined(DEBUG_ALL))
+const LPCWSTR webProcessName = L"WebKit2WebProcess_debug.exe";
+#else
+const LPCWSTR webProcessName = L"WebKit2WebProcess.exe";
+#endif
+
 namespace WebKit {
 
 void ProcessLauncher::launchProcess()
@@ -52,12 +58,8 @@ void ProcessLauncher::launchProcess()
     Vector<UChar> commandLineVector;
 
     // FIXME: We would like to pass a full path to the .exe here.
-#ifndef DEBUG_ALL
-    String commandLine(L"WebKit2WebProcess.exe");
-#else
-    String commandLine(L"WebKit2WebProcess_debug.exe");
-#endif
 
+    String commandLine(webProcessName);
     append(commandLineVector, commandLine);
     append(commandLineVector, " -mode webprocess");
     append(commandLineVector, " -clientIdentifier ");
