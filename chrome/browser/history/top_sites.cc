ec5571f770d7810f29f70403c505aa3782ae0663
nshkrob@chromium.org <nshkrob@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-23 22:00:24 +0000

    Fix broken thumbnails for sites with redirects.
    BUG=52621
    TEST=unit_tests
    
    Review URL: http://codereview.chromium.org/3135035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57113 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/top_sites.cc
+++ chrome/browser/history/top_sites.cc
@@ -250,7 +250,8 @@ void TopSites::GetMostVisitedURLs(CancelableRequestConsumer* consumer,
 
 bool TopSites::GetPageThumbnail(const GURL& url, RefCountedBytes** data) const {
   AutoLock lock(lock_);
-  std::map<GURL, Images>::const_iterator found = top_images_.find(url);
+  std::map<GURL, Images>::const_iterator found =
+      top_images_.find(GetCanonicalURL(url));
   if (found == top_images_.end()) {
     found = temp_thumbnails_map_.find(url);
     if (found == temp_thumbnails_map_.end())
@@ -380,7 +381,9 @@ std::string TopSites::GetURLString(const GURL& url) {
 
 std::string TopSites::GetURLHash(const GURL& url) {
   lock_.AssertAcquired();
-  return MD5String(GetCanonicalURL(url).spec());
+  // We don't use canonical URLs here to be able to blacklist only one of
+  // the two 'duplicate' sites, e.g. 'gmail.com' and 'mail.google.com'.
+  return MD5String(url.spec());
 }
 
 void TopSites::UpdateMostVisited(MostVisitedURLList most_visited) {
@@ -566,8 +569,11 @@ void TopSites::StoreRedirectChain(const RedirectList& redirects,
   }
 
   // Map all the redirected URLs to the destination.
-  for (size_t i = 0; i < redirects.size(); i++)
-    canonical_urls_[redirects[i]] = destination;
+  for (size_t i = 0; i < redirects.size(); i++) {
+    // If this redirect is already known, don't replace it with a new one.
+    if (canonical_urls_.find(redirects[i]) == canonical_urls_.end())
+      canonical_urls_[redirects[i]] = destination;
+  }
 }
 
 GURL TopSites::GetCanonicalURL(const GURL& url) const {
@@ -635,7 +641,7 @@ void TopSites::StartQueryForMostVisited() {
     // Testing with a mockup.
     // QueryMostVisitedURLs is not virtual, so we have to duplicate the code.
     mock_history_service_->QueryMostVisitedURLs(
-        kTopSitesNumber,
+        kTopSitesNumber + blacklist_->size(),
         kDaysOfHistory,
         &cancelable_consumer_,
         NewCallback(this, &TopSites::OnTopSitesAvailable));
@@ -647,7 +653,7 @@ void TopSites::StartQueryForMostVisited() {
     // |hs| may be null during unit tests.
     if (hs) {
       hs->QueryMostVisitedURLs(
-          kTopSitesNumber,
+          kTopSitesNumber + blacklist_->size(),
           kDaysOfHistory,
           &cancelable_consumer_,
           NewCallback(this, &TopSites::OnTopSitesAvailable));
@@ -765,7 +771,6 @@ base::TimeDelta TopSites::GetUpdateDelay() {
 void TopSites::OnTopSitesAvailable(
     CancelableRequestProvider::Handle handle,
     MostVisitedURLList pages) {
-
   AddPrepopulatedPages(&pages);
   ChromeThread::PostTask(ChromeThread::DB, FROM_HERE, NewRunnableMethod(
       this, &TopSites::UpdateMostVisited, pages));
