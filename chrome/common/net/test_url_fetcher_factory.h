c99f7b0e5a1d3b37bfad83159ed6a5a6448619bc
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-23 06:39:42 +0000

    Use ChromeThread::IO instead of virtual set_request_context
    ChromeThread::IO will be used to cancel URLFetcher request and
    clean up URLRequestContextGetter.
    
    BUG=52562,52826
    TEST=valgrind passes
    
    Review URL: http://codereview.chromium.org/3176028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57032 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/test_url_fetcher_factory.h
+++ chrome/common/net/test_url_fetcher_factory.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Typical usage:
 //   // TestURLFetcher requires a MessageLoop:
 //   MessageLoopForUI message_loop;
+//   // And io_thread to release URLRequestContextGetter in URLFetcher::Core.
+//   ChromeThread io_thread(ChromeThread::IO, &message_loop);
 //   // Create and register factory.
 //   TestURLFetcherFactory factory;
 //   URLFetcher::set_factory(&factory);
@@ -53,13 +55,6 @@ class TestURLFetcher : public URLFetcher {
 
   // Returns the data uploaded on this URLFetcher.
   const std::string& upload_data() const { return URLFetcher::upload_data(); }
-
-  // Overriden to do nothing. URLFetcher implementation add reference
-  // to request_context_getter in core_, but it might not be released
-  // because we wouldn't call Core::CancelURLRequest.
-  // Without this, we'll see leaks of URLRequestContext in test.
-  virtual void set_request_context(
-      URLRequestContextGetter* request_context_getter) {}
 
  private:
   const GURL original_url_;
