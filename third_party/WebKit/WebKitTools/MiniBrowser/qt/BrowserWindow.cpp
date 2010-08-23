c9b559cc614fa237197e926ac75d2bf98976a332
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-23 13:58:51 +0000

    2010-08-23  Balazs Kelemen  <kb@inf.u-szeged.hu>
            Reviewed by Ariya Hidayat.
    
            [Qt] Find needed forwarding headers for WebKit2 automatically
            https://bugs.webkit.org/show_bug.cgi?id=44336
    
            * MiniBrowser/qt/BrowserWindow.cpp: Use normal (not framework style) includes here.
            The new script does not satisfy the needs of this file however using normal includes
            is generally better here.
            * MiniBrowser/qt/MiniBrowser.pro: Added include paths.
            * Scripts/enumerate-included-framework-headers: Enumerates headers included from the 
            given frameworks by files in the current directory (and subdirectories).
            In the future this needs to be rewritten in perl for being able to use on Windows.
    2010-08-23  Balazs Kelemen  <kb@inf.u-szeged.hu>
    
            Reviewed by Ariya Hidayat.
    
            [Qt] Find needed forwarding headers for WebKit2 automatically
            https://bugs.webkit.org/show_bug.cgi?id=44336
    
            * DerivedSources.pro: Use the new enumerate-included-framework-headers script for generating
            the list of forwarding headers we want to create.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65808 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/MiniBrowser/qt/BrowserWindow.cpp
+++ third_party/WebKit/WebKitTools/MiniBrowser/qt/BrowserWindow.cpp
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "BrowserWindow.h"
-#include <WebKit2/WKPageNamespace.h>
-#include <WebKit2/qwkpage.h>
+#include "WKPageNamespace.h"
+#include "qwkpage.h"
 
 static QWKPage* createNewPage(QWKPage* page)
 {
