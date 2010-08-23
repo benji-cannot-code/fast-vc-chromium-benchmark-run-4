ccb5bf9d89dd67a566cec43d04a791ecfb369964
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-23 17:42:29 +0000

    --auth-schemes specifies which authentication schemes are supported on the command line.
    --auth-schemes should be a command separated list containing one or more of the following schemes: basic, digest, ntlm, or negotiate.
    
    This will primarily be used to help triage user-reported bugs.
    
    BUG=None
    TEST=specify --auth-schemes and ensure that only the specified schemes are supported. Also, when unspecified all schemes should be supported.
    
    Review URL: http://codereview.chromium.org/3199002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57073 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -34,6 +34,7 @@ extern const char kAppsDebug[];
 extern const char kAppsPanel[];
 extern const char kAppsGalleryURL[];
 extern const char kAppsNoThrob[];
+extern const char kAuthSchemes[];
 extern const char kAuthServerWhitelist[];
 extern const char kAutomationClientChannelID[];
 extern const char kBlockNonSandboxedPlugins[];
@@ -160,7 +161,6 @@ extern const char kEnableWatchdog[];
 extern const char kEnableWin7Location[];
 extern const char kEnableXSSAuditor[];
 // Experimental features.
-extern const char kExperimentalEnableNegotiateAuth[];
 extern const char kExperimentalSpellcheckerFeatures[];
 // End experimental features.
 extern const char kExplicitlyAllowedPorts[];
