5d0117c3c29b3d53bd56c0aee5a496893da7971d
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-23 22:35:18 +0000

    GTTF: next batch of automation separation.
    TEST=none
    BUG=52097
    
    Review URL: http://codereview.chromium.org/3191015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57116 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/automation/testing_automation_provider_win.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/automation/testing_automation_provider.h"
+
+#include <windows.h>
+
+#include "chrome/browser/automation/automation_window_tracker.h"
+
+void TestingAutomationProvider::ActivateWindow(int handle) {
+  if (window_tracker_->ContainsHandle(handle)) {
+    ::SetActiveWindow(window_tracker_->GetResource(handle));
+  }
+}
+
+void TestingAutomationProvider::IsWindowMaximized(int handle,
+                                                  bool* is_maximized,
+                                                  bool* success) {
+  *success = false;
+
+  HWND hwnd = window_tracker_->GetResource(handle);
+  if (hwnd) {
+    *success = true;
+    WINDOWPLACEMENT window_placement;
+    GetWindowPlacement(hwnd, &window_placement);
+    *is_maximized = (window_placement.showCmd == SW_MAXIMIZE);
+  }
+}
+
