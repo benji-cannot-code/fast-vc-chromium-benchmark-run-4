93d51ec8e6258fe1ec6aa15eb4ec3d8abbc2655a
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-23 19:22:48 +0000

    2010-08-23  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Alexey Proskuryakov.
    
            TreeWalker is not calling acceptNode function in filter object
            https://bugs.webkit.org/show_bug.cgi?id=35296
    
            We only accepted raw functions as the NodeFilter on TreeWalker. Fix this to
            look for an 'acceptNode' function on the filter object, and use that if present.
            Also throw an exception if the filter object does not have an acceptNode function.
    
            Test: fast/dom/TreeWalker/acceptNode-filter.html
    
            * bindings/js/JSNodeFilterCondition.cpp:
            (WebCore::JSNodeFilterCondition::acceptNode):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65824 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSNodeFilterCondition.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSNodeFilterCondition.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSNode.h"
 #include "JSNodeFilter.h"
 #include "NodeFilter.h"
+#include <runtime/Error.h>
 #include <runtime/JSLock.h>
 
 namespace WebCore {
@@ -46,9 +47,7 @@ short JSNodeFilterCondition::acceptNode(JSC::ExecState* exec, Node* filterNode) 
 {
     JSLock lock(SilenceAssertionsOnly);
 
-    CallData callData;
-    CallType callType = getCallData(m_filter, callData);
-    if (callType == CallTypeNone)
+    if (!m_filter.isObject())
         return NodeFilter::FILTER_ACCEPT;
 
    // The exec argument here should only be null if this was called from a
@@ -60,6 +59,18 @@ short JSNodeFilterCondition::acceptNode(JSC::ExecState* exec, Node* filterNode) 
     if (!exec)
         return NodeFilter::FILTER_REJECT;
 
+    JSValue function = m_filter;
+    CallData callData;
+    CallType callType = getCallData(function, callData);
+    if (callType == CallTypeNone) {
+        JSValue function = m_filter.get(exec, Identifier(exec, "acceptNode"));
+        callType = getCallData(function, callData);
+        if (callType == CallTypeNone) {
+            throwError(exec, createTypeError(exec, "NodeFilter object does not have an acceptNode function"));
+            return NodeFilter::FILTER_REJECT;
+        }
+    }
+
     MarkedArgumentBuffer args;
     // FIXME: The node should have the prototype chain that came from its document, not
     // whatever prototype chain might be on the window this filter came from. Bug 27662
@@ -67,7 +78,7 @@ short JSNodeFilterCondition::acceptNode(JSC::ExecState* exec, Node* filterNode) 
     if (exec->hadException())
         return NodeFilter::FILTER_REJECT;
 
-    JSValue result = JSC::call(exec, m_filter, callType, callData, m_filter, args);
+    JSValue result = JSC::call(exec, function, callType, callData, m_filter, args);
     if (exec->hadException())
         return NodeFilter::FILTER_REJECT;
 
