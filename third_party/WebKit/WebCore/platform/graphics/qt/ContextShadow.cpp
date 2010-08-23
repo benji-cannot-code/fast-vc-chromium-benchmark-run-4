50f0cadc875c42e9951c27884889574de0d4e259
ariya@webkit.org <ariya@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-23 07:59:48 +0000

    [Qt] Crash when purging the scratch buffer for the shadow https://bugs.webkit.org/show_bug.cgi?id=44384
    Patch by Ariya Hidayat <ariya@sencha.com> on 2010-08-23
    Reviewed by Kenneth Rohde Christiansen.
    
    WebCore::Timer can't be used in a static object bcause it relies on
    thread global data, which is invalid once the application instance is
    destroyed. To overcome the problem, use QObject's timer support for
    the ShadowBuffer class.
    
    * platform/graphics/qt/ContextShadow.cpp:
    (WebCore::):
    (WebCore::ShadowBuffer::ShadowBuffer):
    (WebCore::ShadowBuffer::schedulePurge):
    (WebCore::ShadowBuffer::timerEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/ContextShadow.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/ContextShadow.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "ContextShadow.h"
 
-#include "Timer.h"
+#include <QTimerEvent>
 #include <wtf/Noncopyable.h>
 
 namespace WebCore {
@@ -38,22 +38,25 @@ namespace WebCore {
 // Instead of creating and destroying the buffer for every operation,
 // we create a buffer which will be automatically purged via a timer.
 
-class ShadowBuffer: public Noncopyable {
+class ShadowBuffer: public QObject {
 public:
-    ShadowBuffer();
+    ShadowBuffer(QObject* parent = 0);
 
     QImage* scratchImage(const QSize& size);
 
     void schedulePurge();
 
+protected:
+    void timerEvent(QTimerEvent* event);
+
 private:
     QImage image;
-    void purgeBuffer(Timer<ShadowBuffer>*);
-    Timer<ShadowBuffer> purgeTimer;
+    int timerId;
 };
 
-ShadowBuffer::ShadowBuffer()
-    : purgeTimer(this, &ShadowBuffer::purgeBuffer)
+ShadowBuffer::ShadowBuffer(QObject* parent)
+    : QObject(parent)
+    , timerId(0)
 {
 }
 
@@ -85,12 +88,17 @@ QImage* ShadowBuffer::scratchImage(const QSize& size)
 void ShadowBuffer::schedulePurge()
 {
     static const double BufferPurgeDelay = 2; // seconds
-    purgeTimer.startOneShot(BufferPurgeDelay);
+    killTimer(timerId);
+    timerId = startTimer(BufferPurgeDelay * 1000);
 }
 
-void ShadowBuffer::purgeBuffer(Timer<ShadowBuffer>*)
+void ShadowBuffer::timerEvent(QTimerEvent* event)
 {
-    image = QImage();
+    if (event->timerId() == timerId) {
+        killTimer(timerId);
+        image = QImage();
+    }
+    QObject::timerEvent(event);
 }
 
 Q_GLOBAL_STATIC(ShadowBuffer, scratchShadowBuffer)
