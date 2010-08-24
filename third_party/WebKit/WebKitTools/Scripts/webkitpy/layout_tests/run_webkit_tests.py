acfb5ff2f4b020e59b506436e5297c4601cec245
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-24 23:45:28 +0000

    2010-08-24  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by Eric Seidel.
    
            speed up new-run-webkit-tests unit tests
    
            Add a --no-record-results flag that turns off generating the JSON
            results file on every test run. Generating the file requires us to
            fetch the old results down from the bots, which can be slow. This
            flag is off by default.
    
            Reduce the sleep time in wait_for_threads_to_finish from 0.1s to 0.01s.
    
            These changes together shorten the test cycle from ~4.5s to ~1.5s
            - a 3x speedup.
    
            https://bugs.webkit.org/show_bug.cgi?id=44553
    
            * Scripts/webkitpy/layout_tests/run_webkit_tests.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65949 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/run_webkit_tests.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/run_webkit_tests.py
@@ -688,7 +688,7 @@ class TestRunner:
                 self.update_summary(result_summary)
 
                 if some_thread_is_alive:
-                    time.sleep(0.1)
+                    time.sleep(0.01)
 
         except KeyboardInterrupt:
             keyboard_interrupted = True
@@ -780,12 +780,13 @@ class TestRunner:
             self._expectations, result_summary, retry_summary)
         self._printer.print_unexpected_results(unexpected_results)
 
-        # Write the same data to log files.
-        self._write_json_files(unexpected_results, result_summary,
-                             individual_test_timings)
+        if self._options.record_results:
+            # Write the same data to log files.
+            self._write_json_files(unexpected_results, result_summary,
+                                   individual_test_timings)
 
-        # Upload generated JSON files to appengine server.
-        self._upload_json_files()
+            # Upload generated JSON files to appengine server.
+            self._upload_json_files()
 
         # Write the summary to disk (results.html) and display it if requested.
         wrote_results = self._write_results_html_file(result_summary)
@@ -1546,6 +1547,9 @@ def parse_args(args=None):
             default=False, help="Clobbers test results from previous runs."),
         optparse.make_option("--platform",
             help="Override the platform for expected results"),
+        optparse.make_option("--no-record-results", action="store_false",
+            default=True, dest="record_results",
+            help="Don't record the results."),
         # old-run-webkit-tests also has HTTP toggle options:
         # --[no-]http                     Run (or do not run) http tests
         #                                 (default: run)
