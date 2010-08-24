f81394fc2567ca179e9efdb2e1dbb717334ad824
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-24 08:05:55 +0000

    Add explanatory text to omnibox and infobars API. I realized at the last minute this was missing and that developers probably wouldn't be able to make use of the API without it.
    TBR=kathyw@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57148 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/js/sample_search.js
+++ chrome/common/extensions/docs/js/sample_search.js
@@ -76,6 +76,10 @@ function initSearch() {
   if (canclear) {
     searchinput.addEventListener('click', filterSamples, false);
   }
+
+  if (window.location.hash.length > 1) {
+    setFilter(window.location.hash.substring(1));
+  }
 };
 
 var currentfilter = "";
