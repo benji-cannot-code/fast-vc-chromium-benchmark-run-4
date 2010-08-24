7989b59ff9e2d7cdd79f222173d37b34d842a11a
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-24 14:59:26 +0000

    Fix for <rdar://problem/8153271> and <rdar://problem/8153288> Change behavior of javascript: urls in <embed> and <object> back to how they behaved before r50698.
    Reviewed by Adam Barth.
    
    WebCore: 
    
    Tests: fast/loader/javascript-url-in-embed.html
           fast/loader/javascript-url-in-object.html
    
    * loader/SubframeLoader.cpp:
    (WebCore::SubframeLoader::requestFrame):
    (WebCore::SubframeLoader::requestObject):
    (WebCore::SubframeLoader::loadOrRedirectSubframe):
    * loader/SubframeLoader.h:
    
    LayoutTests: 
    
    * fast/loader/javascript-url-in-embed-expected.txt: Added.
    * fast/loader/javascript-url-in-embed.html: Added.
    * fast/loader/javascript-url-in-object-expected.txt: Added.
    * fast/loader/javascript-url-in-object.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/SubframeLoader.cpp
+++ third_party/WebKit/WebCore/loader/SubframeLoader.cpp
@@ -38,9 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoaderClient.h"
 #include "HTMLAppletElement.h"
 #include "HTMLFrameElementBase.h"
-#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-#include "HTMLMediaElement.h"
-#endif
 #include "HTMLNames.h"
 #include "HTMLPlugInElement.h"
 #include "MIMETypeRegistry.h"
@@ -48,12 +45,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Page.h"
 #include "PluginData.h"
 #include "RenderEmbeddedObject.h"
-#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-#include "RenderVideo.h"
-#endif
 #include "RenderView.h"
 #include "Settings.h"
 #include "XSSAuditor.h"
+
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+#include "HTMLMediaElement.h"
+#include "RenderVideo.h"
+#endif
 
 namespace WebCore {
     
@@ -91,12 +90,7 @@ bool SubframeLoader::requestFrame(HTMLFrameOwnerElement* ownerElement, const Str
     } else
         url = completeURL(urlString);
 
-    Frame* frame = ownerElement->contentFrame();
-    if (frame)
-        frame->redirectScheduler()->scheduleLocationChange(url.string(), m_frame->loader()->outgoingReferrer(), lockHistory, lockBackForwardList, m_frame->loader()->isProcessingUserGesture());
-    else
-        frame = loadSubframe(ownerElement, url, frameName, m_frame->loader()->outgoingReferrer());
-    
+    Frame* frame = loadOrRedirectSubframe(ownerElement, url, frameName, lockHistory, lockBackForwardList);
     if (!frame)
         return false;
 
@@ -139,10 +133,10 @@ bool SubframeLoader::requestObject(RenderEmbeddedObject* renderer, const String&
     ASSERT(renderer->node()->hasTagName(objectTag) || renderer->node()->hasTagName(embedTag));
     HTMLPlugInElement* element = static_cast<HTMLPlugInElement*>(renderer->node());
 
-    // If the plug-in element already contains a subframe, requestFrame will re-use it. Otherwise,
+    // If the plug-in element already contains a subframe, loadOrRedirectSubframe will re-use it. Otherwise,
     // it will create a new frame and set it as the RenderPart's widget, causing what was previously 
     // in the widget to be torn down.
-    return requestFrame(element, completedURL, frameName);
+    return loadOrRedirectSubframe(element, completedURL, frameName, true, true);
 }
 
 
@@ -235,6 +229,16 @@ PassRefPtr<Widget> SubframeLoader::createJavaAppletWidget(const IntSize& size, H
 
     m_containsPlugins = true;
     return widget;
+}
+
+Frame* SubframeLoader::loadOrRedirectSubframe(HTMLFrameOwnerElement* ownerElement, const KURL& url, const AtomicString& frameName, bool lockHistory, bool lockBackForwardList)
+{
+    Frame* frame = ownerElement->contentFrame();
+    if (frame)
+        frame->redirectScheduler()->scheduleLocationChange(url.string(), m_frame->loader()->outgoingReferrer(), lockHistory, lockBackForwardList, m_frame->loader()->isProcessingUserGesture());
+    else
+        frame = loadSubframe(ownerElement, url, frameName, m_frame->loader()->outgoingReferrer());
+    return frame;
 }
 
 Frame* SubframeLoader::loadSubframe(HTMLFrameOwnerElement* ownerElement, const KURL& url, const String& name, const String& referrer)
