b57ff447e977e709d9c0c844ba05f100117a7bab
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-24 18:55:49 +0000

    Fixes for the default texture.
    Since we are simulating non-shared resources on top
    of shared resources we can't actually let contexts
    use texture 0 because they are all sharing it.
    
    Also, moved the black textures to the texture manager.
    
    TEST=unit tests and ran conformance tests
    BUG=none
    
    Review URL: http://codereview.chromium.org/3150026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57214 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/cmd_buffer_engine.h"
 #include "gpu/command_buffer/service/context_group.h"
 #include "gpu/command_buffer/service/program_manager.h"
+#include "gpu/command_buffer/service/test_helper.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using ::gfx::MockGLInterface;
@@ -97,7 +98,8 @@ TEST_F(GLES2DecoderWithShaderTest, DrawArraysBadTextureUsesBlack) {
     EXPECT_CALL(*gl_, ActiveTexture(GL_TEXTURE0))
         .Times(1)
         .RetiresOnSaturation();
-    EXPECT_CALL(*gl_, BindTexture(GL_TEXTURE_2D, kServiceBlackTexture2dId))
+    EXPECT_CALL(*gl_, BindTexture(
+        GL_TEXTURE_2D, TestHelper::kServiceBlackTexture2dId))
         .Times(1)
         .RetiresOnSaturation();
     EXPECT_CALL(*gl_, DrawArrays(GL_TRIANGLES, 0, kNumVertices))
--- gpu/command_buffer/service/texture_manager_unittest.cc
+++ gpu/command_buffer/service/texture_manager_unittest.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/gfx/gl/gl_mock.h"
 #include "gpu/GLES2/gles2_command_buffer.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "gpu/command_buffer/service/test_helper.h"
 
 using ::testing::Pointee;
+using ::testing::_;
 
 namespace gpu {
 namespace gles2 {
@@ -20,6 +22,12 @@ class TextureManagerTest : public testing::Test {
   static const GLint kMaxCubeMapTextureSize = 8;
   static const GLint kMax2dLevels = 5;
   static const GLint kMaxCubeMapLevels = 4;
+
+  static const GLuint kServiceBlackTexture2dId = 701;
+  static const GLuint kServiceBlackTextureCubemapId = 702;
+  static const GLuint kServiceDefaultTexture2dId = 703;
+  static const GLuint kServiceDefaultTextureCubemapId = 704;
+
 
   TextureManagerTest()
       : manager_(false, false, false, kMaxTextureSize, kMaxCubeMapTextureSize) {
@@ -33,6 +41,9 @@ class TextureManagerTest : public testing::Test {
   virtual void SetUp() {
     gl_.reset(new ::testing::StrictMock< ::gfx::MockGLInterface>());
     ::gfx::GLInterface::SetGLInterface(gl_.get());
+
+    TestHelper::SetupTextureManagerInitExpectations(gl_.get());
+    manager_.Initialize();
   }
 
   virtual void TearDown() {
@@ -51,6 +62,10 @@ const GLint TextureManagerTest::kMaxTextureSize;
 const GLint TextureManagerTest::kMaxCubeMapTextureSize;
 const GLint TextureManagerTest::kMax2dLevels;
 const GLint TextureManagerTest::kMaxCubeMapLevels;
+const GLuint TextureManagerTest::kServiceBlackTexture2dId;
+const GLuint TextureManagerTest::kServiceBlackTextureCubemapId;
+const GLuint TextureManagerTest::kServiceDefaultTexture2dId;
+const GLuint TextureManagerTest::kServiceDefaultTextureCubemapId;
 #endif
 
 TEST_F(TextureManagerTest, Basic) {
@@ -86,6 +101,9 @@ TEST_F(TextureManagerTest, Destroy) {
   TextureManager::TextureInfo* info1 = manager_.GetTextureInfo(kClient1Id);
   ASSERT_TRUE(info1 != NULL);
   EXPECT_CALL(*gl_, DeleteTextures(1, ::testing::Pointee(kService1Id)))
+      .Times(1)
+      .RetiresOnSaturation();
+  EXPECT_CALL(*gl_, DeleteTextures(4, _))
       .Times(1)
       .RetiresOnSaturation();
   manager_.Destroy(true);
