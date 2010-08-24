2a71867445e04848169ecd5e56f9dd00f639758a
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-24 20:26:23 +0000

    Change the FileUtilProxy callbacks to report error codes instead of booleans.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/3165050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/redirect_to_file_resource_handler.cc
+++ chrome/browser/renderer_host/redirect_to_file_resource_handler.cc
@@ -154,6 +154,7 @@ RedirectToFileResourceHandler::~RedirectToFileResourceHandler() {
 }
 
 void RedirectToFileResourceHandler::DidCreateTemporaryFile(
+    int /*error_code*/,
     base::PassPlatformFile file_handle,
     FilePath file_path) {
   file_path_ = file_path;
--- chrome/browser/renderer_host/redirect_to_file_resource_handler.h
+++ chrome/browser/renderer_host/redirect_to_file_resource_handler.h
@@ -47,7 +47,8 @@ class RedirectToFileResourceHandler : public ResourceHandler {
 
  private:
   virtual ~RedirectToFileResourceHandler();
-  void DidCreateTemporaryFile(base::PassPlatformFile file_handle,
+  void DidCreateTemporaryFile(int error_code,
+                              base::PassPlatformFile file_handle,
                               FilePath file_path);
   void DidWriteToFile(int result);
   bool WriteMore();
