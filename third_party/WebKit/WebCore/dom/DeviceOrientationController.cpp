065ce08c9a9676d0ae8f046488a035a89b9bc2de
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-24 19:52:25 +0000

    2010-08-24  Hans Wennborg  <hans@chromium.org>
            Reviewed by Steve Block.
    
            Remove null-checks from DeviceOrientationController
            https://bugs.webkit.org/show_bug.cgi?id=44504
    
            Remove checks for m_client being NULL from DeviceOrientationController.
            It will never be NULL, and this is checked by an ASSERT on construction.
    
            Will be covered by layout tests for device orientation.
    
            * dom/DeviceOrientationController.cpp:
            (WebCore::DeviceOrientationController::timerFired):
            (WebCore::DeviceOrientationController::addListener):
            (WebCore::DeviceOrientationController::removeListener):
            (WebCore::DeviceOrientationController::removeAllListeners):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/DeviceOrientationController.cpp
+++ third_party/WebKit/WebCore/dom/DeviceOrientationController.cpp
@@ -45,9 +45,9 @@ DeviceOrientationController::DeviceOrientationController(Page* page, DeviceOrien
 void DeviceOrientationController::timerFired(Timer<DeviceOrientationController>* timer)
 {
     ASSERT_UNUSED(timer, timer == &m_timer);
-    ASSERT(!m_client || m_client->lastOrientation());
+    ASSERT(m_client->lastOrientation());
 
-    RefPtr<DeviceOrientation> orientation = m_client ?  m_client->lastOrientation() : DeviceOrientation::create();
+    RefPtr<DeviceOrientation> orientation = m_client->lastOrientation();
     RefPtr<DeviceOrientationEvent> event = DeviceOrientationEvent::create(eventNames().deviceorientationEvent, orientation.get());
 
     Vector<DOMWindow*> listenersVector;
@@ -59,11 +59,10 @@ void DeviceOrientationController::timerFired(Timer<DeviceOrientationController>*
 
 void DeviceOrientationController::addListener(DOMWindow* window)
 {
-    // If no client is present, we should fire an event with all parameters null. If
-    // the client already has an orientation, we should fire an event with that
-    // orientation. In both cases, the event is fired asynchronously, but without
+    // If the client already has an orientation, we should fire an event with that
+    // orientation. The event is fired asynchronously, but without
     // waiting for the client to get a new orientation.
-    if (!m_client || m_client->lastOrientation()) {
+    if (m_client->lastOrientation()) {
         m_newListeners.add(window);
         if (!m_timer.isActive())
             m_timer.startOneShot(0);
@@ -72,7 +71,7 @@ void DeviceOrientationController::addListener(DOMWindow* window)
     // The client must not call back synchronously.
     bool wasEmpty = m_listeners.isEmpty();
     m_listeners.add(window);
-    if (wasEmpty && m_client)
+    if (wasEmpty)
         m_client->startUpdating();
 }
 
@@ -80,7 +79,7 @@ void DeviceOrientationController::removeListener(DOMWindow* window)
 {
     m_listeners.remove(window);
     m_newListeners.remove(window);
-    if (m_listeners.isEmpty() && m_client)
+    if (m_listeners.isEmpty())
         m_client->stopUpdating();
 }
 
@@ -92,7 +91,7 @@ void DeviceOrientationController::removeAllListeners(DOMWindow* window)
 
     m_listeners.removeAll(window);
     m_newListeners.remove(window);
-    if (m_listeners.isEmpty() && m_client)
+    if (m_listeners.isEmpty())
         m_client->stopUpdating();
 }
 
