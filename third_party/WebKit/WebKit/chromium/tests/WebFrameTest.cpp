5a6791e3569874cd836ae1ac28646e615510123e
rolandsteiner@chromium.org <rolandsteiner@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-24 07:19:49 +0000

    2010-08-24  Roland Steiner  <rolandsteiner@chromium.org>
            Unreviewed build fix.
    
            put the "DISABLED_" in front of the right identifier.
    
            * tests/WebFrameTest.cpp:
            (WebKit::TEST_F):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/tests/WebFrameTest.cpp
+++ third_party/WebKit/WebKit/chromium/tests/WebFrameTest.cpp
@@ -70,7 +70,7 @@ public:
 class TestWebFrameClient : public WebFrameClient {
 };
 
-TEST_F(DISABLED_WebFrameTest, ContentText)
+TEST_F(WebFrameTest, DISABLED_ContentText)
 {
     // Register our resources.
     WebURLResponse response;
