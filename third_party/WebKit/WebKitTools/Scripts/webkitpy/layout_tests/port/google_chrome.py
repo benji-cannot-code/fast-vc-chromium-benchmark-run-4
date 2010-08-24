60681e4f57bc14c9e54e987105ec9884587d4124
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-24 22:37:39 +0000

    2010-08-24  Tony Chang  <tony@chromium.org>
            Reviewed by Eric Seidel.
    
            [chromium] fix the platform result fallback order on mac/win
            https://bugs.webkit.org/show_bug.cgi?id=44483
    
            * Scripts/webkitpy/layout_tests/port/google_chrome.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65944 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/google_chrome.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/google_chrome.py
@@ -37,9 +37,10 @@ def GetGoogleChromePort(port_name, options):
             def baseline_search_path(self):
                 paths = chromium_linux.ChromiumLinuxPort.baseline_search_path(
                     self)
-                paths.insert(0, self._webkit_baseline_path(self._name))
+                paths.insert(0, self._webkit_baseline_path(
+                    'google-chrome-linux32'))
                 return paths
-        return GoogleChromeLinux32Port(port_name, options)
+        return GoogleChromeLinux32Port(None, options)
     elif port_name == 'google-chrome-linux64':
         import chromium_linux
 
@@ -47,9 +48,10 @@ def GetGoogleChromePort(port_name, options):
             def baseline_search_path(self):
                 paths = chromium_linux.ChromiumLinuxPort.baseline_search_path(
                     self)
-                paths.insert(0, self._webkit_baseline_path(self._name))
+                paths.insert(0, self._webkit_baseline_path(
+                    'google-chrome-linux64'))
                 return paths
-        return GoogleChromeLinux64Port(port_name, options)
+        return GoogleChromeLinux64Port(None, options)
     elif port_name.startswith('google-chrome-mac'):
         import chromium_mac
 
@@ -60,7 +62,7 @@ def GetGoogleChromePort(port_name, options):
                 paths.insert(0, self._webkit_baseline_path(
                     'google-chrome-mac'))
                 return paths
-        return GoogleChromeMacPort(port_name, options)
+        return GoogleChromeMacPort(None, options)
     elif port_name.startswith('google-chrome-win'):
         import chromium_win
 
@@ -71,5 +73,5 @@ def GetGoogleChromePort(port_name, options):
                 paths.insert(0, self._webkit_baseline_path(
                     'google-chrome-win'))
                 return paths
-        return GoogleChromeWinPort(port_name, options)
+        return GoogleChromeWinPort(None, options)
     raise NotImplementedError('unsupported port: %s' % port_name)
