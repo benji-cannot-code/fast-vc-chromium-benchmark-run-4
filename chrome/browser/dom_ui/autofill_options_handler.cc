60131f6c4921c866fa65996ba7e03a2b7e6e80f4
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-24 21:33:01 +0000

    DOMUI: Implement the 'Remove...' button on the AutoFill page.
    BUG=49094
    TEST=none
    
    Review URL: http://codereview.chromium.org/3140026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57238 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/autofill_options_handler.cc
+++ chrome/browser/dom_ui/autofill_options_handler.cc
@@ -73,6 +73,13 @@ void AutoFillOptionsHandler::OnPersonalDataChanged() {
 }
 
 void AutoFillOptionsHandler::RegisterMessages() {
+  dom_ui_->RegisterMessageCallback(
+      "removeAddress",
+      NewCallback(this, &AutoFillOptionsHandler::RemoveAddress));
+
+  dom_ui_->RegisterMessageCallback(
+      "removeCreditCard",
+      NewCallback(this, &AutoFillOptionsHandler::RemoveCreditCard));
 }
 
 void AutoFillOptionsHandler::LoadAutoFillData() {
@@ -85,6 +92,7 @@ void AutoFillOptionsHandler::LoadAutoFillData() {
        i != personal_data_->profiles().end(); ++i) {
     DictionaryValue* address = new DictionaryValue();
     address->SetString("label", (*i)->PreviewSummary());
+    address->SetInteger("unique_id", (*i)->unique_id());
     addresses.Append(address);
   }
 
@@ -97,9 +105,36 @@ void AutoFillOptionsHandler::LoadAutoFillData() {
        i != personal_data_->credit_cards().end(); ++i) {
     DictionaryValue* credit_card = new DictionaryValue();
     credit_card->SetString("label", (*i)->PreviewSummary());
+    credit_card->SetInteger("unique_id", (*i)->unique_id());
     credit_cards.Append(credit_card);
   }
 
   dom_ui_->CallJavascriptFunction(L"AutoFillOptions.updateCreditCards",
                                   credit_cards);
+}
+
+void AutoFillOptionsHandler::RemoveAddress(const ListValue* args) {
+  if (!personal_data_->IsDataLoaded())
+    return;
+
+  int unique_id = 0;
+  if (!ExtractIntegerValue(args, &unique_id)) {
+    NOTREACHED();
+    return;
+  }
+
+  personal_data_->RemoveProfile(unique_id);
+}
+
+void AutoFillOptionsHandler::RemoveCreditCard(const ListValue* args) {
+  if (!personal_data_->IsDataLoaded())
+    return;
+
+  int unique_id = 0;
+  if (!ExtractIntegerValue(args, &unique_id)) {
+    NOTREACHED();
+    return;
+  }
+
+  personal_data_->RemoveCreditCard(unique_id);
 }
