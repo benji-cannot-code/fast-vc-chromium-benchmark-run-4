6425daadab1683aac906a1ec3baf4fa89bcda3d6
tkent@chromium.org <tkent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-24 06:08:18 +0000

    webkit_support: Implement IndexedDB initialization.
    BUG=none
    TEST=none; This is a part of test code.
    Review URL: http://codereview.chromium.org/3176031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57145 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/test_webkit_client.cc
+++ webkit/support/test_webkit_client.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/WebKit/chromium/public/WebData.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebDatabase.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebGraphicsContext3D.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebIDBFactory.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebRuntimeFeatures.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebKit.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebScriptController.h"
@@ -237,6 +238,10 @@ void TestWebKitClient::dispatchStorageEvent(const WebKit::WebString& key,
     const WebKit::WebString& origin, const WebKit::WebURL& url,
     bool is_local_storage) {
   // The event is dispatched by the proxy.
+}
+
+WebKit::WebIDBFactory* TestWebKitClient::idbFactory() {
+  return WebKit::WebIDBFactory::create();
 }
 
 #if defined(OS_WIN)
