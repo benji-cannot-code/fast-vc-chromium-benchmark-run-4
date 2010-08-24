d2ff63702253e5890d4f908ecad72d77a22d9455
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-24 17:59:22 +0000

    Paste event fires twice for mac WebKit <rdar://problem/8094611> https://bugs.webkit.org/show_bug.cgi?id=44535
    Reviewed by Oliver Hunt.
    
    WebCore: 
    
    The mac implementation of paste in WebKit was accidentally calling 
    Editor::tryDHTMLPaste twice, once in -[WebHTMLView paste:] and once
    in Editor::pasteAsPlainText (which is called -[WebHTMLView paste:]).
    Use the new pasteAsPlainTextBypassingDHTML function to bypass the
    second call.
    
    Test: editing/pasteboard/paste-event-only-once.html
    
    * WebCore.exp.in:
    * editing/Editor.cpp:
    (WebCore::Editor::pasteAsPlainTextBypassingDHTML): 
    * editing/Editor.h:
    Expose a function which just pastes using from the general pasteboard,
    bypassing all the checking and DHTML pasting.
    
    WebKit/mac: 
    
    The mac implementation of paste in WebKit was accidentally calling 
    Editor::tryDHTMLPaste twice, once in -[WebHTMLView paste:] and once
    in Editor::pasteAsPlainText (which is called -[WebHTMLView paste:]).
    Use the new pasteAsPlainTextBypassingDHTML function to bypass the
    second call.
    
    * WebView/WebHTMLView.mm:
    (-[WebHTMLView paste:]):
    
    LayoutTests: 
    
    * editing/pasteboard/paste-event-only-once-expected.txt: Added.
    * editing/pasteboard/paste-event-only-once.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65912 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebHTMLView.mm
@@ -5177,7 +5177,7 @@ static BOOL writingDirectionKeyBindingsEnabled()
     if (coreFrame->selection()->isContentRichlyEditable())
         [self _pasteWithPasteboard:[NSPasteboard generalPasteboard] allowPlainText:YES];
     else
-        coreFrame->editor()->pasteAsPlainText();
+        coreFrame->editor()->pasteAsPlainTextBypassingDHTML();
 }
 
 - (void)closeIfNotCurrentView
