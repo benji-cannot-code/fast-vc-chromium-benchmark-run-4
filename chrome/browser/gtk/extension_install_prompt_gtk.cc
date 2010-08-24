6a593d58bcc1ded8481ff24f95eb24fc5919efc4
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-24 00:16:39 +0000

    [GTK] Don't line wrap on the extension uninstall dialog.
    BUG=52781
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/3106033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57126 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/extension_install_prompt_gtk.cc
+++ chrome/browser/gtk/extension_install_prompt_gtk.cc
@@ -2,6 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//
+// Currently this file is only used for the uninstall prompt. The install prompt
+// code is in extension_install_prompt2_gtk.cc.
 
 #include <gtk/gtk.h>
 
@@ -20,8 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Profile;
 
 namespace {
-
-const int kRightColumnWidth = 290;
 
 // Left or right margin.
 const int kPanelHorizMargin = 13;
@@ -91,14 +92,10 @@ void ShowInstallPromptDialog(GtkWindow* parent, SkBitmap* skia_icon,
   GtkWidget* heading_label = MakeMarkupLabel("<span weight=\"bold\">%s</span>",
                                              heading_text);
   gtk_misc_set_alignment(GTK_MISC(heading_label), 0.0, 0.5);
-  gtk_label_set_selectable(GTK_LABEL(heading_label), TRUE);
   gtk_box_pack_start(GTK_BOX(right_column_area), heading_label, TRUE, TRUE, 0);
 
   GtkWidget* warning_label = gtk_label_new(UTF16ToUTF8(warning_text).c_str());
-  gtk_label_set_line_wrap(GTK_LABEL(warning_label), TRUE);
-  gtk_widget_set_size_request(warning_label, kRightColumnWidth, -1);
   gtk_misc_set_alignment(GTK_MISC(warning_label), 0.0, 0.5);
-  gtk_label_set_selectable(GTK_LABEL(warning_label), TRUE);
   gtk_box_pack_start(GTK_BOX(right_column_area), warning_label, TRUE, TRUE, 0);
 
   g_signal_connect(dialog, "response", G_CALLBACK(OnDialogResponse), delegate);
