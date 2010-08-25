2bf91168c214dc9ac99c568a665f03337c889a73
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-25 13:50:08 +0000

    2010-08-25  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: support disabled and checked context menu items.
            https://bugs.webkit.org/show_bug.cgi?id=44601
    
            * bindings/js/JSInspectorFrontendHostCustom.cpp:
            (WebCore::JSInspectorFrontendHost::showContextMenu):
            * bindings/v8/custom/V8InspectorFrontendHostCustom.cpp:
            (WebCore::V8InspectorFrontendHost::showContextMenuCallback):
            * inspector/front-end/ContextMenu.js:
            (WebInspector.ContextMenu.prototype.appendItem):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66010 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8InspectorFrontendHostCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8InspectorFrontendHostCustom.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "InspectorController.h"
 #include "InspectorFrontendHost.h"
+#include "PlatformString.h"
 
 #include "V8Binding.h"
 #include "V8MouseEvent.h"
@@ -77,18 +78,26 @@ v8::Handle<v8::Value> V8InspectorFrontendHost::showContextMenuCallback(const v8:
 
     for (size_t i = 0; i < array->Length(); ++i) {
         v8::Local<v8::Object> item = v8::Local<v8::Object>::Cast(array->Get(v8::Integer::New(i)));
-        v8::Local<v8::Value> label = item->Get(v8::String::New("label"));
+        v8::Local<v8::Value> type = item->Get(v8::String::New("type"));
         v8::Local<v8::Value> id = item->Get(v8::String::New("id"));
-        if (label->IsUndefined() || id->IsUndefined()) {
-          items.append(new ContextMenuItem(SeparatorType,
-                                           ContextMenuItemCustomTagNoAction,
-                                           String()));
+        v8::Local<v8::Value> label = item->Get(v8::String::New("label"));
+        v8::Local<v8::Value> enabled = item->Get(v8::String::New("enabled"));
+        v8::Local<v8::Value> checked = item->Get(v8::String::New("checked"));
+        if (!type->IsString())
+            continue;
+        String typeString = toWebCoreStringWithNullCheck(type);
+        if (typeString == "separator") {
+            items.append(new ContextMenuItem(SeparatorType,
+                                             ContextMenuItemCustomTagNoAction,
+                                             String()));
         } else {
-          ContextMenuAction typedId = static_cast<ContextMenuAction>(
-              ContextMenuItemBaseCustomTag + id->ToInt32()->Value());
-          items.append(new ContextMenuItem(ActionType,
-                                           typedId,
-                                           toWebCoreStringWithNullCheck(label)));
+            ContextMenuAction typedId = static_cast<ContextMenuAction>(ContextMenuItemBaseCustomTag + id->ToInt32()->Value());
+            ContextMenuItem* menuItem = new ContextMenuItem((typeString == "checkbox" ? CheckableActionType : ActionType), typedId, toWebCoreStringWithNullCheck(label));
+            if (checked->IsBoolean())
+                menuItem->setChecked(checked->ToBoolean()->Value());
+            if (enabled->IsBoolean())
+                menuItem->setEnabled(enabled->ToBoolean()->Value());
+            items.append(menuItem);
         }
     }
 
