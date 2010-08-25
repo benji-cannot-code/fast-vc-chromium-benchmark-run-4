bf6da3960f58128a9f1e60c7db3693723c14fca3
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-25 23:38:58 +0000

    WebCore: Build correction, no review.
    * WebCore.vcproj/WebCore.vcproj: Add missing implementation
      for PlatformRefPtrCairo.
    
    WebKit2: Build fix, no review.
    
    Add conditional guards around Safari-specific
    include files.
    
    * WebProcess/WebPage/win/WebPageWin.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66054 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebPage/win/WebPageWin.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebPage/win/WebPageWin.cpp
@@ -36,7 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/Page.h>
 #include <WebCore/PlatformKeyboardEvent.h>
 #include <WebCore/Settings.h>
+#if PLATFORM(CG)
 #include <WebKitSystemInterface/WebKitSystemInterface.h>
+#endif
 #include <WinUser.h>
 
 using namespace WebCore;
