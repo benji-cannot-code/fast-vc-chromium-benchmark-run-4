dcb27f67764bc1fa48c56b71d188f5c8b3336fc5
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-25 21:01:34 +0000

    Reapply menu model cleanups, with WrenchMenuModel being a SimpleMenuModel.
    BUG=47320
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/3184021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57384 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/wrench_menu_model.h
+++ chrome/browser/wrench_menu_model.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_WRENCH_MENU_MODEL_H_
 #pragma once
 
+#include "app/menus/accelerator.h"
 #include "app/menus/button_menu_item_model.h"
 #include "app/menus/simple_menu_model.h"
 #include "base/scoped_ptr.h"
@@ -69,25 +70,23 @@ class ToolsMenuModel : public menus::SimpleMenuModel {
 
 // A menu model that builds the contents of the wrench menu.
 class WrenchMenuModel : public menus::SimpleMenuModel,
+                        public menus::SimpleMenuModel::Delegate,
                         public menus::ButtonMenuItemModel::Delegate,
                         public TabStripModelObserver,
                         public NotificationObserver {
  public:
-  WrenchMenuModel(menus::SimpleMenuModel::Delegate* delegate,
-                  Browser* browser);
+  WrenchMenuModel(menus::AcceleratorProvider* provider, Browser* browser);
   virtual ~WrenchMenuModel();
 
-  // Overridden from menus::SimpleMenuModel:
-  virtual bool IsLabelDynamicAt(int index) const;
-  virtual string16 GetLabelAt(int index) const;
-  virtual bool HasIcons() const { return true; }
-  virtual bool GetIconAt(int index, SkBitmap* icon) const;
-
-  // Overridden from menus::ButtonMenuItemModel::Delegate:
+  // Overridden for both ButtonMenuItemModel::Delegate and SimpleMenuModel:
   virtual bool IsLabelForCommandIdDynamic(int command_id) const;
   virtual string16 GetLabelForCommandId(int command_id) const;
   virtual void ExecuteCommand(int command_id);
+  virtual bool IsCommandIdChecked(int command_id) const;
   virtual bool IsCommandIdEnabled(int command_id) const;
+  virtual bool GetAcceleratorForCommandId(
+      int command_id,
+      menus::Accelerator* accelerator);
 
   // Overridden from TabStripModelObserver:
   virtual void TabSelectedAt(TabContents* old_contents,
@@ -112,7 +111,7 @@ class WrenchMenuModel : public menus::SimpleMenuModel,
  private:
   // Testing constructor used for mocking.
   friend class ::MockWrenchMenuModel;
-  WrenchMenuModel() : menus::SimpleMenuModel(NULL) {}
+  WrenchMenuModel();
 
   void Build();
 
@@ -126,7 +125,6 @@ class WrenchMenuModel : public menus::SimpleMenuModel,
 
   string16 GetSyncMenuLabel() const;
   string16 GetAboutEntryMenuLabel() const;
-  bool IsDynamicItem(int index) const;
 
   // Models for the special menu items with buttons.
   scoped_ptr<menus::ButtonMenuItemModel> edit_menu_item_model_;
@@ -138,7 +136,7 @@ class WrenchMenuModel : public menus::SimpleMenuModel,
   // Tools menu.
   scoped_ptr<ToolsMenuModel> tools_menu_model_;
 
-  menus::SimpleMenuModel::Delegate* delegate_; // weak
+  menus::AcceleratorProvider* provider_;  // weak
 
   Browser* browser_;  // weak
   TabStripModel* tabstrip_model_; // weak
