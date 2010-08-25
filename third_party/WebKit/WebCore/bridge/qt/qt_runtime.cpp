6473a704289b0db11cd2564eeab063a01e8aca24
loki@webkit.org <loki@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-25 09:36:51 +0000

    Avoid increasing required alignment of target type warning https://bugs.webkit.org/show_bug.cgi?id=43963
    Reviewed by Gavin Barraclough.
    
    Fix alignment warnings on Qt.
    
    WebCore: 
    
    * bridge/qt/qt_runtime.cpp:
    (JSC::Bindings::convertQVariantToValue):
    * platform/graphics/qt/ImageBufferQt.cpp:
    (WebCore::getImageData):
    (WebCore::putImageData):
    * platform/image-decoders/ImageDecoder.h:
    (WebCore::RGBA32Buffer::getAddr):
    * platform/text/qt/TextCodecQt.cpp:
    (WebCore::TextCodecQt::decode):
    
    WebKit/qt: 
    
    * Api/qwebelement.cpp:
    (QWebElement::evaluateJavaScript):
    * Api/qwebframe.cpp:
    (QWebFrame::addToJavaScriptWindowObject):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@65999 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
+++ third_party/WebKit/WebCore/bridge/qt/qt_runtime.cpp
@@ -906,7 +906,7 @@ JSValue convertQVariantToValue(ExecState* exec, PassRefPtr<RootObject> root, con
             JSValue val = convertQVariantToValue(exec, root.get(), i.value());
             if (val) {
                 PutPropertySlot slot;
-                ret->put(exec, Identifier(exec, (const UChar *)s.constData(), s.length()), val, slot);
+                ret->put(exec, Identifier(exec, reinterpret_cast_ptr<const UChar *>(s.constData()), s.length()), val, slot);
                 // ### error case?
             }
             ++i;
