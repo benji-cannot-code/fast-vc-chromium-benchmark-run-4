e8862159eb6ab152687f2a2b1a5c7a68cf038c50
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-25 13:08:19 +0000

    2010-08-25  Zaheer Ahmad <zaheer.mot@gmail.com>
            Reviewed by Xan Lopez.
    
            [GTK] GTK port doesnt compile with video turned off
            https://bugs.webkit.org/show_bug.cgi?id=44236
    
            Generate the audio GObject DOM bindings only if video enabled
            * WebCore/GNUMakefile.am
            * WebCore/bindings/gobject/WebKitHTMLElementWrapperFactory.cpp:
            Make the audio code invocation conditional
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/gobject/WebKitHTMLElementWrapperFactory.cpp
+++ third_party/WebKit/WebCore/bindings/gobject/WebKitHTMLElementWrapperFactory.cpp
@@ -97,7 +97,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/WebKitDOMHTMLAnchorElementPrivate.h"
 #include "webkit/WebKitDOMHTMLAppletElementPrivate.h"
 #include "webkit/WebKitDOMHTMLAreaElementPrivate.h"
-#include "webkit/WebKitDOMHTMLAudioElementPrivate.h"
 #include "webkit/WebKitDOMHTMLBRElementPrivate.h"
 #include "webkit/WebKitDOMHTMLBaseElementPrivate.h"
 #include "webkit/WebKitDOMHTMLBaseFontElementPrivate.h"
@@ -154,6 +153,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/WebKitDOMHTMLUListElementPrivate.h"
 #include "webkit/webkitdom.h"
 
+#if ENABLE(VIDEO)
+#include "webkit/WebKitDOMHTMLAudioElementPrivate.h"
+#endif
+
 #include <wtf/text/CString.h>
 
 namespace WebKit {
@@ -178,10 +181,12 @@ static gpointer createAreaWrapper(PassRefPtr<HTMLElement> element)
     return wrapHTMLAreaElement(static_cast<HTMLAreaElement*>(element.get()));
 }
 
+#if ENABLE(VIDEO)
 static gpointer createAudioWrapper(PassRefPtr<HTMLElement> element)
 {
     return wrapHTMLAudioElement(static_cast<HTMLAudioElement*>(element.get()));
 }
+#endif
 
 static gpointer createBaseWrapper(PassRefPtr<HTMLElement> element)
 {
@@ -454,7 +459,9 @@ gpointer createHTMLElementWrapper(PassRefPtr<WebCore::HTMLElement> element)
     if (map.isEmpty()) {
        map.set(aTag.localName().impl(), createAnchorWrapper);
        map.set(appletTag.localName().impl(), createAppletWrapper);
+#if ENABLE(VIDEO)
        map.set(audioTag.localName().impl(), createAudioWrapper);
+#endif
        map.set(areaTag.localName().impl(), createAreaWrapper);
        map.set(baseTag.localName().impl(), createBaseWrapper);
        map.set(basefontTag.localName().impl(), createBaseFontWrapper);
