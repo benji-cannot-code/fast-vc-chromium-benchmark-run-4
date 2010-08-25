b6d1670a18a605054d5a33cda9bc0e1fcc51e39b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-25 11:52:32 +0000

    2010-08-25  Pawel Hajdan  <phajdan.jr@chromium.org>
            Reviewed by Darin Fisher.
    
            Add an assertion to prevent re-initializing WebKit.
            https://bugs.webkit.org/show_bug.cgi?id=44545
    
            This will help prevent crashes like:
            - http://code.google.com/p/chromium/issues/detail?id=52731
            - http://code.google.com/p/chromium/issues/detail?id=52643
    
            * src/WebKit.cpp:
            (WebKit::initialize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66006 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebKit.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebKit.cpp
@@ -47,11 +47,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
+// Make sure we are not re-initialized in the same address space.
+// Doing so may cause hard to reproduce crashes.
+static bool s_webKitInitialized = false;
+
 static WebKitClient* s_webKitClient = 0;
 static bool s_layoutTestMode = false;
 
 void initialize(WebKitClient* webKitClient)
 {
+    ASSERT(!s_webKitInitialized);
+    s_webKitInitialized = true;
+
     ASSERT(webKitClient);
     ASSERT(!s_webKitClient);
     s_webKitClient = webKitClient;
