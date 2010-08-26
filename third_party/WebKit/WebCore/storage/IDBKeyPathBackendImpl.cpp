c2d33ffe3178c8e2f1b7c2bb1a562c23a29e122d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-26 08:19:28 +0000

    2010-08-26  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Kent Tamura.
    
            Add ENABLE(INDEXED_DATABASE) guard
            https://bugs.webkit.org/show_bug.cgi?id=44659
    
            Build fix for ports with index database disabled.
    
            * storage/IDBKeyPathBackendImpl.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/IDBKeyPathBackendImpl.cpp
+++ third_party/WebKit/WebCore/storage/IDBKeyPathBackendImpl.cpp
@@ -31,7 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "Chromium should not compile this file and instead define its own version of this factory that navigates the multi-process boundry."
 #endif
 
+#if ENABLE(INDEXED_DATABASE)
+
 void IDBKeyPathBackendImpl::createIDBKeysFromSerializedValuesAndKeyPath(const Vector<RefPtr<SerializedScriptValue>&, 0> values, const String& keyPath, Vector<RefPtr<IDBKey>, 0>& keys)
 {
     // FIXME: Implement this method once JSC supports WireFormat for SerializedScriptValue.
 }
+
+#endif // ENABLE(INDEXED_DATABASE)
