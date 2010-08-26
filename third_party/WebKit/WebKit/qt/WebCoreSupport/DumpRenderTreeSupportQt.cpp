078a264385e0bb67e30e6285e1084831607d2d76
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-26 19:43:35 +0000

    Fix QT bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@66123 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/DumpRenderTreeSupportQt.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/DumpRenderTreeSupportQt.cpp
@@ -389,7 +389,7 @@ void DumpRenderTreeSupportQt::setMediaType(QWebFrame* frame, const QString& type
     WebCore::Frame* coreFrame = QWebFramePrivate::core(frame);
     WebCore::FrameView* view = coreFrame->view();
     view->setMediaType(type);
-    coreFrame->document()->updateStyleSelector();
+    coreFrame->document()->styleSelectorChanged(RecalcStyleImmediately);
     view->layout();
 }
 
