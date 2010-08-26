420a728d3487ad169992ef0136194b49340ab931
yuzo@chromium.org <yuzo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-26 07:13:44 +0000

    Second try for DevTools sanity tests start failing after rolling WebKit from 65992 to 66033
    This time use DISABLED_ instead of FAILS_.
    
    BUG=53406
    TEST=none
    TBR=dumi
    
    Review URL: http://codereview.chromium.org/3186035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57484 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_sanity_unittest.cc
+++ chrome/browser/debugger/devtools_sanity_unittest.cc
@@ -331,7 +331,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsSanityTest,
 }
 
 // Tests set breakpoint.
-IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, FAILS_TestSetBreakpoint) {
+IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, DISABLED_TestSetBreakpoint) {
   RunTest("testSetBreakpoint", kDebuggerTestPage);
 }
 
@@ -342,7 +342,8 @@ IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestPauseOnException) {
 
 // Tests that debugger works correctly if pause event occurs when DevTools
 // frontend is being loaded.
-IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, FAILS_TestPauseWhenLoadingDevTools) {
+IN_PROC_BROWSER_TEST_F(DevToolsSanityTest,
+                       DISABLED_TestPauseWhenLoadingDevTools) {
   RunTest("testPauseWhenLoadingDevTools", kPauseWhenLoadingDevTools);
 }
 
@@ -380,7 +381,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestShowStoragePanel) {
   RunTest("testShowStoragePanel", kDebuggerTestPage);
 }
 
-IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, FAILS_TestMessageLoopReentrant) {
+IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, DISABLED_TestMessageLoopReentrant) {
   RunTest("testMessageLoopReentrant", kDebuggerTestPage);
 }
 
