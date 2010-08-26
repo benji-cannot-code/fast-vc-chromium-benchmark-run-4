d66b0e5943a0828f02df5fb00ad6c14866dc0bab
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-26 06:05:31 +0000

    Revert 57473 and 57476. Best. Programmer. Ever.
    TBR=eroman@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57478 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/shown_sections_handler.cc
+++ chrome/browser/dom_ui/shown_sections_handler.cc
@@ -90,7 +90,7 @@ void ShownSectionsHandler::HandleSetShownSections(const ListValue* args) {
 // static
 void ShownSectionsHandler::RegisterUserPrefs(PrefService* pref_service) {
   pref_service->RegisterIntegerPref(prefs::kNTPShownSections,
-                                    THUMB | RECENT | TIPS | SYNC | APPS);
+                                    THUMB | RECENT | TIPS | SYNC);
 }
 
 // static
@@ -100,10 +100,17 @@ void ShownSectionsHandler::MigrateUserPrefs(PrefService* pref_service,
   bool changed = false;
   int shown_sections = pref_service->GetInteger(prefs::kNTPShownSections);
 
-  if (old_pref_version < 3) {
-    // At version 3, we added apps. To bring attention to the feature, we start
-    // it off as the only expanded section.
-    shown_sections = APPS;
+  if (old_pref_version < 1) {
+    // TIPS was used in early builds of the NNTP but since it was removed before
+    // Chrome 3.0 we want to ensure that it is shown by default.
+    shown_sections |= TIPS | SYNC;
+    changed = true;
+  }
+
+  if (old_pref_version < 2) {
+    // LIST is no longer used. Change to THUMB.
+    shown_sections &= ~LIST;
+    shown_sections |= THUMB;
     changed = true;
   }
 
--- chrome/browser/resources/ntp/apps.js
+++ chrome/browser/resources/ntp/apps.js
@@ -6,14 +6,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function getAppsCallback(data) {
   logEvent('recieved apps');
   var appsSection = $('apps-section');
-  var appsSectionContent = $('apps-section-content');
-  appsSectionContent.textContent = '';
+  var debugSection = $('debug');
+  appsSection.textContent = '';
 
   data.apps.forEach(function(app) {
-    appsSectionContent.appendChild(apps.createElement(app));
+    appsSection.appendChild(apps.createElement(app));
   });
 
-  appsSectionContent.appendChild(apps.createWebStoreElement());
+  // TODO(aa): Figure out what to do with the debug mode when we turn apps on
+  // for everyone.
+  if (appsSection.hasChildNodes()) {
+    appsSection.classList.remove('disabled');
+    if (data.showDebugLink) {
+      debugSection.classList.remove('disabled');
+    }
+
+    appsSection.appendChild(apps.createWebStoreElement());
+  } else {
+    appsSection.classList.add('disabled');
+    debugSection.classList.add('disabled');
+  }
 }
 
 var apps = {
