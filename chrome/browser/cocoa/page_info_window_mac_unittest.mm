1e9f816e5cd4a65bbcaa63f3ed06ef07062fb385
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-26 09:00:00 +0000

    Part 2 of the SSL InfoBubble. 2nd try, this time with test fixed.
    - Added Certificate Information link.
    
    - Changed iconography to be:
     Identity  : Green check mark vs. Red skull and bones
     Connection: Green padlock vs. Orange exclamation point.
     (This used to be Green checkmark and Orange Exclamation point for both sections.
    
    - Removed the icon for the First visit section. Need to see if we want an icon for when you haven't visited the site before. Mocks show no icons when you have visited before.
    
    BUG=52916
    TEST=Open the SSL InfoBubble and notice the new iconography. Click the link and the Certificate dialog should show.
    
    Review URL: http://codereview.chromium.org/3187019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57498 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/page_info_window_mac_unittest.mm
+++ chrome/browser/cocoa/page_info_window_mac_unittest.mm
@@ -22,12 +22,14 @@ class FakeModel : public PageInfoModel {
  public:
   void AddSection(bool state,
                   const string16& title,
-                  const string16& description) {
+                  const string16& description,
+                  SectionInfoType type) {
     sections_.push_back(SectionInfo(
         state,
         title,
         string16(),
-        description));
+        description,
+        type));
   }
 };
 
@@ -114,12 +116,14 @@ TEST_F(PageInfoWindowMacTest, NoHistoryNoSecurity) {
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_SECURITY_TAB_IDENTITY_TITLE),
       l10n_util::GetStringFUTF16(
           IDS_PAGE_INFO_SECURITY_TAB_UNKNOWN_PARTY,
-          ASCIIToUTF16("google.com")));
+          ASCIIToUTF16("google.com")),
+      PageInfoModel::SECTION_INFO_IDENTITY);
   model_->AddSection(false,
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_SECURITY_TAB_CONNECTION_TITLE),
       l10n_util::GetStringFUTF16(
           IDS_PAGE_INFO_SECURITY_TAB_NOT_ENCRYPTED_CONNECTION_TEXT,
-          ASCIIToUTF16("google.com")));
+          ASCIIToUTF16("google.com")),
+      PageInfoModel::SECTION_INFO_CONNECTION);
 
   bridge_->ModelChanged();
 
@@ -132,12 +136,14 @@ TEST_F(PageInfoWindowMacTest, HistoryNoSecurity) {
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_SECURITY_TAB_IDENTITY_TITLE),
       l10n_util::GetStringFUTF16(
           IDS_PAGE_INFO_SECURITY_TAB_UNKNOWN_PARTY,
-          ASCIIToUTF16("google.com")));
+          ASCIIToUTF16("google.com")),
+      PageInfoModel::SECTION_INFO_IDENTITY);
   model_->AddSection(false,
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_SECURITY_TAB_CONNECTION_TITLE),
       l10n_util::GetStringFUTF16(
           IDS_PAGE_INFO_SECURITY_TAB_NOT_ENCRYPTED_CONNECTION_TEXT,
-          ASCIIToUTF16("google.com")));
+          ASCIIToUTF16("google.com")),
+      PageInfoModel::SECTION_INFO_CONNECTION);
 
   // In practice, the history information comes later because it's queried
   // asynchronously, so replicate the double-build here.
@@ -147,7 +153,8 @@ TEST_F(PageInfoWindowMacTest, HistoryNoSecurity) {
       l10n_util::GetStringUTF16(
           IDS_PAGE_INFO_SECURITY_TAB_PERSONAL_HISTORY_TITLE),
       l10n_util::GetStringUTF16(
-          IDS_PAGE_INFO_SECURITY_TAB_FIRST_VISITED_TODAY));
+          IDS_PAGE_INFO_SECURITY_TAB_FIRST_VISITED_TODAY),
+      PageInfoModel::SECTION_INFO_FIRST_VISIT);
 
   bridge_->ModelChanged();
 
@@ -160,7 +167,8 @@ TEST_F(PageInfoWindowMacTest, NoHistoryMixedSecurity) {
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_SECURITY_TAB_IDENTITY_TITLE),
       l10n_util::GetStringFUTF16(
           IDS_PAGE_INFO_SECURITY_TAB_SECURE_IDENTITY,
-          ASCIIToUTF16("Goat Security Systems")));
+          ASCIIToUTF16("Goat Security Systems")),
+      PageInfoModel::SECTION_INFO_IDENTITY);
 
   // This string is super long and the text should overflow the default clip
   // region (kImageSize).
@@ -175,7 +183,8 @@ TEST_F(PageInfoWindowMacTest, NoHistoryMixedSecurity) {
               ASCIIToUTF16("chrome.google.com"),
               base::IntToString16(1024)),
           l10n_util::GetStringUTF16(
-              IDS_PAGE_INFO_SECURITY_TAB_ENCRYPTED_INSECURE_CONTENT_WARNING)));
+              IDS_PAGE_INFO_SECURITY_TAB_ENCRYPTED_INSECURE_CONTENT_WARNING)),
+      PageInfoModel::SECTION_INFO_CONNECTION);
 
   bridge_->ModelChanged();
 
--- chrome/browser/page_info_model.cc
+++ chrome/browser/page_info_model.cc
@@ -100,7 +100,8 @@ PageInfoModel::PageInfoModel(Profile* profile,
       state,
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_SECURITY_TAB_IDENTITY_TITLE),
       head_line,
-      description));
+      description,
+      SECTION_INFO_IDENTITY));
 
   // Connection section.
   // We consider anything less than 80 bits encryption to be weak encryption.
@@ -140,8 +141,9 @@ PageInfoModel::PageInfoModel(Profile* profile,
   if (ssl.security_bits() > 0 && cipher_suite) {
     bool did_fallback = (ssl.connection_status() &
                          net::SSL_CONNECTION_SSL3_FALLBACK) != 0;
-    bool no_renegotiation = (ssl.connection_status() &
-                             net::SSL_CONNECTION_NO_RENEGOTIATION_EXTENSION) != 0;
+    bool no_renegotiation =
+        (ssl.connection_status() &
+        net::SSL_CONNECTION_NO_RENEGOTIATION_EXTENSION) != 0;
     const char *key_exchange, *cipher, *mac;
     net::SSLCipherSuiteToStrings(&key_exchange, &cipher, &mac, cipher_suite);
 
@@ -182,7 +184,8 @@ PageInfoModel::PageInfoModel(Profile* profile,
       state,
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_SECURITY_TAB_CONNECTION_TITLE),
       head_line,
-      description));
+      description,
+      SECTION_INFO_CONNECTION));
 
   // Request the number of visits.
   HistoryService* history = profile->GetHistoryService(
@@ -227,7 +230,8 @@ void PageInfoModel::OnGotVisitCountToHost(HistoryService::Handle handle,
             IDS_PAGE_INFO_SECURITY_TAB_PERSONAL_HISTORY_TITLE),
         string16(),
         l10n_util::GetStringUTF16(
-            IDS_PAGE_INFO_SECURITY_TAB_FIRST_VISITED_TODAY)));
+            IDS_PAGE_INFO_SECURITY_TAB_FIRST_VISITED_TODAY),
+        SECTION_INFO_FIRST_VISIT));
   } else {
     sections_.push_back(SectionInfo(
         true,
@@ -236,7 +240,8 @@ void PageInfoModel::OnGotVisitCountToHost(HistoryService::Handle handle,
         string16(),
         l10n_util::GetStringFUTF16(
             IDS_PAGE_INFO_SECURITY_TAB_VISITED_BEFORE_TODAY,
-            WideToUTF16(base::TimeFormatShortDate(first_visit)))));
+            WideToUTF16(base::TimeFormatShortDate(first_visit))),
+        SECTION_INFO_FIRST_VISIT));
   }
   observer_->ModelChanged();
 }
