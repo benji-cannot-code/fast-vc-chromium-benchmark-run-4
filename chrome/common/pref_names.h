521a43aa397f947f7e6e869633c40b4f6b1f2a9e
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-26 20:10:45 +0000

    Revert 57569 - Implement OSX version for "Start on Login" for background-enabled extensions.
    Review URL: http://codereview.chromium.org/3163029
    
    TBR=dimich@chromium.org
    Review URL: http://codereview.chromium.org/3130052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57573 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -252,7 +252,6 @@ extern const char kPreferencesWindowPlacement[];
 extern const char kMemoryCacheSize[];
 
 extern const char kBackgroundModeEnabled[];
-extern const char kLaunchOnStartupResetAllowed[];
 
 extern const char kDownloadDefaultDirectory[];
 extern const char kDownloadExtensionsToOpen[];
