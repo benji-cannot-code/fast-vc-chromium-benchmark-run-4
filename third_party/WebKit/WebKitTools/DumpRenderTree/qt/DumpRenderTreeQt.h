6ca8d3dac3176a3e7da1aca51ba7a89a0cc41307
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-26 10:17:56 +0000

    [Qt] DumpRenderTree runs only one test from command mode https://bugs.webkit.org/show_bug.cgi?id=44012
    Patch by Mahesh Kulkarni <mahesh.kulkarni@nokia.com> on 2010-08-26
    Reviewed by Kenneth Rohde Christiansen.
    
    Enables support to run multiple test files and single
    folder containing html files in stand alone mode
    
    * DumpRenderTree/qt/DumpRenderTreeQt.cpp:
    (WebCore::DumpRenderTree::DumpRenderTree):
    (WebCore::DumpRenderTree::processArgsLine):
    (WebCore::DumpRenderTree::loadNextTestInStandAloneMode):
    (WebCore::DumpRenderTree::processLine):
    (WebCore::DumpRenderTree::dump):
    * DumpRenderTree/qt/DumpRenderTreeQt.h:
    (WebCore::DumpRenderTree::setStandAloneMode):
    (WebCore::DumpRenderTree::isStandAloneMode):
    * DumpRenderTree/qt/main.cpp:
    (main):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66092 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/qt/DumpRenderTreeQt.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/qt/DumpRenderTreeQt.h
@@ -80,9 +80,6 @@ public:
     void setTextOutputEnabled(bool enable) { m_enableTextOutput = enable; }
     bool isTextOutputEnabled() { return m_enableTextOutput; }
 
-    void setSingleFileMode(bool flag) { m_singleFileMode = flag; }
-    bool isSingleFileMode() { return m_singleFileMode; }
-
     void setGraphicsBased(bool flag) { m_graphicsBased = flag; }
     bool isGraphicsBased() { return m_graphicsBased; }
 
@@ -107,6 +104,7 @@ public:
 #if defined(Q_WS_X11)
     static void initializeFonts();
 #endif
+    void processArgsLine(const QStringList&);
 
 public Q_SLOTS:
     void initJSObjects();
@@ -129,8 +127,12 @@ private Q_SLOTS:
     void showPage();
     void hidePage();
     void dryRunPrint(QWebFrame*);
+    void loadNextTestInStandAloneMode();
 
 private:
+    void setStandAloneMode(bool flag) { m_standAloneMode = flag; }
+    bool isStandAloneMode() { return m_standAloneMode; }
+
     QString dumpFramesAsText(QWebFrame* frame);
     QString dumpBackForwardList(QWebPage* page);
     QString dumpFrameScrollPosition(QWebFrame* frame);
@@ -138,6 +140,7 @@ private:
 
     bool m_dumpPixels;
     QString m_expectedHash;
+    QStringList m_standAloneModeTestList;
 
     WebPage *m_page;
     QWidget* m_mainView;
@@ -151,7 +154,7 @@ private:
 
     QList<QObject*> windows;
     bool m_enableTextOutput;
-    bool m_singleFileMode;
+    bool m_standAloneMode;
     bool m_graphicsBased;
     QString m_persistentStoragePath;
 };
