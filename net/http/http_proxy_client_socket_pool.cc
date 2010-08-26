f87488c112b15022ec512a0f98b5011896f4530c
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-26 21:40:14 +0000

    Ignore certificate errors when connecting to an HTTPS Proxy if LOAD_IGNORE_ALL_CERT_ERRORS is set.  This happens when the --ignore_certificate_errors command line flag is present.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3124044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57585 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_proxy_client_socket_pool.cc
+++ net/http/http_proxy_client_socket_pool.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time.h"
 #include "googleurl/src/gurl.h"
+#include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
 #include "net/http/http_network_session.h"
 #include "net/http/http_proxy_client_socket.h"
@@ -173,6 +174,9 @@ int HttpProxyConnectJob::DoSSLConnect() {
 }
 
 int HttpProxyConnectJob::DoSSLConnectComplete(int result) {
+  if (IsCertificateError(result) &&
+      params_->ssl_params()->load_flags() & LOAD_IGNORE_ALL_CERT_ERRORS)
+    result = OK;
   if (result < 0) {
     if (transport_socket_handle_->socket())
       transport_socket_handle_->socket()->Disconnect();
