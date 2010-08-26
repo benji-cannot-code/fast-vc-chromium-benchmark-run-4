b792fb418d8fb5245be568d911f65a8f87a2f202
dtseng@chromium.org <dtseng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-26 22:54:02 +0000

    Initial TTS implementation on Mac and Windows.BUG=none.TEST=base extension test.  Currently no mocking for the native TTS api's.  Manually tested engines with ChromeVox (navigating around a page, expected interrupts, stopping speech, etc.). Review URL: http://codereview.chromium.org/3149027
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57595 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/extensions/extension_tts_api_gtk.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extension_tts_api.h"
+
+bool ExtensionTtsSpeakFunction::RunImpl() {
+  return false;
+}
+
+bool ExtensionTtsStopSpeakingFunction::RunImpl() {
+  return false;
+}
+
+bool ExtensionTtsIsSpeakingFunction::RunImpl() {
+  return false;
+}
