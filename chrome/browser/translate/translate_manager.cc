0ed4563b37c30692405c6dfa3c3cfc78bfd697b3
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-27 00:50:45 +0000

    Add --disable-translate to help Selenium/WebDriver dudes make Chrome more predictable for automated testing.
    BUG=none
    TEST=Launch Chrome.  Go to http://www.diplomatie.gouv.fr/fr/
    See translate info bar?  Good.
    Now launch Chrome with --disable-translate and go to that page again.
    Make sure translate info bar never shows up.
    
    Review URL: http://codereview.chromium.org/3117044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57620 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_manager.cc
+++ chrome/browser/translate/translate_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/translate/translate_manager.h"
 
 #include "app/resource_bundle.h"
+#include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/histogram.h"
 #include "base/string_util.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/translate/page_translated_details.h"
 #include "chrome/browser/translate/translate_infobar_delegate.h"
 #include "chrome/browser/translate/translate_prefs.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/notification_details.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/notification_source.h"
@@ -337,6 +339,11 @@ void TranslateManager::InitiateTranslation(TabContents* tab,
                                            const std::string& page_lang) {
   PrefService* prefs = tab->profile()->GetPrefs();
   if (!prefs->GetBoolean(prefs::kEnableTranslate))
+    return;
+
+  // Allow disabling of translate from the command line to assist with
+  // automated browser testing.
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kDisableTranslate))
     return;
 
   NavigationEntry* entry = tab->controller().GetActiveEntry();
