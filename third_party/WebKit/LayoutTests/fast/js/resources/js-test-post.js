3a7b7b45ea5b0cfcd66d16a5b9c92940bdf86c51
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-27 11:15:02 +0000

    2010-08-27  Steve Block  <steveblock@google.com>
            Reviewed by Darin Adler.
    
            js-test-post.js is not robust for asynchronous tests
            https://bugs.webkit.org/show_bug.cgi?id=44642
    
            This patch moves finishJSTest() to js-test-pre.js to make it available for when an
            asynchronous test completes before js-test-post.js has been parsed, and updates
            js-test-post.js to handle this case.
    
            * fast/dom/Window/window-property-descriptors-expected.txt:
            * fast/js/resources/js-test-post.js:
            * fast/js/resources/js-test-pre.js:
            (finishJSTest):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66201 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/resources/js-test-post.js
+++ third_party/WebKit/LayoutTests/fast/js/resources/js-test-post.js
@@ -1,14 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function finishJSTest()
-{
-    shouldBeTrue("successfullyParsed");
-    debug('<br /><span class="pass">TEST COMPLETE</span>');
-    if (window.jsTestIsAsync && window.layoutTestController)
-        layoutTestController.notifyDone();
-}
+wasPostTestScriptParsed = true;
 
 if (window.jsTestIsAsync) {
     if (window.layoutTestController)
         layoutTestController.waitUntilDone();
+    if (window.wasFinishJSTestCalled)
+        finishJSTest();
 } else
     finishJSTest();
