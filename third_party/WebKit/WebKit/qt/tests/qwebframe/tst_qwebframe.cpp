9403ce8b6a62cb0165fcf4a27793796bb8639a80
benjamin.poulain@nokia.com <benjamin.poulain@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-27 18:40:39 +0000

    2010-08-27  Benjamin Poulain  <benjamin.poulain@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] tst_QWebFrame::inputFieldFocus() fails on MeeGo Handset with QtWebKit 2.1
            https://bugs.webkit.org/show_bug.cgi?id=44703
    
            Make the test more robust to changes in the execution environment.
            The position where the mouse events are sent is now computed from the element geometry,
            this make the test less dependent on the fonts.
    
            The test now also make sure the window manager mapped the window on screen
            before attempting to send the events.
    
            * tests/qwebframe/tst_qwebframe.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/tests/qwebframe/tst_qwebframe.cpp
+++ third_party/WebKit/WebKit/qt/tests/qwebframe/tst_qwebframe.cpp
@@ -2698,6 +2698,7 @@ void tst_QWebFrame::inputFieldFocus()
     view.setHtml("<html><body><input type=\"text\"></input></body></html>");
     view.resize(400, 100);
     view.show();
+    QTest::qWaitForWindowShown(&view);
     view.setFocus();
     QTRY_VERIFY(view.hasFocus());
 
@@ -2705,7 +2706,8 @@ void tst_QWebFrame::inputFieldFocus()
     int delay = qApp->cursorFlashTime() * 2;
 
     // focus the lineedit and check if it blinks
-    QTest::mouseClick(&view, Qt::LeftButton, 0, QPoint(25, 25));
+    const QWebElement inputElement = view.page()->mainFrame()->documentElement().findFirst(QLatin1String("input[type=text]"));
+    QTest::mouseClick(&view, Qt::LeftButton, 0, inputElement.geometry().center());
     m_inputFieldsTestView = &view;
     view.installEventFilter( this );
     QTest::qWait(delay);
