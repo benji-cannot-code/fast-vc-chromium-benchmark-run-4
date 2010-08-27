51ab6add62778c639e24e4921a06052db1d2290d
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-27 20:12:01 +0000

    Bug 44745 - Number.toFixed/toExponential/toPrecision are inaccurate.
    Reviewed by Oliver Hunt.
    
    These methods should be using a version of dtoa that can generate results accurate
    to the requested precision, whereas our version of dtoa is only currently able to
    support producing results sufficiently accurate to distinguish the value from any
    other IEEE-754 double precision number.
    
    JavaScriptCore: 
    
    This change has no impact on benchmarks we track.
    
    On microbenchmarks for these functions, this is a slight regression where a high
    precision is requested (dtoa now need to iterate further to generate a a greater
    number of digits), but with smaller precision values (hopefully more common) this
    improves performance, since it reduced the accurate of result dtoa is required,
    to produce, and removes the need to pre-round values before calling dtoa. 
    
    * JavaScriptCore.exp:
        doubleToStringInJavaScriptFormat renamed to numberToString
    
    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def:
        doubleToStringInJavaScriptFormat renamed to numberToString
    
    * runtime/UString.cpp:
    (JSC::UString::number):
        doubleToStringInJavaScriptFormat renamed to numberToString
    
    * wtf/DecimalNumber.h:
    (WTF::DecimalNumber::DecimalNumber):
    (WTF::DecimalNumber::toStringDecimal):
    (WTF::DecimalNumber::toStringExponential):
        Remove all pre-rounding of values, instead call dtoa correctly.
    
    * wtf/dtoa.cpp:
    (WTF::dtoa):
    * wtf/dtoa.h:
        Reenable support for rounding to specific-figures/decimal-places in dtoa.
        Modify to remove unbiased rounding, provide ECMA required away-from-zero.
        Rewrite doubleToStringInJavaScriptFormat to use DecimalNumber, rename to
        numberToString.
    
    WebCore: 
    
    * html/HTMLTreeBuilder.cpp:
    (WebCore::serializeForNumberType):
        doubleToStringInJavaScriptFormat renamed to numberToString.
    
    LayoutTests: 
    
    Updating expected results, corrected errors in fast/js/kde/script-tests/Number.js
    (corrected results confirmed with FireFox).
    
    * fast/js/kde/Number-expected.txt:
    * fast/js/kde/script-tests/Number.js:
    * fast/js/number-toExponential-expected.txt:
    * fast/js/number-tofixed-expected.txt:
    * fast/js/number-toprecision-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66245 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/runtime/UString.cpp
+++ third_party/WebKit/JavaScriptCore/runtime/UString.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "JSGlobalObjectFunctions.h"
 #include "Collector.h"
-#include "dtoa.h"
 #include "Identifier.h"
 #include "Operations.h"
 #include <ctype.h>
@@ -37,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdlib.h>
 #include <wtf/ASCIICType.h>
 #include <wtf/Assertions.h>
+#include <wtf/DecimalNumber.h>
 #include <wtf/MathExtras.h>
 #include <wtf/StringExtras.h>
 #include <wtf/Vector.h>
@@ -199,9 +199,8 @@ UString UString::number(long l)
 
 UString UString::number(double d)
 {
-    DtoaBuffer buffer;
-    unsigned length;
-    doubleToStringInJavaScriptFormat(d, buffer, &length);
+    NumberToStringBuffer buffer;
+    unsigned length = numberToString(d, buffer);
     return UString(buffer, length);
 }
 
