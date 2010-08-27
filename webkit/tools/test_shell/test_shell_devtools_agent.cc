428c07412bc45bf7a5b58247dd1a34c44827f8f4
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-27 18:36:42 +0000

    Misc. minor cleanup.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/3107041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57710 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_devtools_agent.cc
+++ webkit/tools/test_shell/test_shell_devtools_agent.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -52,14 +52,8 @@ void TestShellDevToolsAgent::DispatchMessageLoop() {
   current->SetNestableTasksAllowed(old_state);
 }
 
-// Warning at call_method_factory_(this) treated as error is switched off.
-
-#if defined(OS_WIN)
-#pragma warning(disable : 4355)
-#endif // defined(OS_WIN)
-
 TestShellDevToolsAgent::TestShellDevToolsAgent()
-    : call_method_factory_(this),
+    : ALLOW_THIS_IN_INITIALIZER_LIST(call_method_factory_(this)),
       dev_tools_client_(NULL) {
   static int dev_tools_agent_counter;
   routing_id_ = ++dev_tools_agent_counter;
