09583d29cef557c1759c1c607d7e8eb307fc8fb5
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-27 06:03:35 +0000

    Make the 'recently closed' section uncollapseable by adding a 'nocollapse' feature to the sections.
    screencap:
    http://aaronboodman.com/z_dropbox/cr3141046/ntp5.png
    
    BUG=53248
    
    Review URL: http://codereview.chromium.org/3141046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57640 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/ntp_resource_cache.cc
+++ chrome/browser/dom_ui/ntp_resource_cache.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/dom_ui/chrome_url_data_manager.h"
+#include "chrome/browser/dom_ui/shown_sections_handler.h"
 #include "chrome/browser/google_util.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profile.h"
@@ -310,8 +311,8 @@ void NTPResourceCache::CreateNewTabHTML() {
   localized_strings.SetString("has_3d", has_3d ? "true" : "false");
 
   // Pass the shown_sections pref early so that we can prevent flicker.
-  const int shown_sections = profile_->GetPrefs()->GetInteger(
-      prefs::kNTPShownSections);
+  const int shown_sections = ShownSectionsHandler::GetShownSections(
+      profile_->GetPrefs());
   localized_strings.SetInteger("shown_sections", shown_sections);
 
   base::StringPiece new_tab_html(ResourceBundle::GetSharedInstance().
