a3c082cf4caf9fc59ebacc94e5f9316b743a9c85
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-27 23:19:51 +0000

    Disable test that's been crashing.
    BUG=49522
    TEST=Disables AutocompleteTextFieldEditorTest.AddRemoveAutocompleteTextFieldEditorTest and AutocompleteTextFieldEditorTest.DisplayAutocompleteTextFieldEditorTest
    TBR=thakis@chromium.org
    
    Review URL: http://codereview.chromium.org/3229005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57751 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/location_bar/autocomplete_text_field_editor_unittest.mm
+++ chrome/browser/cocoa/location_bar/autocomplete_text_field_editor_unittest.mm
@@ -65,7 +65,12 @@ class AutocompleteTextFieldEditorTest : public CocoaTest {
   scoped_nsobject<AutocompleteTextFieldWindowTestDelegate> window_delegate_;
 };
 
+// Disabled because it crashes sometimes. http://crbug.com/49522
+// Can't rename DISABLED_ because the TEST_VIEW macro prepends.
+//     http://crbug.com/53621
+#if 0
 TEST_VIEW(AutocompleteTextFieldEditorTest, field_);
+#endif
 
 // Test that control characters are stripped from insertions.
 TEST_F(AutocompleteTextFieldEditorTest, InsertStripsControlChars) {
