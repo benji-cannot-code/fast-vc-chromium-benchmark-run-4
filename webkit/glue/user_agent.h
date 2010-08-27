d8561f72992870f837b866af8ea680e037721cf0
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-27 17:13:06 +0000

    Factoring BuildUserAgent out to a separate lib for GCF can use it.
    TEST=There should be no functional change.  If anything breaks it should be the build.
    BUG=50788
    
    Review URL: http://codereview.chromium.org/3214005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57692 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/glue/user_agent.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBKIT_GLUE_USER_AGENT_H_
+#define WEBKIT_GLUE_USER_AGENT_H_
+
+#include <string>
+
+#include "base/basictypes.h"
+
+namespace webkit_glue {
+
+// Construct the User-Agent header, filling in |result|.
+// The other parameters are workarounds for broken websites:
+// - If mimic_windows is true, produce a fake Windows Chrome string.
+void BuildUserAgent(bool mimic_windows, std::string* result);
+
+// Builds a User-agent compatible string that describes the OS and CPU type.
+std::string BuildOSCpuInfo();
+
+}  // namespace webkit_glue
+
+#endif  // WEBKIT_GLUE_USER_AGENT_H_
+
