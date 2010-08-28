34c0a887052aa9380e5df0c85d4149a88f002f72
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-28 06:13:30 +0000

    Disable background mode when associated pref changes.
    BUG=53173
    TEST=new BackgroundModeManager unit tests
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=57642
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=57749
    
    Review URL: http://codereview.chromium.org/3205008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57778 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profile_impl.cc
+++ chrome/browser/profile_impl.cc
@@ -348,7 +348,8 @@ ProfileImpl::ProfileImpl(const FilePath& path)
   // ChromeOS because Chrome is always running (no need for special keep-alive
   // or launch-on-startup support).
 #if !defined(OS_CHROMEOS)
-  background_mode_manager_.reset(new BackgroundModeManager(this));
+  background_mode_manager_.reset(new BackgroundModeManager(this,
+      CommandLine::ForCurrentProcess()));
 #endif
 
   background_contents_service_.reset(
