2971a32eb15c0638b9f27527ef5501f22034576b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-28 07:58:54 +0000

    2010-08-28  Pratik Solanki  <psolanki@apple.com>
            Reviewed by Dan Bernstein.
    
            Add an ENABLE define for purgeable memory support
            https://bugs.webkit.org/show_bug.cgi?id=44777
    
            * wtf/Platform.h:
    2010-08-28  Pratik Solanki  <psolanki@apple.com>
    
            Reviewed by Dan Bernstein.
    
            Add an ENABLE define for purgeable memory support
            https://bugs.webkit.org/show_bug.cgi?id=44777
    
            * platform/PurgeableBuffer.h:
            * platform/mac/PurgeableBufferMac.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66301 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/mac/PurgeableBufferMac.cpp
+++ third_party/WebKit/WebCore/platform/mac/PurgeableBufferMac.cpp
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
-#ifndef BUILDING_ON_TIGER
+#if ENABLE(PURGEABLE_MEMORY)
 
 #include "PurgeableBuffer.h"
 
