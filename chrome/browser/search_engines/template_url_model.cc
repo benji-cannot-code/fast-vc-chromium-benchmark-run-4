a8af8fe2b7adc587634c88f80162afa1c725993a
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-30 22:18:13 +0000

    Allow one to call TemplateURLModel::GenerateSearchURL when not on the UI thread.
    BUG=38475
    TEST=unit_test --gtest_filter=Temp*
    
    Review URL: http://codereview.chromium.org/3240005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57918 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_model.cc
+++ chrome/browser/search_engines/template_url_model.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 #include "chrome/browser/rlz/rlz.h"
 #include "chrome/browser/search_engines/search_host_to_urls_map.h"
+#include "chrome/browser/search_engines/search_terms_data.h"
 #include "chrome/browser/search_engines/template_url_prepopulate_data.h"
 #include "chrome/browser/search_engines/util.h"
 #include "chrome/common/chrome_switches.h"
@@ -148,6 +149,15 @@ std::wstring TemplateURLModel::CleanUserInputKeyword(
 
 // static
 GURL TemplateURLModel::GenerateSearchURL(const TemplateURL* t_url) {
+  DCHECK(t_url);
+  UIThreadSearchTermsData search_terms_data;
+  return GenerateSearchURLWithTermsData(t_url, search_terms_data);
+}
+
+// static
+GURL TemplateURLModel::GenerateSearchURLWithTermsData(
+    const TemplateURL* t_url,
+    const SearchTermsData& search_terms_data) {
   DCHECK(t_url);
   const TemplateURLRef* search_ref = t_url->url();
   // Extension keywords don't have host-based search URLs.
