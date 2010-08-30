25996622d8444656e6ab2d9da55edbb56f65a7f9
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-30 18:09:18 +0000

    Disabling the OpenTab, OpenPanel  and PanelBeforeTab on Windows as well as they randomly fail.
    Bug=43061
    TBR=skerner
    
    Review URL: http://codereview.chromium.org/3252005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57883 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_browsertest.cc
+++ chrome/browser/browser_browsertest.cc
@@ -658,7 +658,7 @@ class BrowserAppRefocusTest : public ExtensionBrowserTest {
   Profile* profile_;
 };
 
-#if defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
+#if (defined(OS_LINUX) && defined(TOOLKIT_VIEWS)) || defined(OS_WIN)
 
 // Tests fail on Chrome OS:  http://crbug.com/43061
 #define MAYBE_OpenTab DISABLED_OpenTab
