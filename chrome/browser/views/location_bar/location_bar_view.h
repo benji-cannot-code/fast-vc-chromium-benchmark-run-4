d726c6f79cd844312093228ff1be425b1d387015
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-30 16:10:31 +0000

    Removes unused field in LocationBarView.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3245004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57863 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/location_bar/location_bar_view.h
+++ chrome/browser/views/location_bar/location_bar_view.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/gtk/accessible_widget_helper_gtk.h"
 #endif
 
-class Browser;
 class CommandUpdater;
 class ContentSettingImageView;
 class EVBubbleView;
@@ -285,9 +284,6 @@ class LocationBarView : public LocationBar,
 
   // Helper to show the first run info bubble.
   void ShowFirstRunBubbleInternal(FirstRun::BubbleType bubble_type);
-
-  // Current browser. Not owned by us.
-  Browser* browser_;
 
   // Current profile. Not owned by us.
   Profile* profile_;
