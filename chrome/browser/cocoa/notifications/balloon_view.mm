9c0931035ae0a9477070dbf7459121fb04000325
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-30 21:13:08 +0000

    Mac notification UI improvements: reduce the roundedness of the corners from 8 to 6px, and remove the visible bounding box from the pressed state of the wrench icon.
    BUG=52545
    TEST=notifications on mac
    
    Review URL: http://codereview.chromium.org/3270007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57908 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/notifications/balloon_view.mm
+++ chrome/browser/cocoa/notifications/balloon_view.mm
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kRoundedCornerSize = 8;
+const int kRoundedCornerSize = 6;
 
 }  // namespace
 
