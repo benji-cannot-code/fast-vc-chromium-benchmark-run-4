df3b9517e83e658453e8457563683063cbcc1734
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-31 19:23:14 +0000

    Use named events instead of lock files for service process
    Use named events to signal that a service process is running.
    
    BUG=52891
    TEST=browser_tests --gtest_filter=ServiceProcess*
    
    Review URL: http://codereview.chromium.org/3268003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58041 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/service_process.cc
+++ chrome/service/service_process.cc
@@ -73,7 +73,7 @@ bool ServiceProcess::Initialize(MessageLoop* message_loop) {
 
   // Check if remoting host is already enabled.
   if (values->GetBoolean(prefs::kRemotingHostEnabled, &remoting_host_enabled) &&
-    remoting_host_enabled) {
+      remoting_host_enabled) {
     // If true then we start the host.
     StartChromotingHost();
   }
@@ -86,11 +86,10 @@ bool ServiceProcess::Initialize(MessageLoop* message_loop) {
       GetServiceProcessChannelName(kServiceProcessCloudPrint)));
   ipc_server_->Init();
 
-  // After the IPC server has started we can create the lock file to indicate
-  // that we have started.
-  bool ret = CreateServiceProcessLockFile(kServiceProcessCloudPrint);
-  DCHECK(ret) << "Failed to create service process lock file.";
-  return ret;
+  // After the IPC server has started we signal that the service process is
+  // running.
+  SignalServiceProcessRunning(kServiceProcessCloudPrint);
+  return true;
 }
 
 bool ServiceProcess::Teardown() {
@@ -115,9 +114,8 @@ bool ServiceProcess::Teardown() {
   network_change_notifier_.reset();
 
   // Delete the service process lock file when it shuts down.
-  bool ret = DeleteServiceProcessLockFile(kServiceProcessCloudPrint);
-  DCHECK(ret) << "Failed to delete service process lock file.";
-  return ret;
+  SignalServiceProcessStopped(kServiceProcessCloudPrint);
+  return true;
 }
 
 void ServiceProcess::Shutdown() {
