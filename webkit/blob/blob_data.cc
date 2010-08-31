d3b11c869537b99b73888d5a554a7dd17882ec81
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-31 23:05:58 +0000

    Remove the workaround that is not needed after the fix for Time::FromDoubleT is landed.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3270016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58093 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/blob/blob_data.cc
+++ webkit/blob/blob_data.cc
@@ -16,16 +16,6 @@ using WebKit::WebBlobData;
 using WebKit::WebData;
 using WebKit::WebString;
 
-namespace {
-
-// Time::FromDoubleT() does not return empty Time object when dt is 0.
-// We have to work around this problem here.
-base::Time DoubleTToTime(double dt) {
-  return dt ? base::Time::FromDoubleT(dt) : base::Time();
-}
-
-}
-
 namespace webkit_blob {
 
 BlobData::BlobData(const WebBlobData& data) {
@@ -45,7 +35,7 @@ BlobData::BlobData(const WebBlobData& data) {
             webkit_glue::WebStringToFilePath(item.filePath),
             static_cast<uint64>(item.offset),
             static_cast<uint64>(item.length),
-            DoubleTToTime(item.expectedModificationTime));
+            base::Time::FromDoubleT(item.expectedModificationTime));
         break;
       case WebBlobData::Item::TypeBlob:
         if (item.length) {
