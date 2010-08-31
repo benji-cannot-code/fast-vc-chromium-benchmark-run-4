b421be4f3b59f01a54bc4ca22a4a4e6918aa781b
antonm@chromium.org <antonm@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-31 19:35:21 +0000

    2010-08-31  Anton Muhin  <antonm@chromium.org>
            Reviewed by Adam Barth.
    
            [v8] More correct and faster error handling when converting v8 objects to various WebCore strings
            https://bugs.webkit.org/show_bug.cgi?id=44678
    
            Split v8 object conversion into two phase: 1st, which can throw an exception, and
            2nd, which must always succeed.  That allows to report correctly the case when exception
            happens.
    
            * bindings/scripts/CodeGeneratorV8.pm:
            * bindings/v8/V8Binding.cpp:
            (WebCore::int32ToWebCoreString):
            (WebCore::v8NonStringValueToWebCoreString):
            * bindings/v8/V8Binding.h:
            (WebCore::V8ParameterBase::operator String):
            (WebCore::V8ParameterBase::operator AtomicString):
            (WebCore::V8ParameterBase::V8ParameterBase):
            (WebCore::V8ParameterBase::prepareBase):
            (WebCore::V8ParameterBase::object):
            (WebCore::V8ParameterBase::setString):
            (WebCore::V8ParameterBase::toString):
            (WebCore::):
            (WebCore::::prepare):
            * bindings/v8/custom/V8BindingMacros.h:
            * bindings/v8/custom/V8DeviceMotionEventCustom.cpp:
            (WebCore::V8DeviceMotionEvent::initDeviceMotionEventCallback):
            * bindings/v8/cstom/V8DeviceOrientationEventCustom.cpp:
            (WebCore::V8DeviceMotionEvent::initDeviceOrientationEventCallback):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66521 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -763,8 +763,11 @@ END
         }
 
         if ($useExceptions) {
-            push(@implContentDecls, "    $nativeType v = ");
-            push(@implContentDecls, "$getterString;\n");
+            if ($nativeType =~ /^V8Parameter/) {
+              push(@implContentDecls, "    " . ConvertToV8Parameter($attribute->signature, $nativeType, "v", $getterString) . ";\n");
+            } else {
+              push(@implContentDecls, "    $nativeType v = $getterString;\n");
+            }
             push(@implContentDecls, GenerateSetDOMException("    "));
             $result = "v";
             $result .= ".release()" if (IsRefPtrType($returnType));
@@ -831,6 +834,8 @@ sub GenerateNormalAttrSetter
     my $implClassName = shift;
     my $interfaceName = shift;
 
+    $implIncludes{"V8BindingMacros.h"} = 1;
+
     my $attrExt = $attribute->signature->extendedAttributes;
 
     my $conditionalString = GenerateConditionalString($attribute->signature);
@@ -891,7 +896,12 @@ END
             push(@implContentDecls, "        return;\n");
         }
     } else {
-        push(@implContentDecls, "    $nativeType v = " . JSValueToNative($attribute->signature, "value") . ";\n");
+        my $value = JSValueToNative($attribute->signature, "value");
+        if ($nativeType =~ /^V8Parameter/) {
+          push(@implContentDecls, "    " . ConvertToV8Parameter($attribute->signature, $nativeType, "v", $value, "VOID") . "\n");
+        } else {
+          push(@implContentDecls, "    $nativeType v = $value;\n");
+        }
     }
 
     my $result = "v";
@@ -1237,13 +1247,7 @@ END
             push(@implContentDecls, "    }\n");
         } elsif ($nativeType =~ /^V8Parameter/) {
             my $value = JSValueToNative($parameter, "args[$paramIndex]", BasicTypeCanFailConversion($parameter) ?  "${parameterName}Ok" : undef);
-            if ($parameter->type eq "DOMString") {
-                $implIncludes{"V8BindingMacros.h"} = 1;
-                push(@implContentDecls, "    STRING_TO_V8PARAMETER_EXCEPTION_BLOCK($nativeType, $parameterName, $value);\n");
-            } else {
-                # Don't know how to properly check for conversion exceptions when $parameter->type is "DOMUserData"
-                push(@implContentDecls, "    $nativeType $parameterName = $value;\n");
-            }
+            push(@implContentDecls, "    " . ConvertToV8Parameter($parameter, $nativeType, $parameterName, $value) . "\n");
         } else {
             $implIncludes{"V8BindingMacros.h"} = 1;
             # For functions with "StrictTypeChecking", if an input parameter's type does not match the signature,
@@ -3315,6 +3319,25 @@ sub GetCallbackClassName
 
     return "V8CustomVoidCallback" if $interfaceName eq "VoidCallback";
     return "V8$interfaceName";
+}
+
+sub ConvertToV8Parameter
+{
+    my $signature = shift;
+    my $nativeType = shift;
+    my $variableName = shift;
+    my $value = shift;
+    my $suffix = shift;
+
+    die "Wrong native type passed: $nativeType" unless $nativeType =~ /^V8Parameter/;
+    if ($signature->type eq "DOMString") {
+      my $macro = "STRING_TO_V8PARAMETER_EXCEPTION_BLOCK";
+      $macro .= "_$suffix" if $suffix;
+      return "$macro($nativeType, $variableName, $value);"
+    } else {
+      # Don't know how to properly check for conversion exceptions when $parameter->type is "DOMUserData"
+      return "$nativeType $variableName($value, true);";
+    }
 }
 
 sub DebugPrint
--- third_party/WebKit/WebCore/bindings/v8/custom/V8DeviceMotionEventCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8DeviceMotionEventCustom.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "DeviceMotionData.h"
 #include "V8Binding.h"
+#include "V8BindingMacros.h"
 #include "V8Proxy.h"
 
 #include <v8.h>
@@ -111,7 +112,7 @@ v8::Handle<v8::Value> V8DeviceMotionEvent::intervalAccessorGetter(v8::Local<v8::
 v8::Handle<v8::Value> V8DeviceMotionEvent::initDeviceMotionEventCallback(const v8::Arguments& args)
 {
     DeviceMotionEvent* imp = V8DeviceMotionEvent::toNative(args.Holder());
-    V8Parameter<> type = args[0];
+    STRING_TO_V8PARAMETER_EXCEPTION_BLOCK(V8Parameter<>, type, args[0]);
     bool bubbles = args[1]->BooleanValue();
     bool cancelable = args[2]->BooleanValue();
     // If any of the parameters are null or undefined, mark them as not provided.
