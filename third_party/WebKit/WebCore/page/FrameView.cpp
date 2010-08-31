41965ca165bd0e1910628b0d3aa820e9a4a0d2b2
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-31 23:49:08 +0000

    Variable name change. m_shouldFirePostLayoutTimer is now m_hasPendingPostLayoutTasks
    Rubber-stamped by Dan Bernstein..
    
    * page/FrameView.cpp:
    (WebCore::FrameView::FrameView):
    (WebCore::FrameView::~FrameView):
    (WebCore::FrameView::reset):
    (WebCore::FrameView::layout):
    (WebCore::FrameView::performPostLayoutTasks):
    * page/FrameView.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66555 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -129,7 +129,7 @@ FrameView::FrameView(Frame* frame)
     , m_fixedObjectCount(0)
     , m_layoutTimer(this, &FrameView::layoutTimerFired)
     , m_layoutRoot(0)
-    , m_shouldFirePostLayoutTimer(false)
+    , m_hasPendingPostLayoutTasks(false)
     , m_inSynchronousPostLayout(false)
     , m_postLayoutTasksTimer(this, &FrameView::postLayoutTimerFired)
     , m_isTransparent(false)
@@ -167,7 +167,7 @@ PassRefPtr<FrameView> FrameView::create(Frame* frame, const IntSize& initialSize
 
 FrameView::~FrameView()
 {
-    if (m_shouldFirePostLayoutTimer) {
+    if (m_hasPendingPostLayoutTasks) {
         m_postLayoutTasksTimer.stop();
         m_scheduledEvents.clear();
         m_enqueueEvents = 0;
@@ -208,7 +208,7 @@ void FrameView::reset()
     m_layoutSchedulingEnabled = true;
     m_inLayout = false;
     m_inSynchronousPostLayout = false;
-    m_shouldFirePostLayoutTimer = false;
+    m_hasPendingPostLayoutTasks = false;
     m_layoutCount = 0;
     m_nestedLayoutCount = 0;
     m_postLayoutTasksTimer.stop();
@@ -643,7 +643,7 @@ void FrameView::layout(bool allowSubtree)
 
     m_layoutSchedulingEnabled = false;
 
-    if (!m_nestedLayoutCount && !m_inSynchronousPostLayout && m_shouldFirePostLayoutTimer) {
+    if (!m_nestedLayoutCount && !m_inSynchronousPostLayout && m_hasPendingPostLayoutTasks) {
         // This is a new top-level layout. If there are any remaining tasks from the previous
         // layout, finish them now.
         m_inSynchronousPostLayout = true;
@@ -828,7 +828,7 @@ void FrameView::layout(bool allowSubtree)
         updateOverflowStatus(layoutWidth() < contentsWidth(),
                              layoutHeight() < contentsHeight());
 
-    if (!m_shouldFirePostLayoutTimer) {
+    if (!m_hasPendingPostLayoutTasks) {
         if (!m_inSynchronousPostLayout) {
             m_inSynchronousPostLayout = true;
             // Calls resumeScheduledEvents()
@@ -836,12 +836,12 @@ void FrameView::layout(bool allowSubtree)
             m_inSynchronousPostLayout = false;
         }
 
-        if (!m_shouldFirePostLayoutTimer && (needsLayout() || m_inSynchronousPostLayout)) {
+        if (!m_hasPendingPostLayoutTasks && (needsLayout() || m_inSynchronousPostLayout)) {
             // If we need layout or are already in a synchronous call to postLayoutTasks(), 
             // defer widget updates and event dispatch until after we return. postLayoutTasks()
             // can make us need to update again, and we can get stuck in a nasty cycle unless
             // we call it through the timer here.
-            m_shouldFirePostLayoutTimer = true;
+            m_hasPendingPostLayoutTasks = true;
             m_postLayoutTasksTimer.startOneShot(0);
             if (needsLayout()) {
                 pauseScheduledEvents();
@@ -1612,7 +1612,7 @@ bool FrameView::updateWidgets()
     
 void FrameView::performPostLayoutTasks()
 {
-    m_shouldFirePostLayoutTimer = false;
+    m_hasPendingPostLayoutTasks = false;
 
     if (m_firstLayoutCallbackPending) {
         m_firstLayoutCallbackPending = false;
