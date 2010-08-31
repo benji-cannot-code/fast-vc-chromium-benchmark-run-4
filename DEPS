2245d751dc52b206a2e5d266c50fc30e80577d2f
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-31 01:16:35 +0000

    Revert 57921 - Pull seccomp-sandbox in via DEPS rather than using an in-tree copy
    This means changes to the sandbox won't have to be committed twice, to
    both trees.
    
    BUG=none
    TEST=smoke test of running chromium with --enable-seccomp-sandbox
    
    Review URL: http://codereview.chromium.org/3249003
    
    TBR=mseaborn@chromium.org
    Review URL: http://codereview.chromium.org/3245011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57933 0039d316-1c4b-4281-b951-d872f2087c98

--- DEPS
+++ DEPS
@@ -22,9 +22,6 @@ deps = {
   "src/googleurl":
     "http://google-url.googlecode.com/svn/trunk@145",
 
-  "src/sandbox/linux/seccomp":
-    "http://seccompsandbox.googlecode.com/svn/trunk@91",
-
   "src/sdch/open-vcdiff":
     "http://open-vcdiff.googlecode.com/svn/trunk@28",
 
---
+++ sandbox/linux/seccomp/getpid.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "debug.h"
+#include "sandbox_impl.h"
+
+namespace playground {
+
+long Sandbox::sandbox_getpid() {
+  long long tm;
+  Debug::syscall(&tm, __NR_getpid, "Executing handler");
+  Debug::elapsed(tm, __NR_getpid);
+  return pid_;
+}
+
+} // namespace
