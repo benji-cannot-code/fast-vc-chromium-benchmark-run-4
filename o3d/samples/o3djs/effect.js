279a59fc80ae28e77c6e2368b306d5aa195a6457
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-31 21:39:18 +0000

    Fixes the build for o3d.
    TEST=it builds
    BUG=none
    
    Review URL: http://codereview.chromium.org/3217016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58073 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/samples/o3djs/effect.js
+++ o3d/samples/o3djs/effect.js
@@ -395,6 +395,7 @@ o3djs.effect.setLanguage = function(language) {
  * 'glsl' or 'o3d'.
  */
 o3djs.effect.getLanguage = function() {
+  var language_namespace = o3djs.effect.o3d;
   if (language_namespace == o3djs.effect.glsl) {
     return 'glsl';
   }
