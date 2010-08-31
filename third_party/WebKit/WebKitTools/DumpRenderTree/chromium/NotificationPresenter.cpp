c970d0131e15071a0883ef73654e0282337ae457
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-31 00:19:17 +0000

    2010-08-30  Kent Tamura  <tkent@chromium.org>
            Reviewed by Adam Barth.
    
            Fix fast/notifications/notifications-display-close-events.html failure
            https://bugs.webkit.org/show_bug.cgi?id=44585
    
            * platform/chromium/drt_expectations.txt:
    2010-08-30  Kent Tamura  <tkent@chromium.org>
    
            Reviewed by Adam Barth.
    
            Fix fast/notifications/notifications-display-close-events.html failure
            https://bugs.webkit.org/show_bug.cgi?id=44585
    
            Apply http://crrev.com/48893. We should dispatch display events
            asynchronously.
    
            * DumpRenderTree/chromium/NotificationPresenter.cpp:
            (deferredDisplayDispatch):
            (NotificationPresenter::show):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66430 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/NotificationPresenter.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/NotificationPresenter.cpp
@@ -32,17 +32,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "NotificationPresenter.h"
 
-#include <wtf/text/CString.h>
-#include <wtf/text/WTFString.h>
-
+#include "base/task.h" // FIXME: Remove this
 #include "googleurl/src/gurl.h"
 #include "public/WebNotification.h"
 #include "public/WebNotificationPermissionCallback.h"
 #include "public/WebSecurityOrigin.h"
 #include "public/WebString.h"
 #include "public/WebURL.h"
+#include "webkit/support/webkit_support.h"
+#include <wtf/text/CString.h>
+#include <wtf/text/WTFString.h>
 
 using namespace WebKit;
+
+static void deferredDisplayDispatch(WebNotification notification)
+{
+    notification.dispatchDisplayEvent();
+}
 
 void NotificationPresenter::grantPermission(const WebString& origin)
 {
@@ -80,7 +86,7 @@ bool NotificationPresenter::show(const WebNotification& notification)
     }
 
     WebNotification eventTarget(notification);
-    eventTarget.dispatchDisplayEvent();
+    webkit_support::PostTaskFromHere(NewRunnableFunction(&deferredDisplayDispatch, eventTarget));
     return true;
 }
 
