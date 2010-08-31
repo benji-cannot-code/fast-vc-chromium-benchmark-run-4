4098b64f433c0fd3df64de7a3db1fb9c06710030
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-31 21:39:26 +0000

    2010-08-31  Darin Adler  <darin@apple.com>
            Reviewed by Anders Carlsson.
    
            Web archives are created with a size of 0 due to PassRefPtr mistake
            https://bugs.webkit.org/show_bug.cgi?id=44984
            rdar://problem/8042283
    
            * loader/archive/ArchiveResource.cpp:
            (WebCore::ArchiveResource::ArchiveResource): Merge the three constructors into one.
            (WebCore::ArchiveResource::create): Merge the two create functions into one, and
            fix the logic so there is no use of a PassRefPtr after it has been passed.
            * loader/archive/ArchiveResource.h: Use default arguments instead of overloading
            to cut down from three create functions to two. Remove two of the constructors.
    2010-08-31  Darin Adler  <darin@apple.com>
    
            Reviewed by Anders Carlsson.
    
            Web archives are created with a size of 0 due to PassRefPtr mistake
            https://bugs.webkit.org/show_bug.cgi?id=44984
            rdar://problem/8042283
    
            * webarchive/test-link-rel-icon-expected.webarchive: Expect a correct size, not 0.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66539 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/archive/ArchiveResource.cpp
+++ third_party/WebKit/WebCore/loader/archive/ArchiveResource.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2008, 2010 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,45 +34,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+inline ArchiveResource::ArchiveResource(PassRefPtr<SharedBuffer> data, const KURL& url, const String& mimeType, const String& textEncoding, const String& frameName, const ResourceResponse& response)
+    : SubstituteResource(url, response, data)
+    , m_mimeType(mimeType)
+    , m_textEncoding(textEncoding)
+    , m_frameName(frameName)
+    , m_shouldIgnoreWhenUnarchiving(false)
+{
+}
+
+PassRefPtr<ArchiveResource> ArchiveResource::create(PassRefPtr<SharedBuffer> data, const KURL& url, const String& mimeType, const String& textEncoding, const String& frameName, const ResourceResponse& response)
+{
+    if (!data)
+        return 0;
+    if (response.isNull()) {
+        unsigned dataSize = data->size();
+        return adoptRef(new ArchiveResource(data, url, mimeType, textEncoding, frameName,
+            ResourceResponse(url, mimeType, dataSize, textEncoding, String())));
+    }
+    return adoptRef(new ArchiveResource(data, url, mimeType, textEncoding, frameName, response));
+}
+
 PassRefPtr<ArchiveResource> ArchiveResource::create(PassRefPtr<SharedBuffer> data, const KURL& url, const ResourceResponse& response)
 {
-    return data ? adoptRef(new ArchiveResource(data, url, response)) : 0;
-}
-
-PassRefPtr<ArchiveResource> ArchiveResource::create(PassRefPtr<SharedBuffer> data, const KURL& url, const String& mimeType, const String& textEncoding, const String& frameName)
-{
-    return data ? adoptRef(new ArchiveResource(data, url, mimeType, textEncoding, frameName)) : 0;
-}
-
-PassRefPtr<ArchiveResource> ArchiveResource::create(PassRefPtr<SharedBuffer> data, const KURL& url, const String& mimeType, const String& textEncoding, const String& frameName, const ResourceResponse& resourceResponse)
-{
-    return data ? adoptRef(new ArchiveResource(data, url, mimeType, textEncoding, frameName, resourceResponse)) : 0;
-}
-
-ArchiveResource::ArchiveResource(PassRefPtr<SharedBuffer> data, const KURL& url, const ResourceResponse& response)
-    : SubstituteResource(url, response, data)
-    , m_mimeType(response.mimeType())
-    , m_textEncoding(response.textEncodingName())
-    , m_shouldIgnoreWhenUnarchiving(false)
-{
-}
-
-ArchiveResource::ArchiveResource(PassRefPtr<SharedBuffer> data, const KURL& url, const String& mimeType, const String& textEncoding, const String& frameName)
-    : SubstituteResource(url, ResourceResponse(url, mimeType, data ? data->size() : 0, textEncoding, String()), data)
-    , m_mimeType(mimeType)
-    , m_textEncoding(textEncoding)
-    , m_frameName(frameName)
-    , m_shouldIgnoreWhenUnarchiving(false)
-{
-}
-
-ArchiveResource::ArchiveResource(PassRefPtr<SharedBuffer> data, const KURL& url, const String& mimeType, const String& textEncoding, const String& frameName, const ResourceResponse& response)
-    : SubstituteResource(url, response.isNull() ? ResourceResponse(url, mimeType, data ? data->size() : 0, textEncoding, String()) : response, data)
-    , m_mimeType(mimeType)
-    , m_textEncoding(textEncoding)
-    , m_frameName(frameName)
-    , m_shouldIgnoreWhenUnarchiving(false)
-{
+    return create(data, url, response.mimeType(), response.textEncodingName(), String(), response);
 }
 
 }
