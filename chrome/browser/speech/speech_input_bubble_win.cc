5c1c234d66620ec438834f5ef1a4d6124fceca9c
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-31 23:46:21 +0000

    Reverting as this breaks Chromium OS. Please fix and reland.
    Revert 58095 - Implement the speech input bubble on linux/gtk.
    
    This is a popup which gets displayed below an <input> element when the user starts speech recognition on it. It displays a recognition status icon, an instruction label ('Speak now') at the top and a Cancel button at the bottom. The speech input module gets callbacks on user actions such as clicking Cancel or clicking any other window, and on both cases recognition stops and the bubble closes.
    
    As part of this CL I had to move a constant from the windows code to the class so it can be used in gtk and mac.
    
    TEST=manual, the code to invoke the bubble depends on another pending CL.
    BUG=53598
    
    Review URL: http://codereview.chromium.org/3231009
    
    TBR=satish@chromium.org
    Review URL: http://codereview.chromium.org/3263010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58100 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/speech_input_bubble_win.cc
+++ chrome/browser/speech/speech_input_bubble_win.cc
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// The speech bubble's arrow is so many pixels from the left of html element.
+const int kBubbleTargetOffsetX = 5;
 const int kBubbleHorizMargin = 40;
 const int kBubbleVertMargin = 0;
 
