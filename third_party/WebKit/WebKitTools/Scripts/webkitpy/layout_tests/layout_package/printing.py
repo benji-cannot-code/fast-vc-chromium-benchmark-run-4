494ae0124e6bd54bb05b8dffd418a2a01dace8c0
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-31 21:53:52 +0000

    2010-08-31  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by Ojan Vafai.
    
            new-run-webkit-tests: add more unit tests
    
            Add more unit tests for new-run-webkit-tests; we now cover all but
            the most obscure code paths in the generic code. We still need to
            add coverage for the http server and web socket paths, and add better
            coverage of the platform-specific logic. Note that the rebaselining
            tool is still not well tested.
    
            Also clean up some of the configuration logic for the printing
            module and the way it interacts with the Python logging module; that
            is a crufty interface, to be certain.
    
            https://bugs.webkit.org/show_bug.cgi?id=44902
    
            * Scripts/webkitpy/layout_tests/data/failures/expected/exception.html: Added.
            * Scripts/webkitpy/layout_tests/data/failures/expected/keyboard.html: Added.
            * Scripts/webkitpy/layout_tests/data/passes/error-expected.txt: Added.
            * Scripts/webkitpy/layout_tests/data/passes/error.html: Added.
            * Scripts/webkitpy/layout_tests/data/platform/test/test_expectations.txt:
            * Scripts/webkitpy/layout_tests/layout_package/dump_render_tree_thread.py:
            * Scripts/webkitpy/layout_tests/layout_package/printing.py:
            * Scripts/webkitpy/layout_tests/layout_package/printing_unittest.py:
            * Scripts/webkitpy/layout_tests/port/base.py:
            * Scripts/webkitpy/layout_tests/port/test.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66542 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/layout_package/printing.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/layout_package/printing.py
@@ -127,8 +127,8 @@ def print_options():
     ]
 
 
-def configure_logging(options, meter):
-    """Configures the logging system."""
+def _configure_logging(options, meter):
+    """Configures the logging system. Return the previous handler, if any."""
     log_fmt = '%(message)s'
     log_datefmt = '%y%m%d %H:%M:%S'
     log_level = logging.INFO
@@ -137,9 +137,23 @@ def configure_logging(options, meter):
                    '%(message)s')
         log_level = logging.DEBUG
 
-    logging.basicConfig(level=log_level, format=log_fmt,
-                        datefmt=log_datefmt, stream=meter)
+    root = logging.getLogger()
+    handler = logging.StreamHandler(meter)
+    handler.setFormatter(logging.Formatter(log_fmt, None))
+    if not root.handlers:
+        old_handler = None
+        root.addHandler(handler)
+    else:
+        old_handler = root.handlers[0]
+        root.handlers[0] = handler
+    root.setLevel(log_level)
+    return old_handler
 
+
+def _restore_logging(handler):
+    root = logging.getLogger()
+    if root and root.handlers[0]:
+        root.handlers[0] = handler
 
 def parse_print_options(print_options, verbose, child_processes,
                         is_fully_parallel):
@@ -238,10 +252,19 @@ class Printer(object):
 
         self._meter = metered_stream.MeteredStream(options.verbose,
                                                    regular_output)
-        configure_logging(self._options, self._meter)
+        self._old_handler = _configure_logging(self._options, self._meter)
 
         self.switches = parse_print_options(options.print_options,
             options.verbose, child_processes, is_fully_parallel)
+
+    def cleanup(self):
+        """Restore logging configuration to its initial settings."""
+        _restore_logging(self._old_handler)
+        self._old_handler = None
+
+    def __del__(self):
+        if self._old_handler:
+            _restore_logging(self._old_handler)
 
     # These two routines just hide the implmentation of the switches.
     def disabled(self, option):
