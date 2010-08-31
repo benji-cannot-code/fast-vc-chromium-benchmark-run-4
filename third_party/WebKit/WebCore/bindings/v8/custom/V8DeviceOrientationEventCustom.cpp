e737cb565ba95f5c2cc904a2952ffc25cd49e460
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-31 21:15:22 +0000

    2010-08-31  Tony Chang  <tony@chromium.org>
            Unreviewed, fix chromium compile.
    
            * bindings/v8/custom/V8DeviceOrientationEventCustom.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66530 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8DeviceOrientationEventCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8DeviceOrientationEventCustom.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "DeviceOrientation.h"
 #include "V8Binding.h"
+#include "V8BindingMacros.h"
 #include "V8Proxy.h"
 
 #include <v8.h>
