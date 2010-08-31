478c43ac1f2313f8d84e4f4a12b8a99165eb59c5
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-08-31 21:31:27 +0000

    * WebInspector/WebInspectorFrontend.mm: (-[WebInspectorFrontend initWithFrontendClient:]): Remove a stray semicolon.
    Reviewed by Anders Carlsson.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66535 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebInspector/WebInspectorFrontend.mm
+++ third_party/WebKit/WebKit/mac/WebInspector/WebInspectorFrontend.mm
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation WebInspectorFrontend
 
-- (id)initWithFrontendClient:(WebInspectorFrontendClient *)frontendClient;
+- (id)initWithFrontendClient:(WebInspectorFrontendClient *)frontendClient
 {
     if (!(self = [super init]))
         return nil;
