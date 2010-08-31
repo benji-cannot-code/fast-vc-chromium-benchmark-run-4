f3edf8ad2e939bdc51bf9896fed790f178a4a777
neb@chromium.org <neb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-31 17:37:35 +0000

    Guarantee PPAPI Audio playbacks are stopped in Pause().
    BUG=none
    TEST=pepper audio demo is playing audio
    
    Review URL: http://codereview.chromium.org/3142005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58014 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/pepper_audio.cc
+++ webkit/glue/plugins/pepper_audio.cc
@@ -114,6 +114,7 @@ AudioConfig* AudioConfig::AsAudioConfig() {
 
 Audio::Audio(PluginModule* module)
     : Resource(module),
+      playing_(false),
       socket_(NULL),
       shared_memory_(NULL),
       shared_memory_size_(0),
@@ -161,6 +162,35 @@ bool Audio::Init(PluginDelegate* plugin_delegate, PP_Resource config_id,
   return audio_.get() != NULL;
 }
 
+bool Audio::StartPlayback() {
+  if (playing_)
+    return true;
+
+  CHECK(!audio_thread_.get());
+  if (callback_ && socket_.get()) {
+    audio_thread_.reset(new base::DelegateSimpleThread(this,
+                                                       "plugin_audio_thread"));
+    audio_thread_->Start();
+  }
+  playing_ = true;
+  return audio_->StartPlayback();
+}
+
+bool Audio::StopPlayback() {
+  if (!playing_)
+    return true;
+
+  if (!audio_->StopPlayback())
+    return false;
+
+  if (audio_thread_.get()) {
+    audio_thread_->Join();
+    audio_thread_.reset();
+  }
+  playing_ = false;
+  return true;
+}
+
 void Audio::StreamCreated(base::SharedMemoryHandle shared_memory_handle,
                           size_t shared_memory_size,
                           base::SyncSocket::Handle socket_handle) {
@@ -170,9 +200,12 @@ void Audio::StreamCreated(base::SharedMemoryHandle shared_memory_handle,
 
   if (callback_) {
     shared_memory_->Map(shared_memory_size_);
-    audio_thread_.reset(new base::DelegateSimpleThread(this,
-                                                       "plugin_audio_thread"));
-    audio_thread_->Start();
+    // In common case StartPlayback() was called before StreamCreated().
+    if (playing_) {
+      audio_thread_.reset(new base::DelegateSimpleThread(this,
+          "plugin_audio_thread"));
+      audio_thread_->Start();
+    }
   }
 }
 
@@ -183,6 +216,9 @@ void Audio::Run() {
   while (sizeof(pending_data) ==
       socket_->Receive(&pending_data, sizeof(pending_data)) &&
       pending_data >= 0) {
+    // Exit the thread on pause.
+    if (pending_data < 0)
+      return;
     callback_(buffer, user_data_);
   }
 }
--- webkit/glue/plugins/pepper_audio.h
+++ webkit/glue/plugins/pepper_audio.h
@@ -59,15 +59,9 @@ class Audio : public Resource,
     return config_->GetReference();
   }
 
-  bool StartPlayback() {
-    // TODO(neb): Make this synchronous.
-    return audio_->StartPlayback();
-  }
+  bool StartPlayback();
 
-  bool StopPlayback() {
-    // TODO(neb): Make this synchronous.
-    return audio_->StopPlayback();
-  }
+  bool StopPlayback();
 
   // Resource override.
   virtual Audio* AsAudio();
@@ -82,6 +76,9 @@ class Audio : public Resource,
   // Audio thread. DelegateSimpleThread::Delegate implementation.
   virtual void Run();
   // End of DelegateSimpleThread::Delegate implementation.
+
+  // True if playing the stream.
+  bool playing_;
 
   // AudioConfig used for creating this Audio object.
   scoped_refptr<AudioConfig> config_;
