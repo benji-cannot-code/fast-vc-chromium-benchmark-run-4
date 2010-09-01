f66981e75521321a7e12afc74c05f2a8f4444cf2
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-01 07:15:17 +0000

    2010-08-31  Nikolas Zimmermann  <nzimmermann@rim.com>
            Reviewed by Dirk Schulze.
    
            Prepare RenderSVGContainer culling patch
            https://bugs.webkit.org/show_bug.cgi?id=44955
    
            Add styleWillChange methods for all SVG renderers, that call setNeedsBoundariesUpdate().
            It's currently a no-op, but will be used once the containers cache their boundaries, which
            happens in a follow-up patch.
    
            * rendering/RenderSVGBlock.cpp:
            (WebCore::RenderSVGBlock::styleWillChange): Call setNeedsBoundariesUpdate().
            * rendering/RenderSVGBlock.h:
            * rendering/RenderSVGImage.cpp:
            (WebCore::RenderSVGImage::styleWillChange): Ditto.
            * rendering/RenderSVGImage.h:
            * rendering/RenderSVGInline.cpp:
            (WebCore::RenderSVGInline::styleWillChange): Ditto.
            * rendering/RenderSVGInline.h:
            * rendering/RenderSVGModelObject.cpp:
            (WebCore::RenderSVGModelObject::styleWillChange): Ditto.
            * rendering/RenderSVGModelObject.h:
            * rendering/RenderSVGRoot.cpp:
            (WebCore::RenderSVGRoot::styleWillChange): Ditto.
            * rendering/RenderSVGRoot.h:
            * rendering/SVGRenderSupport.cpp:
            (WebCore::SVGRenderSupport::computeContainerBoundingBox): Skip hidden containers, not necessary to traverse them.
            * rendering/style/SVGRenderStyle.cpp:
            (WebCore::SVGRenderStyle::diff): svg-shadow changes need to cause relayouts, not only repaints, once container bounds are cached.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66584 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderSVGInline.h
+++ third_party/WebKit/WebCore/rendering/RenderSVGInline.h
@@ -57,6 +57,7 @@ private:
     virtual InlineFlowBox* createInlineFlowBox();
 
     virtual void destroy();
+    virtual void styleWillChange(StyleDifference, const RenderStyle* newStyle);
     virtual void styleDidChange(StyleDifference, const RenderStyle* oldStyle);
     virtual void updateFromElement();
 };
