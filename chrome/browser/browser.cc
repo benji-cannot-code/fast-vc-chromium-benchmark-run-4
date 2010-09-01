78ca397cc20ec034aebbf6cff58de0d8b71802e9
yutak@chromium.org <yutak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-01 06:24:04 +0000

    Mac: Allowed incognito windows to be bookmark open target.
    Patch contributed by Hajime Morita <morrita@google.com>.
    Original review URL: http://codereview.chromium.org/3217007
    
    BUG=23547
    TEST=manual
    
    Review URL: http://codereview.chromium.org/3225009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58141 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser.cc
+++ chrome/browser/browser.cc
@@ -3747,9 +3747,14 @@ bool Browser::CanCloseWithInProgressDownloads() {
 // Browser, Assorted utility functions (private):
 
 // static
+Browser* Browser::GetTabbedBrowser(Profile* profile, bool match_incognito) {
+  return BrowserList::FindBrowserWithType(profile, TYPE_NORMAL,
+                                          match_incognito);
+}
+
+// static
 Browser* Browser::GetOrCreateTabbedBrowser(Profile* profile) {
-  Browser* browser = BrowserList::FindBrowserWithType(profile, TYPE_NORMAL,
-                                                      false);
+  Browser* browser = GetTabbedBrowser(profile, false);
   if (!browser)
     browser = Browser::Create(profile);
   return browser;
