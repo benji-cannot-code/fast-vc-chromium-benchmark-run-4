22e4efc18d1299519b63d19a85e0605cddf645d9
panayiotis@google.com <panayiotis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-01 23:07:29 +0000

    New Shockwave version. Review URL: http://codereview.chromium.org/3259004
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58266 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/plugin_group.cc
+++ chrome/common/plugin_group.cc
@@ -31,7 +31,7 @@ static const PluginGroupDefinition kGroupDefinitions[] = {
     "http://go.microsoft.com/fwlink/?LinkID=185927" },
   { "Flip4Mac", "Flip4Mac", "",  "", "2.2.1",
     "http://www.telestream.net/flip4mac-wmv/overview.htm" },
-  { "Shockwave", "Shockwave for Director", "",  "", "11.5.7.609",
+  { "Shockwave", "Shockwave for Director", "",  "", "11.5.8.612",
     "http://www.adobe.com/shockwave/download/" }
 };
 
@@ -53,7 +53,7 @@ static const PluginGroupDefinition kGroupDefinitions[] = {
     "http://go.microsoft.com/fwlink/?LinkID=185927" },
   { "Silverlight 4", "Silverlight", "4", "5", "",
     "http://go.microsoft.com/fwlink/?LinkID=185927" },
-  { "Shockwave", "Shockwave for Director", "", "", "11.5.7.609",
+  { "Shockwave", "Shockwave for Director", "", "", "11.5.8.612",
     "http://www.adobe.com/shockwave/download/" },
   { "DivX Player", "DivX Web Player", "",  "", "1.4.3.4",
     "http://download.divx.com/divx/autoupdate/player/DivXWebPlayerInstaller.exe" },
