09c6fc639516110d77353b2534f4e2fd7f501cf3
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-01 22:58:53 +0000

    reland r57885 with a fix for linux/views.
    [GTK] a couple of constrained window fixes:
    
    1) don't grab focus when the parent tab isn't showing. Grab the focus when the tab is brought to the front.
    2) handle escape via normal key handling rather than accelerator keys
    3) don't allow the content view to take focus (via tab) when the constrained window is showing.
    
    BUG=53242, 50799
    TEST=see bugs. Also, tabbing between constrained window and url bar should work as expected.
    
    Review URL: http://codereview.chromium.org/3235010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58259 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/focus_store_gtk.h
+++ chrome/browser/gtk/focus_store_gtk.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,8 +20,11 @@ class FocusStoreGtk {
   GtkWidget* widget() const { return widget_; }
 
   // Save the widget that is currently focused for |widget|'s toplevel (NOT
-  // |widget|). Call with NULL to clear |widget_|.
+  // |widget|).
   void Store(GtkWidget* widget);
+
+  // Save |widget| as the focus widget. Call with NULL to clear |widget_|.
+  void SetWidget(GtkWidget* widget);
 
  private:
   // Disconnect the previous destroy handler (if any).
