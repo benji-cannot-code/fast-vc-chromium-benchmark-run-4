78d45cd183d1eb7c23e7b4a16a779b35fab190bd
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-01 19:51:32 +0000

    2010-09-01  Rob Buis  <rwlbuis@gmail.com>
            Reviewed by Darin Adler.
    
            Support for Percentage Values in border-radius
            https://bugs.webkit.org/show_bug.cgi?id=38354
    
            Support percentage values on the border-radius family
            of CSS properties.
    
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::getBorderRadiusCornerValue):
            * css/CSSParser.cpp:
            (WebCore::CSSParser::parseValue):
            (WebCore::CSSParser::parseBorderRadius):
            * css/CSSPrimitiveValue.cpp:
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::applyProperty):
            * page/animation/AnimationBase.cpp:
            (WebCore::AnimationBase::ensurePropertyMap):
            * platform/Length.h:
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::repaintAfterLayoutIfNeeded):
            * rendering/style/BorderData.h:
            (WebCore::BorderData::BorderData):
            (WebCore::BorderData::hasBorderRadius):
            (WebCore::BorderData::topLeft):
            (WebCore::BorderData::topRight):
            (WebCore::BorderData::bottomLeft):
            (WebCore::BorderData::bottomRight):
            * rendering/style/RenderStyle.cpp:
            (WebCore::RenderStyle::getBorderRadiiForRect):
            (WebCore::RenderStyle::getInnerBorderRadiiForRectWithBorderWidths):
            * rendering/style/RenderStyle.h:
            (WebCore::InheritedFlags::borderTopLeftRadius):
            (WebCore::InheritedFlags::borderTopRightRadius):
            (WebCore::InheritedFlags::borderBottomLeftRadius):
            (WebCore::InheritedFlags::borderBottomRightRadius):
            (WebCore::InheritedFlags::setBorderTopLeftRadius):
            (WebCore::InheritedFlags::setBorderTopRightRadius):
            (WebCore::InheritedFlags::setBorderBottomLeftRadius):
            (WebCore::InheritedFlags::setBorderBottomRightRadius):
            (WebCore::InheritedFlags::setBorderRadius):
            (WebCore::InheritedFlags::initialBorderRadius):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/style/BorderData.h
+++ third_party/WebKit/WebCore/rendering/style/BorderData.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BorderData_h
 
 #include "BorderValue.h"
-#include "IntSize.h"
+#include "LengthSize.h"
 #include "NinePieceImage.h"
 
 namespace WebCore {
@@ -35,6 +35,12 @@ namespace WebCore {
 class BorderData {
 friend class RenderStyle;
 public:
+    BorderData() : m_topLeft(Length(0, Fixed), Length(0, Fixed))
+                 , m_topRight(Length(0, Fixed), Length(0, Fixed))
+                 , m_bottomLeft(Length(0, Fixed), Length(0, Fixed))
+                 , m_bottomRight(Length(0, Fixed), Length(0, Fixed))
+    {
+    }
     bool hasBorder() const
     {
         bool haveImage = m_image.hasImage();
@@ -43,13 +49,13 @@ public:
 
     bool hasBorderRadius() const
     {
-        if (m_topLeft.width() > 0)
+        if (m_topLeft.width().rawValue() > 0)
             return true;
-        if (m_topRight.width() > 0)
+        if (m_topRight.width().rawValue() > 0)
             return true;
-        if (m_bottomLeft.width() > 0)
+        if (m_bottomLeft.width().rawValue() > 0)
             return true;
-        if (m_bottomRight.width() > 0)
+        if (m_bottomRight.width().rawValue() > 0)
             return true;
         return false;
     }
@@ -100,10 +106,10 @@ public:
     
     const NinePieceImage& image() const { return m_image; }
     
-    const IntSize& topLeft() const { return m_topLeft; }
-    const IntSize& topRight() const { return m_topRight; }
-    const IntSize& bottomLeft() const { return m_bottomLeft; }
-    const IntSize& bottomRight() const { return m_bottomRight; }
+    const LengthSize& topLeft() const { return m_topLeft; }
+    const LengthSize& topRight() const { return m_topRight; }
+    const LengthSize& bottomLeft() const { return m_bottomLeft; }
+    const LengthSize& bottomRight() const { return m_bottomRight; }
     
 private:
     BorderValue m_left;
@@ -113,10 +119,10 @@ private:
 
     NinePieceImage m_image;
 
-    IntSize m_topLeft;
-    IntSize m_topRight;
-    IntSize m_bottomLeft;
-    IntSize m_bottomRight;
+    LengthSize m_topLeft;
+    LengthSize m_topRight;
+    LengthSize m_bottomLeft;
+    LengthSize m_bottomRight;
 };
 
 } // namespace WebCore
