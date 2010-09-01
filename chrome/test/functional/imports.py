0a78b692290512c40960b0545f6d662d321e93fc
alyssad@google.com <alyssad@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-01 18:06:13 +0000

    Undo the changes made to imports test that sets an environment variable BUG=52124
    This change set an environment variable necessary for passwords to be imported on Mac, but it caused an error with the sandbox on Snow Leopard machines.
    
    Review URL: http://codereview.chromium.org/3294003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/imports.py
+++ chrome/test/functional/imports.py
@@ -47,13 +47,6 @@ class ImportsTest(pyauto.PyUITest):
           pyauto.PyUITest.DataDir(), 'import', 'firefox', 'macwin.zip')
       self._safari_profiles_path = os.path.join(
           os.environ['HOME'], 'Library', 'Safari')
-      # Set the path here since it can't get set on the browser side and it is
-      # necessary for importing passwords on Mac.
-      self._old_path = None
-      if 'DYLD_FALLBACK_LIBRARY_PATH' in os.environ:
-        self._old_path = os.environ['DYLD_FALLBACK_LIBRARY_PATH']
-      os.environ['DYLD_FALLBACK_LIBRARY_PATH'] = os.path.join(
-          self.DataDir(), 'firefox3_nss_mac')
       # Don't import passwords to avoid Keychain popups. See crbug.com/49378.
       self._to_import = ['HISTORY', 'FAVORITES', 'SEARCH_ENGINES', 'HOME_PAGE']
     elif pyauto.PyUITest.IsWin():
@@ -81,12 +74,6 @@ class ImportsTest(pyauto.PyUITest):
 
   def tearDown(self):
     pyauto.PyUITest.tearDown(self)
-    # Re-set the path to its state before the test.
-    if self.IsMac():
-      if self._old_path:
-        os.environ['DYLD_FALLBACK_LIBRARY_PATH'] = self._old_path
-      else:
-        os.environ.pop('DYLD_FALLBACK_LIBRARY_PATH')
     # Delete any replacers to restore the original profiles.
     if self._safari_replacer:
       del self._safari_replacer
