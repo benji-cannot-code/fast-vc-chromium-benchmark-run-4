d2807ba1002fa8b4034e2c6877df0573e7e877fe
twiz@google.com <twiz@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-01 16:53:12 +0000

    A CL implementing a collection of fixes to tidy up experimental popups.
    The following are fixed:
     - Modified test\data\extension\api_test\popup to account for the removal of toolbar support.  The tests now start with a background page that creates a tab pointing to an extension dom-ui that runs the tests.
     - Changed the positioning of experimental popup views with the arrow location set to the right.  Popups used to always be anchored to the left edge of their relative dom element, now popups with a right-arrow will be attached to the right edge of their relative dom anchor.  Note that this only impacts popups with the 'rectangle' chrome.
     - Removed empty directory, test\data\extensions\api_test\popup_api
    
    BUG=46601, 51821
    TEST=ExtensionApiTest.Popup
    
    
    Review URL: http://codereview.chromium.org/3155044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58188 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_popup_apitest.cc
+++ chrome/browser/extensions/extension_popup_apitest.cc
@@ -7,15 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_switches.h"
 
-// Flaky on windows.  http://crbug.com/46601
-#if defined(OS_WIN)
-#define MAYBE_Popup FLAKY_Popup
-#else
-#define MAYBE_Popup Popup
-#endif
 
-// Disabled for some rewriting.   http://crbug.com/51821
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Popup) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Popup) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
 
