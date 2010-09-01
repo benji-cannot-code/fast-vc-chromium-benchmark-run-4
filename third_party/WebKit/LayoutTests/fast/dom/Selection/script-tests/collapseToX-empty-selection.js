c24e0259324db9b846713b0d6aaff512bb29fd19
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-01 10:23:22 +0000

    2010-09-01  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Darin Adler.
    
            DOM Selection: collapseToStart() and collapseToEnd() should throw INVALID_STATE_ERR if there's no selection
            https://bugs.webkit.org/show_bug.cgi?id=44598
    
            Spec links:
            http://www.whatwg.org/specs/web-apps/current-work/#dom-selection-collapsetostart
            http://www.whatwg.org/specs/web-apps/current-work/#dom-selection-collapsetoend
    
            Test: fast/dom/Selection/collapseToX-empty-selection.html
    
            * page/DOMSelection.cpp:
            (WebCore::DOMSelection::collapseToEnd):
            (WebCore::DOMSelection::collapseToStart):
            * page/DOMSelection.h:
            * page/DOMSelection.idl:
    2010-09-01  Andreas Kling  <andreas.kling@nokia.com>
    
            Reviewed by Darin Adler.
    
            DOM Selection: collapseToStart() and collapseToEnd() should throw INVALID_STATE_ERR if there's no selection
            https://bugs.webkit.org/show_bug.cgi?id=44598
    
            Spec links:
            http://www.whatwg.org/specs/web-apps/current-work/#dom-selection-collapsetostart
            http://www.whatwg.org/specs/web-apps/current-work/#dom-selection-collapsetoend
    
            * fast/dom/Selection/collapseToX-empty-selection-expected.txt: Added.
            * fast/dom/Selection/collapseToX-empty-selection.html: Added.
            * fast/dom/Selection/script-tests/collapseToX-empty-selection.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/Selection/script-tests/collapseToX-empty-selection.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test that collapseToStart() and collapseToEnd() throw INVALID_STATE_ERR if no selection is made.");
+
+var sel = window.getSelection();
+var textNode = document.createTextNode("abcdef");
+document.body.appendChild(textNode);
+
+shouldThrow("sel.collapseToStart()", "'Error: INVALID_STATE_ERR: DOM Exception 11'");
+shouldThrow("sel.collapseToEnd()", "'Error: INVALID_STATE_ERR: DOM Exception 11'");
+
+sel.selectAllChildren(textNode);
+
+shouldBe("sel.collapseToStart()", "undefined");
+shouldBe("sel.collapseToEnd()", "undefined");
+
+document.body.removeChild(textNode);
+
+var successfullyParsed = true;
