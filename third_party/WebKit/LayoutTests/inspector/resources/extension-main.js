77643cb607275d37f622865926adc3f539303a6e
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-01 14:19:34 +0000

    2010-09-01  Andrey Kosyakov  <caseq@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector, Extension API: Panel.onSelectionChanged event is never fired
            Added command line API to the lexical context of code evaluated by webInspector.inspectedWindow.evaluate()
            Fixed webInspector.inspectedWindow.evaluate() to return object rather than its stringified representation
            https://bugs.webkit.org/show_bug.cgi?id=44957
    
            Test: inspector/extensions-eval.html
    
            * inspector/front-end/ExtensionAPI.js:
            (WebInspector.injectedExtensionAPI.InspectedWindow.prototype.evaluate):
            * inspector/front-end/ExtensionServer.js:
            (WebInspector.ExtensionServer.prototype._onEvaluateOnInspectedPage):
    
    2010-09-01  Andrey Kosyakov  <caseq@chromium.org>
    
            Reviewed by Yury Semikhatsky.
    
            Web Inspector, Extension API: Panel.onSelectionChanged event is never fired
            Added tests for webInspector.inspectedWindow.eval() and event dispatchers.
            https://bugs.webkit.org/show_bug.cgi?id=44957
    
            * inspector/extensions-eval-expected.txt: Added.
            * inspector/extensions-eval.html: Added.
            * inspector/extensions-expected.txt:
            * inspector/extensions.html:
            * inspector/resources/extension-main.js:
            (fetchTests.callback):
            (fetchTests):
            * platform/chromium/test_expectations.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/inspector/resources/extension-main.js
+++ third_party/WebKit/LayoutTests/inspector/resources/extension-main.js
@@ -3,8 +3,7 @@ function fetchTests()
 {
     function callback(result)
     {
-         var functions = JSON.parse(result.value);
-         window.eval(functions);
+         window.eval(result.value);
          runTests();
     }
     webInspector.inspectedWindow.evaluate("extensionFunctions()", callback);
