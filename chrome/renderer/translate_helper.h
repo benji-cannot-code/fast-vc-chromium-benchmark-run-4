e05e181f4c406b515268b72a13aded4a81cdf671
jcivelli@chromium.org <jcivelli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-01 15:48:37 +0000

    This CL ensures we use the language specified in the language meta tag if any. Also IsPageTranslatable was moved to TranslateHelper as it is where it belongs.
    BUG=51454
    TEST=Run the browser tests.
    
    Review URL: http://codereview.chromium.org/3236008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58175 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/translate_helper.h
+++ chrome/renderer/translate_helper.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class RenderView;
 namespace WebKit {
+class WebDocument;
 class WebFrame;
 }
 
@@ -39,6 +40,18 @@ class TranslateHelper {
   // Cancels any translation that is currently being performed.  This does not
   // revert existing translations.
   void CancelPendingTranslation();
+
+  // Returns whether the page associated with |document| is a candidate for
+  // translation.  Some pages can explictly specify (via a meta-tag) that they
+  // should not be translated.
+  static bool IsPageTranslatable(WebKit::WebDocument* document);
+
+  // Returns the language specified in the language meta tag of |document|, or
+  // an empty string if no such tag was found.
+  // The tag may specify several languages, the first one is returned.
+  // Example of such meta-tag:
+  // <meta http-equiv="content-language" content="en, fr">
+  static std::string GetPageLanguageFromMetaTag(WebKit::WebDocument* document);
 
  protected:
   // The following methods are protected so they can be overridden in
