6ca2ead0ab3100ee94c9e5f5f5aedadbf78f0b88
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-01 14:01:15 +0000

    2010-08-30  Patrick Gansterer  <paroga@paroga.com>
             Reviewed by Dirk Schulze.
    
             RenderImage::imageChanged invalidates wrong area
             https://bugs.webkit.org/show_bug.cgi?id=43779
    
             Remove RenderImage inheritance from RenderSVGImage, it now inherits from RenderSVGModelObject.
    
             * rendering/RenderSVGImage.cpp:
             (WebCore::RenderSVGImage::RenderSVGImage):
             (WebCore::RenderSVGImage::~RenderSVGImage):
             (WebCore::RenderSVGImage::layout):
             (WebCore::RenderSVGImage::paint):
             (WebCore::RenderSVGImage::imageChanged):
             * rendering/RenderSVGImage.h:
             (WebCore::RenderSVGImage::imageResource):
             * rendering/RenderSVGModelObject.h:
             * svg/SVGImageElement.cpp:
    
    2010-09-01  Nikolas Zimmermann  <nzimmermann@rim.com>
    
            Reviewed by Dirk Schulze.
    
            RenderImage::imageChanged invalidates wrong area
            https://bugs.webkit.org/show_bug.cgi?id=43779
    
            Rebaseline SVG image dumpAsText() tests.
            A newline is gone from each of the results, after switching from RenderImage to RenderSVGModelObject in RenderSVGImage.
    
            * svg/custom/loadevents-capturing-expected.txt:
            * svg/custom/loadevents-externalresourcesrequired-expected.txt:
            * svg/custom/loadevents-normal-expected.txt:
            * svg/dynamic-updates/SVGImageElement-dom-height-attr-expected.txt:
            * svg/dynamic-updates/SVGImageElement-dom-preserveAspectRatio-attr-expected.txt:
            * svg/dynamic-updates/SVGImageElement-dom-width-attr-expected.txt:
            * svg/dynamic-updates/SVGImageElement-dom-x-attr-expected.txt:
            * svg/dynamic-updates/SVGImageElement-dom-y-attr-expected.txt:
            * svg/dynamic-updates/SVGImageElement-svgdom-height-prop-expected.txt:
            * svg/dynamic-updates/SVGImageElement-svgdom-preserveAspectRatio-prop-expected.txt:
            * svg/dynamic-updates/SVGImageElement-svgdom-width-prop-expected.txt:
            * svg/dynamic-updates/SVGImageElement-svgdom-x-prop-expected.txt:
            * svg/dynamic-updates/SVGImageElement-svgdom-y-prop-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGImageElement.cpp
+++ third_party/WebKit/WebCore/svg/SVGImageElement.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Attribute.h"
 #include "CSSPropertyNames.h"
+#include "RenderImageResource.h"
 #include "RenderSVGImage.h"
 #include "RenderSVGResource.h"
 #include "SVGDocument.h"
