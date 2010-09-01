8eb9e192491d7431f40186ff38231ed3a64d405e
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-01 04:34:33 +0000

    Allow options pages for hosted apps to be external.
    BUG=53155
    TEST=ExtensionManifestTet.OptionsPageInApps
    
    Review URL: http://codereview.chromium.org/3222008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58125 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_constants.h
+++ chrome/common/extensions/extension_constants.h
@@ -130,6 +130,8 @@ namespace extension_manifest_errors {
   extern const char* kInvalidName;
   extern const char* kInvalidOmniboxKeyword;
   extern const char* kInvalidOptionsPage;
+  extern const char* kInvalidOptionsPageExpectUrlInPackage;
+  extern const char* kInvalidOptionsPageInHostedApp;
   extern const char* kInvalidPageAction;
   extern const char* kInvalidPageActionDefaultTitle;
   extern const char* kInvalidPageActionIconPath;
