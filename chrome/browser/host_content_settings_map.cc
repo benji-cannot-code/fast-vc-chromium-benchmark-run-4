403cc16e75ec1028aa2f80bbf5da275acad06f80
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-01 08:35:47 +0000

    [Mac] Add option to block nonsandboxed plugins to content settings.
    XIB changes: Add a radio button in the middle of the plugin (default) content settings with title ^IDS_PLUGIN_LOAD_SNADBOXED_RADIO.
    
    BUG=53812
    TEST=none
    
    Review URL: http://codereview.chromium.org/3250006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58149 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/host_content_settings_map.cc
+++ chrome/browser/host_content_settings_map.cc
@@ -233,6 +233,7 @@ HostContentSettingsMap::HostContentSettingsMap(Profile* profile)
   prefs->AddPrefObserver(prefs::kDefaultContentSettings, this);
   prefs->AddPrefObserver(prefs::kContentSettingsPatterns, this);
   prefs->AddPrefObserver(prefs::kBlockThirdPartyCookies, this);
+  prefs->AddPrefObserver(prefs::kBlockNonsandboxedPlugins, this);
   notification_registrar_.Add(this, NotificationType::PROFILE_DESTROYED,
                               Source<Profile>(profile_));
 }
@@ -756,6 +757,10 @@ void HostContentSettingsMap::Observe(NotificationType type,
       AutoLock auto_lock(lock_);
       block_third_party_cookies_ = profile_->GetPrefs()->GetBoolean(
           prefs::kBlockThirdPartyCookies);
+    } else if (prefs::kBlockNonsandboxedPlugins == *name) {
+      AutoLock auto_lock(lock_);
+      block_nonsandboxed_plugins_ = profile_->GetPrefs()->GetBoolean(
+          prefs::kBlockNonsandboxedPlugins);
     } else {
       NOTREACHED() << "Unexpected preference observed";
       return;
@@ -909,6 +914,7 @@ void HostContentSettingsMap::UnregisterObservers() {
   prefs->RemovePrefObserver(prefs::kDefaultContentSettings, this);
   prefs->RemovePrefObserver(prefs::kContentSettingsPatterns, this);
   prefs->RemovePrefObserver(prefs::kBlockThirdPartyCookies, this);
+  prefs->RemovePrefObserver(prefs::kBlockNonsandboxedPlugins, this);
   notification_registrar_.Remove(this, NotificationType::PROFILE_DESTROYED,
                                  Source<Profile>(profile_));
   profile_ = NULL;
