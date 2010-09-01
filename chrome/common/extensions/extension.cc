2a69f368706888bea5eda64318f416068ac24cb4
erikkay@chromium.org <erikkay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-01 22:43:43 +0000

    fix history permission prompt
    BUG=54006
    TEST=install an extension with history permission.  it should warn you about access to your browser history.
    
    Review URL: http://codereview.chromium.org/3357002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58251 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension.cc
+++ chrome/common/extensions/extension.cc
@@ -945,7 +945,8 @@ bool Extension::IsPrivilegeIncrease(Extension* old_extension,
 // static
 bool Extension::HasEffectiveBrowsingHistoryPermission() const {
   return HasApiPermission(kTabPermission) ||
-      HasApiPermission(kBookmarkPermission);
+      HasApiPermission(kBookmarkPermission) ||
+      HasApiPermission(kHistoryPermission);
 }
 
 // static
