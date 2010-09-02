be4adb94185d187c9dbeb58eba948f6414812415
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-02 00:01:39 +0000

    Make NTP look good with themes.
    BUG=52913
    TEST=Visual inspection.
    
    Review URL: http://codereview.chromium.org/3340005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58272 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/new_new_tab.js
+++ chrome/browser/resources/new_new_tab.js
@@ -721,7 +721,7 @@ OptionMenu.prototype = {
 };
 
 var optionMenu = new OptionMenu(
-    document.querySelector('#most-visited-section h2 .settings'),
+    document.querySelector('#most-visited-section h2 .settings-wrapper'),
     $('option-menu'));
 optionMenu.commands = {
   'clear-all-blacklisted' : function() {
