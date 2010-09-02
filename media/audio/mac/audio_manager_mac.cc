2e29332ecafb486286e140b799d38a939ed49a32
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-02 16:16:49 +0000

    This test now works for the mac audio capture code as well, and in a future CL I'll enable audio recording in linux and this test will suit that code too.
    In the process I changed a uint32 in a function prototype to int because style guide recommends int for such cases. I also added parameter validation code to the mac audio manager to get it pass this new test.
    
    BUG=none
    TEST=media_unittests should succeed as before.
    
    Review URL: http://codereview.chromium.org/3357004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58365 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/mac/audio_manager_mac.cc
+++ media/audio/mac/audio_manager_mac.cc
@@ -10,8 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/mac/audio_input_mac.h"
 #include "media/audio/mac/audio_manager_mac.h"
 #include "media/audio/mac/audio_output_mac.h"
+#include "media/base/limits.h"
 
 namespace {
+const int kMaxInputChannels = 2;
+const int kMaxSamplesPerPacket = media::Limits::kMaxSampleRate;
+
 bool HasAudioHardware(AudioObjectPropertySelector selector) {
   AudioDeviceID output_device_id = kAudioObjectUnknown;
   const AudioObjectPropertyAddress property_address = {
@@ -49,7 +53,11 @@ AudioOutputStream* AudioManagerMac::MakeAudioOutputStream(
 }
 
 AudioInputStream* AudioManagerMac::MakeAudioInputStream(
-    AudioParameters params, uint32 samples_per_packet) {
+    AudioParameters params, int samples_per_packet) {
+  if (!params.IsValid() || (params.channels > kMaxInputChannels) ||
+      (samples_per_packet > kMaxSamplesPerPacket) || (samples_per_packet < 0))
+    return NULL;
+
   if (params.format == AudioParameters::AUDIO_MOCK) {
     return FakeAudioInputStream::MakeFakeStream(params, samples_per_packet);
   } else if (params.format == AudioParameters::AUDIO_PCM_LINEAR) {
