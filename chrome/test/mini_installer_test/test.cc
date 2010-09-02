62252c0ab651fcdfcbbb0a55f458b56fd00af0b0
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-02 21:03:15 +0000

    Review URL: http://codereview.chromium.org/3163040
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58400 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/mini_installer_test/test.cc
+++ chrome/test/mini_installer_test/test.cc
@@ -114,8 +114,7 @@ TEST_F(MiniInstallTest, FullOverPreviousFullUser) {
     user_inst_->OverInstallOnFullInstaller(kFullInstall);
 }
 TEST_F(MiniInstallTest, FullOverPreviousFullSys) {
-  if (!chrome_frame_)
-    sys_inst_->OverInstallOnFullInstaller(kFullInstall);
+  sys_inst_->OverInstallOnFullInstaller(kFullInstall);
 }
 
 // Overinstall diff installer.
@@ -125,8 +124,7 @@ TEST_F(MiniInstallTest, DiffOverPreviousFullUser) {
 }
 
 TEST_F(MiniInstallTest, DiffOverPreviousFullSys) {
-  if (!chrome_frame_)
-    sys_inst_->OverInstallOnFullInstaller(kDiffInstall);
+  sys_inst_->OverInstallOnFullInstaller(kDiffInstall);
 }
 
 // Repair version folder.
@@ -136,8 +134,7 @@ TEST_F(MiniInstallTest, RepairFolderOnFullUser) {
 }
 
 TEST_F(MiniInstallTest, RepairFolderOnFullSys) {
-  if (!chrome_frame_)
-    sys_inst_->Repair(ChromeMiniInstaller::VERSION_FOLDER);
+  sys_inst_->Repair(ChromeMiniInstaller::VERSION_FOLDER);
 }
 
 // Repair registry.
@@ -146,8 +143,7 @@ TEST_F(MiniInstallTest, RepairRegistryOnFullUser) {
     user_inst_->Repair(ChromeMiniInstaller::REGISTRY);
 }
 TEST_F(MiniInstallTest, RepairRegistryOnFullSys) {
-  if (!chrome_frame_)
-    sys_inst_->Repair(ChromeMiniInstaller::REGISTRY);
+  sys_inst_->Repair(ChromeMiniInstaller::REGISTRY);
 }
 
 // Install standalone.
