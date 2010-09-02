20718cca5d1f698efe69a5372139d69d23163dc4
stoyan@chromium.org <stoyan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-02 16:17:37 +0000

    Add lock since the add/remove tabproxy is invoked from different threads. BUG=53963 Review URL: http://codereview.chromium.org/3306003
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58367 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_frame_automation.cc
+++ chrome_frame/chrome_frame_automation.cc
@@ -52,10 +52,12 @@ class ChromeFrameAutomationProxyImpl::TabProxyNotificationMessageFilter
   }
 
   void AddTabProxy(AutomationHandle tab_proxy) {
+    AutoLock lock(lock_);
     tabs_list_.push_back(tab_proxy);
   }
 
   void RemoveTabProxy(AutomationHandle tab_proxy) {
+    AutoLock lock(lock_);
     tabs_list_.remove(tab_proxy);
   }
 
@@ -93,6 +95,7 @@ class ChromeFrameAutomationProxyImpl::TabProxyNotificationMessageFilter
  private:
   AutomationHandleTracker* tracker_;
   std::list<AutomationHandle> tabs_list_;
+  Lock lock_;
 };
 
 class ChromeFrameAutomationProxyImpl::CFMsgDispatcher
