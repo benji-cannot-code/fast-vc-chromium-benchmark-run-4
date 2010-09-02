67c31f3dbbd1ca123b31f042e31a3081a2e4711c
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-02 07:12:48 +0000

    Warn the user to regenerate docs when changing examples.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3331002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58329 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/PRESUBMIT.py
+++ chrome/common/extensions/PRESUBMIT.py
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os.path
 
+# When files in these directories are changed, we display a warning.
 DEPENDENT_DIRS = set([
   os.path.normpath("chrome/common/extensions/api"),
-  os.path.normpath("chrome/common/extensions/docs/build"),
-  os.path.normpath("chrome/common/extensions/docs/js"),
-  os.path.normpath("chrome/common/extensions/docs/static"),
-  os.path.normpath("chrome/common/extensions/docs/template")
+  os.path.normpath("chrome/common/extensions/docs")
+])
+
+# Except for these directories.
+BLACKLIST_DIRS = set([
+  os.path.normpath("chrome/common/extensions/docs/server")
 ])
 
 REBUILD_WARNING = """
@@ -27,8 +30,12 @@ Be sure to include any modified resulting static files
 def CheckChange(input_api, output_api):  
   for f in input_api.AffectedFiles():
     dir = os.path.normpath(input_api.os_path.dirname(f.LocalPath()))
-    if dir in DEPENDENT_DIRS:
-      return [output_api.PresubmitPromptWarning(REBUILD_WARNING)]
+    while len(dir):
+      if dir in BLACKLIST_DIRS:
+        return []
+      if dir in DEPENDENT_DIRS:
+        return [output_api.PresubmitPromptWarning(REBUILD_WARNING)]
+      dir = os.path.dirname(dir)
   return []
 
 def CheckChangeOnUpload(input_api, output_api):
