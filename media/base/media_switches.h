9e9e9571144793afffee88d2b366237a05005c49
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-02 21:48:54 +0000

    Implement audio recording for Linux via ALSA. There are no new unit tests because a cross platform unit test added in CL 3357004 covers this code.
    BUG=53598
    TEST=media_unittests --gtest_filter=AudioInputTest.*
    
    Review URL: http://codereview.chromium.org/3299005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58409 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 
 #if defined(OS_LINUX)
-extern const char kAlsaDevice[];
+extern const char kAlsaOutputDevice[];
+extern const char kAlsaInputDevice[];
 #endif
 
 extern const char kEnableAcceleratedDecoding[];
