b11c87b0cb5dc8d11357c791ab08c128cec18adf
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-03 19:56:13 +0000

    Changed a link widget to button per feedback from UX.
    BUG=none
    TEST=manual, use speech input on windows and verify that the popup has a cancel button.
    
    Review URL: http://codereview.chromium.org/3367009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58528 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/speech_input_bubble_views.cc
+++ chrome/browser/speech/speech_input_bubble_views.cc
@@ -20,9 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gfx/canvas.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
+#include "views/controls/button/native_button.h"
 #include "views/controls/image_view.h"
 #include "views/controls/label.h"
-#include "views/controls/link.h"
 #include "views/standard_layout.h"
 #include "views/view.h"
 
@@ -34,14 +34,14 @@ const int kBubbleVertMargin = 0;
 // This is the content view which is placed inside a SpeechInputBubble.
 class ContentView
     : public views::View,
-      public views::LinkController {
+      public views::ButtonListener {
  public:
   explicit ContentView(SpeechInputBubbleDelegate* delegate);
 
   void SetRecognizingMode();
 
-  // views::LinkController methods.
-  virtual void LinkActivated(views::Link* source, int event_flags);
+  // views::ButtonListener methods.
+  virtual void ButtonPressed(views::Button* source, const views::Event& event);
 
   // views::View overrides.
   virtual gfx::Size GetPreferredSize();
@@ -51,7 +51,7 @@ class ContentView
   SpeechInputBubbleDelegate* delegate_;
   views::ImageView* icon_;
   views::Label* heading_;
-  views::Link* cancel_;
+  views::NativeButton* cancel_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentView);
 };
@@ -73,10 +73,7 @@ ContentView::ContentView(SpeechInputBubbleDelegate* delegate)
   icon_->SetHorizontalAlignment(views::ImageView::CENTER);
   AddChildView(icon_);
 
-  cancel_ = new views::Link(l10n_util::GetString(IDS_CANCEL));
-  cancel_->SetController(this);
-  cancel_->SetFont(font.DeriveFont(3, gfx::Font::NORMAL));
-  cancel_->SetHorizontalAlignment(views::Label::ALIGN_CENTER);
+  cancel_ = new views::NativeButton(this, l10n_util::GetString(IDS_CANCEL));
   AddChildView(cancel_);
 }
 
@@ -85,7 +82,8 @@ void ContentView::SetRecognizingMode() {
       IDR_SPEECH_INPUT_PROCESSING));
 }
 
-void ContentView::LinkActivated(views::Link* source, int event_flags) {
+void ContentView::ButtonPressed(views::Button* source,
+                                const views::Event& event) {
   if (source == cancel_) {
     delegate_->RecognitionCancelled();
   } else {
