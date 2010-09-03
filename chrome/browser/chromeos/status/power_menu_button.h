f5bb08a6f6fce4cace261cda210d62661e62a274
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-03 19:29:29 +0000

    Added tooltips to stats bar items. BUG=chromium-os:5601 (http://code.google.com/p/chromium-os/issues/detail?id=5601) TEST=Check that hover text over status bar icons seems reasonable. Test different states of network (disconnected, ethernet, connecting to a wifi network, connected to a wifi network) and battery (plugged in, unplugged).
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=57234
    
    Review URL: http://codereview.chromium.org/3158014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58526 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/power_menu_button.h
+++ chrome/browser/chromeos/status/power_menu_button.h
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/controls/menu/menu_2.h"
 #include "views/controls/menu/view_menu_delegate.h"
 
+namespace base {
+class TimeDelta;
+}
+
 class SkBitmap;
 
 namespace chromeos {
@@ -65,14 +69,22 @@ class PowerMenuButton : public StatusAreaButton,
   // This method will draw the |icon| in the appropriate place on the |canvas|.
   void DrawPowerIcon(gfx::Canvas* canvas, SkBitmap icon);
 
-  // Update the power icon depending on the power status.
-  void UpdateIcon();
+  // Update the power icon and menu label info depending on the power status.
+  void UpdateIconAndLabelInfo();
 
   // The number of power images.
   static const int kNumPowerImages;
 
   // The power menu.
   views::Menu2 power_menu_;
+
+  // Stored data gathered CrosLibrary::PowerLibrary.
+  bool battery_is_present_;
+  bool line_power_on_;
+  bool battery_fully_charged_;
+  double battery_percentage_;
+  base::TimeDelta battery_time_to_full_;
+  base::TimeDelta battery_time_to_empty_;
 
   // The currently showing icon bitmap id.
   int icon_id_;
