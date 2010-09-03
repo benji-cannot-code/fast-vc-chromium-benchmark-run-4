d53f4a99863c2d12f70af8bb000133908df43d52
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-03 00:44:35 +0000

    Correct a problem whereby the prefetching experiment would never be scheduled for updates.
    Review URL: http://codereview.chromium.org/3362001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58440 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/utils.cc
+++ chrome_frame/utils.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <shlobj.h>
 #include <wininet.h>
 
+#include <atlsecurity.h>
+
 #include "base/file_util.h"
 #include "base/file_version_info.h"
 #include "base/lazy_instance.h"
@@ -1127,6 +1129,19 @@ bool IsIBrowserServicePatchEnabled() {
   ProtocolPatchMethod patch_method = GetPatchMethod();
   return patch_method == PATCH_METHOD_IBROWSER;
 }
+
+bool IsSystemProcess() {
+  bool is_system = false;
+  CAccessToken process_token;
+  if (process_token.GetProcessToken(TOKEN_QUERY, GetCurrentProcess())) {
+    CSid logon_sid;
+    if (process_token.GetUser(&logon_sid)) {
+      is_system = logon_sid == Sids::System();
+    }
+  }
+  return is_system;
+}
+
 
 std::string BindStatus2Str(ULONG bind_status) {
   std::string s;
