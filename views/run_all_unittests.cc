1736bf6a4f5ecff3451ff4cdf6114438ba76660b
jcivelli@chromium.org <jcivelli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-03 00:41:43 +0000

    Factoring out views related unit-tests in their own exe.
    BUG=None
    TEST=Run view_unit_tests should run successfully on Windows and Linux toolkit views.
    
    Review URL: http://codereview.chromium.org/3256008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58439 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ views/run_all_unittests.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "app/app_paths.h"
+#include "app/resource_bundle.h"
+#include "base/test/test_suite.h"
+
+class ViewTestSuite : public base::TestSuite {
+ public:
+  ViewTestSuite(int argc, char** argv) : base::TestSuite(argc, argv) {}
+
+ protected:
+  virtual void Initialize() {
+    app::RegisterPathProvider();
+    base::TestSuite::Initialize();
+    ResourceBundle::InitSharedInstance("en-US");
+  }
+};
+
+int main(int argc, char **argv) {
+  return ViewTestSuite(argc, argv).Run();
+}
