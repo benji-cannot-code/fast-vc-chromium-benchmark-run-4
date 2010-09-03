db5e5e944c5e52b6fadd3a97554ca041d565578c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-03 07:03:00 +0000

    2010-09-02  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Kent Tamura.
    
            Add ENABLE(DATABASE) guard to DatabaseAuthorizer.cpp
            https://bugs.webkit.org/show_bug.cgi?id=45152
    
            DatabaseAuthorizer is used only with ENABLE(DATABASE).
    
            * storage/DatabaseAuthorizer.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66717 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/DatabaseAuthorizer.cpp
+++ third_party/WebKit/WebCore/storage/DatabaseAuthorizer.cpp
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "DatabaseAuthorizer.h"
 
+#if ENABLE(DATABASE)
+
 #include "PlatformString.h"
 #include <wtf/PassRefPtr.h>
 
@@ -420,3 +422,5 @@ int DatabaseAuthorizer::updateDeletesBasedOnTableName(const String& tableName)
 }
 
 } // namespace WebCore
+
+#endif
