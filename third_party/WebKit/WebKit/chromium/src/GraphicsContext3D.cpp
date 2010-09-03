623a9d98046e25019ab335e7e7d28fb51e742e97
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-03 20:04:32 +0000

    2010-09-03  James Robinson  <jamesr@chromium.org>
            [chromium] Add one more include to fix the mac compile.
    
            * src/GraphicsContext3D.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/GraphicsContext3D.cpp
+++ third_party/WebKit/WebKit/chromium/src/GraphicsContext3D.cpp
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if PLATFORM(CG)
 #include "GraphicsContext.h"
+#include "WebGLRenderingContext.h"
 #include <CoreGraphics/CGContext.h>
 #include <CoreGraphics/CGImage.h>
 #endif
