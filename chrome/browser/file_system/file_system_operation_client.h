4d3adf27775fde181c279cec2ea4099712477645
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-04 01:59:48 +0000

    Rename FileSystemBackend to FileSystemOperation and make it explicit that it's supposed to be used for single time operation per instance.
    Original patch from kkanetkar@chromium.org
    http://codereview.chromium.org/3319005/show
    
    BUG=32277
    TEST=none
    
    Review URL: http://codereview.chromium.org/3360006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58571 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/file_system/file_system_operation_client.h
+++ chrome/browser/file_system/file_system_operation_client.h
@@ -3,18 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_FILE_SYSTEM_FILE_SYSTEM_BACKEND_CLIENT_H_
-#define CHROME_BROWSER_FILE_SYSTEM_FILE_SYSTEM_BACKEND_CLIENT_H_
+#ifndef CHROME_BROWSER_FILE_SYSTEM_FILE_SYSTEM_OPERATION_CLIENT_H_
+#define CHROME_BROWSER_FILE_SYSTEM_FILE_SYSTEM_OPERATION_CLIENT_H_
 
 #include <vector>
 
 #include "base/file_util_proxy.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFileError.h"
 
-// Interface for client of FileSystemBackend.
-class FileSystemBackendClient {
+// Interface for client of FileSystemOperation.
+class FileSystemOperationClient {
  public:
-  virtual ~FileSystemBackendClient() {}
+  virtual ~FileSystemOperationClient() {}
 
   virtual void DidFail(WebKit::WebFileError status, int request_id) = 0;
 
@@ -30,4 +30,5 @@ class FileSystemBackendClient {
       int request_id) = 0;
 };
 
-#endif  // CHROME_BROWSER_FILE_SYSTEM_FILE_SYSTEM_BACKEND_CLIENT_H_
+#endif  // CHROME_BROWSER_FILE_SYSTEM_FILE_SYSTEM_OPERATION_CLIENT_H_
+
