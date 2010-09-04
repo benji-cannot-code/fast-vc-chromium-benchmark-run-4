e2c39f27071f3ff306ce1683b7496971f8d7d03a
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-04 04:12:55 +0000

    Enable SafeBrowsingDatabaseNew.
    Convert from SafeBrowsingDatabaseBloom to SafeBrowsingDatabaseNew for
    default database.
    
    Shift histograms for SB2.AddPrefixes and SB2.SubPrefixes to the store,
    because the database can't see the sub count.
    
    Enable SB2.FailedUpdate for SafeBrowsingStoreSqlite to match bloom
    code.
    
    BUG=28647
    TEST=none
    
    Review URL: http://codereview.chromium.org/3320009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58578 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_store_sqlite.cc
+++ chrome/browser/safe_browsing/safe_browsing_store_sqlite.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/file_util.h"
+#include "base/histogram.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/common/sqlite_compiled_statement.h"
@@ -630,9 +631,13 @@ bool SafeBrowsingStoreSqlite::DoUpdate(
   int rv = insert_transaction_->Commit();
   if (rv != SQLITE_OK) {
     NOTREACHED() << "SafeBrowsing update transaction failed to commit.";
-    // UMA_HISTOGRAM_COUNTS("SB2.FailedUpdate", 1);
+    UMA_HISTOGRAM_COUNTS("SB2.FailedUpdate", 1);
     return false;
   }
+
+  // Record counts before swapping to caller.
+  UMA_HISTOGRAM_COUNTS("SB2.AddPrefixes", add_prefixes.size());
+  UMA_HISTOGRAM_COUNTS("SB2.SubPrefixes", sub_prefixes.size());
 
   add_prefixes_result->swap(add_prefixes);
   add_full_hashes_result->swap(add_full_hashes);
