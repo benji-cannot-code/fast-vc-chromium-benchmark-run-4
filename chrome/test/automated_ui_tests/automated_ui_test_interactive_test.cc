be9315670912dc184d86c886aa1a77d461410860
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-04 01:36:56 +0000

    Revert 58563 - GTTF: Make WaitForTabCountToBecome automation call not Sleep.
    (unfortunately, Mac & Windows started failing tab perf tests on this change)
    
    Sleeping is an unreliable method to wait for things.
    Instead, we set up an observer.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3300011
    
    TBR=phajdan.jr@chromium.org
    Review URL: http://codereview.chromium.org/3344009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58566 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automated_ui_tests/automated_ui_test_interactive_test.cc
+++ chrome/test/automated_ui_tests/automated_ui_test_interactive_test.cc
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 TEST_F(AutomatedUITestBase, DragOut) {
   NewTab();
   NewTab();
-  ASSERT_TRUE(active_browser()->WaitForTabCountToBecome(3));
+  ASSERT_TRUE(active_browser()->
+      WaitForTabCountToBecome(3, action_max_timeout_ms()));
   PlatformThread::Sleep(sleep_timeout_ms());
   ASSERT_TRUE(DragTabOut());
   int window_count;
@@ -22,7 +23,8 @@ TEST_F(AutomatedUITestBase, DragOut) {
 TEST_F(AutomatedUITestBase, DragLeftRight) {
   NewTab();
   NewTab();
-  ASSERT_TRUE(active_browser()->WaitForTabCountToBecome(3));
+  ASSERT_TRUE(active_browser()->
+      WaitForTabCountToBecome(3, action_max_timeout_ms()));
   // TODO(phajdan.jr): We need a WaitForTabstripAnimationsToEnd() function.
   // Every sleep in this file should be replaced with it.
   PlatformThread::Sleep(sleep_timeout_ms());
