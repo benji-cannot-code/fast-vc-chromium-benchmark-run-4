b7212e770f262e78d2c0ad6a4258a161c24b2202
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-06 14:38:31 +0000

    WebCore:
    2010-09-06  Xan Lopez  <xlopez@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] Provide Keyboard Events to Windowless plugins
            https://bugs.webkit.org/show_bug.cgi?id=44613
    
            Fix keyboard event delivery for windowless plugins.
    
            Based on a patch by Bharathwaaj.
    
            * plugins/PluginView.cpp:
            (WebCore::PluginView::handleEvent): also take into account
            focused{in,out} events
            * plugins/gtk/PluginViewGtk.cpp:
            (WebCore::PluginView::handleKeyboardEvent): we want the keyval
            here, not the hardware keycode
            (WebCore::PluginView::handleMouseEvent): focus the pluginview on mouse events
    
    LayoutTests:
    
    2010-09-06  Xan Lopez  <xlopez@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] Provide Keyboard Events to Windowless plugins
            https://bugs.webkit.org/show_bug.cgi?id=44613
    
            Unskip passing test.
    
            * platform/gtk/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/plugins/gtk/PluginViewGtk.cpp
+++ third_party/WebKit/WebCore/plugins/gtk/PluginViewGtk.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "DocumentLoader.h"
 #include "Element.h"
+#include "FocusController.h"
 #include "FrameLoader.h"
 #include "FrameLoadRequest.h"
 #include "FrameTree.h"
@@ -308,7 +309,7 @@ void PluginView::handleKeyboardEvent(KeyboardEvent* event)
     xEvent.xkey.subwindow = 0; // we have no child window
     xEvent.xkey.time = event->timeStamp();
     xEvent.xkey.state = gdkEvent->state; // GdkModifierType mirrors xlib state masks
-    xEvent.xkey.keycode = gdkEvent->hardware_keycode;
+    xEvent.xkey.keycode = gdkEvent->keyval;
     xEvent.xkey.same_screen = true;
 
     // NOTE: As the XEvents sent to the plug-in are synthesized and there is not a native window
@@ -422,6 +423,12 @@ void PluginView::handleMouseEvent(MouseEvent* event)
 
     if (m_isWindowed)
         return;
+
+    if (event->type() == eventNames().mousedownEvent) {
+        if (Page* page = m_parentFrame->page())
+            page->focusController()->setActive(true);
+        focusPluginElement();
+    }
 
     NPEvent xEvent;
 #if defined(XP_UNIX)
