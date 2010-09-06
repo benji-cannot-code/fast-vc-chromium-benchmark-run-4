ef3bbdd38ba7e7e656cf2623d5c335bb256f6c36
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-06 17:19:48 +0000

    2010-09-06  Adam Barth  <abarth@webkit.org>
            [reviewtool] Tweak the ok button to cancel the comment if the comment
            is empty.  Previously we would get into a bad state where a line had a
            comment but there was no longer any way to access it.
    
            * code-review.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/BugsSite/code-review.js
+++ third_party/WebKit/BugsSite/code-review.js
@@ -185,16 +185,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     $(document.body).prepend('<div id="comment_form" class="inactive"><div class="winter"></div><div class="lightbox"><iframe src="attachment.cgi?id=' + attachment_id + '&action=reviewform"></iframe></div></div>');
   });
 
-  $('.comment .cancel').live('click', function() {
+  function cancelComment() {
     var line_id = $(this).parentsUntil('.comment').parent().find('textarea').attr('data-comment-for');
     var line = $('#' + line_id)
     findCommentBlockFor(line).remove();
     line.removeAttr('data-has-comment');
     trimCommentContextToBefore(line);
-  });
+  }
+
+  $('.comment .cancel').live('click', cancelComment);
 
   $('.comment .ok').live('click', function() {
     var comment_textarea = $(this).parentsUntil('.comment').parent().find('textarea');
+    if (comment_textarea.val().trim() == '') {
+      cancelComment.call(this);
+      return;
+    }
     var line_id = comment_textarea.attr('data-comment-for');
     var line = $('#' + line_id)
     findCommentBlockFor(line).hide().after($('<div class="frozen-comment"></div>').text(comment_textarea.val()));
