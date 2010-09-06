a743f909d281163d486dfd880a7257ad13a56a48
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-06 02:09:14 +0000

    Update last prompted URL when user choosed "yes" or "no".
    BUG=54274
    TEST=GoogleURLTrackerTest passes
    
    Review URL: http://codereview.chromium.org/3330008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58619 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google/google_url_tracker.h
+++ chrome/browser/google/google_url_tracker.h
@@ -86,6 +86,7 @@ class GoogleURLTracker : public URLFetcher::Delegate,
 
   // Methods called from InfoBar delegate.
   void AcceptGoogleURL(const GURL& google_url);
+  void CancelGoogleURL(const GURL& google_url);
   void InfoBarClosed();
   void RedoSearch();
 
