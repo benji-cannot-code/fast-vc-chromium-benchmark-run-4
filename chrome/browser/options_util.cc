d2d8f463c397e6b91ccb11208f1ed39d00aa1937
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-07 20:43:31 +0000

    Reset the desktop notifications settings to default.
    When the user clicks in "Reset to default" button on the Preferences/Options dialog, we should reset
    the desktop notifications settings to the default default option, like we already do for the other content settings.
    
    BUG=49656
    TEST=see bug.
    
    Review URL: http://codereview.chromium.org/3332007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58754 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/options_util.cc
+++ chrome/browser/options_util.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/host_content_settings_map.h"
 #include "chrome/browser/host_zoom_map.h"
 #include "chrome/browser/metrics/metrics_service.h"
+#include "chrome/browser/notifications/desktop_notification_service.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profile.h"
 #include "chrome/common/pref_names.h"
@@ -74,6 +75,7 @@ void OptionsUtil::ResetToDefaults(Profile* profile) {
   profile->GetHostContentSettingsMap()->ResetToDefaults();
   profile->GetGeolocationContentSettingsMap()->ResetToDefault();
   profile->GetHostZoomMap()->ResetToDefaults();
+  profile->GetDesktopNotificationService()->ResetToDefaultContentSetting();
   for (size_t i = 0; i < arraysize(kUserPrefs); ++i)
     prefs->ClearPref(kUserPrefs[i]);
 
