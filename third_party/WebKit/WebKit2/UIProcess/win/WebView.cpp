b598f6f609587e0ec4228e084e505807cff93a55
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-07 21:07:22 +0000

    Another windows fix.
    * UIProcess/win/WebView.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66907 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
+++ third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ChunkedUpdateDrawingAreaProxy.h"
 #include "RunLoop.h"
+#include "WebEditCommandProxy.h"
 #include "WebEventFactory.h"
 #include "WebPageNamespace.h"
 #include "WebPageProxy.h"
