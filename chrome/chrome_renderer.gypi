023752022a1785c44fa1f6e344543134e2b62b57
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-07 23:09:13 +0000

    Add a VideoDecodeContext that provides resources for a VideoDecodeEngine
    Also define a Gles2VideoDecodeContext to be used in the chrome renderer.
    
    BUG=53714
    TEST=none
    
    Review URL: http://codereview.chromium.org/3233003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58772 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_renderer.gypi
+++ chrome/chrome_renderer.gypi
@@ -70,6 +70,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'renderer/loadtimes_extension_bindings.cc',
         'renderer/media/audio_renderer_impl.cc',
         'renderer/media/audio_renderer_impl.h',
+        'renderer/media/gles2_video_decode_context.cc',
+        'renderer/media/gles2_video_decode_context.h',
         'renderer/media/ipc_video_decoder.cc',
         'renderer/media/ipc_video_decoder.h',
         'renderer/media/ipc_video_renderer.cc',
---
+++ chrome/renderer/media/gles2_video_decode_context.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/renderer/media/gles2_video_decode_context.h"
+
+Gles2VideoDecodeContext::Gles2VideoDecodeContext(
+    StorageType type, WebKit::WebGLES2Context* context)
+    : type_(type), context_(context) {
+}
+
+Gles2VideoDecodeContext::~Gles2VideoDecodeContext() {
+  // TODO(hclam): Implement.
+}
+
+void* Gles2VideoDecodeContext::GetDevice() {
+  // This decode context is used inside the renderer and so hardware decoder
+  // device handler should be used.
+  return NULL;
+}
+
+void Gles2VideoDecodeContext::AllocateVideoFrames(
+    int n, size_t width, size_t height, AllocationCompleteCallback* callback) {
+  // TODO(hclam): Implement.
+}
+
+void Gles2VideoDecodeContext::ReleaseVideoFrames(int n,
+                                                 media::VideoFrame* frames) {
+  // TODO(hclam): Implement.
+}
+
+void Gles2VideoDecodeContext::Destroy(DestructionCompleteCallback* callback) {
+  // TODO(hclam): Implement.
+}
