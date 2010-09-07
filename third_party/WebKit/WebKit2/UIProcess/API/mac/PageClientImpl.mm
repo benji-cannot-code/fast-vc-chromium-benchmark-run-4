50a850aac8760bb4db716e6835079bf47dac06dc
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-07 19:41:24 +0000

    Fix incorrect parameter name.
    Reviewed by Darin Adler.
    
    * UIProcess/API/mac/PageClientImpl.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/mac/PageClientImpl.mm
+++ third_party/WebKit/WebKit2/UIProcess/API/mac/PageClientImpl.mm
@@ -68,8 +68,8 @@ using namespace WebCore;
 
 @interface WebEditorUndoTargetObjC : NSObject
 
-- (void)undoEditing:(id)arg;
-- (void)redoEditing:(id)arg;
+- (void)undoEditing:(id)sender;
+- (void)redoEditing:(id)sender;
 
 @end
 
