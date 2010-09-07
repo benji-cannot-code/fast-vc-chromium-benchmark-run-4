9dc407e08a4d05e39f0eab8d9e75cc99ac75625e
jshin@chromium.org <jshin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-07 18:40:29 +0000

    Change ICU_UTIL_DATA_IMPL on Linux to match the reality.
    We've bundled the ICU data file for 'ages' on Linux, but ICU_UTIL_DATA_IMPL on Linux
    is still set to ICU_UTIL_DATA_FILE.
    
    Apparently, this hasn't been noticed because u_setDataDirectory and 
    udata_setFileAccess just set the global variable pointing to the data file
    location. This is not totally free because u_setDataDirectory does some
    locking/unlocking.
    
    BUG=NONE
    TEST=Builds go through on Linux and binaries (chrome, test, etc) run just fine.
    
    Review URL: http://codereview.chromium.org/3329012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58725 0039d316-1c4b-4281-b951-d872f2087c98

--- base/i18n/icu_util.cc
+++ base/i18n/icu_util.cc
@@ -30,10 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #define ICU_UTIL_DATA_IMPL ICU_UTIL_DATA_SHARED
-#elif defined(OS_MACOSX)
+#else
 #define ICU_UTIL_DATA_IMPL ICU_UTIL_DATA_STATIC
-#elif defined(OS_POSIX)
-#define ICU_UTIL_DATA_IMPL ICU_UTIL_DATA_FILE
 #endif
 
 #endif  // ICU_UTIL_DATA_IMPL
@@ -78,7 +76,7 @@ bool Initialize() {
   udata_setCommonData(reinterpret_cast<void*>(addr), &err);
   return err == U_ZERO_ERROR;
 #elif (ICU_UTIL_DATA_IMPL == ICU_UTIL_DATA_STATIC)
-  // Mac bundles the ICU data in.
+  // Mac/Linux bundle the ICU data in.
   return true;
 #elif (ICU_UTIL_DATA_IMPL == ICU_UTIL_DATA_FILE)
   // For now, expect the data file to be alongside the executable.
