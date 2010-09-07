81aaba4c74bf91720f7b8dd45f3bb532e6fcc322
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-07 21:57:48 +0000

    2010-09-07  Abhishek Arya  <inferno@chromium.org>
            Reviewed by Darin Adler.
    
            Remove redundant bounds check in originalText(). Add bounds check
            to previousCharacter(). No need of start() > 0 check since m_start
            is unsigned and we already do start() null check inside function.
            https://bugs.webkit.org/show_bug.cgi?id=45303
    
            Test: fast/text/one-letter-transform-crash.html
    
            * rendering/RenderTextFragment.cpp:
            (WebCore::RenderTextFragment::originalText):
            (WebCore::RenderTextFragment::previousCharacter):
    2010-09-07  Abhishek Arya  <inferno@chromium.org>
    
            Reviewed by Darin Adler.
    
            Tests that we dont hit assert i < m_length when trying to read
            previousCharacter for text fragments.
            https://bugs.webkit.org/show_bug.cgi?id=45303
    
            * fast/text/one-letter-transform-crash-expected.txt: Added.
            * fast/text/one-letter-transform-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66911 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTextFragment.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTextFragment.cpp
@@ -49,9 +49,9 @@ PassRefPtr<StringImpl> RenderTextFragment::originalText() const
 {
     Node* e = node();
     RefPtr<StringImpl> result = ((e && e->isTextNode()) ? static_cast<Text*>(e)->dataImpl() : contentString());
-    if (result && (start() > 0 || start() < result->length()))
-        result = result->substring(start(), end());
-    return result.release();
+    if (!result)
+        return 0;
+    return result->substring(start(), end());
 }
 
 void RenderTextFragment::destroy()
@@ -82,7 +82,7 @@ UChar RenderTextFragment::previousCharacter() const
     if (start()) {
         Node* e = node();
         StringImpl*  original = ((e && e->isTextNode()) ? static_cast<Text*>(e)->dataImpl() : contentString());
-        if (original)
+        if (original && start() <= original->length())
             return (*original)[start() - 1];
     }
 
