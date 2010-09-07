b08d1fe646b3d633f5c24e691b9a6196f2c08676
mal@chromium.org <mal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-07 04:38:47 +0000

    Revert 58641 - Add user metrics for nonsandboxed plugin infobar.
    BUG=52341
    TEST=none
    
    Review URL: http://codereview.chromium.org/3303003
    
    TBR=bauerb@chromium.org
    Review URL: http://codereview.chromium.org/3300015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58663 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/blocked_plugin_manager.h
+++ chrome/browser/blocked_plugin_manager.h
@@ -28,7 +28,6 @@ class BlockedPluginManager : public RenderViewHostDelegate::BlockedPlugin,
   virtual SkBitmap* GetIcon() const;
   virtual bool Accept();
   virtual bool Cancel();
-  virtual void InfoBarDismissed();
   virtual bool LinkClicked(WindowOpenDisposition disposition);
 
  private:
