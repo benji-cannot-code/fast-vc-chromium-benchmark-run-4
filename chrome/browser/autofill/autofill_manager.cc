c344cbfc3f40c46556ed23fd22bc546eb8c125aa
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-08 04:53:42 +0000

    Get rid of FormStructure::ConvertToFormData()
    BUG=none
    TEST=unit_tests --gtest_filter=FormStructureTest.SourceURL
    
    Review URL: http://codereview.chromium.org/3309010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58788 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager.cc
+++ chrome/browser/autofill/autofill_manager.cc
@@ -88,7 +88,7 @@ void RemoveDuplicateElements(
 }
 
 bool FormIsHTTPS(FormStructure* form) {
-  return form->ConvertToFormData().origin.SchemeIs(chrome::kHttpsScheme);
+  return form->source_url().SchemeIs(chrome::kHttpsScheme);
 }
 
 }  // namespace
