a7cc8ee7e9a8aba25e6cc7ee72d8aab170f6493c
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-08 22:35:30 +0000

    Fix heapcheck leaks introduced by 58859.
    TEST=heapcheck bot goes green
    
    TBR=raz@chromium.org
    
    Review URL: http://codereview.chromium.org/3361011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58883 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/clear_data_command_unittest.cc
+++ chrome/browser/sync/engine/clear_data_command_unittest.cc
@@ -67,10 +67,11 @@ TEST_F(ClearDataCommandTest, ClearDataCommandExpectFailed) {
   ASSERT_TRUE(dir.good());
 
   ConfigureMockServerConnection();
-  SyncerEventChannel* channel = new SyncerEventChannel();
-  ClearEventHandler* handler = new ClearEventHandler();
-  channel->AddObserver(handler);
-  ScopedSessionContextSyncerEventChannel s_channel(context(), channel);
+  scoped_ptr<SyncerEventChannel> channel(new SyncerEventChannel());
+  scoped_ptr<ClearEventHandler> handler(new ClearEventHandler());
+  scoped_ptr<browser_sync::ChannelHookup<SyncerEvent> > hookup(
+      channel.get()->AddObserver(handler.get()));
+  ScopedSessionContextSyncerEventChannel s_channel(context(), channel.get());
 
   dir->set_store_birthday(mock_server()->store_birthday());
   mock_server()->SetClearUserDataResponseStatus(sync_pb::RETRIABLE_ERROR);
@@ -94,9 +95,9 @@ TEST_F(ClearDataCommandTest, ClearDataCommandExpectFailed) {
   const sync_pb::ClientToServerMessage& r = mock_server()->last_request();
   EXPECT_TRUE(r.has_clear_user_data());
 
-  EXPECT_TRUE(handler->ReceievedClearFailedEvent());
+  EXPECT_TRUE(handler.get()->ReceievedClearFailedEvent());
 
-  EXPECT_FALSE(handler->ReceievedClearSuccessEvent());
+  EXPECT_FALSE(handler.get()->ReceievedClearSuccessEvent());
   EXPECT_FALSE(on_should_stop_syncing_permanently_called_);
 }
 
@@ -105,10 +106,11 @@ TEST_F(ClearDataCommandTest, ClearDataCommandExpectSucess) {
   ASSERT_TRUE(dir.good());
 
   ConfigureMockServerConnection();
-  SyncerEventChannel* channel = new SyncerEventChannel();
-  ClearEventHandler* handler = new ClearEventHandler();
-  channel->AddObserver(handler);
-  ScopedSessionContextSyncerEventChannel s_channel(context(), channel);
+  scoped_ptr<SyncerEventChannel> channel(new SyncerEventChannel());
+  scoped_ptr<ClearEventHandler> handler(new ClearEventHandler());
+  scoped_ptr<browser_sync::ChannelHookup<SyncerEvent> > hookup(
+      channel.get()->AddObserver(handler.get()));
+  ScopedSessionContextSyncerEventChannel s_channel(context(), channel.get());
 
   dir->set_store_birthday(mock_server()->store_birthday());
   mock_server()->SetClearUserDataResponseStatus(sync_pb::SUCCESS);
@@ -121,7 +123,7 @@ TEST_F(ClearDataCommandTest, ClearDataCommandExpectSucess) {
   const sync_pb::ClientToServerMessage& r = mock_server()->last_request();
   EXPECT_TRUE(r.has_clear_user_data());
 
-  EXPECT_TRUE(handler->ReceievedClearSuccessEvent());
+  EXPECT_TRUE(handler.get()->ReceievedClearSuccessEvent());
   EXPECT_TRUE(on_should_stop_syncing_permanently_called_);
 
   EXPECT_FALSE(handler->ReceievedClearFailedEvent());
