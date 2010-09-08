da990ca5f44b48dde4a060c16e8ba8253bf43f76
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-08 08:43:52 +0000

    Don't use anonymous namespace in header files. http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml#Namespaces
    BUG=none
    TEST=compile and run manually
    
    Review URL: http://codereview.chromium.org/3293014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58799 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dom_ui/language_options_util.h
+++ chrome/browser/chromeos/dom_ui/language_options_util.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class ListValue;
 
-namespace {
+namespace chromeos {
 
 // Returns an i18n-content value corresponding to |preference|.
 template <typename T>
@@ -48,18 +48,8 @@ std::string GetTemplateDataMaxName(const T& preference, const char* prefix) {
 // Creates a Value object from the given value. Here we use function
 // overloading to handle string and integer preferences in
 // CreateMultipleChoiceList.
-Value* CreateValue(const char* in_value) {
-  return Value::CreateStringValue(in_value);
-}
-
-// See comments above.
-Value* CreateValue(int in_value) {
-  return Value::CreateIntegerValue(in_value);
-}
-
-}  // namespace
-
-namespace chromeos {
+Value* CreateValue(const char* in_value);
+Value* CreateValue(int in_value);
 
 // Creates a multiple choice list from the given preference.
 template <typename T>
