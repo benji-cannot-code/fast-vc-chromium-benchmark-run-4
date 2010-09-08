afad1728e58e2581a38a73cb8143826461220663
loki@webkit.org <loki@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-08 22:43:25 +0000

    2010-09-08  Gabor Loki  <loki@webkit.org>
            Reviewed by Andreas Kling.
    
            Fix increases required alignment of target type warning on ARM
            https://bugs.webkit.org/show_bug.cgi?id=45301
    
            No new tests needed.
    
            * bindings/js/SerializedScriptValue.cpp:
            (WebCore::CloneDeserializer::readLittleEndian):
            (WebCore::CloneDeserializer::readString):
            * plugins/PluginDatabase.cpp:
            (WebCore::readTime):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67028 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/SerializedScriptValue.cpp
+++ third_party/WebKit/WebCore/bindings/js/SerializedScriptValue.cpp
@@ -801,7 +801,7 @@ private:
         if (sizeof(T) == 1)
             value = *ptr++;
         else {
-            value = *reinterpret_cast<const T*>(ptr);
+            value = *reinterpret_cast_ptr<const T*>(ptr);
             ptr += sizeof(T);
         }
         return true;
@@ -889,7 +889,7 @@ private:
             return false;
 
 #if ASSUME_LITTLE_ENDIAN
-        str = UString(reinterpret_cast<const UChar*>(ptr), length);
+        str = UString(reinterpret_cast_ptr<const UChar*>(ptr), length);
         ptr += length * sizeof(UChar);
 #else
         Vector<UChar> buffer;
