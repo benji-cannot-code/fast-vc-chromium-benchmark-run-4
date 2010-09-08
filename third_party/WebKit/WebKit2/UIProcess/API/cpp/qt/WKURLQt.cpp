d9472b53507b85e61fa2d701a767233f01601000
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-08 04:58:34 +0000

    2010-09-07  Kinuko Yasuda  <kinuko@chromium.org>
            Unreviewed, another Qt release build fix attempt.
    
            * UIProcess/API/cpp/qt/WKURLQt.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66950 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/cpp/qt/WKURLQt.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/cpp/qt/WKURLQt.cpp
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/RefPtr.h>
 #include <wtf/text/WTFString.h>
 
+using namespace WebKit;
+
 WKURLRef WKURLCreateWithQUrl(const QUrl& qURL)
 {
     WTF::String urlString(qURL.toString());
