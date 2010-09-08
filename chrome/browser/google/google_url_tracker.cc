eac8950eba93b437090819b7333a68593bdaf8d2
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-08 23:21:18 +0000

    Add a command line switch "--disable-background-networking", to be used in benchmarking when unexpected background networking can cause undesired vairance.
    The following systems are disabled via this flag:
       - IntranetRedirectDetector (requests randomURLs 2-5s after startup)
       - GoogleUrlTracker (searchdomaincheck)
       - SafeBrowsing updater
       - Extension updater
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3312014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58892 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google/google_url_tracker.cc
+++ chrome/browser/google/google_url_tracker.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "app/l10n_util.h"
+#include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/infobar_delegate.h"
 #include "chrome/browser/tab_contents/navigation_controller.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/net/url_fetcher_protect.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/pref_names.h"
@@ -187,6 +189,10 @@ void GoogleURLTracker::StartFetchIfDesirable() {
   // various members here for more detail on exactly what the conditions are.
   if (in_startup_sleep_ || already_fetched_ || !need_to_fetch_ ||
       !request_context_available_)
+    return;
+
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kDisableBackgroundNetworking))
     return;
 
   already_fetched_ = true;
