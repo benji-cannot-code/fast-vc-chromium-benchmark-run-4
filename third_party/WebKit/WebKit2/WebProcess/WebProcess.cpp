c255a7178827812bb99dbced6be61e18dfd6282d
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-08 21:27:10 +0000

    Add a way to cause the web process to crash at a random time
    Setting the WEBKIT2_CRASH_WEB_PROCESS_RANDOMLY environment variable
    will cause the web process to crash at a random point up to 3 minutes
    after launching.
    
    Fixes <http://webkit.org/b/43058> <rdar://problem/8240150>
    
    Reviewed by Darin Adler.
    
    * WebProcess/WebProcess.cpp:
    (WebKit::sleep): Added an implementation of this function for Windows.
    It just calls through to ::Sleep.
    (WebKit::randomCrashThread): Added. Sleeps for a random amount of time
    up to 3 minutes, then crashes.
    (WebKit::startRandomCrashThreadIfRequested): Added. Starts the crash
    thread if the WEBKIT2_CRASH_WEB_PROCESS_RANDOMLY environment variable
    is set.
    (WebKit::WebProcess::initialize): Added a call to
    startRandomCrashThreadIfRequested.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67016 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebProcess.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebProcess.cpp
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/SchemeRegistry.h>
 #include <WebCore/Settings.h>
 #include <wtf/PassRefPtr.h>
+#include <wtf/RandomNumber.h>
 
 #if PLATFORM(MAC)
 #include "MachPort.h"
@@ -55,6 +56,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using namespace WebCore;
 
 namespace WebKit {
+
+#if OS(WINDOWS)
+static void sleep(unsigned seconds)
+{
+    ::Sleep(seconds * 1000);
+}
+#endif
+
+static void* randomCrashThread(void*)
+{
+    // This delay was chosen semi-arbitrarily. We want the crash to happen somewhat quickly to
+    // enable useful stress testing, but not so quickly that the web process will always crash soon
+    // after launch.
+    static const unsigned maximumRandomCrashDelay = 180;
+
+    sleep(randomNumber() * maximumRandomCrashDelay);
+    CRASH();
+    return 0;
+}
+
+static void startRandomCrashThreadIfRequested()
+{
+    if (!getenv("WEBKIT2_CRASH_WEB_PROCESS_RANDOMLY"))
+        return;
+    createThread(randomCrashThread, 0, "WebKit2: Random Crash Thread");
+}
 
 WebProcess& WebProcess::shared()
 {
@@ -82,6 +109,8 @@ void WebProcess::initialize(CoreIPC::Connection::Identifier serverIdentifier, Ru
     m_connection->open();
 
     m_runLoop = runLoop;
+
+    startRandomCrashThreadIfRequested();
 }
 
 #if ENABLE(WEB_PROCESS_SANDBOX)
