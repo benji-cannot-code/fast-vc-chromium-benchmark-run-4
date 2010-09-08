d46ceca30ff1de8f58d70e06ef98e90258b355da
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-08 15:18:36 +0000

    2010-09-08  Sergio Villar Senin  <svillar@igalia.com>
            Reviewed by Adam Barth.
    
            Errors in LayoutTests/http/tests/security/xss-DENIED-mime-type-execute-as-html.html
            https://bugs.webkit.org/show_bug.cgi?id=45377
    
            The test passes a MIME type as argument of a URL. A PHP code uses
            that MIME type to fill the Content-Type of the response.  If the
            MIME type is application/rss+xml the php decodes the '+' as a
            blank space and thus the returned content type is incorrectly set
            to "rss xml" instead of "rss+xml".
    
            * http/tests/security/resources/send-mime-types.php:
            * http/tests/security/xss-DENIED-mime-type-execute-as-html.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@66985 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/security/resources/send-mime-types.php
+++ third_party/WebKit/LayoutTests/http/tests/security/resources/send-mime-types.php
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-    $mime_type = urldecode($_GET["mt"]);
+    $mime_type = $_GET["mt"];
     
     header("Content-Type: ".$mime_type);
 
