77489f08e6374d82384bcbc03ee43a66661218e1
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-08 23:43:38 +0000

    Revert 52870 - Revert 52740 - Always show the 'Control sync from Google Dashboard' link on the sync options dialog.
    BUG=48844
    TEST=open Personal Stuff on any platform, see dashboard link.
    
    Review URL: http://codereview.chromium.org/2827055
    
    TBR=tim@chromium.org
    Review URL: http://codereview.chromium.org/2881021
    
    TBR=tim@chromium.org
    Review URL: http://codereview.chromium.org/3364012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58894 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/preferences_window_controller.mm
+++ chrome/browser/cocoa/preferences_window_controller.mm
@@ -1856,10 +1856,6 @@ const int kDisabledIndex = 1;
     [syncStatus_ setBackgroundColor:syncStatusNoErrorBackgroundColor_];
     [syncLinkCell setBackgroundColor:syncLinkNoErrorBackgroundColor_];
   }
-
-  if (!CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kShowPrivacyDashboardLink))
-    [privacyDashboardLink_ setHidden:YES];
 }
 
 // Show the preferences window.
