8ae35167243363ea890e70e899b478975cf9eff6
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-09 15:03:01 +0000

    Reviewed by Martin Robinson.
            [GTK] testmimehandling falsely succeeds testing Ogg mime type
            https://bugs.webkit.org/show_bug.cgi?id=45349
    
            Advertize audio/x-vorbis+ogg so MediaDocuments loading local ogg
            files work as well.
    
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::mimeTypeCache):
    
    WebKit/gtk:
    
            Reviewed by Martin Robinson.
    
            [GTK] testmimehandling falsely succeeds testing Ogg mime type
            https://bugs.webkit.org/show_bug.cgi?id=45349
    
            Make this actually test an ogg file request instead of pdf and
            changed the expected result to match libsoup content-sniffing
            result in the case of ogg/vorbis file.
    
            * tests/testmimehandling.c:
            (mime_type_policy_decision_requested_cb):
            (test_mime_ogg):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -1268,6 +1268,7 @@ static HashSet<String> mimeTypeCache()
 
                 if (g_str_equal(name, "audio/x-vorbis")) {
                     cache.add(String("audio/ogg"));
+                    cache.add(String("audio/x-vorbis+ogg"));
                     cached = true;
                 }
 
