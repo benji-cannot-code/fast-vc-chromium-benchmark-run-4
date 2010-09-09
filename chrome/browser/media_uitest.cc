ea02c5337fb6913ee2f2a26a9ec9967504564c92
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-09 18:21:53 +0000

    Disables VideoBearTheora as crashes.
    BUG=54939
    TEST=none
    Review URL: http://codereview.chromium.org/3333023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58968 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_uitest.cc
+++ chrome/browser/media_uitest.cc
@@ -49,7 +49,8 @@ class MediaTest : public UITest {
   }
 };
 
-TEST_F(MediaTest, VideoBearTheora) {
+// Crashes, see http://crbug.com/54939
+TEST_F(MediaTest, DISABLED_VideoBearTheora) {
   PlayVideo("bear.ogv");
 }
 
