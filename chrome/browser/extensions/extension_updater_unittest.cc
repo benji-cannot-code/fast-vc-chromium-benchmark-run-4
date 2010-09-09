70e4c63984523707fe5e8a7efcaeabb3ca128639
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-09 02:43:47 +0000

    Check for updates for a user script only if it has an update URL.
    This is a modified revert of 52461.
    
    BUG=53639
    TEST=unit tests, manually with bettergmail extension
    
    Review URL: http://codereview.chromium.org/3303017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58907 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_updater_unittest.cc
+++ chrome/browser/extensions/extension_updater_unittest.cc
@@ -905,6 +905,9 @@ TEST(ExtensionUpdaterTest, TestManifestFetchesBuilderAddExtension) {
                                false, false, true, false));
   EXPECT_TRUE(builder.GetFetches().empty());
 
+  // TODO(akalin): Test that extensions with empty update URLs
+  // converted from user scripts are rejected.
+
   // Extensions with empty update URLs should have a default one
   // filled in.
   builder.AddPendingExtension(
