bb5ea7e903b20de0742bdfbfb39774eb5a83feba
pam@chromium.org <pam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-09 07:42:35 +0000

    Create a DefaultPrefStore to hold registered application-default preference values.
    This allows notifications to be sent properly when another PrefStore takes control from a default value.
    
    BUG=52719
    TEST=covered by PrefValueStoreTest.* unit tests
    Review URL: http://codereview.chromium.org/3331016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58920 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_url_request_context_unittest.cc
+++ chrome/browser/net/chrome_url_request_context_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/format_macros.h"
 #include "chrome/browser/policy/configuration_policy_pref_store.h"
+#include "chrome/browser/prefs/default_pref_store.h"
 #include "chrome/browser/prefs/pref_value_store.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/testing_pref_service.h"
@@ -159,9 +160,11 @@ TEST(ChromeURLRequestContextTest, CreateProxyConfigTest) {
     SCOPED_TRACE(StringPrintf("Test[%" PRIuS "] %s", i,
                               tests[i].description.c_str()));
     CommandLine command_line(tests[i].command_line);
+    // Only configuration-policy and default prefs are needed.
     PrefService prefs(new TestingPrefService::TestingPrefValueStore(
         new ConfigurationPolicyPrefStore(&command_line, NULL),
-        NULL, NULL, NULL, NULL));  // Only configuration-policy prefs.
+        NULL, NULL, NULL, NULL,
+        new DefaultPrefStore()));
     ChromeURLRequestContextGetter::RegisterUserPrefs(&prefs);
     scoped_ptr<net::ProxyConfig> config(CreateProxyConfig(&prefs));
 
---
+++ chrome/browser/prefs/default_pref_store.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/prefs/default_pref_store.h"
+
+#include "base/values.h"
+
+DefaultPrefStore::DefaultPrefStore() : prefs_(new DictionaryValue()) {
+}
+
+DefaultPrefStore::~DefaultPrefStore() {
+}
+
+DictionaryValue* DefaultPrefStore::prefs() {
+  return prefs_.get();
+}
+
+PrefStore::PrefReadError DefaultPrefStore::ReadPrefs() {
+  return PrefStore::PREF_READ_ERROR_NONE;
+}
