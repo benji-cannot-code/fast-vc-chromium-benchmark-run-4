2b29bc3f002b795a0e77c940aba511d0f2a71be9
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-09 23:44:38 +0000

    2010-09-09  Alexey Marinichev  <amarinichev@chromium.org>
            Reviewed by James Robinson.
    
            [chromium] Disable subpixel rendering in Linux when GPU compositor is active
            https://bugs.webkit.org/show_bug.cgi?id=45087
    
            Covered by all tests that activate the compositor and contain text.
    
            * platform/graphics/chromium/ContentLayerChromium.cpp:
            (WebCore::ContentLayerChromium::updateContents):
            * platform/graphics/chromium/FontLinux.cpp:
            (WebCore::adjustTextRenderMode): Added a check to see if the compositor is active.
            (WebCore::Font::drawGlyphs):
            (WebCore::Font::drawComplexText):
            * platform/graphics/chromium/LayerRendererChromium.cpp:
            (WebCore::LayerRendererChromium::setRootLayerCanvasSize):
            * platform/graphics/chromium/VideoLayerChromium.cpp:
            (WebCore::VideoLayerChromium::updateContents):
            * platform/graphics/skia/ImageBufferSkia.cpp:
            (WebCore::ImageBuffer::ImageBuffer):
            (WebCore::ImageBuffer::clip):
            * platform/graphics/skia/PlatformContextSkia.cpp:
            Made setDrawingToImageBuffer and isDrawingToImageBuffer available to
            Linux; removed all #if OS(WINDOWS) || OS(LINUX).
            (WebCore::PlatformContextSkia::State::State):
            (WebCore::PlatformContextSkia::PlatformContextSkia):
            (WebCore::PlatformContextSkia::isDrawingToImageBuffer):
            (WebCore::PlatformContextSkia::save):
            (WebCore::PlatformContextSkia::beginLayerClippedToImage):
            (WebCore::PlatformContextSkia::restore):
            (WebCore::PlatformContextSkia::applyClipFromImage):
            * platform/graphics/skia/PlatformContextSkia.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67126 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/chromium/FontLinux.cpp
+++ third_party/WebKit/WebCore/platform/graphics/chromium/FontLinux.cpp
@@ -66,13 +66,13 @@ static bool isCanvasMultiLayered(SkCanvas* canvas)
     return !layerIterator.done();
 }
 
-static void adjustTextRenderMode(SkPaint* paint, bool isCanvasMultiLayered)
+static void adjustTextRenderMode(SkPaint* paint, PlatformContextSkia* skiaContext)
 {
     // Our layers only have a single alpha channel. This means that subpixel
     // rendered text cannot be compositied correctly when the layer is
     // collapsed. Therefore, subpixel text is disabled when we are drawing
-    // onto a layer.
-    if (isCanvasMultiLayered)
+    // onto a layer or when the compositor is being used.
+    if (isCanvasMultiLayered(skiaContext->canvas()) || skiaContext->isDrawingToImageBuffer())
         paint->setLCDRenderText(false);
 }
 
@@ -105,14 +105,13 @@ void Font::drawGlyphs(GraphicsContext* gc, const SimpleFontData* font,
 
     SkCanvas* canvas = gc->platformContext()->canvas();
     int textMode = gc->platformContext()->getTextDrawingMode();
-    bool haveMultipleLayers = isCanvasMultiLayered(canvas);
 
     // We draw text up to two times (once for fill, once for stroke).
     if (textMode & cTextFill) {
         SkPaint paint;
         gc->platformContext()->setupPaintForFilling(&paint);
         font->platformData().setupPaint(&paint);
-        adjustTextRenderMode(&paint, haveMultipleLayers);
+        adjustTextRenderMode(&paint, gc->platformContext());
         paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
         paint.setColor(gc->fillColor().rgb());
         canvas->drawPosText(glyphs, numGlyphs << 1, pos, paint);
@@ -125,7 +124,7 @@ void Font::drawGlyphs(GraphicsContext* gc, const SimpleFontData* font,
         SkPaint paint;
         gc->platformContext()->setupPaintForStroking(&paint, 0, 0);
         font->platformData().setupPaint(&paint);
-        adjustTextRenderMode(&paint, haveMultipleLayers);
+        adjustTextRenderMode(&paint, gc->platformContext());
         paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
         paint.setColor(gc->strokeColor().rgb());
 
@@ -647,7 +646,6 @@ void Font::drawComplexText(GraphicsContext* gc, const TextRun& run,
     }
 
     TextRunWalker walker(run, point.x(), this);
-    bool haveMultipleLayers = isCanvasMultiLayered(canvas);
     walker.setWordSpacingAdjustment(wordSpacing());
     walker.setLetterSpacingAdjustment(letterSpacing());
     walker.setPadding(run.padding());
@@ -655,13 +653,13 @@ void Font::drawComplexText(GraphicsContext* gc, const TextRun& run,
     while (walker.nextScriptRun()) {
         if (fill) {
             walker.fontPlatformDataForScriptRun()->setupPaint(&fillPaint);
-            adjustTextRenderMode(&fillPaint, haveMultipleLayers);
+            adjustTextRenderMode(&fillPaint, gc->platformContext());
             canvas->drawPosTextH(walker.glyphs(), walker.length() << 1, walker.xPositions(), point.y(), fillPaint);
         }
 
         if (stroke) {
             walker.fontPlatformDataForScriptRun()->setupPaint(&strokePaint);
-            adjustTextRenderMode(&strokePaint, haveMultipleLayers);
+            adjustTextRenderMode(&strokePaint, gc->platformContext());
             canvas->drawPosTextH(walker.glyphs(), walker.length() << 1, walker.xPositions(), point.y(), strokePaint);
         }
     }
