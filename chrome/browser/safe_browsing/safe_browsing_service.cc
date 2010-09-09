6447a08ffedbf5e440542a2fe8fe7d630ed3bdf3
lzheng@chromium.org <lzheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-09 16:45:49 +0000

    Add database_update_in_progress_ in SafeBrowsingService to indicate if the database is still changing. This could be used in the end to end safebrowsing test so we only verify database after this flag is set to false.
    BUG=none
    TEST=safe_browsing_browsertest.cc
    
    Review URL: http://codereview.chromium.org/3258010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58947 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -56,6 +56,7 @@ SafeBrowsingService::SafeBrowsingService()
       protocol_manager_(NULL),
       enabled_(false),
       update_in_progress_(false),
+      database_update_in_progress_(false),
       closing_database_(false) {
 }
 
@@ -456,7 +457,7 @@ bool SafeBrowsingService::MakeDatabaseAvailable() {
 }
 
 SafeBrowsingDatabase* SafeBrowsingService::GetDatabase() {
-  DCHECK(MessageLoop::current() == safe_browsing_thread_->message_loop());
+  DCHECK_EQ(MessageLoop::current(), safe_browsing_thread_->message_loop());
   if (database_)
     return database_;
 
@@ -526,9 +527,12 @@ void SafeBrowsingService::OnCheckDone(SafeBrowsingCheck* check) {
 }
 
 void SafeBrowsingService::GetAllChunksFromDatabase() {
-  DCHECK(MessageLoop::current() == safe_browsing_thread_->message_loop());
+  DCHECK_EQ(MessageLoop::current(), safe_browsing_thread_->message_loop());
+
   bool database_error = true;
   std::vector<SBListChunkRanges> lists;
+  DCHECK(!database_update_in_progress_);
+  database_update_in_progress_ = true;
   GetDatabase();  // This guarantees that |database_| is non-NULL.
   if (database_->UpdateStarted(&lists)) {
     database_error = false;
@@ -583,7 +587,7 @@ void SafeBrowsingService::DatabaseLoadComplete() {
 void SafeBrowsingService::HandleChunkForDatabase(
     const std::string& list_name,
     SBChunkList* chunks) {
-  DCHECK(MessageLoop::current() == safe_browsing_thread_->message_loop());
+  DCHECK_EQ(MessageLoop::current(), safe_browsing_thread_->message_loop());
   if (chunks) {
     GetDatabase()->InsertChunks(list_name, *chunks);
     delete chunks;
@@ -595,7 +599,7 @@ void SafeBrowsingService::HandleChunkForDatabase(
 
 void SafeBrowsingService::DeleteChunks(
     std::vector<SBChunkDelete>* chunk_deletes) {
-  DCHECK(MessageLoop::current() == safe_browsing_thread_->message_loop());
+  DCHECK_EQ(MessageLoop::current(), safe_browsing_thread_->message_loop());
   if (chunk_deletes) {
     GetDatabase()->DeleteChunks(*chunk_deletes);
     delete chunk_deletes;
@@ -622,8 +626,10 @@ void SafeBrowsingService::NotifyClientBlockingComplete(Client* client,
 }
 
 void SafeBrowsingService::DatabaseUpdateFinished(bool update_succeeded) {
-  DCHECK(MessageLoop::current() == safe_browsing_thread_->message_loop());
+  DCHECK_EQ(MessageLoop::current(), safe_browsing_thread_->message_loop());
   GetDatabase()->UpdateFinished(update_succeeded);
+  DCHECK(database_update_in_progress_);
+  database_update_in_progress_ = false;
 }
 
 void SafeBrowsingService::Start() {
@@ -655,7 +661,7 @@ void SafeBrowsingService::Start() {
 }
 
 void SafeBrowsingService::OnCloseDatabase() {
-  DCHECK(MessageLoop::current() == safe_browsing_thread_->message_loop());
+  DCHECK_EQ(MessageLoop::current(), safe_browsing_thread_->message_loop());
   DCHECK(closing_database_);
 
   // Because |closing_database_| is true, nothing on the IO thread will be
@@ -672,14 +678,14 @@ void SafeBrowsingService::OnCloseDatabase() {
 }
 
 void SafeBrowsingService::OnResetDatabase() {
-  DCHECK(MessageLoop::current() == safe_browsing_thread_->message_loop());
+  DCHECK_EQ(MessageLoop::current(), safe_browsing_thread_->message_loop());
   GetDatabase()->ResetDatabase();
 }
 
 void SafeBrowsingService::CacheHashResults(
   const std::vector<SBPrefix>& prefixes,
   const std::vector<SBFullHashResult>& full_hashes) {
-  DCHECK(MessageLoop::current() == safe_browsing_thread_->message_loop());
+  DCHECK_EQ(MessageLoop::current(), safe_browsing_thread_->message_loop());
   GetDatabase()->CacheHashResults(prefixes, full_hashes);
 }
 
