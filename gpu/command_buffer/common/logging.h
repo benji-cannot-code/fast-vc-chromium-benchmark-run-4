324e739362a1bc4c9883650a515dfc0908f6168f
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-10 18:16:10 +0000

    Removed includion of <iostream> from logging.h.
    So that it does not introduce static initializers into including files.
    
    TEST=try
    BUG=54904
    
    Review URL: http://codereview.chromium.org/3354018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59119 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/common/logging.h
+++ gpu/command_buffer/common/logging.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GPU_COMMAND_BUFFER_COMMON_LOGGING_H_
 
 #include <assert.h>
+#include <stdio.h>
 
-#include <iostream>
+#include <sstream>
+#include <string>
 
 // Windows defines an ERROR macro.
 #ifdef ERROR
@@ -27,8 +29,8 @@ enum LogLevel {
 };
 
 // This is a very simple logger for use in command buffer code. Common and
-// command buffer code cannot be dependent on base. It just outputs the message
-// to stderr.
+// client side command buffer code cannot be dependent on base. It just outputs
+// the message to stderr, flushes and asserts if the error was fatal.
 class Logger {
  public:
   Logger(bool condition, LogLevel level)
@@ -114,8 +116,9 @@ class Logger {
 
   ~Logger() {
     if (!condition_) {
-      std::cerr << std::endl;
-      std::cerr.flush();
+      message_stream_ << std::endl;
+      fputs(message_stream_.str().c_str(), stderr);
+      fflush(stderr);
       if (level_ == FATAL)
         assert(false);
     }
@@ -124,7 +127,7 @@ class Logger {
   template <typename T>
   Logger& operator<<(const T& value) {
     if (!condition_)
-      std::cerr << value;
+      message_stream_ << value;
     return *this;
   }
 
@@ -132,6 +135,7 @@ class Logger {
   Logger(const Logger& logger): condition_(logger.condition_) {
   }
 
+  std::stringstream message_stream_;
   bool condition_;
   LogLevel level_;
 };
