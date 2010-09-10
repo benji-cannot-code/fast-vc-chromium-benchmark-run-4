f5c95a3cb5c1c59490fc64e9e3dd2a51643b4c70
pam@chromium.org <pam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-10 08:25:42 +0000

    Add support for PAC scripts (URL only, no direct data yet) and proxy auto-config to the proxy extension API.
    BUG=48930
    TEST=covered by browser_tests ExtensionAPITest.Proxy*
    Review URL: http://codereview.chromium.org/3348014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59073 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/proxy/individual/test.js
+++ chrome/test/data/extensions/api_test/proxy/individual/test.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // proxy api test
-// browser_tests.exe --gtest_filter=ExtensionApiTest.ProxyIndividual
+// browser_tests.exe --gtest_filter=ExtensionApiTest.ProxyManualIndividual
 
 chrome.test.runTests([
   function setIndividualProxies() {
