4ddcb16e09356a422f17456209837e1ad86253e3
zherczeg@webkit.org <zherczeg@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-10 11:48:21 +0000

    An individual renderer should be assigned to each SVGFE*Element class https://bugs.webkit.org/show_bug.cgi?id=43954
    Reviewed by Nikolas Zimmermann.
    
    Updating the description of the following test (it does not fail anymore):
    
    * platform/mac/svg/dynamic-updates/SVGFEDiffuseLightingElement-lighting-color-css-prop-expected.checksum:
    * platform/mac/svg/dynamic-updates/SVGFEDiffuseLightingElement-lighting-color-css-prop-expected.png:
    * svg/dynamic-updates/SVGFEDiffuseLightingElement-lighting-color-css-prop-expected.txt:
    * svg/dynamic-updates/script-tests/SVGFEDiffuseLightingElement-lighting-color-css-prop.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67189 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/svg/dynamic-updates/script-tests/SVGFEDiffuseLightingElement-lighting-color-css-prop.js
+++ third_party/WebKit/LayoutTests/svg/dynamic-updates/script-tests/SVGFEDiffuseLightingElement-lighting-color-css-prop.js
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // [Name] SVGFEDiffuseLightingElement-lighting-color-css-prop.js
 // [Expected rendering result] A shining circle (performed by diffuse lighting) - and a series of PASS messages
 
-description("Tests dynamic updates of the 'lighting-color' css property of the SVGFEDiffuseLightingElement object (The pixel test currently fails - the image is not changed to yellow)")
+description("Tests dynamic updates of the 'lighting-color' css property of the SVGFEDiffuseLightingElement object");
 createSVGTestCase();
 
 var pointLight = createSVGElement("fePointLight");
