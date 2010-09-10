ebdf40bf62637e63d27a654fe4b3c413a0a98a18
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-10 11:52:20 +0000

    Fix style issues with ConfigDirPolicyProvider.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3331001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59086 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/config_dir_policy_provider.cc
+++ chrome/browser/policy/config_dir_policy_provider.cc
@@ -9,8 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_util.h"
 #include "base/logging.h"
+#include "base/message_loop.h"
+#include "base/task.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
+#include "chrome/browser/chrome_thread.h"
 #include "chrome/common/json_value_serializer.h"
 
 namespace {
@@ -223,7 +226,7 @@ void PolicyDirWatcher::InitWatcher(
     return;
   }
 
-  if (!Watch(loader->config_dir(), loader.get()))
+  if (!watcher_.Watch(loader->config_dir(), loader.get()))
     loader->OnError();
 
   // There might have been changes to the directory in the time between
--- chrome/browser/policy/config_dir_policy_provider_unittest.cc
+++ chrome/browser/policy/config_dir_policy_provider_unittest.cc
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/config_dir_policy_provider.h"
 #include "chrome/browser/policy/mock_configuration_policy_store.h"
 #include "chrome/common/json_value_serializer.h"
-#include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 using testing::Mock;
 
