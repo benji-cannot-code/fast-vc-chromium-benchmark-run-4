66c614167450f907c33673b6729845e35c89b656
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-10 00:41:35 +0000

    2010-09-09  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r67119.
            http://trac.webkit.org/changeset/67119
            https://bugs.webkit.org/show_bug.cgi?id=45505
    
            Extra newlines in results (Requested by tony^work on #webkit).
    
            * platform/chromium-linux/fast/dom/HTMLLinkElement/prefetch-expected.txt: Added.
            * platform/chromium-mac/fast/dom/HTMLLinkElement/prefetch-expected.txt: Added.
            * platform/chromium/test_expectations.txt:
    2010-09-09  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r67119.
            http://trac.webkit.org/changeset/67119
            https://bugs.webkit.org/show_bug.cgi?id=45505
    
            Extra newlines in results (Requested by tony^work on #webkit).
    
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::LayoutTestController):
            (LayoutTestController::reset):
            * DumpRenderTree/chromium/LayoutTestController.h:
            * DumpRenderTree/chromium/TestShell.h:
            * DumpRenderTree/chromium/WebViewHost.cpp:
            (printResponseDescription):
            (printNodeDescription):
            (printRangeDescription):
            (WebViewHost::shouldBeginEditing):
            (WebViewHost::shouldEndEditing):
            (WebViewHost::shouldInsertNode):
            (WebViewHost::shouldChangeSelectedRange):
            (WebViewHost::shouldDeleteRange):
            (WebViewHost::shouldApplyStyle):
            (WebViewHost::didBeginEditing):
            (WebViewHost::didChangeSelection):
            (WebViewHost::didChangeContents):
            (WebViewHost::didEndEditing):
            (WebViewHost::decidePolicyForNavigation):
            (WebViewHost::didCancelClientRedirect):
            (WebViewHost::didStartProvisionalLoad):
            (WebViewHost::didReceiveServerRedirectForProvisionalLoad):
            (WebViewHost::didFailProvisionalLoad):
            (WebViewHost::didCommitProvisionalLoad):
            (WebViewHost::didFinishDocumentLoad):
            (WebViewHost::didHandleOnloadEvents):
            (WebViewHost::didFailLoad):
            (WebViewHost::didFinishLoad):
            (WebViewHost::didChangeLocationWithinPage):
            (WebViewHost::willSendRequest):
            (WebViewHost::didReceiveResponse):
            (WebViewHost::didFinishResourceLoad):
            (WebViewHost::didFailResourceLoad):
            (WebViewHost::didDisplayInsecureContent):
            (WebViewHost::didRunInsecureContent):
            (WebViewHost::printFrameDescription):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67133 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/LayoutTestController.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/LayoutTestController.cpp
@@ -84,7 +84,6 @@ LayoutTestController::LayoutTestController(TestShell* shell)
     bindMethod("dumpBackForwardList", &LayoutTestController::dumpBackForwardList);
     bindMethod("dumpFrameLoadCallbacks", &LayoutTestController::dumpFrameLoadCallbacks);
     bindMethod("dumpResourceLoadCallbacks", &LayoutTestController::dumpResourceLoadCallbacks);
-    bindMethod("dumpResourceResponseMIMETypes", &LayoutTestController::dumpResourceResponseMIMETypes);
     bindMethod("dumpStatusCallbacks", &LayoutTestController::dumpWindowStatusChanges);
     bindMethod("dumpTitleChanges", &LayoutTestController::dumpTitleChanges);
     bindMethod("setAcceptsEditing", &LayoutTestController::setAcceptsEditing);
@@ -287,12 +286,6 @@ void LayoutTestController::dumpResourceLoadCallbacks(const CppArgumentList&, Cpp
     result->setNull();
 }
 
-void LayoutTestController::dumpResourceResponseMIMETypes(const CppArgumentList&, CppVariant* result)
-{
-    m_dumpResourceResponseMIMETypes = true;
-    result->setNull();
-}
-
 void LayoutTestController::dumpChildFrameScrollPositions(const CppArgumentList&, CppVariant* result)
 {
     m_dumpChildFrameScrollPositions = true;
@@ -488,7 +481,6 @@ void LayoutTestController::reset()
     m_dumpEditingCallbacks = false;
     m_dumpFrameLoadCallbacks = false;
     m_dumpResourceLoadCallbacks = false;
-    m_dumpResourceResponseMIMETypes = false;
     m_dumpBackForwardList = false;
     m_dumpChildFrameScrollPositions = false;
     m_dumpChildFramesAsText = false;
