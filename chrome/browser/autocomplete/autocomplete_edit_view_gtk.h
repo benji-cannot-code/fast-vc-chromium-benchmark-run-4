1bdaf56f7ef1c9a5e19bf85b9c5ef291f144114a
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-11 05:20:42 +0000

    Handle FocusSearch() when in forced search mode with leading whitespace.
    BUG=29593
    TEST=browser_tests --gtest_filter=AutocompleteBrowserTest.FocusSearch
    
    Review URL: http://codereview.chromium.org/3280012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59183 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view_gtk.h
+++ chrome/browser/autocomplete/autocomplete_edit_view_gtk.h
@@ -108,6 +108,8 @@ class AutocompleteEditViewGtk : public AutocompleteEditView,
   virtual void SetForcedQuery();
 
   virtual bool IsSelectAll();
+  virtual void GetSelectionBounds(std::wstring::size_type* start,
+                                  std::wstring::size_type* end);
   virtual void SelectAll(bool reversed);
   virtual void RevertAll();
 
--- chrome/browser/autocomplete/autocomplete_edit_view_win.cc
+++ chrome/browser/autocomplete/autocomplete_edit_view_win.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/iat_patch.h"
 #include "base/lazy_instance.h"
 #include "base/ref_counted.h"
+#include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/app/chrome_dll_resource.h"
 #include "chrome/browser/autocomplete/autocomplete_accessibility.h"
@@ -634,16 +635,25 @@ void AutocompleteEditViewWin::SetWindowTextAndCaretPos(const std::wstring& text,
 
 void AutocompleteEditViewWin::SetForcedQuery() {
   const std::wstring current_text(GetText());
-  if (current_text.empty() || (current_text[0] != '?'))
+  const size_t start = current_text.find_first_not_of(kWhitespaceWide);
+  if (start == std::wstring::npos || (current_text[start] != '?'))
     SetUserText(L"?");
   else
-    SetSelection(current_text.length(), 1);
+    SetSelection(current_text.length(), start + 1);
 }
 
 bool AutocompleteEditViewWin::IsSelectAll() {
   CHARRANGE selection;
   GetSel(selection);
   return IsSelectAllForRange(selection);
+}
+
+void AutocompleteEditViewWin::GetSelectionBounds(std::wstring::size_type* start,
+                                                 std::wstring::size_type* end) {
+  CHARRANGE selection;
+  GetSel(selection);
+  *start = static_cast<size_t>(selection.cpMin);
+  *end = static_cast<size_t>(selection.cpMax);
 }
 
 void AutocompleteEditViewWin::SelectAll(bool reversed) {
