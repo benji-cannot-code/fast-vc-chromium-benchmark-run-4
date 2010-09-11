893ec3dc7b4982a0482784e9366f47f992cc8da6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-11 09:48:54 +0000

    2010-09-11  Mihai Parparita  <mihaip@chromium.org>
            Reviewed by Tony Chang.
    
            Implement layoutTestController.dumpResourceResponseMIMETypes in Chromium DRT
            https://bugs.webkit.org/show_bug.cgi?id=45479
    
            Remove tests from Chromium expectations that now pass. Add
            Chromium/Linux expectations for the script test, since it reports a
            different MIME type (application/javascript vs.
            application/x-javascript).
    
            * platform/chromium-linux/fast/dom/HTMLLinkElement/prefetch-expected.txt: Removed.
            * platform/chromium-linux/fast/preloader/script-expected.txt: Added.
            * platform/chromium-mac/fast/dom/HTMLLinkElement/prefetch-expected.txt: Removed.
            * platform/chromium/test_expectations.txt:
    2010-09-11  Mihai Parparita  <mihaip@chromium.org>
    
            Reviewed by Tony Chang.
    
            Implement layoutTestController.dumpResourceResponseMIMETypes in Chromium DRT
            https://bugs.webkit.org/show_bug.cgi?id=45479
    
            Implement layoutTestController.dumpResourceResponseMIMETypes (modelled after
            implementation in ResourceLoadDelegate in the Mac port).
    
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::LayoutTestController):
            (LayoutTestController::dumpResourceResponseMIMETypes):
            (LayoutTestController::reset):
            * DumpRenderTree/chromium/LayoutTestController.h:
            (LayoutTestController::setShouldDumpResourceResponseMIMETypes):
            (LayoutTestController::shouldDumpResourceResponseMIMETypes):
            * DumpRenderTree/chromium/TestShell.h:
            (TestShell::shouldDumpResourceResponseMIMETypes):
            * DumpRenderTree/chromium/WebViewHost.cpp:
            (WebViewHost::didReceiveResponse):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67294 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/LayoutTestController.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/LayoutTestController.cpp
@@ -86,6 +86,7 @@ LayoutTestController::LayoutTestController(TestShell* shell)
     bindMethod("dumpBackForwardList", &LayoutTestController::dumpBackForwardList);
     bindMethod("dumpFrameLoadCallbacks", &LayoutTestController::dumpFrameLoadCallbacks);
     bindMethod("dumpResourceLoadCallbacks", &LayoutTestController::dumpResourceLoadCallbacks);
+    bindMethod("dumpResourceResponseMIMETypes", &LayoutTestController::dumpResourceResponseMIMETypes);
     bindMethod("dumpStatusCallbacks", &LayoutTestController::dumpWindowStatusChanges);
     bindMethod("dumpTitleChanges", &LayoutTestController::dumpTitleChanges);
     bindMethod("setAcceptsEditing", &LayoutTestController::setAcceptsEditing);
@@ -292,6 +293,12 @@ void LayoutTestController::dumpResourceLoadCallbacks(const CppArgumentList&, Cpp
     result->setNull();
 }
 
+void LayoutTestController::dumpResourceResponseMIMETypes(const CppArgumentList&, CppVariant* result)
+{
+    m_dumpResourceResponseMIMETypes = true;
+    result->setNull();
+}
+
 void LayoutTestController::dumpChildFrameScrollPositions(const CppArgumentList&, CppVariant* result)
 {
     m_dumpChildFrameScrollPositions = true;
@@ -487,6 +494,7 @@ void LayoutTestController::reset()
     m_dumpEditingCallbacks = false;
     m_dumpFrameLoadCallbacks = false;
     m_dumpResourceLoadCallbacks = false;
+    m_dumpResourceResponseMIMETypes = false;
     m_dumpBackForwardList = false;
     m_dumpChildFrameScrollPositions = false;
     m_dumpChildFramesAsText = false;
