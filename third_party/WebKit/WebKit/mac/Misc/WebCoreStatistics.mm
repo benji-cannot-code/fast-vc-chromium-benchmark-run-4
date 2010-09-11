b22b121e43230277f9f39fec7f7e1f26067d536c
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-11 01:40:45 +0000

    JavaScriptCore: Refactor JavaScriptCore memory statistics so that WebKit doesn't need to know about the JIT and other implementation details of JavaScriptCore.  Necessary to fix PPC build.
    Reviewed by Alexey Proskuryakov.
    
    https://bugs.webkit.org/show_bug.cgi?id=45528
    
    * JavaScriptCore.exp:
    * JavaScriptCore.xcodeproj/project.pbxproj:
    * runtime/MemoryStatistics.cpp: Added.
    (JSC::memoryStatistics):
    * runtime/MemoryStatistics.h: Added.
    
    WebKit/mac: Refactor JavaScriptCore memory statistics so that WebKit doesn't need to know about the JIT and
    other implementation details of JavaScriptCore.  Necessary to fix PPC build.
            
    Reviewed by Alexey Proskuryakov.
    
    https://bugs.webkit.org/show_bug.cgi?id=45528
    
    * Misc/WebCoreStatistics.mm:
    (+[WebCoreStatistics memoryStatistics]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/Misc/WebCoreStatistics.mm
+++ third_party/WebKit/WebKit/mac/Misc/WebCoreStatistics.mm
@@ -30,11 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebCoreStatistics.h"
 
 #import "DOMElementInternal.h"
-#import <JavaScriptCore/RegisterFile.h>
-#import <JavaScriptCore/ExecutableAllocator.h>
 #import "WebCache.h"
 #import "WebFrameInternal.h"
-#import <runtime/JSLock.h>
+#import <JavaScriptCore/JSLock.h>
+#import <JavaScriptCore/MemoryStatistics.h>
 #import <WebCore/Console.h>
 #import <WebCore/FontCache.h>
 #import <WebCore/Frame.h>
@@ -197,18 +196,19 @@ using namespace WebCore;
 + (NSDictionary *)memoryStatistics
 {
     WTF::FastMallocStatistics fastMallocStatistics = WTF::fastMallocStatistics();
+    
     JSLock lock(SilenceAssertionsOnly);
-    Heap::Statistics jsHeapStatistics = JSDOMWindow::commonJSGlobalData()->heap.statistics();
-    size_t jscStackBytes = RegisterFile::committedByteCount();
-    size_t jscJITBytes = ExecutableAllocator::committedByteCount();
+    Heap::Statistics heapMemoryStats = heapStatistics(JSDOMWindow::commonJSGlobalData());
+    GlobalMemoryStatistics globalMemoryStats = globalMemoryStatistics();
+    
     return [NSDictionary dictionaryWithObjectsAndKeys:
                 [NSNumber numberWithInt:fastMallocStatistics.reservedVMBytes], @"FastMallocReservedVMBytes",
                 [NSNumber numberWithInt:fastMallocStatistics.committedVMBytes], @"FastMallocCommittedVMBytes",
                 [NSNumber numberWithInt:fastMallocStatistics.freeListBytes], @"FastMallocFreeListBytes",
-                [NSNumber numberWithInt:jsHeapStatistics.size], @"JavaScriptHeapSize",
-                [NSNumber numberWithInt:jsHeapStatistics.free], @"JavaScriptFreeSize",
-                [NSNumber numberWithUnsignedInt:(unsigned int)jscStackBytes], @"JavaScriptStackSize",
-                [NSNumber numberWithUnsignedInt:(unsigned int)jscJITBytes], @"JavaScriptJITSize",
+                [NSNumber numberWithInt:heapMemoryStats.size], @"JavaScriptHeapSize",
+                [NSNumber numberWithInt:heapMemoryStats.free], @"JavaScriptFreeSize",
+                [NSNumber numberWithUnsignedInt:(unsigned int)globalMemoryStats.stackBytes], @"JavaScriptStackSize",
+                [NSNumber numberWithUnsignedInt:(unsigned int)globalMemoryStats.JITBytes], @"JavaScriptJITSize",
             nil];
 }
 
