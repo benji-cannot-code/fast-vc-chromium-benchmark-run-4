19b09fc887e2de2d36fe40b4d91b94bfc20c704b
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-12 02:28:10 +0000

    2010-09-11  Adam Barth  <abarth@webkit.org>
            Reviewed by Sam Weinig.
    
            Make SecurityOrigin::canDisplay an instance function
            https://bugs.webkit.org/show_bug.cgi?id=45219
    
            This function was secretly an instance function because callers needed
            to pass in a document (which owns a SecurityOrigin).  Only the
            FrameLoader actually needs the method to be static.
    
            * WebCore.exp.in:
            * html/HTMLMediaElement.cpp:
            (WebCore::HTMLMediaElement::isSafeToLoadURL):
            * loader/Cache.cpp:
            (WebCore::Cache::requestResource):
                - Notice that DocLoader::doc() can never be 0.
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::loadFrameRequest):
            * loader/PingLoader.cpp:
            (WebCore::PingLoader::loadImage):
            * loader/SubframeLoader.cpp:
            (WebCore::SubframeLoader::loadMediaPlayerProxyPlugin):
            (WebCore::SubframeLoader::createJavaAppletWidget):
            (WebCore::SubframeLoader::loadSubframe):
            (WebCore::SubframeLoader::loadPlugin):
            * loader/SubresourceLoader.cpp:
            (WebCore::SubresourceLoader::create):
            * page/SecurityOrigin.cpp:
            (WebCore::SecurityOrigin::canDisplay):
            (WebCore::SecurityOrigin::deprecatedCanDisplay):
            * page/SecurityOrigin.h:
            * plugins/PluginView.cpp:
            (WebCore::PluginView::load):
    2010-09-11  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Sam Weinig.
    
            Make SecurityOrigin::canDisplay an instance function
            https://bugs.webkit.org/show_bug.cgi?id=45219
    
            * Plugins/Hosted/NetscapePluginInstanceProxy.mm:
            (WebKit::NetscapePluginInstanceProxy::loadRequest):
            * Plugins/WebNetscapePluginStream.mm:
            (WebNetscapePluginStream::WebNetscapePluginStream):
            * Plugins/WebNetscapePluginView.mm:
            (-[WebNetscapePluginView loadRequest:inTarget:withNotifyData:sendNotification:]):
            * Plugins/WebPluginContainerCheck.mm:
            (-[WebPluginContainerCheck _isForbiddenFileLoad]):
            * WebView/WebFrame.mm:
            (-[WebFrame _allowsFollowingLink:]):
    2010-09-11  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Sam Weinig.
    
            Make SecurityOrigin::canDisplay an instance function
            https://bugs.webkit.org/show_bug.cgi?id=45219
    
            * WebFrame.cpp:
            (WebFrame::allowsFollowingLink):
    2010-09-11  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Sam Weinig.
    
            Make SecurityOrigin::canDisplay an instance function
            https://bugs.webkit.org/show_bug.cgi?id=45219
    
            * WebProcess/Plugins/PluginView.cpp:
            (WebKit::PluginView::performFrameLoadURLRequest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/SubframeLoader.cpp
+++ third_party/WebKit/WebCore/loader/SubframeLoader.cpp
@@ -157,7 +157,7 @@ PassRefPtr<Widget> SubframeLoader::loadMediaPlayerProxyPlugin(Node* node, const 
     if (!url.isEmpty())
         completedURL = completeURL(url);
 
-    if (!SecurityOrigin::canDisplay(completedURL, String(), m_frame->document())) {
+    if (!m_frame->document()->securityOrigin()->canDisplay(completedURL)) {
         FrameLoader::reportLocalLoadFailed(m_frame, completedURL.string());
         return 0;
     }
@@ -215,7 +215,7 @@ PassRefPtr<Widget> SubframeLoader::createJavaAppletWidget(const IntSize& size, H
 
     if (!codeBaseURLString.isEmpty()) {
         KURL codeBaseURL = completeURL(codeBaseURLString);
-        if (!SecurityOrigin::canDisplay(codeBaseURL, String(), element->document())) {
+        if (!element->document()->securityOrigin()->canDisplay(codeBaseURL)) {
             FrameLoader::reportLocalLoadFailed(m_frame, codeBaseURL.string());
             return 0;
         }
@@ -257,7 +257,7 @@ Frame* SubframeLoader::loadSubframe(HTMLFrameOwnerElement* ownerElement, const K
         marginHeight = o->getMarginHeight();
     }
 
-    if (!SecurityOrigin::canDisplay(url, String(), ownerElement->document())) {
+    if (!ownerElement->document()->securityOrigin()->canDisplay(url)) {
         FrameLoader::reportLocalLoadFailed(m_frame, url.string());
         return 0;
     }
@@ -346,7 +346,7 @@ bool SubframeLoader::loadPlugin(HTMLPlugInImageElement* pluginElement, const KUR
     if (!renderer || useFallback)
         return false;
 
-    if (!SecurityOrigin::canDisplay(url, String(), document())) {
+    if (!document()->securityOrigin()->canDisplay(url)) {
         FrameLoader::reportLocalLoadFailed(m_frame, url.string());
         return false;
     }
