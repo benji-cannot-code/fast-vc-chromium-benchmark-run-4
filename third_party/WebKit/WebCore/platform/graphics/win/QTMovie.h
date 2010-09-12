e4512bf21c2fcd779a5ab0b7abaf427de547b765
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-12 17:38:08 +0000

    2010-09-11  Jer Noble  <jer.noble@apple.com>
            Reviewed by Eric Carlson.
    
            http/tests/media/text-served-as-text.html failing on Windows (Debug+Release)
            https://bugs.webkit.org/show_bug.cgi?id=45603
    
            Disable the eat/text components under Windows the same way as we do on the Mac.
    
            * platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp:
            * platform/graphics/win/QTMovie.cpp:
            * platform/graphics/win/QTMovie.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/QTMovie.h
+++ third_party/WebKit/WebCore/platform/graphics/win/QTMovie.h
@@ -63,6 +63,8 @@ public:
     static bool initializeQuickTime();
     static void taskTimerFired();
 
+    static void disableComponent(uint32_t[5]);
+
     QTMovie(QTMovieClient*);
     ~QTMovie();
 
