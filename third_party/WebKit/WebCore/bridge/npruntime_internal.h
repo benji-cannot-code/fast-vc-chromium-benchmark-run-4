14584cfb1a4d27592f80edd2d7777e6668df0b2c
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-12 13:33:34 +0000

    [Qt][X11] Fix build with V8 against X11 headers.
    Reviewed by Andreas Kling.
    
    Added "True" and "False" to the list of words
    we free from the X11 macro jail.
    
    * bridge/npruntime_internal.h:
    * config.h: The V8 bindings end up indirectly pulling
    in X11 headers and they use True/False. There's no
    central header apart from this one, so include the
    x11 fixing header here for Qt/X11/V8.
    * plugins/qt/PluginViewQt.cpp:
    (WebCore::PluginView::paint): Use false instead of False
    since False isn't available anymore.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67323 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bridge/npruntime_internal.h
+++ third_party/WebKit/WebCore/bridge/npruntime_internal.h
@@ -49,4 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     #undef FontChange
     #undef GrayScale
     #undef NormalState
+    #undef True
+    #undef False
 #endif
