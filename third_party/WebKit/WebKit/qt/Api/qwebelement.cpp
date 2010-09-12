3e1bc0bb51f6ca966558bf72eb6c18d39c036992
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-12 18:21:59 +0000

    [Qt] Partial implementation of Qt bridge using V8 and QtScript.
    Reviewed by Andreas Kling.
    
    * Api/qwebelement.cpp:
    (QWebElement::evaluateJavaScript): Stub it out for now,
    to compile, until we have a conversion path between v8::Object
    and QScriptValue.
    * Api/qwebframe.cpp:
    (QWebFrame::addToJavaScriptWindowObject): Implemented using
    few lines of QtScript code.
    (QWebFrame::evaluateJavaScript): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67336 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebelement.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebelement.cpp
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif USE(V8)
 #include "V8DOMWindow.h"
 #include "V8Binding.h"
-#include "qt_instancev8.h"
+#include "NotImplemented.h"
 #endif
 #include "NodeList.h"
 #include "RenderImage.h"
@@ -787,24 +787,8 @@ QVariant QWebElement::evaluateJavaScript(const QString& scriptSource)
     int distance = 0;
     return JSC::Bindings::convertValueToQVariant(state, result, QMetaType::Void, &distance);
 #elif USE(V8)
-    // Create scope handler
-    v8::HandleScope hs;
-    // Get proxy from scriptcontroller
-    V8Proxy* proxy = scriptController->proxy();
-    // Ask the context from proxy
-    v8::Handle<v8::Context> context = proxy->context();
-    if (context.IsEmpty())
-        return QVariant();
-
-    // Create scope for the context
-    v8::Context::Scope scope(context);
-    v8::Local<v8::Value> object = proxy->evaluate(ScriptSourceCode(scriptSource), static_cast<Node*>(m_element));
-
-    if (object.IsEmpty())
-        return QVariant();
-    String result = v8ValueToWebCoreString(object);
-    int distance = 0;
-    return V8::Bindings::convertValueToQVariant(object, QMetaType::Void, &distance);
+    notImplemented();
+    return QVariant();
 #endif
 }
 
