60810beb939acb168d85f8bce04a1b133298a756
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-13 01:04:35 +0000

    2010-09-12  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Adoption agency should use takeAllChildrenFrom
            https://bugs.webkit.org/show_bug.cgi?id=45570
    
            Before this patch, we were using an unsafe pattern of walking the
            sibling list without grabbing references.  Instead, we should use the
            shiny new takeAllChildrenFrom method that shoves the children into a
            vector first.  Also, update takeAllChildrenFrom to handle the case
            where the old parent is attached.
    
            * dom/ContainerNode.cpp:
            (WebCore::ContainerNode::takeAllChildrenFrom):
            * html/parser/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::callTheAdoptionAgency):
            * html/parser/HTMLTreeBuilder.h:
    2010-09-12  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Eric Seidel.
    
            Adoption agency should use takeAllChildrenFrom
            https://bugs.webkit.org/show_bug.cgi?id=45570
    
            More test cases.
    
            * html5lib/resources/webkit01.dat:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67357 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/parser/HTMLTreeBuilder.h
+++ third_party/WebKit/WebCore/html/parser/HTMLTreeBuilder.h
@@ -171,7 +171,6 @@ private:
     PassRefPtr<NamedNodeMap> attributesForIsindexInput(AtomicHTMLToken&);
 
     HTMLElementStack::ElementRecord* furthestBlockForFormattingElement(Element*);
-    void reparentChildren(Element* oldParent, Element* newParent);
     void callTheAdoptionAgency(AtomicHTMLToken&);
 
     void closeTheCell();
