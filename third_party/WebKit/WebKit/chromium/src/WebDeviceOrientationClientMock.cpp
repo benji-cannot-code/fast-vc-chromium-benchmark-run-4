f12060bc0bd30d07212cdc2f081b2409f097da8c
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-13 11:05:04 +0000

    2010-09-13  Hans Wennborg  <hans@chromium.org>
            Reviewed by Jeremy Orlow.
    
            Add WebDeviceOrientationClientMock::create() and use it from DRT.
            https://bugs.webkit.org/show_bug.cgi?id=45650
    
            Since the virtual member functions of WebDeviceOrientationClientMock
            are not exported from the DLL, construction of the object must be done inside
            the DLL, or else the linker will complain (because the vtable cannot
            be set up). Clients should use the create() member function, and the constructor
            will be made private in a subsequent patch.
    
            * public/WebDeviceOrientationClientMock.h:
            (WebKit::WebDeviceOrientationClientMock::WebDeviceOrientationClientMock):
            * src/WebDeviceOrientationClientMock.cpp:
            (WebKit::WebDeviceOrientationClientMock::create):
    2010-09-13  Hans Wennborg  <hans@chromium.org>
    
            Reviewed by Jeremy Orlow.
    
            Add WebDeviceOrientationClientMock::create() and use it from DRT.
            https://bugs.webkit.org/show_bug.cgi?id=45650
    
            Do not use the WebDeviceOrientationClientMock constructor directly,
            but use the create() member function.
    
            This should fix the linker errors for the multi-DLL Chromium DRT build.
    
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::setMockDeviceOrientation):
            (LayoutTestController::deviceOrientationClient):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67378 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebDeviceOrientationClientMock.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebDeviceOrientationClientMock.cpp
@@ -33,6 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
+WebDeviceOrientationClientMock* WebDeviceOrientationClientMock::create()
+{
+    return new WebDeviceOrientationClientMock();
+}
+
 void WebDeviceOrientationClientMock::setController(WebDeviceOrientationController* controller)
 {
     m_clientMock->setController(controller->controller());
