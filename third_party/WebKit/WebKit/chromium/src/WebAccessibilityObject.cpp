1b9de5bbc5e816a123fb8ce7de0c533311c98087
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-13 22:59:52 +0000

    2010-09-13  Chris Guillory  <chris.guillory@google.com>
            Reviewed by Chris Fleizach.
    
            WebAccessibilityCacheImpl needs to handle invalid accessibility object ids.
            https://bugs.webkit.org/show_bug.cgi?id=45572
    
            * public/WebAccessibilityObject.h:
            * src/WebAccessibilityCacheImpl.cpp:
            (WebKit::WebAccessibilityCacheImpl::addOrGetId):
            * src/WebAccessibilityObject.cpp:
            (WebKit::WebAccessibilityObject::isAxObjectIdValid):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67418 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebAccessibilityObject.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebAccessibilityObject.cpp
@@ -102,6 +102,15 @@ bool WebAccessibilityObject::canSetValueAttribute() const
     return m_private->canSetValueAttribute();
 }
 
+bool WebAccessibilityObject::isValid() const
+{
+    if (!m_private)
+        return false;
+
+    m_private->updateBackingStore();
+    return m_private->axObjectID();
+}
+
 unsigned WebAccessibilityObject::childCount() const
 {
     if (!m_private)
