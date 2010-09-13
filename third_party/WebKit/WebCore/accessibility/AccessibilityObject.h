345974535ad7f3116313342312d2f66248a77c3e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-13 12:29:26 +0000

    2010-09-13  Mario Sanchez Prada  <msanchez@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] ATs should be able to select/unselect text
            https://bugs.webkit.org/show_bug.cgi?id=25673
    
            Implement AtkText's setSelection and removeSelection functions
    
            * accessibility/AccessibilityObject.cpp:
            (WebCore::AccessibilityObject::visiblePositionRangeForRange):
            Moved some GTK specific code from a ifdef-endif region to
            AccessibilityObjectAtk.cpp
            * accessibility/AccessibilityObject.h:
            * accessibility/gtk/AccessibilityObjectAtk.cpp:
            (WebCore::AccessibilityObject::getLengthForTextRange): New.
            * accessibility/gtk/AccessibilityObjectWrapperAtk.cpp:
            (webkit_accessible_text_remove_selection): Implemented following
            the lead of GAIL's implementation of the AtkText interface.
            (webkit_accessible_text_set_selection): Implemented following
            the lead of GAIL's implementation of the AtkText interface.
            (webkit_accessible_text_set_caret_offset): Changed to directly use
            visiblePositionRangeForRange now that there's no longer a problem
            with that, as it was in the past (only worked for text controls).
    2010-09-13  Mario Sanchez Prada  <msanchez@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] Provide unit tests for AtkText's text selection functions
            https://bugs.webkit.org/show_bug.cgi?id=43919
    
            New tests to check getting, setting and removing text selections
    
            * tests/testatk.c:
            (testWekitAtkTextSelections): New unit tests to check all the text
            selection related functions altogether through a single test
            function.
            (main):
    
            Make sure that code dependant on getting information from the
            clipboard gets executed only when there's a GDK window associated
            to the webview widget, as that's not the case when executing the
            unit tests (the wedbview is not inside of any toplevel window) and
            will make the tests crash if not taken into account.
    
            * WebCoreSupport/EditorClientGtk.cpp:
            (WebKit::EditorClient::respondToChangedSelection):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AccessibilityObject.h
+++ third_party/WebKit/WebCore/accessibility/AccessibilityObject.h
@@ -575,8 +575,10 @@ protected:
     
 #if PLATFORM(GTK)
     bool allowsTextRanges() const;
+    unsigned getLengthForTextRange() const;
 #else
     bool allowsTextRanges() const { return isTextControl(); }
+    unsigned getLengthForTextRange() const { return text().length(); }
 #endif
 
 #if PLATFORM(MAC)
