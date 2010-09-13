8d7df6416534c8006552b2228d8a7791611baafd
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-13 22:22:39 +0000

    Defend against a malicious renderer trying to upload a file to which it doesn't have access.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3363014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59290 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/redirect_to_file_resource_handler.cc
+++ chrome/browser/renderer_host/redirect_to_file_resource_handler.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/platform_file.h"
 #include "base/task.h"
+#include "chrome/browser/child_process_security_policy.h"
 #include "chrome/browser/renderer_host/resource_dispatcher_host.h"
 #include "chrome/common/resource_response.h"
 #include "net/base/file_stream.h"
@@ -145,6 +146,7 @@ void RedirectToFileResourceHandler::OnRequestClosed() {
   file_stream_.reset();
 
   // TODO(dumi): delete the temp file when it's no longer needed.
+  // TODO(dumi): revoke the privilege to upload this file.
   // base::FileUtilProxy::Delete(
   //       ChromeThread::GetMessageLoopProxyForThread(ChromeThread::FILE),
   //       file_path_, NULL);
@@ -162,6 +164,8 @@ void RedirectToFileResourceHandler::DidCreateTemporaryFile(
   file_stream_.reset(new net::FileStream(file_handle.ReleaseValue(),
                                          base::PLATFORM_FILE_WRITE |
                                          base::PLATFORM_FILE_ASYNC));
+  ChildProcessSecurityPolicy::GetInstance()->GrantUploadFile(
+      process_id_, file_path);
   host_->StartDeferredRequest(process_id_, request_id_);
 }
 
