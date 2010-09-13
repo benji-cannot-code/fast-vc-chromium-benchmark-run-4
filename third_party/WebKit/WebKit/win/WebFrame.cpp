efbce3414f03e87d4e476cfa399e6dc5ef3a7e33
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-13 21:08:00 +0000

    One more attempt at fixing the Windows build. Not reviewed.
    git-svn-id: svn://svn.chromium.org/blink/trunk@67409 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebFrame.cpp
+++ third_party/WebKit/WebKit/win/WebFrame.cpp
@@ -1605,7 +1605,7 @@ bool WebFrame::canHandleRequest(const ResourceRequest& request) const
     return WebView::canHandleRequest(request);
 }
 
-bool WebFrame::canShowMIMETypeASHTML(const String& /*MIMEType*/) const
+bool WebFrame::canShowMIMETypeAsHTML(const String& /*MIMEType*/) const
 {
     notImplemented();
     return true;
