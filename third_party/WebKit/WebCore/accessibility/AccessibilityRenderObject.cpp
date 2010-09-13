b0702659a56f41b1e354377a470b653b2f605fd7
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-13 23:27:16 +0000

    AX: REGRESSION: WebKit does not report label of web popup buttons https://bugs.webkit.org/show_bug.cgi?id=45715
    Reviewed by Darin Adler.
    
    WebCore: 
    
    Test: platform/mac/accessibility/popup-button-title.html
    
    * WebCore.xcodeproj/project.pbxproj:
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::title):
    
    LayoutTests: 
    
    * platform/mac/accessibility/popup-button-title-expected.txt: Added.
    * platform/mac/accessibility/popup-button-title.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67422 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AccessibilityRenderObject.cpp
+++ third_party/WebKit/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -1311,10 +1311,9 @@ String AccessibilityRenderObject::title() const
         || ariaRole == RadioButtonRole
         || ariaRole == CheckBoxRole
         || ariaRole == TabRole
-        || isHeading())
-        return textUnderElement();
-    
-    if (isLink())
+        || ariaRole == PopUpButtonRole
+        || isHeading()
+        || isLink())
         return textUnderElement();
     
     return String();
