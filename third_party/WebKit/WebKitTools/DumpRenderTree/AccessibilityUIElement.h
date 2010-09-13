9ef5aafb684b737a6f86a6e0e005e89771c11ae5
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-13 16:03:00 +0000

    REGRESSION (r64816-r64889): Crash in WebCore::AccessibilityRenderObject https://bugs.webkit.org/show_bug.cgi?id=43807
    Reviewed by Darin Adler.
    
    WebCore: 
    
    Ensure that visiblePositionForPoint can handle non RenderBoxModelObject types.
    
    Test: platform/mac/accessibility/visible-position-crash-for-text-node.html
    
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::visiblePositionForPoint):
    
    WebKitTools: 
    
    * DumpRenderTree/AccessibilityUIElement.cpp:
    (textMarkerForPointCallback):
    (AccessibilityUIElement::textMarkerForPoint):
    (AccessibilityUIElement::getJSClass):
    * DumpRenderTree/AccessibilityUIElement.h:
    * DumpRenderTree/mac/AccessibilityUIElementMac.mm:
    (AccessibilityUIElement::textMarkerForPoint):
    
    LayoutTests: 
    
    * platform/mac/accessibility/visible-position-crash-for-text-node-expected.txt: Added.
    * platform/mac/accessibility/visible-position-crash-for-text-node.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67390 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/AccessibilityUIElement.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/AccessibilityUIElement.h
@@ -190,6 +190,7 @@ public:
     AccessibilityTextMarkerRange textMarkerRangeForMarkers(AccessibilityTextMarker* startMarker, AccessibilityTextMarker* endMarker);
     AccessibilityTextMarker startTextMarkerForTextMarkerRange(AccessibilityTextMarkerRange*);
     AccessibilityTextMarker endTextMarkerForTextMarkerRange(AccessibilityTextMarkerRange*);
+    AccessibilityTextMarker textMarkerForPoint(int x, int y);
     AccessibilityUIElement accessibilityElementForTextMarker(AccessibilityTextMarker*);
     int textMarkerRangeLength(AccessibilityTextMarkerRange*);
     
