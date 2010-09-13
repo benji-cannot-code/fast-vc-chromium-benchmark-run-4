d6c71320aa36fdc85d419eba143ca24af830ee52
petersont@google.com <petersont@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-13 20:00:18 +0000

    Replaces matrix math functions in o3d-webgl with a new library that uses 1-dimensional arrays to represent matrices instead of 2-dimensional arrays.
    This is a duplicate of Daniel Horn's change http://codereview.chromium.org/3072008 for committal.
    
    Review URL: http://codereview.chromium.org/3273004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59268 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/samples/o3djs/canvas.js
+++ o3d/samples/o3djs/canvas.js
@@ -242,10 +242,10 @@ o3djs.canvas.CanvasInfo = function(pack, root, viewInfo) {
       1,
       1,
       1,
-      [[1, 0, 0, 0],
-       [0, 0, 1, 0],
-       [0, -1, 0, 0],
-       [0, 0, 0, 1]]);
+      o3djs.math.makeMatrix4(1, 0, 0, 0,
+                             0, 0, 1, 0,
+                             0, -1, 0 ,0,
+                             0, 0, 0, 1));
 
   /**
    * A shape for opaque quads.
@@ -258,10 +258,10 @@ o3djs.canvas.CanvasInfo = function(pack, root, viewInfo) {
       1,
       1,
       1,
-      [[1, 0, 0, 0],
-       [0, 0, 1, 0],
-       [0, -1, 0, 0],
-       [0, 0, 0, 1]]);
+      o3djs.math.makeMatrix4(1, 0, 0, 0,
+                             0, 0, 1, 0,
+                             0, -1, 0 ,0,
+                             0, 0, 0, 1));
 };
 /**
  * The CanvasQuad object encapsulates a Transform, a rectangle Shape,
