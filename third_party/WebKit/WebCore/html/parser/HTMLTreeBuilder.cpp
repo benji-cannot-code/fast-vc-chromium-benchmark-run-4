d5a40da40b0fe9e9498ba51824e898443060b86c
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-13 17:40:44 +0000

    2010-09-13  Adam Barth  <abarth@webkit.org>
            Reviewed by Darin Adler.
    
            Fix </sarcasm>
            https://bugs.webkit.org/show_bug.cgi?id=45645
    
            Test that </sarcasm> works properly.
    
            * html5lib/resources/webkit02.dat:
    2010-09-13  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Darin Adler.
    
            Fix </sarcasm>
            https://bugs.webkit.org/show_bug.cgi?id=45645
    
            Umm... Yeah.
    
            * html/parser/HTMLTreeBuilder.cpp:
            (WebCore::HTMLTreeBuilder::processEndTagForInBody):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67396 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/parser/HTMLTreeBuilder.cpp
+++ third_party/WebKit/WebCore/html/parser/HTMLTreeBuilder.cpp
@@ -2011,10 +2011,6 @@ void HTMLTreeBuilder::processEndTagForInBody(AtomicHTMLToken& token)
         m_tree.openElements()->popUntilNumberedHeaderElementPopped();
         return;
     }
-    if (token.name() == "sarcasm") {
-        notImplemented(); // Take a deep breath.
-        return;
-    }
     if (isFormattingTag(token.name())) {
         callTheAdoptionAgency(token);
         return;
