f158e997f053714d61845e34498931c7fc4d881e
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-14 03:40:40 +0000

    Implement SymmetricKey::Import for HMAC-SHA-1 keys on Windows.
    Port SymmetricKeyTest.ImportGeneratedKey and
    SymmetricKeyTest.ImportDerivedKey to all platforms.  On Mac,
    the salt for PBKDF2 must be at least 8 bytes long.
    
    R=tim
    BUG=none
    TEST=base_unittests --gtest_filter=SymmetricKeyTest.ImportDerivedKey
    should pass on Windows.
    Review URL: http://codereview.chromium.org/3361020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59330 0039d316-1c4b-4281-b951-d872f2087c98

--- base/crypto/symmetric_key_unittest.cc
+++ base/crypto/symmetric_key_unittest.cc
@@ -30,8 +30,6 @@ TEST(SymmetricKeyTest, GenerateRandomKey) {
   EXPECT_NE(raw_key, raw_key2);
 }
 
-// TODO(albertb): Port this test to Mac.
-#if defined(USE_NSS) || defined(OS_WIN)
 TEST(SymmetricKeyTest, ImportGeneratedKey) {
   scoped_ptr<base::SymmetricKey> key1(
       base::SymmetricKey::GenerateRandomKey(base::SymmetricKey::AES, 256));
@@ -48,14 +46,12 @@ TEST(SymmetricKeyTest, ImportGeneratedKey) {
 
   EXPECT_EQ(raw_key1, raw_key2);
 }
-#endif  // USE_NSS || OS_WIN
 
-// TODO(albertb): Port this test to Win and Mac.
-#if defined(USE_NSS)
 TEST(SymmetricKeyTest, ImportDerivedKey) {
   scoped_ptr<base::SymmetricKey> key1(
       base::SymmetricKey::DeriveKeyFromPassword(base::SymmetricKey::HMAC_SHA1,
-                                                "password", "salt", 1024, 160));
+                                                "password", "somesalt", 1024,
+                                                160));
   ASSERT_TRUE(NULL != key1.get());
   std::string raw_key1;
   EXPECT_TRUE(key1->GetRawKey(&raw_key1));
@@ -69,7 +65,6 @@ TEST(SymmetricKeyTest, ImportDerivedKey) {
 
   EXPECT_EQ(raw_key1, raw_key2);
 }
-#endif  // USE_NSS
 
 struct PBKDF2TestVector {
   const char* password;
