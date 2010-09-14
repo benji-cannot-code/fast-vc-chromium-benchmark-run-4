a521687777e4e4dd15d62f020f5231ab003d91c8
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-14 08:19:06 +0000

    Enable incognito mode from external_extensions.json
    Add an ability to enable extension in incognito mode by default from external_extensions.json file.
    
    BUG=chromium-os:6516
    TEST=unit_tests --gtest_filter=ExtensionsServiceTest.ExternalPrefProvider
    
    Review URL: http://codereview.chromium.org/3360020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59344 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service.h
+++ chrome/browser/extensions/extensions_service.h
@@ -226,8 +226,10 @@ class ExtensionsService
 
   // Given an extension id and an update URL, schedule the extension
   // to be fetched, installed, and activated.
-  void AddPendingExtensionFromExternalUpdateUrl(const std::string& id,
-                                                const GURL& update_url);
+  void AddPendingExtensionFromExternalUpdateUrl(
+      const std::string& id,
+      const GURL& update_url,
+      bool enable_incognito_on_install);
 
   // Reloads the specified extension.
   void ReloadExtension(const std::string& extension_id);
@@ -562,8 +564,10 @@ class ExtensionsServiceBackend
                                             const FilePath& path,
                                             Extension::Location location);
 
-  virtual void OnExternalExtensionUpdateUrlFound(const std::string& id,
-                                                 const GURL& update_url);
+  virtual void OnExternalExtensionUpdateUrlFound(
+      const std::string& id,
+      const GURL& update_url,
+      bool enable_incognito_on_install);
 
   // Reloads the given extensions from their manifests on disk (instead of what
   // we have cached in the prefs).
