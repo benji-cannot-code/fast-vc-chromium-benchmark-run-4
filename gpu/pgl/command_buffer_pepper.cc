87b8112d9149c059f9582fa901f18be129f9987c
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-14 01:53:28 +0000

    Removed base dependency from PGL.
    This is so it can be statically linked with untrusted NaCl code.
    
    TEST=try
    BUG=none
    
    Review URL: http://codereview.chromium.org/3366021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59321 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/pgl/command_buffer_pepper.cc
+++ gpu/pgl/command_buffer_pepper.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/pgl/command_buffer_pepper.h"
 
-#include "base/logging.h"
 #include "gpu/command_buffer/common/logging.h"
 
 using base::SharedMemory;
@@ -25,7 +24,7 @@ CommandBufferPepper::~CommandBufferPepper() {
 
 // Not implemented in CommandBufferPepper.
 bool CommandBufferPepper::Initialize(int32 size) {
-  NOTREACHED();
+  GPU_NOTREACHED();
   return false;
 }
 
@@ -129,7 +128,7 @@ CommandBuffer::State CommandBufferPepper::Flush(int32 put_offset) {
 
 void CommandBufferPepper::SetGetOffset(int32 get_offset) {
   // Not implemented by proxy.
-  NOTREACHED();
+  GPU_NOTREACHED();
 }
 
 int32 CommandBufferPepper::CreateTransferBuffer(size_t size) {
@@ -157,13 +156,13 @@ Buffer CommandBufferPepper::GetTransferBuffer(int32 id) {
 
 void CommandBufferPepper::SetToken(int32 token) {
   // Not implemented by proxy.
-  NOTREACHED();
+  GPU_NOTREACHED();
 }
 
 void CommandBufferPepper::SetParseError(
     gpu::error::Error error) {
   // Not implemented by proxy.
-  NOTREACHED();
+  GPU_NOTREACHED();
 }
 
 gpu::error::Error CommandBufferPepper::GetCachedError() {
