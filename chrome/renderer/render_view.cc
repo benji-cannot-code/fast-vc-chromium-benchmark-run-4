339e8e37569c2ccf63340610cf424f5f3fe1c59f
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-14 19:35:04 +0000

    Land implementation part of cl 3352009 (that cl was just data files that had to be uploaded first for try runs to work).
    This makes window.open() open a tab or popup window correctly from extensions, even when no user gesture is present.
    
    BUG=54768
    TEST=Added browser tests.
    
    Review URL: http://codereview.chromium.org/3325024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59418 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/devtools_agent.h"
 #include "chrome/renderer/devtools_client.h"
 #include "chrome/renderer/extension_groups.h"
+#include "chrome/renderer/extensions/bindings_utils.h"
 #include "chrome/renderer/extensions/extension_renderer_info.h"
 #include "chrome/renderer/extensions/event_bindings.h"
 #include "chrome/renderer/extensions/extension_process_bindings.h"
@@ -2284,6 +2285,13 @@ void RenderView::show(WebNavigationPolicy policy) {
   if (did_show_)
     return;
   did_show_ = true;
+
+  // Extensions and apps always allowed to create unrequested popups. The second
+  // check is necessary to include content scripts.
+  if (ExtensionRendererInfo::GetByURL(creator_url_) ||
+      bindings_utils::GetInfoForCurrentContext()) {
+    opened_by_user_gesture_ = true;
+  }
 
   // Force new windows to a popup if they were not opened with a user gesture.
   if (!opened_by_user_gesture_) {
