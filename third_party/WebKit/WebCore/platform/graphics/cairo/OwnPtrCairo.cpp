ede48459d38f67c67e1e5cb65dd26b949fed8854
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-14 21:43:54 +0000

    Build correction, no review.
    WebCore: 
    
    * WebCore.vcproj/WebCore.vcproj: Add missing OwnPtrCairo.cpp
      implementation file.
    * platform/graphics/cairo/OwnPtrCairo.cpp: Add missing include
      for <cairo.h>
    
    WebKit2: 
    
    * win/WebKit2.vcproj: Disable build for Cairo targets of
    the WebURLRequestWin.cpp and WebURLResponseWin.cpp files.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67499 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cairo/OwnPtrCairo.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cairo/OwnPtrCairo.cpp
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fontconfig/fcfreetype.h>
 #endif
 
+#include <cairo.h>
+
 namespace WTF {
 
 #if defined(USE_FREETYPE)
