369fc44b5df93869bae86210cb36a5b22b9e0351
pam@chromium.org <pam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-14 08:25:07 +0000

    Create a DefaultPrefStore to hold registered application-default preference values.
    Also rework preference notifications so they're not sent when a pref store rewrites the same value it already had,
    to avoid infinite recursion in places that set a pref in response to a notification that that pref has changed.
    
    This allows notifications to be sent properly when another PrefStore takes control from a default value.
    
    BUG=52719, 54950
    TEST=covered by unit tests (PrefValueStoreTest.*, PrefNotifierTest.*, and ExtensionPrefStoreTest.*)
    Review URL: http://codereview.chromium.org/3323022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59345 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/pref_service_unittest.cc
+++ chrome/browser/prefs/pref_service_unittest.cc
@@ -213,6 +213,11 @@ TEST_F(PrefServiceSetValueTest, SetStringValue) {
   scoped_ptr<Value> default_value(Value::CreateStringValue(default_string));
   prefs_.RegisterStringPref(name_, default_string);
   prefs_.AddPrefObserver(name_, &observer_);
+  // Changing the controlling store from default to user triggers notification.
+  SetExpectPrefChanged();
+  prefs_.Set(name_, *default_value);
+  Mock::VerifyAndClearExpectations(&observer_);
+
   SetExpectNoNotification();
   prefs_.Set(name_, *default_value);
   Mock::VerifyAndClearExpectations(&observer_);
@@ -229,6 +234,8 @@ TEST_F(PrefServiceSetValueTest, SetDictionaryValue) {
   prefs_.RegisterDictionaryPref(name_);
   prefs_.AddPrefObserver(name_, &observer_);
 
+  // Dictionary values are special: setting one to NULL is the same as clearing
+  // the user value, allowing the NULL default to take (or keep) control.
   SetExpectNoNotification();
   prefs_.Set(name_, *null_value_);
   Mock::VerifyAndClearExpectations(&observer_);
@@ -261,6 +268,8 @@ TEST_F(PrefServiceSetValueTest, SetListValue) {
   prefs_.RegisterListPref(name_);
   prefs_.AddPrefObserver(name_, &observer_);
 
+  // List values are special: setting one to NULL is the same as clearing the
+  // user value, allowing the NULL default to take (or keep) control.
   SetExpectNoNotification();
   prefs_.Set(name_, *null_value_);
   Mock::VerifyAndClearExpectations(&observer_);
