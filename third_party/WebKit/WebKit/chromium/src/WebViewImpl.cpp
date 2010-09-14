e759eba8ebfa3013d6221bb04e6904ca139a4213
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-14 17:38:17 +0000

    2010-09-14  Tony Chang  <tony@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            [chromium] implement layoutTestController.setDomainRelaxationForbiddenForURLScheme
            https://bugs.webkit.org/show_bug.cgi?id=45762
    
            * platform/chromium/test_expectations.txt:
    2010-09-14  Tony Chang  <tony@chromium.org>
    
            Reviewed by Dimitri Glazkov.
    
            [chromium] implement layoutTestController.setDomainRelaxationForbiddenForURLScheme
            https://bugs.webkit.org/show_bug.cgi?id=45762
    
            * public/WebView.h: add WebView::setDomainRelaxationForbidden
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::setDomainRelaxationForbidden):
            * src/WebViewImpl.h:
    2010-09-14  Tony Chang  <tony@chromium.org>
    
            Reviewed by Dimitri Glazkov.
    
            [chromium] implement layoutTestController.setDomainRelaxationForbiddenForURLScheme
            https://bugs.webkit.org/show_bug.cgi?id=45762
    
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::LayoutTestController):
            (LayoutTestController::setDomainRelaxationForbiddenForURLScheme):
            * DumpRenderTree/chromium/LayoutTestController.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
@@ -1981,6 +1981,11 @@ bool WebViewImpl::isActive() const
     return (page() && page()->focusController()) ? page()->focusController()->isActive() : false;
 }
 
+void WebViewImpl::setDomainRelaxationForbidden(bool forbidden, const WebString& scheme)
+{
+    SecurityOrigin::setDomainRelaxationForbiddenForURLScheme(forbidden, String(scheme));
+}
+
 void WebViewImpl::setScrollbarColors(unsigned inactiveColor,
                                      unsigned activeColor,
                                      unsigned trackColor) {
--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/LayoutTestController.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/LayoutTestController.h
@@ -223,11 +223,8 @@ public:
     void numberOfActiveAnimations(const CppArgumentList&, CppVariant*);
     void suspendAnimations(const CppArgumentList&, CppVariant*);
     void resumeAnimations(const CppArgumentList&, CppVariant*);
-
     void disableImageLoading(const CppArgumentList&, CppVariant*);
-
     void setIconDatabaseEnabled(const CppArgumentList&, CppVariant*);
-
     void dumpSelectionRect(const CppArgumentList&, CppVariant*);
 
     // Grants permission for desktop notifications to an origin
@@ -235,6 +232,7 @@ public:
     // Simulates a click on a desktop notification.
     void simulateDesktopNotificationClick(const CppArgumentList&, CppVariant*);
 
+    void setDomainRelaxationForbiddenForURLScheme(const CppArgumentList&, CppVariant*);
     void setEditingBehavior(const CppArgumentList&, CppVariant*);
 
     // The following are only stubs.  TODO(pamg): Implement any of these that
