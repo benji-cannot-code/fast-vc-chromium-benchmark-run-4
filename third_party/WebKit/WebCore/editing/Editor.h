1b1ec97e2ad3b459203091ff64281c74ceac15db
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-14 11:07:30 +0000

    2010-09-14  Eric Seidel  <eric@webkit.org>
            Unreviewed, reverting changes r67451 and r67451.
            Broke lots of builders.
    
            Only intercept ESC key press when autocorrection UI is visible.
            https://bugs.webkit.org/show_bug.cgi?id=45071
    
            * editing/Editor.cpp:
            * editing/Editor.h:
            * editing/EditorCommand.cpp:
            (WebCore::createCommandMap):
            * loader/EmptyClients.h:
            * page/EditorClient.h:
    2010-09-14  Eric Seidel  <eric@webkit.org>
    
            Unreviewed, reverting changes r67451 and r67451.
            Broke lots of builders.
    
            Only intercept ESC key press when autocorrection UI is visible.
            https://bugs.webkit.org/show_bug.cgi?id=45071
    
            * WebCoreSupport/WebEditorClient.h:
            * WebCoreSupport/WebEditorClient.mm:
            (WebEditorClient::WebEditorClient):
            (WebEditorClient::dismissCorrectionPanel):
    2010-09-14  Eric Seidel  <eric@webkit.org>
    
            Unreviewed, reverting changes r67451 and r67451.
            Broke lots of builders.
    
            Only intercept ESC key press when autocorrection UI is visible.
            https://bugs.webkit.org/show_bug.cgi?id=45071
    
            * WebProcess/WebCoreSupport/WebEditorClient.cpp:
            (WebKit::WebEditorClient::dismissCorrectionPanel):
            * WebProcess/WebCoreSupport/WebEditorClient.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67458 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/Editor.h
+++ third_party/WebKit/WebCore/editing/Editor.h
@@ -314,7 +314,6 @@ public:
     void handleCancelOperation();
     void startCorrectionPanelTimer();
     void handleRejectedCorrection();
-    bool isShowingCorrectionPanel();
 
     void pasteAsFragment(PassRefPtr<DocumentFragment>, bool smartReplace, bool matchStyle);
     void pasteAsPlainText(const String&, bool smartReplace);
