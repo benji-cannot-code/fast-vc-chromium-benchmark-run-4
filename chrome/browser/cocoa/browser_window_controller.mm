2eb451e562b687f36df4e012715ba5a3aaabdd95
tkent@chromium.org <tkent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-14 07:48:40 +0000

    Mac: push back the detached tab position inside the dragging window.
    BUG=32597
    TEST=manual
    Patch by morrita@google
    Original code review: http://codereview.chromium.org/3333018/show
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59342 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_window_controller.mm
+++ chrome/browser/cocoa/browser_window_controller.mm
@@ -1156,7 +1156,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   gfx::Rect browserRect(windowRect.origin.x, windowRect.origin.y,
                         windowRect.size.width, windowRect.size.height);
 
-  NSRect tabRect = [tabView frame];
+  NSRect sourceTabRect = [tabView frame];
+  NSView* tabStrip = [self tabStripView];
+
+  // Pushes tabView's frame back inside the tabstrip.
+  NSSize tabOverflow =
+      [self overflowFrom:[tabStrip convertRectToBase:sourceTabRect]
+                      to:[tabStrip frame]];
+  NSRect tabRect = NSOffsetRect(sourceTabRect,
+                                -tabOverflow.width, -tabOverflow.height);
 
   // Before detaching the tab, store the pinned state.
   bool isPinned = browser_->tabstrip_model()->IsTabPinned(index);
@@ -1683,6 +1691,27 @@ willAnimateFromState:(bookmarks::VisualState)oldState
   windowTopGrowth_ = 0;
   windowBottomGrowth_ = 0;
   isShrinkingFromZoomed_ = NO;
+}
+
+- (NSSize)overflowFrom:(NSRect)source
+                    to:(NSRect)target {
+  // If |source|'s boundary is outside of |target|'s, set its distance
+  // to |x|.  Note that |source| can overflow to both side, but we
+  // have nothing to do for such case.
+  CGFloat x = 0;
+  if (NSMaxX(target) < NSMaxX(source)) // |source| overflows to right
+    x = NSMaxX(source) - NSMaxX(target);
+  else if (NSMinX(source) < NSMinX(target)) // |source| overflows to left
+    x = NSMinX(source) - NSMinX(target);
+
+  // Same as |x| above.
+  CGFloat y = 0;
+  if (NSMaxY(target) < NSMaxY(source))
+    y = NSMaxY(source) - NSMaxY(target);
+  else if (NSMinY(source) < NSMinY(target))
+    y = NSMinY(source) - NSMinY(target);
+
+  return NSMakeSize(x, y);
 }
 
 // Override to swap in the correct tab strip controller based on the new
