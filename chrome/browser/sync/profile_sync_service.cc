b091292ac4cfa242fc9659868f8a672b80810854
albertb@chromium.org <albertb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-14 23:19:54 +0000

    Hook up the dom_ui sync options to the sync service.
    BUG=chromiumos:6363
    TEST=manual
    
    Review URL: http://codereview.chromium.org/3335017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59451 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -82,6 +82,9 @@ ProfileSyncService::ProfileSyncService(ProfileSyncFactory* factory,
                  NotificationType::SYNC_CONFIGURE_DONE,
                  NotificationService::AllSources());
   registrar_.Add(this,
+                 NotificationType::SYNC_DATA_TYPES_UPDATED,
+                 NotificationService::AllSources());
+  registrar_.Add(this,
                  NotificationType::SYNC_PASSPHRASE_REQUIRED,
                  NotificationService::AllSources());
   registrar_.Add(this,
@@ -880,6 +883,14 @@ void ProfileSyncService::Observe(NotificationType type,
     case NotificationType::SYNC_PASSPHRASE_REQUIRED: {
       // TODO(sync): Show the passphrase UI here.
       SetPassphrase("dummy passphrase");
+      break;
+    }
+    case NotificationType::SYNC_DATA_TYPES_UPDATED: {
+      if (!HasSyncSetupCompleted()) break;
+
+      syncable::ModelTypeSet types;
+      GetPreferredDataTypes(&types);
+      OnUserChoseDatatypes(false, types);
       break;
     }
     case NotificationType::SYNC_PASSPHRASE_ACCEPTED: {
