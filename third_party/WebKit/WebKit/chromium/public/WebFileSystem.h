94fa7f3876506cd39660b9b16adfc482cbc57f54
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-14 06:06:59 +0000

    2010-09-13  Eric Uhrhane  <ericu@chromium.org>
            Reviewed by Darin Fisher.
    
            Add Chromium API for FileWriter
            https://bugs.webkit.org/show_bug.cgi?id=44360
    
            Build file changes.
            * WebKit.gyp:
    
            Added a way to get a WebFileWriter.
            * public/WebFileSystem.h:
            (WebKit::WebFileSystem::createFileWriter):
    
            WebFileWriter writes and truncates files.
            * public/WebFileWriter.h: Added.
    
            WebFileWriterClient reports success/failure and progress events.
            * public/WebFileWriterClient.h: Added.
    
            AsyncFileWriterChromium is a connector that links FileWriter and WebFileWriter, FileWriterClient and WebFileWriterClient.
            * src/AsyncFileWriterChromium.cpp: Added.
            * src/AsyncFileWriterChromium.h: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebFileSystem.h
+++ third_party/WebKit/WebKit/chromium/public/WebFileSystem.h
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 class WebFileSystemCallbacks;
+class WebFileWriter;
+class WebFileWriterClient;
 
 class WebFileSystem {
 public:
@@ -100,6 +102,10 @@ public:
     // WebFileSystemCallbacks::didReadDirectory() must be called when the operation is completed successfully.
     // WebFileSystemCallbacks::didFail() must be called otherwise.
     virtual void readDirectory(const WebString& path, WebFileSystemCallbacks*) { WEBKIT_ASSERT_NOT_REACHED(); }
+
+    // Creates a WebFileWriter that can be used to write to the given file.
+    // This is a fast, synchronous call, and should not stat the filesystem.
+    virtual WebFileWriter* createFileWriter(const WebString& path, WebFileWriterClient*) { WEBKIT_ASSERT_NOT_REACHED(); return 0; }
 
 protected:
     virtual ~WebFileSystem() { }
