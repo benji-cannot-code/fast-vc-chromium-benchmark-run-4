b38fb62c0da29e11e36e430e2b8bf911791b37fc
albertb@chromium.org <albertb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-15 18:41:08 +0000

    Re-land: Hook up the dom_ui sync options to the sync service.
    TBR=tim
    BUG=chromiumos:6363
    TEST=manual
    
    Review URL: http://codereview.chromium.org/3447003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59530 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/sync_options_handler.cc
+++ chrome/browser/dom_ui/sync_options_handler.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
+#include "chrome/browser/sync/sync_setup_flow.h"
 #include "chrome/common/notification_service.h"
 #include "grit/browser_resources.h"
 #include "grit/chromium_strings.h"
@@ -47,4 +48,27 @@ void SyncOptionsHandler::GetLocalizedValues(
       l10n_util::GetStringUTF16(IDS_SYNC_DATATYPE_THEMES));
   localized_strings->SetString("syncapps",
       l10n_util::GetStringUTF16(IDS_SYNC_DATATYPE_APPS));
+}
+
+void SyncOptionsHandler::Initialize() {
+  ProfileSyncService* service =
+      dom_ui_->GetProfile()->GetOriginalProfile()->GetProfileSyncService();
+  DCHECK(service);
+
+  DictionaryValue args;
+  SyncSetupFlow::GetArgsForChooseDataTypes(service, &args);
+
+  dom_ui_->CallJavascriptFunction(L"SyncOptions.setRegisteredDataTypes", args);
+}
+
+void SyncOptionsHandler::RegisterMessages() {
+  dom_ui_->RegisterMessageCallback("updatePreferredDataTypes",
+      NewCallback(this, &SyncOptionsHandler::OnPreferredDataTypesUpdated));
+}
+
+void SyncOptionsHandler::OnPreferredDataTypesUpdated(const ListValue* args) {
+  NotificationService::current()->Notify(
+      NotificationType::SYNC_DATA_TYPES_UPDATED,
+      NotificationService::AllSources(),
+      NotificationService::NoDetails());
 }
