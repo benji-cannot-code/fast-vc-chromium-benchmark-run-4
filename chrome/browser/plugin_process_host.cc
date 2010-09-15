0245eb7fee5e12883e7c8634026ae1bb4754d397
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-15 17:38:42 +0000

    FBTF: Add default ctors/dtors for various param structs.
    This patch also moves ParamTraits method definitions to the cc file.
    
    (hbono@ has a patch out that does this for render_messages_params.h, so this
    patch doesn't touch that to make sure we don't conflict.)
    
    BUG=none
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/3413003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59513 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_process_host.cc
+++ chrome/browser/plugin_process_host.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/path_service.h"
 #include "base/string_util.h"
+#include "base/utf_string_conversions.h"
 #include "chrome/browser/child_process_security_policy.h"
 #include "chrome/browser/chrome_plugin_browsing_context.h"
 #include "chrome/browser/chrome_thread.h"
