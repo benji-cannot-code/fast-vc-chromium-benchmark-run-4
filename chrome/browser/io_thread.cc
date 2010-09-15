f6acd35c437f7a280f4ba9bae038bd2376f69807
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-15 22:24:58 +0000

    Revert 59511 (potentially caused chromiumos leaks) - Take 2: Eagerly set the IO loop used for OCSP.
    Refactor nss_ocsp.cc code even more.  Split up OCSP initialization into two LazyInstances.  One is for the IO loop.  One is for registration of OCSP callbacks in NSS, which will load NSS into memory.  Sync and the normal network stack race to set their loop at the IO loop used by OCSP.  We fix that by providing a SetMessageLoopForOCSP() function which will be called in the IOThread initialization code which runs before the sync thread starts up.  Since we split this up from the registration of the OCSP callbacks, it won't also eagerly load NSS, which previously regressed startup time.  Note that the URLRequestContext pointer is still a global, not associated with either of the LazyInstances, because it's used by worker threads which can run past the lifetime of the LazyInstances.
    
    BUG=36740
    TEST=none
    
    Review URL: http://codereview.chromium.org/3446001
    
    TBR=willchan@chromium.org
    Review URL: http://codereview.chromium.org/3424010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59570 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/io_thread.cc
+++ chrome/browser/io_thread.cc
@@ -25,9 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_util.h"
 #include "net/http/http_auth_filter.h"
 #include "net/http/http_auth_handler_factory.h"
-#if defined(USE_NSS)
-#include "net/ocsp/nss_ocsp.h"
-#endif  // defined(USE_NSS)
 
 namespace {
 
@@ -178,12 +175,6 @@ void IOThread::ChangedToOnTheRecord() {
 
 void IOThread::Init() {
   BrowserProcessSubThread::Init();
-
-  DCHECK_EQ(MessageLoop::TYPE_IO, message_loop()->type());
-
-#if defined(USE_NSS)
-  net::SetMessageLoopForOCSP();
-#endif // defined(USE_NSS)
 
   DCHECK(!globals_);
   globals_ = new Globals;
