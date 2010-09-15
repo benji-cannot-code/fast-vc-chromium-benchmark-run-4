1b45b9baada7b46e2a9a6edf9ca29d30cb04e489
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-15 22:57:04 +0000

    Unreviewed.
    Reverting my changes as it is breaking several tests on Gtk and Qt.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KURL.h"
 #include "Page.h"
 #include "RenderHTMLCanvas.h"
-#include "RenderTheme.h"
 #include "SecurityOrigin.h"
 #include "Settings.h"
 #include "StrokeStyleApplier.h"
@@ -571,7 +570,7 @@ void CanvasRenderingContext2D::setStrokeColor(const String& color)
 {
     if (color == state().m_unparsedStrokeColor)
         return;
-    setStrokeStyle(CanvasStyle::createFromString(color, canvas()->document()));
+    setStrokeStyle(CanvasStyle::createFromString(color));
     state().m_unparsedStrokeColor = color;
 }
 
@@ -612,7 +611,7 @@ void CanvasRenderingContext2D::setFillColor(const String& color)
 {
     if (color == state().m_unparsedFillColor)
         return;
-    setFillStyle(CanvasStyle::createFromString(color, canvas()->document()));
+    setFillStyle(CanvasStyle::createFromString(color));
     state().m_unparsedFillColor = color;
 }
 
