10e0f1b9ce5e3f0854ced12d495cd6309962a7a7
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-15 22:20:02 +0000

    Add help dialog for connectivity issues on welcome screen.
    BUG= http://crosbug.com/6379
    TEST=Help dialogs occur on welcome screen, login screen.
    
    Review URL: http://codereview.chromium.org/3330017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59568 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/network_screen_delegate.h
+++ chrome/browser/chromeos/login/network_screen_delegate.h
@@ -20,6 +20,9 @@ class LanguageSwitchMenu;
 class NetworkScreenDelegate : public views::ButtonListener,
                               public NetworkLibrary::Observer {
  public:
+  // Cleares all error notifications.
+  virtual void ClearErrors() = 0;
+
   virtual LanguageSwitchMenu* language_switch_menu() = 0;
 
  protected:
