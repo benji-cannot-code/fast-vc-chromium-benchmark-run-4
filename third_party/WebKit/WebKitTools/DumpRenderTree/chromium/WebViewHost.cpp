ee4a1bfe67648589bc95da38714d3794f8d6061b
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-15 01:59:19 +0000

    2010-09-14  Mihai Parparita  <mihaip@chromium.org>
            Reviewed by Adam Barth.
    
            Send webkit accessibility notifications to Chromium
            https://bugs.webkit.org/show_bug.cgi?id=45156
    
            Move printf inside shouldDumpAccessibilityNotifications check.
    
            * DumpRenderTree/chromium/WebViewHost.cpp:
            (WebViewHost::postAccessibilityNotification):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebViewHost.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebViewHost.cpp
@@ -493,9 +493,9 @@ void WebViewHost::focusAccessibilityObject(const WebAccessibilityObject& object)
 
 void WebViewHost::postAccessibilityNotification(const WebAccessibilityObject& obj, WebAccessibilityNotification notification)
 {
-    printf("AccessibilityNotification - ");
-    
     if (m_shell->accessibilityController()->shouldDumpAccessibilityNotifications()) {
+        printf("AccessibilityNotification - ");
+
         switch (notification) {
         case WebAccessibilityNotificationActiveDescendantChanged:
             printf("ActiveDescendantChanged");
