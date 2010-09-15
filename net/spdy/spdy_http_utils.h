df27038a7a401723ebc0928dd960d2a8303c1c55
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-15 23:58:18 +0000

    Change forward declaration of HttpRequestInfo from class to struct to fix build breakage.
    BUG=none
    TEST=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59584 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_http_utils.h
+++ net/spdy/spdy_http_utils.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 class HttpResponseInfo;
-class HttpRequestInfo;
+struct HttpRequestInfo;
 
 // Convert a SpdyHeaderBlock into an HttpResponseInfo.
 // |headers| input parameter with the SpdyHeaderBlock.
