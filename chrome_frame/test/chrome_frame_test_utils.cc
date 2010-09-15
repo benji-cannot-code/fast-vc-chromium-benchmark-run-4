c201eb2b6a4852cdff3c921222a915c3bcfdef5a
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-15 00:33:47 +0000

    As a best effort terminate IE at the end of each chrome frame test run on an error.
    Bug=47596
    
    Review URL: http://codereview.chromium.org/3432005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59470 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/chrome_frame_test_utils.cc
+++ chrome_frame/test/chrome_frame_test_utils.cc
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome_frame_test {
 
-const int kDefaultWaitForIEToTerminateMs = 10 * 1000;
-
 const wchar_t kIEImageName[] = L"iexplore.exe";
 const wchar_t kIEBrokerImageName[] = L"ieuser.exe";
 const wchar_t kFirefoxImageName[] = L"firefox.exe";
