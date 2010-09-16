2129af8bbf0010e43f74e43ea02b88a8aea9e9a5
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-16 15:17:46 +0000

    2010-09-16  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Tor Arne Vestbø.
    
            [Qt] V8 port: Fix debug build
    
            Add missing inclusion of <wtf/text/CString.h> where necessary for debug builds.
    
            * history/PageCache.cpp:
            * html/HTMLElement.cpp:
            * html/HTMLMediaElement.cpp:
            * loader/DocumentLoader.cpp:
            * loader/icon/IconDatabase.cpp:
            * loader/icon/IconLoader.cpp:
            * storage/AbstractDatabase.cpp:
            * storage/Database.cpp:
            * storage/DatabaseSync.cpp:
            * storage/DatabaseTracker.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67619 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/history/PageCache.cpp
+++ third_party/WebKit/WebCore/history/PageCache.cpp
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SharedWorkerRepository.h"
 #include "SystemTime.h"
 #include <wtf/CurrentTime.h>
+#include <wtf/text/CString.h>
 
 using namespace std;
 
