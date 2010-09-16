3681a9b8e6e95219dd0781935774d23568edcd3f
chocobo@chromium.org <chocobo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 00:35:43 +0000

    Remove hack to auto-connect to Google-A on browser startup in ChromeOS TEST=none BUG=none Review URL: http://codereview.chromium.org/3395005
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59593 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/chromeos/cros/login_library.h"
-#include "chrome/browser/chromeos/cros/network_library.h"
 #include "chrome/browser/chromeos/external_cookie_handler.h"
 #include "chrome/browser/chromeos/input_method/input_method_util.h"
 #include "chrome/browser/chromeos/login/cookie_fetcher.h"
@@ -99,9 +98,6 @@ class LoginUtilsImpl : public LoginUtils,
                        const NotificationDetails& details);
 
  private:
-  // Attempt to connect to the preferred network if available.
-  void ConnectToPreferredNetwork();
-
   NotificationRegistrar registrar_;
 
   // Indicates if DoBrowserLaunch will actually launch the browser or not.
@@ -157,7 +153,6 @@ void LoginUtilsImpl::CompleteLogin(const std::string& username,
   }
 
   UserManager::Get()->UserLoggedIn(username);
-  ConnectToPreferredNetwork();
 
   // Now launch the initial browser window.
   FilePath user_data_dir;
@@ -239,7 +234,6 @@ void LoginUtilsImpl::CompleteOffTheRecordLogin(const GURL& start_url) {
   LOG(INFO) << "Completing off the record login";
 
   UserManager::Get()->OffTheRecordUserLoggedIn();
-  ConnectToPreferredNetwork();
 
   if (CrosLibrary::Get()->EnsureLoaded()) {
     // For BWSI we ask session manager to restart Chrome with --bwsi flag.
@@ -291,11 +285,6 @@ void LoginUtilsImpl::Observe(NotificationType type,
                              const NotificationDetails& details) {
   if (type == NotificationType::LOGIN_USER_CHANGED)
     base::OpenPersistentNSSDB();
-}
-
-void LoginUtilsImpl::ConnectToPreferredNetwork() {
-  CrosLibrary::Get()->GetNetworkLibrary()->
-      ConnectToPreferredNetworkIfAvailable();
 }
 
 LoginUtils* LoginUtils::Get() {
