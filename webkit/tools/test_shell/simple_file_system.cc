fa566e1c3ebe994525645d05b75c3b7976163ffb
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 22:18:39 +0000

    Fix SimpleFileSystem.getMetadata to readMetadata
    BUG=52799
    TEST=none
    
    Review URL: http://codereview.chromium.org/3441010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59740 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/simple_file_system.cc
+++ webkit/tools/test_shell/simple_file_system.cc
@@ -55,7 +55,7 @@ void SimpleFileSystem::remove(
   GetNewOperation(callbacks)->Remove(filepath);
 }
 
-void SimpleFileSystem::getMetadata(
+void SimpleFileSystem::readMetadata(
     const WebString& path, WebFileSystemCallbacks* callbacks) {
   FilePath filepath(webkit_glue::WebStringToFilePath(path));
 
