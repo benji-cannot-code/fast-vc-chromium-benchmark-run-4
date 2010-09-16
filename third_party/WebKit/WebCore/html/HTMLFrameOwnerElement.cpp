9735e1fd5d7e6b76a45d8c13db7b0db3f0c47d93
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-16 20:18:36 +0000

    2010-09-16  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Fix debug crash in HTMLFrameOwnerElement caused by content frame being used after destruction.
            https://bugs.webkit.org/show_bug.cgi?id=45737
    
            Test: fast/frames/frame-limit.html
    
            * html/HTMLFrameOwnerElement.cpp:
            (WebCore::HTMLFrameOwnerElement::willRemove):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67659 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLFrameOwnerElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLFrameOwnerElement.cpp
@@ -55,6 +55,7 @@ void HTMLFrameOwnerElement::willRemove()
     // FIXME: It is unclear why this can't be moved to removedFromDocument()
     // this is the only implementation of willRemove in WebCore!
     if (Frame* frame = contentFrame()) {
+        RefPtr<Frame> protect(frame);
         frame->loader()->frameDetached();
         frame->disconnectOwnerElement();
     }
