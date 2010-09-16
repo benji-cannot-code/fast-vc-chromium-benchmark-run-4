201f3a94ec7854734d9f6fa6012f938924830e5c
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 22:17:09 +0000

    Fixed notifier_unit_tests failures.
    These were introduced by r59629, but weren't caught since notifier_unit_tests
    isn't on the waterfall (bug 55799).
    
    BUG=None
    TEST=ran notifier_unit_tests manually
    
    Review URL: http://codereview.chromium.org/3416005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59739 0039d316-1c4b-4281-b951-d872f2087c98

--- jingle/notifier/base/xmpp_connection_unittest.cc
+++ jingle/notifier/base/xmpp_connection_unittest.cc
@@ -71,7 +71,7 @@ class XmppConnectionTest : public testing::Test {
   virtual ~XmppConnectionTest() {}
 
   virtual void TearDown() {
-    // Clear out any messages posted by XmppConnections.
+    // Clear out any messages posted by XmppConnection's destructor.
     message_loop_.RunAllPending();
   }
 
@@ -95,6 +95,10 @@ TEST_F(XmppConnectionTest, ImmediateFailure) {
 
   XmppConnection xmpp_connection(buzz::XmppClientSettings(),
                                  &mock_xmpp_connection_delegate_, NULL);
+
+  // We need to do this *before* |xmpp_connection| gets destroyed or
+  // our delegate won't be called.
+  message_loop_.RunAllPending();
 }
 
 TEST_F(XmppConnectionTest, PreAuthFailure) {
@@ -110,6 +114,10 @@ TEST_F(XmppConnectionTest, PreAuthFailure) {
   XmppConnection xmpp_connection(
       buzz::XmppClientSettings(), &mock_xmpp_connection_delegate_,
       mock_pre_xmpp_auth_.release());
+
+  // We need to do this *before* |xmpp_connection| gets destroyed or
+  // our delegate won't be called.
+  message_loop_.RunAllPending();
 }
 
 TEST_F(XmppConnectionTest, FailureAfterPreAuth) {
@@ -124,6 +132,10 @@ TEST_F(XmppConnectionTest, FailureAfterPreAuth) {
   XmppConnection xmpp_connection(
       buzz::XmppClientSettings(), &mock_xmpp_connection_delegate_,
       mock_pre_xmpp_auth_.release());
+
+  // We need to do this *before* |xmpp_connection| gets destroyed or
+  // our delegate won't be called.
+  message_loop_.RunAllPending();
 }
 
 TEST_F(XmppConnectionTest, RaisedError) {
