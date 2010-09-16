93917a390b727fc214242d8dec3d9522c1e664ce
kkanetkar@chromium.org <kkanetkar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 23:05:14 +0000

    Unittest renamed to chrome_file_system_operation_unittest.cc. Deleting original one. Svn rename didn't quite work. Also changed the comments in operations file to have clearer message.
    BUG=None
    TEST=none
    
    Review URL: http://codereview.chromium.org/3415012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59748 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/file_system/chrome_file_system_operation.h
+++ chrome/browser/file_system/chrome_file_system_operation.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/fileapi/file_system_operation.h"
 
 // This class is designed to serve one-time file system operation per instance.
-// Each operation method (CreateFile, CreateDirectory, Copy, Move,
-// DirectoryExists, GetMetadata, ReadDirectory and Remove) must be called
-// only once in its lifetime.
+// Only one method(CreateFile, CreateDirectory, Copy, Move, DirectoryExists,
+// GetMetadata, ReadDirectory and Remove) may be called during the lifetime of
+// this object and it should be called no more than once.
 class ChromeFileSystemOperation : public fileapi::FileSystemOperation {
  public:
   ChromeFileSystemOperation(
