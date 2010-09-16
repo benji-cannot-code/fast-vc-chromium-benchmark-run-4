420819603cfc2897f02481f1ec129bf00d6dbf0b
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-16 11:13:15 +0000

    2010-09-16  Jeremy Orlow  <jorlow@chromium.org>
            Speculative build fix.
    
            * src/IDBIndexBackendProxy.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67603 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/IDBIndexBackendProxy.cpp
+++ third_party/WebKit/WebKit/chromium/src/IDBIndexBackendProxy.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(INDEXED_DATABASE)
 
 #include "IDBCallbacks.h"
+#include "IDBKeyRange.h"
 #include "WebIDBCallbacksImpl.h"
 #include "WebIDBDatabaseError.h"
 #include "WebIDBIndex.h"
