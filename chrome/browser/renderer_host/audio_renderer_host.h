656de822de371f9294f00b168d1b16c02ca7c52e
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 01:15:45 +0000

    Revert 59600 - Make AudioOutputController.Close() truly asynchronous. Added closed_task parameter in this method. This parameter is used to notify the caller when the stream is actually closed. Callbacks may be called until closed_task is executed.
    BUG=55755
    TEST=Unittests, audio still works, doesn't crash
    
    Review URL: http://codereview.chromium.org/3415007
    
    TBR=sergeyu@chromium.org
    Review URL: http://codereview.chromium.org/3425008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59601 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/audio_renderer_host.h
+++ chrome/browser/renderer_host/audio_renderer_host.h
@@ -87,8 +87,7 @@ class AudioRendererHost : public base::RefCountedThreadSafe<
     AudioEntry()
         : render_view_id(0),
           stream_id(0),
-          pending_buffer_request(false),
-          pending_close(false) {
+          pending_buffer_request(false) {
     }
 
     // The AudioOutputController that manages the audio stream.
@@ -108,9 +107,6 @@ class AudioRendererHost : public base::RefCountedThreadSafe<
     scoped_ptr<media::AudioOutputController::SyncReader> reader;
 
     bool pending_buffer_request;
-
-    // Set to true after we called Close() for the controller.
-    bool pending_close;
   };
 
   typedef std::map<AudioEntryId, AudioEntry*> AudioEntryMap;
@@ -223,13 +219,6 @@ class AudioRendererHost : public base::RefCountedThreadSafe<
 
   // Delete all audio entry and all audio streams
   void DeleteEntries();
-
-  // Closes the stream. The stream is then deleted in DeleteEntry() after it
-  // is closed.
-  void CloseAndDeleteStream(AudioEntry* entry);
-
-  // Called on the audio thread after the audio stream is closed.
-  void OnStreamClosed(AudioEntry* entry);
 
   // Delete an audio entry and close the related audio stream.
   void DeleteEntry(AudioEntry* entry);
--- media/audio/audio_output_controller.h
+++ media/audio/audio_output_controller.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/simple_sources.h"
 
 class MessageLoop;
-class Task;
 
 // An AudioOutputController controls an AudioOutputStream and provides data
 // to this output stream. It has an important function that it executes
@@ -136,14 +135,13 @@ class AudioOutputController
   // has effect when the stream is paused.
   void Flush();
 
-  // Closes the audio output stream. The state is changed and the resources
-  // are freed on the audio thread. closed_task is executed after that.
-  // Callbacks (EventHandler and SyncReader) must exist until closed_task is
-  // called.
+  // Closes the audio output stream. It changes state to kClosed and returns
+  // right away. The physical resources are freed on the audio thread if
+  // neccessary.
   //
   // It is safe to call this method more than once. Calls after the first one
   // will have no effect.
-  void Close(Task* closed_task);
+  void Close();
 
   // Sets the volume of the audio output stream.
   void SetVolume(double volume);
@@ -171,7 +169,7 @@ class AudioOutputController
   void DoPlay();
   void DoPause();
   void DoFlush();
-  void DoClose(Task* closed_task);
+  void DoClose();
   void DoSetVolume(double volume);
   void DoReportError(int code);
 
@@ -193,7 +191,8 @@ class AudioOutputController
   uint32 hardware_pending_bytes_;
   base::Time last_callback_time_;
 
-  // The |lock_| must be acquired whenever we access |push_source_|.
+  // The |lock_| must be acquired whenever we access |state_| or call
+  // |handler_|.
   Lock lock_;
 
   // PushSource role is to buffer and it's only used in regular latency mode.
