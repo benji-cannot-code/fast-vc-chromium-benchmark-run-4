e9570167c064a1e1d68f7328d91bd09000888539
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 00:52:58 +0000

    Make our X11 error handler more descriptive
    BUG=none
    TEST=trigger X11 error, look at the error dialog
    
    Review URL: http://codereview.chromium.org/3425006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59595 0039d316-1c4b-4281-b951-d872f2087c98

--- app/x11_util.cc
+++ app/x11_util.cc
@@ -56,7 +56,7 @@ CachedPictFormats* get_cached_pict_formats() {
 const size_t kMaxCacheSize = 5;
 
 int DefaultX11ErrorHandler(Display* d, XErrorEvent* e) {
-  LOG(FATAL) << GetErrorEventDescription(e);
+  LOG(FATAL) << GetErrorEventDescription(d, e);
   return 0;
 }
 
@@ -835,9 +835,13 @@ void SetX11ErrorHandlers(XErrorHandler error_handler,
       io_error_handler ? io_error_handler : DefaultX11IOErrorHandler);
 }
 
-std::string GetErrorEventDescription(XErrorEvent* error_event) {
+std::string GetErrorEventDescription(Display* dpy, XErrorEvent* error_event) {
+  char buf[255];
+  XGetErrorText(dpy, error_event->error_code, buf, 254);
   return base::StringPrintf(
-      "X Error detected: %lu error_code %u request_code %u minor_code %u",
+      "X Error detected: %s "
+      "(serial: %lu, error_code: %u, request_code: %u, minor_code: %u)",
+      buf,
       error_event->serial,
       error_event->error_code,
       error_event->request_code,
