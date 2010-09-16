b071c2bc8e62a0aab18066c60589641382469fa2
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-16 13:56:43 +0000

    2010-09-16  Andrey Kosyakov  <caseq@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: [Resources panel] [HAR] Need a way to save timing data.
            Added support to export HAR to file from Resources panel (conditional on Preferences)
            Added support for HARLog (a higher-level aggregate than HAREntry)
            https://bugs.webkit.org/show_bug.cgi?id=45663
    
            * English.lproj/localizedStrings.js:
            * inspector/front-end/HAREntry.js:
            (WebInspector.HAREntry.prototype.build):
            (WebInspector.HAREntry.prototype._buildTimings):
            (WebInspector.HAREntry._toMilliseconds):
            (WebInspector.HARLog):
            (WebInspector.HARLog.prototype.build):
            (WebInspector.HARLog.prototype._buildPages):
            (WebInspector.HARLog.prototype._buildMainResourceTimings):
            (WebInspector.HARLog.prototype._convertResource):
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.ResourcesPanel):
            (WebInspector.ResourcesPanel.prototype.hide):
            (WebInspector.ResourcesPanel.prototype._contextMenu):
            (WebInspector.ResourcesPanel.prototype._exportAll):
            (WebInspector.ResourcesPanel.prototype._exportResource):
            * inspector/front-end/Settings.js:
            * inspector/front-end/utilities.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/Settings.js
+++ third_party/WebKit/WebCore/inspector/front-end/Settings.js
@@ -45,7 +45,8 @@ var Preferences = {
     profilerAlwaysEnabled: false,
     auditsPanelEnabled: true,
     onlineDetectionEnabled: true,
-    domBreakpointsEnabled: false
+    domBreakpointsEnabled: false,
+    resourceExportEnabled: false
 }
 
 WebInspector.Settings = function(sessionScope)
