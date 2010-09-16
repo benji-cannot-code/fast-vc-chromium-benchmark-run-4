5e735a179072d8637533d38944abf80f0aeb89d2
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 13:41:36 +0000

    Don't evict username/password when handling "stale" digest authentication challenges.
    Stale challenges indicate that the cached nonce (and associated nonce_count) are no longer valid.
    
    BUG=53353
    TEST=Use a digest authentication Squid proxy with default settings (which complain about stale nonce's). Ensure that there are no username/password prompts after getting a stale challenge. Also, net_unittests. 
    
    Review URL: http://codereview.chromium.org/3421005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59643 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_unittest.cc
+++ net/http/http_auth_unittest.cc
@@ -136,12 +136,15 @@ TEST(HttpAuthTest, HandleChallengeResponse_RequestBased) {
       HeadersFromResponseText(
           "HTTP/1.1 401 Unauthorized\n"
           "WWW-Authenticate: Mock token_here\n"));
+  std::string challenge_used;
   EXPECT_EQ(HttpAuth::AUTHORIZATION_RESULT_REJECT,
             HttpAuth::HandleChallengeResponse(
                 mock_handler.get(),
                 headers.get(),
                 HttpAuth::AUTH_SERVER,
-                disabled_schemes));
+                disabled_schemes,
+                &challenge_used));
+  EXPECT_EQ("Mock token_here", challenge_used);
 }
 
 TEST(HttpAuthTest, HandleChallengeResponse_ConnectionBased) {
@@ -151,12 +154,15 @@ TEST(HttpAuthTest, HandleChallengeResponse_ConnectionBased) {
       HeadersFromResponseText(
           "HTTP/1.1 401 Unauthorized\n"
           "WWW-Authenticate: Mock token_here\n"));
+  std::string challenge_used;
   EXPECT_EQ(HttpAuth::AUTHORIZATION_RESULT_ACCEPT,
             HttpAuth::HandleChallengeResponse(
                 mock_handler.get(),
                 headers.get(),
                 HttpAuth::AUTH_SERVER,
-                disabled_schemes));
+                disabled_schemes,
+                &challenge_used));
+  EXPECT_EQ("Mock token_here", challenge_used);
 }
 
 TEST(HttpAuthTest, HandleChallengeResponse_ConnectionBasedNoMatch) {
@@ -166,12 +172,15 @@ TEST(HttpAuthTest, HandleChallengeResponse_ConnectionBasedNoMatch) {
       HeadersFromResponseText(
           "HTTP/1.1 401 Unauthorized\n"
           "WWW-Authenticate: Basic realm=\"happy\"\n"));
+  std::string challenge_used;
   EXPECT_EQ(HttpAuth::AUTHORIZATION_RESULT_REJECT,
             HttpAuth::HandleChallengeResponse(
                 mock_handler.get(),
                 headers.get(),
                 HttpAuth::AUTH_SERVER,
-                disabled_schemes));
+                disabled_schemes,
+                &challenge_used));
+  EXPECT_TRUE(challenge_used.empty());
 }
 
 TEST(HttpAuthTest, ChallengeTokenizer) {
