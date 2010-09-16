63ce3966d5de7ebd6061c84b043750f49c8a6c4e
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 20:44:08 +0000

    Make the test code that changes the GoogleBaseURL do a notification (instead of directly calling a method) to more closely mimic the way this usually happens.
    This is being done to avoid making special "update GoogleBaseURL" method
    calls both here and in future tests, and helps ensure that the notification
    observer code is set-up correctly.
    
    BUG=38475
    TEST=unit_tests --gtest_filter=Temp*
    
    Review URL: http://codereview.chromium.org/3420007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59709 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_model_test_util.cc
+++ chrome/browser/search_engines/template_url_model_test_util.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_temp_dir.h"
 #include "chrome/browser/search_engines/template_url.h"
 #include "chrome/browser/search_engines/template_url_model.h"
+#include "chrome/common/notification_service.h"
+#include "chrome/common/notification_type.h"
 #include "chrome/test/testing_profile.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -188,6 +190,9 @@ std::wstring TemplateURLModelTestUtil::GetAndClearSearchTerm() {
 void TemplateURLModelTestUtil::SetGoogleBaseURL(
     const std::string& base_url) const {
   TemplateURLRef::SetGoogleBaseURL(new std::string(base_url));
+  NotificationService::current()->Notify(NotificationType::GOOGLE_URL_UPDATED,
+                                         NotificationService::AllSources(),
+                                         NotificationService::NoDetails());
 }
 
 WebDataService* TemplateURLModelTestUtil::GetWebDataService() {
