6734e7699f11c0e1848887ce8217448c351902e1
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 16:01:18 +0000

    [GTK] Apply SetLabelWidth workaround in more places.
    - first run search engine ballot explanatory text
    - first run bubble text
    - sync setup label in options dialog's second tab
    
    BUG=54898
    TEST=all places listed above look good in LTR and at least ok in RTL
    
    Review URL: http://codereview.chromium.org/3443006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59657 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/first_run_bubble.cc
+++ chrome/browser/gtk/first_run_bubble.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser.h"
 #include "chrome/browser/browser_list.h"
 #include "chrome/browser/gtk/gtk_theme_provider.h"
+#include "chrome/browser/gtk/gtk_util.h"
 #include "chrome/browser/search_engines/util.h"
 #include "chrome/common/notification_service.h"
 #include "gfx/gtk_util.h"
@@ -134,13 +135,11 @@ void FirstRunBubble::InitializeContentForLarge() {
   GtkWidget* label2 = gtk_label_new(
       l10n_util::GetStringUTF8(IDS_FR_BUBBLE_SUBTEXT).c_str());
   labels_.push_back(label2);
-  gtk_label_set_line_wrap(GTK_LABEL(label2), TRUE);
 
   string16 search_engine = GetDefaultSearchEngineName(profile_);
   GtkWidget* label3 = gtk_label_new(
       l10n_util::GetStringFUTF8(IDS_FR_BUBBLE_QUESTION, search_engine).c_str());
   labels_.push_back(label3);
-  gtk_label_set_line_wrap(GTK_LABEL(label3), TRUE);
 
   GtkWidget* keep_button = gtk_button_new_with_label(
       l10n_util::GetStringFUTF8(IDS_FR_BUBBLE_OK, search_engine).c_str());
@@ -189,7 +188,6 @@ void FirstRunBubble::InitializeContentForMinimal() {
   GtkWidget* label2 =
       gtk_label_new(l10n_util::GetStringUTF8(IDS_FR_BUBBLE_SUBTEXT).c_str());
   labels_.push_back(label2);
-  gtk_label_set_line_wrap(GTK_LABEL(label2), TRUE);
 
   gtk_box_pack_start(GTK_BOX(content_), label1, FALSE, FALSE, 0);
   gtk_box_pack_start(GTK_BOX(content_), label2, FALSE, FALSE, 0);
@@ -205,8 +203,8 @@ void FirstRunBubble::InitializeLabels(int width_resource) {
     // Resize the labels so that they don't wrap more than necessary.  We leave
     // |content_| unsized so that it'll expand as needed to hold the other
     // widgets -- the buttons may be wider than |width| on high-DPI displays.
-    gtk_widget_set_size_request(labels_[i], width, -1);
-    gtk_misc_set_alignment(GTK_MISC(labels_[i]), 0, .5);
+    gtk_util::SetLabelWidth(labels_[i], width);
+    gtk_misc_set_alignment(GTK_MISC(labels_[i]), 0, 0.5);
   }
 }
 
