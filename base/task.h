29d1365c730f306b5a93744792b20a79bffbf2d7
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 00:33:48 +0000

    Various style fixes (indenting, captitalization, adding some comments).
    Also removed a function that is no longer called.
    
    BUG=None
    TEST=Build works.
    
    Review URL: http://codereview.chromium.org/3453006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59592 0039d316-1c4b-4281-b951-d872f2087c98

--- base/task.h
+++ base/task.h
@@ -108,30 +108,30 @@ class ScopedRunnableMethodFactory {
 
   template <class Method, class A, class B, class C>
   inline CancelableTask* NewRunnableMethod(Method method,
-                                 const A& a,
-                                 const B& b,
-                                 const C& c) {
+                                           const A& a,
+                                           const B& b,
+                                           const C& c) {
     return new RunnableMethod<Method, Tuple3<A, B, C> >(
         weak_factory_.GetWeakPtr(), method, MakeTuple(a, b, c));
   }
 
   template <class Method, class A, class B, class C, class D>
   inline CancelableTask* NewRunnableMethod(Method method,
-                                 const A& a,
-                                 const B& b,
-                                 const C& c,
-                                 const D& d) {
+                                           const A& a,
+                                           const B& b,
+                                           const C& c,
+                                           const D& d) {
     return new RunnableMethod<Method, Tuple4<A, B, C, D> >(
         weak_factory_.GetWeakPtr(), method, MakeTuple(a, b, c, d));
   }
 
   template <class Method, class A, class B, class C, class D, class E>
   inline CancelableTask* NewRunnableMethod(Method method,
-                                 const A& a,
-                                 const B& b,
-                                 const C& c,
-                                 const D& d,
-                                 const E& e) {
+                                           const A& a,
+                                           const B& b,
+                                           const C& c,
+                                           const D& d,
+                                           const E& e) {
     return new RunnableMethod<Method, Tuple5<A, B, C, D, E> >(
         weak_factory_.GetWeakPtr(), method, MakeTuple(a, b, c, d, e));
   }
@@ -144,7 +144,8 @@ class ScopedRunnableMethodFactory {
   template <class Method, class Params>
   class RunnableMethod : public CancelableTask {
    public:
-    RunnableMethod(const base::WeakPtr<T>& obj, Method meth,
+    RunnableMethod(const base::WeakPtr<T>& obj,
+                   Method meth,
                    const Params& params)
         : obj_(obj),
           meth_(meth),
@@ -188,6 +189,7 @@ class DeleteTask : public CancelableTask {
   virtual void Cancel() {
     obj_ = NULL;
   }
+
  private:
   T* obj_;
 };
@@ -205,6 +207,7 @@ class ReleaseTask : public CancelableTask {
   virtual void Cancel() {
     obj_ = NULL;
   }
+
  private:
   T* obj_;
 };
