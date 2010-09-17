454058ea197d0b8f4112e6f79984e623af0fddcc
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-17 21:05:16 +0000

    Remove unnecessary log spew from ProfileSyncService.
    BUG=none.
    TEST=examine logs from browser/ui/unit/interactive_ui tests, profile_sync_service should not be in top 20 spammers.
    
    Review URL: http://codereview.chromium.org/3436017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59848 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -91,13 +91,8 @@ ProfileSyncService::ProfileSyncService(ProfileSyncFactory* factory,
   // If this is an official build, it will always be one of the above.
   std::string channel = platform_util::GetVersionStringModifier();
   if (channel.empty() || channel == "beta") {
-    LOG(INFO) << "Detected official build, using official sync server.";
     sync_service_url_ = GURL(kSyncServerUrl);
-  } else {
-    LOG(INFO) << "Detected official build, but using dev channel sync server.";
   }
-#else
-  LOG(INFO) << "Unofficial build, using dev channel sync server.";
 #endif
 }
 
@@ -150,7 +145,6 @@ void ProfileSyncService::LoadMigratedCredentials(const std::string& username,
 }
 
 void ProfileSyncService::Initialize() {
-  LOG(INFO) << "Starting ProfileSyncService.";
   InitSettings();
   RegisterPreferences();
 
@@ -256,8 +250,6 @@ void ProfileSyncService::InitSettings() {
       }
     }
   }
-
-  LOG(INFO) << "Using " << sync_service_url_ << " for sync server URL.";
 
   // Override the notification server host from the command-line, if provided.
   if (command_line.HasSwitch(switches::kSyncNotificationHost)) {
@@ -387,8 +379,6 @@ void ProfileSyncService::StartUp() {
 
   DCHECK(AreCredentialsAvailable());
 
-  LOG(INFO) << "ProfileSyncService bringing up backend host.";
-
   last_synced_time_ = base::Time::FromInternalValue(
       profile_->GetPrefs()->GetInt64(prefs::kSyncLastSyncedTime));
 
@@ -437,8 +427,6 @@ void ProfileSyncService::ClearServerData() {
 }
 
 void ProfileSyncService::DisableForUser() {
-  LOG(INFO) << "Disabling sync for user.";
-
   // Clear prefs (including SyncSetupHasCompleted) before shutting down so
   // PSS clients don't think we're set up while we're shutting down.
   ClearPreferences();
