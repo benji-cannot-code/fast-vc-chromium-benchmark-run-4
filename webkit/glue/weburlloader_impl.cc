45eebf5b4a8b7574ea776aa3413888721d3d7887
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-17 04:25:14 +0000

    Change all callers of WebFileInfo::appendFileRange() to use the new method.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3405010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59767 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/weburlloader_impl.cc
+++ webkit/glue/weburlloader_impl.cc
@@ -432,7 +432,7 @@ void WebURLLoaderImpl::Context::Start(
                 WebStringToFilePath(element.filePath),
                 static_cast<uint64>(element.fileStart),
                 static_cast<uint64>(element.fileLength),
-                base::Time::FromDoubleT(element.fileInfo.modificationTime));
+                base::Time::FromDoubleT(element.modificationTime));
           }
           break;
         case WebHTTPBody::Element::TypeBlob:
