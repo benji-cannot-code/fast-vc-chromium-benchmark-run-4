1a7c5a8c2fb43cea968d1a7612708b1bb83d565e
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-17 19:00:03 +0000

    2010-09-17  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Chris Marrin.
    
            Remove scroll and clip layers for WKCACFLayerRenderer
            https://bugs.webkit.org/show_bug.cgi?id=45922
    
            WKCACFLayerRenderer no longer needs its own layers for managing scrolling
            and clipping, because RenderLayerCompositor provides this functionality.
    
            * platform/graphics/win/MediaPlayerPrivateFullscreenWindow.cpp:
            (WebCore::MediaPlayerPrivateFullscreenWindow::setRootChildLayer): Remove call to setScrollFrame().
            (WebCore::MediaPlayerPrivateFullscreenWindow::wndProc): Ditto.
    
            * platform/graphics/win/WKCACFLayerRenderer.cpp: Remove references to scroll and clip layers
            (WebCore::WKCACFLayerRenderer::WKCACFLayerRenderer):
            (WebCore::WKCACFLayerRenderer::setRootChildLayer):
            (WebCore::WKCACFLayerRenderer::destroyRenderer):
            (WebCore::WKCACFLayerRenderer::resize):
            * platform/graphics/win/WKCACFLayerRenderer.h:
    
            * rendering/RenderLayerCompositor.cpp:
            (WebCore::RenderLayerCompositor::layerTreeAsText): Dump the layers from the root platform layer.
            (WebCore::RenderLayerCompositor::requiresScrollLayer): On Windows,
            always say yes.
    
    2010-09-17  Simon Fraser  <simon.fraser@apple.com>
    
            Reviewed by Chris Marrin.
    
            Remove scroll and clip layers for WKCACFLayerRenderer
            https://bugs.webkit.org/show_bug.cgi?id=45922
    
            WKCACFLayerRenderer no longer needs its own layers for managing scrolling
            and clipping, because RenderLayerCompositor provides this functionality.
    
            * WebView.cpp:
            (WebView::sizeChanged): Moved code that handles the WM_SIZE message
            into this method. Use it to resize the layer renderer.
            (WebView::WebViewWndProc): Call sizeChanged().
            (WebView::updateRootLayerContents): No need to call setScrollFrame() any more.
            (WebView::layerRendererBecameVisible): Move this from the header (no need to be inline).
            * WebView.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67735 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayerRenderer.h
+++ third_party/WebKit/WebCore/platform/graphics/win/WKCACFLayerRenderer.h
@@ -67,7 +67,6 @@ public:
     static bool acceleratedCompositingAvailable();
     static void didFlushContext(CACFContextRef);
 
-    void setScrollFrame(const IntPoint&, const IntSize&);
     void setRootContents(CGImageRef);
     void setRootContentsAndDisplay(CGImageRef);
     void setRootChildLayer(WKCACFLayer* layer);
@@ -79,7 +78,6 @@ public:
     void destroyRenderer();
     void resize();
     void renderSoon();
-    void updateScrollFrame();
 
 protected:
     WKCACFLayer* rootLayer() const;
@@ -106,16 +104,12 @@ private:
     bool m_mightBeAbleToCreateDeviceLater;
     COMPtr<IDirect3DDevice9> m_d3dDevice;
     RefPtr<WKCACFRootLayer> m_rootLayer;
-    RefPtr<WKCACFLayer> m_scrollLayer;
     RefPtr<WKCACFLayer> m_rootChildLayer;
-    RefPtr<WKCACFLayer> m_clipLayer;
     RetainPtr<CACFContextRef> m_context;
     CARenderContext* m_renderContext;
     CARenderOGLContext* m_renderer;
     HWND m_hostWindow;
     Timer<WKCACFLayerRenderer> m_renderTimer;
-    IntPoint m_scrollPosition;
-    IntSize m_scrollSize;
     bool m_backingStoreDirty;
     bool m_mustResetLostDeviceBeforeRendering;
 #ifndef NDEBUG
--- third_party/WebKit/WebCore/rendering/RenderLayerCompositor.h
+++ third_party/WebKit/WebCore/rendering/RenderLayerCompositor.h
@@ -167,6 +167,8 @@ public:
     void frameViewDidChangeSize(const IntPoint& contentsOffset = IntPoint());
     void frameViewDidScroll(const IntPoint& = IntPoint());
 
+    String layerTreeAsText();
+
 private:
     // Whether the given RL needs a compositing layer.
     bool needsToBeComposited(const RenderLayer*) const;
