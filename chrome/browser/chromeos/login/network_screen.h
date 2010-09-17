fc8272c01b63a34d4175df1f209caca0af8ce271
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-17 07:54:33 +0000

    Added tests for OOBE network screen
    Add new tests instead of tests disabled in http://codereview.chromium.org/3166028
    
    BUG=chromium-os:6672
    TEST=browser_tests --gtest_filter=NetworkScreenTest.*
    
    Review URL: http://codereview.chromium.org/3441008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59774 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/network_screen.h
+++ chrome/browser/chromeos/login/network_screen.h
@@ -52,6 +52,8 @@ class NetworkScreen : public ViewScreen<NetworkSelectionView>,
   void Refresh();
 
  private:
+  FRIEND_TEST(NetworkScreenTest, Timeout);
+
   // ViewScreen implementation:
   virtual void CreateView();
   virtual NetworkSelectionView* AllocateView();
