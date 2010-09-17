fb5217e1a763452f474177bf0f15933f6f29f58a
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-17 12:28:32 +0000

    [Mac] Replace the existing browser-child mach ipc with a long-lived listener on a well-known port.
    Before this CL:
    Before fork()ing a child, the browser process creates a mach receive port with a random name.  After the fork() but before exec(), the child uses mach ipc to transmit send rights to its task port.  The child has access to the random name because it inherits it from the browser process.  Unfortunately, some of the library functions involved in sending a mach message are not safe to call after fork().
    
    After this CL:
    Before forking the first child, the browser spins off a new thread that listens on a well-known port for mach ipc from any process.  This well-known port is "com.google.Chrome.<browserpid>".  When a child process starts up, it sends a mach message to its parent browser's well-known port.  On the browser side, we listen for said message, extract the pid of the sending process, and ignore any messages from processes we did not personally fork().  This check is necessary because any arbitrary process on the system could send mach ipc to that port.
    
    BUG=35374
    TEST=Browser should still start up.  The task manager should still show correct cpu/memory data.  There should be no perf regressions.  
    TEST=Mac ui_tests and browser_tests should be less flaky.
    Review URL: http://codereview.chromium.org/3443002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59782 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/child_process_launcher.cc
+++ chrome/browser/child_process_launcher.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>  // For std::pair.
 
 #include "base/command_line.h"
+#include "base/lock.h"
 #include "base/logging.h"
 #include "base/scoped_ptr.h"
 #include "base/thread.h"
@@ -162,20 +163,29 @@ class ChildProcessLauncher::Context
       }
 #endif  // defined(OS_LINUX)
 
-      // Actually launch the app.
-      bool launched;
+      bool launched = false;
 #if defined(OS_MACOSX)
-      task_t child_task;
-      launched = base::LaunchAppAndGetTask(
-          cmd_line->argv(), env, fds_to_map, false, &child_task, &handle);
-      if (launched && child_task != MACH_PORT_NULL) {
-          MachBroker::instance()->RegisterPid(
-              handle,
-              MachBroker::MachInfo().SetTask(child_task));
-      }
-#else
-      launched = base::LaunchApp(cmd_line->argv(), env, fds_to_map,
-                                 /* wait= */false, &handle);
+      // It is possible for the child process to die immediately after
+      // launching.  To prevent leaking MachBroker map entries in this case,
+      // lock around all of LaunchApp().  If the child dies, the death
+      // notification will be processed by the MachBroker after the call to
+      // AddPlaceholderForPid(), enabling proper cleanup.
+      {  // begin scope for AutoLock
+        MachBroker* broker = MachBroker::instance();
+        AutoLock lock(broker->GetLock());
+
+        // This call to |PrepareForFork()| will start the MachBroker listener
+        // thread, if it is not already running.  Therefore the browser process
+        // will be listening for Mach IPC before LaunchApp() is called.
+        broker->PrepareForFork();
+#endif
+        // Actually launch the app.
+        launched = base::LaunchApp(cmd_line->argv(), env, fds_to_map,
+                                   /* wait= */false, &handle);
+#if defined(OS_MACOSX)
+        if (launched)
+          broker->AddPlaceholderForPid(handle);
+      }  // end scope for AutoLock
 #endif
       if (!launched)
         handle = base::kNullProcessHandle;
