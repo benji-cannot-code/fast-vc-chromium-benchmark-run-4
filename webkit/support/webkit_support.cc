68c9a64b635b7630504bc4374bfb4105eae25ca1
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-17 22:35:08 +0000

    Force DumpRenderTree to use the Mesa backend for OpenGL as was previously done for test_shell. Verified that Mesa will be used for WebGL tests after DRT dependencies are updated.
    BUG=none
    TEST=none (ran a couple of WebGL tests in DRT via new-run-webkit-tests)
    
    Review URL: http://codereview.chromium.org/3439007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59866 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/webkit_support.cc
+++ webkit/support/webkit_support.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/support/webkit_support.h"
 
+#include "app/gfx/gl/gl_implementation.h"
 #include "base/at_exit.h"
 #include "base/base64.h"
 #include "base/command_line.h"
@@ -106,6 +107,10 @@ class TestEnvironment {
     if (!unit_test_mode) {
       at_exit_manager_.reset(new base::AtExitManager);
       InitLogging(false);
+
+      // Default to OSMesa for GL, for testing WebGL, 3D CSS and other
+      // GPU-related APIs.
+      gfx::InitializeGLBindings(gfx::kGLImplementationOSMesaGL);
     }
     main_message_loop_.reset(new MessageLoopForUI);
     // TestWebKitClient must be instantiated after the MessageLoopForUI.
