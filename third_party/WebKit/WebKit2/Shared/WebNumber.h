618443d5efbbc8caed9287ace218b36c1156f8dd
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-17 20:11:26 +0000

    Make WebNumbers immutable matching CF.
    Reviewed by Anders Carlsson.
    
    * Shared/WebNumber.h:
    (WebKit::WebNumber::value):
    (WebKit::WebNumber::WebNumber):
    * UIProcess/API/C/WKNumber.cpp:
    (WKUInt64GetValue):
    * UIProcess/API/C/WKNumber.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Shared/WebNumber.h
+++ third_party/WebKit/WebKit2/Shared/WebNumber.h
@@ -43,17 +43,16 @@ public:
     }
 
     NumberType value() const { return m_value; }
-    void setValue(NumberType value) { m_value = value; }
 
 private:
-    WebNumber(NumberType value)
+    explicit WebNumber(NumberType value)
         : m_value(value)
     {
     }
 
     virtual Type type() const { return APIType; }
 
-    NumberType m_value;
+    const NumberType m_value;
 };
 
 typedef WebNumber<bool, APIObject::TypeBoolean> WebBoolean;
