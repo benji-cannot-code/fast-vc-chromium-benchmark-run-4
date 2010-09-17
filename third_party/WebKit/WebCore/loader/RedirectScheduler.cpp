a4db0badea21bcd5229a6c43eb5e1b41c7f59137
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-17 16:36:53 +0000

    2010-09-17  Johnny Ding  <jnd@chromium.org>
            Reviewed by Adam Barth.
    
            Stop history reload navigation to bypass WebKit's popup blocker.
            Now history reload can only navigate the page in self frame, no matter
            what target frame is defined in <base> and no new window can be created.
            https://bugs.webkit.org/show_bug.cgi?id=45369
    
            Test: fast/events/popup-blocked-from-history-reload.html
    
            * loader/RedirectScheduler.cpp:
            (WebCore::ScheduledNavigation::ScheduledNavigation):
            (WebCore::ScheduledNavigation::wasUserGesture):
            Move the m_wasUserGesture to base class ScheduledNavigation. Then all
            asynchronous navigation situations can restore the correct gesture state
            during the real navigation process.
            (WebCore::ScheduledURLNavigation::ScheduledURLNavigation):
            (WebCore::ScheduledURLNavigation::fire):
            (WebCore::ScheduledURLNavigation::referrer):
            (WebCore::ScheduledHistoryNavigation::ScheduledHistoryNavigation):
            (WebCore::ScheduledHistoryNavigation::fire):
            (WebCore::ScheduledFormSubmission::ScheduledFormSubmission):
            (WebCore::ScheduledFormSubmission::fire):
            (WebCore::RedirectScheduler::scheduleHistoryNavigation):
    2010-09-17  Johnny Ding  <jnd@chromium.org>
    
            Reviewed by Adam Barth.
    
            Stop history reload navigation to bypass WebKit's popup blocker.
            https://bugs.webkit.org/show_bug.cgi?id=45369
    
            * fast/events/popup-blocked-from-history-reload-expected.txt: Added.
            * fast/events/popup-blocked-from-history-reload.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67716 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/RedirectScheduler.cpp
+++ third_party/WebKit/WebCore/loader/RedirectScheduler.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RedirectScheduler.h"
 
 #include "BackForwardList.h"
+#include "DOMWindow.h"
 #include "DocumentLoader.h"
 #include "Event.h"
 #include "FormState.h"
@@ -42,9 +43,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoadRequest.h"
 #include "FrameLoader.h"
 #include "FrameLoaderStateMachine.h"
-#include "HistoryItem.h"
 #include "HTMLFormElement.h"
 #include "HTMLFrameOwnerElement.h"
+#include "HistoryItem.h"
 #include "Page.h"
 #include "UserGestureIndicator.h"
 #include <wtf/CurrentTime.h>
@@ -53,12 +54,13 @@ namespace WebCore {
 
 class ScheduledNavigation : public Noncopyable {
 public:
-    ScheduledNavigation(double delay, bool lockHistory, bool lockBackForwardList, bool wasDuringLoad, bool isLocationChange)
+    ScheduledNavigation(double delay, bool lockHistory, bool lockBackForwardList, bool wasDuringLoad, bool isLocationChange, bool wasUserGesture)
         : m_delay(delay)
         , m_lockHistory(lockHistory)
         , m_lockBackForwardList(lockBackForwardList)
         , m_wasDuringLoad(wasDuringLoad)
         , m_isLocationChange(isLocationChange)
+        , m_wasUserGesture(wasUserGesture)
     {
     }
     virtual ~ScheduledNavigation() { }
@@ -74,6 +76,7 @@ public:
     bool lockBackForwardList() const { return m_lockBackForwardList; }
     bool wasDuringLoad() const { return m_wasDuringLoad; }
     bool isLocationChange() const { return m_isLocationChange; }
+    bool wasUserGesture() const { return m_wasUserGesture; }
 
 private:
     double m_delay;
@@ -81,22 +84,22 @@ private:
     bool m_lockBackForwardList;
     bool m_wasDuringLoad;
     bool m_isLocationChange;
+    bool m_wasUserGesture;
 };
 
 class ScheduledURLNavigation : public ScheduledNavigation {
 public:
     ScheduledURLNavigation(double delay, const String& url, const String& referrer, bool lockHistory, bool lockBackForwardList, bool wasUserGesture, bool duringLoad, bool isLocationChange)
-        : ScheduledNavigation(delay, lockHistory, lockBackForwardList, duringLoad, isLocationChange)
+        : ScheduledNavigation(delay, lockHistory, lockBackForwardList, duringLoad, isLocationChange, wasUserGesture)
         , m_url(url)
         , m_referrer(referrer)
-        , m_wasUserGesture(wasUserGesture)
         , m_haveToldClient(false)
     {
     }
 
     virtual void fire(Frame* frame)
     {
-        frame->loader()->changeLocation(KURL(ParsedURLString, m_url), m_referrer, lockHistory(), lockBackForwardList(), m_wasUserGesture, false);
+        frame->loader()->changeLocation(KURL(ParsedURLString, m_url), m_referrer, lockHistory(), lockBackForwardList(), wasUserGesture(), false);
     }
 
     virtual void didStartTimer(Frame* frame, Timer<RedirectScheduler>* timer)
@@ -116,12 +119,10 @@ public:
 
     String url() const { return m_url; }
     String referrer() const { return m_referrer; }
-    bool wasUserGesture() const { return m_wasUserGesture; }
 
 private:
     String m_url;
     String m_referrer;
-    bool m_wasUserGesture;
     bool m_haveToldClient;
 };
 
@@ -152,14 +153,17 @@ public:
 
 class ScheduledHistoryNavigation : public ScheduledNavigation {
 public:
-    explicit ScheduledHistoryNavigation(int historySteps) : ScheduledNavigation(0, false, false, false, true), m_historySteps(historySteps) { }
+    explicit ScheduledHistoryNavigation(int historySteps, bool wasUserGesture) : ScheduledNavigation(0, false, false, false, true, wasUserGesture), m_historySteps(historySteps) { }
 
     virtual void fire(Frame* frame)
     {
+        UserGestureIndicator gestureIndicator(wasUserGesture() ? DefinitelyProcessingUserGesture : DefinitelyNotProcessingUserGesture);
+
         FrameLoader* loader = frame->loader();
         if (!m_historySteps) {
             // Special case for go(0) from a frame -> reload only the frame
-            loader->urlSelected(loader->url(), "", 0, lockHistory(), lockBackForwardList(), false, SendReferrer);
+            // To follow Firefox and IE's behavior, history reload can only navigate the self frame.
+            loader->urlSelected(loader->url(), "_self", 0, lockHistory(), lockBackForwardList(), wasUserGesture(), SendReferrer);
             return;
         }
         // go(i!=0) from a frame navigates into the history of the frame only,
@@ -174,17 +178,16 @@ private:
 class ScheduledFormSubmission : public ScheduledNavigation {
 public:
     ScheduledFormSubmission(PassRefPtr<FormSubmission> submission, bool lockBackForwardList, bool duringLoad, bool wasUserGesture)
-        : ScheduledNavigation(0, submission->lockHistory(), lockBackForwardList, duringLoad, true)
+        : ScheduledNavigation(0, submission->lockHistory(), lockBackForwardList, duringLoad, true, wasUserGesture)
         , m_submission(submission)
         , m_haveToldClient(false)
-        , m_wasUserGesture(wasUserGesture)
     {
         ASSERT(m_submission->state());
     }
 
     virtual void fire(Frame* frame)
     {
-        UserGestureIndicator gestureIndicator(m_wasUserGesture ? DefinitelyProcessingUserGesture : DefinitelyNotProcessingUserGesture);
+        UserGestureIndicator gestureIndicator(wasUserGesture() ? DefinitelyProcessingUserGesture : DefinitelyNotProcessingUserGesture);
 
         // The submitForm function will find a target frame before using the redirection timer.
         // Now that the timer has fired, we need to repeat the security check which normally is done when
@@ -215,7 +218,6 @@ public:
 private:
     RefPtr<FormSubmission> m_submission;
     bool m_haveToldClient;
-    bool m_wasUserGesture;
 };
 
 RedirectScheduler::RedirectScheduler(Frame* frame)
@@ -345,9 +347,9 @@ void RedirectScheduler::scheduleHistoryNavigation(int steps)
         cancel();
         return;
     }
-    
+
     // In all other cases, schedule the history traversal to occur asynchronously.
-    schedule(adoptPtr(new ScheduledHistoryNavigation(steps)));
+    schedule(adoptPtr(new ScheduledHistoryNavigation(steps, m_frame->loader()->isProcessingUserGesture())));
 }
 
 void RedirectScheduler::timerFired(Timer<RedirectScheduler>*)
