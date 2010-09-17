aa24889740c0e0c742d85c11abe5d242e4f912fd
mpcomplete@chromium.org <mpcomplete@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-17 21:57:37 +0000

    2010-09-13  Matt Perry  <mpcomplete@chromium.org>
            Reviewed by Darin Fisher.
    
            Have V8DOMWindowShell ask the embedder whether to run a V8 extension
            in a particular script context.
            https://bugs.webkit.org/show_bug.cgi?id=45721
    
            * bindings/v8/V8DOMWindowShell.cpp:
            (WebCore::V8DOMWindowShell::createNewContext):
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::registeredExtensionWithV8):
            (WebCore::V8Proxy::registerExtension):
            * bindings/v8/V8Proxy.h:
            * loader/EmptyClients.h:
            (WebCore::EmptyFrameLoaderClient::allowScriptExtension):
            * loader/FrameLoaderClient.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebFrameClient.h
+++ third_party/WebKit/WebKit/chromium/public/WebFrameClient.h
@@ -302,6 +302,9 @@ public:
     // scripts.
     virtual void didCreateIsolatedScriptContext(WebFrame*) { }
 
+    // Controls whether the given script extension should run in this frame.
+    virtual bool allowScriptExtension(WebFrame*, const WebString& extensionName, int extensionGroup) { return false; }
+
 
     // Geometry notifications ----------------------------------------------
 
