c08f409043a1d0982e3d398a1d3321ad2191ef7e
dumi@chromium.org <dumi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-17 04:56:00 +0000

    Remove WebHTTPBody::appendFileRange(..., const WebFileInfo&).
    Reviewed by Darin Fisher.
    
    * public/WebHTTPBody.h:
    * src/WebHTTPBody.cpp:
    (WebKit::WebHTTPBody::elementAt):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67693 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebHTTPBody.h
+++ third_party/WebKit/WebKit/chromium/public/WebHTTPBody.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebHTTPBody_h
 
 #include "WebData.h"
-#include "WebFileInfo.h"
 #include "WebNonCopyable.h"
 #include "WebString.h"
 #include "WebURL.h"
@@ -55,8 +54,6 @@ public:
         WebString filePath;
         long long fileStart;
         long long fileLength; // -1 means to the end of the file.
-        // FIXME: remove this field once all users of Element have been switched to use 'modificationTime'.
-        WebFileInfo fileInfo;
         double modificationTime;
         WebURL blobURL;
     };
@@ -89,8 +86,6 @@ public:
     WEBKIT_API void appendFile(const WebString&);
     // Passing -1 to fileLength means to the end of the file.
     WEBKIT_API void appendFileRange(const WebString&, long long fileStart, long long fileLength, double modificationTime);
-    // FIXME: Remove this method once all callers have been switched to use the method above.
-    WEBKIT_API void appendFileRange(const WebString&, long long fileStart, long long fileLength, const WebFileInfo&);
     WEBKIT_API void appendBlob(const WebURL&);
 
     // Identifies a particular form submission instance.  A value of 0 is
--- third_party/WebKit/WebKit/chromium/src/WebHTTPBody.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebHTTPBody.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebHTTPBody.h"
 
 #include "FormData.h"
-#include "WebFileInfo.h"
 
 using namespace WebCore;
 
@@ -79,8 +78,6 @@ bool WebHTTPBody::elementAt(size_t index, Element& result) const
     result.filePath.reset();
     result.fileStart = 0;
     result.fileLength = 0;
-    // FIXME: remove this line once all users of Element have been switched to use 'modificationTime'.
-    result.fileInfo.modificationTime = 0.0;
     result.modificationTime = 0.0;
     result.blobURL = KURL();
 
@@ -95,8 +92,6 @@ bool WebHTTPBody::elementAt(size_t index, Element& result) const
 #if ENABLE(BLOB)
         result.fileStart = element.m_fileStart;
         result.fileLength = element.m_fileLength;
-        // FIXME: remove this line once all users of Element have been switched to use 'modificationTime'.
-        result.fileInfo.modificationTime = element.m_expectedFileModificationTime;
         result.modificationTime = element.m_expectedFileModificationTime;
 #endif
         break;
@@ -134,12 +129,6 @@ void WebHTTPBody::appendFileRange(const WebString& filePath, long long fileStart
     ensureMutable();
     m_private->appendFileRange(filePath, fileStart, fileLength, modificationTime);
 #endif
-}
-
-// FIXME: Remove this method once all callers have been switched to use the method above.
-void WebHTTPBody::appendFileRange(const WebString& filePath, long long fileStart, long long fileLength, const WebFileInfo& fileInfo)
-{
-    return appendFileRange(filePath, fileStart, fileLength, fileInfo.modificationTime);
 }
 
 void WebHTTPBody::appendBlob(const WebURL& blobURL)
