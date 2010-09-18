c60405f2c7f0dd50c9df28fa6074ccefef6126e5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-18 22:48:44 +0000

    2010-09-18  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Andreas Kling.
    
            [BREWMP] Wrong key code type in keyIdentifierForBrewKeyCode
            https://bugs.webkit.org/show_bug.cgi?id=46005
    
            The type of key code in Brew MP is uint16. Take uint16 instead of int16 in keyIdentifierForBrewKeyCode.
    
            * platform/brew/PlatformKeyboardEventBrew.cpp:
            (WebCore::keyIdentifierForBrewKeyCode):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/brew/PlatformKeyboardEventBrew.cpp
+++ third_party/WebKit/WebCore/platform/brew/PlatformKeyboardEventBrew.cpp
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-static String keyIdentifierForBrewKeyCode(int16 keyCode)
+static String keyIdentifierForBrewKeyCode(uint16 keyCode)
 {
     switch (keyCode) {
     case AVK_LALT:
