ee0584c89656ecb5ad15f8d1b338a09e86bc03a7
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-18 10:04:02 +0000

    REGRESSION (r67762): Over 160 layout tests failing due to incorrect zoom factors in subframes https://bugs.webkit.org/show_bug.cgi?id=46031
    Reviewed by Eric Seidel.
    
    * page/Frame.cpp:
    (WebCore::parentPageZoomFactor): Return the zoom factor of the parent frame.
    (WebCore::parentTextZoomFactor): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67777 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Frame.cpp
+++ third_party/WebKit/WebCore/page/Frame.cpp
@@ -135,7 +135,7 @@ static inline float parentPageZoomFactor(Frame* frame)
     Frame* parent = frame->tree()->parent();
     if (!parent)
         return 1;
-    return frame->pageZoomFactor();
+    return parent->pageZoomFactor();
 }
 
 static inline float parentTextZoomFactor(Frame* frame)
@@ -143,7 +143,7 @@ static inline float parentTextZoomFactor(Frame* frame)
     Frame* parent = frame->tree()->parent();
     if (!parent)
         return 1;
-    return frame->textZoomFactor();
+    return parent->textZoomFactor();
 }
 
 inline Frame::Frame(Page* page, HTMLFrameOwnerElement* ownerElement, FrameLoaderClient* frameLoaderClient)
