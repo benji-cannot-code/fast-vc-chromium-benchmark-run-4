1f25532f42a080c94406e7a963f01db52be8c68a
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-18 01:00:10 +0000

    Build fix for platforms that compile Touch.
    * dom/Touch.cpp:
    (WebCore::contentsX):
    (WebCore::contentsY):
    Get zoom factor from Frame rather than FrameView.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67767 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Touch.cpp
+++ third_party/WebKit/WebCore/dom/Touch.cpp
@@ -41,7 +41,7 @@ static int contentsX(Frame* frame)
     FrameView* frameView = frame->view();
     if (!frameView)
         return 0;
-    return frameView->scrollX() / frameView->pageZoomFactor();
+    return frameView->scrollX() / frame->pageZoomFactor();
 }
 
 static int contentsY(Frame* frame)
@@ -51,11 +51,10 @@ static int contentsY(Frame* frame)
     FrameView* frameView = frame->view();
     if (!frameView)
         return 0;
-    return frameView->scrollY() / frameView->pageZoomFactor();
+    return frameView->scrollY() / frame->pageZoomFactor();
 }
 
-Touch::Touch(Frame* frame, EventTarget* target, unsigned identifier, 
-        int screenX, int screenY, int pageX, int pageY)
+Touch::Touch(Frame* frame, EventTarget* target, unsigned identifier, int screenX, int screenY, int pageX, int pageY)
     : m_target(target)
     , m_identifier(identifier)
     , m_clientX(pageX - contentsX(frame))
@@ -70,4 +69,3 @@ Touch::Touch(Frame* frame, EventTarget* target, unsigned identifier,
 } // namespace WebCore
 
 #endif
-
