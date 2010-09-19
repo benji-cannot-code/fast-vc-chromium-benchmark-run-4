085e4d893faac2a44c04c3901b89be235409e948
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-19 20:42:52 +0000

    Revert 59889 - Fix regression introduced by http://src.chromium.org/viewvc/chrome?view=rev&revision=57788
    Add a delegate interface so that chromium have more fine-grained control over
    whether a V8 extension is injected into a script context. This is the
    chromium-side change of webkit bug
    https://bugs.webkit.org/show_bug.cgi?id=45721
    
    BUG=37290
    TEST=covered by unit tests
    
    Review URL: http://codereview.chromium.org/3398001
    
    TBR=mpcomplete@chromium.org
    Review URL: http://codereview.chromium.org/3464003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59906 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_thread.h
+++ chrome/renderer/render_thread.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_RENDERER_RENDER_THREAD_H_
 #pragma once
 
-#include <map>
 #include <string>
 #include <vector>
 
@@ -60,10 +59,6 @@ struct ChannelHandle;
 
 namespace WebKit {
 class WebStorageEventDispatcher;
-}
-
-namespace v8 {
-class Extension;
 }
 
 // The RenderThreadBase is the minimal interface that a RenderView/Widget
@@ -237,13 +232,6 @@ class RenderThread : public RenderThreadBase,
   // on the renderer's main thread.
   scoped_refptr<base::MessageLoopProxy> GetFileThreadMessageLoopProxy();
 
-  // This function is called for every registered V8 extension each time a new
-  // script context is created. Returns true if the given V8 extension is
-  // allowed to run on the given URL and extension group.
-  bool AllowScriptExtension(const std::string& v8_extension_name,
-                            const GURL& url,
-                            int extension_group);
-
  private:
   virtual void OnControlMessageReceived(const IPC::Message& msg);
 
@@ -322,10 +310,6 @@ class RenderThread : public RenderThreadBase,
   // Schedule a call to IdleHandler with the given initial delay.
   void ScheduleIdleHandler(double initial_delay_s);
 
-  // Registers the given V8 extension with WebKit, and also tracks what pages
-  // it is allowed to run on.
-  void RegisterExtension(v8::Extension* extension, bool restrict_to_extensions);
-
   // These objects live solely on the render thread.
   scoped_ptr<ScopedRunnableMethodFactory<RenderThread> > task_factory_;
   scoped_ptr<VisitedLinkSlave> visited_link_slave_;
@@ -385,11 +369,6 @@ class RenderThread : public RenderThreadBase,
 
   // A lazily initiated thread on which file operations are run.
   scoped_ptr<base::Thread> file_thread_;
-
-  // Map of registered v8 extensions. The key is the extension name. The value
-  // is true if the extension should be restricted to extension-related
-  // contexts.
-  std::map<std::string, bool> v8_extensions_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderThread);
 };
