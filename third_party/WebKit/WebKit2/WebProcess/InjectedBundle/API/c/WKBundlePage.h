82c1fed061b409fe5d453a2ddfcdb62d0dc78e89
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-20 05:20:08 +0000

    Add WebKit2 equivalent of the WebFormDelegate's doCommandBySelector <rdar://problem/8377088> https://bugs.webkit.org/show_bug.cgi?id=46073
    Reviewed by Anders Carlsson.
    
    Add bundle client to match the behavior of:
      - (BOOL)textField:(DOMHTMLInputElement *)element doCommandBySelector:(SEL)commandSelector inFrame:(WebFrame *)frame;
    
    * WebProcess/InjectedBundle/API/c/WKBundlePage.h:
    * WebProcess/InjectedBundle/InjectedBundlePageFormClient.cpp:
    (WebKit::InjectedBundlePageFormClient::shouldPerformActionInTextField):
    * WebProcess/InjectedBundle/InjectedBundlePageFormClient.h:
    * WebProcess/WebCoreSupport/WebEditorClient.cpp:
    (WebKit::getActionTypeForKeyEvent):
    (WebKit::WebEditorClient::doTextFieldCommandFromEvent):
    (WebKit::WebEditorClient::textWillBeDeletedInTextField):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67836 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundlePage.h
+++ third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundlePage.h
@@ -29,6 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <WebKit2/WKBase.h>
 #include <WebKit2/WKBundleBase.h>
+
+#ifndef __cplusplus
+#include <stdbool.h>
+#endif
+
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -47,6 +52,17 @@ enum {
     kWKAffinityDownstream
 };
 typedef uint32_t WKAffinityType;
+
+enum {
+    WKInputFieldActionTypeMoveUp,
+    WKInputFieldActionTypeMoveDown,
+    WKInputFieldActionTypeCancel,
+    WKInputFieldActionTypeInsertTab,
+    WKInputFieldActionTypeInsertBacktab,
+    WKInputFieldActionTypeInsertNewline,
+    WKInputFieldActionTypeInsertDelete
+};
+typedef uint32_t WKInputFieldActionType;
 
 // Loader Client
 typedef void (*WKBundlePageDidStartProvisionalLoadForFrameCallback)(WKBundlePageRef page, WKBundleFrameRef frame, WKTypeRef* userData, const void *clientInfo);
@@ -145,6 +161,7 @@ typedef void (*WKBundlePageTextFieldDidBeginEditingCallback)(WKBundlePageRef pag
 typedef void (*WKBundlePageTextFieldDidEndEditingCallback)(WKBundlePageRef page, WKBundleNodeHandleRef htmlInputElementHandle, WKBundleFrameRef frame, const void* clientInfo);
 typedef void (*WKBundlePageTextDidChangeInTextFieldCallback)(WKBundlePageRef page, WKBundleNodeHandleRef htmlInputElementHandle, WKBundleFrameRef frame, const void* clientInfo);
 typedef void (*WKBundlePageTextDidChangeInTextAreaCallback)(WKBundlePageRef page, WKBundleNodeHandleRef htmlTextAreaElementHandle, WKBundleFrameRef frame, const void* clientInfo);
+typedef bool (*WKBundlePageShouldPerformActionInTextFieldCallback)(WKBundlePageRef page, WKBundleNodeHandleRef htmlInputElementHandle, WKInputFieldActionType actionType, WKBundleFrameRef frame, const void* clientInfo);
 typedef void (*WKBundlePageWillSubmitFormCallback)(WKBundlePageRef page, WKBundleNodeHandleRef htmlFormElementHandle, WKBundleFrameRef frame, WKBundleFrameRef sourceFrame, WKDictionaryRef values, WKTypeRef* userData, const void* clientInfo);
 
 struct WKBundlePageFormClient {
@@ -154,6 +171,7 @@ struct WKBundlePageFormClient {
     WKBundlePageTextFieldDidEndEditingCallback                          textFieldDidEndEditing;
     WKBundlePageTextDidChangeInTextFieldCallback                        textDidChangeInTextField;
     WKBundlePageTextDidChangeInTextAreaCallback                         textDidChangeInTextArea;
+    WKBundlePageShouldPerformActionInTextFieldCallback                  shouldPerformActionInTextField;
     WKBundlePageWillSubmitFormCallback                                  willSubmitForm;
 };
 typedef struct WKBundlePageFormClient WKBundlePageFormClient;
