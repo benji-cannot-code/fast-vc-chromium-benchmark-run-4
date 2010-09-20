cdf52393f8af6adf7e823bb4314ff15c7b9a57e4
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-20 16:12:58 +0000

    https://bugs.webkit.org/show_bug.cgi?id=46030, aintitcool.com doesn't paginate correctly when printed.
    Reviewed by Sam Weinig.
    
    This happens because the site always has a document width that will exceed the page width.
    We incorrectly apply a double scale instead of clipping after the first scale still doesn't fit.
    The fix for the issue is to cap the right layout overflow to the page width and to just clip out
    any additional excess.  This is the code in FrameView.cpp.
            
    This patch also cleans up table cell invalidation to reduce the # of relayouts.  This change is not
    a correctness fix.  It's just performance.
            
    I'm not sure how to write a test for this, since the double scale is an artifact of how WebKit mac calls
    back in when really printing.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::forceLayoutForPagination):
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::layoutBlockChild):
    (WebCore::RenderBlock::layoutPositionedObjects):
    (WebCore::RenderBlock::positionNewFloats):
    * rendering/RenderBlock.h:
    (WebCore::RenderBlock::markForPaginationRelayout):
    * rendering/RenderFlexibleBox.cpp:
    (WebCore::RenderFlexibleBox::layoutHorizontalBox):
    (WebCore::RenderFlexibleBox::layoutVerticalBox):
    * rendering/RenderTableSection.cpp:
    (WebCore::RenderTableSection::layoutRows):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67853 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderFlexibleBox.cpp
+++ third_party/WebKit/WebCore/rendering/RenderFlexibleBox.cpp
@@ -370,7 +370,7 @@ void RenderFlexibleBox::layoutHorizontalBox(bool relayoutChildren)
             if (!child->needsLayout() && paginated && view()->layoutState()->m_pageHeight) {
                 RenderBlock* childRenderBlock = child->isRenderBlock() ? toRenderBlock(child) : 0;
                 if (childRenderBlock && view()->layoutState()->pageY(child->y()) != childRenderBlock->pageY())
-                    childRenderBlock->setChildNeedsLayout(true, false);
+                    childRenderBlock->markForPaginationRelayout();
             }
 
             // Now do the layout.
@@ -444,7 +444,7 @@ void RenderFlexibleBox::layoutHorizontalBox(bool relayoutChildren)
             if (!child->needsLayout() && paginated && view()->layoutState()->m_pageHeight) {
                 RenderBlock* childRenderBlock = child->isRenderBlock() ? toRenderBlock(child) : 0;
                 if (childRenderBlock && view()->layoutState()->pageY(child->y()) != childRenderBlock->pageY())
-                    childRenderBlock->setChildNeedsLayout(true, false);
+                    childRenderBlock->markForPaginationRelayout();
             }
 
             child->layoutIfNeeded();
@@ -714,7 +714,7 @@ void RenderFlexibleBox::layoutVerticalBox(bool relayoutChildren)
             if (!child->needsLayout() && paginated && view()->layoutState()->m_pageHeight) {
                 RenderBlock* childRenderBlock = child->isRenderBlock() ? toRenderBlock(child) : 0;
                 if (childRenderBlock && view()->layoutState()->pageY(child->y()) != childRenderBlock->pageY())
-                    childRenderBlock->setChildNeedsLayout(true, false);
+                    childRenderBlock->markForPaginationRelayout();
             }
 
             // Now do a layout.
