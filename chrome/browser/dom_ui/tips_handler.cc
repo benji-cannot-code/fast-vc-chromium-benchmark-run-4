4bd51280650a0855fc6314302d52754154164d45
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-21 01:00:34 +0000

    Add the ability to change NTP logos using a command from the web resource server.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3418020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60001 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/tips_handler.cc
+++ chrome/browser/dom_ui/tips_handler.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 DOMMessageHandler* TipsHandler::Attach(DOMUI* dom_ui) {
   dom_ui_ = dom_ui;
   tips_cache_ = dom_ui_->GetProfile()->GetPrefs()->
-      GetMutableDictionary(prefs::kNTPTipsCache);
+      GetMutableDictionary(prefs::kNTPWebResourceCache);
   return DOMMessageHandler::Attach(dom_ui);
 }
 
@@ -46,8 +46,8 @@ void TipsHandler::HandleGetTips(const ListValue* args) {
   // We need to check here because the new tab page calls for tips before
   // the tip service starts up.
   PrefService* current_prefs = dom_ui_->GetProfile()->GetPrefs();
-  if (current_prefs->HasPrefPath(prefs::kNTPTipsServer)) {
-    std::string server = current_prefs->GetString(prefs::kNTPTipsServer);
+  if (current_prefs->HasPrefPath(prefs::kNTPWebResourceServer)) {
+    std::string server = current_prefs->GetString(prefs::kNTPWebResourceServer);
     std::string locale = g_browser_process->GetApplicationLocale();
     if (!EndsWith(server, locale, false)) {
       dom_ui_->CallJavascriptFunction(L"tips", list_value);
@@ -98,8 +98,8 @@ void TipsHandler::SendTip(const std::string& tip, const std::string& tip_type,
 
 // static
 void TipsHandler::RegisterUserPrefs(PrefService* prefs) {
-  prefs->RegisterDictionaryPref(prefs::kNTPTipsCache);
-  prefs->RegisterStringPref(prefs::kNTPTipsServer,
+  prefs->RegisterDictionaryPref(prefs::kNTPWebResourceCache);
+  prefs->RegisterStringPref(prefs::kNTPWebResourceServer,
                             WebResourceService::kDefaultResourceServer);
 }
 
