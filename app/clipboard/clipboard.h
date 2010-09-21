93092c8b3c3520fdcaa1ab7f0367e143ecc0f23d
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-21 18:33:54 +0000

    [GTK] fix wraparound error in clipboard.
    0U - 1 = pain
    
    BUG=56298
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/3394011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60079 0039d316-1c4b-4281-b951-d872f2087c98

--- app/clipboard/clipboard.h
+++ app/clipboard/clipboard.h
@@ -188,6 +188,7 @@ class Clipboard {
 
  private:
   FRIEND_TEST_ALL_PREFIXES(ClipboardTest, SharedBitmapTest);
+  FRIEND_TEST_ALL_PREFIXES(ClipboardTest, EmptyHTMLTest);
 
   void DispatchObject(ObjectType type, const ObjectMapParams& params);
 
