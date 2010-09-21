d05f73569e049c490e568043ab3dcc471b23791c
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-21 08:02:11 +0000

    2010-09-21  Adam Barth  <abarth@webkit.org>
            Unreviewed.
    
            [reviewtool] Publish button doesn't work
            https://bugs.webkit.org/show_bug.cgi?id=46168
    
            Darin's recent patch had a runtime error.  Sadly, we don't have any
            testing for this code.
    
            * code-review.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67931 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/BugsSite/code-review.js
+++ third_party/WebKit/BugsSite/code-review.js
@@ -476,6 +476,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   $('#post_comments').live('click', function() {
     fillInReviewForm();
-    review_form.find('form').submit();
+    $('#reviewform').contents().find('form').submit();
   });
 })();
