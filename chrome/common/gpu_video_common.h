75a604c1968753bfd8e0e322882479f2e1a9cfb3
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-21 23:20:40 +0000

    SendAllocateVideoFrames to the renderer
    This is separated from a previous patch due to link error on mac.
    
    BUG=53714
    TEST=Mac build bots are happy
    
    Review URL: http://codereview.chromium.org/3414011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60126 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/gpu_video_common.h
+++ chrome/common/gpu_video_common.h
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
 // Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -151,11 +152,8 @@ struct ParamTraits<GpuVideoDecoderFormatChangeParam> {
 };
 
 template <>
-struct ParamTraits<media::VideoFrame::Format> {
-  typedef media::VideoFrame::Format param_type;
-  static void Write(Message* m, const param_type& p);
-  static bool Read(const Message* m, void** iter, param_type* p);
-  static void Log(const param_type& p, std::string* l);
+struct SimilarTypeTraits<media::VideoFrame::Format> {
+  typedef int Type;
 };
 
 }  // namespace IPC
