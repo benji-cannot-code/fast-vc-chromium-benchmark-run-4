b327341401a3a1bbc0f97f3236cd6a6652d977d1
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-21 17:49:19 +0000

    https://bugs.webkit.org/show_bug.cgi?id=46196, add marginStart/Before/End/After accessors to RenderBoxModelObject.
    Reviewed by Dan Bernstein.
    
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::marginBefore):
    (WebCore::RenderBox::marginAfter):
    (WebCore::RenderBox::marginStart):
    (WebCore::RenderBox::marginEnd):
    * rendering/RenderBox.h:
    * rendering/RenderBoxModelObject.h:
    * rendering/RenderInline.cpp:
    (WebCore::computeMargin):
    (WebCore::RenderInline::marginLeft):
    (WebCore::RenderInline::marginRight):
    (WebCore::RenderInline::marginTop):
    (WebCore::RenderInline::marginBottom):
    (WebCore::RenderInline::marginStart):
    (WebCore::RenderInline::marginEnd):
    * rendering/RenderInline.h:
    (WebCore::RenderInline::marginBefore):
    (WebCore::RenderInline::marginAfter):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@67964 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderInline.h
+++ third_party/WebKit/WebCore/rendering/RenderInline.h
@@ -41,7 +41,13 @@ public:
 
     virtual int marginLeft() const;
     virtual int marginRight() const;
-    
+    virtual int marginTop() const;
+    virtual int marginBottom() const;
+    virtual int marginBefore() const { return 0; }
+    virtual int marginAfter() const { return 0; }
+    virtual int marginStart() const;
+    virtual int marginEnd() const;
+
     virtual void absoluteRects(Vector<IntRect>&, int tx, int ty);
     virtual void absoluteQuads(Vector<FloatQuad>&);
 
@@ -109,9 +115,6 @@ private:
     virtual int offsetWidth() const { return linesBoundingBox().width(); }
     virtual int offsetHeight() const { return linesBoundingBox().height(); }
 
-    // Just ignore top/bottom margins on RenderInlines.
-    virtual int marginTop() const { return 0; }
-    virtual int marginBottom() const { return 0; }
     virtual IntRect clippedOverflowRectForRepaint(RenderBoxModelObject* repaintContainer);
     virtual IntRect rectWithOutlineForRepaint(RenderBoxModelObject* repaintContainer, int outlineWidth);
     virtual void computeRectForRepaint(RenderBoxModelObject* repaintContainer, IntRect& rect, bool fixed);
