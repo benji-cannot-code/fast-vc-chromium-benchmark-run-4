a58993f7acb0a87642c2b45cbf3bbb76a48b79c4
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-22 00:00:29 +0000

    Revert r60126 that broke the build.
    TBR=scherkus
    
    BUG=53714
    TEST=
    
    Review URL: http://codereview.chromium.org/3393017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60130 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/gpu_video_common.h
+++ chrome/common/gpu_video_common.h
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-
 // Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -152,8 +151,11 @@ struct ParamTraits<GpuVideoDecoderFormatChangeParam> {
 };
 
 template <>
-struct SimilarTypeTraits<media::VideoFrame::Format> {
-  typedef int Type;
+struct ParamTraits<media::VideoFrame::Format> {
+  typedef media::VideoFrame::Format param_type;
+  static void Write(Message* m, const param_type& p);
+  static bool Read(const Message* m, void** iter, param_type* p);
+  static void Log(const param_type& p, std::string* l);
 };
 
 }  // namespace IPC
