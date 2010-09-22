ec4f53ab0a940586644098bf464caf62cf9a84b9
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-22 09:40:24 +0000

    Default IDR_MENU_DROPARROW is unsuitable for us because it is too opaque. Added IDR_MENU_DROPARROW_SHARP with more transparent background.
    BUG=http://crosbug.com/6568
    TEST=Manual
    Review URL: http://codereview.chromium.org/3479002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/new_user_view.cc
+++ chrome/browser/chromeos/login/new_user_view.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/login/helper.h"
 #include "chrome/browser/chromeos/login/rounded_rect_painter.h"
+#include "grit/app_resources.h"
 #include "grit/generated_resources.h"
 #include "views/controls/button/native_button.h"
 #include "views/controls/label.h"
@@ -136,6 +137,9 @@ void NewUserView::Init() {
   language_switch_menu_.InitLanguageMenu();
   languages_menubutton_ = new views::MenuButton(
       NULL, std::wstring(), &language_switch_menu_, true);
+  languages_menubutton_->set_menu_marker(
+      ResourceBundle::GetSharedInstance().GetBitmapNamed(
+          IDR_MENU_DROPARROW_SHARP));
   languages_menubutton_->SetFocusable(true);
   AddChildView(languages_menubutton_);
 
