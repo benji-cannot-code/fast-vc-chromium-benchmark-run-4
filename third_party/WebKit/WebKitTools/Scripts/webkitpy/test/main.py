f7d5b6dfba78cc1bb13263f9c4f8f499b9cb5939
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-22 20:44:10 +0000

    Make test-webkitpy test WebKit2's scripts
    These scripts can't be in WebKitTools due to limitations of Apple's
    build process. But that doesn't mean we can't test them!
    
    Fixes <http://webkit.org/b/46297> test-webkitpy should test code in
    WebKit2/Scripts
    
    Reviewed by Adam Barth.
    
    * Scripts/test-webkitpy:
    (_clean_packages_with_test): Renamed from _clean_webkitpy_with_test.
    Now takes an external_package_paths parameter and cleans both webkitpy
    and any external packages.
    (init): Added an external_package_paths parameter which we pass along
    to _clean_packages_with_test.
    (top level): Add WebKit2/Scripts/webkit2 as our only external package
    and pass it along to init and Tester.run_tests.
    
    * Scripts/webkitpy/test/main.py:
    (Tester.run_tests): Added an optional external_package_paths
    parameter. We modify sys.path so that the external packages can be
    imported, and search for unittest files inside all external packages
    in addition to inside webkitpy.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68080 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/test/main.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/test/main.py
@@ -79,7 +79,7 @@ class Tester(object):
 
         return modules
 
-    def run_tests(self, sys_argv):
+    def run_tests(self, sys_argv, external_package_paths=None):
         """Run the unit tests in all *_unittest.py modules in webkitpy.
 
         This method excludes "webkitpy.common.checkout.scm_unittest" unless
@@ -89,6 +89,11 @@ class Tester(object):
           sys_argv: A reference to sys.argv.
 
         """
+        if external_package_paths is None:
+            external_package_paths = []
+        else:
+            sys.path.extend(set(os.path.dirname(path) for path in external_package_paths))
+
         if len(sys_argv) > 1 and not sys_argv[-1].startswith("-"):
             # Then explicit modules or test names were provided, which
             # the unittest module is equipped to handle.
@@ -98,9 +103,9 @@ class Tester(object):
         # Otherwise, auto-detect all unit tests.
 
         webkitpy_dir = os.path.dirname(webkitpy.__file__)
-        unittest_paths = self._find_unittest_files(webkitpy_dir)
 
-        modules = self._modules_from_paths(webkitpy_dir, unittest_paths)
+        for path in [webkitpy_dir] + external_package_paths:
+            modules.extend(self._modules_from_paths(path, self._find_unittest_files(path)))
         modules.sort()
 
         # This is a sanity check to ensure that the unit-test discovery
