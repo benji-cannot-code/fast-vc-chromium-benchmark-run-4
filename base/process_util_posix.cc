752de79cabaaa2fec332db1487553d428d8687ca
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-22 17:28:30 +0000

    Cleanup orphaned testserver processes on posix.
    If a chrome test that uses a testserver were to crash, the testserver process ends up being orphaned, potentially affecting subsequent tests. This patch causes child processes of a test case to be killed along with the test case if it were to be abruptly terminated.
    
    BUG=55808
    TEST=sync_integration_tests
    
    Review URL: http://codereview.chromium.org/3423012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60192 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_posix.cc
+++ base/process_util_posix.cc
@@ -196,6 +196,13 @@ bool KillProcess(ProcessHandle process_id, int exit_code, bool wait) {
   return result;
 }
 
+bool KillProcessGroup(ProcessHandle process_group_id) {
+  bool result = kill(-1 * process_group_id, SIGKILL) == 0;
+  if (!result)
+    PLOG(ERROR) << "Unable to terminate process group " << process_group_id;
+  return result;
+}
+
 // A class to handle auto-closing of DIR*'s.
 class ScopedDIRClose {
  public:
@@ -421,12 +428,13 @@ char** AlterEnvironment(const environment_vector& changes,
   return ret;
 }
 
-bool LaunchApp(
+bool LaunchAppImpl(
     const std::vector<std::string>& argv,
     const environment_vector& env_changes,
     const file_handle_mapping_vector& fds_to_remap,
     bool wait,
-    ProcessHandle* process_handle) {
+    ProcessHandle* process_handle,
+    bool start_new_process_group) {
   pid_t pid;
   InjectiveMultimap fd_shuffle1, fd_shuffle2;
   fd_shuffle1.reserve(fds_to_remap.size());
@@ -440,6 +448,13 @@ bool LaunchApp(
 
   if (pid == 0) {
     // Child process
+
+    if (start_new_process_group) {
+      // Instead of inheriting the process group ID of the parent, the child
+      // starts off a new process group with pgid equal to its process ID.
+      if (setpgid(0, 0) < 0)
+        return false;
+    }
 #if defined(OS_MACOSX)
     RestoreDefaultExceptionHandler();
 #endif
@@ -497,6 +512,26 @@ bool LaunchApp(
   }
 
   return true;
+}
+
+bool LaunchApp(
+    const std::vector<std::string>& argv,
+    const environment_vector& env_changes,
+    const file_handle_mapping_vector& fds_to_remap,
+    bool wait,
+    ProcessHandle* process_handle) {
+  return LaunchAppImpl(argv, env_changes, fds_to_remap,
+                       wait, process_handle, false);
+}
+
+bool LaunchAppInNewProcessGroup(
+    const std::vector<std::string>& argv,
+    const environment_vector& env_changes,
+    const file_handle_mapping_vector& fds_to_remap,
+    bool wait,
+    ProcessHandle* process_handle) {
+  return LaunchAppImpl(argv, env_changes, fds_to_remap, wait,
+                       process_handle, true);
 }
 
 bool LaunchApp(const std::vector<std::string>& argv,
