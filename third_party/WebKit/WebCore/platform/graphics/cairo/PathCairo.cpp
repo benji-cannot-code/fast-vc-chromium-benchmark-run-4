03414d14389e903f362dfe299c1d8d9c81b13d03
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-22 16:25:25 +0000

    2010-09-22  Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] r68027 caused many crashes on the bots
            https://bugs.webkit.org/show_bug.cgi?id=46268
    
            Add the appropriate OwnPtrCairo.h include which has the OwnPtr
            specialization for Cairo paths. Without it, we will have memory
            corruption.
    
            * platform/graphics/cairo/PathCairo.cpp: Add OwnPtrCairo.h include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cairo/PathCairo.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cairo/PathCairo.cpp
@@ -30,9 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CairoPath.h"
 #include "FloatRect.h"
 #include "GraphicsContext.h"
+#include "OwnPtrCairo.h"
 #include "PlatformString.h"
 #include "StrokeStyleApplier.h"
-
 #include <cairo.h>
 #include <math.h>
 #include <wtf/MathExtras.h>
