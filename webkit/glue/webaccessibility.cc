040a3c921c46545088ae9e9ca37458817264ede5
ctguil@chromium.org <ctguil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-23 05:45:13 +0000

    Store lower case tag names within web accessibility.
    This is similar to the behavior of firefox.
    
    BUG=none
    TEST=browser_tests:RendererAccessibilityBrowserTest.TestCrossPlatformAccessibilityTree
    TEST=interactive_ui_tests:AccessibilityWinBrowserTest.*
    
    Review URL: http://codereview.chromium.org/3435019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60259 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webaccessibility.cc
+++ webkit/glue/webaccessibility.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/glue/webaccessibility.h"
 
+#include "base/string_util.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebAccessibilityCache.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebAccessibilityObject.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebAccessibilityRole.h"
@@ -303,7 +304,10 @@ void WebAccessibility::Init(const WebKit::WebAccessibilityObject& src,
 
   if (!node.isNull() && node.isElementNode()) {
     WebKit::WebElement element = node.to<WebKit::WebElement>();
-    attributes[ATTR_HTML_TAG] = element.tagName();
+    // TODO(ctguil): The tagName in WebKit is lower cased but
+    // HTMLElement::nodeName calls localNameUpper. Consider adding
+    // a WebElement method that returns the original lower cased tagName.
+    attributes[ATTR_HTML_TAG] = StringToLowerASCII(string16(element.tagName()));
     for (unsigned i = 0; i < element.attributes().length(); i++) {
       html_attributes.push_back(
           std::pair<string16, string16>(
