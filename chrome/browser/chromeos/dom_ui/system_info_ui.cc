90141359e8e88a3937193d95cb131f263f9c0e9a
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-23 20:30:32 +0000

    chromium-os:6108 - about:system should use FILE thread to get system info. Replaced SyslogsLibrary::GetSyslogs with asynchronous RequestSyslogs. Modified BugReportHandler and AboutSource in browser_about_handler.cc appropriately.
    BUG=http://code.google.com/p/chromium-os/issues/detail?id=6108
    TEST=Test chrome:system, and bug reporter. Both should always be responsive. Test multiple instances of each, closing tabs quickly, and sending a bug quickly. Veryify results of bug report. Confirm that about:system no longer shows anything.
    
    Review URL: http://codereview.chromium.org/3295024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60338 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dom_ui/system_info_ui.cc
+++ chrome/browser/chromeos/dom_ui/system_info_ui.cc
@@ -47,6 +47,14 @@ class SystemInfoUIHTMLSource : public ChromeURLDataManager::DataSource {
  private:
   ~SystemInfoUIHTMLSource() {}
 
+  void SyslogsComplete(chromeos::LogDictionaryType* sys_info);
+
+  CancelableRequestConsumer consumer_;
+
+  // Stored data from StartDataRequest()
+  std::string path_;
+  int request_id_;
+
   DISALLOW_COPY_AND_ASSIGN(SystemInfoUIHTMLSource);
 };
 
@@ -72,12 +80,29 @@ class SystemInfoHandler : public DOMMessageHandler,
 ////////////////////////////////////////////////////////////////////////////////
 
 SystemInfoUIHTMLSource::SystemInfoUIHTMLSource()
-    : DataSource(chrome::kChromeUISystemInfoHost, MessageLoop::current()) {
+    : DataSource(chrome::kChromeUISystemInfoHost, MessageLoop::current()),
+      request_id_(0) {
 }
 
 void SystemInfoUIHTMLSource::StartDataRequest(const std::string& path,
                                               bool is_off_the_record,
                                               int request_id) {
+  path_ = path;
+  request_id_ = request_id;
+
+  chromeos::SyslogsLibrary* syslogs_lib =
+      chromeos::CrosLibrary::Get()->GetSyslogsLibrary();
+  if (syslogs_lib) {
+    FilePath* tmpfilename = NULL;  // use default filepath.
+    syslogs_lib->RequestSyslogs(
+        tmpfilename,
+        &consumer_,
+        NewCallback(this, &SystemInfoUIHTMLSource::SyslogsComplete));
+  }
+}
+
+void SystemInfoUIHTMLSource::SyslogsComplete(
+    chromeos::LogDictionaryType* sys_info) {
   DictionaryValue strings;
   strings.SetString("title", l10n_util::GetStringUTF16(IDS_ABOUT_SYS_TITLE));
   strings.SetString("description",
@@ -94,24 +119,19 @@ void SystemInfoUIHTMLSource::StartDataRequest(const std::string& path,
                     l10n_util::GetStringUTF16(IDS_ABOUT_SYS_COLLAPSE));
   SetFontAndTextDirection(&strings);
 
-  chromeos::SyslogsLibrary* syslogs_lib =
-      chromeos::CrosLibrary::Get()->GetSyslogsLibrary();
-  scoped_ptr<chromeos::LogDictionaryType> sys_info;
-  if (syslogs_lib)
-    sys_info.reset(syslogs_lib->GetSyslogs(new FilePath()));
-  if (sys_info.get()) {
+  if (sys_info) {
      ListValue* details = new ListValue();
      strings.Set("details", details);
      chromeos::LogDictionaryType::iterator it;
-     for (it = sys_info.get()->begin(); it != sys_info.get()->end(); ++it) {
+     for (it = sys_info->begin(); it != sys_info->end(); ++it) {
        DictionaryValue* val = new DictionaryValue;
        val->SetString("stat_name", it->first);
        val->SetString("stat_value", it->second);
        details->Append(val);
      }
-     strings.SetString("anchor", path);
+     strings.SetString("anchor", path_);
+     delete sys_info;
   }
-
   static const base::StringPiece systeminfo_html(
       ResourceBundle::GetSharedInstance().GetRawDataResource(
           IDR_ABOUT_SYS_HTML));
@@ -122,7 +142,7 @@ void SystemInfoUIHTMLSource::StartDataRequest(const std::string& path,
   html_bytes->data.resize(full_html.size());
   std::copy(full_html.begin(), full_html.end(), html_bytes->data.begin());
 
-  SendResponse(request_id, html_bytes);
+  SendResponse(request_id_, html_bytes);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
