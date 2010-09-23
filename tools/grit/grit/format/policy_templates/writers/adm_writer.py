5ab0ddf79017b9db5cd6de4d9374ef2ae4fc5c6d
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-23 11:09:33 +0000

    Moving "Writer" related code for processing the policy tree from policy_generator.py to the template_writer.py.
    This is the first CL in a couple of refactoring steps.
    
    BUG=56512
    TEST=Current tests need to pass
    
    Review URL: http://codereview.chromium.org/3496001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60276 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/grit/grit/format/policy_templates/writers/adm_writer.py
+++ tools/grit/grit/format/policy_templates/writers/adm_writer.py
@@ -169,7 +169,7 @@ class AdmWriter(template_writer.TemplateWriter):
       self._PrintLine('END CATEGORY', -1)
       self._PrintLine('', -1)
 
-  def Prepare(self):
+  def Init(self):
     self.policy_list = []
     self.str_list = ['[Strings]']
     self.indent = ''
